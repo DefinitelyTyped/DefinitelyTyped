8d563caa07ad46fdb0c7db8614632a79cec14be7
Boris Yankov <borisyankov@gmail.com>
2012-10-09 19:21:17 +0300

    Mustache.js definitions and tests added

---
+++ Tests/mustache-tests.ts
@@ -0,0 +1,8 @@
+/// <reference path="../Definitions/mustache.d.ts" />
+
+var view = { title: "Joe", calc: function () { return 2 + 4; } };
+var output = Mustache.render("{{title}} spends {{calc}}", view);
+
+var person;
+var template = "<h1>{{firstName}} {{lastName}}</h1>Blog: {{blogURL}}";
+var html = Mustache.to_html(template, person);
