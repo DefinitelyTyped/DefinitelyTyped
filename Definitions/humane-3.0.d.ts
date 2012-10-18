71ba3fb959e9839c755d3f643c1a91a979187cba
Boris Yankov <borisyankov@gmail.com>
2012-10-18 15:15:12 +0300

    Updates/fixes to Humane.js

--- Definitions/humane-3.0.d.ts
+++ Definitions/humane-3.0.d.ts
@@ -1,21 +1,38 @@
+// Type definitions for Humane 3.0
+// Project: http://wavded.github.com/humane-js/
+// Definitions by: https://github.com/jmvrbanac 
+// DefinitelyTyped: https://github.com/borisyankov/DefinitelyTyped
+
 interface HumaneOptions {
-	timeout:number;
-	clickToClose:Boolean;
-	baseCls:String;
-	addnCls:String;
+    queue?: string[];
+    baseCls?: string;
+    addnCls?: string;
+    timeout?: number;
+    waitForMove?: bool;
+    clickToClose?: bool;
+    forceNew?: bool;
 }
 
 interface Humane {
-	create(options?:HumaneOptions):Humane;
-	info:Function;
-	error:Function;
-	spawn(options:HumaneOptions):Function;
-	remove();
-	log(message:String):Humane;
-	log(message:String, callback:Function):Humane;
-	log(message:String, options:HumaneOptions):Humane;
-	log(message:String, callback:Function, options:HumaneOptions):Humane;
-	
-	log(listOfMessages:Array):Humane;
+    queue: string[];
+    baseCls: string;
+    addnCls: string;
+    timeout: number;
+    waitForMove: bool;
+    clickToClose: bool;
+    forceNew: bool;
+
+    create(options?: HumaneOptions): Humane;
+    info: Function;
+    error: Function;
+    spawn(options: HumaneOptions): Function;
+    remove(any): void;
+    log(message: string): Humane;
+    log(message: string, callback: Function): Humane;
+    log(message: string, options: HumaneOptions): Humane;
+    log(message: string, callback: Function, options: HumaneOptions): Humane;
+
+    log(listOfMessages: Array): Humane;
 }
-declare var humane:Humane;
+
+declare var humane: Humane;
