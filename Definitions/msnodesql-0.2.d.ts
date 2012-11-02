2204bd3bd8621e9732b0111b6baacace30981945
Boris Yankov <borisyankov@gmail.com>
2012-11-02 22:12:26 +0200

    Update msnodesql definitions

--- Definitions/msnodesql-0.2.d.ts
+++ Definitions/msnodesql-0.2.d.ts
@@ -10,13 +10,35 @@ declare module "msnodesql" {
     export function open(connectionString: string, callback: Function): Connection;
 
     export function query(connectionString: string, query: string): StreamEvents;
-    export function query(connectionString: string, query: string, callback: Function): StreamEvents;
-    export function query(connectionString: string, query: string, params, callback: Function): StreamEvents;
+    export function query(connectionString: string, query: string, callback: Callback): StreamEvents;
+    export function query(connectionString: string, query: string, params, callback: Callback): StreamEvents;
 
     export function queryRaw(connectionString: string, query: string): StreamEvents;
-    export function queryRaw(connectionString: string, query: string, callback: Function): StreamEvents;
-    export function queryRaw(connectionString: string, query: string, params, callback: Function): StreamEvents;
+    export function queryRaw(connectionString: string, query: string, callback: Callback): StreamEvents;
+    export function queryRaw(connectionString: string, query: string, params, callback: Callback): StreamEvents;
 
-    interface Connection { }
-    interface StreamEvents extends EventEmitter  { }
+    interface Callback {
+        (err: Error, results: any[]): void;
+    }
+
+    interface Errback {
+        (err: Error): void;
+    }
+
+    interface Connection {
+        queryRaw(connectionString: string, query: string): StreamEvents;
+        queryRaw(connectionString: string, query: string, callback: Callback): StreamEvents;
+        queryRaw(connectionString: string, query: string, params, callback: Callback): StreamEvents;
+
+        query(connectionString: string, query: string): StreamEvents;
+        query(connectionString: string, query: string, callback: Callback): StreamEvents;
+        query(connectionString: string, query: string, params, callback: Callback): StreamEvents;
+
+        beginTransaction(callback?: Errback);
+        commit(callback?: Errback);
+        rollback(callback?: Errback);
+        close(callback?: Errback);
+    }
+
+    interface StreamEvents extends EventEmitter { }
 }
