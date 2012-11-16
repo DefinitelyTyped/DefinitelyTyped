584a5f313ba1b68329b686f3e1d9799e3ba9f117
Boris Yankov <borisyankov@gmail.com>
2012-11-16 19:14:15 +0200

    Update jQuery.Validation definitions and tests

--- Definitions/jquery.validation-1.10.d.ts
+++ Definitions/jquery.validation-1.10.d.ts
@@ -3,8 +3,61 @@
 // Definitions by: https://github.com/fdecampredon
 // Definitions: https://github.com/borisyankov/DefinitelyTyped
 
+
 /// <reference path="jquery-1.8.d.ts"/>
 
+interface ValidationOptions {
+    debug?: bool;
+    submitHandler?: Function;
+    invalidHandler?: Function;
+    ignore?: any;
+    rules?: any;
+    messages?: any;
+    groups?: any;
+    onsubmit?: bool;
+    onfocusout?: bool;
+    onkeyup?: bool;
+    onclick?: bool;
+    focusInvalid?: bool;
+    focusCleanup?: bool;
+    meta?: string;
+    errorClass?: string;
+    validClass?: string;
+    errorElement?: string;
+    wrapper?: string;
+    errorLabelContainer?: any;
+    errorContainer?: any;
+    showErrors?: Function;
+    errorPlacement?: Function;
+    success?: any;
+    highlight?: Function;
+    unhighlight?: Function;
+    ignoreTitle?: bool;
+}
+
+interface Validator {
+    format(template: string, ...arguments: string[]): string;
+    form(): bool;
+    element(element: any): bool;
+    resetForm(): void;
+    showErrors(errors: any): void;
+    numberOfInvalids(): number;
+    setDefaults(defaults: ValidationOptions): void;
+    addMethod(name: string, method: (value: any, element: any, ...params: any[]) => any, message?: any): void;
+    addClassRules(rules: any): void;
+    addClassRules(name: string, rules: any): void;
+}
+
+interface JQuery {
+    validate(options?: ValidationOptions): Validator;
+    valid(): bool;
+    rules(): any;
+    rules(methodName: string): any;
+    rules(methodName: string, rules: any): any;
+    removeAttrs(attributes: string): any;
+}
+
 interface JQueryStatic {
-    validate();
+    format(template: string, ...arguments: string[]): string;
+    validator: Validator;
 }
