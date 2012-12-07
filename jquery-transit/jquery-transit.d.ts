cdc21f9c81ca7a3426cad414bc5917a57454d370
unknown <bsears@LP-BSEARS.ARRS.ARRISI.com>
2012-12-07 08:27:00 -0800

    New definitions for jquery-transit

---
+++ jquery-transit/jquery-transit.d.ts
@@ -0,0 +1,25 @@
+// Type definitions for jQuery-transit.js
+
+/// <reference path="../jquery/jquery-1.8.d.ts"/>
+
+// Transit //////////////////////////////////////////////////
+
+interface JQueryTransitOptions {
+	opacity?: number;
+	duration?: number;
+	delay?: number;
+	easing?: string;
+	complete?: () => void;
+	scale?: any;
+}
+
+////////////////////////////////////////////////////////////////////////////////////////////////////
+
+interface JQuery {
+    transition(options: JQueryTransitOptions): JQuery;
+    transition(options: JQueryTransitOptions, duration: number): JQuery;
+    transition(options: JQueryTransitOptions, easing: string): JQuery;
+    transition(options: JQueryTransitOptions, duration: number, easing: string): JQuery;
+    transition(options: JQueryTransitOptions, complete: () => void): JQuery;
+    transition(options: JQueryTransitOptions, duration: number, easing: string, complete: () => void): JQuery;
+}
