e30a04c0b137bdd3c42265b5558645fb775a5ca2
Emil Ivanov <emil.vladev@gmail.com>
2012-12-12 11:02:44 +0200

    Added comment headers and version

--- sockjs/sockjs-0.3.d.ts
+++ sockjs/sockjs-0.3.d.ts
@@ -1,3 +1,8 @@
+// Type definitions for SockJS 0.3.x
+// Project: https://github.com/sockjs/sockjs-client
+// Definitions by: Emil Ivanov <https://github.com/vladev>
+// DefinitelyTyped: https://github.com/borisyankov/DefinitelyTyped
+
 interface SockJSSimpleEvent {
     type: string;
     toString(): string;
