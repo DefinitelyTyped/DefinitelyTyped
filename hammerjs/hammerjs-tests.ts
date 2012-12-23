dda4be94b3ceb1a78d3bd8503935d7892097ffa1
Boris Yankov <borisyankov@gmail.com>
2012-12-23 18:05:19 +0200

    Add hammer.js

---
+++ hammerjs/hammerjs-tests.ts
@@ -0,0 +1,32 @@
+/// <reference path="../jquery/jquery-1.8.d.ts"/>
+/// <reference path="hammerjs-0.6.d.ts" />
+
+var hammer = new Hammer(document.getElementById("container"));
+hammer.ondragstart = function (ev) { };
+hammer.ondrag = function (ev) { };
+hammer.ondragend = function (ev) { };
+hammer.onswipe = function (ev) { };
+
+hammer.ontap = function (ev) { };
+hammer.ondoubletap = function (ev) { };
+hammer.onhold = function (ev) { };
+
+hammer.ontransformstart = function (ev) { };
+hammer.ontransform = function (ev) { };
+hammer.ontransformend = function (ev) { };
+
+hammer.onrelease = function (ev) { };
+
+$("#element")
+   .hammer({
+       // Options
+   })
+   .bind("tap", function (ev) {
+       console.log(ev);
+   });
+
+$("#container").hammer({
+    prevent_default: false,
+    drag_vertical: false
+}).bind("hold tap doubletap transformstart transform transformend dragstart drag dragend release swipe", function (ev) {
+});
