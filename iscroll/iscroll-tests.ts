5556aba3f729626f23f36b5baa3aa6dfb63c833c
Boris Yankov <borisyankov@gmail.com>
2013-01-09 09:08:22 +0200

    Add iScroll tests

---
+++ iscroll/iscroll-tests.ts
@@ -0,0 +1,19 @@
+/// <reference path="iscroll-4.2.d.ts" />
+
+var myScroll1 = new iScroll('wrapper');
+var myScroll2 = new iScroll('wrapper', { hScrollbar: false, vScrollbar: false });
+var myScroll3= new iScroll('wrapper', {
+    snap: true,
+    momentum: false,
+    hScrollbar: false,
+    vScrollbar: false
+});
+var myScroll4 = new iScroll('wrapper', {
+    snap: 'li',
+    momentum: false,
+    hScrollbar: false,
+    vScrollbar: false
+});
+var myScroll6 = new iScroll('wrapper', { scrollbarClass: 'myScrollbar' });
+
+myScroll1.refresh();
