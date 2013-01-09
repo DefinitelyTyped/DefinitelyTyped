45cc7cfeae948ea103df87c997b44cb272c88e26
Boris Yankov <borisyankov@gmail.com>
2013-01-09 11:30:35 +0200

    Update jQuery.Watermark, add tests

---
+++ jquery.watermark/jquery.watermark-tests.ts
@@ -0,0 +1,28 @@
+/// <reference path="../jquery/jquery-1.8.d.ts"/>
+/// <reference path="jquery.watermark-3.1.d.ts"/>
+
+
+$('#inputId').watermark('Required information');
+$('#inputId').watermark('Required information', { className: 'myClassName' });
+$('#inputId').watermark('Search', { useNative: false });
+
+$.watermark.options.className = 'myClass';
+
+$.watermark.options.useNative = false;
+
+var myFunction;
+$.watermark.options.useNative = myFunction;
+
+$.watermark.options.hideBeforeUnload = false;
+
+$.watermark.options = {
+    className: 'myClass',
+    useNative: false,
+    hideBeforeUnload: false
+};
+
+$.watermark.options.hideBeforeUnload = true;
+$.watermark.show('input.optional');
+$.watermark.hide('#myInput');
+$.watermark.showAll();
+$.watermark.hideAll();
