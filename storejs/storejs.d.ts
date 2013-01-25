a17c41099923a8ed23ba0683ef2c040b3b3dcbae
Vincent Bortone <vbortone@gmail.com>
2013-01-25 02:23:03 -0500

    Creation of store.js type definition and test files

---
+++ storejs/storejs.d.ts
@@ -0,0 +1,5 @@
+// Type definitions for store.js
+// store.js exposes a simple API for cross browser local storage
+// Project: https://github.com/marcuswestin/store.js/
+// Definitions by: Vincent Bortone <https://github.com/vbortone/>
+// Definitions: https://github.com/borisyankov/DefinitelyTyped
