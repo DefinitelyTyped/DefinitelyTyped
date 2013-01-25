1cc495a27d84c6bb883161774a9e85f42466b149
Vincent Bortone <vbortone@gmail.com>
2013-01-25 12:49:33 -0500

    Added JSONFormatter interface and tests

--- jsoneditoronline/jsoneditoronline-tests.ts
+++ jsoneditoronline/jsoneditoronline-tests.ts
@@ -16,4 +16,20 @@ var json:JSON = {
 editor.set(json);
 editor.expandAll();
 
-var json2:JSON = editor.get(json);
+var jsonResult:JSON = editor.get(json);
+
+var options2: JSONformatterOptions = {
+    "indentation": 2
+};
+var formatter:JSONFormatter = new JSONFormatter(container, options);
+var json2:JSON = {
+    "Array": [1, 2, 3],
+    "Boolean": true, 
+    "Null": null, 
+    "Number": 123, 
+    "Object": {"a": "b", "c": "d"},
+    "String": "Hello World"
+};
+formatter.set(json2);
+
+var jsonResult2:JSON = formatter.get(json2);
