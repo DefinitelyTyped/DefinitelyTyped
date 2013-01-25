77b48303843916511b6b993686df81e2f438efae
Vincent Bortone <vbortone@gmail.com>
2013-01-25 02:53:31 -0500

    Finish code and tests.

--- storejs/storejs.d.ts
+++ storejs/storejs.d.ts
@@ -3,3 +3,18 @@
 // Project: https://github.com/marcuswestin/store.js/
 // Definitions by: Vincent Bortone <https://github.com/vbortone/>
 // Definitions: https://github.com/borisyankov/DefinitelyTyped
+
+interface StoreJSStatic {
+	set(key: string, value: any): any;
+	get(key: string): any;
+	remove(key: string): void;
+	clear(): void;
+	enabled: bool;
+	disabled: bool;
+	transact(key: string, defaultValue: any, transactionFn?: (val: any) => void): void;
+	getAll(): any;
+	serialize(value: any): string;
+	deserialize(value: string): any;
+}
+
+declare var store: StoreJSStatic;
