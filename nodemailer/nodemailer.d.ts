859b94e5e5ef2cc2d5a56e76f5d2ee1f2f1dfbb9
Vincent Bortone <vbortone@gmail.com>
2013-02-04 01:21:57 -0500

    Finish first attempt of definitions and tests.

--- nodemailer/nodemailer.d.ts
+++ nodemailer/nodemailer.d.ts
@@ -1,61 +1,89 @@
 // Type definitions for Nodemailer
-// JNodemailer is an easy to use module to send e-mails with Node.JS (using SMTP or sendmail or Amazon SES) and is unicode friendly .
+// Nodemailer is an easy to use module to send e-mails with Node.JS (using SMTP or sendmail or Amazon SES) and is unicode friendly .
 // Project: https://github.com/andris9/Nodemailer
 // Definitions by: Vincent Bortone <https://github.com/vbortone/>
 // Definitions: https://github.com/borisyankov/DefinitelyTyped
 
 class Transport {
 	static transports: {
-		SMTP: SMTPTransport,
-    	SES: SESTransport,
-    	SENDMAIL: SendmailTransport,
-    	STUB: StubTransport
+		SMTP: Transport;
+		SES: Transport;
+		SENDMAIL: Transport;
+		STUB: Transport;
 	};
 
-	constructor(type: string, options?: Object);
+	constructor(type: string, options?: any);
 	options: Object;
-	transportType: strng;
+	transportType: string;
 	sendMailWithTransport(emailMessage: MailComposer, callback?: (err: Error) => any): any;
 	useDKIM(dkim: DKIMOptions);
-	close(callback?: (err: Error) => any));
+	close(callback?: (err: Error) => any);
+	sendMail(message: MailComposer, callback?: (err: Error) => any): any;
+	send_mail(message:MailComposer, callback?: (err: Error) => any): any;
 }
 
-interface MailComposer {}
+interface NodeMailerAttachment {
+	fileName: string;
+	filePath?: string;
+	contents?: any;
+	contentType?: string;
+	cid?: string;
+}
 
-interface DKIMOptions{}
+interface MailComposer {	
+	from: string; // sender info
+	to: string;   // Comma separated list of recipients
+	subject: string; // Subject of the message
+	headers?: {};
+	text?: string;  // plaintext body
+	html?: string;  // HTML body
+	attachments?: NodeMailerAttachment[];  // An array of attachments
+	forceEmbeddedImages?: bool;
+}
 
-interface XOAuthGenerator {}
+interface DKIMOptions{
+	domainName: string; // signing domain
+	keySelector: string; // selector name (in this case there's a dkim._domainkey.do-not-trust.node.ee TXT record set up)
+	privateKey: any;
+}
+
+class XOAuthGenerator {
+	constructor(options: XOAuthGeneratorOptions);
+	generate(callback: () => any): string;
+}
+
+interface XOAuthGeneratorOptions {
+	user: string;
+	consumerKey: string; // optional
+	consumerSecret: string; // optional
+	token: string;
+	tokenSecret: string;
+}
+
+interface XOAuth2Options {
+	user: string;
+	clientId: string;
+	clientSecret: string;
+	refreshToken: string;
+}
 
 interface NodemailerTransportOptions {
-	service: string;
-	
-}
-
-interface NodeMailerMessageOptions {}
-
-// Module level exports
-interface NodemailerStatic {
-	X_MAILER_NAME: string;
-	X_MAILER_HOMEPAGE: string;
-	createXOAuthGenerator(options: Object): XOAuthGenerator;
-	createTransport(type: string, options: NodeMailerTransportOptions): Transport;
-	sendMail(options: NodeMailerMessageOptions, callback?: (err: Error) => any): any;
-	send_mail(options: NodeMailerMessageOptions, callback?: (err: Error) => any): any;
-	stripHTML(str: string): string;
+	service?: string;
+	auth?: {
+		user?: string;
+		pass?: string;
+		XOAuthToken?: XOAuthGenerator;
+		XOAuth2?: XOAuth2Options;
+	};
+	debug?: bool;
+	AWSAccessKeyID?: string;
+	AWSSecretKey: string;
+	ServiceUrl: string;
 }
 
 interface Nodemailer {
-	(options: NodeMailerMessageOptions): void;
-	transport: Transport;
-	options: NodeMailerMessageOptions;
-	mailcomposer: MailComposer;
-	generateUserAgentString(): string;
-	getGlobalTransport(): Transport;
-	validateSettings(callback: (err: Error) => any): void;
-	sendMail(callback: () => any): void;
-	generateMailObject(): void;
-	setGeneralOptions(): void;
-	setUserHeaders(): void;
-	setModuleHeaders(): void;
-	setAttachments(): void;
+	createTransport(type: string): Transport;
+	createTransport(type: string, options: NodemailerTransportOptions): Transport;
+	createTransport(type: string, path: string): Transport;
+	createXOAuthGenerator(options: XOAuthGeneratorOptions): XOAuthGenerator;
 }
