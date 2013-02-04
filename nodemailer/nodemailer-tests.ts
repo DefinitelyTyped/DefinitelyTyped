f0e6ab88b96612c7761e044553dea66eb32f6268
Vincent Bortone <vbortone@gmail.com>
2013-02-04 01:21:57 -0500

    Finish first attempt of definitions and tests.

--- nodemailer/nodemailer-tests.ts
+++ nodemailer/nodemailer-tests.ts
@@ -1,33 +1,83 @@
-//Nodemailer test
+/// <reference path="..\node\node.d.ts" />
 /// <reference path="nodemailer.d.ts" />
-var nodemailer:NodemailerStatic;
 
-// create reusable transport method (opens pool of SMTP connections)
-var smtpTransport = nodemailer.createTransport("SMTP",{
-    service: "Gmail",
-    auth: {
-        user: "gmail.user@gmail.com",
-        pass: "userpass"
-    }
+var nodemailer: Nodemailer;
+var fs: any;
+var pathlib: any;
+
+// Create an Amazon SES transport object
+var transport:Transport = nodemailer.createTransport("SES", {
+    AWSAccessKeyID: "AWSACCESSKEY",
+    AWSSecretKey: "/AWS/SECRET",
+    ServiceUrl: "https://email.us-east-1.amazonaws.com" // optional
 });
 
-// setup e-mail data with unicode symbols
-var mailOptions = {
-    from: "Fred Foo ✔ <foo@blurdybloop.com>", // sender address
-    to: "bar@blurdybloop.com, baz@blurdybloop.com", // list of receivers
-    subject: "Hello ✔", // Subject line
-    text: "Hello world ✔", // plaintext body
-    html: "<b>Hello world ✔</b>" // html body
-}
+console.log('SES Configured');
 
-// send mail with defined transport object
-smtpTransport.sendMail(mailOptions, function(error, response){
-    if(error){
-        console.log(error);
-    }else{
-        console.log("Message sent: " + response.message);
+// optional DKIM signing
+
+transport.useDKIM({
+    domainName: "do-not-trust.node.ee", // signing domain
+    keySelector: "dkim", // selector name (in this case there's a dkim._domainkey.do-not-trust.node.ee TXT record set up)
+    privateKey: fs.readFileSync(pathlib.join(__dirname,"test_private.pem"))
+});
+
+
+// Message object
+var message:MailComposer = {
+
+    // sender info
+    from: 'Sender Name <sender@example.com>',
+
+    // Comma separated list of recipients
+    to: '"Receiver Name" <receiver@example.com>',
+
+    // Subject of the message
+    subject: 'Nodemailer is unicode friendly ✔', //
+
+    // plaintext body
+    text: 'Hello to myself!',
+
+    // HTML body
+    html: '<p><b>Hello</b> to myself <img src="cid:note@node"/></p>' +
+         '<p>Here\'s a nyan cat for you as an embedded attachment:<br/><img src="cid:nyan@node"/></p>',
+
+    // An array of attachments
+    attachments: [
+
+        // String attachment
+        {
+            fileName: 'notes.txt',
+            contents: 'Some notes about this e-mail',
+            contentType: 'text/plain' // optional, would be detected from the filename
+        },
+
+        // Binary Buffer attachment
+        {
+            fileName: 'image.png',
+            contents: new Buffer('iVBORw0KGgoAAAANSUhEUgAAABAAAAAQAQMAAAAlPW0iAAAABlBMVEUAAAD/' +
+                                 '//+l2Z/dAAAAM0lEQVR4nGP4/5/h/1+G/58ZDrAz3D/McH8yw83NDDeNGe4U' +
+                                 'g9C9zwz3gVLMDA/A6P9/AFGGFyjOXZtQAAAAAElFTkSuQmCC', 'base64'),
+
+            cid: 'note@node' // should be as unique as possible
+        },
+
+        // File Stream attachment
+        {
+            fileName: 'nyancat.gif',
+            filePath: __dirname + "/nyan.gif",
+            cid: 'nyan@node' // should be as unique as possible
+        }
+    ]
+};
+
+console.log('Sending Mail');
+
+transport.sendMail(message, function (error) {
+    if (error) {
+        console.log('Error occured');
+        console.log(error.message);
+        return;
     }
-
-    // if you don't want to use this transport object anymore, uncomment following line
-    //smtpTransport.close(); // shut down the connection pool, no more messages
+    console.log('Message sent successfully!');
 });
