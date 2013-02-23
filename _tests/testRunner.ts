fd1ae1e4fdb420efef81838a1407d3522115c348
Diullei Gomes <diullei@gmail.com>
2013-02-23 18:51:16 -0300

    CI integration with travis-CI

--- _tests/testRunner.ts
+++ _tests/testRunner.ts
@@ -89,7 +89,7 @@ function runTests(testFiles) {
                 total_failure++;
             }
 
-            console.log('  [\033[35m' + lib + '\033[0m] ' + testFiles[testIndex].substr(cfg.root.length)
+            console.log('  [\033[36m' + lib + '\033[0m] ' + testFiles[testIndex].substr(cfg.root.length)
                 + ' - ' + (testFile.errors.length == 0 ? '\033[32msuccess\033[0m' : '\033[31mfailure\033[0m'));
 
             var test = new Test(lib);
@@ -119,7 +119,7 @@ function runTests(testFiles) {
                     var test = new Test(attr);
                     tests.tests.push(test);
 
-                    console.log('  [\033[35m' + attr + '\033[0m] without tests');
+                    console.log('  [\033[36m' + attr + '\033[0m] without tests');
                     withoutTestsCount++;
                 }
 
@@ -127,9 +127,11 @@ function runTests(testFiles) {
                     + ' tests. '
                     + '\033[32m' + total_success + ' tests success\033[0m, '
                     + '\033[31m' + total_failure + ' tests failed\033[0m and '
-                    + '\033[35m' + withoutTestsCount + ' definitions without tests\033[0m.\n');
+                    + withoutTestsCount + ' definitions without tests.\n');
 
-                if (total_failure > 0) process.exit(0);
+                if (total_failure > 0) {
+                    process.exit(1);
+                }
             }
         });
 }
