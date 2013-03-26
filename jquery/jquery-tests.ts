b6e21d422ebbbfd7e31552bb0eb72eec1ae65a54
Diullei Gomes <diullei@gmail.com>
2013-03-26 10:18:02 -0300

    jquery: parseHTML(...) added.  #421

--- jquery/jquery-tests.ts
+++ jquery/jquery-tests.ts
@@ -2258,3 +2258,25 @@ function test_addBack() {
     // Second Example
     $("div.after-addback").find("p").addBack().addClass("background");
 }
+
+// http://api.jquery.com/jQuery.parseHTML/
+function test_parseHTML() {
+	var $log = $( "#log" ),
+		str = "hello, <b>my name is</b> jQuery.",
+		html = $.parseHTML( str ),
+		nodeNames = [];
+	 
+	// Append the parsed HTML
+	$log.append( html );
+	 
+	// Gather the parsed HTML's node names
+	$.each( html, function( i, el ) {
+		nodeNames[i] = "<li>" + el.nodeName + "</li>";
+	});
+	 
+	// Insert the node names
+	$log.append( "<h3>Node Names:</h3>" );
+	$( "<ol></ol>" )
+	  .append( nodeNames.join( "" ) )
+	  .appendTo( $log );
+}
