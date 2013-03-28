4738eca9a204bc1d86d3881d08836fca9fae51c0
Christiaan Rakowski <cs.rakowski@gmail.com>
2013-03-28 12:02:06 +0100

    Added jquery tinyscrollbar (inititial version)

---
+++ jquery.tinyscrollbar/jquery.tinyscrollbar.d.ts
@@ -0,0 +1,32 @@
+﻿// Type definitions for jQuery tinyscrollbar 1.8
+// Project: http://baijs.nl/tinyscrollbar/
+// Definitions by: Christiaan Rakowski <https://github.com/csrakowski/>
+// Definitions: https://github.com/borisyankov/DefinitelyTyped
+
+/// <reference path="../jquery/jquery.d.ts" />
+
+interface JQueryTinyScrollbar extends JQuery {
+    tinyscrollbar(): JQuery;
+    tinyscrollbar_update(): JQuery;
+}
+
+
+interface JQueryTinyScrollbarOptions {
+    invertscroll?: bool;
+    axis?: string;
+	wheel?: number;
+	scroll?: bool;
+	lockscroll?: bool;
+	size?: any; //string or number
+	sizethumb?: any; //string or number	
+}
+
+interface JQueryTinyScrollbarStatic {   
+
+    tinyscrollbar(options: JQueryTinyScrollbarOptions): JQueryTinyScrollbar;
+    tinyscrollbar_update(options:any): JQueryTinyScrollbar;
+}
+
+interface JQuery extends JQueryTinyScrollbarStatic {
+    
+}
