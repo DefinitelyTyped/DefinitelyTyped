4cf381e5b05ebb4ac9c7a21ebc64c9b3b09c76aa
Andrew Gaspar <andrew.james.gaspar@gmail.com>
2013-03-31 05:21:04 -0500

    Initial commit for azure node.js typescript definitions.

--- azure/azure-tests.ts
+++ azure/azure-tests.ts
@@ -1,6 +1,13 @@
 ﻿/// <reference path="azure.d.ts" />
 
 
-import azure = module("azure");
+import Azure = module("azure");
 
-var ts = new azure.TableService("asdf");
+var ts = new Azure.TableService();
+
+ts.createTable("babbsies", function (error, table) {
+    ts.insertEntity(table.TableName, { PartitionKey: "Midwest", RowKey: "1", Name: "Tommy", Color: "Blue" },
+        function () {
+            ts.deleteEntity(table.TableName, { PartitionKey: "Midwest", RowKey: "1" });
+        });
+});
