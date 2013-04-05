9a34358f5a82a2a9c6687be0e468a6a67b60a436
Kazi Manzur Rashid <kazimanzurrashid@gmail.com>
2013-04-05 09:22:58 +0600

    Included Mocha definition.

---
+++ mocha/mocha.d.ts
@@ -0,0 +1,24 @@
+declare var describe : {
+    (description: string, spec: () => void): void;
+    only(description: string, spec: () => void): void;
+    skip(description: string, spec: () => void): void;
+    timeout(ms: number);
+}
+
+declare var it: {
+    (expectation: string, assertion?: () => void): void;
+    (expectation: string, assertion?: (done: () => void) => void): void;
+    only(expectation: string, assertion?: () => void): void;
+    only(expectation: string, assertion?: (done: () => void) => void): void;
+    skip(expectation: string, assertion?: () => void): void;
+    skip(expectation: string, assertion?: (done: () => void) => void): void;
+    timeout(ms: number);
+};
+
+declare function beforeEach(action: () => void): void;
+
+declare function beforeEach(action: (done: () => void) => void): void;
+
+declare function afterEach(action: () => void): void;
+
+declare function afterEach(action: (done: () => void) => void): void;
