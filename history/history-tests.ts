85f1e0c3584d66bf025c677736415a240030fad5
Boris Yankov <borisyankov@gmail.com>
2013-05-25 18:35:52 +0300

    Fix History.js definitions and tests

--- history/history-tests.ts
+++ history/history-tests.ts
@@ -1,17 +1,19 @@
 /// <reference path="history.d.ts" />
 
-if (!History.enabled) {
-    return false;
+function tests() {
+    if (!Historyjs.enabled) {
+        return false;
+    }
+
+    Historyjs.Adapter.bind(window, 'statechange', () => {
+        var State = Historyjs.getState();
+        Historyjs.log(State.data, State.title, State.url);
+    });
+
+    Historyjs.pushState({ state: 1 }, "State 1", "?state=1");
+    Historyjs.pushState({ state: 2 }, "State 2", "?state=2");
+    Historyjs.replaceState({ state: 3 }, "State 3", "?state=3");
+    Historyjs.pushState(null, null, "?state=4");
+    Historyjs.back();
+    Historyjs.go(2);
 }
-
-History.Adapter.bind(window, 'statechange', () => {
-    var State = History.getState();
-    History.log(State.data, State.title, State.url);
-});
-
-History.pushState({ state: 1 }, "State 1", "?state=1");
-History.pushState({ state: 2 }, "State 2", "?state=2");
-History.replaceState({ state: 3 }, "State 3", "?state=3");
-History.pushState(null, null, "?state=4");
-History.back();
-History.go(2);
