0c9bbcdba1844f020c0b13a5e4ab7eafea1f1fe7
Boris Yankov <borisyankov@gmail.com>
2013-05-25 18:42:10 +0300

    Improve slightly jsDeferred

--- jsdeferred/jsdeferred.d.ts
+++ jsdeferred/jsdeferred.d.ts
@@ -4,95 +4,72 @@
 // Definitions: https://github.com/borisyankov/DefinitelyTyped
 
 interface Loop {
-  begin:number;
-  end:number;
-  step:number;
+    begin: number;
+    end: number;
+    step: number;
 }
 
 interface ConnectOption {
-  target:any;
-  args?:any[];
-  ok?:number;
-  ng?:number;
+    target: any;
+    args?: any[];
+    ok?: number;
+    ng?: number;
 }
 
 interface RetryOption {
-  wait:number;
+    wait: number;
 }
 
-interface DeferredizedFunction { (...arg:any[]):Deferred; }
-interface DeferredizedFunctionWithNumber { (n:number):Deferred; }
-interface FunctionWithNumber { (i:number); }
-interface ErrorCallback { (d:Deferred, ...args:any[]); }
+interface DeferredizedFunction { (...arg: any[]): Deferred; }
+interface DeferredizedFunctionWithNumber { (n: number): Deferred; }
+interface FunctionWithNumber { (i: number); }
+interface ErrorCallback { (d: Deferred, ...args: any[]); }
 
 declare class Deferred {
 
-  static methods:string[];
+    static methods: string[];
+    static isDeferred(obj: any): bool;
+    static next(fun: Function): Deferred;
+    static chain(...args: any[]): Deferred;
+    static wait(n: number): Deferred;
+    static call(fun?: Function, ...args: any[]): Deferred;
+    static parallel(dl: any): Deferred;
+    static earlier(dl: any): Deferred;
 
-  static isDeferred(obj:any):bool;
+    static loop(n: number, fun: FunctionWithNumber): Deferred;
+    static loop(n: Loop, fun: FunctionWithNumber): Deferred;
 
-  static next(fun:Function):Deferred;
+    static repeat(n: number, fun: FunctionWithNumber): Deferred;
+    static register(name: string, fun: DeferredizedFunction): void;
 
-  static chain(...args:any[]):Deferred;
+    static connect(funo: any, options: string): DeferredizedFunction;
+    static connect(funo: Function, options?: ConnectOption): DeferredizedFunction;
 
-  static wait(n:number):Deferred;
+    static retry(retryCount: number, funcDeferred: DeferredizedFunctionWithNumber, options?: RetryOption): Deferred;
+    static define(obj?: any, list?: string[]): any;
 
-  static call(fun?:Function, ...args:any[]):Deferred;
+    constructor();
 
-  static parallel(dl:any):Deferred;
+    next(fun: Function): Deferred;
+    wait(n: number): Deferred;
+    error(fun: ErrorCallback): Deferred;
+    call(val?: any): Deferred;
+    fail(err: any): Deferred;
+    cancel(): Deferred;
+    parallel(dl: any): Deferred;
 
-  static earlier(dl:any):Deferred;
-
-  static loop(n:number, fun:FunctionWithNumber):Deferred;
-
-  static loop(n:Loop, fun:FunctionWithNumber):Deferred;
-
-  static repeat(n:number, fun:FunctionWithNumber):Deferred;
-
-  static register(name:string, fun:DeferredizedFunction):void;
-
-  static connect(funo:any, options:string):DeferredizedFunction;
-
-  static connect(funo:Function, options?:ConnectOption):DeferredizedFunction;
-
-  static retry(retryCount:number, funcDeferred:DeferredizedFunctionWithNumber, options?:RetryOption):Deferred;
-
-  static define(obj?:any, list?:string[]):any;
-
-  constructor();
-
-  next(fun:Function):Deferred;
-
-  wait(n:number):Deferred;
-
-  error(fun:ErrorCallback):Deferred;
-
-  call(val?:any):Deferred;
-
-  fail(err:any):Deferred;
-
-  cancel():Deferred;
-
-  parallel(dl:any):Deferred;
-
-  loop(n:number, fun:FunctionWithNumber):Deferred;
-
-  loop(n:Loop, fun:FunctionWithNumber):Deferred;
-
+    loop(n: number, fun: FunctionWithNumber): Deferred;
+    loop(n: Loop, fun: FunctionWithNumber): Deferred;
 }
 
 
-declare function chain(...args:any[]):Deferred;
+declare function chain(...args: any[]): Deferred;
+declare function wait(n: number): Deferred;
+declare function call(fun?: Function, ...args: any[]): Deferred;
+declare function parallel(dl: any): Deferred;
+declare function earlier(dl: any): Deferred;
 
-declare function wait(n:number):Deferred;
+declare function loop(n: number, fun: FunctionWithNumber): Deferred;
+declare function loop(n: Loop, fun: FunctionWithNumber): Deferred;
 
-declare function call(fun?:Function, ...args:any[]):Deferred;
-
-declare function parallel(dl:any):Deferred;
-
-declare function earlier(dl:any):Deferred;
-
-declare function loop(n:number, fun:FunctionWithNumber):Deferred;
-declare function loop(n:Loop, fun:FunctionWithNumber):Deferred;
-
-declare function repeat(n:number, fun:FunctionWithNumber):Deferred;
+declare function repeat(n: number, fun: FunctionWithNumber): Deferred;
