a75e524aeb4a16835943122770f51ce2ee5c450c
Basarat Syed <bas@picnicsoftware.com>
2013-06-20 16:42:58 +1000

    chai working with TS0.9

--- chai/chai.d.ts
+++ chai/chai.d.ts
@@ -3,120 +3,120 @@
 // Definitions by: Kazi Manzur Rashid <https://github.com/kazimanzurrashid/>
 // DefinitelyTyped: https://github.com/borisyankov/DefinitelyTyped
 
-declare module chai {
-    interface Equality {
-        (expected: any, message?: string): bool;
-    }
+interface Equality {
+    (expected: any, message?: string): bool;
+}
 
-    interface Property {
-        (name: string, value?: any, message?: string): bool;
-    }
+interface Property {
+    (name: string, value?: any, message?: string): bool;
+}
 
-    interface NumberComparer {
-        (value: number, message?: string): bool;
-    }
+interface NumberComparer {
+    (value: number, message?: string): bool;
+}
 
-    interface Eql {
-        (value: any, message?: string): bool;
-    }
+interface Eql {
+    (value: any, message?: string): bool;
+}
 
-    interface Include {
-        (value: Object, message?: string): bool;
-        (value: string, message?: string): bool;
-        (value: number, message?: string): bool;
-        keys(...names: string[]): bool;
-    }
+interface Include {
+    (value: Object, message?: string): bool;
+    (value: string, message?: string): bool;
+    (value: number, message?: string): bool;
+    keys(...names: string[]): bool;
+}
 
-    interface Throw {
-        (constructor: Error, message?: string);
-        (expected: string, message?: string);
-        (expected: RegExp, message?: string);
-    }
+interface Throw {
+    (constructor: Error, message?: string);
+    (expected: string, message?: string);
+    (expected: RegExp, message?: string);
+}
 
-    interface TypeComparison { 
-        (type: string, message?: string): bool;
-         instanceof(type: Object): bool;
-    }
+interface TypeComparison { 
+    (type: string, message?: string): bool;
+        instanceof(type: Object): bool;
+}
 
-    interface NumericComparison {
-        above: NumberComparer;
-        gt: NumberComparer;
-        greaterThan: NumberComparer;
+interface NumericComparison {
+    above: NumberComparer;
+    gt: NumberComparer;
+    greaterThan: NumberComparer;
 
-        below: NumberComparer;
-        lt: NumberComparer;
-        lessThan: NumberComparer;
-    }
+    below: NumberComparer;
+    lt: NumberComparer;
+    lessThan: NumberComparer;
+}
 
-    interface Length extends NumericComparison {
-        (value: number, message?: string): bool;
-    }
+interface Length extends NumericComparison {
+    (value: number, message?: string): bool;
+}
 
-    interface Deep {
-        equal: Equality;
-        property: Property;
-    }
+interface Deep {
+    equal: Equality;
+    property: Property;
+}
 
-    interface Have {
-        property: Property;
-        deep: Deep;
-        length: Length;
-        ownProperty(name: string, message?: string): bool;
-        string(value: string, message?: string): bool;
-        keys(...values: string[]): bool;
-    }
+interface Have {
+    property: Property;
+    deep: Deep;
+    length: Length;
+    ownProperty(name: string, message?: string): bool;
+    string(value: string, message?: string): bool;
+    keys(...values: string[]): bool;
+}
 
-    interface At {
-        least(value: number, message?: string): bool;
-        gte(value: number, message?: string): bool;
-        most(value: number, message?: string): bool;
-        lte(value: number, message?: string): bool;
-    }
+interface At {
+    least(value: number, message?: string): bool;
+    gte(value: number, message?: string): bool;
+    most(value: number, message?: string): bool;
+    lte(value: number, message?: string): bool;
+}
 
-    interface Be extends NumericComparison {
-        ok: bool;
-        true: bool;
-        false: bool;
-        null: bool;
-        undefined: bool;
-        empty: bool;
-        arguments: bool;
-        an: TypeComparison;
-        at: At;
+interface Be extends NumericComparison {
+    ok: bool;
+    true: bool;
+    false: bool;
+    null: bool;
+    undefined: bool;
+    empty: bool;
+    arguments: bool;
+    an: TypeComparison;
+    at: At;
 
-        a(type: string, message?: string): bool;
-        within(start: number, finish: number, message?: string): bool;
-        closeTo(expected: number, delta: number, message?: string): bool;
-    }
+    a(type: string, message?: string): bool;
+    within(start: number, finish: number, message?: string): bool;
+    closeTo(expected: number, delta: number, message?: string): bool;
+}
 
-    interface To {
-        be: Be;
-        not: To;
-        deep: Deep;
-        have: Have;
+interface To {
+    be: Be;
+    not: To;
+    deep: Deep;
+    have: Have;
 
-        exist: bool;
+    exist: bool;
 
-        equal: Equality;
+    equal: Equality;
 
-        include: Include;
-        contain: Include;
-        throw: Throw;
+    include: Include;
+    contain: Include;
+    throw: Throw;
 
-        eql: Eql;
-        eqls: Eql;
+    eql: Eql;
+    eqls: Eql;
 
-        match(value: RegExp, message?: string): bool;
+    match(value: RegExp, message?: string): bool;
 
-        respondTo(method: string, message?: string);
-        satisfy(matcher: Function, message?: string);
-    }
+    respondTo(method: string, message?: string);
+    satisfy(matcher: Function, message?: string);
+}
 
-    interface ExpectMatchers {
-        to: To;
-    }
+interface ExpectMatchers {
+    to: To;
+}
 
-    var expect : {
+interface chai{
+    expect: {
         (target: any): ExpectMatchers;
     }
 }
