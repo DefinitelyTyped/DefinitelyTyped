7d29352c70a79e59faf6eb22511e5eba00b8a4fb
Nicholas Wolverson <nicholas.wolverson@gmail.com>
2013-06-24 09:28:01 +0100

    Simplify optional overloads

--- knockout/knockout.d.ts
+++ knockout/knockout.d.ts
@@ -79,8 +79,7 @@ interface KnockoutObservableArrayStatic {
 
     fn: KnockoutObservableArrayFunctions<any>;
 
-    <T>(): KnockoutObservableArray<T>;
-    <T>(value: T[]): KnockoutObservableArray<T>;
+    <T>(value?: T[]): KnockoutObservableArray<T>;
 }
 
 interface KnockoutObservableArray<T> extends KnockoutObservableArrayFunctions<T> {
@@ -95,7 +94,6 @@ interface KnockoutObservableStatic {
     fn: KnockoutObservableFunctions;
 
     <T>(value?: T): KnockoutObservable<T>;
-    <T>(): KnockoutObservable<T>;
 }
 
 /** use as method to get/set the value */
