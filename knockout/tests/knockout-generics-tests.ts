86bcf40365f1cf42d7d358f0ef0fd7838a8b2b9a
Thomas Butler <thomas@butlersoftwarellc.com>
2013-07-03 20:46:31 -0400

    More tests (currently failing)
    These are failing right now but they should work.

--- knockout/tests/knockout-generics-tests.ts
+++ knockout/tests/knockout-generics-tests.ts
@@ -2,7 +2,7 @@
 
 
 interface MyType { }
-var data: MyType;
+var myData: MyType;
 
 function test_KnockoutSubscribable() {
 
@@ -12,9 +12,56 @@ function test_KnockoutSubscribable() {
     
     var subscription: KnockoutSubscription = subscribable.subscribe((val: MyType) => { });
     
-    subscribable.notifySubscribers(data);
-    subscribable.notifySubscribers(data, 'someevent');
+    subscribable.notifySubscribers(myData);
+    subscribable.notifySubscribers(myData, 'someevent');
 
     subscription.dispose();
     
+}
+
+function test_KnockoutObservable() {
+    
+    //works when unexplicit
+    var obsAny: KnockoutObservable<any> = ko.observable();
+
+    //type inference works
+    var obs: KnockoutObservable<MyType> = ko.observable(myData);
+
+    //inherits properly
+    var sub: KnockoutSubscribable<MyType> = obs;
+
+    //returns correct type
+    var result:MyType = obs();
+
+}
+
+function test_KnockoutObservableArray() {
+
+    //works when unexplicit
+    var obsArrayAny: KnockoutObservableArray<any> = ko.observableArray();
+
+    //type inference works
+    var obsArray: KnockoutObservableArray<MyType> = ko.observableArray([myData]);
+
+    //inherits properly
+    var obs: KnockoutObservable<MyType[]> = obsArray;
+    var sub: KnockoutSubscribable<MyType[]> = obsArray;
+
+    //returns correct type
+    var result:MyType[] = obsArray();
+
+}
+
+function test_KnockoutComputed() {
+
+    //type inference works
+    var computed: KnockoutComputed<MyType> = ko.computed(()=>myData);
+
+    //inherits properly
+    var obs: KnockoutObservable<MyType> = computed;
+    var sub: KnockoutSubscribable<MyType> = computed;
+
+    //returns correct type
+    var result: MyType = computed();
+
 }
