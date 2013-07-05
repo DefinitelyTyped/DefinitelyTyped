cf7e8502bcf08f960197abb556108823271813a2
Carl de Billy <carl.debilly@gmail.com>
2013-07-05 01:03:30 -0400

    Support for more RxJS packages + bool => boolean for TS0.9

--- rx.js/rx.js.html.d.ts
+++
@@ -1,8 +0,0 @@
-﻿///<reference path="rx.js.d.ts"/>
-
-declare module Rx {
-	interface Observable {
-		fromEvent<TEvent>(element: HTMLElement, eventName: string): IObservable<TEvent>;
-		fromEvent<TEvent>(document: HTMLDocument, eventName: string): IObservable<TEvent>;
-	}
-}
