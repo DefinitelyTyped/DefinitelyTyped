b0b20fe6b152c9f050c57098324f4d8b5fca0f1d
Marc-Andre Roy <marc596@gmail.com>
2013-07-17 10:48:04 -0400

    Update version number to 2.3

--- knockout/knockout.d.ts
+++ knockout/knockout.d.ts
@@ -1,4 +1,4 @@
-// Type definitions for Knockout 2.2
+// Type definitions for Knockout 2.3
 // Project: http://knockoutjs.com
 // Definitions by: Boris Yankov <https://github.com/borisyankov/>
 // Definitions: https://github.com/borisyankov/DefinitelyTyped
