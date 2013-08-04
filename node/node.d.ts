cb702d1bbaf01d4f9bb0fe74fa8cac545c5acca6
basarat <basaratali@gmail.com>
2013-08-04 16:07:49 +1000

    path.resolve
    It can take a single "to" parameter : http://nodejs.org/docs/latest/api/path.html#path_path_resolve_from_to and in fact that is *the* way you get "Absolute paths" in node

--- node/node.d.ts
+++ node/node.d.ts
@@ -808,6 +808,7 @@ declare module "fs" {
 declare module "path" {
     export function normalize(p: string): string;
     export function join(...paths: any[]): string;
+    export function resolve(to: string);
     export function resolve(from: string, to: string): string;
     export function resolve(from: string, from2: string, to: string): string;
     export function resolve(from: string, from2: string, from3: string, to: string): string;
