8f5946ef2ca9e697bdd019da1ffb0eac62508b58
Boris Yankov <borisyankov@gmail.com>
2013-08-07 20:01:06 +0300

    Fix threejs tests

--- threejs/three-tests.ts
+++ threejs/three-tests.ts
@@ -8375,9 +8375,9 @@ var container, stats;
 
             var item = table[ i ];
 
-            var object = new THREE.Object3D();
-            object.position.x = ( item[ 3 ] * 160 ) - 1540;
-            object.position.y = - ( item[ 4 ] * 200 ) + 1100;
+            var object2 = new THREE.Object3D();
+            object2.position.x = ( item[ 3 ] * 160 ) - 1540;
+            object2.position.y = - ( item[ 4 ] * 200 ) + 1100;
 
             targets.table.push( object );
 
@@ -8392,17 +8392,17 @@ var container, stats;
             var phi = Math.acos( -1 + ( 2 * i ) / l );
             var theta = Math.sqrt( l * Math.PI ) * phi;
 
-            var object = new THREE.Object3D();
+            var object3 = new THREE.Object3D();
 
-            object.position.x = 1000 * Math.cos( theta ) * Math.sin( phi );
-            object.position.y = 1000 * Math.sin( theta ) * Math.sin( phi );
-            object.position.z = 1000 * Math.cos( phi );
+            object3.position.x = 1000 * Math.cos( theta ) * Math.sin( phi );
+            object3.position.y = 1000 * Math.sin( theta ) * Math.sin( phi );
+            object3.position.z = 1000 * Math.cos( phi );
 
-            vector.copy( object.position ).multiplyScalar( 2 );
+            vector.copy( object3.position ).multiplyScalar( 2 );
 
-            object.lookAt( vector );
+            object3.lookAt( vector );
 
-            targets.sphere.push( object );
+            targets.sphere.push( object3 );
 
         }
 
@@ -8414,19 +8414,19 @@ var container, stats;
 
             var phi = i * 0.175 + Math.PI;
 
-            var object = new THREE.Object3D();
+            var object4 = new THREE.Object3D();
 
-            object.position.x = 1100 * Math.sin( phi );
-            object.position.y = - ( i * 8 ) + 450;
-            object.position.z = 1100 * Math.cos( phi );
+            object4.position.x = 1100 * Math.sin( phi );
+            object4.position.y = - ( i * 8 ) + 450;
+            object4.position.z = 1100 * Math.cos( phi );
 
-            vector.copy( object.position );
+            vector.copy( object4.position );
             vector.x *= 2;
             vector.z *= 2;
 
-            object.lookAt( vector );
+            object4.lookAt( vector );
 
-            targets.helix.push( object );
+            targets.helix.push( object4 );
 
         }
 
@@ -8434,13 +8434,13 @@ var container, stats;
 
         for ( var i = 0; i < objects.length; i ++ ) {
 
-            var object = new THREE.Object3D();
+            var object5 = new THREE.Object3D();
 
-            object.position.x = ( ( i % 5 ) * 400 ) - 800;
-            object.position.y = ( - ( Math.floor( i / 5 ) % 5 ) * 400 ) + 800;
-            object.position.z = ( Math.floor( i / 25 ) ) * 1000 - 2000;
+            object5.position.x = ( ( i % 5 ) * 400 ) - 800;
+            object5.position.y = ( - ( Math.floor( i / 5 ) % 5 ) * 400 ) + 800;
+            object5.position.z = ( Math.floor( i / 25 ) ) * 1000 - 2000;
 
-            targets.grid.push( object );
+            targets.grid.push( object5 );
 
         }
 
