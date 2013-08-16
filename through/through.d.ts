ec27374e4185d3686bf20ff4db77483c22515d63
Andrew Gaspar <andrew.james.gaspar@gmail.com>
2013-08-15 21:45:15 -0500

    Added header comments for through

--- through/through.d.ts
+++ through/through.d.ts
@@ -1,6 +1,9 @@
+// Type definitions for through
+// Project: https://github.com/dominictarr/through
+// Definitions by: Andrew Gaspar <https://github.com/AndrewGaspar/>
+// Definitions: https://github.com/borisyankov/DefinitelyTyped
+
 /// <reference path="../node/node.d.ts" />
-
-
 
 declare module "through" {
 	import Stream = require("stream");
