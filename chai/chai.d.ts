8392c07cbf5d576873af6c2861c9bae92cc91829
Jed Hunsaker <jed.hunsaker@gmail.com>
2013-08-24 11:18:44 -0700

    Redo/fix entire chai.expect defs/tests

--- chai/chai.d.ts
+++ chai/chai.d.ts
@@ -1,121 +1,140 @@
-﻿// Type definitions for chai 1.5.0
+﻿// Type definitions for chai 1.7.2
 // Project: http://chaijs.com/
-// Definitions by: Kazi Manzur Rashid <https://github.com/kazimanzurrashid/>
+// Definitions by: Jed Hunsaker <https://github.com/jedhunsaker/>
 // DefinitelyTyped: https://github.com/borisyankov/DefinitelyTyped
 
+
 declare module chai {
-    interface Equality {
-        (expected: any, message?: string): boolean;
+
+    function expect(target: any): Expect;
+
+    interface Expect extends LanguageChains, NumericComparison, TypeComparison {
+        not: Expect;
+        deep: Deep;
+        a: TypeComparison;
+        an: TypeComparison;
+        include: Include;
+        contain: Include;
+        ok: Expect;
+        true: Expect;
+        false: Expect;
+        null: Expect;
+        undefined: Expect;
+        exist: Expect;
+        empty: Expect;
+        arguments: Expect;
+        Arguments: Expect;
+        equal: Equal;
+        equals: Equal;
+        eq: Equal;
+        eql: Equal;
+        eqls: Equal;
+        property: Property;
+        ownProperty: OwnProperty;
+        haveOwnProperty: OwnProperty;
+        length: Length;
+        lengthOf: Length;
+        match(RegularExpression: RegExp, message?: string): Expect;
+        string(string: string, message?: string): Expect;
+        keys: Keys;
+        key(string: string): Expect;
+        throw: Throw;
+        throws: Throw;
+        Throw: Throw;
+        respondTo(method: string, message?: string): Expect;
+        itself: Expect;
+        satisfy(matcher: Function, message?: string): Expect;
+        closeTo(expected: number, delta: number, message?: string): Expect;
+        members: Members;
     }
 
-    interface Property {
-        (name: string, value?: any, message?: string): boolean;
-    }
-
-    interface NumberComparer {
-        (value: number, message?: string): boolean;
-    }
-
-    interface Eql {
-        (value: any, message?: string): boolean;
-    }
-
-    interface Include {
-        (value: Object, message?: string): boolean;
-        (value: string, message?: string): boolean;
-        (value: number, message?: string): boolean;
-        keys(...names: string[]): boolean;
-    }
-
-    interface Throw {
-        (constructor: Error, message?: string);
-        (expected: string, message?: string);
-        (expected: RegExp, message?: string);
-    }
-
-    interface TypeComparison {
-        (type: string, message?: string): boolean;
-        instanceof(type: Object): boolean;
+    interface LanguageChains {
+        to: Expect;
+        be: Expect;
+        been: Expect;
+        is: Expect;
+        that: Expect;
+        and: Expect;
+        have: Expect;
+        with: Expect;
+        at: Expect;
+        of: Expect;
+        same: Expect;
     }
 
     interface NumericComparison {
         above: NumberComparer;
         gt: NumberComparer;
         greaterThan: NumberComparer;
-
+        least: NumberComparer;
+        gte: NumberComparer;
         below: NumberComparer;
         lt: NumberComparer;
         lessThan: NumberComparer;
+        most: NumberComparer;
+        lte: NumberComparer;
+        within(start: number, finish: number, message?: string): Expect;
     }
 
-    interface Length extends NumericComparison {
-        (value: number, message?: string): boolean;
+    interface NumberComparer {
+        (value: number, message?: string): Expect;
+    }
+
+    interface TypeComparison {
+        (type: string, message?: string): Expect;
+        instanceof: InstanceOf;
+        instanceOf: InstanceOf;
+    }
+
+    interface InstanceOf {
+        (constructor: Object, message?: string): Expect;
     }
 
     interface Deep {
-        equal: Equality;
+        equal: Equal;
         property: Property;
     }
 
-    interface Have {
-        property: Property;
-        deep: Deep;
-        length: Length;
-        ownProperty(name: string, message?: string): boolean;
-        string(value: string, message?: string): boolean;
-        keys(...values: string[]): boolean;
+    interface Equal {
+        (value: any, message?: string): Expect;
     }
 
-    interface At {
-        least(value: number, message?: string): boolean;
-        gte(value: number, message?: string): boolean;
-        most(value: number, message?: string): boolean;
-        lte(value: number, message?: string): boolean;
+    interface Property {
+        (name: string, value?: any, message?: string): Expect;
     }
 
-    interface Be extends NumericComparison {
-        ok: boolean;
-        true: boolean;
-        false: boolean;
-        null: boolean;
-        undefined: boolean;
-        empty: boolean;
-        arguments: boolean;
-        an: TypeComparison;
-        at: At;
-
-        a(type: string, message?: string): boolean;
-        within(start: number, finish: number, message?: string): boolean;
-        closeTo(expected: number, delta: number, message?: string): boolean;
+    interface OwnProperty {
+        (name: string, message?: string): Expect;
     }
 
-    interface To {
-        be: Be;
-        not: To;
-        deep: Deep;
-        have: Have;
-
-        exist: boolean;
-
-        equal: Equality;
-
-        include: Include;
-        contain: Include;
-        throw: Throw;
-
-        eql: Eql;
-        eqls: Eql;
-
-        match(value: RegExp, message?: string): boolean;
-
-        respondTo(method: string, message?: string);
-        satisfy(matcher: Function, message?: string);
+    interface Length extends LanguageChains, NumericComparison {
+        (length: number, message?: string): Expect;
     }
 
-    interface ExpectMatchers {
-        to: To;
+    interface Include {
+        (value: Object, message?: string): Expect;
+        (value: string, message?: string): Expect;
+        (value: number, message?: string): Expect;
+        keys: Keys;
+        members: Members;
     }
 
-    function expect(target: any): chai.ExpectMatchers;
+    interface Keys {
+        (...keys: string[]): Expect;
+        (keys: Array): Expect;
+    }
+
+    interface Members {
+        (set: Array, message?: string): Expect;
+    }
+
+    interface Throw {
+        (): Expect;
+        (expected: string, message?: string): Expect;
+        (expected: RegExp, message?: string): Expect;
+        (constructor: Error, expected?: string, message?: string): Expect;
+        (constructor: Error, expected?: RegExp, message?: string): Expect;
+        (constructor: Function, expected?: string, message?: string): Expect;
+        (constructor: Function, expected?: RegExp, message?: string): Expect;
+    }
 }
-
