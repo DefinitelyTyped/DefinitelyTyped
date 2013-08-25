8a3c46bf294e5d721979ee32d557c1ada0ce7509
Jed Hunsaker <jed.hunsaker@gmail.com>
2013-08-25 16:57:46 -0700

    Fix sinon-chai "always" language chain

--- sinon-chai/sinon-chai-tests.ts
+++ sinon-chai/sinon-chai-tests.ts
@@ -16,17 +16,17 @@ function test() {
     expect(spy).to.have.been.calledBefore(anotherSpy);
     expect(spy).to.have.been.calledAfter(anotherSpy);
     expect(spy).to.have.been.calledWithNew;
-    expect(spy).to.have.been.alwaysCalledWithNew;
+    expect(spy).to.always.have.been.calledWithNew;
     expect(spy).to.have.been.calledOn(context);
-    expect(spy).to.have.been.alwaysCalledOn(context);
+    expect(spy).to.always.have.been.calledOn(context);
     expect(spy).to.have.been.calledWith('foo', 'bar');
-    expect(spy).to.have.been.alwaysCalledWith('foo', 'bar');
+    expect(spy).to.always.have.been.calledWith('foo', 'bar');
     expect(spy).to.have.been.calledWithExactly('foo', 'bar');
-    expect(spy).to.have.been.alwaysCalledWithExactly('foo', 'bar');
+    expect(spy).to.always.have.been.calledWithExactly('foo', 'bar');
     expect(spy).to.have.been.calledWithMatch(match);
-    expect(spy).to.have.been.alwaysCalledWithMatch(match);
+    expect(spy).to.always.have.been.calledWithMatch(match);
     expect(spy).to.have.returned(1);
-    expect(spy).to.have.alwaysReturned(1);
-    expect(spy).threw('an error');
-    expect(spy).alwaysThrew('an error');
+    expect(spy).to.have.always.returned(1);
+    expect(spy).to.have.thrown('an error');
+    expect(spy).to.have.always.thrown('an error');
 }
