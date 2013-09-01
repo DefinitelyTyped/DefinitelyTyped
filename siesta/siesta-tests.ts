cb6db2460c0bc9a4b71a3ae9d5e3795057d9cd0a
Qube <ben.quarmby@callmeqube.com>
2013-09-01 16:27:28 +1000

    Optional scope, tests.

--- siesta/siesta-tests.ts
+++ siesta/siesta-tests.ts
@@ -1,12 +1,25 @@
 /// <reference path="siesta.d.ts" />
 
 StartTest(function (t: Siesta.Test.ExtJS) {
+    t.waitForComponentQuery('#myItemId', () => {
+        t.ajaxRequestAndThen('http://some/url', () => {
+            t.isBoolean(123, 'not a boolean');
+        }, null);
+    }, null, 2000);
 });
 
 startTest(function (t: Siesta.Test.Browser) {
+    t.waitForSelectors(['.class', '#id'], () => { });
 });
 
 describe(function (t: Siesta.Test.jQuery) {
+    var library = t.get$();
+
+    t.describe('My Module', () => {
+        t.it('should do something', () => {
+            t.expect('some string').not.toBe('some other string');
+        });
+    });
 });
 
 var Harness = Siesta.Harness.Browser;
