af5688be9ea4c88c18cf0628e1ae0bb5be3b6a09
tardal <tardal@ebay.com>
2013-09-19 13:19:14 +0200

    Corrections after review

--- jquery.dialog2/jquery.dialog2.d.ts
+++ jquery.dialog2/jquery.dialog2.d.ts
@@ -1,7 +1,65 @@
+// Type definitions for jquery.dialog2
+// Project: https://github.com/Nikku/jquery-bootstrap-scripting
+// Definitions by: Thomas Ardal <http://thomasardal.com>
+// Definitions: https://github.com/borisyankov/DefinitelyTyped
+
+/// <reference path="../jquery/jquery.d.ts"/>
+
+interface Dialog2Settings {
+    /**
+     * id which (if specified) will be added to the dialog to make it accessible later.
+     */
+    id?: string;
+
+    /**
+     * Should the dialog be automatically opened?
+     */
+    autoOpen?: boolean;
+
+    /**
+     * Title of the dialog.
+     */
+    title?: string;
+
+    buttons?: any;
+
+    /**
+     * Should the dialog be closed on overlay click?
+     */
+    closeOnOverlayClick?: boolean;
+
+    /**
+     * Should the dialog be closed if [ESCAPE] key is pressed?
+     */
+    closeOnEscape?: boolean;
+
+    /**
+     * Should the dialog be removed from the document when it is closed?
+     */
+    removeOnClose?: boolean;
+
+    /**
+     * Should a close handle be shown?
+     */
+    showCloseHandle?: boolean;
+
+    /**
+     * Text to be displayed when the dialogs contents are loaded.
+     */
+    initialLoadText?: string;
+}
+
 interface Dialog2 {
-    (settings: any): JQuery;
+    (settings: Dialog2Settings): JQuery;
 }
 
 interface JQuery {
+    /**
+     * Show a new bootstrap dialog.
+     **/
+    dialog2: Dialog2;
+}
+
+interface JQueryStatic {
     dialog2: Dialog2;
 }
