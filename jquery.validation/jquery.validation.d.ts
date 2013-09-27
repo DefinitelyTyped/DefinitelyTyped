c3662379830ee19c18eda4ee3c3183b56b3fbc93
Stijn Herreman <stijn.herreman@telenet.be>
2013-09-27 11:21:46 +0200

    Up the version number.

--- jquery.validation/jquery.validation.d.ts
+++ jquery.validation/jquery.validation.d.ts
@@ -1,4 +1,4 @@
-// Type definitions for jquery.validation 1.10
+// Type definitions for jquery.validation 1.11.1
 // Project: http://bassistance.de/jquery-plugins/jquery-plugin-validation/
 // Definitions by: https://github.com/fdecampredon
 // Definitions: https://github.com/borisyankov/DefinitelyTyped
