f1f15ffd03ab5a072dc6ee198726f0710cd1c5d2
Jacob Boland <jboland@daptiv.com>
2013-10-24 12:49:28 -0700

    Fix parameter types which were actually variable names implicitly defined as any

--- angularjs/angular-mocks.d.ts
+++ angularjs/angular-mocks.d.ts
@@ -1,7 +1,6 @@
-// Type definitions for Angular JS 1.2 (ngMock, ngMockE2E module)
+// Type definitions for Angular JS 1.2.0 (ngMock, ngMockE2E module)
 // Project: http://angularjs.org
-// Definitions by: Diego Vilar <http://github.com/diegovilar>
-// Definitions: https://github.com/borisyankov/DefinitelyTyped
+// Definitions: https://github.com/daptiv/DefinitelyTyped
 
 /// <reference path="angular.d.ts" />
 
@@ -86,21 +85,21 @@ declare module ng {
         verifyNoOutstandingRequest(): void;
 
         expect(method: string, url: string, data?: string, headers?: Object): mock.IRequestHandler;
-        expect(method: string, url: string, data?: string, headers?: (Object) => boolean): mock.IRequestHandler;
+        expect(method: string, url: string, data?: string, headers?: (object: Object) => boolean): mock.IRequestHandler;
         expect(method: string, url: string, data?: RegExp, headers?: Object): mock.IRequestHandler;
-        expect(method: string, url: string, data?: RegExp, headers?: (Object) => boolean): mock.IRequestHandler;
-        expect(method: string, url: string, data?: (string) => boolean, headers?: Object): mock.IRequestHandler;
-        expect(method: string, url: string, data?: (string) => boolean, headers?: (Object) => boolean): mock.IRequestHandler;
+        expect(method: string, url: string, data?: RegExp, headers?: (object: Object) => boolean): mock.IRequestHandler;
+        expect(method: string, url: string, data?: (data: string) => boolean, headers?: Object): mock.IRequestHandler;
+        expect(method: string, url: string, data?: (data: string) => boolean, headers?: (object: Object) => boolean): mock.IRequestHandler;
         expect(method: string, url: string, data?: Object, headers?: Object): mock.IRequestHandler;
-        expect(method: string, url: string, data?: Object, headers?: (Object) => boolean): mock.IRequestHandler;
+        expect(method: string, url: string, data?: Object, headers?: (object: Object) => boolean): mock.IRequestHandler;
         expect(method: string, url: RegExp, data?: string, headers?: Object): mock.IRequestHandler;
-        expect(method: string, url: RegExp, data?: string, headers?: (Object) => boolean): mock.IRequestHandler;
+        expect(method: string, url: RegExp, data?: string, headers?: (object: Object) => boolean): mock.IRequestHandler;
         expect(method: string, url: RegExp, data?: RegExp, headers?: Object): mock.IRequestHandler;
-        expect(method: string, url: RegExp, data?: RegExp, headers?: (Object) => boolean): mock.IRequestHandler;
-        expect(method: string, url: RegExp, data?: (string) => boolean, headers?: Object): mock.IRequestHandler;
-        expect(method: string, url: RegExp, data?: (string) => boolean, headers?: (Object) => boolean): mock.IRequestHandler;
+        expect(method: string, url: RegExp, data?: RegExp, headers?: (object: Object) => boolean): mock.IRequestHandler;
+        expect(method: string, url: RegExp, data?: (data: string) => boolean, headers?: Object): mock.IRequestHandler;
+        expect(method: string, url: RegExp, data?: (data: string) => boolean, headers?: (object: Object) => boolean): mock.IRequestHandler;
         expect(method: string, url: RegExp, data?: Object, headers?: Object): mock.IRequestHandler;
-        expect(method: string, url: RegExp, data?: Object, headers?: (Object) => boolean): mock.IRequestHandler;
+        expect(method: string, url: RegExp, data?: Object, headers?: (object: Object) => boolean): mock.IRequestHandler;
 
         expectDELETE(url: string, headers?: Object): mock.IRequestHandler;
         expectDELETE(url: RegExp, headers?: Object): mock.IRequestHandler;
@@ -113,91 +112,91 @@ declare module ng {
 
         expectPATCH(url: string, data?: string, headers?: Object): mock.IRequestHandler;
         expectPATCH(url: string, data?: RegExp, headers?: Object): mock.IRequestHandler;
-        expectPATCH(url: string, data?: (string) => boolean, headers?: Object): mock.IRequestHandler;
+        expectPATCH(url: string, data?: (data: string) => boolean, headers?: Object): mock.IRequestHandler;
         expectPATCH(url: string, data?: Object, headers?: Object): mock.IRequestHandler;
         expectPATCH(url: RegExp, data?: string, headers?: Object): mock.IRequestHandler;
         expectPATCH(url: RegExp, data?: RegExp, headers?: Object): mock.IRequestHandler;
-        expectPATCH(url: RegExp, data?: (string) => boolean, headers?: Object): mock.IRequestHandler;
+        expectPATCH(url: RegExp, data?: (data: string) => boolean, headers?: Object): mock.IRequestHandler;
         expectPATCH(url: RegExp, data?: Object, headers?: Object): mock.IRequestHandler;
 
         expectPOST(url: string, data?: string, headers?: Object): mock.IRequestHandler;
         expectPOST(url: string, data?: RegExp, headers?: Object): mock.IRequestHandler;
-        expectPOST(url: string, data?: (string) => boolean, headers?: Object): mock.IRequestHandler;
+        expectPOST(url: string, data?: (data: string) => boolean, headers?: Object): mock.IRequestHandler;
         expectPOST(url: string, data?: Object, headers?: Object): mock.IRequestHandler;
         expectPOST(url: RegExp, data?: string, headers?: Object): mock.IRequestHandler;
         expectPOST(url: RegExp, data?: RegExp, headers?: Object): mock.IRequestHandler;
-        expectPOST(url: RegExp, data?: (string) => boolean, headers?: Object): mock.IRequestHandler;
+        expectPOST(url: RegExp, data?: (data: string) => boolean, headers?: Object): mock.IRequestHandler;
         expectPOST(url: RegExp, data?: Object, headers?: Object): mock.IRequestHandler;
 
         expectPUT(url: string, data?: string, headers?: Object): mock.IRequestHandler;
         expectPUT(url: string, data?: RegExp, headers?: Object): mock.IRequestHandler;
-        expectPUT(url: string, data?: (string) => boolean, headers?: Object): mock.IRequestHandler;
+        expectPUT(url: string, data?: (data: string) => boolean, headers?: Object): mock.IRequestHandler;
         expectPUT(url: string, data?: Object, headers?: Object): mock.IRequestHandler;
         expectPUT(url: RegExp, data?: string, headers?: Object): mock.IRequestHandler;
         expectPUT(url: RegExp, data?: RegExp, headers?: Object): mock.IRequestHandler;
-        expectPUT(url: RegExp, data?: (string) => boolean, headers?: Object): mock.IRequestHandler;
+        expectPUT(url: RegExp, data?: (data: string) => boolean, headers?: Object): mock.IRequestHandler;
         expectPUT(url: RegExp, data?: Object, headers?: Object): mock.IRequestHandler;
 
         when(method: string, url: string, data?: string, headers?: Object): mock.IRequestHandler;
-        when(method: string, url: string, data?: string, headers?: (Object) => boolean): mock.IRequestHandler;
+        when(method: string, url: string, data?: string, headers?: (object: Object) => boolean): mock.IRequestHandler;
         when(method: string, url: string, data?: RegExp, headers?: Object): mock.IRequestHandler;
-        when(method: string, url: string, data?: RegExp, headers?: (Object) => boolean): mock.IRequestHandler;
-        when(method: string, url: string, data?: (string) => boolean, headers?: Object): mock.IRequestHandler;
-        when(method: string, url: string, data?: (string) => boolean, headers?: (Object) => boolean): mock.IRequestHandler;
+        when(method: string, url: string, data?: RegExp, headers?: (object: Object) => boolean): mock.IRequestHandler;
+        when(method: string, url: string, data?: (data: string) => boolean, headers?: Object): mock.IRequestHandler;
+        when(method: string, url: string, data?: (data: string) => boolean, headers?: (object: Object) => boolean): mock.IRequestHandler;
         when(method: string, url: string, data?: Object, headers?: Object): mock.IRequestHandler;
-        when(method: string, url: string, data?: Object, headers?: (Object) => boolean): mock.IRequestHandler;
+        when(method: string, url: string, data?: Object, headers?: (object: Object) => boolean): mock.IRequestHandler;
         when(method: string, url: RegExp, data?: string, headers?: Object): mock.IRequestHandler;
-        when(method: string, url: RegExp, data?: string, headers?: (Object) => boolean): mock.IRequestHandler;
+        when(method: string, url: RegExp, data?: string, headers?: (object: Object) => boolean): mock.IRequestHandler;
         when(method: string, url: RegExp, data?: RegExp, headers?: Object): mock.IRequestHandler;
-        when(method: string, url: RegExp, data?: RegExp, headers?: (Object) => boolean): mock.IRequestHandler;
-        when(method: string, url: RegExp, data?: (string) => boolean, headers?: Object): mock.IRequestHandler;
-        when(method: string, url: RegExp, data?: (string) => boolean, headers?: (Object) => boolean): mock.IRequestHandler;
+        when(method: string, url: RegExp, data?: RegExp, headers?: (object: Object) => boolean): mock.IRequestHandler;
+        when(method: string, url: RegExp, data?: (data: string) => boolean, headers?: Object): mock.IRequestHandler;
+        when(method: string, url: RegExp, data?: (data: string) => boolean, headers?: (object: Object) => boolean): mock.IRequestHandler;
         when(method: string, url: RegExp, data?: Object, headers?: Object): mock.IRequestHandler;
-        when(method: string, url: RegExp, data?: Object, headers?: (Object) => boolean): mock.IRequestHandler;
+        when(method: string, url: RegExp, data?: Object, headers?: (object: Object) => boolean): mock.IRequestHandler;
 
         whenDELETE(url: string, headers?: Object): mock.IRequestHandler;
-        whenDELETE(url: string, headers?: (Object) => boolean): mock.IRequestHandler;
+        whenDELETE(url: string, headers?: (object: Object) => boolean): mock.IRequestHandler;
         whenDELETE(url: RegExp, headers?: Object): mock.IRequestHandler;
-        whenDELETE(url: RegExp, headers?: (Object) => boolean): mock.IRequestHandler;
+        whenDELETE(url: RegExp, headers?: (object: Object) => boolean): mock.IRequestHandler;
 
         whenGET(url: string, headers?: Object): mock.IRequestHandler;
-        whenGET(url: string, headers?: (Object) => boolean): mock.IRequestHandler;
+        whenGET(url: string, headers?: (object: Object) => boolean): mock.IRequestHandler;
         whenGET(url: RegExp, headers?: Object): mock.IRequestHandler;
-        whenGET(url: RegExp, headers?: (Object) => boolean): mock.IRequestHandler;
+        whenGET(url: RegExp, headers?: (object: Object) => boolean): mock.IRequestHandler;
 
         whenHEAD(url: string, headers?: Object): mock.IRequestHandler;
-        whenHEAD(url: string, headers?: (Object) => boolean): mock.IRequestHandler;
+        whenHEAD(url: string, headers?: (object: Object) => boolean): mock.IRequestHandler;
         whenHEAD(url: RegExp, headers?: Object): mock.IRequestHandler;
-        whenHEAD(url: RegExp, headers?: (Object) => boolean): mock.IRequestHandler;
+        whenHEAD(url: RegExp, headers?: (object: Object) => boolean): mock.IRequestHandler;
 
         whenJSONP(url: string): mock.IRequestHandler;
         whenJSONP(url: RegExp): mock.IRequestHandler;
 
         whenPATCH(url: string, data?: string, headers?: Object): mock.IRequestHandler;
         whenPATCH(url: string, data?: RegExp, headers?: Object): mock.IRequestHandler;
-        whenPATCH(url: string, data?: (string) => boolean, headers?: Object): mock.IRequestHandler;
+        whenPATCH(url: string, data?: (data: string) => boolean, headers?: Object): mock.IRequestHandler;
         whenPATCH(url: string, data?: Object, headers?: Object): mock.IRequestHandler;
         whenPATCH(url: RegExp, data?: string, headers?: Object): mock.IRequestHandler;
         whenPATCH(url: RegExp, data?: RegExp, headers?: Object): mock.IRequestHandler;
-        whenPATCH(url: RegExp, data?: (string) => boolean, headers?: Object): mock.IRequestHandler;
+        whenPATCH(url: RegExp, data?: (data: string) => boolean, headers?: Object): mock.IRequestHandler;
         whenPATCH(url: RegExp, data?: Object, headers?: Object): mock.IRequestHandler;
 
         whenPOST(url: string, data?: string, headers?: Object): mock.IRequestHandler;
         whenPOST(url: string, data?: RegExp, headers?: Object): mock.IRequestHandler;
-        whenPOST(url: string, data?: (string) => boolean, headers?: Object): mock.IRequestHandler;
+        whenPOST(url: string, data?: (data: string) => boolean, headers?: Object): mock.IRequestHandler;
         whenPOST(url: string, data?: Object, headers?: Object): mock.IRequestHandler;
         whenPOST(url: RegExp, data?: string, headers?: Object): mock.IRequestHandler;
         whenPOST(url: RegExp, data?: RegExp, headers?: Object): mock.IRequestHandler;
-        whenPOST(url: RegExp, data?: (string) => boolean, headers?: Object): mock.IRequestHandler;
+        whenPOST(url: RegExp, data?: (data: string) => boolean, headers?: Object): mock.IRequestHandler;
         whenPOST(url: RegExp, data?: Object, headers?: Object): mock.IRequestHandler;
 
         whenPUT(url: string, data?: string, headers?: Object): mock.IRequestHandler;
         whenPUT(url: string, data?: RegExp, headers?: Object): mock.IRequestHandler;
-        whenPUT(url: string, data?: (string) => boolean, headers?: Object): mock.IRequestHandler;
+        whenPUT(url: string, data?: (data: string) => boolean, headers?: Object): mock.IRequestHandler;
         whenPUT(url: string, data?: Object, headers?: Object): mock.IRequestHandler;
         whenPUT(url: RegExp, data?: string, headers?: Object): mock.IRequestHandler;
         whenPUT(url: RegExp, data?: RegExp, headers?: Object): mock.IRequestHandler;
-        whenPUT(url: RegExp, data?: (string) => boolean, headers?: Object): mock.IRequestHandler;
+        whenPUT(url: RegExp, data?: (data: string) => boolean, headers?: Object): mock.IRequestHandler;
         whenPUT(url: RegExp, data?: Object, headers?: Object): mock.IRequestHandler;
     }
 
