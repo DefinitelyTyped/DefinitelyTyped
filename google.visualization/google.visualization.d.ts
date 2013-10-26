091cdbd110d278058f87152eb5326a7e8599c352
Dan Ludwig <Dan.Ludwig@ucosmic.org>
2013-10-26 14:12:24 -0400

    Add google visualization DataTable classes & interfaces.
    Follow up to my last pull request, which was missing these.

--- google.visualization/google.visualization.d.ts
+++ google.visualization/google.visualization.d.ts
@@ -5,15 +5,76 @@ declare module google {
     function setOnLoadCallback(handler: () => void): void;
 
     module visualization {
-        function arrayToDataTable(data: any[]): any;
+        function arrayToDataTable(data: any[]): DataTable;
 
-        module events {
-            function addListener(chart: any, eventName: string, callback: Function);
-            function addListener(chart: any, eventName: string, callback: () => any);
+        export interface DataObjectColumn {
+            type: string;
+            id?: string;
+            label?: string;
+            pattern?: string;
+            p?: any;
+        }
+
+        export interface DataObjectRow {
+            c: DataObjectCell[];
+            p?: any;
+        }
+
+        export interface DataObject {
+            cols: DataObjectColumn[];
+            rows: DataObjectRow[];
+            p: any;
+        }
+
+        export interface DataObjectCell {
+            v?: any;
+            f?: any;
+            p?: any;
+        }
+
+        export interface DataTableColumnDescription {
+            type?: string;
+            label?: string;
+            id?: string;
+            role?: string;
+            pattern?: string;
+        }
+
+        // https://developers.google.com/chart/interactive/docs/reference#DataTable
+        export class DataTable {
+            constructor(data?: any, version?: any);
+            addColumn(type: string, label?: string, id?: string): number;
+            addColumn(descriptionObject: DataTableColumnDescription): number;
+            addRow(cellObject: DataObjectCell): number;
+            addRow(cellArray?: any[]): number;
+            addRows(count: number): number;
+            addRows(array: DataObjectCell[][]);
+            addRows(array: any[]);
         }
 
         //https://google-developers.appspot.com/chart/interactive/docs/gallery/geochart
-        //export interface GeoChartColor { // can't overload GeoChartOptions.backgroundColor
+        export class GeoChart {
+            constructor(element: Element);
+            draw(chart: DataTable, options: GeoChartOptions);
+        }
+        export interface GeoChartOptions {
+            backgroundColor?: any;
+            colorAxis?: GeoChartColorAxis;
+            datalessRegionColor?: string;
+            displayMode?: string;
+            enableRegionInteractivity?: boolean;
+            height?: number;
+            keepAspectRatio?: boolean;
+            legend?: GeoChartLegend;
+            region?: string;
+            magnifyingGlass?: GeoChartMagnifyingGlass;
+            markerOpacity?: number;
+            resolution?: string;
+            sizeAxis?: GeoChartAxis;
+            tooltip?: GeoChartTooltip;
+            width?: number;
+        }
+        //export interface GeoChartColor {
         //    fill?: string;
         //    stroke?: string;
         //    strokeWidth: number;
@@ -49,26 +110,10 @@ declare module google {
             textStyle?: GeoChartTextStyle;
             trigger?: string;
         }
-        export interface GeoChartOptions {
-            backgroundColor?: any;
-            colorAxis?: GeoChartColorAxis;
-            datalessRegionColor?: string;
-            displayMode?: string;
-            enableRegionInteractivity?: boolean;
-            height?: number;
-            keepAspectRatio?: boolean;
-            legend?: GeoChartLegend;
-            region?: string;
-            magnifyingGlass?: GeoChartMagnifyingGlass;
-            markerOpacity?: number;
-            resolution?: string;
-            sizeAxis?: GeoChartAxis;
-            tooltip?: GeoChartTooltip;
-            width?: number;
-        }
-        export class GeoChart {
-            constructor(element: Element);
-            draw(chart: any, options: any);
+
+        module events {
+            function addListener(chart: any, eventName: string, callback: Function);
+            function addListener(chart: any, eventName: string, callback: () => any);
         }
     }
 }
