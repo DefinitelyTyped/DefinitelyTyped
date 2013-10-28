573bb15ef532646802cc38e7e33b7fa77eae19ce
Jonathan Hedrén <jonathan.hedren@gmail.com>
2013-10-28 12:47:58 +0100

    Added som tests. Updated definitions.

--- flight/flight-tests.ts
+++ flight/flight-tests.ts
@@ -1,1 +1,48 @@
 /// <reference path="flight.d.ts" />
+declare var el: Element;
+declare var els: Element[];
+declare var mixinFn: Function;
+
+function TestComponent() {
+    var self: FlightBase = this;                   
+          
+    self.defaultAttrs({
+       fooSelector: '.bar'
+    });
+
+    this.onClick = function (ev: JQueryEventObject, data) {
+        self.select('fooSelector').addClass('bar');
+    };
+                                           
+    self.around('initialize', function () { });
+    self.before('initialize', function () { });
+    self.after("initialize", function () {
+
+        var $node: JQuery = self.$node;
+        var node: Element = self.node;
+
+        self.on(el, 'click', {});
+        self.on(els, 'click', function () { });
+        self.on(document, 'click', this.onClick);
+        self.on('click', function () { });
+        self.on('click', {
+            fooSelector: this.onClick
+        });
+
+        self.off('click', function () { });
+        self.off(document, 'click');
+        self.off(el, 'click')
+        self.off(els, 'click');
+
+        self.teardown();
+    });
+}
+
+flight.component(TestComponent, mixinFn).attachTo(el);  
+flight.component(TestComponent, mixinFn).attachTo($(els));  
+flight.component(TestComponent, mixinFn).attachTo('.test');
+flight.component(TestComponent, mixinFn).attachTo('.test', {
+    some: 'data'
+});
+
+flight.component.teardownAll();
