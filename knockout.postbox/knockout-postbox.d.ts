b45771bc286a7019553abdaf943e1467de6c8e5c
Daniel Ferreira Monteiro Alves <danfma@gmail.com>
2013-11-05 04:29:31 -0200

    Fix on extend method and others for compatibility with Knockout 3

--- knockout.postbox/knockout-postbox.d.ts
+++ knockout.postbox/knockout-postbox.d.ts
@@ -19,6 +19,14 @@ interface KnockoutObservable<T> {
     syncWith(topic: string, initializeWithLatestValue?: boolean, skipInitialPublish?: boolean, equalityComparer?: (newValue: T, oldValue: T) => boolean): KnockoutObservable<T>;
 }
 
+interface KnockoutObservableArray<T> {
+	subscribeTo(topic: string, useLastPublishedValueToInitialize?: boolean, transform?: (val: any) => T): KnockoutObservableArray<T>;
+	unsubscribeFrom(topic: string): KnockoutObservableArray<T>;
+	publishOn(topic: string, skipInitialPublish?: boolean, equalityComparer?: (newValue: T, oldValue: T) => boolean): KnockoutObservableArray<T>;
+	stopPublishingOn(topic: string): KnockoutObservableArray<T>;
+	syncWith(topic: string, initializeWithLatestValue?: boolean, skipInitialPublish?: boolean, equalityComparer?: (newValue: T, oldValue: T) => boolean): KnockoutObservableArray<T>;
+}
+
 interface KnockoutStatic {
     postbox: KnockoutPostBox;
 }
