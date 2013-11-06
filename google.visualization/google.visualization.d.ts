348fad872f9033a8273a00ef2980fd0890553733
Dan Ludwig <Dan.Ludwig@ucosmic.org>
2013-11-05 19:56:09 -0500

    Detecting travis ci build errors again.

--- google.visualization/google.visualization.d.ts
+++ google.visualization/google.visualization.d.ts
@@ -156,204 +156,204 @@ declare module google {
         }
 
         //#endregion
-        //#region Common
+        ////#region Common
 
-        export interface ChartArea {
-            top: any;
-            left: any;
-            width: any;
-            height: any;
-        }
+        //export interface ChartArea {
+        //    top: any;
+        //    left: any;
+        //    width: any;
+        //    height: any;
+        //}
 
-        export interface ChartTextStyle {
-            color?: string;
-            fontName?: string;
-            fontSize?: number;
-            bold?: boolean;
-            italic?: boolean;
-        }
+        //export interface ChartTextStyle {
+        //    color?: string;
+        //    fontName?: string;
+        //    fontSize?: number;
+        //    bold?: boolean;
+        //    italic?: boolean;
+        //}
 
-        export interface ChartLegend {
-            alignment?: string;
-            maxLines?: number;
-            position?: string;
-            textStyle?: ChartTextStyle;
-        }
+        //export interface ChartLegend {
+        //    alignment?: string;
+        //    maxLines?: number;
+        //    position?: string;
+        //    textStyle?: ChartTextStyle;
+        //}
 
-        // https://google-developers.appspot.com/chart/interactive/docs/animation
-        export interface TransitionAnimation {
-            duration?: number;
-            easing?: string; // linear, in, out, inAndOut
-        }
+        //// https://google-developers.appspot.com/chart/interactive/docs/animation
+        //export interface TransitionAnimation {
+        //    duration?: number;
+        //    easing?: string; // linear, in, out, inAndOut
+        //}
 
-        export interface ChartAxis {
-            baseline?: number; // This option is only supported for a continuous axis. https://google-developers.appspot.com/chart/interactive/docs/customizing_axes#Terminology
-            baselineColor?: string; // google's documentation on this is wrong, specifies it as a number. The color of the baseline for the horizontal axis. Can be any HTML color string, for example: 'red' or '#00cc00'
-            direction?: number; // The direction in which the values along the horizontal axis grow. Specify -1 to reverse the order of the values.
-            format?: string; // icu pattern set http://icu-project.org/apiref/icu4c/classDecimalFormat.html#_details
-            gridlines?: ChartGridlines;
-            logScale?: boolean;
-            textPosition?: string;
-            textStyle?: ChartTextStyle;
-            ticks?: any[];
-            title?: string;
-            titleTextStyle?: ChartTextStyle;
-            allowContainerBoundaryTextCufoff?: boolean;
-            slantedText?: boolean;
-            slantedTextAngle?: number;
-            maxAlternation?: number;
-            maxTextLines?: number;
-            minTextSpacing?: number;
-            showTextEvery?: number;
-            maxValue?: number;
-            minValue?: number;
-            viewWindowMode?: string;
-            viewWindow?: ChartViewWindow;
-        }
+        //export interface ChartAxis {
+        //    baseline?: number; // This option is only supported for a continuous axis. https://google-developers.appspot.com/chart/interactive/docs/customizing_axes#Terminology
+        //    baselineColor?: string; // google's documentation on this is wrong, specifies it as a number. The color of the baseline for the horizontal axis. Can be any HTML color string, for example: 'red' or '#00cc00'
+        //    direction?: number; // The direction in which the values along the horizontal axis grow. Specify -1 to reverse the order of the values.
+        //    format?: string; // icu pattern set http://icu-project.org/apiref/icu4c/classDecimalFormat.html#_details
+        //    gridlines?: ChartGridlines;
+        //    logScale?: boolean;
+        //    textPosition?: string;
+        //    textStyle?: ChartTextStyle;
+        //    ticks?: any[];
+        //    title?: string;
+        //    titleTextStyle?: ChartTextStyle;
+        //    allowContainerBoundaryTextCufoff?: boolean;
+        //    slantedText?: boolean;
+        //    slantedTextAngle?: number;
+        //    maxAlternation?: number;
+        //    maxTextLines?: number;
+        //    minTextSpacing?: number;
+        //    showTextEvery?: number;
+        //    maxValue?: number;
+        //    minValue?: number;
+        //    viewWindowMode?: string;
+        //    viewWindow?: ChartViewWindow;
+        //}
 
-        export interface ChartGridlines {
-            color?: string;
-            count?: number;
-            minorGridlines?: ChartMinorGridlines;
-        }
+        //export interface ChartGridlines {
+        //    color?: string;
+        //    count?: number;
+        //    minorGridlines?: ChartMinorGridlines;
+        //}
 
-        export interface ChartMinorGridlines {
-            color?: string;
-            count?: number;
-        }
+        //export interface ChartMinorGridlines {
+        //    color?: string;
+        //    count?: number;
+        //}
 
-        export interface ChartViewWindow {
-            max?: number;
-            min?: number;
-        }
+        //export interface ChartViewWindow {
+        //    max?: number;
+        //    min?: number;
+        //}
 
-        export interface ChartTooltip {
-            isHtml?: boolean;
-            showColorCode?: boolean;
-            textStyle?: ChartTextStyle;
-            trigger?: string;
-        }
+        //export interface ChartTooltip {
+        //    isHtml?: boolean;
+        //    showColorCode?: boolean;
+        //    textStyle?: ChartTextStyle;
+        //    trigger?: string;
+        //}
 
-        export interface ChartBoundingBox {
-            left: number;
-            top: number;
-            width: number;
-            height: number;
-        }
+        //export interface ChartBoundingBox {
+        //    left: number;
+        //    top: number;
+        //    width: number;
+        //    height: number;
+        //}
 
-        export interface ChartLayoutInterface {
-            getBoundingBox(id: string): ChartBoundingBox;
-            getChartAreaBoundingBox(): ChartBoundingBox;
-            getHAxisValue(position: number, axisIndex?: number): number;
-            getVAxisValue(position: number, axisIndex?: number): number;
-            getXLocation(position: number, axisIndex?: number): number;
-            getYLocation(position: number, axisIndex?: number): number;
-        }
+        //export interface ChartLayoutInterface {
+        //    getBoundingBox(id: string): ChartBoundingBox;
+        //    getChartAreaBoundingBox(): ChartBoundingBox;
+        //    getHAxisValue(position: number, axisIndex?: number): number;
+        //    getVAxisValue(position: number, axisIndex?: number): number;
+        //    getXLocation(position: number, axisIndex?: number): number;
+        //    getYLocation(position: number, axisIndex?: number): number;
+        //}
 
         export interface VisualizationSelectionArray {
             column?: number;
             row?: number;
         }
 
-        //#endregion
-        //#region ColumnChart
+        ////#endregion
+        ////#region ColumnChart
 
-        // https://google-developers.appspot.com/chart/interactive/docs/gallery/columnchart
-        export class ColumnChart {
-            constructor(element: Element);
+        //// https://google-developers.appspot.com/chart/interactive/docs/gallery/columnchart
+        //export class ColumnChart {
+        //    constructor(element: Element);
 
-            // https://google-developers.appspot.com/chart/interactive/docs/gallery/columnchart#Methods
-            draw(data: DataTable, options?: ColumnChartOptions): void;
-            draw(data: DataView, options?: ColumnChartOptions): void;
-            getChartLayoutInterface(): ChartLayoutInterface;
-            getSelection(): any[];
-            setSelection(selection: any[]): void;
-            clearChart(): void;
-        }
+        //    // https://google-developers.appspot.com/chart/interactive/docs/gallery/columnchart#Methods
+        //    draw(data: DataTable, options?: ColumnChartOptions): void;
+        //    draw(data: DataView, options?: ColumnChartOptions): void;
+        //    getChartLayoutInterface(): ChartLayoutInterface;
+        //    getSelection(): any[];
+        //    setSelection(selection: any[]): void;
+        //    clearChart(): void;
+        //}
 
-        // https://google-developers.appspot.com/chart/interactive/docs/gallery/columnchart#Configuration_Options
-        export interface ColumnChartOptions {
-            aggregationTarget?: string;
-            animation?: TransitionAnimation;
-            axisTitlesPosition?: string; // in, out, none
-            backgroundColor?: any;
-            bar?: ColumnChartBarOptions;
-            chartArea?: ChartArea;
-            colors?: string[];
-            enableInteractivity?: boolean;
-            focusTarget?: string;
-            fontSize?: number;
-            fontName?: string;
-            hAxis?: ChartAxis;
-            height?: number;
-            isStacked?: boolean;
-            legend?: ChartLegend;
-            reverseCategories?: boolean;
-            selectionMode?: string // single / multiple
-            series?: any;
-            theme?: string;
-            title?: string;
-            titlePosition?: string;
-            titleTextStyle?: ChartTextStyle;
-            tooltip?: ChartTooltip;
-            vAxes?: any;
-            vAxis?: ChartAxis;
-            width?: number;
-        }
+        //// https://google-developers.appspot.com/chart/interactive/docs/gallery/columnchart#Configuration_Options
+        //export interface ColumnChartOptions {
+        //    aggregationTarget?: string;
+        //    animation?: TransitionAnimation;
+        //    axisTitlesPosition?: string; // in, out, none
+        //    backgroundColor?: any;
+        //    bar?: ColumnChartBarOptions;
+        //    chartArea?: ChartArea;
+        //    colors?: string[];
+        //    enableInteractivity?: boolean;
+        //    focusTarget?: string;
+        //    fontSize?: number;
+        //    fontName?: string;
+        //    hAxis?: ChartAxis;
+        //    height?: number;
+        //    isStacked?: boolean;
+        //    legend?: ChartLegend;
+        //    reverseCategories?: boolean;
+        //    selectionMode?: string // single / multiple
+        //    series?: any;
+        //    theme?: string;
+        //    title?: string;
+        //    titlePosition?: string;
+        //    titleTextStyle?: ChartTextStyle;
+        //    tooltip?: ChartTooltip;
+        //    vAxes?: any;
+        //    vAxis?: ChartAxis;
+        //    width?: number;
+        //}
 
-        export interface ColumnChartBarOptions {
-            groupWidth: any;
-        }
+        //export interface ColumnChartBarOptions {
+        //    groupWidth: any;
+        //}
 
-        //#endregion
-        //#region LineChart
+        ////#endregion
+        ////#region LineChart
 
-        // https://google-developers.appspot.com/chart/interactive/docs/gallery/linechart
-        export class LineChart {
-            constructor(element: Element);
+        //// https://google-developers.appspot.com/chart/interactive/docs/gallery/linechart
+        //export class LineChart {
+        //    constructor(element: Element);
 
-            // https://google-developers.appspot.com/chart/interactive/docs/gallery/linechart#Methods
-            draw(data: DataTable, options: any): void;
-            draw(data: DataView, options: any): void;
-            getChartLayoutInterface(): ChartLayoutInterface;
-            getSelection(): any[];
-            setSelection(selection: any[]): void;
-            clearChart(): void;
-        }
+        //    // https://google-developers.appspot.com/chart/interactive/docs/gallery/linechart#Methods
+        //    draw(data: DataTable, options: any): void;
+        //    draw(data: DataView, options: any): void;
+        //    getChartLayoutInterface(): ChartLayoutInterface;
+        //    getSelection(): any[];
+        //    setSelection(selection: any[]): void;
+        //    clearChart(): void;
+        //}
 
-        // https://google-developers.appspot.com/chart/interactive/docs/gallery/linechart#Configuration_Options
-        export interface LineChartOptions {
-            aggregationTarget?: string;
-            animation?: TransitionAnimation;
-            axisTitlesPosition?: string;
-            backgroundColor?: any;
-            chartArea?: ChartArea;
-            colors?: string[];
-            curveType?: string;
-            enableInteractivity?: boolean;
-            focusTarget?: string;
-            fontSize?: number;
-            fontName?: string;
-            hAxis?: ChartAxis;
-            height?: number;
-            interpolateNulls?: boolean;
-            legend?: ChartLegend;
-            lineWidth?: number;
-            pointSize?: number;
-            reverseCategories?: boolean;
-            selectionMode?: string // single / multiple
-            series?: any;
-            theme?: string;
-            title?: string;
-            titlePosition?: string;
-            titleTextStyle?: ChartTextStyle;
-            tooltip?: ChartTooltip;
-            vAxes?: any;
-            vAxis?: ChartAxis;
-            width?: number;
-        }
+        //// https://google-developers.appspot.com/chart/interactive/docs/gallery/linechart#Configuration_Options
+        //export interface LineChartOptions {
+        //    aggregationTarget?: string;
+        //    animation?: TransitionAnimation;
+        //    axisTitlesPosition?: string;
+        //    backgroundColor?: any;
+        //    chartArea?: ChartArea;
+        //    colors?: string[];
+        //    curveType?: string;
+        //    enableInteractivity?: boolean;
+        //    focusTarget?: string;
+        //    fontSize?: number;
+        //    fontName?: string;
+        //    hAxis?: ChartAxis;
+        //    height?: number;
+        //    interpolateNulls?: boolean;
+        //    legend?: ChartLegend;
+        //    lineWidth?: number;
+        //    pointSize?: number;
+        //    reverseCategories?: boolean;
+        //    selectionMode?: string // single / multiple
+        //    series?: any;
+        //    theme?: string;
+        //    title?: string;
+        //    titlePosition?: string;
+        //    titleTextStyle?: ChartTextStyle;
+        //    tooltip?: ChartTooltip;
+        //    vAxes?: any;
+        //    vAxis?: ChartAxis;
+        //    width?: number;
+        //}
 
-        //#endregion
+        ////#endregion
         //#region Events
 
         module events {
