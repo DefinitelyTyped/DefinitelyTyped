1e9c9bede1d941c6a0ba535fbac76f5b5152fed0
Gregory Dalton <gregolai@gmail.com>
2013-11-19 01:40:09 -0800

    added headers and tests

--- stats/stats.d.ts
+++ stats/stats.d.ts
@@ -1,13 +1,15 @@
-/**
- * @author mrdoob / http://mrdoob.com/
- * stats.js - http://github.com/mrdoob/stats.js
- */
+// Type definitions for Stats.js r11
+// Project: http://github.com/mrdoob/stats.js
+// Definitions by: Gregory Dalton <https://github.com/gregolai>
+// Definitions: https://github.com/borisyankov/DefinitelyTyped
+
 declare class Stats {
 
 	REVISION: number;
 
 	domElement: HTMLDivElement;
 	
+	// 0: fps, 1: ms
 	setMode(value: number): void;
 	
 	begin(): void;
