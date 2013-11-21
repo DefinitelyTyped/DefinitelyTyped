57532148a15265cae32e933238200e3a7b96a59d
Masahiro Wakame <vvakame@gmail.com>
2013-11-21 12:08:25 +0900

    Fix success or failed determine code bug
    currently implementation was erroneous decision, e.g. _infrastructure/tests/typescript/tsc.js not exists.

--- _infrastructure/tests/runner.js
+++ _infrastructure/tests/runner.js
@@ -691,7 +691,7 @@ var DefinitelyTyped;
             }
             Object.defineProperty(TestResult.prototype, "success", {
                 get: function () {
-                    return this.exitCode !== 1;
+                    return this.exitCode === 0;
                 },
                 enumerable: true,
                 configurable: true
