4af9e5a7ea704ef1071802a8acf3a654481b8f68
s_kimura <s_kimura@sa.il24.net>
2013-11-28 12:19:00 +0900

    added type information to tweenjs-test.ts and loadHandler method of soundjs-test.ts.

--- tweenjs/tweenjs-tests.ts
+++ tweenjs/tweenjs-tests.ts
@@ -2,12 +2,11 @@
 ///<reference path="../easeljs/easeljs.d.ts"/>
 
 var target: createjs.DisplayObject = new createjs.DisplayObject();
-var Tween: createjs.Tween = createjs.Tween; 
 
 // source : http://www.createjs.com/Docs/TweenJS/modules/TweenJS.html
 // Chainable modules : 
 target.alpha = 1;
-Tween.get(target).wait(500, false).to({ alpha: 0, visible: false }, 1000).call(onComplete);
+createjs.Tween.get(target).wait(500, false).to({ alpha: 0, visible: false }, 1000).call(onComplete);
 function onComplete() {
     //Tween complete
 }
