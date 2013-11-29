2496728011890f00a33101291f56aac2f538b6ef
John Reilly <johnny_reilly@hotmail.com>
2013-11-29 10:17:15 +0000

    jQuery: Added contributors to header, add change

--- jquery/jquery.d.ts
+++ jquery/jquery.d.ts
@@ -13,7 +13,10 @@ See the Apache Version 2.0 License for specific language governing permissions
 and limitations under the License.
 ***************************************************************************** */
 
-// Typing for the jQuery library, version 2.0.x
+// Typing for the jQuery library, version 1.10.x / 2.0.x
+// Project: http://jquery.com/
+// Definitions by: Boris Yankov <https://github.com/borisyankov/>, Christian Hoffmeister <https://github.com/choffmeister>, Steve Fenton, Diullei Gomes <https://github.com/Diullei>, Tass Iliopoulos <https://github.com/tasoili>, Jason Swearingen, Sean Hill <https://github.com/seanski>, Guus Goossens <https://github.com/Guuz>, Kelly Summerlin <https://github.com/ksummerlin>, Basarat Ali Syed <https://github.com/basarat>, Nicholas Wolverson <https://github.com/nwolverson>, Derek Cicerone <https://github.com/derekcicerone>, Andrew Gaspar <https://github.com/AndrewGaspar>, James Harrison Fisher <https://github.com/jameshfisher>, Seikichi Kondo <https://github.com/seikichi>, Benjamin Jackman <https://github.com/benjaminjackman>, Poul Sorensen <https://github.com/s093294>, Josh Strobl <https://github.com/JoshStrobl>, John Reilly <https://github.com/johnnyreilly/>
+// Definitions: https://github.com/borisyankov/DefinitelyTyped
 
 /*
     Interface for the AJAX setting that will configure the AJAX request
@@ -656,6 +659,10 @@ interface JQuery {
     blur(handler: (eventObject: JQueryEventObject) => any): JQuery;
     blur(eventData?: any, handler?: (eventObject: JQueryEventObject) => any): JQuery;
 
+    /**
+     * Trigger the "change" event on an element.
+     */
+    change(): JQuery;
     /**
      * Bind an event handler to the "change" JavaScript event
      *
