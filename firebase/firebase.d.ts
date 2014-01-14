f77034b331a406274820d26248c1af8a033ce976
Dénes Harmath <harmathdenes@gmail.com>
2014-01-14 14:21:23 +0100

    Consistency: errors can be of any type everywhere

--- firebase/firebase.d.ts
+++ firebase/firebase.d.ts
@@ -44,7 +44,7 @@ interface IFirebaseQuery {
 
 declare class Firebase implements IFirebaseQuery {
 	constructor(firebaseURL: string);
-	auth(authToken: string, onComplete?: (error: string, result: IFirebaseAuthResult) => void, onCancel?:(error: string) => void): void;
+	auth(authToken: string, onComplete?: (error: any, result: IFirebaseAuthResult) => void, onCancel?:(error: any) => void): void;
 	unauth(): void;
 	child(childPath: string): Firebase;
 	parent(): Firebase;
