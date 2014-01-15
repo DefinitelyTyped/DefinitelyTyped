1c2ac883580afc06e4208757b7360ed1e8c68efe
Basarat Syed <bas@picnicsoftware.com>
2014-01-15 16:47:30 +1100

    Revert "added promise chaining for http"
    This reverts commit ef8d6d25e82f5683d586b4be3dd794cc51a1b932. Needs more tests

--- angularjs/angular.d.ts
+++ angularjs/angular.d.ts
@@ -625,7 +625,6 @@ declare module ng {
     interface IHttpPromise<T> extends IPromise<T> {
         success(callback: IHttpPromiseCallback<T>): IHttpPromise<T>;
         error(callback: IHttpPromiseCallback<T>): IHttpPromise<T>;
-        then<TResult>(successCallback: (response: IHttpPromiseCallbackArg<T>) => IHttpPromise<TResult>, errorCallback?: (response: IHttpPromiseCallbackArg<T>) => any): IHttpPromise<TResult>;
         then<TResult>(successCallback: (response: IHttpPromiseCallbackArg<T>) => TResult, errorCallback?: (response: IHttpPromiseCallbackArg<T>) => any): IPromise<TResult>;
         then<TResult>(successCallback: (response: IHttpPromiseCallbackArg<T>) => IPromise<TResult>, errorCallback?: (response: IHttpPromiseCallbackArg<T>) => any): IPromise<TResult>;
     }
