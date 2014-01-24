46ec944eba2ea48f8015945b173b7f080a260bed
dotnetnerd <chn@d60.dk>
2014-01-24 12:53:59 +0100

    Added more annotations

--- ftdomdelegate/ftdomdelegate.d.ts
+++ ftdomdelegate/ftdomdelegate.d.ts
@@ -3,12 +3,20 @@
 // Definitions by: Christian Holm Nielsen
 // Definitions: https://github.com/borisyankov/DefinitelyTyped
 
-
 declare class Delegate
 {
     constructor(element: HTMLElement);
 
-    on(eventType: string, selector: string, callback : (event : any) => any) : void;
+    on(eventType: string, selector: string, handler : (event: Event, targetElement: Element) => void, eventData? : any) : void;
 
-    on(eventType: string, callback:(event : any) => any) : void;
+    on(eventType: string, selector: (element: Element) => boolean, handler : (event: Event, targetElement: Element) => void, eventData? : any) : void;
+
+    on(eventType: string, handler:(event: Event, targetElement: Element) => void, eventData? : any) : void;
+
+    off(eventType? : string, selector? : string, handler? : (event: Event, targetElement: Element) => void) : void;
+    off(eventType? : string, selector?: (element: Element) => boolean, handler? : (event: Event, targetElement: Element) => void) : void;
+    
+    root(element? : Element) : void;
+
+    destroy() : void;
 }
