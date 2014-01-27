82d4b10356f19fee3a379d8ef358f9e0480378bb
Derek Cicerone <derekcicerone@gmail.com>
2014-01-27 14:16:20 -0500

    Update jquery.color.d.ts

--- jquery.color/jquery.color.d.ts
+++ jquery.color/jquery.color.d.ts
@@ -1,36 +1,20 @@
 // Type definitions for jquery.color.js v2.1.2
+// Project: https://github.com/jquery/jquery-color
+// Definitions by: Derek Cicerone <https://github.com/derekcicerone/>
 // Definitions: https://github.com/borisyankov/DefinitelyTyped
 
+/// <reference path="../jquery/jquery.d.ts"/>
+
 interface JQueryColor {
-    /**
-     * Returns the alpha value of this color.
-     */
-    alpha(): number;
-
-    /**
-     * Returns the hexadecimal representation.
-     */
-    toHexString(): string;
-
-    /**
-     * Returns a CSS string like "rgba(255, 255, 255, 0.4)".
-     */
-    toRgbaString(): string;
-
-    /**
-     * The color distance (0.0 - 1.0) of the way between this color and othercolor.
-     */
-    transition(othercolor: JQueryColor, distance: number): JQueryColor;
-
-    /**
-     * Checks if two colors are equal.
-     */
-    is(otherColor: JQueryColor): boolean;
-
     /**
      * Returns the red component of the color (integer from 0 - 255).
      */
     red(): number;
+
+    /**
+     * Returns a copy of the color object with the red set to val.
+     */
+    red(val: number): JQueryColor;
 
     /**
      * Returns the green component of the color (integer from 0 - 255).
@@ -38,9 +22,129 @@ interface JQueryColor {
     green(): number;
 
     /**
+     * Returns a copy of the color object with the green set to val.
+     */
+    green(val: number): JQueryColor;
+
+    /**
      * Returns the blue component of the color (integer from 0 - 255).
      */
     blue(): number;
+
+    /**
+     * Returns a copy of the color object with the blue set to val.
+     */
+    blue(val: number): JQueryColor;
+
+    /**
+     * Returns the alpha value of this color (float from 0.0 - 1.0).
+     */
+    alpha(): number;
+
+    /**
+     * Returns a copy of the color object with the alpha set to val.
+     */
+    alpha(val: number): JQueryColor;
+
+    /**
+     * Returns the hue component of the color (integer from 0 - 359).
+     */
+    hue(): number;
+
+    /**
+     * Returns a copy of the color object with the hue set to val.
+     */
+    hue(val: number): JQueryColor;
+
+    /**
+     * Returns the saturation component of the color (float from 0.0 - 1.0).
+     */
+    saturation(): number;
+
+    /**
+     * Returns a copy of the color object with the saturation set to val.
+     */
+    saturation(val: number): JQueryColor;
+
+    /**
+     * Returns the lightness component of the color (float from 0.0 - 1.0).
+     */
+    lightness(): number;
+
+    /**
+     * Returns a copy of the color object with the lightness set to val.
+     */
+    lightness(val: number): JQueryColor;
+
+    /**
+     * Returns a rgba "tuple" [ red, green, blue, alpha ].
+     */
+    rgba(): number[];
+
+    /**
+     * Returns a copy of the color with any defined values set to the new value.
+     */
+    rgba(red: number, green: number, blue: number, alpha?: number): JQueryColor;
+
+    /**
+     * Returns a copy of the color with any defined values set to the new value.
+     */
+    rgba(val: RgbaColor): JQueryColor;
+
+    /**
+     * Returns a copy of the color with any defined values set to the new value.
+     */
+    rgba(vals: number[]): JQueryColor;
+
+    /**
+     * Returns a HSL tuple [ hue, saturation, lightness, alpha ].
+     */
+    hsla(): number[];
+
+    /**
+     * Returns a copy of the color with any defined values set to the new value.
+     */
+    hsla(hue: number, saturation: number, lightness: number, alpha?: number): JQueryColor;
+
+    /**
+     * Returns a copy of the color with any defined values set to the new value.
+     */
+    hsla(val: HslaColor): JQueryColor;
+
+    /**
+     * Returns a copy of the color with any defined values set to the new value.
+     */
+    hsla(vals: number[]): JQueryColor;
+
+    /**
+     * Returns a CSS string "rgba(255, 255, 255, 0.4)".
+     */
+    toRgbaString(): string;
+
+    /**
+     * Returns a css string "hsla(330, 75%, 25%, 0.4)".
+     */
+    toHslaString(): string;
+
+    /**
+     * Returns a css string "#abcdef", with "includeAlpha" uses "#rrggbbaa" (alpha *= 255).
+     */
+    toHexString(includeAlpha?: boolean): string;
+
+    /**
+     * The color distance (0.0 - 1.0) of the way between this color and othercolor.
+     */
+    transition(othercolor: JQueryColor, distance: number): JQueryColor;
+
+    /**
+     * Will apply this color on top of the other color using alpha blending.
+     */
+    blend(othercolor: JQueryColor): void;
+
+    /**
+     * Checks if two colors are equal.
+     */
+    is(otherColor: JQueryColor): boolean;
 }
 
 interface HslaColor {
