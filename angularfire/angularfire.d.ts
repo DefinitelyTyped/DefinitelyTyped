fb0547ea48c314f1837ab92858889261486078f4
Dénes Harmath <harmathdenes@gmail.com>
2014-02-11 01:23:26 +0100

    Preliminary version of AngularFire definition

---
+++ angularfire/angularfire.d.ts
@@ -0,0 +1,29 @@
+interface AngularFireService {
+	(firebase: Firebase): AngularFire;
+}
+
+interface AngularFire {
+	$add(value: any): void;
+	$remove(key?: string): void;
+	$save(key?: string): void;
+	$child(key: string): AngularFire;
+	$set(value: any): void;
+	$getIndex(): number;
+	$priority: number;
+	$on(eventType: string, callback: (dataSnapshot: IFirebaseDataSnapshot, prevChildName?: string) => void, cancelCallback?: ()=> void, context?: Object): (dataSnapshot: IFirebaseDataSnapshot, prevChildName?: string) => void;
+	$off(eventType: string, callback: (dataSnapshot: IFirebaseDataSnapshot, prevChildName?: string) => void, cancelCallback?: ()=> void, context?: Object): (dataSnapshot: IFirebaseDataSnapshot, prevChildName?: string) => void;
+	$bind($scope: ng.IScope, modelName: string): ng.IPromise<any>;
+}
+
+interface AngularFireAuthService {
+	(firebase: Firebase): AngularFireAuth;
+}
+
+interface AngularFireAuth {
+	$getCurrentUser(): ng.IPromise<any>;
+	$logout(): void;
+	$createUser(email: string, password: string, noLogin?: boolean): ng.IPromise<any>;
+	$changePassword(email: string, oldPassword: string, newPassword: string): ng.IPromise<any>;
+	$removeUser(email: string, password: string): ng.IPromise<any>;
+	$sendPasswordResetEmail(email: string): ng.IPromise<any>;
+}
