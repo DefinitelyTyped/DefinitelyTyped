4a4c40b9a42c01a0e0370e9c81d12eff1be446ea
Václav Oborník <register.ufon@gmail.com>
2014-03-02 14:17:41 +0100

    removed 'on' method from Server definition, it is inherited now

--- restify/restify.d.ts
+++ restify/restify.d.ts
@@ -31,6 +31,8 @@ declare module "restify" {
     secure: boolean;
     time: number;
     params: any;
+
+    body?: any; //available when bodyParser plugin is used
   }
 
   interface Response extends http.ServerResponse {
@@ -56,7 +58,6 @@ declare module "restify" {
     del: (route: any, routeCallBack: RequestHadler) => any;
     get: (route: any, routeCallBack: RequestHadler) => any;
     head: (route: any, routeCallBack: RequestHadler) => any;
-    on: (event: string, callback: Function) => any;
     name: string;
     version: string;
     log: Object;
@@ -127,43 +128,43 @@ declare module "restify" {
     (req: Request, res: Response, next: Function): any;
   }
 
-    export function createServer(options?: ServerOptions): Server;
+  export function createServer(options?: ServerOptions): Server;
 
-    export function createJsonClient(options?: ClientOptions): Client;
-    export function createStringClient(options?: ClientOptions): Client;
-    export function createClient(options?: ClientOptions): HttpClient;
+  export function createJsonClient(options?: ClientOptions): Client;
+  export function createStringClient(options?: ClientOptions): Client;
+  export function createClient(options?: ClientOptions): HttpClient;
 
-    export class ConflictError { constructor(message?: any); }
-    export class InvalidArguementError { constructor(message?: any); }
-    export class RestError { constructor(message?: any); }
-    export class BadDigestError { constructor(message: any); }
-    export class BadMethodError { constructor(message: any); }
-    export class BadRequestError { constructor(message: any); }
-    export class InternalError { constructor(message: any); }
-    export class InvalidContentError { constructor(message: any); }
-    export class InvalidCredentialsError { constructor(message: any); }
-    export class InvalidHeaderError { constructor(message: any); }
-    export class InvalidVersionError { constructor(message: any); }
-    export class MissingParameterError { constructor(message: any); }
-    export class NotAuthorizedError { constructor(message: any); }
-    export class RequestExpiredError { constructor(message: any); }
-    export class RequestThrottledError { constructor(message: any); }
-    export class ResourceNotFoundError { constructor(message: any); }
-    export class WrongAcceptError { constructor(message: any); }
+  export class ConflictError { constructor(message?: any); }
+  export class InvalidArguementError { constructor(message?: any); }
+  export class RestError { constructor(message?: any); }
+  export class BadDigestError { constructor(message: any); }
+  export class BadMethodError { constructor(message: any); }
+  export class BadRequestError { constructor(message: any); }
+  export class InternalError { constructor(message: any); }
+  export class InvalidContentError { constructor(message: any); }
+  export class InvalidCredentialsError { constructor(message: any); }
+  export class InvalidHeaderError { constructor(message: any); }
+  export class InvalidVersionError { constructor(message: any); }
+  export class MissingParameterError { constructor(message: any); }
+  export class NotAuthorizedError { constructor(message: any); }
+  export class RequestExpiredError { constructor(message: any); }
+  export class RequestThrottledError { constructor(message: any); }
+  export class ResourceNotFoundError { constructor(message: any); }
+  export class WrongAcceptError { constructor(message: any); }
 
-    export function acceptParser(parser: any): RequestHadler;
-    export function authorizationParser(): RequestHadler;
-    export function dateParser(skew?: number): RequestHadler;
-    export function queryParser(options?: Object): RequestHadler;
-    export function urlEncodedBodyParser(options?: Object): RequestHadler[];
-    export function jsonp(): RequestHadler;
-    export function gzipResponse(options?: Object): RequestHadler;
-    export function bodyParser(options?: Object): RequestHadler[];
-    export function requestLogger(options?: Object): RequestHadler;
-    export function serveStatic(options?: Object): RequestHadler;
-    export function throttle(options?: ThrottleOptions): RequestHadler;
-    export function conditionalRequest(): RequestHadler[];
-    export function auditLogger(options?: Object): Function;
-    export function fullResponse(): RequestHadler;
-    export var defaultResponseHeaders : any;
+  export function acceptParser(parser: any): RequestHadler;
+  export function authorizationParser(): RequestHadler;
+  export function dateParser(skew?: number): RequestHadler;
+  export function queryParser(options?: Object): RequestHadler;
+  export function urlEncodedBodyParser(options?: Object): RequestHadler[];
+  export function jsonp(): RequestHadler;
+  export function gzipResponse(options?: Object): RequestHadler;
+  export function bodyParser(options?: Object): RequestHadler[];
+  export function requestLogger(options?: Object): RequestHadler;
+  export function serveStatic(options?: Object): RequestHadler;
+  export function throttle(options?: ThrottleOptions): RequestHadler;
+  export function conditionalRequest(): RequestHadler[];
+  export function auditLogger(options?: Object): Function;
+  export function fullResponse(): RequestHadler;
+  export var defaultResponseHeaders : any;
 }
