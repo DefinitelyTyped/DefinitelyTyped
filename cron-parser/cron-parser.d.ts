fc1ef2218c6179f7c118d0493ea17dc24c9c732d
Nishant Kyal <nishant@infollion.com>
2014-03-02 18:30:19 +0530

    Async schedule generation and conflict checking super hack

--- cron-parser/cron-parser.d.ts
+++ cron-parser/cron-parser.d.ts
@@ -1,12 +1,17 @@
 declare module "cron-parser"
 {
     export function parseExpressionSync(expression, options?:Options);
-    export function parseExpression(expression, options:Options, callback:Function);
+    export function parseExpression(expression, options:Options, callback:(error:Error, cronExpression:CronExpression)=>void);
     export function parseString(data, callback:Function);
     export function parseFile(filePath:string, callback:Function);
 
     interface Options {
-        currentDate?:number;
-        endDate?:number;
+        currentDate?:Date;
+        endDate?:Date;
+    }
+
+    export class CronExpression
+    {
+        next():Date;
     }
 }
