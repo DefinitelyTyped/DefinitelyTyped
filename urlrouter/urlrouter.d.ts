e40a2da37d29bfd65794210646aa2767a7fa52ff
soywiz <soywiz@gmail.com>
2014-03-15 12:03:13 +0100

    - Updated urlrouter with tests and README.md

--- urlrouter/urlrouter.d.ts
+++ urlrouter/urlrouter.d.ts
@@ -8,56 +8,62 @@
 declare module "_UrlRouterInternal" {
     import http = require("http");
 
-    export interface ServerRequestExt extends http.ServerRequest {
+    export interface ServerRequest extends http.ServerRequest {
         params: any;
     }
 
-    export interface ServerResponseExt extends http.ServerResponse {
+    export interface ServerResponse extends http.ServerResponse {
 
     }
 
     export interface App {
         // https://github.com/visionmedia/node-methods/blob/master/index.js
-        get(urlpattern: string, handler: (req: ServerRequestExt, res: ServerResponseExt) => void): void;
-        post(urlpattern: string, handler: (req: ServerRequestExt, res: ServerResponseExt) => void): void;
-        put(urlpattern: string, handler: (req: ServerRequestExt, res: ServerResponseExt) => void): void;
-        head(urlpattern: string, handler: (req: ServerRequestExt, res: ServerResponseExt) => void): void;
-        delete(urlpattern: string, handler: (req: ServerRequestExt, res: ServerResponseExt) => void): void;
-        options(urlpattern: string, handler: (req: ServerRequestExt, res: ServerResponseExt) => void): void;
-        trace(urlpattern: string, handler: (req: ServerRequestExt, res: ServerResponseExt) => void): void;
-        copy(urlpattern: string, handler: (req: ServerRequestExt, res: ServerResponseExt) => void): void;
-        lock(urlpattern: string, handler: (req: ServerRequestExt, res: ServerResponseExt) => void): void;
-        mkcol(urlpattern: string, handler: (req: ServerRequestExt, res: ServerResponseExt) => void): void;
-        move(urlpattern: string, handler: (req: ServerRequestExt, res: ServerResponseExt) => void): void;
-        propfind(urlpattern: string, handler: (req: ServerRequestExt, res: ServerResponseExt) => void): void;
-        proppatch(urlpattern: string, handler: (req: ServerRequestExt, res: ServerResponseExt) => void): void;
-        unlock(urlpattern: string, handler: (req: ServerRequestExt, res: ServerResponseExt) => void): void;
-        report(urlpattern: string, handler: (req: ServerRequestExt, res: ServerResponseExt) => void): void;
-        mkactivity(urlpattern: string, handler: (req: ServerRequestExt, res: ServerResponseExt) => void): void;
-        checkout(urlpattern: string, handler: (req: ServerRequestExt, res: ServerResponseExt) => void): void;
-        merge(urlpattern: string, handler: (req: ServerRequestExt, res: ServerResponseExt) => void): void;
-        "m-search"(urlpattern: string, handler: (req: ServerRequestExt, res: ServerResponseExt) => void): void;
-        notify(urlpattern: string, handler: (req: ServerRequestExt, res: ServerResponseExt) => void): void;
-        subscribe(urlpattern: string, handler: (req: ServerRequestExt, res: ServerResponseExt) => void): void;
-        unsubscribe(urlpattern: string, handler: (req: ServerRequestExt, res: ServerResponseExt) => void): void;
-        patch(urlpattern: string, handler: (req: ServerRequestExt, res: ServerResponseExt) => void): void;
-        search(urlpattern: string, handler: (req: ServerRequestExt, res: ServerResponseExt) => void): void;
+        get(urlpattern: string, handler: HttpHandler): void;
+        post(urlpattern: string, handler: HttpHandler): void;
+        put(urlpattern: string, handler: HttpHandler): void;
+        head(urlpattern: string, handler: HttpHandler): void;
+        delete(urlpattern: string, handler: HttpHandler): void;
+        options(urlpattern: string, handler: HttpHandler): void;
+        trace(urlpattern: string, handler: HttpHandler): void;
+        copy(urlpattern: string, handler: HttpHandler): void;
+        lock(urlpattern: string, handler: HttpHandler): void;
+        mkcol(urlpattern: string, handler: HttpHandler): void;
+        move(urlpattern: string, handler: HttpHandler): void;
+        propfind(urlpattern: string, handler: HttpHandler): void;
+        proppatch(urlpattern: string, handler: HttpHandler): void;
+        unlock(urlpattern: string, handler: HttpHandler): void;
+        report(urlpattern: string, handler: HttpHandler): void;
+        mkactivity(urlpattern: string, handler: HttpHandler): void;
+        checkout(urlpattern: string, handler: HttpHandler): void;
+        merge(urlpattern: string, handler: HttpHandler): void;
+        "m-search"(urlpattern: string, handler: HttpHandler): void;
+        notify(urlpattern: string, handler: HttpHandler): void;
+        subscribe(urlpattern: string, handler: HttpHandler): void;
+        unsubscribe(urlpattern: string, handler: HttpHandler): void;
+        patch(urlpattern: string, handler: HttpHandler): void;
+        search(urlpattern: string, handler: HttpHandler): void;
 
-        all(urlpattern: string, handler: (req: ServerRequestExt, res: ServerResponseExt) => void): void;
+        all(urlpattern: string, handler: HttpHandler): void;
         redirect(urlpattern: string, to: string): void;
     }
 
     export interface Options {
         paramsName?: string;
-        pageNotFound?: (req: ServerRequestExt, res: ServerResponseExt) => void;
-        errorHandler?: (err:Error, req: ServerRequestExt, res: ServerResponseExt) => void;
+        pageNotFound?: (req: ServerRequest, res: ServerResponse) => void;
+        errorHandler?: (err:Error, req: ServerRequest, res: ServerResponse) => void;
     }
 
     export function _UrlRouterfunc(handler: (app: App) => void, options?: any): void;
+
+    export interface HttpHandler {
+        (req: ServerRequest, res: ServerResponse, next?: () => void): void;
+    }
+
 }
 
 declare module "urlrouter" {
     import _UrlRouterInternal = require("_UrlRouterInternal");
-    function _call(handler: (app: _UrlRouterInternal.App) => void): void;
-    export = _call;
+
+    function _UrlRouterInternal(handler: (app: _UrlRouterInternal.App) => void): _UrlRouterInternal.HttpHandler;
+    export = _UrlRouterInternal;
 }
