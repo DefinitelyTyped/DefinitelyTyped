cb75155c6112def43bc87455b75526010715a5e6
Nishant Kyal <nishant@infollion.com>
2014-03-18 23:00:33 +0530

    Solved dependency problems

--- twilio/twilio.d.ts
+++ twilio/twilio.d.ts
@@ -1,9 +1,34 @@
 ///<reference path='../q/Q.d.ts'/>
 
+declare module twilio
+{
+
+    export class RestClient
+    {
+        constructor(sid:string, tkn:string, options?:any);
+        sendMessage(options:Object, callback?:Function)
+        makeCall(options:Object, callback?:Function)
+    }
+
+    export class TwimlResponse
+    {
+
+    }
+
+    export class Capability
+    {
+
+    }
+}
+
+interface TwilioStatic
+{
+    (sid:string, tkn:string, options?:any):twilio.RestClient;
+
+}
+declare var twilioStatic:TwilioStatic
+
 declare module "twilio"
 {
-    import q            = require('q');
-
-    export function sendMessage(options:Object, callback?:Function):q.Promise<any>
-    export function makeCall(options:Object, callback?:Function):q.Promise<any>
+    export = twilioStatic;
 }
