74283c28f89208929d42ddab62c7165358851589
Igor Oleinikov <igor@oleinikov.ru>
2014-03-19 15:52:52 +0400

    Fixed `publishValue` and `replay` method signatures; Added `shareValue` and `replayWhileObserved` methods.

--- rx.js/rx.binding.d.ts
+++ rx.js/rx.binding.d.ts
@@ -54,8 +54,20 @@ declare module Rx {
 		publishLast(): ConnectableObservable<T>;
 		publishLast<TResult>(selector: (source: ConnectableObservable<T>) => Observable<TResult>): Observable<TResult>;
 		publishValue(initialValue: T): ConnectableObservable<T>;
-		publishValue<TResult>(selector: (source: ConnectableObservable<T>) => Observable<TResult>, initialValue: TResult): Observable<TResult>;
-
-		replay(selector?: (source: Observable<T>) => ReplaySubject<T>, bufferSize?: number, window?: number, scheduler?: IScheduler): ReplaySubject<T>;
+		publishValue<TResult>(selector: (source: ConnectableObservable<T>) => Observable<TResult>, initialValue: T): Observable<TResult>;
+		/**
+		* Returns an observable sequence that shares a single subscription to the underlying sequence and starts with an initialValue.
+		* This operator is a specialization of publishValue which creates a subscription when the number of observers goes from zero to one, then shares that subscription with all subsequent observers until the number of observers returns to zero, at which point the subscription is disposed.
+		* 
+		* @example
+		* var res = source.shareValue(42);
+		* 
+		* @param initialValue Initial value received by observers upon subscription.
+		* @returns An observable sequence that contains the elements of a sequence produced by multicasting the source sequence.
+		*/
+		shareValue(initialValue: T): Observable<T>;
+		replay(selector?: boolean, bufferSize?: number, window?: number, scheduler?: IScheduler): ConnectableObservable<T>;	// hack to catch first omitted parameter
+		replay(selector: (source: ConnectableObservable<T>) => Observable<T>, bufferSize?: number, window?: number, scheduler?: IScheduler): Observable<T>;
+		replayWhileObserved(bufferSize?: number, window?: number, scheduler?: IScheduler): Observable<T>;
 	}
 }
