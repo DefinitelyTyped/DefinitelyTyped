8823d0b6dc723574a2b542e581cac02865d9094e
Luigi Trabacchin <trabacchin.luigi@gmail.com>
2014-03-27 13:51:33 +0100

    added callbacks
    added callbacks methods for Element

--- svgjs.draggable/svgjs.draggable.d.ts
+++ svgjs.draggable/svgjs.draggable.d.ts
@@ -1,7 +1,20 @@
 declare module svgjs {
+	export module draggable {
+		export interface DragDelta {
+			x: number
+			y: number
+			zoom: number
+		}
+	}
+
 	export interface Element {
 		draggable(): Element
 		draggable(obj: Object):Element
 		fixed(): Element
+		beforedrag: (e) => any
+		dragstart: (delta: draggable.DragDelta, e) => any
+		dragmove: (delta: draggable.DragDelta, e) => any
+		dragend: (delta: draggable.DragDelta, e) => any
+
 	}
 }
