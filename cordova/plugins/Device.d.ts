e9188ebb3caf7b716138ca52e13807a5ef10aa07
sgrebnov <sergei.grebnov@gmail.com>
2014-03-29 18:24:39 +0400

    Adds bindings for Apache Cordova

---
+++ cordova/plugins/Device.d.ts
@@ -0,0 +1,31 @@
+// Type definitions for Apache Cordova Device plugin.
+// Project: https://github.com/apache/cordova-plugin-device
+// Definitions by: Microsoft Open Technologies, Inc. <http://msopentech.com>
+// Definitions: https://github.com/borisyankov/DefinitelyTyped
+// 
+// Copyright (c) Microsoft Open Technologies, Inc.
+// Licensed under the MIT license. 
+
+/**
+ * This plugin defines a global device object, which describes the device's hardware and software.
+ * Although the object is in the global scope, it is not available until after the deviceready event.
+ */
+interface Device {
+    /** Get the version of Cordova running on the device. */
+    cordova: string;
+    /**
+     * The device.model returns the name of the device's model or product. The value is set
+     * by the device manufacturer and may be different across versions of the same product.
+     */
+    model: string;
+    /** device.name is deprecated as of version 2.3.0. Use device.model instead. */
+    name: string;
+    /** Get the device's operating system name. */
+    platform: string;
+    /** Get the device's Universally Unique Identifier (UUID). */
+    uuid: string;
+    /** Get the operating system version. */
+    version: string;
+}
+
+declare var device: Device;
