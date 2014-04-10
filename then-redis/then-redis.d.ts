aac873fc5626cdbd94604e75a3acbe88a63b6012
Nishant Kyal <nishant@infollion.com>
2014-04-10 23:10:12 +0530

    Fixed multiple event listeners attach to redis client

--- then-redis/then-redis.d.ts
+++ then-redis/then-redis.d.ts
@@ -1,4 +1,15 @@
 declare module "then-redis"
 {
+    import q                                       = require('q');
 
+    export function createClient(options?:any):Client;
+    export function connect(options?:any):q.Promise<any>;
+
+    export class Client
+    {
+        constructor(options?:any);
+        connect():q.Promise<any>;
+        disconnect();
+        send(command:string, args:any[]):q.Promise<any>;
+    }
 }
