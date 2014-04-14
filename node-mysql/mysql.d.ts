a11e87c2db93f0d14c6efd7ada7682d7b781c1d5
Nishant Kyal <nishant@infollion.com>
2014-04-14 10:49:24 +0530

    DAO changes from alpha-calling merged

--- node-mysql/mysql.d.ts
+++ node-mysql/mysql.d.ts
@@ -3,4 +3,14 @@ declare module "mysql"
     export function createConnection(config);
     export function createPool(config);
     export function createPoolCluster(config);
+
+    export interface OkPacket
+    {
+        fieldCount:number;
+        affectedRows:number;
+        insertId:number;
+        serverStatus:number;
+        warningCount:number;
+        message:string;
+    }
 }
