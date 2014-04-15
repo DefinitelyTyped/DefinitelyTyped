07c13f0bdecf246ccade204b7525cc08ace75fe9
Pavel Zika <pzika@langmaster.cz>
2014-04-15 13:20:24 +0200

    First version

---
+++ hellojs/hellojs-test.ts
@@ -0,0 +1,30 @@
+hello.init(
+  {
+    'facebook': '<app key>',
+  },
+  {
+    redirect_uri: 'hello.html',
+    display: 'page',
+  }
+  );
+
+hello('facebook').login();
+
+hello('facebook').logout();
+
+hello.on('auth.login', auth => {
+  alert('log to ' + auth.network)
+}).on('auth.logout', auth => {
+    alert('unlog from ' + auth.network)
+  });
+
+hello.getAuthResponse('facebook');
+
+hello.login('facebook', null, () => {
+  var req = hello.getAuthResponse('facebook');
+});
+
+hello.logout('facebook');
+
+var serviceInfo = hello.service('facebook');
+
