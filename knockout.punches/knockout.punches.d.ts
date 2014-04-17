359fb827eefa3faf1cc438d3dbac6ef697ed56d9
stephenlautier <stephen.lautier@outlook.com>
2014-04-17 23:14:33 +0200

    added URITemplate for urijs  and knockout.punches
    - added UriTemplate class + expand method for urijs
    - added knockout.punches module + enableAll

---
+++ knockout.punches/knockout.punches.d.ts
@@ -0,0 +1,13 @@
+﻿interface KnockoutPunchesStatic {
+	enableAll(): void;
+}
+
+interface KnockoutStatic {
+	punches: KnockoutPunchesStatic;
+}
+
+declare module "knockout.punches" {
+	export = punches;
+}
+
+declare var punches: KnockoutPunchesStatic;
