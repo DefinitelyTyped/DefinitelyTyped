3803a78cf76472404ad97bc7b548e80f4c168306
Max Ackley <maxwell.ackley@gmail.com>
2014-04-18 10:05:55 -0700

    Added type definitions and tests for jQuery Finger plugin.

---
+++ jquery.finger/jquery.finger-tests.ts
@@ -0,0 +1,9 @@
+﻿/// <reference path="../jquery/jquery.d.ts"/>
+/// <reference path="jquery.finger.d.ts"/>
+
+$.Finger.doubleTapInterval = 400;
+$.Finger.flickDuration = 250;
+$.Finger.pressDuration = 100;
+$.Finger.motionThreshhold = 10;
+$.Finger.preventDefault = true;
+var fingerEventObject: JQueryFingerEventObject;
