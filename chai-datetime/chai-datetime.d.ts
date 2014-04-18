7e664f7f9e69746befbb91bf7e7ef4b7727d5fe3
Cliff Burger <cliffburger@daptiv.com>
2014-04-17 17:00:04 -0700

    Add chai-datetime and tests that are currently failing

---
+++ chai-datetime/chai-datetime.d.ts
@@ -0,0 +1,10 @@
+// Type definitions for chai-datetime
+// Project: https://github.com/gaslight/chai-datetime.git
+// Definitions by: Cliff Burger <https://github.com/cliffburger/>
+// DefinitelyTyped: https://github.com/borisyankov/DefinitelyTyped
+
+/// <reference path="../chai/chai.d.ts" />
+
+declare module chai {
+
+}
