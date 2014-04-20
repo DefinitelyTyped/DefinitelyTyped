92b0a29235784e09118d089ddd84b07a99080750
David Driscoll <david.driscoll@gmail.com>
2014-04-20 14:35:47 -0400

    Fixed a few gulp definitions.

--- gulp/gulp.d.ts
+++ gulp/gulp.d.ts
@@ -2,6 +2,7 @@
 // Project: http://gulpjs.com/
 // Definitions by: David Driscoll <http://www.blacklite.ca>
 // Definitions: https://github.com/borisyankov/DefinitelyTyped
+/// <reference path="../node/node.d.ts" />
 declare module Gulp {
 	interface ISrcOptions {
 		buffer?: boolean;
@@ -23,22 +24,22 @@ interface IGulp {
 	src(glob: string, options?: Gulp.ISrcOptions): ReadableStream;
     src(globs: string[], options?: Gulp.ISrcOptions): ReadableStream;
 
-    dest(path: string);
+    dest(path: string): ReadableStream;
 
-    task(name: string, fn: (done?: (error?) => void) => void): ReadableStream;
-    task(name: string, deps: string[], fn?: (done?: (error?) => void) => void): ReadableStream;
+    task(name: string, fn: (done?: (error?: any) => void) => void): ReadableStream;
+    task(name: string, deps: string[], fn?: (done?: (error?: any) => void) => void): ReadableStream;
 
-    watch(glob: string, tasks: string[]);
-    watch(glob: string, options: Gulp.IGaze, tasks: string[]);
-    watch(glob: string[], tasks: string[]);
-    watch(glob: string[], options: Gulp.IGaze, tasks: string[]);
+    watch(glob: string, tasks: string[]): NodeEventEmitter;
+    watch(glob: string, options: Gulp.IGaze, tasks: string[]): NodeEventEmitter;
+    watch(glob: string[], tasks: string[]): NodeEventEmitter;
+    watch(glob: string[], options: Gulp.IGaze, tasks: string[]): NodeEventEmitter;
 
-    watch(glob: string, callback: (cb: Gulp.IEvent) => void);
-    watch(glob: string, options: Gulp.IGaze, callback: (cb: Gulp.IEvent) => void);
-    watch(glob: string[], callback: (cb: Gulp.IEvent) => void);
-    watch(glob: string[], options: Gulp.IGaze, callback: (cb: Gulp.IEvent) => void);
+    watch(glob: string, callback: (cb: Gulp.IEvent) => void): NodeEventEmitter;
+    watch(glob: string, options: Gulp.IGaze, callback: (cb: Gulp.IEvent) => void): NodeEventEmitter;
+    watch(glob: string[], callback: (cb: Gulp.IEvent) => void): NodeEventEmitter;
+    watch(glob: string[], options: Gulp.IGaze, callback: (cb: Gulp.IEvent) => void): NodeEventEmitter;
 
-    start(...tasks: any[]);
+    start(...tasks: any[]): void;
 }
 
 declare var __IGulp: IGulp;
