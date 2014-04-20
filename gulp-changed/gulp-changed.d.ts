88b0b0b3d0299b5f08dd6437f516cadadb7a6a91
David Driscoll <david.driscoll@gmail.com>
2014-04-20 14:40:20 -0400

    Added gulp-changed

---
+++ gulp-changed/gulp-changed.d.ts
@@ -0,0 +1,18 @@
+﻿// Type definitions for gulp-changed
+// Project: https://github.com/sindresorhus/gulp-changed
+// Definitions by: David Driscoll <http://www.blacklite.ca>
+// Definitions: https://github.com/borisyankov/DefinitelyTyped
+declare module Gulp {
+	interface IChangedOptions {
+		cwd?: string;
+		extension?: string;
+	}
+}
+interface IGulpChanged {
+    (dest: string, options?: Gulp.IChangedOptions): any;
+}
+
+declare var __IGulpChanged: IGulpChanged;
+declare module "gulp-changed" {
+    export = __IGulpChanged;
+}	
