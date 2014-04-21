47f6a57e0f4ae2972b17afcdc5342e456fd4cf53
Max Ackley <maxwell.ackley@gmail.com>
2014-04-21 13:34:32 -0700

    Added more robust tests for JQueryFingerEventObject. Added override of event type for JQuery touch events.

--- jquery.finger/jquery.finger.d.ts
+++ jquery.finger/jquery.finger.d.ts
@@ -90,6 +90,20 @@ interface JQueryFingerEventObject extends JQueryEventObject {
     direction: number;
 }
 
+interface JQuery {
+    on(events: 'tap', handler: (eventObject: JQueryFingerEventObject, ...args: any[]) => any): JQuery;
+    on(events: 'doubletap', handler: (eventObject: JQueryFingerEventObject, ...args: any[]) => any): JQuery;
+    on(events: 'press', handler: (eventObject: JQueryFingerEventObject, ...args: any[]) => any): JQuery;
+    on(events: 'drag', handler: (eventObject: JQueryFingerEventObject, ...args: any[]) => any): JQuery;
+    on(events: 'flick', handler: (eventObject: JQueryFingerEventObject, ...args: any[]) => any): JQuery;
+
+    on(events: 'tap', data: any, handler: (eventObject: JQueryFingerEventObject, ...args: any[]) => any): JQuery;
+    on(events: 'doubletap', data: any, handler: (eventObject: JQueryFingerEventObject, ...args: any[]) => any): JQuery;
+    on(events: 'press', data: any, handler: (eventObject: JQueryFingerEventObject, ...args: any[]) => any): JQuery;
+    on(events: 'drag', data: any, handler: (eventObject: JQueryFingerEventObject, ...args: any[]) => any): JQuery;
+    on(events: 'flick', data: any, handler: (eventObject: JQueryFingerEventObject, ...args: any[]) => any): JQuery;
+}
+
 interface JQueryStatic {
     Finger: JQueryFinger.JQueryFingerOptions;
 }
