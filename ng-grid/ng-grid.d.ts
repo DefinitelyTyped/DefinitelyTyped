2fb97323889b1daa956e610164e13d5d05b59d90
kentcooper <kent@advantsystems.com>
2014-04-26 00:57:53 -0400

    Added my name the ng-grid header

--- ng-grid/ng-grid.d.ts
+++ ng-grid/ng-grid.d.ts
@@ -1,6 +1,6 @@
 // Type definitions for ng-grid 
 // Project: http://angular-ui.github.io/ng-grid/
-// Definitions by: Ken Smith <https://github.com/smithkl42>
+// Definitions by: Ken Smith <https://github.com/smithkl42> and Kent Cooper <https://github.com/kentcooper>
 // DefinitelyTyped: https://github.com/borisyankov/DefinitelyTyped
 
 // These are very definitely preliminary. Please feel free to improve.
