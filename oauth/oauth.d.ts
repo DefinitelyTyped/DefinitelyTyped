ce202f2246fabb332ce91426756255611f285928
Nishant <nishant.kyal@gmail.com>
2014-04-30 01:54:36 +0530

    Payment page fixed

--- oauth/oauth.d.ts
+++ oauth/oauth.d.ts
@@ -3,5 +3,6 @@ declare module "oauth"
     export class OAuth
     {
         constructor(requestUrl, accessUrl, consumerKey, consumerSecret, version, authorize_callback, signatureMethod, nonceSize?, customHeaders?);
+        get(url:string, accessToken:string, refreshToken:string, callback:Function);
     }
 }
