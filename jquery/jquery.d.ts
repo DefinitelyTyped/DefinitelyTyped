9241ca1c7b282d06c209e56b0e757c09d6dec070
Marc-Andre Roy <marc596@gmail.com>
2014-05-13 06:10:38 -0400

    Update jquery.d.ts

--- jquery/jquery.d.ts
+++ jquery/jquery.d.ts
@@ -1359,6 +1359,13 @@ interface JQuery {
      * @param value A value to set for the attribute.
      */
     attr(attributeName: string, value: number): JQuery;
+        /**
+     * Set one or more attributes for the set of matched elements.
+     *
+     * @param attributeName The name of the attribute to set.
+     * @param value A value to set for the attribute.
+     */
+    attr(attributeName: string, value: boolean): JQuery;
     /**
      * Set one or more attributes for the set of matched elements.
      *
