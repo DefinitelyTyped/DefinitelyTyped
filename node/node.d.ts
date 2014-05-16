6d7ea45eb05d7cf3204c663ce2091c644e2a296b
Colin Snover <csnover@users.noreply.github.com>
2014-05-16 12:25:08 -0500

    Fix bad signature of ClientRequest#setNoDelay
    `noDelay` argument should be boolean, not Function

--- node/node.d.ts
+++ node/node.d.ts
@@ -327,7 +327,7 @@ declare module "http" {
         write(chunk: any, encoding?: string): void;
         abort(): void;
         setTimeout(timeout: number, callback?: Function): void;
-        setNoDelay(noDelay?: Function): void;
+        setNoDelay(noDelay?: boolean): void;
         setSocketKeepAlive(enable?: boolean, initialDelay?: number): void;
 
         // Extended base methods
