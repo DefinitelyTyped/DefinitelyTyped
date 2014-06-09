b07a83f244df88ac568875e86f6bbdceb4c1f604
Jonathan Pirnay <mail@johnnycrab.com>
2014-06-09 22:39:35 +0200

    Changed HMAC and Hash params to `any`, as Buffers are also allowed / returned.

--- node/node.d.ts
+++ node/node.d.ts
@@ -1040,14 +1040,14 @@ declare module "crypto" {
     export interface Credentials { context?: any; }
     export function createCredentials(details: CredentialDetails): Credentials;
     export function createHash(algorithm: string): Hash;
-    export function createHmac(algorithm: string, key: string): Hmac;
+    export function createHmac(algorithm: string, key: any): Hmac;
     interface Hash {
         update(data: any, input_encoding?: string): Hash;
-        digest(encoding?: string): string;
+        digest(encoding?: string): any;
     }
     interface Hmac {
         update(data: any, input_encoding?: string): Hmac;
-        digest(encoding?: string): string;
+        digest(encoding?: string): any;
     }
     export function createCipher(algorithm: string, password: any): Cipher;
     export function createCipheriv(algorithm: string, key: any, iv: any): Cipher;
