3d64ea7395351f031436b52adb5cbc3906c8da4e
Bart van der Schoor <bartvanderschoor@gmail.com>
2014-06-16 01:59:21 +0200

    added missing urls to some authors
    fixed node urls

--- q/Q.d.ts
+++ q/Q.d.ts
@@ -1,6 +1,6 @@
 // Type definitions for Q
 // Project: https://github.com/kriskowal/q
-// Definitions by: Barrie Nemetchek, Andrew Gaspar, John Reilly
+// Definitions by: Barrie Nemetchek <https://github.com/bnemetchek>, Andrew Gaspar <https://github.com/AndrewGaspar/>, John Reilly <https://github.com/johnnyreilly>
 // Definitions: https://github.com/borisyankov/DefinitelyTyped  
 
 /// <reference path="../jquery/jquery.d.ts"/>
