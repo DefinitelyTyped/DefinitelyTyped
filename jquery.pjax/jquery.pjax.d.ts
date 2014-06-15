773cc46b0f1762da6bf361b55dbd27433b33cec7
Junle <lijunle@gmail.com>
2014-06-16 01:53:56 +0800

    Fix the asterisk bug and a trivial bug.

--- jquery.pjax/jquery.pjax.d.ts
+++ jquery.pjax/jquery.pjax.d.ts
@@ -4,10 +4,10 @@
 /// <reference path="../jquery/jquery.d.ts" />
 
 interface JQuery {
-    /*
+    /**
      * Tell PJAX to listen links with delegation selector that, when click on them, fetches the href with ajax into the container.
      * Tries to make sure the back button and ctrl+click work the way you'd expect.
-     * If `options.container` is not defined, the `data-pjax` attribute of the link will be treated as container,
+     * If `options.container` is not defined, the `data-pjax` attribute of the link will be treated as container.
      * If such an attribute is not defined too, the context runs with this statement will be treated as container.
      * @param delegationSelector The selector to limit which links PJAX should listen on.
      * @param options A valid jQuery ajax options object that may include these pjax specific options:
@@ -18,10 +18,10 @@ interface JQuery {
      */
     pjax(delegationSelector: string, options?: JQueryAjaxSettings): JQuery;
 
-    /*
+    /**
      * Tell PJAX to listen links with delegation selector that, when click on them, fetches the href with ajax into the container.
      * Tries to make sure the back button and ctrl+click work the way you'd expect.
-     * If `options.container` is not defined, the `data-pjax` attribute of the link will be treated as container,
+     * If `options.container` is not defined, the `data-pjax` attribute of the link will be treated as container.
      * If such an attribute is not defined too, the context runs with this statement will be treated as container.
      * @param delegationSelector The selector to limit which links PJAX should listen on.
      * @param containerSelector A jQuery selector indicates where to stick the response body. E.g., $(container).html(xhr.responseBody).
