c77967fb9a4821bd170e441377070578df86f225
Junle Li <lijunle@gmail.com>
2014-06-16 01:42:56 +0800

    Fix a asterisk bug.

--- jquery/jquery.d.ts
+++ jquery/jquery.d.ts
@@ -172,7 +172,7 @@ interface JQueryXHR extends XMLHttpRequest, JQueryPromise<any> {
      * Incorporates the functionality of the .done() and .fail() methods, allowing (as of jQuery 1.8) the underlying Promise to be manipulated. Refer to deferred.then() for implementation details.
      */
     then(doneCallback: (data: any, textStatus: string, jqXHR: JQueryXHR) => void, failCallback: (jqXHR: JQueryXHR, textStatus: string, errorThrown: any) => void): JQueryPromise<any>;
-    /*
+    /**
      * Property containing the parsed response if the response Content-Type is json
      */
     responseJSON?: any;
