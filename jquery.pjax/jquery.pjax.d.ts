2b42566ccac817280e5742626b225851b889c5b3
Junle <lijunle@gmail.com>
2014-06-18 16:31:28 +0800

    Add definition and test for $.pjax function.

--- jquery.pjax/jquery.pjax.d.ts
+++ jquery.pjax/jquery.pjax.d.ts
@@ -59,6 +59,16 @@ interface JQueryStatic {
 
 interface PjaxStatic {
     /**
+     * Loads a URL with ajax, puts the response body inside a container, then pushState()'s the loaded URL.
+     * Works just like $.ajax in that it accepts a jQuery ajax settings object (with keys like url, type, data, etc).
+     * @param options PJAX settings, which is a superset of jQuery AJAX settings. It includes the following specific options:
+     * - container: a jQuery selector indicates where to stick the response body. E.g., $(container).html(xhr.responseBody).
+     * - push: a boolean indicates whether to pushState the URL. Default is true.
+     * - replace: a boolean indicates whether to use replaceState instead of pushState. Default is false.
+     */
+    (options?: PjaxSettings): JQueryXHR;
+
+    /**
      * PJAX on click handler.
      * @param event A jQuery click event.
      * @param options PJAX settings, which is a superset of jQuery AJAX settings. It includes the following specific options:
