82463d265645a271180e8b822e56d8d0fc4c379b
Bart van der Schoor <bartvanderschoor@gmail.com>
2014-06-19 17:43:47 +0200

    fixed url in buffer-equal

--- buffer-equal/buffer-equal.d.ts
+++ buffer-equal/buffer-equal.d.ts
@@ -1,5 +1,5 @@
 // Type definitions for buffer-equal 1.0 0
-// Project: https://github.com/chaijs/assertion-error
+// Project: https://github.com/substack/node-buffer-equal
 // Definitions by: Bart van der Schoor <https://github.com/Bartvds>
 // Definitions: https://github.com/borisyankov/DefinitelyTyped
 
