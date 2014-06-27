63d20098dfbb557c8a4992bf9a3c63f578ca21bf
Omid K. Rad <omidkrad@users.noreply.github.com>
2014-06-26 19:38:07 -0700

    Added comments about options of Date type
    and fixed the build

--- bootstrap.datepicker/bootstrap.datepicker.d.ts
+++ bootstrap.datepicker/bootstrap.datepicker.d.ts
@@ -6,20 +6,26 @@
 /// <reference path="../jquery/jquery.d.ts"/>
 
 /**
- * See online docs: http://bootstrap-datepicker.readthedocs.org/
+ * All options that take a “Date” can handle a Date object; a String
+ * formatted according to the given format; or a timedelta relative
+ * to today, eg “-1d”, “+6m +1y”, etc, where valid units are “d” (day),
+ * “w” (week), “m” (month), and “y” (year).
+ * 
+ * See online docs for more info:
+ *  http://bootstrap-datepicker.readthedocs.org/en/release/options.html
  */
 interface DatepickerOptions {
     format?: string;
     weekStart?: number;
-    startDate?: Date;
-    endDate?: Date;
+    startDate?: any;
+    endDate?: any;
     autoclose?: boolean;
     startView?: number;
     todayBtn?: any;
     todayHighlight?: boolean;
     keyboardNavigation?: boolean;
     language?: string;
-    beforeShowDay?: (Date) => any;
+    beforeShowDay?: (date: any) => any;
     calendarWeeks?: boolean;
     clearBtn?: boolean;
     daysOfWeekDisabled?: number[];
