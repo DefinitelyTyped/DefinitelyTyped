e5c769bdb5fbeb9e21024f2bf1898efa934f64df
Franky Lau <killalau1988@hotmail.com>
2014-07-03 12:22:04 +0800

    Add missing API for underscore
    Add utility function unescape()

--- underscore/underscore.d.ts
+++ underscore/underscore.d.ts
@@ -1324,6 +1324,13 @@ interface UnderscoreStatic {
 	escape(str: string): string;
 
 	/**
+	* The opposite of escape, replaces &amp;, &lt;, &gt;, &quot;, and &#x27; with their unescaped counterparts.
+	* @param str HTML escaped string.
+	* @return `str` Raw string.
+	**/
+	unescape(str: string): string;
+
+	/**
 	* If the value of the named property is a function then invoke it; otherwise, return it.
 	* @param object Object to maybe invoke function `property` on.
 	* @param property The function by name to invoke on `object`.
@@ -2135,6 +2142,12 @@ interface Underscore<T> {
 	escape(): string;
 
 	/**
+	* Wrapped type `string`.
+	* @see _.unescape
+	**/
+	unescape(): string;
+
+	/**
 	* Wrapped type `object`.
 	* @see _.result
 	**/
@@ -2920,6 +2933,12 @@ interface _Chain<T> {
 	* @see _.escape
 	**/
 	escape(): _Chain<T>;
+
+	/**
+	* Wrapped type `string`.
+	* @see _.unescape
+	**/
+	unescape(): _Chain<T>;
 
 	/**
 	* Wrapped type `object`.
