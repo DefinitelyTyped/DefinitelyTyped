970cc5da6cd87bee0602c7d234cbd1559af3114c
Nishant <nishant.kyal@gmail.com>
2014-07-09 14:26:08 +0000

    OAuth typescript

--- oauth/oauth.d.ts
+++ oauth/oauth.d.ts
@@ -4,5 +4,6 @@ declare module "oauth"
     {
         constructor(requestUrl, accessUrl, consumerKey, consumerSecret, version, authorize_callback, signatureMethod, nonceSize?, customHeaders?);
         get(url:string, accessToken:string, refreshToken:string, callback:Function);
+        post(url:string, oauth_token:string, oauth_token_secret:string, post_body:Object, post_content_type:string, callback):Function;
     }
 }
