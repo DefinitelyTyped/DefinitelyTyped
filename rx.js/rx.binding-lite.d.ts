3399c7e738d4121fbe034514daf2d6ec09f99e92
Igor Oleinikov <igor@oleinikov.ru>
2014-07-15 09:11:16 -0700

    `shareReplay` moved to `rx.binding-lite.d.ts`. Removed `replayWhileObserved` from `rx.binding.d.ts` (replaced by `shareReplay`).

--- rx.js/rx.binding-lite.d.ts
+++ rx.js/rx.binding-lite.d.ts
@@ -67,5 +67,6 @@ declare module Rx {
 		shareValue(initialValue: T): Observable<T>;
 		replay(selector?: boolean, bufferSize?: number, window?: number, scheduler?: IScheduler): ConnectableObservable<T>;	// hack to catch first omitted parameter
 		replay(selector: (source: ConnectableObservable<T>) => Observable<T>, bufferSize?: number, window?: number, scheduler?: IScheduler): Observable<T>;
+		shareReplay(bufferSize?: number, window?: number, scheduler?: IScheduler): Observable<T>;
 	}
 }
--- rx.js/rx.binding.d.ts
+++ rx.js/rx.binding.d.ts
@@ -6,12 +6,6 @@
 ///<reference path="rx.d.ts" />
 ///<reference path="rx.binding-lite.d.ts" />
 
-declare module Rx {
-	export interface Observable<T> {
-		replayWhileObserved(bufferSize?: number, window?: number, scheduler?: IScheduler): Observable<T>;
-	}
-}
-
 declare module "rx.binding" {
 	export = Rx;
 }
