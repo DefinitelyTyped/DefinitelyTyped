c9370c43268664be14a67f139137c8cc7affa71a
Kensuke Matsuzaki <zakki@peppermint.jp>
2014-07-18 14:45:24 +0900

    Fix jQuery BlockUI Plugin

--- jquery.blockUI/jquery.blockUI-tests.ts
+++ jquery.blockUI/jquery.blockUI-tests.ts
@@ -9,3 +9,17 @@ $.blockUI(opt);
 $.unblockUI();
 $("#test").block().unblock();
 $("#test").block(opt);
+
+$.blockUI.defaults.css.border = '5px solid red';
+$.blockUI.defaults.fadeOut = 200;
+
+$.blockUI({ message: $('#domMessage') });
+$.unblockUI({ fadeOut: 200 });
+
+$.blockUI({
+    fadeIn: 1000,
+    timeout: 2000,
+    onBlock: function() {
+        alert('Page is now blocked; fadeIn complete');
+    }
+});
