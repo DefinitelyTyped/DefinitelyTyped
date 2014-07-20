dc4d81a6bda11013ce1e1752a53a8cf00606adfd
Carl de Billy <carl.debilly@gmail.com>
2014-07-20 12:59:27 -0400

    Remove useless reference to RxJS
    This reference is not needed and is now causing problem because the
    definitelyTyped for RxJS is now part of the RxJS package directly.  The
    Nuget package will needed to be changed also.

--- knockout.rx/knockout.rx.d.ts
+++ knockout.rx/knockout.rx.d.ts
@@ -4,7 +4,6 @@
 // Definitions: https://github.com/borisyankov/DefinitelyTyped
 
 /// <reference path="../knockout/knockout.d.ts"/>
-/// <reference path="../rx.js/rx.d.ts"/>
 
 interface KnockoutSubscribableFunctions<T> {
 	toObservable(event?: string): Rx.Observable<T>;
