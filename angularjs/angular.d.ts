fe47b495c8aa7ee551fb3239fb08c93f15a05f92
Tobias Bengfort <tobias.bengfort@liqd.de>
2014-07-21 11:09:13 +0200

    angularjs: allow $q.when to be called w/o args
    The current definition does not cover the case that the input type is
    void.

--- angularjs/angular.d.ts
+++ angularjs/angular.d.ts
@@ -565,6 +565,12 @@ declare module ng {
          * @param value Value or a promise
          */
         when<T>(value: T): IPromise<T>;
+        /**
+         * Wraps an object that might be a value or a (3rd party) then-able promise into a $q promise. This is useful when you are dealing with an object that might or might not be a promise, or if the promise comes from a source that can't be trusted.
+         * 
+         * @param value Value or a promise
+         */
+        when(): IPromise<void>;
     }
 
     interface IPromise<T> {
