f9f1da7c1adb329c72e5e7c5933764421e64b4f3
San Chen <bigsan.chen@gmail.com>
2014-08-05 09:34:14 +0800

    needle definitions and tests added

--- needle/needle.d.ts
+++ needle/needle.d.ts
@@ -3,14 +3,84 @@
 // Definitions by: San Chen <https://github.com/bigsan>
 // Definitions: https://github.com/borisyankov/DefinitelyTyped
 
-interface INeedle {
-	head(url: string): IReadableStream;
-	head(url: string, callback?: Function): IReadableStream;
-	head(url: string, options?: any, callback?: Function): IReadableStream;
+/// <reference path="../node/node.d.ts" />
 
+declare module Needle {
+
+    interface Callback {
+        (error: Error, response: any, body: any): void;
+    }
+
+    interface RequestOptions {
+        timeout?: number;
+        follow?: any; // number | string
+        multipart?: boolean;
+        proxy?: string;
+        agent?: string;
+        headers?: any;
+        auth?: string; // auto | digest | basic (default)
+        json?: boolean;
+    }
+
+    interface ResponseOptions {
+        decode?: boolean;
+        parse?: boolean;
+        output?: any;
+    }
+
+    interface HttpHeaderOptions {
+        compressed?: boolean;
+        username?: string;
+        password?: string;
+        accept?: string;
+        connection?: string;
+        user_agent?: string;
+    }
+
+    interface TLSOptions {
+        pfx?: any;
+        key?: any;
+        passphrase?: string;
+        cert?: any;
+        ca?: any;
+        ciphers?: any;
+        rejectUnauthorized?: boolean;
+        secureProtocol?: any;
+    }
+
+    interface NeedleOptions extends RequestOptions, ResponseOptions, HttpHeaderOptions, TLSOptions {
+    }
+
+    interface NeedleStatic {
+        defaults(options?: any): void;
+
+        head(url: string): ReadableStream;
+        head(url: string, callback?: Callback): ReadableStream;
+        head(url: string, options?: RequestOptions, callback?: Callback): ReadableStream;
+
+        get(url: string): ReadableStream;
+        get(url: string, callback?: Callback): ReadableStream;
+        get(url: string, options?: RequestOptions, callback?: Callback): ReadableStream;
+
+        post(url: string, data: any): ReadableStream;
+        post(url: string, data: any, callback?: Callback): ReadableStream;
+        post(url: string, data: any, options?: RequestOptions, callback?: Callback): ReadableStream;
+
+        put(url: string, data: any): ReadableStream;
+        put(url: string, data: any, callback?: Callback): ReadableStream;
+        put(url: string, data: any, options?: RequestOptions, callback?: Callback): ReadableStream;
+
+        delete(url: string, data: any): ReadableStream;
+        delete(url: string, data: any, callback?: Callback): ReadableStream;
+        delete(url: string, data: any, options?: RequestOptions, callback?: Callback): ReadableStream;
+
+        request(method: string, url: string, data: any): ReadableStream;
+        request(method: string, url: string, data: any, callback?: Callback): ReadableStream;
+        request(method: string, url: string, data: any, options?: RequestOptions, callback?: Callback): ReadableStream;
+    }
 }
 
 declare module "needle" {
-	var needle: INeedle;
+    var needle: Needle.NeedleStatic;
     export = needle;
 }
