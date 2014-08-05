bfa2effec308d5bd9dcdd6c3f420b873eff8889e
MIZUNE Pine <pinemz@gmail.com>
2014-08-05 11:20:19 +0900

    Fix header

--- http-string-parser/http-string-parser.d.ts
+++ http-string-parser/http-string-parser.d.ts
@@ -1,4 +1,4 @@
-﻿// Type definitions for Backbone v0.9.10
+﻿// Type definitions for http-string-parser
 // Project: https://github.com/apiaryio/http-string-parser
 // Definitions by: MIZUNE Pine <https://github.com/pine613>
 // Definitions: https://github.com/borisyankov/DefinitelyTyped
