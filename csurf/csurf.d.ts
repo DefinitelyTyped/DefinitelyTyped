451f712613ab0effc1318a5d55e047764d1c53ac
Horiuchi_H <horiuchi.g@gmail.com>
2014-08-20 00:27:36 +0900

    change Project URL

--- csurf/csurf.d.ts
+++ csurf/csurf.d.ts
@@ -1,5 +1,5 @@
 // Type definitions for csurf
-// Project: http://expressjs.com
+// Project: https://www.npmjs.org/package/csurf
 // Definitions by: Hiroki Horiuchi <https://github.com/horiuchi/>
 // Definitions: https://github.com/borisyankov/DefinitelyTyped
 
