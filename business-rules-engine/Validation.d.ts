5bb5dac8ad5be3e355f7cacd7118addd5d715af5
rsamec <roman.samec2@gmail.com>
2014-08-20 09:36:15 +0200

    typings header added

--- business-rules-engine/Validation.d.ts
+++ business-rules-engine/Validation.d.ts
@@ -1,3 +1,8 @@
+// Type definitions for business-rules-engine - v1.0.20
+// Project: https://github.com/rsamec/form
+// Definitions by: Roman Samec <https://github.com/rsamec>
+// Definitions: https://github.com/borisyankov/DefinitelyTyped
+
 /// <reference path="../q/q.d.ts" />
 /// <reference path="../underscore/underscore.d.ts" />
 declare module Validation {
