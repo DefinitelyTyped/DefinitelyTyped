a21e99a99968e2156b0f3cb0044d20808d5babbf
Kon P <kon@konpik.com>
2014-08-30 14:15:33 -0700

    Re-adding original definition owner

--- bootbox/bootbox.d.ts
+++ bootbox/bootbox.d.ts
@@ -1,5 +1,6 @@
 // Type definitions for Bootbox 4.0.0
 // Project: https://github.com/makeusabrew/bootbox
+// Definitions by: Vincent Bortone <https://github.com/vbortone/>
 // Definitions by: Kon Pik <https://github.com/konpikwastaken/>
 // Definitions: https://github.com/borisyankov/DefinitelyTyped
 
