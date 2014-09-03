32bfad140fa864dea286f5f5db7e9385f7172cbc
Earl Ferguson <ejferg@earls-mbp.attlocal.net>
2014-09-03 17:08:59 -0400

    Header cleanup

--- parse/parse.d.ts
+++ parse/parse.d.ts
@@ -1,6 +1,6 @@
 // Type definitions for Parse v1.2.19
 // Project: https://parse.com/
-// Definitions by: Ullisen Media Group LLC <http://ullisenmedia.com>
+// Definitions by: Ullisen Media Group <http://ullisenmedia.com>
 // Definitions: https://github.com/borisyankov/DefinitelyTyped
 
 /// <reference path="../node/node.d.ts" />
