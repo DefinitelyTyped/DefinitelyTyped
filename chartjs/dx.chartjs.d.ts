2c3ed445c0d4455288c993abf83a8c74eedf893b
Igor Kriklivets <igor.kriklivets@hotmail.com>
2014-09-11 09:10:21 +0400

    Headers restored

--- chartjs/dx.chartjs.d.ts
+++ chartjs/dx.chartjs.d.ts
@@ -1,1 +1,6 @@
+// Type definitions for ChartJS
+// Project: http://js.devexpress.com/WebDevelopment/Charts/
+// Definitions by: DevExpress Inc. <http://devexpress.com/>
+// Definitions: https://github.com/borisyankov/DefinitelyTyped
+
 /// <reference path="../devextreme/dx.chartjs.d.ts" />
