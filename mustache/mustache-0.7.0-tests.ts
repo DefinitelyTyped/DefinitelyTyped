e4dd6713a9cae2b24b6a79632cf183bf432cd3e7
Mark Bell <me@markashleybell.com>
2014-09-23 06:28:25 +0100

    Add definitions for mustache.js 0.8.2
    Add new definitions for 0.8.2, move 0.7 definitions and tests into
    versioned files and remove empty .tscparams files.

---
+++ mustache/mustache-0.7.0-tests.ts
@@ -0,0 +1,11 @@
+/// <reference path="mustache-0.7.0.d.ts" />
+
+var view = { title: "Joe", calc: function () { return 2 + 4; } };
+var output = Mustache.render("{{title}} spends {{calc}}", view);
+
+var person;
+var template = "<h1>{{firstName}} {{lastName}}</h1>Blog: {{blogURL}}";
+var html = Mustache.to_html(template, person);
+
+var writer = Mustache.compile(template);
+var writerOutput = writer(view);
