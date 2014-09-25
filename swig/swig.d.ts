0e05ddf68ee5df0109c616a484d437857a572ac9
CodeAnimal <codeanimal@outlook.com>
2014-09-25 01:40:38 +0100

    Update header info
    Add Carlos Ballesteros Velasco <https://github.com/soywiz> back into
    "Definitions by" header line.

--- swig/swig.d.ts
+++ swig/swig.d.ts
@@ -1,6 +1,6 @@
 // Type definitions for swig
 // Project: http://github.com/paularmstrong/swig
-// Definitions by: Peter Harris <https://github.com/CodeAnimal>
+// Definitions by: Peter Harris <https://github.com/CodeAnimal>, Carlos Ballesteros Velasco <https://github.com/soywiz>
 // Definitions: https://github.com/borisyankov/DefinitelyTyped
 
 // API Documentation : http://paularmstrong.github.io/swig/docs/api/
