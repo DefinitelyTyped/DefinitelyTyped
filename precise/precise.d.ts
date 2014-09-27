161395bce8a735fe7bcfc6517cf636780192b885
CodeAnimal <codeanimal@outlook.com>
2014-09-27 18:19:07 +0100

    Add precise definitions and tests.

---
+++ precise/precise.d.ts
@@ -0,0 +1,34 @@
+﻿// Type definitions for precise
+// Project: https://github.com/avoidwork/precise
+// Definitions by: Peter Harris <https://github.com/codeanimal>
+// Definitions: https://github.com/borisyankov/DefinitelyTyped
+
+declare module "precise" {
+  /**
+   * Precise factory
+   */
+  function _precise(): _precise.Precise;
+
+  module _precise {
+    class Precise {
+      constructor();
+
+      /**
+       * Starts a timer
+       */
+      start(): Precise;
+
+      /**
+       * Stops a timer
+       */
+      stop(): Precise;
+
+      /**
+       * Returns the nanoseconds from `start()` to `stop()`
+       */
+      diff(): number;
+    }
+  }
+
+  export = _precise;
+}
