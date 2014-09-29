fb9ac3d331fff89cd5641975eb2ae9b42d746978
CodeAnimal <codeanimal@outlook.com>
2014-09-29 20:32:50 +0100

    Update createClient method to better adhere to docs
    See: https://github.com/mranney/node_redis#rediscreateclient

--- redis/redis.d.ts
+++ redis/redis.d.ts
@@ -6,7 +6,9 @@
 // Imported from: https://github.com/soywiz/typescript-node-definitions/redis.d.ts
 
 declare module "redis" {
-	export function createClient(port_arg: number, host_arg: string, options: ClientOpts): RedisClient;
+  export function createClient(port_arg: number, host_arg?: string, options?: ClientOpts): RedisClient;
+  export function createClient(unix_socket: string, options?: ClientOpts): RedisClient;
+  export function createClient(options?: ClientOpts): RedisClient;
 	export function print(err: Error, reply: any): void;
 	export var debug_mode: boolean;
 
