c3da47bb1faa7c83ed3ec3541f9db29ef5b7c0d6
Steve Fenton <Steve-Fenton@users.noreply.github.com>
2014-09-29 10:03:33 +0100

    Update chart.d.ts

--- chartjs/chart.d.ts
+++ chartjs/chart.d.ts
@@ -7,6 +7,7 @@ interface ChartDataSet {
     label: string;
     fillColor: string;
     strokeColor: string;
+
     /* Line, Radar */
     pointColor?: string;
     pointStrokeColor?: string;
@@ -31,16 +32,174 @@ interface CircularChartData {
     label: string;
 }
 
-interface Chart {
-    new (context: CanvasRenderingContext2D): Chart;
-    Line(data: LinearChartData, options?: {});
-    Bar(data: LinearChartData, options?: {});
-    Radar(data: LinearChartData, options?: {});
-
-    PolarArea(data: CircularChartData[], options?: {});
-    Pie(data: CircularChartData[], options?: {});
-    Doughnut(data: CircularChartData[], options?: {});
-
+interface ChartSettings {
+    animation: boolean;
+    animationSteps: number;
+    animationEasing: string;
+    showScale: boolean;
+    scaleOverride: boolean;
+    scaleSteps: number;
+    scaleStepWidth: number;
+    scaleStartValue: number;
+    scaleLineColor: string;
+    scaleLineWidth: number;
+    scaleShowLabels: boolean;
+    scaleLabel: string;
+    scaleIntegersOnly: boolean;
+    scaleBeginAtZero: boolean;
+    scaleFontFamily: string;
+    scaleFontSize: number;
+    scaleFontStyle: string;
+    scaleFontColor: string;
+    responsive: boolean;
+    maintainAspectRatio: boolean;
+    showTooltips: boolean;
+    tooltipEvents: string[];
+    tooltipFillColor: string;
+    tooltipFontFamily: string;
+    tooltipFontSize: number;
+    tooltipFontStyle: string;
+    tooltipFontColor: string;
+    tooltipTitleFontFamily: string;
+    tooltipTitleFontSize: number;
+    tooltipTitleFontStyle: string;
+    tooltipTitleFontColor: string;
+    tooltipYPadding: number;
+    tooltipXPadding: number;
+    tooltipCaretSize: number;
+    tooltipCornerRadius: number;
+    tooltipXOffset: number;
+    tooltipTemplate: string;
+    multiTooltipTemplate: string;
+    onAnimationProgress: () => any;
+    onAnimationComplete: () => any;
 }
 
-declare var Chart: Chart;
+interface ChartOptions {
+    scaleShowGridLines?: boolean;
+    scaleGridLineColor?: string;
+    scaleGridLineWidth?: number;
+    legendTemplate?: string;
+}
+
+interface PointsAtEvent {
+    value: number;
+    label: string;
+    datasetLabel: string;
+    strokeColor: string;
+    fillColor: string;
+    highlightFill: string;
+    highlightStroke: string;
+    x: number;
+    y: number;
+}
+
+interface ChartInstance {
+    clear: () => void;
+    stop: () => void;
+    resize: () => void;
+    destroy: () => void;
+    toBase64Image: () => string;
+    generateLegend: () => string;
+}
+
+interface LinearInstance extends ChartInstance {
+    getPointsAtEvent: (event: Event) => PointsAtEvent[];
+    update: () => void;
+    addData: (valuesArray: number[], label: string) => void;
+    removeData: () => void;
+}
+
+interface CircularInstance extends ChartInstance {
+    getSegmentsAtEvent: (event: Event) => {}[];
+    update: () => void;
+    addData: (valuesArray: CircularChartData[], index: number) => void;
+    removeData: (index: number) => void;
+}
+
+interface LineChartOptions extends ChartOptions {
+    bezierCurve?: boolean;
+    bezierCurveTension?: number;
+    pointDot?: boolean;
+    pointDotRadius?: number;
+    pointDotStrokeWidth?: number;
+    pointHitDetectionRadius?: number;
+    datasetStroke?: boolean;
+    datasetStrokeWidth?: number;
+    datasetFill?: boolean;
+}
+
+interface BarChartOptions extends ChartOptions {
+    scaleBeginAtZero?: boolean;
+    barShowStroke?: boolean;
+    barStrokeWidth?: number;
+    barValueSpacing?: number;
+    barDatasetSpacing?: number;
+}
+
+interface RadarChartOptions {
+    scaleShowLine?: boolean;
+    angleShowLineOut?: boolean;
+    scaleShowLabels?: boolean;
+    scaleBeginAtZero?: boolean;
+    angleLineColor?: string;
+    angleLineWidth?: number;
+    pointLabelFontFamily?: string;
+    pointLabelFontStyle?: string;
+    pointLabelFontSize?: number;
+    pointLabelFontColor?: string;
+    pointDot?: boolean;
+    pointDotRadius?: number;
+    pointDotStrokeWidth?: number;
+    pointHitDetectionRadius?: number;
+    datasetStroke?: boolean;
+    datasetStrokeWidth?: number;
+    datasetFill?: boolean;
+    legendTemplate?: string;
+}
+
+interface PolarAreaChartOptions {
+    scaleShowLabelBackdrop?: boolean;
+    scaleBackdropColor?: string;
+    scaleBeginAtZero?: boolean;
+    scaleBackdropPaddingY?: number;
+    scaleBackdropPaddingX?: number;
+    scaleShowLine?: boolean;
+    segmentShowStroke?: boolean;
+    segmentStrokeColor?: string;
+    segmentStrokeWidth?: number;
+    animationSteps?: number;
+    animationEasing?: string;
+    animateRotate?: boolean;
+    animateScale?: boolean;
+    legendTemplate?: string;
+}
+
+interface PieChartOptions {
+    segmentShowStroke?: boolean;
+    segmentStrokeColor?: string;
+    segmentStrokeWidth?: number;
+    percentageInnerCutout?: number;
+    animationSteps?: number;
+    animationEasing?: string;
+    animateRotate?: boolean;
+    animateScale?: boolean;
+    legendTemplate?: string;
+}
+
+interface Chart {
+    Line(data: LinearChartData, options?: LineChartOptions): LinearInstance;
+    Bar(data: LinearChartData, options?: BarChartOptions): LinearInstance;
+    Radar(data: LinearChartData, options?: RadarChartOptions): LinearInstance;
+
+    PolarArea(data: CircularChartData[], options?: PolarAreaChartOptions): CircularInstance;
+    Pie(data: CircularChartData[], options?: PieChartOptions): CircularInstance;
+    Doughnut(data: CircularChartData[], options?: PieChartOptions): CircularInstance;
+}
+
+declare var Chart: {
+    new (context: CanvasRenderingContext2D): Chart;
+    defaults: {
+        global: ChartSettings;
+    }
+};
