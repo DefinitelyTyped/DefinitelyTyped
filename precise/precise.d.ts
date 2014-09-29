1b1fb403ab22d7df23b58698c0eea3f31b7a05a8
Basarat Ali Syed <basaratali@gmail.com>
2014-09-29 18:18:00 +1000

    precise : prefer npm project link

--- precise/precise.d.ts
+++ precise/precise.d.ts
@@ -1,5 +1,5 @@
 ﻿// Type definitions for precise
-// Project: https://github.com/avoidwork/precise
+// Project: https://www.npmjs.org/package/precise
 // Definitions by: Peter Harris <https://github.com/codeanimal>
 // Definitions: https://github.com/borisyankov/DefinitelyTyped
 
