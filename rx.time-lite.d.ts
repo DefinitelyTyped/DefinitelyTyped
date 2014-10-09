c24dbf968045564ffd2681c145377e83167a4554
Igor Oleinikov <igor@oleinikov.ru>
2014-10-08 17:26:25 -0700

    Moved removed `rx.time` operators from rx.time-lite to rx.time.

--- rx.time-lite.d.ts
+++ rx.time-lite.d.ts
@@ -17,6 +17,7 @@ declare module Rx {
 	}
 
 	export interface Observable<T> {
+		delay(dueTime: Date, scheduler?: IScheduler): Observable<T>;
 		delay(dueTime: number, scheduler?: IScheduler): Observable<T>;
 		throttle(dueTime: number, scheduler?: IScheduler): Observable<T>;
 		timeInterval(scheduler?: IScheduler): Observable<TimeInterval<T>>;
@@ -25,25 +26,6 @@ declare module Rx {
 		sample<TSample>(sampler: Observable<TSample>, scheduler?: IScheduler): Observable<T>;
 		timeout(dueTime: Date, other?: Observable<T>, scheduler?: IScheduler): Observable<T>;
 		timeout(dueTime: number, other?: Observable<T>, scheduler?: IScheduler): Observable<T>;
-
-		delaySubscription(dueTime: number, scheduler?: IScheduler): Observable<T>;
-		delayWithSelector(delayDurationSelector: (item: T) => number): Observable<T>;
-		delayWithSelector(subscriptionDelay: number, delayDurationSelector: (item: T) => number): Observable<T>;
-
-		timeoutWithSelector<TTimeout>(firstTimeout: Observable<TTimeout>, timeoutdurationSelector?: (item: T) => Observable<TTimeout>, other?: Observable<T>): Observable<T>;
-		throttleWithSelector<TTimeout>(throttleDurationSelector: (item: T) => Observable<TTimeout>): Observable<T>;
-
-		skipLastWithTime(duration: number, scheduler?: IScheduler): Observable<T>;
-		takeLastWithTime(duration: number, timerScheduler?: IScheduler, loopScheduler?: IScheduler): Observable<T>;
-
-		takeLastBufferWithTime(duration: number, scheduler?: IScheduler): Observable<T[]>;
-		takeWithTime(duration: number, scheduler?: IScheduler): Observable<T>;
-		skipWithTime(duration: number, scheduler?: IScheduler): Observable<T>;
-
-		skipUntilWithTime(startTime: Date, scheduler?: IScheduler): Observable<T>;
-		skipUntilWithTime(duration: number, scheduler?: IScheduler): Observable<T>;
-		takeUntilWithTime(endTime: Date, scheduler?: IScheduler): Observable<T>;
-		takeUntilWithTime(duration: number, scheduler?: IScheduler): Observable<T>;
 	}
 
 	interface ObservableStatic {
@@ -51,12 +33,5 @@ declare module Rx {
 		interval(dutTime: number, period: number, scheduler?: IScheduler): Observable<number>;
 		timer(dueTime: number, period: number, scheduler?: IScheduler): Observable<number>;
 		timer(dueTime: number, scheduler?: IScheduler): Observable<number>;
-		generateWithRelativeTime<TState, TResult>(
-			initialState: TState,
-			condition: (state: TState) => boolean,
-			iterate: (state: TState) => TState,
-			resultSelector: (state: TState) => TResult,
-			timeSelector: (state: TState) => number,
-			scheduler?: IScheduler): Observable<TResult>;
 	}
 }
--- rx.time.d.ts
+++ rx.time.d.ts
@@ -8,6 +8,25 @@
 
 declare module Rx {
 	export interface Observable<T> {
+		delaySubscription(dueTime: number, scheduler?: IScheduler): Observable<T>;
+		delayWithSelector(delayDurationSelector: (item: T) => number): Observable<T>;
+		delayWithSelector(subscriptionDelay: number, delayDurationSelector: (item: T) => number): Observable<T>;
+
+		timeoutWithSelector<TTimeout>(firstTimeout: Observable<TTimeout>, timeoutdurationSelector?: (item: T) => Observable<TTimeout>, other?: Observable<T>): Observable<T>;
+		throttleWithSelector<TTimeout>(throttleDurationSelector: (item: T) => Observable<TTimeout>): Observable<T>;
+
+		skipLastWithTime(duration: number, scheduler?: IScheduler): Observable<T>;
+		takeLastWithTime(duration: number, timerScheduler?: IScheduler, loopScheduler?: IScheduler): Observable<T>;
+
+		takeLastBufferWithTime(duration: number, scheduler?: IScheduler): Observable<T[]>;
+		takeWithTime(duration: number, scheduler?: IScheduler): Observable<T>;
+		skipWithTime(duration: number, scheduler?: IScheduler): Observable<T>;
+
+		skipUntilWithTime(startTime: Date, scheduler?: IScheduler): Observable<T>;
+		skipUntilWithTime(duration: number, scheduler?: IScheduler): Observable<T>;
+		takeUntilWithTime(endTime: Date, scheduler?: IScheduler): Observable<T>;
+		takeUntilWithTime(duration: number, scheduler?: IScheduler): Observable<T>;
+
 		windowWithTime(timeSpan: number, timeShift: number, scheduler?: IScheduler): Observable<Observable<T>>;
 		windowWithTime(timeSpan: number, scheduler?: IScheduler): Observable<Observable<T>>;
 		windowWithTimeOrCount(timeSpan: number, count: number, scheduler?: IScheduler): Observable<Observable<T>>;
@@ -20,6 +39,13 @@ declare module Rx {
 		timer(dueTime: Date, period: number, scheduler?: IScheduler): Observable<number>;
 		timer(dueTime: Date, scheduler?: IScheduler): Observable<number>;
 
+		generateWithRelativeTime<TState, TResult>(
+			initialState: TState,
+			condition: (state: TState) => boolean,
+			iterate: (state: TState) => TState,
+			resultSelector: (state: TState) => TResult,
+			timeSelector: (state: TState) => number,
+			scheduler?: IScheduler): Observable<TResult>;
 		generateWithAbsoluteTime<TState, TResult>(
 			initialState: TState,
 			condition: (state: TState) => boolean,
