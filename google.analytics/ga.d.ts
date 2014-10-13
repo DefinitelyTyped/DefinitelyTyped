a8e26993d03f4d75a45cccbe235745e502162b51
Pat Kujawa <pat.kujawa@webfilings.com>
2014-10-13 14:51:24 -0600

    Add new GA url to Project line.

--- google.analytics/ga.d.ts
+++ google.analytics/ga.d.ts
@@ -1,5 +1,5 @@
 // Type definitions for Google Analytics (Classic and Universal)
-// Project: https://developers.google.com/analytics/devguides/collection/gajs/
+// Project: https://developers.google.com/analytics/devguides/collection/gajs/, https://developers.google.com/analytics/devguides/collection/analyticsjs/method-reference
 // Definitions by: Ronnie Haakon Hegelund <http://ronniehegelund.blogspot.dk>, Pat Kujawa <http://patkujawa.com>
 // Definitions: https://github.com/borisyankov/DefinitelyTyped
 
