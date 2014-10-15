3f0294fe139df8eb8b8d3f24b38b0f2e4c041a17
GermainBergeron <germain.bergeron1@gmail.com>
2014-10-15 14:33:16 -0400

    add definition for list.js

---
+++ list.js/list.js.d.ts
@@ -0,0 +1,36 @@
+// Type definitions for ListJS v1.1.0
+// Project: https://github.com/javve/list.js
+// Definitions by: Marilyne Chan <https://github.com/marilynechan>
+// Definitions by: Germain Bergeron  <https://github.com/marilynechan>
+// Definitions: https://github.com/borisyankov/DefinitelyTyped
+
+interface ListOptions {
+    valueNames?: string[];
+    item?: string;
+    listClass?: string;
+    searchClass?: string;
+    sortClass?: string;
+    indexAsync?: boolean;
+    page?: number;
+    i?: number;
+}
+
+interface SortOptions {
+    order: string;
+}
+
+declare class List {
+    constructor(idOrElement: any, options: ListOptions, values?: any[]);
+
+    items: Item[];
+    visibleItems: Item[];
+
+    sort(valueName: string, options: SortOptions);
+    search(searchString?: string, columns?: any[]);
+    get(valueName: string, value: any): Item[];
+    update();
+}
+
+declare class Item {
+    values(newValues?: any): any;
+}
