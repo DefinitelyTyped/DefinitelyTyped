6c618f28eda73800d343941d3e877ca24094bedf
in-async <inasync@gmail.com>
2014-11-09 02:21:02 +0900

    Add name to "Definitions by".

--- firebase/firebase.d.ts
+++ firebase/firebase.d.ts
@@ -1,6 +1,6 @@
 // Type definitions for Firebase API 2.0.2
 // Project: https://www.firebase.com/docs/javascript/firebase
-// Definitions by: Vincent Botone <https://github.com/vbortone/>
+// Definitions by: Vincent Botone <https://github.com/vbortone/>, Shin1 Kashimura <https://github.com/in-async/>
 // Definitions: https://github.com/borisyankov/DefinitelyTyped
 
 interface IFirebaseAuthResult {
