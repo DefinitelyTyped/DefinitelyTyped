26c17dce8da4e739784ef3d1c8afae2536f701db
pspi <pspi@users.noreply.github.com>
2014-11-17 08:26:47 +0200

    allow method chaining with .on()

--- request/request-tests.ts
+++ request/request-tests.ts
@@ -197,3 +197,10 @@ r.post(str);
 r(options);
 r.get(options);
 r.post(options);
+
+request
+.get('http://example.com/example.png')
+.on('response', function(response) {
+	// check response
+})
+.pipe(request.put('http://another.com/another.png'));
