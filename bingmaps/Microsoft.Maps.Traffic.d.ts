5c661750c8702d708ae2ad4c5c43654410db3c41
ws_ericrtodd <eric.todd@wirestone.com>
2014-12-03 12:45:00 -0800

    Adding typings for Bing Maps API.  These are for use with the Bing Maps for Windows Store Apps SDKs and the Bing Maps AJAX V7 control.

---
+++ bingmaps/Microsoft.Maps.Traffic.d.ts
@@ -0,0 +1,17 @@
+﻿declare module Microsoft.Maps.Traffic {
+    
+    export class TrafficManager {
+        
+        constructor(map: Map);
+
+        hide(): void;
+        hideFlow(): void;
+        hideIncidents(): void;
+        hideLegend(): void;
+        show(): void;
+        showFlow(): void;
+        showIncidents(): void;
+        showLegend():void;
+    }
+
+}
