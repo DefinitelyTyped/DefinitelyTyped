7ca709a32a75c9735fe9cdbdfc5be07c27a40cea
Tommi Palomäki <tommi.palomaki@cybercom.com>
2014-12-09 11:08:10 +0000

    SocketIOClient.Socket.off() arguments are optional

--- socket.io-client/socket.io-client.d.ts
+++ socket.io-client/socket.io-client.d.ts
@@ -23,7 +23,7 @@ declare module SocketIOClient {
     interface Socket {
         on(event: string, fn: Function): Socket;
         once(event: string, fn: Function): Socket;
-        off(event: string, fn: Function): Socket;
+        off(event?: string, fn?: Function): Socket;
         emit(event: string, ...args: any[]): Socket;
         listeners(event: string): Function[];
         hasListeners(event: string): boolean;
