e2f0638e67b6ae6fd97288c9481ee09bf6e9c159
Pedro Ferreira <plferreira4@gmail.com>
2014-12-13 04:27:19 +0000

    - update preloadjs to v0.6

--- easeljs/easeljs.d.ts
+++ easeljs/easeljs.d.ts
@@ -1,4 +1,4 @@
-// Type definitions for EaselJS 0.7.1
+// Type definitions for EaselJS 0.8.0
 // Project: http://www.createjs.com/#!/EaselJS
 // Definitions by: Pedro Ferreira <https://bitbucket.org/drk4>, Chris Smith <https://github.com/evilangelist>
 // Definitions: https://github.com/borisyankov/DefinitelyTyped
