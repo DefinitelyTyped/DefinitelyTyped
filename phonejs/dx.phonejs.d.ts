cc7abd888056c7d618f49dc52d5ea5ea2658e2dc
Igor Kriklivets <igor.kriklivets@hotmail.com>
2014-12-15 15:41:02 +0300

    Delete dx.phonejs.d.ts

--- phonejs/dx.phonejs.d.ts
+++
@@ -1,6 +0,0 @@
-// Type definitions for PhoneJS 14.1
-// Project: http://js.devexpress.com/MobileDevelopment/
-// Definitions by: DevExpress Inc. <http://devexpress.com/>
-// Definitions: https://github.com/borisyankov/DefinitelyTyped
-
-/// <reference path="../devextreme/14.1/dx.phonejs-14.1.d.ts" />
