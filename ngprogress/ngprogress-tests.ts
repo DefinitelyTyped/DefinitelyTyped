7e077abe19b237956db94b9a68e93c3360268e35
Martin McWhorter <martin@mcwhorter.org>
2014-12-15 12:19:47 +0000

    Definitions for ngProgress
    Definitions for https://github.com/victorbjelkholm/ngprogress

---
+++ ngprogress/ngprogress-tests.ts
@@ -0,0 +1,16 @@
+﻿/// <reference path="./ngprogress.d.ts" />
+
+
+var ngProgress: NgProgress.INgProgress = <any> {};
+
+ngProgress.start();
+ngProgress.height('10px');
+ngProgress.color('red');
+
+var statusResult: number = undefined;
+ngProgress.status();
+
+ngProgress.stop();
+ngProgress.set(50);
+ngProgress.reset();
+ngProgress.complete();
