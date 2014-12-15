14e255a975eb3ebab4ec4f0aceb35ac9ccb119e8
pridekit <pridekit@hotmail.com>
2014-12-15 15:23:11 -0500

    Fixed address in definition by section

--- xrm-2015/xrm-2015.d.ts
+++ xrm-2015/xrm-2015.d.ts
@@ -1,6 +1,6 @@
 // Type definitions for Dyanmics CRM 2015
 // Project: http://www.microsoft.com/en-us/dynamics/crm.aspx
-// Definitions by: Steven Pride - Based on https://xrm2011typescript.codeplex.com/ created by kowgli <https://www.codeplex.com/site/users/view/kowgli>
+// Definitions by: Steven Pride - Based on <https://xrm2011typescript.codeplex.com/> created by kowgli <https://www.codeplex.com/site/users/view/kowgli>
 // Definitions: https://github.com/borisyankov/DefinitelyTyped
 
 declare var Xrm: Xrm;
