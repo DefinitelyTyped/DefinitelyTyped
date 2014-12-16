ff9b0aa7a2590f79eed0b82466bc66d3838dd3b4
odangosan <odangosan333@gmail.com>
2014-12-16 20:20:48 +0900

    Update milkcocoa-tests.ts

--- milkcocoa/milkcocoa-tests.ts
+++ milkcocoa/milkcocoa-tests.ts
@@ -1,28 +1,36 @@
-///<reference path="./milk-cocoa.d.ts"/>
+///<reference path="./milkcocoa.d.ts"/>
 var mk = new MilkCocoa("hostAddress");
 mk.addAccount("sample@test.com", "password", null, (err: MilkCocoa.Error.AddAccount, user: User) => {
   switch (err) {
     case null:
-      console.log('正常に登録が完了しました');
+      console.log("Done.");
       break;
     case MilkCocoa.Error.AddAccount.FormatError:
-      console.log('無効な書式のメールアドレスです');
+      console.log("Invalid mail address.");
       break;
     case MilkCocoa.Error.AddAccount.AlreadyExist:
-      console.log('既に追加されているメールアドレスです');
+      console.log("Already added account.");
       break;
   }
 });
 mk.login("sample@test.com", "password", (err: MilkCocoa.Error.Login, user: User) => {
   if (err === MilkCocoa.Error.Login.FormatError) {
-    console.log('Emailの形式が無効です。');
+    console.log("Invalid mail address.");
   } else if (err === MilkCocoa.Error.Login.LoginError) {
-    console.log('登録されていないEmailか、無効なパスワードです。');
+    console.log("Or Email that is not registered, it is not a valid password.");
   } else if (err === MilkCocoa.Error.Login.EmailNotVerificated) {
-    console.log('まだアカウントは仮登録です');
+    console.log("Still account is temporary registration.");
   } else {
     //成功時はユーザIDを取得できます。
     var user_id = user.id;
   }
 });
-mk.dataStore("datastore")
+mk.dataStore("datastore").push({ message: "this is message." }, (data: DataStoreCallbackData) => {
+  console.log("pushed.");
+});
+mk.dataStore("datastore").child("chat").child("message").on("push", (data: DataStoreCallbackData) => {
+  console.log(data.value.message);
+});
+mk.dataStore("datastore").child("chat").child("message").query().limit(10).done((data) => {
+  console.log(data.message);
+});
