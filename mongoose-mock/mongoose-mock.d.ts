d8e50d6951ae4af4a3666bc40318882a718193f1
Jason Tremper <jason@mechavian.com>
2014-12-17 15:40:50 -0500

    fixing header on mongoose-mock.d.ts

--- mongoose-mock/mongoose-mock.d.ts
+++ mongoose-mock/mongoose-mock.d.ts
@@ -1,5 +1,5 @@
-// Type definitions for multer
-// Project: https://github.com/expressjs/multer
+// Type definitions for mongoose-mock 0.4.0
+// Project: https://github.com/JohanObrink/mongoose-mock
 // Definitions by: jt000 <https://github.com/jt000>
 // Definitions: https://github.com/borisyankov/DefinitelyTyped
 
