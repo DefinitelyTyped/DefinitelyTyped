7a81c1d9c2b705a2424d8bab1eefb1da37652036
Jared <spuzzy2@hotmail.com>
2015-01-02 20:18:09 +1300

    Fix header and make use of tabs consistent in both tests and definition.

--- oboe/oboe.d.ts
+++ oboe/oboe.d.ts
@@ -1,6 +1,6 @@
-// Type definitions for [LIBRARY NAME]
-// Project: [LIBRARY URL]
-// Definitions by: [AUTHOR NAME] <[AUTHOR URL]>
+// Type definitions for oboe v2.0.3
+// Project: https://github.com/jimhigson/oboe.js
+// Definitions by: Jared Klopper <https://github.com/optical>
 // Definitions: https://github.com/borisyankov/DefinitelyTyped
 
 /// <reference path="../node/node.d.ts" />
