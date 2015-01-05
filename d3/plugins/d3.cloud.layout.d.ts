3548e481237227962f88ce8fb018709d5fc027f7
hansrwindhoff <hansrwindhoff@gmail.com>
2015-01-05 15:23:39 -0700

    add cloud layout plug-in defs

---
+++ d3/plugins/d3.cloud.layout.d.ts
@@ -0,0 +1,38 @@
+﻿// Type definitions for d3JS cloud layout plugin by Jason Davies
+// Project: https://github.com/jasondavies/d3-cloud
+// Definitions by: hans windhoff <https://github.com/hansrwindhoff>
+
+
+/// <reference path="../d3.d.ts" />
+
+declare module D3 {
+  export module Layout {
+    export interface CloudLayout {
+      (layers: any[], index?: number): any[];
+      values(accessor?: (d: any) => any): CloudLayout;
+      offset(offset: string): CloudLayout;
+      size: {
+        /**
+      * Gets the available layout size 
+      */
+        (): Array<number>;
+        /**
+      * Sets the available layout size 
+      */
+        (size: Array<number>): CloudLayout;
+      };
+      words: (inputArray: Array<any>) => CloudLayout;
+      rotate: (number) => CloudLayout;
+      padding: (number) => CloudLayout;
+      font: (string) => CloudLayout;
+      fontSize(fctn: (d: any) => number): CloudLayout;
+      on: (eventname: string, callee: (words: string[]) => void) => CloudLayout;
+      start: () => CloudLayout;
+    }
+
+
+    export interface Layout {
+      cloud(): CloudLayout; 
+    }
+  }
+}
