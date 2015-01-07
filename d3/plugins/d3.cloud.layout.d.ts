0898068ad96ab889ed2b7d820833c3996c00f45e
hansrwindhoff <hansrwindhoff@gmail.com>
2015-01-07 14:45:58 -0700

    fix implicit any

--- d3/plugins/d3.cloud.layout.d.ts
+++ d3/plugins/d3.cloud.layout.d.ts
@@ -7,6 +7,13 @@
 
 declare module D3 {
   export module Layout {
+    export interface IRotate      { 
+      (number:number) : CloudLayout;
+        (number:()=>number) : CloudLayout;
+        }
+
+
+
     export interface CloudLayout {
       (layers: any[], index?: number): any[];
       values(accessor?: (d: any) => any): CloudLayout;
@@ -22,11 +29,11 @@ declare module D3 {
         (size: Array<number>): CloudLayout;
       };
       words: (inputArray: Array<any>) => CloudLayout;
-      rotate: (number) => CloudLayout;
-      padding: (number) => CloudLayout;
-      font: (string) => CloudLayout;
+      rotate:IRotate;
+      padding: (number:number) => CloudLayout;
+      font: (string:string) => CloudLayout;
       fontSize(fctn: (d: any) => number): CloudLayout;
-      on: (eventname: string, callee: (words: string[]) => void) => CloudLayout;
+      on: (eventname: string, callee: (words: any[]) => void) => CloudLayout;
       start: () => CloudLayout;
     }
 
