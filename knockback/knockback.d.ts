ffb99eaeb5d71d74e090897014ec7d2336266ba1
Nils Lundquist <stealthwang@gmail.com>
2015-01-08 09:51:45 -0700

    Removing 'a' typo

--- knockback/knockback.d.ts
+++ knockback/knockback.d.ts
@@ -3,7 +3,7 @@
 // Definitions by: Boris Yankov <https://github.com/borisyankov/>
 // Definitions: https://github.com/borisyankov/DefinitelyTyped
 
-/// <reference path="../backbone/backbone.d.ts" />a
+/// <reference path="../backbone/backbone.d.ts" />
 /// <reference path="../knockout/knockout.d.ts" />
 
 declare module Knockback {
