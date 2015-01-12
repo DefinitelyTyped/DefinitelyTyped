223211fd98fc302334105bcbc9f7565cb76481f9
Rogier Schouten <r.schouten@spiritit.com>
2015-01-12 09:45:52 +0100

    jsdom: bugfixes, add comments, add defaultFeatures variable.

--- jsdom/jsdom-tests.ts
+++ jsdom/jsdom-tests.ts
@@ -1,17 +1,17 @@
 import jsdom = require("jsdom");
 
+jsdom.defaultDocumentFeatures.FetchExternalResources = ["img"];
+
 jsdom.env(
     "http://nodejs.org/dist/",
     ["http://code.jquery.com/jquery.js"],
-    function (errors, window) {
-        console.log("there have been nodejs releases!");
+    function (errors: Error[], window: Window) {
     }
 );
 
 jsdom.env(
     "http://nodejs.org/dist/",
-    function (errors, window) {
-        console.log("there have been nodejs releases!");
+    function (errors: Error[], window: Window) {
     }
 );
 
@@ -20,28 +20,21 @@ jsdom.env(
     {
         scripts: ["http://code.jquery.com/jquery.js"],
     },
-    function (errors, window) {
-        console.log("there have been nodejs releases!");
+    function (errors: Error[], window: Window) {
     }
 );
 
 jsdom.env(
     '<p><a class="the-link" href="https://github.com/tmpvar/jsdom">jsdom!</a></p>',
     ["http://code.jquery.com/jquery.js"],
-    function (errors, window) {
-        console.log("contents of a.the-link:", (<any>window).$("a.the-link").text());
+    function (errors: Error[], window: Window) {
     }
 );
 
 jsdom.env({
     url: "http://news.ycombinator.com/",
     scripts: ["http://code.jquery.com/jquery.js"],
-    done: function (errors, window) {
-        var $ = (<any>window).$;
-        console.log("HN Links");
-        $("td.title:not(:last) a").each(function() {
-            console.log(" -", $(this).text());
-        });
+    done: function (errors: Error[], window: Window) {
     }
 });
 
@@ -50,23 +43,9 @@ var jquery: string;
 jsdom.env({
     url: "http://news.ycombinator.com/",
     src: [jquery],
-    done: function (errors, window) {
-        var $ = (<any>window).$;
-        console.log("HN Links");
-        $("td.title:not(:last) a").each(function () {
-            console.log(" -", $(this).text());
-        });
+    done: function (errors: Error[], window: Window) {
     }
 });
 
-var window = jsdom.jsdom().parentWindow;
-var window = jsdom.jsdom("<div>foobar</div>").parentWindow;
-var window = jsdom.jsdom("<div>foobar</div>", {
-scripts: ["http://code.jquery.com/jquery.js"],
-done: function (errors, window) {
-    var $ = (<any>window).$;
-    console.log("HN Links");
-    $("td.title:not(:last) a").each(function() {
-        console.log(" -", $(this).text());
-    });
-}}).parentWindow;
+var window: Window = jsdom.jsdom("<div>foobar</div>").parentWindow;
+var document: Document = jsdom.jsdom("<html></html>");
