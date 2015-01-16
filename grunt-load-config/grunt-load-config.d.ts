b90f23b636d043fa86ef3cdaf74911ef830a2077
Thomas Butler <thomas@butlersoftwarellc.com>
2015-01-15 20:01:14 -0500

    add files to git

---
+++ grunt-load-config/grunt-load-config.d.ts
@@ -0,0 +1,48 @@
+﻿// Type definitions for Grunt 0.4.x
+// Project: http://firstandthird.github.io/load-grunt-config/
+// Definitions by: Thomas Butler <https://github.com/butlersoftware/>
+// Definitions: https://github.com/butlersoftware/DefinitelyTyped
+
+/// <reference path="../gruntjs/gruntjs.d.ts" />
+
+declare module loadConfig  {
+	module config {
+		interface loadGruntTasks {
+			pattern?: string;
+			scope?: string;
+		}
+		interface data {
+			test?: boolean;
+		}
+	}
+    module options {
+    
+        interface data {
+            configPath?: string;
+            init?: boolean;
+            jitGrunt?: boolean;
+            loadGruntTasks?: config.loadGruntTasks;
+            data?:config.data ;
+		}
+	    interface config extends data {
+		    
+	    }
+        interface postProcess {
+            (config: config): any;
+        }
+    }
+    interface options {
+		data?: options.data;
+        config?: options.config;
+        postProcess?: options.postProcess;
+    }
+}
+interface loadConfig {
+	(grunt: IGrunt, options?: loadConfig.options): any;
+}
+
+
+declare module "grunt-load-config" {
+	var exports: loadConfig;
+    export = exports;
+}
---
+++ grunt-load-config/grunt-load-config.tests.ts
@@ -0,0 +1,50 @@
+﻿// Type definitions for Grunt 0.4.x
+// Project: http://firstandthird.github.io/load-grunt-config/
+// Definitions by: Thomas Butler <https://github.com/butlersoftware/>
+// Definitions: https://github.com/butlersoftware/DefinitelyTyped
+
+
+
+// Official code sample from 
+// http://firstandthird.github.io/load-grunt-config/#toc3
+
+
+import loadConfig = require('grunt-load-config');
+import path = require("path");
+
+
+// exports should work same as module.exports 
+exports = (grunt: IGrunt) => {
+	loadConfig(grunt);
+}
+
+
+
+
+exports = (grunt: IGrunt) => {
+
+	loadConfig(grunt, {
+		configPath: path.join(process.cwd(), 'grunt'), //path to task.js files, defaults to grunt dir
+		init: true, //auto grunt.initConfig
+		data: { //data passed into config.  Can use with <%= test %>
+			test: false
+		},
+		loadGruntTasks: { //can optionally pass options to load-grunt-tasks.  If you set to false, it will disable auto loading tasks.
+			pattern: 'grunt-*',
+			config: require('./package.json'),
+			scope: 'devDependencies'
+		},
+		postProcess: function (config) { } //can post process config object before it gets passed to grunt
+	});
+}
+
+
+exports = (grunt: IGrunt) => {
+	loadConfig(grunt, {
+		// ...
+		jitGrunt: {
+			//here you can pass options to jit-grunt (or just jitGrunt: true)
+		}
+	});
+
+}
