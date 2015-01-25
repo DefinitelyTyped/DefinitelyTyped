3e4c84fa45aec4b2131abd5bfe305843989524b3
Zhutian Chen <chenzhutian1992@gmail.com>
2015-01-25 18:42:40 +0800

    Add a overload definition for Transition.each(...)

--- d3/d3.d.ts
+++ d3/d3.d.ts
@@ -966,7 +966,10 @@ declare module D3 {
                 */
                 (elements: EventTarget[]): Transition;
             }
-            each: (type?: any, eachFunction?: (data: any, index: number) => any) => Transition;
+            each: {
+            	(type: (data: any, index: number) => any) => Transition;
+            	(type: string, eachFunction: (data: any, index: number) => any) => Transition;
+            }
             transition: () => Transition;
             ease: (value: string, ...arrs: any[]) => Transition;
             attrTween(name: string, tween: (d: any, i: number, a: any) => BaseInterpolate): Transition;
