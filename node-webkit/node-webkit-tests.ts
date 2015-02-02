107678d49307bed95308a75cf49ff4ace3bbf4cb
reppners <MEuiha14J2013>
2015-02-02 13:01:44 +0100

    + added Screen + changed syntax in order to use Node-Webkit definitions without the usual require("nw.gui") since the method to retrieve a nw.gui Object in the node webkit client side is quite different from the usual mechanism.  + added HtmlWindow property to nw.gui Window  + added require() to Window

--- node-webkit/node-webkit-tests.ts
+++ node-webkit/node-webkit-tests.ts
@@ -203,3 +203,46 @@ import gui = require("nw.gui");
 
     // Open a file in file explorer.
     gui.Shell.showItemInFolder('test.txt');
+
+/* SCREEN */
+
+    function ScreenToString(screen: gui.ScreenObject) {
+        var string = "";
+        string += "screen " + screen.id + " ";
+        var rect = screen.bounds;
+        string += "bound{" + rect.x + ", " + rect.y + ", " + rect.width + ", " + rect.height + "} ";
+        rect = screen.work_area;
+        string += "work_area{" + rect.x + ", " + rect.y + ", " + rect.width + ", " + rect.height + "} ";
+        string += " scaleFactor: " + screen.scaleFactor;
+        string += " isBuiltIn: " + screen.isBuiltIn;
+        string += "<br>";
+        return string;
+    }
+
+    //init must be called once during startup, before any function to gui.Screen can be called
+    gui.Screen.Init();
+    var string = "";
+    var screens = gui.Screen.screens;
+    // store all the screen information into string
+    for(var i=0; i<screens.length; i++) {
+        string += ScreenToString(screens[i]);
+    }
+
+    var screenCB = {
+        onDisplayBoundsChanged : function(screen: gui.ScreenObject) {
+            var out = "OnDisplayBoundsChanged " + ScreenToString(screen);
+        },
+
+        onDisplayAdded : function(screen: gui.ScreenObject) {
+            var out = "OnDisplayAdded " + ScreenToString(screen);
+        },
+
+        onDisplayRemoved : function(screen: gui.ScreenObject) {
+            var out = "OnDisplayRemoved " + ScreenToString(screen);
+        }
+    };
+
+    // listen to screen events
+    gui.Screen.on('displayBoundsChanged', screenCB.onDisplayBoundsChanged);
+    gui.Screen.on('displayAdded', screenCB.onDisplayAdded);
+    gui.Screen.on('displayRemoved', screenCB.onDisplayRemoved);
