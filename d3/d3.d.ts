0584949b841856483fd4ff5327a3c3778fd8f233
Brandon Luong <bluong@palantir.com>
2015-02-18 00:54:44 -0800

    Removing comment disallowing for filter to be used with a string

--- d3/d3.d.ts
+++ d3/d3.d.ts
@@ -778,7 +778,7 @@ declare module D3 {
 
         filter: {
             (filter: (data: any, index: number) => boolean, thisArg?: any): UpdateSelection;
-            //(filter: string): UpdateSelection;
+            (filter: string): UpdateSelection;
         };
 
         call(callback: (selection: Selection, ...args: any[]) => void, ...args: any[]): Selection;
@@ -1751,7 +1751,7 @@ declare module D3 {
         export interface Axis {
             (selection: Selection): void;
             (transition: Transition.Transition): void;
-            
+
             scale: {
                 (): any;
                 (scale: any): Axis;
