e3b1945ac80f6a8fcd975083d6da9cb0696bfc14
Lokesh Peta <peta.lokesh@gmail.com>
2015-02-20 13:35:55 +0000

    Added new maskedinput d.ts

---
+++ jquery.maskedinput/jquery.maskedinput.d.ts
@@ -0,0 +1,54 @@
+﻿// Type definitions for Masked Input plugin for jQuery
+// Definitions by: Lokesh Peta <https://github.com/lokeshpeta/>
+// Definitions: https://github.com/borisyankov/DefinitelyTyped
+
+///<reference path="../jquery/jquery.d.ts" />
+
+interface JQueryMaskedInputOptions {
+    mask?: string;
+    alias?: string;
+    placeholder?: string;
+    repeat?: number;
+    greedy?: boolean;
+    skipOptionalPartCharacter?: string;
+    clearIncomplete?: boolean;
+    clearMaskOnLostFocus?: boolean;
+    autoUnmask?: boolean;
+    showMaskOnFocus?: boolean;
+    showMaskOnHover?: boolean;
+    showToolTip?: boolean;
+    isComplete?: (buffer, options) => {};
+    numeric?: boolean;
+    radixPoint?: string;
+    rightAlignNumerics?: boolean;
+    oncomplete?: (value?: any) => void;
+    onincomplete?: () => void;
+    oncleared?: () => void;
+    onUnMask?: (maskedValue, unmaskedValue) => void;
+    onBeforeMask?: (initialValue) => void;
+    onKeyValidation?: (result) => void;
+    onBeforePaste?: (pastedValue) => void;
+}
+
+interface MaskedInputStatic {
+    defaults: MaskedInputDefaults;
+    isValid: (value: string, options: MaskedInputStaticDefaults) => boolean;
+    format: (value: string, options: MaskedInputStaticDefaults) => boolean;
+}
+
+interface MaskedInputStaticDefaults {
+    alias: string;
+}
+
+interface MaskedInputDefaults {
+    aliases;
+    definitions;
+}
+
+interface JQueryStatic {
+    inputmask: MaskedInputStatic;
+}
+
+interface JQuery {
+    inputmask(mask: string, options?: JQueryMaskedInputOptions): JQuery;
+}
