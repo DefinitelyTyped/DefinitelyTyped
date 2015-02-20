a2197413fd0637ba2b40a457d315f3001780a407
Lokesh Peta <peta.lokesh@gmail.com>
2015-02-20 13:35:55 +0000

    Added new maskedinput d.ts

---
+++ jquery.maskedinput/jquery.maskedinput-tests.ts
@@ -0,0 +1,7 @@
+﻿///<reference path="../jquery/jquery.d.ts" />
+///<reference path="jquery.maskedinput.d.ts" />
+
+$("#test").inputmask("9:000");
+$("#test").inputmask("9:000", { numeric: true });
+
+var alies = $.inputmask.defaults.aliases;
