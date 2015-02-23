f721912afcbf3de81e2ff8fd41975a46a431179e
Lokesh Peta <peta.lokesh@gmail.com>
2015-02-23 10:23:03 +0000

    corrected file and folder names

--- jquery.maskedinput/jquery.maskedinput-tests.ts
+++
@@ -1,7 +0,0 @@
-﻿///<reference path="../jquery/jquery.d.ts" />
-///<reference path="jquery.maskedinput.d.ts" />
-
-$("#test").inputmask("9:000");
-$("#test").inputmask("9:000", { numeric: true });
-
-var alies = $.inputmask.defaults.aliases;
