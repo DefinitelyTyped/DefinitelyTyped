1ca7d40c1fb160daf454dccb885375b26e55bf79
kiri <satoshi_ebihara@xica.net>
2015-02-24 18:01:25 +0000

    use es6-promise.d.ts and move NodeCallback into localforage module.

--- localForage/localForage.d.ts
+++ localForage/localForage.d.ts
@@ -3,17 +3,7 @@
 // Definitions by: david pichsenmeister <https://github.com/3x14159265>
 // Definitions: https://github.com/borisyankov/DefinitelyTyped
 
-interface INodeCallback<T> {
-    (err: any, result: T): void;
-}
-
-interface IPromise<T> {
-    then(successCallback: (result: T) => void, failureCallback: (err: any) => void): void;
-}
-
-interface INoArgPromise {
-    then(successCallback: () => void, failureCallback: (err: any) => void): void;
-}
+/// <reference path="../es6-promise/es6-promise.d.ts" />
 
 declare module localforage {
 
@@ -31,16 +21,20 @@ declare module localforage {
         description?: string;
     }
 
+    interface NodeCallback<T> {
+        (err: any, result: T): void;
+    }
+
     export function setDriver(driver: Driver | [Driver]): void;
     export function config(params: ConfigParams): void;
 
-    export function getItem<T>(key: string, callback?: INodeCallback<T>): IPromise<T>;
-    export function setItem<T>(key: string, value: T, callback?: INodeCallback<T>): IPromise<T>;
-    export function removeItem(key: string, callback?: (err: any /* some error object? */) => void): INoArgPromise;
-    export function clear(callback?: (err: any /* some error object? */) => void): INoArgPromise;
-    export function key(index: number, callback?: INodeCallback<string>): IPromise<string>;
-    export function keys(callback?: INodeCallback<[string]>): IPromise<[string]>;
-    export function length(callback?: INodeCallback<number>): IPromise<number>;
+    export function getItem<T>(key: string, callback?: NodeCallback<T>): Promise<T>;
+    export function setItem<T>(key: string, value: T, callback?: NodeCallback<T>): Promise<T>;
+    export function removeItem(key: string, callback?: (err: any /* some error object? */) => void): Promise<void>;
+    export function clear(callback?: (err: any /* some error object? */) => void): Promise<void>;
+    export function key(index: number, callback?: NodeCallback<string>): Promise<string>;
+    export function keys(callback?: NodeCallback<[string]>): Promise<[string]>;
+    export function length(callback?: NodeCallback<number>): Promise<number>;
 }
 
 declare module "localforage" {
