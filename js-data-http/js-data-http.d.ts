334d419e1bd4be020334d5d35d3818b091c01f20
reppners <MEuiha14J2013>
2015-02-26 10:01:27 +0100

    + initial commit for ds-http-adapter definition

---
+++ js-data-http/js-data-http.d.ts
@@ -0,0 +1,13 @@
+// Type definitions for JSData Http Adapter v1.2.0
+// Project: https://github.com/js-data/js-data-http
+// Definitions by: Stefan Steinhart <https://github.com/reppners>
+// Definitions: https://github.com/borisyankov/DefinitelyTyped
+
+/// <reference path="../js-data/js-data.d.ts" />
+
+declare module JSData_ {
+
+    interface DSHttpAdapter extends IDSAdapter {
+
+    }
+}
