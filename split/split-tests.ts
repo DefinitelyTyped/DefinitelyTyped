b164150c4016cabd95376d2bfb6360bc158485f7
Marcin Porebski <porebski.m@gmail.com>
2015-02-26 10:46:05 +0100

    split def. fix

--- split/split-tests.ts
+++ split/split-tests.ts
@@ -6,11 +6,11 @@ import split = require("split");
 
 var testStream = new stream.Readable();
 
-testStream.pipe = function(dest) {
+testStream.pipe = function(dest: stream.Writable) {
     dest.write("This is \r\n new \r\n line");
     return dest;
 };
 
-testStream.pipe(split(/(\r?\n)/, null, {maxLength: 20})).on("data", function(line) {
-    console.log("Line: " + line + "\r\n");
+testStream.pipe(split(/(\r?\n)/, null, {maxLength: 20})).on("data", function(line: Buffer) {
+    console.log("Line: " + line.toString('ascii') + "\r\n");
 });
