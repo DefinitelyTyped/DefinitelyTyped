8decf27bad87b53c00cb54026faf05cf0002531d
JasonS <JasonS@Novaleaf.com>
2015-03-03 17:43:45 -0800

    add bunyan-prettystream definition.

---
+++ bunyan-prettystream/bunyan-prettystream.d.ts
@@ -0,0 +1,13 @@
+﻿// type definitions for bunyan-prettystream
+// project: https://www.npmjs.com/package/bunyan-prettystream
+// definitions by jasons@novaleaf.com
+
+/// <reference path="../../node/node.d.ts" />
+
+declare module "bunyan-prettystream" {
+	import stream = require("stream");
+	class PrettyStream extends stream.Writable {
+		public pipe<T extends NodeJS.WritableStream>(destination: T, options?: { end?: boolean; }): T;
+	}
+	export = PrettyStream;
+}
