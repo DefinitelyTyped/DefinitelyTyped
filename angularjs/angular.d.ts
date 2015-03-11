9b1453a5a964604680650d1b13156d4d15aa534c
André Werlang <beppe85@gmail.com>
2015-03-11 08:43:49 -0300

    Define patch() on $http

--- angularjs/angular.d.ts
+++ angularjs/angular.d.ts
@@ -1153,6 +1153,15 @@ declare module ng {
         put<T>(url: string, data: any, config?: IRequestShortcutConfig): IHttpPromise<T>;
 
         /**
+         * Shortcut method to perform PATCH request.
+         *
+         * @param url Relative or absolute URL specifying the destination of the request
+         * @param data Request content
+         * @param config Optional configuration object
+         */
+        patch<T>(url: string, data: any, config?: IRequestShortcutConfig): IHttpPromise<T>;
+
+        /**
          * Runtime equivalent of the $httpProvider.defaults property. Allows configuration of default headers, withCredentials as well as request and response transformations.
          */
         defaults: IRequestConfig;
