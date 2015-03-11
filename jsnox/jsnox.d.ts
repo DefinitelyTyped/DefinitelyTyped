04f91f68fb60aab7062cf66fca6ec89b4b3dc2a1
Phips Peter <pspeter333@gmail.com>
2015-03-10 18:55:18 -0700

    Moving React 0.13 to be the default
    React 0.13 is now released so it should be the default type definition.
    http://facebook.github.io/react/blog/2015/03/10/react-v0.13.html

--- jsnox/jsnox.d.ts
+++ jsnox/jsnox.d.ts
@@ -3,7 +3,7 @@
 // Definitions by: Steve Baker <https://github.com/stkb/>
 // Definitions: https://github.com/borisyankov/DefinitelyTyped
 
-/// <reference path="../react/react.d.ts" />
+/// <reference path="../react/legacy/react-0.12.d.ts" />
 
 declare module 'jsnox' {
 
