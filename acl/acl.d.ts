985397ab73833ed0ba3638c4a473bf1fab62224a
A12893 <A12893@57.240.236.133.d.iijmobile.jp>
2015-03-13 03:18:04 +0900

    Rename things

--- acl/acl.d.ts
+++ acl/acl.d.ts
@@ -10,14 +10,14 @@ declare module "acl" {
   import http = require('http');
   import Promise = require("bluebird");
 
-  type Func = ()=>any;
+  type strings = string|string[];
   type Value = string|number;
   type Values = Value|Value[];
-  type strings = string|string[];
-  type ErrCallback = (err: Error) => any;
+  type Action = () => any;
+  type Callback = (err: Error) => any;
   type AnyCallback = (err: Error, obj: any) => any;
   type AllowedCallback = (err: Error, allowed: boolean) => any;
-  type GetUserId = (req: http.ServerRequest, res: http.ServerResponse) => any;
+  type GetUserId = (req: http.ServerRequest, res: http.ServerResponse) => Value;
 
   interface AclStatic {
     new (backend: Backend<any>, logger: Logger, options: Option): Acl;
@@ -31,19 +31,19 @@ declare module "acl" {
   }
 
   interface Acl {
-    addUserRoles: (userId: Value, roles: strings, cb?: ErrCallback) => Promise<void>;
-    removeUserRoles: (userId: Value, roles: strings, cb?: ErrCallback) => Promise<void>;
+    addUserRoles: (userId: Value, roles: strings, cb?: Callback) => Promise<void>;
+    removeUserRoles: (userId: Value, roles: strings, cb?: Callback) => Promise<void>;
     userRoles: (userId: Value, cb?: (err: Error, roles: string[])=>any) => Promise<string[]>;
     roleUsers: (role: Value, cb?: (err: Error, users: Values)=>any) => Promise<any>;
     hasRole: (userId: Value, role: string, cb?: (err: Error, isInRole: boolean)=>any) => Promise<boolean>;
-    addRoleParents: (role: string, parents: Values, cb?: ErrCallback) => Promise<void>;
-    removeRole: (role: string, cb?: ErrCallback) => Promise<void>;
-    removeResource: (resource: string, cb?: ErrCallback) => Promise<void>;
+    addRoleParents: (role: string, parents: Values, cb?: Callback) => Promise<void>;
+    removeRole: (role: string, cb?: Callback) => Promise<void>;
+    removeResource: (resource: string, cb?: Callback) => Promise<void>;
     allow: {
-      (roles: Values, resources: strings, permissions: strings, cb?: ErrCallback): Promise<void>;
+      (roles: Values, resources: strings, permissions: strings, cb?: Callback): Promise<void>;
       (aclSets: AclSet|AclSet[]): Promise<void>;
     }
-    removeAllow: (role: string, resources: strings, permissions: strings, cb?: ErrCallback) => Promise<void>;
+    removeAllow: (role: string, resources: strings, permissions: strings, cb?: Callback) => Promise<void>;
     removePermissions: (role: string, resources: strings, permissions: strings, cb?: Function) => Promise<void>;
     allowedPermissions: (userId: Value, resources: strings, cb?: AnyCallback) => Promise<void>;
     isAllowed: (userId: Value, resources: strings, permissions: strings, cb?: AllowedCallback) => Promise<boolean>;
@@ -76,25 +76,28 @@ declare module "acl" {
     permissions: strings;
   }
 
+  interface MemoryBackend extends Backend<Action[]> { }
+  interface MemoryBackendStatic {
+    new(): MemoryBackend;
+  }
+
+  //
+  // For internal use
+  //
   interface Backend<T> {
     begin: () => T;
-    end: (transaction: T, cb?: Func) => void;
-    clean: (cb?: Func) => void;
-    get: (bucket: string, key: Value, cb?: Func) => void;
-    union: (bucket: string, keys: Value[], cb?: Func) => void;
-    add: (transaction: T, bucket: string, key: Value, values: Value|Value[]) => void;
+    end: (transaction: T, cb?: Action) => void;
+    clean: (cb?: Action) => void;
+    get: (bucket: string, key: Value, cb?: Action) => void;
+    union: (bucket: string, keys: Value[], cb?: Action) => void;
+    add: (transaction: T, bucket: string, key: Value, values: Values) => void;
     del: (transaction: T, bucket: string, keys: Value[]) => void;
-    remove: (transaction: T, bucket: string, key: Value, values: Value|Value[]) => void;
+    remove: (transaction: T, bucket: string, key: Value, values: Values) => void;
 
     endAsync: Function; //TODO: Give more specific function signature
     getAsync: Function;
     cleanAsync: Function;
     unionAsync: Function;
-  }
-
-  interface MemoryBackend extends Backend<Func[]> { }
-  interface MemoryBackendStatic {
-    new(): MemoryBackend;
   }
 
   interface Contract {
