e75741ac0ae1498a1acabf180b17a94a230d58a2
Marco Talento <marco.r.talento@parceiros.nos.pt>
2015-03-13 11:10:28 +0000

    Fix ua-parser-tests.ts

--- ua-parser/ua-parser-tests.ts
+++ ua-parser/ua-parser-tests.ts
@@ -2,8 +2,6 @@
 
 function testParsingUA() {
     var userAgent = "Mozilla/5.0 (Windows NT 6.3; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/40.0.2214.115 Safari/537.36";
-    var UAParser = new UAParser(userAgent);
-    console.log(UAParser.getResult());
+    var uaParser = new UAParser(userAgent);
+    console.log(uaParser.getResult());
 }
-
-testParsingUA();
