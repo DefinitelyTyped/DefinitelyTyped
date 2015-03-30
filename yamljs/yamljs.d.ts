445fd59b0cfe818bbe0150733e954e0397f7ecb0
Tim Jonischkat <tim@emmaeins-media.de>
2015-03-30 15:54:23 +0200

    added documentation to yamljs def

--- yamljs/yamljs.d.ts
+++ yamljs/yamljs.d.ts
@@ -1,4 +1,9 @@
-declare module "yamljs" {
+// Type definitions for yamljs 0.2.1
+// Project: https://github.com/jeremyfa/yaml.js
+// Definitions by: Tim Jonischkat <tim@emmaeins-media.de>
+// Definitions: https://github.com/borisyankov/DefinitelyTyped
+
+declare module yamljs {
 
     export function load(path : string) : any[];
 
