6f60566a5e6cedc17778624d1a231be4f0745031
Alex Ford <aford@forumone.com>
2015-03-30 15:28:48 -0400

    add self to contributor header

--- d3/d3.d.ts
+++ d3/d3.d.ts
@@ -1,6 +1,6 @@
 // Type definitions for d3JS
 // Project: http://d3js.org/
-// Definitions by: Boris Yankov <https://github.com/borisyankov>
+// Definitions by: Alex Ford <https://github.com/gustavderdrache>, Boris Yankov <https://github.com/borisyankov>
 // Definitions: https://github.com/borisyankov/DefinitelyTyped
 
 declare module d3 {
