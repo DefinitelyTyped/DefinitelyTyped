ee270df773c1f9a4a1c1326f0f8b5e44b7c91a85
Michael J. Bennett <dev@michaeljbennett.info>
2015-04-01 20:00:42 -0400

    mariasql definitions are now a ghost module
     On branch mariasql
    	modified:   mariasql/mariasql-tests.ts
    	modified:   mariasql/mariasql.d.ts

--- mariasql/mariasql.d.ts
+++ mariasql/mariasql.d.ts
@@ -3,95 +3,100 @@
 // Definitions by: MichaelBennett <https://github.com/bennett000/>
 // Definitions: https://github.com/borisyankov/DefinitelyTyped
 
+declare module MARIASQL {
+    export interface MariaCallBackError {
+        (error:Error):void
+    }
 
-/**
- */
-interface MariaCallBackError {
-    (error:Error):void
+    export interface MariaCallBackResult {
+        (result:MariaResult):void
+    }
+
+    export interface MariaCallBackRow {
+        (result:Array<any>):void
+    }
+
+    export interface MariaCallBackBoolean {
+        (result:boolean):void
+    }
+
+    export interface MariaCallBackObject {
+        (result:Object):void
+    }
+
+    export interface MariaCallBackVoid {
+        ():void
+    }
+
+    export interface Dictionary {
+        [index: string]: any;
+    }
+
+    export interface MariaPreparedQuery {
+        (values:Dictionary):string;
+        (values:Array<any>):string;
+    }
+
+    export interface ClientConfig {
+        host: string;
+        user: string;
+        password: string;
+        db?: string;
+        port?: number;
+        unixSocket?: string;
+        keepQueries?: boolean;
+        multiStatements?: boolean;
+        connTimeout?: number;
+        pingInterval?: number;
+        secureAuth?: boolean;
+        compress?: boolean;
+        ssl?:any;
+        local_infile?: boolean;
+        read_default_group?: string;
+        charset?: string;
+    }
+
+    export interface MariaResult {
+        on(signal:string, cb:MariaCallBackObject):MariaResult; // signal 'end'
+        on(signal:string, cb:MariaCallBackError):MariaResult;  // signal 'error'
+        on(signal:string, cb:MariaCallBackRow):MariaResult;    // signal 'row'
+        on(signal:string, cb:MariaCallBackVoid):MariaResult;   // signal 'abort'
+        abort():void;
+    }
+
+    export interface MariaQuery {
+        on(signal:string, cb:MariaCallBackResult):MariaQuery; // signal 'result'
+        on(signal:string, cb:MariaCallBackVoid):MariaQuery;   // signal 'end'
+        on(signal:string, cb:MariaCallBackVoid):MariaQuery;   // signal 'abort'
+        on(signal:string, cb:MariaCallBackError):MariaQuery;  // signal 'error'
+        abort():void;
+    }
+
+    export interface MariaClient {
+        connect(config:ClientConfig):void;
+        end():void;
+        destroy():void;
+        escape(query:string):string;
+        query(q:string, placeHolders?:Dictionary, useArray?:boolean):MariaQuery;
+        query(q:string, placeHolders?:Array<any>, useArray?:boolean):MariaQuery;
+        query(q:string, useArray?:boolean):MariaQuery;
+        prepare(query:string): MariaPreparedQuery;
+        isMariaDB():boolean;
+        on(signal:string, cb:MariaCallBackError): MariaClient;    // signal 'error'
+        on(signal:string, cb:MariaCallBackObject): MariaClient;   // signal 'close'
+        on(signal:string, cb:MariaCallBackVoid): MariaClient;     // signal 'connect'
+        connected: boolean;
+        threadId: string;
+    }
+
+    export interface Client {
+        new ():MariaClient;
+        ():MariaClient;
+        prototype: MariaClient;
+    }
 }
 
-interface MariaCallBackResult {
-    (result:MariaResult):void
+declare module "mariasql" {
+    var Client:MARIASQL.Client;
+    export = Client;
 }
-
-interface MariaCallBackRow {
-    (result:Array<any>):void
-}
-
-interface MariaCallBackBoolean {
-    (result:boolean):void
-}
-
-interface MariaCallBackObject {
-    (result:Object):void
-}
-
-interface MariaCallBackVoid {
-    ():void
-}
-
-interface Dictionary {
-    [index: string]: any;
-}
-
-interface MariaPreparedQuery {
-    (values:Dictionary):string;
-    (values:Array<any>):string;
-}
-
-interface ClientConfig {
-    host: string;
-    user: string;
-    password: string;
-    db?: string;
-    port?: number;
-    unixSocket?: string;
-    keepQueries?: boolean;
-    multiStatements?: boolean;
-    connTimeout?: number;
-    pingInterval?: number;
-    secureAuth?: boolean;
-    compress?: boolean;
-    ssl?:any;
-    local_infile?: boolean;
-    read_default_group?: string;
-    charset?: string;
-}
-
-declare class MariaResult {
-    on(signal:string, cb:MariaCallBackObject):MariaResult; // signal 'end'
-    on(signal:string, cb:MariaCallBackError):MariaResult;  // signal 'error'
-    on(signal:string, cb:MariaCallBackRow):MariaResult;    // signal 'row'
-    on(signal:string, cb:MariaCallBackVoid):MariaResult;   // signal 'abort'
-    abort():void;
-}
-
-declare class MariaQuery {
-    on(signal:string, cb:MariaCallBackResult):MariaQuery; // signal 'result'
-    on(signal:string, cb:MariaCallBackVoid):MariaQuery;   // signal 'end'
-    on(signal:string, cb:MariaCallBackVoid):MariaQuery;   // signal 'abort'
-    on(signal:string, cb:MariaCallBackError):MariaQuery;  // signal 'error'
-    abort():void;
-}
-
-declare class MariaClient {
-    connect(config:ClientConfig):void;
-    end():void;
-    destroy():void;
-    escape(query:string):string;
-    query(q:string, placeHolders?:Dictionary, useArray?:boolean):MariaQuery;
-    query(q:string, placeHolders?:Array<any>, useArray?:boolean):MariaQuery;
-    query(q:string, useArray?:boolean):MariaQuery;
-    prepare(query:string): MariaPreparedQuery;
-    isMariaDB():boolean;
-    on(signal:string, cb:MariaCallBackError): MariaClient;    // signal 'error'
-    on(signal:string, cb:MariaCallBackObject): MariaClient;   // signal 'close'
-    on(signal:string, cb:MariaCallBackVoid): MariaClient;     // signal 'connect'
-    connected: boolean;
-    threadId: string;
-}
-
-declare module 'mariasql' {
-    export = MariaClient;
-}
-
