907f991dd979addab808aacd9e3c8e227fd2145a
AdvancedREI <robert@advancedrei.com>
2015-04-01 20:58:39 -0700

    Fixes to Auth0 Lock
    - Made all LockOptions optional.
    - Added separate ConstructorOptions
    - Fixed some method overloads.

--- auth0.lock/auth0.lock-tests.ts
+++ auth0.lock/auth0.lock-tests.ts
@@ -1,9 +1,9 @@
 /// <reference path="../auth0/auth0.d.ts" />
-/// <reference path="auth0.widget.d.ts" />
+/// <reference path="auth0.lock.d.ts" />
 
-var lock: new Auth0Widget('dsa7d77dsa7d7', 'mine.auth0.com');
+var lock = new Auth0Lock('dsa7d77dsa7d7', 'mine.auth0.com');
 
-lock.show({
+lock.showSignin({
     connections: ['facebook', 'google-oauth2', 'twitter', 'Username-Password-Authentication'],
     icon: 'https://contoso.com/logo-32.png',
     socialBigButtons: true
