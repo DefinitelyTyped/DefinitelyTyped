87e0cd9982321a68257502ed6d94c80a71510624
Jason Jarrett <jason@elegantcode.com>
2015-04-10 23:19:04 -0700

    NugetAutomation test

--- breeze/breeze-tests.ts
+++ breeze/breeze-tests.ts
@@ -11,6 +11,7 @@ function test_dataType() {
     var x = typ.parentEnum === <breeze.core.IEnum> breeze.DataType;
     var isFalse = breeze.DataType.contains(breeze.DataType.Double);
     var dt = breeze.DataType.fromName("Decimal");
+    
 }
 
 function test_dataProperty() {
