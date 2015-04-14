1142e2b664b645f054d7f51060dc7b398fd0de25
a3chic9 <a3chic9@users.noreply.github.com>
2015-04-14 10:52:32 -0400

    Update Jasmine version to 2.2

--- jasmine/jasmine.d.ts
+++ jasmine/jasmine.d.ts
@@ -1,4 +1,4 @@
-// Type definitions for Jasmine 2.1
+// Type definitions for Jasmine 2.2
 // Project: http://jasmine.github.io/
 // Definitions by: Boris Yankov <https://github.com/borisyankov/>, Theodore Brown <https://github.com/theodorejb>, David Pärsson <https://github.com/davidparsson/>
 // Definitions: https://github.com/borisyankov/DefinitelyTyped
