afdb9c5d4bcba9c25d796532508ffbdf9ed42ddc
vvakame <vvakame+dev@gmail.com>
2015-04-15 23:23:21 +0900

    fix react/react-tests.ts and react/react-addons-tests.ts compile error

--- react/react-addons-tests.ts
+++ react/react-addons-tests.ts
@@ -201,7 +201,7 @@ myComponent.reset();
 // Attributes
 // --------------------------------------------------------------------------
 
-var children = ["Hello world", [null], React.DOM.span(null)];
+var children: any[] = ["Hello world", [null], React.DOM.span(null)];
 var divStyle = { // CSSProperties
     flex: "1 1 main-size",
     backgroundImage: "url('hello.png')"
