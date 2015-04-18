57db531e5dffa8766b0afcf1eb0e5907e92d24e4
remojansen <remo.jansen@wolksoftware.com>
2015-04-18 11:17:20 +0100

    added header and test

--- inversify/inversify.d.ts
+++ inversify/inversify.d.ts
@@ -1,37 +1,36 @@
-/// <reference path="interfaces.d.ts" />
+// Type definitions for inversify 1.0.0
+// Project: https://github.com/inversify/InversifyJS
+// Definitions by: inversify <https://github.com/inversify>
+// Definitions: https://github.com/borisyankov/DefinitelyTyped
 
-declare enum TypeBindingScopeEnum {
-    Transient = 0,
-    Singleton = 1,
+declare module inversify {
+  enum TypeBindingScopeEnum {
+      Transient = 0,
+      Singleton = 1,
+  }
+
+  class TypeBinding<TServiceType> implements TypeBindingInterface<TServiceType> {
+      runtimeIdentifier: string;
+      implementationType: {
+          new (): TServiceType;
+      };
+      cache: TServiceType;
+      scope: TypeBindingScopeEnum;
+      constructor(runtimeIdentifier: string, implementationType: {
+          new (...args: any[]): TServiceType;
+      }, scopeType?: TypeBindingScopeEnum);
+  }
+
+  class Kernel implements KernelInterface {
+      private _bindings;
+      bind(typeBinding: TypeBindingInterface<any>): void;
+      unbind(runtimeIdentifier: string): void;
+      unbindAll(): void;
+      resolve<TImplementationType>(runtimeIdentifier: string): TImplementationType;
+      private _validateBinding(typeBinding);
+      private _getConstructorArguments(func);
+      private _injectDependencies<TImplementationType>(func);
+      private _construct<TImplementationType>(constr, args);
+      constructor();
+  }
 }
-
-declare class TypeBinding<TServiceType> implements TypeBindingInterface<TServiceType> {
-    runtimeIdentifier: string;
-    implementationType: {
-        new (): TServiceType;
-    };
-    cache: TServiceType;
-    scope: TypeBindingScopeEnum;
-    constructor(runtimeIdentifier: string, implementationType: {
-        new (...args: any[]): TServiceType;
-    }, scopeType?: TypeBindingScopeEnum);
-}
-
-declare class Kernel implements KernelInterface {
-    private _bindings;
-    bind(typeBinding: TypeBindingInterface<any>): void;
-    unbind(runtimeIdentifier: string): void;
-    unbindAll(): void;
-    resolve<TImplementationType>(runtimeIdentifier: string): TImplementationType;
-    private _validateBinding(typeBinding);
-    private _getConstructorArguments(func);
-    private _injectDependencies<TImplementationType>(func);
-    private _construct<TImplementationType>(constr, args);
-    constructor();
-}
-
-declare var inversify: {
-    Kernel: typeof Kernel;
-    TypeBindingScopeEnum: typeof TypeBindingScopeEnum;
-    TypeBinding: typeof TypeBinding;
-};
