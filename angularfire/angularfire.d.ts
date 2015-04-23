c303785b7f1e5cde243122ca17a032c5cfd34693
kpgarrod <kpgarrod@gmail.com>
2015-04-23 09:40:34 +0200

    Update definitions to angularfire 1.0.0
    I'm new at typescript, so no guarantees! Also I haven't updated the auth part because I don't use it.

--- angularfire/angularfire.d.ts
+++ angularfire/angularfire.d.ts
@@ -1,82 +1,64 @@
-// Type definitions for AngularFire 0.8.2
+// Type definitions for AngularFire 1.0.0
 // Project: http://angularfire.com
+// Definitions by: Keith Garrod <http://github.com/kpgarrod>
 // Definitions by: Dénes Harmath <http://github.com/thSoft>
 // Definitions: https://github.com/borisyankov/DefinitelyTyped
 
 /// <reference path="../angularjs/angular.d.ts"/>
 /// <reference path="../firebase/firebase.d.ts"/>
 
-interface AngularFireService {
-	(firebase: Firebase, config?: any): AngularFire;
+interface AngularFireArrayService {
+  (firebase: FirebaseQuery): AngularFireArray;
 }
 
-interface AngularFire {
-	$asArray(): AngularFireArray;
-	$asObject(): AngularFireObject;
-	$ref(): Firebase;
-	$push(data: any): ng.IPromise<Firebase>;
-	$set(key: string, data: any): ng.IPromise<Firebase>;
-	$set(data: any): ng.IPromise<Firebase>;
-	$remove(key?: string): ng.IPromise<Firebase>;
-	$update(key: string, data: Object): ng.IPromise<Firebase>;
-	$update(data: any): ng.IPromise<Firebase>;
-	$transaction(updateFn: (currentData: any) => any, applyLocally?: boolean): ng.IPromise<FirebaseDataSnapshot>;
-	$transaction(key:string, updateFn: (currentData: any) => any, applyLocally?: boolean): ng.IPromise<FirebaseDataSnapshot>;
+interface AngularFireObjectService {
+  (firebase: FirebaseQuery): AngularFireObject;
 }
 
 interface AngularFireObject extends AngularFireSimpleObject {
-	$id: string;
-	$priority: number;
-	$value: any;
-	$save(): ng.IPromise<Firebase>;
-	$loaded(resolve?: (x: AngularFireObject) => ng.IHttpPromise<{}>, reject?: (err: any) => any): ng.IPromise<AngularFireObject>;
-	$loaded(resolve?: (x: AngularFireObject) => ng.IPromise<{}>, reject?: (err: any) => any): ng.IPromise<AngularFireObject>;
-	$loaded(resolve?: (x: AngularFireObject) => void, reject?: (err: any) => any): ng.IPromise<AngularFireObject>;
-	$inst(): AngularFire;
-	$bindTo(scope: ng.IScope, varName: string): ng.IPromise<any>;
-	$watch(callback: Function, context?: any): Function;
-	$destroy(): void;
-}
-interface AngularFireObjectService {
-	$extendFactory(ChildClass: Object, methods?: Object): Object;
+  $remove(key?: string): angular.IPromise<Firebase>;
+  $save(): angular.IPromise<Firebase>;
+  $loaded(resolve?: (x: AngularFireObject) => angular.IHttpPromise<{}>, reject?: (err: any) => any): angular.IPromise<AngularFireObject>;
+  $loaded(resolve?: (x: AngularFireObject) => angular.IPromise<{}>, reject?: (err: any) => any): angular.IPromise<AngularFireObject>;
+  $loaded(resolve?: (x: AngularFireObject) => void, reject?: (err: any) => any): angular.IPromise<AngularFireObject>;
+  $bindTo(scope: angular.IScope, varName: string): angular.IPromise<any>;
+  $watch(callback: Function, context?: any): Function;
 }
 
 interface AngularFireArray extends Array<AngularFireSimpleObject> {
-	$add(newData: any): ng.IPromise<Firebase>;
-	$save(recordOrIndex: any): ng.IPromise<Firebase>;
-	$remove(recordOrIndex: any): ng.IPromise<Firebase>;
-	$getRecord(key: string): AngularFireSimpleObject;
-	$keyAt(recordOrIndex: any): string;
-	$indexFor(key: string): number;
-	$loaded(resolve?: (x: AngularFireArray) => ng.IHttpPromise<{}>, reject?: (err: any) => any): ng.IPromise<AngularFireArray>;
-	$loaded(resolve?: (x: AngularFireArray) => ng.IPromise<{}>, reject?: (err: any) => any): ng.IPromise<AngularFireArray>;
-	$loaded(resolve?: (x: AngularFireArray) => void, reject?: (err: any) => any): ng.IPromise<AngularFireArray>;
-	$inst(): AngularFire;
-	$watch(cb: (event: string, key: string, prevChild: string) => void, context?: any): Function;
-	$destroy(): void;
-}
-interface AngularFireArrayService {
-	$extendFactory(ChildClass: Object, methods?: Object): Object;
+  $add(newData: any): angular.IPromise<Firebase>;
+  $remove(recordOrIndex: any): angular.IPromise<Firebase>;
+  $save(recordOrIndex: any): angular.IPromise<Firebase>;
+  $getRecord(key: string): AngularFireSimpleObject;
+  $keyAt(recordOrIndex: any): string;
+  $indexFor(key: string): number;
+  $loaded(resolve?: (x: AngularFireArray) => angular.IHttpPromise<{}>, reject?: (err: any) => any): angular.IPromise<AngularFireArray>;
+  $loaded(resolve?: (x: AngularFireArray) => angular.IPromise<{}>, reject?: (err: any) => any): angular.IPromise<AngularFireArray>;
+  $loaded(resolve?: (x: AngularFireArray) => void, reject?: (err: any) => any): angular.IPromise<AngularFireArray>;
+  $watch(cb: (event: string, key: string, prevChild: string) => void, context?: any): Function;
 }
 
 interface AngularFireSimpleObject {
-	$id: string;
-	$priority: number;
-	$value: any;
-	[key: string]: any;
+  $id: string;
+  $priority: number;
+  $ref(): Firebase;
+  $value: any;
+  $extend(methods: Object): Object;
+  $destroy(): void;
+  [key: string]: any;
 }
 
 
 interface AngularFireAuthService {
-	(firebase: Firebase): AngularFireAuth;
+  (firebase: Firebase): AngularFireAuth;
 }
 
 interface AngularFireAuth {
-	$getCurrentUser(): ng.IPromise<any>;
-	$login(provider: string, options?: Object): ng.IPromise<any>;
-	$logout(): void;
-	$createUser(email: string, password: string): ng.IPromise<any>;
-	$changePassword(email: string, oldPassword: string, newPassword: string): ng.IPromise<any>;
-	$removeUser(email: string, password: string): ng.IPromise<any>;
-	$sendPasswordResetEmail(email: string): ng.IPromise<any>;
+  $getCurrentUser(): angular.IPromise<any>;
+  $login(provider: string, options?: Object): angular.IPromise<any>;
+  $logout(): void;
+  $createUser(email: string, password: string): angular.IPromise<any>;
+  $changePassword(email: string, oldPassword: string, newPassword: string): angular.IPromise<any>;
+  $removeUser(email: string, password: string): angular.IPromise<any>;
+  $sendPasswordResetEmail(email: string): angular.IPromise<any>;
 }
