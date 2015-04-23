b0253bbce030616a6169f3b94dc936643065684e
Wim Looman <wim.looman@telogis.com>
2015-04-23 12:12:34 +1200

    Fix return types of UUID when a buffer is passed in

--- node-uuid/node-uuid.d.ts
+++ node-uuid/node-uuid.d.ts
@@ -33,17 +33,21 @@ interface UUIDOptions {
 }
 
 interface UUID {
-    v1(options?: UUIDOptions, buffer?: number[], offset?: number): string
-    v1(options?: UUIDOptions, buffer?: Buffer, offset?: number): string
+    v1(options?: UUIDOptions): string
+    v1(options?: UUIDOptions, buffer?: number[], offset?: number): number[]
+    v1(options?: UUIDOptions, buffer?: Buffer, offset?: number): Buffer
 
-    v2(options?: UUIDOptions, buffer?: number[], offset?: number): string
-    v2(options?: UUIDOptions, buffer?: Buffer, offset?: number): string
+    v2(options?: UUIDOptions): string
+    v2(options?: UUIDOptions, buffer?: number[], offset?: number): number[]
+    v2(options?: UUIDOptions, buffer?: Buffer, offset?: number): Buffer
 
-    v3(options?: UUIDOptions, buffer?: number[], offset?: number): string
-    v3(options?: UUIDOptions, buffer?: Buffer, offset?: number): string
+    v3(options?: UUIDOptions): string
+    v3(options?: UUIDOptions, buffer?: number[], offset?: number): number[]
+    v3(options?: UUIDOptions, buffer?: Buffer, offset?: number): Buffer
 
-    v4(options?: UUIDOptions, buffer?: number[], offset?: number): string
-    v4(options?: UUIDOptions, buffer?: Buffer, offset?: number): string
+    v4(options?: UUIDOptions): string
+    v4(options?: UUIDOptions, buffer?: number[], offset?: number): number[]
+    v4(options?: UUIDOptions, buffer?: Buffer, offset?: number): Buffer
 
     parse(id: string, buffer?: number[], offset?: number): number[]
     parse(id: string, buffer?: Buffer, offset?: number): Buffer
