94df0f90410364b1fc5ce1cba863a5ac2f6c9a10
Matt Traynham <mrt6467@alum.uncw.edu>
2015-04-24 14:08:07 -0400

    Colorbrewer definition

---
+++ colorbrewer/colorbrewer-tests.ts
@@ -0,0 +1,8 @@
+/// <reference path="./colorbrewer.d.ts" />
+
+const accent3: string[] = colorbrewer.Accent[3];
+const accent4: string[] = colorbrewer.Accent[4];
+const accent5: string[] = colorbrewer.Accent[5];
+const accent6: string[] = colorbrewer.Accent[6];
+const accent7: string[] = colorbrewer.Accent[7];
+const accent8: string[] = colorbrewer.Accent[8];
