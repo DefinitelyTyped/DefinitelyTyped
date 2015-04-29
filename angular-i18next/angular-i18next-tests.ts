bfc68a72a8f372711842e52caeda7f0a063315e8
Cyril Schumacher <cyril_schumacher@live.fr>
2015-04-29 18:03:29 +0200

    Add test for "angular-i18next" definition.

---
+++ angular-i18next/angular-i18next-tests.ts
@@ -0,0 +1,8 @@
+/// <reference path="angular-i18next.d.ts" />
+/**
+* @summary Test for "angular-i18next" with options.
+*/
+function testOptions() {
+    var $provider: I18nextProvider;
+    $provider.options = {};
+}
