88b29b7995a27a11dfc241cb236c926e151b0a3e
Alexey Gorshkov <agorshkov23@yandex.ru>
2015-05-11 01:51:24 +0300

    Added jsurl

---
+++ jsurl/jsurl.d.ts
@@ -0,0 +1,18 @@
+﻿// Type definitions for jsurl 1.2.2
+// Project: https://github.com/Mikhus/jsurl
+// Definitions by: Alexey Gorshkov <https://github.com/agorshkov23>
+// Definitions: https://github.com/agorshkov23/DefinitelyTyped
+
+declare class Url {
+    constructor(url?: string);
+    query: any;
+    protocol: string;
+    user: string;
+    pass: string;
+    host: string;
+    port: string;
+    path: string;
+    hash: string;
+    href: string;
+    toString(): string;
+}
