f9996b4dda9790d89d5c8ebe5356dab22597f856
Blake Niemyjski <bniemyjski@gmail.com>
2015-05-14 10:14:44 -0500

    Fixed unit tests and naming conventions.

--- stacktrace-js/stacktrace-js-tests.ts
+++ stacktrace-js/stacktrace-js-tests.ts
@@ -1,18 +1,17 @@
-import StackTrace = require('StackTrace');
+/// <reference path="stacktrace-js.d.ts" />
 
 function interestingFn() {
     return 'https://github.com/exceptionless/Exceptionless';
 }
 
-var callback = function(stackframes) {
-    var stringifiedStack = stackframes.map(function(sf) {
+var callback = function(stackframes:StackTrace.StackFrame[]) {
+    var stringifiedStack = stackframes.map(function(sf:StackTrace.StackFrame) {
         return sf.toString();
     }).join('\n');
     console.log(stringifiedStack);
 };
 
-var errback = function(err) { console.log(err.message); };
-
+var errorCallback = function(err:Error) { console.log(err.message); };
 
 StackTrace.get();
 
@@ -21,5 +20,5 @@ var error = new Error('BOOM!');
 StackTrace.fromError(error);
 StackTrace.generateArtificially();
 
-StackTrace.instrument(interestingFn, callback, errback);
+StackTrace.instrument(interestingFn, callback, errorCallback);
 StackTrace.deinstrument(interestingFn);
