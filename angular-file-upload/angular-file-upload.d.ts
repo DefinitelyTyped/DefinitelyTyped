bce7b6232da99692eeb9ea96a52f8c9b8108c9f3
Chris Barr <cbarr@appriver.com>
2015-05-22 07:50:50 -0400

    Fixing header format

--- angular-file-upload/angular-file-upload.d.ts
+++ angular-file-upload/angular-file-upload.d.ts
@@ -1,6 +1,6 @@
 ﻿// Type definitions for Angular File Upload 4.2.1
 // Project: https://github.com/danialfarid/ng-file-upload
-// Definitions by: John Reilly <https://github.com/johnnyreilly> & Chris Barr <https://github.com/chrismbarr>
+// Definitions by: John Reilly <https://github.com/johnnyreilly>
 // Definitions: https://github.com/borisyankov/DefinitelyTyped
 
 /// <reference path="../angularjs/angular.d.ts" />
