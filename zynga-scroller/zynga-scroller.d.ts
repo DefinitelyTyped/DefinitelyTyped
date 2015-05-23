9796c3973d9fbbd214142ec3da32f952ab9dfd74
Haskell Camargo <marcelocamargo@linuxmail.org>
2015-05-23 01:24:25 -0300

    Added URL to project label

--- zynga-scroller/zynga-scroller.d.ts
+++ zynga-scroller/zynga-scroller.d.ts
@@ -1,5 +1,5 @@
 // Type definitions for Zynga Scroller
-// Project: Zynga Scroller
+// Project: http://zynga.github.com/scroller/
 // Definitions by: Marcelo Haskell Camargo <https://github.com/haskellcamargo>
 // Definitions: https://github.com/borisyankov/DefinitelyTyped
 
