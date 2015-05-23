1b749c5f01f4e101db0ac2232069eb5b1256fd77
aditya <dev@chimeracoder.net>
2015-05-22 20:43:31 -0400

    Update JQuery export for ES6
    * Previously compiling with -t ES6 would fail

--- jquery/jquery.d.ts
+++ jquery/jquery.d.ts
@@ -3156,7 +3156,7 @@ interface JQuery {
     queue(queueName: string, callback: Function): JQuery;
 }
 declare module "jquery" {
-    export = $;
+    export default $;
 }
 declare var jQuery: JQueryStatic;
 declare var $: JQueryStatic;
