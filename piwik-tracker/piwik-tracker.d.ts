ca873b08a38c57559d4eebe949737710343dd177
Guilherme Bernal <dev@lbguilherme.com>
2015-05-25 00:23:31 -0300

    piwik-tracker: Update project url

--- piwik-tracker/piwik-tracker.d.ts
+++ piwik-tracker/piwik-tracker.d.ts
@@ -1,5 +1,5 @@
 // Type definitions for PiwikTracker v0.1.1
-// Project: http://piwik.org - https://www.npmjs.com/package/piwik-tracker
+// Project: https://www.npmjs.com/package/piwik-tracker
 // Definitions by: Guilherme Bernal <https://github.com/lbguilherme>
 // Definitions: https://github.com/borisyankov/DefinitelyTyped
 
