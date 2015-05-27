34ef6f1dd3ce8c4ead9c1234ca96f2e582f08160
Kevin Wilson <kevin@kwilson.me.uk>
2015-05-27 15:02:08 +0100

    Added to change log.

--- routie/routie.d.ts
+++ routie/routie.d.ts
@@ -1,6 +1,7 @@
 // Type definitions for routie 0.3.2
 // Project: https://github.com/jgallen23/routie
 // Definitions by: Adilson <https://github.com/Adilson>
+// Definitions by: kwilson <https://github.com/kwilson>
 // Definitions: https://github.com/borisyankov/DefinitelyTyped
 
 declare module routie {
