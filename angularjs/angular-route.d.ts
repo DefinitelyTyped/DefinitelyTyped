c4c9d22e612f10196ea0de6d00c1ccdeb5bbcce1
Bogdan I. Bursuc <bogdanbursuc86@gmail.com>
2015-06-01 17:08:15 +0300

    Remove incorrect ICurrentRoute api

--- angularjs/angular-route.d.ts
+++ angularjs/angular-route.d.ts
@@ -108,8 +108,6 @@ declare module angular.route {
 
     // see http://docs.angularjs.org/api/ng.$route#current
     interface ICurrentRoute extends IRoute {
-        $$route?: IRoute;
-
         locals: {
             $scope: IScope;
             $template: string;
