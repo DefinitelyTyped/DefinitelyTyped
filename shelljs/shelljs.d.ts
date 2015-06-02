ac2f3b72501fdfe7784e3297d565003ef632fde8
Carl Patenaude Poulin <carl.patenaudepoulin@mail.mcgill.ca>
2015-06-02 15:01:16 -0700

    Remove node.d.ts reference in shelljs.d.ts
    The removed reference to node.d.ts in shelljs is an error in TypeScript 1.5.

--- shelljs/shelljs.d.ts
+++ shelljs/shelljs.d.ts
@@ -3,9 +3,6 @@
 // Definitions by: Niklas Mollenhauer <https://github.com/nikeee>
 // Definitions: https://github.com/borisyankov/DefinitelyTyped
 
-
-///<reference path="../node/node.d.ts"/>
-
 declare module "shelljs"
 {
     import child = require("child_process");
