cc16131ae65bbfc4ac3fcbe093f0d7ac1bf0d088
Sven-Leonhard Weiler <slwp@intelligentgraphics.biz>
2015-06-03 16:08:30 +0200

    corrections

--- soapclient/soapclient.d.ts
+++ soapclient/soapclient.d.ts
@@ -1,8 +1,15 @@
+// Type definitions for JavaScript SOAP Client
+// Project: http://javascriptsoapclient.codeplex.com/
+// Definitions by: xDreamCoding <https://github.com/xdreamcoding/>
+// Definitions: https://github.com/borisyankov/DefinitelyTyped
+
 declare class SOAPClientParameters {
-	add(name: string|number, value: string|number|boolean|Object);
-	toXml();
+	public add(name: string|number, value: string|number|boolean|Object): SOAPClientParameters;
+	public toXml(): string;
 }
 
-declare class SOAPClient {
-	invoke(url: string, method: string, parameters: SOAPClientParameters, async: boolean, callback: Function);
+interface SOAPClient {
+	public invoke(url: string, method: string, parameters: SOAPClientParameters, async: boolean, callback?: (response: any) => any): any;
 }
+
+declare var SOAPClient: SOAPClient;
