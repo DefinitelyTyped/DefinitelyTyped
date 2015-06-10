e75a0e995133de6ae22f7df7e3b9aad22819b185
Christopher Brown <io@henrian.com>
2015-06-10 12:39:40 -0500

    Add unorm.d.ts header and unorm tests file

--- unorm/unorm.d.ts
+++ unorm/unorm.d.ts
@@ -1,3 +1,8 @@
+// Type definitions for unorm 1.3.3
+// Project: https://github.com/walling/unorm
+// Definitions by: Christopher Brown <https://github.com/chbrown>
+// Definitions: https://github.com/borisyankov/DefinitelyTyped
+
 declare module unorm {
   interface Static {
     nfd(str: string): string;
