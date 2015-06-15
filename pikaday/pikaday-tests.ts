9baad8ed62aefe8ca323480ca5fa321a278d8135
Rudolph Gottesheim <r.gottesheim@loot.at>
2015-06-15 11:42:23 +0200

    Add Pikaday test

--- pikaday/pikaday-tests.ts
+++ pikaday/pikaday-tests.ts
@@ -55,3 +55,14 @@ new Pikaday({field: $('#datepicker')[0]});
     };
     new Pikaday({i18n});
 })();
+
+(() => {
+    new Pikaday(
+        {
+            field: document.getElementById('datepicker'),
+            firstDay: 1,
+            minDate: new Date('2000-01-01'),
+            maxDate: new Date('2020-12-31'),
+            yearRange: [2000, 2020]
+        });
+})();
