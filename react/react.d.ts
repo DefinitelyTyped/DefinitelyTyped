13044124731da06aacc0324e5f26aae1448559c3
Seivan Heidari <seivan.heidari@icloud.com>
2015-06-20 20:29:49 +0200

    Commenting out HTMLAttributes property? because it won't allow inline styling
    //Bug:
    let x = React.DOM.a( { cols : 2 }) // Doesn't work
    
    //Expected
    let x = React.DOM.a( { cols : 2 })
    
    //Workaround
    var modal = <React.HTMLAttributes>{}
    modal.cols = 2
    React.DOM.a( modal, "sample")

--- react/react.d.ts
+++ react/react.d.ts
@@ -511,7 +511,7 @@ declare module "react" {
         // Non-standard Attributes
         autoCapitalize?: boolean;
         autoCorrect?: boolean;
-        property?: string;
+        // property?: string;
         itemProp?: string;
         itemScope?: boolean;
         itemType?: string;
@@ -778,4 +778,3 @@ declare module "react" {
         identifiedTouch(identifier: number): Touch;
     }
 }
-
