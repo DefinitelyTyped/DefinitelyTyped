7a37cdbfbcb3576021f7e19385a93a17a3ed2ef7
Ákos Lukács <akoslukacs42@gmail.com>
2015-06-23 11:23:50 +0200

    more type arguments + header format fix

--- angular-gettext/angular-gettext-tests.ts
+++ angular-gettext/angular-gettext-tests.ts
@@ -12,19 +12,18 @@ module angular_gettext_tests {
   //Highlighting untranslated strings
   gettextCatalog.debug = true;
 
-
-
+  
   // Marking strings in JavaScript code as translatable. 
   // https://angular-gettext.rocketeer.be/dev-guide/annotate-js/
   var gettext = angular_gettext.gettext;
   var myString = gettext("Hello");
 
   //Translating directly in JavaScript.
-  angular.module("myApp").controller("helloController", function (gettextCatalog) {
+  angular.module("myApp").controller("helloController", function (gettextCatalog: angular_gettext.gettextCatalog) {
     var translated: string = gettextCatalog.getString("Hello");
   });
 
-  angular.module("myApp").controller("helloController", function (gettextCatalog) {
+  angular.module("myApp").controller("helloController", function (gettextCatalog: angular_gettext.gettextCatalog) {
     var myString2: string = gettextCatalog.getPlural(3, "Bird", "Birds");
   });
 
@@ -43,13 +42,15 @@ module angular_gettext_tests {
   });
 
 
+  interface helloControllerScope extends ng.IScope {
+    switchLanguage: (lang: string) => void;
+  }
   // Lazy-loading languages
   // https://angular-gettext.rocketeer.be/dev-guide/lazy-loading/
-  angular.module("myApp").controller("helloController", function ($scope, gettextCatalog: angular_gettext.gettextCatalog) {
+  angular.module("myApp").controller("helloController", function ($scope: helloControllerScope, gettextCatalog: angular_gettext.gettextCatalog) {
     $scope.switchLanguage = function (lang: string) {
       gettextCatalog.setCurrentLanguage(lang);
       gettextCatalog.loadRemote("/languages/" + lang + ".json");
     };
   });
-
 }
