6e60e412b7788a896adbf52464b2c8809d2eaeb3
Gitgiddy <Gitgiddy@users.noreply.github.com>
2015-06-26 11:18:15 -0400

    namespacing syntax corrected

--- github-electron/github-electron-renderer.d.ts
+++ github-electron/github-electron-renderer.d.ts
@@ -6,7 +6,6 @@
 /// <reference path="./github-electron.d.ts" />
 
 declare module GitHubElectron {
-
     class InProcess implements NodeJS.EventEmitter {
         addListener(event: string, listener: Function): InProcess;
         on(event: string, listener: Function): InProcess;
@@ -37,9 +36,62 @@ declare module GitHubElectron {
         sendToHost(channel: string, ...args: any[]): void;
     }
 
-    module Remote {
-        export function getCurrentWindow(): BrowserWindow;
-    }
+	interface Remote {
+	    /**
+	     * @returns The object returned by require(module) in the main process.
+	     */
+	    require(module: string): any;
+	    /**
+	     * @returns The BrowserWindow object which this web page belongs to.
+	     */
+	    getCurrentWindow(): BrowserWindow
+	    /**
+	     * @returns The global variable of name (e.g. global[name]) in the main process.
+	     */
+	    getGlobal(name: string): any;
+	    /**
+	     * Returns the process object in the main process. This is the same as
+	     * remote.getGlobal('process'), but gets cached.
+	     */
+	    process: any;
+	}
+	
+	interface WebFrame {
+	    /**
+	     * Changes the zoom factor to the specified factor, zoom factor is
+	     * zoom percent / 100, so 300% = 3.0.
+	     */
+	    setZoomFactor(factor: number): void;
+	    /**
+	     * @returns The current zoom factor.
+	     */
+	    getZoomFactor(): number;
+	    /**
+	     * Changes the zoom level to the specified level, 0 is "original size", and each
+	     * increment above or below represents zooming 20% larger or smaller to default
+	     * limits of 300% and 50% of original size, respectively.
+	     */
+	    setZoomLevel(level: number): void;
+	    /**
+	     * @returns The current zoom level.
+	     */
+	    getZoomLevel(): number;
+	    /**
+	     * Sets a provider for spell checking in input fields and text areas.
+	     */
+	    setSpellCheckProvider(language: string, autoCorrectWord: boolean, provider: {
+	        /**
+	         * @returns Whether the word passed is correctly spelled.
+	         */
+	        spellCheck: (text: string) => boolean;
+	    }): void;
+	    /**
+	     * Sets the scheme as secure scheme. Secure schemes do not trigger mixed content
+	     * warnings. For example, https and data are secure schemes because they cannot be
+	     * corrupted by active network attackers.
+	     */
+	    registerUrlSchemeAsSecure(scheme: string): void;
+	}
 }
 
 declare module 'ipc' {
@@ -47,65 +99,8 @@ declare module 'ipc' {
     export = InProcess;
 }
 
-interface GitHubElectron.Remote {
-    /**
-     * @returns The object returned by require(module) in the main process.
-     */
-    require(module: string): any;
-    /**
-     * @returns The BrowserWindow object which this web page belongs to.
-     */
-    getCurrentWindow: typeof GitHubElectron.Remote.getCurrentWindow;
-    /**
-     * @returns The global variable of name (e.g. global[name]) in the main process.
-     */
-    getGlobal(name: string): any;
-    /**
-     * Returns the process object in the main process. This is the same as
-     * remote.getGlobal('process'), but gets cached.
-     */
-    process: any;
-}
-
 declare module 'remote' {
     export = GitHubElectron.Remote;
-}
-
-interface GitHubElectron.WebFrame {
-    /**
-     * Changes the zoom factor to the specified factor, zoom factor is
-     * zoom percent / 100, so 300% = 3.0.
-     */
-    setZoomFactor(factor: number): void;
-    /**
-     * @returns The current zoom factor.
-     */
-    getZoomFactor(): number;
-    /**
-     * Changes the zoom level to the specified level, 0 is "original size", and each
-     * increment above or below represents zooming 20% larger or smaller to default
-     * limits of 300% and 50% of original size, respectively.
-     */
-    setZoomLevel(level: number): void;
-    /**
-     * @returns The current zoom level.
-     */
-    getZoomLevel(): number;
-    /**
-     * Sets a provider for spell checking in input fields and text areas.
-     */
-    setSpellCheckProvider(language: string, autoCorrectWord: boolean, provider: {
-        /**
-         * @returns Whether the word passed is correctly spelled.
-         */
-        spellCheck: (text: string) => boolean;
-    }): void;
-    /**
-     * Sets the scheme as secure scheme. Secure schemes do not trigger mixed content
-     * warnings. For example, https and data are secure schemes because they cannot be
-     * corrupted by active network attackers.
-     */
-    registerUrlSchemeAsSecure(scheme: string): void;
 }
 
 declare module 'web-frame' {
