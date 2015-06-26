390123c140d55ada6ff45a3bc70288533d83aa37
Raphael ATALLAH <utfc243f@randomail.net>
2015-06-26 12:14:56 -0700

    Updated definitions author

--- angular-odata-resources/angular-odata-resources.d.ts
+++ angular-odata-resources/angular-odata-resources.d.ts
@@ -1,6 +1,6 @@
 // Type definitions for OData Angular Resources
 // Project: https://github.com/devnixs/ODataAngularResources
-// Definitions by: Raphael ATALLAH http://raphael.atallah.me
+// Definitions by: Raphael ATALLAH <http://raphael.atallah.me>
 // Definitions: https://github.com/borisyankov/DefinitelyTyped
 
 /// <reference path="../angularjs/angular.d.ts" />
