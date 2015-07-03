669dea65e5d88ef1a7cba47f42d751527b79c72e
Tanguy Krotoff <tkrotoff@gmail.com>
2015-07-03 20:02:26 +0200

    condition parameter can be much more than a boolean

--- gulp-if/gulp-if-tests.ts
+++ gulp-if/gulp-if-tests.ts
@@ -1,10 +1,16 @@
-/// <reference path="./gulp-if.d.ts"/>
+/// <reference path="gulp-if.d.ts"/>
 /// <reference path="../gulp/gulp.d.ts"/>
+/// <reference path="../gulp-uglify/gulp-uglify.d.ts"/>
+
 import gulp = require("gulp");
 import _if = require("gulp-if");
+import uglify = require("gulp-uglify");
 
 gulp.src("test.css")
     .pipe(_if(true, gulp.src("test.css")));
 
 gulp.src("test.css")
     .pipe(_if(false, gulp.src("test.css"), gulp.src("test.css")));
+
+gulp.src("index.html")
+    .pipe(_if('*.js', uglify()));
