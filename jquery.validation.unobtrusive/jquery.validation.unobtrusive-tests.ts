f440d7a5a987b82e1eb11cdc9f3132cf3212d91e
Matt Brooks <mattbrooks2010@live.co.uk>
2015-07-06 17:43:17 +0100

    Add Microsoft JQuery Unobtrusive Validation

---
+++ jquery.validation.unobtrusive/jquery.validation.unobtrusive-tests.ts
@@ -0,0 +1,13 @@
+﻿/// <reference path="jquery.validation.unobtrusive.d.ts" />
+
+$.validator.unobtrusive.adapters.add("adapter", () => { });
+$.validator.unobtrusive.adapters.add("adapter", function () {
+});
+
+$.validator.unobtrusive.adapters.addBool("adapter");
+
+$.validator.unobtrusive.adapters.addMethod("adapter",(value, element) => {
+    return true;
+})
+
+$.validator.unobtrusive.parse(document);
