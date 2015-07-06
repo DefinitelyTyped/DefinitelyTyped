705b4cc80bced7c0cc95575741113667d210602f
Christoph Koerner <christoph.koerner@gesim.ch>
2015-07-06 23:29:23 +0200

    Removed version, because angular.d.ts requires it

--- jquery/jquery.d.ts
+++ jquery/jquery.d.ts
@@ -3164,7 +3164,5 @@ interface JQuery {
     queue(queueName: string, callback: Function): JQuery;
 }
 declare module "jquery" {
-    export = $;
 }
 declare var jQuery: JQueryStatic;
-declare var $: JQueryStatic;
