2eb1788cbee0a40cfd6fcea1dc51cd61509225ce
Syoudai Yokoyama <s-yokoyama@timedia.co.jp>
2015-07-06 10:38:04 +0900

    Support JSX spread attribute

--- react/react.d.ts
+++ react/react.d.ts
@@ -125,6 +125,9 @@ declare module React {
     var PropTypes: ReactPropTypes;
     var Children: ReactChildren;
 
+    // Hook for JSX spread syntax
+    var __spread: any;
+
     //
     // Component API
     // ----------------------------------------------------------------------
