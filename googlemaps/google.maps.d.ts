efd764c6430fb5ad0294d38a694c0ceac66dd291
Chris Wrench <c.g.wrench@gmail.com>
2015-07-06 13:22:22 +0100

    Fix typo in license

--- googlemaps/google.maps.d.ts
+++ googlemaps/google.maps.d.ts
@@ -14,7 +14,7 @@ in the Software without restriction, including without limitation the rights
 to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
 copies of the Software, and to permit persons to whom the Software is
 furnished to do so, subject to the following conditions:
-h
+
 The above copyright notice and this permission notice shall be included in
 all copies or substantial portions of the Software.
 
