329ed16b58bc93944008d9eb5d0b8108cda296a3
Robert Baker <se7enalive@gmail.com>
2015-07-07 16:49:25 -0700

    fixed test, versioned old file.

--- angular-material/angular-material-tests.ts
+++ angular-material/angular-material-tests.ts
@@ -3,11 +3,11 @@
 var myApp = angular.module('testModule', ['ngMaterial']);
 
 myApp.config((
-    $mdThemingProvider: ng.material.MDThemingProvider,
-    $mdIconProvider: ng.material.MDIconProvider) => {
+    $mdThemingProvider: ng.material.IThemingProvider,
+    $mdIconProvider: ng.material.IIconProvider) => {
 
     $mdThemingProvider.alwaysWatchTheme(true);
-    var neonRedMap: ng.material.MDPalette = $mdThemingProvider.extendPalette('red', {
+    var neonRedMap: ng.material.IPalette = $mdThemingProvider.extendPalette('red', {
         '500': 'ff0000'
     });
     // Register the new color palette map with the name <code>neonRed</code>
@@ -27,7 +27,7 @@ myApp.config((
         .icon('work:chair', 'my/app/chair.svg');  // Register icon in a specific set
 });
 
-myApp.controller('BottomSheetController', ($scope: ng.IScope, $mdBottomSheet: ng.material.MDBottomSheetService) => {
+myApp.controller('BottomSheetController', ($scope: ng.IScope, $mdBottomSheet: ng.material.IBottomSheetService) => {
     $scope['openBottomSheet'] = () => {
         $mdBottomSheet.show({
             template: '<md-bottom-sheet>Hello!</md-bottom-sheet>'
@@ -37,7 +37,7 @@ myApp.controller('BottomSheetController', ($scope: ng.IScope, $mdBottomSheet: ng
     $scope['cancelBottomSheet'] = $mdBottomSheet.cancel.bind($mdBottomSheet, 'cancel');
 });
 
-myApp.controller('DialogController', ($scope: ng.IScope, $mdDialog: ng.material.MDDialogService) => {
+myApp.controller('DialogController', ($scope: ng.IScope, $mdDialog: ng.material.IDialogService) => {
     $scope['openDialog'] = () => {
         $mdDialog.show({
             template: '<md-dialog>Hello!</md-dialog>'
@@ -55,8 +55,8 @@ myApp.controller('DialogController', ($scope: ng.IScope, $mdDialog: ng.material.
 
 class IconDirective implements ng.IDirective {
 
-    private $mdIcon: ng.material.MDIcon;
-    constructor($mdIcon: ng.material.MDIcon) {
+    private $mdIcon: ng.material.IIcon;
+    constructor($mdIcon: ng.material.IIcon) {
         this.$mdIcon = $mdIcon;
     }
 
@@ -69,9 +69,9 @@ class IconDirective implements ng.IDirective {
         });
     }
 }
-myApp.directive('icon-directive', ($mdIcon: ng.material.MDIcon) => new IconDirective($mdIcon));
+myApp.directive('icon-directive', ($mdIcon: ng.material.IIcon) => new IconDirective($mdIcon));
 
-myApp.controller('MediaController', ($scope: ng.IScope, $mdMedia: ng.material.MDMedia) => {
+myApp.controller('MediaController', ($scope: ng.IScope, $mdMedia: ng.material.IMedia) => {
     $scope.$watch(() => $mdMedia('lg'), (big: boolean) => {
         $scope['bigScreen'] = big;
     });
@@ -80,7 +80,7 @@ myApp.controller('MediaController', ($scope: ng.IScope, $mdMedia: ng.material.MD
     $scope['anotherCustom'] = $mdMedia('max-width: 300px');
 });
 
-myApp.controller('SidenavController', ($scope: ng.IScope, $mdSidenav: ng.material.MDSidenavService) => {
+myApp.controller('SidenavController', ($scope: ng.IScope, $mdSidenav: ng.material.ISidenavService) => {
     var componentId = 'left';
     $scope['toggle'] = () => $mdSidenav(componentId).toggle();
     $scope['open'] = () => $mdSidenav(componentId).open();
@@ -89,6 +89,6 @@ myApp.controller('SidenavController', ($scope: ng.IScope, $mdSidenav: ng.materia
     $scope['isLockedOpen'] = $mdSidenav(componentId).isLockedOpen();
 });
 
-myApp.controller('ToastController', ($scope: ng.IScope, $mdToast: ng.material.MDToastService) => {
+myApp.controller('ToastController', ($scope: ng.IScope, $mdToast: ng.material.IToastService) => {
     $scope['openToast'] = () => $mdToast.show($mdToast.simple().content('Hello!'));
 });
