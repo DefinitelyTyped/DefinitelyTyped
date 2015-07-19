0bae9f2cb769947045340f4a3c6ba9a370365b7b
Watal M. Iwasaki <heavy.watal@gmail.com>
2015-07-19 22:24:02 +0900

    i18next: Modify 'callback' to take two arguments.

--- i18next/i18next.d.ts
+++ i18next/i18next.d.ts
@@ -85,8 +85,7 @@ interface I18nextStatic {
         toLanguages(language: string): string[];
         regexEscape(str: string): string;
     };
-    init(callback?: (t: (key: string, options?: any) => string) => void ): JQueryDeferred<any>;
-    init(options?: I18nextOptions, callback?: (t: (key: string, options?: any) => string) => void ): JQueryDeferred<any>;
+    init(callback?: (err: any, t: (key: string, options?: any) => string) => void ): JQueryDeferred<any>;
     init(options?: I18nextOptions, callback?: (err: any, t: (key: string, options?: any) => string) => void ): JQueryDeferred<any>;
     lng(): string;
     loadNamespace(namespace: string, callback?: () => void ): void;
@@ -101,10 +100,9 @@ interface I18nextStatic {
         rules: any;
         setCurrentLng: (language: string) => void;
     };
-    preload(language: string, callback?: (t: (key: string, options?: any) => string) => void ): void;
-    preload(languages: string[], callback?: (t: (key: string, options?: any) => string) => void ): void;
+    preload(language: string, callback?: (err: any, t: (key: string, options?: any) => string) => void ): void;
+    preload(languages: string[], callback?: (err: any, t: (key: string, options?: any) => string) => void ): void;
     setDefaultNamespace(namespace: string): void;
-    setLng(language: string, callback?: (t: (key: string, options?: any) => string) => void ): void;
     setLng(language: string, callback?: (err: any, t: (key: string, options?: any) => string) => void ): void;
     sync: {
         load: (languages: string[], options: I18nextOptions, callback: (err: Error, store: IResourceStore) => void ) => void;
