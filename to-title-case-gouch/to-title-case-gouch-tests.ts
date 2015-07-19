a265e2f023cbfa51d0cbf04581ca90cc5efd59cb
Sam Saint-Pettersen <s.stpettersen+github@gmail.com>
2015-07-19 14:42:18 +0100

    Type definitions and testsfor to-title-case-gouch

--- to-title-case-gouch/to-title-case-gouch-tests.ts
+++ to-title-case-gouch/to-title-case-gouch-tests.ts
@@ -3,7 +3,7 @@
 
 import fs = require('fs');
 
-fs.readFile('to-title-case.min.js', 'utf-8', function(err: any, code: string) {
+fs.readFile('to-title-case.js', 'utf-8', function(err: any, code: string) {
 	eval(code);
 	var lowerCase: string = 'this title is in lowercase and will be title case';
 	console.log(lowerCase.toTitleCase()); // Now in title case.
