ede30e8f4195bdb4836557e78f83e2b93d2f9d9e
Tingan Ho <tingan87@gmail.com>
2015-07-20 17:34:47 +0800

    Fixes file header issue

--- connect-modrewrite/connect-modrewrite.d.ts
+++ connect-modrewrite/connect-modrewrite.d.ts
@@ -1,5 +1,9 @@
+// Type definitions for connect-modrewrite
+// Project: https://github.com/tinganho/connect-modrewrite
+// Definitions by: Tingan Ho <https://github.com/tinganho/>
+// Definitions: https://github.com/borisyankov/DefinitelyTyped
 
-/// <reference path='../express/express.d.ts' />
+/// <reference path="../express/express.d.ts" />
 
 declare module 'connect-modrewrite' {
     import express = require('express');
