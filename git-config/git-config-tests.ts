3ac0cbdf5dcc527bcd78ef44a83b05a40ead4527
Sam Saint-Pettersen <s.stpettersen+github@gmail.com>
2015-07-20 11:22:39 +0100

    Type definitions and tests for git-config

--- git-config/git-config-tests.ts
+++ git-config/git-config-tests.ts
@@ -2,8 +2,8 @@
 
 import gitConfig = require('git-config');
 
-var config: Object = gitConfig.sync(); // => Object if .gitconfig exists.
+var config: Object = gitConfig.sync();
 console.log(JSON.stringify(config));
 
-config = gitConfig.sync('gitconfig'); // => Object as gitconfig definitely exists.
+config = gitConfig.sync('gitconfig');
 console.log(JSON.stringify(config));
