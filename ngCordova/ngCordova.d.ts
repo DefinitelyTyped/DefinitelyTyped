9f61417db9ec8457005ec9c658510028c8527920
Pascal Vantrepote <pvantrepote@me.com>
2015-07-22 15:22:01 -0400

    Fix build

--- ngCordova/ngCordova.d.ts
+++ ngCordova/ngCordova.d.ts
@@ -1,6 +1,6 @@
 // Type definitions for ngCordova
 // Project: http://ngcordova.com
-// Definitions by: Pascal Vantrepote 
+// Definitions by: Pascal Vantrepote <pvantrepote@gmail.com>
 // Definitions: https://github.com/borisyankov/DefinitelyTyped
 
 /// <reference path="./plugins/DatePicker.d.ts"/>
