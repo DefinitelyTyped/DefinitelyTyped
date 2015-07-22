81a4bc50050dde33ca77301ff518ab8757b62be0
Pascal Vantrepote <pvantrepote@me.com>
2015-07-22 11:50:07 -0400

    DatePicker and ngCordova date picker

--- cordova/cordova.d.ts
+++ cordova/cordova.d.ts
@@ -26,6 +26,7 @@
 /// <reference path="plugins/Vibration.d.ts"/>
 /// <reference path="plugins/WebSQL.d.ts"/>
 /// <reference path="plugins/AppPreferences.d.ts"/>
+/// <reference path="plugins/DatePicker.d.ts"/>
 
 interface Cordova {
     /** Invokes native functionality by specifying corresponding service name, action and optional parameters.
---
+++ ngCordova/ngCordova.d.ts
@@ -0,0 +1,11 @@
+// Type definitions for ngCordova
+// Project: http://ngcordova.com
+// Definitions by: Pascal Vantrepote
+// Definitions: https://github.com/borisyankov/DefinitelyTyped
+
+/// <reference path="./plugins/DatePicker.d.ts"/>
+
+declare module ngCordova {
+
+
+}
