8f2f9955ad225a8cff429147c2eecd075bb9dca1
Bostwickenator <bostwickenator@gmail.com>
2015-07-24 16:32:29 +1200

    Adda a test for .set
    Using the lodash example as a test case

--- lodash/lodash-tests.ts
+++ lodash/lodash-tests.ts
@@ -1068,6 +1068,9 @@ result = <HasName>_.pick({ 'name': 'moe', '_userid': 'moe1' }, function (value, 
     return key.charAt(0) != '_';
 });
 
+result = <any>_.set({ 'a': [{ 'b': { 'c': 3 } }] }, 'a[0].b.c', 4);
+// → 4
+
 result = <number[]>_.transform([1, 2, 3, 4, 5, 6, 7, 8, 9, 10], function (r: number[], num: number) {
     num *= num;
     if (num % 2) {
