a31b9e4f4a023fad0e2bb28c592cc711b9cd4298
Gabriel Garcia <garciat@live.com>
2015-07-24 21:06:31 -0400

    Complete Client API

--- gapi/gapi.client.d.ts
+++ gapi/gapi.client.d.ts
@@ -57,7 +57,29 @@ declare module gapi.auth {
             response_type?: string
         },
         callback: (token: OAuth2Token) => any
-        ): void;
+    ): void;
+    
+    /**
+     * Initializes the authorization feature.
+     * 
+     * Call this when the client loads to prevent popup blockers from blocking
+     * the auth window on gapi.auth.authorize calls.
+     * 
+     * Note: Calling gapi.auth.authorize({immediate: true}) when the client
+     * loads achieves the same effect. If your application does an
+     * immediate-mode check, gapi.auth.init should be omitted entirely.
+     */
+    export function init(callback: () => any): void;
+    
+    /**
+     * Retrieves the OAuth 2.0 token for the application.
+     */
+    export function getToken(): OAuth2Token;
+    
+    /**
+     * Sets the OAuth 2.0 token for the application.
+     */
+    export function setToken(token: OAuth2Token): void;
 }
 
 declare module gapi.client {
@@ -117,6 +139,48 @@ declare module gapi.client {
         locatin?: string;
     }
     
+    interface Batch {
+        add<T>(
+            request: HttpRequest<T>,
+            params?: {
+                /**
+                 * Identifies the response for this request in the map of batch responses.
+                 * If one is not provided, the system generates a random ID.
+                 */
+                id?: string,
+                
+                callback?: (
+                    /**
+                     * The response for this request only.
+                     */
+                    individualResponse: ApiResponse<T>,
+                    
+                    /**
+                     * The raw batch ID-response map as a string.
+                     * It contains all responses to all requests in the batch.
+                     */
+                    rawBatchResponse: string
+                ) => any;
+            }
+        ): void;
+        
+        execute(
+            callback: (
+                responseMap: { [requestId: string]: ApiResponse<any> },
+                
+                rawBatchResponse: string
+            ) => any
+        ): void;
+        
+        then<U>(
+            onFulfilled?: (result: { [requestId: string]: HttpResponse<any> }) => U,
+            
+            onRejected?: (error: HttpResponse<boolean>) => U,
+            
+            context?: any
+        ): void;
+    }
+    
     /**
      * Loads the client library interface to a particular API.
      * 
@@ -131,7 +195,13 @@ declare module gapi.client {
         /**
          * The version of the API to load.
          */
-        version: string
+        version: string,
+        
+        /**
+         * (optional) the function that is called once the API interface is loaded.
+         * If not provided, a promise is returned.
+         */
+        callback?: () => any
     ): { then(callback: () => any): void };
     
     /**
@@ -171,14 +241,42 @@ declare module gapi.client {
         /**
          * URL params in key-value pair form
          */
-        params?: any;
+        params?: { [key: string]: string },
         /**
          * Additional HTTP request headers
          */
-        headers?: any;
+        headers?: { [key: string]: string },
         /**
          * The HTTP request body (applies to PUT or POST).
          */
         body?: any;
     }): HttpRequest<any>;
+    
+    export function newBatch(args: {
+        /**
+         * The URL to handle the request
+         */
+        path: string;
+        /**
+         * The HTTP request method to use. Default is GET
+         */
+        method?: string;
+        /**
+         * URL params in key-value pair form
+         */
+        params?: { [key: string]: string },
+        /**
+         * Additional HTTP request headers
+         */
+        headers?: { [key: string]: string },
+        /**
+         * The HTTP request body (applies to PUT or POST).
+         */
+        body?: any;
+    }): Batch;
+    
+    /**
+     * Sets the API key for the application
+     */
+    export function setApiKey(apiKey: string): void;
 }
