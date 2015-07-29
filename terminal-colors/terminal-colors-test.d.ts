0a6ba714f1501c4666f629bd1eebf8b2c3cc174b
Tingan Ho <tingan87@gmail.com>
2015-07-29 22:09:17 +0800

    Adds info

--- terminal-colors/terminal-colors-test.d.ts
+++ terminal-colors/terminal-colors-test.d.ts
@@ -1,3 +1,7 @@
+// Type definitions for terminal-colors
+// Project: https://github.com/tinganho/terminal-colors
+// Definitions by: Tingan Ho <https://github.com/tinganho>
+// Definitions: https://github.com/borisyankov/DefinitelyTyped
 
 /// <reference path='./terminal-colors.d.ts' />
 
