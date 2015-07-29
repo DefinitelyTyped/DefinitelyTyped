4df47ba03d1c58d87936d6e304e5893c7ded0dfc
benishouga <benishouga@gmail.com>
2015-07-30 01:03:33 +0900

    add svg attribute width and height for react element.

--- react/react.d.ts
+++ react/react.d.ts
@@ -540,6 +540,7 @@ declare module __React {
         fy?: number | string;
         gradientTransform?: string;
         gradientUnits?: string;
+        height?: number | string;
         markerEnd?: string;
         markerMid?: string;
         markerStart?: string;
@@ -564,6 +565,7 @@ declare module __React {
         transform?: string;
         version?: string;
         viewBox?: string;
+        width?: number | string;
         x1?: number | string;
         x2?: number | string;
         x?: number | string;
