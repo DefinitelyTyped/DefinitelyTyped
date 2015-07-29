59fc164a3c5f470b9d97209085f785b5574b122c
Ilya Mochalov <chrootsu@gmail.com>
2015-07-29 08:16:51 +0500

    added a contributor name

--- lodash/lodash.d.ts
+++ lodash/lodash.d.ts
@@ -1,6 +1,6 @@
 // Type definitions for Lo-Dash
 // Project: http://lodash.com/
-// Definitions by: Brian Zengel <https://github.com/bczengel>
+// Definitions by: Brian Zengel <https://github.com/bczengel>, Ilya Mochalov <https://github.com/chrootsu>
 // Definitions: https://github.com/borisyankov/DefinitelyTyped
 
 declare var _: _.LoDashStatic;
