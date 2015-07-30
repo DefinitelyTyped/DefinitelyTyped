f9694258d2d2923c4c185a7c8a64df1ce8b4baa8
Lokesh Peta <lokesh.peta@live.com>
2015-07-30 09:54:07 +0100

    rename to pathjs

--- pathjs/pathjs.d.ts
+++ pathjs/pathjs.d.ts
@@ -1,5 +1,5 @@
 // Type definitions for Pathjs v0.8.4
-// Project: https://github.com/mtrpcic/pathjs/blob/master/path.js
+// Project: https://github.com/mtrpcic/pathjs
 // Definitions by: Lokesh Peta <https://github.com/lokeshpeta/>
 // Definitions: https://github.com/borisyankov/DefinitelyTyped
 
