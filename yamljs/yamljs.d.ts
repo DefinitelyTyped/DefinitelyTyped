6630d61a58334055442c26372109702067792022
Jason Saelhof <jason@deepmarkit.com>
2015-07-31 14:46:55 -0600

    Update definitions and tests

--- yamljs/yamljs.d.ts
+++ yamljs/yamljs.d.ts
@@ -1,9 +1,9 @@
-// Type definitions for yamljs 0.2.1
+// Type definitions for yamljs 0.2.3
 // Project: https://github.com/jeremyfa/yaml.js
 // Definitions by: Tim Jonischkat <http://www.tim-jonischkat.de>
 // Definitions: https://github.com/borisyankov/DefinitelyTyped
 
-declare module "yamljs" {
+declare module YAML {
 
     export function load(path : string) : any;
 
