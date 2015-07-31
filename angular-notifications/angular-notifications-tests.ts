a9c43e3109805f506d1d60ab99aac6eb7c18f84b
Tomasz Ducin <tomasz.ducin@gmail.com>
2015-07-31 09:40:07 +0200

    typo fix

--- angular-notifications/angular-notifications-tests.ts
+++ angular-notifications/angular-notifications-tests.ts
@@ -3,7 +3,7 @@
 var myapp = angular.module("myapp", ["notifications"]);
 
 myapp.controller("MyController", ["$scope", "notifications",
-    function ($scope:ng.IScope, $notifications:angular.notifications.INotificationFactory) { // <-- Inject notifications
+    function ($scope:ng.IScope, notifications:angular.notifications.INotificationFactory) { // <-- Inject notifications
 
         var userData = {'some': 'data', 'optional': true};
         $notification.info("Something happened", "here is the content of what happened", userData);
