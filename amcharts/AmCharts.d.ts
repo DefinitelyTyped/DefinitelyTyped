583b8ce4f06c18996b261b570fa84cf048a2c01f
Peter Albert <peter.albert@data2impact.com>
2015-08-02 14:39:03 +0200

    Updated AmChart class

--- amcharts/AmCharts.d.ts
+++ amcharts/AmCharts.d.ts
@@ -903,7 +903,13 @@ If you do not set properties such as dashLength, lineAlpha, lineColor, etc - val
     	constructor(theme?: any);
     	/** Specifies, if class names should be added to chart elements. */
         addClassNames: boolean
-        
+        /** Array of Labels. Example of label object, with all possible properties:
+{"x": 20, "y": 20, "text": "this is label", "align": "left", "size": 12, "color": "#CC0000", "alpha": 1, "rotation": 0, "bold": true, "url": "http://www.amcharts.com"} */
+        allLabels: Label[];
+        /**  Set this to false if you don't want chart to resize itself whenever its parent container size changes. */
+        autoResize: boolean;
+        /** Opacity of background. Set it to >0 value if you want backgroundColor to work. However we recommend changing div's background-color style for changing background color. */
+        backgroundAlpha: number;
         /** Background color. You should set backgroundAlpha to >0 value in order background to be visible. We recommend setting background color directly on a chart's DIV instead of using this property. #FFFFFF */
         backgroundColor: string;
         /** The chart creates AmBalloon class itself. If you want to customize balloon, get balloon instance using this property, and then change balloon's properties.  AmBalloon */
@@ -912,32 +918,79 @@ If you do not set properties such as dashLength, lineAlpha, lineColor, etc - val
         borderAlpha: number;
         /** Color of chart's border. You should set borderAlpha >0 in order border to be visible. We recommend setting border color directly on a chart's DIV instead of using this property. #000000 */
         borderColor: string;
+        /** This prefix is added to all class names which are added to all visual elements of a chart in case addClassNames is set to true. */
+        classNamePrefix: string
         /** Text color. #000000 */
         color: string;
         /** Array of data objects, for example: [{country:"US", value:524},{country:"UK", value:624},{country:"Lithuania", value:824}]. You can have any number of fields and use any field names. In case of AmMap, data provider should be MapData object. */
         dataProvider: any[];
+        /** Decimal separator.
+            @Default . */
+        decimalSeparator: string;
+        /** Using this property you can add any additional information to SVG, like SVG filters or clip paths. The structure of this object should be identical to XML structure of a object you are adding, only in JSON format. */
+        defs: any;
+        /** Export config. Specifies how export to image/data export/print/annotate menu will look and behave. You can find a lot of examples in amcharts/plugins/export folder. */
+        export: ExportSettings;
         /** Font family. Verdana */
         fontFamily: string;
         /** Font size.
             @default 11
         */
         fontSize: number;
-        /** Height of a chart. "100%" means the chart's height will be equal to it's container's (DIV) height and will resize if height of the container changes. Set a number instead of percents if your chart's size needs to be fixed.
-            @default 1
+        /** If you set this to true, the lines of the chart will be distorted and will produce hand-drawn effect. Try to adjust chart.handDrawScatter and chart.handDrawThickness properties for a more scattered result. 
+            @Default false
         */
-        height: any;
+        handDrawn: boolean;
+        /** Defines by how many pixels hand-drawn line (when handDrawn is set to true) will fluctuate.
+            @Default 2
+        */
+        handDrawScatter: number;
+        /** Defines by how many pixels line thickness will fluctuate (when handDrawn is set to true).
+            @Default 1
+        */
+        handDrawThickness: number;
+        /** Time, in milliseconds after which balloon is hidden if the user rolls-out of the object. Might be useful for AmMap to avoid balloon flickering while moving mouse over the areas. Note, this is not duration of fade-out. Duration of fade-out is set in AmBalloon class. 
+            @Default 150
+        */
+        hideBalloonTime: number;
+        /** Legend of a chart. */
+        legend: AmLegend;
         /** Reference to the div of the legend. */
         legendDiv: HTMLElement;
-        /** Object with precision, decimalSeparator and thousandsSeparator set which will be used for number formatting. Precision set to -1 means that values won't be rounded.  {precision:-1, decimalSeparator:'.', thousandsSeparator:','} */
-        numberFormatter: Object;
+        /** You can add listeners of events using this property. Example: listeners = [{"event":"dataUpdated", "method":handleEvent}]; */
+        listerns: Object[];
         /** This setting affects touch-screen devices only. If a chart is on a page, and panEventsEnabled are set to true, the page won't move if the user touches the chart first. If a chart is big enough and occupies all the screen of your touch device, the user won’t be able to move the page at all. That's why the default value is "false". If you think that selecting/panning the chart or moving/pinching the map is a primary purpose of your users, you should set panEventsEnabled to true. */
         panEventsEnabled: boolean;
-        /** Object with precision, decimalSeparator and thousandsSeparator set which will be used for formatting percent values. {precision:2, decimalSeparator:'.', thousandsSeparator:','} */
-        percentFormatter: Object;
+        /**  Specifies absolute or relative path to amCharts files, i.e. "amcharts/". (where all .js files are located)
+            If relative URLs are used, they will be relative to the current web page, displaying the chart.            
+            You can also set path globally, using global JavaScript variable AmCharts_path. If this variable is set, and "path" is not set in chart config, the chart will assume the path from the global variable. This allows setting amCharts path globally. I.e.:            
+            var AmCharts_path = "/libs/amcharts/";
+            "path" parameter will be used by the charts to locate it's files, like images, plugins or patterns.*/
+        path: string;     
+        /** Specifies path to the folder where images like resize grips, lens and similar are.
+            IMPORTANT: Since V3.14.12, you should use "path" to point to amCharts directory instead. The "pathToImages" will be automatically set and does not need to be in the chart config, unless you keep your images separately from other amCharts files. */
+        pathToImages: string;
+        /** Precision of percent values. -1 means percent values won't be rounded at all and show as they are. 
+            @default 2
+        */
+        percentPrecision: number;
+        /** Precision of values. -1 means values won't be rounded at all and show as they are. 
+            @Default 1*/
+        precision: number;
         /** Prefixes which are used to make big numbers shorter: 2M instead of 2000000, etc. Prefixes are used on value axes and in the legend. To enable prefixes, set usePrefixes property to true. [{number:1e+3,prefix:"k"},{number:1e+6,prefix:"M"},{number:1e+9,prefix:"G"},{number:1e+12,prefix:"T"},{number:1e+15,prefix:"P"},{number:1e+18,prefix:"E"},{number:1e+21,prefix:"Z"},{number:1e+24,prefix:"Y"}] */
         prefixesOfBigNumbers: any[];
         /** Prefixes which are used to make small numbers shorter: 2μ instead of 0.000002, etc. Prefixes are used on value axes and in the legend. To enable prefixes, set usePrefixes property to true. [{number:1e-24, prefix:"y"},{number:1e-21, prefix:"z"},{number:1e-18, prefix:"a"},{number:1e-15, prefix:"f"},{number:1e-12, prefix:"p"},{number:1e-9, prefix:"n"},{number:1e-6, prefix:"μ"},{number:1e-3, prefix:"m"}] */
         prefixesOfSmallNumbers: any[];
+        /** Theme of a chart. Config files of themes can be found in amcharts/themes/ folder. More info about using themes. */
+        theme: string;
+        /** Thousands separator. 
+            @default .
+        */
+        thousandsSeparator: string;
+        /** Array of Title objects. */
+        titles: Title[];
+        /** Type of a chart. Required when creating chart using JSON. Possible types are: serial, pie, xy, radar, funnel, gauge, map, stock. */
+        type: string;
         /** If true, prefixes will be used for big and small numbers. You can set arrays of prefixes via prefixesOfSmallNumbers and prefixesOfBigNumbers properties. */
         usePrefixes: boolean;
         /** Read-only. Indicates current version of a script. */
@@ -1714,9 +1767,9 @@ If you do not set properties such as dashLength, lineAlpha, lineColor, etc - val
 
     /** Creates a label on the chart which can be placed anywhere, multiple can be assigned. */
     class Label {
-        /** Default: 'left' */
+        /** @Default 'left' */
         align: string;
-        /** Default: 1 */
+        /** @Default 1 */
         alpha: number;
         /** Specifies if label is bold or not. */
         bold: boolean;
