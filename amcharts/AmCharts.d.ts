b5af68fdda7acb474f3a456e1bd52313a0405643
Peter Albert <peter.albert@data2impact.com>
2015-08-02 14:38:45 +0200

    Added title class

--- amcharts/AmCharts.d.ts
+++ amcharts/AmCharts.d.ts
@@ -2423,4 +2423,25 @@ Your function should return string.*/
         /** Removes event listener from chart object. */
         removeListener(chart: AmChart, type: string, handler: any);
     }
+    
+    class Title {
+        /** @default 1 */
+        alpha: number;
+        /** Specifies if the tile is bold or not. 
+            @default false*/
+        bold: boolean;
+        /** Text color of a title. */
+        color: string; 
+        /** Unique id of a Title. You don't need to set it, unless you want to. */
+        id: string;
+        /** Text size */
+        size: number;
+        /** Text of a label */
+        text: string;
+    }
+    class ExportSettings {
+        enabled: boolean;
+        libs: Object;
+        menu: Object;
+    }
 }
