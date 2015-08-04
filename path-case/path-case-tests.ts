471c254078813643aad9c278c58121eccf58df66
Sam Saint-Pettersen <s.stpettersen+github@gmail.com>
2015-08-04 14:05:44 +0100

    Type definitions and tests for path-case

--- path-case/path-case-tests.ts
+++ path-case/path-case-tests.ts
@@ -6,4 +6,4 @@ console.log(pathCase('string')); 	 // => "string"
 console.log(pathCase('camelCase'));	 // => "camel/case"
 console.log(pathCase('sentence case'));  // => "sentence/case"
 
-console.log(pathCase('MY STRING', 'tr')); // => "my.strıng" 
+console.log(pathCase('MY STRING', 'tr')); // => "my.strıng"
