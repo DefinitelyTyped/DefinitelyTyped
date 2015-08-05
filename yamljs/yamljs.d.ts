84dc21b73b9a04f6d90806aee64c3fa9d541ff8f
Jason Saelhof <jason@deepmarkit.com>
2015-08-05 15:04:49 -0600

    Update definitions according to the pattern provided by Masahiro Wakame in order to work with nodejs as well

--- yamljs/yamljs.d.ts
+++ yamljs/yamljs.d.ts
@@ -3,12 +3,14 @@
 // Definitions by: Tim Jonischkat <http://www.tim-jonischkat.de>
 // Definitions: https://github.com/borisyankov/DefinitelyTyped
 
-declare module YAML {
+declare var YAML: {
+    load(path : string) : any;
 
-    export function load(path : string) : any;
+    stringify(nativeObject : any, inline? : number, spaces? : number) : string;
 
-    export function stringify(nativeObject : any, inline? : number, spaces? : number) : string;
+    parse(yamlString : string) : any;
+};
 
-    export function parse(yamlString : string) : any;
-
+declare module "yamljs" {
+    export = YAML;
 }
