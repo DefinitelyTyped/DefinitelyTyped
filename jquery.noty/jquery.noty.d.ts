7d007af8ad51c9fb93c904688fbb5913d4cf7d47
Masataka Kuwabara <p.ck.t22@gmail.com>
2015-08-06 11:28:14 +0900

    Update jquery.noty

--- jquery.noty/jquery.noty.d.ts
+++ jquery.noty/jquery.noty.d.ts
@@ -11,16 +11,16 @@ interface NotyOptions {
     theme?: string;
     type?: string;
     /** Text to show. Can be html or string. */
-    text?: string; 
+    text?: string;
     /** If you want to use queue feature set this true. */
-    dismissQueue?: boolean; 
+    dismissQueue?: boolean;
     /** The note`s optional template like '<div class="noty_message"><span class="noty_text"></span><div class="noty_close"></div></div>' */
-    template?: string; 
+    template?: string;
     animation?: NotyAnimationOptions;
     /** Delay for closing event. Set false for sticky notifications */
-    timeout?: any; 
+    timeout?: any;
     /** Adds notification to the beginning of queue when set to true */
-    force?: boolean; 
+    force?: boolean;
     modal?: boolean;
     /** You can set max visible notification for dismissQueue true option */
     maxVisible?: number;
@@ -33,50 +33,47 @@ interface NotyOptions {
 }
 
 interface NotyAnimationOptions {
-	open?: any;
-	close?: any;
-	easing?: string;
-	speed?: number;
+    open?: any;
+    close?: any;
+    easing?: string;
+    speed?: number;
 }
 
 interface NotyCallbackOptions {
-	onShow?: Function;
-	afterShow?: Function;
-	onClose?: Function;
-	afterClose?: Function;
+    onShow?: Function;
+    afterShow?: Function;
+    onClose?: Function;
+    afterClose?: Function;
 }
 
 interface NotyStatic {
-	(notyOptions: NotyOptions);
-	defaults: NotyOptions;
+    defaults: NotyOptions;
 
-	get(id: any);
-	close(id: any);
-	clearQueue();
-	closeAll();
-	setText(id: any, text: string);
-	setType(id: any, type: string);
+    get(id: string): Noty|boolean;
+    close(id: string): void|boolean;
+    clearQueue(): void;
+    closeAll(): void;
+    setText(id: string, text: string): Noty|boolean;
+    setType(id: string, type: string): Noty|boolean;
 }
 
 interface Noty {
-    (notyOptions: NotyOptions);
-
-    show();
-    close();
-    setText(text: string);
-    setType(type: string);
-    setTimeout(timeout: number);
+    show(): Noty;
+    close(): void;
+    setText(text: string): Noty;
+    setType(type: string): Noty;
+    setTimeout(timeout: number): Noty;
 
     closed: boolean;
     shown: boolean;
 }
 
 interface JQueryStatic {
-	noty: NotyStatic;
+    noty: NotyStatic;
 }
 
 interface JQuery {
     noty: Noty;
 }
 
-declare var noty: Noty;
+declare var noty: (options: NotyOptions) => Noty;
