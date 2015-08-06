01b91ca6a175761d2c9cf2eaefa99377a9a38bfa
Osman Nuri Okumuş <onokumus@gmail.com>
2015-08-06 12:28:42 +0300

    Create metismenu.d.ts  On branch master  Your branch is up-to-date with 'origin/master'.
     Changes to be committed:
    	new file:   metismenu/metismenu-tests.ts
    	new file:   metismenu/metismenu.d.ts

---
+++ metismenu/metismenu.d.ts
@@ -0,0 +1,4 @@
+// Type definitions for metisMenu 2.0.3// Project: http://github.com/onokumus/metisMenu/// Definitions by: Osman Nuri Okumuş <https://github.com/onokumus/>// Definitions: https://github.com/borisyankov/DefinitelyTyped
+/// <reference path="../jquery/jquery.d.ts"/>
+interface MetisMenuOptions {  toggle?: boolean;  doubleTapToGo?: boolean;  activeClass?: string;  collapseClass?: string;  collapseInClass?: string;  collapsingClass?: string;}
+interface JQuery {  metisMenu(options?: MetisMenuOptions): JQuery;}
