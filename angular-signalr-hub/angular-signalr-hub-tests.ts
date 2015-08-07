fc6dace9584b7ce97cca3fbbd1124306025570b2
Adam Santaniello <arsantaniello@gmail.com>
2015-08-07 11:17:18 -0400

    Updated to 4-space indentation per contribution guidelines

--- angular-signalr-hub/angular-signalr-hub-tests.ts
+++ angular-signalr-hub/angular-signalr-hub-tests.ts
@@ -2,76 +2,76 @@
 /// <reference path='../angularjs/angular.d.ts' />
 
 angular
-  .module('app', ['SignalR'])
-  .factory('Employees', ngSignalrTest.EmployeesFactory);
+    .module('app', ['SignalR'])
+    .factory('Employees', ngSignalrTest.EmployeesFactory);
 
 module ngSignalrTest {
-  export class EmployeesFactory {
-    static $inject = ['$rootScope', 'Hub', '$timeout'];
-    private hub: ngSignalr.Hub;
-    public all: Array<Employee>;
-    
-    constructor($rootScope: ng.IRootScopeService, Hub: ngSignalr.HubFactory, $timeout: ng.ITimeoutService) {
-      // declaring the hub connection
-      this.hub = new Hub('employee', {
-        // client-side methods
-        listeners: {
-          'lockEmployee': (id: number) => {
-            var employee = this.find(id);
-            employee.Locked = true;
-            $rootScope.$apply();
-          },
-          'unlockEmployee': (id: number) => {
-            var employee = this.find(id);
-            employee.Locked = false;
-            $rootScope.$apply();
-          }
-        },
+    export class EmployeesFactory {
+        static $inject = ['$rootScope', 'Hub', '$timeout'];
+        private hub: ngSignalr.Hub;
+        public all: Array<Employee>;
         
-        // server-side methods
-        methods: ['lock', 'unlock'],
-        
-        // query params sent on initial connection
-        queryParams:{
-            'token': 'exampletoken'
-        },
-        
-        // handle connection error
-        errorHandler: (message: string) => {
-          console.error(message);
-        },
-        
-        stateChanged: (state: SignalRStateChange) => {
-          // your code here
+        constructor($rootScope: ng.IRootScopeService, Hub: ngSignalr.HubFactory, $timeout: ng.ITimeoutService) {
+            // declaring the hub connection
+            this.hub = new Hub('employee', {
+                // client-side methods
+                listeners: {
+                    'lockEmployee': (id: number) => {
+                        var employee = this.find(id);
+                        employee.Locked = true;
+                        $rootScope.$apply();
+                    },
+                    'unlockEmployee': (id: number) => {
+                        var employee = this.find(id);
+                        employee.Locked = false;
+                        $rootScope.$apply();
+                    }
+                },
+                
+                // server-side methods
+                methods: ['lock', 'unlock'],
+                
+                // query params sent on initial connection
+                queryParams:{
+                        'token': 'exampletoken'
+                },
+                
+                // handle connection error
+                errorHandler: (message: string) => {
+                    console.error(message);
+                },
+                
+                stateChanged: (state: SignalRStateChange) => {
+                    // your code here
+                }
+            });
         }
-      });
+        
+        private find(id: number) {
+            for (var i = 0; i < this.all.length; i++) {
+                if (this.all[i].Id === id) return this.all[i];
+            }
+            
+            return null;
+        }
+        
+        public edit = (employee: Employee) => {
+            employee.Edit = true;
+            this.hub.invoke('lock', employee.Id);
+        };
+        
+        public done = (employee: Employee) => {
+            employee.Edit = false;
+            this.hub.invoke('unlock', employee.Id);
+        }
     }
     
-    private find(id: number) {
-      for (var i = 0; i < this.all.length; i++) {
-        if (this.all[i].Id === id) return this.all[i];
-      }
-      
-      return null;
+    interface Employee {
+        Id: number;
+        Name: string;
+        Email: string;
+        Salary: number;
+        Edit: boolean;
+        Locked: boolean;
     }
-    
-    public edit = (employee: Employee) => {
-      employee.Edit = true;
-      this.hub.invoke('lock', employee.Id);
-    };
-    
-    public done = (employee: Employee) => {
-      employee.Edit = false;
-      this.hub.invoke('unlock', employee.Id);
-    }
-  }
-  
-  interface Employee {
-    Id: number;
-    Name: string;
-    Email: string;
-    Salary: number;
-    Edit: boolean;
-    Locked: boolean;
-  }
 }
