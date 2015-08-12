6e63c46f083f2f190d7e991679c3d73f78a44220
Stepan Mikhaylyuk <stepancar@hotmail.com>
2015-08-12 19:59:07 +0300

    tests

--- react-swf/react-swf-tests.ts
+++ react-swf/react-swf-tests.ts
@@ -1,1 +1,8 @@
-import * as ReactSWF from './react-swf';
+/// <reference path="./react-swf.d.ts"/>
+/// <reference path="../react/react.d.ts"/>
+var version = ReactSWF.getFPVersion();
+var isFPVersionSupported = ReactSWF.isFPVersionSupported('5');
+var reactSWF = new ReactSWF();
+reactSWF.props = {
+  src:'',pluginspage:'',width:20,height:20
+}
