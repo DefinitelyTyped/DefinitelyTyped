1bfcc68bb65f58c80256f7db2d7467a67d050ca0
Cyril Schumacher <cyril_schumacher@live.fr>
2015-08-13 22:31:57 +0200

    Add definition for "node-schedule". Update definition for "maildev".

---
+++ node-schedule/node-schedule.d.ts
@@ -0,0 +1,207 @@
+// Type definitions for node-schedule
+// Project: https://github.com/tejasmanohar/node-schedule/
+// Definitions by: Cyril Schumacher <https://github.com/cyrilschumacher>
+// Definitions: https://github.com/borisyankov/DefinitelyTyped
+
+declare module "node-schedule" {
+    import nodeSchedule = require('node-schedule');
+
+    /**
+     * Recurrence rules.
+     */
+    export interface RecurrenceRule {
+        /**
+         * Day of the month.
+         *
+         * @public
+         * @type {number}
+         */
+        date?: number;
+
+        /**
+         * Day of the week.
+         *
+         * @public
+         * @type {number|Array<number|Range>}
+         */
+        dayOfWeek?: number|Array<number|Range>;
+
+        /**
+         * Hour.
+         *
+         * @public
+         * @type {number}
+         */
+        hour?: number;
+
+        /**
+         * Minute.
+         *
+         * @public
+         * @type {number}
+         */
+        minute?: number;
+
+        /**
+         * Month.
+         *
+         * @public
+         * @type {number}
+         */
+        month?: number;
+
+        /**
+         * Second.
+         *
+         * @public
+         * @type {number}
+         */
+        second?: number;
+
+        /**
+         * Year.
+         *
+         * @public
+         * @type {number}
+         */
+        year?: number;
+    }
+
+    /**
+     * Range.
+     *
+     * @class
+     */
+    export class Range {
+        /**
+         * Constructor.
+         *
+         * @constructor
+         * @param {number} start The start.
+         * @param {end}    end   The end.
+         * @param {step}   step  The step.
+         */
+        constructor(start?: number, end?: number, step?: number);
+
+        /**
+         * Return a {boolean} if the class contains the specified value.
+         *
+         * @param {number} value The value.
+         * @returns {boolean} {true} if the class contains the specified value, otherwise, {false}.
+         */
+        contains(value: number): boolean;
+    }
+
+    /**
+     * Scheduler job.
+     *
+     * @class
+     */
+    export class ScheduleJob {
+        /**
+         * Constructor.
+         *
+         * @constructor
+         * @param {RecurrenceRule} rule The rule.
+         * @param
+         */
+        constructor(rule: RecurrenceRule|Date|string, callback: Function);
+    }
+
+    /**
+     * Scheduler jobs.
+     *
+     * @class
+     */
+    export class Job {
+        /**
+         * Constructor.
+         *
+         * @constructor
+         * @param {RecurrenceRule} rule The rule.
+         * @param
+         */
+        constructor(name?: string, job?: Function, callback?: Function);
+
+        /**
+         * Constructor.
+         *
+         * @constructor
+         * @param {RecurrenceRule} rule The rule.
+         * @param
+         */
+        constructor(job?: Function, callback?: Function);
+
+        /**
+         * Attach an event handler function.
+         *
+         * @public
+         * @param {string}    eventName The event name.
+         * @param {Function}  handler   The function to execute when the event is triggered.
+         */
+        on(eventName: string, handler: Function): void;
+
+        /**
+         *
+         * @public
+         */
+        schedule(date: Date): void;
+    }
+
+    /**
+     * Invocation.
+     *
+     * @class
+     */
+    export class Invocation {
+        /**
+         * Fire date.
+         *
+         * @public
+         * @type {Date}
+         */
+        public fireDate: Date;
+
+        /**
+         * Job.
+         *
+         * @public
+         * @type {Job}
+         */
+        public job: Job;
+
+        /**
+         * Recurrence rule.
+         *
+         * @public
+         * @type {RecurrenceRule}
+         */
+        public recurrenceRule: RecurrenceRule;
+
+        /**
+         * Timer identifier.
+         *
+         * @public
+         * @type {number}
+         */
+        public timerID: number;
+
+        /**
+         * Constructor.
+         *
+         * @constructor
+         * @param job             The job.
+         * @param fireDate        The fire date.
+         * @param recurrenceRule  The recurrence rule.
+         */
+        constructor(job: Job, fireDate: Date, recurrenceRule: RecurrenceRule);
+    }
+
+    /**
+     * Cancels the job.
+     *
+     * @param job The job.
+     * @returns {boolean} {true} if the job has been cancelled with success, otherwise, {false}.
+     */
+    export function cancelJob(job: Job): boolean;
+}
