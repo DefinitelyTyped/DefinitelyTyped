b0f98e04f27ace98d211e1106079e1fb5080a33a
Ilya Mochalov <chrootsu@gmail.com>
2015-08-14 01:49:30 +0500

    lodash: added _.inRange() method

--- lodash/lodash.d.ts
+++ lodash/lodash.d.ts
@@ -5974,9 +5974,44 @@ declare module _ {
         add(addend: number): number;
     }
 
-    /*************
-     * Objects   *
-     *************/
+    /**********
+     * Number *
+     **********/
+
+    //_.inRange
+    interface LoDashStatic {
+        /**
+         * Checks if n is between start and up to but not including, end. If end is not specified it’s set to start
+         * with start then set to 0.
+         * @param n The number to check.
+         * @param start The start of the range.
+         * @param end The end of the range.
+         * @return Returns true if n is in the range, else false.
+         */
+        inRange(n: number, start: number, end: number): boolean;
+
+
+        /**
+         * @see _.inRange
+         */
+        inRange(n: number, end: number): boolean;
+    }
+
+    interface LoDashWrapper<T> {
+        /**
+         * @see _.inRange
+         */
+        inRange(start: number, end: number): boolean;
+
+        /**
+         * @see _.inRange
+         */
+        inRange(end: number): boolean;
+    }
+
+    /**********
+     * Object *
+     **********/
 
     //_.assign
     interface LoDashStatic {
