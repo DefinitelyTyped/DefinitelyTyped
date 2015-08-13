6780badbce8ba44b8845733d52bb78f239fe855b
vvakame <vvakame+dev@gmail.com>
2015-08-13 23:32:42 +0900

    refactor yamljs/yamljs.d.ts

--- yamljs/yamljs.d.ts
+++ yamljs/yamljs.d.ts
@@ -3,13 +3,13 @@
 // Definitions by: Tim Jonischkat <http://www.tim-jonischkat.de>
 // Definitions: https://github.com/borisyankov/DefinitelyTyped
 
-declare var YAML: {
-    load(path : string) : any;
+declare namespace YAML {
+    function load(path : string) : any;
 
-    stringify(nativeObject : any, inline? : number, spaces? : number) : string;
+    function stringify(nativeObject : any, inline? : number, spaces? : number) : string;
 
-    parse(yamlString : string) : any;
-};
+    function parse(yamlString : string) : any;
+}
 
 declare module "yamljs" {
     export = YAML;
