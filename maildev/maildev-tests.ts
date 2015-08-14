22a436bafc1f165f16f77d0197a255447e14f500
Cyril Schumacher <cyril_schumacher@live.fr>
2015-08-14 19:33:40 +0200

    Update definition for "maildev".

--- maildev/maildev-tests.ts
+++ maildev/maildev-tests.ts
@@ -5,8 +5,9 @@ import MailDev = require("maildev")
 var options: MailDevOptions = {smtp: 1025};
 var maildev = new MailDev(options);
 
-var errorCallback = (error: Error) => {};
+var errorCallback = (error: Error): void => {};
 maildev.listen(errorCallback);
 
 var eventName = 'new';
-maildev.on(eventName, errorCallback)
+var emailCallback = (email: Object): void => {};
+maildev.on(eventName, emailCallback)
