c9f3dff588e2804c2e24bb6c425e4d2235a143c1
Theodor Esenwein <theo@esenwein.ch>
2015-08-14 10:07:57 +0200

    Missing semicolon

--- mobile-detect/mobile-detect.d.ts
+++ mobile-detect/mobile-detect.d.ts
@@ -8,7 +8,7 @@ declare class MobileDetect {
 	constructor(userAgent: string, maxPhoneWidth?: number);
 	
 	is(key: string): boolean;
-	match(pattern: string|RegExp): boolean
+	match(pattern: string|RegExp): boolean;
 	mobile(): string;
 	mobileGrade(): string;
 	os(): string;
