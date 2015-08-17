a8cceb1d46a2eb036c766c7dedd4d2fc8777a0f1
Daniel Rosenwasser <drosen@microsoft.com>
2015-08-17 15:04:54 -0700

    Made tests pass for 'jqueryui'.

--- jqueryui/jqueryui-tests.ts
+++ jqueryui/jqueryui-tests.ts
@@ -1642,11 +1642,11 @@ function test_tabs() {
     });
     $("#tabs").tabs({
         beforeLoad: function (event, ui) {
-            ui.jqXHR.error(function () {
+            ui.jqXHR.error = function () {
                 ui.panel.html(
                     "Couldn't load this tab. We'll try to fix this as soon as possible. " +
                     "If this wouldn't be a demo.");
-            });
+            };
         }
     });
     $("#tabs").tabs({
