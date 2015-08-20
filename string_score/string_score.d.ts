e06191fa16e0a27c8744723eea2088f7aab703ae
Marcin Porebski <porebski.m@gmail.com>
2015-08-20 14:17:47 +0200

    typo fix

--- string_score/string_score.d.ts
+++ string_score/string_score.d.ts
@@ -1,4 +1,4 @@
-// Type definitions for url-template 0.1.22
+// Type definitions for string_score 0.1.22
 // Project: https://github.com/joshaven/string_score
 // Definitions by: Marcin Porębski <https://github.com/marcinporebski/>
 // Definitions: https://github.com/borisyankov/DefinitelyTyped
