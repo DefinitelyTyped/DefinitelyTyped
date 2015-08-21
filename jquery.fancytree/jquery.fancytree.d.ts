5dfb8d9e90e019cdc712efce11ef4f421697372c
Daniel Rosenwasser <drosen@microsoft.com>
2015-08-21 15:09:20 -0700

    Reorder overloads for better error experience in 'jquery.fancytree'.

--- jquery.fancytree/jquery.fancytree.d.ts
+++ jquery.fancytree/jquery.fancytree.d.ts
@@ -218,30 +218,6 @@ declare module Fancytree {
 
         //#region Methods
         /**
-         * Append (or insert) a single child node. 
-         * 
-         * @param child node to add
-         * @param insertBefore child node to insert this node before. If omitted, the new child is appended.
-         * @returns The child added.
-         */
-        addChildren(child: Fancytree.NodeData, insertBefore?: FancytreeNode): FancytreeNode;
-        /**
-         * Append (or insert) a single child node. 
-         * 
-         * @param child node to add
-         * @param insertBefore key of the child node to insert this node before. If omitted, the new child is appended.
-         * @returns The child added.
-         */
-        addChildren(child: Fancytree.NodeData, insertBefore?: string): FancytreeNode;
-        /**
-         * Append (or insert) a single child node. 
-         * 
-         * @param child node to add
-         * @param insertBefore index of the child node to insert this node before. If omitted, the new child is appended.
-         * @returns The child added.
-         */
-        addChildren(child: Fancytree.NodeData, insertBefore?: number): FancytreeNode;
-        /**
          * Append (or insert) a list of child nodes. 
          * 
          * @param children array of child node definitions (also single child accepted)
@@ -265,6 +241,30 @@ declare module Fancytree {
          * @returns The first child added.
          */
         addChildren(children: Fancytree.NodeData[], insertBefore?: number): FancytreeNode;
+        /**
+         * Append (or insert) a single child node. 
+         * 
+         * @param child node to add
+         * @param insertBefore child node to insert this node before. If omitted, the new child is appended.
+         * @returns The child added.
+         */
+        addChildren(child: Fancytree.NodeData, insertBefore?: FancytreeNode): FancytreeNode;
+        /**
+         * Append (or insert) a single child node. 
+         * 
+         * @param child node to add
+         * @param insertBefore key of the child node to insert this node before. If omitted, the new child is appended.
+         * @returns The child added.
+         */
+        addChildren(child: Fancytree.NodeData, insertBefore?: string): FancytreeNode;
+        /**
+         * Append (or insert) a single child node. 
+         * 
+         * @param child node to add
+         * @param insertBefore index of the child node to insert this node before. If omitted, the new child is appended.
+         * @returns The child added.
+         */
+        addChildren(child: Fancytree.NodeData, insertBefore?: number): FancytreeNode;
 
 
         /** Append or prepend a node, or append a child node. This a convenience function that calls addChildren()  
