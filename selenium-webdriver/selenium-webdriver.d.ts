7ff45ed3e3aa9a01f87461d631b4578139cde287
Kuniwak <yuki.kokubun@mixi.co.jp>
2015-08-22 00:27:35 +0900

    Add my name

--- selenium-webdriver/selenium-webdriver.d.ts
+++ selenium-webdriver/selenium-webdriver.d.ts
@@ -1,6 +1,6 @@
 // Type definitions for Selenium WebDriverJS 2.44.0
 // Project: https://code.google.com/p/selenium/
-// Definitions by: Bill Armstrong <https://github.com/BillArmstrong>
+// Definitions by: Bill Armstrong <https://github.com/BillArmstrong>, Yuki Kokubun <https://github.com/Kuniwak>
 // Definitions: https://github.com/borisyankov/DefinitelyTyped
 
 declare module chrome {
