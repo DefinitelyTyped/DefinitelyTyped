f326043fe6169746915f839e696b5a9013d22fb7
Marcin Porebski <porebski.m@gmail.com>
2015-08-22 14:31:43 +0200

    added empty module to string_score

--- string_score/string_score.d.ts
+++ string_score/string_score.d.ts
@@ -3,6 +3,11 @@
 // Definitions by: Marcin Porębski <https://github.com/marcinporebski/>
 // Definitions: https://github.com/borisyankov/DefinitelyTyped
 
+declare module "string_score"
+{
+    // nothing here as it's only extending the build in String class
+}
+
 interface String {
     score: (word: string, fuzzy?: number) => number;
 }
