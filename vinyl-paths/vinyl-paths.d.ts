25b3502efc4f4750346d2d9a03667398966154b9
tkQubo <tk.qubo@gmail.com>
2015-08-24 22:19:29 +0900

    Add reference to node

--- vinyl-paths/vinyl-paths.d.ts
+++ vinyl-paths/vinyl-paths.d.ts
@@ -3,6 +3,8 @@
 // Definitions by: Qubo <https://github.com/tkQubo>
 // Definitions: https://github.com/borisyankov/DefinitelyTyped
 
+/// <reference path="../node/node.d.ts" />
+
 declare module "vinyl-paths" {
 
     namespace paths {
