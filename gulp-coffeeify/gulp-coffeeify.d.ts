1781feb65cdf1fdf505aef85a00beccbe32f119d
tkQubo <tk.qubo@gmail.com>
2015-08-24 19:56:16 +0900

    Fix project url

--- gulp-coffeeify/gulp-coffeeify.d.ts
+++ gulp-coffeeify/gulp-coffeeify.d.ts
@@ -1,5 +1,5 @@
 // Type definitions for gulp-coffeeify
-// Project: gulp-coffeeify
+// Project: https://github.com/nariyu/gulp-coffeeify
 // Definitions by: Qubo <https://github.com/tkQubo>
 // Definitions: https://github.com/borisyankov/DefinitelyTyped
 
