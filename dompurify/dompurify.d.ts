48eb1c03ea825cd3d2842946d164054a4507208d
bazuzi <samira@bazuzi.com>
2015-08-25 16:44:03 -0400

    Expand typings for DOMPurify
    - A config can be passed in when using sanitize and the
    same config object is passed into the addHook callback.
    
    - Depending on config options, sanitize may return a DOM Node.
    
    - The basic examples from the DOMPurify README will confirm
    that the config object can be used flexibly with sanitize.

--- dompurify/dompurify.d.ts
+++ dompurify/dompurify.d.ts
@@ -1,11 +1,31 @@
 // Type definitions for DOM Purify
 // Project: https://github.com/cure53/DOMPurify
-// Definitions by: Dave Taylor <http://davetayls.me>
+// Definitions by: Dave Taylor <http://davetayls.me>, Samira Bazuzi <https://github.com/bazuzi>
 // Definitions: https://github.com/borisyankov/DefinitelyTyped
 
 interface IDOMPurify {
   sanitize(s:string):string;
-  addHook(hook:string, cb:(currentNode:Element, data:any, config:any) => Element):void;
+  sanitize(s:string, config:IDOMPurifyConfig):Node;
+  sanitize(s:string, config:IDOMPurifyConfig):string;
+
+  addHook(hook:string, cb:(currentNode:Element, data:any, config:IDOMPurifyConfig) => Element):void;
+}
+
+interface IDOMPurifyConfig {
+  ADD_ATTR?:string[];
+  ADD_TAGS?:string[];
+  ALLOW_DATA_ATTR?:boolean;
+  ALLOWED_ATTR?:string[];
+  ALLOWED_TAGS?:string[];
+  FORBID_ATTR?:string[];
+  FORBID_TAGS?:string[];
+  KEEP_CONTENT?:boolean;
+  RETURN_DOM?:boolean;
+  RETURN_DOM_FRAGMENT?:boolean;
+  RETURN_DOM_IMPORT?:boolean;
+  SAFE_FOR_JQUERY?:boolean;
+  SANITIZE_DOM?:boolean;
+  WHOLE_DOCUMENT?:boolean;
 }
 
 declare var DOMPurify:IDOMPurify;
