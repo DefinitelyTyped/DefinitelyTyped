3f0142f94138e1a8f0141ff8a5e9ab75233444d5
Kuniwak <yuki.kokubun@mixi.co.jp>
2015-08-25 16:37:14 +0900

    Use PromisesAPlus.Thenable

--- chai-as-promised/chai-as-promised-tests-with-q.ts
+++
@@ -1,7 +0,0 @@
-/// <reference path="chai-as-promised.d.ts" />
-/// <reference path="../q/Q.d.ts" />
-
-// Compatibility check for Promise/A+ valid libraries
-var thenableNum: Chai.Thenable<number>;
-import Q = require('q');
-thenableNum = Q.resolve(1);
