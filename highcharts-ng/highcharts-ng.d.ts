abbd18b14b67498adcd29e14c9b833a66dc21b20
Scott Hatcher <scott@scotthatcher.com>
2015-08-25 15:41:27 -0700

    Initial push for highcharts-ng definitions and tests.

---
+++ highcharts-ng/highcharts-ng.d.ts
@@ -0,0 +1,37 @@
+// Type definitions for highcharts-ng 0.0.8
+// Project: https://github.com/pablojim/highcharts-ng
+// Definitions by: Scott Hatcher <https://github.com/scatcher>
+// Definitions: https://github.com/borisyankov/DefinitelyTyped
+
+/// <reference path="../highcharts/highcharts.d.ts" />
+
+interface HighChartsNGConfig {
+	options: HighchartsChartOptions;
+	//The below properties are watched separately for changes.
+
+	//Series object (optional) - a list of series using normal highcharts series options.
+	series?: number[]|[number, number][]| HighchartsDataPoint[];
+	//Title configuration (optional)
+	title?: {
+		text?: string;
+	};
+	//Boolean to control showng loading status on chart (optional)
+	//Could be a string if you want to show specific loading text.
+	loading?: boolean;
+	//Configuration for the xAxis (optional). Currently only one x axis can be dynamically controlled.
+	//properties currentMin and currentMax provied 2-way binding to the chart's maximimum and minimum
+	xAxis?: {
+		currentMin?: number;
+		currentMax?: number;
+		title?: { text?: string }
+	},
+	//Whether to use HighStocks instead of HighCharts (optional). Defaults to false.
+	useHighStocks?: boolean;
+	//size (optional) if left out the chart will default to size of the div or something sensible.
+	size?: {
+		width?: number;
+		height?: number;
+	};
+	//function (optional) - setup some logic for the chart
+	func?: (chart) => void;
+}
