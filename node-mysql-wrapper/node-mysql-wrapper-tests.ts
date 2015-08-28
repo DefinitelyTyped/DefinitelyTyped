4d5bfab8b379b47e7542dc3ed8b3aad347af66d2
Makis Maropoulos <makis@ideopod.com>
2015-08-28 06:04:30 +0300

    Update node-mysql-wrapper-tests.ts

--- node-mysql-wrapper/node-mysql-wrapper-tests.ts
+++ node-mysql-wrapper/node-mysql-wrapper-tests.ts
@@ -5,19 +5,19 @@ var db = wrapper.wrap("mysql://kataras:pass@127.0.0.1/taglub?debug=false&charset
 
 db.ready(() => {
 
-    db.table("users").on("insert", (parsedResults) => {
+    db.table("users").on("insert", (parsedResults:any[]) => {
 
     });
 
-    db.table("users").findAll().then((results) => {
+    db.table("users").findAll().then((results:any[]) => {
         console.dir(results);
     });
 
-    db.table("users").find({ yearsOld: 22 }, (results) => {
+    db.table("users").find({ yearsOld: 22 }, (results:any[]) => {
         console.dir(results);
     });
 
-    db.table("users").findById(18, (result) => {
+    db.table("users").findById(18, (result:any) => {
         console.dir(result);
     });
 
