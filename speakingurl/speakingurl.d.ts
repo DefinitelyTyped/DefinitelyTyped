7245145decc53a38753fc720f2fa7246377aecad
chrisweb <chris963@gmail.com>
2015-08-28 16:20:27 +0200

    speakingurl first commit

---
+++ speakingurl/speakingurl.d.ts
@@ -0,0 +1,24 @@
+﻿// Type definitions for speakingurl
+// Project: https://github.com/pid/speakingurl
+// Definitions by: Weber Chris <https://github.com/chrisweb>
+// Definitions: https://github.com/borisyankov/DefinitelyTyped
+
+declare module "speakingurl" {
+
+    interface slugOptions {
+        separator: string,
+        lang: string,
+        symbols: boolean,
+        maintainCase: boolean,
+        titleCase: boolean|string[],
+        truncate: number,
+        uric: boolean,
+        uricNoSlash: boolean,
+        mark: boolean,
+        custom: {}|string[]
+    }
+
+    export function getSlug(input: string, options: string|slugOptions): string;
+    export function createSlug(options: string|slugOptions): (input: string, options: string|slugOptions) => string;
+
+}
