f61406d63cd269d830c861342258745869979fd7
João Correia <joaope@gmail.com>
2015-08-28 21:17:57 +0100

    Updated 'chrome.contentSettings'

--- chrome/chrome.d.ts
+++ chrome/chrome.d.ts
@@ -326,9 +326,11 @@ declare module chrome.commands {
     var onCommand: CommandEvent;
 }
 
-////////////////////
-// Content Settings
-////////////////////
+/**
+ * Use the chrome.contentSettings API to change settings that control whether websites can use features such
+ * as cookies, JavaScript, and plugins. More generally speaking, content settings allow you to customize Chrome's
+ * behavior on a per-site basis instead of globally.
+ */
 declare module chrome.contentSettings {
     interface ClearDetails {
         scope?: string;
