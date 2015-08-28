e91c88f79e2e9fc4591988370252529537fb48ec
tkQubo <tk.qubo@gmail.com>
2015-08-28 21:34:25 +0900

    Rename interfaces

--- karma/karma.d.ts
+++ karma/karma.d.ts
@@ -6,19 +6,31 @@
 declare module 'karma' {
     // See Karma public API https://karma-runner.github.io/0.12/dev/public-api.html
 
-    interface IKarmaServer {
-        start(options?: any, callback?: (exitCode: number) => void): void;
+    namespace karma {
+        interface Karma {
+            /**
+             * `start` method is deprecated since 0.13. It will be removed in 0.14.
+             * Please use
+             * <code>
+             *     server = new Server(config, [done])
+             *     server.start()
+             * </code>
+             * instead.
+             */
+            server: DeprecatedServer;
+            runner: Runner;
+        }
+
+        interface DeprecatedServer {
+            start(options?: any, callback?: (exitCode: number) => void): void;
+        }
+
+        interface Runner {
+            run(options?: any, callback?: (exitCode: number) => void): void;
+        }
     }
 
-    interface IKarmaRunner {
-        run(options?: any, callback?: (exitCode: number) => void): void;
-    }
+    var karma: karma.Karma;
 
-    interface IKarma {
-        server: IKarmaServer;
-        runner: IKarmaRunner;
-    }
-
-    var karma: IKarma;
     export = karma;
 }
