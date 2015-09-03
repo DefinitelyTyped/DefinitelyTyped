89b3391517991271d371489795003bd3a090164a
psnider <snider.peter@gmail.com>
2015-09-03 14:14:58 +0000

    updated attribution

--- tv4/tv4.d.ts
+++ tv4/tv4.d.ts
@@ -1,7 +1,6 @@
 // Type definitions for Tiny Validator tv4 1.2.5
 // Project: https://github.com/geraintluff/tv4
-// Definitions by: Bart van der Schoor <https://github.com/Bartvds>
-// Definitions by: Peter Snider <https://github.com/psnider>
+// Definitions by: Bart van der Schoor <https://github.com/Bartvds>, Peter Snider <https://github.com/psnider>
 // Definitions: https://github.com/borisyankov/DefinitelyTyped
 
 declare module tv4 {
