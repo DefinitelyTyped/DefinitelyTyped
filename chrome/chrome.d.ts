87965af3373a8b57844bdd87e4a6714446087700
Fredrik Høisæther Rasch <fredrik.rasch@gmail.com>
2015-09-05 14:48:05 +0200

    Added/Updated power /w docs

--- chrome/chrome.d.ts
+++ chrome/chrome.d.ts
@@ -5181,6 +5181,21 @@ declare module chrome.platformKeys {
 }
 
 ////////////////////
+// Power
+////////////////////
+/**
+ * Use the chrome.power API to override the system's power management features. 
+ * Permissions:  "power"   
+ * @since Chrome 27.
+ */
+declare module chrome.power {
+	/** Requests that power management be temporarily disabled. |level| describes the degree to which power management should be disabled. If a request previously made by the same app is still active, it will be replaced by the new request. */
+	export function requestKeepAwake(level: string): void;
+	/** Releases a request previously made via requestKeepAwake(). */
+	export function releaseKeepAwake(): void;
+}
+
+////////////////////
 // Privacy
 ////////////////////
 declare module chrome.privacy {
