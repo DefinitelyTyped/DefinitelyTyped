b2c92a8dd6e05911b141236d40fc0f5944938b07
jamiewinder <Jamiewinder@gmail.com>
2015-09-06 18:41:05 +0100

    Create react-redux.d.ts
    init version

---
+++ react-redux/react-redux.d.ts
@@ -0,0 +1,34 @@
+// Type definitions for rackt/react-redux v2.1.0
+// Project: https://github.com/rackt/react-redux
+// Definitions by: Jamie Winder <https://github.com/jamiewinder/>
+// Definitions: https://github.com/borisyankov/DefinitelyTyped
+
+/// <reference path="../react/react.d.ts" />
+
+declare module ReactRedux {
+
+    // Connect
+    type MapStateToPropsFunc = (state: any, ownProps?: boolean) => Object;
+    type MapDispatchToPropsFunc = (dispatch: any, ownProps?: boolean) => Object;
+    type MergePropsFunc = (stateProps: any, dispatchProps: any, ownProps: boolean) => Object;
+
+    interface ConnectOptions {
+        pure: boolean
+    }
+
+    function connect(
+        mapStateToProps?: MapStateToPropsFunc,
+        mapDispatchToProps?: Object | MapDispatchToPropsFunc,
+        mergeProps?: MergePropsFunc,
+        options?: ConnectOptions
+    );
+
+    // Provider
+    class Provider extends __React.Component<any, any>
+    {
+    }
+}
+
+declare module "react-redux" {
+    export = ReactRedux;
+}
