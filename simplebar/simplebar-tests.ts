264cf217b87668b14f6cc815d48bea8cd82e4e52
Gregor Woiwode <g.woiwode@cid.de>
2015-09-07 11:17:37 +0200

    Removes comments from test file

--- simplebar/simplebar-tests.ts
+++ simplebar/simplebar-tests.ts
@@ -1,10 +1,5 @@
 /// <reference path="simplebar.d.ts"/>
 /// <reference path="../jquery/jquery.d.ts"/>
 
-// Type definitions for simplebar.js 1.1.7
-// Project: https://github.com/Grsmto/simplebar
-// Definitions by: Gregor Woiwode <https://github.com/gregonnet>
-// Definitions: https://github.com/borisyankov/DefinitelyTyped
-
 var simplebar = $.fn.simplebar.noConflict();
 $.fn.simplebar = simplebar;
