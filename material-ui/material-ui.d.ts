9be049500f806eaf7fb94348f4173f5089939bf5
nojaf <florian.verdonck@outlook.com>
2015-09-08 22:28:39 +0200

    Initial setup for material-ui

---
+++ material-ui/material-ui.d.ts
@@ -0,0 +1,36 @@
+///<reference path='../react/react.d.ts' />
+
+declare module MaterialUI {
+  import React = __React;
+
+   module Styles {
+    export class ThemeManager{
+      getCurrentTheme():any;
+    }
+  }
+
+  //FlatButton
+  interface FlatButtonProp extends React.HTMLAttributes {
+    label?:string;
+  }
+
+  interface FlatButton extends React.ReactElement<FlatButtonProp> {
+
+  }
+
+  interface FlatButtonClass extends React.ComponentClass<FlatButtonProp> {}
+
+  var FlatButton:FlatButtonClass;
+}
+
+declare module "material-ui" {
+  export = MaterialUI;
+}
+
+declare module __React{
+  //for FlatButton
+  function createElement(
+  type: MaterialUI.FlatButtonClass,
+  props: MaterialUI.FlatButtonProp,
+  ...children: __React.ReactNode[]): MaterialUI.FlatButton;
+}
