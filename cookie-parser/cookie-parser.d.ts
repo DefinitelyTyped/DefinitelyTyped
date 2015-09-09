e2001653ffbd99c2fc80a272ca80d8c958136de4
MarlonFan <marlon@tfan.net>
2015-09-09 13:35:38 +0800

    update version

--- cookie-parser/cookie-parser.d.ts
+++ cookie-parser/cookie-parser.d.ts
@@ -1,4 +1,4 @@
-// Type definitions for cookie-parser
+// Type definitions for cookie-parser v1.3.4
 // Project: https://github.com/expressjs/cookie-parser
 // Definitions by: Santi Albo <https://github.com/santialbo/>
 // Definitions: https://github.com/borisyankov/DefinitelyTyped
