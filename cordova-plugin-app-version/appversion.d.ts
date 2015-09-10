ed61154fea17a0158ab82147d3f80d27c0b662be
Ritzlgrmft <markus.wagner02@web.de>
2015-09-10 10:36:36 +0200

    Definitions added for Cordova plugin cordova-plugin-app-version (https://github.com/whiteoctober/cordova-plugin-app-version).

---
+++ cordova-plugin-app-version/appversion.d.ts
@@ -0,0 +1,8 @@
+﻿interface Cordova {
+    getAppVersion: {
+		getAppName: () => ng.IPromise<string>;
+		getPackageName: () => ng.IPromise<string>;
+		getVersionCode: () => ng.IPromise<string>;
+		getVersionNumber: () => ng.IPromise<string>;
+	};
+}
