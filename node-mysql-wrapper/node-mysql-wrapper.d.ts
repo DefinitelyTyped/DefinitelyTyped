cdffc42fd4b5156db21099ea6a701b41487142a8
Makis Maropoulos <makis@ideopod.com>
2015-09-10 05:32:04 +0300

    I moved the project, update the project's url here also

--- node-mysql-wrapper/node-mysql-wrapper.d.ts
+++ node-mysql-wrapper/node-mysql-wrapper.d.ts
@@ -1,5 +1,5 @@
 // Type definitions for node-mysql-wrapper
-// Project: https://github.com/kataras/node-mysql-wrapper
+// Project: https://github.com/nodets/node-mysql-wrapper
 // Definitions by: Makis Maropoulos <https://github.com/kataras>
 // Definitions: https://github.com/borisyankov/DefinitelyTyped
 
