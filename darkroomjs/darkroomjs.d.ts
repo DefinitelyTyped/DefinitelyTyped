d1d4d047e46b9d0a09c7d05ab6525ad04182ab4e
Stepan Mikhaylyuk <stepancar@hotmail.com>
2015-09-11 19:20:33 +0300

    added darkroomjs.d.ts

---
+++ darkroomjs/darkroomjs.d.ts
@@ -0,0 +1,45 @@
++// Type definitions for darkroomjs
++// Project: https://github.com/MattKetmo/darkroomjs
++// Definitions by: Stepan Mikhaylyuk <https://github.com/stepancar>
++// Definitions: https://github.com/borisyankov/DefinitelyTyped
++interface DarkRoom {
++    new (element: string | HTMLImageElement, options?: Options);
++    plugins: {
++      crop:any;
++      save:any;
++    }
++}
++interface Options {
++    minWidth?: number,
++    minHeight?: number,
++    maxWidth?: number,
++    maxHeight?: number,
++    plugins?: Plugins,
++    initialize?: () => void,
++    backgroundColor?: string;
++    ratio?: number;
++}
++interface Plugins {
++    /**
++    * Crop plugin options. if passed 'false' - disable plugin
++    */
++    crop?: CropPluginOptions | boolean;
++    /**
++    * Save plugin options. if passed 'false' - disable plugin
++    */
++    save?: SavePluginOptions | boolean
++}
++interface CropPluginOptions {
++    minHeight?: number,
++    minWidth?: number,
++    ratio?: number,
++    quickCropKey?: number;
++}
++interface SavePluginOptions {
++
++}
++declare var Darkroom: DarkRoom;
++/*declare module 'darkroom'{
++  var darkRoom: DarkRoom;
++  export = darkRoom;
++}*/
