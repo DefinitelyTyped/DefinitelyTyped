c07469bfe42d19bc5e7a8acac3a3834f2d45441a
Stepan Mikhaylyuk <stepancar@hotmail.com>
2015-09-11 20:52:21 +0300

    minor fix

--- darkroomjs/darkroomjs-tests.ts
+++ darkroomjs/darkroomjs-tests.ts
@@ -1,27 +1,24 @@
-+/// <reference path="./darkroomjs.d.ts" />
-+// test from https://github.com/MattKetmo/darkroomjs
-+var darkroom = new Darkroom('#target', {
-+    // Canvas initialization size
-+    minWidth: 100,
-+    minHeight: 100,
-+    maxWidth: 500,
-+    maxHeight: 500,
-+
-+    // Plugins options
-+    plugins: {
-+        crop: {
-+            minHeight: 50,
-+            minWidth: 50,
-+            ratio: 1
-+        },
-+        save: false // disable plugin
-+    },
-+    // Post initialization method
-+    initialize: function() {
-+        // Active crop selection
-+        this.plugins['crop'].requireFocus();
-+
-+        // Add custom listener
-+        this.addEventListener('core:transformation', function() { /* ... */ });
-+    }
-+});
+/// <reference path="./darkroomjs.d.ts" />
+// test from https://github.com/MattKetmo/darkroomjs
+var y = new Darkroom('#target', {
+    minWidth: 100,
+    minHeight: 100,
+    maxWidth: 500,
+    maxHeight: 500,
+    plugins: {
+        crop: {
+            minHeight: 50,
+            minWidth: 50,
+            ratio: 1
+        },
+        save: false
+    },
+
+    initialize: function() {
+        // Active crop selection
+        this.plugins['crop'].requireFocus();
+
+        // Add custom listener
+        this.addEventListener('core:transformation', function() { /* ... */ });
+    }
+});
