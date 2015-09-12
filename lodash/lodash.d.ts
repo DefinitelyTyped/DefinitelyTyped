743adf764098fa4cd9add2370295d4f9f6946ea2
Ilya Mochalov <chrootsu@gmail.com>
2015-09-13 01:34:51 +0500

    lodash: changed _.noConflict() method

--- lodash/lodash.d.ts
+++ lodash/lodash.d.ts
@@ -8262,9 +8262,17 @@ declare module _ {
     //_.noConflict
     interface LoDashStatic {
         /**
-        * Reverts the '_' variable to its previous value and returns a reference to the lodash function.
-        * @return The lodash function.
-        **/
+         * Reverts the _ variable to its previous value and returns a reference to the lodash function.
+         *
+         * @return Returns the lodash function.
+         */
+        noConflict(): typeof _;
+    }
+
+    interface LoDashWrapperBase<T, TWrapper> {
+        /**
+         * @see _.noConflict
+         */
         noConflict(): typeof _;
     }
 
