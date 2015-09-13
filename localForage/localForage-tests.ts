8ebc7032a119e26f5b87aa3e8af186101c47d090
Aleksey Blokhin <a.blokhin@me.com>
2015-09-13 22:15:08 +0300

    update localForage

--- localForage/localForage-tests.ts
+++ localForage/localForage-tests.ts
@@ -1,68 +1,100 @@
 ﻿/// <reference path="localForage.d.ts" />
 
-declare var localForage: lf.ILocalForage<string>;
-declare var callback: lf.ICallback<string>;
-declare var iterateCallback: lf.IIterateCallback<string>;
-declare var errorCallback: lf.IErrorCallback;
-declare var keyCallback: lf.IKeyCallback;
-declare var keysCallback: lf.IKeysCallback;
-declare var numberCallback: lf.INumberCallback;
-declare var promise: lf.IPromise<string>;
-
-() => {
-    localForage.clear((err: any) => {
-        var newError: any = err;
-    });
-
-    localForage.iterate((str: string, key: string, num: number) => {
-        var newStr: string = str;
-        var newKey: string = key;
-        var newNum: number = num;
-    });
-
-    localForage.length((err: any, num: number) => {
-        var newError: any = err;
-        var newNumber: number = num;
-    });
-
-    localForage.key(0,(err: any, value: string) => {
-        var newError: any = err;
-        var newValue: string = value;
-    });
-
-    localForage.keys((err: any, keys: Array<string>) => {
-        var newError: any = err;
-        var newArray: Array<string> = keys;
-    });
-
-    localForage.getItem("key",(err: any, str: string) => {
-        var newError: any = err;
-        var newStr: string = str
-    });
-
-    localForage.getItem("key").then((err: any, str: string) => {
-        var newError: any = err;
-        var newStr: string = str
-    });
-  
-    localForage.setItem("key", "value",(err: any, str: string) => {
-        var newError: any = err;
-        var newStr: string = str
-    });
-
-    localForage.setItem("key", "value").then((err: any, str: string) => {
-        var newError: any = err;
-        var newStr: string = str;
-    });
-  
-    localForage.removeItem("key",(err: any) => {
-        var newError: any = err;
-    });
-
-    localForage.removeItem("key").then((err: any, str: string) => {
-        var newError: any = err;
-        var newStr: string = str
-    });
-  
-    promise.then(callback);
+interface IType {
+  value : string;
 }
+
+interface IOtherType {
+  value : number;
+}
+
+// IPromise typing check.
+
+var voidPromise      : lf.IPromise<void>,
+    typePromise      : lf.IPromise<IType>,
+    otherTypePromise : lf.IPromise<IOtherType>;
+
+voidPromise = typePromise.then((value : IType) => {});
+voidPromise = typePromise.then((value : IType) => {}, (reason : any) => {});
+
+typePromise = typePromise.then((value : IType) => { return value; });
+
+otherTypePromise = typePromise
+    .then(function(value : IType) { var otherValue : IOtherType; return otherValue; });
+
+otherTypePromise = typePromise
+    .then(function(value : IType) { var otherValue : IOtherType; return otherValue; }, (reason : any) => {});
+
+voidPromise = typePromise.catch((reason : any) => {});
+
+otherTypePromise = typePromise.catch((reason : any) => { var otherValue : IOtherType; return otherValue; });
+
+// IOptions typing check.
+
+var num                 : number,
+    str                 : string,
+    val                 : IType,
+    numberPromise       : lf.IPromise<number>,
+    stringPromise       : lf.IPromise<string>,
+    stringArrayPromise  : lf.IPromise<Array<string>>,
+    LocalForage         : lf.ILocalForage<IType>,
+    localForageInstance : lf.ILocalForageInstance<IType>,
+    options             : lf.IOptions;
+
+options = { driver : LocalForage.INDEXEDDB };
+options = { driver : [LocalForage.LOCALSTORAGE, LocalForage.WEBSQL, LocalForage.INDEXEDDB ]};
+
+options = { name : 'LOCAL_FORAGE' };
+
+options = { version : '1.0.0' };
+
+options = { size : 16777216 };
+
+options = { storeName : 'TEST' };
+
+options = { description : 'Declarations testing.' };
+
+// ILocalForage typing check.
+
+localForageInstance = new LocalForage();
+localForageInstance = new LocalForage(options);
+
+// ILocalForageInstance typing check.
+
+localForageInstance = localForageInstance.createInstance();
+localForageInstance = localForageInstance.createInstance(options);
+
+localForageInstance.config(options);
+options = localForageInstance.config('TEST');
+
+voidPromise = localForageInstance.ready();
+voidPromise = localForageInstance.ready((error : any) => {});
+
+voidPromise = localForageInstance.clear();
+voidPromise = localForageInstance.clear((error : any) => {});
+
+numberPromise = localForageInstance.length();
+numberPromise = localForageInstance.length((error : any, length : number) => {});
+
+stringPromise = localForageInstance.key(num);
+stringPromise = localForageInstance.key(num, (error : any, key : string) => {});
+
+stringArrayPromise = localForageInstance.keys();
+stringArrayPromise = localForageInstance.keys((error : any, keys : Array<string>) => {});
+
+voidPromise = localForageInstance.iterate(
+    (value : IType, key : string, iterationNumber : number) => {});
+otherTypePromise = localForageInstance.iterate(
+    (value : IType, key : string, iterationNumber : number) => { var otherValue : IOtherType; return otherValue; });
+otherTypePromise = localForageInstance.iterate(
+    (value : IType, key : string, iterationNumber : number) => { var otherValue : IOtherType; return otherValue; },
+    (error : any, value : IOtherType) => {});
+
+typePromise = localForageInstance.getItem(str);
+typePromise = localForageInstance.getItem(str , (error : any, value : IType) => {});
+
+typePromise = localForageInstance.setItem(str, val);
+typePromise = localForageInstance.setItem(str, val, (error : any, value : IType) => {});
+
+voidPromise = localForageInstance.removeItem(str);
+voidPromise = localForageInstance.removeItem(str, (error : any) => {});
--- localForage/localForage.d.ts
+++ localForage/localForage.d.ts
@@ -1,73 +1,95 @@
 ﻿// Type definitions for Mozilla's localForage
 // Project: https://github.com/mozilla/localforage
-// Definitions by: yuichi david pichsenmeister <https://github.com/3x14159265>
+// Definitions by: yuichi david pichsenmeister <https://github.com/3x14159265>, Aleksey Blokhin <https://github.com/arma-gast>
 // Definitions: https://github.com/borisyankov/DefinitelyTyped
 
 declare module lf {
-    interface ILocalForage<T> {
-        /**
-         * Removes every key from the database, returning it to a blank slate.
-         */
-        clear(callback: IErrorCallback): void
-        /**
-         * Iterate over all value/key pairs in datastore.
-         */
-        iterate(iterateCallback: IIterateCallback<T>): void
-        /**
-         * Get the name of a key based on its ID.
-         */
-        key(keyIndex: number, callback: IKeyCallback): void
-        /**
-         * Get the list of all keys in the datastore.
-         */
-        keys(callback: IKeysCallback): void;
-        /**
-         * Gets the number of keys in the offline store (i.e. its “length”).
-         */
-        length(callback: INumberCallback): void
-        /**
-         * Gets an item from the storage library and supplies the result to a callback.
-         * If the key does not exist, getItem() will return null.
-         */
-        getItem(key: string, callback: ICallback<T>): void
-        getItem(key: string): IPromise<T>
-        /**
-         * Saves data to an offline store.
-         */
-        setItem(key: string, value: T, callback: ICallback<T>): void
-        setItem(key: string, value: T): IPromise<T>
-        /**
-         * Removes the value of a key from the offline store.
-         */
-        removeItem(key: string, callback: IErrorCallback): void
-        removeItem(key: string): IPromise<T>
-    }
-
-    interface ICallback<T> {
-        (err: any, value: T): void
-    }
-
-    interface IIterateCallback<T> {
-        (value: T, key: string, iterationNumber: number): void
-    }
-
-    interface IErrorCallback {
-        (err: any): void
-    }
-
-    interface IKeyCallback {
-        (err: any, keyName: string): void
-    }
-
-    interface IKeysCallback {
-        (err: any, keys: Array<string>): void
-    }
-
-    interface INumberCallback {
-        (err: any, numberOfKeys: number): void
+    interface IOptions {
+        driver?      : string | Array<string>;
+        name?        : string;
+        version?     : string;
+        size?        : number;
+        storeName?   : string;
+        description? : string;
     }
 
     interface IPromise<T> {
-        then(callback: ICallback<T>): void
+        then<TResult> (successCallback : (value : T) => TResult | IPromise<TResult>, failureCallback? : (reason : any) => any) : IPromise<TResult>;
+        catch<TResult> (failureCallback : (reason : any) => TResult | IPromise<TResult>) : IPromise<TResult>;
+    }
+
+    interface ILocalForageInstance<T> {
+        INDEXEDDB    : string;
+        LOCALSTORAGE : string;
+        WEBSQL       : string;
+
+        /**
+         * Creates new instance of LocalForage with supplied options.
+         */
+        createInstance(options? : IOptions) : ILocalForage<T>;
+
+        /**
+         * Sets database information. Returns instance of Error if datastore
+         * is ready and fully initialised. Otherwise returns true.
+         */
+        config(options? : IOptions) : boolean | Error;
+
+        /**
+         * Returns configuration values for the datastore.
+         */
+        config(storeName : string) : IOptions;
+
+        /**
+         * Determines if datastore is ready to be used.
+         */
+        ready(callback? : (error : any) => void) : IPromise<void>;
+
+        /**
+         * Removes every key from the datastore, returning it to a blank slate.
+         */
+        clear(callback? : (error : any) => void) : IPromise<void>;
+
+        /**
+         * Gets the number of keys in the datastore (i.e. its "length").
+         */
+        length(callback? : (error : any, length : number) => void) : IPromise<number>;
+
+        /**
+         * Get the name of a key based on its ID.
+         */
+        key(n : number, callback? : (error : any, key : string) => void) : IPromise<string>;
+
+        /**
+         * Get the list of all keys in the datastore.
+         */
+        keys(callback? : (error : any, keys : Array<string>) => void) : IPromise<Array<string>>;
+
+        /**
+         * Iterate over all value/key pairs in datastore.
+         */
+        iterate<U>(iterateCallback : (value : T, key : string, iterationNumber : number) => U, successCallback? : (error : any, value : U) => void) : IPromise<U>;
+
+        /**
+         * Gets an item from the datastore and supplies the result to a promise (callback).
+         * If the key does not exist, getItem() will supply null to promise (callback).
+         */
+        getItem(key : string, callback? : (error : any, value : T) => void) : IPromise<T>;
+
+        /**
+         * Saves data to the datastore.
+         */
+        setItem(key : string, value : T, callback? : (error : any, value : T) => void) : IPromise<T>;
+
+        /**
+         * Removes the value of a key from the datastore.
+         */
+        removeItem(key : string, callback? : (error : any) => void) : IPromise<void>;
+    }
+
+    interface ILocalForage<T> extends ILocalForageInstance<T> {
+        /**
+         * Creates new instance of LocalForage with supplied options.
+         */
+        new(options? : IOptions) : ILocalForageInstance<T>;
     }
 }
