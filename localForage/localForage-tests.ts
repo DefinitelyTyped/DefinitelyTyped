efa4f9d53222369df7eb2827dd6d63527bea05cd
Stephen Lautier <stephen.lautier@outlook.com>
2015-09-15 00:42:46 +0200

    included more definitions

--- localForage/localForage-tests.ts
+++ localForage/localForage-tests.ts
@@ -25,7 +25,7 @@ import * as localForage from "localforage";
 
     localForage.keys((err: any, keys: Array<string>) => {
         var newError: any = err;
-        var newArray: Array<string> = keys;
+        var newArray:  Array<string> = keys;
     });
 
     localForage.getItem("key",(err: any, str: string) => {
@@ -35,7 +35,7 @@ import * as localForage from "localforage";
 
     localForage.getItem<string>("key").then((value) => {        
         var newStr: string = value
-    });
+    }); 
 
     localForage.getItem<number>("keyNumber").then((value) => {        
         var newValue: number = value
@@ -44,8 +44,8 @@ import * as localForage from "localforage";
     localForage.setItem("key", "value",(err: any, str: string) => {
         var newError: any = err;
         var newStr: string = str
-    });
-
+  });
+ 
     localForage.setItem("key", "value").then((value) => {
         var v: string = value;
     });
@@ -54,12 +54,22 @@ import * as localForage from "localforage";
         var v: number = value;
     });
   
-    localForage.removeItem("key",(err: any) => {
-        var newError: any = err;
+  localForage.removeItem("key",(err: any) => {
+        var newError: any = err; 
     });
     
     localForage.removeItem("key")
         .then( () => {
             
         });
+
+    var config = localForage.default.config({
+        name: "testyo",
+        driver: localForage.default.LOCALSTORAGE
+    });
+    
+    var store = localForage.default.createInstance({
+        name: "da instance",
+        driver: localForage.default.LOCALSTORAGE
+    });
 }
