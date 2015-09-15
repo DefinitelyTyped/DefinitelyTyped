e24b3227132ba441741d16a9c03b2efab78704e4
Pascal Senn (GIAPSE) <giapse@gia.ch>
2015-09-15 14:57:21 +0200

    Added Gridstack definition file and test

---
+++ gridstack/gridstack-tests.ts
@@ -0,0 +1,16 @@
+﻿// Type definitions for Gridstack
+// Project: http://troolee.github.io/gridstack.js/
+// Definitions by: Pascal Senn  <https://github.com/PascalSenn/>
+// Definitions: https://github.com/borisyankov/DefinitelyTyped
+
+var options = <IGridstackOptions> {
+    float: true;
+};
+var gridstack:GridStack = $(document).gridstack(options);
+
+gridstack.add_widget("test", 1, 2, 3, 4, true);
+gridstack.batch_update();
+gridstack.cell_height();;
+gridstack.cell_height(2);
+gridstack.cell_width();
+gridstack.get_cell_from_pixel(<MousePosition>{ left:20, top: 20 });
