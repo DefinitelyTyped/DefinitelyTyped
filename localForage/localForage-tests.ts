f0fee739c30aafa4a8cdfdd9ddf982d4ec72ba35
Stephen Lautier <stephen.lautier@outlook.com>
2015-09-17 00:23:45 +0200

    merged conflicts taking theirs

--- localForage/localForage-tests.ts
+++ localForage/localForage-tests.ts
@@ -1,10 +1,6 @@
 ﻿/// <reference path="localForage.d.ts" />
 
-<<<<<<< HEAD
-import {default as localForage} from "localforage";
-=======
 declare var localForage: LocalForage;
->>>>>>> 5f480287834a2615274eea31574b713e64decf17
 
 () => {
     localForage.clear((err: any) => {
@@ -29,7 +25,7 @@ declare var localForage: LocalForage;
 
     localForage.keys((err: any, keys: Array<string>) => {
         var newError: any = err;
-        var newArray:  Array<string> = keys;
+        var newArray: Array<string> = keys;
     });
 
     localForage.getItem("key",(err: any, str: string) => {
@@ -37,57 +33,23 @@ declare var localForage: LocalForage;
         var newStr: string = str
     });
 
-<<<<<<< HEAD
-    localForage.getItem<string>("key").then((value) => {        
-        var newStr: string = value
-    }); 
-
-    localForage.getItem<number>("keyNumber").then((value) => {        
-        var newValue: number = value
-=======
     localForage.getItem<string>("key").then((str: string) => {
         var newStr: string = str;
->>>>>>> 5f480287834a2615274eea31574b713e64decf17
     });
   
     localForage.setItem("key", "value",(err: any, str: string) => {
         var newError: any = err;
         var newStr: string = str
-  });
- 
-    localForage.setItem("key", "value").then((value) => {
-        var v: string = value;
     });
 
-<<<<<<< HEAD
-    localForage.setItem("keyNumber", 1337).then((value) => {
-        var v: number = value;
-=======
     localForage.setItem("key", "value").then((str: string) => {
         var newStr: string = str;
->>>>>>> 5f480287834a2615274eea31574b713e64decf17
     });
   
-  localForage.removeItem("key",(err: any) => {
-        var newError: any = err; 
+    localForage.removeItem("key",(err: any) => {
+        var newError: any = err;
     });
-    
-    localForage.removeItem("key")
-        .then( () => {
-            
-        });
 
-<<<<<<< HEAD
-    var config = localForage.config({
-        name: "testyo",
-        driver: localForage.LOCALSTORAGE
-    });
-    
-    var store = localForage.createInstance({
-        name: "da instance",
-        driver: localForage.LOCALSTORAGE
-=======
     localForage.removeItem("key").then(() => {
->>>>>>> 5f480287834a2615274eea31574b713e64decf17
     });
 }
