813a3c7490049f6e66f63969a213aa902addb7a3
Shiak1 <shiyak11211@gmail.com>
2015-09-16 19:07:05 -0400

    Added formData to Options interface
    Fix issue #5787

--- request/request.d.ts
+++ request/request.d.ts
@@ -62,6 +62,7 @@ declare module 'request' {
 			uri?: string;
 			callback?: (error: any, response: http.IncomingMessage, body: any) => void;
 			jar?: any; // CookieJar
+			formData: any; // Object
 			form?: any; // Object or string
 			auth?: AuthOptions;
 			oauth?: OAuthOptions;
