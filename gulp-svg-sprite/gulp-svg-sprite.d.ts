06d310cac577b8e4bfdbc2cd0e367e694029746a
tkqubo <tk.qubo@gmail.com>
2015-09-17 06:01:56 +0900

    Add version

--- gulp-svg-sprite/gulp-svg-sprite.d.ts
+++ gulp-svg-sprite/gulp-svg-sprite.d.ts
@@ -1,4 +1,4 @@
-// Type definitions for gulp-svg-sprite
+// Type definitions for gulp-svg-sprite 1.2.9
 // Project: https://github.com/jkphl/gulp-svg-sprite
 // Definitions by: Qubo <https://github.com/tkqubo>
 // Definitions: https://github.com/borisyankov/DefinitelyTyped
