25ffe5a792a1b940945af7f6960bcc2566e44d3e
tkqubo <tk.qubo@gmail.com>
2015-09-17 06:00:34 +0900

    Add gulp-svg-sprite

---
+++ gulp-svg-sprite/gulp-svg-sprite.d.ts
@@ -0,0 +1,22 @@
+// Type definitions for gulp-svg-sprite
+// Project: https://github.com/jkphl/gulp-svg-sprite
+// Definitions by: Qubo <https://github.com/tkqubo>
+// Definitions: https://github.com/borisyankov/DefinitelyTyped
+
+/// <reference path="../node/node.d.ts" />
+/// <reference path="../svg-sprite/svg-sprite.d.ts" />
+
+declare module "gulp-svg-sprite" {
+    import spriter = require('svg-sprite');
+
+    namespace svgSprite {
+        interface SvgSprite {
+            (options?: spriter.Config): NodeJS.ReadWriteStream;
+        }
+    }
+
+    var svgSprite: svgSprite.SvgSprite;
+
+    export = svgSprite;
+}
+
