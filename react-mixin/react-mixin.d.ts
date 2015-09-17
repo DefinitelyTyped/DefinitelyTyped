bcb620576890cdc33d0f0fce3a13215434b5dda2
tkqubo <tk.qubo@gmail.com>
2015-09-18 05:45:51 +0900

    Add react-mixin

---
+++ react-mixin/react-mixin.d.ts
@@ -0,0 +1,27 @@
+// Type definitions for react-mixin
+// Project: https://github.com/brigand/react-mixin
+// Definitions by: Qubo <https://github.com/tkqubo>
+// Definitions: https://github.com/borisyankov/DefinitelyTyped
+
+/// <reference path="../react/react.d.ts" />
+
+declare module "react-mixin" {
+    import * as React from 'react';
+
+    namespace reactMixin {
+        export interface ClassDecorator {
+            <TFunction extends Function>(target: TFunction): TFunction|void;
+        }
+
+        interface ReactMixin {
+            decorate(mixin: React.Mixin<any, any>): ClassDecorator;
+            onClass<S>(clazz: any, mixin: React.Mixin<any, any>): React.ComponentClass<S>;
+            <S>(clazz: any, mixin: React.Mixin<any, any>): React.ComponentClass<S>;
+        }
+    }
+
+    var reactMixin: reactMixin.ReactMixin;
+
+    export = reactMixin;
+}
+
