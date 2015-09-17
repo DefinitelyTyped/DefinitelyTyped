781e4c294d0721cf31344e721e0107d307c1a66b
Ritzlgrmft <markus.wagner02@web.de>
2015-09-17 14:51:02 +0200

    Travis errors fixed

--- cordova-plugin-app-version/cordova-plugin-app-version.d.ts
+++ cordova-plugin-app-version/cordova-plugin-app-version.d.ts
@@ -1,8 +1,9 @@
-﻿/// <reference path="../Q/Q.d.ts" />
-// Type definitions for cordova-plugin-app-version v0.1.7
+﻿// Type definitions for cordova-plugin-app-version v0.1.7
 // Project: https://github.com/whiteoctober/cordova-plugin-app-version
 // Definitions by: Markus Wagner <https://github.com/Ritzlgrmft/>
 // Definitions: https://github.com/borisyankov/DefinitelyTyped
+
+/// <reference path="../q/Q.d.ts" />
 
 interface Cordova {
     getAppVersion: {
