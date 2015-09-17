3f8e1d06327367446770b4d8f353bb921f7ba7e4
benliddicott <ben@liddicott.com>
2015-09-17 15:51:30 +0100

    Updated header

--- static-eval/static-eval.d.ts
+++ static-eval/static-eval.d.ts
@@ -1,4 +1,10 @@
-﻿declare module 'static-eval' {
+﻿// Type definitions for static-eval v0.2.4
+// Project: https://github.com/substack/static-eval
+// Definitions by: Ben Liddicott <https://github.com/benliddicott/DefinitelyTyped>
+// Definitions: https://github.com/borisyankov/DefinitelyTyped
+ 
+
+declare module 'static-eval' {
     function evaluate(ast, vars: { [name: string]: any });
     export =evaluate;
 }
