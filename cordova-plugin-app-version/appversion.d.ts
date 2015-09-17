3251d88b3a375de5043e7aa47d2706579954ac2a
Ritzlgrmft <markus.wagner02@web.de>
2015-09-17 11:02:48 +0200

    naming convention fixed Travis errors fixed

--- cordova-plugin-app-version/appversion.d.ts
+++
@@ -1,8 +0,0 @@
-﻿interface Cordova {
-    getAppVersion: {
-		getAppName: () => ng.IPromise<string>;
-		getPackageName: () => ng.IPromise<string>;
-		getVersionCode: () => ng.IPromise<string>;
-		getVersionNumber: () => ng.IPromise<string>;
-	};
-}
