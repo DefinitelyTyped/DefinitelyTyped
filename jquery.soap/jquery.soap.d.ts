18280ccc89e35add2976685bb3999236ecaf63fc
tigerxy <roland@stempelgreim.de>
2015-09-20 19:02:43 +0200

    soap tests updated

--- jquery.soap/jquery.soap.d.ts
+++ jquery.soap/jquery.soap.d.ts
@@ -7,68 +7,72 @@
 
 declare module JQuerySOAP {
     interface SOAPEnvelope {
-        attributes: any
-        bodies: any
-        headers: any
+        attributes: Object
+        bodies: Array<SOAPObject>
+        headers: Array<SOAPObject>
         prefix: string
         soapConfig: any
         typeOf: string
-        addAttribute(name, value): void
+        addAttribute(name: String, value: string): void
+        addAttribute(name: String, value: number): void
         addBody(soapObject: SOAPObject): void
         addHeader(soapObject: SOAPObject): void
-        addNamespace(name, uri): void
+        addNamespace(name: String, uri: string): void
         toString(): string
-        send(options): void
+        send(options: Options): void
     }
     interface SOAPResponse {
         toJSON(): any
         toString(): String
         toXML(): XMLDocument
     }
-    
+
     interface SOAPObject {
-        attributes: any
-        children: any
+        attributes: Object
+        children: Array<SOAPObject>
         name: string
-        ns: any
-        _parent: any
+        ns: Object
+        _parent: SOAPObject
         value: any
         typeOf: string
-        addNamespace(name, url): void
-        addParameter(name, value)
-        appendChild(soapObject: SOAPObject)
-        attr(name, value)
-        end()
-        find(name)
-        hasChildren()
-        newChild(name)
-        parent()
+        addNamespace(name: String, url: string): void
+        addParameter(name: String, value: string): void
+        addParameter(name: String, value: number): void
+        appendChild(soapObject: SOAPObject): SOAPObject
+        attr(name: string, value: string): Object
+        attr(name: string, value: number): Object
+        end(): SOAPObject
+        find(name: string): SOAPObject
+        hasChildren(): boolean
+        newChild(name: string): SOAPObject
+        parent(): SOAPObject
         toString(): string
-        val(value)
+        val(value: string): SOAPObject
+        val(value: number): SOAPObject
     }
     interface Options {
-    		appendMethodToURL?: boolean;
-    		async?: boolean;
-    		beforeSend?: (SOAPEnvelope: SOAPEnvelope) => void;
-            context?: any;
-            data?: Object;
-            envAttributes?: any;
-            elementName?: string;
-            enableLogging?: boolean;
-            error?: (SOAPResponse: SOAPResponse) => void;
-            HTTPHeaders?: any;
-            method?: string;
-            namespaceQualifier?: string;
-            namespaceURL?: string;
-            noPrefix?: boolean;
-            request?: (SOAPEnvelope: SOAPEnvelope) => void;
-            soap12?: boolean;
-            SOAPAction?: string;
-            SOAPHeader?: any;
-            statusCode?: any;
-            success?: (SOAPResponse: SOAPResponse) => void;
-            url?: string;
-            wss?: any;
+        appendMethodToURL?: boolean;
+        async?: boolean;
+        beforeSend?: (SOAPEnvelope: SOAPEnvelope) => void;
+        context?: any;
+        data?: Object;
+        envAttributes?: Object;
+        elementName?: string;
+        enableLogging?: boolean;
+        error?: (SOAPResponse: SOAPResponse) => void;
+        HTTPHeaders?: Object;
+        method?: string;
+        namespaceQualifier?: string;
+        namespaceURL?: string;
+        noPrefix?: boolean;
+        request?: (SOAPEnvelope: SOAPEnvelope) => void;
+        soap12?: boolean;
+        SOAPAction?: string;
+        SOAPHeader?: Object;
+        statusCode?: Object;
+        success?: (SOAPResponse: SOAPResponse) => void;
+        url?: string;
+        wss?: Object;
     }
     interface SOAP {
         (options?: Options): JQueryXHR;
