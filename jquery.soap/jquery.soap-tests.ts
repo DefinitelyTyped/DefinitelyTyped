962946746c4d2e6d44294908c498f222ed8877b1
tigerxy <roland@stempelgreim.de>
2015-09-20 19:02:43 +0200

    soap tests updated

--- jquery.soap/jquery.soap-tests.ts
+++ jquery.soap/jquery.soap-tests.ts
@@ -1,6 +1,26 @@
 /// <reference path="jquery.soap.d.ts"/>
 
 $.soap({
+    url: 'http://my.server.com/soapservices/',
+    method: 'helloWorld',
+
+    data: {
+        name: 'Remy Blom',
+        msg: 'Hi!'
+    },
+
+    success: function (soapResponse) {
+        // do stuff with soapResponse
+        // if you want to have the response as JSON use soapResponse.toJSON();
+        // or soapResponse.toString() to get XML string
+        // or soapResponse.toXML() to get XML DOM
+    },
+    error: function (SOAPResponse) {
+        // show error
+    }
+});
+
+$.soap({
     url: 'http://my.server.com/soapservices/',      //endpoint address for the service
     method: 'helloWorld',                           // service operation name
     // 1) will be appended to url if appendMethodToURL=true
@@ -54,4 +74,223 @@ $.soap({
 
     // debugging
     enableLogging: false                            // to enable the local log function set to true, defaults to false (optional)
+})
+
+$.soap({
+    
+}).done(function(data, textStatus, jqXHR) {
+    // do stuff on success here...
+}).fail(function(jqXHR, textStatus, errorThrown) {
+    // do stuff on error here...
+})
+
+$.soap({
+    url: 'http://my.server.com/soapservices/',
+    namespaceQualifier: 'myns',
+    namespaceURL: 'urn://service.my.server.com',
+    error: function (soapResponse) {
+        // show error
+    }
+});
+
+$.soap({
+    method: 'helloWorld',
+    data: {
+        name: 'Remy Blom',
+        msg: 'Hi!'
+    },
+    success: function (soapResponse) {
+        // do stuff with soapResponse
+    }
+});
+
+$.soap({
+    method: 'doSomethingElse',
+    data: {},
+    success: function (soapResponse) {
+        // do stuff with soapResponse
+    }
+});
+
+$.soap({
+    url: 'http://another.server.com/anotherService',
+    method: 'helloWorld',
+    data: {
+        name: 'Remy Blom',
+        msg: 'Hi!'
+    },
+    success: function (soapResponse) {
+        // do stuff with soapResponse
+    },
+    error: function (soapResponse) {
+        alert('that other server might be down...')
+    }
+});
+
+$.soap({
+    // other parameters..
+
+    // WS-Security
+    wss: {
+        username: 'user',
+        password: 'pass',
+        nonce: 'w08370jf7340qephufqp3r4',
+        created: new Date().getTime()
+    }
+});
+
+var username = 'foo';
+var password = 'bar';
+
+$.soap({
+    // other parameters...
+
+    HTTPHeaders: {
+        Authorization: 'Basic ' + btoa(username + ':' + password)
+    }
+});
+
+// jquery.soap/doc/options.md
+
+$.soap({
+    url: 'http://server.com/webServices/',
+    method: 'getItem',
+    appendMethodToURL: false
+})
+
+$.soap({
+    beforeSend: function(SOAPEnvelope) {
+        console.log(SOAPEnvelope.toString());
+    }
+});
+
+$.soap({
+    context: document.body,
+    success: function(SOAPResponse) {
+        console.log(this);
+    }
+});
+
+var xml =
+    ['<soap:Envelope xmlns:soap="http://www.w3.org/2003/05/soap-envelope/">',
+        '<soap:Body>',
+            '<requestNode>',
+            '</requestNode>',
+        '</soap:Body>',
+    '</soap:Envelope>'];
+
+$.soap({
+    data: xml.join('')
+});
+
+$.soap({
+    method: 'requestNode',
+    data: {
+        name: 'Remy Blom',
+        msg: 'Hi!'
+    }
+});
+
+$.soap({
+    envAttributes: {
+        'xmlns:another': 'http://anotherNamespace.com/'
+    }
+})
+
+$.soap({
+    method: 'helloWorld',
+    elementName: 'requestNode'
+})
+
+$.soap({
+    enableLoggin: true
+})
+
+$.soap({
+    error: function(SOAPResponse) {
+        console.log(SOAPResponse.toString())
+    }
+})
+
+$.soap({
+    HTTPHeaders: {
+        'Authorization': 'Basic ' + btoa('user:pass')
+    }
+})
+
+$.soap({
+    url: 'http://server.com/webServices/',
+    method: 'getItem'
+})
+
+$.soap({
+    method: 'helloWorld',
+    namespaceQualifier: 'myns',
+    namespaceURL: 'urn://service.my.server.com'
+})
+
+$.soap({
+    method: 'helloWorld',
+    namespaceQualifier: 'myns',
+    namespaceURL: 'urn://service.my.server.com'
+})
+
+$.soap({
+    method: 'helloWorld',
+    namespaceQualifier: 'myns',
+    namespaceURL: 'urn://service.my.server.com',
+    noPrefix: true
+})
+
+$.soap({
+    request: function(SOAPEnvelope) {
+        console.log(SOAPEnvelope.toString());
+    }
+})
+
+$.soap({
+    soap12: true
+})
+
+$.soap({
+    url: 'http://server.com/webServices/',
+    method: 'getItem',
+    SOAPAction: 'getAnItem'
+})
+
+$.soap({
+    SOAPHeader: {
+        test: [1,2,3]
+    }
+})
+
+$.soap({
+    statusCode: {
+        404: function() {
+            console.log('404 Not Found')
+        },
+        200: function() {
+            console.log('200 OK')
+        }
+    }
+})
+
+$.soap({
+    success: function(SOAPResponse) {
+        console.log(SOAPResponse.toString());
+    }
+})
+
+$.soap({
+    url: 'http://server.com/webServices/',
+    method: 'getItem'
+})
+
+$.soap({
+    wss: {
+        username: 'user',
+        password: 'pass',
+        nonce: 'w08370jf7340qephufqp3r4',
+        created: new Date().getTime()
+    }
 })
