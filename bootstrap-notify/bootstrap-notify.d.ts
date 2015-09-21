c5d9e6d2d237ccaa9397015bb97d3354bd7ad1d0
robert-voica <robert.voica@equilobe.com>
2015-09-21 17:44:00 +0300

    Removed new lines between names

--- bootstrap-notify/bootstrap-notify.d.ts
+++ bootstrap-notify/bootstrap-notify.d.ts
@@ -1,8 +1,6 @@
 // Type definitions for bootstrap-notify v3.1.3
 // Project: http://bootstrap-notify.remabledesigns.com/
-// Definitions by: 	Blake Niemyjski <https://github.com/niemyjski/>, 
-//					Robert McIntosh <https://github.com/mouse0270>, 
-//					Robert Voica <https://github.com/robert-voica>
+// Definitions by: 	Blake Niemyjski <https://github.com/niemyjski/>, Robert McIntosh <https://github.com/mouse0270>, Robert Voica <https://github.com/robert-voica>
 // Definitions: https://github.com/borisyankov/DefinitelyTyped
 
 /// <reference path="../jquery/jquery.d.ts" />
