fcd91d68c8a4a72b5732c7ec5dc04010f888f89f
benliddicott <ben@liddicott.com>
2015-09-21 11:37:37 +0100

    Adding Ejs and static-eval

--- static-eval/static-eval.d.ts
+++ static-eval/static-eval.d.ts
@@ -3,8 +3,14 @@
 // Definitions by: Ben Liddicott <https://github.com/benliddicott/DefinitelyTyped>
 // Definitions: https://github.com/borisyankov/DefinitelyTyped
  
+/// <reference path="../esprima/esprima.d.ts" />
 
 declare module 'static-eval' {
-    function evaluate(ast, vars: { [name: string]: any });
+    /**
+    * Evaluates the given ESTree.Expression, with the given named variables in place.
+    * @param ast [ESTree.Expression] An esprima expression derived from parse.body[].expression
+    * @param vars Named variables, objects or functions which may be referenced in the expression.
+    */
+    function evaluate(ast : ESTree.Expression, vars: { [name: string]: any }): any;
     export =evaluate;
 }
