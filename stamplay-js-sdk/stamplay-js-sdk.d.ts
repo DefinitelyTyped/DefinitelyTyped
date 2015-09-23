9f3c3628ee7245a1a4543f2002e74cfc50384209
ridermansb <ridermansb@gmail.com>
2015-09-23 07:52:56 -0300

    Fix linebreak

--- stamplay-js-sdk/stamplay-js-sdk.d.ts
+++ stamplay-js-sdk/stamplay-js-sdk.d.ts
@@ -1,5 +1,5 @@
 // Type definitions for stamplay-js-sdk 1.2.9
-// Project: https://github.com/Stamplay/stamplay-js-sdk and https://stamplay.com/docs/jssdk#api-ref-model
+// Project: https://github.com/Stamplay/stamplay-js-sdk
 // Definitions by: Riderman de Sousa Barbosa <https://github.com/ridermansb/>
 // Definitions: https://github.com/borisyankov/DefinitelyTyped
 
