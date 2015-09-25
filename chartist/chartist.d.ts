86cdeadf6a58c467e8c96037e989c515d4f8b72b
Matt Gibbs <matt.gibbs@hp.com>
2015-09-25 11:23:30 -0400

    Adding stub of chartist.d.ts

---
+++ chartist/chartist.d.ts
@@ -0,0 +1,8 @@
+﻿// Type definitions for Chartist v0.9.4
+// Project: https://github.com/gionkunz/chartist-js
+// Definitions by: Matt Gibbs <https://github.com/mtgibbs>
+// Definitions: https://github.com/borisyankov/DefinitelyTyped
+
+declare module Chartist {
+
+}
