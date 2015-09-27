68245feb78b78db40e1b8d8b9b65bee099570f08
Ilya Mochalov <chrootsu@gmail.com>
2015-09-27 11:32:08 +0500

    lodash: added _.matches() method

--- lodash/lodash.d.ts
+++ lodash/lodash.d.ts
@@ -8449,6 +8449,38 @@ declare module _ {
         iteratee<TResult>(thisArg?: any): LoDashObjectWrapper<(...args: any[]) => TResult>;
     }
 
+    //_.matches
+    interface LoDashStatic {
+        /**
+         * Creates a function that performs a deep comparison between a given object and source, returning true if the
+         * given object has equivalent property values, else false.
+         *
+         * Note: This method supports comparing arrays, booleans, Date objects, numbers, Object objects, regexes, and
+         * strings. Objects are compared by their own, not inherited, enumerable properties. For comparing a single own
+         * or inherited property value see _.matchesProperty.
+         *
+         * @param source The object of property values to match.
+         * @return Returns the new function.
+         */
+        matches<T>(
+            source: T
+        ): (value: any) => boolean;
+
+        /**
+         * @see _.matches
+         */
+        matches<T, V>(
+            source: T
+        ): (value: V) => boolean;
+    }
+
+    interface LoDashWrapperBase<T, TWrapper> {
+        /**
+         * @see _.matches
+         */
+        matches<V>(): LoDashObjectWrapper<(value: V) => boolean>;
+    }
+
     //_.method
     interface LoDashStatic {
         /**
