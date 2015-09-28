3f7e0fe145c439e17450a1952c6f78b4f5b55efe
Abner Oliveira <abner.silva@gmail.com>
2015-09-27 21:08:47 -0300

    added rosie-tests and comments for methods from project

--- rosie/rosie-tests.ts
+++ rosie/rosie-tests.ts
@@ -11,6 +11,22 @@ resultObj = Factory.build('person');
 resultFactory = Factory.define('some').sequence('id').attr('name', ['id'], (id: number) => { return 'Name ' + id.toString() });
 resultObj = Factory.build('some');
 
+Factory.define('coach')
+  .option('buildPlayer', false)
+  .sequence('id')
+  .attr('players', ['id', 'buildPlayer'], function(id: any, buildPlayer: boolean) {
+    if (buildPlayer) {
+      return [Factory.build('player', {coach_id: id})];
+    }
+  })
+  .after(function(coach: any, options: any) {
+    if (options.buildPlayer) {
+      console.log('built player:', coach.players[0]);
+    }
+  });
 
+Factory.build('coach', {}, {buildPlayer: true});
 
-var rosieFactory = require('rosie').Factory;
+import rosie = require('rosie');
+
+var Factory = rosie.Factory;
