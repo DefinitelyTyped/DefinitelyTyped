4c2111924e40d46dff132817a6fc72cb93a8fd07
jass0 <jass0@netti.fi>
2015-09-30 14:11:12 +0300

    Missing customTooltips.

--- chartjs/chart.d.ts
+++ chartjs/chart.d.ts
@@ -55,6 +55,7 @@ interface ChartSettings {
     maintainAspectRatio: boolean;
     showTooltips: boolean;
     tooltipEvents: string[];
+    customTooltips: boolean | ((tooltip: any) => any);
     tooltipFillColor: string;
     tooltipFontFamily: string;
     tooltipFontSize: number;
