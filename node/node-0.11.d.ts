62c69369dfac8c3b3e34602b7c8a147f828b7d1b
Ryan Schmukler <rschmukler@gmail.com>
2015-09-30 19:54:54 -0400

    add objectMode to node stream.WritableOptions
    references #5658, references #5774

--- node/node-0.11.d.ts
+++ node/node-0.11.d.ts
@@ -1097,6 +1097,7 @@ declare module "crypto" {
     interface Verify extends NodeJS.WritableStream {
         update(data: any): void;
         verify(object: string, signature: string, signature_format?: string): boolean;
+        objectMode?: boolean;
     }
     export function createDiffieHellman(prime_length: number): DiffieHellman;
     export function createDiffieHellman(prime: number, encoding?: string): DiffieHellman;
@@ -1151,6 +1152,7 @@ declare module "stream" {
     export interface WritableOptions {
         highWaterMark?: number;
         decodeStrings?: boolean;
+        objectMode?: boolean;
     }
 
     export class Writable extends events.EventEmitter implements NodeJS.WritableStream {
