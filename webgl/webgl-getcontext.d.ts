95ba4f6de502df06741795c62b0d52b561bed537
Shane S. Anderson <s@ander.nz>
2015-10-02 07:39:13 +1300

    Added all getContext() overloads into webgl-getcontext.d.ts

--- webgl/webgl-getcontext.d.ts
+++ webgl/webgl-getcontext.d.ts
@@ -2,3 +2,28 @@
 // Project: http://webgl.org/
 // Definitions by: Shane S. Anderson <https://github.com/ander-nz/>
 // Definitions: https://github.com/borisyankov/DefinitelyTyped/webgl
+
+interface HTMLCanvasElement {
+  // https://developer.mozilla.org/en-US/docs/Web/API/HTMLCanvasElement/getContext
+  getContext(contextId: "2d", attributes?: CanvasContextAttributes): CanvasRenderingContext2D;
+  getContext(contextId: "experimental-webgl", attributes?: WebGLContextAttributes): WebGLRenderingContext;
+  getContext(contextId: "webgl", attributes?: WebGLContextAttributes): WebGLRenderingContext;
+  getContext(contextId: "experimental-webgl2", attributes?: WebGLContextAttributes): WebGL2RenderingContext;
+  getContext(contextId: "webgl2", attributes?: WebGLContextAttributes): WebGL2RenderingContext;
+  getContext(contextId: string, ...args: any[]): CanvasRenderingContext2D | WebGLRenderingContext | WebGL2RenderingContext;
+}
+
+interface CanvasContextAttributes {
+  // https://developer.mozilla.org/en-US/docs/Web/API/HTMLCanvasElement/getContext
+  alpha?: boolean;
+}
+
+interface WebGLContextAttributes {
+  // https://www.khronos.org/registry/webgl/specs/latest/1.0/index.html#WEBGLCONTEXTATTRIBUTES
+  preferLowPowerToHighPerformance?: boolean;
+  failIfMajorPerformanceCaveat?: boolean;
+}
+
+interface WebGL2RenderingContext {
+  // Defined in a separate definition file.
+}
