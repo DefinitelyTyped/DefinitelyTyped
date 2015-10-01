5a448d7c07168af8a7de87e76223061ce8192499
Shane S. Anderson <s@ander.nz>
2015-10-02 07:21:23 +1300

    Setup file templates for getcontext fixes

---
+++ webgl/webgl-getcontext-tests.ts
@@ -0,0 +1,1 @@
+/// <reference path="webgl-getcontext.d.ts" />
---
+++ webgl/webgl-getcontext.d.ts
@@ -0,0 +1,4 @@
+// Type definitions for WebGL
+// Project: http://webgl.org/
+// Definitions by: Shane S. Anderson <https://github.com/ander-nz/>
+// Definitions: https://github.com/borisyankov/DefinitelyTyped/webgl
