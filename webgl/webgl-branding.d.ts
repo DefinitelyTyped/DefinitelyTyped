97083913a300887803aa8a176d1b6342086d7112
Shane S. Anderson <s@ander.nz>
2015-10-02 08:04:39 +1300

    Added branding to all empty WebGL interfaces

--- webgl/webgl-branding.d.ts
+++ webgl/webgl-branding.d.ts
@@ -2,3 +2,20 @@
 // Project: http://webgl.org/
 // Definitions by: Shane S. Anderson <https://github.com/ander-nz/>
 // Definitions: https://github.com/borisyankov/DefinitelyTyped/webgl
+
+// WebGL types
+interface WebGLObject { __WebGLObject: void; }
+interface WebGLBuffer { __WebGLBuffer: void; }
+interface WebGLFramebuffer { __WebGLFramebuffer: void; }
+interface WebGLProgram { __WebGLProgram: void; }
+interface WebGLRenderbuffer { __WebGLRenderbuffer: void; }
+interface WebGLShader { __WebGLShader: void; }
+interface WebGLTexture { __WebGLTexture: void; }
+interface WebGLUniformLocation { __WebGLUniformLocation: void; }
+
+// WebGL2 types
+interface WebGLQuery { __WebGLQuery: void; }
+interface WebGLSampler { __WebGLSampler: void; }
+interface WebGLSync { __WebGLSync: void; }
+interface WebGLTransformFeedback { __WebGLTransformFeedback: void; }
+interface WebGLVertexArrayObject { __WebGLVertexArrayObject: void; }
