866696a54d6a5376a6749e32eb09116af90249f4
Shane S. Anderson <s@ander.nz>
2015-10-02 14:26:57 +1300

    Added gl-matrix.d.ts as wrapper that populates global scope.

---
+++ gl-matrix/gl-matrix.d.ts
@@ -0,0 +1,15 @@
+// Type definitions for gl-matrix 2.2.2
+// Project: https://github.com/toji/gl-matrix
+// Definitions by: Tat <https://github.com/tatchx>
+// Definitions: https://github.com/borisyankov/DefinitelyTyped
+
+/// <reference path="gl-matrix-module.d.ts" />
+import glMatrix = GLM.glMatrix;
+import vec2 = GLM.vec2;
+import vec3 = GLM.vec3;
+import vec4 = GLM.vec4;
+import mat2 = GLM.mat2;
+import mat2d = GLM.mat2d;
+import mat3 = GLM.mat3;
+import mat4 = GLM.mat4;
+import quat = GLM.quat;
