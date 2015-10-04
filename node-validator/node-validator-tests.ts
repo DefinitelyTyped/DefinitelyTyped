c6fb4cf771a99e1761dcf23fef699eaf9262306d
Ken Gorab <ken.gorab@gmail.com>
2015-10-04 00:30:48 -0400

    Added node-validator type definitions

---
+++ node-validator/node-validator-tests.ts
@@ -0,0 +1,23 @@
+/// <reference path="node-validator.d.ts"/>
+
+import { run, isObject, isString, isIsoDate, isArray } from 'node-validator';
+
+let checkChild = isObject()
+  .withRequired('prop', isString({ regex: /^[abc]+$/ }));
+
+let check = isObject()
+  .withRequired('_id', isString({ regex: /^[abc]+$/ }))
+  .withOptional('date', isIsoDate())
+  .withOptional('children', isArray(checkChild, {min: 1}));
+
+let toValidate = {
+  _id: 'abababa',
+  date: '2013-10-24',
+  children: [{
+    prop: 'zxzx'
+  }]
+};
+
+run(check, toValidate, function(errorCount, errors) {
+
+});
