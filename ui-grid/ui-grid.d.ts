c10edbda62a992751e1f189d2d2b7dad1a4188aa
Ashwin <ashwin.kumar@phx-w-qbkuas.corp.trizetto.com>
2015-10-07 11:01:20 -0700

    Added missing options for infinite scroll
    infiniteScrollRowsFromEnd, infiniteScrollUp and infiniteScrollDown were
    missing in IGridOptions. These were added.

--- ui-grid/ui-grid.d.ts
+++ ui-grid/ui-grid.d.ts
@@ -2087,6 +2087,25 @@ declare module uiGrid {
              * @default true
              */
             enableInfiniteScroll?: boolean;
+            /**
+             * Number of rows from the end of the dataset
+             * at which infinite scroll will trigger a request
+             * for more data
+             * @default 20
+             */
+            infiniteScrollRowsFromEnd?: number;
+            /**
+             * Inform the grid of whether there are rows
+             * to load when scrolling up
+             * @default false
+             */
+            infiniteScrollUp?: boolean,
+            /**
+             * Inform the grid of whether there are rows
+             * to load scrolling down
+             * @default true
+             */
+            infiniteScrollDown?: boolean,
         }
 
         /**
