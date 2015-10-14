41e23e807eb58ddb0731ad1cc33fd641f7bec822
Dominic Alie <dominic@georadix.com>
2015-10-14 11:32:05 -0400

    Leaflet.DomMarkers type definitions

---
+++ leaflet-dom-markers/leaflet-dom-markers.d.ts
@@ -0,0 +1,19 @@
+﻿// Type definitions for Leaflet.DomMarkers
+// Project: https://github.com/ValentinH/Leaflet.DomMarkers
+// Definitions by: Dominic Alie <https://github.com/dalie>
+// Definitions: https://github.com/borisyankov/DefinitelyTyped
+
+/// <reference path="../leaflet/leaflet.d.ts" />
+
+declare module L {
+    module DomMarkers {
+        export interface Icon extends DivIcon {
+        }
+
+        export function icon(options: IDomMarkerOptions): Icon;
+
+        export interface IDomMarkerOptions extends DivIconOptions {
+            element: HTMLElement;
+        }
+    }
+}
