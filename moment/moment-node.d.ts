d8cc83b91564500ed43ca45484282f4ff35c476b
Jared Klopper <spuzzy2@hotmail.com>
2015-10-14 13:39:09 +1300

    Add object parsing overload to moment setter

--- moment/moment-node.d.ts
+++ moment/moment-node.d.ts
@@ -1,4 +1,4 @@
-// Type definitions for Moment.js 2.8.0
+// Type definitions for Moment.js 2.10.6
 // Project: https://github.com/timrwood/moment
 // Definitions by: Michael Lakerveld <https://github.com/Lakerfield>, Aaron King <https://github.com/kingdango>, Hiroki Horiuchi <https://github.com/horiuchi>, Dick van den Brink <https://github.com/DickvdBrink>, Adi Dahiya <https://github.com/adidahiya>, Matt Brooks <https://github.com/EnableSoftware>
 // Definitions: https://github.com/borisyankov/DefinitelyTyped
@@ -303,6 +303,7 @@ declare module moment {
 
         get(unit: string): number;
         set(unit: string, value: number): Moment;
+        set(input: MomentInput): Moment;
     }
 
     type formatFunction = () => string;
