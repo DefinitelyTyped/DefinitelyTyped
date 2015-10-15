55e787adbe023ae0835590559e72a3d350eab6bb
Rob Howard <robmhoward@hotmail.com>
2015-10-15 16:30:42 -0700

    Moving header above license
    Travis failed due to headers appearing below license information.  Moved
    it up.

--- office-js/office-js.d.ts
+++ office-js/office-js.d.ts
@@ -1,3 +1,8 @@
+// Type definitions for Office.js
+// Project: http://dev.office.com
+// Definitions by: OfficeDev <https://github.com/OfficeDev>
+// Definitions: https://github.com/borisyankov/DefinitelyTyped
+
 /*
 ------------------------------------------ START OF LICENSE -----------------------------------------
 office-js
@@ -7,12 +12,6 @@ The above copyright notice and this permission notice shall be included in all c
 THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 ----------------------------------------------- END OF LICENSE ------------------------------------------
 */
-
-// Type definitions for Office.js
-// Project: http://dev.office.com
-// Definitions by: OfficeDev <https://github.com/OfficeDev>
-// Definitions: https://github.com/borisyankov/DefinitelyTyped
-
 
 declare module Office {
     export var context: Context;
