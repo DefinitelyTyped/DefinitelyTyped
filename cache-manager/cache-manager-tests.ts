28e22adec9c204615200b51406394fab840c2f95
Simon Gausmann <Simon.Gausmann@live.de>
2015-10-21 20:45:16 +0200

    removing I from Interface-Names

--- cache-manager/cache-manager-tests.ts
+++ cache-manager/cache-manager-tests.ts
@@ -2,10 +2,10 @@
 
 import * as cacheManager from 'cache-manager'
 
-const memoryCache = cacheManager.caching({store: 'memory', max: 100, ttl: 10/*seconds*/});
+const memoryCache = cacheManager.caching({ store: 'memory', max: 100, ttl: 10/*seconds*/ });
 const ttl = 5;
 
-memoryCache.set('foo', 'bar', {ttl: ttl}, (err) => {
+memoryCache.set('foo', 'bar', { ttl: ttl }, (err) => {
 
     if (err) {
         throw err;
@@ -21,25 +21,25 @@ memoryCache.set('foo', 'bar', {ttl: ttl}, (err) => {
     });
 });
 
-function getUser(id:number, cb:Function) {
+function getUser(id: number, cb: Function) {
 
-    cb(null, {id: id, name: 'Bob'});
+    cb(null, { id: id, name: 'Bob' });
 }
 
 const userId = 123;
 const key = 'user_' + userId;
 
 // Note: ttl is optional in wrap()
-memoryCache.wrap<{id: number, name: string}>(key, (cb) => {
+memoryCache.wrap<{ id: number, name: string }>(key, (cb) => {
 
     getUser(userId, cb);
 
-}, {ttl: ttl}, (err, user) => {
+}, { ttl: ttl }, (err, user) => {
 
     //console.log(user);
 
     // Second time fetches user from memoryCache
-    memoryCache.wrap<{id: number, name: string}>(key, (cb)=> {
+    memoryCache.wrap<{ id: number, name: string }>(key, (cb) => {
 
         getUser(userId, cb);
 
