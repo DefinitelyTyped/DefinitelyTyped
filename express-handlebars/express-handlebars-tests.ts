f57805d719c7616b38755a31dd70f03d9883431c
Sam Saint-Pettersen <s.stpettersen+github@gmail.com>
2015-10-22 17:25:22 +0100

    Update express-handlebars-tests.ts

--- express-handlebars/express-handlebars-tests.ts
+++ express-handlebars/express-handlebars-tests.ts
@@ -6,10 +6,12 @@ import express = require('express');
 import exphbs = require('express-handlebars');
 
 var app = express();
-app.engine('handlebars', exphbs({defaultLayout: 'main'}));
+var hbs: Exphbs = exphbs.create({defaultLayout: 'main'});
+
+app.engine('handlebars', hbs.engine);
 app.set('view engine', 'handlebars');
 
 app.listen(1337);
-console.log('Started dummy server on port 1337...');
+console.log('Test Express Handlebars app on port 1337..');
 console.log('Done');
 process.exit(0);
