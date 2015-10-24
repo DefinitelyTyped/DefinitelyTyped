8243b02a3d5a130740a4322c3927644b9818f975
Ilya Mochalov <chrootsu@gmail.com>
2015-10-24 15:39:12 +0500

    lodash: signatures of the method _.pad changed

--- lodash/lodash.d.ts
+++ lodash/lodash.d.ts
@@ -9078,23 +9078,42 @@ declare module _ {
         kebabCase(): string;
     }
 
+    //_.pad
     interface LoDashStatic {
         /**
+         * Pads string on the left and right sides if it’s shorter than length. Padding characters are truncated if
+         * they can’t be evenly divided by length.
          *
          * @param string The string to pad.
          * @param length The padding length.
          * @param chars The string used as padding.
          * @return Returns the padded string.
          */
-        pad(string?: string, length?: number, chars?: string): string;
+        pad(
+            string?: string,
+            length?: number,
+            chars?: string
+        ): string;
     }
 
-    //_.pad
     interface LoDashImplicitWrapper<T> {
         /**
          * @see _.pad
          */
-        pad(length?: number, chars?: string): string;
+        pad(
+            length?: number,
+            chars?: string
+        ): string;
+    }
+
+    interface LoDashExplicitWrapper<T> {
+        /**
+         * @see _.pad
+         */
+        pad(
+            length?: number,
+            chars?: string
+        ): LoDashExplicitWrapper<string>;
     }
 
     //_.padLeft
