8ccd5a495272bec853075c3fe6bb1fb650e7184c
Dmitri Suvorov <suvjunmd@gmail.com>
2015-10-25 01:35:41 +0300

    Updated angular-sanitize.d.ts to version 1.4

--- angularjs/angular-sanitize.d.ts
+++ angularjs/angular-sanitize.d.ts
@@ -1,8 +1,7 @@
-// Type definitions for Angular JS 1.3 (ngSanitize module)
+// Type definitions for Angular JS 1.4 (ngSanitize module)
 // Project: http://angularjs.org
-// Definitions by: Diego Vilar <http://github.com/diegovilar>
+// Definitions by: Diego Vilar <http://github.com/diegovilar>, Dmitri Suvorov <https://github.com/suvjunmd>
 // Definitions: https://github.com/borisyankov/DefinitelyTyped
-
 
 /// <reference path="angular.d.ts" />
 
@@ -18,22 +17,32 @@ declare module angular.sanitize {
 
     ///////////////////////////////////////////////////////////////////////////
     // SanitizeService
-    // see http://docs.angularjs.org/api/ngSanitize.$sanitize
+    // see https://code.angularjs.org/1.4.7/docs/api/ngSanitize/service/$sanitize
     ///////////////////////////////////////////////////////////////////////////
     interface ISanitizeService {
+        /**
+        * The input is sanitized by parsing the HTML into tokens. All safe tokens (from a whitelist) are then serialized back to properly escaped html string. This means that no unsafe input can make it into the returned string, however, since our parser is more strict than a typical browser parser, it's possible that some obscure input, which would be recognized as valid HTML by a browser, won't make it through the sanitizer. The input may also contain SVG markup. The whitelist is configured using the functions aHrefSanitizationWhitelist and imgSrcSanitizationWhitelist of $compileProvider.
+        *
+        * @param html HTML input.
+        * @return Sanitized HTML.
+        */
         (html: string): string;
     }
 
     ///////////////////////////////////////////////////////////////////////////
     // Filters included with the ngSanitize
-    // see https://github.com/angular/angular.js/tree/v1.2.0/src/ngSanitize/filter
+    // see https://code.angularjs.org/1.4.7/docs/api/ngSanitize/filter
     ///////////////////////////////////////////////////////////////////////////
     export module filter {
 
-        // Finds links in text input and turns them into html links.
-        // Supports http/https/ftp/mailto and plain email address links.
-        // see http://code.angularjs.org/1.2.0/docs/api/ngSanitize.filter:linky
         interface ILinky {
+            /**
+            * Finds links in text input and turns them into html links. Supports http/https/ftp/mailto and plain email address links.
+            *
+            * @param text Input text.
+            * @param target Window (_blank|_self|_parent|_top) or named frame to open links in.
+            * @return Html-linkified text.
+            */
             (text: string, target?: string): string;
         }
     }
