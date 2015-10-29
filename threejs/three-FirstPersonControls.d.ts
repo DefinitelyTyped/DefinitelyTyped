f55e5666a28ecee6941dda592a903b1ff5aa0c9d
Poul Kjeldager Sørensen <pks@s-innovations.net>
2015-10-29 12:38:34 +0100

    fixed build errors from copied source

--- threejs/three-FirstPersonControls.d.ts
+++ threejs/three-FirstPersonControls.d.ts
@@ -1,19 +1,22 @@
-Source : https://github.com/NTaylorMullen/CycleR/blob/master/CycleR/CycleR.Game.Client/Client/Interfaces/ThreeJS/Cameras/FirstPersonControls.d.ts
+//Source : https://github.com/NTaylorMullen/CycleR/blob/master/CycleR/CycleR.Game.Client/Client/Interfaces/ThreeJS/Cameras/FirstPersonControls.d.ts
+// Definitions: https://github.com/borisyankov/DefinitelyTyped
+
+/// <reference path="./three.d.ts" />
 
 interface IFirstPersonControls {
-    object: IObject3D;
-    target: IVector3;
+    object: Object3D;
+    target: Vector3;
     domElement: HTMLCanvasElement;
     movementSpeed: number;
     lookSpeed: number;
-    noFly: bool;
-    lookVertical: bool;
-    autoForward: bool;
-    activeLook: bool;
-    heightSpeed: bool;
-    heightCoef: number;
-    heightMin: number;
-    constrainVertical: bool;
+    noFly: boolean;
+    lookVertical: boolean;
+    autoForward: boolean;
+    activeLook: boolean;
+    heightSpeed: boolean;
+    heightCoef: boolean;
+    heightMin: boolean;
+    constrainVertical: boolean;
     verticalMin: number;
     verticalMax: number;
     autoSpeedFactor: number;
@@ -23,11 +26,11 @@ interface IFirstPersonControls {
     lon: number;
     phi: number;
     theta: number;
-    moveForward: bool;
-    moveBackward: bool;
-    moveLeft: bool;
-    moveRight: bool;
-    freeze: bool;
-    mouseDragOn: bool;
+    moveForward: boolean;
+    moveBackward: boolean;
+    moveLeft: boolean;
+    moveRight: boolean;
+    freeze: boolean;
+    mouseDragOn: boolean;
     update(delta?: number): void;
 }
