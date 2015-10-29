fbbd88a8684b3f72059b3518f4b6d65d2cb9d312
Poul Kjeldager Sørensen <pks@s-innovations.net>
2015-10-29 14:21:21 +0100

    fixed header

--- threejs/three-EarthControls.d.ts
+++ threejs/three-EarthControls.d.ts
@@ -1,4 +1,6 @@
 // Type definitions for three.js
+// Project: http://mrdoob.github.com/three.js/
+// Definitions by: Poul Kjeldager Sørensen
 // Definitions: https://github.com/borisyankov/DefinitelyTyped
 
 /// <reference path="./three.d.ts" />
