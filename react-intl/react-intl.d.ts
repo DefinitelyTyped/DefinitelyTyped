ae0a630136d183297da9a14d11efcdcfdac333b4
cdroulers <droulers.christian@gmail.com>
2015-10-29 08:18:18 -0400

    Add definition for React Intl 2.0.0-pr3.
    Kept backwards compatibility with 1.2.0 by having two files.
    
    Test files is fairly complete!

--- react-intl/react-intl.d.ts
+++ react-intl/react-intl.d.ts
@@ -1,99 +1,239 @@
-// Type definitions for react-intl 1.2.0
+// Type definitions for react-intl 2.0.0-beta1
 // Project: http://formatjs.io/react/
-// Definitions by: Bruno Grieder <https://github.com/bgrieder>
+// Definitions by: Bruno Grieder <https://github.com/bgrieder>, Christian Droulers <https://github.com/cdroulers>
 // Definitions: https://github.com/borisyankov/DefinitelyTyped
 
 ///<reference path='../react/react.d.ts' />
 
-declare module "react-intl" {
+declare module ReactIntl {
+    import React = __React;
 
-    import * as React from 'react'
-
-    module ReactIntl {
-
-
-        interface IIntlMixin extends React.Mixin<any,any> {
-            getIntlMessage(key: string): string
-        }
-
-        var IntlMixin: IIntlMixin
-
-        module IntlComponent {
-            interface Props {
-                locales?: string[]
-                messages?: {[key: string]: any}
-                formats?: string[]
-            }
-        }
-        interface IntlComponent {
-            getIntlMessage(key: string): string;
-        }
-
-
-        module FormattedDate {
-            export interface Props extends IntlComponent.Props {
-                value: Date
-                day?: string
-                month?: string
-                year?: string
-            }
-        }
-        class FormattedDate extends React.Component<FormattedDate.Props,any> {}
-
-
-        module FormattedTime {
-            export interface Props extends IntlComponent.Props  {
-                value: Date
-                day?: string
-                month?: string
-                year?: string
-                format?: string
-            }
-        }
-        class FormattedTime extends React.Component<FormattedTime.Props,any> {}
-
-
-        module FormattedRelative {
-            export interface Props extends IntlComponent.Props  {
-                value: number
-                units?: string //"second", "minute", "hour", "day", "month" or "year"
-                style?: string //"best fit" (default) or "numeric"
-                format?: string
-            }
-        }
-        class FormattedRelative extends React.Component<FormattedRelative.Props,any> {}
-
-
-        module FormattedMessage {
-            export interface Props extends IntlComponent.Props  {
-                message: string;
-                [prop: string]: any
-            }
-        }
-        class FormattedMessage extends React.Component<FormattedMessage.Props,any> {}
-
-
-        module FormattedHTMLMessage {
-            export interface Props extends IntlComponent.Props  {
-                message: string;
-                [prop: string]: any
-            }
-        }
-        class FormattedHTMLMessage extends React.Component<FormattedHTMLMessage.Props,any> {}
-
-
-        module FormattedNumber {
-            export interface Props extends IntlComponent.Props  {
-                value: number
-                style?: string
-                currency?: string
-                format?: string
-            }
-        }
-        class FormattedNumber extends React.Component<FormattedNumber.Props,any> {}
-
+    interface Locale {
+        locale: string;
+        fields?: { [key: string]: string },
+        pluralRuleFunction?: (n: number, ord: boolean) => string;
     }
 
-    export  = ReactIntl
+    function injectIntl<T>(clazz: T): T;
 
+    function addLocaleData(data: Locale[] | Locale): void;
+
+    function hasLocaleData(localeName: string): boolean;
+
+    interface Messages {
+        [key: string]: FormattedMessage.MessageDescriptor
+    }
+
+    function defineMessages<T extends Messages>(messages: Messages): T;
+
+    interface IntlShape extends React.Requireable<any> {
+    }
+
+    var intlShape: IntlShape;
+
+    interface FormatConfig {
+        locale?: string;
+        formats?: any;
+    }
+
+    interface InjectedIntlProps {
+        formatDate?: (date: Date, options?: FormattedDate.PropsBase) => string;
+        formatTime?: (date: Date, options?: FormattedTime.PropsBase) => string;
+        formatRelative?: (value: number, options?: FormattedRelative.PropsBase) => string;
+        formatNumber?: (value: number, options?: FormattedNumber.PropsBase) => string;
+        formatPlural?: (value: number, options?: FormattedPlural.PropsBase) => string;
+        formatMessage?: (messageDescriptor: FormattedMessage.MessageDescriptor, values?: Object) => string;
+        formatHTMLMessage?: (messageDescriptor: FormattedMessage.MessageDescriptor, values?: Object) => string;
+    }
+
+    module IntlComponent {
+        interface DateTimeFormatProps {
+            /*
+            * one of "best fit" (default) | "lookup"
+            */
+            localeMatcher?: string;
+            /*
+            * one of "basic" (default) | "best fit"
+            */
+            formatMatcher?: string;
+            timeZone?: string,
+            hour12?: boolean;
+            /*
+            * one of "narrow" (default) | "short" | "long"
+            */
+            weekday?: string;
+            /*
+            * one of "narrow" (default) | "short" | "long"
+            */
+            era?: string;
+            /*
+            * one of "numeric" (default) | "2-digit"
+            */
+            year?: string;
+            /*
+            * one of "numeric" (default) | "2-digit" | "narrow" | "short" | "long"
+            */
+            month?: string;
+            /*
+            * one of "numeric" (default) | "2-digit"
+            */
+            day?: string;
+            /*
+            * one of "numeric" (default) | "2-digit"
+            */
+            hour?: string;
+            /*
+            * one of "numeric" (default) | "2-digit"
+            */
+            minute?: string;
+            /*
+            * one of "numeric" (default) | "2-digit"
+            */
+            second?: string;
+            /*
+            * one of "short" (default) | "long"
+            */
+            timeZoneName?: string;
+        }
+    }
+
+
+    module FormattedDate {
+        export interface PropsBase extends IntlComponent.DateTimeFormatProps {
+            format?: string;
+        }
+
+        export interface Props extends PropsBase {
+            value: Date;
+        }
+    }
+    class FormattedDate extends React.Component<FormattedDate.Props, any> { }
+
+
+    module FormattedTime {
+        export interface PropsBase extends IntlComponent.DateTimeFormatProps {
+            format?: string;
+        }
+
+        export interface Props extends PropsBase {
+            value: Date;
+        }
+    }
+    class FormattedTime extends React.Component<FormattedTime.Props, any> { }
+
+
+    module FormattedRelative {
+        export interface PropsBase {
+            /*
+            * one of "second", "minute", "hour", "day", "month" or "year"
+            */
+            units?: string;
+            /*
+            * one of "best fit" (default) | "numeric"
+            */
+            style?: string;
+            format?: string;
+            updateInterval?: number;
+            initialNow?: any;
+        }
+
+        export interface Props extends PropsBase {
+            value: number;
+        }
+    }
+    class FormattedRelative extends React.Component<FormattedRelative.Props, any> { }
+
+
+    module FormattedMessage {
+        export interface MessageDescriptor {
+            id: string;
+            description?: string;
+            defaultMessage?: string;
+        }
+
+        export interface Props extends MessageDescriptor {
+            values?: Object;
+            tagName?: string;
+        }
+    }
+    class FormattedMessage extends React.Component<FormattedMessage.Props, any> { }
+
+
+    class FormattedHTMLMessage extends React.Component<FormattedMessage.Props, any> { }
+
+
+    module FormattedNumber {
+        export interface PropsBase {
+            format?: string;
+            /*
+            * one of "best fit" (default) | "lookup"
+            */
+            localeMatcher?: string;
+            /*
+            * one of "decimal" (default) | "currency" | "percent"
+            */
+            style?: string;
+            currency?: string,
+            /*
+            * one of "symbol" (default) | "code" | "name"
+            */
+            currencyDisplay?: string;
+            useGrouping?: boolean;
+            minimumIntegerDigits?: number;
+            minimumFractionDigits?: number;
+            maximumFractionDigits?: number;
+            minimumSignificantDigits?: number;
+            maximumSignificantDigits?: number;
+        }
+
+        export interface Props extends PropsBase {
+            value: number;
+        }
+    }
+    class FormattedNumber extends React.Component<FormattedNumber.Props, any> { }
+
+
+    module FormattedPlural {
+        export interface PropsBase {
+            /*
+            * one of "cardinal" (default) | "ordinal"
+            */
+            style?: string;
+            other?: any;
+            zero?: any;
+            one?: any;
+            two?: any;
+            few?: any;
+            many?: any;
+        }
+
+        export interface Props extends PropsBase {
+            value: number;
+        }
+    }
+    class FormattedPlural extends React.Component<FormattedPlural.Props, any> { }
+
+
+    module IntlProvider {
+        export interface Props {
+            locale?: string;
+            formats?: Object;
+            messages?: Object;
+            defaultLocale?: string;
+            defaultFormats?: Object;
+        }
+    }
+    class IntlProvider extends React.Component<IntlProvider.Props, any> { }
+
+    class LocaleData extends Array<Locale> {
+    }
+}
+
+declare module "react-intl" {
+    export = ReactIntl
+}
+
+declare module "react-intl/lib/locale-data/en" {
+    var data: ReactIntl.LocaleData;
+    export = data;
 }
