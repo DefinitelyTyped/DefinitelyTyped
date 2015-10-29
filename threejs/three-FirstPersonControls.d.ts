1df845d46b911d23abde00af0ebe5de3e181a93b
Poul Kjeldager Sørensen <pks@s-innovations.net>
2015-10-29 12:54:38 +0100

    changed interface to class declaration

--- threejs/three-FirstPersonControls.d.ts
+++ threejs/three-FirstPersonControls.d.ts
@@ -1,36 +1,39 @@
-//Source : https://github.com/NTaylorMullen/CycleR/blob/master/CycleR/CycleR.Game.Client/Client/Interfaces/ThreeJS/Cameras/FirstPersonControls.d.ts
+// Type definitions for three.js
 // Definitions: https://github.com/borisyankov/DefinitelyTyped
+//Source : https://github.com/NTaylorMullen/CycleR/blob/master/CycleR/CycleR.Game.Client/Client/Interfaces/ThreeJS/Cameras/FirstPersonControls.d.ts
 
 /// <reference path="./three.d.ts" />
-
-interface IFirstPersonControls {
-    object: Object3D;
-    target: Vector3;
-    domElement: HTMLCanvasElement;
-    movementSpeed: number;
-    lookSpeed: number;
-    noFly: boolean;
-    lookVertical: boolean;
-    autoForward: boolean;
-    activeLook: boolean;
-    heightSpeed: boolean;
-    heightCoef: boolean;
-    heightMin: boolean;
-    constrainVertical: boolean;
-    verticalMin: number;
-    verticalMax: number;
-    autoSpeedFactor: number;
-    mouseX: number;
-    mouseY: number;
-    lat: number;
-    lon: number;
-    phi: number;
-    theta: number;
-    moveForward: boolean;
-    moveBackward: boolean;
-    moveLeft: boolean;
-    moveRight: boolean;
-    freeze: boolean;
-    mouseDragOn: boolean;
-    update(delta?: number): void;
+declare module THREE {
+    class FirstPersonControls {
+        constructor(object: Camera, domElement?: HTMLElement);
+        object: THREE.Object3D;
+        target: THREE.Vector3;
+        domElement: HTMLCanvasElement;
+        movementSpeed: number;
+        lookSpeed: number;
+        noFly: boolean;
+        lookVertical: boolean;
+        autoForward: boolean;
+        activeLook: boolean;
+        heightSpeed: boolean;
+        heightCoef: boolean;
+        heightMin: boolean;
+        constrainVertical: boolean;
+        verticalMin: number;
+        verticalMax: number;
+        autoSpeedFactor: number;
+        mouseX: number;
+        mouseY: number;
+        lat: number;
+        lon: number;
+        phi: number;
+        theta: number;
+        moveForward: boolean;
+        moveBackward: boolean;
+        moveLeft: boolean;
+        moveRight: boolean;
+        freeze: boolean;
+        mouseDragOn: boolean;
+        update(delta?: number): void;
+    }
 }
