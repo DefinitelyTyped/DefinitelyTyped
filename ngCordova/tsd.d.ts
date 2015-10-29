ac7ddea017ca3e61975da263c29f9e20cbe6af8c
Kapil Sachdeva <ksachdeva17@gmail.com>
2015-10-28 19:09:33 -0500

    Corrections for the conventions for the file headers

--- ngCordova/tsd.d.ts
+++ ngCordova/tsd.d.ts
@@ -1,2 +1,7 @@
+// Type definitions for ngCordova plugins
+// Project: https://github.com/driftyco/ng-cordova
+// Definitions by: Kapil Sachdeva <https://github.com/ksachdeva>
+// Definitions: https://github.com/ksachdeva/DefinitelyTyped
+
 /// <reference path="device.d.ts"/>
 /// <reference path="toast.d.ts"/>
