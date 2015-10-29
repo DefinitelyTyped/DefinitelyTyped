689ec9d4fc9a562270db0a0d038eea408982b59b
Poul Kjeldager Sørensen <pks@s-innovations.net>
2015-10-29 14:27:02 +0100

    Update three-EarthControls.d.ts

--- threejs/three-EarthControls.d.ts
+++ threejs/three-EarthControls.d.ts
@@ -1,6 +1,6 @@
 // Type definitions for three.js
 // Project: http://mrdoob.github.com/three.js/
-// Definitions by: Poul Kjeldager Sørensen
+// Definitions by: Poul Kjeldager Sørensen <https://github.com/s093294>
 // Definitions: https://github.com/borisyankov/DefinitelyTyped
 
 /// <reference path="./three.d.ts" />
