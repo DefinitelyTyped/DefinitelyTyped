5fa991766aa3b0d7ca3ead189993252ff7231ebf
Poul Kjeldager Sørensen <pks@s-innovations.net>
2015-10-29 14:26:40 +0100

    fixed name

--- threejs/three-FirstPersonControls.d.ts
+++ threejs/three-FirstPersonControls.d.ts
@@ -1,6 +1,6 @@
 // Type definitions for three.js
 // Project: http://mrdoob.github.com/three.js/
-// Definitions by: Poul Kjeldager Sørensen
+// Definitions by: Poul Kjeldager Sørensen <https://github.com/s093294>
 // Definitions: https://github.com/borisyankov/DefinitelyTyped
 //Source : https://github.com/NTaylorMullen/CycleR/blob/master/CycleR/CycleR.Game.Client/Client/Interfaces/ThreeJS/Cameras/FirstPersonControls.d.ts
 
