4da8a5f6f94579bfd14ce608f06425bf84907649
Poul Kjeldager Sørensen <pks@s-innovations.net>
2015-10-29 12:59:15 +0100

    Create three-EarthControls.d.ts

---
+++ threejs/three-EarthControls.d.ts
@@ -0,0 +1,10 @@
+// Type definitions for three.js
+// Definitions: https://github.com/borisyankov/DefinitelyTyped
+
+/// <reference path="./three.d.ts" />
+declare module THREE {
+   class EarthControls {
+        constructor(object: Camera, rendere:WebGLRenderer,scene : Scene);
+
+    }
+}
