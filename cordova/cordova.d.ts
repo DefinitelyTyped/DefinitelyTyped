456ad287da94ec8d61b161d9bab088a4ea2b8820
Dan Manastireanu <danmana11@yahoo.com>
2015-10-31 14:37:32 +0200

    Added type definition for Cordova Keyboard plugin. Closes #4699

--- cordova/cordova.d.ts
+++ cordova/cordova.d.ts
@@ -25,6 +25,7 @@
 /// <reference path="plugins/StatusBar.d.ts"/>
 /// <reference path="plugins/Vibration.d.ts"/>
 /// <reference path="plugins/WebSQL.d.ts"/>
+/// <reference path="plugins/Keyboard.d.ts"/>
 
 interface Cordova {
     /** Invokes native functionality by specifying corresponding service name, action and optional parameters.
