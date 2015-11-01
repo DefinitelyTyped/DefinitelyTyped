5f3c5e063c1861fd7ed80d4fedd85f9cc0158dfa
tkqubo <tk.qubo@gmail.com>
2015-11-01 11:39:18 +0900

    webpack: add HMR

--- webpack/webpack-env-tests.ts
+++ webpack/webpack-env-tests.ts
@@ -13,3 +13,30 @@ let contextModule = context<SomeModule>('./someModule');
 require(['./someModule', './otherModule'], (someModule: SomeModule, otherModule: any) => {
 
 });
+
+// check if HMR is enabled
+if(module.hot) {
+    // accept update of dependency
+    module.hot.accept("./handler.js", function() {
+        //...
+    });
+}
+
+module.exports = null;
+
+// check if HMR is enabled
+if(module.hot) {
+
+    // accept itself
+    module.hot.accept();
+
+    // dispose handler
+    module.hot.dispose(function() {
+        // revoke the side effect
+        //...
+    });
+}
+
+
+var status: string = module.hot.status();
+
