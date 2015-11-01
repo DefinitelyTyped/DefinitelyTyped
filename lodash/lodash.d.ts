08edfe7631b1bf0fd10582dc4bf864cf814c5759
Ilya Mochalov <chrootsu@gmail.com>
2015-11-01 06:22:20 +0500

    lodash: signatures of the method _.take changed

--- lodash/lodash.d.ts
+++ lodash/lodash.d.ts
@@ -1460,6 +1460,20 @@ declare module _ {
         take<TResult>(n?: number): LoDashImplicitArrayWrapper<TResult>;
     }
 
+    interface LoDashExplicitArrayWrapper<T> {
+        /**
+         * @see _.take
+         */
+        take(n?: number): LoDashExplicitArrayWrapper<T>;
+    }
+
+    interface LoDashExplicitObjectWrapper<T> {
+        /**
+         * @see _.take
+         */
+        take<TResult>(n?: number): LoDashExplicitArrayWrapper<TResult>;
+    }
+
     //_.takeRight
     interface LoDashStatic {
         /**
