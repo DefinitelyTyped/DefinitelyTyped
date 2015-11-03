72f1dfa7b255c97fa40101767f20691d5b46013c
Ilya Mochalov <chrootsu@gmail.com>
2015-11-03 05:40:44 +0500

    lodash: signatures of the method _.startsWith have been changed

--- lodash/lodash.d.ts
+++ lodash/lodash.d.ts
@@ -10146,19 +10146,37 @@ declare module _ {
     interface LoDashStatic {
         /**
          * Checks if string starts with the given target string.
+         *
          * @param string The string to search.
          * @param target The string to search for.
          * @param position The position to search from.
          * @return Returns true if string starts with target, else false.
          */
-        startsWith(string?: string, target?: string, position?: number): boolean;
+        startsWith(
+            string?: string,
+            target?: string,
+            position?: number
+        ): boolean;
     }
 
     interface LoDashImplicitWrapper<T> {
         /**
          * @see _.startsWith
          */
-        startsWith(target?: string, position?: number): boolean;
+        startsWith(
+            target?: string,
+            position?: number
+        ): boolean;
+    }
+
+    interface LoDashExplicitWrapper<T> {
+        /**
+         * @see _.startsWith
+         */
+        startsWith(
+            target?: string,
+            position?: number
+        ): LoDashExplicitWrapper<boolean>;
     }
 
     //_.template
