3916f5caf12272eeab6c1a6ef4e5c612e2efa0c5
Kapil Sachdeva <ksachdeva17@gmail.com>
2015-11-03 18:10:50 -0600

    add the ngCordova type definitions for network,deviceOrientation,deviceMotion, appAvailability,geolocation,emailComposer and dialogs

--- ng-cordova/tsd.d.ts
+++ ng-cordova/tsd.d.ts
@@ -5,3 +5,10 @@
 
 /// <reference path="device.d.ts"/>
 /// <reference path="toast.d.ts"/>
+/// <reference path="network.d.ts"/>
+/// <reference path="emailComposer.d.ts"/>
+/// <reference path="dialogs.d.ts"/>
+/// <reference path="geolocation.d.ts"/>
+/// <reference path="deviceMotion.d.ts"/>
+/// <reference path="deviceOrientation.d.ts"/>
+/// <reference path="appAvailability.d.ts"/>
