da4d1cc53efa97b2fc47d6926bd9d98d489a7cd4
Martin D <martinduparc@users.noreply.github.com>
2015-11-04 09:20:35 -0500

    No implicit any

--- FilePreviews/FilePreviews-test.ts
+++ FilePreviews/FilePreviews-test.ts
@@ -3,7 +3,7 @@
 var previews = new FilePreviews({debug: false, apiKey: 'yourapikey'});
 var url = 'url';
 
-previews.generate(url, function(err, result) {
+previews.generate(url, function(err:any, result:any) {
     if (result) {
         console.log(result.previewURL);
     }
