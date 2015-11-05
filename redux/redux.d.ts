4f1200e07cfde92402e8ab23a36bc8fcfe4e4cdc
Michael Tiller <michael.tiller@xogeny.com>
2015-11-05 15:17:17 -0500

    More specific type constraints
    I made an attempt to provide more specific type constraints in these definitions.  For example, a dispatch call returns its argument, so I tried to reflect that.  Similarly, a reducer always returns a value that is the same type as its first argument.  Furthermore, I added a type parameter to `Store` to indicate the type of state it is carrying around.
    
    I also added a formal definition of `Action` so checks for things like the presence of `type` and `payload` can be made.
    
    Using these definitions, the `redux-tests.ts` still passes, as is.  But this provides a lot more checking.  In addition, the presence of types provides some much needed documentation.  Even with the `redux` documentation, I had a hard time figuring out exactly what the arguments and return values should be.
    
    Note that `ActionCreator` and `StoreMethods` are not used anywhere in this file.  I'm not sure what they are here for since they clearly are not part of the API.  I think they should really be removed but I left them in for some degree of backward compatibility.
    
    I'm just starting to use `redux`, so I don't have a really thorough way to test this.

--- redux/redux.d.ts
+++ redux/redux.d.ts
@@ -4,46 +4,77 @@
 // Definitions: https://github.com/borisyankov/DefinitelyTyped
 
 declare module Redux {
+    // FSA-compliant action (from redux-actions.d.ts)
+    // See: https://github.com/acdlite/flux-standard-action
+    type Action = {
+        type: string
+        payload?: any
+        error?: boolean
+        meta?: any
+    };
 
-    interface ActionCreator extends Function {
-        (...args: any[]): any;
+	// Not used...?
+	interface ActionCreator extends Function {
+        (...args: any[]): Action;
     }
 
-    interface Reducer extends Function {
-        (state: any, action: any): any;
+	// Also not used here...
+	interface StoreMethods<T,A extends Action> {
+        dispatch: Dispatch;
+        getState(): T;
     }
 
+	// Reducer is a function that takes a state of type T and an
+	// action and returns a new state, also of type T
+    interface Reducer<T> extends Function {
+        (state: T, action: any): T;
+    }
+
+	// Dispatch is a function that takes an action of type A and
+	// returns an instance of that same action (still of type A)
     interface Dispatch extends Function {
-        (action: any): any;
+        <A extends Action>(action: A): A;
     }
 
-    interface StoreMethods {
+	// MiddlewareArg is an instance of middleware that provides a
+	// dispatch method and a getState method.  The type parameter T is
+	// the type of the state.  No type parameter is provided on
+	// Dispatch because the middleware generally needs to be able to
+	// handle any type of action.
+    interface MiddlewareArg<T> {
         dispatch: Dispatch;
-        getState(): any;
+        getState: () => T;
     }
 
-
-    interface MiddlewareArg {
-        dispatch: Dispatch;
-        getState: Function;
+	// Middleware is constructed from an instance of MiddlewareArg<T> where
+	// T is the type of the state.
+    interface Middleware<T> extends Function {
+        (obj: MiddlewareArg<T>): Function;
     }
 
-    interface Middleware extends Function {
-        (obj: MiddlewareArg): Function;
+	// Store manages the state of the system.  The state of the system
+	// should be of type T.
+    class Store<T> {
+        getReducer(): Reducer<T>;
+        replaceReducer(nextReducer: Reducer<T>): void;
+        dispatch<A extends Action>(action: A): A;
+        getState(): T;
+        subscribe(listener: () => void): () => void;
     }
 
-    class Store {
-        getReducer(): Reducer;
-        replaceReducer(nextReducer: Reducer): void;
-        dispatch(action: any): any;
-        getState(): any;
-        subscribe(listener: Function): Function;
-    }
+	// StoreCreator describes any function that can be used to create
+	// a Store<T> (where T is the type of the state).  This type is
+	// used to describe the return type of applyMiddleware since
+	// produces special function that can be used to create a store
+	// that integrates a collection of middleware.
+	interface StoreCreator<T> extends Function {
+		(reducer: Reducer<T>, initialState?: T): Store<T>;
+	}
 
-    function createStore(reducer: Reducer, initialState?: any): Store;
-    function bindActionCreators<T>(actionCreators: T, dispatch: Dispatch): T;
-    function combineReducers(reducers: any): Reducer;
-    function applyMiddleware(...middlewares: Middleware[]): Function;
+    function createStore<T>(reducer: Reducer<T>, initialState?: T): Store<T>;
+    function bindActionCreators<C>(actionCreators: C, dispatch: Dispatch): C;
+    function combineReducers<T>(reducers: { [key: string]: Reducer<T> }): Reducer<T>;
+    function applyMiddleware<T>(...middlewares: Middleware<T>[]): (c: StoreCreator<T>) => StoreCreator<T>;
     function compose<T extends Function>(...functions: Function[]): T;
 }
 
