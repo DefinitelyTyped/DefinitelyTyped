3250f873448d6eb4e4baaec4a51a344affb5b4d3
motemen <motemen@gmail.com>
2015-11-12 11:56:07 +0900

    header for non auto-generated files

--- google-apps-script/google-apps-script.types.d.ts
+++ google-apps-script/google-apps-script.types.d.ts
@@ -1,3 +1,8 @@
+// Type definitions for Google Apps Script 2015-11-12
+// Project: https://developers.google.com/apps-script/
+// Definitions by: motemen <https://github.com/motemen/>
+// Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
+
 declare module GoogleAppsScript {
   type BigNumber = any;
   type Byte = number;
