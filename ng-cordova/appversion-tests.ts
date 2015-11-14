5ae01982bdfdfed7a7556a7bd5a4969bda839bf8
Jacques Kang <jkang.perso@gmail.com>
2015-11-14 21:01:35 +0100

    add definitions for ng-cordova appversion plugin

---
+++ ng-cordova/appversion-tests.ts
@@ -0,0 +1,15 @@
+/// <reference path="appversion.d.ts" />
+
+module ngCordova {
+    function test($cordovaAppVersion: IAppVersionService) {
+        $cordovaAppVersion.getVersionNumber()
+            .then((versionNumber) => {
+                console.log(versionNumber.toLowerCase());
+            });
+
+        $cordovaAppVersion.getVersionCode()
+            .then((versionCode) => {
+                console.log(versionCode.toLowerCase());
+            });
+    };
+}
