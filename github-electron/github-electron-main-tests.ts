fa02306ad8204755356fca91deb5bf5761e81599
rhysd <lin90162@yahoo.co.jp>
2015-11-21 07:15:38 +0900

    DevTools-related methods are moved from BrowserWindow to WebContents
    DevTools-related methods are no longer defined in `BrowserWindow`.
    
    https://github.com/atom/electron/blob/master/docs/api/browser-window.md
    
    Instead, they are defined in `WebContents`.
    
    https://github.com/atom/electron/blob/master/docs/api/web-contents.md

--- github-electron/github-electron-main-tests.ts
+++ github-electron/github-electron-main-tests.ts
@@ -61,9 +61,11 @@ app.on('ready', () => {
 	mainWindow.loadUrl('file://foo/bar', {userAgent: 'cool-agent', httpReferrer: 'greateRefferer'});
 	mainWindow.webContents.loadUrl('file://foo/bar', {userAgent: 'cool-agent', httpReferrer: 'greateRefferer'});
 
-	mainWindow.openDevTools()
-	var opened: boolean = mainWindow.isDevToolsOpened()
-	mainWindow.toggleDevTools()
+	mainWindow.webContents.openDevTools();
+	mainWindow.webContents.toggleDevTools();
+	mainWindow.webContents.openDevTools({detach: true});
+	mainWindow.webContents.closeDevTools();
+	var opened: boolean = mainWindow.webContents.isDevToolsOpened()
 	// Emitted when the window is closed.
 	mainWindow.on('closed', () => {
 		// Dereference the window object, usually you would store windows
@@ -344,7 +346,7 @@ var template = [
 			{
 				label: 'Toggle DevTools',
 				accelerator: 'Alt+Command+I',
-				click: () => { BrowserWindow.getFocusedWindow().toggleDevTools(); }
+				click: () => { BrowserWindow.getFocusedWindow().webContents.toggleDevTools(); }
 			}
 		]
 	},
