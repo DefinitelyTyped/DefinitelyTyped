ba131000c0977b35d566788cdeaa7da323a0d30d
Aya Morisawa <AyaMorisawa4869@gmail.com>
2015-11-22 20:47:52 +0900

    Add gulp-babel.d.ts

---
+++ gulp-babel/gulp-babel-tests.ts
@@ -0,0 +1,7 @@
+/// <reference path="../node/node.d.ts" />
+/// <reference path="./gulp-babel.d.ts" />
+
+import babel from 'gulp-babel';
+
+var x: NodeJS.ReadWriteStream = babel();
+var x: NodeJS.ReadWriteStream = babel({});
