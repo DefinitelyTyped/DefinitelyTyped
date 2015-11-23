fd779c9d1fc57b0b75d31feb84acc8a2019279d3
Alex Ewerlöf <hanifbbz@gmail.com>
2015-11-23 19:50:50 +0100

    updated the headers and file references

--- jasmine-expect/jasmine-expect-tests.ts
+++ jasmine-expect/jasmine-expect-tests.ts
@@ -1,5 +1,5 @@
 /// <reference path="../jasmine/jasmine.d.ts" />
-/// <reference path="jamiemason-jasmine-matchers.d.ts" />
+/// <reference path="jasmine-expect.d.ts" />
 
 // Taken directly from the test directory of the original repo
 
