b1a8018b5ee4a01bb0afe7fe9af928ea6ed68d0c
dencap <d.cappellin@asem.it>
2015-11-25 17:23:03 +0100

    First draft of definition for pako

--- pako/pako-tests.ts
+++ pako/pako-tests.ts
@@ -2,25 +2,16 @@
 
 import pako = require("pako");
 
-var test = { my: 'super', puper: [456, 567], awesome: 'pako' };
-
-var binaryString = pako.deflate(JSON.stringify(test), { to: 'string' });
-
-//
-// Here you can do base64 encode, make xhr requests and so on.
-//
-
-var restored = JSON.parse(pako.inflate(binaryString, { to: 'string' }));
-
-var pako = require('pako')
-  , chunk1 = Uint8Array([1,2,3,4,5,6,7,8,9])
-  , chunk2 = Uint8Array([10,11,12,13,14,15,16,17,18,19]);
+var chunk1 = new Uint8Array([1,2,3,4,5,6,7,8,9])
+var chunk2 = new Uint8Array([10,11,12,13,14,15,16,17,18,19]);
 
 var deflate = new pako.Deflate({ level: 3});
 
 deflate.push(chunk1, false);
 deflate.push(chunk2, true);  // true -> last chunk
 
-if (deflate.err) { throw new Error(deflate.err); }
+if (deflate.err) { 
+	throw new Error( deflate.err.toString() ); 
+}
 
 console.log(deflate.result);
