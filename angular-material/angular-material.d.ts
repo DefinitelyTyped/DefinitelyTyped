afb6532a96b7a00810b56dc8f90f6934a62b99d1
JJJ <book010@hotmail.com>
2015-11-26 15:00:20 +0800

    update version to 1.0.0-rc5

--- angular-material/angular-material.d.ts
+++ angular-material/angular-material.d.ts
@@ -1,4 +1,4 @@
-// Type definitions for Angular Material 0.10.1-rc1+ (angular.material module)
+// Type definitions for Angular Material 1.0.0-rc5+ (angular.material module)
 // Project: https://github.com/angular/material
 // Definitions by: Matt Traynham <https://github.com/mtraynham>
 // Definitions: https://github.com/borisyankov/DefinitelyTyped
