0edde72be8d59eaff8775a8ba992a43dbf2dccc0
Giedrius Grabauskas <GiedriusGrabauskas@users.noreply.github.com>
2015-11-29 00:31:26 +0200

    jwt-decode test added

---
+++ jwt-decode/jwt-decode-test.ts
@@ -0,0 +1,5 @@
+import * as jwtDecode from 'jwt-decode';
+
+let token = "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJmb28iOiJiYXIiLCJleHAiOjEzOTMyODY4OTMsImlhdCI6MTM5MzI2ODg5M30.4-iaDojEVl0pJQMjrbM1EzUIfAZgsbK_kgnVyVxFSVo";
+
+let decodedToken = jwtDecode(responseIdToken);
