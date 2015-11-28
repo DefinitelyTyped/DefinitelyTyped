98b2bf7ede84f80523065750302147196a8f5be3
Giedrius Grabauskas <GiedriusGrabauskas@users.noreply.github.com>
2015-11-29 00:37:54 +0200

    jwt-decode tests added

--- jwt-decode/jwt-decode-tests.ts
+++ jwt-decode/jwt-decode-tests.ts
@@ -2,4 +2,10 @@ import * as jwtDecode from 'jwt-decode';
 
 let token = "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJmb28iOiJiYXIiLCJleHAiOjEzOTMyODY4OTMsImlhdCI6MTM5MzI2ODg5M30.4-iaDojEVl0pJQMjrbM1EzUIfAZgsbK_kgnVyVxFSVo";
 
-let decodedToken = jwtDecode(responseIdToken);
+interface TokenDto {
+  foo: string;
+  exp: number;
+  iat: number;
+}
+
+let decodedToken = jwtDecode(token) as TokenDto;
