d2e347604b34dcf8220512b5caa75717aa0fbe8c
Per Kastman <per@kastman.eu>
2015-12-03 14:26:25 +0100

    Added possibility to register for events

--- jquery-cropbox/jquery-cropbox.d.ts
+++ jquery-cropbox/jquery-cropbox.d.ts
@@ -103,7 +103,13 @@ declare module jQueryCropBox {
          * Remove the cropbox functionality from the image.      
          */
         remove(): void;
+        /**
+             * Attach an event handler function for one event on the Crop Box      
+             */
+        on(event: string, callback: jQueryCropBox.EventCallback): void;
     }
+
+    type EventCallback = (e: Event, data: any, img: jQueryCropBox.Cropbox) => void;
 }
 
 interface JQuery {
