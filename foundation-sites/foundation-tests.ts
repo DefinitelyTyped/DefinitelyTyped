c1396fd43c203803478193218e5393dff6068ab6
Michal Wrzesniewski <michal.wrzesniewski@immoweb.be>
2015-12-03 10:35:11 +0100

    test file: Header added

--- foundation-sites/foundation-tests.ts
+++ foundation-sites/foundation-tests.ts
@@ -1,2 +1,8 @@
+// Tests for type definitions for Foundation Sites v6.0.4
+// Project: http://foundation.zurb.com/
+// Definitions by: Sam Vloeberghs <https://github.com/samvloeberghs/>
+// Definitions by: Michał Wrześniewski <https://github.com/wrzesm01/>
+// Definitions: https://github.com/borisyankov/DefinitelyTyped
+
 /// <reference path="../jquery/jquery.d.ts" />
 /// <reference path="foundation.d.ts" />
