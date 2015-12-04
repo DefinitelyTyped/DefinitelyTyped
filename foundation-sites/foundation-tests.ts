150b05ca742c8b980db88d94bac285e92721a97a
Sam Vloeberghs <sam.vloeberghs@gmail.com>
2015-12-04 09:31:31 +0100

    progress

--- foundation-sites/foundation-tests.ts
+++ foundation-sites/foundation-tests.ts
@@ -10,60 +10,6 @@ $(document).foundation();
 $(document).foundation('method');
 $(document).foundation(['method', 'method2']);
 
-
- Foundation.Abide.($('.selector'));
- Foundation.Abide.($('.selector'), {});
-/*
- Foundation.Accordion.($('.selector'));
- Foundation.Accordion.($('.selector'), {});
-
- Foundation.AccordionMenu.($('.selector'));
- Foundation.AccordionMenu.($('.selector'), {});
-
- Foundation.DrillDown.($('.selector'));
- Foundation.DrillDown.($('.selector'), {});
-
- Foundation.Dropdown.($('.selector'));
- Foundation.Dropdown.($('.selector'), {});
-
- Foundation.DropdownMenu.($('.selector'));
- Foundation.DropdownMenu.($('.selector'), {});
-
- Foundation.Equalizer.($('.selector'));
- Foundation.Equalizer.($('.selector'), {});
-
- Foundation.Interchange.($('.selector'));
- Foundation.Interchange.($('.selector'), {});
-
- Foundation.Magellan.($('.selector'));
- Foundation.Magellan.($('.selector'), {});
-
- Foundation.OffCanvas.($('.selector'));
- Foundation.OffCanvas.($('.selector'), {});
-
- Foundation.Orbit.($('.selector'));
- Foundation.Orbit.($('.selector'), {});
-
- Foundation.Reveal.($('.selector'));
- Foundation.Reveal.($('.selector'), {});
-
- Foundation.Slider.($('.selector'));
- Foundation.Slider.($('.selector'), {});
-
- Foundation.Sticky.($('.selector'));
- Foundation.Sticky.($('.selector'), {});
-
- Foundation.Tabs.($('.selector'));
- Foundation.Tabs.($('.selector'), {});
-
- Foundation.Toggler.($('.selector'));
- Foundation.Toggler.($('.selector'), {});
-
- Foundation.Tooltip.($('.selector'));
- Foundation.Tooltip.($('.selector'), {});
- */
-
-/*
 function pluginList() {
 
     'use strict';
@@ -89,9 +35,62 @@ function pluginList() {
     ];
 }
 
-pluginList().forEach((value:String) => {
-    Foundation[value].($('.selector'));
-    Foundation[value].($('.selector'), {});
+pluginList().forEach((value:string) => {
+    Foundation[value]($('.selector'));
+    Foundation[value]($('.selector'), {});
 });
 
-*/
+/*
+ Foundation.Abide($('.selector'));
+ Foundation.Abide($('.selector'), {});
+
+ Foundation.Accordion($('.selector'));
+ Foundation.Accordion($('.selector'), {});
+
+ Foundation.AccordionMenu($('.selector'));
+ Foundation.AccordionMenu($('.selector'), {});
+
+ Foundation.DrillDown($('.selector'));
+ Foundation.DrillDown($('.selector'), {});
+
+ Foundation.Dropdown($('.selector'));
+ Foundation.Dropdown($('.selector'), {});
+
+ Foundation.DropdownMenu($('.selector'));
+ Foundation.DropdownMenu($('.selector'), {});
+
+ Foundation.Equalizer($('.selector'));
+ Foundation.Equalizer($('.selector'), {});
+
+ Foundation.Interchange($('.selector'));
+ Foundation.Interchange($('.selector'), {});
+
+ Foundation.Magellan($('.selector'));
+ Foundation.Magellan($('.selector'), {});
+
+ Foundation.OffCanvas($('.selector'));
+ Foundation.OffCanvas($('.selector'), {});
+
+ Foundation.Orbit($('.selector'));
+ Foundation.Orbit($('.selector'), {});
+
+ Foundation.Reveal($('.selector'));
+ Foundation.Reveal($('.selector'), {});
+
+ Foundation.Slider($('.selector'));
+ Foundation.Slider($('.selector'), {});
+
+ Foundation.Sticky($('.selector'));
+ Foundation.Sticky($('.selector'), {});
+
+ Foundation.Tabs($('.selector'));
+ Foundation.Tabs($('.selector'), {});
+
+ Foundation.Toggler($('.selector'));
+ Foundation.Toggler($('.selector'), {});
+
+ Foundation.Tooltip($('.selector'));
+ Foundation.Tooltip($('.selector'), {});
+ */
+
+
