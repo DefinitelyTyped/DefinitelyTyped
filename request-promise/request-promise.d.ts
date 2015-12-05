1e2a56dbfcbfce45aed424c3dc53696e0fbb5294
Max <mshmelev@gmail.com>
2015-12-05 07:16:13 -0500

    Overload then, catch, finally definitions

--- request-promise/request-promise.d.ts
+++ request-promise/request-promise.d.ts
@@ -12,7 +12,13 @@ declare module 'request-promise' {
     import request = require('request');
     import http = require('http');
         
-    interface RequestPromise extends request.Request, Promise<any> {
+    interface RequestPromise extends request.Request {
+        then<TResult>(onfulfilled?: (value: any) => TResult | PromiseLike<TResult>, onrejected?: (reason: any) => TResult | PromiseLike<TResult>): Promise<TResult>;
+        then<TResult>(onfulfilled?: (value: any) => TResult | PromiseLike<TResult>, onrejected?: (reason: any) => void): Promise<TResult>;
+        catch(onrejected?: (reason: any) => any | PromiseLike<any>): Promise<any>;
+        catch(onrejected?: (reason: any) => void): Promise<any>;
+        finally<TResult>(handler: () => PromiseLike<TResult>): Promise<any>;
+        finally<TResult>(handler: () => TResult): Promise<any>;
         promise(): Promise<any>;
     }
     
