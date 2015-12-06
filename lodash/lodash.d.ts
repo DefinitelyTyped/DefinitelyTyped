58d66d3c9a8dfcacc85efd00b4ff4103513ca841
Ilya Mochalov <chrootsu@gmail.com>
2015-12-06 14:43:21 +0500

    lodash: signatures of _.negate have been changed

--- lodash/lodash.d.ts
+++ lodash/lodash.d.ts
@@ -8462,6 +8462,7 @@ declare module _ {
         /**
          * Creates a function that negates the result of the predicate func. The func predicate is invoked with
          * the this binding and arguments of the created function.
+         *
          * @param predicate The predicate to negate.
          * @return Returns the new function.
          */
@@ -8483,6 +8484,18 @@ declare module _ {
          * @see _.negate
          */
         negate<TResult extends Function>(): LoDashImplicitObjectWrapper<TResult>;
+    }
+
+    interface LoDashExplicitObjectWrapper<T> {
+        /**
+         * @see _.negate
+         */
+        negate(): LoDashExplicitObjectWrapper<(...args: any[]) => boolean>;
+
+        /**
+         * @see _.negate
+         */
+        negate<TResult extends Function>(): LoDashExplicitObjectWrapper<TResult>;
     }
 
     //_.once
