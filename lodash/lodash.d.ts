2992509044260db2e00b97abed2c7f2bd1408378
hatz48 <hatz48@hatena.ne.jp>
2015-12-07 20:29:15 +0900

    return appropriate Types array if not specify Type value.

--- lodash/lodash.d.ts
+++ lodash/lodash.d.ts
@@ -11979,7 +11979,8 @@ declare module _ {
          * @param object The object to query.
          * @return Returns an array of property values.
          */
-        values<T>(object?: any): T[];
+        values<T>(object?: { [idx: number]: T }): T[];
+        values<T>(object?: { [idx: string]: T }): T[];
     }
 
     interface LoDashImplicitObjectWrapper<T> {
