286b77b1bf6b1ce3e39f646a84538488cb0f736a
Cyril Schumacher <cyril_schumacher@live.fr>
2015-12-07 19:19:49 +0100

    Add definition for "express-brute-mongo".

--- express-brute/express-brute.d.ts
+++ express-brute/express-brute.d.ts
@@ -1,4 +1,4 @@
-// Type definitions for express-validator 2.9.0
+// Type definitions for express-brute
 // Project: https://github.com/AdamPflug/express-brute
 // Definitions by: Cyril Schumacher <https://github.com/cyrilschumacher/>
 // Definitions: https://github.com/borisyankov/DefinitelyTyped
