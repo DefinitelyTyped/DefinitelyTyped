8d9c19c4a500d6340f2770f1266822c0ba59e0fc
r-ising <github@ramonising.de>
2015-12-10 15:02:09 +0100

    update node.d.ts
    in publicEncrypt- and privateDecrypt-function is passpharse and padding optional

--- node/node.d.ts
+++ node/node.d.ts
@@ -1672,12 +1672,12 @@ declare module "crypto" {
     export function pseudoRandomBytes(size: number, callback: (err: Error, buf: Buffer) =>void ): void;
     export interface RsaPublicKey {
         key: string;
-        padding: any;
+        padding?: any;
     }
     export interface RsaPrivateKey {
         key: string;
-        passphrase: string,
-        padding: any;
+        passphrase?: string,
+        padding?: any;
     }
     export function publicEncrypt(public_key: string|RsaPublicKey, buffer: Buffer): Buffer
     export function privateDecrypt(private_key: string|RsaPrivateKey, buffer: Buffer): Buffer
