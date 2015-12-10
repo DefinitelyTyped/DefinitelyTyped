14fe9862233c82adbe94b263cffe620dbfe928ac
Deividas Bakanas <bakanasD@gmail.com>
2015-12-10 20:16:08 +0200

    replace-ext.d.ts added

---
+++ replace-ext/replace-ext.d.ts
@@ -0,0 +1,11 @@
+// Type definitions for replace-ext 0.0.1
+// Project: https://www.npmjs.com/package/google-maps
+// Definitions by: Deividas Bakanas <https://github.com/DeividasBakanas>, Giedrius Grabauskas <https://github.com/GiedriusGrabauskas>
+// Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
+
+
+declare function replaceExt(npath: string, ext: string): string;
+
+declare module 'replace-ext' {
+    export = replaceExt;
+}
