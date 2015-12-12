0bb3a46b8baea5eb91e50560fc5cfd4b5895e1ba
phiresky <phireskyde+git@gmail.com>
2015-12-12 15:58:14 +0100

    wu: fix project link

--- wu/wu.d.ts
+++ wu/wu.d.ts
@@ -1,5 +1,5 @@
 // Type definitions for wu.js v2.1.0
-// Project: http://backbonejs.org/
+// Project: https://fitzgen.github.io/wu.js/
 // Definitions by: phiresky <https://github.com/phiresky/>
 // Definitions: https://github.com/borisyankov/DefinitelyTyped
 
