cfaaa0068ccfc228da3368098098e7f3f278312a
Paul van Brenk <paul.van.brenk@microsoft.com>
2015-12-14 17:02:46 -0800

    Replaced deprecated properties with new versions.

--- threejs/three-orbitcontrols.d.ts
+++ threejs/three-orbitcontrols.d.ts
@@ -7,10 +7,10 @@
 
 declare module THREE {
     class OrbitControls {
-        constructor(object:Camera, domElement?:HTMLElement);
+        constructor(object: Camera, domElement?: HTMLElement);
 
-        object:Camera;
-        domElement:HTMLElement;
+        object: Camera;
+        domElement: HTMLElement;
 
         // API
         enabled: boolean;
@@ -19,13 +19,13 @@ declare module THREE {
         // deprecated
         center: THREE.Vector3;
 
-        noZoom: boolean;
+        enableZoom: boolean;
         zoomSpeed: number;
         minDistance: number;
         maxDistance: number;
-        noRotate: boolean;
+        enableRotate: boolean;
         rotateSpeed: number;
-        noPan: boolean;
+        enablePan: boolean;
         keyPanSpeed: number;
         autoRotate: boolean;
         autoRotateSpeed: number;
@@ -33,24 +33,27 @@ declare module THREE {
         maxPolarAngle: number;
         minAzimuthAngle: number;
         maxAzimuthAngle: number;
-        noKeys: boolean;
+        enableKeys: boolean;
         keys: { LEFT: number; UP: number; RIGHT: number; BOTTOM: number; };
         mouseButtons: { ORBIT: MOUSE; ZOOM: MOUSE; PAN: MOUSE; };
+        enableDamping: boolean;
+        dampingFactor: number;
+
 
         rotateLeft(angle?: number): void;
         rotateUp(angle?: number): void;
         panLeft(distance?: number): void;
         panUp(distance?: number): void;
-        pan( deltaX: number, deltaY: number): void;
+        pan(deltaX: number, deltaY: number): void;
         dollyIn(dollyScale: number): void;
         dollyOut(dollyScale: number): void;
         update(): void;
         reset(): void;
-        getPolarAngle() : number;
+        getPolarAngle(): number;
         getAzimuthalAngle(): number;
         
         // EventDispatcher mixins
-        addEventListener(type: string, listener: (event: any) => void ): void;
+        addEventListener(type: string, listener: (event: any) => void): void;
         hasEventListener(type: string, listener: (event: any) => void): void;
         removeEventListener(type: string, listener: (event: any) => void): void;
         dispatchEvent(event: { type: string; target: any; }): void;
