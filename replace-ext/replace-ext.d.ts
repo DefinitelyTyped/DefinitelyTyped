657bbef0d3b416554ed542a9d7e2903c1c43039f
Deividas Bakanas <bakanasD@gmail.com>
2015-12-15 17:01:31 +0200

    Comments edited

--- replace-ext/replace-ext.d.ts
+++ replace-ext/replace-ext.d.ts
@@ -1,6 +1,6 @@
 // Type definitions for replace-ext 0.0.1
-// Project: https://www.npmjs.com/package/google-maps
-// Definitions by: Deividas Bakanas <https://github.com/DeividasBakanas>, Giedrius Grabauskas <https://github.com/GiedriusGrabauskas>
+// Project: https://github.com/wearefractal/replace-ext
+// Definitions by: Deividas Bakanas <https://github.com/DeividasBakanas>
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
 
 
