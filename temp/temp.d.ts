8c0469357cd869d2766948d84a34561c5b8bd5c5
Daniel Rosenwasser <drosen@microsoft.com>
2015-12-16 23:42:23 -0800

    Made non-synchronous functions return 'void'.

--- temp/temp.d.ts
+++ temp/temp.d.ts
@@ -19,22 +19,22 @@ declare module "temp" {
 
     export function track(value: boolean): typeof temp;
 
-    export function mkDir(affixes: string, callback?: (err: any, dirPath: string) => void);
-    export function mkDir(affixes: AffixOptions, callback?: (err: any, dirPath: string) => void);
+    export function mkDir(affixes: string, callback?: (err: any, dirPath: string) => void): void;
+    export function mkDir(affixes: AffixOptions, callback?: (err: any, dirPath: string) => void): void;
 
     export function mkDirSync(affixes: string): string;
     export function mkDirSync(affixes: AffixOptions): string;
 
-    export function open(affixes: string, callback?: (err: any, result: {path: string, fd: number}) => void);
-    export function open(affixes: AffixOptions, callback?: (err: any, result: {path: string, fd: number}) => void);
+    export function open(affixes: string, callback?: (err: any, result: {path: string, fd: number}) => void): void;
+    export function open(affixes: AffixOptions, callback?: (err: any, result: {path: string, fd: number}) => void): void;
 
     export function openSync(affixes: string): { path: string, fd: number };
     export function openSync(affixes: AffixOptions): { path: string, fd: number };
 
-    export function path(affixes: string, defaultPrefix: string);
-    export function path(affixes: AffixOptions, defaultPrefix: string);
+    export function path(affixes: string, defaultPrefix: string): void;
+    export function path(affixes: AffixOptions, defaultPrefix: string): void;
 
-    export function cleanup(callback?: (result: boolean | {files: number, dirs?: number}) => void);
+    export function cleanup(callback?: (result: boolean | {files: number, dirs?: number}) => void): void;
 
     export function cleanupSync(): boolean | {files: number, dirs: number};
 
