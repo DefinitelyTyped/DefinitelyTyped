4b052305e31baebea67927f957782cff0f03fa6e
Marco <akka81@gmail.com>
2015-12-18 18:18:24 +0100

    Update jquery.raty.d.ts

--- jquery.raty/jquery.raty.d.ts
+++ jquery.raty/jquery.raty.d.ts
@@ -1,5 +1,5 @@
 // Type definitions for jquery raty v2.7.0
-// Project https://github.com/wbotelhos/raty
+// Project: https://github.com/wbotelhos/raty
 // Definitions by: Marco Acchini <https://github.com/akka81/>
 // Definitions: https://github.com/borisyankov/DefinitelyTyped
 
