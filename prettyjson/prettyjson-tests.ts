86d05b254f89027e5f8e9a125ea8069a4a0339c2
WAEL BEN ZID <benzid.wael@hotmail.fr>
2015-12-18 16:18:08 +0100

    Definition for prettyjson package added

---
+++ prettyjson/prettyjson-tests.ts
@@ -0,0 +1,18 @@
+/// <reference path="prettyjson.d.ts" />
+
+var options: prettyjson.IOptions,
+	input: string,
+	output: string;
+
+
+input = 'This is a string';
+output = prettyjson.render(input);
+
+output = prettyjson.render(input, {}, 4);
+
+output = prettyjson.render(['first string', ['nested 1', 'nested 2'], 'second string']);
+
+output = prettyjson.render({param1: 'first string', param2: 'second string'});
+
+output = prettyjson.render({first_param: {subparam: 'first string', subparam2: 'another string'}, second_param: 'second string'});
+
