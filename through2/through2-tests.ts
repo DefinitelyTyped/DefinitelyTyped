cba118b92f538f6fcc3f8226e7bcdb5eb73f359d
Georgios Valotasios <valotas@gmail.com>
2015-12-20 18:52:49 +0100

    Added callback definitions to throught2

--- through2/through2-tests.ts
+++ through2/through2-tests.ts
@@ -22,6 +22,12 @@ rws = through2(function (entry: any, enc: string, callback: () => void) {
 
 });
 
+rws = through2(function (entry: any, enc: string, callback: (error: any, data?: any) => void) {
+	callback(null, 'foo');
+}, (flushCallback: () => void) => {
+	flushCallback();
+});
+
 rws = through2(function (entry: any, enc: string, callback: () => void) {
 	this.push('foo');
 	callback();
@@ -42,3 +48,8 @@ rws = through2.obj(function (entry: any, enc: string, callback: () => void) {
 	callback();
 });
 
+rws = through2.obj(function (entry: any, enc: string, callback: (err: any) => void) {
+	callback('Oups!');
+}, (flashCallback) => {
+	flashCallback();
+});
