54546a1fa0e6913755db29b432964841dc813510
Georgios Valotasios <valotas@gmail.com>
2015-12-20 19:44:13 +0100

    Removed the inner jade module declaration

--- jade/jade.d.ts
+++ jade/jade.d.ts
@@ -4,16 +4,15 @@
 // Definitions: https://github.com/borisyankov/DefinitelyTyped
 
 declare module 'jade' {
-    module jade {
-        function compile(template: string, options?: any): (locals?: any) => string;
-        function compileFile(path: string, options?: any): (locals?: any) => string;
-        function compileClient(template: string, options?: any): (locals?: any) => string;
-        function compileClientWithDependenciesTracked(template: string, options?: any): {
-            body: (locals?: any) => string;
-            dependencies: string[];
-        };
-        function render(template: string, options?: any): string;
-        function renderFile(path: string, options?: any): string;
-    }
+    function compile(template: string, options?: any): (locals?: any) => string;
+    function compileFile(path: string, options?: any): (locals?: any) => string;
+    function compileClient(template: string, options?: any): (locals?: any) => string;
+    function compileClientWithDependenciesTracked(template: string, options?: any): {
+        body: (locals?: any) => string;
+        dependencies: string[];
+    };
+    function render(template: string, options?: any): string;
+    function renderFile(path: string, options?: any): string;
+
     export = jade;
 }
