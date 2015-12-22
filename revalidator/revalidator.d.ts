adaedc02189c2b4a6429637aef7ca8d88e66b063
Jason Turner <jturner@liberty.com.au>
2015-12-22 15:12:14 +1100

    added re-validator

---
+++ revalidator/revalidator.d.ts
@@ -0,0 +1,16 @@
+﻿// Type definitions for axios 0.5.2
+
+// Definitions by:  Jason Turner<https://github.com/brewsoftware>
+// Definitions: https://github.com/borisyankov/DefinitelyTyped
+
+declare module Revalidator {
+    interface RevalidatorStatic {
+        validate(object: any, schema: any, options: any): any;
+    }
+}
+
+declare var revalidator: Revalidator.RevalidatorStatic;
+
+declare module "revalidator" {
+    export = revalidator;
+}
