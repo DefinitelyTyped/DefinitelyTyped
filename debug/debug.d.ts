5ccca9a4efc3d08724ff14ea3a263c7417db0b3d
Gal Talmor <galtalmor@users.noreply.github.com>
2015-12-23 16:51:51 +0200

    Update debug.d.ts

--- debug/debug.d.ts
+++ debug/debug.d.ts
@@ -3,7 +3,7 @@
 // Definitions by: Seon-Wook Park <https://github.com/swook>, Gal Talmor <https://github.com/galtalmor>
 // Definitions: https://github.com/borisyankov/DefinitelyTyped
 
-declare var debug:debug.IDebug;
+declare var debug: debug.IDebug;
 
 // Support AMD require
 declare module 'debug' {
@@ -12,27 +12,27 @@ declare module 'debug' {
 
 declare module debug {
     export interface IDebug {
-        (namespace: string):debug.IDebugger,
-        coerce:(val:any)=>any,
-        disable:()=>void,
-        enable:(namespaces:string)=>void,
-        enabled:(namespaces:string)=>boolean,
+        (namespace: string): debug.IDebugger,
+        coerce: (val: any) => any,
+        disable: () => void,
+        enable: (namespaces: string) => void,
+        enabled: (namespaces: string) => boolean,
 
-        names:string[],
-        skips:string[],
+        names: string[],
+        skips: string[],
 
-        formatters:IFormatters
+        formatters: IFormatters
     }
 
     export interface IFormatters {
-        [formatter:string]: Function
+        [formatter: string]: Function
     }
 
     export interface IDebugger {
         (formatter: any, ...args: any[]): void;
 
-        enabled:boolean;
-        log:Function;
-        namespace:string;
+        enabled: boolean;
+        log: Function;
+        namespace: string;
     }
 }
