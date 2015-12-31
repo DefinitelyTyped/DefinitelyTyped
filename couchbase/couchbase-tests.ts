f1ef87bd985b7d44213f731303474d119b3d9af9
Marwan Aouida <marwan@aouida.com>
2015-12-31 08:20:06 +0100

    updated test using a sample code from official documentation

--- couchbase/couchbase-tests.ts
+++ couchbase/couchbase-tests.ts
@@ -1,25 +1,16 @@
 /// <reference path="couchbase.d.ts"/>
 
 import couchbase = require('couchbase');
-import Cluster = couchbase.Cluster;
-import ViewQuery = couchbase.ViewQuery;
-import Errors = couchbase.errors;
+var cluster = new couchbase.Cluster('couchbase://127.0.0.1');
+var bucket = cluster.openBucket('default');
 
-var cluster = new Cluster('my_connection_string');
-var clusterManager = cluster.manager();
-var bucket = cluster.openBucket('my_bucket');
-var bucketManager = bucket.manager();
+bucket.upsert('testdoc', { name: 'Frank' }, (error) => {
+    if (error) throw error;
 
-var query = ViewQuery.from('users', 'date')
-    .group_level(2)
-    .stale(ViewQuery.Update.BEFORE)
-    .limit(5)
-    .range([2015, 1, 2, 13, 56, 0], [2015, 1, 2, 16, 43, 57], true);
+    bucket.get('testdoc', (err, result) => {
+        if (err) throw err;
 
-bucket.query(query, (err, result) => {
-    if (err != null && err.code === Errors.genericError) {
-        // do something
-    } else {
-        // do something
-    }
+        console.log(result.value);
+        // {name: Frank} 
+    });
 });
