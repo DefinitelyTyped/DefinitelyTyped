d58d48aec908477d01532fdcd21d87d4bfd00303
Anton Karsten <anton.karsten@antvox.com>
2016-01-03 20:31:56 +0100

    added a version number to contentful-resolve-response

--- contentful-resolve-response/contentful-resolve-response.d.ts
+++ contentful-resolve-response/contentful-resolve-response.d.ts
@@ -1,4 +1,4 @@
-// Type definitions for contentful-resolve-response
+// Type definitions for contentful-resolve-response v0.1.2
 // Project: https://github.com/contentful/contentful-resolve-response
 // Definitions by: Anton Karsten <https://github.com/antonkarsten>
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
