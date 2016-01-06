61f56612d8cd504f4bb6c31aecfa2354ae94757b
Xavier Stouder <xavier.stouder@divtec.ch>
2016-01-06 17:31:51 +0100

    Add vec3 typings

---
+++ vec3/vec3.d.ts
@@ -0,0 +1,29 @@
+declare module "vec3"{
+    export class Vec3{
+        constructor(x: number, y: number, z: number);
+        constructor(location: number[]);
+        constructor(location: {x: number; y: number; z: number});
+        constructor(locationStr: string);
+        
+        set(x, y, z): Vec3;
+        update(other: Vec3): Vec3;
+        floored(): Vec3;
+        floor(): Vec3;
+        offset(dx: number, dy: number, dz: number): Vec3;
+        translate(dx: number, dy: number, dz: number): Vec3;
+        add(other: Vec3): Vec3;
+        substract(other: Vec3): Vec3;
+        plus(other: Vec3): Vec3;
+        minus(other: Vec3): Vec3;
+        scaled(scalar: number): Vec3;
+        abs(): Vec3
+        volume(): number;
+        modulus(): Vec3;
+        distanceTo(other: Vec3): number;
+        equals(other: Vec3): boolean;
+        toString(): string;
+        clone(): Vec3;
+        min(other: Vec3): Vec3;
+        max(other: Vec3): Vec3;
+    }
+}
