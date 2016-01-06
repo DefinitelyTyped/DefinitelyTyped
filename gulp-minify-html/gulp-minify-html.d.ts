2961bf02f1ac6eb8b0341664a01f484f25a4fef1
Tanguy Krotoff <tkrotoff@gmail.com>
2016-01-06 16:22:15 +0100

    Add Options interface to the namespace

--- gulp-minify-html/gulp-minify-html.d.ts
+++ gulp-minify-html/gulp-minify-html.d.ts
@@ -6,32 +6,32 @@
 /// <reference path="../node/node.d.ts" />
 
 declare module 'gulp-minify-html' {
-    interface IOptions {
-        // Do not remove empty attributes
-        empty?: boolean;
+    namespace minifyHtml {
+        interface Options {
+            // Do not remove empty attributes
+            empty?: boolean;
 
-        // Do not strip CDATA from scripts
-        cdata?: boolean;
+            // Do not strip CDATA from scripts
+            cdata?: boolean;
 
-        // Do not remove comments
-        comments?: boolean;
+            // Do not remove comments
+            comments?: boolean;
 
-        // Do not remove conditional internet explorer comments
-        conditionals?: boolean;
+            // Do not remove conditional internet explorer comments
+            conditionals?: boolean;
 
-        // Do not remove redundant attributes
-        spare?: boolean;
+            // Do not remove redundant attributes
+            spare?: boolean;
 
-        // Do not remove arbitrary quotes
-        quotes?: boolean;
+            // Do not remove arbitrary quotes
+            quotes?: boolean;
 
-        // Preserve one whitespace
-        loose?: boolean;
+            // Preserve one whitespace
+            loose?: boolean;
+        }
     }
 
-    function minifyHtml(options?: IOptions): NodeJS.ReadWriteStream;
-
-    namespace minifyHtml {}
+    function minifyHtml(options?: minifyHtml.Options): NodeJS.ReadWriteStream;
 
     export = minifyHtml;
 }
