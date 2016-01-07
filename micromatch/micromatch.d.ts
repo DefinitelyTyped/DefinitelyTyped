da35ad861da5ba65611519f13ecd5fbfc32f966d
Glen <glen.84@gmail.com>
2016-01-07 16:49:05 +0200

    Update DefinitelyTyped repository URL

--- micromatch/micromatch.d.ts
+++ micromatch/micromatch.d.ts
@@ -1,7 +1,7 @@
 // Type definitions for micromatch 2.3.7
 // Project: https://github.com/jonschlinkert/micromatch
 // Definitions by: glen-84 <https://github.com/glen-84>
-// Definitions: https://github.com/borisyankov/DefinitelyTyped
+// Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
 
 /// <reference path="../parse-glob/parse-glob.ts" />
 
--- parse-glob/parse-glob.d.ts
+++ parse-glob/parse-glob.d.ts
@@ -1,7 +1,7 @@
 // Type definitions for parse-glob 3.0.4
 // Project: https://github.com/jonschlinkert/parse-glob
 // Definitions by: glen-84 <https://github.com/glen-84>
-// Definitions: https://github.com/borisyankov/DefinitelyTyped
+// Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
 
 declare namespace ParseGlob {
     interface Result {
