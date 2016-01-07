9028ed35ea3b66444911c567ebeaaed2a5e62c73
tiso <sophie.giughese@open-groupe.com>
2016-01-07 16:21:51 +0100

    Add sortDirectionCycle in interface IColumnDefOf

--- ui-grid/ui-grid.d.ts
+++ ui-grid/ui-grid.d.ts
@@ -3766,6 +3766,14 @@ declare module uiGrid {
          * You may specify one of the sortingAlgorithms found in the rowSorter service.
          */
         sortCellFiltered?: boolean;
+        /**
+		* (optional) An array of sort directions, specifying the order that they should cycle through as 
+        * the user repeatedly clicks on the column heading. The default is [null, uiGridConstants.ASC, uiGridConstants.DESC].
+        * Null refers to the unsorted state. This does not affect the initial sort direction; use the sort property for that.
+        * If suppressRemoveSort is also set, the unsorted state will be skipped even if it is listed here. Each direction may 
+        * not appear in the list more than once (e.g. [ASC, DESC, DESC] is not allowed), and the list may not be empty.*
+		*/
+        sortDirectionCycle?: Array<IUiGridConstants>;
         /** Algorithm to use for sorting this column */
         sortingAlgorithm?: (a: any, b: any) => number;
         /**
