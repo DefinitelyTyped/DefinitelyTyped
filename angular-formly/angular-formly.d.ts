4fc1870ffec60da9f2a9742613c46612b124c863
Niko Kovacic <niko.kovacic2@gmail.com>
2016-01-08 15:25:42 +0100

    Small lint fixes.

--- angular-formly/angular-formly.d.ts
+++ angular-formly/angular-formly.d.ts
@@ -16,8 +16,8 @@ declare module 'angular-formly' {
 
 declare module AngularFormly {
 
-    interface IFieldArray extends Array<IFieldConfigurationObject|IFieldGroup> {
-        
+    interface IFieldArray extends Array<IFieldConfigurationObject | IFieldGroup> {
+
     }
 
 	interface IFieldGroup {
@@ -160,7 +160,7 @@ declare module AngularFormly {
 		 */
 		asyncValidators?: {
 			[key: string]: string | IExpressionFunction | IValidator;
-		}
+		};
 
 		/**
 		 * This is a great way to add custom behavior to a specific field. It is injectable with the $scope of the
@@ -210,7 +210,7 @@ declare module AngularFormly {
 		 */
 		expressionProperties?: {
 			[key: string]: string | IExpressionFunction | IValidator;
-		}
+		};
 
 
 		/**
@@ -219,7 +219,7 @@ declare module AngularFormly {
 		 *
 		 * see http://docs.angular-formly.com/docs/field-configuration-object#hide-boolean
 		 */
-		hide?: boolean
+		hide?: boolean;
 
 
 		/**
@@ -432,7 +432,7 @@ declare module AngularFormly {
 			 */
 			show?: boolean;
 
-		}
+		};
 
 
 		/**
@@ -446,7 +446,7 @@ declare module AngularFormly {
 		 */
 		validators?: {
 			[key: string]: string | IExpressionFunction | IValidator;
-		}
+		};
 
 
 		/**
