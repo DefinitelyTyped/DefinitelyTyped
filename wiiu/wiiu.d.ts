5452638368da824143a14d2a1dd3c16f22c28799
mzsm <mizushimaj@gmail.com>
2016-01-09 20:40:38 +0900

    Rename title

--- wiiu/wiiu.d.ts
+++ wiiu/wiiu.d.ts
@@ -1,4 +1,4 @@
-// Type definitions for Wii U Internet Browser, Extended Functionality
+// Type definitions for Extended Functionality of Wii U Internet Browser
 // Project: https://www.nintendo.co.jp/wiiu/hardware/internetbrowser/extended_functionality.html
 // Definitions by: MIZUSHIMA Junki <https://github.com/mzsm>
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
