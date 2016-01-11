102ace9e8286bab0a7a495c9bdc931467bb94bde
Adam Burmister <adam.burmister@pinterest.com>
2016-01-11 15:44:12 -0800

    Tidy

--- pinterest-sdk/pinterest-sdk-tests.ts
+++ pinterest-sdk/pinterest-sdk-tests.ts
@@ -1,11 +1,11 @@
 /// <reference path="pinterest-sdk.d.ts" />
 
+// Examples from https://github.com/pinterest/pinterest-api-demo
+
 const PIN_FIELDS = "id,name,image[small]";
 const PIN_SCOPE =  "read_public, write_public";
 const CALLBACK = (...args: any[]) => {};
 const DATA = { board: "test", note: "test", link: "tets", image_url: "test" };
-
-// Examples from https://github.com/pinterest/pinterest-api-demo
 
 // Auth
 PDK.login({ scope : PIN_SCOPE }, CALLBACK);
