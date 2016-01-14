240d3560b2f29c04e202500d6778913575df8b5e
Matt Gibbs <matt.gibbs@hpe.com>
2016-01-14 15:13:26 -0500

    Bumping chartist version to 0.9.5

--- chartist/chartist.d.ts
+++ chartist/chartist.d.ts
@@ -1,4 +1,4 @@
-// Type definitions for Chartist v0.9.4
+// Type definitions for Chartist v0.9.5
 // Project: https://github.com/gionkunz/chartist-js
 // Definitions by: Matt Gibbs <https://github.com/mtgibbs>
 // Definitions: https://github.com/borisyankov/DefinitelyTyped
