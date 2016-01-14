21f5129d25ee0994b7bd0b68cb22ca30c983a9c8
Matt Gibbs <matt.gibbs@hpe.com>
2016-01-14 15:20:43 -0500

    Adding more tests for bar charts

--- chartist/chartist-tests.ts
+++ chartist/chartist-tests.ts
@@ -357,3 +357,28 @@ new Chartist.Line('.ct-chart', {
       fillHoles: false
     })
   });
+
+var overlappingBarsData: Chartist.IChartistData = {
+  labels: ['Jan', 'Feb', 'Mar', 'Apr', 'Mai', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'],
+  series: [
+    [5, 4, 3, 7, 5, 10, 3, 4, 8, 10, 6, 8],
+    [3, 2, 9, 5, 4, 6, 4, 6, 7, 8, 7, 4]
+  ]
+};
+
+var overlappingBarsOptions: Chartist.IBarChartOptions = {
+  seriesBarDistance: 10
+};
+
+var overlappingBarsResponsiveOptions: Array<Chartist.IResponsiveOptionTuple<Chartist.IBarChartOptions>> = [
+  ['screen and (max-width: 640px)', {
+    seriesBarDistance: 5,
+    axisX: {
+      labelInterpolationFnc: (value: any) => {
+        return value[0];
+      }
+    }
+  }]
+];
+
+new Chartist.Bar('.ct-chart', overlappingBarsData, overlappingBarsOptions, overlappingBarsResponsiveOptions);
