6d59005f672987b77efb0de1632889eba276d5a5
Philip Bjorge <philip.bjorge@leafly.com>
2016-01-19 23:31:41 -0800

    Switch my spaces to tabs in the test file

--- angular-idle/angular-idle-tests.ts
+++ angular-idle/angular-idle-tests.ts
@@ -3,33 +3,33 @@
 angular.module('app', ['ngIdle'])
 	.config(['KeepaliveProvider', 'IdleProvider', 'TitleProvider',
 		(keepaliveProvider: angular.idle.IKeepAliveProvider, idleProvider: angular.idle.IIdleProvider,
-         titleProvider: angular.idle.ITitleProvider) => {
+		 titleProvider: angular.idle.ITitleProvider) => {
 			idleProvider.interrupt('mousemove keydown DOMMouseScroll mousewheel mousedown');
 			idleProvider.idle(5);
 			idleProvider.timeout(5);
 			idleProvider.keepalive(true)
 			idleProvider.autoResume(true);
 
-            const config: ng.IRequestConfig = {
-                url: "http://google.com",
-                method: "GET"
-            };
+			const config: ng.IRequestConfig = {
+				url: "http://google.com",
+				method: "GET"
+			};
 
-            keepaliveProvider.http(config.url); // should accept string and ng.IRequestConfig
-            keepaliveProvider.http(config);
+			keepaliveProvider.http(config.url); // should accept string and ng.IRequestConfig
+			keepaliveProvider.http(config);
 			keepaliveProvider.interval(10);
 
-            titleProvider.enabled(true);
+			titleProvider.enabled(true);
 		}])
 	.run(['Keepalive', 'Idle', 'Title', (Keepalive: angular.idle.IKeepAliveService, Idle: angular.idle.IIdleService,
-                                         Title: angular.idle.ITitleService) => {
-        Idle.setTimeout(Idle.getTimeout());
-        Idle.setIdle(Idle.getIdle());
+										 Title: angular.idle.ITitleService) => {
+		Idle.setTimeout(Idle.getTimeout());
+		Idle.setIdle(Idle.getIdle());
 
 		Idle.watch();
-        Idle.interrupt();
+		Idle.interrupt();
 
-        const expired: boolean = Idle.isExpired();
+		const expired: boolean = Idle.isExpired();
 
 		if (Idle.running() || Idle.idling()) {
 			Idle.unwatch();
@@ -38,15 +38,15 @@ angular.module('app', ['ngIdle'])
 		Keepalive.start();
 		Keepalive.ping();
 		Keepalive.stop();
-        Keepalive.setInterval(10);
+		Keepalive.setInterval(10);
 
-        Title.setEnabled(Title.isEnabled());
-        Title.original(Title.original());
-        Title.value(Title.value());
-        Title.store(false);
-        Title.restore();
-        Title.idleMessage(Title.idleMessage());
-        Title.timedOutMessage(Title.timedOutMessage());
-        Title.setAsIdle(120);
-        Title.setAsTimedOut();
+		Title.setEnabled(Title.isEnabled());
+		Title.original(Title.original());
+		Title.value(Title.value());
+		Title.store(false);
+		Title.restore();
+		Title.idleMessage(Title.idleMessage());
+		Title.timedOutMessage(Title.timedOutMessage());
+		Title.setAsIdle(120);
+		Title.setAsTimedOut();
 	}]);
