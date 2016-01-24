16683f46ff8089fd89b6c2fc5822720b48cb26de
Dan Chao <dan@play-i.com>
2016-01-24 00:41:47 -0800

    indent 4 spaces

--- redux-simple-router/redux-simple-router.d.ts
+++ redux-simple-router/redux-simple-router.d.ts
@@ -7,51 +7,50 @@
 ///<reference path="../redux/redux.d.ts"/>
 
 declare module ReduxSimpleRouter {
-  interface transitionGoFunc {
-    (n?: number): { type: string; payload: Object }
-  }
-  interface transitionToLocationFunc {
-    (nextLocation: string): { type: string; payload: Object }
-  }
-  interface transitionFunc {
-    (): { type: string; payload: Object }
-  }
-  interface RouteActions {
-    push: transitionToLocationFunc;
-    replace: transitionToLocationFunc;
-    go: transitionGoFunc;
-    goBack: transitionFunc;
-    goForward: transitionFunc;
-  }
-  interface LocationState {
-    location: string;
-  }
-  interface Middleware extends Redux.Middleware {
-    listenForReplays: (store: Redux.Store) => any;
-  }
+    interface transitionGoFunc {
+        (n?: number): { type: string; payload: Object }
+    }
+    interface transitionToLocationFunc {
+        (nextLocation: string): { type: string; payload: Object }
+    }
+    interface transitionFunc {
+        (): { type: string; payload: Object }
+    }
+    interface RouteActions {
+        push: transitionToLocationFunc;
+        replace: transitionToLocationFunc;
+        go: transitionGoFunc;
+        goBack: transitionFunc;
+        goForward: transitionFunc;
+    }
+    interface LocationState {
+        location: string;
+    }
+    interface Middleware extends Redux.Middleware {
+        listenForReplays: (store: Redux.Store) => any;
+    }
 }
 
 declare module "redux-simple-router" {
-  import History = ReactRouter.History;
-  import LocationState = ReduxSimpleRouter.LocationState;
-  import Middleware = ReduxSimpleRouter.Middleware;
-  import RouteActions = ReduxSimpleRouter.RouteActions;
-  import transitionFunc = ReduxSimpleRouter.transitionFunc;
-  import transitionToLocationFunc = ReduxSimpleRouter.transitionToLocationFunc;
-  import transitionGoFunc = ReduxSimpleRouter.transitionGoFunc;
+    import History = ReactRouter.History;
+    import LocationState = ReduxSimpleRouter.LocationState;
+    import Middleware = ReduxSimpleRouter.Middleware;
+    import RouteActions = ReduxSimpleRouter.RouteActions;
+    import transitionFunc = ReduxSimpleRouter.transitionFunc;
+    import transitionToLocationFunc = ReduxSimpleRouter.transitionToLocationFunc;
+    import transitionGoFunc = ReduxSimpleRouter.transitionGoFunc;
 
-  export const TRANSITION: string;
-  export const UPDATE_LOCATION: string;
-  export const push: transitionToLocationFunc;
-  export const replace: transitionToLocationFunc;
-  export const go: transitionGoFunc;
-  export const goBack: transitionFunc;
-  export const goForward: transitionFunc;
+    export const TRANSITION: string;
+    export const UPDATE_LOCATION: string;
+    export const push: transitionToLocationFunc;
+    export const replace: transitionToLocationFunc;
+    export const go: transitionGoFunc;
+    export const goBack: transitionFunc;
+    export const goForward: transitionFunc;
 
-  export const routeActions: RouteActions;
+    export const routeActions: RouteActions;
 
-  export function routeReducer(state: LocationState, action: Object): LocationState;
+    export function routeReducer(state: LocationState, action: Object): LocationState;
 
-  export function syncHistory(history: History): Middleware;
-
+    export function syncHistory(history: History): Middleware;
 }
