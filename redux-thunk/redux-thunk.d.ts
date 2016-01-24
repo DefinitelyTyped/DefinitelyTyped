2764445943622084be4f5bde8fdc8147c1f5d662
Daniel Chao <daniel.h.chao@gmail.com>
2016-01-23 23:19:24 -0800

    Update redux-thunk.d.ts
    Namespace interface exports under another module

--- redux-thunk/redux-thunk.d.ts
+++ redux-thunk/redux-thunk.d.ts
@@ -5,17 +5,15 @@
 
 /// <reference path="../redux/redux.d.ts" />
 
-declare module "redux-thunk" {
+declare module ReduxThunk {
     import { Middleware, Dispatch } from 'redux';
-
-    export interface Thunk extends Middleware { }
-
+    export interface Thunk extends Middleware {};
     export interface ThunkInterface {
         <T>(dispatch: Dispatch, getState?: () => T): any;
     }
-
-    var thunk: Thunk;
-
-    export = thunk;
 }
 
+declare module "redux-thunk" {
+    var thunk: ReduxThunk.Thunk;
+    export = thunk;
+}
