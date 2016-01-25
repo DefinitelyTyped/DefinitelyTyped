ba84b1731c46fa7eec9aac1cf193604865b2d5ca
Zafix <willys58l@gmail.com>
2016-01-25 16:53:04 +0100

    Fix

--- riot-api-nodejs/riot-api-nodejs-tests.ts
+++ riot-api-nodejs/riot-api-nodejs-tests.ts
@@ -2,5 +2,5 @@
 
 import * as Api from "riot-api-nodejs";
 
-let ClassicApi = new Api.ClassicAPI([""], region_e.EUW);
+let ClassicApi = new Api.ClassicAPI([""], Api.region_e.EUW);
 let TournamentApi = new Api.TournamentAPI("");
