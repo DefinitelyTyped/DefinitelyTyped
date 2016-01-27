7dcde11de24f69bc8ded751b54feb04a6c4fd5b8
tkqubo <tk.qubo@gmail.com>
2016-01-27 13:19:08 +0900

    chore: add my name on definition

--- mock-fs/mock-fs.d.ts
+++ mock-fs/mock-fs.d.ts
@@ -1,6 +1,6 @@
 // Type definitions for mock-fs 3.6.0
 // Project: https://github.com/tschaub/mock-fs
-// Definitions by: Wim Looman <https://github.com/Nemo157>
+// Definitions by: Wim Looman <https://github.com/Nemo157>, Qubo <https://github.com/tkqubo>
 // Definitions: https://github.com/borisyankov/DefinitelyTyped
 
 /// <reference path="../node/node.d.ts" />
