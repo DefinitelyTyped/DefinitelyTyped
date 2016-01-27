c95ef825990f1f97c1ca9b9f21338242c592398a
tkqubo <tk.qubo@gmail.com>
2016-01-28 01:23:29 +0900

    add typings other than resources

--- asana/asana-tests.ts
+++ asana/asana-tests.ts
@@ -1,6 +1,12 @@
 /// <reference path="asana.d.ts" />
+/// <reference path="../request/request.d.ts" />
 
 import * as asana from 'asana';
 
 let version: string = asana.VERSION;
+
+let n: asana.auth.BaseBrowserFlow = new asana.auth.BaseBrowserFlow(null);
+
+
+import * as request from 'request';
 
