87c7de4e8f35961d9db8dc8f4b6e23c7a403ab8a
Stepan Mikhaylyuk <stepancar@hotmail.com>
2016-01-27 12:35:35 +0300

    fixed Promise.any
    the array passed to all can be a mixture of promise-like objects and other objects.

--- es6-promise/es6-promise.d.ts
+++ es6-promise/es6-promise.d.ts
@@ -57,7 +57,7 @@ declare module Promise {
 	 * the array passed to all can be a mixture of promise-like objects and other objects.
 	 * The fulfillment value is an array (in order) of fulfillment values. The rejection value is the first rejection value.
 	 */
-	function all<R>(promises: (R | Thenable<R>)[]): Promise<R[]>;
+	function all<any>(promises: (any | Thenable<any>)[]): Promise<any[]>;
 
 	/**
 	 * Make a Promise that fulfills when any item fulfills, and rejects if any item rejects.
