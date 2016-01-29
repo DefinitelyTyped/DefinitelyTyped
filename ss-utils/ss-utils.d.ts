861d4f8846969b1002f8aace6faa38cb0fc3d775
Demis Bellot <demis.bellot@gmail.com>
2016-01-29 06:36:02 -0500

    Change to use Module

--- ss-utils/ss-utils.d.ts
+++ ss-utils/ss-utils.d.ts
@@ -8,106 +8,112 @@
 interface JQuery {
     setFieldError: (name: string, msg: string) => void;
     serializeMap: () => { [index: string]: any };
-    applyErrors: (status: ResponseStatus, opt?: SSUtilsApplyErrorsOptions) => JQuery;
+    applyErrors: (status: ssutils.ResponseStatus, opt?: ssutils.ApplyErrorsOptions) => JQuery;
     clearErrors: () => JQuery;
-    bindForm: (opt?: SSUtilsApplyErrorsOptions) => JQuery;
+    bindForm: (opt?: ssutils.ApplyErrorsOptions) => JQuery;
     applyValues: (values: { [index: string]: string }) => JQuery;
     bindHandlers: (handlers: { [index: string]: Function }) => JQuery;
     setActiveLinks: () => JQuery;
-    handleServerEvents: (opt?: SSUtilsHandleServerEventsOptions) => void;
-}
-
-interface SSUtilsValidation {
-    overrideMessages: boolean;
-    messages: { [index: string]: string };
-    errorFilter: (errorMsg: string, errorCode: string, type: string) => void;
-}
-
-interface SSUtilsValidationOptional {
-    overrideMessages?: boolean;
-    messages?: { [index: string]: string };
-    errorFilter?: (errorMsg: string, errorCode: string, type: string) => void;
-}
-
-interface SSUtilsApplyErrorsOptions extends SSUtilsValidationOptional {
-}
-
-interface SSUtilsBindFormOptions {
-    validation?: SSUtilsValidationOptional;
-    validate?: (form: HTMLFormElement) => boolean;
-    onSubmitDisable?: string;
-    complete?: (...args: any[]) => void;
-    error?: (...args: any[]) => void;
-}
-
-interface SSUtilsHandleServerEventsOptions {
-    handlers?: { [index: string]: Function };
-    validate?: (op?: string, target?: string, msg?: string, json?: string) => boolean;
-    heartbeatUrl?: string;
-    heartbeatIntervalMs?: number;
-    unRegisterUrl?: string;
-    receivers?: { [index: string]: any };
-    success?: (selector: string, msg: string, e: any) => void;
-}
-
-interface ResponseStatus {
-    errorCode: string;
-    message: string;
-    stackTrace: string;
-    errors: ResponseError[];
-}
-interface ResponseError {
-    errorCode: string;
-    fieldName: string;
-    message: string;
-}
-
-interface SSUtilsStatic {
-    handlers: { [index: string]: Function };
-    onSubmitDisable: string;
-    validation: SSUtilsValidation;
-    clearAdjacentError: () => void;
-    todate: (s: string) => Date;
-    todfmt: (s: string) => string;
-    dfmt: (d: Date) => string;
-    dfmthm: (d: Date) => string;
-    tfmt12: (d: Date) => string;
-    splitOnFirst: (s: string) => string[];
-    splitOnLast: (s: string) => string[];
-    getSelection: () => string;
-    queryString: (url: string) => { [index: string]: string };
-    createUrl: (route: string, args?: any) => string;
-    humanize: (s: string) => string;
-
-    listenOn: string;
-    eventReceivers: any;
-    reconnectServerEvents: (opt: SSUtilsReconnectServerEventsOptions) => any;
-}
-
-interface SSUtilsReconnectServerEventsOptions {
-    url?: string;
-    onerror?: (...args: any[]) => void;
-    onmessage?: (...args: any[]) => void;
-    errorArgs: any[];
+    handleServerEvents: (opt?: ssutils.HandleServerEventsOptions) => void;
 }
 
 interface JQueryStatic {
-    ss: SSUtilsStatic;
+    ss: ssutils.Static;
 }
 
-interface SSUtilsSSECommand {
-    userId: string;
-    displayName: string;
-    channels: string;
-    profileUrl: string;
+declare module ssutils 
+{
+    interface Static {
+        handlers: { [index: string]: Function };
+        onSubmitDisable: string;
+        validation: Validation;
+        clearAdjacentError: () => void;
+        todate: (s: string) => Date;
+        todfmt: (s: string) => string;
+        dfmt: (d: Date) => string;
+        dfmthm: (d: Date) => string;
+        tfmt12: (d: Date) => string;
+        splitOnFirst: (s: string) => string[];
+        splitOnLast: (s: string) => string[];
+        getSelection: () => string;
+        queryString: (url: string) => { [index: string]: string };
+        createUrl: (route: string, args?: any) => string;
+        humanize: (s: string) => string;
+
+        listenOn: string;
+        eventReceivers: any;
+        reconnectServerEvents: (opt: ReconnectServerEventsOptions) => any;
+    }
+
+    interface Validation {
+        overrideMessages: boolean;
+        messages: { [index: string]: string };
+        errorFilter: (errorMsg: string, errorCode: string, type: string) => void;
+    }
+
+    interface ValidationOptional {
+        overrideMessages?: boolean;
+        messages?: { [index: string]: string };
+        errorFilter?: (errorMsg: string, errorCode: string, type: string) => void;
+    }
+
+    interface ApplyErrorsOptions extends ValidationOptional {
+    }
+
+    interface BindFormOptions {
+        validation?: ValidationOptional;
+        validate?: (form: HTMLFormElement) => boolean;
+        onSubmitDisable?: string;
+        complete?: (...args: any[]) => void;
+        error?: (...args: any[]) => void;
+    }
+
+    interface HandleServerEventsOptions {
+        handlers?: { [index: string]: Function };
+        validate?: (op?: string, target?: string, msg?: string, json?: string) => boolean;
+        heartbeatUrl?: string;
+        heartbeatIntervalMs?: number;
+        unRegisterUrl?: string;
+        receivers?: { [index: string]: any };
+        success?: (selector: string, msg: string, e: any) => void;
+    }
+
+    interface ResponseStatus {
+        errorCode: string;
+        message: string;
+        stackTrace: string;
+        errors: ResponseError[];
+    }
+    interface ResponseError {
+        errorCode: string;
+        fieldName: string;
+        message: string;
+    }
+
+    interface SSECommand {
+        userId: string;
+        displayName: string;
+        channels: string;
+        profileUrl: string;
+    }
+    
+    interface SSEHeartbeat extends SSECommand { }
+    interface SSEJoin extends SSECommand { }
+    interface SSELeave extends SSECommand { }
+    
+    interface SSEConnect extends SSECommand {
+        id: string;
+        unRegisterUrl: string;
+        heartbeatUrl: string;
+        heartbeatIntervalMs: number;
+        idleTimeoutMs: number;
+    }
+
+    interface ReconnectServerEventsOptions {
+        url?: string;
+        onerror?: (...args: any[]) => void;
+        onmessage?: (...args: any[]) => void;
+        errorArgs: any[];
+    }
 }
-interface SSUtilsSSEHeartbeat extends SSUtilsSSECommand {}
-interface SSUtilsSSEJoin extends SSUtilsSSECommand {}
-interface SSUtilsSSELeave extends SSUtilsSSECommand {}
-interface SSUtilsSSEConnect extends SSUtilsSSECommand {
-    id: string;
-    unRegisterUrl: string;
-    heartbeatUrl: string;
-    heartbeatIntervalMs: number;
-    idleTimeoutMs: number;
-}
+
