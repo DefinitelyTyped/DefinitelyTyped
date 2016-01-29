33fc9f203ca0478c4c051a8c87510cb66d2e0270
Demis Bellot <demis.bellot@gmail.com>
2016-01-29 06:36:02 -0500

    Change to use Module

--- ss-utils/ss-utils-tests.ts
+++ ss-utils/ss-utils-tests.ts
@@ -20,10 +20,10 @@ function test_ssutils() {
     var source = new EventSource("/event-stream?channels=home,work");
     $(source).handleServerEvents({
         handlers: {
-            onConnect: function(connect:SSUtilsSSEConnect) {},
-            onHeartbeat: function(msg:SSUtilsSSEHeartbeat, e:MessageEvent){},
-            onJoin: function(msg:SSUtilsSSEJoin) {},
-            onLeave: function(msg:SSUtilsSSELeave) {}            
+            onConnect: function(connect:ssutils.SSEConnect) {},
+            onHeartbeat: function(msg:ssutils.SSEHeartbeat, e:MessageEvent){},
+            onJoin: function(msg:ssutils.SSEJoin) {},
+            onLeave: function(msg:ssutils.SSELeave) {}            
         },
         receivers: {
             tv: {
@@ -37,5 +37,45 @@ function test_ssutils() {
     })
     .on('customEvent', function (e, msg, msgEvent) { });
     
+    $.ss.handlers["changeChannel"]("home");
+}
+
+function test_jQuery_functions(){
+    $("document").setFieldError("name","message");
+    var map = $("form").serializeMap();
+    $("form").applyErrors({errorCode:"",message:"",stackTrace:"",errors:[]});
+    $("form").clearErrors();
+    $("form").bindForm({
+        overrideMessages: true,
+        messages: {"NotFound": "Not Found"},
+        errorFilter: function(errorMsg, errorCode, type){}
+    });      
+    $("form").applyValues({
+        "Key": "Value"
+    });
+    $("form").bindHandlers({
+        "test": function() {}
+    });
+}
+
+function test_ssutils_Static(){
+    $.ss.handlers["key"] = () => 0;
+    $.ss.onSubmitDisable = "class";
+    $.ss.validation.messages["Code"] = "Message";
+    $.ss.clearAdjacentError();
+    var date:Date = $.ss.todate("2001-01-01");
+    var dateFmt:String = $.ss.todfmt("2001-01-01");
+    dateFmt = $.ss.dfmt(new Date(2001,1,1));
+    dateFmt = $.ss.dfmthm(new Date(2001,1,1));
+    dateFmt = $.ss.tfmt12(new Date(2001,1,1));
+    var parts:string[] = $.ss.splitOnFirst("A,B,C"); 
+    parts = $.ss.splitOnLast("A,B,C"); 
+    var selectedText = $.ss.getSelection();
+    var qs:{ [index: string]: string } = $.ss.queryString("http://google.com?a=b&c=d");
+    var relativePath = $.ss.createUrl("/path/to/{File}", {File:"file.js"});
+    var readableText = $.ss.humanize("TheVariableName");
+    
+    $.ss.listenOn = "click onmousedown";
+    $.ss.eventReceivers = { "document": document };
     $.ss.handlers["changeChannel"]("home");
 }
