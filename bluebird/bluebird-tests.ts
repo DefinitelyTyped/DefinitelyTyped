9a579744d2cd8fc66ad0d1aa7319cd6a7e56cb2f
Brandon Furtwangler <brandon.furtwangler@hotmail.com>
2016-02-03 08:59:20 -0800

    bluebird: fixed inference issue with try/attempt

--- bluebird/bluebird-tests.ts
+++ bluebird/bluebird-tests.ts
@@ -673,7 +673,16 @@ fooArrProm = fooArrProm.each<Foo, Bar>((item: Foo, index: number, arrayLength: n
 
 // - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
 
+function getMaybePromise(): Foo|Promise<Foo> {
+    return foo;
+}
 
+fooProm = Promise.try(() => {
+	return getMaybePromise();
+});
+fooProm = Promise.try<Foo>(() => {
+	return getMaybePromise();
+});
 fooProm = Promise.try(() => {
 	return foo;
 });
@@ -698,6 +707,12 @@ fooProm = Promise.try(() => {
 
 // - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
 
+fooProm = Promise.attempt(() => {
+	return getMaybePromise();
+});
+fooProm = Promise.attempt<Foo>(() => {
+	return getMaybePromise();
+});
 fooProm = Promise.attempt(() => {
 	return foo;
 });
