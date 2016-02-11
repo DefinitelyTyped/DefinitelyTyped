b0c256c13829e5847fe06d50c4e3103b76cde3ca
Douglas <site@xnet.com>
2016-02-11 10:04:59 -0600

    Add ability for D3 transition selection to create a nested transition selection

--- d3/d3.d.ts
+++ d3/d3.d.ts
@@ -838,6 +838,8 @@ declare module d3 {
         tween(name: string, factory: () => (t: number) => any): Transition<Datum>;
 
         remove(): Transition<Datum>;
+        
+        transition(name?: string): Transition<Datum>;
 
         select(selector: string): Transition<Datum>;
         select(selector: (d: Datum, i: number) => EventTarget): Transition<Datum>;
