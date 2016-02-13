53d93a78285f1b643ffbad81da3b18b776de934b
Demis Bellot <demis.bellot@gmail.com>
2016-02-12 22:43:11 -0500

    api tweaks

--- ss-utils/ss-utils.d.ts
+++ ss-utils/ss-utils.d.ts
@@ -20,8 +20,10 @@ declare namespace ssutils {
         splitOnFirst: (s: string, delimiter:string) => string[];
         splitOnLast: (s: string, delimiter: string) => string[];
         getSelection: () => string;
+        combinePaths: (...paths:string[]) => string;
         queryString: (url: string) => { [index: string]: string };
-        createUrl: (route: string, args?: any) => string;
+        createPath: (route: string, args: any) => string;
+        createUrl: (route: string, args: any) => string;
         humanize: (s: string) => string;
         parseResponseStatus: (json: string, defaultMsg?: string) => any;
         postJSON: (url: string, data: Object | String, success?: Function, error?: Function) => any;
