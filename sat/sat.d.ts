1a432d36329c799798e5c4d18fcf7a422698789b
omni360 <omni360@qq.com>
2016-02-15 17:56:18 +0800

    fixed error in travis-ci

--- sat/sat.d.ts
+++ sat/sat.d.ts
@@ -1,6 +1,6 @@
 // Type definitions for sat.js
 // Project: https://github.com/jriecken/sat-js
-// Definitions by: Hou,Chunlei <https://github.com/omni360>
+// Definitions by: Hou Chunlei <https://github.com/omni360>
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
 
 declare module SAT {
