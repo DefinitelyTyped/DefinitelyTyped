ccaf9fda1bb47b397cae07305082efad0f76c686
MorganZ <zerbini.morgan@gmail.com>
2016-02-17 13:06:28 +0100

    findDOMNode is now in the root node
    react dom is now at the root level
    
    more : some function are also removed in the current version, I haven't
    change it for compatibly for now (they start with unstable_*). can you
    check

--- react/react-dom.d.ts
+++ react/react-dom.d.ts
@@ -6,47 +6,45 @@
 /// <reference path="react.d.ts" />
 
 declare namespace __React {
-    namespace __DOM {
-        function findDOMNode<E extends Element>(instance: ReactInstance): E;
-        function findDOMNode(instance: ReactInstance): Element;
+    function findDOMNode<E extends Element>(instance: ReactInstance): E;
+    function findDOMNode(instance: ReactInstance): Element;
 
-        function render<P>(
-            element: DOMElement<P>,
-            container: Element,
-            callback?: (element: Element) => any): Element;
-        function render<P, S>(
-            element: ClassicElement<P>,
-            container: Element,
-            callback?: (component: ClassicComponent<P, S>) => any): ClassicComponent<P, S>;
-        function render<P, S>(
-            element: ReactElement<P>,
-            container: Element,
-            callback?: (component: Component<P, S>) => any): Component<P, S>;
+    function render<P>(
+        element: DOMElement<P>,
+        container: Element,
+        callback?: (element: Element) => any): Element;
+    function render<P, S>(
+        element: ClassicElement<P>,
+        container: Element,
+        callback?: (component: ClassicComponent<P, S>) => any): ClassicComponent<P, S>;
+    function render<P, S>(
+        element: ReactElement<P>,
+        container: Element,
+        callback?: (component: Component<P, S>) => any): Component<P, S>;
 
-        function unmountComponentAtNode(container: Element): boolean;
+    function unmountComponentAtNode(container: Element): boolean;
 
-        var version: string;
+    var version: string;
 
-        function unstable_batchedUpdates<A, B>(callback: (a: A, b: B) => any, a: A, b: B): void;
-        function unstable_batchedUpdates<A>(callback: (a: A) => any, a: A): void;
-        function unstable_batchedUpdates(callback: () => any): void;
+    function unstable_batchedUpdates<A, B>(callback: (a: A, b: B) => any, a: A, b: B): void;
+    function unstable_batchedUpdates<A>(callback: (a: A) => any, a: A): void;
+    function unstable_batchedUpdates(callback: () => any): void;
 
-        function unstable_renderSubtreeIntoContainer<P>(
-            parentComponent: Component<any, any>,
-            nextElement: DOMElement<P>,
-            container: Element,
-            callback?: (element: Element) => any): Element;
-        function unstable_renderSubtreeIntoContainer<P, S>(
-            parentComponent: Component<any, any>,
-            nextElement: ClassicElement<P>,
-            container: Element,
-            callback?: (component: ClassicComponent<P, S>) => any): ClassicComponent<P, S>;
-        function unstable_renderSubtreeIntoContainer<P, S>(
-            parentComponent: Component<any, any>,
-            nextElement: ReactElement<P>,
-            container: Element,
-            callback?: (component: Component<P, S>) => any): Component<P, S>;
-    }
+    function unstable_renderSubtreeIntoContainer<P>(
+        parentComponent: Component<any, any>,
+        nextElement: DOMElement<P>,
+        container: Element,
+        callback?: (element: Element) => any): Element;
+    function unstable_renderSubtreeIntoContainer<P, S>(
+        parentComponent: Component<any, any>,
+        nextElement: ClassicElement<P>,
+        container: Element,
+        callback?: (component: ClassicComponent<P, S>) => any): ClassicComponent<P, S>;
+    function unstable_renderSubtreeIntoContainer<P, S>(
+        parentComponent: Component<any, any>,
+        nextElement: ReactElement<P>,
+        container: Element,
+        callback?: (component: Component<P, S>) => any): Component<P, S>;
 
     namespace __DOMServer {
         function renderToString(element: ReactElement<any>): string;
