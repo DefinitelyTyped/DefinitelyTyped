0554ae6f308106ab45a14dee231f43ae7d9ee75b
omni360 <omni360@qq.com>
2016-02-20 23:51:37 +0800

    fix travis-ci test

--- js-clipper/js-clipper.d.ts
+++ js-clipper/js-clipper.d.ts
@@ -1,4 +1,4 @@
-// Type definitions for jsClipper.js
+// Type definitions for js-clipper
 // Project: https://github.com/mathisonian/JsClipper
 // Definitions by: Hou Chunlei <https://github.com/omni360>
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
