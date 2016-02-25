81e4e35ea7a366ff6b82e021e0be853659515e58
Sean Kelley <skelley@palantir.com>
2016-02-25 14:44:52 -0800

    Update typings for redux-logger to 2.6.0.

--- redux-logger/redux-logger.d.ts
+++ redux-logger/redux-logger.d.ts
@@ -1,4 +1,4 @@
-// Type definitions for redux-logger v2.0.0
+// Type definitions for redux-logger v2.6.0
 // Project: https://github.com/fcomb/redux-logger
 // Definitions by: Alexander Rusakov <https://github.com/arusakov/>
 // Definitions: https://github.com/borisyankov/DefinitelyTyped
@@ -7,16 +7,33 @@
 
 declare module 'redux-logger' {
 
-  interface ReduxLoggerOptions {
-    actionTransformer?: (action: any) => any;
-    collapsed?: boolean;
-    duration?: boolean;
-    level?: string;
-    logger?: any;
-    predicate?: (getState: Function, action: any) => boolean;
-    timestamp?: boolean;
-    stateTransformer?: (state: any) => any;
+  type LoggerPredicate = (getState: () => any, action: any) => boolean;
+
+  interface ColorsObject {
+    title?: (action: any) => string;
+    prevState?: (prevState: any) => string;
+    action?: (action: any) => string;
+    nextState?: (nextState: any) => string;
+    error?: (error: any, prevState: any) => string;
   }
 
-  export function createLogger(options?: ReduxLoggerOptions): Redux.Middleware;
+  interface ReduxLoggerOptions {
+    level?: string;
+    duration?: boolean;
+    timestamp?: boolean;
+    colors?: ColorsObject;
+    logger?: any;
+    logErrors?: boolean;
+    collapsed?: boolean | LoggerPredicate;
+    predicate?: LoggerPredicate;
+    stateTransformer?: (state: any) => any;
+    actionTransformer?: (action: any) => any;
+    errorTransformer?: (error: any) => any;
+  }
+
+  // Trickery to get TypeScript to accept that our anonymous, non-default export is a function.
+  // see https://github.com/Microsoft/TypeScript/issues/3612 for more
+  namespace createLogger {}
+  function createLogger(options?: ReduxLoggerOptions): Redux.Middleware;
+  export = createLogger;
 }
