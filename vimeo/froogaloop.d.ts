a6f84522683e0abd14258ffeb0162b9e4c3d1cff
Laszlo Pandy <laszlo.pandy@prezi.com>
2016-02-25 14:19:57 +0100

    Whitespace only: remove tabs in foogaloop.d.ts

--- vimeo/froogaloop.d.ts
+++ vimeo/froogaloop.d.ts
@@ -4,15 +4,17 @@
 // Definitions: https://github.com/borisyankov/DefinitelyTyped
 
 interface VimeoParams {
-	name:string;
-	value:any;
+    name:string;
+    value:any;
 }
+
 interface VimeoPlayerAPI {
     (method: string): any;
     (method: string, callback: (value: any, player_id: any) =>void ): any;
-	(method: string, value: any): any;
-	(method: string, value: VimeoParams[]): any;
+    (method: string, value: any): any;
+    (method: string, value: VimeoParams[]): any;
 }
+
 interface VimeoPlayer {
     api: VimeoPlayerAPI;
     addEvent(eventName: string, callback: (e: any) =>void ): any;
