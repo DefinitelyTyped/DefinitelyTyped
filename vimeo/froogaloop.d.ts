cbf669a7a0cb7e236ef1ee252cc836344a4a9417
Laszlo Pandy <laszlo.pandy@prezi.com>
2016-02-25 14:22:36 +0100

    Whitespace only.

--- vimeo/froogaloop.d.ts
+++ vimeo/froogaloop.d.ts
@@ -10,14 +10,14 @@ interface VimeoParams {
 
 interface VimeoPlayerAPI {
     (method: string): any;
-    (method: string, callback: (value: any, player_id: any) =>void ): any;
+    (method: string, callback: (value: any, player_id: any) => void): any;
     (method: string, value: any): any;
     (method: string, value: VimeoParams[]): any;
 }
 
 interface VimeoPlayer {
     api: VimeoPlayerAPI;
-    addEvent(eventName: string, callback: (e: any) =>void ): any;
+    addEvent(eventName: string, callback: (e: any) => void): any;
     removeEvent(eventName: string): void;
 }
 
