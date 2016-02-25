6dec5e1b65f9a670ffbc10685eaab5678eb8cb11
Laszlo Pandy <laszlo.pandy@prezi.com>
2016-02-25 14:21:23 +0100

    Fix vimeo/froogaloop.d.ts

--- vimeo/froogaloop.d.ts
+++ vimeo/froogaloop.d.ts
@@ -19,12 +19,7 @@ interface VimeoPlayer {
     api: VimeoPlayerAPI;
     addEvent(eventName: string, callback: (e: any) =>void ): any;
     removeEvent(eventName: string): void;
-    postMessage(method: string, params:VimeoParams[], target): void;
-    onMessagReceived(event);
-    storeCallback(eventName: string, callback, target_id: string);
-    getCallback(eventName: string, target_id: string);
-    removeCallback(eventName: string, target_id: string);
-    getDomainFromUrl(url: string): string;
 }
 
 declare var $f: VimeoPlayerAPI;
+declare var Froogaloop: VimeoPlayerAPI;
