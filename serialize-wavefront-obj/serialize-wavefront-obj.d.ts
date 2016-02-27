1580a5aadd41b1f1f1b336f777fdcd9eb6e34dbf
HouChunlei <omni360@qq.com>
2016-02-27 22:06:52 +0800

    update

--- serialize-wavefront-obj/serialize-wavefront-obj.d.ts
+++ serialize-wavefront-obj/serialize-wavefront-obj.d.ts
@@ -4,5 +4,7 @@
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
 
 declare module serialize {
-	export function serialize(cells: ArrayLike<ArrayLike<number>>, positions: ArrayLike<ArrayLike<number>>, vertexNormals: ArrayLike<number>, vertexUVs: ArrayLike<number>, faceNormals: ArrayLike<ArrayLike<number>>, faceUVs: ArrayLike<ArrayLike<number>>, name: string): ArrayLike<string>;
+	export function serialize(cells: ArrayLike<ArrayLike<number>>, positions: ArrayLike<ArrayLike<number>>,
+		 vertexNormals?: ArrayLike<number>, vertexUVs?: ArrayLike<number>, faceNormals?: ArrayLike<ArrayLike<number>>, 
+		 faceUVs?: ArrayLike<ArrayLike<number>>, name?: string): ArrayLike<string>;
 }
