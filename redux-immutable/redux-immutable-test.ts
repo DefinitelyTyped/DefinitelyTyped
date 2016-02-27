d5595b6c9514dc405f4b84c3bf0f7c320dd2cbd6
Pedro.Pereira <cyberafael@msn.com>
2016-02-27 19:08:47 +0000

    Fixed References

--- redux-immutable/redux-immutable-test.ts
+++ redux-immutable/redux-immutable-test.ts
@@ -1,7 +1,7 @@
 /// <reference path="redux-immutable.d.ts" />
 
 import { combineReducers } from 'redux-immutable';
-import { routerReducer } from 'react-router-redux';
+import { routerReducer } from '../react-router-redux/react-router-redux';
 
 combineReducers({
    routing: routerReducer
