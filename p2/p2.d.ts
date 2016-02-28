3a8a48a0460a4e864d542449d75cfaf2b3d30948
Adam Keenan <adam.g.keenan@gmail.com>
2016-02-27 23:06:55 -0600

    P2 module not in quotes

--- p2/p2.d.ts
+++ p2/p2.d.ts
@@ -3,7 +3,7 @@
 // Definitions by: Clark Stevenson <https://github.com/clark-stevenson>
 // Definitions: https://github.com/borisyankov/DefinitelyTyped
 
-declare module p2 {
+declare module "p2" {
 
     export class AABB {
 
