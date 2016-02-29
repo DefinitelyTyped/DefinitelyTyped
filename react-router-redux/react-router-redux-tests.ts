c953f5e0bcb88afbd522e330ce4f3b1e0207c02c
Isman Usoh <isman-usoh@users.noreply.github.com>
2016-02-29 13:28:07 +0700

    Update react-router-redux-tests.ts

--- react-router-redux/react-router-redux-tests.ts
+++ react-router-redux/react-router-redux-tests.ts
@@ -6,10 +6,25 @@
 
 import { createStore, combineReducers, applyMiddleware } from 'redux';
 import { browserHistory } from 'react-router';
-import { syncHistoryWithStore, routerReducer } from 'react-router-redux';
+import { syncHistoryWithStore, routerReducer, routerMiddleware, push, replace, go, goForward, goBack } from 'react-router-redux';
 
 const reducer = combineReducers({ routing: routerReducer });
-const store = createStore(reducer);
+
+// Apply the middleware to the store
+const middleware = routerMiddleware(browserHistory);
+const store = createStore(
+    reducer,
+    applyMiddleware(middleware)
+);
 
 // Create an enhanced history that syncs navigation events with the store
-const history = syncHistoryWithStore(browserHistory, store)
+const history = syncHistoryWithStore(browserHistory, store);
+history.listen(location => console.log(location) );
+history.unsubscribe();
+
+// Dispatch from anywhere like normal.
+store.dispatch(push('/foo'));
+store.dispatch(replace('/foo'));
+store.dispatch(go(1));
+store.dispatch(goForward());
+store.dispatch(goBack());
