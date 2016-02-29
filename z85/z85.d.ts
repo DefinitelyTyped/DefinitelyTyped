2dc4b8b51a1ce5e91538d8da2382201a5efc2a74
Michael Kriegel <michael.kriegel@actifsource.com>
2016-02-29 10:55:38 +0100

    Created definition file for the z85 library (on npm).

---
+++ z85/z85.d.ts
@@ -0,0 +1,9 @@
+// Type definitions for z85
+// Project: https://github.com/msealand/z85.node
+// Definitions by: 0815fox https://github.com/0815fox
+// Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
+
+declare module z85 {
+	function encode(data:Buffer):string;
+	function decode(string:string):Buffer;
+}
