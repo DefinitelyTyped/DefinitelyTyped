7f1d0c5ea8e16b5edd2c72f4480dbdc0cf0efb25
flyfishMT <flyfishMT69@yahoo.com>
2016-03-01 16:37:22 -0700

    kolite command, dirtyflag are no longer added to ko but are added to AMD exports. they are also broken into 3 different files. changed to reflect this and added ambient declarations so everything works in typescript with requirejs.

---
+++ kolite/knockout.command.d.ts
@@ -0,0 +1,44 @@
+// Type definitions for KoLite 1.1
+// Project: https://github.com/CodeSeven/kolite
+// Definitions by: Boris Yankov <https://github.com/borisyankov>
+// Definitions: https://github.com/borisyankov/DefinitelyTyped
+
+
+/// <reference path="../jquery/jquery.d.ts" />
+/// <reference path="../knockout/knockout.d.ts" />
+
+
+// Command /////////////////////////////////////////////
+
+interface KoliteCommand {
+    canExecute: KnockoutComputed<boolean>;
+    execute(...args: any[]): any;
+}
+
+interface KoliteAsyncCommand extends KoliteCommand {
+    isExecuting: KnockoutObservable<boolean>;
+}
+
+interface KoLiteCommandOptions {
+    execute(...args: any[]): any;
+    canExecute?: (isExecuting: boolean) => any;
+}
+
+interface KnockoutCommandStatic {
+    command(options: KoLiteCommandOptions): KoliteCommand;
+    asyncCommand(options: KoLiteCommandOptions): KoliteAsyncCommand;
+}
+
+interface KnockoutUtils {
+    wrapAccessor(accessor): Function;
+}
+
+interface KnockoutBindingHandlers {
+    command: KnockoutBindingHandler;
+}
+
+declare var kocommand: KnockoutCommandStatic;
+declare module 'kocommand'{
+    export = kocommand;
+}
+
