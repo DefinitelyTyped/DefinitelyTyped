40fdafd956ffb3b93e836f792458606f175c0bcd
Markus Mauch <markus.mauch@gmail.com>
2016-03-03 16:55:41 +0100

    Adding reop url

--- microsoft-sdk-soap/microsoft-sdk-soap.d.ts
+++ microsoft-sdk-soap/microsoft-sdk-soap.d.ts
@@ -1,10 +1,9 @@
 // Type definitions for Sdk.Soap.js
 // Project: https://code.msdn.microsoft.com/SdkSoapjs-9b51b99a
-// Definitions by: Markus Mauch
+// Definitions by: Markus Mauch <https://github.com/markusmauch/>
 // Definitions: https://github.com/borisyankov/DefinitelyTyped
 
 /// <reference path="../q/Q.d.ts"/>
-
 
 declare module Sdk
 {
