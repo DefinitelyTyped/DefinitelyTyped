f178a4337634ca1783ef1daaf40a6523c3837285
oizie <cyberafael2005@gmail.com>
2016-03-04 16:26:46 +0000

    added new react scroll type definitions and tests

---
+++ react-scroll/react-scroll.d.ts
@@ -0,0 +1,36 @@
+// Type definitions for redux-immutable v3.0.5
+// Project: https://github.com/fisshy/react-scroll
+// Definitions by: Pedro Pereira <https://github.com/oizie>
+// Definitions: https://github.com/borisyankov/DefinitelyTyped
+
+declare module "react-scroll" {
+    import React = __React
+
+    interface Link extends React.ClassicComponentClass<any> {}
+    interface Element extends React.ClassicComponentClass<any> {}
+
+    interface scrollEvnt {
+        register(evtName: string, callback: Function) : void,
+        remove(evtName: string) : void
+    }
+
+    interface Events {
+        scrollEvent: scrollEvnt
+    }
+
+    interface scroller {
+        scrollTo(to: any, animate?: any, duration?: any, offset?: any)
+    }
+
+    const Link: Link;
+    const Element: Element;
+    const Events: Events;
+    const scroller: scroller;
+
+    export {
+        Link,
+        Element,
+        Events,
+        scroller
+    }
+}
