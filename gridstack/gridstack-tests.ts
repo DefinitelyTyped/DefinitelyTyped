cdb32fdbd2c4a587b0832b26514bca91dac653a6
David Deutsch <david@reverenddave.com>
2016-03-06 16:15:19 -0500

    Changed old tests file to reference old definition file

--- gridstack/gridstack-tests.ts
+++ gridstack/gridstack-tests.ts
@@ -1,20 +1,14 @@
 ﻿/// <reference path="../jquery/jquery.d.ts"/>
 /// <reference path="gridstack.d.ts" />
 
-
-// Type definitions for Gridstack
-// Project: http://troolee.github.io/gridstack.js/
-// Definitions by: Pascal Senn <https://github.com/PascalSenn/>
-// Definitions: https://github.com/borisyankov/DefinitelyTyped
-
-var options = <IGridstackOptions> {
+let options = {
     float: true
 };
-var gridstack:GridStack = $(document).gridstack(options);
+let gridstack = $(document).gridstack(options).data("gridstack");
 
-gridstack.add_widget("test", 1, 2, 3, 4, true);
-gridstack.batch_update();
-gridstack.cell_height();;
-gridstack.cell_height(2);
-gridstack.cell_width();
-gridstack.get_cell_from_pixel(<MousePosition>{ left:20, top: 20 });
+gridstack.addWidget("test", 1, 2, 3, 4, true);
+gridstack.batchUpdate();
+gridstack.cellHeight();
+gridstack.cellHeight("2px");
+gridstack.cellWidth();
+gridstack.getCellFromPixel({ left:20, top: 20 });
