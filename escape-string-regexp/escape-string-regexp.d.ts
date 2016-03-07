01e087c146f8e4230cbc489509253bc056cec68b
Lea Hayes <lea@rotorz.com>
2016-03-07 23:33:57 +0000

    Add escape-string-regexp type definition.

---
+++ escape-string-regexp/escape-string-regexp.d.ts
@@ -0,0 +1,10 @@
+// Type definitions for escape-string-regexp
+// Project: https://github.com/sindresorhus/escape-string-regexp
+// Definitions by: kruncher <https://github.com/kruncher/>
+// Definitions: https://github.com/borisyankov/DefinitelyTyped
+
+declare module "escape-string-regexp" {
+  
+  export default function (str: string): string;
+  
+}
