3d51b999a3094d0b877f51f6dab0497e87fc41c7
etuxxyl <nicole.xx.wang@ericsson.com>
2016-03-08 13:13:09 +0800

    beautify soap definition files

--- soap/soap.d.ts
+++ soap/soap.d.ts
@@ -4,14 +4,14 @@
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
 
 declare module 'soap' {
-  class WSSecurity {
-    constructor(username: string, password: string, options: any);
-  }
-  interface Client {
-    setSecurity(s: WSSecurity): void;
-    [method: string]: (args: any, fn: (err: any, result: any) => void, options?: any) => void;
-    addSoapHeader(headJSON: any): void;
-  }
-  function createClient(wsdlPath: string, options: any, fn: (err: any, client: Client) => void): void;
+    class WSSecurity {
+        constructor(username: string, password: string, options: any);
+    }
+    interface Client {
+        setSecurity(s: WSSecurity): void;
+        [method: string]: (args: any, fn: (err: any, result: any) => void, options?: any) => void;
+        addSoapHeader(headJSON: any): void;
+    }
+    function createClient(wsdlPath: string, options: any, fn: (err: any, client: Client) => void): void;
 
 }
