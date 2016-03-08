bf2b360545d0b2a9b0e852737bc1ae968b8685b7
etuxxyl <nicole.xx.wang@ericsson.com>
2016-03-08 13:13:09 +0800

    beautify soap definition files

--- soap/soap-tests.ts
+++ soap/soap-tests.ts
@@ -3,16 +3,16 @@
 import * as soap from 'soap';
 
 const url = 'http://example.com/wsdl?wsdl';
-const wsdlOptions = {name: 'value'};
+const wsdlOptions = { name: 'value' };
 
 soap.createClient(url, wsdlOptions, function(err: any, client: soap.Client) {
     let securityOptions = { 'hasTimeStamp': false };
     client.setSecurity(new soap.WSSecurity('user', 'password', securityOptions));
     client.addSoapHeader({});
-    client['create']({name: 'value'}, function(err, result) {
+    client['create']({ name: 'value' }, function(err, result) {
         // result is an object
     });
-    client['create']({name: 'value'}, function(err, result) {
+    client['create']({ name: 'value' }, function(err, result) {
         // result is an object
     }, {});
 });
