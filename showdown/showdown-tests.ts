c665847f6b9a701c2eec43a9908d4aebac363950
Aaron <coder@wiredprairie.us>
2016-03-07 20:15:59 -0600

    Update showdown-tests.ts
    Remove tests that can't run without extra install work.

--- showdown/showdown-tests.ts
+++ showdown/showdown-tests.ts
@@ -3,18 +3,10 @@
 // compile: tsc showdown/showdown-tests.ts --noImplicitAny --module commonjs
 // run: node showdown/showdown-tests.js
 
-import Showdown = require('showdown');
+import * as Showdown from 'showdown';
 
 var exampleMarkdown = '#hello, markdown',
-    converter = new Showdown.converter(),
-    preloadedExtensions = [ 'github', 'twitter', 'prettify', 'table' ],
-    extensionsConverter = new Showdown.converter({ extensions: preloadedExtensions });
+    converter = new Showdown.Converter();
 
 console.log(converter.makeHtml(exampleMarkdown));
 // should log '<h1 id="hellomarkdown">hello, markdown</h1>'
-
-console.log(extensionsConverter.makeHtml(exampleMarkdown));
-// should log '<p><a href="http://twitter.com/search/%23hello">#hello</a>, markdown</p>'
-
-Showdown.forEach(preloadedExtensions, console.log);
-// should log each item in the above array
