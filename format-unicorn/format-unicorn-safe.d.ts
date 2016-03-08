9990c208c8cad3f363434c54789c03fe896cfe53
Lea Hayes <lea@rotorz.com>
2016-03-08 01:08:01 +0000

    Add format-unicorn type definition.

---
+++ format-unicorn/format-unicorn-safe.d.ts
@@ -0,0 +1,10 @@
+// Type definitions for format-unicorn
+// Project: https://github.com/tallesl/format-unicorn
+// Definitions by: kruncher <https://github.com/kruncher/>
+// Definitions: https://github.com/borisyankov/DefinitelyTyped
+
+declare module 'format-unicorn/safe' {
+
+  export default function (str: string, replacements: { }): string;
+
+}
