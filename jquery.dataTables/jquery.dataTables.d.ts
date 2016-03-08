13a1418140ce742b9c6999b5b5013654e9f81469
vankooch <koochy@ky-pro.loc>
2016-03-08 13:55:27 +0100

    Add Buttons plug-in to definition

--- jquery.dataTables/jquery.dataTables.d.ts
+++ jquery.dataTables/jquery.dataTables.d.ts
@@ -1,11 +1,11 @@
-﻿// Type definitions for JQuery DataTables 1.10.5
+﻿// Type definitions for JQuery DataTables 1.10.11
 // Project: http://www.datatables.net
 // Definitions by: Kiarash Ghiaseddin <https://github.com/Silver-Connection/DefinitelyTyped>, Omid Rad <https://github.com/omidkrad>, Armin Sander <https://github.com/pragmatrix/>
 // Definitions: https://github.com/borisyankov/DefinitelyTyped
 
 // missing:
 // - Static methods that are defined in JQueryStatic.fn are not typed.
-// - Plugin and extension definitions are not typed.
+// - Not all plugins and extension definitions are not typed.
 // - Some return types are not fully wokring
 
 /// <reference path="../jquery/jquery.d.ts"/>
@@ -1063,11 +1063,16 @@ declare module DataTables {
         * Feature control DataTables' smart column width handling. Since: 1.10
         */
         autoWidth?: boolean;
+        
+        /**
+        * This option provides the ability to have a Buttons instance created automatically when a new DataTable is created. You need teh Buttons plug-in
+        */
+        buttons?: ButtonsSettings | Array<string | ButtonsButtonsSettings>;
 
         /**
         * Feature control deferred rendering for additional speed of initialisation. Since: 1.10
         */
-        deferRender?: boolean;
+        deferRender?: boolean | string[];
 
         /**
         * Feature control table information display field. Since: 1.10
@@ -1831,5 +1836,168 @@ declare module DataTables {
         sPrevious?: string;
     }
     //#endregion "SettingsLegacy"
+
+    //#region "Plug-in: Buttons"
+    
+    export interface ButtonsSettings {
+        /**
+        * As multiple Buttons instances can be attached to a single DataTable, it can be useful to be able to select each instance individually. Since: Buttons 1.0.0
+        */
+        name?: string;
+        
+        /**
+        * Options to control the DOM structure Buttons creates. Since: Buttons 1.0.0
+        */
+        dom?: ButtonsDomSettings;
+        
+        /**
+        * List of buttons to be created. Since: Buttons 1.0.0
+        */
+        buttons?: ButtonsButtonsSettings | Array<string | ButtonsButtonsSettings>;
+    }
+    
+    export interface ButtonsDomSettings {
+        /**
+        * As multiple Buttons instances can be attached to a single DataTable, it can be useful to be able to select each instance individually. Since: Buttons 1.0.0
+        */
+        container?: ButtonsDomContainerSettings;
+        
+        /**
+        * DOM configuration for individual button elements.
+        */
+        button?: ButtonsDomButtonSettings;
+        buttons?: ButtonsDomButtonSettings;
+        
+        /**
+        * DOM configuration of a button container element.
+        */
+        buttonContainer?: ButtonsDomContainerSettings;
+        
+        /**
+        * DOM configuration of a button liner element.
+        */
+        buttonLiner?: ButtonsDomContainerSettings;
+        
+        /**
+        * DOM configuration of the collection display.
+        */
+        collection?: ButtonsDomContainerSettings;
+    }
+    
+    export interface ButtonsDomContainerSettings {
+        /**
+        * Value which defines the element's class name. Multiple classes can be given using space separation.
+        */
+        className?: string;
+        
+        /**
+        * Value which defines the HTML tag to use. There should be no spaces or any other formatting - e.g. it should simply be div, aside, etc.
+        */
+        tag?: string;
+    }
+        
+    export interface ButtonsDomButtonSettings extends ButtonsDomContainerSettings {
+        /**
+        * The class name to assign to the button when the button is in the disabled state.
+        */
+        active?: string;
+        
+        /**
+        * The class name to assign to the button when the button is in the active state.
+        */
+        disabled?: string;
+    }
+    
+    export interface ButtonsButtonsSettings {
+        /**
+        * Action to take when the button is activated.
+        */
+        action?: (e: Event, dt: DataTables, node: JQuery, config: ButtonsSettings) => void;
+        
+        /**
+        * Function that is called when the button is destroyed.
+        */
+        available?: (dt: DataTables, config: ButtonsButtonsSettings) => boolean;
+        
+        /**
+        * Set the class name for the button.
+        */
+        className?: string;
+        
+        /**
+        * Set a button's initial enabled state.
+        */
+        enabled?: boolean;
+        disable?: boolean;
+        
+        /**
+        * Define which button type the button should be based on.
+        */
+        extend?: string;
+        
+        /**
+        * Initialisation function that can be used to add events specific to this button.
+        */
+        init?: (dt: DataTables, node: JQuery, config: ButtonsSettings) => void;
+        
+        /**
+        * Define an activation key for a button.
+        */
+        key?: string | ButtonsButtonsKeySettings;
+        
+        /**
+        * Set a name for each selection.
+        */
+        name?: string;
+        
+        /**
+        * The text to show in the button.
+        */
+        text?: (dt: DataTables, node: JQuery, config: ButtonsSettings) => string | string;
+        
+        /**
+        * Button title attribute text.
+        */
+        titleAttrSince?: string;
+        
+        /**
+        * Unique namespace for every button.
+        */
+        namespace?: string;
+        
+        /**
+        * Function that is called when the button is destroyed.
+        */
+        destroy?: (dt: DataTables, node: JQuery, config: ButtonsButtonsSettings) => void;
+    }
+    
+    export interface ButtonsButtonsKeySettings {
+        /**
+        * The character to listen for. The character is case insensitive.
+        */
+        key?: string;
+        
+        /**
+        * When set to true activation will only occur if the shift key is also being held.
+        */
+        shiftKey?: boolean;
+        
+        /**
+        * When set to true activation will only occur if the alt key is also being held.
+        */
+        altKey?: boolean;
+        
+        /**
+        * When set to true activation will only occur if the ctrl key is also being held.
+        */
+        ctrlKey?: boolean;
+        
+        /**
+        * When set to true activation will only occur if the cmd key (Mac) or Windows key (Windows) is also being held.
+        */
+        metaKey?: boolean;
+    }
+    
+    //#endregion "Plug-in: Buttons"
 
 }
