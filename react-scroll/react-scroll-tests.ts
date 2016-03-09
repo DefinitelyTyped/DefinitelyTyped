16603d175eaa380de1a306aaaab181fcfe9b1943
oizie <cyberafael2005@gmail.com>
2016-03-09 17:28:17 +0000

    Fixed test

--- react-scroll/react-scroll-tests.ts
+++ react-scroll/react-scroll-tests.ts
@@ -1,5 +1,7 @@
 /// <reference path="./react-scroll.d.ts" />
+/// <reference path="../react/react.d.ts" />
 
+import * as React from 'react';
 import { Link, Element, Events, scroller } from 'react-scroll';
 
 class LinkTest extends React.Component<React.Props<{}>, {}> {
