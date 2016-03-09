e3ea8c19b8d38511be08cc7c130f2578e84d7dd8
Gilad Gray <ggray@palantir.com>
2016-03-09 13:30:15 -0800

    update typings for React Select 1.0.0
    - use `export = var` syntax so it works as a global library
    - put interfaces in ghost module so they don't pollute global namespace
    - add JSDoc comments for each prop
    - move old version to -0.9.10

--- react-select/react-select-0.9.10-tests.tsx
+++ react-select/react-select-0.9.10-tests.tsx
@@ -1,19 +1,18 @@
 
 /// <reference path="../react/react.d.ts" />
 /// <reference path="../react/react-dom.d.ts" />
-/// <reference path="./react-select.d.ts" />
+/// <reference path="./react-select-0.9.10.d.ts" />
 
 import * as React from "react"
 import * as ReactDOM from "react-dom"
 
-import * as Select from "react-select"
+import Select from "react-select"
 
 class SelectTest extends React.Component<React.Props<{}>, {}> {
 
     render() {
-        const options: ReactSelect.Option[] = [{ label: "Foo", value: "bar" }]
         return <div>
-           <Select options={options} />
+           <Select />
         </div>
     }
 
