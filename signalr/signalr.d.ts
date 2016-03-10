1a700797659fe8c250a869b358bc8c81d520ed9b
Giedrius Grabauskas <GiedriusGrabauskas@users.noreply.github.com>
2016-03-10 17:23:33 +0200

    Added 1.0 version contributors.

--- signalr/signalr.d.ts
+++ signalr/signalr.d.ts
@@ -1,6 +1,6 @@
 // Type definitions for SignalR 2.2.0
 // Project: http://www.asp.net/signalr
-// Definitions by: Giedrius Grabauskas <https://github.com/GiedriusGrabauskas>
+// Definitions by: Boris Yankov <https://github.com/borisyankov/>, T. Michael Keesey <https://github.com/keesey/>, Giedrius Grabauskas <https://github.com/GiedriusGrabauskas>
 // Definitions: https://github.com/borisyankov/DefinitelyTyped
 
 
