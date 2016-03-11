c140ea2c2eaa0e0b18b9a8d2ab2894d9f4f5cc27
Eugene Gusarov <gusarov.email@gmail.com>
2016-03-12 01:35:53 +0300

    fixed definition and tests

--- phantom/phantom.d.ts
+++ phantom/phantom.d.ts
@@ -1,6 +1,6 @@
 // Type definitions for PhantomJS bridge for NodeJS
 // Project: https://github.com/sgentle/phantomjs-node
-// Definitions by: Random <https://github.com/llRandom/>
+// Definitions by: horiuchi <https://github.com/horiuchi/>, Random <https://github.com/llRandom/>
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
 
 declare module "phantom" {
@@ -14,37 +14,43 @@ declare module "phantom" {
 
   interface WebPage {
     open(url: string): Promise<string>;
-    close(): void;
-
-    get(key: string): Promise<any>;
-    set(key: string, value: any): Promise<any>;
-    setHeaders(headers: Object): Promise<void>;
-    setViewportSize(width: number, height: number): Promise<void>;
-    setPaperSize(options: IPaperSizeOptions): Promise<void>;
-    setZoomFactor(factor: number): Promise<void>;
+    close(): Promise<void>;
 
     evaluate<R>(callback: () => R): Promise<R>;
     evaluate<T>(callback: (arg: T) => void, arg: T): Promise<void>;
     evaluate<T, R>(callback: (arg: T) => R, arg: T): Promise<R>;
     evaluate<T1, T2, R>(callback: (arg1: T1, arg2: T2) => R, arg1: T1, arg2: T2): Promise<R>;
     evaluate<T1, T2, T3, R>(callback: (arg1: T1, arg2: T2, arg3: T3) => R, arg1: T1, arg2: T2, arg3: T3): Promise<R>;
+    evaluate<R>(callback: (...args: any[]) => R, ...args: any[]): Promise<R>;
     includeJs(url: string): Promise<void>;
     injectJs(filename: string): Promise<boolean>;
-    sendEvent(mouseEventType: string, mouseX: number, mouseY: number, button?: string): void;
-    sendEvent(keyboardEventType: string, key: string, null1?: void, null2?: void, modifier?: number): void;
-    uploadFile(selector: string, filename: string): void;
+    sendEvent(mouseEventType: string, mouseX?: number, mouseY?: number, button?: string): Promise<void>;
+    sendEvent(keyboardEventType: string, key: string, null1?: void, null2?: void, modifier?: number): Promise<void>;
 
     render(filename: string): Promise<void>;
     render(filename: string, options?: { format?: string; quality?: string; }): Promise<void>;
     renderBase64(type: string): Promise<string>;
 
-    goBack(): void;
-    goForward(): void;
-    reload(): void;
-
-    getContent(): Promise<string>;
     setContent(html: string, url: string): Promise<string>;
-    getCookies(): Promise<{ name: string; value: string; domain?: string; }[]>;
+    
+    property<T>(key: string): Promise<T>;
+    property<T>(key: string, value: T): Promise<void>;
+    
+    setting<T>(key: string): Promise<T>;
+    
+    addCookie(cookie: ICookie): Promise<boolean>;
+    deleteCookie(cookieName: string): Promise<boolean>;
+    clearCookies(): Promise<void>;
+  }
+
+  interface ICookie {
+      name: string,
+      value: string,
+      domain?: string,
+      path: string,
+      httponly?: boolean,
+      secure?: boolean,
+      expires?: Date
   }
 
   interface IPaperSizeOptions {
