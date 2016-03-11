86bd5827f9b2a4bbfa8d9e02d784bd9ba6a411fb
Egor Komarov <e.komarov@h-h-i.ru>
2016-03-11 17:07:55 +0300

    Travis CI build fix

--- angular-bootstrap-calendar/angular-bootstrap-calendar.d.ts
+++ angular-bootstrap-calendar/angular-bootstrap-calendar.d.ts
@@ -1,6 +1,6 @@
 // Type definitions for angular-bootstrap-calendar
 // Project: https://github.com/mattlewis92/angular-bootstrap-calendar
-// Definitions by: Egor (Odrin) Komarov <https://github.com/Odrin>
+// Definitions by: Egor Komarov <https://github.com/Odrin>
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
 
 /// <reference path="../moment/moment.d.ts" />
