c23d9fd7178b6b3afebfb83ed73e41e0543793d5
Alex Godko <koloboid@gmail.com>
2016-03-12 21:12:08 +0200

    Change chrome.runtime.onConnectExternal type
    Change onConnectExternal type from RuntimeEvent to ExtensionConnectEvent
    according to https://developer.chrome.com/extensions/runtime#event-onConnectExternal and unsuccessuful compilation of my project :)

--- chrome/chrome.d.ts
+++ chrome/chrome.d.ts
@@ -5312,7 +5312,7 @@ declare module chrome.runtime {
 	 * Fired when a connection is made from another extension.
 	 * @since Chrome 26.
 	 */
-	var onConnectExternal: RuntimeEvent;
+	var onConnectExternal: ExtensionConnectEvent;
 	/** Sent to the event page just before it is unloaded. This gives the extension opportunity to do some clean up. Note that since the page is unloading, any asynchronous operations started while handling this event are not guaranteed to complete. If more activity for the event page occurs before it gets unloaded the onSuspendCanceled event will be sent and the page won't be unloaded. */
 	var onSuspend: RuntimeEvent;
 	/**
