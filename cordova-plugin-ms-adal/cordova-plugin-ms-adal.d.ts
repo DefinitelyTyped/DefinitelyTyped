01b2d882bd78afc527fad22de9be66aa2781c7a2
Kai Walter <kai.walter@outlook.de>
2016-03-13 20:41:45 +0100

    fixed header

--- cordova-plugin-ms-adal/cordova-plugin-ms-adal.d.ts
+++ cordova-plugin-ms-adal/cordova-plugin-ms-adal.d.ts
@@ -1,4 +1,4 @@
-// Type definitions for Active Directory Authentication Library (ADAL) plugin for Apache Cordova
+// Type definitions for Active Directory Authentication Library plugin for Apache Cordova
 // Project: https://github.com/AzureAD/azure-activedirectory-library-for-cordova
 // Definitions by: Kai Walter <https://github.com/KaiWalter>
 // Definitions: https://github.com/KaiWalter/DefinitelyTyped
