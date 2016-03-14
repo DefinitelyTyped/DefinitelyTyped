5d3e8313e3686ada34819c96ff1b1f23e1e42d5b
Stepan Mikhaylyuk <stepancar@hotmail.com>
2016-03-14 16:02:24 +0300

    fixed import for ReactDropzone
    Now its available to import in es6 style like
    import * as Dropzone from 'react-dropzone';

--- react-dropzone/react-dropzone.d.ts
+++ react-dropzone/react-dropzone.d.ts
@@ -5,33 +5,30 @@
 
 ///<reference path='../react/react.d.ts' />
 
-declare module "react-dropzone" {
-
-  import * as React from 'react';
-
-  namespace reactDropzone {
-
+declare module ReactDropzone {
+    import React = __React;
     interface DropzoneProps {
-      onDrop?: Function;
-      onDropAccepted?: Function;
-      onDropRejected?: Function;
-      onDragEnter?: Function;
-      onDragLeave?: Function;
-      style?: Object;
-      activeStyle?: Object;
-      className?: string;
-      activeClassName?: string;
-      rejectClassName?: string;
-      disableClick?: boolean;
-      multiple?: boolean;
-      accept?: string;
+        onDrop?: Function;
+        onDropAccepted?: Function;
+        onDropRejected?: Function;
+        onDragEnter?: Function;
+        onDragLeave?: Function;
+        style?: Object;
+        activeStyle?: Object;
+        className?: string;
+        activeClassName?: string;
+        rejectClassName?: string;
+        disableClick?: boolean;
+        multiple?: boolean;
+        accept?: string;
     }
 
     export class Dropzone extends React.Component<DropzoneProps, {}> {
     }
 
-  }
-
-  export = reactDropzone.Dropzone;
 }
 
+declare module "react-dropzone" {
+    const Dropzone: typeof ReactDropzone.Dropzone;
+    export = Dropzone;
+}
