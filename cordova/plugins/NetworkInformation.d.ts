6ecd7d3ebdfd386af4aff8797fab0a9f30f0ed8a
Tom O'Neill <TomONeill@users.noreply.github.com>
2016-03-15 09:01:14 +0100

    Update NetworkInformation.d.ts
    Added addEventListener on Connection interface to support "change".

--- cordova/plugins/NetworkInformation.d.ts
+++ cordova/plugins/NetworkInformation.d.ts
@@ -45,7 +45,8 @@ interface Connection {
      *     Connection.CELL
      *     Connection.NONE
      */
-    type: string
+    addEventListener(type: string, listener: EventListenerOrEventListenerObject, useCapture?: boolean): void;
+    type: string;
 }
 
 declare var Connection: {
