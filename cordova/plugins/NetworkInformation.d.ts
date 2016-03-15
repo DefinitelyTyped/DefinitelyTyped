f8d573e8c3de5eb3ad6c433a6783f92fdb468239
Tom O'Neill <TomONeill@users.noreply.github.com>
2016-03-15 09:09:17 +0100

    Update NetworkInformation.d.ts

--- cordova/plugins/NetworkInformation.d.ts
+++ cordova/plugins/NetworkInformation.d.ts
@@ -45,8 +45,8 @@ interface Connection {
      *     Connection.CELL
      *     Connection.NONE
      */
-    addEventListener(type: string, listener: EventListenerOrEventListenerObject, useCapture?: boolean): void;
     type: string;
+    addEventListener(type: string, listener: EventListenerOrEventListenerObject, useCapture?: boolean): void;
 }
 
 declare var Connection: {
