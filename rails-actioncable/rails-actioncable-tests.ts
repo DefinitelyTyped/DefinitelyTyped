e8d17b2ad85fce2168f1536f967e0e913a337ef0
Vincent Zhu <zhu1230@gmail.com>
2016-03-17 17:13:44 +0800

    rename files

--- rails-actioncable/rails-actioncable-tests.ts
+++ rails-actioncable/rails-actioncable-tests.ts
@@ -1,4 +1,4 @@
-/// <reference path="./actioncable.d.ts" />
+/// <reference path="./rails-actioncable.d.ts" />
 App = {};
 App.cable = ActionCable.createConsumer();
 App.cable.subscriptions.create("NetworkChannel", {});
