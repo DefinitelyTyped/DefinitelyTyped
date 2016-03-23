fff1789bd51e653facb08a5ca6daa6a625e60489
J. Heilingbrunner <j.heilingbrunner@gmail.com>
2016-03-23 20:59:18 +0100

    Project changed

--- node-snap7/node-snap7.d.ts
+++ node-snap7/node-snap7.d.ts
@@ -1,5 +1,5 @@
 // Type definitions for node-snap7 0.2.2
-// Project: https://www.npmjs.com/package/node-snap7
+// Project: https://github.com/mathiask88/node-snap7
 // Definitions by: Heilingbrunner <https://github.com/heilingbrunner>
 // Definitions: https://github.com/borisyankov/DefinitelyTyped
 
