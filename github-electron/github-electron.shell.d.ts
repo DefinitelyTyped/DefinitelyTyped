c43bf44206ba99ef5fc669c64e0726ea0030912c
Milan Burda <milan.burda@gmail.com>
2016-03-25 21:02:03 +0100

    Update shell module

--- github-electron/github-electron.shell.d.ts
+++ github-electron/github-electron.shell.d.ts
@@ -4,7 +4,9 @@
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
 
 declare namespace Electron {
-
+	/**
+	 * The shell module provides functions related to desktop integration.
+	 */
 	interface Shell {
 		/**
 		 * Show the given file in a file manager. If possible, select the file.
@@ -17,12 +19,20 @@ declare namespace Electron {
 		/**
 		 * Open the given external protocol URL in the desktop's default manner
 		 * (e.g., mailto: URLs in the default mail user agent).
+		 * @returns true if an application was available to open the URL, false otherwise.
 		 */
-		openExternal(url: string): void;
+		openExternal(url: string, options?: {
+			/**
+			 * Bring the opened application to the foreground.
+			 * Default: true.
+			 */
+			activate: boolean;
+		}): boolean;
 		/**
-		 * Move the given file to trash and returns boolean status for the operation.
+		 * Move the given file to trash.
+		 * @returns boolean status for the operation.
 		 */
-		moveItemToTrash(fullPath: string): void;
+		moveItemToTrash(fullPath: string): boolean;
 		/**
 		 * Play the beep sound.
 		 */
