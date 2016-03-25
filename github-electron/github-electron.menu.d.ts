ccaff5d339f262bb2599047d638b9e5528397448
Milan Burda <milan.burda@gmail.com>
2016-03-25 21:44:25 +0100

    Update menu + menu-item modules

--- github-electron/github-electron.menu.d.ts
+++ github-electron/github-electron.menu.d.ts
@@ -4,15 +4,18 @@
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
 
 /// <reference path="github-electron.browser-window.d.ts" />
+/// <reference path="github-electron.event-emitter.d.ts" />
 /// <reference path="github-electron.menu-item.d.ts" />
 
 declare namespace Electron {
 	/**
 	 * The Menu class is used to create native menus that can be used as application
-	 * menus and context menus. Each menu consists of multiple menu items, and each
-	 * menu item can have a submenu.
+	 * menus and context menus. This module is a main process module which can be used
+	 * in a render process via the remote module.
+	 *
+	 * Each menu consists of multiple menu items, and each menu item can have a submenu.
 	 */
-	class Menu {
+	class Menu extends EventEmitter {
 		/**
 		 * Creates a new menu.
 		 */
@@ -23,9 +26,9 @@ declare namespace Electron {
 		 */
 		static setApplicationMenu(menu: Menu): void;
 		/**
-		 * Sends the action to the first responder of application, this is used for
-		 * emulating default Cocoa menu behaviors, usually you would just use the
-		 * selector property of MenuItem.
+		 * Sends the action to the first responder of application.
+		 * This is used for emulating default Cocoa menu behaviors,
+		 * usually you would just use the role property of MenuItem.
 		 *
 		 * Note: This method is OS X only.
 		 */
@@ -43,7 +46,7 @@ declare namespace Electron {
 		 * @param x Horizontal coordinate where the menu will be placed.
 		 * @param y Vertical coordinate where the menu will be placed.
 		 */
-		popup(browserWindow: BrowserWindow, x?: number, y?: number): void;
+		popup(browserWindow?: BrowserWindow, x?: number, y?: number): void;
 		/**
 		 * Appends the menuItem to the menu.
 		 */
@@ -52,6 +55,9 @@ declare namespace Electron {
 		 * Inserts the menuItem to the pos position of the menu.
 		 */
 		insert(position: number, menuItem: MenuItem): void;
+		/**
+		 * @returns an array containing the menu’s items.
+		 */
 		items: MenuItem[];
 	}
 }
