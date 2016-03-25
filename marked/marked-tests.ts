943094dd7a194626bb6200e1042fb4905790905c
rhysd <lin90162@yahoo.co.jp>
2016-03-25 17:50:29 +0900

    marked.d.ts: Add missing parameter of MarkedRenderer#heading()
    https://github.com/chjj/marked/blob/18fb6a639a4a77dd59650879bcad10d833c40067/lib/marked.js#L795

--- marked/marked-tests.ts
+++ marked/marked-tests.ts
@@ -37,3 +37,8 @@ console.log(marked.parse('i am using __markdown__.', options, callback));
 var text = 'something';
 var tokens = marked.lexer(text, options);
 console.log(marked.parser(tokens));
+
+var renderer = new marked.Renderer();
+renderer.heading = function(text, level, raw) {
+    return text + level.toString() + raw;
+};
