70e634c0bd3d0611550698f4aafa456c80c07181
Milan Burda <milan.burda@gmail.com>
2016-03-25 21:37:20 +0100

    Update global-shortcut module

--- github-electron/github-electron.global-shortcut.d.ts
+++ github-electron/github-electron.global-shortcut.d.ts
@@ -4,14 +4,18 @@
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
 
 declare namespace Electron {
-
+	/**
+	 * The globalShortcut module can register/unregister a global keyboard shortcut
+	 * with the operating system so that you can customize the operations for various shortcuts.
+	 * Note: The shortcut is global; it will work even if the app does not have the keyboard focus.
+	 * You should not use this module until the ready event of the app module is emitted.
+	 */
 	interface GlobalShortcut {
 		/**
 		 * Registers a global shortcut of accelerator.
 		 * @param accelerator Represents a keyboard shortcut. It can contain modifiers
 		 * and key codes, combined by the "+" character.
 		 * @param callback Called when the registered shortcut is pressed by the user.
-		 * @returns {}
 		 */
 		register(accelerator: string, callback: Function): void;
 		/**
