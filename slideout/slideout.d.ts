d6aed493b064c2aaca1ef55a5a634643a98d95a5
ToastHawaii <markus.peloso@gmail.com>
2016-03-26 13:30:02 +0100

    Fix project link

--- slideout/slideout.d.ts
+++ slideout/slideout.d.ts
@@ -1,5 +1,5 @@
 // Type definitions for Slideout 0.1.12
-// Project: https://github.com/t4t5/sweetalert/
+// Project: https://github.com/mango/slideout
 // Definitions by: Markus Peloso <https://github.com/ToastHawaii/>
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
 
