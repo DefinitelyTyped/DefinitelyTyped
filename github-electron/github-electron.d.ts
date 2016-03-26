375d2e7f8a76149f420c36a65fe405a98baa31e8
Milan Burda <milan.burda@gmail.com>
2016-03-26 21:59:39 +0100

    Update header

--- github-electron/github-electron.d.ts
+++ github-electron/github-electron.d.ts
@@ -1,6 +1,6 @@
 // Type definitions for Electron v0.37.2
 // Project: http://electron.atom.io/
-// Definitions by: jedmao <https://github.com/jedmao/>, rhysd <https://rhysd.github.io>
+// Definitions by: jedmao <https://github.com/jedmao/>, rhysd <https://rhysd.github.io>, Milan Burda <https://github.com/miniak/>
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
 
 /// <reference path="../node/node.d.ts" />
