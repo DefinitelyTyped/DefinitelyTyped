cbf802150d720a8259a350d4f0a8b198813c0202
Andrew Kuklewicz <andrew@beginsinwonder.com>
2016-03-27 19:28:44 -0400

    Add (much deserved credit) for Rhoden

--- evaporate/evaporate.d.ts
+++ evaporate/evaporate.d.ts
@@ -1,6 +1,6 @@
 // Type definitions for EvaporateJS
 // Project: https://github.com/TTLabs/EvaporateJS
-// Definitions by: Andrew Kuklewicz <https://github.com/kookster/>
+// Definitions by: Andrew Kuklewicz <https://github.com/kookster/>, Chris Rhoden <https://github.com/chrisrhoden>
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
 declare class Evaporate {
   cancel(id:string): boolean;
