bf4cfdd6fff36c1ab4ae18bfe0a6deaff412be12
Jussi Kinnula <jussi.kinnula@frantic.com>
2016-03-27 16:16:27 +0300

    dotenv: Use correct GitHub link

--- dotenv/dotenv.d.ts
+++ dotenv/dotenv.d.ts
@@ -1,5 +1,5 @@
 // Type definitions for dotenv 2.0.0
-// Project: https://github.com/bkeepers/dotenv
+// Project: https://github.com/motdotla/dotenv
 // Definitions by: Jussi Kinnula <https://github.com/jussikinnula/>
 // Definitions: https://github.com/jussikinnula/DefinitelyTyped
 
