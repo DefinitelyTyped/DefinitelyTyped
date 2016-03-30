402a133aabff8d763589770594ddcdebf2b63cc5
Dave Allen <david.allen@meliortechnology.com>
2016-03-30 11:51:32 -0700

    Fixed header

--- meteor/meteor.d.ts
+++ meteor/meteor.d.ts
@@ -2,16 +2,6 @@
 // Project: http://www.meteor.com/
 // Definitions by: Dave Allen <https://github.com/fullflavedave>
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
-/**
- *  Meteor definitions for TypeScript
- *  author - Olivier Refalo - orefalo@yahoo.com
- *  author - David Allen - dave@fullflavedave.com
- *
- *  Thanks to Sam Hatoum for the base code for auto-generating this file.
- *
- *  supports Meteor 1.3
- */
-
 
 /**
  * These are the common (for client and server) modules and interfaces that can't be automatically generated from the Meteor data.js file
