94bdbc1dbce6c18474fd7aadfcd7befe92ba17f9
Jason Turner <jturner@liberty.com.au>
2016-04-04 12:43:53 +1000

    Update documentation spec. Update test to include ES5 typescript import.

--- revalidator/revalidator.d.ts
+++ revalidator/revalidator.d.ts
@@ -1,5 +1,5 @@
 ﻿// Type definitions for revalidator 0.3.1
-
+// Project: https://github.com/flatiron/revalidator
 // Definitions by:  Jason Turner<https://github.com/brewsoftware>
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
 
