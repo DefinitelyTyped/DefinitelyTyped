94f9e6615796616aa1e8f6e35f1e5633a4f6521d
Stepan Mikhaylyuk <stepancar@hotmail.com>
2016-04-04 15:45:17 +0300

    test from repo

--- react-motion/react-motion-tests.ts
+++ react-motion/react-motion-tests.ts
@@ -1,1 +1,17 @@
 
+/// <reference path="../react/react.d.ts" />
+/// <reference path="./react-motion.ts"/>
+
+import * as React from 'react';
+import {Motion, spring} from 'react-motion';
+
+
+class Root extends React.Component<{}, {}> {
+    render() {
+        return (
+            <Motion defaultStyle={{ x: 0 }} style = {{ x: spring(10) }}>
+                {value => <div>{ value.x } </div>}
+            </Motion>
+        );
+    }
+}
