444d0fc351895d88f02a899c0bd4a766fb7c8fb9
Giedrius Grabauskas <GiedriusGrabauskas@users.noreply.github.com>
2016-04-05 15:52:04 +0300

    Fixed mistype

--- signalr/signalr.d.ts
+++ signalr/signalr.d.ts
@@ -10,7 +10,7 @@
 declare namespace SignalR {
     
     const enum ConnectionState {
-        Conneting = 0,
+        Connecting = 0,
         Connected = 1,
         Reconnecting = 2,
         Disconnected = 4
