85aeb5ecb055fcd4fed0e90de8c0e5dd13f577c0
Chris Charabaruk <chris.charabaruk@northcloud.com>
2016-04-07 12:23:42 -0400

    Add the promise-pg readme examples as tests

--- promise-pg/promise-pg-tests.ts
+++ promise-pg/promise-pg-tests.ts
@@ -1,4 +1,5 @@
 /// <reference path="promise-pg.d.ts" />
+/// <reference path="../q/Q.d.ts" />
 
 import * as pg from 'promise-pg';
 
@@ -35,4 +36,60 @@ client.connect()
       });
   }, function (err) {
     return console.error("Could not connect to postgres", err);
+  }).done();
+
+// Using buffer query option
+pg.connect(conString)
+  .spread(function (client: pg.Client, done: () => void) {
+    client.query({
+      text: "SELECT * FROM users",
+      buffer: true
+    }).promise.then(
+      function (result) { console.log(result.rows.length + " rows returned"); },
+      function (err) { console.error("Error running query", err); throw err; },
+      function (user: any) {} // called for each returned row
+    ).finally(done);
+  }).done();
+
+// Transactions
+pg.connect(conString)
+  .spread(function (client: pg.Client, done: () => void) {
+    var INSERT = "INSERT INTO users(name, t_birth, country) VALUES ($1, $2, $3)";
+
+    console.log("Transaction I:");
+    var trans = client.transaction(function () {
+      return Q.all([{
+          text: INSERT,
+          values: ["Jake1", "now()", "Oo"]
+        }, {
+          text: INSERT,
+          values: ["Cake2", null, "Küche"]
+        }, {
+          text: INSERT,
+          values: ["Mike3", "now()", null]
+        }].map(function (q) { return client.query(q).promise; }));
+    }).then(function() {
+      console.log("Good - Committed Transaction I.");
+    }, function (err) {
+      console.log("Bad  - Rolled back Transaction I.", err);
+    });
+
+    trans.finally(function () {
+      client.transaction(function () {
+        return Q.all([{
+          text: INSERT,
+          values: ["Jake", "now()", "Oo"]
+        }, {
+          text: INSERT, // Bad, name is NOT NULL
+          values: [null, null, "Küche"]
+        }, {
+          text: INSERT,
+          values: ["Mike", "now()", null]
+        }].map(function (q) { return client.query(q).promise; }));
+      }).then(function () {
+        console.log("Bad  - Committed Transaction II.");
+      }, function (err) {
+        console.log("Good - Rolled back Transaction II.", err.message);
+      }).finally(done).done();
+    });
   }).done();
