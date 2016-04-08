c959bd806b63fa407759509e8e2612b972cea874
Giedrius Grabauskas <GiedriusGrabauskas@users.noreply.github.com>
2016-04-08 14:35:07 +0300

    Added latinize version.

--- latinize/latinize.d.ts
+++ latinize/latinize.d.ts
@@ -1,4 +1,4 @@
-// Type definitions for latinize
+// Type definitions for latinize 0.2.0
 // Project: https://github.com/dundalek/latinize
 // Definitions by: Giedrius Grabauskas <https://github.com/GiedriusGrabauskas>
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
