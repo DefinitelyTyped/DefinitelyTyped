4a58da57477227913531eacb7fdc8ec0842610d8
Giedrius Grabauskas <GiedriusGrabauskas@users.noreply.github.com>
2016-04-08 13:52:33 +0300

    latinize.d.ts file created.

---
+++ latinize/latinize.d.ts
@@ -0,0 +1,17 @@
+// Type definitions for latinize
+// Project: https://github.com/dundalek/latinize
+// Definitions by: Giedrius Grabauskas <https://github.com/GiedriusGrabauskas/>
+// Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
+
+declare namespace Latinize {
+    export interface ILatinize {
+        (str: string): string;
+        characters: { [char: string]: string };
+    }
+}
+
+
+declare module "latinize" {
+    let latinize: Latinize.ILatinize;
+    export = latinize;
+}
