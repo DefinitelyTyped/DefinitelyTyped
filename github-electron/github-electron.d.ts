f90630c2d28f4466e8d6fcb17e765fbb2601cb5b
Milan Burda <milan.burda@gmail.com>
2016-04-09 13:45:03 +0200

    Fix CrashReporter interface

--- github-electron/github-electron.d.ts
+++ github-electron/github-electron.d.ts
@@ -1492,84 +1492,53 @@ declare namespace Electron {
 	interface CrashReporter {
 		/**
 		 * You are required to call this method before using other crashReporter APIs.
+		 *
+		 * Note: On OS X, Electron uses a new crashpad client, which is different from breakpad
+		 * on Windows and Linux. To enable the crash collection feature, you are required to call
+		 * the crashReporter.start API to initialize crashpad in the main process and in each
+		 * renderer process from which you wish to collect crash reports.
 		 */
 		start(options: CrashReporterStartOptions): void;
 		/**
-		 * @returns The date and ID of the last crash report. When there was no crash report
+		 * @returns The crash report. When there was no crash report
 		 * sent or the crash reporter is not started, null will be returned.
 		 */
-		getLastCrashReport(): CrashReporterPayload;
+		getLastCrashReport(): CrashReport;
 		/**
-		 * @returns All uploaded crash reports. Each report contains the date and uploaded ID.
+		 * @returns All uploaded crash reports.
 		 */
-		getUploadedReports(): CrashReporterPayload[];
+		getUploadedReports(): CrashReport[];
 	}
 
 	interface CrashReporterStartOptions {
 		/**
-		* Default: Electron
-		*/
+		 * Default: Electron
+		 */
 		productName?: string;
 		companyName: string;
 		/**
-		* URL that crash reports would be sent to as POST.
-		*/
+		 * URL that crash reports would be sent to as POST.
+		 */
 		submitURL: string;
 		/**
-		* Send the crash report without user interaction.
-		* Default: true.
-		*/
+		 * Send the crash report without user interaction.
+		 * Default: true.
+		 */
 		autoSubmit?: boolean;
 		/**
-		* Default: false.
-		*/
+		 * Default: false.
+		 */
 		ignoreSystemCrashHandler?: boolean;
 		/**
-		* An object you can define which content will be send along with the report.
-		* Only string properties are send correctly.
-		* Nested objects are not supported.
-		*/
+		 * An object you can define that will be sent along with the report.
+		 * Only string properties are sent correctly, nested objects are not supported.
+		 */
 		extra?: {[prop: string]: string};
 	}
 
-	interface CrashReporterPayload extends Object {
-		/**
-		* E.g., "electron-crash-service".
-		*/
-		rept: string;
-		/**
-		* The version of Electron.
-		*/
-		ver: string;
-		/**
-		* E.g., "win32".
-		*/
-		platform: string;
-		/**
-		* E.g., "renderer".
-		*/
-		process_type: string;
-		ptime: number;
-		/**
-		* The version in package.json.
-		*/
-		_version: string;
-		/**
-		* The product name in the crashReporter options object.
-		*/
-		_productName: string;
-		/**
-		* Name of the underlying product. In this case, Electron.
-		*/
-		prod: string;
-		/**
-		* The company name in the crashReporter options object.
-		*/
-		_companyName: string;
-		/**
-		* The crashreporter as a file.
-		*/
-		upload_file_minidump: File;
+	interface CrashReport {
+		id: string;
+		date: Date;
 	}
 
 	// https://github.com/electron/electron/blob/master/docs/api/desktop-capturer.md
