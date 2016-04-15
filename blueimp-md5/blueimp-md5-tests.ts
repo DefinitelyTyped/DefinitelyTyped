148a9d0abd8ca5683366363dfe028bfae2265597
Zen <843968788@qq.com>
2016-04-15 13:02:44 +0800

    Update signature of module

--- blueimp-md5/blueimp-md5-tests.ts
+++ blueimp-md5/blueimp-md5-tests.ts
@@ -3,5 +3,5 @@
 import blueimp = require('blueimp-md5');
 
 function hash(): boolean {
-    return blueimp.md5('hello world') === '5eb63bbbe01eeed093cb22bb8f5acdc3';
+    return blueimp('hello world') === '5eb63bbbe01eeed093cb22bb8f5acdc3';
 }
