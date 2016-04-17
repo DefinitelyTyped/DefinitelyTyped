a3eac7a9a64e6c9017ef37e7a25e094dab170ed2
Atanas Atanasov <nascovt@gmail.com>
2016-04-17 18:14:51 +0300

    Create grid.d.ts

---
+++ gijgo/grid.d.ts
@@ -0,0 +1,4 @@
+// Type definitions for Gijgo v0.6.2
+// Project: http://gijgo.com
+// Definitions by: Atanas Atanasov <https://github.com/atatanasov>
+// Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
