da3d097262b50d3a64a963d81037f6ac4f016c73
Nicholas Albion <nalbion@yahoo.com>
2016-04-23 13:12:53 +1000

    () will return Object you will have to cast it to any to access properties

--- i18next/i18next.d.ts
+++ i18next/i18next.d.ts
@@ -100,7 +100,7 @@ declare namespace I18next {
 
         getFixedT(lng?:string, ns?:string|string[]):TranslationFunction;
 
-        t(key:string, options?:TranslationOptions):string|Object|Array<any>;
+        t(key:string, options?:TranslationOptions):string|any|Array<any>;
 
         exists():boolean;
 
