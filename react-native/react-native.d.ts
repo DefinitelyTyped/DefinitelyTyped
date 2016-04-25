f6b35a9f281cf1046aacac268e49d8f6fefe3bab
Howard Yeh <hayeah@gmail.com>
2016-04-25 09:03:05 -0700

    React native animated (#9057)
    * Type Animated.Value
    
    * react-native Animated.spring
    
    * react-native AnimatedValueXY type
    
    * react-native Animated.parallel
    
    * react-native Animated.event
    
    * react-native Animated components
    
    * react-native tweak Animated types
    
    * react-native add Animated.timing
    
    * react-native rewrite animation config as interface
    
    * react-native remove semi-colons
    
    * react-native refine the type AnimatedValue
    
    * react-native refine the type AnimatedValue
    
    * react-native animated no implicit any
    
    * react-native animated API tests
    
    * react-native more Animated API type coverage
    
    * react-native add Animated.Base as alias of commone base type
    
    * react-native fix typedef bug

--- react-native/react-native.d.ts
+++ react-native/react-native.d.ts
@@ -3453,6 +3453,304 @@ declare module "react-native" {
         vibrate(): void
     }
 
+    export module Animated {
+      // Most (all?) functions where AnimatedValue is used any subclass of Animated can be used as well.
+      type AnimatedValue = Animated;
+      type AnimatedValueXY = ValueXY;
+
+      type Base = Animated;
+
+      class Animated {
+        // Internal class, no public API.
+      }
+
+      class AnimatedWithChildren extends Animated {
+        // Internal class, no public API.
+      }
+
+      class AnimatedInterpolation extends AnimatedWithChildren {
+        interpolate(config: InterpolationConfigType): AnimatedInterpolation;
+      }
+
+      type ExtrapolateType = 'extend' | 'identity' | 'clamp';
+
+      type InterpolationConfigType = {
+        inputRange: number[];
+        outputRange: (number[] | string[]);
+        easing?: ((input: number) => number);
+        extrapolate?: ExtrapolateType;
+        extrapolateLeft?: ExtrapolateType;
+        extrapolateRight?: ExtrapolateType;
+      };
+
+      type ValueListenerCallback = (state: {value: number}) => void;
+
+      /**
+       * Standard value for driving animations.  One `Animated.Value` can drive
+       * multiple properties in a synchronized fashion, but can only be driven by one
+       * mechanism at a time.  Using a new mechanism (e.g. starting a new animation,
+       * or calling `setValue`) will stop any previous ones.
+       */
+      export class Value extends AnimatedWithChildren {
+        constructor(value: number);
+
+        setValue(value: number): void;
+
+        /**
+         * Sets an offset that is applied on top of whatever value is set, whether via
+         * `setValue`, an animation, or `Animated.event`.  Useful for compensating
+         * things like the start of a pan gesture.
+         */
+        setOffset(offset: number): void;
+
+        /**
+         * Merges the offset value into the base value and resets the offset to zero.
+         * The final output of the value is unchanged.
+         */
+        flattenOffset(): void;
+
+        /**
+         * Adds an asynchronous listener to the value so you can observe updates from
+         * animations.  This is useful because there is no way to
+         * synchronously read the value because it might be driven natively.
+         */
+        addListener(callback: ValueListenerCallback): string;
+
+        removeListener(id: string): void;
+
+        removeAllListeners(): void;
+
+        /**
+         * Stops any running animation or tracking.  `callback` is invoked with the
+         * final value after stopping the animation, which is useful for updating
+         * state to match the animation position with layout.
+         */
+        stopAnimation(callback?: (value: number) => void): void;
+
+        /**
+         * Interpolates the value before updating the property, e.g. mapping 0-1 to
+         * 0-10.
+         */
+        interpolate(config: InterpolationConfigType): AnimatedInterpolation;
+      }
+
+      type ValueXYListenerCallback = (value: {x: number; y: number}) => void;
+
+      /**
+       * 2D Value for driving 2D animations, such as pan gestures.  Almost identical
+       * API to normal `Animated.Value`, but multiplexed.  Contains two regular
+       * `Animated.Value`s under the hood.
+       */
+      export class ValueXY extends AnimatedWithChildren {
+        x: AnimatedValue;
+        y: AnimatedValue;
+
+        constructor(valueIn?: {x: number | AnimatedValue; y: number | AnimatedValue});
+
+        setValue(value: {x: number; y: number}): void;
+
+        setOffset(offset: {x: number; y: number}): void;
+
+        flattenOffset(): void
+
+        stopAnimation(callback?: () => number): void;
+
+        addListener(callback: ValueXYListenerCallback): string;
+
+        removeListener(id: string): void;
+
+        /**
+         * Converts `{x, y}` into `{left, top}` for use in style, e.g.
+         *
+         *```javascript
+         *  style={this.state.anim.getLayout()}
+         *```
+         */
+        getLayout(): { left: AnimatedValue, top: AnimatedValue };
+
+        /**
+         * Converts `{x, y}` into a useable translation transform, e.g.
+         *
+         *```javascript
+         *  style={{
+         *    transform: this.state.anim.getTranslateTransform()
+         *  }}
+         *```
+         */
+        getTranslateTransform(): {[key: string]: AnimatedValue}[];
+
+      }
+
+      type EndResult = {finished: boolean};
+      type EndCallback = (result: EndResult) => void;
+
+      interface CompositeAnimation {
+        start: (callback?: EndCallback) => void;
+        stop: () => void;
+      }
+
+      interface AnimationConfig {
+        isInteraction?: boolean;
+        useNativeDriver?: boolean;
+      }
+
+      /**
+       * Animates a value from an initial velocity to zero based on a decay
+       * coefficient.
+       */
+      export function decay(
+        value: AnimatedValue | AnimatedValueXY,
+        config: DecayAnimationConfig
+      ): CompositeAnimation;
+
+      interface DecayAnimationConfig extends AnimationConfig {
+        velocity: number | {x: number, y: number};
+        deceleration?: number;
+      }
+
+      /**
+       * Animates a value along a timed easing curve.  The `Easing` module has tons
+       * of pre-defined curves, or you can use your own function.
+       */
+      export var timing: (
+        value: AnimatedValue | AnimatedValueXY,
+        config: TimingAnimationConfig
+      ) => CompositeAnimation;
+
+      interface TimingAnimationConfig extends AnimationConfig {
+        toValue: number | AnimatedValue | {x: number, y: number} | AnimatedValueXY;
+        easing?: (value: number) => number;
+        duration?: number;
+        delay?: number;
+      }
+
+      interface SpringAnimationConfig extends AnimationConfig {
+        toValue: number | AnimatedValue | {x: number, y: number} | AnimatedValueXY;
+        overshootClamping?: boolean;
+        restDisplacementThreshold?: number;
+        restSpeedThreshold?: number;
+        velocity?: number | {x: number, y: number};
+        bounciness?: number;
+        speed?: number;
+        tension?: number;
+        friction?: number;
+      }
+
+      /**
+       * Creates a new Animated value composed from two Animated values added
+       * together.
+       */
+      export function add(
+        a: Animated,
+        b: Animated
+      ): AnimatedAddition;
+
+      class AnimatedAddition extends AnimatedInterpolation {}
+
+      /**
+       * Creates a new Animated value composed from two Animated values multiplied
+       * together.
+       */
+       export function multiply(
+        a: Animated,
+        b: Animated
+      ): AnimatedMultiplication;
+
+      class AnimatedMultiplication extends AnimatedInterpolation {}
+
+      /**
+       * Creates a new Animated value that is the (non-negative) modulo of the
+       * provided Animated value
+       */
+      export function modulo(
+        a: Animated,
+        modulus: number
+      ): AnimatedModulo;
+
+      class AnimatedModulo extends AnimatedInterpolation {}
+
+      /**
+       * Starts an animation after the given delay.
+       */
+      export function delay(time: number): CompositeAnimation;
+
+      /**
+       * Starts an array of animations in order, waiting for each to complete
+       * before starting the next.  If the current running animation is stopped, no
+       * following animations will be started.
+       */
+      export function sequence(
+        animations: Array<CompositeAnimation>
+      ): CompositeAnimation;
+
+      /**
+       * Array of animations may run in parallel (overlap), but are started in
+       * sequence with successive delays.  Nice for doing trailing effects.
+       */
+
+      export function stagger(
+        time: number,
+        animations: Array<CompositeAnimation>
+      ): CompositeAnimation
+
+      /**
+       * Spring animation based on Rebound and Origami.  Tracks velocity state to
+       * create fluid motions as the `toValue` updates, and can be chained together.
+       */
+      export var spring: (
+        value: AnimatedValue | AnimatedValueXY,
+        config: SpringAnimationConfig
+      ) => CompositeAnimation;
+
+      type ParallelConfig = {
+        stopTogether?: boolean; // If one is stopped, stop all.  default: true
+      }
+
+      /**
+       * Starts an array of animations all at the same time.  By default, if one
+       * of the animations is stopped, they will all be stopped.  You can override
+       * this with the `stopTogether` flag.
+       */
+      var parallel: (
+        animations: Array<CompositeAnimation>,
+        config?: ParallelConfig
+      ) => CompositeAnimation;
+
+      type Mapping = {[key: string]: Mapping} | AnimatedValue;
+      interface EventConfig {
+        listener?: Function
+      }
+
+      /**
+       *  Takes an array of mappings and extracts values from each arg accordingly,
+       *  then calls `setValue` on the mapped outputs.  e.g.
+       *
+       *```javascript
+       *  onScroll={Animated.event(
+       *    [{nativeEvent: {contentOffset: {x: this._scrollX}}}]
+       *    {listener},          // Optional async listener
+       *  )
+       *  ...
+       *  onPanResponderMove: Animated.event([
+       *    null,                // raw event arg ignored
+       *    {dx: this._panX},    // gestureState arg
+       *  ]),
+       *```
+       */
+      var event: (
+        argMapping: Mapping[],
+        config?: EventConfig
+      ) => (...args: any[]) => void;
+
+      /**
+       * Animated variants of the basic native views. Accepts Animated.Value for
+       * props and style.
+       */
+      export var View: any;
+      export var Image: any;
+      export var Text: any;
+    }
+
     //////////////////////////////////////////////////////////////////////////
     //
     //  R E - E X P O R T S
