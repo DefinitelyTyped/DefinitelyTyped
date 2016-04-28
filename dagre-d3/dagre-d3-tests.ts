6995a9fbefa6d68c03a3afe5d88ccc0b9acdecf4
Christian Beilschmidt <beilschmidt@mathematik.uni-marburg.de>
2016-04-28 23:17:20 +0200

    exported `dagre-d3` so it is possible to import it with SystemJS
    * Atom's TypeScript package now accepts the import
    * changed some style issues according to linter (var -> let)

--- dagre-d3/dagre-d3-tests.ts
+++ dagre-d3/dagre-d3-tests.ts
@@ -1,19 +1,18 @@
 /// <reference path="dagre-d3.d.ts"/>
 namespace DagreD3Tests {
-    var gDagre = new dagreD3.graphlib.Graph();
-    var graph = gDagre.graph();
+    let gDagre = new dagreD3.graphlib.Graph();
+    let graph = gDagre.graph();
 
     // has graph methods from dagre.d.ts
     graph.setNode("a", {});
-    var num: number = 251 + graph.height + graph.width;
-    var predecessors: { [vertex:string]: string[] } = {};
-    var successors: { [vertex:string]: string[] } = {};
+    let num: number = 251 + graph.height + graph.width;
+    let predecessors: { [vertex: string]: string[] } = {};
+    let successors: { [vertex: string]: string[] } = {};
 
     predecessors["a"] = graph.predecessors("a");
     successors["a"] = graph.successors("a");
 
-    var render = new dagreD3.render();
-    var svg = d3.select("svg");
+    let render = new dagreD3.render();
+    let svg = d3.select("svg");
     render(svg, graph);
 }
-
