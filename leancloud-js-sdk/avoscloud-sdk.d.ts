48b36c203a7db75c71df42bdc28b966151c1038c
wujun4code <wujun19890209@gmail.com>
2016-04-29 10:40:19 +0800

    update comments.

--- leancloud-js-sdk/avoscloud-sdk.d.ts
+++ leancloud-js-sdk/avoscloud-sdk.d.ts
@@ -1,6 +1,6 @@
 // Type definitions for avoscloud-sdk 0.6.10
 // Project: https://leancloud.cn/
-// Definitions by: Wu Jun@leancloud.cn
+// Definitions by: Wu Jun <http://leancloud.cn>
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
 
 declare module "avoscloud-sdk" {
