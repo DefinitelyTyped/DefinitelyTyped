1af3e2d2ad3146fae2e19e6fde70bb36098f6c7f
Kai Ilbertz <kai.ilbertz@gmail.com>
2016-05-01 15:15:25 +0200

    Add type definition for valdr (https://www.npmjs.com/package/valdr)

---
+++ valdr/valdr-tests.ts
@@ -0,0 +1,38 @@
+﻿/// <reference path="valdr.d.ts" />
+
+function ValdrTests() {
+    var valdr: valdr.IValdr;
+    var validation = valdr.validate('person', 'lastName', 'test');
+    valdr.addConstraints({
+        'person': {
+            'lastName': {
+                'required': {
+                    'message': 'Last name is required.'
+                }
+            }
+        }
+    });
+    valdr.removeConstraints('person');
+    var constraints = valdr.getConstraints();
+    valdr.setClasses({
+        valid: 'demo-is-valid',
+        invalid: 'demo-is-invalid'
+    });
+}
+
+function ValdrProviderTests() {
+    var valdrProvider: valdr.IValdrProvider;
+    valdrProvider.addConstraints({
+        'person': {
+            'lastName': {
+                'required': {
+                    'message': 'Last name is required.'
+                }
+            }
+        }
+    });
+    valdrProvider.removeConstraints('person');
+    valdrProvider.setConstraintUrl('/api/constraints');
+    valdrProvider.addValidator('customValidator');
+    valdrProvider.addConstraintAlias('customValidator', 'customValidatorAlias');
+}
