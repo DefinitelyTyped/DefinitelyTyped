0b96933934c2e37a749aad8e38e23214e21e7dab
Demis Bellot <demis.bellot@gmail.com>
2016-05-01 08:58:35 -0400

    Update react-redux.d.ts (#9103)
    * Update react-redux.d.ts
    
    Options should be optional, added missing withRef option 
    https://github.com/reactjs/react-redux/blob/253ce8b3068d9d9bfe55f70a6f18a5fde313b326/src/components/connect.js#L49
    
    * Update react-redux.d.ts

--- react-redux/react-redux.d.ts
+++ react-redux/react-redux.d.ts
@@ -80,7 +80,12 @@ declare module "react-redux" {
      * Defaults to true.
      * @default true
      */
-    pure: boolean;
+    pure?: boolean;
+    /**
+    * If true, stores a ref to the wrapped component instance and makes it available via 
+    * getWrappedInstance() method. Defaults to false.
+    */
+    withRef?: boolean;
   }
 
   export interface ProviderProps {
