5a45010eaf1a651f161a4886f539395421e99e6d
stephenjelfs <stephenjelfs@Carolines-iMac-4.local>
2016-05-02 18:50:53 +0200

    Fixed tests

--- react-imageloader/react-imageloader-tests.tsx
+++ react-imageloader/react-imageloader-tests.tsx
@@ -1,7 +1,7 @@
 /// <reference path="react-imageloader.d.ts" />
 /// <reference path="../react/react.d.ts" />
 
-import ImageLoader from 'react-imageloader';
+import ImageLoader = require('react-imageloader');
 import * as React from 'react';
 
-export imageLoader = <ImageLoader src='image.png' />;
+let imageLoader = <ImageLoader src='image.png' />;
