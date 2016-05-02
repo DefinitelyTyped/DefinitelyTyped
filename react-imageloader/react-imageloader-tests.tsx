22bbf257a6a90ababeffb38650bbaea1d5721520
stephenjelfs <stephenjelfs@Carolines-iMac-4.local>
2016-05-02 18:47:25 +0200

    Initial react-imageloader typings

---
+++ react-imageloader/react-imageloader-tests.tsx
@@ -0,0 +1,7 @@
+/// <reference path="react-imageloader.d.ts" />
+/// <reference path="../react/react.d.ts" />
+
+import ImageLoader from 'react-imageloader';
+import * as React from 'react';
+
+export imageLoader = <ImageLoader src='image.png' />;
