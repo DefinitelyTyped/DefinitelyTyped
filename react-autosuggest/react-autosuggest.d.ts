c50804f3d1e3ca66437d8705b40bc4d0a67f14c1
Nicolas Schmitt <nicolas.schmitt@amundi.com>
2016-05-09 18:13:02 +0200

    update react-autosuggest

--- react-autosuggest/react-autosuggest.d.ts
+++ react-autosuggest/react-autosuggest.d.ts
@@ -1,51 +1,67 @@
-// Type definitions for react-autosuggest 3.7.1
-// Project: https://github.com/moroshko/react-autosuggest
-// Definitions by: Stephen Jelfs <https://github.com/stephenjelfs>
+// Type definitions for react-autosuggest v3.7.3
+// Project: http://react-autosuggest.js.org/
+// Definitions by: Nicolas Schmitt <https://github.com/nicolas-schmitt>
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
 
-/// <reference path="../react/react.d.ts" />
+/// <reference path="../react/react.d.ts"/>
 
-declare module "react-autosuggest" {
-   interface Suggestion {
-     text: string;
-   }
+declare namespace ReactAutosuggest {
+  import React = __React;
+
+  interface SuggestionUpdateRequest {
+    value: string;
+    reason: string;
+  }
+
+  interface InputValues {
+    value: string;
+    valueBeforeUpDown?: string;
+  }
+
+  interface InputProps extends React.HTMLAttributes {
+    value: string;
+    onChange: React.FormEventHandler;
+  }
+
+  interface SuggestionSelectedEventData {
+    method: string;
+    sectionIndex: number;
+    suggestion: any;
+    suggestionValue: string;
+  }
 
   interface Theme {
-    container?: string;
-    containerOpen?: string;
-    input?: string;
-    suggestionsContainer?: string;
-    suggestion?: string;
-    suggestionFocused?: string;
-    sectionContainer?: string;
-    sectionTitle?: string;
-    sectionSuggestionsContainer?: string;
+    container: string;
+    containerOpen: string;
+    input: string;
+    sectionContainer: string;
+    sectionSuggestionsContainer: string;
+    sectionTitle: string;
+    suggestion: string;
+    suggestionFocused: string;
+    suggestionsContainer: string;
   }
 
-  interface InputProps {
-    value: string;
-    onChange: (event: any, params: {newValue: string, method: string}) => void;
-    type?: string;
-    placeholder?: string;
-  }
-
-  interface AutosuggestProps extends __React.Props<Autosuggest> {
+  interface AutosuggestProps extends React.Props<Autosuggest> {
     suggestions: any[];
-    onSuggestionsUpdateRequested?: (params: {value: string, reason: string}) => void;
+    onSuggestionsUpdateRequested?: (request: SuggestionUpdateRequest) => void;
     getSuggestionValue: (suggestion: any) => string;
-    renderSuggestion: (suggestion: any, params: {value: string, valueBeforeUpDown: string}) => __React.ReactElement<any>;
+    renderSuggestion: (suggestion: any, inputValues: InputValues) => JSX.Element;
     inputProps: InputProps;
     shouldRenderSuggestions?: (value: string) => boolean;
     multiSection?: boolean;
-    renderSectionTitle?: (section: any) =>  __React.ReactElement<any>;
+    renderSectionTitle?: (section: any, inputValues: InputValues) => JSX.Element;
     getSectionSuggestions?: (section: any) => any[];
-    onSuggestionSelected?: (event: any, params: {suggestion: any, suggestionValue: string, method: string}) => void;
+    onSuggestionSelected?: (event: React.FormEvent, data: SuggestionSelectedEventData) => void;
     focusInputOnSuggestionClick?: boolean;
     theme?: Theme;
     id?: string;
   }
 
-  class Autosuggest extends __React.Component<AutosuggestProps, {}> {}
+  class Autosuggest extends React.Component<AutosuggestProps, any> {}
+}
 
+declare module 'react-autosuggest' {
+  import Autosuggest = ReactAutosuggest.Autosuggest;
   export = Autosuggest;
 }
