060558fdf47e55a88cbfb42d79a20ed4f684f19d
Linda_pp <rhysd@users.noreply.github.com>
2016-05-09 17:37:39 +0900

    github-electron: Fix callback's return type of app.makeSingleInstance() (#9250)
    The callback should return `void`, not `boolean`.
    
    https://github.com/electron/electron/blob/master/docs/api/app.md#appmakesingleinstancecallback

--- github-electron/github-electron.d.ts
+++ github-electron/github-electron.d.ts
@@ -280,7 +280,7 @@ declare namespace Electron {
 		 * multiple instances of your app to run, this will ensure that only a single instance
 		 * of your app is running, and other instances signal this instance and exit.
 		 */
-		makeSingleInstance(callback: (args: string[], workingDirectory: string) => boolean): boolean;
+		makeSingleInstance(callback: (args: string[], workingDirectory: string) => void): boolean;
 		/**
 		 * Changes the Application User Model ID to id.
 		 */
