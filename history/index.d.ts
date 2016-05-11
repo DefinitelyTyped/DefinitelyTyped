c59351d9a43e5b81c89a6e585b7b476f58154ff2
Ryan Cavanaugh <RyanCavanaugh@users.noreply.github.com>
2016-05-11 16:32:10 -0700

    Convert 'history' module

--- history/index.d.ts
+++ history/index.d.ts
@@ -1,61 +1,126 @@
-// Type definitions for History.js 1.8.0
-// Project: https://github.com/browserstate/history.js
-// Definitions by: Boris Yankov <https://github.com/borisyankov/>, Gidon Junge <https://github.com/gjunge/>
+// Type definitions for history v2.0.0
+// Project: https://github.com/rackt/history
+// Definitions by: Sergey Buturlakin <https://github.com/sergey-buturlakin>, Nathan Brown <https://github.com/ngbrown>
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
 
+// types based on https://github.com/rackt/history/blob/master/docs/Terms.md
 
-interface HistoryAdapter {
-    bind(element: any, event: string, callback: () => void): void;
-    trigger(element: any, event: string): void;
-    onDomLoad(callback: () => void): void;
+export type Action = string;
+export type BeforeUnloadHook = () => string | boolean;
+export type CreateHistory<T> = (options?: HistoryOptions) => T;
+export type CreateHistoryEnhancer<T, E> = (createHistory: CreateHistory<T>) => CreateHistory<T & E>;
+
+export interface History {
+    listenBefore(hook: TransitionHook): () => void;
+    listen(listener: LocationListener): () => void;
+    transitionTo(location: Location): void;
+    push(path: LocationDescriptor): void;
+    replace(path: LocationDescriptor): void;
+    go(n: number): void;
+    goBack(): void;
+    goForward(): void;
+    createKey(): LocationKey;
+    createPath(path: LocationDescriptor): Path;
+    createHref(path: LocationDescriptor): Href;
+    createLocation(path?: LocationDescriptor, action?: Action, key?: LocationKey): Location;
+
+    /** @deprecated use a location descriptor instead */
+    createLocation(path?: Path, state?: LocationState, action?: Action, key?: LocationKey): Location;
+    /** @deprecated use location.key to save state instead */
+    pushState(state: LocationState, path: Path): void;
+    /** @deprecated use location.key to save state instead */
+    replaceState(state: LocationState, path: Path): void;
+    /** @deprecated use location.key to save state instead */
+    setState(state: LocationState): void;
+    /** @deprecated use listenBefore instead */
+    registerTransitionHook(hook: TransitionHook): void;
+    /** @deprecated use the callback returned from listenBefore instead */
+    unregisterTransitionHook(hook: TransitionHook): void;
 }
 
-// Since History is defined in lib.d.ts as well
-// the name for our interfaces was chosen to be Historyjs
-// However at runtime you would need to do
-// https://github.com/DefinitelyTyped/DefinitelyTyped/issues/277
-// var Historyjs: Historyjs = <any>History;
-
-interface Historyjs {
-
-    enabled: boolean;
-
-    pushState(data: any, title: string, url: string): void;
-    replaceState(data: any, title: string, url: string): void;
-    getState(): HistoryState;
-    getStateByIndex(index: number): HistoryState;
-    getCurrentIndex(): number;
-    getHash(): string;
-
-    Adapter: HistoryAdapter;
-
-    back(): void;
-    forward(): void;
-    go(x: Number): void;
-
-    log(...messages: any[]): void;
-    debug(...messages: any[]): void;
-
-    options: HistoryOptions;
+export type HistoryOptions = {
+    getCurrentLocation?: () => Location;
+    finishTransition?: (nextLocation: Location) => boolean;
+    saveState?: (key: LocationKey, state: LocationState) => void;
+    go?: (n: number) => void;
+    getUserConfirmation?: (message: string, callback: (result: boolean) => void) => void;
+    keyLength?: number;
+    queryKey?: string | boolean;
+    stringifyQuery?: (obj: any) => string;
+    parseQueryString?: (str: string) => any;
+    basename?: string;
+    entries?: string | [any];
+    current?: number;
 }
 
-interface HistoryState {
-    data?: any;
-    title?: string;
-    url: string;
+export type Href = string
+
+export type Location = {
+    pathname: Pathname;
+    search: Search;
+    query: Query;
+    state: LocationState;
+    action: Action;
+    key: LocationKey;
+    basename?: string;
+};
+
+export type LocationDescriptorObject = {
+    pathname?: Pathname;
+    search?: Search;
+    query?: Query;
+    state?: LocationState;
+};
+
+export type LocationDescriptor = LocationDescriptorObject | Path;
+export type LocationKey = string;
+export type LocationListener = (location: Location) => void;
+export type LocationState = Object;
+export type Path = string // Pathname + QueryString;
+export type Pathname = string;
+export type Query = Object;
+export type QueryString = string;
+export type Search = string;
+
+export type TransitionHook = (location: Location, callback: (result: any) => void) => any
+
+
+export interface HistoryBeforeUnload {
+    listenBeforeUnload(hook: BeforeUnloadHook): () => void;
 }
 
-interface HistoryOptions {
-    hashChangeInterval?: number;
-    safariPollInterval?: number;
-    doubleCheckInterval?: number;
-    disableSuid?: boolean;
-    storeInterval?: number;
-    busyDelay?: number;
-    debug?: boolean;
-    initialTitle?: string;
-    html4Mode?: boolean;
-    delayInit?: number;
-
-
+export interface HistoryQueries {
+    pushState(state: LocationState, pathname: Pathname | Path, query?: Query): void;
+    replaceState(state: LocationState, pathname: Pathname | Path, query?: Query): void;
+    createPath(path: Path, query?: Query): Path;
+    createHref(path: Path, query?: Query): Href;
 }
+
+
+// Global usage, without modules, needs the small trick, because lib.d.ts
+// already has `history` and `History` global definitions:
+// var createHistory = ((window as any).History as HistoryModule.Module).createHistory;
+export interface Module {
+    createHistory: CreateHistory<History>;
+    createHashHistory: CreateHistory<History>;
+    createMemoryHistory: CreateHistory<History>;
+    createLocation(path?: Path, state?: LocationState, action?: Action, key?: LocationKey): Location;
+    useBasename<T>(createHistory: CreateHistory<T>): CreateHistory<T>;
+    useBeforeUnload<T>(createHistory: CreateHistory<T>): CreateHistory<T & HistoryBeforeUnload>;
+    useQueries<T>(createHistory: CreateHistory<T>): CreateHistory<T & HistoryQueries>;
+    actions: {
+        PUSH: string;
+        REPLACE: string;
+        POP: string;
+    };
+}
+
+export { default as createHistory } from "history/lib/createBrowserHistory";
+export { default as createHashHistory } from "history/lib/createHashHistory";
+export { default as createMemoryHistory } from "history/lib/createMemoryHistory";
+export { default as createLocation } from "history/lib/createLocation";
+export { default as useBasename } from "history/lib/useBasename";
+export { default as useBeforeUnload } from "history/lib/useBeforeUnload";
+export { default as useQueries } from "history/lib/useQueries";
+import * as Actions from "history/lib/actions";
+export { Actions };
