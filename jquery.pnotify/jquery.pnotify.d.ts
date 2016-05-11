30adeb9756074b68c0951552a63ea7f03904ad8e
Moes <abbasmhd@hotmail.com>
2016-05-11 13:29:25 +1000

    Update jquery.pnotify.d.ts to v3.0.0
    Update jquery.pnotify.d.ts to v3.0.0

--- jquery.pnotify/jquery.pnotify.d.ts
+++ jquery.pnotify/jquery.pnotify.d.ts
@@ -1,4 +1,4 @@
-// Type definitions for jquery.pnotify 2.x
+// Type definitions for jquery.pnotify 3.x
 // Project: https://github.com/sciactive/pnotify
 // Definitions by: David Sichau <https://github.com/DavidSichau>
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
@@ -24,11 +24,98 @@ interface PNotifyLabel {
     stick?: string;
 }
 
+
+interface IPNotifyconfirmButton {
+    text: string;
+    addClass: string;
+    /**
+     *  Whether to trigger this button when the user hits enter in a single line prompt.
+     */
+    promptTrigger: boolean;
+    click: (notice: PNotify, value: any) => void
+}
+
+interface PNotifyconfirm {
+    /**
+     * Make a confirmation box.
+     */
+    confirm: boolean;
+
+    /**
+     * Make a prompt.
+     */
+    prompt: boolean;
+    /**
+     * Classes to add to the input element of the prompt.
+     */
+    prompt_class: string
+
+    /**
+     * The default value of the prompt.
+     */
+    prompt_default: string
+
+    /**
+     * Whether the prompt should accept multiple lines of text.
+     */
+    prompt_multi_line: boolean;
+
+    /**
+     * Where to align the buttons. (right, center, left, justify)
+     */
+    align: string;
+
+    /**
+     * The buttons to display, and their callbacks.
+     */
+    buttons: IPNotifyconfirmButton[];
+
+}
+
+interface IPNotifyButtons {
+    /**
+     * Provide a button for the user to manually close the notice.
+     */
+    closer: boolean;
+    /**
+     * Only show the closer button on hover.
+     */
+    closer_hover: boolean;
+    /**
+     * Provide a button for the user to manually stick the notice.
+     */
+    sticker: boolean;
+    /**
+     * Only show the sticker button on hover.
+     */
+    sticker_hover: boolean;
+    /**
+     * Show the buttons even when the nonblock module is in use.
+     */
+    show_on_nonblock: boolean;
+    /**
+     * The various displayed text, helps facilitating internationalization.
+     */
+    labels: {
+        close: string;
+        stick: string;
+        unstick: string;
+    };
+    /**
+     * The classes to use for button icons. Leave them null to use the classes from the styling you're using.
+     */
+    classes: {
+        closer: string;
+        pin_up: string;
+        pin_down: string;
+    };
+}
+
 interface PNotifyOptions {
     /**
      * The notice's title. Either boolean false or string
      */
-    title?: any;
+    title?: string | boolean;
     /**
      * Whether to escape the content of the title. (Not allow HTML.)
      */
@@ -36,15 +123,15 @@ interface PNotifyOptions {
     /**
      * The notice's text. Either boolean false or string
      */
-    text?: any;
+    text?: string | boolean;
     /**
      * Whether to escape the content of the text. (Not allow HTML.)
      */
     text_escape?: boolean;
     /**
-     * What styling classes to use. (Can be either jqueryui or bootstrap.)
+     * What styling classes to use. (Can be either "brighttheme", "jqueryui", "bootstrap2", "bootstrap3", or "fontawesome".)
      */
-    styling?: string;
+    styling?: "brighttheme" | "jqueryui" | "bootstrap2" | "bootstrap3" | "fontawesome";
     /**
      * Additional classes to be added to the notice. (For custom styling.)
      */
@@ -89,7 +176,7 @@ interface PNotifyOptions {
     /**
      * Type of the notice. "notice", "info", "success", or "error".
      */
-    type?: string;
+    type?: "notice" | "info" | "success" | "error";
     /**
      * Set icon to true to use the default icon for the selected style/type, false for no icon, or a string for your own icon class.
      */
@@ -166,15 +253,38 @@ interface PNotifyOptions {
 }
 
 interface PNotify {
-    elem: JQuery;
-    
+
+    /**
+     * The state can be "initializing", "opening", "open", "closing", and "closed"
+     */
+
+    state: "initializing" | "opening" | "open" | "closing" | "closed";
+
+    /**
+     * This function is for updating the notice.
+     */
     update(options?: PNotifyOptions): void;
+
+    /**
+     * Remove the notice.
+     */
     remove(): void;
+
+    /**
+     *  Display the notice.
+     */
+    open(): void;
+
+    /**
+     *  Get the DOM element.
+     */
+    get(): JQuery;
+
 }
 
 interface PNotifyConstructor {
     new (options?: PNotifyOptions): PNotify;
-    
+
     removeAll(): void;
 }
 
