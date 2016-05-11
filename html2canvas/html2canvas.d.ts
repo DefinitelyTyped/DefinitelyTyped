5bc1d126b888c3f1569d1f3ab296bb6fe001a676
Pei-Tang Huang <tangtheone@gmail.com>
2016-05-11 16:39:59 +0800

    Format.

--- html2canvas/html2canvas.d.ts
+++ html2canvas/html2canvas.d.ts
@@ -36,7 +36,7 @@ declare namespace Html2Canvas {
 
         /** Whether to attempt to load cross-origin images as CORS served, before reverting back to proxy. */
         useCORS?: boolean;
-        
+
         /** Use svg powered rendering where available (FF11+). */
         svgRendering?: boolean;
     }
