154871ce8afdd555dc005a17c12ab55371dc2b85
Jeongho Nam <samchon@samchon.org>
2016-05-11 16:19:13 +0900

    TypeScript-STL v0.9.5-b.
    https://github.com/samchon/stl

---
+++ typescript-stl/typescript-stl-tests.ts
@@ -0,0 +1,9 @@
+﻿/// <reference path="typescript-stl.d.ts" />
+
+declare var global: any;
+declare var require: any;
+
+global["std"] = require("typescript-stl");
+
+std.example.test_all();
+console.log(std);
