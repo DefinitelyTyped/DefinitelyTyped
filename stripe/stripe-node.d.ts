47a09e21e345e9dd85a0f359229ac72343ce8e47
CodeAnimal <codeanimal@outlook.com>
2016-05-11 23:20:10 +0100

    Bump version to stripe-node 4.6.0

--- stripe/stripe-node.d.ts
+++ stripe/stripe-node.d.ts
@@ -1,4 +1,4 @@
-// Type definitions for stripe-node 4.5.0
+// Type definitions for stripe-node 4.6.0
 // Project: https://github.com/stripe/stripe-node/
 // Definitions by: William Johnston <https://github.com/wjohnsto>, Peter Harris <https://github.com/codeanimal/>
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
