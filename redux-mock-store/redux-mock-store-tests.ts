b29413a33259844d7e44972fd3713f942b93a51a
Marian Palkus <marian.palkus@cap3.de>
2016-05-13 13:22:31 +0200

    Add definitions for redux-mock-store.

---
+++ redux-mock-store/redux-mock-store-tests.ts
@@ -0,0 +1,33 @@
+/// <reference path="./redux-mock-store.d.ts" />
+
+// Tests are taken from ../redux/redux-tests.ts
+function counter(state: any, action: any) {
+    if (!state) {
+        state = 0;
+    }
+    switch (action.type) {
+        case 'INCREMENT':
+            return state + 1;
+        case 'DECREMENT':
+            return state - 1;
+        default:
+            return state;
+    }
+}
+
+function loggingMiddleware() {
+    return (next: Redux.Dispatch) => (action: any) => {
+        console.log(action.type);
+        next(action);
+    };
+}
+
+let createStoreWithMiddleware = Redux.applyMiddleware(loggingMiddleware)(Redux.createStore);
+let store = createStoreWithMiddleware(counter);
+
+
+store.subscribe(() =>
+    console.log(store.getState())
+);
+
+store.dispatch({ type: 'INCREMENT' });
