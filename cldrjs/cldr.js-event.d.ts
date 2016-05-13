9a053d6e4d529b9076c59ee97e95558a006fc425
Ryan Cavanaugh <RyanCavanaugh@users.noreply.github.com>
2016-05-12 17:56:58 -0700

    Fix cldrjs

--- cldrjs/cldr.js-event.d.ts
+++ cldrjs/cldr.js-event.d.ts
@@ -5,20 +5,6 @@
 
 // The definition file for event module.
 
-declare namespace cldr {
-    interface CldrStatic {
-        on(event:string, listener:(path:string, value:any) => void): void;
-        once(event:string, listener:(path:string, value:any) => void): void;
-        off(event:string, listener:(path:string, value:any) => void): void;
-    }
-
-    interface CldrFactory {
-        on(event:string, listener:(path:string, value:any) => void): void;
-        once(event:string, listener:(path:string, value:any) => void): void;
-        off(event:string, listener:(path:string, value:any) => void): void;
-    }
-}
-
 declare module "cldr/event" {
-    export = cldr;
+    export = Cldr;
 }
