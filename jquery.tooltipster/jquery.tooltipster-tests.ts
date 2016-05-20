914196de92a04b761228c17072cc46d4477971b2
VorobeY1326 <VorobeY1326@inbox.ru>
2016-05-20 20:27:34 +0500

    Version

--- jquery.tooltipster/jquery.tooltipster-tests.ts
+++ jquery.tooltipster/jquery.tooltipster-tests.ts
@@ -1,6 +1,6 @@
 /// <reference path="jquery.tooltipster.d.ts" />
 
-// Type definition tests for jQuery Tooltipster 3.0.5
+// Type definition tests for jQuery Tooltipster 3.3.0
 // Project: https://github.com/iamceege/tooltipster
 // Definitions by: Patrick Magee <https://github.com/pjmagee/>, Dmitry Pesterev <https://github.com/VorobeY1326/>
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
