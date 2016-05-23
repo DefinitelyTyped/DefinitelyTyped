e68aa42f5a73799bf040dd75611102ca1e4c5e4f
mpicciolli <mpiciolli@sovinty.com>
2016-05-23 13:25:44 +0200

    reorganize module

--- withings-lib/withings-lib.d.ts
+++ withings-lib/withings-lib.d.ts
@@ -3,53 +3,55 @@
 // Definitions by: mpicciolli <https://github.com/mpicciolli>
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
 
-declare module "withings-lib" {
+declare var WithingsClient:WithingsClient;
 
-    interface WithingsStatic {
-        new(options:{
-            consumerKey: string;
-            consumerSecret: string;
-            callbackUrl?: string;
-            accessToken?: string;
-            accessTokenSecret?:string;
-            userID?: string;
-        }): WithingsClient;
-    }
+declare module 'withings-lib' {
+    export = WithingsClient;
+}
 
-    interface WithingsClient {
-        /**
-         * OAuth
-         */
-        getRequestToken(cb:WithingsClientCallback): void;
-        authorizeUrl(token:string, tokenSecret:string):any;
-        getAccessToken(cb:WithingsClientCallback): void;
+interface WithingsClient {
+    /**
+     * Creates a new WithingsClient given the provided configuration
+     */
+    new(options:{
+        consumerKey:string;
+        consumerSecret:string;
+        callbackUrl?:string;
+        accessToken?:string;
+        accessTokenSecret?:string;
+        userID?:string;
+    }):WithingsClient;
 
-        /**
-         * Measures
-         */
-        getDailySteps(date:string|Date, cb:WithingsApiCallback): void;
-        getDailyCalories(date:string|Date, cb:WithingsApiCallback): void;
-        getMeasures(measType:string, startDate:string|Date, endDate: string|Date, cb:WithingsApiCallback):void
-        getWeightMeasures(startDate:string|Date, endDate: string|Date, cb:WithingsApiCallback): void;
-        getPulseMeasures(startDate:string|Date, endDate: string|Date, cb:WithingsApiCallback): void;
-        getSleepSummary(startDate:string|Date, endDate: string|Date, cb:WithingsApiCallback): void;
+    //OAuth
+    getRequestToken(cb:WithingsClientCallback):void;
+    authorizeUrl(token:string, tokenSecret:string):any;
+    getAccessToken(cb:WithingsClientCallback):void;
 
-        /**
-         * Notifications
-         */
-        createNotification(callbackUrl:string, comment:string, appli:number, cb:WithingsApiCallback): void;
-        getNotification(callbackUrl:string, appli:number, cb:WithingsApiCallback): void;
-        listNotifications(appli:number, cb:WithingsApiCallback): void;
-        revokeNotification(callbackUrl:string, appli:number, cb:WithingsApiCallback): void;
-    }
 
-    export interface WithingsClientCallback {
-        (error:string, token:string, tokenSecret:string): void;
-    }
+    //API Base Methods
+    apiCall(url:string, method:string, cb:WithingsApiCallback):void
+    get(service:string, action:string, params:any, cb:WithingsApiCallback):void
+    post(service:string, action:string, params:any, cb:WithingsApiCallback):void
 
-    export interface WithingsApiCallback {
-        (error:string, data:any): void;
-    }
+    //Measures
+    getDailySteps(date:string|Date, cb:WithingsApiCallback):void;
+    getDailyCalories(date:string|Date, cb:WithingsApiCallback):void;
+    getMeasures(measType:string, startDate:string|Date, endDate:string|Date, cb:WithingsApiCallback):void
+    getWeightMeasures(startDate:string|Date, endDate:string|Date, cb:WithingsApiCallback):void;
+    getPulseMeasures(startDate:string|Date, endDate:string|Date, cb:WithingsApiCallback):void;
+    getSleepSummary(startDate:string|Date, endDate:string|Date, cb:WithingsApiCallback):void;
 
-    export var Withings:WithingsStatic;
+    //Notifications
+    createNotification(callbackUrl:string, comment:string, appli:number, cb:WithingsApiCallback):void;
+    getNotification(callbackUrl:string, appli:number, cb:WithingsApiCallback):void;
+    listNotifications(appli:number, cb:WithingsApiCallback):void;
+    revokeNotification(callbackUrl:string, appli:number, cb:WithingsApiCallback):void;
+}
+
+interface WithingsClientCallback {
+    (error:string, token:string, tokenSecret:string):void;
+}
+
+interface WithingsApiCallback {
+    (error:string, data:any):void;
 }
