d321cc33b48e197209d10b8b476a316fc0f80f71
hookclaw <akatonbo73@yahoo.co.jp>
2016-05-24 11:52:43 +0900

    fix comment

--- jsonnet/jsonnet.d.ts
+++ jsonnet/jsonnet.d.ts
@@ -1,5 +1,5 @@
 // Type definitions for jsonnet
-// Project: https://www.npmjs.com/package/jsonnet
+// Project: https://github.com/yosuke-furukawa/node-jsonnet
 // Definitions by: Hookclaw <https://github.com/hookclaw>
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
 declare module "jsonnet" {
