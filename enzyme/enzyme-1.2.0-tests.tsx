b9989e602b3225d48aa4a9dfa0631ae41fea100d
Marian Palkus <marian.palkus@hotmail.de>
2016-05-25 10:39:55 +0200

    Update definitions of enzyme to version 2.3.0. (#9361)
    * Update definitions of enzyme to version 2.3.0.
    
    * Fixed URL of enzyme repository.

--- enzyme/enzyme-1.2.0-tests.tsx
+++ enzyme/enzyme-1.2.0-tests.tsx
@@ -1,4 +1,4 @@
-/// <reference path="enzyme.d.ts" />
+/// <reference path="enzyme-1.2.0.d.ts" />
 /// <reference path="../react/react.d.ts"/>
 
 import { shallow, mount, render, describeWithDOM, spyLifecycle } from "enzyme";
@@ -73,26 +73,6 @@ namespace ShallowWrapperTest {
         boolVal = shallowWrapper.contains(<div className="foo bar"/>);
     }
 
-    function test_containsMatchingElement() {
-        boolVal = shallowWrapper.contains(<div className="foo bar"/>);
-    }
-
-    function test_containsAllMatchingElements() {
-        boolVal = shallowWrapper.containsAllMatchingElements([<div className="foo bar"/>]);
-    }
-
-    function test_containsAnyMatchingElement() {
-        boolVal = shallowWrapper.containsAnyMatchingElements([<div className="foo bar"/>]);
-    }
-
-    function test_equals() {
-        boolVal = shallowWrapper.equals(<div className="foo bar"/>);
-    }
-
-    function test_matchesElement() {
-        boolVal = shallowWrapper.matchesElement(<div className="foo bar"/>);
-    }
-
     function test_hasClass() {
         boolVal = shallowWrapper.find('.my-button').hasClass('disabled');
     }
@@ -108,20 +88,6 @@ namespace ShallowWrapperTest {
     function test_children() {
         shallowWrapper = shallowWrapper.children();
         shallowWrapper.children(MyStatelessComponent).props().stateless;
-    }
-
-    function test_childAt() {
-        const childWrapper: ShallowWrapper<any, any> = shallowWrapper.childAt(0);
-
-        interface TmpType1 {
-            foo: any
-        }
-
-        interface TmpType2 {
-            bar: any
-        }
-
-        const childWrapper2: ShallowWrapper<TmpType1, TmpType2> = shallowWrapper.childAt<TmpType1, TmpType2>(0);
     }
 
     function test_parents() {
@@ -177,13 +143,6 @@ namespace ShallowWrapperTest {
     function test_state() {
         shallowWrapper.state();
         shallowWrapper.state('key');
-        const tmp: String = shallowWrapper.state<String>('key');
-    }
-
-    function test_context() {
-        shallowWrapper.context();
-        shallowWrapper.context('key');
-        const tmp: String = shallowWrapper.context<String>('key');
     }
 
     function test_props() {
@@ -192,12 +151,8 @@ namespace ShallowWrapperTest {
 
     function test_prop() {
         shallowWrapper.prop('key');
-        const tmp: String = shallowWrapper.prop<String>('key');
     }
 
-    function test_key() {
-        stringVal = shallowWrapper.key();
-    }
 
     function test_simulate(...args: any[]) {
         shallowWrapper.simulate('click');
@@ -205,15 +160,15 @@ namespace ShallowWrapperTest {
     }
 
     function test_setState() {
-        shallowWrapper = shallowWrapper.setState({ stateProperty: 'state' });
+        shallowWrapper = shallowWrapper.setState({stateProperty: 'state'});
     }
 
     function test_setProps() {
-        shallowWrapper = shallowWrapper.setProps({ propsProperty: 'foo' });
+        shallowWrapper = shallowWrapper.setProps({propsProperty: 'foo'});
     }
 
     function test_setContext() {
-        shallowWrapper = shallowWrapper.setContext({ name: 'baz' });
+        shallowWrapper = shallowWrapper.setContext({name: 'baz'});
     }
 
     function test_instance() {
@@ -229,11 +184,7 @@ namespace ShallowWrapperTest {
     }
 
     function test_type() {
-        var stringOrFunction: String | Function = shallowWrapper.type();
-    }
-
-    function test_name() {
-        var str: String = shallowWrapper.name();
+        var stringOrFunction: String|Function = shallowWrapper.type();
     }
 
     function test_forEach() {
@@ -299,24 +250,6 @@ namespace ReactWrapperTest {
         reactWrapper = reactWrapper.mount();
     }
 
-    function test_ref() {
-        reactWrapper = reactWrapper.ref('refName');
-        
-        interface TmpType1 {
-            foo: string
-        }
-        
-        interface TmpType2 {
-            bar: string
-        }
-        
-        const tmp: ReactWrapper<TmpType1, TmpType2> = reactWrapper.ref<TmpType1, TmpType2>('refName');
-    }
-    
-    function test_detach() {
-        reactWrapper.detach();
-    }
-    
     function test_find() {
         elementWrapper = reactWrapper.find('.selector');
         reactWrapper = reactWrapper.find(MyComponent);
@@ -345,26 +278,6 @@ namespace ReactWrapperTest {
         boolVal = reactWrapper.contains(<div className="foo bar"/>);
     }
 
-    function test_containsMatchingElement() {
-        boolVal = reactWrapper.contains(<div className="foo bar"/>);
-    }
-
-    function test_containsAllMatchingElements() {
-        boolVal = reactWrapper.containsAllMatchingElements([<div className="foo bar"/>]);
-    }
-
-    function test_containsAnyMatchingElement() {
-        boolVal = reactWrapper.containsAnyMatchingElements([<div className="foo bar"/>]);
-    }
-
-    function test_equals() {
-        boolVal = reactWrapper.equals(<div className="foo bar"/>);
-    }
-
-    function test_matchesElement() {
-        boolVal = reactWrapper.matchesElement(<div className="foo bar"/>);
-    }
-
     function test_hasClass() {
         boolVal = reactWrapper.find('.my-button').hasClass('disabled');
     }
@@ -379,20 +292,6 @@ namespace ReactWrapperTest {
 
     function test_children() {
         reactWrapper = reactWrapper.children();
-    }
-
-    function test_childAt() {
-        const childWrapper: ReactWrapper<any, any> = reactWrapper.childAt(0);
-
-        interface TmpType1 {
-            foo: any
-        }
-
-        interface TmpType2 {
-            bar: any
-        }
-
-        const childWrapper2: ReactWrapper<TmpType1, TmpType2> = reactWrapper.childAt<TmpType1, TmpType2>(0);
     }
 
     function test_parents() {
@@ -435,13 +334,6 @@ namespace ReactWrapperTest {
     function test_state() {
         reactWrapper.state();
         reactWrapper.state('key');
-        const tmp: String = reactWrapper.state<String>('key');
-    }
-
-    function test_context() {
-        reactWrapper.context();
-        reactWrapper.context('key');
-        const tmp: String = reactWrapper.context<String>('key');
     }
 
     function test_props() {
@@ -450,12 +342,8 @@ namespace ReactWrapperTest {
 
     function test_prop() {
         reactWrapper.prop('key');
-        const tmp: String = reactWrapper.prop<String>('key');
     }
 
-    function test_key() {
-        stringVal = reactWrapper.key();
-    }
 
     function test_simulate(...args: any[]) {
         reactWrapper.simulate('click');
@@ -463,15 +351,15 @@ namespace ReactWrapperTest {
     }
 
     function test_setState() {
-        reactWrapper = reactWrapper.setState({ stateProperty: 'state' });
+        reactWrapper = reactWrapper.setState({stateProperty: 'state'});
     }
 
     function test_setProps() {
-        reactWrapper = reactWrapper.setProps({ propsProperty: 'foo' });
+        reactWrapper = reactWrapper.setProps({propsProperty: 'foo'});
     }
 
     function test_setContext() {
-        reactWrapper = reactWrapper.setContext({ name: 'baz' });
+        reactWrapper = reactWrapper.setContext({name: 'baz'});
     }
 
     function test_instance() {
@@ -487,11 +375,7 @@ namespace ReactWrapperTest {
     }
 
     function test_type() {
-        var stringOrFunction: String | Function = reactWrapper.type();
-    }
-
-    function test_name() {
-        var str: String = reactWrapper.name();
+        var stringOrFunction: String|Function = reactWrapper.type();
     }
 
     function test_forEach() {
@@ -576,26 +460,6 @@ namespace CheerioWrapperTest {
         boolVal = cheerioWrapper.contains(<div className="foo bar"/>);
     }
 
-    function test_containsMatchingElement() {
-        boolVal = cheerioWrapper.contains(<div className="foo bar"/>);
-    }
-
-    function test_containsAllMatchingElements() {
-        boolVal = cheerioWrapper.containsAllMatchingElements([<div className="foo bar"/>]);
-    }
-
-    function test_containsAnyMatchingElement() {
-        boolVal = cheerioWrapper.containsAnyMatchingElements([<div className="foo bar"/>]);
-    }
-
-    function test_equals() {
-        boolVal = cheerioWrapper.equals(<div className="foo bar"/>);
-    }
-
-    function test_matchesElement() {
-        boolVal = cheerioWrapper.matchesElement(<div className="foo bar"/>);
-    }
-
     function test_hasClass() {
         boolVal = cheerioWrapper.find('.my-button').hasClass('disabled');
     }
@@ -610,20 +474,6 @@ namespace CheerioWrapperTest {
 
     function test_children() {
         cheerioWrapper = cheerioWrapper.children();
-    }
-
-    function test_childAt() {
-        const childWrapper: CheerioWrapper<any, any> = cheerioWrapper.childAt(0);
-
-        interface TmpType1 {
-            foo: any
-        }
-
-        interface TmpType2 {
-            bar: any
-        }
-
-        const childWrapper2: CheerioWrapper<TmpType1, TmpType2> = cheerioWrapper.childAt<TmpType1, TmpType2>(0);
     }
 
     function test_parents() {
@@ -666,13 +516,6 @@ namespace CheerioWrapperTest {
     function test_state() {
         cheerioWrapper.state();
         cheerioWrapper.state('key');
-        const tmp: String = cheerioWrapper.state<String>('key');
-    }
-
-    function test_context() {
-        cheerioWrapper.context();
-        cheerioWrapper.context('key');
-        const tmp: String = cheerioWrapper.context<String>('key');
     }
 
     function test_props() {
@@ -681,12 +524,8 @@ namespace CheerioWrapperTest {
 
     function test_prop() {
         cheerioWrapper.prop('key');
-        const tmp: String = cheerioWrapper.prop<String>('key');
     }
 
-    function test_key() {
-        stringVal = cheerioWrapper.key();
-    }
 
     function test_simulate(...args: any[]) {
         cheerioWrapper.simulate('click');
@@ -694,15 +533,15 @@ namespace CheerioWrapperTest {
     }
 
     function test_setState() {
-        cheerioWrapper = cheerioWrapper.setState({ stateProperty: 'state' });
+        cheerioWrapper = cheerioWrapper.setState({stateProperty: 'state'});
     }
 
     function test_setProps() {
-        cheerioWrapper = cheerioWrapper.setProps({ propsProperty: 'foo' });
+        cheerioWrapper = cheerioWrapper.setProps({propsProperty: 'foo'});
     }
 
     function test_setContext() {
-        cheerioWrapper = cheerioWrapper.setContext({ name: 'baz' });
+        cheerioWrapper = cheerioWrapper.setContext({name: 'baz'});
     }
 
     function test_instance() {
@@ -718,16 +557,12 @@ namespace CheerioWrapperTest {
     }
 
     function test_type() {
-        var stringOrFunction: String | Function = cheerioWrapper.type();
-    }
-
-    function test_name() {
-        var str: String = cheerioWrapper.name();
+        var stringOrFunction: String|Function = cheerioWrapper.type();
     }
 
     function test_forEach() {
         cheerioWrapper =
-            cheerioWrapper.forEach((aCheerioWrapper: CheerioWrapper<MyComponentProps, MyComponentState>) => {
+            cheerioWrapper.forEach((aCheerioWrapper: CheerioWrapper<MyComponentProps, MyComponentState>)=> {
             });
     }
 
