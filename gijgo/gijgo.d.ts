2309b41ca1914120c0fcf5ccdb2be77eb6988c1f
Atanas Atanasov <nascovt@gmail.com>
2016-05-25 10:01:12 +0300

    added comments

--- gijgo/gijgo.d.ts
+++ gijgo/gijgo.d.ts
@@ -1,4 +1,9 @@
-﻿//Grid
+// Type definitions for Gijgo v0.6.2
+// Project: http://gijgo.com
+// Definitions by: Atanas Atanasov <https://github.com/atatanasov>
+// Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
+
+//Grid
 
 interface GridPager {
     limit: number;
