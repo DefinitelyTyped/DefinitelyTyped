e67a5c16b07017ee14c44d6cb0d6458b4cb931c2
Pei-Tang Huang <tangtheone@gmail.com>
2016-05-31 01:29:09 +0800

    Correct extension interface.

--- showdown/showdown-tests.ts
+++ showdown/showdown-tests.ts
@@ -8,13 +8,14 @@ import showdown = require('showdown');
 var exampleMarkdown = '#hello, markdown',
     converter = new showdown.Converter();
 
-var myExt: showdown.LangExtension = {type: 'lang', filter: (text, converter)=>{return text.replace('#', '*')}};
+var myExt: showdown.ShowdownExtension = { type: 'output', filter: (text, converter) => { return text.replace('#', '*') } };
 showdown.extension('my-ext', myExt);
+
 var preloadedExtensions = [ 'my-ext' ],
     extensionsConverter = new showdown.Converter({ extensions: preloadedExtensions });
 
 var configuredConverter = new showdown.Converter();
-    configuredConverter.addExtension({type: 'lang', filter: (text, converter)=>{return text.replace('#', '*')}}, 'myext');
+    configuredConverter.addExtension({type: 'output', filter: (text, converter)=>{return text.replace('#', '*')}}, 'myext');
 
 console.log(converter.makeHtml(exampleMarkdown));
 // should log '<h1 id="hellomarkdown">hello, markdown</h1>'
@@ -24,3 +25,31 @@ console.log(extensionsConverter.makeHtml(exampleMarkdown));
 
 console.log(configuredConverter.makeHtml(exampleMarkdown));
 // should log '<p>*hello, markdown</p>'
+
+
+showdown.extension('myExt', function () {
+  var matches: string[] = [];
+  return [
+    {
+      type: 'lang',
+      regex: /%start%([^]+?)%end%/gi,
+      replace: function (s: string, match: string) {
+        matches.push(match);
+        var n = matches.length - 1;
+        return '%PLACEHOLDER' + n + '%';
+      }
+    },
+    {
+      type: 'output',
+      filter: function (text) {
+        for (var i = 0; i < matches.length; ++i) {
+          var pat = '<p>%PLACEHOLDER' + i + '% *<\/p>';
+          text = text.replace(new RegExp(pat, 'gi'), matches[i]);
+        }
+        // reset array
+        matches = [];
+        return text;
+      }
+    }
+  ]
+});
