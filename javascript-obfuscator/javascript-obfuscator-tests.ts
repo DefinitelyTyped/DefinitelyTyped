0e20d06bc016c4c1ebdeb698aa3f68a14a12ec46
Timofey Kachalov <sanex3339@users.noreply.github.com>
2016-05-31 17:41:48 +0300

    javascript-obfuscator added new options

--- javascript-obfuscator/javascript-obfuscator-tests.ts
+++ javascript-obfuscator/javascript-obfuscator-tests.ts
@@ -5,6 +5,7 @@ import { JavaScriptObfuscator } from 'javascript-obfuscator';
 let sourceCode1: string = JavaScriptObfuscator.obfuscate('var foo = 1;');
 let sourceCode2: string = JavaScriptObfuscator.obfuscate('var foo = 1;', {
   compact: true,
+  encodeUnicodeArray: true,
   debugProtection: false,
   debugProtectionInterval: false,
   disableConsoleOutput: true,
