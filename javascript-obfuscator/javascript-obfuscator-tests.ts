204efb0b63011abf207928d920f3fd3a26876593
Timofey Kachalov <sanex3339@users.noreply.github.com>
2016-06-01 12:21:22 +0300

    Update javascript-obfuscator-tests.ts

--- javascript-obfuscator/javascript-obfuscator-tests.ts
+++ javascript-obfuscator/javascript-obfuscator-tests.ts
@@ -5,10 +5,11 @@ import { JavaScriptObfuscator } from 'javascript-obfuscator';
 let sourceCode1: string = JavaScriptObfuscator.obfuscate('var foo = 1;');
 let sourceCode2: string = JavaScriptObfuscator.obfuscate('var foo = 1;', {
   compact: true,
-  encodeUnicodeArray: true,
   debugProtection: false,
   debugProtectionInterval: false,
   disableConsoleOutput: true,
+  encodeUnicodeLiterals: true,
   rotateUnicodeArray: true,
+  unicodeArray: true,
   wrapUnicodeArrayCalls: true
 });
