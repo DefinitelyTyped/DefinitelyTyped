2fee4f256c56d941e14b63597b4c5a12006f6b70
Gerhard Stöbich <deb2001-github@yahoo.de>
2016-06-08 08:47:44 +0200

    Add EventEmitter APIs introduced in Node 6:
    emitter.eventNames() ==> https://nodejs.org/dist/latest-v6.x/docs/api/events.html#events_emitter_eventnames
    emitter.prependListener(eventName, listener) ==> https://nodejs.org/dist/latest-v6.x/docs/api/events.html#events_emitter_prependlistener_eventname_listener
    emitter.prependOnceListener(eventName, listener) ==>  https://nodejs.org/dist/latest-v6.x/docs/api/events.html#events_emitter_prependoncelistener_eventname_listener

--- node/node.d.ts
+++ node/node.d.ts
@@ -536,12 +536,15 @@ declare module "events" {
         addListener(event: string, listener: Function): this;
         on(event: string, listener: Function): this;
         once(event: string, listener: Function): this;
+        prependListener(event: string, listener: Function): this;
+        prependOnceListener(event: string, listener: Function): this;
         removeListener(event: string, listener: Function): this;
         removeAllListeners(event?: string): this;
         setMaxListeners(n: number): this;
         getMaxListeners(): number;
         listeners(event: string): Function[];
         emit(event: string, ...args: any[]): boolean;
+        eventNames(): string[];
         listenerCount(type: string): number;
     }
 }
