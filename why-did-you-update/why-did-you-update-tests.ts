0cadc91e97a08e00a04d73d9eae218191913ab38
rhysd <lin90162@yahoo.co.jp>
2016-06-14 11:08:51 +0900

    Add reference magic comments to test code

--- why-did-you-update/why-did-you-update-tests.ts
+++ why-did-you-update/why-did-you-update-tests.ts
@@ -1,4 +1,7 @@
-import * as React from 'react';
+/// <reference path="./why-did-you-update.d.ts" />
+/// <reference path="../react/react.d.ts" />
+
+import * as React from "react";
 import {whyDidYouUpdate} from "why-did-you-update";
 
 whyDidYouUpdate(React);
