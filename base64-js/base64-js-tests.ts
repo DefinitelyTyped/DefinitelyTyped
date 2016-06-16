c9ebd4a27b366d951ae21c6bd7b0905c03f97bf1
Peter Safranek <pe8ter@mac.com>
2016-06-15 22:53:59 -0700

    Fix bad file names.

--- base64-js/base64-js-tests.ts
+++ base64-js/base64-js-tests.ts
@@ -1,6 +1,6 @@
 /// <reference path="base64-js.d.ts" />
 
-import base64js = require('base64-js');
+import * as base64js from 'base64-js';
 
 const bytes: Uint8Array = base64js.toByteArray('shemp');
 const decoded: string = base64js.fromByteArray(new Uint8Array(0));
