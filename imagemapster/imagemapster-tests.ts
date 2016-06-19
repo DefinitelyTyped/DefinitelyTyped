a8ba1c1754ac3619686d39382ea78e56058c466f
delphinus <delphinus@remora.cx>
2016-06-19 13:19:37 +0900

    Rename jquery.imagemapster => imagemapster

--- imagemapster/imagemapster-tests.ts
+++ imagemapster/imagemapster-tests.ts
@@ -1,41 +1,41 @@
-///<reference path="./jquery.imagemapster.d.ts" />
+///<reference path="./imagemapster.d.ts" />
 ///<reference path="../jquery/jquery.d.ts" />
 
-const areaOptions: JQueryImageMapster.AreaRenderingOptions = {
+const areaOptions: ImageMapster.AreaRenderingOptions = {
     key:         "foo",
     includeKeys: "foo",
     isMask:      true,
     toolTip:     "tooltip",
 };
 
-const onClickData: JQueryImageMapster.OnClickData = {
+const onClickData: ImageMapster.OnClickData = {
     listTarget: $(),
     key:        "foo",
     e:          $.Event("click"),
     selected:   true,
 }
 
-const onMouseData: JQueryImageMapster.OnMouseData = {
+const onMouseData: ImageMapster.OnMouseData = {
     key:      "foo",
     selected: true,
     e:        $.Event("click"),
     options:  areaOptions,
 };
 
-const onGetListData: JQueryImageMapster.OnGetListData = {
+const onGetListData: ImageMapster.OnGetListData = {
     key:     "foo",
     value:   "foo",
     area:    [{}],
     options: areaOptions,
 };
 
-const onStateChangeData: JQueryImageMapster.OnStateChangeData = {
+const onStateChangeData: ImageMapster.OnStateChangeData = {
     key:      "foo",
     state:    "highlight",
     selected: true,
 };
 
-const onShowToolTipData: JQueryImageMapster.OnShowToolTipData = {
+const onShowToolTipData: ImageMapster.OnShowToolTipData = {
     toolTip: $(),
     key: "foo",
     selected: true,
@@ -44,7 +44,7 @@ const onShowToolTipData: JQueryImageMapster.OnShowToolTipData = {
 
 const bool = true;
 
-const options: JQueryImageMapster.Options = {
+const options: ImageMapster.Options = {
     mapKey:                "foo",
     mapValue:              "foo",
     clickNavigate:         true,
