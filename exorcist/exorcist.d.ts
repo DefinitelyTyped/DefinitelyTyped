5c5900e72f7511437ad55873343441f12036cb92
TeamworkGuy2 <straitfrommars@rocketmail.com>
2016-06-20 23:56:39 +0000

    project URL mistake

--- exorcist/exorcist.d.ts
+++ exorcist/exorcist.d.ts
@@ -1,5 +1,5 @@
 // Type definitions for exorcist v0.4.0
-// Project: https://github.com/substack/watchify
+// Project: https://github.com/substack/exorcist
 // Definitions by: TeamworkGuy2 <https://github.com/TeamworkGuy2>
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
 
