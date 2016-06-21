25896fc1b3a0e69c593c8fd8d3a8eff3b0079102
Daniel McAssey <daniel.mcassey@gmail.com>
2016-06-21 14:15:40 +0100

    Add version number

--- resumablejs/resumable.d.ts
+++ resumablejs/resumable.d.ts
@@ -1,4 +1,4 @@
-// Type definitions for Resumable.js
+// Type definitions for Resumable.js v1.0.2
 // Project: https://github.com/23/resumable.js
 // Definitions by: Daniel McAssey <https://github.com/DanielMcAssey>
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
