7f7603c775d511dddf6493bad99ad8a3737abda6
Jeongho Nam <samchon@samchon.org>
2016-06-21 16:44:04 +0900

    IEntityGroup.find has deprecated
    ES6 has Array.find method.

--- samchon-framework/samchon-framework-tests.ts
+++ samchon-framework/samchon-framework-tests.ts
@@ -1,9 +1,4 @@
 /// <reference path="samchon-framework.d.ts" />
 
 import samchon = require("samchon-framework");
-
-let xml = new samchon.library.XML();
-xml.setTag("font");
-xml.setProperty("fontSize", "13");
-
-console.log(xml.toString());
+console.log(samchon);
