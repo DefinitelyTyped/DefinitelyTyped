a969442874d475655313f7c4722d40cd056f9238
Jeongho Nam <samchon@samchon.org>
2016-06-21 15:29:04 +0900

    TypeScript-STL and Samchon-Framework, right sentence

--- samchon-framework/samchon-framework-tests.ts
+++ samchon-framework/samchon-framework-tests.ts
@@ -1,7 +1,4 @@
 /// <reference path="samchon-framework.d.ts" />
 
-declare var global: any;
-declare var require: any;
-
-global["samchon"] = require("samchon-framework");
+import samchon = require("samchon-framework");
 console.log(samchon);
