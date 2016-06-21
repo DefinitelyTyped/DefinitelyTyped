e4a48d3dc2d3b05a50dec57bd58d6a781cd6f855
Jeongho Nam <samchon@samchon.org>
2016-06-21 16:11:12 +0900

    Samchon-Framework-Tests

--- samchon-framework/samchon-framework-tests.ts
+++ samchon-framework/samchon-framework-tests.ts
@@ -1,4 +1,9 @@
 /// <reference path="samchon-framework.d.ts" />
 
 import samchon = require("samchon-framework");
-console.log(samchon);
+
+let xml = new samchon.library.XML();
+xml.setTag("font");
+xml.setProperty("fontSize", "13");
+
+console.log(xml.toString());
