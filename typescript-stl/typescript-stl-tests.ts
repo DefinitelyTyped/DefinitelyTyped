bc740ee011e894ed5d5def89dcced2774ff27e87
Jeongho Nam <samchon@samchon.org>
2016-06-21 15:29:04 +0900

    TypeScript-STL and Samchon-Framework, right sentence

--- typescript-stl/typescript-stl-tests.ts
+++ typescript-stl/typescript-stl-tests.ts
@@ -1,7 +1,4 @@
 ﻿/// <reference path="typescript-stl.d.ts" />
 
-declare var global: any;
-declare var require: any;
-
-global["std"] = require("typescript-stl");
+import std = require("typescript-stl");
 std.example.test_all();
