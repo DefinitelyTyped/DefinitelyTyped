e902eb2f75f7edd9cf2d10e00bd434c07b439e30
Lionel <lionel@lumographe.fr>
2016-06-29 17:22:05 +0200

    add missing properties of $websocket instance
    This PR add the missing properties of $websocket instance as mention in the documentation https://github.com/AngularClass/angular-websocket#properties

--- angular-websocket/angular-websocket-tests.ts
+++ angular-websocket/angular-websocket-tests.ts
@@ -1,6 +1,7 @@
 /// <reference path="angular-websocket.d.ts" />
 
 let dummySocket: ng.websocket.IWebSocket;
+let dummyPromise: ng.IPromise<void>;
 
 let provider: ng.websocket.IWebSocketProvider = (url: string) => {
     return dummySocket;
@@ -23,3 +24,24 @@ socket.close();
 
 socket.send("Some great data here!").finally(() => {});
 socket.send({ list: [1, 2, 3, 4] });
+
+socket.socket.send("data");
+socket.socket.close();
+socket.socket.close(1);
+socket.socket.close(1, "reason");
+
+socket.sendQueue.push({ message: "msg", defered: dummyPromise });
+
+socket.onOpenCallbacks.push((event: Event) => {});
+socket.onCloseCallbacks.push((event: CloseEvent) => {});
+socket.onErrorCallbacks.push((event: Event) => {});
+socket.onMessageCallbacks.push({ fn: (event: MessageEvent) => {}, pattern: 'Some Filter', autoApply: true });
+socket.onMessageCallbacks.push({ fn: (event: MessageEvent) => {}, pattern: /Some Filter/, autoApply: true });
+socket.onMessageCallbacks.push({ fn: (event: MessageEvent) => {}, pattern: undefined, autoApply: true });
+
+socket.readyState = 0;
+
+socket.initialTimeout = 10;
+
+socket.maxTimeout = 5000;
+
