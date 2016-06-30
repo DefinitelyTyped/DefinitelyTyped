87148dc961ed944420e7b8e99017de4d76634498
Remo H. Jansen <remo.jansen@wolksoftware.com>
2016-06-30 16:58:44 +0100

    Create reflect-metadata-test.ts

---
+++ reflect-metadata/reflect-metadata-test.ts
@@ -0,0 +1,42 @@
+/// <reference path="reflect-metadata.d.ts" />
+
+// define metadata on an object or property
+Reflect.defineMetadata(metadataKey, metadataValue, target);
+Reflect.defineMetadata(metadataKey, metadataValue, target, propertyKey);
+ 
+// check for presence of a metadata key on the prototype chain of an object or property
+let result = Reflect.hasMetadata(metadataKey, target);
+let result = Reflect.hasMetadata(metadataKey, target, propertyKey);
+ 
+// check for presence of an own metadata key of an object or property
+let result = Reflect.hasOwnMetadata(metadataKey, target);
+let result = Reflect.hasOwnMetadata(metadataKey, target, propertyKey);
+ 
+// get metadata value of a metadata key on the prototype chain of an object or property
+let result = Reflect.getMetadata(metadataKey, target);
+let result = Reflect.getMetadata(metadataKey, target, propertyKey);
+ 
+// get metadata value of an own metadata key of an object or property
+let result = Reflect.getOwnMetadata(metadataKey, target);
+let result = Reflect.getOwnMetadata(metadataKey, target, propertyKey);
+ 
+// get all metadata keys on the prototype chain of an object or property
+let result = Reflect.getMetadataKeys(target);
+let result = Reflect.getMetadataKeys(target, propertyKey);
+ 
+// get all own metadata keys of an object or property
+let result = Reflect.getOwnMetadataKeys(target);
+let result = Reflect.getOwnMetadataKeys(target, propertyKey);
+ 
+// delete metadata from an object or property
+let result = Reflect.deleteMetadata(metadataKey, target);
+let result = Reflect.deleteMetadata(metadataKey, target, propertyKey);
+ 
+// apply metadata via a decorator to a constructor
+@Reflect.metadata(metadataKey, metadataValue)
+class C {
+  // apply metadata via a decorator to a method (property)
+  @Reflect.metadata(metadataKey, metadataValue)
+  method() {  
+  }
+}
