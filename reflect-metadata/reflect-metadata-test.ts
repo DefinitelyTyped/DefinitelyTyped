a38994a6955811f05d6300ed33d6cacee27028bc
Remo H. Jansen <remo.jansen@wolksoftware.com>
2016-06-30 17:07:17 +0100

    Update reflect-metadata-test.ts

--- reflect-metadata/reflect-metadata-test.ts
+++ reflect-metadata/reflect-metadata-test.ts
@@ -15,32 +15,32 @@ Reflect.defineMetadata(metadataKey, metadataValue, target);
 Reflect.defineMetadata(metadataKey, metadataValue, target, propertyKey);
  
 // check for presence of a metadata key on the prototype chain of an object or property
-let result = Reflect.hasMetadata(metadataKey, target);
-let result = Reflect.hasMetadata(metadataKey, target, propertyKey);
+let result1 = Reflect.hasMetadata(metadataKey, target);
+let result2 = Reflect.hasMetadata(metadataKey, target, propertyKey);
  
 // check for presence of an own metadata key of an object or property
-let result = Reflect.hasOwnMetadata(metadataKey, target);
-let result = Reflect.hasOwnMetadata(metadataKey, target, propertyKey);
+let result3 = Reflect.hasOwnMetadata(metadataKey, target);
+let result4 = Reflect.hasOwnMetadata(metadataKey, target, propertyKey);
  
 // get metadata value of a metadata key on the prototype chain of an object or property
-let result = Reflect.getMetadata(metadataKey, target);
-let result = Reflect.getMetadata(metadataKey, target, propertyKey);
+let result5 = Reflect.getMetadata(metadataKey, target);
+let result6 = Reflect.getMetadata(metadataKey, target, propertyKey);
  
 // get metadata value of an own metadata key of an object or property
-let result = Reflect.getOwnMetadata(metadataKey, target);
-let result = Reflect.getOwnMetadata(metadataKey, target, propertyKey);
+let result7 = Reflect.getOwnMetadata(metadataKey, target);
+let result8 = Reflect.getOwnMetadata(metadataKey, target, propertyKey);
  
 // get all metadata keys on the prototype chain of an object or property
-let result = Reflect.getMetadataKeys(target);
-let result = Reflect.getMetadataKeys(target, propertyKey);
+let result9 = Reflect.getMetadataKeys(target);
+let result10 = Reflect.getMetadataKeys(target, propertyKey);
  
 // get all own metadata keys of an object or property
-let result = Reflect.getOwnMetadataKeys(target);
-let result = Reflect.getOwnMetadataKeys(target, propertyKey);
+let result11 = Reflect.getOwnMetadataKeys(target);
+let result12 = Reflect.getOwnMetadataKeys(target, propertyKey);
  
 // delete metadata from an object or property
-let result = Reflect.deleteMetadata(metadataKey, target);
-let result = Reflect.deleteMetadata(metadataKey, target, propertyKey);
+let result13 = Reflect.deleteMetadata(metadataKey, target);
+let result14 = Reflect.deleteMetadata(metadataKey, target, propertyKey);
  
 // apply metadata via a decorator to a constructor
 @Reflect.metadata(metadataKey, metadataValue)
