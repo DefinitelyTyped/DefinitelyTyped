ab0cff75e0dfc980d42cdfa3c6671fda34a82220
Slava Shpitalny <slavik57@gmail.com>
2016-07-01 14:32:05 +0300

    add tests to promise.prototype.finally and fix it

--- promise.prototype.finally/promise.prototype.finally.d.ts
+++ promise.prototype.finally/promise.prototype.finally.d.ts
@@ -1,5 +1,8 @@
-declare module 'es6-promise/dist/es6-promise' {
-    export interface Promise <R> {
-      finally?<U>(onFinally?: () => U | Promise<U>): Promise<U>;
-    }
+// Type definitions for promise.prototype.finally v1.0.1
+// Project: https://github.com/matthew-andrews/Promise.prototype.finally
+// Definitions by: Slava Shpitalny <https://github.com/slavik57>
+// Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
+
+declare interface Promise<T> {
+    finally<U>(onFinally?: () => U | Promise<U>): Promise<U>;
 }
