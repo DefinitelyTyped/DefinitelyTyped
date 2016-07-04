26bf0cacc010ccb4739530437910aba4f4b68b4a
Gayan Ranasinghe <gayan.ranasinghe@solid.lk>
2016-07-04 16:22:02 +0530

    jquery reference

--- kendo-ui/kendo-ui.d.ts
+++ kendo-ui/kendo-ui.d.ts
@@ -2,7 +2,7 @@
 // Project: http://www.telerik.com/kendo-ui
 // Definitions by: Telerik <https://github.com/telerik/>
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
-
+/// <reference path="../jquery/jquery.d.ts" />
 
 
 declare namespace kendo {
