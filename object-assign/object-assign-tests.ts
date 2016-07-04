249668030d716aae5116a97de534a700b4f26809
newash <bpalyi@gmail.com>
2016-07-04 23:50:52 +0100

    ES6 declaration

--- object-assign/object-assign-tests.ts
+++ object-assign/object-assign-tests.ts
@@ -1,5 +1,5 @@
 /// <reference path="object-assign.d.ts" />
-import objectAssign = require("object-assign");
+import objectAssign from "object-assign";
 
 function assign1() {
   var result = objectAssign({hello: "world"});
