cc2b03ca15f7a9e3589020384d474f39a3a07f5b
m-a-wilson <m-a-wilson@users.noreply.github.com>
2016-07-05 17:36:07 -0400

    Add files via upload

---
+++ masonry-layout/masonry-layout.d.ts
@@ -0,0 +1,63 @@
+// Type definitions for Masonry 4.0.0 
+// Project: https://github.com/desandro/masonry 
+// Original definitions by: Travis Brown <https://github.com/warriorrocker>
+// Revised definitions by: Mark Wilson <https://github.com/m-a-wilson>
+// Definitions: https://github.com/borisyankov/DefinitelyTyped 
+
+/// <reference path="../jquery/jquery.d.ts" /> 
+
+declare class Masonry implements MasonryGrid { 
+    constructor(options?: MasonryOptions); 
+    constructor(selector: string, options?: MasonryOptions); 
+} 
+
+interface MasonryGrid { 
+    masonry?(): void; 
+    masonry?(eventName: string, listener: any): void; 
+
+// Layout 
+    layout?(): void; 
+    layoutItems?(items: Array<any>, isStill?: boolean): void; 
+    stamp?(elements: Array<any>): void; 
+    unstamp?(elements: Array<any>): void; 
+
+// Add and Remove Items 
+    appended?(elements: Array<any>): void; 
+    prepended?(elements: Array<any>): void; 
+    addItems?(elements: Array<any>): void; 
+    remove?(elements: Array<any>): void; 
+
+// Events 
+    on?(eventName: string, listener: any): void; 
+    off?(eventName: string, listener: any): void; 
+    once?(eventName: string, listener: any): void; 
+
+// Utilities 
+    reloadItems?(): void; 
+    destroy?(): void; 
+    getItemElements?(): Array<any>; 
+    data?(element: Element): Masonry; 
+} 
+
+interface MasonryOptions { 
+
+// Layout 
+    itemSelector?: string; 
+    columnWidth?: any; 
+    percentPosition?: boolean; 
+    gutter?: any; 
+    stamp?: string; 
+    fitWidth?: boolean; 
+    originLeft?: boolean; 
+    originTop?: boolean; 
+
+// Setup 
+    containerStyle?: Object; 
+    transitionDuration?: any; 
+    resize?: boolean; 
+    initLayout?: boolean; 
+} 
+
+interface JQuery { 
+    masonry(options?: MasonryOptions): JQuery; 
+} 
