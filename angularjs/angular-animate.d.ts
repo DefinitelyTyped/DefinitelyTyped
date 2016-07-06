49f4ec7f682dc113ed176d3bef564bccee2ba396
Andrew Mayorov <xorets@gmail.com>
2016-07-06 10:28:20 +0200

    Removed one overload in favour for optional parameter.

--- angularjs/angular-animate.d.ts
+++ angularjs/angular-animate.d.ts
@@ -69,8 +69,7 @@ declare namespace angular.animate {
         * @param value If provided then set the animation on or off.
         * @returns current animation state
         */
-        enabled(): boolean;
-        enabled(value: boolean): boolean;
+        enabled(value?: boolean): boolean;
         enabled(element: JQuery, value?: boolean): boolean;
 
         /**
