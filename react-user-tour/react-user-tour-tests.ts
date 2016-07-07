e341183c2bb21c90d9203e87095020ccb63eeea0
Carlo Cancellieri <ccancellieri@gmail.com>
2016-07-07 10:36:54 +0200

    fix spacing (4 spaces). Implement the test. Add reference to react.d.ts

--- react-user-tour/react-user-tour-tests.ts
+++
@@ -1,6 +0,0 @@
-// Tests for type definitions for react-user-tour
-// Project: https://github.com/socialtables/react-user-tour
-// Definitions by: Carlo Cancellieri <https://github.com/ccancellieri>
-// Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
-
-/// <reference path="react-user-tour.d.ts" />
