0ede55501a92a835708da567a80f18e883e561df
Carlo Cancellieri <ccancellieri@gmail.com>
2016-07-08 12:17:59 +0200

    applying review changes

--- react-user-tour/react-user-tour-tests.tsx
+++ react-user-tour/react-user-tour-tests.tsx
@@ -3,29 +3,29 @@
 // Definitions by: Carlo Cancellieri <https://github.com/ccancellieri>
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
 
-/// <reference path="react-user-tour.d.ts" />
-/// <reference path="../react/react.d.ts" />
-/// <reference path="../react/react-dom.d.ts" />
+/// <reference path='react-user-tour.d.ts' />
+/// <reference path='../react/react.d.ts' />
+/// <reference path='../react/react-dom.d.ts' />
 
-import React = require("react");
-import ReactDOM = require("react-dom");
+import React = require('react');
+import ReactDOM = require('react-dom');
 
-import ReactUserTour from "react-user-tour";
+import ReactUserTour from 'react-user-tour';
 
 interface State {
-  tourStep:number
-  isTourActive:boolean
+    tourStep:number;
+    isTourActive:boolean;
 }
 
 class TestApp extends React.Component<{}, State> {
 
     constructor(p:any){
-        super(p)
+        super(p);
 
         this.setState({
             isTourActive:true,
             tourStep:1
-        })
+        });
     }
 
     render() {
@@ -37,21 +37,19 @@ class TestApp extends React.Component<{}, State> {
             onCancel={() => this.setState({tourStep: this.state.tourStep, isTourActive: false})}
             steps={[
                 {
-                  step: 1,
-                  selector: ".MyClass",
-                  title: <div>React User Tour</div>,
-                  body: <div>Provide a simple guided tour around a website utilizing css selectors.</div>,
-                  position: "bottom"
+                    step: 1,
+                    selector: '.MyClass',
+                    title: <div>React User Tour</div>,
+                    body: <div>Provide a simple guided tour around a website utilizing css selectors.</div>,
+                    position: 'bottom'
                 }
             ]}
-            />
+            />;
 
-
-
-        return <div id='test-app' class='.MyClass'>
-                {Tour}
-            </div>
+        return  <div id='test-app' class='.MyClass'>
+                    {Tour}
+                </div>;
     }
 }
 
-ReactDOM.render(React.createElement(TestApp, {}), document.getElementById("test-app"));
+ReactDOM.render(React.createElement(TestApp, {}), document.getElementById('test-app'));
