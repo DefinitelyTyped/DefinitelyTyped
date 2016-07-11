bee59cf58c9a98f71a0221e992750fff150d6df1
kaave <kyousuke.abe@aliinc.co.jp>
2016-07-12 08:54:10 +0900

    rails-actioncable: Add npm support

--- rails-actioncable/rails-actioncable.d.ts
+++ rails-actioncable/rails-actioncable.d.ts
@@ -8,24 +8,26 @@ declare module ActionCable {
     unsubscribe(): void;
     perform(action: string, data: {}): void;
   }
+
   interface Subscriptions {
     create(chan_name: string, obj: {}): Channel;
   }
+
   interface Cable {
     subscriptions: Subscriptions;
   }
+
   function createConsumer(): Cable;
   function createConsumer(url: string): Cable;
 }
 
 declare interface AppInterface {
   cable?: ActionCable.Cable;
-  network?: ActionCable.Channel; 
+  network?: ActionCable.Channel;
 }
 
 declare var App: AppInterface;
 
-
-
-
-
+declare module 'actioncable' {
+  export = ActionCable;
+}
