9fd9490da2e6f7dca8707109edca1c900e9d25e5
Cao, Jiannan <frogcjn@163.com>
2016-07-12 18:42:27 +0800

    resolve conflict

--- supertest-as-promised/supertest-as-promised.d.ts
+++ supertest-as-promised/supertest-as-promised.d.ts
@@ -6,19 +6,20 @@
 /// <reference path='../superagent/superagent.d.ts' />
 /// <reference path='../supertest/supertest.d.ts' />
 /// <reference path='../bluebird/bluebird.d.ts' />
+/// <reference path='../core-js/core-js.d.ts' />
 
 declare module "supertest-as-promised" {
   import * as supertest from "supertest";
-  import * as PromiseBluebird from "bluebird";
-
   import { SuperTest, Response } from "supertest";
+  import * as PromiseBlurbird from "bluebird";
 
   function supertestAsPromised(app: any): SuperTest<supertestAsPromised.Test>;
 
   namespace supertestAsPromised {
-    interface Test extends PromiseBluebird<Response>, supertest.Test {
-      toPromise(): PromiseBluebird<Response>;
+    interface Test extends Promise<Response>, supertest.Test {
+      toPromise(): PromiseBlurbird<Response>;
     }
+
     function agent(app?: any): SuperTest<Test>;
 
     interface Response extends supertest.Response {
@@ -28,4 +29,28 @@ declare module "supertest-as-promised" {
     }
   }
   export = supertestAsPromised
+
+  // from core-js.d.ts
+  /**
+   * Represents the completion of an asynchronous operation
+   */
+  interface Promise<T> {
+      /**
+      * Attaches callbacks for the resolution and/or rejection of the Promise.
+      * @param onfulfilled The callback to execute when the Promise is resolved.
+      * @param onrejected The callback to execute when the Promise is rejected.
+      * @returns A Promise for the completion of which ever callback is executed.
+      */
+      then<TResult>(onfulfilled?: (value: T) => TResult | PromiseLike<TResult>, onrejected?: (reason: any) => TResult | PromiseLike<TResult>): Promise<TResult>;
+      then<TResult>(onfulfilled?: (value: T) => TResult | PromiseLike<TResult>, onrejected?: (reason: any) => void): Promise<TResult>;
+
+      /**
+       * Attaches a callback for only the rejection of the Promise.
+       * @param onrejected The callback to execute when the Promise is rejected.
+       * @returns A Promise for the completion of the callback.
+       */
+      catch(onrejected?: (reason: any) => T | PromiseLike<T>): Promise<T>;
+      catch(onrejected?: (reason: any) => void): Promise<T>;
+  }
+
 }
