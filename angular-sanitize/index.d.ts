66bbeb3bc03744b0e6a270958fa15cc3fbab64b1
Mohamed Hegazy <mhegazy@microsoft.com>
2016-07-18 14:29:26 -0700

    Make angular-sanitize a module, and use module augmentation to update the angular module

--- angular-sanitize/index.d.ts
+++ angular-sanitize/index.d.ts
@@ -3,37 +3,36 @@
 // Definitions by: Diego Vilar <http://github.com/diegovilar>
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
 
+declare var _: string;
+export = _;
 
+import * as angular from 'angular';
 
-declare module "angular-sanitize" {
-    var _: string;
-    export = _;
-}
+declare module 'angular' {
+    ///////////////////////////////////////////////////////////////////////////////
+    // ngSanitize module (angular-sanitize.js)
+    ///////////////////////////////////////////////////////////////////////////////
+    namespace sanitize {
+        ///////////////////////////////////////////////////////////////////////////
+        // SanitizeService
+        // see http://docs.angularjs.org/api/ngSanitize.$sanitize
+        ///////////////////////////////////////////////////////////////////////////
+        interface ISanitizeService {
+            (html: string): string;
+        }
 
-///////////////////////////////////////////////////////////////////////////////
-// ngSanitize module (angular-sanitize.js)
-///////////////////////////////////////////////////////////////////////////////
-declare namespace angular.sanitize {
+        ///////////////////////////////////////////////////////////////////////////
+        // Filters included with the ngSanitize
+        // see https://github.com/angular/angular.js/tree/v1.2.0/src/ngSanitize/filter
+        ///////////////////////////////////////////////////////////////////////////
+        export namespace filter {
 
-    ///////////////////////////////////////////////////////////////////////////
-    // SanitizeService
-    // see http://docs.angularjs.org/api/ngSanitize.$sanitize
-    ///////////////////////////////////////////////////////////////////////////
-    interface ISanitizeService {
-        (html: string): string;
-    }
-
-    ///////////////////////////////////////////////////////////////////////////
-    // Filters included with the ngSanitize
-    // see https://github.com/angular/angular.js/tree/v1.2.0/src/ngSanitize/filter
-    ///////////////////////////////////////////////////////////////////////////
-    export module filter {
-
-        // Finds links in text input and turns them into html links.
-        // Supports http/https/ftp/mailto and plain email address links.
-        // see http://code.angularjs.org/1.2.0/docs/api/ngSanitize.filter:linky
-        interface ILinky {
-            (text: string, target?: string): string;
+            // Finds links in text input and turns them into html links.
+            // Supports http/https/ftp/mailto and plain email address links.
+            // see http://code.angularjs.org/1.2.0/docs/api/ngSanitize.filter:linky
+            interface ILinky {
+                (text: string, target?: string): string;
+            }
         }
     }
 }
