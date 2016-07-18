915f1d2bf3a61641c24dbf8960777f4cf1534f37
Piotr Witek <piotrek.witek@gmail.com>
2016-07-18 23:06:56 +0200

    reindented to 4 spaces

--- react-big-calendar/react-big-calendar.d.ts
+++ react-big-calendar/react-big-calendar.d.ts
@@ -6,49 +6,49 @@
 /// <reference path="../react/react.d.ts"/>
 
 declare module 'react-big-calendar' {
-  import * as React from 'react';
+    import * as React from 'react';
 
-  type stringOrDate = string | Date;
-  
-  interface BigCalendarProps extends React.Props<BigCalendar> {
-    date?: stringOrDate;
-    view?: string;
-    events?: Object[];
-    onNavigate?: Function;
-    onView?: Function;
-    onSelectSlot?: (slotInfo: { start: stringOrDate, end: stringOrDate, slots: Date[] | string[] }) => void;
-    onSelectEvent?: (event: Object) => void;
-    onSelecting?: (slotInfo: { start: stringOrDate, end: stringOrDate }) => boolean;
-    views?: Object;
-    toolbar?: boolean;
-    popup?: boolean;
-    popupOffset?: number | { x: number, y: number };
-    selectable?: boolean;
-    step?: number;
-    rtl?: boolean;
-    eventPropGetter?: (event: Object, start: stringOrDate, end: stringOrDate, isSelected: boolean) => void;
-    titleAccessor?: string;
-    allDayAccessor?: boolean;
-    startAccessor?: stringOrDate;
-    endAccessor?: stringOrDate;
-    min?: stringOrDate;
-    max?: stringOrDate;
-    scrollToTime?: stringOrDate;
-    formats?: Object;
-    components?: Object;
-    messages?: Object;
-  }
+    type stringOrDate = string | Date;
 
-  export class BigCalendar extends React.Component<BigCalendarProps, {}> {
-    /**
-     * Setup the localizer by providing the moment Object
-     */
-    static momentLocalizer(momentInstance: Object): void;
-    /**
-     * Setup the localizer by providing the globalize Object
-     */
-    static globalizeLocalizer(globalizeInstance: Object): void;
-  }
+    interface BigCalendarProps extends React.Props<BigCalendar> {
+        date?: stringOrDate;
+        view?: string;
+        events?: Object[];
+        onNavigate?: Function;
+        onView?: Function;
+        onSelectSlot?: (slotInfo: { start: stringOrDate, end: stringOrDate, slots: Date[] | string[] }) => void;
+        onSelectEvent?: (event: Object) => void;
+        onSelecting?: (slotInfo: { start: stringOrDate, end: stringOrDate }) => boolean;
+        views?: Object;
+        toolbar?: boolean;
+        popup?: boolean;
+        popupOffset?: number | { x: number, y: number };
+        selectable?: boolean;
+        step?: number;
+        rtl?: boolean;
+        eventPropGetter?: (event: Object, start: stringOrDate, end: stringOrDate, isSelected: boolean) => void;
+        titleAccessor?: string;
+        allDayAccessor?: boolean;
+        startAccessor?: stringOrDate;
+        endAccessor?: stringOrDate;
+        min?: stringOrDate;
+        max?: stringOrDate;
+        scrollToTime?: stringOrDate;
+        formats?: Object;
+        components?: Object;
+        messages?: Object;
+    }
 
-  export default BigCalendar;
+    export class BigCalendar extends React.Component<BigCalendarProps, {}> {
+        /**
+         * Setup the localizer by providing the moment Object
+         */
+        static momentLocalizer(momentInstance: Object): void;
+        /**
+         * Setup the localizer by providing the globalize Object
+         */
+        static globalizeLocalizer(globalizeInstance: Object): void;
+    }
+
+     export default BigCalendar;
 }
