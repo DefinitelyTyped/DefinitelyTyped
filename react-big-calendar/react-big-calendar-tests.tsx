d6e5c6fcb45fc86010bbaa756f58da27749885c3
Piotr Witek <piotrek.witek@gmail.com>
2016-07-18 23:03:20 +0200

    reindent to 4 spaces

--- react-big-calendar/react-big-calendar-tests.tsx
+++ react-big-calendar/react-big-calendar-tests.tsx
@@ -16,14 +16,14 @@ BigCalendar.momentLocalizer(moment);
 
 // Basic Example Test
 const BasicExample = React.createClass({
-  render() {
-    return (
-      <BigCalendar
-        {...this.props}
-        events={getEvents()}
-      />
-    );
-  }
+    render() {
+        return (
+            <BigCalendar
+            {...this.props}
+            events={getEvents()}
+            />
+        );
+    }
 });
 ReactDOM.render(<BasicExample />, document.body);
 const results = ReactDOMServer.renderToString(<BasicExample />);
@@ -33,74 +33,74 @@ console.log('Test Results -> BasicExample', results);
 
 // test fixtures
 function getEvents(): Object[] {
-  return [
+    return [
     {
-      'title': 'All Day Event',
-      'allDay': true,
-      'start': new Date(2015, 3, 0),
-      'end': new Date(2015, 3, 0)
+        'title': 'All Day Event',
+        'allDay': true,
+        'start': new Date(2015, 3, 0),
+        'end': new Date(2015, 3, 0)
     },
     {
-      'title': 'Long Event',
-      'start': new Date(2015, 3, 7),
-      'end': new Date(2015, 3, 10)
-    },
-
-    {
-      'title': 'DTS STARTS',
-      'start': new Date(2016, 2, 13, 0, 0, 0),
-      'end': new Date(2016, 2, 20, 0, 0, 0)
+        'title': 'Long Event',
+        'start': new Date(2015, 3, 7),
+        'end': new Date(2015, 3, 10)
     },
 
     {
-      'title': 'DTS ENDS',
-      'start': new Date(2016, 10, 6, 0, 0, 0),
-      'end': new Date(2016, 10, 13, 0, 0, 0)
+        'title': 'DTS STARTS',
+        'start': new Date(2016, 2, 13, 0, 0, 0),
+        'end': new Date(2016, 2, 20, 0, 0, 0)
     },
 
     {
-      'title': 'Some Event',
-      'start': new Date(2015, 3, 9, 0, 0, 0),
-      'end': new Date(2015, 3, 9, 0, 0, 0)
+        'title': 'DTS ENDS',
+        'start': new Date(2016, 10, 6, 0, 0, 0),
+        'end': new Date(2016, 10, 13, 0, 0, 0)
+    },
+
+    {
+        'title': 'Some Event',
+        'start': new Date(2015, 3, 9, 0, 0, 0),
+        'end': new Date(2015, 3, 9, 0, 0, 0)
     },
     {
-      'title': 'Conference',
-      'start': new Date(2015, 3, 11),
-      'end': new Date(2015, 3, 13),
-      desc: 'Big conference for important people'
+        'title': 'Conference',
+        'start': new Date(2015, 3, 11),
+        'end': new Date(2015, 3, 13),
+        desc: 'Big conference for important people'
     },
     {
-      'title': 'Meeting',
-      'start': new Date(2015, 3, 12, 10, 30, 0, 0),
-      'end': new Date(2015, 3, 12, 12, 30, 0, 0),
-      desc: 'Pre-meeting meeting, to prepare for the meeting'
+        'title': 'Meeting',
+        'start': new Date(2015, 3, 12, 10, 30, 0, 0),
+        'end': new Date(2015, 3, 12, 12, 30, 0, 0),
+        desc: 'Pre-meeting meeting, to prepare for the meeting'
     },
     {
-      'title': 'Lunch',
-      'start': new Date(2015, 3, 12, 12, 0, 0, 0),
-      'end': new Date(2015, 3, 12, 13, 0, 0, 0),
-      desc: 'Power lunch'
+        'title': 'Lunch',
+        'start': new Date(2015, 3, 12, 12, 0, 0, 0),
+        'end': new Date(2015, 3, 12, 13, 0, 0, 0),
+        desc: 'Power lunch'
     },
     {
-      'title': 'Meeting',
-      'start': new Date(2015, 3, 12, 14, 0, 0, 0),
-      'end': new Date(2015, 3, 12, 15, 0, 0, 0)
+        'title': 'Meeting',
+        'start': new Date(2015, 3, 12, 14, 0, 0, 0),
+        'end': new Date(2015, 3, 12, 15, 0, 0, 0)
     },
     {
-      'title': 'Happy Hour',
-      'start': new Date(2015, 3, 12, 17, 0, 0, 0),
-      'end': new Date(2015, 3, 12, 17, 30, 0, 0),
-      desc: 'Most important meal of the day'
+        'title': 'Happy Hour',
+        'start': new Date(2015, 3, 12, 17, 0, 0, 0),
+        'end': new Date(2015, 3, 12, 17, 30, 0, 0),
+        desc: 'Most important meal of the day'
     },
     {
-      'title': 'Dinner',
-      'start': new Date(2015, 3, 12, 20, 0, 0, 0),
-      'end': new Date(2015, 3, 12, 21, 0, 0, 0)
+        'title': 'Dinner',
+        'start': new Date(2015, 3, 12, 20, 0, 0, 0),
+        'end': new Date(2015, 3, 12, 21, 0, 0, 0)
     },
     {
-      'title': 'Birthday Party',
-      'start': new Date(2015, 3, 13, 7, 0, 0),
-      'end': new Date(2015, 3, 13, 10, 30, 0)
+        'title': 'Birthday Party',
+        'start': new Date(2015, 3, 13, 7, 0, 0),
+        'end': new Date(2015, 3, 13, 10, 30, 0)
     }
-  ];
+    ];
 };
