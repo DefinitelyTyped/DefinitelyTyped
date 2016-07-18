3d51f868334eda9a07806309692378b33349d1f3
Thanh H <thanhhuynh@yahoo.com>
2016-07-18 14:59:13 -0400

    Update definition of ITranslationTable to support namespace (#2433)

--- angular-translate/angular-translate.d.ts
+++ angular-translate/angular-translate.d.ts
@@ -13,7 +13,7 @@ declare module "angular-translate" {
 declare namespace angular.translate {
 
     interface ITranslationTable {
-        [key: string]: any;
+        [key: string]: string | ITranslationTable;
     }
 
     interface ILanguageKeyAlias {
