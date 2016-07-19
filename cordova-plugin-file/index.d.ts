b7031155bfd6a5c4eacd002c09855227f5278b2e
Bowden Kelly <wilkelly@microsoft.com>
2016-07-19 15:54:02 -0700

    Fixed headers

--- cordova-plugin-file/index.d.ts
+++ cordova-plugin-file/index.d.ts
@@ -1,9 +1,9 @@
-// Type definitions for Apache Cordova File System plugin.
+// Type definitions for Apache Cordova File System plugin
 // Project: https://github.com/apache/cordova-plugin-file
-// Definitions by: Microsoft Open Technologies, Inc. <http://msopentech.com>
+// Definitions by: Microsoft Open Technologies Inc <http://msopentech.com>
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
 //
-// Copyright (c) Microsoft Open Technologies, Inc.
+// Copyright (c) Microsoft Open Technologies Inc
 // Licensed under the MIT license.
 
 interface Window {
--- cordova-plugin-inappbrowser/index.d.ts
+++ cordova-plugin-inappbrowser/index.d.ts
@@ -1,9 +1,9 @@
-// Type definitions for Apache Cordova InAppBrowser plugin.
+// Type definitions for Apache Cordova InAppBrowser plugin
 // Project: https://github.com/apache/cordova-plugin-inappbrowser
-// Definitions by: Microsoft Open Technologies, Inc. <http://msopentech.com>
+// Definitions by: Microsoft Open Technologies Inc <http://msopentech.com>
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
 // 
-// Copyright (c) Microsoft Open Technologies, Inc.
+// Copyright (c) Microsoft Open Technologies Inc
 // Licensed under the MIT license.
 
 interface Window {
