8a4c284a48b5c8f6decdf84f47220cdd85e42e98
NN <nn1436401@gmail.com>
2016-07-20 15:43:20 +0300

    Type WebRequestBody.formData

--- chrome/chrome.d.ts
+++ chrome/chrome.d.ts
@@ -7219,7 +7219,7 @@ declare namespace chrome.webRequest {
 		 * Optional.
 		 * If the request method is POST and the body is a sequence of key-value pairs encoded in UTF8, encoded as either multipart/form-data, or application/x-www-form-urlencoded, this dictionary is present and for each key contains the list of all values for that key. If the data is of another media type, or if it is malformed, the dictionary is not present. An example value of this dictionary is {'key': ['value1', 'value2']}.
 		 */
-		formData?: Object;
+		formData?: { [key: string]: string[] };
 		/**
 		 * Optional.
 		 * If the request method is PUT or POST, and the body is not already parsed in formData, then the unparsed request body elements are contained in this array.
