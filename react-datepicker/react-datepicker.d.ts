505e82b8d100d406ab906d1e19ce9ec7826a480a
François Guillot <francois-guillot@wanadoo.fr>
2016-07-20 14:37:30 +0200

    Updated version

--- react-datepicker/react-datepicker.d.ts
+++ react-datepicker/react-datepicker.d.ts
@@ -1,4 +1,4 @@
-// Type definitions for react-datepicker v0.27.0
+// Type definitions for react-datepicker v0.28.1
 // Project: https://github.com/Hacker0x01/react-datepicker
 // Definitions by: Rajab Shakirov <https://github.com/radziksh>, Andrey Balokha <https://github.com/andrewBalekha>
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
