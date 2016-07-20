a91a47c47f2a793f1e0bf54ce9559ac36407a238
Vincent Rouille <vincent@speedy37.fr>
2016-07-20 02:05:08 +0200

    Fix nodejs StringDecoder definition (#10100)

--- node/node-tests.ts
+++ node/node-tests.ts
@@ -19,6 +19,8 @@ import * as childProcess from "child_process";
 import * as cluster from "cluster";
 import * as os from "os";
 import * as vm from "vm";
+import * as string_decoder from "string_decoder";
+
 // Specifically test buffer module regression.
 import {Buffer as ImportedBuffer, SlowBuffer as ImportedSlowBuffer} from "buffer";
 
@@ -721,6 +723,21 @@ namespace readline_tests {
 
         readline.clearScreenDown(stream);
     }
+}
+
+////////////////////////////////////////////////////
+/// string_decoder tests : https://nodejs.org/api/string_decoder.html
+////////////////////////////////////////////////////
+
+namespace string_decoder_tests {
+    const StringDecoder = string_decoder.StringDecoder;
+    const buffer = new Buffer('test');
+    const decoder1 = new StringDecoder();
+    const decoder2 = new StringDecoder('utf8');
+    const part1: string = decoder1.write(new Buffer('test'));
+    const end1: string = decoder1.end();
+    const part2: string = decoder2.write(new Buffer('test'));
+    const end2: string = decoder1.end(new Buffer('test'));
 }
 
 //////////////////////////////////////////////////////////////////////
