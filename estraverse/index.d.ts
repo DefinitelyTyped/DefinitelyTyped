524074cd6a094c9e176830e6a95a477df6459bfe
Timofey Kachalov <sanex3339@users.noreply.github.com>
2016-07-20 22:36:09 +0300

    remove ambient module declaration

--- estraverse/index.d.ts
+++ estraverse/index.d.ts
@@ -3,20 +3,18 @@
 // Definitions by: Sanex3339 <https://github.com/sanex3339>
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
 
-declare module 'estraverse' {
-    export interface Visitor {
-        enter?: (node: any, parentNode: any) => any;
-        leave?: (node: any, parentNode: any) => any;
+ export interface Visitor {
+    enter?: (node: any, parentNode: any) => any;
+    leave?: (node: any, parentNode: any) => any;
 
-        fallback?: string;
+    fallback?: string;
 
-        keys?: {};
-    }
-
-    export enum VisitorOption {
-        Skip, Break, Remove
-    }
-
-    export function traverse (ast: any, visitor: Visitor): any;
-    export function replace (ast: any, visitor: Visitor): any;
+    keys?: {};
 }
+
+export enum VisitorOption {
+    Skip, Break, Remove
+}
+
+export function traverse (ast: any, visitor: Visitor): any;
+export function replace (ast: any, visitor: Visitor): any;
