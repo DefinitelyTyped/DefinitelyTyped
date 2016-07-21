cc37534ea0950ac6cc4a4745c45d3f72f0578b4f
Gabriel JUCHAULT <gabriel.juchault@gmail.com>
2016-07-21 15:25:12 +0200

    Remove header tags from tests

--- countdown/countdown-tests.ts
+++ countdown/countdown-tests.ts
@@ -1,8 +1,3 @@
-// Type definitions for countdown.js
-// Project: http://countdownjs.org/
-// Definitions by: Gabriel Juchault https://github.com/gjuchault
-// Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
-
 /// <reference path="countdown.d.ts" />
 
 import { countdown, Timespan, CountdownStatic, Format } from 'countdown';
