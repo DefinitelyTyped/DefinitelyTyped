fdd86335ae4fe2c15f0a242097a1a9756f744b28
Afshin Darian <git@darian.af>
2016-07-24 07:51:55 +0100

    Rewrite sanitize-html typings. (#10139)
    * Rewrite sanitize-html typings from scratch, reflect new version, full API.
    
    * Remove superfluous exports, concat author instead of replace.

--- sanitize-html/sanitize-html.d.ts
+++ sanitize-html/sanitize-html.d.ts
@@ -1,27 +1,57 @@
-﻿// Type definitions for sanitize-html 1.6.0
+﻿// Type definitions for sanitize-html 1.12.0
 // Project: https://github.com/punkave/sanitize-html
-// Definitions by: Rogier Schouten <https://github.com/rogierschouten>
+// Definitions by: Rogier Schouten <https://github.com/rogierschouten>, Afshin Darian <https://github.com/afshin>
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
 
 
-declare module "sanitize-html" {
+declare function sanitize(dirty: string, options?: sanitize.IOptions): string;
 
-	function sanitizeHtml(s: string, opts?: {
-		allowedTags?: string[];
-		allowedSchemes?: string[];
-		allowedAttributes?: { [index: string]: string[] };
-		allowedClasses?: { [index: string]: string[] };
-		transformTags?: { [index: string]: any };
-		exclusiveFilter?: {
-			[index: string]: (frame: {
-				tag: string;
-				attribs: { [index: string]: string };
-				text: string;
-				tagPosition: number;
-			}) => boolean
-		};
 
-	}): string;
+declare namespace sanitize {
+  type Attributes = { [attr: string]: string };
 
-	export = sanitizeHtml;
+
+  type Tag = { tagName: string; attributes: Attributes; };
+
+
+  type Transformer = (tagName: string, attributes: Attributes) => Tag;
+
+
+  interface IDefaults {
+    allowedAttributes: { [index: string]: string[] };
+    allowedSchemes: string[];
+    allowedSchemesByTag: { [index: string]: string[] };
+    allowedTags: string[];
+    selfClosing: string[];
+  }
+
+
+  interface IFrame {
+    tag: string;
+    attributes: { [index: string]: string };
+    text: string;
+    tagPosition: number;
+  }
+
+
+  interface IOptions {
+    allowedAttributes?: { [index: string]: string[] };
+    allowedClasses?: { [index: string]: string[] };
+    allowedSchemes?: string[];
+    allowedTags?: string[];
+    exclusiveFilter?: (frame: IFrame) => boolean;
+    selfClosing?: string[];
+    transformTags?: { [tagName: string]: string | Transformer };
+  }
+
+
+  var defaults: IDefaults;
+
+
+  function simpleTransform(tagName: string, attributes: Attributes, merge?: boolean): Transformer;
+}
+
+
+declare module 'sanitize-html' {
+  export = sanitize;
 }
