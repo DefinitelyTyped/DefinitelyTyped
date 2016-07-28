727aec27adf0c8e665093188e130693e925d5fd8
Nick Zelei <nick.zelei@garmin.com>
2016-07-28 11:40:33 -0500

    removed interface and moved into module
    removed interface and moved into module

--- utf8/utf8.d.ts
+++ utf8/utf8.d.ts
@@ -3,28 +3,22 @@
 // Definitions by: Nick Zelei <https://github.com/zelein/>
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
 
-interface Utf8 {
+declare module "utf8" {
     /**
      * A string representing the semantic version number.
      */
-    version: string;
+    const version: string;
     /**
      * Encodes any given JavaScript string (string) as UTF-8, and returns the UTF-8-encoded version of the string.
      * It throws an error if the input string contains a non-scalar value, i.e. a lone surrogate.
      * @param {string} inputString
      */
-    encode(inputString: string): string;
+    function encode(inputString: string): string;
 
     /**
      * Decodes any given UTF-8-encoded string (byteString) as UTF-8, and returns the UTF-8-decoded version of the string.
      * It throws an error when malformed UTF-8 is detected.
      * @param {string} byteString
      */
-    decode(byteString: string): string;
+    function decode(byteString: string): string;
 }
-
-declare const Utf8: Utf8;
-declare module "utf8" {
-    export = Utf8;
-}
-
