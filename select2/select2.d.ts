58b0deb938caada8ac8f6d9751df5f777f6a4149
Stephen Penney <StephenPenney@users.noreply.github.com>
2016-07-28 15:43:23 +0100

    Update select2 to have selectOnClose option (#10326)
    Option documented at https://select2.github.io/options.html#can-i-select-the-highlighted-result-when-the-dropdown-is-closed

--- select2/select2.d.ts
+++ select2/select2.d.ts
@@ -79,6 +79,7 @@ interface Select2Options {
     templateSelection?: (object: Select2SelectionObject) => any;
     templateResult?: (object: Select2SelectionObject) => any;
     language?: string | string[] | {};
+    selectOnClose?: boolean;
 }
 
 interface Select2JQueryEventObject extends JQueryEventObject {
