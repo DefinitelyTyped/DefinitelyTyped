1ad718c4c95735d849ad22d4a9bd9c76b4845d07
Mykhailo Stadnyk <mikhus@gmail.com>
2016-07-30 00:07:09 +0300

    Added gulp-help-doc definitions

---
+++ gulp-help-doc/gulp-help-doc-tests.ts
@@ -0,0 +1,24 @@
+/// <reference path="gulp-help-doc.d.ts" />
+/// <reference path="../node/node.d.ts" />
+/// <reference path="../gulp/gulp.d.ts" />
+
+import gulp = require('gulp');
+import usage = require('gulp-help-doc');
+
+/**
+ * Demo task
+ *
+ * @task {demo}
+ * @arg {env} environment
+ */
+gulp.task('demo', function() {});
+
+var logger = {
+    output: '',
+    log: msg => logger.output += msg + '\n'
+};
+
+usage(gulp, {
+    logger: logger,
+    gulpfile: __filename
+}).then(() => console.log(logger.output));
