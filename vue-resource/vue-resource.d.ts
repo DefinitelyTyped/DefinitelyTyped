22b31cce436d6df161f2fc5f292d48a5c5ebacd8
itsFrank <frankobrien11@yahoo.ca>
2016-07-29 07:59:20 +0200

    fixed version number

--- vue-resource/vue-resource.d.ts
+++ vue-resource/vue-resource.d.ts
@@ -1,4 +1,4 @@
-// Type definitions for vue-resoure 0.7.0
+// Type definitions for vue-resoure 0.9.3
 // Project: https://github.com/vuejs/vue-resource
 // Definitions by: kaorun343 <https://github.com/kaorun343>
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
