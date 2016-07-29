edc6134322fecce503f92327edfec8858cfe800f
Johannes Schickling <schickling.j@gmail.com>
2016-07-29 10:52:21 +0200

    Added basic type definitions for react-relay

---
+++ react-relay/react-relay-test.tsx
@@ -0,0 +1,7 @@
+/// <reference path="../react/react.d.ts" />
+/// <reference path="./react-relay.d.ts" />
+
+import * as React from "react"
+import * as Relay from "react-relay"
+
+// TODO write test cases
