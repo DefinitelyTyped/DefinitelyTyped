c02b783e4b722aa7cc7639322cd013328258f37e
Pouya Kary <k@karyfoundation.org>
2016-08-01 21:15:43 +0430

    fixed the problem with URL

--- window-size/window-size.d.ts
+++ window-size/window-size.d.ts
@@ -1,6 +1,6 @@
 // Type definitions for window-size 0.2.0
 // Project: https://github.com/jonschlinkert/window-size
-// Definitions by: Pouya Kary <k@karyfoundation.org>
+// Definitions by: Pouya Kary <https://github.com/pmkary>
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
 
 declare module "window-size" {
