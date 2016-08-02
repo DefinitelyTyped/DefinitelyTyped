869be81f3a9f6bc5eabb82b4e0b8ecd3a80dafea
Carson Full <carsonfull@gmail.com>
2016-08-02 15:37:36 -0500

    Added parse() and format() to Field

--- redux-form/index.d.ts
+++ redux-form/index.d.ts
@@ -457,6 +457,13 @@ interface BaseFieldProps {
   withRef?: boolean;
 
   /**
+   * Formats the value from the Redux store to be displayed in the field input.
+   * Common use cases are to format Numbers into currencies or Dates into a
+   * localized date format.
+   */
+  format?: Formatter;
+
+  /**
    * A function to convert whatever value the user has entered into the value that you
    * want stored in the Redux store for the field.
    *
@@ -466,6 +473,13 @@ interface BaseFieldProps {
   normalize?: Normalizer;
 
   props?: Object;
+
+  /**
+   * Parses the value given from the field input component to the type that you want
+   * stored in the Redux store. Common use cases are to parse currencies into Numbers into
+   * currencies or localized date formats into Dates.
+   */
+  parse?: Parser;
 }
 
 interface Normalizer {
@@ -478,6 +492,14 @@ interface Normalizer {
    *                          This will be an Immutable Map if you are using Immutable JS.
    */
   (value: FieldValue, previousValue?: FieldValue, allValues?: Object, previousAllValues?: Object): FieldValue;
+}
+
+interface Formatter {
+  (value: FieldValue): FieldValue;
+}
+
+interface Parser {
+  (value: FieldValue): FieldValue;
 }
 
 /**
