a6918a72a07936d1cfd85d2dcb9a6f3527dd527c
Mine Yalcinalp Starks <mineyalc@microsoft.com>
2016-08-03 18:17:39 -0700

    Fix bad merge in credential

--- credential/index.d.ts
+++ credential/index.d.ts
@@ -10,25 +10,17 @@ declare module 'credential' {
         hashMethod: string;
     }
 
-    interface hashObject {
-        hash: string;
-        salt: string;
-        keyLength: number;
-        hashMethod: string;
-        iterations: number;
-    }
+interface hashObject {
+    hash: string;
+    salt: string;
+    keyLength: number;
+    hashMethod: string;
+    iterations: number;
+}
 
-    type HashCallback = (err: Error, hash: hashObject) => void;
-    type VerifyCallback = (err: Error, isValid: boolean) => void;
+type HashCallback = (err: Error, hash: hashObject) => void;
+type VerifyCallback = (err: Error, isValid: boolean) => void;
 
-    function credential(defaultOptions?: defaultOptions): {
-        hash(password: string, callback: HashCallback): void;
-        hash(password: string): Promise<hashObject>;
-        // iterations(work: number, base): number;
-        verify(hash: hashObject | string, password: string, callback: VerifyCallback): void;
-        verify(hash: hashObject | string, password: string): Promise<boolean>;
-        expired(hash: string, days: number): boolean;
-    }
 
     namespace credential { }
 
