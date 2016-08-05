274df1c923d5cef996a7da7b184c10060773d471
Nicholas Iannone <nickiannone@gmail.com>
2016-08-05 11:26:34 -0500

    Added initial definitions and tests.

---
+++ winston-dynamodb/winston-dynamodb.d.ts
@@ -0,0 +1,37 @@
+// Type definitions for winston-dynamodb
+// Project: https://github.com/inspiredjw/winston-dynamodb
+// Definitions by: nickiannone <http://github.com/nickiannone>
+// Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
+
+/// <reference path="../winston/winston.d.ts" />
+
+import * as winston from 'winston';
+export interface DynamoDBTransportOptions {
+    useEnvironment?: boolean;
+    accessKeyId?: string;
+    secretAccessKey?: string;
+    region?: string;
+    tableName: string;
+    level: string;
+    dynamoDoc?: boolean;
+}
+export interface DynamoDBTransportInstance extends winston.TransportInstance {
+    new (options?: DynamoDBTransportOptions): DynamoDBTransportInstance;
+}
+export declare class DynamoDB extends winston.Transport implements winston.TransportInstance {
+    regions: string[];
+    name: string;
+    level: string;
+    db: any;
+    AWS: any;
+    region: string;
+    tableName: string;
+    dynamoDoc: any;
+    constructor(options?: DynamoDBTransportOptions);
+    log(level: any, msg: any, meta: any, callback: any): any;
+}
+declare module "winston" {
+    interface Transports {
+        DynamoDB: DynamoDB;
+    }
+}
