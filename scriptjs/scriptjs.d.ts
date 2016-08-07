0099b8b816f3831faa1f01f7011a3a116e1f1acd
Steve Lam <stevyo99@yahoo.ca>
2016-08-07 16:55:35 -0700

    added meta data

--- scriptjs/scriptjs.d.ts
+++ scriptjs/scriptjs.d.ts
@@ -1,3 +1,8 @@
+// Type definitions for scriptjs
+// Project: https://github.com/ded/script.js
+// Definitions by: ssttevee
+// Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
+
 interface $script {
   (paths:string | string[], idOrDone:string | (() => void), optDone?:() => void): $script;
   get(path:string, fn:() => void): void;
