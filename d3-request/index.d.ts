ee27d51c2459e2c125520a90b0276b6af2254c10
Tom Wanzek <tomwanzek@gmail.com>
2016-08-09 19:52:50 -0400

    d3-request * Remove `?` from callback signatures as not required.

--- d3-request/index.d.ts
+++ d3-request/index.d.ts
@@ -57,12 +57,12 @@ export interface Request {
 }
 
 export interface DsvRequest extends Request {
-    row<ParsedRow extends DSVRowAny>(value: (rawRow: DSVRowString, index?: number, columns?: Array<string>) => ParsedRow): DsvRequest;
+    row<ParsedRow extends DSVRowAny>(value: (rawRow: DSVRowString, index: number, columns: Array<string>) => ParsedRow): DsvRequest;
 }
 
 export function csv(url: string): DsvRequest;
 export function csv(url: string, callback: (this: DsvRequest, error: any, d: DSVParsedArray<DSVRowString>) => void): DsvRequest;
-export function csv<ParsedRow extends DSVRowAny>(url: string, row: (rawRow: DSVRowString, index?: number, columns?: Array<string>) => ParsedRow, callback: (this: DsvRequest, error: any, d: DSVParsedArray<ParsedRow>) => void): DsvRequest;
+export function csv<ParsedRow extends DSVRowAny>(url: string, row: (rawRow: DSVRowString, index: number, columns: Array<string>) => ParsedRow, callback: (this: DsvRequest, error: any, d: DSVParsedArray<ParsedRow>) => void): DsvRequest;
 
 export function html(url: string): Request;
 export function html(url: string, callback: (this: Request, error: any, d: DocumentFragment) => void): Request;
@@ -78,7 +78,7 @@ export function text(url: string, callback: (this: Request, error: any, d: strin
 
 export function tsv(url: string): DsvRequest;
 export function tsv(url: string, callback: (this: DsvRequest, error: any, d: DSVParsedArray<DSVRowString>) => void): DsvRequest;
-export function tsv<ParsedRow extends DSVRowAny>(url: string, row: (rawRow: DSVRowString, index?: number, columns?: Array<string>) => ParsedRow, callback: (this: DsvRequest, error: any, d: DSVParsedArray<ParsedRow>) => void): DsvRequest;
+export function tsv<ParsedRow extends DSVRowAny>(url: string, row: (rawRow: DSVRowString, index: number, columns: Array<string>) => ParsedRow, callback: (this: DsvRequest, error: any, d: DSVParsedArray<ParsedRow>) => void): DsvRequest;
 
 export function xml(url: string): Request;
 export function xml(url: string, callback: (this: Request, error: any, d: any) => void): Request;
