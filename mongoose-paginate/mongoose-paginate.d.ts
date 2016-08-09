b7e534cb957bfe5fb6ff6d2ab02903ffe3e024da
linusbrolin <linus.b@gmail.com>
2016-08-09 13:59:13 +0200

    Added more information to header

--- mongoose-paginate/mongoose-paginate.d.ts
+++ mongoose-paginate/mongoose-paginate.d.ts
@@ -1,5 +1,6 @@
 // Type definitions for mongoose-paginate 5.0.0
 // Project: https://github.com/edwardhotchkiss/mongoose-paginate
+// Definitions by: Linus Brolin <https://github.com/linusbrolin/>, simonxca <https://github.com/simonxca/>
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
 
 /// <reference path="../mongoose/mongoose.d.ts" />
