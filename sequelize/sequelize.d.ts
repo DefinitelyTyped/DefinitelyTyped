e08f12080e898ab19e86675e61fb12bc404693cb
Leonard Hecker <leonard@hecker.io>
2016-08-11 13:13:12 +0200

    Fixed references to Bluebird 2.0

--- sequelize/sequelize.d.ts
+++ sequelize/sequelize.d.ts
@@ -6,7 +6,7 @@
 // Based on original work by: samuelneff <https://github.com/samuelneff/sequelize-auto-ts/blob/master/lib/sequelize.d.ts>
 
 /// <reference path='../lodash/lodash.d.ts' />
-/// <reference path="../bluebird/bluebird.d.ts" />
+/// <reference path="../bluebird/bluebird-2.0.d.ts" />
 /// <reference path="../validator/validator.d.ts" />
 
 declare module "sequelize" {
