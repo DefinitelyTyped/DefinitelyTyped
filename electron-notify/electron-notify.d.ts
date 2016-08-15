43dc204b773bf0df209a8d800fec3001a32c2763
Daniel Pereira <dpereira@lifesize.com>
2016-08-15 15:57:19 -0500

    Added INotification and IConfiguration to electron notify

--- electron-notify/electron-notify.d.ts
+++ electron-notify/electron-notify.d.ts
@@ -6,22 +6,54 @@
 /// <reference path="../github-electron/github-electron.d.ts" />
 
 declare namespace ElectronNotify {
+
+  interface INotification {
+    title: string,
+    text?: string,
+    image?: string,
+    url?: string,
+    sound?: string,
+    onClickFunc?: (event: string, id: number, closeNotification) => void,
+    onShowFunc?: (event: string, id: number, closeNotification) => void,
+    onCloseFunc?: (event: string, id: number) => void
+  }
+
+  interface IConfiguration {
+    width?: number,
+    height?: number,
+    padding?: number,
+    borderRadius?: number,
+    displayTime?: number,
+    animationSteps?: number,
+    animationStepMs?: number,
+    animateInParallel?: boolean,
+    appIcon?: string,
+    pathToModule?: string,
+    logging?: boolean,
+    defaultWindow?: Electron.BrowserWindowOptions,
+    defaultStyleContainer?: any,
+    defaultStyleAppIcon?: any,
+    defaultStyleImage?: any,
+    defaultStyleClose?: any,
+    defaultStyleText?: any
+  }
+
 }
 
 /** Nice and simple notifications for electron apps */
 declare module 'electron-notify' {
 
   /** Change some config options. Can be run multiple times, also between notify()-calls to change electron-notifys behaviour. */
-  export function setConfig(configObj);
+  export function setConfig(customConfig: ElectronNotify.IConfiguration);
 
   /** Displays new notification. */
-  export function notify(notificationObj);
+  export function notify(notification: ElectronNotify.INotification);
 
   /** Clears the animation queue and closes all windows opened by electron-notify. Call this to clean up before quiting your app. */
   export function closeAll();
 
-  export function setTemplatePath(path);
+  export function getTemplatePath(): string;
 
-  /** Returns the maximum amount of notifications that fit onto the users screen. */
-  export function calcMaxVisibleNotification(): number;
+  export function setTemplatePath(path: string);
+
 }
