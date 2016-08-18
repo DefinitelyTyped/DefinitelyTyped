89f2476be54fed6552298cb0bceaa776424cc76a
Ivo Stratev <ivo.stratev.tues@gmail.com>
2016-08-18 21:54:51 +0300

    Update simple-assign.d.ts

--- simple-assign/simple-assign.d.ts
+++ simple-assign/simple-assign.d.ts
@@ -4,6 +4,11 @@
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
 
 declare module "simple-assign" {
+  function simpleAssign<T, U>(target: T, source: U): T & U;
+  function simpleAssign<T, U, V>(target: T, source1: U, source2: V): T & U & V;
+  function simpleAssign<T, U, V, W>(target: T, source1: U, source2: V, source3: W): T & U & V & W;
+  function simpleAssign<T, U, V, W, Q>(target: T, source1: U, source2: V, source3: W, source4: Q): T & U & V & W & Q;
+  function simpleAssign<T, U, V, W, Q, R>(target: T, source1: U, source2: V, source3: W, source4: Q, source5: T): T & U & V & W & Q & R;
   function simpleAssign(target: any, ...sources: any[]): any;
   export = simpleAssign;
 }
