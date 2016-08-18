df1c6dde08e68b28954b9c9e4e1c9a06d0531f7d
Ivo Stratev <ivo.stratev.tues@gmail.com>
2016-08-18 15:42:15 +0300

    Create simple-assign.d.ts

---
+++ simple-assign/simple-assign.d.ts
@@ -0,0 +1,8 @@
+// Type definitions for simple-assign 0.1.0
+// Project: https://github.com/sindresorhus/object-assign
+// Definitions by: Ivo Stratev <https://github.com/NoHomey>
+// Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
+
+declare module "simple-assign" {
+  export defualt function(target: any, ...sources: any[]): any;
+}
