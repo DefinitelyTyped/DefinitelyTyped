31137d2be8bed312184a4135c7cd12d1df3b7f75
Ivo Stratev <ivo.stratev.tues@gmail.com>
2016-08-18 15:37:05 +0300

    Update react-redux-2.1.2-tests.tsx

--- react-redux/react-redux-2.1.2-tests.tsx
+++ react-redux/react-redux-2.1.2-tests.tsx
@@ -12,7 +12,7 @@ import * as Router from 'react-router';
 import { Route, RouterState } from 'react-router';
 import { Store, Dispatch, bindActionCreators } from 'redux';
 import { connect, Provider } from 'react-redux';
-import * as objectAssign from 'object-assign';
+import objectAssign from 'object-assign';
 
 //
 // Quick Start
