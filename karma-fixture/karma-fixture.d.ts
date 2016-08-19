68255e581cda159766138cfe912b5840dd048570
Ezekiel Victor <zekevictor@gmail.com>
2016-08-19 12:50:03 -0700

    Update karma-fixture.d.ts

--- karma-fixture/karma-fixture.d.ts
+++ karma-fixture/karma-fixture.d.ts
@@ -12,7 +12,7 @@ declare module fixture {
   function load(file1: string, file2: string, append?: boolean): any;
   function load(file1: string, file2: string, file3: string, append?: boolean): any;
   function load(file1: string, file2: string, file3: string, file4: string, append?: boolean): any;
-  function load(file1: string, file2: string, file3: string, file4: string, file5: string, append?: boolean): any
+  function load(file1: string, file2: string, file3: string, file4: string, file5: string, append?: boolean): any;
 
   function set(...htmlStrs: string[]): HTMLElement|HTMLElement[];
   function set(htmlStr1: string, append?: boolean): HTMLElement|HTMLElement[];
