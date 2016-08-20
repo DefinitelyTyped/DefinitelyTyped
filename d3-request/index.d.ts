5a86b3f5066e10c407afc7b5f3b8dee533f34dff
Tom Wanzek <tomwanzek@gmail.com>
2016-08-20 12:40:43 -0400

    Updated version header comment in d3-request.

--- d3-request/index.d.ts
+++ d3-request/index.d.ts
@@ -1,4 +1,4 @@
-// Type definitions for D3JS d3-request module 1.0.1
+// Type definitions for D3JS d3-request module v1.0.2
 // Project: https://github.com/d3/d3-request/
 // Definitions by: Alex Ford <https://github.com/gustavderdrache>, Boris Yankov <https://github.com/borisyankov>, Hugues Stefanski <https://github.com/Ledragon>, Tom Wanzek <https://github.com/tomwanzek>
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
