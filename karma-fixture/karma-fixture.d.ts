455c4ca7945d912dadc0d1ab22a94a1d36a2ca26
Ezekiel Victor <evictor@gravity.com>
2016-08-26 16:30:28 -0700

    update karma fixture typings

--- karma-fixture/karma-fixture.d.ts
+++ karma-fixture/karma-fixture.d.ts
@@ -3,25 +3,25 @@
 // Definitions by: Ezekiel Victor <https://github.com/evictor/>
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
 
-declare module fixture {
-  var el: HTMLElement;
-  var json: any[];
+declare namespace fixture {
+  export var el: HTMLElement;
+  export var json: any[];
 
-  function load(...files: string[]): HTMLElement[]|Object|any[];
-  function load(file1: string, append?: boolean): HTMLElement[]|Object|any[];
-  function load(file1: string, file2: string, append?: boolean): HTMLElement[]|Object|any[];
-  function load(file1: string, file2: string, file3: string, append?: boolean): HTMLElement[]|Object|any[];
-  function load(file1: string, file2: string, file3: string, file4: string, append?: boolean): HTMLElement[]|Object|any[];
-  function load(file1: string, file2: string, file3: string, file4: string, file5: string, append?: boolean): HTMLElement[]|Object|any[];
+  export function load(...files: string[]): any;
+  export function load(file1: string, append?: boolean): any;
+  export function load(file1: string, file2: string, append?: boolean): any;
+  export function load(file1: string, file2: string, file3: string, append?: boolean): any;
+  export function load(file1: string, file2: string, file3: string, file4: string, append?: boolean): any;
+  export function load(file1: string, file2: string, file3: string, file4: string, file5: string, append?: boolean): any;
 
-  function set(...htmlStrs: string[]): HTMLElement|HTMLElement[];
-  function set(htmlStr1: string, append?: boolean): HTMLElement|HTMLElement[];
-  function set(htmlStr1: string, htmlStr2: string, append?: boolean): HTMLElement|HTMLElement[];
-  function set(htmlStr1: string, htmlStr2: string, htmlStr3: string, append?: boolean): HTMLElement|HTMLElement[];
-  function set(htmlStr1: string, htmlStr2: string, htmlStr3: string, htmlStr4: string, append?: boolean): HTMLElement|HTMLElement[];
-  function set(htmlStr1: string, htmlStr2: string, htmlStr3: string, htmlStr4: string, htmlStr5: string, append?: boolean): HTMLElement|HTMLElement[];
+  export function set(...htmlStrs: string[]): HTMLElement|HTMLElement[];
+  export function set(htmlStr1: string, append?: boolean): HTMLElement|HTMLElement[];
+  export function set(htmlStr1: string, htmlStr2: string, append?: boolean): HTMLElement|HTMLElement[];
+  export function set(htmlStr1: string, htmlStr2: string, htmlStr3: string, append?: boolean): HTMLElement|HTMLElement[];
+  export function set(htmlStr1: string, htmlStr2: string, htmlStr3: string, htmlStr4: string, append?: boolean): HTMLElement|HTMLElement[];
+  export function set(htmlStr1: string, htmlStr2: string, htmlStr3: string, htmlStr4: string, htmlStr5: string, append?: boolean): HTMLElement|HTMLElement[];
 
-  function cleanup(): void;
+  export function cleanup(): void;
 
-  function setBase(fixtureBasePath: string): void;
+  export function setBase(fixtureBasePath: string): void;
 }
