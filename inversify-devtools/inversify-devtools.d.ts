a18cde0d1052144c5ab58c3881b60efd56b683d9
Remo H. Jansen <remo.jansen@wolksoftware.com>
2016-08-28 23:07:10 +0100

    Update inversify-devtools.d.ts

--- inversify-devtools/inversify-devtools.d.ts
+++ inversify-devtools/inversify-devtools.d.ts
@@ -1,6 +1,7 @@
 // Type definitions for inversify-devtools 1.0.0
 // Project: https://github.com/inversify/inversify-devtools
 // Definitions by: inversify <https://github.com/inversify/>
+// Definitions: https://github.com/borisyankov/DefinitelyTyped
 
 /// <reference path="../inversify/inversify.d.ts" />
 
