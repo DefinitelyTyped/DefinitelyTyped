5f7fec9b926f7c963267ff5177a6e3ebee48fc9b
Brice Theurillat <brice.theurillat@idleweiss.ch>
2016-08-29 13:28:16 +0200

    Add missing file header

--- pathfinding/pathfinding.d.ts
+++ pathfinding/pathfinding.d.ts
@@ -1,3 +1,8 @@
+// Type definitions for pathfinding
+// Project: https://github.com/qiao/PathFinding.js
+// Definitions by: BNedry <https://github.com/BNedry>
+// Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
+
 declare module "pathfinding" {
     namespace Pathfinding {
         export namespace Heuristic {
