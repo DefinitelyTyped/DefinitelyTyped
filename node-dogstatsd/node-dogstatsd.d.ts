ac228271b42b0541a598b0602375ed662cd6b879
Chris Bobo <chrisbobo@gmail.com>
2016-08-29 08:28:32 -0700

    Correct project url

--- node-dogstatsd/node-dogstatsd.d.ts
+++ node-dogstatsd/node-dogstatsd.d.ts
@@ -1,5 +1,5 @@
 // Type definitions for Datadog's nodejs metrics client node-dogstatsd
-// Project: https://github.com/datadog/node-dogstatsd
+// Project: https://github.com/joybro/node-dogstatsd
 // Definitions by: Chris Bobo <https://github.com/chrisbobo>
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
 
