58874f89d50da1cbe01e780e265ca7573a5cb675
heavenshell <heavenshell.jp@gmail.com>
2016-08-30 19:49:50 +0900

    Fix 2-space to 4-space and add semicolon
    DefinitelyTyped contributing says 'use tabs or 4-space indentation'.

--- react-daterange-picker/react-daterange-picker.d.ts
+++ react-daterange-picker/react-daterange-picker.d.ts
@@ -8,71 +8,71 @@
 /// <reference path="../moment-range/moment-range.d.ts" />
 
 declare module 'react-daterange-picker' {
-  import DateRangePicker = ReactDateRangePicker.DateRangePicker
-  export = DateRangePicker
+    import DateRangePicker = ReactDateRangePicker.DateRangePicker;
+    export = DateRangePicker;
 }
 
-declare var DateRangePicker: typeof ReactDateRangePicker
+declare var DateRangePicker: typeof ReactDateRangePicker;
 
 declare namespace ReactDateRangePicker {
-  import React = __React
+    import React = __React;
 
-  interface Props extends React.Props<{}> {
-    bemBlock?: string
-    bemNamespace?: string
-    dateStates?: DateState[]
-    defaultState?: string
-    disableNavigation?: boolean
-    firstOfWeek?: number // React.PropTypes.oneOf([0, 1, 2, 3, 4, 5, 6])
-    helpMessage?: string
-    initialFromValue?: boolean
-    initialDate?: Date
-    initialMonth?: number
-    initialYear?: number
-    initialRange?: {}
-    maximumDate?: Date
-    minimumDate?: Date
-    numberOfCalendars?: number
-    onHighlightDate?(date: Date): void
-    onHighlightRange?(date: Date): void
-    onSelect?(value: Props): void
-    onSelectStart?(value: moment.Moment): void
-    paginationArrowComponent?: PaginationArrow
-    selectedLabel?: string
-    selectionType?: 'single' |'range'
-    singleDateRange?: boolean
-    showLegend?: boolean
-    stateDefinitions?: StateDefinitions
-    value?: moment.Moment | moment.Range
-  }
+    interface Props extends React.Props<{}> {
+        bemBlock?: string;
+        bemNamespace?: string;
+        dateStates?: DateState[];
+        defaultState?: string;
+        disableNavigation?: boolean;
+        firstOfWeek?: number; // React.PropTypes.oneOf([0, 1, 2, 3, 4, 5, 6])
+        helpMessage?: string;
+        initialFromValue?: boolean;
+        initialDate?: Date;
+        initialMonth?: number;
+        initialYear?: number;
+        initialRange?: {};
+        maximumDate?: Date;
+        minimumDate?: Date;
+        numberOfCalendars?: number;
+        onHighlightDate?(date: Date): void;
+        onHighlightRange?(date: Date): void;
+        onSelect?(value: Props): void;
+        onSelectStart?(value: moment.Moment): void;
+        paginationArrowComponent?: PaginationArrow;
+        selectedLabel?: string;
+        selectionType?: 'single' |'range';
+        singleDateRange?: boolean;
+        showLegend?: boolean;
+        stateDefinitions?: StateDefinitions;
+        value?: moment.Moment | moment.Range;
+    }
 
-  interface DateState {
-    state: string
-    range: moment.Range
-  }
+    interface DateState {
+        state: string;
+        range: moment.Range;
+    }
 
-  interface StateDefinitions {
-    [key: string]: StateDefinition
-  }
+    interface StateDefinitions {
+        [key: string]: StateDefinition;
+    }
 
-  interface StateDefinition {
-    color: string
-    label: string
-    selectable?: boolean
-  }
+    interface StateDefinition {
+        color: string;
+        label: string;
+        selectable?: boolean;
+    }
 
-  interface PaginationArrowProps extends React.Props<{}> {
-    disabled?: boolean
-    onTrigger?(): void
-    direction?: 'next' | 'previous'
-  }
+    interface PaginationArrowProps extends React.Props<{}> {
+        disabled?: boolean;
+        onTrigger?(): void;
+        direction?: 'next' | 'previous';
+    }
 
-  class PaginationArrow extends React.Component<PaginationArrowProps, {}> {
-  }
+    class PaginationArrow extends React.Component<PaginationArrowProps, {}> {
+    }
 
-  class DateRangePicker extends React.Component<Props, {}> {
-  }
+    class DateRangePicker extends React.Component<Props, {}> {
+    }
 
-  namespace DateRangePicker {
-  }
+    namespace DateRangePicker {
+    }
 }
