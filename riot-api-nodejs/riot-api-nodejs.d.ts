4e252d9a5a2842278d1aa3f91963c3deb71f62bf
Luca Laissue <luca.laissue@idleweiss.ch>
2016-08-30 13:43:24 +0200

    remove useless lib

--- riot-api-nodejs/riot-api-nodejs.d.ts
+++ riot-api-nodejs/riot-api-nodejs.d.ts
@@ -4,7 +4,6 @@
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
 
 /// <reference path="../riot-games-api/riot-games-api.d.ts" />
-/// <reference path="../es6-promise/es6-promise.d.ts"/>
 
 declare module "riot-api-nodejs" {
 
