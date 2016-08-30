e41ddf9b61accea03e6cb6dc2862b9fdcb9350ef
Alejandro Sánchez <sanchezajsa@gmail.com>
2016-08-30 17:41:58 -0600

    Fixed export

--- mitm/mitm-tests.ts
+++ mitm/mitm-tests.ts
@@ -1,18 +1,16 @@
 ///<reference path="mitm.d.ts"/>
 
-import Mitm, {BypassableSocket, SocketOptions} from 'mitm';
-import {Socket} from 'net';
-import {IncomingMessage, ServerResponse} from 'http';
+import Mitm = require('mitm');
 
 const mitm = Mitm();
 mitm.disable();
 
-mitm.on('connect', (socket: BypassableSocket, opts: SocketOptions): void => {
-	socket.bypass();
+mitm.on('connect', (bypassableSocket, opts): void => {
+	bypassableSocket.bypass();
 });
 
-mitm.on('connection', (socket: Socket, opts: SocketOptions): void => {
+mitm.on('connection', (socket, opts): void => {
 });
 
-mitm.on('request', (request: IncomingMessage, response: ServerResponse): void => {
+mitm.on('request', (request, response): void => {
 });
