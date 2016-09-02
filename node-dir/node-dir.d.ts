3d804c2ca09d4a8faee7a34db4f5eb67ab70b5b3
Dmitry Radkovskiy <zlumer@gmail.com>
2016-09-02 14:51:40 +0300

    reference node in node-dir

--- node-dir/node-dir.d.ts
+++ node-dir/node-dir.d.ts
@@ -2,6 +2,7 @@
 // Project: https://github.com/fshost/node-dir
 // Definitions by: Panu Horsmalahti <https://github.com/panuhorsmalahti/>
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
+///<reference path="../node/node.d.ts"/>
 
 declare module "node-dir" {
     import { ReadStream } from "fs";
