c1fa340cd0c309be9c264231521deac3d284db77
Diego Pedro <diegopedro1194@gmail.com>
2016-09-05 11:55:08 -0300

    Add removeBackView in ionic.d.ts

--- ionic/ionic.d.ts
+++ ionic/ionic.d.ts
@@ -213,7 +213,7 @@ declare namespace ionic {
             currentStateName(): string;
 
             goBack(backCount?: number): void;
-            removeBackHistory(): void;
+            removeBackView(): void;
             clearHistory(): void;
             clearCache(): angular.IPromise<any>;
             nextViewOptions(options: IonicHistoryNextViewOptions): void;
