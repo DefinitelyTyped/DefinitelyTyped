dc4c438336b8c2cf0548656af462de87737482c2
Artur Eshenbrener <strate@yandex.ru>
2016-09-05 19:30:46 +0300

    Allow Bluebird to be assignable to ES2016 Promise.
    Add yet another one `.then` overloadings.

--- bluebird/bluebird.d.ts
+++ bluebird/bluebird.d.ts
@@ -47,6 +47,7 @@ class Bluebird<R> implements Bluebird.Thenable<R>, Bluebird.Inspection<R> {
   /**
    * Promises/A+ `.then()`. Returns a new promise chained from this promise. The new promise will be rejected or resolved dedefer on the passed `fulfilledHandler`, `rejectedHandler` and the state of this promise.
    */
+  then<U1, U2>(onFulfill: (value: R) => U1 | Bluebird.Thenable<U1>, onReject?: (error: any) => U2 | Bluebird.Thenable<U2>): Bluebird<U1 | U2>;
   then<U>(onFulfill: (value: R) => U | Bluebird.Thenable<U>, onReject?: (error: any) => U | Bluebird.Thenable<U>): Bluebird<U>;
   then<U>(onFulfill: (value: R) => U | Bluebird.Thenable<U>, onReject?: (error: any) => void | Bluebird.Thenable<void>): Bluebird<U>;
 
