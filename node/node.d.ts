c2df63d4f9ac8c94ab695d8467bd33791da76055
hriss95 <hristian.t.hristov@gmail.com>
2016-09-06 15:17:33 +0100

    Node.d.ts: Introduce module "timers" (#10964)

--- node/node.d.ts
+++ node/node.d.ts
@@ -2647,3 +2647,12 @@ declare module "constants" {
 declare module "process" {
     export = process;
 }
+
+declare module "timers" {
+    export function setTimeout(callback: (...args: any[]) => void, ms: number, ...args: any[]): NodeJS.Timer;
+    export function clearTimeout(timeoutId: NodeJS.Timer): void;
+    export function setInterval(callback: (...args: any[]) => void, ms: number, ...args: any[]): NodeJS.Timer;
+    export function clearInterval(intervalId: NodeJS.Timer): void;
+    export function setImmediate(callback: (...args: any[]) => void, ...args: any[]): any;
+    export function clearImmediate(immediateId: any): void;
+}
