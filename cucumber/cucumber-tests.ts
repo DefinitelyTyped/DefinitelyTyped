28f791f3ea3082f00159a150938f71f4403c36ce
AbraaoAlves <abraao.teodosio@gmail.com>
2016-09-07 09:45:17 -0300

    unify the indent style with other lines in tests

--- cucumber/cucumber-tests.ts
+++ cucumber/cucumber-tests.ts
@@ -3,7 +3,7 @@
 
 function StepSample() {
 	type Callback = cucumber.CallbackStepDefinition;
-  type Table = cucumber.TableDefinition;
+  	type Table = cucumber.TableDefinition;
 	var step = <cucumber.StepDefinitions>this;
 	var hook = <cucumber.Hooks>this;
 
@@ -39,42 +39,42 @@ function StepSample() {
 		}
 	});
   
-  // Type for data_table.js on 
-  // https://github.com/cucumber/cucumber-js/blob/a5fd8251918c278ab2e389226d165cedb44df14a/lib/cucumber/ast/data_table.js
-  
-  step.Given(/^a table step with Table raw$/, function(table:Table){
-    var expected = [
-      ['Cucumber', 'Cucumis sativus'],
-      ['Burr Gherkin', 'Cucumis anguria']
-    ];
-
-    assert.deepEqual(table.raw(), expected);
-  });
-
-   step.Given(/^a table step with Table rows$/, function(table: Table){
-    var expected = [
-      ['Apricot', '5'],
-      ['Brocolli', '2'],
-      ['Cucumber', '10']
-    ];
-    assert.deepEqual(table.rows(), expected)
-  });
-
-  step.Given(/^a table step with Table rowHash$/, function(table:Table){
-    var expected = {
-      'Cucumber': 'Cucumis sativus',
-      'Burr Gherkin': 'Cucumis anguria'
-    };
-    assert.deepEqual(table.rowsHash(), expected)
-  });
-
-  step.Given(/^a table step$/, function(table:Table){
-    var expected = [
-      {'Vegetable': 'Apricot', 'Rating': '5'},
-      {'Vegetable': 'Brocolli', 'Rating': '2'},
-      {'Vegetable': 'Cucumber', 'Rating': '10'}
-    ];
-    assert.deepEqual(table.hashes(), expected)
-  });
+	// Type for data_table.js on 
+	// https://github.com/cucumber/cucumber-js/blob/a5fd8251918c278ab2e389226d165cedb44df14a/lib/cucumber/ast/data_table.js
+	
+	step.Given(/^a table step with Table raw$/, function(table:Table){
+		var expected = [
+			['Cucumber', 'Cucumis sativus'],
+			['Burr Gherkin', 'Cucumis anguria']
+		];
+	
+		assert.deepEqual(table.raw(), expected);
+	});
+	
+	step.Given(/^a table step with Table rows$/, function(table: Table){
+		var expected = [
+			['Apricot', '5'],
+			['Brocolli', '2'],
+			['Cucumber', '10']
+		];
+		assert.deepEqual(table.rows(), expected)
+	});
+	
+	step.Given(/^a table step with Table rowHash$/, function(table:Table){
+		var expected = {
+			'Cucumber': 'Cucumis sativus',
+			'Burr Gherkin': 'Cucumis anguria'
+		};
+		assert.deepEqual(table.rowsHash(), expected)
+	});
+	
+	step.Given(/^a table step$/, function(table:Table){
+		var expected = [
+			{'Vegetable': 'Apricot', 'Rating': '5'},
+			{'Vegetable': 'Brocolli', 'Rating': '2'},
+			{'Vegetable': 'Cucumber', 'Rating': '10'}
+		];
+		assert.deepEqual(table.hashes(), expected)
+	});
 }
 
