3ee905eff3b4568966bdd7395b075c52ab6ff2a6
Joshua DeVinney <josh@sal.tt>
2016-09-07 12:00:43 -0500

    Renaming interfaces to drop the I

--- oibackoff/index.d.ts
+++ oibackoff/index.d.ts
@@ -3,41 +3,41 @@
 // Definitions by: Joshua DeVinney <https://github.com/geoffreak>
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
 
-export declare function backoff(opts: IBackoffOptions): IBackoffInstance;
+export declare function backoff(opts: BackoffOptions): BackoffInstance;
 
-export declare interface IBackoffOptions {
+export declare interface BackoffOptions {
     maxTries?: number;
     algorithm?: 'incremental' | 'exponential' | 'fibonacci';
     delayRatio?: number;
     maxDelay?: number;
 }
 
-export declare interface IBackoffInstance {
+export declare interface BackoffInstance {
     (fn: Function, callback: Function): void;
-    (fn: Function, intermediate: IBackoffIntermediate | Function, callback: Function): void;
+    (fn: Function, intermediate: BackoffIntermediate | Function, callback: Function): void;
     (fn: Function, data0: any, callback: Function): void;
-    (fn: Function, data0: any, intermediate: IBackoffIntermediate | Function, callback: Function): void;
+    (fn: Function, data0: any, intermediate: BackoffIntermediate | Function, callback: Function): void;
     (fn: Function, data0: any, data1: any, callback: Function): void;
-    (fn: Function, data0: any, data1: any, intermediate: IBackoffIntermediate | Function, callback: Function): void;
+    (fn: Function, data0: any, data1: any, intermediate: BackoffIntermediate | Function, callback: Function): void;
     (fn: Function, data0: any, data1: any, data2: any, callback: Function): void;
-    (fn: Function, data0: any, data1: any, data2: any, intermediate: IBackoffIntermediate | Function, callback: Function): void;
+    (fn: Function, data0: any, data1: any, data2: any, intermediate: BackoffIntermediate | Function, callback: Function): void;
     (fn: Function, data0: any, data1: any, data2: any, data3: any, callback: Function): void;
-    (fn: Function, data0: any, data1: any, data2: any, data3: any, intermediate: IBackoffIntermediate | Function, callback: Function): void;
+    (fn: Function, data0: any, data1: any, data2: any, data3: any, intermediate: BackoffIntermediate | Function, callback: Function): void;
     (fn: Function, data0: any, data1: any, data2: any, data3: any, data4: any, callback: Function): void;
-    (fn: Function, data0: any, data1: any, data2: any, data3: any, data4: any, intermediate: IBackoffIntermediate | Function, callback: Function): void;
+    (fn: Function, data0: any, data1: any, data2: any, data3: any, data4: any, intermediate: BackoffIntermediate | Function, callback: Function): void;
     (fn: Function, data0: any, data1: any, data2: any, data3: any, data4: any, data5: any, callback: Function): void;
-    (fn: Function, data0: any, data1: any, data2: any, data3: any, data4: any, data5: any, intermediate: IBackoffIntermediate | Function, callback: Function): void;
+    (fn: Function, data0: any, data1: any, data2: any, data3: any, data4: any, data5: any, intermediate: BackoffIntermediate | Function, callback: Function): void;
     (fn: Function, data0: any, data1: any, data2: any, data3: any, data4: any, data5: any, data6: any, callback: Function): void;
-    (fn: Function, data0: any, data1: any, data2: any, data3: any, data4: any, data5: any, data6: any, intermediate: IBackoffIntermediate | Function, callback: Function): void;
+    (fn: Function, data0: any, data1: any, data2: any, data3: any, data4: any, data5: any, data6: any, intermediate: BackoffIntermediate | Function, callback: Function): void;
     (fn: Function, data0: any, data1: any, data2: any, data3: any, data4: any, data5: any, data6: any, data7: any, callback: Function): void;
-    (fn: Function, data0: any, data1: any, data2: any, data3: any, data4: any, data5: any, data6: any, data7: any, intermediate: IBackoffIntermediate | Function, callback: Function): void;
+    (fn: Function, data0: any, data1: any, data2: any, data3: any, data4: any, data5: any, data6: any, data7: any, intermediate: BackoffIntermediate | Function, callback: Function): void;
     (fn: Function, data0: any, data1: any, data2: any, data3: any, data4: any, data5: any, data6: any, data7: any, data8: any, callback: Function): void;
-    (fn: Function, data0: any, data1: any, data2: any, data3: any, data4: any, data5: any, data6: any, data7: any, data8: any, intermediate: IBackoffIntermediate | Function, callback: Function): void;
+    (fn: Function, data0: any, data1: any, data2: any, data3: any, data4: any, data5: any, data6: any, data7: any, data8: any, intermediate: BackoffIntermediate | Function, callback: Function): void;
     (fn: Function, data0: any, data1: any, data2: any, data3: any, data4: any, data5: any, data6: any, data7: any, data8: any, data9: any, callback: Function): void;
-    (fn: Function, data0: any, data1: any, data2: any, data3: any, data4: any, data5: any, data6: any, data7: any, data8: any, data9: any, intermediate: IBackoffIntermediate | Function, callback: Function): void;
+    (fn: Function, data0: any, data1: any, data2: any, data3: any, data4: any, data5: any, data6: any, data7: any, data8: any, data9: any, intermediate: BackoffIntermediate | Function, callback: Function): void;
 }
 
-export declare interface IBackoffIntermediate {
+export declare interface BackoffIntermediate {
     (err: any, tries: number, delay: number): boolean;
 }
 
