ef83ebd88e6922e0c56b66ea1d56efd97a27a9fd
Denis Carriere <carriere.denis@gmail.com>
2016-09-07 07:39:08 -0400

    geojson2osm fixed comments - renamed test > tests - added space after project @vvakame

--- geojson2osm/geojson2osm.d.ts
+++ geojson2osm/geojson2osm.d.ts
@@ -1,5 +1,5 @@
 // Type definitions for geojson2osm 0.0.5
-// Project:https://github.com/Rub21/geojson2osm
+// Project: https://github.com/Rub21/geojson2osm
 // Definitions by: Denis Carriere <https://github.com/DenisCarriere>
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
 
