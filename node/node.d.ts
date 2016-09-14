5ee054ac95f68355c05b8d168b9a2f1eb4a581f8
hriss95 <hristian.t.hristov@gmail.com>
2016-09-14 13:26:40 +0100

    [Node.d.ts]: Update definitions for module "crypto" (#11186)

--- node/node.d.ts
+++ node/node.d.ts
@@ -2434,6 +2434,7 @@ declare module "crypto" {
         setPrivateKey(private_key: string, encoding: HexBase64Latin1Encoding): void;
     }
     export function createECDH(curve_name: string): ECDH;
+    export var DEFAULT_ENCODING: string;
 }
 
 declare module "stream" {
