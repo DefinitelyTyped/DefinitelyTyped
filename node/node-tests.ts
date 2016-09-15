132df634f0b63654ec34b7072febce30cc149905
TonyYang <tonypythoneer@gmail.com>
2016-09-15 13:43:37 +0800

    The test should test type, not function

--- node/node-tests.ts
+++ node/node-tests.ts
@@ -1110,14 +1110,13 @@ namespace errors_tests {
 ///////////////////////////////////////////////////////////
 
 import * as p from "process";
-namespace process_tests{
+namespace process_tests {
     {
         var eventEmitter: events.EventEmitter;
         eventEmitter = process;                // Test that process implements EventEmitter...
         
         var _p: NodeJS.Process = process;
         _p = p;
-        assert(p === process);
     }
 }
 
@@ -1126,9 +1125,10 @@ namespace process_tests{
 ///////////////////////////////////////////////////////////
 
 import * as c from "console";
-namespace console_tests{
+namespace console_tests {
     {
-        assert(c === console);
+        var _c: Console = console;
+        _c = c;
     }
 }
 
