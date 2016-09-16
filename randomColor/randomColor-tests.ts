1b803f972a8edfcd8341520f195e7354fa091ec2
feitzi <mathias@feitzinger.org>
2016-09-16 12:49:30 +0200

    Added definition file for randomColor

---
+++ randomColor/randomColor-tests.ts
@@ -0,0 +1,2 @@
+/// <reference path="randomColor.d.ts" />
+
---
+++ randomColor/randomColor.d.ts
@@ -0,0 +1,30 @@
+// Type definitions for randomColor 0.4.1
+// Project: https://github.com/davidmerfield/randomColor
+// Definitions by: Mathias Feitzinger <https://github.com/feitzi>
+// Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
+
+declare namespace RandomColor {
+
+ interface RandomColorStatic {
+
+	() : string;
+
+	(options : randomColorOptions): string;
+	
+}
+
+interface randomColorOptions {
+		hue? : string;
+		luminosity? : string;		
+		count? : string;
+		seed?: string;
+		format?: string;
+	}
+}
+
+
+declare var randomColor: RandomColor.RandomColorStatic;
+
+declare module 'randomColor' {
+    export = randomColor;
+}
