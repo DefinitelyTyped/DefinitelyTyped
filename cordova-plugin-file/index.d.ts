27541f076b2e20514cb60456ccaeabf8c9958c88
Andy Hanson <anhans@microsoft.com>
2016-09-16 13:16:58 -0700

    Fix header error: don't need periods at end of name

--- cordova-plugin-file/index.d.ts
+++ cordova-plugin-file/index.d.ts
@@ -1,4 +1,4 @@
-// Type definitions for Apache Cordova File System plugin.
+// Type definitions for Apache Cordova File System plugin
 // Project: https://github.com/apache/cordova-plugin-file
 // Definitions by: Microsoft Open Technologies, Inc. <http://msopentech.com>
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
