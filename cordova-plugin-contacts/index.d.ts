915fd561991a100071ce072acb445344ba4001b8
Andy Hanson <anhans@microsoft.com>
2016-09-16 15:48:23 -0700

    Fix more header punctuation

--- cordova-plugin-contacts/index.d.ts
+++ cordova-plugin-contacts/index.d.ts
@@ -1,6 +1,6 @@
 // Type definitions for Apache Cordova Contacts plugin
 // Project: https://github.com/apache/cordova-plugin-contacts
-// Definitions by: Microsoft Open Technologies, Inc. <http://msopentech.com>
+// Definitions by: Microsoft Open Technologies Inc <http://msopentech.com>
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
 //
 // Copyright (c) Microsoft Open Technologies, Inc.
