246786f0e2c5b741c760dd2796523e44ff636289
Cyril Schumacher <cyril_schumacher@live.fr>
2016-09-16 20:40:44 +0200

    Add definitions for: "bunyan-config", "express-mung" and "node-vitalsigns".

---
+++ node-vitalsigns/node-vitalsigns-tests.ts
@@ -0,0 +1,25 @@
+/// <reference path="node-vitalsigns.d.ts"/>
+
+import * as VitalSigns from "vitalsigns";
+
+var vitals = new VitalSigns();
+
+vitals.monitor('cpu');
+vitals.monitor('mem', {units: 'MB'});
+vitals.monitor('tick');
+
+vitals.unhealthyWhen('cpu', 'usage').equals(100);
+vitals.unhealthyWhen('tick', 'maxMs').greaterThan(500);
+
+vitals.monitor({
+    connections: () => new Object()
+}, {name: 'game'});
+
+var vitals = new VitalSigns({
+    autoCheck: 5000,
+    httpHealthy: 200,
+    httpUnhealthy: 503
+});
+
+vitals.monitor('cpu', {name: 'foo'});
+vitals.unhealthyWhen('foo', 'bar').not.greaterThan(5);
