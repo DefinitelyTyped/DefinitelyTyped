55a4253699078f53919a02a1b440868b79b0f826
Antoine LUCAS <cooluhuru@gmail.com>
2016-09-21 04:41:47 +0700

    More details on the touch parameters - nice screenshot btw

--- shelljs/shelljs.d.ts
+++ shelljs/shelljs.d.ts
@@ -512,16 +512,25 @@ declare module "shelljs"
     export function error(): string;
 
 
+
     export function touch(...files: string[]): void;
     export function touch(files: string[]): void;
 
-    export function touch(options: string, ...files: string[]): void;
-    export function touch(options: string, files: string[]): void;
-    
-    export function touch(options: Object, ...files: string[]): void;
-    export function touch(options: Object, files: string[]): void;
+    type TouchOptionsLiteral = "-a" |  "-c" | "-m" | "-d" | "-r";
 
+    export function touch(options: TouchOptionsLiteral, ...files: string[]): void;
+    export function touch(options: TouchOptionsLiteral, files: string[]): void;
 
+    /**
+     * Update the access and modification times of each FILE to the current time. A FILE argument that does not exist is created empty, unless -c is supplied
+     */
+    type touchOptionsArray = {
+        '-d'?: string;
+        '-r'?: string;
+    };
+
+    export function touch(options: touchOptionsArray, ...files: string[]): void;
+    export function touch(options: touchOptionsArray, files: string[]): void;
 
     // Configuration
 
