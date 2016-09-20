88cb7ba5997549c767f74abee911b0d3e55d8b9a
Joshua DeVinney <josh@sal.tt>
2016-09-20 11:18:23 -0500

    Adding uid-safe version number

--- uid-safe/index.d.ts
+++ uid-safe/index.d.ts
@@ -1,4 +1,4 @@
-// Type definitions for uid-safe
+// Type definitions for uid-safe v2.1
 // Project: https://github.com/crypto-utils/uid-safe
 // Definitions by: Joshua DeVinney <https://github.com/geoffreak>
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
