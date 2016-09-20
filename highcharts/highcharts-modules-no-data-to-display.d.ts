5b5ae174a4018894a590e3d15c4c0f0d8f64c7e3
Subhash Sharma <subhash@modelogiq.com>
2016-09-21 02:12:53 +0530

    Highcharts definition non-instantiated
    * Moved all global interfaces into a namespace
    * Created a __Highcharts namespace for using in dependent files
    * Changed references in dependent files and tests

--- highcharts/highcharts-modules-no-data-to-display.d.ts
+++ highcharts/highcharts-modules-no-data-to-display.d.ts
@@ -3,24 +3,26 @@
 // Definitions by: Andrey Zolotin <http://github.com/nitoloz>
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
 /// <reference path="highcharts.d.ts" />
-interface HighchartsChartObject {
-    /**
-     * Returns true if there are data points within the plot area now
-     * @return {boolean} If chart has any data.
-     * @since 3.0.8
-     */
-    hasData(): boolean;
+declare namespace __Highcharts {
+    interface ChartObject {
+        /**
+         * Returns true if there are data points within the plot area now
+         * @return {boolean} If chart has any data.
+         * @since 3.0.8
+         */
+        hasData(): boolean;
 
-    /**
-     * Hide the 'No data to display' message added by the no-data-to-display plugin.
-     * @since 3.0.8
-     */
-    hideNoData(): void;
+        /**
+         * Hide the 'No data to display' message added by the no-data-to-display plugin.
+         * @since 3.0.8
+         */
+        hideNoData(): void;
 
-    /**
-     * Display a no-data message.
-     * @param {String} message An optional message to show in place of the default one
-     * @since 3.0.8
-     */
-    showNoData(message?: string): void;
+        /**
+         * Display a no-data message.
+         * @param {String} message An optional message to show in place of the default one
+         * @since 3.0.8
+         */
+        showNoData(message?: string): void;
+    }
 }
