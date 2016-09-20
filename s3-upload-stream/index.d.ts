5a37c8cf4a5562bb0509203600db3c7083be81e0
Joshua DeVinney <josh@sal.tt>
2016-09-19 21:36:06 -0500

    Making requested changes by @andy-ms for s3-upload-stream typing

--- s3-upload-stream/index.d.ts
+++ s3-upload-stream/index.d.ts
@@ -4,23 +4,19 @@
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
 
 /// <reference types="node" />
-/// <reference path="../aws-sdk/index.d.ts" />
+/// <reference types="aws-sdk" />
 
 import * as stream from 'stream';
 import * as AWS from 'aws-sdk';
 
-interface IS3Stream {
-    (client: AWS.S3): IS3StreamUploader
+interface S3StreamUploader {
+    upload(destinationDetails: AWS.s3.PutObjectRequest, sessionDetails?: any): S3WriteStream;
 }
 
-interface IS3StreamUploader {
-    upload(destinationDetails: AWS.s3.PutObjectRequest, sessionDetails?: any): IS3WriteStream;
-}
-
-interface IS3WriteStream extends stream.Writable {
+interface S3WriteStream extends stream.Writable {
     maxPartSize(sizeInBytes: number): void;
     concurrentParts(numberOfParts: number): void;
 }
 
-declare var s3Stream: IS3Stream;
+declare function s3Stream(client: AWS.S3): S3StreamUploader;
 export = s3Stream;
