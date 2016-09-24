a2ca59331a8c263b288fde2a8e94bfde05163b11
David Wood <david.p.wood@gmail.com>
2016-09-24 15:33:20 +0100

    Add yayson Store tests

--- yayson/yayson-tests.ts
+++ yayson/yayson-tests.ts
@@ -3,13 +3,30 @@
 import * as Yayson from "yayson";
 
 const yayson = Yayson();
+const Adapter = yayson.Adapter;
+const Store = yayson.Store;
+const Presenter = yayson.Presenter;
 
 interface Thing {
   hello: 'world' | 'you';
 }
 
 function test_store() {
-  const store = new yayson.Store();
-  const syncObj = store.sync({ x: 'y' });
-  const syncTyped = store.sync<Thing>({ any: 'thing' });
+  const store = new Store();
+
+  store.sync({ any: 'thing' });
+
+  const findObj: Object = store.find('mytype', '1234');
+  const findThing: Thing = store.find<Thing>('mytype', '1234');
+
+  const findObjs: Object = store.findAll('mytype');
+  const findThings: Thing[] = store.findAll<Thing>('mytype');
+
+  store.remove('mytype', '1234');
+  store.remove('mytype');
+
+  store.reset();
+
+  const records: any[] = store.records;
+  const relations: { [type: string]: any } = store.relations;
 }
--- yayson/yayson.d.ts
+++ yayson/yayson.d.ts
@@ -5,15 +5,59 @@
 
 declare module 'yayson' {
   class Store {
-    sync(obj: Object): Object;
-    sync<T>(obj: Object): T;
+    sync(obj: Object): void;
+    find(type: string, id: string): Object;
+    find<T>(type: string, id: string): T;
+    findAll(type: string): any[];
+    findAll<T>(type: string): T[];
+    remove(type: string, id: string): void;
+    remove(type: string): void;
+    reset(): void;
+    records: Array<any>;
+    relations: { [key: string]: any };
+  }
+
+  interface Adapter {
+    get(model: any, key: string): any;
+    get(model: any): any;
+    get<T>(model: any, key: string): T;
+    get<T>(model: any): T;
+  }
+
+  class Presenter {
+    static adapter: string;
+    static render(instanceOrCollection: {} | PromiseLike<{}>, options: yayson.JsonOptions): any;
+    static render(instanceOrCollection: {} | PromiseLike<{}>): any;
+    static toJSON(instanceOrCollection: {} | Array<{}>, options: yayson.JsonOptions): any;
+    static toJSON(instanceOrCollection: {} | Array<{}>): any;
+    
+    render(instanceOrCollection: {} | PromiseLike<{}>, options: yayson.JsonOptions): any;
+    render(instanceOrCollection: {} | PromiseLike<{}>): any;
+    toJSON(instanceOrCollection: {} | Array<{}>, options: yayson.JsonOptions): any;
+    toJSON(instanceOrCollection: {} | Array<{}>): any;
+
+    type: string;
   }
 
   interface Yayson {
     Store: typeof Store;
+    Presenter: typeof Presenter;
+    Adapter: Adapter;
+  }
+
+  interface YaysonOptions {
+    adapter?: 'default' | 'sequelize';
   }
 
   function yayson(): Yayson;
-  namespace yayson { }
+  function yayson(arg: YaysonOptions): Yayson;
+
+  namespace yayson {
+    interface JsonOptions {
+      [key: string]: any;
+      meta?: any;
+    }
+  }
+
   export = yayson;
 }
