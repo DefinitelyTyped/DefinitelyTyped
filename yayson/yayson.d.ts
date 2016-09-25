6fe3645aed125f3012d45ebf1cdbd21292410b7a
David Wood <david.p.wood@gmail.com>
2016-09-25 09:41:40 +0100

    Specify version in yayson definition

--- yayson/yayson.d.ts
+++ yayson/yayson.d.ts
@@ -1,4 +1,4 @@
-// Type definitions for yayson
+// Type definitions for yayson v2.0.1
 // Project: https://github.com/confetti/yayson
 // Definitions by: David Wood <https://github.com/Codesleuth>
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
