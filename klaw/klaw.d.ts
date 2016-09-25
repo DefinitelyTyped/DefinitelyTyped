fc5179c689fc73b7eeda05fb28e4b3b8950b181e
Matthew McEachen <matthew+github@mceachen.org>
2016-09-25 15:13:34 -0700

    addressed pr comments and `tsfmt -r klaw/*.ts`

--- klaw/klaw.d.ts
+++ klaw/klaw.d.ts
@@ -1,4 +1,4 @@
-// Type definitions for klaw
+// Type definitions for klaw v1.3.0
 // Project: https://github.com/jprichardson/node-klaw
 // Definitions by: Matthew McEachen <https://github.com/mceachen>
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
@@ -7,38 +7,38 @@
 
 declare module "klaw" {
 
-  import * as fs from "fs"
-  import {Readable, ReadableOptions} from 'stream'
+    import * as fs from "fs"
+    import { Readable, ReadableOptions } from 'stream'
 
-  function K(root: string, options?: K.Options): K.Walker
+    function K(root: string, options?: K.Options): K.Walker
 
-  namespace K {
-    interface Item {
-      path: string
-      stats: fs.Stats
+    namespace K {
+        interface Item {
+            path: string
+            stats: fs.Stats
+        }
+
+        type QueueMethod = "shift" | "pop"
+
+        interface Options extends ReadableOptions {
+            queueMethod?: QueueMethod
+            pathSorter?: (a: Array<Item>) => Array<Item>
+            fs?: any // fs or mock-fs
+            filter?: (a: Item) => boolean
+        }
+
+        type Event = "close" | "data" | "end" | "readable" | "error"
+
+        interface Walker {
+            on(event: Event, listener: Function): this
+            on(event: "close", listener: () => void): this
+            on(event: "data", listener: (item: Item) => void): this
+            on(event: "end", listener: () => void): this
+            on(event: "readable", listener: () => void): this
+            on(event: "error", listener: (err: Error) => void): this
+            read(): Item
+        }
     }
 
-    type QueueMethod = "shift" | "pop"
-
-    interface Options extends ReadableOptions {
-      queueMethod?: QueueMethod
-      pathSorter?: (a: Array<Item>) => Array<Item>
-      fs?: any // fs or mock-fs
-      filter?: (a: Item) => boolean
-    }
-
-    type Event = "close" | "data" | "end" | "readable" | "error"
-
-    interface Walker {
-      on(event: Event, listener: Function): this
-      on(event: "close", listener: () => void): this
-      on(event: "data", listener: (item: Item) => void): this
-      on(event: "end", listener: () => void): this
-      on(event: "readable", listener: () => void): this
-      on(event: "error", listener: (err: Error) => void): this
-      read(): Item
-    }
-  }
-
-  export = K
+    export = K
 }
