a50033be0937f067ce2e901e84e17ca3dddbe1a7
Joshua DeVinney <josh@sal.tt>
2016-09-24 19:34:09 -0500

    Adding version and dropping the I from the interfaces

--- maxmind/index.d.ts
+++ maxmind/index.d.ts
@@ -1,30 +1,30 @@
-// Type definitions for maxmind
+// Type definitions for maxmind v1.3
 // Project: https://github.com/runk/node-maxmind
 // Definitions by: Joshua DeVinney <https://github.com/geoffreak>
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
 
-export declare function open(database: string, opts?: IOptions): IReader;
+export declare function open(database: string, opts?: Options): Reader;
 export declare function validate(ip: string): boolean;
 
-export declare interface IReader {
-    get(ip: string): IResponse | null;
-    readonly metadata: IMetadata;
+export declare interface Reader {
+    get(ip: string): Response | null;
+    readonly metadata: Metadata;
 }
 
-export declare interface IOptions {
+export declare interface Options {
     cache?: {
         max?: number;
         maxAge?: number;
     }
 }
 
-export declare interface IMetadata {
+export declare interface Metadata {
     readonly binaryFormatMajorVersion: number;
     readonly binaryFormatMinorVersion: number;
     readonly buildEpoch: Date;
     readonly databaseType: string;
     readonly languages: string[];
-    readonly description: ITranslations;
+    readonly description: Translations;
     readonly ipVersion: number;
     readonly nodeCount: number;
     readonly recordSize: number;
@@ -33,20 +33,20 @@ export declare interface IMetadata {
     readonly treeDepth: number;
 }
 
-export declare interface IResponse {
+export declare interface Response {
     readonly continent?: {
         readonly geoname_id: number;
-        readonly names: ITranslations;
+        readonly names: Translations;
         readonly code: string;
     };
     readonly country?: {
         readonly geoname_id: number;
-        readonly names: ITranslations;
+        readonly names: Translations;
         readonly iso_code: string;
     };
     readonly registered_country?: {
         readonly geoname_id: number;
-        readonly names: ITranslations;
+        readonly names: Translations;
         readonly iso_code: string;
     };
     readonly location?: {
@@ -58,16 +58,16 @@ export declare interface IResponse {
     };
     readonly subdivisions?: {
         readonly geoname_id: number;
-        readonly names: ITranslations;
+        readonly names: Translations;
         readonly iso_code: string;
     }[];
     readonly city?: {
         readonly geoname_id: number;
-        readonly names: ITranslations;
+        readonly names: Translations;
     };
     readonly postal?: { code: string };
 }
 
-export declare interface ITranslations {
+export declare interface Translations {
     readonly [index: string]: string;
 }
