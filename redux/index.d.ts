e5655ae057c026fc086f42b51f27de125dcac8a8
Stephan <stephantroyer@live.at>
2016-09-26 11:02:45 +0200

    include Func0..3 types as in Redux' index.d.ts

--- redux/index.d.ts
+++ redux/index.d.ts
@@ -366,6 +366,11 @@ declare namespace Redux {
 
     /* compose */
 
+    type Func0<R> = () => R;
+    type Func1<T1, R> = (a1: T1) => R;
+    type Func2<T1, T2, R> = (a1: T1, a2: T2) => R;
+    type Func3<T1, T2, T3, R> = (a1: T1, a2: T2, a3: T3, ...args: any[]) => R;
+
     /**
      * Composes single-argument functions from right to left. The rightmost
      * function can take multiple arguments as it provides the signature for the
@@ -382,45 +387,45 @@ declare namespace Redux {
 
     /* two functions */
     function compose<A, R>(
-        f1: (b: A) => R, f2: () => A
-    ): () => R;
+      f1: (b: A) => R, f2: Func0<A>
+    ): Func0<R>;
     function compose<A, T1, R>(
-        f1: (b: A) => R, f2: (a1: T1) => A
-    ): (a1: T1) => R;
+      f1: (b: A) => R, f2: Func1<T1, A>
+    ): Func1<T1, R>;
     function compose<A, T1, T2, R>(
-        f1: (b: A) => R, f2: (a1: T1, a2: T2) => A
-    ): (a1: T1, a2: T2) => R;
+      f1: (b: A) => R, f2: Func2<T1, T2, A>
+    ): Func2<T1, T2, R>;
     function compose<A, T1, T2, T3, R>(
-        f1: (b: A) => R, f2: (a1: T1, a2: T2, a3: T3, ...args: any[]) => A
-    ): (a1: T1, a2: T2, a3: T3, ...args: any[]) => R;
+      f1: (b: A) => R, f2: Func3<T1, T2, T3, A>
+    ): Func3<T1, T2, T3, R>;
 
     /* three functions */
     function compose<A, B, R>(
-        f1: (b: B) => R, f2: (a: A) => B, f3: () => A
-    ): () => R;
+      f1: (b: B) => R, f2: (a: A) => B, f3: Func0<A>
+    ): Func0<R>;
     function compose<A, B, T1, R>(
-        f1: (b: B) => R, f2: (a: A) => B, f3: (a1: T1) => A
-    ): (a1: T1) => R;
+      f1: (b: B) => R, f2: (a: A) => B, f3: Func1<T1, A>
+    ): Func1<T1, R>;
     function compose<A, B, T1, T2, R>(
-        f1: (b: B) => R, f2: (a: A) => B, f3: (a1: T1, a2: T2) => A
-    ): (a1: T1, a2: T2) => R;
+      f1: (b: B) => R, f2: (a: A) => B, f3: Func2<T1, T2, A>
+    ): Func2<T1, T2, R>;
     function compose<A, B, T1, T2, T3, R>(
-        f1: (b: B) => R, f2: (a: A) => B, f3: (a1: T1, a2: T2, a3: T3, ...args: any[]) => A
-    ): (a1: T1, a2: T2, a3: T3, ...args: any[]) => R;
+      f1: (b: B) => R, f2: (a: A) => B, f3: Func3<T1, T2, T3, A>
+    ): Func3<T1, T2, T3, R>;
 
     /* four functions */
     function compose<A, B, C, R>(
-        f1: (b: C) => R, f2: (a: B) => C, f3: (a: A) => B, f4: () => A
-    ): () => R;
+      f1: (b: C) => R, f2: (a: B) => C, f3: (a: A) => B, f4: Func0<A>
+    ): Func0<R>;
     function compose<A, B, C, T1, R>(
-        f1: (b: C) => R, f2: (a: B) => C, f3: (a: A) => B, f4: (a1: T1) => A
-    ): (a1: T1) => R;
+      f1: (b: C) => R, f2: (a: B) => C, f3: (a: A) => B, f4: Func1<T1, A>
+    ): Func1<T1, R>;
     function compose<A, B, C, T1, T2, R>(
-        f1: (b: C) => R, f2: (a: B) => C, f3: (a: A) => B, f4: (a1: T1, a2: T2) => A
-    ): (a1: T1, a2: T2) => R;
+      f1: (b: C) => R, f2: (a: B) => C, f3: (a: A) => B, f4: Func2<T1, T2, A>
+    ): Func2<T1, T2, R>;
     function compose<A, B, C, T1, T2, T3, R>(
-        f1: (b: C) => R, f2: (a: B) => C, f3: (a: A) => B, f4: (a1: T1, a2: T2, a3: T3, ...args: any[]) => A
-    ): (a1: T1, a2: T2, a3: T3, ...args: any[]) => R;
+      f1: (b: C) => R, f2: (a: B) => C, f3: (a: A) => B, f4: Func3<T1, T2, T3, A>
+    ): Func3<T1, T2, T3, R>;
 
     /* rest */
     function compose<A, B, C, R>(
