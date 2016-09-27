0a256ea3c3b15b6d0a1da97169903f41d2853eee
Emmanuel Surleau <es@impero.dk>
2016-09-27 09:15:51 +0200

    Make SyntheticEvent.target generic, not SyntheticEvent.currentTarget (#11508).

--- react/index.d.ts
+++ react/index.d.ts
@@ -272,7 +272,7 @@ declare namespace React {
 
     interface SyntheticEvent<T> {
         bubbles: boolean;
-        currentTarget: EventTarget & T;
+        currentTarget: EventTarget;
         cancelable: boolean;
         defaultPrevented: boolean;
         eventPhase: number;
@@ -283,7 +283,7 @@ declare namespace React {
         stopPropagation(): void;
         isPropagationStopped(): boolean;
         persist(): void;
-        target: EventTarget;
+        target: EventTarget & T;
         timeStamp: Date;
         type: string;
     }
