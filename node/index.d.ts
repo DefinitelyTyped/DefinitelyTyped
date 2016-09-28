5b5d155dba44b379e1ed0e7d1100c3f6ef128b9d
Herrington Darkholme <nonamesheep1@gmail.com>
2016-09-29 00:47:49 +0800

    Node: fix readline types

--- node/index.d.ts
+++ node/index.d.ts
@@ -1349,10 +1349,7 @@ declare module "readline" {
         (line: string, callback: (err: any, result: CompleterResult) => void): any;
     }
 
-    export interface CompleterResult {
-        completions: string[];
-        line: string;
-    }
+    export type CompleterResult = [string[], string];
 
     export interface ReadLineOptions {
         input: NodeJS.ReadableStream;
