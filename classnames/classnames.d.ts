907c83fe76c9f3a6a7142b5e8396015fda97fb2c
Jonny Stoten <jonny@rocketmakers.com>
2016-09-28 11:36:18 +0100

    Allow null and undefined on classnames

--- classnames/classnames.d.ts
+++ classnames/classnames.d.ts
@@ -3,20 +3,20 @@
 // Definitions by: Dave Keen <http://www.keendevelopment.ch>, Adi Dahiya <https://github.com/adidahiya>, Jason Killian <https://github.com/JKillian>
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
 
-declare type ClassValue = string | number | ClassDictionary | ClassArray;
+declare type ClassValue = string | number | ClassDictionary | ClassArray | null | undefined;
 
 interface ClassDictionary {
-	[id: string]: boolean;
+  [id: string]: boolean | null | undefined;
 }
 
 interface ClassArray extends Array<ClassValue> { }
 
 interface ClassNamesFn {
-	(...classes: ClassValue[]): string;
+  (...classes: ClassValue[]): string;
 }
 
 declare var classNames: ClassNamesFn;
 
 declare module "classnames" {
-	export = classNames
+  export = classNames
 }
