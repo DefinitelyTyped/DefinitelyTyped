f22c19ed50474a671f38d0e58e71de8d64259fe6
Tom Wanzek <tomwanzek@gmail.com>
2016-09-28 10:51:24 -0400

    d3-random * [chore](d3-random): Complete JSDoc comments.

--- d3-random/index.d.ts
+++ d3-random/index.d.ts
@@ -7,6 +7,9 @@
  * Returns a function for generating random numbers with a uniform distribution).
  * The minimum allowed value of a returned number is min, and the maximum is max.
  * If min is not specified, it defaults to 0; if max is not specified, it defaults to 1.
+ *
+ * @param min The minimum allowed value of a returned number, defaults to 0.
+ * @param max The maximum allowed value of a returned number, defaults to 1.
  */
 export function randomUniform(min?: number, max?: number): () => number;
 
@@ -14,27 +17,39 @@ export function randomUniform(min?: number, max?: number): () => number;
  * Returns a function for generating random numbers with a normal (Gaussian) distribution.
  * The expected value of the generated numbers is mu, with the given standard deviation sigma.
  * If mu is not specified, it defaults to 0; if sigma is not specified, it defaults to 1.
+ *
+ * @param mu Expected value, defaults to 0.
+ * @param sigma Standard deviation, defaults to 1.
  */
 export function randomNormal(mu?: number, sigma?: number): () => number;
 
 /**
  * Returns a function for generating random numbers with a log-normal distribution. The expected value of the random variable’s natural logrithm is mu,
  * with the given standard deviation sigma. If mu is not specified, it defaults to 0; if sigma is not specified, it defaults to 1.
+ *
+ * @param mu Expected value, defaults to 0.
+ * @param sigma Standard deviation, defaults to 1.
  */
 export function randomLogNormal(mu?: number, sigma?: number): () => number;
 
 /**
  * Returns a function for generating random numbers with a Bates distribution with n independent variables.
+ *
+ * @param n Number of independent random variables to use.
  */
 export function randomBates(n: number): () => number;
 
 /**
  * Returns a function for generating random numbers with an Irwin–Hall distribution with n independent variables.
+ *
+ * @param n Number of independent random variables to use.
  */
 export function randomIrwinHall(n: number): () => number;
 
 /**
  * Returns a function for generating random numbers with an exponential distribution with the rate lambda;
  * equivalent to time between events in a Poisson process with a mean of 1 / lambda.
+ *
+ * @param lambda Expected time between events.
  */
 export function randomExponential(lambda: number): () => number;
