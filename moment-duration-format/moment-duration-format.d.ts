470539a07f80ace2c177de70e1a18aea100dbbb6
SwintDC <swint.de.coninck+github@gmail.com>
2016-10-03 19:49:30 +0200

    ADD: definition file for the moment-duration-format library (#11557)

---
+++ moment-duration-format/moment-duration-format.d.ts
@@ -0,0 +1,11 @@
+﻿// Type definitions for moment-duration-format v1.3.0
+// Project: https://github.com/jsmreese/moment-duration-format
+// Definitions by: Swint De Coninck <https://github.com/SwintDC>
+// Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
+
+
+declare namespace moment {
+    interface Duration {
+        format(template: string, precision?: string, settings?: any): string;
+    }
+}
