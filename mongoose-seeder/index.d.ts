0c120c20b074ef9e405074538807fd80ade7df6a
Andy <anhans@microsoft.com>
2016-10-03 10:50:09 -0700

    Convert mongoose-seeder to an external module (#11687)

--- mongoose-seeder/index.d.ts
+++ mongoose-seeder/index.d.ts
@@ -3,18 +3,16 @@
 // Definitions by: Crevil <https://github.com/Crevil/>
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
 
-declare module 'mongoose-seeder' {
-  import * as Q from 'q';
+import * as Q from 'q';
 
-  export interface IOptions {
-    dropDatabase?: boolean;
-    dropCollections?: boolean;
-  }
-
-  type seedCallback = (err: any, dbData: any) => void;
-
-  export function seed(data: any, options: IOptions, callback: seedCallback): void;
-  export function seed(data: any, callback: seedCallback): void;
-
-  export function seed(data: any, options: IOptions): Q.Promise<any>;
+export interface IOptions {
+  dropDatabase?: boolean;
+  dropCollections?: boolean;
 }
+
+type seedCallback = (err: any, dbData: any) => void;
+
+export function seed(data: any, options: IOptions, callback: seedCallback): void;
+export function seed(data: any, callback: seedCallback): void;
+
+export function seed(data: any, options: IOptions): Q.Promise<any>;
