0e32d0248fc86ab2da8aec4563ec2106afc73240
Joshua DeVinney <josh@sal.tt>
2016-10-04 14:32:26 -0500

    Adding search to Loggly typing and updating to 1.1

--- loggly/index.d.ts
+++ loggly/index.d.ts
@@ -1,23 +1,66 @@
-// Type definitions for loggly 1.0.8
-// Project: https://github.com/nodejitsu/node-loggly
-// Definitions by: Ray Martone <https://github.com/rmartone>
+// Type definitions for loggly 1.1
+// Project: https://github.com/winstonjs/node-loggly
+// Definitions by: Ray Martone <https://github.com/rmartone>, Joshua DeVinney <https://github.com/geoffreak>
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
 
-export interface LogglyOptions {
-    token: string;
-    subdomain: string;
-    tags?: string[];
-    json?: boolean;
-    host?: string;
-    auth?: {
-        username: string;
-        password: string;
+declare namespace Loggly {
+    export interface LogglyOptions {
+        token: string;
+        subdomain: string;
+        tags?: string[];
+        json?: boolean;
+        host?: string;
+        auth?: {
+            username: string;
+            password: string;
+        }
+    }
+
+    interface SharedSearchOptions {
+        from?: string;
+        until?: string;
+        size?: number;
+        rows?: number;
+    }
+    export interface SearchOptionsWithQ extends SharedSearchOptions {
+        q: string;
+    }
+    export interface SearchOptionsWithQuery extends SharedSearchOptions {
+        query: string;
+    }
+    export type SearchOptions = SearchOptionsWithQ | SearchOptionsWithQuery;
+
+    export interface SearchResults {
+        events: SearchResultsEvent[];
+        total_events: number;
+        page: number;
+    }
+    export interface SearchResultsEvent {
+        id: string;
+        logtypes: string[];
+        event: any;
+        logmsg: string;
+        raw: string;
+        timestamp: number;
+        tags: string[];
+    }
+
+    export interface Loggly {
+        createClient(options: LogglyOptions): LogglyInstance;
+        version: number;
+    }
+
+    export interface LogglyInstance {
+        log(message: any, tags?: string[], callback?: (err: any, results: any) => void): this;
+        log(message: any, callback?: (err: any, results: any) => void): this;
+        search(query: string, callback?: (err: any, results: SearchResults) => void): Search;
+        search(options: SearchOptions, callback?: (err: any, results: SearchResults) => void): Search;
+    }
+
+    export interface Search {
+        run(callback: (err: any, results: SearchResults) => void): this;
     }
 }
 
-export interface Loggly {
-    log(message: any, tags?: string[], callback?: (err: any, results: any) => void): void;
-    log(message: any, callback?: (err: any, results: any) => void): void;
-}
-
-export function createClient(options: LogglyOptions): Loggly;
+declare var Loggly: Loggly.Loggly;
+export = Loggly;
