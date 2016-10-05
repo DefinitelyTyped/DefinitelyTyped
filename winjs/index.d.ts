2737acb54004dd7865bee5af75b0f47fcc2c95fa
Mohamed Hegazy <mhegazy@microsoft.com>
2016-10-05 01:52:35 -0700

    Update WinJS QueryCollection defintion to correctelly implement Array (#11719)

--- winjs/index.d.ts
+++ winjs/index.d.ts
@@ -10019,9 +10019,33 @@ declare namespace WinJS.Utilities {
 
         /**
          * Calls a defined callback function on each element of an array, and returns an array that contains the results.
-         * @param callbackfn A function that accepts up to three arguments. The map method calls the callbackfn function one time for each element in the array.
-         * @param thisArg An object to which the this keyword can refer in the callbackfn function. If thisArg is omitted, undefined is used as the this value.
-        **/
+        * @param callbackfn A function that accepts up to three arguments. The map method calls the callbackfn function one time for each element in the array.
+        * @param thisArg An object to which the this keyword can refer in the callbackfn function. If thisArg is omitted, undefined is used as the this value.
+        */
+        map<U>(this: [T, T, T, T, T], callbackfn: (value: T, index: number, array: T[]) => U, thisArg?: any): [U, U, U, U, U];
+        /**
+         * Calls a defined callback function on each element of an array, and returns an array that contains the results.
+        * @param callbackfn A function that accepts up to three arguments. The map method calls the callbackfn function one time for each element in the array.
+        * @param thisArg An object to which the this keyword can refer in the callbackfn function. If thisArg is omitted, undefined is used as the this value.
+        */
+        map<U>(this: [T, T, T, T], callbackfn: (value: T, index: number, array: T[]) => U, thisArg?: any): [U, U, U, U];
+        /**
+         * Calls a defined callback function on each element of an array, and returns an array that contains the results.
+        * @param callbackfn A function that accepts up to three arguments. The map method calls the callbackfn function one time for each element in the array.
+        * @param thisArg An object to which the this keyword can refer in the callbackfn function. If thisArg is omitted, undefined is used as the this value.
+        */
+        map<U>(this: [T, T, T], callbackfn: (value: T, index: number, array: T[]) => U, thisArg?: any): [U, U, U];
+        /**
+         * Calls a defined callback function on each element of an array, and returns an array that contains the results.
+        * @param callbackfn A function that accepts up to three arguments. The map method calls the callbackfn function one time for each element in the array.
+        * @param thisArg An object to which the this keyword can refer in the callbackfn function. If thisArg is omitted, undefined is used as the this value.
+        */
+        map<U>(this: [T, T], callbackfn: (value: T, index: number, array: T[]) => U, thisArg?: any): [U, U];
+        /**
+         * Calls a defined callback function on each element of an array, and returns an array that contains the results.
+        * @param callbackfn A function that accepts up to three arguments. The map method calls the callbackfn function one time for each element in the array.
+        * @param thisArg An object to which the this keyword can refer in the callbackfn function. If thisArg is omitted, undefined is used as the this value.
+        */
         map<U>(callbackfn: (value: T, index: number, array: T[]) => U, thisArg?: any): U[];
 
         /**
