b413a61916af22320b1b925921c8c8f0c110925e
Michael Jerred <mjerred.work@gmail.com>
2016-10-06 12:33:22 +0100

    Definitions for send

---
+++ send/send-tests.ts
@@ -0,0 +1,33 @@
+/// <reference path="send.d.ts" />
+/// <reference path="../express/express.d.ts" />
+
+import * as express from 'express';
+import * as send from 'send';
+
+var app = express();
+
+app.get('/test.html', (req, res) => {
+    send(req, '/test.html', {
+        maxAge: 0,
+        root: __dirname + '/wwwroot'
+    }).pipe(res);
+});
+
+app.get('/test.html', (req, res) => {
+    send(req, '/test.html')
+        .maxage(0)
+        .root(__dirname + '/wwwroot')
+        .on('error', (err: any) => {
+            res.statusCode = err.status || 500;
+            res.end(err.message);
+        })
+        .on('directory', () => {
+            res.statusCode = 301;
+            res.setHeader('Location', req.url + '/');
+            res.end('Redirecting to ' + req.url + '/');
+        })
+        .on('headers', (res: any, path: string, stat: any) => {
+            res.setHeader('Content-Disposition', 'attachment');
+        })
+        .pipe(res);
+});
