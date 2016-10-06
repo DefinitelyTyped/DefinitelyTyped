a2eaecb1ae34263d5a19237a013f7d2be4138825
Mohamed Hegazy <mhegazy@microsoft.com>
2016-10-06 09:43:47 -0700

    Make moment dependency use open ranges (#11547)
    * Make moment dependency use `>` instead of fixed version
    
    * Remove unused file
    
    * Use moment.MomentFormatSpecification
    
    * Add dependency on moment

--- moment-timezone/index.d.ts
+++ moment-timezone/index.d.ts
@@ -20,19 +20,16 @@ declare namespace MomentTimezone {
         parse(timestamp: number): number
     }
 
-    type MomentFormatSpecification =
-        string | (() => void) | (string | (() => void))[];
-
     interface MomentTimezone {
         (): moment.Moment;
         (timezone: string): moment.Moment;
         (date: number, timezone: string): moment.Moment;
         (date: number[], timezone: string): moment.Moment;
         (date: string, timezone: string): moment.Moment;
-        (date: string, format: MomentFormatSpecification, timezone: string): moment.Moment;
-        (date: string, format: MomentFormatSpecification, strict: boolean, timezone: string): moment.Moment;
-        (date: string, format: MomentFormatSpecification, language: string, timezone: string): moment.Moment;
-        (date: string, format: MomentFormatSpecification, language: string, strict: boolean, timezone: string): moment.Moment;
+        (date: string, format: moment.MomentFormatSpecification, timezone: string): moment.Moment;
+        (date: string, format: moment.MomentFormatSpecification, strict: boolean, timezone: string): moment.Moment;
+        (date: string, format: moment.MomentFormatSpecification, language: string, timezone: string): moment.Moment;
+        (date: string, format: moment.MomentFormatSpecification, language: string, strict: boolean, timezone: string): moment.Moment;
         (date: Date, timezone: string): moment.Moment;
         (date: moment.Moment, timezone: string): moment.Moment;
         (date: Object, timezone: string): moment.Moment;
