b526380bf393a70afea9c5dd04e3931d205df5db
MikeMatusz <mikematusz@gmail.com>
2016-10-06 08:29:19 -0400

    Adding version number.

--- timelinejs3/timelinejs3.d.ts
+++ timelinejs3/timelinejs3.d.ts
@@ -1,4 +1,4 @@
-// Type definitions for timelinejs3
+// Type definitions for TimelineJS3 v3.4.1
 // Project: https://github.com/NUKnightLab/TimelineJS3
 // Definitions by: Michael Matuszak <https://github.com/MikeMatusz>
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
