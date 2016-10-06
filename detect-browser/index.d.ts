b04cd212f6a4d71a3b62a77235d9229e0c72e5da
Rogier Schouten <r.schouten@spiritit.com>
2016-10-06 14:26:09 +0200

    Add typings for detect-browser

---
+++ detect-browser/index.d.ts
@@ -0,0 +1,17 @@
+// Type definitions for detect-browser v1.3.3
+// Project: https://github.com/DamonOehlman/detect-browser
+// Definitions by: Rogier Schouten <https://github.com/rogierschouten>
+// Definitions: https://github.com/borisyankov/DefinitelyTyped
+
+declare module "detect-browser" {
+
+	/**
+	 * Browser name
+	 */
+	export const name: string;
+	
+	/**
+	 * Browser version
+	 */
+	export const version: string;
+}
