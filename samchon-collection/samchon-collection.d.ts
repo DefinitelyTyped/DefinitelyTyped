028e92ff09bee281b53755ab162cfcbf0815f701
Jeongho Nam <samchon@samchon.org>
2016-10-06 17:03:15 +0900

    TypeScript-STL v1.1.2 & Samchon-Framework v2-gamma.3

--- samchon-collection/samchon-collection.d.ts
+++ samchon-collection/samchon-collection.d.ts
@@ -1,4 +1,4 @@
-// Type definitions for Samchon Collection v0.0.2
+// Type definitions for Samchon Collection v0.0.4
 // Project: https://github.com/samchon/framework
 // Definitions by: Jeongho Nam <http://samchon.org>
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
@@ -7,7 +7,7 @@
 
 declare module "samchon-collection"
 {
-	import collection = samchon.collection;
-	export = collection;
+	import collections = samchon.collections;
+	export = collections;
 }
 
