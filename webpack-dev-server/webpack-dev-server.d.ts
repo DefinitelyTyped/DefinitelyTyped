ebec15608260ecb0d096b57728d978c121749439
Mohsen Azimi <me@azimi.me>
2016-10-06 11:53:15 -0700

    Add https option to webpack-deve-server config
    Reference: 
    
    * https://webpack.github.io/docs/webpack-dev-server.html
    * http://stackoverflow.com/a/33190065

--- webpack-dev-server/webpack-dev-server.d.ts
+++ webpack-dev-server/webpack-dev-server.d.ts
@@ -17,6 +17,7 @@ declare module "webpack-dev-server" {
         export interface Configuration {
             contentBase?: string;
             hot?: boolean;
+            https?: boolean;
             historyApiFallback?: boolean;
             compress?: boolean;
             proxy?: any;
