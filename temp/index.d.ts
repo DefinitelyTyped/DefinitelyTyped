62bc03d0624dcbc8d6be1e9ef07499b0ccf7e2e2
Rogier Schouten <r.schouten@spiritit.com>
2016-10-06 14:12:08 +0200

    Improve temp function signatures

--- temp/index.d.ts
+++ temp/index.d.ts
@@ -8,34 +8,45 @@
 import * as temp from "temp";
 import * as fs from "fs";
 
+export interface OpenFile {
+  path: string;
+  fd: number;
+}
+
+export interface Stats {
+  files: number;
+  dirs: number;
+}
+
 export interface AffixOptions {
-    prefix?: string;
-    suffix?: string;
-    dir?: string;
+  prefix?: string;
+  suffix?: string;
+  dir?: string;
 }
 
 export declare var dir: string;
 
 export declare function track(value?: boolean): typeof temp;
 
-export declare function mkdir(affixes: string, callback?: (err: any, dirPath: string) => void): void;
-export declare function mkdir(affixes: AffixOptions, callback?: (err: any, dirPath: string) => void): void;
+export declare function mkdir(affixes?: string, callback?: (err: any, dirPath: string) => void): void;
+export declare function mkdir(affixes?: AffixOptions, callback?: (err: any, dirPath: string) => void): void;
 
-export declare function mkdirSync(affixes: string): string;
-export declare function mkdirSync(affixes: AffixOptions): string;
+export declare function mkdirSync(affixes?: string): string;
+export declare function mkdirSync(affixes?: AffixOptions): string;
 
-export declare function open(affixes: string, callback?: (err: any, result: { path: string, fd: number }) => void): void;
-export declare function open(affixes: AffixOptions, callback?: (err: any, result: { path: string, fd: number }) => void): void;
+export declare function open(affixes?: string, callback?: (err: any, result: OpenFile) => void): void;
+export declare function open(affixes?: AffixOptions, callback?: (err: any, result: OpenFile) => void): void;
 
-export declare function openSync(affixes: string): { path: string, fd: number };
-export declare function openSync(affixes: AffixOptions): { path: string, fd: number };
+export declare function openSync(affixes?: string): OpenFile;
+export declare function openSync(affixes?: AffixOptions): OpenFile;
 
-export declare function path(affixes: string, defaultPrefix?: string): string;
-export declare function path(affixes: AffixOptions, defaultPrefix?: string): string;
+export declare function path(affixes?: string, defaultPrefix?: string): string;
+export declare function path(affixes?: AffixOptions, defaultPrefix?: string): string;
 
-export declare function cleanup(callback?: (result: boolean | { files: number, dirs?: number }) => void): void;
+export declare function cleanup(callback?: (result: boolean | Stats) => void): void;
 
-export declare function cleanupSync(): boolean | { files: number, dirs: number };
+export declare function cleanupSync(): boolean | Stats;
 
-export declare function createWriteStream(affixes: string): fs.WriteStream;
-export declare function createWriteStream(affixes: AffixOptions): fs.WriteStream;
+export declare function createWriteStream(affixes?: string): fs.WriteStream;
+export declare function createWriteStream(affixes?: AffixOptions): fs.WriteStream;
+
