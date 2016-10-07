043d54faadacf35489a85f3ba8d3f6630fa52a1d
Rogier Schouten <r.schouten@spiritit.com>
2016-10-07 09:55:27 +0200

    process review comment

--- ent/index.d.ts
+++ ent/index.d.ts
@@ -4,36 +4,33 @@
 // Definitions: https://github.com/borisyankov/DefinitelyTyped
 
 /// <reference types="node" />
-declare module "ent" {
+
+/**
+ * Options for the encode() function
+ */
+export interface EntOpts {
+	/**
+	 * If opts.numeric is false or opts.named is true, encoding will used named codes like &pi;.
+	 */
+	numeric?: boolean;
 
 	/**
-	 * Options for the encode() function
+	 * If opts.numeric is false or opts.named is true, encoding will used named codes like &pi;.
 	 */
-	export interface EntOpts {
-		/**
-		 * If opts.numeric is false or opts.named is true, encoding will used named codes like &pi;.
-		 */
-		numeric?: boolean;
-
-		/**
-		 * If opts.numeric is false or opts.named is true, encoding will used named codes like &pi;.
-		 */
-		named?: boolean;
-		
-		/**
-		 * If opts.special is set to an Object, the key names will be forced to be encoded (defaults to forcing: <>'"&)
-		 */
-		special?: Object;
-	}
+	named?: boolean;
 	
 	/**
-	 * Escape unsafe characters in str with html entities. By default, entities are encoded with numeric decimal codes
+	 * If opts.special is set to an Object, the key names will be forced to be encoded (defaults to forcing: <>'"&)
 	 */
-	export function encode(s: string, opts?: EntOpts): string;
-	
-	/**
-	 * Convert html entities in str back to raw text.
-	 */
-	export function decode(s: string): string;
+	special?: Object;
 }
 
+/**
+ * Escape unsafe characters in str with html entities. By default, entities are encoded with numeric decimal codes
+ */
+export function encode(s: string, opts?: EntOpts): string;
+
+/**
+ * Convert html entities in str back to raw text.
+ */
+export function decode(s: string): string;
