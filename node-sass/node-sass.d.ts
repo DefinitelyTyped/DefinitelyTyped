bf649a02607c58f38cba515fa1f8c36843a4cf37
Marvin Hagemeister <marvin.hagemeister@sevenval.com>
2016-10-08 17:33:25 +0200

    Add library version

--- node-sass/node-sass.d.ts
+++ node-sass/node-sass.d.ts
@@ -1,4 +1,4 @@
-// Type definitions for Node Sass
+// Type definitions for Node Sass v3.10.1
 // Project: https://github.com/sass/node-sass
 // Definitions by: Asana <https://asana.com>
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
