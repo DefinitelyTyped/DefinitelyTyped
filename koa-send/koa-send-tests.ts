ffe9435d242a3735240b07289599c82a73c31449
Peter Safranek <pe8ter@mac.com>
2016-10-08 01:14:55 -0700

    Add type definitions for koa-send.

---
+++ koa-send/koa-send-tests.ts
@@ -0,0 +1,23 @@
+/// <reference path="../koa/koa.d.ts" />
+/// <reference path="koa-send.d.ts" />
+
+import * as Koa from "koa";
+import * as send from "koa-send";
+
+const app = new Koa();
+
+app.use(async (ctx: Koa.Context) => {
+    const path: string = await send(ctx, 'stimpy.html');
+});
+
+app.use(async (ctx: Koa.Context) => {
+    await send(ctx, 'stimpy.html', {
+        root: '../static-files',
+        index: 'index.html',
+        maxAge: 10,
+        hidden: true,
+        format: true,
+        gzip: true,
+        setHeaders: () => {},
+    });
+});
