30c8746603c4881b0fcdf14703e89f47077173c3
rhysd <lin90162@yahoo.co.jp>
2016-10-09 18:28:18 +0900

    Add a missing header

--- menubar/menubar.d.ts
+++ menubar/menubar.d.ts
@@ -1,3 +1,8 @@
+// Type definitions for menubar 5.1.0
+// Project: https://github.com/maxogden/menubar
+// Definitions by: rhysd <https://rhysd.github.io>
+// Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
+
 declare namespace Menubar {
 	type Position
 		= "trayLeft"
