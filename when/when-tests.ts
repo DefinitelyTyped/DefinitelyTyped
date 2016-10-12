464c46853c72dce3ab43e2b16659cbde628bc506
Cain Cresswell-Miley <cainjonm@gmail.com>
2016-10-13 01:57:24 +1300

    add when.reduce, when.reduceRight, and when.map typings (#11894)

--- when/when-tests.ts
+++ when/when-tests.ts
@@ -96,6 +96,26 @@ when.all<number[]>([when(1), when(2), when(3)]).then(results => {
 	return results.reduce((r, x) => r + x, 0);
 });
 
+/* when.map(arr, fn) */
+when.map<number[]>([when(1), 2, 3], (num: number, index: number) => num * index).then((results) => {
+	return results.reduce((r, x) => r + x, 0);
+});
+
+/* when.reduce(arr, reduceFunc, initialValue) */
+when.reduce<number>([when(1), 2, 3], (reduction: number, value: number) => {
+	return reduction += value;
+}, 0).then((result: number) => {
+	return result;
+});
+
+/* when.reduceRight(arr, reduceFunc, initialValue) */
+when.reduceRight<number>([when(1), 2, 3], (reduction: number, value: number) => {
+	return when(value)
+	.then((v) => reduction += v);
+}, 0).then((result: number) => {
+	return result;
+});
+
 /* when.settle(arr) */
 when.settle<number>([when(1), when(2), when.reject(new Error("Foo"))]).then(descriptors => {
 	return descriptors.filter(d => d.state === 'rejected').reduce((r, d) => r + d.value, 0);
