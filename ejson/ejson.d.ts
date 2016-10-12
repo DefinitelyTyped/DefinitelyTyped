870a8af6495d1c7cf75c906148a7005972cad819
Shantanu Bhadoria <shantanu@cpan.org>
2016-10-12 16:58:46 +0800

    Adding my first EJSON d.ts file, Will test against my own setup first before creating a merge request

---
+++ ejson/ejson.d.ts
@@ -0,0 +1,8 @@
+// Type definitions for ejson v2.1.2
+// Project: https://www.npmjs.com/package/ejson
+// Definitions by: Shantanu Bhadoria <https://github.com/shantanubhadoria>
+// Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
+
+declare module "EJSON" {
+  function stringify(): string;
+}
