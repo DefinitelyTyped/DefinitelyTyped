38b0ab033953b92635364f23ab03f9598d48fbc2
Shantanu Bhadoria <shantanu@cpan.org>
2016-10-12 18:21:37 +0800

    Finished tests, pre-merge-request commit for EJSON types

--- ejson/ejson.d.ts
+++ ejson/ejson.d.ts
@@ -3,6 +3,22 @@
 // Definitions by: Shantanu Bhadoria <https://github.com/shantanubhadoria>
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
 
-declare module "EJSON" {
-  function stringify(): string;
+declare interface IStringifyOptions {
+  canonical: boolean;
+  indent: boolean|number|string;
+}
+declare interface ICloneOptions {
+  keyOrderSensitive: boolean;
+}
+
+declare module "ejson" {
+  function clone<T>(obj: T): T;
+  function parse(str: string): any;
+  function stringify(obj: any, options?: IStringifyOptions): string;
+
+  function toJSONValue(obj: any): string;
+  function fromJSONValue(obj: string): any;
+  function isBinary(value: any): boolean;
+  function newBinary(len: number): Uint8Array;
+  function equals(a: any, b: any, options?: ICloneOptions): boolean;
 }
