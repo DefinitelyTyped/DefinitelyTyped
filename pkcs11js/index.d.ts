ee07e3b9279f44ef2911ec979ab25df346983761
microshine <microshine@mail.ru>
2016-10-13 21:26:15 +0300

    fix type error for Mechanism handle

--- pkcs11js/index.d.ts
+++ pkcs11js/index.d.ts
@@ -206,17 +206,17 @@ export class PKCS11 {
      * Obtains a list of mechanism types supported by a token 
      * 
      * @param {Handle} slot ID of token's slot
-     * @returns {Handle[]} Gets mech. array
+     * @returns {number[]} Gets mech. array
      */
-    C_GetMechanismList(slot: Handle): Handle[];
+    C_GetMechanismList(slot: Handle): number[];
     /**
      * Obtains information about a particular mechanism possibly supported by a token 
      * 
      * @param {Handle} slot ID of the token's slot
-     * @param {Handle} mech Type of mechanism
+     * @param {number} mech Type of mechanism
      * @returns {MechanismInfo} Receives mechanism info
      */
-    C_GetMechanismInfo(slot: Handle, mech: Handle): MechanismInfo;
+    C_GetMechanismInfo(slot: Handle, mech: number): MechanismInfo;
 
     /* Session management */
 
