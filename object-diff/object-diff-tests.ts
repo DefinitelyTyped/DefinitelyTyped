15a09a23f3fe4d2b752c5b2f9a2ffd7f3c8b193a
Rogier Schouten <r.schouten@spiritit.com>
2016-10-14 10:37:09 +0200

    Add typings for object-diff.

---
+++ object-diff/object-diff-tests.ts
@@ -0,0 +1,20 @@
+
+import diff = require('object-diff');
+
+// from the package README.md, typescriptified:
+
+var a = {
+    speed: 4,
+    power: 54,
+    height: undefined as number,
+    level: 1,
+};
+ 
+var b = {
+    speed: 4,			// unchanged 
+    power: 22,			// changed 
+    level: undefined as number,	// changed 
+    weight: 10,			// added 
+};
+ 
+var d: any = diff(a, b);
