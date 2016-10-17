bc1e923e7310bbd62b716d96e32b27bf3817b092
Dave Alden <dpa99c@gmail.com>
2016-10-17 09:12:28 +0100

    Fix version naming conventions

--- uk.co.workingedge.phonegap.plugin.istablet/uk.co.workingedge.phonegap.plugin.istablet.d.ts
+++ uk.co.workingedge.phonegap.plugin.istablet/uk.co.workingedge.phonegap.plugin.istablet.d.ts
@@ -1,4 +1,4 @@
-﻿// Type definitions for uk.co.workingedge.phonegap.plugin.istablet@1.1.0
+// Type definitions for uk.co.workingedge.phonegap.plugin.istablet v1.1.0
 // Project: https://github.com/dpa99c/phonegap-istablet
 // Definitions by: Dave Alden <https://github.com/dpa99c/>
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
