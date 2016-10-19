64b489824691c05994468bf475d7fba2273c21e3
Christian Rackerseder <git@echooff.de>
2016-10-19 16:04:00 +0200

    Fixed types for ora v0.3.0

--- ora/index.d.ts
+++ ora/index.d.ts
@@ -1,25 +1,37 @@
-// Type definitions for ora
+// Type definitions for ora v0.3.0
 // Project: https://github.com/sindresorhus/ora
-// Definitions by: Basarat Ali Syed <https://github.com/basarat/>
+// Definitions by: Christian Rackerseder <https://github.com/screendriver/>
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
 
+/// <reference types="node" />
 
-type Color = 'black' | 'red' | 'green' | 'yellow' | 'blue' | 'magenta' | 'cyan' | 'white' | 'gray';
-type Text = string;
-interface Options {
-    text?: Text;
-    spinner?: string | { interval?: number; frames: string[]; }
-    color?: Color;
-    stream?: any;
+declare module "ora" {
+    type Color = 'black' | 'red' | 'green' | 'yellow' | 'blue' | 'magenta' | 'cyan' | 'white' | 'gray';
+    type Text = string;
+    interface Options {
+        text?: Text;
+        spinner?: string | Spinner;
+        color?: Color;
+        interval?: number;
+        stream?: NodeJS.WritableStream;
+        enabled?: boolean;
+    }
+    interface Spinner {
+        interval?: number;
+        frames: string[];
+    }
+    interface Instance {
+        start(): Instance;
+        stop(): Instance;
+        succeed(): Instance;
+        fail(): Instance;
+        stopAndPersist(symbol?: string): Instance;
+        clear(): Instance;
+        render(): Instance;
+        frame(): Instance;
+        text: string;
+        color: Color;
+    }
+    function ora(options: Options | Text): Instance;
+    export = ora;
 }
-interface Instance {
-    start(): void;
-    stop(): void;
-    clear(): void;
-    frame(): void;
-    render(): void;
-    text: Text;
-    color: Color;
-}
-declare function ora(options: Options | Text): Instance;
-export = ora;
