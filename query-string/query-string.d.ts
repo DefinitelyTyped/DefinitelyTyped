60993e39dcf02791ba4f2a71d72e4f3f4b3133ce
Mohsen Azimi <me@azimi.me>
2016-10-20 00:22:45 -0700

    add encode

--- query-string/query-string.d.ts
+++ query-string/query-string.d.ts
@@ -5,20 +5,22 @@
 
 declare module "query-string" {
     type primitive = string | boolean | number;
+                
+    interface IStringifyOptions { strict: boolean; encode: boolean; }
 
     /**
      * Parse a query string into an object.
      * Leading ? or # are ignored, so you can pass location.search or location.hash directly.
      * @param str
      */
-    export function parse(str: string): {[key: string]: string | string[]};
+    export function parse(str: string): { [key: string]: string | string[] };
 
     /**
      * Stringify an object into a query string, sorting the keys.
      *
      * @param obj
      */
-    export function stringify(obj: {[key: string]: primitive | primitive[]}, options?: { strict: boolean }): string;
+    export function stringify(obj: { [key: string]: primitive | primitive[] }, options?: IStringifyOptions): string;
 
     /**
      * Extract a query string from a URL that can be passed into .parse().
