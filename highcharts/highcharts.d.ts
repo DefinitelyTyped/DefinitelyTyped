0411404973c4853a67af6be364bcaae68ee06751
nathandao <nathan@guynathan.com>
2016-10-22 00:04:12 +0300

    change redraw in update to optional

--- highcharts/highcharts.d.ts
+++ highcharts/highcharts.d.ts
@@ -5958,7 +5958,7 @@ declare namespace __Highcharts {
          * @param [boolean] redraw Whether to redraw the chart. Defaults to true.
          * @since 5.0.0
          */
-        update(options: ChartOptions, redraw: boolean): void;
+        update(options: ChartOptions, redraw?: boolean): void;
         /**
          * This method is deprecated as of 2.0.1. Updating the chart position after a move operation is no longer necessary.
          * @since 1.2.5
