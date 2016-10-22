36c3768375a051bd04a91fd15892e7b20a5e800f
Cyril Schumacher <cyril_schumacher@live.fr>
2016-10-22 16:49:25 +0200

    Improve test.

--- nodemailer-stub-transport/nodemailer-stub-transport-tests.ts
+++ nodemailer-stub-transport/nodemailer-stub-transport-tests.ts
@@ -6,3 +6,5 @@ import stubTransport = require("nodemailer-stub-transport");
 var transport = nodemailer.createTransport(stubTransport());
 nodemailer.createTransport(stubTransport({ error: new Error('Invalid recipient') }));
 transport = nodemailer.createTransport(stubTransport({ keepBcc: true }));
+
+nodemailer.createTransport(stubTransport());
