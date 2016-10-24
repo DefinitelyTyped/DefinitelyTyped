99beff1cd51358b68aa7ab4e87252fa08f0e2773
akarienta <akarienta@gmail.com>
2016-10-24 12:37:45 +0200

    Typings for com.darktalker.cordova.screenshot
    - correction of parameters

--- com.darktalker.cordova.screenshot/index.d.ts
+++ com.darktalker.cordova.screenshot/index.d.ts
@@ -14,7 +14,7 @@ declare namespace com.darktalker.cordova.screenshot {
          * @param errorMessage message of error, null if there isn't any
          * @param successObject object which contains path to the file
          */
-        (errorMessage: string, successObject: SaveSuccessObject): void;
+        (errorMessage?: string, successObject?: SaveSuccessObject): void;
     }
 
     export interface URISuccessObject {
@@ -26,27 +26,27 @@ declare namespace com.darktalker.cordova.screenshot {
          * @param errorMessage message of error, null if there isn't any
          * @param successObject object which contains data URI of taken screenshot
          */
-        (errorMessage: string, successObject: URICallback): void;
+        (errorMessage?: string, successObject?: URICallback): void;
     }
 
     interface Plugin {
         /**
          * Takes a screenshot, saves it to device as JPG and provides a path to the file.
          *
-         * @param SaveCallback callback function, holds results
+         * @param saveCallback callback function, holds results
          * @param format format of to be taken image, possible values are 'jpg' and 'png' ('jpg' is default)
          * @param quality custom quality of to be taken image in percentage (100 is default)
          * @param filename custom filename of to be taken image, ('screenshot_<milliSecondsSince1970>.<format>' is default)
          */
-        save(SaveCallback, format?: string, quality?: number, filename?: string): void;
+        save(saveCallback: SaveCallback, format?: string, quality?: number, filename?: string): void;
 
         /**
          * Takes a screenshot and provides it trough data URI as JPG. No data are saved in the device.
          *
-         * @param URICallback callback function, holds results
+         * @param uriCallback callback function, holds results
          * @param quality custom quality of to be taken image in percentage (100 is default)
          */
-        URI(URICallback, quality?: number): void;
+        URI(uriCallback: URICallback, quality?: number): void;
     }
 }
 
