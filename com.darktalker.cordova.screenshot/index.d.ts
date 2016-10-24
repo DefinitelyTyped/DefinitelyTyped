5db30ad132ad61194fd719be4733d64890d7f849
akarienta <akarienta@gmail.com>
2016-10-24 16:50:02 +0200

    Typings for com.darktalker.cordova.screenshot
    - correction of callbacks

--- com.darktalker.cordova.screenshot/index.d.ts
+++ com.darktalker.cordova.screenshot/index.d.ts
@@ -1,34 +1,9 @@
-// Type definitions for com.darktalker.cordova.screenshot 0.1.5
+// Type definitions for com.darktalker.cordova.screenshot v0.1.5
 // Project: https://github.com/gitawego/cordova-screenshot
 // Definitions by: Ivana Dolezalova <https://github.com/akarienta>
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
 
 declare namespace com.darktalker.cordova.screenshot {
-    export interface SaveSuccessObject {
-        success: boolean;
-        filePath: string;
-    }
-
-    interface SaveCallback {
-        /**
-         * @param errorMessage message of error, null if there isn't any
-         * @param successObject object which contains path to the file
-         */
-        (errorMessage?: string, successObject?: SaveSuccessObject): void;
-    }
-
-    export interface URISuccessObject {
-        URI: string;
-    }
-
-    interface URICallback {
-        /**
-         * @param errorMessage message of error, null if there isn't any
-         * @param successObject object which contains data URI of taken screenshot
-         */
-        (errorMessage?: string, successObject?: URICallback): void;
-    }
-
     interface Plugin {
         /**
          * Takes a screenshot, saves it to device as JPG and provides a path to the file.
@@ -38,7 +13,11 @@ declare namespace com.darktalker.cordova.screenshot {
          * @param quality custom quality of to be taken image in percentage (100 is default)
          * @param filename custom filename of to be taken image, ('screenshot_<milliSecondsSince1970>.<format>' is default)
          */
-        save(saveCallback: SaveCallback, format?: string, quality?: number, filename?: string): void;
+
+        save(saveCallback: (errorMessage: string, successObject: {success: boolean, filePath: string}) => void,
+             format?: string,
+             quality?: number,
+             filename?: string): void;
 
         /**
          * Takes a screenshot and provides it trough data URI as JPG. No data are saved in the device.
@@ -46,7 +25,8 @@ declare namespace com.darktalker.cordova.screenshot {
          * @param uriCallback callback function, holds results
          * @param quality custom quality of to be taken image in percentage (100 is default)
          */
-        URI(uriCallback: URICallback, quality?: number): void;
+        URI(uriCallback: (errorMessage: string, successObject: {URI: string}) => void,
+            quality?: number);
     }
 }
 
