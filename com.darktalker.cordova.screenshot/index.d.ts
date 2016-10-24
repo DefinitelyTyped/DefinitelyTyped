21a2ba0526991622d7c3c99617959ca464069f79
akarienta <akarienta@gmail.com>
2016-10-24 15:20:42 +0200

    Typings for com.darktalker.cordova.screenshot
     - correction of typings header

--- com.darktalker.cordova.screenshot/index.d.ts
+++ com.darktalker.cordova.screenshot/index.d.ts
@@ -1,4 +1,4 @@
-// Type definitions com.darktalker.cordova.screenshot v0.1.5
+// Type definitions for com.darktalker.cordova.screenshot 0.1.5
 // Project: https://github.com/gitawego/cordova-screenshot
 // Definitions by: Ivana Dolezalova <https://github.com/akarienta>
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
