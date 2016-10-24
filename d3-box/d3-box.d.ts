683dcd3fa026a61d365235bce8ad4a40fab31d57
lkchen-x1c <knight0clk@hotmail.com>
2016-10-23 23:36:55 -0400

    add init files

---
+++ d3-box/d3-box.d.ts
@@ -0,0 +1,6 @@
+// Type definitions for d3-slider
+// Project: https://github.com/JacksonGariety/d3-box
+// Definitions by: Linkun Chen <https://github.com/lk-chen>
+// Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
+
+/// <reference path="../d3/d3.d.ts"/>
