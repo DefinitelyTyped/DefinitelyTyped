72812cd17c8a8a90dfa3c6956a593bbd7befe5a9
lkchen-x1c <knight0clk@hotmail.com>
2016-10-24 01:16:44 -0400

    finish .d.ts

--- d3-box/d3-box.d.ts
+++ d3-box/d3-box.d.ts
@@ -4,3 +4,28 @@
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
 
 /// <reference path="../d3/d3.d.ts"/>
+
+declare namespace d3 {
+    export function box(): Box;
+
+    interface Box {
+        (sel: d3.Selection<any>): void;
+        width(): number;
+        width(x: number): Box;
+        height(): number;
+        height(x: number): Box;
+        tickFormat(): (n: number) => string;
+        tickFormat(fun: (n: number) => string): Box;
+        duration(): number;
+        duration(x: number): Box;
+        domain(): () => number[];
+        domain(x: number[]): Box;
+        value(): (d: any) => number;
+        value(x: (d: any) => number): Box;
+        whiskers(): (d: any[]) => number[];
+        whiskers(x: (d: any[]) => number[]): Box;
+        quartiles(): (d: any[]) => number[];
+        quantiles(x: (d: any[]) => number[]): Box;
+    }
+}
+
