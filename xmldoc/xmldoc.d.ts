df0f6c723936537b82e6642202dddfbd138e8d01
Xavier Stouder <xavier@stouder.io>
2016-10-25 09:23:31 +0200

    Fix implicit any

--- xmldoc/xmldoc.d.ts
+++ xmldoc/xmldoc.d.ts
@@ -7,22 +7,22 @@ declare module "xmldoc" {
     export class XmlDocument {
         constructor(xmlString: string);
 
-        eachChild(func: (child: XmlElement, index?: number, array?: XmlElement[]) => void);
+        eachChild(func: (child: XmlElement, index?: number, array?: XmlElement[]) => void): void;
         childNamed(name: string): XmlElement;
         childrenNamed(name: string): XmlElement[];
         childWithAttribute(name: string, value?: string): XmlElement;
         descendantWithPath(path: string): XmlElement;
         valueWithPath(path: string): string;
-        toString(opts?: XmlOptions);
+        toString(opts?: XmlOptions): string;
     }
     export class XmlElement {
-        eachChild(func: (child: XmlElement, index?: number, array?: XmlElement[]) => void);
+        eachChild(func: (child: XmlElement, index?: number, array?: XmlElement[]) => void): void;
         childNamed(name: string): XmlElement;
         childrenNamed(name: string): XmlElement[];
         childWithAttribute(name: string, value?: string): XmlElement;
         descendantWithPath(path: string): XmlElement;
         valueWithPath(path: string): string;
-        toString(opts?: XmlOptions);
+        toString(opts?: XmlOptions): string;
     }
 
     export interface XmlOptions {
