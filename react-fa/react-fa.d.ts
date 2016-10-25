b6f6ba5f89cf1eaf295ca78a0ad74c4dab2457c1
Karol Janyst <lapkom@gmail.com>
2016-10-26 00:29:19 +0900

    Update definitions for react-fa (#12119)

--- react-fa/react-fa.d.ts
+++ react-fa/react-fa.d.ts
@@ -1,28 +1,35 @@
-// Type definitions for react-fa v4.0.0
+// Type definitions for react-fa v4.1.2
 // Project: https://github.com/andreypopp/react-fa
-// Definitions by: Frank Laub <https://github.com/flaub>
+// Definitions by: Frank Laub <https://github.com/flaub>, Karol Janyst <https://github.com/LKay>
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
 
 /// <reference path="../react/react.d.ts" />
 
 declare module "react-fa" {
-	import { ComponentClass, Props } from 'react';
+    import { Component, ClassAttributes } from "react";
 
-	interface IconProps extends Props<Icon> {
-		name: string;
-		className?: string;
-		size?: string;
-		spin?: boolean;
-		rotate?: string;
-		flip?: string;
-		fixedWidth?: boolean;
-		pulse?: boolean;
-		stack?: string;
-		inverse?: boolean;
-	}
+    type IconSize = "lg" | "2x" | "3x" | "4x" | "5x"
 
-	interface Icon extends ComponentClass<IconProps> { }
-	const Icon: Icon;
+    interface IconProps extends ClassAttributes<Icon> {
+        name: string
+        className?: string
+        size?: IconSize
+        rotate?: "45" | "90" | "135" | "180" | "225" | "270" | "315"
+        flip?: "horizontal" | "vertical"
+        fixedWidth?: boolean
+        spin?: boolean
+        pulse?: boolean
+        stack?: "1x" | "2x"
+        inverse?: boolean
+        Component?: string | Function
+    }
+    export class Icon extends Component<IconProps, {}> {}
 
-	export = Icon;
+    interface IconStackProps extends ClassAttributes<IconStack> {
+        className?: string
+        size?: IconSize
+    }
+    export class IconStack extends Component<IconStackProps, {}> {}
+
+    export default Icon
 }
