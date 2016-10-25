dbd5fbc59a3cfe084a06d80b591ed86987234159
Linda_pp <rhysd@users.noreply.github.com>
2016-10-26 00:02:42 +0900

    Add 'react-bytesize-icons' package (#12150)
    * Add 'react-bytesize-icons' package
    
    * Do not use 'import' in augumented module declaration
    
    * Remove non-module namespace

---
+++ react-bytesize-icons/react-bytesize-icons-tests.tsx
@@ -0,0 +1,10 @@
+/// <reference path="../react/react-dom.d.ts" />
+/// <reference path="./react-bytesize-icons.d.ts" />
+
+import * as React from 'react';
+import {render} from 'react-dom';
+import {Activity, External, Export} from 'react-bytesize-icons';
+
+render(<Activity/>, document.getElementById('test'));
+render(<External width={42} color="blue"/>, document.getElementById('test'));
+render(<Export width={42} height={21} strokeWidth="10%" strokeLinejoin="bevel" strokeLinecap="butt" color="blue"/>, document.getElementById('test'));
