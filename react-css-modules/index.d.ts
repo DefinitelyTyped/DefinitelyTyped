1c367f5cad744bdb324cbc4c00b308e7b26c3124
Tadas Dailyda <tadas@dailyda.com>
2016-10-25 18:12:32 +0300

    move 'react-css-modules' declarations to top-level, turn on strictNullChecks

--- react-css-modules/index.d.ts
+++ react-css-modules/index.d.ts
@@ -3,35 +3,30 @@
 // Definitions by: Kostya Esmukov <https://github.com/KostyaEsmukov>, Tadas Dailyda <https://github.com/skirsdeda>
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
 
-import * as React from 'react';
-
-declare module 'react-css-modules' {
-
-    interface TypeOptions {
-        allowMultiple?: boolean;
-        errorWhenNotFound?: boolean;
-    }
-
-    type StylesObject = any;
-
-    interface CSSModules {
-        (defaultStyles: StylesObject, options?: TypeOptions): <C extends Function>(Component: C) => C;
-        <C extends Function>(Component: C, defaultStyles: StylesObject, options?: TypeOptions): C;
-    }
-
-    module CSSModules {
-        // Extend your component's Prop interface with this one to get access to `this.props.styles`
-        //
-        // interface MyComponentProps extends CSSModules.InjectedCSSModuleProps {}
-        interface InjectedCSSModuleProps {
-            styles?: StylesObject;
-        }
-    }
-
-    let CSSModules: CSSModules;
-
-    export = CSSModules;
+interface TypeOptions {
+    allowMultiple?: boolean;
+    errorWhenNotFound?: boolean;
 }
+
+type StylesObject = any;
+
+interface CSSModules {
+    (defaultStyles: StylesObject, options?: TypeOptions): <C extends Function>(Component: C) => C;
+    <C extends Function>(Component: C, defaultStyles: StylesObject, options?: TypeOptions): C;
+}
+
+declare module CSSModules {
+    // Extend your component's Prop interface with this one to get access to `this.props.styles`
+    //
+    // interface MyComponentProps extends CSSModules.InjectedCSSModuleProps {}
+    interface InjectedCSSModuleProps {
+        styles?: StylesObject;
+    }
+}
+
+declare let CSSModules: CSSModules;
+
+export = CSSModules;
 
 declare module 'react' {
     interface HTMLAttributes<T> {
