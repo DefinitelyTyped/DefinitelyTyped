e7a4be348fa0eba18f7bb92cb8ddca417e4bdd00
Bradford Wagner <bradford.wagner@blackrock.com>
2016-10-26 17:10:13 -0400

    refactor interface name in unit test

--- angular-clipboard/angular-clipboard-tests.ts
+++ angular-clipboard/angular-clipboard-tests.ts
@@ -2,7 +2,7 @@
 /// <reference path="../angularjs/angular.d.ts" />
 
 const app = angular.module('testModule', ['angular-clipboard']);
-app.controller('TestController', ($scope: ng.IScope, clipboard: angular.clipboard.Clipboard) => {
+app.controller('TestController', ($scope: ng.IScope, clipboard: angular.clipboard.IClipboardService) => {
 	$scope['testCopy'] = () => {
 		if (clipboard.supported) {
 			clipboard.copyText('hiiiiiii');
