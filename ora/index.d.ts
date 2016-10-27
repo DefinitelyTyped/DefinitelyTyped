bde244c60c867dd3872f6bcddc63ae9fefb7830a
Christian Rackerseder <git@echooff.de>
2016-10-27 11:21:39 +0200

    Removed declare module "ora"

--- ora/index.d.ts
+++ ora/index.d.ts
@@ -5,32 +5,30 @@
 
 /// <reference types="node" />
 
-declare module "ora" {
-    type Color = 'black' | 'red' | 'green' | 'yellow' | 'blue' | 'magenta' | 'cyan' | 'white' | 'gray';
-    interface Options {
-        text?: string;
-        spinner?: string | Spinner;
-        color?: Color;
-        interval?: number;
-        stream?: NodeJS.WritableStream;
-        enabled?: boolean;
-    }
-    interface Spinner {
-        interval?: number;
-        frames: string[];
-    }
-    interface Instance {
-        start(): Instance;
-        stop(): Instance;
-        succeed(): Instance;
-        fail(): Instance;
-        stopAndPersist(symbol?: string): Instance;
-        clear(): Instance;
-        render(): Instance;
-        frame(): Instance;
-        text: string;
-        color: Color;
-    }
-    function ora(options: Options | string): Instance;
-    export = ora;
+type Color = 'black' | 'red' | 'green' | 'yellow' | 'blue' | 'magenta' | 'cyan' | 'white' | 'gray';
+interface Options {
+    text?: string;
+    spinner?: string | Spinner;
+    color?: Color;
+    interval?: number;
+    stream?: NodeJS.WritableStream;
+    enabled?: boolean;
 }
+interface Spinner {
+    interval?: number;
+    frames: string[];
+}
+interface Instance {
+    start(): Instance;
+    stop(): Instance;
+    succeed(): Instance;
+    fail(): Instance;
+    stopAndPersist(symbol?: string): Instance;
+    clear(): Instance;
+    render(): Instance;
+    frame(): Instance;
+    text: string;
+    color: Color;
+}
+declare function ora(options: Options | string): Instance;
+export = ora;
