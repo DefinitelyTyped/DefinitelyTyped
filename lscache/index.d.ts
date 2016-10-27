ef69e889340b035516d5d14102148f7f984ad19f
Steve Jenkins <steve@skysteve.com>
2016-10-27 12:20:38 +0100

    bump version

--- lscache/index.d.ts
+++ lscache/index.d.ts
@@ -1,4 +1,4 @@
-﻿// Type definitions for lscache v1.0.5
+﻿// Type definitions for lscache v1.0.7
 // Project: https://github.com/pamelafox/lscache
 // Definitions by: Chris Martinez <https://github.com/Chris-Martinezz>
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
