b4db553dcfc37fe2e60919e4282b06a7a0777a0c
Willie Streeter <willie.l.streeter@kp.org>
2016-10-28 10:55:58 -0700

    Added type definition 2.0 version for electron-process

---
+++ electron-process/index.d.ts
@@ -0,0 +1,31 @@
+// Type definitions for electron-process v0.1.3
+// Project: https://github.com/smith-kyle/electron-process
+// Definitions by: Will Streeter <https://github.com/WillStreeter>
+// Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
+
+declare  namespace ElectronProcess{
+
+              export interface foreground{
+                   getModule(originalModule:any):any
+               }
+
+              export interface background{
+                   registerModule(backgroundModule:any):void
+               }
+
+              export interface main{
+                  createBackgroundProcess(url:string, debug:boolean):any
+               }
+
+              export interface Processor {
+                 foreground?:foreground,
+                 background?:background,
+                 main?:main
+                }
+}
+
+
+declare module "electron-process" {
+    const electronProcess: ElectronProcess.Processor;
+    export = electronProcess;
+}
