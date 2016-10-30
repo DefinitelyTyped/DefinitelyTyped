a19831d55b0b94b0a3e8430e464d0d708eb7e1b6
Georgii Dolzhykov <thorn.mailbox@gmail.com>
2016-10-30 19:14:46 +0200

    angular: make me a reviewer again

--- angular/index.d.ts
+++ angular/index.d.ts
@@ -1,6 +1,6 @@
 // Type definitions for Angular JS 1.5
 // Project: http://angularjs.org
-// Definitions by: Diego Vilar <http://github.com/diegovilar>
+// Definitions by: Diego Vilar <http://github.com/diegovilar>, Georgii Dolzhykov <http://github.com/thorn0>
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
 
 /// <reference types="jquery" />
