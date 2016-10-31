5b0825a0ac081e41a6c0cccbbab3555fc93bd9f4
Denis Carriere <carriere.denis@gmail.com>
2016-10-31 08:22:10 -0400

    JS2XML removed compact true option

--- xml-js/xml-js-tests.ts
+++ xml-js/xml-js-tests.ts
@@ -1,15 +1,16 @@
 /// <reference path='xml-js.d.ts' />
 import { Element, ElementCompact, xml2json, json2xml } from 'xml-js'
+import * as convert from 'xml-js'
 
 // Declaration
-const declarationCompact1: ElementCompact = { _declaration: { } };
+const declarationCompact1: ElementCompact = { _declaration: { _attributes: { version: 2 } }};
 const declarationCompact2: ElementCompact = { _declaration: { _attributes: { version: '1.0', encoding: 'utf-8' }}};
 const declaration1: Element = { declaration: { }};
 const declaration2: Element = { declaration: { attributes: { version: '1.0', encoding: 'utf-8' }}};
 
 // Comment
 const commentCompact: ElementCompact = { _comment : 'Hello, World!' };
-const comment: Element = {elements: [{ type: 'comment', comment: 'Hello, World!' }]};
+const comment: Element = { elements: [{ type: 'comment', comment: 'Hello, World!' }]};
 
 // CDATA
 const cdataCompact: ElementCompact = { _cdata: '<foo></bar>' };
@@ -17,19 +18,19 @@ const cdata: Element = { elements : [{ type: 'cdata', cdata: '<foo></bar>' }]};
 
 // Element
 const elementCompact1: ElementCompact = { a: {} };
-const element1: Element = {elements:[{ type: 'element', name: 'a' }]};
+const element1: Element = { elements:[{ type: 'element', name: 'a' }]};
 
 const elementCompact2: ElementCompact = { a: { _attributes: { x: '1.234', y:'It\'s' }}};
-const element2: Element = {elements: [{ type: 'element', name: 'a', attributes: { x: '1.234', y: 'It\'s' }}]};
+const element2: Element = { elements: [{ type: 'element', name: 'a', attributes: { x: '1.234', y: 'It\'s' }}]};
 
 const elementCompact3: ElementCompact = { a: { _text: ' Hi ' }};
-const element3: Element = {elements:[{ type: 'element', name: 'a', elements: [{ type: 'text', text: ' Hi ' }]}]};
+const element3: Element = { elements:[{ type: 'element', name: 'a', elements: [{ type: 'text', text: ' Hi ' }]}]};
 
 const elementCompact4: ElementCompact = { a: {}, b: {} };
-const element4: Element = {elements:[{ type: 'element', name: 'a' }, { type: 'element', name: 'b' }]};
+const element4: Element = { elements:[{ type: 'element', name: 'a' }, { type: 'element', name: 'b' }]};
 
 const elementCompact5: ElementCompact = { a: { b: {} }};
-const element5: Element = {elements: [{ type: 'element', name: 'a', elements: [{ type: 'element', name: 'b' }]}]};
+const element5: Element = { elements: [{ type: 'element', name: 'a', elements: [{ type: 'element', name: 'b' }]}]};
 
 // xml2json
 const xml = `
@@ -40,8 +41,8 @@ const xml = `
     <todo>Play</todo>
 </note>`;
 xml2json(xml, { compact: true, spaces: 4 });
-xml2json(xml, { compact: false, spaces: 4 });
+xml2json(xml, { compact: false });
 
 // json2xml
-json2xml(declarationCompact1, { compact: true, spaces: 4 });
-json2xml(elementCompact3, { compact: false, spaces: 4 });
+json2xml({ a: {} }, { compact: true, spaces: 4 });
+json2xml({ elements:[{ type: 'element', name: 'a' }]}, { compact: false });
--- xml-js/xml-js.d.ts
+++ xml-js/xml-js.d.ts
@@ -4,92 +4,80 @@
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
 
 declare module "xml-js" {
-    interface Attribute {
-        [key: string]: string
-    }
-
-    type Types = "comment" | "element" | "cdata" | "text";
-
     export interface ElementCompact {
-        [key: string]: any
-        _attributes?: Attribute
-        _cdata?: string
-        _comment?: string
-        _declaration?: DeclarationCompact
-        _text?: string
+        [key: string]: ElementCompact | string | number
+        _attributes?: {
+            [key: string]: string | number
+        }
+        _cdata?: string | number
+        _comment?: string | number
+        _declaration?: {
+            _attributes?: {
+                version?: string | number
+                encoding?: string | number
+            }
+        }
+        _text?: string | number
     }
 
     export interface Element {
-        attributes?: Attribute
-        cdata?: string
-        comment?: string
-        declaration?: Declaration
-        elements?: Array<Element>
-        text?: string
-        type?: Types
-        name?: string
-    }
-
-    interface DeclarationCompact {
-        _attributes?: {
-            version?: string
-            encoding?: string
-        }
-    }
-
-    interface Declaration {
         attributes?: {
-            version: string
-            encoding: string
+            [key: string]: string | number
+        }
+        cdata?: string | number
+        comment?: string | number
+        declaration?: {
+            attributes?: {
+                version: string | number
+                encoding: string | number
+            }
+        }
+        elements?: Array<Element>
+        text?: string | number
+        type?: string | number
+        name?: string | number
+    }
+
+    namespace Options {
+        interface XML2JS extends ChangingKeyNames, IgnoreOptions {
+            compact?: boolean
+            spaces?: number
+            trim?: boolean
+            sanitize?: boolean
+            nativeType?: boolean
+            addParent?: boolean
+            alwaysChildren?: boolean
+        }
+
+        interface JS2XML extends ChangingKeyNames, IgnoreOptions {
+            spaces?: number
+            compact?: boolean
+            fullTagEmptyElement?: boolean
+        }
+
+        interface IgnoreOptions {
+            ignoreDeclaration?: boolean
+            ignoreAttributes?: boolean
+            ignoreComment?: boolean
+            ignoreCdata?: boolean
+            ignoreText?: boolean
+        }
+
+        interface ChangingKeyNames {
+            declarationKey?: string
+            attributesKey?: string
+            textKey?: string
+            cdataKey?: string
+            commentKey?: string
+            parentKey?: string
+            typeKey?: string
+            nameKey?: string
+            elementsKey?: string
         }
     }
 
-    interface XML2JSOptions extends ChangingKeyNames, IgnoreOptions {
-        compact?: boolean
-        spaces?: number
-        trim?: boolean
-        sanitize?: boolean
-        nativeType?: boolean
-        addParent?: boolean
-        alwaysChildren?: boolean
-    }
-
-    interface JS2XMLOptions extends ChangingKeyNames, IgnoreOptions {
-        spaces?: number
-        compact?: boolean
-        fullTagEmptyElement?: boolean
-    }
-
-    interface IgnoreOptions {
-        ignoreDeclaration?: boolean
-        ignoreAttributes?: boolean
-        ignoreComment?: boolean
-        ignoreCdata?: boolean
-        ignoreText?: boolean
-    }
-
-    interface ChangingKeyNames {
-        declarationKey?: string
-        attributesKey?: string
-        textKey?: string
-        cdataKey?: string
-        commentKey?: string
-        parentKey?: string
-        typeKey?: string
-        nameKey?: string
-        elementsKey?: string
-    }
-
-    type True = true;
-
-    interface JS2XMLOptionsCompact extends JS2XMLOptions {
-        compact: True;
-    }
-
-    export function js2xml(json: ElementCompact, options?: JS2XMLOptionsCompact): string;
-    export function js2xml(json: Element, options?: JS2XMLOptions): string;
-    export function json2xml(json: ElementCompact, options?: JS2XMLOptionsCompact): string;
-    export function json2xml(json: Element, options?: JS2XMLOptions): string;
-    export function xml2json(xml: string, options?: XML2JSOptions): any;
-    export function xml2js(xml: string, options?: XML2JSOptions): any;
+    export function js2xml(json: Element | ElementCompact, options?: Options.JS2XML): string;
+    export function json2xml(json: Element | ElementCompact, options?: Options.JS2XML): string;
+    export function xml2json(xml: string, options?: Options.XML2JS): any;
+    export function xml2js(xml: string, options?: Options.XML2JS): any;
 }
