fdea59a0190baaa293e2d6fc4b375e59d207932e
jeff <jeff@giftbit.com>
2016-10-31 10:15:58 -0700

    Take elasticsearch/elasticsearch-tests.ts back down to 2 space indentation.

--- elasticsearch/elasticsearch-tests.ts
+++ elasticsearch/elasticsearch-tests.ts
@@ -1,240 +1,222 @@
 import elasticsearch = require("elasticsearch");
 
 var client = new elasticsearch.Client({
-    host: 'localhost:9200',
-    log: 'trace'
+  host: 'localhost:9200',
+  log: 'trace'
 });
 
 client = new elasticsearch.Client({
-    hosts: [
-        'box1.server.org',
-        'box2.server.org'
-    ],
-    selector: function(hosts: any) { }
+  hosts: [
+    'box1.server.org',
+    'box2.server.org'
+  ],
+  selector: function(hosts: any) { }
 });
 
 client.ping({
-    requestTimeout: 30000
+  requestTimeout: 30000
 }, function(error) {
 });
 
 client.search({
-    q: 'pants'
+  q: 'pants'
 }).then(function(body) {
-    var hits = body.hits.hits;
+  var hits = body.hits.hits;
 }, function(error) {
 });
 
 client.indices.delete({
-    index: 'test_index',
-    ignore: [404]
+  index: 'test_index',
+  ignore: [404]
 }).then(function(body) {
 }, function(error) {
 });
 
 client.create({
-    id: '123',
-    index: 'index',
-    type: 'type'
+  id: '123',
+  index: 'index',
+  type: 'type'
 });
 
 client.create({
-    id: '123',
-    index: 'index',
-    type: 'type'
+  id: '123',
+  index: 'index',
+  type: 'type'
 }, (err, repsonse, status) => {
 });
 
 client.cluster.getSettings({
-    masterTimeout: 100
+  masterTimeout: 100
 }, (err, response) => {
 });
 
 client.cluster.health({
-    masterTimeout: 100
+  masterTimeout: 100
 }, (err, response) => {
 });
 
 client.cluster.pendingTasks({
-    ignore: 1
+  ignore: 1
 }, (err, response) => {
 });
 
 client.cluster.putSettings({
-    ignore: 1
+  ignore: 1
 }, (err, response) => {
 });
 
 client.cluster.putSettings({
-    ignore: 1
+  ignore: 1
 }, (err, response) => {
 });
 
 client.cluster.reroute({
-    ignore: 1
+  ignore: 1
 }, (err, response) => {
 });
 
 client.cluster.state({
-    ignore: 1
+  ignore: 1
 }, (err, response) => {
 });
 
 client.cluster.stats({
-    ignore: 1
+  ignore: 1
 }, (err, response) => {
 });
 
 client.count({
-    index: 'index_name'
+  index: 'index_name'
 }, function(error, response) {
-    // ...
+  // ...
 });
 
 client.count({
-    index: 'index_name',
-    body: {
-        query: {
-            filtered: {
-                filter: {
-                    terms: {
-                        foo: ['bar']
-                    }
-                }
-            }
+  index: 'index_name',
+  body: {
+    query: {
+      filtered: {
+        filter: {
+          terms: {
+            foo: ['bar']
+          }
         }
+      }
     }
+  }
 }, function(err, response) {
-    // ...
+  // ...
 });
 
 client.explain({
-    // the document to test
-    index: 'myindex',
-    type: 'mytype',
-    id: '1',
+  // the document to test
+  index: 'myindex',
+  type: 'mytype',
+  id: '1',
 
-    // the query to score it against
-    q: 'field:value'
+  // the query to score it against
+  q: 'field:value'
 }, function(error, response) {
-    // ...
+  // ...
 });
 
 client.explain({
-    index: 'myindex',
-    type: 'mytype',
-    id: '1',
-    body: {
-        query: {
-            match: { title: 'test' }
-        }
+  index: 'myindex',
+  type: 'mytype',
+  id: '1',
+  body: {
+    query: {
+      match: { title: 'test' }
     }
+  }
 }, function(error, response) {
-    // ...
+  // ...
 });
 
 client.index({
-    index: 'myindex',
-    type: 'mytype',
-    id: '1',
-    body: {
-        title: 'Test 1',
-        tags: ['y', 'z'],
-        published: true,
-    }
+  index: 'myindex',
+  type: 'mytype',
+  id: '1',
+  body: {
+    title: 'Test 1',
+    tags: ['y', 'z'],
+    published: true,
+  }
 }, function(error, response) {
 
 });
 
 client.mget({
-    body: {
-        docs: [
-            { _index: 'indexA', _type: 'typeA', _id: '1' },
-            { _index: 'indexB', _type: 'typeB', _id: '1' },
-            { _index: 'indexC', _type: 'typeC', _id: '1' }
-        ]
-    }
+  body: {
+    docs: [
+      { _index: 'indexA', _type: 'typeA', _id: '1' },
+      { _index: 'indexB', _type: 'typeB', _id: '1' },
+      { _index: 'indexC', _type: 'typeC', _id: '1' }
+    ]
+  }
 }, function(error, response) {
-    // ...
+  // ...
 });
 
 client.mget({
-    index: 'myindex',
-    type: 'mytype',
-    body: {
-        ids: [1, 2, 3]
-    }
+  index: 'myindex',
+  type: 'mytype',
+  body: {
+    ids: [1, 2, 3]
+  }
 }, function(error, response) {
-    // ...
+  // ...
 });
 
 client.search({
-    index: 'myindex',
-    q: 'title:test'
+  index: 'myindex',
+  q: 'title:test'
 }, function(error, response) {
-    // ...
+  // ...
 });
 
 client.search({
-    index: 'myindex',
-    body: {
-        query: {
-            match: {
-                title: 'test'
-            }
-        },
-        facets: {
-            tags: {
-                terms: {
-                    field: 'tags'
-                }
-            }
+  index: 'myindex',
+  body: {
+    query: {
+      match: {
+        title: 'test'
+      }
+    },
+    facets: {
+      tags: {
+        terms: {
+          field: 'tags'
         }
+      }
     }
+  }
 }, function(error, response) {
-    // ...
+  // ...
 });
 
 client.suggest({
-    index: 'myindex',
-    body: {
-        mysuggester: {
-            text: 'tset',
-            term: {
-                field: 'title'
-            }
-        }
+  index: 'myindex',
+  body: {
+    mysuggester: {
+      text: 'tset',
+      term: {
+        field: 'title'
+      }
     }
+  }
 }, function(error, response) {
-    // response will be formatted like so:
-    //
-    // {
-    //   ...
-    //   mysuggester: [
-    //     {
-    //       text: "tset",
-    //       ...
-    //       options: [
-    //         {
-    //           text: "test",
-    //           score: 0.75,
-    //           freq: 5
-    //         }
-    //       ]
-    //     }
-    //   ]
-    // }
 });
 
 client.indices.updateAliases({
-    body: {
-        actions: [
-            { remove: { index: 'logstash-2014.04', alias: 'logstash-current' } },
-            { add: { index: 'logstash-2014.05', alias: 'logstash-current' } }
-        ]
-    }
+  body: {
+    actions: [
+      { remove: { index: 'logstash-2014.04', alias: 'logstash-current' } },
+      { add: { index: 'logstash-2014.05', alias: 'logstash-current' } }
+    ]
+  }
 }).then(function(response) {
-    // ...
+  // ...
 }, function(error) {
-    // ...
+  // ...
 });
