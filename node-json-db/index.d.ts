1eacd826344b0c309deae7ef1bfe73d95d2ca155
Ilya Kuznetsov <Ilya_Kuznetsov@epam.com>
2016-10-31 15:37:06 +0300

    declaration of module 'node-json-db' was removed

--- node-json-db/index.d.ts
+++ node-json-db/index.d.ts
@@ -3,57 +3,55 @@
 // Definitions by: Ilya Kuznetsov <https://github.com/kuzn-ilya>
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
 
-declare module 'node-json-db' {
-    class JsonDB {
-         /**
-         * Create the JSON database
-         * @param filename where to save the data base
-         * @param saveOnPush saving on modification of the data
-         * @param humanReadable is the json file humand readable 
-         * @returns {JsonDB}
-         * @constructor
-         */
-        constructor(filename: string, saveOnPush?: boolean, humanReadable?: boolean);
-
+declare class JsonDB {
         /**
-         * Get the deta stored in the data base
-         * @param dataPath path leading to the data
-         * @returns {*}
-         */
-        getData(dataPath: string): any;
+     * Create the JSON database
+     * @param filename where to save the data base
+     * @param saveOnPush saving on modification of the data
+     * @param humanReadable is the json file humand readable 
+     * @returns {JsonDB}
+     * @constructor
+     */
+    constructor(filename: string, saveOnPush?: boolean, humanReadable?: boolean);
 
-        /**
-         * Pushing data into the database
-         * @param dataPath path leading to the data
-         * @param data data to push
-         * @param override overriding or not the data, if not, it will merge them
-         */
-        push(dataPath: string, data: any, override?: boolean): void;
+    /**
+     * Get the deta stored in the data base
+     * @param dataPath path leading to the data
+     * @returns {*}
+     */
+    getData(dataPath: string): any;
 
-        /**
-         * Delete the data
-         * @param dataPath path leading to the data
-         */
-        delete(dataPath: string): void;
+    /**
+     * Pushing data into the database
+     * @param dataPath path leading to the data
+     * @param data data to push
+     * @param override overriding or not the data, if not, it will merge them
+     */
+    push(dataPath: string, data: any, override?: boolean): void;
 
-        /**
-         * Reload the database from the file
-         */
-        reload(): void;
+    /**
+     * Delete the data
+     * @param dataPath path leading to the data
+     */
+    delete(dataPath: string): void;
 
-        /**
-         * Manually load the database
-         * It is automatically called when the first getData is done
-         */
-        load(): void;
-                
-        /**
-         * Manually save the database
-         * By default you can't save the database if it's not loaded
-         * @param force force the save of the database
-         */
-        save(force?: boolean): void;
-    }
+    /**
+     * Reload the database from the file
+     */
+    reload(): void;
 
-    export = JsonDB;
+    /**
+     * Manually load the database
+     * It is automatically called when the first getData is done
+     */
+    load(): void;
+            
+    /**
+     * Manually save the database
+     * By default you can't save the database if it's not loaded
+     * @param force force the save of the database
+     */
+    save(force?: boolean): void;
 }
+
+export = JsonDB;
