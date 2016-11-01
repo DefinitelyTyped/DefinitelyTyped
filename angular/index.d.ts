4335497c81958f91326acdb9d50f93f2d2028503
David Schnur <dnschnur@gmail.com>
2016-11-01 05:54:52 -0700

    Add $transclude.isSlotFilled function (#11998)
    This function was added to $transclude when support for multi-slot
    transclusion was added in Angular 1.5.
    
    https://docs.angularjs.org/api/ng/service/$compile#-controller-

--- angular/index.d.ts
+++ angular/index.d.ts
@@ -1227,6 +1227,11 @@ declare namespace angular {
         (scope: IScope, cloneAttachFn: ICloneAttachFunction, futureParentElement?: JQuery, slotName?: string): JQuery;
         // If one argument is provided, then it's assumed to be the cloneAttachFn.
         (cloneAttachFn?: ICloneAttachFunction, futureParentElement?: JQuery, slotName?: string): JQuery;
+
+        /**
+         * Returns true if the specified slot contains content (i.e. one or more DOM nodes)
+         */
+        isSlotFilled(slotName: string): boolean;
     }
 
     ///////////////////////////////////////////////////////////////////////////
