b875b929632dc21b8299f8633a98b7ca0fea5f03
Nicolás Fantone <nfantone@gmail.com>
2016-11-01 23:38:35 +0000

    Update amqplib definitions to v0.5.0

--- amqplib/index.d.ts
+++ amqplib/index.d.ts
@@ -1,13 +1,77 @@
-// Type definitions for amqplib 0.3.x
+// Type definitions for amqplib 0.5.x
 // Project: https://github.com/squaremo/amqp.node
-// Definitions by: Michael Nahkies <https://github.com/mnahkies>, Ab Reitsma <https://github.com/abreits>
+// Definitions by: Michael Nahkies <https://github.com/mnahkies>, Ab Reitsma <https://github.com/abreits>, Nicolás Fantone <https://github.com/nfantone>
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
 
-/// <reference types="when" />
 /// <reference types="node" />
 
-declare module "amqplib/properties" {
-    namespace Replies {
+declare module 'amqplib' {
+    import * as Promise from 'bluebird';
+    import * as events from 'events';
+    import shared = require('amqplib/properties');
+    import callback_api = require('amqplib/callback_api');
+
+    export import Replies = shared.Replies;
+    export import Options = shared.Options;
+    export import Message = shared.Message;
+
+    export interface Connection extends events.EventEmitter {
+        close(): Promise<void>;
+        createChannel(): Promise<Channel>;
+        createConfirmChannel(): Promise<ConfirmChannel>;
+    }
+
+    export interface Channel extends events.EventEmitter {
+        close(): Promise<void>;
+
+        assertQueue(queue: string, options?: Options.AssertQueue): Promise<Replies.AssertQueue>;
+        checkQueue(queue: string): Promise<Replies.AssertQueue>;
+
+        deleteQueue(queue: string, options?: Options.DeleteQueue): Promise<Replies.DeleteQueue>;
+        purgeQueue(queue: string): Promise<Replies.PurgeQueue>;
+
+        bindQueue(queue: string, source: string, pattern: string, args?: any): Promise<Replies.Empty>;
+        unbindQueue(queue: string, source: string, pattern: string, args?: any): Promise<Replies.Empty>;
+
+        assertExchange(exchange: string, type: string, options?: Options.AssertExchange): Promise<Replies.AssertExchange>;
+        checkExchange(exchange: string): Promise<Replies.Empty>;
+
+        deleteExchange(exchange: string, options?: Options.DeleteExchange): Promise<Replies.Empty>;
+
+        bindExchange(destination: string, source: string, pattern: string, args?: any): Promise<Replies.Empty>;
+        unbindExchange(destination: string, source: string, pattern: string, args?: any): Promise<Replies.Empty>;
+
+        publish(exchange: string, routingKey: string, content: Buffer, options?: Options.Publish): boolean;
+        sendToQueue(queue: string, content: Buffer, options?: Options.Publish): boolean;
+
+        consume(queue: string, onMessage: (msg: Message) => any, options?: Options.Consume): Promise<Replies.Consume>;
+
+        cancel(consumerTag: string): Promise<Replies.Empty>;
+        get(queue: string, options?: Options.Get): Promise<Message | boolean>;
+
+        ack(message: Message, allUpTo?: boolean): void;
+        ackAll(): void;
+
+        nack(message: Message, allUpTo?: boolean, requeue?: boolean): void;
+        nackAll(requeue?: boolean): void;
+        reject(message: Message, requeue?: boolean): void;
+
+        prefetch(count: number, global?: boolean): Promise<Replies.Empty>;
+        recover(): Promise<Replies.Empty>;
+    }
+
+    export interface ConfirmChannel extends Channel {
+        publish(exchange:string, routingKey:string, content:Buffer, options?:Options.Publish, callback?:(err:any, ok:Replies.Empty) => void):boolean;
+        sendToQueue(queue:string, content:Buffer, options?:Options.Publish, callback?:(err:any, ok:Replies.Empty) => void):boolean;
+
+        waitForConfirms(): Promise<void>;
+    }
+
+    export function connect(url: string, socketOptions?: any): Promise<Connection>;
+}
+
+declare module 'amqplib/properties' {
+    export namespace Replies {
         interface Empty {
         }
         interface AssertQueue {
@@ -29,7 +93,7 @@ declare module "amqplib/properties" {
         }
     }
 
-    namespace Options {
+    export namespace Options {
         interface AssertQueue {
             exclusive?: boolean;
             durable?: boolean;
@@ -40,6 +104,7 @@ declare module "amqplib/properties" {
             deadLetterExchange?: string;
             deadLetterRoutingKey?: string;
             maxLength?: number;
+            maxPriority?: number;
         }
         interface DeleteQueue {
             ifUnused?: boolean;
@@ -56,7 +121,7 @@ declare module "amqplib/properties" {
             ifUnused?: boolean;
         }
         interface Publish {
-            expiration?: string;
+            expiration?: string | number;
             userId?: string;
             CC?: string | string[];
 
@@ -89,92 +154,35 @@ declare module "amqplib/properties" {
         }
     }
 
-    interface Message {
+    export interface Message {
         content: Buffer;
         fields: any;
         properties: any;
     }
 }
 
-declare module "amqplib" {
+declare module 'amqplib/callback_api' {
+    import events = require('events');
+    import shared = require('amqplib/properties')
 
-    import events = require("events");
-    import when = require("when");
-    import shared = require("amqplib/properties")
     export import Replies = shared.Replies;
     export import Options = shared.Options;
     export import Message = shared.Message;
 
-    interface Connection extends events.EventEmitter {
-        close(): when.Promise<void>;
-        createChannel(): when.Promise<Channel>;
-        createConfirmChannel(): when.Promise<Channel>;
-    }
-
-    interface Channel extends events.EventEmitter {
-        close(): when.Promise<void>;
-
-        assertQueue(queue: string, options?: Options.AssertQueue): when.Promise<Replies.AssertQueue>;
-        checkQueue(queue: string): when.Promise<Replies.AssertQueue>;
-
-        deleteQueue(queue: string, options?: Options.DeleteQueue): when.Promise<Replies.DeleteQueue>;
-        purgeQueue(queue: string): when.Promise<Replies.PurgeQueue>;
-
-        bindQueue(queue: string, source: string, pattern: string, args?: any): when.Promise<Replies.Empty>;
-        unbindQueue(queue: string, source: string, pattern: string, args?: any): when.Promise<Replies.Empty>;
-
-        assertExchange(exchange: string, type: string, options?: Options.AssertExchange): when.Promise<Replies.AssertExchange>;
-        checkExchange(exchange: string): when.Promise<Replies.Empty>;
-
-        deleteExchange(exchange: string, options?: Options.DeleteExchange): when.Promise<Replies.Empty>;
-
-        bindExchange(destination: string, source: string, pattern: string, args?: any): when.Promise<Replies.Empty>;
-        unbindExchange(destination: string, source: string, pattern: string, args?: any): when.Promise<Replies.Empty>;
-
-        publish(exchange: string, routingKey: string, content: Buffer, options?: Options.Publish): boolean;
-        sendToQueue(queue: string, content: Buffer, options?: Options.Publish): boolean;
-
-        consume(queue: string, onMessage: (msg: Message) => any, options?: Options.Consume): when.Promise<Replies.Consume>;
-
-        cancel(consumerTag: string): when.Promise<Replies.Empty>;
-        get(queue: string, options?: Options.Get): when.Promise<Message | boolean>;
-
-        ack(message: Message, allUpTo?: boolean): void;
-        ackAll(): void;
-
-        nack(message: Message, allUpTo?: boolean, requeue?: boolean): void;
-        nackAll(requeue?: boolean): void;
-        reject(message: Message, requeue?: boolean): void;
-
-        prefetch(count: number, global?: boolean): when.Promise<Replies.Empty>;
-        recover(): when.Promise<Replies.Empty>;
-    }
-
-    function connect(url: string, socketOptions?: any): when.Promise<Connection>;
-}
-
-declare module "amqplib/callback_api" {
-
-    import events = require("events");
-    import shared = require("amqplib/properties")
-    export import Replies = shared.Replies;
-    export import Options = shared.Options;
-    export import Message = shared.Message;
-
-    interface Connection extends events.EventEmitter {
+    export interface Connection extends events.EventEmitter {
         close(callback?: (err: any) => void): void;
         createChannel(callback: (err: any, channel: Channel) => void): void;
         createConfirmChannel(callback: (err: any, confirmChannel: ConfirmChannel) => void): void;
     }
 
-    interface Channel extends events.EventEmitter {
+    export interface Channel extends events.EventEmitter {
         close(callback: (err: any) => void): void;
 
-        assertQueue(queue?: string, options?: Options.AssertQueue, callback?: (err:any, ok: Replies.AssertQueue) => void): void;
+        assertQueue(queue?: string, options?: Options.AssertQueue, callback?: (err: any, ok: Replies.AssertQueue) => void): void;
         checkQueue(queue: string, callback?: (err: any, ok: Replies.AssertQueue) => void): void;
 
-        deleteQueue(queue: string, options?: Options.DeleteQueue, callback?: (err:any, ok: Replies.DeleteQueue) => void): void;
-        purgeQueue(queue: string, callback?: (err:any, ok: Replies.PurgeQueue) => void): void;
+        deleteQueue(queue: string, options?: Options.DeleteQueue, callback?: (err: any, ok: Replies.DeleteQueue) => void): void;
+        purgeQueue(queue: string, callback?: (err: any, ok: Replies.PurgeQueue) => void): void;
 
         bindQueue(queue: string, source: string, pattern: string, args?: any, callback?: (err: any, ok: Replies.Empty) => void): void;
         unbindQueue(queue: string, source: string, pattern: string, args?: any, callback?: (err: any, ok: Replies.Empty) => void): void;
@@ -206,14 +214,14 @@ declare module "amqplib/callback_api" {
         recover(callback?: (err: any, ok: Replies.Empty) => void): void;
     }
 
-    interface ConfirmChannel extends Channel {
+    export interface ConfirmChannel extends Channel {
         publish(exchange: string, routingKey: string, content: Buffer, options?: Options.Publish, callback?: (err: any, ok: Replies.Empty) => void): boolean;
         sendToQueue(queue: string, content: Buffer, options?: Options.Publish, callback?: (err: any, ok: Replies.Empty) => void): boolean;
 
         waitForConfirms(callback?: (err: any) => void): void;
     }
 
-    function connect(callback: (err: any, connection: Connection) => void): void;
-    function connect(url: string, callback: (err: any, connection: Connection) => void): void;
-    function connect(url: string, socketOptions: any, callback: (err: any, connection: Connection) => void): void;
+    export function connect(callback: (err: any, connection: Connection) => void): void;
+    export function connect(url: string, callback: (err: any, connection: Connection) => void): void;
+    export function connect(url: string, socketOptions: any, callback: (err: any, connection: Connection) => void): void;
 }
