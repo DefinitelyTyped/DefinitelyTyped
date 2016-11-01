dbf71aa147b7a892010e45504a19028730c32394
jeff <jeff@giftbit.com>
2016-11-01 14:12:46 -0700

    Respond to reviewer feedback.

--- cbor/index.d.ts
+++ cbor/index.d.ts
@@ -7,35 +7,30 @@
 
 import stream = require("stream");
 
-declare namespace cbor {
+export function decode(input: Buffer | string): any;
+export function decodeAll(input: Buffer | string, callback: (error: any, objs: any[]) => void): void;
+export function decodeAllSync(input: Buffer | string): any[];
+export function decodeFirst(input: Buffer | string, callback: (error: any, obj: any) => void): void;
+export function decodeFirstSync(input: Buffer | string): any;
+export function encode(input: any): Buffer;
 
-    export function decode(input: Buffer | string): any;
-    export function decodeAll(input: Buffer | string, callback: (error: any, objs: any[]) => void): void;
-    export function decodeAllSync(input: Buffer | string): any[];
-    export function decodeFirst(input: Buffer | string, callback: (error: any, obj: any) => void): void;
-    export function decodeFirstSync(input: Buffer | string): any;
-    export function encode(input: any): Buffer;
-
-    export class Decoder extends stream.Transform  {
-        constructor(params?: {
-            input?: Buffer | string;
-            encoding?: string;
-            tags?: {[tag: number]: (val: any[]) => any}
-        });
-    }
-
-    export class Encoder extends stream.Transform {
-        constructor();
-        addSemanticType<T>(type: new (...args: any[]) => T, encodeFunction: (encoder: Encoder, t: T) => void): void;
-        pushAny(input: any): void;
-    }
-
-    export namespace leveldb {
-        export function decode(input: Buffer | string): any[];
-        export function encode(input: any): Buffer;
-        export const buffer: boolean;
-        export const name: string;
-    }
+export class Decoder extends stream.Transform  {
+    constructor(params?: {
+        input?: Buffer | string;
+        encoding?: string;
+        tags?: {[tag: number]: (val: any[]) => any}
+    });
 }
 
-export = cbor;
+export class Encoder extends stream.Transform {
+    constructor();
+    addSemanticType<T>(type: new (...args: any[]) => T, encodeFunction: (encoder: Encoder, t: T) => void): void;
+    pushAny(input: any): void;
+}
+
+export namespace leveldb {
+    export function decode(input: Buffer | string): any[];
+    export function encode(input: any): Buffer;
+    export const buffer: boolean;
+    export const name: string;
+}
