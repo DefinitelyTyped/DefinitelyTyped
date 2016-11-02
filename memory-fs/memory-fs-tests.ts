344e1d9ac796a3742c51045526509b8f581da2c5
e-cloud <saintscott119@gmail.com>
2016-11-02 16:36:22 +0800

    fix: enable strictNullChecks and adjust tests

--- memory-fs/memory-fs-tests.ts
+++ memory-fs/memory-fs-tests.ts
@@ -7,9 +7,13 @@ const fs = new MemoryFileSystem({});
 fs.existsSync('./kd/sdkfj');
 
 fs.writeFile('hello', 'hahahahah', function (err) {
-    console.log(err.message);
+    if(err){
+        console.log(err.message);
+    }
 });
 
 fs.writeFile('hello', 'hahahahah', 'utf-8', function (err) {
-    console.log(err.message);
+    if(err){
+        console.log(err.message);
+    }
 });
