cc603d3ddd9d753717b92b0a3022e1244b957b77
bradfordwagner <bradford.wagner@blackrock.com>
2016-11-02 17:35:40 -0400

    adding tests

---
+++ angular-clipboard/angular-clipboard-tests.ts
@@ -0,0 +1,11 @@
+/// <reference path="./index.d.ts" />
+/// <reference path="../angular/index.d.ts" />
+
+const app = angular.module('testModule', ['angular-clipboard']);
+app.controller('TestController', ($scope: ng.IScope, clipboard: angular.clipboard.IClipboardService) => {
+    $scope['testCopy'] = () => {
+        if (clipboard.supported) {
+            clipboard.copyText('hiiiiiii');
+        }
+    };
+});
