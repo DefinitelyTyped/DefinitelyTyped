6cac9996d43b82cf8ad7aa7fe8c52c72dcb69e26
Andy Hanson <anhans@microsoft.com>
2016-11-02 09:41:50 -0700

    Convert highcharts definition to module style

--- highcharts/highcharts-more.d.ts
+++ highcharts/highcharts-more.d.ts
@@ -3,9 +3,9 @@
 // Definitions by: Maciej Suchecki <http://github.com/mc-suchecki>
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
 
-/// <reference path="highcharts.d.ts" />
+import { Static } from "highcharts";
 
-declare var HighchartsMore: (H: __Highcharts.Static) => __Highcharts.Static;
+declare var HighchartsMore: (H: Static) => Static;
 
 declare module "highcharts/highcharts-more" {
     export = HighchartsMore;
