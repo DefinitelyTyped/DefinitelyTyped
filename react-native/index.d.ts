07765863bfc72cfdb767bcc60997f8738fb00a8e
hktonylee <iamtonylee@gmail.com>
2016-11-02 21:18:52 +0800

    added fetch function in react-native (#12408)

--- react-native/index.d.ts
+++ react-native/index.d.ts
@@ -7569,6 +7569,7 @@ declare module "react" {
     // Network Polyfill
     // TODO: Add proper support for fetch
     export type fetch = (url: string, options?: Object) => Promise<any>
+	export const fetch: fetch;
 
     // Timers polyfill
     export type timedScheduler = (fn: string | Function, time: number) => number
