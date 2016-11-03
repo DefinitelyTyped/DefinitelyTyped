6bd5208910d51aadd5e38bc50edb6b0a92ef9eda
Bradford Wagner <bradford.wagner@blackrock.com>
2016-11-02 21:03:27 -0400

    Update angular-clipboard-tests.ts
    triggering build

--- angular-clipboard/angular-clipboard-tests.ts
+++ angular-clipboard/angular-clipboard-tests.ts
@@ -2,7 +2,7 @@
 /// <reference path="../angular/index.d.ts" />
 
 import IClipboardService from "angular-clipboard";
-import * as angular from "angular";
+import * as angular from "angular"; 
 
 const app = angular.module('testModule', ['angular-clipboard']);
 app.controller('TestController', ($scope: ng.IScope, clipboard: angular.clipboard.IClipboardService) => {
