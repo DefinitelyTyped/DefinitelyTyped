f091e92b89c2091e87f93e799d5540a1204f93ba
rusakov.sa <rusakov.sa@esphere.ru>
2016-11-03 11:02:41 +0300

    Version incremented

--- kendo-ui/kendo-ui.d.ts
+++ kendo-ui/kendo-ui.d.ts
@@ -1,4 +1,4 @@
-// Type definitions for Kendo UI Professional v2016.3.1028
+// Type definitions for Kendo UI Professional v2016.3.1029
 // Project: http://www.telerik.com/kendo-ui
 // Definitions by: Telerik <https://github.com/telerik/>
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
