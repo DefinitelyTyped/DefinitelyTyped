e055c42e3fa3b45fa6a470f19fb3438d62c9b680
Rogier Schouten <r.schouten@spiritit.com>
2016-11-07 13:43:29 +0100

    fix tsc errors

--- object-diff/object-diff-tests.ts
+++ object-diff/object-diff-tests.ts
@@ -6,14 +6,12 @@ import diff = require('object-diff');
 var a = {
     speed: 4,
     power: 54,
-    height: undefined as number,
     level: 1,
 };
  
 var b = {
     speed: 4,			// unchanged 
     power: 22,			// changed 
-    level: undefined as number,	// changed 
     weight: 10,			// added 
 };
  
