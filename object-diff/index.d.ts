dbb69a528e9ba664da70872a02bb7d61206dddae
Rogier Schouten <r.schouten@spiritit.com>
2016-11-07 09:00:08 +0100

    review comment

--- object-diff/index.d.ts
+++ object-diff/index.d.ts
@@ -1,4 +1,4 @@
-// Type definitions for object-diff v0.03
+// Type definitions for object-diff v0.0
 // Project: https://github.com/srcagency/object-diff
 // Definitions by: Rogier Schouten <https://github.com/rogierschouten>
 // Definitions: https://github.com/borisyankov/DefinitelyTyped
