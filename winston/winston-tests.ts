27a9d53693fea45cc2a5ed7b388d1cc4af1a80fc
heycalmdown <heyacct@gmail.com>
2016-11-11 17:53:12 +0900

    Add configs(cli, npm, syslog)
    - refactor to abstract configs

--- winston/winston-tests.ts
+++ winston/winston-tests.ts
@@ -267,3 +267,23 @@ var logger: winston.LoggerInstance = new (winston.Logger)({
 logger.configure({ level: 'silly' });
 
 winston.default.warn("Don't export reserved words in JavaScript!");
+
+winston.default.setLevels(winston.config.syslog.levels);
+winston.addColors(winston.config.syslog.colors);
+winston.default.emerg('syslog!');
+
+// module augment for custom level log
+// https://github.com/winstonjs/winston#using-custom-logging-levels
+// https://github.com/Microsoft/TypeScript/issues/7545
+declare module 'winston' {
+  interface LoggerInstance {
+    yay: winston.LeveledLogMethod;
+  }
+}
+const customLevelLogger = new winston.Logger({
+  levels: {yay: 0, haha: 1, hoho: 2},
+  transports: [
+    new (winston.transports.Console)()
+  ]
+});
+customLevelLogger.yay('yay!');
