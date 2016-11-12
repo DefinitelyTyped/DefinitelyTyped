fa858cee7d5ff5f611426ec5eeef7d3187280a9f
Cyril Schumacher <cyril_schumacher@live.fr>
2016-11-12 23:40:32 +0100

    Fix reference in test file.

--- neo4j/index.d.ts
+++ neo4j/index.d.ts
@@ -3,7 +3,7 @@
 // Definitions by: Cyril Schumacher <https://github.com/cyrilschumacher>
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
 
-/// <reference path="../request/request.d.ts"/>
+/// <reference path="../request/index.d.ts"/>
 
 declare module "neo4j" {
     import { Request } from "request";
