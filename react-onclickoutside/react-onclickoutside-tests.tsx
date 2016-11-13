092f5bdee66c3d743a1550438cdfeea860347db1
Yui T <yuisu@microsoft.com>
2016-11-12 22:38:00 -0800

    Remove es6-class style tests

--- react-onclickoutside/react-onclickoutside-tests.tsx
+++ react-onclickoutside/react-onclickoutside-tests.tsx
@@ -8,21 +8,13 @@ import { Component, StatelessComponent, MouseEvent } from "react"
 import { render } from "react-dom"
 import * as onClickOutside from "react-onclickoutside"
 
-interface TestProps extends ReactOnClickOutside.OnClickOutsideProps {}
+interface TestProps {}
 
 var TestStateless: StatelessComponent<TestProps> = (props: TestProps) => {
     return (<div>Test</div>)
 }
+
 var TestStatelessWrapped = onClickOutside(TestStateless)
-
-class Test extends Component<TestProps, any> implements ReactOnClickOutside.OnClickOutsideComponent {
-    handleClickOutside (e: MouseEvent<any>) {}
-
-    render () {
-        return (<div>Test</div>)
-    }
-}
-var TestWrapped = onClickOutside(Test)
 
 render(
     <TestStatelessWrapped eventTypes="click"
@@ -30,14 +22,5 @@ render(
                           preventDefault
                           stopPropagation
                           outsideClickIgnoreClass="ignore" />,
-    document.getElementById("main")
-)
-
-render(
-    <TestWrapped eventTypes={ ["click", "touchend"] }
-                 disableOnClickOutside
-                 preventDefault
-                 stopPropagation
-                 outsideClickIgnoreClass="ignore" />,
     document.getElementById("main")
 )
