f68f50e7363cf155c36422179a431cc237d66e93
Marc Berli <marc.berli@windowslive.com>
2016-11-14 12:56:50 +0100

    Adding module declaration
    Adding module declaration so "cannot find module"-error disappears.

--- snapsvg/snapsvg.d.ts
+++ snapsvg/snapsvg.d.ts
@@ -2,6 +2,10 @@
 // Project: https://github.com/adobe-webplatform/Snap.svg
 // Definitions by: Lars Klein <https://github.com/lhk>
 // Definitions: https://github.com/borisyankov/DefinitelyTyped
+declare module 'snapsvg' {
+  var _snapsvg: any;
+  export = _snapsvg;
+}
 
 declare function mina(a:number, A:number, b:number, B:number, get:Function, set:Function, easing?:Function):Object;
 declare module mina {
