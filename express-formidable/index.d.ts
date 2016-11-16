3310c746c6f6c054e8e17e404ca0b568a1c69dad
Torkild Dyvik Olsen <torkild@webology.no>
2016-11-16 19:41:45 +0100

    Removed module declaration for express-formidable.

--- express-formidable/index.d.ts
+++ express-formidable/index.d.ts
@@ -3,21 +3,19 @@
 // Definitions by: Torkild Dyvik Olsen <https://github.com/tdolsen>
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
 
-declare module "express-formidable" {
-    import { RequestHandler } from "express";
+import { RequestHandler } from "express";
 
-    interface ExpressFormidableOptions {
-        encoding?: string;
-        uploadDir?: string;
-        keepExtensions?: boolean;
-        type?: "multipart" | "urlencoded";
-        maxFieldsSize?: number;
-        maxFields?: number;
-        hash?: boolean | "sha1" | "md5";
-        multiples?: boolean;
-    }
-
-    function ExpressFormidable(options?: ExpressFormidableOptions): RequestHandler;
-
-    export = ExpressFormidable;
+interface ExpressFormidableOptions {
+    encoding?: string;
+    uploadDir?: string;
+    keepExtensions?: boolean;
+    type?: "multipart" | "urlencoded";
+    maxFieldsSize?: number;
+    maxFields?: number;
+    hash?: boolean | "sha1" | "md5";
+    multiples?: boolean;
 }
+
+declare function ExpressFormidable(options?: ExpressFormidableOptions): RequestHandler;
+
+export = ExpressFormidable;
