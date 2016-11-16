4dca78d140e95266ce33c2fa528e42381a9c26d7
Ryan Posener <ryan@praceng.us>
2016-11-16 14:35:46 -0700

    Add Tests and tsconfig.json

--- webnotifications/webnotifications.d.ts
+++ webnotifications/webnotifications.d.ts
@@ -6,77 +6,38 @@ interface Notification {
      * The text displayed in the notification body
      * @readonly
      */
-    body: string;
+    body?: string;
 
 
     /**
      * The direction the popup will show
      * @readonly
      */
-    dir: "auto" | "ltr" | "rtl";
-
-    /**
-     * The image shown in the notification
-     * @readonly
-     */
-    image: string;
-
+    dir?: "auto" | "ltr" | "rtl";
 
     /**
      * The icon shown in the notification
      * @readonly
      */
-    icon: string;
-
-    /**
-     * The badge shown in the notification
-     * @readonly
-     */
-    badge: string;
-
-    /**
-     * The sound for the notification
-     * @readonly
-     */
-    sound: string;
-
+    icon?: string;
 
     /**
      * The title shown in the notification
      * @readonly
      */
-    title: string;
-
+    title?: string;
 
     /**
      * The notification language
      * @readonly
      */
-    lang: string;
+    lang?: string;
 
     /**
-     * Should the notification re-notify
+     * The notification tag
      * @readonly
      */
-    renotify?: boolean;
-
-    /**
-     * Is the Notification Silent
-     * @readonly
-     */
-    silent?: boolean;
-
-    /**
-     * Is the Notification sticky
-     * @readonly
-     */
-    sticky?: boolean;
-
-    /**
-     * Requires Interaction by the User
-     * @readonly
-     */
-    requireInteraction?: boolean;
+    tag?: string;
 
     /**
      * Close the Notification
@@ -89,21 +50,20 @@ interface Notification {
     onclick: (event: Event) => void;
 
     /**
-     * Error Occurred while creating the notification
+     * The Notification is visible
      */
-    onerror: (error: Error) => void;
+    onshow: (event: Event) => void;
 
     /**
-     * Any actions available to the user
+     * The Notification has closed
      */
-    actions?: Array<NotificationAction>;
+    onclose: (event: Event) => void;
 
-}
+    /**
+     * Error Occurred while creating the notification
+     */
+    onerror: (error: ErrorEvent) => void;
 
-interface NotificationAction {
-    action: string;
-    title: string;
-    icon?: string;
 }
 
 interface NotificationStatic {
@@ -117,52 +77,40 @@ interface NotificationStatic {
     /**
      * Request Permission to use Notifications
      */
-    requestPermission(deprecatedCallback?: (permission: "default" | "denied" | "granted") => void): PromiseLike<"default" | "denied" | "granted">;
+    requestPermission(callback?: (permission: "default" | "denied" | "granted") => void): PromiseLike<"default" | "denied" | "granted">;
 
     /**
-     * Maximum Number of Actions that can be performed
-     * @readonly
+     * Creates a new Notification
      */
-    maxActions: number;
-
     new (title: string, options: NotificationOptions): Notification;
 
 }
 
-
-
 declare var Notification: NotificationStatic;
 
-
-
 declare interface NotificationOptions {
-
-    title?: string;
-
+/**
+     * The text displayed in the notification body
+     */
     body?: string;
 
-    image?: string;
-
-    icon?: string;
-
-    badge?: string;
-
-    sound?: string;
-
-    tag?: string;
-
-    lang?: string;
-
-    renotify?: boolean;
-
-    silent?: boolean;
-
-    sticky?: boolean;
-
-    requireInteraction?: boolean;
-
+    /**
+     * The direction the popup will show
+     */
     dir?: "auto" | "ltr" | "rtl";
 
-    actions?: Array<NotificationAction>;
+    /**
+     * The icon shown in the notification
+     */
+    icon?: string;
 
+    /**
+     * The notification language
+     */
+    lang?: string;
+
+    /**
+     * The notification tag (causes replacement of existing notification with same tag)
+     */
+    tag?: string;    
 }
