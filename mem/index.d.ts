b0b45350bcd1c8cb7f8ed7a5b51d1bade69412f1
Sam Verschueren <sam.verschueren@gmail.com>
2016-11-17 11:53:35 +0100

    add mem

---
+++ mem/index.d.ts
@@ -0,0 +1,18 @@
+// Type definitions for mem 1.1.0
+// Project: https://github.com/sindresorhus/mem
+// Definitions by: Sam Verschueren <https://github.com/SamVerschueren>
+// Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
+
+interface Options {
+	maxAge?: number;
+	cacheKey?: (...args: any[]) => string;
+	cache?: any;
+}
+
+declare const mem: {
+	<F extends (...args: any[]) => any>(fn: F, options?: Options): F;
+	clear<F extends (...args: any[]) => any>(fn: F): void;
+};
+
+declare namespace mem { }
+export = mem;
