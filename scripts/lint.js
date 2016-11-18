6ad59b1c48721aa49dc813769d9985135e1a9378
Andy <anhans@microsoft.com>
2016-11-18 13:32:07 -0800

    Support `npm link`-ed types-publisher in `npm run lint` (#12760)

--- scripts/lint.js
+++ scripts/lint.js
@@ -1,8 +1,14 @@
 // Usage: npm run lint -- my-package-name
 const pkg = process.argv[2];
 const execSync = require("child_process").execSync;
-const cmd = `node node_modules/tslint/lib/tslint-cli --format stylish ${pkg}/**/*.d.ts`;
+const existsSync = require("fs").existsSync;
+
+// Path of tslint when `types-publisher` is symlinked
+const symlinkedTslintPath = "node_modules/types-publisher/node_modules/tslint"
+const tslintPath =  existsSync(symlinkedTslintPath) ? symlinkedTslintPath : "node_modules/tslint";
+const cmd = `node ${tslintPath}/lib/tslint-cli --format stylish ${pkg}/**/*.d.ts`;
 console.log(cmd);
+
 try {
     // Child process writes directly to our own stdout
     execSync(cmd, { stdio: "inherit" });
