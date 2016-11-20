933815bb94ab0dca88849fc471fb10a9746ef312
Donald Nairn <donald.nairn@gmail.com>
2016-11-20 23:55:10 +0000

    Add JSDoc comments.

--- angular-file-saver/index.d.ts
+++ angular-file-saver/index.d.ts
@@ -4,7 +4,16 @@
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
 
 declare namespace angular {
+    /**
+     * A core Angular factory proving FileSaver functionality.
+     */
     interface FileSaver {
+        /**
+         * Immediately starts saving a file
+         * @param data: a Blob instance;
+         * @param filename: a String custom filename (an extension is optional);
+         * @param disableAutoBOM : (optional) Boolean Disable automatically provided Unicode text encoding hints;
+         */
         saveAs(blob: Blob, fileName: string, disableBOM: boolean);
     }
 }
