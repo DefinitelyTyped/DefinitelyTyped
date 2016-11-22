4b5835399b8d8d45d6f4f3102dfb00171ffd004f
Adam Charron <Charrondev@gmail.com>
2016-11-22 12:54:30 -0500

    Fix typo in fxn

--- fxn/index.d.ts
+++ fxn/index.d.ts
@@ -1,5 +1,5 @@
-// Type definitions for fxn 0.0.1
-// Project: https://fgithub.com/poly/fxn
+// Type definitions for fxn 0.0.4
+// Project: https://github.com/poly/fxn
 // Definitions by: Adam Charron <https://github.com/charrondev>
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
 
