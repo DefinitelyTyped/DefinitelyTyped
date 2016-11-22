b952cbe30bb17d29ebd1fbf085adba8bb4387f1a
Steve Jenkins <steve@skysteve.com>
2016-11-22 08:42:09 +0000

    update as per comment

--- promise-polyfill/index.d.ts
+++ promise-polyfill/index.d.ts
@@ -3,21 +3,4 @@
 // Definitions by: Steve Jenkins <https://github.com/skysteve>
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
 
-export as namespace Promise;
-
-export = Promise;
-declare var Promise: Promise.IPromise;
-
-declare namespace Promise {
-	export interface IPromise {
-		new <T>(resolver: (resolve: (value: T) => void, reject: (reason: any) => void) => void): IThenable<T>;
-		resolve: <T>(value: T) => IThenable<T>;
-		reject: <T>(value: T) => IThenable<T>;
-		all: (array: Array<IThenable<any>>) => IThenable<Array<any>>;
-	}
-
-	export interface IThenable<T> {
-		then<R>(onFulfilled?: (value: T) => IThenable<R>|R, onRejected?: (error: any) => IThenable<R>|R): IThenable<R>;
-		catch<R>(onRejected?: (error: any) => IThenable<R>|R): IThenable<R>;
-	}
-}
+export default Promise;
