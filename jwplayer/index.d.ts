401d5780575ef241727aa4ae2165b2adabd93353
Isaac Schemm <isaacschemm@gmail.com>
2016-11-23 14:18:51 -0600

    Remove own name from index.d.ts

--- jwplayer/index.d.ts
+++ jwplayer/index.d.ts
@@ -1,6 +1,6 @@
 // Type definitions for JW Player 6
 // Project: http://developer.longtailvideo.com/trac/
-// Definitions by: Martin Duparc <https://github.com/martinduparc/> and Isaac Schemm <https://github.com/IsaacSchemm/>
+// Definitions by: Martin Duparc <https://github.com/martinduparc/>
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
 
 // JW Player is the leading HTML5 & Flash video player, optimized for mobile and the desktop. Easy enough for beginners, advanced enough for pros.
