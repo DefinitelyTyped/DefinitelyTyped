f6b3c4d5ba937de04df9cac3b95ed1515d09c2f9
rhysd <lin90162@yahoo.co.jp>
2016-11-24 00:05:43 +0900

    electron: support Electron 1.4.8

--- electron/github-electron-renderer-tests.ts
+++ electron/github-electron-renderer-tests.ts
@@ -57,7 +57,8 @@ console.log(webFrame.getZoomFactor());
 webFrame.setZoomLevel(200);
 console.log(webFrame.getZoomLevel());
 
-webFrame.setZoomLevelLimits(50, 200);
+webFrame.setVisualZoomLevelLimits(50, 200);
+webFrame.setLayoutZoomLevelLimits(50, 200);
 
 webFrame.setSpellCheckProvider('en-US', true, {
 	spellCheck: text => {
