6d1d3060813f59e23c816d8f96c08d8f5d97b264
Nathan Shively-Sanders <nathansa@microsoft.com>
2016-11-23 11:12:28 -0800

    Make `IAnimateService.closeAndFlush` return void

--- angular-mocks/index.d.ts
+++ angular-mocks/index.d.ts
@@ -311,7 +311,7 @@ declare module 'angular' {
       /**
        * This method will close all pending animations (both Javascript and CSS) and it will also flush any remaining animation frames and/or callbacks.
        */
-      closeAndFlush();
+      closeAndFlush(): void;
 
       /**
        * This method is used to flush the pending callbacks and animation frames to either start an animation or conclude an animation. Note that this will not actually close an actively running animation (see `closeAndFlush()` for that).
