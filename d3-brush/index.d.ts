d7e56fd0d888f3536d51d5db593638da0d8792fa
Tom Wanzek <tomwanzek@gmail.com>
2016-11-25 12:50:14 -0500

    Bump patch versions of underlying modules * (d3-brush) (d3-drag) (d3-force) [Chore]: To indicate up to which patch version the files definitions were validated, patch version number has been bumped

--- d3-brush/index.d.ts
+++ d3-brush/index.d.ts
@@ -1,4 +1,4 @@
-// Type definitions for D3JS d3-brush module v1.0.2
+// Type definitions for D3JS d3-brush module v1.0.3
 // Project: https://github.com/d3/d3-brush/
 // Definitions by: Tom Wanzek <https://github.com/tomwanzek>, Alex Ford <https://github.com/gustavderdrache>, Boris Yankov <https://github.com/borisyankov>
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
