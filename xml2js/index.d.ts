86e8ab2ca2ea3deb4d398af960a226d76f80ca35
Alejandro Sánchez <sanchezajsa@gmail.com>
2016-11-25 22:52:45 -0600

    Expose provided processors living in xml2js/lib/processors

--- xml2js/index.d.ts
+++ xml2js/index.d.ts
@@ -3,7 +3,7 @@
 // Definitions by: Michel Salib <https://github.com/michelsalib>, Jason McNeil <https://github.com/jasonrm>, Christopher Currens <https://github.com/ccurrens>
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
 
-
+/// <reference path="./processors.d.ts" />
 
 export = xml2js;
 
@@ -29,8 +29,8 @@ declare namespace xml2js {
     interface Options {
         async?: boolean;
         attrkey?: string;
-        attrNameProcessors?: [(name: string) => string];
-        attrValueProcessors?: [(name: string) => string];
+        attrNameProcessors?: [(name: string) => any];
+        attrValueProcessors?: [(name: string) => any];
         charkey?: string;
         charsAsChildren?: boolean;
         childkey?: string;
@@ -45,10 +45,10 @@ declare namespace xml2js {
         normalize?: boolean;
         normalizeTags?: boolean;
         strict?: boolean;
-        tagNameProcessors?: [(name: string) => string];
+        tagNameProcessors?: [(name: string) => any];
         trim?: boolean;
         validator?: Function;
-        valueProcessors?: [(name: string) => string];
+        valueProcessors?: [(name: string) => any];
         xmlns?: boolean;
     }
 
@@ -75,4 +75,3 @@ declare namespace xml2js {
         toString(): string;
     }
 }
-
