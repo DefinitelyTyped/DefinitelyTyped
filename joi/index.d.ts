1829342f710aaf3254030a2fa1d68220409e8bb1
ptrckc <ptrckc@users.noreply.github.com>
2016-11-27 20:26:01 -0600

    Bump joi version reference to v10
    See https://github.com/hapijs/joi/issues/1037

--- joi/index.d.ts
+++ joi/index.d.ts
@@ -1,4 +1,4 @@
-// Type definitions for joi v9.0.0
+// Type definitions for joi v10.0.0
 // Project: https://github.com/hapijs/joi
 // Definitions by: Bart van der Schoor <https://github.com/Bartvds>, Laurence Dougal Myers <https://github.com/laurence-myers>, Christopher Glantschnig <https://github.com/cglantschnig>, David Broder-Rodgers <https://github.com/DavidBR-SW>, Gael Magnan de Bornier <hhttps://github.com/GaelMagnan>
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
