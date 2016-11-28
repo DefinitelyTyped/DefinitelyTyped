7d16d3c62508cbc33a99d146357938dc0502b022
Alan Agius <alan.agius4@gmail.com>
2016-11-28 21:17:46 +0100

    feat(): change fuse to fuse.js

--- fuse/index.d.ts
+++ fuse/index.d.ts
@@ -1,30 +1,35 @@
 // Type definitions for Fuse.js 2.5.0
-// Project: https://github.com/krisk/Fuse
-// Definitions by: Greg Smith <https://github.com/smrq/>
+// Definitions by: Alan Agius <https://github.com/alan-agius4/>
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
 
-declare class Fuse {
-	constructor(list: any[], options?: fuse.IFuseOptions);
-	search(pattern: string): any[];
+declare module 'fuse.js' {
+	namespace fuse.js {
+		class Fuse {
+			constructor(list: any[], options?: FuseOptions);
+			search(pattern: string): any[];
+			search<T>(pattern: string): T[];
+		}
+
+		interface FuseOptions {
+			id?: string;
+			caseSensitive?: boolean;
+			include?: string[];
+			shouldSort?: boolean;
+			searchFn?: any;
+			sortFn?: (a: { score: number }, b: { score: number }) => number;
+			getFn?: (obj: any, path: string) => any;
+			keys?: string[] | { name: string; weight: number }[];
+			verbose?: boolean;
+			tokenize?: boolean;
+			tokenSeparator?: RegExp;
+			matchAllTokens?: boolean;
+			location?: number;
+			distance?: number;
+			threshold?: number;
+			maxPatternLength?: number;
+		}
+	}
+	export = fuse.js;
 }
 
-declare namespace fuse {
-	interface IFuseOptions {
-		id?: string;
-		caseSensitive?: boolean;
-		include?: string[];
-		shouldSort?: boolean;
-		searchFn?: any;
-		sortFn?: (a: {score: number}, b: {score: number}) => number;
-		getFn?: (obj: any, path: string) => any;
-		keys?: string[] | { name:string; weight:number} [];
-        	verbose?:boolean;
-		tokenize?: boolean;
-		tokenSeparator? : RegExp;
-		matchAllTokens?: boolean;
-		location?: number;
-		distance?: number;
-		threshold?: number;
-		maxPatternLength?: number;
-	}
-}
+declare const Fuse;
