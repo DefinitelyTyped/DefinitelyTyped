5b719fda7bee1a9dc96ad8bedf9d2270c5fee854
Andy <anhans@microsoft.com>
2016-11-28 11:15:50 -0800

    Lint header: don't need patch version (#12959)

--- mem/index.d.ts
+++ mem/index.d.ts
@@ -1,4 +1,4 @@
-// Type definitions for mem 1.1.0
+// Type definitions for mem 1.1
 // Project: https://github.com/sindresorhus/mem
 // Definitions by: Sam Verschueren <https://github.com/SamVerschueren>
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
