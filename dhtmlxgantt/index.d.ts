0783cf8241b9ed56e479a63420c2cf6594f5b77e
Christophe Camicas <christophe.camicas@gmail.com>
2016-11-29 17:27:22 +0100

    add definition contribution

--- dhtmlxgantt/index.d.ts
+++ dhtmlxgantt/index.d.ts
@@ -1,6 +1,6 @@
 // Type definitions for dhtmlxGantt 4.0.0
 // Project: http://dhtmlx.com/docs/products/dhtmlxGantt
-// Definitions by: Maksim Kozhukh <http://github.com/mkozhukh>
+// Definitions by: Maksim Kozhukh <http://github.com/mkozhukh>, Christophe Camicas <http://github.com/chriscamicas>
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
 
 
