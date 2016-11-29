095e2d86a93248888c354544d9b62eb6ca729223
Prashant Tiwari <prashaantt@gmail.com>
2016-11-30 03:24:10 +0530

    Try pleasing linter for dt-header

--- hoek/index.d.ts
+++ hoek/index.d.ts
@@ -1,4 +1,4 @@
-// Type definitions for Hoek 4.1.0
+// Type definitions for hoek 4.1
 // Project: https://github.com/hapijs/hoek
 // Definitions by: Prashant Tiwari <https://github.com/prashaantt>
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
