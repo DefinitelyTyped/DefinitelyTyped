63631e0ef2e7427c4a901a613dbe52ad14b7d267
Grant Hutchins <ghutchins@pivotal.io>
2016-11-29 09:04:10 -0600

    google-maps: Support version 3.2.1
    Signed-off-by: Kurtis Seebaldt <kseebaldt@pivotal.io>

--- google-maps/index.d.ts
+++ google-maps/index.d.ts
@@ -1,4 +1,4 @@
-// Type definitions for google-maps 3.1.0
+// Type definitions for google-maps 3.2.1
 // Project: https://www.npmjs.com/package/google-maps
 // Definitions by: Deividas Bakanas <https://github.com/DeividasBakanas>, Giedrius Grabauskas <https://github.com/GiedriusGrabauskas>
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
@@ -10,17 +10,25 @@ declare namespace GoogleMapsLoader {
     interface CallBack {
         (google: google): void;
     }
+
     export var KEY: string;
-    export var CLIENT: string;
-    export var VERSION: string;
-    export var SENSOR: boolean;
+    export var URL: string;
     export var LIBRARIES: Array<string>;
+    export var CLIENT: string;
+    export var CHANNEL: string;
     export var LANGUAGE: string;
+    export var REGION: string;
+    export var VERSION: string;
+    export var WINDOW_CALLBACK_NAME: string;
+
     export function release(callBack: Function): void;
     export function onLoad(callBack?: CallBack): void;
     export function load(callBack?: CallBack): void;
     export function isLoaded(): boolean;
-
+    export function createLoader(): void;
+    export function createUrl(): string;
+    export function makeMock(): void;
 }
+
 export = GoogleMapsLoader;
 export as namespace GoogleMapsLoader;
