c3d4782c9fe3b8432e97d7b5fe144af2ced58ae3
Jeff Barnes <jeff.barnes@gmail.com>
2016-11-30 10:23:12 -0900

    pouchdb-mapreduce additions.

--- pouchdb-mapreduce/index.d.ts
+++ pouchdb-mapreduce/index.d.ts
@@ -6,6 +6,44 @@
 /// <reference types="pouchdb-core" />
 
 declare namespace PouchDB {
+
+    interface Filter {
+        map: (doc: any) => void
+        reduce?: (key: string, value: any) => any
+    }
+
+    namespace Query {
+        interface Options {
+            reduce?: ((...args: any[]) => void) | '_sum' | '_count' | '_stats' | boolean
+            include_docs?: boolean
+            conflicts?: boolean
+            attachments?: boolean
+            binary?: boolean
+            startkey?: any
+            endkey?: any
+            inclusive_end?: boolean
+            limit?: number
+            skip?: number
+            descending?: boolean
+            key?: any
+            keys?: any[]
+            group?: boolean
+            group_level?: number
+            stale?: 'ok' | 'update_after'
+        }
+
+        interface Response {
+            total_rows: number
+            offset: number
+            rows: {
+                id: any
+                key: any
+                value: any
+                doc?: any
+            }[]
+        }
+    }
+
     export interface Database<Content extends Core.Encodable> {
         /**
          * Cleans up any stale map/reduce indexes.
@@ -17,6 +55,12 @@ declare namespace PouchDB {
          */
         viewCleanup(callback: PouchDB.Core.Callback<any,void>): void;
         viewCleanup(): Promise<void>;
+
+        query(fun: string, opts: Query.Options, callback: (err: Core.Error, res: Query.Response) => void): void;
+        query(fun: string, callback: (err: Core.Error, res: Query.Response) => void): void;
+        query(fun: Filter, opts: Query.Options, callback: (err: Core.Error, res: Query.Response) => void): void;
+        query(fun: Filter, callback: (err: Core.Error, res: Query.Response) => void): void;
+        query(fun: string | Filter | Function, opts?: Query.Options): Promise<Query.Response>;
     }
 }
 
