2a1dc0e2abebde14ca6eba24711a4ee511fb0eed
Mohamed Hegazy <mhegazy@microsoft.com>
2016-12-01 15:12:35 -0800

    Remove modules that do not exisit on npm

--- lodash.each/index.d.ts
+++
@@ -1,7 +0,0 @@
-// Type definitions for lodash.each 4.14
-// Project: http://lodash.com/
-// Definitions by: Brian Zengel <https://github.com/bczengel>, Ilya Mochalov <https://github.com/chrootsu>, Stepan Mikhaylyuk <https://github.com/stepancar>
-// Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
-
-import { each } from "lodash";
-export = each;
--- lodash.noConflict/index.d.ts
+++
@@ -1,7 +0,0 @@
-// Type definitions for lodash.noConflict 4.14
-// Project: http://lodash.com/
-// Definitions by: Brian Zengel <https://github.com/bczengel>, Ilya Mochalov <https://github.com/chrootsu>, Stepan Mikhaylyuk <https://github.com/stepancar>
-// Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
-
-import { noConflict } from "lodash";
-export = noConflict;
