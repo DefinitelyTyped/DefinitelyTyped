0d39267713e1e188a9092f3ee3e34df22102547e
Max Schaefer <xiemaisi@gmail.com>
2016-12-02 16:24:01 +0000

    Improve model of Node.js events module.
    The module directly exports the `EventEmitter` class, so `typeof require("events") === 'function'`.
    
    This is modelled in the same way as for modules `assert` and `stream` using an auxiliary `internal` class.

--- node/index.d.ts
+++ node/index.d.ts
@@ -569,25 +569,30 @@ declare module "querystring" {
 }
 
 declare module "events" {
-    export class EventEmitter extends NodeJS.EventEmitter {
-        static EventEmitter: EventEmitter;
-        static listenerCount(emitter: EventEmitter, event: string | symbol): number; // deprecated
-        static defaultMaxListeners: number;
+    class internal extends NodeJS.EventEmitter { }
 
-        addListener(event: string | symbol, listener: Function): this;
-        on(event: string | symbol, listener: Function): this;
-        once(event: string | symbol, listener: Function): this;
-        prependListener(event: string | symbol, listener: Function): this;
-        prependOnceListener(event: string | symbol, listener: Function): this;
-        removeListener(event: string | symbol, listener: Function): this;
-        removeAllListeners(event?: string | symbol): this;
-        setMaxListeners(n: number): this;
-        getMaxListeners(): number;
-        listeners(event: string | symbol): Function[];
-        emit(event: string | symbol, ...args: any[]): boolean;
-        eventNames(): (string | symbol)[];
-        listenerCount(type: string | symbol): number;
+    namespace internal {
+        export class EventEmitter extends internal {
+            static listenerCount(emitter: EventEmitter, event: string | symbol): number; // deprecated
+            static defaultMaxListeners: number;
+
+            addListener(event: string | symbol, listener: Function): this;
+            on(event: string | symbol, listener: Function): this;
+            once(event: string | symbol, listener: Function): this;
+            prependListener(event: string | symbol, listener: Function): this;
+            prependOnceListener(event: string | symbol, listener: Function): this;
+            removeListener(event: string | symbol, listener: Function): this;
+            removeAllListeners(event?: string | symbol): this;
+            setMaxListeners(n: number): this;
+            getMaxListeners(): number;
+            listeners(event: string | symbol): Function[];
+            emit(event: string | symbol, ...args: any[]): boolean;
+            eventNames(): (string | symbol)[];
+            listenerCount(type: string | symbol): number;
+        }
     }
+
+    export = internal;
 }
 
 declare module "http" {
