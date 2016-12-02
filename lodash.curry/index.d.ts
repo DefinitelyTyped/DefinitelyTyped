cec05d6d362a3219f25dfa2e1f5275d483318d5d
Mohamed Hegazy <mhegazy@microsoft.com>
2016-12-01 22:08:25 -0800

    Fix lodash sub modules 2 (#13051)
    * Remove modules.d.ts to avoid making lodash a global
    
    * Add separate modules for lodash submodules
    
    * Keep only major.minor for versions

---
+++ lodash.curry/index.d.ts
@@ -0,0 +1,7 @@
+// Type definitions for lodash.curry 4.1
+// Project: http://lodash.com/
+// Definitions by: Brian Zengel <https://github.com/bczengel>, Ilya Mochalov <https://github.com/chrootsu>, Stepan Mikhaylyuk <https://github.com/stepancar>
+// Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
+
+import { curry } from "lodash";
+export = curry;
