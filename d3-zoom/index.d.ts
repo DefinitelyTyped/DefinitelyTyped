70842079eb56036797c50b59195ae516d22f54a2
Tom Wanzek <tomwanzek@gmail.com>
2016-12-02 13:02:42 -0500

    Removed "v" from version header.

--- d3-zoom/index.d.ts
+++ d3-zoom/index.d.ts
@@ -1,4 +1,4 @@
-// Type definitions for d3JS d3-zoom module v1.1
+// Type definitions for d3JS d3-zoom module 1.1
 // Project: https://github.com/d3/d3-zoom/
 // Definitions by: Tom Wanzek <https://github.com/tomwanzek>, Alex Ford <https://github.com/gustavderdrache>, Boris Yankov <https://github.com/borisyankov>
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
