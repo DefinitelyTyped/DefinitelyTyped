06dabb7c2301e96b0e9d6a49b91f52c503755f30
Adam Gordon <adam@gordonizer.com>
2016-12-02 16:44:45 -0800

    normalize require and commonJS exports

--- angular-ui-bootstrap/angular-ui-bootstrap.d.ts
+++ angular-ui-bootstrap/angular-ui-bootstrap.d.ts
@@ -5,15 +5,31 @@
 
 /// <reference path="../angularjs/angular.d.ts" />
 
-// Support for AMD require
-declare module 'angular-bootstrap' {
-    let _: string;
-    export = _;
-}
-
+// Support for AMD require and CommonJS
 declare module 'angular-ui-bootstrap' {
-    let _: string;
-    export = _;
+    export default 'ui.bootstrap';
+    export type IAccordionConfig = angular.ui.bootstrap.IAccordionConfig;
+    export type IButtonConfig = angular.ui.bootstrap.IButtonConfig;
+    export type IDatepickerConfig = angular.ui.bootstrap.IDatepickerConfig;
+    export type IDatepickerPopupConfig = angular.ui.bootstrap.IDatepickerPopupConfig;
+    export type IModalProvider = angular.ui.bootstrap.IModalProvider;
+    export type IModalService = angular.ui.bootstrap.IModalService;
+    export type IModalServiceInstance = angular.ui.bootstrap.IModalServiceInstance;
+    export type IModalScope = angular.ui.bootstrap.IModalScope;
+    export type IModalSettings = angular.ui.bootstrap.IModalSettings;
+    export type IModalStackService = angular.ui.bootstrap.IModalStackService;
+    export type IModalStackedMapKeyValuePair = angular.ui.bootstrap.IModalStackedMapKeyValuePair;
+    export type IPaginationConfig = angular.ui.bootstrap.IPaginationConfig;
+    export type IPagerConfig = angular.ui.bootstrap.IPagerConfig;
+    export type IPositionCoordinates = angular.ui.bootstrap.IPositionCoordinates;
+    export type IPositionService = angular.ui.bootstrap.IPositionService;
+    export type IProgressConfig = angular.ui.bootstrap.IProgressConfig;
+    export type IRatingConfig = angular.ui.bootstrap.IRatingConfig;
+    export type ITimepickerConfig = angular.ui.bootstrap.ITimepickerConfig;
+    export type ITooltipOptions = angular.ui.bootstrap.ITooltipOptions;
+    export type ITooltipProvider = angular.ui.bootstrap.ITooltipProvider;
+    export type ITransitionService = angular.ui.bootstrap.ITransitionService;
+    export type ITransitionServiceOptions = angular.ui.bootstrap.ITransitionServiceOptions;
 }
 
 declare namespace angular.ui.bootstrap {
@@ -387,16 +403,16 @@ declare namespace angular.ui.bootstrap {
          * @default 'model-open'
          */
         openedClass?: string;
-        
+
         /**
          * CSS class(es) to be added to the top modal window.
          */
-        
+
         windowTopClass?: string;
-        
+
         /**
          * Appends the modal to a specific element.
-         * 
+         *
          * @default 'body'
          */
         appendTo?: angular.IAugmentedJQuery;
