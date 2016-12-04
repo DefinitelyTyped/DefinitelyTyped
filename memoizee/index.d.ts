a2a8e875a0bd291ba3c63ff109c6cca60463c0c6
Juan Carlos Picado <juanpicado19@gmail.com>
2016-12-04 21:37:20 +0100

    Fixing semver header

--- memoizee/index.d.ts
+++ memoizee/index.d.ts
@@ -1,4 +1,4 @@
-// Type definitions for memoizee v0.4.1
+// Type definitions for memoizee 0.4
 // Project: https://github.com/medikoo/memoizee
 // Definitions by: Juan Picado <https://github.com/juanpicado>
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
