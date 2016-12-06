38f64b97c44002c891ccbf70a645a5d50043d9bf
Jeff Jacobson <JeffJacobson@users.noreply.github.com>
2016-12-06 15:25:16 -0800

    Fixed tslint errors
    * Added version numbers to headers
    * Fixed other errors reported by TSLint.

--- arcgis-rest-api/index.d.ts
+++ arcgis-rest-api/index.d.ts
@@ -1,4 +1,4 @@
-// Type definitions for arcgis-to-geojson-utils
+// Type definitions for arcgis-to-geojson-utils 10.4
 // Project: http://resources.arcgis.com/en/help/arcgis-rest-api/
 // Definitions by: Jeff Jacobson <https://github.com/JeffJacobson>
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
@@ -66,20 +66,20 @@ declare namespace ArcGis.Rest {
 
     type JsonCurve = CircularArc | Arc | OldCircularArc | BezierCurve;
 
-    interface SpatialReference {
-    }
 
-    interface SpatialReferenceWkid extends SpatialReference {
+    interface SpatialReferenceWkid {
         wkid?: number;
         latestWkid?: number;
         vcsWkid?: number;
         latestVcsWkid?: number;
     }
 
-    interface SpatialReferenceWkt extends SpatialReference {
+    interface SpatialReferenceWkt {
         wkt?: string;
         latestWkt?: string;
     }
+
+    type SpatialReference = SpatialReferenceWkt | SpatialReferenceWkid;
 
     interface Geometry {
         spatialReference?: SpatialReference;
@@ -102,7 +102,7 @@ declare namespace ArcGis.Rest {
     }
 
     interface PolylineWithCurves extends HasZM, Geometry {
-        curvePaths: (Position | JsonCurve)[][];
+        curvePaths: Array<Array<(Position | JsonCurve)>>;
     }
 
     interface Polygon extends HasZM, Geometry {
@@ -110,7 +110,7 @@ declare namespace ArcGis.Rest {
     }
 
     interface PolygonWithCurves extends HasZM, Geometry {
-        curveRings: (Position | JsonCurve)[][];
+        curveRings: Array<Array<(Position | JsonCurve)>>;
     }
 
     interface Multipoint extends HasZM, Geometry {
@@ -133,11 +133,11 @@ declare namespace ArcGis.Rest {
     type esriGeometryType = "esriGeometryPoint" | "esriGeometryMultipoint" | "esriGeometryPolyline" | "esriGeometryPolygon" | "esriGeometryEnvelope";
 
 
-    type Color = [number, number, number, number]
-    type SimpleMarkerSymbolStyle = "esriSMSCircle" | "esriSMSCross" | "esriSMSDiamond" | "esriSMSSquare" | "esriSMSX" | "esriSMSTriangle"
-    type SimpleLineSymbolStyle = "esriSLSDash" | "esriSLSDashDot" | "esriSLSDashDotDot" | "esriSLSDot" | "esriSLSNull" | "esriSLSSolid"
-    type SimpleFillSymbolStyle = "esriSFSBackwardDiagonal" | "esriSFSCross" | "esriSFSDiagonalCross" | "esriSFSForwardDiagonal" | "esriSFSHorizontal" | "esriSFSNull" | "esriSFSSolid" | "esriSFSVertical"
-    type SymbolType = "esriSLS" | "esriSMS" | "esriSFS" | "esriPMS" | "esriPFS" | "esriTS"
+    type Color = [number, number, number, number];
+    type SimpleMarkerSymbolStyle = "esriSMSCircle" | "esriSMSCross" | "esriSMSDiamond" | "esriSMSSquare" | "esriSMSX" | "esriSMSTriangle";
+    type SimpleLineSymbolStyle = "esriSLSDash" | "esriSLSDashDot" | "esriSLSDashDotDot" | "esriSLSDot" | "esriSLSNull" | "esriSLSSolid";
+    type SimpleFillSymbolStyle = "esriSFSBackwardDiagonal" | "esriSFSCross" | "esriSFSDiagonalCross" | "esriSFSForwardDiagonal" | "esriSFSHorizontal" | "esriSFSNull" | "esriSFSSolid" | "esriSFSVertical";
+    type SymbolType = "esriSLS" | "esriSMS" | "esriSFS" | "esriPMS" | "esriPFS" | "esriTS";
 
     interface Symbol {
         "type": SymbolType;
@@ -145,77 +145,77 @@ declare namespace ArcGis.Rest {
     }
 
     interface SimpleLineSymbol extends Symbol {
-        "type": "esriSLS",
-        "style"?: SimpleLineSymbolStyle,
-        "color"?: Color,
-        "width"?: number,
+        "type": "esriSLS";
+        "style"?: SimpleLineSymbolStyle;
+        "color"?: Color;
+        "width"?: number;
     }
 
     interface MarkerSymbol extends Symbol {
-        "angle"?: number,
-        "xoffset"?: number,
-        "yoffset"?: number,
+        "angle"?: number;
+        "xoffset"?: number;
+        "yoffset"?: number;
     }
 
     interface SimpleMarkerSymbol extends MarkerSymbol {
-        "type": "esriSMS",
-        "style"?: SimpleMarkerSymbolStyle,
-        "color"?: Color,
-        "size"?: number,
-        "outline"?: SimpleLineSymbol
+        "type": "esriSMS";
+        "style"?: SimpleMarkerSymbolStyle;
+        "color"?: Color;
+        "size"?: number;
+        "outline"?: SimpleLineSymbol;
     }
 
     interface SimpleFillSymbol extends Symbol {
-        "type": "esriSFS",
-        "style"?: SimpleFillSymbolStyle,
-        "color"?: Color
-        "outline"?: SimpleLineSymbol //if outline has been specified
+        "type": "esriSFS";
+        "style"?: SimpleFillSymbolStyle;
+        "color"?: Color;
+        "outline"?: SimpleLineSymbol; //if outline has been specified
     }
 
     interface PictureSourced {
-        "url"?: string, //Relative URL for static layers and full URL for dynamic layers. Access relative URL using http://<mapservice-url>/<layerId1>/images/<imageUrl11>
-        "imageData"?: string, //"<base64EncodedImageData>",
-        "contentType"?: string,
-        "width"?: number,
-        "height"?: number,
-        "angle"?: number,
-        "xoffset"?: number,
-        "yoffset"?: number
+        "url"?: string; //Relative URL for static layers and full URL for dynamic layers. Access relative URL using http://<mapservice-url>/<layerId1>/images/<imageUrl11>
+        "imageData"?: string; //"<base64EncodedImageData>";
+        "contentType"?: string;
+        "width"?: number;
+        "height"?: number;
+        "angle"?: number;
+        "xoffset"?: number;
+        "yoffset"?: number;
 
     }
 
     interface PictureMarkerSymbol extends MarkerSymbol, PictureSourced {
-        "type": "esriPMS",
+        "type": "esriPMS";
     }
 
     interface PictureFillSymbol extends Symbol, PictureSourced {
-        "type": "esriPFS",
-        "outline"?: SimpleLineSymbol, //if outline has been specified
-        "xscale"?: number,
-        "yscale"?: number,
+        "type": "esriPFS";
+        "outline"?: SimpleLineSymbol; //if outline has been specified
+        "xscale"?: number;
+        "yscale"?: number;
     }
 
     interface Font {
-        "family"?: string, //"<fontFamily>",
-        "size"?: number, //<fontSize>,
-        "style"?: "italic" | "normal" | "oblique"
-        "weight"?: "bold" | "bolder" | "lighter" | "normal",
-        "decoration"?: "line-through" | "underline" | "none"
+        "family"?: string; //"<fontFamily>";
+        "size"?: number; //<fontSize>;
+        "style"?: "italic" | "normal" | "oblique";
+        "weight"?: "bold" | "bolder" | "lighter" | "normal";
+        "decoration"?: "line-through" | "underline" | "none";
     }
 
     interface TextSymbol extends MarkerSymbol {
-        "type": "esriTS",
-        "color"?: Color,
-        "backgroundColor"?: Color,
-        "borderLineSize"?: number, // <size>,
-        "borderLineColor"?: Color,
-        "haloSize"?: number, // <size>,
-        "haloColor"?: Color,
-        "verticalAlignment"?: "baseline" | "top" | "middle" | "bottom",
-        "horizontalAlignment"?: "left" | "right" | "center" | "justify",
-        "rightToLeft"?: boolean,
-        "kerning"?: boolean,
-        "font"?: Font,
-        "text"?: string //only applicable when specified as a client-side graphic.
+        "type": "esriTS";
+        "color"?: Color;
+        "backgroundColor"?: Color;
+        "borderLineSize"?: number; // <size>;
+        "borderLineColor"?: Color;
+        "haloSize"?: number; // <size>;
+        "haloColor"?: Color;
+        "verticalAlignment"?: "baseline" | "top" | "middle" | "bottom";
+        "horizontalAlignment"?: "left" | "right" | "center" | "justify";
+        "rightToLeft"?: boolean;
+        "kerning"?: boolean;
+        "font"?: Font;
+        "text"?: string; //only applicable when specified as a client-side graphic.
     }
 }
--- arcgis-to-geojson-utils/index.d.ts
+++ arcgis-to-geojson-utils/index.d.ts
@@ -1,4 +1,4 @@
-// Type definitions for arcgis-to-geojson-utils
+// Type definitions for arcgis-to-geojson-utils 1.0
 // Project: https://github.com/Esri/arcgis-to-geojson-utils
 // Definitions by: Jeff Jacobson <https://github.com/JeffJacobson>
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
