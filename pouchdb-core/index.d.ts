06a4064f855132367a3f895b60a0229364320547
Jeff Barnes <jeff.barnes@gmail.com>
2016-12-05 15:33:02 -0900

    Remove PouchDB.adapter(...) definition.

--- pouchdb-core/index.d.ts
+++ pouchdb-core/index.d.ts
@@ -433,8 +433,6 @@ declare namespace PouchDB {
 
         debug: debug.IDebug;
 
-        adapter(id: string, adapter: any, addToPreferredAdapters?: boolean): void;
-
         new<Content extends Core.Encodable>(name?: string,
             options?: Configuration.DatabaseConfiguration): Database<Content>;
 
