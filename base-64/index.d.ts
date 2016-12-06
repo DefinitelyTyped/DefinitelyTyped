beea8d692ceb8c61b290ccf0d26efbe8b96d0e49
Dolan <dolan_miu@hotmail.com>
2016-12-06 16:43:14 +0000

    removed patch number

--- base-64/index.d.ts
+++ base-64/index.d.ts
@@ -1,4 +1,4 @@
-// Type definitions for base-64 0.1.0
+// Type definitions for base-64 0.1
 // Project: https://github.com/mathiasbynens/base64
 // Definitions by: Dolan Miu <https://github.com/dolanmiu>
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
