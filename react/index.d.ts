ae4045761e0a08fe8e474867d9e096aaef9542ba
Eric Anderson <e@ericlanderson.com>
2016-12-07 13:39:39 -0500

    Support Partial<> on setState now that TS 2.1 is out

--- react/index.d.ts
+++ react/index.d.ts
@@ -165,8 +165,8 @@ declare namespace React {
     class Component<P, S> implements ComponentLifecycle<P, S> {
         constructor(...args: any[]);
         constructor(props?: P, context?: any);
-        setState(f: (prevState: S, props: P) => S, callback?: () => any): void;
-        setState(state: S, callback?: () => any): void;
+        setState(f: (prevState: S, props: P) => Partial<S>, callback?: () => any): void;
+        setState(state: Partial<S>, callback?: () => any): void;
         forceUpdate(callBack?: () => any): void;
         render(): JSX.Element | null;
 
