c908783b9cbfecf90822a842c973b12e629f4fd8
Jake Thompson <jake@pocketlist.co>
2016-12-07 12:13:48 -0500

    better linting

--- serverless/index.d.ts
+++ serverless/index.d.ts
@@ -8,196 +8,196 @@ export as namespace Serverless;
 
 declare namespace Serverless {
     interface LambdaContext {
-        awsRequestId: string
-        invokeId: string
-        functionName: string
-        functionVersion: string
-        isDefaultFunctionVersion: boolean
-        invokedFunctionArn: string
-        logGroupName: string
-        logStreamName: string
-        memoryLimitInMB: number
+        awsRequestId: string;
+        invokeId: string;
+        functionName: string;
+        functionVersion: string;
+        isDefaultFunctionVersion: boolean;
+        invokedFunctionArn: string;
+        logGroupName: string;
+        logStreamName: string;
+        memoryLimitInMB: number;
     }
 
     type Event = APIGatewayProxyRequestEvent | SNSEvent | KinesisEvent | S3Event | ConfigEvent | CognitoEvent;
 
     export interface APIGatewayProxyRequestEvent {
-        resource: string
-        path: string
-        httpMethod: string
-        headers: HTTPHeaders
-        queryStringParameters: APIGQueryStringParameters
-        pathParameters: APIGPathParameters
-        stageVariables: APIGStageVariables
-        requestContext: APIGProxyRequestContext
-        body: string
-        isBase64Encoded: boolean
+        resource: string;
+        path: string;
+        httpMethod: string;
+        headers: HTTPHeaders;
+        queryStringParameters: APIGQueryStringParameters;
+        pathParameters: APIGPathParameters;
+        stageVariables: APIGStageVariables;
+        requestContext: APIGProxyRequestContext;
+        body: string;
+        isBase64Encoded: boolean;
     }
 
     interface APIGProxyRequestContext {
-        accountId: string
-        resourceId: string
-        stage: string
-        requestId: string
-        identity: RequestIdentity
-        resourcePath: string
-        httpMethod: string
-        apiId: string
+        accountId: string;
+        resourceId: string;
+        stage: string;
+        requestId: string;
+        identity: RequestIdentity;
+        resourcePath: string;
+        httpMethod: string;
+        apiId: string;
     }
 
     interface RequestIdentity {
-        cognitoIdentityPoolId: string
-        accountId: string
-        cognitoIdentityId: string
-        caller: string
-        apiKey: string
-        sourceIp: string
-        accessKey: string
-        cognitoAuthenticationType: string
-        cognitoAuthenticationProvider: string
-        userArn: string
-        userAgent: string
-        user: string
+        cognitoIdentityPoolId: string;
+        accountId: string;
+        cognitoIdentityId: string;
+        caller: string;
+        apiKey: string;
+        sourceIp: string;
+        accessKey: string;
+        cognitoAuthenticationType: string;
+        cognitoAuthenticationProvider: string;
+        userArn: string;
+        userAgent: string;
+        user: string;
     }
 
     interface DynamoDBEvent {
-        records: Array<DynamoDBStreamRecord>
+        records: Array<DynamoDBStreamRecord>;
     }
 
     interface DynamoDBStreamRecord extends DynamoDBRecord {
-        eventSourceArn: string
+        eventSourceArn: string;
     }
 
-    interface DynamoDBRecord { [key: string]: any }
+    interface DynamoDBRecord { [key: string]: any; }
 
     interface SNSEvent {
-        records: Array<SNSRecord>
+        records: Array<SNSRecord>;
     }
 
     interface SNSRecord {
-        eventSource: string
-        eventSubscriptionArd: string
-        eventVersion: string
-        sns: SNSMessage
+        eventSource: string;
+        eventSubscriptionArd: string;
+        eventVersion: string;
+        sns: SNSMessage;
     }
 
     interface SNSMessage {
-        message: string
-        messageAttributes: SNSMessageAttributes
-        messageId: string
-        signature: string
-        signingCertUrl: string
-        subject: string
-        timestamp: string
-        topicArn: string
-        type: string
-        unsubscribeUrl: string
+        message: string;
+        messageAttributes: SNSMessageAttributes;
+        messageId: string;
+        signature: string;
+        signingCertUrl: string;
+        subject: string;
+        timestamp: string;
+        topicArn: string;
+        type: string;
+        unsubscribeUrl: string;
     }
 
     interface SNSMessageAttribute {
-        type: string
-        value: string
+        type: string;
+        value: string;
     }
 
     interface KinesisEvent {
-        records: Array<KinesisEventRecord>
+        records: Array<KinesisEventRecord>;
     }
 
     interface KinesisEventRecord {
-        awsRegion: string
-        eventId: string
-        eventName: string
-        eventSource: string
-        eventSourceArn: string
-        eventVersion: string
-        invokeIdentityArn: string
-        kinesis: any //KinesisRecord
+        awsRegion: string;
+        eventId: string;
+        eventName: string;
+        eventSource: string;
+        eventSourceArn: string;
+        eventVersion: string;
+        invokeIdentityArn: string;
+        kinesis: any; // TODO: Change to KinesisRecord
     }
 
     interface S3Event {
-        eventVersion: string
-        eventSource: string
-        awsRegion: string
-        eventTime: string
-        eventName: string
-        userIdentity: S3UserIdentity
-        requestParmeters: S3RequestParameters
-        responseElements: S3ResponseElements
-        s3: S3Entity
+        eventVersion: string;
+        eventSource: string;
+        awsRegion: string;
+        eventTime: string;
+        eventName: string;
+        userIdentity: S3UserIdentity;
+        requestParmeters: S3RequestParameters;
+        responseElements: S3ResponseElements;
+        s3: S3Entity;
     }
 
     interface S3UserIdentity {
-        principalId: string
+        principalId: string;
     }
 
     interface S3RequestParameters {
-        sourceIpAddress: string
+        sourceIpAddress: string;
     }
 
     interface S3Entity {
-        s3SchemeVersion: string
-        configurationId: string
-        bucket: S3Bucket
-        object: S3Object
+        s3SchemeVersion: string;
+        configurationId: string;
+        bucket: S3Bucket;
+        object: S3Object;
 
     }
 
     interface S3Bucket {
-        name: string
-        arn: string
-        ownerIdentity: S3UserIdentity
+        name: string;
+        arn: string;
+        ownerIdentity: S3UserIdentity;
     }
 
     interface S3Object {
-        key: string
-        size: number
-        eTag: string
-        versionId: string
+        key: string;
+        size: number;
+        eTag: string;
+        versionId: string;
     }
 
     export interface CognitoEvent {
-        datasetName: string
-        datasetRecords: CognitoDatasetRecords
-        eventType: string
-        identityId: string
-        identityPoolId: string
-        region: string
-        version: number
+        datasetName: string;
+        datasetRecords: CognitoDatasetRecords;
+        eventType: string;
+        identityId: string;
+        identityPoolId: string;
+        region: string;
+        version: number;
     }
 
     interface CognitoDatasetRecord {
-        newValue: string
-        oldValue: string
-        op: string
+        newValue: string;
+        oldValue: string;
+        op: string;
     }
 
     export interface ConfigEvent {
-        accountId: string
-        configRuleArn: string
-        configRuleId: string
-        configRuleName: string
-        eventLeftScope: boolean
-        executionRoleArn: string
-        invokingEvent: string
-        resultToken: string
-        ruleParameters: string
-        version: string
+        accountId: string;
+        configRuleArn: string;
+        configRuleId: string;
+        configRuleName: string;
+        eventLeftScope: boolean;
+        executionRoleArn: string;
+        invokingEvent: string;
+        resultToken: string;
+        ruleParameters: string;
+        version: string;
     }
 
     interface Response { }
 
-    type HTTPHeaders = { [key: string]: string }
-    type APIGQueryStringParameters = { [key: string]: string }
-    type APIGPathParameters = { [key: string]: string }
-    type APIGStageVariables = { [key: string]: string }
-    type CognitoDatasetRecords = { [key: string]: CognitoDatasetRecord }
-    type SNSMessageAttributes = { [key: string]: SNSMessageAttribute }
-    type S3ResponseElements = { [key: string]: string }
+    type HTTPHeaders = { [key: string]: string; };
+    type APIGQueryStringParameters = { [key: string]: string; };
+    type APIGPathParameters = { [key: string]: string; };
+    type APIGStageVariables = { [key: string]: string; };
+    type CognitoDatasetRecords = { [key: string]: CognitoDatasetRecord; };
+    type SNSMessageAttributes = { [key: string]: SNSMessageAttribute; };
+    type S3ResponseElements = { [key: string]: string; };
 
     export interface APIGatewayProxyResponse extends Response {
-        statusCode: number
-        headers?: HTTPHeaders
-        body: string
+        statusCode: number;
+        headers?: HTTPHeaders;
+        body: string;
     }
 
     export type Callback<R> = (error: Error, body: R) => void;
