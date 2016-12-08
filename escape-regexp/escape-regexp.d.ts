1780c2719e721d8484b9431511eb127fe0d59f91
syuilo <syuilotan@yahoo.co.jp>
2016-12-08 16:45:21 +0900

    Added declarations for 'escape-regexp'.

---
+++ escape-regexp/escape-regexp.d.ts
@@ -0,0 +1,8 @@
+// Type definitions for escape-regexp v0.0.1
+// Project: https://www.npmjs.com/package/escape-regexp
+// Definitions by: syuilo <https://github.com/syuilo>
+// Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
+
+declare function escape(str: string): string;
+
+export = escape;
