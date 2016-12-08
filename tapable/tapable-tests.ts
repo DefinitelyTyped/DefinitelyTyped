666bff50137ba3d7a67fee95fbdde78517f026b3
e-cloud <saintscott119@gmail.com>
2016-12-08 21:33:05 +0800

    feat: upgrade to tapbale v0.2.5

--- tapable/tapable-tests.ts
+++ tapable/tapable-tests.ts
@@ -29,12 +29,16 @@ let callback: Tapable.CallbackFunction = function () {
 compiler.apply(new DllPlugin());
 
 compiler.applyPlugins('doSomething', 'a', 'b');
+compiler.applyPlugins0('doSomething');
+compiler.applyPlugins1('doSomething', 'a');
+compiler.applyPlugins2('doSomething', 'a', 'b');
 compiler.applyPluginsWaterfall('doSomething', 'a', 'b');
 compiler.applyPluginsWaterfall0('doSomething', 'a');
 compiler.applyPluginsBailResult('doSomething', 'a', 'b');
 compiler.applyPluginsBailResult1('doSomething', ['a', 'b']);
 compiler.applyPluginsAsync('doSomething', 'a', 'b');
 compiler.applyPluginsAsyncSeries('doSomething', 'a', 'b');
+compiler.applyPluginsAsyncSeries1('doSomething', 'a', callback);
 compiler.applyPluginsAsyncSeriesBailResult('doSomething', 'a', 'b');
 compiler.applyPluginsAsyncSeriesBailResult1('doSomething', 'a', callback);
 compiler.applyPluginsAsyncWaterfall('doSomething', 'a', callback);
