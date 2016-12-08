b469c76ea5507a86875d98fb25d1fe59bccdbebc
Andy Hanson <anhans@microsoft.com>
2016-12-08 09:57:56 -0800

    "types" in tsconfig should be empty.

--- babel-code-frame/babel-code-frame-tests.ts
+++ babel-code-frame/babel-code-frame-tests.ts
@@ -1,18 +1,14 @@
 import codeFrame from "babel-code-frame";
 
-describe('babel-code-frame', () => {
-    it('frames given code', () => {
-        const code = `
-            const number = 1;
-            var string = 'foo';
+const code = `
+    const number = 1;
+    var string = 'foo';
 
-            function print(name: string) {
-                console.log(string + name);
-            }
-        `;
+    function print(name: string) {
+        console.log(string + name);
+    }
+`;
 
-        codeFrame(code, 5, 22);
-        codeFrame(code, 5, 22, { forceColor: true });
-        codeFrame(code, 2, 2, { highlightCode: true });
-    });
-});
+codeFrame(code, 5, 22);
+codeFrame(code, 5, 22, { forceColor: true });
+codeFrame(code, 2, 2, { highlightCode: true });
