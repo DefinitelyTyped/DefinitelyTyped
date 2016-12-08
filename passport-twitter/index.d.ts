bc5e2ad94a42f12e7b02fa535ff882dd68bfe453
Delaney Gillilan <DelaneyG@rwin.local>
2016-12-08 10:51:14 -0800

    update header per pull request template

--- passport-twitter/index.d.ts
+++ passport-twitter/index.d.ts
@@ -1,5 +1,5 @@
-// Type definitions for passport-facebook 1.0.3
-// Project: https://github.com/jaredhanson/passport-facebook
+// Type definitions for passport-twitter 1.0.4
+// Project: https://github.com/jaredhanson/passport-twitter
 // Definitions by: James Roland Cabresos <https://github.com/staticfunction>
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
 
