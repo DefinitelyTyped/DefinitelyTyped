dfe7ce091cd1be43c9f04489ac1b34b883cff17f
Xavier Stouder <xavier@stouder.io>
2016-12-09 19:44:15 +0100

    Fix CI

--- koa-pug/index.d.ts
+++ koa-pug/index.d.ts
@@ -2,38 +2,33 @@
 // Project: https://github.com/chrisyip/koa-pug
 // Definitions by: Xavier Stouder <https://github.com/Xstoudi>
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
+import * as koa from "koa";
 
-declare module "koa-pug" {
-    import * as koa from "koa";
-
-    namespace Koa {
-        interface Context {
-            render(viewPath: string, locals?: Pug.PugLocals, overrideOpts?: Pug.PugCtxOptions, noCache?: boolean): void;
-            render(pugStr: string, locals?: Pug.PugLocals, overrideOpts?: Pug.PugCtxOptions): void;
-            render(pugStr: string, locals?: Pug.PugLocals, noCache?: boolean): void;
-        }
+export namespace Koa {
+    interface Context {
+        render(viewPath: string, locals?: Pug.PugLocals, overrideOpts?: Pug.PugCtxOptions, noCache?: boolean): void;
+        render(pugStr: string, locals?: Pug.PugLocals, overrideOpts?: Pug.PugCtxOptions): void;
+        render(pugStr: string, locals?: Pug.PugLocals, noCache?: boolean): void;
     }
-    class Pug {
-        constructor(options?: Pug.PugOptions);
-        use(app: koa): void;
+}
+export class Pug {
+    constructor(options?: Pug.PugOptions);
+    use(app: koa): void;
+}
+export namespace Pug {
+    export interface PugOptions {
+        viewPath?: string;
+        pretty?: boolean;
+        compileDebug?: boolean;
+        debug?: boolean;
+        locals?: PugLocals;
+        noCache?: boolean;
+        helperPath?: string | Array<string | { [str: string]: any }>;
+        basedir?: string;
+        app?: koa;
     }
-    namespace Pug {
-        export interface PugOptions {
-            viewPath?: string;
-            pretty?: boolean;
-            compileDebug?: boolean;
-            debug?: boolean;
-            locals?: PugLocals;
-            noCache?: boolean;
-            helperPath?: string | Array<string | { [str: string]: any }>;
-            basedir?: string;
-            app?: koa;
-        }
-        export interface PugCtxOptions extends PugOptions {
-            fromString: boolean;
-        }
-        export interface PugLocals { [str: string]: any; }
+    export interface PugCtxOptions extends PugOptions {
+        fromString: boolean;
     }
-
-    export = Pug;
+    export interface PugLocals { [str: string]: any; }
 }
