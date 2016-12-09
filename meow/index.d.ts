ef735cb71a52c4c8011c26610371a5f01d6c0998
Markus Wolf <mail@markus-wolf.de>
2016-12-09 10:01:15 +0100

    style: use 4 spaces

--- meow/index.d.ts
+++ meow/index.d.ts
@@ -9,22 +9,22 @@ import * as minimist from 'minimist';
 declare function meow(options: string | string[] | meow.Options, minimistOptions?: minimist.Opts): meow.Result;
 declare namespace meow {
 
-  export interface Options {
-    description?: string | boolean;
-    help?: string | boolean;
-    version?: string | boolean;
-    pkg?: any;
-    argv?: string[];
-    inferType?: boolean;
-  }
+    export interface Options {
+        description?: string | boolean;
+        help?: string | boolean;
+        version?: string | boolean;
+        pkg?: any;
+        argv?: string[];
+        inferType?: boolean;
+    }
 
-  export interface Result {
-    input: string[];
-    flags: { [name: string]: any };
-    pkg: any;
-    help: string;
-    showHelp(code: number): void;
-  }
+    export interface Result {
+        input: string[];
+        flags: { [name: string]: any };
+        pkg: any;
+        help: string;
+        showHelp(code: number): void;
+    }
 
 }
 
