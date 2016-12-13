a7b62c729e854fdbbc330592ef2f397c8971291c
Graham Mendick <graham.mendick@gmail.com>
2016-12-13 20:02:13 +0000

    Replaced tabs with spaces to match index.d.ts

--- navigation-react/navigation-react-tests.tsx
+++ navigation-react/navigation-react-tests.tsx
@@ -3,76 +3,76 @@ import { NavigationBackLink, NavigationLink, RefreshLink } from 'navigation-reac
 import * as React from 'react';
 
 var stateNavigator = new StateNavigator([
-	{ key: 'people', route: 'people/{page}' },
-	{ key: 'person', route: 'person/{id}', trackCrumbTrail: true }
+    { key: 'people', route: 'people/{page}' },
+    { key: 'person', route: 'person/{id}', trackCrumbTrail: true }
 ]);
 
 // Refresh Link
 var RefreshLinkTest = function() {
-	return <RefreshLink>People</RefreshLink>;
+    return <RefreshLink>People</RefreshLink>;
 }
 
 RefreshLinkTest = function() {
-	return (
-		<RefreshLink
-			navigationData={{ page: 2 }}
-			includeCurrentData={true}
-			currentDataKeys="sort"
-			activeCssClass="active"
-			disableActive={true}
-			lazy={false}
-			historyAction="replace"
-			navigating= {(e: MouseEvent, domId: string, link: string) => true} 
-			stateNavigator={stateNavigator}
-			target="_blank"
-			aria-label="Go to the second page of people">
-			People
-		</RefreshLink>
-	);
+    return (
+        <RefreshLink
+            navigationData={{ page: 2 }}
+            includeCurrentData={true}
+            currentDataKeys="sort"
+            activeCssClass="active"
+            disableActive={true}
+            lazy={false}
+            historyAction="replace"
+            navigating= {(e: MouseEvent, domId: string, link: string) => true} 
+            stateNavigator={stateNavigator}
+            target="_blank"
+            aria-label="Go to the second page of people">
+            People
+        </RefreshLink>
+    );
 }
 
 // Navigation Link
 var NavigationLinkTest = function() {
-	return <NavigationLink stateKey="person">Person</NavigationLink>;
+    return <NavigationLink stateKey="person">Person</NavigationLink>;
 }
 
 NavigationLinkTest = function() {
-	return (
-		<NavigationLink
-			stateKey="person"
-			navigationData={{ id: 12 }}
-			includeCurrentData={false}
-			currentDataKeys=""
-			activeCssClass=""
-			disableActive={false}
-			lazy={false}
-			historyAction="add"
-			navigating= {(e: MouseEvent, domId: string, link: string) => true} 
-			stateNavigator={stateNavigator}
-			target="_blank"
-			aria-label="View the person's details">
-			Person
-		</NavigationLink>
-	);
+    return (
+        <NavigationLink
+            stateKey="person"
+            navigationData={{ id: 12 }}
+            includeCurrentData={false}
+            currentDataKeys=""
+            activeCssClass=""
+            disableActive={false}
+            lazy={false}
+            historyAction="add"
+            navigating= {(e: MouseEvent, domId: string, link: string) => true} 
+            stateNavigator={stateNavigator}
+            target="_blank"
+            aria-label="View the person's details">
+            Person
+        </NavigationLink>
+    );
 }
 
 // Navigation Back Link
 var NavigationBackLinkTest = function() {
-	return <NavigationBackLink distance={1}>People</NavigationBackLink>;
+    return <NavigationBackLink distance={1}>People</NavigationBackLink>;
 }
 
 NavigationBackLinkTest = function() {
-	return (
-		<NavigationBackLink
-			distance={1}
-			lazy={false}
-			historyAction="none"
-			navigating= {(e: MouseEvent, domId: string, link: string) => true} 
-			stateNavigator={stateNavigator}
-			target="_blank"
-			aria-label="Go back to the list of people">
-			People
-		</NavigationBackLink>
-	);
+    return (
+        <NavigationBackLink
+            distance={1}
+            lazy={false}
+            historyAction="none"
+            navigating= {(e: MouseEvent, domId: string, link: string) => true} 
+            stateNavigator={stateNavigator}
+            target="_blank"
+            aria-label="Go back to the list of people">
+            People
+        </NavigationBackLink>
+    );
 }
 
