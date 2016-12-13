ef4a736a1ca4247ef299568e715504d96995e221
Vincent Rouffiat <vincent.rouffiat@gmail.com>
2016-12-13 14:26:14 +0100

    feat(react-modal): update typing react-modal for version 1.6.0

--- react-modal/react-modal.d.ts
+++ react-modal/react-modal.d.ts
@@ -1,4 +1,4 @@
-// Type definitions for react-modal v1.3.0
+// Type definitions for react-modal v1.6.0
 // Project: https://github.com/reactjs/react-modal
 // Definitions by: Rajab Shakirov <https://github.com/radziksh>
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
@@ -6,25 +6,29 @@
 /// <reference types="react"/>
 
 declare module "react-modal" {
+    interface StyleReactModal {
+        content?: {
+            [key: string]: any;
+        };
+        overlay?: {
+            [key: string]: any;
+        };
+    }
+
     interface ReactModal {
         isOpen: boolean;
-        style?: {
-            content?: {
-                [key: string]: any;
-            },
-            overlay?: {
-                [key: string]: any;
-            }
-        },
-        appElement?: HTMLElement | {},
-        onAfterOpen?: Function,
-        onRequestClose?: Function,
-        closeTimeoutMS?: number,
-        ariaHideApp?: boolean,
-        shouldCloseOnOverlayClick?: boolean,
-        overlayClassName?: string,
-        className?: string
+        contentLabel: string;
+        style?: StyleReactModal;
+        appElement?: HTMLElement | {};
+        onAfterOpen?: () => void;
+        onRequestClose?: () => void;
+        closeTimeoutMS?: number;
+        ariaHideApp?: boolean;
+        shouldCloseOnOverlayClick?: boolean;
+        overlayClassName?: string;
+        className?: string;
     }
+
     let ReactModal: React.ClassicComponentClass<ReactModal>;
     export = ReactModal;
 }
