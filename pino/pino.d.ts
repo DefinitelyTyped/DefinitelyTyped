0508339131a525ed40c2a65bc0a038d3d2855c2e
Peter Snider <snider.peter@gmail.com>
2016-12-13 05:53:26 -0800

    pino.d.ts updated to v3.0.5
    added namespace to expose interfaces

--- pino/pino.d.ts
+++ pino/pino.d.ts
@@ -1,17 +1,23 @@
-// Type definitions for pino v1.0.1
+// Type definitions for pino v3.0.5 
 // Project: https://github.com/mcollina/pino.git
 // Definitions by: Peter Snider <https://github.com/psnider>
 // Definitions: https://github.com/borisyankov/DefinitelyTyped
 
-/// <reference path="../node/node.d.ts"/>   // only for require()
+/// <reference path='../node/node.d.ts' />
 
-declare module 'pino' {
 
-    import stream = require('stream')
+declare namespace Pino {
 
-    type Level = 'fatal' | 'error' | 'warn' | 'info' | 'debug' | 'trace' | 'silent'
-    type Headers = {[header: string]: string}
-    type LevelLabelsToValues = {[level: string]: number}
+    type CustomLevel = string
+    type Level = 'fatal' | 'error' | 'warn' | 'info' | 'debug' | 'trace' | 'silent' | CustomLevel
+    type LevelLabelsToValues = {
+        trace: number
+        debug: number
+        info: number
+        warn: number
+        error: number
+        fatal: number
+    }
     type LevelValuesToLabels = {[level: number]: string}
 
     interface Serializers {
@@ -19,6 +25,7 @@ declare module 'pino' {
         res?: (res: any) => any
         err?: (error: Error) => any
     }
+
 
     interface LoggerOptions {
         // avoid error causes by circular references in the object tree, default true
@@ -35,31 +42,28 @@ declare module 'pino' {
         // Enables extreme mode, yields an additional 60% performance (from 250ms down to 100ms per 10000 ops).
         // There are trade-off's should be understood before usage. See Extreme mode explained. default false
         extreme?: boolean
+        // one of 'fatal', 'error', 'warn', 'info', 'debug', 'trace';
+        // also 'silent' is supported to disable logging.
+        // Any other value defines a custom level and requires supplying a level value via levelVal.
+        level?: Level
+        // when defining a custom log level via level, set to an integer value to define the new level. Defaults to undefined.
+        levelVal?: number
         // enables logging, defaults to true.
         enabled?: boolean
-        level?: Level
+        stdSerializers?: Pino.Serializers
     }
 
-    interface Pino {
-        (optionsOrStream?: LoggerOptions | stream.Writable | stream.Readable): Logger
-        (options: LoggerOptions, stream: stream.Writable | stream.Readable): Logger
-        levels: {
-            values: LevelLabelsToValues
-            labels: LevelValuesToLabels
-        }
-        LOG_VERSION: number
-        pretty(opts?: {timeTransOnly?: boolean}): stream.Readable
 
-    }
+    type LoggerEventListener = (lvl: string, val: number, prevLvl: string, prevVal: number) => void
 
-    type LevelChangeEventListener = (lvl: string, val: number, prevLvl: string, prevVal: number) => void
-    type LevelChangeEvent = 'level-change'
 
     interface Logger {
         child(bindings: {}): Logger
         level: Level
         levelVal: number
-        on(event: LevelChangeEvent, listener: LevelChangeEventListener): void
+        on(event: 'level-change', listener: LoggerEventListener): void
+        on(event: 'error', listener: (error: Error) => void): void
+        removeListener(event: 'level-change', listener: LoggerEventListener): void
         fatal(msg: string, ...args : any[]): void
         fatal(obj: {}, msg?: string, ...args : any[]): void
         error(msg: string, ...args : any[]): void
@@ -72,12 +76,44 @@ declare module 'pino' {
         debug(obj: {}, msg?: string, ...args : any[]): void
         trace(msg: string, ...args : any[]): void
         trace(obj: {}, msg?: string, ...args : any[]): void
+        flush(): void
+        addLevel(name: string, lvl: number): void
         levels: {
             values: LevelLabelsToValues
             labels: LevelValuesToLabels
         }
         LOG_VERSION: number
-        stdSerializers: Serializers
+        // Custom log levels are also supported, but TypeScript doesn't have a way to declare this use case.
+        // If you define one, you may create another interface, for example:
+        // interface LoggerWithMyLevel extends Pino.Logger {
+        //   myLevel(msg: string, ...args : any[]): void
+        //   myLevel(obj: {}, msg?: string, ...args : any[]): void
+        // }
+    }
+
+
+}
+
+
+declare module 'pino' {
+
+    import stream = require('stream')
+
+    interface PrettyOptions {
+        timeTransOnly?: boolean
+        formatter?: () => string
+    }
+
+    interface Pino {
+        (optionsOrStream?: Pino.LoggerOptions | stream.Writable | stream.Readable): Pino.Logger
+        (options: Pino.LoggerOptions, stream: stream.Writable | stream.Readable): Pino.Logger
+        levels: {
+            values: Pino.LevelLabelsToValues
+            labels: Pino.LevelValuesToLabels
+        }
+        LOG_VERSION: number
+        pretty(opts?: PrettyOptions): stream.Readable
+        stdSerializers: Pino.Serializers
     }
 
     var p: Pino
