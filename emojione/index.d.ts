2fc133fc04d4b5a3a67f7e47559a122eb91dcd0a
Danilo Bargen <mail@dbrgn.ch>
2016-12-13 18:01:08 +0100

    Export as namespace

--- emojione/index.d.ts
+++ emojione/index.d.ts
@@ -3,18 +3,18 @@
 // Definitions by: Danilo Bargen <https://github.com/dbrgn/>
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
 
-declare var emojione: {
-    sprites: boolean,
-    imagePathPNG: string,
-    imagePathSVG: string,
-    imagePathSVGSprites: string,
-    imageType: 'png' | 'svg',
-    unicodeAlt: boolean,
-    ascii: boolean,
-    unicodeRegexp: string,
-    cacheBustParam: string,
-    toShort: (str: string) => string,
-    toImage: (str: string) => string,
-    shortnameToImage: (str: string) => string,
-    unicodeToImage: (str: string) => string,
-};
+export as namespace emojione;
+
+export var sprites: boolean;
+export var imagePathPNG: string;
+export var imagePathSVG: string;
+export var imagePathSVGSprites: string;
+export var imageType: 'png' | 'svg';
+export var unicodeAlt: boolean;
+export var ascii: boolean;
+export var unicodeRegexp: string;
+export var cacheBustParam: string;
+export function toShort(str: string): string;
+export function toImage(str: string): string;
+export function shortnameToImage(str: string): string;
+export function unicodeToImage(str: string): string;
