e1a1c3ad4707c4fd7dabb0c85de8901e0b3cb066
Danilo Bargen <mail@dbrgn.ch>
2016-12-14 19:45:13 +0100

    Add version to header

--- emojione/index.d.ts
+++ emojione/index.d.ts
@@ -1,4 +1,4 @@
-// Type definitions for emojione
+// Type definitions for emojione 2.2
 // Project: https://github.com/Ranks/emojione
 // Definitions by: Danilo Bargen <https://github.com/dbrgn/>
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
