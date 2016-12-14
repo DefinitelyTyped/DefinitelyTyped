cc5dab981c4a402313834249df2ef9b94ee91fa6
Tobias Wolff <Tobias_Wolff@gmx.net>
2016-12-14 13:17:03 +0100

    adding typescript definitions for koa-basic-auth and swagger-parser packages

---
+++ swagger-parser/index.d.ts
@@ -0,0 +1,10 @@
+// Type definitions for koa-basic-auth v2.x
+// Project: https://github.com/BigstickCarpet/swagger-parser
+// Definitions by: Tobias Wolff <https://github.com/Tobias4872/>
+// Definitions: https://github.com/Tobias4872/DefinitelyTyped
+
+declare namespace SwaggerParser {
+  function parse(path: string): any;
+}
+
+export = SwaggerParser;
