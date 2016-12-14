b447274c6edee6ca185e29691bf0c313bbd97e6a
Peter Snider <snider.peter@gmail.com>
2016-12-14 13:02:47 -0800

    reverted back to not use types

--- pino/index.d.ts
+++ pino/index.d.ts
@@ -3,6 +3,7 @@
 // Definitions by: Peter Snider <https://github.com/psnider>
 // Definitions: https://github.com/borisyankov/DefinitelyTyped
 
+/// <reference types="node"/>
 
 
 declare namespace Pino {
