61da2fb04d48ed3fbbd777ce391cb70e649c512d
Horiuchi_H <horiuchi.g@gmail.com>
2016-12-15 11:12:56 +0900

    add version number

--- storejs/index.d.ts
+++ storejs/index.d.ts
@@ -1,4 +1,4 @@
-// Type definitions for store.js
+// Type definitions for store.js 1.3
 // Project: https://github.com/marcuswestin/store.js/
 // Definitions by: Vincent Bortone <https://github.com/vbortone/>
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
