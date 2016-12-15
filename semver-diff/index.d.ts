5ca7b1bc670a934fd8709274601262912cd11c9c
Chris Barr <cbarr@appriver.com>
2016-12-15 12:55:17 -0500

    Fix version number linting violation

--- semver-diff/index.d.ts
+++ semver-diff/index.d.ts
@@ -1,4 +1,4 @@
-// Type definitions for semver-diff v2.1.0
+// Type definitions for semver-diff 2.1
 // Project: https://github.com/sindresorhus/semver-diff
 // Definitions by: Chris Barr <https://github.com/chrismbarr>
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped/tree/master/semver-diff
