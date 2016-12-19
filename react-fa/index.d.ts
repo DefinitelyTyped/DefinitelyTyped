79fb28ccabf744bed5d7a98763112f36f9f7a0e4
Karol Janyst <lapkom@gmail.com>
2016-12-19 12:44:55 +0900

    Change icon interfaces to be more correct

--- react-fa/index.d.ts
+++ react-fa/index.d.ts
@@ -1,35 +1,35 @@
-// Type definitions for react-fa v4.0.0
+// Type definitions for react-fa 4.1
 // Project: https://github.com/andreypopp/react-fa
-// Definitions by: Frank Laub <https://github.com/flaub>
+// Definitions by: Frank Laub <https://github.com/flaub>, Karol Janyst <https://github.com/LKay>
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
 
-/// <reference types="react" />
+import { Component, ComponentClass, HTMLAttributes, StatelessComponent } from "react";
 
+type IconSize = "lg" | "2x" | "3x" | "4x" | "5x";
 
-import { Component, ClassAttributes  } from 'react';
+type CustomComponent = string | ComponentClass<any> | StatelessComponent<any>;
 
-type IconSize = "lg" | "2x" | "3x" | "4x" | "5x"
-
-interface IconProps extends ClassAttributes<Icon> {
-    name: string
-    className?: string
-    size?: IconSize
-    rotate?: "45" | "90" | "135" | "180" | "225" | "270" | "315"
-    flip?: "horizontal" | "vertical"
-    fixedWidth?: boolean
-    spin?: boolean
-    pulse?: boolean
-    stack?: "1x" | "2x"
-    inverse?: boolean
-    Component?: string | Function
+interface IconProps {
+    name: string;
+    rotate?: "45" | "90" | "135" | "180" | "225" | "270" | "315";
+    flip?: "horizontal" | "vertical";
+    fixedWidth?: boolean;
+    spin?: boolean;
+    pulse?: boolean;
+    stack?: "1x" | "2x";
+    inverse?: boolean;
+    Component?: CustomComponent;
+    size?: IconSize;
+    [prop: string]: any;
 }
 
 export class Icon extends Component<IconProps, {}> {}
 
-interface IconStackProps extends ClassAttributes<IconStack> {
-    className?: string
-    size?: IconSize
+interface IconStackProps {
+    size?: IconSize;
+    [props: string]: any;
 }
+
 export class IconStack extends Component<IconStackProps, {}> {}
 
 export default Icon;
