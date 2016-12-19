21310fd008e7bc6713a0ae71e7abde0610c0a456
Bjørn <bjoern.soerensen@gmail.com>
2016-12-19 17:57:07 +0100

    Add optics-agent typings

---
+++ optics-agent/index.d.ts
@@ -0,0 +1,85 @@
+// Type definitions for optics-agent 1.0
+// Project: https://github.com/apollostack/optics-agent-js
+// Definitions by: Crevil <https://github.com/crevil>
+// Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
+
+import { GraphQLSchema } from "graphql";
+import { Request, Response } from "express";
+import { Server } from "hapi";
+
+declare namespace OpticsAgent {
+  export interface Options {
+    /**
+     * Your API key for the Optics service.
+     *
+     * This defaults to the OPTICS_API_KEY environment variable, but can be overridden here.
+     */
+    apiKey?: string;
+
+    /**
+     * Called to determine the query shape for for a GraphQL query.
+     *
+     * You shouldn't need to set this unless you are debugging.
+     */
+    normalizeQuery?: (graphQLResolveInfo: any) => string;
+    /**
+     * Where to send the reports. Defaults to the production Optics endpoint, or the OPTICS_ENDPOINT_URL environment variable if it is set.
+     *
+     * You shouldn't need to set this unless you are debugging.
+     */
+    endpointUrl?: string;
+
+    /**
+     * HTTP proxy to use when sending reports. Default to no proxying, or the HTTPS_PROXY environment variable if it is set.
+     *
+     * You should only set this when your servers cannot connect directly to the Optics service.
+     */
+    proxyUrl?: string;
+
+    /**
+     * How often to send reports in milliseconds. Defaults to 1 minute. Minimum 10 seconds.
+     *
+     * You shouldn't need to set this unless you are debugging.
+     */
+    reportIntervalMs?: number;
+    /**
+     * Print a JSON version of reports as they are sent. This may be useful for debugging. Defaults to false.
+     */
+    printReports?: boolean;
+    /**
+     * Send detailed traces along with usage reports. Defaults to true.
+     */
+    reportTraces?: boolean;
+    /**
+     * Send the query variables along with traces. Defaults to true.
+     */
+    reportVariables?: boolean;
+  }
+
+  export interface Agent {
+    new(options: Options): Agent;
+
+    configureAgent(options: OpticsAgent.Options): OpticsAgent.Agent;
+    instrumentSchema(schema: GraphQLSchema): void;
+    middleware(): (req: Request, res: Response, next?: any) => void;
+    instrumentHapiServer(server: Server): void;
+    context(req: Request): any;
+  }
+}
+
+export function configureAgent(options: OpticsAgent.Options): OpticsAgent.Agent;
+export function instrumentSchema(schema: GraphQLSchema): void;
+export function middleware(): (req: Request, res: Response, next?: any) => void;
+export function instrumentHapiServer(server: Server): void;
+export function context(req: Request): any;
+
+export const Agent: OpticsAgent.Agent;
+
+export default {
+  configureAgent,
+  instrumentSchema,
+  middleware,
+  instrumentHapiServer,
+  context,
+  Agent,
+};
---
+++ optics-agent/optics-agent-tests.ts
@@ -0,0 +1,39 @@
+import OpticsAgent, {
+  configureAgent,
+  instrumentSchema,
+  middleware,
+  instrumentHapiServer,
+  context,
+  Agent,
+} from 'optics-agent';
+import { GraphQLSchema } from 'graphql';
+import * as express from 'express';
+import * as hapi from 'hapi';
+var OpticsAgentRequired = require('optics-agent');
+
+const configOptions = {
+  apiKey: "",
+  reportTraces: false,
+  reportVariables: false,
+  printReports: false,
+  normalizeQuery: (info: any) => "",
+  endpointUrl: "",
+  proxyUrl: "",
+  reportIntervalMs: 1,
+};
+OpticsAgent.configureAgent(configOptions)
+
+let expressServer: express.Express;
+expressServer.use(OpticsAgent.middleware());
+
+let hapiServer: hapi.Server;
+OpticsAgent.instrumentHapiServer(hapiServer);
+
+let req: express.Request;
+OpticsAgent.context(req);
+
+const agent1 = new Agent(configOptions);
+const agent2 = new OpticsAgent.Agent({ apiKey: '1234' });
+
+let schema: GraphQLSchema;
+agent1.instrumentSchema(schema);
