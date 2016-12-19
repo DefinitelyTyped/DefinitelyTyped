82c9f0e000c2676cfdd70204d96f71370c9f581d
Karol Janyst <lapkom@gmail.com>
2016-12-19 09:09:34 +0900

    Fix linter errors

--- redux-recycle/redux-recycle-tests.ts
+++ redux-recycle/redux-recycle-tests.ts
@@ -1,8 +1,8 @@
 import { Reducer } from "redux";
-import { default as recycleState } from "redux-recycle";
+import recycleState from "redux-recycle";
 
-const reducer: Reducer<any> = (state: any, action: any) => ({})
-const otherReducer: Reducer<any> = (state: any, action: any) => ({})
+const reducer: Reducer<any> = (state: any, action: any) => ({});
+const otherReducer: Reducer<any> = (state: any, action: any) => ({});
 
-const resetReducer: Reducer<any> = recycleState(reducer, ["reset"], {})
-const resetToOtherReducer: Reducer<any> = recycleState(reducer, ["reset"], otherReducer)
+const resetReducer: Reducer<any> = recycleState(reducer, ["reset"], {});
+const resetToOtherReducer: Reducer<any> = recycleState(reducer, ["reset"], otherReducer);
