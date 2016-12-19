1c0ab3da9b54a0468b338fbfafd9c860b2434d8d
Karol Janyst <lapkom@gmail.com>
2016-12-19 10:00:18 +0900

    Add definitions for browser-fingerprint

---
+++ browser-fingerprint/index.d.ts
@@ -0,0 +1,7 @@
+// Type definitions for browser-fingerprint 0.1
+// Project: https://www.npmjs.com/package/browser-fingerprint
+// Definitions by: Karol Janyst <https://github.com/LKay>
+// Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
+
+declare function browserFingerprint (): string;
+export = browserFingerprint;
