23778cc4740c14613b0747574b5bb74c3c3de2af
Evan Shortiss <evanshortiss@gmail.com>
2016-12-19 07:52:31 -0800

    add typings for table module

---
+++ table/index.d.ts
@@ -0,0 +1,6 @@
+// Type definitions for table 4.0
+// Project: https://github.com/gajus/table#readme
+// Definitions by: My Self <https://github.com/me>
+// Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
+
+Fill the header in!
---
+++ table/table-tests.ts
@@ -0,0 +1,15 @@
+import table from 'table';
+
+const border = table.getBorderCharacters('norc');
+
+const data = [
+  ['first name', 'last name'],
+  ['jane', 'doe'],
+  ['john', 'doe']
+];
+
+const config = {
+  border: border
+};
+
+console.log(table.table(data, config));
