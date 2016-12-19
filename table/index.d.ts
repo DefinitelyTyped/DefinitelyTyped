d2adf76d1e82fdf063d601dbcb9116b82b33b882
Evan Shortiss <evanshortiss@gmail.com>
2016-12-19 07:53:40 -0800

    fill in index.d.ts

--- table/index.d.ts
+++ table/index.d.ts
@@ -1,6 +1,56 @@
-// Type definitions for table 4.0
-// Project: https://github.com/gajus/table#readme
-// Definitions by: My Self <https://github.com/me>
+// Type definitions for Tabris.js 4.0
+// Project: https://github.com/gajus/table
+// Definitions by: Evan Shortiss <http://github.com/evanshortiss>
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
 
-Fill the header in!
+
+export type borderType = 'honeywell' | 'norc' | 'ramac' | 'void';
+
+export interface ColumnConfig {
+  alignment?: string;
+  width?: number;
+  truncate?: number;
+  paddingLeft?: string;
+  paddingRight?: string;
+}
+
+export interface JoinStruct {
+  topBody?: string;
+  topJoin?: string;
+  topLeft?: string;
+  topRight?: string;
+
+  bottomBody?: string;
+  bottomJoin?: string;
+  bottomLeft?: string;
+  bottomRight?: string;
+
+  bodyLeft?: string;
+  bodyRight?: string;
+  bodyJoin?: string;
+
+  joinBody?: string;
+  joinLeft?: string;
+  joinRight?: string;
+  joinJoin?: string;
+}
+
+export interface TableUserConfig {
+  columns?: {
+    [index: number]: ColumnConfig
+  };
+  drawJoin?: (index: number, size: number) => boolean;
+  border?: JoinStruct;
+  columnDefault?: ColumnConfig;
+}
+
+export interface Table {
+  table (data: any[], userConfig?: TableUserConfig): string;
+
+  createStream (userConfig: TableUserConfig): { write: (string[]) };
+
+  getBorderCharacters (templateName: borderType): JoinStruct;
+}
+
+export const table: Table;
+export default table;
