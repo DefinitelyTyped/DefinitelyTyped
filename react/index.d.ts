f6531f291ffd43a13d702898a80e1236bb40d3bd
Eric Anderson <e@ericlanderson.com>
2016-12-21 08:21:50 -0500

    Add typescript compiler header

--- react/index.d.ts
+++ react/index.d.ts
@@ -1,4 +1,5 @@
 // Type definitions for React v0.14
+// TypeScript Version: 2.1
 // Project: http://facebook.github.io/react/
 // Definitions by: Asana <https://asana.com>, AssureSign <http://www.assuresign.com>, Microsoft <https://microsoft.com>, John Reilly <https://github.com/johnnyreilly/>
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
