18654e7f4b783d7184b95ca726b16e853c1fa771
Ivan Matiishyn <matiyishyn.ivan@gmail.com>
2016-12-21 18:43:46 +0100

    added definitions

--- angular-ui-router/index.d.ts
+++ angular-ui-router/index.d.ts
@@ -1,6 +1,7 @@
 // Type definitions for Angular JS (ui.router module) 1.1.5
 // Project: https://github.com/angular-ui/ui-router
 // Definitions by: Michel Salib <https://github.com/michelsalib>
+// Definitions by: Ivan Matiishyn <https://github.com/matiishyn>
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
 
 import * as angular from 'angular';
