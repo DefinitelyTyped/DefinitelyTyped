5fc35db25b0a55beeda5195c42d77f1a3aa3ffd0
sdcb <sdflysha@qq.com>
2016-12-22 19:46:51 +0800

    fix the header name

--- box2d/index.d.ts
+++ box2d/index.d.ts
@@ -1,4 +1,4 @@
-// Type definitions for bootstrap.timepicker
+// Type definitions for box2d
 // Project: http://code.google.com/p/box2dweb/
 // Definitions by: jbaldwin <https://github.com/jbaldwin/>
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
