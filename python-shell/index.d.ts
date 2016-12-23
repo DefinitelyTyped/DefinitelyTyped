5b8190be499676218352d9b473d4e2ce6ff4d131
Dolan Miu <dolan_miu@hotmail.com>
2016-12-23 20:51:53 +0000

    added tslint fixes

--- python-shell/index.d.ts
+++ python-shell/index.d.ts
@@ -3,40 +3,37 @@
 // Definitions by: Dolan Miu <https://github.com/dolanmiu>
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
 
-declare module "python-shell" {
-    export class PythonShell {
-        on(message: string, callback: (message: string) => void): void;
-        end(callback: (message: string) => void): void;
-        send(message: string): void;
-        send(message: any): void;
+export class PythonShell {
+    on(message: string, callback: (message: string) => void): void;
+    end(callback: (message: string) => void): void;
+    send(message: any | string): void;
 
-        constructor(scriptName: string, options?: InstanceOptions);
-        defaultOptions: RunOptions;
-    }
-
-    export interface RunOptions {
-        mode?: string;
-        formatter?: string;
-        parser?: string;
-        encoding?: string;
-        pythonPath?: string;
-        pythonOptions?: Array<string>;
-        scriptPath?: string;
-        args?: Array<string>;
-    }
-
-    export interface InstanceOptions {
-        script?: string;
-        command?: string;
-        stdin?: any;
-        stdout?: any;
-        stderr?: any;
-        childProcess?: string;
-        terminated?: any;
-        exitCode?: any;
-        args?: Array<any>;
-    }
-
-    export function run(scriptName: string, RunOptions: RunOptions, callback: (err: Error, results?: any) => void): void;
-    export function run(scriptName: string, callback: (err: Error, results?: any) => void): void;
+    constructor(scriptName: string, options?: InstanceOptions);
+    defaultOptions: RunOptions;
 }
+
+export interface RunOptions {
+    mode?: string;
+    formatter?: string;
+    parser?: string;
+    encoding?: string;
+    pythonPath?: string;
+    pythonOptions?: string[];
+    scriptPath?: string;
+    args?: string[];
+}
+
+export interface InstanceOptions {
+    script?: string;
+    command?: string;
+    stdin?: any;
+    stdout?: any;
+    stderr?: any;
+    childProcess?: string;
+    terminated?: any;
+    exitCode?: any;
+    args?: any[];
+}
+
+export function run(scriptName: string, RunOptions: RunOptions, callback: (err: Error, results?: any) => void): void;
+export function run(scriptName: string, callback: (err: Error, results?: any) => void): void;
