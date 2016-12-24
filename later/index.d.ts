a7cf25d984d73521d5ed8239f1e42a15b6dee4e9
Andy Hanson <anhans@microsoft.com>
2016-12-24 16:05:02 -0500

    Use GitHub username for author

--- later/index.d.ts
+++ later/index.d.ts
@@ -1,6 +1,6 @@
 // Type definitions for LaterJS 1.2
 // Project: http://bunkat.github.io/later/
-// Definitions by: Jason D Dryhurst-Smith <http://jasonds.co.uk/>
+// Definitions by: Jason D Dryhurst-Smith <https://github.com/jasond-s>
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
 
 declare namespace later {
