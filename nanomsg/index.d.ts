51f11a1d6667b3b4908959ed39c4857e1b6b9c18
Titan <howay.tan@gmail.com>
2016-12-24 16:12:22 +0800

    Make tslint happy in nanomsg types (#13104)

--- nanomsg/index.d.ts
+++ nanomsg/index.d.ts
@@ -1,22 +1,20 @@
-// Type definitions for nanomsg 3.2.3
-// Project: https://github.com/nickdesaulniers/node-nanomsg
+// Type definitions for nanomsg 3.2
+// Project: https://github.com/nickdesaulniers/node-nanomsg#readme
 // Definitions by: Titan <https://github.com/titan>
-// Definitions: https://github.com/DefinitelyTyped/nanomsg
+// Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
 
 /// <reference types="node" />
 
-declare module "nanomsg" {
-  export interface EventCallback {
-    (data: Buffer): void;
-  }
-  export class Socket {
-    constructor(type: string, opts: Object);
-    shutdown(addr: string): void;
-    bind(addr: string): void;
-    connect(addr: string): void;
-    close(): void;
-    send(buf: Buffer | string): number;
-    on(event: string, cb: EventCallback): void;
-  }
-  export function socket(type: string, opts?: Object): Socket;
+export type EventCallback = (data: Buffer) => void;
+
+export class Socket {
+  constructor(type: string, opts: {});
+  shutdown(addr: string): void;
+  bind(addr: string): void;
+  connect(addr: string): void;
+  close(): void;
+  send(buf: Buffer | string): number;
+  on(event: string, cb: EventCallback): void;
 }
+
+export function socket(type: string, opts?: {}): Socket;
