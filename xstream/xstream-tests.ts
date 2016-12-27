be8dacfac27734a68d94e30d74593cb8ed73f64a
Mohamed Hegazy <mhegazy@microsoft.com>
2016-12-26 16:44:00 -0800

    Add xstream

---
+++ xstream/xstream-tests.ts
@@ -0,0 +1,19 @@
+import xs from 'xstream'
+
+// Tick every second incremental numbers,
+// only pass even numbers, then map them to their square,
+// and stop after 5 seconds has passed
+
+var stream = xs.periodic(1000)
+  .filter(i => i % 2 === 0)
+  .map(i => i * i)
+  .endWhen(xs.periodic(5000).take(1))
+
+// So far, the stream is idle.
+// As soon as it gets its first listener, it starts executing.
+
+stream.addListener({
+  next: i => console.log(i),
+  error: err => console.error(err),
+  complete: () => console.log('completed'),
+})
