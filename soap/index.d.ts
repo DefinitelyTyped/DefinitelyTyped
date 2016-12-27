82cf8e532decfe4e3bcb564d101bb2762b0b560a
Leo Liang <leoliang@gmail.com>
2016-12-28 06:52:11 +0800

    soap.d.ts: ClientSSLSecurty constructor parameters (#13216)

--- soap/index.d.ts
+++ soap/index.d.ts
@@ -1,6 +1,6 @@
 // Type definitions for soap
 // Project: https://www.npmjs.com/package/soap
-// Definitions by: Nicole Wang <https://github.com/nicoleWjie>, Cage Fox <https://github.com/cagefox>
+// Definitions by: Leo Liang <https://github.com/aleung>, Cage Fox <https://github.com/cagefox>
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
 
 /// <reference types="node" />
@@ -15,7 +15,8 @@ export class WSSecurity implements Security {
 }
 
 export class ClientSSLSecurity implements Security {
-    constructor(key: string, cert: string, ca: string, defaults: any);
+    constructor(key: Buffer | string, cert: Buffer | string, ca: Buffer | string, defaults?: any);
+    constructor(key: Buffer | string, cert: Buffer | string, defaults?: any);
 }
 
 interface Client extends events.EventEmitter {
