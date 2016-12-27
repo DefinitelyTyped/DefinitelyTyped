f06e049cd421c45bf92198e39659ff4763a22ec9
Mohamed Hegazy <mhegazy@microsoft.com>
2016-12-26 17:17:59 -0800

    Add user-home

---
+++ user-home/index.d.ts
@@ -0,0 +1,7 @@
+// Type definitions for user-home 2.0
+// Project: https://github.com/sindresorhus/user-home
+// Definitions by: My Self <https://github.com/me>
+// Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
+
+export = user_home;
+declare const user_home: string;
