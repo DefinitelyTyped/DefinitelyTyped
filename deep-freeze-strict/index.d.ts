5b916dcbe3b4a5e0f77f10647c925f9d022d77bf
Mohamed Hegazy <mhegazy@microsoft.com>
2016-12-27 14:40:23 -0800

    Add deep-freeze

---
+++ deep-freeze-strict/index.d.ts
@@ -0,0 +1,7 @@
+// Type definitions for deep-freeze-strict 1.1
+// Project: https://github.com/jsdf/deep-freeze
+// Definitions by: Mohamed Hegazy <https://github.com/mhegazy>
+// Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
+
+export = deep_freeze_strict;
+declare function deep_freeze_strict<T>(o: T): T;
