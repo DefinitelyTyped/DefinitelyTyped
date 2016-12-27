1d54553d45c9505a60c2dde50d9bf05a07f68745
Alex Alonso <alex4551@gmail.com>
2016-12-27 14:18:52 +0800

    add mongo-oplog typings

---
+++ mongo-oplog/mondo-oplog.d.ts
@@ -0,0 +1,18 @@
+// Type definitions for cayasso/mongo-oplog
+// Project: https://github.com/cayasso/mongo-oplog
+// Definitions by: FinalDes <https://github.com/FinalDes>
+// Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
+
+/// <reference types="eventemitter3" />
+
+declare function MongoOplog(url:string,filter:any):OplogTrigger;
+
+declare interface OplogTrigger {
+    tail(): any //return promise
+    on(type: String, callback: (doc: any) => void): any;//return eventemitter3
+    stop(callback: () => void): any //return promise
+}
+
+declare module "mongo-oplog" {
+    export = MongoOplog;
+}
