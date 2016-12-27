8a303d8e3d6922b711822a420c1e8a31aeca80b7
Mohamed Hegazy <mhegazy@microsoft.com>
2016-12-26 17:20:27 -0800

    Add strip-ansi

---
+++ strip-ansi/index.d.ts
@@ -0,0 +1,7 @@
+// Type definitions for strip-ansi 3.0
+// Project: https://github.com/chalk/strip-ansi
+// Definitions by: My Self <https://github.com/me>
+// Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
+
+export = strip_ansi;
+declare function strip_ansi(str: string): string;
