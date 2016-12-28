55557f0fde9324738d4d2934cd0d5911ccac502a
James Nail <james.nail@lanetix.com>
2016-12-28 13:35:20 -0500

    enhanced cors package, up to v2.8

--- cors/cors-tests.ts
+++ cors/cors-tests.ts
@@ -6,5 +6,22 @@ var app = express();
 app.use(cors());
 app.use(cors({
     maxAge: 100,
-    credentials: true
+    credentials: true,
+    optionsSuccessStatus: 200
+}));
+app.use(cors({
+    methods: 'GET,POST,PUT',
+    exposedHeaders: 'Content-Range,X-Content-Range',
+    allowedHeaders: 'Content-Type,Authorization'
+}));
+app.use(cors({
+    methods: ['GET', 'POST', 'PUT', 'DELETE'],
+    exposedHeaders: ['Content-Range', 'X-Content-Range'],
+    allowedHeaders: ['Content-Type', 'Authorization']
+}));
+app.use(cors({
+    origin: true
+}));
+app.use(cors({
+    origin: 'http://example.com'
 }));
