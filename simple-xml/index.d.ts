986f1e5ec080be3dfbe25fc2a8e24e5194460a8a
Vitaliy Isikov <visikov@gmail.com>
2016-12-28 10:46:20 -0500

    Add simple-xml types (#13069)
    * Add simple-xml types
    
    * Review changes

---
+++ simple-xml/index.d.ts
@@ -0,0 +1,12 @@
+// Type definitions for simple-xml 0.10
+// Project: https://github.com/AirAsiaExpedia/node-simple-xml
+// Definitions by: Vitaliy Isikov <https://github.com/notVitaliy>
+// Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
+
+
+declare namespace simple.xml {
+  export function stringify(obj: {}): string;
+  export function parse(data: string): {};
+}
+
+export = simple.xml;
