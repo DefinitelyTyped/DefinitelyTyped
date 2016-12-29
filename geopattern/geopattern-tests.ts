fd2784c20b386d3ce1bb7710b071d4b0d86c0843
Gaelan Steele <gbs@canishe.com>
2016-12-29 15:47:14 -0800

    [geopattern] Add jQuery plugin, make string optional.

--- geopattern/geopattern-tests.ts
+++ geopattern/geopattern-tests.ts
@@ -1,4 +1,7 @@
 /// <reference path="index.d.ts" />
+/// <reference path="../jquery/index.d.ts" />
 
 var pattern = GeoPattern.generate('GitHub');
 pattern.toDataUrl();
+
+$('#geopattern').geopattern('GitHub');
