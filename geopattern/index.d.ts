924a6598d782d5d95aca2b452751e53365602120
Gaelan Steele <gbs@canishe.com>
2016-12-29 02:28:30 -0700

    Fix version string.

--- geopattern/index.d.ts
+++ geopattern/index.d.ts
@@ -1,4 +1,4 @@
-// Type definitions for GeoPattern v1.2.3
+// Type definitions for GeoPattern 1.2
 // Project: https://github.com/btmills/geopattern
 // Definitions by: Gaelan <https://github.com/Gaelan>
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
