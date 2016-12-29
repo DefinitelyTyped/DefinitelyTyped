7b91babfe3787a6f926a6db6ebc273fbd553bf5d
Prashant Tiwari <prashaantt@gmail.com>
2016-12-30 00:29:10 +0530

    Fix typo

--- code/index.d.ts
+++ code/index.d.ts
@@ -9,7 +9,7 @@ export function expect<T>(value: T | T[], prefix?: string): AssertionChain<T>;
 export function fail(message: string): void;
 /** Returns the total number of assertions created using the expect() method. */
 export function count(): number;
-/** Returns an array of the locations where incomplete assertions were exportd or null if no incomplete assertions found. */
+/** Returns an array of the locations where incomplete assertions were declared or null if no incomplete assertions found. */
 export function incomplete(): string[] | null;
 /** Returns the filename, line number, and column number of where the error was created. */
 export function thrownAt(error?: Error): CodeError;
