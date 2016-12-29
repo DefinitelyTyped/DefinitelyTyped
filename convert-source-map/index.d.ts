7713460a7b4198d7881675240041f6a2f29991c5
TeamworkGuy2 <straitfrommars@rocketmail.com>
2016-12-29 21:44:30 +0000

    [convert-source-map] Update convert-source-map to latest v1.3.0 (TypeScript 2.1)

--- convert-source-map/index.d.ts
+++ convert-source-map/index.d.ts
@@ -1,27 +1,87 @@
 // Type definitions for convert-source-map v1.3.0
 // Project: https://github.com/thlorenz/convert-source-map
-// Definitions by: Andrew Gaspar <https://github.com/AndrewGaspar/>, Melvin Groenhoff <https://github.com/mgroenhoff>
+// Definitions by: Andrew Gaspar <https://github.com/AndrewGaspar/>, Melvin Groenhoff <https://github.com/mgroenhoff>, TeamworkGuy2 <https://github.com/TeamworkGuy2>
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
 
-export interface SourceMapConverter {
-    toObject(): any;
-    toJSON(space?: any): string;
-    toBase64(): string;
-    toComment(options?: { multiline?: boolean; }): string;
-    addProperty(key: string, value: any): this;
-    setProperty(key: string, value: any): this;
-    getProperty(key: string): any;
-}
+/** Converts a source-map from/to different formats and allows adding/changing properties.
+ * (documentation based on project's README file)
+ */
+declare module "convert-source-map" {
 
-export declare function fromObject(obj: any): SourceMapConverter;
-export declare function fromJSON(json: string): SourceMapConverter;
-export declare function fromBase64(base64: string): SourceMapConverter;
-export declare function fromComment(comment: string): SourceMapConverter;
-export declare function fromMapFileComment(comment: string, mapFileDir: string): SourceMapConverter;
-export declare function fromSource(source: string, largeSource?: boolean): SourceMapConverter | null;
-export declare function fromMapFileSource(source: string, mapFileDir: string): SourceMapConverter | null;
-export declare function removeComments(src: string): string;
-export declare function removeMapFileComments(src: string): string;
-export declare function generateMapFileComment(file: string, options?: { multiline?: boolean; }): string;
-export declare var commentRegex: RegExp;
-export declare var mapFileCommentRegex: RegExp;
+    export interface SourceMapConverter {
+        /** The parsed sourcemap object */
+        sourcemap: any;
+
+        /** Returns a copy of the underlying source map */
+        toObject(): any;
+
+        /** Converts source map to json string. If space is given (optional), this will be passed to JSON.stringify when the JSON string is generated */
+        toJSON(space?: string | number): string;
+
+        /** Converts source map to base64 encoded json string */
+        toBase64(): string;
+
+        /** Converts source map to an inline comment that can be appended to the source-file.
+         * By default, the comment is formatted like: //# sourceMappingURL=..., which you would normally see in a JS source file.
+         * When options.multiline == true, the comment is formatted like: /*# sourceMappingURL=... *\/, which you would find in a CSS source file
+         */
+        toComment(options?: { multiline?: boolean }): string;
+
+        /** Adds given property to the source map. Throws an error if property already exists */
+        addProperty(key: string, value: any): SourceMapConverter;
+
+        /** Sets given property to the source map. If property doesn't exist it is added, otherwise its value is updated */
+        setProperty(key: string, value: any): SourceMapConverter;
+
+        /** Gets given property of the source map */
+        getProperty(key: string): any;
+    }
+
+
+    /** Returns source map converter from given object */
+    export function fromObject(obj: any): SourceMapConverter;
+
+    /** Returns source map converter from given json string */
+    export function fromJSON(json: string): SourceMapConverter;
+
+    /** Returns source map converter from given base64 encoded json string */
+    export function fromBase64(base64: string): SourceMapConverter;
+
+    /** Returns source map converter from given base64 encoded json string prefixed with //# sourceMappingURL=... */
+    export function fromComment(comment: string): SourceMapConverter;
+
+    /**Returns source map converter from given filename by parsing //# sourceMappingURL=filename.
+     * filename must point to a file that is found inside the mapFileDir. Most tools store this file right next to the generated file, i.e. the one containing the source map.
+     */
+    export function fromMapFileComment(comment: string, commentFileDir: string): SourceMapConverter;
+
+    /** Finds last sourcemap comment in file and returns source map converter or returns null if no source map comment was found.
+     * If largeSource is set to true, an algorithm that does not use regex is applied to find the source map.
+     * This is faster and especially useful if you're running into "call stack size exceeded" errors with the default algorithm.
+     * However, it is less accurate and may match content that isn't a source map comment.
+     */
+    export function fromSource(content: string, largeSource?: boolean): SourceMapConverter | null;
+
+    /** Finds last sourcemap comment in file and returns source map converter or returns null if no source map comment was found.
+     * The sourcemap will be read from the map file found by parsing # sourceMappingURL=file comment. For more info see fromMapFileComment.
+     */
+    export function fromMapFileSource(content: string, commentFileDir: string): SourceMapConverter | null;
+
+    /** Returns src with all source map comments removed */
+    export function removeComments(src: string): string;
+
+    /** Returns src with all source map comments pointing to map files removed */
+    export function removeMapFileComments(src: string): string;
+
+    /** Returns the regex used to find source map comments */
+    export var commentRegex: RegExp;
+
+    /** Returns the regex used to find source map comments pointing to map files */
+    export var mapFileCommentRegex: RegExp;
+
+    /** Returns a comment that links to an external source map via file.
+     * By default, the comment is formatted like: //# sourceMappingURL=..., which you would normally see in a JS source file.
+     * When options.multiline == true, the comment is formatted like: /*# sourceMappingURL=... *\/, which you would find in a CSS source file.
+     */
+    export function generateMapFileComment(file: string, options?: { multiline?: boolean }): string;
+}
