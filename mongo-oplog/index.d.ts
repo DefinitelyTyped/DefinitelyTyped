4b8d4086a9b512555ebceb25742ee8953179eaf6
Alex Alonso <alex4551@gmail.com>
2016-12-30 22:30:25 +0800

    move export at the top

--- mongo-oplog/index.d.ts
+++ mongo-oplog/index.d.ts
@@ -1,8 +1,12 @@
-// TypeScript Version: 2.1
 // Type definitions for [cayasso/mongo-oplog]
 // Project: https://github.com/cayasso/mongo-oplog
 // Definitions by: FinalDes <https://github.com/FinalDes>
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
+// TypeScript Version: 2.1
+
+declare module "mongo-oplog" {
+    export = MongoOplog;
+}
 
 declare function MongoOplog(url:string,option:any): MongoOplog.OplogTrigger;
 
@@ -12,8 +16,4 @@ declare namespace MongoOplog{
         on(type: String, callback: (doc: any) => void): void;
         stop(callback: () => void): void
     }
-}
-
-declare module "mongo-oplog" {
-    export = MongoOplog;
 }
