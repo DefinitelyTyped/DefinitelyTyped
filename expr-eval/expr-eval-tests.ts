282fe578eb0594f07cb45004a0def39c7bac15fb
Connor Peet <connor@peet.io>
2016-12-30 09:57:56 -0800

    Use (slightly) stricter typings for expr-eval

--- expr-eval/expr-eval-tests.ts
+++ expr-eval/expr-eval-tests.ts
@@ -3,6 +3,7 @@ import { Parser, Expression } from 'expr-eval';
 const expr = Parser.parse('2^2');
 Parser.evaluate('2^2');
 Parser.evaluate('2^x', { x: 2 });
+Parser.evaluate('x.toUpperCase()', { x: 'hello world' });
 
 expr.evaluate({ x: 3 });
 expr.simplify();
--- expr-eval/index.d.ts
+++ expr-eval/index.d.ts
@@ -4,21 +4,30 @@
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
 
 declare namespace exprEval {
+    type Value = number
+        | string
+        | ((...args: Value[]) => Value)
+        | { [propertyName: string]: Value };
+
+    interface Values {
+        [propertyName: string]: Value;
+    }
+
     export class Parser {
         constructor(options?: { allowMemberAccess?: boolean });
         parse(expression: string): Expression;
-        evaluate(expression: string, values?: any): number;
+        evaluate(expression: string, values?: Value): number;
         static parse(expression: string): Expression;
-        static evaluate(expression: string, values?: any): number;
+        static evaluate(expression: string, values?: Value): number;
     }
 
     export interface Expression {
-        simplify(values?: any): Expression;
-        evaluate(values?: any): number;
-        substitute(values: any): Expression;
+        simplify(values?: Value): Expression;
+        evaluate(values?: Value): number;
+        substitute(values: Value): Expression;
         symbols(): string[];
         variables(): string[];
-        toJSFunction(params: string, values?: any): (...args: any[]) => number;
+        toJSFunction(params: string, values?: Value): (...args: any[]) => number;
     }
 }
 
