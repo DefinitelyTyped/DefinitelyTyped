ba9af337dd50cef7cad749ae8d6b3a54b6af086c
Alex Alonso <alex4551@gmail.com>
2016-12-30 13:22:35 +0800

    code refactor base on offical guidelines

--- mongo-oplog/index.d.ts
+++ mongo-oplog/index.d.ts
@@ -1,17 +1,16 @@
-// TypeScript Version: 2.1
-// Type definitions for mongo-oplog v2.0.2
-// Project: https://github.com/cayasso/mongo-oplog
-// Definitions by: FinalDes <https://github.com/FinalDes>
+// Type definitions for [cayasso/mongo-oplog]
+// Project: [https://github.com/cayasso/mongo-oplog]
+// Definitions by: [FinalDes] <[https://github.com/FinalDes]>
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
 
-/// <reference types="eventemitter3" />
+declare function MongoOplog(url:string,option:any): MongoOplog.OplogTrigger;
 
-export function MongoOplog(url: string, filter: any): OplogTrigger;
-
-export interface OplogTrigger {
-    tail(): any; // return promise
-    on(type: string, callback: (doc: any) => void): any; // return eventemitter3
-    stop(callback: () => void): any; // return promise
+declare namespace MongoOplog{
+    interface OplogTrigger {
+        tail(): any
+        on(type: String, callback: (doc: any) => void): void;
+        stop(callback: () => void): void
+    }
 }
 
 declare module "mongo-oplog" {
