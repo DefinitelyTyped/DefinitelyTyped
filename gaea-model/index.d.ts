876058e50e1ad28d3544bf3e7bd1805136ca6c1b
576625322@qq.com <hzyandkyn1314>
2017-01-02 12:56:17 +0800

    fix bug

--- gaea-model/index.d.ts
+++ gaea-model/index.d.ts
@@ -15,54 +15,54 @@ export interface ComponentProps extends React.HTMLProps<any> {
     /**
      * 唯一的 key,用来唯一标识这个组件,所有盖亚内部组件都以 gaea-old- 为前缀
      */
-    gaeaUniqueKey: string
+    gaeaUniqueKey: string;
     /**
      * 组件的中文名
      */
-    gaeaName: string
+    gaeaName: string;
     /**
      * 组件图标,为 fontAwesome
      */
-    gaeaIcon?: string
+    gaeaIcon?: string;
     /**
      * 编辑信息
      */
-    gaeaEdit?: Array<ComponentPropsGaeaEdit>
+    gaeaEdit?: ComponentPropsGaeaEdit[];
     /**
      * 事件定义
      */
-    gaeaEvent?: GaeaEvent
+    gaeaEvent?: GaeaEvent;
     /**
      * 是否在预览模式，preivew 会传入 true
      */
-    gaeaPreview?: boolean
+    gaeaPreview?: boolean;
     /**
      * 是否可以拖入子元素
      */
-    canDragIn?: boolean
+    canDragIn?: boolean;
     /**
      * 存储事件设置
      */
-    gaeaEventData?: Array<EventData>
+    gaeaEventData?: EventData[];
     /**
      * 存储native事件设置
      */
-    gaeaNativeEventData?: Array<EventData>
+    gaeaNativeEventData?: EventData[];
     /**
      * 存储变量信息
      */
     gaeaVariables?: {
-        [field: string]: VariableData
-    }
+        [field: string]: VariableData;
+    };
 
-    [x: string]: any
+    [x: string]: any;
 }
 
 /**
  * 组件配置中数组配置
  */
 export interface ComponentPropsOptionsArray extends ComponentPropsGaeaEdit {
-    key: string
+    key: string;
 }
 
 /**
@@ -72,19 +72,19 @@ export interface ComponentPropsOptionsArrayValue {
     /**
      * ComponentPropsOptionsArray 设置的 key
      */
-    key: string
+    key: string;
     /**
      * 用户填入的值
      */
-    value: number | string
+    value: number | string;
 }
 
 /**
  * 组件配置 选择器配置
  */
 export interface ComponentPropsOptionsSelector {
-    key: string
-    value: string
+    key: string;
+    value: string;
 }
 
 /**
@@ -92,14 +92,14 @@ export interface ComponentPropsOptionsSelector {
  */
 export interface Number {
     // 为空时，是数字，否则会以字符串加后缀形式赋值
-    key: string
-    value: string
+    key: string;
+    value: string;
 }
 
 /**
  * 组件的值
  */
-export type ComponentPropsOptionValue = number | string | boolean | Array<ComponentPropsOptionsArrayValue> | any
+export type ComponentPropsOptionValue = number | string | boolean | ComponentPropsOptionsArrayValue[] | any;
 
 /**
  * 组件配置
@@ -108,60 +108,60 @@ export interface ComponentPropsGaeaEdit {
     /**
      * 对应字段名
      */
-    field: string
+    field: string;
     /**
      * 选项名称
      */
-    label: string
+    label: string;
     /**
      * 编辑器类型
      */
-    editor: string
+    editor: string;
     /**
      * 是否可被编辑
      */
-    editable: boolean
+    editable: boolean;
     /**
      * 是否隐藏编辑工具
      */
-    hideTool: boolean
+    hideTool: boolean;
     /**
      * 当 editor 为 array 时的数组配置
      * 数组中的 key,和填入的值,会作为 Array<key:value> 填入到 value 中
      */
-    array?: Array<ComponentPropsOptionsArrayValue>
+    array?: ComponentPropsOptionsArrayValue[];
     /**
      * 当 editor 为 selector 时的数组配置
      */
-    selector?: Array<ComponentPropsOptionsSelector>
+    selector?: ComponentPropsOptionsSelector[];
     /**
      * 值类型, 默认为字符串
      */
-        type?: string
+        type?: string;
     /**
      * 为空时的值，默认为 null
      */
-    emptyValue?: any
+    emptyValue?: any;
     /**
      * 控制 number 单位
      */
         number?: {
-        units: Array<Number>
-        currentUnit: string
-        max?: number
-        min?: number
-        step?: number
-        speed?: number
-        inputRange?: Array<number>
-        outputRange?: Array<number>
-        slider?: boolean
-    }
+        units: number[];
+        currentUnit: string;
+        max?: number;
+        min?: number;
+        step?: number;
+        speed?: number;
+        inputRange?: number[];
+        outputRange?: number[];
+        slider?: boolean;
+    };
     /**
      * 枚举实例
      */
     instance?: Array<{
-        [key: string]: ComponentPropsOptionValue
-    }>
+        [key: string]: ComponentPropsOptionValue;
+    }>;
 }
 
 /**
@@ -169,7 +169,7 @@ export interface ComponentPropsGaeaEdit {
  */
 export interface ComboComponentInfo extends ViewportComponentFullInfo {
     // 组合名
-    name: string
+    name: string;
 }
 
 /**
@@ -177,53 +177,53 @@ export interface ComboComponentInfo extends ViewportComponentFullInfo {
  */
 export interface GetPublishListResult {
     // 版本
-    version: string
+    version: string;
     // 描述
-    description: string
+    description: string;
 }
 
 /**
  * 版本信息
  */
 export interface VersionInfo {
-    content: string
+    content: string;
 }
 
 /**
  * 事件设置
  */
 export interface GaeaEvent {
-    triggers: Array<EventTriggerCondition>
-    effects: Array<EventAction>
+    triggers: EventTriggerCondition[];
+    effects: EventAction[];
 }
 
 export interface EventData {
-    type: string
-    event: string
+    type: string;
+    event: string;
     /**
      * 因为事件可能 type 相同，因此记录是第几个
      */
-    typeIndex: number
-    eventIndex: number
-    typeData?: EventTriggerEvent
-    eventData?: EventActionJumpUrl | EventActionCall | EventActionEvent
+    typeIndex: number;
+    eventIndex: number;
+    typeData?: EventTriggerEvent;
+    eventData?: EventActionJumpUrl | EventActionCall | EventActionEvent;
 }
 
 export interface EventTriggerCondition {
-    name: string
+    name: string;
     // 触发类型
-    type: string
+    type: string;
     // 是否由自己的生命周期触发
-    selfCallback?: boolean
+    selfCallback?: boolean;
 }
 
 export interface EventAction {
-    name: string
+    name: string;
     // 动作类型
     // call: 调用传进来的方法
     // jumpUrl: 跳转一个网址
-    type: string
-    call?: EventCallType
+    type: string;
+    call?: EventCallType;
 }
 
 /**
@@ -231,57 +231,57 @@ export interface EventAction {
  */
 export interface EventCallType {
     // 调用函数名称
-    functionName: string
+    functionName: string;
     // 调用参数
-    param?: Array<EventCallTypeParam>
+    param?: EventCallTypeParam[];
 }
 
 export interface EventCallTypeParam {
-    label: string
-    field: string
-    editor: string // text
+    label: string;
+    field: string;
+    editor: string; // text
 }
 
 /**
  * 事件触发数据类型
  */
 export interface EventTriggerEvent {
-    listen?: string
+    listen?: string;
 }
 
 export interface EventUpdatePropsEvent {
     /**
      * 修改后的 props
      */
-    props: ComponentProps
+    props: ComponentProps;
 }
 
 /**
  * 事件动作数据类型
  */
 export interface EventActionJumpUrl {
-    url?: string
+    url?: string;
 }
 
 export interface EventActionCall {
     // 函数在 map 里的 key
-    key?: string
+    key?: string;
 }
 
 export interface EventActionEvent {
-    emit?: string
+    emit?: string;
 }
 
 export interface ExternalParameter {
     /**
      * 名称
      */
-    name: string
+    name: string;
     /**
      * 类型
      * number string boolean
      */
-        type: string
+        type: string;
 }
 
 /**
@@ -290,11 +290,11 @@ export interface ExternalParameter {
 export interface VariableData {
     // 变量类型
     // 比如是外部传参，还是全局变量
-    variableType: string
+    variableType: string;
     // 变量的哪个字段
-    variableField: string
+    variableField: string;
     // 值类型 string number boolean
-    valueType: string
+    valueType: string;
 }
 
 /**
@@ -302,9 +302,9 @@ export interface VariableData {
  */
 export interface Plugin extends React.ComponentClass<any> {
     // 插件安放位置
-    position: string
-    Action?: any
-    Store?: any
+    position: string;
+    Action?: any;
+    Store?: any;
 }
 
 /********************************************************************************************************
@@ -312,74 +312,74 @@ export interface Plugin extends React.ComponentClass<any> {
  ********************************************************************************************************/
 export interface Diff {
     // 操作类型
-    type: 'add' | 'move' | 'remove' | 'exchange' | 'update' | 'paste' | 'reset' | 'addCombo' | 'addSource'
+    type: 'add' | 'move' | 'remove' | 'exchange' | 'update' | 'paste' | 'reset' | 'addCombo' | 'addSource';
     // 操作组件的 mapUniqueKey
-    mapUniqueKey: string
+    mapUniqueKey: string;
     // 新增操作
     add?: {
         // 新增组件的唯一标识 id
-        uniqueId: string
+        uniqueId: string;
         // 父级 mapKey
-        parentMapUniqueKey: string
+        parentMapUniqueKey: string;
         // 插入的位置
-        index: number
-    }
+        index: number;
+    };
     // 移动到另一个父元素
     move?: {
         // 移动到的父级 mapKey
-        targetParentMapUniqueKey: string
+        targetParentMapUniqueKey: string;
         // 移动前父级 mapKey
-        sourceParentMapUniqueKey: string
+        sourceParentMapUniqueKey: string;
         // 插入的位置
-        targetIndex: number
+        targetIndex: number;
         // 移除的位置
-        sourceIndex: number
-    }
+        sourceIndex: number;
+    };
     // 删除组件
-    remove?: DiffRemove
+    remove?: DiffRemove;
     // 内部交换顺序
     exchange?: {
-        oldIndex: number
-        newIndex: number
-    }
+        oldIndex: number;
+        newIndex: number;
+    };
     // 更新操作
     update?: {
-        oldValue: ComponentProps
-        newValue: ComponentProps
-    }
+        oldValue: ComponentProps;
+        newValue: ComponentProps;
+    };
     // 粘贴操作
-    paste?: DiffRemove
+    paste?: DiffRemove;
     // 重置组件
     reset?: {
         // 重置前的信息
-        beforeProps: ComponentProps
-        beforeName: string
-    }
+        beforeProps: ComponentProps;
+        beforeName: string;
+    };
     // 新增组合
     addCombo?: {
         // 父级 mapKey
-        parentMapUniqueKey: string
+        parentMapUniqueKey: string;
         // 父级的 index
-        index: number
+        index: number;
         // 组合的完整信息（不是 copy 的, 是真正对应的 mapUniqueKey）
-        componentInfo: ViewportComponentFullInfo
-    }
+        componentInfo: ViewportComponentFullInfo;
+    };
     // 新增模板
     addSource?: {
         // 父级 mapKey
-        parentMapUniqueKey: string
+        parentMapUniqueKey: string;
         // 父级的 index
-        index: number
+        index: number;
         // 组合的完整信息（不是 copy 的, 是真正对应的 mapUniqueKey）
-        componentInfo: ViewportComponentFullInfo
-    }
+        componentInfo: ViewportComponentFullInfo;
+    };
 }
 
 export interface DiffRemove extends ViewportComponentFullInfo {
     // 父级元素 mapKey
-    parentMapUniqueKey: string
+    parentMapUniqueKey: string;
     // 删除的位置
-    index: number
+    index: number;
 }
 
 /********************************************************************************************************
@@ -392,16 +392,16 @@ export interface ViewportComponentInfo {
     /**
      * props
      */
-    props: ComponentProps
+    props: ComponentProps;
     /**
      * 子元素（canDragIn才有）
      * 只存子元素在 map 中的 key
      */
-    layoutChilds?: Array<string>
+    layoutChilds?: string[];
     /**
      * 父级 mapUniqueKey, 这样不但方便寻找父级,还能根据是否为 null 判断是否为根元素
      */
-    parentMapUniqueKey: string
+    parentMapUniqueKey: string;
 }
 
 /**
@@ -409,24 +409,24 @@ export interface ViewportComponentInfo {
  */
 export interface MovingComponent {
     // 在编辑区域的组件信息
-    mapUniqueKey: string
+    mapUniqueKey: string;
     // 是否是新拖拽的
-    isNew: boolean
+    isNew: boolean;
     // 组件的唯一标识, 新建的时候采用
-    uniqueKey: string
+    uniqueKey: string;
     // 直接给 source 就直接用
-    source: string
+    source: string;
 }
 
 /**
  * hover 元素的信息
  */
 export interface HoverComponentSpec {
-    left: number
-    top: number
-    width: number
-    height: number
-    hovering: boolean
+    left: number;
+    top: number;
+    width: number;
+    height: number;
+    hovering: boolean;
 }
 
 /**
@@ -434,13 +434,13 @@ export interface HoverComponentSpec {
  */
 export interface ViewportComponentFullInfo {
     // 组件的 mapUniqueKey
-    mapUniqueKey: string
+    mapUniqueKey: string;
     // 组件信息
-    componentInfo: ViewportComponentInfo
+    componentInfo: ViewportComponentInfo;
     // 子元素列表 （包括非直接子集）
     childs: {
-        [mapUniqueKey: string]: ViewportComponentInfo
-    }
+        [mapUniqueKey: string]: ViewportComponentInfo;
+    };
 }
 
 /**
@@ -448,27 +448,27 @@ export interface ViewportComponentFullInfo {
  */
 export interface CurrentDragComponentInfo {
     // 类型
-    type: 'new' | 'combo' | 'viewport'
+    type: 'new' | 'combo' | 'viewport';
     // 开始拖拽父级的 dom
-    dragStartParentElement: HTMLElement
+    dragStartParentElement: HTMLElement;
     // 开始拖拽在父级中的位置
-    dragStartIndex: number
+    dragStartIndex: number;
     newInfo?: {
         // 唯一 key，用来实例化组件
-        uniqueKey: string
-    }
+        uniqueKey: string;
+    };
     comboInfo?: {
         // combo 信息
-        source: string
-    }
+        source: string;
+    };
     viewportInfo?: {
         // 当前拖拽组件的 mapUniqueKey
-        mapUniqueKey: string
+        mapUniqueKey: string;
         // 拖拽目标的 mapUniqueKey
-        targetMapUniqueKey?: string
+        targetMapUniqueKey?: string;
         // 拖拽目标的 index
-        targetIndex?: number
-    }
+        targetIndex?: number;
+    };
 }
 
 /********************************************************************************************************
@@ -478,14 +478,14 @@ export interface CurrentDragComponentInfo {
  * 鼠标移动到组件上/树对应的组件上
  */
 export interface MouseHoverComponentEvent {
-    mapUniqueKey: string
-    type: string
+    mapUniqueKey: string;
+    type: string;
 }
 
 /**
  * 修改某个组件选中状态
  */
 export interface ComponentSelectStatusEvent {
-    mapUniqueKey: string
-    selected: boolean
+    mapUniqueKey: string;
+    selected: boolean;
 }
