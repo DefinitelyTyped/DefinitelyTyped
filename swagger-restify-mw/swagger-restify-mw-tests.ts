6fe9a90232d9b986d055335da998ff8818e23f82
Michael Mrowetz <michael.mrowetz@cdnetworks.co.kr>
2017-01-02 15:23:51 +0900

    remove commented out code

--- swagger-restify-mw/swagger-restify-mw-tests.ts
+++ swagger-restify-mw/swagger-restify-mw-tests.ts
@@ -14,10 +14,6 @@ SwaggerRestify.create(config, function(err, swaggerRestify) {
 
   let port = process.env.PORT || 10010;
   app.listen(port);
-
-  // if (swaggerRestify.runner.swagger.paths['/hello']) {
-  //   console.log('try this:\ncurl http://127.0.0.1:' + port + '/hello?name=Scott');
-  // }
 });
 
 
