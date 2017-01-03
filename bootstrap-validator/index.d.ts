1136ac212958def0a3120a2c0730c23479bde70f
Brady Liles <bradyliles@outlook.com>
2017-01-03 07:48:47 -0700

    Removed Patch Version

--- bootstrap-validator/index.d.ts
+++ bootstrap-validator/index.d.ts
@@ -1,4 +1,4 @@
-// Type definitions for bootstrap-validator 0.11.8
+// Type definitions for bootstrap-validator 0.11
 // Project: https://github.com/1000hz/bootstrap-validator
 // Definitions by: Brady Liles <https://github.com/BradyLiles/>
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
