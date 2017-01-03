59eb1e9028baf0e564023c7af00d88d9ee940d3f
Eric Anderson <eanderson@palantir.com>
2017-01-03 17:00:56 -0500

    Update to properly order headers

--- react/index.d.ts
+++ react/index.d.ts
@@ -1,8 +1,8 @@
 // Type definitions for React v0.14
-// TypeScript Version: 2.1
 // Project: http://facebook.github.io/react/
 // Definitions by: Asana <https://asana.com>, AssureSign <http://www.assuresign.com>, Microsoft <https://microsoft.com>, John Reilly <https://github.com/johnnyreilly/>
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
+// TypeScript Version: 2.1
 
 export = React;
 export as namespace React;
