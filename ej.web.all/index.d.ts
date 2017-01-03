86afdd87a5d198cc207953084d27393451e40c86
Syncfusion-JavaScript <packages@syncfusion.com>
2017-01-03 12:54:15 +0530

    Version updated

--- ej.web.all/index.d.ts
+++ ej.web.all/index.d.ts
@@ -1,4 +1,4 @@
-// Type definitions for ej.web.all 14.4
+// Type definitions for ej.web.all 14.4.20
 // Project: http://help.syncfusion.com/js/typescript
 // Definitions by: Syncfusion <https://github.com/syncfusion/>
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
