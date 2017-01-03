296af89a667ffc9559b3af67e72d539cc10658b6
Paul Lockwood <paul.lockwood@pinnacol.com>
2017-01-03 16:04:41 -0700

    First attempt adding browser-bunyan types

---
+++ browser-bunyan/browser-bunyan-tests.ts
@@ -0,0 +1,7 @@
+var bunyan = require('browser-bunyan');
+var log = bunyan.createLogger({
+        name: 'play',
+        serializers: bunyan.stdSerializers
+});
+log.debug({foo: 'bar'}, 'hi at debug')
+log.trace('hi at trace')
