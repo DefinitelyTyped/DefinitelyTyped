8f5fb39a947c295eab68dc6bb70747ecf93a98fb
Matthew Clive <mattrulz127@gmail.com>
2017-01-03 14:56:54 -0500

    Update index.d.ts

--- configstore/index.d.ts
+++ configstore/index.d.ts
@@ -1,9 +1,9 @@
-// Type definitions for configstore 2.1.0
+// Type definitions for configstore 2.1
 // Project: https://github.com/yeoman/configstore
 // Definitions by: ArcticLight <https://github.com/ArcticLight>
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
 
-export = Configstore
+export = Configstore;
 
 declare class Configstore {
     constructor(packageName: string, defaults?: any, options?: Configstore.ConfigstoreOptions);
