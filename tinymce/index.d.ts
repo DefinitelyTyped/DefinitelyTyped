05310cb6246dad7bfdc03e368418308d44b0d4d0
Martin D <martinduparc@users.noreply.github.com>
2017-01-04 14:01:38 -0500

    Update index.d.ts
    Added namespace

--- tinymce/index.d.ts
+++ tinymce/index.d.ts
@@ -1,44 +1,44 @@
-// Type definitions for TinyMCE (WIP)
+// Type definitions for TinyMCE 4.5
 // Project: https://github.com/tinymce/tinymce
 // Definitions by: Martin Duparc <https://github.com/martinduparc/>
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
 
-interface TinyMceObservable {
-  off: (name?: string, callback?: Function) => Object;
-  on: (name: string, callback: Function) => Object;
-  fire: (name: string, args?: Object, bubble?: Boolean) => Event;
+// Work In Progress
+
+declare namespace TinyMCE {
+  export interface Observable {
+    off: (name?: string, callback?: Function) => Object;
+    on: (name: string, callback: Function) => Object;
+    fire: (name: string, args?: Object, bubble?: Boolean) => Event;
+  }
+
+  export interface Editor extends Observable {
+    destroy: (automatic: boolean) => void;
+    remove: () => void;
+    hide: () => void;
+    show: () => void;
+    getContent: (args?: Object) => string;
+    setContent: (content: string, args?: Object) => string;
+    focus: (skip_focus?: Boolean) => void;
+    undoManager: UndoManager;
+    settings: Object;
+    getDoc: () => Document;
+    editorUpload: any;
+  }
+
+  export interface UndoManager {
+    undo: () => Object;
+    clear: () => void;
+    hasUndo: () => Boolean;
+  }
+
+  export interface Static extends Observable {
+    init: (settings: Object) => void;
+    execCommand: (c: string, u: Boolean, v: string) => Boolean;
+    activeEditor: Editor;
+    get: (id: String) => Editor;
+    triggerSave: () => void;
+  }
 }
 
-interface TinyMceEditor extends TinyMceObservable {
-  destroy: (automatic: boolean) => void;
-  remove: () => void;
-  hide: () => void;
-  show: () => void;
-  getContent: (args?: Object) => string;
-  setContent: (content: string, args?: Object) => string;
-  focus: (skip_focus?: Boolean) => void;
-  undoManager: TinyMceUndoManager;
-  settings: Object;
-  getDoc: () => Document;
-  editorUpload: any;
-}
-
-interface TinyMceUndoManager {
-  undo: () => Object;
-  clear: () => void;
-  hasUndo: () => Boolean;
-}
-
-interface TinyMceEvent {
-
-}
-
-interface TinyMceStatic extends TinyMceObservable {
-  init: (settings: Object) => void;
-  execCommand: (c: string, u: Boolean, v: string) => Boolean;
-  activeEditor: TinyMceEditor;
-  get: (id: String) => TinyMceEditor;
-  triggerSave: () => void;
-}
-
-declare var tinymce: TinyMceStatic;
+declare var tinymce: TinyMCE.Static;
