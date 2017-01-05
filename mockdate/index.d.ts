76467deaf3313cc57dff8b2653b8cd8c5c88fdf3
Bruno Michels <bruno.michels@bravi.com.br>
2017-01-05 14:23:31 -0200

    Fix mockdate to export as namespace

--- mockdate/index.d.ts
+++ mockdate/index.d.ts
@@ -3,16 +3,16 @@
 // Definitions by: Bruno Leonardo Michels <https://github.com/brunolm>
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
 
-declare module "mockdate" {
-    /**
-     * Change the Date implementation to mock a specific date.
-     * @param Date to be set as current
-     * @param timezoneOffset? The value that should be returned by new Date().getTimezoneOffset()
-     */
-    export function set(date: { valueOf(): number; } | number | string, timezoneOffset?: number): void;
+export as namespace MockDate;
 
-    /**
-     * Restore the original Date object back to the native implementation.
-     */
-    export function reset(): void;
-}
+/**
+ * Change the Date implementation to mock a specific date.
+ * @param Date to be set as current
+ * @param timezoneOffset? The value that should be returned by new Date().getTimezoneOffset()
+ */
+export function set(date: { valueOf(): number; } | number | string, timezoneOffset?: number): void;
+
+/**
+ * Restore the original Date object back to the native implementation.
+ */
+export function reset(): void;
