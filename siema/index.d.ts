75901dfd6fea97d2d1317bd416c67fc8e6a393a0
Irmantas Zenkus <irmantas.zenkus@hometogo.com>
2017-01-06 08:41:25 +0200

    tslint added

--- siema/index.d.ts
+++ siema/index.d.ts
@@ -1,28 +1,26 @@
-// Type definitions for siema
+// Type definitions for siema 1.0
 // Project: https://github.com/pawelgrzybek/siema
 // Definitions by: Irmantas Zenkus <https://github.com/Irmiz>
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
-declare module 'siema' {
-    interface SiemaOptions {
-        selector?: string;
-        duration?: number;
-        easing?: string;
-        perPage?: number;
-        startIndex?: number;
-        draggable?: boolean;
-        threshold?: number;
-        loop?: boolean;
-    }
+declare class Siema {
+    currentSlide: number;
 
-    class Siema {
-        public currentSlide: number;
+    constructor(options?: SiemaOptions);
 
-        constructor(options?: SiemaOptions);
-
-        public next(): void;
-        public prev(): void;
-        public goTo(index: number): void;
-    }
-
-    export = Siema;
+    next(): void;
+    prev(): void;
+    goTo(index: number): void;
 }
+
+interface SiemaOptions {
+    selector?: string;
+    duration?: number;
+    easing?: string;
+    perPage?: number;
+    startIndex?: number;
+    draggable?: boolean;
+    threshold?: number;
+    loop?: boolean;
+}
+
+export = Siema;
