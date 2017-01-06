c1bd558e85c6b3237f6624aecb30fe71bf266df0
Andy <anhans@microsoft.com>
2017-01-06 06:34:42 -0800

    cldrjs: Convert cldr.js-supplemental.d.ts to module augmentation, and include in tests (#13777)

--- cldrjs/cldr.js-event-tests.ts
+++ cldrjs/cldr.js-event-tests.ts
@@ -15,19 +15,21 @@ Cldr.off("get", (path, value) => {
     console.log(value);
 });
 
-const cldr = new Cldr("en");
+{
+    const cldr = new Cldr("en");
 
-cldr.on("get", (path, value) => {
-    console.log(path);
-    console.log(value);
-});
+    cldr.on("get", (path, value) => {
+        console.log(path);
+        console.log(value);
+    });
 
-cldr.once("get", (path, value) => {
-    console.log(path);
-    console.log(value);
-});
+    cldr.once("get", (path, value) => {
+        console.log(path);
+        console.log(value);
+    });
 
-cldr.off("get", (path, value) => {
-    console.log(path);
-    console.log(value);
-});
+    cldr.off("get", (path, value) => {
+        console.log(path);
+        console.log(value);
+    });
+}
