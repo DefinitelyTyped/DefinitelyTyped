ae682399f3ba6b76857d8a5df908bafb244c009f
Andres Jimenez <andresdavid@outlook.com>
2017-01-07 15:47:18 -0400

    re-testing

--- loopback-boot/loopback-boot-test.ts
+++ loopback-boot/loopback-boot-test.ts
@@ -1,31 +1,21 @@
+/// <reference path="index.d.ts" />
 import * as loopback from 'loopback';
-import * as express from 'express';
 import * as boot from 'loopback-boot';
+import * as cookieParser from 'cookie-parser';
 
-export class Server {
-  app: loopback.LoopBackApplication;
+class Server {
+    app: loopback.LoopBackApplication;
 
-  static boostrap(): Server {
-    return new Server();
-  }
-  constructor() {
-    this.app = loopback();
+    static boostrap(): Server {
+      return new Server();
+    }
+    constructor() {
+        this.app = loopback();
 
-    this.app.start = function() {
-      // start the web server
-      let server = this.app.listen(function() {
-        this.app.emit('started', server);
-        const baseUrl = this.app.get('url').replace(/\/$/, '');
-        console.log('Web server listening at: %s', baseUrl);
-        if (this.app.get('loopback-component-explorer')) {
-          let explorerPath = this.app.get('loopback-component-explorer').mountPath;
-          console.log('Browse your REST API at %s%s', baseUrl, explorerPath);
-        }
-      });
-      return server;
-    };
-    this.config();
-  }
+        this.app.use(cookieParser());
+
+        this.config();
+    }
 
   private config(): void {
     boot(this.app, __dirname, (err: Error) => {
