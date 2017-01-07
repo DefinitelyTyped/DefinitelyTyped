e1645a7cbb093113c75074ced6d0c4969ddb5032
Andres Jimenez <andresdavid@outlook.com>
2017-01-06 20:12:40 -0400

    loopback typings

--- loopback/loopback-test.ts
+++ loopback/loopback-test.ts
@@ -1,6 +1,20 @@
 /// <reference path="index.d.ts" />
 import * as loopback from 'loopback';
+import * as cookieParser from 'cookie-parser';
 
-var app: LoopBackApplication = loopback();
+class Server {
+    app: loopback.LoopBackApplication;
 
-app.listen(3000);
+    static boostrap(): Server {
+    return new Server();
+    }
+    constructor() {
+        this.app = loopback();
+
+        this.app.use(cookieParser());
+
+        this.app.start = function() {
+        // start the web server
+        };
+    }
+}
