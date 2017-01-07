af81382c50141d88e5e2ab901e7afb6304255348
Andres Jimenez <andresdavid@outlook.com>
2017-01-07 15:34:47 -0400

    prepare proyect link

--- loopback-boot/index.d.ts
+++ loopback-boot/index.d.ts
@@ -1,5 +1,5 @@
 // Type definitions for Loopback-boot 2.23
-// Project: https://github.com/strongloop/loopback
+// Project: https://github.com/strongloop/loopback-boot
 // Definitions by: Andres D Jimenez <https://github.com/kattsushi/>
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
 // TypeScript Version: 2.1
