b37f7dda0b483bac33d825f64c6f5b6c782059b3
0x6368656174 <0x6368656174@gmail.com>
2017-01-07 12:25:18 +1000

    Change Definitions by

--- moviedb/index.d.ts
+++ moviedb/index.d.ts
@@ -1,7 +1,6 @@
 // Type definitions for MovieDB
 // Project: https://github.com/danzajdband/moviedb
-// Definitions by: Basarat Ali Syed <https://github.com/basarat>
-// Definitions by: Pavel Puchkov <https://github.com/0x6368656174>
+// Definitions by: Basarat Ali Syed <https://github.com/basarat>, Pavel Puchkov <https://github.com/0x6368656174>
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
 
 // Ghost module for Types
