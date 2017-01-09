5e054896cd59a926b83108585a84e89e78991511
JD-Robbs <JD-Robbs@users.noreply.github.com>
2017-01-09 18:10:44 +0000

    Add additional tests

--- sha1/sha1-tests.ts
+++ sha1/sha1-tests.ts
@@ -4,7 +4,7 @@ import fs = require("fs");
 import sha1 = require("sha1");
 /**
  * API
- * md5(message)
+ * sha1(message)
  * message -- String or Buffer
  * returns String
  * 
@@ -27,6 +27,9 @@ import sha1 = require("sha1");
  * **************************************************
  */
 console.log(sha1('message')); // should print 6f9b9af3cd6e8b8a73c2cdced37fe9f59226e27d
+
+const testTwo = sha1('message', {asString: true});
+const testThree = sha1('message', {asBytes: true});
 
 fs.readFile('sha1.d.ts', function(err: Error, buf: Buffer) {
 	console.log(sha1(buf));
