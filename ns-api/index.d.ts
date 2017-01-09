13a0b4e5db39c8dc66f46f7ca49d161664e48813
Sander Koenders <sanderkoenders@gmail.com>
2017-01-09 16:12:00 +0100

    Fixed some of the errors

--- ns-api/index.d.ts
+++ ns-api/index.d.ts
@@ -1,74 +1,69 @@
-// Type definitions for ns-api 2.0.5
+// Type definitions for ns-api 2.0
 // Project: https://github.com/fvdm/nodejs-ns-api#readme
 // Definitions by: Sander Koenders <https://github.com/Archcry>
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
 
-declare module "ns-api" {
-    let nsApi: NsApiStatic;
+declare module 'ns-api' {
+    const nsApi: (conf: NsApi.NsApiConfiguration) => NsApi.NsApi;
 
     export = nsApi;
 }
 
-interface NsApiStatic {
-    /**
-     * Default Server constructor
-     */
-    (conf: NsApiConfiguration): NsApi
-}
+declare namespace NsApi {
+    interface NsApi {
+        /**
+         * Vertrektijden - departure times
+         *
+         * @callback callback
+         * @param station {string} - Station ID
+         * @param callback {function} - `function (err, data) any`
+         * @returns {void}
+         */
+        vertrektijden(station: string, callback: (err: string, data: any) => void): void;
 
-interface NsApi {
-    /**
-     * Vertrektijden - departure times
-     *
-     * @callback callback
-     * @param station {string} - Station ID
-     * @param callback {function} - `function (err, data) {}`
-     * @returns {void}
-     */
-    vertrektijden(station: string, callback: (err: string, data: Object) => void): void;
+        /**
+         * Reisadvies - travel advise
+         *
+         * @callback callback
+         * @param params {object} - Parameters
+         * @param callback {function} - `function (err, data) any`
+         * @returns {void}
+         */
+        reisadvies(params: any, callback: (err: string, data: any) => void): void;
 
-    /**
-     * Reisadvies - travel advise
-     *
-     * @callback callback
-     * @param params {object} - Parameters
-     * @param callback {function} - `function (err, data) {}`
-     * @returns {void}
-     */
-    reisadvies(params: Object, callback: (err: string, data: Object) => void): void;
+        /**
+         * Prijzen - tariffs
+         *
+         * @callback callback
+         * @param params {object} - Parameters
+         * @param callback {function} - `function (err, data) any`
+         * @returns {void}
+         */
+        prijzen(params: any, callback: (err: any, data: any) => void): void;
 
-    /**
-     * Prijzen - tariffs
-     *
-     * @callback callback
-     * @param params {object} - Parameters
-     * @param callback {function} - `function (err, data) {}`
-     * @returns {void}
-     */
-    prijzen(params: Object, callback: (err: any, data: Object) => void): void;
+        /**
+         * List available stations
+         *
+         * @callback callback
+         * @param [treeKey] {string} - Group by this key
+         * @param callback {function} - `function (err, data) any`
+         */
+        stations(treeKey: string, callback: (err: string, data: any) => void): void;
+        stations(callback: (err: string, data: any) => void): void;
 
-    /**
-     * List available stations
-     *
-     * @callback callback
-     * @param [treeKey] {string} - Group by this key
-     * @param callback {function} - `function (err, data) {}`
-     */
-    stations(treeKey: string, callback: (err: string, data: Object) => void): void;
-    stations(callback: (err: string, data: Object) => void): void;
+        /**
+         * List disruptions
+         *
+         * @callback callback
+         * @param params {object} - Parameters
+         * @param callback {function} - `function (err, data) any`
+         */
+        storingen(params: any, callback: (err: string, data: any) => void): void;
+    }
 
-    /**
-     * List disruptions
-     *
-     * @callback callback
-     * @param params {object} - Parameters
-     * @param callback {function} - `function (err, data) {}`
-     */
-    storingen(params: Object, callback: (err: string, data: Object) => void): void;
-}
-
-interface NsApiConfiguration {
-    username: string;
-    password: string;
-    timeout?: number;
+    interface NsApiConfiguration {
+        username: string;
+        password: string;
+        timeout?: number;
+    }
 }
