8f1c02df74e77390b6d9ef70ce218f0efda6abd2
JD-Robbs <JD-Robbs@users.noreply.github.com>
2017-01-09 17:49:41 +0000

    Adds SHA1 definition

---
+++ sha1/index.d.ts
@@ -0,0 +1,15 @@
+// Type definitions for sha1 v1.1.1
+// Project: https://github.com/pvorb/node-sha1
+// Definitions adapted from md5 definitions by: Bill Sourour <https://github.com/arcdev1>
+// Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
+
+/// <reference types="node" />
+
+/**
+ * js function for hashing messages with SHA1
+ * 
+ * @param {(string | Buffer)} message - a string or buffer to hash
+ * @returns {string} the resultant SHA1 hash of the given message
+ */
+declare function main(message: string | Buffer): string;
+export = main;
