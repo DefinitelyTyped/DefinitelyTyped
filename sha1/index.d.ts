1581b1253ed584cb6c3c500fb82f7eca192164d4
JD-Robbs <JD-Robbs@users.noreply.github.com>
2017-01-09 18:15:35 +0000

    Edits JSDoc

--- sha1/index.d.ts
+++ sha1/index.d.ts
@@ -7,14 +7,15 @@
 
 /**
  * js function for hashing messages with SHA1
- * 
+ *
  * @param {(string | Buffer)} message - a string or buffer to hash
+ * @param {Sha1Options} options - an options object
  * @returns {string} the resultant SHA1 hash of the given message
  */
 declare function main(message: string | Buffer, options?: Sha1Options): string | Uint8Array;
 export = main;
 
 interface Sha1Options {
-  asBytes?: boolean;
-  asString?: boolean;
+    asBytes?: boolean;
+    asString?: boolean;
 }
