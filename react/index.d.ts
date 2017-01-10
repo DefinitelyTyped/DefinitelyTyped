1e0157fa43bba4fc550520e4ef96f40cf950c8f6
Pat Sissons <pat@marinels.com>
2017-01-10 15:57:08 -0800

    fixing incorrect version header, this should resolve the current npm version issue (#13834)

--- react/index.d.ts
+++ react/index.d.ts
@@ -1,4 +1,4 @@
-// Type definitions for React v15
+// Type definitions for React v15.0
 // Project: http://facebook.github.io/react/
 // Definitions by: Asana <https://asana.com>, AssureSign <http://www.assuresign.com>, Microsoft <https://microsoft.com>, John Reilly <https://github.com/johnnyreilly/>
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
