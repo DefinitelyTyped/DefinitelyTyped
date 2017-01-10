b518befecdb1c6e58941714b021530f6696d9c8f
Alex Efimov <alex.d.efimov@gmail.com>
2017-01-10 06:03:18 +0300

    Typo fix.

--- geolib/index.d.ts
+++ geolib/index.d.ts
@@ -1,6 +1,6 @@
 // Type definitions for geolib v2.0.22
 // Project: https://github.com/manuelbieh/Geolib
-// Definitions by: Vladimir Venegas <https://github.com/vvenegasv>; Alex Efimov <https://github.com/dotnetpart>
+// Definitions by: Vladimir Venegas <https://github.com/vvenegasv>, Alex Efimov <https://github.com/dotnetpart>
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
 
 declare namespace geolib {
