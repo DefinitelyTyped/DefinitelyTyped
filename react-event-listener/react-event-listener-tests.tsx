ce6f4918f59f662754bbeda028ba8c01d767fc43
Alexey Svetliakov <asvetliakov@teledini.com>
2017-01-10 04:17:34 +0100

    added tslint.json, removed empty lines

--- react-event-listener/react-event-listener-tests.tsx
+++ react-event-listener/react-event-listener-tests.tsx
@@ -2,6 +2,5 @@ import * as React from "react";
 import EventListener, { withOptions } from "react-event-listener";
 
 <EventListener target={document} onBeforeUnload={ev => { }} />;
-<EventListener target={window} onResize={withOptions(ev => { }, { passive: true, capture: true })} />;
+<EventListener target={window} onResize={withOptions((ev: UIEvent) => { }, { passive: true, capture: true })} />;
 <EventListener target="window" onResize={() => { }}/>
-
