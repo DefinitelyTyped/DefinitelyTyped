7e6e1eb3db14ef819cea15a39e94ba6d2a2a50c6
CaiHuan <cai.huan25@gmail.com>
2017-01-10 11:54:07 +0800

    commit react-native-swiper define file

---
+++ react-native-swiper/index.d.ts
@@ -0,0 +1,66 @@
+import * as React from 'react';
+import {
+  ViewStyle
+} from 'react-native';
+
+interface SwiperProperties {
+  
+  horizontal?: boolean;
+
+  children?: React.ReactElement<any>;
+
+  style?: ViewStyle;
+
+  pagingEnabled?: boolean;
+
+  showsHorizontalScrollIndicator?: boolean;
+
+  showsVerticalScrollIndicator?: boolean;
+
+  bounces?: boolean;
+
+  scrollsToTop?: boolean;
+
+  removeClippedSubviews?: boolean;
+
+  automaticallyAdjustContentInsets?: boolean;
+
+  showsPagination?: boolean;
+
+  showsButtons?: boolean;
+
+  loadMinimal?: boolean;
+
+  loadMinimalSize?: number;
+
+  loadMinimalLoader?: React.ReactElement<any>;
+
+  loop?: boolean;
+
+  autoplay?: boolean;
+
+  autoplayTimeout?: number;
+
+  autoplayDirection?: boolean;
+
+  index?: number;
+
+  renderPagination?: (index: number, total: number, thisObject: SwiperStatic)=> React.ReactElement<any>;
+
+  dotStyle?: ViewStyle;
+
+  activeDotStyle?: ViewStyle;
+
+  dotColor?: string;
+  
+  activeDotColor?: string;
+}
+
+
+export interface SwiperStatic extends React.ComponentClass<SwiperProperties> {
+}
+
+declare let Swiper: SwiperStatic;
+declare type Swiper = SwiperStatic;
+
+export default Swiper;
