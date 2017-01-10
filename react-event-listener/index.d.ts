c7969027dc14697d26789f526f1acb95ce8d4229
Alexey Svetliakov <asvetliakov@teledini.com>
2017-01-10 04:50:55 +0100

    Fix version

--- react-event-listener/index.d.ts
+++ react-event-listener/index.d.ts
@@ -1,4 +1,4 @@
-// Type definitions for react-event-listener 0.4.0
+// Type definitions for react-event-listener 0.4
 // Project: https://github.com/oliviertassinari/react-event-listener
 // Definitions by: Alexey Svetliakov <https://github.com/asvetliakov>
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
