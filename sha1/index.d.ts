6aacd98e182240c38fab38ba77364e19cc5fb60f
JD-Robbs <JD-Robbs@users.noreply.github.com>
2017-01-10 19:18:26 +0000

    Update index.d.ts

--- sha1/index.d.ts
+++ sha1/index.d.ts
@@ -1,4 +1,4 @@
-// Type definitions for sha1 v1.1
+// Type definitions for sha1 1.1
 // Project: https://github.com/pvorb/node-sha1
 // Definitions by: Bill Sourour <https://github.com/arcdev1>
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
