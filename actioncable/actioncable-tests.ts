a8dc31d5a2a55bf458834680d4653bc452949d84
Chuck Yang <yangchuck@gmail.com>
2017-01-12 11:44:05 -0600

    [actioncable] Use union types for Subscriptions.create method (#13762)

--- actioncable/actioncable-tests.ts
+++ actioncable/actioncable-tests.ts
@@ -20,3 +20,22 @@ const helloChannel = App.cable.subscriptions.create('NetworkChannel', {
 }) as HelloChannel;
 
 helloChannel.hello('World');
+
+const channelParams: ActionCable.ChannelNameWithParams = {
+  channel: 'NetworkChannel',
+  token: 'foo',
+  data: {
+    bar: 'baz'
+  }
+};
+const channelWithParams = App.cable.subscriptions.create(channelParams, {
+  connected(): void {
+    console.log('connected');
+  },
+  disconnected(): void {
+    console.log('disconnected');
+  },
+  received(obj: Object): void {
+    console.log(obj);
+  }
+});
