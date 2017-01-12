404865c55070d56d65f9333d61f873910f6e383a
jpmnteiro <jpmnteiro@gmail.com>
2017-01-12 11:35:21 +0000

    stripped out bom

--- angular-gridster/index.d.ts
+++ angular-gridster/index.d.ts
@@ -1,4 +1,4 @@
-﻿// Type definitions for angular-gridster (gridster module) 0.13.14
+// Type definitions for angular-gridster (gridster module) 0.13.14
 // Project: https://github.com/ManifestWebDesign/angular-gridster
 // Definitions by: Joao Monteiro <https://github.com/jpmnteiro>
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
