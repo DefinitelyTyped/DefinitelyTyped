c01466455c7265ac23f1897599599a04b77074bc
Alexandre Pare <alex.pare.inc@gmail.com>
2017-01-13 18:33:29 -0500

    tsx do not work

---
+++ react-input-mask/react-input-mask-test.ts
@@ -0,0 +1,5 @@
+import * as ReactInputMask from 'react-input-mask';
+import * as React from 'react';
+
+React.createElement(ReactInputMask, { mask: "+(999) 999-9999" });
+React.createElement(ReactInputMask, { mask: "+(999) 999-9999", maskChar: '*' });
