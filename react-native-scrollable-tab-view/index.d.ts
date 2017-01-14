f3188127235187ab439513828b69ebf3b84740ff
CaiHuan <cai.huan25@gmail.com>
2017-01-14 23:12:57 +0800

    commit react-native-scrollable-tab-view

---
+++ react-native-scrollable-tab-view/index.d.ts
@@ -0,0 +1,6 @@
+// Type definitions for react-native-scrollable-tab-view 0.7
+// Project: https://github.com/brentvatne/react-native-scrollable-tab-view#readme
+// Definitions by: My Self <https://github.com/me>
+// Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
+
+Fill the header in!
