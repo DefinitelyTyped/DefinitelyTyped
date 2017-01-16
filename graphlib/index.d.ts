522582c2707ffdbdfe41818583765324b2247848
wolfson <wolfson@bracketedrebels.com>
2017-01-16 23:05:43 +0700

    tests added and contributors field extended

--- graphlib/index.d.ts
+++ graphlib/index.d.ts
@@ -1,6 +1,6 @@
 // Type definitions for graphlib 2.1.1
 // Project: https://github.com/cpettitt/graphlib
-// Definitions by: Dan Vanderkam <http://danvk.org/>
+// Definitions by: Dan Vanderkam <http://danvk.org/>, Dan Mironenko <wolfson@bracketedrebels.com>
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
 
 
