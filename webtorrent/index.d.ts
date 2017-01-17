37e9637a475e2f8ba186e97f53a17d5eab7dcec6
Tomek Łaziuk <tlaziuk@gmail.com>
2017-01-17 13:40:13 +0100

    authors

--- webtorrent/index.d.ts
+++ webtorrent/index.d.ts
@@ -1,6 +1,6 @@
 // Type definitions for WebTorrent 0.98
 // Project: https://github.com/feross/webtorrent
-// Definitions by: Bazyli Brzóska <https://invent.life>, Tomasz Łaziuk <tlaziuk@gmail.com>
+// Definitions by: Bazyli Brzóska <https://github.com/niieani>, Tomasz Łaziuk <https://github.com/tlaziuk>
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
 
 import * as ParseTorrent from 'parse-torrent';
