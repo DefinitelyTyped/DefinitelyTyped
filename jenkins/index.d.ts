e69f1680028b17317304005da4e1f9c26ec75484
rhysd <lin90162@yahoo.co.jp>
2017-01-17 18:48:20 +0900

    jenkins: improve Function types and underlying value type of promises

--- jenkins/index.d.ts
+++ jenkins/index.d.ts
@@ -7,62 +7,62 @@
 
 declare namespace create {
     interface JenkinsAPI {
-        info(callback: Function): void;
+        info(callback: (err: Error, data: any) => void): void;
         build: {
-            get(name: string, n: number, callback: Function): void;
-            log(name: string, callback: Function): void;
-            log(name: string, n: number,  callback: Function): void;
-            log(name: string, n: number, start: number, callback: Function): void;
-            log(name: string, n: number, start: number, type: 'text' | 'html', callback: Function): void;
-            log(name: string, n: number, start: number, type: 'text' | 'html', meta: boolean, callback: Function): void;
+            get(name: string, n: number, callback: (err: Error, data: any) => void): void;
+            log(name: string, callback: (err: Error, data: any) => void): void;
+            log(name: string, n: number,  callback: (err: Error, data: any) => void): void;
+            log(name: string, n: number, start: number, callback: (err: Error, data: any) => void): void;
+            log(name: string, n: number, start: number, type: 'text' | 'html', callback: (err: Error, data: any) => void): void;
+            log(name: string, n: number, start: number, type: 'text' | 'html', meta: boolean, callback: (err: Error, data: any) => void): void;
             logStream(name: string, n: number, type?: 'text' | 'html', delay?: number): NodeJS.ReadableStream;
-            stop(name: string, n: number, callback: Function): void;
+            stop(name: string, n: number, callback: (err: Error) => void): void;
         };
         job: {
-            build(name: string, callback: Function): void;
-            build(name: string, parameters: any, callback: Function): void;
-            build(name: string, parameters: any, token: string, callback: Function): void;
-            build(opts: JobBuildOptions, callback: Function): void;
-            config(name: string, callback: Function): void;
-            config(name: string, xml: string, callback: Function): void;
-            copy(name: string, from: string, callback: Function): void;
-            create(name: string, xml: string, callback: Function): void;
-            destroy(name: string, callback: Function): void;
-            disable(name: string, callback: Function): void;
-            enable(name: string, callback: Function): void;
-            exists(name: string, callback: Function): void;
-            get(name: string, callback: Function): void;
-            list(callback: Function): void;
+            build(name: string, callback: (err: Error, data: any) => void): void;
+            build(name: string, parameters: any, callback: (err: Error, data: any) => void): void;
+            build(name: string, parameters: any, token: string, callback: (err: Error, data: any) => void): void;
+            build(opts: JobBuildOptions, callback: (err: Error, data: any) => void): void;
+            config(name: string, callback: (err: Error, data: any) => void): void;
+            config(name: string, xml: string, callback: (err: Error, data: any) => void): void;
+            copy(name: string, from: string, callback: (err: Error) => void): void;
+            create(name: string, xml: string, callback: (err: Error) => void): void;
+            destroy(name: string, callback: (err: Error) => void): void;
+            disable(name: string, callback: (err: Error) => void): void;
+            enable(name: string, callback: (err: Error) => void): void;
+            exists(name: string, callback: (err: Error, exists: boolean) => void): void;
+            get(name: string, callback: (err: Error, data: any) => void): void;
+            list(callback: (err: Error, data: any) => void): void;
         };
         node: {
-            config(name: string, callback: Function): void;
-            create(name: string, callback: Function): void;
-            destroy(name: string, callback: Function): void;
-            disconnect(name: string, callback: Function): void;
-            disconnect(name: string, message: string, callback: Function): void;
-            disable(name: string, callback: Function): void;
-            disable(name: string, message: string, callback: Function): void;
-            enable(name: string, callback: Function): void;
-            exists(name: string, callback: Function): void;
-            get(name: string, callback: Function): void;
-            list(callback: Function): void;
-            list(full: boolean, callback: Function): void;
+            config(name: string, callback: (err: Error, data: any) => void): void;
+            create(name: string, callback: (err: Error) => void): void;
+            destroy(name: string, callback: (err: Error) => void): void;
+            disconnect(name: string, callback: (err: Error) => void): void;
+            disconnect(name: string, message: string, callback: (err: Error) => void): void;
+            disable(name: string, callback: (err: Error) => void): void;
+            disable(name: string, message: string, callback: (err: Error) => void): void;
+            enable(name: string, callback: (err: Error) => void): void;
+            exists(name: string, callback: (err: Error, data: boolean) => void): void;
+            get(name: string, callback: (err: Error, data: any) => void): void;
+            list(callback: (err: Error, data: any) => void): void;
+            list(full: boolean, callback: (err: Error, data: any) => void): void;
         };
         queue: {
-            list(callback: Function): void;
-            item(n: number, callback: Function): void;
-            cancel(n: number, callback: Function): void;
+            list(callback: (err: Error, data: any) => void): void;
+            item(n: number, callback: (err: Error, data: any) => void): void;
+            cancel(n: number, callback: (err: Error) => void): void;
         };
         view: {
-            config(name: string, callback: Function): void;
-            config(name: string, xml: string, callback: Function): void;
-            create(name: string, type: 'list' | 'my', callback: Function): void;
-            destroy(name: string, callback: Function): void;
-            exists(name: string, callback: Function): void;
-            get(name: string, callback: Function): void;
-            list(callback: Function): void;
-            add(name: string, job: string, callback: Function): void;
-            remove(name: string, job: string, callback: Function): void;
+            config(name: string, callback: (err: Error, data: any) => void): void;
+            config(name: string, xml: string, callback: (err: Error, data: any) => void): void;
+            create(name: string, type: 'list' | 'my', callback: (err: Error) => void): void;
+            destroy(name: string, callback: (err: Error) => void): void;
+            exists(name: string, callback: (err: Error, exists: boolean) => void): void;
+            get(name: string, callback: (err: Error, data: any) => void): void;
+            list(callback: (err: Error, data: any) => void): void;
+            add(name: string, job: string, callback: (err: Error) => void): void;
+            remove(name: string, job: string, callback: (err: Error) => void): void;
         };
     }
 
@@ -72,47 +72,47 @@ declare namespace create {
             get(name: string, n: number): Promise<any>;
             log(name: string, n: number, start?: number, type?: 'text' | 'html', meta?: boolean): Promise<any>;
             logStream(name: string, n: number, type?: 'text' | 'html', delay?: number): Promise<any>;
-            stop(name: string, n: number): Promise<any>;
+            stop(name: string, n: number): Promise<void>;
         };
         job: {
             build(name: string, parameters?: any, token?: string): Promise<any>;
             build(opts: JobBuildOptions): Promise<any>;
             config(name: string): Promise<any>;
             config(name: string, xml: string): Promise<any>;
-            copy(name: string, from: string): Promise<any>;
-            create(name: string, xml: string): Promise<any>;
-            destroy(name: string): Promise<any>;
-            disable(name: string): Promise<any>;
-            enable(name: string): Promise<any>;
-            exists(name: string): Promise<any>;
+            copy(name: string, from: string): Promise<void>;
+            create(name: string, xml: string): Promise<void>;
+            destroy(name: string): Promise<void>;
+            disable(name: string): Promise<void>;
+            enable(name: string): Promise<void>;
+            exists(name: string): Promise<boolean>;
             get(name: string): Promise<any>;
             list(): Promise<any>;
         };
         node: {
             config(name: string): Promise<any>;
-            create(name: string): Promise<any>;
-            destroy(name: string): Promise<any>;
-            disconnect(name: string, message?: string): Promise<any>;
-            disable(name: string, message?: string): Promise<any>;
-            enable(name: string): Promise<any>;
-            exists(name: string): Promise<any>;
+            create(name: string): Promise<void>;
+            destroy(name: string): Promise<void>;
+            disconnect(name: string, message?: string): Promise<void>;
+            disable(name: string, message?: string): Promise<void>;
+            enable(name: string): Promise<void>;
+            exists(name: string): Promise<boolean>;
             get(name: string): Promise<any>;
             list(full?: boolean): Promise<any>;
         };
         queue: {
             list(): Promise<any>;
             item(n: number): Promise<any>;
-            cancel(n: number): Promise<any>;
+            cancel(n: number): Promise<void>;
         };
         view: {
             config(name: string, xml?: string): Promise<any>;
-            create(name: string, type: 'list' | 'my'): Promise<any>;
-            destroy(name: string): Promise<any>;
-            exists(name: string): Promise<any>;
+            create(name: string, type: 'list' | 'my'): Promise<void>;
+            destroy(name: string): Promise<void>;
+            exists(name: string): Promise<boolean>;
             get(name: string): Promise<any>;
             list(): Promise<any>;
-            add(name: string, job: string): Promise<any>;
-            remove(name: string, job: string): Promise<any>;
+            add(name: string, job: string): Promise<void>;
+            remove(name: string, job: string): Promise<void>;
         };
     }
 
@@ -126,7 +126,7 @@ declare namespace create {
         baseUrl?: string;
         crumbIssuer?: boolean;
         headers?: any;
-        promisify?: boolean | Function;
+        promisify?: boolean | ((...args: any[]) => any);
     }
 }
 
--- jenkins/jenkins-tests.ts
+++ jenkins/jenkins-tests.ts
@@ -2,19 +2,19 @@ import * as J from 'jenkins';
 
 const jenkins = J({ baseUrl: 'http://user:pass@localhost:8080', crumbIssuer: true });
 
-jenkins.info(function(err: Error, data: any) {
+jenkins.info(function(err, data) {
   if (err) throw err;
 
   console.log('info', data);
 });
 
-jenkins.build.get('example', 1, function(err: Error, data: any) {
+jenkins.build.get('example', 1, function(err, data) {
   if (err) throw err;
 
   console.log('build', data);
 });
 
-jenkins.build.log('example', 1, function(err: Error, data: any) {
+jenkins.build.log('example', 1, function(err, data) {
   if (err) throw err;
 
   console.log('log', data);
@@ -34,168 +34,168 @@ log.on('end', function() {
   console.log('end');
 });
 
-jenkins.build.stop('example', 1, function(err: Error) {
+jenkins.build.stop('example', 1, function(err) {
   if (err) throw err;
 });
 
-jenkins.job.build('example', function(err: Error, data: any) {
+jenkins.job.build('example', function(err, data) {
   if (err) throw err;
 
   console.log('queue item number', data);
 });
 
-jenkins.job.build({ name: 'example', parameters: { name: 'value' } }, function(err: Error) {
+jenkins.job.build({ name: 'example', parameters: { name: 'value' } }, function(err) {
   if (err) throw err;
 });
 
-jenkins.job.config('example', function(err: Error, data: any) {
+jenkins.job.config('example', function(err, data) {
   if (err) throw err;
 
   console.log('xml', data);
 });
 
-jenkins.job.config('example', '<xml></xml>', function(err: Error) {
+jenkins.job.config('example', '<xml></xml>', function(err) {
   if (err) throw err;
 });
 
-jenkins.job.copy('fromJob', 'example', function(err: Error) {
+jenkins.job.copy('fromJob', 'example', function(err) {
   if (err) throw err;
 });
 
-jenkins.job.create('example', '<xml></xml>', function(err: Error) {
+jenkins.job.create('example', '<xml></xml>', function(err) {
   if (err) throw err;
 });
 
-jenkins.job.destroy('example', function(err: Error) {
+jenkins.job.destroy('example', function(err) {
   if (err) throw err;
 });
 
-jenkins.job.disable('example', function(err: Error) {
+jenkins.job.disable('example', function(err) {
   if (err) throw err;
 });
 
-jenkins.job.enable('example', function(err: Error) {
+jenkins.job.enable('example', function(err) {
   if (err) throw err;
 });
 
-jenkins.job.exists('example', function(err: Error, exists: boolean) {
+jenkins.job.exists('example', function(err, exists) {
   if (err) throw err;
 
   console.log('exists', exists);
 });
 
-jenkins.job.get('example', function(err: Error, data: any) {
+jenkins.job.get('example', function(err, data) {
   if (err) throw err;
 
   console.log('job', data);
 });
 
-jenkins.job.list(function(err: Error, data: any) {
+jenkins.job.list(function(err, data) {
   if (err) throw err;
 
   console.log('jobs', data);
 });
 
-jenkins.node.config('example', function(err: Error, data: any) {
+jenkins.node.config('example', function(err, data) {
   if (err) throw err;
 
   console.log('xml', data);
 });
 
-jenkins.node.create('slave', function(err: Error) {
+jenkins.node.create('slave', function(err) {
   if (err) throw err;
 });
 
-jenkins.node.destroy('slave', function(err: Error) {
+jenkins.node.destroy('slave', function(err) {
   if (err) throw err;
 });
 
-jenkins.node.disconnect('slave', 'no longer used', function(err: Error) {
+jenkins.node.disconnect('slave', 'no longer used', function(err) {
   if (err) throw err;
 });
 
-jenkins.node.disable('slave', 'network failure', function(err: Error) {
+jenkins.node.disable('slave', 'network failure', function(err) {
   if (err) throw err;
 });
 
-jenkins.node.enable('slave', function(err: Error) {
+jenkins.node.enable('slave', function(err) {
   if (err) throw err;
 });
 
-jenkins.node.exists('slave', function(err: Error, exists: boolean) {
+jenkins.node.exists('slave', function(err, exists) {
   if (err) throw err;
 
   console.log('exists', exists);
 });
 
-jenkins.node.get('slave', function(err: Error, data: any) {
+jenkins.node.get('slave', function(err, data) {
   if (err) throw err;
 
   console.log('node', data);
 });
 
-jenkins.node.list(function(err: Error, data: any) {
+jenkins.node.list(function(err, data) {
   if (err) throw err;
 
   console.log('nodes', data);
 });
 
-jenkins.queue.list(function(err: Error, data: any) {
+jenkins.queue.list(function(err, data) {
   if (err) throw err;
 
   console.log('queues', data);
 });
 
-jenkins.queue.item(130, function(err: Error, data: any) {
+jenkins.queue.item(130, function(err, data) {
   if (err) throw err;
 
   console.log('item', data);
 });
 
-jenkins.queue.cancel(23, function(err: Error) {
+jenkins.queue.cancel(23, function(err) {
   if (err) throw err;
 });
 
-jenkins.view.config('example', function(err: Error, data: any) {
+jenkins.view.config('example', function(err, data) {
   if (err) throw err;
 
   console.log('xml', data);
 });
 
-jenkins.view.config('example', '<xml></xml>', function(err: Error) {
+jenkins.view.config('example', '<xml></xml>', function(err) {
   if (err) throw err;
 });
 
-jenkins.view.create('example', 'list', function(err: Error) {
+jenkins.view.create('example', 'list', function(err) {
   if (err) throw err;
 });
 
-jenkins.view.destroy('example', function(err: Error) {
+jenkins.view.destroy('example', function(err) {
   if (err) throw err;
 });
 
-jenkins.view.exists('example', function(err: Error, exists: boolean) {
+jenkins.view.exists('example', function(err, exists) {
   if (err) throw err;
 
   console.log('exists', exists);
 });
 
-jenkins.view.get('example', function(err: Error, data: any) {
+jenkins.view.get('example', function(err, data) {
   if (err) throw err;
 
   console.log('view', data);
 });
 
-jenkins.view.list(function(err: Error, data: any) {
+jenkins.view.list(function(err, data) {
   if (err) throw err;
 
   console.log('views', data);
 });
 
-jenkins.view.add('example', 'jobExample', function(err: Error) {
+jenkins.view.add('example', 'jobExample', function(err) {
   if (err) throw err;
 });
 
-jenkins.view.remove('example', 'jobExample', function(err: Error) {
+jenkins.view.remove('example', 'jobExample', function(err) {
   if (err) throw err;
 });
