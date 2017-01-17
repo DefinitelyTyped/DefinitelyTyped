4f319fd0038ff68a99ff44fa04b01dd7655ac188
Ahmad Ferdous Bin Alam <ahmadferdous@gmail.com>
2017-01-17 18:37:56 -0500

    Remove invalid parameter id from DeleteDocumentByQueryParams (#14014)
    The parameter list of deleteByQuery function does not have `id`.
    The relevant documentation is available at
    https://www.elastic.co/guide/en/elasticsearch/client/javascript-api/current/api-reference.html#api-deletebyquery
    Even the Elasticsearch documentation available at
    https://www.elastic.co/guide/en/elasticsearch/reference/5.x/docs-delete-by-query.html
    does not refer to any id parameter for that function.

--- elasticsearch/index.d.ts
+++ elasticsearch/index.d.ts
@@ -1,6 +1,6 @@
 // Type definitions for elasticsearch 5.0
 // Project: https://www.elastic.co/guide/en/elasticsearch/client/javascript-api/current/index.html
-// Definitions by: Casper Skydt <https://github.com/CasperSkydt>, Blake Smith <https://github.com/bfsmith>, Dave Dunkin <https://github.com/ddunkin>, Jeffery Grajkowski <https://github.com/pushplay>, Margus Lamp <https://github.com/mlamp>
+// Definitions by: Casper Skydt <https://github.com/CasperSkydt>, Blake Smith <https://github.com/bfsmith>, Dave Dunkin <https://github.com/ddunkin>, Jeffery Grajkowski <https://github.com/pushplay>, Margus Lamp <https://github.com/mlamp>, Ahmad Ferdous Bin Alam <https://github.com/ahmadferdous>
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
 
 declare module Elasticsearch {
