9f396a35febe9d229c876d8cbeca26627594a604
Cyril Schumacher <cyril_schumacher@live.fr>
2017-01-18 17:09:27 +0100

    Add "tslint.json" file. Replace "major.minor.build" by "major.minor".

--- shipit-utils/index.d.ts
+++ shipit-utils/index.d.ts
@@ -1,4 +1,4 @@
-// Type definitions for shipit-utils 1.4.0
+// Type definitions for shipit-utils 1.4
 // Project: https://github.com/shipitjs/shipit-utils
 // Definitions by: Cyril Schumacher <https://github.com/cyrilschumacher>
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
--- shipit/index.d.ts
+++ shipit/index.d.ts
@@ -1,4 +1,4 @@
-// Type definitions for shipit-cli 1.5.1
+// Type definitions for shipit-cli 1.5
 // Project: https://github.com/shipitjs/shipit
 // Definitions by: Cyril Schumacher <https://github.com/cyrilschumacher>
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
