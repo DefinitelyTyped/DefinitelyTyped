5ba38150a79d8050cbe8e37e7a70f9cb6758b1e6
Arne Schubert <atd.schubert@gmail.com>
2017-01-18 21:18:38 +0100

    Fix lints

--- spatialite/index.d.ts
+++ spatialite/index.d.ts
@@ -1,25 +1,21 @@
-// Type definitions for spatialite 0.0.6
+// Type definitions for spatialite 0.0
 // Project: https://github.com/zhm/node-spatialite
 // Definitions by: Arne Schubert <https://github.com/atd-schubert/>
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
 
-/// <reference types="sqlite3" />
+export {
+    OPEN_READONLY,
+    OPEN_READWRITE,
+    OPEN_CREATE,
+    cached,
+    RunResult,
+    Statement,
+    verbose } from 'sqlite3';
+import {
+    Database as OriginalDatabase
+} from 'sqlite3';
 
-declare module 'spatialite' {
-    export {
-        OPEN_READONLY,
-        OPEN_READWRITE,
-        OPEN_CREATE,
-        cached,
-        RunResult,
-        Statement,
-        verbose } from 'sqlite3';
-    import {
-        Database as OriginalDatabase
-    } from 'sqlite3';
-
-    export class Database extends OriginalDatabase {
-        spatialite(cb: (err: Error) => void): void;
-    }
+export class Database extends OriginalDatabase {
+    spatialite(cb: (err: Error) => void): void;
 }
 
