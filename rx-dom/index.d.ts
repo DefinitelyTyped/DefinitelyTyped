12f7adfcf88aabbaa206ab32d43713d59d0c9bad
Pat Sissons <pat@marinels.com>
2017-01-18 18:57:31 -0800

    updating version to match the current rx-dom version

--- rx-dom/index.d.ts
+++ rx-dom/index.d.ts
@@ -1,4 +1,4 @@
-// Type definitions for RxJS v2.5.3
+// Type definitions for RxJS 7.0
 // Project: https://github.com/Reactive-Extensions/RxJS-DOM
 // Definitions by: oliver Weichhold <https://github.com/oliverw>
 // Definitions: https://github.com/borisyankov/DefinitelyTyped
