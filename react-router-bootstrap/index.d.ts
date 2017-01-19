f49ca4f7ab7ee036e3aa1deab3558758c70326f0
Karol Janyst <lapkom@gmail.com>
2017-01-19 11:13:06 +0900

    Update react-router-bootstrap definitions

--- react-router-bootstrap/index.d.ts
+++ react-router-bootstrap/index.d.ts
@@ -1,43 +1,7 @@
-// Type definitions for react-router-bootstrap
+// Type definitions for react-router-bootstrap 0.23
 // Project: https://github.com/react-bootstrap/react-router-bootstrap
-// Definitions by: Vincent Lesierse <https://github.com/vlesierse>
+// Definitions by: Vincent Lesierse <https://github.com/vlesierse>, Karol Janyst <https://github.com/LKay>
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
 
-/// <reference types="react"/>
-/// <reference types="react-router"/>
-
-declare namespace ReactRouterBootstrap {
-    interface LinkContainerProps extends ReactRouter.LinkProps {
-        disabled?: boolean
-    }
-    interface LinkContainer extends React.ComponentClass<LinkContainerProps> {}
-    interface LinkContainerElement extends React.ReactElement<LinkContainerProps> {}
-    const LinkContainer: LinkContainer
-
-    const IndexLinkContainer: LinkContainer
-}
-
-declare module "react-router-bootstrap/lib/LinkContainer" {
-
-    export default ReactRouterBootstrap.LinkContainer
-
-}
-
-declare module "react-router-bootstrap/lib/IndexLinkContainer" {
-
-    export default ReactRouterBootstrap.IndexLinkContainer
-
-}
-
-declare module "react-router-bootstrap" {
-
-    import LinkContainer from "react-router-bootstrap/lib/LinkContainer"
-    
-    import IndexLinkContainer from "react-router-bootstrap/lib/IndexLinkContainer"
-
-    export {
-        LinkContainer,
-        IndexLinkContainer
-    }
-
-}
+export { default as LinkContainer } from "react-router-bootstrap/lib/LinkContainer"
+export { default as IndexLinkContainer } from "react-router-bootstrap/lib/IndexLinkContainer"
