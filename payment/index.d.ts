abc157374f5417fa97c1b5b025afad0e04c8c1dc
Alexandre Pare <alex.pare.inc@gmail.com>
2017-01-19 12:06:22 -0500

    add npm payment

---
+++ payment/index.d.ts
@@ -0,0 +1,6 @@
+// Type definitions for payment 2.1
+// Project: https://github.com/jessepollak/payment#readme
+// Definitions by: My Self <https://github.com/me>
+// Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
+
+Fill the header in!
