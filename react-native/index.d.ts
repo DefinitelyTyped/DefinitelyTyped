a5a560b3e1dee1782450abae2a8e7f9e2b605609
Danny Cochran <daniel@cochrans.org>
2017-01-20 13:28:33 -0800

    Update webViewStatic to include postMessage API (#14151)

--- react-native/index.d.ts
+++ react-native/index.d.ts
@@ -2106,6 +2106,11 @@ declare module "react" {
          */
         goForward: () => void
 
+	/**
+	 * Post a message to the WebView in the form of a string.
+	 */
+	postMessage: (message: string) => void
+
         /**
          * Reloads the current page.
          */
