fe580bbbe6bc03b55c32fb540ae742f3eaf1ce4a
Chris Rohlfs <chris@givealittle.co.nz>
2017-01-23 00:12:53 +1300

    Increment version number

--- react-modal/index.d.ts
+++ react-modal/index.d.ts
@@ -1,4 +1,4 @@
-// Type definitions for react-modal v1.6.1
+// Type definitions for react-modal v1.6.5
 // Project: https://github.com/reactjs/react-modal
 // Definitions by: Rajab Shakirov <https://github.com/radziksh>
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
