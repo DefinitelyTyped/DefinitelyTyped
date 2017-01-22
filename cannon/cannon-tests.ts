712b7419b266eb43b9940ab3e51db6a2d90c170f
yyc <395976266@qq.com>
2017-01-22 09:27:42 +0800

    fix

--- cannon/cannon-tests.ts
+++ cannon/cannon-tests.ts
@@ -2,5 +2,5 @@ import cannon = require("cannon");
 
 var aabb = new cannon.AABB()
 
-aabb.setFromPoints(new cannon.Vec3(1,2,3));
+aabb.setFromPoints([new cannon.Vec3(1,2,3)]);
 
--- wonder-frp/wonder-frp-tests.ts
+++ wonder-frp/wonder-frp-tests.ts
@@ -1,6 +1,5 @@
 import wdFrp = require("wonder-frp");
 
 wdFrp.fromArray([1,2,3])
-    .subscribe(null,null,function(){
-        console.log("completed");
+    .subscribe(function(){
     });
