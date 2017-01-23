a6d113976a6f759abb85c948eca760722512a658
John Reilly <johnny_reilly@hotmail.com>
2017-01-23 12:20:37 +0000

    Added @pzavolinsky and @bbenezech as maintainers

--- react/index.d.ts
+++ react/index.d.ts
@@ -1,6 +1,6 @@
 // Type definitions for React v15.0
 // Project: http://facebook.github.io/react/
-// Definitions by: Asana <https://asana.com>, AssureSign <http://www.assuresign.com>, Microsoft <https://microsoft.com>, John Reilly <https://github.com/johnnyreilly/>
+// Definitions by: Asana <https://asana.com>, AssureSign <http://www.assuresign.com>, Microsoft <https://microsoft.com>, John Reilly <https://github.com/johnnyreilly/>, Benoit Benezech <https://github.com/bbenezech>, Patricio Zavolinsky <https://github.com/pzavolinsky>
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
 
 export = React;
