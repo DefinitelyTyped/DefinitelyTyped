42636ca79f11b05f15bc5e5ceeb15d9ff2cea083
Robin maenhaut <robin.maenhaut.coder@gmail.com>
2017-01-23 09:51:05 +0100

    Add test for the new styling object

--- jquery.pnotify/index.d.ts
+++ jquery.pnotify/index.d.ts
@@ -1,6 +1,6 @@
 // Type definitions for jquery.pnotify 3.x
 // Project: https://github.com/sciactive/pnotify
-// Definitions by: David Sichau <https://github.com/DavidSichau>
+// Definitions by: David Sichau <https://github.com/DavidSichau>, Robin Maenhaut <https://github.com/FUNExtreme>
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
 
 /// <reference types="jquery"/>
