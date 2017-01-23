24e002e47f43ffea46b4673e1baecd154e21f996
yyc <395976266@qq.com>
2017-01-24 06:23:05 +0800

    fix: cannon add version

--- cannon/index.d.ts
+++ cannon/index.d.ts
@@ -1,5 +1,5 @@
 // Type definitions for cannon
-// Project: https://github.com/clark-stevenson/cannon.d.ts
+// Project: https://github.com/clark-stevenson/cannon.d.ts v0.1.0
 // Definitions by: Clark Stevenson <https://github.com/clark-stevenson>
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
 
