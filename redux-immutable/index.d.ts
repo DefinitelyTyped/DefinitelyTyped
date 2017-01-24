43c485984df93d829cf0f75ca0bcde584e1189bf
Sebastian Sebald <sebastian.sebald@gmail.com>
2017-01-24 22:34:26 +0100

    Add myself as author

--- redux-immutable/index.d.ts
+++ redux-immutable/index.d.ts
@@ -1,6 +1,6 @@
 // Type definitions for redux-immutable v3.0.10
 // Project: https://github.com/gajus/redux-immutable
-// Definitions by: Pedro Pereira <https://github.com/oizie>
+// Definitions by: Pedro Pereira <https://github.com/oizie>, Sebastian Sebald <https://github.com/sebald/>
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
 
 import * as Redux from 'redux';
