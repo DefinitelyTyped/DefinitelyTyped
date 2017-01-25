9ecb61ea7671d4f19aac7c943fc7db218173181f
Tanguy Krotoff <tkrotoff@gmail.com>
2017-01-25 15:51:39 +0100

    Add unit tests for some React.CSSProperties

---
+++ react/test/cssProperties.tsx
@@ -0,0 +1,19 @@
+import * as React from 'react';
+
+const initialStyle: React.CSSProperties = { fontWeight: 'initial' };
+const initialStyleTest = <div style={initialStyle} />;
+
+const fontSizeAdjustStyle: React.CSSProperties = { fontSizeAdjust: 'none' };
+const fontSizeAdjustStyleTest = <div style={fontSizeAdjustStyle} />;
+
+const fontStretchStyle: React.CSSProperties = { fontStretch: 'condensed' };
+const fontStretchStyleTest = <div style={fontStretchStyle} />;
+
+const fontStyleStyle: React.CSSProperties = { fontStyle: 'italic' };
+const fontStyleStyleTest = <div style={fontStyleStyle} />;
+
+const fontWeightStyle: React.CSSProperties = { fontWeight: 400 };
+const fontWeightStyleTest = <div style={fontWeightStyle} />;
+
+const justifyContentStyle: React.CSSProperties = { justifyContent: 'space-around' };
+const justifyContentStyleTest = <div style={justifyContentStyle} />;
