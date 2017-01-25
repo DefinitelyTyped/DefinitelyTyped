dc67cf81a6c23ce078ecea3504bd78ff010b140f
Michael McCarthy <hencethus@gmail.com>
2017-01-25 16:09:28 -0500

    Edit import to use require, fix version number

--- js-md5/index.d.ts
+++ js-md5/index.d.ts
@@ -1,4 +1,4 @@
-// Type definitions for js-md5 v0.4.2
+// Type definitions for js-md5 v0.4
 // Project: https://github.com/emn178/js-md5
 // Definitions by: Michael McCarthy <https://github.com/mwmccarthy>
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped/
