8619ff0f697aa238b76967e8c9b603ecc55fb78c
Paul Isache <paul@Pauls-MacBook-Pro-2.local>
2017-01-26 15:03:48 +0200

    Add Steed typings

---
+++ steed/index.d.ts
@@ -0,0 +1,56 @@
+// Type definitions for steed 1.1
+// Project: https://github.com/mcollina/steed#readme
+// Definitions by: Paul Isache <https://github.com/Paul-Isache>
+// Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
+
+interface Dictionary<T> { [key: string]: T }
+interface ErrorCallback<T> { (err?: T): void }
+
+interface SteedResultCallback<T, E> { (err: E, result: T): void }
+interface SteedResultArrayCallback<T, E> { (err: E, results: T[]): void }
+interface SteedResultObjectCallback<T, E> { (err: E, results: Dictionary<T>): void }
+
+interface SteedWorker<T, E> { (task: T, callback: ErrorCallback<E>): void }
+interface SteedIterator<T, E> { (item: T, callback: ErrorCallback<E>): void }
+interface SteedResultIterator<T, R, E> { (item: T, callback: SteedResultCallback<R, E>): void }
+interface SteedFunction<T, E> { (callback: (err?: E, result?: T) => void): void }
+
+interface SteedQueue<T> {
+    push<E>(task: T, callback?: ErrorCallback<E>): void;
+    push<E>(task: T, callback?: SteedResultCallback<T, E>): void;
+    push<E>(task: T[], callback?: ErrorCallback<E>): void;
+    unshift<E>(task: T, callback?: ErrorCallback<E>): void;
+    unshift<E>(task: T[], callback?: ErrorCallback<E>): void;
+    pause(): void;
+    resume(): void;
+    idle(): boolean;
+    length(): number;
+    kill(): void;
+    concurrency: number;
+    drain: () => any;
+    empty: () => any;
+    saturated: () => any;
+}
+
+interface Steed {
+    parallel<T, E>(tasks: Array<SteedFunction<T, E>>, callback?: SteedResultArrayCallback<T, E>): void;
+    parallel<T, E>(tasks: Dictionary<SteedFunction<T, E>>, callback?: SteedResultObjectCallback<T, E>): void;
+    series<T, E>(tasks: SteedFunction<T, E>[], callback?: SteedResultArrayCallback<T, E>): void;
+    series<T, E>(tasks: Dictionary<SteedFunction<T, E>>, callback?: SteedResultObjectCallback<T, E>): void;
+    waterfall<T, E>(tasks: Function[], callback?: SteedResultCallback<T, E>): void;
+    each<T, E>(arr: T[], iterator: SteedIterator<T, E>, callback?: ErrorCallback<E>): void;
+    each<T, E>(arr: Dictionary<T>, iterator: SteedIterator<T, E>, callback?: ErrorCallback<E>): void;
+    eachSeries: typeof steed.each;
+    map<T, R, E>(arr: T[], iterator: SteedResultIterator<T, R, E>, callback?: SteedResultArrayCallback<R, E>): void;
+    map<T, R, E>(arr: Dictionary<T>, iterator: SteedResultIterator<T, R, E>, callback?: SteedResultArrayCallback<R, E>): void;
+    mapSeries: typeof steed.map;
+    queue<T, E>(worker: SteedWorker<T, E>, concurrency?: number): SteedQueue<T>;
+    queue<T, R, E>(worker: SteedResultIterator<T, R, E>, concurrency?: number): SteedQueue<T>
+}
+
+
+declare const steed: Steed
+
+declare module "steed" {
+    export = steed
+}
