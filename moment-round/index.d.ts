fab0c1b8dd929e38bc58f569149a34b153eec477
Jacob Baskin <jacob@sidewalklabs.com>
2017-01-26 14:43:58 -0500

    Fix moment-round typings

--- moment-round/index.d.ts
+++ moment-round/index.d.ts
@@ -9,7 +9,7 @@ export = moment;
 
 declare module 'moment' {
     interface Moment {
-        round(precision: number, key: string, direction: 'round' | 'ceil' | 'floor'): Moment;
+        round(precision: number, key: string, direction?: 'round' | 'ceil' | 'floor'): Moment;
         ceil(precision: number, key: string): Moment;
         floor(precision: number, key: string): Moment;
     }
