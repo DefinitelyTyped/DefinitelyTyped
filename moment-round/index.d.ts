66d3bd4a3cf9a31165485ea590d94aa0971facac
Jacob Baskin <jacob@sidewalklabs.com>
2017-01-26 14:32:36 -0500

    Add typings for moment-round

---
+++ moment-round/index.d.ts
@@ -0,0 +1,14 @@
+// Type definitions for moment-round 1.0
+// Project: https://github.com/WebDevTmas/moment-round
+// Definitions by: Jacob Baskin <https://github.com/jacobbaskin>
+// Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
+
+import * as moment from 'moment';
+
+declare module 'moment' {
+    interface Moment {
+        round(precision: number, key: string, direction: 'round' | 'ceil' | 'floor'): Moment;
+        ceil(precision: number, key: string): Moment;
+        floor(precision: number, key: string): Moment;
+    }
+}
