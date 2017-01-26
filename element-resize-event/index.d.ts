63e234c6d51ad8b3f5a5c0238fb237a9ee01d6f6
plgregoire <plgregoire81@gmail.com>
2017-01-26 13:27:45 -0500

    Match version 2.0.7

--- element-resize-event/index.d.ts
+++ element-resize-event/index.d.ts
@@ -4,5 +4,8 @@
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
 
 
-declare function elementResizeEvent(domNode: Element, callback: () => void): void;
-export = elementResizeEvent;
+export = element_resize_event;
+declare function element_resize_event(omNode: Element, callback: () => void): void;
+declare namespace element_resize_event {
+    function unbind(omNode: Element, callback: () => void): void;
+}
