f94fcf6057625d7518a79da06d7a825c6087f1cf
Yves Schelpe <yves.schelpe@gmail.com>
2017-01-29 09:27:32 +0100

    Changed versioning to MAJOR.MINOR
    To comply with maj.min versioning
    ref comment https://github.com/DefinitelyTyped/DefinitelyTyped/pull/14320#discussion_r98345142

--- dot-object/index.d.ts
+++ dot-object/index.d.ts
@@ -1,4 +1,4 @@
-// Type definitions for Dot-Object v1.4.3
+// Type definitions for Dot-Object v1.5
 // Project: https://github.com/rhalff/dot-object
 // Definitions by: Niko Kovačič <https://github.com/nkovacic>
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
