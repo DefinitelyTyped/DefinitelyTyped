05478eb92a04b36ea25246d0806b4ff2fd04b415
Jakub Pistek <mail@jakubpistek.cz>
2017-01-31 19:51:29 +0100

    Update ngStorage to latest version

--- ngstorage/index.d.ts
+++ ngstorage/index.d.ts
@@ -1,29 +1,25 @@
-// Type definitions for ngstorage 0.3.10
+// Type definitions for ngstorage 0.3.11
 // Project: https://github.com/gsklee/ngStorage
 // Definitions by: Jakub Pistek <https://github.com/kubiq>
 // Definitions: https://github.com/kubiq/DefinitelyTyped
 
-import * as angular from 'angular';
+declare module 'ngstorage' {
 
-declare module 'angular' {
-    export namespace storage {
+    interface IStorageService {
+        $default(items: {}): IStorageService;
+        $reset(items?: {}): IStorageService;
+        $apply(): void;
+        $sync(): void;
+        get<T>(key: string): T;
+    }
 
-        export interface IStorageService {
-            $default(items: {}): IStorageService;
-            $reset(items?: {}): IStorageService;
-            $apply(): void;
-            $sync(): void;
-            get<T>(key: string): T;
-        }
+    interface IStorageProvider extends angular.IServiceProvider {
 
-        export interface IStorageProvider extends angular.IServiceProvider {
+        get<T>(key: string): T;
+        set<T>(key: string, value: T): T;
 
-            get<T>(key: string): T;
-            set<T>(key: string, value: T): T;
-
-            setKeyPrefix(prefix: string): void;
-            setSerializer(serializer: (value: any) => string): void;
-            setDeserializer(deserializer: (value: string) => any): void;
-        }
+        setKeyPrefix(prefix: string): void;
+        setSerializer(serializer: (value: any) => string): void;
+        setDeserializer(deserializer: (value: string) => any): void;
     }
 }
