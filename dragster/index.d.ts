240ec85b55bea557fe0eb096453601f67a40bdfa
Zsolt Kovács <k.zsolt@live.com>
2017-01-31 09:44:42 +0100

    Module declaration for dragster

--- dragster/index.d.ts
+++ dragster/index.d.ts
@@ -3,10 +3,32 @@
 // Definitions by: Zsolt Kovacs <https://github.com/zskovacs>
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
 
-declare class Dragster {
-    constructor(elem: HTMLElement);
-    removeListeners(): void;
-    reset(): void;
+// Type definitions for dragster 0.1
+// Project: https://github.com/bensmithett/dragster
+// Definitions by: Zsolt Kovacs <https://github.com/zskovacs>
+// Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
+
+declare namespace Dragster {
+    interface Dragster {
+        removeListeners(): void;
+        reset(): void;
+    }
+
+    interface DragsterStatic {
+        (elem: HTMLElement): Dragster;
+        new (elem: HTMLElement): Dragster;
+    }
 }
 
-export = Dragster;
+// window.Dragster
+declare var Dragster: Dragster.DragsterStatic;
+
+// import Dragster = require('dragster');
+declare module 'dragster' {
+    interface ExportedDragster extends Dragster.Dragster {
+        (elem: HTMLElement): Dragster.Dragster;
+        new (elem: HTMLElement): Dragster.Dragster;
+    }
+    var Dragster: ExportedDragster;
+    export = Dragster;
+}
