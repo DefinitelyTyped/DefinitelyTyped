b2282a583592d3976af68653f7608f9b562e11ab
Rodrigo Cabello <mrcabello@plainconcepts.com>
2017-02-02 18:59:49 +0100

    adding web sockets definitions

--- hyco-ws/index.d.ts
+++ hyco-ws/index.d.ts
@@ -3,12 +3,12 @@
 // Definitions by: Manuel Rodrigo Cabello <https://github.com/mrcabellom>
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
 
-/// <reference types="ws" />
 /// <reference types="node" />
 
-declare module 'hyco-ws' {
 
-    namespace WebSocketRelay {
+import * as WebSocket from 'ws';
+
+declare namespace hycows {
 
         class HybridConnectionWebSocketServer extends NodeJS.EventEmitter {
             constructor(options: Object);
@@ -21,19 +21,13 @@ declare module 'hyco-ws' {
             controlChannel: WebSocket;
         }
 
-        interface RealyConnection {
-
-            createRelayedServer(options: Object, fn: (ws: WebSocket) => void): HybridConnectionWebSocketServer;
-            relayedConnect(address: string, fn: () => void): WebSocket;
-            createRelayToken(uri: string, key_name: string, key: string, expiry?: number): string;
-            appendRelayToken(uri: string, key_name: string, key: string, expiry?: number): string;
-            createRelayBaseUri(serviceBusNamespace: string, path: string): string;
-            createRelaySendUri(serviceBusNamespace: string, path: string, token?: any, id?: any): string;
-            createRelayListenUri(serviceBusNamespace: string, path: string, token?: any, id?: any): string;
-        }
-    }
-
-    var RelayConnection: WebSocketRelay.RealyConnection;
-
-    export = RelayConnection;
+        export function createRelayedServer(options: Object, fn: (ws: WebSocket) => void): HybridConnectionWebSocketServer;
+        export function relayedConnect(address: string, fn: () => void): WebSocket;
+        export function createRelayToken(uri: string, key_name: string, key: string, expiry?: number): string;
+        export function appendRelayToken(uri: string, key_name: string, key: string, expiry?: number): string;
+        export function createRelayBaseUri(serviceBusNamespace: string, path: string): string;
+        export function createRelaySendUri(serviceBusNamespace: string, path: string, token?: any, id?: any): string;
+        export function createRelayListenUri(serviceBusNamespace: string, path: string, token?: any, id?: any): string;
 }
+
+export = hycows;
