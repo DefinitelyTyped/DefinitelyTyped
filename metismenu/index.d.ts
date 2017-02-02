2b8a9e3d485a7e8040a04d5478fb997a979c74e0
Osman Nuri Okumuş <onokumus@gmail.com>
2017-02-02 08:46:28 +0200

    Update index.d.ts

--- metismenu/index.d.ts
+++ metismenu/index.d.ts
@@ -1,19 +1,22 @@
-// Type definitions for metisMenu 2.0.3
+// Type definitions for metisMenu 2.6.2
 // Project: http://github.com/onokumus/metisMenu
-// Definitions by: onokums <https://github.com/onokumus/>
-// Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
+// Definitions by: onokumus <https://github.com/onokumus/>
+// Definitions: https://github.com/borisyankov/DefinitelyTyped
 
 /// <reference types="jquery"/>
 
 interface MetisMenuOptions {
     toggle?: boolean;
-    doubleTapToGo?: boolean;
     activeClass?: string;
     collapseClass?: string;
     collapseInClass?: string;
     collapsingClass?: string;
+    preventDefault?: boolean;
 }
 
+type MetisMenuEvents = "show.metisMenu" | "shown.metisMenu" | "hide.metisMenu" | "hidden.metisMenu";
+
 interface JQuery {
-    metisMenu(options?: MetisMenuOptions): JQuery;
+    metisMenu(options?: MetisMenuOptions | "dispose"): JQuery;
+    on(events: MetisMenuEvents, handler: (eventObject: JQueryEventObject) => any): JQuery;
 }
