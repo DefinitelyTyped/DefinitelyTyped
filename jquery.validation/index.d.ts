125a6bf3406a4594fa75716dac278e15e874b552
Matt Bailey <mattbailey37@gmail.com>
2017-02-03 15:00:02 -0800

    Updated definitions with checkForm() function on Validator prototype

--- jquery.validation/index.d.ts
+++ jquery.validation/index.d.ts
@@ -217,6 +217,7 @@ declare namespace JQueryValidation
     interface Validator
     {
         element(element: string|JQuery): boolean;
+        checkForm(): boolean;
         /**
          * Validates the form, returns true if it is valid, false otherwise.
          */
