ecfea039280d075d972d2ec56458bade08484957
Karol Janyst <lapkom@gmail.com>
2017-02-03 09:05:37 +0900

    Add another signature to match()

--- react-router/lib/match.d.ts
+++ react-router/lib/match.d.ts
@@ -3,14 +3,22 @@ import { Basename, LocationDescriptor, ParseQueryString, RouteConfig, StringifyQ
 
 interface MatchArgs {
     routes: RouteConfig;
-    location: LocationDescriptor;
-    history?: History;
     basename?: Basename;
     parseQueryString?: ParseQueryString;
     stringifyQuery?: StringifyQuery;
 }
 
+interface MatchLocationArgs extends MatchArgs {
+    location: LocationDescriptor;
+    history?: History;
+}
+
+interface MatchHistoryArgs extends MatchArgs {
+    location?: LocationDescriptor;
+    history: History;
+}
+
 export type MatchCallback = (error: any, redirectLocation: Location, renderProps: any) => void;
 
-export default function match(args: MatchArgs, cb: MatchCallback): void;
+export default function match(args: MatchLocationArgs | MatchHistoryArgs, cb: MatchCallback): void;
 
