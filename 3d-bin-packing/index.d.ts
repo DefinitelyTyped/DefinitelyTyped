4b58376953fa5b51560f2bc94745eadfe4870f3c
Jeongho Nam <samchon@samchon.org>
2017-02-05 15:57:35 +0900

    TSTL, Samchon & Packer
    BOM problem has solved.

--- 3d-bin-packing/index.d.ts
+++ 3d-bin-packing/index.d.ts
@@ -1,4 +1,4 @@
-﻿// Type definitions for 3d-bin-packing v1.1.2
+// Type definitions for 3d-bin-packing v1.1.2
 // Project: https://github.com/betterwaysystems/packer
 // Definitions by: Jeongho Nam <http://samchon.org>
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
