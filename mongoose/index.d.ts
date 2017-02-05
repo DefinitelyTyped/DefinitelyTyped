3117141656d5095d77a80147ed22de438efd8c51
Hinell <al.neodim@gmail.com>
2017-02-05 12:50:31 +0300

    [mongoose] Fixed typescript version build
    https://travis-ci.org/DefinitelyTyped/DefinitelyTyped/builds/198517411

--- mongoose/index.d.ts
+++ mongoose/index.d.ts
@@ -2,9 +2,11 @@
 // Project: http://mongoosejs.com/
 // Definitions by: simonxca <https://github.com/simonxca/>, horiuchi <https://github.com/horiuchi/>
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
+// TypeScript Version: 2.1
 
 /// <reference types="mongodb" />
 /// <reference types="node" />
+
 
 /*
  * Guidelines for maintaining these definitions:
