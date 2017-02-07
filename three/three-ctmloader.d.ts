8191468f643e2f21be9f45e79c6b6a4287c34db6
Chunlei Hou <omni360@qq.com>
2017-02-07 22:31:36 +0800

    add three-octree.d.ts and three-ctmloader.d.ts

--- three/three-ctmloader.d.ts
+++ three/three-ctmloader.d.ts
@@ -6,31 +6,31 @@
 /// <reference types="three" />
 
 declare namespace THREE {
-  export class CTMLoader extends THREE.Loader {
-    constructor();
+    export class CTMLoader extends THREE.Loader {
+        constructor();
 
-    /**
-     * load multiple CTM parts defined in JSON.
-     * @param url(required)
-     * @param callback(required)
-     * @param parameters
-     */
-    loadParts(url: string, callback: () => any, parameters?: any): any;
+        /**
+         * load multiple CTM parts defined in JSON.
+         * @param url(required)
+         * @param callback(required)
+         * @param parameters
+         */
+        loadParts(url: string, callback: () => any, parameters?: any): any;
 
-    /**
-     * Load CTMLoader compressed models
-     * @param url(required)
-     * @param callback(required)
-     * @param parameters
-     */
-    load(url: string, callback: () => any, parameters?: any): any;
+        /**
+         * Load CTMLoader compressed models
+         * @param url(required)
+         * @param callback(required)
+         * @param parameters
+         */
+        load(url: string, callback: (geo: any) => any, parameters?: any): any;
 
-    /**
-     * create buffergeometry by ctm file.
-     * @param file(required)
-     * @param callback(required)
-     */
-    createModel(file: string, callback: () => any): any;
+        /**
+         * create buffergeometry by ctm file.
+         * @param file(required)
+         * @param callback(required)
+         */
+        createModel(file: string, callback: () => any): any;
 
-  }
+    }
 }
