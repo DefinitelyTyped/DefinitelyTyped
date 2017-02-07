3bedc18775550ad4a5c033c3bc45c97c2ac2a583
Sindre Moen <sindrenm@gmail.com>
2017-02-07 09:07:44 +0100

    Specify TypeScript version 2.1

--- mongoose/index.d.ts
+++ mongoose/index.d.ts
@@ -2,6 +2,7 @@
 // Project: http://mongoosejs.com/
 // Definitions by: simonxca <https://github.com/simonxca/>, horiuchi <https://github.com/horiuchi/>, sindrenm <https://github.com/sindrenm>
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
+// TypeScript Version: 2.1
 
 /// <reference types="mongodb" />
 /// <reference types="node" />
