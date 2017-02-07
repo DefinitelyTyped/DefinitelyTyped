8a527d816a19013184baa0812c3287621024c026
VorobeY1326 <VorobeY1326@gmail.com>
2017-02-07 19:49:58 +0500

    And now tslint will pass

--- dropkickjs/index.d.ts
+++ dropkickjs/index.d.ts
@@ -1,4 +1,4 @@
-// Type definitions for dropkickjs v2.1
+// Type definitions for dropkickjs 2.1
 // Project: http://dropkickjs.com/
 // Definitions by: Dmitry Pesterev <https://github.com/VorobeY1326/>
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
