0d7e4cd40ee155a0e05c3e3012ba86435290c821
Tomek Łaziuk <tlaziuk@gmail.com>
2017-02-07 16:48:18 +0100

    do not test that

--- body-parser/body-parser-tests.ts
+++
@@ -1,9 +0,0 @@
-import * as bodyParser from 'body-parser';
-import * as express from 'express';
-
-const app = express();
-
-express.use(bodyParser.json());
-express.use(bodyParser.raw());
-express.use(bodyParser.text());
-express.use(bodyParser.urlencoded());
