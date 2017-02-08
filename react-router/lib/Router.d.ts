DefinitelyTyped/DefinitelyTyped

commit 69a9f6d6718fba210ab007541572ff8459c6d1ce

Update react-router definitions to 3.0.x

react-router/lib/Router.d.ts
@@ -1,116 +1,106 @@
-import * as React from 'react';
-import RouterContext from './RouterContext';
+import { Component, ComponentClass, ClassAttributes, ReactNode, StatelessComponent } from "react";
 import {
-    QueryString, Query,
-    Location, LocationDescriptor, LocationState as HLocationState,
-    History, Href,
-    Pathname, Path } from 'history';
+    Hash,
+    History,
+    Href,
+    LocationKey,
+    LocationState,
+    Path,
+    Pathname,
+    Search
+} from "history";
+import { PlainRoute } from "react-router";
 
+export type Basename = string;
+export type Query = any;
+export type Action = "PUSH" | "REPLACE" | "POP";
+export interface Params {
+    [key: string]: string;
+}
+export type RoutePattern = string;
+export type RouteComponent = ComponentClass<any> | StatelessComponent<any>;
+export interface RouteComponents {
+    [name: string]: RouteComponent;
+}
+export type RouteConfig = ReactNode | PlainRoute | PlainRoute[];
+
+export type ParseQueryString = (queryString: Search) => Query;
+export type StringifyQuery = (queryObject: Query) => Search;
+
+type AnyFunction = (...args: any[]) => any;
+
+export type EnterHook = (nextState: RouterState, replace: RedirectFunction, callback?: AnyFunction) => any;
+export type LeaveHook = (prevState: RouterState) => any;
+export type ChangeHook = (prevState: RouterState, nextState: RouterState, replace: RedirectFunction, callback?: AnyFunction) => any;
+export type RouteHook = (nextLocation?: Location) => any;
+
+export interface Location {
+    patname: Pathname;
+    search: Search;
+    query: Query;
+    state: LocationState;
+    action: Action;
+    key: LocationKey;
+}
+
+export interface LocationDescriptorObject {
+    pathname?: Pathname;
+    query?: Query;
+    hash?: Hash;
+    state?: LocationState;
+}
+
+export type LocationDescriptor = Path | LocationDescriptorObject;
+
+export interface RedirectFunction {
+    (location: LocationDescriptor): void;
+    (state: LocationState, pathname: Pathname | Path, query?: Query): void;
+}
+
+export interface RouterState {
+    location: Location;
+    routes: PlainRoute[];
+    params: Params;
+    components: RouteComponent[];
+}
+
+type LocationFunction = (location: LocationDescriptor) => void;
+type GoFunction = (n: number) => void;
+type NavigateFunction = () => void;
+type ActiveFunction = (location: LocationDescriptor, indexOnly?: boolean) => boolean;
+type LeaveHookFunction = (route: any, callback: RouteHook) => void;
+type CreatePartFunction<Part> = (path: Path, query?: any) => Part;
+
+export interface InjectedRouter {
+    push: LocationFunction;
+    replace: LocationFunction;
+    go: GoFunction;
+    goBack: NavigateFunction;
+    goForward: NavigateFunction;
+    setRouteLeaveHook: LeaveHookFunction;
+    createPath: CreatePartFunction<Path>;
+    createHref: CreatePartFunction<Href>;
+    isActive: ActiveFunction;
+}
+
+export interface RouteComponentProps<P, R> {
+    location?: Location;
+    params?: P & R;
+    route?: PlainRoute;
+    router?: InjectedRouter;
+    routeParams?: R;
+}
+
+export interface RouterProps extends ClassAttributes<any> {
+    routes?: RouteConfig;
+    history?: History;
+    createElement?(component: RouteComponent, props: any): any;
+    onError?(error: any): any;
+    onUpdate?(): any;
+    render?(props: any): ReactNode;
+}
+
+type Router = ComponentClass<RouterProps>;
 declare const Router: Router;
-interface Router extends React.ComponentClass<Router.RouterProps> { }
 
 export default Router;
-
-// types based on https://github.com/rackt/react-router/blob/master/docs/Glossary.md
-
-declare namespace Router {
-    type RouteConfig = React.ReactNode | PlainRoute | PlainRoute[];
-    type RoutePattern = string;
-    type RouteComponents = { [key: string]: RouteComponent };
-
-    type ParseQueryString = (queryString: QueryString) => Query;
-    type StringifyQuery = (queryObject: Query) => QueryString;
-
-    type Component = React.ReactType;
-    type RouteComponent = Component;
-
-    type EnterHook = (nextState: RouterState, replace: RedirectFunction, callback?: Function) => void;
-    type LeaveHook = () => void;
-    type ChangeHook = (prevState: RouterState, nextState: RouterState, replace: RedirectFunction, callback: Function) => void;
-    type RouteHook = (nextLocation?: Location) => any;
-
-    type Params = { [param: string]: string };
-
-    type RouterListener = (error: Error, nextState: RouterState) => void;
-
-    type LocationDescriptor = {
-        pathname?: Pathname
-        query?: Query
-        hash?: Href
-        state?: HLocationState
-    }
-
-    interface RedirectFunction {
-        (location: LocationDescriptor): void;
-        /**
-        * @deprecated `replaceState(state, pathname, query) is deprecated; Use `replace(location)` with a location descriptor instead. http://tiny.cc/router-isActivedeprecated
-        */
-        (state: HLocationState, pathname: Pathname | Path, query?: Query): void;
-    }
-
-    interface RouterState {
-        location: Location;
-        routes: PlainRoute[];
-        params: Params;
-        components: RouteComponent[];
-    }
-
-    interface RouterProps extends React.Props<Router> {
-        history?: History;
-        routes?: RouteConfig; // alias for children
-        createElement?: (component: RouteComponent, props: Object) => any;
-        onError?: (error: any) => any;
-        onUpdate?: () => any;
-        parseQueryString?: ParseQueryString;
-        stringifyQuery?: StringifyQuery;
-        basename?: string;
-        render?: (renderProps: React.Props<{}>) => RouterContext;
-    }
-
-    interface PlainRoute {
-        path?: RoutePattern;
-        component?: RouteComponent;
-        components?: RouteComponents;
-        getComponent?: (location: Location, cb: (error: any, component?: RouteComponent) => void) => void;
-        getComponents?: (location: Location, cb: (error: any, components?: RouteComponents) => void) => void;
-        onEnter?: EnterHook;
-        onLeave?: LeaveHook;
-        indexRoute?: PlainRoute;
-        getIndexRoute?: (location: Location, cb: (error: any, indexRoute: RouteConfig) => void) => void;
-        childRoutes?: PlainRoute[];
-        getChildRoutes?: (location: Location, cb: (error: any, childRoutes: RouteConfig) => void) => void;
-    }
-
-    interface RouteComponentProps<P, R> {
-        history?: History;
-        location?: Location;
-        params?: P;
-        route?: PlainRoute;
-        routeParams?: R;
-        routes?: PlainRoute[];
-        children?: React.ReactElement<any>;
-    }
-
-    interface RouterOnContext extends History {
-        setRouteLeaveHook(route: PlainRoute, hook?: RouteHook): () => void;
-        isActive(pathOrLoc: Path | LocationDescriptor, indexOnly?: boolean): boolean;
-    }
-
-    // Wrap a component using withRouter(Component) to provide a router object
-    // to the Component's props, allowing the Component to programmatically call
-    // push and other functions.
-    //
-    // https://github.com/reactjs/react-router/blob/v2.4.0/upgrade-guides/v2.4.0.md
-
-    interface InjectedRouter {
-        push: (pathOrLoc: Path | LocationDescriptor) => void
-        replace: (pathOrLoc: Path | LocationDescriptor) => void
-        go: (n: number) => void
-        goBack: () => void
-        goForward: () => void
-        setRouteLeaveHook(route: PlainRoute, callback: RouteHook): void
-        createPath(path: History.Path, query?: History.Query): History.Path
-        createHref(path: History.Path, query?: History.Query): History.Href
-        isActive: (pathOrLoc: Path | LocationDescriptor, indexOnly?: boolean) => boolean
-    }
-}

react-router/v2/lib/Router.d.ts
@@ -1,0 +1,116 @@
+import * as React from 'react';
+import RouterContext from './RouterContext';
+import {
+    QueryString, Query,
+    Location, LocationDescriptor, LocationState as HLocationState,
+    History, Href,
+    Pathname, Path } from 'history';
+
+declare const Router: Router;
+interface Router extends React.ComponentClass<Router.RouterProps> { }
+
+export default Router;
+
+// types based on https://github.com/rackt/react-router/blob/master/docs/Glossary.md
+
+declare namespace Router {
+    type RouteConfig = React.ReactNode | PlainRoute | PlainRoute[];
+    type RoutePattern = string;
+    type RouteComponents = { [key: string]: RouteComponent };
+
+    type ParseQueryString = (queryString: QueryString) => Query;
+    type StringifyQuery = (queryObject: Query) => QueryString;
+
+    type Component = React.ReactType;
+    type RouteComponent = Component;
+
+    type EnterHook = (nextState: RouterState, replace: RedirectFunction, callback?: Function) => void;
+    type LeaveHook = () => void;
+    type ChangeHook = (prevState: RouterState, nextState: RouterState, replace: RedirectFunction, callback: Function) => void;
+    type RouteHook = (nextLocation?: Location) => any;
+
+    type Params = { [param: string]: string };
+
+    type RouterListener = (error: Error, nextState: RouterState) => void;
+
+    type LocationDescriptor = {
+        pathname?: Pathname
+        query?: Query
+        hash?: Href
+        state?: HLocationState
+    }
+
+    interface RedirectFunction {
+        (location: LocationDescriptor): void;
+        /**
+        * @deprecated `replaceState(state, pathname, query) is deprecated; Use `replace(location)` with a location descriptor instead. http://tiny.cc/router-isActivedeprecated
+        */
+        (state: HLocationState, pathname: Pathname | Path, query?: Query): void;
+    }
+
+    interface RouterState {
+        location: Location;
+        routes: PlainRoute[];
+        params: Params;
+        components: RouteComponent[];
+    }
+
+    interface RouterProps extends React.Props<Router> {
+        history?: History;
+        routes?: RouteConfig; // alias for children
+        createElement?: (component: RouteComponent, props: Object) => any;
+        onError?: (error: any) => any;
+        onUpdate?: () => any;
+        parseQueryString?: ParseQueryString;
+        stringifyQuery?: StringifyQuery;
+        basename?: string;
+        render?: (renderProps: React.Props<{}>) => RouterContext;
+    }
+
+    interface PlainRoute {
+        path?: RoutePattern;
+        component?: RouteComponent;
+        components?: RouteComponents;
+        getComponent?: (location: Location, cb: (error: any, component?: RouteComponent) => void) => void;
+        getComponents?: (location: Location, cb: (error: any, components?: RouteComponents) => void) => void;
+        onEnter?: EnterHook;
+        onLeave?: LeaveHook;
+        indexRoute?: PlainRoute;
+        getIndexRoute?: (location: Location, cb: (error: any, indexRoute: RouteConfig) => void) => void;
+        childRoutes?: PlainRoute[];
+        getChildRoutes?: (location: Location, cb: (error: any, childRoutes: RouteConfig) => void) => void;
+    }
+
+    interface RouteComponentProps<P, R> {
+        history?: History;
+        location?: Location;
+        params?: P;
+        route?: PlainRoute;
+        routeParams?: R;
+        routes?: PlainRoute[];
+        children?: React.ReactElement<any>;
+    }
+
+    interface RouterOnContext extends History {
+        setRouteLeaveHook(route: PlainRoute, hook?: RouteHook): () => void;
+        isActive(pathOrLoc: Path | LocationDescriptor, indexOnly?: boolean): boolean;
+    }
+
+    // Wrap a component using withRouter(Component) to provide a router object
+    // to the Component's props, allowing the Component to programmatically call
+    // push and other functions.
+    //
+    // https://github.com/reactjs/react-router/blob/v2.4.0/upgrade-guides/v2.4.0.md
+
+    interface InjectedRouter {
+        push: (pathOrLoc: Path | LocationDescriptor) => void
+        replace: (pathOrLoc: Path | LocationDescriptor) => void
+        go: (n: number) => void
+        goBack: () => void
+        goForward: () => void
+        setRouteLeaveHook(route: PlainRoute, callback: RouteHook): void
+        createPath(path: History.Path, query?: History.Query): History.Path
+        createHref(path: History.Path, query?: History.Query): History.Href
+        isActive: (pathOrLoc: Path | LocationDescriptor, indexOnly?: boolean) => boolean
+    }
+}

react-router/index.d.ts
@@ -1,91 +1,56 @@
-// Type definitions for react-router v2.0.0
+// Type definitions for react-router 3.0
 // Project: https://github.com/rackt/react-router
-// Definitions by: Sergey Buturlakin <https://github.com/sergey-buturlakin>, Yuichi Murata <https://github.com/mrk21>, Václav Ostrožlík <https://github.com/vasek17>, Nathan Brown <https://github.com/ngbrown>, Alex Wendland <https://github.com/awendland>, Kostya Esmukov <https://github.com/KostyaEsmukov>
+// Definitions by: Sergey Buturlakin <https://github.com/sergey-buturlakin>, Yuichi Murata <https://github.com/mrk21>, Václav Ostrožlík <https://github.com/vasek17>, Nathan Brown <https://github.com/ngbrown>, Alex Wendland <https://github.com/awendland>, Kostya Esmukov <https://github.com/KostyaEsmukov>, Karol Janyst <https://github.com/LKay>
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
 
-/// <reference types="history" />
-
-export as namespace ReactRouter;
-
-import * as React from 'react';
-
-export const routerShape: React.Requireable<any>
-
-export const locationShape: React.Requireable<any>
-
-import Router from "./lib/Router";
-import Link from "./lib/Link";
-import IndexLink from "./lib/IndexLink";
-import IndexRedirect from "./lib/IndexRedirect";
-import IndexRoute from "./lib/IndexRoute";
-import Redirect from "./lib/Redirect";
-import Route from "./lib/Route";
-import * as History from "./lib/routerHistory";
-import Lifecycle from "./lib/Lifecycle";
-import RouteContext from "./lib/RouteContext";
-import browserHistory from "./lib/browserHistory";
-import hashHistory from "./lib/hashHistory";
-import useRoutes from "./lib/useRoutes";
-import { createRoutes } from "./lib/RouteUtils";
-import { formatPattern } from "./lib/PatternUtils";
-import RouterContext from "./lib/RouterContext";
-import PropTypes from "./lib/PropTypes";
-import match from "./lib/match";
-import useRouterHistory from "./lib/useRouterHistory";
-import createMemoryHistory from "./lib/createMemoryHistory";
-import withRouter from "./lib/withRouter";
-import applyRouterMiddleware from "./lib/applyRouterMiddleware";
-
-// PlainRoute is defined in the API documented at:
-// https://github.com/rackt/react-router/blob/master/docs/API.md
-// but not included in any of the .../lib modules above.
-export type PlainRoute = Router.PlainRoute;
-
-// The following definitions are also very useful to export
-// because by using these types lots of potential type errors
-// can be exposed:
-export type EnterHook = Router.EnterHook;
-export type LeaveHook = Router.LeaveHook;
-export type ParseQueryString = Router.ParseQueryString;
-export type LocationDescriptor = Router.LocationDescriptor;
-export type RedirectFunction = Router.RedirectFunction;
-export type RouteComponent = Router.RouteComponent;
-export type RouteComponentProps<P, R> = Router.RouteComponentProps<P, R>;
-export type RouteConfig = Router.RouteConfig;
-export type RouteHook = Router.RouteHook;
-export type StringifyQuery = Router.StringifyQuery;
-export type RouterListener = Router.RouterListener;
-export type RouterState = Router.RouterState;
-export type InjectedRouter = Router.InjectedRouter;
-
-export type HistoryBase = History.HistoryBase;
-export type RouterOnContext = Router.RouterOnContext;
-export type RouteProps = Route.RouteProps;
-export type LinkProps = Link.LinkProps;
-
 export {
-    Router,
-    Link,
-    IndexLink,
-    IndexRedirect,
-    IndexRoute,
-    Redirect,
-    Route,
-    History,
-    browserHistory,
-    hashHistory,
-    Lifecycle,
-    RouteContext,
-    useRoutes,
-    createRoutes,
-    formatPattern,
-    RouterContext,
-    PropTypes,
-    match,
-    useRouterHistory,
-    createMemoryHistory,
-    withRouter,
-    applyRouterMiddleware
-};
+    Basename,
+    ChangeHook,
+    EnterHook,
+    InjectedRouter,
+    LeaveHook,
+    Location,
+    LocationDescriptor,
+    ParseQueryString,
+    RouteComponent,
+    RouteComponents,
+    RouteComponentProps,
+    RouteConfig,
+    RoutePattern,
+    RouterProps,
+    RouterState,
+    StringifyQuery,
+    Query
+} from "react-router/lib/Router";
+export { LinkProps } from "react-router/lib/Link";
+export { IndexLinkProps } from "react-router/lib/IndexLink";
+export { RouteProps, PlainRoute } from "react-router/lib/Route";
+export { IndexRouteProps } from "react-router/lib/IndexRoute";
+export { RedirectProps } from "react-router/lib/Redirect";
+export { IndexRedirectProps } from "react-router/lib/IndexRedirect";
 
-export default Router;
+/* components */
+export { default as Router } from "react-router/lib/Router";
+export { default as Link } from "react-router/lib/Link";
+export { default as IndexLink } from "react-router/lib/IndexLink";
+export { default as withRouter } from "react-router/lib/withRouter";
+
+/* components (configuration) */
+export { default as IndexRedirect } from "react-router/lib/IndexRedirect";
+export { default as IndexRoute } from "react-router/lib/IndexRoute";
+export { default as Redirect } from "react-router/lib/Redirect";
+export { default as Route } from "react-router/lib/Route";
+
+/* utils */
+export { createRoutes } from "react-router/lib/RouteUtils";
+export { default as RouterContext } from "react-router/lib/RouterContext";
+export { routerShape, locationShape } from "react-router/lib/PropTypes";
+export { default as match } from "react-router/lib/match";
+export { default as useRouterHistory } from "react-router/lib/useRouterHistory";
+export { formatPattern } from "react-router/lib/PatternUtils";
+export { default as applyRouterMiddleware } from "react-router/lib/applyRouterMiddleware";
+
+/* histories */
+export { default as browserHistory } from "react-router/lib/browserHistory";
+export { default as hashHistory } from "react-router/lib/hashHistory";
+export { default as createMemoryHistory } from "react-router/lib/createMemoryHistory";

react-router/lib/IndexLink.d.ts
@@ -1,5 +1,15 @@
-import Link from './Link';
+import { ComponentClass, CSSProperties, HTMLProps } from "react";
+import { Location, LocationDescriptor } from "react-router/lib/Router";
 
-declare const IndexLink: Link;
+type ToLocationFunction = (location: Location) => LocationDescriptor;
+
+export interface IndexLinkProps extends HTMLProps<any> {
+    to: LocationDescriptor | ToLocationFunction;
+    activeClassName?: string;
+    activeStyle?: CSSProperties;
+}
+
+type IndexLink = ComponentClass<IndexLinkProps>;
+declare const IndexLink: IndexLink;
+
 export default IndexLink;
-

react-router/lib/IndexRedirect.d.ts
@@ -1,17 +1,12 @@
-import Router from './Router';
-import * as React from 'react';
-import * as H from 'history';
+import { ComponentClass, ClassAttributes } from "react";
+import { RoutePattern, Query } from "react-router";
 
-declare const self: self.IndexRedirect;
-type self = self.IndexRedirect;
-export default self;
-
-declare namespace self {
-	interface IndexRedirectProps extends React.Props<self> {
-	    to: Router.RoutePattern;
-	    query?: H.Query;
-	    state?: H.LocationState;
-	}
-	interface IndexRedirectElement extends React.ReactElement<IndexRedirectProps> { }
-	interface IndexRedirect extends React.ComponentClass<self.IndexRedirectProps> { }
+export interface IndexRedirectProps extends ClassAttributes<any> {
+	to: RoutePattern;
+	query?: Query;
 }
+
+type IndexRedirect = ComponentClass<IndexRedirectProps>;
+declare const IndexRedirect: IndexRedirect;
+
+export default IndexRedirect;

react-router/lib/IndexRoute.d.ts
@@ -1,20 +1,28 @@
-import * as React from 'react';
-import Router from './Router';
-import * as H from 'history';
+import { ComponentClass, ClassAttributes } from "react";
+import { LocationState } from "history";
+import {
+    EnterHook,
+    ChangeHook,
+    LeaveHook,
+    RouteComponent,
+    RouteComponents,
+    RouterState
+} from "react-router";
 
-declare const self: self.IndexRoute;
-type self = self.IndexRoute;
-export default self;
+type ComponentCallback = (err: any, component: RouteComponent) => void;
+type ComponentsCallback = (err: any, components: RouteComponents) => void;
 
-declare namespace self {
-    interface IndexRouteProps extends React.Props<IndexRoute> {
-        component?: Router.RouteComponent;
-        components?: Router.RouteComponents;
-        getComponent?: (location: H.Location, cb: (error: any, component?: Router.RouteComponent) => void) => void;
-        getComponents?: (location: H.Location, cb: (error: any, components?: Router.RouteComponents) => void) => void;
-        onEnter?: Router.EnterHook;
-        onLeave?: Router.LeaveHook;
-    }
-    interface IndexRoute extends React.ComponentClass<IndexRouteProps> { }
-    interface IndexRouteElement extends React.ReactElement<IndexRouteProps> { }
+export interface IndexRouteProps {
+    component?: RouteComponent;
+    components?: RouteComponents;
+    getComponent?(nextState: RouterState, callback: ComponentCallback): void;
+    getComponents?(nextState: RouterState, callback: ComponentsCallback): void;
+    onEnter?: EnterHook;
+    onChange?: ChangeHook;
+    onLeave?: LeaveHook;
 }
+
+type IndexRoute = ComponentClass<IndexRouteProps>;
+declare const IndexRoute: IndexRoute;
+
+export default IndexRoute;

react-router/lib/Link.d.ts
@@ -1,19 +1,11 @@
-import * as React from 'react';
-import Router from './Router';
+import { ComponentClass, CSSProperties, HTMLProps } from "react";
+import { IndexLinkProps } from "react-router/lib/IndexLink";
 
+export interface LinkProps extends IndexLinkProps {
+    onlyActiveOnIndex?: boolean;
+}
+
+type Link = ComponentClass<LinkProps>;
 declare const Link: Link;
-type Link = Link.Link;
 
 export default Link;
-
-declare namespace Link {
-    interface LinkProps extends React.HTMLAttributes<Link> {
-        activeStyle?: React.CSSProperties;
-        activeClassName?: string;
-        onlyActiveOnIndex?: boolean;
-        to: Router.RoutePattern | Router.LocationDescriptor | ((...args: any[]) => Router.LocationDescriptor);
-    }
-
-    interface Link extends React.ComponentClass<LinkProps> {}
-    interface LinkElement extends React.ReactElement<LinkProps> {}
-}

react-router/lib/PatternUtils.d.ts
@@ -1,1 +1,3 @@
-export function formatPattern(pattern: string, params: {}): string;
+import { RoutePattern } from "react-router";
+
+export function formatPattern(pattern: RoutePattern, params: any): string;

commit 770c51950cbf4933cce0b632bb61d9239d765f9c

Fix typo introduced in 69a9f6d671.

react-router/lib/Router.d.ts
@@ -37,7 +37,7 @@ export type ChangeHook = (prevState: RouterState, nextState: RouterState, replac
 export type RouteHook = (nextLocation?: Location) => any;
 
 export interface Location {
-    patname: Pathname;
+    pathname: Pathname;
     search: Search;
     query: Query;
     state: LocationState;