f80e883dedaa2c3595d5c8626606dbd4e93bd9e2
Caleb Eggensperger <calebegg@google.com>
2017-02-12 11:53:40 -0500

    Fix version number

--- pako/index.d.ts
+++ pako/index.d.ts
@@ -1,4 +1,4 @@
-// Type definitions for pako 0.1.4
+// Type definitions for pako 1.0.4
 // Project: https://github.com/nodeca/pako
 // Definitions by: Denis Cappellin <https://github.com/cappellin>, Caleb Eggensperger <https://github.com/calebegg>
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
