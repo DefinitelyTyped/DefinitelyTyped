6f607fdf751d0ede6526169827ba73cae1a2cd03
mudkip <i@mudkip.me>
2017-02-13 19:49:34 +0800

    Update koa-hbs test file

--- koa-hbs/koa-hbs-tests.ts
+++ koa-hbs/koa-hbs-tests.ts
@@ -1,16 +1,15 @@
-import * as Hbs from 'koa-hbs';
 import * as Koa from 'koa';
+import hbs = require("koa-hbs");
 import * as Path from 'path';
 
 const app = new Koa();
-const hbs = new Hbs();
 
 app.use(hbs.middleware({
     viewPath: Path.join(__dirname, './views')
 }));
 
-app.use(function *(next: any) {
-    yield this.render('index', {
+app.use(async (ctx, next) => {
+    await this.render('index', {
         title: 'Hello World!'
     });
 });
