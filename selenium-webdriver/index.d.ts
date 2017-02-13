3c11ade2d0d3ab0addb43cc20080e40b03e91e99
Philip Tom <ptom@gohealth.com>
2017-02-13 15:21:34 -0600

    [selenium-webdriver] adding typings for remote file detector
    Needed to mark indexd.d.ts with TypeScript Version 2.1 since the existing
    test requires TS 2.1.
    
    Also, needed to mark protractor-helpers with TS 2.1 since it's Jasmine
    dependency requries TS 2.1.

--- selenium-webdriver/index.d.ts
+++ selenium-webdriver/index.d.ts
@@ -2,6 +2,7 @@
 // Project: https://github.com/SeleniumHQ/selenium/tree/master/javascript/node/selenium-webdriver
 // Definitions by: Bill Armstrong <https://github.com/BillArmstrong>, Yuki Kokubun <https://github.com/Kuniwak>, Craig Nishina <https://github.com/cnishina>
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
+// TypeScript Version: 2.1
 
 import * as chrome from './chrome';
 import * as edge from './edge';
