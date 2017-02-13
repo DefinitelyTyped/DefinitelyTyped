9b396a5a42900511092b65d7f3cecde1ff8d32dd
mudkip <i@mudkip.me>
2017-02-13 19:29:55 +0800

    Fix type declaration of koa-hbs

--- koa-hbs/index.d.ts
+++ koa-hbs/index.d.ts
@@ -1,22 +1,21 @@
-// Type definitions for koa-favicon v2.x
+// Type definitions for koa-hbs v1.x
 // Project: https://github.com/gilt/koa-hbs
 // Definitions by: Jacob Malone <https://github.com/jcbmln/>
 // Definitions: https://github.com/jcbmln/DefinitelyTyped
 
 /* =================== USAGE ===================
 
-    import * as Hbs from "koa-hbs";
     import * as Koa from "koa";
+....import hbs = require("koa-hbs");
 
-    var hbs = new Hbs();
     var app = new Koa();
 
     app.use(hbs.middleware({
         viewPath: __dirname + '/views'
     }));
 
-    app.use(function *() {
-        yield this.render('main', {
+    app.use(async (ctx, next) => {
+        await ctx.render('main', {
             title: 'koa-hbs'
         });
     });
@@ -25,31 +24,33 @@
 
 /// <reference types="koa" />
 
-declare module "koa-hbs" {
+import * as Koa from "koa";
 
-    import * as Koa from "koa";
-
-    namespace Hbs {
-        export interface Middleware {
-            viewPath: Array<string> | string,
-            handlebars?: Function,
-            templateOptions?: {},
-            extname?: string,
-            partialsPath?: Array<string> | string,
-            defaultLayout?: string,
-            layoutsPath?: string,
-            contentHelperName?: string,
-            blockHelperName?: string,
-            disableCache?: boolean
-        }
+declare namespace Hbs {
+    export interface Middleware {
+        viewPath: Array<string> | string,
+        handlebars?: Function,
+        templateOptions?: {},
+        extname?: string,
+        partialsPath?: Array<string> | string,
+        defaultLayout?: string,
+        layoutsPath?: string,
+        contentHelperName?: string,
+        blockHelperName?: string,
+        disableCache?: boolean 
     }
+}
 
-    class Hbs {
-        constructor();
+declare class Hbs {
+    constructor();
+    middleware(opts: Hbs.Middleware): any;
+}
 
-        middleware(opts: Hbs.Middleware): any;
+declare const hbs: Hbs;
+export = hbs;
+
+declare module "koa" {
+    export interface Context extends Request, Response {
+        render(tpl: string, locals?: {[key: string]: any}): Promise<void>;
     }
-
-    namespace Hbs {}
-    export = Hbs;
 }
