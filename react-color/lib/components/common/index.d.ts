0372d8efc639dba1ba86c47a6e02c108d42386ab
Karol Janyst <lapkom@gmail.com>
2017-02-13 14:20:22 +0900

    Update definitions for react-color

---
+++ react-color/lib/components/common/index.d.ts
@@ -0,0 +1,5 @@
+export { default as Alpha } from "react-color/lib/components/common/Alpha";
+export { default as Checkboard } from "react-color/lib/components/common/Checkboard";
+export { default as EditableInput } from "react-color/lib/components/common/EditableInput";
+export { default as Hue } from "react-color/lib/components/common/Hue";
+export { default as Saturation } from "react-color/lib/components/common/Saturation";
