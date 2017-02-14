09236930174e6d5c1c269af0f36549a6503c99b3
James Smith <kaigth@hotmail.com>
2017-02-13 16:04:03 -0800

    Update index.d.ts
    the `export as namespace ReduxActions;` is causing breakage in the newer renditions of typescript. The error now thrown is:
    
    error TS1316: Global module exports may only appear at top level.

--- redux-actions/index.d.ts
+++ redux-actions/index.d.ts
@@ -3,7 +3,6 @@
 // Definitions by: Jack Hsu <https://github.com/jaysoo>, Alex Gorbatchev <https://github.com/alexgorbatchev>
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
 
-export as namespace ReduxActions;
 
 // FSA-compliant action.
 // See: https://github.com/acdlite/flux-standard-action
