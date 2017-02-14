f29c2632db972660c78bbea06cb823386027c748
Ozimek <albert.ozimek@e-nov8.ch>
2017-02-14 05:41:45 +0100

    Type definitions for Highmaps 4.2.7 - Project: http://www.highcharts.com/products/highmaps

---
+++ highmaps/index.d.ts
@@ -0,0 +1,14 @@
+// Type definitions for Highmaps 4.2.7
+// Project: http://www.highcharts.com/products/highmaps
+// Definitions by: Albert Ozimek <https://github.com/AlbertOzimek>
+// Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
+
+
+import { Highcharts } from "highcharts";
+
+ declare namespace Highmaps {
+     interface Static extends Highcharts.Static {
+         mapChart(renderTo: string | HTMLElement, options: any, callback?: (chart: any) => void): any;
+     }
+ }
+
