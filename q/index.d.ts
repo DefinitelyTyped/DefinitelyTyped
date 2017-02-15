d88d96a2a9fbc8910fb3f1794bab1b97e97f3006
ashleybrener <ashley@starlogik.com>
2017-02-15 15:44:15 -0500

    Added Definitions by ...

--- q/index.d.ts
+++ q/index.d.ts
@@ -1,6 +1,6 @@
 // Type definitions for Q
 // Project: https://github.com/kriskowal/q
-// Definitions by: Barrie Nemetchek <https://github.com/bnemetchek>, Andrew Gaspar <https://github.com/AndrewGaspar/>, John Reilly <https://github.com/johnnyreilly>
+// Definitions by: Barrie Nemetchek <https://github.com/bnemetchek>, Andrew Gaspar <https://github.com/AndrewGaspar/>, John Reilly <https://github.com/johnnyreilly>, Ashley Brener <https://github.com/johnnyreilly>
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
 
 /**
