66532cf46800732f7981f882056813ff58506cd0
Karol Janyst <lapkom@gmail.com>
2017-02-16 09:15:46 +0900

    Remove unnecessary definitions for redux-persist-transform-compress, update other redux-persist plugins

--- redux-persist-transform-compress/index.d.ts
+++
@@ -1,9 +0,0 @@
-// Type definitions for redux-persist-transform-compress 4.1
-// Project: https://github.com/rt2zz/redux-persist-transform-compress
-// Definitions by: Karol Janyst <https://github.com/LKay>
-// Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
-
-import { PersistorConfig, Transform } from "redux-persist";
-
-export = createCompressor;
-declare function createCompressor<State, Raw>(config?: PersistorConfig): Transform<State, Raw>;
