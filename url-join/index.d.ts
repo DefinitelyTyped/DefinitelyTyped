67ec3d5b5521422a4d04a61fae40c4ed569b6e52
Mike Deverell <mike@pelotoncycle.com>
2017-02-17 14:54:25 -0500

    make url-join compatible with `import * as` syntax

--- url-join/index.d.ts
+++ url-join/index.d.ts
@@ -6,7 +6,10 @@
 /**
  * Join all arguments together and normalize the resulting url.
  * This works similar to path.join but you shouldn't use path.join for urls since it will work different depending of the operative systems but also doesn't work for some cases.
- */	
-declare function urljoin(...parts: string[]): string;
+ */
 
-export = urljoin;
+declare var urljoin: (...parts: string[]) => string;
+
+declare module "url-join" {
+    export = urljoin;
+}
---
+++ url-join/url-join-import-star-tests.ts
@@ -0,0 +1,3 @@
+import * as urljoin from "url-join";
+
+const s: string = urljoin("foo", "bar", "baz");
