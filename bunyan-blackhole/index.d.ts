9ff23a4dd7d6613971bd59afd82737004c711791
Olivier Chevet <olivr70@outlook.fr>
2017-02-18 01:00:55 +0100

    added tslint compliance

--- bunyan-blackhole/index.d.ts
+++ bunyan-blackhole/index.d.ts
@@ -1,18 +1,14 @@
-// Type definitions for bunyan-blackhole v0.2.0
+// Type definitions for bunyan-blackhole 0.2
 // Project: https://github.com/Floby/node-bunyan-blackhole
 // Definitions by: Olivier Chevet <https://github.com/olivr70>
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
 
-/// <reference types="bunyan"/>
+import * as Logger from "bunyan";
 
-declare module "bunyan-blackhole" {
-    import * as Logger from "bunyan";
-
-    /**
-     * Constructor.
-     * @param {string} [name] name of the blackhole Logger
-     * @return {Logger} A bunyan logger .
-     */
-    function bunyanBlackHole(name:string): Logger;
-    export = bunyanBlackHole;
-}
+/**
+ * Constructor.
+ * @param {string} [name] name of the blackhole Logger
+ * @return {Logger} A bunyan logger .
+ */
+declare function bunyanBlackHole(name: string): Logger;
+export = bunyanBlackHole;
