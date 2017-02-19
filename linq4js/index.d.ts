a3940297b356734f60d2ecd0de148ee306137a25
morrisjdev <morrisj@live.de>
2017-02-19 22:03:08 +0100

    fixed version

--- linq4js/index.d.ts
+++ linq4js/index.d.ts
@@ -1,4 +1,4 @@
-// Type definitions for Linq4JS 2.0
+// Type definitions for Linq4JS 2.1
 // Project: https://github.com/morrisjdev/Linq4JS
 // Definitions by: Morris Janatzek <https://github.com/morrisjdev>
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
