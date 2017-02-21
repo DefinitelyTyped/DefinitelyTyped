c58e43f59eb9c58dcbb73acd5bbd0936474b4b96
Anton Kandybo <Anton Kandybo>
2017-02-21 16:45:21 +0200

    Added missing properties

--- react-fontawesome/index.d.ts
+++ react-fontawesome/index.d.ts
@@ -1,6 +1,6 @@
-// Type definitions for react-fontawesome 1.1
+// Type definitions for react-fontawesome 1.5.0
 // Project: https://github.com/danawoodman/react-fontawesome
-// Definitions by: Timur Rustamov <https://github.com/timurrustamov>
+// Definitions by: Timur Rustamov <https://github.com/timurrustamov>, Anton Kandybo <https://github.com/dublicator>
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
 // TypeScript Version: 2.1
 
@@ -12,19 +12,21 @@ declare module "react-fontawesome" {
   type FontAwesomeSize = 'lg' | '2x' | '3x' | '4x' | '5x';
 
   interface FontAwesomeProps {
-
+    ariaLabel?: string,
     border?: boolean,
     className?: string,
+    cssModule?: any,
     fixedWidth?: boolean,
     flip?: boolean,
-    inverse?: boolean
+    inverse?: boolean,
     name: string,
     pulse?: boolean,
     rotate?: number,
     size?: FontAwesomeSize,
     spin?: boolean,
     stack?: string,
-    style?: React.CSSProperties
+    style?: React.CSSProperties,
+    tag?: string
   }
 
   class FontAwesome extends React.Component<FontAwesomeProps, {}> {}
