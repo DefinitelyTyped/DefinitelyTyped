a075f20c9e8baa6faf0d82b4b8d018b344aba42c
Julien CROUZET <contact@juliencrouzet.fr>
2017-02-22 15:43:04 +0100

    Add version in header : 0.13

--- uws/index.d.ts
+++ uws/index.d.ts
@@ -1,4 +1,4 @@
-// Type definitions for µWS
+// Type definitions for µWS 0.13
 // Project: https://github.com/uWebSockets/uWebSockets
 // Definitions by: York Yao <https://github.com/plantain-00>
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
