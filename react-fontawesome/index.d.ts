27971c3c3d11c52863e762f4ad033a80e0d97b62
Anton Kandybo <dublicator@hotmail.com>
2017-02-23 00:42:22 +0200

    Fixing flip and stack properties types

--- react-fontawesome/index.d.ts
+++ react-fontawesome/index.d.ts
@@ -10,6 +10,8 @@ declare module "react-fontawesome" {
   import React = require('react');
 
   type FontAwesomeSize = 'lg' | '2x' | '3x' | '4x' | '5x';
+  type FontAwesomeStack = "1x" | "2x";
+  type FontAwesomeFlip = "horizontal" | "vertical";
 
   interface FontAwesomeProps {
     ariaLabel?: string,
@@ -17,14 +19,14 @@ declare module "react-fontawesome" {
     className?: string,
     cssModule?: any,
     fixedWidth?: boolean,
-    flip?: boolean,
+    flip?: FontAwesomeFlip,
     inverse?: boolean,
     name: string,
     pulse?: boolean,
     rotate?: number,
     size?: FontAwesomeSize,
     spin?: boolean,
-    stack?: string,
+    stack?: FontAwesomeStack,
     style?: React.CSSProperties,
     tag?: string
   }
