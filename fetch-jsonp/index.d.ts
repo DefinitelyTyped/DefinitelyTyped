d1a477c46afe174d349d007c1021332d8cfce73c
Tanguy Krotoff <tkrotoff@gmail.com>
2017-02-23 13:29:09 +0100

    Add “TypeScript Version: 2.2” header

--- fetch-jsonp/index.d.ts
+++ fetch-jsonp/index.d.ts
@@ -2,6 +2,7 @@
 // Project: https://github.com/camsong/fetch-jsonp
 // Definitions by: Tanguy Krotoff <https://github.com/tkrotoff>
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
+// TypeScript Version: 2.2
 
 declare namespace fetchJsonp {
   interface Options {
