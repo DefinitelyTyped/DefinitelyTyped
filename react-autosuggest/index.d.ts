09d3052cc260863cdd6a9e76cf2f1666c6607ea2
Terry Bayne <baynet@saic.com>
2017-02-23 12:39:17 -0600

    Updated typings for the react-autosuggest package

--- react-autosuggest/index.d.ts
+++ react-autosuggest/index.d.ts
@@ -1,37 +1,43 @@
-// Type definitions for react-autosuggest 7.0
-// Project: http://react-autosuggest.js.org/
-// Definitions by: Nicolas Schmitt <https://github.com/nicolas-schmitt>, Philip Ottesen <https://github.com/pjo256>, Robert Essig <https://github.com/robessog>
-// Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
-// TypeScript Version: 2.1
+// Type definitions for react-autosuggest 7.0 Project:
+// http://react-autosuggest.js.org/ Definitions by: Nicolas Schmitt
+// <https://github.com/nicolas-schmitt>, Philip Ottesen
+// <https://github.com/pjo256>, Robert Essig <https://github.com/robessog>,
+// Terry Bayne <https://github.com/tbayne> Definitions:
+// https://github.com/DefinitelyTyped/DefinitelyTyped TypeScript Version: 2.1
 
 import * as React from 'react';
+declare class Autosuggest extends React.Component < any,
+any > {}
+export = Autosuggest;
+export as namespace Autosuggest;
 
+declare namespace Autosuggest {
   interface SuggestionsFetchRequest {
-    value: string;
-    reason: string;
+    value : string;
+    reason : string;
   }
 
   interface InputValues {
-    value: string;
+    value : string;
     valueBeforeUpDown?: string;
   }
 
   interface ChangeEvent {
-    newValue: string;
-    method: 'down' | 'up' | 'escape' | 'enter' | 'click' | 'type';
+    newValue : string;
+    method : 'down' | 'up' | 'escape' | 'enter' | 'click' | 'type';
   }
 
   interface BlurEvent {
-    focusedSuggestion: any;
+    focusedSuggestion : any;
   }
 
-  interface InputProps extends React.HTMLAttributes<any> {
+  interface InputProps extends React.HTMLAttributes < any > {
     value: string;
-    onChange: (event: React.FormEvent<any>, params?: ChangeEvent) => void;
-    onBlur?: (event: React.FormEvent<any>, params?: BlurEvent) => void;
+    onChange: (event : React.FormEvent < any >, params?: ChangeEvent) => void;
+    onBlur?: (event : React.FormEvent < any >, params?: BlurEvent) => void;
   }
 
- export interface SuggestionSelectedEventData<TSuggestion> {
+  export interface SuggestionSelectedEventData < TSuggestion > {
     method: 'click' | 'enter';
     sectionIndex: number | null;
     suggestion: TSuggestion;
@@ -50,25 +56,25 @@ import * as React from 'react';
     suggestionsContainer?: string;
   }
 
-  interface AutosuggestProps extends React.Props<Autosuggest> {
+  interface AutosuggestProps extends React.Props < Autosuggest > {
     suggestions: any[];
-    onSuggestionsFetchRequested: (request: SuggestionsFetchRequest) => void;
+    onSuggestionsFetchRequested: (request : SuggestionsFetchRequest) => void;
     onSuggestionsClearRequested?: () => void;
-    getSuggestionValue: (suggestion: any) => any;
-    renderSuggestion: (suggestion: any, inputValues: InputValues) => JSX.Element;
+    getSuggestionValue: (suggestion : any) => any;
+    renderSuggestion: (suggestion : any, inputValues : InputValues) => JSX.Element;
     inputProps: InputProps;
-    onSuggestionSelected?: (event: React.FormEvent<any>, data: SuggestionSelectedEventData<any>) => void;
-    shouldRenderSuggestions?: (value: string) => boolean;
+    onSuggestionSelected?: (event : React.FormEvent < any >, data : SuggestionSelectedEventData < any >) => void;
+    shouldRenderSuggestions?: (value : string) => boolean;
     alwaysRenderSuggestions?: boolean;
     focusFirstSuggestion?: boolean;
     focusInputOnSuggestionClick?: boolean;
     multiSection?: boolean;
-    renderSectionTitle?: (section: any, inputValues: InputValues) => JSX.Element;
-    getSectionSuggestions?: (section: any) => any[];
+    renderSectionTitle?: (section : any, inputValues : InputValues) => JSX.Element;
+    getSectionSuggestions?: (section : any) => any[];
     renderInputComponent?: () => JSX.Element;
-    renderSuggestionsContainer?: (children: any) => JSX.Element;
+    renderSuggestionsContainer?: (children : any) => JSX.Element;
     theme?: Theme;
     id?: string;
   }
 
-  export class Autosuggest extends React.Component<any, any> {}
+}
