d69515099e629fdde9a9b5e5cdec50608427cd2a
Julien CROUZET <contact@juliencrouzet.fr>
2017-02-23 12:25:35 +0100

    mu to u

--- uws/index.d.ts
+++ uws/index.d.ts
@@ -1,4 +1,4 @@
-// Type definitions for µWS 0.13
+// Type definitions for uWS 0.13
 // Project: https://github.com/uWebSockets/uWebSockets
 // Definitions by: York Yao <https://github.com/plantain-00>
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
