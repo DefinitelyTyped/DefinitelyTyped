c967d734502ed41a65b64bada42ab8c5f4d4bfc5
beverts312 <texas312@gmail.com>
2017-02-24 09:24:12 -0600

    fix yeoman typings

--- yeoman-generator/index.d.ts
+++ yeoman-generator/index.d.ts
@@ -8,77 +8,79 @@ import { Questions, Answers } from 'inquirer';
 
 declare type Callback = (err: any) => void;
 
-declare namespace Base {
-    class Storage {
-        constructor(name: string, fs: MemFsEditor, configPath: string);
+export class Storage {
+    constructor(name: string, fs: MemFsEditor, configPath: string);
 
-        defaults(defaults: {}): {};
-        delete(key: string): void;
-        get(key: string): any;
-        getAll(): { [key: string]: any };
-        save(): void;
-        set(key: string, value: any): any;
-    }
-    interface InstallOptions {
-        npm?: boolean;
-        bower?: boolean;
-        yarn?: boolean;
-        skipMessage?: boolean;
-        callback?: Callback;
-    }
-    interface ArgumentConfig {
-        description?: string;
-        required?: boolean;
-        optional?: boolean;
-        type?: typeof String|typeof Number|typeof Array|typeof Object;
-        default?: any;
-    }
-    interface OptionConfig {
-        alias?: string;
-        default?: any;
-        description?: string;
-        hide?: boolean;
-        type?: typeof Boolean|typeof String|typeof Number;
-    }
-    interface MemFsEditor {
-        read(filepath: string, options?: {}): string;
-        readJSON(filepath: string, defaults?: {}): any;
-        write(filepath: string, contents: string): void;
-        writeJSON(filepath: string, contents: {}, replacer?: (key: string, value: any) => any, space?: number): void;
-        extendJSON(filepath: string, contents: {}, replacer?: (key: string, value: any) => any, space?: number): void;
-        delete(filepath: string, options?: {}): void;
-        copy(from: string, to: string, options?: {}): void;
-        copyTpl(from: string, to: string, context: {}, templateOptions?: {}, copyOptions?: {}): void;
-        move(from: string, to: string, options?: {}): void;
-        exists(filepath: string): boolean;
-        commit(callback: Callback): void;
-        commit(filters: any[], callback: Callback): void;
-    }
+    defaults(defaults: {}): {};
+    delete(key: string): void;
+    get(key: string): any;
+    getAll(): { [key: string]: any };
+    save(): void;
+    set(key: string, value: any): any;
 }
 
-declare class Base extends EventEmitter {
+export interface InstallOptions {
+    npm?: boolean;
+    bower?: boolean;
+    yarn?: boolean;
+    skipMessage?: boolean;
+    callback?: Callback;
+}
+
+export interface ArgumentConfig {
+    description?: string;
+    required?: boolean;
+    optional?: boolean;
+    type?: typeof String | typeof Number | typeof Array | typeof Object;
+    default?: any;
+}
+
+export interface OptionConfig {
+    alias?: string;
+    default?: any;
+    description?: string;
+    hide?: boolean;
+    type?: typeof Boolean | typeof String | typeof Number;
+}
+
+export interface MemFsEditor {
+    read(filepath: string, options?: {}): string;
+    readJSON(filepath: string, defaults?: {}): any;
+    write(filepath: string, contents: string): void;
+    writeJSON(filepath: string, contents: {}, replacer?: (key: string, value: any) => any, space?: number): void;
+    extendJSON(filepath: string, contents: {}, replacer?: (key: string, value: any) => any, space?: number): void;
+    delete(filepath: string, options?: {}): void;
+    copy(from: string, to: string, options?: {}): void;
+    copyTpl(from: string, to: string, context: {}, templateOptions?: {}, copyOptions?: {}): void;
+    move(from: string, to: string, options?: {}): void;
+    exists(filepath: string): boolean;
+    commit(callback: Callback): void;
+    commit(filters: any[], callback: Callback): void;
+}
+
+export class Base {
     static extend(protoProps?: any, staticProps?: any): Base;
 
-    constructor(args: string|string[], options: {});
+    constructor(args: string | string[], options: {});
 
     env: {};
     args: {};
     resolved: string;
     description: string;
     appname: string;
-    config: Base.Storage;
-    fs: Base.MemFsEditor;
+    config: Storage;
+    fs: MemFsEditor;
     options: {};
     log(message?: string, context?: any): void;
 
-    argument(name: string, config: Base.ArgumentConfig): this;
-    composeWith(namespace: string, options: { [name: string]: any }, settings?: { local: string, link: 'weak'|'strong' }): this;
+    argument(name: string, config: ArgumentConfig): this;
+    composeWith(namespace: string, options: { [name: string]: any }, settings?: { local: string, link: 'weak' | 'strong' }): this;
     destinationPath(...path: string[]): string;
     destinationRoot(rootPath?: string): string;
     determineAppname(): string;
-    option(name: string, config: Base.OptionConfig): this;
+    option(name: string, config: OptionConfig): this;
     prompt(questions: Questions): Promise<Answers>;
-    registerTransformStream(stream: {}|Array<{}>): this;
+    registerTransformStream(stream: {} | Array<{}>): this;
     rootGeneratorName(): string;
     rootGeneratorVersion(): string;
     run(cb?: Callback): this;
@@ -97,11 +99,11 @@ declare class Base extends EventEmitter {
     spawnCommandSync(command: string, args: string[], opt?: {}): any;
 
     // actions/install mixin
-    bowerInstall(component?: string|string[], options?: {}, cb?: Callback, spawnOptions?: {}): this;
-    installDependencies(options?: Base.InstallOptions): this;
-    npmInstall(pkgs?: string|string[], options?: {}, cb?: Callback, spawnOptions?: {}): this;
-    runInstall(installer: string, paths?: string|string[], options?: {}, cb?: Callback, spawnOptions?: {}): this;
-    yarnInstall(pkgs?: string|string[], options?: {}, cb?: Callback, spawnOptions?: {}): this;
+    bowerInstall(component?: string | string[], options?: {}, cb?: Callback, spawnOptions?: {}): this;
+    installDependencies(options?: InstallOptions): this;
+    npmInstall(pkgs?: string | string[], options?: {}, cb?: Callback, spawnOptions?: {}): this;
+    runInstall(installer: string, paths?: string | string[], options?: {}, cb?: Callback, spawnOptions?: {}): this;
+    yarnInstall(pkgs?: string | string[], options?: {}, cb?: Callback, spawnOptions?: {}): this;
 
     // actions/user mixin
     readonly user: {
@@ -114,4 +116,3 @@ declare class Base extends EventEmitter {
         }
     };
 }
-export = Base;
