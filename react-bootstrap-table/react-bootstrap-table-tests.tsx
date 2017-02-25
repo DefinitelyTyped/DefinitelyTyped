94e97b236e0612d60d53693443ef011d8288183e
Andrey Kyrdyumov <kant2002@gmail.com>
2017-02-25 11:40:59 +0600

    Add fitlers paramters to react-bootstrap-table

--- react-bootstrap-table/react-bootstrap-table-tests.tsx
+++ react-bootstrap-table/react-bootstrap-table-tests.tsx
@@ -2,7 +2,7 @@
 
 import * as React from 'react';
 import { render } from 'react-dom';
-import { BootstrapTable, TableHeaderColumn } from 'react-bootstrap-table';
+import { BootstrapTable, TableHeaderColumn, ApplyFilterParameter } from 'react-bootstrap-table';
 
 var products = [{
   id: 1,
@@ -27,3 +27,56 @@ render(
     </BootstrapTable>,
   document.getElementById("app")
 );
+
+const qualityType = {
+  0: 'good',
+  1: 'bad',
+  2: 'unknown'
+};
+
+function enumFormatter(cell: any, row: any, enumObject: any) {
+  return enumObject[cell];
+}
+
+class SelectFilterWithDefaultValue extends React.Component<any, any> {
+  render() {
+    return (
+      <BootstrapTable data={ products }>
+          <TableHeaderColumn dataField='id' isKey>Product ID</TableHeaderColumn>
+          <TableHeaderColumn dataField='name'>Product Name</TableHeaderColumn>
+          <TableHeaderColumn dataField='quality' filterFormatted dataFormat={ enumFormatter }
+            formatExtraData={ qualityType } filter={ { type: 'SelectFilter', options: qualityType, defaultValue: 1 } }>Product Quality</TableHeaderColumn>
+      </BootstrapTable>
+    );
+  }
+}
+
+class TextFilterWithCondition extends React.Component<any, any> {
+  render() {
+    return (
+      <BootstrapTable data={ products }>
+        <TableHeaderColumn dataField='id' isKey>Product ID</TableHeaderColumn>
+        <TableHeaderColumn dataField='name' filter={ { type: 'TextFilter', delay: 1000, condition: 'eq' } }>Product Name</TableHeaderColumn>
+        <TableHeaderColumn dataField='price'>Product Price</TableHeaderColumn>
+      </BootstrapTable>
+    );
+  }
+}
+
+function getCustomFilter(filterHandler: (parameters?: ApplyFilterParameter) => void, customFilterParameters: any) {
+  return (
+    <div />
+  );
+}
+
+class CustomFilter extends React.Component<any, any> {
+  render() {
+    return (
+      <BootstrapTable data={ products }>
+        <TableHeaderColumn dataField='id' isKey>Product ID</TableHeaderColumn>
+        <TableHeaderColumn dataField='name'>Product Name</TableHeaderColumn>
+        <TableHeaderColumn dataField='isInStock' filter={ { type: 'CustomFilter', getElement: getCustomFilter, customFilterParameters: { textOK: 'yes', textNOK: 'no' } } }>Product Is In Stock</TableHeaderColumn>
+      </BootstrapTable>
+    );
+  }
+}
