a9b17e6e2669276dfacde9d52e3a10e6c2b9e043
Wayne <wayne.carson@gmail.com>
2017-02-26 12:50:09 -0500

    Updated indent formatting to meet DT standards

--- csvtojson/csvtojson-tests.ts
+++ csvtojson/csvtojson-tests.ts
@@ -11,35 +11,35 @@ const csvStr: string = `1,2,3
 
 // event emitter version using factory function
 csv({ noheader: true })
-	.fromString(csvStr)
-	.on('csv', (csvRow: string[]) => {	// this func will be called 3 times
-	    console.log(csvRow);						// => [1,2,3] , [4,5,6]  , [7,8,9]
-	})
-	.on('done', () => {
-	    //parsing finished
-	});
+    .fromString(csvStr)
+    .on('csv', (csvRow: string[]) => {  // this func will be called 3 times
+        console.log(csvRow);                        // => [1,2,3] , [4,5,6]  , [7,8,9]
+    })
+    .on('done', () => {
+        //parsing finished
+    });
 
 // event emitter version using Converter class
 new csv.Converter({ noheader: true })
-	.fromString(csvStr)
-	.on('csv', (csvRow: string[]) => {	// this func will be called 3 times
-	    console.log(csvRow);						// => [1,2,3] , [4,5,6]  , [7,8,9]
-	})
-	.on('done', () => {
-	    //parsing finished
-	});
+    .fromString(csvStr)
+    .on('csv', (csvRow: string[]) => {  // this func will be called 3 times
+        console.log(csvRow);                        // => [1,2,3] , [4,5,6]  , [7,8,9]
+    })
+    .on('done', () => {
+        //parsing finished
+    });
 
 // callback version using Converter class
 new csv.Converter({ noheader: true })
-	.fromString(csvStr, (err, result) => {
-		console.log(JSON.stringify(result));
-	});
+    .fromString(csvStr, (err, result) => {
+        console.log(JSON.stringify(result));
+    });
 
 // callback version using factory function
 csv({ noheader: true })
-	.fromString(csvStr, (err, result) => {
-		console.log(JSON.stringify(result));
-	});
+    .fromString(csvStr, (err, result) => {
+        console.log(JSON.stringify(result));
+    });
 
 /////////////////////////////
 // From CSV File
@@ -47,35 +47,35 @@ const filePath = './test.csv';
 
 // event emitter version using factory function
 csv()
-	.fromFile(filePath)
-	.on('json', (jsonObj: any) => {
-	    console.log(JSON.stringify(jsonObj));
-	})
-	.on('done', (error: any) => {
-	    console.log('end');
-	});
+    .fromFile(filePath)
+    .on('json', (jsonObj: any) => {
+        console.log(JSON.stringify(jsonObj));
+    })
+    .on('done', (error: any) => {
+        console.log('end');
+    });
 
 // event emitter version using Converter class
 new csv.Converter()
-	.fromFile(filePath)
-	.on('json', (jsonObj: any) => {
-	    console.log(JSON.stringify(jsonObj));
-	})
-	.on('done', (error: any) => {
-	    console.log('end');
-	});
+    .fromFile(filePath)
+    .on('json', (jsonObj: any) => {
+        console.log(JSON.stringify(jsonObj));
+    })
+    .on('done', (error: any) => {
+        console.log('end');
+    });
 
 // callback version using factory function
 csv()
-	.fromFile(filePath, (err, result) => {
-		console.log(JSON.stringify(result));
-	});
+    .fromFile(filePath, (err, result) => {
+        console.log(JSON.stringify(result));
+    });
 
 // callback version using Converter class
 new csv.Converter()
-	.fromFile(filePath, (err, result) => {
-		console.log(JSON.stringify(result));
-	});
+    .fromFile(filePath, (err, result) => {
+        console.log(JSON.stringify(result));
+    });
 
 /////////////////////////////
 // From CSV Stream
@@ -84,31 +84,31 @@ const stream = fs.createReadStream(filePath);
 
 // event emitter version using factory function
 csv().fromStream(stream)
-	.on('json', (jsonObj: any) => {
-	    console.log(JSON.stringify(jsonObj));
-	})
-	.on('done', (error: any) => {
-	    console.log('end');
-	});
+    .on('json', (jsonObj: any) => {
+        console.log(JSON.stringify(jsonObj));
+    })
+    .on('done', (error: any) => {
+        console.log('end');
+    });
 
 // event emitter version using Converter class
 new csv.Converter()
-	.fromStream(stream)
-	.on('json', (jsonObj: any) => {
-	    console.log(JSON.stringify(jsonObj));
-	})
-	.on('done', (error: any) => {
-	    console.log('end');
-	});
+    .fromStream(stream)
+    .on('json', (jsonObj: any) => {
+        console.log(JSON.stringify(jsonObj));
+    })
+    .on('done', (error: any) => {
+        console.log('end');
+    });
 
 // callback version using factory function
 csv()
-	.fromStream(stream, (err, result) => {
-		console.log(JSON.stringify(result));
-	});
+    .fromStream(stream, (err, result) => {
+        console.log(JSON.stringify(result));
+    });
 
 // callback version using Converter class
 new csv.Converter()
-	.fromStream(stream, (err, result) => {
-		console.log(JSON.stringify(result));
-	});
+    .fromStream(stream, (err, result) => {
+        console.log(JSON.stringify(result));
+    });
