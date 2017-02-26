b1976050bf400759f6c9e24a1f11c3c956b9e85e
Tim Jacobi <dev@tim-jacobi.com>
2017-02-26 00:22:47 +0000

    update after revisiting source

--- couchdb-nano/index.d.ts
+++ couchdb-nano/index.d.ts
@@ -6,13 +6,84 @@
 import { EventEmitter } from 'events';
 import { Request, CoreOptions } from 'request';
 
-declare function nano(config: nano.Config | string): nano.Client;
+declare function nano(config: nano.Configuration | string): nano.ServerScope | nano.DocumentScope;
 
 declare namespace nano {
-    interface UpdatesParams {
-        feed: "longpoll" | "continuous" | "eventsource"
-        timeout: number
-        heartbeat: boolean
+    interface Configuration {
+        url: string
+        requestDefaults?: CoreOptions
+        log?(id: string, args: any): void
+        parseUrl?: boolean
+    }
+
+    interface ServerScope {
+        readonly config: ServerConfig
+        db: DatabaseScope
+        use(db: string): DocumentScope
+        scope(db: string): DocumentScope
+        request: RequestFunction
+        relax: RequestFunction
+        dinosaur: RequestFunction
+        auth(username: string, userpass: string, callback?: Function): Request
+        session(callback?: Callback): Request
+        updates(params?: UpdatesParams, callback?: Callback): Request
+        followUpdates(params?: object, callback?: Callback): EventEmitter
+        uuids(num: number, callback: Function): Request
+    }
+
+    interface DatabaseScope {
+        create(name: string, callback?: Callback): Request
+        get(name: string, callback?: Callback): Request
+        destroy(name: string, callback?: Callback): Request
+        list(callback?: Callback): Request
+        use(db: string): DocumentScope
+        compact(name: string, designname?: string, callback?: Callback): Request
+        replicate(source: string, target: string, options?: object, callback?: Callback): Request
+        changes(name: string, params?: object, callback?: Callback): Request
+        follow(source: string, params?: FollowUpdatesParams, callback?: Callback): any
+        followUpdates(params?: object, callback?: Callback): EventEmitter
+        updates(params?: UpdatesParams, callback?: Callback): Request
+    }
+
+    interface DocumentScope {
+        readonly config: ServerConfig
+        info(callback?: Callback): Request
+        replicate(target: string, options?: object, callback?: Callback): Request
+        compact(callback?: Callback): Request
+        changes(params?: object, callback?: Callback): Request
+        follow(params?: FollowUpdatesParams, callback?: Callback): any
+        auth(username: string, userpass: string, callback?: Function): Request
+        session(callback?: Callback): Request
+        insert(document: object, params?: string | object, callback?: Callback): Request
+        get(docname: string, params?: object, callback?: Callback): Request
+        head(docname: string, callback: Callback): Request
+        copy(src_document: object, dst_document: object, options: object, callback?: Callback): Request
+        destroy(docname: string, rev: string, callback?: Callback): Request
+        bulk(docs: BulkModifyDocsWrapper, params?: object, callback?: Callback): Request
+        list(params?: object, callback?: Callback): Request
+        fetch(docnames: BulkFetchDocsWrapper, params?: object, callback?: Callback): Request
+        fetchRevs(docnames: BulkFetchDocsWrapper, params?: object, callback?: Callback): Request
+        multipart: Multipart
+        attachment: Attachment
+        show(designname: string, showname: string, doc_id: string, params?: object, callback?: Callback): Request
+        atomic(designname: string, updatename: string, docname: string, body?: object, callback?: Callback): Request
+        updateWithHandler(designname: string, updatename: string, docname: string, body?: object, callback?: Callback): Request
+        search(designname: string, searchname: string, params?: object, callback?: Callback): Request
+        spatial(ddoc: string, viewname: string, params?: object, callback?: Callback): Request
+        view(designname: string, viewname: string, params?: object, callback?: Callback): Request
+        viewWithList(designname: string, viewname: string, listname: string, params?: object, callback?: Callback): Request
+        server: ServerScope
+    }
+
+    interface Multipart {
+        insert(doc: object, attachments: object[], params: string | object, callback?: Callback): Request
+        get(docname: string, params?: string | object, callback?: Callback): Request
+    }
+
+    interface Attachment {
+        insert(docname: string, attname: string, att: any, contenttype: string, params?: object, callback?: Callback): Request
+        get(docname: string, attname: string, params?: object, callback?: Callback): Request
+        destroy(docname: string, attname: string, params?: object, callback?: Callback): Request
     }
 
     interface ServerConfig {
@@ -22,10 +93,6 @@ declare namespace nano {
 
     interface RequestFunction {
         (options: RequestOptions, callback?: Callback): void
-    }
-
-    interface Callback {
-        (error: any, result: any): void
     }
 
     interface RequestOptions {
@@ -42,45 +109,14 @@ declare namespace nano {
         multipart: any[]
     }
 
-    interface DatabaseAccessor {
-        create(name: string, callback?: Callback): Request
-        get(name: string, callback?: Callback): Request
-        destroy(name: string, callback?: Callback): Request
-        list(callback?: Callback): Request
-        compact(name: string, designname?: string, callback?: Callback): Request
-        replicate(source: string, target: string, options?: object, callback?: Callback): Request
-        changes(source: string, params?: object, callback?: Callback): Request
-        follow(source: string, params?: FollowUpdatesParams, callback?: Callback): any
-        info(callback?: Callback): Request
+    interface UpdatesParams {
+        feed: "longpoll" | "continuous" | "eventsource"
+        timeout: number
+        heartbeat: boolean
     }
 
-    interface MultipartAccessor {
-        insert(doc: object, attachments: object[], params: string | object, callback?: Callback): Request
-        get(docname: string, params?: string | object, callback?: Callback): Request
-    }
-
-    interface AttachmentAccessor {
-        insert(docname: string, attname: string, att: any, contenttype: string, params?: object, callback?: Callback): Request
-        get(docname: string, attname: string, params?: object, callback?: Callback): Request
-        destroy(docname: string, attname: string, params?: object, callback?: Callback): Request
-    }
-
-    interface DocumentAccessor {
-        insert(document: object, params?: string | object, callback?: Callback): Request
-        destroy(docname: string, rev: string, callback?: Callback): Request
-        get(docname: string, params?: object, callback?: Callback): Request
-        head(docname: string, callback: Callback): Request
-        copy(src_document: object, dst_document: object, options: object, callback?: Callback): Request
-        bulk(docs: BulkModifyDocsWrapper, params?: object, callback?: Callback): Request
-        fetch(docnames: BulkFetchDocsWrapper, params?: object, callback?: Callback): Request
-        fetchRevs(docnames: BulkFetchDocsWrapper, params?: object, callback?: Callback): Request
-        view(designname: string, viewname: string, params?: object, callback?: Callback): Request
-        viewWithList(designname: string, viewname: string, listname: string, params?: object, callback?: Callback): Request
-        show(designname: string, showname: string, doc_id: string, params?: object, callback?: Callback): Request
-        atomic(designname: string, updatename: string, docname: string, body?: object, callback?: Callback): Request
-        search(designname: string, searchname: string, params?: object, callback?: Callback): Request
-        multipart: MultipartAccessor
-        attachment: AttachmentAccessor
+    interface Callback {
+        (error: any, result: any): void
     }
 
     interface BulkModifyDocsWrapper {
@@ -89,17 +125,6 @@ declare namespace nano {
 
     interface BulkFetchDocsWrapper {
         keys: string[]
-    }
-
-    interface Config {
-        url: string
-        requestDefaults?: CoreOptions
-        log?(id: string, args: any): void
-        parseUrl?: boolean
-    }
-
-    interface FollowUpdatesParamsFilterFunction {
-        (doc: any, req: any): boolean
     }
 
     interface FollowUpdatesParams {
@@ -117,18 +142,8 @@ declare namespace nano {
         response_grace_time?: number
     }
 
-    interface Client {
-        db: DatabaseAccessor
-        use(db: string): DocumentAccessor
-        auth(username: string, userpass: string, callback?: Function): Request
-        session(callback?: Callback): Request
-        uuids(num: number, callback: Function): Request
-        request: RequestFunction
-        relax: RequestFunction
-        dinosaur: RequestFunction
-        readonly config: ServerConfig
-        updates(params?: UpdatesParams, callback?: Callback): Request
-        followUpdates(params?: object, callback?: Callback): EventEmitter
+    interface FollowUpdatesParamsFilterFunction {
+        (doc: any, req: any): boolean
     }
 }
 
