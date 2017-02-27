62546c0eca527708f26e8ea46b2166b24accc0e9
Alan <alan.agius4@gmail.com>
2017-02-27 16:52:43 +0100

    remove redundent namespace

--- stylelint/index.d.ts
+++ stylelint/index.d.ts
@@ -3,39 +3,33 @@
 // Definitions by: Alan Agius <https://github.com/alan-agius4/>
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
 
-export declare namespace stylelint {
-
-    interface Options {
-        code?: string;
-        codeFilename?: string;
-        config?: JSON;
-        configBasedir?: string;
-        configFile?: string;
-        configOverrides?: JSON;
-        files?: string | string[];
-        formatter?: "json" | "string" | "verbose";
-        ignoreDisables?: boolean;
-        reportNeedlessDisables?: boolean;
-        ignorePath?: boolean;
-        syntax?: "scss" | "less" | "sugarss";
-        customSyntax?: string;
-    }
-
-    interface PromiseResult {
-        errored: boolean;
-        output: string;
-        postcssResults: any[];
-        results: LintResult[];
-    }
-
-    interface LintResult {
-        source: string;
-        ignored: boolean;
-        warnings: string[];
-    }
-
-    function lint(options?: Options): Promise<PromiseResult>;
-
+export interface Options {
+    code?: string;
+    codeFilename?: string;
+    config?: JSON;
+    configBasedir?: string;
+    configFile?: string;
+    configOverrides?: JSON;
+    files?: string | string[];
+    formatter?: "json" | "string" | "verbose";
+    ignoreDisables?: boolean;
+    reportNeedlessDisables?: boolean;
+    ignorePath?: boolean;
+    syntax?: "scss" | "less" | "sugarss";
+    customSyntax?: string;
 }
 
-export as namespace stylelint;
+export interface PromiseResult {
+    errored: boolean;
+    output: string;
+    postcssResults: any[];
+    results: LintResult[];
+}
+
+export interface LintResult {
+    source: string;
+    ignored: boolean;
+    warnings: string[];
+}
+
+export function lint(options?: Options): Promise<PromiseResult>;
