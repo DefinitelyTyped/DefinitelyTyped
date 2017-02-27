495c6c3f22584917ed76ad72b85d5cde51a48b89
Alan <alan.agius4@gmail.com>
2017-02-27 16:43:57 +0100

    feat(stylelint): add typings for `stylelint`

---
+++ stylelint/index.d.ts
@@ -0,0 +1,42 @@
+// Type definitions for Stylelint 7.9.0
+// Project: https://github.com/stylelint/stylelint
+// Definitions by: Alan Agius <https://github.com/alan-agius4/>
+// Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
+
+declare namespace stylelint {
+
+    interface Options {
+        code?: string;
+        codeFilename?: string;
+        config?: JSON;
+        configBasedir?: string;
+        configFile?: string;
+        configOverrides?: JSON;
+        files?: string | string[];
+        formatter?: "json" | "string" | "verbose";
+        ignoreDisables?: boolean;
+        reportNeedlessDisables?: boolean;
+        ignorePath?: boolean;
+        syntax?: "scss" | "less" | "sugarss";
+        customSyntax?: string;
+    }
+
+    interface PromiseResult {
+        errored: boolean;
+        output: string;
+        postcssResults: any[];
+        results: LintResult[];
+    }
+
+    interface LintResult {
+        source: string;
+        ignored: boolean;
+        warnings: string[];
+    }
+
+    function lint(options?: Options): Promise<PromiseResult>;
+
+}
+
+export = stylelint;
+export as namespace stylelint;
---
+++ stylelint/stylelint-tests.ts
@@ -0,0 +1,18 @@
+import { Options, lint, LintResult, PromiseResult } from "stylelint";
+
+const options: Options = {
+    code: "div { color: red }",
+    files: ["**/**.scss"],
+    formatter: "json",
+    ignoreDisables: true,
+    reportNeedlessDisables: true,
+    ignorePath: true,
+    syntax: "scss"
+}
+
+lint(options).then((x: PromiseResult) => {
+    const err: boolean = x.errored;
+    const output: string = x.output;
+    const postcssResults: any[] = x.postcssResults;
+    const results: LintResult[] = x.results;
+});
