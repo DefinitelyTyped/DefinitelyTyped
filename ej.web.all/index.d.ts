b277e917f8598fab088622ebd6b8a120fb05c348
Syncfusion-JavaScript <packages@syncfusion.com>
2017-02-28 14:43:43 +0530

    Lint errors fixed.

--- ej.web.all/index.d.ts
+++ ej.web.all/index.d.ts
@@ -1,4 +1,4 @@
-// Type definitions for ej.web.all 15.1.33
+// Type definitions for ej.web.all 15.1
 // Project: http://help.syncfusion.com/js/typescript
 // Definitions by: Syncfusion <https://github.com/syncfusion/>
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
