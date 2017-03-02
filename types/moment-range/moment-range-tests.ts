cc9c1baac9c49eba9ea8e58e840a3efa8169aa09
Juan Francisco Adame Lorite <juanfrancisco.adame@nagra.com>
2017-03-03 00:36:00 +0100

    Moment-range library update to new API interface 3.0.3

--- types/moment-range/moment-range-tests.ts
+++ types/moment-range/moment-range-tests.ts
@@ -7,41 +7,60 @@ var range4: moment.Range = moment.range("2015-01-17T09:50:04+00:00/2015-04-17T08
 
 var date: moment.Moment = moment('2012-05- 15');
 
-var res1: boolean = range.contains(date);
-var res2: boolean = range.contains(date, false);
-var res3: boolean = range.contains(date, true);
+var res1: boolean = range.adjacent(range2);
 
-var res4: boolean = date.within(range);
+var it0: Iterable<moment.Range> = range.by('days');
+var it1: Iterable<moment.Range> = range.by('months', {exclusive: true});
+var it2: Iterable<moment.Range> = range.by('years', {exclusive: false, step: 2});
 
-var res5: boolean = range.overlaps(range2);
+var it3: Iterable<moment.Range> = range.byRange(range);
+var it4: Iterable<moment.Range> = range.byRange(range, {exclusive: true});
+var it5: Iterable<moment.Range> = range.byRange(range, {exclusive: false, step: 2});
 
-var res6: moment.Range = range.intersect(range2);
+var res2: moment.Moment = range.center();
 
-var res7: moment.Range = range.add(range2);
+var res3: moment.Range = range.clone();
 
-var res8: Array<moment.Range> = range.subtract(range2);
+var res4: boolean = range.contains(range);
+var res5: boolean = range.contains(range, {exclusive: true});
+var res6: boolean = range.contains(range, {exclusive: false});
 
-range.by('days', (moment: moment.Moment) => {  });
-range.by(range2, (moment: moment.Moment) => {  });
-range.by('d', (moment: moment.Moment) => {  }, true);
-
-var res9: boolean = range.isSame(range2);
-
-var res10: number = range.diff('months');
+var res10: number = range.diff('months', true);
 var res11: number = range.diff('days');
 var res12: number = range.diff();
 
-var res13: Date = range.toDate();
-var res14: string = range.toString();
+var res13: number = range.duration('months', true);
+var res14: number = range.duration('days');
+var res15: number = range.duration();
 
-var res15: number = range.valueOf();
+var res16: moment.Range = range.intersect(range2);
 
-var res16: number = range.center();
+var res17: boolean = range.isSame(range2);
 
-var res17: moment.Range = range.clone();
+var res18: boolean = range.isEqual(range2);
 
-var res18: moment.Moment = range.start;
-var res19: moment.Moment = range.end;
+var res19: boolean = date.within(range);
 
-var res20: moment.Moment[] = range.toArray('days');
-var res22: moment.Moment[] = range.toArray('days', true);
+var res20: boolean = range.overlaps(range2);
+var res21: boolean = range.overlaps(range2, {adjacent: true});
+var res22: boolean = range.overlaps(range2, {adjacent: false});
+
+var it6: Iterable<moment.Range> = range.reverseBy('days');
+var it7: Iterable<moment.Range> = range.reverseBy('months', {exclusive: true});
+var it8: Iterable<moment.Range> = range.reverseBy('years', {exclusive: false, step: 2});
+
+var it9: Iterable<moment.Range> = range.reverseByRange(range);
+var it10: Iterable<moment.Range> = range.reverseByRange(range, {exclusive: true});
+var it11: Iterable<moment.Range> = range.reverseByRange(range, {exclusive: false, step: 2});
+
+var res23: moment.Range = range.add(range2);
+
+var res24: Array<moment.Range> = range.subtract(range2);
+
+var res25: Date[] = range.toDate();
+var res26: string = range.toString();
+
+var res27: number = range.valueOf();
+
+var res28: moment.Moment = range.start;
+var res29: moment.Moment = range.end;
