9d4c31f2ba34a2ca6552fff01b3274385880ad1c
Birk Skyum <birkskyum@gmail.com>
2017-03-07 13:29:27 +0100

    Format header such that it passes npm test

--- meteor/index.d.ts
+++ meteor/index.d.ts
@@ -1,11 +1,7 @@
 // Type definitions for Meteor 1.4
 // Project: http://www.meteor.com/
-// Definitions by:
-// Alex Borodach <https://github.com/barbatus>
-// Dave Allen <https://github.com/fullflavedave>
-// Olivier Refalo <https://github.com/orefalo>
-// Daniel Neveux <https://github.com/dagatsoin>
-// Birk Skyum <https://github.com/birkskyum>
+// Definitions by: Alex Borodach <https://github.com/barbatus>, Dave Allen <https://github.com/fullflavedave>, Olivier Refalo <https://github.com/orefalo>, Daniel Neveux <https://github.com/dagatsoin>, Birk Skyum <https://github.com/birkskyum>
+// Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
 
 interface URLS {
     resetPassword: (token: string) => string;
