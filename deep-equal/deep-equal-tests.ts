f102b10f38a9dc9fd96fd56a982a5ceb87fbbaf7
Jay Anslow <jay@anslow.me.uk>
2017-03-07 11:11:45 +0000

    Enable linting and fix errors

--- deep-equal/deep-equal-tests.ts
+++ deep-equal/deep-equal-tests.ts
@@ -1,11 +1,11 @@
 
 import deepEqual = require("deep-equal");
 
-let isDeepEqual1: boolean = deepEqual({}, {});
-let isDeepEqual2: boolean = deepEqual({}, {}, { strict: true });
-let isDeepEqual3: boolean = deepEqual({}, {}, { strict: false });
-let isDeepEqual4: boolean = deepEqual(undefined, undefined);
-let isDeepEqual5: boolean = deepEqual(3, false);
-let isDeepEqual6: boolean = deepEqual("a-string", null);
+const isDeepEqual1: boolean = deepEqual({}, {});
+const isDeepEqual2: boolean = deepEqual({}, {}, { strict: true });
+const isDeepEqual3: boolean = deepEqual({}, {}, { strict: false });
+const isDeepEqual4: boolean = deepEqual(undefined, undefined);
+const isDeepEqual5: boolean = deepEqual(3, false);
+const isDeepEqual6: boolean = deepEqual("a-string", null);
 
 console.log(isDeepEqual1, isDeepEqual2, isDeepEqual3, isDeepEqual4, isDeepEqual5, isDeepEqual6);
