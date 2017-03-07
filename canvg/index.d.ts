df060ebe185e695c05a70ab9a712343d745e146b
Pedro Pereira <pedro.inacio@aon.ie>
2017-03-07 13:59:43 +0000

    Fixed the tests

--- canvg/index.d.ts
+++ canvg/index.d.ts
@@ -1,3 +1,7 @@
+// Type definitions for canvg-browser v1.0.0
+// Project: https://github.com/ricardomatias/canvg-browser
+// Definitions by: Pedro Pereira <https://github.com/oizie>
+// Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
 
 declare module "canvg" {
   let canvg: Function;
