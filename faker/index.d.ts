c105965b68d0639a6360c5a6bb3b7c98c79b2bee
Ben Swartz <ben.swartz@justin.tv>
2017-03-08 11:28:16 -0800

    Change Header Comment

--- faker/index.d.ts
+++ faker/index.d.ts
@@ -1,4 +1,4 @@
-// Based off type definitions for faker v4.1.0
+// Type definitions for faker v4.1.0
 // Project: http://marak.com/faker.js/
 // Definitions by: Ben Swartz <https://github.com/bensw/>, Bas Pennings <https://github.com/basp/>, Yuki Kokubun <https://github.com/Kuniwak>
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
