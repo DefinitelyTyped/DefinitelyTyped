047aac08dd96d77580539251c58574b417b0fc91
Birk Skyum <birkskyum@gmail.com>
2017-03-09 01:48:50 +0100

    Add files to tsconfig.json

--- meteor/index.d.ts
+++ meteor/index.d.ts
@@ -2,22 +2,3 @@
 // Project: http://www.meteor.com/
 // Definitions by: Alex Borodach <https://github.com/barbatus>, Dave Allen <https://github.com/fullflavedave>, Olivier Refalo <https://github.com/orefalo>, Daniel Neveux <https://github.com/dagatsoin>, Birk Skyum <https://github.com/birkskyum>
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
-
-import * as AccountsBasePackage from 'meteor/accounts-base'
-import * as BlazePackage from 'meteor/blaze'
-import * as BrowserPolicyCommonPackage from 'meteor/browser-policy-common'
-import * as CheckPackage from 'meteor/check'
-import * as DDPPackage from 'meteor/ddp'
-import * as DDPRateLimiterPackage from 'meteor/ddp-rate-limiter'
-import * as EJSONPackage from 'meteor/ejson'
-import * as EmailPackage from 'meteor/email'
-import * as HTTPPackage from 'meteor/http'
-import * as MeteorPackage from 'meteor/meteor'
-import * as MongoPackage from 'meteor/mongo'
-import * as RandomPackage from 'meteor/random'
-import * as ReactiveVarPackage from 'meteor/reactive-var'
-import * as SessionPackage from 'meteor/session'
-import * as TemplatingPackage from 'meteor/templating'
-import * as TinyTestPackage from 'meteor/tiny-test'
-import * as ToolsPackage from 'meteor/tools'
-import * as TrackerPackage from 'meteor/tracker'
