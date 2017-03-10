9e069e784e7a5bc12f1abacd71b319a9f02f1c8b
Anton Kandybo <dublicator@hotmail.com>
2017-03-10 10:13:16 +0200

    Using require syntax in tests

--- compression-webpack-plugin/compression-webpack-plugin-tests.ts
+++ compression-webpack-plugin/compression-webpack-plugin-tests.ts
@@ -1,5 +1,5 @@
-import { Configuration } from 'webpack'
-import * as CompressionPlugin from 'compression-webpack-plugin'
+import { Configuration } from 'webpack';
+import CompressionPlugin = require('compression-webpack-plugin');
 
 const c: Configuration = {
     plugins: [
