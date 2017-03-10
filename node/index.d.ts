d1ddb075193d61026e541fb7bf2aa47c659e2fa3
Andy <anhans@microsoft.com>
2017-03-09 21:25:49 -0800

    node: Set "lib" to "es5" (#15093)

--- node/index.d.ts
+++ node/index.d.ts
@@ -40,6 +40,17 @@ interface WeakMapConstructor { }
 interface SetConstructor { }
 interface WeakSetConstructor { }
 
+// Forward-declare needed types from lib.es2015.d.ts (in case users are using `--lib es5`)
+interface Iterable<T> {}
+interface Iterator<T> {
+    next(value?: any): IteratorResult<T>;
+}
+interface IteratorResult<T> {}
+interface SymbolConstructor {
+    readonly iterator: symbol;
+}
+declare var Symbol: SymbolConstructor;
+
 /************************************************
 *                                               *
 *                   GLOBAL                      *
