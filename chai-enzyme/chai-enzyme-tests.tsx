1c2f3aec5d538a64d980bd94b71913467ad4dde8
Andy <anhans@microsoft.com>
2017-03-10 07:23:35 -0800

    Fix chai-enzyme tests (#15106)

--- chai-enzyme/chai-enzyme-tests.tsx
+++ chai-enzyme/chai-enzyme-tests.tsx
@@ -34,7 +34,7 @@ expect(wrapper).to.have.ref("test");
 expect(wrapper).to.be.selected();
 expect(wrapper).to.have.tagName("div");
 expect(wrapper).to.have.text("");
-expect(wrapper).to.have.Type(Test);
+expect(wrapper).to.have.type(Test);
 expect(wrapper).to.have.value("test");
 expect(wrapper).to.have.attr("test", "test");
 expect(wrapper).to.have.data("test", "Test");
