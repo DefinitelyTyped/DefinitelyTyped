b97ac0824d91a103285aa963d52c4b5984851c54
kikedB <edelbosque@e-sant.com>
2017-03-11 13:05:31 -0600

    fixed rowRenderer def to pass style and key (#14565)

--- react-virtualized/index.d.ts
+++ react-virtualized/index.d.ts
@@ -275,7 +275,130 @@ interface ISortDirection {
      */
     ASC: 'ASC',
 
-    /**
+    interface VirtualScrollProps {
+        className?: string;
+        autoHeight?: boolean;
+        estimatedRowSize?: number;
+        height: number;
+        noRowsRenderer?: Function;
+        onRowsRendered?: (info: { overscanStartIndex: number, overscanStopIndex: number, startIndex: number, stopIndex: number }) => void;
+        onScroll?: (info: { clientHeight: number, scrollHeight: number, scrollTop: number }) => void;
+        overscanRowCount?: number;
+        rowHeight: number | ((info: { index: number }) => number);
+        rowRenderer: (info: { index: number, isScrolling?: boolean, key: any, style: string }) => React.ReactNode;
+        rowClassName?: string | ((info: { index: number }) => string);
+        rowCount: number;
+        rowStyle?: React.CSSProperties | ((info: { index: number }) => React.CSSProperties);
+        scrollToAlignment?: string;
+        scrollToIndex?: number;
+        scrollTop?: number;
+        style?: React.CSSProperties;
+        tabIndex?: number;
+        width: number;
+    }
+    export class VirtualScroll extends React.Component<VirtualScrollProps, {}> { }
+
+    type CollectionProps = any;
+    export class Collection extends React.Component<CollectionProps, {}> { }
+
+    type FlexTableProps = any;
+    export class FlexTable extends React.Component<FlexTableProps, {}> { }
+
+    type FlexColumnProps = any;
+    export class FlexColumn extends React.Component<FlexColumnProps, {}> { }
+
+    type SortDirectionProps = any;
+    export class SortDirection extends React.Component<SortDirectionProps, {}> { }
+
+    type GridProps = any;
+    export class Grid extends React.Component<GridProps, {}> { }
+
+    interface ListProps {
+        className?: string;
+        autoHeight?: boolean;
+        estimatedRowSize?: number;
+        height: number;
+        noRowsRenderer?: Function;
+        onRowsRendered?: (info: { overscanStartIndex: number, overscanStopIndex: number, startIndex: number, stopIndex: number }) => void;
+        onScroll?: (info: { clientHeight: number, scrollHeight: number, scrollTop: number }) => void;
+        overscanRowCount?: number;
+        rowHeight: number | ((info: { index: number }) => number);
+        rowRenderer: (info: { index: number, isScrolling?: boolean, key: any, style: string }) => React.ReactNode;
+        rowCount: number;
+        scrollToAlignment?: string;
+        scrollToIndex?: number;
+        scrollTop?: number;
+        style?: React.CSSProperties;
+        tabIndex?: number;
+        width: number;
+    }
+    export class List extends React.Component<ListProps, {}> { }
+
+    // https://github.com/bvaughn/react-virtualized/blob/master/docs/Column.md
+    interface ColumnProps {
+    	cellDataGetter?: (info: { columnData: any, dataKey: string, rowData: any }) => any;
+	cellRenderer?: (info: { cellData: any, columnData: any, dataKey: string, isScrolling: boolean, rowData: any, rowIndex: number }) => any;
+	className?: string;
+	columnData?: any;
+	dataKey: any;
+	disableSort?: boolean;
+	flexGrow?: number;
+	flexShrink?: number;
+	headerClassName?: string;
+	headerRenderer?: (info: { columnData: any, dataKey: string, disableSort: boolean, label: string, sortBy: string, sortDirection: string }) => any;
+	label?: string;
+	maxWidth?: number;
+	minWidth?: number;
+	style?: React.CSSProperties;
+	width: number;
+    }
+
+    export class Column extends React.Component<ColumnProps, {}> { }
+
+    // ref: https://github.com/bvaughn/react-virtualized/blob/master/docs/Table.md
+    interface TableProps {
+        autoHeight?: boolean;
+        children?: React.ReactNode;
+        className?: string;
+        disableHeader?: boolean;
+        estimatedRowSize?: number;
+        gridClassName?: string;
+        gridStyle?: any;
+        headerClassName?: string;
+        headerHeight: number;
+        headerStyle?: any;
+        height: number;
+        id?: string;
+        noRowsRender?: () => void;
+        onHeaderClick?: (dataKey: string, columnData: any) => void;
+        onRowClick?: (info: { index: number }) => void;
+        onRowDoubleClick?: (info: { index: number }) => void;
+        onRowMouseOut?: (info: { index: number }) => void;
+        onRowMouseOver?: (info: { index: number }) => void;
+        onRowsRendered?: (info: { index: number }) => void;
+        overscanRowCount?: number;
+        onScroll?: (info: { clientHeight: number, scrollHeight: number, scrollTop: number }) => void;
+        rowClassName?: string | ((info: { index: number }) => string);
+        rowCount: number;
+        rowGetter?: (info: { index: number }) => any;
+        rowHeight: number | ((info: { index: number }) => number);
+        rowRenderer?: (info: { index: number, isScrolling?: boolean, key: any, style: string }) => React.ReactNode;
+        rowStyle?: React.CSSProperties | ((info: { index: number }) => React.CSSProperties);
+        scrollToAlignment?: string;
+        scrollToIndex?: number;
+        scrollTop?: number;
+        sort?: (info: { sortBy: string, sortDirection: 'ASC' | 'DESC' }) => void;
+        sortBy?: string;
+        sortDirection?: 'ASC' | 'DESC';
+        style?: React.CSSProperties;
+        tabIndex?: number;
+        width: number;
+    }
+
+    export class Table extends React.Component<TableProps, {}> { }
+
+    /*
+     * Higher-Order Components
      * Sort items in descending order.
      * This means arranging from the highest value to the lowest (e.g. z-a, 9-0).
      */
