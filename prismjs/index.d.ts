6b90165d6011d9f5149c294a0b5d6178c9b6a3b2
Andre Wiggins <andrewiggins@live.com>
2017-03-10 17:40:45 -0800

    Rewrap long comments

--- prismjs/index.d.ts
+++ prismjs/index.d.ts
@@ -6,8 +6,7 @@
 export as namespace Prism;
 
 /**
- * @deprecated
- * Use the Prism namespace directly directly
+ * @deprecated Use the Prism namespace directly directly
  */
 export as namespace PrismJS;
 
@@ -17,10 +16,13 @@ export const plugins: any;
 export const hooks: Hooks;
 
 /**
- * This is the most high-level function in Prism’s API. It fetches all the elements that have a .language-xxxx class and then calls Prism.highlightElement() on each one of them.
+ * This is the most high-level function in Prism’s API. It fetches all the elements that have a .language-xxxx class and
+ * then calls Prism.highlightElement() on each one of them.
  *
- * @param  {boolean} async - Whether to use Web Workers to improve performance and avoid blocking the UI when highlighting very large chunks of code. False by default.
- * @param  {(element:Element)=>void} callback? - An optional callback to be invoked after the highlighting is done. Mostly useful when async is true, since in that case, the highlighting is done asynchronously.
+ * @param  {boolean} async - Whether to use Web Workers to improve performance and avoid blocking the UI when
+ * highlighting very large chunks of code. False by default.
+ * @param  {(element:Element)=>void} callback? - An optional callback to be invoked after the highlighting is done.
+ * Mostly useful when async is true, since in that case, the highlighting is done asynchronously.
  * @returns void
  */
 export function highlightAll(async: boolean, callback?: (element: Element) => void): void;
@@ -28,31 +30,38 @@ export function highlightAll(async: boolean, callback?: (element: Element) => vo
 /**
  * Highlights the code inside a single element.
  *
- * @param  {Element} element - The element containing the code. It must have a class of language-xxxx to be processed, where xxxx is a valid language identifier.
- * @param  {boolean} async - Whether to use Web Workers to improve performance and avoid blocking the UI when highlighting very large chunks of code. False by default.
- * @param  {(element:Element)=>void} callback? - An optional callback to be invoked after the highlighting is done. Mostly useful when async is true, since in that case, the highlighting is done asynchronously.
+ * @param  {Element} element - The element containing the code. It must have a class of language-xxxx to be processed,
+ * where xxxx is a valid language identifier.
+ * @param  {boolean} async - Whether to use Web Workers to improve performance and avoid blocking the UI when
+ * highlighting very large chunks of code. False by default.
+ * @param  {(element:Element)=>void} callback? - An optional callback to be invoked after the highlighting is done.
+ * Mostly useful when async is true, since in that case, the highlighting is done asynchronously.
  * @returns void
  */
 export function highlightElement(element: Element, async: boolean, callback?: (element: Element) => void): void;
 
 
 /**
- * Low-level function, only use if you know what you’re doing. It accepts a string of text as input and the language definitions to use, and returns a string with the HTML produced.
+ * Low-level function, only use if you know what you’re doing. It accepts a string of text as input and the language
+ * definitions to use, and returns a string with the HTML produced.
  *
  * @param  {string} text - A string with the code to be highlighted.
- * @param  {LanguageDefinition} grammer - An object containing the tokens to use. Usually a language definition like Prism.languages.markup
+ * @param  {LanguageDefinition} grammer - An object containing the tokens to use. Usually a language definition like
+ * Prism.languages.markup
  * @param  {LanguageDefinition} language
  * @returns string - The highlighted HTML
  */
 export function highlight(text: string, grammer: LanguageDefinition, language?: LanguageDefinition): string;
 
 /**
- * This is the heart of Prism, and the most low-level function you can use. It accepts a string of text as input and the language definitions to use,
- * and returns an array with the tokenized code. When the language definition includes nested tokens, the function is called recursively on each of
- * these tokens. This method could be useful in other contexts as well, as a very crude parser.
+ * This is the heart of Prism, and the most low-level function you can use. It accepts a string of text as input and the
+ * language definitions to use, and returns an array with the tokenized code. When the language definition includes
+ * nested tokens, the function is called recursively on each of these tokens. This method could be useful in other
+ * contexts as well, as a very crude parser.
  *
  * @param  {string} text - A string with the code to be highlighted.
- * @param  {LanguageDefinition} grammar - An object containing the tokens to use. Usually a language definition like Prism.languages.markup
+ * @param  {LanguageDefinition} grammar - An object containing the tokens to use. Usually a language definition like
+ * Prism.languages.markup
  * @returns Array - An array of strings, tokens (class Prism.Token) and other arrays.
  */
 export function tokenize(text: string, grammar: LanguageDefinition): Array<Token | string>;
