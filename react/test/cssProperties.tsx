829d8ab6a3065a2bc6b43342bf18175583d5600e
Matt <begincalendar@users.noreply.github.com>
2017-03-11 11:09:13 +1100

    [react] Added a "boxShadow" optional property to the "CSSProperties" interface (#14726)
    * [react] Added a "boxShadow" optional property to the "CSSProperties" interface.
    MDN: https://developer.mozilla.org/en-US/docs/Web/CSS/box-shadow
    
    * Added a test for CSSProperties.boxShadow

--- react/test/cssProperties.tsx
+++ react/test/cssProperties.tsx
@@ -24,6 +24,8 @@ const fontWeightStyleTest = <div style={fontWeightStyle} />;
 const justifyContentStyle: React.CSSProperties = { justifyContent: 'space-evenly' };
 const justifyContentStyleTest = <div style={justifyContentStyle} />;
 
+const boxShadowStyle: React.CSSProperties = { boxShadow: '2px 2px 2px 1px rgba(0, 0, 0, 0.2)' };
+const boxShadowStyleTest = <div style={boxShadowStyle} />;
 
 // SVG specific style attribute declarations
 
