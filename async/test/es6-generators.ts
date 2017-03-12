44063a39a648f6131aae9fbb66b063474d091c18
Wayne Dela Cruz <ddwwcruz@gmail.com>
2017-03-12 18:23:07 +0800

    Create tests for filters, selects, and rejects
    * filter
    * filterSeries
    * filterLimit
    * select
    * selectSeries
    * selectLimit
    * reject
    * rejectSeries
    * rejectLimit

--- async/test/es6-generators.ts
+++ async/test/es6-generators.ts
@@ -3,6 +3,19 @@ function* funcCollection<T>(): IterableIterator<T> { }
 function funcMapIterator<T, E>(value: T, callback: AsyncResultCallback<T, E>) { }
 function funcMapComplete<T, E>(error: E, results: T[]) { }
 
+function funcCbErrBoolean<T>(v: T, cb: (err: Error, res: boolean) => void) { }
+
 async.map(funcCollection(), funcMapIterator, funcMapComplete)
 async.mapSeries(funcCollection(), funcMapIterator, funcMapComplete)
 async.mapLimit(funcCollection(), 2, funcMapIterator, funcMapComplete)
+
+async.filter(funcCollection(), funcCbErrBoolean, function (err: Error, results: any[]) { })
+async.filterSeries(funcCollection(), funcCbErrBoolean, function (err: Error, results: any[]) { })
+async.filterLimit(funcCollection(), 2, funcCbErrBoolean, function (err: Error, results: any[]) { })
+async.select(funcCollection(), funcCbErrBoolean, function (err: Error, results: any[]) { })
+async.selectSeries(funcCollection(), funcCbErrBoolean, function (err: Error, results: any[]) { })
+async.selectLimit(funcCollection(), 2, funcCbErrBoolean, function (err: Error, results: any[]) { })
+
+async.reject(funcCollection(), funcCbErrBoolean, function (err: Error, results: any[]) { })
+async.rejectSeries(funcCollection(), funcCbErrBoolean, function (err: Error, results: any[]) { })
+async.rejectLimit(funcCollection(), 2, funcCbErrBoolean, function (err: Error, results: any[]) { })
