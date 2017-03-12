a36360d8adb31eddb8e5a1304b4bf50cad814f62
Wayne Dela Cruz <ddwwcruz@gmail.com>
2017-03-12 20:40:35 +0800

    Rename funcCollection to collectionGenerator
    This makes the purpose of the function more easily understood

--- async/test/es6-generators.ts
+++ async/test/es6-generators.ts
@@ -1,4 +1,4 @@
-function* funcCollection<T>(): IterableIterator<T> { }
+function* collectionGenerator<T>(): IterableIterator<T> { }
 
 function funcMapIterator<T, E>(value: T, callback: AsyncResultCallback<T, E>) { }
 function funcMapComplete<T, E>(error: E, results: T[]) { }
@@ -10,45 +10,45 @@ function eachOfIterator<T, K, E>(item: T, key: K, callback: (err: Error) => void
 
 function concatIterator<T, R, E>(item: T, callback: (err: E, res: R[]) => void) { }
 
-async.map(funcCollection(), funcMapIterator, funcMapComplete)
-async.mapSeries(funcCollection(), funcMapIterator, funcMapComplete)
-async.mapLimit(funcCollection(), 2, funcMapIterator, funcMapComplete)
+async.map(collectionGenerator(), funcMapIterator, funcMapComplete)
+async.mapSeries(collectionGenerator(), funcMapIterator, funcMapComplete)
+async.mapLimit(collectionGenerator(), 2, funcMapIterator, funcMapComplete)
 
-async.filter(funcCollection(), booleanIterator, function (err: Error, results: any[]) { })
-async.filterSeries(funcCollection(), booleanIterator, function (err: Error, results: any[]) { })
-async.filterLimit(funcCollection(), 2, booleanIterator, function (err: Error, results: any[]) { })
-async.select(funcCollection(), booleanIterator, function (err: Error, results: any[]) { })
-async.selectSeries(funcCollection(), booleanIterator, function (err: Error, results: any[]) { })
-async.selectLimit(funcCollection(), 2, booleanIterator, function (err: Error, results: any[]) { })
+async.filter(collectionGenerator(), booleanIterator, function (err: Error, results: any[]) { })
+async.filterSeries(collectionGenerator(), booleanIterator, function (err: Error, results: any[]) { })
+async.filterLimit(collectionGenerator(), 2, booleanIterator, function (err: Error, results: any[]) { })
+async.select(collectionGenerator(), booleanIterator, function (err: Error, results: any[]) { })
+async.selectSeries(collectionGenerator(), booleanIterator, function (err: Error, results: any[]) { })
+async.selectLimit(collectionGenerator(), 2, booleanIterator, function (err: Error, results: any[]) { })
 
-async.reject(funcCollection(), booleanIterator, function (err: Error, results: any[]) { })
-async.rejectSeries(funcCollection(), booleanIterator, function (err: Error, results: any[]) { })
-async.rejectLimit(funcCollection(), 2, booleanIterator, function (err: Error, results: any[]) { })
+async.reject(collectionGenerator(), booleanIterator, function (err: Error, results: any[]) { })
+async.rejectSeries(collectionGenerator(), booleanIterator, function (err: Error, results: any[]) { })
+async.rejectLimit(collectionGenerator(), 2, booleanIterator, function (err: Error, results: any[]) { })
 
-async.each(funcCollection(), eachIterator, function (err: Error) { })
-async.eachLimit(funcCollection(), 2, eachIterator, function (err: Error) { })
-async.eachSeries(funcCollection(), eachIterator, function (err: Error) { })
-async.eachOf(funcCollection(), eachOfIterator, function (err: Error) { })
-async.eachOfLimit(funcCollection(), 2, eachOfIterator, function (err: Error) { })
-async.eachOfSeries(funcCollection(), eachOfIterator, function (err: Error) { })
-async.forEach(funcCollection(), eachIterator, function (err: Error) { })
-async.forEachLimit(funcCollection(), 2, eachIterator, function (err: Error) { })
-async.forEachSeries(funcCollection(), eachIterator, function (err: Error) { })
-async.forEachOf(funcCollection(), eachOfIterator, function (err: Error) { })
-async.forEachOfLimit(funcCollection(), 2, eachOfIterator, function (err: Error) { })
-async.forEachOfSeries(funcCollection(), eachOfIterator, function (err: Error) { })
+async.each(collectionGenerator(), eachIterator, function (err: Error) { })
+async.eachLimit(collectionGenerator(), 2, eachIterator, function (err: Error) { })
+async.eachSeries(collectionGenerator(), eachIterator, function (err: Error) { })
+async.eachOf(collectionGenerator(), eachOfIterator, function (err: Error) { })
+async.eachOfLimit(collectionGenerator(), 2, eachOfIterator, function (err: Error) { })
+async.eachOfSeries(collectionGenerator(), eachOfIterator, function (err: Error) { })
+async.forEach(collectionGenerator(), eachIterator, function (err: Error) { })
+async.forEachLimit(collectionGenerator(), 2, eachIterator, function (err: Error) { })
+async.forEachSeries(collectionGenerator(), eachIterator, function (err: Error) { })
+async.forEachOf(collectionGenerator(), eachOfIterator, function (err: Error) { })
+async.forEachOfLimit(collectionGenerator(), 2, eachOfIterator, function (err: Error) { })
+async.forEachOfSeries(collectionGenerator(), eachOfIterator, function (err: Error) { })
 
-async.every(funcCollection(), booleanIterator, function (err: Error, res: boolean) { })
-async.everyLimit(funcCollection(), 2, booleanIterator, function (err: Error, res: boolean) { })
-async.everySeries(funcCollection(), booleanIterator, function (err: Error, res: boolean) { })
+async.every(collectionGenerator(), booleanIterator, function (err: Error, res: boolean) { })
+async.everyLimit(collectionGenerator(), 2, booleanIterator, function (err: Error, res: boolean) { })
+async.everySeries(collectionGenerator(), booleanIterator, function (err: Error, res: boolean) { })
 
-async.some(funcCollection(), booleanIterator, function (err: Error, res: boolean) { })
-async.someLimit(funcCollection(), 2, booleanIterator, function (err: Error, res: boolean) { })
-async.someSeries(funcCollection(), booleanIterator, function (err: Error, res: boolean) { })
+async.some(collectionGenerator(), booleanIterator, function (err: Error, res: boolean) { })
+async.someLimit(collectionGenerator(), 2, booleanIterator, function (err: Error, res: boolean) { })
+async.someSeries(collectionGenerator(), booleanIterator, function (err: Error, res: boolean) { })
 
-async.detect(funcCollection(), booleanIterator, function (err: Error, res: boolean) { })
-async.detectLimit(funcCollection(), 2, booleanIterator, function (err: Error, res: boolean) { })
-async.detectSeries(funcCollection(), booleanIterator, function (err: Error, res: boolean) { })
+async.detect(collectionGenerator(), booleanIterator, function (err: Error, res: boolean) { })
+async.detectLimit(collectionGenerator(), 2, booleanIterator, function (err: Error, res: boolean) { })
+async.detectSeries(collectionGenerator(), booleanIterator, function (err: Error, res: boolean) { })
 
-async.concat(funcCollection(), concatIterator, (err: Error, res) => { })
-async.concatSeries(funcCollection(), concatIterator, (err: Error, res) => { })
+async.concat(collectionGenerator(), concatIterator, (err: Error, res) => { })
+async.concatSeries(collectionGenerator(), concatIterator, (err: Error, res) => { })
