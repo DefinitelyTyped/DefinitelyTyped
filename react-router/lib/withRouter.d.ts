29d3efdd949cf82da5c097a849dad266606677e6
Hajime TAKAHASHI <cc.sig@icloud.com>
2017-03-13 19:58:26 +0900

    Fix declaration order

--- react-router/lib/withRouter.d.ts
+++ react-router/lib/withRouter.d.ts
@@ -6,7 +6,7 @@ interface Options {
 
 type ComponentConstructor<P> = ComponentClass<P> | StatelessComponent<P>;
 
-declare function withRouter<P>(component: ComponentConstructor<P>, options?: Options): ComponentClass<P>;
 declare function withRouter<P, S>(component: ComponentConstructor<P> & S, options?: Options): ComponentClass<P> & S;
+declare function withRouter<P>(component: ComponentConstructor<P>, options?: Options): ComponentClass<P>;
 
 export default withRouter;
