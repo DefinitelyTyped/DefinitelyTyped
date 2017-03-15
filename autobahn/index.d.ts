364ecd49f01a8277dd278e6f72ad95dd704c9a1c
Eunchong Yu <gracie@spoqa.com>
2017-03-16 00:53:48 +0900

    Increase version of @types/autobahn

--- autobahn/index.d.ts
+++ autobahn/index.d.ts
@@ -1,4 +1,4 @@
-// Type definitions for AutobahnJS v0.9.6
+// Type definitions for AutobahnJS v0.9.7
 // Project: http://autobahn.ws/js/
 // Definitions by: Elad Zelingher <https://github.com/darkl/>, Andy Hawkins <https://github.com/a904guy/,http://a904guy.com/,http://www.bmbsqd.com>
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
