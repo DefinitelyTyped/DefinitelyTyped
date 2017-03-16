4d866889fed0a7f0c7094d974d490f37a00fae4b
ioan.coleman90 <ioan.coleman@gmail.com>
2017-03-16 14:48:53 +0000

    Updating file version number

--- mapbox-gl/index.d.ts
+++ mapbox-gl/index.d.ts
@@ -1,4 +1,4 @@
-// Type definitions for Mapbox GL JS v0.29.0
+// Type definitions for Mapbox GL JS v0.30.0
 // Project: https://github.com/mapbox/mapbox-gl-js
 // Definitions by: Dominik Bruderer <https://github.com/dobrud>
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
