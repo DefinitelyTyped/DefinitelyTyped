029e384fa0531cdfcdf74fada54ebff5a899b32f
Tanguy Krotoff <tkrotoff@gmail.com>
2017-03-16 09:52:31 +0100

    Require TypeScript 2.2 due to the use of object

--- react-router-dom/index.d.ts
+++ react-router-dom/index.d.ts
@@ -2,7 +2,7 @@
 // Project: https://github.com/ReactTraining/react-router
 // Definitions by: Tanguy Krotoff <https://github.com/tkrotoff>
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
-// TypeScript Version: 2.1
+// TypeScript Version: 2.2
 
 declare module 'react-router-dom' {
   import {
