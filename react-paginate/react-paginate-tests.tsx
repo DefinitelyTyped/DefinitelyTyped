2468af34fbd34d53a1caa6d5dfcb419621414e24
Peter Glas <pglas@anwb.nl>
2017-03-16 13:37:11 +0100

    Added typings for latest version 4.2.0

--- react-paginate/react-paginate-tests.tsx
+++ react-paginate/react-paginate-tests.tsx
@@ -2,12 +2,35 @@ import * as React from "react";
 import ReactPaginate = require("react-paginate");
 
 class Test extends React.Component<{}, {}> {
+
+    private onClick(obj: any): void {
+        //
+    }
+
     public render() {
         return (
             <ReactPaginate
-                pageNum={1}
+                pageCount={1}
                 pageRangeDisplayed={10}
                 marginPagesDisplayed={2}
+                previousLabel={'previous'}
+                nextLabel={'next'}
+                breakLabel={'break-me'}
+                breakClassName={'break-class'}
+                onPageChange={(nr: number) => this.onClick(nr)}
+                initialPage={2}
+                forcePage={3}
+                disableInitialCallback={false}
+                containerClassName={'container'}
+                pageClassName={'page-li'}
+                pageLinkClassName={'page-a'}
+                activeClassName={'active'}
+                previousClassName={'previous-li'}
+                nextClassName={'next-li'}
+                previousLinkClassName={'previous-a'}
+                nextLinkClassName={'next-a'}
+                disabledClassName={'disabled'}
+                hrefBuilder={(a: string) => this.onClick(a)}
             />
         );
     }
