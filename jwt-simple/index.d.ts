78d4a9d2962c16b9874399d26c49c97f6f79f0eb
David Findley <david.findley@ou.edu>
2017-03-16 22:48:35 -0500

    jwt-simple: Adding optional 'algorithm' param to decode()

--- jwt-simple/index.d.ts
+++ jwt-simple/index.d.ts
@@ -1,4 +1,4 @@
-// Type definitions for jwt-simple v0.5.0
+// Type definitions for jwt-simple v0.5.1
 // Project: https://github.com/hokaccha/node-jwt-simple
 // Definitions by: Ken Fukuyama <https://github.com/kenfdev>, Gael Magnan <https://github.com/GaelMagnan>
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
@@ -11,7 +11,7 @@
    * @param algorithm default is HS256
  * @api public
  */
-export function decode(token: any, key: string, noVerify?: boolean): any;
+export function decode(token: any, key: string, noVerify?: boolean, algorithm?: string): any;
 /**
  * Encode jwt
  * @param payload
