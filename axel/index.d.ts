10d20cf894ca3a76722d2b1f927730cfe06ba9eb
Ruslan Molodyko <ruslan.molodyko@gmail.com>
2017-03-19 19:19:13 +0200

    Added returnet type of module

--- axel/index.d.ts
+++ axel/index.d.ts
@@ -3,30 +3,35 @@
 // Definitions by: Ruslan Molodyko <https://github.com/ruslan-molodyko>
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
 
+declare class Axel {
+    brush: string;
+    rows: number;
+    cols: number;
+    cursor: cursorInterface;
+
+    lerp(p1: number, p2: number, m: number): number;
+    circ(x: number, y: number, m: number): number;
+    goto(x: number, y: number): void;
+    scrub(x1: number, y1: number, w: number, h: number): void;
+    box(x1: number, y1: number, w: number, h: number): void;
+    point(x: number, y: number, char: string): void;
+    dist(x1: number, y1: number, x2: number, y2: number): number;
+    line(x1: number, y1: number, x2: number, y2: number): void;
+    text(x: number, y: number, text: string): void;
+    fg(r: number, g: number, b: number): void;
+    bg(r: number, g: number, b: number): void;
+    draw(cb: Function): void;
+    clear(): void;
+}
+
+declare interface cursorInterface {
+    on(): void;
+    off(): void;
+    reset(): void;
+    restore(): void;
+}
+
 declare module 'axel' {
-    export interface cursorInterface {
-        on(): void;
-        off(): void;
-        reset(): void;
-        restore(): void;
-    }
-
-    export let brush: string;
-    export let rows: number;
-    export let cols: number;
-    export let cursor: cursorInterface;
-
-    export function lerp(p1: number, p2: number, m: number): number;
-    export function circ(x: number, y: number, m: number): number;
-    export function goto(x: number, y: number): void;
-    export function scrub(x1: number, y1: number, w: number, h: number): void;
-    export function box(x1: number, y1: number, w: number, h: number): void;
-    export function point(x: number, y: number, char: string): void;
-    export function dist(x1: number, y1: number, x2: number, y2: number): number;
-    export function line(x1: number, y1: number, x2: number, y2: number): void;
-    export function text(x: number, y: number, text: string): void;
-    export function fg(r: number, g: number, b: number): void;
-    export function bg(r: number, g: number, b: number): void;
-    export function draw(cb: Function): void;
-    export function clear(): void;
+    const instance: Axel;
+    export = instance;
 }
