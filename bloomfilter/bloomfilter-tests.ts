4756dc63a8c4252398a42721f1b68f328b7b0a72
Shchaurouski Sviataslau <sshchaurouski@exadel.com>
2017-03-20 17:18:39 +0300

    15183: adds types for bloomfilter

---
+++ bloomfilter/bloomfilter-tests.ts
@@ -0,0 +1,11 @@
+import { BloomFilter } from './index';
+function test_bloomfilter() {
+    const m: number = 10;
+    const k: number = 2;
+
+    let bloomFilter = new BloomFilter(m, k);
+    let array: Array<Int32Array> = bloomFilter.buckets;
+    let length: number = bloomFilter.buckets.length;
+    let add: void = bloomFilter.add('someString');
+    let test: boolean = bloomFilter.test('someString');
+}
