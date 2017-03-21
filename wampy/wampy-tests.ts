77983ed9cd95e52f957d76ad24c9add5b0d4dd81
421p <andrew.bakal@gmail.com>
2017-03-21 03:56:33 +0200

    update to v5

--- wampy/wampy-tests.ts
+++ wampy/wampy-tests.ts
@@ -26,10 +26,12 @@ let id: number = ws.getSessionId();
 ws.disconnect();
 ws.abort();
 
-ws.subscribe('system.monitor.update', (args: any[], kwargs: any) => {
+ws.subscribe('system.monitor.update', (args: any[], kwargs: any) =>
+    {
         console.log('Received system.monitor.update event!');
     })
-    .subscribe('client.message', function (args: any[], kwargs: any) {
+    .subscribe('client.message', function (args: any[], kwargs: any)
+    {
         console.log('Received client.message event!');
     });
 
@@ -40,11 +42,13 @@ ws.unsubscribe('subscribed.topic', f1);
 ws.unsubscribe('chat.message.received');
 
 ws.call('get.server.time', null, {
-    onSuccess: (args: any[], kwargs: any) => {
+    onSuccess: (args: any[], kwargs: any) =>
+    {
         console.log('RPC successfully called');
         console.log('Server time is ' + kwargs);
     },
-    onError: (err: string, details: any, args: any[], kwargs: any) => {
+    onError: (err: string, details: any, args: any[], kwargs: any) =>
+    {
         console.log('RPC call failed with error ' + err);
     }
 });
@@ -55,45 +59,45 @@ ws.getOpStatus();
 ws.publish('user.logged.in');
 ws.publish('chat.message.received', 'user message');
 ws.publish('chat.message.received', ['user message1', 'user message2']);
-ws.publish('user.modified', { field1: 'field1', field2: true, field3: 123 });
-ws.publish('user.modified', { field1: 'field1', field2: true, field3: 123 }, {
-  onSuccess: () => console.log('User successfully modified')
+ws.publish('user.modified', {field1: 'field1', field2: true, field3: 123});
+ws.publish('user.modified', {field1: 'field1', field2: true, field3: 123}, {
+    onSuccess: () => console.log('User successfully modified')
 });
-ws.publish('user.modified', { field1: 'field1', field2: true, field3: 123 }, {
-  onSuccess: () => console.log('User successfully modified'),
-  onError: (err: string, details: any) => console.log('User modification failed', err)
+ws.publish('user.modified', {field1: 'field1', field2: true, field3: 123}, {
+    onSuccess: () => console.log('User successfully modified'),
+    onError: (err: string, details: any) => console.log('User modification failed', err)
 });
-ws.publish('chat.message.received', ['Private message'], null, { eligible: 123456789 });
+ws.publish('chat.message.received', ['Private message'], null, {eligible: 123456789});
 
 ws.call('server.time', null, (args: any[], kwargs: any) => console.log('Server time is ' + args[0]));
 
 ws.call('start.migration', null, {
     onSuccess: (args: any[], kwargs: any) => console.log('RPC successfully called'),
-    onError: (err: string, details: any, args: any[], kwargs: any) => console.log('RPC call failed!',err)
+    onError: (err: string, details: any, args: any[], kwargs: any) => console.log('RPC call failed!', err)
 });
 
-ws.call('restore.backup', { backupFile: 'backup.zip' }, {
+ws.call('restore.backup', {backupFile: 'backup.zip'}, {
     onSuccess: (args: any[], kwargs: any) => console.log('Backup successfully restored'),
-    onError: (err: string, details: any, args: any[], kwargs: any) => console.log('Restore failed!',err)
+    onError: (err: string, details: any, args: any[], kwargs: any) => console.log('Restore failed!', err)
 });
 
 ws.call('start.migration', null, {
     onSuccess: (args: any[], kwargs: any) => console.log('RPC successfully called'),
-    onError: (err: string, details: any, args: any[], kwargs: any) => console.log('RPC call failed!',err)
+    onError: (err: string, details: any, args: any[], kwargs: any) => console.log('RPC call failed!', err)
 });
 
 let status = ws.getOpStatus();
 
 ws.cancel(status.reqId);
 
-let sqrt_f = (x: number, y: any) => [{}, x*x];
+let sqrt_f = (args: any[], kwargs: any, options: any) => [{}, kwargs * kwargs];
 
 ws.register('sqrt.value', sqrt_f);
 
 ws.register('sqrt.value', {
     rpc: sqrt_f,
-    onSuccess: (data: any) => console.log('RPC successfully registered'),
-    onError: (err: string, details: any) => console.log('RPC registration failed!',err)
+    onSuccess: (args: any[], kwargs: any) => console.log('RPC successfully registered'),
+    onError: (err: string, details: any) => console.log('RPC registration failed!', err)
 });
 
 ws.unregister('sqrt.value');
