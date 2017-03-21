b7361b38a995ed71255076f33cc0611848a682bc
Paul van Brenk <paul.van.brenk@microsoft.com>
2017-03-21 16:34:51 -0700

    Fix encoding

--- openfin/openfin-tests.ts
+++ openfin/openfin-tests.ts
@@ -385,7 +385,7 @@ function test_system() {
 		certificate: {
 			trusted: true,
 			subject: 'O=OpenFin INC., L=New York, S=NY, C=US',
-			thumbprint: '‎3c a5 28 19 83 05 fe 69 88 e6 8f 4b 3a af c5 c5 1b 07 80 5b'
+			thumbprint: '3c a5 28 19 83 05 fe 69 88 e6 8f 4b 3a af c5 c5 1b 07 80 5b'
 		},
 		listener(result) {
 			console.log('the exit code', result.exitCode);
@@ -454,7 +454,7 @@ function test_system() {
 	});
 	// terminateExternalProcess
 	fin.desktop.System.launchExternalProcess({
-		// notepad is in the system’s PATH
+		// notepad is in the system's PATH
 		path: "notepad",
 		arguments: "",
 		listener(result) {
