e565949d7f5d469a8284339f0f0bee22b8cc8ee9
Paul van Brenk <paul.van.brenk@microsoft.com>
2017-03-22 13:53:50 -0700

    * Remove test from nedb-logger that didn't actually test the definition. * Fix up whitespace

--- nedb-logger/index.d.ts
+++ nedb-logger/index.d.ts
@@ -9,12 +9,11 @@ export as namespace NedbLogger;
 declare namespace NeDBLoggerDataStore { }
 declare class NeDBLoggerDataStore {
 
-    constructor(path?: string | {filename: string});
+    constructor(path?: string | { filename: string });
 
     /**
      * Insert a new document
      * @param {Function} cb Optional callback, signature: err, insertedDoc
      */
     insert<T>(newDoc: T, cb?: (err: Error, document: T) => void): void;
-
 }
--- nedb-logger/nedb-logger-tests.ts
+++ nedb-logger/nedb-logger-tests.ts
@@ -7,35 +7,7 @@
 
 import * as es6styleimport from 'nedb-logger';
 
-import Q = require('q');
 import nedblogger = require('nedb-logger');
-
-class BaseCollection<T> {
-
-    private dataStore: nedblogger;
-
-    constructor(dataStore: nedblogger) {
-
-        this.dataStore = dataStore;
-    }
-
-    insert(document: T): Q.Promise<T> {
-
-        var deferred = Q.defer<T>();
-
-        this.dataStore.insert<T>(document, function(err: Error, newDoc: T) {   // Callback is optional
-            // newDoc is the newly inserted document, including its _id
-            if (err) {
-                deferred.reject(err);
-            } else {
-                deferred.resolve(newDoc);
-            }
-        });
-
-        return deferred.promise;
-    }
-
-}
 
 // Type 1: Persistent datastore with manual loading
 
