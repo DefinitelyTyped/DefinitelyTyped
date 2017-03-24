9f3baafaa0eb3e3bf2193e56227f3d420f3542ad
Marco Falkenberg <marco@mfal.de>
2017-03-24 15:41:39 +0100

    Add typings for reactstrap

---
+++ reactstrap/lib/Row.d.ts
@@ -0,0 +1,10 @@
+/// <reference types='react' />
+
+interface Props {
+  className?: string;
+  tag?: React.ReactType;
+  noGutters?: boolean;
+}
+
+declare var Row: React.StatelessComponent<React.HTMLProps<any>>;
+export default Row;
