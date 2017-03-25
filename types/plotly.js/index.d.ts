89000be665b77a0bdb85dc1a7a4c1f53423f01b0
Chris Gervang <chris@gervang.com>
2017-03-24 22:04:42 -0700

    Typescript version 2.2

--- types/plotly.js/index.d.ts
+++ types/plotly.js/index.d.ts
@@ -2,6 +2,7 @@
 // Project: https://plot.ly/javascript/
 // Definitions by: Chris Gervang <https://github.com/chrisgervang>, Martin Duparc <https://github.com/martinduparc>
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
+// TypeScript Version: 2.2
 
 declare namespace Plots {
     interface StaticPlots {
