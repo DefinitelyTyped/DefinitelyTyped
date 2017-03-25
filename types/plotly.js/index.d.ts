0070f246f5b068dbb8ecec7749da8668e68f834b
Chris Gervang <chris@gervang.com>
2017-03-24 22:01:44 -0700

    Version formatting

--- types/plotly.js/index.d.ts
+++ types/plotly.js/index.d.ts
@@ -1,5 +1,4 @@
-// Type definitions for plotly.js
-// Version: 1.22
+// Type definitions for plotly.js 1.22
 // Project: https://plot.ly/javascript/
 // Definitions by: Chris Gervang <https://github.com/chrisgervang>, Martin Duparc <https://github.com/martinduparc>
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
