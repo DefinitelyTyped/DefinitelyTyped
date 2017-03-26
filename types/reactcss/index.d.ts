8e5ac6483cceabcf0ff1ea068fae433a2b87fa29
Chris Gervang <gervangster@live.com>
2017-03-25 17:44:29 -0700

    expanded the test file to use all of the library correctly. corrected LoopableProps definition.

--- types/reactcss/index.d.ts
+++ types/reactcss/index.d.ts
@@ -1,4 +1,4 @@
-// Type definitions for ReactCSS v1.2.0
+// Type definitions for ReactCSS 1.2.0
 // Project: http://reactcss.com/
 // Definitions by: Chris Gervang <https://github.com/chrisgervang>, Karol Janyst <https://github.com/LKay>
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
@@ -6,15 +6,15 @@
 
 import * as React from "react"
 
-interface LoopableProps {
+interface LoopableProps extends React.Props<any> {
+    "nth-child": number
     "first-child"?: boolean
     "last-child"?: boolean
     even?: boolean
     odd?: boolean
-    [nthChild: string]: boolean
 }
 
-interface HoverProps {
+interface HoverProps<T> extends React.Props<T> {
     hover?: boolean
 }
 
@@ -25,5 +25,5 @@ interface Classes<T> {
 
 export type CSS = React.CSSProperties
 export function hover<A>(component: React.ComponentClass<A> | React.StatelessComponent<A>): React.ComponentClass<A>
-export function loop(i: number, length: number): LoopableProps
+export function loop(index: number, length: number): LoopableProps
 export default function reactCSS<T>(classes: Classes<T>, ...activations: Array<any>): T
