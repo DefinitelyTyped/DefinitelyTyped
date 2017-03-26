48032bc9c4a645a82f81656c436a4370a6119809
Chris Gervang <gervangster@live.com>
2017-03-25 17:44:29 -0700

    expanded the test file to use all of the library correctly. corrected LoopableProps definition.

--- types/reactcss/reactcss-tests.tsx
+++ types/reactcss/reactcss-tests.tsx
@@ -1,19 +1,67 @@
 import * as React from "react"
-import { StatelessComponent } from "react"
+import { SFC } from "react"
 import { render } from "react-dom"
-import { default as reactCSS, hover, loop, LoopableProps, HoverProps } from "reactcss"
+import { default as reactCSS, hover, loop, LoopableProps, HoverProps, CSS } from "reactcss"
 
-interface TestProps extends HoverProps { }
+interface TestHoverProps extends HoverProps<any> { }
 
-var styles: any = reactCSS({
-    default: {},
-    hover: {}
-}, { hover: true })
+const TestHover: SFC<TestHoverProps> = ({hover}) => {
+    const styles = reactCSS<{title: CSS}>({
+        default: {
+            title: {
+                color: "black"
+            }
+        },
+        hover: {
+            title: {
+                color: "blue"
+            }
+        }
+    }, { hover })
 
-var loopProps: LoopableProps = loop(1, 10)
+    const list = ["First!", "Second!", "Third!"]
 
-var TestComponent: StatelessComponent<TestProps>
-var Test = hover(TestComponent)
+    return (
+        <div>
+            <div style={ styles.title }>
+                Cool Title!
+            </div>
+
+            {list.map((item, index) => (
+                <TestLoop key={index} {...loop(index, list.length)}>{item}</TestLoop>
+            ))}
+        </div>
+    )
+}
+
+interface TestLoopProps extends LoopableProps { }
+
+const TestLoop: SFC<TestLoopProps> = (props) => {
+    const styles = reactCSS<{element: CSS}>({
+        default: {
+            element: {
+                width: "200px",
+                border: "1px solid black"
+            }
+        },
+        first: {
+            element: {
+                borderTopLeftRadius: "2px",
+                borderTopRightRadius: "2px"
+            }
+        },
+        last: {
+            element: {
+                borderBottomLeftRadius: "2px",
+                borderBottomRightRadius: "2px"
+            }
+        }
+    }, { first: props["first-child"], last: props["last-child"] })
+
+    return <div style={styles.element}>{props.children}</div>
+}
+
+const Test = hover(TestHover)
 
 render(
     <Test />,
