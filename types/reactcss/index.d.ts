6c5cb22aa5a1e4a9575922a8913a7155a772384a
Chris Gervang <chrisgervang@users.noreply.github.com>
2017-03-25 17:13:32 -0700

    Updating ReactCSS types to version 1.2

--- types/reactcss/index.d.ts
+++ types/reactcss/index.d.ts
@@ -1,11 +1,10 @@
-// Type definitions for ReactCSS v1.0.0
+// Type definitions for ReactCSS v1.2.0
 // Project: http://reactcss.com/
-// Definitions by: Karol Janyst <https://github.com/LKay>
+// Definitions by: Chris Gervang <https://github.com/chrisgervang>, Karol Janyst <https://github.com/LKay>
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
-// TypeScript Version: 2.1
+// TypeScript Version: 2.2
 
 import * as React from "react"
-
 
 interface LoopableProps {
     "first-child"?: boolean
@@ -19,11 +18,12 @@ interface HoverProps {
     hover?: boolean
 }
 
-interface Classes {
-    default: any
-    [scope: string]: any
+interface Classes<T> {
+    default: Partial<T>
+    [scope: string]: Partial<T>
 }
 
+export type CSS = React.CSSProperties
 export function hover<A>(component: React.ComponentClass<A> | React.StatelessComponent<A>): React.ComponentClass<A>
 export function loop(i: number, length: number): LoopableProps
-export default function reactCSS(classes: Classes, ...activations: Array<any>): any
+export default function reactCSS<T>(classes: Classes<T>, ...activations: Array<any>): T
