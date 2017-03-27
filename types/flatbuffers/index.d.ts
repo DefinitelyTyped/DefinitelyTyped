06da8c57a6da86673dfc989536218646bf601d61
Kamil Rojewski <kamil.rojewski@gmail.com>
2017-03-27 10:50:53 +0200

    Lint fixes

--- types/flatbuffers/index.d.ts
+++ types/flatbuffers/index.d.ts
@@ -1,4 +1,4 @@
-// Type definitions for flatbuffers 1.6.0
+// Type definitions for flatbuffers 1.6
 // Project: http://google.github.io/flatbuffers/index.html
 // Definitions by: Kamil Rojewski <kamil.rojewski@gmail.com>
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
