15cc13ef489f3342c361a50774f2d423fd2d8c16
Deyan Kamburov <dkamburov@infragistics.com>
2017-03-27 18:19:07 +0300

    Add header to typings definitions

--- types/ignite-ui/index.d.ts
+++ types/ignite-ui/index.d.ts
@@ -1,3 +1,7 @@
+// Type definitions for Ignite UI
+// Project: https://github.com/IgniteUI/ignite-ui
+// Definitions by: Ignite UI <https://github.com/IgniteUI>
+// Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
 
 interface DataSourceSettingsPaging {
 
