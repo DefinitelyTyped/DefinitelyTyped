91381a1fdecf7ae6e820cf97074ed7ef6fc841e8
gldfdp <gregoire.larreur@ozytis.fr>
2017-03-27 11:16:56 +0200

    Adding react-onsenui

---
+++ types/react-onsenui/react-onsenui-tests.tsx
@@ -0,0 +1,49 @@
+import * as React from "react";
+import * as ReactDOM from "react-dom";
+import { SplitterSide, Splitter, SplitterContent, Page } from "react-onsenui";
+
+class AppState {
+    isOpen: boolean = false;
+}
+
+interface AppProps {
+}
+
+export class App extends React.Component<AppProps, AppState>{
+
+    constructor(props?: AppProps) {
+        super(props);
+        this.state = new AppState();
+    }
+
+    hide() {
+        this.setState({ isOpen: false });
+    }
+
+    render() {
+
+        return (
+            <Splitter>
+                <SplitterSide
+                    side='left'
+                    collapse={true}
+                    isOpen={this.state.isOpen}
+                    onClose={() => this.hide()}
+                    isSwipeable={true}>
+                    <Page>
+                        Menu content
+					</Page>
+                </SplitterSide>
+                <SplitterContent>
+                    <Page>
+                        Test page
+					</Page>
+                </SplitterContent>
+            </Splitter>
+        );
+    }
+
+}
+
+
+ReactDOM.render(<App />, document.getElementById('react-body'));
