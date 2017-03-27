a98fdea48a4bc315f4be654f6b783911febf5d03
huhuanming <workboring@gmail.com>
2017-03-27 23:13:52 +0800

    Add hegiht && paginationStyle

--- types/react-native-swiper/index.d.ts
+++ types/react-native-swiper/index.d.ts
@@ -1,6 +1,6 @@
 // Type definitions for react-native-swiper 1.5
 // Project: https://github.com/leecade/react-native-swiper#readme
-// Definitions by: CaiHuan <https://github.com/CaiHuan>
+// Definitions by: CaiHuan <https://github.com/CaiHuan>,  HuHuanming <https://github.com/huhuanming>
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
 // TypeScript Version: 2.1
 
@@ -58,6 +58,12 @@ interface SwiperProperties extends React.Props<Swiper> {
   dotColor?: string;
 
   activeDotColor?: string;
+
+  height?: number;
+
+  width?: number;
+
+  paginationStyle?: ViewStyle;
 }
 
 export default class Swiper extends React.Component<SwiperProperties, {}> {
