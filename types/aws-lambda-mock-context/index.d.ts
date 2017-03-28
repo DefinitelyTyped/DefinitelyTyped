119c20e4d0d0e8417c308dbb1350c8cf3745e6e2
Morgan Moskalyk <morgan.moskalyk@gmail.com>
2017-03-28 14:36:13 -0400

    updated index.d.ts

--- types/aws-lambda-mock-context/index.d.ts
+++ types/aws-lambda-mock-context/index.d.ts
@@ -3,35 +3,32 @@
 // Definitions by: Morgan Moskalyk <morgan.moskalyk@gmail.com>, Anand Nimkar <anand.a.nimkar@gmail.com>
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
 
-declare module "aws-lambda-mock-context" {
+declare function context(options?: Options): Context;
 
-    export = context;
-
-    function context(options?: IOptions): IContext;
-
-    interface IContext {
-        Promise: Promise<any>;
-        callbackWaitsForEmptyEventLoop: boolean;
-        functionName: string;
-        functionVersion: string;
-        invokedFunctionArn: string;
-        memoryLimitInMB: string;
-        awsRequestId: string;
-        invokeid: string;
-        logGroupName: string;
-        logStreamName: string;
-        getRemainingTimeInMillis: number;
-        succeed(result: any): Promise<any>;
-        fail(err: any): Promise<any>;
-        done(err: any, result: any): Promise<any>;
-    }
-
-    interface IOptions {
-        region: string;
-        account: string;
-        functionName: string;
-        functionVersion: string;
-        memoryLimitInMB: string;
-        alias?: string;
-    }
+interface Context {
+    Promise: Promise<any>;
+    callbackWaitsForEmptyEventLoop: boolean;
+    functionName: string;
+    functionVersion: string;
+    invokedFunctionArn: string;
+    memoryLimitInMB: string;
+    awsRequestId: string;
+    invokeid: string;
+    logGroupName: string;
+    logStreamName: string;
+    getRemainingTimeInMillis: number;
+    succeed(result: any): Promise<any>;
+    fail(err: any): Promise<any>;
+    done(err: any, result: any): Promise<any>;
 }
+
+interface Options {
+    region: string;
+    account: string;
+    functionName: string;
+    functionVersion: string;
+    memoryLimitInMB: string;
+    alias?: string;
+}
+
+export { context };
