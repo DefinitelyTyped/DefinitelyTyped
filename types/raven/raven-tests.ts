05bb80c0df0704e515eea69dde5d14ed70cb0e3d
Scott Cooper <scttcper@gmail.com>
2017-03-27 22:01:53 -0700

    use 4 spaces

--- types/raven/raven-tests.ts
+++ types/raven/raven-tests.ts
@@ -5,24 +5,24 @@ const dsn = '___DSN___';
 const client = Raven.config(dsn, { autoBreadcrumbs: true }).install();
 Raven.config(dsn).install({ captureUnhandledRejections: true });
 client.setContext({});
-client.on('logged', () => {});
+client.on('logged', () => { });
 client.process({});
-client.captureMessage('Broken!', (err, eventId) => {});
+client.captureMessage('Broken!', (err, eventId) => { });
 new Raven.Client({ name: 'YAY!' });
 new Raven.Client(dsn, { name: 'YAY!' });
 new Raven.Client(dsn);
 
 try {
-  throw new Error();
+    throw new Error();
 } catch (e) {
-  var eventId = Raven.captureException(e, (sendErr, eventId) => {});
+    var eventId = Raven.captureException(e, (sendErr, eventId) => { });
 }
 
 Raven.setContext({});
 Raven.mergeContext({});
 Raven.context(() => {
-  Raven.captureBreadcrumb({});
+    Raven.captureBreadcrumb({});
 });
-var doIt = () => {}
+var doIt = () => { }
 setTimeout(Raven.wrap(doIt), 1000)
 Raven.parseDSN('https://8769c40cf49c4cc58b51fa45d8e2d166:296768aa91084e17b5ac02d3ad5bc7e7@app.getsentry.com/269')
