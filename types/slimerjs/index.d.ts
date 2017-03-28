0ef2f338ca6265e3f617807647df22d194fd8b37
nedunk <nedunk@local.org>
2017-03-28 21:02:36 +0200

    MAJOR.MINIOR fix

--- types/slimerjs/index.d.ts
+++ types/slimerjs/index.d.ts
@@ -1,4 +1,4 @@
-// Type definitions for SlimerJS 0.10.3
+// Type definitions for SlimerJS 0.10
 // Project: https://docs.slimerjs.org/current/index.html#api-reference
 // Definitions by: Alex Wall <https://github.com/alexwall>
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
