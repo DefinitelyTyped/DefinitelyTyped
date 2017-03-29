32484a4bc9d11b6cdf55aa6836d6b50b76891dcc
Morgan Moskalyk <morgan.moskalyk@gmail.com>
2017-03-28 21:33:47 -0400

    decremented version back to 1.0

--- types/aws-lambda-mock-context/index.d.ts
+++ types/aws-lambda-mock-context/index.d.ts
@@ -1,4 +1,4 @@
-// Type definitions for AWS Lambda Mock Context SDK 1.1
+// Type definitions for AWS Lambda Mock Context SDK 1.0
 // Project: https://github.com/moskalyk/typed-aws-lambda-mock-context
 // Definitions by: Morgan Moskalyk <morgan.moskalyk@gmail.com>, Anand Nimkar <anand.a.nimkar@gmail.com>
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
