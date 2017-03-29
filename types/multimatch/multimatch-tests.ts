132919890f10e8eaa64cdc499bea3613717f4fe9
Stephen Lautier <stephen.lautier@outlook.com>
2017-03-29 18:47:50 +0200

    added definitions for multimatch

---
+++ types/multimatch/multimatch-tests.ts
@@ -0,0 +1,8 @@
+import * as multimatch from "multimatch";
+
+const options: multimatch.MultimatchOptions = {
+	debug: true
+};
+
+multimatch(["unicorn", "cake", "rainbows"], ["*", "!cake"]);
+multimatch(["unicorn", "cake", "rainbows"], ["*", "!cake"], options);
