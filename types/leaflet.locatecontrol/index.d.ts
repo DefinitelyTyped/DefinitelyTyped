05513e0a0ad8acd01216587583de7b4bee2eae9a
Denis <carriere.denis@gmail.com>
2017-03-30 08:30:25 -0400

    leaflet.locate - remove useless package.json

--- types/leaflet.locatecontrol/index.d.ts
+++ types/leaflet.locatecontrol/index.d.ts
@@ -1,4 +1,4 @@
-// Type definitions for Leaflet.locate 0.60
+// Type definitions for leaflet.locatecontrol 0.60
 // Project: https://github.com/domoritz/leaflet-locatecontrol
 // Definitions by: Denis Carriere <https://github.com/DenisCarriere>
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
