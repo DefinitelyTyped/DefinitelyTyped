93410826e1d07c3846424a6397cc66bfd92ba5e6
Denis <carriere.denis@gmail.com>
2017-03-30 17:49:01 -0400

    Fix linting

--- types/leaflet.locatecontrol/index.d.ts
+++ types/leaflet.locatecontrol/index.d.ts
@@ -7,32 +7,32 @@
 
 declare namespace L {
     namespace Control {
-        export interface Locate extends L.Control {}
-        export interface LocateOptions {
-            position?: string
-            layer?: L.Layer
-            setView?: boolean | string
-            flyTo?: boolean
-            keepCurrentZoomLevel?: boolean
-            clickBehavior?: any
-            returnToPrevBounds?: boolean
-            cacheLocation?: boolean
-            drawCircle?: boolean
-            drawMarker?: boolean
-            markerClass?: any
-            circleStyle?: L.PathOptions
-            markerStyle?: L.PathOptions
-            followCircleStyle?: L.PathOptions
-            followMarkerStyle?: L.PathOptions
-            icon?: string
-            iconLoading?: string
-            iconElementTag?: string
-            circlePadding?: number[]
-            onLocationError?: any
-            onLocationOutsideMapBounds?: any
-            showPopup?: boolean
-            strings?: any
-            locateOptions?: L.LocateOptions
+        interface Locate extends L.Control {}
+        interface LocateOptions {
+            position?: string;
+            layer?: L.Layer;
+            setView?: boolean | string;
+            flyTo?: boolean;
+            keepCurrentZoomLevel?: boolean;
+            clickBehavior?: any;
+            returnToPrevBounds?: boolean;
+            cacheLocation?: boolean;
+            drawCircle?: boolean;
+            drawMarker?: boolean;
+            markerClass?: any;
+            circleStyle?: L.PathOptions;
+            markerStyle?: L.PathOptions;
+            followCircleStyle?: L.PathOptions;
+            followMarkerStyle?: L.PathOptions;
+            icon?: string;
+            iconLoading?: string;
+            iconElementTag?: string;
+            circlePadding?: number[];
+            onLocationError?: any;
+            onLocationOutsideMapBounds?: any;
+            showPopup?: boolean;
+            strings?: any;
+            locateOptions?: L.LocateOptions;
         }
     }
 
