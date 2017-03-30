c0083ec134066c4fcc96470deb9aebaf1ae42ef3
Justin Firth <jmfirth@gmail.com>
2017-03-30 13:03:14 -0400

    Added version commit

--- types/hyperscript/index.d.ts
+++ types/hyperscript/index.d.ts
@@ -2,6 +2,7 @@
 // Project: https://github.com/dominictarr/hyperscript
 // Definitions by: Mike Linkovich <https://github.com/spacejack>
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
+// TypeScript Version: 2.2
 
 declare module 'hyperscript' {
 
