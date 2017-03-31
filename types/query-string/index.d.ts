b6c343f2b39ea4f6a61ddd23abea390f455f4301
Andy <anhans@microsoft.com>
2017-03-31 11:01:26 -0700

    Apply new lint rules to yet more packages (#15548)
    * Apply new lint rules to yet more packages
    
    * Fix leaflet lint

--- types/query-string/index.d.ts
+++ types/query-string/index.d.ts
@@ -4,36 +4,33 @@
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
 // TypeScript Version: 2.2
 
-declare module "query-string" {
-
-    interface ParseOptions {
-        arrayFormat?: 'bracket' | 'index' | 'none';
-    }
-
-    /**
-     * Parse a query string into an object.
-     * Leading ? or # are ignored, so you can pass location.search or location.hash directly.
-     * @param str
-     */
-    export function parse(str: string, options?: ParseOptions): any;
-
-    interface StringifyOptions {
-        strict?: boolean;
-        encode?: boolean;
-        arrayFormat?: 'bracket' | 'index' | 'none';
-    }
-
-    /**
-     * Stringify an object into a query string, sorting the keys.
-     *
-     * @param obj
-     */
-    export function stringify(obj: object, options?: StringifyOptions): string;
-
-    /**
-     * Extract a query string from a URL that can be passed into .parse().
-     *
-     * @param str
-     */
-    export function extract(str: string): string;
+export interface ParseOptions {
+    arrayFormat?: 'bracket' | 'index' | 'none';
 }
+
+/**
+ * Parse a query string into an object.
+ * Leading ? or # are ignored, so you can pass location.search or location.hash directly.
+ * @param str
+ */
+export function parse(str: string, options?: ParseOptions): any;
+
+export interface StringifyOptions {
+    strict?: boolean;
+    encode?: boolean;
+    arrayFormat?: 'bracket' | 'index' | 'none';
+}
+
+/**
+ * Stringify an object into a query string, sorting the keys.
+ *
+ * @param obj
+ */
+export function stringify(obj: object, options?: StringifyOptions): string;
+
+/**
+ * Extract a query string from a URL that can be passed into .parse().
+ *
+ * @param str
+ */
+export function extract(str: string): string;
