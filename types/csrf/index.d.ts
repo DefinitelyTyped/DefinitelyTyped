51dc93b12d81f80ed20c48f8f5fcafc2317fcaeb
Markis Taylor <m@rkis.cc>
2017-03-30 23:38:42 -0700

    Add definitions for CSRF module

--- types/csrf/index.d.ts
+++ types/csrf/index.d.ts
@@ -1,62 +1,51 @@
-// Type definitions for b_ 1.3
+// Type definitions for csrf 3.0.6
 // Project: https://github.com/pillarjs/csrf
 // Definitions by: Markis Taylor <https://github.com/markis>
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
 
-type SecretCallback = (err: Error | null, secret: string) => void;
+declare namespace csrf {
+    /**
+     * The callback from creating a secret
+     */
+    type SecretCallback = (err: Error | null, secret: string) => void;
 
-interface TokensOptions {
-    /**
-     * The string length of the salt (default: 8)
-     */
-    saltLength: number;
-    /**
-     * The byte length of the secret key (default: 18)
-     */
-    secretLength: number;
+    interface TokensOptions {
+        /**
+         * The string length of the salt (default: 8)
+         */
+        saltLength: number;
+        /**
+         * The byte length of the secret key (default: 18)
+         */
+        secretLength: number;
+    }
+
+    class Tokens {
+        /**
+         * Token generation/verification class.
+         */
+        constructor(options?: TokensOptions)
+
+        /**
+         * Create a new CSRF token.
+         */
+        create(secret: string): string;
+
+        /**
+         * Create a new secret key.
+         */
+        secret(callback?: SecretCallback): Promise<string>;
+
+        /**
+         * Create a new secret key synchronously.
+         */
+        secretSync(): string;
+
+        /**
+         * Verify if a given token is valid for a given secret.
+         */
+        verify(secret: string, token: string): boolean;
+    }
 }
 
-declare class Tokens {
-    /**
-     * Token generation/verification class.
-     *
-     * @param {object} [options]
-     * @param {number} [options.saltLength=8] The string length of the salt
-     * @param {number} [options.secretLength=18] The byte length of the secret key
-     * @public
-     */
-    constructor(options?: TokensOptions)
-
-    /**
-     * Create a new CSRF token.
-     *
-     * @param {string} secret The secret for the token.
-     * @public
-     */
-    create(secret: string): string;
-
-    /**
-     * Create a new secret key.
-     *
-     * @param {function} [callback]
-     * @public
-     */
-    secret(callback?: SecretCallback): Promise<string>;
-
-    /**
-     * Create a new secret key synchronously.
-     * @public
-     */
-    secretSync(): string;
-
-    /**
-     * Verify if a given token is valid for a given secret.
-     *
-     * @param {string} secret
-     * @param {string} token
-     * @public
-     */
-    verify(secret: string, token: string): boolean;
-}
-
-export = Tokens;
+export = csrf.Tokens;
