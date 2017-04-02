c42efa8b3f69bcc871997dc497f034861e36fc49
Cyril Schumacher <cyril_schumacher@live.fr>
2017-04-02 16:27:39 +0200

    Add definition for "throng". Improve file: "tsconfig.json" for "pidusage" and "procfs-stats".

--- types/pidusage/pidusage-tests.ts
+++ types/pidusage/pidusage-tests.ts
@@ -1,8 +1,8 @@
 import pusage = require("pidusage")
 
 pusage.stat(0, function(err, stat) {
-	console.log("Pcpu: %s", stat.cpu);
-	console.log("Mem: %s", stat.memory);
+	const cpu = stat.cpu;
+	const memory = stat.memory;
 });
 
 pusage.unmonitor(0);
