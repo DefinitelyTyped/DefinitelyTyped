dc409828b4169a338ae8cb04a4e938e08f323d8c
Ganesh <ganeshyb@gmail.com>
2017-04-02 18:31:59 -0700

    Updated with type defintions header

--- types/reactivex-rxjs/index.d.ts
+++ types/reactivex-rxjs/index.d.ts
@@ -1,7 +1,8 @@
-// Global Rx for @reactivex/rxjs
-// TypeScript Version: 2.1.
+// Type definitions for @reactivex/rxjs 5.1
+// Project: https://github.com/ReactiveX/rxjs
 // Definitions by: Ganesh <https://github.com/ganeshyb>
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
+// TypeScript Version: 2.1.
 
 import * as _Rx from '@reactivex/rxjs/dist/cjs/Rx';
 
