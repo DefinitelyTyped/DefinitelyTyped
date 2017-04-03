a08b6ba3b2e1babd8531cef9ebbac0310dd98332
Ganesh <ganeshyb@gmail.com>
2017-04-02 18:48:54 -0700

    removed extra period after typescript version

--- types/reactivex-rxjs/index.d.ts
+++ types/reactivex-rxjs/index.d.ts
@@ -2,7 +2,7 @@
 // Project: https://github.com/ReactiveX/rxjs
 // Definitions by: Ganesh <https://github.com/ganeshyb>
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
-// TypeScript Version: 2.1.
+// TypeScript Version: 2.1
 
 import * as _Rx from '@reactivex/rxjs/dist/cjs/Rx';
 
