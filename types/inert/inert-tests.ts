c24bf276ae989adcd4ba3f346889d39d808d8ffc
Alexander James Phillips <ajp.global@gmail.com>
2017-04-03 18:02:25 +0100

    Update definition for inert 4.2 and add more tests

--- types/inert/inert-tests.ts
+++ types/inert/inert-tests.ts
@@ -3,3 +3,26 @@ import * as InertES6 from 'inert';
 
 const server = new HapiES6.Server({});
 server.register(InertES6, () => {});
+
+server.route({
+    path: '',
+    method: 'GET',
+    file: {
+        path: '',
+        confine: true,
+    },
+    directory: {
+        path: '',
+        listing: true
+    }
+})
+
+var fileHandler: HapiES6.IFileHandler = {
+    path: '',
+    confine: true,
+}
+
+var directoryHandler: HapiES6.IDirectoryHandler = {
+    path: '',
+    listing: true,
+}
