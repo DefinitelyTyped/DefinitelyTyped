81a8d0336428dcc08cbf98d49ff80b83ebeb9ac3
Ganesh <ganeshyb@gmail.com>
2017-04-02 17:46:27 -0700

    Adding global typings for @reactivex/rxjs.
    Adding Rx global namespace for consuming within typescript using
    namesapces

---
+++ types/reactivex-rxjs/index.d.ts
@@ -0,0 +1,9 @@
+// Global Rx for @reactivex/rxjs
+// TypeScript Version: 2.1.
+// Definitions by: Ganesh <https://github.com/ganeshyb>
+// Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
+
+import * as _Rx from '@reactivex/rxjs/dist/cjs/Rx';
+
+export as namespace Rx;
+export = _Rx;
---
+++ types/reactivex-rxjs/reactivex-rxjs-tests.ts
@@ -0,0 +1,6 @@
+
+Rx.Observable.of(1);
+
+let x = new Rx.Subject();
+
+let y = new Rx.BehaviorSubject<string>("hello");
