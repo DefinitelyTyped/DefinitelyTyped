988b8ceff92f21da7658dcc97ee9feeca6a58b89
Huy Nguyen <huy@huy-nguyen.com>
2017-04-04 10:16:14 -0400

    Fix lint errors

--- types/react-router-redux/react-router-redux-tests.tsx
+++ types/react-router-redux/react-router-redux-tests.tsx
@@ -1,35 +1,35 @@
-import * as React from 'react'
-import * as ReactDOM from 'react-dom'
+import * as React from 'react';
+import * as ReactDOM from 'react-dom';
 
-import { createStore, combineReducers, applyMiddleware, Reducer } from 'redux'
-import { Provider } from 'react-redux'
+import { createStore, combineReducers, applyMiddleware, Reducer } from 'redux';
+import { Provider } from 'react-redux';
 
-import createHistory from 'history/createBrowserHistory'
-import { Route } from 'react-router'
+import createHistory from 'history/createBrowserHistory';
+import { Route } from 'react-router';
 
-import { ConnectedRouter, routerReducer, routerMiddleware, push, RouterState } from 'react-router-redux'
+import { ConnectedRouter, routerReducer, routerMiddleware, push, RouterState } from 'react-router-redux';
 
 // Create a history of your choosing (we're using a browser history in this case)
-const history = createHistory()
+const history = createHistory();
 
 // Build the middleware for intercepting and dispatching navigation actions
-const middleware = routerMiddleware(history)
+const middleware = routerMiddleware(history);
 
 interface State {
-  router: RouterState
+  router: RouterState;
 }
 
 // For testing, assume the router reducer is the only sub-reducer:
-const reducers: Reducer<State> = combineReducers<State>({router: routerReducer})
+const reducers: Reducer<State> = combineReducers<State>({router: routerReducer});
 
 // Add the reducer to your store on the `router` key
 // Also apply our middleware for navigating
 const store = createStore(
   reducers,
   applyMiddleware(middleware)
-)
+);
 
-const Home = () => <div>Home</div>
+const Home = () => <div>Home</div>;
 ReactDOM.render(
   <Provider store={store}>
     { /* ConnectedRouter will use the store from Provider automatically */ }
@@ -40,7 +40,7 @@ ReactDOM.render(
     </ConnectedRouter>
   </Provider>,
   document.getElementById('root')
-)
+);
 
 // Now you can dispatch navigation actions from anywhere!
-store.dispatch(push('/foo'))
+store.dispatch(push('/foo'));
