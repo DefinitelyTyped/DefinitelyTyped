483e80063c88c47c75950e6d203ed14e2106187a
s.atanasov@everybag.de <s.atanasov@everybag.de>
2017-04-05 14:09:55 +0200

    Fixed definistions that were giving me errors on my working code. Added test cases for the changes.

--- types/knex/knex-tests.ts
+++ types/knex/knex-tests.ts
@@ -667,3 +667,10 @@ knex.seed.make(name);
 
 knex.seed.run(config);
 knex.seed.run();
+
+
+knex.schema
+  .dropTableIfExists('A')
+  .createTable('A', table => {
+    table.integer('C').unsigned().references('B.id').notNullable();
+  });
