cc4ea4af88130defe9894868cfcfc56ca1239251
Wink Saville <wink@saville.com>
2017-04-05 09:12:47 -0700

    Remove README.md move contents ti index.d.ts
    Also only a single author.

--- types/webassembly-js-api/index.d.ts
+++ types/webassembly-js-api/index.d.ts
@@ -1,7 +1,31 @@
 // Type definitions for WebAssembly v1 (MVP)
 // Project: https://github.com/winksaville/test-webassembly-js-ts
-// Definitions by: 01alchemist (https://twitter.com/01alchemist) && Wink Saville (https://github.com/winksaville)
+// Definitions by: 01alchemist <https://twitter.com/01alchemist>
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
+
+/**
+ * WebAssembly v1 (MVP) declaration file for TypeScript
+ *
+ * Authors: 01alchemist <https://twitter.com/01alchemist>
+ *          and Wink Saville <wink@saville.com>
+ *
+ * ## Installation
+ * ```
+ * npm install @types/webassembly-js-api --save-dev
+ * ```
+ *
+ * The WebAssembly namespace will now be available to your code TypeScript code.
+ * See [WebAssembly JS API](http://webassembly.org/getting-started/js-api/) and the contents of
+ * @types/webassembly-js-api/index.d.ts for more details.
+ *
+ * Do not use an `import * as WebAssembly from "webassembly-js-api"` as this will cause
+ * the `tsc` to emit a `require` statement for the WebAssembly module which doesn't exist as
+ * WebAssembly is part of the global namespace.
+ *
+ * Instead use a `///<reference path="./node_modules/@types/webassembly-js-api"/>` statement.
+ * Note: this `<reference path="..."/>` maybe optional, but YMMV.
+ */
+
 declare namespace WebAssembly {
     /**
      * WebAssembly.Module
