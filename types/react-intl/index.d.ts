67b7f2ef0510422a55bfbeeac01ec47f49d0cc0f
Jaakko Hannikainen <jaakko.hannikainen@solita.fi>
2017-04-06 16:43:50 +0300

    [react-intl] Inline types for formatMessage, formatHTMLMessage and FormattedMessage

--- types/react-intl/index.d.ts
+++ types/react-intl/index.d.ts
@@ -10,14 +10,6 @@
 
 declare namespace ReactIntl {
 
-    interface Values {
-        [key: string]: string | JSX.Element;
-    }
-
-    interface HTMLValues {
-        [key: string]: string;
-    }
-
     interface Locale {
         locale: string;
         fields?: { [key: string]: string },
@@ -74,8 +66,8 @@ declare namespace ReactIntl {
         formatRelative: (value: number, options?: FormattedRelative.PropsBase & { now?: any }) => string;
         formatNumber: (value: number, options?: FormattedNumber.PropsBase) => string;
         formatPlural: (value: number, options?: FormattedPlural.Base) => keyof FormattedPlural.PropsBase;
-        formatMessage: (messageDescriptor: FormattedMessage.MessageDescriptor, values?: Values) => string;
-        formatHTMLMessage: (messageDescriptor: FormattedMessage.MessageDescriptor, values?: HTMLValues) => string;
+        formatMessage: (messageDescriptor: FormattedMessage.MessageDescriptor, values?: {[key: string]: string}) => string;
+        formatHTMLMessage: (messageDescriptor: FormattedMessage.MessageDescriptor, values?: {[key: string]: string}) => string;
         locale: string;
         formats: any;
         messages: { [id: string]: string };
@@ -143,7 +135,7 @@ declare namespace ReactIntl {
         }
 
         export interface Props extends MessageDescriptor {
-            values?: Values;
+            values?: {[key: string]: string | JSX.Element};
             tagName?: string;
         }
     }
