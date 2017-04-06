0118a8ae696c0279857c91573d80cfa41bd97ac0
Chris Stone <chstone@microsoft.com>
2017-04-06 10:56:40 -0400

    fix key name

--- types/documentdb/index.d.ts
+++ types/documentdb/index.d.ts
@@ -391,7 +391,7 @@ export interface CollectionPartitionKey {
      * 
      * The array must contain only a single value.
      */
-    path: string[];
+    paths: string[];
 
     /**
      * The algorithm used for partitioning. Only Hash is supported.
