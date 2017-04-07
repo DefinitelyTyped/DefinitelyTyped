a78ab4b203ca794618274fdf23bebde79418549a
Niklas Walter <walter.niklas@gmail.com>
2017-04-07 18:20:08 +0200

    Add type declaration for settings object to duration format method.

--- types/moment-duration-format/index.d.ts
+++ types/moment-duration-format/index.d.ts
@@ -7,6 +7,13 @@ import * as moment from "moment";
 
 declare module "moment" {
     interface Duration {
-        format(template: string, precision?: string, settings?: any): string;
+        format(template: string, precision?: string, settings?: DurationFormatSettings): string;
+    }
+
+    interface DurationFormatSettings {
+        template: string;
+        precision: number;
+        trim: boolean;
+        forceLength: boolean;
     }
 }
