5c5290d1d4806af7103cdb447d824d8caeac2cf2
bglee <deptno@gmail.com>
2017-04-08 17:21:06 +0900

    align v2.1.2 rename tests.tsx => ts

--- react-ga/index.d.ts
+++ react-ga/index.d.ts
@@ -1,4 +1,4 @@
-// Type definitions for react-fa v1.4.1
+// Type definitions for react-ga v2.1.2
 // Project: https://github.com/react-ga/react-ga
 // Definitions by: Tim Aldridge <https://github.com/telshin>
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
@@ -14,17 +14,41 @@ declare namespace __reactGA {
 
     export interface InitializeOptions {
         debug?: boolean;
+        titleCase?: boolean;
+        gaOptions?: string;
     }
 
     export interface FieldsObject {
         [i: string]: any;
     }
 
+    interface TimingArgs {
+        category: string;
+        variable: string;
+        value: number;
+        label?: string;
+    }
+
+    interface Plugin {
+        require(name: string, options: any): void;
+        execute(pluginName: string, action: string, actionTypeOrPayload: string|Object, payload?: Object): void;
+    }
+
+    interface OutboundLinkArgs {
+        label: string;
+    }
+
     export function initialize(trackingCode: string, options?: InitializeOptions): void;
+    export function ga(): Function;
+    export function set(fieldsObject: FieldsObject): void;
+    export function send(fieldsObject: FieldsObject): void;
     export function pageview(path: string): void;
     export function modalview(name: string): void;
+    export function timing(args: TimingArgs): void;
     export function event(args: EventArgs): void;
-    export function set(fieldsObject: FieldsObject): void;
+    export function exception(fieldsObject: FieldsObject): void;
+    export const plugin: Plugin;
+    export function outboundLink(args: OutboundLinkArgs, hitCallback: Function): void;
 }
 
 declare module 'react-ga' {
