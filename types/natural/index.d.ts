220199e75bfca818e0a63a18437bc3084d630410
viskin <r.viskin@terikon.com>
2017-04-08 13:39:50 +0300

    @types/natural: increased version

--- types/natural/index.d.ts
+++ types/natural/index.d.ts
@@ -1,4 +1,4 @@
-// Type definitions for Natural 0.2.1
+// Type definitions for Natural 0.2.2
 // Project: https://github.com/NaturalNode/natural
 // Definitions by: Dylan R. E. Moonfire <https://github.com/dmoonfire/>
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
