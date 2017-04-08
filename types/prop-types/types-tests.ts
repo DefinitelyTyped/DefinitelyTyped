dc86f9fc3b00bff53d07c8263eabf25087ad3ef0
Dovydas Navickas <dovydas@quatrodev.com>
2017-04-08 22:46:20 +0300

    Update types-tests.ts

--- types/prop-types/types-tests.ts
+++ types/prop-types/types-tests.ts
@@ -1,2 +1,14 @@
 import * as PropTypes from "prop-types";
 
+let testObj: any = {
+    anyIsRequired: PropTypes.any.isRequired,
+    arrayIsRequired: PropTypes.array.isRequired,
+    boolIsRequired: PropTypes.bool.isRequired,
+    funcIsRequired: PropTypes.func.isRequired,
+    numberIsRequired: PropTypes.number.isRequired,
+    objectIsRequired: PropTypes.object.isRequired,
+    stringIsRequired: PropTypes.string.isRequired,
+    nodeIsRequired: PropTypes.node.isRequired,
+    elementIsRequired: PropTypes.element.isRequired
+};
+console.log(testObj);
