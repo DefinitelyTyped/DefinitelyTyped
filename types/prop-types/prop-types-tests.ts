f22198d51fc0aa7ec29770c01a307e0d00839f47
Dovydas Navickas <dovydas@quatrodev.com>
2017-04-09 04:32:42 +0300

    Update prop-types-tests.ts

--- types/prop-types/prop-types-tests.ts
+++ types/prop-types/prop-types-tests.ts
@@ -1,13 +1,26 @@
 import * as PropTypes from "prop-types";
+import * as React from "react";
 
-let testObj: any = {
-    anyIsRequired: PropTypes.any.isRequired,
-    arrayIsRequired: PropTypes.array.isRequired,
-    boolIsRequired: PropTypes.bool.isRequired,
-    funcIsRequired: PropTypes.func.isRequired,
-    numberIsRequired: PropTypes.number.isRequired,
-    objectIsRequired: PropTypes.object.isRequired,
-    stringIsRequired: PropTypes.string.isRequired,
-    nodeIsRequired: PropTypes.node.isRequired,
-    elementIsRequired: PropTypes.element.isRequired
+interface Props {
+    any: any;
+    array: string[];
+    bool: boolean;
+    func: () => void;
+    string: string;
+    number: number;
+    object: {};
+    node: any;
+    element: any;
+}
+
+let propTypes: React.ValidationMap<Props> = {
+    any: PropTypes.any.isRequired,
+    array: PropTypes.array.isRequired,
+    bool: PropTypes.bool.isRequired,
+    func: PropTypes.func.isRequired,
+    number: PropTypes.number.isRequired,
+    object: PropTypes.object.isRequired,
+    string: PropTypes.string.isRequired,
+    node: PropTypes.node.isRequired,
+    element: PropTypes.element.isRequired
 };
