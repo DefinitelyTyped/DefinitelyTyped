bc01345e8ce3b7811eca345b9b2c966982c42f33
Niklas Walter <walter.niklas@gmail.com>
2017-04-10 09:54:08 +0200

    Correct type for precision parameter

--- types/moment-duration-format/index.d.ts
+++ types/moment-duration-format/index.d.ts
@@ -7,7 +7,7 @@ import * as moment from "moment";
 
 declare module "moment" {
     interface Duration {
-        format(template: string, precision?: string, settings?: DurationFormatSettings): string;
+        format(template: string, precision?: number, settings?: DurationFormatSettings): string;
     }
 
     interface DurationFormatSettings {
