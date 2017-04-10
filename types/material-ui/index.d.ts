dd83feba230dba857b0239a2c05f13a7e5d72422
Igor Belagorudsky <theigor@gmail.com>
2017-04-10 15:38:33 -0400

    upped version number

--- types/material-ui/index.d.ts
+++ types/material-ui/index.d.ts
@@ -1,4 +1,4 @@
-// Type definitions for material-ui v0.16.0
+// Type definitions for material-ui v0.16.1
 // Project: https://github.com/callemall/material-ui
 // Definitions by: Nathan Brown <https://github.com/ngbrown>, Oliver Herrmann <https://github.com/herrmanno>, Igor Belagorudsky <https://github.com/theigor>
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
