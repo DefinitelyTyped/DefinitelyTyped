df6be33879240aee50d08d318b4d1f8f7e3a12b1
Jørn Andre Tangen @gorillatron <jornandretangen@gmail.com>
2017-04-11 11:33:45 +0200

    no module declaration. top level export

--- types/virgen-acl/index.d.ts
+++ types/virgen-acl/index.d.ts
@@ -1,58 +1,53 @@
 
 
-declare module "virgen-acl" {
-
-  export interface IResourceProp {
-    resource_id:string;
-  }
-
-  export interface IResourceGetter {
-    getResourceId():string;
-  }
-
-  export type IResource = IResourceGetter | IResourceProp | string
-
-
-
-  export interface IRoleProp {
-    role_id:string;
-  }
-
-  export interface IRoleGetter {
-    getRoleId():string;
-  }
-
-  export type IRole = IRoleGetter | IRoleProp | string
-  
-
-
-  export interface IQueryCallback {
-    (err?:Error, result?:boolean): void
-  }
-
-  export interface IAssertion<Ro extends IRole, Re extends IResource> {
-    (err:Error, role:Ro, resource:Re, action:string, result:IAssertionResult, next:Function): void
-  }
-
-  export interface IAssertionResult {
-    (error?:Error, result?:boolean): void
-  }
-
-
-  export class Acl {
-
-    addRole(role:string, parent?:string):void
-
-    addResource(resource:string, parent?:string):void
-
-    allow<Ro extends IRole, Re extends IResource> (role?:string, resource?:string, actions?:string[], assertion?:boolean|IAssertion<Ro, Re>):void
-
-    deny<Ro extends IRole, Re extends IResource> (role?:string, resource?:string, actions?:string[], assertion?:boolean|IAssertion<Ro, Re>):void
-
-    query(role:IRole, resource:IResource, action:string, done:IQueryCallback):void
-
-  }
-
+export interface IResourceProp {
+  resource_id:string;
 }
 
+export interface IResourceGetter {
+  getResourceId():string;
+}
+
+export type IResource = IResourceGetter | IResourceProp | string
+
+
+
+export interface IRoleProp {
+  role_id:string;
+}
+
+export interface IRoleGetter {
+  getRoleId():string;
+}
+
+export type IRole = IRoleGetter | IRoleProp | string
+
+
+
+export interface IQueryCallback {
+  (err?:Error, result?:boolean): void
+}
+
+export interface IAssertion<Ro extends IRole, Re extends IResource> {
+  (err:Error, role:Ro, resource:Re, action:string, result:IAssertionResult, next:Function): void
+}
+
+export interface IAssertionResult {
+  (error?:Error, result?:boolean): void
+}
+
+
+export class Acl {
+
+  addRole(role:string, parent?:string):void
+
+  addResource(resource:string, parent?:string):void
+
+  allow<Ro extends IRole, Re extends IResource> (role?:string, resource?:string, actions?:string[], assertion?:boolean|IAssertion<Ro, Re>):void
+
+  deny<Ro extends IRole, Re extends IResource> (role?:string, resource?:string, actions?:string[], assertion?:boolean|IAssertion<Ro, Re>):void
+
+  query(role:IRole, resource:IResource, action:string, done:IQueryCallback):void
+
+}
 
