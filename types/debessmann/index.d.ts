22c9625421f5a594094f5586f1d141024e4109ee
vladlv <vladislavs.korehovs@ideaportriga.lv>
2017-04-11 17:17:50 +0300

    fixing lint

--- types/debessmann/index.d.ts
+++ types/debessmann/index.d.ts
@@ -13,13 +13,13 @@ export class Event {
     id: string;
     timestamp: string;
     payload: any;
-    headers: { [key:string]:string; };
-    metrics: { [key:string]:number };
+    headers: { [key: string]: string; };
+    metrics: { [key: string]: number; };
 }
 
 export class DM {
-    init(endpoint:string, authkey:string): void ;
-    send(data:Event): void ;
+    init(endpoint: string, authkey: string): void;
+    send(data: Event): void;
 }
 
 declare global {
