6190a1e37e7a1672a237bb1688d2bc3dc3e1149c
Bartosz Szewczyk <bszewczyk91@gmail.com>
2017-04-13 14:52:51 +0200

    Fixed ref definition in react-notifcation-system

--- types/react-notification-system/index.d.ts
+++ types/react-notification-system/index.d.ts
@@ -1,6 +1,6 @@
 // Type definitions for React Notification System 0.2
 // Project: https://www.npmjs.com/package/react-notification-system
-// Definitions by: Giedrius Grabauskas <https://github.com/GiedriusGrabauskas>, Deividas Bakanas <https://github.com/DeividasBakanas>, Karol Janyst <https://github.com/LKay>
+// Definitions by: Giedrius Grabauskas <https://github.com/GiedriusGrabauskas>, Deividas Bakanas <https://github.com/DeividasBakanas>, Karol Janyst <https://github.com/LKay>, Bartosz Szewczyk <https://github.com/sztobar>
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
 // TypeScript Version: 2.1
 
@@ -8,7 +8,7 @@ import * as React from "react";
 
 declare namespace NotificationSystem {
 
-    export interface System extends React.Component<any, any> {
+    export interface System extends React.Component<Attributes, State> {
         addNotification(notification: Notification): Notification;
         removeNotification(uidOrNotification: number | string | Notification): void;
         clearNotifications(): void;
@@ -70,9 +70,13 @@ declare namespace NotificationSystem {
 
     export interface Attributes {
         noAnimation?: boolean;
-        ref?: string;
+        ref?: string | ((instance: System) => any);
         style?: Style | boolean;
         allowHTML?: boolean;
+    }
+
+    export interface State {
+        notifications: Notification[]
     }
 }
 
