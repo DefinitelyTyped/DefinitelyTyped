891a9a785ad9cf170f6f0d2ac8306d8961c6f431
Martynas Žilinskas <martiogalaltu@gmail.com>
2017-04-14 01:38:59 +0300

    Fixed header.

--- types/react-dom/index.d.ts
+++ types/react-dom/index.d.ts
@@ -1,6 +1,6 @@
 // Type definitions for React (react-dom) 0.14
 // Project: http://facebook.github.io/react/
-// Definitions by: Asana <https://asana.com>, AssureSign <http://www.assuresign.com>, Microsoft <https://microsoft.com>
+// Definitions by: Asana <https://asana.com>, AssureSign <http://www.assuresign.com>, Microsoft <https://microsoft.com>, MartynasZilinskas <https://github.com/MartynasZilinskas>
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
 // TypeScript Version: 2.1
 
