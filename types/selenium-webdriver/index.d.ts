650f6957422af3b98c5733fa37e930097260da92
Ben Dixon <bdixon@directs.com>
2017-04-13 17:31:32 -0500

    fixing the typings for selenium-webdriver/testing

--- types/selenium-webdriver/index.d.ts
+++ types/selenium-webdriver/index.d.ts
@@ -3,7 +3,8 @@
 // Definitions by: Bill Armstrong <https://github.com/BillArmstrong>,
 //   Yuki Kokubun <https://github.com/Kuniwak>,
 //   Craig Nishina <https://github.com/cnishina>,
-//   Simon Gellis <https://github.com/SupernaviX>
+//   Simon Gellis <https://github.com/SupernaviX>,
+//   Ben Dixon <https://github.com/bendxn>
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
 // TypeScript Version: 2.1
 
