55eccee002d273baa4991df9f85092262315c6f8
CaselIT <cfederico87@gmail.com>
2017-04-13 18:42:01 +0200

    Added missing methods and more tests

--- types/algebra.js/index.d.ts
+++ types/algebra.js/index.d.ts
@@ -2,6 +2,7 @@
 // Project: http://algebra.js.org
 // Definitions by: Federico Caselli <https://github.com/CaselIT>
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
+// TypeScript Version: 2.2
 
 declare class Term {
   coefficients: algebra.js.Fraction[];
@@ -14,27 +15,46 @@ declare class Variable {
   variable: string;
   toString(): string;
 }
+
+declare type Union = string | number | algebra.js.Fraction | Term;
+
 declare namespace algebra.js {
   function parse(input: string): Equation | Expression;
+  function toTex(input: Fraction | Expression | Equation | object | Array<Fraction | object>): string;
 
   class Equation {
     lhs: Expression;
     rhs: Expression;
+    constructor(lhs: Expression, rhs: Expression | Fraction | number)
+    solveFor(variable: string): Fraction | Fraction[] | number[];
     toString(): string;
   }
 
   class Expression {
     constants: Fraction[];
     terms: Term[];
-    simplify(): Expression;
+    constructor(variable: Union | undefined)
+    add(other: Union | Expression, simplify?: boolean): Expression;
+    divide(other: Fraction | number, simplify?: boolean): Expression;
+    eval(p: object, simplify?: boolean): Expression;
+    multiply(other: Union | Expression, simplify?: boolean): Expression;
+    pow(p: number, simplify?: boolean): Expression;
     constant(): Fraction;
+    simplify(): Expression;
+    subtract(other: Union | Expression, simplify?: boolean): Expression;
+    summation(variable: string, lower: number, upper: number, simplify?: boolean): Expression;
     toString(): string;
   }
 
   class Fraction {
     denom: number;
     numer: number;
+    constructor(num: number, denom: number)
     abs(): Fraction;
+    add(other: Fraction | number, simplify?: boolean): Fraction;
+    divide(other: Fraction | number, simplify?: boolean): Fraction;
+    multiply(other: Fraction | number, simplify?: boolean): Fraction;
+    subtract(other: Fraction | number, simplify?: boolean): Fraction;
     toString(): string;
     valueOf(): number;
   }
