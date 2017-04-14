7d503413d312a63a03cba4e2568b05f4206e6f94
Derek Rada <Dirrk@users.noreply.github.com>
2017-04-14 14:12:22 -0400

    Updated this with everything from the jsdoc

--- types/archiver/index.d.ts
+++ types/archiver/index.d.ts
@@ -5,15 +5,13 @@
 
 /* =================== USAGE ===================
 
-    import Archiver = require('archiver);
-    var archiver = Archiver.create('zip');
-    archiver.pipe(FS.createWriteStream('xxx'));
-    archiver.append(FS.createReadStream('xxx'));
-    archiver.finalize();
+	import Archiver = require('archiver);
+	var archiver = Archiver.create('zip');
+	archiver.pipe(FS.createWriteStream('xxx'));
+	archiver.append(FS.createReadStream('xxx'));
+	archiver.finalize();
 
  =============================================== */
-
-/// <reference types="node" />
 
 import * as FS from 'fs';
 import * as STREAM from 'stream';
@@ -23,44 +21,93 @@ declare function archiver(format: string, options?: archiver.Options): archiver.
 
 declare namespace archiver {
 
-    export function create(format: string, options?: Options): Archiver;
+	// allows anything to be set on the options object which could be passed to external modules
+	export interface ExternalOptions {
+		[keyName: string]: any
+	}
 
+	export interface CoreOptions extends ExternalOptions {
+		 statConcurrency?: number;
+	}
 
-    export interface nameInterface {
-        name?: string;
-    }
+	export interface TarOptions extends ExternalOptions {
+		gzip?: boolean;
+		gzipOptions?: ExternalOptions;
+	}
 
-    export interface Archiver extends STREAM.Transform {
-        append(source: STREAM.Readable | Buffer | string, name: nameInterface): void;
+	export interface JsonOptions extends ExternalOptions  {
+	}
 
-        directory(dirpath: string, destpath: nameInterface | string): void;
-        directory(dirpath: string, destpath: nameInterface | string, data: any | Function): void;
-        
-        file(filepath: string, data: EntryData): void;
-        glob(pattern: string, options: any, data: EntryData): void;
+	export interface TransformOptions  {
+		allowHalfOpen?: boolean;
+		readableObjectMode?: boolean;
+		writableObjectMode?: boolean;
+		decodeStrings?: boolean;
+		encoding?: string;
+		highWaterMark?: number;
+		objectMode?: boolean;
+	}
+	export interface ZipOptions extends ExternalOptions {
+		comment?: string;
+		forceLocalTime?: boolean;
+		forceZip64?: boolean;
+		store?: boolean;
+		zlib?: ExternalOptions;
+	}
 
-        bulk(mappings: any): void;
-        finalize(): void;
-    }
+	export type Options = CoreOptions | TarOptions | JsonOptions | TransformOptions | ZipOptions;
 
-    export interface Options {
-        statConcurrency?: number;
-    }
-    
-    export interface EntryData {
-        name: string;
-        date?: string | Date;
-        mode?: number;
-        prefix?: string;
-        stats?: FS.Stats;
-    }
-    
-    export interface ZipEntryData extends EntryData {
-        store?: boolean;
-    }
-    
-    export interface TarEntryData extends EntryData {
-    }
+	export interface module {
+		(): any;
+	}
+	export interface IFormatter extends module {
+		append(source: STREAM.Readable | Buffer | string, name: nameInterface): void;
+		finalize(): void;
+	}
+
+	export function create(format: string, options?: Options): Archiver;
+	export function registerFormat(format: string, module: IFormatter): void;
+
+	export interface nameInterface {
+		name?: string;
+	}
+
+	export interface EntryData {
+		name: string;
+		date?: string | any;
+		mode?: number;
+		prefix?: string;
+		stats?: FS.Stats;
+	}
+
+	export interface ZipEntryData extends EntryData {
+		store?: boolean;
+	}
+
+	export interface TarEntryData extends EntryData {
+	}
+
+	export type Data = EntryData | TarEntryData | ZipEntryData;
+
+	export interface Archiver extends STREAM.Transform {
+		append(source: STREAM.Readable | Buffer | string, name: nameInterface): Archiver;
+
+		directory(dirpath: string, destpath: nameInterface | string): Archiver;
+		directory(dirpath: string, destpath: nameInterface | string, data: Data): Archiver;
+
+		file(filepath: string, data: Data): Archiver;
+		glob(pattern: string, options: ExternalOptions, data: Data): Archiver;
+
+		// Deprecated
+		bulk(mappings: any): void;
+
+		setFormat(format: string): Archiver;
+		setModule(module: Function): Archiver;
+
+		pointer(): number;
+		abort(): void;
+		finalize(): void;
+	}
 }
 
 export = archiver;
