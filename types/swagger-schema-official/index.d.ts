1b2be916402938410d6625596a2c87d27e54c8d7
Ben Southgate <bsouthga@gmail.com>
2017-04-14 12:38:33 -0400

    chore: update author

--- types/swagger-schema-official/index.d.ts
+++ types/swagger-schema-official/index.d.ts
@@ -1,6 +1,6 @@
 // Type definitions for swagger-schema-official 2.0
 // Project: http://swagger.io/specification/
-// Definitions by: Mohsen Azimi <https://github.com/mohsen1>
+// Definitions by: Mohsen Azimi <https://github.com/mohsen1>, Ben Southgate <https://github.com/bsouthga>
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
 
 export interface Info {
