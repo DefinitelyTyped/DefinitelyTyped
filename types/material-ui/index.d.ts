2da37f4b7ce1a57e70316a311c45a943fbf66746
Igor Belagorudsky <theigor@gmail.com>
2017-04-14 11:07:22 -0400

    changed the version to match when fullWidth was added to FlatButton

--- types/material-ui/index.d.ts
+++ types/material-ui/index.d.ts
@@ -1,4 +1,4 @@
-// Type definitions for material-ui v0.16.1
+// Type definitions for material-ui v0.17.1
 // Project: https://github.com/callemall/material-ui
 // Definitions by: Nathan Brown <https://github.com/ngbrown>, Oliver Herrmann <https://github.com/herrmanno>, Igor Belagorudsky <https://github.com/theigor>
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
