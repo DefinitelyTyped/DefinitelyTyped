46dd8f3aef0f7520d768d3593793b371f1eac661
Dolan <dolan_miu@hotmail.com>
2017-04-15 22:09:49 +0100

    added glob options and EntryData

--- types/archiver/index.d.ts
+++ types/archiver/index.d.ts
@@ -16,32 +16,35 @@
 import * as fs from 'fs';
 import * as stream from 'stream';
 import * as express from 'express';
+import * as glob from 'glob';
 
-declare function archiver(format: archiver.Format, options?: archiver.Options): archiver.Archiver;
+declare function archiver(format: archiver.Format, options?: archiver.ArchiverOptions): archiver.Archiver;
 
 declare namespace archiver {
     type Format = 'zip' | 'tar';
-    function create(format: string, options?: Options): Archiver;
+    function create(format: string, options?: ArchiverOptions): Archiver;
 
-    interface FileParams {
+    interface EntryData {
         name?: string;
+        prefix?: string;
+        stats?: string;
     }
 
     interface Archiver extends stream.Transform {
-        append(source: stream.Readable | Buffer | string, name: FileParams): void;
+        append(source: stream.Readable | Buffer | string, name?: EntryData): void;
 
-        directory(dirpath: string, options: FileParams | string, data?: any): void;
+        directory(dirpath: string, options: EntryData | string, data?: EntryData): void;
 
         bulk(mappings: any): void;
         finalize(): void;
 
-        glob(dirpath: string): void;
-        file(filename: string, options: FileParams): void;
+        glob(pattern: string, options?: glob.IOptions, data?: EntryData): void;
+        file(filename: string, data: EntryData): void;
 
         pipe(stream: fs.WriteStream | express.Response): void;
     }
 
-    interface Options {
+    interface ArchiverOptions {
         store?: boolean;
         gzip?: boolean;
         gzipOptions?: {
