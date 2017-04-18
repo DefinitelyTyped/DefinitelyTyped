8779c5d029d0f75cdddb08865f7fd2c873ac435a
Andy <anhans@microsoft.com>
2017-04-18 13:54:27 -0700

    mithril: Apply more lint rules (#15863)

--- types/mithril/test/test-request.ts
+++ types/mithril/test/test-request.ts
@@ -1,24 +1,24 @@
-import {request} from 'mithril/request'
+import {request} from 'mithril/request';
 
 interface Result {
-	id: number
+	id: number;
 }
 
 request<Result>({method: "GET", url: "/item"}).then(result => {
-	console.log(result.id)
-})
+	console.log(result.id);
+});
 
 request<{a: string}>("/item", {method: "POST"}).then(result => {
-	console.log(result.a)
-})
+	console.log(result.a);
+});
 
 request<any>({
 	method: "GET",
 	url: "/item",
 	data: {x: "y"}
 }).then(result => {
-	console.log(result)
-})
+	console.log(result);
+});
 
 request<Result>({
 	method: "GET",
@@ -26,37 +26,37 @@ request<Result>({
 	data: 5,
 	serialize: (data: number) => "id=" + data.toString()
 }).then(result => {
-	console.log(result)
-})
+	console.log(result);
+});
 
 request<Result>('/item', {
 	method: "GET",
 	deserialize: str => JSON.parse(str) as Result
 }).then(result => {
-	console.log(result.id)
-})
+	console.log(result.id);
+});
 
 request<Result>('/id', {
 	method: "GET",
 	extract: xhr => ({id: Number(xhr.responseText)})
 }).then(result => {
-	console.log(result.id)
-})
+	console.log(result.id);
+});
 
 request<Result>('/item', {
 	config: xhr => {
-		xhr.setRequestHeader('accept', '*')
+		xhr.setRequestHeader('accept', '*');
 	},
 	headers: {"Content-Type": "application/json"},
 	background: true,
 }).then(result => {
-	console.log(result.id)
-})
+	console.log(result.id);
+});
 
 class Item {
-	identifier: number
+	identifier: number;
 	constructor(result: Result) {
-		this.identifier = result.id
+		this.identifier = result.id;
 	}
 }
 
@@ -69,5 +69,5 @@ request<Item>('/item', {
 	type: Item,
 	useBody: false
 }).then(item => {
-	console.log(item.identifier)
-})
+	console.log(item.identifier);
+});
