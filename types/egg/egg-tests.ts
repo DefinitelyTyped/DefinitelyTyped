fabc33d066c92a9186529ea895b86f25bd12a2e4
Eward Song <eward.song@gmail.com>
2017-04-19 18:52:06 +0800

    feat: add egg and egg-mock

---
+++ types/egg/egg-tests.ts
@@ -0,0 +1,35 @@
+/* tslint:disable */
+import { Controller, Service, Application } from 'egg';
+
+// controller
+class FooController extends Controller {
+  async getData() {
+    this.ctx.body = await this.service.foo.bar();
+  }
+}
+
+// add user controller and service
+declare module 'egg' {
+  export interface controller {
+    foo: FooController;
+  }
+  export interface service {
+    foo: FooService;
+  }
+}
+
+class FooService extends Service {
+  async bar() {
+    //
+    return this.config.env;
+  }
+}
+
+// router
+
+function router(app: Application) {
+  const controller = app.controller;
+  app.get('/foo', controller.foo.getData);
+  app.post('/', controller.foo.getData);
+}
+/* tslint:enable*/
