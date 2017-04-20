c59b52e585596333be352ae4e94ecbe5f7e19a17
efokschaner <eddyaod@gmail.com>
2017-04-20 07:02:52 -0700

    (Hopefully) Remove test dependencies from npm package (#16013)

--- types/three/test/references.d.ts
+++
@@ -1,9 +0,0 @@
-// References used by tests
-// Includes using THREE as a global namespace rather than as a module
-
-/// <reference types="dat-gui" />
-/// <reference types="stats.js" />
-/// <reference types="tween.js" />
-
-/// <reference path="../index.d.ts" />
-/// <reference path="../detector.d.ts" />
