849737d51851d210cbd216bd36fcc7334037002f
cwmoo740 <cwmoo740@gmail.com>
2017-04-20 17:01:41 -0400

    downloadjs inline types, remove unnecessary toString

--- types/downloadjs/downloadjs-tests.ts
+++ types/downloadjs/downloadjs-tests.ts
@@ -7,13 +7,6 @@ download(new Blob(['hello world']), 'dlTextBlob.txt', 'text/plain');
 
 download('/robots.txt');
 
-const str = 'hello world';
-const arr = new Uint8Array(str.length);
-str.split('').forEach((a, b) => {
-    arr[b] = a.charCodeAt(0);
-});
-
-download(arr, 'textUInt8Array.txt', 'text/plain');
 download(document.documentElement.outerHTML, 'dlHTML.html', 'text/html');
 download(new Blob(['hello world'.bold()]), 'dlHtmlBlob.html', 'text/html');
 download('/diff6.png');
--- types/downloadjs/index.d.ts
+++ types/downloadjs/index.d.ts
@@ -5,11 +5,6 @@
 // TypeScript Version: 2.2
 
 declare namespace download {
-    type Data = string | File | Blob;
-    interface Stringable {
-        toString(): string;
-    }
 }
-declare function download(data: download.Data, filename?: string, mimeType?: string): void;
-declare function download<T extends download.Stringable>(data: T, filename?: string, mimeType?: string): void;
+declare function download(data: string | File | Blob, filename?: string, mimeType?: string): void;
 export = download;
