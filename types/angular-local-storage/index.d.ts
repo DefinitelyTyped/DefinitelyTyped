8278164cf097c9aa041e287bdd123761f7885484
Radosław Krzepkowski <krzepa@gmail.com>
2017-04-21 11:06:34 +0200

    updates version info to 0.1.6

--- types/angular-local-storage/index.d.ts
+++ types/angular-local-storage/index.d.ts
@@ -1,4 +1,4 @@
-// Type definitions for angular-local-storage v0.1.5
+// Type definitions for angular-local-storage v0.1.6
 // Project: https://github.com/grevory/angular-local-storage
 // Definitions by: Ken Fukuyama <https://github.com/kenfdev>
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
