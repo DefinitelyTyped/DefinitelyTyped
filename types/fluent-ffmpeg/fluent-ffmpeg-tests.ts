e6fd3c607171d68b3765861971f0595dceaf3fa7
丁偉哲 <d.weizhe@gmail.com>
2017-04-21 11:40:59 +0800

    tslint: one-variable-per-declaration

--- types/fluent-ffmpeg/fluent-ffmpeg-tests.ts
+++ types/fluent-ffmpeg/fluent-ffmpeg-tests.ts
@@ -1,5 +1,7 @@
 import * as ffmpeg from "fluent-ffmpeg";
 
-let source: string, format: string, output: string;
+let source: string;
+let format: string;
+let output: string;
 
 ffmpeg({ source }).format(format).save(output);
