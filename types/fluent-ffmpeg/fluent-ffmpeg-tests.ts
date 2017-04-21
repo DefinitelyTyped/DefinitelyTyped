4a019293a73322e46c239caed17aad5821db3bc4
丁偉哲 <d.weizhe@gmail.com>
2017-04-21 11:28:08 +0800

    tslint: semicolon

--- types/fluent-ffmpeg/fluent-ffmpeg-tests.ts
+++ types/fluent-ffmpeg/fluent-ffmpeg-tests.ts
@@ -1,5 +1,5 @@
 import * as ffmpeg from "fluent-ffmpeg";
 
-let source: string, format: string, output: string
+let source: string, format: string, output: string;
 
-ffmpeg({ source: source }).format(format).save(output)
+ffmpeg({ source: source }).format(format).save(output);
