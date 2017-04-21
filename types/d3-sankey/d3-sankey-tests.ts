ba3d406a0e1df9c705228c615a90de6680f66cbb
Tom Wanzek <tomwanzek@gmail.com>
2017-04-21 00:40:01 -0400

    d3-sankey: *  Initial incomplete draft of definitions (see TODOs) In particular issue with circular definitions given the source code approach to mutating nodes and links. * Added tsconfig and tslint.

---
+++ types/d3-sankey/d3-sankey-tests.ts
@@ -0,0 +1,4 @@
+import * as d3Sankey from 'd3-sankey';
+import {select, Selection} from 'd3-selection';
+
+// TODO: include shape tests
---
+++ types/d3-sankey/index.d.ts
@@ -0,0 +1,155 @@
+// Type definitions for D3JS d3-sankey module 0.4
+// Project: https://github.com/d3/d3-sankey/
+// Definitions by: Tom Wanzek <https://github.com/tomwanzek>
+// Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
+
+// Last module patch version validated against: 0.4.2
+
+// TODO: Find robust approach to defining SankeyNode and SankeyLink given the circular definitons inherent in the JS implementation
+// TODO: Definitions are based on actural JS code, as d3-sankey API documentation misses key methods and refers to the use of accessor functions, when
+// the source code does not support it. API documentation should be clarified as a separate step.
+
+export interface SankeyNode {
+    /**
+     * Array of links which have this node as source
+     */
+    sourceLinks?: SankeyLink[];
+    /**
+     * Array of links which have this node as target
+     */
+    targetLinks?: SankeyLink[];
+    /**
+     * Node value calculated by Sankey Layout Generator based on values of incoming and outgoing links
+     */
+    value?: number;
+    /**
+     * Node horizontal position calculated by Sankey layout generator
+     */
+    x?: number;
+    /**
+     * Node width calculated by Sankey layout generator
+     */
+    dx?: number;
+    /**
+     * Node vertical position (depth) calculated by Sankey layout generator
+     */
+    y?: number;
+    /**
+     * Node height (vertical extent based on node value) calculated by Sankey layout generator
+     */
+    dy?: number;
+}
+
+export interface SankeyLink<N extends SankeyNode> {
+    /**
+     * Source node of the link. For convenience, when initializing a Sankey layout, source may be the index of the source node in the nodes array
+     * passed into the Sankey layout generator. Once the layout(...) method is invoked, the numeric index will be replaced with the corresponding
+     * source node object.
+     */
+    source: number | N;
+    /**
+     * Target node of the link. For convenience, when initializing a Sankey layout, target may be the index of the target node in the nodes array
+     * passed into the Sankey layout generator. Once the layout(...) method is invoked, the numeric index will be replaced with the corresponding
+     * target node object.
+     */
+    target: number | N;
+    /**
+     * Value of the link
+     */
+    value: number;
+    /**
+     * Link width calculated by Sankey layout generator
+     */
+    dy?: number;
+    /**
+     * Vertical starting position of the link (at source node) calculated by Sankey layout generator
+     */
+    sy?: number;
+    /**
+     * Vertical end position of the link (at target node) calculated by Sankey layout generator
+     */
+    ty?: number;
+
+}
+
+export interface SankeyLinkPathGenerator<L extends SankeyLink> {
+    /**
+     * Return svg path string for a given link.
+     *
+     * @param link A Sankey diagram link, for which the layout has already been calculated.
+     */
+    (link: L): string;
+    /**
+     * Returns the current curvature used to calculate svg paths for links.
+     * The default curvature is 0.5.
+     */
+    curvature():number;
+    /**
+     * Set the curvature used to calculate svg paths for links and return the updated link path generator.
+     *
+     * @param curvature Curvature to be used when calculating svg paths for links. The default curvature is 0.5.
+     */
+    curvature(curvature: number): this;
+}
+
+export interface SankeyLayout<N extends SankeyNode, L extends SankeyLink> {
+    /**
+     * Return the current width of a node in pixels, which defaults to 24.
+     */
+    nodeWidth(): number;
+    /**
+     * Set the node width to the specified number and return this Sankey layout generator.
+     *
+     * @param width Width of node in pixels, which defaults to 24.
+     */
+    nodeWidth(width: number): this;
+
+    /**
+     * Return the current node padding in pixels, which defaults to 8.
+     *
+     * Node padding refers to the vertical space between nodes which occupy the same horizontal space.
+     */
+    nodePadding(): number;
+    /**
+     * Set the node padding to the specified number and return this Sankey layout generator.
+     *
+     * Node padding refers to the vertical space between nodes which occupy the same horizontal space.
+     *
+     * @param padding Node padding in pixels, which defaults to 8.
+     */
+    nodePadding(padding: number): this;
+
+    nodes(): () => N[];
+    nodes(nodes: N[]): this;
+
+    links(): () => L[];
+    links(links: L[]): this;
+
+    layout(iterations?: number):this;
+
+    /**
+     * Recalculate the depth of links and return this Sankey layout generator.
+     * Primarily used when a node is moved vertically.
+     */
+    relayout():this;
+
+    /**
+     * Get the current size of the layout in pixels. The size is a two element array of [width, height] which defaults to [1, 1].
+     */
+    size(): [number, number];
+    /**
+     * Set the size of the layout in pixels and return this Sankey layout generator.
+     *
+     * @param size A two element array of [width, height] in pixels which defaults to [1, 1].
+     */
+    size(size: [number, number]):this;
+
+    /**
+     * Obtain an svg path generator for the links of the calculated Sankey diagram layout.
+     * By default the path  generator uses a curvature of 0.5.
+     */
+    link():SankeyLinkPathGenerator<L>;
+
+}
+
+export function sankey<N extends SankeyNode, L extends SankeyLink>(): SankeyLayout<N, L>;
