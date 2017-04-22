4d1ff532702efff600f67210097f9303bc8bf419
Nicolas Voigt <nicolas_voigt@outlook.com>
2017-04-22 18:35:27 +0200

    new typings

--- types/bonjour/bonjour-tests.ts
+++ types/bonjour/bonjour-tests.ts
@@ -1,20 +1,19 @@
 import * as bonjour from 'bonjour';
 
-var bonjourOptions: bonjour.BonjourOptions;
-var bonjourInstance: bonjour.Bonjour;
+let bonjourOptions: bonjour.BonjourOptions;
+let bonjourInstance: bonjour.Bonjour;
 
-var serviceOptions: bonjour.ServiceOptions;
-var service: bonjour.Service;
+let serviceOptions: bonjour.ServiceOptions;
+let service: bonjour.Service;
 
-var browserOptions: bonjour.BrowserOptions;
-var browser: bonjour.Browser;
+let browserOptions: bonjour.BrowserOptions;
+let browser: bonjour.Browser;
 
-bonjourOptions = { interface: '192.168.1.1', port: 5353 };
-bonjourInstance = new bonjour.Bonjour(bonjourOptions);
+bonjourOptions = { interface: '192.168.0.80', port: 5353 };
+bonjourInstance = bonjour(bonjourOptions);
 
 serviceOptions = { name: 'My Web Server', type: 'http', port: 3000 };
 service = bonjourInstance.publish(serviceOptions);
 
 browserOptions = { protocol: 'tcp', type: 'http' };
-browser = bonjour.find(browserOptions);
-
+browser = bonjourInstance.find(browserOptions);
