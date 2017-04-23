ca7130dfb120b8db9edcbf29979fd3133e24ff70
Jan Wolf <mail@jan-wolf.de>
2017-04-23 22:39:49 +0200

    no message

---
+++ types/ably/auto-sni/index.d.ts
@@ -0,0 +1,10 @@
+// Type definitions for auto-sni v2.1.1
+// Project: https://www.npmjs.com/package/auto-sni
+// Definitions by: Jan Wolf <https://github.com/janwo>
+// Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
+
+declare namespace createServer {
+
+}
+declare function createServer(opts: any, app?: any): any;
+export = createServer;
