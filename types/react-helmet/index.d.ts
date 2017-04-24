8e2684f3fce99d2fdd06d6d49468cc872a44be3f
Kanchalai Tanglertsampan <yuisu@microsoft.com>
2017-04-24 14:47:56 -0700

    update header

--- types/react-helmet/index.d.ts
+++ types/react-helmet/index.d.ts
@@ -1,6 +1,6 @@
 // Type definitions for react-helmet 5.0
 // Project: https://github.com/nfl/react-helmet
-// Definitions by: Evan Bremer <https://github.com/evanbb>, Isman Usoh <https://github.com/isman-usoh>, François Nguyen <https://github.com/lith-light-g>, Kok Sam <https://github.com/sammkj>
+// Definitions by: Evan Bremer <https://github.com/evanbb>, Isman Usoh <https://github.com/isman-usoh>, François Nguyen <https://github.com/lith-light-g>, Kok Sam <https://github.com/sammkj>, Yui T. <https://github.com/yuit>
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
 // TypeScript Version: 2.1
 
