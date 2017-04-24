deb8de162746a13844cd53b79839afdb2579cba8
Guo Yunhe <guoyunhebrave@gmail.com>
2017-04-24 20:03:59 +0300

    js-search: fix project url

--- types/js-search/index.d.ts
+++ types/js-search/index.d.ts
@@ -1,5 +1,5 @@
 // Type definitions for js-search v1.4.0
-// Project: https://github.com/bvaughn/js-search/releases
+// Project: https://github.com/bvaughn/js-search
 // Definitions by: Guo Yunhe <https://github.com/guoyunhe>
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
 
