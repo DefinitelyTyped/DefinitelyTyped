8ef1c7265ce820c4d28ecfed3d592ae8f4d5ecf8
Cyril Schumacher <cyril_schumacher@live.fr>
2017-04-25 17:54:39 +0200

    Fix tslint errors.

--- types/throng/index.d.ts
+++ types/throng/index.d.ts
@@ -1,21 +1,21 @@
-// Type definitions for throng 4.0.0
+// Type definitions for throng 4.0
 // Project: https://github.com/hunterloftis/throng
 // Definitions by: Cyril Schumacher <https://github.com/cyrilschumacher/>
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
 
-declare module "throng" {
-    namespace throng {
-        type WorkerCallback = (id: number) => void;
+declare function throng(options: throng.WorkerCallback | throng.Options | number, startFunction?: throng.WorkerCallback): never;
+declare namespace throng {
+    type MasterCallback = () => void;
+    type WorkerCallback = (id: number) => void;
 
-        export interface Options {
-            grace?: number;
-            lifetime?: number;
-            master: () => void;
-            start: WorkerCallback;
-            workers: number;
-        }
+    interface Options {
+        grace?: number;
+        lifetime?: number;
+        master: MasterCallback;
+        start: WorkerCallback;
+        workers: number;
     }
-
-    function throng(options: throng.WorkerCallback | throng.Options | number, startFunction?: throng.WorkerCallback): never;
-    export = throng;
 }
+
+export = throng;
+export as namespace throng;
