9c791780aed75dd132d74f7f69aa65c0b078eba9
Ilia Choly <icholy@accipiterradar.com>
2017-04-25 14:25:43 -0400

    Add header

--- types/strophe/muc.d.ts
+++ types/strophe/muc.d.ts
@@ -1,3 +1,7 @@
+// Type definitions for strophe.muc.js v1.2.0
+// Project: https://github.com/metajack/strophejs-plugins
+// Definitions by: Ilia Choly <https://github.com/icholy/>
+// Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
 
 declare namespace Strophe {
 
