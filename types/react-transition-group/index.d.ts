eaa708a9f4b6f6b89804135e1651ee9577290b2a
Karol Janyst <lapkom@gmail.com>
2017-04-25 11:37:07 +0900

    Add definitions for react-transition-group

---
+++ types/react-transition-group/index.d.ts
@@ -0,0 +1,32 @@
+// Type definitions for react-transition-group 1.1
+// Project: https://github.com/reactjs/react-transition-group
+// Definitions by: Asana <https://asana.com>,
+//                 AssureSign <http://www.assuresign.com>,
+//                 Microsoft <https://microsoft.com>,
+//                 Karol Janyst <https://github.com/LKay>
+// Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
+// TypeScript Version: 2.1
+
+import { HTMLAttributes, ReactElement, ReactType } from "react";
+
+export interface HTMLTransitionGroupProps<T> extends HTMLAttributes<T> {
+    component?: ReactType;
+    childFactory?(child: ReactElement<any>): ReactElement<any>;
+}
+
+import * as TransitionGroup from "./TransitionGroup";
+export {
+    TransitionGroupProps,
+    TransitionGroupChildLifecycle
+} from "./TransitionGroup";
+
+import * as CSSTransitionGroup from "./CSSTransitionGroup";
+export {
+    CSSTransitionGroupProps,
+    CSSTransitionGroupTransitionName
+} from "./CSSTransitionGroup";
+
+export {
+    TransitionGroup,
+    CSSTransitionGroup
+};
