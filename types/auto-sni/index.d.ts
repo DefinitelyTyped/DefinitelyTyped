408fb4d5aa31e6ffa9de83ee7272b841e95de1ed
Jan Wolf <mail@jan-wolf.de>
2017-04-25 18:41:16 +0200

    Added more specific types

--- types/auto-sni/index.d.ts
+++ types/auto-sni/index.d.ts
@@ -3,8 +3,23 @@
 // Definitions by: Jan Wolf <https://github.com/janwo>
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
 
+/// <reference types="node" />
+
+import {Server} from "https";
+
 declare namespace createServer {
 
 }
-declare function createServer(opts: any, app?: any): any;
+
+declare function createServer(opts: {
+	email: string,
+	agreeTos: boolean,
+	domains: Array<string|string[]>,
+	ports?: {
+		http?: number,
+		https?: number
+	},
+	debug?: boolean
+}, app?: any): Server;
+
 export = createServer;
