d07ac371fa5ca1999e6bf62f31b8c85753d48f22
Akshar Patel <akshar.patel.47@gmail.com>
2017-04-26 01:26:18 +0530

    node.js: Add type guards as return types of util methods (#16011)
    * node.js: Add type guards for return types of util methods
    
    * use object instead of Object in isObject typeguard
    
    * use es2016 as target for Node 7 types
    
    Node 7 supports almost 99% of ES2015 and 100% of ES2016. Hence, its safe
    to use the target of es2016 in tsconfig.json
    
    * remove typeguard from isObject function
    
    * rename Symbol to symbol
    
    * remove es2016 target in tsconfig.json

--- types/node/index.d.ts
+++ types/node/index.d.ts
@@ -3650,23 +3650,23 @@ declare module "util" {
     export function log(string: string): void;
     export function inspect(object: any, showHidden?: boolean, depth?: number, color?: boolean): string;
     export function inspect(object: any, options: InspectOptions): string;
-    export function isArray(object: any): boolean;
-    export function isRegExp(object: any): boolean;
-    export function isDate(object: any): boolean;
-    export function isError(object: any): boolean;
+    export function isArray(object: any): object is any[];
+    export function isRegExp(object: any): object is RegExp;
+    export function isDate(object: any): object is Date;
+    export function isError(object: any): object is Error;
     export function inherits(constructor: any, superConstructor: any): void;
     export function debuglog(key: string): (msg: string, ...param: any[]) => void;
-    export function isBoolean(object: any): boolean;
-    export function isBuffer(object: any): boolean;
+    export function isBoolean(object: any): object is boolean;
+    export function isBuffer(object: any): object is Buffer;
     export function isFunction(object: any): boolean;
-    export function isNull(object: any): boolean;
-    export function isNullOrUndefined(object: any): boolean;
-    export function isNumber(object: any): boolean;
+    export function isNull(object: any): object is null;
+    export function isNullOrUndefined(object: any): object is null | undefined;
+    export function isNumber(object: any): object is number;
     export function isObject(object: any): boolean;
     export function isPrimitive(object: any): boolean;
-    export function isString(object: any): boolean;
-    export function isSymbol(object: any): boolean;
-    export function isUndefined(object: any): boolean;
+    export function isString(object: any): object is string;
+    export function isSymbol(object: any): object is symbol;
+    export function isUndefined(object: any): object is undefined;
     export function deprecate(fn: Function, message: string): Function;
 }
 
