d3666dda8f79b0e7c52d7e2cfc0661b76ab21071
David Fox <djfox14@gmail.com>
2017-04-24 20:06:38 -0500

    Use correct response typings
    Use `request.RequestResponse` instead of `any`

--- types/request-promise-native/index.d.ts
+++ types/request-promise-native/index.d.ts
@@ -8,8 +8,8 @@ import http = require('http');
 
 declare namespace requestPromise {
     interface RequestPromise extends request.Request {
-        then<TResult>(onfulfilled?: (value: any) => TResult | PromiseLike<TResult>, onrejected?: (reason: any) => TResult | PromiseLike<TResult>): Promise<TResult>;
-        then<TResult>(onfulfilled?: (value: any) => TResult | PromiseLike<TResult>, onrejected?: (reason: any) => void): Promise<TResult>;
+        then<TResult>(onfulfilled?: (value: request.RequestResponse) => TResult | PromiseLike<TResult>, onrejected?: (reason: any) => TResult | PromiseLike<TResult>): Promise<TResult>;
+        then<TResult>(onfulfilled?: (value: request.RequestResponse) => TResult | PromiseLike<TResult>, onrejected?: (reason: any) => void): Promise<TResult>;
         catch(onrejected?: (reason: any) => any | PromiseLike<any>): Promise<any>;
         catch(onrejected?: (reason: any) => void): Promise<any>;
         promise(): Promise<any>;
