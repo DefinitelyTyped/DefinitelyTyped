b88eab1ec430e9fcb62e7095efb15d87d7555fdf
inuscript <inuscript@gmail.com>
2017-04-26 08:40:36 +0900

    Add regression test

---
+++ types/react-router/test/EmptyObjectProps.tsx
@@ -0,0 +1,35 @@
+import * as React from 'react'
+import {
+  BrowserRouter as Router,
+  Route
+} from 'react-router-dom'
+import { connect } from "react-redux"
+
+const EmptyPropsSfc = (props: {}) => (
+  <div>
+    <h2>Empty Props SFC</h2>
+  </div>
+)
+
+class EmptyPropsClassComponent extends React.Component<{}, void> {
+  render() {
+    return <div>
+      <h2>Empty Props Class</h2>
+    </div>
+  }
+}
+
+const ConnectedComponent = connect(() => ({}), {})((props) => {
+  return <div>
+    <h2>Empty Props Connected Class</h2>
+  </div>
+})
+
+const Routing = () => (
+  // Cannot pass this component.
+  <Router>
+    <Route path="/sfc" component={EmptyPropsSfc} />
+    <Route path="/class" component={EmptyPropsClassComponent} />
+    <Route path="/connected" component={ConnectedComponent} />
+  </Router>
+)
