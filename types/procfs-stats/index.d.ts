e69e8ea152a4acd9a6e55729b9c1e94855cca530
Cyril Schumacher <cyril_schumacher@live.fr>
2017-04-25 18:33:57 +0200

    Update definition header.

--- types/procfs-stats/index.d.ts
+++ types/procfs-stats/index.d.ts
@@ -1,4 +1,4 @@
-// Type definitions for node-procfs-stats 1.0
+// Type definitions for procfs-stats 1.0
 // Project: https://github.com/soldair/node-procfs-stats
 // Definitions by: Cyril Schumacher <https://github.com/cyrilschumacher/>
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
