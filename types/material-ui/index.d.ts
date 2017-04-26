8a3d14012101170333b590575b440c2fa7e9b3de
Aurélien Allienne <aurelien.allienne@gmail.com>
2017-04-26 19:27:42 +0200

    Move className from SharedEnhancedButtonProps to FlatButtonProps and increase minor version (#16142)

--- types/material-ui/index.d.ts
+++ types/material-ui/index.d.ts
@@ -1,6 +1,6 @@
-// Type definitions for material-ui v0.17.4
+// Type definitions for material-ui v0.17.5
 // Project: https://github.com/callemall/material-ui
-// Definitions by: Nathan Brown <https://github.com/ngbrown>, Igor Belagorudsky <https://github.com/theigor>, Ali Taheri Moghaddar <https://github.com/alitaheri>, Oliver Herrmann <https://github.com/herrmanno>, Daniel Roth <https://github.com/DaIgeb>
+// Definitions by: Nathan Brown <https://github.com/ngbrown>, Igor Belagorudsky <https://github.com/theigor>, Ali Taheri Moghaddar <https://github.com/alitaheri>, Oliver Herrmann <https://github.com/herrmanno>, Daniel Roth <https://github.com/DaIgeb>, Aurelién Allienne <https://github.com/allienna>
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
 // TypeScript Version: 2.2
 
@@ -627,7 +627,6 @@ declare namespace __MaterialUI {
     // non generally overridden elements of EnhancedButton
     interface SharedEnhancedButtonProps<T> {
         centerRipple?: boolean;
-        className?: string;
         disableFocusRipple?: boolean;
         disableKeyboardFocus?: boolean;
         disableTouchRipple?: boolean;
@@ -662,6 +661,7 @@ declare namespace __MaterialUI {
     interface FlatButtonProps extends React.DOMAttributes<{}>, SharedEnhancedButtonProps<FlatButton> {
         // <EnhancedButton/> is the element that get the 'other' properties
         backgroundColor?: string;
+        className?: string;
         disabled?: boolean;
         fullWidth?: boolean;
         hoverColor?: string;
