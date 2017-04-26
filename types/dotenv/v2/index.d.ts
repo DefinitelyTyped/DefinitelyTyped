9667b739e2137c08121c5e78dd7ee3fa7c1180e4
Michael Strobel <kryops@users.noreply.github.com>
2017-04-27 02:11:08 +0800

    [dotenv] Add types for v4.0.0 (#16087)

--- types/dotenv/v2/index.d.ts
+++ types/dotenv/v2/index.d.ts
@@ -1,4 +1,4 @@
-// Type definitions for dotenv 4.0
+// Type definitions for dotenv 2.0
 // Project: https://github.com/motdotla/dotenv
 // Definitions by: Jussi Kinnula <https://github.com/jussikinnula/>, Borek Bernard <https://github.com/borekb>, Eric Naeseth <https://github.com/enaeseth>
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
@@ -14,15 +14,18 @@ export function parse(src: string | Buffer): {[name: string]: string};
  * Loads `.env` into `process.env`.
  *
  * @param options
- * @return Object Object with either
- * - "parsed" containing the parsed keys and values or
- * - "error" containing an Error object
- *
- * Example: 'KEY=value' becomes { parsed: { KEY: 'value' } }
+ * @return Object Object with the parsed keys and values, e.g., 'KEY=value' becomes { KEY: 'value' }
  */
-export function config(options?: DotenvOptions): DotenvResult;
+export function config(options?: DotenvOptions): {[name: string]: string} | false;
 
 export interface DotenvOptions {
+    /**
+     * Dotenv outputs a warning to your console if missing a .env file. Suppress this warning using silent.
+     *
+     * @default false
+     */
+    silent?: boolean;
+
     /**
      * You can specify a custom path if your file containing environment variables is named or located differently.
      *
@@ -36,11 +39,4 @@ export interface DotenvOptions {
      * @default 'utf8'
      */
     encoding?: string;
-}
-
-export interface DotenvResult {
-    error?: Error;
-    parsed?: {
-        [name: string]: string;
-    };
 }
