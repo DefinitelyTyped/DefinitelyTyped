86d6ae08baa3f72397cfc503fb184c252ba04ca6
Martynas Kunigėlis <martynas@ignitenet.com>
2017-04-27 00:57:43 +0300

    knex: added declarations for with(), withRaw() and withWrapped() (#15966)
    Add an optional extended description…

--- types/knex/knex-tests.ts
+++ types/knex/knex-tests.ts
@@ -315,6 +315,29 @@ knex('accounts').where('activated', false).delete();
 knex('accounts').where('activated', false).delete('id');
 knex('accounts').where('activated', false).delete(['id', 'title']);
 
+knex.with('old_books', function(qb) {
+  qb.select('*').from('books').where('published_date', '<', 1970);
+}).select('*').from('old_books');
+
+knex.with('new_books', knex.raw('select * from books where published_date >= 2016'))
+  .select('*').from('new_books');
+
+knex.with('new_books', 'select * from books where published_date >= :year', { year: 2016 })
+  .select('*').from('new_books');
+
+knex.with('new_books', 'select * from books where published_date >= ?', [2016])
+  .select('*').from('new_books');
+
+knex.withRaw('recent_books', 'select * from books where published_date >= :year', { year: 2013 })
+  .select('*').from('recent_books');
+
+knex.withRaw('recent_books', knex.raw('select * from books where published_date >= ?', [2013]))
+  .select('*').from('recent_books');
+
+knex.withWrapped("antique_books", function (qb) {
+  qb.select('*').from('books').where('published_date', '<', 1899);
+}).select('*').from('antique_books');
+
 var someExternalMethod: Function;
 
 knex.transaction(function(trx) {
