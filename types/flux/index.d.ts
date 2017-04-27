65476d005e4acf2a12f81c5e9decd574540d2bf8
Giedrius Grabauskas <giedrius@quatrodev.com>
2017-04-27 20:21:41 +0300

    Typescript version changed to 2.2.

--- types/flux/index.d.ts
+++ types/flux/index.d.ts
@@ -3,6 +3,7 @@
 // Definitions by: Steve Baker <https://github.com/stkb/>
 //                 Giedrius Grabauskas <https://github.com/GiedriusGrabauskas/>
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
+// TypeScript Version: 2.2
 
 import * as Dispatcher from "./lib/Dispatcher";
 
