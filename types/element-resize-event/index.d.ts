1673622f056bd78b2ee33632f255bc23bc0856c8
Pierre-Luc Gregoire <pierre-luc.gregoire@ca.thalesgroup.com>
2017-04-28 10:28:50 -0400

    update library version

--- types/element-resize-event/index.d.ts
+++ types/element-resize-event/index.d.ts
@@ -1,4 +1,4 @@
-// Type definitions for element-resize-event 1.0.1
+// Type definitions for element-resize-event 2.0.9
 // Project: https://github.com/KyleAMathews/element-resize-event
 // Definitions by: Rogier Schouten <https://github.com/rogierschouten>, Pierre-Luc Gregoire <https://github.com/plgregoire>
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
