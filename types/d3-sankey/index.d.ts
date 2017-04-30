3388dae5298ded1811a01b9b8f2ecbc68f9bea9a
Tom Wanzek <tomwanzek@gmail.com>
2017-04-30 13:45:00 -0400

    Add Co-Maintainer

--- types/d3-sankey/index.d.ts
+++ types/d3-sankey/index.d.ts
@@ -1,6 +1,6 @@
 // Type definitions for D3JS d3-sankey module 0.4
 // Project: https://github.com/d3/d3-sankey/
-// Definitions by: Tom Wanzek <https://github.com/tomwanzek>
+// Definitions by: Tom Wanzek <https://github.com/tomwanzek>, Alex Ford <https://github.com/gustavderdrache>
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
 
 // Last module patch version validated against: 0.4.2
