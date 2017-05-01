0186359df83f8027f0a145b79c5600150b7b68d3
Iker Perez Brunelli <yo@ikerperez.es>
2017-05-01 02:44:43 +0200

    Make interfaces and types available to import

--- types/snekfetch/index.d.ts
+++ types/snekfetch/index.d.ts
@@ -9,7 +9,7 @@
 import { ClientRequest } from 'http';
 import { Readable } from 'stream';
 
-declare namespace snekfetch {
+declare namespace Snekfetch {
     interface SnekfetchOptions {
         headers?: { [key: string]: any };
         data?: any;
@@ -59,70 +59,68 @@ declare namespace snekfetch {
         | 'UNLOCK'
         | 'UNSUBSCRIBE'
         | 'BREW';
-
-    class Snekfetch extends Readable {
-        static METHODS: methods[];
-        static version: string;
-
-        data: any;
-        request: ClientRequest;
-        readonly response: Result | null;
-
-        constructor(method: methods, url: string, opts?: SnekfetchOptions);
-
-        static acl(url: string): Snekfetch;
-        static bind(url: string): Snekfetch;
-        static checkout(url: string): Snekfetch;
-        static connect(url: string): Snekfetch;
-        static copy(url: string): Snekfetch;
-        static delete(url: string): Snekfetch;
-        static get(url: string): Snekfetch;
-        static head(url: string): Snekfetch;
-        static link(url: string): Snekfetch;
-        static lock(url: string): Snekfetch;
-        static msearch(url: string): Snekfetch;
-        static merge(url: string): Snekfetch;
-        static mkactivity(url: string): Snekfetch;
-        static mkcalendar(url: string): Snekfetch;
-        static mkcol(url: string): Snekfetch;
-        static move(url: string): Snekfetch;
-        static notify(url: string): Snekfetch;
-        static options(url: string): Snekfetch;
-        static patch(url: string): Snekfetch;
-        static post(url: string): Snekfetch;
-        static propfind(url: string): Snekfetch;
-        static proppatch(url: string): Snekfetch;
-        static purge(url: string): Snekfetch;
-        static put(url: string): Snekfetch;
-        static rebind(url: string): Snekfetch;
-        static report(url: string): Snekfetch;
-        static search(url: string): Snekfetch;
-        static subscribe(url: string): Snekfetch;
-        static trace(url: string): Snekfetch;
-        static unbind(url: string): Snekfetch;
-        static unlink(url: string): Snekfetch;
-        static unlock(url: string): Snekfetch;
-        static unsubscribe(url: string): Snekfetch;
-        static brew(url: string): Snekfetch;
-
-        query(name: string, value: string): Snekfetch;
-
-        set(name: string | { [key: string]: string | string[] }, value?: string | string[]): Snekfetch;
-
-        attach(name: string, data: string | object | Buffer, filename?: string): Snekfetch;
-
-        send(data?: any): Snekfetch;
-
-        then(): Promise<Result>;
-        then<T>(resolver: (res: Result) => T, rejector?: (err: Error) => any): Promise<T>;
-
-        catch(rejector: (err: Error) => any): Promise<Result>;
-
-        end(): Promise<Result>;
-        end<T>(cb: (err: Error | null, res: Result | Error | null) => T): Promise<T>;
-    }
 }
 
-declare const sf: typeof snekfetch.Snekfetch;
+declare class Snekfetch extends Readable {
+    static METHODS: Snekfetch.methods[];
+    static version: string;
 
-export = sf;
+    data: any;
+    request: ClientRequest;
+    readonly response: Snekfetch.Result | null;
+
+    constructor(method: Snekfetch.methods, url: string, opts?: Snekfetch.SnekfetchOptions);
+
+    static acl(url: string): Snekfetch;
+    static bind(url: string): Snekfetch;
+    static checkout(url: string): Snekfetch;
+    static connect(url: string): Snekfetch;
+    static copy(url: string): Snekfetch;
+    static delete(url: string): Snekfetch;
+    static get(url: string): Snekfetch;
+    static head(url: string): Snekfetch;
+    static link(url: string): Snekfetch;
+    static lock(url: string): Snekfetch;
+    static msearch(url: string): Snekfetch;
+    static merge(url: string): Snekfetch;
+    static mkactivity(url: string): Snekfetch;
+    static mkcalendar(url: string): Snekfetch;
+    static mkcol(url: string): Snekfetch;
+    static move(url: string): Snekfetch;
+    static notify(url: string): Snekfetch;
+    static options(url: string): Snekfetch;
+    static patch(url: string): Snekfetch;
+    static post(url: string): Snekfetch;
+    static propfind(url: string): Snekfetch;
+    static proppatch(url: string): Snekfetch;
+    static purge(url: string): Snekfetch;
+    static put(url: string): Snekfetch;
+    static rebind(url: string): Snekfetch;
+    static report(url: string): Snekfetch;
+    static search(url: string): Snekfetch;
+    static subscribe(url: string): Snekfetch;
+    static trace(url: string): Snekfetch;
+    static unbind(url: string): Snekfetch;
+    static unlink(url: string): Snekfetch;
+    static unlock(url: string): Snekfetch;
+    static unsubscribe(url: string): Snekfetch;
+    static brew(url: string): Snekfetch;
+
+    query(name: string, value: string): Snekfetch;
+
+    set(name: string | { [key: string]: string | string[] }, value?: string | string[]): Snekfetch;
+
+    attach(name: string, data: string | object | Buffer, filename?: string): Snekfetch;
+
+    send(data?: any): Snekfetch;
+
+    then(): Promise<Snekfetch.Result>;
+    then<T>(resolver: (res: Snekfetch.Result) => T, rejector?: (err: Error) => any): Promise<T>;
+
+    catch(rejector: (err: Error) => any): Promise<Snekfetch.Result>;
+
+    end(): Promise<Snekfetch.Result>;
+    end<T>(cb: (err: Error | null, res: Snekfetch.Result | Error | null) => T): Promise<T>;
+}
+
+export = Snekfetch;
