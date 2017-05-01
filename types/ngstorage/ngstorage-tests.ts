40863f70ea309f0d5a9bcdc98cc22abd8e314189
Stephan Classen <st.classen@gmx.ch>
2017-05-01 15:49:16 +0200

    update ngStorage interfaces
    - remove the method StorageService.get() as it does not exist
    - add the method StorageService.$supported() which was missing
    - StorageService.$default() and StorageService.$reset() return an intersection type of its argument and StorageService
    - add the method StorageProvider.remove() which was missing
    - add the method StorageProvider.supported which was missing

--- types/ngstorage/ngstorage-tests.ts
+++ types/ngstorage/ngstorage-tests.ts
@@ -1,66 +1,81 @@
 import * as angular from "angular";
-import { ngstorage } from "ngstorage";
+import { ngStorage } from "ngstorage";
 
 let app: angular.IModule = angular.module('at', ['ngStorage']);
 
-app.controller('LocalCtrl', ($localStorage: ngstorage.StorageService) => {
-    $localStorage.$default({
-        counter: 1
-    });
+app.controller('LocalCtrl', ($localStorage: ngStorage.StorageService) => {
+    if ($localStorage.$supported()) {
+        const store: MyStore & ngStorage.StorageService = $localStorage.$default<MyStore>({
+            counter: 1
+        });
 
-    $localStorage.$reset({
-        counter: 1
-    });
+        store.$reset<MyStore>({
+            counter: 1
+        });
 
-    $localStorage.$reset();
-    $localStorage.$apply();
-    $localStorage.$sync();
+        store.$reset();
+        store.$apply();
+        store.$sync();
+    }
 });
 
-app.controller('SessionCtrl', ($sessionStorage: ngstorage.StorageService) => {
-    $sessionStorage.$default({
-        counter: 1
-    });
+app.controller('SessionCtrl', ($sessionStorage: ngStorage.StorageService) => {
+    if ($sessionStorage.$supported()) {
+        const store: MyStore & ngStorage.StorageService = $sessionStorage.$default<MyStore>({
+            counter: 1
+        });
 
-    $sessionStorage.$reset({
-        counter: 1
-    });
+        store.$reset<MyStore>({
+            counter: 1
+        });
 
-    $sessionStorage.$reset();
-    $sessionStorage.$apply();
-    $sessionStorage.$sync();
+        store.$reset();
+        store.$apply();
+        store.$sync();
+    }
 });
 
-app.config(($localStorageProvider: ngstorage.StorageProvider) => {
-    $localStorageProvider.setKeyPrefix('NewPrefix');
-    $localStorageProvider.get('MyKey');
-    $localStorageProvider.set('MyKey', { counter: 'value' });
+app.config(($localStorageProvider: ngStorage.StorageProvider) => {
+    if ($localStorageProvider.supported()) {
+        $localStorageProvider.setKeyPrefix('NewPrefix');
+        $localStorageProvider.set('MyKey', { counter: 'value' });
+        $localStorageProvider.get('MyKey');
+        $localStorageProvider.remove('MyKey');
 
-    const mySerializer = (value: any): string => {
-        return value.toString();
-    };
+        const mySerializer = (value: any): string => {
+            return value.toString();
+        };
 
-    const myDeserializer = (value: string): any => {
-        return value;
-    };
+        const myDeserializer = (value: string): any => {
+            return value;
+        };
 
-    $localStorageProvider.setSerializer(mySerializer);
-    $localStorageProvider.setDeserializer(myDeserializer);
+        $localStorageProvider.setSerializer(mySerializer);
+        $localStorageProvider.setDeserializer(myDeserializer);
+    }
 });
 
-app.config(($sessionStorageProvider: ngstorage.StorageProvider) => {
-    $sessionStorageProvider.setKeyPrefix('NewPrefix');
-    $sessionStorageProvider.get('MyKey');
-    $sessionStorageProvider.set('MyKey', { counter: 'value' });
+app.config(($sessionStorageProvider: ngStorage.StorageProvider) => {
+    if ($sessionStorageProvider.supported()) {
+        $sessionStorageProvider.setKeyPrefix('NewPrefix');
+        $sessionStorageProvider.set('MyKey', { counter: 'value' });
+        $sessionStorageProvider.get('MyKey');
+        $sessionStorageProvider.remove('MyKey');
 
-    const mySerializer = (value: any): string => {
-        return value.toString();
-    };
+        const mySerializer = (value: any): string => {
+            return value.toString();
+        };
 
-    const myDeserializer = (value: string): any => {
-        return value;
-    };
+        const myDeserializer = (value: string): any => {
+            return value;
+        };
 
-    $sessionStorageProvider.setSerializer(mySerializer);
-    $sessionStorageProvider.setDeserializer(myDeserializer);
+        $sessionStorageProvider.setSerializer(mySerializer);
+        $sessionStorageProvider.setDeserializer(myDeserializer);
+    }
 });
+
+interface MyStore {
+    counter?: number;
+    foo?: string;
+}
