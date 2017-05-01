1bc2731f8482887c6742625816fcd4274ef837d1
Budi Irawan <deerawan@gmail.com>
2017-05-02 06:44:20 +1000

    improve fallback language (#16236)
    - add def for array of string
    - add def for object list

--- types/i18next/index.d.ts
+++ types/i18next/index.d.ts
@@ -1,6 +1,6 @@
-// Type definitions for i18next v2.3.4
+// Type definitions for i18next v2.3.5
 // Project: http://i18next.com
-// Definitions by: Michael Ledin <https://github.com/mxl>
+// Definitions by: Michael Ledin <https://github.com/mxl>, Budi Irawan <https://github.com/deerawan>
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
 
 // Sources: https://github.com/i18next/i18next/
@@ -18,6 +18,12 @@ declare namespace i18n {
     interface ResourceStoreKey {
         [key: string]: any;
     }
+
+    interface FallbackLngObjList {
+        [language: string]: string[]
+    }
+
+    type FallbackLng = string | string[] | FallbackLngObjList;
 
     interface InterpolationOptions {
         escapeValue?: boolean;
@@ -41,7 +47,7 @@ declare namespace i18n {
         replace?: any;
         lng?: string;
         lngs?: string[];
-        fallbackLng?: string;
+        fallbackLng?: FallbackLng;
         ns?: string | string[];
         keySeparator?: string;
         nsSeparator?: string;
@@ -57,7 +63,7 @@ declare namespace i18n {
         debug?: boolean;
         resources?: ResourceStore;
         lng?: string;
-        fallbackLng?: string;
+        fallbackLng?: FallbackLng;
         ns?: string | string[];
         defaultNS?: string;
         fallbackNS?: string | string[];
