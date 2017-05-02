640edcd27d467bf15c962fdbf963b732743d1755
Tom Wanzek <tomwanzek@gmail.com>
2017-05-02 01:34:22 -0400

    [d3-contour] * Initial definitions * Add DOM to compilation context libraries

--- types/d3-contour/index.d.ts
+++ types/d3-contour/index.d.ts
@@ -3,3 +3,63 @@
 // Definitions by: Tom Wanzek <https://github.com/tomwanzek>
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
 
+// Last module patch version validated against: 1.1.0
+
+import { MultiPolygon } from 'geojson';
+import { ThresholdArrayGenerator, ThresholdCountGenerator } from 'd3-array';
+
+export interface ContourMultiPolygon extends MultiPolygon {
+    value: number;
+}
+
+/**
+ * A contour generator
+ */
+export interface Contours {
+    (values: number[]): ContourMultiPolygon[];
+    size(): [number, number];
+    size(size: [number, number]): this;
+    smooth():boolean;
+    smooth(smooth: boolean): this;
+    thresholds(): ThresholdCountGenerator | ThresholdArrayGenerator<number>;
+    thresholds(count: number): this;
+    thresholds(thresholds: number[]): this;
+    thresholds(thresholds: ThresholdCountGenerator): this;
+    thresholds(thresholds: ThresholdArrayGenerator<number>): this;
+}
+
+/**
+ * Construct a new contour generator with the default settings.
+ */
+export function contours(): Contours;
+
+/**
+ * A contour generator for density estimates.
+ */
+export interface ContourDensity<Datum> {
+    (data: Datum[]): ContourMultiPolygon[];
+    x(): (d: Datum) => number;
+    x(x: (d: Datum) => number): this;
+    y(): (d: Datum) => number;
+    y(y: (d: Datum) => number): this;
+    size(): [number, number];
+    size(size: [number, number]): this;
+    cellSize(): number;
+    cellSize(cellSize: number): this;
+    thresholds(): ThresholdCountGenerator | ThresholdArrayGenerator<number>;
+    thresholds(count: number): this;
+    thresholds(thresholds: number[]): this;
+    thresholds(thresholds: ThresholdCountGenerator): this;
+    thresholds(thresholds: ThresholdArrayGenerator<number>): this;
+    bandwidth(): number;
+    bandwidth(bandwidth: number): this;
+}
+
+/**
+ * Construct a new density estimator with the default settings.
+ */
+export function contourDensity(): ContourDensity<[number, number]>;
+/**
+ * Construct a new density estimator with the default settings.
+ */
+export function contourDensity<Datum>(): ContourDensity<Datum>;
