7cdf9e42d73dc438e16805087e3c7f716150ec81
Yui <yuit@users.noreply.github.com>
2017-05-02 13:13:30 -0700

    Update header of the index.d.ts file

--- types/react-leaflet/index.d.ts
+++ types/react-leaflet/index.d.ts
@@ -1,6 +1,6 @@
 // Type definitions for react-leaflet 1.1
 // Project: https://github.com/PaulLeCam/react-leaflet
-// Definitions by: Dave Leaver <https://github.com/danzel>, David Schneider <https://github.com/davschne>
+// Definitions by: Dave Leaver <https://github.com/danzel>, David Schneider <https://github.com/davschne>, Yui T. <https://github.com/yuit>
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
 // TypeScript Version: 2.3
 
