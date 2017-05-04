9dbb45df5adf181f5fb3a635daa917ee32aa7743
Fred K. Schott <fschott@google.com>
2017-05-04 11:51:37 -0700

    add version info to launchpad header

--- types/launchpad/index.d.ts
+++ types/launchpad/index.d.ts
@@ -1,4 +1,4 @@
-// Type definitions for launchpad
+// Type definitions for launchpad 0.6.0
 // Project: https://www.npmjs.com/package/launchpad
 // Definitions by: Peter Burns <https://github.com/rictic>
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
