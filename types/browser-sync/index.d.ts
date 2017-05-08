3dd7e9e4cae1b4a8ec06507eed99f6b6e5cb9788
Thomas Deinhamer <thasmo@gmail.com>
2017-05-08 23:50:48 +0200

    Add `cors` option

--- types/browser-sync/index.d.ts
+++ types/browser-sync/index.d.ts
@@ -135,6 +135,12 @@ declare namespace browserSync {
          */
         browser?: string | string[];
         /**
+         * Add HTTP access control (CORS) headers to assets served by Browsersync.
+         * Default: false
+         * Note: requires at least version 2.16.0
+         */
+        cors?: boolean;
+        /**
          * Requires an internet connection - useful for services such as Typekit as it allows you to configure
          * domains such as *.xip.io in your kit settings
          * Default: false
