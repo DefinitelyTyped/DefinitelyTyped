b5143b18190ecab15f4e3466a60132c88dce762c
Alexander James Phillips <ajp.global@gmail.com>
2017-05-08 20:31:18 +0100

    Remove unnecessary module wrapper

--- types/nes/client.d.ts
+++ types/nes/client.d.ts
@@ -3,55 +3,52 @@
 // Definitions by: Ivo Stratev <https://github.com/NoHomey>
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
 
-declare module 'nes/client' {
-
-    class Client {
-        constructor(url: string, options?: Client.ClientOptions);
-        onError: (err: any) => void;
-        onConnect: () => void;
-        onDisconnect: () => void;
-        onUpdate: (message: any) => void;
-        connect(options: Client.ClientConnectOptions, callback: (err?: any) => void): void;
-        connect(callback: (err?: any) => void): void;
-        disconnect(): void;
-        id: any;  // can be `null | number` but also the "socket" value from websocket message data.
-        request(options: string | Client.ClientRequestOptions, callback: (err: any, payload: any, statusCode?: number, headers?: Object) => void): void;
-        message(message: any, callback: (err: any, message: any) => void): void;
-        subscribe(path: string, handler: Client.Handler, callback: (err?: any) => void): void;
-        unsubscribe(path: string, handler: Client.Handler, callback: (err?: any) => void): void;
-        subscriptions(): string[];
-        overrideReconnectionAuth(auth: any): void;
-    }
-
-    namespace Client {
-        interface Handler {
-            (message: any, flags: Client.ClientSubscribeFlags): void;
-        }
-
-        interface ClientOptions {
-            ws?: any;
-            timeout?: number | boolean;
-        }
-
-        interface ClientConnectOptions {
-            auth?: any;
-            delay?: number;
-            maxDelay?: number;
-            retries?: number;
-            timeout?: number;
-        }
-
-        interface ClientRequestOptions {
-            path: string;
-            method?: string;
-            headers?: Object;
-            payload?: any;
-        }
-
-        interface ClientSubscribeFlags {
-            revoked?: boolean;
-        }
-    }
-
-    export = Client;
+declare class Client {
+    constructor(url: string, options?: Client.ClientOptions);
+    onError: (err: any) => void;
+    onConnect: () => void;
+    onDisconnect: () => void;
+    onUpdate: (message: any) => void;
+    connect(options: Client.ClientConnectOptions, callback: (err?: any) => void): void;
+    connect(callback: (err?: any) => void): void;
+    disconnect(): void;
+    id: any;  // can be `null | number` but also the "socket" value from websocket message data.
+    request(options: string | Client.ClientRequestOptions, callback: (err: any, payload: any, statusCode?: number, headers?: Object) => void): void;
+    message(message: any, callback: (err: any, message: any) => void): void;
+    subscribe(path: string, handler: Client.Handler, callback: (err?: any) => void): void;
+    unsubscribe(path: string, handler: Client.Handler, callback: (err?: any) => void): void;
+    subscriptions(): string[];
+    overrideReconnectionAuth(auth: any): void;
 }
+
+declare namespace Client {
+    interface Handler {
+        (message: any, flags: Client.ClientSubscribeFlags): void;
+    }
+
+    interface ClientOptions {
+        ws?: any;
+        timeout?: number | boolean;
+    }
+
+    interface ClientConnectOptions {
+        auth?: any;
+        delay?: number;
+        maxDelay?: number;
+        retries?: number;
+        timeout?: number;
+    }
+
+    interface ClientRequestOptions {
+        path: string;
+        method?: string;
+        headers?: Object;
+        payload?: any;
+    }
+
+    interface ClientSubscribeFlags {
+        revoked?: boolean;
+    }
+}
+
+export = Client;
