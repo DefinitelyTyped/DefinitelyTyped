a4893234bdbc3cdcab47606ebe2e3dd9063efef4
Bart Ledoux <bledoux@enablon.net>
2017-05-11 17:23:30 -0500

    fix whitespaces in tests

--- types/jasmine-theories/jasmine-theories-tests.ts
+++ types/jasmine-theories/jasmine-theories-tests.ts
@@ -1,4 +1,4 @@
 import theoretically from 'jasmine-theories';
 
-theoretically.it('should run this test', ['a', 1, {}], (testedValue:any) => {});
-theoretically.xit('should not run this test', ['a', 1, {}], (testedValue:any) => {});
+theoretically.it('should run this test', ['a', 1, {}], (testedValue: any) => {});
+theoretically.xit('should not run this test', ['a', 1, {}], (testedValue: any) => {});
