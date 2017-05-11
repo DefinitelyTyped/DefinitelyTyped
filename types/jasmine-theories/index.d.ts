12a789b904be9bbf6526c2f12900cae05565af9e
Bart Ledoux <bledoux@enablon.net>
2017-05-11 17:40:09 -0500

    fix last missing whitespaces

--- types/jasmine-theories/index.d.ts
+++ types/jasmine-theories/index.d.ts
@@ -3,15 +3,15 @@
 // Definitions by: Bart Ledoux <https://github.com/elevatebart/>
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
 
-declare namespace jasmineTheories{
-	interface jasmineTheoriesStatic{
+declare namespace jasmineTheories {
+	interface jasmineTheoriesStatic {
 		/**
 		 * Runs the jasmine test function with each of the given arguments in the list
 		 * @param description the description of the test
 		 * @param args argunments passed to the test
 		 * @param testFunction the test
 		 */
-		it(description: string, args: any[], testFunction: (testedValue:any) => void): void;
+		it(description: string, args: any[], testFunction: (testedValue: any) => void): void;
 
 		/**
 		 * Ignored version of the same test (the x means excluded)
@@ -19,7 +19,7 @@ declare namespace jasmineTheories{
 		 * @param args argunments passed to the test
 		 * @param testFunction the test
 		 */
-		xit(description: string, args: any[], testFunction:(testedValue: any) => void): void;
+		xit(description: string, args: any[], testFunction: (testedValue: any) => void): void;
 	}
 }
 declare var jsminTheories: jasmineTheories.jasmineTheoriesStatic;
