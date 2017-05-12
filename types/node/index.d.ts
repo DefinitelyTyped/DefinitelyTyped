6cd166245a9ecd2d4aa6eab95140e3ad9cc45128
Brendan Forster <brendan@github.com>
2017-05-12 14:25:48 +1000

    document the uncaught exception and include notes from the source

--- types/node/index.d.ts
+++ types/node/index.d.ts
@@ -432,6 +432,44 @@ declare namespace NodeJS {
         send?(message: any, sendHandle?: any): void;
         disconnect(): void;
         connected: boolean;
+
+        /**
+         * The 'uncaughtException' event is emitted when an uncaught JavaScript
+         * exception bubbles all the way back to the event loop. By default,
+         * Node.js handles such exceptions by printing the stack trace to
+         * stderr and exiting. Adding a handler for the 'uncaughtException'
+         * event overrides this default behavior.
+         *
+         * NOTES:
+         *
+         * 'uncaughtException' is a crude mechanism for exception handling
+         * intended to be used only as a last resort. The event should not be
+         * used as an equivalent to `On Error Resume Next`. Unhandled
+         * exceptions inherently mean that an application is in an undefined
+         * state. Attempting to resume application code without properly
+         * recovering from the exception can cause additional unforeseen and
+         * unpredictable issues.
+         *
+         * Exceptions thrown from within the event handler will not be caught.
+         * Instead the process will exit with a non-zero exit code and the
+         * stack trace will be printed. This is to avoid infinite recursion.
+         *
+         * Attempting to resume normally after an uncaught exception can be
+         * similar to pulling out of the power cord when upgrading a computer
+         * -- nine out of ten times nothing happens - but the 10th time, the
+         * system becomes corrupted.
+         *
+         * The correct use of 'uncaughtException' is to perform synchronous
+         * cleanup of allocated resources (e.g. file descriptors, handles, etc)
+         * before shutting down the process. It is not safe to resume normal
+         * operation after 'uncaughtException'.
+         *
+         * To restart a crashed application in a more reliable way, whether
+         * 'uncaughtException' is emitted or not, an external monitor should be
+         * employed in a separate process to detect application failures and
+         * recover or restart as needed.
+         */
+        on(event: 'uncaughtException', listener: (error: Error) => void): this;
     }
 
     export interface Global {
