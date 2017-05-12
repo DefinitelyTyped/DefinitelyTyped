dc35ced17db6c3f45306597e2c7ec9b800b1d4c6
Alexander James Phillips <ajp.global@gmail.com>
2017-05-12 12:13:09 +0100

    Allow exclusion of conflicting properties #16416

--- types/hapi/test/route/additional-options.ts
+++ types/hapi/test/route/additional-options.ts
@@ -8,31 +8,32 @@ var authConfig: Hapi.RouteAdditionalConfigurationOptions = {
 
 // Handler in config
 const user: Hapi.RouteAdditionalConfigurationOptions = {
-    cache: { expiresIn: 5000 },
+    cache: { expiresIn: 5000, statuses: [200, 201] },
     handler: function (request, reply) {
 
         return reply({ name: 'John' });
     }
 };
 
-
 // Add in addition to examples in docs
 
 var cache: Hapi.RouteCacheOptions = {
     privacy: 'default',
-    statuses: [200, 201],
     expiresIn: 5000,
 };
 
 cache = {
     privacy: 'default',
-    statuses: [200, 201],
     expiresAt: '22:44',
 };
 
-// /* typings should cause this to error, but currently does not
+cache = {
+    privacy: 'default',
+};
+
+/* should error (as does!)
 var cache: Hapi.RouteCacheOptions = {
     expiresIn: 5000,
     expiresAt: '22:44',
 };
-// */
+*/
