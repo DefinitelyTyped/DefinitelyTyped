a7ec8f87bceed72a6acd730a0dbe372ffefcadaa
Ali Taheri Moghaddar <ali.taheri.m@gmail.com>
2017-05-14 14:37:39 +0430

    Fix lint errors

--- types/react-file-reader-input/index.d.ts
+++ types/react-file-reader-input/index.d.ts
@@ -17,30 +17,30 @@ declare namespace FileInput {
         /**
          * what format the `FileReader` should read the file as
          * (i.e., `'buffer'`, `'binary'`, `'url'`, `'text'`).
-         * 
+         *
          * Defaults to `'url'`.
          */
         as?: Format;
 
         /**
          * Callback function called when the files are choosen by the user.
-         * 
+         *
          * Results will be an array of arrays, the size of which depending
          * on how many files were selected.
-         * 
+         *
          * Each result will be an array of two items:
-         * 
+         *
          * `progressEvent`: `result[0]` is a `ProgressEvent` object.
          * You can retrieve the raw results at `progressEvent.target.result`
          * among other things.
-         * 
+         *
          * `file`: `result[1]` is a `File` object. You can retrieve the file name
          * at file.name among other things.
-         * 
+         *
          * @param event The event that triggered file changes
          * @param results The array of files
          */
-        onChange: (event: React.SyntheticEvent<any>, results: Result[]) => void;
+        onChange(event: React.SyntheticEvent<any>, results: Result[]): void;
     }
 }
 
