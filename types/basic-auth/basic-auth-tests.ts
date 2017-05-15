56b2a624924d1dc69926498995c380a6e95f0560
Vesa Poikajärvi <vesa.poikajarvi@leonidasoy.fi>
2017-05-15 16:05:20 +0300

    [basic-auth] Add auth.parse

--- types/basic-auth/basic-auth-tests.ts
+++ types/basic-auth/basic-auth-tests.ts
@@ -2,3 +2,5 @@
 import auth = require('basic-auth');
 
 auth(null);
+
+auth.parse('QmFzaWM6QXV0aA==');
--- types/basic-auth/index.d.ts
+++ types/basic-auth/index.d.ts
@@ -1,6 +1,6 @@
-// Type definitions for basic-auth
+// Type definitions for basic-auth v1.1.0
 // Project: https://github.com/jshttp/basic-auth
-// Definitions by: Clément Bourgeois <https://github.com/moonpyk>
+// Definitions by: Clément Bourgeois <https://github.com/moonpyk>, Vesa Poikajärvi <https://github.com/vesse>
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
 
 /// <reference types="node" />
@@ -14,6 +14,9 @@ declare namespace auth {
         name: string;
         pass: string;
     }
+
+    // See https://github.com/jshttp/basic-auth/blob/v1.1.0/index.js#L87-L95
+    function parse(authorizationHeader: string): auth.BasicAuthResult;
 }
 
 export = auth;
