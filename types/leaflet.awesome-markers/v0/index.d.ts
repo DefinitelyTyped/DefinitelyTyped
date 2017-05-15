a8980d69d148684d6b1ea6e8106bf3647a922ca1
Marcel Šebek <sebek@ttc.cz>
2017-05-15 16:15:40 +0200

    feat(leaflet.awesome-markers): support both v0 and v1 leaflet

--- types/leaflet.awesome-markers/v0/index.d.ts
+++ types/leaflet.awesome-markers/v0/index.d.ts
@@ -1,6 +1,6 @@
-// Type definitions for Leaflet.awesome-markers plugin v2.0
+// Type definitions for Leaflet.awesome-markers plugin v2.0 with Leaflet 0.x
 // Project: https://github.com/sigma-geosistemas/Leaflet.awesome-markers#properties
-// Definitions by: Marcel Sebek <https://github.com/sebek64>
+// Definitions by: Egor Komarov <https://github.com/Odrin>, Marcel Sebek <https://github.com/sebek64>
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
 
 import * as Leaflet from "leaflet";
@@ -11,12 +11,27 @@ declare global { namespace L {
 
         function icon(options: AwesomeMarkers.IconOptions): AwesomeMarkers.Icon;
 
-        class Icon extends Leaflet.BaseIcon {
-            constructor(options?: AwesomeMarkers.IconOptions);
+        interface IconStatic extends Leaflet.IconStatic {
+            /**
+              * Creates an icon instance with the given options.
+              */
+            new (options: IconOptions): Icon;
+
+            Default: {
+                /**
+                  * Creates a default icon instance with the given options.
+                  */
+                new (options?: IconOptions): Icon.Default;
+
+                imagePath: string;
+            };
+        }
+
+        interface Icon extends Leaflet.Icon {
             options: AwesomeMarkers.IconOptions;
         }
 
-        interface IconOptions extends Leaflet.BaseIconOptions {
+        interface IconOptions extends Leaflet.IconOptions {
             /**
             * Name of the icon. See glyphicons or font-awesome.
             */
@@ -47,5 +62,7 @@ declare global { namespace L {
             */
             extraClasses?: string;
         }
+
+        var Icon: AwesomeMarkers.IconStatic;
     }
 } }
