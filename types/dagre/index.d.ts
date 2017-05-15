7c049f27acd18d179e40600f8c217cad97091ed5
Gordon Smith <GordonSmith@users.noreply.github.com>
2017-05-15 13:17:49 +0100

    Add (some) missing declarations. Fixed some existing declarations.
    Signed-off-by: Gordon Smith <gordonjsmith@gmail.com>

--- types/dagre/index.d.ts
+++ types/dagre/index.d.ts
@@ -6,15 +6,37 @@
 export as namespace dagre;
 
 export namespace graphlib {
+    interface GraphOptions {
+        directed?: boolean;
+        multigraph?: boolean;
+        compound?: boolean;
+    }
+
     class Graph {
+        constructor(opts?: GraphOptions);
+
+        isDirected(): boolean;
+        isMultigraph(): boolean;
+        isCompound(): boolean;
+
+        graph(): { [key: string]: any };
         edges(): Edge[];
         edge(id: any): any;
         nodes(): string[];
         node(id: any): any;
-        setDefaultEdgeLabel(callback: () => void): Graph;
-        setEdge(sourceId: string, targetId: string, options?: { [key: string]: any }): Graph;
+        setDefaultNodeLabel(callback: () => any): Graph;
+        setDefaultEdgeLabel(callback: () => any): Graph;
+        setEdge(sourceId: string, targetId: string, options?: { [key: string]: any }, id?: string): Graph;
         setGraph(options: { [key: string]: any }): Graph;
         setNode(id: string, node: { [key: string]: any }): Graph;
+        setParent(childId: string, parentId: string): Graph;
+        hasNode(id: string): boolean;
+        hasEdge(id: string): boolean;
+    }
+
+    namespace json {
+        function read(json: any): Graph;
+        function write(graph: Graph): any;
     }
 }
 
