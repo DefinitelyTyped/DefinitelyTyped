a57c069dbbc0ce4a7e91e318caa05d357e5b8a6d
Gordon Smith <GordonSmith@users.noreply.github.com>
2017-05-15 13:17:49 +0100

    Update index.d.ts
    Add (some) missing declarations.

--- types/dagre/index.d.ts
+++ types/dagre/index.d.ts
@@ -7,14 +7,25 @@ export as namespace dagre;
 
 export namespace graphlib {
     class Graph {
+        constructor(opts?: any);
+        graph: any;
         edges(): Edge[];
         edge(id: any): any;
         nodes(): string[];
         node(id: any): any;
+        setDefaultNodeLabel(callback: () => void): Graph;
         setDefaultEdgeLabel(callback: () => void): Graph;
-        setEdge(sourceId: string, targetId: string, options?: { [key: string]: any }): Graph;
+        setEdge(sourceId: string, targetId: string, options?: { [key: string]: any }, id?: string): Graph;
         setGraph(options: { [key: string]: any }): Graph;
         setNode(id: string, node: { [key: string]: any }): Graph;
+        setParent(childId: string, parentId: string);
+        hasNode(id: string): boolean;
+        hasEdge(id: string): boolean;
+        delNode(node: any)
+    }
+
+    namespace json {
+        function write(graph: Graph);
     }
 }
 
