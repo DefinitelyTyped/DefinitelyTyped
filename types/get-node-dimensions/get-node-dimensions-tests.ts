3b36e391a7fa6afc79e894ae1a5ab567f0d4126b
Kovács Vince <vincekovacs@hotmail.com>
2017-05-16 22:53:49 +0200

    Type definitions for get-node-dimensions 1.2

---
+++ types/get-node-dimensions/get-node-dimensions-tests.ts
@@ -0,0 +1,3 @@
+import * as NodeDimensions from 'get-node-dimensions';
+
+const dimensions: NodeDimensions.NodeDimensions = NodeDimensions.getNodeDimension();
---
+++ types/get-node-dimensions/index.d.ts
@@ -0,0 +1,17 @@
+// Type definitions for get-node-dimensions 1.2
+// Project: https://github.com/souporserious/get-node-dimensions
+// Definitions by: Kovács Vince <https://github.com/vincekovacs>
+// Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
+
+export as namespace getNodeDimensions;
+
+export interface NodeDimensions {
+  width: number,
+  height: number,
+  top: number,
+  right: number,
+  bottom: number,
+  left: number
+}
+
+export function getNodeDimension (): NodeDimensions;
