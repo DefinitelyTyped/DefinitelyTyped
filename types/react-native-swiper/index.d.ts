cef30d4872a315b6c32b541d337864268e671624
gengrq <mhcgrq@live.com>
2017-05-16 23:15:39 +0800

    fix `Definitions by`

--- types/react-native-swiper/index.d.ts
+++ types/react-native-swiper/index.d.ts
@@ -1,6 +1,8 @@
 // Type definitions for react-native-swiper 1.5
 // Project: https://github.com/leecade/react-native-swiper#readme
-// Definitions by: CaiHuan <https://github.com/CaiHuan>,  HuHuanming <https://github.com/huhuanming>,  mhcgrq <https://github.com/mhcgrq>
+// Definitions by: CaiHuan <https://github.com/CaiHuan>
+//                 HuHuanming <https://github.com/huhuanming>
+//                 mhcgrq <https://github.com/mhcgrq>
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
 // TypeScript Version: 2.1
 
