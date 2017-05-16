eb792c57778e5b248493e154e0048669b10300e6
Bastien <bmoulia@gmail.com>
2017-05-16 14:32:20 +0200

    [angular-pdfjs-viewer] Creation
    For https://github.com/legalthings/angular-pdfjs-viewer

---
+++ types/angular-pdfjs-viewer/index.d.ts
@@ -0,0 +1,20 @@
+// Type definitions for angular-pdfjs-viewer 1.0
+// Project: https://github.com/legalthings/angular-pdfjs-viewer
+// Definitions by: My Self <https://github.com/bastienmoulia>
+// Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
+
+import * as angular from 'angular';
+
+type Verbosity = "errors" | "warnings" | "infos";
+
+declare module 'angular' {
+    export namespace pdfjsViewer {
+        interface IConfigProvider {
+            setWorkerSrc?: (src: string) => void;
+            setCmapDir?: (dir: string) => void;
+            setImageDir?: (dir: string) => void;
+            disableWorker?: () => void;
+            setVerbosity?: (verbosity: Verbosity) => void;
+        }
+    }
+}
