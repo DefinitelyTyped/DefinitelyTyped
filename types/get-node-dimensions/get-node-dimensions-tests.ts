9e49fa9e7f01f97ca81c902df0704383e7b4f2bb
Kovács Vince <vincekovacs@hotmail.com>
2017-05-17 11:28:08 +0200

    Make option key as optional

--- types/get-node-dimensions/get-node-dimensions-tests.ts
+++ types/get-node-dimensions/get-node-dimensions-tests.ts
@@ -5,3 +5,7 @@ const div = document.getElementById('div-to-measure');
 if (div !== null) {
   const dimensions = getNodeDimensions(div);
 }
+
+if (div !== null) {
+  const dimensionsWithMargin = getNodeDimensions(div, {margin: true});
+}
