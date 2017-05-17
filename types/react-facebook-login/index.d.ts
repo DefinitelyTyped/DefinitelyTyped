dcab77774c502fbb326e62833d234e76555f9f75
Jan Karres <webmaster@jankarres.de>
2017-05-17 19:03:06 +0200

    Update react-facebook-login for v3.6.0 fix lint tests

--- types/react-facebook-login/index.d.ts
+++ types/react-facebook-login/index.d.ts
@@ -2,49 +2,46 @@
 // Project: https://github.com/keppelen/react-facebook-login
 // Definitions by: Alexandre Paré <https://github.com/apare>, Jan Karres <https://github.com/jankarres>
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
-// TypeScript Version: 2.3
 
 import * as React from "react";
 
-declare namespace ReactFacebookLogin {
-    interface ReactFacebookLoginProps {
-        appId: string;
-        callback(userInfo: ReactFacebookLoginInfo): void;
+interface ReactFacebookLoginProps {
+    appId: string;
+    callback(userInfo: ReactFacebookLoginInfo): void;
 
-        autoLoad?: boolean;
-        buttonStyle?: React.CSSProperties;
-        containerStyle?: React.CSSProperties;
-        cookie?: boolean;
-        cssClass?: string;
-        disableMobileRedirect?: boolean;
-        fields?: string;
-        icon?: string | React.ReactNode;
-        isDisabled?: boolean;
-        language?: string;
-        onClick?(): void;
-        reAuthenticate?: boolean;
-        redirectUri?: string;
-        scope?: string;
-        size?: "small" | "medium" | "metro";
-        textButton?: string;
-        typeButton?: string;
-        version?: string;
-        xfbml?: boolean;
-        isMobile?: boolean;
-        tag?: Node | Function;
-    }
-
-    interface ReactFacebookLoginInfo {
-        id: string;
-        name: string;
-    }
-
-    interface ReactFacebookLoginState {
-        isSdkLoaded?: boolean;
-        isProcessing?: boolean;
-    }
+    autoLoad?: boolean;
+    buttonStyle?: React.CSSProperties;
+    containerStyle?: React.CSSProperties;
+    cookie?: boolean;
+    cssClass?: string;
+    disableMobileRedirect?: boolean;
+    fields?: string;
+    icon?: string | React.ReactNode;
+    isDisabled?: boolean;
+    language?: string;
+    onClick?(): void;
+    reAuthenticate?: boolean;
+    redirectUri?: string;
+    scope?: string;
+    size?: "small" | "medium" | "metro";
+    textButton?: string;
+    typeButton?: string;
+    version?: string;
+    xfbml?: boolean;
+    isMobile?: boolean;
+    tag?: Node | React.Component<any, any>;
 }
 
-declare class ReactFacebookLogin extends React.Component<ReactFacebookLogin.ReactFacebookLoginProps, ReactFacebookLogin.ReactFacebookLoginState> { }
+export interface ReactFacebookLoginInfo {
+    id: string;
+    name: string;
+}
+
+interface ReactFacebookLoginState {
+    isSdkLoaded?: boolean;
+    isProcessing?: boolean;
+}
+
+declare class ReactFacebookLogin extends React.Component<ReactFacebookLoginProps, ReactFacebookLoginState> { }
 
 export default ReactFacebookLogin;
