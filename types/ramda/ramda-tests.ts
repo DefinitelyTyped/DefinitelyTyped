f1252e8a9c3be4eb9da4aac4859a9b0549295a54
Matt Dziuban <mrdziuban@gmail.com>
2017-05-17 09:32:16 -0400

    Fix tryCatch definition.

--- types/ramda/ramda-tests.ts
+++ types/ramda/ramda-tests.ts
@@ -972,8 +972,9 @@ type Pair = KeyValuePair<string, number>
 
 () => {
     const x = R.prop('x');
-    const a: boolean = R.tryCatch<boolean>(R.prop('x'), R.F, {x: true}); //=> true
-    const b: boolean = R.tryCatch<boolean>(R.prop('x'), R.F, null);      //=> false
+    const a: boolean = R.tryCatch<boolean>(R.prop('x'), R.F)({x: true}); //=> true
+    const b: boolean = R.tryCatch<boolean>(R.prop('x'), R.F)(null);      //=> false
+    const c: boolean = R.tryCatch<boolean>(R.and, R.F)(true, true);      //=> true
 }
 
 () => {
