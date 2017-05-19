1c09309c27547cd5fc5162f5047f1d05d8d5ca6f
Dolan <dolan_miu@hotmail.com>
2017-05-19 12:26:21 +0100

    Exported SalesforceId and fixed typo

--- types/jsforce/index.d.ts
+++ types/jsforce/index.d.ts
@@ -1,4 +1,4 @@
-// Type definitions for archiver 1.8
+// Type definitions for jsforce 1.8
 // Project: https://github.com/jsforce/jsforce
 // Definitions by: Dolan Miu <https://github.com/dolanmiu>
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
@@ -12,3 +12,4 @@ export { Date } from './date-enum';
 export { RecordResult } from './record-result';
 export { Connection } from './connection';
 export { SObject } from './salesforce-object';
+export { SalesforceId } from './salesforce-id';
