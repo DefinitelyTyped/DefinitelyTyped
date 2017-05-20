ab1492337d702014d5b29b3155f9543280724d00
Aluan Haddad <aluanh@gmail.com>
2017-05-20 10:12:03 -0400

    updated to use Mapped Types and align with Object.freeze in lib.d.ts
    Updated the declaration to take advantage of Mapped Types. Specifically, I added a `DeepReadonly` recursive generic mapped type.
    
    Additionally, I took the opportunity to add overloads that mirroring those of `Object.freeze` as specified in the latest `lib.d.ts`.
    
    I removed the interface as it is now unused.

--- types/deep-freeze/index.d.ts
+++ types/deep-freeze/index.d.ts
@@ -1,13 +1,13 @@
-// Type definitions for deep-freeze
+// Type definitions for deep-freeze 0.1
 // Project: https://github.com/substack/deep-freeze
 // Definitions by: Bart van der Schoor <https://github.com/Bartvds>
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
+// TypeScript Version: 2.1
 
-declare var deepFreeze: DeepFreeze.DeepFreezeInterface;
 export = deepFreeze;
 
-declare namespace DeepFreeze {
-   export interface DeepFreezeInterface {
-      <T>(obj: T): T;
-    }
-}
+declare function deepFreeze<T>(a: T[]): ReadonlyArray<DeepReadonly<T>>;
+declare function deepFreeze<T extends Function>(f: T): T;
+declare function deepFreeze<T>(o: T): DeepReadonly<T>;
+
+declare type DeepReadonly<T> = Readonly<{ [P in keyof T]: Readonly<T[P]> }>;
