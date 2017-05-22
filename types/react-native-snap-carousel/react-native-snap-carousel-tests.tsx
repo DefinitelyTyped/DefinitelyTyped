a1fde6da297543940d51c48b31c9b48fada45dbf
Jonas Thiel <jonas@thiel.io>
2017-05-22 10:27:16 +0200

    Update react-native-snap-carousel to v2.1.4

--- types/react-native-snap-carousel/react-native-snap-carousel-tests.tsx
+++ types/react-native-snap-carousel/react-native-snap-carousel-tests.tsx
@@ -1,5 +1,7 @@
 import * as React from 'react';
 import {
+    NativeSyntheticEvent,
+    NativeScrollEvent,
     StyleSheet,
     Text,
     View,
@@ -23,6 +25,7 @@ class SnapCarouselTest extends React.Component<{}, {}> {
                     enableMomentum={true}
                     keyboardDismissMode='interactive'
                     onSnapToItem={this.onSnapToItem}
+                    onScrollViewScroll={this.onScroll}
                 >
                     <View
                         style={styles.item}
@@ -36,6 +39,10 @@ class SnapCarouselTest extends React.Component<{}, {}> {
 
     private onSnapToItem = (index: number) => {
         console.log("Snapped to: ", index);
+    }
+
+    private onScroll = (event: NativeSyntheticEvent<NativeScrollEvent>) => {
+        console.log("Scrolled: ", event);
     }
 }
 
