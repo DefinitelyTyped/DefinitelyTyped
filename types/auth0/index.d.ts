c6288cb4e254d16f5599fb43d622b5a10ab965e2
amiram <amiramk@gmail.com>
2017-05-23 10:44:03 +0300

    auth0 fix getUser returns only a single user fix deleteUser returns void fix getUser and deleteUser mandatory params

--- types/auth0/index.d.ts
+++ types/auth0/index.d.ts
@@ -1,6 +1,6 @@
 // Type definitions for auth0 2.4
 // Project: https://github.com/auth0/node-auth0
-// Definitions by: Wilson Hobbs <https://github.com/wbhob>, Seth Westphal <https://github.com/westy92>
+// Definitions by: Wilson Hobbs <https://github.com/wbhob>, Seth Westphal <https://github.com/westy92>, Amiram Korach <https://github.com/amiram>
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
 // TypeScript Version: 2.3
 
