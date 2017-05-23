6c293ff7835e720d49ed9ca282632b8e947c767b
Runyang <runyang@teambition.com>
2017-05-23 12:06:48 +0800

    Fix a composing problem

--- types/react-textarea-autosize/index.d.ts
+++ types/react-textarea-autosize/index.d.ts
@@ -1,7 +1,6 @@
 // Type definitions for react-textarea-autosize 4.3.0
 // Project: https://github.com/andreypopp/react-textarea-autosize
-// Definitions by: Alexey Svetliakov <https://github.com/asvetliakov>
-//                 Jerry Zou <https://github.com/zry656565>
+// Definitions by: Alexey Svetliakov <https://github.com/asvetliakov>, Jerry Zou <https://github.com/zry656565>
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
 // TypeScript Version: 2.1
 
