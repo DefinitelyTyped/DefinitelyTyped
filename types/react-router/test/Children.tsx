3b8b7625b71efa6cfcd06787ef37c848a28fa508
Daniel Rosenwasser <Daniel.Rosenwasser@microsoft.com>
2017-05-30 01:17:26 -0700

    Lints.

--- types/react-router/test/Children.tsx
+++ types/react-router/test/Children.tsx
@@ -2,21 +2,21 @@ import * as React from 'react';
 import { Route } from 'react-router';
 
 function RouteWithFunctionChildrenAttribute() {
-    return <Route path="/" children={() => <div>Hello!</div>} />
+    return <Route path="/" children={() => <div>Hello!</div>} />;
 }
 
 function RouteWithFunctionJsxChildren() {
     return <Route path="/">
         {() => <div>Hello!</div>}
-    </Route>
+    </Route>;
 }
 
 function RouteWithElementChildrenAttribute() {
-    return <Route path="/" children={<div>Hello!</div>} />
+    return <Route path="/" children={<div>Hello!</div>} />;
 }
 
 function RouteWithElementJsxChildren() {
     return <Route path="/">
         {<div>Hello!</div>}
-    </Route>
+    </Route>;
 }
