2e0b21f2e6f3b2dda9efaefb09d956ca91d9fb67
Bradley Ayers <bayers@atlassian.com>
2017-05-30 13:46:55 +1000

    Upgrade prosemirror-model to 0.21

--- types/prosemirror-model/prosemirror-model-tests.ts
+++ types/prosemirror-model/prosemirror-model-tests.ts
@@ -1,3 +1,13 @@
 import * as model from 'prosemirror-model';
 
 let fragment = new model.Fragment();
+
+let domOutputSpec: model.DOMOutputSpec;
+
+domOutputSpec = ['div'];
+domOutputSpec = ['div', { class: 'foo' }];
+domOutputSpec = ['div', { class: 'foo' }, 0];
+domOutputSpec = ['div', 0];
+domOutputSpec = ['div', ['div', 0]];
+domOutputSpec = ['div', ['div', { class: 'foo' }]];
+domOutputSpec = ['div', ['div', { class: 'foo' }, 0]];
