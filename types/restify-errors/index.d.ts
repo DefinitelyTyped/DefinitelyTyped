b563bb006be42464cae07916b353ec0d0e5c5de4
Steve Hipwell <steve.hipwell@rbi.co.uk>
2017-06-01 13:25:39 +0100

    Update correct definition

--- types/restify-errors/index.d.ts
+++ types/restify-errors/index.d.ts
@@ -2,6 +2,7 @@
 // Project: http://www.restify.com
 // Definitions by: Steve Hipwell <https://github.com/stevehipwell>
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
+// TypeScript Version: 2.1
 
 import { VError, Options as vErrorOptions } from "verror";
 
