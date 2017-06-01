3f95ea32d75f9d6889d2aa74f209c07f6f807d86
Leonard Thieu <leonard-thieu@users.noreply.github.com>
2017-06-01 15:05:57 -0400

    [node] Narrow down type that can be passed to url.format(). (#16850)
    * Narrow down type that can be passed to url.format().
    
    * Add format(URL).
    
    * [node] Fix build error.
    universal-analytics package has invalid tslint.json.
    
    * [node] Fix build error.
    universal-analytics package has invalid version number.

--- types/node/v6/index.d.ts
+++ types/node/v6/index.d.ts
@@ -1801,8 +1801,21 @@ declare module "url" {
         path?: string;
     }
 
+    export interface UrlObject {
+        protocol?: string;
+        slashes?: boolean;
+        auth?: string;
+        host?: string;
+        hostname?: string;
+        port?: string | number;
+        pathname?: string;
+        search?: string;
+        query?: { [key: string]: any; };
+        hash?: string;
+    }
+
     export function parse(urlStr: string, parseQueryString?: boolean, slashesDenoteHost?: boolean): Url;
-    export function format(url: Url): string;
+    export function format(urlObject: UrlObject): string;
     export function resolve(from: string, to: string): string;
 }
 
