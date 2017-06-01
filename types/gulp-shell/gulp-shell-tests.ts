84c34fc4d068be5f532175e541cebb99fb2e075c
Giedrius Grabauskas <giedrius@quatrodev.com>
2017-06-02 00:27:31 +0300

    Fixed `gulp-shell` tests compatibility with gulp@4 types.

--- types/gulp-shell/gulp-shell-tests.ts
+++ types/gulp-shell/gulp-shell-tests.ts
@@ -29,13 +29,7 @@ var paths: any = {
 
 gulp.task('test', shell.task('mocha -R spec'));
 
-gulp.task('coverage', ['test'], shell.task('istanbul cover _mocha -- -R spec'));
-
-gulp.task('coveralls', ['coverage'], shell.task('cat coverage/lcov.info | coveralls'));
-
 gulp.task('lint', shell.task('eslint ' + paths.js.join(' ')));
-
-gulp.task('default', ['coverage', 'lint']);
 
 gulp.task('watch', function () {
     gulp.watch(paths.js, ['default'])
