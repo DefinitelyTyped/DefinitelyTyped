d503732352055d0883585e56d79e4c9fa0db71bd
Sudarsan Balaji <Sudarsan.Balaji@outlook.com>
2017-06-01 09:59:07 +0530

    Make requested changes

--- types/react-loader/index.d.ts
+++ types/react-loader/index.d.ts
@@ -3,43 +3,41 @@
 // Definitions by: Sudarsan Balaji <https://github.com/artfuldev>
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
 
-declare module 'react-loader' {
-	import { Component } from 'react';
+import { Component } from 'react';
 
-	interface LoaderOptions {
-		lines?: number;
-		length?: number;
-		width?: number;
-		radius?: number;
-		scale?: number;
-		corners?: number;
-		color?: string;
-		opacity?: number;
-		rotate?: number;
-		direction?: number;
-		speed?: number;
-		trail?: number;
-		fps?: number;
-		zIndex?: number;
-		top?: string;
-		left?: string;
-		shadow?: boolean;
-		hwaccel?: boolean;
-		position?: string;
-		loadedClassName?: string;
-	}
-
-	interface LoaderProps extends LoaderOptions {
-		loaded: boolean;
-		options?: LoaderOptions;
-		className?: string;
-	}
-
-	class ReactLoader extends Component<LoaderProps, any>  {
-	}
-
-	namespace ReactLoader {
-	}
-
-	export = ReactLoader;
+interface LoaderOptions {
+	lines?: number;
+	length?: number;
+	width?: number;
+	radius?: number;
+	scale?: number;
+	corners?: number;
+	color?: string;
+	opacity?: number;
+	rotate?: number;
+	direction?: number;
+	speed?: number;
+	trail?: number;
+	fps?: number;
+	zIndex?: number;
+	top?: string;
+	left?: string;
+	shadow?: boolean;
+	hwaccel?: boolean;
+	position?: string;
+	loadedClassName?: string;
 }
+
+interface LoaderProps extends LoaderOptions {
+	loaded: boolean;
+	options?: LoaderOptions;
+	className?: string;
+}
+
+declare class ReactLoader extends Component<LoaderProps, any>  {
+}
+
+declare namespace ReactLoader {
+}
+
+export = ReactLoader;
