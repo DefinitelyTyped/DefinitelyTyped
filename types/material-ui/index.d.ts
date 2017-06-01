0b9fa356c81dc1b54a7c5e13d4769f7769f259f8
schlesingermatthias <schlesinger.matthias@gmail.com>
2017-06-01 14:44:17 -0400

    [material-ui] Added missing Props to DropDownMenu and ListItem (#16896)
    * DropDownMenu: add missing prop onChange; ListItem: add missing prop hoverColor
    
    * fix error

--- types/material-ui/index.d.ts
+++ types/material-ui/index.d.ts
@@ -1,6 +1,6 @@
 // Type definitions for material-ui v0.17.51
 // Project: https://github.com/callemall/material-ui
-// Definitions by: Nathan Brown <https://github.com/ngbrown>, Igor Belagorudsky <https://github.com/theigor>, Ali Taheri Moghaddar <https://github.com/alitaheri>, Oliver Herrmann <https://github.com/herrmanno>, Daniel Roth <https://github.com/DaIgeb>, Aurelién Allienne <https://github.com/allienna>
+// Definitions by: Nathan Brown <https://github.com/ngbrown>, Igor Beagorudsky <https://github.com/theigor>, Ali Taheri Moghaddar <https://github.com/alitaheri>, Oliver Herrmann <https://github.com/herrmanno>, Daniel Roth <https://github.com/DaIgeb>, Aurelién Allienne <https://github.com/allienna>, Matthias Schlesinger <https://github.com/schlesingermatthias>
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
 // TypeScript Version: 2.2
 
@@ -1088,6 +1088,7 @@ declare namespace __MaterialUI {
             autoGenerateNestedIndicator?: boolean;
             disableKeyboardFocus?: boolean;
             disabled?: boolean;
+            hoverColor?: string;
             initiallyOpen?: boolean;
             innerDivStyle?: React.CSSProperties;
             insetChildren?: boolean;
@@ -1223,6 +1224,7 @@ declare namespace __MaterialUI {
             maxHeight?: number;
             menuStyle?: React.CSSProperties;
             onChange?: (e: TouchTapEvent, index: number, menuItemValue: any) => void;
+            onClose?: (e: TouchTapEvent) => void;
             openImmediately?: boolean;
             style?: React.CSSProperties;
             underlineStyle?: React.CSSProperties;
