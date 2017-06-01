a39e51dceddc66d365da610e18da75e851058c98
Andy Hanson <anhans@microsoft.com>
2017-06-01 07:55:17 -0700

    Fix lint

--- types/react-native/index.d.ts
+++ types/react-native/index.d.ts
@@ -234,7 +234,7 @@ interface EventEmitter extends EventEmitterListener {
      *
      *   emitter.emit('someEvent', 'abc'); // logs 'abc'
      */
-    emit(eventType: string,...param:any[]): void
+    emit(eventType: string, ...params: any[]): void
 
     /**
      * Removes the given listener for event of specific type.
