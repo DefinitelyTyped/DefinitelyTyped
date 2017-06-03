eebd2e5e476f460f86097285580e91ecd3081d79
Carl Winkler <carl@winkler.id.au>
2017-06-03 10:15:18 +0800

    Respond to PR feedback - Add semicoloms - Remove namespace - Change line rules

--- types/amqp/amqp-tests.ts
+++ types/amqp/amqp-tests.ts
@@ -1,41 +1,41 @@
-import * as amqp from 'amqp'
+import * as amqp from 'amqp';
 
 async function connect() {
   const promise = new Promise<amqp.AMQPClient>((resolve, reject) => {
     const client = amqp.createConnection({
       url: 'amqp://admin:password@localhost:5672'
-    })
+    });
 
-    client.once('error', reject)
-    client.once('ready', resolve)
-  })
+    client.once('error', reject);
+    client.once('ready', resolve);
+  });
 
-  return promise
+  return promise;
 }
 
 async function start() {
   try {
-    const client = await connect()
-    console.log('Connected')
+    const client = await connect();
+    console.log('Connected');
 
     const queue = client.queue('perth-now',
       {
         autoDelete: false,
         durable: true,
       }, q => {
-        console.log('Queue opened')
-        console.log('Name: %s Channel: %s', q.name, q.channel)
+        console.log('Queue opened');
+        console.log('Name: %s Channel: %s', q.name, q.channel);
 
         queue.bind('amq.fanout', '#', () => {
           queue.subscribe(
             { ack: true },
             (msg, _, __, ack) => {
-              ack.acknowledge(true)
-            })
-        })
-      })
+              ack.acknowledge(true);
+            });
+        });
+      });
 
-    const exchange = client.exchange('amq.fanout', { confirm: true })
+    const exchange = client.exchange('amq.fanout', { confirm: true });
 
     exchange.once('open', () => {
       exchange.publish(
@@ -44,24 +44,26 @@ async function start() {
         { deliveryMode: 2 },
         (err, msg) => {
           if (!err) {
-            return
+            return;
           }
-          throw new Error(`Failed to publish: ${msg}`)
+          throw new Error(`Failed to publish: ${msg}`);
         }
-      )
+      );
 
       exchange.publish('content', { message: 'content message' }, () => {
-        console.log('Published')
-      })
-    })
+        console.log('Published');
+      });
+    });
   } catch (ex) {
-    console.log(ex)
-    process.exit(1)
+    console.log(ex);
+    process.exit(1);
   }
 }
 
 async function wait(ms: number) {
   return new Promise(resolve => {
-    setTimeout(resolve, ms)
-  })
+    setTimeout(resolve, ms);
+  });
 }
+
+start();
