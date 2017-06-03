ac9c488dd1358d0431fba5045b0b4737fbadfd43
TeamworkGuy2 <straitfrommars@rocketmail.com>
2017-06-03 20:44:01 +0000

    Fixed combine-source-map definition, added tslint.json

--- types/combine-source-map/combine-source-map-tests.ts
+++ types/combine-source-map/combine-source-map-tests.ts
@@ -1,13 +1,12 @@
-
 import combine = require("combine-source-map");
 
-var sourcemap = combine.create();
+const sourcemap = combine.create();
 
 sourcemap.addFile({ sourceFile: "file.js", source: "return 123" })
     .addFile({ sourceFile: "source.js", source: "return 'abc'" });
 
-var res: string = sourcemap.base64();
+const res: string = sourcemap.base64();
 
-res = sourcemap.comment();
+const res2 = sourcemap.comment();
 
 combine.removeComments("(123 | 'abc') // #sourceMappingURL=file.js.map");
--- types/combine-source-map/index.d.ts
+++ types/combine-source-map/index.d.ts
@@ -6,41 +6,41 @@
 /** Add source maps of multiple files, offset them and then combine them into one source map.
  * (documentation based on project's README file)
  */
-declare namespace CombineSourceMap {
+declare class Combiner {
+    constructor(file?: string, sourceRoot?: string);
 
+    /** Adds map to underlying source map.
+     * If source contains a source map comment that has the source of the original file inlined it will offset these
+     * mappings and include them.
+     * If no source map comment is found or it has no source inlined, mappings for the file will be generated and included
+     * @param opts the 'sourceFile' path/name and the file's 'source' contents
+     * @param offset the source file 'line' number and 'column' number offsets
+     */
+    addFile(opts: { sourceFile: string; source: string }, offset?: Combiner.Offset): Combiner;
+
+    /** output the combined source map in base64 format
+     * @return base64 encoded combined source map
+     */
+    base64(): string;
+
+    /** generate a base64 encoded sourceMappingURL comment
+     * @return base64 encoded sourceMappingUrl comment of the combined source map
+     */
+    comment(): string;
+
+    _addGeneratedMap(sourceFile: string, source: string, offset?: Combiner.Offset): Combiner;
+
+    _addExistingMap(sourceFile: string, source: string, existingMap: any, offset?: Combiner.Offset): Combiner;
+}
+
+declare namespace Combiner {
+    /** An offset line and column number */
     interface Offset {
         line?: number;
         column?: number;
     }
 
-    interface Combiner {
-        //new (file?: string, sourceRoot?: string): Combiner;
-
-        /** Adds map to underlying source map.
-         * If source contains a source map comment that has the source of the original file inlined it will offset these
-         * mappings and include them.
-         * If no source map comment is found or it has no source inlined, mappings for the file will be generated and included
-         * @param opts 'sourceFile' String and 'source' String
-         * @param offset 'line' Number and 'column' Number
-         */
-        addFile(opts: { sourceFile: string; source: string }, offset?: Offset): Combiner;
-
-        /** base64
-         * @return base64 encoded combined source map
-         */
-        base64(): string;
-
-        /** comment
-         * @return base64 encoded sourceMappingUrl comment of the combined source map
-         */
-        comment(): string;
-
-        _addGeneratedMap(sourceFile: string, source: string, offset?: Offset): Combiner;
-
-        _addExistingMap(sourceFile: string, source: string, existingMap: any, offset?: Offset): Combiner;
-    }
-
-    /** create
+    /** Create a source map combiner that accepts multiple files, offsets them and then combines them into one source map.
      * @param file optional name of the generated file
      * @param sourceRoot optional sourceRoot of the map to be generated
      * @return Combiner instance to which source maps can be added and later combined
@@ -54,4 +54,6 @@ declare namespace CombineSourceMap {
     function removeComments(src: string): string;
 }
 
-export = CombineSourceMap;
+export = Combiner;
+
+export as namespace Combiner;
