2a4b4c411648474ccc229f6137296e8e81a7c096
Eric Byers <ebyers@markettrack.com>
2017-06-05 12:25:58 -0500

    Fixing some linting issues

--- types/requestretry/index.d.ts
+++ types/requestretry/index.d.ts
@@ -1,4 +1,4 @@
-// Type definitions for requestretry 1.12.0
+// Type definitions for requestretry 1.12
 // Project: https://github.com/FGRibreau/node-request-retry
 // Definitions by: Eric Byers <https://github.com/EricByers>
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
@@ -6,36 +6,31 @@
 /// <reference types="node" />
 
 declare module 'requestretry' {
-
 	import request = require('request');
 	import http = require('http');
 
-	interface IRetryStrategy {
+	interface RetryStrategy {
 		(err: Error, response: http.IncomingMessage, body: any):boolean;
 	}
 
 	namespace requestRetry {
-
 		export interface RequestAPI extends request.RequestAPI<request.Request, requestRetry.RequestRetryOptions, request.RequiredUriUrl> {
-
 			RetryStrategies: {
-				'HttpError': IRetryStrategy;
-				'HTTPOrNetworkError': IRetryStrategy;
-				'NetworkError': IRetryStrategy;
-			}
+				'HttpError': RetryStrategy;
+				'HTTPOrNetworkError': RetryStrategy;
+				'NetworkError': RetryStrategy;
+			};
 		}
 
 		interface RequestRetryOptions extends request.CoreOptions {
 			maxAttempts?: number;
 			retryDelay?: number;
-			retryStrategy?: IRetryStrategy
+			retryStrategy?: RetryStrategy;
 		}
 
 		export type OptionsWithUri = request.UriOptions & RequestRetryOptions;
 		export type OptionsWithUrl = request.UrlOptions & RequestRetryOptions;
 		export type Options = OptionsWithUri | OptionsWithUrl;
-
-
 	}
 
 	let requestRetryExport: requestRetry.RequestAPI;
