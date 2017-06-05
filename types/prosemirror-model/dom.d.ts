869032309f97fd7713af46e30e70fed8b463dde1
Bradley Ayers <bayers@atlassian.com>
2017-06-06 09:48:05 +1000

    Update prosemirror-collab prosemirror-commands prosemirror-history prosemirror-inputrules prosemirror-keymap prosemirror-markdown prosemirror-model prosemirror-state prosemirror-transform prosemirror-view to 0.21

---
+++ types/prosemirror-model/dom.d.ts
@@ -0,0 +1,21 @@
+export type DOMDataTransfer = DataTransfer;
+export type DOMDocument = Document;
+export type DOMElement = HTMLElement;
+export type DOMEvent = Event;
+export type DOMFragment = DocumentFragment;
+export type DOMKeyboardEvent = KeyboardEvent;
+export type DOMMouseEvent = MouseEvent;
+export type DOMMutationRecord = MutationRecord;
+export type DOMNode = Node;
+
+export {
+  DOMDataTransfer as DataTransfer,
+  DOMDocument as Document,
+  DOMElement as Element,
+  DOMEvent as Event,
+  DOMFragment as DocumentFragment,
+  DOMKeyboardEvent as KeyboardEvent,
+  DOMMouseEvent as MouseEvent,
+  DOMMutationRecord as MutationRecord,
+  DOMNode as Node,
+};
