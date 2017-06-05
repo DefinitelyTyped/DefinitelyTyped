52fbfebcb6051948ac425ac5e2c797c3ed3bc04b
Konstantin Vasilev <konstantin.vasilev@jetbrains.com>
2017-06-05 12:01:17 +0300

    fix single module declaration

--- types/react-swipeable/index.d.ts
+++ types/react-swipeable/index.d.ts
@@ -4,38 +4,36 @@
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
 // TypeScript Version: 2.2
 
-declare module 'react-swipeable' {
-    import * as React from 'react';
+import * as React from 'react';
 
-    type onSwipingCallback = (event: React.TouchEvent<HTMLElement>, deltaX: number, deltaY: number, absX: number, absY: number, velocity: number) => void;
-    type OnSwipedCallback = (event: React.TouchEvent<HTMLElement>, deltaX: number, deltaY: number, isFlick: boolean, velocity: number) => void;
-    type OnSwipedDirectionCallback = (event: React.TouchEvent<HTMLElement>, delta: number, isFlick: boolean) => void;
-    type OnSwipingDirectionCallback = (event: React.TouchEvent<HTMLElement>, delta: number) => void;
-    type OnTapCallback = (event: React.TouchEvent<HTMLElement>) => void;
+type onSwipingCallback = (event: React.TouchEvent<HTMLElement>, deltaX: number, deltaY: number, absX: number, absY: number, velocity: number) => void;
+type OnSwipedCallback = (event: React.TouchEvent<HTMLElement>, deltaX: number, deltaY: number, isFlick: boolean, velocity: number) => void;
+type OnSwipedDirectionCallback = (event: React.TouchEvent<HTMLElement>, delta: number, isFlick: boolean) => void;
+type OnSwipingDirectionCallback = (event: React.TouchEvent<HTMLElement>, delta: number) => void;
+type OnTapCallback = (event: React.TouchEvent<HTMLElement>) => void;
 
-    interface Props extends React.ClassAttributes<ReactSwipeable>, React.HTMLAttributes<HTMLElement> {
-        onSwiped?: OnSwipedCallback;
-        onSwiping?: onSwipingCallback;
-        onSwipingUp?: OnSwipingDirectionCallback;
-        onSwipingRight?: OnSwipingDirectionCallback;
-        onSwipingDown?: OnSwipingDirectionCallback;
-        onSwipingLeft?: OnSwipingDirectionCallback;
-        onSwipedUp?: OnSwipedDirectionCallback;
-        onSwipedRight?: OnSwipedDirectionCallback;
-        onSwipedDown?: OnSwipedDirectionCallback;
-        onSwipedLeft?: OnSwipedDirectionCallback;
-        onTap?: OnTapCallback;
-        flickThreshold?: number;
-        delta?: number;
-        preventDefaultTouchmoveEvent?: boolean;
-        stopPropagation?: boolean;
-        nodeName?: string;
-        trackMouse?: boolean;
-        children?: React.ReactNode;
-    }
-
-    class ReactSwipeable extends React.Component<Props, void> {}
-    namespace ReactSwipeable {}
-
-    export = ReactSwipeable;
+interface Props extends React.ClassAttributes<ReactSwipeable>, React.HTMLAttributes<HTMLElement> {
+    onSwiped?: OnSwipedCallback;
+    onSwiping?: onSwipingCallback;
+    onSwipingUp?: OnSwipingDirectionCallback;
+    onSwipingRight?: OnSwipingDirectionCallback;
+    onSwipingDown?: OnSwipingDirectionCallback;
+    onSwipingLeft?: OnSwipingDirectionCallback;
+    onSwipedUp?: OnSwipedDirectionCallback;
+    onSwipedRight?: OnSwipedDirectionCallback;
+    onSwipedDown?: OnSwipedDirectionCallback;
+    onSwipedLeft?: OnSwipedDirectionCallback;
+    onTap?: OnTapCallback;
+    flickThreshold?: number;
+    delta?: number;
+    preventDefaultTouchmoveEvent?: boolean;
+    stopPropagation?: boolean;
+    nodeName?: string;
+    trackMouse?: boolean;
+    children?: React.ReactNode;
 }
+
+declare class ReactSwipeable extends React.Component<Props, void> {}
+declare namespace ReactSwipeable {}
+
+export = ReactSwipeable;
