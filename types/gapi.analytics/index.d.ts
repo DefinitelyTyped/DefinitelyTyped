8e1a93ae7fcd786654d76d788c4a6af8b2fe8ac9
Tanguy Krotoff <tkrotoff@gmail.com>
2017-06-06 01:45:53 +0200

    Improve gapi HttpRequestPromise thanks to TypeScript 2.3 (#16194)
    * Improve gapi HttpRequestPromise thanks to TypeScript 2.3
    
    * Fix typings that use gapi

--- types/gapi.analytics/index.d.ts
+++ types/gapi.analytics/index.d.ts
@@ -2,6 +2,7 @@
 // Project: https://developers.google.com/analytics/devguides/reporting/core/v4/
 // Definitions by: César Costas <https://github.com/gatsbimantico>
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
+// TypeScript Version: 2.3
 
 /// <reference types="gapi" />
 
