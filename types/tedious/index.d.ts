33853d9ad97d4af4155675b4a6e41913bf072561
Chris Thompson <chris@thompson-web.org>
2017-06-05 17:50:52 -0600

    Add ConnectionError and RequestError

--- types/tedious/index.d.ts
+++ types/tedious/index.d.ts
@@ -548,3 +548,23 @@ export declare class Connection extends events.EventEmitter {
     close(): void;
 
 }
+
+/**
+ * Error emitted by Connection class 'error' event
+ */
+export declare class ConnectionError extends Error {
+    constructor(message?: string, code?: number);
+
+    public message: string;
+    public code: number;
+}
+
+/**
+ * Error emitted by Request class
+ */
+export declare class RequestError extends Error {
+    constructor(message?: string, code?: number);
+
+    public message: string;
+    public code: number;
+}
