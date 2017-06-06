2165e560e8e3461b103ffa3201b5f17ed2515a25
Denis <carriere.denis@gmail.com>
2017-06-06 14:27:27 -0400

    Update replacer option

--- types/write-json-file/write-json-file-tests.ts
+++ types/write-json-file/write-json-file-tests.ts
@@ -4,6 +4,16 @@ import * as write from 'write-json-file';
 write('foo.json', {foo: true}).then(() => { });
 write.sync('foo.json', {foo: true});
 
+// With options
+const options: write.Options = {
+    indent: '  ',
+    detectIndent: true,
+    sortKeys: true,
+    mode: 0o666
+};
+write('foo.json', {foo: true}, options).then(() => { });
+write.sync('foo.json', {foo: true}, options);
+
 // JSON.stringify replacer
 function replacer(key: any, value: any) {
   // Filtering out properties
@@ -13,13 +23,12 @@ function replacer(key: any, value: any) {
   return value;
 }
 
-// With options
-const options: write.Options = {
-    indent: '  ',
-    detectIndent: true,
-    sortKeys: true,
-    replacer,
-    mode: 0o666
-};
-write('foo.json', {foo: true}, options).then(() => { });
-write.sync('foo.json', {foo: true}, options);
+write.sync('foo.json', {foo: true}, {replacer});
+write.sync('foo.json', {foo: true}, {replacer: null});
+write.sync('foo.json', {foo: true}, {replacer: [1, 2, 3]});
+write.sync('foo.json', {foo: true}, {replacer: ['a', 'b', 'c']});
+
+JSON.stringify({foo: true}, replacer);
+JSON.stringify({foo: true}, null);
+JSON.stringify({foo: true}, [1, 2, 3]);
+JSON.stringify({foo: true}, ['a', 'b', 'c']);
