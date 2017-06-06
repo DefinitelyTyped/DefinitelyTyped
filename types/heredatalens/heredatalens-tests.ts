71b90dcce74d5fbe9f20c85d9e322d5e1b6bf41e
Bernd Hacker <hacker.bernd@gmail.com>
2017-06-06 16:13:40 +0200

    fix tests and dtslint and improve typings

--- types/heredatalens/heredatalens-tests.ts
+++ types/heredatalens/heredatalens-tests.ts
@@ -6,10 +6,6 @@
  * are not intended as functional tests.
  */
 
-import './index';
-
-
-let service: H.datalens.Service;
 let serviceOptions: H.datalens.Service.Options = {
   subDomain: 'sub',
   version: 'version',
@@ -31,26 +27,23 @@ let queryTileProviderOptions: H.datalens.QueryTileProvider.Options = {
   tileParamNames: queryTileProviderTileParamNames
 };
 
+let service: H.datalens.Service = new H.datalens.Service(serviceOptions);
+service.request('method', 'endpoint', 'params', 'body', (result: any) => {}, (error: any) => {}).then((data: any) => {});
+service.fetchQueryData('id', 'params', (result: any) => {}, (error: any) => {}).then((data: any) => {});
+service.fetchQueryStats('id', 'query', (result: any) => {}, (error: any) => {}).then((data: any) => {});
+service.fetchLayer('name', 'params', (result: any) => {}, (error: any) => {}).then((data: any) => {});
+service.fetchLayerTile('name', 1, 2, 3, 'params', (result: any) => {}, (error: any) => {}).then((data: any) => {});
+service.setTokens('token', 'refresh_token');
+service.configure('app_id', 'app_code', true, true, new H.service.Url('scheme', 'host'));
+
 let queryTileProvider = new H.datalens.QueryTileProvider(service, queryTileProviderOptions);
 queryTileProvider.setQueryId('some id');
 queryTileProvider.setQueryParams('some params');
 queryTileProvider.setTileParamNames(queryTileProviderTileParamNames);
 
-
-service = new H.datalens.Service(serviceOptions);
-service.request('method', 'endpoint', 'params', 'body', function(result) {}, function(error) {}).then(data => {});
-service.fetchQueryData('id', 'params', function(result) {}, function(error) {}).then(data => {});
-service.fetchQueryStats('id', 'query', function(result) {}, function(error) {}).then(data => {});
-service.fetchLayer('name', 'params', function(result) {}, function(error) {}).then(data => {});
-service.fetchLayerTile('name', 1, 2, 3, 'params', function(result) {}, function(error) {}).then(data => {});
-service.setTokens('token', 'refresh_token');
-service.configure('app_id', 'app_code', true, true, new H.service.Url('scheme', 'host'));
-
-
 let provider = new H.datalens.Provider({ columns: ['1', '2'], rows: [[], []]});
 provider.getData();
 provider.setData({ columns: ['3', '4'], rows: [[], []]});
-
 
 let queryProviderOptions: H.datalens.QueryProvider.Options = {
   queryId: 'id',
@@ -63,26 +56,24 @@ queryProvider.reload();
 queryProvider.getData();
 queryProvider.setData({ columns: ['3', '4'], rows: [[], []]});
 
-
 let rasterLayerOptions: H.datalens.RasterLayer.Options = {
-  dataToRows: (): H.datalens.RasterLayer.Row[] => { return []; },
-  rowToTilePoint: (): H.datalens.RasterLayer.TilePoint => { return { x: 1, y: 2 }; },
-  buffer: (): number => { return 1; },
+  dataToRows: (): H.datalens.RasterLayer.Row[] => [],
+  rowToTilePoint: (): H.datalens.RasterLayer.TilePoint => ({ x: 1, y: 2 }),
+  buffer: (): number => 1,
   renderTile: () => {}
 };
 
 let rasterLayer = new H.datalens.RasterLayer();
 rasterLayer.redraw();
 
-
 let heatmapLayerOptions: H.datalens.HeatmapLayer.Options = {
-  dataToRows: (): H.datalens.HeatmapLayer.Row[] => { return []; },
-  rowToTilePoint: (): H.datalens.HeatmapLayer.TilePoint => { return { x: 1, y: 2, value: 5, count: 5 }; },
+  dataToRows: (): H.datalens.HeatmapLayer.Row[] => [],
+  rowToTilePoint: (): H.datalens.HeatmapLayer.TilePoint => ({ x: 1, y: 2, value: 5, count: 5 }),
   bandwidth: 5,
-  valueRange: (): number[] => { return []; },
-  countRange: (): number[] => { return []; },
-  colorScale: (): string => { return '2'; },
-  alphaScale: (): number => { return 1; },
+  valueRange: (): number[] => [],
+  countRange: (): number[] => [],
+  colorScale: (): string => '2',
+  alphaScale: (): number => 1,
   aggregation: H.datalens.HeatmapLayer.Aggregation.SUM,
   inputScale: H.datalens.HeatmapLayer.InputScale.LINEAR
 };
@@ -92,28 +83,26 @@ heatmapLayer.getOptionsPerZoom(5);
 heatmapLayer.dispose();
 heatmapLayer.redraw();
 
-
 let objectLayerOptions: H.datalens.ObjectLayer.Options = {
-  dataToRows: (): H.datalens.ObjectLayer.Row[] => { return []; },
-  rowToMapObject: (): H.map.Object => { return new H.map.Object() },
-  rowToStyle: (): H.datalens.ObjectLayer.ObjectStyleOptions => { return { icon: new H.map.Icon('x'), style: {}, arrows: {}, zIndex: 1 }; },
+  dataToRows: (): H.datalens.ObjectLayer.Row[] => [],
+  rowToMapObject: (): H.map.Object => new H.map.Object(),
+  rowToStyle: (): H.datalens.ObjectLayer.ObjectStyleOptions => ({ icon: new H.map.Icon('x'), style: {}, arrows: {}, zIndex: 1 }),
   dataDomains: 2,
-  clustering: { rowToDataPoint: (): H.clustering.DataPoint => { return; }, options: (): H.clustering.Provider.ClusteringOptions => { return {}; } }
-}
+  clustering: { rowToDataPoint: (): H.clustering.DataPoint => ({ lat: 12.1, lng: 1.12, alt: 5, ctx: H.geo.AltitudeContext.GL, wt: 2, data: {} }),
+                options: (): H.clustering.Provider.ClusteringOptions => ({}) }
+};
 
 let objectLayer = new H.datalens.ObjectLayer(provider, objectLayerOptions);
 objectLayer.redraw();
-objectLayer.updateObjectStyle(new H.map.Object, { icon: new H.map.Icon('x'), style: {}, arrows: {}, zIndex: 1 });
-
+objectLayer.updateObjectStyle(new H.map.Object(), { icon: new H.map.Icon('x'), style: {}, arrows: {}, zIndex: 1 });
 
 let rawDataProviderOptions: H.datalens.RawDataProvider.Options = {
   dataUrl: 'url',
-  dataToFeatures: (): H.datalens.RawDataProvider.Feature[] => { return []; },
-  featuresToRows: (): H.datalens.ObjectLayer.Row[] => { return []; }
+  dataToFeatures: (): H.datalens.RawDataProvider.Feature[] => [],
+  featuresToRows: (): H.datalens.ObjectLayer.Row[] => []
 };
 
 let rawDataProvider = new H.datalens.RawDataProvider(rawDataProviderOptions);
-
 
 let spatialTileProviderOptions: H.datalens.SpatialTileProvider.Options = {
   layerName: 'name',
@@ -123,14 +112,12 @@ let spatialTileProviderOptions: H.datalens.SpatialTileProvider.Options = {
 let spatialProvider: H.datalens.SpatialTileProvider = new H.datalens.SpatialTileProvider(service, spatialTileProviderOptions);
 
 let spatialLayerOptions: H.datalens.SpatialLayer.Options = {
-  dataToRows: (): H.datalens.SpatialLayer.Row[] => { return []; },
-  rowToSpatialId: (): string => { return 'asd'; },
-  featureToSpatialId: (): string => { return 'asd'; },
+  dataToRows: (): H.datalens.SpatialLayer.Row[] => [],
+  rowToSpatialId: (): string => 'asd',
+  featureToSpatialId: (): string => 'asd',
   rowToStyle: (): any => { return; },
   defaultStyle: (): any => { return; },
   transformFeature: 1
 };
 
 let spatialLayer = new H.datalens.SpatialLayer(provider, spatialProvider, spatialLayerOptions);
-
-
