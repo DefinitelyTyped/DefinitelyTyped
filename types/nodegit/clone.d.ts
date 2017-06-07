0c0761a2f4b457788e74260f6a730de4ed785f8e
Dolan <dolan_miu@hotmail.com>
2017-06-07 03:09:58 +0100

    Added Clone and co

---
+++ types/nodegit/clone.d.ts
@@ -0,0 +1,16 @@
+import { Repository } from './repository';
+import { CloneOptions } from './clone-options';
+
+export namespace Clone {
+    enum LOCAL {
+        AUTO = 0,
+        LOCAL = 1,
+        NO_LOCAL = 2,
+        NO_LINKS = 3
+    }
+}
+
+export class Clone {
+    static clone(url: string, local_path: string, options?: CloneOptions): Promise<Repository>;
+    static initOptions(opts: CloneOptions, version: number): number;
+}
