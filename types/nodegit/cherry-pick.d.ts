959bd0bf845e080e2fda767e9edad8dacd35e6a3
Dolan <dolan_miu@hotmail.com>
2017-06-07 23:51:00 +0100

    Added Cherrypick

---
+++ types/nodegit/cherry-pick.d.ts
@@ -0,0 +1,9 @@
+import { Repository } from './repository';
+import { Commit } from './commit';
+import { MergeOptions } from './merge-options';
+
+export class Cherrypick {
+    static cherrypick(repo: Repository, commit: Commit, options?: CherrypickOptions): Promise<number>;
+    static commit(repo: Repository, cherrypickCommit: Commit, ourCommit: Commit, mainline: number, mergeOptions?: MergeOptions): Promise<number>;
+    static initOptions(opts: CherrypickOptions, version: number): number;
+}
