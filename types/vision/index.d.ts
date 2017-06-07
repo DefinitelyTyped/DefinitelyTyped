5168f91568af3f81636da980dc902a95adba992f
Niklas Wulf <Kampfgnom@users.noreply.github.com>
2017-06-07 15:37:05 +0200

    [Hapi] Fix input and response validation function signatures (#16802)
    * [Hapi] Fix input and response validation function signatures
    
    Fixes #16750
    
    * [Hapi] Allow custom validation options in route input and response
    
    * [Hapi] Adjust validation contexts to match Hapi version 16.1.1
    
    * [Hapi] Adjust test/route/validate.ts to match newest changes
    
    * [Hapi] Add custom options to response validation test
    
    * [Hapi] Update TypeScript version to 2.3 in packages that import Hapi
    
    * [Hapi] Clarify documentation on validation function value, allow null

--- types/vision/index.d.ts
+++ types/vision/index.d.ts
@@ -2,7 +2,7 @@
 // Project: https://github.com/hapijs/vision
 // Definitions by: Jason Swearingen <http://github.com/jasonswearingen>, AJP <https://github.com/AJamesPhillips>
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
-// TypeScript Version: 2.2
+// TypeScript Version: 2.3
 
 import * as Hapi from 'hapi';
 
