498c2392a18eaf36f5b3dc0502a13fb9c2d85d71
Dolan <dolan_miu@hotmail.com>
2017-06-07 02:14:52 +0100

    Added Indexer

---
+++ types/nodegit/indexer.d.ts
@@ -0,0 +1,8 @@
+import { TransferProgress } from './transfer-progress';
+import { Oid } from './oid';
+
+export class Indexer {
+    commit(stats: TransferProgress): number;
+    free(): void;
+    hash(): Oid;
+}
