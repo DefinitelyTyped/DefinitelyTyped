b54f72f4fdc599ec885558653073b74dfa2efd99
Dolan <dolan_miu@hotmail.com>
2017-06-08 02:22:49 +0100

    Added documentation for Branch

--- types/nodegit/branch.d.ts
+++ types/nodegit/branch.d.ts
@@ -12,14 +12,116 @@ export namespace Branch {
 }
 
 export class Branch {
-    static create(repo: Repository, branch_name: string, target: Commit, force: number): Promise<Reference>;
-    static createFromAnnotated(repository: Repository, branch_name: string, commit: AnnotatedCommit, force: number): Reference;
+    /**
+     *
+     *
+     * @static
+     * @param {Repository} repo
+     * @param {string} branchName
+     * @param {Commit} target
+     * @param {number} force
+     * @returns {Promise<Reference>}
+     *
+     * @memberof Branch
+     */
+    static create(repo: Repository, branchName: string, target: Commit, force: number): Promise<Reference>;
+    /**
+     *
+     *
+     * @static
+     * @param {Repository} repository
+     * @param {string} branchName
+     * @param {AnnotatedCommit} commit
+     * @param {number} force
+     * @returns {Reference}
+     *
+     * @memberof Branch
+     */
+    static createFromAnnotated(repository: Repository, branchName: string, commit: AnnotatedCommit, force: number): Reference;
+    /**
+     *
+     *
+     * @static
+     * @param {Reference} branch
+     * @returns {number}
+     *
+     * @memberof Branch
+     */
     static delete(branch: Reference): number;
+    /**
+     *
+     *
+     * @static
+     * @param {Reference} branch
+     * @returns {number}
+     *
+     * @memberof Branch
+     */
     static isHead(branch: Reference): number;
-    static iteratorNew(repo: Repository, list_flags: number): Promise<any>;
-    static lookup(repo: Repository, branch_name: string, branch_type: number): Promise<Reference>;
-    static move(branch: Reference, new_branch_name: string, force: number): Promise<Reference>;
+    /**
+     *
+     *
+     * @static
+     * @param {Repository} repo
+     * @param {number} listFlags
+     * @returns {Promise<any>}
+     *
+     * @memberof Branch
+     */
+    static iteratorNew(repo: Repository, listFlags: number): Promise<any>;
+    /**
+     *
+     *
+     * @static
+     * @param {Repository} repo
+     * @param {string} branchName
+     * @param {number} branchType
+     * @returns {Promise<Reference>}
+     *
+     * @memberof Branch
+     */
+    static lookup(repo: Repository, branchName: string, branchType: number): Promise<Reference>;
+    /**
+     *
+     *
+     * @static
+     * @param {Reference} branch
+     * @param {string} newBranchName
+     * @param {number} force
+     * @returns {Promise<Reference>}
+     *
+     * @memberof Branch
+     */
+    static move(branch: Reference, newBranchName: string, force: number): Promise<Reference>;
+    /**
+     *
+     *
+     * @static
+     * @param {Reference} ref
+     * @returns {Promise<string>}
+     *
+     * @memberof Branch
+     */
     static name(ref: Reference): Promise<string>;
-    static setUpstream(branch: Reference, upstream_name: string): Promise<number>;
+    /**
+     *
+     *
+     * @static
+     * @param {Reference} branch
+     * @param {string} upstreamName
+     * @returns {Promise<number>}
+     *
+     * @memberof Branch
+     */
+    static setUpstream(branch: Reference, upstreamName: string): Promise<number>;
+    /**
+     *
+     *
+     * @static
+     * @param {Reference} branch
+     * @returns {Promise<Reference>}
+     *
+     * @memberof Branch
+     */
     static upstream(branch: Reference): Promise<Reference>;
 }
