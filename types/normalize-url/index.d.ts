cac84aec6f7e947dda842910e13be180743989a7
odin3 <ascii@live.ru>
2017-06-08 20:11:18 +0300

    Add types for normalize-url

---
+++ types/normalize-url/index.d.ts
@@ -0,0 +1,20 @@
+// Type definitions for normalize-url v1.9.1
+// Project: https://github.com/sindresorhus/normalize-url
+// Definitions by: odin3 <https://github.com/odin3>
+// Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
+
+declare module NormalizeUrl {
+  interface INormalizeUrlArgs {
+		normalizeProtocol: boolean;
+		normalizeHttps: boolean;
+		stripFragment: boolean;
+		stripWWW: boolean;
+		removeQueryParameters: string[];
+		removeTrailingSlash: boolean;
+		removeDirectoryIndex: RegExp[];
+	}
+								
+	function normalizeUrl(url: string, args?: INormalizeUrlArgs): string;
+}
+
+export = NormalizeUrl.normalizeUrl;
