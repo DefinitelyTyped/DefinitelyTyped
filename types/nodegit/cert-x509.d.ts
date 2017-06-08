5217c986509016f65bcc5f63d2be8c385bd2b9c7
Dolan <dolan_miu@hotmail.com>
2017-06-08 02:35:38 +0100

    Added documentation for CertX509

--- types/nodegit/cert-x509.d.ts
+++ types/nodegit/cert-x509.d.ts
@@ -1,5 +1,25 @@
+import { Cert } from './cert';
+
 export class CertX509 {
-    certType: number;
+    /**
+     *
+     *
+     * @type {Buffer}
+     * @memberof CertX509
+     */
     data: Buffer;
+    /**
+     *
+     *
+     * @type {number}
+     * @memberof CertX509
+     */
     len: number;
+    /**
+     *
+     *
+     * @type {Cert}
+     * @memberof CertX509
+     */
+    parent: Cert;
 }
