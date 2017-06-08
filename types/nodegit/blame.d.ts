20499792a7a8215cb4922291466683fed97cf173
Dolan <dolan_miu@hotmail.com>
2017-06-08 02:13:24 +0100

    Added documentation to Blame

--- types/nodegit/blame.d.ts
+++ types/nodegit/blame.d.ts
@@ -14,12 +14,71 @@ export namespace Blame {
 }
 
 export class Blame {
+    /**
+     * Retrieve the blame of a file
+     *
+     * @static
+     * @param {Repository} repo - Repository that contains the file
+     * @param {string} path - to the file to get the blame of
+     * @param {BlameOptions} [options] - Options for the blame
+     * @returns {Blame}
+     *
+     * @memberof Blame
+     */
     static file(repo: Repository, path: string, options?: BlameOptions): Blame;
+    /**
+     *
+     *
+     * @static
+     * @param {BlameOptions} opts - The git_blame_options struct to initialize
+     * @param {number} version - Version of struct; pass GIT_BLAME_OPTIONS_VERSION
+     * @returns {number}
+     *
+     * @memberof Blame
+     */
     static initOptions(opts: BlameOptions, version: number): number;
 
-    buffer(buffer: string, buffer_len: number): Promise<Blame>;
+    /**
+     *
+     *
+     * @param {string} buffer
+     * @param {number} bufferLen
+     * @returns {Promise<Blame>}
+     *
+     * @memberof Blame
+     */
+    buffer(buffer: string, bufferLen: number): Promise<Blame>;
+    /**
+     *
+     *
+     *
+     * @memberof Blame
+     */
     free(): void;
+    /**
+     *
+     *
+     * @param {number} index
+     * @returns {BlameHunk} - the hunk at the given index, or NULL on error
+     *
+     * @memberof Blame
+     */
     getHunkByIndex(index: number): BlameHunk;
-    getHunkByLine(lineno: number): BlameHunk;
+    /**
+     *
+     *
+     * @param {number} lineNo
+     * @returns {BlameHunk} - 	the hunk that contains the given line, or NULL on error
+     *
+     * @memberof Blame
+     */
+    getHunkByLine(lineNo: number): BlameHunk;
+    /**
+     *
+     *
+     * @returns {number}
+     *
+     * @memberof Blame
+     */
     getHunkCount(): number;
 }
