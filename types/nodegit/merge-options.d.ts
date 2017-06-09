21a5f52f750a3034f1a6ff00d98c7979d61fc4f0
Dolan <dolan_miu@hotmail.com>
2017-06-09 01:33:54 +0100

    Added documentation

--- types/nodegit/merge-options.d.ts
+++ types/nodegit/merge-options.d.ts
@@ -1,8 +1,58 @@
-export interface MergeOptions {
-    version: number;
-    treeFlags: number;
-    renameThreshold: number;
-    targetLimit: number;
-    fileFavor: number;
-    fileFlags: number;
+export class MergeOptions {
+    /**
+     *
+     *
+     * @type {number}
+     * @memberof MergeOptions
+     */
+    version?: number;
+    /**
+     *
+     *
+     * @type {number}
+     * @memberof MergeOptions
+     */
+    renameThreshold?: number;
+    /**
+     *
+     *
+     * @type {number}
+     * @memberof MergeOptions
+     */
+    targetLimit?: number;
+    /**
+     *
+     *
+     * @type {number}
+     * @memberof MergeOptions
+     */
+    fileFavor?: number;
+    /**
+     *
+     *
+     * @type {number}
+     * @memberof MergeOptions
+     */
+    fileFlags?: number;
+    /**
+     *
+     *
+     * @type {string}
+     * @memberof MergeOptions
+     */
+    defaultDriver?: string;
+    /**
+     *
+     *
+     * @type {number}
+     * @memberof MergeOptions
+     */
+    flags?: number;
+    /**
+     *
+     *
+     * @type {number}
+     * @memberof MergeOptions
+     */
+    recursionLimit?: number;
 }
--- types/nodegit/note.d.ts
+++ types/nodegit/note.d.ts
@@ -3,16 +3,122 @@ import {Signature} from './signature';
 import {Oid} from './oid';
 
 export class Note {
+    /**
+     *
+     *
+     * @static
+     * @param {Repository} repo
+     * @param {string} notes_ref
+     * @param {Signature} author
+     * @param {Signature} committer
+     * @param {Oid} oid
+     * @param {string} note
+     * @param {number} force
+     * @returns {Promise<Oid>}
+     *
+     * @memberof Note
+     */
     static create(repo: Repository, notes_ref: string, author: Signature, committer: Signature, oid: Oid, note: string, force: number): Promise<Oid>;
+    /**
+     *
+     *
+     * @static
+     * @param {Repository} repo
+     * @param {string} notes_ref
+     * @param {Function} note_cb
+     * @param {*} payload
+     * @returns {Promise<number>}
+     *
+     * @memberof Note
+     */
     static foreach(repo: Repository, notes_ref: string, note_cb: Function, payload: any): Promise<number>;
+    /**
+     *
+     *
+     * @static
+     * @param {Repository} repo
+     * @param {string} notes_ref
+     * @returns {Promise<any>}
+     *
+     * @memberof Note
+     */
     static iteratorNew(repo: Repository, notes_ref: string): Promise<any>;
+    /**
+     *
+     *
+     * @static
+     * @param {Oid} note_id
+     * @param {Oid} annotated_id
+     * @param {*} it
+     * @returns {number}
+     *
+     * @memberof Note
+     */
     static next(note_id: Oid, annotated_id: Oid, it: any): number;
+    /**
+     *
+     *
+     * @static
+     * @param {Repository} repo
+     * @param {string} notes_ref
+     * @param {Oid} oid
+     * @returns {Promise<Note>}
+     *
+     * @memberof Note
+     */
     static read(repo: Repository, notes_ref: string, oid: Oid): Promise<Note>;
+    /**
+     *
+     *
+     * @static
+     * @param {Repository} repo
+     * @param {string} notes_ref
+     * @param {Signature} author
+     * @param {Signature} committer
+     * @param {Oid} oid
+     * @returns {Promise<number>}
+     *
+     * @memberof Note
+     */
     static remove(repo: Repository, notes_ref: string, author: Signature, committer: Signature, oid: Oid): Promise<number>;
 
+    /**
+     *
+     *
+     * @returns {Signature}
+     *
+     * @memberof Note
+     */
     author(): Signature;
+    /**
+     *
+     *
+     * @returns {Signature}
+     *
+     * @memberof Note
+     */
     committer(): Signature;
+    /**
+     *
+     *
+     *
+     * @memberof Note
+     */
     free(): void;
+    /**
+     *
+     *
+     * @returns {Oid}
+     *
+     * @memberof Note
+     */
     id(): Oid;
+    /**
+     *
+     *
+     * @returns {string}
+     *
+     * @memberof Note
+     */
     message(): string;
 }
