df4bb363e25a8f6c239feb47a32517ead9ee9b31
Denys Sedchenko <dsedchenko@llnw.com>
2017-06-09 12:57:22 +0300

    Add tslint.json and add style fixes

--- types/normalize-url/index.d.ts
+++ types/normalize-url/index.d.ts
@@ -1,20 +1,21 @@
-// Type definitions for normalize-url v1.9.1
+// Type definitions for normalize-url 1.9
 // Project: https://github.com/sindresorhus/normalize-url
 // Definitions by: odin3 <https://github.com/odin3>
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
 
-declare module NormalizeUrl {
-  interface INormalizeUrlArgs {
-		normalizeProtocol: boolean;
-		normalizeHttps: boolean;
-		stripFragment: boolean;
-		stripWWW: boolean;
-		removeQueryParameters: string[];
-		removeTrailingSlash: boolean;
-		removeDirectoryIndex: RegExp[];
+declare namespace NormalizeUrl {
+  interface NormalizeUrlArgs {
+		normalizeProtocol?: boolean;
+		normalizeHttps?: boolean;
+		stripFragment?: boolean;
+		stripWWW?: boolean;
+		removeQueryParameters?: string[];
+		removeTrailingSlash?: boolean;
+		removeDirectoryIndex?: RegExp[];
 	}
 
-	function normalizeUrl(url: string, args?: INormalizeUrlArgs): string;
+  // tslint:disable-next-line align
+	function normalizeUrl(url: string, args?: NormalizeUrlArgs): string;
 }
 
 export = NormalizeUrl.normalizeUrl;
