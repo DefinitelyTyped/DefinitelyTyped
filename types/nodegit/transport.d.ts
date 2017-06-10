b74bf960f39d7ec7a086b3a92ea4cf6a207ec920
Dolan <dolan_miu@hotmail.com>
2017-06-10 02:30:11 +0100

    Added more documentation

--- types/nodegit/transport.d.ts
+++ types/nodegit/transport.d.ts
@@ -1,5 +1,6 @@
 import { Remote } from './remote';
 import { Strarray } from './str-array';
+import { Cert } from './cert';
 
 export namespace Transport {
     const enum FLAGS {
@@ -8,7 +9,45 @@ export namespace Transport {
 }
 
 export class Transport {
+    /**
+     * å
+     *
+     * @static
+     * @param {Remote} owner
+     * @param {Strarray} payload
+     * @returns {Promise<Transport>}
+     *
+     * @memberof Transport
+     */
     static sshWithPaths(owner: Remote, payload: Strarray): Promise<Transport>;
+    /**
+     *
+     *
+     * @static
+     * @param {string} prefix
+     * @returns {number}
+     *
+     * @memberof Transport
+     */
     static unregister(prefix: string): number;
+    /**
+     *
+     *
+     * @param {number} version
+     * @returns {number}
+     *
+     * @memberof Transport
+     */
     init(version: number): number;
+    /**
+     *
+     *
+     * @param {Cert} cert
+     * @param {number} valid
+     * @param {string} hostName
+     * @returns {number}
+     *
+     * @memberof Transport
+     */
+    smartCertificateCheck(cert: Cert, valid: number, hostName: string): number;
 }
