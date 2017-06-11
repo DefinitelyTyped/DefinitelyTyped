94330023d9fe246f72c306086d683c3a4b4946e6
Cyril Schumacher <cyril_schumacher@live.fr>
2017-06-11 13:37:59 +0200

    Update bunnymq definition and tests.

--- types/bunnymq/bunnymq-tests.ts
+++ types/bunnymq/bunnymq-tests.ts
@@ -1,19 +1,23 @@
 import * as bunnymq from "bunnymq";
 
 // Basic usage
-var instance = bunnymq({ host: 'amqp://localhost' });
+var instance = bunnymq({ host: "amqp://localhost" });
 
 // Publisher
 instance.publish("queue:name", "message");
+instance.producer.produce("queue:name", "message");
+
 // Subscriber
-instance.subscribe('queue:name', (message: string) => "hello,world");
+instance.subscribe("queue:name", (message: string) => "response");
+instance.consumer.consume("queue:name", (message: string) => "response");
 
 // RPC Support
-instance.publish('queue:name', { message: 'content' }, { routingKey: 'my-routing-key', rpc: true }).then((consumerResponse: string) => { });
+instance.publish("queue:name", { message: "content" }, { routingKey: "my-routing-key", rpc: true, timeout: 1000 }).then((consumerResponse: string) => "response");
+instance.producer.produce("queue:name", { message: "content" }, { rpc: true, routingKey: "my-routing-key", timeout: 1000 });
 
 // Config
 var custom = bunnymq({
-    host: 'amqp://localhost',
+    host: "amqp://localhost",
     prefetch: 5,
     requeue: true,
     timeout: 1000,
