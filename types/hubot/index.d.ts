94a1825a2572857271d183993d98c9a7368cdf95
Dirk Gadsden <dirk@esherido.com>
2017-06-10 21:28:25 -0700

    `dts-gen` template for type definitions for `hubot`

---
+++ types/hubot/index.d.ts
@@ -0,0 +1,4 @@
+// Type definitions for hubot 2.19
+// Project: https://github.com/github/hubot
+// Definitions by: Dirk Gadsden <https://github.com/dirk>
+// Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
