df992d2f940ded0fff867386c62bbc4fb3078799
Yui T <yuisu@microsoft.com>
2017-06-11 21:27:18 -0700

    Remove test failure cause

--- types/cordova-plugin-battery-status/cordova-plugin-battery-status-tests.ts
+++ types/cordova-plugin-battery-status/cordova-plugin-battery-status-tests.ts
@@ -1,4 +1,3 @@
-/// <reference path="../cordova-plugin-inappbrowser/index.d.ts" />
 window.addEventListener('batterystatus', (ev) => { console.log('Battery level is ' + ev.level); });
 window.addEventListener('batterycritical', (ev) => { console.log('Battery is critical: ' + ev.level); });
 window.addEventListener('batterylow', (ev) => { console.log('Battery is low: ' + ev.level); });
