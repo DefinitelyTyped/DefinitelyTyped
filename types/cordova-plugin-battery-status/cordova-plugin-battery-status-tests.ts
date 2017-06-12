127ed8a779e7f6d7c84c09134be141a3122524bc
Yui T <yuisu@microsoft.com>
2017-06-11 20:46:21 -0700

    Update tests

--- types/cordova-plugin-battery-status/cordova-plugin-battery-status-tests.ts
+++ types/cordova-plugin-battery-status/cordova-plugin-battery-status-tests.ts
@@ -1,5 +1,11 @@
-window.addEventListener('batterystatus',
-    (ev: BatteryStatusEvent) => { console.log('Battery level is ' + ev.level); });
+/// <reference path="../cordova-plugin-inappbrowser/index.d.ts" />
+window.addEventListener('batterystatus', (ev) => { console.log('Battery level is ' + ev.level); });
+window.addEventListener('batterycritical', (ev) => { console.log('Battery is critical: ' + ev.level); });
+window.addEventListener('batterylow', (ev) => { console.log('Battery is low: ' + ev.level); });
+
+window.removeEventListener('batterystatus', (ev) => { console.log('Remove: Battery level is ' + ev.level); });
+window.removeEventListener('batterycritical', (ev) => { console.log('Remove: Battery is critical: ' + ev.level); });
+window.removeEventListener('batterylow', (ev) => { console.log('Remove: Battery is low: ' + ev.level); });
 
 window.addEventListener('batterycritical',
     () => { alert('Battery is critical low!'); });
