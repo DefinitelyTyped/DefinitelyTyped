5d6ce14e9aa4e58300b208f4e89f8dc1bb29c9fc
Kanchalai Tanglertsampan <yuisu@microsoft.com>
2017-06-13 11:21:40 -0700

    Update tests

--- types/cordova-plugin-inappbrowser/cordova-plugin-inappbrowser-tests.ts
+++ types/cordova-plugin-inappbrowser/cordova-plugin-inappbrowser-tests.ts
@@ -5,17 +5,28 @@
 // is similar to native window.open signature, so the compiler can's
 // select proper overload, but we cast result to InAppBrowser manually.
 var iab = <InAppBrowser>window.open('google.com', '_self');
+
 iab.addEventListener('loadstart', (ev: InAppBrowserEvent) => { console.log('Start opening ' + ev.url); });
 iab.addEventListener('loadstart', (ev) => { console.log('loadstart' + ev.url); });
 iab.addEventListener('loadstop', (ev) => { console.log('loadstop' + ev.code); });
 iab.addEventListener('loaderror', (ev) => { console.log('loaderror' + ev.code); });
 iab.addEventListener('exit', (ev) => { console.log('exit' + ev.code); });
 
-iab.removeEventListener('loadstart', (ev: InAppBrowserEvent) => { console.log('Remove loadstart' + ev.url); });
-iab.removeEventListener('loadstart', (ev) => { console.log('Remove loadstart' + ev.url); });
-iab.removeEventListener('loadstop', (ev) => { console.log('Remove loadstop ' + ev.code); });
-iab.removeEventListener('loaderror', (ev) => { console.log('Remove loaderror ' + ev.code); });
-iab.removeEventListener('exit', (ev) => { console.log('Remove exit ' + ev.code); });
+function inAppBrowserCallBack(ev: InAppBrowserEvent) {
+    console.log('InAppBrowser callback ' + ev.url);
+}
+
+iab.addEventListener('loadstart', inAppBrowserCallBack);
+iab.addEventListener('loadstart', inAppBrowserCallBack);
+iab.addEventListener('loadstop', inAppBrowserCallBack);
+iab.addEventListener('loaderror', inAppBrowserCallBack);
+iab.addEventListener('exit', inAppBrowserCallBack);
+
+iab.removeEventListener('loadstart', inAppBrowserCallBack);
+iab.removeEventListener('loadstop', inAppBrowserCallBack);
+iab.removeEventListener('loaderror', inAppBrowserCallBack);
+iab.removeEventListener('exit', inAppBrowserCallBack);
+
 iab.show();
 iab.executeScript(
     { code: "console.log('Injected script in action')" },
