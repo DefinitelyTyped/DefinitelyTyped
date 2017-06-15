e34616fd3c94b0b4a1fc8a13c2becd801cdd97ba
abrahambotros <abrahambotros@gmail.com>
2017-06-14 22:58:27 -0700

    [react-navigation] Improve test, add screen component props type checks

--- types/react-navigation/react-navigation-tests.tsx
+++ types/react-navigation/react-navigation-tests.tsx
@@ -2,6 +2,7 @@ import * as React from 'react';
 import { View } from 'react-native';
 import {
     addNavigationHelpers,
+    NavigationScreenProps,
     NavigationStackAction,
     NavigationTabScreenOptions,
     StackNavigator,
@@ -10,14 +11,33 @@ import {
     TabBarBottom,
 } from 'react-navigation';
 
-const Start = (
-    <View />
-);
+/**
+ * @class StartScreen @extends React.Component
+ * @desc Simple screen component class with typed component props that should
+ *     receive the navigation prop from the AppNavigator.
+ */
+interface StartScreenComponentProps {
+    id: number,
+    s: string,
+}
+interface StartScreenProps extends NavigationScreenProps<StartScreenComponentProps> { }
+class StartScreen extends React.Component<StartScreenProps, {}> {
+    render() {
+        // Implicit type checks.
+        const navigationStateParams: StartScreenComponentProps = this.props.navigation.state.params;
+        const id = this.props.navigation.state.params.id;
+        const s = this.props.navigation.state.params.s;
+
+        return (
+            <View />
+        );
+    }
+}
 
 export const AppNavigator = StackNavigator({
     StartImage: {
         path: 'startImage',
-        screen: Start,
+        screen: StartScreen,
     },
 }, {
     initialRouteName: 'StartImage',
