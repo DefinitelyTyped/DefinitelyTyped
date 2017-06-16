c77ba6bb5178ff4afa8c300ab26c7c658384b909
Nathan Shively-Sanders <nathansa@microsoft.com>
2017-06-16 10:19:12 -0700

    Fix incomplete merge

--- types/request-promise/request-promise-tests.ts
+++ types/request-promise/request-promise-tests.ts
@@ -427,32 +427,7 @@ request.get('http://10.255.255.1', {timeout: 1500}, err => {
     process.exit(0);
 });
 
-<<<<<<< HEAD
-var rand = Math.floor(Math.random()*100000000).toString();
-  request(
-    { method: 'PUT'
-    , uri: 'http://mikeal.iriscouch.com/testjs/' + rand
-    , multipart:
-      [ {
-          headers: { 'content-type': 'application/json' }
-        ,  body: JSON.stringify({foo: 'bar', _attachments: {'message.txt': {follows: true, length: 18, 'content_type': 'text/plain' }}})
-        }
-      , { body: 'I am an attachment' }
-      ]
-    }
-  , function (error, response, body) {
-      if(response.statusCode == 201){
-        console.log('document saved as: http://mikeal.iriscouch.com/testjs/'+ rand)
-      } else {
-        console.log('error: '+ response.statusCode)
-        console.log(body)
-      }
-    }
-  );
-  
-=======
 const rand = Math.floor(Math.random() * 100000000).toString();
->>>>>>> master
 request(
   {
     method: 'PUT',
