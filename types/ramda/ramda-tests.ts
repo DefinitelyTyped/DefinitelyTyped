33d52686cca65e60bd17b8843fa5f7553605d5c8
Alejandro Haro <Alejandro.Haro@adstream.com>
2017-06-16 09:46:49 +0100

    jslint compliance

--- types/ramda/ramda-tests.ts
+++ types/ramda/ramda-tests.ts
@@ -1639,11 +1639,11 @@ class Rectangle {
 
 () => {
     let people = [
-        {name: 'Agy', age:33}, {name: 'Bib', age: 15}, {name: 'Cari', age: 16}
+        {name: 'Agy', age: 33}, {name: 'Bib', age: 15}, {name: 'Cari', age: 16}
     ];
 
     R.sortWith([R.ascend(R.prop('age')), R.descend(R.prop('name'))], people);
-}
+};
 
 () => {
     function add(a: number, b: number) {
