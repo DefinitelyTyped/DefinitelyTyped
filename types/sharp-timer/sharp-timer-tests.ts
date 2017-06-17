0a28af9b9159306f0664b3a3034497a2126ac38a
afractal <hermesxgjini@gmail.com>
2017-06-17 11:55:42 +0200

    add documentation

--- types/sharp-timer/sharp-timer-tests.ts
+++ types/sharp-timer/sharp-timer-tests.ts
@@ -1,7 +1,11 @@
-import { Timer, Stopwatch } from 'sharp-timer';
+import { Timer, Stopwatch, Timespan } from 'sharp-timer';
+import * as s from 'sharp-timer';
+import S = require('sharp-timer');
+
+type Elapsed = SharpTimer.ElapsedEvent;
 
 let timer = new Timer(10);
-
+timer.toString();
 timer.onIntervalElapsing(i => { });
 timer.onIntervalElapsed(() => {
     timer.stop();
