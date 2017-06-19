9e58e7e879402ea4972ec666a9c8e5f145ade035
Leonard Thieu <leonard.thieu@gmail.com>
2017-06-19 14:46:49 -0400

    [angular] Remove dependency on @types/jquery.

--- types/angular/index.d.ts
+++ types/angular/index.d.ts
@@ -2,9 +2,8 @@
 // Project: http://angularjs.org
 // Definitions by: Diego Vilar <http://github.com/diegovilar>, Georgii Dolzhykov <http://github.com/thorn0>, Caleb St-Denis <https://github.com/calebstdenis>
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
-// TypeScript Version: 2.3
 
-/// <reference types="jquery" />
+/// <reference path="jqlite.d.ts" />
 
 declare var angular: angular.IAngularStatic;
 
@@ -2107,24 +2106,5 @@ declare namespace angular {
      */
     interface IHttpParamSerializer {
         (obj: Object): string;
-    }
-}
-
-declare global {
-    interface JQuery {
-        // TODO: events, how to define?
-        // $destroy
-
-        find(element: any): JQuery;
-        find(obj: JQuery): JQuery;
-        controller(name?: string): any;
-        injector(): ng.auto.IInjectorService;
-        /** It's declared generic for custom scope interfaces */
-        scope<T extends ng.IScope>(): T;
-        isolateScope<T extends ng.IScope>(): T;
-
-        inheritedData(key: string, value: any): JQuery;
-        inheritedData(obj: { [key: string]: any; }): JQuery;
-        inheritedData(key?: string): any;
     }
 }
