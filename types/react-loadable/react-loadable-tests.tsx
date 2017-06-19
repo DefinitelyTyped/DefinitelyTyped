294e98d261920f5cf33c2f737c25aa6ede6a87b8
Oden <ops991923@gmail.com>
2017-06-19 00:24:18 -0700

    Use React.ComponentType

--- types/react-loadable/react-loadable-tests.tsx
+++ types/react-loadable/react-loadable-tests.tsx
@@ -70,7 +70,7 @@ const LoadableMap = Loadable.Map({
     Component: () => Promise.resolve({ default: Component }),
     text: () => Promise.resolve("test text")
   },
-  render(loaded, props) {
+  render(loaded, props: ComponentProps) {
     let Component = loaded.Component.default;
     return <Component {...props} text={loaded.text} />;
   }
