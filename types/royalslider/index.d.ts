3deb1215ca99c7915a03f830559ce3f3805779cc
Leonard Thieu <leonard.thieu@gmail.com>
2017-06-19 20:45:48 -0400

    [royalslider] Update to use @types/jquery v3 types.

--- types/royalslider/index.d.ts
+++ types/royalslider/index.d.ts
@@ -1,7 +1,9 @@
-// Type definitions for jQuery royal-slider v9.4.0
+// Type definitions for jQuery royal-slider 9.4
 // Project: http://dimsemenov.com/plugins/royal-slider/documentation/
 // Definitions by: Christiaan Rakowski <https://github.com/csrakowski/>
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
+
+/// <reference types="jquery" />
 
 declare namespace RoyalSlider {
     export interface RoyalSliderThumbsOptions {
