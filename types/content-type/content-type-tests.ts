6410fd28780edad7d61ed714e7d6529c530f045e
Michiel de Bruijne <m.debruijne@matrict.nl>
2017-06-20 22:48:23 +0200

    [content-type] fix redeclaration and add linting

--- types/content-type/content-type-tests.ts
+++ types/content-type/content-type-tests.ts
@@ -1,19 +1,18 @@
 import contentType = require('content-type');
 import express = require('express');
 
-var obj = contentType.parse('image/svg+xml; charset=utf-8');
+let obj = contentType.parse('image/svg+xml; charset=utf-8');
 
 console.log(obj.type);  // => 'image/svg+xml'
 console.log(obj.parameters.charset);  // => 'utf-8'
 
-
-var req: express.Request;
+let req: express.Request;
 obj = contentType.parse(req);
 
-var res: express.Response;
+let res: express.Response;
 obj = contentType.parse(res);
 
-var str: string = contentType.format({type: 'image/svg+xml'});
+let str: string = contentType.format({type: 'image/svg+xml'});
 
-var media: contentType.MediaType;
+let media: contentType.MediaType;
 contentType.format(media);
