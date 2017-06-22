db41c117dbe261893c1aa895eae5861a9b7e816b
Sergei Dorogin <sergei@dorogin.com>
2017-06-22 18:16:40 +0300

    [jquery.notify] added tslint, fixed tsconfig (noImplicitAny, noImplicitThis, strictNullChecks are all true), fixed header and whitespaces

--- types/jquery.notify/index.d.ts
+++ types/jquery.notify/index.d.ts
@@ -1,5 +1,5 @@
-// Type definitions for jQuery Notify UI Widget 1.5 by Eric Hynds
-// Project: https://github.com/ehynds/jquery-notify
+// Type definitions for jquery.notify 1.5
+// Project: https://github.com/ehynds/jquery-notify (jQuery Notify UI Widget by Eric Hynds)
 // Definitions by: Sergei Dorogin <https://github.com/evil-shrike>
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
 // TypeScript Version: 2.3
@@ -17,16 +17,16 @@ interface JQueryNotifyOptions {
 	stack?: "below" | "above";
 }
 interface JQuery {
-	notify (options?: JQueryNotifyOptions): JQueryNotifyWidget;
-	notify (method: string, template: number, params?: Object, opts?: JQueryNotifyOptions): JQueryNotifyInstance;
-	notify (method: string, params?: Object, opts?: JQueryNotifyOptions): JQueryNotifyInstance;
+	notify(options?: JQueryNotifyOptions): JQueryNotifyWidget;
+	notify(method: string, template: number, params?: object, opts?: JQueryNotifyOptions): JQueryNotifyInstance;
+	notify(method: string, params?: object, opts?: JQueryNotifyOptions): JQueryNotifyInstance;
 }
 interface JQueryNotifyInstance extends JQuery {
 	element: JQuery;
 	isOpen: boolean;
 	options: JQueryNotifyOptions;
-	close (): void;
-	open (): void;
+	close(): void;
+	open(): void;
 }
 interface JQueryNotifyWidget extends JQuery  {
 }
