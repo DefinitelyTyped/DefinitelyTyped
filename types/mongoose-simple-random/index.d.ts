e585cd9ffe2c66145eda2e044d55432f79c920df
Roberts Slisans <rsxdalv@gmail.com>
2017-06-22 19:53:01 +0300

    Created type definition & wrote test case

---
+++ types/mongoose-simple-random/index.d.ts
@@ -0,0 +1,12 @@
+// Type definitions for mongoose-simple-random 0.4
+// Project: https://github.com/larryprice/mongoose-simple-random
+// Definitions by: My Self <https://github.com/me>
+// Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
+/// <reference types="mongoose" />
+
+declare module "mongoose" {
+    interface Model<T extends Document> extends NodeJS.EventEmitter, ModelProperties {
+        findRandom(conditions: Object, projection?: Object | null, options?: Object | null, callback?: (err: any, res: T[]) => void)
+            : void;
+    }
+}
