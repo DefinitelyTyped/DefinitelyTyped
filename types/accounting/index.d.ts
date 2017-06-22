9d1d71714895ce5bb5ec6e06eb129111e4d17e17
Christopher Eck <chris@krypton.io>
2017-06-21 17:37:33 -0700

    Updated project link

--- types/accounting/index.d.ts
+++ types/accounting/index.d.ts
@@ -1,5 +1,5 @@
 // Type definitions for accounting.js 0.4
-// Project: http://josscrowcroft.github.io/accounting.js/
+// Project: http://openexchangerates.github.io/accounting.js/
 // Definitions by: Sergey Gerasimov <https://github.com/gerich-home/>
 //                 Christopher Eck <https://github.com/chrisleck/>
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
