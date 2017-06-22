8df4a3060fbbdcddd4c1407882fc297f2c6f3bdd
Leonard Thieu <leonard.thieu@gmail.com>
2017-06-21 20:43:31 -0400

    [royalslider] Add TypeScript Version: 2.3 to header.

--- types/royalslider/index.d.ts
+++ types/royalslider/index.d.ts
@@ -2,6 +2,7 @@
 // Project: http://dimsemenov.com/plugins/royal-slider/documentation/
 // Definitions by: Christiaan Rakowski <https://github.com/csrakowski/>
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
+// TypeScript Version: 2.3
 
 /// <reference types="jquery" />
 
