5d1a267479c08aaa2d2fe22902aa79c7bb357c2a
york yao <yaoao12306@outlook.com>
2017-06-23 19:49:35 +0800

    add types for uppercamelcase

---
+++ types/uppercamelcase/index.d.ts
@@ -0,0 +1,7 @@
+// Type definitions for uppercamelcase
+// Project: https://github.com/samverschueren/uppercamelcase
+// Definitions by: York Yao <https://github.com/plantain-00>
+// Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
+
+declare function upperCamelCase(...args: string[]): string;
+export = upperCamelCase;
