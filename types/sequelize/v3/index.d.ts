74a003f70687b92415723227b63f8d4ef5a785ad
James D. Callahan III <jcallahan@curse.com>
2017-06-23 08:39:54 -0500

    Add missing null to the type union for WhereOptions index signature, and change Object to object.

--- types/sequelize/v3/index.d.ts
+++ types/sequelize/v3/index.d.ts
@@ -3056,7 +3056,8 @@ declare namespace sequelize {
      * typesafety, but there is no way to pass the tests if we just remove it.
      */
     interface WhereOptions {
-        [field: string]: string | number | WhereLogic | WhereOptions | col | and | or | WhereGeometryOptions | Array<string | number> | Object;
+        [field: string]: string | number | WhereLogic | WhereOptions | col | and | or | WhereGeometryOptions | Array<string | number> | object | null;
+
     }
 
     /**
