0a906433045b63da8089ba170b91f7a768e5bda9
Mahdi Abedi <abedi@jeyserver.com>
2017-06-23 18:32:17 +0430

    add contributor name

--- types/jquery.fancytree/index.d.ts
+++ types/jquery.fancytree/index.d.ts
@@ -1,6 +1,7 @@
 // Type definitions for jquery.fancytree 2.7.0
 // Project: https://github.com/mar10/fancytree
 // Definitions by: Peter Palotas <https://github.com/alphaleonis>
+//                 Mahdi Abedi <https://github.com/abedi-ir>
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
 // TypeScript Version: 2.3
 
