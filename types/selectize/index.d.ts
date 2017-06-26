8aeab9e8e619c55671ba00c19a4cd7313bd67299
Koloto <alkoloto@hotmail.com>
2017-06-26 17:02:18 +0300

    [selectize] Extended plugins option, added options and items API properties: updated version

--- types/selectize/index.d.ts
+++ types/selectize/index.d.ts
@@ -1,4 +1,4 @@
-// Type definitions for Selectize 0.12.13
+// Type definitions for Selectize 0.12.14
 // Project: https://github.com/brianreavis/selectize.js
 // Definitions by: Adi Dahiya <https://github.com/adidahiya>, Natalie Bausch <https://github.com/naBausch>
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
