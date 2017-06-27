7d8f74b5d5cdb4a11dbeda4a876329fc3817d28f
Paul Datsiuk <jthotbox@gmail.com>
2017-06-27 10:49:28 +0300

    add typescript definition for browser-report

---
+++ types/browser-report/browser-report-tests.ts
@@ -0,0 +1,13 @@
+import 'browser-report';
+
+function test_sync() {
+	var browserInfo = window.browserReportSync();
+	console.log(browserInfo.browser.name);	
+}
+
+function test_async() {
+	window.browserReport((error, result) => {
+		console.log(result.ip + ' ' + result.browser.name);
+		debugger;
+	});	
+}
