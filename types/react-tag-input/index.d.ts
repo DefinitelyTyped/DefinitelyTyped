6a3db40a8b88751a383046b1198ea72f06ea4dc7
Oscar Andersson <oscar.andersson89@gmail.com>
2017-06-27 11:24:15 +0200

    Added react-tag-input

---
+++ types/react-tag-input/index.d.ts
@@ -0,0 +1,26 @@
+// Type definitions for React-Tags (react-tag-input) 4.7.2
+// Project: https://github.com/prakhar1989/react-tags
+// NPM: https://www.npmjs.com/package/react-tag-input
+// Definitions by: Ogglas <https://stackoverflow.com/users/3850405/ogglas>
+// Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
+// TypeScript Version: 2.3
+
+import * as React from "react";
+
+export interface ReactTagsProps {
+    tags?: TagItem[];
+    suggestions?: string[];
+    handleDelete: ((i: number) => void);
+    handleAddition: ((tag: string) => void);
+    handleDrag?: ((tag: TagItem, currPos: number, newPos: number) => void);
+    placeholder?: string;
+}
+
+export interface TagItem {
+    id: number;
+    text: string;
+}
+
+export class WithContext extends React.Component<ReactTagsProps, {}> { }
+
+export default WithContext;
