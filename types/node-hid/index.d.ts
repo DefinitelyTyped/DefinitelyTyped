6f114653600265e936e1c88e8ebcc8776c53fc1d
Róbert Kiss <ert78gb@gmail.com>
2017-06-27 22:55:55 +0200

    node-hid: set optional properties

--- types/node-hid/index.d.ts
+++ types/node-hid/index.d.ts
@@ -7,14 +7,14 @@
 export interface Device {
     vendorId: number;
     productId: number;
-    path: string;
-    serialNumber: string;
-    manufacturer: string;
-    product: string;
+    path?: string;
+    serialNumber?: string;
+    manufacturer?: string;
+    product?: string;
     release: number;
     interface: number;
-    usagePage: number;
-    usage: number;
+    usagePage?: number;
+    usage?: number;
 }
 
 export class HID {
