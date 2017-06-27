bf57cf89e24d4d4fb695f50cb5d53356d1fb7f86
Luka Zakrajsek <luka@bancek.net>
2017-06-27 14:43:22 +0200

    Update definitions by

--- types/redux-form/index.d.ts
+++ types/redux-form/index.d.ts
@@ -1,6 +1,6 @@
 // Type definitions for redux-form 6.6
 // Project: https://github.com/erikras/redux-form
-// Definitions by: Carson Full <https://github.com/carsonf>, Daniel Lytkin <https://github.com/aikoven>, Karol Janyst <https://github.com/LKay>
+// Definitions by: Carson Full <https://github.com/carsonf>, Daniel Lytkin <https://github.com/aikoven>, Karol Janyst <https://github.com/LKay>, Luka Zakrajsek <https://github.com/bancek>
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
 // TypeScript Version: 2.3
 
