970140449f22ee0be56a225488bafc6b4bf61e98
Paul van Brenk <paul.van.brenk@outlook.com>
2017-06-27 15:26:30 -0700

    Update index.d.ts

--- types/async.nexttick/index.d.ts
+++ types/async.nexttick/index.d.ts
@@ -1,4 +1,4 @@
-// Type definitions for async.nexttick 0.5.2
+// Type definitions for async.nexttick 0.5
 // Project: https://www.npmjs.com/package/async.nexttick
 // Definitions by: Damien "pyrho" Rajon <https://github.com/pyrho>
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
