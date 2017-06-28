4af2e2ac5f13d20f4c5929254eb022f5a0b452cf
Oscar Andersson <oscar.andersson89@gmail.com>
2017-06-28 09:28:40 +0200

    Update index.d.ts

--- types/react-tag-input/index.d.ts
+++ types/react-tag-input/index.d.ts
@@ -1,6 +1,5 @@
 // Type definitions for React-Tags (react-tag-input) 4.7.2
 // Project: https://github.com/prakhar1989/react-tags
-// NPM: https://www.npmjs.com/package/react-tag-input
 // Definitions by: Ogglas <https://stackoverflow.com/users/3850405/ogglas>
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
 // TypeScript Version: 2.3
