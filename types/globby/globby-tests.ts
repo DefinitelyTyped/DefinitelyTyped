7ffb68610a08314ff7890f99a6e8dd3194a7a3bd
Douglas Duteil <douglasduteil@gmail.com>
2017-06-28 11:28:46 +0200

    fix(globby): lint error

--- types/globby/globby-tests.ts
+++ types/globby/globby-tests.ts
@@ -1,21 +1,18 @@
-//
-
 import { IOptions } from 'glob';
 
 import * as globby from "globby";
 
 (async () => {
     let result: string[];
-    result = await globby('*.tmp')
-    result = await globby(['a.tmp', '*.tmp', '!{c,d,e}.tmp'])
+    result = await globby('*.tmp');
+    result = await globby(['a.tmp', '*.tmp', '!{c,d,e}.tmp']);
 
-    result = globby.sync('*.tmp')
-    result = globby.sync(['a.tmp', '*.tmp', '!{c,d,e}.tmp'])
+    result = globby.sync('*.tmp');
+    result = globby.sync(['a.tmp', '*.tmp', '!{c,d,e}.tmp']);
 
-    result = await globby('*.tmp', Object.freeze({ignore: Object.freeze([])}))
-    result = globby.sync('*.tmp', Object.freeze({ignore: Object.freeze([])}))
-})()
-
+    result = await globby('*.tmp', Object.freeze({ignore: Object.freeze([])}));
+    result = globby.sync('*.tmp', Object.freeze({ignore: Object.freeze([])}));
+})();
 
 const tasks: Array<{
     pattern: string,
