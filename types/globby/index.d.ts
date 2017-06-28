23bb7eed66e50b535c4ce78e1247c59bbb0781fe
Douglas Duteil <douglasduteil@gmail.com>
2017-06-28 11:35:01 +0200

    fix(globby): remove unnecessary "Partial"

--- types/globby/index.d.ts
+++ types/globby/index.d.ts
@@ -2,15 +2,17 @@
 // Project: https://github.com/sindresorhus/globby#readme
 // Definitions by: Douglas Duteil <https://github.com/douglasduteil>
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
+// TypeScript Version: 2.3
 
 import { IOptions } from 'glob';
 
-declare function globby(patterns: string | string[], options?: Partial<IOptions>): Promise<string[]>;
+declare function globby(patterns: string | string[], options?: IOptions): Promise<string[]>;
 
 declare namespace globby {
-  function sync(patterns: string | string[], options?: Partial<IOptions>): string[];
-  function generateGlobTasks(patterns: string | string[], options?: Partial<IOptions>): Array<{pattern: string, options: IOptions}>;
-  function hasMagic(patterns: string | string[], options?: Partial<IOptions>): boolean;
+  function sync(patterns: string | string[], options?: IOptions): string[];
+  function generateGlobTasks(patterns: string | string[], options?: IOptions): Array<{pattern: string, options: IOptions}>;
+  function hasMagic(patterns: string | string[], options?: IOptions): boolean;
 }
 
 export = globby;
+
