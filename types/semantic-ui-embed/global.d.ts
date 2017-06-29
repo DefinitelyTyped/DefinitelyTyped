3c6279f8e9367e27957d87963c3478b6aaa98149
Leonard Thieu <leonard.thieu@gmail.com>
2017-06-29 10:49:50 -0400

    [semantic-ui-embed] Revert weak-type workaround.

--- types/semantic-ui-embed/global.d.ts
+++ types/semantic-ui-embed/global.d.ts
@@ -52,279 +52,237 @@ declare namespace SemanticUI {
         (behavior: 'destroy'): JQuery;
         <K extends keyof EmbedSettings>(behavior: 'setting', name: K, value?: undefined): EmbedSettings[K];
         <K extends keyof EmbedSettings>(behavior: 'setting', name: K, value: EmbedSettings[K]): JQuery;
-        (behavior: 'setting', value: EmbedSettings.Param): JQuery;
-        (settings?: EmbedSettings.Param): JQuery;
+        (behavior: 'setting', value: EmbedSettings): JQuery;
+        (settings?: EmbedSettings): JQuery;
     }
 
     /**
      * @see {@link http://semantic-ui.com/modules/embed.html#/settings}
      */
-    interface EmbedSettings extends Pick<EmbedSettings._Impl, keyof EmbedSettings._Impl> { }
+    interface EmbedSettings {
+        /**
+         * Specifies a url to use for embed
+         *
+         * @default false
+         */
+        url?: false | string;
 
-    namespace EmbedSettings {
-        type Param = EmbedSettings | object;
+        // region Embed Settings
 
-        interface _Impl {
-            /**
-             * Specifies a url to use for embed
-             *
-             * @default false
-             */
-            url: false | string;
+        /**
+         * Specifies an icon to use with placeholder content
+         *
+         * @default false
+         */
+        icon?: false | string;
+        /**
+         * Specifies a source to use, if no source is provided it will be determined from the domain of a specified url.
+         *
+         * @default false
+         */
+        source?: false | string;
+        /**
+         * Specifies an id value to replace with the {id} value found in templated urls
+         *
+         * @default false
+         */
+        id?: false | string;
+        /**
+         * Specify an object containing key/value pairs to add to the iframes GET parameters
+         *
+         * @default false
+         */
+        parameters?: false | Embed.ParametersSettings;
 
-            // region Embed Settings
+        // endregion
 
-            /**
-             * Specifies an icon to use with placeholder content
-             *
-             * @default false
-             */
-            icon: false | string;
-            /**
-             * Specifies a source to use, if no source is provided it will be determined from the domain of a specified url.
-             *
-             * @default false
-             */
-            source: false | string;
-            /**
-             * Specifies an id value to replace with the {id} value found in templated urls
-             *
-             * @default false
-             */
-            id: false | string;
-            /**
-             * Specify an object containing key/value pairs to add to the iframes GET parameters
-             *
-             * @default false
-             */
-            parameters: false | Embed.ParametersSettings;
+        // region Video Settings
 
-            // endregion
+        /**
+         * Default setting auto will only autoplay content when a placeholder is specified. Setting to true or false will force autoplay.
+         *
+         * @default 'auto'
+         */
+        autoplay?: 'auto' | boolean;
+        /**
+         * Specifies a default chrome color with Vimeo or YouTube.
+         *
+         * @default '#444444'
+         */
+        color?: string;
+        /**
+         * Whether to prefer HD content
+         *
+         * @default true
+         */
+        hd?: boolean;
+        /**
+         * Whether to show networks branded UI like title cards, or after video calls to action.
+         *
+         * @default false
+         */
+        brandedUI?: boolean;
 
-            // region Video Settings
+        // endregion
 
-            /**
-             * Default setting auto will only autoplay content when a placeholder is specified. Setting to true or false will force autoplay.
-             *
-             * @default 'auto'
-             */
-            autoplay: 'auto' | boolean;
-            /**
-             * Specifies a default chrome color with Vimeo or YouTube.
-             *
-             * @default '#444444'
-             */
-            color: string;
-            /**
-             * Whether to prefer HD content
-             *
-             * @default true
-             */
-            hd: boolean;
-            /**
-             * Whether to show networks branded UI like title cards, or after video calls to action.
-             *
-             * @default false
-             */
-            brandedUI: boolean;
+        // region Callbacks
 
-            // endregion
+        /**
+         * Callback when iframe is generated
+         */
+        onCreate?(this: JQuery, url: string): void;
+        /**
+         * Whenever an iframe contents is shown
+         */
+        onDisplay?(this: JQuery): void;
+        /**
+         * Callback immediately before Embed is removed from DOM
+         */
+        onPlaceholderDisplay?(this: JQuery): void;
+        /**
+         * Callback when module parameters are determined. Allows you to adjust parameters at run time by returning a new parameters object.
+         */
+        onEmbed?(this: JQuery, parameters: Embed.ParametersSettings): Embed.ParametersSettings;
 
-            // region Callbacks
+        // endregion
 
-            /**
-             * Callback when iframe is generated
-             */
-            onCreate(this: JQuery, url: string): void;
-            /**
-             * Whenever an iframe contents is shown
-             */
-            onDisplay(this: JQuery): void;
-            /**
-             * Callback immediately before Embed is removed from DOM
-             */
-            onPlaceholderDisplay(this: JQuery): void;
-            /**
-             * Callback when module parameters are determined. Allows you to adjust parameters at run time by returning a new parameters object.
-             */
-            onEmbed(this: JQuery, parameters: Embed.ParametersSettings): Embed.ParametersSettings;
+        // region DOM Settings
 
-            // endregion
+        /**
+         * DOM Selectors used internally
+         */
+        selector?: Embed.SelectorSettings;
+        /**
+         * HTML Data attributes used to store data
+         */
+        metadata?: Embed.MetadataSettings;
+        /**
+         * Class names used to attach style to state
+         */
+        className?: Embed.ClassNameSettings;
+        templates?: Embed.TemplatesSettings;
 
-            // region DOM Settings
+        // endregion
 
-            /**
-             * DOM Selectors used internally
-             */
-            selector: Embed.SelectorSettings;
-            /**
-             * HTML Data attributes used to store data
-             */
-            metadata: Embed.MetadataSettings;
-            /**
-             * Class names used to attach style to state
-             */
-            className: Embed.ClassNameSettings;
-            templates: Embed.TemplatesSettings;
+        // region Debug Settings
 
-            // endregion
+        error?: Embed.ErrorSettings;
 
-            // region Debug Settings
+        // endregion
 
-            error: Embed.ErrorSettings;
+        // region Component Settings
 
-            // endregion
+        // region DOM Settings
 
-            // region Component Settings
+        /**
+         * Event namespace. Makes sure module teardown does not effect other events attached to an element.
+         */
+        namespace?: string;
 
-            // region DOM Settings
+        // endregion
 
-            /**
-             * Event namespace. Makes sure module teardown does not effect other events attached to an element.
-             */
-            namespace: string;
+        // region Debug Settings
 
-            // endregion
+        /**
+         * Name used in log statements
+         */
+        name?: string;
+        /**
+         * Silences all console output including error messages, regardless of other debug settings.
+         */
+        silent?: boolean;
+        /**
+         * Debug output to console
+         */
+        debug?: boolean;
+        /**
+         * Show console.table output with performance metrics
+         */
+        performance?: boolean;
+        /**
+         * Debug output includes all internal behaviors
+         */
+        verbose?: boolean;
 
-            // region Debug Settings
+        // endregion
 
-            /**
-             * Name used in log statements
-             */
-            name: string;
-            /**
-             * Silences all console output including error messages, regardless of other debug settings.
-             */
-            silent: boolean;
-            /**
-             * Debug output to console
-             */
-            debug: boolean;
-            /**
-             * Show console.table output with performance metrics
-             */
-            performance: boolean;
-            /**
-             * Debug output includes all internal behaviors
-             */
-            verbose: boolean;
-
-            // endregion
-
-            // endregion
-        }
+        // endregion
     }
 
     namespace Embed {
-        interface ParametersSettings extends Pick<ParametersSettings._Impl, keyof ParametersSettings._Impl> { }
-
-        namespace ParametersSettings {
-            type Param = ParametersSettings | object;
-
-            interface _Impl {
-                [key: string]: any;
-            }
+        interface ParametersSettings {
+            [key: string]: any;
         }
 
-        interface SelectorSettings extends Pick<SelectorSettings._Impl, keyof SelectorSettings._Impl> { }
-
-        namespace SelectorSettings {
-            type Param = SelectorSettings | object;
-
-            interface _Impl {
-                /**
-                 * @default '.embed'
-                 */
-                embed: string;
-                /**
-                 * @default '.placeholder'
-                 */
-                placeholder: string;
-                /**
-                 * @default '.play'
-                 */
-                play: string;
-            }
+        interface SelectorSettings {
+            /**
+             * @default '.embed'
+             */
+            embed?: string;
+            /**
+             * @default '.placeholder'
+             */
+            placeholder?: string;
+            /**
+             * @default '.play'
+             */
+            play?: string;
         }
 
-        interface MetadataSettings extends Pick<MetadataSettings._Impl, keyof MetadataSettings._Impl> { }
-
-        namespace MetadataSettings {
-            type Param = MetadataSettings | object;
-
-            interface _Impl {
-                /**
-                 * @default 'id'
-                 */
-                id: string;
-                /**
-                 * @default 'icon'
-                 */
-                icon: string;
-                /**
-                 * @default 'placeholder'
-                 */
-                placeholder: string;
-                /**
-                 * @default 'source'
-                 */
-                source: string;
-                /**
-                 * @default 'url'
-                 */
-                url: string;
-            }
+        interface MetadataSettings {
+            /**
+             * @default 'id'
+             */
+            id?: string;
+            /**
+             * @default 'icon'
+             */
+            icon?: string;
+            /**
+             * @default 'placeholder'
+             */
+            placeholder?: string;
+            /**
+             * @default 'source'
+             */
+            source?: string;
+            /**
+             * @default 'url'
+             */
+            url?: string;
         }
 
-        interface ClassNameSettings extends Pick<ClassNameSettings._Impl, keyof ClassNameSettings._Impl> { }
-
-        namespace ClassNameSettings {
-            type Param = ClassNameSettings | object;
-
-            interface _Impl {
-                /**
-                 * @default 'active'
-                 */
-                active: string;
-                /**
-                 * @default 'embed'
-                 */
-                embed: string;
-            }
+        interface ClassNameSettings {
+            /**
+             * @default 'active'
+             */
+            active?: string;
+            /**
+             * @default 'embed'
+             */
+            embed?: string;
         }
 
-        interface TemplatesSettings extends Pick<TemplatesSettings._Impl, keyof TemplatesSettings._Impl> { }
-
-        namespace TemplatesSettings {
-            type Param = TemplatesSettings | object;
-
-            interface _Impl {
-                /**
-                 * returns html for iframe
-                 */
-                iframe(url: string, parameters: string): string;
-                /**
-                 * returns html for placeholder element
-                 */
-                placeholder(image: string, icon: string): string;
-            }
+        interface TemplatesSettings {
+            /**
+             * returns html for iframe
+             */
+            iframe?(url: string, parameters: string): string;
+            /**
+             * returns html for placeholder element
+             */
+            placeholder?(image: string, icon: string): string;
         }
 
-        interface ErrorSettings extends Pick<ErrorSettings._Impl, keyof ErrorSettings._Impl> { }
-
-        namespace ErrorSettings {
-            type Param = ErrorSettings | object;
-
-            interface _Impl {
-                /**
-                 * @default 'No URL specified'
-                 */
-                noURL: string;
-                /**
-                 * @default 'The method you called is not defined'
-                 */
-                method: string;
-            }
+        interface ErrorSettings {
+            /**
+             * @default 'No URL specified'
+             */
+            noURL?: string;
+            /**
+             * @default 'The method you called is not defined'
+             */
+            method?: string;
         }
     }
 }
