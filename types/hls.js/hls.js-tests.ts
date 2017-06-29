f5c35ffd35958d2715fd9446478a4071109a87b9
John Gainfort Jr <john.gainfort@realeyes.com>
2017-06-29 14:10:33 -0600

    fixed extending defaultconfig property and adding optional properties in hls instantiation

--- types/hls.js/hls.js-tests.ts
+++ types/hls.js/hls.js-tests.ts
@@ -1,8 +1,31 @@
 import * as Hls from 'hls.js';
 
+function process(playlist: string) {
+  return playlist;
+}
+
+class pLoader extends Hls.DefaultConfig.loader {
+  constructor(config: Hls.LoaderConfig) {
+    super(config);
+    const load = this.load.bind(this);
+    this.load = (context: Hls.LoaderContext, cfg: Hls.LoaderConfig, callbacks: Hls.LoaderCallbacks) => {
+      if (context.type === 'manifest') {
+        const onSuccess = callbacks.onSuccess;
+        callbacks.onSuccess = (response: Hls.LoaderResponse, stats: Hls.LoaderStats, context: Hls.LoaderContext) => {
+          response.data = process(response.data as string);
+          onSuccess(response, stats, context);
+        }
+      }
+      load(context, config, callbacks);
+    }
+  }
+}
+
 if (Hls.isSupported()) {
-  const video = <HTMLVideoElement> document.getElementById('video');
-  const hls = new Hls();
+  const video = <HTMLVideoElement>document.getElementById('video');
+  const hls = new Hls({
+    pLoader: pLoader
+  });
   const version: string = Hls.version;
   hls.loadSource('http://www.streambox.fr/playlists/test_001/stream.m3u8');
   hls.attachMedia(video);
@@ -10,3 +33,4 @@ if (Hls.isSupported()) {
     video.play();
   });
 }
+
