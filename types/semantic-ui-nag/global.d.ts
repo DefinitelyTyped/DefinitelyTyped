cfeaa246d9e89b576a023d69b718404627322311
Leonard Thieu <leonard.thieu@gmail.com>
2017-06-29 11:00:45 -0400

    [semantic-ui-nag] Revert weak-type workaround.

--- types/semantic-ui-nag/global.d.ts
+++ types/semantic-ui-nag/global.d.ts
@@ -15,209 +15,179 @@ declare namespace SemanticUI {
         (behavior: 'destroy'): JQuery;
         <K extends keyof NagSettings>(behavior: 'setting', name: K, value?: undefined): NagSettings[K];
         <K extends keyof NagSettings>(behavior: 'setting', name: K, value: NagSettings[K]): JQuery;
-        (behavior: 'setting', value: NagSettings.Param): JQuery;
-        (settings?: NagSettings.Param): JQuery;
+        (behavior: 'setting', value: NagSettings): JQuery;
+        (settings?: NagSettings): JQuery;
     }
 
     /**
      * @see {@link http://semantic-ui.com/modules/nag.html}
      */
-    interface NagSettings extends Pick<NagSettings._Impl, keyof NagSettings._Impl> { }
+    interface NagSettings {
+        // region Behavior
 
-    namespace NagSettings {
-        type Param = NagSettings | object;
+        /**
+         * allows cookie to be overridden
+         *
+         * @default false
+         */
+        persist?: boolean;
+        /**
+         * set to zero to require manually dismissal, otherwise hides on its own
+         *
+         * @default 0
+         */
+        displayTime?: number;
+        animation?: Nag.AnimationSettings;
+        /**
+         * @default false
+         */
+        context?: false | string | JQuery;
+        /**
+         * @default false
+         */
+        detachable?: boolean;
 
-        interface _Impl {
-            // region Behavior
+        /**
+         * @default 30
+         */
+        expires?: number;
+        /**
+         * @default false
+         */
+        domain?: false | string;
+        /**
+         * @default '/'
+         */
+        path?: string;
 
-            /**
-             * allows cookie to be overridden
-             *
-             * @default false
-             */
-            persist: boolean;
-            /**
-             * set to zero to require manually dismissal, otherwise hides on its own
-             *
-             * @default 0
-             */
-            displayTime: number;
-            animation: Nag.AnimationSettings;
-            /**
-             * @default false
-             */
-            context: false | string | JQuery;
-            /**
-             * @default false
-             */
-            detachable: boolean;
+        /**
+         * type of storage to use
+         *
+         * @default 'cookie'
+         */
+        storageMethod?: 'cookie' | 'localstorage' | 'sessionstorage';
 
-            /**
-             * @default 30
-             */
-            expires: number;
-            /**
-             * @default false
-             */
-            domain: false | string;
-            /**
-             * @default '/'
-             */
-            path: string;
+        /**
+         * @default 'nag'
+         */
+        key?: any;
+        /**
+         * @default 'dismiss'
+         */
+        value?: any;
 
-            /**
-             * type of storage to use
-             *
-             * @default 'cookie'
-             */
-            storageMethod: 'cookie' | 'localstorage' | 'sessionstorage';
+        /**
+         * @default 500
+         */
+        speed?: number;
+        /**
+         * @default 'easeOutQuad'
+         */
+        easing?: string;
 
-            /**
-             * @default 'nag'
-             */
-            key: any;
-            /**
-             * @default 'dismiss'
-             */
-            value: any;
+        // endregion
 
-            /**
-             * @default 500
-             */
-            speed: number;
-            /**
-             * @default 'easeOutQuad'
-             */
-            easing: string;
+        // region Callbacks
 
-            // endregion
+        onHide?(this: JQuery): void;
 
-            // region Callbacks
+        // endregion
 
-            onHide(this: JQuery): void;
+        // region DOM Settings
 
-            // endregion
+        className?: Nag.ClassNameSettings;
+        selector?: Nag.SelectorSettings;
 
-            // region DOM Settings
+        // endregion
 
-            className: Nag.ClassNameSettings;
-            selector: Nag.SelectorSettings;
+        // region Debug Settings
 
-            // endregion
+        error?: Nag.ErrorSettings;
 
-            // region Debug Settings
+        // endregion
 
-            error: Nag.ErrorSettings;
+        // region Component Settings
 
-            // endregion
+        // region DOM Settings
 
-            // region Component Settings
+        /**
+         * Event namespace. Makes sure module teardown does not effect other events attached to an element.
+         */
+        namespace?: string;
 
-            // region DOM Settings
+        // endregion
 
-            /**
-             * Event namespace. Makes sure module teardown does not effect other events attached to an element.
-             */
-            namespace: string;
+        // region Debug Settings
 
-            // endregion
+        /**
+         * Name used in log statements
+         */
+        name?: string;
+        /**
+         * Silences all console output including error messages, regardless of other debug settings.
+         */
+        silent?: boolean;
+        /**
+         * Debug output to console
+         */
+        debug?: boolean;
+        /**
+         * Show console.table output with performance metrics
+         */
+        performance?: boolean;
+        /**
+         * Debug output includes all internal behaviors
+         */
+        verbose?: boolean;
 
-            // region Debug Settings
+        // endregion
 
-            /**
-             * Name used in log statements
-             */
-            name: string;
-            /**
-             * Silences all console output including error messages, regardless of other debug settings.
-             */
-            silent: boolean;
-            /**
-             * Debug output to console
-             */
-            debug: boolean;
-            /**
-             * Show console.table output with performance metrics
-             */
-            performance: boolean;
-            /**
-             * Debug output includes all internal behaviors
-             */
-            verbose: boolean;
-
-            // endregion
-
-            // endregion
-        }
+        // endregion
     }
 
     namespace Nag {
-        interface AnimationSettings extends Pick<AnimationSettings._Impl, keyof AnimationSettings._Impl> { }
-
-        namespace AnimationSettings {
-            type Param = AnimationSettings | object;
-
-            interface _Impl {
-                /**
-                 * @default 'slide'
-                 */
-                show: string;
-                /**
-                 * @default 'slide'
-                 */
-                hide: string;
-            }
+        interface AnimationSettings {
+            /**
+             * @default 'slide'
+             */
+            show?: string;
+            /**
+             * @default 'slide'
+             */
+            hide?: string;
         }
 
-        interface ClassNameSettings extends Pick<ClassNameSettings._Impl, keyof ClassNameSettings._Impl> { }
-
-        namespace ClassNameSettings {
-            type Param = ClassNameSettings | object;
-
-            interface _Impl {
-                /**
-                 * @default 'bottom'
-                 */
-                bottom: string;
-                /**
-                 * @default 'fixed'
-                 */
-                fixed: string;
-            }
+        interface ClassNameSettings {
+            /**
+             * @default 'bottom'
+             */
+            bottom?: string;
+            /**
+             * @default 'fixed'
+             */
+            fixed?: string;
         }
 
-        interface SelectorSettings extends Pick<SelectorSettings._Impl, keyof SelectorSettings._Impl> { }
-
-        namespace SelectorSettings {
-            type Param = SelectorSettings | object;
-
-            interface _Impl {
-                /**
-                 * @default '.close.icon'
-                 */
-                close: string;
-            }
+        interface SelectorSettings {
+            /**
+             * @default '.close.icon'
+             */
+            close?: string;
         }
 
-        interface ErrorSettings extends Pick<ErrorSettings._Impl, keyof ErrorSettings._Impl> { }
-
-        namespace ErrorSettings {
-            type Param = ErrorSettings | object;
-
-            interface _Impl {
-                /**
-                 * @default '$.cookie is not included. A storage solution is required.'
-                 */
-                noCookieStorage: string;
-                /**
-                 * @default 'Neither $.cookie or store is defined. A storage solution is required for storing state'
-                 */
-                noStorage: string;
-                /**
-                 * @default 'The method you called is not defined.'
-                 */
-                method: string;
-            }
+        interface ErrorSettings {
+            /**
+             * @default '$.cookie is not included. A storage solution is required.'
+             */
+            noCookieStorage?: string;
+            /**
+             * @default 'Neither $.cookie or store is defined. A storage solution is required for storing state'
+             */
+            noStorage?: string;
+            /**
+             * @default 'The method you called is not defined.'
+             */
+            method?: string;
         }
     }
 }
