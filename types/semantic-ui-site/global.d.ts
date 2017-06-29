094d1511efffe0917b7c584faed8a0b4fca08dbb
Leonard Thieu <leonard.thieu@gmail.com>
2017-06-29 12:52:10 -0400

    [semantic-ui-site] Revert weak-type workaround.

--- types/semantic-ui-site/global.d.ts
+++ types/semantic-ui-site/global.d.ts
@@ -13,73 +13,61 @@ declare namespace SemanticUI {
         (behavior: 'destroy'): JQuery;
         <K extends keyof SiteSettings>(behavior: 'setting', name: K, value?: undefined): SiteSettings[K];
         <K extends keyof SiteSettings>(behavior: 'setting', name: K, value: SiteSettings[K]): JQuery;
-        (behavior: 'setting', value: SiteSettings.Param): JQuery;
-        (settings?: SiteSettings.Param): JQuery;
+        (behavior: 'setting', value: SiteSettings): JQuery;
+        (settings?: SiteSettings): JQuery;
     }
 
-    interface SiteSettings extends Pick<SiteSettings._Impl, keyof SiteSettings._Impl> { }
+    interface SiteSettings {
+        modules?: string[];
+        siteNamespace?: string;
+        namespaceStub?: Site.NamespaceStubSettings;
 
-    namespace SiteSettings {
-        type Param = SiteSettings | object;
+        // region Component Settings
 
-        interface _Impl {
-            modules: string[];
-            siteNamespace: string;
-            namespaceStub: Site.NamespaceStubSettings;
+        // region DOM Settings
 
-            // region Component Settings
+        /**
+         * Event namespace. Makes sure module teardown does not effect other events attached to an element.
+         */
+        namespace?: string;
 
-            // region DOM Settings
+        // endregion
 
-            /**
-             * Event namespace. Makes sure module teardown does not effect other events attached to an element.
-             */
-            namespace: string;
+        // region Debug Settings
 
-            // endregion
+        /**
+         * Name used in log statements
+         */
+        name?: string;
+        /**
+         * Silences all console output including error messages, regardless of other debug settings.
+         */
+        silent?: boolean;
+        /**
+         * Debug output to console
+         */
+        debug?: boolean;
+        /**
+         * Show console.table output with performance metrics
+         */
+        performance?: boolean;
+        /**
+         * Debug output includes all internal behaviors
+         */
+        verbose?: boolean;
 
-            // region Debug Settings
+        // endregion
 
-            /**
-             * Name used in log statements
-             */
-            name: string;
-            /**
-             * Silences all console output including error messages, regardless of other debug settings.
-             */
-            silent: boolean;
-            /**
-             * Debug output to console
-             */
-            debug: boolean;
-            /**
-             * Show console.table output with performance metrics
-             */
-            performance: boolean;
-            /**
-             * Debug output includes all internal behaviors
-             */
-            verbose: boolean;
-
-            // endregion
-
-            // endregion
-        }
+        // endregion
     }
 
     namespace Site {
-        interface NamespaceStubSettings extends Pick<NamespaceStubSettings._Impl, keyof NamespaceStubSettings._Impl> { }
-
-        namespace NamespaceStubSettings {
-            type Param = NamespaceStubSettings | object;
-
-            interface _Impl {
-                cache: any;
-                config: any;
-                sections: any;
-                section: any;
-                utilities: any;
-            }
+        interface NamespaceStubSettings {
+            cache?: any;
+            config?: any;
+            sections?: any;
+            section?: any;
+            utilities?: any;
         }
     }
 }
