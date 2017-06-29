4a2147de1347b15a1e16cafea6ed30f6885cd7a7
Douglas Duteil <douglasduteil@gmail.com>
2017-06-29 16:40:40 +0200

    feat(dedent): add typings for `dedent`

---
+++ types/dedent/index.d.ts
@@ -0,0 +1,7 @@
+// Type definitions for dedent v0.7.0
+// Project: https://github.com/dmnd/dedent
+// Definitions by: Douglas Duteil <https://github.com/douglasduteil>
+// Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
+
+export default function dedent(literals: string): string;
+export default function dedent(literals: TemplateStringsArray, ...placeholders: any[]): string;
