c1aeed1c41a81ae9cd5cc219cd391267456e8db8
Tim Jacobi <dev@tim-jacobi.com>
2017-06-30 23:32:28 +0100

    fix lint errors

--- types/nano/nano-tests.ts
+++ types/nano/nano-tests.ts
@@ -7,7 +7,7 @@ import * as path from "path";
  */
 const config: nano.Configuration = {
     url: "http://localhost:5984/foo",
-    requestDefaults: { "proxy": "http://someproxy" }
+    requestDefaults: { proxy: "http://someproxy" }
 };
 
 const cfgInstance = nano(config);
@@ -15,73 +15,72 @@ const cfgInstance = nano(config);
 /*
  * Server Scope
  */
-const instance: nano.ServerScope = <nano.ServerScope>nano("http://localhost:5984/emails");
+const instance: nano.ServerScope = <nano.ServerScope> nano("http://localhost:5984/emails");
 
-instance.dinosaur("", function (err, response) { });
+instance.dinosaur("", (err, response) => {});
 const url = instance.config.url;
 
-instance.relax({ method: "POST", path: "_users", body: { _id: "org.couchdb.user:", type: "user", name: "ss", roles: ["admin"], password: "rf" } }, function (err: any) { });
-instance.session(function (error, session) { });
-instance.request({ db: "shared_headers", doc: "new", headers: { "If-None-Match": JSON.stringify({}) } }, function (error, helloWorld, rh) { });
-instance.relax({ db: "document_list", doc: "_all_docs", method: "GET", qs: { limit: 1 } }, function (error, docs) { });
-instance.auth("", "", function (err: any, _: any, headers: any) { });
-instance.uuids(3, function (error, data) { });
+instance.relax({ method: "POST", path: "_users", body: { _id: "org.couchdb.user:", type: "user", name: "ss", roles: ["admin"], password: "rf" } }, (err: any) => {});
+instance.session((error, session) => {});
+instance.request({ db: "shared_headers", doc: "new", headers: { "If-None-Match": JSON.stringify({}) } }, (error, helloWorld, rh) => {});
+instance.relax({ db: "document_list", doc: "_all_docs", method: "GET", qs: { limit: 1 } }, (error, docs) => {});
+instance.auth("", "", (err: any, _: any, headers: any) => {});
+instance.uuids(3, (error, data) => {});
 
 /*
  * Database Scope
  */
 const db: nano.DatabaseScope = instance.db;
 
-db.create("az09_$()+-/", function (err) { });
-db.get("database_get", function (error, response) { });
-db.destroy("_users", function () { });
-db.list(function (error, list) { });
-db.replicate(instance.use("a"), instance.use("b"), function (error: any) { });
-db.replicate("a", "b", function (error: any) { });
-
+db.create("az09_$()+-/", (err) => {});
+db.get("database_get", (err, response) => {});
+db.destroy("_users", () => {});
+db.list((error, list) => {});
+db.replicate(instance.use("a"), instance.use("b"), (error: any) => {});
+db.replicate("a", "b", (error: any) => {});
 
 /*
  * Document Scope
  */
 const mydb: nano.DocumentScope = instance.use("mydb");
 
-mydb.insert({ "foo": "baz" }, null, function (error, response) { });
-mydb.insert({ "foo": "baz" }, "foobar", function (error, foo) { });
-mydb.get("foobaz", { "revs_info": true }, function (error, foobaz) { });
-mydb.head("foobaz", function (error, body, headers) { });
-mydb.copy("foo_src", "foo_dest", { overwrite: true }, function (error, response, headers) { });
-mydb.destroy(undefined, undefined, function (error, response) { });
-mydb.bulk({ "docs": [{ "key": "baz", "name": "bazzel" }, { "key": "bar", "name": "barry" }] }, function (error: any, response: any) { });
-mydb.list(function (error: any, docs: any) { });
-mydb.fetch({ keys: ["foobar"] }, function (error: any, docs: any) { });
-mydb.fetchRevs({ keys: ["foobar"] }, function (error: any, docs: any) { });
-mydb.changes({ since: 0 }, function (error, response) { });
-mydb.compact(function (error) { });
+mydb.insert({ foo: "baz" }, null, (err, response) => {});
+mydb.insert({ foo: "baz" }, "foobar", (error, foo) => {});
+mydb.get("foobaz", { revs_info: true }, (error, foobaz) => {});
+mydb.head("foobaz", (error, body, headers) => {});
+mydb.copy("foo_src", "foo_dest", { overwrite: true }, (error, response, headers) => {});
+mydb.destroy("x", "x", (err, response) => {});
+mydb.bulk({ docs: [{ key: "baz", name: "bazzel" }, { key: "bar", name: "barry" }] }, (error: any, response: any) => {});
+mydb.list((error: any, docs: any) => {});
+mydb.fetch({ keys: ["foobar"] }, (error: any, docs: any) => {});
+mydb.fetchRevs({ keys: ["foobar"] }, (error: any, docs: any) => {});
+mydb.changes({ since: 0 }, (err, response) => {});
+mydb.compact((error) => {});
 const feed = mydb.follow({ since: "0" });
-feed.on("change", function (change: any) { });
-mydb.atomic("update", "addbaz", "baz", function (error: any, response: any) { });
-mydb.viewWithList("people", "by_name_and_city", "my_list", { key: ["Derek", "San Francisco"] }, function (error, list) { });
-mydb.view("alice", "by_id", { keys: ["foobar", "barfoo"], "include_docs": true }, function (err, view) { });
-mydb.show("people", "singleDoc", "p_clemens", function (error: any, doc: any, rh: any) { });
-mydb.replicate("database_replica", function (error: any) { });
+feed.on("change", (change: any) => {});
+mydb.atomic("update", "addbaz", "baz", (error: any, response: any) => {});
+mydb.viewWithList("people", "by_name_and_city", "my_list", { key: ["Derek", "San Francisco"] }, (error, list) => {});
+mydb.view("alice", "by_id", { keys: ["foobar", "barfoo"], include_docs: true }, (err, view) => {});
+mydb.show("people", "singleDoc", "p_clemens", (error: any, doc: any, rh: any) => {});
+mydb.replicate("database_replica", (error: any) => {});
 
 /*
  * Attachments
  */
-mydb.attachment.insert("new", "att", "Hello World!", "text/plain", function (error: any, att: any) { });
-mydb.attachment.destroy("new", "att", { rev: "123" }, function (error, response) { });
-mydb.attachment.get("new_string", "att", function (error: any, helloWorld: any) { });
+mydb.attachment.insert("new", "att", "Hello World!", "text/plain", (error: any, att: any) => {});
+mydb.attachment.destroy("new", "att", { rev: "123" }, (err, response) => {});
+mydb.attachment.get("new_string", "att", (error: any, helloWorld: any) => {});
 
 /*
  * Multipart
  */
-mydb.multipart.insert({ "foo": "baz" }, [{}], "foobaz", function (error, foo) { });
-mydb.multipart.get("foobaz", function (error: any, foobaz: any, headers: any) { });
+mydb.multipart.insert({ foo: "baz" }, [{}], "foobaz", (error, foo) => {});
+mydb.multipart.get("foobaz", (error: any, foobaz: any, headers: any) => {});
 
 /*
  * Piping
  */
 const rs = fs.createReadStream("");
 const is = mydb.attachment.insert("nodejs", "logo.png", null, "image/png");
-is.on("end", function () { });
+is.on("end", () => {});
 rs.pipe(is);
