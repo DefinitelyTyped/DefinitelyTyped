422bcbc894989de746f542e0e60e961af0701a53
Andrey Kyrdyumov <kant2002@gmail.com>
2017-06-30 13:14:56 +0600

    Fix lint warning for mina

--- types/mina/index.d.ts
+++ types/mina/index.d.ts
@@ -1,20 +1,20 @@
-// Type definitions for mina Snap-SVG 0.4.1
+// Type definitions for mina Snap-SVG 0.4
 // Project: https://github.com/adobe-webplatform/Snap.svg
 // Definitions by: Lars Klein <https://github.com/lhk>, Mattanja Kern <https://github.com/mattanja>, Andrey Kurdyumov <https://github.com/kant2002>
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
 
-declare function mina(a:number, A:number, b:number, B:number, get:Function, set:Function, easing?:(num:number)=>number):mina.AnimationDescriptor;
+declare function mina(a: number, A: number, b: number, B: number, get: () => number, set: (time: number) => void, easing?: (num: number) => number): mina.AnimationDescriptor;
 declare namespace mina {
-    export interface MinaAnimation {
+    interface MinaAnimation {
         id: string;
-        duration: Function;
-        easing: Function;
-        speed: Function;
-        status: Function;
-        stop: Function;
+        duration(): number;
+        easing(): number;
+        speed(): number;
+        status(): number;
+        stop(): void;
     }
 
-    export interface AnimationDescriptor {
+    interface AnimationDescriptor {
         id: string;
         start: number;
         end: number;
@@ -37,14 +37,14 @@ declare namespace mina {
         update(): void;
     }
 
-    export function backin(n:number):number;
-    export function backout(n:number):number;
-    export function bounce(n:number):number;
-    export function easein(n:number):number;
-    export function easeinout(n:number):number;
-    export function easeout(n:number):number;
-    export function elastic(n:number):number;
-    export function getById(id:string):AnimationDescriptor;
-    export function linear(n:number):number;
-    export function time():number;
+    function backin(n: number): number;
+    function backout(n: number): number;
+    function bounce(n: number): number;
+    function easein(n: number): number;
+    function easeinout(n: number): number;
+    function easeout(n: number): number;
+    function elastic(n: number): number;
+    function getById(id: string): AnimationDescriptor;
+    function linear(n: number): number;
+    function time(): number;
 }
