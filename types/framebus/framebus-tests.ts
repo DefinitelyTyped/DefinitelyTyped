d4d886d72801bfe00b20556840b8983be74f1fe9
kamil bukum <kamilbukum@gmail.com>
2017-07-01 03:02:49 +0300

    * fixed dom and callback problems in test.

--- types/framebus/framebus-tests.ts
+++ types/framebus/framebus-tests.ts
@@ -6,9 +6,10 @@ framebus.emit('hello popup and friends!');
 
 framebus.target('https://example.com').on('my cool event', () => {});
 
-framebus.publish('Marco!', callback, 'http://listener.example.com');
-
 let callback = (data: any) => {
     console.log('Got back %s as a reply!', data);
 };
+
+framebus.publish('Marco!', callback, 'http://listener.example.com');
+
 framebus.publish('Marco!', callback, 'http://listener.example.com');
