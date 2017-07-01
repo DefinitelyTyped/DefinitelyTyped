fb96955d50672f3639e151ff36e87a37e2e774e3
kimu_shu <alfvegardrisc@gmail.com>
2017-07-01 12:34:00 +0900

    Add tslint.json and fix lint errors

--- types/promise.prototype.finally/index.d.ts
+++ types/promise.prototype.finally/index.d.ts
@@ -1,4 +1,4 @@
-// Type definitions for promise.prototype.finally v2.0.1
+// Type definitions for promise.prototype.finally 2.0
 // Project: https://github.com/matthew-andrews/Promise.prototype.finally
 // Definitions by: Slava Shpitalny <https://github.com/slavik57>
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
