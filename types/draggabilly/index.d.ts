1bdaeb9cbfade9ba325392fde20f5e9d57d0c72e
Jason Wu <jason.a.wu@accenture.com>
2017-07-02 11:47:34 -0500

    Wrote it as an external module

--- types/draggabilly/index.d.ts
+++ types/draggabilly/index.d.ts
@@ -4,43 +4,35 @@
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
 // TypeScript Version: 2.3
 
-// export = Backbone;
-// export as namespace Backbone;
+export interface Position {
+    x: number;
+    y: number;
+}
 
-declare module 'draggabilly' {
-    interface Position {
-        x: number;
-        y: number;
-    }
+export interface DraggabillyOptions {
+    axis?: 'x' | 'y';
+    containment?: Element | string | boolean;
+    grid?: [number, number];
+    handle?: string;
+}
 
-    export interface DraggabillyOptions {
-        axis?: 'x' | 'y';
-        containment?: Element | string | boolean;
-        grid?: [number, number];
-        handle?: string;
-    }
+export type DraggabillyEventName = 'dragStart' | 'dragMove' | 'dragEnd' | 'pointerDown' | 'pointerMove' | 'pointerUp'
+    | 'staticClick';
 
-    export type DraggabillyEventName = 'dragStart' | 'dragMove' | 'dragEnd' | 'pointerDown' | 'pointerMove' | 'pointerUp'
-        | 'staticClick';
+export default class Draggabilly {
+    position: Position;
 
-    export default class Draggabilly {
-        position: Position;
+    constructor(element: Element | string, options?: DraggabillyOptions);
 
-        constructor(element: Element | string, options?: DraggabillyOptions);
+    on(eventName: DraggabillyEventName, listener: (event: Event, pointer: MouseEvent | Touch, moveVector: Position) => void): Draggabilly;
 
-        on(eventName: DraggabillyEventName,
-           listener: (event: Event, pointer: MouseEvent | Touch, moveVector: Position) => void): void;
+    off(eventName: DraggabillyEventName, listener: (event: Event, pointer: MouseEvent | Touch, moveVector: Position) => void): Draggabilly;
 
-        off(eventName: DraggabillyEventName,
-            listener: (event: Event, pointer: MouseEvent | Touch, moveVector: Position) => void): void;
+    once(eventName: DraggabillyEventName, listener: (event: Event, pointer: MouseEvent | Touch, moveVector?: Position) => void): Draggabilly;
 
-        once(eventName: DraggabillyEventName,
-             listener: (event: Event, pointer: MouseEvent | Touch, moveVector?: Position) => void): void;
+    enable(): void;
 
-        enable(): void;
+    disable(): void;
 
-        disable(): void;
-
-        destroy(): void;
-    }
+    destroy(): void;
 }
