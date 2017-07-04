36064effea5ac79a58328e374eaf7355aaf4c475
Guo Yunhe <guoyunhebrave@gmail.com>
2017-07-04 15:57:47 +0300

    react-modal: do not augment module

--- types/react-modal/index.d.ts
+++ types/react-modal/index.d.ts
@@ -7,39 +7,37 @@
 import * as React from 'react';
 import * as ReactDOM from 'react-dom';
 
-declare module 'react-modal' {
-    interface ModalProps {
-        isOpen: boolean;
-        style?: ModalStyles;
-        portalClassName?: string;
-        bodyOpenClassName?: string;
-        className?: string | object;
-        overlayClassName?: string | object;
-        appElement?: HTMLElement;
-        onAfterOpen?(): void;
-        onRequestClose?(): void;
-        closeTimeoutMS?: number;
-        ariaHideApp?: boolean;
-        shouldCloseOnOverlayClick?: boolean;
-        parentSelector?(): void;
-        aria?: object;
-        role?: string;
-        contentLabel?: string;
-    }
+interface ModalProps {
+    isOpen: boolean;
+    style?: ModalStyles;
+    portalClassName?: string;
+    bodyOpenClassName?: string;
+    className?: string | object;
+    overlayClassName?: string | object;
+    appElement?: HTMLElement;
+    onAfterOpen?(): void;
+    onRequestClose?(): void;
+    closeTimeoutMS?: number;
+    ariaHideApp?: boolean;
+    shouldCloseOnOverlayClick?: boolean;
+    parentSelector?(): void;
+    aria?: object;
+    role?: string;
+    contentLabel?: string;
+}
 
-    interface ModalStyles {
-        overlay: {};
-        content: {};
-    }
+interface ModalStyles {
+    overlay: {};
+    content: {};
+}
 
-    class Modal extends React.Component<ModalProps, {}> {
-        static setAppElement(element: HTMLElement): void;
-        static injectCSS(): void;
-        static defaultProps: ModalProps;
-        static defaultStyles: ModalStyles;
-        removePortal(): void;
-        renderPortal(props: ModalProps): void;
-    }
+declare class Modal extends React.Component<ModalProps, {}> {
+    static setAppElement(element: HTMLElement): void;
+    static injectCSS(): void;
+    static defaultProps: ModalProps;
+    static defaultStyles: ModalStyles;
+    removePortal(): void;
+    renderPortal(props: ModalProps): void;
 }
 
 export default Modal;
