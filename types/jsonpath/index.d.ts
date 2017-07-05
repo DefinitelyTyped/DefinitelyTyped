377eb09ae0399563dbb439c75938663cba8e46c6
Ika <ikatyang@gmail.com>
2017-07-05 21:15:46 +0800

    feat(jsonpath): update to version 0.2.11

--- types/jsonpath/index.d.ts
+++ types/jsonpath/index.d.ts
@@ -1,17 +1,17 @@
-// Type definitions for jsonpath 0.1.3
+// Type definitions for jsonpath 0.2.11
 // Project: https://www.npmjs.org/package/jsonpath
-// Definitions by: Hiroki Horiuchi <https://github.com/horiuchi>
+// Definitions by: Hiroki Horiuchi <https://github.com/horiuchi>, Ika <https://github.com/ikatyang>
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
 
 
 
 type PathComponent = string | number;
 
-export declare function query(obj: any, pathExpression: string): any[];
-export declare function paths(obj: any, pathExpression: string): PathComponent[][];
-export declare function nodes(obj: any, pathExpression: string): { path: PathComponent[]; value: any; }[];
+export declare function query(obj: any, pathExpression: string, count?: number): any[];
+export declare function paths(obj: any, pathExpression: string, count?: number): PathComponent[][];
+export declare function nodes(obj: any, pathExpression: string, count?: number): { path: PathComponent[]; value: any; }[];
 export declare function value(obj: any, pathExpression: string): any;
-export declare function value(obj: any, pathExpression: string, newValue: any): any;
+export declare function value<T>(obj: any, pathExpression: string, newValue: T): T;
 export declare function parent(obj: any, pathExpression: string): any;
 export declare function apply(obj: any, pathExpression: string, fn: (x: any) => any): { path: PathComponent[]; value: any; }[];
 export declare function parse(pathExpression: string): any[];
