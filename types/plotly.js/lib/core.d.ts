367746da59fdcf6289b32360022f76523e439992
Grant Nestor <grantnestor@gmail.com>
2017-07-05 14:07:26 -0700

    Add alias for 'plotly.js/lib/core' submodule

---
+++ types/plotly.js/lib/core.d.ts
@@ -0,0 +1,9 @@
+// Type definitions for plotly.js 1.27
+// Project: https://plot.ly/javascript/
+// Definitions by: Chris Gervang <https://github.com/chrisgervang>, Martin Duparc <https://github.com/martinduparc>
+// Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
+// TypeScript Version: 2.2
+
+import * as Plotly from '..';
+
+export = Plotly;
