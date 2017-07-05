ddc01963e7d974a746010011c3b6c60afee0e6c9
Adrian (Grim) Roach <adrianroach@ebongarde.com>
2017-07-04 19:59:31 -0700

    Added ebongarde-root type definition.

---
+++ types/ebongarde-root/index.d.ts
@@ -0,0 +1,8 @@
+// Type definitions for ebongarde-root 1.7
+// Project: https://github.com/Ebongarde/ebongarde-root#readme
+// Definitions by: My Self <https://github.com/me>
+// Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
+
+/// <reference types= "node" />
+
+export declare var ROOT: string;
