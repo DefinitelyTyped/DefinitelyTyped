3fe38edbc4e0ff74d5d9d2c6a06b571c3db72fc0
york yao <yaoao12306@outlook.com>
2017-07-06 23:22:20 +0800

    improve types of exit

--- types/exit/index.d.ts
+++ types/exit/index.d.ts
@@ -1,8 +1,8 @@
-// Type definitions for exit 0.1.2
+// Type definitions for exit 0.1
 // Project: https://github.com/cowboy/node-exit
 // Definitions by: Bart van der Schoor <https://github.com/Bartvds>
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
 
-
 declare function exit(code: number): void;
+declare namespace exit { }
 export = exit;
