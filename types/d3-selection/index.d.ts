881de33223d5a58b70c8ea20318e24808a64e1d2
Bernie Telles <btelles@gmail.com>
2017-07-06 07:44:42 -0700

    Move TS version directive to last line of header.
    It's unclear to me why the test runner is not picking up the TS version directive. Perhaps the test runner looks at the last "//" comments in the file?

--- types/d3-selection/index.d.ts
+++ types/d3-selection/index.d.ts
@@ -2,13 +2,13 @@
 // Project: https://github.com/d3/d3-selection/
 // Definitions by: Tom Wanzek <https://github.com/tomwanzek>, Alex Ford <https://github.com/gustavderdrache>, Boris Yankov <https://github.com/borisyankov>
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
-// TypeScript Version: 2.3
 
 // Last module patch version validated against: 1.1
 
 // --------------------------------------------------------------------------
 // Shared Type Definitions and Interfaces
 // --------------------------------------------------------------------------
+// TypeScript Version: 2.3
 
 /**
  * BaseType serves as an alias for the 'minimal' data type which can be selected
