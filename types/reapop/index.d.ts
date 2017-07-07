a2e5ed8b4c5d259e6453036152d59304c07a642c
Georgy Krivonosov <Barrokgl@users.noreply.github.com>
2017-07-07 10:50:01 +0300

    fix typescript version

--- types/reapop/index.d.ts
+++ types/reapop/index.d.ts
@@ -2,7 +2,7 @@
 // Project: https://github.com/LouisBarranqueiro/reapop#readme
 // Definitions by: Barrokgl <https://github.com/Barrokgl>
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
-// TypeScript Version: 2.1.1
+// TypeScript Version: 2.3
 
 import * as React from 'react';
 
