0477d380e5f1dc4fbcf7e94f03767e3ed20c0a72
Peter Keuter <github@peterkeuter.nl>
2017-07-07 17:09:56 +0200

    update version number

--- types/mssql/index.d.ts
+++ types/mssql/index.d.ts
@@ -1,4 +1,4 @@
-// Type definitions for mssql 4.0.2
+// Type definitions for mssql 4.0.4
 // Project: https://www.npmjs.com/package/mssql
 // Definitions by: COLSA Corporation <http://www.colsa.com/>, Ben Farr <https://github.com/jaminfarr>, Vitor Buzinaro <https://github.com/buzinas>, Matt Richardson <https://github.com/mrrichar/>, Jørgen Elgaard Larsen <https://github.com/elhaard/>, Peter Keuter <https://github.com/pkeuter/>
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
