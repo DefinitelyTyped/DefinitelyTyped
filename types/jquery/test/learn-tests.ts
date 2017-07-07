a18fdfd2f408170ed0b8b200e7cb24f937fa9b58
stonio <stonio@users.noreply.github.com>
2017-07-07 18:14:42 +0200

    Create learn-tests.ts

---
+++ types/jquery/test/learn-tests.ts
@@ -0,0 +1,20 @@
+import jQueryFactory = require('jquery');
+const jQuery = jQueryFactory(window, true);
+
+# From https://learn.jquery.com/plugins/basic-plugin-creation/
+
+## Basic Plugin Authoring
+
+interface JQuery {
+    greenify: GreenifyPlugin;
+}
+
+interface GreenifyPlugin {
+   (this: JQuery): void;
+}
+
+jQuery.fn.greenify = function() {
+    this.css( "color", "green" );
+};
+
+jQuery( "a" ).greenify(); // Makes all the links green.
