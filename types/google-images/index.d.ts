8a40d0e84d385cd87676f4feebea7165ea764172
Dolan <dolan_miu@hotmail.com>
2017-07-08 01:11:05 +0100

    Initial commit

---
+++ types/google-images/index.d.ts
@@ -0,0 +1,12 @@
+// Type definitions for google-images 1.2
+// Project: https://github.com/beatgammit/base64-js
+// Definitions by: Dolan Miu <https://github.com/dolanmiu>
+// Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
+
+declare namespace GoogleImages {
+    interface GoogleImages {
+
+    }
+}
+
+export = GoogleImages;
