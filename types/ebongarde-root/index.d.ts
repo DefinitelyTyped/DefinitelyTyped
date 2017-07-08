519562234ca5b527de447db297c464fcda98cbbc
Adrian (Grim) Roach <adrianroach@ebongarde.com>
2017-07-07 21:17:02 -0700

    Update index.d.ts

--- types/ebongarde-root/index.d.ts
+++ types/ebongarde-root/index.d.ts
@@ -1,6 +1,6 @@
 // Type definitions for ebongarde-root 1.7
 // Project: https://github.com/Ebongarde/ebongarde-root#readme
-// Definitions by: My Self <https://github.com/me>
+// Definitions by: Adrian Roach <https://github.com/Ebongarde>
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
 
 /// <reference types= "node" />
