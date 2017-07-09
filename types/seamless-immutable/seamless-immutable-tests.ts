fb798e5fabb9cd792dc27c95ae3e963d02964d6d
Stepan Burguchev <me@sburg.net>
2017-07-09 18:07:06 +0300

    final polishing

--- types/seamless-immutable/seamless-immutable-tests.ts
+++ types/seamless-immutable/seamless-immutable-tests.ts
@@ -25,8 +25,8 @@ interface ExtendedUser extends User {
         lastName: string;
     };
 
-    const arrayOfNumbers: number[] & Immutable.ImmutableArray<number> = Immutable.from([0, 2]);
-    const user: User & Immutable.ImmutableObject<User> = Immutable.from({
+    const arrayOfNumbers: Immutable.ImmutableArray<number> = Immutable.from([0, 2]);
+    const user: Immutable.ImmutableObject<User> = Immutable.from({
         firstName: 'Angry',
         lastName: 'Monkey'
     });
@@ -45,14 +45,14 @@ interface ExtendedUser extends User {
 // Instance syntax: immutable array
 // ---------------------------------------------------------------
 {
-    const array: User[] & Immutable.ImmutableArray<User> = Immutable.from<User>([ { firstName: 'Angry', lastName: 'Monkey' } ]);
+    const array: Immutable.ImmutableArray<User> = Immutable.from<User>([ { firstName: 'Angry', lastName: 'Monkey' } ]);
 
     // asMutable
     const mutableArray1: User[] = array.asMutable();
     const mutableArray2: User[] = array.asMutable({ deep: true });
 
     // flatMap
-    const flatMappedArray: User[] & Immutable.ImmutableArray<User> = array.flatMap((value: User) =>
+    const flatMappedArray: Immutable.ImmutableArray<User> = array.flatMap((value: User) =>
         [value, value]
     );
 
@@ -78,26 +78,26 @@ interface ExtendedUser extends User {
     });
 
     // set: property name is strongly checked
-    const updatedUser: User & Immutable.ImmutableObject<User> = immutableUser.set('firstName', 'Whirlwind');
+    const updatedUser: Immutable.ImmutableObject<User> = immutableUser.set('firstName', 'Whirlwind');
 
-    // setIn: property path is strongly checked for up to 4 arguments
-    const updatedUser2: ExtendedUser & Immutable.ImmutableObject<ExtendedUser> = immutableUserEx.setIn(['address', 'line1'], 'Small house');
+    // setIn: property path is strongly checked for up to 5 arguments
+    const updatedUser2: Immutable.ImmutableObject<ExtendedUser> = immutableUserEx.setIn(['address', 'line1'], 'Small house');
 
     // asMutable
     const mutableUser1: User = immutableUser.asMutable();
     const mutableUser2: User = immutableUser.asMutable({ deep: true });
 
     // merge: merged part is strongly checked as a deeply partial object
-    const mergedUser: User & Immutable.ImmutableObject<User> = immutableUserEx.merge({ address: { line1: 'Small house' }, firstName: 'Jack' });
+    const mergedUser: Immutable.ImmutableObject<User> = immutableUserEx.merge({ address: { line1: 'Small house' }, firstName: 'Jack' });
 
     // update: property name is strongly checked
-    const updatedUser3: User & Immutable.ImmutableObject<User> = immutableUser.update('firstName', x => x.toLowerCase() + ' Whirlwind');
+    const updatedUser3: Immutable.ImmutableObject<User> = immutableUser.update('firstName', x => x.toLowerCase() + ' Whirlwind');
 
-    // updateIn: property path is strongly checked for up to 4 arguments
-    const updatedUser4: User & Immutable.ImmutableObject<User> = immutableUserEx.updateIn([ 'address', 'line1' ], x => x.toLowerCase() + ' 43');
+    // updateIn: property path is strongly checked for up to 5 arguments
+    const updatedUser4: Immutable.ImmutableObject<User> = immutableUserEx.updateIn([ 'address', 'line1' ], x => x.toLowerCase() + ' 43');
 
     // without: the return type must be specified explicitly or it will be `any`
     const simpleUser1: Immutable.ImmutableObject<any> = immutableUserEx.without('address');
-    const simpleUser2: User & Immutable.ImmutableObject<User> = immutableUserEx.without<User>('address');
+    const simpleUser2: Immutable.ImmutableObject<User> = immutableUserEx.without<User>('address');
     const simpleUser3: Immutable.ImmutableObject<any> = immutableUserEx.without('firstName', 'lastName');
 }
