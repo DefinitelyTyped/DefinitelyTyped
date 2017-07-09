e0ef2faf9719cc6d4275f475564b5e46359ae2f6
Nicolas Penin <nicolas.penin@dragon-angel.fr>
2017-07-09 12:30:53 +0200

    added json-rpc-ws type definition

---
+++ types/json-rpc-ws/json-rpc-ws-tests.ts
@@ -0,0 +1,28 @@
+import * as JsonRpcWs from '.';
+
+var server = JsonRpcWs.createServer();
+
+server.expose('mirror', function mirror(params, reply)
+{
+
+    console.log('mirror handler', params);
+    reply(null, params);
+});
+
+server.start({ port: 8080 }, function started()
+{
+
+    console.log('Server started on port 8080');
+});
+
+var client = JsonRpcWs.createClient();
+
+client.connect('ws://localhost:8080', function connected()
+{
+
+    client.send('mirror', ['a param', 'another param'], function mirrorReply(error, reply)
+    {
+
+        console.log('mirror reply', reply);
+    });
+});
