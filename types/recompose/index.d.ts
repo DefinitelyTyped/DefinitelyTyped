45cb0b0c7b33705038df181c9e96b47abb9a4d83
Ciarán Curley <ciaran.curley@singlepoint.ie>
2017-07-10 16:45:11 +0100

    Fix CI Fail; forgot Ctrl-S on my end

--- types/recompose/index.d.ts
+++ types/recompose/index.d.ts
@@ -15,7 +15,7 @@ declare module 'recompose' {
     type Component<P> = ComponentClass<P> | StatelessComponent<P>;
     type mapper<TInner, TOutter> = (input: TInner) => TOutter;
     type predicate<T> = mapper<T, boolean>;
-    type predicateDiff<T> = (current: T, next: T) => boolean}
+    type predicateDiff<T> = (current: T, next: T) => boolean
 
     interface Subscribable<T> {
         subscribe(observer: any):any;
