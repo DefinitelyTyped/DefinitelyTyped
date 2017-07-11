5dd4d38774f4551a2634198e7839161eaea446c1
Bastien <bmoulia@gmail.com>
2017-07-11 14:00:37 +0200

    Fix lint errors

--- types/spark-md5/index.d.ts
+++ types/spark-md5/index.d.ts
@@ -2,6 +2,7 @@
 // Project: https://github.com/satazor/js-spark-md5#readme
 // Definitions by: My Self <https://github.com/bastienmoulia>
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
+// TypeScript Version: 2.3
 
 interface State {
 	buff: Uint8Array;
@@ -9,34 +10,37 @@ interface State {
 	hash: number[];
 }
 
-interface IArrayBuffer extends ArrayBuffer {}
+// copy of ArrayBuffer because of a conflict with the class SparkMD5.ArrayBuffer
+interface ArrayBufferCopy extends ArrayBuffer {}
 
 declare class SparkMD5 {
 	constructor();
 	append(str: string): SparkMD5;
 	appendBinary(contents: string): SparkMD5;
-	end (raw?: boolean): string;
-	reset (): SparkMD5;
+	end(raw?: boolean): string;
+	reset(): SparkMD5;
 	getState(): State;
 	setState(state: State): State;
 	destroy(): void;
-	static hash(str: string, raw?: boolean): string
-	static hashBinary(content: string, raw?: boolean): string
+	static hash(str: string, raw?: boolean): string;
+	static hashBinary(content: string, raw?: boolean): string;
 }
 
 declare namespace SparkMD5 {
 	class ArrayBuffer {
 		constructor();
 		append(str: string): ArrayBuffer;
-		end (raw?: boolean): string;
-		reset (): ArrayBuffer;
+		end(raw?: boolean): string;
+		reset(): ArrayBuffer;
 		getState(): State;
 		setState(state: State): State;
 		destroy(): void;
-		static hash(arr: IArrayBuffer, raw?: boolean): string
+		static hash(arr: ArrayBufferCopy, raw?: boolean): string;
 	}
 }
 
-declare module "spark-md5" {
+export = SparkMD5;
+
+/*declare module "spark-md5" {
 	export = SparkMD5;
-}
+}*/
--- types/spark-md5/spark-md5-tests.ts
+++ types/spark-md5/spark-md5-tests.ts
@@ -4,17 +4,17 @@ SparkMD5.hash('Hi there');
 SparkMD5.hash('Hi there', true);
 SparkMD5.hashBinary("");
 
-var spark = new SparkMD5();
+let spark = new SparkMD5();
 spark.append('Hi');
 spark.append(' there');
-var hexHash = spark.end();
-var rawHash = spark.end(true);
+let hexHash = spark.end();
+let rawHash = spark.end(true);
 
-var sparkArr = new SparkMD5.ArrayBuffer();
+let sparkArr = new SparkMD5.ArrayBuffer();
 sparkArr.append('Hi');
 sparkArr.append(' there');
-var hexHash = sparkArr.end();
-var rawHash = sparkArr.end(true);
+hexHash = sparkArr.end();
+rawHash = sparkArr.end(true);
 
 const arr = new ArrayBuffer(8);
 SparkMD5.ArrayBuffer.hash(arr);
