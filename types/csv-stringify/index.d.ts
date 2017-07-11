9045ee35ebe9c56e00f3994de1827c952141badb
Arjen van der Ende <arjen.vanderende@gmail.com>
2017-07-11 15:09:09 +0200

    [csv-stringify] Bump version number in header

--- types/csv-stringify/index.d.ts
+++ types/csv-stringify/index.d.ts
@@ -1,4 +1,4 @@
-// Type definitions for csv-stringify 1.0
+// Type definitions for csv-stringify 1.4
 // Project: https://github.com/wdavidw/node-csv-stringify
 // Definitions by: Rogier Schouten <https://github.com/rogierschouten>
 //                 Arjen van der Ende <https://github.com/arjenvanderende>
