4763e19733c5d1fbb0550c198086acaac767341d
Mika Kuitunen <mika.kuitunen@wapice.com>
2017-07-13 11:50:11 +0300

    react-redux-toastr: remove module declaration

--- types/react-redux-toastr/index.d.ts
+++ types/react-redux-toastr/index.d.ts
@@ -9,64 +9,61 @@
 import { Component } from 'react';
 import { Action, ActionCreator, Reducer } from 'redux';
 
-declare module "react-redux-toastr" {
-
-    interface ToastrOptions {
-        timeOut?: number;
-        newestOnTop?: boolean;
-        position?: string;
-        confirmOptions?: ConfirmOptions;
-        preventDuplicates?: boolean;
-        transitionIn?: 'bounceIn' | 'bounceInDown' | 'fadeIn';
-        transitionOut?: 'bounceOut' | 'bounceOutUp' | 'fadeOut';
-        progressBar?: boolean;
-    }
-
-    interface ConfirmOptions {
-        okText: string;
-        cancelText: string;
-    }
-
-    export default class ReduxToastr extends Component<ToastrOptions, {}> {}
-
-    interface EmitterOptions {
-        icon?: string;
-        timeOut?: number;
-        removeOnHover?: boolean;
-        removeOnClick?: boolean;
-        component?: React.Component<any, any>;
-        onShowComplete?: () => void;
-        onHideComplete?: () => void;
-    }
-
-    interface ToastrConfirmOptions {
-        onOk: () => void;
-        onCancel: () => void;
-    }
-
-    interface ToastrEmitter {
-        message: (title: string, message: string, options?: EmitterOptions) => void;
-        info: (title: string, message: string, options?: EmitterOptions) => void;
-        success: (title: string, message: string, options?: EmitterOptions) => void;
-        warning: (title: string, message: string, options?: EmitterOptions) => void;
-        error: (title: string, message: string, options?: EmitterOptions) => void;
-        clean: () => void;
-        confirm: (message: string, options: ToastrConfirmOptions) => void;
-    }
-
-    interface Actions {
-        addToastrAction: Action;
-        clean: Action;
-        remove: Action;
-        success: Action;
-        info: Action;
-        warning: Action;
-        error: Action;
-        showConfirm: Action;
-        hideConfirm: Action;
-    }
-
-    export const actions: ActionCreator<Actions>;
-    export const reducer: Reducer<any>;
-    export const toastr: ToastrEmitter;
+interface ToastrOptions {
+    timeOut?: number;
+    newestOnTop?: boolean;
+    position?: string;
+    confirmOptions?: ConfirmOptions;
+    preventDuplicates?: boolean;
+    transitionIn?: 'bounceIn' | 'bounceInDown' | 'fadeIn';
+    transitionOut?: 'bounceOut' | 'bounceOutUp' | 'fadeOut';
+    progressBar?: boolean;
 }
+
+interface ConfirmOptions {
+    okText: string;
+    cancelText: string;
+}
+
+export default class ReduxToastr extends Component<ToastrOptions, {}> {}
+
+interface EmitterOptions {
+    icon?: string;
+    timeOut?: number;
+    removeOnHover?: boolean;
+    removeOnClick?: boolean;
+    component?: React.Component<any, any>;
+    onShowComplete?: () => void;
+    onHideComplete?: () => void;
+}
+
+interface ToastrConfirmOptions {
+    onOk: () => void;
+    onCancel: () => void;
+}
+
+interface ToastrEmitter {
+    message: (title: string, message: string, options?: EmitterOptions) => void;
+    info: (title: string, message: string, options?: EmitterOptions) => void;
+    success: (title: string, message: string, options?: EmitterOptions) => void;
+    warning: (title: string, message: string, options?: EmitterOptions) => void;
+    error: (title: string, message: string, options?: EmitterOptions) => void;
+    clean: () => void;
+    confirm: (message: string, options: ToastrConfirmOptions) => void;
+}
+
+interface Actions {
+    addToastrAction: Action;
+    clean: Action;
+    remove: Action;
+    success: Action;
+    info: Action;
+    warning: Action;
+    error: Action;
+    showConfirm: Action;
+    hideConfirm: Action;
+}
+
+export const actions: ActionCreator<Actions>;
+export const reducer: Reducer<any>;
+export const toastr: ToastrEmitter;
