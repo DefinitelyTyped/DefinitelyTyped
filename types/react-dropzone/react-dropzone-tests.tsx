45aa10637d2cac6502055a995cc7a145afda8e4a
Karol Janyst <lapkom@gmail.com>
2017-07-13 11:36:36 +0900

    Update definitions for react-dropzone

--- types/react-dropzone/react-dropzone-tests.tsx
+++ types/react-dropzone/react-dropzone-tests.tsx
@@ -1,10 +1,7 @@
-import * as React from 'react';
-import Dropzone = require('react-dropzone');
+import * as React from "react";
+import * as Dropzone from "react-dropzone";
 
 class Test extends React.Component {
-  constructor(props: any) {
-    super(props);
-  }
 
   dz: Dropzone;
 
@@ -12,33 +9,41 @@ class Test extends React.Component {
     this.dz.open();
   }
 
+  handleDropFile = (files: File[]) => {};
+
+  handleDropFiles = (accepted: File[], rejected?: File[]) => {};
+
   render() {
     return (
       <div>
         <Dropzone
           ref={(node) => { this.dz = node } }
-          onDrop={(e: any) => { e.preventDefault(); } }
-          onDropAccepted={(e: any) => { e.preventDefault(); } }
-          onDropRejected={(e: any) => { e.preventDefault(); } }
-          onDragStart={(e: any) => { e.preventDefault(); } }
-          onDragEnter={(e: any) => { e.preventDefault(); } }
-          onDragLeave={(e: any) => { e.preventDefault(); } }
+          onClick={ (e: any) => { e.preventDefault(); } }
+          onDrop={ this.handleDropFiles }
+          onDropAccepted={ this.handleDropFile }
+          onDropRejected={ this.handleDropFile }
+          onDragStart={ (e: any) => { e.preventDefault(); } }
+          onDragEnter={ (e: any) => { e.preventDefault(); } }
+          onDragLeave={ (e: any) => { e.preventDefault(); } }
+          onFileDialogCancel={ () => undefined }
           style={{ borderStyle: "dashed" }}
           activeStyle={{ borderStyle: "dotted" }}
           rejectStyle={{ borderStyle: "dotted" }}
           className="regular"
           activeClassName="active"
           rejectClassName="reject"
-          minSize={2000}
-          maxSize={Infinity}
-          disablePreview={true}
-          disableClick={true}
-          multiple={false}
+          minSize={ 2000 }
+          maxSize={ Infinity }
+          disablePreview
+          disableClick
+          multiple={ false }
           accept="*.png"
           name="dropzone"
-          inputProps={{ id: "dropzone" }}
+          inputProps={ { id : "dropzone" } }
         />
       </div>
+
+
     );
   }
 }
