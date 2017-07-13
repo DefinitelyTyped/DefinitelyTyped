faa13ca5cc11ebd096c39ef5848fcaf7a54c3ecb
Karol Janyst <lapkom@gmail.com>
2017-07-13 09:17:06 +0900

    Fix types for transition components

--- types/react-transition-group/CSSTransition.d.ts
+++ types/react-transition-group/CSSTransition.d.ts
@@ -1,5 +1,4 @@
 import { Component } from "react";
-import { TransitionActions } from "react-transition-group";
 import { TransitionProps } from "react-transition-group/Transition";
 
 export interface CSSTransitionClassNames {
@@ -11,6 +10,25 @@ export interface CSSTransitionClassNames {
     exitActive?: string;
 }
 
+/**
+ * The animation classNames applied to the component as it enters or exits.
+ * A single name can be provided and it will be suffixed for each stage: e.g.
+ *
+ * `classNames="fade"` applies `fade-enter`, `fade-enter-active`,
+ * `fade-exit`, `fade-exit-active`, `fade-appear`, and `fade-appear-active`.
+ * Each individual classNames can also be specified independently like:
+ *
+ * ```js
+ * classNames={{
+ *  appear: 'my-appear',
+ *  appearActive: 'my-active-appear',
+ *  enter: 'my-enter',
+ *  enterActive: 'my-active-enter',
+ *  exit: 'my-exit',
+ *  exitActive: 'my-active-exit',
+ * }}
+ * ```
+ */
 export interface CSSTransitionProps extends TransitionProps {
     classNames: string | CSSTransitionClassNames;
 }
