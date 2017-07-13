3fd1e439fbd2b1a42854733c2b798b5445e5d71a
Alex Frazer <alexadfrazer@gmail.com>
2017-07-13 10:16:00 -0400

    update jwplayer to 7.7.5

--- types/jwplayer/index.d.ts
+++ types/jwplayer/index.d.ts
@@ -1,118 +1,325 @@
-// Type definitions for JW Player 6.0
-// Project: http://developer.longtailvideo.com/trac/
-// Definitions by: Martin Duparc <https://github.com/martinduparc/>
-// Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
-
-// JW Player is the leading HTML5 & Flash video player, optimized for mobile and the desktop. Easy enough for beginners, advanced enough for pros.
-
-interface JWPlayer {
-	addButton(icon: string, label: string, handler: () => void, id: string): void;
-	getAudioTracks(): any[];
-	getBuffer(): number;
-	getCaptionsList(): any[];
-	getControls(): boolean;
-	getCurrentAudioTrack(): number;
-	getCurrentCaptions(): number;
-	getCurrentQuality(): number;
-	getDuration(): number;
-	getHeight(): number;
-	getFullscreen(): boolean;
-	getMute(): boolean;
-	getPlaylist(): any[];
-	getPlaylistIndex(): number;
-	getPlaylistItem(index?: number): any;
-	getPosition(): number;
-	getQualityLevels(): any[];
-	getRenderingMode(): string;
-	getSafeRegion(): any[];
-	getState(): string;
-	getVolume(): number;
-	getWidth(): number;
-	load(playlist: any[]): void;
-	load(playlist: string): void;
-	onAdClick(callback: (o: { tag: string; }) => void): void;
-	onAdCompanions(callback: (o: {
-		tag: string;
-		companions: any[];
-	}) => void): void;
-	onAdComplete(callback: (o: { tag: string; }) => void): void;
-	onAdError(callback: (o: {
-		tag: string;
-		message: string;
-	}) => void): void;
-	onAdImpression(callback: (o: { tag: string; }) => void): void;
-	onAdPause(callback: (o: { tag: string; }) => void): void;
-	onAdPlay(callback: (o: { tag: string; }) => void): void;
-	onAdSkipped(callback: (o: { tag: string; }) => void): void;
-	onAdTime(callback: (o: {
-		tag: string;
-		position: number;
-		duration: number;
-		sequence: number;
-	}) => void): void;
-	onAudioTracks(callback: (o: { levels: any[]; }) => void): void;
-	onAudioTrackChange(callback: (o: { currentTrack: number; }) => void): void;
-	onBeforeComplete(callback: () => void): void;
-	onBeforePlay(callback: () => void): void;
-	onBuffer(callback: (o: { oldstate: string }) => void): void;
-	onBufferChange(callback: () => void): void;
-	onCaptionsChange(callback: (o: { track: number; }) => void): void;
-	onCaptionsList(callback: (o: { tracks: any[]; }) => void): void;
-	onComplete(callback: () => void): void;
-	onControls(callback: (o: { controls: boolean; }) => void): void;
-	onDisplayClick(callback: () => void): void;
-	onError(callback: (o: { message: string }) => void): void;
-	onFullscreen(callback: (o: { fullscreen: boolean; }) => void): void;
-	onIdle(callback: (o: { oldstate: string }) => void): void;
-	onLevelsChanged(callback: (o: { currentQuality: number }) => void): void;
-	onMeta(callback: (o: { metadata: any; }) => void): void;
-	onMute(callback: (o: { mute: boolean; }) => void): void;
-	onPause(callback: (o: { oldstate: string }) => void): void;
-	onPlay(callback: (o: { oldstate: string }) => void): void;
-	onPlaylist(callback: () => void): void;
-	onPlaylistItem(callback: () => void): void;
-	onPlaylistComplete(callback: () => void): void;
-	onReady(callback: () => void): void;
-	onResize(callback: () => void): void;
-	onQualityChange(callback: () => void): void;
-	onQualityLevels(callback: (o: { levels: any[] }) => void): void;
-	onSeek(callback: (o: {
-		position: number;
-		offset: number;
-	}) => void): void;
-	onSetupError(callback: (o: {
-		fallback: boolean;
-		message: string;
-	}) => void): void;
-	onTime(callback: (o: {
-		duration: number;
-		position: number;
-	}) => void): void;
-	onVolume(callback: (o: { volume: number; }) => void): void;
-	pause(state?: boolean): void;
-	play(state?: boolean): void;
-	playAd(tag: string): void;
-	playlistItem(index: number): void;
-	registerPlugin(id: string, target: string, jsPlugin: () => void, swfURL?: string): void;
-	remove(): void;
-	removeButton(id: string): void;
-	resize(width: number, height: number): void;
-	seek(position: number): void;
-	setControls(controls: boolean): void;
-	setCurrentAudioTrack(index: number): void;
-	setCurrentCaptions(index: number): void;
-	setCurrentQuality(index: number): void;
-	setFullscreen(state: boolean): void;
-	setMute(state?: boolean): void;
-	setup(options: any): JWPlayer;
-	setVolume(volume: number): void;
-	stop(): void;
+/**
+ * JWPlayer@7.7.4 types
+ * @author Alex Frazer
+ */
+declare enum Primary { 'flash', 'html5' }
+declare enum Preload { 'none', 'metadata', 'auto' }
+declare enum TrackKind { 'captions', 'chapters', 'thumbnails' }
+declare enum Stretching { 'uniform', 'exactfit', 'fill', 'none' }
+declare enum LogoPosition { 'top-left', 'top-right', 'bottom-left', 'bottom-right' }
+declare enum SharingSites { 'facebook', 'twitter', 'interest', 'email', 'tumblr', 'googleplus', 'reddit', 'linkedin' }
+declare enum RelatedComplete { 'hide', 'show', 'autoplay' }
+declare enum AdvertisingClient { 'vast', 'googima', 'freewheel' }
+declare enum PlayState { 'idle', 'buffering', 'playing', 'paused' }
+declare enum QualityReason { 'initial choice', 'auto', 'api' }
+declare enum Events {
+  'ready',
+  'setupError',
+  'playlist',
+  'playlistItem',
+  'playlistComplete',
+  'bufferChange',
+  'play',
+  'pause',
+  'buffer',
+  'idle',
+  'complete',
+  'error',
+  'seek',
+  'seeked',
+  'time',
+  'mute',
+  'volume',
+  'fullscreen',
+  'resize',
+  'levels',
+  'levelsChanged',
+  'captionsList',
+  'captionsChange',
+  'controls',
+  'displayClick',
+  'adClick',
+  'adCompanions',
+  'adComplete',
+  'adError',
+  'adimpression',
+  'adTime',
+  'adSkipped',
+  'beforePlay',
+  'beforeComplete',
+  'meta',
 }
 
-interface JWPlayerStatic {
-	(id: string): JWPlayer;
-	key: string;
-	version: string;
+interface Track {
+  file?: string,
+  kind?: TrackKind,
+  label?: string,
+  default?: boolean,
 }
 
-declare var jwplayer:JWPlayerStatic;
+interface Source {
+  file: string,
+  label?: string,
+  type?: string,
+  default?: boolean,
+  drm?: DRM,
+}
+
+interface AdSchedule {
+  [adbreak: string]: {
+    offset?: string|number,
+    tag?: string,
+  }
+}
+
+interface Skin {
+  name?: string,
+  active?: string,
+  inactive?: string,
+  background?: string,
+  url?: string,
+}
+
+interface PlaylistItem {
+  file: string,
+  withCredentials?: boolean,
+  title?: string,
+  description?: string,
+  image?: string,
+  mediaid?: string,
+  recommendations?: string,
+  minDvrWindow?: number,
+  stereomode?: string,
+  sources?: Array<Source>,
+  tracks?: Array<Track>,
+  adschedule?: AdSchedule,
+}
+
+interface Captions {
+  color?: string,
+  fontSize?: number,
+  fontFamily?: string,
+  fontOpacity?: number,
+  backgroundColor?: string,
+  backgroundOpacity?: number,
+  edgeStyle?: string,
+  windowColor?: string,
+  windowOpacity?: string,
+}
+
+interface RTMP {
+  bufferlength?: number,
+  subscribe?: boolean,
+  securetoken?: string,
+}
+
+interface Logo {
+  file?: string,
+  hide?: boolean,
+  link?: string,
+  margin?: number,
+  position?: LogoPosition,
+}
+
+interface Sharing {
+  link?: string,
+  code?: string,
+  heading?: string,
+  sites?: Array<SharingSites>,
+}
+
+interface GoogleAnalytics {
+  label?: string,
+}
+
+interface Related {
+  file: string,
+  oncomplete?: RelatedComplete,
+  heading?: string,
+  autoplaytimer?: string,
+  autoplaymessage?: string,
+}
+
+interface CompanionDiv {
+  height?: number,
+  width?: number,
+  id?: string,
+}
+
+interface Advertising {
+  client: AdvertisingClient,
+  tag?: string,
+  admessage?: string,
+  skipoffset?: string,
+  cuetext?: string,
+  skipmessage?: string,
+  skiptext?: string,
+  vpaidmode?: string,
+  schedule?: string|AdSchedule,
+  companiondiv?: CompanionDiv,
+  autoplayadsmuted?: boolean,
+  enablepreloading?: boolean,
+  vpaidcontrols?: boolean,
+  forceNonLinearFullSlot?: boolean,
+  setLocale?: string,
+  creativeTimeout?: string,
+  requestTimeout?: string,
+}
+
+interface DRMHeader {
+  name: string,
+  value: string,
+}
+
+interface PlayReady {
+  url: string,
+  headers?: Array<DRMHeader>,
+}
+
+interface WideVine {
+  url: string,
+  serverCertificateUrl: string,
+  headers?: Array<DRMHeader>,
+}
+
+interface FairPlay {
+  certificateUrl: string,
+  processSpcUrl: () => string|string,
+  extractContentId?: (initData: string) => string,
+  licenseRequestHeaders?: Array<DRMHeader>,
+  licenseResponseType?: string,
+  extractKey?: (ckc: string) => string|Promise<string>,
+}
+
+interface ClearKey {
+  key: string,
+  keyId: string,
+}
+
+interface DRM {
+  playready?: PlayReady,
+  widevine?: WideVine,
+  fairplay?: FairPlay,
+  clearkey?: ClearKey,
+}
+
+interface Localization {
+  nextUp?: string,
+  playlist?: string,
+  related?: string,
+}
+
+declare interface JWPlayerConfig {
+  file: string,
+  image?: string,
+  title?: string,
+  description?: string,
+  mediaid?: string,
+  mute?: boolean,
+  autostart?: string,
+  nextupoffset?: number,
+  repeat?: boolean,
+  abouttext?: string,
+  aboutlink?: string,
+  playbackRateControls?: boolean|Array<number>,
+  controls?: boolean,
+  localization?: Localization,
+  aspectratio?: string,
+  height?: number,
+  width?: number|string,
+  displaytitle?: boolean,
+  displaydescription?: boolean,
+  stretching: Stretching,
+  timeSliderAbove?: boolean,
+  nextUpDisplay?: boolean,
+  qualityLabels?: Array<{ [quality: string]: string }>,
+  primary?: Primary,
+  flashplayer?: string,
+  base?: string,
+  preload?: Preload,
+  playlist?: Array<PlaylistItem>,
+  skin?: Skin,
+  captions?: Captions,
+  rtmp?: RTMP,
+  logo?: Logo,
+  sharing?: Sharing,
+  ga?: GoogleAnalytics,
+  related?: Related,
+  drm?: DRM,
+}
+
+interface QualityLevels {
+  bitrate: number,
+  height: number,
+  width: number,
+  label: string,
+}
+
+interface VisualQuality {
+  mode: string,
+  level: QualityReason,
+  reason: string,
+}
+
+interface AudioTrack {
+  autoselect: boolean,
+  defaulttrack: boolean,
+  language: string,
+  name: string,
+}
+
+interface SafeRegion {
+  x: number,
+  y: number,
+  width: number,
+  height: number,
+}
+
+declare interface JWPlayerInstance {
+  on(event: Event, callback: (args: any) => void): void,
+  once(event: Event, callback: (args: any) => void): void,
+  off(event: Event, callback: (args: any) => void): void,
+  trigger(event: Event, callback: (args: any) => void): void,
+  getViewable(): number,
+  next(): void,
+  getPlaylist(): Array<PlaylistItem>,
+  getPlaylistItem(index: number): PlaylistItem,
+  getPlaylistIndex(): number,
+  load(playlist: Array<PlaylistItem>): void,
+  playlistitem(index: number): void,
+  getBuffer(): number,
+  play(state: boolean): void,
+  pause(state: boolean): void,
+  stop(): void,
+  getState(): PlayState,
+  getPlaybackRate(): number,
+  getPosition(): number,
+  getDuration(): number,
+  seek(position: number): void,
+  getMute(): boolean,
+  getVolume(): number,
+  setMute(state: boolean): void,
+  setVolume(state: number): void,
+  getFullscreen(): boolean,
+  getHeight(): number,
+  getWidth(): number,
+  resize(width: number, height: number): void,
+  getQualityLevels(): Array<QualityLevels>,
+  getCurrentQuality(): number,
+  getVisualQuality(): VisualQuality,
+  setCurrentQuality(index: number): void,
+  getAudioTracks(): Array<AudioTrack>,
+  getCurrentAudioTrack(): AudioTrack,
+  setCurrentAudioTrack(index: number): void,
+  setCaptions(styles: Captions): void,
+  getCaptionList(): Array<{ id: string, label: string }>,
+  getCurrentCaptions(): number,
+  setCurrentCaptions(index: number): void,
+  getControls(): boolean,
+  getSafeRegion(): SafeRegion,
+  addButton(o: { icon: string, label: string, handler: string, id: string }): void,
+  removeButton(id: string): void,
+  setControls(state: boolean): void,
+  playAd(tag: string): void,
+  getPlugin(plugin: string): any,
+}
