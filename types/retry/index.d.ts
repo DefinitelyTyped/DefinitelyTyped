3a514e8e36a8e7af70d4a01c4a330e395d9776e7
Stan Goldmann <goldmann@traum-ferienwohnungen.de>
2017-07-14 11:06:53 +0200

    Address requested code changes
    * Remove options file and include options as well as  JSDocs inline
    * Hint retry to `Error` classes
    * Remove optional flag of `current` in the callback parameter
    * Fix typos

--- types/retry/index.d.ts
+++ types/retry/index.d.ts
@@ -4,94 +4,140 @@
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
 // TypeScript Version: 2.3
 
-import { Options } from './options'
-export * from './options'
-
 export interface RetryOperation {
-    /**
-     * Defines the function that is to be retried and executes it for the first time right away.
-     *
-     * @param {function} callback The function that is to be retried
-     * @param {current} [callback.current] Number of attempts callback has been executed so far.
-     * @param {Options.Attempt} [options]
-     *
-     *
-     * @return {void}
-     */
-    attempt(callback: (current?: number) => void, options?: Options.Attempt): void
+  /**
+   * Defines the function that is to be retried and executes it for the first time right away.
+   *
+   * @param {function} callback The function that is to be retried
+   * @param {number} callback.current Number of attempts callback has been executed so far.
+   * @param {number} [options.timeout] A timeout in milliseconds.
+   * @param {function} [options.callback] Callback to execute when the operation takes longer than the timeout.
+   *
+   * @return {void}
+   */
+  attempt(callback: (current: number) => void, options?: {
+    timeout?: number
+    callback?: () => void
+  }): void
 
-    /**
-     * Returns false when no error value is given, or the maximum amount of retries has been reached.
-     * Otherwise it returns true, and retries the operation after the timeout for the current attempt number.
-     *
-     * @param {(Error|any)} [err]
-     *
-     * @return {boolean}
-     */
-    retry(err?: Error | any): boolean
+  /**
+   * Returns false when no error value is given, or the maximum amount of retries has been reached.
+   * Otherwise it returns true, and retries the operation after the timeout for the current attempt number.
+   *
+   * @param {Error} [err]
+   *
+   * @return {boolean}
+   */
+  retry(err?: Error): boolean
 
-    /**
-     * The number of attempts it took to call the retrying function before it was successful.
-     *
-     * @return {number}
-     */
-    attempts(): number
+  /**
+   * The number of attempts it took to call the retrying function before it was successful.
+   *
+   * @return {number}
+   */
+  attempts(): number
 
-    /**
-     * A reference to the error object that occured most frequently.
-     * Errors are compared using the error.message property.
-     *
-     * @return {(Error|null)} If no errors occured so far the value will be null.
-     */
-    mainError(): Error | null
+  /**
+   * A reference to the error object that occured most frequently.
+   * Errors are compared using the error.message property.
+   *
+   * @return {(Error|null)} If no errors occured so far the value will be null.
+   */
+  mainError(): Error | null
 
-    /**
-     * Returns an array of all errors that have been passed to RetryOperation.retry() so far.
-     *
-     * @return {Error[]}
-     */
-    errors(): Error[]
+  /**
+   * Returns an array of all errors that have been passed to RetryOperation.retry() so far.
+   *
+   * @return {Error[]}
+   */
+  errors(): Error[]
 
-    /**
-     * Stopts the operation being retried. Useful for aborting the operation on a fatal error etc.
-     */
-    stop(): void
+  /**
+   * Stops the operation being retried. Useful for aborting the operation on a fatal error etc.
+   */
+  stop(): void
 }
 /**
  * Create a new RetryOperation object.
  *
- * @param {Options.Operation} [options]
+ * @param {number} [options.retries=10] The maximum amount of times to retry the operation.
+ * @param {number} [options.factor=2] The exponential factor to use.
+ * @param {number} [options.minTimeout=1000] The number of milliseconds before starting the first retry.
+ * @param {number} [options.maxTimeout=Infinity] The maximum number of milliseconds between two retries.
+ * @param {boolean} [options.randomize=false] Randomizes the timeouts by multiplying a factor between 1-2.
+ * @param {boolean} [options.forever=false] Wether to retry forever.
+ * @param {boolean} [options.unref=false] Wether to unref the setTimeout's.
  *
  * @return {RetryOperation}
  */
-export function operation(options?: Options.Operation): RetryOperation
+export function operation(options?: {
+  retries?: number
+  factor?: number
+  minTimeout?: number
+  maxTimeout?: number
+  randomize?: boolean
+  forever?: boolean
+  unref?: boolean
+}): RetryOperation
 
 /**
  * Get an array with timeouts and their return values in milliseconds.
  *
- * @param {Options.Timeouts} [options]
+ * @param {number} [options.retries=10] The maximum amount of times to retry the operation.
+ * @param {number} [options.factor=2] The exponential factor to use.
+ * @param {number} [options.minTimeout=1000] The number of milliseconds before starting the first retry.
+ * @param {number} [options.maxTimeout=Infinity] The maximum number of milliseconds between two retries.
+ * @param {boolean} [options.randomize=false] Randomizes the timeouts by multiplying a factor between 1-2.
  *
  * @return {number[]}
  */
-export function timeouts(options?: Options.Timeouts): number[]
+export function timeouts(options?: {
+  retries?: number
+  factor?: number
+  minTimeout?: number
+  maxTimeout?: number
+  randomize?: boolean
+}): number[]
 
 /**
  * Create a new timeout (in milliseconds) based on the given parameters.
  *
  * @param {number} attempt  Representing for which retry the timeout should be calculated.
- * @param {Options.Timeout} [options]
+ * @param {number} [options.factor=2] The exponential factor to use.
+ * @param {number} [options.minTimeout=1000] The number of milliseconds before starting the first retry.
+ * @param {number} [options.maxTimeout=Infinity] The maximum number of milliseconds between two retries.
+ * @param {boolean} [options.randomize=false] Randomizes the timeouts by multiplying a factor between 1-2.
  *
  * @return {number} timeout
  */
-export function createTimeout(attempt: number, options?: Options.Timeout): number
+export function createTimeout(attempt: number, options?: {
+  factor?: number
+  minTimeout?: number
+  maxTimeout?: number
+  randomize?: boolean
+}): number
 
 /**
  * Wrap all functions of the object with retry.
  *
  * @param {object} object The object to be wrapped
- * @param {Options.Operation} [options]
+ * @param {number} [options.retries=10] The maximum amount of times to retry the operation.
+ * @param {number} [options.factor=2] The exponential factor to use.
+ * @param {number} [options.minTimeout=1000] The number of milliseconds before starting the first retry.
+ * @param {number} [options.maxTimeout=Infinity] The maximum number of milliseconds between two retries.
+ * @param {boolean} [options.randomize=false] Randomizes the timeouts by multiplying a factor between 1-2.
+ * @param {boolean} [options.forever=false] Wether to retry forever.
+ * @param {boolean} [options.unref=false] Wether to unref the setTimeout's.
  * @param {string[]} [methods] Methods which need to be wrapped
  *
  * @return {void}
  */
-export function wrap(object: object, options?: Options.Operation, methods?: string[]): void
+export function wrap(object: object, options?: {
+  retries?: number
+  factor?: number
+  minTimeout?: number
+  maxTimeout?: number
+  randomize?: boolean
+  forever?: boolean
+  unref?: boolean // tslint:disable-next-line:align
+}, methods?: string[]): void
