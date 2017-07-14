d954b4f01220baff90c9b87f6ad8857b7b3fedc4
Karol Janyst <lapkom@gmail.com>
2017-07-14 10:03:59 +0900

    Improve types for handlers

--- types/react-dropzone/react-dropzone-tests.tsx
+++ types/react-dropzone/react-dropzone-tests.tsx
@@ -1,5 +1,7 @@
 import * as React from "react";
-import * as Dropzone from "react-dropzone";
+import { DragEvent, SyntheticEvent} from "react";
+import Dropzone = require("react-dropzone");
+import { ImageFile } from "react-dropzone";
 
 class Test extends React.Component {
 
@@ -9,41 +11,43 @@ class Test extends React.Component {
     this.dz.open();
   }
 
-  handleDropFile = (files: File[]) => {};
+  handleDropFile = (files: ImageFile[], e: DragEvent<HTMLDivElement>) => {};
 
-  handleDropFiles = (accepted: File[], rejected?: File[]) => {};
+  handleDropFiles = (accepted: File[], rejected: File[], event: DragEvent<HTMLDivElement>) => {};
+
+  handleDefault = (e: SyntheticEvent<HTMLDivElement>) => {}
+
+  handleFileDialog = () => {}
 
   render() {
     return (
       <div>
         <Dropzone
-          ref={(node) => { this.dz = node } }
-          onClick={ (e: any) => { e.preventDefault(); } }
-          onDrop={ this.handleDropFiles }
-          onDropAccepted={ this.handleDropFile }
-          onDropRejected={ this.handleDropFile }
-          onDragStart={ (e: any) => { e.preventDefault(); } }
-          onDragEnter={ (e: any) => { e.preventDefault(); } }
-          onDragLeave={ (e: any) => { e.preventDefault(); } }
-          onFileDialogCancel={ () => undefined }
+          ref={(node) => { this.dz = node }}
+          onClick={this.handleDefault}
+          onDrop={this.handleDropFiles}
+          onDropAccepted={this.handleDropFile }
+          onDropRejected={this.handleDropFile }
+          onDragStart={this.handleDefault}
+          onDragEnter={this.handleDefault}
+          onDragLeave={this.handleDefault}
+          onFileDialogCancel={this.handleFileDialog}
           style={{ borderStyle: "dashed" }}
           activeStyle={{ borderStyle: "dotted" }}
           rejectStyle={{ borderStyle: "dotted" }}
           className="regular"
           activeClassName="active"
           rejectClassName="reject"
-          minSize={ 2000 }
-          maxSize={ Infinity }
+          minSize={2000}
+          maxSize={Infinity}
           disablePreview
           disableClick
-          multiple={ false }
+          multiple={false}
           accept="*.png"
           name="dropzone"
-          inputProps={ { id : "dropzone" } }
+          inputProps={{ id : "dropzone" }}
         />
       </div>
-
-
     );
   }
 }
