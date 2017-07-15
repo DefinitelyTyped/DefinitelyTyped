1601fe14ac90abbba4e90760cfaa36e063e48dcb
Ben Windsor <benwindsor@gmail.com>
2017-07-15 23:24:57 +0100

    Update SES test to match new defintion

--- types/nodemailer-ses-transport/index.d.ts
+++ types/nodemailer-ses-transport/index.d.ts
@@ -1,4 +1,4 @@
-// Type definitions for nodemailer-ses-transport 1.4
+// Type definitions for nodemailer-ses-transport 3.1.5
 // Project: https://github.com/andris9/nodemailer-ses-transport
 // Definitions by: Seth Westphal <https://github.com/westy92/>
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
@@ -8,10 +8,10 @@ import * as nodemailer from "nodemailer";
 
 declare namespace sesTransport {
 	interface SesOptions {
-		SES?: AWS.SES;
+		SES: AWS.SES;
 		component?: string;
-		maxConnections?: string;
-		sendingRate?: string;
+		maxConnections?: number;
+		sendingRate?: number;
 	}
 }
 
--- types/nodemailer-ses-transport/nodemailer-ses-transport-tests.ts
+++ types/nodemailer-ses-transport/nodemailer-ses-transport-tests.ts
@@ -3,9 +3,10 @@ import * as nodemailer from "nodemailer";
 import * as sesTransport from 'nodemailer-ses-transport';
 
 const opts: sesTransport.SesOptions = {
-  ses: new AWS.SES(),
-  rateLimit: 5,
-  maxConnections: 3,
+  SES: new AWS.SES(),
+  component: "string",
+  sendingRate: 5,
+  maxConnections: 3
 };
 
 const transport: nodemailer.Transport = sesTransport(opts);
