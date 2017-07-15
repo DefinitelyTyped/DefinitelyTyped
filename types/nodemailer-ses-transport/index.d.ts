bbc66e9d3fe1b9f8e7b524a00ece05a3b3f347f3
Ben Windsor <benwindsor@gmail.com>
2017-07-15 23:41:15 +0100

    Change version to only have x.x

--- types/nodemailer-ses-transport/index.d.ts
+++ types/nodemailer-ses-transport/index.d.ts
@@ -1,4 +1,4 @@
-// Type definitions for nodemailer-ses-transport 3.1.5
+// Type definitions for nodemailer-ses-transport 3.1
 // Project: https://github.com/andris9/nodemailer-ses-transport
 // Definitions by: Seth Westphal <https://github.com/westy92/>
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
