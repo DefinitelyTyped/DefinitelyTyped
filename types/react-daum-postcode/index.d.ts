de7a88f21c3163747ede22143e4eaa8836aff808
Sa-ryong Kang <justfaceit@gmail.com>
2017-07-16 18:30:13 +0900

    initial update for react-daum-postcode

---
+++ types/react-daum-postcode/index.d.ts
@@ -0,0 +1,23 @@
+// Type definitions for react-daum-postcode 1.2.1
+// Project: https://github.com/kimminsik-bernard/react-daum-postcode
+// Definitions by: Sa-ryong Kang <https://github.com/Sa-ryong>
+// Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
+// TypeScript Version: 2.4
+
+import { Component } from 'react';
+
+export interface DaumPostcodeProps {
+  onComplete?: any;
+  width?: any;
+  height?: any;
+  autoClose?: any;
+  autoResize?: any;
+  animation?: any;
+  style?: any;
+  defaultQuery?: any;
+  theme?: any;
+}
+
+export default class DaumPostcode extends Component<DaumPostcodeProps, any> {
+  render(): JSX.Element;
+}
