756a462d6ea73de703f494a719ac50f22007170b
dyst5422 <dylan.stewart@colorado.edu>
2017-07-17 09:53:22 -0700

    Fixed removed definition author

--- types/point-in-polygon/index.d.ts
+++ types/point-in-polygon/index.d.ts
@@ -1,6 +1,7 @@
 // Type definitions for point-in-polygon
 // Project: https://github.com/substack/point-in-polygon
 // Definitions by: dyst5422 <https://github.com/dyst5422>
+//                 kogai <https://github.com/kogai>
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
 
 declare module 'point-in-polygon' {
