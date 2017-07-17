20cdbc7d7db98e6aad915da7ac080b0fb0ba0939
Nick Graham <nickbgraham83@gmail.com>
2017-07-17 17:05:12 -0500

    Adds 'any' as state interface

--- types/react-router-redux/index.d.ts
+++ types/react-router-redux/index.d.ts
@@ -23,7 +23,7 @@ export interface ConnectedRouterProps<State> {
     store?: Store<State>;
     history?: History;
 }
-export class ConnectedRouter<State> extends React.Component<ConnectedRouterProps<State>> {}
+export class ConnectedRouter<State> extends React.Component<ConnectedRouterProps<State>, any> {}
 
 export const LOCATION_CHANGE: string;
 
