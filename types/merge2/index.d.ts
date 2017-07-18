d4f6232b4ae7adcdb44afd8962ecaec236a40620
smac89 <smac89@users.noreply.github.com>
2017-07-18 12:27:04 -0600

    Fixed indentation to use space rather than mixed tab and space

--- types/merge2/index.d.ts
+++ types/merge2/index.d.ts
@@ -1,7 +1,7 @@
 // Type definitions for merge2 v1.1.0
 // Project: https://github.com/teambition/merge2
 // Definitions by: Tanguy Krotoff <https://github.com/tkrotoff>
-//				   Chigozirim C. <https://github.com/smac89>
+//                 Chigozirim C. <https://github.com/smac89>
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
 
 /// <reference types="node" />
@@ -11,24 +11,24 @@ import {DuplexOptions} from 'stream';
 type StreamType = NodeJS.ReadableStream | IMerge2Stream;
 
 interface IMerge2Stream extends NodeJS.ReadWriteStream {
-	/**
-	 * @brief      Add more streams to an existing merged stream
-	 *
-	 * @param      args  streams to add
-	 *
-	 * @return     The merged stream
-	 */
+    /**
+     * @brief      Add more streams to an existing merged stream
+     *
+     * @param      args  streams to add
+     *
+     * @return     The merged stream
+     */
     add(...args: Array<StreamType | Array<StreamType>>): IMerge2Stream;
 }
 
 /**
  * @brief      This function takes an arbitrary number of streams, but the duplexoption
- * 			   has to be the last parameter of the function.
- * 			   
+ *             has to be the last parameter of the function.
+ *             
  * @param      args  The arguments
  *
  * @return     A merged duplex stream
- */	
+ */ 
 declare function IMerge2 (...args: Array<StreamType | Array<StreamType> | DuplexOptions>): IMerge2Stream;
 
 declare namespace IMerge2 {}
