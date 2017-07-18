8f5f2eb8eb6208d4d284f43a2960e21378f89b0c
smac89 <smac89@users.noreply.github.com>
2017-07-18 00:53:41 -0600

    Updated merge streams to simply use readablestream rather than readwritestream because readablestream is a much better super type of the type of streams supported

--- types/merge2/index.d.ts
+++ types/merge2/index.d.ts
@@ -1,23 +1,21 @@
-// Type definitions for merge2 v0.3.6
+// Type definitions for merge2 v1.1.0
 // Project: https://github.com/teambition/merge2
 // Definitions by: Tanguy Krotoff <https://github.com/tkrotoff>
+//				   Chigozirim C. <https://github.com/smac89>
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
 
 /// <reference types="node" />
 
+import {DuplexOptions} from 'stream';
 
-interface IOptions {
-    end?: boolean;
-    objectMode?: boolean;
-}
+type StreamType = NodeJS.ReadableStream | IMerge2Stream;
 
 interface IMerge2Stream extends NodeJS.ReadWriteStream {
-    add(...args: Array<NodeJS.ReadWriteStream | IMerge2Stream | Array<NodeJS.ReadWriteStream | IMerge2Stream | IOptions>>): IMerge2Stream;
+    add(...args: Array<StreamType | Array<StreamType>>): IMerge2Stream;
 }
 
-interface IMerge2 {
-    (...args: Array<NodeJS.ReadWriteStream | IMerge2Stream | Array<NodeJS.ReadWriteStream | IMerge2Stream> | IOptions>): IMerge2Stream;
-}
+declare function IMerge2 (...args: Array<StreamType | Array<StreamType> | DuplexOptions>): IMerge2Stream;
 
-declare var _tmp: IMerge2;
-export = _tmp;
+declare namespace IMerge2 {}
+
+export = IMerge2;
