ed7bf7126e9bb90bceeaa36324aa8d01692b89dc
Neek Sandhu <neek.sandhu@outlook.com>
2017-07-17 23:03:38 -0700

    Fulfill compliance with tslint.json

--- types/css-modules/css-modules-tests.ts
+++ types/css-modules/css-modules-tests.ts
@@ -1,21 +1,21 @@
-import styles from './main.css'
-import * as classNames from 'classnames'
+import styles from './main.sass';
+import * as classNames from 'classnames';
 
 class App {
-    private theme: string
+    private theme: string;
     constructor(theme: string) {
-        this.theme = theme
+        this.theme = theme;
     }
 
     render() {
         // as a style tag (using webpack's css-loader)
-        const tpl = `<div style="${styles.toString()}"></div>`
+        const tpl = `<div style="${styles.toString()}"></div>`;
         // or as scoped unique classes, also latest typescript versions allow prop access using dot like styles.darkUI instead of styles['darkUI']
         return `
-            <div class="${classNames((this.theme === 'dark')? 
-                styles['darkUI'] : 
-                styles['lightUI'].toString())}">
+            <div class="${classNames((this.theme === 'dark') ?
+                styles.darkUI :
+                styles.lightUI.toString())}">
             </div>
-        `
+        `;
     }
 }
