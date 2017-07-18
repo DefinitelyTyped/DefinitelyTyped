c95f362fff5af1a00715472b65d35a460ae58c5a
Dimitri Benin <dimitri.benin@it-designers.de>
2017-07-18 02:16:32 +0200

    [http-proxy-middleware] fix incomplete/incorrect typings, fix lint issues, add missing tests

--- types/http-proxy-middleware/http-proxy-middleware-tests.ts
+++ types/http-proxy-middleware/http-proxy-middleware-tests.ts
@@ -1,7 +1,7 @@
-// Derived from https://github.com/chimurai/http-proxy-middleware#example
-
-import * as express from 'express';
-import * as proxy from 'http-proxy-middleware';
+import express = require('express');
+import proxy = require('http-proxy-middleware');
+import { IncomingMessage } from "http";
+import * as Winston from "winston";
 
 const options = {
   target: 'http://www.example.org',
@@ -16,8 +16,112 @@ const options = {
   }
 };
 
-const myProxy = proxy(options);
+// create the proxy (without context)
+const exampleProxy = proxy(options);
 
+// mount `exampleProxy` in web server
 const app = express();
-app.use('/api', myProxy);
+app.use('/api', exampleProxy);
 app.listen(3000);
+
+proxy(options);
+proxy('/', options);
+proxy('/api', options);
+proxy(['/api', '/ajax', '/someotherpath'], options);
+
+const filter = (pathname: string, req: IncomingMessage) => {
+  return !!(pathname.match('^/api') && req.method === 'GET');
+};
+const apiProxy = proxy(filter, {target: 'http://www.example.org'});
+
+proxy({pathRewrite: {'^/old/api': '/new/api'}});
+proxy({
+  pathRewrite(path, req) {
+    return path.replace('/api', '/base/api') || '';
+  }
+});
+
+proxy({
+  router: {
+    'integration.localhost:3000': 'http://localhost:8001',
+    'staging.localhost:3000': 'http://localhost:8002',
+    'localhost:3000/api': 'http://localhost:8003',
+    '/rest': 'http://localhost:8004',
+  }
+});
+proxy({
+  router(req) {
+    return 'http://localhost:8004';
+  }
+});
+
+proxy({logLevel: 'debug'});
+
+proxy({
+  logProvider(provider) {
+    return Winston;
+  }
+});
+proxy({
+  logProvider(provider) {
+    const logger = new (Winston.Logger)();
+
+    return {
+      log: logger.log,
+      debug: logger.debug,
+      info: logger.info,
+      warn: logger.warn,
+      error: logger.error
+    };
+  }
+});
+
+proxy({
+  onError(err, req, res) {
+    res.writeHead(500, {
+      'Content-Type': 'text/plain'
+    });
+    res.end('Something went wrong. And we are reporting a custom error message.');
+  }
+});
+proxy({
+  onProxyRes(proxyRes, req, res) {
+    proxyRes.headers['x-added'] = 'foobar';
+    delete proxyRes.headers['x-removed'];
+  }
+});
+proxy({
+  onProxyReq(proxyReq, req, res) {
+    proxyReq.setHeader('x-added', 'foobar');
+  }
+});
+proxy({
+  onProxyReqWs(proxyReq, req, socket, options, head) {
+    proxyReq.setHeader('X-Special-Proxy-Header', 'foobar');
+  }
+});
+proxy({
+  onOpen(proxySocket) {
+    proxySocket.on('data', (data: Buffer) => {
+    });
+  }
+});
+proxy({
+  onClose(res, socket, head) {
+    console.log('Client disconnected');
+  }
+});
+
+proxy({cookieDomainRewrite: false});
+proxy({cookieDomainRewrite: "new.domain"});
+proxy({
+  cookieDomainRewrite: {
+    "unchanged.domain": "unchanged.domain",
+    "old.domain": "new.domain",
+    "*": ""
+  }
+});
+
+proxy({
+  headers: {host: 'www.example.org'}
+});
