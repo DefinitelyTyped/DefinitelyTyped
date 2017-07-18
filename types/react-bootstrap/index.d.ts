ea849edb9117f439b3b20129afa0edd16f9ad6d3
Vito <vito@vitosamson.com>
2017-07-18 19:32:28 -0400

    add tslint.json, fix some linting errors

--- types/react-bootstrap/index.d.ts
+++ types/react-bootstrap/index.d.ts
@@ -1,6 +1,13 @@
-// Type definitions for react-bootstrap
+// Type definitions for react-bootstrap 0.31
 // Project: https://github.com/react-bootstrap/react-bootstrap
-// Definitions by: Walker Burgin <https://github.com/walkerburgin>, Vincent Siao <https://github.com/vsiao>, Danilo Barros <https://github.com/danilojrr>, Batbold Gansukh <https://github.com/Batbold-Gansukh>, Raymond May Jr. <https://github.com/octatone>, Cheng Sieu Ly <https://github.com/chengsieuly>, Kat Busch <https://github.com/katbusch>, Vito Samson <https://github.com/vitosamson>
+// Definitions by: Walker Burgin <https://github.com/walkerburgin>,
+//                 Vincent Siao <https://github.com/vsiao>,
+//                 Danilo Barros <https://github.com/danilojrr>,
+//                 Batbold Gansukh <https://github.com/Batbold-Gansukh>,
+//                 Raymond May Jr. <https://github.com/octatone>,
+//                 Cheng Sieu Ly <https://github.com/chengsieuly>,
+//                 Kat Busch <https://github.com/katbusch>,
+//                 Vito Samson <https://github.com/vitosamson>
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
 // TypeScript Version: 2.3
 
@@ -100,20 +107,20 @@ export type Sizes = 'xs' | 'xsmall' | 'sm' | 'small' | 'medium' | 'lg' | 'large'
 export interface SelectCallback extends React.EventHandler<any> {
   (eventKey: any, e: React.SyntheticEvent<{}>): void;
   /**
-      @deprecated
-      This signature is a hack so can still derive from HTMLProps.
-      It does not reflect the underlying event and should not be used.
-  */
+   * @deprecated
+   * This signature is a hack so can still derive from HTMLProps.
+   * It does not reflect the underlying event and should not be used.
+   */
   (e: React.MouseEvent<{}>): void;
 }
 
 export interface TransitionCallbacks {
-  onEnter?: (node: HTMLElement) => any;
-  onEntered?: (node: HTMLElement) => any;
-  onEntering?: (node: HTMLElement) => any;
-  onExit?: (node: HTMLElement) => any;
-  onExited?: (node: HTMLElement) => any;
-  onExiting?: (node: HTMLElement) => any;
+  onEnter?(node: HTMLElement): any;
+  onEntered?(node: HTMLElement): any;
+  onEntering?(node: HTMLElement): any;
+  onExit?(node: HTMLElement): any;
+  onExited?(node: HTMLElement): any;
+  onExiting?(node: HTMLElement): any;
 }
 
 export {
