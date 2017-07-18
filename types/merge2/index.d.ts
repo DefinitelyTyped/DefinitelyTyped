6a5984cf5fa8ca7a75c9f5f92c7e36bea1949498
smac89 <smac89@users.noreply.github.com>
2017-07-18 01:31:58 -0600

    Added comments to the functions

--- types/merge2/index.d.ts
+++ types/merge2/index.d.ts
@@ -11,9 +11,24 @@ import {DuplexOptions} from 'stream';
 type StreamType = NodeJS.ReadableStream | IMerge2Stream;
 
 interface IMerge2Stream extends NodeJS.ReadWriteStream {
+	/**
+	 * @brief      Add more streams to an existing merged stream
+	 *
+	 * @param      args  streams to add
+	 *
+	 * @return     The merged stream
+	 */
     add(...args: Array<StreamType | Array<StreamType>>): IMerge2Stream;
 }
 
+/**
+ * @brief      This function takes an arbitrary number of streams, but the duplexoption
+ * 			   has to be the last parameter of the function.
+ * 			   
+ * @param      args  The arguments
+ *
+ * @return     A merged duplex stream
+ */	
 declare function IMerge2 (...args: Array<StreamType | Array<StreamType> | DuplexOptions>): IMerge2Stream;
 
 declare namespace IMerge2 {}
