8106a35557b674adf64af4a497f822659e24b8a3
Cayle Sharrock <cayle@coinbase.com>
2017-07-19 09:48:11 +0200

    Make bintrees TS 2.4 compliant

--- types/bintrees/bintrees-tests.ts
+++ types/bintrees/bintrees-tests.ts
@@ -4,7 +4,6 @@ let treeA = new RBTree((a:number, b:number) => a - b);
 
 treeA.insert(5);
 treeA.insert(3);
-treeA.remove(3);
 
 let treeB = new BinTree((a:string, b:string) => a.length - b.length);
 
@@ -12,3 +11,6 @@ treeB.insert('hi');
 treeB.insert('there');
 treeB.insert('how');
 treeB.insert('are');
+
+treeB.remove('how');
+
