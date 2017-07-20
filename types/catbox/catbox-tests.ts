f0c4d5af7b147aecd77bd154110604b27f457828
Silas Rech <silas@panascais.io>
2017-07-20 14:15:58 +0200

    Fix tests

--- types/catbox/catbox-tests.ts
+++ types/catbox/catbox-tests.ts
@@ -1,8 +1,19 @@
 import Catbox = require("catbox");
-import Memory = require("catbox-memory");
 
-const client = new Catbox.Client(Memory);
+const Memory = (): void => {};
+
+const client = new Catbox.Client(Memory as any, { partition: 'cache' });
 
 const cache = new Catbox.Policy({
     expiresIn: 5000,
 }, client, 'cache');
+
+cache.set('foo', 'bar', 5000, () => {});
+
+cache.get('foo', () => {});
+
+cache.drop('foo', () => {});
+
+cache.isReady();
+
+cache.stats();
