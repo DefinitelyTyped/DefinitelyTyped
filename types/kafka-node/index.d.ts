2153a08415a76665029d8ea6b60d37bed89384cc
bkim54 <bill.kim1007@gmail.com>
2017-07-20 12:43:11 -0700

    fixed typos

--- types/kafka-node/index.d.ts
+++ types/kafka-node/index.d.ts
@@ -122,7 +122,7 @@ export interface RetryOptions {
     retries: number;
     factor: number;
     minTimeout: number;
-    maxtimeout: number;
+    maxTimeout: number;
     randomize: boolean;
 }
 
