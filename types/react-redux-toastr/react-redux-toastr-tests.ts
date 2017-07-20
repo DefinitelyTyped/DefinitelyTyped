3b95a7158075fb8795e1de774740610ba5d99a64
Mika Kuitunen <mika.kuitunen@hotmail.com>
2017-07-20 19:59:37 +0300

    [react-redux-toastr] Rewrite types for package v7.0.0 and TypeScript 2.4 (#18024)
    * react-redux-toastr: unify code style
    
    * react-redux-toastr: Change React.Component types to use state and props
    
    * react-redux-toastr: change to named imports
    
    * react-redux-toastr: remove module declaration
    
    * react-redux-toastr: add union types for known strings
    
    * react-redux-toastr: rename ToastrConfirmOptions to ConfirmToastrOptions for unified style, refine properties
    
    * react-redux-toastr: Add interfaces for all of the Toastr option objects
    
    * react-redux-toastr: Refactor some variables to alphabetical order
    
    * react-redux-toastr: Add interfaces to toastrs in reducer state, add toastr action payload, toastr reducer state
    
    * react-redux-toastr: Rename ToastOptions to ToastrOptions to unify style
    
    * react-redux-toastr: Rename ToastrOptions to ReduxToastrProps and rewrite properties
    
    * react-redux-toastr: Slight refactor of ReduxToastrProps
    
    * react-redux-toastr: Remove confirmOptions interface
    
    * react-redux-toastr: Remove duplicate ConfirmToastrOptions interface
    
    * react-redux-toastr: Rewrite ToastrEmitter, modify tests to comply with version 7.0.0
    
    * react-redux-toastr: rewrite action creators, start using ToastrState
    
    * react-redux-toastr: Slight refactor, alphabetize stuff
    
    * react-redux-toastr: remove unused Component state and props
    
    * react-redux-toastr: fix TS version
    
    * Fix version number for TypeScript

--- types/react-redux-toastr/react-redux-toastr-tests.ts
+++ types/react-redux-toastr/react-redux-toastr-tests.ts
@@ -17,7 +17,7 @@ function test() {
     toastr.clean();
     toastr.confirm("Test", { onOk: callback, onCancel: callback });
     toastr.error("Error", "Error message");
-    toastr.info("Info", "Info test", { timeOut: 1000, removeOnHover: true, removeOnClick: true, onShowComplete: callback });
+    toastr.info("Info", "Info test", { timeOut: 1000, removeOnHover: true, onShowComplete: callback });
     toastr.success("Test", "Test message", { component: new React.Component() });
 }
 
