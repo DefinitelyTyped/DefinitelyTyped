d9ee8bb6c7a7cccdd0678ccaf7a161c2f4dad2dc
Dimitri Benin <dimitri.benin@it-designers.de>
2017-07-22 23:51:23 +0200

    [get-stream] add myself to contributors list

--- types/get-stream/index.d.ts
+++ types/get-stream/index.d.ts
@@ -1,6 +1,7 @@
 // Type definitions for get-stream 3.0
 // Project: https://github.com/sindresorhus/get-stream#readme
 // Definitions by: Douglas Duteil <https://github.com/douglasduteil>
+//                 BendingBender <https://github.com/BendingBender>
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
 
 /// <reference types="node" />
