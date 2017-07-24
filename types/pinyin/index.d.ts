514da3d1589ec51ba75d2589db242fe86613c96d
Anjun Wang <test@example.com>
2017-07-24 16:30:52 +0800

    add pinyin 2.8.3

---
+++ types/pinyin/index.d.ts
@@ -0,0 +1,24 @@
+// Type definitions for pinyin 2.8.3
+// Project: https://github.com/hotoo/pinyin
+// Definitions by: AnJun Wang <https://github.com/wanganjun>
+// Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
+
+export = pinyin;
+
+declare function pinyin(words: any, options?: Options): Array<Array<string>>;
+
+declare namespace pinyin {
+    export function compare(a: any, b: any): -1 | 0 | 1;
+    export const STYLE_NORMAL: number;
+    export const STYLE_TONE: number;
+    export const STYLE_TONE2: number;
+    export const STYLE_TO3NE: number;
+    export const STYLE_INITIALS: number;
+    export const STYLE_FIRST_LETTER: number;
+}
+
+interface Options {
+    style?: number;
+    segment?: boolean;
+    heteronym?: boolean;
+}
---
+++ types/pinyin/pinyin-tests.ts
@@ -0,0 +1,23 @@
+import pinyin = require("pinyin");
+
+pinyin("中心");
+pinyin("中心", {
+    heteronym: true
+});
+pinyin("中心", {
+  heteronym: true,
+  segment: true
+});
+pinyin("中心", {
+  style: pinyin.STYLE_INITIALS,
+  heteronym: true
+});
+
+pinyin.compare('测', '这');
+
+pinyin.STYLE_NORMAL;
+pinyin.STYLE_TONE;
+pinyin.STYLE_TONE2;
+pinyin.STYLE_TO3NE;
+pinyin.STYLE_INITIALS;
+pinyin.STYLE_FIRST_LETTER;
