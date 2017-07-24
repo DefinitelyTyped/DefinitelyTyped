ae7f436be1196e02f3b4fde89a299a54cf728b11
Juan Pablo Garcia <garjuanpablo@gmail.com>
2017-07-24 10:33:14 -0500

    Fix test and changed the file name

--- types/tangocode-react-big-calendar/index.d.ts
+++ types/tangocode-react-big-calendar/index.d.ts
@@ -1,12 +1,12 @@
-// This is a fork of @types/react-big-calendar.
-// We are creating this type in order to be able to work with https://github.com/tangocode/react-big-calendar.
-// Which is a for of https://github.com/intljusticemission/react-big-calendar
-
 // Type definitions for react-big-calendar 0.14.0
 // Project: https://github.com/intljusticemission/react-big-calendar
 // Definitions by: Piotr Witek <http://piotrwitek.github.io>
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
 // TypeScript Version: 2.3
+
+// This is a fork of @types/react-big-calendar.
+// We are creating this type in order to be able to work with https://github.com/tangocode/react-big-calendar.
+// Which is a for of https://github.com/intljusticemission/react-big-calendar
 
 declare module 'tangocode-react-big-calendar' {
     import * as React from 'react';
