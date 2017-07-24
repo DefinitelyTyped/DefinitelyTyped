2da07011ac4c9da24d624342bd144c0d0d1fca14
Andy <anhans@microsoft.com>
2017-07-24 11:58:49 -0700

    point-in-polygon: Lint (#18349)

--- types/point-in-polygon/index.d.ts
+++ types/point-in-polygon/index.d.ts
@@ -1,10 +1,8 @@
-// Type definitions for point-in-polygon
+// Type definitions for point-in-polygon 1.0
 // Project: https://github.com/substack/point-in-polygon
 // Definitions by: dyst5422 <https://github.com/dyst5422>
 //                 kogai <https://github.com/kogai>
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
 
-declare module 'point-in-polygon' {
-    const inside: (point: number[], polygon: number[][]) => boolean;
-    export = inside;
-}
+declare function inside(point: ReadonlyArray<number>, polygon: ReadonlyArray<ReadonlyArray<number>>): boolean;
+export = inside;
