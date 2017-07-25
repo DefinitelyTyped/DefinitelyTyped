3f266bf2ccd2df962dfc16935745f2ef1b785dce
Konstantin Nesterov <wasd171@gmail.com>
2017-07-25 17:29:40 +0200

    Add myself to "definitions by"

--- types/react-virtualized/index.d.ts
+++ types/react-virtualized/index.d.ts
@@ -2,6 +2,7 @@
 // Project: https://github.com/bvaughn/react-virtualized
 // Definitions by: Kalle Ott <https://github.com/kaoDev>
 //                 John Gunther <https://github.com/guntherjh>
+//                 Konstantin Nesterov <https://github.com/wasd171>
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
 // TypeScript Version: 2.3
 
