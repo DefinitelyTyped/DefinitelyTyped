e40eba0ed8dd9ec376ea245165aadef83b7e42b4
Sean Sobey SAHL <seans@sahomeloans.com>
2017-07-25 09:33:49 +0200

    Updated defintions and tests.

--- types/saywhen/index.d.ts
+++ types/saywhen/index.d.ts
@@ -5,29 +5,33 @@
 
 /// <reference types="jasmine" />
 
-declare function when<T extends (...args: any[]) => any>(spy: T & jasmine.Spy): CallHandler<T>;
+type Func = (...args: any[]) => any;
+
+declare function when<T extends Func>(spy: T & jasmine.Spy): CallHandler<T>;
 
 declare namespace when {
-	function captor(val?: any): MatcherProxy;
+	function captor<T>(val?: T): MatcherProxy<T>;
 
 	function noConflict(): void;
+
+	function is<T>(val: T): boolean;
 }
 
-declare class CallHandler<T extends (...args: any[]) => any> {
+interface CallHandler<T extends Func> {
 	readonly isCalled: Proxy<T>;
 	isCalledWith(...args: any[]): Proxy<T>;
 }
 
-interface Proxy<T extends (...args: any[]) => any> {
+interface Proxy<T extends Func> {
 	then(fn: T): Proxy<T>;
 	thenReturn(val: any): Proxy<T>;
 	thenThrow(err: Error): Proxy<T>;
 }
 
-interface MatcherProxy {
-	(arg: any): boolean;
-	readonly latest: any | undefined;
-	values(): any[];
+interface MatcherProxy<T> {
+	(arg: T): boolean;
+	readonly latest: T;
+	values(): T[];
 }
 
 export = when;
--- types/saywhen/saywhen-tests.ts
+++ types/saywhen/saywhen-tests.ts
@@ -2,3 +2,7 @@ import * as when from 'saywhen';
 
 when(jasmine.createSpy('test')); // $ExpectType CallHandler<(...args: any[]) => any>
 when(jasmine.createSpy('test')).isCalled; // $ExpectType Proxy<(...args: any[]) => any>
+
+when.captor();	// $ExpectType MatcherProxy<{}>
+when.captor(jasmine.any(Number));	// $ExpectType MatcherProxy<jasmine.Any>
+when.noConflict();	// $ExpectType void
