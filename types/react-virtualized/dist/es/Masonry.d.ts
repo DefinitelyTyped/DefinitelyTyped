c63577d6c3370099d0bd75f4a950a73754a1ffef
Konstantin Nesterov <wasd171@gmail.com>
2017-07-25 19:07:26 +0200

    Added comment about purpose of arbitrary values

--- types/react-virtualized/dist/es/Masonry.d.ts
+++ types/react-virtualized/dist/es/Masonry.d.ts
@@ -47,6 +47,13 @@ export type MasonryProps = {
     style?: React.CSSProperties,
     tabIndex?: number,
     width: number,
+    /**
+     * PLEASE NOTE
+     * The [key: string]: any; line is here on purpose
+     * This is due to the need of force re-render of PureComponent
+     * Check the following link if you want to know more
+     * https://github.com/bvaughn/react-virtualized#pass-thru-props
+     */
     [key: string]: any;
 }
 
