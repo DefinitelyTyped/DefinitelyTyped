52e152e7c55845461b03ccde7bed537f1d2be77e
dylandupreez <dylandp@privateproperty.co.za>
2017-07-26 16:16:15 +0200

    Incremented version number as this is that latest version of the node module

--- types/react-native-modalbox/index.d.ts
+++ types/react-native-modalbox/index.d.ts
@@ -1,4 +1,4 @@
-// Type definitions for react-native-modalbox 1.3
+// Type definitions for react-native-modalbox 1.4
 // Project: https://github.com/maxs15/react-native-modalbox#readme
 // Definitions by: Kyle Roach <https://github.com/me>
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
