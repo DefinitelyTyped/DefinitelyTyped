3af14280d31bea6ca5006a816eb573a3929046d5
Sean Sobey SAHL <seans@sahomeloans.com>
2017-07-26 08:33:58 +0200

    Fixed linting

--- types/saywhen/index.d.ts
+++ types/saywhen/index.d.ts
@@ -2,6 +2,7 @@
 // Project: https://github.com/pushtechnology/saywhen
 // Definitions by: Sean Sobey <https://github.com/SeanSobey>
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
+// TypeScript Version: 2.1
 
 /// <reference types="jasmine" />
 
