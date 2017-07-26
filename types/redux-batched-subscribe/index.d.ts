9b242af7d2717e72363955bb96dca204431d31e9
Dibyo Majumdar <dibyo.majumdar@gmail.com>
2017-07-25 18:51:06 -0700

    Add redux-batched-subscribe types

---
+++ types/redux-batched-subscribe/index.d.ts
@@ -0,0 +1,8 @@
+// Type definitions for redux-batched-subscribe 0.1.6
+// Project: https://github.com/tappleby/redux-batched-subscribe
+// Definitions by: Dibyo Majumdar <https://github.com/mDibyo>
+// Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
+
+import {GenericStoreEnhancer} from 'redux';
+
+export function batchedSubscribe(batch:(notify: () => void) => void): GenericStoreEnhancer;
