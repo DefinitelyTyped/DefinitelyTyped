2f76bf3760ad79f4db8ff6e873a5c33e1f347bad
Zlatko Andonovski <zlatko.andonovski@alite-international.com>
2017-07-26 11:10:41 +0200

    Added definition for properties-reader.

---
+++ types/properties-reader/index.d.ts
@@ -0,0 +1,25 @@
+// Type definitions for properties-reader 0.0
+// Project: https://github.com/steveukx/properties
+// Definitions by: Zlatko Andonovski <https://github.com/Goldsmith42/>
+// Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
+
+declare namespace PropertiesReader {
+    type Value = string|number|boolean;
+
+    interface Reader {
+        get(propertyName: string): Value|null;
+        getRaw(propertyName: string): string|null;
+        path(): {};
+        append(path: string): Reader;
+        read(properties: string): Reader;
+        set(propertyName: string, value: Value): Reader;
+        length: number;
+        each(iterator: (key: string, value: Value) => void, scope?: any): Reader;
+        getAllProperties(): { [key: string]: Value; };
+        clone(): Reader;
+    }
+}
+
+declare function PropertiesReader(path: string): PropertiesReader.Reader;
+
+export = PropertiesReader;
