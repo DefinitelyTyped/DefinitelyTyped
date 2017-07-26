73b7c952347d1210983113f984a4ea9b6c947467
Dibyo Majumdar <dibyo.majumdar@gmail.com>
2017-07-25 19:01:16 -0700

    Address lint errors

--- types/redux-batched-subscribe/index.d.ts
+++ types/redux-batched-subscribe/index.d.ts
@@ -1,8 +1,8 @@
-// Type definitions for redux-batched-subscribe 0.1.6
+// Type definitions for redux-batched-subscribe 0.1
 // Project: https://github.com/tappleby/redux-batched-subscribe
 // Definitions by: Dibyo Majumdar <https://github.com/mDibyo>
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
 
-import {GenericStoreEnhancer} from 'redux';
+import { GenericStoreEnhancer } from 'redux';
 
-export function batchedSubscribe(batch:(notify: () => void) => void): GenericStoreEnhancer;
+export function batchedSubscribe(batch: (notify: () => void) => void): GenericStoreEnhancer;
