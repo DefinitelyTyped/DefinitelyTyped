ca210a9d1aff90c761b5a4f9c228110cfb4abe5b
Jan Petzold <hacker_und_koch@protonmail.com>
2017-07-26 12:13:26 +0200

    add saml20 typings

---
+++ types/saml20/index.d.ts
@@ -0,0 +1,32 @@
+// Type definitions for saml20 0.1
+// Project: https://github.com/leandrob/saml20
+// Definitions by: Jan Petzold <https://github.com/HackerUndKoch>
+// Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
+
+
+export interface Profile {
+    issuer: string;
+    claims: any;
+    audience?: string;
+    sessionIndex?: string; // only SAML 2.0
+}
+
+export declare type ValidationOptions = ValidationOptionsWithKey | ValidationOptionsWithPrint;
+
+export interface ValidationOptionsWithKey {
+    publicKey: string;
+    bypassExpiration?: boolean;
+    audience?: any;
+}
+
+export interface ValidationOptionsWithPrint {
+    thumbprint: string;
+    bypassExpiration?: boolean;
+    audience?: any;
+}
+
+
+export declare type ParseCallback = (err: Error, profile: Profile, version: string) => void;
+
+export function parse(rawAssertion: string, cb: ParseCallback): void;
+export function validate(rawAssertion: string, options: ValidationOptions, cb: ParseCallback): void;
