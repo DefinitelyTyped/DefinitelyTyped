b4323c1fd486996947e692e56d702f7c4450672b
bsurai <bs08@ukr.net>
2017-07-27 13:35:33 +0300

    header

--- types/virtual-keyboard/index.d.ts
+++ types/virtual-keyboard/index.d.ts
@@ -1,4 +1,8 @@
 // Type definitions for 'virtual-keyboard'
+// Project: https://www.npmjs.com/package/virtual-keyboard		
+// Definitions by: Bogdan Surai <https://github.com/bsurai>		
+// Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped		
+// TypeScript Version: 2.4
 
 declare module "virtual-keyboard" {
     import * as jQuery from "jquery";
