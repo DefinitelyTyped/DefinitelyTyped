81320b20cab8778aedd750533a4ada05186d8014
bsurai <bs08@ukr.net>
2017-07-27 13:41:03 +0300

    Error: At 3:60 : Expected one of

--- types/virtual-keyboard/index.d.ts
+++ types/virtual-keyboard/index.d.ts
@@ -1,7 +1,7 @@
 // Type definitions for 'virtual-keyboard'
 // Project: https://www.npmjs.com/package/virtual-keyboard		
-// Definitions by: Bogdan Surai <https://github.com/bsurai>		
-// Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped		
+// Definitions by: Bogdan Surai https://github.com/bsurai
+// Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
 // TypeScript Version: 2.4
 
 declare module "virtual-keyboard" {
