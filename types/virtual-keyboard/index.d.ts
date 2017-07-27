277ab8e2b3c40e1e2794cfdeb5fbf681d1b899a0
bsurai <bs08@ukr.net>
2017-07-27 13:52:57 +0300

    header

--- types/virtual-keyboard/index.d.ts
+++ types/virtual-keyboard/index.d.ts
@@ -1,6 +1,6 @@
 // Type definitions for 'virtual-keyboard'
 // Project: https://www.npmjs.com/package/virtual-keyboard		
-// Definitions by: Bogdan Surai (https://github.com/bsurai)
+// Definitions by: Bogdan Surai <https://github.com/bsurai>
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
 // TypeScript Version: 2.4
 
