1eb0d76336ddfe50c09581189a7313f544f52eac
scc <chushu.scc@alibaba-inc.com>
2017-07-28 09:24:15 +0800

    change version

--- types/svg2png/index.d.ts
+++ types/svg2png/index.d.ts
@@ -1,4 +1,4 @@
-// Type definitions for svg2png node package 
+// Type definitions for svg2png 4.1
 // Project: https://github.com/domenic/svg2png
 // Definitions by: hans windhoff <https://github.com/hansrwindhoff>
 // Definitions by: songChengcheng <https://github.com/sccgithub>
