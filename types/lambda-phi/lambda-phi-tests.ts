1b38223c6f30d970262cf57230d202fb071010df
Ethan Dave B. Gomez <egomez@stansberryresearch.com>
2017-07-29 00:20:41 -0400

    Fix test and index.d.ts after running npm run lint lambda-phi

--- types/lambda-phi/lambda-phi-tests.ts
+++ types/lambda-phi/lambda-phi-tests.ts
@@ -1,7 +1,19 @@
-import {Lambda, Event, Context, PostConstructor, PreLambdaTimeout, PreLambdaCallback, Callback,} from 'lambda-phi';
-import { Path, PathParam, Get, Post, Delete, Headers, PathParams, QueryParams, Method, Any, Body } from 'lambda-phi/lib/api-gateway';
+import { Lambda, Event, Context, PostConstructor, PreLambdaTimeout, PreLambdaCallback, Callback } from 'lambda-phi';
+import {
+    Path,
+    PathParam,
+    Get,
+    Post,
+    Delete,
+    Headers,
+    PathParams,
+    QueryParams,
+    Method,
+    Any,
+    Body
+} from 'lambda-phi/lib/api-gateway';
 
-@Lambda({allowNullInjection:false})
+@Lambda({allowNullInjection: false})
 class LambdaClassTest {
     @Context() context;
     @Method() method; // GET, POST, ...
@@ -13,37 +25,37 @@ class LambdaClassTest {
 
     @Get()
     @Post()
-    public otherWise() {
-        this.callback(null, {body:JSON.stringify("otherwise\n")});
-        //this.callback(null, {event:this.event, context:this.context});
+    otherWise() {
+        this.callback(null, {body: JSON.stringify("otherwise\n")});
+        // this.callback(null, {event:this.event, context:this.context});
     }
 
     @Path('/affiliates/:id')
-    public normalCallback() {
+    normalCallback() {
         this.callback(null, this.context);
     }
 
     @Path('/body/:id')
-    public path1(@PathParam('id') id:string) {
-        this.callback(null, {body:JSON.stringify("my path 1\n")});
+    path1(@PathParam('id') id: string) {
+        this.callback(null, {body: JSON.stringify("my path 1\n")});
     }
 
-/*    @Path('/affiliates')
-    public affiliates() {
-        this.callback(null, this.event);
-    }*/
+    /*    @Path('/affiliates')
+        public affiliates() {
+            this.callback(null, this.event);
+        }*/
 
     @Path('/body/:my/:path2')
     @Get()
     @Post()
-    public path2(@PathParam('path2') path:string, @PathParam('my') here:string) {
+    path2(@PathParam('path2') path: string, @PathParam('my') here: string) {
         this.callback(null, {
-            body: JSON.stringify('path: '+path+', my:'+here+', query: '+this.queryParams.var+"\n")
+            body: JSON.stringify('path: ' + path + ', my:' + here + ', query: ' + this.queryParams.var + "\n")
         });
     }
 
     @Any()
-    public any() {
-        this.callback(null, {body:JSON.stringify("any method")});
+    any() {
+        this.callback(null, {body: JSON.stringify("any method")});
     }
 }
