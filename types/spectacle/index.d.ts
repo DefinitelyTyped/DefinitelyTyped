bcccf9b337b6b607862a9454fb217129bb19434f
Jake Ginnivan <jake.ginnivan@wanews.com.au>
2017-07-30 18:48:42 +0800

    Updated version number

--- types/spectacle/index.d.ts
+++ types/spectacle/index.d.ts
@@ -1,4 +1,4 @@
-// Type definitions for Spectacle 1.1.1
+// Type definitions for Spectacle 1.2.0
 // Project: https://github.com/FormidableLabs/victory
 // Definitions by: Zachary Maybury <https://github.com/zmaybury>
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
