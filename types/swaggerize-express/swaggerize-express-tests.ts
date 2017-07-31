548a2b6263ef2f563f188b624139170216703f66
Nick Morton <nick.morton@smoothwall.com>
2017-07-31 14:38:04 +0100

    Add RouteSegment i/f and extended Options.handlers to accept

--- types/swaggerize-express/swaggerize-express-tests.ts
+++ types/swaggerize-express/swaggerize-express-tests.ts
@@ -19,6 +19,30 @@ app.use(swaggerize(<swaggerize.Options>{
     handlers: './handlers'
 }));
 
+app.use(swaggerize(<swaggerize.Options>{
+    api: {
+        swagger: "2.0",
+        host: "localhost:8080",
+        info: {
+            title: "swaggerize-express.d.ts test",
+            version: "1"
+        },
+        paths: {
+
+        }
+    },
+    docspath: '/api-docs',
+    handlers: {
+        'api': {
+            'v1': {
+                'version': {
+                    '$get': (req: express.Request, res: express.Response) => res.send('v1')
+                }
+            }
+        }
+    }
+}));
+
 var server = app.listen(18888, 'localhost', function () {
     (<swaggerize.SwaggerizedExpress>app).swagger.api.host = server.address().address + ':' + server.address().port;
 });
