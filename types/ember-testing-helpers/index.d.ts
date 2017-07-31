9b53cc27cfee37a1fa16a04c570d16007b50f11f
Chris Krycho <chris@chriskrycho.com>
2017-07-31 17:47:19 -0400

    Update RSVP and its Ember dependents. (#18383)
    - Add default export and test code to RSVP, including sample code from
      the RSVP library.
    - Use imported RSVP types in Ember and ember-testing-helpers definitions.
    - Set minimum TS version in RSVP, Ember, and ember-testing-helpers.
    - Fix ember-testing-helpers header.
    - Make RSVP a two-type-parameter type; update Ember typedefs accordingly.
    - Update authors, definitions link in RSVP header.

--- types/ember-testing-helpers/index.d.ts
+++ types/ember-testing-helpers/index.d.ts
@@ -1,5 +1,5 @@
 // Type definitions for ember-testing/lib/helpers
-// Project: Ember.js Testing Helpers: https://github.com/emberjs/ember.js/tree/master/packages/ember-testing/lib/helpers
+// Project: https://github.com/emberjs/ember.js/tree/master/packages/ember-testing/lib/helpers
 // Definitions by: Chris Krycho <github.com/chriskrycho>
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
 // TypeScript Version: 2.3
@@ -9,55 +9,61 @@
 // not easily or straightforwardly exported from the Ember type definitions.
 
 /// <reference types="jquery" />
-/// <reference types="rsvp" />
+
+import RSVP from 'rsvp';
 
 type KeyEventType = 'keydown' | 'keyup' | 'keypress';
-type VoidWaitResult = RSVP.Promise<void>;
+type WaitResult<T> = RSVP.Promise<T, never>;
 
 declare global {
-  // https://github.com/emberjs/ember.js/blob/master/packages/ember-testing/lib/helpers/and_then.js
-  function andThen<T>(callback: <T>(...args: any[]) => T): RSVP.Promise<T>;
+    // https://github.com/emberjs/ember.js/blob/master/packages/ember-testing/lib/helpers/and_then.js
+    function andThen<T>(callback: (...args: any[]) => T): RSVP.Promise<T, never>;
 
-  // https://github.com/emberjs/ember.js/blob/master/packages/ember-testing/lib/helpers/click.js
-  function click(selector: string, context?: Object): VoidWaitResult;
+    // https://github.com/emberjs/ember.js/blob/master/packages/ember-testing/lib/helpers/click.js
+    function click(selector: string, context?: Object): WaitResult<void>;
 
-  // https://github.com/emberjs/ember.js/blob/master/packages/ember-testing/lib/helpers/current_path.js
-  function currentPath(): string;
+    // https://github.com/emberjs/ember.js/blob/master/packages/ember-testing/lib/helpers/current_path.js
+    function currentPath(): string;
 
-  // https://github.com/emberjs/ember.js/blob/master/packages/ember-testing/lib/helpers/current_route_name.js
-  function currentRouteName(): string;
+    // https://github.com/emberjs/ember.js/blob/master/packages/ember-testing/lib/helpers/current_route_name.js
+    function currentRouteName(): string;
 
-  // https://github.com/emberjs/ember.js/blob/master/packages/ember-testing/lib/helpers/current_url.js
-  function currentURL(): string;
+    // https://github.com/emberjs/ember.js/blob/master/packages/ember-testing/lib/helpers/current_url.js
+    function currentURL(): string;
 
-  // https://github.com/emberjs/ember.js/blob/master/packages/ember-testing/lib/helpers/fill_in.js
-  function fillIn(selector: string, context: Object, text: string): VoidWaitResult;
-  function fillIn(selector: string, text: string): VoidWaitResult;
+    // https://github.com/emberjs/ember.js/blob/master/packages/ember-testing/lib/helpers/fill_in.js
+    function fillIn(selector: string, context: Object, text: string): WaitResult<void>;
+    function fillIn(selector: string, text: string): WaitResult<void>;
 
-  // https://github.com/emberjs/ember.js/blob/master/packages/ember-testing/lib/helpers/find.js
-  function find(selector: string, context?: Object): JQuery<Node>;
+    // https://github.com/emberjs/ember.js/blob/master/packages/ember-testing/lib/helpers/find.js
+    function find(selector: string, context?: Object): JQuery<Node>;
 
-  // https://github.com/emberjs/ember.js/blob/master/packages/ember-testing/lib/helpers/find_with_assert.js
-  function findWithAssert(selector: string, context?: Object): JQuery<Node>;
+    // https://github.com/emberjs/ember.js/blob/master/packages/ember-testing/lib/helpers/find_with_assert.js
+    function findWithAssert(selector: string, context?: Object): JQuery<Node>;
 
-  // https://github.com/emberjs/ember.js/blob/master/packages/ember-testing/lib/helpers/key_event.js
-  function keyEvent(selector: string, type: KeyEventType, keyCode: number): VoidWaitResult;
+    // https://github.com/emberjs/ember.js/blob/master/packages/ember-testing/lib/helpers/key_event.js
+    function keyEvent(selector: string, type: KeyEventType, keyCode: number): WaitResult<void>;
 
-  // https://github.com/emberjs/ember.js/blob/master/packages/ember-testing/lib/helpers/pause_test.js
-  function pauseTest(): RSVP.Promise<{}>;
-  function resumeTest(): void;
+    // https://github.com/emberjs/ember.js/blob/master/packages/ember-testing/lib/helpers/pause_test.js
+    function pauseTest(): RSVP.Promise<{}, never>;
+    function resumeTest(): void;
 
-  // https://github.com/emberjs/ember.js/blob/master/packages/ember-testing/lib/helpers/trigger_event.js
-  function triggerEvent(selector: string, context: Object, type: string, options: Object): VoidWaitResult;
-  function triggerEvent(selector: string, context: Object, type: string): VoidWaitResult;
-  function triggerEvent(selector: string, type: string, options: Object): VoidWaitResult;
-  function triggerEvent(selector: string, type: string): VoidWaitResult;
+    // https://github.com/emberjs/ember.js/blob/master/packages/ember-testing/lib/helpers/trigger_event.js
+    function triggerEvent(
+        selector: string,
+        context: Object,
+        type: string,
+        options: Object
+    ): WaitResult<void>;
+    function triggerEvent(selector: string, context: Object, type: string): WaitResult<void>;
+    function triggerEvent(selector: string, type: string, options: Object): WaitResult<void>;
+    function triggerEvent(selector: string, type: string): WaitResult<void>;
 
-  // https://github.com/emberjs/ember.js/blob/master/packages/ember-testing/lib/helpers/visit.js
-  function visit<T>(route: string): VoidWaitResult;
+    // https://github.com/emberjs/ember.js/blob/master/packages/ember-testing/lib/helpers/visit.js
+    function visit<T>(route: string): WaitResult<void>;
 
-  // https://github.com/emberjs/ember.js/blob/master/packages/ember-testing/lib/helpers/wait.js
-  function wait<T>(value: T): RSVP.Promise<T>;
+    // https://github.com/emberjs/ember.js/blob/master/packages/ember-testing/lib/helpers/wait.js
+    function wait<T>(value: T): WaitResult<T>;
 }
 
 export {};
