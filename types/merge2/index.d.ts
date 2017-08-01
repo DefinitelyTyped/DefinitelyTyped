6b96c4d09417470952d1d88fa3f120608f6ad168
smac89 <smac89@users.noreply.github.com>
2017-08-01 17:54:55 -0600

    Fixed errors with files

--- types/merge2/index.d.ts
+++ types/merge2/index.d.ts
@@ -1,4 +1,4 @@
-// Type definitions for merge2 v1.1.0
+// Type definitions for merge2 1.1.0
 // Project: https://github.com/teambition/merge2
 // Definitions by: Tanguy Krotoff <https://github.com/tkrotoff>
 //                 Chigozirim C. <https://github.com/smac89>
@@ -10,7 +10,7 @@ import {DuplexOptions} from 'stream';
 
 type StreamType = NodeJS.ReadableStream | IMerge2Stream;
 
-interface IMerge2Stream extends NodeJS.ReadWriteStream {
+interface Merge2Stream extends NodeJS.ReadWriteStream {
     /**
      * @brief      Add more streams to an existing merged stream
      *
@@ -18,18 +18,16 @@ interface IMerge2Stream extends NodeJS.ReadWriteStream {
      *
      * @return     The merged stream
      */
-    add(...args: Array<StreamType | Array<StreamType>>): IMerge2Stream;
+    add(...args: (StreamType | Array<StreamType>)[]): Merge2Stream;
 }
 
 /**
  * @brief      This function takes an arbitrary number of streams, but the duplexoption
  *             has to be the last parameter of the function.
- *             
  * @param      args  The arguments
- *
  * @return     A merged duplex stream
- */ 
-declare function IMerge2 (...args: Array<StreamType | Array<StreamType> | DuplexOptions>): IMerge2Stream;
+ */
+declare function IMerge2 (...args: (StreamType | Array<StreamType> | DuplexOptions)[]): Merge2Stream;
 
 declare namespace IMerge2 {}
 
