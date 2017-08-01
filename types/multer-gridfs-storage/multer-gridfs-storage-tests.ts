71b3b95aa490793170418fa9e2b67a9f737903f7
devconcept <devconcept@outlook.com>
2017-08-01 19:15:55 -0400

    Added multer-gridfs-storage version 2

--- types/multer-gridfs-storage/multer-gridfs-storage-tests.ts
+++ types/multer-gridfs-storage/multer-gridfs-storage-tests.ts
@@ -1,43 +1,51 @@
 import * as MulterGridfsStorage from 'multer-gridfs-storage';
-import { Grid } from "gridfs-stream";
+import {Db, MongoClient, Server} from "mongodb";
 
 // Exported interfaces
-let opt1: MulterGridfsStorage.GfsStorageOptions;
+let opt1: MulterGridfsStorage.DbStorageOptions;
 let opt2: MulterGridfsStorage.UrlStorageOptions;
+let conf: MulterGridfsStorage.FileConfig;
+
+// Connection promise
+let dbPromise = MongoClient.connect('url');
+
+let server = new Server('localhost', 27017);
+let db = new Db('database', server);
 
 // All options
-let gfsCtr = new MulterGridfsStorage({
-	gfs: new Grid(),
-	filename: (req, file, cb) => cb(null, ''),
-	chunkSize: (req, file, cb) => cb(null, 1),
-	identifier: (req, file, cb) => cb(null, ''),
-	metadata: (req, file, cb) => cb(new Error(), null),
-	logLevel: 'all',
-	root: (req, file, cb) => cb(null, 'unicorns')
+let dbFileStorage = new MulterGridfsStorage({
+    db: db,
+    file: function(req, file) {
+        return 'a';
+    }
 });
 
-let urlCtr = new MulterGridfsStorage({
-	url: '',
-	filename: (req, file, cb) => cb(null, ''),
-	chunkSize: (req, file, cb) => cb(null, 1),
-	identifier: (req, file, cb) => cb(null, ''),
-	metadata: (req, file, cb) => cb(new Error(), {}),
-	logLevel: 'all',
-	root: (req, file, cb) => cb(null, '')
+let urlFileStorage = new MulterGridfsStorage({
+    url: 'mongodb://yourhost:27017/database',
+    connectionOpts: {},
+    file: (req, file) => {
+        return {};
+    }
 });
+
 
 // Other properties are optional
-let gfsOnly = new MulterGridfsStorage({
-	gfs: new Grid()
+let promiseStorage = new MulterGridfsStorage({
+    db: dbPromise
 });
 
-let urlOnly = new MulterGridfsStorage({
-	url: ''
+let dbStorage = new MulterGridfsStorage({
+    db: db
+});
+
+let urlStorage = new MulterGridfsStorage({
+    url: 'mongodb://yourhost:27017/database'
 });
 
 function noop() {
 }
+
 // Extends event emitter
-gfsCtr.on('connection', noop);
-urlCtr.addListener('conection', noop);
-gfsOnly.removeAllListeners('conection');
+promiseStorage.on('connection', noop);
+urlStorage.addListener('conection', noop);
+dbStorage.removeAllListeners('conection');
