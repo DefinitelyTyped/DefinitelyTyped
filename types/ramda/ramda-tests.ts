8fa64b11d0a85f8a3620f590b3b9c2aaeed0509e
Alexander James Phillips <ajp.global@gmail.com>
2017-08-01 15:24:02 +0100

    Ramda - Curry intersection

--- types/ramda/ramda-tests.ts
+++ types/ramda/ramda-tests.ts
@@ -2241,3 +2241,6 @@ class Why {
     R.intersperse(0, [1, 2]); // => [1, 0, 2]
     R.intersperse(0, [1]); // => [1]
 };
+
+R.intersection([1, 2, 3], [2, 3, 3, 4]) // => [2, 3]
+R.intersection([1, 2, 3])([2, 3, 3, 4]) // => [2, 3]
