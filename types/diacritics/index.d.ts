4053e9dd4c232bb5e1c6d8d8ab71f140eb3a4c4a
otociulis <otociulis@users.noreply.github.com>
2017-08-01 12:54:31 -0700

    Adding diacritics package (#18509)
    * Adding diacritics package
    
    * Adding linter
    
    * Fixing linting errors
    
    * Fixing warnings and errors
    
    * Removing unnecessary namespace

---
+++ types/diacritics/index.d.ts
@@ -0,0 +1,14 @@
+// Type definitions for diacritics 1.3
+// Project: https://github.com/andrewrk/node-diacritics
+// Definitions by: Oto Ciulis <https://github.com/otociulis/>
+// Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
+
+export function remove(str: string): string;
+export const replacementList: Array<{
+    base: string;
+    chars: string;
+}>;
+
+export const diacriticsMap: {
+    [key: string]: string;
+};
