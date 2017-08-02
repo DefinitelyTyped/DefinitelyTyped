2e3cc426aee755a0e2fbce49ff3bc9b7fb8e0e43
Matthieu Montaudouin <matmon@gmail.com>
2017-08-02 11:54:13 +0100

    [react-color] Type updates for the individual APIs
    Update to the type definition of the individual APIs to reflect their specificity (adding of onSwatchHover for example)

--- types/react-color/lib/components/block/Block.d.ts
+++ types/react-color/lib/components/block/Block.d.ts
@@ -1,9 +1,11 @@
 import { Component } from "react";
-import { ColorPickerProps } from "react-color";
+import { ColorPickerProps, Color } from "react-color";
 
 export interface BlockPickerProps extends ColorPickerProps<BlockPicker> {
     colors?: string[];
     width?: string;
+    triangle?: 'hide' | 'top';
+    onSwatchHover?(color: Color, event: MouseEvent): void;
 }
 
 export default class BlockPicker extends Component<BlockPickerProps> {}
--- types/react-color/lib/components/sketch/Sketch.d.ts
+++ types/react-color/lib/components/sketch/Sketch.d.ts
@@ -1,10 +1,11 @@
 import { Component } from "react";
-import { ColorPickerProps } from "react-color";
+import { ColorPickerProps, Color } from "react-color";
 
 export interface SketchPickerProps extends ColorPickerProps<SketchPicker> {
     disableAlpha?: boolean;
     presetColors?: string[];
     width?: string;
+    onSwatchHover?(color: Color, event: MouseEvent): void;
 }
 
 export default class SketchPicker extends Component<SketchPickerProps> {}
