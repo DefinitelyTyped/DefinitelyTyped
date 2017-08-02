52a05e1b891751de0aa4b84bf6dfca78cf452ba2
Dimitri Benin <dimitri.benin@it-designers.de>
2017-08-02 23:54:30 +0200

    [p-any] add missing options interface

--- types/p-any/p-any-tests.ts
+++ types/p-any/p-any-tests.ts
@@ -8,3 +8,15 @@ pAny([
 ]).then(first => {
     let str: string = first;
 });
+
+pAny(new Set([
+    got.head('github.com').then(() => 'github'),
+    got.head('google.com').then(() => 'google'),
+    got.head('twitter.com').then(() => 'twitter'),
+]), {
+    filter(str: string) {
+        return true;
+    }
+}).then(first => {
+    let str: string = first;
+});
