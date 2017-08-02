02ff752907efc578064bdcdc5139d88601129bcd
Dimitri Benin <BendingBender@users.noreply.github.com>
2017-08-02 02:35:16 +0200

    [cucumber] add this definition to steps and hooks, lint code (#18369)
    Add an optional extended description…

--- types/cucumber/v1/cucumber-tests.ts
+++ types/cucumber/v1/cucumber-tests.ts
@@ -1,44 +1,44 @@
-import * as assert from "power-assert";
-import cucumber = require("cucumber");
+import * as assert from 'power-assert';
+import cucumber = require('cucumber');
 
-function StepSample() {
+function StepSample(this: cucumber.StepDefinitions & cucumber.Hooks) {
     type Callback = cucumber.CallbackStepDefinition;
     type Table = cucumber.TableDefinition;
     type HookScenario = cucumber.HookScenario;
-    const step = <cucumber.StepDefinitions>this;
-    const hook = <cucumber.Hooks>this;
+    const step = this;
+    const hook = this;
 
-    hook.Before(function (scenario: HookScenario, callback: Callback) {
+    hook.Before((scenario: HookScenario, callback: Callback) => {
         scenario.isFailed() && callback.pending();
     });
 
-    hook.Before({ timeout: 1000 }, function(scenario: HookScenario, callback: Callback) {
+    hook.Before({ timeout: 1000 }, (scenario: HookScenario, callback: Callback) => {
 		callback();
 	});
 
-    hook.After({ timeout: 1000 }, function(scenario: HookScenario, callback: Callback) {
+    hook.After({ timeout: 1000 }, (scenario: HookScenario, callback: Callback) => {
 		callback();
 	});
 
-    hook.Around(function (scenario: HookScenario, runScenario: (error: string, callback?: Function) => void) {
-        scenario.isFailed() && runScenario(null, function () {
+    hook.Around((scenario: HookScenario, runScenario: (error: string | null, callback?: () => void) => void) => {
+        scenario.isFailed() && runScenario(null, () => {
             console.log('finish tasks');
         });
     });
 
-    hook.registerHandler('AfterFeatures', function (event: any, callback: Function) {
+    hook.registerHandler('AfterFeatures', (event: any, callback: () => void) => {
         callback();
     });
 
-    step.Given(/^I am on the Cucumber.js GitHub repository$/, function (callback: Callback) {
+    step.Given(/^I am on the Cucumber.js GitHub repository$/, function(callback: Callback) {
         this.visit('https://github.com/cucumber/cucumber-js', callback);
     });
 
-    step.When(/^I go to the README file$/, function (title: string, callback: Callback) {
+    step.When(/^I go to the README file$/, (title: string, callback: Callback) => {
         callback(null, 'pending');
     });
 
-    step.Then(/^I should see "(.*)" as the page title$/, {timeout: 60 * 1000}, function (title: string, callback: Callback) {
+    step.Then(/^I should see "(.*)" as the page title$/, {timeout: 60 * 1000}, function(title: string, callback: Callback) {
         const pageTitle = this.browser.text('title');
 
         if (title === pageTitle) {
@@ -51,7 +51,7 @@ function StepSample() {
     // Type for data_table.js on
     // https://github.com/cucumber/cucumber-js/blob/a5fd8251918c278ab2e389226d165cedb44df14a/lib/cucumber/ast/data_table.js
 
-    step.Given(/^a table step with Table raw$/, function (table: Table) {
+    step.Given(/^a table step with Table raw$/, (table: Table) => {
         const expected = [
             ['Cucumber', 'Cucumis sativus'],
             ['Burr Gherkin', 'Cucumis anguria']
@@ -60,7 +60,7 @@ function StepSample() {
         assert.deepEqual(actual, expected);
     });
 
-    step.Given(/^a table step with Table rows$/, function (table: Table) {
+    step.Given(/^a table step with Table rows$/, (table: Table) => {
         const expected = [
             ['Apricot', '5'],
             ['Brocolli', '2'],
@@ -70,31 +70,29 @@ function StepSample() {
         assert.deepEqual(actual, expected);
     });
 
-    step.Given(/^a table step with Table rowHash$/, function (table: Table) {
+    step.Given(/^a table step with Table rowHash$/, (table: Table) => {
         const expected = {
-            'Cucumber': 'Cucumis sativus',
+            Cucumber: 'Cucumis sativus',
             'Burr Gherkin': 'Cucumis anguria'
         };
-        const actual: { [firstCol:string]: string } = table.rowsHash();
+        const actual: { [firstCol: string]: string } = table.rowsHash();
         assert.deepEqual(actual, expected);
     });
 
-    step.Given(/^a table step$/, function (table: Table) {
+    step.Given(/^a table step$/, (table: Table) => {
         const expected = [
-            {'Vegetable': 'Apricot', 'Rating': '5'},
-            {'Vegetable': 'Brocolli', 'Rating': '2'},
-            {'Vegetable': 'Cucumber', 'Rating': '10'}
+            {Vegetable: 'Apricot', Rating: '5'},
+            {Vegetable: 'Brocolli', Rating: '2'},
+            {Vegetable: 'Cucumber', Rating: '10'}
         ];
-        const actual: { [colName:string]: string }[] = table.hashes();
+        const actual: Array<{ [colName: string]: string }> = table.hashes();
         assert.deepEqual(actual, expected);
     });
-
 }
 
 function registerListener(): cucumber.EventListener {
     let listener = Object.assign(cucumber.Listener(), {
-        handleBeforeScenarioEvent: (scenario: cucumber.events.ScenarioPayload, callback: () => void) => {
-
+        handleBeforeScenarioEvent(scenario: cucumber.events.ScenarioPayload, callback: () => void) {
             // do some interesting stuff ...
 
             callback();
