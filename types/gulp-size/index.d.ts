cf2d46f9cd862242417ab756719a4ce127caed44
remisery <20260860+remisery@users.noreply.github.com>
2017-08-02 09:25:07 +0200

    Add tslint.json and fix lint errors

--- types/gulp-size/index.d.ts
+++ types/gulp-size/index.d.ts
@@ -1,11 +1,10 @@
-// Type definitions for gulp-size v2.1.0
+// Type definitions for gulp-size 2.1
 // Project: https://github.com/sindresorhus/gulp-size
 // Definitions by: Tanguy Krotoff <https://github.com/tkrotoff>
 //                 Remisery <https://github.com/remisery>
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
 
 /// <reference types="node" />
-
 
 declare namespace size {
     interface Options {
