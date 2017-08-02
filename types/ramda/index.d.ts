fbf9cfe947234a2bc225b8f0f8df748231723e57
Miloslav Nenadal <nenadalm@gmail.com>
2017-08-02 17:30:47 +0200

    [ramda]: Fix a few tslint issues

--- types/ramda/index.d.ts
+++ types/ramda/index.d.ts
@@ -585,8 +585,8 @@ declare namespace R {
          */
         filter<T1 extends Array<T2>, T2>(fn: (value: T2) => boolean): (list: T1) => T1;
         filter<T1 extends Array<T2>, T2>(fn: (value: T2) => boolean, list: T1): T1;
-        filter<T extends Object>(fn: (value: T[keyof T]) => boolean, obj: T): Partial<T>;
-        filter<T extends Object>(fn: (value: T[keyof T]) => boolean): (obj: T) => Partial<T>;
+        filter<T>(fn: (value: T[keyof T]) => boolean, obj: T): Partial<T>;
+        filter<T>(fn: (value: T[keyof T]) => boolean): (obj: T) => Partial<T>;
 
         /**
          * Returns the first element of the list which matches the predicate, or `undefined` if no
