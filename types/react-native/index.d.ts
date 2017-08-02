6e8ad8b33244f239100e5c26c493cff486433429
huhuanming <workboring@gmail.com>
2017-08-03 03:59:19 +0800

    bump to 0.47 (#18561)

--- types/react-native/index.d.ts
+++ types/react-native/index.d.ts
@@ -1,4 +1,4 @@
-// Type definitions for react-native 0.46
+// Type definitions for react-native 0.47
 // Project: https://github.com/facebook/react-native
 // Definitions by: Eloy Durán <https://github.com/alloy>
 //                 Fedor Nezhivoi <https://github.com/gyzerok>
