4a9466fcb0d426ac7a2ae8cada1edca7e212c645
smac89 <smac89@users.noreply.github.com>
2017-08-02 08:06:05 -0600

    Fixed jsdocs and added an event emitted by the stream

--- types/merge2/index.d.ts
+++ types/merge2/index.d.ts
@@ -12,20 +12,39 @@ type StreamType = NodeJS.ReadableStream | Merge2Stream;
 
 interface Merge2Stream extends NodeJS.ReadWriteStream {
     /**
-     * @brief      Add more streams to an existing merged stream
+     * @summary    Add more streams to an existing merged stream
      *
-     * @param      args  streams to add
+     * @param      {...(StreamType|StreamType[])} args  streams to add
      *
      * @return     The merged stream
      */
     add(...args: Array<StreamType | StreamType[]>): Merge2Stream;
+
+    /**
+     * @summary    It will emit 'queueDrain' when all streams merged.
+     *             If you set end === false in options, this event give you a notice that
+     *             you should add more streams to merge, or end the mergedStream.
+     *
+     * @param      {string} event  The 'queueDrain' event
+     *
+     * @return     This stream
+     */
+    on(event: 'queueDrain', callback: () => void): this;
 }
 
 /**
- * @brief      This function takes an arbitrary number of streams, but the duplexoption
- *             has to be the last parameter of the function.
- * @param      args  The arguments
- * @return     A merged duplex stream
+ * @summary        This function takes an arbitrary number of streams and returns a
+ *                 Merge2Stream.
+ *
+ * @description    If a DuplexOption is specified, it has to be specified last in the
+ *                 argument list
+ *
+ * @see            { @link https://github.com/teambition/merge2#api }
+ *
+ * @param          {...(StreamType|StreamType[])} args  StreamTypes
+ * @param          {DuplexOptions} opts  Optional DuplexOption to be specified last
+ *
+ * @return         A merged duplex stream
  */
 declare function IMerge2(...args: Array<StreamType | StreamType[] | DuplexOptions>): Merge2Stream;
 
