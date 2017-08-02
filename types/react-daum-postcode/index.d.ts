59eef3ab8b9b8daf984a3b8b4fdc057d1c685b88
Sa-ryong <saryong@shoe4rabbit.com>
2017-08-02 11:29:06 +0900

    [react-daum-postcode] bump js package version to 1.3.0

--- types/react-daum-postcode/index.d.ts
+++ types/react-daum-postcode/index.d.ts
@@ -1,23 +1,29 @@
-// Type definitions for react-daum-postcode 1.2.1
+// Type definitions for react-daum-postcode 1.3.0
 // Project: https://github.com/kimminsik-bernard/react-daum-postcode
 // Definitions by: Sa-ryong Kang <https://github.com/Sa-ryong>
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
-// TypeScript Version: 2.3
+// TypeScript Version: 2.2
 
-import { Component } from 'react';
+import * as React from 'react';
 
-export interface DaumPostcodeProps {
-  onComplete?: any;
-  width?: any;
-  height?: any;
-  autoClose?: any;
-  autoResize?: any;
-  animation?: any;
-  style?: any;
-  defaultQuery?: any;
-  theme?: any;
+export as namespace DaumPostcode;
+export = DaumPostcode;
+
+declare namespace DaumPostcode {
+  export interface Props {
+    onComplete: any;
+    width?: number | string;
+    height?: number | string;
+    autoClose?: boolean;
+    autoResize?: boolean;
+    animation?: boolean;
+    style?: any;
+    defaultQuery?: string;
+    theme?: any;
+    scriptUrl?: string;
+  }
 }
 
-export default class DaumPostcode extends Component<DaumPostcodeProps, any> {
+declare class DaumPostcode extends React.Component<DaumPostcode.Props, any> {
   render(): JSX.Element;
 }
