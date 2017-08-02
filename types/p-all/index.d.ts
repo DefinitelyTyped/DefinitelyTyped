50311a9c533cc88d1343ba445d87047509ae40b3
Dimitri Benin <dimitri.benin@it-designers.de>
2017-08-03 00:28:07 +0200

    [p-all] remove unnecessary default to any type

--- types/p-all/index.d.ts
+++ types/p-all/index.d.ts
@@ -5,7 +5,6 @@
 
 export = pAll;
 
-declare function pAll<TAll>(tasks: Iterable<PromiseFactory<TAll>>, options?: pAll.Options): Promise<TAll[]>;
 declare function pAll<T1, T2, T3, T4, T5, T6, T7, T8, T9, T10>(
     tasks: [PromiseFactory<T1>, PromiseFactory<T2>, PromiseFactory<T3>, PromiseFactory<T4>, PromiseFactory<T5>, PromiseFactory<T6>, PromiseFactory<T7>,
         PromiseFactory<T8>, PromiseFactory<T9>, PromiseFactory<T10>],
@@ -33,7 +32,7 @@ declare function pAll<T1, T2, T3, T4>(
 declare function pAll<T1, T2, T3>(tasks: [PromiseFactory<T1>, PromiseFactory<T2>, PromiseFactory<T3>], options?: pAll.Options): Promise<[T1, T2, T3]>;
 declare function pAll<T1, T2>(tasks: [PromiseFactory<T1>, PromiseFactory<T2>], options?: pAll.Options): Promise<[T1, T2]>;
 declare function pAll<T1>(tasks: [PromiseFactory<T1>], options?: pAll.Options): Promise<[T1]>;
-declare function pAll(tasks: Iterable<PromiseFactory<any>>, options?: pAll.Options): Promise<any[]>;
+declare function pAll<TAll>(tasks: Iterable<PromiseFactory<TAll>> | Array<PromiseFactory<TAll>>, options?: pAll.Options): Promise<TAll[]>;
 
 type PromiseFactory<T> = () => Promise<T> | PromiseLike<T>;
 
