2425a019f21bcad5b9105587aad00ad42025d48c
remisery <20260860+remisery@users.noreply.github.com>
2017-08-02 20:12:27 +0200

    [gulp-size] Update type definitions to v2.1.0  (#18551)
    * extend api
    
    * Add tslint.json and fix lint errors

--- types/gulp-size/gulp-size-tests.ts
+++ types/gulp-size/gulp-size-tests.ts
@@ -1,6 +1,3 @@
-
-
-
 import * as gulp from 'gulp';
 import * as size from 'gulp-size';
 import * as debug from 'gulp-debug';
@@ -11,16 +8,14 @@ gulp.task('default', () =>
         .pipe(gulp.dest('dist'))
 );
 
-
 gulp.task('default', () => {
-    var s = size();
+    let s = size();
 
     return gulp.src('fixture.js')
         .pipe(s)
         .pipe(gulp.dest('dist'))
         .pipe(debug({title: 'Total size ' + s.prettySize}));
 });
-
 
 size();
 size({showFiles: true, gzip: true});
