803914b0a37c5e26f1920d18c21ff427e52f704f
smac89 <smac89@users.noreply.github.com>
2017-08-01 22:24:43 -0600

    Fixed major minor PITA!

--- types/merge2/index.d.ts
+++ types/merge2/index.d.ts
@@ -1,4 +1,4 @@
-// Type definitions for merge2 1.1.0
+// Type definitions for merge2 1.1
 // Project: https://github.com/teambition/merge2
 // Definitions by: Tanguy Krotoff <https://github.com/tkrotoff>
 //                 Chigozirim C. <https://github.com/smac89>
