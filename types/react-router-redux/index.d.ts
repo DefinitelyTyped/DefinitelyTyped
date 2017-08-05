bb2156cb99a0cb5a374826dd32a4c1b6defbf3ee
Dmytro Borysov <dborysov@users.noreply.github.com>
2017-08-05 02:18:30 +0200

    [react-router-redux] - switching action type from `string` to a specific string (#18566)
    * [react-router-redux] - switching action type from `string` to a specific string
    
    * [react-router-redux] - exposing value instead of type

--- types/react-router-redux/index.d.ts
+++ types/react-router-redux/index.d.ts
@@ -25,7 +25,7 @@ export interface ConnectedRouterProps<State> {
 }
 export class ConnectedRouter<State> extends React.Component<ConnectedRouterProps<State>> {}
 
-export const LOCATION_CHANGE: string;
+export const LOCATION_CHANGE = '@@router/LOCATION_CHANGE';
 
 export interface RouterState {
     location: Location | null;
@@ -33,7 +33,7 @@ export interface RouterState {
 
 export const routerReducer: Reducer<RouterState>;
 
-export const CALL_HISTORY_METHOD: string;
+export const CALL_HISTORY_METHOD = '@@router/CALL_HISTORY_METHOD';
 
 export function push(location: LocationDescriptor, state?: LocationState): RouterAction;
 export function replace(location: LocationDescriptor, state?: LocationState): RouterAction;
