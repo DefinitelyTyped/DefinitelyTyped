e4c1f77d850ccf0a5c884385766b639bdecaed0c
Dimitri Benin <dimitri.benin@it-designers.de>
2017-08-05 14:36:49 +0200

    [p-every] introduce typings

---
+++ types/p-every/index.d.ts
@@ -0,0 +1,17 @@
+// Type definitions for p-every 1.0
+// Project: https://github.com/kevva/p-every#readme
+// Definitions by: BendingBender <https://github.com/BendingBender>
+// Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
+// TypeScript Version: 2.4
+
+export = pEvery;
+
+declare function pEvery<T>(name: Iterable<PromiseLike<T> | T>,
+                           testFn: (element: T, index: number) => boolean | Promise<boolean>,
+                           options?: pEvery.Options): Promise<boolean>;
+
+declare namespace pEvery {
+    interface Options {
+        concurrency?: number;
+    }
+}
