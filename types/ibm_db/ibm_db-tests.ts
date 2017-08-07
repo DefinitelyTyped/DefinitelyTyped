06c477ce4dde20986bcd3feb416329bb2d880840
unknown <voga06@gmail.com>
2017-08-07 22:33:46 +0200

    Add some test

--- types/ibm_db/ibm_db-tests.ts
+++ types/ibm_db/ibm_db-tests.ts
@@ -1,4 +1,12 @@
-import ibmdb from 'ibm_db';
+import * as ibmdb from 'ibm_db';
 const cn = '';
-ibmdb().open(cn, (err: Error, conn: any) => {
+ibmdb.open("DATABASE=<dbname>;HOSTNAME=<myhost>;UID=db2user;PWD=password;PORT=<dbport>;PROTOCOL=TCPIP", function (err: Error, conn: ibmdb.Database) {
+  if (err) return false;
+
+  conn.query('select 1 from sysibm.sysdummy1', function (err, data) {
+    if (err) return false
+    conn.close(function () {
+      //
+    });
+  });
 });
