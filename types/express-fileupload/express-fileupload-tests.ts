f2a207c5fd118bf15685bbe1589d43612001170b
Gintautas Miselis <gintautas.miselis@eckoh.com>
2017-08-07 14:52:02 +0100

    Fixed all lint errors

--- types/express-fileupload/express-fileupload-tests.ts
+++ types/express-fileupload/express-fileupload-tests.ts
@@ -1,14 +1,29 @@
 import * as express from 'express';
-import { RequestHandler } from 'express-serve-static-core';
-import fileUpload = require('express-fileupload');
+import { RequestHandler, Request, Response, NextFunction } from 'express-serve-static-core';
+import { fileUpload, UploadedFile } from 'express-fileupload';
 
-const app = express();
+const app: express.Express = express();
 
-app.use(fileUpload());
+app.use(fileUpload({debug: true}));
 
-const uploadHandler: RequestHandler = (req, res, next) => {
-    console.log(req.files.field.name);
-    console.log(req.files.field2[0].name);
+function isUploadedFile(file: UploadedFile | UploadedFile[]): file is UploadedFile {
+    return typeof file === 'object' && (<UploadedFile> file).name !== undefined;
+}
+
+const uploadHandler: RequestHandler = (req: Request, res: Response, next: NextFunction) => {
+    if (typeof req.files === 'object') {
+        const fileField = req.files.field;
+        if (isUploadedFile(fileField)) {
+            console.log(fileField.name);
+        }
+
+        const fileList = req.files.fileList;
+        if (Array.isArray(fileList)) {
+            for (const file of fileList) {
+                console.log(file.name);
+            }
+        }
+    }
 };
 
 app.post('/upload', uploadHandler);
--- types/express-fileupload/index.d.ts
+++ types/express-fileupload/index.d.ts
@@ -2,25 +2,35 @@
 // Project: https://github.com/richardgirges/express-fileupload#readme
 // Definitions by: Gintautas Miselis <https://github.com/Naktibalda>
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
+// TypeScript Version: 2.3
 
-/// <reference types="express" />
+import express = require('express');
 
-declare namespace Express {
-    interface Request {
-        files?: ExpressFileUpload.FileArray;
+declare global {
+    namespace Express {
+        interface Request {
+            files?: FileArray;
+        }
     }
 }
 
-declare namespace ExpressFileUpload {
-    class FileArray {
-        [index: string]: UploadedFile | UploadedFile[]
-    }
-
-    interface UploadedFile {
-        name: string;
-        encoding: string;
-        mimetype: string;
-        data: Buffer;
-        mv(targetLocation: string, callback: (err: any) => {}): void;
-    }
+export class FileArray {
+    [index: string]: UploadedFile | UploadedFile[]
 }
+
+export interface UploadedFile {
+    name: string;
+    encoding: string;
+    mimetype: string;
+    data: Buffer;
+    mv(path: string, callback: (err: any) => {}): void;
+}
+
+export interface Options {
+    debug?: boolean;
+    safeFileNames?: boolean;
+    preserveExtension?: boolean | string | number;
+    [property: string]: any;
+}
+
+export function fileUpload(options?: Options): express.RequestHandler;
