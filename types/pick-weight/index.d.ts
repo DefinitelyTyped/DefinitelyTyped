c490fb9147cc37c5bd9aea69f034f3fc3781007c
Roberts Slisans <rsxdalv@gmail.com>
2017-08-07 18:07:49 +0300

    Fix no-single-module

--- types/pick-weight/index.d.ts
+++ types/pick-weight/index.d.ts
@@ -3,11 +3,9 @@
 // Definitions by: Roberts Slisans <https://github.com/rsxdalv>
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
 
-declare module "pick-weight" {
-    // Dummy function allows to avoid hard to kill or fix tslint warning
-    // (exporting pluginFunc will make this a non-importable module)
-    function weighted<T>(arr: T[], weights: number[]): T;
-    // Let allows typescript to still use ES2015 style imports
-    let y: typeof weighted;
-    export = y;
-}
+// Dummy function allows to avoid hard to kill or fix tslint warning
+// (exporting pluginFunc will make this a non-importable module)
+declare function weighted<T>(arr: T[], weights: number[]): T;
+// Let allows typescript to still use ES2015 style imports
+declare let y: typeof weighted;
+export = y;
