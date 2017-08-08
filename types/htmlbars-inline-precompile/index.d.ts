e624f55fafbb0901d1cdef6f20ef45586b0d8aa4
Chris Krycho <chris@chriskrycho.com>
2017-08-08 19:41:28 -0400

    Add htmlbars-inline-precompile definition.

---
+++ types/htmlbars-inline-precompile/index.d.ts
@@ -0,0 +1,13 @@
+// Type definitions for htmlbars-inline-precompile 1.0
+// Project: ember-cli-htmlbars-inline-precompile
+// Definitions by: Chris Krycho <https://github.com/chriskrycho>
+// Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
+
+// This is a bit of a funky one: it's from a [Babel plugin], but is exported for
+// Ember applications as the module `"htmlbars-incline-precompile"`. It acts
+// like a tagged string from the perspective of consumers, but is actually an
+// AST transformation which generates a function as its output.
+//
+// [Babel plugin]: https://github.com/ember-cli/babel-plugin-htmlbars-inline-precompile#babel-plugin-htmlbars-inline-precompile-
+
+export default function hbs(tagged: TemplateStringsArray): () => {};
