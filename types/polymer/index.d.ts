ae7877bc0c0b1a8b69395b6aa43a68875aaa8bc1
Derek Brans <dbrans@google.com>
2017-08-08 10:37:02 -0700

    Bump version in header

--- types/polymer/index.d.ts
+++ types/polymer/index.d.ts
@@ -1,4 +1,4 @@
-// Type definitions for polymer v1.1.6
+// Type definitions for polymer v1.2.6
 // Project: https://github.com/Polymer/polymer
 // Definitions by: Louis Grignon <https://github.com/lgrignon>, Suguru Inatomi <https://github.com/laco0416>
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
