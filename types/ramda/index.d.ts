b4456e08cdb9dee559d51738b443eb80e1be2d47
Alexander James Phillips <ajp.global@gmail.com>
2017-08-08 19:41:42 +0100

    [Ramda] Correct pipe with 8 steps, add pipe with 9 and 10 (#18700)
    * Correct pipe with 8 steps, add pipe with 9 and 10
    
    * Ramda - Add single and double argument versions of 9 and 10 pipe

--- types/ramda/index.d.ts
+++ types/ramda/index.d.ts
@@ -1325,7 +1325,7 @@ declare namespace R {
             fn3: (x: T3) => T4,
             fn4: (x: T4) => T5,
             fn5: (x: T5) => T6,
-            fn6: (x: T5) => T6,
+            fn6: (x: T6) => T7,
             fn7: (x: T7) => T8): (x0: V0, x1: V1) => T8;
         pipe<V0, V1, V2, T1, T2, T3, T4, T5, T6, T7, T8>(
             fn0: (x0: V0, x1: V1, x2: V2) => T1,
@@ -1334,8 +1334,73 @@ declare namespace R {
             fn3: (x: T3) => T4,
             fn4: (x: T4) => T5,
             fn5: (x: T5) => T6,
-            fn6: (x: T5) => T6,
+            fn6: (x: T6) => T7,
             fn7: (x: T7) => T8): (x0: V0, x1: V1, x2: V2) => T8;
+
+        pipe<V0, T1, T2, T3, T4, T5, T6, T7, T8, T9>(
+            fn0: (x0: V0) => T1,
+            fn1: (x: T1) => T2,
+            fn2: (x: T2) => T3,
+            fn3: (x: T3) => T4,
+            fn4: (x: T4) => T5,
+            fn5: (x: T5) => T6,
+            fn6: (x: T6) => T7,
+            fn7: (x: T7) => T8,
+            fn8: (x: T8) => T9): (x0: V0) => T9;
+        pipe<V0, V1, T1, T2, T3, T4, T5, T6, T7, T8, T9>(
+            fn0: (x0: V0, x1: V1) => T1,
+            fn1: (x: T1) => T2,
+            fn2: (x: T2) => T3,
+            fn3: (x: T3) => T4,
+            fn4: (x: T4) => T5,
+            fn5: (x: T5) => T6,
+            fn6: (x: T6) => T7,
+            fn7: (x: T7) => T8,
+            fn8: (x: T8) => T9): (x0: V0, x1: V1) => T9;
+        pipe<V0, V1, V2, T1, T2, T3, T4, T5, T6, T7, T8, T9>(
+            fn0: (x0: V0, x1: V1, x2: V2) => T1,
+            fn1: (x: T1) => T2,
+            fn2: (x: T2) => T3,
+            fn3: (x: T3) => T4,
+            fn4: (x: T4) => T5,
+            fn5: (x: T5) => T6,
+            fn6: (x: T6) => T7,
+            fn7: (x: T7) => T8,
+            fn8: (x: T8) => T9): (x0: V0, x1: V1, x2: V2) => T9;
+
+        pipe<V0, T1, T2, T3, T4, T5, T6, T7, T8, T9, T10>(
+            fn0: (x0: V0) => T1,
+            fn1: (x: T1) => T2,
+            fn2: (x: T2) => T3,
+            fn3: (x: T3) => T4,
+            fn4: (x: T4) => T5,
+            fn5: (x: T5) => T6,
+            fn6: (x: T6) => T7,
+            fn7: (x: T7) => T8,
+            fn8: (x: T8) => T9,
+            fn9: (x: T9) => T10): (x0: V0) => T10;
+        pipe<V0, V1, T1, T2, T3, T4, T5, T6, T7, T8, T9, T10>(
+            fn0: (x0: V0, x1: V1) => T1,
+            fn1: (x: T1) => T2,
+            fn2: (x: T2) => T3,
+            fn3: (x: T3) => T4,
+            fn4: (x: T4) => T5,
+            fn5: (x: T5) => T6,
+            fn6: (x: T6) => T7,
+            fn7: (x: T7) => T8,
+            fn8: (x: T8) => T9,
+            fn9: (x: T9) => T10): (x0: V0, x1: V1) => T10;
+        pipe<V0, V1, V2, T1, T2, T3, T4, T5, T6, T7, T8, T9, T10>(
+            fn0: (x0: V0, x1: V1, x2: V2) => T1,
+            fn1: (x: T1) => T2,
+            fn2: (x: T2) => T3,
+            fn3: (x: T3) => T4,
+            fn4: (x: T4) => T5,
+            fn5: (x: T5) => T6,
+            fn6: (x: T6) => T7,
+            fn7: (x: T7) => T8,
+            fn8: (x: T8) => T9,
+            fn9: (x: T9) => T10): (x0: V0, x1: V1, x2: V2) => T10;
 
         /**
          * Returns a new list by plucking the same named property off all objects in the list supplied.
