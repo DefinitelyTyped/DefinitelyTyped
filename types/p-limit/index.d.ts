a573285132b9e5b50833bcd897089fe6a8175ae4
Dimitri Benin <BendingBender@users.noreply.github.com>
2017-08-08 22:53:36 +0200

    [p-limit] introduce typings (#18721)

---
+++ types/p-limit/index.d.ts
@@ -0,0 +1,8 @@
+// Type definitions for p-limit 1.1
+// Project: https://github.com/sindresorhus/p-limit#readme
+// Definitions by: BendingBender <https://github.com/BendingBender>
+// Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
+
+export = pLimit;
+
+declare function pLimit<T>(concurrency: number): (fn: () => PromiseLike<T>) => Promise<T>;
