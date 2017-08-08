cc629a8677208d7d00e52aed4a6f3849e39e705d
e-cloud <saintscott119@gmail.com>
2017-08-08 21:04:24 +0800

    feat(rimraf): add the missing options types

--- types/rimraf/index.d.ts
+++ types/rimraf/index.d.ts
@@ -5,11 +5,38 @@
 
 // Imported from: https://github.com/soywiz/typescript-node-definitions/rimraf.d.ts
 
+/// <reference types="node" />
+/// <reference types="glob" />
 
+import glob = require('glob');
+import fs = require('fs');
+
+declare function rimraf(path: string, options: rimraf.Options, callback: (error: Error) => void): void;
 declare function rimraf(path: string, callback: (error: Error) => void): void;
 declare namespace rimraf {
+    export function sync(path: string, options: rimraf.Options): void;
     export function sync(path: string): void;
     export var EMFILE_MAX: number;
     export var BUSYTRIES_MAX: number;
+    export interface Options {
+        maxBusyTries?: number;
+        emfileWait?: boolean;
+        disableGlob?: boolean;
+        glob?: glob.IOptions | false;
+
+        unlink?: typeof fs.unlink
+        chmod?: typeof fs.chmod
+        stat?: typeof fs.stat
+        lstat?: typeof fs.lstat
+        rmdir?: typeof fs.rmdir
+        readdir?: typeof fs.readdir
+
+        unlinkSync?: typeof fs.unlinkSync
+        chmodSync?: typeof fs.chmodSync
+        statSync?: typeof fs.statSync
+        lstatSync?: typeof fs.lstatSync
+        rmdirSync?: typeof fs.rmdirSync
+        readdirSync?: typeof fs.readdirSync
+    }
 }
 export = rimraf;
--- types/rimraf/rimraf-tests.ts
+++ types/rimraf/rimraf-tests.ts
@@ -6,5 +6,10 @@ rimraf('./xyz', (err: Error) => {
 });
 rimraf.sync('./xyz');
 
+rimraf('./xyz', { glob: { ignore: '' } }, (err: Error) => {
+
+});
+rimraf.sync('./xyz', { glob: { ignore: '' } });
+
 rimraf.EMFILE_MAX = 0;
 rimraf.BUSYTRIES_MAX = 0;
