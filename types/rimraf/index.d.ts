d116c4005187ca2f7498e84f01b7d6da608d3b33
e-cloud <saintscott119@gmail.com>
2017-08-08 21:24:30 +0800

    refactor: format code

--- types/rimraf/index.d.ts
+++ types/rimraf/index.d.ts
@@ -1,4 +1,4 @@
-// Type definitions for rimraf
+// Type definitions for rimraf 2.0
 // Project: https://github.com/isaacs/rimraf
 // Definitions by: Carlos Ballesteros Velasco <https://github.com/soywiz>, e-cloud <https://github.com/e-cloud>
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
@@ -6,7 +6,6 @@
 // Imported from: https://github.com/soywiz/typescript-node-definitions/rimraf.d.ts
 
 /// <reference types="node" />
-/// <reference types="glob" />
 
 import glob = require('glob');
 import fs = require('fs');
@@ -14,29 +13,28 @@ import fs = require('fs');
 declare function rimraf(path: string, options: rimraf.Options, callback: (error: Error) => void): void;
 declare function rimraf(path: string, callback: (error: Error) => void): void;
 declare namespace rimraf {
-    export function sync(path: string, options: rimraf.Options): void;
-    export function sync(path: string): void;
-    export var EMFILE_MAX: number;
-    export var BUSYTRIES_MAX: number;
-    export interface Options {
+    function sync(path: string, options?: rimraf.Options): void;
+    let EMFILE_MAX: number;
+    let BUSYTRIES_MAX: number;
+    interface Options {
         maxBusyTries?: number;
         emfileWait?: boolean;
         disableGlob?: boolean;
         glob?: glob.IOptions | false;
 
-        unlink?: typeof fs.unlink
-        chmod?: typeof fs.chmod
-        stat?: typeof fs.stat
-        lstat?: typeof fs.lstat
-        rmdir?: typeof fs.rmdir
-        readdir?: typeof fs.readdir
+        unlink?: typeof fs.unlink;
+        chmod?: typeof fs.chmod;
+        stat?: typeof fs.stat;
+        lstat?: typeof fs.lstat;
+        rmdir?: typeof fs.rmdir;
+        readdir?: typeof fs.readdir;
 
-        unlinkSync?: typeof fs.unlinkSync
-        chmodSync?: typeof fs.chmodSync
-        statSync?: typeof fs.statSync
-        lstatSync?: typeof fs.lstatSync
-        rmdirSync?: typeof fs.rmdirSync
-        readdirSync?: typeof fs.readdirSync
+        unlinkSync?: typeof fs.unlinkSync;
+        chmodSync?: typeof fs.chmodSync;
+        statSync?: typeof fs.statSync;
+        lstatSync?: typeof fs.lstatSync;
+        rmdirSync?: typeof fs.rmdirSync;
+        readdirSync?: typeof fs.readdirSync;
     }
 }
 export = rimraf;
