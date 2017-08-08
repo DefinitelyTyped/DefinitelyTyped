b2afe5c2ae7a0e33a84b2b63237ff3da79fab008
Dimitri Benin <dimitri.benin@it-designers.de>
2017-08-08 13:24:34 +0200

    [cpy] update typings to 5.1; [cp-file] add typings

--- types/cpy/index.d.ts
+++ types/cpy/index.d.ts
@@ -1,10 +1,31 @@
-// Type definitions for cpy 5.0
+// Type definitions for cpy 5.1
 // Project: https://github.com/sindresorhus/cpy#readme
 // Definitions by: Mohamed Hegazy <https://github.com/mhegazy>
+//                 BendingBender <https://github.com/BendingBender>
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
 
+import glob = require('glob');
+import cpFile = require('cp-file');
+
 export = cpy;
-declare function cpy(
-    src: string | string[],
-    dest: string,
-    opts?: { cwd?: string, parents?: boolean, rename?(s: string): string }): Promise<void>;
+declare function cpy(files: string | string[], destination: string, opts?: cpy.Options): Promise<void> & cpy.ProgressEmitter;
+
+declare namespace cpy {
+    interface ProgressEmitter {
+        on(event: 'progress', handler: (progress: ProgressData) => void): Promise<void>;
+    }
+
+    type Options = CpyOptions & glob.IOptions & cpFile.Options;
+
+    interface CpyOptions {
+        cwd?: string;
+        parents?: boolean;
+        rename?: string | ((basename: string) => string);
+    }
+
+    interface ProgressData {
+        completedFiles: number;
+        totalFiles: number;
+        completedSize: number;
+    }
+}
