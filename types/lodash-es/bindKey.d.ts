19fb606bb4b8b8051b7c3072994953f76a5604c5
Andy Hanson <anhans@microsoft.com>
2017-08-08 08:54:08 -0700

    Use `lodash/scripts/generate-modules.ts` to generate `lodash-es` too

---
+++ types/lodash-es/bindKey.d.ts
@@ -0,0 +1,2 @@
+import { bindKey } from "lodash";
+export default bindKey;
--- types/lodash-es/sortedIndex/index.d.ts
+++
@@ -1,3 +0,0 @@
-import * as _ from "lodash";
-declare const sortedIndex: typeof _.sortedIndex;
-export default sortedIndex;
