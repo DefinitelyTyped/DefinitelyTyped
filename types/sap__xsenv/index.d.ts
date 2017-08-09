8c9bd290cf499744e8f7722c9b3d8f6d3acfb408
Michael Müller <michael.mueller01@sap.com>
2017-08-09 10:51:52 +0200

    Fix linter message

--- types/sap__xsenv/index.d.ts
+++ types/sap__xsenv/index.d.ts
@@ -8,7 +8,7 @@
 export function loadEnv(jsonFile?: string): void;
 export function readCFServices(): any;
 
-type ServiceFilter = string | { name?: string; label?: string; tag?: string; plan?: string } | ((service: any) => boolean);
+export type ServiceFilter = string | { name?: string; label?: string; tag?: string; plan?: string } | ((service: any) => boolean);
 
 /**
  * Reads service configuration from CloudFoundry environment variable <code>VCAP_SERVICES</code>.
