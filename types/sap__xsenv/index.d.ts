53d2b433ac0c937624b4dd848dcaed6197d1fbcd
Michael Müller <michael.mueller01@sap.com>
2017-08-09 10:05:20 +0200

    Rework links to documentation

--- types/sap__xsenv/index.d.ts
+++ types/sap__xsenv/index.d.ts
@@ -1,7 +1,9 @@
 // Type definitions for @sap/xsenv 1.2
-// Project: https://npm.sap.com/@sap/xsenv
+// Project: https://help.sap.com/hana_platform
 // Definitions by: [Michael Müller] <https://github.com/mad-mike>
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
+// For more information see the Developer Guide for SAP HANA XS Advanced Model run time.
+// The module is available on the https://npm.sap.com registry.
 
 export function loadEnv(jsonFile?: string): void;
 export function readCFServices(): any;
