2f570a98c38d6d288846aadfb64c54fe3cfdd645
Björn Lundström <bjoern.lundstroem@brockhaus-ag.de>
2017-08-10 08:04:52 +0200

    Revert "- Add new function in Namespace : ExtractJwt.fromAuthHeaderAsBearerToken()"
    This reverts commit 53d7a3e84d73c5cabbc5d7644d411d08699479e9.

--- types/passport-jwt/index.d.ts
+++ types/passport-jwt/index.d.ts
@@ -1,14 +1,15 @@
-// Type definitions for passport-jwt 2.2.1
+// Type definitions for passport-jwt 2.0
 // Project: https://github.com/themikenicholson/passport-jwt
 // Definitions by: TANAKA Koichi <https://github.com/mugeso/>
 //                 Alex Young <https://github.com/alsiola/>
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
 
 import { Strategy as PassportStrategy } from 'passport-strategy';
-import { Request } from 'express';
+import {Request} from 'express';
 
-export class Strategy extends PassportStrategy {
-    constructor(opt: StrategyOptions, verify: VerifyCallback | VerifyCallbackWithRequest );
+export declare class Strategy extends PassportStrategy {
+    constructor(opt: StrategyOptions, verify: VerifyCallback);
+    constructor(opt: StrategyOptions, verify: VerifyCallbackWithRequest);
 }
 
 export interface StrategyOptions {
@@ -21,17 +22,27 @@ export interface StrategyOptions {
     passReqToCallback?: boolean;
 }
 
-export type VerifyCallback = (payload: any, done: VerifiedCallback) => void;
-export type VerifyCallbackWithRequest = (req: Request, payload: any, done: VerifiedCallback) => void;
-export type VerifiedCallback = (error: any, user?: any, info?: any) => void;
-export type JwtFromRequestFunction = (req: Request) => string;
+export interface VerifyCallback {
+    (payload: any, done: VerifiedCallback): void;
+}
 
-export namespace ExtractJwt {
-    function fromHeader(header_name: string): JwtFromRequestFunction;
-    function fromBodyField(field_name: string): JwtFromRequestFunction;
-    function fromUrlQueryParameter(param_name: string): JwtFromRequestFunction;
-    function fromAuthHeaderWithScheme(auth_scheme: string): JwtFromRequestFunction;
-    function fromAuthHeader(): JwtFromRequestFunction;
-    function fromAuthHeaderAsBearerToken(): JwtFromRequestFunction;
-    function fromExtractors(extractors: JwtFromRequestFunction[]): JwtFromRequestFunction;
+export interface VerifyCallbackWithRequest {
+    (req: Request, payload: any, done: VerifiedCallback): void;
+}
+
+export interface VerifiedCallback {
+    (error: any, user?: any, info?: any): void;
+}
+
+export interface JwtFromRequestFunction {
+    (req: Request): string;
+}
+
+export declare namespace ExtractJwt {
+    export function fromHeader(header_name: string): JwtFromRequestFunction;
+    export function fromBodyField(field_name: string): JwtFromRequestFunction;
+    export function fromUrlQueryParameter(param_name: string): JwtFromRequestFunction;
+    export function fromAuthHeaderWithScheme(auth_scheme: string): JwtFromRequestFunction;
+    export function fromAuthHeader(): JwtFromRequestFunction;
+    export function fromExtractors(extractors: JwtFromRequestFunction[]): JwtFromRequestFunction;
 }
