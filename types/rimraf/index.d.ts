01e6067cf642a2b94b954f6976422f2e195ecdcf
Ruben Schmidmeister <ruben@hausformat.com>
2017-08-10 09:49:57 +0200

    Add util.promisify types for rimraf

--- types/rimraf/index.d.ts
+++ types/rimraf/index.d.ts
@@ -1,6 +1,8 @@
 // Type definitions for rimraf 2.0
 // Project: https://github.com/isaacs/rimraf
-// Definitions by: Carlos Ballesteros Velasco <https://github.com/soywiz>, e-cloud <https://github.com/e-cloud>
+// Definitions by: Carlos Ballesteros Velasco <https://github.com/soywiz>
+//                 e-cloud <https://github.com/e-cloud>
+//                 Ruben Schmidmeister <https://github.com/bash>
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
 
 // Imported from: https://github.com/soywiz/typescript-node-definitions/rimraf.d.ts
@@ -13,6 +15,7 @@ import fs = require('fs');
 declare function rimraf(path: string, options: rimraf.Options, callback: (error: Error) => void): void;
 declare function rimraf(path: string, callback: (error: Error) => void): void;
 declare namespace rimraf {
+    function __promisify__(path: string, options?: rimraf.Options): Promise<void>;
     function sync(path: string, options?: rimraf.Options): void;
     let EMFILE_MAX: number;
     let BUSYTRIES_MAX: number;
