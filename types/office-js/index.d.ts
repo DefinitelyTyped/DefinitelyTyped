19d96c3e3a930faa189e06aa91f32eb90f61613c
Ricky Kirkham <rickki@microsoft.com>
2017-08-10 17:09:08 -0700

    New property in AuthOptions type

--- types/office-js/index.d.ts
+++ types/office-js/index.d.ts
@@ -203,6 +203,10 @@ declare namespace Office {
          */
         forceAddAccount?: boolean,
         /**
+         * Optional. Causes Office to prompt the user to provide the additional factor when the tenancy being targeted by Microsoft Graph requires multifactor authentication. The string value identifies the type of additional factor that is required. In most cases, you won't know at development time whether the user's tenant requires an additional factor or what the string should be. So this option would be used in a "second try" call of getAccessTokenAsync after Microsoft Graph has sent an error requesting the additional factor and containing the string that should be used with the authChallenge option.  
+         */
+        authChallenge?: string
+        /**
          * Optional. A user-defined item of any type that is returned in the AsyncResult object without being altered.
          */
         asyncContext?: any
