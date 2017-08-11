bfe223c48f571de188adbe4946331a159c411307
Meno Abels <meno.abels@adviser.com>
2017-08-11 21:55:20 +0200

    * make the test running again

--- types/rxjs-exec/rxjs-exec-tests.ts
+++ types/rxjs-exec/rxjs-exec-tests.ts
@@ -4,5 +4,5 @@ import * as RxJs from 'rxjs';
 const _exec = RxJsExec.exec("", {binary: true});
 const _execFilter = RxJsExec.execFilter("");
 const _mapeExecr = RxJsExec.mapExec("", {input: "String"});
-const _mapExecFilter = RxJsExec.mapExecFilter("", {stdin: process.stdin});
+const _mapExecFilter = RxJsExec.mapExecFilter("", {stdin: true});
 // RxJsExec.patch(RxJs);
