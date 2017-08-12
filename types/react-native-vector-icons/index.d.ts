f83fda4486bc103b222a8274c7b4d019647ea0f9
Tim Wang <timwangdev@users.noreply.github.com>
2017-08-12 23:05:51 +0800

    Bump up version to 4.3

--- types/react-native-vector-icons/index.d.ts
+++ types/react-native-vector-icons/index.d.ts
@@ -1,6 +1,7 @@
-// Type definitions for react-native-vector-icons 4.2
+// Type definitions for react-native-vector-icons 4.3
 // Project: https://github.com/oblador/react-native-vector-icons
 // Definitions by: Kyle Roach <https://github.com/iRoachie>
+//                 Tim Wang <https://github.com/timwangdev>
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
 // TypeScript Version: 2.3
 
