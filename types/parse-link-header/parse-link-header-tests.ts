a474a9250b9c43525bff729d10c0b68f9e20cec7
Nick Zelei <nickzelei@gmail.com>
2017-08-15 16:02:26 -0500

    Update parse-link-header-tests.ts
    updated test to include null type.

--- types/parse-link-header/parse-link-header-tests.ts
+++ types/parse-link-header/parse-link-header-tests.ts
@@ -5,4 +5,4 @@ const linkHeader =
     '<https://api.github.com/user/9287/repos?page=1&per_page=100>; rel="prev"; pet="cat", ' +
     '<https://api.github.com/user/9287/repos?page=5&per_page=100>; rel="last"';
 
-const links: parse.Links = parse(linkHeader);
+const links: parse.Links | null = parse(linkHeader);
