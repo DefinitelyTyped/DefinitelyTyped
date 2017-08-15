bbf02108e1beddcbc577ccc89d481bb1ebf71f0c
Aditya <aditya@biowink.com>
2017-08-15 15:09:26 +0200

    client-sessions: add definitions

---
+++ types/client-sessions/client-sessions-tests.ts
@@ -0,0 +1,25 @@
+import * as express from "express";
+import * as session from "client-sessions";
+
+const secret = "yolo";
+const app = express();
+const options = { secret };
+
+let middleware = session(options);
+middleware = session({ secret, cookieName: "_s" });
+middleware = session({ secret, duration: 600000 });
+middleware = session({ secret, activeDuration: 42 });
+middleware = session({
+  secret,
+  cookie: {
+    httpOnly: false,
+  }
+});
+
+app.use(middleware);
+app.use((req: any, res: any) => {
+  req.session = { test: true };
+});
+
+const encoded = session.util.encode(options, { test: true });
+session.util.decode(options, encoded);
