ba5a8e8c1555ea0438cbab249725025b3344991b
Muhammad Ragib Hasin <ragib.hasin@gmail.com>
2017-08-15 18:44:53 +0600

    [intl] add typings

---
+++ types/intl/index.d.ts
@@ -0,0 +1,6 @@
+// Type definitions for intl v1.2.x
+// Project: https://github.com/stiang/remove-markdown
+// Definitions by: Muhammad Ragib Hasin <https://github.com/RagibHasin>
+// Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
+
+export = Intl
