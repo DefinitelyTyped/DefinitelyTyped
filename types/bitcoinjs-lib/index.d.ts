adaa7ac401bb71a968df805c798a3e704dcb7141
Daniel Lebrecht <dlebrecht@gmail.com>
2017-08-15 09:18:17 +0200

    fixed bugs following @dcousens review

--- types/bitcoinjs-lib/index.d.ts
+++ types/bitcoinjs-lib/index.d.ts
@@ -208,7 +208,7 @@ export class TransactionBuilder {
 
     addInput(txhash: Buffer | string | Transaction, vout: number, sequence?: number, prevOutScript?: Buffer): number;
 
-    addOutput(scriptPubKey: Buffer | string, value: number): number;
+    addOutput(scriptPubKey: Buffer, value: number): number;
 
     build(): Transaction;
 
@@ -350,7 +350,7 @@ export const opcodes: {
 };
 
 export namespace address {
-    function fromBase58Check(address: string): Buffer;
+    function fromBase58Check(address: string): { hash: Buffer, version: number };
 
     function fromOutputScript(outputScript: Buffer, network?: Network): Buffer;
 
@@ -466,7 +466,7 @@ export namespace script {
         output: {
             check(script: Buffer): boolean;
             decode(buffer: Buffer): Buffer;
-            encode(pubKeyHash: Buffer | number): Buffer;
+            encode(pubKeyHash: Buffer): Buffer;
         };
     };
 
@@ -504,7 +504,7 @@ export namespace script {
         output: {
             check(script: Buffer): boolean;
             decode(buffer: Buffer): Buffer;
-            encode(pubKeyHash: Buffer | number): Buffer;
+            encode(pubKeyHash: Buffer): Buffer;
         };
     };
 
