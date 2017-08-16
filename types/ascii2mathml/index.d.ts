4564d6a5ebb4689259d549027dc57fed5f710b51
Muhammad Ragib Hasin <ragib.hasin@gmail.com>
2017-08-16 09:08:21 +0600

    [ascii2mathml] fix test error

--- types/ascii2mathml/index.d.ts
+++ types/ascii2mathml/index.d.ts
@@ -7,36 +7,29 @@ export = A2MML;
 
 declare var A2MML: ascii2mathml;
 
-interface ascii2mathml {
-    /**
-     * Converts ASCIIMath expression to MathML markup.
-     * @param asciimath {string} ASCIIMath expression
-     * @param options Options
-     */
-    (asciimath: string, options?: {
-        decimalMark?: string
-        colSep?: string
-        rowSep?: string
-        display?: 'inline' | 'block'
-        dir?: 'ltr' | 'rtl'
-        bare?: boolean
-        standalone?: boolean
-        annotate?: boolean
-    }): string;
+interface Options {
+    decimalMark?: string;
+    colSep?: string;
+    rowSep?: string;
+    display?: 'inline' | 'block';
+    dir?: 'ltr' | 'rtl';
+    bare?: boolean;
+    standalone?: boolean;
+    annotate?: boolean;
+}
 
+interface ascii2mathml {
     /**
      * Generates a function with default options set to convert
      * ASCIIMath expression to MathML markup.
      * @param options Options
      */
-    (options: {
-        decimalMark?: string
-        colSep?: string
-        rowSep?: string
-        display?: 'inline' | 'block'
-        dir?: 'ltr' | 'rtl'
-        bare?: boolean
-        standalone?: boolean
-        annotate?: boolean
-    }): ascii2mathml;
+    (options: Options): ascii2mathml;
+
+    /**
+     * Converts ASCIIMath expression to MathML markup.
+     * @param asciimath {string} ASCIIMath expression
+     * @param options Options
+     */
+    (asciimath: string, options?: Options): string;
 }
