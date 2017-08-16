9b68089f2fe12a3fd9f9ca4e1e5ae39f8bfb6a7e
Bijan Vakili <bijanvakili@users.noreply.github.com>
2017-08-16 00:51:07 -0700

    Fix webpack WatchIgnorePlugin constructor arguments to allow providing an array of string paths

--- types/webpack/index.d.ts
+++ types/webpack/index.d.ts
@@ -919,7 +919,7 @@ declare namespace webpack {
     }
 
     class WatchIgnorePlugin extends Plugin {
-        constructor(paths: RegExp[]);
+        constructor(paths: string[] | RegExp[]);
     }
 
     namespace optimize {
