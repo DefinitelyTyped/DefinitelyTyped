613daf6bcad03d73c601e8dad2a7a2d9d846d8ed
Nicolas Penin <nicolas.penin@dragon-angel.fr>
2017-08-17 19:52:20 +0200

    fixed tslint issues

--- types/sequencify/index.d.ts
+++ types/sequencify/index.d.ts
@@ -1,16 +1,22 @@
-// Type definitions for sequencify v0.0
+// Type definitions for sequencify 0.0
 // Project: https://github.com/robrich/sequencify
 // Definitions by: Nicolas Penin <https://github.com/npenin>
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
 
 // Definition file started by dts-gen
+// TypeScript Version: 2.1
 
 export = sequencify;
 
 declare namespace sequencify {
-    export type Task = { name: string, dep: string[] };
+    interface Task {
+        name: string;
+        dep: string[];
+    }
 
-    export type TaskMap = { [name: string]: Task }
+    interface TaskMap {
+        [name: string]: Task;
+    }
 }
 
-declare function sequencify(tasks: sequencify.TaskMap, names: (keyof sequencify.TaskMap)[], results: string[], nest?: string[]): void;
+declare function sequencify(tasks: sequencify.TaskMap, names: Array<keyof sequencify.TaskMap>, results: string[], nest?: string[]): void;
