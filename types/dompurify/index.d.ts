183d6078943727e98cd68cb8df4d2a7eac918461
falsandtru <falsandtru@users.noreply.github.com>
2017-08-17 23:47:18 +0900

    apply code formatting

--- types/dompurify/index.d.ts
+++ types/dompurify/index.d.ts
@@ -4,31 +4,33 @@
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
 
 interface IDOMPurify {
-  sanitize(s:string):string;
-  sanitize(s:string, config:IDOMPurifyConfig):any;
+    sanitize(s: string): string;
+    sanitize(s: string, config: IDOMPurifyConfig & { RETURN_DOM: true }): HTMLBodyElement;
+    sanitize(s: string, config: IDOMPurifyConfig & { RETURN_DOM_FRAGMENT: true }): DocumentFragment;
+    sanitize<T extends string | HTMLBodyElement | DocumentFragment>(s: string, config: IDOMPurifyConfig): T;
 
-  addHook(hook:string, cb:(currentNode:Element, data:any, config:IDOMPurifyConfig) => Element):void;
+    addHook(hook: string, cb: (currentNode: Element, data: any, config: IDOMPurifyConfig) => Element): void;
 }
 
 interface IDOMPurifyConfig {
-  ADD_ATTR?:string[];
-  ADD_TAGS?:string[];
-  ALLOW_DATA_ATTR?:boolean;
-  ALLOWED_ATTR?:string[];
-  ALLOWED_TAGS?:string[];
-  FORBID_ATTR?:string[];
-  FORBID_TAGS?:string[];
-  KEEP_CONTENT?:boolean;
-  RETURN_DOM?:boolean;
-  RETURN_DOM_FRAGMENT?:boolean;
-  RETURN_DOM_IMPORT?:boolean;
-  SAFE_FOR_JQUERY?:boolean;
-  SANITIZE_DOM?:boolean;
-  WHOLE_DOCUMENT?:boolean;
+    ADD_ATTR?: string[];
+    ADD_TAGS?: string[];
+    ALLOW_DATA_ATTR?: boolean;
+    ALLOWED_ATTR?: string[];
+    ALLOWED_TAGS?: string[];
+    FORBID_ATTR?: string[];
+    FORBID_TAGS?: string[];
+    KEEP_CONTENT?: boolean;
+    RETURN_DOM?: boolean;
+    RETURN_DOM_FRAGMENT?: boolean;
+    RETURN_DOM_IMPORT?: boolean;
+    SAFE_FOR_JQUERY?: boolean;
+    SANITIZE_DOM?: boolean;
+    WHOLE_DOCUMENT?: boolean;
 }
 
-declare var DOMPurify:IDOMPurify;
+declare var DOMPurify: IDOMPurify;
 
 declare module 'dompurify' {
-  export = DOMPurify;
+    export = DOMPurify;
 }
