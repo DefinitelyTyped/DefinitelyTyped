460bc42e7523ee8140eeec38e98a2a2461f206df
Brendan Forster <brendan@github.com>
2017-08-21 12:20:43 +1000

    add another reviewer

--- types/mri/index.d.ts
+++ types/mri/index.d.ts
@@ -1,6 +1,6 @@
 // Type definitions for mri 1.1
 // Project: https://github.com/lukeed/mri
-// Definitions by: Brendan Forster <https://github.com/shiftkey>
+// Definitions by: Brendan Forster <https://github.com/shiftkey>, Jed Fox <https://github.com/j-f1>
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
 // TypeScript Version: 2.4
 
