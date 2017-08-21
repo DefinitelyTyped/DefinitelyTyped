cd0eff9476dbe391e5adb27ace4db369e615e27e
Brendan Forster <brendan@github.com>
2017-08-21 11:50:25 +1000

    scaffolding package for mri

---
+++ types/mri/index.d.ts
@@ -0,0 +1,13 @@
+// Type definitions for mri 1.1
+// Project: https://github.com/lukeed/mri
+// Definitions by: Brendan Forster <https://github.com/shiftkey>
+// Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
+
+export = mri;
+
+declare function mri(args: any, opts: any): any;
+
+declare namespace mri {
+    const prototype: {
+    };
+}
