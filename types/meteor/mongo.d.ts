36735d136a29a107600a658bf15e02a941c2bff6
Andy <anhans@microsoft.com>
2017-08-22 10:57:26 -0700

    meteor: Fix compile errors (#19075)

--- types/meteor/mongo.d.ts
+++ types/meteor/mongo.d.ts
@@ -204,7 +204,7 @@ declare module "meteor/mongo" {
         interface Cursor<T> {
             count(applySkipLimit?: boolean): number;
             fetch(): Array<T>;
-            forEach(callback: < T > (doc: T, index: number, cursor: Cursor<T>) => void, thisArg?: any): void;
+            forEach(callback: (doc: T, index: number, cursor: Cursor<T>) => void, thisArg?: any): void;
             map<U>(callback: (doc: T, index: number, cursor: Cursor<T>) => U, thisArg?: any): Array<U>;
             observe(callbacks: ObserveCallbacks): Meteor.LiveQueryHandle;
             observeChanges(callbacks: ObserveChangesCallbacks): Meteor.LiveQueryHandle;
