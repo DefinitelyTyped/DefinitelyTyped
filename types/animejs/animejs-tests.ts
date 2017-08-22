f283c0ba4eebf2dad155dc0bd7e05913da750d9c
Andy <anhans@microsoft.com>
2017-08-22 13:36:35 -0700

    animejs: Lint (#19252)

--- types/animejs/animejs-tests.ts
+++ types/animejs/animejs-tests.ts
@@ -4,17 +4,15 @@ const test1 = anime({
 	targets: 'div',
 	duration: 40,
 	color: "#FFFFFF"
-})
+});
 
-
-
-const callback = (anim : any) => {
+const callback = (anim: any) => {
 	console.log(anim.completed);
-}
+};
 
 const test2 = anime({
 	targets: 'div',
-	translateX: (el : HTMLElement, i : number, index : number) => {
+	translateX: (el: HTMLElement, i: number, index: number) => {
 		return 0;
 	},
 	translateY: '40px',
@@ -28,26 +26,26 @@ const test2 = anime({
 	},
 	update: callback,
 	complete: callback
-})
+});
 
 const someNodes = document.querySelector('button');
 
 const test3 = anime({
 	targets: someNodes,
 	top: "-5000000em"
-})
+});
 
 const tl = anime.timeline({
 	loop: false,
 	direction: 'normal'
-})
+});
 
 tl.add({
 	targets: ".tiny-divvy-div",
 	scale: 10000000
-})
+});
 
-var path = anime.path('#motionPath path');
+const path = anime.path('#motionPath path');
 
 test1.play();
 test2.reverse();
@@ -56,14 +54,14 @@ tl.seek(4000);
 
 tl.finished.then(() => {
 	console.log("I wonder if anyone will ever actually read this.");
-})
+});
 
-let usesEnums = anime({
+const usesEnums = anime({
 	targets: ".usingEnumsIsAReallyHandyThing",
-	direction: anime.DirectionEnum.Reverse,
-	easing: anime.EasingsEnum.InOutExpo,
+	direction: "reverse",
+	easing: "inoutexpo",
 	someProperty: "+=4000"
-})
+});
 
 const bezier = anime.bezier(0, 0, 100, 100);
 // anime.speed = 100000000;
