a28000e94014af514a3e6cab9193187dbb0eb00f
Daniel Rosenwasser <DanielRosenwasser@users.noreply.github.com>
2017-08-21 21:21:53 -0700

    Fix up linting issue regarding array types.

--- types/webpack/index.d.ts
+++ types/webpack/index.d.ts
@@ -919,7 +919,7 @@ declare namespace webpack {
     }
 
     class WatchIgnorePlugin extends Plugin {
-        constructor(paths: (string | RegExp)[]);
+        constructor(paths: Array<string | RegExp>);
     }
 
     namespace optimize {
