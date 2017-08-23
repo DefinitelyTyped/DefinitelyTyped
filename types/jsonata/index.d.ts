9f1f82caeab7d9a30bdd8dd190b07bc77ac31f62
nick121212 <xiao.feng695@chinaredstar.com>
2017-08-23 18:55:55 +0800

    add defined for jsonata

---
+++ types/jsonata/index.d.ts
@@ -0,0 +1,21 @@
+// Type definitions for jsonata 1.3
+// Project: https://github.com/jsonata-js/jsonata
+// Definitions by: Nick <https://github.com/nick121212>
+// Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
+
+// declare class jsonata1 {
+//     constructor(str: string);
+//     assign(key: string, func: (...args: any[]) => any): any;
+
+//     evaluate(input: any, bindings?: any, callback?: () => any): any;
+// }
+
+declare function jsonata(str: string): jsonata.Expression;
+declare namespace jsonata {
+    interface Expression {
+        evaluate(input: any, bindings?: any, callback?: () => any): any;
+        assign(key: string, func: (...args: any[]) => any): void;
+    }
+}
+
+export = jsonata;
