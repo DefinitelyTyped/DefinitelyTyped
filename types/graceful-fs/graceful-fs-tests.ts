1b5128e44a372ecc0dc34f79aa62aa34bbdc39c4
Dimitri Benin <dimitri.benin@it-designers.de>
2017-08-23 02:21:47 +0200

    [graceful-fs] upgrade to 4.1, improve typings, enable strict null checks and linting

--- types/graceful-fs/graceful-fs-tests.ts
+++ types/graceful-fs/graceful-fs-tests.ts
@@ -1,7 +1,25 @@
-
 import fs = require('graceful-fs');
+import * as fs2 from 'graceful-fs';
+import * as nodeFs from 'fs';
+import * as fse from 'fs-extra';
+import { promisify } from 'util';
 
-var str: string;
-var buf: Buffer;
+const str = '';
+const buf = new Buffer('');
 
 fs.renameSync(str, str);
+fs2.chmodSync(buf, 1);
+
+const gracefulified = fs.gracefulify(nodeFs);
+gracefulified; // $ExpectType typeof "fs"
+gracefulified.lutimes; // $ExpectType typeof lutimes
+
+const fseGrace = fs.gracefulify(fse);
+fseGrace.lutimes; // $ExpectType typeof lutimes
+
+nodeFs.lutimes(buf, str, str);
+nodeFs.lutimes(buf, str, str, err => {
+    err; // $ExpectType ErrnoException | null
+});
+nodeFs.lutimesSync(buf, str, str);
+promisify(nodeFs.lutimes); // $ExpectType (path: string | Buffer | URL, atime: string | number | Date, mtime: string | number | Date) => Promise<void>
--- types/graceful-fs/index.d.ts
+++ types/graceful-fs/index.d.ts
@@ -1,10 +1,61 @@
-// Type definitions for graceful-fs 2.0.0
+// Type definitions for graceful-fs 4.1
 // Project: https://github.com/cowboy/graceful-fs
 // Definitions by: Bart van der Schoor <https://github.com/Bartvds>
+//                 BendingBender <https://github.com/BendingBender>
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
 
 /// <reference types="node" />
 
+declare module 'graceful-fs' {
+    import * as nodeFs from 'fs';
 
-import fs = require('fs');
-export = fs;
+    export = gracefulFs;
+
+    const gracefulFs: gracefulFs & typeof nodeFs;
+
+    interface gracefulFs {
+        gracefulify<T>(fsModule: T): T & typeof nodeFs;
+    }
+}
+
+declare module 'fs' {
+    import { URL } from 'url';
+
+    /**
+     * Asynchronously change file timestamps of the file referenced by the supplied path.
+     * If path refers to a symbolic link, then the link is not dereferenced: instead, the timestamps
+     * of the symbolic link are changed.
+     * @param path A path to a file. If a URL is provided, it must use the `file:` protocol.
+     * @param atime The last access time. If a string is provided, it will be coerced to number.
+     * @param mtime The last modified time. If a string is provided, it will be coerced to number.
+     */
+    function lutimes(
+        path: string | Buffer | URL,
+        atime: string | number | Date,
+        mtime: string | number | Date,
+        callback?: (err: NodeJS.ErrnoException | null) => void
+    ): void;
+
+    // NOTE: This namespace provides design-time support for util.promisify. Exported members do not exist at runtime.
+    namespace lutimes {
+        /**
+         * Asynchronously change file timestamps of the file referenced by the supplied path.
+         * If path refers to a symbolic link, then the link is not dereferenced: instead, the timestamps
+         * of the symbolic link are changed.
+         * @param path A path to a file. If a URL is provided, it must use the `file:` protocol.
+         * @param atime The last access time. If a string is provided, it will be coerced to number.
+         * @param mtime The last modified time. If a string is provided, it will be coerced to number.
+         */
+        function __promisify__(path: string | Buffer | URL, atime: string | number | Date, mtime: string | number | Date): Promise<void>;
+    }
+
+    /**
+     * Synchronously change file timestamps of the file referenced by the supplied path.
+     * If path refers to a symbolic link, then the link is not dereferenced: instead, the timestamps
+     * of the symbolic link are changed.
+     * @param path A path to a file. If a URL is provided, it must use the `file:` protocol.
+     * @param atime The last access time. If a string is provided, it will be coerced to number.
+     * @param mtime The last modified time. If a string is provided, it will be coerced to number.
+     */
+    function lutimesSync(path: string | Buffer | URL, atime: string | number | Date, mtime: string | number | Date): void;
+}
