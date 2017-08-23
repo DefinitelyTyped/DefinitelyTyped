0cc939d536c0c7252bb46f56da68197cf27092e1
TotPeRo <www.totpe.ro@gmail.com>
2017-08-23 20:56:26 +0300

    Update waitMe-tests.ts

--- types/waitMe/waitMe-tests.ts
+++ types/waitMe/waitMe-tests.ts
@@ -1,30 +1,23 @@
-//import $ = require('jquery');
+ $("#container").waitMe();
 
-$(() => {
-    // examples from http://vadimsva.github.io/waitMe/
+$('#container').waitMe({});
 
-    $("#container").waitMe();
+$("#container").waitMe({ effect: "none", text: "" });
 
-    $('#container').waitMe({});
+$('#container').waitMe({
+    effect: 'bounce',
+    text: '',
+    bg: 'rgba(255, 255, 255, 0.7)',
+    color: "#000",
+    maxSize: '',
+    textPos: 'vertical',
+    fontSize: '',
+    source: ''
+});
 
-    $("#container").waitMe({ effect: "none", text: "" });
-
-    $('#container').waitMe({
-        effect: 'bounce',
-        text: '',
-        bg: 'rgba(255, 255, 255, 0.7)',
-        color: "#000",
-        maxSize: '',
-        textPos: 'vertical',
-        fontSize: '',
-        source: ''
+$('#container').waitMe()
+    .on('close', (e) => {
+            //console.log(e);
     });
 
-    $('#container').waitMe()
-        .on('close', (e) => {
-                //console.log(e);
-        });
-
-    $("asdsa").waitMe("hide");
-
-});
+$("asdsa").waitMe("hide");
