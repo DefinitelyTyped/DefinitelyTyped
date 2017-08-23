7651949914b0f202c361835883371ac38012b245
TotPeRo <www.totpe.ro@gmail.com>
2017-08-23 19:44:22 +0300

    added waitMe

---
+++ types/waitMe/waitMe-tests.ts
@@ -0,0 +1,31 @@
+﻿//import $ = require('jquery');
+
+$(() => {
+    // examples from http://vadimsva.github.io/waitMe/
+
+    $("#container").waitMe();
+
+    $('#container').waitMe({});
+
+    $("#container").waitMe({ effect: "none", text: "" });
+
+    $('#container').waitMe({
+        effect: 'bounce',
+        text: '',
+        bg: 'rgba(255, 255, 255, 0.7)',
+        color: "#000",
+        maxSize: '',
+        textPos: 'vertical',
+        fontSize: '',
+        source: ''
+    });
+
+    $('#container').waitMe()
+        .on('close', (e) => {
+                //console.log(e);
+        });
+
+    $("asdsa").waitMe("hide");
+
+
+});
