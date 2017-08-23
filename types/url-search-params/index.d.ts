d180389a26e11f53a9cff330e76636d379fd0277
nick121212 <xiao.feng695@chinaredstar.com>
2017-08-23 15:17:17 +0800

    add package url-search-params

---
+++ types/url-search-params/index.d.ts
@@ -0,0 +1,25 @@
+// Type definitions for url-search-params 0.10
+// Project: https://github.com/WebReflection/url-search-params
+// Definitions by: Nick <https://github.com/nick121212>
+// Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
+
+interface URLSearchParams {
+    append(name: string, value: any): void;
+    delete(name: string): void;
+    entries(pointer: string): Iterator<string>;
+    get(name: string): string;
+    getAll(): string[];
+    has(name: string): boolean;
+    keys(): Iterator<string>;
+    set(name: string, value: any): void;
+    values(pointer: string): Iterator<string>;
+}
+
+interface URLSearchParamsConstructor {
+    prototype: URLSearchParams;
+    new(init?: string | URLSearchParams | any): URLSearchParams;
+}
+
+declare const URLSearchParams: URLSearchParamsConstructor;
+
+export = URLSearchParams;
