b0610e27930e3cc2a7c7de6c91bfe0116ad973d1
TotPeRo <www.totpe.ro@gmail.com>
2017-08-23 19:42:25 +0300

    added jquery.filterTable

---
+++ types/jquery.filtertable/jquery.filtertable-tests.ts
@@ -0,0 +1,16 @@
+﻿//import $ = require('jquery');
+
+$(() => {
+    // examples from http://sunnywalker.github.io/jQuery.FilterTable/
+
+    $('table').filterTable();
+
+    $('table').filterTable({ quickList: ['class', 'tag'] });
+
+    $('table').filterTable({
+        callback(term, table) {
+            table.find('tr').removeClass('striped').filter(':visible:even').addClass('striped');
+        }
+    });
+
+});
