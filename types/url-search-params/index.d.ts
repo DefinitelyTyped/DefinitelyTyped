bc1d1418c1748ff41dab7a978151b77d80ebe3c9
nick121212 <xiao.feng695@chinaredstar.com>
2017-08-23 15:52:28 +0800

    change defined for url-search-params

--- types/url-search-params/index.d.ts
+++ types/url-search-params/index.d.ts
@@ -3,7 +3,25 @@
 // Definitions by: Nick <https://github.com/nick121212>
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
 
-interface URLSearchParams {
+// interface URLSearchParams {
+//     append(name: string, value: any): void;
+//     delete(name: string): void;
+//     entries(pointer: string): Iterator<string>;
+//     get(name: string): string;
+//     getAll(): string[];
+//     has(name: string): boolean;
+//     keys(): Iterator<string>;
+//     set(name: string, value: any): void;
+//     values(pointer: string): Iterator<string>;
+// }
+
+// interface URLSearchParamsConstructor {
+//     prototype: URLSearchParams;
+//     new(init?: string | URLSearchParams | any): URLSearchParams;
+// }
+
+declare class URLSearchParams {
+    constructor(init?: string | URLSearchParams | any);
     append(name: string, value: any): void;
     delete(name: string): void;
     entries(pointer: string): Iterator<string>;
@@ -15,11 +33,6 @@ interface URLSearchParams {
     values(pointer: string): Iterator<string>;
 }
 
-interface URLSearchParamsConstructor {
-    prototype: URLSearchParams;
-    new(init?: string | URLSearchParams | any): URLSearchParams;
-}
-
-declare const URLSearchParams: URLSearchParamsConstructor;
+// declare const URLSearchParams: URLSearchParams;
 
 export = URLSearchParams;
