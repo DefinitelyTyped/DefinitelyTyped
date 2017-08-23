67bc462124f8a637241e553a6db648008a2ffb84
Peter Scott <peter@steel-hub.co.uk>
2017-08-23 14:34:45 +0100

    esprima v4.0.0

--- types/esprima/esprima-tests.ts
+++ types/esprima/esprima-tests.ts
@@ -8,11 +8,31 @@ var string: string;
 
 // esprima
 string = esprima.version;
-program = esprima.parse('code');
-program = esprima.parse('code', {range: true});
-program = esprima.parse('import * as code from "code"', {sourceType: 'module'})
+
+// Parse Module & Parse Script
+program = esprima.parseScript('answer = 42');
+program = esprima.parseModule('import { sqrt } from "math.js"');
+
+// Parsing Options
+program = esprima.parseScript('var el= <title>${product}</title>', { jsx: true });
+program = esprima.parseScript('if (x) function y() {}');
+program = esprima.parseScript('"use strict"; with (x) {}', { tolerant: true });
+program = esprima.parseScript('answer = 42', { range: true });
+program = esprima.parseScript('answer = 42', { range: true });
+program = esprima.parseScript('const answer = 42', { tokens: true });
+program = esprima.parseScript('answer = 42 // TODO: why', { comment: true });
+program = esprima.parseScript('answer = 42 // TODO: why', { comment: true, range: true });
+
+// Tokenizing
 token = esprima.tokenize('code')[0];
 token = esprima.tokenize('code', {range: true})[0];
+
+// Syntax Delegate
+esprima.parseScript('answer = 42', {}, function (node) {
+    if (node.type === esprima.Syntax.VariableDeclaration) {
+
+    }
+});
 
 // Token
 string = token.type;
