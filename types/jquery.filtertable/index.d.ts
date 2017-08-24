6af9cd73104ad34046c23b455b09a7418bd286c0
TotPeRo <www.totpe.ro@gmail.com>
2017-08-24 15:00:57 +0300

    Update index.d.ts

--- types/jquery.filtertable/index.d.ts
+++ types/jquery.filtertable/index.d.ts
@@ -1,6 +1,6 @@
 // Type definitions for jquery.filterTable v1.5.7
 // Project: https://github.com/sunnywalker/jQuery.FilterTable
-// Definitions by: TotPeRo <https://github.com/totpero/>
+// Definitions by: TotPeRo <https://github.com/totpero>
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
 // TypeScript Version: 2.3
 
