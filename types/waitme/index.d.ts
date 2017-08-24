bc08278eb87834fe71ce62039bf9cd1c24c43ac6
TotPeRo <www.totpe.ro@gmail.com>
2017-08-24 15:00:22 +0300

    Update index.d.ts

--- types/waitme/index.d.ts
+++ types/waitme/index.d.ts
@@ -1,6 +1,6 @@
 // Type definitions for waitMe 1.18
 // Project: https://github.com/vadimsva/waitMe
-// Definitions by: TotPeRo <https://github.com/totpero/>
+// Definitions by: TotPeRo <https://github.com/totpero>
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
 // TypeScript Version: 2.3
 
