42e17e48b08a43bf48ad56afcfab52cdd7966648
TotPeRo <www.totpe.ro@gmail.com>
2017-08-24 14:23:06 +0300

    Update index.d.ts

--- types/waitme/index.d.ts
+++ types/waitme/index.d.ts
@@ -2,6 +2,7 @@
 // Project: https://github.com/vadimsva/waitMe
 // Definitions by: TotPeRo <https://github.com/totpero/>
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
+// TypeScript Version: 2.3
 
 ///<reference types="jquery" />
 
