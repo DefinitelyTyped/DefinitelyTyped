f692f194b4bb8678a7fd4527fc02fdb3850633b5
Christoph Spielmann <spielc@gmail.com>
2017-08-27 17:30:24 +0200

    Add typings for react-json 0.2!

---
+++ types/react-json/index.d.ts
@@ -0,0 +1,16 @@
+// Type definitions for react-json 0.2
+// Project: https://github.com/arqex/react-json
+// Definitions by: Christoph Spielmann <https://github.com/spielc>
+// Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
+
+import * as React from 'react';
+
+export type OnChangeHandler = (value: any) => void;
+
+export interface JsonProperties {
+    value: any;
+    onChange?: OnChangeHandler;
+}
+
+export class Json extends React.Component<JsonProperties, {}> {
+}
