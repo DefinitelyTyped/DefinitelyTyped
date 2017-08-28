c9ffc09d9d769ea88bcd626f439e376d8f820187
Vladimir Djukic <vladimirdjukic90@gmail.com>
2017-08-28 16:24:02 +0200

    fix test

--- types/nats-hemera/nats-hemera-tests.ts
+++ types/nats-hemera/nats-hemera-tests.ts
@@ -1,17 +1,10 @@
 import { Hemera } from 'nats-hemera';
-
-const nats = {}
-
+const nats = {};
 const hemera: any = new Hemera(nats, {});
-
-
-
 hemera.ready(() => {
-
   hemera.add({
     topic: 'math',
     cmd: 'add'
-  }, function (req:any, cb: any) {
-   //....
-  })
-})
+  }, (req: any, cb: any) => {
+  });
+});
