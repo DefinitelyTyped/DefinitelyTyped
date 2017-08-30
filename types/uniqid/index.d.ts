b98d24e5bb0b867505cd6d4aced45588eb747e8a
idchlife <idchlife@gmail.com>
2017-08-30 16:46:36 +0300

    Parameter is not required

--- types/uniqid/index.d.ts
+++ types/uniqid/index.d.ts
@@ -4,7 +4,7 @@
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
 
 // Commmon function signature
-declare function f(prefix: string): string;
+declare function f(prefix?: string): string;
 
 // let x -> Workaround for ES6 imports
 // Combined type because of assigning to function object in original module
