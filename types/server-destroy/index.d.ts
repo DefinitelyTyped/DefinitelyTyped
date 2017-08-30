0509da95af35ed4caba7aa4ec615fa99bf99931b
Gyula Szalai <gyula.szalai@vanio.hu>
2017-08-30 10:29:21 +0200

    Fix tsconfig of server-destroy typings

--- types/server-destroy/index.d.ts
+++ types/server-destroy/index.d.ts
@@ -1,7 +1,8 @@
 // Type definitions for server-destroy 1.0
-// Project: https://github.com/isaacs/server-destroy
+// Project: https://github.com/isaacs/server-destroy#readme
 // Definitions by: Gyula Szalai <https://github.com/gyszalai>
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
+/// <reference types="node" />
 
 import * as http from "http";
 
