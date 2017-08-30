c510750a119198de8a4a798b54aef367f920fe85
Michael Glitzos <mglitzos@outlook.com>
2017-08-30 18:33:34 -0400

    Updated definition of remote Added additional properties to satisfy options for react-bootstrap-table v3.

--- types/react-bootstrap-table/react-bootstrap-table-tests.tsx
+++ types/react-bootstrap-table/react-bootstrap-table-tests.tsx
@@ -21,10 +21,10 @@ function priceFormatter(cell: any, row: any) {
 
 render(
   <BootstrapTable data={products} striped={true} hover={true} ignoreSinglePage>
-      <TableHeaderColumn dataField="id" isKey={true} dataAlign="center" dataSort={true}>Product ID</TableHeaderColumn>
-      <TableHeaderColumn dataField="name" dataSort={true} editable={{ type: 'textarea', rows: 10 }}>Product Name</TableHeaderColumn>
-      <TableHeaderColumn dataField="price" dataFormat={priceFormatter}>Product Price</TableHeaderColumn>
-    </BootstrapTable>,
+    <TableHeaderColumn dataField="id" isKey={true} dataAlign="center" dataSort={true}>Product ID</TableHeaderColumn>
+    <TableHeaderColumn dataField="name" dataSort={true} editable={{ type: 'textarea', rows: 10 }}>Product Name</TableHeaderColumn>
+    <TableHeaderColumn dataField="price" dataFormat={priceFormatter}>Product Price</TableHeaderColumn>
+  </BootstrapTable>,
   document.getElementById("app")
 );
 
@@ -41,11 +41,11 @@ function enumFormatter(cell: any, row: any, enumObject: any) {
 class SelectFilterWithDefaultValue extends React.Component {
   render() {
     return (
-      <BootstrapTable data={ products }>
-          <TableHeaderColumn dataField='id' isKey>Product ID</TableHeaderColumn>
-          <TableHeaderColumn dataField='name'>Product Name</TableHeaderColumn>
-          <TableHeaderColumn dataField='quality' filterFormatted dataFormat={ enumFormatter }
-            formatExtraData={ qualityType } filter={ { type: 'SelectFilter', options: qualityType, defaultValue: 1 } }>Product Quality</TableHeaderColumn>
+      <BootstrapTable data={products}>
+        <TableHeaderColumn dataField='id' isKey>Product ID</TableHeaderColumn>
+        <TableHeaderColumn dataField='name'>Product Name</TableHeaderColumn>
+        <TableHeaderColumn dataField='quality' filterFormatted dataFormat={enumFormatter}
+          formatExtraData={qualityType} filter={{ type: 'SelectFilter', options: qualityType, defaultValue: 1 }}>Product Quality</TableHeaderColumn>
       </BootstrapTable>
     );
   }
@@ -54,9 +54,9 @@ class SelectFilterWithDefaultValue extends React.Component {
 class TextFilterWithCondition extends React.Component {
   render() {
     return (
-      <BootstrapTable data={ products }>
+      <BootstrapTable data={products}>
         <TableHeaderColumn dataField='id' isKey>Product ID</TableHeaderColumn>
-        <TableHeaderColumn dataField='name' filter={ { type: 'TextFilter', delay: 1000, condition: 'eq' } }>Product Name</TableHeaderColumn>
+        <TableHeaderColumn dataField='name' filter={{ type: 'TextFilter', delay: 1000, condition: 'eq' }}>Product Name</TableHeaderColumn>
         <TableHeaderColumn dataField='price'>Product Price</TableHeaderColumn>
       </BootstrapTable>
     );
@@ -72,15 +72,35 @@ function getCustomFilter(filterHandler: (parameters?: ApplyFilterParameter) => v
 class CustomFilter extends React.Component {
   render() {
     return (
-      <BootstrapTable data={ products }>
+      <BootstrapTable data={products}>
         <TableHeaderColumn dataField='id' isKey>Product ID</TableHeaderColumn>
         <TableHeaderColumn dataField='name'>Product Name</TableHeaderColumn>
-        <TableHeaderColumn dataField='isInStock' filter={ { type: 'CustomFilter', getElement: getCustomFilter, customFilterParameters: { textOK: 'yes', textNOK: 'no' } } }>Product Is In Stock</TableHeaderColumn>
+        <TableHeaderColumn dataField='isInStock' filter={{ type: 'CustomFilter', getElement: getCustomFilter, customFilterParameters: { textOK: 'yes', textNOK: 'no' } }}>Product Is In Stock</TableHeaderColumn>
       </BootstrapTable>
     );
   }
 }
 
+class RemoteProps extends React.Component {
+  render() {
+    return (
+      <BootstrapTable
+        data={products}
+        remote={(remoteObj) => {
+          remoteObj.cellEdit = true;
+          return remoteObj;
+        }}
+        options={{
+          onCellEdit: (row: any, fieldName: string, value: any) => { console.info(row); }
+        }}
+      >
+        <TableHeaderColumn dataField='id' isKey>Product ID</TableHeaderColumn>
+        <TableHeaderColumn dataField='name'>Product Name</TableHeaderColumn>
+        <TableHeaderColumn dataField='isInStock' filter={{ type: 'CustomFilter', getElement: getCustomFilter, customFilterParameters: { textOK: 'yes', textNOK: 'no' } }}>Product Is In Stock</TableHeaderColumn>
+      </BootstrapTable>
+    );
+  }
+}
 // Adopted from https://github.com/AllenFang/react-bootstrap-table/blob/master/examples/js/column-header-span/column-header-span-complex.js
 export default class ColumnHeaderSpanComplex extends React.Component {
   render() {
@@ -94,15 +114,15 @@ export default class ColumnHeaderSpanComplex extends React.Component {
       blurToSave: true
     };
     return (
-     <BootstrapTable data={ products }
+      <BootstrapTable data={products}
         insertRow deleteRow exportCSV>
-        <TableHeaderColumn row={0} rowSpan={2} dataField='id' isKey={ true } >ID</TableHeaderColumn>
+        <TableHeaderColumn row={0} rowSpan={2} dataField='id' isKey={true} >ID</TableHeaderColumn>
         <TableHeaderColumn row={0} colSpan={3} dataSort csvHeader='Product' headerAlign='right'>Product</TableHeaderColumn>
         <TableHeaderColumn row={1} dataField='name' width='175' dataAlign='center'>name</TableHeaderColumn>
         <TableHeaderColumn row={1} dataField='price' dataSort>price</TableHeaderColumn>
         <TableHeaderColumn row={1} dataField='coupon' width='70'>Coupon</TableHeaderColumn>
         <TableHeaderColumn row={0} csvHeader='In stock' rowSpan={2} dataField='status'>In stock</TableHeaderColumn>
-        <TableHeaderColumn row={0} colSpan={2} csvHeader='Customer' filter={ { type: 'TextFilter', delay: 1000 } }>Customer</TableHeaderColumn>
+        <TableHeaderColumn row={0} colSpan={2} csvHeader='Customer' filter={{ type: 'TextFilter', delay: 1000 }}>Customer</TableHeaderColumn>
         <TableHeaderColumn row={1} csvHeader='name' dataField='customer'>name</TableHeaderColumn>
         <TableHeaderColumn row={1} csvHeader='order' dataField='order' dataSort>order</TableHeaderColumn>
       </BootstrapTable>
