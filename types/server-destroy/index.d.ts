990867515ecb8db66aee6858f6d7442854605e64
Gyula Szalai <gyula.szalai@vanio.hu>
2017-08-30 10:09:00 +0200

    Typings for server-destroy npm package

---
+++ types/server-destroy/index.d.ts
@@ -0,0 +1,16 @@
+// Type definitions for server-destroy 1.0
+// Project: https://github.com/isaacs/server-destroy
+// Definitions by: Gyula Szalai <https://github.com/gyszalai>
+// Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
+
+import * as http from "http";
+
+declare module "http" {
+  interface Server {
+    destroy(callback?: () => void): void;
+  }
+}
+
+declare function enableDestroy(server: http.Server): void;
+
+export = enableDestroy;
