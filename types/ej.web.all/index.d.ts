7f8fe97aa842073aa1aece7baced9a7f064d5350
Syncfusion-JavaScript <packages@syncfusion.com>
2017-09-05 14:47:54 +0530

    Fix pull request #19478 issue

--- types/ej.web.all/index.d.ts
+++ types/ej.web.all/index.d.ts
@@ -1,4 +1,4 @@
-﻿// Type definitions for ej.web.all 15.3.29
+// Type definitions for ej.web.all 15.3.29
 // Project: http://help.syncfusion.com/js/typescript
 // Definitions by: Syncfusion <https://github.com/syncfusion/>
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
