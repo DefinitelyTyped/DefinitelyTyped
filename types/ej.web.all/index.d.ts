9582c6882701ee8fb8fcd1fc584c64ada9da1ed4
Syncfusion-JavaScript <packages@syncfusion.com>
2017-09-05 14:13:07 +0530

    Fix for pull request issue #19478

--- types/ej.web.all/index.d.ts
+++ types/ej.web.all/index.d.ts
@@ -1,10 +1,8 @@
-// Type definitions for ej.web.all 15.3.29
+﻿// Type definitions for ej.web.all 15.3.29
 // Project: http://help.syncfusion.com/js/typescript
 // Definitions by: Syncfusion <https://github.com/syncfusion/>
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
 // TypeScript Version: 2.3
-
-/// <reference types="jquery" />
 
 /*!
 *  filename: ej.web.all.d.ts
