f0374153ddcc01ecb8b64faca4b0047f14cc28a4
Syncfusion-JavaScript <packages@syncfusion.com>
2017-09-06 12:12:33 +0530

    Fix ej.web.all Compilation issue

--- types/ej.web.all/index.d.ts
+++ types/ej.web.all/index.d.ts
@@ -1,4 +1,4 @@
-// Type definitions for ej.web.all 15.3.29
+﻿// Type definitions for ej.web.all 15.3
 // Project: http://help.syncfusion.com/js/typescript
 // Definitions by: Syncfusion <https://github.com/syncfusion/>
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
