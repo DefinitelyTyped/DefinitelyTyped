8ace32a0405e2ccb35f9b43007bb851ad1746595
Junyoung Choi <fluke8259@gmail.com>
2017-09-07 00:41:24 +0900

    Exploit generics to assert typings of custom data

--- types/vfile/vfile-tests.ts
+++ types/vfile/vfile-tests.ts
@@ -1,6 +1,13 @@
 import vfile = require("vfile");
 
-const file = vfile({path: '~/example.txt', contents: 'Alpha *braavo* charlie.'});
+const file = vfile({
+    path: '~/example.txt',
+    contents: 'Alpha *braavo* charlie.',
+    custom: 'Custom tango',
+    data: {
+        custom: 12345
+    }
+});
 
 file.path; // => '~/example.txt'
 file.dirname; // => '~'
@@ -16,3 +23,7 @@ file.history; // => ['~/example.txt', '~/example.md', '~/index.text']
 file.message('`braavo` is misspelt; did you mean `bravo`?', {line: 1, column: 8});
 
 console.log(file.messages);
+
+// Keep typings of custom data
+const custom: string = file.custom; // 'Custom tango'
+const dataCustom: number = file.data.custom; // 12345
