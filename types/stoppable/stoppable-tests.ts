b71e2304ffb6b13d80f4efb22ad51638f3531399
Eric Byers <ebyers@markettrack.com>
2017-09-08 16:14:07 -0500

    Adding stoppable typings

---
+++ types/stoppable/stoppable-tests.ts
@@ -0,0 +1,8 @@
+import http = require('http');
+import stoppable = require('stoppable');
+
+const server:http.Server = stoppable(http.createServer());
+server.stop();
+
+const server2:http.Server = stoppable(http.createServer(), 10000);
+server2.stop();
