eca6b8dcf88a0117f4e4a2827cd348ee2e6e1646
Eric Byers <ebyers@markettrack.com>
2017-09-08 16:28:32 -0500

    TSLint fixes

--- types/stoppable/stoppable-tests.ts
+++ types/stoppable/stoppable-tests.ts
@@ -1,8 +1,8 @@
-import http = require('http');
+import * as http from 'http';
 import stoppable = require('stoppable');
 
-const server:http.Server = stoppable(http.createServer());
+const server: http.Server = stoppable(http.createServer());
 server.stop();
 
-const server2:http.Server = stoppable(http.createServer(), 10000);
+const server2: http.Server = stoppable(http.createServer(), 10000);
 server2.stop();
