59f2c997fa18fc55fed5c0626755610656ce382b
Eric Byers <ebyers@markettrack.com>
2017-09-08 16:14:07 -0500

    Adding stoppable typings

---
+++ types/stoppable/index.d.ts
@@ -0,0 +1,7 @@
+// Type definitions for stoppable 1.0
+// Project: https://github.com/hunterloftis/stoppable
+// Definitions by: Eric Byers <https://github.com/EricByers>
+// Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
+
+
+export const stoppable: (server:http.Server, grace?:number) => http.Server;
