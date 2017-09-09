b0b8a5d0a47fc41ea570c5eebbdfd614f24c3162
Paolo Scanferla <paolo.scanferla@gmail.com>
2017-09-09 11:20:07 +0200

    morgan: add definitions author

--- types/morgan/index.d.ts
+++ types/morgan/index.d.ts
@@ -1,6 +1,7 @@
 // Type definitions for morgan 1.7
 // Project: https://github.com/expressjs/morgan
 // Definitions by: James Roland Cabresos <https://github.com/staticfunction>
+//                 Paolo Scanferla <https://github.com/pscanf>
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
 // TypeScript Version: 2.2
 
