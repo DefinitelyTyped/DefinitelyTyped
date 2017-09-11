7e079651cf950f6a24f8871ae7978650a084a3b0
Arne Bahlo <hallo@arne.me>
2017-09-11 18:43:25 +0200

    Switch to constructor method

--- types/friendly-errors-webpack-plugin/friendly-errors-webpack-plugin-tests.ts
+++ types/friendly-errors-webpack-plugin/friendly-errors-webpack-plugin-tests.ts
@@ -3,6 +3,6 @@ import * as FriendlyErrorsPlugin from 'friendly-errors-webpack-plugin';
 
 const compiler = webpack({
     plugins: [
-        FriendlyErrorsPlugin(),
+        new FriendlyErrorsPlugin(),
     ],
 });
