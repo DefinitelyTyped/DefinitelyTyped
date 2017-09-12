6b2a088e0e95f456559724520f98b06e4b4f6106
Chris Devine <chris.devine89@gmail.com>
2017-09-12 16:48:48 +0100

    Update index.d.ts
    update to use import and work with ES6/ ES2015 - style import

--- types/point-in-polygon/index.d.ts
+++ types/point-in-polygon/index.d.ts
@@ -1,8 +1,13 @@
-// Type definitions for point-in-polygon 1.0
+// Type definitions for point-in-polygon 1.1
 // Project: https://github.com/substack/point-in-polygon
 // Definitions by: dyst5422 <https://github.com/dyst5422>
 //                 kogai <https://github.com/kogai>
+//                 Chris-Devine <https://github.com/chris-devine>
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
 
-declare function inside(point: ReadonlyArray<number>, polygon: ReadonlyArray<ReadonlyArray<number>>): boolean;
 export = inside;
+export as namespace inside;
+
+declare namespace inside {
+    function inside(point: ReadonlyArray<number>, polygon: ReadonlyArray<ReadonlyArray<number>>): boolean;
+}
