cca704b3b423f72604b17b67a3fa9940f9bd60d8
Frank Tan <tansongyang@gmail.com>
2017-09-14 10:36:09 -0400

    [blob-util] Increment version number

--- types/blob-util/index.d.ts
+++ types/blob-util/index.d.ts
@@ -1,4 +1,4 @@
-// Type definitions for blob-util 1.2
+// Type definitions for blob-util 1.3
 // Project: https://github.com/nolanlawson/blob-util#readme
 // Definitions by: Max Battcher <https://github.com/WorldMaker>
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
