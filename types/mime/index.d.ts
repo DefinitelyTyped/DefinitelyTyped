67731e6e72f4ec886167b54e01558a55aa4de731
Peter Keuter <github@peterkeuter.nl>
2017-09-14 22:12:13 +0200

    update definitions to be compatible with v2.0.0

--- types/mime/index.d.ts
+++ types/mime/index.d.ts
@@ -1,20 +1,12 @@
-// Type definitions for mime 1.3
+// Type definitions for mime 2.1
 // Project: https://github.com/broofa/node-mime
-// Definitions by: Jeff Goddard <https://github.com/jedigo>
+// Definitions by: Jeff Goddard <https://github.com/jedigo>, Peter Keuter <https://github.com/pkeuter>
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
 
 // Imported from: https://github.com/soywiz/typescript-node-definitions/mime.d.ts
 
 export as namespace mime;
 
-export function lookup(path: string, fallback?: string): string;
-export function extension(mime: string): string;
-export function load(filepath: string): void;
+export function getType(path: string, fallback?: string): string;
+export function getExtension(mime: string): string;
 export function define(mimes: { [key: string]: any }): void;
-
-export interface Charsets {
-    lookup(mime: string): string;
-}
-
-export const charsets: Charsets;
-export const default_type: string;
