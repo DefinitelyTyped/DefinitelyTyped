7cc3e555f1d9bc8d3c8e421c52da343b0519f615
Gavin Draper <g@vindraper.co.uk>
2017-09-18 11:54:48 +0100

    Update version number

--- types/react-big-calendar/index.d.ts
+++ types/react-big-calendar/index.d.ts
@@ -1,4 +1,4 @@
-// Type definitions for react-big-calendar 0.14.0
+// Type definitions for react-big-calendar 0.15.0
 // Project: https://github.com/intljusticemission/react-big-calendar
 // Definitions by: Piotr Witek <http://piotrwitek.github.io>
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
