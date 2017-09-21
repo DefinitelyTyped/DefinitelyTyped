85c2fdfb70446b1d9758c3862f826e0205f91bf9
Kyle <kroach.work@gmail.com>
2017-09-21 01:38:51 -0400

    feat(types): Definitions for querystring

---
+++ types/querystring/index.d.ts
@@ -0,0 +1,9 @@
+// Type definitions for querystring 0.2
+// Project: https://github.com/Gozala/querystring
+// Definitions by: Kyle Roach <https://github.com/iRoachie>
+// Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
+// TypeScript Version: 2.4
+
+ export function stringify(val: object): string;
+
+ export function parse(val: string): any;
