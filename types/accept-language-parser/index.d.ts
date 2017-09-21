50ef506345c179dd66312d65eeebcf9c85ed91e7
Niklas Wulf <wlfn@viessmann.com>
2017-09-21 12:23:06 +0200

    [accept-language-header] Confirm to dtslint

--- types/accept-language-parser/index.d.ts
+++ types/accept-language-parser/index.d.ts
@@ -2,6 +2,7 @@
 // Project: https://github.com/opentable/accept-language-parser
 // Definitions by: Niklas Wulf <https://github.com/kampfgnom>
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
+// TypeScript Version: 2.1
 
 // https://github.com/opentable/accept-language-parser/blob/v1.4.1/index.js
 
