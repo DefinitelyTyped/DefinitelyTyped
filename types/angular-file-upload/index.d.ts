ac3d43c2e44f4f3baa9f9c32a02fba185f9591b8
Cyril Gandon <cgandon@waya-tech.com>
2017-09-21 13:31:12 +0200

    angular-file-upload: add typescript version information

--- types/angular-file-upload/index.d.ts
+++ types/angular-file-upload/index.d.ts
@@ -2,6 +2,7 @@
 // Project: https://github.com/nervgh/angular-file-upload
 // Definitions by: Cyril Gandon <https://github.com/cyrilgandon>
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
+// TypeScript Version: 2.5
 
 import * as angular from 'angular';
 
