3ddf99df4fc1482463f8e13f341014fd335cdf90
Denis <carriere.denis@gmail.com>
2017-09-25 17:13:15 -0400

    GeoJSON replace properties with "any"

--- types/geojson/index.d.ts
+++ types/geojson/index.d.ts
@@ -2,7 +2,6 @@
 // Project: http://geojson.org/
 // Definitions by: Jacob Bruun <https://github.com/cobster>
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
-// TypeScript Version: 2.2
 
 export as namespace GeoJSON;
 
@@ -93,7 +92,7 @@ export interface GeometryCollection extends GeoJsonObject {
 export interface Feature<T extends GeometryObject> extends GeoJsonObject {
     type: 'Feature';
     geometry: T;
-    properties: {[key: string]: any} | null;
+    properties: any;
     id?: string;
 }
 
