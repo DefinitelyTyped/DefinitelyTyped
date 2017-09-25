8be73c8ce2ab23c643f61e0b396dd100d914b11b
Nick Morton <nick.morton@smoothwall.com>
2017-09-25 15:00:34 +0100

    Removed spread operators to downgrade to TypeScript 2.0

--- types/swaggerize-express/swaggerize-express-tests.ts
+++ types/swaggerize-express/swaggerize-express-tests.ts
@@ -2,28 +2,27 @@ import http = require('http');
 import express = require('express');
 import swaggerize = require('swaggerize-express');
 
-const api: Partial<swaggerize.Options> = {
-    api:  {
-        swagger: "2.0",
-        host: "localhost:8080",
-        info: {
-            title: "swaggerize-express.d.ts test",
-            version: "1"
-        },
-        paths: {
-        }
+const api = {
+    swagger: "2.0",
+    host: "localhost:8080",
+    info: {
+        title: "swaggerize-express.d.ts test",
+        version: "1"
     },
-    docspath: '/api-docs',
+    paths: {
+    }
 };
 
 var app = express();
 app.use(swaggerize(<swaggerize.Options>{
-    ...api,
+    api,
+    docspath: '/api-docs',
     handlers: './handlers'
 }));
 
 app.use(swaggerize(<swaggerize.Options>{
-    ...api,
+    api,
+    docspath: '/api-docs',
     handlers: {
         'api': {
             'v1': {
@@ -36,7 +35,8 @@ app.use(swaggerize(<swaggerize.Options>{
 }));
 
 app.use(swaggerize(<swaggerize.Options>{
-    ...api,
+    api,
+    docspath: '/api-docs',
     handlers: {
         'api': {
             'authenticated-path': {
