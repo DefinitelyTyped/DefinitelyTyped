ff00c165b35374263f1d674e1a378194dfa0ee0b
Raffael Krebs <kreba@users.noreply.github.com>
2017-09-26 01:55:44 +0200

    @types/reactstrap Add prop autoFocus for Modal component (#19791)
    * @types/reactstrap Add prop autoFocus for Modal component
    
    * bump version number to 4.6

--- types/reactstrap/index.d.ts
+++ types/reactstrap/index.d.ts
@@ -1,4 +1,4 @@
-// Type definitions for reactstrap 4.3
+// Type definitions for reactstrap 4.6
 // Project: https://github.com/reactstrap/reactstrap#readme
 // Definitions by: Ali Hammad Baig <https://github.com/alihammad>, Marco Falkenberg <https://github.com/mfal>, Danilo Barros <https://github.com/danilobjr>
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
