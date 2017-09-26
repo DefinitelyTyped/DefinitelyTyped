89962dc045c2b23c1bed6b67875e2c67080c4a2c
Pratheek Adidela <apratheek@unaxiom.com>
2017-09-26 17:08:49 +0530

    Linting is successful.

--- types/node-horseman/index.d.ts
+++ types/node-horseman/index.d.ts
@@ -2,6 +2,7 @@
 // Project: https://github.com/johntitus/node-horseman#readme
 // Definitions by: Pratheek Adidela <https://github.com/apratheek>
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
+// TypeScript Version: 2.2
 
 /*~ If this module is a UMD module that exposes a global variable 'myLib' when
  *~ loaded outside a module loader environment, declare that global here.
@@ -10,242 +11,240 @@
 
 export = horseman;
 
-declare module horseman {
-    namespace horseman{}
+declare namespace horseman {
+    class horseman {}
 }
 
 declare class horseman {
-    
     constructor(options: any);
-    /**Handle page events inside PhantomJS. Phantom receives callback return value with .at but not with .on */
-    at(eventType: string, callback: Function): this;
+    /** Handle page events inside PhantomJS. Phantom receives callback return value with .at but not with .on */
+    at(eventType: string, callback: () => void): this;
 
-    /**Get the value of an attribute for a selector. */
+    /** Get the value of an attribute for a selector. */
     attribute(selector: string, attr: string): string;
 
-    /**Use basic HTTP authentication when opening a page. */
+    /** Use basic HTTP authentication when opening a page. */
     authentication(username: string, password: string): this;
 
-    /**Go back a page. */
+    /** Go back a page. */
     back(): this;
 
-    /**Get the bounding rectangle of a selector. */
+    /** Get the bounding rectangle of a selector. */
     boundingRectangle(selector: string): ClientRect;
 
-    /**Clear an input field. */
+    /** Clear an input field. */
     clear(selector: string): any;
 
-    /**Click on a selector and fire a 'click event'. */
+    /** Click on a selector and fire a 'click event'. */
     click(selector: string): this;
 
     /** Close the instance and perform cleanup. */
     close(): this;
 
-    /**Close a tab and release its resources. */
+    /** Close a tab and release its resources. */
     closeTab(tabNumber: string): this;
 
-    /**Set the cookies for Phantom. */
+    /** Set the cookies for Phantom. */
     cookies(args: object | object[] | string): this;
-    
-    /**Get the cookies from Phantom. */
+    /** Get the cookies from Phantom. */
     cookies(): string;
 
-    /**Count the number of occurances of 'selector' on the page. */
+    /** Count the number of occurances of 'selector' on the page. */
     count(selector: string): number;
 
-    /**Save a cropped screenshot to disk. */
+    /** Save a cropped screenshot to disk. */
     crop(area: string | object, path: string): this;
 
-    /**Take a base64 encoded cropped screenshot. */
+    /** Take a base64 encoded cropped screenshot. */
     cropBase64(area: string, type: string | 'PNG'): any;
 
-    /**Get the value of an css property of a selector. */
+    /** Get the value of an css property of a selector. */
     cssProperty(selector: string, prop: string): string;
 
-    /**Execute a function without breaking the api chain. */
-    do(fn: Function): this;
+    /** Execute a function without breaking the api chain. */
+    do(fn: () => void): this;
 
-    /**Download a URL. */
+    /** Download a URL. */
     download(url: string, path: string, binary: boolean, method: string, data: string): this;
-    
-    /**Run a javascript function on the current page and optionally return the results. */
-    evaluate(fn: Function, args?: any[]): this;
 
-    /**Determine if the selector exists, at least once, on the page. */
+    /** Run a javascript function on the current page and optionally return the results. */
+    evaluate(fn: () => void, args?: any[]): this;
+
+    /** Determine if the selector exists, at least once, on the page. */
     exists(selector: string): boolean;
 
-    /**Go forward a page. */
+    /** Go forward a page. */
     forward(): this;
 
-    /**Get the count of frames inside the current frame. */
+    /** Get the count of frames inside the current frame. */
     frameCount(): number;
 
-    /**Get the name of the current frame. */
+    /** Get the name of the current frame. */
     frameName(): string;
 
-    /**Get the names of the frames inside the current frame. */
+    /** Get the names of the frames inside the current frame. */
     frameNames(): string;
 
-    /**Set headers sent to the remote server during an 'open'. */
-    headers(headers: Object[]): this;
+    /** Set headers sent to the remote server during an 'open'. */
+    headers(headers: object[]): this;
 
-    /**Get the height of an element. */
+    /** Get the height of an element. */
     height(selector: string): number;
 
-    /**Get the HTML for the page, or optionally for a selector. */
+    /** Get the HTML for the page, or optionally for a selector. */
     html(selector?: string, file?: string): Promise<string>;
 
-    /**Includes javascript script from a url on the page. */
+    /** Includes javascript script from a url on the page. */
     includeJs(url: string): this;
 
-    /**Injects javascript from a file into the page. */
+    /** Injects javascript from a file into the page. */
     injectJs(file: string): this;
 
-    /**Fire a key event. */
+    /** Fire a key event. */
     keyboardEvent(type?: string, key?: string, modifier?: number): this;
 
-    /**Log the output from either a previous chain method, or a string the user passed in. */
+    /** Log the output from either a previous chain method, or a string the user passed in. */
     log(output?: string): this;
 
-    /**Run javascript on the page. */
-    manipulate(fn: Function, args?: any[]): this;
+    /** Run javascript on the page. */
+    manipulate(fn: () => void, args?: any[]): this;
 
-    /**Fire a mouse event. */
+    /** Fire a mouse event. */
     mouseEvent(type?: string, x?: number, y?: number, button?: string): this;
 
-    /**Handles page events. * eventType can be one of:
-         *  initialized - callback()
-         *  loadStarted - callback()
-         *  loadFinished - callback(status)
-         *  tabCreated - callback(tabNum, tab)
-         *  tabClosed - callback(tabNum, tab)
-         *  urlChanged - callback(targetUrl)
-         *  navigationRequested - callback(url, type, willNavigate, main)
-         *  resourceRequested - callback(requestData, networkRequest)
-         *  resourceReceived - callback(response)
-         *  pageCreated - callback(newPage)
-         *  consoleMessage(msg, lineNum, sourceId)
-         *  alert - callback(msg)
-         *  confirm - callback(msg)
-         *  prompt - callback(msg, defaultVal)
-         *  filePicker - callback(oldFile)
-         *  error - callback(msg, trace);
-         *  timeout - callback(type) 
-    */
-    on(event: string | "timeout" | "tabCreated" | "tabClosed"| "resourceTimeout"| "urlChanged"| "resourceReceived"| "pageCreated"| "loadFinished", func: Function ): this;
+    /** Handles page events. * eventType can be one of:
+     *  initialized - callback()
+     *  loadStarted - callback()
+     *  loadFinished - callback(status)
+     *  tabCreated - callback(tabNum, tab)
+     *  tabClosed - callback(tabNum, tab)
+     *  urlChanged - callback(targetUrl)
+     *  navigationRequested - callback(url, type, willNavigate, main)
+     *  resourceRequested - callback(requestData, networkRequest)
+     *  resourceReceived - callback(response)
+     *  pageCreated - callback(newPage)
+     *  consoleMessage(msg, lineNum, sourceId)
+     *  alert - callback(msg)
+     *  confirm - callback(msg)
+     *  prompt - callback(msg, defaultVal)
+     *  filePicker - callback(oldFile)
+     *  error - callback(msg, trace);
+     *  timeout - callback(type)
+     */
+    on(event: string | "timeout" | "tabCreated" | "tabClosed"| "resourceTimeout"| "urlChanged"| "resourceReceived"| "pageCreated"| "loadFinished", func: (...args: any[]) => void): this;
 
-    /**Open a url in Phantom. */
+    /** Open a url in Phantom. */
     open(url: string, method?: string | 'GET'): this;
 
-    /**Open URL in a new tab */
+    /** Open URL in a new tab */
     openTab(url: string): this;
 
     pageMaker(url: any, _page: any, ...args: any[]): any;
 
-    /**Save the current page as a pdf. */
+    /** Save the current page as a pdf. */
     pdf(path: string, paperSize: {
         format?: "A3" | "A4" | "A5" | "Legal" | "Letter" | "Tabloid",
         orientation?: "portrait" | "landscape",
         margin?: string
     }): this;
 
-    /**Get the plain text for the body of the page. */
+    /** Get the plain text for the body of the page. */
     plainText(): string;
 
-    /**Send Post data to a url. */
+    /** Send Post data to a url. */
     post(url: string, postData: string): this;
 
-    /**Send Put data to a url. */
+    /** Send Put data to a url. */
     put(url: string, putData: string): this;
 
-    /**Reload the page. */
+    /** Reload the page. */
     reload(): this;
 
-    /**Save a screenshot to disk. */
+    /** Save a screenshot to disk. */
     screenshot(path: string): this;
 
-    /**Take a base64 encoded screenshot, e.g., PNG. */
+    /** Take a base64 encoded screenshot, e.g., PNG. */
     screenshotBase64(type: string | 'PNG'): any;
 
-    /**Scroll to a position on the page. */
+    /** Scroll to a position on the page. */
     scrollTo(top: number, left: number): this;
 
-    /**Select a value in an html select element. */
+    /** Select a value in an html select element. */
     select(selector: string, value: string): any;
 
-    /**Change the proxy settings. */
+    /** Change the proxy settings. */
     setProxy(ip: string, port: string, type: string, username: string, password: string): this;
 
-    /**Get the HTTP status of the last opened page. */
+    /** Get the HTTP status of the last opened page. */
     status(): string;
 
-    /**Switch to a child frame. */
+    /** Switch to a child frame. */
     switchToChildFrame(nameOrPosition: string | number): this;
 
-    /**Switch to the focused frame. */
+    /** Switch to the focused frame. */
     switchToFocusedFrame(): this;
 
-    /**Switch to a frame inside the current frame. */
+    /** Switch to a frame inside the current frame. */
     switchToFrame(nameOrPosition: string | number): this;
 
-    /**Switch to the main frame. */
+    /** Switch to the main frame. */
     switchToMainFrame(): this;
 
-    /**Switch to the parent frame of the current frame. */
+    /** Switch to the parent frame of the current frame. */
     switchToParentFrame(): this;
 
-    /**Switch to another of the open tabs */
+    /** Switch to another of the open tabs */
     switchToTab(tabNumber: number): this;
 
-    /**Returns the number of tabs */
+    /** Returns the number of tabs */
     tabCount(): number;
 
-    /**Get the text for the body of the page, or optionally for a selector. */
+    /** Get the text for the body of the page, or optionally for a selector. */
     text(selector?: string): string;
 
-    /**Get the title of the current page. */
+    /** Get the title of the current page. */
     title(): string;
 
-    /**Simulate a keypress on a selector */
-    type(selector: string, text: string, options?: Object): this;
+    /** Simulate a keypress on a selector */
+    type(selector: string, text: string, options?: object): this;
 
-    /**Upload a file to the page. */
+    /** Upload a file to the page. */
     upload(selector: string, file: string): this;
 
-    /**Get the url of the current page. */
+    /** Get the url of the current page. */
     url(): string;
 
-    /**Get or set the user agent for Phantom. */
+    /** Get or set the user agent for Phantom. */
     userAgent(...args: any[]): any;
 
-    /**Get the value of an element. */
+    /** Get the value of an element. */
     value(selector: string): string;
-    /**Sets the value of an element. */
+    /** Sets the value of an element. */
     value(selector: string, value: string): this;
 
-    /**Set the size of the viewport. */
+    /** Set the size of the viewport. */
     viewport(width: number, height: number): this;
-    /**Get the size of the viewport. */
-    viewport(): Object;
+    /** Get the size of the viewport. */
+    viewport(): object;
 
-    /**Determines if an element is visible. */
+    /** Determines if an element is visible. */
     visible(selector: string): boolean;
 
-    /**Wait for a specified period of time */
+    /** Wait for a specified period of time */
     wait(milliseconds: number): this;
 
-    /**Waits for a function to evaluate to a given value in browser. If optsOrFn is a function, use the classic signature waitFor(fn, arg1, arg2, value), If arg is an object, use waitFor(options). */
+    /** Waits for a function to evaluate to a given value in browser. If optsOrFn is a function, use the classic signature waitFor(fn, arg1, arg2, value), If arg is an object, use waitFor(options). */
     waitFor(...args: any[]): this;
 
-    /**Wait for a page load to occur */
-    waitForNextPage(object?: Object): this;
+    /** Wait for a page load to occur */
+    waitForNextPage(object?: object): this;
 
-    /**Wait for a selector to be present on the current page. */
-    waitForSelector(selector: string, options: Object): this;
+    /** Wait for a selector to be present on the current page. */
+    waitForSelector(selector: string, options: object): this;
 
-    /**Get the width of an element. */
+    /** Get the width of an element. */
     width(selector: string): number;
 
-    /**Set the zoom factor of the page. */
+    /** Set the zoom factor of the page. */
     zoom(zoomFactor: number): this;
 }
--- types/node-horseman/node-horseman-tests.ts
+++ types/node-horseman/node-horseman-tests.ts
@@ -1,11 +1,11 @@
 import * as horseman from 'node-horseman';
 
-let horse = new horseman({
+const horse = new horseman({
     timeout: 10000
 });
 
 horse.on('error', (msg: any, trace: any) => { console.log(msg, trace); })
-.open("http://example.org/").waitForNextPage().open("https://google.com").waitForNextPage().html().then(pageContent => {
+.open("http://example.org/").waitForNextPage().open("https://google.com").waitForNextPage().html().then((pageContent: string) => {
     console.log(pageContent);
     horse.close();
 });
