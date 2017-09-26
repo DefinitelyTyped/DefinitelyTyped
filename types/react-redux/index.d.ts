56020403503935d6271f97117110101839133f05
Dibyo Majumdar <dibyo.majumdar@gmail.com>
2017-09-26 10:48:58 -0700

    Update documentation for Connect

--- types/react-redux/index.d.ts
+++ types/react-redux/index.d.ts
@@ -47,7 +47,26 @@ export interface InferableComponentEnhancerWithProps<TInjectedProps, TNeedsProps
 export type InferableComponentEnhancer<TInjectedProps> =
     InferableComponentEnhancerWithProps<TInjectedProps, {}>
 
-export interface ConnectFunction {
+/**
+ * Connects a React component to a Redux store.
+ *
+ * - Without arguments, just wraps the component, without changing the behavior / props
+ *
+ * - If 2 params are passed (3rd param, mergeProps, is skipped), default behavior
+ * is to override ownProps (as stated in the docs), so what remains is everything that's
+ * not a state or dispatch prop
+ *
+ * - When 3rd param is passed, we don't know if ownProps propagate and whether they
+ * should be valid component props, because it depends on mergeProps implementation.
+ * As such, it is the user's responsibility to extend ownProps interface from state or
+ * dispatch props or both when applicable
+ *
+ * @param mapStateToProps
+ * @param mapDispatchToProps
+ * @param mergeProps
+ * @param options
+ */
+export interface Connect {
     (): InferableComponentEnhancer<DispatchProp<any>>;
 
     <TStateProps = {}, no_dispatch = {}, TOwnProps = {}>(
@@ -118,25 +137,9 @@ export interface ConnectFunction {
 }
 
 /**
- * Connects a React component to a Redux store.
- *
- * - Without arguments, just wraps the component, without changing the behavior / props
- *
- * - If 2 params are passed (3rd param, mergeProps, is skipped), default behavior
- * is to override ownProps (as stated in the docs), so what remains is everything that's
- * not a state or dispatch prop
- *
- * - When 3rd param is passed, we don't know if ownProps propagate and whether they
- * should be valid component props, because it depends on mergeProps implementation.
- * As such, it is the user's responsibility to extend ownProps interface from state or
- * dispatch props or both when applicable
- *
- * @param mapStateToProps
- * @param mapDispatchToProps
- * @param mergeProps
- * @param options
+ * The connect function. See {@type Connect} for details.
  */
-export declare const connect: ConnectFunction;
+export declare const connect: Connect;
 
 interface MapStateToProps<TStateProps, TOwnProps> {
     (state: any, ownProps: TOwnProps): TStateProps;
