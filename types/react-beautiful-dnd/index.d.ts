e60c170ed06aa005389a6d084c80f353d7d6b9d3
varHarrie <varharrie@gmail.com>
2017-09-27 13:43:30 +0800

    Fix lint issues

--- types/react-beautiful-dnd/index.d.ts
+++ types/react-beautiful-dnd/index.d.ts
@@ -1,106 +1,104 @@
-// Type definitions for react-beautiful-dnd 2.3.1
+// export Type definitions for react-beautiful-dnd 2.3.1
 // Project: https://github.com/atlassian/react-beautiful-dnd.git
 // Definitions by: varHarrie <https://github.com/varHarrie>
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
 // TypeScript Version: 2.3
 
-declare module 'react-beautiful-dnd' {
-    import * as React from 'react'
+import * as React from 'react'
 
-    export type Id = string
+export type Id = string
 
-    export type DraggableId = Id
+export type DraggableId = Id
 
-    export type DroppableId = Id
+export type DroppableId = Id
 
-    export type TypeId = Id
+export type TypeId = Id
 
-    export type DraggableLocation = {
-      droppableId: DroppableId
-      index: number
-    }
+export interface DraggableLocation {
+    droppableId: DroppableId
+    index: number
+}
 
-    /**
-     *  DragDropContext
-     */
+/**
+ *  DragDropContext
+ */
 
-    export type DragStart = {
-      draggableId: DraggableId
-      type: TypeId
-      source: DraggableLocation
-    }
+export interface DragStart {
+    draggableId: DraggableId
+    type: TypeId
+    source: DraggableLocation
+}
 
-    export type DropResult = {
-      draggableId: DraggableId,
-      type: TypeId
-      source: DraggableLocation
-      destination?: DraggableLocation
-    }
+export interface DropResult {
+    draggableId: DraggableId,
+    type: TypeId
+    source: DraggableLocation
+    destination?: DraggableLocation
+}
 
-    export interface IDragDropContextProps {
-      onDragStart?: (initial: DragStart) => void
-      onDragEnd: (result: DropResult) => void
-    }
+export interface DragDropContextProps {
+    onDragStart?: (initial: DragStart) => void
+    onDragEnd: (result: DropResult) => void
+}
 
-    export class DragDropContext extends React.Component<IDragDropContextProps> {}
+export class DragDropContext extends React.Component<DragDropContextProps> {}
 
-    /**
-     *  Droppable
-     */
+/**
+ *  Droppable
+ */
 
-    export type DroppableProvided = {
-      innerRef: (element: HTMLElement | null) => any
-      placeholder?: React.ReactElement<any>
-    }
+export interface DroppableProvided {
+    innerRef: (element: HTMLElement | null) => any
+    placeholder?: React.ReactElement<any>
+}
 
-    export type DroppableStateSnapshot = {
-      isDraggingOver: boolean
-    }
+export interface DroppableStateSnapshot {
+    isDraggingOver: boolean
+}
 
-    export interface IDroppableProps {
-      droppableId: DroppableId
-      type?: TypeId
-      isDropDisabled?: boolean
-      direction?: 'vertical' | 'horizontal'
-      children: (provided: DroppableProvided, snapshot: DroppableStateSnapshot) => React.ReactElement<any>
-    }
+export interface DroppableProps {
+    droppableId: DroppableId
+    type?: TypeId
+    isDropDisabled?: boolean
+    direction?: 'vertical' | 'horizontal'
+    children: (provided: DroppableProvided, snapshot: DroppableStateSnapshot) => React.ReactElement<any>
+}
 
-    export class Droppable extends React.Component<IDroppableProps> {}
+export class Droppable extends React.Component<DroppableProps> {}
 
-    /**
-     *  Draggable
-     */
+/**
+ *  Draggable
+ */
 
-    export type DraggableStyle = any
+export type DraggableStyle = any
 
-    export type DragHandleProps = {
-      onMouseDown: (event: MouseEvent) => void
-      onKeyDown: (event: KeyboardEvent) => void
-      onClick: (event: MouseEvent) => void
-      tabIndex: number
-      'aria-grabbed': boolean
-      draggable: boolean
-      onDragStart: () => void
-      onDrop: () => void
-    }
+export interface DragHandleProps {
+    onMouseDown: (event: MouseEvent) => void
+    onKeyDown: (event: KeyboardEvent) => void
+    onClick: (event: MouseEvent) => void
+    tabIndex: number
+    'aria-grabbed': boolean
+    draggable: boolean
+    onDragStart: () => void
+    onDrop: () => void
+}
 
-    export type DraggableProvided = {
-      innerRef: (element?: HTMLElement | null) => any
-      draggableStyle?: DraggableStyle
-      dragHandleProps?: DragHandleProps
-      placeholder?: React.ReactElement<any>
-    }
+export interface DraggableProvided {
+    innerRef: (element?: HTMLElement | null) => any
+    draggableStyle?: DraggableStyle
+    dragHandleProps?: DragHandleProps
+    placeholder?: React.ReactElement<any>
+}
 
-    export type DraggableStateSnapshot = {
-      isDragging: boolean
-    }
+export interface DraggableStateSnapshot {
+    isDragging: boolean
+}
 
-    export interface IDraggableProps {
-      draggableId: DroppableId
-      type?: TypeId
-      isDragDisabled?: boolean
-      children: (provided: DraggableProvided, snapshot: DraggableStateSnapshot) => React.ReactElement<any>
-    }
+export interface DraggableProps {
+    draggableId: DroppableId
+    type?: TypeId
+    isDragDisabled?: boolean
+    children: (provided: DraggableProvided, snapshot: DraggableStateSnapshot) => React.ReactElement<any>
+}
 
-    export class Draggable extends React.Component<IDraggableProps> {}
-  }
+export class Draggable extends React.Component<DraggableProps> {}
