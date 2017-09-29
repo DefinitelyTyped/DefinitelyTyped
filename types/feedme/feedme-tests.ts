292702cd19f669e0536f58a4603b689e3496e3a2
CodeAnimal <codeanimal@outlook.com>
2017-09-29 12:57:53 +0100

    Add tslint.json
    Correct issues raised by dtslint.

--- types/feedme/feedme-tests.ts
+++ types/feedme/feedme-tests.ts
@@ -1,62 +1,63 @@
 import * as FeedMe from "feedme";
 import * as http from "http";
 
-http.get('https://nodejs.org/en/feed/blog.xml', function(res) {
-  var feedme = new FeedMe(true);
-  var feedmeWithoutBuffer = new FeedMe();
+http.get('https://nodejs.org/en/feed/blog.xml', (res) => {
+  const feedme = new FeedMe(true);
+  const feedmeWithoutBuffer = new FeedMe();
 
   res.pipe(feedme);
 
-  feedme.on('end', function() {
-    let document: FeedMe.Document = feedme.done();
-    
-    let metatype: FeedMe.Type = document.type;
+  feedme.on('end', () => {
+    const document: FeedMe.Document = feedme.done();
+
+    const metatype: FeedMe.Type = document.type;
     // "#version": string;
-    let metatitle: string = document.title;
-    let metadescription: string = document.description;
-    let metadate: string = document.date;
-    let metapubdate: string = document.pubdate;
-    let metalink: string = document.link;
-    let metaxmlurl: string = document.xmlurl;
-    let metaauthor: string = document.author;
-    let metalanguage: string = document.language;
-    let metaimage: FeedMe.Image = document.image;
-    let metafavicon: string = document.favicon;
-    let metacopyright: string = document.copyright;
-    let metagenerator: string = document.generator;
-    let metacategories: string[] = document.categories;
+    const metatitle: string = document.title;
+    const metadescription: string = document.description;
+    const metadate: string = document.date;
+    const metapubdate: string = document.pubdate;
+    const metalink: string = document.link;
+    const metaxmlurl: string = document.xmlurl;
+    const metaauthor: string = document.author;
+    const metalanguage: string = document.language;
+    const metaimage: FeedMe.Image = document.image;
+    const metafavicon: string = document.favicon;
+    const metacopyright: string = document.copyright;
+    const metagenerator: string = document.generator;
+    const metacategories: string[] = document.categories;
 
-    if (document.type == "atom" || 
-        document.type == "json" || 
-        document.type == "rss 0.90" || 
-        document.type == "rss 0.91" || 
-        document.type == "rss 0.92" || 
-        document.type == "rss 0.93" || 
-        document.type == "rss 0.94" || 
-        document.type == "rss 1.0" || 
-        document.type == "rss 2.0") {
-
+    switch (document.type) {
+      case "atom":
+      case "json":
+      case "rss 0.90":
+      case "rss 0.91":
+      case "rss 0.92":
+      case "rss 0.93":
+      case "rss 0.94":
+      case "rss 1.0":
+      case "rss 2.0":
+        break;
     }
 
-    let misc: string = document["misc"];
+    const misc: string = document["misc"];
   });
 
-  feedme.on('item', function(item) {
-    let title:string = item.title;
-    let description: string = item.description;
-    let summary: string = item.summary;
-    let date: string = item.date;
-    let pubdate: string = item.pubdate;
-    let link: string = item.link;
-    let origlink: string = item.origlink;
-    let author: string = item.author;
-    let guid: FeedMe.Guid | string = item.guid;
-    let comments: string = item.comments;
-    let image: FeedMe.Image = item.image;
-    let categories: string[] = item.categories;
-    let enclosures: string[] = item.enclosures;
-    let meta: FeedMe.Meta = item.meta;
+  feedme.on('item', (item) => {
+    const title: string = item.title;
+    const description: string = item.description;
+    const summary: string = item.summary;
+    const date: string = item.date;
+    const pubdate: string = item.pubdate;
+    const link: string = item.link;
+    const origlink: string = item.origlink;
+    const author: string = item.author;
+    const guid: FeedMe.Guid | string = item.guid;
+    const comments: string = item.comments;
+    const image: FeedMe.Image = item.image;
+    const categories: string[] = item.categories;
+    const enclosures: string[] = item.enclosures;
+    const meta: FeedMe.Meta = item.meta;
 
-    let misc: string = item["misc"];
+    const misc: string = item["misc"];
   });
 });
