c414ea72187392b5041b54f87533b2552e0754f4
Wolfgang Faust <wolfgang42@git>
2017-09-28 21:57:29 -0400

    Rename object-map's type variables.
    Per suggestion from @plantain-00

--- types/object-map/index.d.ts
+++ types/object-map/index.d.ts
@@ -3,10 +3,10 @@
 // Definitions by: Wolfgang Faust <https://github.com/wolfgang42>
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
 
-declare function objectMap<I, O>(
-	target: {[k: string]: I},
-	callback: (currentValue: I, key: string, object: {[k: string]: I}) => O,
+declare function objectMap<TInput, TOutput>(
+	target: {[k: string]: TInput},
+	callback: (currentValue: TInput, key: string, object: {[k: string]: TInput}) => TOutput,
 	thisArg?: any
-): {[k: string]: O};
+): {[k: string]: TOutput};
 
 export = objectMap;
