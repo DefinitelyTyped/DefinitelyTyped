26116436c33f3c6e8fc2d3a09aa52d8ba95b7661
DESKTOP-EA3UTGO\ujjwal <ujjwalkumargupta4u@gmail.com>
2017-09-30 19:32:19 +0530

    Added type definitions for jsstore

---
+++ types/jsstore/jsstore-tests.ts
@@ -0,0 +1,43 @@
+var Connection: JsStore.Instance;
+function initConnection() {
+    var Database_Name: any = "Demo";
+    JsStore.isDbExist(Database_Name, function (isExist) {
+        if (isExist) {
+            Connection = new JsStore.Instance();
+        }
+        else {
+            var Table: Model.ITable = <Model.ITable>{
+                Name: "TableName",
+                Columns: [
+                    {
+                        Name: 'Column_1',
+                        PrimaryKey: true,
+                        AutoIncrement: true
+                    }
+                ]
+            };
+            var DataBase = <Model.IDataBase>{
+                Name: Database_Name,
+                Tables: [Table]
+            };
+            Connection = new JsStore.Instance().createDb(DataBase);
+        }
+    }, function (err) {
+        alert(err);
+    })
+}
+
+function selectData() {
+    var Query = <JsStore.ISelect>{
+        From: "TableName"
+    }
+    Connection.select(Query, function (results) {
+        console.log(results);
+    });
+}
+
+function clearTableData() {
+    Connection.clear("TableName", function () {
+        alert('Table cleared successfully');
+    });
+}
