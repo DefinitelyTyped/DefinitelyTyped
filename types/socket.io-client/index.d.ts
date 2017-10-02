b9880928539463ccb7496588c5971ff65ae02413
Zbyszek Wieczorek <zbyszek.wieczorek@gmail.com>
2017-10-02 13:03:25 +0200

    add managers property to SocketIOClientStatic
    which is exposed in lib/index.js
    
    /**
    * Managers cache.
    */
    var cache = exports.managers = {};

--- types/socket.io-client/index.d.ts
+++ types/socket.io-client/index.d.ts
@@ -1,4 +1,4 @@
-// Type definitions for socket.io-client 1.4.4
+// Type definitions for socket.io-client 1.4.5
 // Project: http://socket.io/
 // Definitions by: PROGRE <https://github.com/progre>, Damian Connolly <https://github.com/divillysausages>, Florent Poujol <https://github.com/florentpoujol>
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
