2e13401dbbbe7373c7c4d9ad7a35b6828d2a8e80
Nathan Shively-Sanders <nathansa@microsoft.com>
2017-10-03 13:43:36 -0700

    Fix test failures

--- types/hopscotch/index.d.ts
+++ types/hopscotch/index.d.ts
@@ -1,6 +1,6 @@
 // Type definitions for Hopscotch v0.2.5
 // Project: http://linkedin.github.io/hopscotch/
-// Definitions by: Tim Perry <https://github.com/pimterry> , Aurimas <https://github.com/Aurimas1>
+// Definitions by: Tim Perry <https://github.com/pimterry>, Aurimas <https://github.com/Aurimas1>
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
 
 declare type CallbackNameNamesOrDefinition = string | string[] | (() => void);
