306045a5ae71823ff5f670e56829748b48107481
Ashish Gaurav <ashishgaurav.iitd@gmail.com>
2017-10-04 18:44:36 +0530

    [dygraphs] correcting author url

--- types/dygraphs/index.d.ts
+++ types/dygraphs/index.d.ts
@@ -1,6 +1,6 @@
 // Type definitions for dygraphs 1.1.3
 // Project: http://dygraphs.com
-// Definitions by: Dan Vanderkam <http://danvk.org>
+// Definitions by: Dan Vanderkam <https://github.com/danvk>
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
 
 /// <reference types="google.visualization" />
