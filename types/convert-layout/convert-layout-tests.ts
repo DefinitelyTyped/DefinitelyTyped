4349d4c50fc57063d1c2849344987af32c44ef3d
Mikhail Aksenov <xeningem@gmail.com>
2017-10-05 19:52:11 +0300

    Fix tslint and common mistakes

--- types/convert-layout/convert-layout-tests.ts
+++ types/convert-layout/convert-layout-tests.ts
@@ -1,9 +1,5 @@
-import layouts from 'convert-layout'
-import ru from 'convert-layout/ru'
+import { ru } from 'convert-layout/ru';
 
 const s = 'Lorem ipsum dolor sit amet.';
-console.log(ru.toEn(s));
-console.log(ru.fromEn(s));
-
-layout = layouts["de"];
-console.log(layout.fromEn(s));
+let result = ru.toEn(s);
+result = ru.fromEn(s);
