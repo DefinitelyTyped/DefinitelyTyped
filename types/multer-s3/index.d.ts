2e67dcdaf9a339208bca8aa5b71d5fc78d23714a
Gal Talmor <galtalmor@users.noreply.github.com>
2017-10-05 19:05:44 -0700

    Update index.d.ts

--- types/multer-s3/index.d.ts
+++ types/multer-s3/index.d.ts
@@ -12,7 +12,7 @@ interface Options {
     key?(req: Express.Request, file: Express.Multer.File, callback: (error: any, key?: string) => void): void;
     acl?: ((req: Express.Request, file: Express.Multer.File, callback: (error: any, acl?: string) => void) => void) | string;
     contentType?(req: Express.Request, file: Express.Multer.File, callback: (error: any, mime?: string, stream?: NodeJS.ReadableStream) => void): void;
-    metadata?(req: Express.Request, file: Express.Multer.File, callback: (error: any, metadata?: object) => void): void;
+    metadata?(req: Express.Request, file: Express.Multer.File, callback: (error: any, metadata?: any) => void): void;
     cacheControl?: ((req: Express.Request, file: Express.Multer.File, callback: (error: any, cacheControl?: string) => void) => void) | string;
 }
 
