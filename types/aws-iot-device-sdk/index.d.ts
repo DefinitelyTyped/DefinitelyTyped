5e78b5912c40271449f87de45897d87d9893c064
Thomas Chia <thomas.chia@amaas.com>
2017-10-06 14:11:14 +0800

    Update version.

--- types/aws-iot-device-sdk/index.d.ts
+++ types/aws-iot-device-sdk/index.d.ts
@@ -1,4 +1,4 @@
-// Type definitions for aws-iot-device-sdk 1.0.13
+// Type definitions for aws-iot-device-sdk 2.1.0
 // Project: https://github.com/aws/aws-iot-device-sdk-js
 // Definitions by: Markus Olsson <https://github.com/niik>
 //                 Margus Lamp <https://github.com/mlamp>
