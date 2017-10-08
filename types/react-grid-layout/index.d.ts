b96709c6e6aa801e086619cdd5eb092a6c9f72e3
Andrew Hathaway <Andrew@andrewhathaway.net>
2017-10-08 17:52:37 +0100

    Add in author details

--- types/react-grid-layout/index.d.ts
+++ types/react-grid-layout/index.d.ts
@@ -2,7 +2,8 @@
 // Project: https://github.com/STRML/react-grid-layout
 // Definitions by: Andrew Birkholz <https://github.com/abirkholz>,
 //                 Ali Taheri <https://github.com/alitaheri>,
-//                 Zheyang Song <https://github.com/ZheyangSong>
+//                 Zheyang Song <https://github.com/ZheyangSong>,
+//                 Andrew Hathaway <https://github.com/andrewhathaway>
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
 // TypeScript Version: 2.3
 
