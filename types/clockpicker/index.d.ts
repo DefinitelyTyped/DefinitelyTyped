b3d4efa9dbf5c8efa8f8639f7756cd7d9df440c5
jfcere <jfcere@sherweb.com>
2017-10-07 22:02:37 -0400

    Removed dt-header rule and fix version

--- types/clockpicker/index.d.ts
+++ types/clockpicker/index.d.ts
@@ -1,4 +1,4 @@
-// Type definitions for ClockPicker 0.0.7
+// Type definitions for ClockPicker 0.0
 // Project: https://github.com/weareoutman/clockpicker
 // Definitions by: jfcere <https://github.com/jfcere>
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
