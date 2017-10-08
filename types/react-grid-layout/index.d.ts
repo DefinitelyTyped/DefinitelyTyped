b17f2b6b30ad7149f5ce3f181ad04673506ae40b
Andrew Hathaway <Andrew@andrewhathaway.net>
2017-10-08 17:48:42 +0100

    Bump version of typings

--- types/react-grid-layout/index.d.ts
+++ types/react-grid-layout/index.d.ts
@@ -1,4 +1,4 @@
-// Type definitions for react-grid-layout 0.14
+// Type definitions for react-grid-layout 0.16
 // Project: https://github.com/STRML/react-grid-layout
 // Definitions by: Andrew Birkholz <https://github.com/abirkholz>,
 //                 Ali Taheri <https://github.com/alitaheri>,
