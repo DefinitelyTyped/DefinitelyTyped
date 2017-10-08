4735663e9a1b522691469cc05ae81498bed877a6
John Gozde <john@gozde.ca>
2017-10-08 13:10:34 -0600

    create-react-class: add definitions

---
+++ types/create-react-class/index.d.ts
@@ -0,0 +1,12 @@
+// Type definitions for create-react-class 15.6
+// Project: https://facebook.github.io/react/
+// Definitions by: Asana <https://asana.com>, AssureSign <http://www.assuresign.com>, Microsoft <https://microsoft.com>
+// Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
+
+import { ComponentSpec, ClassicComponentClass } from "react";
+
+declare namespace createReactClass {}
+declare function createReactClass<P, S>(spec: ComponentSpec<P, S>): ClassicComponentClass<P>;
+
+export as namespace createReactClass;
+export = createReactClass;
