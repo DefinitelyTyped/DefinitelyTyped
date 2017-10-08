0108deabb8d7855ff9dc1ec9915e822b6380d2a8
Tanguy Krotoff <tkrotoff@gmail.com>
2017-10-09 01:18:06 +0200

    Add enzyme-adapter-react-15 and enzyme-adapter-react-16

---
+++ types/enzyme-adapter-react-15/index.d.ts
@@ -0,0 +1,12 @@
+// Type definitions for enzyme-adapter-react-15 1.0
+// Project: https://github.com/airbnb/enzyme
+// Definitions by: Tanguy Krotoff <https://github.com/tkrotoff>
+// Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
+
+declare class ReactFifteenAdapter {
+}
+
+declare namespace ReactFifteenAdapter {
+}
+
+export = ReactFifteenAdapter;
---
+++ types/enzyme-adapter-react-16/index.d.ts
@@ -0,0 +1,12 @@
+// Type definitions for enzyme-adapter-react-16 1.0
+// Project: https://github.com/airbnb/enzyme
+// Definitions by: Tanguy Krotoff <https://github.com/tkrotoff>
+// Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
+
+declare class ReactSixteenAdapter {
+}
+
+declare namespace ReactSixteenAdapter {
+}
+
+export = ReactSixteenAdapter;
