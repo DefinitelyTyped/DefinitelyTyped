ec9be35ecce86fe091916f9e78514c2a324a54a1
pc home <516226907@qq.com>
2017-10-08 09:42:28 +0800

    add @types/fuelux

---
+++ types/fuelux/index.d.ts
@@ -0,0 +1,30 @@
+// Type definitions for jQuery jqgrid Plugin 1.3
+// Project: https://github.com/ExactTarget/fuelux
+// Definitions by: xiao liu <https://github.com/LiuXiao617111>
+// Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
+// TypeScript Version: 2.3
+
+///<reference types="jquery" />
+
+// http://getfuelux.com/getting-started.html
+
+
+interface JQuery {
+    checkbox: string |any;
+    combobox : string |any;
+    datepicker: string |any;
+    infinitescroll: string |any;
+    loader: string |any;
+    pillbox: string |any;
+    placard: string |any;
+    radio: string |any;
+    repeater: string |any;
+    scheduler: string |any;
+    search: string |any;
+    selectlist: string |any;
+    spinbox: string |any;
+    tree: string  | any;
+    ace_tree: string  | any;
+    wizard: string  | any;
+}
+
