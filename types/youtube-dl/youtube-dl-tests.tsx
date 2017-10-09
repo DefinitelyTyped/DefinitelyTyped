5601f2cead88c00b5efa8b137fe4eb0d0b6ad582
bsurai <bs08@ukr.net>
2017-10-09 16:08:37 +0300

    fixes

--- types/youtube-dl/youtube-dl-tests.tsx
+++ types/youtube-dl/youtube-dl-tests.tsx
@@ -1,17 +1,19 @@
 import 'node';
 import * as fs from 'fs';
+
 const youtubedl = require('youtube-dl');
+
 const video = youtubedl('http://www.youtube.com/watch?v=90AiXO1pAiA',
   // Optional arguments passed to youtube-dl.
   ['--format=18'],
   // Additional options can be given for calling `child_process.execFile()`.
   { cwd: __dirname });
- 
+
 // Will be called when the download starts.
-video.on('info', function(info) {
+video.on('info', (info) => {
   console.log('Download started');
   console.log('filename: ' + info.filename);
   console.log('size: ' + info.size);
 });
- 
+
 video.pipe(fs.createWriteStream('myvideo.mp4'));
