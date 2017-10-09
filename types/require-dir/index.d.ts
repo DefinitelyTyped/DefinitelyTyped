bae92df29e54f2b7867c6841224ec9d474d350b0
weekens <weekens@gmail.com>
2017-10-09 16:25:04 +0300

    Added typings for "require-dir".

---
+++ types/require-dir/index.d.ts
@@ -0,0 +1,8 @@
+// Type definitions for require-dir 0.3
+// Project: https://github.com/aseemk/requireDir
+// Definitions by: My Self <https://github.com/me>
+// Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
+
+declare function requireDir(directory: string): { [path: string]: any };
+
+export = requireDir;
---
+++ types/require-dir/require-dir-tests.ts
@@ -0,0 +1,3 @@
+var requireDir = require('require-dir');
+
+requireDir('./test-directory');
