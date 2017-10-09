a21a051967e2169c89ff709d5cbc230101675005
Kuba Matjanowski <k.matjanowski@gmail.com>
2017-10-09 20:47:16 +0200

    updated tests

--- types/rivets/rivets-tests.ts
+++ types/rivets/rivets-tests.ts
@@ -1,6 +1,6 @@
+import { Rivets } from '.'
 
-
-rivets.configure({
+Rivets.configure({
     // Attribute prefix in templates
     prefix: 'rv',
     // Preload templates with initial data on bind
@@ -10,13 +10,13 @@ rivets.configure({
     // Template delimiters for text bindings
     templateDelimiters: ['[[', ']]'],
     // Augment the event handler of the on-* binder
-    handler: function(target:any, event:any, binding:any) {
+    handler: function (target: any, event: any, binding: any) {
         this.call(target, event, binding.view.models)
     }
 });
 
-var t = {test: ["hello", "one", "two"]}
-var opts = {bar: "foo"};
-rivets.bind(document.getElementById("para1"), t);
-rivets.bind(document.getElementById("para1"), t, opts);
-rivets.bind([document.getElementById("para1"), document.getElementById("para2")], t);
+var t = { test: ["hello", "one", "two"] }
+var opts = { bar: "foo" };
+Rivets.bind(document.getElementById("para1"), t);
+Rivets.bind(document.getElementById("para1"), t, opts);
+Rivets.bind([document.getElementById("para1"), document.getElementById("para2")], t);
