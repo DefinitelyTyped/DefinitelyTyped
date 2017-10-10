437cda25b8c573d6d31740f8634731b0b51db7f4
karak <karak97@gmail.com>
2017-10-10 10:00:34 +0900

    Replace mixed tabs by spaces.

--- types/diff-match-patch/index.d.ts
+++ types/diff-match-patch/index.d.ts
@@ -46,17 +46,17 @@ export class diff_match_patch {
     diff_fromDelta(text1: string, delta: string): Diff[];
 
 	match_main(text: string, pattern: string, loc: number): number;
-	match_bitap_(text: string, pattern: string, loc: number): number;
-	match_alphabet_(pattern: string): {[char: string]: number};
+    match_bitap_(text: string, pattern: string, loc: number): number;
+    match_alphabet_(pattern: string): {[char: string]: number};
 
-	patch_addContext_(patch: patch_obj, text: string): void;
+    patch_addContext_(patch: patch_obj, text: string): void;
     patch_make(a: string, opt_b: string | Diff[]): patch_obj[];
     patch_make(a: Diff[]): patch_obj[];
 	patch_make(a: string, opt_b: string, opt_c: Diff[]): patch_obj[];
     patch_deepCopy(patches: patch_obj[]): patch_obj[];
     patch_apply(patches: patch_obj[], text: string): [string, boolean[]];
 	patch_addPadding(patches: patch_obj[]): string;
-	patch_splitMax(patches: patch_obj[]): void;
+    patch_splitMax(patches: patch_obj[]): void;
     patch_fromText(text: string): patch_obj[];
     patch_toText(patches: patch_obj[]): string;
 }
