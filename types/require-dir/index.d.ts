36d1baea3176282ebf776ddc49f18b78cbd7e3a2
weekens <weekens@gmail.com>
2017-10-10 11:52:53 +0300

    Fixed By field.

--- types/require-dir/index.d.ts
+++ types/require-dir/index.d.ts
@@ -1,6 +1,6 @@
 // Type definitions for require-dir 0.3
 // Project: https://github.com/aseemk/requireDir
-// Definitions by: My Self <https://github.com/me>
+// Definitions by: weekens <https://github.com/weekens>
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
 
 declare function requireDir(directory: string): { [path: string]: any };
