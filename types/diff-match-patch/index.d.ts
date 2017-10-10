335a74114ed726d0b249922653548637be0a983e
karak <karak97@gmail.com>
2017-10-11 07:54:34 +0900

    Replace more tabs by spaces.

--- types/diff-match-patch/index.d.ts
+++ types/diff-match-patch/index.d.ts
@@ -25,13 +25,13 @@ export class diff_match_patch {
     Match_MaxBits: number;
 
     diff_main(text1: string, text2: string, opt_checklines?: boolean, opt_deadline?: number): Diff[];
-	diff_bisect_(text1: string, text2: string, deadline: number): Diff[];
-	diff_linesToChars_(text1: string, text2: string): { chars1: string; chars2: string; lineArray: string[]; };
-	diff_charsToLines_(diffs: Diff[], lineArray: string[]): void;
+    diff_bisect_(text1: string, text2: string, deadline: number): Diff[];
+    diff_linesToChars_(text1: string, text2: string): { chars1: string; chars2: string; lineArray: string[]; };
+    diff_charsToLines_(diffs: Diff[], lineArray: string[]): void;
     diff_commonPrefix(text1: string, text2: string): number;
     diff_commonSuffix(text1: string, text2: string): number;
-	diff_commonOverlap_(text1: string, text2: string): number;
-	diff_halfMatch_(text1: string, text2: string): string[];
+    diff_commonOverlap_(text1: string, text2: string): number;
+    diff_halfMatch_(text1: string, text2: string): string[];
     diff_cleanupSemantic(diffs: Diff[]): void;
     diff_cleanupSemanticLossless(diffs: Diff[]): void;
     diff_cleanupEfficiency(diffs: Diff[]): void;
@@ -44,17 +44,17 @@ export class diff_match_patch {
     diff_toDelta(diffs: Diff[]): string;
     diff_fromDelta(text1: string, delta: string): Diff[];
 
-	match_main(text: string, pattern: string, loc: number): number;
+    match_main(text: string, pattern: string, loc: number): number;
     match_bitap_(text: string, pattern: string, loc: number): number;
     match_alphabet_(pattern: string): {[char: string]: number};
 
     patch_addContext_(patch: patch_obj, text: string): void;
     patch_make(a: string, opt_b: string | Diff[]): patch_obj[];
     patch_make(a: Diff[]): patch_obj[];
-	patch_make(a: string, opt_b: string, opt_c: Diff[]): patch_obj[];
+    patch_make(a: string, opt_b: string, opt_c: Diff[]): patch_obj[];
     patch_deepCopy(patches: patch_obj[]): patch_obj[];
     patch_apply(patches: patch_obj[], text: string): [string, boolean[]];
-	patch_addPadding(patches: patch_obj[]): string;
+    patch_addPadding(patches: patch_obj[]): string;
     patch_splitMax(patches: patch_obj[]): void;
     patch_fromText(text: string): patch_obj[];
     patch_toText(patches: patch_obj[]): string;
