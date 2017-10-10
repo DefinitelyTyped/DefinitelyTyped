da0552598f141ecef5416ac3ea13745b1eecd24e
Alexandre Rieux <alexr.3165@gmail.com>
2017-10-10 22:42:12 +0100

    Increase version and fix ts options

--- types/mapbox-gl/index.d.ts
+++ types/mapbox-gl/index.d.ts
@@ -1,4 +1,4 @@
-// Type definitions for Mapbox GL JS v0.39.1
+// Type definitions for Mapbox GL JS v0.40.1
 // Project: https://github.com/mapbox/mapbox-gl-js
 // Definitions by: Dominik Bruderer <https://github.com/dobrud>, Patrick Reames <https://github.com/patrickr>
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
