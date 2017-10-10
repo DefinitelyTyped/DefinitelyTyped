9bd84109f4721923661b448f0d0065fb0d9bd702
Rasmus Vermeulen <r.vermeulen@xit.nl>
2017-10-10 14:58:32 +0200

    Bumped version to current

--- types/sequelize-fixtures/index.d.ts
+++ types/sequelize-fixtures/index.d.ts
@@ -1,4 +1,4 @@
-// Type definitions for Sequelize-Fixtures 0.4.7
+// Type definitions for Sequelize-Fixtures 0.6.0
 // Project: https://github.com/domasx2/sequelize-fixtures
 // Definitions by: Christian Schwarz <https://github.com/cschwarz>
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
