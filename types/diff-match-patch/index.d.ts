b165f9c5663aebf242caddad63a13b3f8cb3a6e4
karak <karak97@gmail.com>
2017-10-11 09:41:11 +0900

    Reverted header and disable a relevant rule of lint.

--- types/diff-match-patch/index.d.ts
+++ types/diff-match-patch/index.d.ts
@@ -1,6 +1,6 @@
 // Type definitions for diff-match-patch 1.0
 // Project: https://www.npmjs.com/package/diff-match-patch
-// Definitions by: Asana <https://github.com/asana>
+// Definitions by: Asana <https://asana.com>
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
 
 export type Diff = [number, string];
