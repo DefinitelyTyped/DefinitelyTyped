6fb3f5d2d476d87f661971e17ca586ef0f259ec5
Alex Coles <alex@alexbcoles.com>
2017-10-12 01:37:35 +0100

    Point jest-diff project URL to GitHub

--- types/jest-diff/index.d.ts
+++ types/jest-diff/index.d.ts
@@ -1,5 +1,5 @@
 // Type definitions for jest-diff 20.0
-// Project: http://facebook.github.io/jest/
+// Project: https://github.com/facebook/jest/tree/master/packages/jest-diff
 // Definitions by: Alex Coles <https://github.com/myabc>
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
 // TypeScript Version: 2.2
