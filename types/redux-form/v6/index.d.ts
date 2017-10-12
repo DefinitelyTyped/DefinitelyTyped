69be318b5ba292df7dd5ca885646f11b7926d502
Mihhail Arhipov <mihhail.arhipov@gmail.com>
2017-10-12 12:41:09 +0100

    Changed the type from string to any to match v7

--- types/redux-form/v6/index.d.ts
+++ types/redux-form/v6/index.d.ts
@@ -16,7 +16,7 @@ export type FieldState = {
     active?: boolean;
     touched?: boolean;
     visited?: boolean;
-    error?: string;
+    error?: any;
 }
 
 export type FieldType = "Field" | "FieldArray";
