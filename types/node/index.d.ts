1ee1cb383ef91536e835ccd005390d5de7cb33bd
Jinwoo Lee <jinwoo68@gmail.com>
2017-10-12 10:29:37 -0700

    Add types for Writable#_writev() & Duplex#_writev().
    As written in the doc:
    https://nodejs.org/dist/latest-v8.x/docs/api/stream.html#stream_writable_writev_chunks_callback

--- types/node/index.d.ts
+++ types/node/index.d.ts
@@ -5110,6 +5110,7 @@ declare module "stream" {
             writable: boolean;
             constructor(opts?: WritableOptions);
             _write(chunk: any, encoding: string, callback: Function): void;
+            _writev(chunks: Array<{chunk: any, encoding: string}>, callback: Function): void;
             _destroy(err: Error, callback: Function): void;
             _final(callback: Function): void;
             write(chunk: any, cb?: Function): boolean;
@@ -5200,6 +5201,7 @@ declare module "stream" {
             writable: boolean;
             constructor(opts?: DuplexOptions);
             _write(chunk: any, encoding: string, callback: Function): void;
+            _writev(chunks: Array<{chunk: any, encoding: string}>, callback: Function): void;
             _destroy(err: Error, callback: Function): void;
             _final(callback: Function): void;
             write(chunk: any, cb?: Function): boolean;
