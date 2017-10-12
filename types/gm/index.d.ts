11cf322a61e482f6749ffc45b10d9579bc16ac1b
Maarten van Vliet <maartenvanvliet@gmail.com>
2017-10-12 19:30:11 +0200

    Add myself to definitions editors field

--- types/gm/index.d.ts
+++ types/gm/index.d.ts
@@ -1,6 +1,6 @@
 // Type definitions for gm 1.17
 // Project: https://github.com/aheckmann/gm
-// Definitions by: Joel Spadin <https://github.com/ChaosinaCan>
+// Definitions by: Joel Spadin <https://github.com/ChaosinaCan>, Maarten van Vliet <https://github.com/maartenvanvliet>
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
 
 /// <reference types="node"/>
