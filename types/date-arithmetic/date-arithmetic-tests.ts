91bde315946a9e5752813dd59f3e63f230248e8f
HeeL <parizhskiy@gmail.com>
2017-10-13 16:28:03 +0200

    Add tests for date-arithmetic package

---
+++ types/date-arithmetic/date-arithmetic-tests.ts
@@ -0,0 +1,11 @@
+import dateArithmetic = require('dateArithmetic');
+
+dateArithmetic.add(new Date(2010, 7, 23), 2, 'second');
+dateArithmetic.add(new Date(2010, 7, 23), 2, 'minutes');
+dateArithmetic.add(new Date(2010, 7, 23), 2, 'hours');
+dateArithmetic.add(new Date(2010, 7, 23), 2, 'day');
+dateArithmetic.add(new Date(2010, 7, 23), 2, 'week');
+dateArithmetic.add(new Date(2010, 7, 23), 2, 'month');
+dateArithmetic.add(new Date(2010, 7, 23), 2, 'year');
+dateArithmetic.add(new Date(2010, 7, 23), 2, 'decade');
+dateArithmetic.add(new Date(2010, 7, 23), 2, 'century');
