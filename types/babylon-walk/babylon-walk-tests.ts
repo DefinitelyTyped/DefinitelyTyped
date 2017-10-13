cbeeddc4bbbda184734b0bfa3207c6a5b8cea3d9
Marek <marek.b@cstechnologies.cz>
2017-10-13 15:40:21 +0200

    test definitions fix

--- types/babylon-walk/babylon-walk-tests.ts
+++ types/babylon-walk/babylon-walk-tests.ts
@@ -1,30 +1,32 @@
-// Example from https://github.com/thejameskyle/babel-handbook/blob/master/translations/en/plugin-handbook.md#babylon
-import * as walk from 'babylon-walk';
+import * as babelTypes from "babel-types";
+import * as walk from "babylon-walk";
 declare function assert(expr: boolean): void;
 
-const simpleVisitors = {
-    File(node, state) {
+const simpleVisitors: walk.visitors<walk.SimpleVisitor> = {
+    File: (node: babelTypes.Node, state: any) => {
         state.out = 1;
-    }
+    },
 };
 
-const ancestorVisitors = {
-    File(node, state, ancestors) {
+const ancestorVisitors: walk.visitors<walk.AncestorVisitor> = {
+    File: (node: babelTypes.Node, state: any, ancestors: babelTypes.Node[]) => {
         state.out = 2;
-    }
+    },
 };
 
-const recursiveVisitors = {
-    File(node, state, next){
+const recursiveVisitors: walk.visitors<walk.RecursiveVisitor> = {
+    File: (node: babelTypes.Node, state: any, next: (node: babelTypes.Node) => void) => {
         state.out = 3;
-    }
+    },
 };
 
-const node = {
-    type: 'File'
+const node: any = {
+    type: "File",
 };
 
-const state = {};
+const state: any = {
+    out: 0,
+};
 walk.simple(node, simpleVisitors, state);
 assert(state.out === 1);
 
