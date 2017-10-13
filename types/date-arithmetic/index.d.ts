6b0f6adcbdc26e88344ef4edacf1d32268ca38fa
HeeL <parizhskiy@gmail.com>
2017-10-13 16:27:24 +0200

    Add type definitions and config to new package date-arithmetic

---
+++ types/date-arithmetic/index.d.ts
@@ -0,0 +1,17 @@
+// Type definitions for date-arithmetic v3.1.0
+// Project: https://github.com/jquense/date-math
+// Definitions by: Sergii Paryzhskyi <https://github.com/HeeL>
+// Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
+
+type Unit = 'second' | 'minutes' | 'hours' | 'day' | 'week' | 'month' | 'year' | 'decade' | 'century';
+
+/** dateArithmetic Public Instance Methods */
+interface dateArithmeticStatic {
+    /** Add specified amount of units to a  provided date and return new date as a result */
+    add(date: Date, num: number, unit: Unit): Date;
+}
+
+declare module 'dateArithmetic' {
+    const dateArithmetic: dateArithmeticStatic;
+    export = dateArithmetic;
+}
