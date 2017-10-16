55a284355b800bef9c4652f8b3fe9b0e56287a2b
Martijn Schrage <martijn@oblomov.com>
2017-10-16 17:47:24 +0200

    Add missing method readline.emitKeypressEvents to package node (#19733)

--- types/node/node-tests.ts
+++ types/node/node-tests.ts
@@ -1672,6 +1672,14 @@ namespace readline_tests {
     }
 
     {
+        let stream: NodeJS.ReadableStream;
+        let readLineInterface: readline.ReadLine;
+
+        readline.emitKeypressEvents(stream);
+        readline.emitKeypressEvents(stream, readLineInterface);
+    }
+
+    {
         let stream: NodeJS.WritableStream;
         let dx: number | string;
         let dy: number | string;
