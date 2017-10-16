ea8b7af8c6b87f37396342ade7cfd4c5298ac6f6
kazuyamamoto <kazu@rao.co.jp>
2017-10-17 05:16:08 +0900

    [node] Add tls.getCiphers() and tls.DEFAULT_ECDH_CURVE (#20482)
    https://nodejs.org/api/tls.html#tls_tls_getciphers
    https://nodejs.org/api/tls.html#tls_tls_default_ecdh_curve

--- types/node/node-tests.ts
+++ types/node/node-tests.ts
@@ -979,6 +979,9 @@ namespace tls_tests {
             port: 55
         };
         var tlsSocket = tls.connect(connOpts);
+
+        const ciphers: string[] = tls.getCiphers();
+        const curve: string = tls.DEFAULT_ECDH_CURVE;
     }
 
     {
