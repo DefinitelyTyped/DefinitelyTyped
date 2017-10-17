bdde889f7bd6086fe35e8bf539e3d40e6d37f524
Alessandro Vergani <alessandro.vergani@gmail.com>
2017-10-17 18:16:06 +0200

    Add lookup to dgram SocketOptions (#20636)

--- types/node/node-tests.ts
+++ types/node/node-tests.ts
@@ -1375,6 +1375,7 @@ namespace dgram_tests {
         });
         ds.send(new Buffer("hello"), 5000, "127.0.0.1");
         ds.setMulticastInterface("127.0.0.1");
+        ds = dgram.createSocket({ type: "udp4", reuseAddr: true, recvBufferSize: 1000, sendBufferSize: 1000, lookup: dns.lookup });
     }
 
     {
