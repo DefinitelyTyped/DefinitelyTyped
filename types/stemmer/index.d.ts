ac66f610d4a4c7e56fe6dfd0131de2f9f70f6859
Will Ockmore <will.ockmore@gmail.com>
2017-10-18 10:47:12 +0100

    Add types for stemmer package
    Adds type definition for single default function in
    package.

---
+++ types/stemmer/index.d.ts
@@ -0,0 +1,6 @@
+// Type definitions for stemmer 1.0
+// Project: https://github.com/wooorm/stemmer#readme
+// Definitions by: Will Ockmore <https://github.com/will-ockmore>
+// Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
+
+export default function stemmer(value: string): string;
