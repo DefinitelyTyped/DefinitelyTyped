b84f5b0d7e604a572e14c6c1f04fb80d9b94406e
bsurai <bs08@ukr.net>
2017-10-18 09:06:55 +0300

    fixes

--- types/youtube-dl/index.d.ts
+++ types/youtube-dl/index.d.ts
@@ -7,17 +7,16 @@
 /// <reference types="node" />
 import * as fs from "fs";
 
-interface Info {
-    _filename: string;
-    filename: string;
-    size: number;
-}
-
-interface Youtubedl {
-    (url: string, arg: string[], opt: {[key: string]: string}): this;
-    on(event: string, func: (info: Info) => void): this;
-    pipe(stream: fs.WriteStream): this;
-}
-
-declare var youtubedl: Youtubedl;
 export = youtubedl;
+declare function youtubedl(url: string, arg: string[], opt: {[key: string]: string}): youtubedl.Youtubedl;
+declare namespace youtubedl {
+    interface Youtubedl {
+        on(event: string, func: (info: Info) => void): this;
+        pipe(stream: fs.WriteStream): this;
+    }
+    interface Info {
+        _filename: string;
+        filename: string;
+        size: number;
+    }
+}
