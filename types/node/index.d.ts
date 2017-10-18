0960f82a0b929849dfa6ebc079af5918f98aebc5
Oliver Joseph Ash <oliverjash@gmail.com>
2017-10-18 16:56:47 +0100

    Correct type of Node url.query (#20650)
    * Correct type of Node url.query
    
    https://nodejs.org/api/url.html#url_urlobject_query
    
    * Add null
    
    * Node url.query: query value as array of strings
    
    * Node: narrow query type to fix tests

--- types/node/index.d.ts
+++ types/node/index.d.ts
@@ -2181,7 +2181,7 @@ declare module "url" {
         pathname?: string;
         port?: string | number;
         protocol?: string;
-        query?: string | { [key: string]: any; };
+        query?: string | null | { [key: string]: string | string[] };
         search?: string;
         slashes?: boolean;
     }
