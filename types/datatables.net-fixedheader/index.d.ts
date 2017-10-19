7cb9690193c7a54545deebc1aadd1bb376947c76
Kiarash <kiarash@si-co.net>
2017-10-19 04:40:58 +0200

    Fix: Add TypeScript Version

--- types/datatables.net-fixedheader/index.d.ts
+++ types/datatables.net-fixedheader/index.d.ts
@@ -2,6 +2,8 @@
 // Project: https://datatables.net/extensions/fixedheader/
 // Definitions by: Jared Szechy <https://github.com/szechyjs>, Kiarash Ghiaseddin <https://github.com/Silver-Connection>
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
+// TypeScript Version: 2.3
+
 /// <reference types="jquery" />
 /// <reference types="datatables.net"/>
 
