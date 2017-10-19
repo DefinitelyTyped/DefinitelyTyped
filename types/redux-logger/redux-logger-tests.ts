5ba9961605ca984239b1697c8df9c3a2bf3dfaa5
Cory Deppen <cory@deppen.us>
2017-10-19 17:20:00 -0500

    Enable linting
    Resolve errors and comply with DT formatting recommendations.

--- types/redux-logger/redux-logger-tests.ts
+++ types/redux-logger/redux-logger-tests.ts
@@ -1,105 +1,104 @@
-import { createLogger } from 'redux-logger';
-import logger from 'redux-logger';
+import logger, { createLogger } from 'redux-logger';
 import { applyMiddleware, createStore } from 'redux';
 
-let loggerSimple = createLogger();
+const loggerSimple = createLogger();
 
-let loggerSimpleOpts = createLogger({
-  duration: true,
-  timestamp: true,
-  logger: console,
-  logErrors: true,
-  predicate: (getState, action) => true,
-  stateTransformer: state => state,
-  actionTransformer: action => action,
-  errorTransformer: error => error,
-  diff: true,
-  diffPredicate: (getState, action) => true
+const loggerSimpleOpts = createLogger({
+    duration: true,
+    timestamp: true,
+    logger: console,
+    logErrors: true,
+    predicate: (getState, action) => true,
+    stateTransformer: state => state,
+    actionTransformer: action => action,
+    errorTransformer: error => error,
+    diff: true,
+    diffPredicate: (getState, action) => true
 });
 
-let loggerCollapsedBool = createLogger({
-  collapsed: true
+const loggerCollapsedBool = createLogger({
+    collapsed: true
 });
 
-let loggerCollapsedPredicate = createLogger({
-  collapsed: (getAction, action) => true
+const loggerCollapsedPredicate = createLogger({
+    collapsed: (getAction, action) => true
 });
 
-let loggerCollapsedLogEntryPredicate = createLogger({
-  collapsed: (getAction, action, logEntry) =>
-    logEntry !== undefined && !logEntry.error
+const loggerCollapsedLogEntryPredicate = createLogger({
+    collapsed: (getAction, action, logEntry) =>
+        logEntry !== undefined && !logEntry.error
 });
 
-let loggerColorsOverallBoolean = createLogger({
-  colors: false
+const loggerColorsOverallBoolean = createLogger({
+    colors: false
 });
 
-let loggerColorsBoolean = createLogger({
-  colors: {
-    title: false,
-    prevState: false,
-    action: false,
-    nextState: false,
-    error: false
-  }
+const loggerColorsBoolean = createLogger({
+    colors: {
+        title: false,
+        prevState: false,
+        action: false,
+        nextState: false,
+        error: false
+    }
 });
 
-let loggerColorsFunction = createLogger({
-  colors: {
-    title: action => '#000',
-    prevState: state => '#000',
-    action: action => '#000',
-    nextState: state => '#000',
-    error: (error, prevState) => '#000'
-  }
+const loggerColorsFunction = createLogger({
+    colors: {
+        title: action => '#000',
+        prevState: state => '#000',
+        action: action => '#000',
+        nextState: state => '#000',
+        error: (error, prevState) => '#000'
+    }
 });
 
-let loggerLevelString = createLogger({
-  level: 'log'
+const loggerLevelString = createLogger({
+    level: 'log'
 });
 
-let loggerLevelFunction = createLogger({
-  level: action => 'log'
+const loggerLevelFunction = createLogger({
+    level: action => 'log'
 });
 
-let loggerLevelObjectFunction = createLogger({
-  level: {
-    prevState: state => 'log',
-    action: action => 'log',
-    nextState: state => 'log',
-    error: (error, prevState) => 'log'
-  }
+const loggerLevelObjectFunction = createLogger({
+    level: {
+        prevState: state => 'log',
+        action: action => 'log',
+        nextState: state => 'log',
+        error: (error, prevState) => 'log'
+    }
 });
 
-let loggerLevelObjectBoolean = createLogger({
-  level: {
-    prevState: false,
-    action: false,
-    nextState: false,
-    error: false
-  }
+const loggerLevelObjectBoolean = createLogger({
+    level: {
+        prevState: false,
+        action: false,
+        nextState: false,
+        error: false
+    }
 });
 
-let loggerLevelObjectString = createLogger({
-  level: {
-    prevState: 'log',
-    action: 'log',
-    nextState: 'log',
-    error: 'log'
-  }
+const loggerLevelObjectString = createLogger({
+    level: {
+        prevState: 'log',
+        action: 'log',
+        nextState: 'log',
+        error: 'log'
+    }
 });
 
-let createStoreWithMiddleware = applyMiddleware(
-  logger,
-  loggerSimpleOpts,
-  loggerCollapsedBool,
-  loggerCollapsedPredicate,
-  loggerCollapsedLogEntryPredicate,
-  loggerColorsBoolean,
-  loggerColorsFunction,
-  loggerLevelString,
-  loggerLevelFunction,
-  loggerLevelObjectFunction,
-  loggerLevelObjectBoolean,
-  loggerLevelObjectString
+const createStoreWithMiddleware = applyMiddleware(
+    logger,
+    loggerSimpleOpts,
+    loggerCollapsedBool,
+    loggerCollapsedPredicate,
+    loggerCollapsedLogEntryPredicate,
+    loggerColorsBoolean,
+    loggerColorsFunction,
+    loggerLevelString,
+    loggerLevelFunction,
+    loggerLevelObjectFunction,
+    loggerLevelObjectBoolean,
+    loggerLevelObjectString
 )(createStore);
