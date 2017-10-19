928ece6644c1e7798c7b88f7d6b5a50d0fbd0164
Simon McClive <simon.mcclive@gmail.com>
2017-10-19 19:02:58 +0100

    Remove multiple calls of parent() in tests

--- types/parent-package-json/parent-package-json-tests.ts
+++ types/parent-package-json/parent-package-json-tests.ts
@@ -1,13 +1,10 @@
 import * as parent from 'parent-package-json';
 
 const hasParent = parent();
+
 if (hasParent !== false) {
-    const pathToParent: string = parent().path;
-    const pathToParentOfCustomPath: string = parent('/My/Cool/Folder').path;
-    const pathToParentOfParent: string = parent(null, 1).path;
-    const contentOfParent: string = parent().read();
-    const JSONOfParent = parent().parse();
+    const pathToParent: string = hasParent.path;
+    const contentOfParent: string = hasParent.read();
+    const JSONOfParent = hasParent.parse();
     const versionOfParent = JSONOfParent.version;
 }
-
-const hasParentFalse: false = parent();
