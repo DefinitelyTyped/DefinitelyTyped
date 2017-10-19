b4e99c01a95804d9afa0952d581a65829c552656
Oz Weiss <thewizarodofoz@gmail.com>
2017-10-19 17:27:47 +0300

    fix Worker.id type
    https://nodejs.org/docs/latest-v7.x/api/cluster.html#cluster_worker_id

--- types/node/v7/index.d.ts
+++ types/node/v7/index.d.ts
@@ -850,7 +850,7 @@ declare module "cluster" {
     }
 
     export class Worker extends events.EventEmitter {
-        id: string;
+        id: number;
         process: child.ChildProcess;
         suicide: boolean;
         send(message: any, sendHandle?: any, callback?: (error: Error) => void): boolean;
