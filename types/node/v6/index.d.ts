66711b599c84cfd8797ce9b9bc5e4f2237ac1e6d
Oz Weiss <thewizarodofoz@gmail.com>
2017-10-19 21:31:34 +0300

    worker.id is number, not string

--- types/node/v6/index.d.ts
+++ types/node/v6/index.d.ts
@@ -833,7 +833,7 @@ declare module "cluster" {
     }
 
     export class Worker extends events.EventEmitter {
-        id: string;
+        id: number;
         process: child.ChildProcess;
         suicide: boolean;
         send(message: any, sendHandle?: any, callback?: (error: Error) => void): boolean;
