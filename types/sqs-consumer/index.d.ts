39214997527a0b82ca06db2bd98ed1948561a411
Ezinwa Okpoechi <brainmaestro@outlook.com>
2017-10-20 12:11:49 +0200

    Fix indentation to 4 spaces

--- types/sqs-consumer/index.d.ts
+++ types/sqs-consumer/index.d.ts
@@ -1,6 +1,8 @@
-// Type definitions for sqs-consumer 3.5
+// Type definitions for sqs-consumer 3.7
 // Project: https://github.com/BBC/sqs-consumer
-// Definitions by: Daniel Chao <http://dchao.co/>, Eric Byers <http://www.github.com/EricByers>
+// Definitions by: Daniel Chao <http://dchao.co/>
+//                 Eric Byers <http://github.com/EricByers>
+//                 Ezinwa Okpoechi <http://github.com/BrainMaestro>
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
 
 /// <reference types="node" />
@@ -9,24 +11,24 @@ import { SQS } from "aws-sdk";
 export type ConsumerDone = (error?: SQSError) => void;
 
 export interface Options {
-  queueUrl: string;
-  handleMessage(message: SQS.Message, done: ConsumerDone): any;
-  region?: string;
-  attributeNames?: string[];
-  messageAttributeNames?: string[];
-  batchSize?: number;
-  visibilityTimeout?: number;
-  waitTimeSeconds?: number;
-  authenticationErrorTimeout?: number;
-  sqs?: SQS;
+    queueUrl: string;
+    handleMessage(message: SQS.Message, done: ConsumerDone): any;
+    region?: string;
+    attributeNames?: string[];
+    messageAttributeNames?: string[];
+    batchSize?: number;
+    visibilityTimeout?: number;
+    waitTimeSeconds?: number;
+    authenticationErrorTimeout?: number;
+    sqs?: SQS;
 }
 
 declare class SQSError extends Error {}
 
 declare class Consumer extends NodeJS.EventEmitter {
-  start(): void;
-  stop(): void;
-  static create(opts: Options): Consumer;
+    start(): void;
+    stop(): void;
+    static create(opts: Options): Consumer;
 }
 
 export default Consumer;
