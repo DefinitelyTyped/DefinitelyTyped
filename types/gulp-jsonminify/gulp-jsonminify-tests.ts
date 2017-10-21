e6812e340fd90084e8cf889225e20dc8f3cbebd6
Pine Mizune <pinemz@gmail.com>
2017-10-21 22:43:24 +0900

    fix ci errors

--- types/gulp-jsonminify/gulp-jsonminify-tests.ts
+++ types/gulp-jsonminify/gulp-jsonminify-tests.ts
@@ -1,8 +1,8 @@
 import * as gulp from 'gulp';
 import * as jsonminify from 'gulp-jsonminify';
 
-gulp.task('minify', function() {
+gulp.task('minify', () => {
   return gulp.src('src/*.json')
     .pipe(jsonminify())
-    .pipe(gulp.dest('dist'))
+    .pipe(gulp.dest('dist'));
 });
