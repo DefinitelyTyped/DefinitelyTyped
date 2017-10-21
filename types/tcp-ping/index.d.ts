b69294ad4a3918fe9ad065a0b52c14d265194c9b
Stegano <stegano@naver.com>
2017-10-22 02:01:29 +0900

    Update: Apply the DTSLint rules.

--- types/tcp-ping/index.d.ts
+++ types/tcp-ping/index.d.ts
@@ -1,56 +1,54 @@
-// Type definitions for tcp-ping 0.1.1
+// Type definitions for tcp-ping 0.1
 // Project: https://github.com/wesolyromek/tcp-ping/issues
 // Definitions by: JUNG YONG WOO <https://github.com/stegano>
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
 
-type options = {
-    address?: string,
-    port?: number,
-    attempts?: number,
-    timeout?: number
-};
-
-declare module 'tcp-ping' {
-    /**
-     * ping
-     * @param options {Object}
-     * @param callback {Function}
-     * @return void
-     * */
-    function ping(options: options,
-                  callback: (error: Error, result: {
-                      address: string,
-                      port: number,
-                      attempts: number,
-                      avg: number,
-                      max: number,
-                      min: number,
-                      results: {
-                          seq: number,
-                          time: number,
-                          error: Error
-                      }
-                  }) => void): void;
-
-    /**
-     * probe
-     * @param address [string]
-     * @param port [number]
-     * @param callback {Function}
-     * @return void
-     * */
-    function probe(address: string, port: number,
-                   callback: (error: Error, result: {
-                       address: string,
-                       port: number,
-                       attempts: number,
-                       avg: number,
-                       max: number,
-                       min: number,
-                       results: {
-                           seq: number,
-                           time: number,
-                           error: Error
-                       }
-                   }) => void): void;
+export interface Option {
+    address?: string;
+    port?: number;
+    attempts?: number;
+    timeout?: number;
 }
+
+/**
+ * ping
+ * @param options {Object}
+ * @param callback {Function}
+ * @return void
+ */
+export function ping(options: Option,
+              callback: (error: Error, result: {
+                  address: string,
+                  port: number,
+                  attempts: number,
+                  avg: number,
+                  max: number,
+                  min: number,
+                  results: {
+                      seq: number,
+                      time: number,
+                      error: Error
+                  }
+              }) => void): void;
+
+/**
+ * probe
+ * @param address [string]
+ * @param port [number]
+ * @param callback {Function}
+ * @return void
+ */
+export function probe(address: string, port: number,
+               callback: (error: Error, result: {
+                   address: string,
+                   port: number,
+                   attempts: number,
+                   avg: number,
+                   max: number,
+                   min: number,
+                   results: {
+                       seq: number,
+                       time: number,
+                       error: Error
+                   }
+               }) => void): void;
