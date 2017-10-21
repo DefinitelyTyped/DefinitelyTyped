d137285ea0bba4ab3655b584da09ae6d3ef075ef
Stegano <stegano@naver.com>
2017-10-22 02:50:13 +0900

    Change: Removes all unnecessary characters.

--- types/tcp-ping/tcp-ping-tests.ts
+++ types/tcp-ping/tcp-ping-tests.ts
@@ -1,12 +1,10 @@
 import * as tp from 'tcp-ping';
 
 tp.ping({
-    "address": "localhost",
-    "port": 8080
+    address: "localhost",
+    port: 8080
 }, (err, result) => {
-    console.log(result);
 });
 
 tp.probe("localhost", 8080, (err, result) => {
-    console.log(result);
 });
