44f2375b73ac3b84790ac38bd8a38f35de3db14c
doomsower <K.Kuznetcov@gmail.com>
2017-10-22 11:17:41 +0300

    Export BaseFieldProps

--- types/redux-form/redux-form-tests.tsx
+++ types/redux-form/redux-form-tests.tsx
@@ -11,6 +11,7 @@ import {
     formValueSelector,
     Field,
     GenericField,
+    BaseFieldProps,
     WrappedFieldProps,
     Fields,
     GenericFields,
@@ -124,6 +125,11 @@ const MyField: StatelessComponent<MyFieldProps> = ({
     return null;
 };
 const FieldCustom = Field as new () => GenericField<MyFieldCustomProps>;
+
+type FieldProps = BaseFieldProps<MyFieldCustomProps> & MyFieldCustomProps;
+const FieldCustomComp: StatelessComponent<FieldProps> = props => (
+    <FieldCustom {...props} component={MyField} />
+)
 
 const MyFieldImm: StatelessComponent<MyFieldProps> = ({
     children,
@@ -259,6 +265,11 @@ const Test = reduxForm({
                             <FieldCustom
                                 name="field4"
                                 component={ MyField }
+                                foo="bar"
+                            />
+
+                            <FieldCustomComp
+                                name="field_4_comp"
                                 foo="bar"
                             />
 
