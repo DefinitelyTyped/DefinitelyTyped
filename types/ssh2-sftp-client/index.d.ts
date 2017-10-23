3096641fd09130df0d1083e59a8c448777cfdf38
khimaros <sllawap@gmail.com>
2017-10-23 09:23:23 -0700

    Remove patch level version number to fix tests

--- types/ssh2-sftp-client/index.d.ts
+++ types/ssh2-sftp-client/index.d.ts
@@ -1,4 +1,4 @@
-// Type definitions for ssh2-sftp-client 2.0.1
+// Type definitions for ssh2-sftp-client 2.0
 // Project: https://www.npmjs.com/package/ssh2-sftp-client
 // Definitions by: igrayson <https://github.com/igrayson>, Ascari Andrea <https://github.com/ascariandrea>
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
