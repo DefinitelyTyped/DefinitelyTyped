4ec5862c1e25d86288b688b978b01d195ce30697
Andy <anhans@microsoft.com>
2017-10-23 13:29:35 -0700

    nodegit: Fix no-redundant-jsdoc lint errors (#20897)

--- types/nodegit/submodule-update-options.d.ts
+++ types/nodegit/submodule-update-options.d.ts
@@ -2,32 +2,8 @@ import { CheckoutOptions } from './checkout-options';
 import { FetchOptions } from './fetch-options';
 
 export interface SubmoduleUpdateOptions {
-    /**
-     *
-     *
-     * @type {number}
-     * @memberof SubmoduleUpdateOptions
-     */
     version?: number;
-    /**
-     *
-     *
-     * @type {CheckoutOptions}
-     * @memberof SubmoduleUpdateOptions
-     */
     checkoutOpts?: CheckoutOptions;
-    /**
-     *
-     *
-     * @type {FetchOptions}
-     * @memberof SubmoduleUpdateOptions
-     */
     fetchOpts?: FetchOptions;
-    /**
-     *
-     *
-     * @type {number}
-     * @memberof SubmoduleUpdateOptions
-     */
     cloneCheckoutStrategy?: number;
 }
