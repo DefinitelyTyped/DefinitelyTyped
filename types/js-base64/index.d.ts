e7c68dcd2eefcae5003992a47e678f12e9b65b1d
tlent <tcl293@gmail.com>
2017-10-23 08:28:43 -0400

    Add name to authors

--- types/js-base64/index.d.ts
+++ types/js-base64/index.d.ts
@@ -1,6 +1,6 @@
 // Type definitions for js-base64 v2.3.2
 // Project: https://github.com/dankogai/js-base64
-// Definitions by: Denis Carriere <https://github.com/DenisCarriere>
+// Definitions by: Denis Carriere <https://github.com/DenisCarriere>, Tommy Lent <https://github.com/tlent/>
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
 
 /**
