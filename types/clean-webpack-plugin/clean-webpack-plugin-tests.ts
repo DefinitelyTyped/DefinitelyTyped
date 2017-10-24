2e04d67cdd7d9380596f86f84d21e42e8b67ccec
Jed Fox <git@twopointzero.us>
2017-10-24 18:02:09 -0400

    Add babel-webpack-plugin and clean-webpack-plugin

---
+++ types/clean-webpack-plugin/clean-webpack-plugin-tests.ts
@@ -0,0 +1,17 @@
+import * as CleanWebpackPlugin from 'clean-webpack-plugin';
+
+const paths = [
+	'path',
+	'glob/**/*.js',
+];
+
+new CleanWebpackPlugin(paths);
+new CleanWebpackPlugin(paths, 'root-directory');
+new CleanWebpackPlugin(paths, {});
+new CleanWebpackPlugin(paths, {
+	root: 'root-directory',
+	verbose: true,
+	dry: true,
+	watch: true,
+	exclude: ['a, b'],
+});
