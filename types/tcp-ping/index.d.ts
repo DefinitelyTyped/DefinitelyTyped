723edc19c23833112855829c5396b34a89313676
Stegano <stegano@naver.com>
2017-10-24 21:11:07 +0900

    Update: `tcp-ping/index.d.ts`

--- types/tcp-ping/index.d.ts
+++ types/tcp-ping/index.d.ts
@@ -1,41 +1,28 @@
 // Type definitions for tcp-ping 0.1
-// Project: https://github.com/wesolyromek/tcp-ping/issues
+// Project: https://github.com/wesolyromek/tcp-ping
 // Definitions by: JUNG YONG WOO <https://github.com/stegano>
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
 
-export interface Option {
+export interface Options {
     address?: string;
     port?: number;
     attempts?: number;
     timeout?: number;
 }
 
-export function ping(options: Option,
-              callback: (error: Error, result: {
-                  address: string,
-                  port: number,
-                  attempts: number,
-                  avg: number,
-                  max: number,
-                  min: number,
-                  results: {
-                      seq: number,
-                      time: number,
-                      error: Error
-                  }
-              }) => void): void;
+export interface Result {
+    address: string;
+    port: number;
+    attempts: number;
+    avg: number;
+    max: number;
+    min: number;
+    results: {
+        seq: number | undefined,
+        time: number | undefined;
+        error?: Error
+    };
+}
 
-export function probe(address: string, port: number,
-               callback: (error: Error, result: {
-                   address: string,
-                   port: number,
-                   attempts: number,
-                   avg: number,
-                   max: number,
-                   min: number,
-                   results: {
-                       seq: number,
-                       time: number,
-                       error: Error
-                   }
-               }) => void): void;
+export function ping(options: Options, callback: (error: Error, result: Result) => void): void;
+export function probe(address: string, port: number, callback: (error: Error, result: Result) => void): void;
