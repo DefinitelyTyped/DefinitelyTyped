e07d674b15b1fec2f696dc6b058f2f73113cbda9
Alexandre <alexr.3165@gmail.com>
2017-10-24 01:23:31 +0100

    Allow typing Express body, params, query and cookies through generics (#20820)
    * Allow typing express body, params, query and cookies through generics
    
    * Increase minor version
    
    * Add ts version comment
    
    * Fix ts requirement comment typo
    
    * Bump all ts version packages
    
    * Bump Morgan package

--- types/express-brute-redis/index.d.ts
+++ types/express-brute-redis/index.d.ts
@@ -2,6 +2,7 @@
 // Project: https://github.com/AdamPflug/express-brute-redis
 // Definitions by: Scott Harwell <https://github.com/scottharwell>
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
+// TypeScript Version: 2.3
 
 import { ClientOpts } from "redis";
 
