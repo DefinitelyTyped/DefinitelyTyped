8c6c695fbd076c9dd639dbbdbb76ee731be5583b
Michael Marner <michael@20papercups.net>
2017-10-26 17:08:41 +1030

    aws-lambda dynamo tests

--- types/aws-lambda/aws-lambda-tests.ts
+++ types/aws-lambda/aws-lambda-tests.ts
@@ -101,93 +101,96 @@ str = customAuthorizerEvt.authorizationToken;
 str = customAuthorizerEvt.methodArn;
 
 /* DynamoDB Stream Event */
-var dynamoDBStreamEvent: DynamoDBStreamEvent = {
-  "Records": [
+var dynamoDBStreamEvent: AWSLambda.DynamoDBStreamEvent = {
+  Records: [
     {
-      "eventID": "1",
-      "eventVersion": "1.0",
-      "dynamodb": {
-        "Keys": {
-          "Id": {
-            "N": "101"
+      eventID: '1',
+      eventVersion: '1.0',
+      dynamodb: {
+        Keys: {
+          Id: {
+            N: 101
           }
         },
-        "NewImage": {
-          "Message": {
-            "S": "New item!"
+        NewImage: {
+          Message: {
+            S: 'New item!'
           },
-          "Id": {
-            "N": "101"
+          Id: {
+            N: 101
           }
         },
-        "StreamViewType": "NEW_AND_OLD_IMAGES",
-        "SequenceNumber": "111",
-        "SizeBytes": 26
+        StreamViewType: 'NEW_AND_OLD_IMAGES',
+        SequenceNumber: '111',
+        SizeBytes: 26
       },
-      "awsRegion": "us-west-2",
-      "eventName": "INSERT",
-      "eventSourceARN": "arn:aws:dynamodb:us-west-2:account-id:table/ExampleTableWithStream/stream/2015-06-27T00:48:05.899",
-      "eventSource": "aws:dynamodb"
+      awsRegion: 'us-west-2',
+      eventName: 'INSERT',
+      eventSourceARN:
+        'arn:aws:dynamodb:us-west-2:account-id:table/ExampleTableWithStream/stream/2015-06-27T00:48:05.899',
+      eventSource: 'aws:dynamodb'
     },
     {
-      "eventID": "2",
-      "eventVersion": "1.0",
-      "dynamodb": {
-        "OldImage": {
-          "Message": {
-            "S": "New item!"
+      eventID: '2',
+      eventVersion: '1.0',
+      dynamodb: {
+        OldImage: {
+          Message: {
+            S: 'New item!'
           },
-          "Id": {
-            "N": "101"
+          Id: {
+            N: 101
           }
         },
-        "SequenceNumber": "222",
-        "Keys": {
-          "Id": {
-            "N": "101"
+        SequenceNumber: '222',
+        Keys: {
+          Id: {
+            N: 101
           }
         },
-        "SizeBytes": 59,
-        "NewImage": {
-          "Message": {
-            "S": "This item has changed"
+        SizeBytes: 59,
+        NewImage: {
+          Message: {
+            S: 'This item has changed'
           },
-          "Id": {
-            "N": "101"
+          Id: {
+            N: 101
           }
         },
-        "StreamViewType": "NEW_AND_OLD_IMAGES"
+        StreamViewType: 'NEW_AND_OLD_IMAGES'
       },
-      "awsRegion": "us-west-2",
-      "eventName": "MODIFY",
-      "eventSourceARN": "arn:aws:dynamodb:us-west-2:account-id:table/ExampleTableWithStream/stream/2015-06-27T00:48:05.899",
-      "eventSource": "aws:dynamodb"
+      awsRegion: 'us-west-2',
+      eventName: 'MODIFY',
+      eventSourceARN:
+        'arn:aws:dynamodb:us-west-2:account-id:table/ExampleTableWithStream/stream/2015-06-27T00:48:05.899',
+      eventSource: 'aws:dynamodb'
     },
     {
-      "eventID": "3",
-      "eventVersion": "1.0",
-      "dynamodb": {
-        "Keys": {
-          "Id": {
-            "N": "101"
+      eventID: '3',
+      eventVersion: '1.0',
+      dynamodb: {
+        Keys: {
+          Id: {
+            N: 101
           }
         },
-        "SizeBytes": 38,
-        "SequenceNumber": "333",
-        "OldImage": {
-          "Message": {
-            "S": "This item has changed"
+        SizeBytes: 38,
+        SequenceNumber: '333',
+        OldImage: {
+          Message: {
+            S: 'This item has changed'
           },
-          "Id": {
-            "N": "101"
+          Id: {
+            N: 101
           }
         },
-        "StreamViewType": "NEW_AND_OLD_IMAGES"
+        StreamViewType: 'NEW_AND_OLD_IMAGES'
       },
-      "awsRegion": "us-west-2",
-      "eventName": "REMOVE",
-      "eventSourceARN": "arn:aws:dynamodb:us-west-2:account-id:table/ExampleTableWithStream/stream/2015-06-27T00:48:05.899",
-      "eventSource": "aws:dynamodb"
+      awsRegion: 'us-west-2',
+      eventName: 'REMOVE',
+      eventSourceARN:
+        'arn:aws:dynamodb:us-west-2:account-id:table/ExampleTableWithStream/stream/2015-06-27T00:48:05.899',
+      eventSource: 'aws:dynamodb'
     }
   ]
 };
