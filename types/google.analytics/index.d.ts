7825ab180ef9f31dce1f29161c1352aaff90a9d2
Tyler Murphy <tylermurphyj@gmail.com>
2017-10-27 13:50:48 -0400

    Adds my name to the list of contributors for google.analytics.

--- types/google.analytics/index.d.ts
+++ types/google.analytics/index.d.ts
@@ -1,6 +1,6 @@
 // Type definitions for Google Analytics (Classic and Universal)
 // Project: https://developers.google.com/analytics/devguides/collection/gajs/, https://developers.google.com/analytics/devguides/collection/analyticsjs/method-reference
-// Definitions by: Ronnie Haakon Hegelund <http://ronniehegelund.blogspot.dk>, Pat Kujawa <http://patkujawa.com>
+// Definitions by: Ronnie Haakon Hegelund <http://ronniehegelund.blogspot.dk>, Pat Kujawa <http://patkujawa.com>, Tyler Murphy <https://github.com/tyler-murphy>
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
 
 declare class Tracker {
