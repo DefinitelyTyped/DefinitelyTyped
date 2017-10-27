c791a9a9660029c8822bdd9b859ec4e0cb0aa742
Daniel Cottone <daniel.cottone@asurion.com>
2017-10-27 14:58:35 -0500

    adding contact info

--- types/aws-lambda/index.d.ts
+++ types/aws-lambda/index.d.ts
@@ -8,6 +8,7 @@
 //                 Yoriki Yamaguchi <https://github.com/y13i>
 //                 wwwy3y3 <https://github.com/wwwy3y3>
 //                 Ishaan Malhi <https://github.com/OrthoDex>
+//                 Daniel Cottone <https://github.com/daniel-cottone>
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
 // TypeScript Version: 2.2
 
