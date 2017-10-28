bbf068446ded2a942188cca0904854c6652c5d0f
rhysd <lin90162@yahoo.co.jp>
2017-10-28 19:20:36 +0900

    twitter-text: Add missing options (fix #19595)

--- types/twitter-text/index.d.ts
+++ types/twitter-text/index.d.ts
@@ -1,4 +1,4 @@
-// Type definitions for twitter-text v1.13.4
+// Type definitions for twitter-text v1.14.7
 // Project: https://github.com/twitter/twitter-text
 // Definitions by: rhysd <https://rhysd.github.io>
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
@@ -36,6 +36,10 @@ interface Indices {
     indices: [number, number];
 }
 
+interface Attributes {
+    [name: string]: string | null;
+}
+
 export declare function htmlEscape(text: string): string;
 export declare function splitTags(text: string): string[];
 
@@ -69,12 +73,17 @@ export interface AutoLinkOptions {
     usernameClass?: string;
     usernameUrlBase?: string;
     listUrlBase?: string;
-    htmlAttrs?: string;
     invisibleTagAttrs?: string;
     htmlEscapeNonEntities?: boolean;
     targetBlank?: boolean;
     suppressNoFollow?: boolean;
     urlEntities?: UrlEntity[];
+    usernameIncludeSymbol?: boolean;
+    linkAttributeBlock?: (entity: EntityWithIndices, attributes: Attributes) => void;
+    linkTextBlock?: (entity: EntityWithIndices, text: string) => void;
+    symbolTag?: string;
+    textWithSymbolTag?: string;
+    htmlAttrs?: Attributes;
 }
 
 export declare function autoLink(text: string, options?: AutoLinkOptions): string;
