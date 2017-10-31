19e333938155dcb6003592ef87bc10a958800f6d
Josh Goldberg <joshuakgoldberg@outlook.com>
2017-10-31 15:22:01 -0700

    Made name optional in jasmine::createSpy
    See https://github.com/jasmine/jasmine/issues/1438

--- types/jasmine/index.d.ts
+++ types/jasmine/index.d.ts
@@ -137,7 +137,7 @@ declare namespace jasmine {
 
     function arrayContaining(sample: any[]): ArrayContaining;
     function objectContaining<T>(sample: Partial<T>): ObjectContaining<T>;
-    function createSpy(name: string, originalFn?: Function): Spy;
+    function createSpy(name?: string, originalFn?: Function): Spy;
 
     function createSpyObj(baseName: string, methodNames: any[] | {[methodName: string]: any}): any;
     function createSpyObj<T>(baseName: string, methodNames: any[] | {[methodName: string]: any}): SpyObj<T>;
