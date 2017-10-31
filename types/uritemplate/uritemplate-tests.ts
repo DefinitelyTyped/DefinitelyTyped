a2cdbb8d730c5427b3e76f9159079715e181a8b6
Ruben Taelman <ruben.taelman@ugent.be>
2017-10-31 10:38:09 +0900

    Fix incorrect uritemplate typings

--- types/uritemplate/uritemplate-tests.ts
+++ types/uritemplate/uritemplate-tests.ts
@@ -1,28 +1,28 @@
 
-import { UriTemplate } from 'uritemplate';
+import {parse as parseUriTemplate, UriTemplate, UriTemplateError} from "uritemplate";
 
 function test_uritemplate() {
 
     // syntax check: UriTemplate.parse
-    var template = UriTemplate.parse('http://localhost/categories{/categoryId}{?sort,pageNumber}');
+    var template: UriTemplate = parseUriTemplate('http://localhost/categories{/categoryId}{?sort,pageNumber}');
 
     // syntax check: template.expand
-    var url = template.expand({
+    var url: string = template.expand({
         categoryId : 'shoes',
         sort: 'price',
         pageNumber: 8
     });
 
     // import module check
-    var expectedUrl = 'http://localhost/categories/shoes?sort=price&pageNumber=8';
+    var expectedUrl: string = 'http://localhost/categories/shoes?sort=price&pageNumber=8';
     if (expectedUrl != url) {
         throw `Expected ${expectedUrl}, got ${url}`;
     } else {
         console.log('Test passed');
     }
 
-    // syntax check: new UriTemplate.UriTemplateError
-    let error = new UriTemplate.UriTemplateError({
+    // syntax check: new UriTemplateError
+    let error: UriTemplateError = new UriTemplateError({
         expressionText: 'error expression',
         message: 'error message',
         position: 5
