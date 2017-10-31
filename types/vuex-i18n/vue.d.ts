68f2ec4f72e5457bc4217b30ab03063ea719a5b5
Cedric Kemp <jaeggerr>
2017-10-31 11:29:11 -0400

    Fixed linter issues

--- types/vuex-i18n/vue.d.ts
+++ types/vuex-i18n/vue.d.ts
@@ -1,9 +1,3 @@
-// Type definitions for vuex-i18n 1.7.0
-// Project: https://github.com/dkfbasel/vuex-i18n
-// Definitions by: Cedric Kemp <https://github.com/jaeggerr>
-// Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
-/// TypeScript Version: 2.3
-
 /**
  * Extends interfaces in Vue.js
  */
