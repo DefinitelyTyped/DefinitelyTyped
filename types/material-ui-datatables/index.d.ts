9fbca8fa75f5d50b99939224c9fe971cf25061fa
Ravi Luthra <coding@gmail.com>
2017-10-31 20:36:06 -0700

    Fix parameter names

--- types/material-ui-datatables/index.d.ts
+++ types/material-ui-datatables/index.d.ts
@@ -103,6 +103,17 @@ export interface DataTableProps {
     filterValue: string;
     /** Show the icon to turn on the filtering feature */
     showHeaderToolbarFilterIcon: boolean;
+    onRowSizeChange: (index: number, value: any) => void;
+    onSortOrderChange: (key: string, order: string) => void;
+    /** Callback when the cell is clicked. This callback is only active when selectable is false. */
+    onCellClick: (rowIndex: number, columnIndex: number, row: any, columnValue: any, event: any) => void;
+    /** Similar to onCellClick, activated when the cell is double clicked. Fires even if rows are selectable. */
+    onCellDoubleClick: (rowIndex: number, columnIndex: number, row: any, columnValue: any, event: any) => void;
+    /** Notification if the filter value changes */
+    onFilterValueChange: (value: string) => void;
+    onNextPageClick: (event: any) => void;
+    onPreviousPageClick: (event: any) => void;
+    onRowSelection: (selectedRows: any) => void;
 }
 
 export default class DataTable extends React.Component<Partial<DataTableProps>> { }
