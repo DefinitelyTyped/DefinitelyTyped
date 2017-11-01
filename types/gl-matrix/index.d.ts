7aa4d751a304ba96b3936837b9bd9d8446c7f03b
Wayne Langman <Wayne.Langman@derivco.co.za>
2017-11-01 14:22:43 +0200

    Updated the gl-matrix version.

--- types/gl-matrix/index.d.ts
+++ types/gl-matrix/index.d.ts
@@ -1,4 +1,4 @@
-// Type definitions for gl-matrix 2.3.2
+// Type definitions for gl-matrix 2.4.0
 // Project: https://github.com/toji/gl-matrix
 // Definitions by: Mattijs Kneppers <https://github.com/mattijskneppers>, based on definitions by Tat <https://github.com/tatchx>
 //                 Nikolay Babanov <https://github.com/nbabanov>
