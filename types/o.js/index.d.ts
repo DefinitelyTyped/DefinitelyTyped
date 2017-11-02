96232887cd995694bc318bda39f23bcdb79caafa
Jean-Christophe Chalté <jc.chalte@outlook.com>
2017-11-02 22:31:35 +0100

    Increased version

--- types/o.js/index.d.ts
+++ types/o.js/index.d.ts
@@ -1,6 +1,6 @@
-// Type definitions for o.js v0.3.3
+// Type definitions for o.js v0.3.4
 // Project: https://github.com/janhommes/o.js
-// Definitions by: Matteo Antony Mistretta <https://github.com/IceOnFire>, Brad Zacher <https://github.com/bradzacher>, Jan Hommes <https://github.com/janhommes>
+// Definitions by: Matteo Antony Mistretta <https://github.com/IceOnFire>, Brad Zacher <https://github.com/bradzacher>, Jan Hommes <https://github.com/janhommes>, Jean-Christophe Chalte <https://github.com/jcchalte>
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
 
 declare module 'o.js' {
