ef74cd057eb6f863b596ff4768886bb7a21511cf
Steve Xian <steve@deliverr.com>
2017-11-01 17:35:05 -0700

    incremented version

--- types/bookshelf/index.d.ts
+++ types/bookshelf/index.d.ts
@@ -1,4 +1,4 @@
-// Type definitions for bookshelfjs v0.9.3
+// Type definitions for bookshelfjs v0.9.4
 // Project: http://bookshelfjs.org/
 // Definitions by: Andrew Schurman <https://github.com/arcticwaters>, Vesa Poikajärvi <https://github.com/vesse>
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
