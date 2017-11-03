fa9de4a08ce729296b9fb8977bfe6e90f8d85fed
Oscar Andersson <oscar.andersson89@gmail.com>
2017-11-03 15:14:31 +0100

    Update index.d.ts

--- types/react-bootstrap-table/index.d.ts
+++ types/react-bootstrap-table/index.d.ts
@@ -2,7 +2,7 @@
 // Project: https://github.com/AllenFang/react-bootstrap-table
 // Definitions by: Frank Laub <https://github.com/flaub>, Aleksander Lode <https://github.com/alelode>, Josué Us <https://github.com/UJosue10>, Oscar Andersson <https://github.com/Ogglas>
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
-// TypeScript Version: 2.6
+// TypeScript Version: 2.5
 
 /// <reference types="node" />
 
