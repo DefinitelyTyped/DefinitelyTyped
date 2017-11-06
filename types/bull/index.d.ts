1c4f1ef167d50e0632d5fa3381fe64dcb3353545
Gregor Yushinov <weeco@users.noreply.github.com>
2017-11-06 17:51:28 +0100

    Don't include patch version in header

--- types/bull/index.d.ts
+++ types/bull/index.d.ts
@@ -1,4 +1,4 @@
-// Type definitions for bull 3.3.5
+// Type definitions for bull 3.3
 // Project: https://github.com/OptimalBits/bull
 // Definitions by: Bruno Grieder <https://github.com/bgrieder>
 //                 Cameron Crothers <https://github.com/JProgrammer>
@@ -233,7 +233,7 @@ declare namespace Bull {
     failed: number;
     delayed: number;
   }
-  
+
   interface JobInformation {
     key: string;
     name: string;
