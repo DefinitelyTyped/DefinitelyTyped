7073ad9cc5f65e69675aede0ba24572834406861
Matthew Bull <matt_wings@hotmail.com>
2017-11-06 10:30:33 +0000

    Got linting working, updated tests a bit

--- types/koa-joi-router/koa-joi-router-tests.ts
+++ types/koa-joi-router/koa-joi-router-tests.ts
@@ -1,9 +1,20 @@
 import * as router from 'koa-joi-router';
 
+const { Joi } = router;
+
 const spec1 = {
-  route: '/user',
-  method: 'POST',
-  handler: (ctx) => ctx.body = '';
+    route: '/user',
+    method: 'POST',
+    handler: (ctx) => ctx.body = '';
 };
 
 router().route(spec1);
+
+const spec2: ISpec = {
+  method: 'PATCH',
+  route: '/user',
+  validate: {},
+  handler: (ctx) => ctx.stat
+};
+
+router().route(spec2);
