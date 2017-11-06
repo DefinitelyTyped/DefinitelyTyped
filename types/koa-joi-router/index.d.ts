a9b5d8c180d660b3fd3d319c6f1ec5e65e819735
Matthew Bull <matt_wings@hotmail.com>
2017-11-06 10:30:33 +0000

    Got linting working, updated tests a bit

--- types/koa-joi-router/index.d.ts
+++ types/koa-joi-router/index.d.ts
@@ -1,26 +1,33 @@
+// Type definitions for koa-joi-router 5.0
+// Project: https://github.com/koajs/joi-router
+// Definitions by: Matthew Bull <https://github.com/wingsbob>
+// Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
 import * as Koa from 'koa';
 import * as Joi from 'joi';
 
-declare type METHODS = 'POST' | 'GET' | 'DELETE' | 'PUT' | 'PATCH' | 'OPTIONS' | 'HEAD';
+type METHODS = 'POST' | 'GET' | 'DELETE' | 'PUT' | 'PATCH' | 'OPTIONS' | 'HEAD';
 
-interface ISpec {
-  method: METHODS;
-  path: string|RegExp;
-  handler: (ctx: Koa.Context) => void;
-  validate: {
-    type: string;
-    body?: Joi.AnySchema;
-    params?: Joi.AnySchema;
-    [status: number]: Joi.AnySchema;
-  };
+interface Spec {
+    method: METHODS;
+    path: string|RegExp;
+    handler: (ctx: Koa.Context) => void;
+    validate: {
+        type: string;
+        body?: Joi.AnySchema;
+        params?: Joi.AnySchema;
+        [status: number]: Joi.AnySchema;
+    };
 }
 
 declare class Router {
-  route(spec: ISpec): Router;
-  middleware(): Koa.Middleware;
+    route(spec: Spec): Router;
+    middleware(): Koa.Middleware;
 }
 
-declare function createRouter(): Router;
+interface createRouter {
+    (): Router;
+    Joi: typeof Joi;
+}
 
 declare namespace createRouter {}
 
