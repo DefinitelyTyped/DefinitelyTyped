34d681bbc7527d35656ba576a8dbcf127db709a4
CarbonAtom <chjah04@gmail.com>
2017-11-06 11:39:02 -0500

    updated perfect scrollbar to v1.1

--- types/perfect-scrollbar/perfect-scrollbar-tests.ts
+++ types/perfect-scrollbar/perfect-scrollbar-tests.ts
@@ -3,38 +3,27 @@
  */
 
 import * as Ps from 'perfect-scrollbar'
-const container = document.body
+const container = document.body;
 
-// To initialise the plugin, `Ps.initialize` is used.
-Ps.initialize(container)
+// initialize scrollbar.
+let scrollbar = new PerfectScrollbar(container);
 
-// It can be initialised with optional parameters.
-Ps.initialize(container, {
-  wheelSpeed: 2,
-  wheelPropagation: true,
-  minScrollbarLength: 20
+// destroys the scrollbar
+scrollbar.destroy();
+
+// initializes scrollbar with params.
+scrollbar = new PerfectScrollbar(container, {
+    wheelSpeed: 2,
+    wheelPropagation: true,
+    minScrollbarLength: 20,
 })
 
 // If the size of your container or content changes, call `update`.
-Ps.update(container)
-
-// If you want to destroy the scrollbar, use `destroy`.
-Ps.destroy(container)
+scrollbar.update()
 
 // If you want to scroll to somewhere, just use a `scrollTop` property and update.
-container.scrollTop = 0
-Ps.update(container)
+container.scrollTop = 0;
+scrollbar.update();
 
-/**
- * https://github.com/noraesae/perfect-scrollbar#jquery
- */
-
-import mountJQuery = require('perfect-scrollbar/jquery')
-mountJQuery($)
-
-$('#container').perfectScrollbar()            // Initialize
-$('#container').perfectScrollbar({ /*...*/ }) // with options
-$('#container').perfectScrollbar('update')    // Update
-$('#container').perfectScrollbar('destroy')   // Destroy
-
-container.addEventListener('ps-x-reach-start', () => { });
+// check autocomplete for events
+(container as HTMLPerfectScrollbarElement).addEventListener('ps-scroll-down', (ev) => { });
