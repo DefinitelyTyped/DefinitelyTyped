3fb4c53b02b086033cb994021ff84aa4c086386c
segayuu <segayuu@gmail.com>
2017-11-07 12:28:03 +0900

    cleanup ignore lint error: dt-header

--- types/bluebird-retry/index.d.ts
+++ types/bluebird-retry/index.d.ts
@@ -1,4 +1,4 @@
-// Type definitions for bluebird-retry
+// Type definitions for bluebird-retry 0.11
 // Project: https://github.com/jut-io/bluebird-retry
 // Definitions by: Pascal Vomhoff <https://github.com/pvomhoff>
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
