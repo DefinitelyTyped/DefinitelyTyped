4e2362c427997e0f74e6165cc48095ae8fe25a9a
Hamed Fathi <noeffect@outlook.com>
2017-11-07 14:31:15 +0330

    zenscroll.d.ts updated :)

--- types/zenscroll/index.d.ts
+++ types/zenscroll/index.d.ts
@@ -3,26 +3,26 @@
 // Definitions by: Hamed Fathi <https://github.com/HamedFathi/>
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
 
-interface setupOption {
-    defaultDuration: number;
-    edgeOffset: number;
-}
+declare var zenScroll: ZenScroll.zenscroll;
+export = zenScroll;
+export as namespace zenScroll;
 
-interface zenscroll {
-    setup(defaultDuration?: number, edgeOffset?: number): setupOption;
-    to(elem: HTMLElement, duration?: number, onDone?: () => void): void;
-    toY(targetY: number, duration?: number, onDone?: () => void): void;
-    intoView(elem: HTMLElement, duration?: number, onDone?: () => void): void;
-    center(elem: HTMLElement, duration?: number, offset?: number, onDone?: () => void): void;
-    stop(): void;
-    moving(): boolean;
-    getY(): number;
-    getTopOf(elem: HTMLElement): number;
-    createScroller(scrollContainer: HTMLElement, defaultDuration?: number, edgeOffset?: number): zenscroll;
-}
+declare namespace ZenScroll {
+    interface setupOption {
+        defaultDuration: number;
+        edgeOffset: number;
+    }
 
-declare var zenScroll: zenscroll;
-
-declare module "zenscroll" {
-    export = zenScroll;
+    interface zenscroll {
+        setup(defaultDuration?: number, edgeOffset?: number): setupOption;
+        to(elem: HTMLElement, duration?: number, onDone?: () => void): void;
+        toY(targetY: number, duration?: number, onDone?: () => void): void;
+        intoView(elem: HTMLElement, duration?: number, onDone?: () => void): void;
+        center(elem: HTMLElement, duration?: number, offset?: number, onDone?: () => void): void;
+        stop(): void;
+        moving(): boolean;
+        getY(): number;
+        getTopOf(elem: HTMLElement): number;
+        createScroller(scrollContainer: HTMLElement, defaultDuration?: number, edgeOffset?: number): zenscroll;
+    }
 }
