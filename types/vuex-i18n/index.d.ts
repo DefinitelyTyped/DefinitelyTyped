c870d67fe9ca282ad6bedf36c270f962fcb87a5d
Daniel Rosenwasser <DanielRosenwasser@users.noreply.github.com>
2017-11-07 10:43:04 -0800

    slash-slash-slash -> slash-slash

--- types/vuex-i18n/index.d.ts
+++ types/vuex-i18n/index.d.ts
@@ -2,7 +2,7 @@
 // Project: https://github.com/dkfbasel/vuex-i18n
 // Definitions by: Cedric Kemp <https://github.com/jaeggerr>
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
-/// TypeScript Version: 2.3
+// TypeScript Version: 2.3
 
 import _Vue, { PluginObject } from "vue";
 
