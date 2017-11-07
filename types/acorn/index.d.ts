6ee0e959ce87b34db7d21e2351fdf6a53fde78f1
segayuu <segayuu@gmail.com>
2017-11-07 13:06:38 +0900

    cleanup ignore lint error: dt-header

--- types/acorn/index.d.ts
+++ types/acorn/index.d.ts
@@ -1,4 +1,4 @@
-// Type definitions for Acorn v4.0.3
+// Type definitions for Acorn 4.0
 // Project: https://github.com/marijnh/acorn
 // Definitions by: RReverser <https://github.com/RReverser>, e-cloud <https://github.com/e-cloud>
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
