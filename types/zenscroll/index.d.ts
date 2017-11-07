33ce693b07c3d8fd96530b7c5d57737acce192b3
Hamed Fathi <noeffect@outlook.com>
2017-11-07 15:13:21 +0330

    updated

--- types/zenscroll/index.d.ts
+++ types/zenscroll/index.d.ts
@@ -1,4 +1,4 @@
-// Type definitions for zenscroll 4.0.0
+// Type definitions for zenscroll 4.0
 // Project: https://zengabor.github.io/zenscroll/
 // Definitions by: Hamed Fathi <https://github.com/HamedFathi/>
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
