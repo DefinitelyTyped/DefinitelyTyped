805406a97a3388e67284171931d7f1d45074622b
segayuu <segayuu@gmail.com>
2017-11-07 10:59:06 +0900

    cleanup ignore lint error: dt-header

--- types/nunjucks/index.d.ts
+++ types/nunjucks/index.d.ts
@@ -1,4 +1,4 @@
-// Type definitions for nunjucks
+// Type definitions for nunjucks 3.0
 // Project: http://mozilla.github.io/nunjucks/
 // Definitions by: Ruben Slabbert <https://github.com/RubenSlabbert>
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
