41fb40bb68b666424d4188867da9fe319c8d3fca
segayuu <segayuu@gmail.com>
2017-11-07 10:48:01 +0900

    cleanup ignore lint errors: no-consecutive-blank-lines, trim-file

--- types/nunjucks/index.d.ts
+++ types/nunjucks/index.d.ts
@@ -3,7 +3,6 @@
 // Definitions by: Ruben Slabbert <https://github.com/RubenSlabbert>
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
 
-
 declare namespace Nunjucks {
     function render(name: string, context?: Object): string;
     function render(name: string, context?: Object, callback?: (err: any, res: string) => any): void;
