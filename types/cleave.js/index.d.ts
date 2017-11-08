a29d1f5a2a3f05140755914a029070a36d7eb456
Jason Giancono <Jason.Giancono@sportsbet.com.au>
2017-11-08 11:44:43 +1100

    changed version for cleave.js to MAJOR.MINOR

--- types/cleave.js/index.d.ts
+++ types/cleave.js/index.d.ts
@@ -1,4 +1,4 @@
-// Type definitions for cleave.js 1.0.3
+// Type definitions for cleave.js 1.0
 // Project: https://github.com/nosir/cleave.js
 // Definitions by: C Lentfort <https://github.com/clentfort>, J Giancono <https://github.com/jasongi-at-sportsbet>
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
