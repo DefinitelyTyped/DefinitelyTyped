736cbe1ae88af2ea070171e52e5aba1197e5c07c
surtr-isaz <31244636+surtr-isaz@users.noreply.github.com>
2017-11-08 09:44:43 +0200

    Update index.d.ts
    Fixed version number.

--- types/gl-matrix/index.d.ts
+++ types/gl-matrix/index.d.ts
@@ -1,4 +1,4 @@
-// Type definitions for gl-matrix 2.4.0
+// Type definitions for gl-matrix 2.4
 // Project: https://github.com/toji/gl-matrix
 // Definitions by: Mattijs Kneppers <https://github.com/mattijskneppers>, based on definitions by Tat <https://github.com/tatchx>
 //                 Nikolay Babanov <https://github.com/nbabanov>
