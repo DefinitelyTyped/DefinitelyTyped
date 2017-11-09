df0fabe13ba692276e60777fab3f0497c4d60bfa
Daniel Rosenwasser <DanielRosenwasser@users.noreply.github.com>
2017-11-09 14:06:59 -0800

    Update TypeScript version dependency in 'mock-knex'. (#21384)
    * Update TypeScript version dependency in 'mock-knex'.
    
    * Account for 'promise-retry'.
    
    * Add 'x-ray' too.

--- types/x-ray/index.d.ts
+++ types/x-ray/index.d.ts
@@ -2,6 +2,7 @@
 // Project: https://github.com/lapwinglabs/x-ray#readme
 // Definitions by: Matt Traynham <https://github.com/mtraynham>
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
+// TypeScript Version: 2.3
 
 /// <reference types="node" />
 
