9d841df367006b8ffb3cbffe0b45a26048a99d0d
Piotr Roszatycki <piotr.roszatycki@gmail.com>
2017-11-10 11:35:21 +0100

    Fixed errors after dts-gen

--- types/wtfnode/index.d.ts
+++ types/wtfnode/index.d.ts
@@ -1,6 +1,6 @@
 // Type definitions for wtfnode 0.5
 // Project: https://github.com/myndzi/wtfnode
-// Definitions by: Piotr Roszatycki <https://github.com/wtfnode>
+// Definitions by: Piotr Roszatycki <https://github.com/dex4er>
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
 
 export function dump(): void;
