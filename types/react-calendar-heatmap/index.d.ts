2f83d96a3feeb81da975dca867744b87675d666d
Keisuke Kan <9renpoto@gmail.com>
2017-11-10 10:08:41 +0900

    refactor(types/react-calendar-heatmap) Update Function types

--- types/react-calendar-heatmap/index.d.ts
+++ types/react-calendar-heatmap/index.d.ts
@@ -1,32 +1,30 @@
-// Type definitions for react-calendar-heatmap v1.6.1
+// Type definitions for react-calendar-heatmap 1.6
 // Project: https://github.com/patientslikeme/react-calendar-heatmap
 // Definitions by: Keisuke Kan <https://github.com/9renpoto>
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
 // TypeScript Version: 2.3
 
-/// <reference types="react" />
+import * as React from 'react';
 
-declare module "react-calendar-heatmap" {
-    interface Props {
-        classForValue?: Function
-        endDate?: string | number | Date
-        gutterSize?: number
-        horizontal?: boolean
-        monthLabels?: string[]
-        numDays?: number
-        onClick?: Function
-        onMouseLeave?: Function, 
-        onMouseOver?: Function, 
-        showMonthLabels?: boolean
-        showOutOfRangeDays?: boolean
-        showWeekdayLabels?: boolean, 
-        startDate?: string | number | Date,
-        titleForValue?: Function,
-        tooltipDataAttrs?: Object | Function
-        transformDayElement?: Function,
-        values: any[]
-        weekdayLabels?: string[] 
-    }
-    let ReactCalendarHeatmap : React.ClassicComponentClass<Props>;
-    export default ReactCalendarHeatmap;
+export interface Props {
+    classForValue?: (value: any) => 'color-filled' | 'color-empty';
+    endDate?: string | number | Date;
+    gutterSize?: number;
+    horizontal?: boolean;
+    monthLabels?: string[];
+    numDays?: number;
+    onClick?: (value: any) => void;
+    onMouseLeave?: (e: any, value: any) => void;
+    onMouseOver?: (e: any, value: any) => void;
+    showMonthLabels?: boolean;
+    showOutOfRangeDays?: boolean;
+    showWeekdayLabels?: boolean;
+    startDate?: string | number | Date;
+    titleForValue?: (value: any) => any;
+    tooltipDataAttrs?: object;
+    transformDayElement?: (rect: any, value: any, index: number) => any;
+    values: any[];
+    weekdayLabels?: string[];
 }
+
+export default class ReactCalendarHeatmap extends React.Component<Props> {}
