d2322cd63bbfb6a872cc49b011eeed6f8a24a569
Piotr Roszatycki <piotr.roszatycki@gmail.com>
2017-11-10 11:23:08 +0100

    Simplified tests without bunyan module

--- types/bunyan-format/bunyan-format-tests.ts
+++ types/bunyan-format/bunyan-format-tests.ts
@@ -1,8 +1,5 @@
-import bunyan = require('bunyan');
 import BunyanFormatWritable = require('bunyan-format');
 
 const formatOut = new BunyanFormatWritable({ outputMode: 'short' });
-
-const log = bunyan.createLogger({ name: 'app', stream: formatOut, level: 'debug' });
 
 const formatOut2 = new BunyanFormatWritable({ outputMode: 'bunyan', levelInString: true });
