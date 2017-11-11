28dcaa438ed5421453461456978ab62af803f4f6
Robert Imig <rimig@palantir.com>
2017-11-10 18:56:41 -0600

    update latlon-geohash types

--- types/latlon-geohash/latlon-geohash-tests.ts
+++ types/latlon-geohash/latlon-geohash-tests.ts
@@ -5,10 +5,10 @@ const atx_geohash: string = Geohash.encode(30.2672, -97.7431);
 const atx_geohash_p3: string = Geohash.encode(30.2672, -97.7431, 3);
 
 // Decoding
-const atx_latlong: number[] = Geohash.decode(atx_geohash);
+const atx_latlong: Geohash.Point = Geohash.decode(atx_geohash);
 
 // Bounds
-const atx_bounds: number[] = Geohash.bounds(atx_geohash);
+const atx_bounds: Geohash.Bounds = Geohash.bounds(atx_geohash);
 
 // Adjacent
 const atx_adj_cell1: string = Geohash.adjacent(atx_geohash, Geohash.Direction.North);
