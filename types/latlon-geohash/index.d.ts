16f784425bb48f8359cba2b3334ac28701a85958
Robert Imig <rimig@palantir.com>
2017-11-11 11:17:13 -0600

    add header ts ver 2.4

--- types/latlon-geohash/index.d.ts
+++ types/latlon-geohash/index.d.ts
@@ -2,6 +2,7 @@
 // Project: https://github.com/chrisveness/latlon-geohash
 // Definitions by: Robert Imig <https://github.com/rimig>
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
+// TypeScript Version: 2.4
 
 export enum Direction {
     North = "N",
