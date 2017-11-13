36fd482fc0189a0e751881bf6d979b44c439e517
linxiaowu66 <linguang661990@126.com>
2017-11-13 16:05:32 +0800

    feat(better-scroll): tsconfig.json update config

--- types/better-scroll/better-scroll-tests.ts
+++ types/better-scroll/better-scroll-tests.ts
@@ -13,16 +13,18 @@ const BScroll4 = new BScroll('#wrapper', {
     selectedIndex: 0,
   },
 });
-const BScroll6 = new BScroll('#wrapper', {
-  snap: {
-    loop: false,
-    el: document.querySelector('div-test'),
-    threshold: 0.1,
-    stepX: 100,
-    stepY: 100,
-    listenFlick: true,
-  },
-});
+if (document.querySelector('div-test')) {
+  const BScroll6 = new BScroll('#wrapper', {
+    snap: {
+      loop: false,
+      el: document.querySelector('div-test')!,
+      threshold: 0.1,
+      stepX: 100,
+      stepY: 100,
+      listenFlick: true,
+    },
+  });
+}
 const BScroll7 = new BScroll('#wrapper', {
   scrollbar: {
     fade: true,
@@ -43,25 +45,25 @@ BScroll1.scrollTo(0, 100, 200);
 BScroll1.scrollToElement('selectedElement');
 BScroll1.scrollToElement('selectedElement', 250);
 
-BScroll1.scrollToElement(document.getElementById('selectedElement'));
-BScroll1.scrollToElement(document.getElementById('selectedElement'), 250);
+BScroll1.scrollToElement(document.getElementById('selectedElement')!);
+BScroll1.scrollToElement(document.getElementById('selectedElement')!, 250);
 
 BScroll2.on('scrollStart', () => { console.log('scroll started'); });
 
-const BScroll9 = new BScroll(document.getElementById('wrapper'));
-const BScroll10 = new BScroll(document.getElementById('wrapper'), { freeScroll: true });
-const BScroll11 = new BScroll(document.getElementById('wrapper'), {
+const BScroll9 = new BScroll(document.getElementById('wrapper')!);
+const BScroll10 = new BScroll(document.getElementById('wrapper')!, { freeScroll: true });
+const BScroll11 = new BScroll(document.getElementById('wrapper')!, {
   preventDefaultException: {
     tagName: /^(INPUT|TEXTAREA|BUTTON|SELECT)$/,
   },
 });
-const BScroll12 = new BScroll(document.getElementById('wrapper'), {
+const BScroll12 = new BScroll(document.getElementById('wrapper')!, {
   preventDefaultException: {
     className: /(^|\s)test(\s|$)/,
   },
 });
 
-const BScroll13 = new BScroll(document.getElementById('wrapper'), {
+const BScroll13 = new BScroll(document.getElementById('wrapper')!, {
   swipeBounceTime: 1000,
 });
 
