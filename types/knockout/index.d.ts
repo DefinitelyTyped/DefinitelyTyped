d47c89cf13462ad54a73fbde41ab5b093c27a808
Mathias Lykkegaard Lorenzen <mathias.lorenzen@live.com>
2017-11-13 14:16:48 +0100

    Update index.d.ts

--- types/knockout/index.d.ts
+++ types/knockout/index.d.ts
@@ -1,10 +1,6 @@
 // Type definitions for Knockout v3.4.0
 // Project: http://knockoutjs.com
-// Definitions by: Boris Yankov <https://github.com/borisyankov/>
-//                 Igor Oleinikov <https://github.com/Igorbek/>
-//                 Clément Bourgeois <https://github.com/moonpyk/>
-//                 Matt Brooks <https://github.com/EnableSoftware>
-//                 Benjamin Eckardt <https://github.com/BenjaminEckardt>
+// Definitions by: Boris Yankov <https://github.com/borisyankov/>, Igor Oleinikov <https://github.com/Igorbek/>, Clément Bourgeois <https://github.com/moonpyk/>, Matt Brooks <https://github.com/EnableSoftware>, Benjamin Eckardt <https://github.com/BenjaminEckardt>, Mathias Lorenzen <https://github.com/ffMathy>
 // TypeScript Version: 2.3
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
 
