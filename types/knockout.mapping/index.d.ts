d0450dac5ac95d8de2e010db19e5981537e6b93b
Mathias Lykkegaard Lorenzen <mathias.lorenzen@live.com>
2017-11-13 23:50:58 +0100

    Update index.d.ts

--- types/knockout.mapping/index.d.ts
+++ types/knockout.mapping/index.d.ts
@@ -1,8 +1,8 @@
 // Type definitions for Knockout.Mapping 2.0
 // Project: https://github.com/SteveSanderson/knockout.mapping
-// Definitions by: Boris Yankov <https://github.com/borisyankov>, Mathias Lorenzen <https://github.com/ffMathy>
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
 // TypeScript Version: 2.3
+// Definitions by: Boris Yankov <https://github.com/borisyankov>, Mathias Lorenzen <https://github.com/ffMathy>
 /// <reference types="knockout" />
 
 export as namespace mapping;
