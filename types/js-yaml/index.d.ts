825a9697c209915b62c3d521d8c989981828156d
segayuu <segayuu@gmail.com>
2017-11-13 12:01:48 +0900

    cleanup ignore lint error: dt-header

--- types/js-yaml/index.d.ts
+++ types/js-yaml/index.d.ts
@@ -1,4 +1,4 @@
-// Type definitions for js-yaml 3.9.1
+// Type definitions for js-yaml 3.9
 // Project: https://github.com/nodeca/js-yaml
 // Definitions by: Bart van der Schoor <https://github.com/Bartvds>, Sebastian Clausen <https://github.com/sclausen>
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
