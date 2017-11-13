442aea922c24fa9c820487b7591ab0ca8fc0fd61
cynecx <me@cynecx.net>
2017-11-13 10:23:30 +0100

    [React] Use constructor overloading instead of an optional paramter

--- types/react/index.d.ts
+++ types/react/index.d.ts
@@ -277,7 +277,8 @@ declare namespace React {
     // tslint:disable-next-line:no-empty-interface
     interface Component<P = {}, S = {}> extends ComponentLifecycle<P, S> { }
     class Component<P, S> {
-        constructor(props?: P, context?: any);
+        constructor(props: P, context?: any);
+        constructor();
 
         // Disabling unified-signatures to have separate overloads. It's easier to understand this way.
         // tslint:disable:unified-signatures
