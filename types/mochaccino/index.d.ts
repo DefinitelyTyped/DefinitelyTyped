4151969646cd9caaac0af2330d5aea8432be7768
Thomas Puttkamer <tvp1981@gmx.de>
2017-11-13 18:48:47 +0100

    added type definitions for mochaccino

---
+++ types/mochaccino/index.d.ts
@@ -0,0 +1,71 @@
+// Type definitions for mochaccino 1.2
+// Project: https://github.com/pawelgalazka/mochaccino#readme
+// Definitions by: Thomas-P <https://github.com/thomas-p>
+// Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
+
+/// <reference path="../sinon/index.d.ts" />
+
+
+declare module 'mochaccino' {
+
+    import * as Sinon from "sinon";
+
+    interface Expect {
+        not: Expect;
+
+        toBe<T>(arg: T): void;
+
+        toContain<T>(arg: T): void;
+
+        toEqual<T>(arg: T): void;
+
+        toHaveBeenCalled(): void;
+
+        toHaveBeenCalledWith(...arg: any[]): void;
+
+        toHaveBeenCalledTimes(callCount: number): void;
+
+        toBeDefined(): void;
+
+        toBeUndefined(): void;
+
+        toBeNull(): void;
+
+        toBeTruthy(): void;
+
+        toBeFalsy(): void;
+
+        toBeLessThan(other: number): void;
+
+        toBeGreaterThan(other: number): void;
+
+        toThrow(): void;
+
+        toThrowError<T>(errType: T): void;
+
+        toMatch<T>(matchExpression: T): void;
+    }
+
+
+    interface SpyProxy {
+        and: SpyProxy;
+        spyProxy: true;
+        getSubject: () => Sinon.SinonStub;
+        callThrough: () => void;
+        returnValue: (obj: any) => void;
+        callFake: (fake: Function) => void;
+    }
+
+    interface Dom {
+        exposedProperties: ['window', 'navigator', 'document'];
+        create: () => void;
+        destroy: () => void;
+        clear: () => void;
+    }
+
+    export function expect<T>(value: T): Expect;
+
+    export function spy(...config: any[]): (...args: any[]) => SpyProxy;
+
+    export const dom: Dom;
+}
