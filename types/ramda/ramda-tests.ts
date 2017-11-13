b8cf0375d4fa04ccbd0fd6f4e3aaeb06236ae0d6
Miloslav Nenadal <nenadalm@gmail.com>
2017-11-13 17:38:52 +0100

    [ramda]: Fix `prop` type

--- types/ramda/ramda-tests.ts
+++ types/ramda/ramda-tests.ts
@@ -1535,6 +1535,16 @@ class Rectangle {
 
 () => {
     const x: number = R.prop("x", {x: 100}); // => 100
+    const obj = {
+        str: 'string',
+        num: 5,
+    };
+
+    const strVal: string = R.prop('str', obj); // => 'string'
+    const numVal: number = R.prop('num', obj); // => 5
+
+    const strValCur: string = R.prop('str')(obj); // => 'string'
+    const numValCur: number = R.prop('num')(obj); // => 5
 };
 
 () => {
