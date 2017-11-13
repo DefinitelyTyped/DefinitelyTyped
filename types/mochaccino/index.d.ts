b2c11e682b667aec74ccfe75496cc1e63cc3288a
Thomas Puttkamer <tvp1981@gmx.de>
2017-11-13 19:05:34 +0100

    added TypeScript version to remove error on test

--- types/mochaccino/index.d.ts
+++ types/mochaccino/index.d.ts
@@ -2,6 +2,7 @@
 // Project: https://github.com/pawelgalazka/mochaccino#readme
 // Definitions by: Thomas-P <https://github.com/thomas-p>
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
+// TypeScript Version: 2.3
 
 /// <reference types="sinon" />
 
