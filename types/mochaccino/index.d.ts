f0c9f7b5fed6870712528457817202044757fe95
Thomas Puttkamer <tvp1981@gmx.de>
2017-11-13 19:21:52 +0100

    fixed errors on travis test #1

--- types/mochaccino/index.d.ts
+++ types/mochaccino/index.d.ts
@@ -6,67 +6,48 @@
 
 /// <reference types="sinon" />
 
+import * as Sinon from "sinon";
 
-declare module 'mochaccino' {
-
-    import * as Sinon from "sinon";
-
-    interface Expect {
-        not: Expect;
-
-        toBe<T>(arg: T): void;
-
-        toContain<T>(arg: T): void;
-
-        toEqual<T>(arg: T): void;
-
-        toHaveBeenCalled(): void;
-
-        toHaveBeenCalledWith(...arg: any[]): void;
-
-        toHaveBeenCalledTimes(callCount: number): void;
-
-        toBeDefined(): void;
-
-        toBeUndefined(): void;
-
-        toBeNull(): void;
-
-        toBeTruthy(): void;
-
-        toBeFalsy(): void;
-
-        toBeLessThan(other: number): void;
-
-        toBeGreaterThan(other: number): void;
-
-        toThrow(): void;
-
-        toThrowError<T>(errType: T): void;
-
-        toMatch<T>(matchExpression: T): void;
-    }
-
-
-    interface SpyProxy {
-        and: SpyProxy;
-        spyProxy: true;
-        getSubject: () => Sinon.SinonStub;
-        callThrough: () => void;
-        returnValue: (obj: any) => void;
-        callFake: (fake: Function) => void;
-    }
-
-    interface Dom {
-        exposedProperties: ['window', 'navigator', 'document'];
-        create: () => void;
-        destroy: () => void;
-        clear: () => void;
-    }
-
-    export function expect<T>(value: T): Expect;
-
-    export function spy(...config: any[]): (...args: any[]) => SpyProxy;
-
-    export const dom: Dom;
+interface Expect {
+    not: Expect;
+    toBe(arg: any): void;
+    toContain(arg: any): void;
+    toEqual(arg: any): void;
+    toHaveBeenCalled(): void;
+    toHaveBeenCalledWith(...arg: any[]): void;
+    toHaveBeenCalledTimes(callCount: number): void;
+    toBeDefined(): void;
+    toBeUndefined(): void;
+    toBeNull(): void;
+    toBeTruthy(): void;
+    toBeFalsy(): void;
+    toBeLessThan(other: number): void;
+    toBeGreaterThan(other: number): void;
+    toThrow(): void;
+    toThrowError(errType: any): void;
+    toMatch(matchExpression: any): void;
 }
+
+
+interface SpyProxy {
+    and: SpyProxy;
+    spyProxy: true;
+    getSubject: () => Sinon.SinonStub;
+    callThrough: () => void;
+    returnValue: (obj: any) => void;
+    callFake: (fake: Function) => void;
+}
+
+interface Dom {
+    exposedProperties: ['window', 'navigator', 'document'];
+    create: () => void;
+    destroy: () => void;
+    clear: () => void;
+}
+
+export function expect(value: any): Expect;
+
+export function spy(...config: any[]): (...args: any[]) => SpyProxy;
+
+export const dom: Dom;
+
