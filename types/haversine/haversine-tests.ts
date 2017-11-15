fec98c71f8986469ad16fe8339b98f2f383e8196
Daniel Hritzkiv <daniel.hritzkiv@gmail.com>
2017-11-15 11:26:32 -0500

    haversine: cleanup union types, simplify LatLonTuple, add GeoJSON test

--- types/haversine/haversine-tests.ts
+++ types/haversine/haversine-tests.ts
@@ -1,11 +1,11 @@
 import haversine = require('haversine');
 
-const start: haversine.CoordinateLongNames = {
+const start = {
   longitude: 48.1548256,
   latitude: 11.4017529
 };
 
-const end: haversine.CoordinateLongNames = {
+const end = {
   longitude: 52.5065133,
   latitude: 13.1445551
 };
@@ -17,19 +17,17 @@ const options: haversine.Options = {
 
 haversine(start, end, options);
 
-const startShort: haversine.CoordinateShortNames = {
+const startShort = {
   lon: 48.1548256,
   lat: 11.4017529
 };
 
-const endShort: haversine.CoordinateShortNames = {
+const endShort = {
   lon: 52.5065133,
   lat: 13.1445551
 };
 
 const optionsShort: haversine.Options = {
-  unit: 'km',
-  threshold: 1,
   format: '{lon,lat}'
 };
 
@@ -40,9 +38,29 @@ const startLatLon: haversine.LatLonTuple = [11.4017529, 48.1548256];
 const endLatLon: haversine.LatLonTuple = [13.1445551, 52.5065133];
 
 const optionsLatLon: haversine.Options = {
-  unit: 'km',
-  threshold: 1,
   format: '[lat,lon]'
 };
 
 haversine(startLatLon, endLatLon, optionsLatLon);
+
+const startGeoJSON = {
+  type: "Feature",
+  geometry: {
+    type: "LineString",
+    coordinates: startLatLon
+  }
+};
+
+const endGeoJSON = {
+  type: "Feature",
+  geometry: {
+    type: "LineString",
+    coordinates: endLatLon
+  }
+};
+
+const optionsGeoJSON: haversine.Options = {
+  format: 'geojson'
+};
+
+haversine(startGeoJSON, endGeoJSON, optionsGeoJSON);
