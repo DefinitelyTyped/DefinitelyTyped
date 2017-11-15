5564fca4c45b0bf81bbe1c6d386b8c1f63d96e90
Daniel Hritzkiv <daniel.hritzkiv@gmail.com>
2017-11-14 21:12:21 -0500

    Update haversine-tests.ts

--- types/haversine/haversine-tests.ts
+++ types/haversine/haversine-tests.ts
@@ -1,11 +1,11 @@
 import haversine = require('haversine');
 
-const start: haversine.Coordinate = {
+const start: haversine.CoordinateLongNames = {
   longitude: 48.1548256,
   latitude: 11.4017529
 };
 
-const end: haversine.Coordinate = {
+const end: haversine.CoordinateLongNames = {
   longitude: 52.5065133,
   latitude: 13.1445551
 };
@@ -16,3 +16,33 @@ const options: haversine.Options = {
 };
 
 haversine(start, end, options);
+
+const startShort: haversine.CoordinateShortNames = {
+  lon: 48.1548256,
+  lat: 11.4017529
+};
+
+const endShort: haversine.CoordinateShortNames = {
+  lon: 52.5065133,
+  lat: 13.1445551
+};
+
+const optionsShort: haversine.Options = {
+  unit: 'km',
+  threshold: 1,
+  format: '{lon,lat}'
+};
+
+haversine(startShort, endShort, optionsShort);
+
+const startLatLon: haversine.LatLonTuple = [11.4017529, 48.1548256];
+
+const endLatLon: haversine.LatLonTuple = [13.1445551, 52.5065133];
+
+const optionsLatLon: haversine.Options = {
+  unit: 'km',
+  threshold: 1,
+  format: '[lat,lon]'
+};
+
+haversine(startLatLon, endLatLon, optionsLatLon);
