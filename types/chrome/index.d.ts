533581997cdaaa211505f467fd68b3c622d007d6
Adam Pyle <adampyle@google.com>
2017-11-16 12:20:01 +1100

    Added additional parameters for chrome/chrome-app
    * Added extra parameters to LaunchData per API docs
    https://developer.chrome.com/apps/app_runtime#event-onLaunched
    
    * Changed typescript version required to 2.4 since named enums are used

--- types/chrome/index.d.ts
+++ types/chrome/index.d.ts
@@ -2,7 +2,7 @@
 // Project: http://developer.chrome.com/extensions/
 // Definitions by: Matthew Kimber <https://github.com/matthewkimber>, otiai10 <https://github.com/otiai10>, couven92 <https://github.com/couven92>, RReverser <https://github.com/rreverser>, sreimer15 <https://github.com/sreimer15>
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
-// TypeScript Version: 2.3
+// TypeScript Version: 2.4
 
 /// <reference types="filesystem" />
 
