8344355fa73fbe61bc55ac087000e343bf808587
Denis Malinochkin <mrmlnc@yandex-team.ru>
2017-11-16 18:18:26 +0300

    chore(mkdirp): Add me to authors

--- types/mkdirp/index.d.ts
+++ types/mkdirp/index.d.ts
@@ -1,6 +1,7 @@
 // Type definitions for mkdirp 0.5
 // Project: https://github.com/substack/node-mkdirp
 // Definitions by: Bart van der Schoor <https://github.com/Bartvds>
+//                 mrmlnc <https://github.com/mrmlnc>
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
 
 /// <reference types="node" />
