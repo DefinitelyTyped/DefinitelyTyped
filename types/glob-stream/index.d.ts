db5b48d15445654db916f91f327ff3ba743c730e
Denis Malinochkin <mrmlnc@yandex-team.ru>
2017-11-16 18:17:02 +0300

    chore(glob-stream): Add me to authors

--- types/glob-stream/index.d.ts
+++ types/glob-stream/index.d.ts
@@ -1,6 +1,7 @@
 // Type definitions for glob-stream v6.1.0
 // Project: https://github.com/wearefractal/glob-stream
 // Definitions by: Bart van der Schoor <https://github.com/Bartvds>
+//                 mrmlnc <https://github.com/mrmlnc>
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
 
 /// <reference types="node" />
