afabeda5cb3fadb35e02ece953ff2651c163af97
Chris Chares <chris@chares.io>
2017-11-21 15:52:19 -0700

    Increment version number

--- types/pg/index.d.ts
+++ types/pg/index.d.ts
@@ -1,4 +1,4 @@
-// Type definitions for pg 7.1
+// Type definitions for pg 7.4
 // Project: https://github.com/brianc/node-postgres
 // Definitions by: Phips Peter <https://github.com/pspeter3>
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
