2e99dd0165b180627f38df68f769106e7e57d3b0
Simon Schick <demwizzy@gmail.com>
2017-11-21 23:22:46 +0100

    enable that rule

--- types/elasticsearch/index.d.ts
+++ types/elasticsearch/index.d.ts
@@ -1,6 +1,12 @@
 // Type definitions for elasticsearch 5.0
 // Project: https://www.elastic.co/guide/en/elasticsearch/client/javascript-api/current/index.html
-// Definitions by: Casper Skydt <https://github.com/CasperSkydt>, Blake Smith <https://github.com/bfsmith>, Dave Dunkin <https://github.com/ddunkin>, Jeffery Grajkowski <https://github.com/pushplay>, Margus Lamp <https://github.com/mlamp>, Ahmad Ferdous Bin Alam <https://github.com/ahmadferdous>
+// Definitions by: Casper Skydt <https://github.com/CasperSkydt>
+//                 Blake Smith <https://github.com/bfsmith>
+//                 Dave Dunkin <https://github.com/ddunkin>
+//                 Jeffery Grajkowski <https://github.com/pushplay>
+//                 Margus Lamp <https://github.com/mlamp>
+//                 Ahmad Ferdous Bin Alam <https://github.com/ahmadferdous>
+//                 Simon Schick <demwizzy@gmail.com>
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
 // TypeScript Version: 2.2
 
