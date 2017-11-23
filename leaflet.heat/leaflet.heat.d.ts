8981174bc5ba89a4f36f774e6add0b527e43bc82
Önder Ceylan <onderceylan@gmail.com>
2017-11-23 23:11:08 +0100

    leaflet.heat TypeScript type definitions

---
+++ leaflet.heat/leaflet.heat.d.ts
@@ -0,0 +1,32 @@
+// Type definitions for Leaflet.heat v0.2.0
+// Project: https://github.com/Leaflet/Leaflet.heat
+// Definitions by: Önder Ceylan <https://github.com/onderceylan>
+// Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
+
+/// <reference path="../leaflet/leaflet.d.ts" />
+
+declare namespace L {
+    type HeatLatLngTuple = [number, number, number];
+
+    interface ColorGradientConfig {
+        [key: number]: string;
+    }
+
+    interface HeatMapOptions {
+        minOpacity?: number;
+        maxZoom?: number;
+        max?: number;
+        radius?: number;
+        blur?: number;
+        gradient?: ColorGradientConfig;
+    }
+
+    interface HeatLayer extends TileLayer {
+        setOptions?(options: HeatMapOptions): HeatLayer;
+        addLatLng?(latlng: LatLng | HeatLatLngTuple): HeatLayer;
+        setLatLngs?(latlngs: Array<LatLng | HeatLatLngTuple>): HeatLayer;
+        redraw(): HeatLayer;
+    }
+
+    function heatLayer(latlngs: Array<LatLng | HeatLatLngTuple>, options: HeatMapOptions): HeatLayer;
+}
