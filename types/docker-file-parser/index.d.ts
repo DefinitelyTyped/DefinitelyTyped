62b1ebf18c0f818a8765bd40ffdefb45444235eb
Yash Kulshrestha <yash.kulshrestha@gmail.com>
2017-11-22 19:10:24 -0500

    removed the module declaration

--- types/docker-file-parser/index.d.ts
+++ types/docker-file-parser/index.d.ts
@@ -3,21 +3,19 @@
 // Definitions by: Yash Kulshrestha <https://github.com/yashdalfthegray>
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
 
-declare module "docker-file-parser" {
-    export interface CommandEntry {
-        name: string;
-        args: string[];
-        lineno: number;
-        raw: string;
-        error?: string;
-    }
-
-    export interface ParseOptions {
-        includeComments: boolean;
-    }
-
-    export function parse(
-        contents: string,
-        options?: ParseOptions
-    ): CommandEntry[];
+export interface CommandEntry {
+    name: string;
+    args: string[];
+    lineno: number;
+    raw: string;
+    error?: string;
 }
+
+export interface ParseOptions {
+    includeComments: boolean;
+}
+
+export function parse(
+    contents: string,
+    options?: ParseOptions
+): CommandEntry[];
