00121a0418878414f9b1510725e4031c418f8b2b
Alex Lyman <alex@ajjlyman.com>
2017-11-23 18:56:17 -0800

    Fix linting failure.
    Weird, when I run lint locally, it demands that I have the tslint.json derived from "dtslint/dtslint.json" but Travis CI apparently disagrees and wants "dtslint/dt.json".

--- types/loadware/index.d.ts
+++ types/loadware/index.d.ts
@@ -1,4 +1,4 @@
-// Type definitions for loadware 2.0.0
+// Type definitions for loadware 2.0
 // Project: https://github.com/franciscop/loadware
 // Definitions by: A.J.J. Lyman <https://github.com/ALyman>
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
