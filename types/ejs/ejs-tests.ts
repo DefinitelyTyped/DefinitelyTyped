b8cabc4f9961690607009150de7e7eca79bbaadc
segayuu <segayuu@gmail.com>
2017-11-24 13:55:09 +0900

    Add Tests and Cleanup ignore lint errors

--- types/ejs/ejs-tests.ts
+++ types/ejs/ejs-tests.ts
@@ -1,3 +1,35 @@
 import ejs = require("ejs");
+import * as fs from 'graceful-fs';
+
+const fileName = 'test.ejs';
 const people = ['geddy', 'neil', 'alex'];
-const html = ejs.render('<%= people.join(", "); %>', { people: people });
+const data = { people };
+const template = '<%= people.join(", "); %>';
+const options = {delimiter: '$'};
+let result: string;
+let cacheResult: string;
+let ejsFunction: ejs.TemplateFunction;
+
+const SimpleCallback = (err: any, html?: string) => {
+    if (err) {
+        return null;
+    }
+    return html;
+};
+
+result = ejs.render(template);
+result = ejs.render(template, data);
+result = ejs.render(template, data, options);
+
+cacheResult = ejs.renderFile(fileName, SimpleCallback);
+cacheResult = ejs.renderFile(fileName, data, SimpleCallback);
+cacheResult = ejs.renderFile(fileName, data, options, SimpleCallback);
+
+ejsFunction = ejs.compile(template);
+ejsFunction({});
+ejsFunction(data);
+ejs.compile(template, options);
+
+ejs.fileLoader = (str: string) => str;
+
+ejs.clearCache();
