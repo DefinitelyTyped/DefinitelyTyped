53fea0a846ecbbc7ccfeb44350a1f37857fa8928
Önder Ceylan <onderceylan@gmail.com>
2017-11-25 10:18:08 +0100

    Set references to global leaflet definition Added package.json Removed optional flags from methods

--- types/leaflet.heat/index.d.ts
+++ types/leaflet.heat/index.d.ts
@@ -3,7 +3,7 @@
 // Definitions by: Önder Ceylan <https://github.com/onderceylan>
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
 
-/// <reference path="../../leaflet/leaflet.d.ts" />
+/// <reference types="leaflet" />
 
 declare namespace L {
     type HeatLatLngTuple = [number, number, number];
@@ -22,9 +22,9 @@ declare namespace L {
     }
 
     interface HeatLayer extends TileLayer {
-        setOptions?(options: HeatMapOptions): HeatLayer;
-        addLatLng?(latlng: LatLng | HeatLatLngTuple): HeatLayer;
-        setLatLngs?(latlngs: Array<LatLng | HeatLatLngTuple>): HeatLayer;
+        setOptions(options: HeatMapOptions): HeatLayer;
+        addLatLng(latlng: LatLng | HeatLatLngTuple): HeatLayer;
+        setLatLngs(latlngs: Array<LatLng | HeatLatLngTuple>): HeatLayer;
     }
 
     function heatLayer(latlngs: Array<LatLng | HeatLatLngTuple>, options: HeatMapOptions): HeatLayer;
