45013eff07a0864f0434ab002c9d34f4c71271dc
Önder Ceylan <onderceylan@gmail.com>
2017-11-25 10:50:57 +0100

    Added dependent TypeScript version

--- types/leaflet.heat/index.d.ts
+++ types/leaflet.heat/index.d.ts
@@ -1,7 +1,8 @@
-// Type definitions for Leaflet.heat
+// Type definitions for Leaflet.heat v0.2.0
 // Project: https://github.com/Leaflet/Leaflet.heat
 // Definitions by: Önder Ceylan <https://github.com/onderceylan>
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
+// TypeScript Version: 2.3
 
 /// <reference types="leaflet" />
 
