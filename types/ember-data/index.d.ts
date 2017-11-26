ef1bff7d81423e45b07c5dcc93c526944ea148de
Guangda Zhang <guangda@prosperworks.com>
2017-11-25 20:56:09 -0800

    optional third parameters

--- types/ember-data/index.d.ts
+++ types/ember-data/index.d.ts
@@ -1035,11 +1035,11 @@ declare namespace DS {
     /**
      * Takes a URL, an HTTP method and a hash of data, and makes an HTTP request.
      */
-    ajax(url: string, type: string, options: object): Promise<any>;
+    ajax(url: string, type: string, options?: object): Promise<any>;
     /**
      * Generate ajax options
      */
-    ajaxOptions(url: string, type: string, options: object): object;
+    ajaxOptions(url: string, type: string, options?: object): object;
     /**
      * By default, the RESTAdapter will send the query params sorted alphabetically to the
      * server.
