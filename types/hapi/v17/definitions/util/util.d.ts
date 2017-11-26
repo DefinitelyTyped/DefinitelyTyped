b35db538c2c9108f36e8ae64de2a67f35e087b0f
rafaelsouzaf <rafaelsouzaf@gmail.com>
2017-11-26 18:50:39 -0300

    Replaced enum HttpMethod to types like v16. Not very elegant solution but works easily with lower/upper case :)

--- types/hapi/v17/definitions/util/util.d.ts
+++ types/hapi/v17/definitions/util/util.d.ts
@@ -2,9 +2,6 @@ export interface Dictionary<T> {
     [key: string]: T;
 }
 
-export enum HttpMethod {
-    Get = "get",
-    Post = "post",
-    Put = "put",
-    Delete= "delete",
-}
+export type HTTP_METHODS_PARTIAL_LOWERCASE = 'get' | 'post' | 'put' | 'patch' | 'delete' | 'options';
+export type HTTP_METHODS_PARTIAL = 'GET' | 'POST' | 'PUT' | 'PATCH' | 'DELETE' | 'OPTIONS' | HTTP_METHODS_PARTIAL_LOWERCASE;
+export type HTTP_METHODS = 'HEAD' | 'head' | HTTP_METHODS_PARTIAL;
