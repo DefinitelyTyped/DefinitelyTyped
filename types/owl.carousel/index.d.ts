a49da75063a2e83f9d826e9237839cf65795103b
kennethanCeyer <kennethan@nhpcw.com>
2017-11-26 22:38:53 +0900

    fix dts header, append pr to #21752

--- types/owl.carousel/index.d.ts
+++ types/owl.carousel/index.d.ts
@@ -1,7 +1,7 @@
-// Type definitions for owl.carousel 2.2.1
+// Type definitions for owl.carousel 2.2
 // Project: https://github.com/OwlCarousel2/OwlCarousel2
 // Definitions by: Ismael Gorissen <https://github.com/igorissen>
-// Updated by: Kenneth Ceyer <https://github.com/KennethanCeyer>
+//                 Kenneth Ceyer <https://github.com/KennethanCeyer>
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
 // TypeScript Version: 2.3
 
