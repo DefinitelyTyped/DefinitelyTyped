c6affc02fcb6fe441acbe9a88db78c7e2749fdd6
yasupeke <yasupeke2@gmail.com>
2017-11-27 18:17:39 +0900

    [events]Fix parameter type

--- types/events/events-tests.ts
+++ types/events/events-tests.ts
@@ -10,8 +10,11 @@ const listener1 = () => {
 const listener2 = () => {
   console.log('listener2');
 };
-const listener3 = (arg1: string, arg2: string, arg3: number) => {
-  console.log('listener3');
+const listener3 = (arg1: string) => {
+  console.log('listener3', arg1);
+};
+const listener4 = () => {
+  console.log('type of number');
 };
 
 emitter.setMaxListeners(100);
@@ -24,30 +27,34 @@ emitter.on('send', listener3);
 
 emitter.once('send', listener);
 
+emitter.once(1, listener4);
+
 emitter.listeners('send');
 emitter.listenerCount('send');
 
 EventEmitter.defaultMaxListeners = 100;
 console.log(`count(static): ${EventEmitter.listenerCount(emitter, 'send')}`);
+console.log(`ncount: ${emitter.listenerCount('send')}`);
 
 setTimeout(() => {
-  console.log(`\ncount: ${emitter.listenerCount('send')}`);
+  console.log('\n');
   emitter.emit('send');
 }, 1000);
 
 setTimeout(() => {
-  console.log(`\ncount: ${emitter.listenerCount('send')}`);
+  console.log('\n');
   emitter.emit('send');
   emitter.removeListener('send', listener2);
 }, 2000);
 
 setTimeout(() => {
-  console.log(`\ncount: ${emitter.listenerCount('send')}`);
-  emitter.emit('send');
+  console.log('\n');
+  emitter.emit('send', 'params1');
   emitter.removeAllListeners('send');
 }, 3000);
 
 setTimeout(() => {
-  console.log(`\ncount: ${emitter.listenerCount('send')}`);
+  console.log('\n');
+  emitter.emit(1);
   emitter.emit('send');
 }, 3000);
