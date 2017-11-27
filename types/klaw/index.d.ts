c74475e985a15b76ed063edf5bc8fb9677be2558
Carlos Precioso <cprecioso@users.noreply.github.com>
2017-11-27 04:07:19 +0100

    Bump version number in klaw

--- types/klaw/index.d.ts
+++ types/klaw/index.d.ts
@@ -1,4 +1,4 @@
-// Type definitions for klaw v2.1.0
+// Type definitions for klaw v2.1.1
 // Project: https://github.com/jprichardson/node-klaw
 // Definitions by: Matthew McEachen <https://github.com/mceachen>
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
