c5ad054706234b4b6a98a483a56689f7fa91e019
胡玮文 <huww98@163.com>
2017-11-29 00:52:43 +0800

    add jQuery support add Tabs

--- types/materialize-css/test/materialize-css-module.test.ts
+++ types/materialize-css/test/materialize-css-module.test.ts
@@ -1,21 +1,48 @@
 import * as materialize from "materialize-css";
 
+const elem = document.querySelector('.whatever')!;
+
 // Sidenav
-const elem = document.querySelector('.sidenav')!;
+// $ExpectType Sidenav
+new materialize.Sidenav(elem);
 // $ExpectType Sidenav
 const sidenav = new materialize.Sidenav(elem, {
     edge: "left",
     inDuration: 300,
-    onCloseStart: () => console.log("closing")
+    onCloseStart(el) {
+        // $ExpectType Sidenav
+        this;
+        // $ExpectType Element
+        el;
+    }
 });
 // $ExpectType void
 sidenav.open();
 // $ExpectType void
 sidenav.destroy();
-
 // $ExpectType SidenavOptions
 sidenav.options;
 // $ExpectType Element
 sidenav.el;
 // $ExpectType boolean
 sidenav.isOpen;
+
+// Tabs
+// $ExpectType Tabs
+const tabs = new materialize.Tabs(elem, {
+    duration: 200,
+    onShow(content) {
+        // $ExpectType Tabs
+        this;
+        // $ExpectType Element
+        content;
+    }
+});
+// $ExpectType void
+tabs.destroy();
+// $ExpectType void
+tabs.select("id");
+// $ExpectType TabsOptions
+tabs.options;
+// $ExpectType number
+tabs.index;
