cd28d1935edf5d07dab7f63555c2d5a8e0f3963b
Evgeny Parkhomenko <rjpackito@inbox.ru>
2017-11-29 13:39:54 +0300

    Update index.d.ts

--- types/yandex-maps/index.d.ts
+++ types/yandex-maps/index.d.ts
@@ -1,7 +1,6 @@
 // Type definitions for yandex-maps 2.1
 // Project: https://github.com/Delagen/typings-yandex-maps
 // Definitions by: Delagen <https://github.com/Delagen>
-// <https://github.com/rjpackito>
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
 // TypeScript Version: 2.4
 
