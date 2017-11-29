6bd33023831818c610c3dd2cf2d62d155d2f8e36
Unknown <giles@roadnight.name>
2017-11-29 20:41:14 +0000

    Added author name / address

--- types/parse-ms/index.d.ts
+++ types/parse-ms/index.d.ts
@@ -1,6 +1,6 @@
 // Type definitions for parse-ms 1.0
 // Project: https://github.com/sindresorhus/parse-ms#readme
-// Definitions by: My Self <https://github.com/me>
+// Definitions by: Giles Roadnight <https://github.com/Roaders>
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
 
 export default function parseMs(ms: number): {
