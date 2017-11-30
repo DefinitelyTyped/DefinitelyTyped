c46598893cdd00eeb70d797ff2f2556e772898cc
Micha Reiser <micha.reiser@isolutions.ch>
2017-11-30 21:15:13 +0100

    React-i18next Generic translate
    Fixes #16395

--- types/react-i18next/index.d.ts
+++ types/react-i18next/index.d.ts
@@ -1,10 +1,10 @@
-// Type definitions for react-i18next 4.6
+// Type definitions for react-i18next 7.0
 // Project: https://github.com/i18next/react-i18next
 // Definitions by: Giedrius Grabauskas <https://github.com/GiedriusGrabauskas>
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
-// TypeScript Version: 2.3
+// TypeScript Version: 2.4
 
-import { TranslationFunction } from "i18next";
+import { i18n as I18n, TranslationFunction } from "i18next";
 
 import I18nextProvider from "./src/I18nextProvider";
 import Interpolate from "./src/interpolate";
@@ -22,16 +22,6 @@ export {
     TranslationFunction
 };
 
-/**
- * Extend your component's Prop interface with this one to get access to `this.props.t`
- *
- * Please note that if you use the `translateFuncName` option, you should create
- * your own interface just like this one, but with your name of the translation function.
- *
- * interface MyComponentProps extends ReactI18next.InjectedTranslateProps {}
- */
-export interface InjectedTranslateProps {
-    t?: TranslationFunction;
-}
+export { InjectedI18nProps, InjectedTranslateProps } from "./src/props";
 
 export as namespace reactI18Next;
