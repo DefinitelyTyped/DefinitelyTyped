6b6f5d25191d70ddf02b97ee9c3b4b0f535b56c2
rafaelsouzaf <rafaelsouzaf@gmail.com>
2017-11-30 20:25:27 -0300

    Fix in JsDocs.

--- types/hapi/v17/definitions/plugin/plugin-registered.d.ts
+++ types/hapi/v17/definitions/plugin/plugin-registered.d.ts
@@ -6,12 +6,12 @@ export interface PluginsListRegistered {
 }
 
 /**
- * [See docs](https://github.com/hapijs/hapi/blob/master/API.md#-serverregistrations)
  * An object of the currently registered plugins where each key is a registered plugin name and the value is an
  * object containing:
  * * version - the plugin version.
  * * name - the plugin name.
  * * options - (optional) options passed to the plugin during registration.
+ * [See docs](https://github.com/hapijs/hapi/blob/master/API.md#-serverregistrations)
  */
 export interface PluginRegistered {
 
