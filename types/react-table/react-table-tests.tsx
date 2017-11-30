d5667f7de2e758de897d3011fe19016e717472e7
Krzysztof Porębski <krzysztof.porebski@pirios.com>
2017-12-01 00:49:22 +0100

    Add missing semicolons

--- types/react-table/react-table-tests.tsx
+++ types/react-table/react-table-tests.tsx
@@ -69,26 +69,26 @@ const Component = (props: {}) => {
         defaultResized={[]}
         defaultExpanded={{}}
         defaultFilterMethod={(filter, row, column) => {
-          const id = filter.pivotId || filter.id
-          return row[id] !== undefined ? String(row[id]).startsWith(filter.value) : true
+          const id = filter.pivotId || filter.id;
+          return row[id] !== undefined ? String(row[id]).startsWith(filter.value) : true;
         }}
         columns={columns}
         defaultSortMethod={(a, b, desc) => {
           // force null and undefined to the bottom
-          a = (a === null || a === undefined) ? -Infinity : a
-          b = (b === null || b === undefined) ? -Infinity : b
+          a = (a === null || a === undefined) ? -Infinity : a;
+          b = (b === null || b === undefined) ? -Infinity : b;
           // force any string values to lowercase
-          a = a === 'string' ? a.toLowerCase() : a
-          b = b === 'string' ? b.toLowerCase() : b
+          a = a === 'string' ? a.toLowerCase() : a;
+          b = b === 'string' ? b.toLowerCase() : b;
           // Return either 1 or -1 to indicate a sort priority
           if (a > b) {
-            return 1
+            return 1;
           }
           if (a < b) {
-            return -1
+            return -1;
           }
           // returning 0, undefined or any falsey value will use subsequent sorts or the index as a tiebreaker
-          return 0
+          return 0;
         }}
         PadRowComponent={() => <span>&nbsp;</span>}
         page={undefined}
