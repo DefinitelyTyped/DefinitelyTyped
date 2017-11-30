7d6c5756865f2f52040d4554077887b47946afd0
rafaelsouzaf <rafaelsouzaf@gmail.com>
2017-11-30 15:15:14 -0300

    Reviewed Request declarations.

--- types/hapi/v17/definitions/request/request-route.d.ts
+++ types/hapi/v17/definitions/request/request-route.d.ts
@@ -1,7 +1,13 @@
 import {RouteOptions, ServerRealm, HTTP_METHODS_PARTIAL} from "hapi";
 
 /**
- * The request route information object
+ * The request route information object, where:
+ * * method - the route HTTP method.
+ * * path - the route path.
+ * * vhost - the route vhost option if configured.
+ * * realm - the active realm associated with the route.
+ * * settings - the route options object with all defaults applied.
+ * * fingerprint - the route internal normalized string representing the normalized path.
  * [See docs](https://github.com/hapijs/hapi/blob/master/API.md#-requestroute)
  */
 export interface RequestRoute {
--- types/hapi/v17/definitions/request/request.d.ts
+++ types/hapi/v17/definitions/request/request.d.ts
@@ -8,6 +8,7 @@ import {PluginsStates} from "../plugin/plugin";
 import {RequestRoute} from "./request-route";
 import {RequestAuth} from "./request-auth";
 import {RequestInfo} from "./request-info";
+import * as Podium from "podium";
 
 /**
  * TODO both objects ReplyValue and _ReplyValue I found in the v16 TS definition, but I don't found it in the documentation. Need review.
@@ -16,232 +17,227 @@ import {RequestInfo} from "./request-info";
 export type ReplyValue = _ReplyValue | Promise<_ReplyValue>;
 export type _ReplyValue = null | undefined | string | number | boolean | Buffer | Error | stream.Stream | Object; // | array;
 
+/**
+ * An object containing the values of params, query, and payload before any validation modifications made. Only set when input validation is performed.
+ * For context [See docs](https://github.com/hapijs/hapi/blob/master/API.md#-requestorig)
+ */
 export interface RequestOrig {
     params: any;
     query: any;
     payload: any;
 }
 
-export interface Request {
+/**
+ * The request object is created internally for each incoming request. It is not the same object received from the node
+ * HTTP server callback (which is available via [request.raw.req](https://github.com/hapijs/hapi/blob/master/API.md#request.raw)). The request properties change throughout
+ * the request [lifecycle](https://github.com/hapijs/hapi/blob/master/API.md#request-lifecycle).
+ */
+export interface Request extends Podium {
 
     /**
-     Access: read / write.
-     Application-specific state. Provides a safe place to store application data without potential conflicts with the framework. Should not be used by plugins which should use plugins[name].
+     * Application-specific state. Provides a safe place to store application data without potential conflicts with the framework. Should not be used by plugins which should use plugins[name].
+     * [See docs](https://github.com/hapijs/hapi/blob/master/API.md#-requestapp)
      */
     app: any;
 
     /**
-     Access: read only.
-     Authentication information:
-     artifacts - an artifact object received from the authentication strategy and used in authentication-related actions.
-     credentials - the credential object received during the authentication process. The presence of an object does not mean successful authentication.
-     error - the authentication error is failed and mode set to 'try'.
-     isAuthenticated - true if the request has been successfully authenticated, otherwise false.
-     isAuthorized - true is the request has been successfully authorized against the route authentication access configuration. If the route has not access rules defined or if the request failed authorization, set to false.
-     mode - the route authentication mode.
-     strategy - the name of the strategy used.
+     * Authentication information:
+     * * artifacts - an artifact object received from the authentication strategy and used in authentication-related actions.
+     * * credentials - the credential object received during the authentication process. The presence of an object does not mean successful authentication.
+     * * error - the authentication error is failed and mode set to 'try'.
+     * * isAuthenticated - true if the request has been successfully authenticated, otherwise false.
+     * * isAuthorized - true is the request has been successfully authorized against the route authentication access configuration. If the route has not access rules defined or if the request failed authorization, set to false.
+     * * mode - the route authentication mode.
+     * * strategy - the name of the strategy used.
+     * [See docs](https://github.com/hapijs/hapi/blob/master/API.md#-requestauth)
      */
     readonly auth: RequestAuth;
 
     /**
-     Access: read only and the public podium interface.
-     The request.events supports the following events:
-     'peek' - emitted for each chunk of payload data read from the client connection. The event method signature is function(chunk, encoding).
-     'finish' - emitted when the request payload finished reading. The event method signature is function ().
-     'disconnect' - emitted when a request errors or aborts unexpectedly.
+     * Access: read only and the public podium interface.
+     * The request.events supports the following events:
+     * * 'peek' - emitted for each chunk of payload data read from the client connection. The event method signature is function(chunk, encoding).
+     * * 'finish' - emitted when the request payload finished reading. The event method signature is function ().
+     * * 'disconnect' - emitted when a request errors or aborts unexpectedly.
+     * [See docs](https://github.com/hapijs/hapi/blob/master/API.md#-requestevents)
      */
-    events: 'peek' | 'finish' | 'disconnect';
+    events: 'peek' | 'finish' | 'disconnect'; // TODO I think it's wrong. Maybe is events.on(' ', ' '). It needs revision.
 
     /**
-     Access: read only.
-     The raw request headers (references request.raw.req.headers).
+     * The raw request headers (references request.raw.req.headers).
+     * [See docs](https://github.com/hapijs/hapi/blob/master/API.md#-requestheaders)
      */
-    headers: Dictionary<string>;
+    readonly headers: Dictionary<string>;
 
     /**
-     Access: read only.
-     Request information:
-         acceptEncoding - the request preferred encoding.
-         cors - if CORS is enabled for the route, contains the following:
-         isOriginMatch - true if the request 'Origin' header matches the configured CORS restrictions. Set to false if no 'Origin' header is found or if it does not match. Note that this is only available after the 'onRequest' extension point as CORS is configured per-route and no routing decisions are made at that point in the request lifecycle.
-         host - content of the HTTP 'Host' header (e.g. 'example.com:8080').
-         hostname - the hostname part of the 'Host' header (e.g. 'example.com').
-         id - a unique request identifier (using the format '{now}:{connection.info.id}:{5 digits counter}').
-         received - request reception timestamp.
-         referrer - content of the HTTP 'Referrer' (or 'Referer') header.
-         remoteAddress - remote client IP address.
-         remotePort - remote client port.
-         responded - request response timestamp (0 is not responded yet).
-     Note that the request.info object is not meant to be modified.
+     * Request information:
+     * * acceptEncoding - the request preferred encoding.
+     * * cors - if CORS is enabled for the route, contains the following:
+     * * isOriginMatch - true if the request 'Origin' header matches the configured CORS restrictions. Set to false if no 'Origin' header is found or if it does not match. Note that this is only available after the 'onRequest' extension point as CORS is configured per-route and no routing decisions are made at that point in the request lifecycle.
+     * * host - content of the HTTP 'Host' header (e.g. 'example.com:8080').
+     * * hostname - the hostname part of the 'Host' header (e.g. 'example.com').
+     * * id - a unique request identifier (using the format '{now}:{connection.info.id}:{5 digits counter}').
+     * * received - request reception timestamp.
+     * * referrer - content of the HTTP 'Referrer' (or 'Referer') header.
+     * * remoteAddress - remote client IP address.
+     * * remotePort - remote client port.
+     * * responded - request response timestamp (0 is not responded yet).
+     * Note that the request.info object is not meant to be modified.
+     * [See docs](https://github.com/hapijs/hapi/blob/master/API.md#-requestinfo)
      */
     readonly info: RequestInfo;
 
     /**
-     Access: read only.
-     An array containing the logged request events.
-     Note that this array will be empty if route log.collect is set to false.
+     * An array containing the logged request events.
+     * Note that this array will be empty if route log.collect is set to false.
      */
-    logs: any[]; // TODO check if type any is apropiate here.
+    readonly logs: any[];
 
     /**
-     Access: read only.
-     The request method in lower case (e.g. 'get', 'post').
+     * The request method in lower case (e.g. 'get', 'post').
      */
-    method: HTTP_METHODS_PARTIAL_LOWERCASE;
+    readonly method: HTTP_METHODS_PARTIAL_LOWERCASE;
 
     /**
-     Access: read only.
-     The parsed content-type header. Only available when payload parsing enabled and no payload error occurred.
+     * The parsed content-type header. Only available when payload parsing enabled and no payload error occurred.
      */
-    mime: string;
+    readonly mime: string;
 
     /**
-     Access: read only.
-     An object containing the values of params, query, and payload before any validation modifications made. Only set when input validation is performed.
+     * An object containing the values of params, query, and payload before any validation modifications made. Only set when input validation is performed.
      */
-    orig: RequestOrig;
+    readonly orig: RequestOrig;
 
     /**
-     Access: read only.
-     An object where each key is a path parameter name with matching value as described in Path parameters.
+     * An object where each key is a path parameter name with matching value as described in [Path parameters](https://github.com/hapijs/hapi/blob/master/API.md#path-parameters).
      */
-    params: Dictionary<string>;
+    readonly params: Dictionary<string>;
 
     /**
-     Access: read only.
-     An array containing all the path params values in the order they appeared in the path.
+     * An array containing all the path params values in the order they appeared in the path.
      */
-    paramsArray: string[];
+    readonly paramsArray: string[];
 
     /**
-     Access: read only.
-     The request URI's pathname component.
+     * The request URI's pathname component.
      */
-    path: string;
+    readonly path: string;
 
     /**
-     Access: read only.
-     The request payload based on the route payload.output and payload.parse settings.
+     * The request payload based on the route payload.output and payload.parse settings.
      * TODO check this typing and add references / links.
      */
-    payload: stream.Readable | Buffer | any;
+    readonly payload: stream.Readable | Buffer | any;
 
     /**
-     Access: read / write.
-     Plugin-specific state. Provides a place to store and pass request-level plugin data. The plugins is an object where each key is a plugin name and the value is the state.
+     * Plugin-specific state. Provides a place to store and pass request-level plugin data. The plugins is an object where each key is a plugin name and the value is the state.
      */
     plugins: PluginsStates;
 
     /**
-     Access: read only.
-     An object where each key is the name assigned by a route pre-handler methods function. The values are the raw values provided to the continuation function as argument. For the wrapped response object, use responses.
+     * An object where each key is the name assigned by a route pre-handler methods function. The values are the raw values provided to the continuation function as argument. For the wrapped response object, use responses.
      */
-    pre: Object;
+    readonly pre: Object;
 
     /**
-     Access: read / write (see limitations below).
-     The response object when set. The object can be modified but must not be assigned another object. To replace the response with another from within an extension point, use reply(response) to override with a different response. Contains null when no response has been set (e.g. when a request terminates prematurely when the client disconnects).
+     * Access: read / write (see limitations below).
+     * The response object when set. The object can be modified but must not be assigned another object. To replace the response with another from within an extension point, use reply(response) to override with a different response. Contains null when no response has been set (e.g. when a request terminates prematurely when the client disconnects).
      */
     response: ResponseObject | null;
 
     /**
-     Access: read only.
-     Same as pre but represented as the response object created by the pre method.
+     * Same as pre but represented as the response object created by the pre method.
      */
-    preResponses: Object;
+    readonly preResponses: Object;
 
     /**
-     Access: read only.
-     By default the object outputted from node's URL parse() method. Might also be set indirectly via request.setUrl in which case it may be a string (if url is set to an object with the query attribute as an unparsed string).
+     * By default the object outputted from node's URL parse() method. Might also be set indirectly via request.setUrl in which case it may be a string (if url is set to an object with the query attribute as an unparsed string).
      */
-    query: any;
+    readonly query: any;
 
     /**
-     Access: read only.
-     An object containing the Node HTTP server objects. Direct interaction with these raw objects is not recommended.
-     req - the node request object.
-     res - the node response object.
+     * An object containing the Node HTTP server objects. Direct interaction with these raw objects is not recommended.
+     * * req - the node request object.
+     * * res - the node response object.
      */
-    raw: {
+    readonly raw: {
         req: http.IncomingMessage;
         res: http.ServerResponse;
     };
 
     /**
-     Access: read only.
-     The request route information object, where:
-         method - the route HTTP method.
-         path - the route path.
-         vhost - the route vhost option if configured.
-         realm - the active realm associated with the route.
-         settings - the route options object with all defaults applied.
-         fingerprint - the route internal normalized string representing the normalized path.
+     * The request route information object, where:
+     * * method - the route HTTP method.
+     * * path - the route path.
+     * * vhost - the route vhost option if configured.
+     * * realm - the active realm associated with the route.
+     * * settings - the route options object with all defaults applied.
+     * * fingerprint - the route internal normalized string representing the normalized path.
+     * [See docs](https://github.com/hapijs/hapi/blob/master/API.md#-requestroute)
      */
-    route: RequestRoute;
+    readonly route: RequestRoute;
 
     /**
-     Access: read only and the public server interface.
-     The server object.
+     * Access: read only and the public server interface.
+     * The server object.
      */
-    server: Server; // TODO check if this server object should be right.
+    server: Server;
 
     /**
-     Access: read only.
-     An object containing parsed HTTP state information (cookies) where each key is the cookie name and value is the matching cookie content after processing using any registered cookie definition.
+     * An object containing parsed HTTP state information (cookies) where each key is the cookie name and value is the matching cookie content after processing using any registered cookie definition.
      */
-    state: Dictionary<any>;
+    readonly state: Dictionary<any>;
 
     /**
-     Access: read only.
-     The parsed request URI.
+     * The parsed request URI.
      */
-    url: url.Url;
+    readonly url: url.Url;
 
     /**
-     * request.generateResponse(source, [options])
      * Returns a response which you can pass into the reply interface where:
-     * [See docs] (https://hapijs.com/api/17.0.1#request.generateResponse())
      * @param source - the value to set as the source of the reply interface, optional.
      * @param options - options for the method, optional.
+     * @return ResponseObject
+     * [See docs] (https://hapijs.com/api/17.0.1#request.generateResponse())
      */
     generateResponse(source?: ReplyValue, options?: {marshal?: any; prepare?: any; close?: any; variety?: any}): ResponseObject;
 
     /**
-     * request.log(tags, [data])
      * Logs request-specific events. When called, the server emits a 'request' event which can be used by other listeners or plugins. The arguments are:
      * @param tags - a string or an array of strings (e.g. ['error', 'database', 'read']) used to identify the event. Tags are used instead of log levels and provide a much more expressive mechanism for describing and filtering events.
      * @param data - (optional) an message string or object with the application data being logged. If data is a function, the function signature is function() and it called once to generate (return value) the actual data emitted to the listeners.
      * Any logs generated by the server internally will be emitted only on the 'request-internal' channel and will include the event.internal flag set to true.
+     * @return void
+     * [See docs](https://github.com/hapijs/hapi/blob/master/API.md#-requestlogtags-data)
      */
     log(tags: string | string[], data?: string | Object | (() => string | Object)): void;
 
     /**
-     * request.route.auth.access(request)
-     * [See docs] (https://hapijs.com/api/17.0.1#-requestrouteauthaccessrequest)
      * Validates a request against the route's authentication access configuration, where:
      * @param request - the request object.
-     * Return value: true if the request would have passed the route's access requirements.
+     * @return Return value: true if the request would have passed the route's access requirements.
      * Note that the route's authentication mode and strategies are ignored. The only match is made between the request.auth.credentials scope and entity information and the route access configuration.
      * If the route uses dynamic scopes, the scopes are constructed against the request.query, request.params, request.payload, and request.auth.credentials which may or may not match between the route and the request's route. If this method is called using a request that has not been authenticated (yet or not at all), it will return false if the route requires any authentication.
+     * [See docs] (https://hapijs.com/api/17.0.1#-requestrouteauthaccessrequest)
      */
-    // TODO how to implement it? It is a function inside route.auth! The v16 doesn't have this option.
+    // TODO how to implement it? It is a function inside route.auth! We have another "route" properti in this file. (The v16 doesn't have this option)
 
     /**
-     * request.setMethod(method)
      * Changes the request method before the router begins processing the request where:
      * @param method - is the request HTTP method (e.g. 'GET').
+     * @return void
      * Can only be called from an 'onRequest' extension method.
      * [See docs] (https://hapijs.com/api/17.0.1#-requestsetmethodmethod)
      */
     setMethod(method: HTTP_METHODS_PARTIAL): void;
 
     /**
-     * request.setUrl(url, [stripTrailingSlash]
-     * [See docs] (https://hapijs.com/api/17.0.1#-requestseturlurl-striptrailingslash)
      * Changes the request URI before the router begins processing the request where:
      * Can only be called from an 'onRequest' extension method.
      * @param url - the new request URI. If url is a string, it is parsed with node's URL parse() method with parseQueryString set to true. url can also be set to an object compatible with node's URL parse() method output.
      * @param stripTrailingSlash - if true, strip the trailing slash from the path. Defaults to false.
+     * @return void
+     * [See docs] (https://hapijs.com/api/17.0.1#-requestseturlurl-striptrailingslash)
      */
     setUrl(url: string | url.URL, stripTrailingSlash?: boolean): void;
 
