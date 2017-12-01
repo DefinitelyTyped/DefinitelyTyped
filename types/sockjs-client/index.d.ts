b00fe9b49ce3532069400e0d396cf571988e8d6f
Soner Koksal <soner.koksal@teleservice.com>
2017-12-01 18:34:32 +0300

    revised version and added author

--- types/sockjs-client/index.d.ts
+++ types/sockjs-client/index.d.ts
@@ -1,8 +1,9 @@
-// Type definitions for sockjs-client 1.0
+// Type definitions for sockjs-client 1.1.4
 // Project: https://github.com/sockjs/sockjs-client
 // Definitions by: Emil Ivanov <https://github.com/vladev>
 //                 Alexander Rusakov <https://github.com/arusakov>
 //                 BendingBender <https://github.com/BendingBender>
+//                 Soner Köksal <https://github.com/renjfk>
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
 
 export = SockJS;
