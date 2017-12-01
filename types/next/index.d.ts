8c42c5e861f53eb06874be860846b96d9700cf3c
Brice BERNARD <brikou@gmail.com>
2017-12-01 11:18:38 +0100

    Add my name to index.d.ts

--- types/next/index.d.ts
+++ types/next/index.d.ts
@@ -1,6 +1,7 @@
 // Type definitions for next 2.4
 // Project: https://github.com/zeit/next.js
 // Definitions by: Drew Hays <https://github.com/dru89>
+//                 Brice BERNARD <https://github.com/brikou>
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
 // TypeScript Version: 2.3
 
