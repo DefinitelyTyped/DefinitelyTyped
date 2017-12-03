53bf3b04acc8acebd7536caa516b17d652eeaea2
Marcelo Waisman <marcelo.waisman@gmail.com>
2017-12-03 11:56:31 +0200

    recharts: add some missing label definitions to BarProps XAxisProps and YAxisProps

--- types/recharts/index.d.ts
+++ types/recharts/index.d.ts
@@ -202,6 +202,7 @@ export interface BarProps extends EventAttributes, Partial<PresentationAttribute
     hide?: boolean;
     shape?: React.ReactElement<any> | ContentRenderer<RectangleProps>;
     data?: BarData[];
+    label?: boolean | Label | React.SFC<LabelProps> | React.ReactElement<LabelProps> | ContentRenderer<Label>;
 }
 
 export class Bar extends React.Component<BarProps> { }
@@ -820,6 +821,14 @@ export interface Label {
     content?: React.ReactElement<any> | ContentRenderer<Label>;
 }
 
+export interface LabelProps extends ViewBox {
+    index: number;
+    value: string | number;
+    offset?: number;
+    viewBox: ViewBox;
+    content?: React.ReactElement<any> | ContentRenderer<Label>;
+}
+
 export type AxisDomain = string | number | ContentRenderer<any> | 'auto' | 'dataMin' | 'dataMax';
 
 export interface XPadding {
@@ -868,6 +877,7 @@ export interface XAxisProps extends EventAttributes {
     tickSize?: number;
     interval?: AxisInterval;
     reversed?: boolean;
+    label?: string | number | Label;
 }
 
 export class XAxis extends React.Component<XAxisProps> { }
@@ -915,6 +925,7 @@ export interface YAxisProps extends EventAttributes {
     tickSize?: number;
     interval?: AxisInterval;
     reversed?: boolean;
+    label?: string | number | Label;
 }
 
 export class YAxis extends React.Component<YAxisProps> { }
