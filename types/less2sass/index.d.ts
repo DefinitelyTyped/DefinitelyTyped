c0828a80fd5a246fcc606394928d22a1e84ee1af
William Lohan <william.g.lohan@gmail.com>
2017-12-02 21:38:58 -0800

    remove private methods

--- types/less2sass/index.d.ts
+++ types/less2sass/index.d.ts
@@ -5,15 +5,6 @@
 
 declare class Less2Sass {
     convert(file: string): string;
-    private convertColourHelpers(): Less2Sass;
-    private convertExtend(): Less2Sass;
-    private convertFileExtensions(): Less2Sass;
-    private convertFunctionUnit(): Less2Sass;
-    private convertInterpolatedVariables(): Less2Sass;
-    private convertMixins(): Less2Sass;
-    private convertTildaStrings(): Less2Sass;
-    private convertVariables(): Less2Sass;
-    private includeMixins(): Less2Sass;
 }
 
 declare const less2sass: Less2Sass;
