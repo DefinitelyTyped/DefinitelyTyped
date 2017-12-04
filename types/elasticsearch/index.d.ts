886c0a0b655a549864a3374a6fcd57f0c55ac13f
daphnes <daphne@tamtam.nl>
2017-12-04 12:26:48 +0100

    Remove tabbing

--- types/elasticsearch/index.d.ts
+++ types/elasticsearch/index.d.ts
@@ -620,30 +620,30 @@ export interface SearchParams extends GenericParams {
     type?: NameList;
 }
 
-    export interface SearchResponse<T> {
-        took: number;
-        timed_out: boolean;
-        _scroll_id?: string;
-        _shards: ShardsResponse;
-        hits: {
-            total: number;
-            max_score: number;
-            hits: Array<{
-                _index: string;
-                _type: string;
-                _id: string;
-                _score: number;
-                _source: T;
-                _version?: number;
-                _explanation?: Explanation;
-                fields?: any;
-                highlight?: any;
-                inner_hits?: any;
-                sort?: string[];
-            }>;
-        };
-        aggregations?: any;
-    }
+export interface SearchResponse<T> {
+    took: number;
+    timed_out: boolean;
+    _scroll_id?: string;
+    _shards: ShardsResponse;
+    hits: {
+        total: number;
+        max_score: number;
+        hits: Array<{
+            _index: string;
+            _type: string;
+            _id: string;
+            _score: number;
+            _source: T;
+            _version?: number;
+            _explanation?: Explanation;
+            fields?: any;
+            highlight?: any;
+            inner_hits?: any;
+            sort?: string[];
+        }>;
+    };
+    aggregations?: any;
+}
 
 export interface SearchShardsParams extends GenericParams {
     preference?: string;
