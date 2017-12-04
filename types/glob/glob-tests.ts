b56c3ff22f1d89212fd0b08b5974ee7590e9d65d
segayuu <segayuu@gmail.com>
2017-12-04 11:32:21 +0900

    cleanup ignore lint error: only-arrow-functions

--- types/glob/glob-tests.ts
+++ types/glob/glob-tests.ts
@@ -5,7 +5,11 @@ const Glob = glob.Glob;
 	const pattern = "test/a/**/[cg]/../[cg]";
 	console.log(pattern);
 
-	const mg = new Glob(pattern, {mark: true, sync: true}, function(er, matches) {
+	const mg = new Glob(pattern, {mark: true, sync: true}, (er, matches) => {
+		if (er) {
+			console.error(er);
+			return;
+		}
 		console.log("matches", matches);
 	});
 	console.log("after");
@@ -15,7 +19,11 @@ const Glob = glob.Glob;
 	const pattern = "{./*/*,/*,/usr/local/*}";
 	console.log(pattern);
 
-	const mg = new Glob(pattern, {mark: true}, function(er, matches) {
+	const mg = new Glob(pattern, {mark: true}, (er, matches) => {
+		if (er) {
+			console.error(er);
+			return;
+		}
 		console.log("matches", matches);
 	});
 	console.log("after");
