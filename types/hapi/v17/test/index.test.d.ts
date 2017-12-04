095a0035552e45db671fddd1babf5a005edfa96b
BorntraegerMarc <marc.borntraeger@gmail.com>
2017-12-04 09:34:47 +0100

    Added first hapi TS tests

---
+++ types/hapi/v17/test/index.test.d.ts
@@ -0,0 +1,12 @@
+// Type definitions for hapi 17.0
+// Project: https://github.com/hapijs/hapi
+// Definitions by: Marc Bornträger<https://github.com/BorntraegerMarc>
+//                 Rafael Souza Fijalkowski <https://github.com/rafaelsouzaf>
+// Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
+
+// Note/Disclaimer: None.
+
+/// <reference types="node" />
+
+/** Getting Started */
+export * from './getting-started/01-creating-a-server';
