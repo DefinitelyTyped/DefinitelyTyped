46fac18d3959cb0195e2846dbd4f45493fef10d7
Bradley Odell <btodell@hotmail.com>
2017-12-04 17:35:14 -0800

    Fixed incorrect definitions for 'wheel'. Updated tests for 'wheel' definitions.

--- types/wheel/index.d.ts
+++ types/wheel/index.d.ts
@@ -6,20 +6,24 @@
 /**
  * Adds a callback to receive mouse wheel events from the given element.
  */
-export default function addWheelListener(element: GlobalEventHandlers,
-                                         callback: (event: WheelEvent) => void,
-                                         useCapture?: boolean): void;
+declare function wheel(element: GlobalEventHandlers,
+    callback: (event: WheelEvent) => void,
+    useCapture?: boolean): void;
 
-/**
- * Adds a callback to receive mouse wheel events from the given element.
- */
-export function addWheelListener(element: GlobalEventHandlers,
-                                 callback: (event: WheelEvent) => void,
-                                 useCapture?: boolean): void;
+declare namespace wheel {
+    /**
+     * Adds a callback to receive mouse wheel events from the given element.
+     */
+    function addWheelListener(element: GlobalEventHandlers,
+        callback: (event: WheelEvent) => void,
+        useCapture?: boolean): void;
 
-/**
- * Removes a previously added wheel listener callback.
- */
-export function removeWheelListener(element: GlobalEventHandlers,
-                                    callback: (event: WheelEvent) => void,
-                                    useCapture?: boolean): void;
+    /**
+     *  Removes a previously added wheel listener callback.
+     */
+    function removeWheelListener(element: GlobalEventHandlers,
+        callback: (event: WheelEvent) => void,
+        useCapture?: boolean): void;
+}
+
+export = wheel;
