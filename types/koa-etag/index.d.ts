312333650405897e589174a2f0a0e94e2d3896ff
Matthew Bull <matt_wings@hotmail.com>
2017-12-06 20:53:04 +0000

    Add types for koa-etag

---
+++ types/koa-etag/index.d.ts
@@ -0,0 +1,11 @@
+// Type definitions for koa-etag 3.0
+// Project: https://github.com/koajs/etag#readme
+// Definitions by: Matthew Bull <https://github.com/wingsbob>
+// Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
+
+import * as koa from 'koa';
+import * as etag from 'etag';
+
+declare function koaEtag(options?: etag.Options): koa.Middleware;
+
+export = koaEtag;
