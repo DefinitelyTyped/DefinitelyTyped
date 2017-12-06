8a8d4bcbce5a2b6621dd9ca28a26bbe9b7b4b31d
Matthew Bull <wingsbob@users.noreply.github.com>
2017-12-06 21:00:14 +0000

    Add typescript version, due to dependency on koa

--- types/koa-etag/index.d.ts
+++ types/koa-etag/index.d.ts
@@ -2,6 +2,7 @@
 // Project: https://github.com/koajs/etag#readme
 // Definitions by: Matthew Bull <https://github.com/wingsbob>
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
+// TypeScript Version: 2.3
 
 import * as koa from 'koa';
 import * as etag from 'etag';
