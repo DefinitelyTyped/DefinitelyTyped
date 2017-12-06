d1ad427dfe009c927dbdbb291716488eca612a87
Matthew James <matthew.d.james87@gmail.com>
2017-12-06 19:48:30 +0000

    Added definition and tests

---
+++ types/trash/index.d.ts
@@ -0,0 +1,15 @@
+// Type definitions for trash 4.2
+// Project: https://github.com/sindresorhus/trash#readme
+// Definitions by: My Self <https://github.com/me>
+// Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
+
+declare module "trash" {
+
+    interface TrashOptions {
+        glob: boolean;
+    }
+
+    function trash(iterable: Iterable<string>, opts?: TrashOptions): Promise<void>;
+
+    export default trash;
+}
