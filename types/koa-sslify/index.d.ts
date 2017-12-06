f31dca883b02239695384c2144a5322094046154
Matthew Bull <wingsbob@users.noreply.github.com>
2017-12-06 21:24:05 +0000

    Actually put my name on it :facepalm:

--- types/koa-sslify/index.d.ts
+++ types/koa-sslify/index.d.ts
@@ -1,6 +1,6 @@
 // Type definitions for koa-sslify 2.1
 // Project: https://github.com/turboMaCk/koa-sslify#readme
-// Definitions by: My Self <https://github.com/me>
+// Definitions by: Matthew Bull <https://github.com/wingsbob>
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
 // TypeScript Version: 2.3
 
