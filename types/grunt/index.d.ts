47b5be85a4c3024ede894f606d2420b45c5b5cca
Kiarash <kiarash@si-co.net>
2017-12-07 19:44:25 +0100

    Update version

--- types/grunt/index.d.ts
+++ types/grunt/index.d.ts
@@ -1,4 +1,4 @@
-// Type definitions for Grunt 0.4.x
+// Type definitions for Grunt 1.0.x
 // Project: http://gruntjs.com
 // Definitions by: Jeff May <https://github.com/jeffmay>
 //                 Basarat Ali Syed <https://github.com/basarat>
