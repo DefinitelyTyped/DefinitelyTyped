4aaa97a69fc660175096fd6772997a4a077abeb9
Howard Mak <howard@grabcad.com>
2017-12-07 12:57:11 -0500

    PR feedback: Tighten signatures for CallSite methods

--- types/node/index.d.ts
+++ types/node/index.d.ts
@@ -47,9 +47,14 @@ interface Error {
 
 // Declare "static" methods in Error
 interface ErrorConstructor {
+    /** Create .stack property on a target object */
     captureStackTrace(targetObject: Object, constructorOpt?: Function): void;
 
-    // See https://github.com/v8/v8/wiki/Stack%20Trace%20API#customizing-stack-traces
+    /**
+     * Optional override for formatting stack traces
+     *
+     * @see https://github.com/v8/v8/wiki/Stack%20Trace%20API#customizing-stack-traces
+     */
     prepareStackTrace?: (err: Error, stackTraces: NodeJS.CallSite[]) => any;
 
     stackTraceLimit: number;
@@ -317,19 +322,44 @@ declare namespace NodeJS {
     }
 
     export interface CallSite {
-        getThis: Function; // returns the value of this
-        getTypeName(): string; // returns the type of this as a string. This is the name of the function stored in the constructor field of this, if available, otherwise the object's [[Class]] internal property.
-        getFunction: Function; // returns the current function
-        getFunctionName: Function; // returns the name of the current function, typically its name property. If a name property is not available an attempt will be made to try to infer a name from the function's context.
-        getMethodName: Function; // returns the name of the property of this or one of its prototypes that holds the current function
-        getFileName: Function; // if this function was defined in a script returns the name of the script
-        getLineNumber: Function; // if this function was defined in a script returns the current line number
-        getColumnNumber: Function; // if this function was defined in a script returns the current column number
-        getEvalOrigin: Function; // if this function was created using a call to eval returns a CallSite object representing the location where eval was called
-        isToplevel: Function; // is this a toplevel invocation, that is, is this the global object?
-        isEval: Function; // does this call take place in code defined by a call to eval?
-        isNative: Function; // is this call in native V8 code?
-        isConstructor: Function; // is this a constructor call?
+        /** Value of this */
+        getThis(): any;
+
+        /** Type of this as a string. This is the name of the function stored in the constructor field of this, if available, otherwise the object's [[Class]] internal property. */
+        getTypeName(): string | null;
+
+        /** Current function */
+        getFunction(): Function | undefined;
+
+        /** Name of the current function, typically its name property. If a name property is not available an attempt will be made to try to infer a name from the function's context. */
+        getFunctionName(): string | null;
+
+        /** Name of the property [of this or one of its prototypes] that holds the current function */
+        getMethodName(): string | null;
+
+        /** Name of the script [if this function was defined in a script] */
+        getFileName(): string | null;
+
+        /** Current line number [if this function was defined in a script] */
+        getLineNumber(): number | null;
+
+        /** Current column number [if this function was defined in a script] */
+        getColumnNumber(): number | null;
+
+        /** A call site object representing the location where eval was called [if this function was created using a call to eval] */
+        getEvalOrigin(): string | undefined;
+
+        /** Is this a toplevel invocation, that is, is this the global object? */
+        isToplevel(): boolean;
+
+        /** Does this call take place in code defined by a call to eval? */
+        isEval(): boolean;
+
+        /** Is this call in native V8 code? */
+        isNative(): boolean;
+
+        /** Is this a constructor call? */
+        isConstructor(): boolean;
     }
 
     export interface ErrnoException extends Error {
