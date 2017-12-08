0155c83bc53d2006a9ca36b02619915f1be791f6
rafaelsouzaf <rafaelsouzaf@gmail.com>
2017-12-08 20:49:42 -0300

    Add request.route.auth.access() method

--- types/hapi/v17/definitions/request/request.d.ts
+++ types/hapi/v17/definitions/request/request.d.ts
@@ -160,14 +160,9 @@ export interface Request extends Podium {
     };
 
     /**
-     * The request route information object, where:
-     * * method - the route HTTP method.
-     * * path - the route path.
-     * * vhost - the route vhost option if configured.
-     * * realm - the active realm associated with the route.
-     * * settings - the route options object with all defaults applied.
-     * * fingerprint - the route internal normalized string representing the normalized path.
+     * The request route information object and method
      * [See docs](https://github.com/hapijs/hapi/blob/master/API.md#-requestroute)
+     * [See docs](https://github.com/hapijs/hapi/blob/master/API.md#-requestrouteauthaccessrequest)
      */
     readonly route: RequestRoute;
 
@@ -208,16 +203,6 @@ export interface Request extends Podium {
      */
     log(tags: string, data?: string | object | (() => string | object)): void;
     log(tags: string[], data?: string | object | (() => string | object)): void;
-
-    /**
-     * Validates a request against the route's authentication access configuration, where:
-     * @param request - the request object.
-     * @return Return value: true if the request would have passed the route's access requirements.
-     * Note that the route's authentication mode and strategies are ignored. The only match is made between the request.auth.credentials scope and entity information and the route access configuration.
-     * If the route uses dynamic scopes, the scopes are constructed against the request.query, request.params, request.payload, and request.auth.credentials which may or may not match between the route and the request's route. If this method is called using a request that has not been authenticated (yet or not at all), it will return false if the route requires any authentication.
-     * [See docs](https://hapijs.com/api/17.0.1#-requestrouteauthaccessrequest)
-     */
-    // TODO how to implement it? It is a function inside route.auth! We have another "route" properti in this file. (The v16 doesn't have this option)
 
     /**
      * Changes the request method before the router begins processing the request where:
