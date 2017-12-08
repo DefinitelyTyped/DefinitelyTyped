9eb9bd632346e169a1e2b334c808a236682efc1d
Tomek Łaziuk <tlaziuk@gmail.com>
2017-12-08 17:17:42 +0100

    improve typings

--- types/koa-passport/index.d.ts
+++ types/koa-passport/index.d.ts
@@ -22,7 +22,7 @@ import * as passport from "passport";
 declare module "koa" {
     interface Context {
         login(user: any): Promise<void>;
-        login(user: any, options: Object): Promise<void>;
+        login(user: any, options: any): Promise<void>;
         logIn: Context["login"];
 
         logout(): void;
@@ -36,7 +36,7 @@ declare module "koa" {
 
 declare namespace KoaPassport {
 
-    interface KoaPassport {
+    class KoaPassport {
         use(strategy: passport.Strategy): this;
         use(name: string, strategy: passport.Strategy): this;
         unuse(name: string): this;
@@ -44,22 +44,19 @@ declare namespace KoaPassport {
         initialize(options?: { userProperty: string; }): Middleware;
         session(options?: { pauseStream: boolean; }): Middleware;
 
-        authenticate(strategy: string, callback?: Function): Middleware;
-        authenticate(strategy: string, options: Object, callback?: Function): Middleware;
-        authenticate(strategies: string[], callback?: Function): Middleware;
-        authenticate(strategies: string[], options: Object, callback?: Function): Middleware;
-        authorize(strategy: string, callback?: Function): Middleware;
-        authorize(strategy: string, options: Object, callback?: Function): Middleware;
-        authorize(strategies: string[], callback?: Function): Middleware;
-        authorize(strategies: string[], options: Object, callback?: Function): Middleware;
-        serializeUser(fn: (user: any, done: (err: any, id: any) => void) => void): void;
-        deserializeUser(fn: (id: any, done: (err: any, user: any) => void) => void): void;
-        transformAuthInfo(fn: (info: any, done: (err: any, info: any) => void) => void): void;
+        authenticate(strategy: string | string[], callback?: (...args: any[]) => any): Middleware;
+        authenticate(strategy: string | string[], options: passport.AuthenticateOptions, callback?: (...args: any[]) => any): Middleware;
+        authorize(strategy: string | string[], callback?: (...args: any[]) => any): Middleware;
+        authorize(strategy: string | string[], options: any, callback?: (...args: any[]) => any): Middleware;
+
+        serializeUser: passport.Passport["serializeUser"];
+        deserializeUser: passport.Passport["deserializeUser"];
+        transformAuthInfo: passport.Passport["transformAuthInfo"];
     }
 
     interface Static extends KoaPassport {
-        KoaPassport: { new(): KoaPassport };
-        Passport: { new(): passport.Passport };
+        KoaPassport: typeof KoaPassport;
+        Passport: typeof passport.Passport;
         Authenticator: Static["Passport"];
     }
 }
