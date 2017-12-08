c85138726596ee201791a9be12908b52d7667d92
segayuu <segayuu@gmail.com>
2017-12-08 11:41:59 +0900

    cleanup ignore lint error: no-var-keyword

--- types/minimatch/minimatch-tests.ts
+++ types/minimatch/minimatch-tests.ts
@@ -1,17 +1,17 @@
 import mm = require("minimatch");
-
-var pattern = "**/*.ts";
-var options = {
+let b: boolean;
+let pattern = "**/*.ts";
+let options = {
 	debug: true
 };
-var m = new mm.Minimatch(pattern, options);
-var r = m.makeRe();
+let m = new mm.Minimatch(pattern, options);
+let r = m.makeRe();
 
-var f = ["test.ts"];
+let f = ["test.ts"];
 mm.match(f, pattern, options);
 
 f.filter(mm.filter(pattern, options));
 
-var s: string = "hello";
-var b: boolean = mm(s, pattern, options);
-var b: boolean = mm(s, pattern);
+let s: string = "hello";
+b = mm(s, pattern, options);
+b = mm(s, pattern);
