b16bb8ce99f1c90405e47b74881b5416dde5ea48
Michal Miszczyszyn <michal@mmiszy.pl>
2017-12-09 12:38:47 +0100

    Add authors

--- types/prismjs/index.d.ts
+++ types/prismjs/index.d.ts
@@ -1,6 +1,8 @@
-// Type definitions for prism 1.6
+// Type definitions for prism 1.9
 // Project: http://prismjs.com/
-// Definitions by: Erik Lieben <https://github.com/eriklieben>, Andre Wiggins <https://github.com/andrewiggins>
+// Definitions by: Erik Lieben <https://github.com/eriklieben>
+//                 Andre Wiggins <https://github.com/andrewiggins>
+//                 Michał Miszczyszyn <https://github.com/mmiszy>
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
 
 export as namespace Prism;
