46ed786ffc3093cb25e94cf8db63d0963cf95989
Sinziana Nicolae <sinziana.nicolae@hootsuite.com>
2017-12-10 20:30:07 +0200

    Fix linting issues

--- types/clamp-js-main/clamp-js-main-tests.ts
+++ types/clamp-js-main/clamp-js-main-tests.ts
@@ -1,4 +1,4 @@
-import clamp, { IClampOptions, IClampResponse } from 'clamp-js-main';
+import clamp, { ClampOptions, ClampResponse } from 'clamp-js-main';
 
 const element: HTMLElement = document.createElement('div');
 element.style.setProperty('width', '5px');
@@ -9,9 +9,9 @@ const span: HTMLSpanElement = document.createElement('span');
 span.textContent = "Lorem ipsum. Lorem ipsum";
 element.appendChild(span);
 
-const simpleClamp: IClampResponse = clamp(element);
+const simpleClamp: ClampResponse = clamp(element);
 
-const options: IClampOptions = {
+const options: ClampOptions = {
   clamp: 2,
   useNativeClamp: false,
   splitOnChars: ['.'],
@@ -19,4 +19,4 @@ const options: IClampOptions = {
   truncationChar: '--',
   truncationHTML: '<span></span>'
 };
-const clampWithOptions: IClampResponse = clamp(element, options);
+const clampWithOptions: ClampResponse = clamp(element, options);
