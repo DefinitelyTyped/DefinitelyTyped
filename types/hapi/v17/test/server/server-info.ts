3d6b18467eae8940f5834142d38f0a2d15817854
rafaelsouzaf <rafaelsouzaf@gmail.com>
2017-12-10 17:08:40 -0300

    Remove asserts. It's not necessary here.

--- types/hapi/v17/test/server/server-info.ts
+++ types/hapi/v17/test/server/server-info.ts
@@ -1,15 +1,10 @@
 //  https://github.com/hapijs/hapi/blob/master/API.md#-serverinfo
 import {Server} from "hapi";
-import * as assert from "assert";
 
 const server = new Server({
     port: 8000,
 });
 server.start();
 
-// check the correct port
 console.log(server.info);
-if (server.info) assert(server.info.port === 8000);
-
-assert(server.info !== null);
 console.log('Server started at: ' + server.info.uri);
