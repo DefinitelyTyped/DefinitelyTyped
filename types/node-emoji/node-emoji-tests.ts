515de0de45aa8a2a077fe3d844111d8a839a2141
Stephanie Yu <stephaniey@palantir.com>
2017-12-10 20:09:07 -0500

    Update node-emoji types to 1.8.1

--- types/node-emoji/node-emoji-tests.ts
+++ types/node-emoji/node-emoji-tests.ts
@@ -1,7 +1,8 @@
+import { IEmoji } from "./index";
 import emoji = require('node-emoji');
 
 const coffee: string = emoji.get('coffee');
-const result: string = emoji.random();
+const result: IEmoji = emoji.random();
 
 const cofee_name: string = emoji.which('☕️');
 
@@ -10,3 +11,15 @@ const emoji_string: string = emoji.emojify('I :heart:  :coffee:! -  :hushed::sta
 const emoji_string2: string = emoji.emojify('I :unknown_emoji: :star: :another_one:', (name: string) => name);
 
 const emoji_direct: string = emoji.emoji.coffee;
+
+const emoji_search: IEmoji[] = emoji.search('cof');
+
+const unemojified_string: string = emoji.unemojify('I ❤️ 🍕');
+
+const found_emoji: IEmoji = emoji.find('🍕');
+
+const hasEmoji: boolean = emoji.hasEmoji('🍕');
+
+const stripped_emoji: string = emoji.strip('⚠️ 〰️ 〰️ low disk space');
+
+const replaced_emoji: string = emoji.replace('⚠️ 〰️ 〰️ low disk space', (emoji) => `${emoji.key}:`);
