ef68bf75d342b9d36da1c594be880964db23131a
yhpnorra <aaronphy@outlook.com>
2017-12-11 16:22:42 +0800

    update the ts versin

--- types/mirrorx/mirrorx-tests.ts
+++ types/mirrorx/mirrorx-tests.ts
@@ -2,21 +2,17 @@ import { Component, ReactElement } from 'react';
 import * as React from 'react';
 import * as ReactDOM from 'react-dom';
 import * as mirror from 'mirrorx';
-import { render,actions,withRouter,Link,NavLink,Router,Route,connect,Redirect} from 'mirrorx';
 /**
  * add mirror.model.tests
  * 
  */
 mirror.model({
     name:'app',
-
     initialState: 0,
-    
     reducers:{
         increment(state:any) { return state + 1 },
         decrement(state:any) { return state - 1 }
     },
-
     effects:{
         async incrementAsync() {
             await new Promise((resolve, reject) => {
