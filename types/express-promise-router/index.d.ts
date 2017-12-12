03f79837756e985d12e56b60879adffc0ad14156
Anjun Wang <test@example.com>
2017-12-12 17:59:24 +0800

    specify typescript version

--- types/express-promise-router/index.d.ts
+++ types/express-promise-router/index.d.ts
@@ -2,6 +2,7 @@
 // Project: https://github.com/express-promise-router/express-promise-router
 // Definitions by: Anjun Wang <https://github.com/wanganjun>
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
+// TypeScript Version: 2.3
 
 /* =================== USAGE ===================
 import Router = require('express-promise-router');
