57b350b4f99afc6972e06de1a037505cbdc34b68
rollymaduk <rolly.maduk@gmail.com>
2017-12-13 15:56:49 +0100

    Added name to defintions by section

--- types/pubnub/index.d.ts
+++ types/pubnub/index.d.ts
@@ -1,6 +1,7 @@
 // Type definitions for pubnub 4.0
 // Project: https://github.com/pubnub/javascript
 // Definitions by: bitbankinc <https://github.com/bitbankinc>
+//                   rollymaduk <https://github.com/rollymaduk>
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
 // @see https://www.pubnub.com/docs/web-javascript/api-reference-configuration
 
