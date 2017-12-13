eb219595ed06a99a0500ee4f0e947761fe20fef7
endigo <endigo_18@yahoo.com>
2017-12-14 00:33:42 +0800

    FIX: MAJOR.MINOR

--- types/react-alice-carousel/index.d.ts
+++ types/react-alice-carousel/index.d.ts
@@ -1,4 +1,4 @@
-// Type definitions for react-alice-carousel 1.7.1
+// Type definitions for react-alice-carousel 1.7
 // Project: https://github.com/maxmarinich/react-alice-carousel
 // Definitions by: endigo <https://github.com/endigo>
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
