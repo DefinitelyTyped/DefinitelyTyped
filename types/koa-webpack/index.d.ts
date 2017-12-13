354e7b0785b1e74c6bc4a5c4c794ec5389ce8022
Luka Maljic <luka.maljic@gmail.com>
2017-12-13 10:07:05 +0100

    Added types for koa-webpack

---
+++ types/koa-webpack/index.d.ts
@@ -0,0 +1,33 @@
+// Type definitions for koa-webpack 1.0
+// Project: https://github.com/shellscape/koa-webpack#readme
+// Definitions by: Luka Maljic <https://github.com/malj>
+// Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
+
+import * as Koa from 'koa'
+import * as webpack from 'webpack'
+import * as webpackDevMiddleware from 'webpack-dev-middleware'
+import * as webpackHotMiddleware from 'webpack-hot-middleware'
+import { NextHandleFunction } from 'connect'
+
+declare function koaWebpack(
+    options?: koaWebpack.Options
+): Koa.Middleware & koaWebpack.CombinedWebpackMiddleware
+
+declare namespace koaWebpack
+{
+    interface Options
+    {
+        compiler?: webpack.Compiler
+        config?: webpack.Configuration
+        dev?: webpackDevMiddleware.Options
+        hot?: webpackHotMiddleware.Options
+    }
+
+    interface CombinedWebpackMiddleware
+    {
+        dev: NextHandleFunction & webpackDevMiddleware.WebpackDevMiddleware
+        hot: NextHandleFunction & webpackHotMiddleware.EventStream
+    }
+}
+
+export = koaWebpack
