ee3025252a32bba2a0c09872b70b414368810d99
Jim Smart <jim@jimsmart.org>
2017-12-13 21:18:14 +0000

    Added my details to 'Definitions by' section

--- types/math3d/index.d.ts
+++ types/math3d/index.d.ts
@@ -1,6 +1,7 @@
 // Type definitions for math3d 0.2
 // Project: https://github.com/adragonite/math3d
 // Definitions by: Laszlo Jakab <https://github.com/laszlojakab>
+//                 Jim Smart <https://github.com/jimsmart>
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
 
 export class Vector3 {
