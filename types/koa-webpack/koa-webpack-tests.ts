a6711f167e85e2f39f307658b3ebafc74610ffe3
Luka Maljic <luka.maljic@gmail.com>
2017-12-13 10:13:46 +0100

    Fixed dtslint warnings

--- types/koa-webpack/koa-webpack-tests.ts
+++ types/koa-webpack/koa-webpack-tests.ts
@@ -1,11 +1,10 @@
-import * as Koa from 'koa'
-import * as webpack from 'webpack'
-import * as koaWebpack from 'koa-webpack'
+import * as Koa from 'koa';
+import * as webpack from 'webpack';
+import * as koaWebpack from 'koa-webpack';
 
-const app = new Koa()
-const config: webpack.Configuration = {}
-const compiler = webpack(config)
-
+const app = new Koa();
+const config: webpack.Configuration = {};
+const compiler = webpack(config);
 
 // Using the middleware
 
@@ -36,15 +35,13 @@ const middleware = koaWebpack({
         path: '/__what',
         heartbeat: 2000
     }
-})
+});
 
-app.use(middleware)
-
+app.use(middleware);
 
 // Accessing the underlying middleware
 
-middleware.dev.close()
-middleware.dev.invalidate()
-middleware.dev.waitUntilValid()
-middleware.hot.publish(null)
-
+middleware.dev.close();
+middleware.dev.invalidate();
+middleware.dev.waitUntilValid();
+middleware.hot.publish(null);
