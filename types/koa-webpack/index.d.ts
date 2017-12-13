329c092b7e3abde88ded597e8fba968c6d3cc312
Luka Maljic <luka.maljic@gmail.com>
2017-12-13 10:13:46 +0100

    Fixed dtslint warnings

--- types/koa-webpack/index.d.ts
+++ types/koa-webpack/index.d.ts
@@ -3,31 +3,28 @@
 // Definitions by: Luka Maljic <https://github.com/malj>
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
 
-import * as Koa from 'koa'
-import * as webpack from 'webpack'
-import * as webpackDevMiddleware from 'webpack-dev-middleware'
-import * as webpackHotMiddleware from 'webpack-hot-middleware'
-import { NextHandleFunction } from 'connect'
+import * as Koa from 'koa';
+import * as webpack from 'webpack';
+import * as webpackDevMiddleware from 'webpack-dev-middleware';
+import * as webpackHotMiddleware from 'webpack-hot-middleware';
+import { NextHandleFunction } from 'connect';
 
 declare function koaWebpack(
     options?: koaWebpack.Options
-): Koa.Middleware & koaWebpack.CombinedWebpackMiddleware
+): Koa.Middleware & koaWebpack.CombinedWebpackMiddleware;
 
-declare namespace koaWebpack
-{
-    interface Options
-    {
-        compiler?: webpack.Compiler
-        config?: webpack.Configuration
-        dev?: webpackDevMiddleware.Options
-        hot?: webpackHotMiddleware.Options
+declare namespace koaWebpack {
+    interface Options {
+        compiler?: webpack.Compiler;
+        config?: webpack.Configuration;
+        dev?: webpackDevMiddleware.Options;
+        hot?: webpackHotMiddleware.Options;
     }
 
-    interface CombinedWebpackMiddleware
-    {
-        dev: NextHandleFunction & webpackDevMiddleware.WebpackDevMiddleware
-        hot: NextHandleFunction & webpackHotMiddleware.EventStream
+    interface CombinedWebpackMiddleware {
+        dev: NextHandleFunction & webpackDevMiddleware.WebpackDevMiddleware;
+        hot: NextHandleFunction & webpackHotMiddleware.EventStream;
     }
 }
 
-export = koaWebpack
+export = koaWebpack;
