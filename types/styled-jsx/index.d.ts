c62202ce0ff5a9dda40588dfba9a69cbee14f7b7
Andrei Volchenko <volchenko.andrei@gmail.com>
2017-12-14 14:28:43 +0300

    add types for styled-jsx

---
+++ types/styled-jsx/index.d.ts
@@ -0,0 +1,13 @@
+// Type definitions for styled-jsx 2.2.1
+// Project: https://github.com/zeit/styled-jsx
+// Definitions by: R1ZZU <https://github.com/R1ZZU>
+// Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
+
+import * as React from 'react';
+
+declare module 'react' {
+    interface StyleHTMLAttributes<T> extends React.HTMLAttributes<T> {
+        jsx?: boolean;
+        global?: boolean;
+    }
+}
