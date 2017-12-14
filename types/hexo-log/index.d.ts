087c926f050fa3ba6308ccca0dc5d57f1403bf3d
Andy Hanson <anhans@microsoft.com>
2017-12-14 13:12:28 -0800

    Remove `// tslint:disable` comments

--- types/hexo-log/index.d.ts
+++ types/hexo-log/index.d.ts
@@ -8,8 +8,6 @@ import bunyan = require('hexo-bunyan');
 import streams = require('stream');
 
 declare class HexoLogger extends bunyan {
-    /* tslint:disable:unified-signatures */
-
     /**
      * Returns a boolean: is the `debug` level enabled?
      *
@@ -154,8 +152,6 @@ declare class HexoLogger extends bunyan {
      * Uses `util.format` for msg formatting.
      */
     log(format: any, ...params: any[]): void;
-
-    /* tslint:enable:unified-signatures */
 }
 
 declare function createLogger(options?: { name?: string; silent?: boolean; debug?: boolean; }): HexoLogger;
