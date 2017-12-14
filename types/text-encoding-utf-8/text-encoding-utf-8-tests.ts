2cad227300f19c2f333a67c5ed6a10cf9d22fc51
Paul Taylor <paul.e.taylor@me.com>
2017-12-13 20:35:40 -0800

    fix lint

--- types/text-encoding-utf-8/text-encoding-utf-8-tests.ts
+++ types/text-encoding-utf-8/text-encoding-utf-8-tests.ts
@@ -1,8 +1,6 @@
-
-
 function test_encoder() {
-    var text = "plain text";
-    var uint8array: Uint8Array;
+    const text = "plain text";
+    let uint8array: Uint8Array;
 
     // constructor
     uint8array = new TextEncoder().encode(text);
@@ -12,9 +10,9 @@ function test_encoder() {
     uint8array = TextEncoder('utf-8').encode(text);
 
     // attributes
-    var encoder = new TextEncoder();
+    const encoder = new TextEncoder();
     encoder.encoding = 'utf-8';
-    var encoding: string = encoder.encoding;
+    const encoding: string = encoder.encoding;
 
     // methods
     encoder.encode();
@@ -22,9 +20,9 @@ function test_encoder() {
 }
 
 function test_decoder() {
-    var text = "plain text";
-    var uint8array: Uint8Array = TextEncoder().encode(text);
-    var arrayBuffer: ArrayBuffer = uint8array.buffer
+    let text = "plain text";
+    const uint8array: Uint8Array = TextEncoder().encode(text);
+    const arrayBuffer: ArrayBuffer = uint8array.buffer;
 
     // constructor
     text = new TextDecoder().decode(uint8array);
@@ -34,16 +32,16 @@ function test_decoder() {
     text = TextDecoder('utf-8').decode(uint8array);
 
     // attributes
-    var decoder = new TextDecoder();
+    const decoder = new TextDecoder();
 
     decoder.encoding = 'utf-8';
-    var encoding: string = decoder.encoding;
+    const encoding: string = decoder.encoding;
 
     decoder.fatal = true;
-    var fatal: boolean = decoder.fatal;
+    const fatal: boolean = decoder.fatal;
 
     decoder.ignoreBOM = true;
-    var ignoreBOM: boolean = decoder.ignoreBOM;
+    const ignoreBOM: boolean = decoder.ignoreBOM;
 
     // methods
     decoder.decode();
