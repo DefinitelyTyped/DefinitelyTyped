3e29b6e832466a72e5fc91a34ca405294e9acfa8
rafaelsouzaf <rafaelsouzaf@gmail.com>
2017-12-13 22:32:26 -0300

    Move v17 definitions and tests to "types/hapi" root folder and delete v17 temp folder.

--- types/hapi/v17/test/route/handler.ts
+++
@@ -1,9 +0,0 @@
-import {Lifecycle, Request, ResponseToolkit} from "hapi";
-
-const handler: Lifecycle.Method = (request: Request, h: ResponseToolkit) => {
-    return 'success';
-}
-
-const strictHandler: Lifecycle.Method = (request: Request, h: ResponseToolkit) => {
-    return 123;
-}
