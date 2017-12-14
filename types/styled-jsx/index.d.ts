ca2179bf66a419d1e552a8e76768bf4750b371d8
Andrei Volchenko <volchenko.andrei@gmail.com>
2017-12-14 14:36:08 +0300

    specify ts version

--- types/styled-jsx/index.d.ts
+++ types/styled-jsx/index.d.ts
@@ -2,10 +2,11 @@
 // Project: https://github.com/zeit/styled-jsx
 // Definitions by: R1ZZU <https://github.com/R1ZZU>
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
+// TypeScript Version: 2.3
 
-import * as React from 'react';
+import * as React from "react";
 
-declare module 'react' {
+declare module "react" {
     interface StyleHTMLAttributes<T> extends React.HTMLAttributes<T> {
         jsx?: boolean;
         global?: boolean;
