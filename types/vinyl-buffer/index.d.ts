4055ef4f0cc483eaee55b903ca2a2ed9a915d79a
segayuu <segayuu@gmail.com>
2017-12-15 11:33:32 +0900

    cleanup ignore lint error: dt-header

--- types/vinyl-buffer/index.d.ts
+++ types/vinyl-buffer/index.d.ts
@@ -1,4 +1,4 @@
-// Type definitions for vinyl-buffer
+// Type definitions for vinyl-buffer 1.0
 // Project: https://github.com/hughsk/vinyl-buffer
 // Definitions by: Qubo <https://github.com/tkQubo>
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
