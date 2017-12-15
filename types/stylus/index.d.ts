260fc4a0dc0ff7f380d8d600cf3dcd7d85ae9e72
segayuu <segayuu@gmail.com>
2017-12-15 12:01:52 +0900

    cleanup lint ignore error: dt-header

--- types/stylus/index.d.ts
+++ types/stylus/index.d.ts
@@ -1,4 +1,4 @@
-// Type definitions for stylus 0.48.1
+// Type definitions for stylus 0.48
 // Project: https://github.com/LearnBoost/stylus
 // Definitions by: Maxime LUCE <https://github.com/SomaticIT>
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
