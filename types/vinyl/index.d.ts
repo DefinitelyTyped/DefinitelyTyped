24178125b1f830d2383e79e5ad297d514f6d4030
segayuu <segayuu@gmail.com>
2017-12-15 10:26:43 +0900

    cleanup ignore lint error: dt-header

--- types/vinyl/index.d.ts
+++ types/vinyl/index.d.ts
@@ -1,4 +1,4 @@
-// Type definitions for vinyl 2.0.0
+// Type definitions for vinyl 2.0
 // Project: https://github.com/gulpjs/vinyl
 // Definitions by: vvakame <https://github.com/vvakame>, jedmao <https://github.com/jedmao>, Georgii Dolzhykov <https://github.com/thorn0>
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
