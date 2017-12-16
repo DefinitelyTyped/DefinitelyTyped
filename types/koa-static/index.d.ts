560dd3c5f7701d98bd87344b547fe8b27ce29e20
Ben Stevens <ben@nebstone.co.uk>
2017-12-16 11:34:47 +0000

    koa-static add set headers

--- types/koa-static/index.d.ts
+++ types/koa-static/index.d.ts
@@ -1,8 +1,8 @@
-// Type definitions for koa-static 3.0
+// Type definitions for koa-static 4.0
 // Project: https://github.com/koajs/static
 // Definitions by: Jerry Chin <https://github.com/hellopao>
 // Definitions: https://github.com/hellopao/DefinitelyTyped
-// TypeScript Version: 2.3
+// TypeScript Version: 2.4
 
 /* =================== USAGE ===================
 
@@ -15,37 +15,48 @@
  =============================================== */
 
 import * as Koa from "koa";
+import {Context} from "koa";
+import {Stats} from "fs";
 
-declare function serve(root: string, opts?: {
-    /**
-     * Default file name, defaults to 'index.html'
-     */
-    index?: boolean | string;
+declare function serve(root: string, opts?: serve.ServeOptions): Koa.Middleware;
+declare namespace serve {
+    type SetHeaders = (res: Context["res"], path: string, stats: Stats) => any;
 
-    /**
-     * If true, serves after return next(),allowing any downstream middleware to respond first.
-     */
-    defer?: boolean;
-
-    /**
-     * Browser cache max-age in milliseconds. defaults to 0
-     */
-    maxage?: number;
-
-    /**
-     * Allow transfer of hidden files. defaults to false
-     */
-    hidden?: boolean;
-
-    /**
-     * Try to serve the gzipped version of a file automatically when gzip is supported by a client and if the requested file with .gz extension exists. defaults to true.
-     */
-    gzip?: boolean;
-
-    /**
-     * Try to match extensions from passed array to search for file when no extension is sufficed in URL. First found is served. (defaults to `false`)
-     */
-    extensions?: string[];
-}): Koa.Middleware;
-declare namespace serve {}
+    interface ServeOptions {
+        /**
+         * Default file name, defaults to 'index.html'
+         */
+        index?: boolean | string;
+    
+        /**
+         * If true, serves after return next(),allowing any downstream middleware to respond first.
+         */
+        defer?: boolean;
+    
+        /**
+         * Browser cache max-age in milliseconds. defaults to 0
+         */
+        maxage?: number;
+    
+        /**
+         * Allow transfer of hidden files. defaults to false
+         */
+        hidden?: boolean;
+    
+        /**
+         * Try to serve the gzipped version of a file automatically when gzip is supported by a client and if the requested file with .gz extension exists. defaults to true.
+         */
+        gzip?: boolean;
+    
+        /**
+         * Try to match extensions from passed array to search for file when no extension is sufficed in URL. First found is served. (defaults to `false`)
+         */
+        extensions?: string[];
+    
+        /**
+         * Function to set custom headers on response.
+         */
+        setHeaders?: SetHeaders;
+    }
+}
 export = serve;
