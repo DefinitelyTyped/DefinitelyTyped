37b941a4d0580e489547ac70e20575a32982da69
rafaelsouzaf <rafaelsouzaf@gmail.com>
2017-12-18 19:50:55 -0300

    Remove the module declaration, this is implicit in DT.

--- types/iron/index.d.ts
+++ types/iron/index.d.ts
@@ -7,97 +7,105 @@
 
 /// <reference types="node" />
 
-/**
- * iron is a cryptographic utility for sealing a JSON object using symmetric key encryption with message integrity
- * verification. Or in other words, it lets you encrypt an object, send it around (in cookies, authentication
- * credentials, etc.), then receive it back and decrypt it. The algorithm ensures that the message was not tampered
- * with, and also provides a simple mechanism for password rotation.
- * [See docs](https://github.com/hueniverse/iron)
- */
-declare module 'iron' {
+export interface ISomething {
 
     /**
-     * saltBits - the size of the salt (random buffer used to ensure that two identical objects will generate a different encrypted result.
-     * algorithm - the algorithm used ('aes-256-cbc' for encryption and 'sha256' for integrity are the only two supported at this time).
-     * iterations - the number of iterations used to derive a key from the password. Set to 1 by default. The number of ideal iterations to use is dependent on your application's performance requirements. More iterations means it takes longer to generate the key.
-     * minPasswordlength - minimum password size
+     * the size of the salt (random buffer used to ensure that two identical objects will generate a different encrypted result.
      */
-    export interface ISomething {
-        saltBits: number;
-        algorithm: string;
-        iterations: number;
-        minPasswordlength: number;
-    }
+    saltBits: number;
 
     /**
-     * iron provides a few options for customizing the key deriviation algorithm used to generate encryption and
-     * integrity verification keys as well as the algorithms and salt sizes used. The 'seal()' and 'unseal()'
-     * methods take an options object with the following required keys:
-     * * encryption - defines the options used by the encryption process.
-     * * integrity - defines the options used by the HMAC integrity verification process.
-     * Each of these option objects includes the following required keys:
-     * * saltBits - the size of the salt (random buffer used to ensure that two identical objects will generate a different encrypted result.
-     * * algorithm - the algorithm used ('aes-256-cbc' for encryption and 'sha256' for integrity are the only two supported at this time).
-     * * iterations - the number of iterations used to derive a key from the password. Set to 1 by default. The number of ideal iterations to use is dependent on your application's performance requirements. More iterations means it takes longer to generate the key.
-     * * minPasswordlength - minimum password size
-     * The 'seal()' and 'unseal()' methods also take the following optional options keys:
-     * * ttl - sealed object lifetime in milliseconds where 0 means forever. Defaults to 0.
-     * * timestampSkewSec - number of seconds of permitted clock skew for incoming expirations. Defaults to 60 seconds.
-     * * localtimeOffsetMsec - local clock time offset, expressed in number of milliseconds (positive or negative). Defaults to 0.
-     * [See docs](https://github.com/hueniverse/iron#options)
+     * the algorithm used ('aes-256-cbc' for encryption and 'sha256' for integrity are the only two supported at this time).
      */
-    export interface ISealOptions {
-        encryption: ISomething;
-        integrity: ISomething;
-        ttl: number;
-        timestampSkewSec: number;
-        localtimeOffsetMsec: number;
-    }
+    algorithm: string;
 
-    export interface IAlgorithms {
-        'aes-128-ctr': {
-            keyBits: number;
-            ivBits: number;
-        };
-        'aes-256-cbc': {
-            keyBits: number;
-            ivBits: number;
-        };
-        'sha256': {
-            keyBits: number;
-        };
-    }
+    /**
+     * the number of iterations used to derive a key from the password. Set to 1 by default. The number of ideal iterations to use is dependent on your application's performance requirements. More iterations means it takes longer to generate the key.
+     */
+    iterations: number;
 
-    export const defaults: ISealOptions;
-    export const algorithms: IAlgorithms;
-    export const macFormatVersion: string;
-    export const macPrefix: string;
-
-    export interface IGenerateKeyOptions extends Pick<ISomething, 'algorithm' | 'iterations' | 'minPasswordlength'> {
-        saltBits?: number;
-        salt?: string;
-        iv?: string;
-    }
-
-    export interface IKey {
-        key: Buffer;
-        salt: string;
-        iv: string;
-    }
-
-    export function generateKey (password: string, options: IGenerateKeyOptions): Promise<IKey>;
-    export function encrypt (password: string, options: IGenerateKeyOptions, data: string): Promise<{ data: Buffer, key: IKey }>;
-    export function decrypt (password: string, options: IGenerateKeyOptions, data: string): Promise<Buffer>;
-
-    export interface IHMacResult {
-        digest: string;
-        salt: string;
-    }
-
-    export function hmacWithPassword(password: string, options: IGenerateKeyOptions, data: string): Promise<IHMacResult>;
-
-    export function seal (obj: object, password: string, options: ISealOptions): Promise<string>;
-    export function unseal (data: string, password: string, options: ISealOptions): Promise<object>;
+    /**
+     * minimum password size
+     */
+    minPasswordlength: number;
 
 }
 
+/**
+ * iron provides a few options for customizing the key derivation algorithm used to generate encryption and
+ * integrity verification keys as well as the algorithms and salt sizes used.
+ * For context [See docs](https://github.com/hueniverse/iron#options)
+ */
+export interface ISealOptions {
+
+    /**
+     * defines the options used by the encryption process.
+     */
+    encryption: ISomething;
+
+    /**
+     * defines the options used by the HMAC integrity verification process.
+     */
+    integrity: ISomething;
+
+    /**
+     * sealed object lifetime in milliseconds where 0 means forever. Defaults to 0.
+     */
+    ttl: number;
+
+    /**
+     * number of seconds of permitted clock skew for incoming expirations. Defaults to 60 seconds.
+     */
+    timestampSkewSec: number;
+
+    /**
+     * local clock time offset, expressed in number of milliseconds (positive or negative). Defaults to 0.
+     */
+    localtimeOffsetMsec: number;
+
+}
+
+export interface IAlgorithms {
+    'aes-128-ctr': {
+        keyBits: number;
+        ivBits: number;
+    };
+    'aes-256-cbc': {
+        keyBits: number;
+        ivBits: number;
+    };
+    'sha256': {
+        keyBits: number;
+    };
+}
+
+export const defaults: ISealOptions;
+export const algorithms: IAlgorithms;
+export const macFormatVersion: string;
+export const macPrefix: string;
+
+export interface IGenerateKeyOptions extends Pick<ISomething, 'algorithm' | 'iterations' | 'minPasswordlength'> {
+    saltBits?: number;
+    salt?: string;
+    iv?: string;
+}
+
+export interface IKey {
+    key: Buffer;
+    salt: string;
+    iv: string;
+}
+
+export function generateKey (password: string, options: IGenerateKeyOptions): Promise<IKey>;
+export function encrypt (password: string, options: IGenerateKeyOptions, data: string): Promise<{ data: Buffer, key: IKey }>;
+export function decrypt (password: string, options: IGenerateKeyOptions, data: string): Promise<Buffer>;
+
+export interface IHMacResult {
+    digest: string;
+    salt: string;
+}
+
+export function hmacWithPassword(password: string, options: IGenerateKeyOptions, data: string): Promise<IHMacResult>;
+
+export function seal (obj: object, password: string, options: ISealOptions): Promise<string>;
+export function unseal (data: string, password: string, options: ISealOptions): Promise<object>;
