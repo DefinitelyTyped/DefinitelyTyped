6d3e5f5594aec63edeff2ce2ed604188567d4e37
Andy Hanson <anhans@microsoft.com>
2017-12-19 11:18:29 -0800

    Remove angular-ui-router and ui-router-extras, which have been replaced by packages that provide their own types

--- types/angular-breadcrumb/index.d.ts
+++ types/angular-breadcrumb/index.d.ts
@@ -4,8 +4,6 @@
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
 // TypeScript Version: 2.3
 
-/// <reference types="angular-ui-router" />
-
 import * as angular from 'angular';
 
 declare var _: string;
