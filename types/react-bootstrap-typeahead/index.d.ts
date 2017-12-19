06fa62213c6a571b66f35f783211cbe3cb665d96
Stéphane S <stephane.saquet@gmail.com>
2017-12-19 19:09:17 +0100

    Set min TypeScript Version to 2.3

--- types/react-bootstrap-typeahead/index.d.ts
+++ types/react-bootstrap-typeahead/index.d.ts
@@ -2,6 +2,7 @@
 // Project: https://github.com/ericgio/react-bootstrap-typeahead
 // Definitions by: My Self <https://github.com/Guymestef>
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
+// TypeScript Version: 2.3
 
 import * as React from 'react';
 
