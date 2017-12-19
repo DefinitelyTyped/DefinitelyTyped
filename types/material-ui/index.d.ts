59141deed18c70bc1d4928300a2ade53f320a84d
devuxer <danspamable@gmail.com>
2017-12-18 17:41:57 -0800

    Updated material-ui version in header

--- types/material-ui/index.d.ts
+++ types/material-ui/index.d.ts
@@ -1,4 +1,4 @@
-// Type definitions for material-ui v0.18.17
+// Type definitions for material-ui v0.20.0
 // Project: https://github.com/callemall/material-ui
 // Definitions by: Nathan Brown <https://github.com/ngbrown>
 //                 Igor Beagorudsky <https://github.com/theigor>
