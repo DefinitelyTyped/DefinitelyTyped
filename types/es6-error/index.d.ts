767a8a946792f276a7d4c2ed196a6e73e2cee103
Andy Hanson <anhans@microsoft.com>
2017-12-19 13:15:33 -0800

    es6-error: Provides its own types

--- types/es6-error/index.d.ts
+++
@@ -1,6 +0,0 @@
-// Type definitions for es6-error v4.0.0
-// Project: https://github.com/bjyoungblood/es6-error
-// Definitions by: Karol Janyst <https://github.com/LKay>
-// Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
-
-export = Error;
