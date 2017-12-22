f91e43df7a52d214a1f93303010b3596b168a44e
rafaelsouzaf <rafaelsouzaf@gmail.com>
2017-12-21 23:24:44 -0300

    Hapi v17 setup.

---
+++ types/hapi/definitions/route/route-options-pre.d.ts
@@ -0,0 +1,35 @@
+import {Lifecycle} from "hapi";
+
+/**
+ * For context [See docs](https://github.com/hapijs/hapi/blob/master/API.md#-routeoptionspre)
+ */
+export type RouteOptionsPreArray = RouteOptionsPreAllOptions[];
+
+/**
+ * For context [See docs](https://github.com/hapijs/hapi/blob/master/API.md#-routeoptionspre)
+ */
+export type RouteOptionsPreAllOptions = RouteOptionsPreObject | RouteOptionsPreObject[] | Lifecycle.Method;
+
+/**
+ * An object with:
+ * * method - a lifecycle method.
+ * * assign - key name used to assign the response of the method to in request.pre and request.preResponses.
+ * * failAction - A failAction value which determine what to do when a pre-handler method throws an error. If assign is specified and the failAction setting is not 'error', the error will be assigned.
+ * For context [See docs](https://github.com/hapijs/hapi/blob/master/API.md#-routeoptionspre)
+ */
+export interface RouteOptionsPreObject {
+    /**
+     * a lifecycle method.
+     */
+    method: Lifecycle.Method;
+    /**
+     * key name used to assign the response of the method to in request.pre and request.preResponses.
+     */
+    assign: string;
+    /**
+     * A failAction value which determine what to do when a pre-handler method throws an error. If assign is specified and the failAction setting is not 'error', the error will be assigned.
+     */
+    failAction?: Lifecycle.FailAction;
+}
+
+
---
+++ types/hapi/test/server/server-plugins.ts
@@ -0,0 +1,19 @@
+// https://github.com/hapijs/hapi/blob/master/API.md#-serverplugins
+import { Plugin, Server, ServerRegisterOptions } from "hapi";
+
+const plugin: Plugin = {
+    name: 'example',
+    register: (server: Server, options: ServerRegisterOptions) => {
+        server.expose('key', 'value');
+        server.plugins.example.other = 'other';
+        console.log(server.plugins.example.key);      // 'value'
+        console.log(server.plugins.example.other);    // 'other'
+    }
+};
+
+const server = new Server({
+    port: 8000,
+});
+
+server.start();
+server.register(plugin);
