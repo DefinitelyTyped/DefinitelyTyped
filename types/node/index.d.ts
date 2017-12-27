f9d91dd159f555c76ef569edaa4157d5bb2a65bc
Olaf Tomalka <olaf@tomalka.me>
2017-12-27 14:22:01 +0100

    Revert the decision to use TS 2.1 in @types/node
    Updating Typescript version would require to update
    all other types that rely on Node, which may be quite a lot.
    Fixing a bug is not a big enough reason to add new version to all
    of those packages.

--- types/node/index.d.ts
+++ types/node/index.d.ts
@@ -18,7 +18,6 @@
 //                 Hannes Magnusson <https://github.com/Hannes-Magnusson-CK>
 //                 Alberto Schiabel <https://github.com/jkomyno>
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
-// TypeScript Version: 2.1
 
 /************************************************
 *                                               *
@@ -4574,6 +4573,28 @@ declare module "path" {
          */
         name: string;
     }
+    export interface FormatInputPathObject {
+        /**
+         * The root of the path such as '/' or 'c:\'
+         */
+        root?: string;
+        /**
+         * The full directory path such as '/home/user/dir' or 'c:\path\dir'
+         */
+        dir?: string;
+        /**
+         * The file name including extension (if any) such as 'index.html'
+         */
+        base?: string;
+        /**
+         * The file extension (if any) such as '.html'
+         */
+        ext?: string;
+        /**
+         * The file name without extension (if any) such as 'index'
+         */
+        name?: string;
+    }
 
     /**
      * Normalize a string path, reducing '..' and '.' parts.
@@ -4650,7 +4671,7 @@ declare module "path" {
      *
      * @param pathString path to evaluate.
      */
-    export function format(pathObject: Partial<ParsedPath>): string;
+    export function format(pathObject: FormatInputPathObject): string;
 
     export module posix {
         export function normalize(p: string): string;
@@ -4664,7 +4685,7 @@ declare module "path" {
         export var sep: string;
         export var delimiter: string;
         export function parse(p: string): ParsedPath;
-        export function format(pP: Partial<ParsedPath>): string;
+        export function format(pP: FormatInputPathObject): string;
     }
 
     export module win32 {
@@ -4679,7 +4700,7 @@ declare module "path" {
         export var sep: string;
         export var delimiter: string;
         export function parse(p: string): ParsedPath;
-        export function format(pP: Partial<ParsedPath>): string;
+        export function format(pP: FormatInputPathObject): string;
     }
 }
 
--- types/node/v4/index.d.ts
+++ types/node/v4/index.d.ts
@@ -4,7 +4,6 @@
 //                 DefinitelyTyped <https://github.com/DefinitelyTyped/DefinitelyTyped>
 //                 Sebastian Silbermann <https://github.com/eps1lon>
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
-// TypeScript Version: 2.1
 
 /************************************************
 *                                               *
@@ -1994,6 +1993,28 @@ declare module "path" {
          */
         name: string;
     }
+    export interface FormatInputPathObject {
+        /**
+         * The root of the path such as '/' or 'c:\'
+         */
+        root?: string;
+        /**
+         * The full directory path such as '/home/user/dir' or 'c:\path\dir'
+         */
+        dir?: string;
+        /**
+         * The file name including extension (if any) such as 'index.html'
+         */
+        base?: string;
+        /**
+         * The file extension (if any) such as '.html'
+         */
+        ext?: string;
+        /**
+         * The file name without extension (if any) such as 'index'
+         */
+        name?: string;
+    }
 
     /**
      * Normalize a string path, reducing '..' and '.' parts.
@@ -2077,7 +2098,7 @@ declare module "path" {
      *
      * @param pathString path to evaluate.
      */
-    export function format(pathObject: Partial<ParsedPath>): string;
+    export function format(pathObject: FormatInputPathObject): string;
 
     export module posix {
       export function normalize(p: string): string;
@@ -2091,7 +2112,7 @@ declare module "path" {
       export var sep: string;
       export var delimiter: string;
       export function parse(p: string): ParsedPath;
-      export function format(pP: Partial<ParsedPath>): string;
+      export function format(pP: FormatInputPathObject): string;
     }
 
     export module win32 {
@@ -2106,7 +2127,7 @@ declare module "path" {
       export var sep: string;
       export var delimiter: string;
       export function parse(p: string): ParsedPath;
-      export function format(pP: Partial<ParsedPath>): string;
+      export function format(pP: FormatInputPathObject): string;
     }
 }
 
