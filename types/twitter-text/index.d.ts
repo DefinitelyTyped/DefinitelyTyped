953b0293c50764d9c6cbf287dd13e7265f7decd2
Matt Lewis <mattlewis92@users.noreply.github.com>
2017-12-27 21:45:33 +0000

    Update from review

--- types/twitter-text/index.d.ts
+++ types/twitter-text/index.d.ts
@@ -1,6 +1,6 @@
 // Type definitions for twitter-text v2.0.0
 // Project: https://github.com/twitter/twitter-text
-// Definitions by: rhysd <https://rhysd.github.io>
+// Definitions by: rhysd <https://github.com/rhysd>
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
 
 
