946851996010cf4142611a390e6ca39eea04ca08
Timur Manyanov <gtibalt@gmail.com>
2017-12-27 17:04:30 +0100

    Add more consistency with the real code

--- types/lasso/middleware.d.ts
+++
@@ -1,9 +0,0 @@
-import { RequestHandler } from 'express';
-import Lasso from './lib/Lasso';
-
-export interface MiddlewareOptions {
-  lasso?: Lasso;
-  sendOptions?: any;
-}
-
-export function serveStatic(options?: MiddlewareOptions): RequestHandler;
