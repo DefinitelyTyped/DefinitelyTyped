6325ab5d1763164426b1b98d7664adbeb6eb1b48
Haroen Viaene <hello@haroen.me>
2017-12-28 15:25:28 +0100

    fix(algolia): batch has multiple "action"s

--- types/algoliasearch/index.d.ts
+++ types/algoliasearch/index.d.ts
@@ -192,14 +192,14 @@ declare namespace algoliasearch {
      * @param cb(err, res)
      * https://github.com/algolia/algoliasearch-client-js#custom-batch---batch
      */
-    batch(action: AlgoliaAction, cb: (err: Error, res: any) => void): void;
+    batch(action: AlgoliaAction[], cb: (err: Error, res: any) => void): void;
     /**
      * Perform multiple operations with one API call to reduce latency
      * @param action
      * return {Promise}
      * https://github.com/algolia/algoliasearch-client-js#custom-batch---batch
      */
-    batch(action: AlgoliaAction): Promise<any>;
+    batch(action: AlgoliaAction[]): Promise<any>;
     /**
      * Lists global API Keys
      * @param cb(err, res)
