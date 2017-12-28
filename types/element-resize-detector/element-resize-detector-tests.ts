bd86df90917426712e118be18f900380ccccc555
saranshkataria <saranshisthebest@gmail.com>
2017-12-28 10:02:57 +0530

    fixed tslint issues

--- types/element-resize-detector/element-resize-detector-tests.ts
+++ types/element-resize-detector/element-resize-detector-tests.ts
@@ -1,15 +1,15 @@
-import * as elementResizeDetectorMaker from 'element-resize-detector'
+import * as elementResizeDetectorMaker from 'element-resize-detector';
 
-let erd = elementResizeDetectorMaker({
+const erd = elementResizeDetectorMaker({
     strategy: "scroll"
   });
 
 const testElement: HTMLElement = null;
 
-  erd.listenTo(testElement, function(element) {
-    var width = element.offsetWidth;
-    var height = element.offsetHeight;
-    console.log("Size: " + width + "x" + height);
+  erd.listenTo(testElement, (element) => {
+    const width = element.offsetWidth;
+    const height = element.offsetHeight;
+    console.log(`Size: " + ${width} + "x" + ${height}`);
   });
 
   erd.removeListener(testElement, (testElement) => {});
--- types/element-resize-detector/index.d.ts
+++ types/element-resize-detector/index.d.ts
@@ -1,23 +1,20 @@
-// Type definitions for element-resize-detector 1.1.12
+// Type definitions for element-resize-detector 1.1
 // Project: https://github.com/wnr/element-resize-detector
 // Definitions by: Saransh Kataria <https://github.com/saranshkataria>
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
 
-declare module 'element-resize-detector' {
-    function elementResizeDetectorMaker(options?: elementResizeDetectorMaker.ErdmOptions)
-                                            : elementResizeDetectorMaker.Erd;
+declare function elementResizeDetectorMaker(options?: elementResizeDetectorMaker.ErdmOptions) : elementResizeDetectorMaker.Erd;
 
-    namespace elementResizeDetectorMaker {
-        interface ErdmOptions {
-        strategy?: 'scroll' | 'object';
-        }
-
-        interface Erd {
-        listenTo(element: HTMLElement, callback: (elem: HTMLElement) => void): void;
-        removeListener(element: HTMLElement, callback: (elem: HTMLElement) => void): void;
-        removeAllListeners(element: HTMLElement): void;
-        uninstall(element: HTMLElement): void;
-        }
+declare namespace elementResizeDetectorMaker {
+    interface ErdmOptions {
+    strategy?: 'scroll' | 'object';
     }
-    export = elementResizeDetectorMaker;
+
+    interface Erd {
+    listenTo(element: HTMLElement, callback: (elem: HTMLElement) => void): void;
+    removeListener(element: HTMLElement, callback: (elem: HTMLElement) => void): void;
+    removeAllListeners(element: HTMLElement): void;
+    uninstall(element: HTMLElement): void;
+    }
 }
+export = elementResizeDetectorMaker;
