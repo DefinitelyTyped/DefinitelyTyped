f7459d517f277ed309eafeb66cf9790cd90d2fa4
Paito Anderson <pj.paito@gmail.com>
2017-12-28 15:02:16 -0500

    Added empty overload for email()

--- types/react-native-communications/index.d.ts
+++ types/react-native-communications/index.d.ts
@@ -6,6 +6,7 @@
 // TypeScript Version: 2.3
 
 export function phonecall(phoneNumber: string, isPrompt: boolean): void;
+export function email(): void;
 export function email(to: string[] | null, cc: string[]| null, bcc: string[] | null, subject: string | null, body: string | null): void;
 export function text(phoneNumber?: string | null, body?: string | null): void;
 export function textWithoutEncoding(phoneNumber?: string | null, body?: string | null): void;
