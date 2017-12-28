6ba8518b4414f21ff362f8d3ce8e3e09121b5f27
Paito Anderson <pj.paito@gmail.com>
2017-12-28 15:02:16 -0500

    Added empty overload for email()

--- types/react-native-communications/react-native-communications-tests.ts
+++ types/react-native-communications/react-native-communications-tests.ts
@@ -2,6 +2,7 @@ import { phonecall, email, text, textWithoutEncoding, web } from 'react-native-c
 
 phonecall('123456789', true);
 
+email();
 email(['a@example.com'], null, ['a@example.com'], 'subject', null);
 
 text();
