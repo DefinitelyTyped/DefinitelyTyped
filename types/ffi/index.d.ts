2b61acabdc6dc35892922db5bbbcfa6799ac5415
Li Yin <liyin@live.com>
2017-12-28 19:21:19 +0800

    Set TypeScript version to 2.2 for package depending on ref-struct

--- types/ffi/index.d.ts
+++ types/ffi/index.d.ts
@@ -2,6 +2,7 @@
 // Project: https://github.com/rbranson/node-ffi
 // Definitions by: Paul Loyd <https://github.com/loyd>
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
+// TypeScript Version: 2.2
 
 /// <reference types="node" />
 
