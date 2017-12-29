2f9e1d4c40a3ac370f0d32a4d46a127c4eb55b1e
Evan Shortiss <evanshortiss@gmail.com>
2017-12-29 01:04:10 -0600

    formatting

--- types/osmosis/index.d.ts
+++ types/osmosis/index.d.ts
@@ -1,6 +1,7 @@
 // Type definitions for osmosis 1.1
 // Project: https://github.com/rchipka/node-osmosis
-// Definitions by: Juraj Kočan <https://github.com/jurajkocan>, Evan Shortiss <https://github.com/evanshortiss>
+// Definitions by: Juraj Kočan <https://github.com/jurajkocan>
+//                 Evan Shortiss <https://github.com/evanshortiss>
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
 
 interface Osmosis {
