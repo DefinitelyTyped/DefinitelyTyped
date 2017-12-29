fcf263c0254548eaab986eaa65df84eb6297eb97
Bao Hua <red_s_bean@163.com>
2017-12-29 10:33:50 +0800

    add getLocalImgData definition which was missed
    wx.getLocalImgData definition is missed, add it

--- types/jweixin/index.d.ts
+++ types/jweixin/index.d.ts
@@ -1,6 +1,7 @@
 // Type definitions for jweixin 1.0
 // Project: https://mp.weixin.qq.com/wiki/11/74ad127cc054f6b80759c40f77ec03db.html
 // Definitions by: taoqf <https://github.com/taoqf>
+// 					gomydodo <https://github.com/gomydodo>
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
 /* =================== USAGE ===================
 import * as wx from 'jweixin';
