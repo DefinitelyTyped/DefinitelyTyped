9279f291b110539de48dce6165a03411a008e2d6
Klaus Meinhardt <klaus.meinhardt1@gmail.com>
2017-12-29 23:38:45 +0100

    Add to-absolute-glob

---
+++ types/to-absolute-glob/to-absolute-glob-tests.ts
@@ -0,0 +1,6 @@
+import * as toAbsoluteGlob from 'to-absolute-glob';
+
+let result: string = toAbsoluteGlob('foo.*');
+result = toAbsoluteGlob('/foo.*', {root: '/bar'});
+result = toAbsoluteGlob('foo.*', {cwd: '/bar'});
+result = toAbsoluteGlob('foo.*', {cwd: '/bar', root: '/bar'});
