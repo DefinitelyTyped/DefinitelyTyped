8e18a7d9b3b3aa946cfb6d9129a0a7b63116f998
John Downs <john@johndowns.co.nz>
2018-01-01 16:02:25 +1300

    Update author name format to match convention

--- types/documentdb-server/index.d.ts
+++ types/documentdb-server/index.d.ts
@@ -1,7 +1,6 @@
 // Type definitions for Cosmos DB server-side JavaScript SDK
 // Project: http://azure.github.io/azure-documentdb-js-server/
-// Definitions by: François Nguyen <https://github.com/lith-light-g>
-//                 John Downs <https://github.com/johndowns/>
+// Definitions by: François Nguyen <https://github.com/lith-light-g>, John Downs <https://github.com/johndowns/>
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
 
 /** The Context object provides access to all operations that can be performed on Cosmos DB data, as well as access to the request and response objects. */
