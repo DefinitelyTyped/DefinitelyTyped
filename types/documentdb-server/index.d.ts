11f3d8c3d34fa3b26cb0e093f5299ddf52b76d47
John Downs <john@johndowns.co.nz>
2018-01-01 15:26:34 +1300

    Update metadata

--- types/documentdb-server/index.d.ts
+++ types/documentdb-server/index.d.ts
@@ -1,9 +1,10 @@
-// Type definitions for DocumentDB server side JavaScript SDK
-// Project: http://dl.windowsazure.com/documentDB/jsserverdocs
+// Type definitions for Cosmos DB server-side JavaScript SDK
+// Project: http://azure.github.io/azure-documentdb-js-server/
 // Definitions by: François Nguyen <https://github.com/lith-light-g>
+//                 John Downs <https://github.com/johndowns/>
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
 
-/** The Context object provides access to all operations that can be performed on DocumentDB data, as well as access to the request and response objects. */
+/** The Context object provides access to all operations that can be performed on Cosmos DB data, as well as access to the request and response objects. */
 interface IContext {
     /** Gets the collection object. */
     getCollection(): ICollection;
