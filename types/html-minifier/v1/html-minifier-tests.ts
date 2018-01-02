0d5e00675068defdf30cff337b6141e2c2bc2afa
Riku <riku.ayanokoji@gmail.com>
2018-01-03 04:08:33 +0800

    update html-minifier version to 3.5 (#21907)
    * update html-minifier to v3
    
    * sweep lint issues

--- types/html-minifier/v1/html-minifier-tests.ts
+++ types/html-minifier/v1/html-minifier-tests.ts
@@ -1,7 +1,8 @@
+
 import * as HTMLMinifier from 'html-minifier';
 const minify = HTMLMinifier.minify;
 
-const result = minify('<p title="blah" id="moo">foo</p>', {
+var result = minify('<p title="blah" id="moo">foo</p>', {
   removeAttributeQuotes: true
 });
 result; // '<p title=blah id=moo>foo</p>'
