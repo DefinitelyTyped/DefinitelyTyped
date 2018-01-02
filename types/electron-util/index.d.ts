58d62d2215bb84ca48513606ea0276e8e761df39
Mohamed Hegazy <mhegazy@microsoft.com>
2018-01-02 12:05:30 -0800

    Fix project link

--- types/electron-util/index.d.ts
+++ types/electron-util/index.d.ts
@@ -1,5 +1,5 @@
 // Type definitions for electron-util 0.2
-// Project: https://github.com/kputh
+// Project:  https://github.com/sindresorhus/electron-util
 // Definitions by: Kai Puth <https://github.com/kputh>
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
 
