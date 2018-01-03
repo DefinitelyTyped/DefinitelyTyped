c1ac1f6260fac08322d2621fb3bdb65cdd920213
york yao <yaoao12306@outlook.com>
2018-01-03 20:05:42 +0800

    add types of pangu

---
+++ types/pangu/index.d.ts
@@ -0,0 +1,24 @@
+// Type definitions for pangu 3.3
+// Project: https://github.com/vinta/pangu.js
+// Definitions by: York Yao <https://github.com/plantain-00>
+// Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
+
+export function spacing(text: string): string;
+
+export function spacingFile(path: string, callback: (error: Error, data: string) => void): void;
+export function spacingFile(path: string): Promise<string>;
+
+export function spacingFileSync(path: string): string;
+
+export function spacingText(text: string, callback: (error: Error, data: string) => void): void;
+export function spacingText(text: string): Promise<string>;
+
+export function spacingPage(): void;
+export function spacingElementById(id: string): void;
+export function spacingElementByClassName(className: string): void;
+export function spacingElementByTagName(tagName: string): void;
+
+export namespace Pangu {
+}
+export default Pangu;
+export as namespace pangu;
