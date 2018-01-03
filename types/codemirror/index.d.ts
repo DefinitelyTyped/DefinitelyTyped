c7450fa3201c7c8e6e519cc8146fb3f135963b2f
Joseph Safwat Khella <josephskhella@gmail.com>
2018-01-03 10:08:09 +0200

    Update index.d.ts in @types/codemirror (#22595)
    * Update index.d.ts
    
    * Remove BOM

--- types/codemirror/index.d.ts
+++ types/codemirror/index.d.ts
@@ -788,6 +788,9 @@ declare namespace CodeMirror {
         (it will default to be to the right of all other gutters). These class names are the keys passed to setGutterMarker. */
         gutters?: string[];
 
+        /** Provides an option foldGutter, which can be used to create a gutter with markers indicating the blocks that can be folded. */
+        foldGutter?: boolean;
+
         /** Determines whether the gutter scrolls along with the content horizontally (false)
         or whether it stays fixed during horizontal scrolling (true, the default). */
         fixedGutter?: boolean;
