aa4e6b00c4e0943d564b60953e90b2cea5f58215
Shawn Clabough <shawnc@outtrack.com>
2018-01-03 11:22:17 -0800

    Update index.d.ts for Kendo-UI GridColumn definition (#22100)
    * Update index.d.ts
    
    Since the command property of a GridColumn can be an array of strings, for example:
    
    { command: ["edit", "destroy"], title: "&nbsp;", width: "250px" }
    
    The current definition is
    
    command?: GridColumnCommandItem[];
    
    but it should be
    
    command?: (string|GridColumnCommandItem)[];
    
    * Update index.d.ts for Kendo-UI GridColumn definition
    
    Additional update for command property of GridColumn - following documentation at https://docs.telerik.com/kendo-ui/api/jsp/grid/column-commanditem

--- types/kendo-ui/index.d.ts
+++ types/kendo-ui/index.d.ts
@@ -3748,7 +3748,7 @@ declare namespace kendo.ui {
         aggregates?: any;
         attributes?: any;
         columns?: any;
-        command?: GridColumnCommandItem[];
+        command?: string|(string|GridColumnCommandItem)[];
         editable?: Function;
         encoded?: boolean;
         field?: string;
