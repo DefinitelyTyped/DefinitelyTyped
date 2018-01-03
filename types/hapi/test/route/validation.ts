230f9e3bc74d1207879beb6e5df2150ceaa46a1d
Rafael S. Fijalkowski <rsouza@13.cl>
2018-01-03 18:08:50 -0300

    Add server route validation test file.

---
+++ types/hapi/test/route/validation.ts
@@ -0,0 +1,30 @@
+// from https://hapijs.com/tutorials/validation?lang=en_US
+import { ServerRouteConfig, Request, ResponseToolkit, Server, ServerOptions, ServerRoute } from "hapi";
+import * as Joi from "joi";
+
+const options: ServerOptions = {
+    port: 8000,
+};
+
+const configObject: ServerRouteConfig = {
+    validate: {
+        params: {
+            name: Joi.string().min(3).max(10)
+        }
+    }
+}
+
+const serverRoute: ServerRoute = {
+    path: '/',
+    method: 'GET',
+    handler: (request: Request, h: ResponseToolkit) => {
+        return 'ok: ' + request.path;
+    },
+    config: configObject
+};
+
+const server = new Server(options);
+server.route(serverRoute);
+
+server.start();
+console.log('Server started at: ' + server.info.uri);
