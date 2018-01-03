abb6cbad16ac7803175ee07dd55591d515516620
Olaf Tomalka <olaf.tomalka@gmail.com>
2018-01-03 23:44:18 +0100

    Fix Node's path.format() requiring all options (#22525)
    * Fix Node's path.format() requiring all options
    
    The bug manifested during use of path.win32 and path.posix submodules.
    Both functions required all of the options to exist which is not
    the case as some override others.
    Further research indicated that previous Node versions were also
    susceptible, up to v4.
    
    I've used the Typescript's Partial<> type which makes all properties
    optional, allowing for easier maintenance and readabillity at the cost
    of now requiring Typescript 2.1, which I believe is a good trade-off.
    
    * Revert the decision to use TS 2.1 in @types/node
    
    Updating Typescript version would require to update
    all other types that rely on Node, which may be quite a lot.
    Fixing a bug is not a big enough reason to add new version to all
    of those packages.

--- types/node/v7/index.d.ts
+++ types/node/v7/index.d.ts
@@ -3005,6 +3005,28 @@ declare module "path" {
          */
         name: string;
     }
+    export interface FormatInputPathObject {
+        /**
+         * The root of the path such as '/' or 'c:\'
+         */
+        root?: string;
+        /**
+         * The full directory path such as '/home/user/dir' or 'c:\path\dir'
+         */
+        dir?: string;
+        /**
+         * The file name including extension (if any) such as 'index.html'
+         */
+        base?: string;
+        /**
+         * The file extension (if any) such as '.html'
+         */
+        ext?: string;
+        /**
+         * The file name without extension (if any) such as 'index'
+         */
+        name?: string;
+    }
 
     /**
      * Normalize a string path, reducing '..' and '.' parts.
@@ -3081,7 +3103,7 @@ declare module "path" {
      *
      * @param pathString path to evaluate.
      */
-    export function format(pathObject: ParsedPath): string;
+    export function format(pathObject: FormatInputPathObject): string;
 
     export module posix {
         export function normalize(p: string): string;
@@ -3095,7 +3117,7 @@ declare module "path" {
         export var sep: string;
         export var delimiter: string;
         export function parse(p: string): ParsedPath;
-        export function format(pP: ParsedPath): string;
+        export function format(pP: FormatInputPathObject): string;
     }
 
     export module win32 {
@@ -3110,7 +3132,7 @@ declare module "path" {
         export var sep: string;
         export var delimiter: string;
         export function parse(p: string): ParsedPath;
-        export function format(pP: ParsedPath): string;
+        export function format(pP: FormatInputPathObject): string;
     }
 }
 
