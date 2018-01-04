d4b376122b1ba6a6b8c88778fafc482193a7b249
Nicholas Higgins <nicholashza@gmail.com>
2018-01-04 15:41:09 +0200

    Version update in header

--- types/howler/index.d.ts
+++ types/howler/index.d.ts
@@ -1,4 +1,4 @@
-// Type definitions for howler.js v2.0.5
+// Type definitions for howler.js v2.0.7
 // Project: https://github.com/goldfire/howler.js
 // Definitions by: Pedro Casaubon <https://github.com/xperiments>, Todd Dukart <https://github.com/tdukart>, Alexander Leon <https://github.com/alien35>, Nicholas Higgins <https://github.com/nicholashza> 
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
