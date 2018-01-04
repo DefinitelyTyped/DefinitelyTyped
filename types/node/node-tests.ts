46afc73cfb20b1ef50a5a8155d171fec6d22412d
Klaus Meinhardt <klaus.meinhardt1@gmail.com>
2018-01-04 18:25:08 +0100

    node: add module.paths (#22578)

--- types/node/node-tests.ts
+++ types/node/node-tests.ts
@@ -3913,6 +3913,8 @@ namespace module_tests {
 
     const m1: Module = new Module("moduleId");
     const m2: Module = new Module.Module("moduleId");
+    let paths: string[] = module.paths;
+    paths = m1.paths;
 }
 
 ////////////////////////////////////////////////////
