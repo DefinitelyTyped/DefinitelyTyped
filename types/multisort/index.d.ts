eff35420f8a6eab92f4c60961c3dc1c9dac5a54c
Marek <marek.b@cstechnologies.cz>
2018-01-05 10:23:17 +0100

    tsconfig, tslint fixes

--- types/multisort/index.d.ts
+++ types/multisort/index.d.ts
@@ -8,7 +8,7 @@
  * @param sortings Sort criterions.
  */
 declare function multisort<T>(
-    criterions: multisort.Criterion<T> | multisort.Criterion<T>[],
+    criterions: multisort.Criterion<T> | Array<multisort.Criterion<T>>,
 ): multisort.SortFunction<T>;
 
 /**
@@ -18,7 +18,7 @@ declare function multisort<T>(
  */
 declare function multisort<T>(
     toSort: T[],
-    criterions: multisort.Criterion<T> | multisort.Criterion<T>[],
+    criterions: multisort.Criterion<T> | Array<multisort.Criterion<T>>,
 ): T[];
 
 declare namespace multisort {
@@ -34,18 +34,18 @@ declare namespace multisort {
      * - Allow an initial dot: ".prop.subprop" as well as "prop.subprop".
      * - Allow existential criterion by tailing question mark: "prop.subprop?".
      */
-    export type Criterion<T> = ((item: T) => any) | number | string;
+    type Criterion<T> = ((item: T) => any) | number | string;
 
     /**
      * Sorting function.
      */
-    export type SortFunction<T> = {
+    interface SortFunction<T> {
         (toSort: T[]): void;
         /**
          * To allow this to plug in to other sorting mechanisms.
          */
         comparator: (a: T, b: T) => number;
-    };
+    }
 }
 
 export = multisort;
--- types/multisort/multisort-tests.ts
+++ types/multisort/multisort-tests.ts
@@ -1,4 +1,4 @@
-import * as multisort from '.';
+import * as multisort from 'multisort';
 
 const gameShowHosts = [
     {
@@ -11,13 +11,11 @@ const gameShowHosts = [
         fame: 2,
         show: 'The Price is Right',
         say: {
-            name: function() {
-                return 'Bob Barker';
-            },
-            catchphrase: function(exclaimations: any) {
-                if (!exclaimations) throw 'No arguments passed'; // To test that sorting function passes arguments in.
-                var catchphrase = 'Come on down';
-                for (var i = 0; i < exclaimations; i++) {
+            name: () => 'Bob Barker',
+            catchphrase: (exclaimations: any) => {
+                if (!exclaimations) throw new Error('No arguments passed'); // To test that sorting function passes arguments in.
+                let catchphrase = 'Come on down';
+                for (let i = 0; i < exclaimations; i++) {
                     catchphrase = catchphrase + '!';
                 }
                 return catchphrase;
@@ -34,13 +32,11 @@ const gameShowHosts = [
         fame: 3,
         show: 'Who Wants to be a Millionaire',
         say: {
-            name: function() {
-                return 'Regis Philbin';
-            },
-            catchphrase: function(questionMarks: any) {
-                if (!questionMarks) throw 'No arguments passed';
-                var catchphrase = 'Is that your final answer';
-                for (var i = 0; i < questionMarks; i++) {
+            name: () => 'Regis Philbin',
+            catchphrase: (questionMarks: any) => {
+                if (!questionMarks) throw new Error('No arguments passed');
+                let catchphrase = 'Is that your final answer';
+                for (let i = 0; i < questionMarks; i++) {
                     catchphrase = catchphrase + '?';
                 }
                 return catchphrase;
@@ -56,13 +52,11 @@ const gameShowHosts = [
         fame: 3,
         show: 'Jeopardy',
         say: {
-            name: function() {
-                return 'Alex Trebek';
-            },
-            catchphrase: function(exclaimations: any) {
-                if (!exclaimations) throw 'No arguments passed';
-                var catchphrase = 'This is Jeopardy';
-                for (var i = 0; i < exclaimations; i++) {
+            name: () => 'Alex Trebek',
+            catchphrase: (exclaimations: any) => {
+                if (!exclaimations) throw new Error('No arguments passed');
+                let catchphrase = 'This is Jeopardy';
+                for (let i = 0; i < exclaimations; i++) {
                     catchphrase = catchphrase + '!';
                 }
                 return catchphrase;
@@ -79,13 +73,11 @@ const gameShowHosts = [
         fame: 1,
         retired: true,
         say: {
-            name: function() {
-                return 'Ben Bailey';
-            },
-            catchphrase: function(exclaimations: any) {
-                if (!exclaimations) throw 'No arguments passed';
-                var catchphrase = "You're in The Cash Cab";
-                for (var i = 0; i < exclaimations; i++) {
+            name: () => 'Ben Bailey',
+            catchphrase: (exclaimations: any) => {
+                if (!exclaimations) throw new Error('No arguments passed');
+                let catchphrase = "You're in The Cash Cab";
+                for (let i = 0; i < exclaimations; i++) {
                     catchphrase = catchphrase + '!';
                 }
                 return catchphrase;
@@ -101,13 +93,11 @@ const gameShowHosts = [
         fame: 2,
         show: 'Wheel of Fortune',
         say: {
-            name: function() {
-                return 'Pat Sajak';
-            },
-            catchphrase: function(exclaimations: any) {
-                if (!exclaimations) throw 'No arguments passed';
-                var catchphrase = 'Give the wheel a spin';
-                for (var i = 0; i < exclaimations; i++) {
+            name: () => 'Pat Sajak',
+            catchphrase: (exclaimations: any) => {
+                if (!exclaimations) throw new Error('No arguments passed');
+                let catchphrase = 'Give the wheel a spin';
+                for (let i = 0; i < exclaimations; i++) {
                     catchphrase = catchphrase + '!';
                 }
                 return catchphrase;
@@ -118,9 +108,7 @@ const gameShowHosts = [
 
 const input1 = [3, 4, 2, 5, 1];
 multisort(input1, 1);
-multisort(input1, function(a) {
-    return a;
-});
+multisort(input1, (a) => a);
 
 const input2 = ['hello', [1], [1, 2], []];
 multisort(input2, 'length');
@@ -148,7 +136,7 @@ multisort(input4, 'name.toLowerCase()');
 const input5 = ['dog', 'Dog', 'DOG', 'demon', 'Demon', 'DEMON'];
 multisort(input5, 'toLowerCase()');
 
-const input6: { func: () => any }[] = [
+const input6: Array<{ func: () => any }> = [
     { func: () => true },
     { func: () => null },
 ];
