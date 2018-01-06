dc086c892f3eb341192e990ae5fe350e03f5b22c
Aayush Kapoor <xeoneux@icloud.com>
2018-01-06 18:35:45 +0530

    Update speakeasy.d.ts for missing functions

--- types/speakeasy/index.d.ts
+++ types/speakeasy/index.d.ts
@@ -3,7 +3,15 @@
 // Definitions by: Lucas Woo <https://github.com/legendecas>, Alexander Batukhtin <https://github.com/mrOlorin>
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
 
-interface Key {
+declare module "speakeasy" {
+  export = speakeasy;
+
+  interface SharedOptions {
+    encoding?: string;
+    algorithm?: string;
+  }
+
+  interface Key {
     ascii: string;
     base32: string;
     hex: string;
@@ -12,83 +20,100 @@ interface Key {
     qr_code_base32: string;
     google_auth_qr: string;
     otpauth_url?: string;
-}
+  }
 
-interface DigestOptions {
+  interface DigestOptions extends SharedOptions {
     secret: string;
     counter: string;
-    encoding?: string;
-    algorithm?: string;
     key?: string;
-}
+  }
 
-interface GenerateOptions {
+  interface GenerateOptions {
     length?: number;
     symbols?: boolean;
     qr_codes?: boolean;
     google_auth_qr?: boolean;
     name?: string;
-}
+  }
 
-interface GenerateSecretOptions {
+  interface GenerateSecretOptions {
     length?: number;
     name?: string;
     qr_codes?: boolean;
     google_auth_qr?: boolean;
     otpauth_url?: boolean;
     symbols?: boolean;
-}
+  }
 
-interface TotpOptions {
+  interface TotpOptions extends SharedOptions {
     key?: string;
     step?: number;
     time?: number;
     initial_time?: number;
     length?: number;
-    encoding?: string;
     counter?: number;
     epoch?: number;
     secret?: string;
     digits?: number;
     digest?: () => string;
-    algorithm?: string;
-}
+  }
 
-interface HotpOptions {
+  interface TotpVerifyOptions extends SharedOptions {
+    secret: string;
+    token: string;
+    time?: number;
+    step?: number;
+    epoch?: number;
+    counter?: number;
+    digits?: number;
+    window?: number;
+  }
+
+  interface HotpOptions extends SharedOptions {
     key: string;
     counter: number;
     length?: number;
-    encoding?: string;
     digits?: number;
     digest?: () => string;
-}
+  }
 
-interface OtpauthURLOptions {
+  interface HotpVerifyOptions extends SharedOptions {
+    secret: string;
+    token: string;
+    counter: number;
+    digits?: number;
+    window?: number;
+  }
+
+  interface OtpauthURLOptions extends SharedOptions {
     secret: string;
     label: string;
     issuer?: any;
     type?: string;
     counter?: number;
-    algorithm?: string;
     digits?: number;
     period?: number;
-    encoding?: string;
+  }
+
+  interface Speakeasy {
+    digest: (options: DigestOptions) => Buffer;
+    hotp: {
+      (options: HotpOptions): string;
+      verifyDelta: (options: HotpVerifyOptions) => boolean;
+      verify: (options: HotpVerifyOptions) => boolean;
+    };
+    totp: {
+      (options: TotpOptions): string;
+      verifyDelta: (options: TotpVerifyOptions) => boolean;
+      verify: (options: TotpVerifyOptions) => boolean;
+    };
+    time(options: TotpOptions): string;
+    counter(options: HotpOptions): string;
+    generate_key(options: GenerateOptions): Key;
+    generateSecret: (options?: GenerateSecretOptions) => Key;
+    generateSecretASCII: (length?: number, symbols?: boolean) => string;
+    otpauthURL: (options: OtpauthURLOptions) => string;
+  }
+
+  const speakeasy: Speakeasy;
 }
-
-export declare function digest(options: DigestOptions): string;
-
-export declare function generate_key(options: GenerateOptions): Key;
-
-export declare function generateSecret(options: GenerateSecretOptions): Key;
-
-export declare function generateSecretASCII(length?: number, symbols?: boolean): string;
-
-export declare function otpauthURL(options: OtpauthURLOptions): string;
-
-export declare function hotp(options: HotpOptions): string;
-
-export declare function counter(options: HotpOptions): string;
-
-export declare function totp(options: TotpOptions): string;
-
-export declare function time(options: TotpOptions): string;
