df9528285bae1882cd8ed3ddd59f9235d8fbe485
Alberto Vasquez <alberto.vasquez@gmail.com>
2018-01-06 22:33:56 -0500

    Update index.d.ts

--- types/aws-serverless-express/index.d.ts
+++ types/aws-serverless-express/index.d.ts
@@ -1,6 +1,6 @@
 // Type definitions for aws-serverless-express 2.1
 // Project: https://github.com/awslabs/aws-serverless-express
-// Definitions by: Ben Speakman <https://github.com/threesquared>, Josh Caffey <https://github.com/jcaffey>, Matthias Meyer <https://github.com/mattmeye>
+// Definitions by: Ben Speakman <https://github.com/threesquared>, Josh Caffey <https://github.com/jcaffey>, Matthias Meyer <https://github.com/mattmeye>, Alberto Vasquez <https://github.com/albertovasquez>
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
 // TypeScript Version: 2.3
 
