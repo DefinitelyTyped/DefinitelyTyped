d3a74b87d79e5e60caf7b7143f16e707a91794ab
Alberto Vasquez <alberto.vasquez@gmail.com>
2018-01-08 17:13:10 -0500

    [aws-serverless-express] createServer should expect a method that returns void (#22724)
    * createServer should expect a method that returns void
    
    Node's index.d.ts has the following type definitions for createServer
    export function createServer(requestListener?: (request: IncomingMessage, response: ServerResponse) =>void ): Server;
    
    * Update index.d.ts
    
    * Updated version in header

--- types/aws-serverless-express/index.d.ts
+++ types/aws-serverless-express/index.d.ts
@@ -1,6 +1,6 @@
-// Type definitions for aws-serverless-express 2.1
+// Type definitions for aws-serverless-express 3.0.2
 // Project: https://github.com/awslabs/aws-serverless-express
-// Definitions by: Ben Speakman <https://github.com/threesquared>, Josh Caffey <https://github.com/jcaffey>, Matthias Meyer <https://github.com/mattmeye>
+// Definitions by: Ben Speakman <https://github.com/threesquared>, Josh Caffey <https://github.com/jcaffey>, Matthias Meyer <https://github.com/mattmeye>, Alberto Vasquez <https://github.com/albertovasquez>
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
 // TypeScript Version: 2.3
 
@@ -9,7 +9,7 @@ import * as http from 'http';
 import * as lambda from 'aws-lambda';
 
 export function createServer(
-    requestListener: (request: http.IncomingMessage, response: http.ServerResponse) => http.Server,
+    requestListener: (request: http.IncomingMessage, response: http.ServerResponse) => void,
     serverListenCallback?: () => any,
     binaryMimeTypes?: string[]
 ): http.Server;
