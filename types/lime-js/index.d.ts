dfefa9ad3fefffb0aea122d9f9a87ede5e3c430e
Samuel Martins <samuelmartins.sw@gmail.com>
2018-01-08 10:21:33 -0200

    Increase version number

--- types/lime-js/index.d.ts
+++ types/lime-js/index.d.ts
@@ -1,4 +1,4 @@
-// Type definitions for lime-js 0.0.3
+// Type definitions for lime-js 0.0.4
 // Project: https://github.com/takenet/lime-js
 // Definitions by: Arthur Xavier <https://github.com/arthur-xavier>
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
