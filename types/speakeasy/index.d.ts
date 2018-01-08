8e38462155666dc3703ff03c16fc6b27091a77b7
Aayush Kapoor <xeoneux@icloud.com>
2018-01-08 15:30:55 +0530

    Fix namespace export for speakeasy

--- types/speakeasy/index.d.ts
+++ types/speakeasy/index.d.ts
@@ -3,117 +3,119 @@
 // Definitions by: Lucas Woo <https://github.com/legendecas>, Alexander Batukhtin <https://github.com/mrOlorin>, Aayush Kapoor <https://github.com/xeoneux>
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
 
-declare module "speakeasy" {
-  export = speakeasy;
+export as namespace speakeasy;
+export = speakeasy;
 
-  interface SharedOptions {
-    encoding?: string;
-    algorithm?: string;
-  }
+declare namespace speakeasy {
+    interface SharedOptions {
+        encoding?: string;
+        algorithm?: string;
+    }
 
-  interface Key {
-    ascii: string;
-    base32: string;
-    hex: string;
-    qr_code_ascii: string;
-    qr_code_hex: string;
-    qr_code_base32: string;
-    google_auth_qr: string;
-    otpauth_url?: string;
-  }
+    interface Key {
+        ascii: string;
+        base32: string;
+        hex: string;
+        qr_code_ascii: string;
+        qr_code_hex: string;
+        qr_code_base32: string;
+        google_auth_qr: string;
+        otpauth_url?: string;
+    }
 
-  interface DigestOptions extends SharedOptions {
-    secret: string;
-    counter: string;
-    key?: string;
-  }
+    interface DigestOptions extends SharedOptions {
+        secret: string;
+        counter: string;
+        key?: string;
+    }
 
-  interface GenerateOptions {
-    length?: number;
-    symbols?: boolean;
-    qr_codes?: boolean;
-    google_auth_qr?: boolean;
-    name?: string;
-  }
+    interface GenerateOptions {
+        length?: number;
+        symbols?: boolean;
+        qr_codes?: boolean;
+        google_auth_qr?: boolean;
+        name?: string;
+    }
 
-  interface GenerateSecretOptions {
-    length?: number;
-    name?: string;
-    qr_codes?: boolean;
-    google_auth_qr?: boolean;
-    otpauth_url?: boolean;
-    symbols?: boolean;
-  }
+    interface GenerateSecretOptions {
+        length?: number;
+        name?: string;
+        qr_codes?: boolean;
+        google_auth_qr?: boolean;
+        otpauth_url?: boolean;
+        symbols?: boolean;
+    }
 
-  interface TotpOptions extends SharedOptions {
-    key?: string;
-    step?: number;
-    time?: number;
-    initial_time?: number;
-    length?: number;
-    counter?: number;
-    epoch?: number;
-    secret?: string;
-    digits?: number;
-    digest?: () => string;
-  }
+    interface TotpOptions extends SharedOptions {
+        key?: string;
+        step?: number;
+        time?: number;
+        initial_time?: number;
+        length?: number;
+        counter?: number;
+        epoch?: number;
+        secret?: string;
+        digits?: number;
+        digest?: () => string;
+    }
 
-  interface TotpVerifyOptions extends SharedOptions {
-    secret: string;
-    token: string;
-    time?: number;
-    step?: number;
-    epoch?: number;
-    counter?: number;
-    digits?: number;
-    window?: number;
-  }
+    interface TotpVerifyOptions extends SharedOptions {
+        secret: string;
+        token: string;
+        time?: number;
+        step?: number;
+        epoch?: number;
+        counter?: number;
+        digits?: number;
+        window?: number;
+    }
 
-  interface HotpOptions extends SharedOptions {
-    key: string;
-    counter: number;
-    length?: number;
-    digits?: number;
-    digest?: () => string;
-  }
+    interface HotpOptions extends SharedOptions {
+        key: string;
+        counter: number;
+        length?: number;
+        digits?: number;
+        digest?: () => string;
+    }
 
-  interface HotpVerifyOptions extends SharedOptions {
-    secret: string;
-    token: string;
-    counter: number;
-    digits?: number;
-    window?: number;
-  }
+    interface HotpVerifyOptions extends SharedOptions {
+        secret: string;
+        token: string;
+        counter: number;
+        digits?: number;
+        window?: number;
+    }
 
-  interface OtpauthURLOptions extends SharedOptions {
-    secret: string;
-    label: string;
-    issuer?: any;
-    type?: string;
-    counter?: number;
-    digits?: number;
-    period?: number;
-  }
+    interface OtpauthURLOptions extends SharedOptions {
+        secret: string;
+        label: string;
+        issuer?: any;
+        type?: string;
+        counter?: number;
+        digits?: number;
+        period?: number;
+    }
 
-  interface Speakeasy {
-    digest: (options: DigestOptions) => string;
-    hotp: {
-      (options: HotpOptions): string;
-      verifyDelta: (options: HotpVerifyOptions) => boolean;
-      verify: (options: HotpVerifyOptions) => boolean;
-    };
-    totp: {
-      (options: TotpOptions): string;
-      verifyDelta: (options: TotpVerifyOptions) => boolean;
-      verify: (options: TotpVerifyOptions) => boolean;
-    };
-    time(options: TotpOptions): string;
-    counter(options: HotpOptions): string;
-    generate_key(options: GenerateOptions): Key;
-    generateSecret: (options?: GenerateSecretOptions) => Key;
-    generateSecretASCII: (length?: number, symbols?: boolean) => string;
-    otpauthURL: (options: OtpauthURLOptions) => string;
-  }
+    interface Hotp {
+        (options: HotpOptions): string;
+        verifyDelta: (options: HotpVerifyOptions) => boolean;
+        verify: (options: HotpVerifyOptions) => boolean;
+    }
 
-  const speakeasy: Speakeasy;
+    interface Totp {
+        (options: TotpOptions): string;
+        verifyDelta: (options: TotpVerifyOptions) => boolean;
+        verify: (options: TotpVerifyOptions) => boolean;
+    }
+
+    const hotp: Hotp;
+    const totp: Totp;
+
+    function digest(options: DigestOptions): string;
+    function time(options: TotpOptions): string;
+    function counter(options: HotpOptions): string;
+    function generate_key(options: GenerateOptions): Key;
+    function generateSecret(options?: GenerateSecretOptions): Key;
+    function generateSecretASCII(length?: number, symbols?: boolean): string;
+    function otpauthURL(options: OtpauthURLOptions): string;
 }
