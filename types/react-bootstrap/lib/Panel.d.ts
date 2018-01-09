d548609c78b5aca226eeef9947edcbe4598f30ce
Ritorna <baumgaertel.sasch@hotmail.de>
2018-01-09 13:07:12 +0100

    [react-bootstrap] Added Panel typings for Version 0.32.0
    Provided Panel typings for react Bootstrap Version 0.32.0. 
    While being backward compatible with the additional string type on the collapsiable Property for the Panel.

--- types/react-bootstrap/lib/Panel.d.ts
+++ types/react-bootstrap/lib/Panel.d.ts
@@ -6,14 +6,34 @@ declare namespace Panel {
         bsClass?: string;
         bsSize?: Sizes;
         bsStyle?: string;
-        collapsible?: boolean;
+        collapsible?: string | boolean;
         defaultExpanded?: boolean;
         eventKey?: any;
         expanded?: boolean;
         footer?: React.ReactNode;
         header?: React.ReactNode;
+        onToggle?: () => void;
         onSelect?: SelectCallback;
     }
+
+    export interface HeadingProps {
+        componentClass?: JSX.Element;
+        bsClass?: string;
+    }
+
+    export interface BodyProps {
+        bsClass?: string;
+        collapsible?: boolean;
+    }
+
+    export class Heading extends React.Component<Panel.PanelProps> {
+    }
+
+    export class Body extends React.Component<Panel.BodyProps> {
+    }
 }
-declare class Panel extends React.Component<Panel.PanelProps> { }
+
+declare class Panel extends React.Component<Panel.PanelProps> {
+}
+
 export = Panel;
