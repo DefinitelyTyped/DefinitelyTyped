a4276fd9f4565a64ae5a57bcc4cfa353e5a5b251
Andy <anhans@microsoft.com>
2018-01-08 16:43:13 -0800

    aws-serverless-express: Fix lint (#22769)

--- types/aws-serverless-express/index.d.ts
+++ types/aws-serverless-express/index.d.ts
@@ -1,6 +1,9 @@
-// Type definitions for aws-serverless-express 3.0.2
+// Type definitions for aws-serverless-express 3.0
 // Project: https://github.com/awslabs/aws-serverless-express
-// Definitions by: Ben Speakman <https://github.com/threesquared>, Josh Caffey <https://github.com/jcaffey>, Matthias Meyer <https://github.com/mattmeye>, Alberto Vasquez <https://github.com/albertovasquez>
+// Definitions by: Ben Speakman <https://github.com/threesquared>
+//                 Josh Caffey <https://github.com/jcaffey>
+//                 Matthias Meyer <https://github.com/mattmeye>
+//                 Alberto Vasquez <https://github.com/albertovasquez>
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
 // TypeScript Version: 2.3
 
