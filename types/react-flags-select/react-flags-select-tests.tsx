816c1df7d5727f0be9cdfc2b0350d4a76c709bff
Artur Sianiuk <a.sianiuk@rozumsoft.by>
2018-01-10 00:24:40 +0300

    Added type definitions for react-flags-select

---
+++ types/react-flags-select/react-flags-select-tests.tsx
@@ -0,0 +1,8 @@
+import * as React from 'react';
+import ReactFlagsSelect from 'react-flags-select';
+
+export class ReactFlagsSelectTest extends React.Component {
+    render() {
+        <ReactFlagsSelect />
+    }
+}
