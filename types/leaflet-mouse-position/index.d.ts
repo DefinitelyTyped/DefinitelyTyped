e11dcd6941a5f82abccacd55cd86c0dfb42f8af1
Hanyon <yohann.marchat@worldline.com>
2018-01-09 09:49:28 +0100

    According header to match original github repo

--- types/leaflet-mouse-position/index.d.ts
+++ types/leaflet-mouse-position/index.d.ts
@@ -1,5 +1,5 @@
 // Type definitions for leaflet-mouse-position 1.1
-// Project: https://github.com/ardhi/Leaflet.MousePosition
+// Project: https://github.com/danwild/Leaflet.MousePosition
 // Definitions by: Hanyon <https://github.com/Hanyon>
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
 // TypeScript Version: 2.3
