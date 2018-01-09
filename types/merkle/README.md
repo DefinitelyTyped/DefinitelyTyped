33c4cf226ff9e6fa0ad4273a9e3a9ebe3d3899da
Yu-Hsin Lu <kerol2r20@gmail.com>
2018-01-09 22:57:58 +0800

    Add merkle module types

---
+++ types/merkle/README.md
@@ -0,0 +1,16 @@
+# Installation
+> `npm install --save @types/merkle`
+
+# Summary
+This package contains type definitions for merkle (https://github.com/c-geek/merkle).
+
+# Details
+Files were exported from https://www.github.com/DefinitelyTyped/DefinitelyTyped/tree/master/types/merkle
+
+Additional Details
+ * Last updated: Tue, 09 Jan 2018 14:31:26 GMT
+ * Dependencies: none
+ * Global values: none
+
+# Credits
+These definitions were written by Yu Hsin Lu <https://github.com/kerol2r20>.
---
+++ types/merkle/index.d.ts
@@ -0,0 +1,37 @@
+// Type definitions for merkle 0.6.0
+// Project: https://github.com/c-geek/merkle
+// Definitions by: Yu Hsin Lu <https://github.com/kerol2r20>
+// Definitions: https://github.com/kerol2r20/DefinitelyTyped
+// TypeScript Version: 2.3
+
+/* =================== USAGE ===================
+
+    import merkle = require("merkle");
+
+    const stream = merkle('sha256');
+    const merkleTree = stream.sync([1, 2, 3, 4, 5, 6, 7, 8]);
+    merkleTree.root();
+
+ =============================================== */
+
+import stream = require("stream");
+
+declare function merkle(
+    hashname: "sha512" | "sha256" | "sha1" | "md5" | "ripemd160" | "whirlpool" | "none", 
+    useUpperCaseForHash?: boolean): MerkleStream;
+
+declare class MerkleStream extends stream.Transform {
+    public sync(leaves: any[]): MerkleTree;
+    public async(leaves: any[], callback: (err: string, tree: MerkleTree) => void): void;
+    public json(): MerkleStream;
+}
+
+declare class MerkleTree {
+    public root(): string;
+    public level(level: number): number | undefined;
+    public depth(): number;
+    public levels(): number;
+    public nodes(): number;
+}
+
+export = merkle
