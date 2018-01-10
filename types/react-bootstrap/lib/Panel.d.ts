2c73ebe080927859c84ac715c26fe38e7013259c
Ritorna <baumgaertel.sasch@hotmail.de>
2018-01-10 21:11:49 +0100

    provided full Panel Typings for 0.32.0

--- types/react-bootstrap/lib/Panel.d.ts
+++ types/react-bootstrap/lib/Panel.d.ts
@@ -1,5 +1,5 @@
 import * as React from 'react';
-import { TransitionCallbacks, Sizes, SelectCallback } from 'react-bootstrap';
+import { Collapse as RBCollapse, SelectCallback, Sizes, TransitionCallbacks } from 'react-bootstrap';
 
 declare namespace Panel {
     export interface PanelProps extends TransitionCallbacks, React.HTMLProps<Panel> {
@@ -12,7 +12,7 @@ declare namespace Panel {
         expanded?: boolean;
         footer?: React.ReactNode;
         header?: React.ReactNode;
-        onToggle?: () => void;
+        onToggle?: (expanded: boolean, event: React.SyntheticEvent<Panel>) => void;
         onSelect?: SelectCallback;
     }
 
@@ -21,16 +21,54 @@ declare namespace Panel {
         bsClass?: string;
     }
 
+    export interface TitleProps {
+        componentClass?: JSX.Element;
+        bsClass?: string;
+        toggle?: boolean;
+    }
+
     export interface BodyProps {
         bsClass?: string;
+        /**
+         * Documentation marked it as required, but its default is false
+         * @default false
+         */
         collapsible?: boolean;
     }
 
+    export interface ToggleProps {
+        componentClass?: JSX.Element;
+        onClick?: React.SyntheticEvent<Panel>;
+    }
+
+    export interface FooterProps {
+        bsClass?: string;
+    }
+
     export class Heading extends React.Component<Panel.PanelProps> {
+
+    }
+
+    export class Title extends React.Component<Panel.TitleProps> {
+
+    }
+
+    export class Toggle extends React.Component<ToggleProps> {
+
+    }
+
+    export class Collapse extends RBCollapse {
+
     }
 
     export class Body extends React.Component<Panel.BodyProps> {
+
     }
+
+    export class Footer extends React.Component<Panel.FooterProps> {
+
+    }
+
 }
 
 declare class Panel extends React.Component<Panel.PanelProps> {
