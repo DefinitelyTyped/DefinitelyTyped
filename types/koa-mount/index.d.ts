eb2e4adab443208147d1ec7a7a4b669d894a9c9a
Tomek Łaziuk <tlaziuk@gmail.com>
2018-01-13 00:09:26 +0100

    version fix and dtslint

--- types/koa-mount/index.d.ts
+++ types/koa-mount/index.d.ts
@@ -1,6 +1,7 @@
-// Type definitions for koa-mount v2.0.0
+// Type definitions for koa-mount 3.0
 // Project: https://github.com/koajs/mount
 // Definitions by: AmirSaber Sharifi <https://github.com/amirsaber>
+//                 Tomek Łaziuk <https://github.com/tlaziuk>
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
 // TypeScript Version: 2.3
 
