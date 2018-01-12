ec0932a304ee31e94eb9cadf7459fd482df5acce
Muhammad Rehan Saeed <RehanSaeed@users.noreply.github.com>
2018-01-12 16:19:58 +0000

    Bump version

--- types/compression-webpack-plugin/index.d.ts
+++ types/compression-webpack-plugin/index.d.ts
@@ -1,4 +1,4 @@
-// Type definitions for compression-webpack-plugin 0.3
+// Type definitions for compression-webpack-plugin 0.4
 // Project: https://github.com/webpack-contrib/compression-webpack-plugin
 // Definitions by: Anton Kandybo <https://github.com/dublicator>
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
