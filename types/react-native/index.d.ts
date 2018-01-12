b9c5d01caac4fd6d65f163eb729dc9d3b9e57ea2
Tim Wang <timwangdev@users.noreply.github.com>
2018-01-12 16:32:31 +0800

    [react-native] Bump version

--- types/react-native/index.d.ts
+++ types/react-native/index.d.ts
@@ -1,4 +1,4 @@
-// Type definitions for react-native 0.51
+// Type definitions for react-native 0.52
 // Project: https://github.com/facebook/react-native
 // Definitions by: Eloy Durán <https://github.com/alloy>
 //                 HuHuanming <https://github.com/huhuanming>
