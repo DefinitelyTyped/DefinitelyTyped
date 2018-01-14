cb8a7d07f21a3e9b10a85b6b827dda19e5896915
Arturas Molcanovas <a.molcanovas@gmail.com>
2018-01-14 14:57:09 +0000

    merge function signature

--- types/find-root/index.d.ts
+++ types/find-root/index.d.ts
@@ -1,4 +1,4 @@
-// Type definitions for find-root 1.1.0
+// Type definitions for find-root 1.1
 // Project: https://github.com/js-n/find-root
 // Definitions by: Arturas Molcanovas <https://github.com/Alorel>
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
@@ -8,18 +8,12 @@
 type FindRootCheckFn = (dir: string) => boolean;
 
 /**
- * Returns the path for the nearest directory to startingPath containing a package.json file
- * @param startingPath The path to start searching form, e.g. __dirname
- * @throws {Error} if package.json cannot be found
- */
-declare function findRoot(startingPath: string): string;
-
-/**
- * Returns the path for the nearest directory to startingPath where the check function reeturns true
+ * Returns the path for the nearest directory to startingPath containing a package.json file. If a check function is
+ * provided, then this will return the nearest directory for which the function returns true.
  * @param startingPath The path to start searching form, e.g. __dirname
  * @param check The check predicate
- * @throws {Error} If the function never returns true
+ * @throws {Error} if package.json cannot be found or if the function never returns true
  */
-declare function findRoot(startingPath: string, check: FindRootCheckFn): string;
+declare function findRoot(startingPath: string, check?: FindRootCheckFn): string;
 
 export = findRoot;
