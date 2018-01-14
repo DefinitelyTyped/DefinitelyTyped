fcee1d64798900878268eb7494e4d4d209c8306f
Pencl <mXnE4221github>
2018-01-14 09:27:26 +0100

    Add myself as contributor

--- types/rpio/index.d.ts
+++ types/rpio/index.d.ts
@@ -1,6 +1,7 @@
 // Type definitions for node-rpio
 // Project: https://github.com/jperkin/node-rpio
 // Definitions by: Dominik Palo <https://github.com/DominikPalo>
+//                 Hannes Früchtenicht <https://github.com/Pencl>
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
 
 /// <reference types="node" />
