6d1312be523f62992f015ad65bec1452016ca881
taoqf <tao_qiufeng@126.com>
2018-01-15 17:01:49 +0800

    add package jsreport-core

---
+++ types/jsreport-core/index.d.ts
@@ -0,0 +1,98 @@
+// Type definitions for jsreport-core 1.5
+// Project: http://jsreport.net
+// Definitions by: taoqf <https://github.com/taoqf>
+// Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
+
+// TypeScript Version: 2.6
+
+/// <reference types="node" />
+
+import { Buffer } from 'buffer';
+import { Readable } from 'stream';
+
+type helpers = (...args: any[]) => any;
+
+interface RenderOptions {
+	template: {
+		content: string;
+		engine: 'jsrender' | 'handlebars' | 'ejs' | 'jade';
+		helpers?: string | { [fun: string]: helpers };
+		recipe: 'phantom-pdf' | 'electron-pdf' | 'text' | 'xlsx' | 'html-to-xlsx' | 'phantom-image' | 'html-to-text' | 'fop-pdf' | 'client-html' | 'wrapped-html' | 'wkhtmltopdf';
+	};
+	data?: any;
+}
+
+interface Report {
+	content: Buffer;
+	stream: Readable;
+	headers: {
+		[header: string]: string | number | boolean;
+	};
+}
+
+interface Request {
+	template: {
+		content: string;
+	};
+}
+
+// interface Response {
+// 	// todo
+// }
+
+type Response = any;
+
+interface Listener {
+	add(type: string, callback: (req: Request, res: Response, err: any) => void): void;
+}
+
+interface Logger {
+	add(logger: any, options?: {
+		level: 'debug' | 'info' | 'log' | 'warn' | 'error';
+	}): void;
+}
+
+interface Collection {
+	find(query: {
+		[field: string]: any;
+	}): Promise<any>;
+}
+
+interface DocumentStore {
+	collection(options: string): Collection;
+}
+
+interface JsReporter {
+	afterRenderListeners: Listener;
+	afterTemplatingEnginesExecutedListeners: Listener;
+	beforeRenderListeners: Listener;
+	documentStore: DocumentStore;
+	initializeListeners: Listener;
+	logger: Logger;
+	validateRenderListeners: Listener;
+	init(): Promise<void>;
+	render(options: RenderOptions): Promise<Report>;
+	use(extension: any): any;
+}
+
+type JsReportStatic = (options?: Partial<{
+	autoTempCleanup: boolean;
+	dataDirectory: string;
+	extensionsLocationCache: boolean;
+	loadConfig: boolean;
+	logger: {
+		silent: boolean;
+	};
+	rootDirectory: string;
+	scripts: {
+		allowedModules: string[];
+	};
+	tasks: {
+		[task: string]: any;
+	};
+	tempDirectory: string;
+}>) => JsReporter;
+
+declare const JsReport: JsReportStatic;
+
+export = JsReport;
---
+++ types/jsreport-core/jsreport-core-tests.ts
@@ -0,0 +1,21 @@
+import * as fs from 'fs';
+import * as JsReport from 'jsreport-core';
+
+const jsreport = JsReport();
+jsreport.init().then(() => {
+	return jsreport.render({
+		template: {
+			content: '<h1>Hello {{:foo}}</h1>',
+			engine: 'jsrender',
+			recipe: 'phantom-pdf'
+		},
+		data: {
+			foo: "world"
+		}
+	}).then((resp) => {
+		// prints pdf with headline Hello world
+		console.log(resp.content.toString());
+	});
+}).catch((e) => {
+	console.log(e);
+});
