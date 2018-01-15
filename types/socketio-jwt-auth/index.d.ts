786211ca2d300bada7e14616c0294f7f56fb5e72
CodeAnimal <codeanimal@outlook.com>
2018-01-15 11:23:34 +0000

    Correct tslint issues.

--- types/socketio-jwt-auth/index.d.ts
+++ types/socketio-jwt-auth/index.d.ts
@@ -8,7 +8,7 @@ import SocketIO = require("socket.io");
 
 /**
  * This function returns a middleware function for use with Socket.IO that authenticates a new connection.
- * 
+ *
  * @param options is an object literal that contains options.
  * @param verify is a function with two args payload, and done.
  */
@@ -25,7 +25,7 @@ export interface authOptions {
 
 /**
  * This is a function with two args payload, and done.
- * 
+ *
  * `payload` is the decoded JWT payload
  * `done` is an error-first callback with three args: done(err, user, message).
  */
