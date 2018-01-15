0a2ddfec9c680285c94e2c6dae83b8cdc36a99e5
Chris Krycho <chris@chriskrycho.com>
2018-01-15 12:15:38 -0700

    Inline (in-file?) `assertType` for ember-feature-flags.

--- types/ember-feature-flags/ember-feature-flags-tests.ts
+++ types/ember-feature-flags/ember-feature-flags-tests.ts
@@ -1,6 +1,11 @@
 import Features from 'ember-feature-flags';
 import 'ember-feature-flags/tests/helpers/with-feature';
-import { assertType } from 'ember-feature-flags/tests/lib/assert';
+
+/** Static assertion that `value` has type `T` */
+// Disable tslint here b/c the generic is used to let us do a type coercion and
+// validate that coercion works for the type value "passed into" the function.
+// tslint:disable-next-line:no-unnecessary-generics
+export declare function assertType<T>(value: T): void;
 
 declare module 'ember-feature-flags' {
     export default interface Features {
