bdd652b99aa7f8c6c45e2d7213cb6c6ea9767073
Martin Pfund <m.pfund@mid.de>
2018-01-16 10:13:39 +0100

    Extended index.d.ts for more specific definitions + updated tslint.json

--- types/sceditor/index.d.ts
+++ types/sceditor/index.d.ts
@@ -6,90 +6,101 @@
 
 // Work In Progress, may contains errors
 
-declare namespace sceditor {
-	interface SCEditor {
-		dom: DOM;
+// declare module sceditor {
+declare module app.common.sceditor {
+    interface ISCEditor {
+        dom: IDOM;
+        command: ICommand;
 
-		create(selector: HTMLElement, args?: SCEditorArguments): void;
-		instance(selector: HTMLElement): SCEditorInstance;
-	}
+        create(selector: HTMLElement, args?: ISCEditorOptions): void;
+        instance(selector: HTMLElement): ISCEditorInstance;
+    }
 
-	interface SCEditorArguments {
-		plugins?: string;
-		format?: string;
-		toolbar?: string;
-		locale?: string;
-	}
+    interface ICommand {
+        set(command: string, options: ICommandOptions): void;
+    }
 
-	interface SCEditorInstance {
-		// _(...args: string[]): string;
-		// addShortcut(shortcut: string, handler);
-		// bind(events, func, excludeWysiwyg?, excludeSource?);
-		// blur(func?, excludeWysiwyg?, excludeSource?);
-		// clearBlockFormatting();
-		// closeDropDown();
-		// createDropDown();
-		// css();
-		// currentBlockNode();
-		// currentNode();
-		// destroy();
-		// dimensions();
-		// emoticons();
-		// execCommand();
-		// expandToContent();
-		focus(callback?: (event: Event) => void): void;
-		// getBody();
-		// getRangeHelper(): ISCEditorRangeHelper;
-		// getSourceEditorValue();
-		// getWysiwygEditorValue();
-		// height();
-		// ie;
-		// inSourceMode();
-		// insert();
-		// insertText();
-		// isWysiwygSupported;
-		// keyDown();
-		// keyPress();
-		// keyUp();
-		// maximize();
-		// nodeChanged();
-		// readOnly()
-		// removeShortcut();
-		// rtl();
-		// selectionChanged();
-		// setSourceEditorValue();
-		// setWysiwygEditorValue();
-		// sourceEditorInsertText();
-		// sourceMode();
-		// toggleSourceMode();
-		// unbind();
-		// updateOriginal();
-		val(value?: string, filter?: boolean): string;
-		width(value?: number|string): any;
-		// wysiwygEditorInsertHtml();
-		// wysiwygEditorInsertText();
-	}
+    interface ICommandOptions {
+        exec?: (caller: HTMLElement) => void;
+        tooltip?: string;
+    }
 
-	// interface ISCEditorRangeHelper {
-	// 	cloneSelected();
-	// 	getFirstBlockParent(selector?);
-	// 	getMarker(id: string);
-	// 	getOuterText(before: boolean, length: number): string;
-	// 	insertHTML(startHtml: string, endHtml?: string): void;
-	// }
+    interface ISCEditorOptions {
+        plugins?: string;
+        format?: string;
+        toolbar?: string;
+        locale?: string;
+    }
 
-	interface DOM {
-		blockLevelList: string;
-		// copyCSS(from, to);
-		// extractContents(start, end): boolean;
-		// findCommonAncestor(nodeA, nodeB);
-		// fixNesting(node);
-		// hasStyling(): boolean;
-		// isInline();
-		// rTraverse();
-		// traverse();
-	}
+    interface ISCEditorInstance {
+        // _(...args: string[]): string;
+        // addShortcut(shortcut: string, handler);
+        // bind(events: string, callback: (e) => void, excludeWysiwyg?, excludeSource?): void;
+        // blur(func?, excludeWysiwyg?, excludeSource?);
+        // clearBlockFormatting();
+        closeDropDown(focus?: boolean): void;
+        // createDropDown(mItem, name: string, content, unselectable?: boolean): void;
+        // css();
+        // currentBlockNode();
+        // currentNode();
+        // destroy();
+        // dimensions();
+        // emoticons();
+        execCommand(command: string, param?: string): void;
+        // expandToContent();
+        focus(callback?: (event: Event) => void): void;
+        // getBody();
+        // getRangeHelper(): ISCEditorRangeHelper;
+        // getSourceEditorValue();
+        // getWysiwygEditorValue();
+        // height();
+        // ie;
+        // inSourceMode();
+        // insert();
+        // insertText();
+        // isWysiwygSupported;
+        // keyDown();
+        // keyPress();
+        // keyUp();
+        // maximize();
+        // nodeChanged();
+        // readOnly()
+        // removeShortcut();
+        // rtl();
+        // selectionChanged();
+        // setSourceEditorValue();
+        // setWysiwygEditorValue();
+        // sourceEditorInsertText();
+        // sourceMode();
+        // toggleSourceMode();
+        // unbind();
+        // updateOriginal();
+        val(value?: string, filter?: boolean): string;
+        width(value?: number|string): any;
+        // wysiwygEditorInsertHtml();
+        // wysiwygEditorInsertText();
+    }
+
+    // interface ISCEditorRangeHelper {
+    // 	cloneSelected();
+    // 	getFirstBlockParent(selector?);
+    // 	getMarker(id: string);
+    // 	getOuterText(before: boolean, length: number): string;
+    // 	insertHTML(startHtml: string, endHtml?: string): void;
+    // }
+
+    interface IDOM {
+        blockLevelList: string;
+        // copyCSS(from, to);
+        // extractContents(start, end): boolean;
+        // findCommonAncestor(nodeA, nodeB);
+        // fixNesting(node);
+        // hasStyling(): boolean;
+        // isInline();
+        // rTraverse();
+        // traverse();
+    }
 }
 
-declare const sceditor: sceditor.SCEditor;
+declare const sceditor: app.common.sceditor.ISCEditor;
 export = sceditor;
