79328d262c863a32c1266a95941ac7040e267d8c
Nabeelah Ali <naal@microsoft.com>
2018-01-16 10:41:28 +0100

    Add types for enzyme-adapter-react-15.4.

---
+++ types/enzyme-adapter-react-15.4/index.d.ts
@@ -0,0 +1,13 @@
+// Type definitions for enzyme-adapter-react-15.4 1.0
+// Project: http://airbnb.io/enzyme/
+// Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
+
+import { EnzymeAdapter } from 'enzyme';
+
+declare class ReactFifteenFourAdapter extends EnzymeAdapter {
+}
+
+declare namespace ReactFifteenFourAdapter {
+}
+
+export = ReactFifteenFourAdapter;
