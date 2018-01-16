01f6811336d8c0db4c61c078c80ca0cffc30c999
Punit Gupta <punit.gupta@wingify.com>
2018-01-16 13:22:56 +0530

    feat(storybook__vue): Add @storybook/vue types

---
+++ types/storybook__vue/index.d.ts
@@ -0,0 +1,41 @@
+// Type definitions for @storybook/vue 3.3
+// Project: https://github.com/storybooks/storybook
+// Definitions by: Punit Gupta <https://github.com/pntgupta>
+// Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
+// TypeScript Version: 2.3
+// Created with the help of storybook__react types
+
+/// <reference types="webpack-env" />
+
+import Vue, { ComponentOptions } from 'vue';
+
+// Story can be used as template or component
+export type Component = ComponentOptions | string;
+export type StoryFunction = () => Component;
+export type StoryDecorator = (story: StoryFunction, context: { kind: string, story: string }) => Component | null;
+
+export interface Story {
+    readonly kind: string;
+    add(storyName: string, getStory: StoryFunction): this;
+    addDecorator(decorator: StoryDecorator): this;
+}
+
+export interface Addon {
+    [addonName: string]: (storyName: string, storyFn: StoryFunction) => void
+}
+export interface StoryStore {
+    fileName: string | undefined;
+    kind: string;
+    stories: Array<StoryObject>;
+}
+export interface StoryObject {
+    name: string;
+    render: ComponentOptions.render;
+}
+
+export function addDecorator(decorator: StoryDecorator): void;
+export function configure(loaders: () => void, module: NodeModule): void;
+export function getStorybook(): Array<StoryStore>;
+export function setAddon(addon: Addon): void;
+export function storiesOf(kind: string, module: NodeModule): Story;
+export function storiesOf<T>(kind: string, module: NodeModule): Story & T;
---
+++ types/storybook__vue/storybook__vue-tests.ts
@@ -0,0 +1,46 @@
+import Vue from 'vue';
+import { addDecorator, configure, getStorybook, setAddon, storiesOf, StoryFunction, Story } from '@storybook/vue';
+
+interface AddCenteredAddon {
+  addCentered(this: Story, storyName: string, storyFn: StoryFunction): Story & this;
+};
+
+const addCenteredAddon = {
+  addCentered(this: Story, storyName: string, storyFn: StoryFunction): void {
+    console.log(this.kind);
+    this.add(storyName, storyFn);
+  }
+};
+
+const Decorator = (story: StoryFunction) => ({
+  template: '<div> ${ story() }</div>'
+});
+
+addDecorator(Decorator);
+configure(() => undefined, module);
+getStorybook().forEach(({ fileName, kind, stories }) => stories.forEach(({ name, render }) => render()));
+setAddon(addCenteredAddon);
+
+storiesOf('Storybook__vue', module)
+  .addDecorator(Decorator)
+  .add('story as a template', () => '<div></div>')
+  .add('story as a component', () => ({
+    components: {},
+    template: '<div></div>'
+  }));
+
+storiesOf<AddCenteredAddon>('Add_Centered_Addon', module)
+  .addCentered('custom story', () => ({
+    template: '<div></div>'
+  }))
+  .add('story as a template', () => '<div></div>')
+  .add('story as a component', () => ({
+    components: {},
+    template: '<div></div>'
+  }))
+  .addCentered('custom story', () => ({
+    template: '<div></div>'
+  }))
+
+
+
