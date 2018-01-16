2519d0edc97f83a48eacf1f03b8156abb8b68c61
Johnny Icarus <marin.aeschbach@gmail.com>
2018-01-16 11:08:09 +0100

    Bump version

--- types/masonry-layout/index.d.ts
+++ types/masonry-layout/index.d.ts
@@ -1,4 +1,4 @@
-// Type definitions for Masonry 4.0
+// Type definitions for Masonry 4.2
 // Project: https://github.com/desandro/masonry
 // Definitions by: Mark Wilson <https://github.com/m-a-wilson>, Travis Brown <https://github.com/warriorrocker>
 // Definitions: https://github.com/borisyankov/DefinitelyTyped
