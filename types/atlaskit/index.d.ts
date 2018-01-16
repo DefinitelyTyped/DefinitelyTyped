5a491a9a13d1c640db72405e6f8563f77a26647b
Jimmy Luong <>
2018-01-16 16:34:11 +1100

    atlaskit: add @atlaskit/button types

--- types/atlaskit/index.d.ts
+++ types/atlaskit/index.d.ts
@@ -1,4 +1,4 @@
-// Type definitions for "@atlaskit/dynamic-table 6.1.4
+// Type definitions for "@atlaskit/dynamic-table 6.1
 // Project: https://bitbucket.org/atlassian/atlaskit-mk-2/
 // Definitions by: Jimmy Luong <https://github.com/dijimsta>
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
@@ -6,42 +6,42 @@
 declare module "@atlaskit/dynamic-table" {
     import { Component, ReactNode, ReactElement } from "react";
 
-    export type RowCell = {
+    type RowCell = {
         readonly key?: string | number;
         readonly content: ReactNode;
     };
 
-    export type Row = {
+    type Row = {
         readonly cells: ReadonlyArray<RowCell>;
     };
 
-    export type SortOrder = "ASC" | "DESC";
+    type SortOrder = "ASC" | "DESC";
 
-    export type SpinnerSize = "small" | "medium" | "large" | "xlarge";
+    type SpinnerSize = "small" | "medium" | "large" | "xlarge";
 
-    export type LoadingSpinnerSize = "small" | "large";
+    type LoadingSpinnerSize = "small" | "large";
 
-    export type HeadCell = RowCell & {
+    type HeadCell = RowCell & {
         readonly isSortable?: boolean;
         readonly width?: number;
         readonly shouldTruncate?: boolean;
     };
 
-    export type Head = {
+    type Head = {
         readonly cells: ReadonlyArray<HeadCell>;
     };
 
-    export type SetPageEventHandler = (page: number) => void;
+    type SetPageEventHandler = (page: number) => void;
 
-    export type SortEventPayload = {
+    type SortEventPayload = {
         readonly key: string | number;
         readonly item: RowCell;
         readonly sortOrder: SortOrder;
     };
 
-    export type SortEventHandler = (payload: SortEventPayload) => void;
+    type SortEventHandler = (payload: SortEventPayload) => void;
 
-    export type DynamicTableStatelessProps = {
+    type DynamicTableStatelessProps = {
         readonly caption?: ReactNode;
         readonly head?: Head;
         readonly rows?: ReadonlyArray<Row>;
@@ -57,26 +57,88 @@ declare module "@atlaskit/dynamic-table" {
         readonly sortOrder?: SortOrder;
     };
 
-    export class DynamicTableStateless extends Component<
-        DynamicTableStatelessProps
-    > {}
+    class DynamicTableStateless extends Component<DynamicTableStatelessProps> {}
 
-    export type DynamicTableProps = DynamicTableStatelessProps & {
+    type DynamicTableProps = DynamicTableStatelessProps & {
         readonly defaultPage: number;
         readonly defaultSortKey?: string;
         readonly defaultSortOrder?: SortOrder;
     };
 
-    export type DynamicTableState = {
+    type DynamicTableState = {
         readonly page: number;
         readonly sortKey?: string;
         readonly sortOrder?: SortOrder;
     };
 
-    class DynamicTable extends Component<
+    export default class extends Component<
         DynamicTableProps,
         DynamicTableState
     > {}
+}
 
-    export default DynamicTable;
+// Type definitions for "@atlaskit/button 6.2
+// Project: https://bitbucket.org/atlassian/atlaskit-mk-2/
+// Definitions by: Jimmy Luong <https://github.com/dijimsta>
+// Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
+// TypeScript Version: 2.4
+declare module "@atlaskit/button" {
+    import { Component, ComponentClass, ReactNode, ReactElement } from "react";
+
+    type ButtonAppearances =
+        | "default"
+        | "danger"
+        | "link"
+        | "primary"
+        | "subtle"
+        | "subtle-link"
+        | "warning"
+        | "help";
+
+    type ButtonProps = {
+        /** The base styling to apply to the button. */
+        appearance?: ButtonAppearances;
+        /** Pass aria-controls to underlying html button. */
+        ariaControls?: string;
+        /** Pass aria-expanded to underlying html button. */
+        ariaExpanded?: boolean;
+        /** Pass aria-haspopup to underlying html button. */
+        ariaHaspopup?: boolean;
+        /** This button's child nodes. */
+        children?: ReactNode;
+        /** Add a classname to the button. */
+        className?: string;
+        /** A custom component to use instead of the default button. */
+        component?: ComponentClass;
+        /** Name property of a linked form that the button submits when clicked. */
+        form?: string;
+        /** Provides a url for buttons being used as a link. */
+        href?: string;
+        /** Places an icon within the button, after the button's text. */
+        iconAfter?: ReactElement<any>;
+        /** Places an icon within the button, before the button's text. */
+        iconBefore?: ReactElement<any>;
+        /** Pass a reference on to the styled component */
+        innerRef?: any;
+        /** Provide a unique id to the button. */
+        id?: string;
+        /** Set if the button is disabled. */
+        isDisabled?: boolean;
+        /** Change the style to indicate the button is selected. */
+        isSelected?: boolean;
+        /** Handler to be called on click. */
+        onClick?: () => void;
+        /** Set the amount of padding in the button. */
+        spacing?: "compact" | "default" | "none";
+        /** Assign specific tabIndex order to the underlying html button. */
+        tabIndex?: number;
+        /** Pass target down to a link within the button component, if a href is provided. */
+        target?: string;
+        /** Set whether it is a button or a form submission. */
+        type?: "button" | "submit";
+        /** Option to fit button width to its parent width */
+        shouldFitContainer?: boolean;
+    };
+
+    export default class extends Component<ButtonProps> {}
 }
