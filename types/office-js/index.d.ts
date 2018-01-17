5244cc0eccd558871890e97bd136dbcdfedc0787
Michael Zlatkovsky <Zlatkovsky@users.noreply.github.com>
2018-01-17 07:40:06 -0800

    Update maintainers list of office-js (#22972)

--- types/office-js/index.d.ts
+++ types/office-js/index.d.ts
@@ -1,6 +1,6 @@
 // Type definitions for Office.js
 // Project: http://dev.office.com
-// Definitions by: OfficeDev <https://github.com/OfficeDev>, Lance Austin <https://github.com/LanceEA>
+// Definitions by: OfficeDev <https://github.com/OfficeDev>, Lance Austin <https://github.com/LanceEA>, Michael Zlatkovsky <https://github.com/Zlatkovsky>, Kim Brandl <https://github.com/kbrandl>, Ricky Kirkham <https://github.com/Rick-Kirkham>
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
 
 /*
