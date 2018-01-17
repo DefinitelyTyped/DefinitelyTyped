aaa7f48c1f3eab67686330a56517f35606885072
york yao <yaoao12306@outlook.com>
2018-01-17 23:58:00 +0800

    fix: CI failure because express require typescript 2.2

--- types/zipkin-instrumentation-express/index.d.ts
+++ types/zipkin-instrumentation-express/index.d.ts
@@ -2,6 +2,7 @@
 // Project: https://github.com/openzipkin/zipkin-js#readme
 // Definitions by: York Yao <https://github.com/plantain-00>
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
+// TypeScript Version: 2.2
 
 import express = require('express');
 import { Tracer } from 'zipkin';
