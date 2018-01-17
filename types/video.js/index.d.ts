508e9a9688f8d045d20e1efe4adabed7c1fc846d
Ciprian Florescu <ciprian.florescu@softwire.com>
2018-01-17 12:17:31 +0200

    Improved definitions of Video.js 6+ for those who want to implement custom Techs

--- types/video.js/index.d.ts
+++ types/video.js/index.d.ts
@@ -4,6 +4,7 @@
 //                 Simon Clériot <https://github.com/scleriot>
 //                 Sean Bennett <https://github.com/SWBennett06>
 //                 Christoph Wagner <https://github.com/IgelCampus>
+//                 Ciprian Florescu <https://github.com/cipacda>
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
 
 // The Video.js API allows you to interact with the video through
@@ -42,7 +43,8 @@ declare namespace videojs {
 		src: string;
 	}
 
-	interface Player {
+	class Player {
+		constructor(techId: string, options: any): Player;
 		play(): Player;
 		pause(): Player;
 		paused(): boolean;
@@ -72,4 +74,93 @@ declare namespace videojs {
 		poster(val?: string): string | Player;
 		playbackRate(rate?: number): number;
 	}
+
+	interface Component {
+		protected options_: any;
+		protected el_: any;
+		protected player_: any;
+		protected isReady_: boolean;
+
+		setTimeout(fn: () => void, timeout?: number): string;
+    dispose(): void;
+    trigger(event: any): void;
+    triggerReady(): void;
+	}
+
+	interface Tech extends Component {
+		new(options: any, ready: () => void): Tech;
+		getTech(name: string): Tech;
+
+    setPoster(poster: any): void;
+    manualProgressOn(): void;
+    manualProgressOff(): void;
+    trackProgress(event: any): void;
+    onDurationChange(event: any): void;
+    buffered(): any;
+    bufferedPercent(): number;
+    stopTrackingProgress(): void;
+    manualTimeUpdatesOn(): void;
+    manualTimeUpdatesOff(): void;
+    trackCurrentTime(): void;
+    stopTrackingCurrentTime(): void;
+    clearTracks(types: string|string[]): void;
+    cleanupAutoTextTracks(): void;
+    reset(): void;
+    error(error: MediaError): MediaError;
+    played(): any;
+    setCurrentTime(seconds: number): void;
+    initTrackListeners(): void;
+    addWebVttScript_(): void;
+    emulateTextTracks(): void;
+    addTextTrack(kind: string, label: string, language: string): TextTrack;
+    createRemoteTextTrack(options: any): HTMLTrackElement;
+    addRemoteTextTrack(options?: any, manualCleanup?: boolean): HTMLTrackElement;
+    removeRemoteTextTrack(track: TextTrack): void;
+    getVideoPlaybackQuality(): any;
+    playsinline(): boolean;
+    setPlaysinline(playsinline: boolean): void;
+    canPlayType(type: string): boolean;
+
+    static canPlayType(type: string): boolean;
+    static canPlaySource(srcObj: any, options: any): boolean;
+    static isTech(component: any): boolean;
+    static registerTech(name: string, tech: any): void;
+    static getTech(name: string): Tech;
+	}
+
+	interface MediaError {
+		code: number;
+		message: string;
+		status?: any[];
+	}
+
+	interface Track {
+		id: string;
+		kind: string;
+		label: string;
+		language: string;
+	}
+
+  interface TextTrack extends Track {
+		default: boolean;
+		mode: string;
+	}
+
+	interface HTMLTrackElement {
+		track: TextTrack;
+		readyState:  HTMLTrackElementReadyState;
+	}
+
+	interface HTMLTrackElementReadyState {
+		NONE: 0,
+		LOADING: 1,
+		LOADED: 2,
+		ERROR: 3,
+	}
+
+	export function getComponent(name: string): Component;
+	export function getTech(name: string): Tech;
+	export function registerTech(name: string, tech: Tech): void;
+	export function createTimeRange(start?: number, end?: number): any;
+	const log: any;
 }
