c26e3fa2bce99453403db4f958895dd3e992ea54
Dolan <dolan_miu@hotmail.com>
2018-01-17 23:29:33 +0000

    Version bump

--- types/archiver/index.d.ts
+++ types/archiver/index.d.ts
@@ -1,4 +1,4 @@
-// Type definitions for archiver 2.0
+// Type definitions for archiver 2.1
 // Project: https://github.com/archiverjs/node-archiver
 // Definitions by: Esri <https://github.com/archiverjs/node-archiver>, Dolan Miu <https://github.com/dolanmiu>, Crevil <https://github.com/crevil>
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
