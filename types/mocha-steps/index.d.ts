042ae2a682e06efae7933d90209bce95e970e645
Arylo <arylo.open@gmail.com>
2018-01-18 18:33:46 +0800

    Add type definition for mocha-steps

---
+++ types/mocha-steps/index.d.ts
@@ -0,0 +1,9 @@
+// Type definitions for mocha-steps 1.1
+// Project: https://github.com/rprieto/mocha-steps
+// Definitions by: AryloYeung <https://github.com/Arylo>
+// Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
+
+/// <reference types="mocha" />
+
+declare var step: Mocha.IContextDefinition;
+declare var xstep: Mocha.IContextDefinition;
