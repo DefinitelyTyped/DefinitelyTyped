63f1484cf617d1494a6ae48e9bc678b6651d954a
Ahn <anhpnnd@gmail.com>
2018-01-20 19:51:45 +0100

    Adjust version

--- types/jest/index.d.ts
+++ types/jest/index.d.ts
@@ -1,4 +1,4 @@
-// Type definitions for Jest 22.0
+// Type definitions for Jest 22.1
 // Project: http://facebook.github.io/jest/
 // Definitions by: Asana <https://asana.com>
 //                 Ivo Stratev <https://github.com/NoHomey>
