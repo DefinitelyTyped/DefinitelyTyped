e61a87b47cfe66d795f570de245c3c73f6c53953
Boris Breuer <borisbreuer1@gmail.com>
2018-01-21 16:34:57 +0100

    According to the documentation, execute arguments are optional. See  https://jasmine.github.io/2.3/node.html for more information.

--- types/jasmine/index.d.ts
+++ types/jasmine/index.d.ts
@@ -718,7 +718,7 @@ declare module "jasmine" {
         addSpecFile(filePath: string): void;
         addSpecFiles(files: string[]): void;
         configureDefaultReporter(options: any, ...args: any[]): void;
-        execute(files: any, filterString: any): any;
+        execute(files?: string[], filterString?: string): any;
         exitCodeCompletion(passed: any): void;
         loadConfig(config: any): void;
         loadConfigFile(configFilePath: any): void;
