5f92145aaa9111454a230e6f0f4b785ee756bb8a
Ika <ikatyang@gmail.com>
2018-01-21 13:43:57 +0800

    docs: add descriptions

--- types/vfile-location/index.d.ts
+++ types/vfile-location/index.d.ts
@@ -10,7 +10,14 @@ declare function vfileLocation(vfile: string | VFile.VFile<{}>): vfileLocation.L
 
 declare namespace vfileLocation {
     interface Location {
+        /**
+         * Get the `offset` (`number`) for a line and column-based `position` in the bound file.
+         * Returns `-1` when given invalid or out of bounds input.
+         */
         toOffset(position: { line: number; column: number }): number;
+        /**
+         * Get the line and column-based `position` for `offset` in the bound file.
+         */
         toPosition(offset: number): { line: number; column: number; offset: number };
     }
 }
