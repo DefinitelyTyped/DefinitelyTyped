f97a98472de5004d60cf787db2714e2da2c554ef
Jan Aagaard <jan@aagaard.net>
2018-01-22 20:24:48 +0100

    Move Asset to alphabetical position

--- types/expo/index.d.ts
+++ types/expo/index.d.ts
@@ -119,6 +119,55 @@ export type AppLoadingProps = {
 export class AppLoading extends Component<AppLoadingProps> { }
 // #endregion AppLoading
 
+/** This module provides an interface to Expo’s asset system. An asset is any file that lives alongside the source code of your app that the app needs at runtime. Examples include images, fonts and sounds. Expo’s asset system integrates with React Native’s, so that you can refer to files with require('path/to/file'). This is how you refer to static image files in React Native for use in an Image component, for example. */
+export class Asset {
+    constructor({ name, type, hash, uri, width, height }: {
+        name: string;
+        type: string;
+        hash: string;
+        uri: string;
+        width?: number;
+        height?: number;
+    });
+
+    /** The MD5 hash of the asset’s data. */
+    hash: Md5;
+
+    /** The name of the asset file without the extension. Also without the part from @ onward in the filename (used to specify scale factor for images). */
+    name: string;
+
+    /** The extension of the asset filename. */
+    type: string;
+
+    /** A URI that points to the asset’s data on the remote server. When running the published version of your app, this refers to the the location on Expo’s asset server where Expo has stored your asset. When running the app from XDE during development, this URI points to XDE’s server running on your computer and the asset is served directly from your computer. */
+    uri: string;
+
+    /** If the asset has been downloaded (by calling `downloadAsync()`), the `file://` URI pointing to the local file on the device that contains the asset data. */
+    localUri: string;
+
+    /** If the asset is an image, the width of the image data divided by the scale factor. The scale factor is the number after `@` in the filename, or `1` if not present. */
+    width?: number;
+
+    /** If the asset is an image, the height of the image data divided by the scale factor. The scale factor is the number after `@` in the filename, or `1` if not present. */
+    height?: number;
+
+    downloading: boolean;
+    downloaded: boolean;
+    downloadCallbacks: Array<{ resolve: () => any, reject: (e?: any) => any }>;
+
+    /** Downloads the asset data to a local file in the device’s cache directory. Once the returned promise is fulfilled without error, the localUri field of this asset points to a local file containing the asset data. The asset is only downloaded if an up-to-date local file for the asset isn’t already present due to an earlier download. */
+    downloadAsync(): Promise<void>;
+
+    /** Returns the `Expo.Asset` instance representing an asset given its module. */
+    static fromModule(module: RequireSource): Asset;
+
+    /**
+     * A helper that wraps `Expo.Asset.fromModule(module).downloadAsync` for convenience.
+     * @param moduleIds An array of `require('path/to/file')`. Can also be just one module without an Array.
+     */
+    static loadAsync(module: RequireSource[] | RequireSource): Promise<void>;
+}
+
 /**
  * Provides basic sample playback and recording.
  *
@@ -362,55 +411,6 @@ export namespace Audio {
             onPlaybackStatusUpdate?: ((status: PlaybackStatus) => void) | null
         ): Promise<{ sound: Sound, status: PlaybackStatus }>;
     }
-}
-
-/** This module provides an interface to Expo’s asset system. An asset is any file that lives alongside the source code of your app that the app needs at runtime. Examples include images, fonts and sounds. Expo’s asset system integrates with React Native’s, so that you can refer to files with require('path/to/file'). This is how you refer to static image files in React Native for use in an Image component, for example. */
-export class Asset {
-    constructor({ name, type, hash, uri, width, height }: {
-        name: string;
-        type: string;
-        hash: string;
-        uri: string;
-        width?: number;
-        height?: number;
-    });
-
-    /** The MD5 hash of the asset’s data. */
-    hash: Md5;
-
-    /** The name of the asset file without the extension. Also without the part from @ onward in the filename (used to specify scale factor for images). */
-    name: string;
-
-    /** The extension of the asset filename. */
-    type: string;
-
-    /** A URI that points to the asset’s data on the remote server. When running the published version of your app, this refers to the the location on Expo’s asset server where Expo has stored your asset. When running the app from XDE during development, this URI points to XDE’s server running on your computer and the asset is served directly from your computer. */
-    uri: string;
-
-    /** If the asset has been downloaded (by calling `downloadAsync()`), the `file://` URI pointing to the local file on the device that contains the asset data. */
-    localUri: string;
-
-    /** If the asset is an image, the width of the image data divided by the scale factor. The scale factor is the number after `@` in the filename, or `1` if not present. */
-    width?: number;
-
-    /** If the asset is an image, the height of the image data divided by the scale factor. The scale factor is the number after `@` in the filename, or `1` if not present. */
-    height?: number;
-
-    downloading: boolean;
-    downloaded: boolean;
-    downloadCallbacks: Array<{ resolve: () => any, reject: (e?: any) => any }>;
-
-    /** Downloads the asset data to a local file in the device’s cache directory. Once the returned promise is fulfilled without error, the localUri field of this asset points to a local file containing the asset data. The asset is only downloaded if an up-to-date local file for the asset isn’t already present due to an earlier download. */
-    downloadAsync(): Promise<void>;
-
-    /** Returns the `Expo.Asset` instance representing an asset given its module. */
-    static fromModule(module: RequireSource): Asset;
-
-    /**
-     * A helper that wraps `Expo.Asset.fromModule(module).downloadAsync` for convenience.
-     * @param moduleIds An array of `require('path/to/file')`. Can also be just one module without an Array.
-     */
-    static loadAsync(module: RequireSource[] | RequireSource): Promise<void>;
 }
 
 /**
