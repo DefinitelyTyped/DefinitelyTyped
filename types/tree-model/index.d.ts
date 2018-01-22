d076b076f0684310bc5c7d3831bea0575dc781b4
Abhas Bhattacharya <abhas@fusioncharts.com>
2018-01-22 08:43:52 +0530

    remove single module declaration in tree-model

--- types/tree-model/index.d.ts
+++ types/tree-model/index.d.ts
@@ -1,4 +1,4 @@
-// Type definitions for tree-model 1.0.6
+// Type definitions for tree-model 1.0
 // Project: https://github.com/joaonuno/tree-model-js
 // Definitions by: Abhas Bhattacharya <https://github.com/bendtherules>
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
@@ -6,74 +6,72 @@
 /*~ This declaration specifies that the class constructor function
  *~ is the exported object from the file
  */
-declare module "tree-model" {
-    export = TreeModel;
+export = TreeModel;
 
-    class TreeModel {
-        constructor(config?: TreeModelMisc.Config);
+declare class TreeModel {
+    constructor(config?: TreeModelMisc.Config);
 
-        private config: TreeModelMisc.Config;
+    private config: TreeModelMisc.Config;
 
-        parse(model: TreeModelMisc.Model): TreeModelMisc.Node;
+    parse(model: TreeModelMisc.Model): TreeModelMisc.Node;
 
+}
+
+declare namespace TreeModelMisc {
+
+    class Node {
+        constructor(config: any, model: TreeModelMisc.Model);
+
+        isRoot(): boolean;
+        hasChildren(): boolean;
+        addChild(child: Node): Node;
+        addChildAtIndex(child: Node, index: number): Node;
+        setIndex(index: number): Node;
+        getPath(): Array<Node>;
+        getIndex(): number;
+
+        walk(options: TreeModelMisc.Options, fn: TreeModelMisc.NodeVisitorFunction, ctx: Object): void;
+        walk(options: TreeModelMisc.Options, fn: TreeModelMisc.NodeVisitorFunction): void;
+        walk(fn: TreeModelMisc.NodeVisitorFunction, ctx?: Object): void;
+
+        all(options: TreeModelMisc.Options, fn: TreeModelMisc.NodeVisitorFunction, ctx: Object): Array<Node>;
+        all(options: TreeModelMisc.Options, fn: TreeModelMisc.NodeVisitorFunction): Array<Node>;
+        all(fn: TreeModelMisc.NodeVisitorFunction, ctx?: Object): Array<Node>;
+
+        first(options: TreeModelMisc.Options, fn: TreeModelMisc.NodeVisitorFunction, ctx: Object): Node | undefined;
+        first(options: TreeModelMisc.Options, fn: TreeModelMisc.NodeVisitorFunction): Node | undefined;
+        first(fn: TreeModelMisc.NodeVisitorFunction, ctx?: Object): Node | undefined;
+
+        drop(): Node;
+
+        [propName: string]: any;
     }
 
-    namespace TreeModelMisc {
+    interface Config {
+        /**
+         * The name for the children array property. Default is "children".
+         */
+        childrenPropertyName?: string;
+        modelComparatorFn?: ComparatorFunction;
+        [propName: string]: any;
+    }
 
-        class Node {
-            constructor(config: any, model: TreeModelMisc.Model);
+    interface Options {
+        strategy: StrategyName;
+    }
 
-            isRoot(): boolean;
-            hasChildren(): boolean;
-            addChild(child: Node): Node;
-            addChildAtIndex(child: Node, index: number): Node;
-            setIndex(index: number): Node;
-            getPath(): Array<Node>;
-            getIndex(): number;
+    type StrategyName = "pre" | "post" | "breadth";
 
-            walk(options: TreeModelMisc.Options, fn: TreeModelMisc.NodeVisitorFunction, ctx: Object): void;
-            walk(options: TreeModelMisc.Options, fn: TreeModelMisc.NodeVisitorFunction): void;
-            walk(fn: TreeModelMisc.NodeVisitorFunction, ctx?: Object): void;
+    type ComparatorFunction = (left: any, right: any) => boolean;
+    type NodeVisitorFunction = (visitingNode: Node) => boolean;
 
-            all(options: TreeModelMisc.Options, fn: TreeModelMisc.NodeVisitorFunction, ctx: Object): Array<Node>;
-            all(options: TreeModelMisc.Options, fn: TreeModelMisc.NodeVisitorFunction): Array<Node>;
-            all(fn: TreeModelMisc.NodeVisitorFunction, ctx?: Object): Array<Node>;
-
-            first(options: TreeModelMisc.Options, fn: TreeModelMisc.NodeVisitorFunction, ctx: Object): Node | undefined;
-            first(options: TreeModelMisc.Options, fn: TreeModelMisc.NodeVisitorFunction): Node | undefined;
-            first(fn: TreeModelMisc.NodeVisitorFunction, ctx?: Object): Node | undefined;
-
-            drop(): Node;
-
-            [propName: string]: any;
-        }
-
-        interface Config {
-            /**
-             * The name for the children array property. Default is "children".
-             */
-            childrenPropertyName?: string;
-            modelComparatorFn?: ComparatorFunction;
-            [propName: string]: any;
-        }
-
-        interface Options {
-            strategy: StrategyName;
-        }
-
-        type StrategyName = "pre" | "post" | "breadth";
-
-        type ComparatorFunction = (left: any, right: any) => boolean;
-        type NodeVisitorFunction = (visitingNode: Node) => boolean;
-
-        interface Model {
-            /**
-             * Children array property name is set from `config.childrenPropertyName`(default: "children")
-             *
-             * config is passed in `TreeModel` constructor
-             */
-            children?: Array<Model>;
-            [propName: string]: any;
-        }
+    interface Model {
+        /**
+         * Children array property name is set from `config.childrenPropertyName`(default: "children")
+         *
+         * config is passed in `TreeModel` constructor
+         */
+        children?: Array<Model>;
+        [propName: string]: any;
     }
 }
