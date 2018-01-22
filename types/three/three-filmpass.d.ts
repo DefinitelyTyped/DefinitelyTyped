941241500de5d3cf04ba4d46eaa31457a31033ae
Apurva Ojas <aojas@sapient.com>
2018-01-22 11:47:21 +0530

    chore(THREE.FilmPass): Definitions added of variables and functions
    #14042

--- types/three/three-filmpass.d.ts
+++ types/three/three-filmpass.d.ts
@@ -1,4 +1,14 @@
+import { Camera, Scene, WebGLRenderTarget, WebGLRenderer, Material, Mesh, IUniform } from "./three-core";
+
 export class FilmPass  {
     constructor(noiseIntensity: number, scanlinesIntensity: number, scanlinesCount: number, grayscale: boolean);
+    
     renderToScreen: boolean;
+    scene: Scene;
+    camera: Camera;
+    uniforms: IUniform;
+    material: Material;
+    quad: Mesh;
+
+    render(renderer: WebGLRenderer, writeBuffer: WebGLRenderTarget, readBuffer: WebGLRenderTarget, delta: number, maskActive: boolean): void;
 }
