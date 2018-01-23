d07c7a6a46b4eb7cf05c0b542349929424bb8ac0
Ivan Goncharov <ivan.goncharov.ua@gmail.com>
2018-01-23 23:27:35 +0200

    [graphql] Reformat all files using prettier.
    See https://github.com/graphql/graphql-js/pull/1112

--- types/graphql/type/introspection.d.ts
+++ types/graphql/type/introspection.d.ts
@@ -1,12 +1,12 @@
 import {
-    GraphQLScalarType,
-    GraphQLObjectType,
-    GraphQLInterfaceType,
-    GraphQLUnionType,
-    GraphQLEnumType,
-    GraphQLInputObjectType,
-    GraphQLList,
-    GraphQLNonNull,
+  GraphQLScalarType,
+  GraphQLObjectType,
+  GraphQLInterfaceType,
+  GraphQLUnionType,
+  GraphQLEnumType,
+  GraphQLInputObjectType,
+  GraphQLList,
+  GraphQLNonNull,
 } from './definition';
 import { GraphQLField } from './definition';
 
@@ -19,14 +19,14 @@ export const __InputValue: GraphQLObjectType;
 export const __EnumValue: GraphQLObjectType;
 
 export const TypeKind: {
-    SCALAR: 'SCALAR',
-    OBJECT: 'OBJECT',
-    INTERFACE: 'INTERFACE',
-    UNION: 'UNION',
-    ENUM: 'ENUM',
-    INPUT_OBJECT: 'INPUT_OBJECT',
-    LIST: 'LIST',
-    NON_NULL: 'NON_NULL',
+  SCALAR: 'SCALAR';
+  OBJECT: 'OBJECT';
+  INTERFACE: 'INTERFACE';
+  UNION: 'UNION';
+  ENUM: 'ENUM';
+  INPUT_OBJECT: 'INPUT_OBJECT';
+  LIST: 'LIST';
+  NON_NULL: 'NON_NULL';
 };
 
 export const __TypeKind: GraphQLEnumType;
