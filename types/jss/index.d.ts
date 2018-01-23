3310f73f729ce2dff583be9c85dfeb6c9dc3ea76
Tom Crockett <pelotom@gmail.com>
2018-01-23 08:56:53 -0800

    Bump version number, add author

--- types/jss/index.d.ts
+++ types/jss/index.d.ts
@@ -1,7 +1,8 @@
-// Type definitions for jss 9.3
+// Type definitions for jss 9.5
 // Project: https://github.com/cssinjs/jss#readme
 // Definitions by: Brenton Simpson <https://github.com/appsforartists>
 //                 Oleg Slobodskoi <https://github.com/kof>
+//                 Thomas Crockett <https://github.com/pelotom>
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
 // TypeScript Version: 2.3
 
