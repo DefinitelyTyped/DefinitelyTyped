a6007ebb59079c9a67709b5597efb8110527055e
Apurva Ojas <aojas@sapient.com>
2018-01-23 15:42:59 +0530

    feat(THREE.Pass/THREE.FilmPass): Added Definitions for THREE.Pass and Extend it on THREE.FilmPass
    #14042

--- types/three/three-filmpass.d.ts
+++ types/three/three-filmpass.d.ts
@@ -1,14 +1,14 @@
 import { Camera, Scene, WebGLRenderTarget, WebGLRenderer, Material, Mesh, IUniform } from "./three-core";
+import {Pass} from "./three-effectcomposer";
 
-export class FilmPass  {
+export class FilmPass extends Pass {
     constructor(noiseIntensity: number, scanlinesIntensity: number, scanlinesCount: number, grayscale: boolean);
     
-    renderToScreen: boolean;
     scene: Scene;
     camera: Camera;
     uniforms: IUniform;
     material: Material;
     quad: Mesh;
 
-    render(renderer: WebGLRenderer, writeBuffer: WebGLRenderTarget, readBuffer: WebGLRenderTarget, delta: number, maskActive: boolean): void;
+   render(renderer: WebGLRenderer, writeBuffer: WebGLRenderTarget, readBuffer: WebGLRenderTarget, delta: number, maskActive?: boolean): void;
 }
