4e4bb2ed709fb7b0ea3a451b0df45c6ab76267f5
Ivan Goncharov <ivan.goncharov.ua@gmail.com>
2018-01-24 19:00:08 +0200

    [graphql] Reformat all files using prettier. (#23142)
    See https://github.com/graphql/graphql-js/pull/1112

--- types/graphql/utilities/TypeInfo.d.ts
+++ types/graphql/utilities/TypeInfo.d.ts
@@ -1,12 +1,12 @@
 import { GraphQLSchema } from '../type/schema';
 import {
-    GraphQLOutputType,
-    GraphQLCompositeType,
-    GraphQLInputType,
-    GraphQLField,
-    GraphQLArgument,
-    GraphQLEnumValue,
-    GraphQLType,
+  GraphQLOutputType,
+  GraphQLCompositeType,
+  GraphQLInputType,
+  GraphQLField,
+  GraphQLArgument,
+  GraphQLEnumValue,
+  GraphQLType,
 } from '../type/definition';
 import { GraphQLDirective } from '../type/directives';
 import { ASTNode, FieldNode } from '../language/ast';
@@ -17,27 +17,27 @@ import { ASTNode, FieldNode } from '../language/ast';
  * AST during a recursive descent by calling `enter(node)` and `leave(node)`.
  */
 export class TypeInfo {
-    constructor(
-        schema: GraphQLSchema,
-        // NOTE: this experimental optional second parameter is only needed in order
-        // to support non-spec-compliant codebases. You should never need to use it.
-        // It may disappear in the future.
-        getFieldDefFn?: getFieldDef
-    );
+  constructor(
+    schema: GraphQLSchema,
+    // NOTE: this experimental optional second parameter is only needed in order
+    // to support non-spec-compliant codebases. You should never need to use it.
+    // It may disappear in the future.
+    getFieldDefFn?: getFieldDef,
+  );
 
-    getType(): GraphQLOutputType;
-    getParentType(): GraphQLCompositeType;
-    getInputType(): GraphQLInputType;
-    getFieldDef(): GraphQLField<any, any>;
-    getDirective(): GraphQLDirective;
-    getArgument(): GraphQLArgument;
-    getEnumValue(): GraphQLEnumValue;
-    enter(node: ASTNode): any;
-    leave(node: ASTNode): any;
+  getType(): GraphQLOutputType;
+  getParentType(): GraphQLCompositeType;
+  getInputType(): GraphQLInputType;
+  getFieldDef(): GraphQLField<any, any>;
+  getDirective(): GraphQLDirective;
+  getArgument(): GraphQLArgument;
+  getEnumValue(): GraphQLEnumValue;
+  enter(node: ASTNode): any;
+  leave(node: ASTNode): any;
 }
 
 export type getFieldDef = (
-    schema: GraphQLSchema,
-    parentType: GraphQLType,
-    fieldNode: FieldNode,
+  schema: GraphQLSchema,
+  parentType: GraphQLType,
+  fieldNode: FieldNode,
 ) => GraphQLField<any, any>;
--- types/graphql/utilities/findBreakingChanges.d.ts
+++ types/graphql/utilities/findBreakingChanges.d.ts
@@ -1,35 +1,35 @@
 import {
-    getNamedType,
-    GraphQLScalarType,
-    GraphQLEnumType,
-    GraphQLInputObjectType,
-    GraphQLInterfaceType,
-    GraphQLObjectType,
-    GraphQLUnionType,
-    GraphQLNamedType,
+  getNamedType,
+  GraphQLScalarType,
+  GraphQLEnumType,
+  GraphQLInputObjectType,
+  GraphQLInterfaceType,
+  GraphQLObjectType,
+  GraphQLUnionType,
+  GraphQLNamedType,
 } from '../type/definition';
 import { GraphQLSchema } from '../type/schema';
 
 export const BreakingChangeType: {
-    FIELD_CHANGED_KIND: 'FIELD_CHANGED_KIND',
-    FIELD_REMOVED: 'FIELD_REMOVED',
-    TYPE_CHANGED_KIND: 'TYPE_CHANGED_KIND',
-    TYPE_REMOVED: 'TYPE_REMOVED',
-    TYPE_REMOVED_FROM_UNION: 'TYPE_REMOVED_FROM_UNION',
-    VALUE_REMOVED_FROM_ENUM: 'VALUE_REMOVED_FROM_ENUM',
+  FIELD_CHANGED_KIND: 'FIELD_CHANGED_KIND';
+  FIELD_REMOVED: 'FIELD_REMOVED';
+  TYPE_CHANGED_KIND: 'TYPE_CHANGED_KIND';
+  TYPE_REMOVED: 'TYPE_REMOVED';
+  TYPE_REMOVED_FROM_UNION: 'TYPE_REMOVED_FROM_UNION';
+  VALUE_REMOVED_FROM_ENUM: 'VALUE_REMOVED_FROM_ENUM';
 };
 
 export type BreakingChangeKey =
-    | 'FIELD_CHANGED_KIND'
-    | 'FIELD_REMOVED'
-    | 'TYPE_CHANGED_KIND'
-    | 'TYPE_REMOVED'
-    | 'TYPE_REMOVED_FROM_UNION'
-    | 'VALUE_REMOVED_FROM_ENUM';
+  | 'FIELD_CHANGED_KIND'
+  | 'FIELD_REMOVED'
+  | 'TYPE_CHANGED_KIND'
+  | 'TYPE_REMOVED'
+  | 'TYPE_REMOVED_FROM_UNION'
+  | 'VALUE_REMOVED_FROM_ENUM';
 
 export interface BreakingChange {
-    type: BreakingChangeKey;
-    description: string;
+  type: BreakingChangeKey;
+  description: string;
 }
 
 /**
@@ -37,8 +37,8 @@ export interface BreakingChange {
  * of breaking changes covered by the other functions down below.
  */
 export function findBreakingChanges(
-    oldSchema: GraphQLSchema,
-    newSchema: GraphQLSchema
+  oldSchema: GraphQLSchema,
+  newSchema: GraphQLSchema,
 ): BreakingChange[];
 
 /**
@@ -46,8 +46,8 @@ export function findBreakingChanges(
  * changes in the newSchema related to removing an entire type.
  */
 export function findRemovedTypes(
-    oldSchema: GraphQLSchema,
-    newSchema: GraphQLSchema
+  oldSchema: GraphQLSchema,
+  newSchema: GraphQLSchema,
 ): BreakingChange[];
 
 /**
@@ -55,8 +55,8 @@ export function findRemovedTypes(
  * changes in the newSchema related to changing the type of a type.
  */
 export function findTypesThatChangedKind(
-    oldSchema: GraphQLSchema,
-    newSchema: GraphQLSchema
+  oldSchema: GraphQLSchema,
+  newSchema: GraphQLSchema,
 ): BreakingChange[];
 
 /**
@@ -65,8 +65,8 @@ export function findTypesThatChangedKind(
  * a field has been removed from a type or if a field has changed type.
  */
 export function findFieldsThatChangedType(
-    oldSchema: GraphQLSchema,
-    newSchema: GraphQLSchema
+  oldSchema: GraphQLSchema,
+  newSchema: GraphQLSchema,
 ): BreakingChange[];
 
 /**
@@ -74,8 +74,8 @@ export function findFieldsThatChangedType(
  * changes in the newSchema related to removing types from a union type.
  */
 export function findTypesRemovedFromUnions(
-    oldSchema: GraphQLSchema,
-    newSchema: GraphQLSchema
+  oldSchema: GraphQLSchema,
+  newSchema: GraphQLSchema,
 ): BreakingChange[];
 
 /**
@@ -83,6 +83,6 @@ export function findTypesRemovedFromUnions(
  * changes in the newSchema related to removing values from an enum type.
  */
 export function findValuesRemovedFromEnums(
-    oldSchema: GraphQLSchema,
-    newSchema: GraphQLSchema
+  oldSchema: GraphQLSchema,
+  newSchema: GraphQLSchema,
 ): BreakingChange[];
