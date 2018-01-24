3154b51d76ad91f29ef44750c9df6f352405cfc4
Donkey-Kurt <381890216@qq.com>
2018-01-24 10:47:40 +0800

    add missing props for android webview
    adding    mixedContentMode?: "never" | "always" | "compatibility";
    
    http://facebook.github.io/react-native/docs/webview.html#mixedcontentmode

--- types/react-native/index.d.ts
+++ types/react-native/index.d.ts
@@ -1945,6 +1945,15 @@ export interface WebViewPropertiesAndroid {
      * Sets the user-agent for the WebView.
      */
     userAgent?: string;
+    
+    /**
+    * Specifies the mixed content mode. i.e WebView will allow a secure origin to load content from any other origin.
+Possible values for mixedContentMode are:
+'never' (default) - WebView will not allow a secure origin to load content from an insecure origin.
+'always' - WebView will allow a secure origin to load content from any other origin, even if that origin is insecure.
+'compatibility' - WebView will attempt to be compatible with the approach of a modern web browser with regard to mixed content.
+    */
+    mixedContentMode?: "never" | "always" | "compatibility";
 }
 
 export interface WebViewIOSLoadRequestEvent {
