11c89fe9b4742d79c86793231b509654523078dd
Eugene Rodin <eugene.rodin1@gmail.com>
2018-01-24 19:11:50 +0300

    feat(ReactDynamicNumber): fix validation and linter

--- types/react-dynamic-number/index.d.ts
+++ types/react-dynamic-number/index.d.ts
@@ -2,6 +2,7 @@
 // Project: https://github.com/uhlryk/react-dynamic-number
 // Definitions by: Eugene Rodin <https://github.com/eugrdn>
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
+// TypeScript Version: 2.6
 
 import * as React from 'react';
 
