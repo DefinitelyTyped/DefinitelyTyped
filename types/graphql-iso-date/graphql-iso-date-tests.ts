6b7b6d8873dda2fb799a7da0d675fe9aeda532d2
jwaldrip <jason@waldrip.net>
2018-01-25 16:03:18 -0700

    add graphql-iso-date types

---
+++ types/graphql-iso-date/graphql-iso-date-tests.ts
@@ -0,0 +1,7 @@
+import { GraphQLDate, GraphQLTime, GraphQLDateTime } from "graphql-iso-date";
+
+namespace graphQLIsoDateTests {
+    GraphQLDate.name;
+    GraphQLTime.name;
+    GraphQLDateTime.name;
+}
--- types/graphql-iso-date/index.d.ts
+++ types/graphql-iso-date/index.d.ts
@@ -1,3 +1,10 @@
+// Type definitions for graphql-iso-date 3.3
+// Project: https://github.com/excitement-engineer/graphql-iso-date
+// Definitions by: Jason Waldrip <https://github.com/jwaldrip>
+// Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
+
+/// <reference types="graphql" />
+
 import { GraphQLScalarType } from "graphql";
 
 export const GraphQLDate: GraphQLScalarType;
