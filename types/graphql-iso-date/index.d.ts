d730e6ec31a4c5d6967e1a6a3e93e64db0b3455c
Jason Waldrip <jason@waldrip.net>
2018-01-25 15:12:54 -0700

    Create index.d.ts

---
+++ types/graphql-iso-date/index.d.ts
@@ -0,0 +1,10 @@
+	import { GraphQLScalarType } from "graphql";
+
+	export interface GraphQLDate extends GraphQLScalarType {
+	}
+
+	export interface GraphQLTime extends GraphQLScalarType {
+	}
+
+	export interface GraphQLDateTime extends GraphQLScalarType {
+	}
