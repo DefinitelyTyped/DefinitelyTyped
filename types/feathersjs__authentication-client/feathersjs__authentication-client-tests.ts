d0ffe9554c93ff47bd08e3f1b69d3d1667b0c968
Jan Lohage <git@j2L4e.de>
2018-01-25 17:38:16 +0100

    fix feathersjs__authentication-client

--- types/feathersjs__authentication-client/feathersjs__authentication-client-tests.ts
+++ types/feathersjs__authentication-client/feathersjs__authentication-client-tests.ts
@@ -5,3 +5,7 @@ const app = feathers();
 app.configure(feathersAuthClient());
 app.authenticate({strategy : 'abcdef'}).then(() => {});
 app.logout().then(() => {});
+
+// check if the non-augmented @feathersjs/feathers typings still work
+app.on('asd', () => {});
+app.service('asd').get(0).then(() => {});
