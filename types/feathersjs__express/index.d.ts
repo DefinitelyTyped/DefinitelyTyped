9dec8df9643274f62cf4369978edd5bdd229145a
Jan Lohage <j2L4e@users.noreply.github.com>
2018-01-25 22:41:41 +0100

    fix(@feathersjs/various): proper module augmentation, add reexports (#23195)
    * fix feathersjs__{authentication-client,express,socket-commons}
    
    * fix feathersjs__authentication-client

--- types/feathersjs__express/index.d.ts
+++ types/feathersjs__express/index.d.ts
@@ -5,6 +5,18 @@
 // TypeScript Version: 2.2
 
 import { Application as FeathersApplication } from '@feathersjs/feathers';
-import { Application as ExpressApplication } from 'express';
+import * as express from 'express';
 
-export default function feathersExpress<T>(app: FeathersApplication<T>): ExpressApplication & FeathersApplication<T>;
+export default function feathersExpress<T>(app: FeathersApplication<T>): express.Application & FeathersApplication<T>;
+
+export function errorHandler(options?: any): express.ErrorRequestHandler;
+export function notFound(): express.RequestHandler;
+export const rest: {
+    (): () => void;
+    formatter: express.RequestHandler;
+};
+
+/*
+ * Re-export of the express package. Can't be typed properly without just copying everything.
+ **/
+export const original: any;
