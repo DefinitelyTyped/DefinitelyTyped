3094dfd69132a3c7085ce23cf98b80a2e122445e
Sergei Dorogin <sergei@dorogin.com>
2018-01-25 19:10:05 +0300

    waypoints: fixes: waypoints-tests.ts: fixed prefer-const  Identifier 'waypoint1' is never reassigned; use 'const' instead of 'let'.

--- types/waypoints/waypoints-tests.ts
+++ types/waypoints/waypoints-tests.ts
@@ -4,7 +4,7 @@ declare function notify(text: string): void;
 
 // A Basic Waypoint
 // --------------------------------------------------------------------------------------------------------------------
-let waypoint1 = new Waypoint({
+const waypoint1 = new Waypoint({
     element: document.getElementById('basic-waypoint')!,
     handler() {
         notify('Basic waypoint triggered');
@@ -13,7 +13,7 @@ let waypoint1 = new Waypoint({
 
 // Directions
 // --------------------------------------------------------------------------------------------------------------------
-let waypoint2 = new Waypoint({
+const waypoint2 = new Waypoint({
     element: document.getElementById('direction-waypoint')!,
     handler(direction) {
         notify('Direction: ' + direction);
@@ -22,7 +22,7 @@ let waypoint2 = new Waypoint({
 
 // Offsets
 // --------------------------------------------------------------------------------------------------------------------
-let waypoint3 = new Waypoint({
+const waypoint3 = new Waypoint({
     element: document.getElementById('px-offset-waypoint')!,
     handler(direction) {
         notify('I am 20px from the top of the window');
@@ -32,7 +32,7 @@ let waypoint3 = new Waypoint({
 
 // this?
 // --------------------------------------------------------------------------------------------------------------------
-let waypoint4 = new Waypoint({
+const waypoint4 = new Waypoint({
     element: document.getElementById('element-waypoint')!,
     handler(direction) {
         notify(`${this.element.id} triggers at ${this.triggerPoint}`);
@@ -48,26 +48,26 @@ import $ = require('jquery');
 // $.fn.waypoint
 // --------------------------------------------------------------------------------------------------------------------
 
-let waypoints10 = $('#options-only').waypoint({
+const waypoints10 = $('#options-only').waypoint({
   handler: function fn(this: Waypoint, direction?: string) {
     notify(this.element.id + ' hit');
   }
 });
 
-let waypoints11 = $('#handler-first').waypoint(function() {
+const waypoints11 = $('#handler-first').waypoint(function() {
   notify(this.element.id + ' hit 25% from top of window');
 }, {
   offset: '25%'
 });
 
-let waypoints12 = $('#handler-only').waypoint(function() {
+const waypoints12 = $('#handler-only').waypoint(function() {
   notify(this.element.id + ' hit');
 });
 
 // Context Option
 // --------------------------------------------------------------------------------------------------------------------
 
-let waypoints13 = $('#context-example-offset').waypoint({
+const waypoints13 = $('#context-example-offset').waypoint({
   handler: function fn(this: Waypoint, direction?: string) {
     notify('Hit midpoint of my context');
   },
