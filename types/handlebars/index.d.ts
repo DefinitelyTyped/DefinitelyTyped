819cb2884be1d1424e52c06282ea86b075fdb7bd
Sergei Dorogin <sergei@dorogin.com>
2018-01-26 15:23:19 +0300

    Handlebars: updated authors, bumped up version (to the latest 4.0.11)

--- types/handlebars/index.d.ts
+++ types/handlebars/index.d.ts
@@ -1,6 +1,6 @@
-// Type definitions for Handlebars v4.0.5
+// Type definitions for Handlebars v4.0.11
 // Project: http://handlebarsjs.com/
-// Definitions by: Boris Yankov <https://github.com/borisyankov>
+// Definitions by: Boris Yankov <https://github.com/borisyankov>, Sergei Dorogin <https://github.com/evil-shrike>
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
 // TypeScript Version: 2.3
 
