6971c1148447a0c6d050e9585ada203819ae752a
Alexandr Evtushenko <aevtushenko@xiag.ch>
2018-01-26 09:32:24 +0700

    Add `react-overlays/Overlay` component test.

--- types/react-overlays/test/react-overlays-tests-individual.tsx
+++ types/react-overlays/test/react-overlays-tests-individual.tsx
@@ -9,6 +9,8 @@ import * as Affix from 'react-overlays/lib/Affix';
 import * as AutoAffix from 'react-overlays/lib/AutoAffix';
 import * as RootCloseWrapper from 'react-overlays/lib/RootCloseWrapper';
 
+import { OverlayFade } from './react-overlays-tests-transition';
+
 function testTransition() {
   return (
     <Transition
@@ -31,6 +33,32 @@ class TestAffix extends React.Component {
           <div>Test</div>
         </AutoAffix>
       </div>
+    );
+  }
+}
+
+class TestOverlay extends React.Component<{}, {open: boolean}> {
+  target: HTMLElement | null = null;
+  state = {open: false};
+
+  render(): JSX.Element {
+    const { open } = this.state;
+
+    return (
+        <div>
+            <button type="button"
+                    ref={(ref) => this.target = ref}
+                    onClick={() => this.setState({open: !open})}>Click me</button>
+
+            <Overlay show={open}
+                     rootClose={true}
+                     target={() => this.target}
+                     transition={OverlayFade}
+                     onHide={() => this.setState({open: false})}
+                     placement="bottom">
+                <div>Popover content</div>
+            </Overlay>
+        </div>
     );
   }
 }
--- types/react-overlays/test/react-overlays-tests.tsx
+++ types/react-overlays/test/react-overlays-tests.tsx
@@ -11,6 +11,8 @@ import {
   RootCloseWrapper,
 } from "react-overlays";
 
+import { OverlayFade } from './react-overlays-tests-transition';
+
 function testTransition() {
   return (
     <Transition
@@ -33,6 +35,32 @@ class TestAffix extends React.Component {
           <div>Test</div>
         </AutoAffix>
       </div>
+    );
+  }
+}
+
+class TestOverlay extends React.Component<{}, {open: boolean}> {
+  target: HTMLElement | null = null;
+  state = {open: false};
+
+  render(): JSX.Element {
+    const { open } = this.state;
+
+    return (
+        <div>
+            <button type="button"
+                    ref={(ref) => this.target = ref}
+                    onClick={() => this.setState({open: !open})}>Click me</button>
+
+            <Overlay show={open}
+                     rootClose={true}
+                     target={() => this.target}
+                     transition={OverlayFade}
+                     onHide={() => this.setState({open: false})}
+                     placement="bottom">
+                <div>Popover content</div>
+            </Overlay>
+        </div>
     );
   }
 }
