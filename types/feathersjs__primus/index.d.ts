9929e7486b7f5abdb8e405730198da430e7b078f
Jan Lohage <git@j2L4e.de>
2018-01-27 20:34:11 +0100

    [@feathersjs] add @feathersjs/socket-commons import to @feathersjs/socketio and @feathersjs/primus packages

--- types/feathersjs__primus/index.d.ts
+++ types/feathersjs__primus/index.d.ts
@@ -6,5 +6,7 @@
 
 /// <reference types="feathersjs__socket-commons"/>
 
+/// <reference types="feathersjs__socket-commons"/>
+
 // primus removed its typings from the repo https://github.com/primus/primus/pull/623, as of 01/2018 there are none on DT
 export default function feathersPrimus(options: any, callback?: (primus: any) => void): () => void;
