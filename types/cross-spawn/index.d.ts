8aca86ac4bed6622d0de83c0541240bcab6b8655
Arturas Molcanovas <a.molcanovas@gmail.com>
2018-01-27 18:04:34 +0000

    Add sync methods

--- types/cross-spawn/index.d.ts
+++ types/cross-spawn/index.d.ts
@@ -5,9 +5,30 @@
 
 /// <reference types="node" />
 
-import * as child_process from 'child_process';
+import {
+    ChildProcess,
+    SpawnOptions,
+    SpawnSyncOptions,
+    SpawnSyncReturns,
+    SpawnSyncOptionsWithStringEncoding,
+    SpawnSyncOptionsWithBufferEncoding
+} from 'child_process';
 
-/** @inheritDoc */
-declare const spawn: typeof child_process.spawn;
+declare function spawn(command: string, args?: string[], options?: SpawnOptions): ChildProcess;
+
+// Disable unified-signatures to have sync methods act as a 1:1 mirror to child_process.sync types
+declare namespace spawn {
+    function sync(command: string): SpawnSyncReturns<Buffer>;
+    function sync(command: string, options?: SpawnSyncOptionsWithStringEncoding): SpawnSyncReturns<string>;
+    // tslint:disable-next-line:unified-signatures
+    function sync(command: string, options?: SpawnSyncOptionsWithBufferEncoding): SpawnSyncReturns<Buffer>;
+    // tslint:disable-next-line:unified-signatures
+    function sync(command: string, options?: SpawnSyncOptions): SpawnSyncReturns<Buffer>;
+    function sync(command: string, args?: string[], options?: SpawnSyncOptionsWithStringEncoding): SpawnSyncReturns<string>;
+    // tslint:disable-next-line:unified-signatures
+    function sync(command: string, args?: string[], options?: SpawnSyncOptionsWithBufferEncoding): SpawnSyncReturns<Buffer>;
+    // tslint:disable-next-line:unified-signatures
+    function sync(command: string, args?: string[], options?: SpawnSyncOptions): SpawnSyncReturns<Buffer>;
+}
 
 export = spawn;
