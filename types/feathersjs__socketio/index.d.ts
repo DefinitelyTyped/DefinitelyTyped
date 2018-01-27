de79942baec8e19a88638a4681f0188932c9c320
Jan Lohage <git@j2L4e.de>
2018-01-27 20:37:45 +0100

    [@feathersjs] fix ts version in @feathersjs/socketio and @feathersjs/primus packages

--- types/feathersjs__socketio/index.d.ts
+++ types/feathersjs__socketio/index.d.ts
@@ -2,6 +2,7 @@
 // Project: http://feathersjs.com/
 // Definitions by: Jan Lohage <https://github.com/j2L4e>
 // Definitions: https://github.com/feathersjs-ecosystem/feathers-typescript
+// TypeScript Version: 2.3
 
 /// <reference types="socket.io" />
 /// <reference types="feathersjs__socket-commons"/>
