6dff74817b385382c30bc13379dd592e0a88cad5
Arturas Molcanovas <a.molcanovas@gmail.com>
2018-01-27 19:10:00 +0000

    rm namespace comment

--- types/cross-spawn/index.d.ts
+++ types/cross-spawn/index.d.ts
@@ -9,7 +9,6 @@ import * as child_process from 'child_process';
 
 declare function spawn(command: string, args?: string[], options?: child_process.SpawnOptions): child_process.ChildProcess;
 
-// Disable unified-signatures to have sync methods act as a 1:1 mirror to child_process.sync types
 declare namespace spawn {
     const sync: typeof child_process.spawnSync;
 }
