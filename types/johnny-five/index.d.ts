8eb68162c8841a6ce483aff79d3766126cfd0f9c
Simon Colmer <simon@colmer.me>
2018-01-28 00:40:46 +0000

    Add Credits

--- types/johnny-five/index.d.ts
+++ types/johnny-five/index.d.ts
@@ -2,6 +2,7 @@
 // Project: https://github.com/rwaldron/johnny-five
 // Definitions by: Toshiya Nakakura <https://github.com/nakakura>
 //                 Zoltan Ujvary <https://github.com/ujvzolee>
+//                 Simon Colmer <https://github.com/workshop2>
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
 
 ///<reference types="node"/>
