2a858618c3f721512c52cbf8bbe943d8b7be4931
Hugues Stefanski <hugues.stefanski@gmail.com>
2018-01-29 20:44:25 +0100

    Backbone for d3-fetch

---
+++ types/d3-fetch/index.d.ts
@@ -0,0 +1,5 @@
+// Type definitions for d3-fetch 1.0.1
+// Project: https://d3js.org/d3-fetch/
+// Definitions by: Hugues Stefanski <https://github.com/ledragon>
+// Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
+
