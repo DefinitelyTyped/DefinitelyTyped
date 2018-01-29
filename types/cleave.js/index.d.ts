b4eccbc0cedf6371651f39a48f30d188a966e552
Alex Shakun <alx.safari@gmail.com>
2018-01-29 11:19:24 +0300

    bump version of type definitions for cleave.js after type adding

--- types/cleave.js/index.d.ts
+++ types/cleave.js/index.d.ts
@@ -1,4 +1,4 @@
-// Type definitions for cleave.js 1.0
+// Type definitions for cleave.js 1.1
 // Project: https://github.com/nosir/cleave.js
 // Definitions by: C Lentfort <https://github.com/clentfort>,
 //                 J Giancono <https://github.com/jasongi-at-sportsbet>,
