8bc67e1c1734f9b2210b3b81c1b7a471e119c635
Kevin Nacios <kevin.nacios@thehumangeo.com>
2018-01-29 16:51:46 -0500

    Add types for humanize-string

---
+++ types/humanize-string/index.d.ts
@@ -0,0 +1,9 @@
+// Type definitions for humanize-string 1.0
+// Project: https://github.com/sindresorhus/humanize-string/
+// Definitions by: Kevin Nacios <https://github.com/ragnarok56>
+// Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
+
+export = humanizeString;
+
+declare function humanizeString(text: string): string;
+declare namespace humanizeString {}
