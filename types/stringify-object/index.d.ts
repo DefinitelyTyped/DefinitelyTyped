6d71332915d4f206911909609a011da3e701b0c3
Emily Marigold Klassen <forivall@gmail.com>
2018-01-29 11:58:06 -0800

    fix: set typescript version to 2.2

--- types/stringify-object/index.d.ts
+++ types/stringify-object/index.d.ts
@@ -2,6 +2,7 @@
 // Project: https://github.com/yeoman/stringify-object
 // Definitions by: Chris Khoo <https://github.com/khoomeister>
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
+// TypeScript Version: 2.2
 
 declare namespace stringifyObject { }
 
