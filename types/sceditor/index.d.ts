aa94e464090bcf7df34e56b75d48151272c31539
Martin Pfund <m.pfund@mid.de>
2018-01-30 09:57:09 +0100

    Updated version

--- types/sceditor/index.d.ts
+++ types/sceditor/index.d.ts
@@ -1,4 +1,4 @@
-// Type definitions for SCEditor 2.1.2
+// Type definitions for SCEditor 2.1
 // Project: https://www.sceditor.com/
 // Definitions by: Martin Pfund
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
