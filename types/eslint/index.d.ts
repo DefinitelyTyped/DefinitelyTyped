bc35c5e8afc4d3c190d10ecf3635f21afbf11a27
Pierre-Marie <p.dartus@salesforce.com>
2018-01-30 21:55:15 -0800

    Add @j-f1 to the definition author list

--- types/eslint/index.d.ts
+++ types/eslint/index.d.ts
@@ -1,6 +1,7 @@
 // Type definitions for eslint 4.16
 // Project: https://eslint.org
 // Definitions by: Pierre-Marie Dartus <https://github.com/pmdartus>
+//                 Jed Fox <https://github.com/j-f1>
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
 // TypeScript Version: 2.2
 
