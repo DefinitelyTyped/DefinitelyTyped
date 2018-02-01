87e566c278cf8d283d2c3e2da204b33b8d8c7f7c
Windware Solution LLC <github@windware.com>
2018-02-01 20:32:04 +0900

    Error when importing into TypeScript with 'non-module entity' error.
    Fixes the error,
    
    >Module "./node_modules/@types/pad/index" resolves to a non-module entity and cannot be imported using this construct.
    
    using,
    
    > import * as pad from 'pad'

--- types/pad/index.d.ts
+++ types/pad/index.d.ts
@@ -1,6 +1,7 @@
-// Type definitions for pad 1.0
+// Type definitions for pad 1.1
 // Project: https://github.com/wdavidw/node-pad#readme
 // Definitions by: Mohamed Hegazy <https://github.com/mhegazy>
+//                 windware <https://github.com/windware>
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
 
 export = pad;
@@ -13,3 +14,5 @@ declare function pad(length: number, text: string, options?: { char?: string, co
 declare function pad(text: string, length: number, char?: string): string;
 // tslint:disable-next-line unified-signatures
 declare function pad(text: string, length: number, options?: { char?: string, colors?: boolean, strip?: boolean }): string;
+
+declare namespace pad {}
