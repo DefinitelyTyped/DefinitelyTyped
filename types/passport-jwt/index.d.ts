23e3840183a9a66375f96715dc841480394d4502
jindev <bjkim.dev@gmail.com>
2018-02-01 10:58:23 +0900

    feat: add contributor name on passport-jwt types.

--- types/passport-jwt/index.d.ts
+++ types/passport-jwt/index.d.ts
@@ -4,6 +4,7 @@
 //                 Alex Young <https://github.com/alsiola/>
 //                 David Ng <https://github.com/davidNHK/>
 //                 Carlos Eduardo Scheffer <https://github.com/carlosscheffer/>
+//                 Byungjin Kim <https://github.com/jindev>
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
 // TypeScript Version: 2.3
 
