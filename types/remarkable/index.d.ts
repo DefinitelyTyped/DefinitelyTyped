dccec54dc012cacb97133b69f46654e98695fc77
makepost <makepost@firemail.cc>
2018-02-03 01:23:09 +0200

    Remarkable: Types

---
+++ types/remarkable/index.d.ts
@@ -0,0 +1,8 @@
+// Type definitions for remarkable 1.7
+// Project: https://github.com/jonschlinkert/remarkable
+// Definitions by: makepost <https://github.com/makepost>
+// Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
+
+import Remarkable = require("./lib");
+
+export = Remarkable;
