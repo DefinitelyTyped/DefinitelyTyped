11b262328140691d26c8e6f0b1964f8da3cadcd2
Tim Chen <timchen1@gmail.com>
2018-02-03 18:47:26 -0500

    add myself to contributors

--- types/reactstrap/index.d.ts
+++ types/reactstrap/index.d.ts
@@ -7,6 +7,7 @@
 //                 FaithForHumans <https://github.com/FaithForHumans>
 //                 Kurt Preston <https://github.com/KurtPreston>
 //                 Kræn Hansen <https://github.com/kraenhansen>
+//                 Tim Chen <https://github.com/timc13>
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
 // TypeScript Version: 2.3
 
