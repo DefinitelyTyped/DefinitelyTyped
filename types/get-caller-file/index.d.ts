a19e81e8c98038c375888c9f5c52ece5c9f439b8
Klaus Meinhardt <klaus.meinhardt1@gmail.com>
2018-02-03 22:22:13 +0100

    Add get-caller-file

---
+++ types/get-caller-file/index.d.ts
@@ -0,0 +1,9 @@
+// Type definitions for get-caller-file 1.0
+// Project: https://github.com/stefanpenner/get-caller-file#readme
+// Definitions by: My Self <https://github.com/me>
+// Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
+
+declare function getCallerFile(position?: number): string;
+declare namespace getCallerFile {}
+
+export = getCallerFile;
