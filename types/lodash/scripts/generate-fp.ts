a8cd8a4592ff97bc206329cb67d777a78cb1d4e7
AJ Richardson <aj.ric.ar+git@gmail.com>
2018-02-02 19:25:12 -0500

    Generate generic args and multiple overloads correctly (ish)

--- types/lodash/scripts/generate-fp.ts
+++ types/lodash/scripts/generate-fp.ts
@@ -3,7 +3,6 @@ import * as path from "path";
 import ary = require("lodash/ary");
 import without = require("lodash/without");
 import convert = require("lodash/fp/convert");
-import { overlayMode } from "codemirror";
 
 interface Overload {
     typeParams: TypeParam[];
@@ -15,8 +14,8 @@ interface TypeParam {
     extends?: string;
 }
 
-function curryOverload(overload: Overload, name: string) {
-    const baseName = name[0].toUpperCase() + name.substr(1);
+function curryOverload(overload: Overload, functionName: string, overloadId: number) {
+    const baseName = functionName[0].toUpperCase() + functionName.substr(1);
     let output = "";
     if (overload.params.length === 0) {
         // Nothing to curry. Just use a basic function type.
@@ -27,35 +26,45 @@ function curryOverload(overload: Overload, name: string) {
         return output;
     }
 
+    const topInterface = curryParams(overload.params, overload.typeParams, overload.returnType, baseName, [], overloadId, 0);
+    let passTypeParams: TypeParam[] = [];
     for (let i = 0; i < overload.params.length; ++i) {
-        // TODO: filter out used typeParams, and pass them as interfaceTypeParams instead
-        output += curryParams(overload.params.slice(i), overload.typeParams, overload.returnType, baseName, [], i);
+        const interfaceName = getInterfaceName(baseName, overloadId, i, passTypeParams);
+        output += `
+interface ${interfaceName} {
+${curryParams(overload.params.slice(i), without(overload.typeParams, ...passTypeParams), overload.returnType, baseName, passTypeParams, overloadId, i)}
+}`;
+        const joinedUnusedParams = overload.params.slice(i).concat(overload.returnType).join(", ");
+        passTypeParams = overload.typeParams.filter(tp => new RegExp(`\\b${tp.name}\\b`).test(joinedUnusedParams));
     }
     return output;
 }
 
-function curryParams(params: string[], typeParams: TypeParam[], returnType: string, baseName: string, interfaceTypeParams: TypeParam[], index: number) {
+function curryParams(params: string[], typeParams: TypeParam[], returnType: string, baseName: string, interfaceTypeParams: TypeParam[], overloadId: number, index: number) {
     // Assume params.length >= 1
-    const interfaceName = baseName + (index > 0 ? index : "");
-    // TODO: we can't declare the top-level interface like this, because the top-level interface will contain multiple overloads
-    let output = `interface ${interfaceName} {
-    (): ${interfaceName};`;
+    const interfaceName = getInterfaceName(baseName, overloadId, index, interfaceTypeParams);
+    let output = `    (): ${interfaceName};`;
     for (let i = 1; i <= params.length; ++i) {
         const joinedParams = params.slice(0, i).join(", ");
-        let currentReturnType = i < params.length ? `${baseName}${index + i}` : returnType;
         const usedTypeParams = i < params.length ? typeParams.filter(tp => new RegExp(`\\b${tp.name}\\b`).test(joinedParams)) : typeParams;
-        const unusedTypeParams = without(typeParams, ...usedTypeParams);
-        if (usedTypeParams.length > 0 && i < params.length)
-            currentReturnType += `<${usedTypeParams.map(tp => tp.name).join(", ")}>`;
+        const joinedUnusedParams = params.slice(i).concat(returnType).join(", ");
+        const passTypeParams = typeParams.concat(interfaceTypeParams).filter(tp => new RegExp(`\\b${tp.name}\\b`).test(joinedUnusedParams));
+        const currentReturnType = i < params.length ? getInterfaceName(baseName, overloadId, index + i, passTypeParams, false) : returnType;
 
         output += `\n    ${createTypeParamDefinition(usedTypeParams)}(${joinedParams}): ${currentReturnType};`;
     }
-    output += "\n}\n";
     return output;
 }
 
-function createTypeParamDefinition(typeParams: TypeParam[]) {
-    return typeParams.length > 0 ? `<${typeParams.map(tp => tp.name + (tp.extends ? " extends " + tp.extends : "")).join(", ")}>` : "";
+function getInterfaceName(baseName: string, overloadId: number, index: number, typeParams: TypeParam[], includeConstraints = true) {
+    let interfaceName = baseName;
+    if (index > 0)
+        interfaceName += `${overloadId}x${index}${createTypeParamDefinition(typeParams, includeConstraints)}`;
+    return interfaceName;
+}
+
+function createTypeParamDefinition(typeParams: TypeParam[], includeConstraints = true) {
+    return typeParams.length > 0 ? `<${typeParams.map(tp => tp.name + (includeConstraints && tp.extends ? " extends " + tp.extends : "")).join(", ")}>` : "";
 }
 
 const builder = {
@@ -69,19 +78,28 @@ const builder = {
                     "predicate?: ListIterateeCustom<T, boolean>",
                 ],
                 returnType: "boolean",
-            }
+            },
+            {
+                typeParams: [{ name: "T", extends: "object" }],
+                params: [
+                    "collection: T | null | undefined",
+                    "predicate?: ObjectIterateeCustom<T, boolean>",
+                ],
+                returnType: "boolean",
+            },
         ];
+        // No optional parameters
         for (const o of overloads) {
             o.params = o.params.map(p => p.replace(/\?:/g, ":"));
         }
         return overloads
-            .map(o => {
+            .map((o, i) => {
                 const reargParams = o.params.map((p, i) => o.params[args[i]]);
                 return curryOverload({
                     typeParams: o.typeParams,
                     params: reargParams,
                     returnType: o.returnType,
-                }, "some");
+                }, "some", i + 1);
             })
             .join("\n");
     }, 2),//TODO: is ary needed?
@@ -103,12 +121,12 @@ type PartialDeep<T> = {
     [P in keyof T]?: PartialDeep<T[P]>;
 };
 
-interface ExpectedSome {
-    (): ExpectedSome;
-    <T>(predicate: ListIterateeCustom<T, boolean>): ExpectedSome1<T>;
+interface Some {
+    (): Some;
+    <T>(predicate: ListIterateeCustom<T, boolean>): Some1x1<T>;
     <T>(predicate: ListIterateeCustom<T, boolean>, collection: List<T> | null | undefined): boolean;
 }
-interface ExpectedSome1<T> {
-    (): ExpectedSome1<T>;
+interface Some1x1<T> {
+    (): Some1x1<T>;
     (collection: List<T> | null | undefined): boolean;
 }
