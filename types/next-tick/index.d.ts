8c38aff40b360778b05b67b50b5287d1e56a645c
Isaac Ferreira <zaclummys@gmail.com>
2018-02-03 02:12:55 -0200

    Update index.d.ts

--- types/next-tick/index.d.ts
+++ types/next-tick/index.d.ts
@@ -1,3 +1,8 @@
+// Type definitions for next-tick 1.0
+// Project: https://github.com/medikoo/next-tick
+// Definitions by: zaclummys <https://github.com/zaclummys>
+// Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
+
 declare function nextTick (cb: () => void): void;
 
 export = nextTick;
