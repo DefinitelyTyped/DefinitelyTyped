b75d332b2c9f9ad97b57f37cb4adf86f7bf36008
FaithForHumans <vogel.spencer@gmail.com>
2018-02-02 18:10:13 -0600

    Adding prepend/append for input group addons.
    Removing old cardblock, inputgroupbutton, and navdropdown components.
    Adding new InputGroupbuttonDropdown

--- types/reactstrap/lib/Uncontrolled.d.ts
+++ types/reactstrap/lib/Uncontrolled.d.ts
@@ -1,17 +1,14 @@
 import { UncontrolledAlertProps          } from './Alert';
 import { UncontrolledButtonDropdownProps } from './ButtonDropdown';
 import { UncontrolledDropdownProps       } from './Dropdown';
-import { UncontrolledNavDropdownProps    } from './NavDropdown';
 import { UncontrolledTooltipProps        } from './Tooltip';
 
 export const UncontrolledAlert: React.StatelessComponent<UncontrolledAlertProps>;
 export const UncontrolledButtonDropdown: React.StatelessComponent<UncontrolledButtonDropdownProps>;
 export const UncontrolledDropdown: React.StatelessComponent<UncontrolledDropdownProps>;
-export const UncontrolledNavDropdown: React.StatelessComponent<UncontrolledNavDropdownProps>;
 export const UncontrolledTooltip: React.StatelessComponent<UncontrolledTooltipProps>;
 
 export { UncontrolledAlertProps          } from './Alert';
 export { UncontrolledButtonDropdownProps } from './ButtonDropdown';
 export { UncontrolledDropdownProps       } from './Dropdown';
-export { UncontrolledNavDropdownProps    } from './NavDropdown';
 export { UncontrolledTooltipProps        } from './Tooltip';
