a13d90b79bf57376cfab7268c83ea312c435cc20
Caleb Everett <everettcaleb95@gmail.com>
2018-02-05 11:47:49 -0500

    Made s3-download-stream-tests.ts indentation 4 spaces

--- types/s3-download-stream/s3-download-stream-tests.ts
+++ types/s3-download-stream/s3-download-stream-tests.ts
@@ -5,14 +5,14 @@ import * as fs from "fs";
 // Documentation for "params":
 // http://docs.aws.amazon.com/AWSJavaScriptSDK/latest/AWS/S3.html#getObject-property
 var config = {
-  client: new AWS.S3({}),
-  concurrency: 6,
-  chunkSize: '512KB',
-  params: {
-    Key: 'key-name',
-    Bucket: 'bucket-name'
-  }
+    client: new AWS.S3({}),
+    concurrency: 6,
+    chunkSize: '512KB',
+    params: {
+        Key: 'key-name',
+        Bucket: 'bucket-name'
+    }
 }
 
 S3Stream(config)
-  .pipe(fs.createWriteStream('/path/to/a/file'));
+    .pipe(fs.createWriteStream('/path/to/a/file'));
