03b874398656b57e1ead4f79b127bdd718792306
Pete Johanson <peter@peterjohanson.com>
2018-02-06 11:51:12 -0500

    Add myself to `Definitions` header of urijs.

--- types/urijs/index.d.ts
+++ types/urijs/index.d.ts
@@ -1,6 +1,6 @@
 // Type definitions for URI.js 1.15.1
 // Project: https://github.com/medialize/URI.js
-// Definitions by: RodneyJT <https://github.com/RodneyJT>, Brian Surowiec <https://github.com/xt0rted>
+// Definitions by: RodneyJT <https://github.com/RodneyJT>, Brian Surowiec <https://github.com/xt0rted>, Pete Johanson <https://github.com/petejohanson>
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
 // TypeScript Version: 2.3
 
