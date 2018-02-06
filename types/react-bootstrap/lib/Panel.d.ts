2be99c76c1a1a8a89d7bf089c5d88920544ede25
Andy <anhans@microsoft.com>
2018-02-05 18:27:31 -0800

    Convert more "import *" to "import =" (#23446)

--- types/react-bootstrap/lib/Panel.d.ts
+++ types/react-bootstrap/lib/Panel.d.ts
@@ -1,11 +1,11 @@
 import * as React from 'react';
 import { TransitionCallbacks, Sizes, SelectCallback } from 'react-bootstrap';
-import * as PanelHeading from './PanelHeading'
-import * as PanelTitle from './PanelTitle'
-import * as PanelToggle from './PanelToggle'
-import * as PanelCollapse from './PanelCollapse'
-import * as PanelBody from './PanelBody'
-import * as PanelFooter from './PanelFooter'
+import PanelHeading = require('./PanelHeading');
+import PanelTitle = require('./PanelTitle');
+import PanelToggle = require('./PanelToggle');
+import PanelCollapse = require('./PanelCollapse');
+import PanelBody = require('./PanelBody');
+import PanelFooter = require('./PanelFooter');
 
 declare namespace Panel {
     export interface PanelProps extends TransitionCallbacks, React.HTMLProps<Panel> {
@@ -17,7 +17,7 @@ declare namespace Panel {
         onToggle?: SelectCallback;
     }
 }
-declare class Panel extends React.Component<Panel.PanelProps> { 
+declare class Panel extends React.Component<Panel.PanelProps> {
     static Heading: typeof PanelHeading;
     static Title: typeof PanelTitle;
     static Toggle: typeof PanelToggle;
