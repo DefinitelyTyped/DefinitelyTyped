ac85aa198cd5eaee651e03f832dd7dfef77424fa
Bas Luksenburg <basluksenburg@gmail.com>
2018-02-06 13:47:35 +0100

    Added myself as author of changes to Definitions by

--- types/chai-http/index.d.ts
+++ types/chai-http/index.d.ts
@@ -3,6 +3,7 @@
 // Definitions by: Wim Looman <https://github.com/Nemo157>
 //                 Liam Jones <https://github.com/G1itcher>
 //                 Federico Caselli <https://github.com/CaselIT>
+//                 Bas Luksenburg <https://github.com/bas-l>
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
 // TypeScript Version: 2.3
 
