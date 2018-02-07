d74751dfb11ae643574c1d5fd311ab45375e8cb1
Artur Sianiuk <senukartur@gmail.com>
2018-02-07 19:41:44 +0300

    fixed issues

--- types/autosuggest-highlight/index.d.ts
+++ types/autosuggest-highlight/index.d.ts
@@ -2,6 +2,7 @@
 // Project: https://github.com/moroshko/autosuggest-highlight#readme
 // Definitions by: Artur Sianiuk <https://github.com/senukartur>
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
+// TypeScript Version: 2.3
 
 export { default as match } from './match';
 export { default as parse } from './parse';
