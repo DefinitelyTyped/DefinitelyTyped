4bb00c69f459735a65c721d8f8114d44c69718d6
David Paz <davidmpaz@gmail.com>
2018-02-07 23:20:26 +0100

    Remove path from version

--- types/jquery.stickem/index.d.ts
+++ types/jquery.stickem/index.d.ts
@@ -1,4 +1,4 @@
-// Type definitions for jquery.stickem.js 1.4.1
+// Type definitions for jquery.stickem.js 1.4
 // Project: https://github.com/davist11/jQuery-Stickem
 // Definitions by: David Paz <https://github.com/davidmpaz>
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
