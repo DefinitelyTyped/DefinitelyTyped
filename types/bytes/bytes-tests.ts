03957cb2727629860454dc131967a26b87860324
believer <rickard.laurin@gmail.com>
2018-02-08 19:38:08 +0100

    Update bytes to 3.0.0

--- types/bytes/bytes-tests.ts
+++ types/bytes/bytes-tests.ts
@@ -1,17 +1,24 @@
-import bytes = require('bytes');
+import bytes = require("bytes");
 
 // 1024*1024 = 1048576
+console.log(bytes(undefined));
+console.log(bytes(null));
+
+console.log(bytes(1024));
 console.log(bytes(104857));
-console.log(bytes(104857, { thousandsSeparator: ' ' }));
+console.log(bytes(104857, { thousandsSeparator: " " }));
+console.log(bytes(104857, { decimalPlaces: 2 }));
+console.log(bytes(104857, { fixedDecimals: true }));
+console.log(bytes(104857, { unitSeparator: "-" }));
+console.log(bytes(104857, { unit: "GB" }));
 
 console.log(bytes.format(104857));
-console.log(bytes.format(104857, { thousandsSeparator: ' ' }));
+console.log(bytes.format(104857, { thousandsSeparator: " " }));
 console.log(bytes.format(104857, { decimalPlaces: 2 }));
 console.log(bytes.format(104857, { fixedDecimals: true }));
-console.log(bytes.format(104857, { unitSeparator: '-' }));
-console.log(bytes.format(104857, { unit: 'GB' }));
-console.log(bytes('1024kb'));
-console.log(bytes(1024));
+console.log(bytes.format(104857, { unitSeparator: "-" }));
+console.log(bytes.format(104857, { unit: "GB" }));
 
-console.log(bytes.parse('1024kb'));
+console.log(bytes("1024kb"));
+console.log(bytes.parse("1024kb"));
 console.log(bytes.parse(1024));
