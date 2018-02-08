640588da796121c4efc962432ed670a28efbeb8c
Marcin Biernat <marcin.biernat@pragmaticcoders.com>
2018-02-08 15:35:42 +0100

    Ramda defaultTo does not return null or undefined

--- types/ramda/ramda-tests.ts
+++ types/ramda/ramda-tests.ts
@@ -2249,6 +2249,12 @@ class Rectangle {
     defaultTo42(null);  // => 42
     defaultTo42(undefined);  // => 42
     defaultTo42("Ramda");  // => 'Ramda'
+
+    const valueOrUndefined = 2 as number | undefined;
+    defaultTo42(valueOrUndefined) - 2; // => 0
+
+    const valueOrNull = 2 as number | null;
+    defaultTo42(valueOrNull) - 2; // => 0
 };
 
 () => {
