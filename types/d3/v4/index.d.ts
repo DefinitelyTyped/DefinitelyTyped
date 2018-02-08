8a502167432f5ff26b51451cacff9978cd1241e9
Tom Wanzek <tomwanzek@gmail.com>
2018-02-08 17:09:07 -0500

    feat(d3): update d3 to major version 5
    BREAKING CHANGE:
    * feat(d3) :add d3-contour, d3-fetch, and d3-scale-chromatic
    * refactor(d3): remove d3-request and d3-queue in favour of d3-fetch
    * chore(d3): update major version number to 5.0
    * chore(d3): move legacy 4.x definitions with old dependencies into `v4` folder. Apply path-mapping to d3-scale v1 and d3 v4. Update minor version number to 4.13

--- types/d3/v4/index.d.ts
+++ types/d3/v4/index.d.ts
@@ -1,13 +1,10 @@
-// Type definitions for D3JS d3 standard bundle 5.0
+// Type definitions for D3JS d3 standard bundle 4.13
 // Project: https://github.com/d3/d3
 // Definitions by: Tom Wanzek <https://github.com/tomwanzek>, Alex Ford <https://github.com/gustavderdrache>, Boris Yankov <https://github.com/borisyankov>
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
 // TypeScript Version: 2.3
 
-// Last module patch version validated against: 5.0.0 RC3
-
-// NOTE TO MAINTAINERS: Review D3 v4.x module dependencies (see v4 sub-folder) and update its path-mappings in tsconfig (v4 folder),
-// if new MAJOR version of D3 v4 modules are released!!!
+// NOTE TO MAINTAINERS: Review and update path-mappings in tsconfig, if new major version of D3 v4 modules are released!!!
 
 export as namespace d3;
 
@@ -18,12 +15,10 @@ export * from 'd3-brush';
 export * from 'd3-chord';
 export * from 'd3-collection';
 export * from 'd3-color';
-export * from 'd3-contour';
 export * from 'd3-dispatch';
 export * from 'd3-drag';
 export * from 'd3-dsv';
 export * from 'd3-ease';
-export * from 'd3-fetch';
 export * from 'd3-force';
 export * from 'd3-format';
 export * from 'd3-geo';
@@ -32,9 +27,10 @@ export * from 'd3-interpolate';
 export * from 'd3-path';
 export * from 'd3-polygon';
 export * from 'd3-quadtree';
+export * from 'd3-queue';
 export * from 'd3-random';
+export * from 'd3-request';
 export * from 'd3-scale';
-export * from 'd3-scale-chromatic';
 export * from 'd3-selection';
 export * from 'd3-shape';
 export * from 'd3-time';
