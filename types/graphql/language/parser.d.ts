6f5689d2510a8f3ae5e33cc33877a23bae9d964e
Cole Turner <turner.cole@gmail.com>
2018-02-08 21:30:25 -0800

    feat(GraphQL): add additional GraphQL ParseOptions

--- types/graphql/language/parser.d.ts
+++ types/graphql/language/parser.d.ts
@@ -6,12 +6,51 @@ import { Lexer } from './lexer';
  * Configuration options to control parser behavior
  */
 export interface ParseOptions {
+
   /**
    * By default, the parser creates AST nodes that know the location
    * in the source that they correspond to. This configuration flag
    * disables that behavior for performance or testing.
    */
-  noLocation?: boolean;
+  noLocation?: boolean,
+
+  /**
+   * If enabled, the parser will parse empty fields sets in the Schema
+   * Definition Language. Otherwise, the parser will follow the current
+   * specification.
+   *
+   * This option is provided to ease adoption of the final SDL specification
+   * and will be removed in a future major release.
+   */
+  allowLegacySDLEmptyFields?: boolean,
+
+  /**
+   * If enabled, the parser will parse implemented interfaces with no `&`
+   * character between each interface. Otherwise, the parser will follow the
+   * current specification.
+   *
+   * This option is provided to ease adoption of the final SDL specification
+   * and will be removed in a future major release.
+   */
+  allowLegacySDLImplementsInterfaces?: boolean,
+
+  /**
+   * EXPERIMENTAL:
+   *
+   * If enabled, the parser will understand and parse variable definitions
+   * contained in a fragment definition. They'll be represented in the
+   * `variableDefinitions` field of the FragmentDefinitionNode.
+   *
+   * The syntax is identical to normal, query-defined variables. For example:
+   *
+   *   fragment A($var: Boolean = false) on T  {
+   *     ...
+   *   }
+   *
+   * Note: this feature is experimental and may change or be removed in the
+   * future.
+   */
+  experimentalFragmentVariables?: boolean,
 }
 
 /**
