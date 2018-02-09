382ec9044052a1fc36c1b44f182453da390ca4fc
Klaus Meinhardt <klaus.meinhardt1@gmail.com>
2018-02-09 18:29:00 +0100

    fix CI

--- types/builtin-modules/static.d.ts
+++ types/builtin-modules/static.d.ts
@@ -1,7 +1,2 @@
-// Type definitions for builtin-modules 2.0
-// Project: https://github.com/sindresorhus/builtin-modules#readme
-// Definitions by: Klaus Meinhardt <https://github.com/ajafff>
-// Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
-
 declare const builtinModules: ReadonlyArray<string>;
 export = builtinModules;
