6d15de32c3eb29eacc27ae4babc512124f1cab8e
Andrew Crites <acrites@mobiquityinc.com>
2018-02-09 17:53:36 -0500

    fix(vanilla-tilt): bumping version

--- types/vanilla-tilt/index.d.ts
+++ types/vanilla-tilt/index.d.ts
@@ -1,4 +1,4 @@
-// Type definitions for vanilla-tilt 1.3
+// Type definitions for vanilla-tilt 1.4
 // Project: https://github.com/micku7zu/vanilla-tilt.js
 // Definitions by: Livio Brunner <https://github.com/BrunnerLivio>
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
