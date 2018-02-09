bc271f6c44cd108134a027d1b63022946b939a6a
Chris Barth <chrisjbarth@hotmail.com>
2018-02-09 10:35:12 -0500

    Cleanup tests

--- types/passport-http/index.d.ts
+++ types/passport-http/index.d.ts
@@ -2,6 +2,7 @@
 // Project: https://github.com/jaredhanson/passport-http
 // Definitions by: Christophe Vidal <https://github.com/krizalys>
 //                 Tomek Łaziuk <https://github.com/tlaziuk>
+//                 Chris Barth <https://githum..com/cjbarth>
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
 // TypeScript Version: 2.3
 
