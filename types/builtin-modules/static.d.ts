e1b08d63781f745a4446c0f6de76f984e4f4e735
Klaus Meinhardt <klaus.meinhardt1@gmail.com>
2018-02-09 15:25:55 +0100

    Add types for 'builtin-modules'

---
+++ types/builtin-modules/static.d.ts
@@ -0,0 +1,7 @@
+// Type definitions for builtin-modules 2.0
+// Project: https://github.com/sindresorhus/builtin-modules#readme
+// Definitions by: Klaus Meinhardt <https://github.com/ajafff>
+// Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
+
+declare const builtinModules: ReadonlyArray<string>;
+export = builtinModules;
