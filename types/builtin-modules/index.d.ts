4a944979e2954cafb902241b40b50973a5e91333
Klaus Meinhardt <klaus.meinhardt1@gmail.com>
2018-02-10 02:10:11 +0100

    update header

--- types/builtin-modules/index.d.ts
+++ types/builtin-modules/index.d.ts
@@ -1,6 +1,6 @@
 // Type definitions for builtin-modules 2.0
 // Project: https://github.com/sindresorhus/builtin-modules#readme
-// Definitions by: My Self <https://github.com/me>
+// Definitions by: Klaus Meinhardt <https://github.com/ajafff>
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
 
 declare const builtinModules: ReadonlyArray<string>;
