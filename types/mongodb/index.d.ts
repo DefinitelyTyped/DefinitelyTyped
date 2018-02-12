391e8c17e978e9d1f90c50140aa17e5099c556ad
Wayne Dela Cruz <ddwwcruz@gmail.com>
2018-02-12 23:31:22 +0800

    Add more typings

--- types/mongodb/index.d.ts
+++ types/mongodb/index.d.ts
@@ -645,8 +645,30 @@ type FilterQuery<T> = {
         $lte?: T[P];
         $ne?: T[P];
         $nin?: T[P][];
-        $and?: FilterQuery<T[P]>[];
-        $not?: FilterQuery<T[P]>[] | T[P];
+        $and?: (FilterQuery<T[P]> | T[P])[];
+        $not?: (FilterQuery<T[P]> | T[P])[] | T[P];
+        $expr?: any;
+        $jsonSchema?: any;
+        $mod?: [number, number];
+        $regex?: RegExp;
+        $options?: string;
+        $text?: {
+            $search: string;
+            $language?: string;
+            $caseSensitive?: boolean;
+            $diacraticSensitive?: boolean;
+        };
+        $where: Object;
+        $geoIntersects?: Object;
+        $geoWithin?: Object;
+        $near?: Object;
+        $nearSphere?: Object;
+        $elemMatch?: Object;
+        $size?: number;
+        $bitsAllClear?: Object;
+        $bitsAllSet?: Object;
+        $bitsAnyClear?: Object;
+        $bitsAnySet?: Object;
     };
 };
 
