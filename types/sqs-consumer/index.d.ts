fc0c188d7cf6a0cb2d92c0b2374b6fa0ebb2c46c
Eric Byers <eric@ericbyers.com>
2018-02-13 16:54:43 -0600

    Increment version.

--- types/sqs-consumer/index.d.ts
+++ types/sqs-consumer/index.d.ts
@@ -1,4 +1,4 @@
-// Type definitions for sqs-consumer 3.7
+// Type definitions for sqs-consumer 3.8
 // Project: https://github.com/BBC/sqs-consumer
 // Definitions by: Daniel Chao <http://dchao.co/>
 //                 Eric Byers <http://github.com/EricByers>
