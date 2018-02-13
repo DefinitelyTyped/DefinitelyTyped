9110bfe1c8c25f59daaa63c72cab6ae1ca3259ea
Klaus Meinhardt <klaus.meinhardt1@gmail.com>
2018-02-13 09:08:43 +0100

    fix link

--- types/npm-packlist/index.d.ts
+++ types/npm-packlist/index.d.ts
@@ -1,5 +1,5 @@
 // Type definitions for npm-packlist 1.1
-// Project: https://www.npmjs.com/package/npm-packlist
+// Project: https://github.com/npm/npm-packlist
 // Definitions by: Klaus Meinhardt <https://github.com/ajafff>
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
 // TypeScript Version: 2.1
