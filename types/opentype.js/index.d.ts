b7d2e66ca0db2c6540c30b108081b848759c1549
Edgar Simson <edgar.simson@orangelv.com>
2018-02-13 13:33:07 +0200

    opentype.js: update to 0.7.3, add more tests

--- types/opentype.js/index.d.ts
+++ types/opentype.js/index.d.ts
@@ -1,63 +1,151 @@
-// Type definitions for opentype.js
+// Type definitions for opentype.js 0.7.3
 // Project: https://github.com/nodebox/opentype.js
 // Definitions by: Dan Marshall <https://github.com/danmarshall>
+//                 Edgar Simson <https://github.com/edzis>
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
+// TypeScript Version: 2.1
 
 export as namespace opentype;
 
-interface Contour extends Array<Point> {
-}
-
-export class Encoding {
-    charset: string;
-    charToGlyphIndex(c: string): number;
-    font: Font;
-}
-
-interface Field {
-    name: string;
-    type: string;
-    value: any;
-}
+/******************************************
+ * FONT
+ ******************************************/
 
 export class Font {
-    private nametoGlyphIndex;
-    private supported;
-    constructor(options: FontOptions);
+    names: FontNames;
+    unitsPerEm: number;
     ascender: number;
-    cffEncoding: Encoding;
+    descender: number;
+    createdTimestamp: number;
+    tables: { [tableName: string]: Table };
+    supported: boolean;
+    glyphs: GlyphSet;
+    encoding: Encoding;
+    substitution: Substitution;
+
+    readonly defaultRenderOptions: RenderOptions;
+
+    constructor(options: FontConstructorOptions);
+
     charToGlyph(c: string): Glyph;
     charToGlyphIndex(s: string): number;
-    descender: number;
-    download(): void;
-    draw(ctx: CanvasRenderingContext2D, text: string, x: number, y: number, fontSize: number, options?: RenderOptions): void;
-    drawMetrics(ctx: CanvasRenderingContext2D, text: string, x: number, y: number, fontSize: number, options?: RenderOptions): void;
-    drawPoints(ctx: CanvasRenderingContext2D, text: string, x: number, y: number, fontSize: number, options?: RenderOptions): void;
-    encoding: Encoding;
-    forEachGlyph(text: string, x: number, y: number, fontSize: number, options: RenderOptions, callback: { (glyph: Glyph, x: number, y: number, fontSize: number, options?: RenderOptions): void; }): void;
+    download(fileName?: string): void;
+    draw(
+        ctx: CanvasRenderingContext2D,
+        text: string,
+        x?: number,
+        y?: number,
+        fontSize?: number,
+        options?: RenderOptions
+    ): void;
+    drawMetrics(
+        ctx: CanvasRenderingContext2D,
+        text: string,
+        x?: number,
+        y?: number,
+        fontSize?: number,
+        options?: RenderOptions
+    ): void;
+    drawPoints(
+        ctx: CanvasRenderingContext2D,
+        text: string,
+        x?: number,
+        y?: number,
+        fontSize?: number,
+        options?: RenderOptions
+    ): void;
+    forEachGlyph(
+        text: string,
+        x: number | undefined,
+        y: number | undefined,
+        fontSize: number | undefined,
+        options: RenderOptions | undefined,
+        callback: {
+            (
+                glyph: Glyph,
+                x: number,
+                y: number,
+                fontSize: number,
+                options?: RenderOptions
+            ): void;
+        }
+    ): number;
+    getAdvanceWidth(
+        text: string,
+        fontSize?: number,
+        options?: RenderOptions
+    ): number;
     getEnglishName(name: string): string;
-    getGposKerningValue: { (leftGlyph: Glyph | number, rightGlyph: Glyph | number): number; };
-    getKerningValue(leftGlyph: Glyph | number, rightGlyph: Glyph | number): number;
-    getPath(text: string, x: number, y: number, fontSize: number, options?: RenderOptions): Path;
-    getPaths(text: string, x: number, y: number, fontSize: number, options?: RenderOptions): Path[];
-    glyphs: GlyphSet;
+    getKerningValue(
+        leftGlyph: Glyph | number,
+        rightGlyph: Glyph | number
+    ): number;
+    getPath(
+        text: string,
+        x: number,
+        y: number,
+        fontSize: number,
+        options?: RenderOptions
+    ): Path;
+    getPaths(
+        text: string,
+        x: number,
+        y: number,
+        fontSize: number,
+        options?: RenderOptions
+    ): Path[];
     glyphIndexToName(gid: number): string;
     glyphNames: GlyphNames;
     hasChar(c: string): boolean;
     kerningPairs: KerningPairs;
-    names: FontNames;
     nameToGlyph(name: string): Glyph;
     nameToGlyphIndex(name: string): number;
     numberOfHMetrics: number;
     numGlyphs: number;
     outlinesFormat: string;
     stringToGlyphs(s: string): Glyph[];
-    tables: { [tableName: string]: Table; };
     toArrayBuffer(): ArrayBuffer;
     toBuffer(): ArrayBuffer;
     toTables(): Table;
-    unitsPerEm: number;
     validate(): void;
+}
+
+export type FontConstructorOptions = FontConstructorOptionsBase &
+    Partial<FontOptions> & {
+        glyphs: Glyph[];
+    };
+
+interface FontOptions {
+    empty?: boolean;
+    familyName: string;
+    styleName: string;
+    fullName?: string;
+    postScriptName?: string;
+    designer?: string;
+    designerURL?: string;
+    manufacturer?: string;
+    manufacturerURL?: string;
+    license?: string;
+    licenseURL?: string;
+    version?: string;
+    description?: string;
+    copyright?: string;
+    trademark?: string;
+    unitsPerEm: number;
+    ascender: number;
+    descender: number;
+    createdTimestamp: number;
+    weightClass?: string;
+    widthClass?: string;
+    fsSelection?: string;
+}
+
+interface FontConstructorOptionsBase {
+    familyName: string;
+    styleName: string;
+    unitsPerEm: number;
+    ascender: number;
+    descender: number;
 }
 
 interface FontNames {
@@ -77,27 +165,32 @@ interface FontNames {
     version: LocalizedName;
 }
 
-interface FontOptions {
-    copyright?: string;
-    ascender?: number;
-    descender?: number;
-    description?: string;
-    designer?: string;
-    designerURL?: string;
-    empty?: boolean;
-    familyName?: string;
-    fullName?: string;
-    glyphs?: Glyph[] | GlyphSet;
-    license?: string;
-    licenseURL?: string;
-    manufacturer?: string;
-    manufacturerURL?: string;
-    postScriptName?: string;
-    styleName?: string;
-    unitsPerEm?: number;
-    trademark?: string;
-    version?: string;
+interface Table {
+    [propName: string]: any;
+    encode(): number[];
+    fields: Field[];
+    sizeOf(): number;
+    tables: Table[];
+    tableName: string;
 }
+
+interface KerningPairs {
+    [pair: string]: number;
+}
+
+interface LocalizedName {
+    [lang: string]: string;
+}
+
+interface Field {
+    name: string;
+    type: string;
+    value: any;
+}
+
+/******************************************
+ * GLYPH
+ ******************************************/
 
 export class Glyph {
     private index;
@@ -106,22 +199,49 @@ export class Glyph {
     private yMin;
     private yMax;
     private points;
-    constructor(options: GlyphOptions);
-    addUnicode(unicode: number): void;
-    advanceWidth: number;
-    bindConstructorValues(options: GlyphOptions): void;
-    draw(ctx: CanvasRenderingContext2D, x: number, y: number, fontSize: number): void;
-    drawMetrics(ctx: CanvasRenderingContext2D, x: number, y: number, fontSize: number): void;
-    drawPoints(ctx: CanvasRenderingContext2D, x: number, y: number, fontSize: number): void;
-    getContours(): Contour[];
-    getMetrics(): Metrics;
-    getPath(x: number, y: number, fontSize: number): Path;
+
     name: string;
-    path: Path | { (): Path; };
+    path: Path | { (): Path };
     unicode: number;
     unicodes: number[];
-}
+    advanceWidth: number;
 
+    constructor(options: GlyphOptions);
+
+    addUnicode(unicode: number): void;
+    bindConstructorValues(options: GlyphOptions): void;
+    draw(
+        ctx: CanvasRenderingContext2D,
+        x?: number,
+        y?: number,
+        fontSize?: number,
+        options?: RenderOptions
+    ): void;
+    drawMetrics(
+        ctx: CanvasRenderingContext2D,
+        x?: number,
+        y?: number,
+        fontSize?: number,
+        options?: RenderOptions
+    ): void;
+    drawPoints(
+        ctx: CanvasRenderingContext2D,
+        x?: number,
+        y?: number,
+        fontSize?: number,
+        options?: RenderOptions
+    ): void;
+    getBoundingBox(): BoundingBox;
+    getContours(): Contour;
+    getMetrics(): Metrics;
+    getPath(
+        x?: number,
+        y?: number,
+        fontSize?: number,
+        options?: RenderOptions,
+        font?: Font
+    ): Path;
+}
 interface GlyphOptions {
     advanceWidth?: number;
     index?: number;
@@ -146,68 +266,10 @@ export class GlyphNames {
 export class GlyphSet {
     private font;
     private glyphs;
-    constructor(font: Font, glyphs: Glyph[] | { (): Glyph; }[]);
+    constructor(font: Font, glyphs: Glyph[] | { (): Glyph }[]);
     get(index: number): Glyph;
     length: number;
-    push(index: number, loader: { (): Glyph; }): void;
-}
-
-interface KerningPairs {
-    [pair: string]: number;
-}
-
-export function load(url: string, callback: { (error: any, font?: Font): void; }): void;
-
-export function loadSync(url: string): Font;
-
-interface LocalizedName {
-    [lang: string]: string;
-}
-
-interface Metrics {
-    leftSideBearing: number;
-    rightSideBearing?: number;
-    xMax: number;
-    xMin: number;
-    yMax: number;
-    yMin: number;
-}
-
-export function parse(buffer: any): Font;
-
-export class Path {
-    private fill;
-    private stroke;
-    private strokeWidth;
-    constructor();
-    bezierCurveTo(x1: number, y1: number, x2: number, y2: number, x: number, y: number): void;
-    close: () => void;
-    closePath(): void;
-    commands: PathCommand[];
-    curveTo: (x1: number, y1: number, x2: number, y2: number, x: number, y: number) => void;
-    draw(ctx: CanvasRenderingContext2D): void;
-    extend(pathOrCommands: Path | PathCommand[]): void;
-    lineTo(x: number, y: number): void;
-    moveTo(x: number, y: number): void;
-    quadraticCurveTo(x1: number, y1: number, x: number, y: number): void;
-    quadTo: (x1: number, y1: number, x: number, y: number) => void;
-    toPathData(decimalPlaces: number): string;
-    toSVG(decimalPlaces: number): string;
-    unitsPerEm: number;
-}
-
-interface PathCommand {
-    type: string;
-    x?: number;
-    y?: number;
-    x1?: number;
-    y1?: number;
-    x2?: number;
-    y2?: number;
-}
-
-interface Point {
-    lastPointOfContour?: boolean;
+    push(index: number, loader: { (): Glyph }): void;
 }
 
 interface Post {
@@ -227,14 +289,110 @@ interface Post {
 }
 
 interface RenderOptions {
-    kerning: boolean;
+    script?: string;
+    language?: string;
+    kerning?: boolean;
+    xScale?: number;
+    yScale?: number;
+    features?: {
+        [key: string]: boolean;
+    };
 }
 
-interface Table {
-    [propName: string]: any;
-    encode(): number[];
-    fields: Field[];
-    sizeOf(): number;
-    tables: Table[];
-    tableName: string;
+export interface Metrics {
+    leftSideBearing: number;
+    rightSideBearing?: number;
+    xMax: number;
+    xMin: number;
+    yMax: number;
+    yMin: number;
 }
+
+export interface Contour extends Array<Point> {}
+
+interface Point {
+    lastPointOfContour?: boolean;
+}
+
+/******************************************
+ * PATH
+ ******************************************/
+
+export class Path {
+    private fill;
+    private stroke;
+    private strokeWidth;
+    constructor();
+    bezierCurveTo(
+        x1: number,
+        y1: number,
+        x2: number,
+        y2: number,
+        x: number,
+        y: number
+    ): void;
+    close: () => void;
+    closePath(): void;
+    commands: PathCommand[];
+    curveTo: (
+        x1: number,
+        y1: number,
+        x2: number,
+        y2: number,
+        x: number,
+        y: number
+    ) => void;
+    draw(ctx: CanvasRenderingContext2D): void;
+    extend(pathOrCommands: Path | PathCommand[] | BoundingBox): void;
+    getBoundingBox(): BoundingBox;
+    lineTo(x: number, y: number): void;
+    moveTo(x: number, y: number): void;
+    quadraticCurveTo(x1: number, y1: number, x: number, y: number): void;
+    quadTo: (x1: number, y1: number, x: number, y: number) => void;
+    toDOMElement(decimalPlaces: number): SVGPathElement;
+    toPathData(decimalPlaces: number): string;
+    toSVG(decimalPlaces: number): string;
+    unitsPerEm: number;
+}
+
+interface PathCommand {
+    type: string;
+    x?: number;
+    y?: number;
+    x1?: number;
+    y1?: number;
+    x2?: number;
+    y2?: number;
+}
+
+/******************************************
+ * UTIL CLASSES
+ ******************************************/
+
+export class BoundingBox {
+    // TODO add methods
+}
+
+export class Encoding {
+    charset: string;
+    charToGlyphIndex(c: string): number;
+    font: Font;
+}
+
+export class Substitution {
+    constructor(font: Font);
+    // TODO add methods
+}
+
+/******************************************
+ * STATIC
+ ******************************************/
+
+export function load(
+    url: string,
+    callback: { (error: any, font?: Font): void }
+): void;
+
+export function loadSync(url: string): Font;
+
+export function parse(buffer: any): Font;
