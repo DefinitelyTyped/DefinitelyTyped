5125e547db88124b51875d094d18c61de504c391
humpedli <hump@hump.hu>
2018-02-13 16:24:31 +0100

    pulltorefreshjs - Version increase

--- types/pulltorefreshjs/index.d.ts
+++ types/pulltorefreshjs/index.d.ts
@@ -1,4 +1,4 @@
-// Type definitions for pulltorefreshjs 0.1
+// Type definitions for pulltorefreshjs 0.1.11
 // Project: https://github.com/BoxFactura/pulltorefresh.js
 // Definitions by: Daniel Rosenwasser <https://github.com/DanielRosenwasser>
 //                 Tamas Kinsztler <https://github.com/humpedli>
