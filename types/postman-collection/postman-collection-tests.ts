720e4ea9819553d8c5a8507237f7e8ce01046c82
Kyle Buzby <kbuzby@epic.com>
2018-02-15 09:55:50 -0600

    add example tests

---
+++ types/postman-collection/postman-collection-tests.ts
@@ -0,0 +1,7 @@
+import * as pmCollection from 'postman-collection';
+
+/* Example https://github.com/DefinitelyTyped/DefinitelyTyped/blob/master/types/base64-js/base64-js-tests.ts
+base64js.byteLength(""); // $ExpectType number
+base64js.toByteArray(""); // $ExpectType Uint8Array
+base64js.fromByteArray(new Uint8Array(0)); // $ExpectType string
+*/
