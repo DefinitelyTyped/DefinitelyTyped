62ee1e250463da48364968b59f3fc0e83e99e521
nayni <rutgerhendrickx@gmail.com>
2018-02-16 21:10:23 +0100

    Add type definitions for graphql-resolve-batch

---
+++ types/graphql-resolve-batch/index.d.ts
@@ -0,0 +1,26 @@
+// Type definitions for graphql-resolve-batch 1.0
+// Project: https://github.com/calebmer/graphql-resolve-batch#readme
+// Definitions by: Rutger Hendrickx <https://github.com/nayni>
+// Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
+// TypeScript Version: 2.3
+
+export function createBatchResolver<
+    TSource,
+    TReturn,
+    TArgs = any,
+    TContext = any
+>(
+    batchResolveFn: BatchResolveFunction<TSource, TArgs, TContext, TReturn>
+): ResolverFunction<TSource, TArgs, TContext, TReturn>;
+
+export type ResolverFunction<TSource, TArgs, TContext, TReturn> = (
+    source: TSource,
+    args: TArgs,
+    context: TContext
+) => Promise<TReturn>;
+
+export type BatchResolveFunction<TSource, TArgs, TContext, TReturn> = (
+    sources: ReadonlyArray<TSource>,
+    args: TArgs,
+    context: TContext
+) => TReturn[] | Promise<TReturn>[];
