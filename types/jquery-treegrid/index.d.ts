500c02f2aea1ad9dde9df61a058797e07b24dd8c
Pablo Morelli <pablo.morelli@fravega.com.ar>
2018-02-16 11:14:56 -0300

    Added typings for jquery-treegrid

---
+++ types/jquery-treegrid/index.d.ts
@@ -0,0 +1,13 @@
+// Type definitions for Jquery Tree Grid v0.3.0
+// Project: https://github.com/maxazan/jquery-treegrid
+// Definitions by: Pablo Morelli <https://github.com/pmorelli92>
+// Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
+// TypeScript Version: 2.5
+
+/// <reference types="jquery" />
+
+interface JQuery {
+    treegrid(options?: any): JQuery;    
+}
+
+declare var treegrid: JQueryStatic;
