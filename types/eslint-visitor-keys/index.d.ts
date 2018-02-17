55338de9054f03e2dd31676f88f024380b084794
Toru Nagashima <star.ctor@gmail.com>
2018-02-17 17:56:44 +0900

    remove comment

--- types/eslint-visitor-keys/index.d.ts
+++ types/eslint-visitor-keys/index.d.ts
@@ -4,10 +4,6 @@
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
 // TypeScript Version: 2.2
 
-/*~ If this module is a UMD module that exposes a global variable 'myLib' when
- *~ loaded outside a module loader environment, declare that global here.
- *~ Otherwise, delete this declaration.
- */
 export type VisitorKeys = Readonly<{
     [type: string]: ReadonlyArray<string> | undefined
 }>;
