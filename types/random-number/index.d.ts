0aaad0d322e9afe765432aec26124f64a2093da4
Openbyte <development.openbyte@gmail.com>
2018-02-17 07:35:12 +0100

    removed null types from UnparsedOptions

--- types/random-number/index.d.ts
+++ types/random-number/index.d.ts
@@ -8,9 +8,9 @@
 type Generator = (min?: number|null, max?: number|null, integer?: boolean|null) => number;
 
 interface UnparsedOptions {
-    min?: number|null;
-    max?: number|null;
-    integer?: boolean|null;
+    min?: number;
+    max?: number;
+    integer?: boolean;
 }
 interface Options {
     min: number;
