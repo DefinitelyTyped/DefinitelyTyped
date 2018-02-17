576af6c02225974624cd64470cc7936ad018caaf
Tommaso Ricci <t.ricci@molecle.com>
2018-02-17 16:17:04 +0100

    Bumped undertaker version

--- types/undertaker/index.d.ts
+++ types/undertaker/index.d.ts
@@ -1,4 +1,4 @@
-// Type definitions for undertaker 1.1
+// Type definitions for undertaker 1.2
 // Project: https://github.com/phated/undertaker
 // Definitions by: Qubo <https://github.com/tkqubo>, Giedrius Grabauskas <https://github.com/GiedriusGrabauskas>
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
