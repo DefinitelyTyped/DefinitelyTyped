50cca3e67766cc3f78229e468476c3a68fc62294
Matt Perry <muffinman616@gmail.com>
2018-02-17 20:26:35 -0500

    Expand hubot typings

--- types/hubot/index.d.ts
+++ types/hubot/index.d.ts
@@ -3,21 +3,41 @@
 // Definitions by: Dirk Gadsden <https://github.com/dirk>
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
 
+/// <reference types="node" />
+
+import { EventEmitter } from 'events';
+
 declare namespace Hubot {
-    class Brain {
-        userForId(id: any): any;
-        userForName(name: string): any;
+    class Brain extends EventEmitter {
+        constructor(robot: Robot);
+        set<T = any>(key: string, value: T): this;
+        get<T = any>(key: string): T;
+        remove(key: string): this;
+        save(): void;
+        close(): void;
+        setAutoSave(enabled: boolean): void;
+        resetSaveInterval(seconds: number): void;
+        mergeData(data: any): void;
+        users(): User[];
+        userForId(id: any): User;
+        userForName(name: string): User;
+        userForRawFuzzyName(fuzzyName: string): User;
+        userForFuzzyName(fuzzyName: string): User;
     }
 
     class User {
+        constructor(id: any, options?: any);
         id: any;
         name: string;
+        [property: string]: any;
     }
 
     class Message {
+        constructor(user: User, done?: boolean)
         user: User;
         text: string;
         id: string;
+        finish(): void;
     }
 
     class Response {
@@ -26,7 +46,11 @@ declare namespace Hubot {
 
         constructor(robot: Robot, message: Message, match: RegExpMatchArray);
         send(...strings: string[]): void;
+        emote(...strings: string[]): void;
         reply(...strings: string[]): void;
+        topic(...strings: string[]): void;
+        play(...strings: string[]): void;
+        locked(...strings: string[]): void;
         random<T>(items: T[]): T;
     }
 
@@ -40,6 +64,12 @@ declare namespace Hubot {
         hear(regex: RegExp, options: any, callback: ListenerCallback): void;
         respond(regex: RegExp, callback: ListenerCallback): void;
         respond(regex: RegExp, options: any, callback: ListenerCallback): void;
+        enter(callback: ListenerCallback): void;
+        enter(options: any, callback: ListenerCallback): void;
+        topic(callback: ListenerCallback): void;
+        topic(options: any, callback: ListenerCallback): void;
+        on(event: string | symbol, listener: (...args: any[]) => void): this;
+        emit(event: string | symbol, ...args: any[]): boolean;
     }
 }
 
