399cb914879dc939781ed09ae35547dc374e5d1d
Stan Goldmann <goldmann@traum-ferienwohnungen.de>
2018-02-19 12:03:22 +0100

    Address requested reviewer changes

--- types/anybar/index.d.ts
+++ types/anybar/index.d.ts
@@ -1,8 +1,9 @@
-// Type definitions for anybar 3.0.0
+// Type definitions for anybar 2.0
 // Project: https://github.com/sindresorhus/anybar
 // Definitions by: Chris Khoo <https://github.com/khoomeister>
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
+// TypeScript Version: 2.1
 
-declare namespace anybar { }
+declare function anybar(color: string, options?: { port: number }): void;
 
-export default function anybar(color: string, options?: { port: number }): void
+export = anybar;
