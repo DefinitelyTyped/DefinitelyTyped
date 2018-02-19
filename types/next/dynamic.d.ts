93365ba8062a7d5d9f075e12bffbc7f90c624077
Lukwago Allan <epicallan.al@gmail.com>
2018-02-19 12:19:22 +0300

    Correct default function for esnext style import
    When one uses the [esnext import style](https://github.com/DefinitelyTyped/DefinitelyTyped/blob/master/types/next/test/next-dynamic-tests.tsx#L4), next/dynamic types don't type check.

--- types/next/dynamic.d.ts
+++ types/next/dynamic.d.ts
@@ -24,6 +24,6 @@ export class SameLoopPromise<T> extends Promise<T> {
     runIfNeeded(): void;
 }
 export default function<TCProps, TLProps>(
-    componentPromise: Promise<React.ComponentType<TCProps>>,
+    componentPromise: Promise<React.ComponentType<TCProps> | any>,
     options?: DynamicOptions<TCProps, TLProps>,
 ): React.ComponentType<TCProps & TLProps>;
