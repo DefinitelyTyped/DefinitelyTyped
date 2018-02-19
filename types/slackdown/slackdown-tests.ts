a85bc8d2e4aecf6863727c8dfe2e6e1793d71290
nju33 <nju33.ki@gmail.com>
2018-02-19 18:40:01 +0900

    fix: Delete tslint comment and the second import
    https://github.com/DefinitelyTyped/DefinitelyTyped/pull/23750#pullrequestreview-97448636

--- types/slackdown/slackdown-tests.ts
+++ types/slackdown/slackdown-tests.ts
@@ -1,6 +1,3 @@
 import * as slackdown from 'slackdown';
-// tslint:disable-next-line no-duplicate-imports
-import { parse } from 'slackdown';
 
 slackdown.parse('xxx <http://xxxxx.jp|xxxxx.jp>');
-parse('xxx <http://xxxxx.jp|xxxxx.jp>');
