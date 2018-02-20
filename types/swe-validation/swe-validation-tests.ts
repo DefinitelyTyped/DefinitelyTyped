6b7fa008b6f42a3f94bff116a739c8d33ba0bd83
Robert Hjalmers <robert.hjalmers@gmail.com>
2018-02-20 11:14:53 +0100

    fixed swe-validation typings and test

--- types/swe-validation/swe-validation-tests.ts
+++ types/swe-validation/swe-validation-tests.ts
@@ -1,4 +1,4 @@
 import validate = require("swe-validation");
 
-let corporateId = validate.cin(5500123456);
-let personalId = validate.ssn(192301120123);
+const corporateId = validate.cin(5500123456);
+const personalId = validate.ssn(192301120123);
