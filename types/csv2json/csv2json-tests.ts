21e8f440d5b11490ef7c90b67b2133465c3336e6
Piotr Roszatycki <piotr.roszatycki@gmail.com>
2018-02-20 20:23:41 +0100

    New typings for csv2json

---
+++ types/csv2json/csv2json-tests.ts
@@ -0,0 +1,8 @@
+import csv2json from "csv2json";
+import fs from "fs";
+
+fs.createReadStream('data.csv')
+  .pipe(csv2json({
+    separator: ';'
+  }))
+  .pipe(fs.createWriteStream('data.json'));
---
+++ types/csv2json/index.d.ts
@@ -0,0 +1,15 @@
+// Type definitions for csv2json 1.4
+// Project: https://github.com/julien-f/csv2json
+// Definitions by: Piotr Roszatycki <https://github.com/dex4er>
+// Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
+
+/// <reference types="node" />
+
+import { Writable } from "stream";
+
+export interface CSV2JSONOptions {
+  dynamicTyping?: boolean;
+  separator?: string;
+}
+
+export default function csv2json (options: CSV2JSONOptions): Writable;
