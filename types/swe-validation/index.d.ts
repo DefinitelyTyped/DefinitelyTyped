f790371b13dde94e009cccb11ab606fda8b612b7
Robert Hjalmers <hjalmers@users.noreply.github.com>
2018-02-20 10:02:25 +0100

    Create index.d.ts

---
+++ types/swe-validation/index.d.ts
@@ -0,0 +1,25 @@
+declare module 'swe-validation' {
+
+    interface cin {
+        isValid:boolean,
+        corporation: {
+            type:string,
+            id:string
+        }
+    }
+
+    interface ssn {
+        isValid:boolean,
+        person?: {
+            type:string,
+            sex:string,
+            ssn:string
+        }
+    }
+
+    let validate: {
+        ssn(number:number): ssn;
+        cin(number:number): cin;
+    };
+    export = validate;
+}
