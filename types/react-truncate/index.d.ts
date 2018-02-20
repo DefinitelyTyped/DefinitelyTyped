a4551084b2b6a13971aacbb80fc8def71614c9d9
Robin Franken <franken@lequest.nl>
2018-02-20 13:09:30 +0100

    Updated react-truncate definition
    Added `trimWhitespace` prop.

--- types/react-truncate/index.d.ts
+++ types/react-truncate/index.d.ts
@@ -1,4 +1,4 @@
-// Type definitions for react-truncate 2.1
+// Type definitions for react-truncate 2.3
 // Project: https://github.com/One-com/react-truncate
 // Definitions by: Matt Perry <https://github.com/mattvperry>
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
@@ -10,6 +10,7 @@ export interface TruncateProps extends React.HTMLProps<Truncate> {
     lines?: number | false;
     ellipsis?: React.ReactNode;
     onTruncate?(isTruncated: boolean): void;
+    trimWhitespace?: boolean;
 }
 
 declare class Truncate extends React.Component<TruncateProps> { }
