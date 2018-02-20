5d238e65774c39762cb0d34f2a5b587639e93a26
Tom Crockett <pelotom@gmail.com>
2018-02-20 14:26:56 -0800

    Add runtime API, tests

--- types/vega/index.d.ts
+++ types/vega/index.d.ts
@@ -3,6 +3,10 @@
 // Definitions by: Tom Crockett <https://github.com/pelotom>
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
 
+// ============================================================================================
+// Spec typings adapted from https://github.com/vega/vega-parser/tree/as/typings
+// ============================================================================================
+
 import { AutoSize } from './autosize';
 import { Background } from './background';
 import { Padding } from './padding';
@@ -18,3 +22,49 @@ export interface Spec extends Scope, Encode<EncodeEntry> {
     autosize?: AutoSize;
     background?: Background;
 }
+
+// ============================================================================================
+// Runtime typings adapted from https://github.com/vega/vega-lite/blob/master/typings/vega.d.ts
+// ============================================================================================
+
+// TODO
+export type Runtime = any
+
+export const version: string;
+export function parse(spec: any, opt?: any): Runtime;
+export function isString(value: any): value is string;
+
+export type Loader = {
+  load: (uri: string, options?: any) => Promise<string>
+  sanitize: (uri: string, options: any) => Promise<{href: string}>
+  http: (uri: string, options: any) => Promise<string>
+  file: (filename: string) => Promise<string>
+}
+
+export class View {
+  constructor(runtime: Runtime, config?: any);
+  public initialize(dom?: Element | string): View;
+  public finalize(): void;
+  public logLevel(level: number): View;
+  public renderer(renderer: 'canvas' | 'svg' | 'none'): View;
+  public loader(loader: Loader): View;
+
+  public hover(): View;
+  public run(): View;
+  public change(name: string, changeset: any): View;
+  public changeset(): any;
+  public data(name: string): object[];
+
+  public width(w: number): View;
+  public height(h: number): View;
+  public padding(p: number | {left?: number, right?: number, top?: number, bottom?: number}): View;
+
+  public toImageURL(type: string): Promise<string>;
+
+  toSVG(): Promise<string>;
+  toCanvas(): Promise<any>; // TODO node-canvas result
+}
+
+export const Warn: number;
+export const changeset: any;
+export const loader: (opt?: any) => Loader;
--- types/vega/vega-tests.ts
+++ types/vega/vega-tests.ts
@@ -1,4 +1,6 @@
-import { Spec } from 'vega'
+import * as vega from 'vega'
+
+type Spec = vega.Spec
 
 // https://vega.github.io/editor/#/examples/vega/bar-chart
 const barChart: Spec ={
@@ -4188,3 +4190,44 @@ const distortionComparison: Spec = {
     }
   ]
 };
+
+// Runtime examples from https://vega.github.io/vega/usage/
+
+function clientSideApi() {
+  var view;
+
+  vega.loader()
+    .load('https://vega.github.io/vega/examples/bar-chart.vg.json')
+    .then(function(data) { render(JSON.parse(data)); });
+
+  function render(spec: Spec) {
+    view = new vega.View(vega.parse(spec))
+      .renderer('canvas')  // set renderer (canvas or svg)
+      .initialize('#view') // initialize view within parent DOM container
+      .hover()             // enable hover encode set processing
+      .run();
+  }
+}
+
+function serverSideApi() {
+  // create a new view instance for a given Vega JSON spec
+  var view = new vega.View(vega.parse(histogram))
+    .renderer('none')
+    .initialize();
+  
+  // generate a static SVG image
+  view.toSVG()
+    .then(function(svg) {
+      // process svg string
+    })
+    .catch(function(err) { console.error(err); });
+  
+  // generate a static PNG image
+  view.toCanvas()
+    .then(function(canvas) {
+      // process node-canvas instance
+      // for example, generate a PNG stream to write
+      var stream = canvas.createPNGStream();
+    })
+    .catch(function(err) { console.error(err); });
+}
