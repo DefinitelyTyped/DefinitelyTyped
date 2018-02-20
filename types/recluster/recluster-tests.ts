1e086c8aae9a934dd511dfcf59700ab74eb135cf
Piotr Roszatycki <piotr.roszatycki@gmail.com>
2018-02-20 22:20:11 +0100

    New typings for recluster

---
+++ types/recluster/recluster-tests.ts
@@ -0,0 +1,9 @@
+import recluster from "recluster";
+import path = require("path");
+
+const cluster = recluster(path.join(__dirname, 'server.js'));
+cluster.run();
+
+process.on('SIGUSR2', () => {
+  cluster.reload();
+});
