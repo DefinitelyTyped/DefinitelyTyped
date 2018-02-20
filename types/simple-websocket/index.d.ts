5592de580aca19f0f6ed705ddb8266f7082798c5
Piotr Roszatycki <piotr.roszatycki@gmail.com>
2018-02-20 16:46:33 +0100

    Compatible with simple-socket 7.0

--- types/simple-websocket/index.d.ts
+++ types/simple-websocket/index.d.ts
@@ -1,33 +1,28 @@
-// Type definitions for simple-websocket 6.0
+// Type definitions for simple-websocket 7.0
 // Project: https://github.com/feross/simple-websocket
 // Definitions by: Piotr Roszatycki <https://github.com/dex4er>
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
 
 /// <reference types="node" />
 
-import { Duplex, DuplexOptions } from 'stream';
-import WebSocket = require('ws');
+import { Duplex, DuplexOptions } from "stream";
+import WebSocket from "ws";
 
-declare namespace Socket {
-  interface Options extends DuplexOptions {
-    /** websocket server url */
-    url?: string;
-    /** raw websocket instance to wrap */
-    socket?: WebSocket;
-  }
-
-  let WEBSOCKET_SUPPORT: boolean;
+export interface SocketOptions extends DuplexOptions {
+  /** websocket server url */
+  url?: string;
+  /** raw websocket instance to wrap */
+  socket?: WebSocket;
 }
 
-declare class Socket extends Duplex {
-  constructor(options: Socket.Options | string);
+export default class Socket extends Duplex {
+  static WEBSOCKET_SUPPORT: boolean;
+
+  constructor(options: SocketOptions | string);
 
   /** Send text/binary data to the WebSocket server */
   send(chunk: any): void;
 
-  // TODO destroy method not compatible with Duplex
-  // destroy(err?: Error): void;
-  // destroy(callback?: (err?: Error) => void): void;
+  /** Destroy and cleanup this websocket connection */
+  destroy(err?: Error): void;
 }
-
-export = Socket;
--- types/simple-websocket/simple-websocket-tests.ts
+++ types/simple-websocket/simple-websocket-tests.ts
@@ -1,4 +1,4 @@
-import Socket = require('simple-websocket');
+import Socket from "simple-websocket";
 
 if (Socket.WEBSOCKET_SUPPORT) {
   const socket = new Socket('ws://echo.websocket.org');
@@ -9,15 +9,16 @@ if (Socket.WEBSOCKET_SUPPORT) {
 
   socket.on('data', (data: string | Buffer) => {
     console.log('got message: ' + data);
-    // socket.destroy(() => {
-    //   console.log('socket destroyed');
-    // });
+  });
+
+  socket.on('error', (err: Error) => {
+    socket.destroy(err);
   });
 }
 
-import Server = require('simple-websocket/server');
+import Server from "simple-websocket/server";
 
-const server = new Server({ port: 8080 }); // see `ws` docs for other options
+const server = new Server({ port: 8080 });
 
 server.on('connection', (socket: Socket) => {
   socket.write('pong');
