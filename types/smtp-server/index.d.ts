4c5b64755f4a541ef44b328405f5381e3c8fedba
Tadeusz Wawszczak <Tadeusz.Wawszczak@mapp.com>
2018-02-21 11:55:43 +0100

    envelope.mailFrom property could be false

--- types/smtp-server/index.d.ts
+++ types/smtp-server/index.d.ts
@@ -109,7 +109,7 @@ export interface SMTPServerEnvelope {
     /**
      * includes an address object or is set to false
      */
-    mailFrom: SMTPServerAddress;
+    mailFrom: SMTPServerAddress | false;
     /**
      * includes an array of address objects
      */
