83b4bb5b0eff4968cd6aa49cf195436ab6bbfa9b
Aleksei Tsikov <atsikov@evolutiongaming.com>
2018-02-21 20:54:09 +0200

    Update version properly

--- types/events/index.d.ts
+++ types/events/index.d.ts
@@ -1,4 +1,4 @@
-// Type definitions for events 1.1.1
+// Type definitions for events 1.2
 // Project: https://github.com/Gozala/events
 // Definitions by: Yasunori Ohoka <https://github.com/yasupeke>
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
