689987d90ba7a7b122ac21bdb6a3019031e2325f
Moritz Gunz <moritz.gunz@gmail.com>
2018-02-21 15:19:04 +0100

    Add definition creators

--- types/spotify-web-playback-sdk/index.d.ts
+++ types/spotify-web-playback-sdk/index.d.ts
@@ -1,6 +1,8 @@
 // Type definitions for spotify-web-playback-sdk 0.1
 // Project: https://beta.developer.spotify.com/documentation/web-playback-sdk/reference/
 // Definitions by: Festify Dev Team <https://github.com/Festify>
+//                 Marcus Weiner <https://github.com/mraerino>
+//                 Moritz Gunz <https://github.com/NeoLegends>
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
 
 interface Window {
