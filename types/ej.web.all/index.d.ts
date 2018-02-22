2e4ab6494c8f6e38e3c2a626b176af742f61c3b0
Syncfusion-JavaScript <packages@syncfusion.com>
2018-02-22 15:14:24 +0530

    Updated Typescript

--- types/ej.web.all/index.d.ts
+++ types/ej.web.all/index.d.ts
@@ -2,7 +2,7 @@
 // Project: http://help.syncfusion.com/js/typescript
 // Definitions by: Syncfusion <https://github.com/syncfusion>
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
-// TypeScript Version:2.3
+// TypeScript Version: 2.3
 
 /// <reference types="jquery" />
 
