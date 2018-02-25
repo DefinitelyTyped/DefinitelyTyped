0ce8d6cf775e53babe08db95135ca8c5f8b8d22c
leovujanic <leonard.vujanic@gmail.com>
2018-02-25 01:18:51 +0100

    Added type definitions for parity-pmr 0.1

---
+++ types/parity-pmr/parity-pmr-tests.ts
@@ -0,0 +1,30 @@
+/// <reference types="node" />
+import { format, parse, PMRMessage } from "parity-pmr";
+
+const buffer = new Buffer("test");
+const message: PMRMessage = {
+    messageType: 'E'
+};
+
+// $ExpectType Buffer
+format(message);
+
+// Invalid type
+// $ExpectError
+format('');
+
+// $ExpectError
+format({});
+
+// Invalid sub type
+// $ExpectError
+format({
+    messageType: 1
+});
+
+// $ExpectType PMRMessage
+parse(buffer);
+
+// Invalid type
+// $ExpectError
+parse('');
