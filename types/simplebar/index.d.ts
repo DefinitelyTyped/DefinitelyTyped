810f16088010e0b48b89d3285ad11012d83836df
Leonard Thieu <leonard.thieu@gmail.com>
2018-02-25 14:43:04 -0500

    [simplebar] Add missing author.

--- types/simplebar/index.d.ts
+++ types/simplebar/index.d.ts
@@ -1,6 +1,6 @@
 // Type definitions for simplebar.js 2.4
 // Project: https://github.com/Grsmto/simplebar
-// Definitions by: Leonard Thieu <https://github.com/leonard-thieu>
+// Definitions by: Gregor Woiwode <https://github.com/gregonnet>, Leonard Thieu <https://github.com/leonard-thieu>
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
 // TypeScript Version: 2.1
 
