8379cbc514c0a6e55663136f2fcd344468f9cf7a
AEPKILL <a@aepkill.com>
2018-02-25 15:31:26 +0800

    fix some error

--- types/sass-webpack-plugin/sass-webpack-plugin-tests.ts
+++ types/sass-webpack-plugin/sass-webpack-plugin-tests.ts
@@ -1,24 +1,20 @@
-import SassPlugin from 'sass-webpack-plugin';
+import SassPlugin, { NODE_ENV } from 'sass-webpack-plugin';
+
+const env = process.env.NODE_ENV as NODE_ENV;
 
 new SassPlugin('./src/styles/index.scss');
 
 // production ready
-new SassPlugin('./src/styles/index.scss', process.env.NODE_ENV);
+new SassPlugin('./src/styles/index.scss', env);
 
 // multi files
-new SassPlugin(
-  ['./src/styles/one.scss', './src/styles/two.sass'],
-  process.env.NODE_ENV
-);
+new SassPlugin(['./src/styles/one.scss', './src/styles/two.sass'], env);
 
 // a different output filename
-new SassPlugin(
-  { './src/styles/index.scss': 'bundle.css' },
-  process.env.NODE_ENV
-);
+new SassPlugin({ './src/styles/index.scss': 'bundle.css' }, env);
 
 // with sass tuning
-new SassPlugin('./src/styles/index.scss', process.env.NODE_ENV, {
+new SassPlugin('./src/styles/index.scss', env, {
   sass: {
     includePaths: ['node_modules/bootstrap-sass/assets/stylesheets']
   }
