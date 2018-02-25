048b6f2b71f43c23df72a089f33367371d7d4fc5
AEPKILL <a@aepkill.com>
2018-02-25 15:20:16 +0800

    pass lint

--- types/sass-webpack-plugin/index.d.ts
+++ types/sass-webpack-plugin/index.d.ts
@@ -1,6 +1,6 @@
-// Type definitions for sass-webpack-plugin 1.0.0
+// Type definitions for sass-webpack-plugin 1.0
 // Project: https://github.com/jalkoby/sass-webpack-plugin
-// Definitions by: AEPKILL <https://github.com/AepKill/>
+// Definitions by: AEPKILL <https://github.com/AepKill>
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
 // TypeScript Version: 2.6
 
