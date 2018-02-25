9550c81046aef251d46c1b9b7570676cdbf732b9
azu <azuciao@gmail.com>
2018-02-25 14:27:56 +0900

    Add type assertion

--- types/structured-source/structured-source-tests.ts
+++ types/structured-source/structured-source-tests.ts
@@ -2,13 +2,13 @@ import StructuredSource = require('structured-source');
 
 const src = new StructuredSource('aaa\u2028aaaa\u2029aaaaa\n');
 
-// positionToIndex({ line: number, column: number) -> number
+// $ExpectType: number
 src.positionToIndex({ line: 1, column: 2 });
-// indexToPosition(number) -> { line: number, column: number }
+// $ExpectType: { line: number, column: number }
 src.indexToPosition(2);
-// rangeToLocation([ number, number ]) -> { start: { line: number, column: number}, end: { line: number, column: number } }
+// $ExpectType: { start: { line: number, column: number}, end: { line: number, column: number } }
 src.rangeToLocation([0, 2]);
-// locationToRange({ start: { line: number, column: number}, end: { line: number, column: number } }) -> [ number, number ]
+// $ExpectType: [ number, number ]
 src.locationToRange({
     start: { line: 1, column: 0 },
     end: { line: 1, column: 2 }
