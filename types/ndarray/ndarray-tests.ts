aa8fbcae0e8e6e4844d9b244bf22d85dab3461dd
Brian Schlenker <bschlenk@umich.edu>
2018-02-25 19:23:54 -0800

    Export ndarray interface from typings

--- types/ndarray/ndarray-tests.ts
+++ types/ndarray/ndarray-tests.ts
@@ -22,3 +22,7 @@ console.log(a.get(0, 0, 0) === 1);
 console.log(a.index(1, 1, 1) === 12);
 
 const b = a.lo(0, 0, 0).hi(1, 1, 1);
+
+function returnsAnNdArray(): ndarray.ndarray {
+    return a;
+}
