cb82b1fa149024e5a8bd3ff91e05d8607788f7a6
Chase Willden <chase.willden@gmail.com>
2018-02-26 16:45:04 -0700

    Have DateTimePickerProps extend HTMLElement

--- types/react-flatpickr/index.d.ts
+++ types/react-flatpickr/index.d.ts
@@ -7,12 +7,11 @@
 import { Component } from 'react';
 import { Options } from 'flatpickr';
 
-export interface DateTimePickerProps {
+export interface DateTimePickerProps extends HTMLElement {
     defaultValue?: string;
     options?: Options.Options;
     onChange?: Options.Hook;
     value?: string;
-    className?: string;
 }
 
 export default class DatePicker extends Component<DateTimePickerProps> {}
