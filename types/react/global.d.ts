e327a3e895003b3ab3dd2cc055261b1094bd9724
Josh Goldberg <joshuakgoldberg@outlook.com>
2018-02-26 12:12:31 -0800

    Sentence fragment

--- types/react/global.d.ts
+++ types/react/global.d.ts
@@ -1,5 +1,5 @@
 /*
-React projects that don't include the DOM library these interfaces need to compile.
+React projects that don't include the DOM library need these interfaces to compile.
 React Native applications use React, but there is no DOM available. The JavaScript runtime
 is ES6/ES2015 only. These definitions allow such projects to compile with only `--lib ES6`.
 */
