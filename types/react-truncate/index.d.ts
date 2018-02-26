7c4520744af57b2e531c616ab5856acfbbf059be
Andy Hanson <anhans@microsoft.com>
2018-02-26 11:31:09 -0800

    fix version

--- types/react-truncate/index.d.ts
+++ types/react-truncate/index.d.ts
@@ -1,4 +1,4 @@
-// Type definitions for react-truncate 2.3.0
+// Type definitions for react-truncate 2.3
 // Project: https://github.com/One-com/react-truncate
 // Definitions by: Matt Perry <https://github.com/mattvperry>
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
