49592c787948dffce8634c81cd172e3624cd206d
Ben Grynhaus <bengr@microsoft.com>
2018-02-26 23:01:30 +0200

    Standartize ipcheck.d.ts

--- types/ipcheck/index.d.ts
+++ types/ipcheck/index.d.ts
@@ -4,28 +4,26 @@
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
 // TypeScript Version: 2.3
 
-interface IPCheck {
-    address: number[];
-    input: string;
-    ipv: 4 | 6 | 0;
-    mask: number;
-    valid: boolean;
+declare const ipcheck: ipcheck.IPCheckStatic;
+export = ipcheck;
+export as namespace ipcheck;
 
-    match(cidr: IPCheck | string): boolean;
+declare namespace ipcheck {
+    interface IPCheck {
+        address: number[];
+        input: string;
+        ipv: 4 | 6 | 0;
+        mask: number;
+        valid: boolean;
+
+        match(cidr: IPCheck | string): boolean;
+    }
+
+    interface IPCheckConstructor {
+        new (input: string): IPCheck;
+    }
+
+    interface IPCheckStatic extends IPCheckConstructor {
+        match(ip: IPCheck | string, cidr: IPCheck | string): boolean;
+    }
 }
-
-interface IPCheckConstructor {
-    new (input: string): IPCheck;
-}
-
-interface IPCheckStatic extends IPCheckConstructor {
-    match(ip: IPCheck | string, cidr: IPCheck | string): boolean;
-}
-
-declare module "ipcheck" {
-    var ipcheck: IPCheckStatic;
-
-    export = ipcheck;
-}
-
-declare var ipcheck: IPCheckStatic;
