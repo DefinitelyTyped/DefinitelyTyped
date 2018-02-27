cb961fa41311caaaefe376b9432505678d920d74
Chen Junda <smallda@outlook.com>
2018-02-27 11:25:53 +0800

    make it work with require

--- types/react-svg/index.d.ts
+++ types/react-svg/index.d.ts
@@ -5,30 +5,37 @@
 // TypeScript Version: 2.6
 
 import * as React from "react";
-export interface ReactSVGProps {
-  /**
-   * Path to the SVG.
-   */
-  path: string;
-  /**
-   * Function to call after the SVG is injected. Receives the newly injected SVG DOM element as a parameter. Defaults to null.
-   */
-  callback?: (dom: SVGSVGElement) => void;
-  /**
-   * Class name to be added to the SVG. Defaults to ''.
-   */
-  className?: string;
-  /**
-   * Class name to be added to the wrapping div. Defaults to ''.
-   */
-  wrapperClassName?: string;
-  /**
-   * Run any script blocks found in the SVG (always, once, or never). Defaults to never.
-   */
-  evalScripts?: 'always' | 'once' | 'never';
-  /**
-   * Inline styles to be added to the SVG. Defaults to {}.
-   */
-  style?: any;
+
+declare class ReactSVG extends React.Component<ReactSVG.ReactSVGProps> {}
+
+export = ReactSVG;
+
+declare namespace ReactSVG {
+
+  export interface ReactSVGProps {
+    /**
+     * Path to the SVG.
+     */
+    path: string;
+    /**
+     * Function to call after the SVG is injected. Receives the newly injected SVG DOM element as a parameter. Defaults to null.
+     */
+    callback?: (dom: SVGSVGElement) => void;
+    /**
+     * Class name to be added to the SVG. Defaults to ''.
+     */
+    className?: string;
+    /**
+     * Class name to be added to the wrapping div. Defaults to ''.
+     */
+    wrapperClassName?: string;
+    /**
+     * Run any script blocks found in the SVG (always, once, or never). Defaults to never.
+     */
+    evalScripts?: 'always' | 'once' | 'never';
+    /**
+     * Inline styles to be added to the SVG. Defaults to {}.
+     */
+    style?: any;
+  }
 }
-export default class ReactSVG extends React.Component<ReactSVGProps> { }
--- types/react-svg/react-svg-tests.tsx
+++ types/react-svg/react-svg-tests.tsx
@@ -1,6 +1,6 @@
 import * as React from 'react';
 import * as ReactDOM from 'react-dom';
-import ReactSVG from 'react-svg';
+import ReactSVG = require("react-svg");
 
 const complete = <ReactSVG
     path={"atomic.svg"}
