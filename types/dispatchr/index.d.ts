45fc12910b1aa34c6b616c3a2971c644b94602f1
Ragg <Ragg-@users.noreply.github.com>
2018-02-28 02:15:17 +0900

    Regenerate by dts-gen

--- types/dispatchr/index.d.ts
+++ types/dispatchr/index.d.ts
@@ -1,3 +1,7 @@
+// Type definitions for dispatchr 1.2
+// Project: https://github.com/yahoo/fluxible#readme
+// Definitions by: Ragg <https://github.com/Ragg->
+// Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
 // TypeScript Version: 2.3
 /// <reference types="node" />
 import { EventEmitter } from 'events';
