76cfb34e13ca0edc556d3fc48096fbf4b79fe186
Dennis George <dennispg@gmail.com>
2018-02-27 15:48:32 -0600

    updated webpack to be 4.0 specific; added v3 subfolder for old version

--- types/webpack/v3/index.d.ts
+++ types/webpack/v3/index.d.ts
@@ -1,4 +1,4 @@
-// Type definitions for webpack 4.0
+// Type definitions for webpack 3.8
 // Project: https://github.com/webpack/webpack
 // Definitions by: Qubo <https://github.com/tkqubo>
 //                 Benjamin Lim <https://github.com/bumbleblym>
@@ -35,8 +35,6 @@ declare function webpack(options: webpack.Configuration[]): webpack.MultiCompile
 
 declare namespace webpack {
     interface Configuration {
-        /** Specify which set of defaults to use. */
-        mode?: "development" | "production";
         /** Name of the configuration. Used when loading multiple configurations. */
         name?: string;
         /**
@@ -107,9 +105,6 @@ declare namespace webpack {
         performance?: Options.Performance;
         /** Limit the number of parallel processed modules. Can be used to fine tune performance or to get more reliable profiling results */
         parallelism?: number;
-
-        /** Optimization options */
-        optimization?: Options.Optimization;
     }
 
     interface Entry {
@@ -187,13 +182,9 @@ declare namespace webpack {
         sourcePrefix?: string;
         /** This option enables cross-origin loading of chunks. */
         crossOriginLoading?: string | boolean;
-        /** A Constructor to a custom hash function. You can provide a non-crypto hash function for performance reasons */
-        hashFunction?: (algorithm: string, options?: any) => any;
-        /** Allow to choose the global object reference in runtime exitCode. */
-        globalObject?: any;
     }
 
-    interface Module {
+    interface BaseModule {
         /** A array of applied pre loaders. */
         preLoaders?: Rule[];
         /** A array of applied post loaders. */
@@ -212,9 +203,17 @@ declare namespace webpack {
         wrappedContextRecursive?: boolean;
         wrappedContextCritical?: boolean;
         strictExportPresence?: boolean;
+    }
+    interface OldModule extends BaseModule {
+        /** An array of automatically applied loaders. */
+        loaders: Rule[];
+    }
+    interface NewModule extends BaseModule {
         /** An array of rules applied for modules. */
         rules: Rule[];
     }
+
+    type Module = OldModule | NewModule;
 
     interface Resolve {
         /**
@@ -377,10 +376,7 @@ declare namespace webpack {
         options?: { [name: string]: any };
     }
     type Loader = string | OldLoader | NewLoader;
-    interface ParserOptions {
-        [optName: string]: any;
-        system?: boolean;
-    }
+
     /**
      * There are direct and delegate rules. Direct Rules need a test, Delegate rules delegate to subrules bringing their own.
      * Direct rules can optionally contain delegate keys (oneOf, rules).
@@ -420,17 +416,11 @@ declare namespace webpack {
          * For each different parser options object a new parser is created and plugins can apply plugins depending on the parser options.
          * Many of the default plugins apply their parser plugins only if a property in the parser options is not set or true.
          */
-        parser?: ParserOptions;
+        parser?: { [optName: string]: any };
         /** An array of Rules that is also used when the Rule matches. */
         rules?: Rule[];
         /** An array of Rules from which only the first matching Rule is used when the Rule matches. */
         oneOf?: Rule[];
-        /** Configures module resolving. */
-        resolve?: Resolve;
-        /** Configures the module type. */
-        type?: "javascript/auto" | "javascript/esm" | "javascript/dynamic" | "json" | "webassembly/experimental";
-        /** TODO: Update this when the v4 documentation is complete. */
-        sideEffects?: any;
     }
     interface BaseDirectRule extends BaseRule {
         /** A condition that must be met */
@@ -506,56 +496,6 @@ declare namespace webpack {
         }
         type Stats = Stats.ToStringOptions;
         type WatchOptions = ICompiler.WatchOptions;
-
-        interface Optimization {
-            /**
-             *  Modules are removed from chunks when they are already available in all parent chunk groups.
-             *  This reduces asset size. Smaller assets also result in faster builds since less code generation has to be performed.
-             */
-            removeAvailableModules?: boolean;
-            /** Empty chunks are removed. This reduces load in filesystem and results in faster builds. */
-            removeEmptyChunks?: boolean;
-            /** Equal chunks are merged. This results in less code generation and faster builds. */
-            mergeDuplicateChunks?: boolean;
-            /** Chunks which are subsets of other chunks are determined and flagged in a way that subsets don’t have to be loaded when the bigger chunk has been loaded. */
-            flagIncludedChunks?: boolean;
-            /** Give more often used ids smaller (shorter) values. */
-            occurrenceOrder?: boolean;
-            /** Determine exports for each module when possible. This information is used by other optimizations or code generation. I. e. to generate more efficient code for export * from. */
-            providedExports?: boolean;
-            /**
-             *  Determine used exports for each module. This depends on optimization.providedExports. This information is used by other optimizations or code generation.
-             *  I. e. exports are not generated for unused exports, export names are mangled to single char identifiers when all usages are compatible.
-             *  DCE in minimizers will benefit from this and can remove unused exports.
-             */
-            usedExports?: boolean;
-            /**
-             *  Recognise the sideEffects flag in package.json or rules to eliminate modules. This depends on optimization.providedExports and optimization.usedExports.
-             *  These dependencies have a cost, but eliminating modules has positive impact on performance because of less code generation. It depends on your codebase.
-             *  Try it for possible performance wins.
-             */
-            sideEffects?: boolean;
-            /** Tries to find segments of the module graph which can be safely concatenated into a single module. Depends on optimization.providedExports and optimization.usedExports. */
-            concatenateModules?: boolean;
-            /** Finds modules which are shared between chunk and splits them into separate chunks to reduce duplication or separate vendor modules from application modules. */
-            splitChunks?: boolean;
-            /** Create a separate chunk for the webpack runtime code and chunk manifest. This chunk should be inlined into the HTML */
-            runtimeChunk?: boolean;
-            /** Don’t write output assets when compilation errors. */
-            noEmitOnErrors?: boolean;
-            /** Instead of numeric ids, give modules useful names. */
-            namedModules?: boolean;
-            /** Instead of numeric ids, give chunks useful names. */
-            namedChunks?: boolean;
-            /** Defines the process.env.NODE_ENV constant to a compile-time-constant value. This allows to remove development only code from code. */
-            nodeEnv?: boolean;
-            /** Use the minimizer (optimization.minimizer, by default uglify-js) to minimize output assets. */
-            minimize?: boolean;
-            /** Configure minimzers and options TODO: add type for this */
-            minimizer?: any;
-            /** Identifiers used in records are relative to context directory. */
-            portableRecords?: boolean;
-        }
     }
 
     // tslint:disable-next-line:interface-name
@@ -889,7 +829,6 @@ declare namespace webpack {
         constructor(options: any);
     }
 
-    /** @deprecated use config.optimization.namedModules */
     class NamedModulesPlugin extends Plugin {
         constructor();
     }
@@ -898,12 +837,15 @@ declare namespace webpack {
         constructor(nameResolver?: (chunk: any) => string | null);
     }
 
-    /** @deprecated use config.optimization.noEmitOnErrors */
     class NoEmitOnErrorsPlugin extends Plugin {
         constructor();
     }
 
     /** @deprecated use webpack.NoEmitOnErrorsPlugin */
+    class NoErrorsPlugin extends Plugin {
+        constructor();
+    }
+
     class NormalModuleReplacementPlugin extends Plugin {
         constructor(resourceRegExp: any, newResource: any);
     }
@@ -957,7 +899,6 @@ declare namespace webpack {
     }
 
     namespace optimize {
-        /** @deprecated use config.optimization.concatenateModules */
         class ModuleConcatenationPlugin extends Plugin { }
         class AggressiveMergingPlugin extends Plugin {
             constructor(options?: AggressiveMergingPlugin.Options);
@@ -981,6 +922,62 @@ declare namespace webpack {
                  * Defaults to false.
                  */
                 moveToParents?: boolean;
+            }
+        }
+
+        class CommonsChunkPlugin extends Plugin {
+            constructor(options?: CommonsChunkPlugin.Options);
+        }
+
+        namespace CommonsChunkPlugin {
+            type MinChunksFn = (module: any, count: number) => boolean;
+
+            interface Options {
+                /**
+                 * The chunk name of the commons chunk. An existing chunk can be selected by passing a name of an existing chunk.
+                 * If an array of strings is passed this is equal to invoking the plugin multiple times for each chunk name.
+                 * If omitted and `options.async` or `options.children` is set all chunks are used,
+                 * otherwise `options.filename` is used as chunk name.
+                 */
+                name?: string;
+                names?: string[];
+
+                /**
+                 * The filename template for the commons chunk. Can contain the same placeholders as `output.filename`.
+                 * If omitted the original filename is not modified (usually `output.filename` or `output.chunkFilename`).
+                 */
+                filename?: string;
+
+                /**
+                 * The minimum number of chunks which need to contain a module before it's moved into the commons chunk.
+                 * The number must be greater than or equal 2 and lower than or equal to the number of chunks.
+                 * Passing `Infinity` just creates the commons chunk, but moves no modules into it.
+                 * By providing a `function` you can add custom logic. (Defaults to the number of chunks)
+                 */
+                minChunks?: number | MinChunksFn;
+
+                /**
+                 * Select the source chunks by chunk names. The chunk must be a child of the commons chunk.
+                 * If omitted all entry chunks are selected.
+                 */
+                chunks?: string[];
+
+                /**
+                 * If `true` all children of the commons chunk are selected
+                 */
+                children?: boolean;
+
+                /**
+                 * If `true` a new async commons chunk is created as child of `options.name` and sibling of `options.chunks`.
+                 * It is loaded in parallel with `options.chunks`. It is possible to change the name of the output file
+                 * by providing the desired string instead of `true`.
+                 */
+                async?: boolean | string;
+
+                /**
+                 * Minimum size of all common module before a commons chunk is created.
+                 */
+                minSize?: number;
             }
         }
 
@@ -1201,6 +1198,11 @@ declare namespace webpack {
             inputValue: any;
 
             /**
+             * The options passed to the Compiler.
+             */
+            options: any;
+
+            /**
              * A boolean flag. It is set when in debug mode.
              */
             debug: boolean;
@@ -1254,9 +1256,6 @@ declare namespace webpack {
              * Hacky access to the Module object being loaded.
              */
             _module: any;
-
-            /** Flag if HMR is enabled */
-            hot: boolean;
         }
     }
 
