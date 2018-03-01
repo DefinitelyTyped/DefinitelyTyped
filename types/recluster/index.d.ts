b1052651ced21f77b60499e3b004f1b874ccf609
Daniel Rosenwasser <Daniel.Rosenwasser@microsoft.com>
2018-03-01 14:21:25 -0800

    Correct 'recluster' to a CJS 'export ='-style declaration.

--- types/recluster/index.d.ts
+++ types/recluster/index.d.ts
@@ -7,40 +7,43 @@
 
 import { ChildProcess } from "child_process";
 
-export interface LogOptions {
-  respawns?: boolean;
+declare namespace recluster {
+  export interface LogOptions {
+    respawns?: boolean;
+  }
+
+  export interface Logger {
+    log(message?: any, ...optionalParams: any[]): void;
+  }
+
+  export interface BalancerOptions {
+    /** number of active workers */
+    workers?: number;
+    /** kill timeout for old workers after reload (sec) */
+    timeout?: number;
+    /** min time between respawns when workers die */
+    respawn?: number;
+    /** max time between respawns when workers die */
+    backoff?: number;
+    /** when does the worker become ready? 'listening' or 'started' */
+    readyWhen?: string;
+    /** arguments to pass to the worker (default: []) */
+    args?: string[];
+    /** what to log to stdout (default: {respawns: true}) */
+    log?: LogOptions;
+    /** logger to use, needs `log` method (default: console) */
+    logger?: Logger;
+  }
+
+  export class Balancer {
+    constructor(file: string, options: BalancerOptions);
+    run(): void;
+    reload(cb?: () => void): void;
+    stop(): void;
+    workers(): ChildProcess[];
+    activeWorkers(): ChildProcess[];
+  }
 }
 
-export interface Logger {
-  log(message?: any, ...optionalParams: any[]): void;
-}
-
-export interface BalancerOptions {
-  /** number of active workers */
-  workers?: number;
-  /** kill timeout for old workers after reload (sec) */
-  timeout?: number;
-  /** min time between respawns when workers die */
-  respawn?: number;
-  /** max time between respawns when workers die */
-  backoff?: number;
-  /** when does the worker become ready? 'listening' or 'started' */
-  readyWhen?: string;
-  /** arguments to pass to the worker (default: []) */
-  args?: string[];
-  /** what to log to stdout (default: {respawns: true}) */
-  log?: LogOptions;
-  /** logger to use, needs `log` method (default: console) */
-  logger?: Logger;
-}
-
-export class Balancer {
-  constructor(file: string, options: BalancerOptions);
-  run(): void;
-  reload(cb?: () => void): void;
-  stop(): void;
-  workers(): ChildProcess[];
-  activeWorkers(): ChildProcess[];
-}
-
-export default function recluster(file: string, options?: BalancerOptions): Balancer;
+declare function recluster(file: string, options?: recluster.BalancerOptions): recluster.Balancer;
+export = recluster;
