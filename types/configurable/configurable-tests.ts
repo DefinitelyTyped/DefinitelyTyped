c16c5f1ac46dd08211803a01f505e4038e9f8a56
jewbre <jewbre18@gmail.com>
2018-03-01 22:47:38 +0100

    Added tests for errorsπ

--- types/configurable/configurable-tests.ts
+++ types/configurable/configurable-tests.ts
@@ -38,3 +38,63 @@ c.disable('first');
 
 // $ExpectType boolean
 c.disabled('first');
+
+// $ExpectType Configurable
+configTwo.set('second', 'first as a string');
+
+// $ExpectType any
+configTwo.get('second');
+
+// $ExpectType Configurable
+configTwo.enable('second');
+
+// $ExpectType boolean
+configTwo.enabled('second');
+
+// $ExpectType Configurable
+configTwo.disable('second');
+
+// $ExpectType boolean
+configTwo.disabled('second');
+
+// $ExpectError
+c.set(5, 'first as a string');
+
+// $ExpectError
+c.set('first');
+
+// $ExpectError
+c.get(5);
+
+// $ExpectError
+c.enable(5);
+
+// $ExpectError
+c.enabled(5);
+
+// $ExpectError
+c.disable(5);
+
+// $ExpectError
+c.disabled(5);
+
+// $ExpectError
+configTwo.set(5, 'second as a string');
+
+// $ExpectError
+configTwo.set('second');
+
+// $ExpectError
+configTwo.get(5);
+
+// $ExpectError
+configTwo.enable(5);
+
+// $ExpectError
+configTwo.enabled(5);
+
+// $ExpectError
+configTwo.disable(5);
+
+// $ExpectError
+configTwo.disabled(5);
