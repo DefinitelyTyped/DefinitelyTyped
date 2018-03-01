5b0ff4bc9f7b356fa0c1c6dc4e5628e85632912d
Christopher Woodland <christopher.woodland@pearson.com>
2018-03-01 16:03:15 -0500

    Add  Johnny Li <https://github.com/johnny4753> to  Definitions by

--- types/frisby/index.d.ts
+++ types/frisby/index.d.ts
@@ -1,6 +1,7 @@
 // Type definitions for Frisby 2.0
 // Project: https://github.com/vlucas/frisby
 // Definitions by: Christopher E. Woodland <https://github.com/cwoodland>
+//                 Johnny Li <https://github.com/johnny4753>
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
 // TypeScript Version: 2.6
 
