4c2a9739f6556eef47ed2d54add8eb39b0b139cb
Daniel Machado <dmachado@privacycloud.com>
2018-03-02 11:41:03 +0100

    Add me as a new author

--- types/boom/index.d.ts
+++ types/boom/index.d.ts
@@ -4,6 +4,7 @@
 //                 AJP <https://github.com/AJamesPhillips>
 //                 Jinesh Shah <https://github.com/jineshshah36>
 //                 Timon van Spronsen <https://github.com/TimonVS>
+//                 Daniel Machado <https://github.com/danielmachado>
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
 // TypeScript Version: 2.4
 
