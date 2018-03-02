a33ab0865cbe26f220db777dbc02d2cdfe466458
cafesanu <cafesanu@gmail.com>
2018-03-02 10:42:59 -0500

    chore - reformat test to have 4-space indentation

--- types/angular-ui-router/angular-ui-router-tests.ts
+++ types/angular-ui-router/angular-ui-router-tests.ts
@@ -5,8 +5,8 @@ import uiRouterModule from "angular-ui-router";
 var myApp = angular.module("testModule", [uiRouterModule]);
 
 interface MyAppScope extends ng.IScope {
-	items: string[];
-	things: string[];
+    items: string[];
+    things: string[];
 }
 
 myApp.config((
@@ -14,120 +14,144 @@ myApp.config((
     $urlRouterProvider: ng.ui.IUrlRouterProvider,
     $urlMatcherFactory: ng.ui.IUrlMatcherFactory) => {
 
-  var matcher: ng.ui.IUrlMatcher = $urlMatcherFactory.compile("/foo/:bar?param1");
+    var matcher: ng.ui.IUrlMatcher = $urlMatcherFactory.compile("/foo/:bar?param1");
 
-  $urlMatcherFactory.caseInsensitive(false);
-  var isCaseInsensitive = $urlMatcherFactory.caseInsensitive();
+    $urlMatcherFactory.caseInsensitive(false);
+    var isCaseInsensitive = $urlMatcherFactory.caseInsensitive();
 
-  $urlMatcherFactory.defaultSquashPolicy("nosquash");
+    $urlMatcherFactory.defaultSquashPolicy("nosquash");
 
-  $urlMatcherFactory.strictMode(true);
-  var isStrictMode = $urlMatcherFactory.strictMode();
+    $urlMatcherFactory.strictMode(true);
+    var isStrictMode = $urlMatcherFactory.strictMode();
 
-  $urlMatcherFactory.type("myType2", {
-    encode: function (item: any) { return item; },
-    decode: function (item: any) { return item; },
-    is: function (item: any) { return true; }
-  });
-
-  $urlMatcherFactory.type("fullType", {
-    decode: (val) => parseInt(val, 10),
-    encode: (val) => val && val.toString(),
-    equals: (a, b) => this.is(a) && a === b,
-    is: (val) => angular.isNumber(val) && isFinite(val) && val % 1 === 0,
-    pattern: /\d+/
-  });
-
-  var obj: Object = matcher.exec('/user/bob', { x:'1', q:'hello' });
-  var concat: ng.ui.IUrlMatcher = matcher.concat('/test');
-  var str: string = matcher.format({ id:'bob', q:'yes' });
-  var arr: string[] = matcher.parameters();
-
-  $urlRouterProvider
-      .when('/test', '/list')
-      .when('/test', '/list')
-      .when('/test', '/list')
-      .when(/\/test\d/, '/list')
-      .when(/\/test\d/, ($injector: ng.auto.IInjectorService, $location: ng.ILocationService) => '/list')
-      .when(/\/test\d/,['$injector', '$location', ($injector: ng.auto.IInjectorService, $location: ng.ILocationService) => '/list'])
-      .when(matcher, '/list')
-      .when(matcher, ($injector: ng.auto.IInjectorService, $location: ng.ILocationService) => '/list')
-      .when(matcher, ['$injector', '$location', ($injector: ng.auto.IInjectorService, $location: ng.ILocationService) => '/list'])
-      .otherwise("/state1");
-
-  // Now set up the states
-  $stateProvider
-    .state('state1', {
-      url: "/state1",
-      templateUrl: "partials/state1.html",
-      params: {
-        param1: "defaultValue",
-        param2: undefined
-      }
-    })
-    .state('state1.list', {
-      url: "/list",
-      templateUrl: "partials/state1.list.html",
-        controller: function ($scope: MyAppScope) {
-        $scope.items = ["A", "List", "Of", "Items"];
-      }
-    })
-    .state('state1.list', {
-      url: "/list",
-      templateUrl: "partials/state1.list.html",
-      controller: ['$scope', function ($scope: MyAppScope) {
-        $scope.items = ["A", "List", "Of", "Items"];
-      }]
-    })
-    .state('state2', {
-      url: "/state2",
-      templateUrl: "partials/state2.html"
-    })
-    .state('state2.list', {
-      url: "/list",
-        templateUrl: "partials/state2.list.html",
-        controller: function ($scope: MyAppScope) {
-          $scope.things = ["A", "Set", "Of", "Things"];
+    $urlMatcherFactory.type("myType2", {
+        encode: function(item: any) {
+            return item;
+        },
+        decode: function(item: any) {
+            return item;
+        },
+        is: function(item: any) {
+            return true;
         }
-      })
-    .state('list', {
-      parent: 'state3',
-      url: "/list",
-      templateUrl: "partials/state3.list.html",
-      controller: function ($scope: MyAppScope) {
-        $scope.things = ["A", "Set", "Of", "Things"];
-      }
-    })
-    .state('state4', {
-      url: "/state4",
-      templateUrl: function($stateParams: ng.ui.IStateParamsService){
-        //Logic could go here based on $stateParams
-        return "partials/state4.html";
-      }
-    })
-    .state('index', {
-      url: "",
-      views: {
-        "viewA": { template: "index.viewA" },
-        "viewB": { template: "index.viewB" }
-      }
-    })
-    .state('route1', {
-      url: "/route1",
-      views: {
-        "viewA": { template: "route1.viewA" },
-        "viewB": { template: "route1.viewB" }
-      }
-    })
-    .state('route2', {
-      url: "/route2",
-      views: {
-        "viewA": { template: "route2.viewA" },
-        "viewB": { template: "route2.viewB" }
-      }
     });
 
-  $stateProvider.state("route2", {
+    $urlMatcherFactory.type("fullType", {
+        decode: (val) => parseInt(val, 10),
+        encode: (val) => val && val.toString(),
+        equals: (a, b) => this.is(a) && a === b,
+        is: (val) => angular.isNumber(val) && isFinite(val) && val % 1 === 0,
+        pattern: /\d+/
+    });
+
+    var obj: Object = matcher.exec('/user/bob', {
+        x: '1',
+        q: 'hello'
+    });
+    var concat: ng.ui.IUrlMatcher = matcher.concat('/test');
+    var str: string = matcher.format({
+        id: 'bob',
+        q: 'yes'
+    });
+    var arr: string[] = matcher.parameters();
+
+    $urlRouterProvider
+        .when('/test', '/list')
+        .when('/test', '/list')
+        .when('/test', '/list')
+        .when(/\/test\d/, '/list')
+        .when(/\/test\d/, ($injector: ng.auto.IInjectorService, $location: ng.ILocationService) => '/list')
+        .when(/\/test\d/, ['$injector', '$location', ($injector: ng.auto.IInjectorService, $location: ng.ILocationService) => '/list'])
+        .when(matcher, '/list')
+        .when(matcher, ($injector: ng.auto.IInjectorService, $location: ng.ILocationService) => '/list')
+        .when(matcher, ['$injector', '$location', ($injector: ng.auto.IInjectorService, $location: ng.ILocationService) => '/list'])
+        .otherwise("/state1");
+
+    // Now set up the states
+    $stateProvider
+        .state('state1', {
+            url: "/state1",
+            templateUrl: "partials/state1.html",
+            params: {
+                param1: "defaultValue",
+                param2: undefined
+            }
+        })
+        .state('state1.list', {
+            url: "/list",
+            templateUrl: "partials/state1.list.html",
+            controller: function($scope: MyAppScope) {
+                $scope.items = ["A", "List", "Of", "Items"];
+            }
+        })
+        .state('state1.list', {
+            url: "/list",
+            templateUrl: "partials/state1.list.html",
+            controller: ['$scope', function($scope: MyAppScope) {
+                $scope.items = ["A", "List", "Of", "Items"];
+            }]
+        })
+        .state('state2', {
+            url: "/state2",
+            templateUrl: "partials/state2.html"
+        })
+        .state('state2.list', {
+            url: "/list",
+            templateUrl: "partials/state2.list.html",
+            controller: function($scope: MyAppScope) {
+                $scope.things = ["A", "Set", "Of", "Things"];
+            }
+        })
+        .state('list', {
+            parent: 'state3',
+            url: "/list",
+            templateUrl: "partials/state3.list.html",
+            controller: function($scope: MyAppScope) {
+                $scope.things = ["A", "Set", "Of", "Things"];
+            }
+        })
+        .state('state4', {
+            url: "/state4",
+            templateUrl: function($stateParams: ng.ui.IStateParamsService) {
+                //Logic could go here based on $stateParams
+                return "partials/state4.html";
+            }
+        })
+        .state('index', {
+            url: "",
+            views: {
+                "viewA": {
+                    template: "index.viewA"
+                },
+                "viewB": {
+                    template: "index.viewB"
+                }
+            }
+        })
+        .state('route1', {
+            url: "/route1",
+            views: {
+                "viewA": {
+                    template: "route1.viewA"
+                },
+                "viewB": {
+                    template: "route1.viewB"
+                }
+            }
+        })
+        .state('route2', {
+            url: "/route2",
+            views: {
+                "viewA": {
+                    template: "route2.viewA"
+                },
+                "viewB": {
+                    template: "route2.viewB"
+                }
+            }
+        });
+
+    $stateProvider.state("route2", {
         views: {
             viewA: "AComponent"
         }
@@ -184,7 +208,7 @@ class UrlLocatorTestService implements IUrlLocatorTestService {
     ) {
         $rootScope.$on("$locationChangeSuccess", (event: ng.IAngularEvent) => this.onLocationChangeSuccess(event));
         $rootScope.$on('$stateNotFound', (event: ng.IAngularEvent, unfoundState: ng.ui.IUnfoundState, fromState: ng.ui.IState, fromParams: {}) =>
-                                              this.onStateNotFound(event, unfoundState, fromState, fromParams));
+            this.onStateNotFound(event, unfoundState, fromState, fromParams));
     }
 
     public currentUser: any;
@@ -197,7 +221,10 @@ class UrlLocatorTestService implements IUrlLocatorTestService {
 
             // Note that we do not concern ourselves with what to do if this request fails,
             // because if it fails, the web page will be redirected away to the login screen.
-            this.$http({ url: "/api/me", method: "GET" }).then((response: ng.IHttpResponse<any>) => {
+            this.$http({
+                url: "/api/me",
+                method: "GET"
+            }).then((response: ng.IHttpResponse < any > ) => {
                 this.currentUser = response.data;
 
                 // sync the ui-state with the location in the browser, which effectively
@@ -207,10 +234,10 @@ class UrlLocatorTestService implements IUrlLocatorTestService {
         }
     }
 
-     private onStateNotFound(event: ng.IAngularEvent,
-                             unfoundState: ng.ui.IUnfoundState,
-                             fromState: ng.ui.IState,
-                             fromParams: {}) {
+    private onStateNotFound(event: ng.IAngularEvent,
+        unfoundState: ng.ui.IUnfoundState,
+        fromState: ng.ui.IState,
+        fromParams: {}) {
         var unfoundTo: string = unfoundState.to;
         var unfoundToParams: {} = unfoundState.toParams;
         var unfoundOptions: ng.ui.IStateOptions = unfoundState.options
@@ -222,13 +249,13 @@ class UrlLocatorTestService implements IUrlLocatorTestService {
         this.$state.transitionTo("myState");
         this.$state.transitionTo(this.$state.current);
         if (this.$state.includes("myState") === true) {
-          //
+            //
         }
         if (this.$state.is("myState") === true) {
-          //
+            //
         }
         if (this.$state.href("myState") === "/myState") {
-          //
+            //
         }
         this.$state.get();
         this.$state.get("myState");
@@ -246,16 +273,16 @@ class UrlLocatorTestService implements IUrlLocatorTestService {
 
         // http://angular-ui.github.io/ui-router/site/#/api/ui.router.state.$state#properties
         if (this.$state.transition) {
-          var transitionPromise: ng.IPromise<{}> = this.$state.transition;
-          transitionPromise.then(() => {
-            // transition success
-          }, () => {
-            // transition failure
-          }).catch(() => {
-            // transition failure
-          }).finally(() => {
-            // transition ended (success or failure)
-          });
+            var transitionPromise: ng.IPromise < {} > = this.$state.transition;
+            transitionPromise.then(() => {
+                // transition success
+            }, () => {
+                // transition failure
+            }).catch(() => {
+                // transition failure
+            }).finally(() => {
+                // transition ended (success or failure)
+            });
         }
 
         // Accesses the currently resolved values for the current state
@@ -282,7 +309,7 @@ namespace UiViewScrollProviderTests {
 
 interface ITestUserService {
     isLoggedIn: () => boolean;
-    handleLogin: () => ng.IPromise<{}>;
+    handleLogin: () => ng.IPromise < {} > ;
 }
 
 namespace UrlRouterProviderTests {
