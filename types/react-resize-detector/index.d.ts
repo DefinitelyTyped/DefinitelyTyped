3f1fafbac699e6fa0c2912f634c0334482d2727d
Matthew James <matthew.d.james87@gmail.com>
2018-03-02 22:32:45 +0000

    Added comments

--- types/react-resize-detector/index.d.ts
+++ types/react-resize-detector/index.d.ts
@@ -7,10 +7,15 @@
 import * as React from "react";
 
 interface ReactResizeDetectorProps extends React.Props<ReactResizeDetector> {
+    /** Function that will be invoked with width and height arguments */
     onResize: (width: number, height: number) => void;
+    /** Trigger onResize on height change. Default: false */
     handleHeight?: boolean;
+    /** Trigger onResize on width change. Default: false */
     handleWidth?: boolean;
+    /** Do not trigger onResize when a component mounts. Default: false */
     skipOnMount?: boolean;
+    /** Id of the element we want to observe. If none provided, parentElement of the component will be used. Default: "" */
     resizableElementId?: string;
 }
 
