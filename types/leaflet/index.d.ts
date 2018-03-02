d15cc69dfb10fc10e80c921917d7798387679b9e
Michael Auer <auer@uni-heidelberg.de>
2018-03-02 15:19:40 +0100

    Update index.d.ts
    changed "Definitions by" in header section

--- types/leaflet/index.d.ts
+++ types/leaflet/index.d.ts
@@ -2,6 +2,7 @@
 // Project: https://github.com/Leaflet/Leaflet
 // Definitions by: Alejandro Sánchez <https://github.com/alejo90>
 //                 Arne Schubert <https://github.com/atd-schubert>
+//                 Michael Auer <https://github.com/mcauer>
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
 // TypeScript Version: 2.3
 
