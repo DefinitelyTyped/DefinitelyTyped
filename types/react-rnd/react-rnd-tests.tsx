55e319a33580fc89fbcab067c4368f23e0655100
Ragg <Ragg-@users.noreply.github.com>
2018-03-03 18:56:54 +0900

    Export handler and options types

--- types/react-rnd/react-rnd-tests.tsx
+++ types/react-rnd/react-rnd-tests.tsx
@@ -1,4 +1,20 @@
 import * as React from 'react';
 import Rnd from 'react-rnd';
 
-<Rnd  />;
+const onResize: Rnd.ResizeHandler = (e, direction, ref, delta, position) => {
+    direction === 'right';
+    delta.width;
+    delta.height;
+    position.x;
+    position.y;
+};
+
+<Rnd
+    className="class"
+    lockAspectRatio
+    maxWidth={100}
+    enableResizing={false}
+    resizeHandleStyles={{
+        top: { background: '#000' }
+    }}
+/>;
