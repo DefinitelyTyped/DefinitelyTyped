578ce030e0a57abad1dfb6e1c7e2143d00817422
Dusan Radovanovic <dusan.radovanovic@elasticpath.com>
2018-03-05 00:55:16 -0800

    Exported Listr types through namespace

--- types/listr/index.d.ts
+++ types/listr/index.d.ts
@@ -7,39 +7,41 @@
 
 import * as stream from "stream";
 
-interface ListrRenderer {
-    nonTTY: boolean;
-    render(): void;
-    end(): void;
-}
+declare namespace Listr {
+    interface ListrRenderer {
+        nonTTY: boolean;
+        render(): void;
+        end(): void;
+    }
 
-interface ListrOptions {
-    concurrent?: boolean | number;
-    exitOnError?: boolean;
-    renderer?: "silent" | "default" | "verbose" | ListrRenderer;
-    nonTTYRenderer?: "silent" | "default" | "verbose" | ListrRenderer;
-}
+    interface ListrOptions {
+        concurrent?: boolean | number;
+        exitOnError?: boolean;
+        renderer?: "silent" | "default" | "verbose" | ListrRenderer;
+        nonTTYRenderer?: "silent" | "default" | "verbose" | ListrRenderer;
+    }
 
-interface ListrTask {
-    title: string;
-    output?: string;
-    task: (ctx: any, task: ListrTaskWrapper) => void | string | Promise<any> | stream.Readable | Listr;
-    skip?: (ctx: any, task: ListrTaskWrapper) => boolean | Promise<boolean> | string | void;
-    enabled?: (ctx: any, task: ListrTaskWrapper) => boolean | Promise<boolean>;
-}
+    interface ListrTask {
+        title: string;
+        output?: string;
+        task: (ctx: any, task: ListrTaskWrapper) => void | string | Promise<any> | stream.Readable | Listr;
+        skip?: (ctx: any, task: ListrTaskWrapper) => boolean | Promise<boolean> | string | void;
+        enabled?: (ctx: any, task: ListrTaskWrapper) => boolean | Promise<boolean>;
+    }
 
-interface ListrTaskWrapper {
-    title: string;
-    output: any;
-    report(error: Error): void;
-    skip(message: string): void;
-    run(ctx?: any): Promise<any>;
+    interface ListrTaskWrapper {
+        title: string;
+        output: any;
+        report(error: Error): void;
+        skip(message: string): void;
+        run(ctx?: any): Promise<any>;
+    }
 }
 
 declare class Listr {
-    constructor(tasks?: ReadonlyArray<ListrTask>, options?: ListrOptions);
-    constructor(options?: ListrOptions);
-    add(tasks: ListrTask | ReadonlyArray<ListrTask>): void;
+    constructor(tasks?: ReadonlyArray<Listr.ListrTask>, options?: Listr.ListrOptions);
+    constructor(options?: Listr.ListrOptions);
+    add(tasks: Listr.ListrTask | ReadonlyArray<Listr.ListrTask>): void;
     run(ctx?: any): Promise<any>;
 }
 
