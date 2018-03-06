088c5e4f4ecf146cd07beaf9636607ec464a0914
Craig Boland <craig.boland@gmail.com>
2018-03-06 12:42:37 -0600

    Added author credits

--- types/bootstrap3-dialog/index.d.ts
+++ types/bootstrap3-dialog/index.d.ts
@@ -1,6 +1,7 @@
 // Type definitions for bootstrap3-dialog 1.35
 // Project: http://nakupanda.github.io/bootstrap3-dialog
-// Definitions by: Craig Boland <https://github.com/cnboland>
+// Definitions by: Dante <https://github.com/nakupanda>
+//                 Craig Boland <https://github.com/cnboland>
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
 // TypeScript Version: 2.3
 
