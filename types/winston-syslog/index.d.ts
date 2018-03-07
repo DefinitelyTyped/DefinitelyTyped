a3c4aadbeb3bdfcbdceb1c6e74a7e3142a3b2351
Chris Barth <chrisjbarth@hotmail.com>
2018-03-07 10:25:21 -0500

    Add typings for winston-syslog

---
+++ types/winston-syslog/index.d.ts
@@ -0,0 +1,25 @@
+// Type definitions for winston-syslog 1.0
+// Project: https://github.com/indexzero/winston-syslog#readme
+// Definitions by: Chris Barth <https://github.com/cjbarth>
+// Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
+
+import * as winston from "winston";
+export interface SyslogTransportOptions extends winston.GenericTextTransportOptions {
+	host?: string;
+	port?: number;
+	path?: string;
+	protocol?: string;
+	pid?: number;
+	facility?: string;
+	localhost?: string;
+	type?: string;
+	app_name?: string;
+	eol?: string;
+}
+export class Syslog extends winston.Transport implements winston.TransportInstance {
+}
+declare module "winston" {
+	interface Transports {
+		Syslog: Syslog;
+	}
+}
