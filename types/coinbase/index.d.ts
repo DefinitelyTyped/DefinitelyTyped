c6cc85cd99143cbb5100399719aaa7775dad3e28
Benny Neugebauer <bn@bennyn.de>
2018-03-08 23:31:22 +0100

    Adjusted header file

--- types/coinbase/index.d.ts
+++ types/coinbase/index.d.ts
@@ -1,4 +1,4 @@
-// Type definitions for coinbase 2.0
+// Type definitions for coinbase 2.0.0
 // Project: https://github.com/coinbase/coinbase-node
 // Definitions by: Rogier Schouten <https://github.com/rogierschouten>
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
