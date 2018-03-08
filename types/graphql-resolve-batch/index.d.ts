4b05fa0d9ddbdcf32f412f3e932f06967f7194e6
nayni <rutgerhendrickx@gmail.com>
2018-03-08 18:17:09 +0100

    graphql-resolve-batch: bump version

--- types/graphql-resolve-batch/index.d.ts
+++ types/graphql-resolve-batch/index.d.ts
@@ -1,4 +1,4 @@
-// Type definitions for graphql-resolve-batch 1.0
+// Type definitions for graphql-resolve-batch 1.1
 // Project: https://github.com/calebmer/graphql-resolve-batch#readme
 // Definitions by: Rutger Hendrickx <https://github.com/nayni>
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
