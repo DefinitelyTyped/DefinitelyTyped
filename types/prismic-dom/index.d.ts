2f3607131c9716b01fcd7e7f76cb82764fb9b4ac
Nick Whyte <nick@nickwhyte.com>
2018-03-09 12:32:31 +1100

    .

--- types/prismic-dom/index.d.ts
+++ types/prismic-dom/index.d.ts
@@ -1,8 +1,8 @@
 // Type definitions for prismic-dom 2.0
 // Project: https://github.com/prismicio/prismic-dom#readme
-// Definitions by: Nick Whyte <https://github.com/nickw444/>
+// Definitions by: Nick Whyte <https://github.com/nickw444>
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
 
-export class RichText {
-    static asHtml(richText: any, linkResolver?: (doc: any) => string): string;
+export namespace RichText {
+    function asHtml(richText: any, linkResolver?: (doc: any) => string): string;
 }
