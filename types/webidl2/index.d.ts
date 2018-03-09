5305f69e77728b820b396378837e277cf6ee98e0
Kagami Sascha Rosylight <saschanaz@outlook.com>
2018-03-09 14:01:58 +0900

    use dts-gen

--- types/webidl2/index.d.ts
+++ types/webidl2/index.d.ts
@@ -1,7 +1,9 @@
-// Type definitions for webidl2.js 10.2
-// Project: https://github.com/w3c/webidl2.js/
+// Type definitions for webidl2 10.2
+// Project: https://github.com/w3c/webidl2.js#readme
 // Definitions by: Kagama Sascha Rosylight <https://github.com/saschanaz>
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
+
+export as namespace WebIDL2;
 
 export function parse(str: string, options?: ParseOptions): IDLRootType[];
 
