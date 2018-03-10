a21f8e8f4165c393c7671f4b24f8ae0849260c93
Divyendu Singh <divyendu.z@gmail.com>
2018-03-10 14:32:35 +0530

    conform with structure of graphql-js in graphql typings

--- types/graphql/utilities/buildASTSchema.d.ts
+++ types/graphql/utilities/buildASTSchema.d.ts
@@ -1,12 +1,17 @@
 import { DocumentNode, Location, StringValueNode } from '../language/ast';
 import { Source } from '../language/source';
-import { GraphQLSchema } from '../type/schema';
+import { GraphQLSchema, GraphQLSchemaValidationOptions } from '../type/schema';
 
-type BuildSchemaOptions = {
-    assumeValid?: boolean;
-    allowedLegacyNames?: ReadonlyArray<string>;
-    commentDescriptions?: boolean;
-};
+interface BuildSchemaOptions extends GraphQLSchemaValidationOptions {
+  /**
+   * Descriptions are defined as preceding string literals, however an older
+   * experimental version of the SDL supported preceding comments as
+   * descriptions. Set to true to enable this deprecated behavior.
+   *
+   * Default: false
+   */
+  commentDescriptions?: boolean;
+}
 
 /**
  * This takes the ast of a schema document produced by the parse function in
@@ -21,13 +26,18 @@ type BuildSchemaOptions = {
 export function buildASTSchema(ast: DocumentNode): GraphQLSchema;
 
 /**
- * Given an ast node, returns its string description based on a contiguous
- * block full-line of comments preceding it.
+ * Given an ast node, returns its string description.
+ *
+ * Accepts options as a second argument:
+ *
+ *    - commentDescriptions:
+ *        Provide true to use preceding comments as the description.
+ *
  */
 export function getDescription(
-    node: { description?: StringValueNode; loc?: Location },
-    options: BuildSchemaOptions
-  ): string;
+  node: { description?: StringValueNode; loc?: Location },
+  options: BuildSchemaOptions
+): string;
 
 /**
  * A helper function to build a GraphQLSchema directly from a source
