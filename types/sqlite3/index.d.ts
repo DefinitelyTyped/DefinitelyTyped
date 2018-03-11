6d7596886844c498730bf91e34287d62a1459689
Behind The Math <info@behindthemath.io>
2018-03-11 16:10:12 -0400

    Bug fixes

--- types/sqlite3/index.d.ts
+++ types/sqlite3/index.d.ts
@@ -1,4 +1,4 @@
-// Type definitions for sqlite3 3.1.13
+// Type definitions for sqlite3 3.1
 // Project: https://github.com/mapbox/node-sqlite3
 // Definitions by: Nick Malaguti <https://github.com/nmalaguti>
 //                 Sumant Manne <https://github.com/dpyro>
