fe2683d345a58d267c33a6ca110449af7692ddeb
Fu Lain Weng <turtle11311@gmail.com>
2018-03-12 17:12:38 +0800

    Add single import test

--- types/nodegit/nodegit-tests.ts
+++ types/nodegit/nodegit-tests.ts
@@ -51,3 +51,58 @@ const blameOptions = new Git.BlameOptions();
 Git.Branch.lookup(repo, "branch_name", Git.Branch.BRANCH.LOCAL).then((reference) => {
     // Use reference
 });
+
+
+// test single import
+import { AnnotatedCommit, Attr, BlameOptions, Branch, Clone, Reference, Repository, Oid } from 'nodegit';
+Repository.discover("startPath", 1, "ceilingDirs").then((string) => {
+    // Use string
+});
+
+Repository.init("path", 0).then((repository) => {
+    // Use repository
+});
+
+const repo_s = new Repository();
+const id_s = new Oid();
+const ref_s = new Reference();
+
+// AnnotatedCommit Tests
+
+AnnotatedCommit.fromFetchhead(repo_s, "branch_name", "remote_url", id_s).then((annotatedCommit) => {
+    // Use annotatedCommit
+});
+
+AnnotatedCommit.fromRef(repo_s, ref_s).then((annotatedCommit) => {
+    // Use annotatedCommit
+});
+
+AnnotatedCommit.fromRevspec(repo_s, "revspec").then((annotatedCommit) => {
+    // Use annotatedCommit
+});
+
+AnnotatedCommit.lookup(repo_s, id_s).then((annotatedCommit) => {
+    // Use annotatedCommit
+    annotatedCommit.free();
+    annotatedCommit.id();
+});
+
+// Attr tests
+
+let result_s = Attr.addMacro(repo_s, "name", "values");
+
+Attr.cacheFlush(repo_s);
+
+Attr.get(repo, 1, "path", "name").then((string) => {
+    // Use string
+});
+
+const array_s = Attr.getMany(repo_s, 1, "path", 1, "names");
+
+result_s = Attr.value("attr");
+
+const blameOptions_s = new BlameOptions();
+
+Branch.lookup(repo_s, "branch_name", Branch.BRANCH.LOCAL).then((reference) => {
+    // Use reference
+});
