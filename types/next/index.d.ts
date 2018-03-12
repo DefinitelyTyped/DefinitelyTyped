bcf98ec1f542feb91866a95818824960ec0c796f
James Hegedus <jthegedus@jttech.io>
2018-03-12 20:38:23 +1100

    add self to receive update notifications

--- types/next/index.d.ts
+++ types/next/index.d.ts
@@ -2,6 +2,7 @@
 // Project: https://github.com/zeit/next.js
 // Definitions by: Drew Hays <https://github.com/dru89>
 //                 Brice BERNARD <https://github.com/brikou>
+//                 James Hegedus <https://github.com/jthegedus>
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
 // TypeScript Version: 2.6
 
