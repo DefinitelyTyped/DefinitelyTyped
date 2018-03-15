fdd4f121420c940837f2bffa09ab96f9ff9d70d3
Hoishin <hoishinxii@gmail.com>
2018-03-15 12:13:11 +0900

    Update "definition by"

--- types/trash/index.d.ts
+++ types/trash/index.d.ts
@@ -1,6 +1,7 @@
 // Type definitions for trash 4.2
 // Project: https://github.com/sindresorhus/trash#readme
 // Definitions by: Matthew James <https://github.com/matthew-matvei>
+//                 Keiichiro Amemiya <https://github.com/hoishin>
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
 
 interface TrashOptions {
