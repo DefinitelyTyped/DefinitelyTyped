2806e24e92b604a9d51f769bdef0fe4525bd9cd6
Pat Gaffney <pat@hypepat.com>
2018-03-16 13:11:23 -0500

    reactstrap: Add hideArrow PropType to <Popover/>

--- types/reactstrap/reactstrap-tests.tsx
+++ types/reactstrap/reactstrap-tests.tsx
@@ -2533,7 +2533,7 @@ class PopoverItem extends React.Component<any, any> {
         <Button className="mr-1" color="secondary" id={'Popover-' + this.props.id} onClick={this.toggle}>
           {this.props.item.text}
         </Button>
-        <Popover placement={this.props.item.placement} isOpen={this.state.popoverOpen} target={'Popover-' + this.props.id} toggle={this.toggle}>
+        <Popover placement={this.props.item.placement} isOpen={this.state.popoverOpen} target={'Popover-' + this.props.id} toggle={this.toggle} hideArrow={true}>
           <PopoverHeader>Popover Title</PopoverHeader>
           <PopoverBody>Sed posuere consectetur est at lobortis. Aenean eu leo quam. Pellentesque ornare sem lacinia quam venenatis vestibulum.</PopoverBody>
         </Popover>
