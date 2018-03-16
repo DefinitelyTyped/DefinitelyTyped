ad851008d6ca570ff27fcd191bc78244c22513db
James Munro <me@jdmunro.net>
2018-03-16 17:22:36 +0000

    Updated authors.

--- types/mysql/index.d.ts
+++ types/mysql/index.d.ts
@@ -3,6 +3,7 @@
 // Definitions by:  William Johnston <https://github.com/wjohnsto>
 // 	                Kacper Polak <https://github.com/kacepe>
 // 	                Krittanan Pingclasai <https://github.com/kpping>
+// 	                James Munro <https://github.com/jdmunro>
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
 // TypeScript Version: 2.1
 
