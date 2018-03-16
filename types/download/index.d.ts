7eca634065b9894f2fb5a3ecf7ad32c06b8fb117
Dominique <domi.rau@gmail.com>
2018-03-16 17:50:26 +0100

    chore(ts): change typescript version dependancy

--- types/download/index.d.ts
+++ types/download/index.d.ts
@@ -2,7 +2,7 @@
 // Project: https://github.com/kevva/download
 // Definitions by: Nico Jansen <https://github.com/nicojs>
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
-// TypeScript Version: 2.2
+// TypeScript Version: 2.3
 
 /// <reference types="node" />
 import { DecompressOptions } from 'decompress';
