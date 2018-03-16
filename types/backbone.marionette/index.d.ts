11421a98b0b55b6cd28cc3561960f4aa02653b05
Volker Nauruhn <nauruhn@cynatix.com>
2018-03-16 17:14:57 +0100

    added me to contributor list

--- types/backbone.marionette/index.d.ts
+++ types/backbone.marionette/index.d.ts
@@ -1,6 +1,9 @@
 // Type definitions for Marionette 3.3
 // Project: https://github.com/marionettejs/
-// Definitions by: Zeeshan Hamid <https://github.com/zhamid>, Natan Vivo <https://github.com/nvivo>, Sven Tschui <https://github.com/sventschui>
+// Definitions by: Zeeshan Hamid <https://github.com/zhamid>,
+//                 Natan Vivo <https://github.com/nvivo>,
+//                 Sven Tschui <https://github.com/sventschui>,
+//                 Volker Nauruhn <https://github.com/razorness>
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
 // TypeScript Version: 2.3
 
