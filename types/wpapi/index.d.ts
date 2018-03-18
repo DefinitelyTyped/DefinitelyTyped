cd32df9e19e8ac08eaf3ba28b4de072237653d99
Guo Yunhe <guoyunhebrave@gmail.com>
2018-03-18 19:39:04 +0200

    wpapi: restrict typescript 2.2

--- types/wpapi/index.d.ts
+++ types/wpapi/index.d.ts
@@ -1,4 +1,5 @@
 // Type definitions for wpapi 1.1
+// TypeScript Version: 2.2
 // Project: https://github.com/wp-api/node-wpapi
 // Definitions by: Guo Yunhe <https://github.com/guoyunhe>
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
