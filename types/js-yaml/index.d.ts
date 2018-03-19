68ca3aca7c5b403a1ef105dbb4bd96799cee8ef1
Fred K. Schott <fschott@google.com>
2018-03-19 10:53:22 -0700

    update version

--- types/js-yaml/index.d.ts
+++ types/js-yaml/index.d.ts
@@ -1,4 +1,4 @@
-// Type definitions for js-yaml 3.10
+// Type definitions for js-yaml 3.11
 // Project: https://github.com/nodeca/js-yaml
 // Definitions by: Bart van der Schoor <https://github.com/Bartvds>, Sebastian Clausen <https://github.com/sclausen>
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
