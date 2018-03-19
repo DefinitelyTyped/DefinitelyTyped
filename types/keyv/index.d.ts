517e90e1cffdf93be26169ebdbf281b60a25fded
Arylo <arylo.open@gmail.com>
2018-03-19 15:13:43 +0800

    Add Annotations

--- types/keyv/index.d.ts
+++ types/keyv/index.d.ts
@@ -6,24 +6,50 @@
 
 /// <reference types="node" />
 
-// import * as events from 'events';
-
 interface KeyvOptions {
+    /** Namespace for the current instance. */
     namespace?: string;
+    /** A custom serialization function. */
     serialize: (data: any) => string;
+    /** A custom deserialization function. */
     deserialize: (data: string) => any;
+    /** The connection string URI. */
     uri?: string;
+    /** The storage adapter instance to be used by Keyv. */
     store?: any;
+    /** Default TTL. Can be overridden by specififying a TTL on `.set()`. */
     ttl?: number;
+    /** Specify an adapter to use. e.g `'redis'` or `'mongodb'`. */
     adapter?: string;
 }
 
 declare class Keyv extends NodeJS.EventEmitter {
+    /**
+     * @param uri The connection string URI.
+     *
+     * Merged into the options object as options.uri.
+     * @param opts The options object is also passed through to the storage adapter. Check your storage adapter docs for any extra options.
+     */
     constructor(uri?: string, opts?: KeyvOptions);
+    /**
+     * @param opts The options object is also passed through to the storage adapter. Check your storage adapter docs for any extra options.
+     */
     constructor(opts?: KeyvOptions);
+    /** Returns the value. */
     get(key: string): Promise<any>;
+    /**
+     * Set a value.
+     *
+     * By default keys are persistent. You can set an expiry TTL in milliseconds.
+     */
     set(key: string, value: any, ttl?: number): Promise<boolean>;
+    /**
+     * Deletes an entry.
+     *
+     * Returns `true` if the key existed, `false` if not.
+     */
     delete(key: string): Promise<boolean>;
+    /** Delete all entries in the current namespace. */
     clear(): Promise<void>;
 }
 
