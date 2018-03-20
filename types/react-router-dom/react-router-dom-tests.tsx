34622e91c3b2de793c5fd1aba0fb334412966816
vilicvane <i@vilic.info>
2018-03-20 22:14:19 +0800

    Add test for history prop

--- types/react-router-dom/react-router-dom-tests.tsx
+++ types/react-router-dom/react-router-dom-tests.tsx
@@ -6,11 +6,15 @@ import {
 } from 'react-router-dom';
 import * as H from 'history';
 
+const history = H.createBrowserHistory();
+
 const getIsActive = (extraProp: string) => (match: match<any>, location: H.Location) => !!extraProp;
 
 interface Props extends NavLinkProps {
   extraProp: string;
 }
+
+export const router = <Router history={history}></Router>;
 
 export default function(props: Props) {
   const {extraProp, ...rest} = props;
