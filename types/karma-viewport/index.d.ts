b745db2194e27703c8dde8b7af4e2fd20ac5da8e
karak <karak97@gmail.com>
2018-03-21 08:41:20 +0900

    Fix author.

--- types/karma-viewport/index.d.ts
+++ types/karma-viewport/index.d.ts
@@ -1,6 +1,6 @@
 // Type definitions for karma-viewport 0.4
 // Project: https://github.com/squidfunk/karma-viewport
-// Definitions by: My Self <https://github.com/me>
+// Definitions by: Karak <https://github.com/karak>
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
 // Type definition file for 'karma-viewport'
 // https://www.npmjs.com/package/karma-viewport
