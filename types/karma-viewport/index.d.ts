387b17a0846d1d7e902af138870bed2505450f2c
karak <karak97@gmail.com>
2018-03-20 13:36:12 +0900

    [karma-viewport] Added boilerplate.

---
+++ types/karma-viewport/index.d.ts
@@ -0,0 +1,13 @@
+// Type definitions for karma-viewport 0.4
+// Project: https://github.com/squidfunk/karma-viewport
+// Definitions by: My Self <https://github.com/me>
+// Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
+
+export = karma_viewport;
+
+declare const karma_viewport: {
+    "framework:viewport": string[];
+    "middleware:viewport": string[];
+    "preprocessor:viewport": string[];
+};
+
