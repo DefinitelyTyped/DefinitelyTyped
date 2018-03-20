3467b281b3f7032ac596f464bfb69df95a000bd1
Ajay Shrestha <ajay.gopal.shrestha@hotmail.com>
2018-03-20 21:07:45 +0545

    Travis issue fixed

--- types/tlds/index.d.ts
+++ types/tlds/index.d.ts
@@ -1,9 +1,9 @@
-// Type definitions for tlds 1.199.0
+// Type definitions for tlds 1.199
 // Project: https://github.com/stephenmathieson/node-tlds
 // Definitions by: Ajay Gopal Shrestha <https://github.com/ajshres>
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
 // TypeScript Version: 2.6
-/* 
+/*
 Declaring const tlds and exporting it
 */
-export declare const tlds: string[];
+export const tlds: string[];
