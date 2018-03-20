e6264ccd657296dc6578c9300ed3454fabacf2c2
Gareth Parker <gareth.parker@century.tech>
2018-03-20 16:56:31 +0000

    Created Glue project

---
+++ glue/hapi-glue-test.ts
@@ -0,0 +1,20 @@
+import * as Glue from "glue";
+import * as Hapi from "hapi";
+
+const manifest: Glue.Manifest = {
+  server: {
+    port: 3000
+  },
+  register: {
+    plugins: [
+      {
+        plugin: "./test",
+        routes: {
+          prefix: "test"
+        }
+      }
+    ]
+  }
+}
+
+Glue.compose(manifest);
