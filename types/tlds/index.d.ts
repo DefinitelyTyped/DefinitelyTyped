a1dd23a169bd7a6ff23b2bce356b944c8e9bb7b1
Ajay Shrestha <ajay.gopal.shrestha@hotmail.com>
2018-03-20 20:47:08 +0545

    Adding comments nad typescript version for passing Travis CI

--- types/tlds/index.d.ts
+++ types/tlds/index.d.ts
@@ -1,1 +1,9 @@
+// Type definitions for tlds
+// Project: https://github.com/stephenmathieson/node-tlds
+// Definitions by: Ajay Gopal Shrestha <https://github.com/ajshres>
+// Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
+// TypeScript Version: 2.6
+/* 
+Declaring const tlds and exporting it
+*/
 export declare const tlds: string[];
