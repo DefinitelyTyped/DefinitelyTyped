232dec4fc19796fe3acdce233daaf1d2a2a3ce3f
Margus Lamp <margus.lamp@heathmont.net>
2018-03-21 11:59:46 +0200

    Changed types reflecting flow types, added validationRules

--- types/express-graphql/express-graphql-tests.ts
+++ types/express-graphql/express-graphql-tests.ts
@@ -1,31 +1,35 @@
 import express = require('express');
 import 'express-session';
 import graphqlHTTP = require('express-graphql');
+import { GraphQLSchema } from 'graphql/type/schema';
 
 const app = express();
-const schema = {};
+const schema = {} as GraphQLSchema;
 
-const graphqlOption: graphqlHTTP.OptionsObj = {
+const graphqlOption: graphqlHTTP.OptionsData = {
     graphiql: true,
     schema: schema,
     formatError: (error: Error) => ({
         message: error.message
     }),
-    extensions: (args) => { }
+    validationRules: [() => false, () => true],
+    extensions: ({ document, variables, operationName, result }) => ({ key: "value", key2: "value"}),
 };
 
-const graphqlOptionRequest = (request: express.Request, response: express.Response): graphqlHTTP.OptionsObj => ({
+const graphqlOptionRequest = (request: express.Request): graphqlHTTP.OptionsData => ({
     graphiql: true,
     schema: schema,
-    context: request.session
+    context: request.session,
+    validationRules: [() => false, () => true],
 });
 
-const graphqlOptionRequestAsync = async (request: express.Request, response: express.Response): Promise<graphqlHTTP.OptionsObj> => {
+const graphqlOptionRequestAsync = async (request: express.Request): Promise<graphqlHTTP.OptionsData> => {
     return {
         graphiql: true,
         schema: await Promise.resolve(schema),
         context: request.session,
-        extensions: async (args) => { }
+        extensions: async (args) => { },
+        validationRules: [() => false, () => true],
     };
 };
 
--- types/express-graphql/index.d.ts
+++ types/express-graphql/index.d.ts
@@ -1,14 +1,15 @@
-// Type definitions for express-graphql
-// Project: https://www.npmjs.org/package/express-graphql
+// Type definitions for express-graphql 0.6.12
+// Project: https://github.com/graphql/express-graphql
 // Definitions by: Isman Usoh <https://github.com/isman-usoh>
 //                 Nitin Tutlani <https://github.com/nitintutlani>
 //                 Daniel Fader <https://github.com/hubel>
 //                 Ehsan Ziya <https://github.com/zya>
+//                 Margus Lamp <https://github.com/mlamp>
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
 // TypeScript Version: 2.3
 
 import { Request, Response } from "express";
-
+import { DocumentNode, GraphQLSchema, GraphQLError } from 'graphql';
 export = graphqlHTTP;
 
 declare namespace graphqlHTTP {
@@ -16,12 +17,15 @@ declare namespace graphqlHTTP {
      * Used to configure the graphQLHTTP middleware by providing a schema
      * and other configuration options.
      */
-    export type Options = ((req: Request, res: Response) => OptionsObj) | ((req: Request, res: Response) => Promise<OptionsObj>) | OptionsObj
-    export type OptionsObj = {
+    export type Options = ((request: Request,
+        response: Response,
+        params?: GraphQLParams) => OptionsResult) | OptionsResult;
+    export type OptionsResult = OptionsData | Promise<OptionsData>;
+    export type OptionsData = {
         /**
          * A GraphQL schema from graphql-js.
          */
-        schema: Object,
+        schema: GraphQLSchema,
 
         /**
          * A value to pass as the context to the graphql() function.
@@ -43,27 +47,63 @@ declare namespace graphqlHTTP {
          * fulfilling a GraphQL operation. If no function is provided, GraphQL's
          * default spec-compliant `formatError` function will be used.
          */
-        formatError?: Function,
+        formatError?: (error: GraphQLError) => any,
+
+        /**
+         * An optional array of validation rules that will be applied on the document
+         * in additional to those defined by the GraphQL spec.
+         */
+        validationRules?: any[],
+
+        /**
+         * An optional function for adding additional metadata to the GraphQL response
+         * as a key-value object. The result will be added to "extensions" field in
+         * the resulting JSON. This is often a useful place to add development time
+         * info such as the runtime of a query or the amount of resources consumed.
+         *
+         * Information about the request is provided to be used.
+         *
+         * This function may be async.
+         */
+        extensions?: (info: RequestInfo) => { [key: string]: any },
 
         /**
          * A boolean to optionally enable GraphiQL mode.
          */
         graphiql?: boolean,
-
-        /**
-         * An optional function for adding additional metadata to the GraphQL response as a key-value object.
-         * The result will be added to "extensions" field in the resulting JSON.
-         */
-        extensions?: ((args: ExtenstionsArgs) => any) | ((args: ExtenstionsArgs) => Promise<any>);
-
     };
 
-    interface ExtenstionsArgs {
-        document: object,
-        variables: object,
-        operationName: any,
-        result: object
-    }
+    /**
+     * All information about a GraphQL request.
+     */
+    export type RequestInfo = {
+        /**
+         * The parsed GraphQL document.
+         */
+        document?: DocumentNode,
+    
+        /**
+         * The variable values used at runtime.
+         */
+        variables?: { [name: string]: any },
+    
+        /**
+         * The (optional) operation name requested.
+         */
+        operationName?: string,
+    
+        /**
+         * The result of executing the operation.
+         */
+        result?: any,
+    };
+  
+    export type GraphQLParams = {
+        query?: string,
+        variables?: { [name: string]: any },
+        operationName?: string,
+        raw?: boolean,
+    };
 
     type Middleware = (request: Request, response: Response) => void;
 }
