8a2283e0e6f36f88c708407511e5f5093bad634b
Hugues Stefanski <hugues.stefanski@gmail.com>
2018-03-22 21:23:19 +0100

    d3-geo version increase

--- types/d3-geo/index.d.ts
+++ types/d3-geo/index.d.ts
@@ -1,10 +1,10 @@
-// Type definitions for D3JS d3-geo module 1.9
+// Type definitions for D3JS d3-geo module 1.10
 // Project: https://github.com/d3/d3-geo/
 // Definitions by: Hugues Stefanski <https://github.com/Ledragon>, Tom Wanzek <https://github.com/tomwanzek>, Alex Ford <https://github.com/gustavderdrache>, Boris Yankov <https://github.com/borisyankov>
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
 // TypeScript Version: 2.3
 
-// Last module patch version validated against: 1.9.0
+// Last module patch version validated against: 1.10.0
 
 import * as GeoJSON from 'geojson';
 
