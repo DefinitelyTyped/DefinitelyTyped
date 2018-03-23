7c20629f4f629b7a1e67b916d0324f567b10bf8c
ozum <ozum@ozum.net>
2018-03-23 18:52:48 +0300

    [cosmiconfig] Fixed lint errors

--- types/cosmiconfig/index.d.ts
+++ types/cosmiconfig/index.d.ts
@@ -1,9 +1,10 @@
-// Type definitions for cosmiconfig 4.0.0
+// Type definitions for cosmiconfig 4.0
 // Project: https://github.com/davidtheclark/cosmiconfig
 // Definitions by: ozum <https://github.com/ozum>
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
+// TypeScript Version: 2.2
 
-type Options = {
+interface Options {
     packageProp?: string | false;
     rc?: string | false;
     js?: string | false;
@@ -15,16 +16,17 @@ type Options = {
     transform?: (result: Result) => Promise<Result> | Result;
     configPath?: string;
     format?: "json" | "yaml" | "js";
-};
+}
 
-type Result = {
-    config: Object;
+interface Result {
+    config: object;
     filePath: string;
-};
+}
 
-declare interface Explorer {
+interface Explorer {
+    // You should provide either searchPath or configPath for load method. To disallow both, overloaded definitions added.
     load(searchPath?: string): Promise<Result>;
-    load(searchPath?: null | undefined, configPath?: string): Promise<Result>;
+    load(searchPath: null | undefined, configPath?: string): Promise<Result>;
 
     clearFileCache(): void;
     clearDirectoryCache(): void;
