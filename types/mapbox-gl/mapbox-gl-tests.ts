371c3bad16e85ae06a2b8bd5b0ec240d02f1119f
Lenny <lenny.cunningham@gmail.com>
2018-03-23 08:39:11 -0500

    @types\mapbox-gl Change RasterSource URL to optional.

--- types/mapbox-gl/mapbox-gl-tests.ts
+++ types/mapbox-gl/mapbox-gl-tests.ts
@@ -206,6 +206,22 @@ map.addSource('some id', videoSourceObj); // add
 map.removeSource('some id');  // remove
 
 /**
+ * Add Raster Source /// made URL optional to allow only tiles.
+ */
+map.addSource('radar', {
+	type: 'raster',
+	tiles: ['https://nowcoast.noaa.gov/arcgis/services/nowcoast/radar_meteo_imagery_nexrad_time/MapServer/WmsServer?bbox={bbox-epsg-3857}&service=WMS&request=GetMap&version=1.3.0&layers=1&styles=&format=image/png&transparent=true&height=256&width=256&crs=EPSG:3857'],
+	tileSize: 256
+})
+
+map.addLayer({
+	id: 'radar',
+	type: 'raster',
+	source: 'radar',
+	paint: {}
+})
+
+/**
  * Popup
  */
 var popup = new mapboxgl.Popup({closeOnClick: false, closeButton: true, anchor: 'top-right', offset: {'top': [0,0], 'bottom': [25,-50]} })
