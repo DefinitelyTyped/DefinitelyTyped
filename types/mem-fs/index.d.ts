a9dc8167a09f03aa3c2247ece908cb4abd6c185f
Walter Rumsby <walter.rumsby@myfoodbag.co.nz>
2018-03-24 07:27:28 +1300

    Remove readme from project link

--- types/mem-fs/index.d.ts
+++ types/mem-fs/index.d.ts
@@ -1,5 +1,5 @@
 // Type definitions for mem-fs 1.1
-// Project: https://github.com/sboudrias/mem-fs#readme
+// Project: https://github.com/sboudrias/mem-fs
 // Definitions by: My Food Bag <https://github.com/MyFoodBag>
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
 
