feaaa26a75cb0852581454e06a72014c1a5fc5f8
Anton Myzenkov <amyzenkov@gmail.com>
2018-03-23 20:52:48 +0300

    Added "TypeScript Version: 2.3" for Travis

--- types/ws/index.d.ts
+++ types/ws/index.d.ts
@@ -5,6 +5,7 @@
 //                 Margus Lamp <https://github.com/mlamp>
 //                 Philippe D'Alva <https://github.com/TitaneBoy>
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
+// TypeScript Version: 2.3
 
 /// <reference types="node" />
 
