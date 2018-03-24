23083887e514e10c6be89a42ec8cb27ad7a84d0d
Gareth Parker <gareth.parker@century.tech>
2018-03-24 10:01:39 +0000

    Added typescript version

--- types/glue/index.d.ts
+++ types/glue/index.d.ts
@@ -2,6 +2,7 @@
 // Project: https://github.com/hapijs/glue
 // Definitions by: Gareth Parker <https://github.com/garfty>
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
+// TypeScript Version: 2.4
 
 import { Server, ServerOptions } from "hapi";
 
