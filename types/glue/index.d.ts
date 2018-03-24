00f880e1fc44db89dc7c7b8b3d6259894f1f382d
Gareth Parker <gareth.parker@century.tech>
2018-03-24 09:57:13 +0000

    Fixing build

--- types/glue/index.d.ts
+++ types/glue/index.d.ts
@@ -1,35 +1,31 @@
 // Type definitions for glue
 // Project: https://github.com/hapijs/glue
-// Definitions by: Gareth Parker
+// Definitions by: Gareth Parker <https://github.com/garfty>
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
-// TypeScript Version: 2.7
 
-declare module "glue" {
-  
-  import { Server, ServerOptions } from "hapi";
+import { Server, ServerOptions } from "hapi";
 
-  export interface Options {
-    relativeTo: string;
-    preConnections?: (Server:Server, next:(err:any)=>void ) => void;
-    preRegister?: (Server:Server, next:(err:any)=>void ) => void;
-  }
-  
-  interface Plugin {
-    plugin: string | {
-        register:string;
-        options?:any;
-    };
-    options?: any;
-    routes?: any
-  }
-  
-  interface Manifest {
-    server: ServerOptions;
-    register?: {
-      plugins: Array<Plugin>
-    }
-  }
-  
-  export function compose(manifest: Manifest, options?: Options): Server;
-
+export interface Options {
+  relativeTo: string;
+  preConnections?: (Server:Server, next:(err:any)=>void ) => void;
+  preRegister?: (Server:Server, next:(err:any)=>void ) => void;
 }
+
+interface Plugin {
+  plugin: string | {
+      register:string;
+      options?:any;
+  };
+  options?: any;
+  routes?: any
+}
+
+interface Manifest {
+  server: ServerOptions;
+  register?: {
+    plugins: Array<Plugin>
+  }
+}
+
+export function compose(manifest: Manifest, options?: Options): Server;
+
