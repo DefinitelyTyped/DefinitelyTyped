f859332b8c7608cc836cc544a1f025173e58fe2b
Firede <firede@firede.us>
2018-03-25 13:17:31 +0800

    update `type/directives` -> `v0.13.2`.

--- types/graphql/type/directives.d.ts
+++ types/graphql/type/directives.d.ts
@@ -3,25 +3,30 @@ import { DirectiveDefinitionNode } from "../language/ast";
 import { DirectiveLocationEnum } from "../language/directiveLocation";
 
 /**
+ * Test if the given value is a GraphQL directive.
+ */
+export function isDirective(directive: any): directive is GraphQLDirective;
+
+/**
  * Directives are used by the GraphQL runtime as a way of modifying execution
  * behavior. Type system creators will usually not create these directly.
  */
 export class GraphQLDirective {
     name: string;
-    description?: string;
+    description: string | void;
     locations: DirectiveLocationEnum[];
     args: GraphQLArgument[];
-    astNode?: DirectiveDefinitionNode;
+    astNode: DirectiveDefinitionNode | void;
 
     constructor(config: GraphQLDirectiveConfig);
 }
 
 export interface GraphQLDirectiveConfig {
     name: string;
-    description?: string;
+    description?: string | void;
     locations: DirectiveLocationEnum[];
-    args?: GraphQLFieldConfigArgumentMap;
-    astNode?: DirectiveDefinitionNode;
+    args?: GraphQLFieldConfigArgumentMap | void;
+    astNode?: DirectiveDefinitionNode | void;
 }
 
 /**
@@ -47,4 +52,6 @@ export const GraphQLDeprecatedDirective: GraphQLDirective;
 /**
  * The full list of specified directives.
  */
-export const specifiedDirectives: GraphQLDirective[];
+export const specifiedDirectives: ReadonlyArray<GraphQLDirective>;
+
+export function isSpecifiedDirective(directive: GraphQLDirective): boolean;
