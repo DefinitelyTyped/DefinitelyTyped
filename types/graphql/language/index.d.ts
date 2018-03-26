2ffd5370a56156c5561aca306f502960a19ef5b8
Firede <firede@firede.us>
2018-03-26 16:19:34 +0800

    update `language/visitor` & add tests.

--- types/graphql/language/index.d.ts
+++ types/graphql/language/index.d.ts
@@ -5,5 +5,15 @@ export { createLexer, TokenKind, Lexer } from "./lexer";
 export { parse, parseValue, parseType, ParseOptions } from "./parser";
 export { print } from "./printer";
 export { Source } from "./source";
-export { visit, visitInParallel, visitWithTypeInfo, getVisitFn, BREAK } from "./visitor";
+export {
+    visit,
+    visitInParallel,
+    visitWithTypeInfo,
+    getVisitFn,
+    BREAK,
+    ASTVisitor,
+    Visitor,
+    VisitFn,
+    VisitorKeyMap,
+} from "./visitor";
 export { DirectiveLocation, DirectiveLocationEnum } from "./directiveLocation";
