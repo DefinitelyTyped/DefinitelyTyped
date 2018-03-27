a3021f80409f7f797bb32fa6f613e9da81dfd9f5
Firede <firede@firede.us>
2018-03-28 02:01:32 +0800

    update `graphql/validation/*` -> `v0.13.2`.
    https://github.com/graphql/graphql-js/tree/v0.13.2/src/validation

--- types/graphql/validation/specifiedRules.d.ts
+++ types/graphql/validation/specifiedRules.d.ts
@@ -1,6 +1,9 @@
-import { ValidationContext } from "./validate"; // It needs to check.
+import ValidationContext from "./ValidationContext";
 
 /**
  * This set includes all validation rules defined by the GraphQL spec.
+ *
+ * The order of the rules in this list has been adjusted to lead to the
+ * most clear output when encountering multiple validation errors.
  */
 export const specifiedRules: Array<(context: ValidationContext) => any>;
