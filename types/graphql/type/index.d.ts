b1a1e3f9f199586a4a2de7640d962ce7f446c87c
Firede <firede@firede.us>
2018-03-28 00:39:46 +0800

    update `graphql/type/*` -> `v0.13.2`.
    https://github.com/graphql/graphql-js/tree/v0.13.2/src/type

--- types/graphql/type/index.d.ts
+++ types/graphql/type/index.d.ts
@@ -1,29 +1,46 @@
-// GraphQL Schema definition
-export { GraphQLSchema } from "./schema";
+export {
+    // Predicate
+    isSchema,
+    // GraphQL Schema definition
+    GraphQLSchema,
+    GraphQLSchemaConfig,
+} from "./schema";
 
 export * from "./definition";
 
 export {
-    // "Enum" of Directive Locations
-    DirectiveLocation,
+    // Predicate
+    isDirective,
     // Directives Definition
     GraphQLDirective,
     // Built-in Directives defined by the Spec
+    isSpecifiedDirective,
     specifiedDirectives,
     GraphQLIncludeDirective,
     GraphQLSkipDirective,
     GraphQLDeprecatedDirective,
     // Constant Deprecation Reason
     DEFAULT_DEPRECATION_REASON,
+    GraphQLDirectiveConfig,
 } from "./directives";
 
 // Common built-in scalar instances.
-export { GraphQLInt, GraphQLFloat, GraphQLString, GraphQLBoolean, GraphQLID } from "./scalars";
+export {
+    isSpecifiedScalarType,
+    specifiedScalarTypes,
+    GraphQLInt,
+    GraphQLFloat,
+    GraphQLString,
+    GraphQLBoolean,
+    GraphQLID,
+} from "./scalars";
 
 export {
     // "Enum" of Type Kinds
     TypeKind,
     // GraphQL Types for introspection.
+    isIntrospectionType,
+    introspectionTypes,
     __Schema,
     __Directive,
     __DirectiveLocation,
@@ -38,4 +55,4 @@ export {
     TypeNameMetaFieldDef,
 } from "./introspection";
 
-export { DirectiveLocationEnum } from "./directives";
+export { validateSchema, assertValidSchema } from "./validate";
