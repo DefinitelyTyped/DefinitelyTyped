937c042d7d77e2c5a4b3a59012c6a1ca7fa4c2cf
Diogo Franco (Kovensky) <diogomfranco@gmail.com>
2018-03-28 12:00:48 +0900

    Bump version

--- types/react/index.d.ts
+++ types/react/index.d.ts
@@ -1,4 +1,4 @@
-// Type definitions for React 16.1
+// Type definitions for React 16.3
 // Project: http://facebook.github.io/react/
 // Definitions by: Asana <https://asana.com>
 //                 AssureSign <http://www.assuresign.com>
