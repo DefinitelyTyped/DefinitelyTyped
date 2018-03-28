b091e75be0d0fe9fbe04f9dca2e81ef9e939c3b9
Christian Rackerseder <git@echooff.de>
2018-03-28 15:37:13 +0200

    Added types for enquire.js

---
+++ types/enquire.js/enquire.js-tests.ts
@@ -0,0 +1,17 @@
+import * as enquire from 'enquire.js';
+
+enquire
+    .register('(min-width: 300px) and (max-width: 600px)', {
+      deferSetup: true,
+      match: () => {},
+      unmatch: () => {},
+      setup: () => {},
+      destroy: () => {}
+    })
+    .register('(min-width: 300px) and (max-width: 600px)', {});
+
+enquire.register('(min-width: 300px) and (max-width: 600px)', [{}, {}], true);
+
+enquire.register('(min-width: 300px) and (max-width: 600px)', () => {}, true);
+
+enquire.unregister('(min-width: 300px) and (max-width: 600px)');
