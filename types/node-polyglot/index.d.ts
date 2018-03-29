ba30b27a773c7217e6e68f9168fb28461f336cec
daniel <daniel.bisch@gmx.de>
2018-03-29 22:02:39 +0200

    Bumped version

--- types/node-polyglot/index.d.ts
+++ types/node-polyglot/index.d.ts
@@ -1,4 +1,4 @@
-// Type definitions for node-polyglot v0.4.1
+// Type definitions for node-polyglot v0.4.2
 // Project: https://github.com/airbnb/polyglot.js
 // Definitions by: Tim Jackson-Kiely <https://github.com/timjk>
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
