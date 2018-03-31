cf13044f207dd85bead06d25a273defbedd8e2c1
jewbre <jewbre18@gmail.com>
2018-03-31 14:56:58 +0200

    types for escape-regexp 0.0

---
+++ types/escape-regexp/index.d.ts
@@ -0,0 +1,9 @@
+// Type definitions for escape-regexp 0.0
+// Project: https://github.com/baz/foo (Does not have to be to GitHub, but prefer linking to a source code repository rather than to a project website.)
+// Definitions by: Vilim Stubičan <https://github.com/jewbre>
+// Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
+// TypeScript Version: 2.4
+
+declare function escapeRegExp(str: string): string;
+
+export = escapeRegExp;
