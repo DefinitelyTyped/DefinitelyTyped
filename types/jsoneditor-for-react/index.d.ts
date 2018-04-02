9427fe3dc5160e939807ae2465c64fa227aac335
Josh Goldberg <josh@fullscreenmario.com>
2018-04-02 00:08:52 -0700

    Lint fixups

--- types/jsoneditor-for-react/index.d.ts
+++ types/jsoneditor-for-react/index.d.ts
@@ -1,17 +1,17 @@
-// Type definitions for jsoneditor-for-react 0.0.1
+// Type definitions for jsoneditor-for-react 0.0
 // Project: https://github.com/mixj93/jsoneditor-for-react#readme
 // Definitions by: JoshGoldberg <https://github.com/joshuakgoldberg>
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
 // TypeScript Version: 2.6
 
-import * as React from "react"
-import JSONEditor, { JSONEditorOptions } from "jsoneditor"
+import * as React from "react";
+import JSONEditor, { JSONEditorOptions } from "jsoneditor";
 
-export interface IReactJsoneditorProps {
-    values: Object
+export interface ReactJsonEditorProps {
+    values: {};
 }
 
-export default class ReactJsoneditor extends React.Component<IReactJsoneditorProps> {
-    private editor?: JSONEditor
-    private options?: JSONEditorOptions
+export default class ReactJsoneditor extends React.Component<ReactJsonEditorProps> {
+    private editor?: JSONEditor;
+    private options?: JSONEditorOptions;
 }
