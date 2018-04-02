9a50625d28bc22297c4c210bd8a3dce3cbd73511
Diogo Franco <diogomfranco@gmail.com>
2018-04-02 22:59:49 +0900

    Add new React 16.3 lifecycle events (#24577)
    * Add new lifecycle events to React (16.3)
    
    * Add tests, fix lint
    
    * Bump version
    
    * Improve inference of snapshot
    
    * Improve tests
    
    * Correctly support returning partial state from getDerivedStateFromProps
    
    * Remove redundant type
    
    * Fix lint error
    
    * Write the incorrect subclassing test in a more verbose way

--- types/react/test/tsx.tsx
+++ types/react/test/tsx.tsx
@@ -129,3 +129,38 @@ export abstract class SetStateTestForAndedState<P, S> extends React.Component<P,
 		this.setState({ baseProp: 'foobar' });
 	}
 }
+
+interface NewProps { foo: string; }
+interface NewState { bar: string; }
+
+class ComponentWithNewLifecycles extends React.Component<NewProps, NewState, { baz: string }> {
+    static getDerivedStateFromProps: React.GetDerivedStateFromProps<NewProps, NewState> = (nextProps) => {
+        return { bar: `${nextProps.foo}bar` };
+    }
+
+    getSnapshotBeforeUpdate(prevProps: Readonly<NewProps>) {
+        return { baz: `${prevProps.foo}baz` };
+    }
+
+    componentDidUpdate(prevProps: Readonly<NewProps>, prevState: Readonly<NewState>, snapshot: { baz: string }) {
+        return;
+    }
+
+    render() {
+        return this.state.bar;
+    }
+}
+
+class ComponentWithLargeState extends React.Component<{}, Record<'a'|'b'|'c', string>> {
+    static getDerivedStateFromProps: React.GetDerivedStateFromProps<{}, Record<'a'|'b'|'c', string>> = () => {
+        return { a: 'a' };
+    }
+}
+
+const componentWithBadLifecycle = new (class extends React.Component<{}, {}, number> {})({});
+componentWithBadLifecycle.getSnapshotBeforeUpdate = () => { // $ExpectError
+    return 'number';
+};
+componentWithBadLifecycle.componentDidUpdate = (prevProps: {}, prevState: {}, snapshot?: string) => { // $ExpectError
+    return;
+};
