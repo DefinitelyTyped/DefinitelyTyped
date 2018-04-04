f8b253436b897755e887fe00f631ed0be8852874
efokschaner <eddyaod@gmail.com>
2018-04-03 18:03:28 -0700

    Change maintainer of webvr-api

--- types/webvr-api/index.d.ts
+++ types/webvr-api/index.d.ts
@@ -1,6 +1,6 @@
 // Type definitions for WebVR API
 // Project: https://w3c.github.io/webvr/
-// Definitions by: six a <https://github.com/lostfictions>
+// Definitions by: efokschaner <https://github.com/efokschaner>
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
 
 // Typescript doesn't allow redefinition of type aliases even if they match,
