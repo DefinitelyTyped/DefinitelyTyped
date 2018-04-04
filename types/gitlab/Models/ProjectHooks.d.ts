c4e09994449f8a6e42aabc50a32ea2adda84dedc
Arylo <arylo.open@gmail.com>
2018-04-05 00:22:02 +0800

    Update type definition files

--- types/gitlab/Models/ProjectHooks.d.ts
+++ types/gitlab/Models/ProjectHooks.d.ts
@@ -5,10 +5,12 @@ interface IAddParam {
     [key: string]: any;
 }
 
+type HooksCb = (hooks: any[]) => any;
+
 export class ProjectHooks extends BaseModel {
-    public list(projectId: TId, fn?: Function): any;
+    public list(projectId: TId, fn?: HooksCb): any;
     public show(projectId: TId, hookId: number, fn?: Function): any;
     public add(projectId: TId, params: IAddParam | string, fn?: Function): any;
-    public update(projectId: TId, hookId: number, url: any, fn?: Function): any;
+    public update(projectId: TId, hookId: number, url: string, fn?: Function): any;
     public remove(projectId: TId, hookId: number, fn?: Function): any;
 }
--- types/gitlab/Models/ProjectRepository.d.ts
+++ types/gitlab/Models/ProjectRepository.d.ts
@@ -1,10 +1,18 @@
 import { BaseModel, TId } from '../BaseModel.d';
 
-interface IShowFileParam {
+interface IShowFileParams {
     file_path: string;
     ref?: any;
     file_id?: any;
     [key: string]: any;
+}
+
+interface IAddTagParams {
+    id: TId;
+    tag_name: string;
+    ref: string;
+    message?: string;
+    release_description?: string;
 }
 
 export class ProjectRepository extends BaseModel {
@@ -14,7 +22,7 @@ export class ProjectRepository extends BaseModel {
     public unprotectBranch(projectId: TId, branchId: string, fn?: Function): any;
     public createBranch(params: object, fn?: Function): any;
     public deleteBranch(projectId: TId, branchId: string, fn?: Function): any;
-    public addTag(params: object, fn?: Function): any;
+    public addTag(params: IAddTagParams, fn?: Function): any;
     public deleteTag(projectId: TId, tagName: string, fn?: Function): any;
     public showTag(projectId: TId, tagName: string, fn?: Function): any;
     public listTags(projectId: TId, fn?: Function): any;
@@ -24,7 +32,7 @@ export class ProjectRepository extends BaseModel {
     public listTree(projectId: TId, fn?: Function): any;
     public listTree(projectId: TId, params?: object, fn?: Function): any;
     public showFile(projectId: TId, fn?: Function): any;
-    public showFile(projectId: TId, params?: IShowFileParam, fn?: Function): any;
+    public showFile(projectId: TId, params?: IShowFileParams, fn?: Function): any;
     public createFile(params?: object, fn?: Function): any;
     public updateFile(params?: object, fn?: Function): any;
     public compare(params?: object, fn?: Function): any;
