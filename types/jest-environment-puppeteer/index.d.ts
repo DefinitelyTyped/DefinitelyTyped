aa17c34403a635bf3b88c6c736583457418741e5
Joshua Goldberg <josh@fullscreenmario.com>
2018-04-05 12:41:29 -0700

    Used sub-monorepo link

--- types/jest-environment-puppeteer/index.d.ts
+++ types/jest-environment-puppeteer/index.d.ts
@@ -1,5 +1,5 @@
 // Type definitions for jest-environment-puppeteer 2.2
-// Project: https://github.com/smooth-code/jest-puppeteer
+// Project: https://github.com/smooth-code/jest-puppeteer/tree/master/packages/jest-environment-puppeteer
 // Definitions by: Josh Goldberg <https://github.com/joshuakgoldberg>
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
 // TypeScript Version: 2.3
