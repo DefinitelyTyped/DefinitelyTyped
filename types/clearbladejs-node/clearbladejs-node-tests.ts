b7d8ef70b20fc2fc9f0c6f132f18cd3bc775d8ca
Jim Bouquet <jbouquet@clearblade.com>
2018-04-05 17:10:03 -0500

    Added type definitions for clearbladejs-client, clearbladejs-server, and clearbladejs-node.

---
+++ types/clearbladejs-node/clearbladejs-node-tests.ts
@@ -0,0 +1,128 @@
+import { ClearBlade, Resp, QuerySortDirections, QueryConditions } from ".";
+
+// Sample code for clearbladejs Node SDK v1.0.0 used to test typescript definitions
+// Project: https://github.com/ClearBlade/Node-SDK
+// Definitions by: Jim Bouquet <https://github.com/ClearBlade/>
+// Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
+
+const constants = require("./constants.json");
+
+var genericCallback = function(error: boolean, response: Resp) {};
+
+///////////////////////////////////////
+//ClearBlade object API invocations
+///////////////////////////////////////
+ClearBlade.init({
+	email: "a@a.com",
+	password: "a",
+	systemKey: constants.systemKey,
+	systemSecret: constants.systemSecret,
+	URI: constants.URL,
+	messagingURI: constants.messageURL,
+	callback: genericCallback
+})
+
+ClearBlade.setUser("test@test.com", "password");
+ClearBlade.registerUser("test@test.com", "password", genericCallback);
+ClearBlade.isCurrentUserAuthenticated(genericCallback);
+if (!ClearBlade.isObjectEmpty({prop: "test"})) {
+	ClearBlade.logger("Object is not empty");
+}
+
+ClearBlade.validateEmailPassword("test@test.com", "password");
+
+ClearBlade.logoutUser(genericCallback);
+ClearBlade.loginAnon(genericCallback);
+ClearBlade.loginUser("test@test.com", "password", genericCallback);
+
+ClearBlade.sendPush([], {}, "appId: string", genericCallback);
+
+// execute(error: Object, response: Object, callback: ClearBladeCallback): void;
+// makeKVPair(key: string, value: string): KeyValuePair;
+// request(options: RequestOptions, callback: RequestCallback): void;
+
+var coll1 = ClearBlade.Collection("collectionID");
+var coll2 = ClearBlade.Collection({collectionName: "collectionName"});
+var coll3 = ClearBlade.Collection({collectionID: "collectionID"});
+
+var query1 = ClearBlade.Query("collectionID");
+var query2 = ClearBlade.Query({offset: 5, limit: 5, collectionID: "collectionID"});
+var query3 = ClearBlade.Query({collectionName: "collectionName"});
+var query4 = ClearBlade.Query({collection: "collectionID"});
+
+ClearBlade.addToQuery(query1, "key", "value");
+ClearBlade.addFilterToQuery(query1, QueryConditions.QUERY_GREATERTHAN, "key", "value");
+ClearBlade.addSortToQuery(query1, QuerySortDirections.QUERY_SORT_ASCENDING, "column1");
+
+var opQueryStr = ClearBlade.parseOperationQuery(query1.query);
+var parse1:string = ClearBlade.parseQuery(query1.query);
+var parse2:string = ClearBlade.parseQuery(query1);
+
+
+var item1 = ClearBlade.Item({}, "hello");
+var item2 = ClearBlade.Item({}, {collectionID: "hello"});
+
+var code = ClearBlade.Code();
+var user = ClearBlade.User();
+
+var messaging = ClearBlade.Messaging({}, genericCallback);
+
+///////////////////////////////////////
+//Collection API invocations
+///////////////////////////////////////
+coll1.fetch(query1, genericCallback);
+coll1.create(ClearBlade.Item({}, ""), genericCallback);
+coll1.update(query1.query, {}, genericCallback);
+coll1.remove(query1.query, genericCallback);
+
+///////////////////////////////////////
+//Query API invocations
+///////////////////////////////////////
+query1.ascending("string");
+query1.descending("string");
+query1.equalTo("string", "string");
+query1.greaterThan("string", 2);
+query1.greaterThanEqualTo("string", false);
+query1.lessThan("string", "string");
+query1.lessThanEqualTo("string", "string");
+query1.notEqualTo("string", "string");
+query1.or(query2);
+query1.setPage(1, 1);
+query1.fetch(genericCallback);
+query1.update({}, genericCallback);
+query1.remove(genericCallback);
+
+///////////////////////////////////////
+//Item API invocations
+///////////////////////////////////////
+item1.save();
+item1.refresh();
+item1.destroy();
+
+///////////////////////////////////////
+//Code API invocations
+///////////////////////////////////////
+code.execute("codeName", {}, genericCallback);
+
+///////////////////////////////////////
+//User API invocations
+///////////////////////////////////////
+user.getUser(genericCallback);
+user.setUser({}, genericCallback);
+user.allUsers(query1.query, genericCallback);
+
+///////////////////////////////////////
+//Messaging API invocations
+///////////////////////////////////////
+messaging.getMessageHistory("topic: string", 5, 15, genericCallback);
+
+messaging.publish("topic: string", {});
+
+messaging.subscribe("my/topic", {}, messageReceivedCb);
+
+function messageReceivedCb (message: string) {
+	messaging.unsubscribe("my/topic");
+}
+
+
+
---
+++ types/clearbladejs-node/index.d.ts
@@ -0,0 +1,256 @@
+import { Response, RequestCallback } from "request/index";
+import { MqttClient, PacketCallback } from "mqtt";
+
+// Type definitions for clearbladejs Node SDK v1.0.0
+// Project: https://github.com/ClearBlade/Node-SDK
+// Definitions by: Jim Bouquet <https://github.com/ClearBlade/>
+// Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
+//
+// TypeScript Version: 2.1
+
+/// <reference types="request" />
+/// <reference types="mqtt" />
+
+declare enum MessagingQOS {
+    MESSAGING_QOS_AT_MOST_ONCE = 0,
+    MESSAGING_QOS_AT_LEAST_ONCE = 1,
+    MESSAGING_QOS_EXACTLY_ONCE = 2
+}
+
+export interface Resp {
+    error(msg: any): never; // todo: figure out if we can have the compiler throw an error if someone adds code after this
+    success(msg: any): never;
+}
+
+export interface InitOptions { 
+    systemKey: string;
+    systemSecret: string;
+    logging?: boolean;
+    callback?: CbCallback;
+    email?: string;
+    password?: string;
+    registerUser?: boolean;
+    useUser?: APIUser;
+    URI?: string;
+    messagingURI?: string;
+    messagingPort?: number;
+    defaultQoS?: MessagingQOS;
+    callTimeout?: number;
+}
+
+export interface RequestOptions {
+    systemKey: string;
+    systemSecret: string;
+    method?: string;
+    endpoint?: string;
+    body?: string;
+    qs?: string;
+    URI?: string;
+    useUser?: boolean;
+    authToken?: string;
+    user?: APIUser;
+}
+
+export interface APIUser { 
+    email: string;
+    authToken: string;
+}
+
+export interface KeyValuePair {
+    [key: string]: any;
+}
+
+export interface CbCallback {
+    (error: boolean, response: Resp): void
+} 
+
+export default interface ClearBladeGlobal extends ClearBladeInt {
+    isCurrentUserAuthenticated(callback: CbCallback): void;
+}
+
+export interface ClearBladeInt {
+    addToQuery(queryObj: QueryObj, key: string, value: string): void;
+    addFilterToQuery(queryObj: QueryObj, condition: QueryConditions, key: string, value: QueryValue): void;
+    addSortToQuery(queryObj: QueryObj, direction: QuerySortDirections, column: string): void;
+    Code() :Code;
+    Collection(options: string | CollectionOptionsWithName | CollectionOptionsWithID) :Collection;
+    execute(error: Object, response: Object, callback: CbCallback): void;
+    init(options: InitOptions): void;
+    isObjectEmpty(obj: Object): boolean;
+    Item(data: Object, options: string | ItemOptions) :Item;
+    logger(message: string): void;
+    loginAnon(callback: CbCallback): void;
+    loginUser(email: string, password: string, callback: CbCallback): void;
+    logoutUser(callback: CbCallback): void;
+    makeKVPair(key: string, value: string): KeyValuePair;
+    parseOperationQuery(query: Query): string;
+    parseQuery(query: Query | QueryObj): string;
+    Query(options: string | QueryOptionsWithCollection | QueryOptionsWithName | QueryOptionsWithID) :QueryObj;
+    registerUser(email: string, password: string, callback: CbCallback): void;
+    request(options: RequestOptions, callback: RequestCallback): void;
+    setUser(email: string, password: string): void;
+    User() :AppUser;
+    Messaging(options: MessagingOptions, callback: CbCallback) :Messaging;
+    sendPush(users: string[], payload: Object, appId: string, callback: CbCallback): void;
+    validateEmailPassword(email: string, password:string): void;
+}
+
+export interface CollectionOptionsWithName { 
+    collectionName: string;
+}
+
+export interface CollectionOptionsWithID { 
+    collectionID: string;
+}
+
+export interface Collection {
+    endpoint: string;
+    user: APIUser;
+    URI: string;
+    systemKey: string;
+    systemSecret: string;
+
+    fetch(query: QueryObj, callback: CbCallback): void;
+    create(newItem: Item, callback: CbCallback): void;
+    update(query: Query, changes: Object, callback: CbCallback): void;
+    remove(query: Query, callback: CbCallback): void;
+}
+
+export declare const enum QuerySortDirections {
+    QUERY_SORT_ASCENDING = 'ASC',
+    QUERY_SORT_DESCENDING = 'DESC'
+}
+
+export declare const enum QueryConditions {
+    QUERY_EQUAL = 'EQ',
+    QUERY_NOTEQUAL = 'NEQ',
+    QUERY_GREATERTHAN = 'GT',
+    QUERY_GREATERTHAN_EQUAL = 'GTE',
+    QUERY_LESSTHAN = 'LT',
+    QUERY_LESSTHAN_EQUAL = 'LTE',
+    QUERY_MATCHES = 'RE'
+}
+
+export type QueryValue = string|number|boolean;
+
+export interface QueryOptions { 
+    offset?: number;
+    limit?: number;
+}
+
+export interface QueryOptionsWithCollection extends QueryOptions{
+    collection: string;
+}
+
+export interface QueryOptionsWithName extends CollectionOptionsWithName, QueryOptions{}
+export interface QueryOptionsWithID extends CollectionOptionsWithID, QueryOptions{}
+
+export interface Query {
+    SELECTCOLUMNS?: string[];
+    SORT?: QuerySortDirections;
+    FILTERS?: QueryFilter[];
+    PAGESIZE?: number;
+    PAGENUM?: number;
+}
+
+export interface QueryFilter {
+    [QueryConditions: string]: QueryFilterValue
+}
+
+export interface QueryFilterValue {
+    [name: string]: QueryValue
+}
+
+export interface QueryObj {
+    endpoint: string;
+    user: APIUser;
+    URI: string;
+    systemKey: string;
+    systemSecret: string;
+    query: Query;
+    OR: Query[];
+    offset: number;
+    limit: number;
+
+    ascending(field: string): Query;
+    descending(field: string): Query;
+    equalTo(field: string, value: QueryValue): Query;
+    greaterThan(field: string, value: QueryValue): Query;
+    greaterThanEqualTo(field: string, value: QueryValue): Query;
+    lessThan(field: string, value: QueryValue): Query;
+    lessThanEqualTo(field: string, value: QueryValue): Query;
+    notEqualTo(field: string, value: QueryValue): Query;
+    matches(field: string, pattern: string): Query;
+    or(query: QueryObj): Query;
+    setPage(pageSize: number, pageNum: number): Query;
+    fetch(callback: CbCallback): void;
+    update(changes: Object, callback: CbCallback): void;
+    remove(callback: CbCallback): void;
+}
+
+export interface ItemOptions extends CollectionOptionsWithID{}
+
+export interface Item {
+    data: Object;
+
+    save(): void;
+    refresh(): void;
+    destroy(): void;
+}
+
+export interface Code {
+    user: APIUser;
+    URI: string;
+    systemKey: string;
+    systemSecret: string;
+    callTimeout: number;
+    URIPrefix: string;
+
+    execute(name: string, params: Object, callback: CbCallback): void;
+}
+
+export interface AppUser {
+    user: APIUser;
+    URI: string;
+    systemKey: string;
+    systemSecret: string;
+
+    getUser(callback: CbCallback): void;
+    setUser(data: Object, callback: CbCallback): void;
+    allUsers(query: Query, callback: CbCallback): void;
+}
+
+export interface Messaging {
+    user: APIUser;
+    URI: string;
+    systemKey: string;
+    systemSecret: string;
+    client: MqttClient;
+
+    getMessageHistory(topic: string, startTime: number, count: number, callback: CbCallback): void;
+    publish(topic: string, payload: Object): void;
+    subscribe(topic: string, options: MessagingSubscribeOptions, messageCallback: MessageCallback): void;
+    unsubscribe(topic: string, callback?: PacketCallback): void;
+}
+
+export interface CommonMessagingProperties {
+    hosts?: string;
+    ports?: string;
+}
+
+export interface MessagingOptions extends CommonMessagingProperties {
+    qos?: MessagingQOS
+}
+
+export interface MessagingSubscribeOptions {
+    qos?: MessagingQOS;
+    timeout?: number;
+}
+
+export interface MessageCallback {
+    (message: string): void;
+}
+
+declare var ClearBlade: ClearBladeGlobal;
+
+export {ClearBlade};
