d7b5a4865e11f89b5119287119cf8f4e3f6a8f62
Joshua Goldberg <josh@fullscreenmario.com>
2018-04-04 17:51:36 -0700

    Added missing TS version

--- types/jest-environment-puppeteer/index.d.ts
+++ types/jest-environment-puppeteer/index.d.ts
@@ -2,6 +2,7 @@
 // Project: https://github.com/smooth-code/jest-puppeteer
 // Definitions by: Josh Goldberg <https://github.com/joshuakgoldberg>
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
+// TypeScript Version: 2.3
 
 import { Browser, Page } from "puppeteer";
 
