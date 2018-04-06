239fc6b40b0ad231d9668f4c9cd7c7ac01b75804
Vincent Langlet <vincentl@theodo.fr>
2018-04-06 19:11:54 +0200

    Use generic type for Stylesheet.flatten

--- types/react-native/index.d.ts
+++ types/react-native/index.d.ts
@@ -4804,10 +4804,7 @@ export namespace StyleSheet {
      * their respective objects, merged as one and then returned. This also explains
      * the alternative use.
      */
-    export function flatten<T>(style?: RegisteredStyle<T>): T;
-    export function flatten(style?: StyleProp<ViewStyle>): ViewStyle;
-    export function flatten(style?: StyleProp<TextStyle>): TextStyle;
-    export function flatten(style?: StyleProp<ImageStyle>): ImageStyle;
+    export function flatten<T>(style?: StyleProp<T>): T;
 
     /**
      * This is defined as the width of a thin line on the platform. It can be
