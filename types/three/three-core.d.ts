6c5c0a88983b7243ae13f08fd515cad2922bb2d2
Tiger Oakes <tigeroakes@gmail.com>
2018-04-09 13:26:18 -0700

    [three] CubeTextureLoader: Fixed crossOrigin typo and made types more precise (#24848)
    * Updated CubeTextureLoader types
    
    * Feedback: keep `urls` as plain array

--- types/three/three-core.d.ts
+++ types/three/three-core.d.ts
@@ -2303,12 +2303,12 @@ export class CubeTextureLoader {
     constructor(manager?: LoadingManager);
 
     manager: LoadingManager;
-    corssOrigin: string;
-    path: string;
+    crossOrigin: string;
+    path?: string;
 
     load(urls: Array<string>, onLoad?: (texture: CubeTexture) => void, onProgress?: (event: ProgressEvent) => void, onError?: (event: ErrorEvent) => void): CubeTexture;
-    setCrossOrigin(crossOrigin: string): CubeTextureLoader;
-    setPath(path: string): CubeTextureLoader;
+    setCrossOrigin(crossOrigin: string): this;
+    setPath(path: string): this;
 }
 
 export class DataTextureLoader {
