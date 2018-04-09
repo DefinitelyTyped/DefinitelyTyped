7a971dcb5eb291afa784af9f319718cd21d30603
Rob Moran <rob.moran@arm.com>
2018-04-09 11:44:47 +0100

    Only use MAJOR.MINOR version

--- types/node-hid/index.d.ts
+++ types/node-hid/index.d.ts
@@ -1,4 +1,4 @@
-// Type definitions for node-hid 0.7.2
+// Type definitions for node-hid 0.7
 // Project: https://github.com/node-hid/node-hid#readme
 // Definitions by: Mohamed Hegazy <https://github.com/mhegazy>
 //                 Robert Kiss <https://github.com/ert78gb>
