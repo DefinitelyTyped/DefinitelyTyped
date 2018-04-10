6e2efd24b76a73c666dc3caf3e06c5c45d9cca58
Rasmus Eneman <rasmus@eneman.eu>
2018-04-10 19:21:14 +0200

    [react-dom] fix signature of findDOMNode (#24211)
    fixes #24167

--- types/react/v15/test/index.ts
+++ types/react/v15/test/index.ts
@@ -255,8 +255,8 @@ const str: string = ReactDOMServer.renderToString(element);
 const markup: string = ReactDOMServer.renderToStaticMarkup(element);
 const notValid: boolean = React.isValidElement(props); // false
 const isValid = React.isValidElement(element); // true
-let domNode: Element = ReactDOM.findDOMNode(component);
-domNode = ReactDOM.findDOMNode(domNode);
+let domNode = ReactDOM.findDOMNode(component);
+domNode = ReactDOM.findDOMNode(domNode as Element);
 
 //
 // React Elements
