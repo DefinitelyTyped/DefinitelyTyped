9d8520c7d54a08c6530ff0c40810a6c02a3c42f6
Kerwyn Rojas <kerwyn.rg@gmail.com>
2018-04-11 13:14:22 -0600

    Removed rules from tslint.json

--- types/react-timeout/index.d.ts
+++ types/react-timeout/index.d.ts
@@ -1,6 +1,6 @@
-// Type definitions for react-timeout v1.1.1
+// Type definitions for react-timeout 1.1
 // Project: https://github.com/plougsgaard/react-timeout
-// Definitions by: Kerwyn Rojas <https://github.com/kerwynrg/>
+// Definitions by: Kerwyn Rojas <https://github.com/kerwynrg>
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
 // TypeScript Version: 2.6
 
