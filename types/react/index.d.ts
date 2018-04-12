6da421045e282dbd2086f2019ac30708246f308b
Fredrik Nicol <fredrik.nicol@gmail.com>
2018-04-12 09:31:10 +0200

    [react] Add comment about removed index signature

--- types/react/index.d.ts
+++ types/react/index.d.ts
@@ -897,7 +897,16 @@ declare namespace React {
         onTransitionEndCapture?: TransitionEventHandler<T>;
     }
 
-    export interface CSSProperties extends CSS.Properties<string | number> {}
+    export interface CSSProperties extends CSS.Properties<string | number> {
+        /**
+         * The index signature was removed to enable closed typing for style
+         * using CSSType. You're able to use type assertion or module augmentation
+         * to add properties or an index signature of your own.
+         *
+         * For examples and more information, visit:
+         * https://github.com/frenic/csstype#what-should-i-do-when-i-get-type-errors
+         */
+    }
 
     interface HTMLAttributes<T> extends DOMAttributes<T> {
         // React-specific Attributes
