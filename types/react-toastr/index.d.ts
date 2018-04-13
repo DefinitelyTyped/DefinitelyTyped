7bfbf91a77a26d5def58f93d301b572bc45d1b00
Dan Regazzi <dregazzi@dminc.com>
2018-04-13 10:54:09 -0400

    Revert version number to match toastr's version number Change toast notify functions to use string or ReactNode types explicitly Revert change to options override type

--- types/react-toastr/index.d.ts
+++ types/react-toastr/index.d.ts
@@ -1,4 +1,4 @@
-// Type definitions for react-toastr 3.1
+// Type definitions for react-toastr 3.0
 // Project: https://github.com/tomchentw/react-toastr
 // Definitions by: Josh Holmer <https://github.com/shssoichiro>, Dan Regazzi <https://github.com/DanRegazzi>
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
@@ -10,10 +10,10 @@ export class ToastContainer extends Component<{
     toastMessageFactory: any;
     className?: string;
 }> {
-    error: (message: any, title: any, optionsOverride?: any) => void;
-    info: (message: any, title: any, optionsOverride?: any) => void;
-    success: (message: any, title: any, optionsOverride?: any) => void;
-    warning: (message: any, title: any, optionsOverride?: any) => void;
+    error: (message: string | React.ReactNode, title: string | React.ReactNode, optionsOverride?: {}) => void;
+    info: (message: string | React.ReactNode, title: string | React.ReactNode, optionsOverride?: {}) => void;
+    success: (message: string | React.ReactNode, title: string | React.ReactNode, optionsOverride?: {}) => void;
+    warning: (message: string | React.ReactNode, title: string | React.ReactNode, optionsOverride?: {}) => void;
     clear: () => void;
 }
 export const ToastMessageAnimated: keyof ReactHTML;
