580b014ce5d3bb77fd71be4acc50f0be32675283
Dmitrii Sorin <info@staypositive.ru>
2018-04-14 06:54:44 +1000

    Update index.d.ts

--- types/mocha/index.d.ts
+++ types/mocha/index.d.ts
@@ -1,10 +1,11 @@
-// Type definitions for mocha 5.0
+// Type definitions for mocha 5.1
 // Project: http://mochajs.org/
 // Definitions by: Kazi Manzur Rashid <https://github.com/kazimanzurrashid>
 //                 otiai10 <https://github.com/otiai10>
 //                 jt000 <https://github.com/jt000>
 //                 Vadim Macagon <https://github.com/enlight>
 //                 Andrew Bradley <https://github.com/cspotcode>
+//                 Dmitrii Sorin <https://github.com/1999>
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
 // TypeScript Version: 2.1
 
