07cf1fe3952f5f82469714c1d6f62e3ce82ad97c
Kees C. Bakker <KeesCBakker@users.noreply.github.com>
2018-04-14 12:34:16 +0200

    Added some extra defintions
    - Alias and name can be used to identify the name of the bot.
    - helpCommands will list all help commands
    - loadFile will load a script file from a directory into the bot

--- types/hubot/index.d.ts
+++ types/hubot/index.d.ts
@@ -33,12 +33,16 @@ declare namespace Hubot {
     type ListenerCallback<R> = (response: Response<R>) => void;
 
     class Robot<A> {
+        alias: string;
         brain: Brain;
+        name: string;
         readonly adapter: A;
 
         constructor(adapterPath: string, adapter: string, httpd: boolean, name: string, alias?: string);
         hear(regex: RegExp, callback: ListenerCallback<this>): void;
         hear(regex: RegExp, options: any, callback: ListenerCallback<this>): void;
+        helpCommands(): Array<string>;
+        loadFile(directory: string, fileName: string): void;                                       
         respond(regex: RegExp, callback: ListenerCallback<this>): void;
         respond(regex: RegExp, options: any, callback: ListenerCallback<this>): void;
     }
