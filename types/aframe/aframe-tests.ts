324a90bfe3d175706bfa13d212294d69017d607c
Trygve Wastvedt <twastvedt@azahner.com>
2018-04-14 10:59:51 -0500

    Add tests for class inheritance.

--- types/aframe/aframe-tests.ts
+++ types/aframe/aframe-tests.ts
@@ -38,20 +38,39 @@ entity.addEventListener('child-detached', (event) => {
 });
 
 // Components
-const Component = AFRAME.registerComponent('test', {
+
+class TestComponent extends AFrame.Component {
+	multiply: (f: number) => number;
+
+	data: {
+		myProperty: any[],
+		string: string,
+		num: number
+	};
+
+	system: TestSystem;
+}
+
+const Component = AFRAME.registerComponent<TestComponent>('test-component', {
 	schema: {
 		myProperty: {
 			default: [],
-			parse: function () { },
+			parse() { return [true]; },
 		},
-		string: { type: 'string' }
+		string: { type: 'string' },
+		num: 0
 	},
-	init: function () {},
-	update: function () {},
-	tick: function () {},
-	remove: function () {},
-	pause: function () {},
-	play: function () {}
+	init() {},
+	update() {},
+	tick() {},
+	remove() {},
+	pause() {},
+	play() {},
+
+	multiply(this: TestComponent, f: number) {
+		// Reference to system because both were registered with the same name.
+		return f * this.data.num * this.system.data.counter;
+	}
 });
 
 // Scene
@@ -59,14 +78,40 @@ const scene = document.querySelector('a-scene');
 scene.hasLoaded;
 
 // System
-const system = scene.systems['systemName'];
+
+class TestSystem extends AFrame.System {
+	data: {
+		counter: number;
+	};
+}
+
+const testSystem: AFrame.SystemDefinition<TestSystem> = {
+	schema: {
+		counter: 0
+	},
+
+	init(this: TestSystem) {
+		this.data.counter = 1;
+	}
+};
+
+AFRAME.registerSystem('test-component', testSystem);
 
 // Register Custom Geometry
-AFRAME.registerGeometry('a-test-geometry', {
+
+class TestGeometry extends AFrame.Geometry {
+	schema: AFrame.MultiPropertySchema<{
+		groupIndex: number;
+	}>;
+}
+
+AFRAME.registerGeometry<TestGeometry>('a-test-geometry', {
 	schema: {
 		groupIndex: { default: 0 }
 	},
 	init(data) {
 		this.geometry = new THREE.Geometry();
+		const temp = data.groupIndex;
+		temp;
 	}
 });
