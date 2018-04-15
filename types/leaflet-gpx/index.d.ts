4fc2b81d67f75468160e3afc00e708f6d44554fc
Molnár Márton <molnar.marton@gmail.com>
2018-04-15 20:44:51 +0200

    fixed header

--- types/leaflet-gpx/index.d.ts
+++ types/leaflet-gpx/index.d.ts
@@ -1,4 +1,4 @@
-// Type definitions for leaflet-gpx 1.3.1
+// Type definitions for leaflet-gpx 1.3
 // Project: https://github.com/mpetazzoni/leaflet-gpx
 // Definitions by: Viktor Soucek <https://github.com/soucekv>, Márton Molnár <https://github.com/molnarm>
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
