463405d9f201b602fa7ba1b6df1a4ecb3405f51e
Molnár Márton <molnar.marton@gmail.com>
2018-04-15 19:19:38 +0200

    updated header

--- types/leaflet-gpx/index.d.ts
+++ types/leaflet-gpx/index.d.ts
@@ -1,6 +1,6 @@
-// Type definitions for leaflet-gpx 1.1
+// Type definitions for leaflet-gpx 1.3.1
 // Project: https://github.com/mpetazzoni/leaflet-gpx
-// Definitions by: Viktor Soucek <https://github.com/soucekv>
+// Definitions by: Viktor Soucek <https://github.com/soucekv>, Márton Molnár <https://github.com/molnarm>
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
 // TypeScript Version: 2.3
 
