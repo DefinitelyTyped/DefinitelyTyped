630343624e10757e554b3ace3b8fb7f2a5d859cc
mtsg <mtsg4code@outlook.jp>
2018-04-15 23:57:31 +0900

    Declare TypeScript version.

--- types/react-router-param-link/index.d.ts
+++ types/react-router-param-link/index.d.ts
@@ -2,6 +2,7 @@
 // Project: https://github.com/mtsg/react-router-param-link
 // Definitions by: Motosugi Murata <https://github.com/mtsg>
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
+// TypeScript Version: 2.6
 
 import * as React from "react";
 import { LinkProps } from "react-router-dom";
