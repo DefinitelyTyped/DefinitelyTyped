d217d13a2b344b92abfd6441daaf3dc10754717a
Kees C. Bakker <KeesCBakker@users.noreply.github.com>
2018-04-15 21:50:13 +0200

    Update index.d.ts

--- types/hubot/index.d.ts
+++ types/hubot/index.d.ts
@@ -1,6 +1,7 @@
 // Type definitions for hubot 2.19
 // Project: https://github.com/github/hubot
 // Definitions by: Dirk Gadsden <https://github.com/dirk>
+//                 Kees C. Bakker <https://github.com/KeesCBakker>
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
 
 declare namespace Hubot {
