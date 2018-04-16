5fc520a6b2ca84faf6f09c4522468d87c87de1d2
Tomislav Fabeta <tomislav.fabeta@gmail.com>
2018-04-16 16:56:15 +0200

    removed declare module

--- types/schema-registry/index.d.ts
+++ types/schema-registry/index.d.ts
@@ -5,54 +5,52 @@
 // TypeScript Version: 2.4
 
 /// <reference types="node" />
-declare module "schema-registry" {
-    interface RegistryClientConfig {
-        host: string;
-        port: number;
-        protocol?: string;
-        logger?: object;
-        type?: string;
-    }
+export interface RegistryClientConfig {
+    host: string;
+    port: number;
+    protocol?: string;
+    logger?: object;
+    type?: string;
+}
 
-    interface AvroSchemaResponseInterface {
-        subject: string;
-        version: number;
-        id: number;
-        schema: any;
-    }
+export interface AvroSchemaResponseInterface {
+    subject: string;
+    version: number;
+    id: number;
+    schema: any;
+}
 
-    type RegistryRequest = Promise<AvroSchemaResponseInterface>;
+export type RegistryRequest = Promise<AvroSchemaResponseInterface>;
 
-    class LivingAvroSchema extends RegistryClient {
-        fetch: (poll?: boolean) => RegistryRequest;
-        toBuffer: (object: object) => Buffer;
-        fromBuffer: (buffer: Buffer) => any;
-        on: (...args: any[]) => undefined;
-        removeListener: (...args: any[]) => undefined;
-        stop: () => undefined;
-        constructor(subject: string, version: string, config: RegistryClientConfig);
-    }
+export class LivingAvroSchema extends RegistryClient {
+    fetch: (poll?: boolean) => RegistryRequest;
+    toBuffer: (object: object) => Buffer;
+    fromBuffer: (buffer: Buffer) => any;
+    on: (...args: any[]) => undefined;
+    removeListener: (...args: any[]) => undefined;
+    stop: () => undefined;
+    constructor(subject: string, version: string, config: RegistryClientConfig);
+}
 
-    class RegistryClient {
-         host: string;
-         port: number;
-         protocol: string;
-         type: string;
-         logger: object;
+export class RegistryClient {
+    host: string;
+    port: number;
+    protocol: string;
+    type: string;
+    logger: object;
 
-         request: (options: object, expectedStatusCode: number) => RegistryRequest;
-         isAlive: () => RegistryRequest;
-         registerSubjectVersion: (subject: string, schema: object) => RegistryRequest;
-         getVersionsForSubject: (subject: string) => RegistryRequest;
-         getConfig: () => RegistryRequest;
-         setConfig: (config: object) => RegistryRequest;
-         setSubjectConfig: (subject: string, config: object) => RegistryRequest;
-         getSubjectConfig: (subject: string) => RegistryRequest;
-         getSchemaById: (id: number) => RegistryRequest;
-         getSubjects: () => RegistryRequest;
-         getSubjectSchemaForVersion: (subject: string, version: number) => RegistryRequest;
-         getLatestSubjectSchema: (subject: string) => RegistryRequest;
-         checkSubjectRegistration: (subject: string, schema: object) => RegistryRequest;
-         constructor(config: RegistryClientConfig);
-    }
+    request: (options: object, expectedStatusCode: number) => RegistryRequest;
+    isAlive: () => RegistryRequest;
+    registerSubjectVersion: (subject: string, schema: object) => RegistryRequest;
+    getVersionsForSubject: (subject: string) => RegistryRequest;
+    getConfig: () => RegistryRequest;
+    setConfig: (config: object) => RegistryRequest;
+    setSubjectConfig: (subject: string, config: object) => RegistryRequest;
+    getSubjectConfig: (subject: string) => RegistryRequest;
+    getSchemaById: (id: number) => RegistryRequest;
+    getSubjects: () => RegistryRequest;
+    getSubjectSchemaForVersion: (subject: string, version: number) => RegistryRequest;
+    getLatestSubjectSchema: (subject: string) => RegistryRequest;
+    checkSubjectRegistration: (subject: string, schema: object) => RegistryRequest;
+    constructor(config: RegistryClientConfig);
 }
