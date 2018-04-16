81d8610731a78a5fdd4aa9a91b4f0f262c648d6a
Periklis Tsirakidis <periklis.tsirakidis@holidaycheck.com>
2018-04-16 15:03:49 +0200

    react-lazyload: Update to 2.3.0

--- types/react-lazyload/index.d.ts
+++ types/react-lazyload/index.d.ts
@@ -1,15 +1,15 @@
-// Type definitions for react-lazyload ver 2.2
+// Type definitions for react-lazyload ver 2.3
 // Project: https://github.com/jasonslyvia/react-lazyload
 // Definitions by: m0a <https://github.com/m0a>
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
 // TypeScript Version: 2.6
 
-import { Component } from 'react';
+import { Component } from "react";
 
 export interface LazyLoadProps {
     once?: boolean;
     height?: number | string;
-    offset?: number | string;
+    offset?: number | number[];
     overflow?: boolean;
     scroll?: boolean;
     children?: JSX.Element;
@@ -23,6 +23,6 @@ export default class LazyLoad extends Component<LazyLoadProps> {
     constructor(props: LazyLoad);
 }
 
-export  function lazyload(option: {}): LazyLoad;
+export function lazyload(option: {}): LazyLoad;
 
 export function forceCheck(): void;
