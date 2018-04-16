594af57f4214e02edbc28a793389098b3afc8b33
Arne Schubert <arne.schubert@rewe-digital.com>
2018-04-16 09:47:39 +0200

    Merge changes from generator

--- types/is-ci/index.d.ts
+++ types/is-ci/index.d.ts
@@ -1,9 +1,8 @@
 // Type definitions for is-ci 1.1
 // Project: https://github.com/watson/is-ci
-// Definitions by: My Self <https://github.com/me>
+// Definitions by: Arne Schubert <https://github.com/atd-schubert>
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
 
 export = is_ci;
 
 declare const is_ci: boolean;
-
