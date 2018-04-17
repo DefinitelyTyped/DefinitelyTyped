e460cfa0a3ac6d24a9a2ac9897054a71f1f1b04f
Kyle Roach <kroach.work@gmail.com>
2018-04-17 11:35:25 -0400

    (react-native) Adds SectionList scrollToLocation (#25050)

--- types/react-native/index.d.ts
+++ types/react-native/index.d.ts
@@ -3736,6 +3736,14 @@ export interface SectionListData<ItemT> extends SectionBase<ItemT> {
     [key: string]: any;
 }
 
+export interface SectionListScrollParams {
+    animated?: boolean;
+    itemIndex: number;
+    sectionIndex: number;
+    viewOffset?: number;
+    viewPosition?: number;
+}
+
 export interface SectionListProperties<ItemT> extends ScrollViewProperties {
     /**
      * Rendered in between adjacent Items within each section.
@@ -3844,6 +3852,13 @@ export interface SectionListProperties<ItemT> extends ScrollViewProperties {
      * Only enabled by default on iOS because that is the platform standard there.
      */
     stickySectionHeadersEnabled?: boolean;
+
+    /**
+     * Scrolls to the item at the specified sectionIndex and itemIndex (within the section)
+     * positioned in the viewable area such that viewPosition 0 places it at the top
+     * (and may be covered by a sticky header), 1 at the bottom, and 0.5 centered in the middle.
+     */
+    scrollToLocation?(params: SectionListScrollParams): void;
 }
 
 export interface SectionListStatic<SectionT> extends React.ComponentClass<SectionListProperties<SectionT>> {}
