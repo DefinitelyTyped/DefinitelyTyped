cfa60b7c664ad32b5fd9c008f83178f90cda7b58
Chris Krycho <chris@chriskrycho.com>
2018-04-17 19:12:12 -0600

    Ember: Make `RegistryProxyMixin.register` options optional. (#25076)
    * Ember: Make `RegistryProxyMixin.register` options optional.
    
    * Add tests for register change.

--- types/ember/index.d.ts
+++ types/ember/index.d.ts
@@ -325,7 +325,7 @@ declare module 'ember' {
              * `inject`) or for service lookup. Each factory is registered with
              * a full name including two parts: `type:name`.
              */
-            register(fullName: string, factory: Function, options: {}): any;
+            register(fullName: string, factory: Function, options?: { singleton?: boolean, instantiate?: boolean }): any;
             /**
              * Unregister a factory.
              */
