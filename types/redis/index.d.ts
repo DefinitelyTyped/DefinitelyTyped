1feff910d7db0b5c21be93341235f2169dd23d30
Markmark@gamekon.com <mark@gamekon.com>
2018-04-18 14:16:31 +0800

    hset can have an array as the 1st param
    hset can have an array as the 1st param as the offical example shows.

--- types/redis/index.d.ts
+++ types/redis/index.d.ts
@@ -514,6 +514,8 @@ export interface Commands<R> {
      */
     hset(key: string, field: string, value: string, cb?: Callback<number>): R;
     HSET(key: string, field: string, value: string, cb?: Callback<number>): R;
+    hset(setArr:Array<string>,cb? Callback<number>): R;
+    HSET(setArr:Array<string>,cb? Callback<number>): R;
 
     /**
      * Set the value of a hash field, only if the field does not exist.
