74791cede040715168890de0903ba0865c4da9b4
Melissa Hong <mel@shoesofprey.com>
2018-04-18 18:26:56 -0700

    Don't include patch version

--- types/react-slick/index.d.ts
+++ types/react-slick/index.d.ts
@@ -1,4 +1,4 @@
-// Type definitions for react-slick 0.23.1
+// Type definitions for react-slick 0.23
 // Project: https://github.com/akiran/react-slick
 // Definitions by: Andrey Balokha <https://github.com/andrewBalekha>
 //                 Giedrius Grabauskas <https://github.com/GiedriusGrabauskas>
