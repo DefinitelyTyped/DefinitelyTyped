3b59d7997f5a8e41af66f6ed13c9f4eedaef666e
Alex Watson <suniahk@users.noreply.github.com>
2018-04-18 23:18:03 -0400

    Adding credit for updates

--- types/react-native-drawer/index.d.ts
+++ types/react-native-drawer/index.d.ts
@@ -1,6 +1,7 @@
 // Type definitions for react-native-drawer 2.3
 // Project: https://github.com/root-two/react-native-drawer
 // Definitions by: jnbt <https://github.com/jnbt>
+                   suniahk <https://github.com/suniahk>
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
 // TypeScript Version: 2.6
 
