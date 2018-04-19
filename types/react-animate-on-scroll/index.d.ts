d43a2664058c610456110eb8ff56e57eee1f3d74
Ricardo Albuquerque Pinto <ralbuque@sereno.com.br>
2018-04-19 10:35:08 -0300

    Corrected itens pointed by tslint

--- types/react-animate-on-scroll/index.d.ts
+++ types/react-animate-on-scroll/index.d.ts
@@ -1,27 +1,24 @@
-// Type definitions for react-animate-on-scroll v2.1.4
+// Type definitions for react-animate-on-scroll 2.1
 // Project: https://github.com/dbramwell/react-animate-on-scroll
-// Definitions by: Ricardo Albuquerque <https://github.com/ralbuque/>
+// Definitions by: Ricardo Albuquerque <https://github.com/ralbuque>
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
 // TypeScript Version: 2.6
 
 import * as React from 'react';
 
 export interface ScrollAnimationProps {
-    animateIn?: string,
-    animateOut?: string,
-    offset?: number,
-    duration?: number,
-    delay?: number,
-    initiallyVisible?: boolean,
-    animateOnce?: boolean,
-    style?: object,
-    scrollableParentSelector?: string,
-    className?: string
+    animateIn?: string;
+    animateOut?: string;
+    offset?: number;
+    duration?: number;
+    delay?: number;
+    initiallyVisible?: boolean;
+    animateOnce?: boolean;
+    style?: object;
+    scrollableParentSelector?: string;
+    className?: string;
 }
 
 export default class ScrollAnimation extends React.Component<ScrollAnimationProps> {
-
     constructor(props: ScrollAnimationProps);
-
 }
-
