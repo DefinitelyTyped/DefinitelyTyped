25d6d56853eea9615b4674d3a2fe776987381caa
jbencook <ben.cook@hudl.com>
2018-04-23 15:09:54 -0500

    fix linting and header

--- types/base64-arraybuffer/index.d.ts
+++ types/base64-arraybuffer/index.d.ts
@@ -1,4 +1,4 @@
-// Type definitions for base64-arraybuffer 0.1.5
+// Type definitions for base64-arraybuffer 0.1
 // Project: https://github.com/niklasvh/base64-arraybuffer
 // Definitions by: Ben Cook <https://github.com/jbencook>
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
