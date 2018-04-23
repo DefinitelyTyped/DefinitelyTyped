03acef6453ec04a6ea82560f32e195c9d4c86bf5
Alan Plum <me@pluma.io>
2018-04-23 09:20:36 +0200

    Remove attribution as per @mihhail-lapushkin's wishes

--- types/tea-merge/index.d.ts
+++ types/tea-merge/index.d.ts
@@ -1,6 +1,5 @@
 // Type definitions for tea-merge
 // Project: https://github.com/qualiancy/tea-merge
-// Definitions by: Mihhail Lapushkin <https://github.com/mihhail-lapushkin/>
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
 
 
