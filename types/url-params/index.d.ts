c8b505dd20359c7df5a6472db1d7fe50d4b94398
Daniel <mytechde@outlook.com>
2018-04-23 10:06:07 +0200

    Added type definitions for url-params

---
+++ types/url-params/index.d.ts
@@ -0,0 +1,27 @@
+// Type definitions for url-params 1.0
+// Project: https://github.com/AtenDesignGroup/url-params
+// Definitions by: Daniel Sogl <https://github.com/danielsogl>
+// Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
+export function add(oldUrl: string, param: string, value: any): string;
+
+export function createUrlObject(oldUrl: string): any;
+
+export function remove(oldUrl: string, param: string, value: any): string;
+
+export function set(oldUrl: string, param: string, value: any): string;
+
+export namespace add {
+  const prototype: {};
+}
+
+export namespace createUrlObject {
+  const prototype: {};
+}
+
+export namespace remove {
+  const prototype: {};
+}
+
+export namespace set {
+  const prototype: {};
+}
