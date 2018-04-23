02dc065b9d19b82cb2c3db7b042723130fc4315c
Alan Plum <me@pluma.io>
2018-04-23 17:47:09 +0200

    More example code

--- types/arangodb/arangodb-tests.ts
+++ types/arangodb/arangodb-tests.ts
@@ -1,4 +1,6 @@
 import { db } from "@arangodb";
+import { md5 } from "@arangodb/crypto";
+import { createRouter } from "@arangodb/foxx";
 
 console.warnStack(new Error(), "something went wrong");
 
@@ -13,4 +15,28 @@ console.log(doc.username);
 
 const users = coll as ArangoDB.Collection<User>;
 const admin = users.firstExample({ username: "admin" })!;
-users.update(admin, { password: "hunter2" });
+users.update(admin, { password: md5("hunter2") });
+console.logLines("user", admin._key, admin.username);
+
+db._createDocumentCollection("bananas").ensureIndex({
+    type: "hash",
+    unique: true,
+    fields: ["color", "shape"]
+});
+
+const router = createRouter();
+module.context.use(router);
+
+router.get("/", (req, res) => {
+    if (req.cookie("sid", { secret: "keyboardcat" })) {
+        res.set("content-type", "text/plain");
+        res.write("Welcome back, Commander");
+    } else {
+        res.json({ success: false });
+    }
+});
+
+router.use((req, res, next) => {
+    if (req.is("json")) res.throw("i'm a teapot");
+    next();
+});
