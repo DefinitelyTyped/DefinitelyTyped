b95ceaa703a5a10fb64301cc821e41215a3083e3
Michael Utz <michael.utz@nordsafety.com>
2018-04-23 10:48:19 +0300

    added jest-each module declarations

---
+++ types/jest-each/jest-each-tests.ts
@@ -0,0 +1,17 @@
+import * as each from 'jest-each';
+
+const params = [[1, 0, 1], [1, 1, 0], ['1', 'two', 'three']];
+
+each(params).test('', () => {});
+each(params).it('', () => {});
+each(params).test.only('', () => {});
+each(params).fit('', () => {});
+each(params).test.skip('', () => {});
+each(params).xit('', () => {});
+each(params).xtest('', () => {});
+
+each(params).describe('', () => {});
+each(params).describe.only('', () => {});
+each(params).fdescribe('', () => {});
+each(params).describe.skip('', () => {});
+each(params).xdescribe('', () => {});
