8ca8584b69063fd0adcd8e9983efdd867a017a70
Thomas Charlat <thomas.charlat@gmail.com>
2018-04-23 11:33:43 +0200

    feat(react-redux): add new commiters

--- types/react-redux/index.d.ts
+++ types/react-redux/index.d.ts
@@ -8,6 +8,9 @@
 //                 Nicholas Boll <https://github.com/nicholasboll>
 //                 Dibyo Majumdar <https://github.com/mdibyo>
 //                 Prashant Deva <https://github.com/pdeva>
+//                 Thomas Charlat <https://github.com/kallikrein>
+//                 Valentin Descamps <https://github.com/val1984>
+//                 Johann Rakotoharisoa <https://github.com/jrakotoharisoa>
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
 // TypeScript Version: 2.6
 
