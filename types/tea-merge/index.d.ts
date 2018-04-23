83e8f3038ef9b1e78b709d9791a443ecd1c9b94f
Alan Plum <me@pluma.io>
2018-04-23 18:11:02 +0200

    Update index.d.ts

--- types/tea-merge/index.d.ts
+++ types/tea-merge/index.d.ts
@@ -1,5 +1,6 @@
 // Type definitions for tea-merge
 // Project: https://github.com/qualiancy/tea-merge
+// Definitions by: No one <null@example.com>
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
 
 
