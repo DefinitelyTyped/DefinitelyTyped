3e11f9283cbe39061afbbc29b51f031733932e5e
Zev Spitz <shivisi@hotmail.com>
2018-04-24 18:59:45 +0200

    Default properties

--- types/activex-msxml2/activex-msxml2.d.ts
+++ types/activex-msxml2/activex-msxml2.d.ts
@@ -1,9 +1,3 @@
-// Type definitions for Microsoft XML, v6.0 - MSXML2 6.0
-// Project: https://msdn.microsoft.com/en-us/library/ms763742.aspx
-// Definitions by: Zev Spitz <https://github.com/zspitz>
-// Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
-// TypeScript Version: 2.3
-
 declare namespace MSXML2 {
     /** Schema Object Model Content Types */
     const enum _SCHEMACONTENTTYPE {
