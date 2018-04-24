508d1e2eaad9de885feb9ae642ed57485ea40f6d
Michael Utz <michael.utz@nordsafety.com>
2018-04-24 09:17:02 +0300

    test: updated tests for `it.skip` and `it.only`

--- types/jest-each/jest-each-tests.ts
+++ types/jest-each/jest-each-tests.ts
@@ -5,8 +5,10 @@ const params = [[1, 0, 1], [1, 1, 0], ['1', 'two', 'three']];
 each(params).test('', () => {});
 each(params).it('', () => {});
 each(params).test.only('', () => {});
+each(params).it.only('', () => {});
 each(params).fit('', () => {});
 each(params).test.skip('', () => {});
+each(params).it.skip('', () => {});
 each(params).xit('', () => {});
 each(params).xtest('', () => {});
 
