5fcc94e103e5affdc079cb9ef0e9baf87049eec5
Behind The Math <info@behindthemath.io>
2018-04-24 17:35:11 -0400

    Add minimum TS version
    These definitions depends on request, which has a minimum TS version
     of 2.3

--- types/proxy-verifier/index.d.ts
+++ types/proxy-verifier/index.d.ts
@@ -2,6 +2,7 @@
 // Project: https://github.com/chill117/proxy-verifier#readme
 // Definitions by: BehindTheMath <https://github.com/BehindTheMath>
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
+// TypeScript Version: 2.3
 
 import { CoreOptions as RequestOptions } from "request";
 
