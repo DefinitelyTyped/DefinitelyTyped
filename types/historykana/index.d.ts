fe608b3271780c372489cd0329572fd2d72020a0
h-shiratsuki <siratsuki2125@yahoo.co.jp>
2018-04-24 09:39:54 +0900

    Fixed test error

--- types/historykana/index.d.ts
+++ types/historykana/index.d.ts
@@ -1,26 +1,18 @@
-// Type definitions for HistoryKana 1.0.0
+// Type definitions for HistoryKana 1.0
 // Project: https://github.com/terrierscript/historykana
 // Definitions by: shiratsuki <https://github.com/h-shiratsuki>
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
-// HistoryKana Version: 1.0.4
+// TypeScript Version：2.2
 
-declare module "historykana" {
-    export default HistoryKana;
-}
+/**
+ * Get Kana string from input history array.
+ * @param histories Input History Array
+ */
+export default function historykana(histories: string[]): string;
 
-declare const HistoryKana: HistoryKanaStatic;
-
-interface HistoryKanaStatic {
-    /**
-     * Get Kana string from input history array.
-     * @param histories Input History Array
-     */
-    (histories: string[]): string;
-
-    /**
-     * Get Kana string from input history array.
-     * @param histories Input History Array
-     * @param options kanaRegexp : Hiragana detection regular expression rule.
-     */
-    (histories: string[], options: object): string;
-}
+/**
+ * Get Kana string from input history array.
+ * @param histories Input History Array
+ * @param options kanaRegexp : Hiragana detection regular expression rule.
+ */
+export default function historykana(histories: string[], options: object): string;
