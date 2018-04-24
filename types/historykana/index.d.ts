9a424d36219dca1274666ee136ec28af35c3185a
h-shiratsuki <siratsuki2125@yahoo.co.jp>
2018-04-24 09:57:01 +0900

    Fixed unified-signatures error

--- types/historykana/index.d.ts
+++ types/historykana/index.d.ts
@@ -2,17 +2,14 @@
 // Project: https://github.com/terrierscript/historykana
 // Definitions by: shiratsuki <https://github.com/h-shiratsuki>
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
-// TypeScript Version：2.2
-
-/**
- * Get Kana string from input history array.
- * @param histories Input History Array
- */
-export default function historykana(histories: string[]): string;
+// TypeScript Version: 2.3
 
 /**
  * Get Kana string from input history array.
  * @param histories Input History Array
  * @param options kanaRegexp : Hiragana detection regular expression rule.
  */
-export default function historykana(histories: string[], options: object): string;
+export default function historykana(
+    histories: string[],
+    options?: object
+): string;
