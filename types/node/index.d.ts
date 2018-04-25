d01ccfcdcbae2e4e934e4bdd540f78e85ff548a5
Simon Chan <cnsimonchan@live.com>
2018-04-25 13:46:27 +0800

    fix(node): fix a typo

--- types/node/index.d.ts
+++ types/node/index.d.ts
@@ -2882,7 +2882,7 @@ declare module "net" {
         listen(options: ListenOptions, listeningListener?: Function): this;
         listen(handle: any, backlog?: number, listeningListener?: Function): this;
         listen(handle: any, listeningListener?: Function): this;
-        close(callback?: () => {}): this;
+        close(callback?: () => void): this;
         address(): { port: number; family: string; address: string; };
         getConnections(cb: (error: Error | null, count: number) => void): void;
         ref(): this;
