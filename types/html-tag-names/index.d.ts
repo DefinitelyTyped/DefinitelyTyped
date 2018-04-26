21d07f0fcfe33b45ac44efcd508af63353ce8744
Nathan Shively-Sanders <293473+sandersn@users.noreply.github.com>
2018-04-26 09:18:33 -0700

    Add author header

--- types/html-tag-names/index.d.ts
+++ types/html-tag-names/index.d.ts
@@ -1,2 +1,6 @@
+// Type definitions for html-tag-names 1.1.2
+// Project: https://github.com/wooorm/html-tag-names
+// Definitions by: Nathan Shively-Sanders <https://github.com/sandersn>
+// Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
 declare const htmlTagNames: string[];
 export = htmlTagNames;
