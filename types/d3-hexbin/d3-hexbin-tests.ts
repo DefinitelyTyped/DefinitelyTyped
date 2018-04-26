04c40017c79b9a64573ed9fc565fcb61e1023252
denisname <denisname@users.noreply.github.com>
2018-04-26 19:31:47 +0200

    Linting d3-hexbin and d3-hierarchy (#25325)

--- types/d3-hexbin/d3-hexbin-tests.ts
+++ types/d3-hexbin/d3-hexbin-tests.ts
@@ -22,22 +22,21 @@ interface Point {
     b.y()([41, 42]); // === 42;
     b.radius();      // === 1;
 
-
     // hexbin(points) bins the specified points into hexagonal bins
     const bins = d3Hexbin.hexbin()([
         [0, 0], [0, 1], [0, 2],
         [1, 0], [1, 1], [1, 2],
         [2, 0], [2, 1], [2, 2]
-    ])
+    ]);
 
-    bins.map((bin: any) => {})
+    bins.map((bin: any) => {});
 }
 
 {
     // hexbin(points) observes the current x- and y-accessors
-    const x = function(d: any) { return d.x; },
-          y = function(d: any) { return d.y; },
-          bins = d3Hexbin.hexbin<Point>().x(x).y(y)([
+    const x = (d: any) => d.x;
+    const y = (d: any) => d.y;
+    const bins = d3Hexbin.hexbin<Point>().x(x).y(y)([
         {x0: 0, y0: 0}, {x0: 0, y0: 1}, {x0: 0, y0: 2},
         {x0: 1, y0: 0}, {x0: 1, y0: 1}, {x0: 1, y0: 2},
         {x0: 2, y0: 0}, {x0: 2, y0: 1}, {x0: 2, y0: 2}
@@ -52,7 +51,7 @@ interface Point {
       [1, 0], [1, 1], [1, 2],
       [2, 0], [2, 1], [2, 2]
     ]);
-    bins.map((bin: any)=> {});
+    bins.map((bin: any) => {});
 }
 
 {
@@ -62,15 +61,15 @@ interface Point {
     }
 
     // hexbin.x(x) sets the x-coordinate accessor
-    const x = function(d: PointX) { return d.x; },
-          b = d3Hexbin.hexbin<PointX>().x(x),
-          bins = b([{x: 1, 1: 2}]);
+    const x = (d: PointX) => d.x;
+    const b = d3Hexbin.hexbin<PointX>().x(x);
+    const bins = b([{x: 1, 1: 2}]);
 
     b.x();          // should be x;
     bins.length;    // should be 1;
     bins[0].x;      // should be 0.8660254037844386;
     bins[0].y;      // should be 1.5;
-    bins[0].length  // should be 1;
+    bins[0].length; // should be 1;
 }
 
 {
@@ -79,9 +78,9 @@ interface Point {
         [key: number]: number;
     }
     // hexbin.y(y) sets the y-coordinate accessor
-    const y = function(d: PointY) { return d.y; },
-          b = d3Hexbin.hexbin<PointY>().y(y),
-          bins = b([{0: 1, y: 2}]);
+    const y = (d: PointY) => d.y;
+    const b = d3Hexbin.hexbin<PointY>().y(y);
+    const bins = b([{0: 1, y: 2}]);
 
     bins.length;    // should be 1;
     bins[0].x;      // should be 0.8660254037844386;
@@ -148,9 +147,8 @@ interface Point {
 }
 
 {
-
     let hb: d3Hexbin.Hexbin<Point>;
-    let bins: d3Hexbin.HexbinBin<Point>[];
+    let bins: Array<d3Hexbin.HexbinBin<Point>>;
 
     // Create generator =======================================
 
@@ -160,8 +158,8 @@ interface Point {
 
     // x Accessor ----------------------------------------------
 
-    let x: (d:Point) => number;
-    x = function (d: Point) { return d.x0; };
+    let x: (d: Point) => number;
+    x = (d: Point) => d.x0;
 
     // test setter
     hb = hb.x(x);
@@ -171,8 +169,8 @@ interface Point {
 
     // y Accessor ----------------------------------------------
 
-    let y: (d:Point) => number;
-    y = function (d: Point) { return d.y0; };
+    let y: (d: Point) => number;
+    y = (d: Point) => d.y0;
 
     // test setter
     hb = hb.y(y);
@@ -193,7 +191,7 @@ interface Point {
         points: Point[];
     }
 
-    let remappedBins: Array<RemappedBin>;
+    let remappedBins: RemappedBin[];
 
     remappedBins = bins.map(bin => {
         const x: number = bin.x; // x-coordinate of bin
