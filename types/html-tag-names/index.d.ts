1c5364d085c16143bef913d192521c78ddb4a684
Nathan Shively-Sanders <293473+sandersn@users.noreply.github.com>
2018-04-26 09:21:48 -0700

    Fix lint

--- types/html-tag-names/index.d.ts
+++ types/html-tag-names/index.d.ts
@@ -1,4 +1,4 @@
-// Type definitions for html-tag-names 1.1.2
+// Type definitions for html-tag-names 1.1
 // Project: https://github.com/wooorm/html-tag-names
 // Definitions by: Nathan Shively-Sanders <https://github.com/sandersn>
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
