0b85e1acd36784591f962d12d215faa587f6109a
h-shiratsuki <siratsuki2125@yahoo.co.jp>
2018-04-27 10:00:06 +0900

    Deleted module.

--- types/historykana/index.d.ts
+++ types/historykana/index.d.ts
@@ -4,12 +4,11 @@
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
 // TypeScript Version: 2.3
 
-declare module "historykana" {
-    /**
-     * Get Kana string from input history array.
-     * @param histories Input History Array
-     * @param options kanaRegexp : Hiragana detection regular expression rule.
-     */
-    function historykana(histories: string[], options?: object): string;
-    export = historykana;
-}
+/**
+ * Get Kana string from input history array.
+ * @param histories Input History Array
+ * @param options kanaRegexp : Hiragana detection regular expression rule.
+ */
+declare function historykana(histories: string[], options?: object): string;
+
+export = historykana;
