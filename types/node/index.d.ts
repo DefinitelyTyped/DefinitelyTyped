60bb4edc54cdecb27770bd7e304f98adac652ca0
Ron Buckton <ron.buckton@microsoft.com>
2018-04-26 17:31:00 -0700

    Remove URL and URLSearchParams from nodeJS global

--- types/node/index.d.ts
+++ types/node/index.d.ts
@@ -2377,44 +2377,40 @@ declare module "url" {
         unicode?: boolean;
     }
 
-    global {
-        class URL {
-            constructor(input: string, base?: string | URL);
-            hash: string;
-            host: string;
-            hostname: string;
-            href: string;
-            readonly origin: string;
-            password: string;
-            pathname: string;
-            port: string;
-            protocol: string;
-            search: string;
-            readonly searchParams: URLSearchParams;
-            username: string;
-            toString(): string;
-            toJSON(): string;
-        }
-
-        class URLSearchParams implements Iterable<[string, string]> {
-            constructor(init?: URLSearchParams | string | { [key: string]: string | string[] | undefined } | Iterable<[string, string]> | Array<[string, string]>);
-            append(name: string, value: string): void;
-            delete(name: string): void;
-            entries(): IterableIterator<[string, string]>;
-            forEach(callback: (value: string, name: string) => void): void;
-            get(name: string): string | null;
-            getAll(name: string): string[];
-            has(name: string): boolean;
-            keys(): IterableIterator<string>;
-            set(name: string, value: string): void;
-            sort(): void;
-            toString(): string;
-            values(): IterableIterator<string>;
-            [Symbol.iterator](): IterableIterator<[string, string]>;
-        }
+    export class URL {
+        constructor(input: string, base?: string | URL);
+        hash: string;
+        host: string;
+        hostname: string;
+        href: string;
+        readonly origin: string;
+        password: string;
+        pathname: string;
+        port: string;
+        protocol: string;
+        search: string;
+        readonly searchParams: URLSearchParams;
+        username: string;
+        toString(): string;
+        toJSON(): string;
     }
 
-    export { URL, URLSearchParams };
+    export class URLSearchParams implements Iterable<[string, string]> {
+        constructor(init?: URLSearchParams | string | { [key: string]: string | string[] | undefined } | Iterable<[string, string]> | Array<[string, string]>);
+        append(name: string, value: string): void;
+        delete(name: string): void;
+        entries(): IterableIterator<[string, string]>;
+        forEach(callback: (value: string, name: string) => void): void;
+        get(name: string): string | null;
+        getAll(name: string): string[];
+        has(name: string): boolean;
+        keys(): IterableIterator<string>;
+        set(name: string, value: string): void;
+        sort(): void;
+        toString(): string;
+        values(): IterableIterator<string>;
+        [Symbol.iterator](): IterableIterator<[string, string]>;
+    }
 }
 
 declare module "dns" {
