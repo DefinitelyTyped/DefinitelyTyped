02e0340fda2be3a482944a2a016a958a6e0230b4
Hanai <ihanai1991@gmail.com>
2018-04-28 14:51:06 +0800

    [react-faux-dom] replace Omit<T, K>
    replace Omit<T, K> with Pick<T, Exclude<keyof T, K>>

--- types/react-faux-dom/index.d.ts
+++ types/react-faux-dom/index.d.ts
@@ -5,7 +5,7 @@
 //                 Michał Kostrzyński <https://github.com/deviousm>
 //                 Hanai <https://github.com/hanai>
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
-// TypeScript Version: 2.6
+// TypeScript Version: 2.8
 
 import * as React from "react";
 
@@ -35,6 +35,4 @@ export interface ReactFauxDomProps {
     isAnimatingFauxDOM(): boolean;
 }
 
-type Omit<T, K extends keyof T> = Pick<T, ({ [P in keyof T]: P } & { [P in K]: never } & { [x: string]: never, [x: number]: never })[keyof T]>;
-
-export function withFauxDOM<P extends { [key: string]: any }>(WrappedComponent: React.ComponentClass<P>): React.ComponentClass<Omit<P, keyof ReactFauxDomProps>>;
+export function withFauxDOM<P>(WrappedComponent: React.ComponentClass<P>): React.ComponentClass<Pick<P, Exclude<keyof P, keyof ReactFauxDomProps>>>;
