bd3f46b25e232e9fc126f17e15819b5135898a23
Thor Sedeke <thor.sedeke@gmail.com>
2018-04-29 18:04:49 +0200

    Add definition for koa-response-time

---
+++ types/koa-response-time/index.d.ts
@@ -0,0 +1,11 @@
+// Type definitions for koa-response-time 2.0
+// Project: https://github.com/koajs/response-time#readme
+// Definitions by: Thor Sedeke <https://github.com/thorsedeke>
+// Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
+// TypeScript Version: 2.3
+
+import * as koa from 'koa';
+
+declare function koa_response_time(): koa.Middleware;
+
+export = koa_response_time;
