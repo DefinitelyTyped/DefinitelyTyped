5765c232775eaa3578a24c6bbd6afc0751a21b9c
David Evans Farinha <davidfarinha@hotmail.co.uk>
2018-04-30 17:37:11 +0300

    Creating type definitions for "react-native-keychain"

---
+++ types/react-native-keychain/index.d.ts
@@ -0,0 +1,81 @@
+declare module 'react-native-keychain' {
+
+    type SecAccessible =
+    'AccessibleWhenUnlocked'
+    | 'AccessibleAfterFirstUnlock'
+    | 'AccessibleAlways'
+    | 'AccessibleWhenPasscodeSetThisDeviceOnly'
+    | 'AccessibleWhenUnlockedThisDeviceOnly'
+    | 'AccessibleAfterFirstUnlockThisDeviceOnly'
+    | 'AccessibleAlwaysThisDeviceOnly';
+    
+    type SecAccessControl =
+    'UserPresence'
+    | 'BiometryAny'
+    | 'BiometryCurrentSet'
+    | 'DevicePasscode'
+    | 'ApplicationPassword'
+    | 'BiometryAnyOrDevicePasscode'
+    | 'BiometryCurrentSetOrDevicePasscode';
+  
+    type LAPolicy = 'Authentication' | 'AuthenticationWithBiometrics';
+
+    export interface Options {
+        accessControl?: SecAccessControl,
+        accessGroup?: string,
+        accessible?: SecAccessible,
+        authenticationPrompt?: string,
+        authenticationType?: LAPolicy,
+        service?: string,
+    }
+
+    export interface UserCredentials {
+        username: string;
+        password: string;
+    }
+
+    export interface SharedWebCredentials {
+        server: string;
+        username: string;
+        password: string;
+    }
+
+    function setInternetCredentials(
+        server: string,
+        username: string,
+        password: string
+    ): Promise<boolean>;
+
+    function getInternetCredentials(
+        server: string
+    ): Promise<UserCredentials>;
+
+    function resetInternetCredentials(
+        server: string
+    ): Promise<boolean>;
+
+    function setGenericPassword(
+        username: string,
+        password: string,
+        service?: string
+    ): Promise<boolean>;
+
+    function getGenericPassword(
+        service?: string
+    ): Promise<boolean>;
+
+    function resetGenericPassword(
+        serviceOrOptions?: string | Options
+    ): Promise<string>;
+
+    function requestSharedWebCredentials(
+    ): Promise<SharedWebCredentials>;
+
+    function setSharedWebCredentials(
+        server: string,
+        username: string,
+        password: string
+    ): Promise<boolean>;
+}
+
+// Definitions by: David Evans Farinha <https://github.com/DavidFarinha>
---
+++ types/react-native-keychain/react-native-keychain-tests.ts
@@ -0,0 +1,15 @@
+import * as Keychain from 'react-native-keychain';
+
+async () => {
+  const username = 'username';
+  const password = 'password';
+
+  await Keychain.setGenericPassword(username, password);
+
+  try {
+    const credentials = await Keychain.getGenericPassword();
+  } catch (error) {
+    throw error;
+  }
+  await Keychain.resetGenericPassword()
+}
