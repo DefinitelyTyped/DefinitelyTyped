161024a37a00c29829caf77c056847618241c6ab
Ville Venäläinen <ville@mindhive.fi>
2018-04-30 14:48:49 +0300

    Definition file for react-native-material-textfield

---
+++ types/react-native-material-textfield/react-native-material-textfield-tests.tsx
@@ -0,0 +1,10 @@
+import * as React from 'react';
+import { View, Text } from 'react-native';
+import { TextField } from 'react-native-material-textfield';
+
+
+const Example = () =>
+    <View>
+        <TextField label="Example" />
+    </View>;
+
