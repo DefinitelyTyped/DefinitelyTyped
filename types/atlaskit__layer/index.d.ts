fb3dfada690e0bdebfd5a0baa6e4a648d793ae10
Lee Standen <lstanden@atlassian.com>
2018-04-30 12:25:54 -0700

    fix name of module in comment

--- types/atlaskit__layer/index.d.ts
+++ types/atlaskit__layer/index.d.ts
@@ -1,4 +1,4 @@
-// Type definitions for atlaskit__layer 3.1
+// Type definitions for @atlaskit/layer 3.1
 // Project: https://bitbucket.org/atlassian/atlaskit-mk-2/src/master/
 // Definitions by: Lee Standen <https://github.com/lstanden>
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
