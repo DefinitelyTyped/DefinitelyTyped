31a5880939de505166f22d783de2a6b6c23c64bb
Gaurav Lahoti <Dante-101@users.noreply.github.com>
2018-04-30 21:42:49 +0530

    redux-persist-transform-encrypt 2.0 definitions (#25231)

--- types/redux-persist-transform-encrypt/index.d.ts
+++ types/redux-persist-transform-encrypt/index.d.ts
@@ -1,12 +1,14 @@
-// Type definitions for redux-persist-transform-encrypt 1.0
+// Type definitions for redux-persist-transform-encrypt 2.0
 // Project: https://github.com/maxdeviant/redux-persist-transform-encrypt
 // Definitions by: Karol Janyst <https://github.com/LKay>
+//                 Gaurav Lahoti <https://github.com/dante-101>
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
 
 import { Transform } from "redux-persist";
 
 export interface EncryptorConfig {
     secretKey: string;
+    onError?: (err: Error) => void;
 }
 
 export default function createEncryptor<State, Raw>(config: EncryptorConfig): Transform<State, Raw>;
