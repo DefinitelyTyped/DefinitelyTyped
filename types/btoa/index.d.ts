82d2304fc82b8056fadef24c1ec34f077d1d1523
John Wright <johngeorge.wright@gmail.com>
2018-04-30 12:33:20 +0100

    feat(types): add the atob btoa npm module definitions

---
+++ types/btoa/index.d.ts
@@ -0,0 +1,9 @@
+// Type definitions for btoa 1.2.1
+// Project: https://git.coolaj86.com/coolaj86/btoa.js
+// Definitions by: John Wright <https://github.com/johngeorgewright>
+// Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
+// TypeScript Version: 2.8.3
+
+declare module 'btoa' {
+    export default function (str: string): string
+}
