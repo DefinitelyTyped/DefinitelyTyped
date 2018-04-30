ceb7ba7c4e8e5f65ac7fa06e727b97219c365983
plylrnsdy <plylrnsdy@163.com>
2018-04-30 21:06:09 +0800

    update ansi-styles test

--- types/ansi-styles/ansi-styles-tests.ts
+++ types/ansi-styles/ansi-styles-tests.ts
@@ -1,76 +1,50 @@
-import ansiStyles = require('ansi-styles');
 import { EscapeCode } from './EscapeCode';
+import AnsiStyles = require('ansi-styles');
 
 let
-    modifier = [
-        'reset', 'bold', 'dim', 'italic', 'underline', 'inverse', 'hidden', 'strikethrough'
-    ],
-    color = [
-        'black', 'red', 'green', 'yellow', 'blue', 'magenta', 'cyan', 'white', 'gray',
-        // Bright Color
-        'redBright', 'greenBright', 'yellowBright', 'blueBright', 'magentaBright', 'cyanBright', 'whiteBright'
-    ],
-    bgColor = [
-        'bgBlack', 'bgRed', 'bgGreen', 'bgYellow', 'bgBlue', 'bgMagenta', 'bgCyan',
-        // Bright bgColor
-        'bgBlackBright', 'bgRedBright', 'bgGreenBright', 'bgYellowBright', 'bgBlueBright', 'bgMagentaBright', 'bgCyanBright', 'bgWhiteBright'
-    ],
-    styles = [...modifier, ...color, ...bgColor],
-    codeTypes = [
-        'ansi', 'ansi256', 'ansi16m'
-    ],
-    colorFormats = [
-        'ansi', 'rgb', 'hsl', 'hsv', 'hwb', 'cmyk', 'xyz', 'lab', 'lch', 'hex', 'keyword', 'ansi256', 'hcg', 'apple', 'gray'
-    ],
-    namespaces = [
-        'modifier', 'color', 'bgColor'
-    ],
+    ansiStyles = AnsiStyles as any,
+    nsNames = ['modifier', 'color', 'bgColor'],
+    namespaces = {
+        modifier: ['reset', 'bold', 'dim', 'italic', 'underline', 'inverse', 'hidden', 'strikethrough'],
+        color: ['black', 'red', 'green', 'yellow', 'blue', 'magenta', 'cyan', 'white', 'gray',
+            'redBright', 'greenBright', 'yellowBright', 'blueBright', 'magentaBright', 'cyanBright', 'whiteBright'],
+        bgColor: ['bgBlack', 'bgRed', 'bgGreen', 'bgYellow', 'bgBlue', 'bgMagenta', 'bgCyan',
+            'bgBlackBright', 'bgRedBright', 'bgGreenBright', 'bgYellowBright', 'bgBlueBright', 'bgMagentaBright', 'bgCyanBright', 'bgWhiteBright'],
+    } as any,
+    styles = [...namespaces.modifier, ...namespaces.color, ...namespaces.bgColor],
+    codePair = ['open', 'close'],
+    codeTypes = ['ansi', 'ansi256', 'ansi16m'],
+    colorFormats = ['ansi', 'rgb', 'hsl', 'hsv', 'hwb', 'cmyk', 'xyz', 'lab', 'lch', 'hex', 'keyword', 'ansi256', 'hcg', 'apple', 'gray'],
     codesMap = 'codes';
 
 
-checkStyle(styles, ansiStyles as any);
-checkStyle(modifier, (ansiStyles as any)['modifier']);
-checkStyle(color, (ansiStyles as any)['color']);
-checkStyle(bgColor, (ansiStyles as any)['bgColor']);
+checkStyle(ansiStyles, styles);
+nsNames.map(ns => checkStyle(ansiStyles[ns], namespaces[ns]));
 
-if (!((ansiStyles as any)[codesMap] instanceof Map)) {
-    throw new Error(`ansiStyles.${codesMap} not a Map.`);
-}
-for (let namespace of namespaces) {
-    if (isUninitialized((ansiStyles as any)[namespace])) {
-        throw new Error('key not found ~> ansiStyles.' + namespace);
-    }
-}
+checkIsMap(ansiStyles[codesMap], `ansiStyles.${codesMap} not a Map.`);
+nsNames.map(ns => checkExist(ansiStyles[ns], `ansiStyles.${ns} is not exist.`));
 
-checkColorFormatConverter(colorFormats, 'color', (ansiStyles as any)['color']);
-checkColorFormatConverter(colorFormats, 'bgColor', (ansiStyles as any)['bgColor']);
+['color', 'bgColor'].map(ns => checkConverter(ns, ansiStyles[ns], colorFormats));
 
-function checkStyle(styleList: string[], namespace: any) {
-    for (let style of styleList) {
-        checkCodePair(style, namespace[style]);
-    }
+
+function checkStyle(namespace: any, styles: string[]) {
+    styles.map(s => checkCodePair(s, namespace[s]));
 }
-function checkCodePair(styleName: string, pair: EscapeCode.CodePair): void {
-    if (isUninitialized(pair.open)) {
-        throw new Error('key not found ~> ' + styleName + '.open');
-    }
-    if (isUninitialized(pair.close)) {
-        throw new Error('key not found ~> ' + styleName + '.close');
-    }
-}
-function checkColorFormatConverter(formatList: string[], name: string, namespace: any) {
-    for (let format of formatList) {
-        for (let type of codeTypes) {
-            if (typeof namespace[type][format] != 'function') {
-                throw new Error(`ansiStyles.${name}.${type}.${format} not a function.`);
-            }
-        }
-    }
-    if (isUninitialized(namespace.close)) {
-        throw new Error(`key not found ~> ansiStyles.${name}.close .`);
-    }
+function checkCodePair(styleName: string, pair: any): void {
+    codePair.map(p => checkExist(pair[p], `${styleName}.${p} is not exist.`));
 }
 
-function isUninitialized(val: any): boolean {
-    return val === null || val === undefined;
+function checkConverter(nsName: string, namespace: any, formats: string[]) {
+    formats.map(f => codeTypes.map(t => checkIsFunction(namespace[t][f], `ansiStyles.${nsName}.${t}.${f} is not a function.`)));
+    checkExist(namespace.close, `${namespace}.close is not exist.`);
+}
+
+function checkExist(val: any, failMsg: string): void {
+    if (val == null) throw new Error(failMsg);
+}
+function checkIsFunction(fn: any, failMsg: string): void {
+    if (typeof fn != 'function') throw new Error(failMsg);
+}
+function checkIsMap(map: any, failMsg: string): void {
+    if (!(map instanceof Map)) throw new Error(failMsg);
 }
