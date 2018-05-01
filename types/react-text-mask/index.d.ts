2ce3458275607d1dc5ffe157146f15ab309523cb
Jeff Lau <jeff.ch.lau@gmail.com>
2018-05-01 16:09:22 +1000

    Corrected error previously checked in.

--- types/react-text-mask/index.d.ts
+++ types/react-text-mask/index.d.ts
@@ -1,14 +1,15 @@
 // Type definitions for react-text-mask 5.1
 // Project: https://github.com/text-mask/text-mask
 // Definitions by: Guilherme Hübner <https://github.com/guilhermehubner>
+//                 Jeff Lau <https://github.com/jeff-lau>
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
 // TypeScript Version: 2.6
 
-import * as React from 'react';
+import * as React from "react";
 
-declare module 'react-text-mask' {
+export type maskArray = Array<string | RegExp>;
 
-  export interface MaskedInputProps extends React.InputHTMLAttributes<HTMLInputElement> {
+export interface MaskedInputProps extends React.InputHTMLAttributes<HTMLInputElement> {
     mask?: maskArray | ((value: string) => maskArray);
 
     guide?: boolean;
@@ -22,6 +23,15 @@ declare module 'react-text-mask' {
     showMask?: boolean;
 
     render: (ref: HTMLInputElement, props: any) => JSX.Element;
-
-  }
 }
+
+export interface conformToMaskResult {
+    conformedValue: string;
+    meta: {
+        someCharsRejected: boolean
+    };
+}
+
+export default class MaskedInput extends React.Component<MaskedInputProps, any> {}
+
+export function conformToMask(text: string, mask: maskArray, config: any): conformToMaskResult;
