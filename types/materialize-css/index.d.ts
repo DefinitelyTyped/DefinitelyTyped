db8e2a145d2a1ebb9e2e84955c1a85f9a7f675ce
Daniel Hoenes <daniel.p.hoenes@monsanto.com>
2018-05-02 07:38:26 -0500

    update contributors per PR#25393

--- types/materialize-css/index.d.ts
+++ types/materialize-css/index.d.ts
@@ -2,6 +2,7 @@
 // Project: http://materializecss.com/
 // Definitions by:  胡玮文 <https://github.com/huww98>
 //                  Maxim Balaganskiy <https://github.com/MaximBalaganskiy>
+//                  David Moniz <https://github.com/MonizDave>
 //                  Daniel Hoenes <https://github.com/broccoliarchy>
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
 // TypeScript Version: 2.8
