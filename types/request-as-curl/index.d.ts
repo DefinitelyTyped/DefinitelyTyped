f0a94a294c2764dedd8dbd87463137837c1e9192
Arne Schubert <arne.schubert@rewe-digital.com>
2018-05-02 11:59:39 +0200

    Fix TypeScript version in comment

--- types/request-as-curl/index.d.ts
+++ types/request-as-curl/index.d.ts
@@ -2,6 +2,7 @@
 // Project: https://github.com/baz/foo (Does not have to be to GitHub, but prefer linking to a source code repository rather than to a project website.)
 // Definitions by: Arne Schubert <https://github.com/atd-schubert>
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
+// TypeScript Version: 2.3
 
 import { CoreOptions, Request, RequestAPI, RequiredUriUrl } from "request";
 
