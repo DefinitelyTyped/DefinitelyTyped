9e95fb13a5444edc92b320b81c6ab00d28cb5fb2
Jinesh Shah <jineshshah36@gmail.com>
2018-05-03 11:21:01 -0400

    splitting pull requests by package

--- types/feather-icons/feather-icons-tests.ts
+++
@@ -1,7 +0,0 @@
-import * as feather from "feather-icons";
-
-feather.replace(); // $ExpectType void
-feather.icons[""].toSvg(); // $ExpectType string
-feather.icons[""].name; // $ExpectType string
-feather.icons[""].contents; // $ExpectType string
-feather.icons[""].tags; // $ExpectType string[]
