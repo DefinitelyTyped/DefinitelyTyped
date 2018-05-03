5a5b56dd18f80ade7f7f571d9ba4020c34767173
Airex <airex.s@gmail.com>
2018-05-03 14:57:06 +0300

    Update index.d.ts
    Added new SubGroupStackOptions dynamic structure and updated type of DataGroup.subgroupStack to SubGroupStackOptions | boolean

--- types/vis/index.d.ts
+++ types/vis/index.d.ts
@@ -88,6 +88,10 @@ export interface PointItem extends DataItem {
   y: number;
 }
 
+export interface SubGroupStackOptions{
+    [name:string]: boolean;
+}
+
 export interface DataGroup {
   className?: string;
   content: string;
@@ -97,7 +101,7 @@ export interface DataGroup {
   subgroupOrder?: string | (() => void);
   title?: string;
   nestedGroups?: number[];
-  subgroupStack?: object | boolean;
+  subgroupStack?: SubGroupStackOptions | boolean;
 }
 
 export interface DataGroupOptions {
