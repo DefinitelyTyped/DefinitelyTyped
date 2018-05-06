160675221ae7b48b2d04f88abc2c76f0476a7a2a
Seamus Leahy <seamus@votion.co>
2018-05-06 15:48:30 -0700

    Increase version number

--- types/gm/index.d.ts
+++ types/gm/index.d.ts
@@ -1,4 +1,4 @@
-// Type definitions for gm 1.17
+// Type definitions for gm 1.18
 // Project: https://github.com/aheckmann/gm
 // Definitions by: Joel Spadin <https://github.com/ChaosinaCan>, Maarten van Vliet <https://github.com/maartenvanvliet>
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
