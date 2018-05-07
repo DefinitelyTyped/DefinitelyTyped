44cb792e09cf49ce733e7e4f84218e27147ea694
Andy Hanson <anhans@microsoft.com>
2018-05-07 09:10:08 -0700

    Remove unnecessary

--- types/freshy/freshy-tests.ts
+++ types/freshy/freshy-tests.ts
@@ -6,22 +6,16 @@ declare function require(x: string): any;
 unload('minimist'); // $ExpectType boolean
 
 const reloaded = reload('minimist'); // $ExpectType any
-// $ExpectType boolean
 minimist === reloaded; // false
 
 const freshlyLoaded = freshy('minimist'); // $ExpectType any
-// $ExpectType boolean
 minimist === freshlyLoaded; // false
 
 let alsofresh: any;
 // $ExpectType any
 const fresh = freshy('minimist', (fresh) => {
     alsofresh = require('minimist');
-
-    // $ExpectType boolean
     fresh === alsofresh; // true
 });
-// $ExpectType boolean
 minimist === fresh; // false
-// $ExpectType boolean
 fresh === alsofresh; // true
