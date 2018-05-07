dc9dcfa5bbf287a4987fa7bfd5e147d5034141d8
Tim Stirrat <tstirrat@google.com>
2018-05-08 05:22:48 +1000

    bump TS version to match mousetrap

--- types/redux-shortcuts/index.d.ts
+++ types/redux-shortcuts/index.d.ts
@@ -2,7 +2,7 @@
 // Project: https://github.com/nak2k/node-redux-shortcuts
 // Definitions by: Tim Stirrat <https://github.com/tstirrat>
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
-// TypeScript Version: 2.1
+// TypeScript Version: 2.3
 
 import { ActionCreator, Action, Dispatch } from "redux";
 import Mousetrap = require('mousetrap');
