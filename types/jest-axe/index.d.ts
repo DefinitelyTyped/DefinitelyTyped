05cfefa3212603f14f232df7c3e459de1064f934
Joshua Goldberg <jogol@microsoft.com>
2018-05-08 12:43:33 -0700

    Fixed up some things

--- types/jest-axe/index.d.ts
+++ types/jest-axe/index.d.ts
@@ -1,8 +1,7 @@
-// Type definitions for expect-puppeteer 2.2
-// Project: https://github.com/smooth-code/jest-puppeteer/tree/master/packages/expect-puppeteer
+// Type definitions for jest-axe 2.2
+// Project: https://github.com/nickcolley/jest-axe
 // Definitions by: Josh Goldberg <https://github.com/JoshuaKGoldberg>
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
-// TypeScript Version: 2.4
 
 /// <reference types="jest" />
 
