e54e1ba90ece9c45226be130b85030bc7d00eb5a
leanazulyoro <leanazulyoro@gmail.com>
2018-05-08 11:36:44 -0300

    Fix: return type for restify's conditionaHandler; Added: test for restify's conditionalHandler

--- types/restify/index.d.ts
+++ types/restify/index.d.ts
@@ -955,7 +955,7 @@ export namespace plugins {
     /**
      * Runs first handler that matches to the condition
      */
-    function conditionalHandler(candidates: HandlerCandidate | HandlerCandidate[]): RequestHandler[];
+    function conditionalHandler(candidates: HandlerCandidate | HandlerCandidate[]): RequestHandler;
 
     /**
      * Conditional headers (If-*)
