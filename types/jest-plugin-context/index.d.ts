44e7ee920c0890170d6bdddc6d43f23670593c8f
Aneil Mallavarapu <aneilmallavar@gmail.com>
2018-05-09 06:37:37 -0700

    Add definition for jest-plugin-context

---
+++ types/jest-plugin-context/index.d.ts
@@ -0,0 +1,12 @@
+// Type definitions for jest-plugin-context 2.9
+// Project: https://github.com/negativetwelve/jest-plugins/tree/master/packages/jest-plugin-context
+// Definitions by: Aneil Mallavarapu <https://github.com/aneilbaboo>
+// Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
+// TypeScript Version: 2.3
+
+/// <reference types="jest" />
+
+declare module 'jest-plugin-context' {
+  const context: jest.Describe;
+  export default context;
+}
