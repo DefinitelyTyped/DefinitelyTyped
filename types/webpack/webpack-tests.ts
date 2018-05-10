DefinitelyTyped/DefinitelyTyped

commit 37a8e994f89b1460bea3d4712a60e5c2aaebdfb6

webpack: Updated module rules types against WebpackOptions.json schema in v4 (#25566)
* Updated module rules types against WebpackOptions.json schema in v4

* Fixed T[] lintting error

* Removed redundant definition in RuleSetCondition

* Fixed Loader reference in extract-text-webpack-plugin

types/webpack/index.d.ts
@@ -205,9 +205,9 @@ declare namespace webpack {
 
     interface Module {
         /** A array of applied pre loaders. */
-        preLoaders?: Rule[];
+        preLoaders?: RuleSetRule[];
         /** A array of applied post loaders. */
-        postLoaders?: Rule[];
+        postLoaders?: RuleSetRule[];
         /** A RegExp or an array of RegExps. Don’t parse files matching. */
         noParse?: RegExp | RegExp[] | ((content: string) => boolean);
         unknownContextRequest?: string;
@@ -223,7 +223,7 @@ declare namespace webpack {
         wrappedContextCritical?: boolean;
         strictExportPresence?: boolean;
         /** An array of rules applied for modules. */
-        rules: Rule[];
+        rules: RuleSetRule[];
     }
 
     interface Resolve {
@@ -357,143 +357,187 @@ declare namespace webpack {
         __dirname?: boolean | string;
         [nodeBuiltin: string]: boolean | string | undefined;
     }
-
-    interface BaseConditionSpec {
-        /** The Condition must match. The convention is the provide a string or array of strings here, but it's not enforced. */
-        include?: Condition;
-        /** The Condition must NOT match. The convention is the provide a string or array of strings here, but it's not enforced. */
-        exclude?: Condition;
-    }
-    interface TestConditionSpec extends BaseConditionSpec {
-        /** The Condition must match. The convention is the provide a RegExp or array of RegExps here, but it's not enforced. */
-        test: Condition;
-    }
-    interface AndConditionSpec extends BaseConditionSpec {
-        /** All Conditions must match. */
-        and: Condition[];
-    }
-    interface OrConditionSpec extends BaseConditionSpec {
-        /** Any Condition must match. */
-        or: Condition[];
-    }
-    interface NotConditionSpec extends BaseConditionSpec {
-        /** The Condition must NOT match. */
-        not: Condition;
-    }
-    type ConditionSpec = TestConditionSpec | OrConditionSpec | AndConditionSpec | NotConditionSpec;
-
-    // tslint:disable-next-line:no-empty-interface
-    interface ConditionArray extends Array<Condition> { }
-    type Condition = string | RegExp | ((absPath: string) => boolean) | ConditionSpec | ConditionArray;
-
-    interface OldLoader {
-        loader: string;
-        query?: { [name: string]: any };
-    }
     interface NewLoader {
         loader: string;
         options?: { [name: string]: any };
     }
-    type Loader = string | OldLoader | NewLoader;
+    type Loader = string | NewLoader;
+
     interface ParserOptions {
         [optName: string]: any;
         system?: boolean;
     }
-    /**
-     * There are direct and delegate rules. Direct Rules need a test, Delegate rules delegate to subrules bringing their own.
-     * Direct rules can optionally contain delegate keys (oneOf, rules).
-     *
-     * These types exist to enforce that a rule has the keys `((loader XOR loaders) AND test) OR oneOf OR rules`
-     */
-    interface BaseRule {
-        /**
-         * Specifies the category of the loader. No value means normal loader.
-         *
-         * There is also an additional category "inlined loader" which are loaders applied inline of the import/require.
-         *
-         * All loaders are sorted in the order post, inline, normal, pre and used in this order.
-         *
-         * All normal loaders can be omitted (overridden) by prefixing ! in the request.
-         *
-         * All normal and pre loaders can be omitted (overridden) by prefixing -! in the request.
-         *
-         * All normal, post and pre loaders can be omitted (overridden) by prefixing !! in the request.
-         *
-         * Inline loaders and ! prefixes should not be used as they are non-standard. They may be use by loader generated code.
-         */
-        enforce?: 'pre' | 'post';
-        /** A condition that must be met */
-        test?: Condition | Condition[];
-        /** A condition that must not be met */
-        exclude?: Condition | Condition[];
-        /** A condition that must be met */
-        include?: Condition | Condition[];
-        /** A Condition matched with the resource. */
-        resource?: Condition | Condition[];
-        /** A Condition matched with the resource query. */
-        resourceQuery?: Condition | Condition[];
-        /** A condition matched with the issuer */
-        issuer?: Condition | Condition[];
-        /**
-         * An object with parser options. All applied parser options are merged.
-         *
-         * For each different parser options object a new parser is created and plugins can apply plugins depending on the parser options.
-         * Many of the default plugins apply their parser plugins only if a property in the parser options is not set or true.
-         */
-        parser?: ParserOptions;
-        /** An array of Rules that is also used when the Rule matches. */
-        rules?: Rule[];
-        /** An array of Rules from which only the first matching Rule is used when the Rule matches. */
-        oneOf?: Rule[];
-        /** Configures module resolving. */
-        resolve?: Resolve;
-        /** Configures the module type. */
-        type?: "javascript/auto" | "javascript/esm" | "javascript/dynamic" | "json" | "webassembly/experimental";
-        /** Flags a module as with or without side effects */
-        sideEffects?: boolean;
-    }
-    interface BaseDirectRule extends BaseRule {
-        /** A condition that must be met */
-        test: Condition | Condition[];
-    }
-    // Direct Rules
-    interface BaseSingleLoaderRule extends BaseDirectRule {
-        /** Loader name or an object with name and options */
-        loader: Loader;
-    }
-    interface OldLoaderRule extends BaseSingleLoaderRule {
-        /**
-         * Loader options
-         * @deprecated:
-         */
-        query?: { [name: string]: any };
-    }
-    interface NewLoaderRule extends BaseSingleLoaderRule {
-        options?: { [name: string]: any };
-    }
-    type LoaderRule = OldLoaderRule | NewLoaderRule;
-    interface OldUseRule extends BaseDirectRule {
-        /**
-         * A array of loaders.
-         * @deprecated  use `use` instead
-         */
-        loaders: string[];
-    }
-    interface NewUseRule extends BaseDirectRule {
-        /** A loader or array of loaders */
-        use: Loader | Loader[];
-    }
-    type UseRule = OldUseRule | NewUseRule;
 
-    // Delegate Rules
-    interface RulesRule extends BaseRule {
-        /** An array of Rules that is also used when the Rule matches. */
-        rules: Rule[];
+    type RuleSetCondition = string
+        | {
+            [k: string]: any;
+        }
+        | {
+            /**
+             * Logical AND
+             */
+            and?: RuleSetConditions;
+            /**
+             * Exclude all modules matching any of these conditions
+             */
+            exclude?: RuleSetCondition;
+            /**
+             * Exclude all modules matching not any of these conditions
+             */
+            include?: RuleSetCondition;
+            /**
+             * Logical NOT
+             */
+            not?: RuleSetConditions;
+            /**
+             * Logical OR
+             */
+            or?: RuleSetConditions;
+            /**
+             * Exclude all modules matching any of these conditions
+             */
+            test?: RuleSetCondition;
+        };
+    type RuleSetConditions = RuleSetCondition[];
+
+    interface RuleSetRule {
+        /**
+         * Enforce this rule as pre or post step
+         */
+        enforce?: "pre" | "post";
+        /**
+         * Shortcut for resource.exclude
+         */
+        exclude?: RuleSetCondition & {
+            [k: string]: any;
+        };
+        /**
+         * Shortcut for resource.include
+         */
+        include?: RuleSetCondition & {
+            [k: string]: any;
+        };
+        /**
+         * Match the issuer of the module (The module pointing to this module)
+         */
+        issuer?: RuleSetCondition & {
+            [k: string]: any;
+        };
+        /**
+         * Shortcut for use.loader
+         */
+        loader?: RuleSetLoader | RuleSetUse;
+        /**
+         * Shortcut for use.loader
+         */
+        loaders?: RuleSetUse;
+        /**
+         * Only execute the first matching rule in this array
+         */
+        oneOf?: RuleSetRules;
+        /**
+         * Shortcut for use.options
+         */
+        options?: RuleSetQuery;
+        /**
+         * Options for parsing
+         */
+        parser?: {
+            [k: string]: any;
+        };
+        /**
+         * Options for the resolver
+         */
+        resolve?: Resolve;
+        /**
+         * Flags a module as with or without side effects
+         */
+        sideEffects?: boolean;
+        /**
+         * Shortcut for use.query
+         */
+        query?: RuleSetQuery;
+        /**
+         * Module type to use for the module
+         */
+        type?: "javascript/auto" | "javascript/dynamic" | "javascript/esm" | "json" | "webassembly/experimental";
+        /**
+         * Match the resource path of the module
+         */
+        resource?: RuleSetCondition & {
+            [k: string]: any;
+        };
+        /**
+         * Match the resource query of the module
+         */
+        resourceQuery?: RuleSetCondition;
+        /**
+         * Match the child compiler name
+         */
+        compiler?: RuleSetCondition;
+        /**
+         * Match and execute these rules when this rule is matched
+         */
+        rules?: RuleSetRules;
+        /**
+         * Shortcut for resource.test
+         */
+        test?: RuleSetCondition & {
+            [k: string]: any;
+        };
+        /**
+         * Modifiers applied to the module when rule is matched
+         */
+        use?: RuleSetUse;
     }
-    interface OneOfRule extends BaseRule {
-        oneOf: Rule[];
-    }
-    type Rule = LoaderRule | UseRule | RulesRule | OneOfRule;
+    type RuleSetLoader = string;
+    type RuleSetUse =
+        | RuleSetUseItem
+        | {
+            [k: string]: any;
+        }
+        | RuleSetUseItem[];
+    type RuleSetUseItem =
+        | RuleSetLoader
+        | {
+            [k: string]: any;
+        }
+        | {
+            /**
+             * Loader name
+             */
+            loader?: RuleSetLoader;
+            /**
+             * Loader options
+             */
+            options?: RuleSetQuery;
+            /**
+             * Unique loader identifier
+             */
+            ident?: string;
+            /**
+             * Loader query
+             */
+            query?: RuleSetQuery;
+        };
+    type RuleSetQuery =
+        | {
+            [k: string]: any;
+        }
+        | string;
+    type CommonArrayOfStringOrStringArrayValues = Array<(string | string[])>;
+    type CommonArrayOfStringValues = string[];
+    type RuleSetRules = RuleSetRule[];
+
+    /**
+     * @deprecated Use RuleSetCondition instead
+     */
+    type Condition = RuleSetCondition;
+
+    /**
+     * @deprecated Use RuleSetRule instead
+     */
+    type Rule = RuleSetRule;
 
     namespace Options {
         // tslint:disable-next-line:max-line-length

types/webpack/webpack-tests.ts
@@ -684,3 +684,17 @@ class BannerPlugin extends webpack.Plugin {
         });
     }
 }
+
+configuration = {
+    module: {
+        rules: [
+            {
+                test: /\.css$/,
+                oneOf: [
+                    { resourceQuery: /global/, use: ["style-loader", "css-loader"] },
+                    { use: ["to-string-loader", "css-loader"] }
+                ]
+            }
+        ]
+    }
+};

types/extract-text-webpack-plugin/index.d.ts
@@ -4,15 +4,9 @@
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
 // TypeScript Version: 2.3
 
-import { Plugin, NewLoader, OldLoader } from 'webpack';
+import { Plugin, Loader } from 'webpack';
 
 export = ExtractTextPlugin;
-
-/**
- * extract-text-webpack-plugin has no support for .options instead of .query yet.
- * See https://github.com/webpack/extract-text-webpack-plugin/issues/281
- */
-type Loader = string | OldLoader | NewLoader;
 
 /**
  * Use an `ExtractTextPlugin` instance and a loader returned by `extract` in concert to write files to disk instead of loading them into others.

commit 375495a2e41a1d20bd5ac3af1cee698fccf756bd

webpack: Fix some problems introduced in #25566

types/webpack/index.d.ts
@@ -253,7 +253,7 @@ declare namespace webpack {
          * Defaults to `["browser", "module", "main"]` or `["module", "main"]`,
          * depending on the value of the `target` `Configuration` value.
          */
-        mainFields?: string[];
+        mainFields?: string[] | string[][];
 
         /**
          * A list of fields in a package description object to try to parse
@@ -264,7 +264,7 @@ declare namespace webpack {
          *
          * @see alias
          */
-        aliasFields?: string[];
+        aliasFields?: string[] | string[][];
 
         /**
          * A list of file names to search for when requiring directories that
@@ -368,15 +368,16 @@ declare namespace webpack {
         system?: boolean;
     }
 
-    type RuleSetCondition = string
-        | {
-            [k: string]: any;
-        }
+    type RuleSetCondition =
+        | RegExp
+        | string
+        | ((path: string) => boolean)
+        | RuleSetConditions
         | {
             /**
              * Logical AND
              */
-            and?: RuleSetConditions;
+            and?: RuleSetCondition[];
             /**
              * Exclude all modules matching any of these conditions
              */
@@ -388,17 +389,19 @@ declare namespace webpack {
             /**
              * Logical NOT
              */
-            not?: RuleSetConditions;
+            not?: RuleSetCondition[];
             /**
              * Logical OR
              */
-            or?: RuleSetConditions;
+            or?: RuleSetCondition[];
             /**
              * Exclude all modules matching any of these conditions
              */
             test?: RuleSetCondition;
         };
-    type RuleSetConditions = RuleSetCondition[];
+
+    // A hack around circular type referencing
+    interface RuleSetConditions extends Array<RuleSetCondition> {}
 
     interface RuleSetRule {
         /**
@@ -408,25 +411,19 @@ declare namespace webpack {
         /**
          * Shortcut for resource.exclude
          */
-        exclude?: RuleSetCondition & {
-            [k: string]: any;
-        };
+        exclude?: RuleSetCondition;
         /**
          * Shortcut for resource.include
          */
-        include?: RuleSetCondition & {
-            [k: string]: any;
-        };
+        include?: RuleSetCondition;
         /**
          * Match the issuer of the module (The module pointing to this module)
          */
-        issuer?: RuleSetCondition & {
-            [k: string]: any;
-        };
+        issuer?: RuleSetCondition;
         /**
          * Shortcut for use.loader
          */
-        loader?: RuleSetLoader | RuleSetUse;
+        loader?: RuleSetUse;
         /**
          * Shortcut for use.loader
          */
@@ -434,7 +431,7 @@ declare namespace webpack {
         /**
          * Only execute the first matching rule in this array
          */
-        oneOf?: RuleSetRules;
+        oneOf?: RuleSetRule[];
         /**
          * Shortcut for use.options
          */
@@ -442,9 +439,7 @@ declare namespace webpack {
         /**
          * Options for parsing
          */
-        parser?: {
-            [k: string]: any;
-        };
+        parser?: { [k: string]: any };
         /**
          * Options for the resolver
          */
@@ -464,9 +459,7 @@ declare namespace webpack {
         /**
          * Match the resource path of the module
          */
-        resource?: RuleSetCondition & {
-            [k: string]: any;
-        };
+        resource?: RuleSetCondition;
         /**
          * Match the resource query of the module
          */
@@ -478,56 +471,49 @@ declare namespace webpack {
         /**
          * Match and execute these rules when this rule is matched
          */
-        rules?: RuleSetRules;
+        rules?: RuleSetRule[];
         /**
          * Shortcut for resource.test
          */
-        test?: RuleSetCondition & {
-            [k: string]: any;
-        };
+        test?: RuleSetCondition;
         /**
          * Modifiers applied to the module when rule is matched
          */
         use?: RuleSetUse;
     }
-    type RuleSetLoader = string;
+
     type RuleSetUse =
         | RuleSetUseItem
-        | {
-            [k: string]: any;
-        }
-        | RuleSetUseItem[];
+        | RuleSetUseItem[]
+        | ((data: any) => RuleSetUseItem | RuleSetUseItem[]);
+
+    interface RuleSetLoader {
+        /**
+         * Loader name
+         */
+        loader?: string;
+        /**
+         * Loader options
+         */
+        options?: RuleSetQuery;
+        /**
+         * Unique loader identifier
+         */
+        ident?: string;
+        /**
+         * Loader query
+         */
+        query?: RuleSetQuery;
+    }
+
     type RuleSetUseItem =
+        | string
         | RuleSetLoader
-        | {
-            [k: string]: any;
-        }
-        | {
-            /**
-             * Loader name
-             */
-            loader?: RuleSetLoader;
-            /**
-             * Loader options
-             */
-            options?: RuleSetQuery;
-            /**
-             * Unique loader identifier
-             */
-            ident?: string;
-            /**
-             * Loader query
-             */
-            query?: RuleSetQuery;
-        };
+        | ((data: any) => string | RuleSetLoader);
+
     type RuleSetQuery =
-        | {
-            [k: string]: any;
-        }
-        | string;
-    type CommonArrayOfStringOrStringArrayValues = Array<(string | string[])>;
-    type CommonArrayOfStringValues = string[];
-    type RuleSetRules = RuleSetRule[];
+        | string
+        | { [k: string]: any };
 
     /**
      * @deprecated Use RuleSetCondition instead

types/webpack/webpack-tests.ts
@@ -698,3 +698,37 @@ configuration = {
         ]
     }
 };
+
+configuration = {
+    module: {
+        rules: [
+            {
+                test: /\.ts$/,
+                include: '/foo/bar',
+                exclude: path => path.startsWith('/foo'),
+                resourceQuery: ['foo', 'bar'],
+                resolve: {
+                    mainFields: ['foo'],
+                    aliasFields: [['bar']],
+                },
+                loader: 'foo-loader',
+                loaders: [
+                    'foo-loader',
+                    {
+                        loader: 'bar-loader',
+                        query: 'baz'
+                    }
+                ],
+                use: () => ([
+                    'foo-loader',
+                    {
+                        loader: 'bar-loader',
+                        query: {
+                            baz: 'qux'
+                        }
+                    },
+                ])
+            }
+        ]
+    }
+};