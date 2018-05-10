52342a2bad0e9ea5e35196266899f98c5c680c63
denis <denisname@users.noreply.github.com>
2018-05-10 17:32:57 +0200

    Fix TS version

--- types/select2/index.d.ts
+++ types/select2/index.d.ts
@@ -3,7 +3,7 @@
 // Definitions by: Boris Yankov <https://github.com/borisyankov>
 //                 denisname <https://github.com/denisname>
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
-// TypeScript Version: 2.3
+// TypeScript Version: 2.6
 
 /// <reference types="jquery"/>
 /// <reference types="requirejs"/>
