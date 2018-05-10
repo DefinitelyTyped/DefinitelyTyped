b42236e28d99e59f14340e276539b0289fcc4b87
Sam Walsh <web@samwalsh.co.nz>
2018-05-10 16:16:52 +1200

    Add samwalshnz as contributor to react-content-loader

--- types/react-content-loader/index.d.ts
+++ types/react-content-loader/index.d.ts
@@ -1,6 +1,7 @@
 // Type definitions for react-content-loader 3.1
 // Project: https://github.com/danilowoz/react-content-loader
 // Definitions by: Alaa Masoud <https://github.com/alaatm>
+//                 Sam Walsh <https://github.com/samwalshnz>
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
 // TypeScript Version: 2.6
 
