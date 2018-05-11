3c5384acca044e06c4e214cf5b113b139a95cb7c
Resi Respati <resir014@gmail.com>
2018-05-11 23:12:07 +0700

    [next] Added AppComponentContext type

--- types/next/app.d.ts
+++ types/next/app.d.ts
@@ -1,8 +1,15 @@
 import * as React from "react";
+import { NextContext } from "next";
 
 export interface AppComponentProps {
     Component: React.ComponentType<any>;
     pageProps: any
+}
+
+export interface AppComponentContext {
+    Component: any;
+    router: any; // TODO: could be SingletonRouter?
+    ctx: NextContext;
 }
 
 export class Container extends React.Component {}
