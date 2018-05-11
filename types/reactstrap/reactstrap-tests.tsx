ccbd12e286f14d5e55d37b3bfcd5cc4270a1ed40
Flosch <florent.schildknecht@humblebee.se>
2018-05-11 18:08:10 +0200

    [reactstrap] Add borderless type to Table

--- types/reactstrap/reactstrap-tests.tsx
+++ types/reactstrap/reactstrap-tests.tsx
@@ -4053,7 +4053,7 @@ function GenericPropExample() {
   );
 }
 
-class Example118 extends React.Component {
+class Example119 extends React.Component<any, any> {
   render() {
     return (
       <Form>
@@ -4137,6 +4137,43 @@ class Example118 extends React.Component {
           <CustomInput type="file" id="exampleCustomFileBrowser" name="customFile" disabled />
         </FormGroup>
       </Form>
+    );
+  }
+}
+
+class Example120 extends React.Component<any, any> {
+  render() {
+    return (
+      <Table borderless>
+        <thead>
+          <tr>
+            <th>#</th>
+            <th>First Name</th>
+            <th>Last Name</th>
+            <th>Username</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <th scope="row">1</th>
+            <td>Mark</td>
+            <td>Otto</td>
+            <td>@mdo</td>
+          </tr>
+          <tr>
+            <th scope="row">2</th>
+            <td>Jacob</td>
+            <td>Thornton</td>
+            <td>@fat</td>
+          </tr>
+          <tr>
+            <th scope="row">3</th>
+            <td>Larry</td>
+            <td>the Bird</td>
+            <td>@twitter</td>
+          </tr>
+        </tbody>
+      </Table>
     );
   }
 }
