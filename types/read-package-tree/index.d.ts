fb0c888f4ed49e531e3ec5f2f50b5936e50ba1d4
Ivan Akulov <gxoptg@gmail.com>
2018-05-12 23:12:13 +0300

    Bump the package version

--- types/read-package-tree/index.d.ts
+++ types/read-package-tree/index.d.ts
@@ -1,4 +1,4 @@
-// Type definitions for read-package-tree 5.1
+// Type definitions for read-package-tree 5.2
 // Project: https://github.com/npm/read-package-tree
 // Definitions by: Melvin Groenhoff <https://github.com/mgroenhoff>
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
