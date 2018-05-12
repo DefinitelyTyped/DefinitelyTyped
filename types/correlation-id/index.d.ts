564b5fbf63850c345d1fdcd8895103dd944e7054
Nate Mara <nate.mara@kroger.com>
2018-05-12 15:48:45 -0400

    fix(correlation-id) Update type of bindID function

--- types/correlation-id/index.d.ts
+++ types/correlation-id/index.d.ts
@@ -6,7 +6,7 @@
 export function withId(id: string, work: () => void): void;
 export function withId(work: () => void): void;
 
-export function bindId<T extends Function>(id: string, work: T): T;
-export function bindId<T extends Function>(work: T): T;
+export function bindId<T extends (...p: any[]) => any>(id: string, work: T): T;
+export function bindId<T extends (...p: any[]) => any>(work: T): T;
 
 export function getId(): string | undefined;
