c0a2b65fd6dd3da8ec209b48f09db987e6ab1ef7
Arnav Gupta <arnav@codingblocks.com>
2018-05-12 16:03:57 +0530

    tslint fixes
    Signed-off-by: Arnav Gupta <arnav@codingblocks.com>

--- types/discourse-sso/index.d.ts
+++ types/discourse-sso/index.d.ts
@@ -3,28 +3,25 @@
 // Definitions by: Arnav Gupta <https://github.com/championswimmer>
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
 
-
-
-
 declare class DiscourseSSO {
-    constructor(ssoSecret: string)
-    validate(payload: string, sig: string): boolean
-    getNonce(payload: string): string
-    buildLoginString(params: DiscourseSSO.UserParams): string
+    constructor(ssoSecret: string);
+    validate(payload: string, sig: string): boolean;
+    getNonce(payload: string): string;
+    buildLoginString(params: DiscourseSSO.UserParams): string;
 }
+
 declare namespace DiscourseSSO {
-    
     export interface UserParams {
-        nonce: string,
-        external_id: string,
-        email: string,
-        admin?: boolean,
-        moderator?: boolean,
-        username?: string,
-        name?: string,
-        avatar_url?: string,
-        add_groups?: string[],
-        remove_groups?: string[]
+        nonce: string;
+        external_id: string;
+        email: string;
+        admin?: boolean;
+        moderator?: boolean;
+        username?: string;
+        name?: string;
+        avatar_url?: string;
+        add_groups?: string[];
+        remove_groups?: string[];
     }
 }
-export = DiscourseSSO
+export = DiscourseSSO;
