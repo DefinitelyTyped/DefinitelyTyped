3949ddb99430758144735b84ea36a127eaab2b55
Vince Maiuri <vince.maiuri@directsupply.com>
2018-05-14 08:25:25 -0500

    test updates

--- types/react-native-sortable-list/index.d.ts
+++ types/react-native-sortable-list/index.d.ts
@@ -1,6 +1,6 @@
-// Type definitions for react-native-sortable-list
+// Type definitions for react-native-sortable-list 0.0.22
 // Project: https://github.com/gitim/react-native-sortable-list
-// Definitions by: Michael Sivolobov <https://github.com/sivolobov>,Vince Maiuri <https://github.com/RookY2K>
+// Definitions by: Michael Sivolobov <https://github.com/sivolobov>, Vince Maiuri <https://github.com/RookY2K>
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
 // TypeScript Version: 2.6
 
