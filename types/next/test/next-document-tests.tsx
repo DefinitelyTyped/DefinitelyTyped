9a235d9000e64ec73b397698a909cf6c09b01523
Ben Saufley <bensaufley+git@gmail.com>
2018-05-14 16:05:53 -0400

    [next] Elaborate on Document types
    Adjust renderPage and Enhancer; add tests
    
    Allows Enhancer to receive a React ComponentType that accepts one set of props and return a ComponentType that accepts a different set of props, the latter including at least url

--- types/next/test/next-document-tests.tsx
+++ types/next/test/next-document-tests.tsx
@@ -1,7 +1,7 @@
-import Document, { Head, Main, NextScript, NextDocumentContext } from 'next/document';
+import Document, { DocumentProps, Enhancer, Head, Main, NextScript, NextDocumentContext, PageProps } from 'next/document';
 import * as React from "react";
 
-const results = (
+const basicResults = (
     <Document any="property" should="work" here>
         <Head some="more" properties>
             <meta name="description" content="Head can have children, too!" />
@@ -38,3 +38,66 @@ export default class MyDocument extends Document {
         );
     }
 }
+
+class MyDoc extends Document {
+    static async getInitialProps({ renderPage }: NextDocumentContext) {
+        const enhancer: Enhancer<PageProps, {}> = (App) => (props) => (<App />);
+        const { html, head, errorHtml, chunks, buildManifest } = renderPage(enhancer);
+
+        const now = Date.now();
+
+        return { html, head, errorHtml, chunks, buildManifest, now };
+    }
+
+    render() {
+        return (
+            <html>
+                <Head>
+                    <title>My page</title>
+                    rendered at {this.props.now}
+                </Head>
+                <body>
+                    <Main />
+                    <NextScript />
+                    {this.props.children}
+                </body>
+            </html>
+        );
+    }
+}
+
+const extendedResults = (
+    <MyDoc any="property" should="work" here>
+        <Head some="more" properties>
+            <meta name="description" content="Head can have children, too!" />
+        </Head>
+        <h1>Hey there</h1>
+    </MyDoc>
+);
+
+const renderPage: NextDocumentContext['renderPage'] = (enhancer) => ({
+    buildManifest: {},
+    chunks: { names: [], filenames: [] },
+    html: '',
+    head: [<React.Fragment />],
+    errorHtml: '',
+});
+
+interface PageInitialProps extends PageProps {
+    foo: string;
+    bar: number;
+}
+
+interface ProcessedInitialProps {
+    fooLength: number;
+    bar: boolean;
+}
+
+const enhancerExplicit: Enhancer<PageProps, {}> = (App) => (props) => (<App />);
+const enhancerInferred = (App: React.ComponentType<ProcessedInitialProps>) => ({ foo, bar }: PageInitialProps) => (<App fooLength={foo.length} bar={!!bar} />);
+const explicitEnhancerRenderResponse = renderPage(enhancerExplicit);
+const inferredEnhancerRenderResponse = renderPage(enhancerInferred);
+const defaultedTypesRenderResponse = renderPage((App) => (props) => (<App url={props.url} />));
+const defaultedTypesExtendedRenderResponse = renderPage((App) => (props) => (<App foo="bar" url={props.url} />));
+const explicitTypesRenderResponseOne = renderPage<PageProps, {}>((App) => (props) => (<App />));
+const explicitTypesRenderResponseTwo = renderPage<PageInitialProps, ProcessedInitialProps>((App) => ({ foo, bar }) => (<App fooLength={foo.length} bar={!!bar} />));
