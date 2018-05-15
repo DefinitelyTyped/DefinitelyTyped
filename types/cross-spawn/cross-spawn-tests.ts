07a04c11d3e439691c50341948463a351436b326
khai96_ <hvksmr1996@gmail.com>
2018-05-15 21:04:50 +0700

    Fix cross-spawn-tests.ts

--- types/cross-spawn/cross-spawn-tests.ts
+++ types/cross-spawn/cross-spawn-tests.ts
@@ -10,42 +10,21 @@ pw.on('error', (err: Error) => {
 
 function testBuffer() {
     const res = spawn.sync('foo');
-
-    if (res.error) {
-        const r1: Error = res.error;
-        const r2: null = res.output;
-        const r3: null = res.stdout;
-        const r4: null = res.stderr;
-        const r5: null = res.status;
-        const r6: null = res.signal;
-    } else {
-        const r1: null = res.error;
-        const r2: [Buffer | null, Buffer | null, Buffer | null] = res.output;
-        const r3: Buffer | null = res.stdout;
-        const r4: Buffer | null = res.stderr;
-        const r5: number = res.status;
-        const r6: string | null = res.signal;
-    }
+    const r1: Error | null = res.error;
+    const r2: [Buffer | null, Buffer | null, Buffer | null] | null = res.output;
+    const r3: Buffer | null = res.stdout;
+    const r4: Buffer | null = res.stderr;
+    const r5: number | null = res.status;
+    const r6: string | null = res.signal;
 }
 
 function testString() {
     const res = spawn.sync('foo', {encoding: 'utf8'});
-
-    if (res.error) {
-        const r1: Error = res.error;
-        const r2: null = res.output;
-        const r3: null = res.stdout;
-        const r4: null = res.stderr;
-        const r5: null = res.status;
-        const r6: null = res.signal;
-    } else {
-        const r1: null = res.error;
-        const r2: [string | null, string | null, string | null] = res.output;
-        const r3: string | null = res.stdout;
-        const r4: string | null = res.stderr;
-        const r5: number = res.status;
-        const r6: string | null = res.signal;
-    }
+    const r1: Error | null = res.error;
+    const r2: [string | null, string | null, string | null] | null = res.output; const r3: string | null = res.stdout;
+    const r4: string | null = res.stderr;
+    const r5: number | null = res.status;
+    const r6: string | null = res.signal;
 }
 
 spawn.sync('foo', ['bar']);
