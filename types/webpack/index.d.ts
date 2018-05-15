83e18d70e6d045eb7a44eecd611fae631d29e022
Machisté Quintana <machiste@slack-corp.com>
2018-05-15 14:04:28 -0700

    Bump webpack typings to v4.4.x

--- types/webpack/index.d.ts
+++ types/webpack/index.d.ts
@@ -1,4 +1,4 @@
-// Type definitions for webpack 4.1
+// Type definitions for webpack 4.4
 // Project: https://github.com/webpack/webpack
 // Definitions by: Qubo <https://github.com/tkqubo>
 //                 Benjamin Lim <https://github.com/bumbleblym>
