0d02c685c52f6042f9189d9c498f7f551a0a6711
Edward Sammut Alessi <Slessi@users.noreply.github.com>
2018-05-16 11:15:57 +0200

    Update index.d.ts
    Update maintainer name

--- types/intl-locales-supported/index.d.ts
+++ types/intl-locales-supported/index.d.ts
@@ -1,6 +1,6 @@
 // Type definitions for intl-locales-supported 1.0
 // Project: https://github.com/yahoo/intl-locales-supported
-// Definitions by: My Self <https://github.com/me>
+// Definitions by: Edward Sammut Alessi <https://github.com/Slessi>
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
 
 export default function areIntlLocalesSupported(locales: string | string[]): void;
