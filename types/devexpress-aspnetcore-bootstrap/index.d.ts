8a498aace206e32578d0a32318060d2506575cb0
skubarenko.andrey <andrey.skubarenko@devexpress.com>
2018-05-17 10:53:21 +0300

    Add a github id

--- types/devexpress-aspnetcore-bootstrap/index.d.ts
+++ types/devexpress-aspnetcore-bootstrap/index.d.ts
@@ -1,6 +1,7 @@
 // Type definitions for DevExpress ASP.NET 181.3
 // Project: http://devexpress.com/
 // Definitions by: DevExpress Inc. <https://github.com/devexpress>
+//                 Andrey Skubarenko <https://github.com/skubarenko>
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
 // TypeScript Version: 2.4
 
