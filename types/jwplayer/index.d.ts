78fd70b148641aa6e5479e2f104a191e1f60787a
Benjamin Dobson <benjamin.dobson@softwire.com>
2018-05-17 17:44:26 +0100

    Add my name and minimum typescript version

--- types/jwplayer/index.d.ts
+++ types/jwplayer/index.d.ts
@@ -4,7 +4,9 @@
 //                 Tomer Kruvi <https://github.com/kutomer>
 //                 Philipp Gürtler <https://github.com/philippguertler>
 //                 Daniel McGraw <https://github.com/danielmcgraw>
+//                 Benjamin Dobson <https://github.com/bpdsw>
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
+// TypeScript Version: 2.2
 
 // JW Player is the leading HTML5 & Flash video player, optimized for mobile and the desktop. Easy enough for beginners, advanced enough for pros.
 
