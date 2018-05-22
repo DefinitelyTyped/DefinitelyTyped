c1e00e25e96757183996545fd546cd74b38e7e4e
Amir.Tugi@Gmail.com <Amir.T@logz.io>
2018-05-22 17:32:22 +0300

    Fill correct metadata

--- types/express-paginate/index.d.ts
+++ types/express-paginate/index.d.ts
@@ -1,6 +1,6 @@
 // Type definitions for express-paginate 1.0
 // Project: https://github.com/niftylettuce/express-paginate
-// Definitions by: My Self <https://github.com/me>
+// Definitions by: Amir Tugendhaft <https://github.com/AmirTugi>
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
 // TypeScript Version: 2.7
 
