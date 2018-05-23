4a7b434868f8ad87e539c54266fb15c9e85f4022
Olegs Jeremejevs <olegs@jeremejevs.com>
2018-05-23 18:54:24 +0300

    is-svg: Bump to 3.0

--- types/is-svg/index.d.ts
+++ types/is-svg/index.d.ts
@@ -1,4 +1,4 @@
-// Type definitions for is-svg 2.1
+// Type definitions for is-svg 3.0
 // Project: https://github.com/sindresorhus/is-svg#readme
 // Definitions by: BendingBender <https://github.com/BendingBender>
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
