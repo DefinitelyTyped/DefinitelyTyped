0231e7b9449a847618912c446112e7a9f30987c8
Vincent Siao <vincent@asana.com>
2018-05-23 21:35:11 -0700

    [create-subscription] Bump required TS version to match react

--- types/create-subscription/index.d.ts
+++ types/create-subscription/index.d.ts
@@ -3,7 +3,7 @@
 // Definitions by: Asana <https://github.com/Asana>
 //                 Vincent Siao <https://github.com/vsiao>
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
-// TypeScript Version: 2.3
+// TypeScript Version: 2.6
 
 import * as React from "react";
 
