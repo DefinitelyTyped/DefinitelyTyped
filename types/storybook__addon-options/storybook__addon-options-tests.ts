62d867ed550721b08a8179c36849a1b378ac1668
Simon Nielsen <simonhn@gmail.com>
2018-05-24 21:49:24 +0200

    add hierarchyRootSeparator

--- types/storybook__addon-options/storybook__addon-options-tests.ts
+++ types/storybook__addon-options/storybook__addon-options-tests.ts
@@ -1,17 +1,18 @@
 import { setOptions } from '@storybook/addon-options';
 
 setOptions({
-  name: 'My Storybook',
-  url: 'https://example.com',
-  goFullScreen: false,
-  showLeftPanel: false,
-  showDownPanel: false,
-  showSearchBox: false,
-  downPanelInRight: false,
-  sortStoriesByKind: false,
-  hierarchySeparator: /\//,
+    name: 'My Storybook',
+    url: 'https://example.com',
+    goFullScreen: false,
+    showLeftPanel: false,
+    showDownPanel: false,
+    showSearchBox: false,
+    downPanelInRight: false,
+    sortStoriesByKind: false,
+    hierarchySeparator: /\//,
+    hierarchyRootSeparator: /\|/
 });
 
 setOptions({
-  hierarchySeparator: '.',
+    hierarchySeparator: '.'
 });
