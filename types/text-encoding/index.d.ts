c0b3b895956ad144c7d083665ec95c0c2f4dd1a0
Mohsen Azimi <me@azimi.me>
2018-05-30 11:45:46 -0700

    bring back namespace

--- types/text-encoding/index.d.ts
+++ types/text-encoding/index.d.ts
@@ -5,5 +5,20 @@
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
 // TypeScript Version: 2.8
 
-export const TextEncoder: TextEncoder;
-export const TextDecoder: TextDecoder;
+declare namespace TextEncoding {
+    interface TextEncoderOptions {
+        NONSTANDARD_allowLegacyEncoding?: boolean;
+    }
+    interface TextEncoderStatic {
+        (utfLabel?: string, options?: TextEncoderOptions): TextEncoder;
+        new (utfLabel?: string, options?: TextEncoderOptions): TextEncoder;
+    }
+    export const TextEncoder: TextEncoder;
+    export const TextDecoder: TextDecoder;
+}
+
+declare var TextEncoding: TextEncoding.TextEncodingStatic;
+
+declare module "text-encoding" {
+    export = TextEncoding;
+}
