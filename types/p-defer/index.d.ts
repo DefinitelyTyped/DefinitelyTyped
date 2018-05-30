ec8120cba984be937ea19e0d2f802a670d94c2e7
Braxton M. Christensen <braxton.christensen@domo.com>
2018-05-30 09:46:02 -0600

    chore(p-defer): bump version
    bump version number

--- types/p-defer/index.d.ts
+++ types/p-defer/index.d.ts
@@ -1,4 +1,4 @@
-// Type definitions for p-defer 1.0
+// Type definitions for p-defer 1.0.2
 // Project: https://github.com/sindresorhus/p-defer
 // Definitions by: Sam Verschueren <https://github.com/SamVerschueren>
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
