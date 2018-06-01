ca0fbc2e08699be3749e631c3798e09374809ec5
Steve Fenton <Steve-Fenton@users.noreply.github.com>
2018-06-01 14:07:04 +0100

    Update index.d.ts
    TypeScript 2.8 comment

--- types/sharedworker/index.d.ts
+++ types/sharedworker/index.d.ts
@@ -2,6 +2,8 @@
 // Project: http://www.w3.org/TR/workers/
 // Definitions by: Toshiya Nakakura <https://github.com/nakakura>
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
+// TypeScript Version: 2.8
+
 declare namespace SharedWorker {
     interface AbstractWorker extends EventTarget {
         onerror: (ev: ErrorEvent) => any;
