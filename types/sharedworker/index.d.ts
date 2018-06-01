2231f8cf14d1f9354c424b149a1170a0cb4ee432
Steve Fenton <Steve-Fenton@users.noreply.github.com>
2018-06-01 13:51:02 +0100

    Update index.d.ts
    Fulfilling SharedWorkerGlobalScope.

--- types/sharedworker/index.d.ts
+++ types/sharedworker/index.d.ts
@@ -2,7 +2,6 @@
 // Project: http://www.w3.org/TR/workers/
 // Definitions by: Toshiya Nakakura <https://github.com/nakakura>
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
-
 declare namespace SharedWorker {
     interface AbstractWorker extends EventTarget {
         onerror: (ev: ErrorEvent) => any;
@@ -16,6 +15,10 @@ declare namespace SharedWorker {
          */
         port: MessagePort;
     }
+
+    interface SharedWorkerGlobalScope {
+        onconnect: (event: MessageEvent) => void;
+    }
 }
 
 declare var SharedWorker: {
@@ -25,5 +28,5 @@ declare var SharedWorker: {
      * @param {string} stringUrl    Pathname to JavaScript file
      * @param {string} name         Name of the worker to execute
      */
-    new (stringUrl: string, name?: string): SharedWorker.SharedWorker;
+    new(stringUrl: string, name?: string): SharedWorker.SharedWorker;
 };
