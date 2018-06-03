70c27888b38575856c20673cc7f3506432f5ffc6
Jiaren Liu <jrliu@alauda.io>
2018-06-03 20:27:32 +0800

    feat: add declaration file for koa-static-cache

---
+++ types/koa-static-cache/koa-static-cache-tests.ts
@@ -0,0 +1,46 @@
+import * as Koa from 'koa';
+import * as staticCache from 'koa-static-cache';
+
+const app = new Koa();
+
+app.use(staticCache('.'));
+
+app.use(
+    staticCache('.', {
+        maxAge: 0,
+        filter: ['']
+    })
+);
+
+const files: staticCache.Files = {};
+
+app.use(
+    staticCache(
+        '.',
+        {
+            maxAge: 0,
+            filter: path => !!path
+        },
+        files
+    )
+);
+
+files['/service-worker.js'].maxAge = 0;
+
+app.use(
+    staticCache({
+        dir: '.',
+        maxAge: 0,
+        files
+    })
+);
+
+app.use(
+    staticCache(
+        {
+            dir: '.',
+            maxAge: 0
+        },
+        files
+    )
+);
