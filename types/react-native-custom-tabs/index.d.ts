342e47182a782ef3c601cdd7406b31c9e6222a5d
Phil Nova <pnova8@gmail.com>
2018-06-04 12:40:59 -0700

    Fix project link

--- types/react-native-custom-tabs/index.d.ts
+++ types/react-native-custom-tabs/index.d.ts
@@ -1,5 +1,5 @@
 // Type definitions for react-native-custom-tabs 0.1
-// Project: https://github.com/droibit/react-native-custom-tabs/pull/28
+// Project: https://github.com/droibit/react-native-custom-tabs
 // Definitions by: Phil Nova <https://github.com/philnova>
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
 
