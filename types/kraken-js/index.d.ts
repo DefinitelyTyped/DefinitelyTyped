7c501b22e6e258f570d0eccdfbaa792653dd782d
Satana Charuwichitratana <micksatana@live.com>
2018-06-04 12:02:53 +0700

    Add a contributor

--- types/kraken-js/index.d.ts
+++ types/kraken-js/index.d.ts
@@ -1,6 +1,7 @@
 // Type definitions for krakenjs 2.2
 // Project: http://krakenjs.com
 // Definitions by: Timur Manyanov <https://github.com/darkwebdev>
+//                 Satana Charuwichitratana <https://github.com/micksatana>
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
 // TypeScript Version: 2.2
 
