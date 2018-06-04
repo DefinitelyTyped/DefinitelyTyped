11d193de2628cafe46b89a1f629ea9db3eb28505
Cerberuser <computers05@mail.ru>
2018-06-04 08:54:24 +0700

    Update cytoscape-tests.ts
    Added a whole set of options for startup

--- types/cytoscape/cytoscape-tests.ts
+++ types/cytoscape/cytoscape-tests.ts
@@ -68,7 +68,35 @@ const cy = cytoscape({
       { data: { id: 'eb', source: 'e', target: 'b' } }
     ]
   },
+  
+  // initial viewport state:
+  zoom: 1,
+  pan: { x: 0, y: 0 },
 
+  // interaction options:
+  minZoom: 1e-50,
+  maxZoom: 1e50,
+  zoomingEnabled: true,
+  userZoomingEnabled: true,
+  panningEnabled: true,
+  userPanningEnabled: true,
+  selectionType: 'single',
+  touchTapThreshold: 8,
+  desktopTapThreshold: 4,
+  autolock: false,
+  autoungrabify: false,
+
+  // rendering options:
+  headless: false,
+  styleEnabled: true,
+  hideEdgesOnViewport: false,
+  hideLabelsOnViewport: false,
+  textureOnViewport: false,
+  motionBlur: false,
+  motionBlurOpacity: 0.2,
+  wheelSensitivity: 1,
+  pixelRatio: 'auto',
+    
   layout: {
     name: 'preset',
     padding: 5
