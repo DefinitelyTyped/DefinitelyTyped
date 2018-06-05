ebcb84149445bd02f026ccdfe4abb2e75d0a9c0f
Nikolai Ommundsen <post@niikoo.net>
2018-06-05 08:51:44 +0200

    Chrome Apps: Doc update

--- types/chrome-apps/index.d.ts
+++ types/chrome-apps/index.d.ts
@@ -15,8 +15,8 @@
 ////////////////////
 /**
  * Use the chrome.accessibilityFeatures API to manage Chrome's accessibility features. This API relies on the ChromeSetting prototype of the type API for getting and setting individual accessibility features. In order to get feature states the extension must request accessibilityFeatures.read permission. For modifying feature state, the extension needs accessibilityFeatures.modify permission. Note that accessibilityFeatures.modify does not imply accessibilityFeatures.read permission.
- * Availability: Since Chrome 37.
- * Permissions: "accessibilityFeatures.read"
+ * @since Availability: Since Chrome 37.
+ * Permissions: "accessibilityFeatures.read" (For read access); "accessibilityFeatures.modify" (For modifications; Note that accessibilityFeatures.modify does not imply accessibilityFeatures.read permission.)
  * Important: This API works only on Chrome OS.
  */
 declare namespace chrome {
