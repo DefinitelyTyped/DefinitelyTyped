e4681a9927510f50a1ec6009af2da4e06d3cd61d
Gabriel <gabriel@gabrielgil.es>
2018-06-05 11:51:32 +0200

    ng-file-upload: Make method optional
    The method parameter is not required, given that it fallbacks to POST.

--- types/ng-file-upload/index.d.ts
+++ types/ng-file-upload/index.d.ts
@@ -256,7 +256,7 @@ declare module 'angular' {
             /**
              * Add which HTTP method to use: 'POST' or 'PUT' (html5)
              */
-            method: string;
+            method?: string;
             /**
              * This is to accommodate server implementations expecting nested data object keys in .key or [key] format.
              * Example: data: {rec: {name: 'N', pic: file}} sent as: rec[name] -> N, rec[pic] -> file
