6b4596af03fb28634fe99461b3a81c23b2c0e18f
Jokcy <jokcylou@hotmail.com>
2018-06-06 15:43:54 +0800

    Update index.d.ts
    WebpackServe function accept args like:
    ```ts
    {
      config: webpackConfig,
      compiler: webpackCompiler
    }
    ```
    rather then
    ```ts
    {
      config: {
        config: webpackConfig,
        compiler: webpackCompiler
      }
    }
    ```

--- types/webpack-serve/index.d.ts
+++ types/webpack-serve/index.d.ts
@@ -21,7 +21,7 @@ declare module 'webpack' {
 }
 
 declare function WebpackServe(
-  { config }: { config: webpack.Configuration }
+  config: webpack.Configuration
 ): Promise<WebpackServe.Instance>;
 
 declare namespace WebpackServe {
