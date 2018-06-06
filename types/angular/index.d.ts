43448845ed60d8e2672bc2ebfd531f70ece68402
Steffen Kowalski <scipper@myscipper.de>
2018-06-06 14:14:35 +0200

    Added myself to "Definitions by"

--- types/angular/index.d.ts
+++ types/angular/index.d.ts
@@ -4,6 +4,7 @@
 //                 Georgii Dolzhykov <https://github.com/thorn0>
 //                 Caleb St-Denis <https://github.com/calebstdenis>
 //                 Leonard Thieu <https://github.com/leonard-thieu>
+//                 Steffen Kowalski <https://github.com/scipper>
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
 // TypeScript Version: 2.3
 
