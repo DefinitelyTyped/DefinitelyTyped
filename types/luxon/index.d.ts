3a411662084b68d38b20b8328f72f9cba5cc09c1
Lyle Johnson <lyle.johnson@synapsewireless.com>
2018-06-06 11:45:39 -0500

    remove patchlevel change from version number

--- types/luxon/index.d.ts
+++ types/luxon/index.d.ts
@@ -1,4 +1,4 @@
-// Type definitions for luxon 0.5.3
+// Type definitions for luxon 0.5
 // Project: https://github.com/moment/luxon#readme
 // Definitions by: Colby DeHart <https://github.com/colbydehart>
 //                 Hyeonseok Yang <https://github.com/FourwingsY>
