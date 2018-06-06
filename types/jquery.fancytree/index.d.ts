5f701a0df2536e3ba5e5a0ca8c4fe7a9d840295a
Nikolai Ommundsen <post@niikoo.net>
2018-06-06 14:25:03 +0200

    Set correct version number

--- types/jquery.fancytree/index.d.ts
+++ types/jquery.fancytree/index.d.ts
@@ -1,4 +1,4 @@
-// Type definitions for jquery.fancytree 2.7.0
+// Type definitions for jquery.fancytree 2.28.2-0
 // Project: https://github.com/mar10/fancytree
 // Definitions by: Peter Palotas <https://github.com/alphaleonis>
 //                 Mahdi Abedi <https://github.com/abedi-ir>
