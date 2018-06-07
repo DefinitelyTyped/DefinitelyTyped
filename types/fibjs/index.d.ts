80dc540ca7d25b20dd909a521d705cb2a1c6b3e5
Richardo2016 <richardo2016@gmail.com>
2018-06-07 15:02:26 +0800

    add UNUSED_FILES.txt for types/fibjs

--- types/fibjs/index.d.ts
+++ types/fibjs/index.d.ts
@@ -1,5 +1,5 @@
-// Type definitions for fibjs 0.1
-// Project: http://github.com/fibjs/fib-types
+// Type definitions for fibjs 0.25
+// Project: https://github.com/fibjs/fibjs
 // Definitions by: Richard <https://github.com/richardo2016>
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
 
