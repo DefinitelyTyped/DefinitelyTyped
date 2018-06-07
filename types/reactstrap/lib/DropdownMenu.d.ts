03041459989ac55f3031e9bef0492482a5575f91
Flosch <florent.schildknecht@humblebee.se>
2018-06-07 22:49:21 +0200

    Replace local Popper typings by popper.js dependency

--- types/reactstrap/lib/DropdownMenu.d.ts
+++ types/reactstrap/lib/DropdownMenu.d.ts
@@ -1,6 +1,6 @@
 import * as React from 'react';
+import * as Popper from 'popper.js';
 import { CSSModule } from '../index';
-import { Popper } from './Popper';
 
 export type DropdownMenuProps<T = {}> = React.HTMLAttributes<HTMLElement> & {
   tag?: React.ReactType;
