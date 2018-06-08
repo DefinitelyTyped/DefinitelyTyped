0f53b0059db2cf39c09d7992c34e4b2db926d6b5
Sunny Juneja <jr.sunny@gmail.com>
2018-06-08 15:46:23 -0700

    Chartist: Relax constraints on meta.

--- types/chartist/index.d.ts
+++ types/chartist/index.d.ts
@@ -1,6 +1,6 @@
 // Type definitions for Chartist v0.9.81
 // Project: https://github.com/gionkunz/chartist-js
-// Definitions by: Matt Gibbs <https://github.com/mtgibbs>, Simon Pfeifer <https://github.com/psimonski>, Cassey Lottman <https://github.com/clottman>, Anastasiia Antonova <https://github.com/affilnost>
+// Definitions by: Matt Gibbs <https://github.com/mtgibbs>, Simon Pfeifer <https://github.com/psimonski>, Cassey Lottman <https://github.com/clottman>, Anastasiia Antonova <https://github.com/affilnost>, Sunny Juneja <https://github.com/sunnyrjuneja>
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
 
 declare namespace Chartist {
@@ -101,7 +101,7 @@ declare namespace Chartist {
         value?: number;
         data?: Array<number>;
         className?: string;
-        meta?: string; // I assume this could probably be a number as well?
+        meta?: any;
     }
 
     interface IChartistBase<T extends IChartOptions> {
