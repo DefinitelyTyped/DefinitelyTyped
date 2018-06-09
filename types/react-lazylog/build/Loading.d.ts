84e66f7ede43c3b5a834da559d3fb5d33bbd41a7
= <bromano@snapchat.com>
2018-06-09 02:43:18 -0700

    Initial creation of react-lazylog types

---
+++ types/react-lazylog/build/Loading.d.ts
@@ -0,0 +1,5 @@
+import { Component } from "react";
+
+export interface LoadingProps {}
+
+export default class Loading extends Component<LoadingProps> {}
---
+++ types/react-lazylog/index.d.ts
@@ -0,0 +1,8 @@
+// Type definitions for react-lazylog 3.1.4
+// Project: https://github.com/mozilla-frontend-infra/react-lazylog
+// Definitions by: Benjamin Romano <https://github.com/benjaminRomano>
+// Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
+// TypeScript Version: 2.3
+
+export * from "./build/LazyLog";
+export * from "./build/ScrollFollow";
