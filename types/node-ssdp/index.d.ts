2d5a526375eebe71d02dcb7eb898cf71eec1d131
Olivia Trewin <zancoproductions@gmail.com>
2018-06-08 19:17:22 -0500

    Used dts-gen to generate

--- types/node-ssdp/index.d.ts
+++ types/node-ssdp/index.d.ts
@@ -1,5 +1,5 @@
 // Type definitions for node-ssdp 3.3
-// Project: https://github.com/diversario/node-ssdp
+// Project: https://github.com/diversario/node-ssdp#readme
 // Definitions by: Olivia Trewin <https://github.com/OrionNebula>
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
 
