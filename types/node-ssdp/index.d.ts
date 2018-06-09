501576defc99c45922e28302d0f7534adf868f09
Olivia Trewin <zancoproductions@gmail.com>
2018-06-08 19:48:33 -0500

    Corrected 'customLogger' definition

--- types/node-ssdp/index.d.ts
+++ types/node-ssdp/index.d.ts
@@ -45,7 +45,9 @@ export interface SsdpOptions {
 	/** Path to SSDP description file */
 	description?: string;
 	/** Additional headers */
-	headers?: SsdpHeaders;
+    headers?: SsdpHeaders;
+    /** A logger function to use instead of the default. The first argument to the function can contain a format string. */
+	customLogger?: (format: string, ...args: any[]) => void;
 }
 
 export interface ClientOptions extends SsdpOptions {
@@ -53,8 +55,6 @@ export interface ClientOptions extends SsdpOptions {
 	interfaces?: string[];
 	/** Bind sockets to each discovered interface explicitly instead of relying on the system. Might help with issues with multiple NICs. */
 	explicitSocketBind?: boolean;
-	/** A logger function to use instead of the default. The first argument to the function can contain a format string. */
-	customLogger?: (format?: string) => void;
 	/**
 	 * When true socket.bind() will reuse the address, even if another process has already bound a socket on it.
 	 * @default true
