90e7a161f5425490812edf2899db28b285dddfca
Mattias Buelens <mattias@buelens.com>
2018-06-10 00:30:52 +0200

    Update contributors

--- types/whatwg-streams/index.d.ts
+++ types/whatwg-streams/index.d.ts
@@ -2,6 +2,7 @@
 // Project: https://github.com/whatwg/streams
 // Definitions by: Kagami Sascha Rosylight <https://github.com/saschanaz>
 //                 Konstantin Simon Maria Möllers <https://github.com/ksm2>
+//                 Mattias Buelens <https://github.com/MattiasBuelens>
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
 // TypeScript Version: 2.3
 
