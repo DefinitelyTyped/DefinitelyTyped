b8e67225bed2bec8c57c51a923629bdc29095073
Jesse Bethke <jbethke@localhost.localdomain>
2018-06-10 12:24:42 -0700

    Updated definitions by tag

--- types/decompress/index.d.ts
+++ types/decompress/index.d.ts
@@ -1,6 +1,7 @@
 // Type definitions for decompress 4.2
 // Project: https://github.com/kevva/decompress#readme
 // Definitions by: York Yao <https://github.com/plantain-00>
+//                 Jesse Bethke <https://github.com/jbethke>
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
 
 /// <reference types="node" />
