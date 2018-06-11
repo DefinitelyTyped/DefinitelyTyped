11c510ffedf81966bd6e72e16a5b369eabaaf1ff
Konstantin Rohde <konstantin.rohde@wiegandsystem.de>
2018-06-11 12:55:31 +0200

    Neu erstellt mit dts-gen

--- types/datatables.net-scroller/index.d.ts
+++ types/datatables.net-scroller/index.d.ts
@@ -1,8 +1,7 @@
 // Type definitions for datatables.net-scroller 1.4
-// Project: https://datatables.net/extensions/scroller/
+// Project: https://datatables.net
 // Definitions by: Konstantin Rohde <https://github.com/RohdeK>
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
-// TypeScript Version: 2.4
 
 /// <reference types="jquery" />
 /// <reference types="datatables.net"/>
