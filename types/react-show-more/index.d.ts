d051db2865c3955fd5fcbd61fb9e4d794a6c9730
Naor Torgeman <naortor@gmail.com>
2018-06-12 19:01:15 +0300

    Update index.d.ts

--- types/react-show-more/index.d.ts
+++ types/react-show-more/index.d.ts
@@ -1,4 +1,4 @@
-// Type definitions for react-show-more
+// Type definitions for react-show-more 2.0
 // Project: https://github.com/One-com/react-show-more
 // Definitions by: Naor Torgeman <https://github.com/naortor>
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
