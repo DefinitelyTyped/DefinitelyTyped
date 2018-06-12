334b07cf44e8e9ba55a57ad3a7e1a82f51566533
adelmurzin <iKBAHT@gmail.com>
2018-06-12 11:40:51 +0300

    Added type definitions for emoji-regex

---
+++ types/emoji-regex/index.d.ts
@@ -0,0 +1,7 @@
+// Type definitions for emoji-regex 7.0
+// Project: https://mths.be/emoji-regex
+// Definitions by: iKBAHT <https://github.com/iKBAHT>
+// Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
+
+declare const createRegExp: () => RegExp;
+export default createRegExp;
