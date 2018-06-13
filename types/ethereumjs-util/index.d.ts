f8ad00e5ff76b72b72ec94b8fcdb31a63d3f50d1
austaras <austaras@outlook.com>
2018-06-13 11:42:48 +0800

    modify version

--- types/ethereumjs-util/index.d.ts
+++ types/ethereumjs-util/index.d.ts
@@ -1,4 +1,4 @@
-// Type definitions for ethereumjs-util 5.1
+// Type definitions for ethereumjs-util 5.2
 // Project: https://github.com/ethereumjs/ethereumjs-util#readme
 // Definitions by: Juan J. Jimenez-Anca <https://github.com/cortopy>
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
