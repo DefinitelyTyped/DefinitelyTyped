973ae2b14cc2eb3335e89785a150684a0b8a80b9
clark-stevenson <a.scotsman@gmail.com>
2018-06-13 22:43:54 +0100

    I meant to fix this as I am the maintainer and why TypeScript Bot doesn't recognise me. (I believe)

--- types/pixi.js/index.d.ts
+++ types/pixi.js/index.d.ts
@@ -1,6 +1,6 @@
 // Type definitions for pixi.js 4.8
 // Project: https://github.com/pixijs/pixi.js/tree/dev
-// Definitions by: clark-stevenson <https://github.com/pixijs/pixi-typescript>
+// Definitions by: clark-stevenson <https://github.com/clark-stevenson>
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
 // TypeScript Version: 2.1
 
