5a5a38f8f60704fffb429a15d2fd210004bae1e1
Dave Lee <d.lee.1@elsevier.com>
2018-06-13 13:54:48 -0500

    Add new types for http-graceful-shutdown

---
+++ types/http-graceful-shutdown/http-graceful-shutdown-tests.ts
@@ -0,0 +1,23 @@
+/// <reference types="node" />
+
+import * as GracefulShutdown from 'http-graceful-shutdown';
+import * as http from "http";
+
+const opts: GracefulShutdown.Options = {
+    signals: "SIGINT SIGTERM",
+    timeout: 1337,
+    development: false,
+    onShutdown: () => {
+        console.log('fake shutdown handler');
+        return Promise.resolve();
+    },
+    finally: () => {
+        console.log('fake finally handler');
+    }
+};
+
+const server = http.createServer((req, res) => {
+    res.end();
+});
+
+GracefulShutdown(server, opts);
