d17e27be5575239b15cc77b064c7ddc5b8619329
Ernesto Cruz <ernesto.cruzoviedo@gmail.com>
2018-06-14 12:40:46 -0500

    Fix tslint errors

--- types/react-js-pagination/index.d.ts
+++ types/react-js-pagination/index.d.ts
@@ -6,36 +6,35 @@
 
 import * as React from "react";
 
-declare module 'react-js-pagination' {
-    export interface ReactJsPaginationProps {
-        totalItemsCount: number;
-        onChange: (pageNumber: number) => void;
-        activePage: number;
-        itemsCountPerPage?: number;
-        pageRangeDisplayed?: number;
-        prevPageText?: string| React.ReactElement<any>;
-        nextPageText?: string | React.ReactElement<any>;
-        lastPageText?: string | React.ReactElement<any>;
-        firstPageText?: string | React.ReactElement<any>;
-        disabledClass?: string;
-        hideDisabled?: boolean;
-        hideNavigation?: boolean;
-        innerClass?: string;
-        itemClass?: string;
-        itemClassFirst?: string;
-        itemClassPrev?: string;
-        itemClassNext?: string;
-        itemClassLast?: string;
-        linkClass?: string;
-        activeClass?: string;
-        activeLinkClass?: string;
-        linkClassFirst?: string;
-        linkClassPrev?: string;
-        linkClassNext?: string;
-        linkClassLast?: string;
-        hideFirstLastPages?: boolean;
-        getPageUrl?(): string;
-    }
-    const Pagination: React.ClassicComponentClass<ReactJsPaginationProps>;
+export interface ReactJsPaginationProps {
+    totalItemsCount: number;
+    onChange: (pageNumber: number) => void;
+    activePage: number;
+    itemsCountPerPage?: number;
+    pageRangeDisplayed?: number;
+    prevPageText?: string| React.ReactElement<any>;
+    nextPageText?: string | React.ReactElement<any>;
+    lastPageText?: string | React.ReactElement<any>;
+    firstPageText?: string | React.ReactElement<any>;
+    disabledClass?: string;
+    hideDisabled?: boolean;
+    hideNavigation?: boolean;
+    innerClass?: string;
+    itemClass?: string;
+    itemClassFirst?: string;
+    itemClassPrev?: string;
+    itemClassNext?: string;
+    itemClassLast?: string;
+    linkClass?: string;
+    activeClass?: string;
+    activeLinkClass?: string;
+    linkClassFirst?: string;
+    linkClassPrev?: string;
+    linkClassNext?: string;
+    linkClassLast?: string;
+    hideFirstLastPages?: boolean;
+    getPageUrl?(): string;
 }
+declare const Pagination: React.ClassicComponentClass<ReactJsPaginationProps>;
+
 export default Pagination;
--- types/react-js-pagination/react-js-pagination-tests.tsx
+++ types/react-js-pagination/react-js-pagination-tests.tsx
@@ -1,27 +1,24 @@
 import * as React from 'react';
 import Pagination from 'react-js-pagination';
 
-class ReactPagination extends React.Component<{},{activePage:number} > {
+class ReactPagination extends React.Component<{}, {activePage: number}> {
 	constructor(props: {}) {
 		super(props);
 		this.state = {
-			activePage: 1			
+			activePage: 1
 		};
 		this.handleChange = this.handleChange.bind(this);
 	}
-
-	handleChange(pageNumber:number){
+	handleChange(pageNumber: number) {
 		this.setState({
 			activePage: pageNumber
 		});
-	};
-
-	render(){
+	}
+	render() {
         return (
             <Pagination activePage={this.state.activePage} onChange={this.handleChange}
                 totalItemsCount={100}
             />
-
         );
     }
 }
