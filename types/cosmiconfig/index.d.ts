50e74c675aa0f11ca9daee07f3a98b0b419c066c
Jinwoo Lee <jinwoo@google.com>
2018-06-14 11:17:55 -0700

    add myself to "Definitions by"

--- types/cosmiconfig/index.d.ts
+++ types/cosmiconfig/index.d.ts
@@ -3,6 +3,7 @@
 // Definitions by: ozum <https://github.com/ozum>
 //                 szeck87 <https://github.com/szeck87>
 //                 saadq <https://github.com/saadq>
+//                 jinwoo <https://github.com/jinwoo>
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
 // TypeScript Version: 2.2
 
