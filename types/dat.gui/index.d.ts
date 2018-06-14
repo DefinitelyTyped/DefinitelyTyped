6c1361ab11619752b5c9b3561c740c0695085486
Farouk Abdou <kaktus40@hotmail.fr>
2018-06-14 08:36:07 +0200

    add step parameter

--- types/dat.gui/index.d.ts
+++ types/dat.gui/index.d.ts
@@ -51,8 +51,7 @@ export class GUI {
     __folders: GUI[];
     domElement: HTMLElement;
 
-    add(target: Object, propName:string): GUIController;
-    add(target: Object, propName:string, min: number, max: number): GUIController;
+    add(target: Object, propName:string, min?: number, max?: number, step?: number): GUIController;
     add(target: Object, propName:string, status: boolean): GUIController;
     add(target: Object, propName:string, items:string[]): GUIController;
     add(target: Object, propName:string, items:number[]): GUIController;
