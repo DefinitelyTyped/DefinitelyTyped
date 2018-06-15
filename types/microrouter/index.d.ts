f61d99d5b05b53881726546d1a654169cf045d82
Robin Bellini Olsson <rbo@hopper.dk>
2018-06-15 13:35:53 +0200

    fix version header

--- types/microrouter/index.d.ts
+++ types/microrouter/index.d.ts
@@ -1,4 +1,4 @@
-// Type definitions for microrouter 3.1.3
+// Type definitions for microrouter 3.1
 // Project: https://github.com/pedronauck/micro-router#readme
 // Definitions by: Mathieu Dutour <https://github.com/mathieudutour>
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
