e9cc562e245539540cbe4d30e8a42bcd267989a1
Manuel Thalmann <m@nuth.ch>
2018-06-19 00:04:11 +0200

    Increase the version-number

--- types/mustache/index.d.ts
+++ types/mustache/index.d.ts
@@ -1,4 +1,4 @@
-// Type definitions for Mustache 0.8.2
+// Type definitions for Mustache 0.8.3
 // Project: https://github.com/janl/mustache.js
 // Definitions by: Mark Ashley Bell <https://github.com/markashleybell>, Manuel Thalmann <https://github.com/manuth>
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
