2b09e0ea8243d20e570f75d715b0a7b4c112a3ad
adelmurzin <iKBAHT@gmail.com>
2018-06-18 18:42:30 +0300

    fixed by review

--- types/emoji-regex/index.d.ts
+++ types/emoji-regex/index.d.ts
@@ -1,7 +1,7 @@
 // Type definitions for emoji-regex 7.0
-// Project: https://mths.be/emoji-regex
+// Project: https://github.com/mathiasbynens/emoji-regex
 // Definitions by: iKBAHT <https://github.com/iKBAHT>
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
 
 declare function createRegExp(): RegExp;
-export default createRegExp;
+export = createRegExp;
