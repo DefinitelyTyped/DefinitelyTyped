474ff5890649dd1246dbd92565f455d2c848e71d
Josef Hornych <hornych@engeto.com>
2018-06-18 12:37:48 +0200

    Added Client class and definitions of functions for Client.users

--- types/intercom-client/index.d.ts
+++ types/intercom-client/index.d.ts
@@ -3,6 +3,9 @@
 // Definitions by: Jinesh Shah <https://github.com/jineshshah36>
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
 
+import {List as UserList, User, UserIdentifier} from './User';
+import {Scroll} from './Scroll';
+
 export interface IdentityVerificationOptions {
     secretKey: string;
     identifier: string;
@@ -11,3 +14,36 @@ export interface IdentityVerificationOptions {
 export const IdentityVerification: {
     userHash(opts: IdentityVerificationOptions): string;
 };
+
+
+export declare class Client {
+    constructor(auth: { token: string });
+    constructor(username: string, password: string);
+    constructor(auth: { appId: string, appApiKey: string });
+    
+    users: Users;
+}
+
+
+interface Company {
+    readonly "id": string
+}
+
+
+declare class Users {
+    create(user: Partial<User>): Promise<User>
+
+    update(user: UserIdentifier & Partial<User> ): Promise<User>
+
+    find(identifier: UserIdentifier): Promise<User>
+
+    list(): Promise<UserList>
+
+    listBy(params: {tag_id: string, segment_id: string}): Promise<UserList>
+
+    scroll: Scroll<User>
+
+    archive(): Promise<User>
+
+    requestPermanentDeletion(): Promise<{id: number}>
+}
