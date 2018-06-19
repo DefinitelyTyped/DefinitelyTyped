bf354048f8d4eb3553a6d7758137c988363ee703
maplerichie <babierichie@hotmail.com>
2018-06-20 01:19:54 +0800

    Added type definitions for ex-react-native-i18n

---
+++ types/ex-react-native-i18n/index.d.ts
@@ -0,0 +1,10 @@
+// Type definitions for ex-react-native-i18n 0.0.4
+// Project: https://github.com/xcarpentier/ex-react-native-i18n/
+// Definitions by: LikKee Richie <https://github.com/maplerichie>
+// Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
+// TypeScript Version: 2.2
+
+import I18n = require("i18n-js");
+//import I18n from 'ex-react-native-i18n';
+
+export default I18n;
