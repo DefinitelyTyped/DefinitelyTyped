12ae2681434db90e9e7cb485ebf4790cbbd1f800
Nattapong Sirilappanich <s.nattapong@gmail.com>
2018-06-20 16:14:46 +0700

    Define type information for package connect-mongodb-session.

---
+++ types/connect-mongodb-session/index.d.ts
@@ -0,0 +1,31 @@
+// Type definitions for connect-mongodb-session
+// Project: https://github.com/kcbanner/connect-mongodb-session
+// Definitions by: Nattapong Sirilappanich <https://github.com/NattapongSiri>
+// Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
+// TypeScript Version: 2.3
+
+/// <reference types="express-session" />
+
+import session = require('express-session');
+import * as express from 'express';
+import {MongoClientOptions} from 'mongodb'
+import { StringifyOptions } from 'querystring';
+
+declare function connect(fn : (options?: session.SessionOptions) => express.RequestHandler) : connectMongodbSession.MongoDBStore
+
+declare namespace connectMongodbSession {
+    export interface MongoDBStore extends session.Store {
+        new(connection?: ConnectionInfo, callback?: (error) => void)
+    }
+
+    export interface ConnectionInfo {
+        idField? : string
+        collection : string
+        connectionOptions?: MongoClientOptions
+        databaseName?: string
+        expires?: number
+        uri : string
+    }
+}
+
+export = connect
