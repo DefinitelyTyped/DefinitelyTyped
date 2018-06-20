a2d6384e4a8b249fbb2066afb7fae70336d9f92f
SIDENIS\Andrey.Chalkin <andrey.chalkin@sidenis.com>
2018-06-20 18:04:19 +0700

    Added type defintions for http-rx

---
+++ types/http-rx/index.d.ts
@@ -0,0 +1,22 @@
+// Type definitions for http-rx 1.1
+// Project: https://github.com/JasonRammoray/HttpRx
+// Definitions by: L2jLiga <https://github.com/L2jLiga>
+// Definitions: https://github.com/DefinitelyTyped/http-rx
+
+import { Observable } from 'rxjs';
+
+interface HttpRx {
+    get(url: string, options: any): Observable<any>;
+
+    head(url: string, options: any): Observable<any>;
+
+    patch(url: string, options: any): Observable<any>;
+
+    post(url: string, options: any): Observable<any>;
+
+    put(url: string, options: any): Observable<any>;
+
+    'delete'(url: string, options: any): Observable<any>;
+}
+
+export = HttpRx;
