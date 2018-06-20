2a60a12d0a0f7285e9fd4eab4e3d295322ce52a9
Claas Ahlrichs <claasahl@gmail.com>
2018-06-20 06:43:48 +0200

    added dedicated export for getting a single starwars name

--- types/starwars-names/index.d.ts
+++ types/starwars-names/index.d.ts
@@ -5,4 +5,5 @@
 
 export as namespace starwarsNames;
 export const all: string[];
-export function random(number?: number): number;
+export function random(): string;
+export function random(number: number): string[];
