49e1c1d7a858aec82db46319337c0f93fa00713f
Alex Jerabek <aljerabe@microsoft.com>
2018-06-20 10:23:08 -0700

    Removing erroneous text

--- types/office-js/index.d.ts
+++ types/office-js/index.d.ts
@@ -13588,12 +13588,12 @@ declare namespace OfficeExtension {
          */
         expand?: string | string[];
         /**
-         * Only usable on collection types. Specifies the maximum number of collection items that can be included in the result. 
+         * Only usable on collection types. Specifies the maximum number of collection items that can be included in the result.
          */
         top?: number;
         /**
          * Only usable on collection types. Specifies the number of items in the collection that are to be skipped and not included in the result. If top is specified, the result set will start after skipping the specified number of items.
-         */ 
+         */
         skip?: number;
     }
     /**
