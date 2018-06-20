32d32a03024cc1949e5cc517ea69f3aef0b80c35
Mark Nelissen <mark.nelissen@gemsotec.com>
2018-06-20 19:51:07 +0200

    align TypeScript version on the one from react

--- types/react-beautiful-dnd/index.d.ts
+++ types/react-beautiful-dnd/index.d.ts
@@ -5,7 +5,7 @@
 //                 Austin Turner <https://github.com/paustint>
 //                 Mark Nelissen <https://github.com/marknelissen>
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
-// TypeScript Version: 2.9
+// TypeScript Version: 2.6
 
 import * as React from 'react';
 
