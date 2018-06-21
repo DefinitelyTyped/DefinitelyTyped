d6af3047baeec3425b78539be17bc30dc5770b48
tsmalla <tjark.smalla@gmx.de>
2018-06-21 17:44:39 +0200

    satisfy linter

--- types/react-native-background-timer/index.d.ts
+++ types/react-native-background-timer/index.d.ts
@@ -3,31 +3,29 @@
 // Definitions by: Tjark Smalla <https://github.com/chillkroeteTTS>
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
 
-
 /*~ You can declare types that are available via importing the module */
 export type IntervalId = number;
 export type TimeoutId = number;
 
 /*~ You can declare properties of the module using const, let, or var */
 declare class BackgroundTimer {
-
     constructor();
 
-    start(delay?:number): any;
+    start(delay?: number): any;
 
     stop(): void;
 
-    runBackgroundTimer(callback: ()=>void, delay: number): void;
+    runBackgroundTimer(callback: () => void, delay: number): void;
 
-    backgroundClockMethod(callback: ()=>void, delay: number): void;
+    backgroundClockMethod(callback: () => void, delay: number): void;
 
     stopBackgroundTimer(): void;
 
-    setTimeout(callback: ()=>void, timeout: number): TimeoutId;
+    setTimeout(callback: () => void, timeout: number): TimeoutId;
 
     clearTimeout(timeoutId: TimeoutId): void;
 
-    setInterval(callback: ()=>void, timeout: number): IntervalId;
+    setInterval(callback: () => void, timeout: number): IntervalId;
 
     clearInterval(intervalId: IntervalId): void;
 }
