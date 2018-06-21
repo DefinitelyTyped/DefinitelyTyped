01d036688ee13bed63ba3af009031051aea202e9
Saulo Tauil <saulogt@gmail.com>
2018-06-20 20:10:07 -0700

    Converting CompanyIdentifier to interface

--- types/intercom-client/Company.d.ts
+++ types/intercom-client/Company.d.ts
@@ -1,5 +1,7 @@
 
-export type CompanyIdentifier = {company_id: string }
+export interface CompanyIdentifier {
+    company_id: string
+}
 
 export interface Company {
     readonly "type": "company",
