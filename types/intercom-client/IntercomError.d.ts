a461a4c4e2571cdfa8d4b639a33a3851ebebe096
Saulo Tauil <saulogt@gmail.com>
2018-06-20 20:20:16 -0700

    Turning IntercomError.errors a more clear array type

--- types/intercom-client/IntercomError.d.ts
+++ types/intercom-client/IntercomError.d.ts
@@ -2,15 +2,13 @@
 export interface IntercomError {
   statusCode: number,
   body: {
-      type: "error.list",
-      request_id: string,
-      errors: [
-          {
-              code: string, //"400",
-              message: string
-          }
-      ]
+    type: "error.list",
+    request_id: string,
+    errors: Array<{
+      code: string, //"400",
+      message: string
+    }>
   },
-  headers: { status: string } & {[k: string]: string}
+  headers: { status: string } & { [k: string]: string }
 }
 
