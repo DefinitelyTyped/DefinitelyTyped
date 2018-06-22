f01ac6f070e886e03119ddabd07111adc96176f9
HenryNguyen5 <ngu.henry@hotmail.com>
2018-06-22 15:34:35 -0400

    Add contributor and compiler version used

--- types/bn.js/index.d.ts
+++ types/bn.js/index.d.ts
@@ -1,7 +1,9 @@
 // Type definitions for bn.js 4.11
 // Project: https://github.com/indutny/bn.js
 // Definitions by: Leonid Logvinov <https://github.com/LogvinovLeon>
+//                 Henry Nguyen <https://github.com/HenryNguyen5>
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
+// TypeScript Version: 2.9
 
 /// <reference types="node"/>
 
