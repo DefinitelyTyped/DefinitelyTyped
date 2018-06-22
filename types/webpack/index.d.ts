cc4a54af684ca6e9bfd103ed7f565f4cc9341122
Artur <arthurandrosovich@gmail.com>
2018-06-22 18:33:34 +0300

    Add devserver options

--- types/webpack/index.d.ts
+++ types/webpack/index.d.ts
@@ -13,6 +13,7 @@
 //                 Dennis George <https://github.com/dennispg>
 //                 Christophe Hurpeau <https://github.com/christophehurpeau>
 //                 ZSkycat <https://github.com/ZSkycat>
+//                 Artur Androsovych <https://github.com/arturovt>
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
 // TypeScript Version: 2.3
 
@@ -91,7 +92,7 @@ declare namespace webpack {
         /** Switch loaders to debug mode. */
         debug?: boolean;
         /** Can be used to configure the behaviour of webpack-dev-server when the webpack config is passed to webpack-dev-server CLI. */
-        devServer?: any; // TODO: Type this
+        devServer?: Options.DevServerOptions;
         /** Include polyfills or mocks for various node stuff */
         node?: Node;
         /** Set the value of require.amd and define.amd. */
@@ -645,6 +646,80 @@ declare namespace webpack {
             minimizer?: Array<Plugin | Tapable.Plugin>;
             /** Generate records with relative paths to be able to move the context folder". */
             portableRecords?: boolean;
+        }
+        interface DevServerOptions {
+            /** Provides the ability to execute custom middleware after all other middleware internally within the server. */
+            after?: Function;
+            /** This option allows you to whitelist services that are allowed to access the dev server. */
+            allowedHosts?: string[];
+            /** Provides the ability to execute custom middleware prior to all other middleware internally within the server. */
+            before?: Function;
+            /** This option broadcasts the server via ZeroConf networking on start. */
+            bonjour?: boolean;
+            /** When using inline mode, the console in your DevTools will show you messages e.g. before reloading, before an error or when Hot Module Replacement is enabled. This may be too verbose. */
+            clientLogLevel?: 'none' | 'error' | 'warning' | 'info';
+            /** Enable gzip compression for everything served. */
+            compress?: boolean;
+            /** Tell the server where to serve content from. This is only necessary if you want to serve static files. devServer.publicPath will be used to determine where the bundles should be served from, and takes precedence. */
+            contentBase?: boolean | string | string[];
+            /** When set to true this option bypasses host checking. THIS IS NOT RECOMMENDED as apps that do not check the host are vulnerable to DNS rebinding attacks. */
+            disableHostCheck?: boolean;
+            /** his option lets you reduce the compilations in lazy mode. By default in lazy mode, every request results in a new compilation. With filename, it's possible to only compile when a certain file is requested. */
+            filename?: string;
+            /** Adds headers to all responses. */
+            headers?: object;
+            /** When using the HTML5 History API, the index.html page will likely have to be served in place of any 404 responses. */
+            historyApiFallback?: boolean | object;
+            /** Specify a host to use. By default this is localhost. */
+            host?: string;
+            /** Enable webpack's Hot Module Replacement feature. */
+            hot?: boolean;
+            /** Enables Hot Module Replacement (see devServer.hot) without page refresh as fallback in case of build failures. */
+            hotOnly?: boolean;
+            /** By default dev-server will be served over HTTP. It can optionally be served over HTTP/2 with HTTPS. */
+            https?: boolean | object;
+            /** The filename that is considered the index file. */
+            index?: string;
+            /** Toggle between the dev-server's two different modes. By default the application will be served with inline mode enabled. This means that a script will be inserted in your bundle to take care of live reloading, and build messages will appear in the browser console. */
+            inline?: boolean;
+            /** When lazy is enabled, the dev-server will only compile the bundle when it gets requested. This means that webpack will not watch any file changes. */
+            lazy?: boolean;
+            /** With noInfo enabled, messages like the webpack bundle information that is shown when starting up and after each save, will be hidden. Errors and warnings will still be shown. */
+            noInfo?: boolean;
+            /** When open is enabled, the dev server will open the browser. */
+            open?: boolean;
+            /** Specify a page to navigate to when opening the browser. */
+            openPage?: string;
+            /** Shows a full-screen overlay in the browser when there are compiler errors or warnings. Disabled by default. */
+            overlay?: boolean | object;
+            /** When used via the CLI, a path to an SSL .pfx file. If used in options, it should be the bytestream of the .pfx file. */
+            pfx?: string;
+            /** The passphrase to a SSL PFX file. */
+            pfxPassphrase?: string;
+            /** Specify a port number to listen for requests on. */
+            port?: number;
+            /** Proxying some URLs can be useful when you have a separate API backend development server and you want to send API requests on the same domain. */
+            proxy?: object;
+            /** When using inline mode and you're proxying dev-server, the inline client script does not always know where to connect to. It will try to guess the URL of the server based on window.location, but if that fails you'll need to use this. */
+            public?: string;
+            /** The bundled files will be available in the browser under this path. */
+            publicPath?: string;
+            /** With quiet enabled, nothing except the initial startup information will be written to the console. This also means that errors or warnings from webpack are not visible. */
+            quiet?: boolean;
+            /** @deprecated Here you can access the Express app object and add your own custom middleware to it. */
+            setup?: Function;
+            /** The Unix socket to listen to (instead of a host). */
+            socket?: string;
+            /** It is possible to configure advanced options for serving static files from contentBase. */
+            staticOptions?: object;
+            /** This option lets you precisely control what bundle information gets displayed. This can be a nice middle ground if you want some bundle information, but not all of it. */
+            stats?: string | object;
+            /** This option lets the browser open with your local IP. */
+            useLocalIp?: boolean;
+            /** Tell the server to watch the files served by the devServer.contentBase option. File changes will trigger a full page reload. */
+            watchContentBase?: boolean;
+            /** Control options related to watching the files. */
+            watchOptions?: object;
         }
     }
 
