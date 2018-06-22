0b82a39c925109c2e467582d641e77a536513687
HenryNguyen5 <ngu.henry@hotmail.com>
2018-06-22 15:15:45 -0400

    Add interop for module import syntax

--- types/bn.js/bn.js-tests.ts
+++ types/bn.js/bn.js-tests.ts
@@ -1,6 +1,15 @@
-import BN = require('bn.js');
+// test that it works as module when esModuleInterop is enabled
+import BN from 'bn.js';
+
+// test that it works as a require
+import BN2 = require('bn.js');
 
 let bn = new BN(42);
 bn = bn.add(bn);
 bn.isZero();
 bn.byteLength;
+
+let bn2 = new BN2(42);
+bn2 = bn2.add(bn2);
+bn2.isZero();
+bn2.byteLength;
