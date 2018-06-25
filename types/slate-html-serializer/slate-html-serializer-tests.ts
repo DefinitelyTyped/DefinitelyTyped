bdd638fe64ab546429faaa672637a9275aa7a0c3
YangusKhan <9inchsamurai@gmail.com>
2018-06-25 13:47:21 -0600

    Revert "add slate-html-serializer definitions"
    This reverts commit 6650ffbd92aa9fe7a5367312defc09f701ba33ef.

--- types/slate-html-serializer/slate-html-serializer-tests.ts
+++
@@ -1,9 +0,0 @@
-import Html, { Rule } from "slate-html-serializer";
-
-const myRule: Rule = {
-    deserialize: (el: Element, next) => {},
-    serialize: (obj, children) => null
-};
-const serializer = new Html({
-    rules: [myRule]
-});
