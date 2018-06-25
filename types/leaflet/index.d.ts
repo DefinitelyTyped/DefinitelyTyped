da28b94ff5db408d1a91eea10ee0ea35b9c04bd2
Zakaria Hmaidouch <z.hmaidouch@gmail.com>
2018-06-25 17:42:29 +0200

    Update index.d.ts

--- types/leaflet/index.d.ts
+++ types/leaflet/index.d.ts
@@ -398,7 +398,7 @@ export class Layer extends Evented {
     remove(): this;
     removeFrom(map: Map): this;
     getPane(name?: string): HTMLElement | undefined;
-    toGeoJSON(): geojson.Feature<any>;
+    toGeoJSON(): FeatureCollection<Point | Polygon | MultiPolygon>;
 
     // Popup methods
     bindPopup(content: ((layer: Layer) => Content) | Content | Popup, options?: PopupOptions): this;
