0181d8a3780d5f8d9d3ae50bdd00961f18f2832b
Nick McCurdy <nick@nickmccurdy.com>
2018-06-25 18:44:10 -0400

    Add my name to TypeScript bot

--- types/jest-each/index.d.ts
+++ types/jest-each/index.d.ts
@@ -1,6 +1,6 @@
 // Type definitions for jest-each 0.3
 // Project: https://github.com/mattphillips/jest-each
-// Definitions by: Michael Utz <https://github.com/theutz>
+// Definitions by: Michael Utz <https://github.com/theutz>, Nick McCurdy <https://github.com/nickmccurdy>
 // Definitions: <https://github.com/DefinitelyTyped/DefinitelyTyped>
 // TypeScript Version: 2.1
 
