37c46864fcbad31cc81075653531298c59d94ea1
Nick Krichevsky <nick@ollien.com>
2018-06-27 06:54:55 -0400

    Correct version of twig modle

--- types/twig/index.d.ts
+++ types/twig/index.d.ts
@@ -1,4 +1,4 @@
-// Type definitions for twig 2.0
+// Type definitions for twig 1.12.0
 // Project: https://github.com/twigjs/twig.js
 // Definitions by: Carlos Ballesteros Velasco <https://github.com/soywiz>
 //                 Tim Schumacher <https://github.com/enko>
