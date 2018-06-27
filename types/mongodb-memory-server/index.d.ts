1e4f8657edff0721b7aa97bf33ee191a25573e25
d.rogozhny@gmail.com <d.rogozhny@gmail.com>
2018-06-27 18:20:26 +0300

    Set definitions by value

--- types/mongodb-memory-server/index.d.ts
+++ types/mongodb-memory-server/index.d.ts
@@ -1,6 +1,6 @@
 // Type definitions for mongodb-memory-server 1.8
 // Project: https://github.com/nodkz/mongodb-memory-server
-// Definitions by: My Self <https://github.com/me>
+// Definitions by: Dmitry Rogozhny <https://github.com/dmitryrogozhny>
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
 // TypeScript Version: 2.8
 
