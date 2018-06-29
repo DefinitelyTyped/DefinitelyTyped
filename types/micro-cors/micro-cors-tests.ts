1858c7c5aefb061a0ef5fcffcb265890263831a4
Callum Denby <me@callumdenby.com>
2018-06-29 16:24:44 +0100

    Fix linting

--- types/micro-cors/micro-cors-tests.ts
+++ types/micro-cors/micro-cors-tests.ts
@@ -1,25 +1,23 @@
 import cors = require('micro-cors');
 import micro from 'micro';
 
-namespace micro_cors_tests {
-    const handler = cors();
+const handler = cors();
 
-    cors({ allowMethods: ['PUT', 'POST'] })
+cors({ allowMethods: ['PUT', 'POST'] });
 
-    cors({
-        maxAge: '1000',
-        origin: '*',
-        allowHeaders: [],
-        allowMethods: [],
-        exposeHeaders: []
-    })
+cors({
+    maxAge: '1000',
+    origin: '*',
+    allowHeaders: [],
+    allowMethods: [],
+    exposeHeaders: []
+});
 
-    handler(async (req, res) => {})
-    handler(async (req, res) => 'data')
+handler(async (req, res) => {});
+handler(async (req, res) => 'data');
 
-    handler((req, res) => {})
-    handler((req, res) => Promise.resolve('data'))
-    handler((req, res) => 'data')
+handler((req, res) => {});
+handler((req, res) => Promise.resolve('data'));
+handler((req, res) => 'data');
 
-    micro(handler((req, res) => {}))
-}
+micro(handler((req, res) => {}));
