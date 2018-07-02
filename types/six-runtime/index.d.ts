b43070cf1cc55b6dfb5e75badbbf3b4085f42333
李勇 <liyong@liyongdeMacBook-Pro.local>
2018-07-02 14:33:52 +0800

    style

--- types/six-runtime/index.d.ts
+++ types/six-runtime/index.d.ts
@@ -1,4 +1,4 @@
-// Type definitions for six-runtime 0.0.1
+// Type definitions for six-runtime 0.1
 // Project: https://github.com/493636333/six
 // Definitions by: liyong <https://github.com/493636333>
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
@@ -10,223 +10,211 @@ interface Six {
     /**
      * process.env.NODE_ENV
      */
-    readonly NODE_ENV: string
+    readonly NODE_ENV: string;
 
     /**
      * NODE_ENV的缩短版
-     * 
+     *
      * production、prod => prod
-     * 
+     *
      * dev、develop、development => dev
      */
-    readonly ENV: string
+    readonly ENV: string;
 
     /**
      * Six的rootPath
      */
-    readonly ROOT_PATH: string
+    readonly ROOT_PATH: string;
 
     /**
      * Six的plugin的map
      */
-    readonly plugins: Six.PluginsMap
+    readonly plugins: Six.PluginsMap;
 
     /**
      * 针对这次请求生效的数据缓存 set
      */
-    set(key: string, value: any): void
+    set(key: string, value: any): void;
 
     /**
      * 针对这次请求生效的数据缓存 get
      */
-    get(key: string): any
+    get(key: string): any;
 
     /**
      * DataProvider类，所有的data-provider必须继承这个类
      */
-    DataProvider: Six.plugins.dataProvider.DataProvider
+    DataProvider: Six.plugins.dataProvider.DataProvider;
 
     /**
      * 执行某个data-provider，并返回执行之后的值
      */
-    dp(name: string): any
+    dp(name: string): any;
 
     /**
      * 发送请求的工具方法，会将用户的cookie和traceId透传过去
      */
-    http: Six.plugins.http
+    http: Six.plugins.http;
 
     /**
      * 打日志方法
      */
-    log: Six.plugins.log
+    log: Six.plugins.log;
 
     /**
      * rpc调用工具方法，返回ThriftPool实例
      */
-    rpc: Six.plugins.rpc
+    rpc: Six.plugins.rpc;
 
     /**
      * 时间统计工具
      */
-    time: Six.plugins.time.timmer
+    time: Six.plugins.time.timmer;
 
-    render: Six.plugins.view.render
+    /**
+     * 渲染函数
+     */
+    render: Six.plugins.view.render;
 }
 
 declare namespace Six {
     interface PluginsMap {
-        sso: plugins.sso.ssoFactory
-        upm: plugins.upm.upmFactory
-        uac: plugins.uac.uacFactory,
-        static: plugins.static.staticFactory
+        sso: plugins.sso.ssoFactory;
+        upm: plugins.upm.upmFactory;
+        uac: plugins.uac.uacFactory;
+        static: plugins.static.staticFactory;
     }
 
-    interface next {
-        (): any
-    }
+    type next = () => any
 
-    interface middleware {
-        (ctx: Object, next: next): any
-    }
+    type middleware = (ctx: object, next: next) => any
 
     namespace plugins{
-        interface whiteList {
-            (ctx: Object): boolean
-        }
+        type whiteList = (ctx: Object) => boolean;
+
         namespace sso {
-            interface ssoFactory {
-                (conf: ssoConf): any
-            }
+            type ssoFactory = (conf: ssoConf) => any;
+
             interface ssoConf {
-                whiteList: string|RegExp|Array<string>|whiteList
-                ssoConf: ssoClientConf
-                enableCache: boolean
-                hasLoginCacheMaxAge: number
-                adapter(user: Object): Object
+                whiteList: string|RegExp|string[]|whiteList;
+                ssoConf: ssoClientConf;
+                enableCache: boolean;
+                hasLoginCacheMaxAge: number;
+                adapter(user: object): object;
             }
             interface ssoClientConf {
-                clientId: string
-                secret: string
-                env: string
-                token: token
-                protocol: string,
-                errorHook(error: Error): void
-                callbackUrl: string
+                clientId: string;
+                secret: string;
+                env: string;
+                token: token;
+                protocol: string;
+                errorHook(error: Error): void;
+                callbackUrl: string;
             }
 
             interface token {
-                name: string
-                path: string
-                httpOnly: boolean
-                expiredTime: number
+                name: string;
+                path: string;
+                httpOnly: boolean;
+                expiredTime: number;
             }
         }
 
         namespace upm {
-            interface upmFactory {
-                (conf: upmConf): any
-            }
+            type upmFactory = (conf: upmConf) => any;
+
             interface upmConf {
-                whiteList: string|RegExp|Array<string>|whiteList
-                enableMock: boolean
-                domain: string
-                path: string
-                clientId: string
-                clientSecret: string
-                skipOffline: boolean
-                failHook(err: Error, ctx: Object, next: next): any
-                getPath(ctx: Object): string
+                whiteList: string|RegExp|string[]|whiteList;
+                enableMock: boolean;
+                domain: string;
+                path: string;
+                clientId: string;
+                clientSecret: string;
+                skipOffline: boolean;
+                failHook(err: Error, ctx: object, next: next): any;
+                getPath(ctx: object): string;
             }
         }
 
         namespace uac {
-            interface uacFactory {
-                (conf: uacConf): any;
-            }
+            type uacFactory = (conf: uacConf) => any;
             interface uacConf {
-                whiteList: string|RegExp|Array<string>|whiteList
-                enableMock: boolean
-                client_id: string
-                client_secret: string
-                skipOffline: boolean
-                failHook(err: Error, ctx: Object, next: next): any
-                getPath(ctx: Object): string
+                whiteList: string|RegExp|string[]|whiteList;
+                enableMock: boolean;
+                client_id: string;
+                client_secret: string;
+                skipOffline: boolean;
+                failHook(err: Error, ctx: object, next: next): any;
+                getPath(ctx: object): string;
             }
         }
 
         namespace dataProvider {
             abstract class DataProvider {
-                static deps: Array<string>
-                static timeout: number
-                static retry: number
-                prepare(ctx: Object, depsRes: Object):any
-                abstract execute(ctx: Object, args: any): any
+                static deps: string[];
+                static timeout: number;
+                static retry: number;
+                prepare(ctx: object, depsRes: object):any;
+                abstract execute(ctx: object, args: any): any;
             }
         }
 
         interface http {
-            proxy(options: Object): middleware
-            get(url: any, options?: Object): Promise<any>
-            post(url: any, options?: Object): Promise<any>
-            put(url: any, options?: Object): Promise<any>
-            patch(url: any, options?: Object): Promise<any>
-            del(url: any, options?: Object): Promise<any>
-            head(url: any, options?: Object): Promise<any>
+            proxy(options: object): middleware;
+            get(url: any, options?: object): Promise<any>;
+            post(url: any, options?: object): Promise<any>;
+            put(url: any, options?: object): Promise<any>;
+            patch(url: any, options?: object): Promise<any>;
+            del(url: any, options?: object): Promise<any>;
+            head(url: any, options?: object): Promise<any>;
         }
 
         interface log {
-            debug(msg: string, ...restOfVar: any[]):void
-            info(msg: string, ...restOfVar: any[]):void
-            warn(msg: string, ...restOfVar: any[]):void
-            error(msg: string|Error, ...restOfVar: any[]):void
+            debug(msg: string, ...restOfVar: any[]):void;
+            info(msg: string, ...restOfVar: any[]):void;
+            warn(msg: string, ...restOfVar: any[]):void;
+            error(msg: string|Error, ...restOfVar: any[]):void;
         }
 
-        interface rpc {
-            (config: Object): Object
-        }
+        type rpc = (config: Object) => Object;
 
         namespace static {
             interface staticFactory {
-                (conf: staticConf): middleware
+                (conf: staticConf): middleware;
             }
             interface staticConf {
-                rootPath: string
-                cacheConf: cacheConf
+                rootPath: string;
+                cacheConf: cacheConf;
             }
 
             interface cacheConf {
-                defer: boolean
-                cache: boolean
-                maxAge: number
-                index: string
-                gzip: boolean
-                extensions: boolean|Array<string>
+                defer: boolean;
+                cache: boolean;
+                maxAge: number;
+                index: string;
+                gzip: boolean;
+                extensions: boolean|string[];
             }
         }
 
         namespace time {
             interface timmer {
-                now(): any
-                start(key: string): void
-                end(key: string): number,
-                serialize(between?: string, end?: string, filter?: filter): string
+                now(): any;
+                start(key: string): void;
+                end(key: string): number;
+                serialize(between?: string, end?: string, filter?: filter): string;
             }
 
-            interface filter {
-                (key: string): boolean
-            }
+            type filter = (key: string) => boolean;
         }
 
         namespace view {
-            interface render {
-                (name: string, runtimeConf?: runtimeConf|void, data?: Object): Promise<any>
-            }
+            type render = (name: string, runtimeConf?: runtimeConf|void, data?: Object) => Promise<any>;
 
             interface runtimeConf {
-                codeKey: string,
-                msgKey: string,
-                dataKey: string
+                codeKey: string;
+                msgKey: string;
+                dataKey: string;
             }
         }
     }
