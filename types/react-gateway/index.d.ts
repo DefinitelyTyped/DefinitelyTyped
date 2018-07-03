871372d76865913f39483585fb92133c2b29e2f4
Jason Unger <jason@rocketrip.com>
2018-07-03 17:13:13 -0400

    Remove unnecessary lib folder

--- types/react-gateway/index.d.ts
+++ types/react-gateway/index.d.ts
@@ -4,4 +4,18 @@
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
 // TypeScript Version: 2.6
 
-export * from './lib';
+import Gateway = require('./Gateway');
+import { GatewayProps } from './Gateway';
+import GatewayDest = require('./GatewayDest');
+import { GatewayDestProps } from './GatewayDest';
+import GatewayProvider = require('./GatewayProvider');
+import GatewayRegistry = require('./GatewayRegistry');
+
+export {
+    Gateway,
+    GatewayProps,
+    GatewayDest,
+    GatewayDestProps,
+    GatewayProvider,
+    GatewayRegistry
+};
