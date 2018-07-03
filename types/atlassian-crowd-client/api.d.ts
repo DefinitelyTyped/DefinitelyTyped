7dab3a1d50a4eaff78dedc80d31a6ea225f0c823
Mohamed Hegazy <mhegazy@microsoft.com>
2018-07-03 09:48:17 -0700

    Use `export =` instead of `export default`

--- types/atlassian-crowd-client/api.d.ts
+++ types/atlassian-crowd-client/api.d.ts
@@ -1,8 +1,10 @@
 import { Settings } from "./settings";
 
-export default class CrowdApi {
+declare class CrowdApi {
     settings: Settings;
     constructor(settings: Settings);
     request(method: string, path: string, data?: any): Promise<any>;
     log(...args: any[]): void;
 }
+
+export = CrowdApi;
