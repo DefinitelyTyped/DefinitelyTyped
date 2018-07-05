b6a2180e224c1212fc0d8720541d080e3d4ae16d
YangusKhan <9inchsamurai@gmail.com>
2018-07-05 13:50:08 -0600

    missed renaming the package in comment

--- types/slate-base64-serializer/index.d.ts
+++ types/slate-base64-serializer/index.d.ts
@@ -1,4 +1,4 @@
-// Type definitions for slate-html-serializer 0.2
+// Type definitions for slate-base64-serializer 0.2
 // Project: https://github.com/ianstormtaylor/slate
 // Definitions by: Brandon Shelton <https://github.com/YangusKhan>
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
