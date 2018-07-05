9dd412a6e337ea7a58e5d8c757a018cf145671ca
Andrey Tselischev <vorteq@gmail.com>
2018-07-05 16:37:44 +0300

    [@types/bson] Changed header version number to 1.0.6

--- types/bson/index.d.ts
+++ types/bson/index.d.ts
@@ -1,4 +1,4 @@
-// Type definitions for bson 1.0.4
+// Type definitions for bson 1.0.6
 // Project: https://github.com/mongodb/js-bson
 // Definitions by: Hiroki Horiuchi <https://github.com/horiuchi>
 //                 Federico Caselli <https://github.com/CaselIT>
