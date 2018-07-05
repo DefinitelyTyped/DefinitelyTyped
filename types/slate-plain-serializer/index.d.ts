74b3e7e57500b83cb6f70b8581641b97cab928d9
YangusKhan <9inchsamurai@gmail.com>
2018-07-05 13:41:53 -0600

    added plain, html, and base64 serializer definitions

---
+++ types/slate-plain-serializer/index.d.ts
@@ -0,0 +1,14 @@
+// Project: https://github.com/ianstormtaylor/slate
+// Definitions by: Brandon Shelton <https://github.com/YangusKhan>
+// Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
+// TypeScript Version: 2.6
+import { Slate } from 'slate';
+
+export interface DeserializeOptions {
+    toJson?: boolean;
+    defaultBlock?: Slate.BlockProperties;
+    defaultMarks?: Slate.MarkProperties[] | Set<Slate.MarkProperties>;
+}
+
+export function deserialize(string: string, options?: DeserializeOptions): Slate.Value;
+export function serialize(value: Slate.Value): string;
