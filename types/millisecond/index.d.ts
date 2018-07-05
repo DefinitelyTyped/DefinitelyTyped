ec3d9503168d6ea7d4f07b9cbf2db64038374ff4
Aayush Kapoor <xeoneux@icloud.com>
2018-07-05 18:08:27 +0530

    Update definition

--- types/millisecond/index.d.ts
+++ types/millisecond/index.d.ts
@@ -3,5 +3,8 @@
 // Definitions by: Aayush Kapoor <https://github.com/xeoneux>
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
 
-export function millisecond(ms: string): number;
-export function millisecond(ms: number): number;
+declare function millisecond(ms: string | number): number;
+
+declare namespace millisecond { }
+
+export = millisecond;
--- types/millisecond/millisecond-tests.ts
+++ types/millisecond/millisecond-tests.ts
@@ -1,4 +1,4 @@
-import { millisecond } from ".";
+import * as millisecond from "millisecond";
 
 millisecond(100);
-millisecond("1years");
+millisecond("1 years");
