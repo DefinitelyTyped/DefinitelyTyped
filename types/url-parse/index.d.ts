634003a7952a6539a414b5c75804c63a712cbb6d
Hari Sivaramakrishnan <harisiva@users.noreply.github.com>
2018-07-07 16:15:24 -0700

    Add to definitions by list

--- types/url-parse/index.d.ts
+++ types/url-parse/index.d.ts
@@ -1,6 +1,7 @@
 // Type definitions for url-parse 1.1
 // Project: https://github.com/unshiftio/url-parse
 // Definitions by: Pavlo Chernenko <https://github.com/ChernenkoPaul>
+//                 Hari Sivaramakrishnan <https://github.com/harisiva>
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
 // TypeScript Version: 2.2
 
