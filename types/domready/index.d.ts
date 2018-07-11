057213556c08fcea08cca5edbf45c28af276452d
Daniel Cassidy <mail@danielcassidy.me.uk>
2018-07-11 04:05:17 +0100

    Set domready version number.
    domready types are up to date as of domready 1.0.8.

--- types/domready/index.d.ts
+++ types/domready/index.d.ts
@@ -1,4 +1,4 @@
-// Type definitions for domready
+// Type definitions for domready 1.0
 // Project: https://github.com/ded/domready
 // Definitions by: Christian Holm Nielsen <https://github.com/dotnetnerd>
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
