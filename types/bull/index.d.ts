b3ab90383ed8bbca78d152558699e340c062af2f
Luke Autry <lukeautry@gmail.com>
2018-07-12 12:33:49 -0400

    remove patch version from header

--- types/bull/index.d.ts
+++ types/bull/index.d.ts
@@ -1,4 +1,4 @@
-// Type definitions for bull 3.3.12
+// Type definitions for bull 3.3
 // Project: https://github.com/OptimalBits/bull
 // Definitions by: Bruno Grieder <https://github.com/bgrieder>
 //                 Cameron Crothers <https://github.com/JProgrammer>
