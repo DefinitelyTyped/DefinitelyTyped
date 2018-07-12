548473f28b794b9d80b5338252d92e060e9b2fcb
segayuu <segayuu@gmail.com>
2018-07-12 18:35:17 +0900

    move testfile Reason: There are too many type definition files   depending on jquery and it can not be managed.

--- types/bluebird-global/test/jquery-tests.ts
+++ types/bluebird-global/test/jquery-tests.ts
@@ -1,4 +1,4 @@
-/// <reference types="bluebird-global" />
+/// <reference types="jquery" />
 
 // Pulls in bluebird-global to test compatibility.
 // Fixes https://github.com/DefinitelyTyped/DefinitelyTyped/issues/26328.
