d44184757ab36f27420bc17ee2c6ddbdf8e1ded9
Ryan Friedman <ryan.friedman@ni.com>
2018-07-13 13:56:38 -0500

    Updating Segment class to better reflect 11.5 documentation and implementation.

--- types/paper/index.d.ts
+++ types/paper/index.d.ts
@@ -2572,7 +2572,7 @@ declare module paper {
          *        1.0: the chordal Catmull-Rom spline
          *   'geometric' use a simple heuristic and empiric geometric method to smooth the segment’s handles. The handles were weighted, meaning that big differences in distances between the segments will lead to probably undesired results.
          *      The optionally passed factor defines the tension parameter (0…1), controlling the amount of smoothing as a factor by which to scale each handle.
-         * @param options [optional] - default { type:'asymmetric', factor: 0.5 for 'catmull-rom', 0.4 for 'geometric }
+         * @param options [optional] - default { type:'asymmetric', factor: 0.5 for 'catmull-rom', 0.4 for 'geometric' }
          * @param options.type - the type of smoothing method: ‘continuous’, ‘asymmetric’, ‘catmull-rom’, ‘geometric’ — default: ‘asymmetric’
          * @param options.factor - the factor parameterizing the smoothing method — default: 0.5 for 'catmull-rom', 0.4 for 'geometric'
          * @param options.from - the segment or curve at which to start smoothing, if not the full path shall be smoothed (inclusive). This can either be a segment index, or a segment or curve object that is part of the path. If the passed number is negative, the index is wrapped around the end of the path.
@@ -3195,11 +3195,6 @@ declare module paper {
         handleOut: Point;
 
         /**
-         * Specifies whether the segment has no handles defined, meaning it connects two straight lines.
-         */
-        linear: boolean;
-
-        /**
          * Specifies whether the point of the segment is selected.
          */
         selected: boolean;
@@ -3208,48 +3203,83 @@ declare module paper {
          * The index of the segment in the path.segments array that the segment belongs to.
          * Read Only
          */
-        index: number;
+        readonly index: number;
 
         /**
          * The path that the segment belongs to.
          * Read Only
          */
-        path: Path;
+        readonly path: Path;
 
         /**
          * The curve that the segment belongs to. For the last segment of an open path, the previous segment is returned.
          * Read only.
          */
-        curve: Curve;
+        readonly curve: Curve;
 
         /**
          * The curve location that describes this segment's position ont the path.
          * Read Only.
          */
-        location: CurveLocation;
+        readonly location: CurveLocation;
 
         /**
          * The next segment in the path.segments array that the segment belongs to. If the segments belongs to a closed path, the first segment is returned for the last segment of the path.
          * Read Only.
          */
-        next: Segment;
+        readonly next: Segment;
 
         /**
          * The previous segment in the path.segments array that the segment belongs to. If the segments belongs to a closed path, the last segment is returned for the first segment of the path.
          * Read Only.
          */
-        previous: Segment;
+        readonly previous: Segment;
 
         /**
-         * Returns true if the the two segments are the beginning of two lines and if these two lines are running parallel.
-         * @param segment
+         * Checks if the segment has any curve handles set.
          */
-        isColinear(segment: Segment): boolean;
+        hasHandles(): boolean;
 
         /**
-         * Returns true if the segment at the given index is the beginning of an orthogonal arc segment. The code looks at the length of the handles and their relation to the distance to the imaginary corner point. If the relation is kappa, then it's an arc.
+         * Checks if the segment connects two curves smoothly, meaning that its two handles are collinear and segment does not form a corner.
          */
-        isArc(): boolean;
+        isSmooth(): boolean;
+
+        /**
+         * Clears the segment’s handles by setting their coordinates to zero, turning the segment into a corner.
+         */
+        clearHandles(): void;
+
+        /**
+         * Smooths the bezier curves that pass through this segment by taking into account the segment’s position and distance to the neighboring segments and changing the direction and length of the segment’s handles accordingly without moving the segment itself.
+         * Two different smoothing methods are available:
+         *   'catmull-rom' uses the Catmull-Rom spline to smooth the segment.
+         *      The optionally passed factor controls the knot parametrization of the algorithm:
+         *        0.0: the standard, uniform Catmull-Rom spline
+         *        0.5: the centripetal Catmull-Rom spline, guaranteeing no self-intersections
+         *        1.0: the chordal Catmull-Rom spline
+         *   'geometric' use a simple heuristic and empiric geometric method to smooth the segment’s handles. The handles were weighted, meaning that big differences in distances between the segments will lead to probably undesired results.
+         * The optionally passed factor defines the tension parameter (0…1), controlling the amount of smoothing as a factor by which to scale each handle.
+         * @param options [optional] - the smoothing options. default { type: 'catmull-rom', factor: 0.5 for 'catmull-rom', 0.4 for 'geometric'}
+         * @param options.type - the type of smoothing method: ‘catmull-rom’, ‘geometric’
+         * @param options.factor - the factor parameterizing the smoothing method — default: 0.5 for 'catmull-rom', 0.4 for 'geometric'
+         */
+        smooth(options?:{ type?: string; factor?: number; }): void;
+
+        /**
+         * Checks if the this is the first segment in the path.segments array.
+         */
+        isFirst(): boolean;
+
+        /**
+         * Checks if the this is the last segment in the path.segments array.
+         */
+        isLast(): boolean;
+
+        /**
+         * Returns the reversed the segment, without modifying the segment itself.
+         */
+        reversed(): Segment;
 
         /**
          * Returns the reversed the segment, without modifying the segment itself.
@@ -3271,6 +3301,14 @@ declare module paper {
          * @param matrix - the matrix to transform the segment by
          */
         transform(matrix: Matrix): void;
+
+        /**
+         * Interpolates between the two specified segments and sets the point and handles of this segment accordingly.
+         * @param from - the segment defining the geometry when factor is 0
+         * @param to - the segment defining the geometry when factor is 1
+         * @param factor - the interpolation coefficient, typically between 0 and 1, but extrapolation is possible too
+         */
+        interpolate(from: Segment, to: Segment, factor: number): void;
 
     }
     /**
