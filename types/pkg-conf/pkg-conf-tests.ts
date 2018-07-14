a733b838e0efaa0020e053e5dabaffcd13173502
Jorge Gonzalez <jrgqhd@gmail.com>
2018-07-14 12:47:54 -0400

    Remove fixture

--- types/pkg-conf/pkg-conf-tests.ts
+++ types/pkg-conf/pkg-conf-tests.ts
@@ -1,9 +1,6 @@
 import * as pkgConf from 'pkg-conf';
 
-const options = { cwd: './fixture' };
-
-pkgConf('pony', options); // $ExpectType Promise<JsonMap>
-const config = pkgConf.sync('pony', options); // $ExpectType JsonMap
+pkgConf('name'); // $ExpectType Promise<JsonMap>
+const config = pkgConf.sync('bugs'); // $ExpectType JsonMap
 pkgConf.filepath(config); // $ExpectType string | null
-config.foo; // $ExpectType AnyJson
-config.baz; // $ExpectType AnyJson
+config.url; // $ExpectType AnyJson
