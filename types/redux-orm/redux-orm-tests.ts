bbb524bd87f706adfcd417216c9078131f161494
Ahmed T. Ali <ah.tajelsir@gmail.com>
2018-07-14 20:22:54 +0300

    chore(@types/redux-orm): revert formating changes

--- types/redux-orm/redux-orm-tests.ts
+++ types/redux-orm/redux-orm-tests.ts
@@ -1,45 +1,35 @@
-import {
-    attr,
-    createSelector as createSelectorORM,
-    ORMCommonState,
-    ORMId,
-    QuerySet,
-    TableState,
-    SessionWithModels,
-    Model,
-    ORM
-} from 'redux-orm';
+import { attr, createSelector as createSelectorORM, ORMCommonState, ORMId, QuerySet, TableState, SessionWithModels, Model, ORM } from 'redux-orm';
 
 // Model
 export class Test extends Model<TestStateItem, FetchIndicatorState> {
-    static modelName = 'Test';
+  static modelName = 'Test';
 
-    static fields = {
-        test: attr(),
-        isFetching: attr({ getDefault: () => false }),
-        id: attr()
-    };
+  static fields = {
+    test: attr(),
+    isFetching: attr({ getDefault: () => false }),
+    id: attr()
+  };
 }
 
 // core data which we do not have defaults for
 export interface TestStateItem {
-    test: string;
+  test: string;
 }
 
 // optional data we provide defaults for
 export interface FetchIndicatorState {
-    isFetching: boolean;
+  isFetching: boolean;
 }
 
 // id attr is added automatically by redux-orm therefore we have IORMId interface
 export type TestState = TableState<TestStateItem & ORMId & FetchIndicatorState>;
 
 export interface TestORMState extends ORMCommonState {
-    Test: TestState;
+  Test: TestState;
 }
 
 interface TestORMModels {
-    Test: typeof Test;
+  Test: typeof Test;
 }
 
 const orm = new ORM<TestORMState>();
@@ -47,50 +37,42 @@ orm.register<TestORMModels>(Test);
 
 // Reducer
 interface TestDTO {
-    test: string;
+  test: string;
 }
 
 interface Action<P> {
-    type: string;
-    payload: P;
+  type: string;
+  payload: P;
 }
 
-const reducerAddItem = (
-    state: TestORMState,
-    action: Action<TestDTO>
-): TestORMState => {
-    const session = orm.session(state);
-    session.Test.upsert<TestStateItem>(action.payload);
-    session.Test.idExists('id');
-    return session.state;
+const reducerAddItem = (state: TestORMState, action: Action<TestDTO>): TestORMState => {
+  const session = orm.session(state);
+  session.Test.upsert<TestStateItem>(action.payload);
+  return session.state;
 };
 
 // Selector
 interface TestDisplayItem {
-    test: string;
+  test: string;
 }
 type TestDisplayItemList = TestDisplayItem[];
 
 // Just for the example below. Use real createSelector from reselect in your app
-const createSelector = <S, P1, R>(
-    param1Creator: (state: S) => P1,
-    combiner: (param1: P1) => R
-): ((state: S) => R) => state => combiner(param1Creator(state));
+const createSelector = <S, P1, R>(param1Creator: (state: S) => P1, combiner: (param1: P1) => R): (state: S) => R => (state) =>
+  combiner(param1Creator(state));
 
 interface RootState {
-    test: TestORMState;
+  test: TestORMState;
 }
 
 export const makeGetTestDisplayList = () => {
-    const ormSelector = createSelectorORM<TestORMState>(
-        orm,
-        (session: SessionWithModels<TestORMState>) =>
-            (session.Test.all() as QuerySet<TestStateItem, FetchIndicatorState>)
-                .toRefArray()
-                .map(item => ({ ...item }))
-    );
-    return createSelector<RootState, TestORMState, TestDisplayItemList>(
-        ({ test }) => test,
-        ormSelector
-    );
+  const ormSelector = createSelectorORM<TestORMState>(orm, (session: SessionWithModels<TestORMState>) =>
+    (session.Test.all() as QuerySet<TestStateItem, FetchIndicatorState>)
+      .toRefArray()
+      .map((item) => ({ ...item }))
+  );
+  return createSelector<RootState, TestORMState, TestDisplayItemList>(
+    ({ test }) => test,
+    ormSelector
+  );
 };
