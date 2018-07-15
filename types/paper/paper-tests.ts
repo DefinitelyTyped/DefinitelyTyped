fd5da1ff4b34c19d188787a34dc0bd4db71d878e
Ryan Friedman <ryan.friedman@ni.com>
2018-07-15 18:29:41 -0500

    Adding the Example 'Boolean Operations' rewritten as typescript to the tests. This hopefully mimics what a user might have done to adapt the examples and ensure that all the example interfaces are implemented correctly.

--- types/paper/paper-tests.ts
+++ types/paper/paper-tests.ts
@@ -79,4 +79,128 @@ hitTestResults = paper.project.hitTestAll(hitTestPoint, hitOptionsInterfaceEmpty
 hitTestResults = paper.project.hitTestAll(hitTestPoint, hitOptionsInterfacePartial);
 hitTestResults = paper.project.hitTestAll(hitTestPoint, hitOptionsInterfaceFull);
 
-
+function Examples() {
+    function BooleanOperations(){
+        let text = new paper.PointText({
+            position: paper.view.center.add(new paper.Point([0, 200])),
+            fillColor: 'black',
+            justification: 'center',
+            fontSize: 20
+        });
+        
+        let originals = new paper.Group({ insert: false }); // Don't insert in DOM.
+        
+        let square = new paper.Path.Rectangle({
+            position: paper.view.center,
+            size: 300,
+            parent: originals,
+            fillColor: 'white'
+        });
+        
+        // Make a ring using subtraction of two circles:
+        let inner = new paper.Path.Circle({
+            center: paper.view.center,
+            radius: 100,
+            parent: originals,
+            fillColor: 'white'
+        });
+        
+        let outer = new paper.Path.Circle({
+            center: paper.view.center,
+            radius: 140,
+            parent: originals,
+            fillColor: 'white'
+        });
+        
+        let ring = outer.subtract(inner);
+        
+        let operations = ['unite', 'intersect', 'subtract', 'exclude', 'divide'];
+        let colors = ['red', 'green', 'blue', 'black'];
+        let curIndex = -1;
+        let operation: string;
+        let result: paper.PathItem | null;
+        let activeItem: paper.PathItem | null;
+        
+        // Change the mode every 3 seconds:
+        setInterval(setMode, 3000);
+        
+        // Set the initial mode:
+        setMode();
+        
+        function setMode() {
+            curIndex++;
+            if (curIndex == operations.length * 2)
+                curIndex = 0;
+            operation = operations[curIndex % operations.length];
+        }
+        
+        function onMouseDown(event: paper.ToolEvent) {
+            let hitResult = originals.hitTest(event.point);
+            activeItem = hitResult && hitResult.item as paper.PathItem;
+        }
+        
+        function onMouseDrag(event: paper.ToolEvent) {
+            if (activeItem)
+                activeItem.position = event.point;
+        }
+        
+        function onMouseUp() {
+            activeItem = null;
+            square.position = paper.view.center;
+        }
+        
+        function onFrame(event: paper.IFrameEvent) {
+            if (activeItem != ring) {
+                // Move the ring around:
+                let offset = new paper.Point(140, 80).multiply(new paper.Point([Math.sin(event.count / 60), Math.sin(event.count / 40)]));
+                ring.position = paper.view.center.add(offset);
+            }
+        
+            // Remove the result of the last path operation:
+            if (result)
+                result.remove();
+        
+            function performOperation(operation:string, primaryItem:paper.PathItem, secondaryItem:paper.PathItem){
+                switch(operation){
+                    case 'unite':
+                        return primaryItem.unite(secondaryItem);
+                    case 'intersect':
+                        return primaryItem.intersect(secondaryItem);
+                    case 'subtract':
+                        return primaryItem.subtract(secondaryItem);
+                    case 'exclude':
+                        return primaryItem.exclude(secondaryItem);
+                    case 'divide':
+                        return primaryItem.divide(secondaryItem);
+                    default:
+                        return null;
+                }
+            }
+            // Perform the path operation on the ring:
+            if (curIndex < operations.length) {
+                result = performOperation(operation, square, ring);
+                text.content = 'square.' + operation + '(ring)';
+            } else {
+                result = performOperation(operation, square, ring);
+                text.content = 'ring.' + operation + '(square)';
+            }
+            if(result){
+                result.selected = true;
+                result.fillColor = colors[curIndex % colors.length];
+                result.moveBelow(text);
+            }
+        
+            // If the result is a group, color each of its children differently:
+            if (result instanceof paper.Group) {
+                for (let i = 0; i < result.children.length; i++) {
+                    result.children[i].fillColor = colors[i];
+                }
+            }
+        };
+        
+        function onResize() {
+            text.position = paper.view.center.add(new paper.Point([0, 200]));
+            square.position = paper.view.center;
+        }
+    }
+}
