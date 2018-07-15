8889e5cff032aef3538a019c4941c3f1b1f1aab8
Ryan Friedman <ryan.friedman@ni.com>
2018-07-15 18:45:24 -0500

    Adding the Example 'Candy Crash' rewritten as typescript to the tests. This hopefully mimics what a user might have done to adapt the examples and ensure that all the example interfaces are implemented correctly.

--- types/paper/paper-tests.ts
+++ types/paper/paper-tests.ts
@@ -203,4 +203,142 @@ function Examples() {
             square.position = paper.view.center;
         }
     }
+
+    function CandyCrash(){
+        // kynd.info 2014
+        class Ball{
+            radius: number;
+            point: paper.Point;
+            vector: paper.Point;
+            maxVec: number;
+            numSegment: number;
+            boundOffset: number[];
+            boundOffsetBuff: number[];
+            sidePoints: paper.Point[];
+            path: paper.Path;
+            
+            constructor(r: number, p: paper.Point, v: paper.Point) {
+                this.radius = r;
+                this.point = p;
+                this.vector = v;
+                this.maxVec = 15;
+                this.numSegment = Math.floor(r / 3 + 2);
+                this.boundOffset = [];
+                this.boundOffsetBuff = [];
+                this.sidePoints = [];
+                this.path = new paper.Path({
+                    fillColor: {
+                        hue: Math.random() * 360,
+                        saturation: 1,
+                        brightness: 1
+                    },
+                    blendMode: 'lighter'
+                });
+
+                for (let i = 0; i < this.numSegment; i ++) {
+                    this.boundOffset.push(this.radius);
+                    this.boundOffsetBuff.push(this.radius);
+                    this.path.add(new paper.Point({}));
+                    this.sidePoints.push(new paper.Point({
+                        angle: 360 / this.numSegment * i,
+                        length: 1
+                    }));
+                }
+            }
+            iterate() {
+                this.checkBorders();
+                if (this.vector.length > this.maxVec)
+                    this.vector.length = this.maxVec;
+                this.point = this.point.add(this.vector);
+                this.updateShape();
+            }
+            checkBorders() {
+                let size = paper.view.size;
+                if (this.point.x < -this.radius)
+                    this.point.x = size.width + this.radius;
+                if (this.point.x > size.width + this.radius)
+                    this.point.x = -this.radius;
+                if (this.point.y < -this.radius)
+                    this.point.y = size.height + this.radius;
+                if (this.point.y > size.height + this.radius)
+                    this.point.y = -this.radius;
+            }
+            updateShape() {
+                let segments = this.path.segments;
+                for (let i = 0; i < this.numSegment; i ++)
+                    segments[i].point = this.getSidePoint(i);
+
+                this.path.smooth();
+                for (let i = 0; i < this.numSegment; i ++) {
+                    if (this.boundOffset[i] < this.radius / 4)
+                        this.boundOffset[i] = this.radius / 4;
+                    let next = (i + 1) % this.numSegment;
+                    let prev = (i > 0) ? i - 1 : this.numSegment - 1;
+                    let offset = this.boundOffset[i];
+                    offset += (this.radius - offset) / 15;
+                    offset += ((this.boundOffset[next] + this.boundOffset[prev]) / 2 - offset) / 3;
+                    this.boundOffsetBuff[i] = this.boundOffset[i] = offset;
+                }
+            }
+            react(b:Ball) {
+                let dist = this.point.getDistance(b.point);
+                if (dist < this.radius + b.radius && dist != 0) {
+                    let overlap = this.radius + b.radius - dist;
+                    let direc = (this.point.subtract(b.point)).normalize(overlap * 0.015);
+                    this.vector = this.vector.add(direc);
+                    b.vector = b.vector.subtract(direc);
+
+                    this.calcBounds(b);
+                    b.calcBounds(this);
+                    this.updateBounds();
+                    b.updateBounds();
+                }
+            }
+            getBoundOffset(p:paper.Point) {
+                let diff = this.point.subtract(p);
+                let angle = (diff.angle + 180) % 360;
+                return this.boundOffset[Math.floor(angle / 360 * this.boundOffset.length)];
+            }
+            calcBounds(b:Ball) {
+                for (let i = 0; i < this.numSegment; i ++) {
+                    let tp = this.getSidePoint(i);
+                    let bLen = b.getBoundOffset(tp);
+                    let td = tp.getDistance(b.point);
+                    if (td < bLen) {
+                        this.boundOffsetBuff[i] -= (bLen  - td) / 2;
+                    }
+                }
+            }
+            getSidePoint(index: number) {
+                return this.point.add(this.sidePoints[index].multiply(this.boundOffset[index]));
+            }
+            updateBounds() {
+                for (let i = 0; i < this.numSegment; i ++)
+                    this.boundOffset[i] = this.boundOffsetBuff[i];
+            }
+        }
+        //--------------------- main ---------------------
+        let balls: Ball[] = [];
+        let numBalls = 18;
+        for (let i = 0; i < numBalls; i++) {
+            let position = paper.Point.random().multiply(new paper.Point(paper.view.size.width, paper.view.size.height));
+            let vector = new paper.Point({
+                angle: 360 * Math.random(),
+                length: Math.random() * 10
+            });
+            let radius = Math.random() * 60 + 60;
+            balls.push(new Ball(radius, position, vector));
+        }
+
+        function onFrame() {
+            for (let i = 0; i < balls.length - 1; i++) {
+                for (let j = i + 1; j < balls.length; j++) {
+                    balls[i].react(balls[j]);
+                }
+            }
+            for (let i = 0, l = balls.length; i < l; i++) {
+                balls[i].iterate();
+            }
+        }
+    }
 }
