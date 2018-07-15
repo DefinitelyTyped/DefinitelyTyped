8be741dae8c2314f73a04b71b10dc87693047d9f
Konstantin Simon Maria Möllers <ksm.moellers@gmail.com>
2018-07-16 01:35:17 +0200

    [webpagetest] Creating initial definitions

---
+++ types/webpagetest/webpagetest-tests.ts
@@ -0,0 +1,46 @@
+import WebPageTest = require('webpagetest');
+
+/* Examples from NPM */
+
+// 1. Instantiating
+const wpt = new WebPageTest('my-wpt.foo.com'); // default: www.webpagetest.org
+const wptPublic = new WebPageTest('www.webpagetest.org', 'MY_API_KEY');
+
+// 2. Get available locations
+wpt.getLocations((err, data) => {
+  console.log(err || data);
+});
+
+// 3. Run test on https://twitter.com/marcelduran from San Jose on IE9
+wpt.runTest('https://twitter.com/marcelduran', {location: 'SanJose_IE9'}, (err, data) => {
+  console.log(err || data);
+});
+
+// 4. Check current test status
+wpt.getTestStatus('121025_PT_N8K', (err, data) => {
+  console.log(err || data);
+});
+
+// 5. Get test results
+wpt.getTestResults('121025_PT_N8K', (err, data) => {
+  console.log(err || data);
+});
+
+// 6. Get test waterfall thumbnail from repeat view as data URI
+wpt.getWaterfallImage('121025_PT_N8K', {
+  thumbnail: true,
+  repeatView: true,
+  dataURI: true
+}, (err, data, info) => {
+  console.log(err || data, info);
+});
+
+// Run test on https://twitter.com/marcelduran and poll results every 5 seconds timing out in 60 seconds
+wpt.runTest('https://twitter.com/marcelduran', {pollResults: 5, timeout: 60}, (err, data) => {
+  console.log(err || data);
+});
+
+// Or run test on https://twitter.com/marcelduran and wait results listening on localhost* port 8000**
+wpt.runTest('https://twitter.com/marcelduran', {waitResults: 'localhost:8000'}, (err, data) => {
+  console.log(err || data);
+});
