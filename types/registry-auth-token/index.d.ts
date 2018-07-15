012fe4fd3aa0b6237a86037d2771b20ff110b6e7
Joseph Carroll <jdsalingerjr@gmail.com>
2018-07-15 16:27:09 -0400

    Fix: Clean-up test cases

--- types/registry-auth-token/index.d.ts
+++ types/registry-auth-token/index.d.ts
@@ -1,10 +1,9 @@
-// Type definitions for Registry Auth Token 3.3.2
+// Type definitions for Registry Auth Token 3.3
 // Project: https://github.com/rexxars/registry-auth-token
 // Definitions by: Maurice de Beijer <https://github.com/mauricedb>
 //                 James Liang <https://github.com/LiangZugeng>
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
 // TypeScript Version: 2.8
-
 
 /**
  * Simple namespace describing the inputs and outputs for `registry-auth-token`
