a65e077f5e9ad005929147a999bf24efdfaf8c89
Nicolás Serrano Arévalo <itnico.las.me@gmail.com>
2018-07-16 00:51:06 +0300

    Added Project Github URL

--- types/gestalt/index.d.ts
+++ types/gestalt/index.d.ts
@@ -1,5 +1,5 @@
 // Type definitions for gestalt 0.75
-// Project: https://pinterest.github.io/gestalt
+// Project: https://github.com/pinterest/gestalt
 // Definitions by: Nicolás Serrano Arévalo <https://github.com/serranoarevalo>
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
 // TypeScript Version: 2.6
