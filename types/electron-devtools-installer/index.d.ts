633bd8165bc94fe42b4500bf7a71c665d8387571
Bjørn Madsen <bm@aeons.dk>
2018-07-16 15:28:48 +0200

    Undo reformatting

--- types/electron-devtools-installer/index.d.ts
+++ types/electron-devtools-installer/index.d.ts
@@ -1,4 +1,4 @@
-// Type definitions for electron-devtools-installer v2.0.1
+// Type definitions for electron-devtools-installer v2.2.4
 // Project: https://github.com/MarshallOfSound/electron-devtools-installer
 // Definitions by: Robin Van den Broeck <https://github.com/gamesmaxed>
 //                 M. Fatih Mar <https://github.com/mfatihmar>
