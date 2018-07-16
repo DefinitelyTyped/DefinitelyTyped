bbad9b34432a061301f8c2320445e3f6414ca561
Ryan Friedman <ryan.friedman@ni.com>
2018-07-15 23:29:15 -0500

    Adding the Example 'Voronoi' rewritten as typescript to the tests. This hopefully mimics what a user might have done to adapt the examples and ensure that all the example interfaces are implemented correctly.

--- types/paper/paper-tests.ts
+++ types/paper/paper-tests.ts
@@ -321,7 +321,7 @@ function Examples() {
         let balls: Ball[] = [];
         let numBalls = 18;
         for (let i = 0; i < numBalls; i++) {
-            let position = paper.Point.random().multiply(new paper.Point(paper.view.size.width, paper.view.size.height));
+            let position = paper.Point.random().multiply(new paper.Point(paper.view.size));
             let vector = new paper.Point({
                 angle: 360 * Math.random(),
                 length: Math.random() * 10
@@ -746,7 +746,7 @@ function Examples() {
 
         // Add the boids:
         for (let i = 0; i < 30; i++) {
-            let position = paper.Point.random().multiply(new paper.Point(paper.view.size.width, paper.view.size.height));
+            let position = paper.Point.random().multiply(new paper.Point(paper.view.size));
             boids.push(new Boid(position, 10, 0.05));
         }
 
@@ -834,7 +834,7 @@ function Examples() {
             // Place the instances of the symbol:
             for (let i = 0; i < count; i++) {
                 // The center position is a random point in the view:
-                let center = paper.Point.random().multiply(new paper.Point(paper.view.size.width, paper.view.size.height));
+                let center = paper.Point.random().multiply(new paper.Point(paper.view.size));
                 let placed = symbol.place(center);
                 placed.scale(i / count + 0.01);
                 placed.data = {
@@ -1193,6 +1193,127 @@ function Examples() {
                 angle: radians * 180 / Math.PI,
                 length: length
             });
+        }
+    }
+    function Voronoi(){
+        let voronoi:any = {};
+        let sites = generateBeeHivePoints(paper.view.size.divide(200), true);
+        let bbox:any, diagram;
+        let oldSize = paper.view.size;
+        let spotColor = new paper.Color('red');
+        let mousePos = paper.view.center;
+        let selected = false;
+
+        onResize();
+
+        function onMouseDown(event: paper.ToolEvent) {
+            sites.push(event.point);
+            renderDiagram();
+        }
+
+        function onMouseMove(event: paper.ToolEvent) {
+            mousePos = event.point;
+            if (event.count == 0)
+                sites.push(event.point);
+            sites[sites.length - 1] = event.point;
+            renderDiagram();
+        }
+
+        function renderDiagram() {
+            paper.project.activeLayer.children = [];
+            let diagram = voronoi.compute(sites, bbox);
+            if (diagram) {
+                for (let i = 0, l = sites.length; i < l; i++) {
+                    let cell = diagram.cells[(sites[i] as any).voronoiId];
+                    if (cell) {
+                        let halfedges = cell.halfedges,
+                            length = halfedges.length;
+                        if (length > 2) {
+                            let points = [];
+                            for (let j = 0; j < length; j++) {
+                                let v = halfedges[j].getEndpoint();
+                                points.push(new paper.Point(v));
+                            }
+                            createPath(points, sites[i]);
+                        }
+                    }
+                }
+            }
+        }
+
+        function removeSmallBits(path:paper.Path) {
+            let averageLength = path.length / path.segments.length;
+            let min = path.length / 50;
+            for(let i = path.segments.length - 1; i >= 0; i--) {
+                let segment = path.segments[i];
+                let cur = segment.point;
+                let nextSegment = segment.next;
+                let next = nextSegment.point.add(nextSegment.handleIn);
+                if (cur.getDistance(next) < min) {
+                    segment.remove();
+                }
+            }
+        }
+
+        function generateBeeHivePoints(size:paper.Size, loose: boolean) {
+            let points:paper.Point[] = [];
+            let col = paper.view.size.divide(size);
+            for(let i = -1; i < size.width + 1; i++) {
+                for(let j = -1; j < size.height + 1; j++) {
+                    let point = new paper.Point(i, j).divide(new paper.Point(size)).multiply(new paper.Point(paper.view.size)).add(new paper.Point(col).divide(2));
+                    if(j % 2)
+                        point = point.add(new paper.Point(col.width / 2, 0));
+                    if(loose)
+                        point = point.add(new paper.Point(col).divide(4).multiply(paper.Point.random()).subtract(new paper.Point(col.divide(4))));
+                    points.push(point);
+                }
+            }
+            return points;
+        }
+        function createPath(points: paper.Point[], center: paper.Point) {
+            let path = new paper.Path();
+            if (!selected) { 
+                path.fillColor = spotColor;
+            } else {
+                path.fullySelected = selected;
+            }
+            path.closed = true;
+
+            for (let i = 0, l = points.length; i < l; i++) {
+                let point = points[i];
+                let next = points[(i + 1) == points.length ? 0 : i + 1];
+                let vector = (next.subtract(point)).divide(2);
+                path.add(new paper.Point({
+                    point: point.add(vector),
+                    handleIn: vector.multiply(-1),
+                    handleOut: vector
+                }));
+            }
+            path.scale(0.95);
+            removeSmallBits(path);
+            return path;
+        }
+
+        function onResize() {
+            let margin = 20;
+            bbox = {
+                xl: margin,
+                xr: paper.view.bounds.width - margin,
+                yt: margin,
+                yb: paper.view.bounds.height - margin
+            };
+            for (let i = 0, l = sites.length; i < l; i++) {
+                sites[i] = sites[i].multiply(new paper.Point(paper.view.size.divide(oldSize)));
+            }
+            oldSize = paper.view.size;
+            renderDiagram();
+        }
+
+        function onKeyDown(event: paper.KeyEvent) {
+            if (event.key == 'space') {
+                selected = !selected;
+                renderDiagram();
+            }
         }
     }
 }
