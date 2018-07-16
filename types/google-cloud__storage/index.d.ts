b7f15b857be77efd2a7c1c177fdfd3822d69262a
Futa Ogawa <ogawa0071@gmail.com>
2018-07-16 21:02:54 +0900

    google-cloud__storage / Fix definitions metadata.

--- types/google-cloud__storage/index.d.ts
+++ types/google-cloud__storage/index.d.ts
@@ -1,9 +1,11 @@
-// Type definitions for @google-cloud/storage 1.1
-// Project: https://github.com/GoogleCloudPlatform/google-cloud-node/tree/master/packages/storage
+// Type definitions for @google-cloud/storage 1.7
+// Project: https://github.com/googleapis/nodejs-storage
 // Definitions by: Brian Love <https://github.com/blove>
 //                 Nathan Brooker Perry <https://github.com/nbperry>
 //                 Matt Welke <https://github.com/welkie>
+//                 Futa Ogawa <https://github.com/ogawa0071>
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
+// TypeScript Version: 2.3
 
 /// <reference types="node" />
 
