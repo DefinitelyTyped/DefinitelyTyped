49d2ad2590e9914abebf1d8ac992a587c7d181bc
Ryan Friedman <ryan.friedman@ni.com>
2018-07-16 01:36:49 -0500

    Adding the Example 'Path Simplification' rewritten as typescript to the tests. This hopefully mimics what a user might have done to adapt the examples and ensure that all the example interfaces are implemented correctly.

--- types/paper/paper-tests.ts
+++ types/paper/paper-tests.ts
@@ -1851,6 +1851,55 @@ function Examples() {
                 }).removeOnMove();
             }
         }
+    }
+    function PathSimplification(){
+        var path:paper.Path;
 
+        var textItem = new paper.PointText({
+            content: 'Click and drag to draw a line.',
+            point: new paper.Point(20, 30),
+            fillColor: 'black',
+        });
+
+        function onMouseDown(event:paper.ToolEvent) {
+            // If we produced a path before, deselect it:
+            if (path) {
+                path.selected = false;
+            }
+
+            // Create a new path and set its stroke color to black:
+            path = new paper.Path({
+                segments: [event.point],
+                strokeColor: 'black',
+                // Select the path, so we can see its segment points:
+                fullySelected: true
+            });
+        }
+
+        // While the user drags the mouse, points are added to the path
+        // at the position of the mouse:
+        function onMouseDrag(event:paper.ToolEvent) {
+            path.add(event.point);
+
+            // Update the content of the text item to show how many
+            // segments it has:
+            textItem.content = 'Segment count: ' + path.segments.length;
+        }
+
+        // When the mouse is released, we simplify the path:
+        function onMouseUp(event:paper.ToolEvent) {
+            var segmentCount = path.segments.length;
+
+            // When the mouse is released, simplify it:
+            path.simplify(10);
+
+            // Select the path, so we can see its segments:
+            path.fullySelected = true;
+
+            var newSegmentCount = path.segments.length;
+            var difference = segmentCount - newSegmentCount;
+            var percentage = 100 - Math.round(newSegmentCount / segmentCount * 100);
+            textItem.content = difference + ' of the ' + segmentCount + ' segments were removed. Saving ' + percentage + '%';
+        }
     }
 }
