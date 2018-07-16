1470c83f5d63626950a153a75c8a75392a576a41
Sebastian Lenz <sebastian-lenz@users.noreply.github.com>
2018-07-16 17:00:03 +0200

    Add third parameter `parent` to matchPath
    MathPath takes a third parameter, see https://github.com/ReactTraining/react-router/blob/master/packages/react-router/modules/matchPath.js#L28

--- types/react-router/index.d.ts
+++ types/react-router/index.d.ts
@@ -115,7 +115,7 @@ export interface match<P> {
 // Omit taken from https://www.typescriptlang.org/docs/handbook/release-notes/typescript-2-8.html
 export type Omit<T, K extends keyof T> = Pick<T, Exclude<keyof T, K>>;
 
-export function matchPath<P>(pathname: string, props: RouteProps): match<P> | null;
+export function matchPath<P>(pathname: string, props: RouteProps, parent?: match<P> | null): match<P> | null;
 
 export function generatePath(pattern: string, params?: { [paramName: string]: string | number | boolean }): string;
 
