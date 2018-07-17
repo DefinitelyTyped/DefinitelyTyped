fbbad1e85f1b26348ebe006021909210e50efed4
Fabio Berta <fnberta@gmail.com>
2018-07-17 13:59:41 +0200

    add myself as author

--- types/react-map-gl/index.d.ts
+++ types/react-map-gl/index.d.ts
@@ -1,6 +1,7 @@
 // Type definitions for react-map-gl 3.2
 // Project: https://github.com/uber/react-map-gl#readme
 // Definitions by: Robert Imig <https://github.com/rimig>
+//                 Fabio Berta <https://github.com/fnberta>
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
 // TypeScript Version: 2.6
 
