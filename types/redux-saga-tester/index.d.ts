5eb6aa7e325343f55694e27ed425bb2e2d0d5a5a
Ben <benlorantfy@gmail.com>
2018-07-17 23:41:47 -0400

    added docs from readme

--- types/redux-saga-tester/index.d.ts
+++ types/redux-saga-tester/index.d.ts
@@ -28,14 +28,56 @@ export interface SagaTesterOptions<StateType extends object> {
 
 export default class SagaTester<StateType extends object> {
     constructor(options?: SagaTesterOptions<StateType>);
+
+    /**
+     * Starts execution of the provided saga.
+     */
     start(saga: SagaFunction): void;
+
+    /**
+     * Dispatches an action to the redux store.
+     */
     dispatch(action: AnyAction): void;
+
+    /**
+     * Assigns the newState into the current state. (Only works with the default reducer.)
+     */
     updateState(state: StateType): void;
+
+    /**
+     * Returns the state of the redux store.
+     */
     getState(): StateType;
+
+    /**
+     * Returns a promise that will resolve if the specified action is dispatched to the store.
+     * @param futureOnly Causes waitFor to only resolve if the action is called in the future.
+     */
     waitFor(actionType: string, futureOnly?: boolean): PromiseLike<void>;
+
+    /**
+     * Returns whether the specified was dispatched in the past.
+     */
     wasCalled(actionType: string): boolean;
+
+    /**
+     * Returns the number of times an action with the given type was dispatched.
+     */
     numCalled(actionType: string): number;
+
+    /**
+     * Returns the last action dispatched to the store.
+     */
     getLatestCalledAction(): AnyAction;
+
+    /**
+     * Returns an array of all actions dispatched.
+     */
     getCalledActions(): AnyAction[];
+
+    /**
+     * Reset the store state back to initialState
+     * @param clearActionList Clears the history of past actions (defaults to false).
+     */
     reset(clearActionList?: boolean): void;
 }
