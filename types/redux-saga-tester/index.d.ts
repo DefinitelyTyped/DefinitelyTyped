53beef880c9d04d36ff27cb1abc74026805f55b5
Ben <benlorantfy@gmail.com>
2018-07-17 22:57:01 -0400

    use object instead of AnyObject

--- types/redux-saga-tester/index.d.ts
+++ types/redux-saga-tester/index.d.ts
@@ -2,29 +2,25 @@
 // Project: https://github.com/wix/redux-saga-tester#readme
 // Definitions by: Ben Lorantfy <https://github.com/BenLorantfy>
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
-export interface AnyObject {
-    [key: string]: any;
-}
 
 export type AnyAction = { type: string, [key: string]: any };
-export type AnyGenerator = Function;
-export type Reducer = (state: AnyObject, action: AnyAction) => AnyObject;
+export type Reducer = (state: object, action: AnyAction) => object;
 export interface ReducerMap {
     [key: string]: Reducer;
 }
 
-export interface SagaTesterOptions<StateType extends AnyObject> {
+export interface SagaTesterOptions<StateType extends object> {
     initialState?: StateType,
     reducers?: ReducerMap|Reducer,
     middlewares?: Function[],
     combineReducers?: (map: ReducerMap) => Reducer,
     ignoreReduxActions?: boolean,
-    options?: AnyObject
+    options?: object
 }
 
-export default class SagaTester<StateType extends AnyObject> {
+export default class SagaTester<StateType extends object> {
     constructor(options?: SagaTesterOptions<StateType>);
-    start(saga: AnyGenerator): void;
+    start(saga: Function): void;
     dispatch(action: AnyAction): void;
     updateState(state: StateType): void;
     getState(): StateType;
