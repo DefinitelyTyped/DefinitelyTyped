d423fb3eee5156703148cd00ef0bcf4a526d0063
swist <me@swistofon.pl>
2018-07-18 11:25:23 +0100

    Fix lint errors

--- types/chai-roughly/index.d.ts
+++ types/chai-roughly/index.d.ts
@@ -3,24 +3,25 @@
 // Definitions by: Tomasz Nguyen <https://github.com/swist>
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
 /// <reference types="chai" />
-declare namespace Chai {
-    // For BDD APIs
-    interface Roughly extends Assertion {
-        (tolerance?: number): Assertion
-    }
 
-    interface Assertion extends LanguageChains, NumericComparison, TypeComparison {
-        roughly: Roughly;
-    }
+declare global {
+    namespace Chai {
+        // For BDD APIs
+        interface Roughly extends Assertion {
+            (tolerance?: number): Assertion;
+        }
 
-    // For Assert APIs
-    interface Assert {
-        roughly: Roughly;
+        interface Assertion extends LanguageChains, NumericComparison, TypeComparison {
+            roughly: Roughly;
+        }
+
+        // For Assert APIs
+        interface Assert {
+            roughly: Roughly;
+        }
     }
 }
 
-declare module 'chai-roughly' {
-    function chaiRoughly(chai: any, utils: any): void;
-    namespace chaiRoughly { }
-    export = chaiRoughly;
-}
+declare function chaiRoughly(chai: any, utils: any): void;
+declare namespace chaiRoughly { }
+export = chaiRoughly;
