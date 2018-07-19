daeba1f24995bceefc9b1da4fb17715b79152731
pravdomil <pravdomil.toman@gmail.com>
2018-07-19 20:06:15 +0200

    set default state to empty object

--- types/react/index.d.ts
+++ types/react/index.d.ts
@@ -354,7 +354,7 @@ declare namespace React {
         displayName?: string;
     }
 
-    interface ComponentClass<P = {}, S = any> extends StaticLifecycle<P, S> {
+    interface ComponentClass<P = {}, S = {}> extends StaticLifecycle<P, S> {
         new (props: P, context?: any): Component<P, ComponentState>;
         propTypes?: ValidationMap<P>;
         contextTypes?: ValidationMap<any>;
