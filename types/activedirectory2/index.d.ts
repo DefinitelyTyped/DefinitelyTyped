9922c0e590d7f47e500734107f978fa91a3bfb39
pas <pas@zomg.hu>
2018-07-19 17:03:48 +0200

    activedirectory2: header added, and minimal test file showing the only sane way to import it; and default tslint.json

--- types/activedirectory2/index.d.ts
+++ types/activedirectory2/index.d.ts
@@ -1,3 +1,7 @@
+// Type definitions for activedirectory2 1.2
+// Project: https://github.com/jsumners/node-activedirectory#readme
+// Definitions by: pas <https://github.com/pasthelod>
+// Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
 // TypeScript Version: 2.5
 
 import { Filter } from "ldapjs";
