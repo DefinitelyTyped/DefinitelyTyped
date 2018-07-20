7c23c0b16a7df95a9018b2a70c75828cfe079a7c
Brian Crowell <brian@fluggo.com>
2018-07-20 16:14:47 -0500

    couchbase: Add version number and add me
    Even though this doesn't cover everything in the latest SDK, that's the
    version I based my changes on.

--- types/couchbase/index.d.ts
+++ types/couchbase/index.d.ts
@@ -1,6 +1,7 @@
-// Type definitions for Couchbase Node.js SDK 2.1.2
+// Type definitions for Couchbase Node.js SDK 2.4.5
 // Project: https://github.com/couchbase/couchnode
 // Definitions by: Marwan Aouida <https://github.com/maouida>
+//      Brian Crowell <https://github.com/fluggo>
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
 
 /// <reference types="node"/>
