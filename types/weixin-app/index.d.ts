5ba0287527b82b2d457c123fe92b2f735629cd14
Jiayu Liu <Jimexist@users.noreply.github.com>
2018-07-21 08:39:32 +0800

    update weixin-app's Component<T> interface (#27333)

--- types/weixin-app/index.d.ts
+++ types/weixin-app/index.d.ts
@@ -3213,7 +3213,7 @@ interface Component<T> {
    * 检查组件是否具有 behavior
    * 检查时会递归检查被直接或间接引入的所有behavior
    */
-  hasBehavior(behavior: any): void;
+  hasBehavior(behavior: any): boolean;
   /**
    * 触发事件，参见 [组件事件](https://mp.weixin.qq.com/debug/wxadoc/dev/framework/custom-component/events.html)
    */
@@ -3226,20 +3226,20 @@ interface Component<T> {
    * 创建一个 SelectorQuery 对象
    * 选择器选取范围为这个[组件实例](https://mp.weixin.qq.com/debug/wxadoc/dev/api/wxml-nodes-info.html)内
    */
-  createSelectorQuery(query: string): void;
+  createSelectorQuery(): wx.SelectorQuery;
   /**
    * 使用选择器选择组件实例节点
    * 返回匹配到的第一个组件实例对象
    */
-  selectComponent(selector: string): void;
+  selectComponent(selector: string): Component<any>;
   /**
    * selector  使用选择器选择组件实例节点，返回匹配到的全部组件实例对象组成的数组
    */
-  selectAllComponents(selector: string): void;
+  selectAllComponents(selector: string): Array<Component<any>>;
   /**
    * 获取所有这个关系对应的所有关联节点，参见 [组件间关系](https://mp.weixin.qq.com/debug/wxadoc/dev/framework/custom-component/relations.html)
    */
-  getRelationNodes(relationKey: string): void;
+  getRelationNodes(relationKey: string): { [key: string]: ComponentRelation };
 }
 declare function Component<D, M, O, P>(options?: ThisTypedComponentOptionsWithRecordProps<Component<D>, D, M, O, P>): ExtendedComponent<Component<D>, D, M, O, P>;
 /**
