097c9e20ad43f9f73d4c851b510b5a7caf28ed9e
Krzysztof Bogdan <krzysztof.bogdan@codeweaver.pl>
2018-07-21 02:37:25 +0200

    three.js add missing math.lerp and webglrenderer methods (#27317)
    * Update three-core.d.ts
    
    Add missing WebGLRenderer methods (getDrawingBufferSize, setDrawingBufferSize, getCurrentViewport)
    
    * Update three-core.d.ts
    
    remove unnecessary whitespace
    
    * Add missing lerp method from Math namespace
    
    * Fixed indentation & order

--- types/three/three-core.d.ts
+++ types/three/three-core.d.ts
@@ -3650,6 +3650,17 @@ export namespace Math {
 
     export function isPowerOfTwo(value: number): boolean;
 
+	/**
+     * Returns a value linearly interpolated from two known points based
+     * on the given interval - t = 0 will return x and t = 1 will return y.
+     *
+     * @param x Start point.
+     * @param y End point.
+     * @param t interpolation factor in the closed interval [0, 1]
+     * @return {number}
+     */
+    export function lerp (x: number, y: number, t: number): number;
+
     /**
      * @deprecated Use {@link Math#floorPowerOfTwo .floorPowerOfTwo()}
      */
@@ -5532,14 +5543,18 @@ export class WebGLRenderer implements Renderer {
 
     getPixelRatio(): number;
     setPixelRatio(value: number): void;
+    
+    getDrawingBufferSize(): { width: number; height: number; };
+    setDrawingBufferSize(width: number, height: number, pixelRatio: number): void;
 
     getSize(): { width: number; height: number; };
-
+	
     /**
      * Resizes the output canvas to (width, height), and also sets the viewport to fit that size, starting in (0, 0).
      */
     setSize(width: number, height: number, updateStyle?: boolean): void;
 
+    getCurrentViewport(): Vector4;
     /**
      * Sets the viewport to render from (x, y) to (x + width, y + height).
      */
