3fa67d28ad276aa93d562ccadc7e92f9dd431cbf
Errietta Kostala <errietta@errietta.me>
2018-07-22 18:01:55 +0100

    reference new version

--- types/jsoneditor/index.d.ts
+++ types/jsoneditor/index.d.ts
@@ -1,4 +1,4 @@
-// Type definitions for jsoneditor v5.5.7
+// Type definitions for jsoneditor v5.19.0
 // Project: https://github.com/josdejong/jsoneditor
 // Definitions by: Alejandro Sánchez <https://github.com/alejo90>
 // Definitions by: Errietta Kostala <https://github.com/errietta>
