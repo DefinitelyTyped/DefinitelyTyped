8d4befa30f1a7012a84b84ed7cbaf15195034af4
Sosuke Suzuki <aosukeke@gmail.com>
2018-07-23 18:08:09 +0900

    Add my name as comment

--- types/url-regex/index.d.ts
+++ types/url-regex/index.d.ts
@@ -1,6 +1,7 @@
 // Type definitions for url-regex 4.0
 // Project: https://github.com/kevva/url-regex
 // Definitions by: Daniel Perez Alvarez <https://github.com/unindented>
+//                 sosukesuzuki <https://github.com/sosukesuzuki>
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
 
 /** Regular expression for matching URLs. */
