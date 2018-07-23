4b0462ee32653a2ef1301ff7c60bbf50cd4e9ace
Sosuke Suzuki <aosukeke@gmail.com>
2018-07-23 18:31:36 +0900

    Remove patch version

--- types/url-regex/index.d.ts
+++ types/url-regex/index.d.ts
@@ -1,4 +1,4 @@
-// Type definitions for url-regex 4.1.1
+// Type definitions for url-regex 4.1
 // Project: https://github.com/kevva/url-regex
 // Definitions by: Daniel Perez Alvarez <https://github.com/unindented>
 //                 sosukesuzuki <https://github.com/sosukesuzuki>
