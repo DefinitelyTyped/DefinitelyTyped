c757178001a0e4cf433d3231022766d4739abb8e
Sosuke Suzuki <aosukeke@gmail.com>
2018-07-23 17:33:31 +0900

    Update url-regex/index.d.ts

--- types/url-regex/index.d.ts
+++ types/url-regex/index.d.ts
@@ -7,6 +7,8 @@
 declare function urlRegex(options?: {
     /** Only match an exact string. Useful with RegExp#test to check if a string is a URL. */
     exact?: boolean;
+    /** Force URLs to start with a valid protocol or www. If set to false it'll match the TLD against a list of valid TLDs(https://github.com/stephenmathieson/node-tlds). */
+    strict?: boolean;
 }): RegExp;
 
 export = urlRegex;
