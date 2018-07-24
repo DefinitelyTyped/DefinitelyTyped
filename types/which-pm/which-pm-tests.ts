8bd2960a16c5e4d53d112c87c7841e55bd628b64
Jorge Gonzalez <jrgqhd@gmail.com>
2018-07-24 13:12:57 -0400

    Add types for which-pm@1.0

---
+++ types/which-pm/which-pm-tests.ts
@@ -0,0 +1,13 @@
+import whichpm = require("which-pm");
+
+whichpm(process.cwd())
+    .then(pm => {
+        pm; // $ExpectType PMInfo | null
+        if (pm !== null && pm !== undefined) {
+            pm.name; // $ExpectType string
+            pm.version; // $ExpectType string | undefined
+        }
+    })
+    .catch(err => console.error(err));
+
+const _wpm = whichpm(process.cwd()); // $ExpectType Promise<PMInfo | null>
