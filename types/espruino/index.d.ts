79e9a11a05b71968fad62a1b3141aa3fd5e0dcbc
Jurand Niemczycki <createdbyjurand@gmail.com>
2018-07-24 19:22:39 +0200

    espruino added myself to definitions

--- types/espruino/index.d.ts
+++ types/espruino/index.d.ts
@@ -1,6 +1,7 @@
 // Type definitions for Espruino 1.94
 // Project: http://www.espruino.com/
 // Definitions by: Stanislav Berkov <https://github.com/stasberkov>
+//                 Jurand Niemczycki <https://github.com/createdbyjurand>
 // Definitions: https://github.com/borisyankov/DefinitelyTyped
 
 declare interface Object {
