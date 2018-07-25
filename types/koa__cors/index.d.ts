9e5cd12be75102b6fad17dc22dfa7be4c5e39523
Steven McDowall <sjm@hawoods.org>
2018-07-25 14:41:03 -0400

    Fixed type for function passing to cors() origin
    If you look at the actual code within @koa/cors and where it invokes any passed function within the origin: option, it invokes it with ctx, the standard Koa context, NOT the Request.  Here is the code fragment..
    
    https://github.com/koajs/cors/blob/b5a937fc24666761c61b1ac633f49f54612d1315/index.js#L58
    
    Thus changed the signature of the origin option to reflect this.

--- types/koa__cors/index.d.ts
+++ types/koa__cors/index.d.ts
@@ -1,6 +1,9 @@
 // Type definitions for @koa/cors 2.2
 // Project: https://github.com/koajs/cors
-// Definitions by: Xavier Stouder <https://github.com/Xstoudi>, Izayoi Ko <https://github.com/izayoiko>, Steve Hipwell <https://github.com/stevehipwell>
+// Definitions by: Xavier Stouder <https://github.com/Xstoudi>
+//                 Izayoi Ko <https://github.com/izayoiko>
+//                 Steve Hipwell <https://github.com/stevehipwell>
+//                 Steven McDowall <https://github.com/sjmcdowall>
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
 // TypeScript Version: 2.3
 
@@ -12,7 +15,7 @@ declare function cors(options?: cors.Options): Koa.Middleware;
 
 declare namespace cors {
     interface Options {
-        origin?: ((req: Koa.Request) => string) | string;
+        origin?: ((ctx: Koa.Context) => string) | string;
         allowMethods?: string[] | string;
         exposeHeaders?: string[] | string;
         allowHeaders?: string[] | string;
--- types/koa__cors/koa__cors-tests.ts
+++ types/koa__cors/koa__cors-tests.ts
@@ -1,5 +1,13 @@
-import Koa = require('koa');
-import cors = require('@koa/cors');
+import Koa = require("koa");
+import cors = require("@koa/cors");
 
 const app = new Koa();
 app.use(cors());
+
+// Trying using cors() passing in a function ..
+function testCorsFunction(ctx: Koa.Context) {
+    const requestOrigin = ctx.request.origin;
+    return requestOrigin;
+}
+
+app.use(cors({ origin: testCorsFunction }));
