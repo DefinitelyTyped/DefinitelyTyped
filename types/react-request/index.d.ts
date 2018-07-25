91f6d439f4863f748e06adf71ba1c19463c67d40
Daniel <dcochran@netflix.com>
2018-07-25 11:07:15 -0700

    pin to 2.9 instead of 2.9.2

--- types/react-request/index.d.ts
+++ types/react-request/index.d.ts
@@ -2,7 +2,7 @@
 // Project: https://github.com/jamesplease/react-request
 // Definitions by: Danny Cochran <https://github.com/dannycochran>
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
-// TypeScript Version: 2.9.2
+// TypeScript Version: 2.9
 
 declare module 'react-request' {
   import * as React from 'react';
