bdec8ce83351b2d32e13b119494d6768e09cf874
Steven McDowall <sjm@hawoods.org>
2018-07-25 14:53:01 -0400

    Revert back to single quotes -- stupid prettier
    Fixed type for function passing to cors() origin
    
    If you look at the actual code within @koa/cors and where it invokes any passed function within the origin: option, it invokes it with ctx, the standard Koa context, NOT the Request.  Here is the code fragment..
    
    https://github.com/koajs/cors/blob/b5a937fc24666761c61b1ac633f49f54612d1315/index.js#L58
    
    Thus changed the signature of the origin option to reflect this.

--- types/koa__cors/index.d.ts
+++ types/koa__cors/index.d.ts
@@ -7,7 +7,7 @@
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
 // TypeScript Version: 2.3
 
-import * as Koa from "koa";
+import * as Koa from 'koa';
 
 export = cors;
 
--- types/koa__cors/koa__cors-tests.ts
+++ types/koa__cors/koa__cors-tests.ts
@@ -1,5 +1,5 @@
-import Koa = require("koa");
-import cors = require("@koa/cors");
+import Koa = require('koa');
+import cors = require('@koa/cors');
 
 const app = new Koa();
 app.use(cors());
