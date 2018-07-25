cc0488e0805fe326b54bc12c5347dd21541d443d
Daniel <dcochran@netflix.com>
2018-07-25 10:53:40 -0700

    try specifying a typescript version in index.d.ts

--- types/react-request/index.d.ts
+++ types/react-request/index.d.ts
@@ -2,6 +2,7 @@
 // Project: https://github.com/jamesplease/react-request
 // Definitions by: Danny Cochran <https://github.com/dannycochran>
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
+// TypeScript Version: 2.9.2
 
 declare module 'react-request' {
   import * as React from 'react';
