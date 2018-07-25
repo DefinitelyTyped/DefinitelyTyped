f2a7ced0aa60eb1ab35baa3b205bfd6bc3356290
Daniel <dcochran@netflix.com>
2018-07-25 11:24:41 -0700

    fix index.d.ts lint issues

--- types/react-request/index.d.ts
+++ types/react-request/index.d.ts
@@ -4,49 +4,47 @@
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
 // TypeScript Version: 2.9
 
-declare module 'react-request' {
-  import * as React from 'react';
+import * as React from 'react';
 
-  interface RenderProps<T> {
-    requestName: string;
-    requestKey: string;
-    fetching: boolean;
-    failed: boolean;
-    error: Error | null;
-    response: Response | null;
-    url: string;
-    data: T | null;
-  }
-
-  interface FetchRequestProps extends RequestInit {
-    lazy?: boolean;
-    url: string;
-  }
-
-  interface FetchProps<T = any> extends FetchRequestProps {
-    children?: (renderProps: RenderProps<T>) => React.ReactNode;
-  }
-
-  // TODO(dannycochran) RequestKeyOptions, ProxyRequest, fetchDedupe, getRequestKey, isRequestInFlight, clearRequestCache
-  // should all be defined in an adjacent typings directory for fetch-dedupe.
-  interface RequestKeyOptions {
-    url?: string;
-    method?: string;
-    responseType?: string;
-    body?: string;
-  }
-
-  interface ProxyRequest {
-    requestKey: string;
-    res: Response;
-  }
-
-  // TODO(dannycochran) Fill out fetchDedupe options.
-  const fetchDedupe: (input: any, init?: any, dedupeOptions?: any) => Promise<ProxyRequest>;
-  const getRequestKey: (keyOptions?: RequestKeyOptions) => string;
-  const isRequestInFlight: () => boolean;
-  const clearRequestCache: () => void;
-  const clearResponseCache: () => void;
-
-  class Fetch<T = any> extends React.Component<FetchProps<T>, {}> { }
+export interface RenderProps<T> {
+  requestName: string;
+  requestKey: string;
+  fetching: boolean;
+  failed: boolean;
+  error: Error | null;
+  response: Response | null;
+  url: string;
+  data: T | null;
 }
+
+export interface FetchRequestProps extends RequestInit {
+  lazy?: boolean;
+  url: string;
+}
+
+export interface FetchProps<T = any> extends FetchRequestProps {
+  children?: (renderProps: RenderProps<T>) => React.ReactNode;
+}
+
+// TODO(dannycochran) RequestKeyOptions, ProxyRequest, fetchDedupe, getRequestKey, isRequestInFlight, clearRequestCache
+// should all be defined in an adjacent typings directory for fetch-dedupe.
+export interface RequestKeyOptions {
+  url?: string;
+  method?: string;
+  responseType?: string;
+  body?: string;
+}
+
+export interface ProxyRequest {
+  requestKey: string;
+  res: Response;
+}
+
+// TODO(dannycochran) Fill out fetchDedupe options.
+export function fetchDedupe(input: any, init?: any, dedupeOptions?: any): Promise<ProxyRequest>;
+export function getRequestKey(keyOptions?: RequestKeyOptions): string;
+export function isRequestInFlight(): boolean;
+export function clearRequestCache(): void;
+export function clearResponseCache(): void;
+
+export class Fetch<T = any> extends React.Component<FetchProps<T>> { }
