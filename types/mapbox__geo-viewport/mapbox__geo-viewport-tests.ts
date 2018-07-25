69a1479c01ff00e572be591af1bf14af8bb59f74
Fabio Berta <fnberta@gmail.com>
2018-07-25 09:42:50 +0200

    add types for @mapbox/geo-viewport

---
+++ types/mapbox__geo-viewport/mapbox__geo-viewport-tests.ts
@@ -0,0 +1,13 @@
+import { viewport, bounds } from '@mapbox/geo-viewport';
+
+const geoViewport = viewport([
+    5.668343999999995,
+    45.111511000000014,
+    5.852471999999996,
+    45.26800200000002,
+], [640, 480]);
+
+const boundingBox = bounds({
+    lon: 100,
+    lat: 200,
+}, 14, [640, 480]);
