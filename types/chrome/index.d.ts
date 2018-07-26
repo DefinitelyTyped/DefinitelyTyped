6990103aa91539e541e08f4977851fcdcf43304d
Felix Rubio <FMRb@users.noreply.github.com>
2018-07-26 17:15:52 +0200

    Chrome: Add optional property initiator on ResourceRequest (#27571)
    * Add optional property initiator in ResourceRequest
    
    * Add optional property initiator in ResourceRequest

--- types/chrome/index.d.ts
+++ types/chrome/index.d.ts
@@ -7383,6 +7383,10 @@ declare namespace chrome.webRequest {
         type: ResourceType;
         /** The time when this signal is triggered, in milliseconds since the epoch. */
         timeStamp: number;
+        /** The origin where the request was initiated. This does not change through redirects. If this is an opaque origin, the string 'null' will be used.
+         * @since Since Chrome 63.
+        */
+        initiator?: string;
     }
 
     export interface WebRequestDetails extends ResourceRequest {
