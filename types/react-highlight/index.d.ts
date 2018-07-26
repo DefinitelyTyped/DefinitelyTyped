b82abe0725786298c35216ea48ab616fa6147863
Joshua Goldberg <josh@fullscreenmario.com>
2018-07-26 10:34:39 -0700

    React TS version 2.6

--- types/react-highlight/index.d.ts
+++ types/react-highlight/index.d.ts
@@ -2,6 +2,7 @@
 // Project: https://github.com/akiran/react-highlight
 // Definitions by: Josh Goldberg <https://github.com/joshuakgoldberg>
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
+// TypeScript Version: 2.6
 
 import * as React from "react";
 
