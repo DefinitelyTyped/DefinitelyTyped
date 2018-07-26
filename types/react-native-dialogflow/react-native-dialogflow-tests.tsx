353331f386284bfd88883e503920eed4d528ec07
Jason Merino <jason.merino@build.com>
2018-07-26 10:36:16 -0700

    Fix lint issues.

--- types/react-native-dialogflow/react-native-dialogflow-tests.tsx
+++ types/react-native-dialogflow/react-native-dialogflow-tests.tsx
@@ -14,14 +14,13 @@ const privateKey = '-----BEGIN PRIVATE KEY-----\n_\n-----END PRIVATE KEY-----';
 const projectId = 'projectId';
 
 class Screen1 extends React.Component<any> {
-
     componentDidMount() {
         Dialogflow.setConfiguration(accessToken, Dialogflow.LANG_ENGLISH);
         Dialogflow.setEntities([]);
         Dialogflow.startListening(() => {}, () => {});
         Dialogflow.finishListening();
-        Dialogflow.requestEvent(eventName, eventData, () => {}, () => {})
-        Dialogflow.requestQuery(query, () => {}, () => {})
+        Dialogflow.requestEvent(eventName, eventData, () => {}, () => {});
+        Dialogflow.requestQuery(query, () => {}, () => {});
         Dialogflow.onListeningStarted(() => {});
         Dialogflow.onListeningFinished(() => {});
         Dialogflow.onAudioLevel(() => {});
@@ -32,8 +31,8 @@ class Screen1 extends React.Component<any> {
         Dialogflow_V2.setConfiguration(serviceAccount, privateKey, Dialogflow_V2.LANG_ENGLISH, projectId);
         Dialogflow_V2.startListening(() => {}, () => {});
         Dialogflow_V2.finishListening();
-        Dialogflow_V2.requestEvent(eventName, eventData, () => {}, () => {})
-        Dialogflow_V2.requestQuery(query, () => {}, () => {})
+        Dialogflow_V2.requestEvent(eventName, eventData, () => {}, () => {});
+        Dialogflow_V2.requestQuery(query, () => {}, () => {});
         Dialogflow_V2.onListeningStarted(() => {});
         Dialogflow_V2.onListeningFinished(() => {});
         Dialogflow_V2.onAudioLevel(() => {});
