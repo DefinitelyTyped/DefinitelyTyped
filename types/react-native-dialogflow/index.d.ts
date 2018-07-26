c3c24dcb9d3d558f37906bb7d7d868d9da04abea
Jason Merino <jason.merino@build.com>
2018-07-26 10:47:31 -0700

    Formatting changes.

--- types/react-native-dialogflow/index.d.ts
+++ types/react-native-dialogflow/index.d.ts
@@ -1,93 +1,93 @@
 // Type definitions for react-native-dialogflow 3.2
-// Project: https://github.com/innFactory/react-native-dialogflow
+// Project: https://github.com/innFactory/react-native-dialogflow#readme
 // Definitions by: Jason Merino <https://github.com/jasonmerino>
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
 // TypeScript Version: 2.6
 
 export namespace Dialogflow {
-    const LANG_CHINESE_CHINA = 'zh-CN';
-    const LANG_CHINESE_HONGKONG: 'zh-HK';
-    const LANG_CHINESE_TAIWAN: 'zh-TW';
-    const LANG_DUTCH: 'nl';
-    const LANG_ENGLISH: 'en';
-    const LANG_ENGLISH_GB: 'en-GB';
-    const LANG_ENGLISH_US: 'en-US';
-    const LANG_FRENCH: 'fr';
-    const LANG_GERMAN: 'de';
-    const LANG_ITALIAN: 'it';
-    const LANG_JAPANESE: 'ja';
-    const LANG_KOREAN: 'ko';
-    const LANG_PORTUGUESE: 'pt';
-    const LANG_PORTUGUESE_BRAZIL: 'pt-BR';
-    const LANG_RUSSIAN: 'ru';
-    const LANG_SPANISH: 'es';
-    const LANG_UKRAINIAN: 'uk';
+    export const LANG_CHINESE_CHINA = 'zh-CN';
+    export const LANG_CHINESE_HONGKONG: 'zh-HK';
+    export const LANG_CHINESE_TAIWAN: 'zh-TW';
+    export const LANG_DUTCH: 'nl';
+    export const LANG_ENGLISH: 'en';
+    export const LANG_ENGLISH_GB: 'en-GB';
+    export const LANG_ENGLISH_US: 'en-US';
+    export const LANG_FRENCH: 'fr';
+    export const LANG_GERMAN: 'de';
+    export const LANG_ITALIAN: 'it';
+    export const LANG_JAPANESE: 'ja';
+    export const LANG_KOREAN: 'ko';
+    export const LANG_PORTUGUESE: 'pt';
+    export const LANG_PORTUGUESE_BRAZIL: 'pt-BR';
+    export const LANG_RUSSIAN: 'ru';
+    export const LANG_SPANISH: 'es';
+    export const LANG_UKRAINIAN: 'uk';
 
-    function setConfiguration(accessToken: string, languageTag: string): void;
-    function setEntities(entities: any[]): void;
-    function startListening(resultCallback: (result: object) => void, errorCallback: (error: Error) => void): void;
-    function finishListening(): void;
-    function requestEvent(
+    export function setConfiguration(accessToken: string, languageTag: string): void;
+    export function setEntities(entities: any[]): void;
+    export function startListening(resultCallback: (result: object) => void, errorCallback: (error: Error) => void): void;
+    export function finishListening(): void;
+    export function requestEvent(
         eventName: string,
         eventData: object,
         resultCallback: (result: object) => void,
         errorCallback: (error: Error) => void
     ): Promise<any>;
-    function requestQuery(
+    export function requestQuery(
         query: string,
         resultCallback: (result: object) => void,
         errorCallback: (error: Error) => void
     ): Promise<any>;
-    function onListeningStarted(callback: () => void): void;
-    function onListeningFinished(callback: () => void): void;
-    function onAudioLevel(callback: (level: number) => void): void;
-    function setContexts(contexts: any[]): void;
-    function resetContexts(resultCallback: (result: object) => void, errorCallback: (error: Error) => void): void;
-    function setPermanentContexts(contexts: any[]): void;
+    export function onListeningStarted(callback: () => void): void;
+    export function onListeningFinished(callback: () => void): void;
+    export function onAudioLevel(callback: (level: number) => void): void;
+    export function setContexts(contexts: any[]): void;
+    export function resetContexts(resultCallback: (result: object) => void, errorCallback: (error: Error) => void): void;
+    export function setPermanentContexts(contexts: any[]): void;
 }
 
 export namespace Dialogflow_V2 {
-    const LANG_CHINESE_CHINA = 'zh-CN';
-    const LANG_CHINESE_HONGKONG: 'zh-HK';
-    const LANG_CHINESE_TAIWAN: 'zh-TW';
-    const LANG_DUTCH: 'nl';
-    const LANG_ENGLISH: 'en';
-    const LANG_ENGLISH_GB: 'en-GB';
-    const LANG_ENGLISH_US: 'en-US';
-    const LANG_FRENCH: 'fr';
-    const LANG_GERMAN: 'de';
-    const LANG_ITALIAN: 'it';
-    const LANG_JAPANESE: 'ja';
-    const LANG_KOREAN: 'ko';
-    const LANG_PORTUGUESE: 'pt';
-    const LANG_PORTUGUESE_BRAZIL: 'pt-BR';
-    const LANG_RUSSIAN: 'ru';
-    const LANG_SPANISH: 'es';
-    const LANG_UKRAINIAN: 'uk';
+    export const LANG_CHINESE_CHINA = 'zh-CN';
+    export const LANG_CHINESE_HONGKONG: 'zh-HK';
+    export const LANG_CHINESE_TAIWAN: 'zh-TW';
+    export const LANG_DUTCH: 'nl';
+    export const LANG_ENGLISH: 'en';
+    export const LANG_ENGLISH_GB: 'en-GB';
+    export const LANG_ENGLISH_US: 'en-US';
+    export const LANG_FRENCH: 'fr';
+    export const LANG_GERMAN: 'de';
+    export const LANG_ITALIAN: 'it';
+    export const LANG_JAPANESE: 'ja';
+    export const LANG_KOREAN: 'ko';
+    export const LANG_PORTUGUESE: 'pt';
+    export const LANG_PORTUGUESE_BRAZIL: 'pt-BR';
+    export const LANG_RUSSIAN: 'ru';
+    export const LANG_SPANISH: 'es';
+    export const LANG_UKRAINIAN: 'uk';
 
-    function setConfiguration(
+    export function setConfiguration(
         serviceAccount: string,
         privateKey: string,
         language: string,
         projectId: string
     ): void;
-    function startListening(resultCallback: (result: object) => void, errorCallback: (error: Error) => void): void;
-    function finishListening(): void;
-    function requestEvent(
+    export function startListening(resultCallback: (result: object) => void, errorCallback: (error: Error) => void): void;
+    export function finishListening(): void;
+    export function requestEvent(
         eventName: string,
         eventData: object,
         resultCallback: (result: object) => void,
         errorCallback: (error: Error) => void
     ): Promise<any>;
-    function requestQuery(
+    export function requestQuery(
         query: string,
         resultCallback: (result: object) => void,
         errorCallback: (error: Error) => void
     ): Promise<any>;
-    function onListeningStarted(callback: () => void): void;
-    function onListeningFinished(callback: () => void): void;
-    function onAudioLevel(callback: (level: number) => void): void;
-    function setContexts(contexts: any[]): void;
-    function resetContexts(resultCallback: (result: object) => void, errorCallback: (error: Error) => void): void;
-    function setPermanentContexts(contexts: any[]): void;
+    export function onListeningStarted(callback: () => void): void;
+    export function onListeningFinished(callback: () => void): void;
+    export function onAudioLevel(callback: (level: number) => void): void;
+    export function setContexts(contexts: any[]): void;
+    export function resetContexts(resultCallback: (result: object) => void, errorCallback: (error: Error) => void): void;
+    export function setPermanentContexts(contexts: any[]): void;
 }
