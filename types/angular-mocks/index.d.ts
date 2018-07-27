d5627ab7a85984f83dc30ede59d2cb8e6874b07d
Georgii Dolzhykov <thorn.mailbox@gmail.com>
2018-07-27 20:50:46 +0300

    angular-mocks: add myself to reviewers (#27620)
    * angular-mocks: add myself to reviewers
    
    * remove blank line

--- types/angular-mocks/index.d.ts
+++ types/angular-mocks/index.d.ts
@@ -1,6 +1,8 @@
 // Type definitions for Angular JS (ngMock, ngMockE2E module) 1.6
 // Project: http://angularjs.org
-// Definitions by: Diego Vilar <https://github.com/diegovilar>, Tony Curtis <https://github.com/daltin>
+// Definitions by: Diego Vilar <https://github.com/diegovilar>
+//                 Tony Curtis <https://github.com/daltin>
+//                 Georgii Dolzhykov <https://github.com/thorn0>
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
 // TypeScript Version: 2.4
 
