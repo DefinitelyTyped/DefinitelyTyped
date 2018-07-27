c34761a386d69cc337d276b80440a40f6af6e204
jicjjang <jicjjang12@gmail.com>
2018-07-27 14:35:26 +0900

    Updates new functions in @storybook/addon-actions

--- types/storybook__addon-actions/index.d.ts
+++ types/storybook__addon-actions/index.d.ts
@@ -1,11 +1,12 @@
-// Type definitions for @storybook/addon-actions 3.0
+// Type definitions for @storybook/addon-actions 3.4
 // Project: https://github.com/storybooks/storybook
-// Definitions by: Joscha Feth <https://github.com/joscha>
+// Definitions by: Joscha Feth <https://github.com/joscha>, June <https://github.com/jicjjang>
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
-// TypeScript Version: 2.6
+// TypeScript Version: 2.9
 
 export type HandlerFunction = (...args: any[]) => undefined;
 export type DecoratorFunction = (args: any[]) => any[];
 
-export function decorateAction(decorators: DecoratorFunction[]): (name: string) => HandlerFunction;
+export function decorateAction(decorators: DecoratorFunction[]): (name: string, options?: object) => HandlerFunction;
+export function configureActions(options: object): undefined;
 export function action(name: string): HandlerFunction;
