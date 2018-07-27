0f7a310414d879c756672c20c63b68b64893d92e
Arturas <a.molcanovas@gmail.com>
2018-07-27 13:18:52 +0100

    Add node-zopfli-es types

---
+++ types/node-zopfli-es/index.d.ts
@@ -0,0 +1,8 @@
+// Type definitions for node-zopfli-es 0.0
+// Project: https://github.com/jaeh/node-zopfli-es
+// Definitions by: My Self <https://github.com/me>
+// Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
+
+import * as zopfli from 'node-zopfli';
+
+export = zopfli;
