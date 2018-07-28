13b193e9ec7ffb0f4539155e3af3e7106396fee5
Art <a.molcanovas@gmail.com>
2018-07-28 10:49:25 +0100

    Update index.d.ts

--- types/node-zopfli-es/index.d.ts
+++ types/node-zopfli-es/index.d.ts
@@ -2,7 +2,6 @@
 // Project: https://github.com/jaeh/node-zopfli-es
 // Definitions by: Alorel <https://github.com/Alorel>
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
-// Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
 // TypeScript Version: 2.3
 
 /// <reference types="node" />
