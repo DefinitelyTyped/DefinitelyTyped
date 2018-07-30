f8485f63111d3fc453fa367c6aa961cdfe4b8e1f
Jurand Niemczycki <32194442+createdbyjurand@users.noreply.github.com>
2018-07-30 21:00:47 +0200

    espruino: added boolean to digitalwrite (#27577)
    * espruino added type for wifi module for access point
    
    * espruino added myself to definitions
    
    * changed tabsize to 4
    
    * espruino: added boolean to digitalwrite

--- types/espruino/espruino-tests.ts
+++ types/espruino/espruino-tests.ts
@@ -10,5 +10,8 @@ wifi.startAP('ssid', { password: 'pass', authMode: 'wpa_wpa2' }, err => {
     console.log("created");
 });
 
+digitalWrite(D2, false);
+digitalWrite(D2, true);
+
 I2C1.setup({ scl: D4, sda: D0 });
 Serial2.setup(9600, { rx: D16, tx: D17 });
