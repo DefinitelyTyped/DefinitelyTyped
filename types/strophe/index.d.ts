d280f2e41133c8d7e0c96faf792c2e7a064844ce
Tijs Zwinkels - TinkerTank <tijs@tinkertank.eu>
2018-07-30 13:23:29 +0200

    TS version 2.1 is high enough to make tests fail with prev. definitions.

--- types/strophe/index.d.ts
+++ types/strophe/index.d.ts
@@ -2,7 +2,7 @@
 // Project: http://strophe.im/strophejs/
 // Definitions by: David Deutsch <https://github.com/DavidKDeutsch>
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
-// TypeScript Version: 2.9
+// TypeScript Version: 2.1
 
 export namespace Strophe {
     /** Constant: VERSION
