2867e46cd4d7483cfb6a7e23ce43a5ce65eb9f63
Romain Faust <romain.faust@gmail.com>
2018-07-30 18:28:12 +0200

    Fix incorrect TypeScript version

--- types/koa-html-minifier/index.d.ts
+++ types/koa-html-minifier/index.d.ts
@@ -2,6 +2,7 @@
 // Project: https://github.com/koajs/html-minifier
 // Definitions by: Romain Faust <https://github.com/romain-faust>
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
+// TypeScript Version: 2.3
 
 import { Middleware } from 'koa';
 import { Options } from 'html-minifier';
