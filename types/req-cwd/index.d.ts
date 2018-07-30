d85a19e638485614857dffb83fda06f1778bd571
Arturas <a.molcanovas@gmail.com>
2018-07-30 10:28:45 +0100

    Set typescript version to 2.3

--- types/req-cwd/index.d.ts
+++ types/req-cwd/index.d.ts
@@ -2,6 +2,7 @@
 // Project: https://github.com/sindresorhus/req-cwd
 // Definitions by: Arturas Molcanovas <https://github.com/Alorel>
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
+// TypeScript Version: 2.3
 
 /** Like require(), throws when the module can't be found. */
 declare function reqCwd(moduleId: string): any;
