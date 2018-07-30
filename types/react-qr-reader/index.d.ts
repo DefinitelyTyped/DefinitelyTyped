6d06b55613b3ac51df62df344f5344adefd63740
davidkevork <davidkevork@hotmail.com>
2018-07-30 10:38:16 +1000

    Fix react-qr-reader issues

--- types/react-qr-reader/index.d.ts
+++ types/react-qr-reader/index.d.ts
@@ -1,4 +1,4 @@
-// Type definitions for react-qr-reader 2.1.0
+// Type definitions for react-qr-reader 2.1
 // Project: https://github.com/JodusNodus/react-qr-reader
 // Definitions by: David Kevork <https://github.com/davidkevork>
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
