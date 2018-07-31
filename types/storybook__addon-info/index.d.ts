59c946793fca2f3e30ea0d5a88739149ecf05ccb
Nicolas Cochard <ncochard@costar.co.uk>
2018-07-31 19:46:32 +0100

    The function withInfo in @storybook/addon-info should be able to accept no arguments.

--- types/storybook__addon-info/index.d.ts
+++ types/storybook__addon-info/index.d.ts
@@ -1,4 +1,4 @@
-// Type definitions for @storybook/addon-info 3.2
+// Type definitions for @storybook/addon-info 3.3
 // Project: https://github.com/storybooks/storybook
 // Definitions by: Mark Kornblum <https://github.com/mkornblum>
 //                 Mattias Wikstrom <https://github.com/fyrkant>
