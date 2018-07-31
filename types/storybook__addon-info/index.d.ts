5d47ac3b6a1096d59af70e42e0733171400cc958
Nicolas Cochard <ncochard@costar.co.uk>
2018-07-31 20:07:53 +0100

    Corrected the version number of @storybook/addon-info. This type definition was only tested with version 3.4.8 of @storybook/addon-info

--- types/storybook__addon-info/index.d.ts
+++ types/storybook__addon-info/index.d.ts
@@ -1,4 +1,4 @@
-// Type definitions for @storybook/addon-info 3.4.8
+// Type definitions for @storybook/addon-info 3.4
 // Project: https://github.com/storybooks/storybook
 // Definitions by: Mark Kornblum <https://github.com/mkornblum>
 //                 Mattias Wikstrom <https://github.com/fyrkant>
