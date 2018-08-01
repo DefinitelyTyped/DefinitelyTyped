253339569f35a44d11a461d89754179bee899974
Jimmy Shimizu <jimmy@shimizu.se>
2018-08-01 20:38:08 +0200

    fix: Correct KeepAlive settings for 2.2 and 3.x versions (#27753)

--- types/mongodb/v2/index.d.ts
+++ types/mongodb/v2/index.d.ts
@@ -158,8 +158,10 @@ export interface SocketOptions {
     autoReconnect?: boolean;
     // TCP Socket NoDelay option. default:true
     noDelay?: boolean;
-    // TCP KeepAlive on the socket with a X ms delay before start. default:0
-    keepAlive?: number;
+    // TCP KeepAlive enabled on the socket. default:true
+    keepAlive?: boolean;
+    // TCP KeepAlive initial delay before sending first keep-alive packet when idle. default:300000
+    keepAliveInitialDelay?: number;
     // TCP Connection timeout setting. default 0
     connectTimeoutMS?: number;
     // TCP Socket timeout setting. default 0
