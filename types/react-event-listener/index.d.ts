786f4ee08d9e0485269d7ae90607d35f12843590
Ferdy Budhidharma <ferd312@gmail.com>
2018-07-31 22:36:29 -0500

    feat(react-dependents): update to TS 2.8

--- types/react-event-listener/index.d.ts
+++ types/react-event-listener/index.d.ts
@@ -2,7 +2,7 @@
 // Project: https://github.com/oliviertassinari/react-event-listener
 // Definitions by: Alexey Svetliakov <https://github.com/asvetliakov>
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
-// TypeScript Version: 2.6
+// TypeScript Version: 2.8
 
 import * as React from "react";
 
--- types/react-highlight-words/index.d.ts
+++ types/react-highlight-words/index.d.ts
@@ -3,7 +3,7 @@
 // Definitions by: Mohamed Hegazy <https://github.com/mhegazy>
 //                 Diogo Corrêa <https://github.com/diogodca>
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
-// TypeScript Version: 2.6
+// TypeScript Version: 2.8
 
 import * as React from "react";
 
