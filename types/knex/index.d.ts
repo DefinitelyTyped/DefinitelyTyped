12be6e51b75212221322a8d078c293abe0aa06da
Scott Trinh <scott@ca.la>
2018-08-02 14:26:48 -0400

    [knex] Allow QueryBuilder in union parameters

--- types/knex/index.d.ts
+++ types/knex/index.d.ts
@@ -312,9 +312,9 @@ declare namespace Knex {
     }
 
     interface Union {
-        (callback: QueryCallback, wrap?: boolean): QueryBuilder;
-        (callbacks: QueryCallback[], wrap?: boolean): QueryBuilder;
-        (...callbacks: QueryCallback[]): QueryBuilder;
+        (callback: QueryCallback | QueryBuilder, wrap?: boolean): QueryBuilder;
+        (callbacks: (QueryCallback | QueryBuilder)[], wrap?: boolean): QueryBuilder;
+        (...callbacks: (QueryCallback | QueryBuilder)[]): QueryBuilder;
         // (...callbacks: QueryCallback[], wrap?: boolean): QueryInterface;
     }
 
