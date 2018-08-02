7f1df1af8567a837230c356d5b991f3a40276513
Max Rumpf <max.rumpf1998@gmail.com>
2018-08-03 01:24:23 +0200

    Increase version number

--- types/dnssd/index.d.ts
+++ types/dnssd/index.d.ts
@@ -1,4 +1,4 @@
-// Type definitions for dnssd 0.3
+// Type definitions for dnssd 0.4
 // Project: https://github.com/DeMille/dnssd.js#readme
 // Definitions by: Angel Merino <https://github.com/angelmerino>
 //                 Max Rumpf <https://github.com/Maxr1998>
