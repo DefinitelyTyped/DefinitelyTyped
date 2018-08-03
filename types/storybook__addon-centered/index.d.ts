349d3c4b94a37f5e28f7264da06acd3f63555e96
Ferdy Budhidharma <ferd312@gmail.com>
2018-08-03 13:30:13 -0500

    fix version mismatches

--- types/storybook__addon-centered/index.d.ts
+++ types/storybook__addon-centered/index.d.ts
@@ -2,7 +2,7 @@
 // Project: https://github.com/storybooks/storybook
 // Definitions by: kiyopikko <https://github.com/kiyopikko>
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
-// TypeScript Version: 2.6
+// TypeScript Version: 2.8
 
 import * as React from "react";
 import { StoryDecorator } from "@storybook/react";
