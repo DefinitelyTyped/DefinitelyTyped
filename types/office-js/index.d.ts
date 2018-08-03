20b2f1f9f1917247247769339d5c19b6f9e903bb
Elizabeth Samuel <elizs@microsoft.com>
2018-08-03 08:46:59 -0700

    office-js: Update links

--- types/office-js/index.d.ts
+++ types/office-js/index.d.ts
@@ -280,7 +280,7 @@ declare namespace Office {
      * 
      * @remarks
      * 
-     * Returned by the {@link Office.AsyncResult.status | status} property of the {@link Office.AsyncResult | AsyncResult} object.
+     * Returned by the `status` property of the {@link Office.AsyncResult | AsyncResult} object.
      * 
      * **Support details**
      * 
@@ -477,9 +477,9 @@ declare namespace Office {
         * @remarks
         * <table><tr><td>Hosts</td><td>Access, Excel, Outlook, PowerPoint, Project, Word</td></tr></table>
         * 
-        * You access the AsyncResult object in the function passed as the argument to the callback parameter of an "Async" method, such as the {@link Office.Document.getSelectedDataAsync | getSelectedDataAsync} and {@link Office.Document.setSelectedDataAsync | setSelectedDataAsync} methods of the Document object.
+        * You access the AsyncResult object in the function passed as the argument to the callback parameter of an "Async" method, such as the `getSelectedDataAsync` and `setSelectedDataAsync` methods of the {@link Office.Document | Document} object.
         *
-        * Note: What the value property returns for a particular "Async" method varies depending on the purpose and context of that method. To determine what is returned by the value property for an "Async" method, refer to the "Callback value" section of the method's topic. For a complete listing of the "Async" methods, see the Remarks section of the {@link Office.AsyncResult | AsyncResult} object topic.
+        * Note: What the value property returns for a particular "Async" method varies depending on the purpose and context of that method. To determine what is returned by the value property for an "Async" method, refer to the "Callback value" section of the method's topic.
         */
         value: any;
     }
@@ -1334,7 +1334,7 @@ declare namespace Office {
         /**
          * Return a JSON object that contains an array of the ids, titles, and indexes of the selected slides. For example, `{"slides":[{"id":257,"title":"Slide 2","index":2},{"id":256,"title":"Slide 1","index":1}]}` for a selection of two slides.
          * 
-         * Note: Only applies to data in PowerPoint when calling the {@link Office.Document.getSelectedData | Document.getSelectedData} method to get the current slide or selected range of slides.
+         * Note: Only applies to data in PowerPoint when calling the `{@link Office.Document | Document}.getSelectedData` method to get the current slide or selected range of slides.
          */
         SlideRange,
         /**
@@ -1665,7 +1665,7 @@ declare namespace Office {
         Previous
     }
     /**
-     * Specifies whether to select (highlight) the location to navigate to (when using the {@link Office.Document.goToByIdAsync | Document.goToByIdAsync} method).
+     * Specifies whether to select (highlight) the location to navigate to (when using the `{@link Office.Document | Document}.goToByIdAsync` method).
      *
      * @remarks
      * 
@@ -3716,11 +3716,11 @@ declare namespace Office {
     */
     interface TextBinding extends Binding { }
     /**
-     * Specifies the project fields that are available as a parameter for the {@link Office.Document.getProjectFieldAsync | getProjectFieldAsync} method.
+     * Specifies the project fields that are available as a parameter for the `{@link Office.Document | Document}.getProjectFieldAsync` method.
      *
      * @remarks
      * 
-     * A ProjectProjectFields constant can be used as a parameter of the {@link Office.Document.getProjectFieldAsync | getProjectFieldAsync} method.
+     * A ProjectProjectFields constant can be used as a parameter of the `{@link Office.Document | Document}.getProjectFieldAsync` method.
      * 
      * **Support details**
      * 
@@ -3786,10 +3786,10 @@ declare namespace Office {
         WSSList
     }
     /**
-     * Specifies the resource fields that are available as a parameter for the {@link Office.Document.getResourceFieldAsync | getResourceFieldAsync} method.
+     * Specifies the resource fields that are available as a parameter for the `{@link Office.Document | Document}.getResourceFieldAsync` method.
      *
      * @remarks
-     * A ProjectResourceFields constant can be used as a parameter of the {@link Office.Document.getResourceFieldAsync | getResourceFieldAsync} method.
+     * A ProjectResourceFields constant can be used as a parameter of the `{@link Office.Document | Document}.getResourceFieldAsync` method.
      *
      * For more information about working with fields in Project, see {@link https://support.office.com/article/Available-fields-reference-615a4563-1cc3-40f4-b66f-1b17e793a460 | Available fields} reference. In Project Help, search for Available fields.
      * 
@@ -4608,10 +4608,10 @@ declare namespace Office {
         Text9
     }
     /**
-     * Specifies the task fields that are available as a parameter for the {@link Office.Document.getTaskFieldAsync | getTaskFieldAsync} method.
+     * Specifies the task fields that are available as a parameter for the `{@link Office.Document | Document}.getTaskFieldAsync` method.
      *
      * @remarks
-     * A ProjectTaskFields constant can be used as a parameter of the {@link Office.Document.getTaskFieldAsync | getTaskFieldAsync} method.
+     * A ProjectTaskFields constant can be used as a parameter of the `{@link Office.Document | Document}.getTaskFieldAsync` method.
      *
      * For more information about working with fields in Project, see the {@link https://support.office.com/article/Available-fields-reference-615a4563-1cc3-40f4-b66f-1b17e793a460 | Available fields} reference. In Project Help, search for Available fields.
      * 
@@ -5756,10 +5756,10 @@ declare namespace Office {
         WSSID
     }
     /**
-     * Specifies the types of views that the {@link Office.Document.getSelectedViewAsync | getSelectedViewAsync} method can recognize.
+     * Specifies the types of views that the `{@link Office.Document | Document}.getSelectedViewAsync` method can recognize.
      *
      * @remarks
-     * The {@link Office.Document.getSelectedViewAsync | getSelectedViewAsync} method returns the ProjectViewTypes constant value and name that corresponds to the active view.
+     * The `{@link Office.Document | Document}.getSelectedViewAsync` method returns the ProjectViewTypes constant value and name that corresponds to the active view.
      * 
      * **Support details**
      * 
