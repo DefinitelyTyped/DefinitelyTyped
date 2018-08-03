d0fb538778790a643e76164fef0bb4de77b638f9
rdrgn <rdrgn@users.noreply.github.com>
2018-08-04 06:00:48 +0900

    fix format

--- types/react-resize-detector/index.d.ts
+++ types/react-resize-detector/index.d.ts
@@ -4,32 +4,32 @@
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
 // TypeScript Version: 2.6
 
-import * as React from 'react';
+import * as React from "react";
 
 interface ReactResizeDetectorProps extends React.Props<ReactResizeDetector> {
-  /** Function that will be invoked with width and height arguments */
-  onResize?: (width: number, height: number) => void;
-  /** Trigger onResize on height change. Default: false */
-  handleHeight?: boolean;
-  /** Trigger onResize on width change. Default: false */
-  handleWidth?: boolean;
-  /** Do not trigger onResize when a component mounts. Default: false */
-  skipOnMount?: boolean;
-  /** Id of the element we want to observe. If none provided, parentElement of the component will be used. Default: "" */
-  resizableElementId?: string;
-  /** Possible values: throttle and debounce */
-  refreshMode?: 'throttle' | 'debounce';
-  /** Makes sense only when refreshMode is set. Default: 1000. */
-  refreshRate?: number;
+    /** Function that will be invoked with width and height arguments */
+    onResize?: (width: number, height: number) => void;
+    /** Trigger onResize on height change. Default: false */
+    handleHeight?: boolean;
+    /** Trigger onResize on width change. Default: false */
+    handleWidth?: boolean;
+    /** Do not trigger onResize when a component mounts. Default: false */
+    skipOnMount?: boolean;
+    /** Id of the element we want to observe. If none provided, parentElement of the component will be used. Default: "" */
+    resizableElementId?: string;
+    /** Possible values: throttle and debounce */
+    refreshMode?: "throttle" | "debounce";
+    /** Makes sense only when refreshMode is set. Default: 1000. */
+    refreshRate?: number;
 
-  render?: (props: any) => React.ReactNode;
+    render?: (props: any) => React.ReactNode;
 }
 
-declare class ReactResizeDetector extends React.PureComponent<ReactResizeDetectorProps> {}
+declare class ReactResizeDetector extends React.PureComponent<ReactResizeDetectorProps> { }
 
 export declare function withResizeDetector(
-  WrappedComponent: React.ReactNode,
-  props?: ReactResizeDetectorProps
+    WrappedComponent: React.ReactNode,
+    props?: ReactResizeDetectorProps
 ): React.Component;
 
 export default ReactResizeDetector;
