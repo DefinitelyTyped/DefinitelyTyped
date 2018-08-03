4c8b1f9c081ca3b56efa49780ba839bbed248321
Nikolai Ommundsen <post@niikoo.net>
2018-08-03 17:01:12 +0200

    Bumped typescript version to support new language features.

--- types/chrome-apps/index.d.ts
+++ types/chrome-apps/index.d.ts
@@ -2,7 +2,7 @@
 // Project: http://developer.chrome.com/apps/
 // Definitions by: Nikolai Ommundsen <https://github.com/niikoo>, Adam Lay <https://github.com/AdamLay>, MIZUNE Pine <https://github.com/pine613>, MIZUSHIMA Junki <https://github.com/mzsm>, Ingconst Stepanyan <https://github.com/RReverser>, Adam Pyle <https://github.com/pyle>, Matthew Kimber <https://github.com/matthewkimber>, otiai10 <https://github.com/otiai10>, couven92 <https://github.com/couven92>, RReverser <https://github.com/rreverser>, sreimer15 <https://github.com/sreimer15>
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
-// TypeScript Version: 2.6
+// TypeScript Version: 2.8
 
 /// <reference types='filesystem'/>
 
