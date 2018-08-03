6d2fc7181a0e01417fdfba6fa511ae9bd9a553bd
Ferdy Budhidharma <ferd312@gmail.com>
2018-08-03 15:01:14 -0500

    feat(react-dependents): update to TS 2.8 (part 1 of 2) (#27743)
    * feat(react-dependents): update to TS 2.8
    
    * exclude react-dom
    
    * fix version mismatches

--- types/react-facebook-login/index.d.ts
+++ types/react-facebook-login/index.d.ts
@@ -2,7 +2,7 @@
 // Project: https://github.com/keppelen/react-facebook-login
 // Definitions by: Alexandre Paré <https://github.com/apare>, Jan Karres <https://github.com/jankarres>
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
-// TypeScript Version: 2.6
+// TypeScript Version: 2.8
 
 import * as React from "react";
 
