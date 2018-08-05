74cbee6e0d263a28a2b000c70855a6c14a2fc04b
rdrgn <rdrgn@users.noreply.github.com>
2018-08-05 20:47:14 +0900

    add an author

--- types/react-resize-detector/index.d.ts
+++ types/react-resize-detector/index.d.ts
@@ -1,6 +1,8 @@
 // Type definitions for react-resize-detector 3.1
 // Project: https://github.com/maslianok/react-resize-detector
-// Definitions by: Matthew James <https://github.com/matthew-matvei>, James Greenleaf <https://github.com/aMoniker>
+// Definitions by: Matthew James <https://github.com/matthew-matvei>
+//                 James Greenleaf <https://github.com/aMoniker>
+//                 Remin <https://github.com/rdrgn>
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
 // TypeScript Version: 2.6
 
