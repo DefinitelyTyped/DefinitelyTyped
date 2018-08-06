1d9679b2dcf7c9643d623b626c0820f84eaa8d7e
Ferdy Budhidharma <ferd312@gmail.com>
2018-08-06 12:51:26 -0500

    [react] add library managed attributes and upgrade TS requirement to 2.8 (#27901)
    * feat(react): add library managed attributes and upgrade to 2.8
    
    * fix broken tests
    
    * fix lint errors

--- types/recompose/recompose-tests.tsx
+++ types/recompose/recompose-tests.tsx
@@ -257,7 +257,7 @@ function testWithState() {
     // We can also actually provide the generic necessary
     const enhancer3 = withState<OutterProps, number, "count", "setCount">("count", "setCount", 1);
     const Enhanced3 = enhancer3(props => {
-        return <div>{props.title}</div>; 
+        return <div>{props.count}</div>;
     });
     const rendered3 = (
         <Enhanced3 title="foo" />
