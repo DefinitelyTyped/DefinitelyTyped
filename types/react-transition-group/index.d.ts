5a65652c43717633a826ca1e1dcceefce232c255
Masafumi Koba <ybiquitous@gmail.com>
2018-08-06 18:54:40 +0900

    [react-transition-group] Restrict Transition child function's argument type
    > A `function` child can be used instead of a React element.
    > This function is called with the current transition status
    > ('entering', 'entered', 'exiting', 'exited', 'unmounted'), which can be used
    > to apply context specific props to a component.
    
    https://github.com/reactjs/react-transition-group/blob/b8da4f2f2314b23a6471d78cb14c22efe13481dd/src/Transition.js#L379
    
    See also #27693

--- types/react-transition-group/index.d.ts
+++ types/react-transition-group/index.d.ts
@@ -2,6 +2,7 @@
 // Project: https://github.com/reactjs/react-transition-group
 // Definitions by: Karol Janyst <https://github.com/LKay>
 //                 Epskampie <https://github.com/Epskampie>
+//                 Masafumi Koba <https://github.com/ybiquitous>
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
 // TypeScript Version: 2.8
 
