9b8b697b902638e8bc8a4b88b3e557d3b048dfdb
yasupeke <yasupeke2@gmail.com>
2018-08-09 21:30:43 +0900

    update react-paginate

--- types/react-paginate/v4/index.d.ts
+++ types/react-paginate/v4/index.d.ts
@@ -1,8 +1,11 @@
-// Type definitions for react-paginate 5.2
+// Type definitions for react-paginate 4.3
 // Project: https://github.com/AdeleD/react-paginate
-// Definitions by: Yasunori Ohoka <https://github.com/yasupeke>
+// Definitions by: Simon Hartcher <https://github.com/deevus>
+//                 Wouter Hardeman <https://github.com/wouterhardeman>
+//                 pegel03 <https://github.com/pegel03>
+//                 Simon Archer <https://github.com/archy-bold>
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
-// TypeScript Version: 2.9
+// TypeScript Version: 2.8
 
 import * as React from 'react';
 
@@ -119,4 +122,4 @@ interface ReactPaginateProps {
 }
 
 declare const ReactPaginate: React.ComponentClass<ReactPaginateProps>;
-export default ReactPaginate;
+export = ReactPaginate;
