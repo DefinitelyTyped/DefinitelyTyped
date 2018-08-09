534d41d8624ec4431ed990e3bb1a5d1923d80a3a
Abraham White <abelincoln.white@gmail.com>
2018-08-09 14:12:15 -0600

    Add QuickAction class and use Callback type
    The callback definitions for all of these methods were a mess. I
    created a Callback type and had every callback parameter use this type
    for consistency.
    
    I also went through and added comments for places where the types do not
    match the documentation

---
+++ types/jsforce/global.d.ts
@@ -0,0 +1,1 @@
+export type Callback<T> = (err: Error | null, ret: T) => void;
--- types/jsforce/salesforce-object.d.ts
+++ types/jsforce/salesforce-object.d.ts
@@ -5,79 +5,97 @@ import { DescribeSObjectResult } from './describe-result';
 import { Query } from './query';
 import { Record, RecordReference } from './record';
 import { RecordResult } from './record-result';
-import { Connection } from './connection';
+import { Connection, Callback } from './connection';
 import { SalesforceId } from './salesforce-id';
 import { Batch, BatchResultInfo } from './batch';
+import { QuickAction, QuickActionInfo } from './quick-action';
 
 export class SObject<T> {
     record(id: SalesforceId): RecordReference<T>;
-    retrieve(id: SalesforceId, options?: Object, callback?: (err: Error, record: Record<T>) => void): Promise<Record<T>>;
-    retrieve(ids: SalesforceId[], options?: Object, callback?: (err: Error, ret: Array<Record<T>>) => void): Promise<Array<Record<T>>>;
-    update(record: Partial<T>, options?: Object, callback?: (err: Error, ret: RecordResult) => void): Promise<RecordResult>;
-    update(records: Array<Partial<T>>, options?: Object, callback?: (err: Error, ret: RecordResult[]) => void): Promise<RecordResult[]>;
-    updateBulk(input?: Record[] | stream.Stream | string, callback?: (err: Error, ret: RecordResult[]) => void): Batch;
-    updated(start: string | Date, end: string | Date, callback?: (err: Error, ret: UpdatedRecordsInfo) => void): Promise<UpdatedRecordsInfo>;
-    upsert(records: Record<T>, extIdField: SalesforceId, options?: Object, callback?: (err: Error, ret: RecordResult) => void): Promise<RecordResult>;
-    upsert(records: Array<Record<T>>, extIdField: SalesforceId, options?: Object, callback?: (err: Error, ret: RecordResult[]) => void): Promise<RecordResult[]>;
-    upsertBulk(input?: Array<Record<T>> | stream.Stream | string, callback?: (err: Error, ret: RecordResult[] | BatchResultInfo[]) => void): Batch;
+    retrieve(id: SalesforceId, options?: Object, callback?: Callback<Record<T>>): Promise<Record<T>>;
+    retrieve(ids: SalesforceId[], options?: Object, callback?: Callback<Array<Record<T>>>): Promise<Array<Record<T>>>;
+    update(record: Partial<T>, options?: Object, callback?: Callback<RecordResult>): Promise<RecordResult>;
+    update(records: Array<Partial<T>>, options?: Object, callback?: Callback<RecordResult[]>): Promise<RecordResult[]>;
+    // FIXME: should input really be optional? the documentation says so, but how can you actually update without it?
+    updateBulk(input?: Record[] | stream.Stream | string, callback?: Callback<RecordResult[]>): Batch;
+    updated(start: string | Date, end: string | Date, callback?: Callback<UpdatedRecordsInfo>): Promise<UpdatedRecordsInfo>;
+    upsert(records: Record<T>, extIdField: SalesforceId, options?: Object, callback?: Callback<RecordResult>): Promise<RecordResult>;
+    upsert(records: Array<Record<T>>, extIdField: SalesforceId, options?: Object, callback?: Callback<RecordResult[]>): Promise<RecordResult[]>;
+    upsertBulk(input?: Array<Record<T>> | stream.Stream | string, callback?: Callback<RecordResult[] | BatchResultInfo[]>): Batch;
 
-    find<T>(query?: any, callback?: (err: Error, ret: T[]) => void): Query<T[]>;
-    find<T>(query?: any, fields?: Object | string[] | string, callback?: (err: Error, ret: T[]) => void): Query<T[]>;
-    find<T>(query?: any, fields?: Object | string[] | string, options?: Object, callback?: (err: Error, ret: T[]) => void): Query<T[]>;
+    find<T>(query?: object | string, callback?: Callback<Array<Record<T>>>): Query<Array<Record<T>>>;
+    find<T>(query?: object | string, fields?: Object | string[] | string, callback?: Callback<Array<Record<T>>>): Query<Array<Record<T>>>;
+    find<T>(query?: object | string, fields?: Object | string[] | string, options?: FindOptions, callback?: Callback<Array<Record<T>>>): Query<Array<Record<T>>>;
 
-    findOne<T>(query?: any, callback?: (err: Error, ret: T) => void): Query<T>;
-    findOne<T>(query?: any, fields?: Object | string[] | string, callback?: (err: Error, ret: T) => void): Query<T>;
-    findOne<T>(query?: any, fields?: Object | string[] | string, options?: Object, callback?: (err: Error, ret: T) => void): Query<T>;
+    findOne<T>(query?: object | string, callback?: Callback<Record<T>>): Query<Record<T>>;
+    findOne<T>(query?: object | string, fields?: Object | string[] | string, callback?: Callback<Record<T>>): Query<Record<T>>;
+    findOne<T>(query?: object | string, fields?: Object | string[] | string, options?: FindOptions, callback?: Callback<Record<T>>): Query<Record<T>>;
 
     approvalLayouts$: {
         /** Returns a value from the cache if it exists, otherwise calls SObject.approvalLayouts */
-        (callback?: (err: Error, layoutInfo: ApprovalLayoutInfo) => void): ApprovalLayoutInfo;
+        (callback?: Callback<ApprovalLayoutInfo>): ApprovalLayoutInfo;
         clear(): void;
     }
-    approvalLayouts(callback?: (layoutInfo: ApprovalLayoutInfo) => void): Promise<ApprovalLayoutInfo>;
-    bulkload(operation: string, options?: { extIdField?: string }, input?: Array<Record<T>> | stream.Stream | string, callback?: (err: Error, ret: RecordResult[]) => void): Batch;
+    approvalLayouts(callback?: Callback<ApprovalLayoutInfo>): Promise<ApprovalLayoutInfo>;
+    bulkload(operation: string, options?: { extIdField?: string }, input?: Array<Record<T>> | stream.Stream | string, callback?: Callback<RecordResult[]>): Batch;
     compactLayouts$: {
         /** Returns a value from the cache if it exists, otherwise calls SObject.compactLayouts */
-        (callback?: (err: Error, layoutInfo: CompactLayoutInfo) => void): CompactLayoutInfo;
+        (callback?: Callback<CompactLayoutInfo>): CompactLayoutInfo;
         clear(): void;
     }
-    compactLayouts(callback?: CompactLayoutInfo): Promise<CompactLayoutInfo>;
-    count(conditions?: Object | string, callback?: (err: Error, num: number) => void): Query<number>;
-    create(options: any | any[], callback?: (err: Error, ret: RecordResult | RecordResult[]) => void): Promise<RecordResult | RecordResult[]>;
-    createBulk(input?: Array<Record<T>> | stream.Stream | string, callback?: (err: Error, ret: RecordResult) => void): Batch;
-    del(id: string, callback?: (err: Error, ret: RecordResult) => void): Promise<RecordResult>;
-    del(ids: string[], callback?: (err: Error, ret: RecordResult[]) => void): Promise<RecordResult[]>;
-    destroy(id: string, callback?: (err: Error, ret: RecordResult) => void): Promise<RecordResult>;
-    destroy(ids: string[], callback?: (err: Error, ret: RecordResult[]) => void): Promise<RecordResult[]>;
-    delete(id: string, callback?: (err: Error, ret: RecordResult) => void): Promise<RecordResult>;
-    delete(ids: string[], callback?: (err: Error, ret: RecordResult[]) => void): Promise<RecordResult[]>;
-    deleteBulk(input?: Array<Record<T>> | stream.Stream | string, callback?: (err: Error, ret: RecordResult) => void): Batch;
-    destroyBulk(input?: Array<Record<T>> | stream.Stream | string, callback?: (err: Error, ret: RecordResult) => void): Batch;
-    destroyHardBulk(input?: Array<Record<T>> | stream.Stream | string, callback?: (err: Error, ret: RecordResult) => void): Batch;
-    deleted(start: Date | string, end: Date | string, callback?: (info: DeletedRecordsInfo) => void): Promise<DeletedRecordsInfo>;
-    deleteHardBulk(input?: Array<Record<T>> | stream.Stream | string, callback?: (err: Error, ret: RecordResult) => void): Batch;
-    describe(callback?: (err: Error, ret: DescribeSObjectResult) => void): Promise<DescribeSObjectResult>;
+    compactLayouts(callback?: Callback<CompactLayoutInfo>): Promise<CompactLayoutInfo>;
+    count(conditions?: Object | string, callback?: Callback<number>): Query<number>;
+    create(record: Record<T>, options: object, callback?: Callback<RecordResult>): Promise<RecordResult>;
+    create(record: Record<T>, callback?: Callback<RecordResult>): Promise<RecordResult>;
+    create(record: Array<Record<T>>, options: object, callback?: Callback<RecordResult[]>): Promise<RecordResult[]>;
+    create(record: Array<Record<T>>, callback?: Callback<RecordResult[]>): Promise<RecordResult[]>;
+    // FIXME: why does the callback return a single RecordResult instead of an array as in the documentation?
+    createBulk(input?: Array<Record<T>> | stream.Stream | string, callback?: Callback<RecordResult>): Batch;
+    del(id: string, callback?: Callback<RecordResult>): Promise<RecordResult>;
+    del(ids: string[], callback?: Callback<RecordResult[]>): Promise<RecordResult[]>;
+    destroy(id: string, callback?: Callback<RecordResult>): Promise<RecordResult>;
+    destroy(ids: string[], callback?: Callback<RecordResult[]>): Promise<RecordResult[]>;
+    delete(id: string, callback?: Callback<RecordResult>): Promise<RecordResult>;
+    delete(ids: string[], callback?: Callback<RecordResult[]>): Promise<RecordResult[]>;
+    // FIXME: why does the callback return a single RecordResult instead of an array as in the documentation?
+    deleteBulk(input?: Array<Record<T>> | stream.Stream | string, callback?: Callback<RecordResult>): Batch;
+    // FIXME: why does the callback return a single RecordResult instead of an array as in the documentation?
+    destroyBulk(input?: Array<Record<T>> | stream.Stream | string, callback?: Callback<RecordResult>): Batch;
+    // FIXME: why does the callback return a single RecordResult instead of an array as in the documentation?
+    destroyHardBulk(input?: Array<Record<T>> | stream.Stream | string, callback?: Callback<RecordResult>): Batch;
+    deleted(start: Date | string, end: Date | string, callback?: Callback<DeletedRecordsInfo>): Promise<DeletedRecordsInfo>;
+    // FIXME: why does the callback return a single RecordResult instead of an array as in the documentation?
+    deleteHardBulk(input?: Array<Record<T>> | stream.Stream | string, callback?: Callback<RecordResult>): Batch;
+    describe(callback?: Callback<DescribeSObjectResult>): Promise<DescribeSObjectResult>;
     describe$: {
         /** Returns a value from the cache if it exists, otherwise calls SObject.describe */
-        (callback?: (err: Error, ret: DescribeSObjectResult) => void): DescribeSObjectResult;
+        (callback?: Callback<DescribeSObjectResult>): DescribeSObjectResult;
         clear(): void;
     }
-    insert(options: any | any[], callback?: (err: Error, ret: RecordResult | RecordResult[]) => void): Promise<RecordResult | RecordResult[]>;
-    insertBulk(input?: Array<Record<T>> | stream.Stream | string, callback?: (err: Error, ret: RecordResult) => void): Batch;
+    insert(record: Record<T>, callback?: Callback<RecordResult>): Promise<RecordResult>;
+    insert(records: Array<Record<T>>, callback?: Callback<RecordResult[]>): Promise<RecordResult[]>;
+    insertBulk(input?: Array<Record<T>> | stream.Stream | string, callback?: Callback<RecordResult>): Batch;
     /** Returns a value from the cache if it exists, otherwise calls SObject.layouts */
     layouts$: {
-        (layoutName?: string, callback?: (err: Error, info: LayoutInfo) => void): LayoutInfo;
+        (layoutName?: string, callback?: Callback<LayoutInfo>): LayoutInfo;
         clear(): void;
     }
-    layouts(layoutName?: string, callback?: (err: Error, info: LayoutInfo) => void): Promise<LayoutInfo>;
+    layouts(layoutName?: string, callback?: Callback<LayoutInfo>): Promise<LayoutInfo>;
     listview(id: string): ListView;
-    listviews(callback?: (err: Error, info: ListViewsInfo) => void): Promise<ListViewsInfo>;
+    listviews(callback?: Callback<ListViewsInfo>): Promise<ListViewsInfo>;
     quickAction(actionName: string): QuickAction;
-    quickActions(callback?: (err: Error, info: any) => void): Promise<any>;
-    recent(callback?: (err: Error, ret: RecordResult) => void): Promise<RecordResult>;
-    select(callback?: (err: Error, ret: T[]) => void): Query<T[]>;
+    quickActions(callback?: Callback<QuickActionInfo[]>): Promise<QuickActionInfo[]>;
+    // FIXME: why does the callback return a single RecordResult instead of an array as in the documentation?
+    recent(callback?: Callback<RecordResult>): Promise<RecordResult>;
+    select(callback?: Callback<T[]>): Query<T[]>;
     // TODO:use a typed pluck to turn `fields` into a subset of T's fields so that the output is slimmed down appropriately
-    select(fields?: {[P in keyof T]: boolean}  | Array<(keyof T)> | (keyof T), callback?: (err: Error, ret: Array<Partial<T>>) => void): Query<Array<Partial<T>>>;
+    select(fields?: {[P in keyof T]: boolean}  | Array<(keyof T)> | (keyof T), callback?: Callback<Array<Partial<T>>>): Query<Array<Partial<T>>>;
+}
+
+export interface FindOptions {
+    limit?: number;
+    offset?: number;
+    skip?: number;
 }
 
 export interface UpdatedRecordsInfo {
@@ -114,4 +132,5 @@ export class ListView {
 }
 
 export class ListViewsInfo { }
-export class QuickAction { }
+// TODO: Remove this export
+export { QuickAction } // for compatibility if anyone had imported it from this file
