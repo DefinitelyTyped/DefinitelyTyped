641c48d48e5cbe1b9fd22cb3677c32eddf275ad7
Thomas den Hollander <denhollander.thomas@gmail.com>
2018-08-09 11:55:41 +0200

    Add types for ttf2woff2

---
+++ types/ttf2woff2/index.d.ts
@@ -0,0 +1,10 @@
+// Type definitions for ttf2woff2 2.0
+// Project: https://github.com/nfroidure/ttf2woff2
+// Definitions by: My Self <https://github.com/me>
+// Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
+
+/// <reference types="node" />
+
+declare function ttf2woff2(inputContent: Buffer): Buffer;
+
+export = ttf2woff2;
