71ade4b2ce64736e4624ed54a54954dca2dc8578
Luke Schoen <ltfschoen@gmail.com>
2018-08-09 16:48:37 +0200

    Add type definitions for react-file-reader

---
+++ types/react-file-reader/index.d.ts
@@ -0,0 +1,30 @@
+// Type definitions for react-file-reader
+// Project: https://www.npmjs.com/package/react-file-reader
+// Definitions by: Luke Schoen <https://github.com/ltfschoen>
+//                 Jaco Greeff <https://github.com/jacogr>
+// Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
+// TypeScript Version: 2.8
+
+///<reference types="react" />
+
+import React, { Component, ReactElement } from "react";
+
+export type FileTypes = '.csv' | '.zip' | '.json' | 'image/*';
+
+declare module "react-file-reader" {
+    interface ReactFileReaderProps {
+        fileTypes: FileTypes | Array<FileTypes>
+        multipleFiles: boolean,
+        base64: boolean,
+        disabled: boolean,
+        handleFiles: (event: React.SyntheticEvent<any>, fileList: ReadonlyArray<FileList>) => void;
+        children: any;
+    }
+
+    interface FileReader {
+        new(): FileReader;
+    }
+
+    export class ReactFileReader extends React.Component<ReactFileReaderProps> {
+    }
+}
