cb332fb8495d401358b25ae31395bc630612b2f6
felix-hoc <kaminwurschze@yahoo.de>
2018-08-10 11:24:46 +0200

    Fix some linting issues.

--- types/winston-syslog/index.d.ts
+++ types/winston-syslog/index.d.ts
@@ -1,4 +1,4 @@
-// Type definitions for winston-syslog v2.0.0
+// Type definitions for winston-syslog 2.0
 // Project: https://github.com/winstonjs/winston-syslog
 // Definitions by: Felix Hochgruber <https://github.com/felix-hoc>
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
@@ -7,30 +7,26 @@ import * as Transport from 'winston-transport';
 import * as dgram from 'dgram';
 import * as net from 'net';
 
-declare module 'winston-syslog' {
+export interface SyslogTransportOptions extends Transport.TransportStreamOptions {
+    host?: string;
+    port?: number;
+    path?: string;
+    protocol?: string;
+    pid?: number;
+    facility?: string;
+    localhost?: string;
+    type?: string;
+    app_name?: string;
+    eol?: string;
+}
 
-    export interface SyslogTransportOptions extends Transport.TransportStreamOptions {
-        host?: string;
-        port?: number;
-        path?: string;
-        protocol?: string;
-        pid?: number;
-        facility?: string;
-        localhost?: string;
-        type?: string;
-        app_name?: string;
-        eol?: string;
-    }
+export interface SyslogTransportInstance extends Transport {
+    producer: any;
+    socket: dgram.Socket | net.Socket;
 
-    interface SyslogTransportInstance extends Transport {
+    connect(callback: (err: (true | null)) => any): void;
 
-        producer: any;
-        socket: dgram.Socket | net.Socket;
-
-        connect(callback: (err: (true | null)) => any): void;
-
-        new(options?: SyslogTransportOptions): SyslogTransportInstance;
-    }
+    new(options?: SyslogTransportOptions): SyslogTransportInstance;
 }
 
 export const Syslog: SyslogTransportInstance;
