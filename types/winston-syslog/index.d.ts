86f3c3d26c91638ef8069c70228275767869617e
felix-hoc <kaminwurschze@yahoo.de>
2018-08-10 10:44:01 +0200

    Add types for winston-syslog v2.0.0, move existing types to v1/

--- types/winston-syslog/index.d.ts
+++ types/winston-syslog/index.d.ts
@@ -1,25 +1,36 @@
-// Type definitions for winston-syslog 1.0
-// Project: https://github.com/indexzero/winston-syslog#readme
-// Definitions by: Chris Barth <https://github.com/cjbarth>
+// Type definitions for winston-syslog v2.0.0
+// Project: https://github.com/winstonjs/winston-syslog
+// Definitions by: Felix Hochgruber <https://github.com/felix-hoc>
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
 
-import * as winston from "winston";
-export interface SyslogTransportOptions extends winston.GenericTextTransportOptions {
-	host?: string;
-	port?: number;
-	path?: string;
-	protocol?: string;
-	pid?: number;
-	facility?: string;
-	localhost?: string;
-	type?: string;
-	app_name?: string;
-	eol?: string;
+import * as Transport from 'winston-transport';
+import * as dgram from 'dgram';
+import * as net from 'net';
+
+declare module 'winston-syslog' {
+
+    export interface SyslogTransportOptions extends Transport.TransportStreamOptions {
+        host?: string;
+        port?: number;
+        path?: string;
+        protocol?: string;
+        pid?: number;
+        facility?: string;
+        localhost?: string;
+        type?: string;
+        app_name?: string;
+        eol?: string;
+    }
+
+    interface SyslogTransportInstance extends Transport {
+
+        producer: any;
+        socket: dgram.Socket | net.Socket;
+
+        connect(callback: (err: (true | null)) => any): void;
+
+        new(options?: SyslogTransportOptions): SyslogTransportInstance;
+    }
 }
-export class Syslog extends winston.Transport implements winston.TransportInstance {
-}
-declare module "winston" {
-	interface Transports {
-		Syslog: Syslog;
-	}
-}
+
+export const Syslog: SyslogTransportInstance;
