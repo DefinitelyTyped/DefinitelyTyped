3e2a1253d666f3528b5f825ad9851ea951404867
Matt McCutchen <matt@mattmccutchen.net>
2018-08-12 17:41:56 -0400

    meteor: Fix the global copy of Mongo.Cursor.forEach.
    (C.f. 36735d1.)

--- types/meteor/mongo.d.ts
+++ types/meteor/mongo.d.ts
@@ -205,7 +205,7 @@ declare module Mongo {
     interface Cursor<T> {
         count(applySkipLimit?: boolean): number;
         fetch(): Array<T>;
-        forEach(callback: < T > (doc: T, index: number, cursor: Cursor<T>) => void, thisArg?: any): void;
+        forEach(callback: (doc: T, index: number, cursor: Cursor<T>) => void, thisArg?: any): void;
         map<U>(callback: (doc: T, index: number, cursor: Cursor<T>) => U, thisArg?: any): Array<U>;
         observe(callbacks: ObserveCallbacks): Meteor.LiveQueryHandle;
         observeChanges(callbacks: ObserveChangesCallbacks): Meteor.LiveQueryHandle;
