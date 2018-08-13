b48b1c803bf0eeeaad90579404450b24a3afa437
Thomas den Hollander <ThomasdenH@users.noreply.github.com>
2018-08-13 17:56:46 +0200

    Add author note

--- types/ttf2woff2/index.d.ts
+++ types/ttf2woff2/index.d.ts
@@ -1,6 +1,6 @@
 // Type definitions for ttf2woff2 2.0
 // Project: https://github.com/nfroidure/ttf2woff2
-// Definitions by: My Self <https://github.com/me>
+// Definitions by: Thomas den Hollander <https://github.com/ThomasdenH>
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
 
 /// <reference types="node" />
