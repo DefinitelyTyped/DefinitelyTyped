3c0af23842d7c962a0dcbdc62df80d91cc4edc1c
Steve Oney <swloney@gmail.com>
2018-08-13 14:00:37 -0400

    rolling websocketjson stream in

--- types/sharedb/sharedb-tests.ts
+++ types/sharedb/sharedb-tests.ts
@@ -2,7 +2,31 @@
 import * as ShareDB from 'sharedb';
 import * as http from 'http';
 import * as WebSocket from 'ws';
-import { WebSocketJSONStream } from './websocketJSONStream';
+import {Duplex} from 'stream';
+
+//Adapted from https://github.com/avital/websocket-json-stream
+class WebSocketJSONStream extends Duplex {
+    constructor(private readonly ws:WebSocket) {
+        super({objectMode: true});
+        this.ws.on('message', (msg: string) => {
+            this.push(JSON.parse(msg))
+        });
+        this.ws.on('close', () => {
+            this.push(null); // end readable stream
+            this.end(); // end writable stream
+
+            this.emit('close');
+            this.emit('end');
+        });
+        this.on('error', function() { ws.close(); });
+        this.on('end',   function() { ws.close(); });
+    };
+    public _read():void {};
+    public _write(msg:any, encoding:string, next:Function):void {
+        this.ws.send(JSON.stringify(msg));
+        next();
+    };
+};
 
 const backend = new ShareDB();
 const connection = backend.connect();
