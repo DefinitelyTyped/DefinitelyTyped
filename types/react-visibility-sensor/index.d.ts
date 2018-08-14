b480e92ae719f458f34e10d0f719c6c5f7159cf4
jrasmusbm <JRasmusBm@gmail.com>
2018-08-14 10:24:47 +0200

    Add react-visibility-sensor

---
+++ types/react-visibility-sensor/index.d.ts
@@ -0,0 +1,36 @@
+// Type definitions for react-visibility-sensor 3.11.1
+// Project: https://github.com/joshwnj/react-visibility-sensor
+// Definitions by: Rasmus Bergström <https://github.com/JRasmusBm>
+// Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
+
+declare module "react-visibility-sensor" {
+  import * as React from "react";
+
+  interface Shape {
+    top?: number;
+    left?: number;
+    bottom?: number;
+    right?: number;
+  }
+
+  interface Props {
+    onChange: (isVisible: boolean, visibilityRect?: Shape) => void;
+    active?: boolean;
+    partialVisibility?: boolean;
+    offset?: Shape;
+    minTopValue?: number;
+    intervalCheck?: boolean;
+    intervalDelay?: number;
+    scrollCheck?: boolean;
+    scrollDelay?: number;
+    scrollThrottle?: number;
+    resizeCheck?: boolean;
+    resizeDelay?: number;
+    resizeThrottle?: number;
+    delayedCall?: boolean;
+  }
+
+  const ReactVisibilitySensor: React.StatelessComponent<Props>;
+
+  export = ReactVisibilitySensor;
+}
