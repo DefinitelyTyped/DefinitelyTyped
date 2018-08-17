3b1261a247a8f9568b01b3a9bb996095054c5d34
yasupeke <yasupeke2@gmail.com>
2018-08-17 16:24:00 +0900

    fix comment

--- types/react-paginate/index.d.ts
+++ types/react-paginate/index.d.ts
@@ -1,8 +1,12 @@
 // Type definitions for react-paginate 5.2
 // Project: https://github.com/AdeleD/react-paginate
-// Definitions by: Yasunori Ohoka <https://github.com/yasupeke>
+// Definitions by: Simon Hartcher <https://github.com/deevus>
+//                 Wouter Hardeman <https://github.com/wouterhardeman>
+//                 pegel03 <https://github.com/pegel03>
+//                 Simon Archer <https://github.com/archy-bold>
+//                 Yasunori Ohoka <https://github.com/yasupeke>
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
-// TypeScript Version: 2.9
+// TypeScript Version: 2.8
 
 import * as React from 'react';
 
