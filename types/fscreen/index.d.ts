3118fe7f1f253c93624be8b7f0eb9436f1ae6b74
Terry Mun <tm@ipaper.io>
2018-08-17 11:25:31 +0200

    Added author credits

--- types/fscreen/index.d.ts
+++ types/fscreen/index.d.ts
@@ -1,6 +1,7 @@
 // Type definitions for fscreen 1.0
 // Project: https://github.com/rafrex/fscreen#readme
 // Definitions by: Joscha Feth <https://github.com/joscha>
+//                 Terry Mun <https://github.com/terrymun>
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
 // TypeScript Version: 2.3
 
