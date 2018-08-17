2132a14ea96e61a3ef951d3dce3dad43e07d339c
Tyrone Dougherty <tyrone@kasada.io>
2018-08-17 11:11:59 +1000

    @types/highcharts - add AnimationOptions type to ChartObject#update method signature and update tests accordingly

--- types/highcharts/test/index.ts
+++ types/highcharts/test/index.ts
@@ -2457,6 +2457,11 @@ function test_ChartObject() {
     chart.update(<Highcharts.Options> {});
     chart.update(<Highcharts.Options> {}, true);
     chart.update(<Highcharts.Options> {}, true, true);
+    chart.update(<Highcharts.Options> {}, true, true, true);
+    chart.update(<Highcharts.Options> {}, true, true, { 
+        delay: 500,
+        duration: 3000
+    });
 }
 
 function test_ElementObject() {
