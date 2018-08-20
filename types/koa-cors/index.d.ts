8fe2e725aef615c015cc8137e954898bd36ad77b
Romain Faust <romain.faust@gmail.com>
2018-08-20 23:14:52 +0200

    Fix incorrect TypeScript version

--- types/koa-cors/index.d.ts
+++ types/koa-cors/index.d.ts
@@ -2,6 +2,7 @@
 // Project: https://github.com/evert0n/koa-cors
 // Definitions by: Romain Faust <https://github.com/romain-faust>
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
+// TypeScript Version: 2.3
 
 import { Middleware, Request } from 'koa';
 
