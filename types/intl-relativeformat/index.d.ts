b57c9a6be50f4868465028464238d21afcaa5d16
Mohsen Azimi <mohsen.azimi@airbnb.com>
2018-08-22 11:34:54 -0700

    Add header

--- types/intl-relativeformat/index.d.ts
+++ types/intl-relativeformat/index.d.ts
@@ -1,3 +1,8 @@
+// Type definitions for intl-relativeformat 2.1
+// Project: https://github.com/yahoo/intl-relativeformat
+// Definitions by: Mohsen Azimi <https://github.com/mohsen1>
+// Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
+
 /**
  * This package aims to provide a way to format different variations of relative time.
  * You can use this package in the browser and on the server via Node.js.
