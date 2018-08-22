5625e639ab00979014a0eea183c38607ebe73ac2
Romain Faust <romain.faust@gmail.com>
2018-08-22 02:08:57 +0200

    Add missing tests for imagemin.buffer

--- types/imagemin/imagemin-tests.ts
+++ types/imagemin/imagemin-tests.ts
@@ -1,5 +1,6 @@
 import * as Imagemin from 'imagemin';
 
+const buffer = Buffer.from('Hello World!');
 const input = ['fileA.jpg', 'fileB.png'];
 const output = 'output';
 const options = { plugins: [] };
@@ -8,3 +9,6 @@ Imagemin(input);
 Imagemin(input, output);
 Imagemin(input, options);
 Imagemin(input, output, options);
+
+Imagemin.buffer(buffer);
+Imagemin.buffer(buffer, options);
