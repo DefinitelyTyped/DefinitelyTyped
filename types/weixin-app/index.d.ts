df3eeb0dd0e9f86a11c178fce9ba6a0678df7e44
Jiayu Liu <jiayu.liu@airbnb.com>
2018-08-22 23:52:04 +0800

    add Jimexist

--- types/weixin-app/index.d.ts
+++ types/weixin-app/index.d.ts
@@ -2,6 +2,7 @@
 // Project: https://mp.weixin.qq.com/debug/wxadoc/dev/api/
 // Definitions by: taoqf <https://github.com/taoqf>
 //                 AlexStacker <https://github.com/AlexStacker>
+//                 Jimexist <https://github.com/Jimexist>
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
 // TypeScript Version: 2.3
 
