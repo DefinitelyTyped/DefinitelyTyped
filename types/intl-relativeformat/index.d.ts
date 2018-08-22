64c01ceaf68ec411865b5f7aaa4f730872ef2734
Mohsen Azimi <mohsen.azimi@airbnb.com>
2018-08-22 11:13:28 -0700

    Clean up

--- types/intl-relativeformat/index.d.ts
+++ types/intl-relativeformat/index.d.ts
@@ -5,6 +5,8 @@
  * This implementation is very similar to moment.js, in concept, although it provides
  * only formatting features based on the Unicode CLDR locale data, an industry
  * standard that supports more than 200 languages.
+ *
+ * @see https://github.com/yahoo/intl-relativeformat
  */
 export default class IntlRelativeFormat {
     /**
@@ -59,30 +61,30 @@ export default class IntlRelativeFormat {
     );
 
     /**
-       * This method returns an object with the options values that were resolved
-       * during instance creation. It currently only contains a locale property
-       *  here's an example:
-       ```
-          var rf = new IntlRelativeFormat('en-us');
-          console.log(rf.resolvedOptions().locale); // => "en-US"
-       ```
-       * Notice how the specified locale was the all lower-case value: "en-us",
-       * but it was resolved and normalized to: "en-US".
-       */
+     * This method returns an object with the options values that were resolved
+     * during instance creation. It currently only contains a locale property
+     *  here's an example:
+     ```
+     var rf = new IntlRelativeFormat('en-us');
+     console.log(rf.resolvedOptions().locale); // => "en-US"
+     ```
+     * Notice how the specified locale was the all lower-case value: "en-us",
+     * but it was resolved and normalized to: "en-US".
+     */
     resolvedOptions(): { locale: string };
 
     /**
-       * The format method (which takes a JavaScript date or timestamp) and optional
-       * options arguments will compare the date with "now" (or options.now),
-       * and returns the formatted string; e.g., "3 hours ago" in the corresponding
-       * locale passed into the constructor.
-       *
-       ```
-       var output = rf.format(new Date());
-       console.log(output); // => "now"
-       ```
-       * If you wish to specify a "now" value, it can be provided via options.now and
-       *  will be used instead of querying Date.now() to get the current "now" value.
-       */
+     * The format method (which takes a JavaScript date or timestamp) and optional
+     * options arguments will compare the date with "now" (or options.now),
+     * and returns the formatted string; e.g., "3 hours ago" in the corresponding
+     * locale passed into the constructor.
+     *
+     ```
+     var output = rf.format(new Date());
+     console.log(output); // => "now"
+     ```
+     * If you wish to specify a "now" value, it can be provided via options.now and
+     *  will be used instead of querying Date.now() to get the current "now" value.
+     */
     format(date: Date, options?: { now?: Date }): string;
 }
