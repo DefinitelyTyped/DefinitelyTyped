678d5777b03a244f31027e488a5dcdaedca84a43
Resi Respati <resir014@gmail.com>
2018-08-24 00:43:04 +0700

    [screeps] Release 2.4.1

--- types/screeps/index.d.ts
+++ types/screeps/index.d.ts
@@ -78,7 +78,7 @@ declare const COLORS_ALL: ColorConstant[];
 
 declare const CREEP_SPAWN_TIME: 3;
 declare const CREEP_LIFE_TIME: 1500;
-declare const CREEP_CLAIM_LIFE_TIME: 500;
+declare const CREEP_CLAIM_LIFE_TIME: 600;
 declare const CREEP_CORPSE_RATE: 0.2;
 
 declare const OBSTACLE_OBJECT_TYPES: [
@@ -633,6 +633,8 @@ declare const LOOK_TOMBSTONES: 'tombstone';
 
 declare const ORDER_SELL: "sell";
 declare const ORDER_BUY: "buy";
+
+declare const INVADERS_ENERGY_GOAL: number;
 
 declare const SYSTEM_USERNAME: string;
 
