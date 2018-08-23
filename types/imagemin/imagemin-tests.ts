e25a5cc4c09c24254ba99fec74f52517cc17dd6b
Romain Faust <romain.faust@gmail.com>
2018-08-23 19:32:01 +0200

    Add missing parenthesis

--- types/imagemin/imagemin-tests.ts
+++ types/imagemin/imagemin-tests.ts
@@ -5,5 +5,5 @@ imagemin(['*.png'], 'dist').then((results: imagemin.Result[]) => { /* ... */ });
 imagemin(['*.png'], { plugins: [] }).then((results: imagemin.Result[]) => { /* ... */ });
 imagemin(['*.png'], 'dist', { plugins: [] }).then((results: imagemin.Result[]) => { /* ... */ });
 
-imagemin.buffer(Buffer.from[/* ... */]).then((result: Buffer) => { /* ... */ });
-imagemin.buffer(Buffer.from[/* ... */], { plugins: [] }).then((result: Buffer) => { /* ... */ });
+imagemin.buffer(Buffer.from([/* ... */])).then((result: Buffer) => { /* ... */ });
+imagemin.buffer(Buffer.from([/* ... */]), { plugins: [] }).then((result: Buffer) => { /* ... */ });
