dbe5e3ecdc6e55edff55631c6bb6c2a4a6ee0f40
Alex Jerabek <aljerabe@microsoft.com>
2018-08-23 10:44:12 -0700

    Line breaks and links

--- types/office-js/index.d.ts
+++ types/office-js/index.d.ts
@@ -4542,15 +4542,19 @@ declare namespace Office {
      * @remarks
      * <table><tr><td>Requirement Sets</td><td>Settings</td></tr></table>
      * 
-     * To add an event handler for the settingsChanged event, use the addHandlerAsync method of the Settings object.
+     * To add an event handler for the settingsChanged event, use the addHandlerAsync method of the 
+     * {@link Office.Settings} object.
      * 
-     * The settingsChanged event fires only when your add-in's script calls the Settings.saveAsync method to persist the in-memory copy of the settings into the document file. 
-     * The settingsChanged event is not triggered when the Settings.set or Settings.remove methods are called.
+     * The settingsChanged event fires only when your add-in's script calls the Settings.saveAsync method to persist 
+     * the in-memory copy of the settings into the document file. The settingsChanged event is not triggered when the 
+     * Settings.set or Settings.remove methods are called.
      * 
-     * The settingsChanged event was designed to let you to handle potential conflicts when two or more users are attempting to save settings at the same time when your add-in is used in a shared (co-authored) document.
+     * The settingsChanged event was designed to let you to handle potential conflicts when two or more users are 
+     * attempting to save settings at the same time when your add-in is used in a shared (co-authored) document.
      * 
-     * **Important:** Your add-in's code can register a handler for the settingsChanged event when the add-in is running with any Excel client, 
-     * but the event will fire only when the add-in is loaded with a spreadsheet that is opened in Excel Online, and more than one user is editing the spreadsheet (co-authoring). 
+     * **Important:** Your add-in's code can register a handler for the settingsChanged event when the add-in 
+     * is running with any Excel client, but the event will fire only when the add-in is loaded with a spreadsheet 
+     * that is opened in Excel Online, and more than one user is editing the spreadsheet (co-authoring). 
      * Therefore, effectively the settingsChanged event is supported only in Excel Online in co-authoring scenarios.
      * 
      * **Support details**
