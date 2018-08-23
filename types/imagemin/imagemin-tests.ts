de1d976af971743b5bdded0303371f7729ecbe25
Romain Faust <11320210+romain-faust@users.noreply.github.com>
2018-08-23 19:25:56 +0200

    Simplify tests

--- types/imagemin/imagemin-tests.ts
+++ types/imagemin/imagemin-tests.ts
@@ -1,21 +1,9 @@
 import imagemin = require('imagemin');
 
-const input = ['/input/**/*'];
-const output = '/output/dir';
-const options = { plugins: [] };
-const handler = (result: imagemin.Result[]) => {
-    // ...
-};
+imagemin(['*.png']).then((results: imagemin.Result[]) => { /* ... */ });
+imagemin(['*.png'], 'dist').then((results: imagemin.Result[]) => { /* ... */ });
+imagemin(['*.png'], { plugins: [] }).then((results: imagemin.Result[]) => { /* ... */ });
+imagemin(['*.png'], 'dist', { plugins: [] }).then((results: imagemin.Result[]) => { /* ... */ });
 
-imagemin(input).then(handler);
-imagemin(input, output).then(handler);
-imagemin(input, options).then(handler);
-imagemin(input, output, options).then(handler);
-
-const buffer = Buffer.from([ /* File content */ ]);
-const bufferHandler = (result: Buffer) => {
-    // ...
-};
-
-imagemin.buffer(buffer).then(bufferHandler);
-imagemin.buffer(buffer, options).then(bufferHandler);
+imagemin.buffer(Buffer.from[/* ... */]).then((result: Buffer) => { /* ... */ });
+imagemin.buffer(Buffer.from[/* ... */], { plugins: [] }).then((result: Buffer) => { /* ... */ });
