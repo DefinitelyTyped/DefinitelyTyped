86f0182f170a777ccfe57e75b82aa352c192e4da
Daniel Byrne <danwbyrne@gmail.com>
2018-08-25 13:30:06 -0700

    adds typings for the 'toposort' package

--- types/toposort/toposort-tests.ts
+++ types/toposort/toposort-tests.ts
@@ -1,18 +1,9 @@
-import toposort from 'toposort';
+import toposort = require('toposort');
 
 const testGraph: ReadonlyArray<[string, string]> = [
-  [
-    'string1',
-    'string2',
-  ],
-  [
-    'string2',
-    'string3',
-  ],
-  [
-    'string3',
-    'string1',
-  ],
+    ["string1", "string2"],
+    ["string2", "string3"],
+    ["string3", "string1"]
 ];
 
 // $ExpectType ReadonlyArray<string>
