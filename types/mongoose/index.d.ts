459a5f8cf35819ee0bdec7bb6ed64863eb4f6758
mernxl <mernxl@gmail.com>
2018-08-25 20:21:04 +0100

    UpdateType(mongoose): add interfaces for promisified schema validators
    Add `AsyncPromiseValidationFn`, `AsyncPromiseValidationOpts` as described in this document https://mongoosejs.com/docs/validation.html

--- types/mongoose/index.d.ts
+++ types/mongoose/index.d.ts
@@ -977,7 +977,9 @@ declare module "mongoose" {
     validate?: RegExp | [RegExp, string] |
       SchemaTypeOpts.ValidateFn<T> | [SchemaTypeOpts.ValidateFn<T>, string] |
       SchemaTypeOpts.ValidateOpts | SchemaTypeOpts.ValidateOpts[] |
-      SchemaTypeOpts.AsyncValidateOpts | SchemaTypeOpts.AsyncValidateOpts[];
+      SchemaTypeOpts.AsyncValidateOpts | SchemaTypeOpts.AsyncValidateOpts[] |
+      SchemaTypeOpts.AsyncPromiseValidationFn<T> | SchemaTypeOpts.AsyncPromiseValidationFn<T>[] |
+      SchemaTypeOpts.AsyncPromiseValidationOpts | SchemaTypeOpts.AsyncPromiseValidationOpts[];
 
     /** Declares an unique index. */
     unique?: boolean | any;
@@ -1058,6 +1060,14 @@ declare module "mongoose" {
     interface AsyncValidateOpts extends ValidateOptsBase {
       isAsync: true;
       validator: AsyncValidateFn<any>;
+    }
+
+    interface AsyncPromiseValidationFn<T> {
+      (value: T): Promise<boolean>;
+    }
+
+    interface AsyncPromiseValidationOpts extends ValidateOptsBase {
+      validator: AsyncPromiseValidationFn<any>;
     }
 
     interface EnumOpts<T> {
