66002d01c36174214b1e28ac617140daa291d6f7
Luca Lindhorst <info@lucalindhorst.de>
2018-08-27 00:24:38 +0200

    Added string-format defintions

---
+++ types/string-format/string-format-tests.ts
@@ -0,0 +1,11 @@
+import * as Format from "string-format";
+
+Format.extend(String.prototype, {});
+
+const fmt = Format.create({
+	upper: (s: string) => s.toUpperCase()
+});
+
+fmt("Hallo {name!upper}, you are {age} years old!", { name: "Somebody", age: 10 });
+
+Format("Hallo {name}, you are {age} years old!", { name: "Somebody", age: 10 });
