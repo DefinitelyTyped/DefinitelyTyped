61897c31613106e5c2462747b04a8910b46d7890
Mike North <michael.l.north@gmail.com>
2018-08-27 00:09:43 -0700

    [ember/v2] disable that fails due to TS 3.1 breaking change

--- types/ember/v2/test/array-ext.ts
+++ types/ember/v2/test/array-ext.ts
@@ -13,7 +13,12 @@ const person = Person.create({ name: 'Joe' });
 const array = [person];
 
 assertType<number>(array.get('length'));
-assertType<Person | undefined>(array.get('firstObject'));
+// This test must be disabled due to a breaking change in TS 3.1
+// see: https://github.com/Microsoft/TypeScript/issues/26120
+//      https://github.com/typed-ember/ember-cli-typescript/issues/246
+//      https://github.com/Microsoft/TypeScript/pull/26063
+//
+// assertType<Person | undefined>(array.get('firstObject'));
 assertType<string[]>(array.mapBy('name'));
 assertType<string[]>(array.map(p => p.get('name')));
 assertType<Person[]>(array.sortBy('name'));
