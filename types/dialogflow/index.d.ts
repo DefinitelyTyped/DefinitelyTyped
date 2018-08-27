b2631c0d964c96d1fd4320d78feb1ca60779eac3
Michael Beemer <michael.beemer@dynatrace.com>
2018-08-27 15:08:13 -0400

    Dropped the Dialogflow version back to 6

--- types/dialogflow/index.d.ts
+++ types/dialogflow/index.d.ts
@@ -1,4 +1,4 @@
-// Type definitions for dialogflow 0.7
+// Type definitions for dialogflow 0.6
 // Project: https://github.com/dialogflow/dialogflow-nodejs-client-v2#readme
 // Definitions by: Daniel Dyla <https://github.com/dyladan>
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
