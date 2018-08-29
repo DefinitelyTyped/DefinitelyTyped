72a7247089c167355dc5d155404a8524c38a5501
jrasmusbm <JRasmusBm@gmail.com>
2018-08-29 10:56:18 +0200

    Fix TypeScript Version

--- types/react-visibility-sensor/index.d.ts
+++ types/react-visibility-sensor/index.d.ts
@@ -2,7 +2,7 @@
 // Project: https://github.com/joshwnj/react-visibility-sensor
 // Definitions by: Rasmus Bergström <https://github.com/JRasmusBm>
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
-// TypeScript Version: 2.7
+// TypeScript Version: 2.8
 
 declare module "react-visibility-sensor" {
   /// <reference types="cheerio" />
