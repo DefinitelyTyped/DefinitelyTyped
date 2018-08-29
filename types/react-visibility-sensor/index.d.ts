b553b09b386275713ae0e2819b06ccd6ebe8b344
jrasmusbm <JRasmusBm@gmail.com>
2018-08-29 11:46:48 +0200

    Add tests and make them pass

--- types/react-visibility-sensor/index.d.ts
+++ types/react-visibility-sensor/index.d.ts
@@ -1,38 +1,35 @@
-// Type definitions for react-visibility-sensor 3.11.1
+// Type definitions for react-visibility-sensor 3.11
 // Project: https://github.com/joshwnj/react-visibility-sensor
 // Definitions by: Rasmus Bergström <https://github.com/JRasmusBm>
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
 // TypeScript Version: 2.8
 
-declare module "react-visibility-sensor" {
-  /// <reference types="cheerio" />
-  import * as React from "react";
+import * as React from "react";
 
-  interface Shape {
-    top?: number;
-    left?: number;
-    bottom?: number;
-    right?: number;
-  }
-
-  interface Props {
-    onChange: (isVisible: boolean, visibilityRect?: Shape) => void;
-    active?: boolean;
-    partialVisibility?: boolean;
-    offset?: Shape;
-    minTopValue?: number;
-    intervalCheck?: boolean;
-    intervalDelay?: number;
-    scrollCheck?: boolean;
-    scrollDelay?: number;
-    scrollThrottle?: number;
-    resizeCheck?: boolean;
-    resizeDelay?: number;
-    resizeThrottle?: number;
-    delayedCall?: boolean;
-  }
-
-  const ReactVisibilitySensor: React.StatelessComponent<Props>;
-
-  export = ReactVisibilitySensor;
+interface Shape {
+  top?: number;
+  left?: number;
+  bottom?: number;
+  right?: number;
 }
+
+interface Props {
+  onChange: (isVisible: boolean, visibilityRect?: Shape) => void;
+  active?: boolean;
+  partialVisibility?: boolean;
+  offset?: Shape;
+  minTopValue?: number;
+  intervalCheck?: boolean;
+  intervalDelay?: number;
+  scrollCheck?: boolean;
+  scrollDelay?: number;
+  scrollThrottle?: number;
+  resizeCheck?: boolean;
+  resizeDelay?: number;
+  resizeThrottle?: number;
+  delayedCall?: boolean;
+}
+
+declare const ReactVisibilitySensor: React.StatelessComponent<Props>;
+
+export default ReactVisibilitySensor;
--- types/react-visibility-sensor/react-visibility-sensor-tests.tsx
+++ types/react-visibility-sensor/react-visibility-sensor-tests.tsx
@@ -1,1 +1,25 @@
-export default null
+import * as React from "react";
+
+import ReactVisibilitySensor from "react-visibility-sensor";
+const num = 1;
+const bool = true;
+const shape = { top: num, left: num, bottom: num, right: num };
+
+const component = (
+  <ReactVisibilitySensor
+    onChange={(bool, shape) => {}}
+    active={bool}
+    partialVisibility={bool}
+    offset={shape}
+    minTopValue={num}
+    intervalCheck={bool}
+    intervalDelay={num}
+    scrollCheck={bool}
+    scrollDelay={num}
+    scrollThrottle={num}
+    resizeCheck={bool}
+    resizeDelay={num}
+    resizeThrottle={num}
+    delayedCall={bool}
+  />
+);
