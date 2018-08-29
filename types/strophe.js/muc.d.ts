f1b0fae9912cf1cf06004de6293468542985408e
Tijs Zwinkels - TinkerTank <tijs@tinkertank.eu>
2018-08-29 17:21:59 +0200

    [strophe.js] fix the headers.

--- types/strophe.js/muc.d.ts
+++ types/strophe.js/muc.d.ts
@@ -1,8 +1,3 @@
-// Type definitions for strophe.muc.js v1.2.0
-// Project: https://github.com/metajack/strophejs-plugins
-// Definitions by: Ilia Choly <https://github.com/icholy/>
-// Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
-
 import { Strophe } from "strophe.js";
 
 declare module "strophe.js" {
