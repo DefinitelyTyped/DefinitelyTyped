88c946138c6c7a0d74bc442be052878c06272b84
Daniel Byrne <danwbyrne@gmail.com>
2018-08-28 18:42:25 -0700

    adds typings for the 'safe-stable-stringify' package

---
+++ types/safe-stable-stringify/index.d.ts
@@ -0,0 +1,10 @@
+// Type definitions for safe-stable-stringify 1.0
+// Project: https://github.com/BridgeAR/safe-stable-stringify
+// Definitions by: Daniel Byrne <https://github.com/danwbyrne>
+// Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
+
+/// <reference types = "node" />
+
+declare const stringify: typeof JSON.stringify;
+
+export = stringify;
