713ad0f55f5db2689f75423c19dc2b05e630a490
Daniel Byrne <danwbyrne@gmail.com>
2018-08-29 12:33:09 -0700

    adds typings for the 'bitwise' package

---
+++ types/bitwise/bits/to-string.d.ts
@@ -0,0 +1,5 @@
+export default function toString(
+  bit: ReadonlyArray<number>,
+  spacing?: number,
+  spacer?: string
+): string;
---
+++ types/bitwise/buffer/modify.d.ts
@@ -0,0 +1,7 @@
+/// <reference types= "node" />
+
+export default function modify(
+  buffer: Buffer,
+  newBits: ReadonlyArray<number>,
+  bitOffset?: number
+): void;
