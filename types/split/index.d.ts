8d74e8882bb6c5abfce1d26bdfed06e3ca7d3269
Wouter van Heeswijk <wouter.vanheeswijk@tomtom.com>
2018-08-29 15:10:14 +0200

    Increase version number for split package

--- types/split/index.d.ts
+++ types/split/index.d.ts
@@ -1,4 +1,4 @@
-// Type definitions for split v0.3.3
+// Type definitions for split v1.0.1
 // Project: https://github.com/dominictarr/split
 // Definitions by: Marcin Porębski <https://github.com/marcinporebski>
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
