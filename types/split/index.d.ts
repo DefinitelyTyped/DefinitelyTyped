0dc195e3a79ff8509aa8af1a4d3fa8fe8d307740
Wouter van Heeswijk <wouter.vanheeswijk@tomtom.com>
2018-08-29 14:58:22 +0200

    Specify return type as ThroughStream

--- types/split/index.d.ts
+++ types/split/index.d.ts
@@ -4,13 +4,15 @@
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
 
 /// <reference types="node" />
+/// <reference types="through" />
 
-
+import { Transform, TransformOptions } from 'stream';
+import { ThroughStream } from 'through';
 
 interface SplitOptions {
     maxLength: number
 }
 
-declare function split(matcher?: any, mapper?: any, options?: SplitOptions): any;
+declare function split(matcher?: any, mapper?: any, options?: SplitOptions): ThroughStream;
 
 export = split;
