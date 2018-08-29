238df04ce8e6d254fa16d59129f5663d4e9c4a70
Eduardo Henke <eduardo_henke@hotmail.com>
2018-08-29 17:08:35 -0300

    Fix lint errors

--- types/tiny-secp256k1/index.d.ts
+++ types/tiny-secp256k1/index.d.ts
@@ -1,6 +1,6 @@
-// Type definitions for tiny-secp256k1 v1.0.0
+// Type definitions for tiny-secp256k1 1.0
 // Project: https://github.com/bitcoinjs/tiny-secp256k1
-// Definitions by: Eduardo Henke <https://github.com/eduhenke/>
+// Definitions by: Eduardo Henke <https://github.com/eduhenke>
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
 /// <reference types="node" />
 
