17c721836fd76b9c3e6154510104d6ed5dbae543
Mark Wolff <marwolff@microsoft.com>
2018-08-30 14:18:37 -0700

    [applicationinsights-js] remove fetch for v1.0.20

--- types/applicationinsights-js/index.d.ts
+++ types/applicationinsights-js/index.d.ts
@@ -1,7 +1,6 @@
 // Type definitions for ApplicationInsights-JS 1.0
 // Project: https://github.com/Microsoft/ApplicationInsights-JS
-// Definitions by: Kamil Szostak <https://github.com/kamilszostak>
-//                 Mark Wolff <https://github.com/markwolff>
+// Definitions by: Mark Wolff <https://github.com/markwolff>
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
 
 declare module AI {
@@ -561,7 +560,6 @@ declare module Microsoft.ApplicationInsights {
         samplingPercentage?: number;
         autoTrackPageVisitTime?: boolean;
         disableAjaxTracking?: boolean;
-        disableFetchTracking?: boolean;
         overridePageViewDuration?: boolean;
         maxAjaxCallsPerView?: number;
         disableDataLossAnalysis?: boolean;
