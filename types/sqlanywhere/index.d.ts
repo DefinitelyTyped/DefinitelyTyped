11f7b15db6340a956ab98a1e8d60bd053440adf1
Peter Keuter <github@peterkeuter.nl>
2018-08-30 17:45:02 +0200

    Fix linting errors

--- types/sqlanywhere/index.d.ts
+++ types/sqlanywhere/index.d.ts
@@ -2,58 +2,47 @@
 // Project: https://github.com/sqlanywhere/node-sqlanywhere
 // Definitions by: Peter Keuter <https://github.com/pkeuter>
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
-declare module "sqlanywhere" {
-    export function createConnection(): SybaseConnection;
+export function createConnection(): SybaseConnection;
 
-    export interface SybaseConnection {
-      connect(
+export interface SybaseConnection {
+    connect(
         params: ConnectionParameters,
-        cb: (err: Error | undefined) => void,
-      ): void;
-      close(cb: (err?: Error) => void): void;
-      disconnect(cb: (err?: Error) => void): void;
-      connected(): boolean;
-      exec(query: string, cb: (err: Error | undefined, result: any) => void);
-      exec(
+        cb: (err: Error | undefined) => void
+    ): void;
+    close(cb: (err?: Error) => void): void;
+    disconnect(cb: (err?: Error) => void): void;
+    connected(): boolean;
+    exec(query: string, cb: (err: Error | undefined, result: any) => void);
+    exec(
         query: string,
         placeholders: any[],
-        cb: (err: Error | undefined, result: any) => void,
-      );
-      exec<T>(query: string, cb: (err: Error | undefined, result: T[]) => void);
-      exec<T>(
+        cb: (err: Error | undefined, result: any) => void
+    );
+    prepare(
         query: string,
-        placeholders: any[],
-        cb: (err: Error | undefined, result: T[]) => void,
-      );
-      prepare(
-        query: string,
-        cb: (err: Error | undefined, stmt: Statement) => void,
-      );
-      prepare(query: string): Statement;
-      commit(cb: (err: Error | undefined) => void): void;
-      rollback(cb: (err: Error | undefined) => void): void;
-    }
+        cb: (err: Error | undefined, stmt: Statement) => void
+    );
+    prepare(query: string): Statement;
+    commit(cb: (err: Error | undefined) => void): void;
+    rollback(cb: (err: Error | undefined) => void): void;
+}
 
-    export interface ConnectionParameters {
-      Server: string;
-      UserId: string;
-      DatabaseFile?: string;
-      AutoStart?: string;
-      Password: string;
-      Host?: string;
-    }
+export interface ConnectionParameters {
+    Server: string;
+    UserId: string;
+    DatabaseFile?: string;
+    AutoStart?: string;
+    Password: string;
+    Host?: string;
+}
 
-    export interface Statement {
-      exec(args: any[], cb: (err: Error | undefined, rows: any[]) => void);
-      exec<T>(args: any[], cb: (err: Error | undefined, rows: T[]) => void);
-      exec(args: any[]): any[];
-      exec<T>(args: any[]): T[];
-      getMoreResults(): any[];
-      getMoreResults<T>(): T[];
-      drop(cb: (err: Error | undefined) => void): void;
-    }
-    const _default: {
-        createConnection: typeof createConnection;
-    };
-    export default _default;
-  }
+export interface Statement {
+    exec(args: any[], cb: (err: Error | undefined, rows: any[]) => void);
+    exec(args: any[]): any[];
+    getMoreResults(): any[];
+    drop(cb: (err: Error | undefined) => void): void;
+}
+declare const _default: {
+    createConnection: typeof createConnection;
+};
+export default _default;
