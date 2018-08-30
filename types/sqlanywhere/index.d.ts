2ea2373dc3192b5346448dcefdd983f1ba8ff1f8
Peter Keuter <github@peterkeuter.nl>
2018-08-30 19:22:51 +0200

    Fix common mistakes

--- types/sqlanywhere/index.d.ts
+++ types/sqlanywhere/index.d.ts
@@ -1,4 +1,4 @@
-// Type definitions for sqlanywhere
+// Type definitions for sqlanywhere 1.0
 // Project: https://github.com/sqlanywhere/node-sqlanywhere
 // Definitions by: Peter Keuter <https://github.com/pkeuter>
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
