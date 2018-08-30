524e92f75697a01dce26984c32e85ad6f2586208
Mark Wolff <marwolff@microsoft.com>
2018-08-30 16:12:18 -0700

    Add applicationinsights-js maintainers

--- types/applicationinsights-js/index.d.ts
+++ types/applicationinsights-js/index.d.ts
@@ -1,6 +1,8 @@
 // Type definitions for ApplicationInsights-JS 1.0
 // Project: https://github.com/Microsoft/ApplicationInsights-JS
 // Definitions by: Mark Wolff <https://github.com/markwolff>
+//                 Piyali Jana <https://github.com/jpiyali>
+//                 Basal Rustum <https://github.com/barustum>
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
 
 declare module AI {
