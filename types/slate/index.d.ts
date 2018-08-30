c5a172a5962eea14f35c9a2a0e9408fe6c2db2b6
Irwan Fario Subastian <irwan@cadmus.io>
2018-08-30 16:30:31 +1000

    add missing changes and fix value.selection

--- types/slate/index.d.ts
+++ types/slate/index.d.ts
@@ -81,7 +81,7 @@ export class Schema extends Immutable.Record({}) {
 
 export interface ValueProperties {
     document?: Document;
-    selection?: Range;
+    selection?: Selection;
     history?: History;
     schema?: Schema;
     data?: Data;
@@ -921,6 +921,7 @@ export class Change extends Immutable.Record({}) {
     unwrapNodeByKey(key: string): Change;
     wrapInlineByKey(key: string, properties: InlineProperties | string): Change;
     wrapBlockByKey(key: string, properties: BlockProperties | string): Change;
+    wrapNodeByKey(key: string): Change;
 
     // by path
     addMarkByPath(
@@ -950,6 +951,7 @@ export class Change extends Immutable.Record({}) {
         options: ChangeOption
     ): Change;
     mergeNodeByPath(path: Path, options: ChangeOption): Change;
+    mergeNodeByKey(key: string, options: ChangeOption): Change;
     moveNodeByPath(
         path: Path,
         newPath: Path,
@@ -964,6 +966,7 @@ export class Change extends Immutable.Record({}) {
         options: ChangeOption
     ): Change;
     removeAllMarksByPath(path: Path, options: ChangeOption): Change;
+    removeAllMarksByKey(key: string, options: ChangeOption): Change;
     removeNodeByPath(path: Path, options: ChangeOption): Change;
     removeTextByPath(
         path: Path,
@@ -974,6 +977,14 @@ export class Change extends Immutable.Record({}) {
     replaceNodeByPath(path: Path, newNode: Node, options: ChangeOption): Change;
     replaceTextByPath(
         path: Path,
+        offset: number,
+        length: number,
+        text: string,
+        marks: Immutable.Set<Mark> | null | undefined,
+        options: ChangeOption
+    ): Change;
+    replaceTextByKey(
+        key: string,
         offset: number,
         length: number,
         text: string,
@@ -995,6 +1006,12 @@ export class Change extends Immutable.Record({}) {
     ): Change;
     setTextByPath(
         path: Path,
+        text: string,
+        marks: Immutable.Set<Mark> | null | undefined,
+        options: ChangeOption
+    ): Change;
+    setTextByKey(
+        key: string,
         text: string,
         marks: Immutable.Set<Mark> | null | undefined,
         options: ChangeOption
