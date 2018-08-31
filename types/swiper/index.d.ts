674d402a7f889aec5be85b420bd617609e1cc05d
Luiz Filipe Machado Barni <machado@odahcam.com>
2018-08-31 09:07:47 -0300

    fix lint header error and no self import

--- types/swiper/index.d.ts
+++ types/swiper/index.d.ts
@@ -1,6 +1,6 @@
 // Type definitions for Swiper 4.2
 // Project: https://github.com/nolimits4web/Swiper
-// Definitions by: Sebastián Galiano <https://github.com/sgaliano>, Luca Trazzi <https://github.com/lucax88x>, Eugene Matseruk <https://github.com/ematseruk>, Luiz <https://github.com/odahcam/>
+// Definitions by: Sebastián Galiano <https://github.com/sgaliano>, Luca Trazzi <https://github.com/lucax88x>, Eugene Matseruk <https://github.com/ematseruk>, Luiz M. <https://github.com/odahcam>
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
 // TypeScript Version: 2.9
 
