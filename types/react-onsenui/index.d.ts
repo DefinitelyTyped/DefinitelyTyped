0d1b6d3866a2a75fa4567c6e58561fd827c1e471
Meykel Gruel <meykel.gruel@logicline.de>
2018-08-31 10:46:44 +0200

    Updated header information

--- types/react-onsenui/index.d.ts
+++ types/react-onsenui/index.d.ts
@@ -1,6 +1,8 @@
-// Type definitions for React Onsen UI (react-onsenui) 2.9
+// Type definitions for React Onsen UI (react-onsenui) 2.9.2
 // Project: https://onsen.io/v2/docs/guide/react/
-// Definitions by: Ozytis <https://ozytis.fr>, Salim <https://github.com/salim7>, Jemmyw <https://github.com/jemmyw>
+// Definitions by: Ozytis <https://ozytis.fr>,
+//                 Salim <https://github.com/salim7>,
+//                 Jemmyw <https://github.com/jemmyw>
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
 // TypeScript Version: 2.8
 
