2bda1600b643c5ba50f4b4a6631d68f339592719
Ihor Chulinda <ichulinda@gmail.com>
2018-09-02 18:30:03 +0200

    chore(styled-components): add myself to list of .d.ts maintainers

--- types/styled-components/index.d.ts
+++ types/styled-components/index.d.ts
@@ -1,6 +1,7 @@
 // Type definitions for styled-components 3.0
 // Project: https://github.com/styled-components/styled-components
 // Definitions by: Igor Oleinikov <https://github.com/Igorbek>
+//                 Ihor Chulinda <https://github.com/Igmat>
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
 // TypeScript Version: 2.8
 
