06ace8c1c5a422f1ba1e986d7ac981fe8b0180ca
djereg <djereg@gmail.com>
2018-09-03 16:17:04 +0200

    my name is not My Self

--- types/react-native-mauron85-background-geolocation/index.d.ts
+++ types/react-native-mauron85-background-geolocation/index.d.ts
@@ -1,6 +1,6 @@
 // Type definitions for react-native-mauron85-background-geolocation 0.5
 // Project: https://github.com/mauron85/react-native-background-geolocation#readme
-// Definitions by: My Self <https://github.com/djereg>
+// Definitions by: Györög Norbert <https://github.com/djereg>
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
 // TypeScript Version: 2.8
 
