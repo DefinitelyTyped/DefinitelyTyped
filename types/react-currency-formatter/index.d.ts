9a9bc1c840c83644f77397bced390dd6684d789b
Yury Pastushenko <y.pastushenko@dodopizza.xom>
2018-09-04 16:43:48 +0300

    Type definitions for react-currency-formatter added.

---
+++ types/react-currency-formatter/index.d.ts
@@ -0,0 +1,21 @@
+// Type definitions for react-currency-formatter 1.1
+// Project: https://github.com/xDae/react-currency-formatter
+// Definitions by: Yury Pastushenko <https://github.com/pastushenkoy>
+// Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
+// TypeScript Version: 2.8
+
+declare module 'react-currency-formatter' {
+	import * as React from 'react'
+
+	interface CurrencyFormatterProps {
+		quantity: number
+		currency?: string
+		locale?: string
+		pattern?: string
+		decimal?: string
+		group?: string
+	}
+
+	const reactCurrencyFormatter: React.ComponentClass<CurrencyFormatterProps>
+	export = reactCurrencyFormatter
+}
