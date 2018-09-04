d8f85b5605793326e32554497ffaae086650aaad
Yury Pastushenko <y.pastushenko@dodopizza.xom>
2018-09-04 18:06:29 +0300

    Added author.

--- types/react-currency-formatter/index.d.ts
+++ types/react-currency-formatter/index.d.ts
@@ -1,6 +1,7 @@
 // Type definitions for react-currency-formatter 1.1
 // Project: https://github.com/xDae/react-currency-formatter;
 // Definitions by: Yury Pastushenko <https://github.com/pastushenkoy>
+//                 Evgeniy Vasiliev <https://github.com/Jeka-Vasiliev>
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
 // TypeScript Version: 2.8
 
