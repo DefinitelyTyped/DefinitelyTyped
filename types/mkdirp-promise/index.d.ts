dafc8e08281944b15cb705f83e842e9fcfc52c26
Niels Dequeker <niels.dequeker@fastmail.com>
2018-09-04 20:57:28 +0200

    Update index.d.ts

--- types/mkdirp-promise/index.d.ts
+++ types/mkdirp-promise/index.d.ts
@@ -1,7 +1,7 @@
 // Type definitions for mkdirp-promise 5.0
 // Project: https://github.com/ahmadnassri/mkdirp-promise
 // Definitions by: Alan Plum <https://github.com/pluma>
-                   Niels Dequeker <https://github.com/voles>
+//                 Niels Dequeker <https://github.com/voles>
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
 // TypeScript Version: 2.1
 
