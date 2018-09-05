2808790776eee68a619a3b9d9b5d20d1d63a5be7
m93a <m93a.cz@gmail.com>
2018-09-05 12:59:47 +0200

    repaired according to linter

--- types/ml-levenberg-marquardt/index.d.ts
+++ types/ml-levenberg-marquardt/index.d.ts
@@ -1,37 +1,36 @@
-// Type definitions for ml-levenberg-marquardt 1.0.3
+// Type definitions for ml-levenberg-marquardt 1.0
 // Project: https://github.com/mljs/levenberg-marquardt#readme
 // Definitions by: m93a <https://github.com/m93a>
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
-
 
 declare namespace LM {
     /**
      * Function that receives an array of parameters and returns
      * a function with the independent variable as a parameter.
      */
-    export type FittedFunction = (parameters: number[]) => (x: number) => number;
+    type FittedFunction = (parameters: number[]) => (x: number) => number;
 
     /**
      * Coordinates of points to fit.
      */
-    export interface Data {
+    interface Data {
         x: number[];
         y: number[];
     }
 
-    export type Options = {
-        damping: number,
-        initialValues: number[],
-        gradientDifference: number,
-        maxIterations: number,
-        errorTolerance: number
-    };
+    interface Options {
+        damping: number;
+        initialValues: number[];
+        gradientDifference: number;
+        maxIterations: number;
+        errorTolerance: number;
+    }
 
-    export type Result =  {
-        iterations: number,
-        parameterError: number,
-        parameterValues: number[]
-    };
+    interface Result {
+        iterations: number;
+        parameterError: number;
+        parameterValues: number[];
+    }
 }
 
 declare function LM(d: LM.Data, fn: LM.FittedFunction, o: LM.Options): LM.Result;
