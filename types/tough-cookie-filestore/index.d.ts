1a764dbc6094820c9f5a24b9aae64b980ee55068
Christian Friedow <christian@friedow.com>
2018-09-05 18:00:17 +0200

    Removed namespace export

--- types/tough-cookie-filestore/index.d.ts
+++ types/tough-cookie-filestore/index.d.ts
@@ -4,7 +4,6 @@
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
 
 export = FileCookieStore;
-export as namespace FileCookieStore;
 
 declare namespace FileCookieStore {
     interface Cookie {
