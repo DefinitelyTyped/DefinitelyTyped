7b2a3988c4a1071ac4d4daa592031ff3b09a48a9
Asgeir Sognefest <sogasg@gmail.com>
2018-09-06 14:56:47 +0200

    Fix typo

--- types/web3/web3-tests.ts
+++ types/web3/web3-tests.ts
@@ -21,4 +21,4 @@ myContract.options.gas = 5000000;
 const weiStr: string = web3.utils.toWei("100", "gwei");
 const weiBn: BigNumber = web3.utils.toWei(web3.utils.toBN("1"));
 const rndHex: string = Web3.utils.randomHex(10);
-const sshPromise: Promise<string> = web3.shh.generateSymKeyFromPassword("xyz");
+const shhPromise: Promise<string> = web3.shh.generateSymKeyFromPassword("xyz");
