83fbe79d343f9860acfeea4cc09028279d57ecd1
Stephan Classen <st.classen@gmx.ch>
2018-09-06 23:52:07 +0200

    format header comment

--- types/angular-block-ui/index.d.ts
+++ types/angular-block-ui/index.d.ts
@@ -1,6 +1,8 @@
 // Type definitions for angular-block-ui 0.2
 // Project: https://github.com/McNull/angular-block-ui
-// Definitions by: Lasse Nørregaard <https://github.com/lassebn>, Stephan Classen <https://github.com/sclassen>, Soner Köksal <https://github.com/renjfk>
+// Definitions by: Lasse Nørregaard <https://github.com/lassebn>
+//                 Stephan Classen <https://github.com/sclassen>
+//                 Soner Köksal <https://github.com/renjfk>
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
 // TypeScript Version: 2.3
 
