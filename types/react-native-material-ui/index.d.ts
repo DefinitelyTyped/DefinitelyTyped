a7f8850a0156677d5a5aace88451bd75e68eb845
Simon <sstur@me.com>
2018-09-06 16:59:07 -0500

    Version bump

--- types/react-native-material-ui/index.d.ts
+++ types/react-native-material-ui/index.d.ts
@@ -1,4 +1,4 @@
-// Type definitions for react-native-material-ui 1.30
+// Type definitions for react-native-material-ui 1.31
 // Project: https://github.com/xotahal/react-native-material-ui
 // Definitions by: Kyle Roach <https://github.com/iRoachie>
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
