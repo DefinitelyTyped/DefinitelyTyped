38d4ac14add04a557448973434084132c9bd02a4
Mike North <michael.l.north@gmail.com>
2018-09-07 11:48:08 -0700

    [ember] fix: runInDebug should return void fixes https://github.com/typed-ember/ember-cli-typescript/issues/284

--- types/ember/index.d.ts
+++ types/ember/index.d.ts
@@ -3064,7 +3064,7 @@ declare module 'ember' {
         /**
          * Run a function meant for debugging.
          */
-        function runInDebug(func: () => void): any;
+        function runInDebug(func: () => any): void;
         /**
          * Display a warning with the provided message.
          */
