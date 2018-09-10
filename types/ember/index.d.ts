9e32fc7a7973b6d19145af7979521f238fb6a9da
Mike North <michael.l.north@gmail.com>
2018-09-10 14:58:51 -0700

    [@types/ember] - Fix several "empty check" function zero-argument cases (#28686)
    * fix: allow Ember.typeOf zero-argument case
    
    Fixes: https://github.com/typed-ember/ember-cli-typescript/issues/260
    
    * fix: remove outdated Ember.String.fmt
    
    Fixes: https://github.com/typed-ember/ember-cli-typescript/issues/280
    
    * fix: allow zero-argument usage of Ember.isBlank, .isEmpty, .isPresent and .isNone
    
    Fixes https://github.com/typed-ember/ember-cli-typescript/issues/258
    Fixes https://github.com/typed-ember/ember-cli-typescript/issues/255
    Fixes https://github.com/typed-ember/ember-cli-typescript/issues/256
    Fixes https://github.com/typed-ember/ember-cli-typescript/issues/254

--- types/ember/index.d.ts
+++ types/ember/index.d.ts
@@ -3122,22 +3122,22 @@ declare module 'ember' {
         /**
          * A value is blank if it is empty or a whitespace string.
          */
-        function isBlank(obj: any): boolean;
+        function isBlank(obj?: any): boolean;
         /**
          * Verifies that a value is `null` or an empty string, empty array,
          * or empty function.
          */
-        function isEmpty(obj: any): boolean;
+        function isEmpty(obj?: any): boolean;
         /**
          * Returns true if the passed value is null or undefined. This avoids errors
          * from JSLint complaining about use of ==, which can be technically
          * confusing.
          */
-        function isNone(obj: any): obj is null | undefined;
+        function isNone(obj?: any): obj is null | undefined;
         /**
          * A value is present if it not `isBlank`.
          */
-        function isPresent(obj: any): boolean;
+        function isPresent(obj?: any): boolean;
         /**
          * Merge the contents of two objects together into the first object.
          * @deprecated Use Object.assign
@@ -3286,7 +3286,7 @@ declare module 'ember' {
         /**
          * Returns a consistent type for the passed object.
          */
-        function typeOf(item: any): string;
+        function typeOf(item?: any): string;
         /**
          * Copy properties from a source object to a target object.
          * @deprecated Use Object.assign
@@ -3869,7 +3869,6 @@ declare module '@ember/string' {
     export const classify: typeof Ember.String.classify;
     export const dasherize: typeof Ember.String.dasherize;
     export const decamelize: typeof Ember.String.decamelize;
-    export const fmt: typeof Ember.String.fmt;
     export const htmlSafe: typeof Ember.String.htmlSafe;
     export const isHTMLSafe: typeof Ember.String.isHTMLSafe;
     export const loc: typeof Ember.String.loc;
