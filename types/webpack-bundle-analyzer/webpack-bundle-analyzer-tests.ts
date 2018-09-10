f1e2fe8cbe0672d9a95fc68f84c39d63d7471bec
Michael Strobel <michael@kryops.de>
2018-09-10 17:34:54 +0200

    [webpack-bundle-analyzer] Add excludeAssets option
    This was added in webpack-bundle-analyzer 2.12.0

--- types/webpack-bundle-analyzer/webpack-bundle-analyzer-tests.ts
+++ types/webpack-bundle-analyzer/webpack-bundle-analyzer-tests.ts
@@ -19,6 +19,11 @@ const config: webpack.Configuration = {
             statsOptions: {
                 source: false
             },
+            excludeAssets: [
+                'foo',
+                /foo/,
+                assetName => assetName.indexOf('foo') !== -1
+            ],
             logLevel: 'info'
         })
     ]
