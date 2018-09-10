e3000c26be0d7c5871dcff409f589f1f956931ac
Maxim Gerasimov <maxim.gerasimov@technomos.ru>
2018-09-11 01:14:04 +0300

    add missing description

--- types/fluxible-addons-react/index.d.ts
+++ types/fluxible-addons-react/index.d.ts
@@ -8,10 +8,22 @@ import * as React from 'react';
 import BaseStore = require('fluxible/addons/BaseStore');
 import { ComponentContext } from "fluxible";
 
+/**
+ * Registers change listeners and retrieves state from stores using the `getStateFromStores`
+ * method. Concept provided by Dan Abramov via
+ * @param [Component] component to pass state as props to.
+ * @param stores List of stores to listen for changes
+ * @param getStateFromStores function that receives all stores and should return
+ *      the full state object. Receives `stores` hash and component `props` as arguments
+ * @param [customContextTypes] additional `contextTypes` that could be accessed from your `getStateFromStores`
+ *      function
+ * @returns React.Component
+ */
 export function connectToStores(
  Component: typeof React.Component,
  stores: Array<typeof BaseStore> | string[],
- getStateFromStores: (context: ComponentContext, props: any) => any): typeof React.Component;
+ getStateFromStores: (context: ComponentContext, props: any) => any,
+ customContextTypes?: any): typeof React.Component;
 
 /**
  * Provides context prop to all children as React context
