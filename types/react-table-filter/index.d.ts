9f0266511c507b4602a5765de3baa300eca03475
Mike North <michael.l.north@gmail.com>
2018-09-11 10:37:23 -0700

    [react-table-filter] build fixes

--- types/react-table-filter/index.d.ts
+++ types/react-table-filter/index.d.ts
@@ -1,16 +1,16 @@
-// Type definitions for react-table-filter 1.0.0
+// Type definitions for react-table-filter 1.0
 // Project: https://github.com/cheekujha/react-table-filter
-// Definitions by: Gnanavel N <https://github.com/gjsln/>
+// Definitions by: Gnanavel N <https://github.com/gjsln>
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
 // TypeScript Version: 2.8
 
 import * as React from 'react';
 
 export interface TabfilterProps {
-    rows: string | Array<string> | { [name: string]: boolean; };
-	onFilterUpdate: (filteredArray: Array<string>, currentFilters: any[]) => any[] | void;
+    rows: string | string[] | { [name: string]: boolean; };
+	onFilterUpdate: (filteredArray: string[], currentFilters: any[]) => any[] | void;
 	rowClass?: string;
-	initialFilters?: string | Array<string> | { [name: string]: boolean; };
+	initialFilters?: string | string[] | { [name: string]: boolean; };
 }
 
-export declare class TableFilter extends React.Component<TabfilterProps> {}
+export class TableFilter extends React.Component<TabfilterProps> {}
