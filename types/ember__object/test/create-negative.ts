067c6edd2fe88f8dc7ddf1687dff6ad1feaf73c8
Mike North <michael.l.north@gmail.com>
2018-09-12 14:40:06 -0700

    Update create-negative.ts

--- types/ember__object/test/create-negative.ts
+++ types/ember__object/test/create-negative.ts
@@ -7,5 +7,3 @@ Person.create({}, { firstName: 99 }); // $ExpectError
 Person.create({}, {}, { firstName: 99 }); // $ExpectError
 
 const p4 = new PersonWithNumberName();
-
-// assertType<Ember.ComputedProperty<string, string>>(p4.fullName); // $ExpectError
