af4cc764aaaf581a5cf17a562f88b585086b13e1
Michelle Scharlock <mischarl@microsoft.com>
2018-09-13 10:09:02 -0700

    Changing weird spacing

--- types/custom-function-mapping/custom-function-mappings-tests.ts
+++ types/custom-function-mapping/custom-function-mappings-tests.ts
@@ -1,10 +1,10 @@
 async function getStockValues(ticker: string, handler: CustomFunctions.StreamingHandler<number>) {
     const dollars = await (await fetch(`myService.com/prices/${ticker}`)).json();
-      handler.setResult(dollars)
+        handler.setResult(dollars)
 }
 
 async function getStockValuesOneTime(ticker: string, handler: CustomFunctions.CancelableHandler) {
-      var shouldStop = false;
+    var shouldStop = false;
     handler.onCanceled = () => shouldStop = true;
     await pause(1000);
 
@@ -17,8 +17,8 @@ async function getStockValuesOneTime(ticker: string, handler: CustomFunctions.Ca
 }
 
 async function getStockValuesNowWithNoCancelling(ticker: string) {
-      const dollars = await (await fetch(`myService.com/prices/${ticker}`)).json();
-      return dollars;
+    const dollars = await (await fetch(`myService.com/prices/${ticker}`)).json();
+    return dollars;
 }
 
 function add10(n: number) {
