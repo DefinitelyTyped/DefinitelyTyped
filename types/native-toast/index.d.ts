d213c2408e64ca93919f3c9a2178b1a1fd5cdf41
Michael Nahkies <michael@nahkies.co.nz>
2018-09-13 13:31:00 +0100

    fix lint / write as external module

--- types/native-toast/index.d.ts
+++ types/native-toast/index.d.ts
@@ -3,34 +3,32 @@
 // Definitions by: Michael Nahkies <https://github.com/mnahkies>
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
 
-declare module "native-toast" {
-  const toast: {
+declare const toast: {
     (options: ToastOptions): Toast,
     success: (options: ToastOptions) => Toast,
     warning: (options: ToastOptions) => Toast,
     info: (options: ToastOptions) => Toast,
     error: (options: ToastOptions) => Toast,
-  }
+};
 
-  export default toast
+export default toast;
 
-  export interface ToastOptions {
-    message?: string
-    position?: "center" | "west" | "east" | "south" | "south-west" | "south-east" | "north" | "north-west" | "north-east"
-    timeout?: number
-    el?: any // = document.body,
-    rounded?: boolean
-    type?: "success" | "warning" | "info" | "error"
-    debug?: boolean
-    edge?: boolean
-    icon?: boolean
-    closeOnClick?: boolean
-    elements?: any[]
-  }
+export interface ToastOptions {
+    message?: string;
+    position?: "center" | "west" | "east" | "south" | "south-west" | "south-east" | "north" | "north-west" | "north-east";
+    timeout?: number;
+    el?: HTMLElement;
+    rounded?: boolean;
+    type?: "success" | "warning" | "info" | "error";
+    debug?: boolean;
+    edge?: boolean;
+    icon?: boolean;
+    closeOnClick?: boolean;
+    elements?: HTMLElement[];
+}
 
-  export interface Toast {
-    show(): void
-    hide(): void
-    destroy(): void
-  }
+export interface Toast {
+    show(): void;
+    hide(): void;
+    destroy(): void;
 }
