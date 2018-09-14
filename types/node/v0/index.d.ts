31519390e922b1ac830ed6ca56dbd67b9d3af5a4
Jordan Harband <ljharb@gmail.com>
2018-09-14 13:52:12 -0700

    `node`: [fix] node buffers have a `constructor` property

--- types/node/v0/index.d.ts
+++ types/node/v0/index.d.ts
@@ -379,6 +379,7 @@ declare namespace NodeJS {
  * @deprecated
  */
 interface NodeBuffer {
+    constructor: typeof NodeBuffer;
     [index: number]: number;
     write(string: string, offset?: number, length?: number, encoding?: string): number;
     toString(encoding?: string, start?: number, end?: number): string;
