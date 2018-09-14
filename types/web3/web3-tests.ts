e4060f0683cff1f9df17b44457dcf7240784ad56
Donam Kim <donam.kim@mcngamehub.com>
2018-09-14 11:32:42 +1000

    Remove consecutive blank lines

--- types/web3/web3-tests.ts
+++ types/web3/web3-tests.ts
@@ -11,17 +11,14 @@ const myProvider = new web3.providers.HttpProvider("http://localhost:5454");
 web3.setProvider(myProvider);
 web3.eth.setProvider(myProvider);
 
-
 //
 // web3.eth
 // --------------------------------------------------------------------------
 const storage: Promise<string> = web3.eth.getStorageAt(contractAddress, 0);
 
-
 //
 // web3.eth.subscribe
 // --------------------------------------------------------------------------
-
 
 //
 // web3.eth.Contract
@@ -39,11 +36,9 @@ myContract.options.from = "0x1234567890123456789012345678901234567891";
 myContract.options.gasPrice = "20000000000000";
 myContract.options.gas = 5000000;
 
-
 //
 // web3.eth.accounts
 // --------------------------------------------------------------------------
-
 
 //
 // web3.eth.personal
@@ -54,32 +49,26 @@ web3.eth.personal.unlockAccount(
     600
 );
 
-
 //
 // web3.eth.ens
 // --------------------------------------------------------------------------
-
 
 //
 // web3.eth.Iban
 // --------------------------------------------------------------------------
 
-
 //
 // web3.eth.abi
 // --------------------------------------------------------------------------
-
 
 //
 // web3.bzz
 // --------------------------------------------------------------------------
 
-
 //
 // web3.shh
 // --------------------------------------------------------------------------
 const shhPromise: Promise<string> = web3.shh.generateSymKeyFromPassword("xyz");
-
 
 //
 // web3.utils
