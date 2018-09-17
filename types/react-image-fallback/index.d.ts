f7fac83320cf049f1fc690a6f609a47c4f019e17
UCLES\smithd <smith.b@cambridgeassessment.org.uk>
2018-09-17 16:05:01 +0100

    Added typings for react-image-fallback package

---
+++ types/react-image-fallback/index.d.ts
@@ -0,0 +1,28 @@
+// Type definitions for react-image-fallback 8.0
+// Project: https://github.com/socialtables/react-image-fallback
+// Definitions by: Ben Smith <https://github.com/8enSmith>
+// Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
+
+declare module "react-image-fallback" {
+    import * as React from "react"
+
+    class ReactImageFallback extends React.Component<
+        ReactImageFallbackProps &
+        React.DetailedHTMLProps<
+        React.ImgHTMLAttributes<HTMLImageElement>,
+        HTMLImageElement
+        >,
+        any
+        > { }
+
+    export interface ReactImageFallbackProps {
+        src: string
+        fallbackImage: string | React.ReactElement<any> | any[]
+        initialImage?: string | React.ReactElement<any>
+        onLoad?: (event: React.SyntheticEvent<HTMLImageElement>) => void
+        onError?: (event: React.SyntheticEvent<HTMLImageElement>) => void
+        initialTimeout?: number
+    }
+
+    export default ReactImageFallback
+}
