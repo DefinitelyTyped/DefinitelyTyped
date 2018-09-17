aabb48847bafc8cb8cdc434e7e4df82cee716fbe
Borys Kupar <borys.kupar@ottonova.de>
2018-09-17 13:53:34 +0200

    [jsforce] Added Apex definitions for the connection

--- types/jsforce/index.d.ts
+++ types/jsforce/index.d.ts
@@ -5,12 +5,14 @@
 //                 Thomas Dvornik <https://github.com/amphro>
 //                 Tim Noonan <https://github.com/tnoonan-salesforce>
 //                 Abraham White <https://github.com/whiteabelincoln>
+//                 Borys Kupar <https://github.com/borys-kupar>
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
 // TypeScript Version: 2.3
 
 export * from './api/analytics';
 export * from './api/chatter';
 export * from './api/metadata';
+export * from './api/apex';
 export * from './batch';
 export * from './bulk';
 export * from './cache';
