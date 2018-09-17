18f00d08e91e6352319bbf81fc313cb72c70d17a
Rene <rene keijzer>
2018-09-17 22:41:55 +0200

    fix: linting errors

--- types/node-redis-pubsub/index.d.ts
+++ types/node-redis-pubsub/index.d.ts
@@ -1,27 +1,23 @@
-// Type definitions for node-redis-pubsub 3.0.0
+// Type definitions for node-redis-pubsub 3.0
 // Project: https://github.com/louischatriot/node-redis-pubsub#readme
 // Definitions by: Rene Keijzer <https://github.com/renekeijzer>
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
+// TypeScript Version: 2.2
 
+import * as redis from 'redis';
 
-
-
-
-import *  as redis from 'redis';
-
-declare function NRP(options:object) : NRP.NodeRedisPubSub;
+declare function NRP(options: object): NRP.NodeRedisPubSub;
 declare namespace NRP {
-    function initClient(options:object) : NRP.NodeRedisPubSub;
-
+    function initClient(options: object): NodeRedisPubSub;
     class NodeRedisPubSub {
-        constructor(options : object);
-        getRedisClient(any:void): redis.RedisClient;
-        on(channel:string, handler:Function, callback?:Function):Function;
-        subscribe(channel:string, handler:Function, callback?:Function):Function;
-        emit(channel:string, message:string) : any;
-        publish(channel:string, message:string) : any;
-        quit(any:void):void;
-        end(any:void):void;
+        constructor(options: object);
+        getRedisClient(): redis.RedisClient;
+        on(channel: string, handler: () => void, callback?: () => void): () => void;
+        subscribe(channel: string, handler: () => void, callback?: () => void): () => void;
+        emit(channel: string, message: string): void;
+        publish(channel: string, message: string): void;
+        quit(): void;
+        end(): void;
     }
 }
 
