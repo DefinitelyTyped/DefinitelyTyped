1922aa7f1ed2d4d9e9125e7201ef859bef81cf9f
Carlos Q <carlos.quiroga.informatica@gmail.com>
2018-09-17 20:29:47 +0200

    Update to 1.0
    Add options argument

--- types/require-dir/index.d.ts
+++ types/require-dir/index.d.ts
@@ -1,8 +1,8 @@
-// Type definitions for require-dir 0.3
+// Type definitions for require-dir 1.0
 // Project: https://github.com/aseemk/requireDir
 // Definitions by: weekens <https://github.com/weekens>
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
 
-declare function requireDir(directory: string): { [path: string]: any };
+declare function requireDir(directory: string, options: object): { [path: string]: any };
 
 export = requireDir;
