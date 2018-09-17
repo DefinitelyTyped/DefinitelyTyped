b2d6067bdb5d3e6132e4f0d46c79c578de0712ce
UCLES\smithd <smith.b@cambridgeassessment.org.uk>
2018-09-17 16:24:21 +0100

    Specified typescript version

--- types/react-image-fallback/index.d.ts
+++ types/react-image-fallback/index.d.ts
@@ -2,6 +2,7 @@
 // Project: https://github.com/socialtables/react-image-fallback
 // Definitions by: Ben Smith <https://github.com/8enSmith>
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
+// TypeScript Version: 2.8
 
 declare module "react-image-fallback" {
     import * as React from "react"
