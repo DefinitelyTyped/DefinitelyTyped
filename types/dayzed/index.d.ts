ddb6bc655aaf3a300099a13dee0b2b1c712bfd9d
Sam A. Horvath-Hunt <git@samhh.com>
2018-09-18 10:44:49 +0100

    Bump minimum supported TS version to match React

--- types/dayzed/index.d.ts
+++ types/dayzed/index.d.ts
@@ -2,7 +2,7 @@
 // Project: https://github.com/deseretdigital/dayzed
 // Definitions by: Sam A. Horvath-Hunt <https://github.com/samhh>
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
-// TypeScript Version: 2.3
+// TypeScript Version: 2.8
 
 import { Component, ReactNode } from 'react';
 
