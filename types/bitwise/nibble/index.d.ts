e775c51437c2923c79bf78db9ba443c5225461a8
Daniel Byrne <danwbyrne@gmail.com>
2018-09-18 13:11:28 -0700

    removes typings for bitwise

--- types/bitwise/nibble/index.d.ts
+++
@@ -1,9 +0,0 @@
-import nibbleRead from './read';
-import nibbleWrite from './write';
-
-declare namespace nibble {
-  const read: typeof nibbleRead;
-  const write: typeof nibbleWrite;
-}
-
-export default nibble;
