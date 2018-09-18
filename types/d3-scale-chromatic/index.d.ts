8eeacf7fa956633eb4c0d7e8dcd07ab1e0427aa7
Hugues Stefanski <hugues.stefanski@gmail.com>
2018-09-18 19:15:01 +0200

    Update d3-scale-chromatic number

--- types/d3-scale-chromatic/index.d.ts
+++ types/d3-scale-chromatic/index.d.ts
@@ -1,4 +1,4 @@
-// Type definitions for D3JS d3-scale-chromatic module 1.2
+// Type definitions for D3JS d3-scale-chromatic module 1.3
 // Project: https://github.com/d3/d3-scale-chromatic/
 // Definitions by: Hugues Stefanski <https://github.com/Ledragon>,
 //                 Alex Ford <https://github.com/gustavderdrache>,
@@ -6,7 +6,7 @@
 //                 Henrique Machado <https://github.com/henriquefm>
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
 
-// Last module patch version validated against: 1.2.0
+// Last module patch version validated against: 1.3.2
 
 // -----------------------------------------------------------------------
 // Categorical
