a645a094790f58b44261e28db1579f78ce56f8c9
Josef Reinhard Brandl <mail@josefbrandl.de>
2018-09-19 10:44:06 +0200

    [k6] Correct author name

--- types/k6/index.d.ts
+++ types/k6/index.d.ts
@@ -1,6 +1,6 @@
 // Type definitions for k6 0.0
 // Project: https://github.com/loadimpact/k6#readme
-// Definitions by: My Self <https://github.com/MajorBreakfast>
+// Definitions by: MajorBreakfast <https://github.com/MajorBreakfast>
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
 // TypeScript Version: 2.2
 
