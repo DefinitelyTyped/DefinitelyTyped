1bb91cda27ef5db4882fa6a59434dc6097de9382
Dennis Kugelmann <kugelmann.dennis@gmail.com>
2018-09-19 13:54:54 +0200

    Add author

--- types/in-app-purchase/index.d.ts
+++ types/in-app-purchase/index.d.ts
@@ -1,6 +1,6 @@
 // Type definitions for in-app-purchase 1.9
 // Project: https://github.com/voltrue2/in-app-purchase#readme
-// Definitions by: Jonas Lochmann <https://github.com/l-jonas>
+// Definitions by: Jonas Lochmann <https://github.com/l-jonas>, Dennis Kugelmann <https://github.com/IchordeDionysos>
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
 // TypeScript Version: 2.2
 
