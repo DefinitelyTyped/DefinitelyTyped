d792085eed21a3e5f29a8f1a2261a3a885c8d2ac
Maarten Staa <maarten.staa@brightalley.nl>
2018-09-20 16:00:28 +0200

    Sortablejs: Update version in header.

--- types/sortablejs/index.d.ts
+++ types/sortablejs/index.d.ts
@@ -1,4 +1,4 @@
-// Type definitions for Sortable.js 1.3
+// Type definitions for Sortable.js 1.7
 // Project: https://github.com/RubaXa/Sortable
 // Definitions by: Maw-Fox <https://github.com/Maw-Fox>
 //                 Maarten Staa <https://github.com/maartenstaa>
