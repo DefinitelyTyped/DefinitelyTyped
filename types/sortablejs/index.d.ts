05fa0c611dad992b40775f465806090754f60c90
Maarten Staa <maarten.staa@brightalley.nl>
2018-09-20 15:49:08 +0200

    Sortablejs: Add myself to authors.

--- types/sortablejs/index.d.ts
+++ types/sortablejs/index.d.ts
@@ -1,6 +1,7 @@
 // Type definitions for Sortable.js 1.3
 // Project: https://github.com/RubaXa/Sortable
 // Definitions by: Maw-Fox <https://github.com/Maw-Fox>
+//                 Maarten Staa <https://github.com/maartenstaa>
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
 // TypeScript Version: 2.1
 
