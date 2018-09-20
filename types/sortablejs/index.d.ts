d33b7ec2d8a0f59ce52810b692574375bd1d0585
Maarten Staa <maarten.staa@brightalley.nl>
2018-09-20 15:47:29 +0200

    Sortablejs: Support Typescript 2.1+ for keyof support.

--- types/sortablejs/index.d.ts
+++ types/sortablejs/index.d.ts
@@ -2,6 +2,7 @@
 // Project: https://github.com/RubaXa/Sortable
 // Definitions by: Maw-Fox <https://github.com/Maw-Fox>
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
+// TypeScript Version: 2.1
 
 export = Sortable;
 
