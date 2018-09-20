c470a2b80242380b4a2d36d164ec75070b0dee73
Simon Schick <SimonSchick@users.noreply.github.com>
2018-09-20 18:59:56 +0200

    Update index.d.ts

--- types/shot/index.d.ts
+++ types/shot/index.d.ts
@@ -1,7 +1,7 @@
 // Type definitions for shot 3.4
 // Project: https://github.com/hapijs/shot
-// Definitions by:  AJP <https://github.com/AJamesPhillips>
-//                  Simon Schick <https://github.com/SimonSchick>
+// Definitions by: AJP <https://github.com/AJamesPhillips>
+//                 Simon Schick <https://github.com/SimonSchick>
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
 // TypeScript Version: 2.2
 
