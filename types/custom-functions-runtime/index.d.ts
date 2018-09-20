502af779fb6a9c77f1d30e6ad5af34e01283a0dd
mscharlock <mischarl@microsoft.com>
2018-09-19 21:34:31 -0700

    adding comment

--- types/custom-functions-runtime/index.d.ts
+++ types/custom-functions-runtime/index.d.ts
@@ -9,6 +9,9 @@ office-js
 Copyright (c) Microsoft Corporation
 */
 
+/* 
+* Enables you to map your own name that uses lowercase letters to a function. 
+*/
 declare let CustomFunctionMappings: { [key: string]: Function };
 
 declare namespace CustomFunctions {
