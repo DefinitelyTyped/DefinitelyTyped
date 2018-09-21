20a5aec10e1b4a624296075a75cf541ddfc8261e
erikma <erikma1@hotmail.com>
2018-09-20 20:30:38 -0700

    Fix test failure

--- types/pigpio-dht/pigpio-dht-tests.ts
+++ types/pigpio-dht/pigpio-dht-tests.ts
@@ -1,7 +1,7 @@
 import * as dht from 'pigpio-dht';
 
 const dhtReadPin = 2;
-const sensor: dht.Dht = dht(dhtReadPin, dht.DhtType.DHT11);
+const sensor: dht.Dht = dht(dhtReadPin, 11);
 sensor.on('start', () => console.log("start"));
 sensor.on('end', () => console.log("end"));
 sensor.on('result', (result: dht.DhtResult) => console.log(`result: ${result.temperature} degC, ${result.humidity}% relative humidity`));
