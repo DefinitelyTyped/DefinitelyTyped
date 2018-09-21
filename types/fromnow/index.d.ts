5f85f488ce87e029b9d0820fc043c417b8a477a8
Luke Edwards <luke.edwards05@gmail.com>
2018-09-21 14:44:00 -0700

    bump version

--- types/fromnow/index.d.ts
+++ types/fromnow/index.d.ts
@@ -1,4 +1,4 @@
-// Type definitions for fromnow v2.0.0
+// Type definitions for fromnow v3.0.0
 // Project: https://github.com/lukeed/fromNow
 // Definitions by: Martin Bukovics <https://github.com/marinewater>
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
