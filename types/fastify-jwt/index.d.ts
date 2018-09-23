21a6230627cdb3555e2da51dd757d836a052214e
Jannik Keye <jannik.keye@gmail.com>
2018-09-23 10:02:46 +0200

    fix my name

--- types/fastify-jwt/index.d.ts
+++ types/fastify-jwt/index.d.ts
@@ -1,6 +1,6 @@
 // Type definitions for fastify-jwt 0.4
 // Project: https://github.com/fastify/fastify-jwt#readme
-// Definitions by: My Self <https://github.com/jannikkeye>
+// Definitions by: Jannik Keye <https://github.com/jannikkeye>
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
 // TypeScript Version: 2.3
 import fastify = require("fastify");
