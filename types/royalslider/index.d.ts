3492bc451662597795f47ababb19a45defcb8de9
Pecinka, Libor <LiborP@oriflame.com>
2018-09-24 13:42:34 +0200

    slides type changed from any[] to RoyalSliderSlide[]

--- types/royalslider/index.d.ts
+++ types/royalslider/index.d.ts
@@ -1,6 +1,6 @@
 // Type definitions for jQuery royal-slider 9.4
 // Project: http://dimsemenov.com/plugins/royal-slider/documentation/
-// Definitions by: Christiaan Rakowski <https://github.com/csrakowski>
+// Definitions by: Christiaan Rakowski <https://github.com/csrakowski>, sKopheK <https://github.com/sKopheK/DefinitelyTyped>
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
 // TypeScript Version: 2.3
 
@@ -366,73 +366,73 @@ declare namespace RoyalSlider {
          */
         visibleNearby?: RoyalSliderVisibleOptions;
     }
-	
-	export interface RoyalSliderSlide {
-		/**
-		 * TODO
-		 */
-		appendOnLoaded: boolean;
-		/**
-		 * slide element
-		 */
-		content: JQuery;
-		/**
-		 * TODO
-		 */
-		contentAdded: boolean;
-		/**
-		 * slide element wrapper
-		 */
-		holder: JQuery;
-		/**
-		 * slide index
-		 */
-		id: number;
-		/**
-		 * TODO
-		 */
-		images: null;
-		/**
-		 * TODO
-		 */
-		isAdded: boolean;
-		/**
-		 * TODO
-		 */
-		isAppended: boolean;
-		/**
-		 * TODO
-		 */
-		isBig: boolean;
-		/**
-		 * TODO
-		 */
-		isLoaded: boolean;
-		/**
-		 * TODO
-		 */
-		isLoading: boolean;
-		/**
-		 * TODO
-		 */
-		isRendered: boolean;
-		/**
-		 * TODO
-		 */
-		loadedTriggered: boolean;
-		/**
-		 * TODO
-		 */
-		positionSet: boolean;
-		/**
-		 * TODO
-		 */
-		sizeReady: boolean;
-		/**
-		 * TODO
-		 */
-		thumbnail: string;
-	}
+
+    export interface RoyalSliderSlide {
+        /**
+         * TODO
+         */
+        appendOnLoaded: boolean;
+        /**
+         * slide element
+         */
+        content: JQuery;
+        /**
+         * TODO
+         */
+        contentAdded: boolean;
+        /**
+         * slide element wrapper
+         */
+        holder: JQuery;
+        /**
+         * slide index
+         */
+        id: number;
+        /**
+         * TODO
+         */
+        images: null;
+        /**
+         * TODO
+         */
+        isAdded: boolean;
+        /**
+         * TODO
+         */
+        isAppended: boolean;
+        /**
+         * TODO
+         */
+        isBig: boolean;
+        /**
+         * TODO
+         */
+        isLoaded: boolean;
+        /**
+         * TODO
+         */
+        isLoading: boolean;
+        /**
+         * TODO
+         */
+        isRendered: boolean;
+        /**
+         * TODO
+         */
+        loadedTriggered: boolean;
+        /**
+         * TODO
+         */
+        positionSet: boolean;
+        /**
+         * TODO
+         */
+        sizeReady: boolean;
+        /**
+         * TODO
+         */
+        thumbnail: string;
+    }
 
     export interface RoyalSlider { //TODO: extends/implements JQuery? (giving problems due to next(), prev(), width and height and 'selector'.
         /**
@@ -538,7 +538,7 @@ declare namespace RoyalSlider {
         /**
          * contains all data about each slide
          */
-        slides: any[]; //TODO: what type?
+        slides: RoyalSliderSlide[];
         /**
          * Contains list of HTML slides that are added to slider
          */
