873dd39d02d5f8d53a422e1368483cb846cd1d9d
Pecinka, Libor <LiborP@oriflame.com>
2018-09-24 14:19:28 +0200

    Definitions by URL fix

--- types/royalslider/index.d.ts
+++ types/royalslider/index.d.ts
@@ -1,6 +1,6 @@
 // Type definitions for jQuery royal-slider 9.4
 // Project: http://dimsemenov.com/plugins/royal-slider/documentation/
-// Definitions by: Christiaan Rakowski <https://github.com/csrakowski>, sKopheK <https://github.com/sKopheK/DefinitelyTyped>
+// Definitions by: Christiaan Rakowski <https://github.com/csrakowski>, sKopheK <https://github.com/sKopheK>
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
 // TypeScript Version: 2.3
 
