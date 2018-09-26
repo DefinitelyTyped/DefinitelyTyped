9d4aa5c00e158dae7bb9aac1b6e21a0105515e1b
Aaron Holderman <aaron@pieinsurance.com>
2018-09-26 14:57:37 -0600

    update author and TS version

--- types/apostrophe/index.d.ts
+++ types/apostrophe/index.d.ts
@@ -1,8 +1,8 @@
 // Type definitions for apostrophe 2.67
 // Project: https://github.com/apostrophecms/apostrophe#readme
-// Definitions by: My Self <https://github.com/me>
+// Definitions by: Aaron Holderman <https://github.com/afholderman>
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
-// TypeScript Version: 2.8
+// TypeScript Version: 2.2
 
 export = apostrophe;
 export as namespace apos;
