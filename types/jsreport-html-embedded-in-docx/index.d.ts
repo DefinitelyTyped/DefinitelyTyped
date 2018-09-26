fd4267dea3d0053a6e54ca5b38b53a7ec7685ed1
taoqf <tao_qiufeng@126.com>
2018-09-26 15:47:07 +0800

    update authorlist

--- types/jsreport-html-embedded-in-docx/index.d.ts
+++ types/jsreport-html-embedded-in-docx/index.d.ts
@@ -1,6 +1,6 @@
 // Type definitions for jsreport-html-embedded-in-docx 1.0
 // Project: https://github.com/jsreport/jsreport-html-embedded-in-docx
-// Definitions by: My Self <https://github.com/me>
+// Definitions by: taoqf <https://github.com/taoqf>
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
 // TypeScript Version: 2.3
 
