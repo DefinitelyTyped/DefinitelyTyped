2d225037de8cb740be6734b2ac6786ee3d1974f9
Lise Petersen <petersen@monsenso.com>
2018-09-26 08:49:57 +0200

    Elaborate comment

--- types/highcharts/index.d.ts
+++ types/highcharts/index.d.ts
@@ -5768,7 +5768,13 @@ declare namespace Highcharts {
          *            color: '#FF00FF'
          *        }]
          *
-         * 4. An array of arrays with three values for ranges. In this case the values correspond x, yMin and yMax.
+         * 4. An array of arrays with three values for ranges. In this case the values correspond x, yMin and yMax. If the
+         *    first value is a string it is applied as the name of the point, and the x value is inferred.
+         *        data: [
+         *            [1, 2, 3],
+         *            [2, 4, 6],
+         *            [3, 7, 8]
+         *
          */
         data?: Array<number | [number, number] | [string, number] | [string, number, number] | [number, number, number] | DataPoint>;
         /**
