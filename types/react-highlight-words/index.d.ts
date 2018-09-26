ed285e0bc63a27bc815a0636368a01a20e8fa53e
kellyrmilligan <kelly.milligan@gmail.com>
2018-09-26 09:58:58 -0500

    added name to typings header

--- types/react-highlight-words/index.d.ts
+++ types/react-highlight-words/index.d.ts
@@ -2,6 +2,7 @@
 // Project: https://github.com/bvaughn/react-highlight-words#readme
 // Definitions by: Mohamed Hegazy <https://github.com/mhegazy>
 //                 Diogo Corrêa <https://github.com/diogodca>
+//                 Kelly Milligan <https://github.com/kellyrmilligan>
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
 // TypeScript Version: 2.8
 
