0e42a951d2a6982126bcc480d7840574e1084c96
Michael Doughty <mdoughty440@gmail.com>
2018-09-26 11:30:47 -0400

    Lazy.JS 0.3.4

--- types/lazy.js/index.d.ts
+++ types/lazy.js/index.d.ts
@@ -1,4 +1,4 @@
-// Type definitions for Lazy.js 0.3.3
+// Type definitions for Lazy.js 0.3.4
 // Project: https://github.com/dtao/lazy.js/
 // Definitions by: Bart van der Schoor <https://github.com/Bartvds>, Mike Doughty <https://github.com/miso440>
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
