f80774245f4d94eb1e6e1d5b1994929762fa1c76
Jared Szechy <jared.szechy@gmail.com>
2018-09-27 12:24:02 -0400

    [actioncable] Add missing function to Cable/Consumer interface

--- types/actioncable/index.d.ts
+++ types/actioncable/index.d.ts
@@ -1,7 +1,9 @@
-// Type definitions for ActionCable
-// Project: https://github.com/rails/rails/tree/master/actioncable
+// Type definitions for ActionCable 5.2
+// Project: https://github.com/rails/rails/tree/master/actioncable/app/assets/javascripts
 // Definitions by: Vincent Zhu <https://github.com/zhu1230>
+//                 Jared Szechy <https://github.com/szechyjs>
 // Definitions: https://github.com/zhu1230/DefinitelyTyped
+// TypeScript Version: 2.3
 
 declare module ActionCable {
   interface Channel {
@@ -16,6 +18,10 @@ declare module ActionCable {
 
   interface Cable {
     subscriptions: Subscriptions;
+    send(data: Object): void;
+    connect(): void;
+    disconnect(): void;
+    ensureActiveConnection(): void;
   }
 
   interface CreateMixin {
