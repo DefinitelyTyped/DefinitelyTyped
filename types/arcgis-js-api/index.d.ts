104430bd48936ac911709edad2bf146cbbdc8781
Dasa Paddock <dpaddock@esri.com>
2018-09-27 16:26:25 -0700

    Bump minimum TypeScript version to 2.7
    This is needed for AbortSignal.

--- types/arcgis-js-api/index.d.ts
+++ types/arcgis-js-api/index.d.ts
@@ -3,7 +3,7 @@
 // Definitions by: Esri <https://github.com/Esri>
 //                 Bjorn Svensson <https://github.com/bsvensson>
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
-// TypeScript Version: 2.3
+// TypeScript Version: 2.7
 
 
 interface HashMap<T = any> {
