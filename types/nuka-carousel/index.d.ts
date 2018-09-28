05640a794fe7b641f1fc85c25c5fb9f9c1c9f81b
Alex Smith <alexsmith@MacBook-Pro-7.local>
2018-09-28 12:56:28 +0100

    nuka-carousel - Updates version number

--- types/nuka-carousel/index.d.ts
+++ types/nuka-carousel/index.d.ts
@@ -1,4 +1,4 @@
-// Type definitions for nuka-carousel 4.2
+// Type definitions for nuka-carousel 4.3
 // Project: https://github.com/FormidableLabs/nuka-carousel
 // Definitions by: Roman Charugin <https://github.com/Romic>, Alex Smith <https://github.com/altaudio>
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
