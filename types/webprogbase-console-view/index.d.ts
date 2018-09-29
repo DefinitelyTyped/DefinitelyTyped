3dcb42e5864ff37bee96e22ce57b1d923a57592b
VitahaKryvenko <gerzoh1@gmail.com>
2018-09-30 00:00:28 +0300

    added tsc >=2.2 restriction to the header

--- types/webprogbase-console-view/index.d.ts
+++ types/webprogbase-console-view/index.d.ts
@@ -2,6 +2,7 @@
 // Project: https://github.com/DevInCube/webprogbase-console-view#readme
 // Definitions by: Veetaha <https://github.com/veetaha>
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
+// TypeScript Version: 2.2
 
 export interface StringToStringMap {
     [key: string]: string;
