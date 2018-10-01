37adf292af330edf80344153cb7b060f2160cf7d
Filipe Guerra <alias.mac@gmail.com>
2018-10-01 11:31:09 -0700

    fix ReactTestIntance type typing
    Tested with the following code:
    ```
    component.root.find(
      node => node.type === MyButton && node.props.type === ‘submit’
    )
    ```

--- types/react-test-renderer/index.d.ts
+++ types/react-test-renderer/index.d.ts
@@ -25,7 +25,7 @@ export interface ReactTestRendererTree extends ReactTestRendererJSON {
 }
 export interface ReactTestInstance {
     instance: any;
-    type: string;
+    type: ReactType;
     props: { [propName: string]: any };
     parent: null | ReactTestInstance;
     children: Array<ReactTestInstance | string>;
