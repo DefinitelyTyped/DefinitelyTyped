34d6ce0f5a9ae5e78f9f0c3d3dbdff2b7603b91c
William Robertson <wr222ae@student.lnu.se>
2018-10-02 08:38:14 +0200

    Updated @param disableAutoBOM
    Removed "don't" since it doesn't reflect the official documentation.
    
    see -> https://github.com/eligrey/FileSaver.js/#syntax

--- types/file-saver/index.d.ts
+++ types/file-saver/index.d.ts
@@ -10,7 +10,7 @@ declare namespace FileSaver {
      * FileSaver.js implements the saveAs() FileSaver interface in browsers that do not natively support it.
      * @param data - The actual file data blob.
      * @param filename - The optional name of the file to be downloaded. If omitted, the name used in the file data will be used. If none is provided "download" will be used.
-     * @param disableAutoBOM - Optional & defaults to `false`. Set to `true` if you don't want FileSaver.js to automatically provide Unicode text encoding hints
+     * @param disableAutoBOM - Optional & defaults to `false`. Set to `true` if you want FileSaver.js to automatically provide Unicode text encoding hints
      */
     function saveAs(data: Blob, filename?: string, disableAutoBOM?: boolean): void;
 }
