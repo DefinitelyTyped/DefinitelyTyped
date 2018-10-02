b0b15a7a322f8f32a56a99e0eca4d41a6d3ce80f
Garrett Dawson <killtheliterate@gmail.com>
2018-10-01 22:12:58 -0600

    react-map-gl - Make mapboxApiAccessToken prop optional (#29241)
    * Make mapboxApiAccessToken optional
    
    * Revise transformRequest

--- types/react-map-gl/index.d.ts
+++ types/react-map-gl/index.d.ts
@@ -23,20 +23,20 @@ export interface MapError {
 
 export interface MapRequest {
     url: string;
-    headers: {};
-    credentials: string;
+    headers?: { [index: string]: string };
+    credentials?: string;
 }
 
 export interface MapboxProps extends Partial<Viewport> {
     container?: {};
-    mapboxApiAccessToken: string;
+    mapboxApiAccessToken?: string;
     attributionControl?: boolean;
     preserveDrawingBuffer?: boolean;
     onLoad?: () => void;
     onError?: (e: MapError) => void;
     resuseMaps?: boolean;
     resuseMap?: boolean;
-    transformRequest?: () => MapRequest;
+    transformRequest?: (url?: string, resourceType?: string) => MapRequest;
 
     mapStyle?: string | {};
 
