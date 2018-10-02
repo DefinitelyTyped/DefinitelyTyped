8177d60798f50fb683006452786b3cd4e21516db
leocavalcante <lc@leocavalcante.com>
2018-10-02 16:01:39 -0300

    Add next-redux-saga types

---
+++ types/next-redux-saga/index.d.ts
@@ -0,0 +1,11 @@
+// Type definitions for next-redux-saga 3.0
+// Project: https://github.com/bmealhouse/next-redux-saga
+// Definitions by: Leo Cavalcante <https://github.com/leocavalcante>
+// Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
+
+import { ComponentType } from "react";
+
+declare function nextReduxSaga<P = any>(arg: ComponentType<P>): ComponentType<P>
+declare function nextReduxSaga<P = any>(arg: { async: boolean }): ((BaseComponent: ComponentType<P>) => ComponentType<P>);
+
+export default nextReduxSaga;
