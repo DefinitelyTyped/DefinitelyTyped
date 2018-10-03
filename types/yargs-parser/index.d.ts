44adba1fa62c986b26e2984312081ae769cdcb6d
Miles Johnson <mileswjohnson@gmail.com>
2018-10-03 15:08:40 +0200

    Add yargs-parser package.

---
+++ types/yargs-parser/index.d.ts
@@ -0,0 +1,49 @@
+// Type definitions for yargs-parser 10.1
+// Project: https://github.com/yargs/yargs-parser#readme
+// Definitions by: Miles Johnson <https://github.com/milesj>
+// Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
+
+import { Arguments } from 'yargs';
+
+export interface Configuration {
+    'boolean-negation': boolean;
+    'camel-case-expansion': boolean;
+    'combine-arrays': boolean;
+    'dot-notation': boolean;
+    'duplicate-arguments-array': boolean;
+    'flatten-duplicate-arrays': boolean;
+    'negation-prefix': string;
+    'parse-numbers': boolean;
+    'populate--': boolean;
+    'set-placeholder-key': boolean;
+    'short-option-groups': boolean;
+}
+
+export interface Options {
+    alias?: { [key: string]: string | string[] };
+    array?: string[];
+    boolean?: string[];
+    config?: string | string[] | { [key: string]: boolean };
+    configuration?: Configuration;
+    coerce?: { [key: string]: (arg: any) => any };
+    count?: string[];
+    default?: { [key: string]: any };
+    envPrefix?: string;
+    narg?: { [key: string]: number };
+    normalize?: string[];
+    string?: string[];
+    number?: string[];
+    '--'?: boolean;
+}
+
+export interface DetailedArguments {
+    argv: Arguments;
+    error: Error | null;
+    aliases: { [alias: string]: string[] };
+    newAliases: { [alias: string]: boolean };
+    configuration: Configuration;
+}
+
+export function detailed(argv: string | string[], opts?: Options): DetailedArguments;
+
+export default function parse(argv: string | string[], opts?: Options): Arguments;
