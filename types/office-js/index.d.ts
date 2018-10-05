255f8b0bc2643b9a35da5df5ecab79cec57b5347
Elizabeth Samuel <elizs@microsoft.com>
2018-10-05 16:09:45 -0700

    office-js-update-project-name

--- types/office-js/index.d.ts
+++ types/office-js/index.d.ts
@@ -1,5 +1,5 @@
 // Type definitions for Office.js
-// Project: https://github.com/OfficeDev/office-js-docs-reference
+// Project: https://github.com/OfficeDev/office-js
 // Definitions by: OfficeDev <https://github.com/OfficeDev>, Lance Austin <https://github.com/LanceEA>, Michael Zlatkovsky <https://github.com/Zlatkovsky>, Kim Brandl <https://github.com/kbrandl>, Ricky Kirkham <https://github.com/Rick-Kirkham>, Alex Jerabek <https://github.com/AlexJerabek>, Elizabeth Samuel <https://github.com/ElizabethSamuel-MSFT>
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
 // TypeScript Version: 2.4
