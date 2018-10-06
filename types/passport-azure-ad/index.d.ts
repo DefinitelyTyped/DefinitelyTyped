c86b1a943074611982fc283bc498197b1e1f96f5
Shekhar Nain <shnain@microsoft.com>
2018-10-06 17:53:01 +0530

    Typescript version fixed

--- types/passport-azure-ad/index.d.ts
+++ types/passport-azure-ad/index.d.ts
@@ -2,6 +2,7 @@
 // Project: https://github.com/AzureAD/passport-azure-ad#readme
 // Definitions by: Shekhar Nain <https://github.com/ShekharNain>
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
+// TypeScript Version: 2.9
 
 export { ITokenPayload, VerifyCallback } from "./common";
 export {
