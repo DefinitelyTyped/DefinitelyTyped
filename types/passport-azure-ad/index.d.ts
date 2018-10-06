e72b050bbafc236b818cef4b32de30e39e360c74
Shekhar Nain <shnain@microsoft.com>
2018-10-06 17:25:49 +0530

    Added types for passport azure ad strategy

---
+++ types/passport-azure-ad/index.d.ts
@@ -0,0 +1,21 @@
+// Type definitions for passport-azure-ad 4.0
+// Project: https://github.com/AzureAD/passport-azure-ad#readme
+// Definitions by: Shekhar Nain https://github.com/ShekharNain
+// Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
+
+export { ITokenPayload, VerifyCallback } from "./common";
+export {
+    BearerStrategy,
+    IBearerStrategyOption,
+    IBearerStrategyOptionWithRequest,
+    VerifyBearerFunction,
+    VerifyBearerFunctionWithReq
+} from "./bearer-strategy";
+export {
+    OIDCStrategy,
+    IOIDCStrategyOption,
+    IOIDCStrategyOptionWithRequest,
+    IProfile,
+    VerifyOIDCFunction,
+    VerifyOIDCFunctionWithReq
+} from "./oidc-strategy";
