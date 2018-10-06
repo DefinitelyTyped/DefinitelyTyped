475698638fa525daa83c34ad01d9603644416a21
zlumer <zlumer@gmail.com>
2018-10-06 14:33:21 +0300

    web3: split sign/signTransaction return values to specific interfaces

--- types/web3/eth/accounts.d.ts
+++ types/web3/eth/accounts.d.ts
@@ -6,11 +6,17 @@ export interface Account {
 }
 
 export interface Signature {
-	message: string;
-	hash: string;
+	messageHash: string;
 	r: string;
 	s: string;
 	v: string;
+}
+export interface MessageSignature extends Signature {
+	message: string;
+	signature: string;
+}
+export interface TxSignature extends Signature {
+	rawTransaction: string;
 }
 
 export interface PrivateKey {
@@ -42,15 +48,13 @@ export default interface Accounts {
 	signTransaction(
 		tx: Tx,
 		privateKey: string,
-		returnSignature?: boolean,
-		cb?: (err: Error, result: string | Signature) => void
-	): Promise<string> | Signature;
-	recoverTransaction(signature: string | Signature): string;
+		cb?: (err: Error, result: TxSignature) => void
+	): Promise<TxSignature>;
+	recoverTransaction(signature: string): string;
 	sign(
 		data: string,
-		privateKey: string,
-		returnSignature?: boolean
-	): string | Signature;
+		privateKey: string
+	): MessageSignature;
 	recover(
 		sigOrHash: string | Signature,
 		sigOrV?: string,
