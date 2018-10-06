d92e48158b7633ea8d3f41fc36994c567bec9a55
Jesse Hallett <jesse@sitr.us>
2018-10-06 15:31:36 -0400

    array.prototype.flatmap: importing "/auto" extends the Array prototype

--- types/array.prototype.flatmap/auto.d.ts
+++ types/array.prototype.flatmap/auto.d.ts
@@ -1,2 +1,6 @@
-declare const noExports: {};
-export default noExports;
+interface Array<T> {
+    flatMap<U, R extends object | undefined = undefined>(
+        fn: (this: R, x: T, index: number, array: this) => U[],
+        thisArg?: R
+    ): U[];
+}
