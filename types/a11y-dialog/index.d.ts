f8f22f52ceebc7335fa32d5dd2ae0bd681d6addc
Yuto Otaguro <yotagur@gmail.com>
2018-10-06 22:06:49 -0500

    Finish up implementation for a11y-dialog.

--- types/a11y-dialog/index.d.ts
+++ types/a11y-dialog/index.d.ts
@@ -1,22 +1,37 @@
-// Type definitions for a11y-dialog
+// Type definitions for a11y-dialog 5.2.0
 // Project: https://github.com/edenspiekermann/a11y-dialog
 // Definitions by: Yuto Otaguro <https://github.com/Goyatuzo>
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
-declare class A11yDialog {
-    constructor(el: Element, containers: NodeList | Element | string);
 
-    show();
-    hide();
+type DialogEvents = "show" | "hide" | "destroy" | "create";
+declare class A11yDialog {
+    constructor(el: Element | null, containers?: NodeList | Element | string);
+
+    /**
+     * Shows the dialog.
+     */
+    show(): void;
+    /**
+     * Hides the dialog.
+     */
+    hide(): void;
     /**
      * Unbind click listeners from dialog openers and closers and remove all bound custom event listeners registered with `.on()`
      */
-    destroy();
+    destroy(): void;
     /**
      * Bind click listeners to dialog openers and closers.
      */
-    create();
+    create(): void;
 
-    on(evt: "show" | "hide" | "destroy" | "create", callback: (dialogElement: Element, event: Event) => void);
-    on(evt: "show" | "hide" | "destroy" | "create", callback: (dialogElement: Element) => void);
-    on(evt: "show" | "hide" | "destroy" | "create", callback: () => void);
+
+    on<T extends Element>(evt: DialogEvents, callback: (dialogElement: T, event: Event) => void): void;
+    on<T extends Element>(evt: DialogEvents, callback: (dialogElement: T) => void): void;
+    on(evt: DialogEvents, callback: () => void): void;
+
+    off<T extends Element>(evt: DialogEvents, callback: (dialogElement: T, event: Event) => void): void;
+    off<T extends Element>(evt: DialogEvents, callback: (dialogElement: T) => void): void;
+    off(evt: DialogEvents, callback: () => void): void;
 }
+
+export = A11yDialog;
