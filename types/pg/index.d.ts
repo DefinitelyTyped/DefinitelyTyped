2d64a4ca0c4cab105f48d603a5a6058763603bbf
borkaborka <4437770+borkaborka@users.noreply.github.com>
2018-10-07 23:08:42 +0300

    Added query(QueryArrayConfig, values) overload

--- types/pg/index.d.ts
+++ types/pg/index.d.ts
@@ -154,6 +154,7 @@ export class Pool extends events.EventEmitter {
 
     query<T extends Submittable>(queryStream: T): T;
     query(queryConfig: QueryArrayConfig): Promise<QueryArrayResult>;
+    query(queryConfig: QueryArrayConfig, values?: any[]): Promise<QueryArrayResult>;
     query(queryConfig: QueryConfig): Promise<QueryResult>;
     query(queryTextOrConfig: string | QueryConfig, values?: any[]): Promise<QueryResult>;
     query(queryConfig: QueryArrayConfig, callback: (err: Error, result: QueryArrayResult) => void): Query;
@@ -172,6 +173,7 @@ export class ClientBase extends events.EventEmitter {
 
     query<T extends Submittable>(queryStream: T): T;
     query(queryConfig: QueryArrayConfig): Promise<QueryArrayResult>;
+    query(queryConfig: QueryArrayConfig, values?: any[]): Promise<QueryArrayResult>;
     query(queryConfig: QueryConfig): Promise<QueryResult>;
     query(queryTextOrConfig: string | QueryConfig, values?: any[]): Promise<QueryResult>;
     query(queryConfig: QueryArrayConfig, callback: (err: Error, result: QueryArrayResult) => void): Query;
