eec0c69ef4381efd544e4617b5b6b2cb389235e0
leocavalcante <lc@leocavalcante.com>
2018-10-08 14:34:58 -0300

    require(next-redux-saga) is itself a function, not an object with a default property

--- types/next-redux-saga/index.d.ts
+++ types/next-redux-saga/index.d.ts
@@ -9,4 +9,4 @@ import { ComponentType } from "react";
 declare function nextReduxSaga<P = any>(arg: { async: boolean }): ((BaseComponent: ComponentType<P>) => ComponentType<P>);
 declare function nextReduxSaga<P = any>(arg: ComponentType<P>): ComponentType<P>;
 
-export default nextReduxSaga;
+export = nextReduxSaga;
