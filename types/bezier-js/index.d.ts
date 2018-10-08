910fe1c3163d99252ac9bcb9597a6a6ff1a1fa5a
Simon Epskamp <simon@frontwise.com>
2018-10-08 19:51:52 +0200

    Add updated by

--- types/bezier-js/index.d.ts
+++ types/bezier-js/index.d.ts
@@ -1,6 +1,7 @@
 // Type definitions for Bezier.js
 // Project: https://github.com/Pomax/bezierjs
 // Definitions by: Dan Marshall <https://github.com/danmarshall>
+//                 Simon <https://github.com/Epskampie>
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
 
 declare namespace BezierJs {
