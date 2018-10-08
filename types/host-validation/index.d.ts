f50ff86e1402056ef1ea92522afedf37bd2b5334
Rich <rich@Mac-Pro.local>
2018-10-07 22:53:41 -0700

    Changed T[] to Array<T> because it is non-simple type.

--- types/host-validation/index.d.ts
+++ types/host-validation/index.d.ts
@@ -1,12 +1,12 @@
-// Type definitions for host-validation 2.0.0
+// Type definitions for host-validation 2.0
 // Project: https://github.com/brannondorsey/host-validation
 // Definitions by: Rich Liu <https://github.com/dintopple>
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
 // TypeScript Version: 2.8
-import { Request, Response, NextFunction} from 'express';
+import { Request, Response, NextFunction } from 'express';
 
 interface config {
-    hosts: (string|RegExp)[];
+    hosts: Array<string|RegExp>;
 }
 
 declare function hostValidation(hosts: config): (req: Request, res: Response, next: NextFunction) => void;
