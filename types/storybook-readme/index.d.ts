0873d294b950c34a895bdd8417df0b2b33f8e80f
Taeheon Kim <skyrabbits1@gmail.com>
2018-10-08 12:25:59 +0900

    hm... correct typo

--- types/storybook-readme/index.d.ts
+++ types/storybook-readme/index.d.ts
@@ -1,4 +1,4 @@
-// Type definitions for storybook/readme 4.0
+// Type definitions for storybook-readme 4.0
 // Project: https://github.com/tuchk4/storybook-readme
 // Definitions by: Taeheon Kim  <https://github.com/lonyele>
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
