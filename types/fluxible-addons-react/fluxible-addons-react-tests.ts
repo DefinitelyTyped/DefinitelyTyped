5424ed98371a0766394d04b5975d3dab049b80ed
Maxim Gerasimov <maxim.gerasimov@technomos.ru>
2018-10-09 12:09:25 +0300

    add tests

---
+++ types/fluxible-addons-react/fluxible-addons-react-tests.ts
@@ -0,0 +1,43 @@
+import { connectToStores, provideContext } from 'fluxible-addons-react';
+import { Fluxible, ComponentContext } from 'fluxible';
+import BaseStore = require('fluxible/addons/BaseStore');
+import * as React from 'react';
+
+interface HomeProps {
+	stringValue: string;
+}
+
+class Home extends React.Component<HomeProps, any> {
+	constructor(props: HomeProps) {
+		super(props);
+	}
+}
+
+class ExtendedStore extends BaseStore {
+    static handlers = {
+        ACTION_NAME: 'actionHandler'
+    };
+
+	private data: string;
+
+    actionHandler() {
+        this.emitChange();
+    }
+
+	setData(data: string): void {
+		this.data = data;
+	}
+
+	getData(): string {
+		return this.data;
+	}
+}
+
+// connecting Home react component to ExtendedStore
+const ConnectedComponent = connectToStores(Home, [ExtendedStore], (context: ComponentContext, props: HomeProps) => {
+    return {
+        stringValue: context.getStore(ExtendedStore).getData(),
+    };
+});
+
+const ConnectedComponentWithContext = provideContext(ConnectedComponent);
