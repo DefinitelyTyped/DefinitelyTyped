30d9fa1ce81f26719239b59e497a9c705a4627ca
Guo Yunhe <i@guoyunhe.me>
2018-10-09 09:04:29 +0300

    react-breadcrumbs: remove previous author
    Previous author asked to remove his name because he doesn't use the type and cannot review future changes

--- types/react-breadcrumbs/index.d.ts
+++ types/react-breadcrumbs/index.d.ts
@@ -1,6 +1,6 @@
 // Type definitions for react-breadcrumbs 2.1
 // Project: https://github.com/svenanders/react-breadcrumbs
-// Definitions by: Guo Yunhe <https://github.com/guoyunhe>, Kostya Esmukov <https://github.com/KostyaEsmukov>
+// Definitions by: Guo Yunhe <https://github.com/guoyunhe>
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
 // TypeScript Version: 2.8
 
