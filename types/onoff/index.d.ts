181a353307cd566968f07011e5b058900a3bcd79
Kalle <khirvola@gmail.com>
2018-10-10 09:25:25 +0300

    Incorrect header
    Added a ">"

--- types/onoff/index.d.ts
+++ types/onoff/index.d.ts
@@ -1,7 +1,7 @@
 // Type definitions for onoff v3.2
 // Project: https://github.com/fivdi/onoff
 // Definitions by: Marcel Ernst <https://github.com/marcel-ernst>
-//                 Kallu609 <https://github.com/Kallu609
+//                 Kallu609 <https://github.com/Kallu609>
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
 
 /// <reference types="node" />
