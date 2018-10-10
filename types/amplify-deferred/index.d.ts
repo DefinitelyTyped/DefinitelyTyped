cbf965608ecb1e67f699d2282c56a3eaad07c465
Leonard Thieu <leonard.thieu@gmail.com>
2018-10-10 08:47:58 -0400

    [amplify-deferred] Requires TypeScript 2.8 due to dependency on `@types/jquery`.

--- types/amplify-deferred/index.d.ts
+++ types/amplify-deferred/index.d.ts
@@ -2,7 +2,7 @@
 // Project: http://amplifyjs.com/
 // Definitions by: Jonas Eriksson <https://github.com/joeriks>, Laurentiu Stamate <https://github.com/laurentiustamate94>
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
-// TypeScript Version: 2.3
+// TypeScript Version: 2.8
 
 /// <reference types="jquery" />
 
