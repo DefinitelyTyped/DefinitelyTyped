41f5c84058194c97e30e2c86f9056f28c83baac0
Leonard Thieu <leonard.thieu@gmail.com>
2018-10-10 11:15:14 -0400

    [jquery.cookie] Requires TypeScript 2.8 due to dependency on `@types/jquery`.

--- types/jquery.cookie/index.d.ts
+++ types/jquery.cookie/index.d.ts
@@ -2,7 +2,7 @@
 // Project: https://github.com/carhartl/jquery-cookie
 // Definitions by: Roy Goode <https://github.com/RoyGoode>, Ben Lorantfy <https://github.com/BenLorantfy>
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
-// TypeScript Version: 2.3
+// TypeScript Version: 2.8
 
 ///<reference types="jquery" />
 
