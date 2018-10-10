1ab9dab92fe4a0ad860109bd0b0c902dbac4e150
Leonard Thieu <leonard.thieu@gmail.com>
2018-10-10 11:14:46 -0400

    [datatables.net-select] Requires TypeScript 2.8 due to dependency on `@types/jquery`.

--- types/datatables.net-select/index.d.ts
+++ types/datatables.net-select/index.d.ts
@@ -2,7 +2,7 @@
 // Project: https://datatables.net/extensions/select/
 // Definitions by: Jared Szechy <https://github.com/szechyjs>
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
-// TypeScript Version: 2.4
+// TypeScript Version: 2.8
 
 /// <reference types="jquery" />
 /// <reference types="datatables.net"/>
