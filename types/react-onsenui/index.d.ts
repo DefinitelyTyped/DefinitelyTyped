6eb1ca06c924618c5604d5cb73d957f89bc0cc78
Salim <salim@youos.com>
2018-10-10 21:20:41 +0200

    react-onsenui: increased version number

--- types/react-onsenui/index.d.ts
+++ types/react-onsenui/index.d.ts
@@ -1,4 +1,4 @@
-// Type definitions for React Onsen UI (react-onsenui) 2.9.4
+// Type definitions for React Onsen UI (react-onsenui) 2.9.5
 // Project: https://onsen.io/v2/docs/guide/react/
 // Definitions by: Ozytis <https://ozytis.fr>,
 //                 Salim <https://github.com/salim7>,
