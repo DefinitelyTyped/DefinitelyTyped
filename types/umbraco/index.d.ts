602c8d6e0aac8b13b83b693073b6a593e37688d4
Leonard Thieu <leonard.thieu@gmail.com>
2018-10-10 11:29:57 -0400

    [umbraco] Requires TypeScript 2.8 due to dependency on `@types/jquery`.

--- types/umbraco/index.d.ts
+++ types/umbraco/index.d.ts
@@ -2,7 +2,7 @@
 // Project: https://github.com/umbraco
 // Definitions by: DeCareSystemsIreland <https://github.com/DeCareSystemsIreland>
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
-// TypeScript Version: 2.3
+// TypeScript Version: 2.8
 
 /// <reference path="umbraco-resources.d.ts" />
 /// <reference path="umbraco-services.d.ts" />
