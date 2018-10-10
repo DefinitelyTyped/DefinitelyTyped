8b0400476289b68924214738467440167ae9860c
Leonard Thieu <leonard.thieu@gmail.com>
2018-10-10 11:52:47 -0400

    [gulp-help] Requires TypeScript 2.8 due to dependency on `@types/jquery`.

--- types/gulp-help/index.d.ts
+++ types/gulp-help/index.d.ts
@@ -2,7 +2,7 @@
 // Project: https://github.com/chmontgomery/gulp-help
 // Definitions by: Qubo <https://github.com/tkQubo>
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
-// TypeScript Version: 2.3
+// TypeScript Version: 2.8
 
 /// <reference types="node" />
 /// <reference types="gulp" />
