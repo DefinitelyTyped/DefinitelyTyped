469e0ed5b63bc246ae8b8635989c2debc45e799d
Leonard Thieu <leonard.thieu@gmail.com>
2018-10-10 11:15:27 -0400

    [jquery.sortelements] Requires TypeScript 2.8 due to dependency on `@types/jquery`.

--- types/jquery.sortelements/index.d.ts
+++ types/jquery.sortelements/index.d.ts
@@ -2,7 +2,7 @@
 // Project: http://james.padolsey.com/javascript/sorting-elements-with-jquery/
 // Definitions by: Tim Bureck <https://github.com/tbureck>
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
-// TypeScript Version: 2.3
+// TypeScript Version: 2.8
 
 /// <reference types="jquery"/>
 
