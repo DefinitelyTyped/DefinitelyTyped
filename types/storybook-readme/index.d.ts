d972b9a9d56c871704f9f258c72025d9482bc5e8
Taeheon Kim <skyrabbits1@gmail.com>
2018-10-10 13:34:18 +0900

    Fix "npm lint storybook-readme" 1. I'm just curious why all the types should be exported.  Isn't it cleaner putting "export" to only actually used types? (such as withDocs, wthReadme, Doc, Marked) Hm.. or maybe... to be used from actual user?

--- types/storybook-readme/index.d.ts
+++ types/storybook-readme/index.d.ts
@@ -5,20 +5,20 @@
 // TypeScript Version: 2.9
 
 // Shared Types
-type Renderable = React.ComponentType | JSX.Element;
-type RenderFunction = () => Renderable | Renderable[];
-type Readme = string | string[];
+export type Renderable = React.ComponentType | JSX.Element;
+export type RenderFunction = () => Renderable | Renderable[];
+export type Readme = string | string[];
 
-type DecoratorPattern = (
+export type DecoratorPattern = (
   story: RenderFunction,
   context: { kind: string; story: string }
 ) => Renderable | null;
 
-type HOCPattern = (story: RenderFunction) => Renderable | null;
+export type HOCPattern = (story: RenderFunction) => Renderable | null;
 
 // WithReadme Types
-type WithReadmeAsDecorator = (readme: Readme) => DecoratorPattern;
-type WithReadmeAsHOC = (
+export type WithReadmeAsDecorator = (readme: Readme) => DecoratorPattern;
+export type WithReadmeAsHOC = (
   readme: Readme,
   story: RenderFunction
 ) => RenderFunction;
@@ -26,23 +26,23 @@ type WithReadmeAsHOC = (
 export const withReadme: WithReadmeAsDecorator & WithReadmeAsHOC;
 
 // WithDocs Types
-interface CustomComponents {
+export interface CustomComponents {
   PreviewComponent: (props: { children: JSX.Element }) => JSX.Element;
   FooterComponent: (props: { children: JSX.Element }) => JSX.Element;
 }
 
-interface AddFooterDocs {
+export interface AddFooterDocs {
   addFooterDocs: (footerDoc: string) => void;
 }
 
-interface WithDocsAsHOC {
+export interface WithDocsAsHOC {
   (custom: CustomComponents): (readme: Readme) => HOCPattern;
   (readme: Readme, story: RenderFunction): RenderFunction;
 }
 
-type WithDocsAsDecorator = (readme: Readme) => DecoratorPattern;
+export type WithDocsAsDecorator = (readme: Readme) => DecoratorPattern;
 
 export const withDocs: AddFooterDocs & WithDocsAsHOC & WithDocsAsDecorator;
 
 // Doc Types
-export const doc: (readme: string) => any;
+export function doc(readme: string): RenderFunction;
