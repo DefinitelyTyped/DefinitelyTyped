8a6d6b26d62d20dced3068dcf5927fc504d2fe6a
Leonard Thieu <leonard.thieu@gmail.com>
2018-10-10 11:15:09 -0400

    [jquery-toastmessage-plugin] Requires TypeScript 2.8 due to dependency on `@types/jquery`.

--- types/jquery-toastmessage-plugin/index.d.ts
+++ types/jquery-toastmessage-plugin/index.d.ts
@@ -2,7 +2,7 @@
 // Project: https://github.com/akquinet/jquery-toastmessage-plugin
 // Definitions by: Joe Skeen <https://github.com/joeskeen>
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
-// TypeScript Version: 2.3
+// TypeScript Version: 2.8
 
 /// <reference types="jquery" />
 
