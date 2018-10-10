1515d46289dbf345a08a28c252ea9d5f32d6a42b
Leonard Thieu <leonard.thieu@gmail.com>
2018-10-10 11:14:47 -0400

    [dropzone] Requires TypeScript 2.8 due to dependency on `@types/jquery`.

--- types/dropzone/index.d.ts
+++ types/dropzone/index.d.ts
@@ -8,7 +8,7 @@
 //                 Ted Bicknell <https://github.com/tedbcsgpro>
 //                 Daniel Waxweiler <https://github.com/dwaxweiler>
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
-// TypeScript Version: 2.3
+// TypeScript Version: 2.8
 
 /// <reference types="jquery"/>
 
