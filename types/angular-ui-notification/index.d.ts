78f33a964140a21eaf8500d5c00b6b722cb378df
Leonard Thieu <leonard.thieu@gmail.com>
2018-10-10 09:37:40 -0400

    [angular-ui-notification] Requires TypeScript 2.8 due to dependency on `@types/jquery`.

--- types/angular-ui-notification/index.d.ts
+++ types/angular-ui-notification/index.d.ts
@@ -2,7 +2,7 @@
 // Project: https://github.com/alexcrack/angular-ui-notification
 // Definitions by: Kamil Rojewski <https://github.com/krojew/DefinitelyTyped>
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
-// TypeScript Version: 2.3
+// TypeScript Version: 2.8
 
 /// <reference types="angular" />
 
