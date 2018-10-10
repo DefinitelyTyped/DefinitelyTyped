70174a30b0db9f1d01946f25afde156c5ca7a335
Leonard Thieu <leonard.thieu@gmail.com>
2018-10-10 09:41:35 -0400

    [angularlocalstorage] Requires TypeScript 2.8 due to dependency on `@types/jquery`.

--- types/angularlocalstorage/index.d.ts
+++ types/angularlocalstorage/index.d.ts
@@ -2,7 +2,7 @@
 // Project: https://github.com/agrublev/angularLocalStorage
 // Definitions by: Horiuchi_H <https://github.com/horiuchi>
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
-// TypeScript Version: 2.3
+// TypeScript Version: 2.8
 
 /// <reference types="angular"/>
 
