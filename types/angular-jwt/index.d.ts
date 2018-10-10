e179e007b0f2d3938953013642f835f4801ec68b
Leonard Thieu <leonard.thieu@gmail.com>
2018-10-10 09:13:53 -0400

    [angular-jwt] Requires TypeScript 2.8 due to dependency on `@types/jquery`.

--- types/angular-jwt/index.d.ts
+++ types/angular-jwt/index.d.ts
@@ -2,7 +2,7 @@
 // Project: https://github.com/auth0/angular-jwt
 // Definitions by: Reto Rezzonico <https://github.com/rerezz>
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
-// TypeScript Version: 2.3
+// TypeScript Version: 2.8
 
 /// <reference types="angular" />
 
