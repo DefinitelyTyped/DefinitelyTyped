d974635c9612ebc0792ebd96c10fbb214d4ba5f3
Leonard Thieu <leonard.thieu@gmail.com>
2018-10-10 09:53:26 -0400

    [bootstrap v3] Requires TypeScript 2.8 due to dependency on `@types/jquery`.

--- types/bootstrap/v3/index.d.ts
+++ types/bootstrap/v3/index.d.ts
@@ -3,7 +3,7 @@
 // Definitions by: Boris Yankov <https://github.com/borisyankov>
 //                 denisname <https://github.com/denisname>
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
-// TypeScript Version: 2.3
+// TypeScript Version: 2.8
 
 /// <reference types="jquery"/>
 
