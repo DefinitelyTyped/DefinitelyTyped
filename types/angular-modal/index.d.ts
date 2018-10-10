8785702e14f18363f18e9ab30fb162e25b0fffbc
Leonard Thieu <leonard.thieu@gmail.com>
2018-10-10 09:21:13 -0400

    [angular-modal] Requires TypeScript 2.8 due to dependency on `@types/jquery`.

--- types/angular-modal/index.d.ts
+++ types/angular-modal/index.d.ts
@@ -2,7 +2,7 @@
 // Project: https://github.com/btford/angular-modal
 // Definitions by: Paul Lessing <https://github.com/paullessing>
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
-// TypeScript Version: 2.3
+// TypeScript Version: 2.8
 
 /// <reference types="angular" />
 
