ab3733b2884997ec3353c2493507a089b12efb25
Leonard Thieu <leonard.thieu@gmail.com>
2018-10-10 08:56:48 -0400

    [angular-cookie] Requires TypeScript 2.8 due to dependency on `@types/jquery`.

--- types/angular-cookie/index.d.ts
+++ types/angular-cookie/index.d.ts
@@ -2,7 +2,7 @@
 // Project: https://github.com/ivpusic/angular-cookie
 // Definitions by: Borislav Zhivkov <https://github.com/borislavjivkov>
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
-// TypeScript Version: 2.3
+// TypeScript Version: 2.8
 
 /// <reference types="angular" />
 
