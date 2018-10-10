9b532c7cfd2f14208e3d26ecb7e37708913d881b
Leonard Thieu <leonard.thieu@gmail.com>
2018-10-10 11:15:57 -0400

    [space-pen] Requires TypeScript 2.8 due to dependency on `@types/jquery`.

--- types/space-pen/index.d.ts
+++ types/space-pen/index.d.ts
@@ -2,7 +2,7 @@
 // Project: https://github.com/atom/space-pen
 // Definitions by: vvakame <https://github.com/vvakame>
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
-// TypeScript Version: 2.3
+// TypeScript Version: 2.8
 
 /// <reference types="jquery" />
 
