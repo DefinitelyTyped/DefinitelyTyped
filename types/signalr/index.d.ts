12a54055551f5599c96298ab9714fd0b729933fa
Leonard Thieu <leonard.thieu@gmail.com>
2018-10-10 11:15:55 -0400

    [signalr] Requires TypeScript 2.8 due to dependency on `@types/jquery`.

--- types/signalr/index.d.ts
+++ types/signalr/index.d.ts
@@ -2,7 +2,7 @@
 // Project: http://www.asp.net/signalr
 // Definitions by: Boris Yankov <https://github.com/borisyankov>, T. Michael Keesey <https://github.com/keesey>, Giedrius Grabauskas <https://github.com/GiedriusGrabauskas>
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
-// TypeScript Version: 2.3
+// TypeScript Version: 2.8
 
 
 /// <reference types="jquery" />
