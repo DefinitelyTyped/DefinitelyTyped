a312e0a2c5fff5b55019d4bc3dd4e5c3fb9e6465
Leonard Thieu <leonard.thieu@gmail.com>
2018-10-10 11:22:30 -0400

    [qlik-visualizationextensions] Requires TypeScript 2.8 due to dependency on `@types/jquery`.

--- types/qlik-visualizationextensions/index.d.ts
+++ types/qlik-visualizationextensions/index.d.ts
@@ -2,7 +2,7 @@
 // Project: http://help.qlik.com/en-US/sense-developer/June2017/Subsystems/Extensions/Content/extensions-introduction.htm
 // Definitions by: Konrad Mattheis <https://github.com/konne>
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
-// TypeScript Version: 2.3
+// TypeScript Version: 2.8
 
 /// <reference types="angular" />
 
