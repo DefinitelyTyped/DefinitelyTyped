cb8eeb969916394bb7b4de4a4fec3161c1345db0
Leonard Thieu <leonard.thieu@gmail.com>
2018-10-10 11:14:50 -0400

    [ember v1] Requires TypeScript 2.8 due to dependency on `@types/jquery`.

--- types/ember/v1/index.d.ts
+++ types/ember/v1/index.d.ts
@@ -2,7 +2,7 @@
 // Project: http://emberjs.com/
 // Definitions by: Jed Mao <https://github.com/jedmao>
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
-// TypeScript Version: 2.3
+// TypeScript Version: 2.8
 
 /// <reference types="jquery"/>
 /// <reference types="handlebars"/>
