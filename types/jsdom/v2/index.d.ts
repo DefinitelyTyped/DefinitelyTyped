ea81d85bcfb3537e9e28277b15b4724695b1b724
Leonard Thieu <leonard.thieu@gmail.com>
2018-10-10 11:15:35 -0400

    [jsdom v2] Requires TypeScript 2.8 due to dependency on `@types/jquery`.

--- types/jsdom/v2/index.d.ts
+++ types/jsdom/v2/index.d.ts
@@ -2,7 +2,7 @@
 // Project: https://github.com/tmpvar/jsdom
 // Definitions by: Asana <https://asana.com>
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
-// TypeScript Version: 2.3
+// TypeScript Version: 2.8
 
 /// <reference types="node" />
 /// <reference types="jquery" />
