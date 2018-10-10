b6211c3a332b2c2c9e07d664a6a5a0f7efe46307
Leonard Thieu <leonard.thieu@gmail.com>
2018-10-10 09:24:41 -0400

    [angular-oauth2] Requires TypeScript 2.8 due to dependency on `@types/jquery`.

--- types/angular-oauth2/index.d.ts
+++ types/angular-oauth2/index.d.ts
@@ -2,7 +2,7 @@
 // Project: https://github.com/oauthjs/angular-oauth2
 // Definitions by: Antério Vieira <https://github.com/anteriovieira>
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
-// TypeScript Version: 2.3
+// TypeScript Version: 2.8
 
 import * as angular from 'angular';
 
