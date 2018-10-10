d521e555d8c6acc1f020ef7e092738cde9f314da
Leonard Thieu <leonard.thieu@gmail.com>
2018-10-10 09:51:18 -0400

    [bootbox] Requires TypeScript 2.8 due to dependency on `@types/jquery`.

--- types/bootbox/index.d.ts
+++ types/bootbox/index.d.ts
@@ -2,7 +2,7 @@
 // Project: https://github.com/makeusabrew/bootbox
 // Definitions by: Vincent Bortone <https://github.com/vbortone>, Kon Pik <https://github.com/konpikwastaken>, Anup Kattel <https://github.com/kanup>, Dominik Schroeter <https://github.com/icereed>, Troy McKinnon <https://github.com/trodi>, Stanny Nuytkens <https://github.com/stannynuytkens>, Soner Köksal <https://github.com/renjfk>
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
-// TypeScript Version: 2.3
+// TypeScript Version: 2.8
 
 /// <reference types="jquery" />
 
