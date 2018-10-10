a4dd05554100b7c35551a21badaa20bf10e80450
Leonard Thieu <leonard.thieu@gmail.com>
2018-10-10 11:29:51 -0400

    [gridstack] Requires TypeScript 2.8 due to dependency on `@types/jquery`.

--- types/gridstack/index.d.ts
+++ types/gridstack/index.d.ts
@@ -2,7 +2,7 @@
 // Project: http://troolee.github.io/gridstack.js/
 // Definitions by: Pascal Senn <https://github.com/PascalSenn>, Ricky Blankenaufulland <https://github.com/ZoolWay>, Sl1MBoy <https://github.com/Sl1MBoy>
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
-// TypeScript Version: 2.3
+// TypeScript Version: 2.8
 
 interface JQuery {
     gridstack(options: IGridstackOptions): JQuery;
