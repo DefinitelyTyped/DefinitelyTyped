581f097d5d979523ea361616185361827b0f4214
Leonard Thieu <leonard.thieu@gmail.com>
2018-10-10 09:30:27 -0400

    [angular-q-spread] Requires TypeScript 2.8 due to dependency on `@types/jquery`.

--- types/angular-q-spread/index.d.ts
+++ types/angular-q-spread/index.d.ts
@@ -2,7 +2,7 @@
 // Project: https://www.npmjs.com/package/angular-q-spread
 // Definitions by: rafw87 <https://github.com/rafw87>
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
-// TypeScript Version: 2.3
+// TypeScript Version: 2.8
 
 import * as a from "angular";
 
