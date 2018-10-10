d01cf37d9c67e795018842800d79c42ad7f3acb0
Leonard Thieu <leonard.thieu@gmail.com>
2018-10-10 11:15:01 -0400

    [jquery-cropbox] Requires TypeScript 2.8 due to dependency on `@types/jquery`.

--- types/jquery-cropbox/index.d.ts
+++ types/jquery-cropbox/index.d.ts
@@ -2,7 +2,7 @@
 // Project: https://github.com/acornejo/jquery-cropbox
 // Definitions by: Per Kastman <https://github.com/PerKastman>
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
-// TypeScript Version: 2.3
+// TypeScript Version: 2.8
 
 /// <reference types="jquery"/>
 
