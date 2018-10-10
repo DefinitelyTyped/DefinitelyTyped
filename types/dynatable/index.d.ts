17bd62b37cac5c5c7070afd29839f3e28fc6b607
Leonard Thieu <leonard.thieu@gmail.com>
2018-10-10 11:14:49 -0400

    [dynatable] Requires TypeScript 2.8 due to dependency on `@types/jquery`.

--- types/dynatable/index.d.ts
+++ types/dynatable/index.d.ts
@@ -2,7 +2,7 @@
 // Project: http://www.dynatable.com/
 // Definitions by: François Massart <https://github.com/francoismassart>
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
-// TypeScript Version: 2.3
+// TypeScript Version: 2.8
 
 /// <reference types="jquery" />
 
