23e1039c7b6a07777c3c71cadaef403627d110d2
Leonard Thieu <leonard.thieu@gmail.com>
2018-10-10 11:15:18 -0400

    [jquery.form] Requires TypeScript 2.8 due to dependency on `@types/jquery`.

--- types/jquery.form/index.d.ts
+++ types/jquery.form/index.d.ts
@@ -2,7 +2,7 @@
 // Project: http://malsup.com/jquery/form/
 // Definitions by: François Guillot <http://fguillot.developpez.com/>
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
-// TypeScript Version: 2.3
+// TypeScript Version: 2.8
 
 
 /// <reference types="jquery"/>
