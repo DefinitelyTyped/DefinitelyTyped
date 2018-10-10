73027e56c86168a4e89f1b41fbeff793373c3fa8
Leonard Thieu <leonard.thieu@gmail.com>
2018-10-10 11:51:35 -0400

    [gulp v3] Requires TypeScript 2.8 due to dependency on `@types/jquery`.

--- types/gulp/v3/index.d.ts
+++ types/gulp/v3/index.d.ts
@@ -2,7 +2,7 @@
 // Project: http://gulpjs.com
 // Definitions by: Drew Noakes <https://drewnoakes.com>
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
-// TypeScript Version: 2.3
+// TypeScript Version: 2.8
 
 /// <reference types="node" />
 /// <reference types="orchestrator" />
