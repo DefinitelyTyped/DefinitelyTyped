aefd4d3676caba3893504ec242ad1b24946496b9
Leonard Thieu <leonard.thieu@gmail.com>
2018-10-10 11:15:47 -0400

    [rx-jquery] Requires TypeScript 2.8 due to dependency on `@types/jquery`.

--- types/rx-jquery/index.d.ts
+++ types/rx-jquery/index.d.ts
@@ -2,7 +2,7 @@
 // Project: https://github.com/Reactive-Extensions/RxJS-jQuery/
 // Definitions by: Igor Oleinikov <https://github.com/Igorbek>
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
-// TypeScript Version: 2.3
+// TypeScript Version: 2.8
 
 ///<reference types="jquery"/>
 ///<reference types="rx"/>
