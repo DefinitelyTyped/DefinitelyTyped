3c8d2b45911fa0b7e03e58b4200f2f204ae73832
Leonard Thieu <leonard.thieu@gmail.com>
2018-10-10 11:14:52 -0400

    [featherlight] Requires TypeScript 2.8 due to dependency on `@types/jquery`.

--- types/featherlight/index.d.ts
+++ types/featherlight/index.d.ts
@@ -2,7 +2,7 @@
 // Project: https://noelboss.github.io/featherlight/
 // Definitions by: Kaur Kuut <https://github.com/xStrom>
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
-// TypeScript Version: 2.3
+// TypeScript Version: 2.8
 
 ///<reference types="jquery" />
 
