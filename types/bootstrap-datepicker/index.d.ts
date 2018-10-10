3faf5fcf577b86ce5c2c28687fca7433d761a8fe
Leonard Thieu <leonard.thieu@gmail.com>
2018-10-10 11:14:36 -0400

    [bootstrap-datepicker] Requires TypeScript 2.8 due to dependency on `@types/jquery`.

--- types/bootstrap-datepicker/index.d.ts
+++ types/bootstrap-datepicker/index.d.ts
@@ -2,7 +2,7 @@
 // Project: https://github.com/eternicode/bootstrap-datepicker
 // Definitions by: Boris Yankov <https://github.com/borisyankov>
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
-// TypeScript Version: 2.3
+// TypeScript Version: 2.8
 
 /// <reference types="jquery"/>
 
