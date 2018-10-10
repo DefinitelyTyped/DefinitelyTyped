44277c96397ac9a0de649d85c3ed5749bee5b613
Leonard Thieu <leonard.thieu@gmail.com>
2018-10-10 11:15:03 -0400

    [jquery-fullscreen] Requires TypeScript 2.8 due to dependency on `@types/jquery`.

--- types/jquery-fullscreen/index.d.ts
+++ types/jquery-fullscreen/index.d.ts
@@ -2,7 +2,7 @@
 // Project: https://github.com/kayahr/jquery-fullscreen-plugin
 // Definitions by: Bruno Grieder <https://github.com/bgrieder>
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
-// TypeScript Version: 2.3
+// TypeScript Version: 2.8
 
 /// <reference types="jquery"/>
 
