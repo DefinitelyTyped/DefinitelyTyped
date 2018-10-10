ff04cede7d1a18d6d4e842ebeb687cc2f6849796
Leonard Thieu <leonard.thieu@gmail.com>
2018-10-10 11:14:40 -0400

    [bootstrap-validator] Requires TypeScript 2.8 due to dependency on `@types/jquery`.

--- types/bootstrap-validator/index.d.ts
+++ types/bootstrap-validator/index.d.ts
@@ -2,7 +2,7 @@
 // Project: https://github.com/1000hz/bootstrap-validator
 // Definitions by: Brady Liles <https://github.com/BradyLiles>
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
-// TypeScript Version: 2.3
+// TypeScript Version: 2.8
 
 /// <reference types="jquery"/>
 
