9b239c829800f011cd29d4133707ab258ef8f8d5
Aaron Reisman <aaron@lifeiscontent.net>
2018-10-10 18:00:17 -0700

    Update types

--- types/jest-cli/index.d.ts
+++ types/jest-cli/index.d.ts
@@ -1,15 +1,159 @@
-export interface SnapshotSummary {}
-export interface TestResult {}
-export interface ReporterConfig {}
-export interface FileCoverage {}
-export interface SnapshotUpdateState {}
-export interface RawFileCoverage {}
-export interface RawCoverage {}
-export type Argv = string[];
+// TypeScript Version: 2.2
 
-export type Path = string;
+export interface UncheckedSnapshot {
+    filePath: string;
+    keys: string[];
+}
 
-export type Glob = string;
+export interface SnapshotSummary {
+    added: number;
+    didUpdate: boolean;
+    failure: boolean;
+    filesAdded: number;
+    filesRemoved: number;
+    filesUnmatched: number;
+    filesUpdated: number;
+    matched: number;
+    total: number;
+    unchecked: number;
+    uncheckedKeysByFile: UncheckedSnapshot[];
+    unmatched: number;
+    updated: number;
+}
+export type LogMessage = string;
+
+export interface LogEntry {
+    message: LogMessage;
+    origin: string;
+    type: LogType;
+}
+
+export interface LogCounters {
+    [label: string]: number;
+}
+
+export interface LogTimers {
+    [label: string]: Date;
+}
+
+export type LogType =
+    | "assert"
+    | "count"
+    | "debug"
+    | "dir"
+    | "dirxml"
+    | "error"
+    | "group"
+    | "groupCollapsed"
+    | "info"
+    | "log"
+    | "time"
+    | "warn";
+
+export type ConsoleBuffer = LogEntry[];
+
+export interface Callsite {
+    column: number;
+    line: number;
+}
+
+export type Status = "passed" | "failed" | "skipped" | "pending";
+
+export interface AssertionResult {
+    ancestorTitles: string[];
+    duration?: number;
+    failureMessages: string[];
+    fullName: string;
+    location?: Callsite;
+    numPassingAsserts: number;
+    status: Status;
+    title: string;
+}
+
+export interface SerializableError {
+    code?: any;
+    message: string;
+    stack?: string;
+    type?: string;
+}
+
+export interface TestResult {
+    console?: ConsoleBuffer;
+    coverage?: RawCoverage;
+    displayName?: string;
+    failureMessage?: string;
+    leaks: boolean;
+    memoryUsage?: number;
+    numFailingTests: number;
+    numPassingTests: number;
+    numPendingTests: number;
+    openHandles: Error[];
+    perfStats: {
+        end: number;
+        start: number;
+    };
+    skipped: boolean;
+    snapshot: {
+        added: number;
+        fileDeleted: boolean;
+        matched: number;
+        unchecked: number;
+        uncheckedKeys: string[];
+        unmatched: number;
+        updated: number;
+    };
+    sourceMaps: { [sourcePath: string]: string };
+    testExecError?: SerializableError;
+    testFilePath: string;
+    testResults: AssertionResult[];
+}
+
+export type ReporterConfig = [string, object];
+
+export interface FileCoverageTotal {
+    total: number;
+    covered: number;
+    skipped: number;
+    pct: number;
+}
+
+export interface CoverageSummary {
+    lines: FileCoverageTotal;
+    statements: FileCoverageTotal;
+    branches: FileCoverageTotal;
+    functions: FileCoverageTotal;
+    merge: (other: CoverageSummary) => void;
+}
+
+export interface FileCoverage {
+    getLineCoverage: () => object;
+    getUncoveredLines: () => number[];
+    getBranchCoverageByLine: () => object;
+    toJSON: () => object;
+    merge: (other: object) => void;
+    computeSimpleTotals: (property: string) => FileCoverageTotal;
+    computeBranchTotals: () => FileCoverageTotal;
+    resetHits: () => void;
+    toSummary: () => CoverageSummary;
+}
+
+export type SnapshotUpdateState = "all" | "new" | "none";
+
+export interface RawFileCoverage {
+    path: string;
+    s: { [statementId: number]: number };
+    b: { [branchId: number]: number };
+    f: { [functionId: number]: number };
+    l: { [lineId: number]: number };
+    fnMap: { [functionId: number]: any };
+    statementMap: { [statementId: number]: any };
+    branchMap: { [branchId: number]: any };
+    inputSourceMap?: object;
+}
+
+export interface RawCoverage {
+    [filePath: string]: RawFileCoverage;
+}
 
 export interface AggregatedResultWithoutCoverage {
     numFailedTests: number;
@@ -48,7 +192,7 @@ export interface GlobalConfig {
     changedSince: string;
     changedFilesWithAncestor: boolean;
     collectCoverage: boolean;
-    collectCoverageFrom: Glob[];
+    collectCoverageFrom: string[];
     collectCoverageOnlyFrom?: { [key: string]: boolean };
     coverageDirectory: string;
     coveragePathIgnorePatterns?: string[];
@@ -58,7 +202,7 @@ export interface GlobalConfig {
     detectOpenHandles: boolean;
     enabledTestsMap?: { [key: string]: { [key: string]: boolean } };
     expand: boolean;
-    filter?: Path;
+    filter?: string;
     findRelatedTests: boolean;
     forceExit: boolean;
     json: boolean;
@@ -73,15 +217,15 @@ export interface GlobalConfig {
     noSCM?: boolean;
     notify: boolean;
     notifyMode: string;
-    outputFile?: Path;
+    outputFile?: string;
     onlyChanged: boolean;
     onlyFailures: boolean;
     passWithNoTests: boolean;
-    projects: Glob[];
+    projects: string[];
     replname?: string;
     reporters: Array<string | ReporterConfig>;
     runTestsByPath: boolean;
-    rootDir: Path;
+    rootDir: string;
     silent: boolean;
     skipFilter: boolean;
     errorOnDeprecated: boolean;
@@ -98,8 +242,9 @@ export interface GlobalConfig {
     watchPlugins?: Array<{ path: string; config: { [index: string]: any } }>;
 }
 
-export function run(maybeArgv?: Argv, project?: Path): Promise<void>;
+export function run(maybeArgv?: string[], project?: string): Promise<void>;
+
 export function runCLI(
-    argv: Argv,
-    projects: Path[]
+    argv: string[],
+    projects: string[]
 ): Promise<{ results: AggregatedResult; globalConfig: GlobalConfig }>;
