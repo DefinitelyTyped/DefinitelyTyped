753dac6d555112d294caf27c931b1e8cbb79045c
Yash Kulshrestha <yash.kulshrestha@gmail.com>
2018-10-10 22:10:05 -0700

    added types for is-docker

---
+++ types/is-docker/index.d.ts
@@ -0,0 +1,6 @@
+// Type definitions for is-docker 1.1
+// Project: https://github.com/sindresorhus/is-docker#readme
+// Definitions by: Yash Kulshrestha <https://github.com/YashdalfTheGray>
+// Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
+
+export default function isDocker(): boolean;
