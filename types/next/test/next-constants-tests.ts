dab682e6f807cc326e13494c34805fa461b4742e
Resi Respati <resir014@gmail.com>
2018-10-12 00:47:24 +0700

    [next] Add support for `next/constants` (#29633)
    * [next] Add support for `next/constants`
    
    * minor whitespace fix

---
+++ types/next/test/next-constants-tests.ts
@@ -0,0 +1,30 @@
+import {
+    PHASE_DEVELOPMENT_SERVER,
+    IS_BUNDLED_PAGE_REGEX
+} from "next/constants";
+
+const isIndexPage = IS_BUNDLED_PAGE_REGEX.test(
+    "static/CjW0mFnyG80HdP4eSUiy7/pages/index.js"
+);
+
+// Example taken from: https://github.com/cyrilwanner/next-compose-plugins/blob/a25b313899638912cc9defc0be072f4fe4a1e855/README.md
+const config = (nextConfig: any = {}) => {
+    return {
+        ...nextConfig,
+
+        // define in which phases this plugin should get applied.
+        // you can also use multiple phases or negate them.
+        // however, users can still overwrite them in their configuration if they really want to.
+        phases: [PHASE_DEVELOPMENT_SERVER],
+
+        webpack(config: any, options: any) {
+            // do something here which only gets applied during development server phase
+
+            if (typeof nextConfig.webpack === "function") {
+                return nextConfig.webpack(config, options);
+            }
+
+            return config;
+        }
+    };
+};
