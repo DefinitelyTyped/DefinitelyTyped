942332c77446c66a9a14f72c6b54ebff29c237c4
zhangluyao01 <zhangluyao01@baidu.com>
2018-10-11 10:23:02 +0800

    Fix test errors

--- types/react-draft-wysiwyg/index.d.ts
+++ types/react-draft-wysiwyg/index.d.ts
@@ -4,75 +4,73 @@
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
 // TypeScript Version: 2.8
 
-declare module 'react-draft-wysiwyg' {
-    import * as React from 'react';
-    import * as Draft from 'draft-js';
+import * as React from 'react';
+import * as Draft from 'draft-js';
 
-    type SyntheticKeyboardEvent = React.KeyboardEvent<{}>;
-    type SyntheticEvent = React.SyntheticEvent<{}>;
-    type RawDraftContentState = Draft.RawDraftContentState;
+type SyntheticKeyboardEvent = React.KeyboardEvent<{}>;
+type SyntheticEvent = React.SyntheticEvent<{}>;
+type RawDraftContentState = Draft.RawDraftContentState;
 
-    class EditorState extends Draft.EditorState {}
-    class ContentState extends Draft.ContentState {}
-    class ContentBlock extends Draft.ContentBlock {}
-    class SelectionState extends Draft.SelectionState {}
+class EditorState extends Draft.EditorState {}
+class ContentState extends Draft.ContentState {}
+class ContentBlock extends Draft.ContentBlock {}
+class SelectionState extends Draft.SelectionState {}
 
-    interface EditorProps {
-        onChange?(contentState: ContentState): RawDraftContentState;
-        onEditorStateChange?(editorState: EditorState): void;
-        onContentStateChange?(contentState: ContentState): RawDraftContentState;
-        initialContentState?: RawDraftContentState;
-        defaultContentState?: RawDraftContentState;
-        contentState?: RawDraftContentState;
-        editorState?: EditorState;
-        defaultEditorState?: EditorState;
-        toolbarOnFocus?: boolean;
-        spellCheck?: boolean;
-        stripPastedStyles?: boolean;
-        toolbar?: object;
-        toolbarCustomButtons?: Array<React.ReactElement<HTMLElement>>;
-        toolbarClassName?: string;
-        toolbarHidden?: boolean;
-        locale?: string;
-        localization?: object;
-        editorClassName?: string;
-        wrapperClassName?: string;
-        toolbarStyle?: object;
-        editorStyle?: React.CSSProperties;
-        wrapperStyle?: React.CSSProperties;
-        uploadCallback?(file: object): Promise<object>;
-        onFocus?(event: SyntheticEvent): void;
-        onBlur?(event: SyntheticEvent): void;
-        onTab?(event: SyntheticKeyboardEvent): void;
-        mention?: object;
-        hashtag?: object;
-        textAlignment?: string;
-        tabIndex?: number;
-        ariaLabel?: string;
-        ariaOwneeID?: string;
-        ariaActiveDescendantID?: string;
-        ariaAutoComplete?: string;
-        ariaDescribedBy?: string;
-        ariaExpanded?: string;
-        ariaHasPopup?: string;
-        customBlockRenderFunc?(block: ContentBlock): any;
-        wrapperId?: number;
-        customDecorators?: object[];
-        editorRef?(ref: object): void;
-        handlePastedText?(text: string, html: string, editorState: EditorState, onChange: (editorState: EditorState) => void): boolean;
-    }
-
-    class Editor extends React.Component<EditorProps, {}> {
-        focusEditor():void;
-    }
-
-    export {
-        Editor,
-        EditorProps,
-        EditorState,
-        ContentState,
-        ContentBlock,
-        RawDraftContentState,
-        SelectionState
-    };
+interface EditorProps {
+    onChange?(contentState: ContentState): RawDraftContentState;
+    onEditorStateChange?(editorState: EditorState): void;
+    onContentStateChange?(contentState: ContentState): RawDraftContentState;
+    initialContentState?: RawDraftContentState;
+    defaultContentState?: RawDraftContentState;
+    contentState?: RawDraftContentState;
+    editorState?: EditorState;
+    defaultEditorState?: EditorState;
+    toolbarOnFocus?: boolean;
+    spellCheck?: boolean;
+    stripPastedStyles?: boolean;
+    toolbar?: object;
+    toolbarCustomButtons?: Array<React.ReactElement<HTMLElement>>;
+    toolbarClassName?: string;
+    toolbarHidden?: boolean;
+    locale?: string;
+    localization?: object;
+    editorClassName?: string;
+    wrapperClassName?: string;
+    toolbarStyle?: object;
+    editorStyle?: React.CSSProperties;
+    wrapperStyle?: React.CSSProperties;
+    uploadCallback?(file: object): Promise<object>;
+    onFocus?(event: SyntheticEvent): void;
+    onBlur?(event: SyntheticEvent): void;
+    onTab?(event: SyntheticKeyboardEvent): void;
+    mention?: object;
+    hashtag?: object;
+    textAlignment?: string;
+    tabIndex?: number;
+    ariaLabel?: string;
+    ariaOwneeID?: string;
+    ariaActiveDescendantID?: string;
+    ariaAutoComplete?: string;
+    ariaDescribedBy?: string;
+    ariaExpanded?: string;
+    ariaHasPopup?: string;
+    customBlockRenderFunc?(block: ContentBlock): any;
+    wrapperId?: number;
+    customDecorators?: object[];
+    editorRef?(ref: object): void;
+    handlePastedText?(text: string, html: string, editorState: EditorState, onChange: (editorState: EditorState) => void): boolean;
 }
+
+class Editor extends React.Component<EditorProps, {}> {
+    focusEditor(): void;
+}
+
+export {
+    Editor,
+    EditorProps,
+    EditorState,
+    ContentState,
+    ContentBlock,
+    RawDraftContentState,
+    SelectionState
+};
