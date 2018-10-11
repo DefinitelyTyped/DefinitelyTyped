5f073e6da011aba48dbb74051d8059e37c273741
Aaron Reisman <aaron@lifeiscontent.net>
2018-10-10 18:07:43 -0700

    Lint

--- types/jest-cli/index.d.ts
+++ types/jest-cli/index.d.ts
@@ -1,3 +1,7 @@
+// Type definitions for jest-cli 23.6
+// Project: https://jestjs.io/
+// Definitions by: Aaron Reisman <https://github.com/lifeiscontent>
+// Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
 // TypeScript Version: 2.2
 
 export interface UncheckedSnapshot {
