8467dc497589391158d94828ed5948312f9209c1
zhangluyao01 <zhangluyao01@baidu.com>
2018-10-11 11:10:59 +0800

    Add declare

--- types/react-draft-wysiwyg/index.d.ts
+++ types/react-draft-wysiwyg/index.d.ts
@@ -7,63 +7,75 @@
 import * as React from 'react';
 import * as Draft from 'draft-js';
 
-type SyntheticKeyboardEvent = React.KeyboardEvent<{}>;
-type SyntheticEvent = React.SyntheticEvent<{}>;
-type RawDraftContentState = Draft.RawDraftContentState;
+export as namespace Wysiwyg;
 
-class EditorState extends Draft.EditorState {}
-class ContentState extends Draft.ContentState {}
-class ContentBlock extends Draft.ContentBlock {}
-class SelectionState extends Draft.SelectionState {}
+declare namespace Wysiwyg {
+    type SyntheticKeyboardEvent = React.KeyboardEvent<{}>;
+    type SyntheticEvent = React.SyntheticEvent<{}>;
+    type RawDraftContentState = Draft.RawDraftContentState;
 
-interface EditorProps {
-    onChange?(contentState: ContentState): RawDraftContentState;
-    onEditorStateChange?(editorState: EditorState): void;
-    onContentStateChange?(contentState: ContentState): RawDraftContentState;
-    initialContentState?: RawDraftContentState;
-    defaultContentState?: RawDraftContentState;
-    contentState?: RawDraftContentState;
-    editorState?: EditorState;
-    defaultEditorState?: EditorState;
-    toolbarOnFocus?: boolean;
-    spellCheck?: boolean;
-    stripPastedStyles?: boolean;
-    toolbar?: object;
-    toolbarCustomButtons?: Array<React.ReactElement<HTMLElement>>;
-    toolbarClassName?: string;
-    toolbarHidden?: boolean;
-    locale?: string;
-    localization?: object;
-    editorClassName?: string;
-    wrapperClassName?: string;
-    toolbarStyle?: object;
-    editorStyle?: React.CSSProperties;
-    wrapperStyle?: React.CSSProperties;
-    uploadCallback?(file: object): Promise<object>;
-    onFocus?(event: SyntheticEvent): void;
-    onBlur?(event: SyntheticEvent): void;
-    onTab?(event: SyntheticKeyboardEvent): void;
-    mention?: object;
-    hashtag?: object;
-    textAlignment?: string;
-    tabIndex?: number;
-    ariaLabel?: string;
-    ariaOwneeID?: string;
-    ariaActiveDescendantID?: string;
-    ariaAutoComplete?: string;
-    ariaDescribedBy?: string;
-    ariaExpanded?: string;
-    ariaHasPopup?: string;
-    customBlockRenderFunc?(block: ContentBlock): any;
-    wrapperId?: number;
-    customDecorators?: object[];
-    editorRef?(ref: object): void;
-    handlePastedText?(text: string, html: string, editorState: EditorState, onChange: (editorState: EditorState) => void): boolean;
+    class EditorState extends Draft.EditorState {}
+    class ContentState extends Draft.ContentState {}
+    class ContentBlock extends Draft.ContentBlock {}
+    class SelectionState extends Draft.SelectionState {}
+
+    interface EditorProps {
+        onChange?(contentState: ContentState): RawDraftContentState;
+        onEditorStateChange?(editorState: EditorState): void;
+        onContentStateChange?(contentState: ContentState): RawDraftContentState;
+        initialContentState?: RawDraftContentState;
+        defaultContentState?: RawDraftContentState;
+        contentState?: RawDraftContentState;
+        editorState?: EditorState;
+        defaultEditorState?: EditorState;
+        toolbarOnFocus?: boolean;
+        spellCheck?: boolean;
+        stripPastedStyles?: boolean;
+        toolbar?: object;
+        toolbarCustomButtons?: Array<React.ReactElement<HTMLElement>>;
+        toolbarClassName?: string;
+        toolbarHidden?: boolean;
+        locale?: string;
+        localization?: object;
+        editorClassName?: string;
+        wrapperClassName?: string;
+        toolbarStyle?: object;
+        editorStyle?: React.CSSProperties;
+        wrapperStyle?: React.CSSProperties;
+        uploadCallback?(file: object): Promise<object>;
+        onFocus?(event: SyntheticEvent): void;
+        onBlur?(event: SyntheticEvent): void;
+        onTab?(event: SyntheticKeyboardEvent): void;
+        mention?: object;
+        hashtag?: object;
+        textAlignment?: string;
+        tabIndex?: number;
+        ariaLabel?: string;
+        ariaOwneeID?: string;
+        ariaActiveDescendantID?: string;
+        ariaAutoComplete?: string;
+        ariaDescribedBy?: string;
+        ariaExpanded?: string;
+        ariaHasPopup?: string;
+        customBlockRenderFunc?(block: ContentBlock): any;
+        wrapperId?: number;
+        customDecorators?: object[];
+        editorRef?(ref: object): void;
+        handlePastedText?(text: string, html: string, editorState: EditorState, onChange: (editorState: EditorState) => void): boolean;
+    }
+
+    class Editor extends React.Component<EditorProps, {}> {
+        focusEditor(): void;
+    }
 }
 
-class Editor extends React.Component<EditorProps, {}> {
-    focusEditor(): void;
-}
+import Editor = Wysiwyg.Editor;
+import EditorProps = Wysiwyg.EditorProps;
+import EditorState = Wysiwyg.EditorState;
+import ContentState = Wysiwyg.EditorState;
+import ContentBlock = Wysiwyg.ContentBlock;
+import RawDraftContentState = Wysiwyg.RawDraftContentState;
+import SelectionState = Wysiwyg.SelectionState;
 
 export {
     Editor,
