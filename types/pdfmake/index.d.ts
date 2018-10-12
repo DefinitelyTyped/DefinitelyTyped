2e84dad3b60000168ca92a1e5b9c15aeda6d064f
Enzo Volkmann <enzo@exportarts.io>
2018-10-12 18:14:14 +0200

    Added more types

--- types/pdfmake/index.d.ts
+++ types/pdfmake/index.d.ts
@@ -16,14 +16,6 @@ declare module 'pdfmake/build/pdfmake' {
     let fonts: { [name: string]: TFontFamilyTypes };
     function createPdf(documentDefinitions: TDocumentDefinitions): TCreatedPdf;
 
-    type pageSizeType =
-        '4A0' | '2A0' | 'A0' | 'A1' | 'A2' | 'A3' | 'A4' | 'A5' | 'A6' | 'A7' | 'A8' | 'A9' | 'A10' |
-        'B0' | 'B1' | 'B2' | 'B3' | 'B4' | 'B5' | 'B6' | 'B7' | 'B8' | 'B9' | 'B10' |
-        'C0' | 'C1' | 'C2' | 'C3' | 'C4' | 'C5' | 'C6' | 'C7' | 'C8' | 'C9' | 'C10' |
-        'RA0' | 'RA1' | 'RA2' | 'RA3' | 'RA4' |
-        'SRA0' | 'SRA1' | 'SRA2' | 'SRA3' | 'SRA4' |
-        'EXECUTIVE' | 'FOLIO' | 'LEGAL' | 'LETTER' | 'TABLOID';
-
     enum PageSize {
         A0_x_4 = '4A0',
         A0_x_2 = '2A0',
@@ -48,10 +40,37 @@ declare module 'pdfmake/build/pdfmake' {
         B7 = 'B7',
         B8 = 'B8',
         B9 = 'B9',
-        B1O = 'B10'
+        B1O = 'B10',
+        CO = 'C0',
+        C1 = 'C1',
+        C2 = 'C2',
+        C3 = 'C3',
+        C4 = 'C4',
+        C5 = 'C5',
+        C6 = 'C6',
+        C7 = 'C7',
+        C8 = 'C8',
+        C9 = 'C9',
+        C1O = 'C10',
+        RA1 = 'RA1',
+        RA2 = 'RA2',
+        RA3 = 'RA3',
+        RA4 = 'RA4',
+        SRA1 = 'SRA1',
+        SRA2 = 'SRA2',
+        SRA3 = 'SRA3',
+        SRA4 = 'SRA4',
+        EXECUTIVE = 'EXECUTIVE',
+        FOLIO = 'FOLIO',
+        LEGAL = 'LEGAL',
+        LETTER = 'LETTER',
+        TABLOID = 'TABLOID'
     }
 
-    type pageOrientationType = "portrait" | "landscape";
+    enum PageOrientation {
+        PORTRAIT = 'PORTRAIT',
+        LANDSCAPE = 'LANDSCAPE'
+    }
 
     let pdfMake: pdfMakeStatic;
 
@@ -104,18 +123,64 @@ declare module 'pdfmake/build/pdfmake' {
 		leadingIndent?: any;
     }
 
+    type TableRowFunction = (row: number) => number;
+
+    type TableLayoutFunctions = {
+        hLineWidth?: (i: number, node: any) => number;
+        vLineWidth?: (i: number, node: any) => number;
+        hLineColor?: (i: number, node: any) => string;
+        vLineColor?: (i: number, node: any) => string;
+        fillColor?: (i: number, node: any) => string;
+        paddingLeft?: (i: number, node: any) => number;
+        paddingRight?: (i: number, node: any) => number;
+        paddingTop?: (i: number, node: any) => number;
+        paddingBottom?: (i: number, node: any) => number;
+    };
+
+    interface TableCell {
+        text: string;
+        rowSpan?: number;
+        colSpan?: number;
+        fillColor?: string;
+        border?: [boolean, boolean, boolean, boolean]
+    }
+
+    interface Table {
+        widths?: (string | number)[];
+        heights?: (string | number)[] | TableRowFunction;
+        headerRows?: number;
+        body: Content[][] | TableCell[][];
+        layout?: string | TableLayoutFunctions;
+    }
+
+    interface Content {
+        style?: 'string';
+        margin?: Margins;
+        text?: string | string[] | Content[];
+        columns?: Content[];
+        stack?: Content[];
+        image?: string;
+        width?: string | number;
+        height?: string | number;
+        fit?: [number, number]
+        pageBreak?: 'before' | 'after';
+        alignment?: Alignment;
+        table?: Table;
+        ul?: Content[];
+        ol?: Content[];
+        [additionalProperty: string]: any;
+    }
+
     interface TDocumentDefinitions {
         info?: TDocumentInformation;
         header?: TDocumentHeaderFooterFunction;
         footer?: TDocumentHeaderFooterFunction;
-        content: any;
+        content: string | Content;
         styles?: Styles;
         pageSize?: PageSize;
-        pageOrientation?: pageOrientationType;
+        pageOrientation?: PageOrientation;
         pageMargins?: Margins;
-        defaultStyle?: {
-            font?: string;
-        };
+        defaultStyle?: Styles;
     }
 
     type CreatedPdfParams = (
