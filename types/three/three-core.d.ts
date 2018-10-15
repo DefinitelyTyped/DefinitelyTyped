3e3b7cf82eaf74402fbf5db2f818f7fc824072ad
Nathan Bierema <nbierema@gmail.com>
2018-10-15 09:30:35 -0700

    Update three-core.d.ts (#29695)

--- types/three/three-core.d.ts
+++ types/three/three-core.d.ts
@@ -1857,7 +1857,7 @@ export interface Intersection {
     face?: Face3 | null;
     faceIndex?: number;
     object: Object3D;
-    uv: Vector2;
+    uv?: Vector2;
 }
 
 export interface RaycasterParameters {
