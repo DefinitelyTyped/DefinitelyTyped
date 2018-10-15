2c516109bbb678bcb65b1464fadd54f189205eae
Prescott, Ste <Ste.Prescott@sky.uk>
2018-10-15 15:13:56 +0100

    Expose properties of HttpProxyAgent

--- types/http-proxy-agent/http-proxy-agent-tests.ts
+++ types/http-proxy-agent/http-proxy-agent-tests.ts
@@ -5,6 +5,11 @@ new Agent('url');
 new Agent({});
 new Agent({ host: 'url' });
 
+new Agent({}).options;
+new Agent({}).proxy;
+new Agent({}).timeout;
+new Agent({}).secureProxy;
+
 // $ExpectError
 new Agent();
 
