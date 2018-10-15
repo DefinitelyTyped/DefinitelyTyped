2473a0e19fd9d3258363a428fcb09405e041569f
Jimmy Headdon <jimmy.headdon@mclaren.com>
2018-10-15 14:50:50 +0100

    Bumped version number and declared constructor

--- types/enzyme-adapter-react-16/index.d.ts
+++ types/enzyme-adapter-react-16/index.d.ts
@@ -1,12 +1,14 @@
-// Type definitions for enzyme-adapter-react-16 1.0
+// Type definitions for enzyme-adapter-react-16 1.1
 // Project: https://github.com/airbnb/enzyme
 // Definitions by: Tanguy Krotoff <https://github.com/tkrotoff>
+//                 Jimmy Headdon <https://github.com/jimmyheaddon>
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
 // TypeScript Version: 2.8
 
 import { EnzymeAdapter } from 'enzyme';
 
 declare class ReactSixteenAdapter extends EnzymeAdapter {
+    constructor();
 }
 
 declare const Adapter: typeof ReactSixteenAdapter;
