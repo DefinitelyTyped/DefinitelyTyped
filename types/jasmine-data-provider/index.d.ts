2a1da8c87459a19682d9b5a59ceab62594adf15e
Tomek Łaziuk <tlaziuk@gmail.com>
2018-10-16 09:14:52 +0200

    Revert "change required ts version"
    This reverts commit 6ec355299606fb5b19b45126d8d4c0090f139ab0.

--- types/jasmine-data-provider/index.d.ts
+++ types/jasmine-data-provider/index.d.ts
@@ -2,7 +2,7 @@
 // Project: https://github.com/MortalFlesh/jasmine-data-provider
 // Definitions by: Tomek Łaziuk <https://github.com/tlaziuk>
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
-// TypeScript Version: 2.1
+// TypeScript Version: 2.3
 
 declare function using<T>(values: using.ValueType<T[]>, func: (data: T) => any): void;
 declare function using<T, K extends keyof T>(values: using.ValueType<T>, func: (data: T[K], description: K) => any): void;
