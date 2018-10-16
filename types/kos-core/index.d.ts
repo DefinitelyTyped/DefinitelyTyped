a3967cdecfaa72e4052db45735e8f808b49b7f45
Li Zhen <leejaen@gmail.com>
2018-10-16 17:33:39 +0800

    Fixed wrong dt-header

--- types/kos-core/index.d.ts
+++ types/kos-core/index.d.ts
@@ -1,4 +1,4 @@
-// Type definitions for kos-core
+// Type definitions for kos-core 0.3.2
 // Project: https://github.com/ali-Kos/Kos
 // Definitions by: alibaba ali-Kos <https://github.com/ali-Kos>
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
