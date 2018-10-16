d176ea976a428896e8eb3198010c6c02cbcffa83
Tomek Łaziuk <tlaziuk@gmail.com>
2018-10-16 19:49:07 +0200

    apply requested changes

--- types/jasmine-data-provider/index.d.ts
+++ types/jasmine-data-provider/index.d.ts
@@ -4,8 +4,8 @@
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
 // TypeScript Version: 2.3
 
-declare function using<T>(values: using.ValueType<T[]>, func: (data: T) => any): void;
-declare function using<T, K extends keyof T>(values: using.ValueType<T>, func: (data: T[K], description: K) => any): void;
+declare function using<T>(values: using.ValueType<T[]>, func: (data: T) => void): void;
+declare function using<T, K extends keyof T>(values: using.ValueType<T>, func: (data: T[K], description: K) => void): void;
 
 declare namespace using {
     type ValueType<T> = T | (() => T);
