5a3b79720586d57280bf1c29ed89790bf62aa987
Daniel Brodin <daniel@itiden.se>
2018-10-16 09:07:12 +0200

    Add additional props

--- types/react-countup/index.d.ts
+++ types/react-countup/index.d.ts
@@ -25,19 +25,39 @@ declare namespace ReactCountUp {
         decimals?: number;
         delay?: number;
         duration?: number;
-        easingFn?(): void;
         end?: number;
-        formattingFn?(): void;
-        onComplete?(): void;
-        onEnd?(): void;
-        onStart?(): void;
         prefix?: string;
         redraw?: boolean;
         separator?: string;
         start?: number;
-        style?: React.CSSProperties;
         suffix?: string;
         useEasing?: boolean;
+        easingFn?(t: number, b: number, c: number, d: number): void;
+        formattingFn?(value: number): string;
+        onComplete?(): void;
+        onEnd?(providedFn: {
+            pauseResume(): void;
+            reset(): void;
+            start(): void;
+            update(): void;
+        }): void;
+        onStart?(providedFn: {
+            pauseResume(): void;
+            reset(): void;
+            update(): void;
+        }): void;
+        onPauseResume?(): (
+            providedFn: { reset(): void; start(): void; update(): void }
+        ) => void;
+        onReset?(): (
+            providedFn: { pauseResume(): void; start(): void; update(): void }
+        ) => void;
+        onUpdate?(providedFn: {
+            pauseResume(): void;
+            reset(): void;
+            start(): void;
+        }): void;
+        style?: React.CSSProperties;
         children?(data: RenderProps): React.ReactElement<any>;
     }
 }
