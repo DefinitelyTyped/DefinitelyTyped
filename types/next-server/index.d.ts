f6ae4d7f4aca20c4daafb020fe7f7ef19c679269
AJ Livingston <livingston.aj@gmail.com>
2018-10-16 11:15:13 -0400

    added maintainers from next to next-server

--- types/next-server/index.d.ts
+++ types/next-server/index.d.ts
@@ -1,6 +1,12 @@
 // Type definitions for next-server 7.0
 // Project: https://github.com/zeit/next.js/packages/next-server
-// Definitions by: AJ Livingston <https://github.com/ajliv>
+// Definitions by: Drew Hays <https://github.com/dru89>
+//                 Brice BERNARD <https://github.com/brikou>
+//                 James Hegedus <https://github.com/jthegedus>
+//                 Resi Respati <https://github.com/resir014>
+//                 Scott Jones <https://github.com/scottdj92>
+//                 Joao Vieira <https://github.com/joaovieira>
+//                 AJ Livingston <https://github.com/ajliv>
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
 // TypeScript Version: 2.8
 
