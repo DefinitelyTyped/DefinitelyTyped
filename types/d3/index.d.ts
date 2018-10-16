05e0a38adb9410f5a2bbd9d7c30260dabb868f2a
denis <denisname@users.noreply.github.com>
2018-10-16 23:48:07 +0200

    Oops

--- types/d3/index.d.ts
+++ types/d3/index.d.ts
@@ -5,7 +5,6 @@
 //                 Boris Yankov <https://github.com/borisyankov>
 //                 denisname <https://github.com/denisname>
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
-// Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
 // TypeScript Version: 2.3
 
 // Last module patch version validated against: 5.0.0 RC3
