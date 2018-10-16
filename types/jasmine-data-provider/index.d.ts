b4614a1177d956a83279f09fc5ee8e8f7796fc57
Tomek Łaziuk <tlaziuk@gmail.com>
2018-10-16 09:00:41 +0200

    fix strict-export-declare-modifiers

--- types/jasmine-data-provider/index.d.ts
+++ types/jasmine-data-provider/index.d.ts
@@ -4,11 +4,11 @@
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
 // TypeScript Version: 2.3
 
-export = using;
-
 declare function using<T>(values: using.ValueType<T[]>, func: (data: T) => any): void;
 declare function using<T, K extends keyof T>(values: using.ValueType<T>, func: (data: T[K], description: K) => any): void;
 
 declare namespace using {
-    export type ValueType<T> = T | (() => T);
+    type ValueType<T> = T | (() => T);
 }
+
+export = using;
