38728d9cc8d956d6a1c391a2a0e99a8236923699
Tomek Łaziuk <tlaziuk@gmail.com>
2018-10-16 13:57:01 +0200

    description

--- types/range-parser/index.d.ts
+++ types/range-parser/index.d.ts
@@ -4,11 +4,11 @@
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
 
 /**
- * Returns `-1` when unsatisfiable and `-2` when syntactically invalid.
- *
  * When ranges are returned, the array has a "type" property which is the type of
  * range that is required (most commonly, "bytes"). Each array element is an object
  * with a "start" and "end" property for the portion of the range.
+ *
+ * @returns `-1` when unsatisfiable and `-2` when syntactically invalid, ranges otherwise.
  */
 declare function RangeParser(size: number, str: string, options?: RangeParser.Options): RangeParser.Result | RangeParser.Ranges;
 
