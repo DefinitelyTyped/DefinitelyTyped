25f085d39e78e8d665f06259caf95dbd67334d22
Andy Hanson <anhans@microsoft.com>
2018-10-16 17:55:03 -0700

    More fixes from dtslint improvements  (2)

--- types/ansi-styles/index.d.ts
+++ types/ansi-styles/index.d.ts
@@ -3,7 +3,7 @@
 // Definitions by: bryn austin bellomy <https://github.com/brynbellomy>
 //                 plylrnsdy <https://github.com/plylrnsdy>
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
-
+// TypeScript Version: 2.1
 
 import { EscapeCode } from './escape-code';
 
