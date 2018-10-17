0728afd421060aa85887ee888180622de3a4deb8
Nikolaj Kappler <Nikolaj-Kappler@web.de>
2018-10-17 15:36:43 +0200

    updated index.d.ts
    added comment 
    // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
    
    to fix test build

--- types/astring/index.d.ts
+++ types/astring/index.d.ts
@@ -1,6 +1,7 @@
 // Type definitions for astring 1.3
 // Project: https://github.com/davidbonnet/astring
 // Definitions by: Nikolaj Kappler <https://github.com/nkappler>
+// Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
 // TypeScript Version: 2.8
 
 import * as ESTree from 'estree';
