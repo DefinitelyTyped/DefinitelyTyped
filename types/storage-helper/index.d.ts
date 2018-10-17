01211e2fbbb1f6591583314a712cc6d2332920f0
Klaus Meinhardt <klaus.meinhardt1@gmail.com>
2018-10-17 21:05:08 +0200

    review comments

--- types/storage-helper/index.d.ts
+++ types/storage-helper/index.d.ts
@@ -2,11 +2,15 @@
 // Project: https://github.com/MatteoGabriele/storage-helper#readme
 // Definitions by: Klaus Meinhardt <https://github.com/ajafff>
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
+// TypeScript Version: 2.8
+
+type UndefinedToNull<T> = T extends undefined ? null : T;
+type IfTrue<T, Then, Else> = T extends true ? Then : Else;
 
 export function clear(): void;
 export function showStorageLogger(enabled: boolean): void;
 export function setItem(key: string, value: string, permanent?: boolean): void;
-export function getItem(key: string): string | null;
+export function getItem<T extends boolean | undefined = false, U = undefined>(key: string, parse?: T, fallback?: U): IfTrue<typeof parse, any, string | UndefinedToNull<typeof fallback>>;
 export function removeItem(key: string): void;
 
 declare const storageHelper: {
