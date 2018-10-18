9b5566b664c0cd53bcbc167d23b17489193ef5f7
Dom Parfitt <dom.parfitt@gmail.com>
2018-10-18 08:17:00 +0100

    Moved global tests into separate file

--- types/graphlib-dot/graphlib-dot-tests.ts
+++ types/graphlib-dot/graphlib-dot-tests.ts
@@ -2,15 +2,8 @@ import { Graph } from "graphlib";
 import { read, readMany, write } from 'graphlib-dot';
 
 // Module tests
-let graph: Graph = read('digraph {node 1}');
+const graph: Graph = read('digraph {node 1}');
 
-let graphs: Graph[] = readMany('digraph { node1 }');
+const graphs: Graph[] = readMany('digraph { node1 }');
 
-let dotStr: string = write(graph);
-
-// Global tests
-graph = graphlibDot.read('digraph {node 1}');
-
-graphs = graphlibDot.readMany('digraph { node1 }');
-
-dotStr = graphlibDot.write(graph);
+const dotStr: string = write(graph);
