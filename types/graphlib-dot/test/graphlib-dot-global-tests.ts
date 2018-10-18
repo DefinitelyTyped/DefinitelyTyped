3687409909426f08b54dc83d6ede48e97899ab56
Dom Parfitt <dom.parfitt@gmail.com>
2018-10-18 17:47:32 +0100

    Removed import from global tests

--- types/graphlib-dot/test/graphlib-dot-global-tests.ts
+++ types/graphlib-dot/test/graphlib-dot-global-tests.ts
@@ -1,8 +1,6 @@
-import { Graph } from "graphlib";
-
 // Global tests
-const graph: Graph = graphlibDot.read('digraph {node 1}');
+const graph = graphlibDot.read('digraph {node 1}');
 
-const graphs: Graph[] = graphlibDot.readMany('digraph { node1 }');
+const graphs = graphlibDot.readMany('digraph { node1 }');
 
-const dotStr: string = graphlibDot.write(graph);
+const dotStr = graphlibDot.write(graph);
