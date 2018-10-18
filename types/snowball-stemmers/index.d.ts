34c2a0b03a9b92d19beaf846726f6bfeeb6d2efb
Ryan <ryanvolum@users.noreply.github.com>
2018-10-18 14:36:59 -0700

    fixed github url header to https

--- types/snowball-stemmers/index.d.ts
+++ types/snowball-stemmers/index.d.ts
@@ -1,6 +1,6 @@
 // Type definitions for snowball-stemmers 0.6
 // Project: https://github.com/mazko/jssnowball#readme
-// Definitions by: Ryan Volum <http://github.com/ryanvolum>
+// Definitions by: Ryan Volum <https://github.com/ryanvolum>
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
 
 export interface Stemmer {
