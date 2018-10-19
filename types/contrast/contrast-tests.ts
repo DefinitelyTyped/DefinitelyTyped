a5c5079743b2bd34f43bd024cbcbc068059af871
Darragh Kirwan <Darragh.Kirwan@genesys.com>
2018-10-19 14:59:32 +0100

    add types for contrast

---
+++ types/contrast/contrast-tests.ts
@@ -0,0 +1,5 @@
+import contrast = require('contrast');
+
+// $ExpectType string
+contrast('#000000');
+contrast('#ffffff');
---
+++ types/contrast/index.d.ts
@@ -0,0 +1,7 @@
+// Type definitions for contrast 1.0
+// Project: https://github.com/scottcorgan/contrast
+// Definitions by: Darragh Kirwan <https://github.com/DKirwan>
+// Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
+
+declare function contrast(hex: string): string;
+export = contrast;
