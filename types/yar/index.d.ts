76ce9400c0faebc96decd23b0d48dc6cc5cf375c
Simon Schick <demwizzy@gmail.com>
2018-10-20 14:36:17 +0200

    fix(hapi): make cache generic

--- types/yar/index.d.ts
+++ types/yar/index.d.ts
@@ -2,7 +2,7 @@
 // Project: https://github.com/hapijs/yar#readme
 // Definitions by: Simon Schick <https://github.com/SimonSchick>
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
-// TypeScript Version: 2.4
+// TypeScript Version: 2.8
 // From https://github.com/hapijs/yar/blob/master/API.md
 
 import {
