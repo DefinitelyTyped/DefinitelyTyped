df210a0704eeb296a8ea43b5ba628c4e050ff2c2
sjx233 <sjx233@qq.com>
2018-10-20 16:22:47 +0800

    Add type definitions for gl 4.1

---
+++ types/gl/gl-tests.ts
@@ -0,0 +1,11 @@
+import createContext from "gl";
+
+const gl = createContext(0, 0); gl; // $ExpectType WebGLRenderingContext
+const resizeDrawingbuffer = gl.getExtension("STACKGL_resize_drawingbuffer"); resizeDrawingbuffer; // $ExpectType STACKGL_resize_drawingbuffer | null
+const destroyContext = gl.getExtension("STACKGL_destroy_context"); destroyContext; // $ExpectType STACKGL_destroy_context | null
+createContext(0, 0, { preserveDrawingBuffer: true }); // $ExpectType WebGLRenderingContext
+createContext(0, 0, { preserveDrawingBuffer: 0 }); // $ExpectError
+resizeDrawingbuffer!.resize(0, 0); // $ExpectType void
+resizeDrawingbuffer!.resize(0, ""); // $ExpectError
+destroyContext!.destroy(); // $ExpectType void
+destroyContext!.somethingElse(); // $ExpectError
