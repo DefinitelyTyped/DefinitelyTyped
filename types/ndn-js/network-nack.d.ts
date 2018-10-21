652219757831072026191bcfa9ae40bbeebe6a5a
Junxiao Shi <git@mail1.yoursunny.com>
2018-10-20 22:34:49 -0400

    ndn-js: NetworkNack

--- types/ndn-js/network-nack.d.ts
+++ types/ndn-js/network-nack.d.ts
@@ -1,1 +1,14 @@
-export type NetworkNack = any;
+export namespace NetworkNack {
+    enum Reason {
+        NONE       = 0,
+        CONGESTION = 50,
+        DUPLICATE  = 100,
+        NO_ROUTE   = 150,
+        OTHER_CODE = 0x7fff,
+    }
+}
+
+export class NetworkNack {
+    getOtherReasonCode(): number;
+    getReason(): NetworkNack.Reason;
+}
