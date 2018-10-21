c1b9499626e072b4986b82231d936c99b6ebe487
jan@pidgin <jan@esserjan.de>
2018-10-21 19:21:19 +0200

    @types/vuelidate

---
+++ types/vuelidate/index.d.ts
@@ -0,0 +1,16 @@
+// Type definitions for vuelidate 0.7
+// Project: https://github.com/janesser/vuelidate.ts
+// Definitions by: Jan Esser <https://github.com/janesser>
+// Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
+// TypeScript Version: 2.4
+
+import './vue'
+import './lib/validators'
+
+import { Vuelidate } from './vuelidate'
+export default Vuelidate
+
+export {
+    validationMixin,
+    Validation
+} from './vuelidate'
