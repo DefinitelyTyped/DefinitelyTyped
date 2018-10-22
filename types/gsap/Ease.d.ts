a1afe3580e626c12daaa11a7312b155826c0cda7
Leonardo Melo <leonardomelo@sideplay.com>
2018-10-22 11:20:05 +0100

    Revert "Multiple linter fixes to gsap definition files"
    This reverts commit f8a1d6645d5995315fa510e30bd76232bad92890.

--- types/gsap/Ease.d.ts
+++ types/gsap/Ease.d.ts
@@ -1,51 +1,52 @@
 declare namespace gsap {
-    class Ease {
+    export class Ease {
         constructor(func?: () => void, extraParams?: any[], type?: number, power?: number);
 
         /** Translates the tween's progress ratio into the corresponding ease ratio. */
         getRatio(p: number): number;
     }
 
-    interface EaseLookup {
-        find(name: string): Ease;
+    export class EaseLookup {
+        static find(name: string): Ease;
     }
 
-    class Back extends Ease {
+    export class Back extends Ease {
         static easeIn: Back;
         static easeInOut: Back;
         static easeOut: Back;
         config(overshoot: number): Elastic;
+
     }
-    class Bounce extends Ease {
+    export class Bounce extends Ease {
         static easeIn: Bounce;
         static easeInOut: Bounce;
         static easeOut: Bounce;
     }
-    class Circ extends Ease {
+    export class Circ extends Ease {
         static easeIn: Circ;
         static easeInOut: Circ;
         static easeOut: Circ;
     }
-    class Cubic extends Ease {
+    export class Cubic extends Ease {
         static easeIn: Cubic;
         static easeInOut: Cubic;
         static easeOut: Cubic;
     }
 
-    class Elastic extends Ease {
+    export class Elastic extends Ease {
         static easeIn: Elastic;
         static easeInOut: Elastic;
         static easeOut: Elastic;
         config(amplitude: number, period: number): Elastic;
     }
 
-    class Expo extends Ease {
+    export class Expo extends Ease {
         static easeIn: Expo;
         static easeInOut: Expo;
         static easeOut: Expo;
     }
 
-    class Linear extends Ease {
+    export class Linear extends Ease {
         static ease: Linear;
         static easeIn: Linear;
         static easeInOut: Linear;
@@ -53,59 +54,60 @@ declare namespace gsap {
         static easeOut: Linear;
     }
 
-    class Quad extends Ease {
+    export class Quad extends Ease {
         static easeIn: Quad;
         static easeInOut: Quad;
         static easeOut: Quad;
     }
 
-    class Quart extends Ease {
+    export class Quart extends Ease {
         static easeIn: Quart;
         static easeInOut: Quart;
         static easeOut: Quart;
     }
 
-    class Quint extends Ease {
+    export class Quint extends Ease {
         static easeIn: Quint;
         static easeInOut: Quint;
         static easeOut: Quint;
     }
 
-    class Sine extends Ease {
+    export class Sine extends Ease {
         static easeIn: Sine;
         static easeInOut: Sine;
         static easeOut: Sine;
     }
 
-    class SlowMo extends Ease {
+    export class SlowMo extends Ease {
         static ease: SlowMo;
         config(linearRatio: number, power: number, yoyoMode: boolean): SlowMo;
     }
 
-    class SteppedEase extends Ease {
+    export class SteppedEase extends Ease {
         constructor(staps: number);
         config(steps: number): SteppedEase;
     }
 
-    interface RoughEaseConfig {
+    export interface RoughEaseConfig {
         clamp?: boolean;
         points?: number;
         randomize?: boolean;
         strength?: number;
-        taper?: "in" | "out" | "both" | "none";
+        taper?: 'in' | 'out' | 'both' | 'none';
         template?: Ease;
     }
 
-    class RoughEase extends Ease {
+    export class RoughEase extends Ease {
         static ease: RoughEase;
         constructor(vars: RoughEaseConfig);
         config(steps?: number): RoughEase;
     }
 
-    const Power0: typeof Linear;
-    const Power1: typeof Quad;
-    const Power2: typeof Cubic;
-    const Power3: typeof Quart;
-    const Power4: typeof Quint;
-    const Strong: typeof Quint;
+
+    export var Power0: typeof Linear;
+    export var Power1: typeof Quad;
+    export var Power2: typeof Cubic;
+    export var Power3: typeof Quart;
+    export var Power4: typeof Quint;
+    export var Strong: typeof Quint;
 }
--- types/gsap/Plugins.d.ts
+++ types/gsap/Plugins.d.ts
@@ -1,15 +1,15 @@
 declare namespace gsap {
-    interface BezierPlugin extends TweenPlugin {
+    export interface BezierPlugin extends TweenPlugin {
         bezierThrough(values: any[], curviness?: number, quadratic?: boolean, correlate?: string, prepend?: {}, calcDifs?: boolean): {};
         cubicToQuadratic(a: number, b: number, c: number, d: number): any[];
         quadraticToCubic(a: number, b: number, c: number): {};
     }
 
-    interface CSSRulePlugin extends TweenPlugin {
+    export interface CSSRulePlugin extends TweenPlugin {
         getRule(selector: string): {};
     }
 
-    interface TweenPlugin {
+    export interface TweenPlugin {
         activate(plugins: any[]): boolean;
     }
 }
