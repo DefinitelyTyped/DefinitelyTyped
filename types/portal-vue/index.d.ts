c8cee29c6475d007e7e9640fc64b5bb7ce999e6d
nodegin <10@nodeg.in>
2018-10-23 16:42:21 +0800

    add portal-vue

---
+++ types/portal-vue/index.d.ts
@@ -0,0 +1,6 @@
+// Type definitions for portal-vue 1.4.0
+// Project: https://github.com/LinusBorg/portal-vue
+// Definitions by: nodegin <https://github.com/nodegin>
+// Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
+
+declare module 'portal-vue'
