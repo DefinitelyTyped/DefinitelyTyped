7bd54136c298f73236db4c55a950ddbd61a340ed
khoi-fish <hi.khoi.nguyen@gmail.com>
2018-10-23 15:00:59 -0700

    Added type definitions for git-rev-sync

---
+++ types/git-rev-sync/index.d.ts
@@ -0,0 +1,15 @@
+// Type definitions for git-rev-sync 1.12.0
+// Project: https://github.com/tblobaum/git-rev
+// Definitions by: Khoi Nguyen <http://hellokhoi.com>
+// Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
+
+export function short(filePath?: string, length?: number): string;
+export function long(filePath?: string): string;
+export function branch(branch?: string): void;
+export function count(): number;
+export function date(): Date;
+export function isDirty(): boolean;
+export function isTagDirty(): boolean;
+export function message(): string;
+export function remoteUrl(): string;
+export function tag(makeDirty?: boolean): string;
