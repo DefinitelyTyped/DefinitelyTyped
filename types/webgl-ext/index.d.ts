aec45b1b4d67ef012d874ac3ddf80e10e677fb09
Arthur Langereis <puurtuur@me.com>
2018-10-23 14:44:30 +0200

    Add TS min version header to webgl-ext

--- types/webgl-ext/index.d.ts
+++ types/webgl-ext/index.d.ts
@@ -2,6 +2,7 @@
 // Project: http://webgl.org/
 // Definitions by: Arthur Langereis <https://github.com/zenmumbler>
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped/webgl-ext
+// TypeScript Version: 2.7
 
 // render-gl1/declarations - WebGL 1 extension definitions (beyond TS lib)
 // Extracted from Stardazed - https://github.com/stardazed/stardazed
