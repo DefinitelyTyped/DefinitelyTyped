af28fa1d9b4b8a2b5a7014996a7509dd9f2bc1ae
Sebastian Greaves <sgreav@microsoft.com>
2018-10-24 13:52:16 +0100

    Undo unintended line removal

--- types/slate/index.d.ts
+++ types/slate/index.d.ts
@@ -11,6 +11,7 @@
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
 // TypeScript Version: 2.3
 import * as Immutable from "immutable";
+
 export class Data extends Immutable.Record({}) {
     [key: string]: any;
 
