74d977311d0bebacb7e112699b407f0620996952
Andrey Sakharov <caxap_8787@mail.ru>
2018-10-25 15:30:57 +0300

    types definitions for async-mkdirp were added

---
+++ types/async-mkdirp/index.d.ts
@@ -0,0 +1,12 @@
+// Type definitions for async-mkdirp v.1.2.4
+// Project: https://github.com/simonlc/async-mkdirp
+// Definitions by: Andrey Sakharov <https://github.com/muturgan>
+// Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
+
+/// <reference types="node" />
+
+import fs = require('fs');
+
+declare function mkdirp(path: fs.PathLike, mode?: number): Promise<void>;
+
+export = mkdirp;
