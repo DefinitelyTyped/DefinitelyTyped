6d557bb095ffa828ed8629462dc8201b1bc44014
Berkay GURSOY <berkaygursoy@gmail.com>
2018-10-25 19:19:09 +0300

    typescript version header 2.1 fix

--- types/prompts/index.d.ts
+++ types/prompts/index.d.ts
@@ -2,7 +2,7 @@
 // Project: https://github.com/terkelg/prompts
 // Definitions by: Berkay GURSOY <https://github.com/Berkays>
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
-// TypeScript Version: 3.1
+// TypeScript Version: 2.1
 
 export = prompts;
 
