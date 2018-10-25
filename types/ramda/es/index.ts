6faec331be6f8b79a1c0816b9128ab622b74a5f1
Alper Kilci <alperkilci@gmail.com>
2018-10-25 21:49:04 +0200

    add index.ts to es folder

---
+++ types/ramda/es/index.ts
@@ -0,0 +1,486 @@
+import add from './add';
+import addIndex from './addIndex';
+import adjust from './adjust';
+import all from './all';
+import allPass from './allPass';
+import always from './always';
+import and from './and';
+import any from './any';
+import anyPass from './anyPass';
+import ap from './ap';
+import aperture from './aperture';
+import append from './append';
+import apply from './apply';
+import applySpec from './applySpec';
+import applyTo from './applyTo';
+import ascend from './ascend';
+import assoc from './assoc';
+import assocPath from './assocPath';
+import binary from './binary';
+import bind from './bind';
+import both from './both';
+import call from './call';
+import chain from './chain';
+import clamp from './clamp';
+import clone from './clone';
+import comparator from './comparator';
+import complement from './complement';
+import compose from './compose';
+import composeK from './composeK';
+import composeP from './composeP';
+import concat from './concat';
+import cond from './cond';
+import construct from './construct';
+import constructN from './constructN';
+import contains from './contains';
+import converge from './converge';
+import countBy from './countBy';
+import curry from './curry';
+import curryN from './curryN';
+import dec from './dec';
+import defaultTo from './defaultTo';
+import descend from './descend';
+import difference from './difference';
+import differenceWith from './differenceWith';
+import dissoc from './dissoc';
+import dissocPath from './dissocPath';
+import divide from './divide';
+import drop from './drop';
+import dropLast from './dropLast';
+import dropLastWhile from './dropLastWhile';
+import dropLastWhile from './dropLastWhile';
+import either from './either';
+import empty from './empty';
+import endsWith from './endsWith';
+import eqBy from './eqBy';
+import eqProps from './eqProps';
+import equals from './equals';
+import evolve from './evolve';
+import F from './F';
+import filter from './filter';
+import find from './find';
+import findIndex from './findIndex';
+import findLast from './findLast';
+import findLastIndex from './findLastIndex';
+import flatten from './flatten';
+import flip from './flip';
+import forEach from './forEach';
+import forEachObjIndexed from './forEachObjIndexed';
+import fromPairs from './fromPairs';
+import groupBy from './groupBy';
+import groupWith from './groupWith';
+import gt from './gt';
+import gte from './gte';
+import has from './has';
+import hasIn from './hasIn';
+import head from './head';
+import identical from './identical';
+import identity from './identity';
+import ifElse from './ifElse';
+import inc from './inc';
+import indexBy from './indexBy';
+import indexOf from './indexOf';
+import init from './init';
+import insert from './insert';
+import insertAll from './insertAll';
+import intersection from './intersection';
+import intersectionWith from './intersectionWith';
+import intersperse from './intersperse';
+import into from './into';
+import invert from './invert';
+import invertObj from './invertObj';
+import invoker from './invoker';
+import is from './is';
+import isArrayLike from './isArrayLike';
+import isEmpty from './isEmpty';
+import isNaN from './isNaN';
+import isNil from './isNil';
+import join from './join';
+import juxt from './juxt';
+import keys from './keys';
+import keysIn from './keysIn';
+import last from './last';
+import lastIndexOf from './lastIndexOf';
+import length from './length';
+import lens from './lens';
+import lensIndex from './lensIndex';
+import lensPath from './lensPath';
+import lift from './lift';
+import lt from './lt';
+import lte from './lte';
+import map from './map';
+import mapAccum from './mapAccum';
+import mapAccumRight from './mapAccumRight';
+import mapObjIndexed from './mapObjIndexed';
+import match from './match';
+import mathMod from './mathMod';
+import max from './max';
+import maxBy from './maxBy';
+import mean from './mean';
+import median from './median';
+import memoize from './memoize';
+import memoizeWith from './memoizeWith';
+import merge from './merge';
+import mergeAll from './mergeAll';
+import mergeDeepLeft from './mergeDeepLeft';
+import mergeDeepRight from './mergeDeepRight';
+import mergeDeepWith from './mergeDeepWith';
+import mergeDeepWithKey from './mergeDeepWithKey';
+import mergeWith from './mergeWith';
+import mergeWithKey from './mergeWithKey';
+import min from './min';
+import minBy from './minBy';
+import modulo from './modulo';
+import multiply from './multiply';
+import nAry from './nAry';
+import negate from './negate';
+import none from './none';
+import not from './not';
+import nth from './nth';
+import nthArg from './nthArg';
+import objOf from './objOf';
+import of from './of';
+import omit from './omit';
+import once from './once';
+import or from './or';
+import over from './over';
+import pair from './pair';
+import partial from './partial';
+import partialRight from './partialRight';
+import partition from './partition';
+import path from './path';
+import pathEq from './pathEq';
+import pathOr from './pathOr';
+import pathSatisfies from './pathSatisfies';
+import pick from './pick';
+import pickAll from './pickAll';
+import pickBy from './pickBy';
+import pipe from './pipe';
+import pipeK from './pipeK';
+import pipeP from './pipeP';
+import pluck from './pluck';
+import prepend from './prepend';
+import product from './product';
+import project from './project';
+import prop from './prop';
+import propEq from './propEq';
+import propIs from './propIs';
+import propOr from './propOr';
+import props from './props';
+import propSatisfies from './propSatisfies';
+import range from './range';
+import reduce from './reduce';
+import reduceBy from './reduceBy';
+import reduced from './reduced';
+import reduceRight from './reduceRight';
+import reduceWhile from './reduceWhile';
+import reject from './reject';
+import remove from './remove';
+import repeat from './repeat';
+import replace from './replace';
+import reverse from './reverse';
+import scan from './scan';
+import set from './set';
+import slice from './slice';
+import sort from './sort';
+import sortBy from './sortBy';
+import sortWith from './sortWith';
+import split from './split';
+import splitAt from './splitAt';
+import splitEvery from './splitEvery';
+import splitWhen from './splitWhen';
+import startsWith from './startsWith';
+import subtract from './subtract';
+import sum from './sum';
+import symmetricDifference from './symmetricDifference';
+import symmetricDifferenceWith from './symmetricDifferenceWith';
+import T from './T';
+import tail from './tail';
+import take from './take';
+import takeLast from './takeLast';
+import takeLastWhile from './takeLastWhile';
+import takeWhile from './takeWhile';
+import tap from './tap';
+import test from './test';
+import times from './times';
+import toLower from './toLower';
+import toPairs from './toPairs';
+import toPairsIn from './toPairsIn';
+import toString from './toString';
+import toUpper from './toUpper';
+import transduce from './transduce';
+import transpose from './transpose';
+import traverse from './traverse';
+import trim from './trim';
+import tryCatch from './tryCatch';
+import type from './type';
+import unapply from './unapply';
+import unary from './unary';
+import uncurryN from './uncurryN';
+import unfold from './unfold';
+import union from './union';
+import unionWith from './unionWith';
+import uniq from './uniq';
+import uniqBy from './uniqBy';
+import uniqWith from './uniqWith';
+import unless from './unless';
+import unnest from './unnest';
+import until from './until';
+import update from './update';
+import useWith from './useWith';
+import values from './values';
+import valuesIn from './valuesIn';
+import view from './view';
+import when from './when';
+import where from './where';
+import whereEq from './whereEq';
+import without from './without';
+import wrap from './wrap';
+import xprod from './xprod';
+import zip from './zip';
+import zipObj from './zipObj';
+import zipWith from './zipWith';
+
+export {
+    add,
+    addIndex,
+    adjust,
+    all,
+    allPass,
+    always,
+    and,
+    any,
+    anyPass,
+    ap,
+    aperture,
+    append,
+    apply,
+    applySpec,
+    applyTo,
+    ascend,
+    assoc,
+    assocPath,
+    binary,
+    bind,
+    both,
+    call,
+    chain,
+    clamp,
+    clone,
+    comparator,
+    complement,
+    compose,
+    composeK,
+    composeP,
+    concat,
+    cond,
+    construct,
+    constructN,
+    contains,
+    converge,
+    countBy,
+    curry,
+    curryN,
+    dec,
+    defaultTo,
+    descend,
+    difference,
+    differenceWith,
+    dissoc,
+    dissocPath,
+    divide,
+    drop,
+    dropLast,
+    dropLastWhile,
+    either,
+    empty,
+    endsWith,
+    eqBy,
+    eqProps,
+    equals,
+    evolve,
+    F,
+    filter,
+    find,
+    findIndex,
+    findLast,
+    findLastIndex,
+    flatten,
+    flip,
+    forEach,
+    forEachObjIndexed,
+    fromPairs,
+    groupBy,
+    groupWith,
+    gt,
+    gte,
+    has,
+    hasIn,
+    head,
+    identical,
+    identity,
+    ifElse,
+    inc,
+    indexBy,
+    indexOf,
+    init,
+    insert,
+    insertAll,
+    intersection,
+    intersectionWith,
+    intersperse,
+    into,
+    invert,
+    invertObj,
+    invoker,
+    is,
+    isArrayLike,
+    isEmpty,
+    isNaN,
+    isNil,
+    join,
+    juxt,
+    keys,
+    keysIn,
+    last,
+    lastIndexOf,
+    length,
+    lens,
+    lensIndex,
+    lensPath,
+    lift,
+    lt,
+    lte,
+    map,
+    mapAccum,
+    mapAccumRight,
+    mapObjIndexed,
+    match,
+    mathMod,
+    max,
+    maxBy,
+    mean,
+    median,
+    memoize,
+    memoizeWith,
+    merge,
+    mergeAll,
+    mergeDeepLeft,
+    mergeDeepRight,
+    mergeDeepWith,
+    mergeDeepWithKey,
+    mergeWith,
+    mergeWithKey,
+    min,
+    minBy,
+    modulo,
+    multiply,
+    nAry,
+    negate,
+    none,
+    not,
+    nth,
+    nthArg,
+    objOf,
+    of,
+    omit,
+    once,
+    or,
+    over,
+    pair,
+    partial,
+    partialRight,
+    partition,
+    path,
+    pathEq,
+    pathOr,
+    pathSatisfies,
+    pick,
+    pickAll,
+    pickBy,
+    pipe,
+    pipeK,
+    pipeP,
+    pluck,
+    prepend,
+    product,
+    project,
+    prop,
+    propEq,
+    propIs,
+    propOr,
+    props,
+    propSatisfies,
+    range,
+    reduce,
+    reduceBy,
+    reduced,
+    reduceRight,
+    reduceWhile,
+    reject,
+    remove,
+    repeat,
+    replace,
+    reverse,
+    scan,
+    set,
+    slice,
+    sort,
+    sortBy,
+    sortWith,
+    split,
+    splitAt,
+    splitEvery,
+    splitWhen,
+    startsWith,
+    subtract,
+    sum,
+    symmetricDifference,
+    symmetricDifferenceWith,
+    T,
+    tail,
+    take,
+    takeLast,
+    takeLastWhile,
+    takeWhile,
+    tap,
+    test,
+    times,
+    toLower,
+    toPairs,
+    toPairsIn,
+    toString,
+    toUpper,
+    transduce,
+    transpose,
+    traverse,
+    trim,
+    tryCatch,
+    type,
+    unapply,
+    unary,
+    uncurryN,
+    unfold,
+    union,
+    unionWith,
+    uniq,
+    uniqBy,
+    uniqWith,
+    unless,
+    unnest,
+    until,
+    update,
+    useWith,
+    values,
+    valuesIn,
+    view,
+    when,
+    where,
+    whereEq,
+    without,
+    wrap,
+    xprod,
+    zip,
+    zipObj,
+    zipWith
+};
