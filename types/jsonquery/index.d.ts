829f92bf84b1208b0c7eef69f4a47cced2630062
Jim Buck <jim@jimmyboh.com>
2018-10-24 23:23:15 -0400

    Updated typings to pass linting.

--- types/jsonquery/index.d.ts
+++ types/jsonquery/index.d.ts
@@ -2,45 +2,47 @@
 // Project: https://github.com/eugeneware/jsonquery
 // Definitions by: Jim Buck <https://github.com/JimmyBoh>
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
+// TypeScript Version: 2.1
 
 /// <reference types="node" />
 
 declare function jsonquery<T>(query: jsonquery.Query<T>): NodeJS.ReadWriteStream;
 
 declare namespace jsonquery {
-    export function match<T>(haystack: T, predicate: Query<T>): boolean;
+    function match<T>(haystack: T, predicate: Query<T>): boolean;
 
-    export type Query<T> = BinaryQueryCondition<T> | QueryValue<T> | PathQuery;
+    type Query<T> = BinaryQueryCondition<T> | QueryValue<T> | PathQuery;
 
-    export interface OrQueryCondition<T> {
+    interface OrQueryCondition<T> {
         $or: Array<Query<T>>;
     }
 
-    export interface AndQueryCondition<T> {
+    interface AndQueryCondition<T> {
         $and: Array<Query<T>>;
     }
 
-    export type BinaryQueryCondition<T> = OrQueryCondition<T> | AndQueryCondition<T>;
+    type BinaryQueryCondition<T> = OrQueryCondition<T> | AndQueryCondition<T>;
 
-    export interface BaseCondition<P> {
+    interface BaseCondition<P> {
         $lt: P;
         $lte: P;
         $gt: P;
         $gte: P;
         $mod: [number, number];
         $ne: P;
-        $in: Array<P>
-        $nin: Array<P>
-        $all: Array<P>;
+        $in: P[];
+        $nin: P[];
+        $all: P[];
         $elemMatch: Partial<P>;
     }
 
-    export type PathQuery = {
+    interface PathQuery {
         [path: string]: any;
     }
-    export type QueryValue<T> = {
+
+    type QueryValue<T> = {
         [P in keyof T]?: T[P] | BaseCondition<T[P]>;
-    }
+    };
 }
 
 export = jsonquery;
--- types/jsonquery/jsonquery-tests.ts
+++ types/jsonquery/jsonquery-tests.ts
@@ -1,4 +1,3 @@
-
 import jsonquery = require('jsonquery');
 
 interface TestData {
@@ -11,100 +10,111 @@ interface TestData {
     tree: {
         a: number;
         b: number;
-    }
+    };
 }
 
-const fakeTestDataInstance: TestData = {} as TestData;
+const testDataInstance: TestData = {
+    name: "Name 1",
+    number: "Number 1",
+    val: 10,
+    favorites: [10, 20],
+    awesome: true,
+    nullify: null,
+    tree: {
+        a: 1,
+        b: 2,
+    }
+};
 
-// $ExpectType NodeJS.ReadWriteStream
+// $ExpectType ReadWriteStream
 jsonquery<TestData>({ number: 'Number 7' });
 
-// $ExpectType NodeJS.ReadWriteStream
+// $ExpectType ReadWriteStream
 jsonquery<TestData>({ number: 'Number 7', val: 70 });
 
-// $ExpectType NodeJS.ReadWriteStream
+// $ExpectType ReadWriteStream
 jsonquery<TestData>({ $or:  [ { number: 'Number 7' }, { val: 50 } ] });
 
-// $ExpectType NodeJS.ReadWriteStream
+// $ExpectType ReadWriteStream
 jsonquery<TestData>({ $and:  [ { number: 'Number 7' }, { val: 70 } ] });
 
-// $ExpectType NodeJS.ReadWriteStream
+// $ExpectType ReadWriteStream
 jsonquery<TestData>({ $or:  [ { $and: [ { number: 'Number 7' }, { val: 70 } ] }, { val: 50 } ] });
 
-// $ExpectType NodeJS.ReadWriteStream
+// $ExpectType ReadWriteStream
 jsonquery<TestData>({ val: { $in: [ 70, 50 ] } });
 
-// $ExpectType NodeJS.ReadWriteStream
+// $ExpectType ReadWriteStream
 jsonquery<TestData>({ val: { $or: [ { $in: [ 70, 50 ] }, { $in: [ 60, 20 ] } ] } });
 
-// $ExpectType NodeJS.ReadWriteStream
+// $ExpectType ReadWriteStream
 jsonquery<TestData>({ val: { $gt: 900 } });
 
-// $ExpectType NodeJS.ReadWriteStream
+// $ExpectType ReadWriteStream
 jsonquery<TestData>({ val: { $lt: 900 } });
 
-// $ExpectType NodeJS.ReadWriteStream
+// $ExpectType ReadWriteStream
 jsonquery<TestData>({ val: { $or: [ { $lt: 20 }, { $gt: 950 } ] } });
 
-// $ExpectType NodeJS.ReadWriteStream
+// $ExpectType ReadWriteStream
 jsonquery<TestData>({ val: { $and: [ { $gt: 970 }, { $gt: 950 } ] } });
 
-// $ExpectType NodeJS.ReadWriteStream
+// $ExpectType ReadWriteStream
 jsonquery<TestData>({ val: { $ne: 900 } });
 
-// $ExpectType NodeJS.ReadWriteStream
+// $ExpectType ReadWriteStream
 jsonquery<TestData>({ val: { $lte: 900 } });
 
-// $ExpectType NodeJS.ReadWriteStream
+// $ExpectType ReadWriteStream
 jsonquery<TestData>({ val: { $gte: 900 } });
 
-// $ExpectType NodeJS.ReadWriteStream
+// $ExpectType ReadWriteStream
 jsonquery<TestData>({ favorites: { $all: [50, 60] } });
 
-// $ExpectType NodeJS.ReadWriteStream
+// $ExpectType ReadWriteStream
 jsonquery<TestData>({ tree: { $elemMatch: { a: 1, b: 2 } } });
 
-// $ExpectType NodeJS.ReadWriteStream
+// $ExpectType ReadWriteStream
 jsonquery<TestData>({ 'tree.a': 1 });
 
-// $ExpectType NodeJS.ReadWriteStream
+// $ExpectType ReadWriteStream
 jsonquery<TestData>({ 'tree.a': { $in: [1, 5] } });
 
-// $ExpectType NodeJS.ReadWriteStream
+// $ExpectType ReadWriteStream
 jsonquery<TestData>({ number: /er 7$/ });
 
-// $ExpectType NodeJS.ReadWriteStream
+// $ExpectType ReadWriteStream
 jsonquery<TestData>({ number: { $in: [ /er 7$/, /er 5$/ ] } });
 
-// $ExpectType NodeJS.ReadWriteStream
+// $ExpectType ReadWriteStream
 jsonquery<TestData>({ favorites: { $all: [/^50$/, /^60$/] } });
 
-// $ExpectType NodeJS.ReadWriteStream
+// $ExpectType ReadWriteStream
 jsonquery<TestData>({ tree: { $elemMatch: { a: /^1$/, b: 2 } } });
 
-// $ExpectType NodeJS.ReadWriteStream
+// $ExpectType ReadWriteStream
 jsonquery<TestData>({ val: { $nin: [ 70, 50 ] } });
 
-// $ExpectType NodeJS.ReadWriteStream
+// $ExpectType ReadWriteStream
 jsonquery<TestData>({ val: { $mod: [ 7, 1 ] } });
 
-// $ExpectType NodeJS.ReadWriteStream
+// $ExpectType ReadWriteStream
 jsonquery<TestData>({ favorites: { $size: 2 } });
 
-// $ExpectType NodeJS.ReadWriteStream
+// $ExpectType ReadWriteStream
 jsonquery<TestData>({ $and: [ { tree: { $exists: true } }, { missing: { $exists: false } } ] });
 
-// $ExpectType NodeJS.ReadWriteStream
+// $ExpectType ReadWriteStream
 jsonquery<TestData>({ $not: { number: 'Number 7', val: 70 } });
 
-// $ExpectType NodeJS.ReadWriteStream
+// $ExpectType ReadWriteStream
 jsonquery<TestData>({ number: { $not: { $nin: ['Number 7'] } } });
 
-// $ExpectType NodeJS.ReadWriteStream
+// $ExpectType ReadWriteStream
 jsonquery<TestData>({ foo: { $all: ['bar'] } });
 
 // $ExpectType boolean
-jsonquery.match<TestData>(fakeTestDataInstance, { val: 7 });
+jsonquery.match<TestData>(testDataInstance, { val: 7 });
 
 // $ExpectType boolean
-jsonquery.match<TestData>(fakeTestDataInstance, { 'tree.a': { $in: [1, 5] } });
+jsonquery.match<TestData>(testDataInstance, { 'tree.a': { $in: [1, 5] } });
