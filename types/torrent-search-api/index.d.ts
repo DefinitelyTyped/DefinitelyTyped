d1ec5f0f694cbcaf476992b842e77843c209c7ca
Nicolas GIRARDIN <ngirardin@gmail.com>
2018-10-26 20:47:05 +0200

    Fix repo url

--- types/torrent-search-api/index.d.ts
+++ types/torrent-search-api/index.d.ts
@@ -1,5 +1,5 @@
 // Type definitions for torrent-search-api 2.0
-// Project: https://github.com/JimmyLaurent/torrent-search-api#readme
+// Project: https://github.com/JimmyLaurent/torrent-search-api
 // Definitions by: Nicolas Girardin <https://github.com/ngirardin>
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
 
