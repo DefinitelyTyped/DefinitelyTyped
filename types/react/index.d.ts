e3b90c1ead444f839a1ab3b44cbd580bbd1287f0
Jessica <jessicamfranco3@gmail.com>
2018-10-26 21:06:09 +0900

    Add types for the proposed Hooks API

--- types/react/index.d.ts
+++ types/react/index.d.ts
@@ -1,4 +1,4 @@
-// Type definitions for React 16.4
+// Type definitions for React 16.7
 // Project: http://facebook.github.io/react/
 // Definitions by: Asana <https://asana.com>
 //                 AssureSign <http://www.assuresign.com>
@@ -559,6 +559,120 @@ declare namespace React {
     function createRef<T>(): RefObject<T>;
 
     function forwardRef<T, P = {}>(Component: RefForwardingComponent<T, P>): ComponentType<P & ClassAttributes<T>>;
+
+    //
+    // React Hooks
+    // ----------------------------------------------------------------------
+
+    // based on the code in https://github.com/facebook/react/pull/13968
+
+    type SetStateAction<S> = S | ((prevState: S) => S);
+    // this technically does accept a second argument, but it's already under a deprecation warning
+    // and it's not even released so probably better to not define it.
+    type Dispatch<A> = (value: A) => void;
+    // Unlike redux, the actions _can_ be anything
+    type Reducer<S, A> = (prevState: S, action: A) => S;
+    // The identity check is done with the SameValue algorithm (Object.is), which is stricter than ===
+    type IdentityCheckInputList = ReadonlyArray<any>;
+
+    // NOTE: the effect callbacks are actually allowed to return anything, but functions are treated
+    // specially. I don't think it's intended to accept async functions, should use throw instead of
+    // await to properly engage Suspense.
+    type EffectCallback = () => (void | (() => void));
+
+    interface MutableRefObject<T> {
+        current: T | null;
+    }
+
+    /**
+     * Accepts a context object (the value returned from `React.createContext`) and returns the current
+     * context value, as given by the nearest context provider for the given context.
+     *
+     * @see https://reactjs.org/docs/hooks-reference.html#usecontext
+     */
+    function useContext<T>(context: Context<T> /*, (not public API) observedBits?: number|boolean */): T;
+    /**
+     * Returns a stateful value, and a function to update it.
+     *
+     * @see https://reactjs.org/docs/hooks-reference.html#usestate
+     */
+    function useState<S>(initialState: S | (() => S)): [S, Dispatch<SetStateAction<S>>];
+    /**
+     * An alternative to `useState`.
+     *
+     * `useReducer` is usually preferable to `useState` when you have complex state logic that involves
+     * multiple sub-values. It also lets you optimize performance for components that trigger deep
+     * updates because you can pass `dispatch` down instead of callbacks.
+     *
+     * @see https://reactjs.org/docs/hooks-reference.html#usereducer
+     */
+    function useReducer<S, A>(reducer: Reducer<S, A>, initialState: S, initialAction?: A | null): [S, Dispatch<A>];
+    /**
+     * `useRef` returns a mutable ref object whose `.current` property is initialized to the passed argument
+     * (`initialValue`). The returned object will persist for the full lifetime of the component.
+     *
+     * Note that `useRef()` is useful for more than the `ref` attribute. It’s handy for keeping any mutable
+     * value around similar to how you’d use instance fields in classes.
+     *
+     * @see https://reactjs.org/docs/hooks-reference.html#useref
+     */
+    function useRef<T>(initialValue: T): MutableRefObject<T>;
+    /**
+     *
+     * The signature is identical to `useEffect`, but it fires synchronously during the same phase that
+     * React performs its DOM mutations, before sibling components have been updated. Use this to perform
+     * custom DOM mutations.
+     *
+     * Prefer the standard `useEffect` when possible to avoid blocking visual updates.
+     *
+     * @see https://reactjs.org/docs/hooks-reference.html#usemutationeffect
+     */
+    function useMutationEffect(effect: EffectCallback, inputs?: IdentityCheckInputList): void;
+    /**
+     * The signature is identical to `useEffect`, but it fires synchronously after all DOM mutations.
+     * Use this to read layout from the DOM and synchronously re-render. Updates scheduled inside
+     * `useLayoutEffect` will be flushed synchronously, before the browser has a chance to paint.
+     *
+     * Prefer the standard `useEffect` when possible to avoid blocking visual updates.
+     *
+     * If you’re migrating code from a class component, `useLayoutEffect` fires in the same phase as
+     * `componentDidMount` and `componentDidUpdate`.
+     * @see https://reactjs.org/docs/hooks-reference.html#uselayouteffect
+     */
+    function useLayoutEffect(effect: EffectCallback, inputs?: IdentityCheckInputList): void;
+    /**
+     * Accepts a function that contains imperative, possibly effectful code.
+     *
+     * @param effect Imperative function that can return a cleanup function
+     * @param inputs If present, effect will only activate if the values in the list change.
+     *
+     * @see https://reactjs.org/docs/hooks-reference.html#useeffect
+     */
+    function useEffect(effect: EffectCallback, inputs?: IdentityCheckInputList): void;
+    // NOTE: this does not accept strings, but this will have to be fixed by removing strings from type Ref<T>
+    /**
+     * `useImperativeMethods` customizes the instance value that is exposed to parent components when using
+     * `ref`. As always, imperative code using refs should be avoided in most cases.
+     *
+     * `useImperativeMethods` should be used with `React.forwardRef`.
+     *
+     * @see https://reactjs.org/docs/hooks-reference.html#useimperativemethods
+     */
+    function useImperativeMethods<T>(ref: Ref<T>, init: () => T): void;
+    // I made 'inputs' required here and in useMemo as there's no point to memoizing without the memoization key
+    /**
+     * `useCallback` will return a memoized version of the callback that only changes if one of the `inputs`
+     * has changed.
+     *
+     * @see https://reactjs.org/docs/hooks-reference.html#usecallback
+     */
+    function useCallback<T extends (...args: any[]) => any>(callback: T, inputs: IdentityCheckInputList): T;
+    /**
+     * `useMemo` will only recompute the memoized value when one of the `inputs` has changed.
+     *
+     * @see https://reactjs.org/docs/hooks-reference.html#usememo
+     */
+    function useMemo<T>(factory: () => T, inputs: IdentityCheckInputList): T;
 
     //
     // Event System
