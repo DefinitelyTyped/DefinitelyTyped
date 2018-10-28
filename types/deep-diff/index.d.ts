f4cff5517cf4bf5154e37c32ae2e8b0952232098
nixx quality <nixx@is-fantabulo.us>
2018-10-28 23:13:52 +0100

    maybe over here?

--- types/deep-diff/index.d.ts
+++ types/deep-diff/index.d.ts
@@ -1,8 +1,8 @@
 // Type definitions for deep-diff 1.0.2
-// TypeScript Version: 2.3
 // Project: https://github.com/flitbit/diff/
 // Definitions by: ZauberNerd <https://github.com/ZauberNerd>
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
+// TypeScript Version: 2.3
 
 declare namespace deepDiff {
 
