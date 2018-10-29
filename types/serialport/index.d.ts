42b2226022943251c9a23867e3e7dcc88812265f
Ondrej Slinták <ondrowan@gmail.com>
2018-10-29 17:30:23 +0100

    Bump root serialport bindings to 7.0

--- types/serialport/index.d.ts
+++ types/serialport/index.d.ts
@@ -1,4 +1,4 @@
-// Type definitions for serialport 6.0
+// Type definitions for serialport 7.0
 // Project: https://github.com/EmergingTechnologyAdvisors/node-serialport
 // Definitions by: Jeremy Foster <https://github.com/codefoster>
 //                 Andrew Pearson <https://github.com/apearson>
