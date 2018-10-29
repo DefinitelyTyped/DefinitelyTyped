26d377a9bcaf6e920cdf007ebb8c57bc3e20547f
jeffy-g <hirotom1107@gmail.com>
2018-10-29 20:48:28 +0900

    Update index.d.ts

--- types/react-table/index.d.ts
+++ types/react-table/index.d.ts
@@ -1,8 +1,6 @@
 // Type definitions for react-table 6.7
 // Project: https://github.com/react-tools/react-table
-// Definitions by: Roy Xue <https://github.com/royxue>,
-//                 Pavel Sakalo <https://github.com/psakalo>,
-//                 Krzysztof Porębski <https://github.com/Havret>
+// Definitions by: Roy Xue <https://github.com/royxue>, Pavel Sakalo <https://github.com/psakalo>, Krzysztof Porębski <https://github.com/Havret>, Andy S <https://github.com/andys8>
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
 // TypeScript Version: 2.8
 import * as React from 'react';
