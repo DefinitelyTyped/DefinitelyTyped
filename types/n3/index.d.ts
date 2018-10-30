5207f41e887fa403cf29a0ab0e981cf838522b03
Laurens Rietveld <laurens.rietveld@gmail.com>
2018-10-30 11:48:11 +0100

    Added LaurensRietveld as contributor

--- types/n3/index.d.ts
+++ types/n3/index.d.ts
@@ -2,6 +2,7 @@
 // Project: https://github.com/RubenVerborgh/N3.js
 // Definitions by: Fred Eisele <https://github.com/phreed>
 //                 Ruben Taelman <https://github.com/rubensworks>
+//                 Laurens Rietveld <https://github.com/LaurensRietveld>
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
 // TypeScript Version: 2.3
 
--- types/rdf-js/index.d.ts
+++ types/rdf-js/index.d.ts
@@ -1,6 +1,7 @@
 // Type definitions for the RDFJS specification 1.0
 // Project: https://github.com/rdfjs/representation-task-force
 // Definitions by: Ruben Taelman <https://github.com/rubensworks>
+//                 Laurens Rietveld <https://github.com/LaurensRietveld>
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
 // TypeScript Version: 2.3
 
