0f855d0d5d3fdb64cf9bd6643b286d656f687e94
Hugues Stefanski <hugues.stefanski@gmail.com>
2018-10-31 12:24:25 +0100

    #30160 : updated d3-contour version

--- types/d3-contour/index.d.ts
+++ types/d3-contour/index.d.ts
@@ -1,10 +1,10 @@
-// Type definitions for d3-contour 1.2
+// Type definitions for d3-contour 1.3
 // Project: https://d3js.org/d3-contour/
 // Definitions by: Tom Wanzek <https://github.com/tomwanzek>, Hugues Stefanski <https://github.com/Ledragon>
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
 // TypeScript Version: 2.3
 
-// Last module patch version validated against: 1.2.0
+// Last module patch version validated against: 1.3.2
 
 import { MultiPolygon } from 'geojson';
 import { ThresholdNumberArrayGenerator, ThresholdCountGenerator } from 'd3-array';
