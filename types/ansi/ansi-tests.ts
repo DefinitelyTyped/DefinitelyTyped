f09c1b738272f418c2e7b016ef4c497ee618df20
Gustavo R. 'Gustavo6046' Rehermann <gugurehermann@gmail.com>
2018-11-01 12:10:26 -0300

    Fixed slight issues regarding casts.

--- types/ansi/ansi-tests.ts
+++ types/ansi/ansi-tests.ts
@@ -22,9 +22,9 @@ Object.keys({
 }).forEach((color) => {
     const cname = color.replace(/([a-z])([A-Z])/g, (_: string, l: string, u: string): string => `${l} ${u.toLowerCase()}`);
 
-    (((<(...anything: any[]) => ansi.Cursor> cursor[color])()
+    ((<(...anything: any[]) => ansi.Cursor> cursor[color])()
         .bold()
-        .bg as ansi.Colorer)[color === 'blue' || color === 'brightBlue' ? 'black' : 'blue']() as ansi.Cursor)
+        .bg)[color === 'blue' || color === 'brightBlue' ? 'black' : 'blue']()
         .write(
             `Hello, bold ${cname} world!\n`
         )
