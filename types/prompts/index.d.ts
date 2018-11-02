1596d8fa41b3f4ead68a9dfc9d299ace430bd375
Daniel Perez Alvarez <danielpza@protonmail.com>
2018-11-02 16:22:38 -0400

    [prompts] upgrade ts version

--- types/prompts/index.d.ts
+++ types/prompts/index.d.ts
@@ -2,7 +2,7 @@
 // Project: https://github.com/terkelg/prompts
 // Definitions by: Berkay GURSOY <https://github.com/Berkays>
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
-// TypeScript Version: 2.1
+// TypeScript Version: 2.3
 
 export = prompts;
 
