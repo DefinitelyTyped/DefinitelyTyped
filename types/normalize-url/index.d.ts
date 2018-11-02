d1cf1594470716ad1eab9f6c7dd2a81fb9bc589c
Pierre Rahier <pierre@sensu.one>
2018-11-02 16:49:37 +0900

    Update the normalize-url types to normalize-url version 3.3.0

--- types/normalize-url/index.d.ts
+++ types/normalize-url/index.d.ts
@@ -1,18 +1,22 @@
-// Type definitions for normalize-url 1.9
+// Type definitions for normalize-url 1.10
 // Project: https://github.com/sindresorhus/normalize-url
-// Definitions by: odin3 <https://github.com/odin3>
+// Definitions by: xenux <https://github.com/xenux>
+//                 odin3 <https://github.com/odin3>
 //                 BendingBender <https://github.com/BendingBender>
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
 
 declare namespace normalizeUrl {
     interface Options {
-        normalizeProtocol?: boolean;
-        normalizeHttps?: boolean;
-        stripFragment?: boolean;
-        stripWWW?: boolean;
-        removeQueryParameters?: Array<RegExp | string>;
-        removeTrailingSlash?: boolean;
-        removeDirectoryIndex?: Array<RegExp | string>;
+		defaultProtocol?: string;
+		normalizeProtocol?: boolean;
+		forceHttp?: boolean;
+		forceHttps?: boolean;
+		stripHash?: boolean;
+		stripWWW?: boolean;
+		removeQueryParameters?: Array<RegExp | string>;
+		removeTrailingSlash?: boolean;
+		removeDirectoryIndex?: Array<RegExp | string> | true;
+		sortQueryParameters?: boolean;
     }
 }
 
--- types/normalize-url/normalize-url-tests.ts
+++ types/normalize-url/normalize-url-tests.ts
@@ -2,16 +2,16 @@ import normalizeUrl = require('normalize-url');
 
 let str: string;
 str = normalizeUrl('sindresorhus.com');
-str = normalizeUrl('HTTP://xn--xample-hva.com:80/?b=bar&a=foo');
+str = normalizeUrl('HTTP://xn--xample-hva.com:80/?b=bar&a=foo#contact');
 
+normalizeUrl('sindresorhus.com:80/', {defaultProtocol: 'https:'});
 normalizeUrl('//sindresorhus.com:80/', {normalizeProtocol: false});
-normalizeUrl('https://sindresorhus.com:80/', {normalizeHttps: true});
-normalizeUrl('sindresorhus.com/about.html#contact', {stripFragment: false});
+normalizeUrl('https://sindresorhus.com:80/', {forceHttp: true});
+normalizeUrl('http://sindresorhus.com:80/', {forceHttps: true});
+normalizeUrl('sindresorhus.com/about.html#contact', {stripHash: false});
 normalizeUrl('http://www.sindresorhus.com/about.html#contact', {stripWWW: false});
-normalizeUrl('www.sindresorhus.com?foo=bar&ref=test_ref', {
-    removeQueryParameters: ['ref', /test/]
-});
+normalizeUrl('www.sindresorhus.com?foo=bar&ref=test_ref', { removeQueryParameters: ['ref', /test/] });
 normalizeUrl('http://sindresorhus.com/', {removeTrailingSlash: false});
-normalizeUrl('www.sindresorhus.com/foo/default.php', {
-    removeDirectoryIndex: [/^default\.[a-z]+$/, 'foo']
-});
+normalizeUrl('www.sindresorhus.com/foo/default.php', { removeDirectoryIndex: [/^default\.[a-z]+$/, 'foo'] });
+normalizeUrl('www.sindresorhus.com/foo/index.php', { removeDirectoryIndex: true });
+normalizeUrl('www.sindresorhus.com?b=foo&a=bar', { sortQueryParameters: false });
