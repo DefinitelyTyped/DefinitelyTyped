76cb0a95c9ec11e43ba79a074d808be8e7944726
Daniel Perez Alvarez <danielpza@protonmail.com>
2018-11-02 18:07:39 -0400

    Update index.d.ts

--- types/prompts/index.d.ts
+++ types/prompts/index.d.ts
@@ -1,6 +1,7 @@
 // Type definitions for prompts 1.1
 // Project: https://github.com/terkelg/prompts
 // Definitions by: Berkay GURSOY <https://github.com/Berkays>
+//                 Daniel Perez Alvarez <https://github.com/danielpa9708>
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
 // TypeScript Version: 2.3
 
