6d184f9ca9d7adfaf1b7d61eb1e3611800040714
Everspace <7560635+Everspace@users.noreply.github.com>
2018-11-01 21:10:55 -0700

    Semicolons

--- types/react/index.d.ts
+++ types/react/index.d.ts
@@ -2324,7 +2324,7 @@ declare global {
         // tslint:disable-next-line:no-empty-interface
         interface IntrinsicClassAttributes<T> extends React.ClassAttributes<T> { }
 
-        type ElementAttributes<K extends keyof IntrinsicElements> = IntrinsicElements[K]
+        type ElementAttributes<K extends keyof IntrinsicElements> = IntrinsicElements[K];
         interface IntrinsicElements {
             // HTML
             a: React.DetailedHTMLProps<React.AnchorHTMLAttributes<HTMLAnchorElement>, HTMLAnchorElement>;
--- types/react/v15/index.d.ts
+++ types/react/v15/index.d.ts
@@ -3633,7 +3633,7 @@ declare global {
         // tslint:disable-next-line:no-empty-interface
         interface IntrinsicClassAttributes<T> extends React.ClassAttributes<T> { }
 
-        type ElementAttributes<K extends keyof IntrinsicElements> = IntrinsicElements[K]
+        type ElementAttributes<K extends keyof IntrinsicElements> = IntrinsicElements[K];
         interface IntrinsicElements {
             // HTML
             a: React.DetailedHTMLProps<React.AnchorHTMLAttributes<HTMLAnchorElement>, HTMLAnchorElement>;
