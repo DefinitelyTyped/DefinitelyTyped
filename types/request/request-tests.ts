fb73e71ee0eb490a8b5fddabd7b3a5e623f664e5
Leo Liang <leo.liang@ericsson.com>
2018-11-02 17:11:34 +0800

    fix lint error

--- types/request/request-tests.ts
+++ types/request/request-tests.ts
@@ -626,25 +626,26 @@ request.get('http://10.255.255.1', {timeout: 1500}, (err) => {
 });
 
 const rand = Math.floor(Math.random() * 100000000).toString();
-  request(
-    { method: 'PUT'
-    , uri: 'http://mikeal.iriscouch.com/testjs/' + rand
-    , multipart:
-      [ { headers: { 'content-type': 'application/json' }
-        , body: JSON.stringify({foo: 'bar', _attachments: {'message.txt': {follows: true, length: 18, content_type: 'text/plain' }}})
-        }
-      , { body: 'I am an attachment' }
-      ]
+
+request(
+{ method: 'PUT'
+, uri: 'http://mikeal.iriscouch.com/testjs/' + rand
+, multipart:
+    [ { headers: { 'content-type': 'application/json' }
+    , body: JSON.stringify({foo: 'bar', _attachments: {'message.txt': {follows: true, length: 18, content_type: 'text/plain' }}})
     }
-  , (error, response, body) => {
-      if (response.statusCode === 201) {
-        console.log('document saved as: http://mikeal.iriscouch.com/testjs/' + rand);
-      } else {
-        console.log('error: ' + response.statusCode);
-        console.log(body);
-      }
+    , { body: 'I am an attachment' }
+    ]
+}
+, (error, response, body) => {
+    if (response.statusCode === 201) {
+    console.log('document saved as: http://mikeal.iriscouch.com/testjs/' + rand);
+    } else {
+    console.log('error: ' + response.statusCode);
+    console.log(body);
     }
-  );
+}
+);
 
 request(
     { method: 'GET'
