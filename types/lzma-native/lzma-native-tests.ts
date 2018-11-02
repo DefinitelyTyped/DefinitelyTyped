2607ff16899410f665872764468e0c17285edfb3
Evan Cameron <evan@vectorface.com>
2018-11-02 16:36:07 -0400

    Fix lint errors

--- types/lzma-native/lzma-native-tests.ts
+++ types/lzma-native/lzma-native-tests.ts
@@ -1,32 +1,31 @@
 import * as lzma from "lzma-native";
-import { CHECK_CRC32 } from "lzma-native";
-var fs = require("fs");
+import * as fs from "fs";
 
-var compressor = lzma.createCompressor();
-var input = fs.createReadStream("tsconfig.json");
-var output = fs.createWriteStream("tsconfig.json.xz");
+const compressor = lzma.createCompressor();
+const input = fs.createReadStream("tsconfig.json");
+const output = fs.createWriteStream("tsconfig.json.xz");
 
 input.pipe(compressor).pipe(output);
 
-lzma.compress("Banana", undefined, function(result) {
+lzma.compress("Banana", undefined, result => {
     console.log(result); // <Buffer fd 37 7a 58 5a 00 00 01 69 22 de 36 02 00 21 ...>
 });
 
-lzma.compress("Bananas", 6, function(result) {
-    lzma.decompress(result, undefined, function(decompressedResult) {
-        console.log(decompressedResult.toString() == "Bananas");
+lzma.compress("Bananas", 6, result => {
+    lzma.decompress(result, undefined, decompressedResult => {
+        console.log(decompressedResult.toString() === "Bananas");
     });
 });
 
-lzma.LZMA().compress("Bananas", 4, function(result) {
-    lzma.LZMA().decompress(result, function(decompressedResult) {
-        console.log("Bananas" == decompressedResult.toString());
+lzma.LZMA().compress("Bananas", 4, result => {
+    lzma.LZMA().decompress(result, decompressedResult => {
+        console.log("Bananas" === decompressedResult.toString());
     });
 });
 
-var compressor = lzma.Compressor();
+const comp = lzma.Compressor();
 
-process.stdin.pipe(compressor).pipe(process.stdout);
+process.stdin.pipe(comp).pipe(process.stdout);
 lzma.crc32("Banana"); // => 69690105
 lzma.checkSize("CHECK_SHA256"); // => 16
 lzma.checkSize("CHECK_CRC32"); // => 4
@@ -36,16 +35,17 @@ lzma.versionString(); // => '5.2.3'
 lzma.versionNumber(); // => 50020012
 lzma.isXZ("Banana"); // => false
 
-fs.open("test/hamlet.txt.xz", "r", function(err: string, fd: number) {
+fs.open("test/hamlet.txt.xz", "r", (err: any, fd: number) => {
+    if (err) return;
     // handle error
-    lzma.parseFileIndexFD(fd, function(err, info) {
+    lzma.parseFileIndexFD(fd, (err, info) => {
         // handle error
         if (err) {
             console.log(err);
         }
         // do something with e.g. info.uncompressedSize
 
-        fs.close(fd, function(err: any) {
+        fs.close(fd, (err: any) => {
             /* handle error */
             if (err) {
                 console.log(err);
