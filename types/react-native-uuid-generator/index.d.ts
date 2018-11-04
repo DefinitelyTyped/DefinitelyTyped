31ad9b4fa2fdc0b47df683a7ed4c730ccb2e8a60
Bartosz Dotryw <burtekdotryw@gmail.com>
2018-11-04 23:58:47 +0100

    Changed author's name

--- types/react-native-uuid-generator/index.d.ts
+++ types/react-native-uuid-generator/index.d.ts
@@ -1,6 +1,6 @@
 // Type definitions for react-native-uuid-generator 4.0
 // Project: https://github.com/Traviskn/react-native-uuid-generator#readme
-// Definitions by: Bartosz Dotryw <https://github.com/burtek>
+// Definitions by: burtek <https://github.com/burtek>
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
 
 declare namespace UUIDGenerator {
