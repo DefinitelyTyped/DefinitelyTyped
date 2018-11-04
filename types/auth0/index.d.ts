539220016ae821540c339352487c2ef53b873b65
Wilson Hobbs <wilsonhobbs1@gmail.com>
2018-11-04 12:33:29 -0500

    Remove amiram

--- types/auth0/index.d.ts
+++ types/auth0/index.d.ts
@@ -1,6 +1,6 @@
 // Type definitions for auth0 2.9.2
 // Project: https://github.com/auth0/node-auth0
-// Definitions by: Seth Westphal <https://github.com/westy92>, Amiram Korach <https://github.com/amiram>
+// Definitions by: Seth Westphal <https://github.com/westy92>
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
 // TypeScript Version: 2.8
 
