396819ef8ca2b34d9d02e8a2757fa3c307d8fc12
Devilsparta <devilsparta@live.cn>
2018-11-05 21:00:06 +0800

    Update three-fbxloader.d.ts
    add animation for result value

--- types/three/three-fbxloader.d.ts
+++ types/three/three-fbxloader.d.ts
@@ -3,12 +3,15 @@
 import { LoadingManager } from "./three-core";
 import {Group} from "./three-core";
 
+interface ILoadedModel extends Group {
+	animations?: AnimationClip[];
+}
 export class FBXLoader {
 
     constructor(manager?: LoadingManager);
     manager: LoadingManager;
 
-    load(url: string, onLoad: (group: Group) => void, onProgress?: (event: ProgressEvent) => void, onError?: (event: ErrorEvent) => void): void;
+    load(url: string, onLoad: (group: ILoadedModel) => void, onProgress?: (event: ProgressEvent) => void, onError?: (event: ErrorEvent) => void): void;
     parse(FBXText: string, resourceDirectory: string) : Group;
 
 }
