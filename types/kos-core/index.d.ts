6c3dbb98745b46a8f068b4707d7de774d997c58e
Li Zhen <leejaen@gmail.com>
2018-11-05 16:58:51 +0800

    Update version to 0.4

--- types/kos-core/index.d.ts
+++ types/kos-core/index.d.ts
@@ -1,4 +1,4 @@
-// Type definitions for kos-core 0.3
+// Type definitions for kos-core 0.4
 // Project: https://github.com/ali-Kos/Kos
 // Definitions by: alibaba ali-Kos <https://github.com/ali-Kos>
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
