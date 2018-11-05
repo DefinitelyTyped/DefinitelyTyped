34ea6caa86b3b2f9be4650997565696dddfd6609
Andrei Horodinca <andrei.horodinca@gmail.com>
2018-11-05 16:53:13 +0100

    Add TypeScript version

--- types/smooth-scroll/index.d.ts
+++ types/smooth-scroll/index.d.ts
@@ -2,6 +2,7 @@
 // Project: https://github.com/cferdinandi/smooth-scroll
 // Definitions by: Andrei Horodinca <https://github.com/andreiho>
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
+// TypeScript Version: 2.1
 
 export as namespace SmoothScroll;
 
