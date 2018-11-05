035d8a9db936d25197bc6a056bcb02c4154680e3
Pavlos Vinieratos <pvinis@gmail.com>
2018-11-05 14:34:57 +0100

    Add typings for react-native-safe-area

---
+++ types/react-native-safe-area/index.d.ts
@@ -0,0 +1,35 @@
+// Type definitions for react-native-safe-area 0.5
+// Project: https://github.com/miyabi/react-native-safe-area#readme
+// Definitions by: Pavlos Vinieratos <https://github.com/pvinis>
+// Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
+
+/// <reference types="react" />
+
+declare module 'react-native-safe-area' {
+    type Direction =
+        | 'top'
+        | 'bottom'
+        | 'left'
+        | 'right'
+        | 'topLeft' // DEPRECATED
+        | 'topAndLeft'
+        | 'topRight' // DEPRECATED
+        | 'topAndRight'
+        | 'bottomLeft' // DEPRECATED
+        | 'bottomAndLeft'
+        | 'bottomRight' // DEPRECATED
+        | 'bottomAndRight'
+        | 'vertical'
+        | 'horizontal'
+        | 'verticalAndLeft'
+        | 'verticalAndRight'
+        | 'horizontalAndTop'
+        | 'horizontalAndBottom'
+        | 'all';
+
+    export function withSafeArea(
+        WrappedComponent: React.ComponentType<any>,
+        applyTo: 'margin' | 'padding' | 'absolutePosition' | 'contentInset' = 'margin',
+        direction: Direction = 'all',
+    ): React.ComponentType<any>;
+}
