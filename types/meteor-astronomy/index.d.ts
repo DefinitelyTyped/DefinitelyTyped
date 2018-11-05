a6a9e871a055704a8575dd97b1b9a4b443ad8270
Deadly0 <xdeadlyx@ya.ru>
2018-11-05 10:13:25 +0300

    Move global types to namespace.

--- types/meteor-astronomy/index.d.ts
+++ types/meteor-astronomy/index.d.ts
@@ -6,102 +6,104 @@
 
 /// <reference types="meteor" />
 
-type TypeOptionsPrimitives = typeof String | typeof Date | typeof Boolean | typeof Object | typeof Number;
-type TypeOptions = TypeOptionsPrimitives | TypeOptionsPrimitives[] | Class<any> | Enum<any>;
-type MongoQuery = object | string;
+declare namespace MeteorAstronomy {
+    type TypeOptionsPrimitives = typeof String | typeof Date | typeof Boolean | typeof Object | typeof Number;
+    type TypeOptions = TypeOptionsPrimitives | TypeOptionsPrimitives[] | Class<any> | Enum<any>;
+    type MongoQuery = object | string;
 
-interface SaveAndValidateOptions<K> {
-    fields?: K[];
-    stopOnFirstError?: boolean;
-    simulation?: boolean;
-    cast?: boolean;
-}
+    interface SaveAndValidateOptions<K> {
+        fields?: K[];
+        stopOnFirstError?: boolean;
+        simulation?: boolean;
+        cast?: boolean;
+    }
 
-type SaveAndValidateCallback = (err: any, id: any) => void;
-type RemoveCallback = (err: any, result: any) => void;
+    type SaveAndValidateCallback = (err: any, id: any) => void;
+    type RemoveCallback = (err: any, result: any) => void;
 
-interface Validator {
-    type: string;
-    param: any;
-}
+    interface Validator {
+        type: string;
+        param: any;
+    }
 
-interface ModelFullField<Field, Doc> {
-    type: TypeOptions;
-    optional?: boolean;
-    transient?: boolean;
-    immutable?: boolean;
-    default?: () => Field | Field;
-    index?: string | number;
-    validators?: Validator[];
-    resolve?: (doc: Doc) => Field;
-}
+    interface ModelFullField<Field, Doc> {
+        type: TypeOptions;
+        optional?: boolean;
+        transient?: boolean;
+        immutable?: boolean;
+        default?: () => Field | Field;
+        index?: string | number;
+        validators?: Validator[];
+        resolve?: (doc: Doc) => Field;
+    }
 
-type ModelField<Field, Doc> = ModelFullField<Field, Doc> | TypeOptions;
+    type ModelField<Field, Doc> = ModelFullField<Field, Doc> | TypeOptions;
 
-type Fields<T> = {
-    [P in keyof T]: ModelField<T[P], T>;
-};
+    type Fields<T> = {
+        [P in keyof T]: ModelField<T[P], T>;
+    };
 
-interface ClassModel<T> {
-    name: string;
-    collection?: Mongo.Collection<T>;
-    fields: Fields<T>;
-    behaviors?: object;
-    secured?: {
-        insert: boolean,
-        update: boolean,
-        remove: boolean,
-    } | boolean;
-    helpers?: object;
-    events?: object;
-    meteorMethods?: object;
-    indexes?: object;
-}
+    interface ClassModel<T> {
+        name: string;
+        collection?: Mongo.Collection<T>;
+        fields: Fields<T>;
+        behaviors?: object;
+        secured?: {
+            insert: boolean,
+            update: boolean,
+            remove: boolean,
+        } | boolean;
+        helpers?: object;
+        events?: object;
+        meteorMethods?: object;
+        indexes?: object;
+    }
 
-interface EnumModel<T> {
-    name: string;
-    identifiers: T[] | object;
-}
+    interface EnumModel<T> {
+        name: string;
+        identifiers: T[] | object;
+    }
 
-type Model<T> = T & {
-    set(fields: Partial<T>, options?: {cast?: boolean; clone?: boolean; merge?: boolean}): void;
-    set(field: string, value: any): void;
-    get(field: string): any;
-    get(fields: string[]): any[];
-    isModified(field?: string): boolean;
-    getModified(): any;
-    getModifiedValues(options?: {old?: boolean, raw?: boolean}): Partial<T>;
-    getModifier(): any;
-    raw(): T;
-    raw(field: string): any;
-    raw(fields: string[]): any[];
-    save(options?: SaveAndValidateOptions<keyof T>, callback?: SaveAndValidateCallback): void;
-    save(callback?: SaveAndValidateCallback): void;
-    copy(save: boolean): any;
-    validate(options?: SaveAndValidateOptions<keyof T>, callback?: SaveAndValidateCallback): void;
-    validate(callback?: SaveAndValidateCallback): void;
-    remove(callback?: RemoveCallback): void;
-};
+    type Model<T> = T & {
+        set(fields: Partial<T>, options?: {cast?: boolean; clone?: boolean; merge?: boolean}): void;
+        set(field: string, value: any): void;
+        get(field: string): any;
+        get(fields: string[]): any[];
+        isModified(field?: string): boolean;
+        getModified(): any;
+        getModifiedValues(options?: {old?: boolean, raw?: boolean}): Partial<T>;
+        getModifier(): any;
+        raw(): T;
+        raw(field: string): any;
+        raw(fields: string[]): any[];
+        save(options?: SaveAndValidateOptions<keyof T>, callback?: SaveAndValidateCallback): void;
+        save(callback?: SaveAndValidateCallback): void;
+        copy(save: boolean): any;
+        validate(options?: SaveAndValidateOptions<keyof T>, callback?: SaveAndValidateCallback): void;
+        validate(callback?: SaveAndValidateCallback): void;
+        remove(callback?: RemoveCallback): void;
+    };
 
-interface Class<T> {
-    new(data?: Partial<T>): Model<T>;
+    interface Class<T> {
+        new(data?: Partial<T>): Model<T>;
 
-    findOne(query?: MongoQuery): Model<T>;
-    find(query?: MongoQuery): Array<Model<T>>;
-    update(search: object | string, query: object, callback?: () => void): void;
-}
+        findOne(query?: MongoQuery): Model<T>;
+        find(query?: MongoQuery): Array<Model<T>>;
+        update(search: object | string, query: object, callback?: () => void): void;
+    }
 
-interface Enum<T> {
-    getValues(): any[];
-    getIdentifier(identifier: T): any;
+    interface Enum<T> {
+        getValues(): any[];
+        getIdentifier(identifier: T): any;
+    }
 }
 
 declare module 'meteor/jagi:astronomy' { // tslint:disable-line:no-single-declare-module
     namespace Class {
-        function create<T extends {}>(model: ClassModel<T>): Class<T>;
+        function create<T extends {}>(model: MeteorAstronomy.ClassModel<T>): MeteorAstronomy.Class<T>;
     }
 
     namespace Enum {
-        function create<T>(model: EnumModel<T>): Enum<T>;
+        function create<T>(model: MeteorAstronomy.EnumModel<T>): MeteorAstronomy.Enum<T>;
     }
 }
