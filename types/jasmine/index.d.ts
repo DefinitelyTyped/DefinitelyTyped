e9e9c365effbccdac4304f4fc473a6d96f16ea9d
Roaders <giles@roadnight.name>
2018-11-06 11:35:20 +0000

    Update index.d.ts

--- types/jasmine/index.d.ts
+++ types/jasmine/index.d.ts
@@ -1,6 +1,5 @@
 // Type definitions for Jasmine 3.3.0
 // Project: http://jasmine.github.io/
-
 // Definitions by: Boris Yankov <https://github.com/borisyankov>
 //                 Theodore Brown <https://github.com/theodorejb>
 //                 David Pärsson <https://github.com/davidparsson>
@@ -9,7 +8,6 @@
 //                 Boris Breuer <https://github.com/Engineer2B>
 //                 Chris Yungmann <https://github.com/cyungmann>
 //                 Giles Roadnight <https://github.com/Roaders>
-
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
 // TypeScript Version: 2.8
 // For ddescribe / iit use : https://github.com/DefinitelyTyped/DefinitelyTyped/blob/master/karma-jasmine/karma-jasmine.d.ts
