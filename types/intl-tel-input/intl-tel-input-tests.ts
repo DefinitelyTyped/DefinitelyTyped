c030374c27147fbfb75803a7b3a33dfed7022afc
Márton Molnár <molnar.marton@gmail.com>
2018-11-06 13:24:09 +0100

    moved jQuery definitions into same file

--- types/intl-tel-input/intl-tel-input-tests.ts
+++ types/intl-tel-input/intl-tel-input-tests.ts
@@ -1,3 +1,7 @@
+/// <reference types="jquery" />
+
+// Normal version
+
 const input = document.querySelector("#phone");
 window.intlTelInput(input);
 
@@ -73,6 +77,86 @@ window.intlTelInput(input, {
 });
 
 window.intlTelInput(input, {
+    allowDropdown: false,
+    autoHideDialCode: false,
+    autoPlaceholder: "aggressive",
+    dropdownContainer: document.body,
+    excludeCountries: ["us", "uk"],
+    formatOnDisplay: false,
+    hiddenInput: "hidden-input",
+    localizedCountries: { de: "Deutschland" },
+    preferredCountries: ["us", "gb"],
+    separateDialCode: false
+});
+
+// jQuery-wrapped version
+
+$('#phone').intlTelInput();
+
+$('#phone').intlTelInput({
+    customPlaceholder: (selectedCountryPlaceholder, selectedCountryData) => {
+        return 'e.g. ' + selectedCountryPlaceholder;
+    }
+});
+
+$('#phone').intlTelInput({
+    placeholderNumberType: intlTelInputUtils.numberType.MOBILE,
+});
+
+$('#phone').intlTelInput({
+    geoIpLookup: (callback) => {
+        const countryCode = 'XY';
+        callback(countryCode);
+    }
+});
+
+$('#phone').intlTelInput('destroy');
+
+const jqueryExtension = $('#phone').intlTelInput('getExtension');
+
+const jqueryIntlNumber = $('#phone').intlTelInput('getNumber');
+const jqueryNtlNumber = $('#phone').intlTelInput('getNumber', intlTelInputUtils.numberFormat.NATIONAL);
+
+const jqueryNumberType = $('#phone').intlTelInput('getNumberType');
+if (numberType === intlTelInputUtils.numberType.MOBILE) { }
+
+const jquerySelectedCountryData = $('#phone').intlTelInput('getSelectedCountryData');
+
+const jqueryError = $('#phone').intlTelInput('getValidationError');
+if (error === intlTelInputUtils.validationError.TOO_SHORT) { }
+
+const jqueryIsValid = $('#phone').intlTelInput('isValidNumber');
+
+$('#phone').intlTelInput('setCountry', 'gb');
+
+$('#phone').intlTelInput('setNumber', '+447733123456');
+
+$('#phone').intlTelInput('setPlaceholderNumberType', intlTelInputUtils.numberType.FIXED_LINE);
+
+$('#phone').intlTelInput({
+    utilsScript: '../../build/js/utils.js'
+});
+
+$('#phone').intlTelInput({
+    initialCountry: 'auto',
+    geoIpLookup: (callback) => {
+        const countryCode = 'XY';
+        callback(countryCode);
+    },
+    utilsScript: '../../build/js/utils.js'
+});
+
+$('#phone').intlTelInput({
+    nationalMode: true,
+    utilsScript: '../../build/js/utils.js'
+});
+
+$('#phone').intlTelInput({
+    onlyCountries: ['al'],
+    utilsScript: '../../build/js/utils.js'
+});
+
+$('#phone').intlTelInput({
     allowDropdown: false,
     autoHideDialCode: false,
     autoPlaceholder: "aggressive",
