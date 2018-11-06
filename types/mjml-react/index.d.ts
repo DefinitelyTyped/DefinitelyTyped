38319c9163cf632a6fa12d3f1675b9ac0779c1db
Henri Normak <henri.normak@gmail.com>
2018-11-06 16:38:09 +0200

    Use correct version of typescript

--- types/mjml-react/index.d.ts
+++ types/mjml-react/index.d.ts
@@ -2,7 +2,7 @@
 // Project: https://github.com/wix-incubator/mjml-react
 // Definitions by: Henri Normak <https://github.com/henrinormak>
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
-// TypeScript Version: 2.3
+// TypeScript Version: 2.8
 
 import * as React from 'react';
 
