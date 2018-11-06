749614f8af4f8911007a13f78d694c7cebed7b43
Márton Molnár <molnar.marton@gmail.com>
2018-11-06 13:28:53 +0100

    added version header to v13

--- types/intl-tel-input/v13/index.d.ts
+++ types/intl-tel-input/v13/index.d.ts
@@ -1,4 +1,4 @@
-// Type definitions for intl-tel-input
+// Type definitions for intl-tel-input 13.0
 // Project: https://github.com/jackocnr/intl-tel-input
 // Definitions by: Fidan Hakaj <https://github.com/fdnhkj>, Leonard Thieu <https://github.com/leonard-thieu>
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
