06f6b1d872029dffa376c0dee026dd2fb6517165
JuanJoseGonGi <juanjose.eng@gmail.com>
2018-11-07 14:27:29 -0500

    Fixed version

--- types/critters-webpack-plugin/index.d.ts
+++ types/critters-webpack-plugin/index.d.ts
@@ -1,4 +1,4 @@
-// Type definitions for critters-webpack-plugin 2.0.1
+// Type definitions for critters-webpack-plugin 2.0
 // Project: https://github.com/GoogleChromeLabs/critters
 // Definitions by: Juan José González Giraldo <https://github.com/JuanJoseGonGi>
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
