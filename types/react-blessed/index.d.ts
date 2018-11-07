b120dc36c9a4a10522a67025be9474dead058547
guoshencheng <648772021@qq.com>
2018-11-07 10:27:13 +0800

    add typescript version

--- types/react-blessed/index.d.ts
+++ types/react-blessed/index.d.ts
@@ -2,6 +2,7 @@
 // Project: https://github.com/yomguithereal/react-blessed#readme
 // Definitions by: My Self <https://github.com/me>
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
+// TypeScript Version: 2.8
 
 declare module 'react-blessed' {
     import * as React from 'react';
