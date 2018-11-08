991911f933c06b2f80feb6945255c6c33485dc31
tofu_on_fire <kazakami@kmc.gr.jp>
2018-11-08 17:42:23 +0900

    Modify type of onLoad callback argument

--- types/three/three-gltfloader.d.ts
+++ types/three/three-gltfloader.d.ts
@@ -1,7 +1,17 @@
 // https://github.com/mrdoob/three.js/blob/master/examples/js/loaders/GLTFLoader.js
 
+import {AnimationClip} from "./three-core";
+import {Camera} from "./three-core";
 import {LoadingManager} from "./three-core";
-import {Group} from "./three-core";
+import {Scene} from "./three-core";
+
+export class GLTF {
+    animations: AnimationClip[];
+    scene: Scene;
+    scenes: Scene[];
+    cameras: Camera[];
+    asset: object;
+}
 
 export class GLTFLoader {
 
@@ -9,10 +19,11 @@ export class GLTFLoader {
     manager: LoadingManager;
     path: string;
 
-    load(url: string, onLoad: (group: Group) => void, onProgress?: (event: ProgressEvent) => void, onError?: (event: ErrorEvent) => void) : void;
+    load(url: string, onLoad: (gltf: GLTF) => void, onProgress?: (event: ProgressEvent) => void, onError?: (event: ErrorEvent) => void) : void;
     setPath(path: string) : GLTFLoader;
     setResourcePath(path: string) : GLTFLoader;
     setCrossOrigin(value: string): void;
     setDRACOLoader(dracoLoader: object): void;
-    parse(data: ArrayBuffer, path: string, onLoad: (group: Group) => void, onError?: (event: ErrorEvent) => void) : void;
+    parse(data: ArrayBuffer, path: string, onLoad: (gltf: GLTF) => void, onError?: (event: ErrorEvent) => void) : void;
+
 }
