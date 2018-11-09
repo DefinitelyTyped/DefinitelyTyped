674faabadfe0c0378181b51033ba278a17566a3d
Stefan Mansson <st.mansson@icloud.com>
2018-11-09 11:23:18 +0200

    Increment flowdoc version

--- types/flowdoc/index.d.ts
+++ types/flowdoc/index.d.ts
@@ -1,4 +1,4 @@
-// Type definitions for flowdoc 1.0
+// Type definitions for flowdoc 1.1
 // Project: https://github.com/overflowapp/Flow
 // Definitions by: Stefan Mansson <https://github.com/animify>
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
