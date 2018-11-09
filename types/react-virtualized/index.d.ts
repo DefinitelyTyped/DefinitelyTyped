491bc63faba44db7e25335d8051cd8d0562a9a3f
Sebastian Busch <s.busch@qbus-enet.de>
2018-11-09 11:59:07 +0100

    List calls rowRenderer with rowIndex renamed to index. Also dont redefine style prop
    * So we omit rowIndex from GridCellProps and add index with same type
    * ListRowProps already inherit style prop from GridCellProps

--- types/react-virtualized/index.d.ts
+++ types/react-virtualized/index.d.ts
@@ -8,6 +8,7 @@
 //                 Steve Zhang <https://github.com/Stevearzh>
 //                 Maciej Goszczycki <https://github.com/mgoszcz2>
 //                 Brandon Hall <https://github.com/brandonhall>
+//                 Sebastian Busch <https://github.com/sbusch>
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
 // TypeScript Version: 2.8
 
