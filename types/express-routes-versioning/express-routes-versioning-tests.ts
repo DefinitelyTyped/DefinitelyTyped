3a93862a6aef352547cade95c48d5b8907dc0149
Weffe <rogelio_negrete@live.com>
2018-11-09 11:05:56 -0800

    Update test file

--- types/express-routes-versioning/express-routes-versioning-tests.ts
+++ types/express-routes-versioning/express-routes-versioning-tests.ts
@@ -1,15 +1,16 @@
 import * as express from 'express';
-import routesVersioning from 'express-routes-versioning';
-
-const routesVersioningMiddleware = routesVersioning();
+import routesVersioning, { Options, RoutesVersioningMiddleware } from 'express-routes-versioning';
 
 const app = express();
-app.listen(3000);
 
-app.get('/test', routesVersioningMiddleware({
+const routesVersioningMiddleware: RoutesVersioningMiddleware = routesVersioning();
+
+const versioningOptions: Options = {
     "1.0.0": respondV1,
     "~2.2.1": respondV2
-}));
+};
+
+app.get('/test', routesVersioningMiddleware(versioningOptions));
 
 function respondV1(req: express.Request, res: express.Response, next: express.NextFunction) {
     res.status(200).send('ok v1');
