fc82df4868b78ce0656ed7423f77e5312cf4a6fa
Sean Zhu <interestinglythere@gmail.com>
2018-11-09 18:38:23 -0800

    [await-timeout] // TypeScript Version: 2.1

--- types/await-timeout/index.d.ts
+++ types/await-timeout/index.d.ts
@@ -2,6 +2,7 @@
 // Project: https://github.com/vitalets/await-timeout
 // Definitions by: Sean Zhu <https://github.com/szhu>
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
+// TypeScript Version: 2.1
 
 export default Timeout;
 
