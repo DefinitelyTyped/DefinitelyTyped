e9b486b9065591cb640120fbd92e1d1ad9c39159
Lucas <git@roang.info>
2018-11-10 20:22:08 +0100

    Remove unnecessary import

--- types/kdbxweb/index.d.ts
+++ types/kdbxweb/index.d.ts
@@ -3,8 +3,6 @@
 // Definitions by: Roang-zero1 <https://github.com/Roang-zero1>
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
 
-import Crypto from "crypto";
-
 /*~ If this module is a UMD module that exposes a global variable 'myLib' when
  *~ loaded outside a module loader environment, declare that global here.
  *~ Otherwise, delete this declaration.
