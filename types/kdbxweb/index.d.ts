13502ed0fd81121e1206821b520f29a1587bfc83
Lucas <git@roang.info>
2018-11-10 20:49:16 +0100

    Update header

--- types/kdbxweb/index.d.ts
+++ types/kdbxweb/index.d.ts
@@ -2,11 +2,8 @@
 // Project: https://github.com/keeweb/kdbxweb#readme
 // Definitions by: Roang-zero1 <https://github.com/Roang-zero1>
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
+// TypeScript Version: 2.2' just under the header
 
-/*~ If this module is a UMD module that exposes a global variable 'myLib' when
- *~ loaded outside a module loader environment, declare that global here.
- *~ Otherwise, delete this declaration.
- */
 export as namespace kdbxweb;
 
 export type KdbxObject = Entry | Group;
