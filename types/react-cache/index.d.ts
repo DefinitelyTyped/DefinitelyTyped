7d819d0520bc716a134d08fc1e993f8a615f6ceb
Spencer Miskoviak <smiskoviak@gmail.com>
2018-11-11 08:09:04 -0800

    Resolve linting warnings

--- types/react-cache/index.d.ts
+++ types/react-cache/index.d.ts
@@ -1,18 +1,18 @@
-// Type definitions for react-cache 2.0.0-alpha.1
+// Type definitions for react-cache 2.0
 // Project: https://github.com/facebook/react/tree/master/packages/react-cache
 // Definitions by: Spencer Miskoviak <https://github.com/skovy>
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
 
-type Thenable<Value> = {
+export interface Thenable<Value> {
   then(resolve: (value: Value) => any, reject: (reason: any) => any): any;
-};
+}
 
-type Resource<Input, Value> = {
+export interface Resource<Input, Value> {
   read(key: Input): Value;
   preload(key: Input): void;
-};
+}
 
-export function unstable_createResource<Input, Key extends string | number, Value>(
+export function unstable_createResource<Input, Value>(
   fetch: (input: Input) => Thenable<Value>,
-  maybeHashInput?: (input: Input) => Key
+  maybeHashInput?: (input: Input) => string | number
 ): Resource<Input, Value>;
