aad3676bc4a7c159a79fbb26128354277e6ec708
Grant Timmerman <timmerman+devrel@google.com>
2018-11-11 01:01:01 +0100

    Move google-apps-script to script

---
+++ types/script/index.d.ts
@@ -0,0 +1,75 @@
+// Type definitions for script
+// Project: https://developers.google.com/apps-script/
+// Definitions by: motemen <https://github.com/motemen>, grant <https://github.com/grant>
+// Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
+// TypeScript Version: 2.2
+
+// Base Types
+/// <reference path="base.d.ts"/>
+/// <reference path="cache.d.ts"/>
+/// <reference path="types.d.ts"/>
+
+// Google Services (Google APIs)
+/// <reference path="calendar.d.ts"/>
+/// <reference path="card.d.ts"/>
+/// <reference path="charts.d.ts"/>
+/// <reference path="contacts.d.ts"/>
+/// <reference path="content.d.ts"/>
+/// <reference path="document.d.ts"/>
+/// <reference path="drive.d.ts"/>
+/// <reference path="forms.d.ts"/>
+/// <reference path="gmail.d.ts"/>
+/// <reference path="groups.d.ts"/>
+/// <reference path="mail.d.ts"/>
+/// <reference path="maps.d.ts"/>
+/// <reference path="sites.d.ts"/>
+/// <reference path="slides.d.ts"/>
+/// <reference path="spreadsheet.d.ts"/>
+
+// Script Services (Other *App / *Service)
+/// <reference path="html.d.ts"/>
+/// <reference path="jdbc.d.ts"/>
+/// <reference path="language.d.ts"/>
+/// <reference path="lock.d.ts"/>
+/// <reference path="properties.d.ts"/>
+/// <reference path="optimization.d.ts"/>
+/// <reference path="script.d.ts"/>
+/// <reference path="ui.d.ts"/>
+/// <reference path="url-fetch.d.ts"/>
+/// <reference path="utilities.d.ts"/>
+/// <reference path="xml-service.d.ts"/>
+
+// API Types (Advanced Google Services)
+/// <reference path="apis/adsense/v1.4.d.ts"/>
+/// <reference path="apis/admin/directory_v1.d.ts"/>
+/// <reference path="apis/admin/reports_v1.d.ts"/>
+/// <reference path="apis/bigquery/v2.d.ts"/>
+/// <reference path="apis/calendar/v3.d.ts"/>
+/// <reference path="apis/content/v2.d.ts"/>
+/// <reference path="apis/dfareporting/v2.8.d.ts"/>
+/// <reference path="apis/dfareporting/v3.0.d.ts"/>
+/// <reference path="apis/dfareporting/v3.1.d.ts"/>
+/// <reference path="apis/dfareporting/v3.2.d.ts"/>
+/// <reference path="apis/drive/v2.d.ts"/>
+/// <reference path="apis/appsactivity/v1.d.ts"/>
+/// <reference path="apis/reseller/v1.d.ts"/>
+/// <reference path="apis/licensing/v1.d.ts"/>
+/// <reference path="apis/fusiontables/v1.d.ts"/>
+/// <reference path="apis/fusiontables/v2.d.ts"/>
+/// <reference path="apis/gmail/v1.d.ts"/>
+/// <reference path="apis/analytics/v3.d.ts"/>
+/// <reference path="apis/analyticsreporting/v4.d.ts"/>
+/// <reference path="apis/classroom/v1.d.ts"/>
+/// <reference path="apis/mirror/v1.d.ts"/>
+/// <reference path="apis/sheets/v4.d.ts"/>
+/// <reference path="apis/slides/v1.d.ts"/>
+/// <reference path="apis/plus/v1.d.ts"/>
+/// <reference path="apis/plusDomains/v1.d.ts"/>
+/// <reference path="apis/groupsmigration/v1.d.ts"/>
+/// <reference path="apis/groupssettings/v1.d.ts"/>
+/// <reference path="apis/tagmanager/v1.d.ts"/>
+/// <reference path="apis/tasks/v1.d.ts"/>
+/// <reference path="apis/urlshortener/v1.d.ts"/>
+/// <reference path="apis/youtubeAnalytics/v1.d.ts"/>
+/// <reference path="apis/youtube/v3.d.ts"/>
+/// <reference path="apis/youtubePartner/v1.d.ts"/>
