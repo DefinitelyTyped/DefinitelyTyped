c3194818a5d5952b1638fbdd304ded4b8c98fa38
John Wood <johnjwood@uk.ibm.com>
2018-11-11 02:35:40 +0000

    Bump typescript version

--- types/sinon/index.d.ts
+++ types/sinon/index.d.ts
@@ -9,7 +9,7 @@
 //                 Greg Jednaszewski <https://github.com/gjednaszewski>
 //                 John Wood <https://github.com/johnjesse>
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
-// TypeScript Version: 2.8
+// TypeScript Version: 3.0
 
 // sinon uses DOM dependencies which are absent in browser-less environment like node.js
 // to avoid compiler errors this monkey patch is used
