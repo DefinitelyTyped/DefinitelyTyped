d4111d2f7fe4270f0a5ef8b6179fc6fe9a7d11c4
Mike Lazer-Walker <michael@lazerwalker.com>
2018-11-11 23:10:09 +0100

    [javascript-astar] Add @lazerwalker to header

--- types/javascript-astar/index.d.ts
+++ types/javascript-astar/index.d.ts
@@ -1,6 +1,6 @@
 // Type definitions for javascript-astar
 // Project: https://github.com/bgrins/javascript-astar
-// Definitions by: brian ridley <https://github.com/ptlis>
+// Definitions by: brian ridley <https://github.com/ptlis>, Mike Lazer-Walker <https://github.com/lazerwalker>
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
 
 declare class Graph {
