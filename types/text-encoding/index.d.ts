51504860024a8c85d01104ce152968dcb0244b3c
Pine Mizune <pinemz@gmail.com>
2018-11-13 17:24:13 +0900

    Fix GitHub URL

--- types/text-encoding/index.d.ts
+++ types/text-encoding/index.d.ts
@@ -1,6 +1,6 @@
 // Type definitions for text-encoding
 // Project: https://github.com/inexorabletash/text-encoding
-// Definitions by: MIZUNE Pine <https://github.com/pine613>
+// Definitions by: Pine Mizune <https://github.com/pine>
 //                 Mohsen Azimi <https://github.com/mohsen1>
 //                  Thomas Nicollet <https://github.com/nwmqpa>
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
