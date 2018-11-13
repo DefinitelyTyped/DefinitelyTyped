4a528d7e0b1da537ff1b0718a9b24144be21fcca
Frank Li <frankvsense@gmail.com>
2018-11-13 12:44:43 -0800

    Update index.d.ts

--- types/element-resize-detector/index.d.ts
+++ types/element-resize-detector/index.d.ts
@@ -1,6 +1,7 @@
-// Type definitions for element-resize-detector 1.1
+// Type definitions for element-resize-detector 1.2
 // Project: https://github.com/wnr/element-resize-detector
 // Definitions by: Saransh Kataria <https://github.com/saranshkataria>
+//                 Frank Li <https://github.com/franklixuefei>
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
 
 export interface ElementResizeDetectorProps {
