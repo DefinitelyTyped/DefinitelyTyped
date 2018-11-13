458417f36157c7791dab26802034b7c7350dc2c3
Jessica <jessicamfranco3@gmail.com>
2018-11-13 20:04:27 +0900

    Bump version

--- types/styled-components/index.d.ts
+++ types/styled-components/index.d.ts
@@ -1,4 +1,4 @@
-// Type definitions for styled-components 4.0
+// Type definitions for styled-components 4.1
 // Project: https://github.com/styled-components/styled-components
 // Definitions by: Igor Oleinikov <https://github.com/Igorbek>
 //                 Ihor Chulinda <https://github.com/Igmat>
