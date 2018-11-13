ed8b337be0713c4cb4bb12313a6c04c1174c48c3
Pine Mizune <pinemz@gmail.com>
2018-11-13 17:24:41 +0900

    Fix indent

--- types/text-encoding/index.d.ts
+++ types/text-encoding/index.d.ts
@@ -2,7 +2,7 @@
 // Project: https://github.com/inexorabletash/text-encoding
 // Definitions by: Pine Mizune <https://github.com/pine>
 //                 Mohsen Azimi <https://github.com/mohsen1>
-//                  Thomas Nicollet <https://github.com/nwmqpa>
+//                 Thomas Nicollet <https://github.com/nwmqpa>
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
 // TypeScript Version: 2.8
 
