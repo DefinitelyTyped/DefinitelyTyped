67f14c9a73e33f25a4f5f284f71de9493500ca65
Sebastian Richter <sebastian.max.richter@gmail.com>
2018-11-13 10:46:14 +0100

    Reference lib "dom"
    Currently lib "dom" is required for compilation.
    
    However, including "dom" in lib can cause some opaque bugs on a server.
    
    For example, the TS compiler won't complain when using `fetch` without something like `import fetch from "node-fetch"` or using `URL` without `import { URL } from "url"`.

--- types/jsdom/index.d.ts
+++ types/jsdom/index.d.ts
@@ -5,6 +5,7 @@
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
 // TypeScript Version: 2.2
 
+/// <reference lib="dom" />
 /// <reference types="node" />
 
 import { EventEmitter } from 'events';
