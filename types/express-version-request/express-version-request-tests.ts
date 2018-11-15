a7ef2a96d5387c6e31ef49014718a0ca88553137
Weffe <rogelio_negrete@live.com>
2018-11-15 12:21:43 -0800

    fix: update and add missing function to tests.ts

--- types/express-version-request/express-version-request-tests.ts
+++ types/express-version-request/express-version-request-tests.ts
@@ -10,8 +10,13 @@ app.use(versionRequest.setVersionByHeader('My-HTTP-Header-Name'));
 
 app.use(versionRequest.setVersionByQueryParam());
 app.use(versionRequest.setVersionByQueryParam('myQueryParam'));
-app.use(versionRequest.setVersionByQueryParam('myQueryParam', { removeQueryParam: true }));
+
+const queryParamOptions: versionRequest.SetVersionByQueryParamOptions = { removeQueryParam: true };
+app.use(versionRequest.setVersionByQueryParam('myQueryParam', queryParamOptions));
 
 const customParsingFunction: versionRequest.CustomParsingFunction = (header) => "1.2.3";
 app.use(versionRequest.setVersionByAcceptHeader());
 app.use(versionRequest.setVersionByAcceptHeader(customParsingFunction));
+
+const formattedVersion1 = versionRequest.formatVersion("1.2.3.4.5");
+const formattedVersion2 = versionRequest.formatVersion({ v: '1.2.3.4.5' });
