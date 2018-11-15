3e5bfdadac3b0d8b22d1c948a496c42d4905c400
Nathan Shively-Sanders <293473+sandersn@users.noreply.github.com>
2018-11-15 14:17:07 -0800

    Fix react-native on TS 3.1+
    Fixes #29265, based on comment
    https://github.com/Microsoft/TypeScript/issues/27421#issuecomment-433249163

--- types/react-native/index.d.ts
+++ types/react-native/index.d.ts
@@ -5226,7 +5226,7 @@ export namespace StyleSheet {
     /**
      * Creates a StyleSheet style reference from the given object.
      */
-    export function create<T extends NamedStyles<T>>(styles: T): { [P in keyof T]: RegisteredStyle<T[P]> };
+    export function create<T extends NamedStyles<T> | NamedStyles<any>>(styles: T): { [P in keyof T]: RegisteredStyle<T[P]> };
 
     /**
      * Flattens an array of style objects, into one aggregated style object.
