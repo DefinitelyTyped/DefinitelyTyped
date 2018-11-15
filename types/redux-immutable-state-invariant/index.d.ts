a0df8f39a019eeded79d2a5c6abf1a90fd5b0f7a
Mihai Dinculescu <faithraven@gmail.com>
2018-11-15 12:07:20 +0000

    redux-immutable-state-invariant: redux 4 support.

--- types/redux-immutable-state-invariant/index.d.ts
+++ types/redux-immutable-state-invariant/index.d.ts
@@ -1,6 +1,6 @@
-// Type definitions for redux-immutable-state-invariant v2.0.0
+// Type definitions for redux-immutable-state-invariant v2.1.0
 // Project: https://github.com/leoasis/redux-immutable-state-invariant
-// Definitions by: Remo H. Jansen <https://github.com/remojansen>, Ben Rogers <https://github.com/highflying>
+// Definitions by: Remo H. Jansen <https://github.com/remojansen>, Ben Rogers <https://github.com/highflying>, Mihai Dinculescu <https://github.com/mihai-dinculescu>
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
 
 import * as Redux from "redux";
