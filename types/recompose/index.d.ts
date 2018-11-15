71ad01c972a6fe35871462c61a2db14d5b078f9d
Mathieu Masy <masy.mathieu@gmail.com>
2018-11-15 00:52:35 -0500

    Update recompose@0.30

--- types/recompose/index.d.ts
+++ types/recompose/index.d.ts
@@ -1,4 +1,4 @@
-// Type definitions for Recompose 0.27
+// Type definitions for Recompose 0.30
 // Project: https://github.com/acdlite/recompose
 // Definitions by: Iskander Sierra <https://github.com/iskandersierra>
 //                 Samuel DeSota <https://github.com/mrapogee>
@@ -6,6 +6,7 @@
 //                 Rasmus Eneman <https://github.com/Pajn>
 //                 Lucas Terra <https://github.com/lucasterra>
 //                 Brian Adams <https://github.com/brian-lives-outdoors>
+//                 Mathieu Masy <https://github.com/TiuSh>
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
 // TypeScript Version: 2.8
 
