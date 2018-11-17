a9161b8abd4c2fb39bb725dd6f954a41de7bdeaf
LFSCamargo <luizepauloxd@gmail.com>
2018-11-17 13:34:39 -0200

    fixing ts errorz

--- types/react-navigation-fluid-transitions/index.d.ts
+++ types/react-navigation-fluid-transitions/index.d.ts
@@ -4,55 +4,57 @@
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped\
 // TypeScript Version: 2.8
 
-import {
-  NavigationRouteConfig,
-  StackNavigatorConfig,
-  NavigationContainer
-} from "react-navigation";
-import * as React from "react";
-import { Animated, Dimensions, StyleSheet } from "react-native";
-
-// tslint-disable-next-line
-type TransitionEnum =
-  | "scale"
-  | "bottom"
-  | "top"
-  | "left"
-  | "right"
-  | "horizontal"
-  | "vertical"
-  | "flip";
-
-export interface TransitionProps {
-    appear?: TransitionEnum;
-    disappear?: TransitionEnum;
-    anchor?: string;
-    delay?: boolean;
-    animated?: string;
-    shared?: string;
+declare module 'react-navigation-fluid-transitions' {
+  import {
+    NavigationRouteConfig,
+    StackNavigatorConfig,
+    NavigationContainer
+  } from "react-navigation";
+  import * as React from "react";
+  import { Animated, Dimensions, StyleSheet } from "react-native";
+  
+  // tslint-disable-next-line
+  type TransitionEnum =
+    | "scale"
+    | "bottom"
+    | "top"
+    | "left"
+    | "right"
+    | "horizontal"
+    | "vertical"
+    | "flip";
+  
+  export interface TransitionProps {
+      appear?: TransitionEnum;
+      disappear?: TransitionEnum;
+      anchor?: string;
+      delay?: boolean;
+      animated?: string;
+      shared?: string;
+  }
+  
+  interface Metrics {
+    x?: number | string;
+    y?: number | string;
+    width?: number | string;
+    height?: number | string;
+  }
+  
+  export class Transition extends React.Component<TransitionProps, any> {}
+  
+  export interface TransitionInfo {
+    progress: Animated.Value;
+    metrics: Metrics;
+    boundingbox: Metrics;
+    name: string;
+    start: number;
+    route: string;
+    end: number;
+    dimensions: Dimensions;
+  }
+  
+  export function FluidNavigator(
+    routeConfig: NavigationRouteConfig,
+    stachConfig?: StackNavigatorConfig
+  ): NavigationContainer;
 }
-
-interface Metrics {
-  x?: number | string;
-  y?: number | string;
-  width?: number | string;
-  height?: number | string;
-}
-
-export class Transition extends React.Component<TransitionProps, any> {}
-
-export interface TransitionInfo {
-  progress: Animated.Value;
-  metrics: Metrics;
-  boundingbox: Metrics;
-  name: string;
-  route: string;
-  start: number;
-  end: number;
-  dimensions: Dimensions;
-}
-
-export function FluidNavigator(
-  routeConfig: NavigationRouteConfig,
-  stachConfig?: StackNavigatorConfig
-): NavigationContainer;
