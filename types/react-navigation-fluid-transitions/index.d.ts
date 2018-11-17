256da633b209307db79bb34c61aa86424f7b9d97
LFSCamargo <luizepauloxd@gmail.com>
2018-11-17 02:08:11 -0200

    fixes using npx

--- types/react-navigation-fluid-transitions/index.d.ts
+++ types/react-navigation-fluid-transitions/index.d.ts
@@ -1,27 +1,25 @@
-// Type definitions for react-navigation-fluid-transitions 0.2.73
-// Project: https://github.com/fram-x/FluidTransitions
-// Definitions by: LFSCamargo <https://github.com/LFSCamargo>
+// Type definitions for react-navigation-fluid-transitions 0.2
+// Project: https://github.com/fram-x/FluidTransitions#readme
+// Definitions by: My Self <https://github.com/me>
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
-// TypeScript Version: 2.8
-
 import {
-    NavigationRouteConfig,
-    StackNavigatorConfig,
-    NavigationContainer
+  NavigationRouteConfig,
+  StackNavigatorConfig,
+  NavigationContainer
 } from "react-navigation";
 import * as React from "react";
 import { Animated, Dimensions, StyleSheet } from "react-native";
 
 // tslint-disable-next-line
 type TransitionEnum =
-    | "scale"
-    | "bottom"
-    | "top"
-    | "left"
-    | "right"
-    | "horizontal"
-    | "vertical"
-    | "flip";
+  | "scale"
+  | "bottom"
+  | "top"
+  | "left"
+  | "right"
+  | "horizontal"
+  | "vertical"
+  | "flip";
 
 export interface TransitionProps {
     appear?: TransitionEnum;
@@ -33,26 +31,26 @@ export interface TransitionProps {
 }
 
 interface Metrics {
-    x?: number | string;
-    y?: number | string;
-    width?: number | string;
-    height?: number | string;
+  x?: number | string;
+  y?: number | string;
+  width?: number | string;
+  height?: number | string;
 }
 
 export class Transition extends React.Component<TransitionProps, any> {}
 
 export interface TransitionInfo {
-    progress: Animated.Value;
-    metrics: Metrics;
-    boundingbox: Metrics;
-    name: string;
-    route: string;
-    start: number;
-    end: number;
-    dimensions: Dimensions;
+  progress: Animated.Value;
+  metrics: Metrics;
+  boundingbox: Metrics;
+  name: string;
+  route: string;
+  start: number;
+  end: number;
+  dimensions: Dimensions;
 }
 
 export function FluidNavigator(
-    routeConfig: NavigationRouteConfig,
-    stachConfig?: StackNavigatorConfig
+  routeConfig: NavigationRouteConfig,
+  stachConfig?: StackNavigatorConfig
 ): NavigationContainer;
