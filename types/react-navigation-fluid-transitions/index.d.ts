f8a866a35e2515a2b483cbf1aae79cda9fbb6666
LFSCamargo <luizepauloxd@gmail.com>
2018-11-17 12:49:44 -0200

    using same TS version as react-navigation

--- types/react-navigation-fluid-transitions/index.d.ts
+++ types/react-navigation-fluid-transitions/index.d.ts
@@ -2,7 +2,7 @@
 // Project: https://github.com/fram-x/FluidTransitions#readme
 // Definitions by: My Self <https://github.com/me>
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped\
-// TypeScript Version: 2.3
+// TypeScript Version: 2.8
 
 import {
   NavigationRouteConfig,
