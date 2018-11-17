e5d125f9ec5febf9c9954b1fbf07db3654dcf6d0
LFSCamargo <luizepauloxd@gmail.com>
2018-11-17 13:42:45 -0200

    TSLint Fixed

--- types/react-navigation-fluid-transitions/index.d.ts
+++ types/react-navigation-fluid-transitions/index.d.ts
@@ -4,57 +4,57 @@
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped\
 // TypeScript Version: 2.8
 
-declare module 'react-navigation-fluid-transitions' {
-  import {
-    NavigationRouteConfig,
-    StackNavigatorConfig,
-    NavigationContainer
-  } from "react-navigation";
-  import * as React from "react";
-  import { Animated, Dimensions, StyleSheet } from "react-native";
-  
-  // tslint-disable-next-line
-  type TransitionEnum =
-    | "scale"
-    | "bottom"
-    | "top"
-    | "left"
-    | "right"
-    | "horizontal"
-    | "vertical"
-    | "flip";
-  
-  interface TransitionProps {
-      appear?: TransitionEnum;
-      disappear?: TransitionEnum;
-      anchor?: string;
-      delay?: boolean;
-      animated?: string;
-      shared?: string;
-  }
-  
-  interface Metrics {
-    x?: number | string;
-    y?: number | string;
-    width?: number | string;
-    height?: number | string;
-  }
-  
-  class Transition extends React.Component<TransitionProps, any> {}
-  
-  interface TransitionInfo {
-    progress: Animated.Value;
-    metrics: Metrics;
-    boundingbox: Metrics;
-    name: string;
-    start: number;
-    route: string;
-    end: number;
-    dimensions: Dimensions;
-  }
-  
-  function FluidNavigator(
-    routeConfig: NavigationRouteConfig,
-    stachConfig?: StackNavigatorConfig
-  ): NavigationContainer;
+// tslint:disable-next-line:no-single-declare-module
+declare module "react-navigation-fluid-transitions" {
+    import {
+        NavigationRouteConfig,
+        StackNavigatorConfig,
+        NavigationContainer
+    } from "react-navigation";
+    import * as React from "react";
+    import { Animated, Dimensions, StyleSheet } from "react-native";
+
+    type TransitionEnum =
+        | "scale"
+        | "bottom"
+        | "top"
+        | "left"
+        | "right"
+        | "horizontal"
+        | "vertical"
+        | "flip";
+
+    interface TransitionProps {
+        appear?: TransitionEnum;
+        disappear?: TransitionEnum;
+        anchor?: string;
+        delay?: boolean;
+        animated?: string;
+        shared?: string;
+    }
+
+    interface Metrics {
+        x?: number | string;
+        y?: number | string;
+        width?: number | string;
+        height?: number | string;
+    }
+
+    class Transition extends React.Component<TransitionProps, any> {}
+
+    interface TransitionInfo {
+        progress: Animated.Value;
+        metrics: Metrics;
+        boundingbox: Metrics;
+        name: string;
+        start: number;
+        route: string;
+        end: number;
+        dimensions: Dimensions;
+    }
+
+    function FluidNavigator(
+        routeConfig: NavigationRouteConfig,
+        stachConfig?: StackNavigatorConfig
+    ): NavigationContainer;
 }
