c8e750d0df0a20ff9d1e3d641fa70cbeeac2a359
Sergey <sokolcjrjk@gmail.com>
2018-11-17 07:37:20 +0300

    jenkins. increased version

--- types/jenkins/index.d.ts
+++ types/jenkins/index.d.ts
@@ -1,4 +1,4 @@
-// Type definitions for jenkins 0.20
+// Type definitions for jenkins 0.23
 // Project: https://github.com/silas/node-jenkins
 // Definitions by: rhysd <https://rhysd.github.io>
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
