a27d1adfaf259409dd6dc13863c1c902c792ea47
Ika <ikatyang@gmail.com>
2018-11-19 01:09:38 +0800

    fix(prettier): add missing options (#30581)

--- types/prettier/index.d.ts
+++ types/prettier/index.d.ts
@@ -52,6 +52,10 @@ export interface RequiredOptions extends doc.printer.Options {
      */
     singleQuote: boolean;
     /**
+     * Use single quotes in JSX.
+     */
+    jsxSingleQuote: boolean;
+    /**
      * Print trailing commas wherever possible.
      */
     trailingComma: 'none' | 'es5' | 'all';
@@ -108,6 +112,14 @@ export interface RequiredOptions extends doc.printer.Options {
      * The plugin API is in a beta state.
      */
     plugins: Array<string | Plugin>;
+    /**
+     * How to handle whitespaces in HTML.
+     */
+    htmlWhitespaceSensitivity: 'css' | 'strict' | 'ignore';
+    /**
+     * Which end of line characters to apply.
+     */
+    endOfLine: 'auto' | 'lf' | 'crlf' | 'cr';
 }
 
 export interface ParserOptions extends RequiredOptions {
