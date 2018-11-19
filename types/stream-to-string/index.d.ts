3ee44a1fedd5bd80235385f24d1149f29425f203
Dimitri Benin <dimitri.benin@gmail.com>
2018-11-19 20:44:56 +0100

    Add types for stream-to-string

---
+++ types/stream-to-string/index.d.ts
@@ -0,0 +1,20 @@
+// Type definitions for stream-to-string 1.1
+// Project: https://github.com/jasonpincin/stream-to-string
+// Definitions by: BendingBender <https://github.com/BendingBender>
+// Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
+
+/// <reference types="node" />
+
+export = streamToString;
+
+// tslint:disable:unified-signatures
+
+declare function streamToString(stream: NodeJS.ReadableStream): Promise<string>;
+declare function streamToString(stream: NodeJS.ReadableStream, cb: Callback): Promise<string>;
+declare function streamToString(
+    stream: NodeJS.ReadableStream,
+    enc: string,
+    cb?: Callback
+): Promise<string>;
+
+type Callback = (error: Error | undefined, str: string) => void;
