2087e1b9f13248e441fc923ab638a23bec69255c
Rhys van der Waerden <rhys.vdw@gmail.com>
2018-11-19 12:09:14 +1100

    compression-webpack-plugin: Add myself

--- types/compression-webpack-plugin/index.d.ts
+++ types/compression-webpack-plugin/index.d.ts
@@ -1,6 +1,7 @@
 // Type definitions for compression-webpack-plugin 0.4
 // Project: https://github.com/webpack-contrib/compression-webpack-plugin
 // Definitions by: Anton Kandybo <https://github.com/dublicator>
+//                 Rhys van der Waerden <https://github.com/rhys-vdw>
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
 // TypeScript Version: 2.4
 
