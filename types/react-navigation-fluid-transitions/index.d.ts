63967c1de94b87591e88752f5eafd1b40237bcad
LFSCamargo <luizepauloxd@gmail.com>
2018-11-20 11:10:27 -0200

    Fixes comments

--- types/react-navigation-fluid-transitions/index.d.ts
+++ types/react-navigation-fluid-transitions/index.d.ts
@@ -1,6 +1,6 @@
 // Type definitions for react-navigation-fluid-transitions 0.2
 // Project: https://github.com/fram-x/FluidTransitions#readme
-// Definitions by: My Self <https://github.com/me>
+// Definitions by: LFSCamargo <https://github.com/LFSCamargo>
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped\
 // TypeScript Version: 2.8
 
