3daac759f4ce410664691073d7a16f735e5fe66a
Benjamin Lupton <b@lupton.cc>
2018-11-21 21:46:45 +1100

    ansicolors: accurate types with recursion support

--- types/ansicolors/index.d.ts
+++ types/ansicolors/index.d.ts
@@ -1,8 +1,44 @@
 // Type definitions for ansicolors
 // Project: https://github.com/thlorenz/ansicolors
-// Definitions by: rogierschouten <https://github.com/rogierschouten>
+// Definitions by: Benjamin Arthur Lupton <https://github.com/balupton>
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
 
-
-declare var colors: { [index: string]: (s: string) => string; };
-export = colors;
+interface Colors extends String {
+	white: Colors
+	black: Colors
+	blue: Colors
+	cyan: Colors
+	green: Colors
+	magenta: Colors
+	red: Colors
+	yellow: Colors
+	brightBlack: Colors
+	brightRed: Colors
+	brightGreen: Colors
+	brightYellow: Colors
+	brightBlue: Colors
+	brightMagenta: Colors
+	brightCyan: Colors
+	brightWhite: Colors
+	bgBlack: Colors
+	bgRed: Colors
+	bgGreen: Colors
+	bgYellow: Colors
+	bgBlue: Colors
+	bgMagenta: Colors
+	bgCyan: Colors
+	bgWhite: Colors
+	bgBrightBlack: Colors
+	bgBrightRed: Colors
+	bgBrightGreen: Colors
+	bgBrightYellow: Colors
+	bgBrightBlue: Colors
+	bgBrightMagenta: Colors
+	bgBrightCyan: Colors
+	bgBrightWhite: Colors
+	open: Colors
+	close: Colors
+	colors: Colors
+}
+declare var colors: Colors
+export default colors
