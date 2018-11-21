e326ad10f34ecb6316d0650abb72d6bab7ea22fb
Ville Saarinen <ville.saarinen@gmail.com>
2018-11-21 09:43:29 +0200

    Fixes to header

--- types/rc-tooltip/index.d.ts
+++ types/rc-tooltip/index.d.ts
@@ -1,7 +1,7 @@
-// Type definitions for rc-tooltip v3.7.3
+// Type definitions for rc-tooltip 3.7
 // Project: https://github.com/react-component/tooltip
-// Definitions by: rhysd <https://rhysd.github.io>
-//                 ahstro <http://ahst.ro>
+// Definitions by: rhysd <https://github.com/rhysd>
+//                 ahstro <https://github.com/ahstro>
 //                 vsaarinen <https://github.com/vsaarinen>
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
 // TypeScript Version: 2.8
