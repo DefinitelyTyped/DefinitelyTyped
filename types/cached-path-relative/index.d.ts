bd6515d5871238514dab001bcf0c062f0f25d721
TeamworkGuy2 <straitfrommars@rocketmail.com>
2018-11-22 16:34:26 +0000

    [cached-path-relative] Add cached-path-relative@1.0.2 definition

---
+++ types/cached-path-relative/index.d.ts
@@ -0,0 +1,8 @@
+// Type definitions for cached-path-relative 1.0
+// Project: https://github.com/ashaffer/cached-path-relative
+// Definitions by: TeamworkGuy2 <https://github.com/TeamworkGuy2>
+// Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
+
+declare function cachedPathRelative(from: string, to: string): string;
+
+export = cachedPathRelative;
