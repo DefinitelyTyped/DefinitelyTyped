b4dee98a6c42291a8e25fd49125dcf7df69f03bf
Pavlos Vinieratos <pvinis@gmail.com>
2018-11-23 12:31:17 +0100

    make optional, since they have a default value

--- types/react-native-safe-area/index.d.ts
+++ types/react-native-safe-area/index.d.ts
@@ -43,6 +43,6 @@ export type Direction =
 
 export function withSafeArea(
     WrappedComponent: React.ComponentType<any>,
-    applyTo: 'margin' | 'padding' | 'absolutePosition' | 'contentInset',
-    direction: Direction,
+    applyTo?: 'margin' | 'padding' | 'absolutePosition' | 'contentInset',
+    direction?: Direction,
 ): React.ComponentType<any>;
