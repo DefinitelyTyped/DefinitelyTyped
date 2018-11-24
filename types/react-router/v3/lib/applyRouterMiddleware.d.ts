5c12fd304c41b5adce894160f1e18619b1c06a44
Jessica <jessicamfranco3@gmail.com>
2018-11-24 22:37:15 +0900

    Fix lint warnings

--- types/react-router/v3/lib/applyRouterMiddleware.d.ts
+++ types/react-router/v3/lib/applyRouterMiddleware.d.ts
@@ -2,8 +2,8 @@ import React = require("react");
 import { RouteComponent } from "react-router";
 import RouterContext from "react-router/lib/RouterContext";
 
-export type RouterContextElement = React.TypedReactElement<RouterContext>
-export type RouteComponentElement = React.TypedReactElement<RouteComponent>
+export type RouterContextElement = React.TypedReactElement<RouterContext>;
+export type RouteComponentElement = React.TypedReactElement<RouteComponent>;
 
 export interface Middleware {
     renderRouterContext?(previous: RouterContextElement, props: any): RouterContextElement;
