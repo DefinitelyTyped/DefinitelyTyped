a169bc5dda5c595439062712c4d0429b4def512d
TeamworkGuy2 <straitfrommars@rocketmail.com>
2018-11-24 14:26:28 +0000

    [cached-path-relative] Add documentation to new definition

--- types/cached-path-relative/index.d.ts
+++ types/cached-path-relative/index.d.ts
@@ -3,6 +3,11 @@
 // Definitions by: TeamworkGuy2 <https://github.com/TeamworkGuy2>
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
 
+/**
+ * Memoize the results of the path.relative function. path.relative can be an expensive operation
+ * if it happens a lot, and its results shouldn't change for the same arguments.
+ * Use it just like your normal path.relative, but it's memoized.
+ */
 declare function cachedPathRelative(from: string, to: string): string;
 
 export = cachedPathRelative;
