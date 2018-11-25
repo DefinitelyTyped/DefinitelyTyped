3465be17ba8e9ec2628e3bfae2d5b67cdd1955ac
erikma <erikma1@hotmail.com>
2018-11-25 12:35:43 -0800

    fix lint issue

--- types/pigpio/index.d.ts
+++ types/pigpio/index.d.ts
@@ -1,6 +1,6 @@
 // Type definitions for pigpio 1.2
 // Project: https://github.com/fivdi/pigpio
-// Definitions by: ManerFan <https://github.com/manerfan> and erikma <https://github.com/erikma>
+// Definitions by: ManerFan <https://github.com/manerfan>, erikma <https://github.com/erikma>
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
 
 /// <reference types="node" />
