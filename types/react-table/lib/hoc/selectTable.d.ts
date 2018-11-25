bc05cf043193f7e163c3c6f68773b70c1fdb6384
Grzegorz Rozdzialik <voreny.gelio@gmail.com>
2018-11-25 19:28:42 +0100

    fix react-table interface names, relax component props requirements

--- types/react-table/lib/hoc/selectTable.d.ts
+++ types/react-table/lib/hoc/selectTable.d.ts
@@ -12,7 +12,7 @@ export interface SelectInputComponentProps {
     row: any;
 }
 
-export interface SelectAllInputComponent {
+export interface SelectAllInputComponentProps {
     selectType: SelectType;
     checked: boolean;
     onClick: () => any;
@@ -28,7 +28,7 @@ export interface SelectTableAdditionalProps {
 
     selectAll?: boolean;
 
-    toggleAll?: SelectAllInputComponent['onClick'];
+    toggleAll?: SelectAllInputComponentProps['onClick'];
 
     toggleSelection?: SelectInputComponentProps['onClick'];
 
@@ -37,8 +37,10 @@ export interface SelectTableAdditionalProps {
      */
     selectType?: SelectType;
 
-    SelectInputComponent?: ComponentType<SelectInputComponentProps>;
-    SelectAllInputComponent?: ComponentType<SelectAllInputComponent>;
+    SelectInputComponent?: ComponentType<Partial<SelectInputComponentProps>>;
+    SelectAllInputComponent?: ComponentType<
+        Partial<SelectAllInputComponentProps>
+    >;
 }
 
 export interface SelectTableHOCOptions {
