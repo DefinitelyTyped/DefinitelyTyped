620ad409303126f5066dc1dda42ec23ce0bf2f4f
Karol Samborski <karol.samborski@espeo.eu>
2018-11-25 22:50:42 +0100

    Type definitions for multiSamlStrategy

--- types/passport-saml/index.d.ts
+++ types/passport-saml/index.d.ts
@@ -2,6 +2,7 @@
 // Project: https://github.com/bergie/passport-saml
 // Definitions by: Chris Barth <https://github.com/cjbarth>
 //                 Damian Assennato <https://github.com/dassennato>
+//                 Karol Samborski <https://github.com/ksamborski>
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
 // TypeScript Version: 2.3
 
