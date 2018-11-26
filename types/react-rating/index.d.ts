0d31729bb502c73c36882151f0b9f8b957f0ba6b
Kyle Bonar <kylewb2011@gmail.com>
2018-11-25 21:28:57 -0600

    Init commit

---
+++ types/react-rating/index.d.ts
@@ -0,0 +1,52 @@
+// Type definitions for react-rating 1.5
+// Project: https://github.com/dreyescat/react-rating
+// Definitions by: Kyle Davis <https://github.com/kyledavisdev/>,
+//                 Konrad Szwarc <https://github.com/szwarckonrad/>
+// Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
+
+import * as React from "react";
+
+export as namespace Rating;
+
+export default Rating;
+
+export namespace Rating {
+    interface props {
+        start?: number;
+        stop?: number;
+        step?: number;
+        fractions?: number;
+        initialRating?: number;
+        className?: string;
+        placeholderRating?: number;
+        readonly?: boolean;
+        quiet?: boolean;
+        direction?: "rtl" | "ltr";
+        emptySymbol?:
+            | string
+            | string[]
+            | any
+            | any[]
+            | JSX.Element[]
+            | JSX.Element;
+        fullSymbol?:
+            | string
+            | string[]
+            | any
+            | any[]
+            | JSX.Element[]
+            | JSX.Element;
+        placeholderSymbol?:
+            | string
+            | string[]
+            | any
+            | any[]
+            | JSX.Element[]
+            | JSX.Element;
+        onChange?: (value: number) => void;
+        onHover?: (value: number) => void;
+        onClick?: (value: number) => void;
+    }
+}
+
+declare class Rating extends React.Component<Rating.props> {}
---
+++ types/react-rating/react-rating-tests.tsx
@@ -0,0 +1,8 @@
+import * as React from "react";
+import Rating from "react-rating";
+
+class Test extends React.Component {
+    render() {
+        return <Rating />;
+    }
+}
