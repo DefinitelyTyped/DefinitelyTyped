9d01bdd867de647f6469fd6e5e830295e8585c65
John Plusje <john.plusje@ivengi.com>
2018-11-26 15:24:11 +0100

    Add declarations for stoppable 1.1

--- types/stoppable/stoppable-tests.ts
+++ types/stoppable/stoppable-tests.ts
@@ -1,8 +1,11 @@
 import * as http from 'http';
 import stoppable = require('stoppable');
 
-const server: http.Server = stoppable(http.createServer());
-server.close();
+const server = stoppable(http.createServer());
+server.stop();
 
-const server2: http.Server = stoppable(http.createServer(), 10000);
-server2.close();
+const server2 = stoppable(http.createServer(), 10000);
+server2.stop();
+
+const server3 = stoppable(http.createServer());
+server.stop((err, gracefully) => {});
