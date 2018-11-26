b01b70a1f8357e8f2ca228627ff0cf78ff42c697
Junyoung Choi <fluke8259@gmail.com>
2018-11-26 16:53:27 +0900

    Add is-buffer

---
+++ types/is-buffer/index.d.ts
@@ -0,0 +1,10 @@
+// Type definitions for is-buffer 2.0
+// Project: https://github.com/feross/is-buffer#readme
+// Definitions by: Junyoung Choi <https://github.com/rokt33r>
+// Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
+
+/// <reference types='node' />
+
+declare function isBuffer(obj: any): boolean;
+
+export = isBuffer;
