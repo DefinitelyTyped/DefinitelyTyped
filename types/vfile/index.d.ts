f6a42a5032d678cfe4c210e72eb5da78107a2bd7
Junyoung Choi <fluke8259@gmail.com>
2018-11-26 15:49:41 +0900

    Update vfile version

--- types/vfile/index.d.ts
+++ types/vfile/index.d.ts
@@ -1,4 +1,4 @@
-// Type definitions for VFile 2.2
+// Type definitions for VFile 3.0
 // Project: https://github.com/vfile/vfile
 // Definitions by: bizen241 <https://github.com/bizen241>
 //                 Junyoung Choi <https://github.com/rokt33r>
