dcc3948836f0a3683aeb8cb9b6395742760757a8
Junyoung Choi <fluke8259@gmail.com>
2018-11-27 14:49:51 +0900

    Upgrade typescript version because it is using `unknown`

--- types/vfile/index.d.ts
+++ types/vfile/index.d.ts
@@ -3,7 +3,7 @@
 // Definitions by: bizen241 <https://github.com/bizen241>
 //                 Junyoung Choi <https://github.com/rokt33r>
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
-// TypeScript Version: 2.4
+// TypeScript Version: 3.0
 
 /// <reference types='node' />
 
