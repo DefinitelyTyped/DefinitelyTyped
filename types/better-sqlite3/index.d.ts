aa086a84c1eb3b2499f77d0d075f91a672f11573
Santiago Aguilar Hernández <santiago777@outlook.com>
2018-11-26 19:16:28 -0500

    * Applied better practices

--- types/better-sqlite3/index.d.ts
+++ types/better-sqlite3/index.d.ts
@@ -5,7 +5,91 @@
 //                 Santiago Aguilar <https://github.com/sant123>
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
 
-import { DatabaseConstructor } from "./types";
+import Integer = require("integer");
+
+declare class Statement {
+    database: Database;
+    source: string;
+    returnsData: boolean;
+    constructor(db: Database, sources: string[]);
+
+    run(...params: any[]): Database.RunResult;
+    get(...params: any[]): any;
+    all(...params: any[]): any[];
+    each(params: any, cb: (row: any) => void): void;
+    each(cb: (row: any) => void): void;
+    each(...params: any[]): void;
+    pluck(toggleState?: boolean): this;
+    bind(...params: any[]): this;
+    safeIntegers(toggleState?: boolean): this;
+}
+
+declare class Transaction {
+    database: Database;
+    source: string;
+    constructor(db: Database, sources: string[]);
+
+    run(...params: any[]): Database.RunResult;
+    bind(...params: any[]): this;
+    safeIntegers(toggleState?: boolean): this;
+}
+
+interface Database {
+    memory: boolean;
+    readonly: boolean;
+    name: string;
+    open: boolean;
+    inTransaction: boolean;
+
+    prepare(source: string): Statement;
+    transaction(sources: string[]): Transaction;
+    exec(source: string): this;
+    pragma(source: string, simplify?: boolean): any;
+    checkpoint(databaseName?: string): this;
+    register(cb: (...params: any[]) => any): this;
+    register(
+        options: Database.RegistrationOptions,
+        cb: (...params: any[]) => any
+    ): this;
+    close(): this;
+    defaultSafeIntegers(toggleState?: boolean): this;
+}
+
+declare class SqliteError implements Error {
+    name: string;
+    message: string;
+    code: string;
+    constructor(message: string, code: string);
+}
+
+interface DatabaseConstructor {
+    new (filename: string, options?: Database.Options): Database;
+    (filename: string, options?: Database.Options): Database;
+    prototype: Database;
+
+    Integer: typeof Integer;
+    SqliteError: typeof SqliteError;
+}
+
+declare namespace Database {
+    interface RunResult {
+        changes: number;
+        lastInsertRowid: Integer.IntLike;
+    }
+
+    interface Options {
+        memory?: boolean;
+        readonly?: boolean;
+        fileMustExist?: boolean;
+    }
+
+    interface RegistrationOptions {
+        name?: string;
+        varargs?: boolean;
+        deterministic?: boolean;
+        safeIntegers?: boolean;
+    }
+}
 
 declare const Database: DatabaseConstructor;
 export = Database;
