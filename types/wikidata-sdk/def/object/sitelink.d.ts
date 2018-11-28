22e936663027c8f82280ce4a3883fe42ea30e8ee
Kamontat Chantrachirathumrong <kamontat.c@hotmail.com>
2018-11-28 15:09:40 +0100

    Add sitelink object

---
+++ types/wikidata-sdk/def/object/sitelink.d.ts
@@ -0,0 +1,12 @@
+import { WikidataSite } from "wikidata-sdk";
+
+export type WikidataSiteLink = { [Key in WikidataSite]: { site: Key; title: string; badges: string[] } };
+
+export type WikidataSiteLinkSimplify = { [key in WikidataSite]: string };
+
+export type WikidataSiteLinkWithUrlSimplify = {
+  [key in WikidataSite]: {
+    title: string;
+    url: string;
+  }
+};
