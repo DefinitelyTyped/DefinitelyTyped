1748c8d191505ad7a17165f5078dd45ee8575e9b
Joe Flateau <joe@joeflateau.net>
2018-11-28 12:14:34 -0500

    Update types/node-forge/index.d.ts
    Co-Authored-By: timhwang21 <timhwang21@users.noreply.github.com>

--- types/node-forge/index.d.ts
+++ types/node-forge/index.d.ts
@@ -1,4 +1,4 @@
-// Type definitions for node-forge 0.7.7
+// Type definitions for node-forge 0.7.6
 // Project: https://github.com/digitalbazaar/forge
 // Definitions by: Seth Westphal    <https://github.com/westy92>
 //                 Kay Schecker     <https://github.com/flynetworks>
