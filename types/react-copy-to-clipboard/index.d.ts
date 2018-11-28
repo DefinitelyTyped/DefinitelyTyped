7406f78c349277879667b49031ce9ae3fdd23892
inje <byeo630@gmail.com>
2018-11-27 17:18:19 -0800

    react-copy-to-clipboard: Remove 'export = CopyToClipboard

--- types/react-copy-to-clipboard/index.d.ts
+++ types/react-copy-to-clipboard/index.d.ts
@@ -1,15 +1,14 @@
-// Type definitions for react-copy-to-clipboard 4.2
+// Type definitions for react-copy-to-clipboard 5.0
 // Project: https://github.com/nkbt/react-copy-to-clipboard
 // Definitions by: Meno Abels <https://github.com/mabels>
 //                 Bernabe <https://github.com/BernabeFelix>
+//                 Inje Yeo <https://github.com/inje>
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
 // TypeScript Version: 2.8
 
 import * as React from "react";
 
 export as namespace CopyToClipboard;
-
-export = CopyToClipboard;
 
 declare namespace CopyToClipboard {
   interface Options {
