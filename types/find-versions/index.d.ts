8ceca9db8f12523151c437f28b885c15686c510f
Chris Arnesen <chris.arnesen@gmail.com>
2018-11-28 15:29:38 +0700

    Update types/find-versions/index.d.ts

--- types/find-versions/index.d.ts
+++ types/find-versions/index.d.ts
@@ -1,7 +1,7 @@
 // Type definitions for find-versions 3.0
 // Project: https://github.com/sindresorhus/find-versions
 // Definitions by: Leonid Logvinov <https://github.com/LogvinovLeon>
-//                 Chris Arnesen <https://github.com/carnesen
+//                 Chris Arnesen <https://github.com/carnesen>
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
 
 declare function findVersions(version: string, options?: { loose: boolean }): string[];
