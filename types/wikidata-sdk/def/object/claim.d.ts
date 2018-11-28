d284105dcadcce897d96a1d38a5040e396095f35
Kamontat Chantrachirathumrong <kamontat.c@hotmail.com>
2018-11-28 15:08:33 +0100

    Update claim name

--- types/wikidata-sdk/def/object/claim.d.ts
+++ types/wikidata-sdk/def/object/claim.d.ts
@@ -1,34 +1,34 @@
-export interface TimeDataValue {
-    type: "time";
-    value: {
-        time: string;
-        timezone: number;
-        before: number;
-        after: number;
-        precision: number;
-        calendermodel: string;
-    };
+export interface WikidataTimeDataValue {
+  type: "time";
+  value: {
+    time: string;
+    timezone: number;
+    before: number;
+    after: number;
+    precision: number;
+    calendermodel: string;
+  };
 }
 
-export interface EntityDataValue {
-    type: "wikibase-entityid";
-    value: {
-        "numeric-id": number;
-        "entity-type": string;
-    };
+export interface WikidataEntityDataValue {
+  type: "wikibase-entityid";
+  value: {
+    "numeric-id": number;
+    "entity-type": string;
+  };
 }
 
-export interface MainSnak {
-    datatype: string;
-    snaktype: string;
-    property: string;
-    hash: string;
-    datavalue: TimeDataValue | EntityDataValue;
+export interface WikidataMainSnak {
+  datatype: string;
+  snaktype: string;
+  property: string;
+  hash: string;
+  datavalue: WikidataTimeDataValue | WikidataEntityDataValue;
 }
 
-export interface Claim {
-    rank: string;
-    id: string;
-    type: string;
-    mainsnak: MainSnak;
+export interface WikidataClaim {
+  rank: string;
+  id: string;
+  type: string;
+  mainsnak: WikidataMainSnak;
 }
