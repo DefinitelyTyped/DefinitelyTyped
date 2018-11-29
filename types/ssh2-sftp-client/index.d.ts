7b89694607cb4a4d602f7600bb582880075b03cd
Michael Pertl <mpertl@dance-all-day.com>
2018-11-29 16:08:35 +0100

    [@types/ssh2-sftp-client] "Formatting: Either use all tabs, or always use 4 spaces"

--- types/ssh2-sftp-client/index.d.ts
+++ types/ssh2-sftp-client/index.d.ts
@@ -5,35 +5,48 @@
 
 import * as ssh2 from 'ssh2';
 import * as ssh2Stream from 'ssh2-streams';
+
 export = sftp;
 
 declare class sftp {
-  connect(options: ssh2.ConnectConfig): Promise<void>;
-  list(remoteFilePath: string): Promise<sftp.FileInfo[]>;
-  get(remoteFilePath: string, useCompression?: boolean, encoding?: string | null): Promise<NodeJS.ReadableStream>;
-  fastGet(remoteFilePath: string, localPath: string, options?: ssh2Stream.TransferOptions): Promise<string>;
-  put(input: string | Buffer | NodeJS.ReadableStream, remoteFilePath: string, useCompression?: boolean, encoding?: string): Promise<void>;
-  fastPut(localPath: string, emoteFilePath: string, options?: ssh2Stream.TransferOptions): Promise<string>;
-  mkdir(remoteFilePath: string, recursive?: boolean): Promise<void>;
-  rmdir(remoteFilePath: string, recursive?: boolean): Promise<void>;
-  delete(remoteFilePath: string): Promise<void>;
-  rename(remoteSourcePath: string, remoteDestPath: string): Promise<void>;
-  end(): Promise<void>;
-  on(event: string, callback: (...args: any[]) => void): void;
+    connect(options: ssh2.ConnectConfig): Promise<void>;
+
+    list(remoteFilePath: string): Promise<sftp.FileInfo[]>;
+
+    get(remoteFilePath: string, useCompression?: boolean, encoding?: string | null): Promise<NodeJS.ReadableStream>;
+
+    fastGet(remoteFilePath: string, localPath: string, options?: ssh2Stream.TransferOptions): Promise<string>;
+
+    put(input: string | Buffer | NodeJS.ReadableStream, remoteFilePath: string, useCompression?: boolean, encoding?: string): Promise<void>;
+
+    fastPut(localPath: string, emoteFilePath: string, options?: ssh2Stream.TransferOptions): Promise<string>;
+
+    mkdir(remoteFilePath: string, recursive?: boolean): Promise<void>;
+
+    rmdir(remoteFilePath: string, recursive?: boolean): Promise<void>;
+
+    delete(remoteFilePath: string): Promise<void>;
+
+    rename(remoteSourcePath: string, remoteDestPath: string): Promise<void>;
+
+    end(): Promise<void>;
+
+    on(event: string, callback: (...args: any[]) => void): void;
 }
+
 declare namespace sftp {
-  interface FileInfo {
-    type: string;
-    name: string;
-    size: number;
-    modifyTime: number;
-    accessTime: number;
-    rights: {
-      user: string;
-      group: string;
-      other: string;
-    };
-    owner: number;
-    group: number;
-  }
+    interface FileInfo {
+        type: string;
+        name: string;
+        size: number;
+        modifyTime: number;
+        accessTime: number;
+        rights: {
+            user: string;
+            group: string;
+            other: string;
+        };
+        owner: number;
+        group: number;
+    }
 }
