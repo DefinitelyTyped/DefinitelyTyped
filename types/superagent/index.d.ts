09cf6f908a2bd4d341d80a89aa735d52fd5ad8a9
Lukas Elmer <lukas.elmer@gmail.com>
2018-11-29 17:36:21 +0100

    update header

--- types/superagent/index.d.ts
+++ types/superagent/index.d.ts
@@ -6,6 +6,7 @@
 //                 Shrey Jain <https://github.com/shreyjain1994>
 //                 Alec Zopf <https://github.com/zopf>
 //                 Adam Haglund <https://github.com/beeequeue>
+//                 Lukas Elmer <https://github.com/lukaselmer>
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
 // TypeScript Version: 2.2
 
