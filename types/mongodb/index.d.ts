56761f59b80614fbfc67fd190cbe11f9461f4919
Dobes Vandermeer <dobesv@gmail.com>
2018-11-29 12:40:58 -0800

    Add hint to CollectionAggregationOptions
    Although this option is, for some reason, not document.  It is supported.
    
    It follows the format specified here:
    
    https://docs.mongodb.com/manual/reference/command/aggregate/

--- types/mongodb/index.d.ts
+++ types/mongodb/index.d.ts
@@ -1294,6 +1294,7 @@ export interface CollectionAggregationOptions {
      * Allow driver to bypass schema validation in MongoDB 3.2 or higher.
      */
     bypassDocumentValidation?: boolean;
+    hint?: string | Object;   
     raw?: boolean;
     promoteLongs?: boolean;
     promoteValues?: boolean;
