c74a7e8088af068f6e1925eb5c953affd3d05a45
Michael Pertl <mpertl@dance-all-day.com>
2018-11-29 16:23:22 +0100

    [@types/ssh2-sftp-client] multi-line "definitions by"; added new tests

--- types/ssh2-sftp-client/index.d.ts
+++ types/ssh2-sftp-client/index.d.ts
@@ -1,6 +1,9 @@
 // Type definitions for ssh2-sftp-client 2.4
 // Project: https://www.npmjs.com/package/ssh2-sftp-client
-// Definitions by: igrayson <https://github.com/igrayson>, Ascari Andrea <https://github.com/ascariandrea>, Kartik Malik <https://github.com/kartik2406>, Michael Pertl <https://github.com/viamuli>
+// Definitions by: igrayson <https://github.com/igrayson>
+//                 Ascari Andrea <https://github.com/ascariandrea>
+//                 Kartik Malik <https://github.com/kartik2406>
+//                 Michael Pertl <https://github.com/viamuli>
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
 
 import * as ssh2 from 'ssh2';
