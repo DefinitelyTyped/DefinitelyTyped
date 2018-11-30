544bbdef11e4a15ad8dddae8305fc6f6b5b783a8
kingdaro <kingdaro@gmail.com>
2018-11-30 00:38:50 -0600

    bump version

--- types/react-facebook-login/index.d.ts
+++ types/react-facebook-login/index.d.ts
@@ -1,4 +1,4 @@
-// Type definitions for react-facebook-login 3.6
+// Type definitions for react-facebook-login 4.1
 // Project: https://github.com/keppelen/react-facebook-login
 // Definitions by: Alexandre Paré <https://github.com/apare>, Jan Karres <https://github.com/jankarres>
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
