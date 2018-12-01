6e11b5df9d359a15d2851144b130139259f36ae4
Adam Misiorny <adam.misiorny@gmail.com>
2018-12-01 16:02:18 +0100

    Add author

--- types/storybook__addon-options/index.d.ts
+++ types/storybook__addon-options/index.d.ts
@@ -3,6 +3,7 @@
 // Definitions by: Joscha Feth <https://github.com/joscha>
 //                 Simon Helle Nielsen <https://github.com/simonhn>
 //                 A.MacLeay <https://github.com/amacleay>
+//                 Adam Misiorny <https://github.com/adam187>
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
 // TypeScript Version: 2.3
 
