1bd81e2845069ea2a1696fc367041dec9bb30eb3
Adam Misiorny <adam.misiorny@gmail.com>
2018-12-01 15:11:19 +0100

    Add author info

--- types/rosie/index.d.ts
+++ types/rosie/index.d.ts
@@ -3,6 +3,7 @@
 // Definitions by: Abner Oliveira <https://github.com/abner>,
 //                 Chris Grigg <https://github.com/subvertallchris>,
 //                 Alex Bukurov <https://github.com/abukurov>
+//                 Adam Misiorny <https://github.com/adam187>
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
 // TypeScript Version: 2.3
 
