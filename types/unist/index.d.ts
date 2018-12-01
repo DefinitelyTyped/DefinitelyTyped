0084184b14ff5aaeb264188421e51f5af205e779
Hernan Rajchert <hrajchert@gmail.com>
2018-12-01 11:58:20 -0300

    refactor: converted comments in multi-line to see them in the IDE

--- types/unist/index.d.ts
+++ types/unist/index.d.ts
@@ -7,55 +7,89 @@
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
 // TypeScript Version: 3.0
 
-// Syntactic units in unist syntax trees are called nodes.
+/**
+ * Syntactic units in unist syntax trees are called nodes.
+ */
 export interface Node {
-    // The variant of a node.
+    /**
+     * The variant of a node.
+     */
     type: string;
 
-    // Information from the ecosystem.
+    /**
+     * Information from the ecosystem.
+     */
     data?: Data;
 
-    // Location of a node in a source document.
-    // Must not be present if a node is generated.
+    /**
+     * Location of a node in a source document.
+     * Must not be present if a node is generated.
+     */
     position?: Position;
 }
 
+/**
+ * Information associated by the ecosystem with the node.
+ * Space is guaranteed to never be specified by unist or specifications
+ * implementing unist.
+ */
 export interface Data {
     [key: string]: unknown;
 }
 
-// Location of a node in a source file.
+/**
+ * Location of a node in a source file.
+ */
 export interface Position {
-    // Place of the first character of the parsed source region.
+    /**
+     * Place of the first character of the parsed source region.
+     */
     start: Point;
 
-    // Place of the first character after the parsed source region.
+    /**
+     * Place of the first character after the parsed source region.
+     */
     end: Point;
 
-    // Start column at each index (plus start line) in the source region,
-    // for elements that span multiple lines.
+    /**
+     * Start column at each index (plus start line) in the source region,
+     * for elements that span multiple lines.
+     */
     indent?: number[];
 }
 
-// One place in a source file.
+/**
+ * One place in a source file.
+ */
 export interface Point {
-    // Line in a source file (1-indexed integer).
+    /**
+     * Line in a source file (1-indexed integer).
+     */
     line: number;
 
-    // Column in a source file (1-indexed integer).
+    /**
+     * Column in a source file (1-indexed integer).
+     */
     column: number;
-
-    // Character in a source file (0-indexed integer).
+    /**
+     * Character in a source file (0-indexed integer).
+     */
     offset?: number;
 }
 
-// Nodes containing other nodes.
+/**
+ * Nodes containing other nodes.
+ */
 export interface Parent extends Node {
-    // List representing the children of a node.
+    /**
+     * List representing the children of a node.
+     */
     children: Node[];
 }
 
-// Nodes containing a value.
+/**
+ * Nodes containing a value.
+ */
 export interface Literal extends Node {
     value: unknown;
 }
