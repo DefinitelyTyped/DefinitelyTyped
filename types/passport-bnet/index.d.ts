47fa3e69a48443e99093497e234cd5fb4159761d
Ivan Fernandes <ivandalcantara@gmail.com>
2018-12-02 02:12:46 -0200

    fixing version notation

--- types/passport-bnet/index.d.ts
+++ types/passport-bnet/index.d.ts
@@ -1,4 +1,4 @@
-// Type definitions for passport-bnet 2.0.
+// Type definitions for passport-bnet 2.0
 // Project: https://github.com/Blizzard/passport-bnet#readme
 // Definitions by: Ivan Fernandes <https://github.com/ivan94>
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
