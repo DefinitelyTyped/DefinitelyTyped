eda0aeb732af70c9406e6369115a902825ff0e8e
Jessica <jessicamfranco3@gmail.com>
2018-12-03 20:29:24 +0900

    Fix lint warnings

--- types/react/index.d.ts
+++ types/react/index.d.ts
@@ -324,17 +324,17 @@ declare namespace React {
     const StrictMode: ExoticComponent<{ children?: ReactNode }>;
 
     interface SuspenseProps {
-        children?: ReactNode
+        children?: ReactNode;
 
         /** A fallback react tree to show when a Suspense child (like React.lazy) suspends */
-        fallback: NonNullable<ReactNode>|null
+        fallback: NonNullable<ReactNode>|null;
 
         // I tried looking at the code but I have no idea what it does.
         // https://github.com/facebook/react/issues/13206#issuecomment-432489986
         /**
          * Not implemented yet, requires unstable_ConcurrentMode
          */
-        // maxDuration?: number
+        // maxDuration?: number;
     }
     /**
      * This feature is not yet available for server-side rendering.
