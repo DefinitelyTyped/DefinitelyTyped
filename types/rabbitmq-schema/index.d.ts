539736fa10140c46156aa34e4918a4648ca3c72c
Alex Duka <aduka@alteryx.com>
2018-12-05 01:52:42 +0200

    fixed linting issues, ts version > 2.2

--- types/rabbitmq-schema/index.d.ts
+++ types/rabbitmq-schema/index.d.ts
@@ -1,8 +1,8 @@
+// TypeScript Version: 2.2' just under the header.
 // Type definitions for rabbitmq-schema 2.0
 // Project: https://github.com/tjmehta/rabbitmq-schema
 // Definitions by: Alex Duka <https://github.com/rulezzz1987>
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
-
 
 interface Binding {
     destination: Topology;
@@ -18,7 +18,6 @@ interface TopicBinding extends Binding {
 interface DirectBinding extends Binding {
     routingPattern: string;
 }
-
 
 interface Topology {
     options?: object;
