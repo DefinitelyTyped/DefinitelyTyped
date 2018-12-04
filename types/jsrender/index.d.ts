38def021fa25a42f61ce4bdad034bce8708c35ff
Jon Russell <jrussell@nodeomega.com>
2018-12-04 11:40:11 -0800

    Contributor Info

--- types/jsrender/index.d.ts
+++ types/jsrender/index.d.ts
@@ -1,6 +1,7 @@
 // Type definitions for JsRender
 // Project: http://www.jsviews.com/#jsrender
 // Definitions by: Kensuke Matsuzaki <https://github.com/zakki>
+//                 Jon Russell <https://github.com/nodeomega>
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
 // TypeScript Version: 2.3
 
