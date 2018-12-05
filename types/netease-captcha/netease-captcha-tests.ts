3c1d73bddbe0f8b771798a2ce355846425b6f3e5
Minjie Shen <minjie@canva.com>
2018-12-05 16:57:56 +1100

    Add typing for netease-captcha package

---
+++ types/netease-captcha/netease-captcha-tests.ts
@@ -0,0 +1,21 @@
+const config : NeteaseCaptcha.Config = {
+        captchaId: 'FAKE ID',
+        element: '#captcha',
+        mode: 'popup',
+        protocol: 'https',
+        width: '200px',
+        lang: 'en',
+        onVerify: (error: any, data?: NeteaseCaptcha.Data) => {
+            console.log(error, data);
+        }
+    };
+
+const onLoad : NeteaseCaptcha.onLoad = (instance : NeteaseCaptcha.Instance) => void {
+    instance.refresh();
+    instance.destroy();
+}
+
+function init(initNECaptcha : NeteaseCaptcha.InitFunction) => void {
+    initNECaptcha(config, onLoad);
+}
+
