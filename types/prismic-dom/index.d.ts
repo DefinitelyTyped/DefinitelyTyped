b4a4d6de289b1859054a71ea90bdcb39539550ca
Kaveh Sajjadi <kaveh.sajjadi@gmail.com>
2018-12-05 14:46:30 +1100

    Add HTMLSerializer type

--- types/prismic-dom/index.d.ts
+++ types/prismic-dom/index.d.ts
@@ -1,17 +1,26 @@
-// Type definitions for prismic-dom 2.0
+// Type definitions for prismic-dom 2.1
 // Project: https://github.com/prismicio/prismic-dom#readme
 // Definitions by: Nick Whyte <https://github.com/nickw444>
 //                 Siggy Bilstein <https://github.com/sbilstein>
+//                 Kaveh Sajjadi <https://github.com/kavehsajjadi>
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
 
 interface RichText {
-    asHtml(richText: any, linkResolver?: (doc: any) => string): string;
-    asText(richText: any,  joinString?: string): string;
+    asHtml<T>(richText: any, linkResolver?: (doc: any) => string, htmlSersializer?: HTMLSerializer<T>): string;
+    asText(richText: any, joinString?: string): string;
 }
 
 interface Link {
     url(link: any, linkResolver?: (doc: any) => string): string;
 }
+
+export type HTMLSerializer<T> = (
+    type: string,
+    element: any,
+    content?: string | null,
+    children?: T[],
+    index?: number,
+) => T
 
 export const RichText: RichText;
 export const Link: Link;
