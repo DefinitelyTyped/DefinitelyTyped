321271c1c3e0337fe42005e8e0425ad69664f77c
Daniel Rose <daniel.rose@inform-software.com>
2018-12-05 09:21:57 +0100

    Use standard dtslint settings.

--- types/fleximap/index.d.ts
+++ types/fleximap/index.d.ts
@@ -1,4 +1,4 @@
-// Type definitions for fleximap 0.9.10
+// Type definitions for fleximap 0.9
 // Project: https://github.com/SocketCluster/fleximap
 // Definitions by: Daniel Rose <https://github.com/DanielRose>
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
