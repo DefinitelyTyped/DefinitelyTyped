84aed54793d7e0c8962213aabac41fa172d32a5c
Riley <rtbrown560@gmail.com>
2018-12-05 11:17:44 -0700

    Should be just a simple object for radio options

--- types/storybook__addon-knobs/index.d.ts
+++ types/storybook__addon-knobs/index.d.ts
@@ -47,7 +47,7 @@ export function color(name: string, value: string, groupId?: string): string;
 
 export function object<T>(name: string, value: T, groupId?: string): T;
 
-export function radios(name: string, options: KnobOption<T>, value: string | null, groupId?: string): string;
+export function radios(name: string, options: object, value: string | null, groupId?: string): string;
 
 export type SelectValue = string | number;
 export function select<T extends string>(name: string, options: { [s: string]: string }, value: T, groupId?: string): T;
