8b5e6afb2fee6cb6ae8b31845d18e4ff85bc49ba
Alan Plum <me@pluma.io>
2018-12-05 23:39:57 +0100

    Unbreak the build

--- types/json_ml/index.d.ts
+++ types/json_ml/index.d.ts
@@ -2,6 +2,7 @@
 // Project: https://github.com/59naga/json-ml
 // Definitions by: Alan Plum <https://github.com/pluma>
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
+// TypeScript Version: 3.0
 
 /// <reference types="cheerio" />
 
