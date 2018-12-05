00f0ac3ca31598c7d723ec7c3dc58c1128c02e30
kiliw <kilocheat@gmail.com>
2018-12-05 11:06:45 +0100

    Add ifElse overload to accept binary functions and link to ramda REPL.

--- types/ramda/ramda-tests.ts
+++ types/ramda/ramda-tests.ts
@@ -2651,6 +2651,17 @@ class Rectangle {
 };
 
 () => {
+    const incCount = R.ifElse(
+        R.has('count'),
+        R.over(R.lensProp('count'), R.inc),
+        R.assoc('count', 1)
+      );
+      incCount({});           // => { count: 1 }
+      incCount({ count: 1 }); // => { count: 2 }
+      R.ifElse(R.identical, R.add as (a: number, b: number) => number, R.always(""))(2, 2); // https://goo.gl/CVUSs9
+};
+
+() => {
     R.isEmpty([1, 2, 3]); // => false
     R.isEmpty([]); // => true
     R.isEmpty(""); // => true
