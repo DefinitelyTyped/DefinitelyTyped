f047781c2fa912f136447ac7bcdd54140ff8fc0b
Christian Sporer <IanStorm28@outlook.com>
2018-12-05 08:27:11 +0100

    Using real name in the definitions file as per "convention" (and it is now anyways visible on my profile)

--- types/list-stream/index.d.ts
+++ types/list-stream/index.d.ts
@@ -1,6 +1,6 @@
 // Type definitions for list-stream 1.0
 // Project: https://github.com/rvagg/list-stream
-// Definitions by: IanStorm <https://github.com/IanStorm>
+// Definitions by: Christian Sporer <https://github.com/IanStorm>
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
 // TypeScript Version: 2.7
 
