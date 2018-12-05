5b92e6f79cd8c387921efa99dda8aa6b923f877d
Craig Bruce <craig@craig-bruce.com>
2018-12-05 11:32:33 +1100

    Had do disable tslint rules for global import in tests as it defeats the object

--- types/chromecast-caf-receiver/chromecast-caf-receiver-tests.ts
+++ types/chromecast-caf-receiver/chromecast-caf-receiver-tests.ts
@@ -2,33 +2,45 @@ import { MediaMetadata } from "chromecast-caf-receiver/cast.framework.messages";
 
 // The following test showcases how you can import individual types directly from the namespace:
 
-const mediaMetadata = new MediaMetadata();
+const mediaMetadata = new MediaMetadata("GENERIC");
 mediaMetadata.metadataType = "TV_SHOW";
 
 // The following tests showcase how you can globally access 'cast' types using
 // the nested namespace style. This is the preferred method as it
 // conforms exactly to the CAF documentation.
 
+// tslint:disable-next-line
 const breaksEvent = new cast.framework.events.BreaksEvent("BREAK_STARTED");
 breaksEvent.breakId = "some-break-id";
 breaksEvent.breakClipId = "some-break-clip-id";
 
+// tslint:disable-next-line
 const track = new cast.framework.messages.Track(1, "TEXT");
+// tslint:disable-next-line
 const breakClip = new cast.framework.messages.BreakClip("id");
+// tslint:disable-next-line
 const adBreak = new cast.framework.messages.Break("id", ["id"], 1);
+// tslint:disable-next-line
 const rEvent = new cast.framework.events.RequestEvent("BITRATE_CHANGED", {
     requestId: 2
 });
+// tslint:disable-next-line
 const pManager = new cast.framework.PlayerManager();
 pManager.addEventListener("STALLED", () => {});
+// tslint:disable-next-line
 const ttManager = new cast.framework.TextTracksManager();
+// tslint:disable-next-line
 const qManager = new cast.framework.QueueManager();
+// tslint:disable-next-line
 const qBase = new cast.framework.QueueBase();
 const items = qBase.fetchItems(1, 3, 4);
+// tslint:disable-next-line
 const breakSeekData = new cast.framework.breaks.BreakSeekData(0, 100, []);
+// tslint:disable-next-line
 const breakClipLoadContext = new cast.framework.breaks.BreakClipLoadInterceptorContext(
     adBreak
 );
+// tslint:disable-next-line
 const breakManager: cast.framework.breaks.BreakManager = {
     getBreakById: () => adBreak,
     getBreakClipById: () => breakClip,
@@ -41,6 +53,7 @@ const breakManager: cast.framework.breaks.BreakManager = {
     setVastTrackingInterceptor: () => {}
 };
 
+// tslint:disable-next-line
 const lrd: cast.framework.messages.LoadRequestData = {
     requestId: 1,
     activeTrackIds: [1, 2],
@@ -58,6 +71,7 @@ const lrd: cast.framework.messages.LoadRequestData = {
     queueData: {}
 };
 
+// tslint:disable-next-line
 const appData: cast.framework.system.ApplicationData = {
     id: () => "id",
     launchingSenderId: () => "launch-id",
@@ -66,8 +80,10 @@ const appData: cast.framework.system.ApplicationData = {
     sessionId: () => 1
 };
 
+// tslint:disable-next-line
 const readyEvent = new cast.framework.system.ReadyEvent(appData);
 const data = readyEvent.data;
+// tslint:disable-next-line
 const pData: cast.framework.ui.PlayerData = {
     breakPercentagePositions: [1],
     contentType: "video",
@@ -79,6 +95,7 @@ const pData: cast.framework.ui.PlayerData = {
     isLive: true,
     isPlayingBreak: false,
     isSeeking: true,
+    // tslint:disable-next-line
     metadata: new cast.framework.messages.MediaMetadata("GENERIC"),
     nextSubtitle: "sub",
     nextThumbnailUrl: "url",
@@ -90,5 +107,6 @@ const pData: cast.framework.ui.PlayerData = {
     title: "title",
     whenSkippable: 321
 };
+// tslint:disable-next-line
 const binder = new cast.framework.ui.PlayerDataBinder(pData);
 binder.addEventListener("ANY_CHANGE", e => {});
