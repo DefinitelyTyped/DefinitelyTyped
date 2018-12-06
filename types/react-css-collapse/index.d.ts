fa958ebb69b88685129d6f7fc8f081ed6f3485ab
danford <dford@staff.compass.edu.au>
2018-12-06 15:50:48 +1100

    remove stray semicolon

--- types/react-css-collapse/index.d.ts
+++ types/react-css-collapse/index.d.ts
@@ -1,5 +1,5 @@
 // Type definitions for react-css-collapse 3.6.0
-// Project: https://github.com/SparebankenVest/react-css-collapse;
+// Project: https://github.com/SparebankenVest/react-css-collapse
 // Definitions by: Daniel Ford <https://github.com/dford07>
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
 // TypeScript Version: 2.8
