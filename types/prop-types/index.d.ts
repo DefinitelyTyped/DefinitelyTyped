fa36a8b5d78de3e75c473d9d76e13dbd4f39b198
Andrew Branch <andrew@wheream.io>
2018-12-06 11:22:17 -0800

    Fix InferProps when there are no required keys

--- types/prop-types/index.d.ts
+++ types/prop-types/index.d.ts
@@ -45,8 +45,8 @@ export type ValidationMap<T> = { [K in keyof T]?: Validator<T[K]> };
 
 export type InferType<V> = V extends Validator<infer T> ? T : any;
 export type InferProps<V> =
-    & InferPropsInner<Pick<V, RequiredKeys<V>>>
-    & Partial<InferPropsInner<Pick<V, OptionalKeys<V>>>>;
+    & (RequiredKeys<V> extends undefined ? {} : InferPropsInner<Pick<V, RequiredKeys<V>>>)
+    & (OptionalKeys<V> extends undefined ? {} : Partial<InferPropsInner<Pick<V, OptionalKeys<V>>>>);
 
 export const any: Requireable<any>;
 export const array: Requireable<any[]>;
