42a5abdf16075d754a8532601a3ee4539d12536b
Kylie Stewart <kylieastewart@gmail.com>
2018-12-06 17:35:02 -0700

    Bump Spectacle version to latest

--- types/spectacle/index.d.ts
+++ types/spectacle/index.d.ts
@@ -1,4 +1,4 @@
-// Type definitions for Spectacle 1.2.0
+// Type definitions for Spectacle 5.2.2
 // Project: https://github.com/FormidableLabs/spectacle
 // Definitions by: Zachary Maybury <https://github.com/zmaybury>
 //                 Kylie Stewart <https://github.com/kale-stew>
