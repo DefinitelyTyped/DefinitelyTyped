a4682d23b072fc58c9cf3333d3a35c5753808d41
runebaas <runebaas@gmail.com>
2018-12-08 01:39:53 +0100

    [microservice-utilities] Rewrite example file

--- types/microservice-utilities/microservice-utilities-tests.ts
+++ types/microservice-utilities/microservice-utilities-tests.ts
@@ -1,13 +1,17 @@
 import { Authorizer, PlatformClient, RequestLogger, ServiceTokenProvider } from 'microservice-utilities';
 
-const authorizer = new Authorizer((msg: any) => msg, {jwkKeyListUrl: 'aaa'});
-const authorizerPolicy = authorizer.getPolicy({test: true});
+(async (): Promise<void> => {
+    const authorizer = new Authorizer((msg: any) => msg, { jwkKeyListUrl: 'aaa' });
+    const authorizerPolicy = authorizer.getPolicy({ test: true });
 
-const platformClient = new PlatformClient((msg: any) => msg);
-platformClient.get('https://www.typescriptlang.org/');
-platformClient.post('https://www.typescriptlang.org/', {testData: 'abc'});
+    const platformClient = new PlatformClient((msg: any) => msg);
+    const result = await platformClient.get('https://www.typescriptlang.org/');
+    const data = result.data as string;
 
-const serviceTokenProvider = new ServiceTokenProvider(platformClient, {});
+    platformClient.post('https://www.typescriptlang.org/', { testData: 'abc' });
 
-const requestLogger = new RequestLogger({jsonSpace: 4});
-requestLogger.log('hello world');
+    const serviceTokenProvider = new ServiceTokenProvider(platformClient, {});
+
+    const requestLogger = new RequestLogger({ jsonSpace: 4 });
+    requestLogger.log('hello world');
+})();
