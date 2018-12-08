9b44edaff66e035bd0ad495e9466447e778297d4
Garrett McCullough <gwmccull@yahoo.com>
2018-12-07 16:04:06 -0800

    update version number

--- types/victory/index.d.ts
+++ types/victory/index.d.ts
@@ -1,4 +1,4 @@
-// Type definitions for Victory 0.9.3
+// Type definitions for Victory 0.9.4
 // Project: https://github.com/FormidableLabs/victory
 // Definitions by: Alexey Svetliakov <https://github.com/asvetliakov>
 //                 snerks <https://github.com/snerks>
