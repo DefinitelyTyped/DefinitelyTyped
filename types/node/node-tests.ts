354829f2f744b1d8968ac0ca715190e399aef0b8
Dimitri Benin <dimitri.benin@gmail.com>
2018-12-08 12:39:23 +0100

    Add types for experimental worker_threads module

--- types/node/node-tests.ts
+++ types/node/node-tests.ts
@@ -17,6 +17,7 @@ import * as path from "path";
 import * as readline from "readline";
 import * as childProcess from "child_process";
 import * as cluster from "cluster";
+import * as workerThreads from "worker_threads";
 import * as os from "os";
 import * as vm from "vm";
 import * as console2 from "console";
@@ -2929,6 +2930,56 @@ async function asyncStreamPipelineFinished() {
                 console.log('worker %d is dead', worker.process.pid);
             }
         });
+    }
+}
+
+//////////////////////////////////////////////////////////////////////
+/// worker_threads tests: https://nodejs.org/api/worker_threads.html ///
+//////////////////////////////////////////////////////////////////////
+
+{
+    {
+        if (workerThreads.isMainThread) {
+            module.exports = async function parseJSAsync(script: string) {
+                return new Promise((resolve, reject) => {
+                    const worker = new workerThreads.Worker(__filename, {
+                        workerData: script
+                    });
+                    worker.on('message', resolve);
+                    worker.on('error', reject);
+                    worker.on('exit', (code) => {
+                        if (code !== 0)
+                            reject(new Error(`Worker stopped with exit code ${code}`));
+                    });
+                });
+            };
+        } else {
+            const script = workerThreads.workerData;
+            workerThreads.parentPort.postMessage(script);
+        }
+    }
+
+    {
+        const { port1, port2 } = new workerThreads.MessageChannel();
+        port1.on('message', (message) => console.log('received', message));
+        port2.postMessage({ foo: 'bar' });
+    }
+
+    {
+        if (workerThreads.isMainThread) {
+            const worker = new workerThreads.Worker(__filename);
+            const subChannel = new workerThreads.MessageChannel();
+            worker.postMessage({ hereIsYourPort: subChannel.port1 }, [subChannel.port1]);
+            subChannel.port2.on('message', (value) => {
+                console.log('received:', value);
+            });
+        } else {
+            workerThreads.parentPort.once('message', (value) => {
+                assert(value.hereIsYourPort instanceof MessagePort);
+                value.hereIsYourPort.postMessage('the worker is sending this');
+                value.hereIsYourPort.close();
+            });
+        }
     }
 }
 
