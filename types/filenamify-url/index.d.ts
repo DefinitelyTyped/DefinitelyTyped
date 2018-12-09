d708da03b6ddd096d8aee031e22627bc568dd322
Carlos Precioso <kalprestito@gmail.com>
2018-12-09 12:52:57 +0100

    Add filenamify-url

---
+++ types/filenamify-url/index.d.ts
@@ -0,0 +1,7 @@
+// Type definitions for filenamify-url 1.0
+// Project: https://github.com/sindresorhus/filenamify-url
+// Definitions by: My Self <https://github.com/cprecioso>
+// Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
+
+import * as filenamifyUrl from "filenamify";
+export = filenamifyUrl;
