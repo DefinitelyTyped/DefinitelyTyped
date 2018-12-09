703717f428b06d19fad377748bf3f835ac9580e8
Carlos Precioso <kalprestito@gmail.com>
2018-12-09 15:38:13 +0100

    [ink] Upgrade typescript requirement in line with prop-types

--- types/ink/index.d.ts
+++ types/ink/index.d.ts
@@ -2,6 +2,7 @@
 // Project: https://github.com/vadimdemedes/ink#readme
 // Definitions by: Carlos Precioso <https://github.com/cprecioso>
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
+// TypeScript Version: 2.8
 
 /// <reference types="node" />
 
