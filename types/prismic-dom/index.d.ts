a55f6c9ba9351081b7e5588f1f9e015b0b4eda39
Kaveh Sajjadi <kaveh.sajjadi@gmail.com>
2018-12-10 15:43:29 +1100

    add prismic-richtext NodeElement type

--- types/prismic-dom/index.d.ts
+++ types/prismic-dom/index.d.ts
@@ -5,8 +5,20 @@
 //                 Kaveh Sajjadi <https://github.com/kavehsajjadi>
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
 
+import * as nodes from "prismic-richtext/src/nodes";
+
+// This exists because HTMLSerializers will render `element` to a string
+// Without this, something like `element.alt` will throw a type error
+// despite _working in prismic-richtext_ go figure :|
+export type NodeElement = nodes.NodeElement & {
+    url?: string,
+    alt?: string
+}
+
+export type Serializer<T> = (type: string, element: NodeElement, text: string | null, children: T[], index: number) => T;
+
 interface RichText {
-    asHtml(richText: any, linkResolver?: (doc: any) => string, htmlSersializer?: HTMLSerializer): string;
+    asHtml(richText: any, linkResolver?: (doc: any) => string, htmlSersializer?: Serializer<any>): string;
     asText(richText: any, joinString?: string): string;
     Elements: Elements;
 }
@@ -15,38 +27,30 @@ interface Link {
     url(link: any, linkResolver?: (doc: any) => string): string;
 }
 
-export type HTMLSerializer = (
-    type: string,
-    element: any,
-    content?: string | null,
-    children?: any[],
-    index?: number,
-) => any;
-
 export const RichText: RichText;
 export const Link: Link;
 
 declare const _default: { RichText: RichText, Link: Link };
 export default _default;
 
-interface Elements {
-    heading1: any;
-    heading2: any;
-    heading3: any;
-    heading4: any;
-    heading5: any;
-    heading6: any;
-    paragraph: any;
-    preformatted: any;
-    strong: any;
-    em: any;
-    listItem: any;
-    oListItem: any;
-    list: any;
-    oList: any;
-    image: any;
-    embed: any;
-    hyperlink: any;
-    label: any;
-    span: any;
-}
+export type Elements = {
+    heading1: "heading1",
+    heading2: "heading2",
+    heading3: "heading3",
+    heading4: "heading4",
+    heading5: "heading5",
+    heading6: "heading6",
+    paragraph: "paragraph",
+    preformatted: "preformatted",
+    strong: "strong",
+    em: "em",
+    listItem: "list-item",
+    oListItem: "o-list-item",
+    list: "group-list-item",
+    oList: "group-o-list-item",
+    image: "image",
+    embed: "embed",
+    hyperlink: "hyperlink",
+    label: "label",
+    span: "span",
+};
