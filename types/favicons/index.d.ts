242eb56ec6748c52a6b21182a75591c2b927d318
Nikk Radetskiy <metsawyr@gmail.com>
2018-12-10 19:29:00 +0200

    version bump

--- types/favicons/index.d.ts
+++ types/favicons/index.d.ts
@@ -1,4 +1,4 @@
-// Type definitions for favicons 5.1
+// Type definitions for favicons 5.3
 // Project: https://github.com/evilebottnawi/favicons/blob/master/package.json
 // Definitions by: Mohsen Azimi <https://github.com/mohsen1>, Nikk Radetskiy <https://github.com/metsawyr>
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
