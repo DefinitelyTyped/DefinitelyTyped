02d64c33574bff2ed6122aec973d0c83004f0c6b
Junyoung Choi <fluke8259@gmail.com>
2018-12-10 10:55:40 +0900

    Add typescript version

--- types/is-buffer/index.d.ts
+++ types/is-buffer/index.d.ts
@@ -2,6 +2,7 @@
 // Project: https://github.com/feross/is-buffer#readme
 // Definitions by: Junyoung Choi <https://github.com/rokt33r>
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
+// TypeScript Version: 3.0
 
 /// <reference types='node' />
 
