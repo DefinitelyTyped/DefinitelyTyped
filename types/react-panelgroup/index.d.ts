4ff411d8c3b8bcb3e9b92d3edded31e2489716e2
Quentin Golsteyn <qgolsteyn@gmail.com>
2018-12-09 17:54:08 -0800

    Add name

--- types/react-panelgroup/index.d.ts
+++ types/react-panelgroup/index.d.ts
@@ -1,6 +1,6 @@
 // Type definitions for react-panelgroup 1.0
 // Project: https://github.com/DanFessler/react-panelgroup
-// Definitions by: My Self <https://github.com/qgolsteyn>
+// Definitions by: Quentin Golsteyn <https://github.com/qgolsteyn>
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
 // TypeScript Version: 3.0
 
