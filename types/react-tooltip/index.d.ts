f481d05ee6866c0deaced9c0fbe62211f182a662
Vijayasingam Thanasekaran <t.vijayasingam@gmail.com>
2018-12-11 23:03:25 +0800

    Removed Patch Version Update

--- types/react-tooltip/index.d.ts
+++ types/react-tooltip/index.d.ts
@@ -1,4 +1,4 @@
-// Type definitions for react-tooltip 3.9.0
+// Type definitions for react-tooltip 3.9
 // Project: https://github.com/wwayne/react-tooltip
 // Definitions by: Deividas Bakanas <https://github.com/DeividasBakanas>, Vijayasingam <https://github.com/Vijayasingam>
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
