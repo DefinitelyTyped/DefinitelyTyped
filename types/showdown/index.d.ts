7d2e44dab89402a2ac93cf365d1ee871f123fa7d
Jesse Donat <jdonat@myon.com>
2018-12-11 11:31:35 -0600

    Updates header version number

--- types/showdown/index.d.ts
+++ types/showdown/index.d.ts
@@ -1,4 +1,4 @@
-// Type definitions for Showdown 1.7.3
+// Type definitions for Showdown 1.9.0
 // Project: https://github.com/coreyti/showdown
 // Definitions by: cbowdon <https://github.com/cbowdon>, Pei-Tang Huang <https://github.com/tan9>, Ariel-Saldana <https://github.com/arielsaldana>
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
