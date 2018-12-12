f8077c82027918706a453dbb6b1b89b9729dc309
Nokky Goren <30292793+ApeNox@users.noreply.github.com>
2018-12-12 15:20:49 +0200

    update contributor name

--- types/react-places-autocomplete/index.d.ts
+++ types/react-places-autocomplete/index.d.ts
@@ -2,7 +2,7 @@
 // Project: https://github.com/kenny-hibino/react-places-autocomplete/
 // Definitions by: Guilherme Hübner <https://github.com/guilhermehubner>
 //                 Andrew Makarov <https://github.com/r3nya>
-//                 Nokky Goren
+//                 Nokky Goren <https://github.com/ApeNox>
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
 // TypeScript Version: 2.8
 //
