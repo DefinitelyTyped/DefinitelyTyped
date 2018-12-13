e265533307c820a080e2a5e5d0e2a50dec957d01
Bryan Kendall <bkend@google.com>
2018-12-13 15:01:12 -0800

    multi-progress version to '2.0'

--- types/multi-progress/index.d.ts
+++ types/multi-progress/index.d.ts
@@ -1,4 +1,4 @@
-// Type definitions for multi-progress 2.0.0
+// Type definitions for multi-progress 2.0
 // Project: https://github.com/pitaj/multi-progress
 // Definitions by: David Brett <https://github.com/DHBrett>
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
