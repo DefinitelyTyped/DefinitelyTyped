3e1cfd5f9ac3c4f8a4d449954ed378314762398f
Alexander Wong <alex@udia.ca>
2018-12-13 12:26:57 -0700

    Made serverless-http type definitions more generic

--- types/serverless-http/index.d.ts
+++ types/serverless-http/index.d.ts
@@ -5,16 +5,23 @@
 // TypeScript Version: 2.3
 
 import { Context, Callback } from "aws-lambda";
-import { Application as ExpressApplication } from "express";
-import * as KoaApplication from "koa";
-import { Server as ConnectApplication } from "connect";
 
 declare namespace ServerlessHttp {
+    type FrameworkApplication = {
+        handle: Function;
+        router: {
+            route: Function;
+        }
+        _core: {
+            _dispatch: Function;
+        }
+    }
+    type HandlerCompatibleApp = Function | Partial<FrameworkApplication>;
     type LambdaPartial = (event: any, context: Context, callback?: Callback) => void;
 }
 
 declare function serverlessHttp(
-    app: (ExpressApplication | KoaApplication | ConnectApplication),
+    app: ServerlessHttp.HandlerCompatibleApp,
     opts?: any
 ): ServerlessHttp.LambdaPartial;
 
--- types/serverless-http/serverless-http-tests.ts
+++ types/serverless-http/serverless-http-tests.ts
@@ -1,13 +1,9 @@
 import serverlessHttp from "serverless-http";
 import * as express from "express";
-import * as koa from "koa";
 import * as connect from "connect";
 
 // express application
 serverlessHttp(express());
-
-// Koa application
-serverlessHttp(new koa());
 
 // Connect application
 serverlessHttp(connect());
