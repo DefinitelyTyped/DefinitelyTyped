0153a990fa81120d4ec60550b5ccb9f25dfff141
nkgrnkgr <beefupme@gmail.com>
2018-12-14 23:24:43 +0900

    increase the version number

--- types/react-color/index.d.ts
+++ types/react-color/index.d.ts
@@ -1,4 +1,4 @@
-// Type definitions for react-color 2.13
+// Type definitions for react-color 2.14
 // Project: https://github.com/casesandberg/react-color/
 // Definitions by: Karol Janyst <https://github.com/LKay>, Marks Polakovs <https://github.com/markspolakovs>, Matthieu Montaudouin <https://github.com/mntdn>, Nokogiri <https://github.com/nkgrnkgr>
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
