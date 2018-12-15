0d0e02dc2f8b99d8b9ad5e45e4a17e00e1b00190
Michelle Scharlock <mischarl@microsoft.com>
2018-12-14 16:17:16 -0800

    Update custom-functions-runtime-tests.ts

--- types/custom-functions-runtime/custom-functions-runtime-tests.ts
+++ types/custom-functions-runtime/custom-functions-runtime-tests.ts
@@ -1,4 +1,4 @@
-/* Note that this is a sample function for the purpose of running the below CustomFunctions.Associate() test */
+/* Note that this is a sample function for the purpose of running the below CustomFunctions.associate() test */
 function add10(n: number) {
     return n + 10;
 }
