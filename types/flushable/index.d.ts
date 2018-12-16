f9f74af90ed31afb0f70cabb5281b906e32c956d
Parth Mehta <tech@iamdifferent.io>
2018-12-16 21:36:52 +1100

    :bug: fixed structure according to guidelines in the DefinitelyTyped's README

--- types/flushable/index.d.ts
+++ types/flushable/index.d.ts
@@ -1,8 +1,7 @@
-// Type definitions for flushable 1.0.0
-// Project: https://github.com/petegleeson/flushable
+// Type definitions for flushable 1.0
+// Project: https://github.com/petegleeson/flushable#readme
 // Definitions by: Parth Mehta <https://github.com/pash90>
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
-// TypeScript Version: 3.0
 
 export type FlushableOnCompleteHandler = (flushed: boolean) => any;
 
