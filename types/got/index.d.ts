4c4dc3a84956b0cea6695690ed74ebba251ad13e
ikokostya <ikokostya@gmail.com>
2018-12-16 18:49:33 +0300

    [got] Add more timeout options for v9

--- types/got/index.d.ts
+++ types/got/index.d.ts
@@ -140,9 +140,44 @@ declare namespace got {
         cache?: Cache;
     }
 
+    /**
+     * Constrains the duration of each phase of the request lifecycle.
+     *
+     * @see https://github.com/sindresorhus/got#timeout
+     */
     interface TimeoutOptions {
+        /**
+         * Starts when a socket is assigned and ends when the hostname has been resolved. Does not
+         * apply when using a Unix domain socket.
+         */
+        lookup?: number;
+        /**
+         * Starts when `lookup` completes (or when the socket is assigned if lookup does not apply
+         * to the request) and ends when the socket is connected.
+         */
         connect?: number;
+        /**
+         * Starts when `connect` completes and ends when the handshaking process completes (HTTPS
+         * only).
+         */
+        secureConnect?: number;
+        /**
+         * Starts when the socket is connected. See [request.setTimeout](https://nodejs.org/api/http.html#http_request_settimeout_timeout_callback).
+         */
         socket?: number;
+        /**
+         * Starts when the request has been written to the socket and ends when the response headers
+         * are received.
+         */
+        response?: number;
+        /**
+         * Starts when the socket is connected and ends with the request has been written to the
+         * socket.
+         */
+        send?: number;
+        /**
+         * Starts when the request is initiated and ends when the response's end event fires.
+         */
         request?: number;
     }
 
