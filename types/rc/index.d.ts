ec8b72f24fc47947136dafbff73f5793ceba2a3a
Andrew Bradley <cspotcode@gmail.com>
2018-12-16 15:11:52 -0500

    Clarify `argv` and `parse` arguments

--- types/rc/index.d.ts
+++ types/rc/index.d.ts
@@ -6,7 +6,15 @@
 declare function rc(
 	name: string,
 	defaults?: any,
+    /**
+     * Parsed argv object.  For example, if args were '--foo bar' then this value might be {foo: 'bar'}
+     * If null then rc's default argv parser will parse process.argv
+     */
 	argv?: {} | null,
+    /**
+     * Custom config file parser.
+      * Will be passed the string contents of each discovered config file and should return a parsed object dictionary.
+     */
 	parse?: ((content: string) => any) | null
 ): any;
 
