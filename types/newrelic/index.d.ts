641c9b6a1ae6c42883934297e30e029b92d55593
Michael Bond <software@the-forgotten.org>
2018-12-16 19:03:02 -0500

    Update version per PR Request.

--- types/newrelic/index.d.ts
+++ types/newrelic/index.d.ts
@@ -1,4 +1,4 @@
-// Type definitions for newrelic 3.4
+// Type definitions for newrelic 4.11
 // Project: http://github.com/newrelic/node-newrelic
 // Definitions by: Matt R. Wilson <https://github.com/mastermatt>
 //                 Brooks Patton <https://github.com/brookspatton>
