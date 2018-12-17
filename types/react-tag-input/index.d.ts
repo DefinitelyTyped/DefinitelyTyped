f60a638f529a9c3dfea3562133f1856ef6bc6a2e
Todd Dukart <786421+tdukart@users.noreply.github.com>
2018-12-17 15:42:27 -0500

    Remove patch version from input.d.ts

--- types/react-tag-input/index.d.ts
+++ types/react-tag-input/index.d.ts
@@ -1,4 +1,4 @@
-// Type definitions for React-Tags (react-tag-input) 6.1.0
+// Type definitions for React-Tags (react-tag-input) 6.1
 // Project: https://github.com/prakhar1989/react-tags
 // Definitions by: Ogglas <https://github.com/Ogglas>
 //                  Jan Karres <https://github.com/jankarres>
