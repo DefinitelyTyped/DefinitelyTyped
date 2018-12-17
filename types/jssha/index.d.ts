a2bd092cadf78793bbdc2a863e40124b85022baa
Kannan Goundan <kannan@cakoose.com>
2018-12-17 05:58:01 -0800

    jssha: Add myself to "Definitions by" list

--- types/jssha/index.d.ts
+++ types/jssha/index.d.ts
@@ -1,6 +1,8 @@
 // Type definitions for jsSHA 2.0
 // Project: https://github.com/Caligatio/jsSHA
-// Definitions by: David Li <https://github.com/randombk>, Tobias Kahlert <https://github.com/SrTobi>
+// Definitions by: David Li <https://github.com/randombk>
+//                 Tobias Kahlert <https://github.com/SrTobi>
+//                 Kannan Goundan <https://github.com/cakoose>
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
 
 declare namespace jsSHA {
