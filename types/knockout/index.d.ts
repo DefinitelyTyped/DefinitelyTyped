043f16d7e60a8fccd9087c6d3982f57f122b61dd
ltlombardi <rrocha5@slb.com>
2018-12-17 09:09:44 -0200

    yet again

--- types/knockout/index.d.ts
+++ types/knockout/index.d.ts
@@ -13,7 +13,7 @@
 
 interface KnockoutSubscribableFunctions<T> {
     notifySubscribers(valueToWrite?: T, event?: string): void;
-} // test 2
+} // test 3
 
 interface KnockoutComputedFunctions<T> {
 }
