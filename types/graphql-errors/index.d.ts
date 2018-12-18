d3be2c81faa2d828d9716dedff22a7008d480886
Matías Olivera <matiasolivera@outlook.com>
2018-12-18 13:33:45 -0300

    [graphql-errors] Change type case to StudlyCase

--- types/graphql-errors/index.d.ts
+++ types/graphql-errors/index.d.ts
@@ -6,11 +6,11 @@
 
 import { GraphQLSchema } from 'graphql';
 
-export type handlerFunction = (err: Error) => Error;
+export type HandlerFunction = (err: Error) => Error;
 
-export function setDefaultHandler(fn: handlerFunction): void;
+export function setDefaultHandler(fn: HandlerFunction): void;
 
-export function maskErrors(schema: GraphQLSchema, fn?: handlerFunction): void;
+export function maskErrors(schema: GraphQLSchema, fn?: HandlerFunction): void;
 
 export class UserError extends Error {
     constructor(message: string);
