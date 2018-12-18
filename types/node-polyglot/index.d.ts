e6a8fd232f5c299206dae3531f47a2dd598bd49a
liamross <liamross.code@gmail.com>
2018-12-18 10:48:16 -0800

    bumped package version

--- types/node-polyglot/index.d.ts
+++ types/node-polyglot/index.d.ts
@@ -1,8 +1,8 @@
-// Type definitions for node-polyglot v0.4.2
+// Type definitions for node-polyglot v0.4.3
 // Project: https://github.com/airbnb/polyglot.js
 // Definitions by: Tim Jackson-Kiely <https://github.com/timjk>
+//                 Liam Ross <https://github.com/liamross>
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
-
 
 declare namespace Polyglot {
     interface InterpolationOptions {
