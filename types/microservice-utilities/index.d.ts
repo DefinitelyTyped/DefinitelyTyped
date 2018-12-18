ab9d47b6ec817328ae6f21540cb2889aa8e45760
Daan Boerlage <dboerlage@cimpress.com>
2018-12-18 13:41:06 +0100

    [microservice-utilities] Fix typing issue on the PlatformClient constructor

--- types/microservice-utilities/index.d.ts
+++ types/microservice-utilities/index.d.ts
@@ -1,4 +1,4 @@
-// Type definitions for microservice-utilities 0.1
+// Type definitions for microservice-utilities 0.2
 // Project: https://github.com/Cimpress-MCP/microservice-utilities.js
 // Definitions by: Daan Boerlage <https://github.com/runebaas>
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
@@ -41,7 +41,7 @@ export interface PlatformClientResponse {
 }
 
 export class PlatformClient {
-    constructor(logFunction: (msg: any) => void, tokenResolverFunction?: Promise<() => string>, configuration?: PlatformClientConfiguration)
+    constructor(logFunction: (msg: any) => void, tokenResolverFunction?: () => Promise<string>, configuration?: PlatformClientConfiguration)
     get(url: string, headers?: { [s: string]: string; }, type?: string): Promise<PlatformClientResponse>;
     post(url: string, data: object, headers?: { [s: string]: string; }): Promise<PlatformClientResponse>;
     put(url: string, data: object, headers?: { [s: string]: string; }): Promise<PlatformClientResponse>;
