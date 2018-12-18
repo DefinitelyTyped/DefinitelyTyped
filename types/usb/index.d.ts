ec3affa0f637715c0e597d7010d9c8430b79d728
David Sanders <dsanders11@ucsbalum.com>
2018-12-18 10:43:04 -0700

    Fixes from review

--- types/usb/index.d.ts
+++ types/usb/index.d.ts
@@ -8,7 +8,7 @@
 
 import { EventEmitter } from "events";
 
-export class libusbException extends Error {
+export class LibUSBException extends Error {
   errno: number;
 }
 
@@ -29,7 +29,7 @@ export class Device {
   open(defaultConfig?: boolean): void;
   close(): void;
   interface(addr: number): Interface;
-  controlTransfer(bmRequestType: number, bRequest: number, wValue: number, wIndex: number, data_or_length: any, callback: (error?: libusbException, buf?: Buffer) => void): Device;
+  controlTransfer(bmRequestType: number, bRequest: number, wValue: number, wIndex: number, data_or_length: any, callback: (error?: LibUSBException, buf?: Buffer) => void): Device;
   getStringDescriptor(desc_index: number, callback: (error?: string, buf?: Buffer) => void): void;
   setConfiguration(desired: number, cb: (err?: string) => void): void;
   reset(callback: (err?: string) => void): void;
@@ -108,7 +108,7 @@ export class InEndpoint extends EventEmitter implements Endpoint {
   timeout: number;
   descriptor: EndpointDescriptor;
   constructor(device: Device, descriptor: EndpointDescriptor);
-  transfer(length: number, callback: (error: libusbException, data: Buffer) => void): InEndpoint;
+  transfer(length: number, callback: (error: LibUSBException, data: Buffer) => void): InEndpoint;
   startPoll(nTransfers?: number, transferSize?: number): void;
   stopPoll(cb?: () => void): void;
 }
@@ -119,8 +119,8 @@ export class OutEndpoint extends EventEmitter implements Endpoint {
   timeout: number;
   descriptor: EndpointDescriptor;
   constructor(device: Device, descriptor: EndpointDescriptor);
-  transfer(buffer: Buffer, cb: (err?: libusbException) => void): OutEndpoint;
-  transferWithZLP(buf: Buffer, cb: (err?: libusbException) => void): void;
+  transfer(buffer: Buffer, cb: (err?: LibUSBException) => void): OutEndpoint;
+  transferWithZLP(buf: Buffer, cb: (err?: LibUSBException) => void): void;
 }
 
 export class EndpointDescriptor {
