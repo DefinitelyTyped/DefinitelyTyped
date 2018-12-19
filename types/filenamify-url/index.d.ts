80cf80081a6e9bc338a339d659928c8d8f8bdb02
Carlos Precioso <cprecioso@users.noreply.github.com>
2018-12-19 18:10:04 +0100

    [filenamify-url] Add author name

--- types/filenamify-url/index.d.ts
+++ types/filenamify-url/index.d.ts
@@ -1,6 +1,6 @@
 // Type definitions for filenamify-url 1.0
 // Project: https://github.com/sindresorhus/filenamify-url
-// Definitions by: My Self <https://github.com/cprecioso>
+// Definitions by: Carlos Precioso <https://github.com/cprecioso>
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
 
 import * as filenamifyUrl from "filenamify";
