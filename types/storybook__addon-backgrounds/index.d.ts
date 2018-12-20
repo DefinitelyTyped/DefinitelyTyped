81dec5c332a8f1a04b3305e3a81391b06f289e42
adhrinae <adhrinae@gmail.com>
2018-12-21 00:40:05 +0900

    [@storybook/addon-backgrounds] Fix wrong version

--- types/storybook__addon-backgrounds/index.d.ts
+++ types/storybook__addon-backgrounds/index.d.ts
@@ -1,4 +1,4 @@
-// Type definitions for @storybook/addon-backgrounds 4.3.1
+// Type definitions for @storybook/addon-backgrounds 4.1
 // Project: https://github.com/storybooks/storybook
 // Definitions by: HyunSeob <https://github.com/hyunseob>
 //                 An Dohyung <https://github.com/adhrinae>
