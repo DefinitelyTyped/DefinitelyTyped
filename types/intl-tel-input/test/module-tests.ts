5196b80b78f9565731377379df46217ce5602ba4
Leonard Thieu <leonard.thieu@gmail.com>
2018-12-21 13:37:52 -0500

    [intl-tel-input] Hide types that are not imported in modules.

--- types/intl-tel-input/test/module-tests.ts
+++ types/intl-tel-input/test/module-tests.ts
@@ -2,3 +2,8 @@ import intlTelInput = require('intl-tel-input');
 
 // $ExpectType (node: Element, options?: Options) => Plugin
 intlTelInput;
+
+// $ExpectError
+const options: IntlTelInput.Options = {
+    allowDropdown: true
+};
