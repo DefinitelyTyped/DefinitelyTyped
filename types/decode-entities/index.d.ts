d215b4741d86e87c4aecbd5311a352a159799fa0
Nicolas <36765044+waspothegreat@users.noreply.github.com>
2018-12-21 15:13:21 -0500

    Update index.d.ts

--- types/decode-entities/index.d.ts
+++ types/decode-entities/index.d.ts
@@ -1,6 +1,6 @@
 // Type definitions for decode-entities 1.0
 // Project: https://github.com/waspothegreat/decode-entities#readme
-// Definitions by: waspothegreat <https://github.com/waspothegreat/>
+// Definitions by: waspothegreat <https://github.com/waspothegreat>
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
 
 declare function decode(encodedString: string): string;
