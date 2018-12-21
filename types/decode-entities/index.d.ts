afa0c1e93a7a86baab48af8710ab265dfac87f4e
Nicolas <nicolas.husereau@theojcs.ca>
2018-12-21 11:49:02 -0500

    Add decode-entities type definitions

---
+++ types/decode-entities/index.d.ts
@@ -0,0 +1,8 @@
+// Type definition for decode-entities v1.0.4
+// Project: https://github.com/waspothegreat/decode-entities#readme
+// Definitions by: waspothegreat <https://github.com/waspothegreat/>
+// Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
+
+declare function decode(encodedString: string): string;
+
+export = decode;
