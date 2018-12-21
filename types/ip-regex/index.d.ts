f3f109d840ba9d44fe5d0d8b0495028609705a9c
Renārs Vilnis <renars.vilnis@gmail.com>
2018-12-21 23:21:43 +0200

    Remove "declare module ..." wrapper and revert back to "import =" syntax in test

--- types/ip-regex/index.d.ts
+++ types/ip-regex/index.d.ts
@@ -3,17 +3,14 @@
 // Definitions by: Renārs Vilnis <https://github.com/renarsvilnis>
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
 
-// tslint:disable-next-line no-single-declare-module no-declare-current-package
-declare module "ip-regex" {
-    function ipRegex(options?: ipRegex.IpRegexOptions): RegExp;
+declare function ip(options?: ip.IpRegexOptions): RegExp;
 
-    namespace ipRegex {
-        interface IpRegexOptions {
-            exact?: boolean;
-        }
-
-        function v4(options?: IpRegexOptions): RegExp;
-        function v6(options?: IpRegexOptions): RegExp;
+declare namespace ip {
+    interface IpRegexOptions {
+        exact?: boolean;
     }
-    export = ipRegex;
+
+    function v4(options?: IpRegexOptions): RegExp;
+    function v6(options?: IpRegexOptions): RegExp;
 }
+export = ip;
--- types/ip-regex/ip-regex-tests.ts
+++ types/ip-regex/ip-regex-tests.ts
@@ -1,4 +1,4 @@
-import * as ipRegex from 'ip-regex';
+import ipRegex = require('ip-regex');
 
 ipRegex().test('unicorn 192.168.0.1 cake 1:2:3:4:5:6:7:8 rainbow');
 ipRegex({exact: true}).test('unicorn 192.168.0.1 cake 1:2:3:4:5:6:7:8 rainbow');
