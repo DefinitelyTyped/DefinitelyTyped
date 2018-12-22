5f3eac430fe99ffc4aa8e9922190edb429e2b222
Andrew Vetovitz <AndrewVetovitz@gmail.com>
2018-12-21 20:01:12 -0500

    updating patch version again

--- types/passport-linkedin-oauth2/index.d.ts
+++ types/passport-linkedin-oauth2/index.d.ts
@@ -1,4 +1,4 @@
-// Type definitions for passport-linkedin-oauth2 1.5.0
+// Type definitions for passport-linkedin-oauth2 1.5
 // Project: https://github.com/auth0/passport-linkedin-oauth2
 // Definitions by: Andrew Vetovitz <https://github.com/andrewvetovitz>
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
