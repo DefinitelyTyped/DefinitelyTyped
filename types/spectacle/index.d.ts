0f6699af963f7d1a66a0c34d24697b766448b003
Tegan Churchill <churchill.tegan@gmail.com>
2018-12-21 17:04:42 -0800

    Update Spectacle version number

--- types/spectacle/index.d.ts
+++ types/spectacle/index.d.ts
@@ -1,4 +1,4 @@
-// Type definitions for Spectacle 5.2.2
+// Type definitions for Spectacle 5.3.0
 // Project: https://github.com/FormidableLabs/spectacle
 // Definitions by: Zachary Maybury <https://github.com/zmaybury>
 //                 Kylie Stewart <https://github.com/kale-stew>
