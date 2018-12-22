e2b5e6a3776a10930ea7f8b7b9a47425245c739c
Andrew Vetovitz <AndrewVetovitz@gmail.com>
2018-12-21 19:59:35 -0500

    update headers for passport-linkedin

--- types/passport-linkedin-oauth2/index.d.ts
+++ types/passport-linkedin-oauth2/index.d.ts
@@ -1,10 +1,8 @@
-// Type definitions for passport-linkedin-oauth2
+// Type definitions for passport-linkedin-oauth2 1.5.0
 // Project: https://github.com/auth0/passport-linkedin-oauth2
 // Definitions by: Andrew Vetovitz <https://github.com/andrewvetovitz>
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
 // TypeScript Version: 2.8
-
-/// <reference types="passport"/>
 
 import { Profile as passportProfile, AuthenticateOptions, Strategy as passportStrategy } from 'passport';
 import { Request } from 'express';
