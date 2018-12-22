c85aed7a91f009387731ea1899742c0a984eb62a
Renārs Vilnis <renars.vilnis@gmail.com>
2018-12-23 00:11:56 +0200

    Add ioredis client support

--- types/connect-redis/connect-redis-tests.ts
+++ types/connect-redis/connect-redis-tests.ts
@@ -1,10 +1,16 @@
 import connectRedis = require("connect-redis");
 import session = require("express-session");
+import IORedisClient = require("ioredis");
 
-let RedisStore = connectRedis(session);
+const RedisStore = connectRedis(session);
 const store = new RedisStore({
     host: 'localhost',
     port: 6379,
     logErrors: error => console.warn(error),
     scanCount: 80,
+});
+
+const ioRedisClient = new IORedisClient();
+const anotherStore = new RedisStore({
+    client: ioRedisClient
 });
--- types/connect-redis/index.d.ts
+++ types/connect-redis/index.d.ts
@@ -1,17 +1,20 @@
 // Type definitions for connect-redis
 // Project: https://npmjs.com/package/connect-redis
 // Definitions by: Xavier Stouder <https://github.com/xstoudi>
+//                 Renārs Vilnis <https://github.com/renarsvilnis>
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
 // TypeScript Version: 2.2
 
 /// <reference types="express" />
 /// <reference types="express-session" />
 /// <reference types="redis" />
+/// <reference types="ioredis" />
 
 declare module "connect-redis" {
     import * as express from "express";
     import * as session from "express-session";
     import * as redis from "redis";
+    import * as ioredis from "ioredis";
 
 
     function s(options: (options?: session.SessionOptions) => express.RequestHandler): s.RedisStore;
@@ -21,7 +24,7 @@ declare module "connect-redis" {
             new (options: RedisStoreOptions): session.Store;
         }
         interface RedisStoreOptions {
-            client?: redis.RedisClient;
+            client?: redis.RedisClient | ioredis.Redis;
             host?: string;
             port?: number;
             socket?: string;
