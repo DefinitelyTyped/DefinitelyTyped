a111a24a5f5d7be46b6e572eb474376f3d979cbf
Anton Spirin <rock.on@mail.ru>
2018-12-22 23:20:29 +0200

    added TypeSctipt Version: 2.8

--- types/react-frontload/index.d.ts
+++ types/react-frontload/index.d.ts
@@ -2,6 +2,7 @@
 // Project: https://github.com/davnicwil/react-frontload
 // Definitions by: Anton Spirin <https://github.com/rockon404>
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
+// TypeScript Version: 2.8
 
 import { ComponentClass, ComponentType } from 'react';
 
