d233ab4c06214652f7f919bd731a3bd3b53bdfa1
James Adarich <jamesrichford@googlemail.com>
2018-12-22 14:24:22 +0000

    add name

--- types/passport-http/index.d.ts
+++ types/passport-http/index.d.ts
@@ -3,6 +3,7 @@
 // Definitions by: Christophe Vidal <https://github.com/krizalys>
 //                 Tomek Łaziuk <https://github.com/tlaziuk>
 //                 Chris Barth <https://github.com/cjbarth>
+//                 James Adarich <https://github.com/jamesadarich>
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
 // TypeScript Version: 2.3
 
