6ea82ff17cac967602007d2a51cc14b7bea20911
Adam <admosity@users.noreply.github.com>
2018-12-23 01:43:27 -0500

    feat: export `Condition`
    Conditions can be used at the top level:
    
    ```typescript
    db.inventory.find( { $or: [ { quantity: { $lt: 20 } }, { price: 10 } ] } )
    ```
    
    Seems like the types in `@types/mongoose` marked the conditions as `any`, but exposing this `Condition` would provide for better typing in there as well.
    
    ```typescript
        findOne(conditions?: any,
          callback?: (err: any, res: T | null) => void): DocumentQuery<T | null, T> & QueryHelpers;
        findOne(conditions: any, projection: any,
          callback?: (err: any, res: T | null) => void): DocumentQuery<T | null, T> & QueryHelpers;
        findOne(conditions: any, projection: any, options: any,
          callback?: (err: any, res: T | null) => void): DocumentQuery<T | null, T> & QueryHelpers;
    ```

--- types/mongodb/index.d.ts
+++ types/mongodb/index.d.ts
@@ -999,7 +999,7 @@ export interface Collection<TSchema = Default> {
     watch(pipeline?: Object[], options?: ChangeStreamOptions & { startAtClusterTime?: Timestamp, session?: ClientSession }): ChangeStream;
 }
 
-type Condition<T, P extends keyof T> = {
+export type Condition<T, P extends keyof T> = {
     $eq?: T[P];
     $gt?: T[P];
     $gte?: T[P];
