e94a4f8eb50cfcde55dca229853a98efec526f4c
Agustín Carrasco <asermax@gmail.com>
2018-12-23 21:48:21 -0300

    [moment-timezone] `setDefault()` can be called without arguments
    Reference: https://momentjs.com/timezone/docs/#/using-timezones/default-timezone/

--- types/moment-timezone/index.d.ts
+++ types/moment-timezone/index.d.ts
@@ -1,6 +1,8 @@
 // Type definitions for moment-timezone.js 0.5
 // Project: http://momentjs.com/timezone/
-// Definitions by: Michel Salib <https://github.com/michelsalib>, Alan Brazil Lins <https://github.com/alanblins>
+// Definitions by: Michel Salib <https://github.com/michelsalib>
+//                 Alan Brazil Lins <https://github.com/alanblins>
+//                 Agustin Carrasco <https://github.com/asermax>
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
 
 import moment = require('moment');
@@ -53,7 +55,7 @@ declare module "moment" {
         names(): string[];
         guess(ignoreCache?: boolean): string;
 
-        setDefault(timezone: string): MomentTimezone;
+        setDefault(timezone?: string): MomentTimezone;
     }
 
     interface Moment {
