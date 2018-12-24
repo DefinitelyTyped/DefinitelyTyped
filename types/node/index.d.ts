e798583274f1e9d6a7fc7c4207822bf4cb7e464c
Andru Cherny <8457572+wirwolf@users.noreply.github.com>
2018-12-24 13:07:23 +0200

    Add props to ChildProcess
    Add  spawnargs spawnfile and connected to ChildProcess

--- types/node/index.d.ts
+++ types/node/index.d.ts
@@ -2732,6 +2732,9 @@ declare module "child_process" {
         stdout: stream.Readable;
         stderr: stream.Readable;
         stdio: [stream.Writable, stream.Readable, stream.Readable];
+        spawnargs: [string];
+        spawnfile: string;
+        connected: string;
         killed: boolean;
         pid: number;
         kill(signal?: string): void;
@@ -2742,7 +2745,7 @@ declare module "child_process" {
         disconnect(): void;
         unref(): void;
         ref(): void;
-
+        
         /**
          * events.EventEmitter
          * 1. close
