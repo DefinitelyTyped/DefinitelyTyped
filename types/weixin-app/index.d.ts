1daffc34495d0cb274236fb229fc0e76ba2c9c37
Jiayu Liu <etareduce@gmail.com>
2018-12-24 16:58:38 +0800

    fix doc

--- types/weixin-app/index.d.ts
+++ types/weixin-app/index.d.ts
@@ -3421,16 +3421,12 @@ declare namespace wx {
 			/** 向微信后台请求检查更新结果事件的回调函数 */
 			callback: (result: { hasUpdate: boolean }) => void
 		): void;
-		/** [UpdateManager.onUpdateFailed(function callback)](UpdateManager.onUpdateFailed.md)
-		 *
-		 * 监听小程序更新失败事件。小程序有新版本，客户端主动触发下载（无需开发者触发），下载失败（可能是网络原因等）后回调 */
+		/** 监听小程序更新失败事件。小程序有新版本，客户端主动触发下载（无需开发者触发），下载失败（可能是网络原因等）后回调 */
 		onUpdateFailed(
 			/** 小程序更新失败事件的回调函数 */
 			callback: (res: { errMsg: string }) => void
 		): void;
-		/** [UpdateManager.onUpdateReady(function callback)](UpdateManager.onUpdateReady.md)
-		 *
-		 * 监听小程序有版本更新事件。客户端主动触发下载（无需开发者触发），下载成功后回调 */
+		/** 监听小程序有版本更新事件。客户端主动触发下载（无需开发者触发），下载成功后回调 */
 		onUpdateReady(
 			/** 小程序有版本更新事件的回调函数 */
 			callback: () => void
