e4af16453d93b1247deed24b6d7ca648e81b9762
Martynas Kadiša <8273932+martynaskadisa@users.noreply.github.com>
2018-12-25 16:27:09 +0200

    downgrade ts version for webpack plugin

--- types/loadable__webpack-plugin/index.d.ts
+++ types/loadable__webpack-plugin/index.d.ts
@@ -2,7 +2,7 @@
 // Project: https://github.com/smooth-code/loadable-components
 // Definitions by: Martynas Kadiša <https://github.com/martynaskadisa>
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
-// TypeScript Version: 2.8
+// TypeScript Version: 2.3
 
 import { Plugin } from 'webpack';
 
