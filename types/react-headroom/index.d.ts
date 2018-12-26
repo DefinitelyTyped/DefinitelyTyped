e04bc450a2ff9cbd6e45330767a3ff26e464caa2
zerocho <zerohch0@gmail.com>
2018-12-26 18:03:40 +0900

    Fix: dt-headers

--- types/react-headroom/index.d.ts
+++ types/react-headroom/index.d.ts
@@ -1,7 +1,8 @@
 // Type definitions for react-headroom 2.2
 // Project: https://kyleamathews.github.io/react-headroom/
-// Definitions by: Zero Cho <[https://github.com/zerocho]>
+// Definitions by: Zero Cho <https://github.com/zerocho>
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
+// TypeScript Version: 2.8
 
 import { CSSProperties, ReactNode, Component } from 'react';
 
