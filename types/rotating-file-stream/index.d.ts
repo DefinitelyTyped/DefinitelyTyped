f84864122cf1bd37ceae4f18ad40ea68bed00066
Daniel Schopf <daniel.schopf@go-springtime.com>
2018-12-27 16:12:56 +0100

    add new package rotating-file-stream

---
+++ types/rotating-file-stream/index.d.ts
@@ -0,0 +1,32 @@
+// Type definitions for rotating-file-stream 1.3
+// Project: https://github.com/iccicci/rotating-file-stream#readme
+// Definitions by: Daniel Schopf <https://github.com/Danscho>
+// Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
+
+/// <reference types="node" />
+
+import { WriteStream } from "fs";
+
+export = rfs;
+declare function rfs(filename: string, options: rfs.RfsOptions): WriteStream
+
+declare namespace rfs {
+
+    export type CompressFn = (srcFileName: string, destinationFileName: string) => string
+
+    interface RfsOptions {
+        compress?: string | true | CompressFn,
+        highWaterMark?: number;
+        history?: string;
+        immutable?: boolean;
+        initialRotation?: boolean;
+        interval?: string;
+        maxFiles?: number;
+        maxSize?: string;
+        mode?: number;
+        path?: string;
+        rotate?: number;
+        rotationTime?: boolean;
+        size?: string
+    }
+}
---
+++ types/rotating-file-stream/rotating-file-stream-tests.ts
@@ -0,0 +1,23 @@
+import * as rfs from 'rotating-file-stream';
+
+const compress: rfs.CompressFn = (src: string, dest: string): string => {
+    return `cat ${src} | gzip -c9 > ${dest}`;
+}
+
+const accessLogStream = rfs('access.log', {
+    interval: '1d',
+    path: "path/to/",
+    compress,
+    highWaterMark: 1,
+    history: "myHistory",
+    immutable: false,
+    initialRotation: false,
+    maxFiles: 10,
+    maxSize: '1G',
+    mode: 1,
+    rotate: 10,
+    rotationTime: true,
+    size: '500M'
+});
+
+accessLogStream.close();
