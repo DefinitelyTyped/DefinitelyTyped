a16bd493c365b6631036ad9d021de2355e648aaa
Dimitri Benin <dimitri.benin@gmail.com>
2018-12-29 18:20:34 +0100

    [url-join] Move v0 types to sub-dir

--- types/url-join/v0/index.d.ts
+++ types/url-join/v0/index.d.ts
@@ -1,4 +1,4 @@
-// Type definitions for url-join v0.8.3
+// Type definitions for url-join 0.8
 // Project: https://github.com/jfromaniello/url-join
 // Definitions by: Rogier Schouten <https://github.com/rogierschouten>, Mike Deverell <https://github.com/devrelm>
 // Definitions: https://github.com/borisyankov/DefinitelyTyped
@@ -8,8 +8,6 @@
  * This works similar to path.join but you shouldn't use path.join for urls since it will work different depending of the operative systems but also doesn't work for some cases.
  */
 
-declare var urljoin: (...parts: string[]) => string;
+declare function urljoin(...parts: string[]): string;
 
-declare module "url-join" {
-    export = urljoin;
-}
+export = urljoin;
