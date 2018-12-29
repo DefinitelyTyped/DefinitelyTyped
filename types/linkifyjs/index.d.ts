3778d7dce48c38b5ff0317bb43e01aec11911e2f
Ovidiu Bute <ovidiu.bute@gmail.com>
2018-12-29 19:27:02 +0200

    Change minimum TypeScript version.

--- types/linkifyjs/index.d.ts
+++ types/linkifyjs/index.d.ts
@@ -3,7 +3,7 @@
 // Definitions by: Sean Zhu <https://github.com/szhu>
 //                 Ovidiu Bute <https://github.com/ovidiubute>
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
-// TypeScript Version: 2.3
+// TypeScript Version: 2.8
 
 export type PossiblyFuncOfHrefAndType<T> =
     | T
