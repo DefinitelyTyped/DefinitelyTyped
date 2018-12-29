852afe7ad698d74ef5b33c4502b1ce6e08521fac
James Lismore <jlismore@axio.com>
2018-12-28 23:35:10 -0500

    Add TS version

--- types/trie-prefix-tree/index.d.ts
+++ types/trie-prefix-tree/index.d.ts
@@ -2,6 +2,7 @@
 // Project: https://github.com/lyndseybrowning/trie-prefix#readme
 // Definitions by: James Lismore <https://github.com/jlismore>
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
+// TypeScript Version: 2.8
 
 export default function Trie(
     strings: string[]
