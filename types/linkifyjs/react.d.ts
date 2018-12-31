793383d2a415b91273a63aa9c4d94e45957c3659
Ovidiu Bute <ovidiu.bute@gmail.com>
2018-12-31 09:34:21 +0200

    Simplify export syntax.

--- types/linkifyjs/react.d.ts
+++ types/linkifyjs/react.d.ts
@@ -1,12 +1,10 @@
 import * as React from "react";
 import { LinkifyOptions } from "./index";
 
-interface LinkifyProps {
+export interface LinkifyProps {
     options?: LinkifyOptions;
 }
 
 declare class Linkify extends React.Component<LinkifyProps> {}
-
-export { LinkifyProps };
 
 export default Linkify;
