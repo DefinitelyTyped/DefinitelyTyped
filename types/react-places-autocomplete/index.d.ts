1ede73f418f0c90860b78784e919cb3429c3e03b
Aziz Khambati <aziz.h.khambati@gmail.com>
2018-12-31 21:26:25 +0530

    [react-places-autocomplete] Split v7 and v6 definitions. Improve v7 definitions. (#31698)
    * [react-places-autocomplete] Add definitions for children
    
    * Add Readonly
    
    * Revert to React.ReactNode
    
    * Fix link
    
    * Split v7 and v6 types

--- types/react-places-autocomplete/index.d.ts
+++ types/react-places-autocomplete/index.d.ts
@@ -1,8 +1,9 @@
 // Type definitions for react-places-autocomplete 7.2
-// Project: https://github.com/kenny-hibino/react-places-autocomplete/
+// Project: https://github.com/hibiken/react-places-autocomplete/
 // Definitions by: Guilherme Hübner <https://github.com/guilhermehubner>
 //                 Andrew Makarov <https://github.com/r3nya>
 //                 Nokky Goren <https://github.com/ApeNox>
+//                 Aziz Khambati <https://github.com/azizhk>
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
 // TypeScript Version: 2.8
 //
@@ -10,35 +11,26 @@
 
 import * as React from "react";
 
-export interface formattedSuggestionType {
-    mainText: string;
-    secundaryText: string;
+export type AutocompletePrediction = google.maps.places.AutocompletePrediction;
+
+export interface Suggestion {
+    id: string;
+    active: boolean;
+    index: number;
+    description: AutocompletePrediction["description"];
+    placeId: AutocompletePrediction["place_id"];
+    formattedSuggestion: {
+        mainText: AutocompletePrediction["structured_formatting"]["main_text"];
+        secondaryText: AutocompletePrediction["structured_formatting"]["secondary_text"];
+    };
+    matchedSubstrings: AutocompletePrediction["matched_substrings"];
+    terms: AutocompletePrediction["terms"];
+    types: AutocompletePrediction["types"];
 }
 
 export interface PropTypes {
-    inputProps?: {
-        type?: string;
-        name?: string;
-        placeholder?: string;
-        disabled?: boolean;
-    };
     onError?: (status: string, clearSuggestion: () => void) => void;
     onSelect?: (address: string, placeID: string) => void;
-    renderSuggestion?: (suggestion: string, formattedSuggestion: formattedSuggestionType) => React.ReactNode;
-    classNames?: {
-        root?: string;
-        input?: string;
-        autocompleteContainer?: string;
-        autocompleteItem?: string;
-        autocompleteItemActive?: string;
-    };
-    styles?: {
-        root?: React.CSSProperties;
-        input?: React.CSSProperties;
-        autocompleteContainer?: React.CSSProperties;
-        autocompleteItem?: React.CSSProperties;
-        autocompleteItemActive?: React.CSSProperties;
-    };
     searchOptions?: {
         bounds?: google.maps.LatLngBounds | google.maps.LatLngBoundsLiteral;
         componentRestrictions?: google.maps.GeocoderComponentRestrictions;
@@ -49,18 +41,44 @@ export interface PropTypes {
     };
     value?: string;
     onChange?: (value: string) => void;
-    onBlur?: (event: React.FocusEvent<HTMLInputElement>) => void;
 
     debounce?: number;
     highlightFirstSuggestion?: boolean;
-    renderFooter?: () => React.ReactNode;
-    shouldFetchSuggestions?: (value: string) => boolean;
+    shouldFetchSuggestions?: boolean;
+
+    children: (opts: Readonly<{
+        loading: boolean;
+        suggestions: ReadonlyArray<Suggestion>;
+        getInputProps: <InputProps extends {}>(options?: InputProps) => {
+            type: 'text';
+            autoComplete: 'off';
+            role: 'combobox';
+            'aria-autocomplete': 'list';
+            'aria-expanded': boolean;
+            'aria-activedescendant': string | null;
+            disabled: boolean;
+            onKeyDown: React.KeyboardEventHandler;
+            onBlur: () => void;
+            value: string | undefined;
+            onChange: (ev: { target: { value: string }}) => void;
+        } & InputProps;
+        getSuggestionItemProps: <SuggestionProps extends {}>(suggestion: Suggestion, options?: SuggestionProps) => {
+            key: number;
+            id: string | null;
+            role: 'option';
+            onMouseEnter: () => void;
+            onMouseLeave: () => void;
+            onMouseDown: React.MouseEventHandler;
+            onMouseUp: () => void;
+            onTouchStart: () => void;
+            onTouchEnd: () => void;
+            onClick: (event?: Event) => void;
+        } & SuggestionProps;
+    }>) => React.ReactNode;
 }
 
-export function geocodeByAddress(address: string, callback: (results: google.maps.GeocoderResult[], status: google.maps.GeocoderStatus) => void): void;
 export function geocodeByAddress(address: string): Promise<google.maps.GeocoderResult[]>;
 
-export function geocodeByPlaceId(placeId: string, callback: (results: google.maps.GeocoderResult[], status: google.maps.GeocoderStatus) => void): void;
 export function geocodeByPlaceId(placeId: string): Promise<google.maps.GeocoderResult[]>;
 
 export function getLatLng(results: google.maps.GeocoderResult): Promise<google.maps.LatLngLiteral>;
