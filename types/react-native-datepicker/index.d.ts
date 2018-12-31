f35e50837d12ad8035ba0886ee75fd1976deab2a
nossbigg <chenggibson@gmail.com>
2018-12-31 10:43:55 +0800

    Add test ID-related props to react-native-datepicker

--- types/react-native-datepicker/index.d.ts
+++ types/react-native-datepicker/index.d.ts
@@ -1,6 +1,7 @@
 // Type definitions for react-native-datepicker 1.6
 // Project: https://github.com/xgfe/react-native-datepicker
 // Definitions by: Jacob Baskin <https://github.com/jacobbaskin>
+//                 Cheng Gibson <https://github.com/nossbigg>
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
 // TypeScript Version: 2.8
 
@@ -37,6 +38,9 @@ export interface DatePickerProps {
     minuteInterval?: number;
     TouchableComponent?: React.Component;
     locale?: string;
+    testID?: string;
+    cancelBtnTestID?: string;
+    confirmBtnTestID?: string;
 }
 
 declare class DatePicker extends React.Component<DatePickerProps> {
