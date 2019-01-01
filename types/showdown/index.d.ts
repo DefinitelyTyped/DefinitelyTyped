36fa54b6d15414e50c362bee7e1c07dfa98f5253
Hamed Baatour <mouyahama@gmail.com>
2019-01-01 16:31:52 +0300

    types(showdown): fix header authors format

--- types/showdown/index.d.ts
+++ types/showdown/index.d.ts
@@ -1,6 +1,9 @@
-// Type definitions for Showdown 1.9.0
+// Type definitions for Showdown 1.9.
 // Project: https://github.com/showdownjs/showdown
-// Definitions by: Hamed Baatour<https://github.com/hamedbaatour>, cbowdon <https://github.com/cbowdon>, Pei-Tang Huang <https://github.com/tan9>, Ariel-Saldana <https://github.com/arielsaldana>
+// Definitions by: Hamed Baatour <https://github.com/hamedbaatour>,
+//                 cbowdon <https://github.com/cbowdon>,
+//                 Pei-Tang Huang <https://github.com/tan9>,
+//                 Ariel-Saldana <https://github.com/arielsaldana>
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
 
 export = Showdown;
