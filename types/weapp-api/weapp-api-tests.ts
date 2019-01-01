feac900bf5d10bfcbd916bacd84dab1b4e4fe13a
bocai <zhangpengbo2014@gmail.com>
2019-01-01 23:26:42 +0800

    增加UpdateManager、onLaunch测试

--- types/weapp-api/weapp-api-tests.ts
+++ types/weapp-api/weapp-api-tests.ts
@@ -347,6 +347,30 @@ wx.getSystemInfo({
   }
 })
 
+const updateManager = wx.getUpdateManager()
+
+// updateManager.onCheckForUpdate(function (res) {
+//     //请求完新版本信息的回调
+//     console.log(res.hasUpdate)
+// })
+
+updateManager.onUpdateReady(function () {
+    // wx.showModal({
+    //     title: '更新提示',
+    //     content: '新版本已经准备好，是否重启应用？',
+    //     success(res) {
+    //         if (res.confirm) {
+    //             // 新的版本已经下载好，调用 applyUpdate 应用新版本并重启
+    //             updateManager.applyUpdate()
+    //         }
+    //     }
+    // })
+})
+
+updateManager.onUpdateFailed(function () {
+    // 新版本下载失败
+})
+
 wx.onAccelerometerChange(function(res) {
   console.log(res.x)
   console.log(res.y)
@@ -672,4 +696,10 @@ wx.requestPayment({
    },
    'fail':function(res){
    }
+})
+
+App({
+    onLaunch(options){
+        console.log(options.query)
+    }
 })
