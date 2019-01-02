627f354d1d7dbf8fc615bace43ba24bb25747cb7
Ovidiu Bute <ovidiu.bute@gmail.com>
2019-01-02 11:39:46 +0200

    Remove unused imports.

--- types/linkifyjs/linkifyjs-tests.tsx
+++ types/linkifyjs/linkifyjs-tests.tsx
@@ -1,6 +1,4 @@
 import * as React from "react";
-import * as ReactDOM from "react-dom";
-import { LinkifyOptions } from "linkifyjs";
 import linkifyHtml from "linkifyjs/html";
 import Linkify from "linkifyjs/react";
 
