283edb4db7fa99ea33db2f7dc740865aa9dc9b25
Nathanael McDaniel <leemcd56@gmail.com>
2019-01-02 10:35:16 -0600

    Update index.d.ts
    Updated project URL with GitHub repo.

--- types/wallop/index.d.ts
+++ types/wallop/index.d.ts
@@ -1,5 +1,5 @@
 // Type definitions for Wallop 2.4
-// Project: http://pedroduarte.me/wallop
+// Project: https://github.com/peduarte/wallop
 // Definitions by: Nathanael McDaniel <https://github.com/leemcd56>
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
 // TypeScript Version: 2.3
