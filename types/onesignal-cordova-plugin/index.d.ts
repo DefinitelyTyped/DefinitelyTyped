8508ed3ddc883f4b3b39cf35bfac4e66bacb153e
Jonathan Price <notclive@gmail.com>
2019-01-02 14:43:59 +0000

    Remove patch version from header

--- types/onesignal-cordova-plugin/index.d.ts
+++ types/onesignal-cordova-plugin/index.d.ts
@@ -1,4 +1,4 @@
-// Type definitions for onesignal-cordova-plugin 2.2.1
+// Type definitions for onesignal-cordova-plugin 2.2
 // Project: https://github.com/onesignal/OneSignal-Cordova-SDK#readme
 // Definitions by: David Broder-Rodgers <https://github.com/broder>
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
