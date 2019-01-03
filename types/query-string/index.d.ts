9284ed563bcac4f67cf23b7cf0b73935f18408a5
Simon VDB <simonvdbroeck@gmail.com>
2019-01-03 16:35:10 +0100

    Add to contributor list

--- types/query-string/index.d.ts
+++ types/query-string/index.d.ts
@@ -1,10 +1,11 @@
-// Type definitions for query-string 6.1
+// Type definitions for query-string 6.2
 // Project: https://github.com/sindresorhus/query-string
 // Definitions by: Sam Verschueren <https://github.com/SamVerschueren>
 //                 Tanguy Krotoff <https://github.com/tkrotoff>
 //                 HuHuanming <https://github.com/huhuanming>
 //                 Madara Uchiha <https://github.com/MadaraUchiha>
 //                 Josh Holmer <https://github.com/shssoichiro>
+//                 Simon Van den Broeck <https://github.com/jarrku>
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
 // TypeScript Version: 2.2
 
