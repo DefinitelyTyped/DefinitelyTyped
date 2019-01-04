76de3577c706e4cda1a2280a767877ea4ca5803a
Dmitri Trofimov <dmitri@grabcad.com>
2019-01-04 16:40:46 +0200

    Added myself to the list of contributors

--- types/async/index.d.ts
+++ types/async/index.d.ts
@@ -1,6 +1,11 @@
 // Type definitions for Async 2.0.1
 // Project: https://github.com/caolan/async
-// Definitions by: Boris Yankov <https://github.com/borisyankov>, Arseniy Maximov <https://github.com/kern0>, Joe Herman <https://github.com/Penryn>, Angus Fenying <https://github.com/fenying>, Pascal Martin <https://github.com/pascalmartin>
+// Definitions by: Boris Yankov <https://github.com/borisyankov>
+//                 Arseniy Maximov <https://github.com/kern0>
+//                 Joe Herman <https://github.com/Penryn>
+//                 Angus Fenying <https://github.com/fenying>
+//                 Pascal Martin <https://github.com/pascalmartin>
+//                 Dmitri Trofimov <https://github.com/Dmitri1337>
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
 // TypeScript Version: 2.3
 
