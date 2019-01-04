8d21ac9216f47c1a4739b5deb4df266a98fa2160
davidgoli <davigoli@gmail.com>
2019-01-03 18:09:14 -0800

    add extra parse signature

--- types/url-parse/url-parse-tests.ts
+++ types/url-parse/url-parse-tests.ts
@@ -5,6 +5,10 @@ new URL('foo/bar', 'https://github.com/');
 new URL('foo/bar', 'https://github.com/', (query: string) => ({ query }));
 parse('foo/bar', 'https://github.com/');
 parse('foo/bar', 'https://github.com/', (query: string) => ({ query }));
+const result = parse('foo/bar?baz=quux', true);
+if (result.query.baz !== 'quux') {
+  throw new Error('bad query parsing');
+}
 
 const url1: URL = new URL('https://github.com/foo/bar?baz=true');
 url1.hash;
