f4417875ccd9964b8e64822b7dd8c582353178b8
paibamboo <gavinsitti@hotmail.com>
2019-01-04 13:26:08 +0700

    Fix build error patch version not allowed

--- types/react-modal/index.d.ts
+++ types/react-modal/index.d.ts
@@ -1,4 +1,4 @@
-// Type definitions for react-modal 3.8.1
+// Type definitions for react-modal 3.8
 // Project: https://github.com/reactjs/react-modal
 // Definitions by: Rajab Shakirov <https://github.com/radziksh>,
 //                 Drew Noakes <https://github.com/drewnoakes>,
