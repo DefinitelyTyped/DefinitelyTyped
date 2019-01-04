03a557cfb7702fbdfd025cd1ad663ba0c6464958
Dan <danpopa1998@gmail.com>
2019-01-05 01:55:01 +0200

    write react-kawaii definitions and tests

---
+++ types/react-kawaii/react-kawaii-tests.tsx
@@ -0,0 +1,22 @@
+import * as React from "react";
+import {
+    Backpack,
+    Browser,
+    Cat,
+    CreditCard,
+    Ghost,
+    IceCream,
+    Mug,
+    Planet,
+    SpeechBubble
+} from "react-kawaii";
+
+<Backpack size={320} mood="excited" color="#FFD882" />;
+<Browser size={200} mood="lovestruck" color="#61DDBC" />;
+<Cat size={320} mood="excited" color="#596881" />;
+<CreditCard size={200} mood="sad" color="#83D1FB" />;
+<Ghost size={240} mood="blissful" color="#E0E4E8" />;
+<IceCream size={300} mood="blissful" color="#FDA7DC" />;
+<Mug size={170} mood="happy" color="#A6E191" />;
+<Planet size={220} mood="happy" color="#FCCB7E" />;
+<SpeechBubble size={220} mood="happy" color="#83D1FB" />;
