0ccaf36b05c786d4f332658a9f471f0c5bd71acb
antoinebrault <antoinebrault@gmail.com>
2019-01-05 15:16:49 -0500

    add jest-when type inference

--- types/jest-when/index.d.ts
+++ types/jest-when/index.d.ts
@@ -6,13 +6,17 @@
 
 /// <reference types="jest" />
 
-export type PartialMockInstance<T, Y extends any[]> = Pick<jest.MockInstance<T, Y>, 'mockReturnValue' | 'mockReturnValueOnce' | 'mockResolvedValue'
-  | 'mockResolvedValueOnce' | 'mockRejectedValue' | 'mockRejectedValueOnce'>;
-
-export interface When<T = {}, Y extends any[] = any> {
-  (fn: jest.Mock<T, Y>): When<T, Y>;
-  calledWith(...matchers: Y): PartialMockInstance<T, Y>;
-  expectCalledWith(...matchers: Y): PartialMockInstance<T, Y>;
+export interface WhenMock<T = any, Y extends any[] = any> extends jest.Mock<T, Y> {
+  calledWith(...matchers: Y): WhenMock<T, Y>;
+  expectCalledWith(...matchers: Y): WhenMock<T, Y>;
+  mockReturnValue(value: T): WhenMock<T, Y>;
+  mockReturnValueOnce(value: T): WhenMock<T, Y>;
+  mockResolvedValue(value: T | PromiseLike<T>): WhenMock<Promise<T>, Y>;
+  mockResolvedValueOnce(value: T | PromiseLike<T>): WhenMock<Promise<T>, Y>;
+  mockRejectedValue(value: T | PromiseLike<T>): WhenMock<Promise<T>, Y>;
+  mockRejectedValueOnce(value: T | PromiseLike<T>): WhenMock<Promise<T>, Y>;
 }
+
+export type When = <T, Y extends any[]>(fn: jest.Mock<T, Y>) => WhenMock<T, Y>;
 
 export const when: When;
