7af0053a0bc5cd73974dc2c2f7b1495f87b6b728
kimcoder-bm <kimcoder@woowahan.com>
2019-01-05 16:27:38 +0900

    add @types/body-scroll-lock

---
+++ types/body-scroll-lock/index.d.ts
@@ -0,0 +1,13 @@
+// Type definitions for body-scroll-lock 2.6
+// Project: https://github.com/willmcpo/body-scroll-lock (Does not have to be to GitHub, but prefer linking to a source code repository rather than to a project website.)
+// Definitions by: kimcoder <https://github.com/kimcoder>
+// Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
+
+export interface BodyScrollOptions {
+    reserveScrollBarGap?: boolean;
+    allowTouchMove?: () => void;
+}
+
+export function disableBodyScroll(targetElement: HTMLElement, options?: BodyScrollOptions): void;
+export function enableBodyScroll(targetElement: HTMLElement): void;
+export function clearAllBodyScrollLocks(): void;
