1dc5761b46c1c7b2f6ebf319815936ef8d17524d
Matías Olivera <matiasolivera@outlook.com>
2019-01-06 15:59:11 -0300

    Update version to 3.15.0

--- types/validatorjs/index.d.ts
+++ types/validatorjs/index.d.ts
@@ -1,7 +1,8 @@
-// Type definitions for validatorjs v3.7.0
+// Type definitions for validatorjs v3.15.0
 // Project: https://github.com/skaterdav85/validatorjs
 // Definitions by: Karol Janyst <https://github.com/LKay>
 //                 Dan Manastireanu <https://github.com/danmana>
+//                 Matías Olivera <https://github.com/MatiasOlivera>
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
 
 declare namespace Validator {
