bfc5ff013be07248bd779ad4b04542a965e1b0d9
chenc <double_cl@163.com>
2019-01-08 01:59:30 +0800

    fix lint error

--- types/node-xlsx/index.d.ts
+++ types/node-xlsx/index.d.ts
@@ -2,19 +2,20 @@
 // Project: https://github.com/mgcrea/node-xlsx#readme
 // Definitions by: chenc <https://github.com/cWatermelon>
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
+// TypeScript Version: 3.0
 
 /**
  * parsing a xlsx from file/buffer, outputs an object of worksheets
  * @param mixed file or buffer
  * @param options options is for xlsx
- * @returns worksheets data, like: [{ name, data: [] }]
+ * @returns worksheets data, like: [{ name, data: [[]] }]
  */
 export declare function parse(
     mixed: any,
     options?: {}
   ): Array<{
     name: string;
-    data: {}[];
+    data: Array<[]>;
   }>;
 
   /**
