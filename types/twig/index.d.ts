37573444ef76fa53c88ebaab4a94c722d821fe71
MaikTizziani <mtizziani@gmail.com>
2019-01-07 12:23:54 +0100

    added contributor name

--- types/twig/index.d.ts
+++ types/twig/index.d.ts
@@ -2,6 +2,7 @@
 // Project: https://github.com/twigjs/twig.js
 // Definitions by: Carlos Ballesteros Velasco <https://github.com/soywiz>
 //                 Tim Schumacher <https://github.com/enko>
+//                 Maik Tizziani <https://github.com/mtizziani>
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
 
 // Imported from: https://github.com/soywiz/typescript-node-definitions/twig.d.ts
