f6d46c5dac36366f90e3ca7e199f0bd2214070ad
chenc <double_cl@163.com>
2019-01-08 02:23:24 +0800

    Add comments and improve return values

--- types/node-xlsx/index.d.ts
+++ types/node-xlsx/index.d.ts
@@ -6,27 +6,30 @@
 
 /**
  * parsing a xlsx from file/buffer, outputs an object of worksheets
- * @param mixed file or buffer
+ * @param mixed file path or buffer
  * @param options options is for xlsx
- * @returns worksheets data, like: [{ name, data: [[]] }]
+ * @returns worksheets data, like: { name: 'worksheets', data: [[1,2,3],['1', '2','word']] }
  */
 export declare function parse(
-    mixed: any,
+    mixed: string | ArrayBuffer,
     options?: {}
-  ): Array<{
+): Array<{
     name: string;
     data: Array<[]>;
-  }>;
+}>;
 
-  /**
-   * building a xlsx
-   * @param worksheets worksheets data, like:[[1,2,3],['1', '2','word']]
-   * @param options spannig multiple rows A1:A4
-   * @returns returns a buffer of worksheets
-   */
-  export declare function build(worksheets: any, options?: {}): any;
-  declare const _default: {
+/**
+ * building a xlsx
+ * @param worksheets worksheets data, like:{ name: 'worksheets', data: [[1,2,3],['1', '2','word']] }
+ * @param options spannig multiple rows A1:A4
+ * @returns returns a buffer of worksheets
+ */
+export declare function build(
+    worksheets: Array<{ name: string; data: Array<any[]> }>,
+    options?: {}
+): ArrayBuffer;
+declare const _default: {
     parse: typeof parse;
     build: typeof build;
-  };
-  export default _default;
+};
+export default _default;
