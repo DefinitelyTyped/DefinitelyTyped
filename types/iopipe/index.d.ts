39aec29f6a2da81b1a8483dfadb1ccb78690bdf7
Javon Harper <javon.d.harper@gmail.com>
2019-01-08 13:09:49 -0500

    Use 4 spaces

--- types/iopipe/index.d.ts
+++ types/iopipe/index.d.ts
@@ -2,21 +2,21 @@
 // Project: https://github.com/iopipe/iopipe-js
 
 interface IOPipeConfig {
-  debug?: boolean;
-  token?: string;
-  networkTimeout?: number;
-  timeoutWindow?: number;
+    debug?: boolean;
+    token?: string;
+    networkTimeout?: number;
+    timeoutWindow?: number;
 }
   
 interface Mark {
-  start(label: string): void;
-  end(label: string): void;
+    start(label: string): void;
+    end(label: string): void;
 }
   
 declare module "@iopipe/iopipe" {
-  export function label(label: string): void;
-  export function metric(label: string, number: number): void;
-  export let mark: Mark
+    export function label(label: string): void;
+    export function metric(label: string, number: number): void;
+    export let mark: Mark
   
-  export default function iopipe(config?: IOPipeConfig): Function;
+    export default function iopipe(config?: IOPipeConfig): Function;
 }
