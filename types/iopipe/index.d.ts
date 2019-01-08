a1653d1bea63d267613fd8a1191e1c11b893e1b2
Javon Harper <javon.d.harper@gmail.com>
2019-01-08 12:54:39 -0500

    Add iopipe-js type declarations

---
+++ types/iopipe/index.d.ts
@@ -0,0 +1,22 @@
+// Type definitions for iopipe
+// Project: https://github.com/iopipe/iopipe-js
+
+interface IOPipeConfig {
+  debug?: boolean;
+  token?: string;
+  networkTimeout?: number;
+  timeoutWindow?: number;
+}
+  
+interface Mark {
+  start(label: string): void;
+  end(label: string): void;
+}
+  
+declare module "@iopipe/iopipe" {
+  export function label(label: string): void;
+  export function metric(label: string, number: number): void;
+  export let mark: Mark
+  
+  export default function iopipe(config?: IOPipeConfig): Function;
+}
---
+++ types/iopipe/iopipe-tests.ts
@@ -0,0 +1,17 @@
+import iopipe, { label, mark, metric } from '@iopipe/iopipe'
+
+const configuredLib = iopipe({
+  token: 'ABCDEFG' 
+});
+
+const run = () => {}
+
+configuredLib(run)
+
+label('a label');
+
+mark.start('trace');
+mark.end('trace');
+
+metric('metric-label', 42);
+
