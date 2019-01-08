7c8b923faac7b2df41ef407988d487a302849fcf
Benjamin Makus <benneq@users.noreply.github.com>
2019-01-08 03:09:00 +0100

    add typings for spected

--- types/spected/index.d.ts
+++ types/spected/index.d.ts
@@ -1,26 +1,30 @@
-// Type definitions for yup 0.26
+// Type definitions for spected 0.26
 // Project: https://github.com/25th-floor/spected
-// Definitions by: Benjamin Makus <https://github.com/benneq>,
+// Definitions by: Benjamin Makus <https://github.com/benneq>
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
-// TypeScript Version: 3.2
+// TypeScript Version: 2.1
 
 declare var spected: Spected;
 
-export type Spected = (spec: Spec, input: any) => Result
+export type Spected = (spec: Spec, input: any) => Result;
 
-export type Predicate = (value: any, inputs: any) => boolean
+export type Predicate = (value: any, inputs: any) => boolean;
 
 export type ErrorMsg =
     | string
-    | ((value: any, field: string) => string)
+    | ((value: any, field: string) => string);
 
-export type Spec = {[key: string]: SpecValue }
+export interface Spec {
+    [key: string]: SpecValue;
+}
 
-export type SpecValue = 
+export type SpecValue =
     | ReadonlyArray<[Predicate, ErrorMsg]>
     | ((value: any) => any)
-    | Spec
+    | Spec;
 
-export type Result = {[key: string]: true | Array<string> | Result}
+export interface Result {
+    [key: string]: true | string[] | Result;
+}
 
 export default spected;
