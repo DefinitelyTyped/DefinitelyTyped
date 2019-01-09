b2bdcdfef4beca0f141dff610aa8a28bc66fcbdc
Dimitri DO BAIRRO <dimitri.dobairro@dimsolution.com>
2019-01-09 10:19:14 +0100

    fix(types/rimiti__stimmy/index.d.ts) definition improved

--- types/rimiti__stimmy/index.d.ts
+++ types/rimiti__stimmy/index.d.ts
@@ -1,8 +1,8 @@
-// Type definitions for @rimiti/stimmy 1.8.0
+// Type definitions for @rimiti/stimmy 1.8
 // Project: https://github.com/rimiti/stimmy
 // Definitions by: Dimitri DO BAIRRO <https://github.com/rimiti>
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
 
 export = replacer;
 
-declare function replacer(start?: string, end?: string): (val: string, replace: {} | []) => string;
+declare function replacer(start?: string, end?: string): (val: string, replace: {} | string[] | number[]) => string;
