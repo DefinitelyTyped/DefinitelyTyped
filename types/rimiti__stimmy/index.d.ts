5916e933ec7bf77ae869dbe5abcb903ec9052c81
Dimitri DO BAIRRO <dimitri.dobairro@dimsolution.com>
2019-01-09 09:54:28 +0100

    feat(types/rimiti__stimmy/index.d.ts) definition file created

---
+++ types/rimiti__stimmy/index.d.ts
@@ -0,0 +1,8 @@
+// Type definitions for @rimiti/stimmy 1.8.0
+// Project: https://github.com/rimiti/stimmy
+// Definitions by: Dimitri DO BAIRRO <https://github.com/rimiti>
+// Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
+
+export = replacer;
+
+declare function replacer(start?: string, end?: string): (val: string, replace: {} | []) => string;
