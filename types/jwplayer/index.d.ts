5837145fd6cf1849fb6e1b91d50379cd6ae8136b
Daniel Cassidy <mail@danielcassidy.me.uk>
2019-01-11 09:45:44 +0000

    Fix violations of 'dt-header' lint rule.

--- types/jwplayer/index.d.ts
+++ types/jwplayer/index.d.ts
@@ -1,11 +1,11 @@
-// Type definitions for JW Player V8.2
-// Project: https://github.com/jwplayer/jwplayer/
+// Type definitions for JW Player 8.2
+// Project: https://github.com/jwplayer/jwplayer
 // Definitions by: Martin Duparc <https://github.com/martinduparc>
 //                 Tomer Kruvi <https://github.com/kutomer>
 //                 Philipp Gürtler <https://github.com/philippguertler>
 //                 Daniel McGraw <https://github.com/danielmcgraw>
 //                 Benjamin Dobson <https://github.com/bpdsw>
-//                 Be Birchall <https://gitnub.com/bebebebebe>
+//                 Be Birchall <https://github.com/bebebebebe>
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
 // TypeScript Version: 2.2
 
