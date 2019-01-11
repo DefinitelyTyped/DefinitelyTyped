0d53a4bb7e01195c63d4acd052dfc27419e4cf45
Kallu609 <khirvola@gmail.com>
2019-01-11 04:24:10 +0200

    added types for js-levenshtein

---
+++ types/js-levenshtein/js-levenshtein-tests.ts
@@ -0,0 +1,9 @@
+// Type definitions for js-levenshtein 1.1.6
+// Project: https://github.com/gustf/js-levenshtein
+// Definitions by: Kallu609 <https://github.com/Kallu609>
+//                 Pyry Rouvila <https://github.com/naftis>
+// Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
+
+import levenshtein from 'js-levenshtein';
+
+levenshtein('kittens', 'mittens'); // $ExpectType number
