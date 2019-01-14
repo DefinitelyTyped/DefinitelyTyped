843947a4dc0d3bcdb12fe01039e3586b20c4778a
bySabi Files <bysabi.files@gmail.com>
2019-01-14 16:24:08 +0100

    [react-lifecycles-compat] add typedef

---
+++ types/react-lifecycles-compat/index.d.ts
@@ -0,0 +1,9 @@
+// Type definitions for react-lifecycles-compat 3.0.4
+// Project: https://github.com/reactjs/react-lifecycles-compat#readme
+// Definitions by: bySabi Files <https://github.com/bySabi>
+// Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
+// TypeScript Version: 2.8
+
+import * as React from 'react';
+
+export function polyfill<P>(Comp: React.ComponentType<P>): React.ComponentType<P>;
