d77452c5562010f20f0122e73d6ca35bcaac1bc7
SnakeDrak <SnakeDrak@users.noreply.github.com>
2019-01-14 16:11:14 +0100

    Added header to notify

--- types/eventsource/index.d.ts
+++ types/eventsource/index.d.ts
@@ -2,6 +2,7 @@
 // Project: http://github.com/EventSource/eventsource
 // Definitions by: Scott Lee Davis <https://github.com/scottleedavis>
 //                 Ali Afroozeh <https://github.com/afroozeh>
+//                 Pedro Gámez <https://github.com/snakedrak>
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
 // TypeScript Version: 2.8
 
