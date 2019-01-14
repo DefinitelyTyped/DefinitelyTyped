6d5059591db1c2115b9f797b668cca6bc4c4bbe8
SnakeDrak <SnakeDrak@users.noreply.github.com>
2019-01-14 16:55:27 +0100

    Update version number

--- types/eventsource/index.d.ts
+++ types/eventsource/index.d.ts
@@ -1,4 +1,4 @@
-// Type definitions for eventsource 1.0
+// Type definitions for eventsource 1.1
 // Project: http://github.com/EventSource/eventsource
 // Definitions by: Scott Lee Davis <https://github.com/scottleedavis>
 //                 Ali Afroozeh <https://github.com/afroozeh>
