c736f43326c7dfb9b6c5bc228a1ffb39db7e1104
Bryan Huang <dukedhx@hotmail.com>
2019-01-15 15:49:50 +0800

    moved module definitions to index.d.ts

--- types/forge-apis/index.d.ts
+++ types/forge-apis/index.d.ts
@@ -1,4 +1,4 @@
-// Type definitions for Forge Node.js SDK
+// Type definitions for Forge-apis
 // Project: https://github.com/Autodesk-Forge/forge-api-nodejs-client
 // Definitions by: Autodesk Forge Partner Development <forge.help@autodesk.com>
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
@@ -21,4 +21,95 @@
 //
 /// <reference path='./src/viewer.d.ts' />
 /// <reference path='./src/forge.d.ts' />
-/// <reference path='./src/forge-apis.d.ts' />
+
+export class AuthClientTwoLegged {
+    constructor(clientId: string, clientSecret: string, scope: string[]);
+
+    authenticate(): Promise<Autodesk.Forge.AuthToken>;
+    getCredentials(): Autodesk.Forge.AuthToken;
+    setCredentials(credentials: Autodesk.Forge.AuthToken): void;
+    isAuthorized(): boolean;
+}
+
+export class AuthClientThreeLegged {
+    constructor(clientId: string, clientSecret: string, redirectUri: string, scope: string[]);
+
+    generateAuthUrl(): string;
+    getToken(code: string): Promise<Autodesk.Forge.AuthToken>;
+    refreshToken(credentials: Autodesk.Forge.AuthToken): Promise<Autodesk.Forge.AuthToken>;
+}
+
+export interface ApiResponse {
+    body: any;
+    headers: any;
+    statusCode: number;
+}
+
+export interface ApiError {
+    statusCode: number;
+    statusMessage: string;
+}
+
+export class BucketsApi {
+    constructor();
+
+    createBucket(postBuckets: object, opts: object, credentials: Autodesk.Forge.AuthToken): Promise<ApiResponse>;
+    deleteBucket(bucketKey: string, credentials: Autodesk.Forge.AuthToken): Promise<ApiResponse>;
+    getBucketDetails(bucketKey: string, credentials: Autodesk.Forge.AuthToken): Promise<ApiResponse>;
+    getBuckets(options: object, credentials: Autodesk.Forge.AuthToken): Promise<ApiResponse>;
+}
+
+export class HubsApi {
+    constructor();
+
+    getHub(hubId: string, opts: object, credentials: Autodesk.Forge.AuthToken): Promise<ApiResponse>;
+    getHubs(opts: object, credentials: Autodesk.Forge.AuthToken): Promise<ApiResponse>;
+    getHubProjects(hubId: string, opts: object, credentials: Autodesk.Forge.AuthToken): Promise<ApiResponse>;
+}
+
+export class FoldersApi {
+    constructor();
+
+    getFolderContents(projectId: string, folderId: string, opts: object, credentials: Autodesk.Forge.AuthToken): Promise<ApiResponse>;
+}
+
+export interface JobPayload {
+    input: JobPayloadInput;
+    output: JobPayloadOutput;
+}
+
+export interface JobPayloadInput {
+    urn: string;
+    compressedUrn?: boolean;
+    rootFilename?: string;
+}
+
+export interface JobPayloadOutput {
+    formats: JobPayloadItem[];
+}
+
+export interface JobPayloadItem {
+    type: string;
+    views?: string[];
+    advanced?: {
+        applicationProtocol: string;
+        tolerance: number;
+    };
+    format?: string;
+    exportColor?: boolean;
+    exportFileStructure?: string;
+}
+
+export class DerivativesApi {
+    constructor();
+
+    deleteManifest(urn: string, credentials: Autodesk.Forge.AuthToken): Promise<ApiResponse>;
+    getDerivativeManifest(urn: string, derivativeUrn: string, opts: object, credentials: Autodesk.Forge.AuthToken): Promise<ApiResponse>;
+    getFormats(opts: object, credentials: Autodesk.Forge.AuthToken): Promise<ApiResponse>;
+    getManifest(urn: string, opts: object, credentials: Autodesk.Forge.AuthToken): Promise<ApiResponse>;
+    getMetadata(urn: string, opts: object, credentials: Autodesk.Forge.AuthToken): Promise<ApiResponse>;
+    getModelviewMetadata(urn: string, guid: string, opts: object, credentials: Autodesk.Forge.AuthToken): Promise<ApiResponse>;
+    getModelviewProperties(urn: string, guid: string, opts: object, credentials: Autodesk.Forge.AuthToken): Promise<ApiResponse>;
+    getThumbnail(urn: string, opts: object, credentials: Autodesk.Forge.AuthToken): Promise<ApiResponse>;
+    translate(job: JobPayload, opts: { xAdsForce?: boolean }, credentials: Autodesk.Forge.AuthToken): Promise<ApiResponse>;
+}
