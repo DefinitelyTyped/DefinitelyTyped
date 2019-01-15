6fd6cdeb9a33e5c5388a22e3f050021c2a34bb39
StefanWerW <scaw.dev@gmail.com>
2019-01-15 15:24:27 +0100

    Fix MAJOR.MINOR version error

--- types/rheostat/index.d.ts
+++ types/rheostat/index.d.ts
@@ -1,4 +1,4 @@
-// Type definitions for rheostat 3.0.2
+// Type definitions for rheostat 3.0
 // Project: https://github.com/airbnb/rheostat
 // Definitions by: Sasha Bayan <https://github.com/SashaBayan>, Wil Lee <https://github.com/kourge>, Stefan Wer <https://github.com/StefanWerW>
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
