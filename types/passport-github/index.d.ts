9e4bef07c73d5e2f690f3bcc0167987eda74921e
Manuel Ruck <github@manuelruck.de>
2019-01-16 21:48:32 +0100

    increase version

--- types/passport-github/index.d.ts
+++ types/passport-github/index.d.ts
@@ -1,4 +1,4 @@
-// Type definitions for passport-github 1.1
+// Type definitions for passport-github 1.2
 // Project: https://github.com/jaredhanson/passport-github
 // Definitions by:  Yasunori Ohoka <https://github.com/yasupeke>
 //                  Manuel Ruck <https://github.com/reggaepanda>
