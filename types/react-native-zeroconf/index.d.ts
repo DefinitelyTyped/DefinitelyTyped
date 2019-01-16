0f4eafc7e9119bc4fdc72131ff1ceb511871223a
Peter Matta <pet3r.matta@gmail.com>
2019-01-15 21:59:54 -0600

    Regenerated files with `dts-gen`

--- types/react-native-zeroconf/index.d.ts
+++ types/react-native-zeroconf/index.d.ts
@@ -1,5 +1,5 @@
-// Type definitions for react-native-zeroconf 0.9.0
-// Project: https://github.com/balthazar/react-native-zeroconf
+// Type definitions for react-native-zeroconf 0.9
+// Project: https://github.com/Apercu/react-native-zeroconf#readme
 // Definitions by: Peter Matta <https://github.com/mattapet>
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
 
