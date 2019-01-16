1bbc85aaf175706e6b02ab4e6a323dd08d720eb1
Bryan Huang <dukedhx@hotmail.com>
2019-01-16 10:44:43 +0800

    moving globals from forge.d.ts to top level

--- types/forge-apis/index.d.ts
+++ types/forge-apis/index.d.ts
@@ -20,14 +20,13 @@
 // UNINTERRUPTED OR ERROR FREE.
 //
 /// <reference path='./src/viewer.d.ts' />
-/// <reference path='./src/forge.d.ts' />
 
 export class AuthClientTwoLegged {
     constructor(clientId: string, clientSecret: string, scope: string[]);
 
-    authenticate(): Promise<Autodesk.Forge.AuthToken>;
-    getCredentials(): Autodesk.Forge.AuthToken;
-    setCredentials(credentials: Autodesk.Forge.AuthToken): void;
+    authenticate(): Promise<AuthToken>;
+    getCredentials(): AuthToken;
+    setCredentials(credentials: AuthToken): void;
     isAuthorized(): boolean;
 }
 
@@ -35,8 +34,8 @@ export class AuthClientThreeLegged {
     constructor(clientId: string, clientSecret: string, redirectUri: string, scope: string[]);
 
     generateAuthUrl(): string;
-    getToken(code: string): Promise<Autodesk.Forge.AuthToken>;
-    refreshToken(credentials: Autodesk.Forge.AuthToken): Promise<Autodesk.Forge.AuthToken>;
+    getToken(code: string): Promise<AuthToken>;
+    refreshToken(credentials: AuthToken): Promise<AuthToken>;
 }
 
 export interface ApiResponse {
@@ -53,24 +52,24 @@ export interface ApiError {
 export class BucketsApi {
     constructor();
 
-    createBucket(postBuckets: object, opts: object, credentials: Autodesk.Forge.AuthToken): Promise<ApiResponse>;
-    deleteBucket(bucketKey: string, credentials: Autodesk.Forge.AuthToken): Promise<ApiResponse>;
-    getBucketDetails(bucketKey: string, credentials: Autodesk.Forge.AuthToken): Promise<ApiResponse>;
-    getBuckets(options: object, credentials: Autodesk.Forge.AuthToken): Promise<ApiResponse>;
+    createBucket(postBuckets: object, opts: object, credentials: AuthToken): Promise<ApiResponse>;
+    deleteBucket(bucketKey: string, credentials: AuthToken): Promise<ApiResponse>;
+    getBucketDetails(bucketKey: string, credentials: AuthToken): Promise<ApiResponse>;
+    getBuckets(options: object, credentials: AuthToken): Promise<ApiResponse>;
 }
 
 export class HubsApi {
     constructor();
 
-    getHub(hubId: string, opts: object, credentials: Autodesk.Forge.AuthToken): Promise<ApiResponse>;
-    getHubs(opts: object, credentials: Autodesk.Forge.AuthToken): Promise<ApiResponse>;
-    getHubProjects(hubId: string, opts: object, credentials: Autodesk.Forge.AuthToken): Promise<ApiResponse>;
+    getHub(hubId: string, opts: object, credentials: AuthToken): Promise<ApiResponse>;
+    getHubs(opts: object, credentials: AuthToken): Promise<ApiResponse>;
+    getHubProjects(hubId: string, opts: object, credentials: AuthToken): Promise<ApiResponse>;
 }
 
 export class FoldersApi {
     constructor();
 
-    getFolderContents(projectId: string, folderId: string, opts: object, credentials: Autodesk.Forge.AuthToken): Promise<ApiResponse>;
+    getFolderContents(projectId: string, folderId: string, opts: object, credentials: AuthToken): Promise<ApiResponse>;
 }
 
 export interface JobPayload {
@@ -103,13 +102,93 @@ export interface JobPayloadItem {
 export class DerivativesApi {
     constructor();
 
-    deleteManifest(urn: string, credentials: Autodesk.Forge.AuthToken): Promise<ApiResponse>;
-    getDerivativeManifest(urn: string, derivativeUrn: string, opts: object, credentials: Autodesk.Forge.AuthToken): Promise<ApiResponse>;
-    getFormats(opts: object, credentials: Autodesk.Forge.AuthToken): Promise<ApiResponse>;
-    getManifest(urn: string, opts: object, credentials: Autodesk.Forge.AuthToken): Promise<ApiResponse>;
-    getMetadata(urn: string, opts: object, credentials: Autodesk.Forge.AuthToken): Promise<ApiResponse>;
-    getModelviewMetadata(urn: string, guid: string, opts: object, credentials: Autodesk.Forge.AuthToken): Promise<ApiResponse>;
-    getModelviewProperties(urn: string, guid: string, opts: object, credentials: Autodesk.Forge.AuthToken): Promise<ApiResponse>;
-    getThumbnail(urn: string, opts: object, credentials: Autodesk.Forge.AuthToken): Promise<ApiResponse>;
-    translate(job: JobPayload, opts: { xAdsForce?: boolean }, credentials: Autodesk.Forge.AuthToken): Promise<ApiResponse>;
+    deleteManifest(urn: string, credentials: AuthToken): Promise<ApiResponse>;
+    getDerivativeManifest(urn: string, derivativeUrn: string, opts: object, credentials: AuthToken): Promise<ApiResponse>;
+    getFormats(opts: object, credentials: AuthToken): Promise<ApiResponse>;
+    getManifest(urn: string, opts: object, credentials: AuthToken): Promise<ApiResponse>;
+    getMetadata(urn: string, opts: object, credentials: AuthToken): Promise<ApiResponse>;
+    getModelviewMetadata(urn: string, guid: string, opts: object, credentials: AuthToken): Promise<ApiResponse>;
+    getModelviewProperties(urn: string, guid: string, opts: object, credentials: AuthToken): Promise<ApiResponse>;
+    getThumbnail(urn: string, opts: object, credentials: AuthToken): Promise<ApiResponse>;
+    translate(job: JobPayload, opts: { xAdsForce?: boolean }, credentials: AuthToken): Promise<ApiResponse>;
+}
+
+export interface Credentials {
+    client_id: string;
+    client_secret: string;
+    grant_type: string;
+    scope?: string;
+}
+
+export interface AuthToken {
+    access_token: string;
+    expires_in: number;
+    token_type: string;
+    refresh_token?: string;
+}
+
+export namespace Dm {
+    interface BucketResponse {
+        bucketKey: string;
+        bucketOwner: string;
+        createdDate: number;
+        permissions: Array<{
+            access: string;
+            authId: string;
+        }>;
+        policyKey: string;
+    }
+
+    interface ItemResponse {
+        data: any[];
+        included: Item[];
+        jsonapi: {
+            version: string;
+        };
+        links: any;
+    }
+
+    interface Item {
+        attributes: {
+            createTime: string;
+            createUserId: string;
+            displayName: string;
+            extension: object;
+            fileType: string;
+            lastModifiedTime: string;
+            lastModifiedUserId: string;
+            mimeType: string;
+            name: string;
+            storageSize: number;
+            versionNumber: number;
+        };
+        id: string;
+        links: {
+            self: {
+                href: string;
+            };
+        };
+        relationships: {
+            derivatives: {
+                data: {
+                    id: string;
+                },
+                meta: {
+                    link: {
+                        href: string;
+                    }
+                }
+            },
+            item: any;
+            refs: any;
+            storage: {
+                data: {
+                    id: string;
+                    type: string;
+                }
+            };
+            thumbnail: any;
+        };
+        type: string;
+    }
 }
