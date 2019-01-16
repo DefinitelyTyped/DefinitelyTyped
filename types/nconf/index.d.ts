d376dc2e498ba1c965a6d3d6cc13c414f4b130e2
Dave Houston <dhouston@cpan.org>
2019-01-15 21:03:44 -0800

    - Update maintainer list

--- types/nconf/index.d.ts
+++ types/nconf/index.d.ts
@@ -1,6 +1,8 @@
 // Type definitions for nconf
 // Project: https://github.com/flatiron/nconf
-// Definitions by: Jeff Goddard <https://github.com/jedigo>, Jean-Martin Thibault <https://github.com/jmthibault>
+// Definitions by: Jeff Goddard <https://github.com/jedigo>, 
+//                 Jean-Martin Thibault <https://github.com/jmthibault>
+//                 Dave Houston <https://github.com/ew73>
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
 
 // Imported from: https://github.com/soywiz/typescript-node-definitions/nconf.d.ts
