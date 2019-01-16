3eb9905ed7aa7ffb722981a95d340906db69e01c
Cameron Martin <cameron_martin@shelter.org.uk>
2019-01-16 11:41:14 +0000

    [webpack]: Added types for SingleEntryPlugin.
    See https://github.com/webpack/webpack/blob/6f722436697ffa946d4c5c69d25e6cbba4aa0796/lib/SingleEntryPlugin.js

--- types/webpack/webpack-tests.ts
+++ types/webpack/webpack-tests.ts
@@ -446,6 +446,11 @@ plugin = new webpack.HashedModuleIdsPlugin({
     hashDigest: 'hex',
     hashDigestLength: 20
 });
+plugin = new webpack.SingleEntryPlugin(
+    '/home',
+    './main.js',
+    'main'
+);
 
 //
 // http://webpack.github.io/docs/node.js-api.html
