702679027bea3b6d7a4c92bef621ae00b955c0ca
ManAnRuck <gitHub@manuelruck.de>
2019-01-17 12:09:05 +0100

    revert spaces change in other types lib

--- types/passport-github2/passport-github2-tests.ts
+++ types/passport-github2/passport-github2-tests.ts
@@ -17,15 +17,15 @@ const clientID = process.env.PASSPORT_GITHUB_CONSUMER_KEY;
 const clientSecret = process.env.PASSPORT_GITHUB_CONSUMER_SECRET;
 
 if (typeof callbackURL === "undefined") {
-    throw new Error("callbackURL is undefined");
+  throw new Error("callbackURL is undefined");
 }
 
 if (typeof clientID === "undefined") {
-    throw new Error("clientID is undefined");
+  throw new Error("clientID is undefined");
 }
 
 if (typeof clientSecret === "undefined") {
-    throw new Error("clientSecret is undefined");
+  throw new Error("clientSecret is undefined");
 }
 
 passport.use(new github.Strategy(
