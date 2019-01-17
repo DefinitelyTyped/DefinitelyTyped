f23879e485dd039ab34d07c45f8c71e503104e5b
Nick McCurdy <nick@nickmccurdy.com>
2019-01-17 04:46:10 -0500

    Remove patch from version

--- types/react-inlinesvg/index.d.ts
+++ types/react-inlinesvg/index.d.ts
@@ -1,4 +1,4 @@
-// Type definitions for react-inlinesvg 0.8.3
+// Type definitions for react-inlinesvg 0.8
 // Project: https://github.com/gilbarbara/react-inlinesvg#readme
 // Definitions by: MyCrypto <https://github.com/MyCryptoHQ>, Nick <https://github.com/nickmccurdy>
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
