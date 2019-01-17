af911f7e4abf8777c85453d6f5680bba9ed53213
Nick McCurdy <nick@nickmccurdy.com>
2019-01-17 04:40:14 -0500

    Use React's minimum TypeScript version

--- types/react-inlinesvg/index.d.ts
+++ types/react-inlinesvg/index.d.ts
@@ -2,6 +2,7 @@
 // Project: https://github.com/gilbarbara/react-inlinesvg#readme
 // Definitions by: MyCrypto <https://github.com/MyCryptoHQ>, Nick <https://github.com/nickmccurdy>
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
+// TypeScript Version: 2.8
 
 import { ComponentType, ReactNode } from 'react';
 
