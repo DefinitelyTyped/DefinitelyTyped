4ae250110c3ed4cc33a106592ef177744ba67dd3
lincoln <lincoln@dynamsoft.com>
2019-01-18 15:31:26 +0800

    rollback

--- types/dwt/addon.pdf.d.ts
+++ types/dwt/addon.pdf.d.ts
@@ -1,18 +1,12 @@
-// Type definitions for dwt 14.3.1
-// Project: http://www.dynamsoft.com/Products/WebTWAIN_Overview.aspx
-// Definitions by: Xiao Ling <https://github.com/yushulx>
-//                 Josh Hall <https://github.com/jbh>
-//                 Lincoln Hu <https://github.com/lincoln2018>
-//                 Tom Kent <https://github.com/Tom-Dynamsoft>
-// Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
-// TypeScript Version: 3.2.2
-
 /*!
+* Dynamsoft WebTwain PDF Addon
+* Based on Dynamsoft WebTwain JavaScript Intellisense
 * Product: Dynamsoft Web Twain
 * Web Site: http://www.dynamsoft.com
 *
 * Copyright 2019, Dynamsoft Corporation
 * Author: Dynamsoft Support Team
+* Version: 14.3.1
 */
 
 declare enum EnumDWT_ConvertMode {
@@ -78,4 +72,8 @@ interface PDF {
 
 interface WebTwainAddon {
     PDF: PDF;
+}
+
+interface WebTwain {
+    Addon: WebTwainAddon;
 }
