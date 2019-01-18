c02bd4c1d04fcc8a6a2890d7f98e201eef4b297b
lincoln <37287480+lincoln2018@users.noreply.github.com>
2019-01-18 16:29:54 +0800

    Update index.d.ts
    TypeScript Version can be 3.2
    2017 => 2019

--- types/dwt/V12/index.d.ts
+++ types/dwt/V12/index.d.ts
@@ -2,14 +2,14 @@
 // Project: http://www.dynamsoft.com/Products/WebTWAIN_Overview.aspx
 // Definitions by: Xiao Ling <https://github.com/yushulx>
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
-// TypeScript Version: 2.2
+// TypeScript Version: 3.2
 
 /*!
 * Dynamsoft WebTwain JavaScript Intellisense
 * Product: Dynamsoft Web Twain
 * Web Site: http://www.dynamsoft.com
 *
-* Copyright 2017, Dynamsoft Corporation
+* Copyright 2019, Dynamsoft Corporation
 * Author: Dynamsoft Support Team
 * Version: 12.3
 */
