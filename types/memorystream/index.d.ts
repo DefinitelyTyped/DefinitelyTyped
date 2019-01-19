b0be3eb402850fb43d867b92f1536a8f915ac73f
bangbang93 <bangbang93@163.com>
2019-01-19 09:44:42 +0800

    fill author

--- types/memorystream/index.d.ts
+++ types/memorystream/index.d.ts
@@ -1,6 +1,6 @@
 // Type definitions for memorystream 0.3
 // Project: https://github.com/JSBizon/node-memorystream
-// Definitions by: My Self <https://github.com/me>
+// Definitions by: bangbang93 <https://github.com/bangbang93>
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
 /// <reference types="node" />
 
