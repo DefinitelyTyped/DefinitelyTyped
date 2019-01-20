015cd936b1fb75d94469e5235d89d72b91ab9786
耐小心 <qiqizjl@qq.com>
2019-01-21 03:23:15 +0800

    update

--- types/qiniu-js/index.d.ts
+++ types/qiniu-js/index.d.ts
@@ -1,6 +1,7 @@
 // Type definitions for qiniu-js 2.5.3
 // Project: https://github.com/qiniu/js-sdk#readme
 // Definitions by: taoqf <https://github.com/taoqf>
+// Definitions by: naixiaoxin <https://github.com/qiqizjl>
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
 // TypeScript Version: 2.1
 
