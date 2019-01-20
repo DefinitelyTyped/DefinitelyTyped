29eb2dd893a215ebc97ed580a5fecad80ec05f84
Ferdy Budhidharma <ferd312@gmail.com>
2019-01-20 14:34:59 -0600

    semicolons..

--- types/styled-react-modal/index.d.ts
+++ types/styled-react-modal/index.d.ts
@@ -10,7 +10,7 @@ import { StyledComponent, AnyStyledComponent, CSSObject, InterpolationFunction }
 declare const BaseModalBackground: StyledComponent<'div', any>;
 
 interface ModalProps {
-  children?: React.ReactNode
+  children?: React.ReactNode;
   isOpen: boolean;
   allowScroll?: boolean;
   afterOpen?: () => void;
