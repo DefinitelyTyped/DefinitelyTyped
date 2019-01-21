3c55900c9c6c09ca616377e4a66d0fb62f85314a
Razvan Muscalu <34776591+RazzM13@users.noreply.github.com>
2019-01-21 22:16:46 +0200

    [@types/feathersjs__errors] Added "RazzM13" to "Definitions by" section as per the contribution guidelines.

--- types/feathersjs__errors/index.d.ts
+++ types/feathersjs__errors/index.d.ts
@@ -1,6 +1,7 @@
 // Type definitions for @feathersjs/errors 3.2
 // Project: http://feathersjs.com/
 // Definitions by: Jan Lohage <https://github.com/j2L4e>
+//                 RazzM13 <https://github.com/RazzM13>
 // Definitions: https://github.com/feathersjs-ecosystem/feathers-typescript
 // TypeScript Version: 2.2
 
