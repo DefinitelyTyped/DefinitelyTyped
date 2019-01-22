56ad2793de0dde27009d8c79fc4a99202ea8ee46
Dobes Vandermeer <dobesv@gmail.com>
2019-01-21 16:04:44 -0800

    Mark discounts option in stripe
    The `discount` can be (and usually is) `null` for invoices
    and subscriptions.

--- types/stripe/index.d.ts
+++ types/stripe/index.d.ts
@@ -1999,7 +1999,7 @@ declare namespace Stripe {
              */
             description: string;
 
-            discount: coupons.IDiscount;
+            discount: coupons.IDiscount | null;
 
             /**
              * The date on which payment for this invoice is due. This value will be null for invoices where billing=charge_automatically.
@@ -4629,7 +4629,7 @@ declare namespace Stripe {
              * Describes the current discount applied to this subscription, if there is one. When billing, a discount applied to a
              * subscription overrides a discount applied on a customer-wide basis.
              */
-            discount: coupons.IDiscount;
+            discount: coupons.IDiscount | null;
 
             /**
              * If the subscription has ended (either because it was canceled or because the customer was switched to a subscription
