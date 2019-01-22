3ff73fa2da03c807b697f3e64fe8a251dcfe3aaf
Denis Artyuhovich <i@denis.by>
2019-01-22 14:33:01 +0200

    Add plyableDuration to getPhotosReturnType in RN

--- types/react-native/index.d.ts
+++ types/react-native/index.d.ts
@@ -6742,6 +6742,7 @@ export interface GetPhotosReturnType {
                 uri: string;
                 height: number;
                 width: number;
+                playableDuration: number;
                 isStored?: boolean;
             };
             timestamp: number;
