4083961a03f33f8f071b9a5d6857351e1425a1b5
Philipp Gfeller <tuelsch@gmail.com>
2019-01-23 12:32:26 +0700

    Fix header comments

--- types/fastify-favicon/index.d.ts
+++ types/fastify-favicon/index.d.ts
@@ -1,6 +1,6 @@
-// Type definitions for fastify-favicon v0.3.0
+// Type definitions for fastify-favicon 0.3
 // Project: https://github.com/smartiniOnGitHub/fastify-favicon
-// Definitions by: Philipp Gfeller <https://github.com/tuelsch/>
+// Definitions by: Philipp Gfeller <https://github.com/tuelsch>
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
 
 declare function defaultFaviconPlugin(): void;
