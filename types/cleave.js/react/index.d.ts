0d3bb83141c45ca5ae7cd77d8308e56ea53a044a
Hiep Le <hieptuanle@gmail.com>
2019-01-24 02:35:06 +0700

    Add test and move ChangeEvent interface

--- types/cleave.js/react/index.d.ts
+++ types/cleave.js/react/index.d.ts
@@ -2,9 +2,4 @@ import * as React from "react";
 import { Props } from "./props";
 
 declare var Cleave: React.ComponentClass<Props>;
-declare namespace Cleave {
-    interface ChangeEvent<T> extends React.ChangeEvent<T> {
-        target: { rawValue: string } & EventTarget & T
-    }
-}
 export = Cleave;
