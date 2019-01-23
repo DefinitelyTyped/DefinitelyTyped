ccc4e0ce4523a639cd0151f1a8d6d89e702aff49
Hiep Le <hieptuanle@gmail.com>
2019-01-24 02:52:13 +0700

    Add semicolon

--- types/cleave.js/react/props.d.ts
+++ types/cleave.js/react/props.d.ts
@@ -4,7 +4,7 @@ import { CleaveOptions } from "../options";
 export type InitHandler = (owner: React.ReactInstance) => void;
 
 export interface ChangeEvent<T> extends React.ChangeEvent<T> {
-    target: { rawValue: string } & EventTarget & T
+    target: { rawValue: string } & EventTarget & T;
 }
 
 export type ChangeEventHandler<T = Element> = React.EventHandler<ChangeEvent<T>>;
@@ -13,5 +13,5 @@ export interface Props extends React.InputHTMLAttributes<HTMLInputElement> {
     onInit?: InitHandler;
     options: CleaveOptions;
     htmlRef?: (i: any) => void;
-    onChange?: ChangeEventHandler<HTMLInputElement>
+    onChange?: ChangeEventHandler<HTMLInputElement>;
 }
