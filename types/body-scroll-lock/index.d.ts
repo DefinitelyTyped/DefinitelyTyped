3d225d7d7d2e29ee59580d961ab1ce69c56ac2b7
kimcoder <kimcoder@me.com>
2019-01-24 07:39:22 +0900

    restore Type definition for body-scroll-lock 2.6

--- types/body-scroll-lock/index.d.ts
+++ types/body-scroll-lock/index.d.ts
@@ -1,3 +1,4 @@
+// Type definitions for body-scroll-lock 2.6
 // Project: https://github.com/willmcpo/body-scroll-lock
 // Definitions by: kimcoder <https://github.com/kimcoder>
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
