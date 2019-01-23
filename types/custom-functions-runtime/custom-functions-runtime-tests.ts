fdf388954edab077171456d5467921e70532458e
Adam Krantz <Adam.Krantz@microsoft.com>
2019-01-23 07:47:47 -0800

    Update tests and tweak comments

--- types/custom-functions-runtime/custom-functions-runtime-tests.ts
+++ types/custom-functions-runtime/custom-functions-runtime-tests.ts
@@ -10,15 +10,18 @@ CustomFunctions.associate({
     RANDOM: (n: number) => n * Math.random()
 });
 
+function callerAddress(invocation: CustomFunctions.Invocation) {
+    return invocation.address;
+}
+
 async function getStockValues(ticker: string): Promise<number> {
     const response = await fetch(`myService.com/prices/${ticker}`);
     return (await response.json())["price"];
 }
 
-async function getStockValuesCancellable(
-    ticker: string,
-    invocation: CustomFunctions.CancelableInvocation
-): Promise<number> {
+async function getStockValuesCancellable(ticker: string,
+    invocation: CustomFunctions.CancelableInvocation): Promise<number> {
+    const address = invocation.address;
     let shouldStop = false;
     invocation.onCanceled = () => (shouldStop = true);
     await pause(1000);
@@ -31,10 +34,9 @@ async function getStockValuesCancellable(
     return (await response.json())["price"];
 }
 
-function stockPriceStream(
-    ticker: string,
-    invocation: CustomFunctions.StreamingInvocation<number>
-) {
+function stockPriceStream(ticker: string,
+    invocation: CustomFunctions.StreamingInvocation<number>) {
+    const address = invocation.address;
     const updateFrequency = 10 /* milliseconds*/;
     let isPending = false;
 
--- types/custom-functions-runtime/index.d.ts
+++ types/custom-functions-runtime/index.d.ts
@@ -13,89 +13,82 @@ Copyright (c) Microsoft Corporation
 */
 
 /**
- * CustomFunctions namespace, used by Excel Custom Functions
  * @beta
+ * CustomFunctions namespace, used by Excel Custom Functions
  */
 declare namespace CustomFunctions {
     /**
-     * Associates the JavaScript function to the name given by the "id" property in the metadata JSON file.
      * @beta
+     * Associates the JavaScript function to the name given by the "id" property in the metadata JSON file.
      */
     function associate(id: string, functionObject: Function): void;
     /**
-     * Associates the JavaScript functions to the names given by the "id" properties in the metadata JSON file.
      * @beta
+     * Associates the JavaScript functions to the names given by the "id" properties in the metadata JSON file.
      */
     function associate(mappings: { [key: string]: Function }): void;
 
     /**
-     * Provides information about the invocation of a custom function.
      * @beta
+     * Provides information about the invocation of a custom function.
      */
     interface Invocation {
         /**
+         * @beta
          * The cell address where the function is being called, if requested, otherwise undefined.
          * 
          * To request the address for the function, in the metadata JSON file, the function options should specify:
+         * `{ "requiresAddress": true }`
          * 
-         * {
-         *     "requiresAddress": true
-         * }
-         * 
-         * If the metadata JSON file is being generated from JSDoc comments, include the tag "@requiresAddress".
+         * If the metadata JSON file is being generated from JSDoc comments, include the tag `@requiresAddress`.
          */
         address?: string;
     }
 
     /**
+     * @beta
      * Provides information about the invocation of a cancelable custom function.
      * A cancelable custom function can provide a handler for the onCanceled event.
-     * @beta
      * 
      * To indicate that a function is cancelable, in the metadata JSON file, the function options should specify:
+     * `{ "cancelable": true }`
      * 
-     * {
-     *     "cancelable": true
-     * }
-     * 
-     * If the metadata JSON file is being generated from JSDoc comments, include the tag "@cancelable".
+     * If the metadata JSON file is being generated from JSDoc comments, include the tag `@cancelable`.
      */
     interface CancelableInvocation extends Invocation {
         /**
-         * Event handler called when the custom function is canceled.
          * @beta
+         * Event handler called when the custom function is canceled.
          */
         onCanceled: () => void;
     }
 
     /**
-     * Provides information about the invocation of a cancelable custom function.
      * @beta
-     * @deprecated Use CancelableInvocation instead.
+     * @deprecated Use `CancelableInvocation` instead.
      */
     interface CancelableHandler extends CancelableInvocation {        
     }
 
     /**
+     * @beta
      * Provides information about the invocation of a streaming custom function.
      * A streaming custom function can provide results which can change over time.
      * 
-     * Call setResult() one or more times to provide the result instead of returning
+     * Call `setResult()` one or more times to provide the result instead of returning
      * a result from the function.
-     * @beta
      */
     interface StreamingInvocation<ResultType> extends CancelableInvocation {
         /**
-         * Sets the result for the custom function. May be called more than once.
          * @beta
+         * Set the result for the custom function. May be called more than once.
          */
         setResult: (value: ResultType | Error) => void;
     }
 
     /**
-     * Provides information about the invocation of a streaming custom function.
      * @beta
-     * @deprecated Use StreamingInvocation<ResultType> instead.
+     * @deprecated Use `StreamingInvocation<ResultType>` instead.
      */
     interface StreamingHandler<ResultType> extends StreamingInvocation<ResultType> {
     }
