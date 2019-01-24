c958653f20b51739b2da65ef0c86468cca2c7390
Volodymyr <kravchuk.vladimir@gmail.com>
2019-01-25 01:53:32 +0200

    Fix header validation for tween.js according to dt-header rules

--- types/tween.js/index.d.ts
+++ types/tween.js/index.d.ts
@@ -1,4 +1,4 @@
-// Type definitions for tween.js v17.2.0
+// Type definitions for tween.js 17.2
 // Project: https://github.com/tweenjs/tween.js/
 // Definitions by: jordan <https://github.com/Amos47>, sunetos <https://github.com/sunetos>, jzarnikov <https://github.com/jzarnikov>, alexburner <https://github.com/alexburner>
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
