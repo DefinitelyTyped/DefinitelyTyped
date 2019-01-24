154a1d13d446e06c5ad8497ba69b8b698db9b901
Levin Keller <post@levinkeller.de>
2019-01-24 13:04:26 +0100

    Add negative test for toWei.

--- types/web3/web3-tests.ts
+++ types/web3/web3-tests.ts
@@ -39,36 +39,72 @@ web3.eth.setProvider(myProvider);
 // --------------------------------------------------------------------------
 const storage: Promise<string> = web3.eth.getStorageAt(contractAddress, 0);
 const balance1: Promise<BigNumber> = web3.eth.getBalance(contractAddress);
-const balance2: Promise<BigNumber> = web3.eth.getBalance(contractAddress, "latest");
+const balance2: Promise<BigNumber> = web3.eth.getBalance(
+  contractAddress,
+  "latest"
+);
 const balance3: Promise<BigNumber> = web3.eth.getBalance(contractAddress, 1);
-web3.eth.getBalance(contractAddress, "latest", (error: Error, balance: BigNumber) => { });
-web3.eth.getBalance(contractAddress, 1, (error: Error, balance: BigNumber) => { });
+web3.eth.getBalance(
+  contractAddress,
+  "latest",
+  (error: Error, balance: BigNumber) => {}
+);
+web3.eth.getBalance(
+  contractAddress,
+  1,
+  (error: Error, balance: BigNumber) => {}
+);
 
-const sendSignedTransactionTxReceipt0: PromiEvent<TransactionReceipt> = web3.eth.sendSignedTransaction("",
-    (error: Error, txHash: string) => { });
-const sendSignedTransactionTxReceipt1: PromiEvent<TransactionReceipt> = web3.eth.sendSignedTransaction("")
-    .on("transactionHash", (txHash: string) => { });
-const sendSignedTransactionTxReceipt2: PromiEvent<TransactionReceipt> = web3.eth.sendSignedTransaction("")
-    .on("receipt", (txReceipt: TransactionReceipt) => {
-        const { status }: { status: boolean }  = txReceipt;
-    });
-const sendSignedTransactionTxReceipt3: PromiEvent<TransactionReceipt> = web3.eth.sendSignedTransaction("")
-    .on("confirmation", (confNumber: number, receipt: TransactionReceipt) => { });
-const sendSignedTransactionTxReceipt4: PromiEvent<TransactionReceipt> = web3.eth.sendSignedTransaction("")
-    .on("receipt", (txReceipt: TransactionReceipt) => { })
-    .on("confirmation", (confNumber: number, receipt: TransactionReceipt) => { });
+const sendSignedTransactionTxReceipt0: PromiEvent<
+  TransactionReceipt
+> = web3.eth.sendSignedTransaction("", (error: Error, txHash: string) => {});
+const sendSignedTransactionTxReceipt1: PromiEvent<
+  TransactionReceipt
+> = web3.eth
+  .sendSignedTransaction("")
+  .on("transactionHash", (txHash: string) => {});
+const sendSignedTransactionTxReceipt2: PromiEvent<
+  TransactionReceipt
+> = web3.eth
+  .sendSignedTransaction("")
+  .on("receipt", (txReceipt: TransactionReceipt) => {
+    const { status }: { status: boolean } = txReceipt;
+  });
+const sendSignedTransactionTxReceipt3: PromiEvent<
+  TransactionReceipt
+> = web3.eth
+  .sendSignedTransaction("")
+  .on("confirmation", (confNumber: number, receipt: TransactionReceipt) => {});
+const sendSignedTransactionTxReceipt4: PromiEvent<TransactionReceipt> = web3.eth
+  .sendSignedTransaction("")
+  .on("receipt", (txReceipt: TransactionReceipt) => {})
+  .on("confirmation", (confNumber: number, receipt: TransactionReceipt) => {});
 
-const sendTransactionTxReceipt0: PromiEvent<TransactionReceipt> = web3.eth.sendTransaction({ to: "0x1" },
-    (error: Error, txHash: string) => { });
-const sendTransactionTxReceipt1: PromiEvent<TransactionReceipt> = web3.eth.sendTransaction({ to: "0x1" })
-    .on("transactionHash", (txHash: string) => { });
-const sendTransactionTxReceipt2: PromiEvent<TransactionReceipt> = web3.eth.sendTransaction({ to: "0x1" })
-    .on("receipt", (txReceipt: TransactionReceipt) => { });
-const sendTransactionTxReceipt3: PromiEvent<TransactionReceipt> = web3.eth.sendTransaction({ to: "0x1" })
-    .on("confirmation", (confNumber: number, receipt: TransactionReceipt) => { });
-const sendTransactionTxReceipt4: PromiEvent<TransactionReceipt> = web3.eth.sendTransaction({ to: "0x1" })
-    .on("receipt", (txReceipt: TransactionReceipt) => { })
-    .on("confirmation", (confNumber: number, receipt: TransactionReceipt) => { });
+const sendTransactionTxReceipt0: PromiEvent<
+  TransactionReceipt
+> = web3.eth.sendTransaction(
+  { to: "0x1" },
+  (error: Error, txHash: string) => {}
+);
+const sendTransactionTxReceipt1: PromiEvent<
+  TransactionReceipt
+> = web3.eth
+  .sendTransaction({ to: "0x1" })
+  .on("transactionHash", (txHash: string) => {});
+const sendTransactionTxReceipt2: PromiEvent<
+  TransactionReceipt
+> = web3.eth
+  .sendTransaction({ to: "0x1" })
+  .on("receipt", (txReceipt: TransactionReceipt) => {});
+const sendTransactionTxReceipt3: PromiEvent<
+  TransactionReceipt
+> = web3.eth
+  .sendTransaction({ to: "0x1" })
+  .on("confirmation", (confNumber: number, receipt: TransactionReceipt) => {});
+const sendTransactionTxReceipt4: PromiEvent<TransactionReceipt> = web3.eth
+  .sendTransaction({ to: "0x1" })
+  .on("receipt", (txReceipt: TransactionReceipt) => {})
+  .on("confirmation", (confNumber: number, receipt: TransactionReceipt) => {});
 
 //
 // web3.eth.subscribe
@@ -77,14 +113,10 @@ const sendTransactionTxReceipt4: PromiEvent<TransactionReceipt> = web3.eth.sendT
 //
 // web3.eth.Contract
 // --------------------------------------------------------------------------
-const myContract = new web3.eth.Contract(
-    [],
-    contractAddress,
-    {
-        from: "0x1234567890123456789012345678901234567891",
-        gasPrice: "20000000000"
-    }
-);
+const myContract = new web3.eth.Contract([], contractAddress, {
+  from: "0x1234567890123456789012345678901234567891",
+  gasPrice: "20000000000"
+});
 
 myContract.options.from = "0x1234567890123456789012345678901234567891";
 myContract.options.gasPrice = "20000000000000";
@@ -99,16 +131,20 @@ const account = web3.eth.accounts.privateKeyToAccount("0x1234");
 const noPublicKeyInAccount: typeof account & { publicKey?: never } = account;
 
 const testTx = {
-    to: '0xF0109fC8DF283027b6285cc889F5aA624EaC1F55',
-    value: '1000000000',
-    gas: 2000000
+  to: "0xF0109fC8DF283027b6285cc889F5aA624EaC1F55",
+  value: "1000000000",
+  gas: 2000000
 };
 
 web3.eth.accounts.signTransaction(testTx, "").then(txSig => {
-    txSig.messageHash = "0x1234";
-    txSig.rawTransaction = "0x5678";
+  txSig.messageHash = "0x1234";
+  txSig.rawTransaction = "0x5678";
 
-    const noHashFieldInTxSig: typeof txSig & { hash?: never, message?: never, signature?: never } = txSig;
+  const noHashFieldInTxSig: typeof txSig & {
+    hash?: never;
+    message?: never;
+    signature?: never;
+  } = txSig;
 });
 
 const msgSig = web3.eth.accounts.sign("0x1234", "0x5678");
@@ -116,24 +152,23 @@ msgSig.messageHash = "0x1234";
 msgSig.message = "0x5678";
 msgSig.signature = "0x90ab";
 
-const noHashFieldInMsgSig: typeof msgSig & { hash?: never, rawTransaction?: never } = msgSig;
+const noHashFieldInMsgSig: typeof msgSig & {
+  hash?: never;
+  rawTransaction?: never;
+} = msgSig;
 
 const encryptedKeystore = web3.eth.accounts.encrypt("0x1234", "5678");
 encryptedKeystore.crypto.cipher = "aes-128-ctr";
 
 const msgSignature: string = account.sign("0x1234").signature;
 account.signTransaction(testTx).then(txSig => {
-    const txSignature: string = txSig.rawTransaction;
+  const txSignature: string = txSig.rawTransaction;
 });
 
 //
 // web3.eth.personal
 // --------------------------------------------------------------------------
-web3.eth.personal.unlockAccount(
-    web3.eth.defaultAccount,
-    "passphrase",
-    600
-);
+web3.eth.personal.unlockAccount(web3.eth.defaultAccount, "passphrase", 600);
 
 //
 // web3.eth.ens
@@ -162,6 +197,8 @@ const shhPromise: Promise<string> = web3.shh.generateSymKeyFromPassword("xyz");
 // web3.utils
 // --------------------------------------------------------------------------
 const weiStr: string = web3.utils.toWei("100", "gwei");
+// $ExpectError
+const weiStr2 = web3.utils.toWei(100);
 const weiBn: BigNumber = web3.utils.toWei(web3.utils.toBN("1"));
 const rndHex: string = Web3.utils.randomHex(10);
 const sha3: string = web3.utils.soliditySha3(0, 1, "abc");
