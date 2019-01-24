141c38ba8767f62417d0cb43744f14544479d271
Andrew Potter <golawaya@users.noreply.github.com>
2019-01-24 14:58:14 -0700

    Fixed version number

--- types/clean-css/index.d.ts
+++ types/clean-css/index.d.ts
@@ -1,10 +1,10 @@
-// Type definitions for clean-css 4.2.1
+// Type definitions for clean-css 4.2
 // Project: https://github.com/jakubpawlowicz/clean-css
 // Definitions by: Tanguy Krotoff <https://github.com/tkrotoff>
 //                 Andrew Potter <https://github.com/GolaWaya>
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
-/// <reference types="node" />
 
+/// <reference types="node" />
 import { RequestOptions as HttpsRequestOptions } from "https";
 import { RequestOptions as HttpRequestOptions } from "http";
 
