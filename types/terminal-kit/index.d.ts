e7961e1511b6614b3b7dbc52a15e7a77a480ea6c
katsanva <this.is.ftm@gmail.com>
2019-01-25 03:33:00 +0400

    Add types for terminal-kit

---
+++ types/terminal-kit/index.d.ts
@@ -0,0 +1,49 @@
+// Type definitions for terminal-kit 1.26
+// Project: https://github.com/cronvel/terminal-kit#readme
+// Definitions by: katsanva <https://github.com/katsanva>
+// Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
+// TypeScript Version: 2.7
+
+import Terminal = require("./Terminal");
+import * as SB from "./ScreenBuffer";
+import * as SBHD from "./ScreenBufferHD";
+import * as R from "./Rect";
+import * as TB from "./TextBuffer";
+
+export as namespace termkit;
+
+export const terminal: Terminal;
+
+export const realTerminal: Terminal;
+
+export function createTerminal(
+  createOptions?: Terminal.CreateOptions
+): Terminal;
+
+export function getParentTerminalInfo(
+  callback: (error: any, codename?: string, name?: string, pid?: number) => void
+): void;
+
+export function getDetectedTerminal(
+  calback: (error: any, term: Terminal) => void
+): void;
+
+export function autoComplete(
+  array: ReadonlyArray<string>,
+  startString: string,
+  returnAlternatives?: boolean,
+  prefix?: string,
+  postfix?: string
+): string;
+
+export function stripEscapeSequences(str: string): string;
+
+export function stringWidth(str: string): number;
+
+export function truncateString(str: string, maxWidth: number): string;
+
+export { default as ScreenBufferHD } from "./ScreenBufferHD";
+export { default as ScreenBuffer } from "./ScreenBuffer";
+export { default as Rect } from "./Rect";
+export { default as TextBuffer } from "./TextBuffer";
+export { default as Terminal } from "./Terminal";
