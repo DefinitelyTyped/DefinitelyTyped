fcc885dceb686d8cecbe99cab1bf08ddcf04cbed
SPWizard01 <the.mtrix@gmail.com>
2019-01-25 16:19:56 +0200

    bump version number to 6.2.1

--- types/react-paginate/index.d.ts
+++ types/react-paginate/index.d.ts
@@ -1,4 +1,4 @@
-// Type definitions for react-paginate 6.0
+// Type definitions for react-paginate 6.2.1
 // Project: https://github.com/AdeleD/react-paginate
 // Definitions by: Simon Hartcher <https://github.com/deevus>
 //                 Wouter Hardeman <https://github.com/wouterhardeman>
@@ -6,6 +6,7 @@
 //                 Simon Archer <https://github.com/archy-bold>
 //                 Yasunori Ohoka <https://github.com/yasupeke>
 //                 Shingo Sato <https://github.com/sugarshin>
+//                 SPWizard01 <https://github.com/SPWizard01>
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
 // TypeScript Version: 2.8
 
