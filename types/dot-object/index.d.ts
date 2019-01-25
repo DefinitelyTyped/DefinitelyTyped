9f4e7a9f6115786da122a8a3cc0419d4b87cdfa1
Josh <MechJosh0@users.noreply.github.com>
2019-01-25 10:37:45 +0000

    Update index.d.ts

--- types/dot-object/index.d.ts
+++ types/dot-object/index.d.ts
@@ -1,8 +1,8 @@
 // Type definitions for Dot-Object 1.7
-// TypeScript Version: 2.2
 // Project: https://github.com/rhalff/dot-object
 // Definitions by: Niko Kovačič <https://github.com/nkovacic>
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
+// TypeScript Version: 2.2
 
 
 declare namespace DotObject {
