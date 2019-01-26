3034597b9c9438494326f139796943da9680149b
Hugo Alliaume <kocal@live.fr>
2019-01-26 17:22:08 +0100

    fix(buble): add "dom" lib, fix sourcemap and linting issues

--- types/buble/buble-tests.ts
+++ types/buble/buble-tests.ts
@@ -4,74 +4,74 @@ const input = 'const answer = () => 42;';
 
 // Simple transform
 (() => {
-  const output = buble.transform(input);
+    const output = buble.transform(input);
 
-  console.log(output.code);
-  console.log(output.map.toString());
-  console.log(output.map.toUrl);
+    console.log(output.code);
+    console.log(output.map.toString());
+    console.log(output.map.toUrl);
 })();
 
 // Transform for Chrome & Firefox
 (() => {
-  const output = buble.transform(input, {
-    target: {
-      chrome: 71,
-      firefox: 64,
-    },
-  });
+    const output = buble.transform(input, {
+        target: {
+            chrome: 71,
+            firefox: 64,
+        },
+    });
 })();
 
 // Transform with sourcemaps support
 (() => {
-  const output = buble.transform(input, {
-    file: 'output.js',
-    input: 'input.js',
-    includeContent: true,
-  });
+    const output = buble.transform(input, {
+        source: 'input.js',
+        file: 'output.js',
+        includeContent: true,
+    });
 })();
 
 // Transform for Preact
 (() => {
-  const output = buble.transform(input, {
-    jsx: 'h'
-  });
+    const output = buble.transform(input, {
+        jsx: 'h'
+    });
 })();
 
 // Transform with Object assign/spread support
 (() => {
-  let output = buble.transform(input, {
-    objectAssign: true,
-  });
-  output = buble.transform(input, {
-    objectAssign: 'MyCustomObjectAssign',
-  });
+    let output = buble.transform(input, {
+        objectAssign: true,
+    });
+    output = buble.transform(input, {
+        objectAssign: 'MyCustomObjectAssign',
+    });
 })();
 
 // Transform with every transforms
 (() => {
-  const output = buble.transform(input, {
-    transforms: {
-      arrow: true,
-      classes: false,
-      collections: true,
-      computedProperty: false,
-      conciseMethodProperty: true,
-      constLoop: false,
-      dangerousForOf: true,
-      dangerousTaggedTemplateString: false,
-      defaultParameter: true,
-      destructuring: false,
-      forOf: true,
-      generator: false,
-      letConst: true,
-      modules: false,
-      numericLiteral: true,
-      parameterDestructuring: false,
-      reservedProperties: true,
-      spreadRest: false,
-      stickyRegExp: true,
-      templateString: false,
-      unicodeRegExp: true,
-    }
-  });
+    const output = buble.transform(input, {
+        transforms: {
+            arrow: true,
+            classes: false,
+            collections: true,
+            computedProperty: false,
+            conciseMethodProperty: true,
+            constLoop: false,
+            dangerousForOf: true,
+            dangerousTaggedTemplateString: false,
+            defaultParameter: true,
+            destructuring: false,
+            forOf: true,
+            generator: false,
+            letConst: true,
+            modules: false,
+            numericLiteral: true,
+            parameterDestructuring: false,
+            reservedProperties: true,
+            spreadRest: false,
+            stickyRegExp: true,
+            templateString: false,
+            unicodeRegExp: true,
+        }
+    });
 })();
