18de647d22cf0c498173e5b650883659653e3622
Hugo Alliaume <kocal@live.fr>
2019-01-26 21:31:52 +0100

    chore: setup type

---
+++ types/rollup-plugin-node-builtins/index.d.ts
@@ -0,0 +1,5 @@
+// Type definitions for rollup-plugin-node-builtins 2.1
+// Project: https://github.com/calvinmetcalf/rollup-plugin-node-builtins#readme
+// Definitions by: Kocal <https://github.com/Kocal>
+// Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
+
