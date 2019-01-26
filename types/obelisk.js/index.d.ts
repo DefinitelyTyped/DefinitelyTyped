7f3293acfc9931805585202606ca63079f5a332d
rhysd <lin90162@yahoo.co.jp>
2019-01-26 15:51:43 +0900

    enable file header check and fix version format

--- types/obelisk.js/index.d.ts
+++ types/obelisk.js/index.d.ts
@@ -1,4 +1,4 @@
-// Type definitions for obelisk.js v1.2.1
+// Type definitions for obelisk.js 1.2
 // Project: https://github.com/nosir/obelisk.js/
 // Definitions by: Brian Drupieski <https://github.com/bdrupieski>
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
