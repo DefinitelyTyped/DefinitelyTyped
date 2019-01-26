fba358da45c6dda900a64c299bb08055de535e65
Vincent Langlet <VincentLanglet@users.noreply.github.com>
2019-01-26 13:27:05 +0100

    Update enzyme-tests.tsx

--- types/enzyme/enzyme-tests.tsx
+++ types/enzyme/enzyme-tests.tsx
@@ -868,10 +868,10 @@ function ReactWrapperTest() {
 
     function test_component_type() {
       const wrapper1 = shallow(<div><ComponentType stringProp={"S"} numberProp={1} /></div>);
-      wrapper1.find(ComponentType).props().stringProp; // $ExpectType string
+      wrapper1.find<MyComponentProps>(ComponentType).props().stringProp; // $ExpectType string
 
       const wrapper2 = mount(<div><ComponentType stringProp={"S"} numberProp={1} /></div>);
-      wrapper2.find(ComponentType).props().stringProp; // $ExpectType string
+      wrapper2.find<MyComponentProps>(ComponentType).props().stringProp; // $ExpectType string
     }
 }
 
