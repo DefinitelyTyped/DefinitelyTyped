7f27ae1f9169cdf2225fb89466d44cbaa1f6cf88
Dolan Miu <dolan_miu@hotmail.com>
2019-01-27 17:49:49 +0000

    Version bump

--- types/nodegit/index.d.ts
+++ types/nodegit/index.d.ts
@@ -1,4 +1,4 @@
-// Type definitions for nodegit 0.22
+// Type definitions for nodegit 0.24
 // Project: https://github.com/nodegit/nodegit
 // Definitions by: Dolan Miu <https://github.com/dolanmiu>, Tobias Nießen <https://github.com/tniessen>
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
