ef068528b46455cfb2c28c164e2db7b913e3b8d8
Hugo Alliaume <kocal@live.fr>
2019-01-27 10:09:58 +0100

    feat: add typings for defaults-deep

---
+++ types/defaults-deep/index.d.ts
@@ -0,0 +1,10 @@
+// Type definitions for defaults-deep 0.2
+// Project: https://github.com/jonschlinkert/defaults-deep
+// Definitions by: Kocal <https://github.com/Kocal>
+// Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
+
+interface Obj {
+    [k: string]: any;
+}
+
+export default function defaultDeep(obj?: Obj, ...objs: Obj[]): Obj;
