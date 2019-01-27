ce8bddf8b0cb96a2d070b9816a4edf6e696c938d
Hugo Alliaume <kocal@live.fr>
2019-01-27 12:00:26 +0100

    chore: use my name

--- types/rollup-plugin-node-builtins/index.d.ts
+++ types/rollup-plugin-node-builtins/index.d.ts
@@ -1,6 +1,6 @@
 // Type definitions for rollup-plugin-node-builtins 2.1
 // Project: https://github.com/calvinmetcalf/rollup-plugin-node-builtins#readme
-// Definitions by: Kocal <https://github.com/Kocal>
+// Definitions by: Hugo Alliaume <https://github.com/Kocal>
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
 // TypeScript Version: 2.3
 
