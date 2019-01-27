73f13bed814d26fdd051a945022959ef81fb8b04
Ryan Sonshine <rsonshine@lenderful.com>
2019-01-27 14:54:17 -0500

    Removes docstring from dlv type definition

--- types/dlv/index.d.ts
+++ types/dlv/index.d.ts
@@ -8,10 +8,5 @@
  * Safely get a dot-notated path within a nested object, with ability to
  * return a default if the full key path does not exist or the value is
  * undefined
- *
- * @param {object} object Object to look up the key on
- * @param {(string | string[])} key Key in dot notation to lookup. Can also be an array key.
- * @param {any} [defaultValue] Default value to return if the full key path does not exist on the object
- * @returns {any} Value if found in path, the default if not found, undefined if no default provided
-*/
+ */
 export default function dlv(object: object, key: string | string[], defaultValue?: any): any;
