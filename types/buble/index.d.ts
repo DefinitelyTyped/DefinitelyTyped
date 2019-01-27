54ff498709b81ecac06d6842057ae3512939e8f7
Hugo Alliaume <kocal@live.fr>
2019-01-27 12:01:38 +0100

    chore: use my name

--- types/buble/index.d.ts
+++ types/buble/index.d.ts
@@ -1,6 +1,6 @@
 // Type definitions for buble 0.19
 // Project: https://github.com/Rich-Harris/buble#README
-// Definitions by: Kocal <https://github.com/Kocal>
+// Definitions by: Hugo Alliaume <https://github.com/Kocal>
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
 // TypeScript Version: 2.1
 
