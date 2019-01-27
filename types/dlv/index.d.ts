f8ce3b3f9fc26484f43dbf85a311788ed7a7f364
Ryan Sonshine <rsonshine@lenderful.com>
2019-01-27 14:11:52 -0500

    Added type definitions for dlv

---
+++ types/dlv/index.d.ts
@@ -0,0 +1,22 @@
+// Type definitions for dlv 1.1.2
+// Project: https://github.com/developit/dlv
+// Definitions by: Ryan Sonshine <https://github.com/ryansonshine>
+// Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
+// TypeScript Version: 2.8
+declare module 'dlv' {
+    /**
+     * Safely get a dot-notated path within a nested object, with ability to
+     * return a default if the full key path does not exist or the value is
+     * undefined
+     *
+     * @param {object} object Object to look up the key on
+     * @param {(string | string[])} key Key in dot notation to lookup. Can also be an array key.
+     * @param {T} [defaultValue] Default value to return if the full key path does not exist on the object
+     * @returns {(any | T))} Value if found in path, the default if not found, undefined if no default provided
+    */
+    export default function dlv<T>(
+        object       : object,
+        key          : string | string[],
+        defaultValue?: T,
+    ): any | T;
+}
