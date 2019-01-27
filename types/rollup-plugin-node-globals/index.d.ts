a3ce51ce058fe7d35f7e507af91184bc7e09c0ce
Hugo Alliaume <kocal@live.fr>
2019-01-27 12:00:53 +0100

    chore: use my name

--- types/rollup-plugin-node-globals/index.d.ts
+++ types/rollup-plugin-node-globals/index.d.ts
@@ -1,6 +1,6 @@
 // Type definitions for rollup-plugin-node-globals 1.4
 // Project: https://github.com/calvinmetcalf/rollup-plugin-node-globals#readme
-// Definitions by: Kocal <https://github.com/kocal>
+// Definitions by: Hugo Alliaume <https://github.com/kocal>
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
 // TypeScript Version: 2.3
 
