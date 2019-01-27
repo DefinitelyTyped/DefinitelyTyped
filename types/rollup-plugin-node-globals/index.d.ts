3f944ca274d0eb2404905e1de7d4ba76535074e1
Hugo Alliaume <kocal@live.fr>
2019-01-27 08:48:50 +0100

    chore: setup type

---
+++ types/rollup-plugin-node-globals/index.d.ts
@@ -0,0 +1,4 @@
+// Type definitions for rollup-plugin-node-globals 1.4
+// Project: https://github.com/calvinmetcalf/rollup-plugin-node-globals#readme
+// Definitions by: Kocal <https://github.com/kocal>
+// Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
