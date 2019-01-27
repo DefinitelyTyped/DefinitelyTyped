5b4074d724687ec45fde17a7df405cc1c9c54bd2
Hugo Alliaume <kocal@live.fr>
2019-01-27 11:59:55 +0100

    chore: use my name

--- types/defaults-deep/index.d.ts
+++ types/defaults-deep/index.d.ts
@@ -1,6 +1,6 @@
 // Type definitions for defaults-deep 0.2
 // Project: https://github.com/jonschlinkert/defaults-deep
-// Definitions by: Kocal <https://github.com/Kocal>
+// Definitions by: Hugo Alliaume <https://github.com/Kocal>
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
 
 interface Obj {
