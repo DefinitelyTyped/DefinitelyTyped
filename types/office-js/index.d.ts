754b2a35843567a6b2eb5ad0f275132671e46d24
Elizabeth Samuel <elizs@microsoft.com>
2019-01-28 11:20:10 -0800

    [office-js] [office-js-preview] Fix occurrence typos (#32551)

--- types/office-js/index.d.ts
+++ types/office-js/index.d.ts
@@ -10844,8 +10844,8 @@ declare namespace Office {
          *   </tr>
          *   <tr>
          *     <td>Appointment items</td>
-         *     <td>These are calendar items of the item class IPM.Appointment or IPM.Appointment.Occurence.</td>
-         *     <td>IPM.Appointment,IPM.Appointment.Occurence</td>
+         *     <td>These are calendar items of the item class IPM.Appointment or IPM.Appointment.Occurrence.</td>
+         *     <td>IPM.Appointment,IPM.Appointment.Occurrence</td>
          *   </tr>
          *   <tr>
          *     <td>Message items</td>
@@ -12648,8 +12648,8 @@ declare namespace Office {
          *   </tr>
          *   <tr>
          *     <td>Appointment items</td>
-         *     <td>These are calendar items of the item class IPM.Appointment or IPM.Appointment.Occurence.</td>
-         *     <td>IPM.Appointment,IPM.Appointment.Occurence</td>
+         *     <td>These are calendar items of the item class IPM.Appointment or IPM.Appointment.Occurrence.</td>
+         *     <td>IPM.Appointment,IPM.Appointment.Occurrence</td>
          *   </tr>
          *   <tr>
          *     <td>Message items</td>
@@ -14303,8 +14303,8 @@ declare namespace Office {
          *   </tr>
          *   <tr>
          *     <td>Appointment items</td>
-         *     <td>These are calendar items of the item class IPM.Appointment or IPM.Appointment.Occurence.</td>
-         *     <td>IPM.Appointment,IPM.Appointment.Occurence</td>
+         *     <td>These are calendar items of the item class IPM.Appointment or IPM.Appointment.Occurrence.</td>
+         *     <td>IPM.Appointment,IPM.Appointment.Occurrence</td>
          *   </tr>
          *   <tr>
          *     <td>Message items</td>
