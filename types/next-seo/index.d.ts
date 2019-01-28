ac4b9e6cdd80c9bcda463b8024ae501cf5732cdd
Thomas Kosiewski <thoma471@googlemail.com>
2019-01-28 19:44:37 +0100

    Next-seo: Test fixes

--- types/next-seo/index.d.ts
+++ types/next-seo/index.d.ts
@@ -2,84 +2,83 @@
 // Project: https://github.com/garmeeh/next-seo#readme
 // Definitions by: Thomas Kosiewski <https://github.com/ThomasK33>
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
+// TypeScript Version: 2.8
 
 import { Component } from "react";
 
-declare module "next-seo" {
-    interface INextSeoProps {
-        config: IConfig;
-    }
-
-    interface IConfig {
-        title?: string;
-        titleTemplate?: string;
-        description?: string;
-        canonical?: string;
-
-        dangerouslySetAllPagesToNoIndex?: boolean;
-        noindex?: boolean;
-
-        twitter?: ITwitter;
-        facebook?: IFacebook;
-        openGraph?: IOpenGraph;
-    }
-
-    interface ITwitter {
-        cardType?: string;
-        site?: string;
-        handle?: string;
-    }
-
-    interface IFacebook {
-        appId?: number;
-    }
-
-    interface IOpenGraph {
-        url?: string;
-        type?: string;
-        title?: string;
-        description?: string;
-        images?: ReadonlyArray<IOpenGraphImages>;
-        defaultImageHeight?: number;
-        defaultImageWidth?: number;
-        locale?: string;
-        site_name?: string;
-
-        profile?: IOpenGraphProfile;
-        book?: IOpenGraphBook;
-        article?: IOpenGraphArticle;
-    }
-
-    interface IOpenGraphImages {
-        url?: string,
-        width?: number,
-        height?: number,
-        alt?: string,
-    }
-
-    interface IOpenGraphProfile {
-        firstName?: string;
-        lastName?: string;
-        username?: string;
-        gender?: string;
-    }
-
-    interface IOpenGraphBook {
-        authors?: ReadonlyArray<string>;
-        isbn?: string;
-        releaseDate?: string;
-        tags?: ReadonlyArray<string>;
-    }
-
-    interface IOpenGraphArticle {
-        publishedTime?: string;
-        modifiedTime?: string;
-        expirationTime?: string;
-
-        authors?: ReadonlyArray<string>;
-        section?: string;
-        tags?: ReadonlyArray<string>;
-    }
-
-    export default class NextSeo extends Component<INextSeoProps> { }
+export interface NextSeoProps {
+    config: Config;
 }
+
+export interface Config {
+    title?: string;
+    titleTemplate?: string;
+    description?: string;
+    canonical?: string;
+
+    dangerouslySetAllPagesToNoIndex?: boolean;
+    noindex?: boolean;
+
+    twitter?: Twitter;
+    facebook?: Facebook;
+    openGraph?: OpenGraph;
+}
+
+export interface Twitter {
+    cardType?: string;
+    site?: string;
+    handle?: string;
+}
+
+export interface Facebook {
+    appId?: number;
+}
+
+export interface OpenGraph {
+    url?: string;
+    type?: string;
+    title?: string;
+    description?: string;
+    images?: ReadonlyArray<OpenGraphImages>;
+    defaultImageHeight?: number;
+    defaultImageWidth?: number;
+    locale?: string;
+    site_name?: string;
+
+    profile?: OpenGraphProfile;
+    book?: OpenGraphBook;
+    article?: OpenGraphArticle;
+}
+
+export interface OpenGraphImages {
+    url?: string;
+    width?: number;
+    height?: number;
+    alt?: string;
+}
+
+export interface OpenGraphProfile {
+    firstName?: string;
+    lastName?: string;
+    username?: string;
+    gender?: string;
+}
+
+export interface OpenGraphBook {
+    authors?: ReadonlyArray<string>;
+    isbn?: string;
+    releaseDate?: string;
+    tags?: ReadonlyArray<string>;
+}
+
+export interface OpenGraphArticle {
+    publishedTime?: string;
+    modifiedTime?: string;
+    expirationTime?: string;
+
+    authors?: ReadonlyArray<string>;
+    section?: string;
+    tags?: ReadonlyArray<string>;
+}
+
+export default class NextSeo extends Component<NextSeoProps> { }
