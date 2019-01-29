36daff0a635274083f15352d3d460be8bd8ef68c
Priscila Moneo <priscila_moneo@hotmail.com.ar>
2019-01-29 13:28:54 -0300

    Added missing props to type definition

--- types/reactable/index.d.ts
+++ types/reactable/index.d.ts
@@ -1,8 +1,8 @@
 // Type definitions for reactable 0.14
 // Project: https://github.com/glittershark/reactable
-// Definitions by: Christoph Spielmann <https://github.com/spielc>
+// Definitions by: Christoph Spielmann <https://github.com/spielc>, Priscila Moneo <https://github.com/priscila-moneo>
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
-// TypeScript Version: 2.8
+// TypeScript Version: 3.1.6
 
 import * as React from 'react';
 
@@ -23,10 +23,16 @@ export interface TableComponentProperties<T> {
     columns?: ColumnsType[];
     defaultSort?: { column: string, direction: SortDirection };
     id?: string;
-    sortable?: string[];
+    sortable?: string[] | boolean;
+    sortBy?: boolean;
     filterable?: string[];
     filterBy?: string;
     onFilter?: FilterMethodType;
+    itemsPerPage?: number;
+    noDataText?: string;
+    pageButtonLimit?: number;
+    currentPage?: number;
+    hideFilterInput?: boolean;
 }
 
 export interface ThProperties {
