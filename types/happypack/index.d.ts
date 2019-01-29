c37c0288cc72c5c611c7fd29a507f781e6a4ed73
Akash Vishwakarma <akash.vishwakarma@milliman.com>
2019-01-29 13:32:53 +0530

    Update index.d.ts

--- types/happypack/index.d.ts
+++ types/happypack/index.d.ts
@@ -2,6 +2,7 @@
 // Project: https://github.com/amireh/happypack
 // Definitions by: Akash Vishwakarma <https://github.com/akashishu777>
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
+// TypeScript Version: 2.8
 
 import { Plugin } from 'webpack';
 
