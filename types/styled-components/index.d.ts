df00c1d61d95173b0e153897706a288c146bdce2
andretshurotshka <andwebar@gmail.com>
2019-01-30 18:00:01 +0000

    Replace Omit with loose version

--- types/styled-components/index.d.ts
+++ types/styled-components/index.d.ts
@@ -330,7 +330,9 @@ export type ThemedCssFunction<T extends object> = BaseThemedCssFunction<
 >;
 
 // Helper type operators
-type Omit<T, K extends keyof T> = Pick<T, Exclude<keyof T, K>>;
+type Omit<T, K extends keyof any> = T extends any
+  ? Pick<T, Exclude<keyof T, K>>
+  : never;
 type WithOptionalTheme<P extends { theme?: T }, T> = Omit<P, "theme"> & {
     theme?: T;
 };
