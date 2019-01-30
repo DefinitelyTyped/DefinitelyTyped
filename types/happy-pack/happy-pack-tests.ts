f78899b07047dae4a0eaa04011533d0ad9b3c10f
Akash Vishwakarma <akash.vishwakarma@milliman.com>
2019-01-30 13:38:14 +0530

    updating code hope this time test will run without errror

---
+++ types/happy-pack/happy-pack-tests.ts
@@ -0,0 +1,5 @@
+import * as happyPack from 'happy-pack';
+
+const ref: happyPack = new happyPack({
+	loaders: ['ts-loader']
+});
--- types/happy-pack/happy-packs-tests.ts
+++
@@ -1,5 +0,0 @@
-import * as happyPacks from 'happy-pack';
-
-const ref: happyPacks = new happyPacks({
-	loaders: ['ts-loader']
-});
