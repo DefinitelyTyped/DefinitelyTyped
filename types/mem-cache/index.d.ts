c07384eacc4acd36be2db8eb9790c932a78d2c15
MutterPedro <pedro@behive.io>
2019-01-30 21:52:49 -0200

    fix header

--- types/mem-cache/index.d.ts
+++ types/mem-cache/index.d.ts
@@ -1,4 +1,4 @@
-// Type definitions for mem-cache v1.0.0
+// Type definitions for mem-cache 1.0
 // Project: https://github.com/silviom/node-mem-cache
 // Definitions by: Pedro Mutter <https://github.com/MutterPedro>
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
