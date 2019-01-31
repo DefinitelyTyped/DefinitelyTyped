52fc881af691c7e85bf1e945c262f163ecd0a238
serderovsh <steein.shamsudin@gmail.com>
2019-01-31 07:15:44 +0300

    update tronWeb

---
+++ types/tronweb/tronweb-tests.ts
@@ -0,0 +1,8 @@
+import { TronWeb } from "tronweb";
+
+const HttpProvider = TronWeb.providers.HttpProvider; // $ExpectType Plugin
+const fullNode = new HttpProvider('https://api.trongrid.io'); // $ExpectType Plugin
+const solidityNode = new HttpProvider('https://api.trongrid.io:'); // $ExpectType Plugin
+const eventServer = 'https://api.trongrid.io'; // $ExpectType Plugin
+
+const tronWeb = new TronWeb(fullNode, solidityNode, eventServer); // $ExpectType Plugin
