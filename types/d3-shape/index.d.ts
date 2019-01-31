48c18d585800319f2a7b4a1d58a3cda2a7ecd339
denis <denisname@users.noreply.github.com>
2019-01-31 21:50:23 +0100

    Update version in definition header

--- types/d3-shape/index.d.ts
+++ types/d3-shape/index.d.ts
@@ -1,4 +1,4 @@
-// Type definitions for D3JS d3-shape module 1.2
+// Type definitions for D3JS d3-shape module 1.3
 // Project: https://github.com/d3/d3-shape/
 // Definitions by: Tom Wanzek <https://github.com/tomwanzek>
 //                 Alex Ford <https://github.com/gustavderdrache>
