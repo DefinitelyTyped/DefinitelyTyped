47028c5c757fdc5787ae1990b3f2912b3d333c43
Anthony Lenglet <36004334+AnthonyLenglet@users.noreply.github.com>
2019-02-01 10:48:18 +0100

    Bump @types/nouislider version

--- types/nouislider/index.d.ts
+++ types/nouislider/index.d.ts
@@ -1,4 +1,4 @@
-// Type definitions for nouislider v9.0.0
+// Type definitions for nouislider v9.0.1
 // Project: https://github.com/leongersen/noUiSlider
 // Definitions by: Patrick Davies <https://github.com/bleuarg>, Guust Nieuwenhuis <https://github.com/lagaffe>
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
