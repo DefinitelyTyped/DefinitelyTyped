6f5cfcb9e0e7ecf7561cfe6a922d002ea14ebe2b
Shireesha Bongarala <bongaralashireesha@gmail.com>
2019-02-03 13:05:34 +0530

    Add minor version

--- types/mathjs/index.d.ts
+++ types/mathjs/index.d.ts
@@ -1,4 +1,4 @@
-// Type definitions for mathjs 5
+// Type definitions for mathjs 5.0
 // Project: http://mathjs.org/
 // Definitions by: Ilya Shestakov <https://github.com/siavol>,
 //                  Andy Patterson <https://github.com/andnp>,
