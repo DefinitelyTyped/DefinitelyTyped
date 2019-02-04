dacd441485616ed5c513252998def8d92b4245fc
Anthony Dionise <dionisea@frib.msu.edu>
2019-02-04 18:37:00 -0500

    remove patch version

--- types/connect-history-api-fallback-exclusions/index.d.ts
+++ types/connect-history-api-fallback-exclusions/index.d.ts
@@ -1,4 +1,4 @@
-// Type definitions for connect-history-api-fallback-exclusions 1.5.1
+// Type definitions for connect-history-api-fallback-exclusions 1.5
 // Project: https://github.com/Wirewheel/connect-history-api-fallback#readme
 // Definitions by: Tony Stone <https://github.com/tonystonee>
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
