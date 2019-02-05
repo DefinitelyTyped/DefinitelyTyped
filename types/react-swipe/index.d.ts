28d02d9cf27fdc9ae89683771b2b75cd46e2c09d
Ammar Alakkad <am.alakkad@gmail.com>
2019-02-05 12:53:50 +0300

    Remove extra whitespace

--- types/react-swipe/index.d.ts
+++ types/react-swipe/index.d.ts
@@ -1,6 +1,6 @@
 // Type definitions for react-swipe 6.0
 // Project: https://github.com/voronianski/react-swipe
-// Definitions by: Deividas Bakanas <https://github.com/DeividasBakanas>, Ammar Alakkad <https://github.com/AAlakkad> 
+// Definitions by: Deividas Bakanas <https://github.com/DeividasBakanas>, Ammar Alakkad <https://github.com/AAlakkad>
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
 // TypeScript Version: 2.8
 
