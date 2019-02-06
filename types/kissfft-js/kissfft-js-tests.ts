14e3e2b16793f34e0e0525494a192a532ca5e540
racerhere <a0283001@lc02v23l3hv2r.dhcp.ti.com>
2019-02-06 11:28:01 -0600

    [kissfft-js] update tests

--- types/kissfft-js/kissfft-js-tests.ts
+++ types/kissfft-js/kissfft-js-tests.ts
@@ -1,12 +1,11 @@
-import {FFT} from "kissfft-js";
-import {FFTR} from "kissfft-js";
+import * as kissFFT from "kissfft-js";
 
-const fft = new FFT(0); // $ExpectType FFT
+const fft = new kissFFT.FFT(0); // $ExpectType FFT
 fft.forward([1, 2, 3]); // $ExpectType number[]
 fft.inverse([1, 2, 3]); // $ExpectType number[]
 fft.dispose(); // $ExpectType void
 
-const fftr = new FFTR(0); // $ExpectType FFTR
+const fftr = new kissFFT.FFTR(0); // $ExpectType FFTR
 fftr.forward([1, 2, 3]); // $ExpectType number[]
 fftr.inverse([1, 2, 3]); // $ExpectType number[]
 fftr.dispose(); // $ExpectType void
