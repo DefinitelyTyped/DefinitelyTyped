12e28d0e287d0a2fe41700639cb6e2f891c0ac0a
Vince Picone <Vincent.Patrick.Picone@ibm.com>
2019-02-07 09:49:53 -0600

    Add carbon motion

---
+++ types/carbon__motion/index.d.ts
@@ -0,0 +1,28 @@
+// Type definitions for carbon__motion-ts 0.0
+// Project: https://github.com/baz/foo (Does not have to be to GitHub, but prefer linking to a source code repository rather than to a project website.)
+// Definitions by: My Self <https://github.com/me>
+// Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
+
+export const easings: {
+    entrance: {
+        expressive: string;
+        productive: string;
+    };
+    exit: {
+        expressive: string;
+        productive: string;
+    };
+    standard: {
+        expressive: string;
+        productive: string;
+    };
+};
+
+export function motion(name: any, mode: any): any;
+
+export namespace motion {
+    const prototype: {
+    };
+
+}
+
