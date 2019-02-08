8d1a5817eb44ba8ff815e77e12772ca2f63c6ff6
Josh Sullivan <jaysche.sullivan@gmail.com>
2019-02-07 23:24:31 -0500

    Reverted eureka-js-client version format

--- types/eureka-js-client/index.d.ts
+++ types/eureka-js-client/index.d.ts
@@ -1,4 +1,4 @@
-// Type definitions for eureka-js-client 4.4.2
+// Type definitions for eureka-js-client 4.4
 // Project: https://github.com/jquatier/eureka-js-client
 // Definitions by: Ilko Hoffmann <https://github.com/Schnillz>
 //                 Karl O. <https://github.com/karl-run>
