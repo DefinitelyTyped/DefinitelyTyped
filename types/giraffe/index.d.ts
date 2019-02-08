a483095c3b1f446a23702efca2894fd6aaf2c001
Julian Gonggrijp <dev@juliangonggrijp.com>
2019-02-08 20:31:37 +0100

    Add my name to giraffe

--- types/giraffe/index.d.ts
+++ types/giraffe/index.d.ts
@@ -1,6 +1,7 @@
 // Type definitions for Giraffe
 // Project: https://github.com/barc/backbone.giraffe
 // Definitions by: Matt McCray <https://github.com/darthapo>
+//                 Julian Gonggrijp <https://github.com/jgonggrijp>
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
 // TypeScript Version: 2.3
 
