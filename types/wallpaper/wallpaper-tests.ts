605ded5e5daeec0ce02058e79b45ead5f641b55a
Federico Vitale <fedevitale99@gmail.com>
2019-02-09 10:28:15 +0100

    tests

--- types/wallpaper/wallpaper-tests.ts
+++ types/wallpaper/wallpaper-tests.ts
@@ -1,7 +1,7 @@
 import wallpaper = require('wallpaper');
 
 wallpaper.set('unicorn.jpg').then(() => {});
-wallpaper.set('unicorn.jpg', {scale: 'fill'}).then(() => {});
+wallpaper.set('unicorn.jpg', { scale: 'fill' }).then(() => {});
 
 wallpaper.get().then(imagePath => {
     const str: string = imagePath;
