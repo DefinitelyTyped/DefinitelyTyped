3b5442e54fb37ec127c5114edf91368a9f4f2819
Zhu Zijia <littlepiggy03@hotmail.com>
2019-02-10 23:44:18 +0800

    Fix ajv-merge-patch types

--- types/ajv-merge-patch/index.d.ts
+++ types/ajv-merge-patch/index.d.ts
@@ -2,6 +2,7 @@
 // Project: https://github.com/epoberezkin/ajv-merge-patch#readme
 // Definitions by: Zhu Zijia <https://github.com/littlepiggy03>
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
+// TypeScript Version: 2.2
 
 import * as Ajv from "ajv";
 
