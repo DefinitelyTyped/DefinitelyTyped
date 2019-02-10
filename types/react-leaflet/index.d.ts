3b11b16d95cfa12060c83020a056b707eb2043f3
Henry Wu <hywu925@gmail.com>
2019-02-10 09:40:55 -0800

    Revert to older version of TS for compatibility with react-leaflet-markercluster

--- types/react-leaflet/index.d.ts
+++ types/react-leaflet/index.d.ts
@@ -2,7 +2,7 @@
 // Project: https://github.com/PaulLeCam/react-leaflet
 // Definitions by: Dave Leaver <https://github.com/danzel>, David Schneider <https://github.com/davschne>, Yui T. <https://github.com/yuit>
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
-// TypeScript Version: 3.2
+// TypeScript Version: 2.8
 
 import * as Leaflet from 'leaflet';
 import * as React from 'react';
@@ -176,9 +176,9 @@ export interface DivOverlayTypes extends Leaflet.Evented {
 export class DivOverlay<P extends DivOverlayProps, E extends DivOverlayTypes> extends MapComponent<P, E> {
     createLeafletElement(_props: P): void;
     updateLeafletElement(_prevProps: P, _props: P): void;
-    onClose(): void;
-    onOpen(): void;
-    onRender(): void;
+    onClose: () => void;
+    onOpen: () => void;
+    onRender: () => void;
 }
 
 export interface PaneProps {
