1b6290fe2a3ae93d2b01527dd4e32fe20fd3827e
Zhu Zijia <littlepiggy03@hotmail.com>
2019-02-10 13:31:21 +0800

    Fix ajv-merge-patch types

--- types/ajv-merge-patch/index.d.ts
+++ types/ajv-merge-patch/index.d.ts
@@ -1,12 +1,11 @@
 // Type definitions for ajv-merge-patch 4.1
-// Project: https://github.com/epoberezkin/ajv-merge-patch
+// Project: https://github.com/epoberezkin/ajv-merge-patch#readme
 // Definitions by: Zhu Zijia <https://github.com/littlepiggy03>
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
 
-import { Ajv } from "ajv";
+import * as Ajv from "ajv";
 
 declare namespace ajvMergePatch {}
-
 declare function ajvMergePatch(ajv: Ajv): void;
 
 export = ajvMergePatch;
