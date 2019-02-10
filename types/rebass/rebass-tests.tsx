1ec6cdc9d273a95462d636027f63e448e9a766c0
Angus Fretwell <angusfretwell@me.com>
2019-02-10 18:11:12 +1100

    [rebass] extend styled-system interfaces, support html attributes, stricter as and css props

--- types/rebass/rebass-tests.tsx
+++ types/rebass/rebass-tests.tsx
@@ -1,13 +1,17 @@
-import * as React from 'react';
-import { Box, Flex, Text, Heading, Button, Link, Image, Card } from 'rebass';
+import * as React from "react";
+import { Box, Flex, Text, Heading, Button, Link, Image, Card } from "rebass";
+
+const CustomComponent: React.FunctionComponent = ({ children }) => {
+    return <div>{children}</div>;
+};
 
 () => (
-    <Box width={1} css={{ height: '100vh' }} py={[1, 2, 3]} ml="1em">
+    <Box width={1} css={{ height: "100vh" }} py={[1, 2, 3]} ml="1em">
         <Flex width={1} alignItems="center" justifyContent="center">
             <Heading fontSize={5} fontWeight="bold">
                 Hi, I'm a heading.
             </Heading>
-            <Text fontSize={3} lineHeight="1em" letterSpacing="1rem">
+            <Text as="p" fontSize={3} lineHeight="1em" letterSpacing="1rem">
                 Hi, I'm text.
             </Text>
             <Card
@@ -26,7 +30,10 @@ import { Box, Flex, Text, Heading, Button, Link, Image, Card } from 'rebass';
                 src="https://source.unsplash.com/random/1280x720"
                 borderRadius="1em"
             />
-            <Link href="https://rebassjs.org">Link</Link>
+            <Link href="https://rebassjs.org" title="Rebass" target="_blank">
+                Link
+            </Link>
+            <Box as={CustomComponent}>CustomComponent</Box>
             <Button bg="magenta" border="1em" borderRadius="1em">
                 Button
             </Button>
