cc7c708a5b30715c562af232cb5d4096e5e40aec
Antonio Pavlinovic <apavlinovic@neogov.net>
2019-02-11 11:14:09 +0100

    Export changes

--- types/elo-rank/index.d.ts
+++ types/elo-rank/index.d.ts
@@ -3,10 +3,12 @@
 // Definitions by: Antonio Pavlinovic <https://github.com/apavlinovic>
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
 
-export class EloRank {
+declare class EloRank {
 	constructor(kFactor?: number);
 	setKFactor(kFactor: number): void;
 	getKFactor(): number;
 	getExpected(a: number, b: number): number;
 	updateRating(expected: number, actual: number, current: number): number;
 }
+
+export = EloRank;
