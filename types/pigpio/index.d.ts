aa5e75ea959dd4d5594347639ff02d786c9face7
Joosung Park <park012241@park012241.me>
2019-02-11 22:45:38 +0900

    Add Definitions by
    add "Definitions by"

--- types/pigpio/index.d.ts
+++ types/pigpio/index.d.ts
@@ -1,6 +1,8 @@
 // Type definitions for pigpio 1.2
 // Project: https://github.com/fivdi/pigpio
-// Definitions by: ManerFan <https://github.com/manerfan>, erikma <https://github.com/erikma>
+// Definitions by: ManerFan <https://github.com/manerfan>
+//                 erikma <https://github.com/erikma>
+//                 park012241 <https://github.com/park012241>
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
 
 /// <reference types="node" />
