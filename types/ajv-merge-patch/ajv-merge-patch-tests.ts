db06b28505ca459604749066d74be4d1c3c3f50c
Zhu Zijia <littlepiggy03@hotmail.com>
2019-02-12 09:19:17 +0800

    Fix ajv-merge-patch types

--- types/ajv-merge-patch/ajv-merge-patch-tests.ts
+++ types/ajv-merge-patch/ajv-merge-patch-tests.ts
@@ -1,5 +1,6 @@
 import * as Ajv from "ajv";
-import * as ajvMergePatch from "ajv-merge-patch";
+
+import ajvMergePatch = require("ajv-merge-patch");
 
 const ajv = new Ajv();
 
--- types/ajv-merge-patch/index.d.ts
+++ types/ajv-merge-patch/index.d.ts
@@ -6,7 +6,6 @@
 
 import { Ajv } from "ajv";
 
-declare namespace ajvMergePatch {}
 declare function ajvMergePatch(ajv: Ajv): void;
 
 export = ajvMergePatch;
