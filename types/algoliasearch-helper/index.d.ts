e66b8e62483d19142d860a13db2581d3beca39b0
Gordon <gburgett@watermark.org>
2019-02-12 07:51:47 -0600

    Add Haroen and Samuel to Authors line

--- types/algoliasearch-helper/index.d.ts
+++ types/algoliasearch-helper/index.d.ts
@@ -1,6 +1,8 @@
 // Type definitions for algoliasearch-helper 2.26
 // Project: https://community.algolia.com/algoliasearch-helper-js/
 // Definitions by: Gordon Burgett <https://github.com/gburgett>
+//                 Haroen Viaene <https://github.com/haroenv>
+//                 Samuel Vaillant <https://github.com/samouss>
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
 // TypeScript Version: 2.8
 
