7835b1c0f49347c5fd6ec4763b711021f8e599e4
Nathan Shively-Sanders <293473+sandersn@users.noreply.github.com>
2019-02-12 10:40:37 -0800

    Updates through flexmonster

--- types/fancybox/index.d.ts
+++ types/fancybox/index.d.ts
@@ -1,5 +1,5 @@
 // Type definitions for fancyBox 2.1
-// Project: https://github.com/fancyapps/fancyBox, https://github.com/olsonpm/fancybox
+// Project: https://github.com/fancyapps/fancyBox
 // Definitions by: Boris Yankov <https://github.com/borisyankov>
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
 // TypeScript Version: 2.3
