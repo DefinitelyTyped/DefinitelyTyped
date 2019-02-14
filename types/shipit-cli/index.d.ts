20c09018e02133d7cd8697285e7b64da4d0a38f7
Nathan Shively-Sanders <293473+sandersn@users.noreply.github.com>
2019-02-13 16:29:07 -0800

    Fix build

--- types/shipit-cli/index.d.ts
+++ types/shipit-cli/index.d.ts
@@ -1,4 +1,4 @@
-// Type definitions for non-npm package shipit-cli 1.5
+// Type definitions for shipit-cli 1.5
 // Project: https://github.com/shipitjs/shipit
 // Definitions by: Cyril Schumacher <https://github.com/cyrilschumacher>
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
