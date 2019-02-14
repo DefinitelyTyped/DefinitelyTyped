1ff50d8f7f74a1588da7cfa3b36dfdaf0204e5ea
James G. Best <jim-at-jibba@users.noreply.github.com>
2019-02-14 11:48:24 +0000

    Fixed quotes

--- types/hedron/hedron-tests.tsx
+++ types/hedron/hedron-tests.tsx
@@ -1,15 +1,15 @@
-import * as React from "react";
-import * as Grid from "hedron";
+import * as React from 'react';
+import * as Grid from 'hedron';
 
 <Grid.Provider
     padding='20px'
-    breakpoints={{ sm: "-500", md: "501-750", lg: "+750" }}>
+    breakpoints={{ sm: '-500', md: '501-750', lg: '+750' }}>
     <Grid.Bounds direction='vertical'>
         <Grid.Box sm={{ hidden: true }}>
             This header hides on small screens
         </Grid.Box>
         <Grid.Box>Content</Grid.Box>
-        <Grid.Box lg={{ padding: "50px" }}>
+        <Grid.Box lg={{ padding: '50px' }}>
             This footer gains extra padding on large screens
         </Grid.Box>
     </Grid.Bounds>
