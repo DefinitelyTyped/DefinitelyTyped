088506cb1f4598467fd03167c7d187719edd7210
Julian Hundeloh <jaulz@users.noreply.github.com>
2019-02-15 07:32:56 +0100

    fix: add spaces and contributor

--- types/tinycon/index.d.ts
+++ types/tinycon/index.d.ts
@@ -1,9 +1,10 @@
 // Type definitions for tinycon 0.6
 // Project: https://github.com/tommoor/tinycon
 // Definitions by: Daniel Waxweiler <https://github.com/dwaxweiler>
+//                 Julian Hundeloh <https://github.com/jaulz>
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
 
-export function setBubble(count: number|string|null): void;
+export function setBubble(count: number | string | null): void;
 
 export function setOptions(options: TinyconOptions): void;
 
