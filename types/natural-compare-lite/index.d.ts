b2373af1a29c1ffa9b7c5b902aced1360159f0bb
doniyor2109 <doniyor2109@gmail.com>
2019-02-15 11:56:37 +0500

    Fix headers

--- types/natural-compare-lite/index.d.ts
+++ types/natural-compare-lite/index.d.ts
@@ -1,4 +1,4 @@
-// Type definitions for natural-compare-lite 1.4.0
+// Type definitions for natural-compare-lite 1.4
 // Project: https://github.com/litejs/natural-compare-lite
 // Definitions by: Doniyor Aliyev <https://github.com/doniyor2109>
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
