2e60bca3febc046b6a477d1d4081832baa346a83
doniyor2109 <doniyor2109@gmail.com>
2019-02-15 12:03:48 +0500

    Remove headers other than index.d.ts

--- types/natural-compare-lite/natural-compare-lite-tests.ts
+++ types/natural-compare-lite/natural-compare-lite-tests.ts
@@ -1,8 +1,3 @@
-// Type definitions for natural-compare-lite 1.4
-// Project: https://github.com/litejs/natural-compare-lite
-// Definitions by: Doniyor Aliyev <https://github.com/doniyor2109>
-// Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
-
 import compare = require("natural-compare-lite");
 
 ['a', 's', 'd'].sort(compare);
