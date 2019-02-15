9dbc92e9e905cd9c5cc8baad450583a991565d30
Martin Jurča <martin.jurca@firma.seznam.cz>
2019-02-15 11:04:06 +0100

    generated the meta-files using dts-gen --dt

--- types/youtube-player/index.d.ts
+++ types/youtube-player/index.d.ts
@@ -1,8 +1,7 @@
 // Type definitions for youtube-player 5.5
-// Project: https://github.com/gajus/youtube-player
+// Project: https://github.com/gajus/youtube-player#readme
 // Definitions by: Martin Jurča <https://github.com/jurca>
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
-// TypeScript Version: 3.0
 
 import PlayerFactory from './dist/index';
 
