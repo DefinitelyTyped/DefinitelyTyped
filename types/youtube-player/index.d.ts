3b534be4d099d334b905d7be25dc27b4f7bab964
Martin Jurča <martin.jurca@firma.seznam.cz>
2019-02-15 11:05:29 +0100

    fixed configuration

--- types/youtube-player/index.d.ts
+++ types/youtube-player/index.d.ts
@@ -2,6 +2,7 @@
 // Project: https://github.com/gajus/youtube-player#readme
 // Definitions by: Martin Jurča <https://github.com/jurca>
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
+// TypeScript Version: 2.2
 
 import PlayerFactory from './dist/index';
 
