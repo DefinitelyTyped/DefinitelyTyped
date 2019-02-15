c0b82cd8f294de928d3a180e52c518909dd33f87
supaiku <supaiku@protonmail.com>
2019-02-15 04:03:01 +0100

    chore: add contributor

--- types/animejs/index.d.ts
+++ types/animejs/index.d.ts
@@ -1,6 +1,7 @@
 // Type definitions for animejs 2.0
 // Project: http://animejs.com
-// Definitions by: Andrew Babin <https://github.com/A-Babin>
+// Definitions by: Andrew Babin     <https://github.com/A-Babin>
+//                 supaiku0         <https://github.com/supaiku0>
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
 // TypeScript Version: 2.4
 
