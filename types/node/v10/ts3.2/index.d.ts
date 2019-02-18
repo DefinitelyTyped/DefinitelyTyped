350497b46b15f448fdd9f662ab4bb40be25d4546
Jonathan Stewmon <jstewmon@gmail.com>
2019-02-18 16:09:53 -0600

    add process.hrtime.bigint() to node v10

---
+++ types/node/v10/ts3.2/index.d.ts
@@ -0,0 +1,19 @@
+// NOTE: These definitions support NodeJS and TypeScript 3.2.
+
+// NOTE: TypeScript version-specific augmentations can be found in the following paths:
+//          - ~/base.d.ts         - Shared definitions common to all TypeScript versions
+//          - ~/index.d.ts        - Definitions specific to TypeScript 2.1
+//          - ~/ts3.2/index.d.ts  - Definitions specific to TypeScript 3.2
+
+// Reference required types from the default lib:
+/// <reference lib="es2018" />
+/// <reference lib="esnext.asyncIterable" />
+/// <reference lib="esnext.intl" />
+/// <reference lib="esnext.bigint" />
+
+// Base definitions for all NodeJS modules that are not specific to any version of TypeScript:
+// tslint:disable-next-line:no-bad-reference
+/// <reference path="../ts3.1/index.d.ts" />
+
+// TypeScript 3.2-specific augmentations:
+/// <reference path="globals.d.ts" />
