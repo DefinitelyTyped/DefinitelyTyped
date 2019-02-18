5bf116d71b09273334b70f5e96a272ad5fa4c104
Olga Isakova <penumbral@yandex.ru>
2019-02-19 02:19:33 +0500

    Tests, TS version

--- types/mongo-sanitize/index.d.ts
+++ types/mongo-sanitize/index.d.ts
@@ -2,7 +2,7 @@
 // Project: https://github.com/vkarpov15/mongo-sanitize
 // Definitions by: Cedric Cazin <https://github.com/CedricCazin>, Olga Isakova <https://github.com/penumbra1>
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
-// TypeScript Version: 2.3
+// TypeScript Version: 3.3
 
 declare function sanitize<T>(v: T): T;
 
