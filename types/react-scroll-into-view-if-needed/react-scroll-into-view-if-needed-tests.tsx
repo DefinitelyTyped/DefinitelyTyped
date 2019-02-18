c77d6dad5e37018c8ec14f99b3e2188fd1de9990
Jonathan Ly <jonathanlyhv@gmail.com>
2019-02-18 13:25:13 +1100

    [react-scroll-into-view-if-needed] add types

---
+++ types/react-scroll-into-view-if-needed/react-scroll-into-view-if-needed-tests.tsx
@@ -0,0 +1,52 @@
+import * as React from 'react';
+import ReactScrollIntoViewIfNeeded from 'react-scroll-into-view-if-needed';
+
+const validOptions = {
+    block: 'start',
+    scrollMode: 'if-needed',
+    skipOverflowHiddenElements: true
+};
+
+const invalidOptions = {
+    invalidOption: 'foobar'
+};
+
+() => (
+    <ReactScrollIntoViewIfNeeded
+        active={true}
+        options={validOptions}
+        elementType="h1"
+    >
+        Children
+    </ReactScrollIntoViewIfNeeded>
+);
+
+() => (
+    <ReactScrollIntoViewIfNeeded
+        active={true}
+        options={invalidOptions}
+        elementType="h1"
+    >
+        Children
+    </ReactScrollIntoViewIfNeeded>
+);
+
+() => (
+    <ReactScrollIntoViewIfNeeded
+        active={5} // $ExpectError
+        options={validOptions}
+        elementType="h1"
+    >
+        Children
+    </ReactScrollIntoViewIfNeeded>
+);
+
+() => (
+    <ReactScrollIntoViewIfNeeded
+        active={true}
+        options={validOptions}
+        elementType="h8" // $ExpectError
+    >
+        Children
+    </ReactScrollIntoViewIfNeeded>
+);
