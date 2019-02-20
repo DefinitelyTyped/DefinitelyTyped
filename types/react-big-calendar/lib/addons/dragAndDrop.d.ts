84d49f513578f029f836fbc3f7019d67ffe0e95b
Tomáš Hübelbauer <tomas@hubelbauer.net>
2019-02-20 13:18:54 +0100

    Address more build failures

--- types/react-big-calendar/lib/addons/dragAndDrop.d.ts
+++ types/react-big-calendar/lib/addons/dragAndDrop.d.ts
@@ -1,11 +1,11 @@
 import BigCalendar, { BigCalendarProps, Event, stringOrDate } from '../../index';
-import React from 'react';
+import React = require('react');
 
 interface withDragAndDropProps<TEvent> {
   onEventDrop?: (args: { event: TEvent, start: stringOrDate, end: stringOrDate, allDay: boolean }) => void;
   onEventResize?: (args: { event: TEvent, start: stringOrDate, end: stringOrDate, allDay: boolean }) => void;
   resizable?: boolean;
-};
+}
 
 declare class DragAndDropCalendar<TEvent extends Event = Event, TResource extends object = object>
   extends React.Component<BigCalendarProps<TEvent, TResource> & withDragAndDropProps<TEvent>> {}
