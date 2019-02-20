e50e51a73ab644ca2be9783807fd199caa373521
Felix Becker <felix.b@outlook.com>
2019-02-20 10:35:23 +0100

    Fix TSLint

--- types/sinon/ts3.1/index.d.ts
+++ types/sinon/ts3.1/index.d.ts
@@ -1180,7 +1180,7 @@ declare namespace Sinon {
          * Passes if provided spies were called in the specified order.
          * @param spies
          */
-        callOrder(...spies: SinonSpy<any>[]): void;
+        callOrder(...spies: Array<SinonSpy<any>>): void;
         /**
          * Passes if spy was ever called with obj as its this value.
          * It’s possible to assert on a dedicated spy call: sinon.assert.calledOn(spy.firstCall, arg1, arg2, ...);.
