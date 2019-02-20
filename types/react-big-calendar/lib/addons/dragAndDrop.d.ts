4d2fc7e27b21e9d74b43d8fd5bcff772079dbb36
Tomáš Hübelbauer <tomas@hubelbauer.net>
2019-02-20 12:07:17 +0100

    Fix indentation

--- types/react-big-calendar/lib/addons/dragAndDrop.d.ts
+++ types/react-big-calendar/lib/addons/dragAndDrop.d.ts
@@ -1,14 +1,14 @@
-  import BigCalendar, { BigCalendarProps, Event } from 'react-big-calendar';
+import BigCalendar, { BigCalendarProps, Event } from 'react-big-calendar';
 
-  type withDragAndDropProps<TEvent> = {
-      onEventDrop?: (args: { event: TEvent, start: stringOrDate, end: stringOrDate, allDay: boolean }) => void;
-      onEventResize?: (args: { event: TEvent, start: stringOrDate, end: stringOrDate, allDay: boolean }) => void;
-      resizable?: boolean;
-  };
+type withDragAndDropProps<TEvent> = {
+  onEventDrop?: (args: { event: TEvent, start: stringOrDate, end: stringOrDate, allDay: boolean }) => void;
+  onEventResize?: (args: { event: TEvent, start: stringOrDate, end: stringOrDate, allDay: boolean }) => void;
+  resizable?: boolean;
+};
 
-  declare class DragAndDropCalendar<TEvent extends Event = Event, TResource extends object = object>
-      extends React.Component<BigCalendarProps<TEvent, TResource> & withDragAndDropProps<TEvent>>, {}
+declare class DragAndDropCalendar<TEvent extends Event = Event, TResource extends object = object>
+  extends React.Component<BigCalendarProps<TEvent, TResource> & withDragAndDropProps<TEvent>>, {}
 
-  function withDragAndDrop(calendar: typeof BigCalendar): typeof DragAndDropCalendar;
-  export = withDragAndDrop;
-  
+function withDragAndDrop(calendar: typeof BigCalendar): typeof DragAndDropCalendar;
+export = withDragAndDrop;
+
