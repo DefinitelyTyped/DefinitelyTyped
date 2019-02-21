d93b9f3b627f1fe4216d0ad8c717510f8630a68d
Ohad Maishar <ohad@smore.com>
2019-02-21 10:45:15 +0200

    Adding Definitions by

--- types/indefinite/index.d.ts
+++ types/indefinite/index.d.ts
@@ -1,6 +1,6 @@
 // Type definitions for indefinite 2.2
 // Project: https://github.com/tandrewnichols/indefinite
-// Definitions by: My Self <https://github.com/me>
+// Definitions by: omaishar <https://github.com/omaishr>
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
 
 export interface Options {
