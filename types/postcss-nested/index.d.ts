a62d18d64f1481092f69bdbe56e01ce762bc6f92
Maxim Vorontsov <maxie@yandex-team.ru>
2019-02-21 11:30:34 +0500

    Specified lower TypeScript version

--- types/postcss-nested/index.d.ts
+++ types/postcss-nested/index.d.ts
@@ -2,7 +2,7 @@
 // Project: https://github.com/postcss/postcss-nested#readme
 // Definitions by: Maxim Vorontsov <https://github.com/VorontsovMaxim>
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
-// TypeScript Version: 2.8
+// TypeScript Version: 2.2
 
 import { Plugin } from 'postcss';
 
