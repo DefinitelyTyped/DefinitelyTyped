6f84f2c1c174d749e763f7d5056dbadfe847175b
Adam Zerella <adamzerella@users.noreply.github.com>
2019-02-21 11:17:45 +1100

    Removed specific typescript version

--- types/pretty/index.d.ts
+++ types/pretty/index.d.ts
@@ -2,7 +2,6 @@
 // Project: https://github.com/jonschlinkert/pretty
 // Definitions by:  Adam Zerella <https://github.com/adamzerella>
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
-// TypeScript Version: 3.3
 
 interface PrettyOptions {
     ocd: boolean;
