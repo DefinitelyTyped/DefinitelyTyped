84e348cac8f720643fb3550a6ac36730a7489c85
Haseeb Majid <hmajid2301@gmail.com>
2019-02-21 21:51:02 +0000

    Added react-native-snackbar-component
    Added typedefinitions for react-native-snackbar-component,
    https://github.com/SiDevesh/React-Native-SnackBar-Component

---
+++ types/react-native-snackbar-component/index.d.ts
@@ -0,0 +1,24 @@
+// Type definitions for react-native-snackbar-components 1.1.0
+// Project: https://github.com/SiDevesh/React-Native-SnackBar-Component
+// Definitions by: Haseeb Majid <https://github.com/hmajid2301>
+// Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
+
+import * as React from 'react';
+import { TextStyle } from 'react-native';
+
+export interface SnackbarComponentProps {
+    accentColor?: string;
+    actionText?: string;
+    messageColor?: string;
+    backgroundColor?: string;
+    distanceCallback?: () => void;
+    left?: number;
+    right?: number;
+    bottom?: number;
+    position?: string;
+    textMessage?: string;
+    autoHidingTime?: number;
+    visible?: boolean;
+}
+
+export default class SnackbarComponent extends React.Component<SnackbarComponentProps> {}
