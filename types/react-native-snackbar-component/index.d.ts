86d40e8561632dad20c89becc030f1472d81a61c
Haseeb Majid <hmajid2301@gmail.com>
2019-02-22 20:38:34 +0000

    More linting fixes

--- types/react-native-snackbar-component/index.d.ts
+++ types/react-native-snackbar-component/index.d.ts
@@ -1,4 +1,4 @@
-// Type definitions for react-native-snackbar-components 1.1.0
+// Type definitions for react-native-snackbar-components 1.1
 // Project: https://github.com/sidevesh/react-native-snackbar
 // Definitions by: Haseeb Majid <https://github.com/hmajid2301>
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
