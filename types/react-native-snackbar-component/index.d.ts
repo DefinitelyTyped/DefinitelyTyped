af48c993b9c4281d684d9d727f40bf4612ff8fba
Haseeb Majid <hmajid2301@gmail.com>
2019-02-22 20:31:15 +0000

    added typescript version

--- types/react-native-snackbar-component/index.d.ts
+++ types/react-native-snackbar-component/index.d.ts
@@ -2,6 +2,7 @@
 // Project: https://github.com/sidevesh/react-native-snackbar
 // Definitions by: Haseeb Majid <https://github.com/hmajid2301>
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
+// TypeScript Version: 2.8
 
 import * as React from 'react';
 import { TextStyle } from 'react-native';
