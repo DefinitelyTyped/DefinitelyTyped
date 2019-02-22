454beec14042e174de5179af0c54bfe28cc094f5
Nick Roberts <djparadyme@gmail.com>
2019-02-22 10:31:41 -0500

    Update ioredis cluster types

--- types/ioredis/ioredis-tests.ts
+++ types/ioredis/ioredis-tests.ts
@@ -1,4 +1,4 @@
-import Redis = require("ioredis");
+import Redis = require('ioredis');
 
 const redis = new Redis();
 
@@ -38,23 +38,25 @@ redis.set('key', '100', ['EX', 10, 'NX'], (err, data) => {});
 redis.setBuffer('key', '100', 'NX', 'EX', 10, (err, data) => {});
 
 redis.exists('foo').then(result => result * 1);
-redis.exists('foo', ((err, data) => data * 1));
+redis.exists('foo', (err, data) => data * 1);
 
 // Should support usage of Buffer
 redis.set(Buffer.from('key'), '100');
 redis.setBuffer(Buffer.from('key'), '100', 'NX', 'EX', 10);
 
-new Redis();       // Connect to 127.0.0.1:6379
-new Redis(6380);   // 127.0.0.1:6380
-new Redis(6379, '192.168.1.1');       // 192.168.1.1:6379
+new Redis(); // Connect to 127.0.0.1:6379
+new Redis(6380); // 127.0.0.1:6380
+new Redis(6379, '192.168.1.1'); // 192.168.1.1:6379
 new Redis('/tmp/redis.sock');
 new Redis({
-    port: 6379,          // Redis port
-    host: '127.0.0.1',   // Redis host
-    family: 4,           // 4 (IPv4) or 6 (IPv6)
+    port: 6379, // Redis port
+    host: '127.0.0.1', // Redis host
+    family: 4, // 4 (IPv4) or 6 (IPv6)
     password: 'auth',
     db: 0,
-    retryStrategy() { return false; },
+    retryStrategy() {
+        return false;
+    },
     maxRetriesPerRequest: 20,
     showFriendlyErrorStack: true,
     tls: {
@@ -97,25 +99,35 @@ pipeline.exec((err, results) => {
 });
 
 // You can even chain the commands:
-redis.pipeline().set('foo', 'bar').del('cc').exec((err, results) => {
-});
+redis
+    .pipeline()
+    .set('foo', 'bar')
+    .del('cc')
+    .exec((err, results) => {});
 
 // `exec` also returns a Promise:
-const promise = redis.pipeline().set('foo', 'bar').get('foo').exec();
-promise.then((result) => {
+const promise = redis
+    .pipeline()
+    .set('foo', 'bar')
+    .get('foo')
+    .exec();
+promise.then(result => {
     // result === [[null, 'OK'], [null, 'bar']]
 });
 
-redis.pipeline().set('foo', 'bar').get('foo', (err, result) => {
-    // result === 'bar'
-}).exec((err, result) => {
-    // result[1][1] === 'bar'
-});
+redis
+    .pipeline()
+    .set('foo', 'bar')
+    .get('foo', (err, result) => {
+        // result === 'bar'
+    })
+    .exec((err, result) => {
+        // result[1][1] === 'bar'
+    });
 
-redis.pipeline([
-    ['set', 'foo', 'bar'],
-    ['get', 'foo']
-]).exec(() => { /* ... */ });
+redis.pipeline([['set', 'foo', 'bar'], ['get', 'foo']]).exec(() => {
+    /* ... */
+});
 
 Redis.Command.setArgumentTransformer('set', args => {
     return args;
@@ -126,28 +138,33 @@ Redis.Command.setReplyTransformer('get', (result: any) => {
 });
 
 redis.scan(0, 'match', '*foo*', 'count', 20).then(([nextCursor, keys]) => {
-  // nextCursor is always a string
-  if (nextCursor === '0') {
-    // keys is always an array of strings and it might be empty
-    return keys.map(key => key.trim());
-  }
+    // nextCursor is always a string
+    if (nextCursor === '0') {
+        // keys is always an array of strings and it might be empty
+        return keys.map(key => key.trim());
+    }
 });
 
-redis.pipeline().scan(0, 'count', 20, 'match', '*foo*').exec((err, result) => {
-  // result = [[null, [nextCursor, keys]]]
-});
+redis
+    .pipeline()
+    .scan(0, 'count', 20, 'match', '*foo*')
+    .exec((err, result) => {
+        // result = [[null, [nextCursor, keys]]]
+    });
 
 // multi
-redis.multi().set('foo', 'bar').set('foo', 'baz').get('foo', (err, result) => {
-    // result === 'QUEUED'
-}).exec((err, results) => {
-    // results = [[null, 'OK'], [null, 'OK'], [null, 'baz']]
-});
+redis
+    .multi()
+    .set('foo', 'bar')
+    .set('foo', 'baz')
+    .get('foo', (err, result) => {
+        // result === 'QUEUED'
+    })
+    .exec((err, results) => {
+        // results = [[null, 'OK'], [null, 'OK'], [null, 'baz']]
+    });
 
-redis.multi([
-    ['set', 'foo', 'bar'],
-    ['get', 'foo']
-]).exec((err, results) => {
+redis.multi([['set', 'foo', 'bar'], ['get', 'foo']]).exec((err, results) => {
     // results = [[null, 'OK'], [null, 'bar']]
 });
 
@@ -157,26 +174,28 @@ redis.mget(...keys);
 redis.mset(...['foo', 'bar']);
 redis.mset({ foo: 'bar' });
 
+new Redis.Cluster(['localhost']);
+
+new Redis.Cluster([6379]);
+
 new Redis.Cluster([
-    'localhost'
+    {
+        host: 'localhost'
+    }
 ]);
 
 new Redis.Cluster([
-    6379
+    {
+        port: 6379
+    }
 ]);
 
-new Redis.Cluster([{
-    host: 'localhost'
-}]);
-
-new Redis.Cluster([{
-    port: 6379
-}]);
-
-new Redis.Cluster([{
-    host: 'localhost',
-    port: 6379
-}]);
+new Redis.Cluster([
+    {
+        host: 'localhost',
+        port: 6379
+    }
+]);
 
 redis.xack('streamName', 'groupName', 'id');
 redis.xadd('streamName', '*', 'field', 'name');
@@ -206,4 +225,22 @@ new Redis.Cluster([], {
 
 new Redis.Cluster([], {
     clusterRetryStrategy: (times: number, reason?: Error) => 1
+});
+
+// Cluster types
+const clusterOptions: Redis.ClusterOptions = {};
+const cluster = new Redis.Cluster(
+    [
+        {
+            host: 'localhost',
+            port: 6379
+        }
+    ],
+    clusterOptions
+);
+cluster.nodes.map(node => {
+    node.pipeline()
+        .flushdb()
+        .exec()
+        .then(result => console.log(result));
 });
