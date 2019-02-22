e10db0f11b9a784426fb54dcff67f0119cc8ba79
Sean Kelly <skelly@aaski.com>
2019-02-22 12:02:36 -0500

    lint

--- types/dragscroll/index.d.ts
+++ types/dragscroll/index.d.ts
@@ -1,4 +1,4 @@
-// Type definitions for dragscroll v 0.0.8
+// Type definitions for dragscroll v 0.0
 // Project: https://github.com/asvd/dragscroll#readme
 // Definitions by: Sean Kelly <https://github.com/spkellydev/>
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
