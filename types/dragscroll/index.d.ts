c31d394c731e00895e7f9b4486f316237b2e1763
Sean Kelly <skelly@aaski.com>
2019-02-22 13:26:20 -0500

    single module

--- types/dragscroll/index.d.ts
+++ types/dragscroll/index.d.ts
@@ -2,6 +2,4 @@
 // Project: https://github.com/asvd/dragscroll#readme
 // Definitions by: Sean Kelly <https://github.com/spkellydev>
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
-declare module "dragscroll" {
-    function reset(i?: number, el?: any[]): void;
-}
+export function reset(i ?: number, el ?: any): void;
