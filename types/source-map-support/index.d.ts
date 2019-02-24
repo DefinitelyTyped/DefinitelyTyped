257469f5ffeb7a6bb24dcd26d27b03fcfe8acc3e
Alcedo Nathaniel De Guzman Jr <natealcedo@gmail.com>
2019-02-24 09:42:37 +0800

    Add myself to definitions by

--- types/source-map-support/index.d.ts
+++ types/source-map-support/index.d.ts
@@ -1,6 +1,6 @@
 // Type definitions for source-map-support 0.5
 // Project: https://github.com/evanw/node-source-map-support
-// Definitions by: Bart van der Schoor <https://github.com/Bartvds>, Jason Cheatham <https://github.com/jason0x43>
+// Definitions by: Bart van der Schoor <https://github.com/Bartvds>, Jason Cheatham <https://github.com/jason0x43>, Alcedo Nathaniel De Guzman Jr <https://github.com/natealcedo>
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
 
 /// <reference types="node" />
