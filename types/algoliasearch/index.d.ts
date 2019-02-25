5cf2cac315357c424d79eb89c0fcb1a601cfcb57
Antoine Rousseau <antoine@rousseau.im>
2019-02-25 15:10:07 +0100

    set languages list

--- types/algoliasearch/index.d.ts
+++ types/algoliasearch/index.d.ts
@@ -1792,7 +1792,7 @@ declare namespace algoliasearch {
      * such as ignorePlurals, removeStopWords, and CJK word-detection.
      * https://www.algolia.com/doc/api-reference/api-parameters/queryLanguages/
      */
-    queryLanguages?: string[];
+    queryLanguages?: Array<'af' | 'ar' | 'az' | 'bg' | 'bn' | 'ca' | 'cs' | 'cy' | 'da' | 'de' | 'el' | 'en' | 'eo' | 'es' | 'et' | 'eu' | 'fa' | 'fi' | 'fo' | 'fr' | 'ga' | 'gl' | 'he' | 'hi' | 'hu' | 'hy' | 'id' | 'is' | 'it' | 'ja' | 'ka' | 'kk' | 'ko' | 'ku' | 'ky' | 'lt' | 'lv' | 'mi' | 'mn' | 'mr' | 'ms' | 'mt' | 'nb' | 'nl' | 'no' | 'ns' | 'pl' | 'ps' | 'pt' | 'pt-br' | 'qu' | 'ro' | 'ru' | 'sk' | 'sq' | 'sv' | 'sw' | 'ta' | 'te' | 'th' | 'tl' | 'tn' | 'tr' | 'tt' | 'uk' | 'ur' | 'uz' | 'zh'>;
   }
 
   interface Response<T=any> {
