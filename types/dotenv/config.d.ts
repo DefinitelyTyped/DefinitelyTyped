61c9abcdd85d7c5158c4e053ba095475246cc9cf
Jeremie Rodriguez <contact@jeremierodriguez.com>
2019-02-25 15:08:29 +0100

    fix(dotenv): Add config.d.ts that was missing

---
+++ types/dotenv/config.d.ts
@@ -0,0 +1,9 @@
+// Type definitions for dotenv 6.1
+// Project: https://github.com/motdotla/dotenv
+// Definitions by: Jussi Kinnula <https://github.com/jussikinnula>
+//                 Borek Bernard <https://github.com/borekb>
+//                 Eric Naeseth <https://github.com/enaeseth>
+//                 Max Beatty <https://github.com/maxbeatty>
+// Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
+
+export interface Dotenv {}
