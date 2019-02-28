af4fcef3bf8b374638827f1a4ae5ce6e970150cd
Federico Della Rovere <federico.dellarovere@apptecsrl.com>
2019-02-28 17:50:59 +0100

    Update twitch ext definitions with configuration feature

--- types/twitch-ext/index.d.ts
+++ types/twitch-ext/index.d.ts
@@ -30,6 +30,11 @@ interface TwitchExt {
 	actions: TwitchExtActions;
 
 	/**
+	 * @see https://dev.twitch.tv/docs/extensions/reference/#helper-configuration
+	 */
+	configuration: TwitchExtConfiguration;
+
+	/**
 	 * @see https://dev.twitch.tv/docs/extensions/reference/#twitch-extension-feature-flags
 	 */
 	features: TwitchExtFeatures;
@@ -169,6 +174,46 @@ interface TwitchExtActions {
 	 */
 	requestIdShare(): void;
 }
+
+/**
+ * @see TwitchExt.configuration
+ */
+interface TwitchExtConfiguration {
+	/**
+	 * This property returns the record for the broadcaster segment if one is found; otherwise, undefined.
+	 */
+	broadcaster: { version: string; content: string } | undefined;
+
+	/**
+	 * This property returns the record for the developer segment if one is found; otherwise, undefined.
+	*/
+	developer: { version: string; content: string } | undefined;
+
+	/**
+	 * This property returns the record for the global segment if one is found; otherwise, undefined.
+	 */
+	global: { version: string; content: string } | undefined;
+
+	/**
+	 * This function registers a callback that is called whenever an extension configuration is received.
+	 * The callback function takes no input and returns nothing. After this is called for the first time,
+	 * the records for the global, developer and broadcaster segments will be set if the data is available.
+	 * @param callback The callback that is fired.
+	*/
+	onChanged(
+		callback: () => void
+	): void
+
+
+	/**
+	 * This function can be called by the front end to set an extension configuration.
+	 * @param segment The string-encoded configuration.
+	 * @param version The configuration segment to set.
+	 * @param content The version of configuration with which the segment is stored.
+	 */
+	set(segment: string, version: string, content: string): void;
+}
+
 
 interface TwitchExtFeatureFlags {
 	/**
