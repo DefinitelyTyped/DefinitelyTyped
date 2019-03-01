18b5c68588aa8b20e1f5eeedaaa8a2fb2278b9b6
Mike North <michael.l.north@gmail.com>
2019-03-01 09:17:38 -0800

    remove trailing whitespace from @ember/runloop header

--- types/ember__runloop/index.d.ts
+++ types/ember__runloop/index.d.ts
@@ -1,7 +1,7 @@
 // Type definitions for non-npm package @ember/runloop 3.0
 // Project: https://emberjs.com/api/ember/3.4/modules/@ember%2Frunloop
 // Definitions by: Mike North <https://github.com/mike-north>
-//                 Steve Calvert <https://github.com/scalvert> 
+//                 Steve Calvert <https://github.com/scalvert>
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
 // TypeScript Version: 2.8
 
