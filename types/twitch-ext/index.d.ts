3796d81ce2f6df3e1dcc839245881086c8d10c1a
Federico Della Rovere <federico.dellarovere@apptecsrl.com>
2019-03-01 10:33:24 +0100

    increase twitch-ext version

--- types/twitch-ext/index.d.ts
+++ types/twitch-ext/index.d.ts
@@ -1,4 +1,4 @@
-// Type definitions for non-npm package twitch-ext 1.15
+// Type definitions for non-npm package twitch-ext 1.16
 // Project: https://dev.twitch.tv/docs/extensions/reference/#javascript-helper
 // Definitions by: Benedict Etzel <https://github.com/beheh>
 //                 Federico Della Rovere <https://github.com/FedeDR>
