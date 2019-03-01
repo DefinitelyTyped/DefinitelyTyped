43efd77e21a898c11fccab24a214fd2efc94c1ae
Adam A. Zerella <adamzerella@users.noreply.github.com>
2019-03-01 20:22:16 +1100

    Added type defs for repeat-element

---
+++ types/repeat-element/index.d.ts
@@ -0,0 +1,9 @@
+// Type definitions for repeat-element 1.1
+// Project: https://github.com/jonschlinkert/repeat-element
+// Definitions by:  Adam Zerella <https://github.com/adamzerella>
+// Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
+
+/**
+ * Create an array by repeating the given value n times.
+ */
+export default function(element: any, num: number): any[];
---
+++ types/repeat-element/repeat-element-tests.ts
@@ -0,0 +1,9 @@
+import Repeat from "repeat-element";
+
+Repeat(5, 5);
+
+Repeat({some: 'object'}, 5);
+
+Repeat('a', 5);
+
+Repeat(null, 5);
