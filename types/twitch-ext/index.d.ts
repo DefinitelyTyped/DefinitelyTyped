129237bb3c8098e4b013233ea2ce9484d9d3e37d
Federico Della Rovere <federico.dellarovere@apptecsrl.com>
2019-03-01 10:23:01 +0100

    add contribution sign

--- types/twitch-ext/index.d.ts
+++ types/twitch-ext/index.d.ts
@@ -1,6 +1,7 @@
 // Type definitions for non-npm package twitch-ext 1.15
 // Project: https://dev.twitch.tv/docs/extensions/reference/#javascript-helper
 // Definitions by: Benedict Etzel <https://github.com/beheh>
+//                 Federico Della Rovere <https://github.com/FedeDR>
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
 // TypeScript Version: 2.4
 
