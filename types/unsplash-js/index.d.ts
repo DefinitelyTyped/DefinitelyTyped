1b6ef0e8f0c6a468d2f2caf070791c52447a9fe2
Andrew <andrew.devels@gmail.com>
2019-03-01 12:26:42 +0500

    feat(unsplash-js): hide internal API

--- types/unsplash-js/index.d.ts
+++ types/unsplash-js/index.d.ts
@@ -5,14 +5,14 @@
 // TypeScript Version: 2.2
 
 export default class Unsplash {
-    auth: Auth;
-    categories: CategoriesApi;
-    collections: CollectionsApi;
-    currentUser: CurrentUserApi;
-    users: UsersApi;
-    photos: PhotoApi;
-    search: SearchApi;
-    stats: StatsApi;
+    auth: UnsplashApi.Auth;
+    categories: UnsplashApi.Categories;
+    collections: UnsplashApi.Collections;
+    currentUser: UnsplashApi.CurrentUser;
+    users: UnsplashApi.Users;
+    photos: UnsplashApi.Photo;
+    search: UnsplashApi.Search;
+    stats: UnsplashApi.Stats;
 
     constructor(options: {
         apiUrl?: string;
@@ -36,192 +36,208 @@ export default class Unsplash {
 
 export function toJson(response: any): any;
 
-export interface PhotoApi {
-    listPhotos(
-        page?: number,
-        perPage?: number,
-        orderBy?: string
-    ): Promise<Response>;
+declare module UnsplashApi {
+    interface Photo {
+        listPhotos(
+            page?: number,
+            perPage?: number,
+            orderBy?: string
+        ): Promise<Response>;
 
-    listCuratedPhotos(
-        page?: number,
-        perPage?: number,
-        orderBy?: string
-    ): Promise<Response>;
+        listCuratedPhotos(
+            page?: number,
+            perPage?: number,
+            orderBy?: string
+        ): Promise<Response>;
 
-    searchPhotos(
-        query: string,
-        categories: ReadonlyArray<string>,
-        page: number,
-        perPage: number
-    ): Promise<Response>;
+        searchPhotos(
+            query: string,
+            categories: ReadonlyArray<string>,
+            page: number,
+            perPage: number
+        ): Promise<Response>;
 
-    getPhoto(
-        id: string,
-        width?: number,
-        height?: number,
-        rectangle?: ReadonlyArray<number>
-    ): Promise<Response>;
+        getPhoto(
+            id: string,
+            width?: number,
+            height?: number,
+            rectangle?: ReadonlyArray<number>
+        ): Promise<Response>;
 
-    getPhotoStats(id: string): Promise<Response>;
+        getPhotoStats(id: string): Promise<Response>;
 
-    getRandomPhoto(options: {
-        width?: number;
-        height?: number;
-        query?: string;
-        username?: string;
-        featured?: boolean;
-        collections?: ReadonlyArray<string>;
-        count?: number;
-    }): Promise<Response>;
+        getRandomPhoto(options: {
+            width?: number;
+            height?: number;
+            query?: string;
+            username?: string;
+            featured?: boolean;
+            collections?: ReadonlyArray<string>;
+            count?: number;
+        }): Promise<Response>;
 
-    uploadPhoto(photo: object): void;
+        uploadPhoto(photo: object): void;
 
-    likePhoto(id: string): Promise<Response>;
+        likePhoto(id: string): Promise<Response>;
 
-    unlikePhoto(id: string): Promise<Response>;
+        unlikePhoto(id: string): Promise<Response>;
 
-    downloadPhoto(photo: {
-        links: { download_location: string };
-    }): Promise<Response>;
-}
+        downloadPhoto(photo: {
+            links: { download_location: string };
+        }): Promise<Response>;
+    }
 
-export interface CollectionsApi {
-    listCollections(
-        page?: number,
-        perPage?: number,
-        orderBy?: string
-    ): Promise<Response>;
+    interface Collections {
+        listCollections(
+            page?: number,
+            perPage?: number,
+            orderBy?: string
+        ): Promise<Response>;
 
-    listCuratedCollections(page?: number, perPage?: number): Promise<Response>;
+        listCuratedCollections(
+            page?: number,
+            perPage?: number
+        ): Promise<Response>;
 
-    listFeaturedCollections(page?: number, perPage?: number): Promise<Response>;
+        listFeaturedCollections(
+            page?: number,
+            perPage?: number
+        ): Promise<Response>;
 
-    getCollection(id: number): Promise<Response>;
+        getCollection(id: number): Promise<Response>;
 
-    getCollectionPhotos(
-        id: number,
-        page?: number,
-        perPage?: number,
-        orderBy?: string
-    ): Promise<Response>;
+        getCollectionPhotos(
+            id: number,
+            page?: number,
+            perPage?: number,
+            orderBy?: string
+        ): Promise<Response>;
 
-    getCuratedCollectionPhotos(
-        id: number,
-        page?: number,
-        perPage?: number,
-        orderBy?: string
-    ): Promise<Response>;
+        getCuratedCollectionPhotos(
+            id: number,
+            page?: number,
+            perPage?: number,
+            orderBy?: string
+        ): Promise<Response>;
 
-    createCollection(
-        title: string,
-        description?: string,
-        private?: boolean
-    ): Promise<Response>;
+        createCollection(
+            title: string,
+            description?: string,
+            private?: boolean
+        ): Promise<Response>;
 
-    updateCollection(
-        id: number,
-        title?: string,
-        description?: string,
-        private?: boolean
-    ): Promise<Response>;
+        updateCollection(
+            id: number,
+            title?: string,
+            description?: string,
+            private?: boolean
+        ): Promise<Response>;
 
-    deleteCollection(id: number): Promise<Response>;
+        deleteCollection(id: number): Promise<Response>;
 
-    addPhotoToCollection(
-        collectionId: number,
-        photoId: string
-    ): Promise<Response>;
+        addPhotoToCollection(
+            collectionId: number,
+            photoId: string
+        ): Promise<Response>;
 
-    removePhotoFromCollection(
-        collectionId: number,
-        photoId: string
-    ): Promise<Response>;
+        removePhotoFromCollection(
+            collectionId: number,
+            photoId: string
+        ): Promise<Response>;
 
-    listRelatedCollections(collectionId: number): Promise<Response>;
-}
+        listRelatedCollections(collectionId: number): Promise<Response>;
+    }
 
-export interface SearchApi {
-    all(keyword: string, page: number, per_page: number): Promise<Response>;
+    interface Search {
+        all(keyword: string, page: number, per_page: number): Promise<Response>;
 
-    photos(
-        keyword: string,
-        page?: number,
-        per_page?: number
-    ): Promise<Response>;
+        photos(
+            keyword: string,
+            page?: number,
+            per_page?: number
+        ): Promise<Response>;
 
-    users(keyword: string, page?: number, per_page?: number): Promise<Response>;
+        users(
+            keyword: string,
+            page?: number,
+            per_page?: number
+        ): Promise<Response>;
 
-    collections(
-        keyword: string,
-        page?: number,
-        per_page?: number
-    ): Promise<Response>;
-}
+        collections(
+            keyword: string,
+            page?: number,
+            per_page?: number
+        ): Promise<Response>;
+    }
 
-export interface StatsApi {
-    total(): Promise<Response>;
-}
+    interface Stats {
+        total(): Promise<Response>;
+    }
 
-export interface CurrentUserApi {
-    profile(): Promise<Response>;
+    interface CurrentUser {
+        profile(): Promise<Response>;
 
-    updateProfile(options: {
-        username?: string;
-        firstName?: string;
-        lastName?: string;
-        email?: string;
-        url?: string;
-        location?: string;
-        bio?: string;
-        instagramUsername?: string;
-    }): Promise<Response>;
-}
+        updateProfile(options: {
+            username?: string;
+            firstName?: string;
+            lastName?: string;
+            email?: string;
+            url?: string;
+            location?: string;
+            bio?: string;
+            instagramUsername?: string;
+        }): Promise<Response>;
+    }
 
-export interface UsersApi {
-    profile(username: string): Promise<Response>;
+    interface Users {
+        profile(username: string): Promise<Response>;
 
-    statistics(
-        username: string,
-        resolution?: string,
-        quantity?: number
-    ): Promise<Response>;
+        statistics(
+            username: string,
+            resolution?: string,
+            quantity?: number
+        ): Promise<Response>;
 
-    photos(
-        username: string,
-        page?: number,
-        perPage?: number,
-        orderBy?: string,
-        stats?: boolean
-    ): Promise<Response>;
+        photos(
+            username: string,
+            page?: number,
+            perPage?: number,
+            orderBy?: string,
+            stats?: boolean
+        ): Promise<Response>;
 
-    likes(
-        username: string,
-        page?: number,
-        perPage?: number,
-        orderBy?: string
-    ): Promise<Response>;
+        likes(
+            username: string,
+            page?: number,
+            perPage?: number,
+            orderBy?: string
+        ): Promise<Response>;
 
-    collections(
-        username: string,
-        page?: number,
-        perPage?: number,
-        orderBy?: string
-    ): Promise<Response>;
-}
+        collections(
+            username: string,
+            page?: number,
+            perPage?: number,
+            orderBy?: string
+        ): Promise<Response>;
+    }
 
-export interface CategoriesApi {
-    listCategories(): Promise<Response>;
+    interface Categories {
+        listCategories(): Promise<Response>;
 
-    category(id: any): Promise<Response>;
+        category(id: any): Promise<Response>;
 
-    categoryPhotos(id: any, page?: number, perPage?: number): Promise<Response>;
-}
+        categoryPhotos(
+            id: any,
+            page?: number,
+            perPage?: number
+        ): Promise<Response>;
+    }
 
-export interface Auth {
-    getAuthenticationUrl(scopes?: ReadonlyArray<string>): string;
+    interface Auth {
+        getAuthenticationUrl(scopes?: ReadonlyArray<string>): string;
 
-    userAuthentication(code: string): Promise<Response>;
+        userAuthentication(code: string): Promise<Response>;
 
-    setBearerToken(accessToken: string): void;
+        setBearerToken(accessToken: string): void;
+    }
 }
