ab46cb11161c5d26ec627868ef2b75b0d4b0ccc3
Hugo "ThePooN" Denizart <thepoon@cartooncraft.fr>
2019-03-02 09:09:10 +0100

    Remove patch number

--- types/mailparser/index.d.ts
+++ types/mailparser/index.d.ts
@@ -1,4 +1,4 @@
-// Type definitions for mailparser v2.4.2
+// Type definitions for mailparser v2.4
 // Project: https://www.npmjs.com/package/mailparser
 // Definitions by: Peter Snider <https://github.com/psnider>
 //                 Andrey Volynkin <https://github.com/Avol-V/>
