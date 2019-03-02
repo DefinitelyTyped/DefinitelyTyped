e1f5902b818ca6bad10eb5b1ffba96ec5150ba6f
Adam Zerella <adamzerella@users.noreply.github.com>
2019-03-02 15:52:56 +1100

    Added type defs for humanize-ms

---
+++ types/humanize-ms/humanize-ms-tests.ts
@@ -0,0 +1,5 @@
+import HumanizeMs from "humanize-ms";
+
+HumanizeMs('1s');
+
+HumanizeMs(1000);
---
+++ types/humanize-ms/index.d.ts
@@ -0,0 +1,6 @@
+// Type definitions for humanize-ms 1.2
+// Project: https://github.com/node-modules/humanize-ms
+// Definitions by:  Adam Zerella <https://github.com/adamzerella>
+// Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
+
+export default function(input: string|number): number;
