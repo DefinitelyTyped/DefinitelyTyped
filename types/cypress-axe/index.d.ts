1b05c90543db62948cc508199160b9e9c4a134b8
Justin Hall <justin.r.hall@gmail.com>
2019-03-02 21:25:29 -0700

    [cypress-axe] augment axe-core's Node type

--- types/cypress-axe/index.d.ts
+++ types/cypress-axe/index.d.ts
@@ -3,7 +3,12 @@
 // Definitions by: Justin Hall <https://github.com/wKovacs64>
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
 // TypeScript Version: 2.3
+
 import { ElementContext, RunOptions } from 'axe-core';
+
+declare module "axe-core" {
+	interface Node {}
+}
 
 declare namespace Cypress {
     interface Chainable<Subject = any> {
