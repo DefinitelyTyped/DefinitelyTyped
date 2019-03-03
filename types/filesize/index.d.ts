f5b66cb1740ee4f7afb2d399525a82aace1706bd
Sam <ffxsam@users.noreply.github.com>
2019-03-03 11:28:17 -0600

    Modified file header

--- types/filesize/index.d.ts
+++ types/filesize/index.d.ts
@@ -1,8 +1,9 @@
-// Type definitions for filesize 4.0
+// Type definitions for filesize 4.1.2
 // Project: https://github.com/avoidwork/filesize.js, https://filesizejs.com
 // Definitions by: Giedrius Grabauskas <https://github.com/GiedriusGrabauskas>
 //                 Renaud Chaput <https://github.com/renchap>
 //                 Roman Nuritdinov <https://github.com/Ky6uk>
+//                 Sam Hulick <https://github.com/ffxsam>
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
 
 declare var fileSize: Filesize.Filesize;
