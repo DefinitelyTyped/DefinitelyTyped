ca7e81b1f39cca6cb508613455af0eece498dc08
Adam Zerella <adamzerella@users.noreply.github.com>
2019-03-03 17:16:26 +1100

    Added type defs for pretty-time

---
+++ types/pretty-time/index.d.ts
@@ -0,0 +1,40 @@
+// Type definitions for pretty-time 1.1
+// Project: https://github.com/jonschlinkert/pretty-time
+// Definitions by:  Adam Zerella <https://github.com/adamzerella>
+// Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
+// TypeScript Version: 3.3
+
+export type Increment =
+    | 'ns'
+    | 'nano'
+    | 'nanosecond'
+    | 'nanoseconds'
+    | 'μs'
+    | 'micro'
+    | 'microsecond'
+    | 'microseconds'
+    | 'ms'
+    | 'milli'
+    | 'millisecond'
+    | 'milliseconds'
+    | 's'
+    | 'sec'
+    | 'second'
+    | 'seconds'
+    | 'm'
+    | 'min'
+    | 'minute'
+    | 'minutes'
+    | 'h'
+    | 'hr'
+    | 'hour'
+    | 'hours'
+    | 'd'
+    | 'day'
+    | 'days'
+    | 'w'
+    | 'wk'
+    | 'week'
+    | 'weeks';
+
+export default function(time: number[], smallest?: Increment, digits?: number): string;
