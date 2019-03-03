1c43f86f73ca178bdc96fbd98e8e8c3b6b8219d9
kouros51 <sboui.houssem@gmail.com>
2019-03-03 17:32:17 -0500

    Add in the test a use case explaining the need to expose those too new functions

--- types/cls-hooked/index.d.ts
+++ types/cls-hooked/index.d.ts
@@ -1,4 +1,4 @@
-// Type definitions for cls-hooked 4.2
+// Type definitions for cls-hooked 4.3
 // Project: https://github.com/jeff-lewis/cls-hooked
 // Definitions by: Leo Liang <https://github.com/aleung>
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
@@ -12,14 +12,14 @@ export interface Namespace {
 
     set<T>(key: string, value: T): T;
     get(key: string): any;
-    enter(context: any): void;
-    exit(context: any): void;
     run(fn: (...args: any[]) => void): void;
     runAndReturn<T>(fn: (...args: any[]) => T): T;
     runPromise<T>(fn: (...args: any[]) => Promise<T>): Promise<T>;
     bind<F extends Function>(fn: F, context?: any): F; // tslint:disable-line: ban-types
     bindEmitter(emitter: EventEmitter): void;
     createContext(): any;
+    enter(context: any): void;
+    exit(context: any): void;
 }
 
 export function createNamespace(name: string): Namespace;
