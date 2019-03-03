dc27067ac0e57e4609ffcaa8f4af841cd56f404a
Sam <ffxsam@users.noreply.github.com>
2019-03-03 15:46:09 -0600

    Removed patch part from version

--- types/filesize/index.d.ts
+++ types/filesize/index.d.ts
@@ -1,4 +1,4 @@
-// Type definitions for filesize 4.1.2
+// Type definitions for filesize 4.1
 // Project: https://github.com/avoidwork/filesize.js, https://filesizejs.com
 // Definitions by: Giedrius Grabauskas <https://github.com/GiedriusGrabauskas>
 //                 Renaud Chaput <https://github.com/renchap>
