aad8e76a58b28168ceb10a16a75019d41e48b6ca
Kenneth Kidmose Johnsen <kkj@intranote.dk>
2019-03-04 15:29:14 +0100

    added definition credits for proper notifications about issues

--- types/resumablejs/index.d.ts
+++ types/resumablejs/index.d.ts
@@ -1,6 +1,7 @@
 // Type definitions for Resumable.js v1.0.2
 // Project: https://github.com/23/resumable.js
 // Definitions by: Daniel McAssey <https://github.com/DanielMcAssey>
+//                 Kenneth Kidmose Johnsen <https://github.com/Johns3n>
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
 
 declare namespace Resumable {
