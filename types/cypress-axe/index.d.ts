1136cb28a384d483bfe0eabca24e4c8e6417e918
Justin Hall <justin.r.hall@gmail.com>
2019-03-03 19:06:56 -0700

    [cypress-axe] fix all the things

--- types/cypress-axe/index.d.ts
+++ types/cypress-axe/index.d.ts
@@ -2,17 +2,20 @@
 // Project: https://github.com/avanslaars/cypress-axe#readme
 // Definitions by: Justin Hall <https://github.com/wKovacs64>
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
-// TypeScript Version: 3.1
+// TypeScript Version: 2.3
 
-/// <reference lib="dom" />
+import { ElementContext, RunOptions } from 'axe-core';
 
-declare namespace Cypress {
-    interface Chainable<Subject = any> {
-        injectAxe(): void;
-        checkA11y(
-            context?: import('axe-core').ElementContext,
-            options?: import('axe-core').RunOptions,
-        ): void;
-        configureAxe(options?: import('axe-core').RunOptions): void;
+declare module 'axe-core' {
+    interface Node {}
+}
+
+declare global {
+    namespace Cypress {
+        interface Chainable<Subject = any> {
+            injectAxe(): void;
+            checkA11y(context?: ElementContext, options?: RunOptions): void;
+            configureAxe(options?: RunOptions): void;
+        }
     }
 }
