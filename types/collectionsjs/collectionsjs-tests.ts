56bcf7fd2418407623b3e95e8320779340bff866
Jamie Sykes <jaymehsykes@gmail.com>
2019-03-04 22:01:49 +0000

    Make use of Generic's and ensure tests are up to date.

--- types/collectionsjs/collectionsjs-tests.ts
+++ types/collectionsjs/collectionsjs-tests.ts
@@ -11,47 +11,50 @@ const characters = [
     { name: 'Catelyn Stark', age: 35}
 ];
 
-const item = { 6: 7 };
+const item = { name: 'Sansa Stark', age: 13 };
 
-const array = ['a', 'b', 'c'];
+const array = [
+    { name: 'Robert Baratheon', age: 40 },
+    { name: 'Joffrey Baratheon', age: 13 }
+];
 
-const collection = new Collection(collectable); // $ExpectType Collection
+const collection = new Collection(collectable); // $ExpectType Collection<{ name: string; age: number; }>
 
-collection.add(item); // $ExpectType Collection
-collection.all(); // $ExpectType Collection
+collection.add(item); // $ExpectType Collection<{ name: string; age: number; }>
+collection.all(); // $ExpectType Collection<{ name: string; age: number; }>
 collection.average('age'); // $ExpectType number
-collection.chunk(2).all(); // $ExpectType Collection
-collection.collect(collectable); // $ExpectType Collection
-collection.concat(characters); // $ExpectType Collection
+collection.chunk(2).all(); // $ExpectType Collection<{ name: string; age: number; }>
+collection.collect(collectable); // $ExpectType Collection<{ name: string; age: number; }>
+collection.concat(characters); // $ExpectType Collection<{ name: string; age: number; }>
 collection.contains(stark => stark.name === 'John Snow'); // $ExpectType boolean
 collection.count(); // $ExpectType number
-collection.each(t => t = 3); // $ExpectType Collection
-collection.filter(stark => stark.age === 14); // $ExpectType Collection
+collection.each(stark => stark.age = 3); // $ExpectType Collection<{ name: string; age: number; }>
+collection.filter(stark => stark.age === 14); // $ExpectType Collection<{ name: string; age: number; }>
 collection.find('bran'); // $ExpectType number
-collection.first(item => item.age > 7); // $ExpectType any
-collection.flatten(true); // $ExpectType Collection
-collection.get(2); // $ExpectType any
+collection.first(item => item.age > 7); // $ExpectType { name: string; age: number; }
+collection.flatten(true); // $ExpectType Collection<{ name: string; age: number; }>
+collection.get(2); // $ExpectType { name: string; age: number; }
 collection.has({ name: 'Bran Stark', age: 7 }); // $ExpectType boolean
 collection.join(); // $ExpectType string
-collection.keys(); // $ExpectType Collection
-collection.last(); // $ExpectType any
-collection.map(stark => stark.name); // $ExpectType Collection
-collection.pluck('name'); // $ExpectType Collection
-collection.push({name: 'Robb Stark', age: 17}); // $ExpectType Collection
-collection.reduce((previous, current) => previous + current, 0); // $ExpectType any
-collection.reject(stark => stark.age < 14); // $ExpectType Collection
+collection.keys(); // $ExpectType Collection<{ name: string; age: number; }>
+collection.last(); // $ExpectType { name: string; age: number; }
+collection.map(stark => stark.name); // $ExpectType Collection<{ name: string; age: number; }>
+collection.pluck('name'); // $ExpectType Collection<{ name: string; age: number; }>
+collection.push({name: 'Robb Stark', age: 17}); // $ExpectType Collection<{ name: string; age: number; }>
+collection.reduce((previous, current) => previous.age + current.age, 0); // $ExpectType any
+collection.reject(stark => stark.age < 14); // $ExpectType Collection<{ name: string; age: number; }>
 collection.remove({name: 'Robb Stark', age: 17}); // $ExpectType boolean
-collection.reverse(); // $ExpectType Collection
-collection.skip(2); // $ExpectType Collection
-collection.slice(1, 3); // $ExpectType Collection
-collection.sort(); // $ExpectType Collection
-collection.sortBy('name'); // $ExpectType Collection
+collection.reverse(); // $ExpectType Collection<{ name: string; age: number; }>
+collection.skip(2); // $ExpectType Collection<{ name: string; age: number; }>
+collection.slice(1, 3); // $ExpectType Collection<{ name: string; age: number; }>
+collection.sort(); // $ExpectType Collection<{ name: string; age: number; }>
+collection.sortBy('name'); // $ExpectType Collection<{ name: string; age: number; }>
 collection.stringify(); // $ExpectType string
 collection.sum('age'); // $ExpectType any
-collection.take(2); // $ExpectType Collection
+collection.take(2); // $ExpectType Collection<{ name: string; age: number; }>
 collection.macro('addToMembers', (collection, n) => collection.map((collectionItem: any) => collectionItem + n)); // $ExpectType any
-collection.unique(s => s.grade); // $ExpectType Collection
-collection.values(); // $ExpectType Collection
-collection.where('age', 14); // $ExpectType Collection
-collection.where(stark => stark.age === 14); // $ExpectType Collection
-collection.zip(array); // $ExpectType Collection
+collection.unique(stark => stark.age); // $ExpectType Collection<{ name: string; age: number; }>
+collection.values(); // $ExpectType Collection<{ name: string; age: number; }>
+collection.where('age', 14); // $ExpectType Collection<{ name: string; age: number; }>
+collection.where(stark => stark.age === 14); // $ExpectType Collection<{ name: string; age: number; }>
+collection.zip(array); // $ExpectType Collection<{ name: string; age: number; }>
--- types/collectionsjs/index.d.ts
+++ types/collectionsjs/index.d.ts
@@ -4,43 +4,43 @@
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
 // TypeScript Version: 3.1
 
-export default class Collection {
-    constructor(items?: any);
-    add(item: any): Collection;
-    all(): Collection;
+export default class Collection<T> {
+    constructor(items?: T[]);
+    add(item: T): Collection<T>;
+    all(): Collection<T>;
     average(property?: string | ((property?: number) => number)): number;
-    chunk(size: number): Collection;
-    collect(collectable: any[]|string): Collection;
-    concat(collection: any[]|Collection): Collection;
-    contains(closure: ((item: any) => boolean)): boolean;
+    chunk(size: number): Collection<T>;
+    collect(collectable: T[]): Collection<T>;
+    concat(collection: T[]|Collection<T>): Collection<T>;
+    contains(closure: ((item: T) => boolean)): boolean;
     count(): number;
-    each(callback: (item: any) => void): Collection;
-    filter(callback: (item: any) => any): Collection;
+    each(callback: (item: T) => void): Collection<T>;
+    filter(callback: (item: T) => boolean): Collection<T>;
     find(item: any): number;
-    first(callback?: ((item: any) => any)|null): any;
-    flatten(deep?: boolean): Collection;
-    get(index: number): any;
-    has(item: any): boolean;
+    first(callback?: ((item: T) => boolean)|null): T;
+    flatten(deep?: boolean): Collection<T>;
+    get(index: number): T;
+    has(item: T): boolean;
     join(separator?: string): string;
-    keys(): Collection;
-    last(callback?: ((item: any) => any)|null): any;
-    map(callback: (item: any) => any): Collection;
-    pluck(property: string): Collection;
-    push(item: any): Collection;
-    reduce(callback: (previous: any, current: any) => any, initial: any): any;
-    reject(callback: (item: any) => any): Collection;
+    keys(): Collection<T>;
+    last(callback?: ((item: T) => boolean)|null): T;
+    map(callback: (item: T) => any): Collection<T>;
+    pluck(property: string): Collection<T>;
+    push(item: T): Collection<T>;
+    reduce(callback: (previous: T, current: T) => any, initial: any): any;
+    reject(callback: (item: T) => boolean): Collection<T>;
     remove(item: any): boolean;
-    reverse(): Collection;
-    skip(count: number): Collection;
-    slice(start: number, end?: number): Collection;
-    sort(compare?: () => any): Collection;
-    sortBy(property: string, order?: string): Collection;
+    reverse(): Collection<T>;
+    skip(count: number): Collection<T>;
+    slice(start: number, end?: number): Collection<T>;
+    sort(compare?: () => boolean): Collection<T>;
+    sortBy(property: string, order?: string): Collection<T>;
     stringify(): string;
     sum(property?: string|null): any;
-    take(count: number): Collection;
+    take(count: number): Collection<T>;
     macro(name: string, callback: (...args: any) => any): any;
-    unique(callback?: string|null|((item: any) => any)): Collection;
-    values(): Collection;
-    where(callback: ((item: any) => any)|string, value?: any): Collection;
-    zip(array: any[]|Collection): Collection;
+    unique(callback?: string|null|((item: T) => any)): Collection<T>;
+    values(): Collection<T>;
+    where(callback: ((item: T) => boolean)|string, value?: any): Collection<T>;
+    zip(array: T[]|Collection<T>): Collection<T>;
 }
