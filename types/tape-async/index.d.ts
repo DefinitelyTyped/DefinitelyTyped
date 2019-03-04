af9058995f6df0e81288249277d69f879360946e
ExE Boss <3889017+ExE-Boss@users.noreply.github.com>
2019-03-04 23:00:00 +0100

    chore(tape‑async): Require TypeScript 2.3
    Co-Authored-By: Nathan Shively-Sanders <293473+sandersn@users.noreply.github.com>

--- types/tape-async/index.d.ts
+++ types/tape-async/index.d.ts
@@ -2,6 +2,7 @@
 // Project: https://github.com/parro-it/tape-async
 // Definitions by: ExE Boss <https://github.com/ExE-Boss>
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
+// TypeScript Version: 2.3
 
 /// <reference types="node" />
 
