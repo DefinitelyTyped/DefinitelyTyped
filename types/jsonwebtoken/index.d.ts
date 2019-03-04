6f9e0fddedb7ec27aa6885cd382a8f7bdeb18c9a
Kjell Dießel <com.github@kettil.de>
2019-03-04 18:15:30 +0100

    Update jsonwebtoken index.d.ts

--- types/jsonwebtoken/index.d.ts
+++ types/jsonwebtoken/index.d.ts
@@ -4,7 +4,8 @@
 //                 Daniel Heim <https://github.com/danielheim>,
 //                 Brice BERNARD <https://github.com/brikou>,
 //                 Veli-Pekka Kestilä <https://github.com/vpk>,
-//                 Daniel Parker <https://github.com/rlgod>
+//                 Daniel Parker <https://github.com/rlgod>,
+//                 Kjell Dießel <https://github.com/kettil>
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
 // TypeScript Version: 2.2
 
