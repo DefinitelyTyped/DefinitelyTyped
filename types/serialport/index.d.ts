a72ea5ad9e85b8c1fb6f783bfb32b0f63e8c9ce6
Cameron Tacklind <cameron@tacklind.com>
2019-03-03 18:34:39 -0800

    Add myself to author

--- types/serialport/index.d.ts
+++ types/serialport/index.d.ts
@@ -2,6 +2,7 @@
 // Project: https://github.com/EmergingTechnologyAdvisors/node-serialport
 // Definitions by: Jeremy Foster <https://github.com/codefoster>
 //                 Andrew Pearson <https://github.com/apearson>
+//                 Cameron Tacklind <https://github.com/cinderblock>
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
 
 /// <reference types="node" />
