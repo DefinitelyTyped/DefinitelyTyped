95ae13766fa48b9cd17ee73b90ad6f9811f91705
Rob Valentine <rob@codemonkeys.co.za>
2019-03-05 14:35:13 +0200

    Contrib

--- types/react-dnd-multi-backend/index.d.ts
+++ types/react-dnd-multi-backend/index.d.ts
@@ -2,6 +2,7 @@
 // Project: https://github.com/LouisBrunner/react-dnd-multi-backend, https://louisbrunner.github.io/dnd-multi-backend/packages/react-dnd-multi-backend
 // Definitions by: Janeene Beeforth <https://github.com/dawnmist>
 //                 Adam Haglund <https://github.com/beeequeue>
+//                 Rob Valentine <https://github.com/robcodemonkey>
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
 // TypeScript Version: 2.8
 
