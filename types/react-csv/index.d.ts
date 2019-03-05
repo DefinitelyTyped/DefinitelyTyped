7059ba1e9f54c3c439d522b7234e2a7dde8103a0
Vincent Pizzo <vincent.pizzo@singlewire.com>
2019-03-05 14:31:36 -0800

    Don't use patch in version

--- types/react-csv/index.d.ts
+++ types/react-csv/index.d.ts
@@ -1,4 +1,4 @@
-// Type definitions for react-csv 1.1.1
+// Type definitions for react-csv 1.1
 // Project: https://github.com/react-csv/react-csv
 // Definitions by: Vincent Pizzo <https://github.com/vincentjames501>
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
