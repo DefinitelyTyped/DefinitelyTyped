56de1e479c09182b87d995775b1397dccebe6f0e
Adam A. Zerella <adamzerella@users.noreply.github.com>
2019-03-06 16:17:47 +1100

    Updated any type to unknown

--- types/is-date-object/index.d.ts
+++ types/is-date-object/index.d.ts
@@ -2,7 +2,8 @@
 // Project: https://github.com/ljharb/is-date-object
 // Definitions by:  Adam Zerella <https://github.com/adamzerella>
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
+// TypeScript Version: 3.3
 
-declare function isDateObject(value?: any): boolean;
+declare function isDateObject(value?: unknown): boolean;
 
 export = isDateObject;
