f68370866006cd8f3b6566af111e09c535be1381
Po Chen <chenpaul914@gmail.com>
2019-03-06 13:12:45 +1100

    Update index.d.ts

--- types/phoenix/index.d.ts
+++ types/phoenix/index.d.ts
@@ -1,4 +1,4 @@
-// Type definitions for phoenix 1.4.1
+// Type definitions for phoenix 1.4
 // Project: https://github.com/phoenixframework/phoenix
 // Definitions by: Mirosław Ciastek <https://github.com/mciastek>, John Goff <https://github.com/John-Goff>, Po Chen <https://github.com/princemaple>
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
