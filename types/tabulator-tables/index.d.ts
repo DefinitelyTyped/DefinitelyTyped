c79ee7b65f500c19ec2bf6e5dccfa9111efe434f
Jojoshua <jojoshua@gmail.com>
2019-03-06 10:39:56 -0500

    Update project url

--- types/tabulator-tables/index.d.ts
+++ types/tabulator-tables/index.d.ts
@@ -1,5 +1,5 @@
 // Type definitions for tabulator-tables 4.2
-// Project: https://github.com/olifolkerd/tabulator
+// Project: http://tabulator.info
 // Definitions by: Josh Harris <https://github.com/jojoshua>
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
 // TypeScript Version: 2.3
