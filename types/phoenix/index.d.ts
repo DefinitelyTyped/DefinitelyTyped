b65c210c271d7522f79d7c862ee18b3586ba8d14
Po Chen <chenpaul914@gmail.com>
2019-03-06 12:09:15 +1100

    Update index.d.ts

--- types/phoenix/index.d.ts
+++ types/phoenix/index.d.ts
@@ -4,7 +4,7 @@
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
 // TypeScript Version: 2.3
 
-declare module "phoenix" {
+declare module 'phoenix' {
   class Push {
     constructor(
       channel: Channel,
