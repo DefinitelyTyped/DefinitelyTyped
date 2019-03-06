0288a7263ccf353b3867ebf1923d1922624565b5
Lorenzo Rapetti <lorenzo.rapetti.94@gmail.com>
2019-03-06 12:55:46 +0100

    Remove patch number from version

--- types/istanbul-lib-coverage/index.d.ts
+++ types/istanbul-lib-coverage/index.d.ts
@@ -1,4 +1,4 @@
-// Type definitions for istanbul-lib-coverage 2.0.3
+// Type definitions for istanbul-lib-coverage 2.0
 // Project: https://github.com/istanbuljs/istanbuljs
 // Definitions by: Jason Cheatham <https://github.com/jason0x43>
 //                 Lorenzo Rapetti <https://github.com/loryman>
