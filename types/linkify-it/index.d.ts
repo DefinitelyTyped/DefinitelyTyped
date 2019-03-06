33ab6268026e6d3887ce9e2f09d4d6192ac6420e
Alex Plumb <alex@sidedooraccess.com>
2019-03-06 19:58:54 -0400

    Update version number

--- types/linkify-it/index.d.ts
+++ types/linkify-it/index.d.ts
@@ -1,4 +1,4 @@
-// Type definitions for linkify-it 2.0.3
+// Type definitions for linkify-it 2.1.0
 // Project: https://github.com/markdown-it/linkify-it
 // Definitions by: Lindsey Smith <https://github.com/praxxis>, Robert Coie <https://github.com/rapropos/typed-linkify-it>, Alex Plumb <https://github.com/alexplumb>
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
