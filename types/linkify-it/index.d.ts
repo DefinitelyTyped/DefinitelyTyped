973f17bb946f4d583265ed8c427b1f10f1b774f9
Alex Plumb <alex@sidedooraccess.com>
2019-03-06 20:20:28 -0400

    Fix header

--- types/linkify-it/index.d.ts
+++ types/linkify-it/index.d.ts
@@ -1,8 +1,8 @@
 // Type definitions for linkify-it 2.1.0
 // Project: https://github.com/markdown-it/linkify-it
-// TypeScript Version: 2.2
 // Definitions by: Lindsey Smith <https://github.com/praxxis>, Robert Coie <https://github.com/rapropos/typed-linkify-it>, Alex Plumb <https://github.com/alexplumb>
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
+// TypeScript Version: 2.2
 
 declare const LinkifyIt: {
     (
