96f6a298d7eae6d41eab0049e64106da28834712
qiang <guoshengqiang@live.com>
2019-03-07 16:09:42 +0800

    Added new types for uni-app

---
+++ types/uni-app/index.d.ts
@@ -0,0 +1,12 @@
+// Type definitions for non-npm package uni-app 1.0
+// Project: https://github.com/dcloudio/uni-app
+// Definitions by: DCloud <https://github.com/dcloudio>
+// Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
+
+// TypeScript Version: 2.3
+
+import "./lib/app";
+import "./lib/common";
+import "./lib/page";
+import "./lib/uni";
+import "./lib/vue";
---
+++ types/uni-app/lib/vue.d.ts
@@ -0,0 +1,15 @@
+import Vue from "vue";
+
+declare module "*.vue" {
+    export default Vue;
+}
+
+declare module "vue/types/options" {
+    type Hooks = App.AppInstance & Page.PageInstance;
+    interface ComponentOptions<V extends Vue> extends Hooks {
+        /**
+         * 组件类型
+         */
+        mpType?: string;
+    }
+}
