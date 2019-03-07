842f8a476eaa16aff9f57bd1c3fcdbc35f242a42
Alex Plumb <alex@sidedooraccess.com>
2019-03-06 20:19:42 -0400

    Fix test

--- types/linkify-it/index.d.ts
+++ types/linkify-it/index.d.ts
@@ -1,6 +1,6 @@
 // Type definitions for linkify-it 2.1.0
-// TypeScript Version: 2.2
 // Project: https://github.com/markdown-it/linkify-it
+// TypeScript Version: 2.2
 // Definitions by: Lindsey Smith <https://github.com/praxxis>, Robert Coie <https://github.com/rapropos/typed-linkify-it>, Alex Plumb <https://github.com/alexplumb>
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
 
