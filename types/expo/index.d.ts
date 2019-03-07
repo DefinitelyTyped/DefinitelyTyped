6b5fa0f70981a50237c1a8a8f21a599a959e3be5
Vinit Sood <vinitsood90@gmail.com>
2019-03-07 21:34:08 +0100

    add definitions for ScreenOrientation and AppLoadingProps

--- types/expo/index.d.ts
+++ types/expo/index.d.ts
@@ -16,6 +16,7 @@
 //                 Bartosz Dotryw <https://github.com/burtek>
 //                 Jason Killian <https://github.com/jkillian>
 //                 Satyajit Sahoo <https://github.com/satya164>
+//                 Vinit Sood <https://github.com/vinitsood> 
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
 // TypeScript Version: 2.8
 
@@ -200,6 +201,9 @@ export interface AppLoadingProps {
 
     /** If `startAsync` throws an error, it is caught and passed into the function provided to `onError`. */
     onError?: (error: Error) => void;
+
+    /**  Whether to hide the native splash screen as soon as you unmount the AppLoading component. */
+    autoHideSplash?: boolean
 }
 
 /**
@@ -2463,7 +2467,11 @@ export namespace ScreenOrientation {
 
     const Orientation: Orientations;
 
+    /** Deprecated in favour of ScreenOrientation.allowAsync. */
     function allow(orientation: keyof Orientations): void;
+
+    /** Allow a screen orientation. You can call this function multiple times with multiple orientations to allow multiple orientations. */
+    function allowAsync(orientation: keyof Orientations): void;
 }
 
 /**
