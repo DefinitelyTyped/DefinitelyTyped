4a027eccebaa9d1aee1f7edd9ab04ddb58677385
Konrad Klockgether <k.klockgether@28apps.de>
2019-03-08 13:37:08 +0100

    [polygon] simple test

--- types/polygon/polygon-tests.ts
+++ types/polygon/polygon-tests.ts
@@ -1,2 +1,16 @@
 // no tests yet
-const ok = 1;
+import Polygon from 'polygon';
+
+const polygon1 = new Polygon([
+    [0, 0],
+    [5, 0],
+    [5, 5],
+    [0, 5],
+    [0, 0],
+]);
+const shouldBeTrue = polygon1.contains({
+    x: 3,
+    y: 3,
+    w: 4,
+    h: 3,
+});
