98aed985909e030e0c02e418747c996c70b468db
Klaus Reimer <k.reimer@iplabs.de>
2019-03-08 07:41:32 +0100

    Remove declare global

--- types/offscreencanvas/index.d.ts
+++ types/offscreencanvas/index.d.ts
@@ -5,53 +5,49 @@
 
 // TypeScript Version: 3.1
 
-declare global {
-    // https://html.spec.whatwg.org/multipage/canvas.html#canvasdrawimage
-    interface CanvasDrawImage {
-        drawImage(image: CanvasImageSource | OffscreenCanvas, dx: number, dy: number): void;
-        drawImage(image: CanvasImageSource | OffscreenCanvas, dx: number, dy: number, dw: number, dh: number): void;
-        drawImage(image: CanvasImageSource | OffscreenCanvas, sx: number, sy: number, sw: number, sh: number,
-            dx: number, dy: number, dw: number, dh: number): void;
-    }
-
-    // https://html.spec.whatwg.org/multipage/imagebitmap-and-animations.html#dom-createimagebitmap
-    function createImageBitmap(image: ImageBitmapSource | OffscreenCanvas): Promise<ImageBitmap>;
-    function createImageBitmap(image: ImageBitmapSource | OffscreenCanvas, sx: number, sy: number,
-        sw: number, sh: number): Promise<ImageBitmap>;
-
-    // https://html.spec.whatwg.org/multipage/canvas.html#dom-canvas-transfercontroltooffscreen
-    interface HTMLCanvasElement extends HTMLElement {
-        transferControlToOffscreen(): OffscreenCanvas;
-    }
-
-    // https://html.spec.whatwg.org/multipage/canvas.html#offscreencanvasrenderingcontext2d
-    interface OffscreenCanvasRenderingContext2D extends CanvasState, CanvasTransform, CanvasCompositing,
-            CanvasImageSmoothing, CanvasFillStrokeStyles, CanvasShadowStyles, CanvasFilters, CanvasRect,
-            CanvasDrawPath, CanvasText, CanvasDrawImage, CanvasImageData, CanvasPathDrawingStyles,
-            CanvasTextDrawingStyles, CanvasPath {
-        readonly canvas: OffscreenCanvas;
-    }
-    var OffscreenCanvasRenderingContext2D: {
-        prototype: OffscreenCanvasRenderingContext2D;
-        new (): OffscreenCanvasRenderingContext2D;
-    };
-
-    // https://html.spec.whatwg.org/multipage/canvas.html#the-offscreencanvas-interface
-    interface OffscreenCanvas extends EventTarget {
-        width: number;
-        height: number;
-        getContext(contextId: "2d", contextAttributes?: CanvasRenderingContext2DSettings):
-            OffscreenCanvasRenderingContext2D | null;
-        getContext(contextId: "webgl", contextAttributes?: WebGLContextAttributes): WebGLRenderingContext | null;
-        getContext(contextId: string, contextAttributes?: {}): OffscreenCanvasRenderingContext2D
-            | WebGLRenderingContext | null;
-        transferToImageBitmap(): ImageBitmap;
-        convertToBlob(options?: { type?: string, quality?: number }): Promise<Blob>;
-    }
-    var OffscreenCanvas: {
-        prototype: OffscreenCanvas;
-        new (width: number, height: number): OffscreenCanvas;
-    };
+// https://html.spec.whatwg.org/multipage/canvas.html#canvasdrawimage
+interface CanvasDrawImage {
+    drawImage(image: CanvasImageSource | OffscreenCanvas, dx: number, dy: number): void;
+    drawImage(image: CanvasImageSource | OffscreenCanvas, dx: number, dy: number, dw: number, dh: number): void;
+    drawImage(image: CanvasImageSource | OffscreenCanvas, sx: number, sy: number, sw: number, sh: number,
+        dx: number, dy: number, dw: number, dh: number): void;
 }
 
-export { };
+// https://html.spec.whatwg.org/multipage/imagebitmap-and-animations.html#dom-createimagebitmap
+declare function createImageBitmap(image: ImageBitmapSource | OffscreenCanvas): Promise<ImageBitmap>;
+declare function createImageBitmap(image: ImageBitmapSource | OffscreenCanvas, sx: number, sy: number,
+    sw: number, sh: number): Promise<ImageBitmap>;
+
+// https://html.spec.whatwg.org/multipage/canvas.html#dom-canvas-transfercontroltooffscreen
+interface HTMLCanvasElement extends HTMLElement {
+    transferControlToOffscreen(): OffscreenCanvas;
+}
+
+// https://html.spec.whatwg.org/multipage/canvas.html#offscreencanvasrenderingcontext2d
+interface OffscreenCanvasRenderingContext2D extends CanvasState, CanvasTransform, CanvasCompositing,
+        CanvasImageSmoothing, CanvasFillStrokeStyles, CanvasShadowStyles, CanvasFilters, CanvasRect,
+        CanvasDrawPath, CanvasText, CanvasDrawImage, CanvasImageData, CanvasPathDrawingStyles,
+        CanvasTextDrawingStyles, CanvasPath {
+    readonly canvas: OffscreenCanvas;
+}
+declare var OffscreenCanvasRenderingContext2D: {
+    prototype: OffscreenCanvasRenderingContext2D;
+    new (): OffscreenCanvasRenderingContext2D;
+};
+
+// https://html.spec.whatwg.org/multipage/canvas.html#the-offscreencanvas-interface
+interface OffscreenCanvas extends EventTarget {
+    width: number;
+    height: number;
+    getContext(contextId: "2d", contextAttributes?: CanvasRenderingContext2DSettings):
+        OffscreenCanvasRenderingContext2D | null;
+    getContext(contextId: "webgl", contextAttributes?: WebGLContextAttributes): WebGLRenderingContext | null;
+    getContext(contextId: string, contextAttributes?: {}): OffscreenCanvasRenderingContext2D
+        | WebGLRenderingContext | null;
+    transferToImageBitmap(): ImageBitmap;
+    convertToBlob(options?: { type?: string, quality?: number }): Promise<Blob>;
+}
+declare var OffscreenCanvas: {
+    prototype: OffscreenCanvas;
+    new (width: number, height: number): OffscreenCanvas;
+};
