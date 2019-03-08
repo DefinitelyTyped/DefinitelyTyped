c5d1aa342aa297ac2a690fb043e8bd018f97a4ac
João Moura <joao.moura@b-i.com>
2019-03-08 23:36:43 +0000

    Added my self has the contributor

--- types/voucher-code-generator/index.d.ts
+++ types/voucher-code-generator/index.d.ts
@@ -1,6 +1,6 @@
 // Type definitions for voucher-code-generator 1.1
 // Project: http://www.voucherify.io/
-// Definitions by: My Self <https://github.com/me>
+// Definitions by: João Moura <https://github.com/JWebCoder>
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
 
 /*~ If this module has methods, declare them as functions like so.
