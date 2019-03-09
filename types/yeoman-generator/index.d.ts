cfde12269ccf42b9c1db2a04e260a4f8ed4ccf62
Richard Lea <chigix@zoho.com>
2019-03-10 01:01:16 +0900

    fix(inquirer): update api definitions to 6.x
    Signed-off-by: Richard Lea <chigix@zoho.com>

--- types/yeoman-generator/index.d.ts
+++ types/yeoman-generator/index.d.ts
@@ -7,6 +7,7 @@
 //                 Arthur Corenzan <https://github.com/haggen>
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
 // TypeScript Version: 2.3
+/// <reference types="rx" />
 
 import { EventEmitter } from 'events';
 import * as inquirer from 'inquirer';
