c5da7548036e6868200642cea64bf57280087f47
Claas Ahlrichs <claasahl@gmail.com>
2019-03-10 14:10:57 +0100

    added test case

--- types/protocol-buffers-schema/index.d.ts
+++ types/protocol-buffers-schema/index.d.ts
@@ -2,6 +2,7 @@
 // Project: https://github.com/mafintosh/protocol-buffers-schema
 // Definitions by: Claas Ahlrichs <https://github.com/claasahl>
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
+// TypeScript Version: 2.7
 
 /// <reference types="node" />
 
