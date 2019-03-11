a4becaf31aee5a1bcca7695ea1753bc59cee046a
Konrad Klockgether <k.klockgether@28apps.de>
2019-03-11 10:19:53 +0100

    [vec2] remove member-access rule

--- types/vec2/index.d.ts
+++ types/vec2/index.d.ts
@@ -11,8 +11,8 @@
  * Also, since `Infinity`and `NaN` are so insidious, this library will throw as soon as it detects either of these so you can take action to fix your data/algorithm.
  */
 declare class Vec2 {
-    public readonly x: number;
-    public readonly y: number;
+    readonly x: number;
+    readonly y: number;
 
     constructor(xy: number[]);
     constructor(x: number, y: number);
@@ -22,56 +22,56 @@ declare class Vec2 {
      * `fn` signature: `function(vec, prev) {}` - where `prev` is a clone of the vector before the last operation.
      * this function returns the passed `fn`
      */
-    public change(fn?: (vec: Vec2, prev: Vec2) => any): Vec2;
+    change(fn?: (vec: Vec2, prev: Vec2) => any): Vec2;
 
     /**
      * Pass a `fn` to remove it from the observers list. Calling this function without a `fn` will remove all observers.
      */
-    public ignore(fn?: (vec: Vec2, prev: Vec2) => any): Vec2;
+    ignore(fn?: (vec: Vec2, prev: Vec2) => any): Vec2;
 
     /**
      * Sets the `x` and `y` coordinates of this vector.  If `false` is passed for `notify`, none of the observers will be called.
      */
-    public set(x: number, y: number, notify: boolean): Vec2;
+    set(x: number, y: number, notify: boolean): Vec2;
 
     /**
      * Sets the `x` and `y` of this vector to `0`
      */
-    public zero(): Vec2;
+    zero(): Vec2;
 
     /**
      * Returns a clone of this vector.
      * _Note_: this does not clone observers
      */
-    public zero(): Vec2;
+    zero(): Vec2;
 
     /**
      * Negate the `x` and `y` coords of this vector.  If `returnNew` is truthy, a new vector with the negated coordinates will be returned.
      */
-    public negate(returnNew?: boolean): Vec2;
+    negate(returnNew?: boolean): Vec2;
 
     /**
      * Add the `x` and `y` to this vector's coordinates.
      * If `returnNew` is truthy, return a new vector containing the resulting coordinates. Otherwise apply them to this vector and return it.
      */
-    public add(x: number, y: number, returnNew?: boolean): Vec2;
-    public add(vec2: number[] | Vec2, returnNew?: boolean): Vec2;
+    add(x: number, y: number, returnNew?: boolean): Vec2;
+    add(vec2: number[] | Vec2, returnNew?: boolean): Vec2;
 
-    public subtract(x: number, y: number, returnNew?: boolean): Vec2;
-    public subtract(vec2: Vec2 | number[], returnNew?: boolean): Vec2;
+    subtract(x: number, y: number, returnNew?: boolean): Vec2;
+    subtract(vec2: Vec2 | number[], returnNew?: boolean): Vec2;
 
     /**
      * Multiply this vectors components with the incoming, returning a clone if `returnNew` is truthy.
      */
-    public multiply(x: number, y: number, returnNew?: boolean): Vec2;
-    public multiply(scalarArrayVec2: number | number[] | Vec2, returnNew?: boolean): Vec2;
+    multiply(x: number, y: number, returnNew?: boolean): Vec2;
+    multiply(scalarArrayVec2: number | number[] | Vec2, returnNew?: boolean): Vec2;
 
     /**
      * Divide this vectors components by the incoming, returning a clone if `returnNew` is truthy.
      * _note_: this method will throw if you attempt to divide by zero or pass values that cause NaNs
      */
-    public divide(x: number, y: number, returnNew?: boolean): Vec2;
-    public divide(scalarArrayVec2: number | number[] | Vec2, returnNew?: boolean): Vec2;
+    divide(x: number, y: number, returnNew?: boolean): Vec2;
+    divide(scalarArrayVec2: number | number[] | Vec2, returnNew?: boolean): Vec2;
 
     /**
      * Rotate this vector's cordinates around `(0,0)`.  If `returnNew` is specified, a new `Vec2` will be created and populated with the result and returned.
@@ -79,63 +79,63 @@ declare class Vec2 {
      * `inverse` - inverts the direction of the rotation
      * `returnNew` - causes the result to be applied to a new `Vec2`, otherwise the result is applied to `this`
      */
-    public rotate(radians: number, inverse?: number, returnNew?: boolean): Vec2;
+    rotate(radians: number, inverse?: number, returnNew?: boolean): Vec2;
 
     /**
      * Returns the length of this vector from `(0,0)`
      */
-    public length(): number;
+    length(): number;
 
     /**
      * Returns the length of this vector prior to the `Math.sqrt` call.
      * This is usefull when you don't need to know the actual distance, but need a normalized value to compare with another `Vec2#lengthSquared` or similar.
      */
-    public lengthSquared(): number;
+    lengthSquared(): number;
 
     /**
      * _returns_: the distance between this vector and the incoming
      */
-    public distance(vec2: Vec2): number;
+    distance(vec2: Vec2): number;
 
     /**
      * _returns_: closest vector in array to this vector.
      */
-    public nearest(array: Vec2[]): Vec2;
+    nearest(array: Vec2[]): Vec2;
 
     /**
      * Normalizes this vector.  If `returnNew` is truthy, a new vector populated with the normalized coordinates will be returned.
      */
-    public normalize(returnNew?: boolean): Vec2;
+    normalize(returnNew?: boolean): Vec2;
 
     /**
      * Returns true if the incoming coordinates are the same as this vector's
      */
-    public equal(x: number, y: number): boolean;
-    public equal(arrayVec2: number[] | Vec2): boolean;
+    equal(x: number, y: number): boolean;
+    equal(arrayVec2: number[] | Vec2): boolean;
 
     /**
      * Return a `Vec2` that contains the absolute value of each of this vector's parts.
      * If `returnNew` is truthy, create a new `Vec2` and return it. Otherwise apply the absolute values to to `this`.
      */
-    public abs(returnNew?: boolean): Vec2;
+    abs(returnNew?: boolean): Vec2;
 
     /**
      * Return a `Vec2` consisting of the smallest values from this vector and the incoming
      * When returnNew is truthy, a new `Vec2` will be returned otherwise the minimum values in either this or `vec` will be applied to this vector.
      */
-    public min(vec: Vec2, returnNew?: boolean): Vec2;
+    min(vec: Vec2, returnNew?: boolean): Vec2;
 
     /**
      * Return a `Vec2` consisting of the largest values from this vector and the incoming
      * When returnNew is truthy, a new `Vec2` will be returned otherwise the maximum values in either `this` or `vec` will be applied to this vector.
      */
-    public max(vec: Vec2, returnNew?: boolean): Vec2;
+    max(vec: Vec2, returnNew?: boolean): Vec2;
 
     /**
      * Clamp the coordinates of this vector to the high/low of the incoming vec2s.  If `returnNew` apply the result to the new vector and return.
      * Otherwise apply to this vector.
      */
-    public clamp(low: Vec2, high: Vec2, returnNew?: boolean): Vec2;
+    clamp(low: Vec2, high: Vec2, returnNew?: boolean): Vec2;
 
     /**
      *
@@ -143,49 +143,49 @@ declare class Vec2 {
      * `amount` - the percentage along the path to place the vector
      * `returnNew` - if `truthy`, apply the result to a new vector and return it, otherwise return `this`
      */
-    public lerp(vec: Vec2, amount: number, returnNew?: boolean): Vec2;
+    lerp(vec: Vec2, amount: number, returnNew?: boolean): Vec2;
 
     /**
      * Returns a vector set with the `(-y,x)` coordinates of this vector.  If `returnNew` a new vector is created and the operation is applied to the new vector.
      */
-    public skew(returnNew?: boolean): Vec2;
+    skew(returnNew?: boolean): Vec2;
 
     /**
      * _returns_: `double`
      */
-    public dot(): number;
+    dot(): number;
 
     /**
      * _returns_: `double`
      */
-    public perpDot(): number;
+    perpDot(): number;
 
     /**
      * Returns the angle from this vector to the incoming.
      */
-    public angleTo(vec: Vec2): number;
+    angleTo(vec: Vec2): number;
 
     /**
      * Where `start` and `end` are vec2-like (e.g. `start.x` and `start.y`)
      */
-    public isPointOnLine(start: Vec2, end: Vec2): boolean;
+    isPointOnLine(start: Vec2, end: Vec2): boolean;
 
     /**
      * _returns_: `[x, y]`
      */
-    public toArray(): number[];
+    toArray(): number[];
 
     /**
      * Applies the `[0]` to `this.x` and `[1]` to `this.y`
      */
-    public fromArray(array: number[]): Vec2;
+    fromArray(array: number[]): Vec2;
 
-    public toJSON(): { x: number, y: number };
+     toJSON(): { x: number, y: number };
 
     /**
      * _returns_: `'(x, y)'`
      */
-    public toString(): string;
+     toString(): string;
 }
 
 export default Vec2;
