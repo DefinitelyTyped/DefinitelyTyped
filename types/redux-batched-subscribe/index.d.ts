819546fdacefe7912d6274bb5d9587e570659870
Aziz Khambati <aziz.h.khambati@gmail.com>
2019-03-12 00:11:04 +0530

    [redux-batched-subscribe] Update redux version

--- types/redux-batched-subscribe/index.d.ts
+++ types/redux-batched-subscribe/index.d.ts
@@ -1,11 +1,16 @@
 // Type definitions for redux-batched-subscribe 0.1
 // Project: https://github.com/tappleby/redux-batched-subscribe
 // Definitions by: Dibyo Majumdar <https://github.com/mDibyo>
+//                 Aziz Khambati <https://github.com/azizhk>
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
 
-import { GenericStoreEnhancer } from 'redux';
+import { StoreEnhancer } from "redux";
 
 export type NotifyFunction = () => void;
 export type BatchFunction = (notify: NotifyFunction) => void;
 
-export function batchedSubscribe(batch: BatchFunction): GenericStoreEnhancer;
+export interface StoreExtension {
+    subscribeImmediate: () => void;
+}
+
+export function batchedSubscribe(batch: BatchFunction): StoreEnhancer<StoreExtension>;
