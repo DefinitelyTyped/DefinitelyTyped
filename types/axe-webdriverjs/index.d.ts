b0ddea4f92d95a4432f1f38f92a7bc51fc133fd5
Tyler Krupicka <Tyler_Krupicka@intuit.com>
2019-03-10 20:41:32 -0700

    axe-webdriverjs: update authors based on new linting rules

--- types/axe-webdriverjs/index.d.ts
+++ types/axe-webdriverjs/index.d.ts
@@ -1,6 +1,7 @@
 // Type definitions for axe-webdriverjs 2.1
 // Project: https://github.com/dequelabs/axe-webdriverjs#readme
-// Definitions by: My Self <https://github.com/JoshuaKGoldberg>
+// Definitions by: Joshua Goldberg <https://github.com/JoshuaKGoldberg>
+//                Tyler Krupicka <https://github.com/tylerkrupicka>
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
 // TypeScript Version: 2.3
 
