4e7314fbd4242323cfd1403690d69b2a05048666
Spencer Miskoviak <smiskoviak@gmail.com>
2019-03-11 19:02:50 -0700

    Review feedback: remove namespace, esModuleInterop and add KEYWORDS

--- types/reserved-words/index.d.ts
+++ types/reserved-words/index.d.ts
@@ -4,33 +4,37 @@
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
 // TypeScript Version: 3.0
 
-declare namespace reserved {
-    type Dialect =
-        | "es3"
-        | 3
-        | "es5"
-        | 5
-        | "es2015"
-        | 6
-        | "es7"
-        | 7
-        | "es6"
-        | "next";
+export type Dialect =
+    | "es3"
+    | 3
+    | "es5"
+    | 5
+    | "es2015"
+    | 6
+    | "es7"
+    | 7
+    | "es6"
+    | "next";
+
+export interface Keywords {
+    [index: string]: {
+        [index: string]: true;
+    };
 }
 
-interface Reserved {
-    /**
-     * Checks a word for being a reserved word and returns true if the provided
-     * identifier string is a reserved word in some ECMAScript dialect.
-     *
-     * @param word word to check
-     * @param dialect dialect or version
-     * @param strict strict mode additionally checks whether word is a keyword or
-     * future reserved word under strict mode.
-     */
-    check(word: string, dialect?: reserved.Dialect, strict?: boolean): boolean;
-}
+export const KEYWORDS: Keywords;
 
-declare const reserved: Reserved;
-export = reserved;
-export as namespace reserved;
+/**
+ * Checks a word for being a reserved word and returns true if the provided
+ * identifier string is a reserved word in some ECMAScript dialect.
+ *
+ * @param word word to check
+ * @param dialect dialect or version
+ * @param strict strict mode additionally checks whether word is a keyword or
+ * future reserved word under strict mode.
+ */
+export function check(
+    word: string,
+    dialect?: Dialect,
+    strict?: boolean
+): boolean;
--- types/reserved-words/reserved-words-tests.ts
+++ types/reserved-words/reserved-words-tests.ts
@@ -1,21 +1,24 @@
-import reserved, { Dialect } from "reserved-words";
+import { check, Dialect, KEYWORDS } from "reserved-words";
 
 const dialect: Dialect = "es3";
 
 // $ExpectType boolean
-reserved.check("while");
+check("while");
 
 // $ExpectType boolean
-reserved.check("while", "es6");
+check("while", "es6");
 
 // $ExpectType boolean
-reserved.check("while", "es6", true);
+check("while", "es6", true);
 
 // $ExpectType boolean
-reserved.check("yield", 3);
+check("yield", 3);
 
 // $ExpectError
-reserved.check("yield", 1);
+check("yield", 1);
 
 // $ExpectError
-reserved.check("yield", "es4");
+check("yield", "es4");
+
+// $ExpectType true
+KEYWORDS["6-strict"]["break"];
