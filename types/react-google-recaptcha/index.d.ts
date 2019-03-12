036c90be607c9dfc8c3c50a6ca33c1f90ef53c18
Max Bo <mbo@atlassian.com>
2019-03-12 13:30:38 +1100

    Add Max Bo to react-google-recaptcah definitions file

--- types/react-google-recaptcha/index.d.ts
+++ types/react-google-recaptcha/index.d.ts
@@ -1,6 +1,8 @@
 // Type definitions for React Google Recaptcha 1.0
 // Project: https://github.com/dozoisch/react-google-recaptcha
-// Definitions by: Koala Human <https://github.com/KoalaHuman>, Tom Sturge <https://github.com/tomsturge>
+// Definitions by: Koala Human <https://github.com/KoalaHuman>
+//                 Tom Sturge <https://github.com/tomsturge>
+//                 Max Bo <https://github.com/MaxwellBo>
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
 // TypeScript Version: 2.8
 
