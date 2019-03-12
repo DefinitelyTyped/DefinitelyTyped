d11cb50ef1e5ea929f9beb80700c0ded58612f90
Aziz Khambati <aziz.h.khambati@gmail.com>
2019-03-12 11:22:29 +0530

    Add subscribeImmediate tests

--- types/redux-batched-subscribe/index.d.ts
+++ types/redux-batched-subscribe/index.d.ts
@@ -4,13 +4,13 @@
 //                 Aziz Khambati <https://github.com/azizhk>
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
 
-import { StoreEnhancer } from "redux";
+import { Store, StoreEnhancer, Action, AnyAction } from "redux";
 
 export type NotifyFunction = () => void;
 export type BatchFunction = (notify: NotifyFunction) => void;
 
-export interface StoreExtension {
-    subscribeImmediate: () => void;
+export interface StoreExtension<S = any, A extends Action = AnyAction> {
+    subscribeImmediate: Store<S, A>["subscribe"];
 }
 
-export function batchedSubscribe(batch: BatchFunction): StoreEnhancer<StoreExtension>;
+export function batchedSubscribe<S = any, A extends Action = AnyAction>(batch: BatchFunction): StoreEnhancer<StoreExtension<S, A>>;
