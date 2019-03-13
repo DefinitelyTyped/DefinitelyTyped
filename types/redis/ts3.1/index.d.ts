e4c6c89f61a63202314a13ff9c156542290bd5e8
Adebayo Opesanya <bayoopesanya@gmail.com>
2019-03-13 19:19:34 +0100

    remove header from submodule

--- types/redis/ts3.1/index.d.ts
+++ types/redis/ts3.1/index.d.ts
@@ -1,20 +1,3 @@
-// Type definitions for redis 2.8
-// Project: https://github.com/NodeRedis/node_redis
-// Definitions by: Carlos Ballesteros Velasco <https://github.com/soywiz>
-//                 Peter Harris <https://github.com/CodeAnimal>
-//                 TANAKA Koichi <https://github.com/MugeSo>
-//                 Stuart Schechter <https://github.com/UppaJung>
-//                 Junyoung Choi <https://github.com/Rokt33r>
-//                 James Garbutt <https://github.com/43081j>
-//                 Bartek Szczepański <https://github.com/barnski>
-//                 Pirasis Leelatanon <https://github.com/1pete>
-//                 Stanislav Dzhus <https://github.com/blablapolicja>
-//                 Jake Ferrante <https://github.com/ferrantejake>
-//                 Adebayo Opesanya <https://github.com/OpesanyaAdebayo>
-// Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
-
-// Imported from: https://github.com/types/npm-redis
-
 /// <reference types="node" />
 
 import { EventEmitter } from 'events';
