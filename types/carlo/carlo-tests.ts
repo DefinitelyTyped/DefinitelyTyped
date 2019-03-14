4443dc8aa73d90b21c9995974a6a625fac14c1a8
Adam Dierkens <adam@dierkens.com>
2019-03-13 18:14:22 -0700

    lint

--- types/carlo/carlo-tests.ts
+++ types/carlo/carlo-tests.ts
@@ -17,16 +17,16 @@ async function main() {
 (async () => {
     // Launch the browser.
     const app = await carlo.launch();
-   
+
     // Terminate Node.js process on app window closing.
     app.on('exit', () => process.exit());
-   
+
     // Tell carlo where your web files are located.
     app.serveFolder(__dirname);
-   
+
     // Expose 'env' function in the web environment.
     await app.exposeFunction('env', _ => process.env);
-   
+
     // Navigate to the main page of your app.
     await app.load('example.html');
-  })();
+})();
