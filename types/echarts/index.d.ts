d4da8c89582b5c9d6f924e1b6b983334409eae90
Dustin Whitney <dustin.whitney@gmail.com>
2019-03-14 11:27:01 -0400

    added dwhitney as a contributor

--- types/echarts/index.d.ts
+++ types/echarts/index.d.ts
@@ -7,6 +7,7 @@
 //                 Roman <https://github.com/iRON5>
 //                 Bilal <https://github.com/bilalucar>
 //                 TMTron <https://github.com/tmtron>
+//                 dwhitney <https://github.com/dwhitney>
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
 // TypeScript Version: 2.3
 
