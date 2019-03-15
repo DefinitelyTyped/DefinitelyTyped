2492054ca346f1086c6437e1c2ad5bcb6493309d
Haseeb Majid <hmajid2301@gmail.com>
2019-03-15 23:01:30 +0000

    Updated Definitions

--- types/react-native-canvas/index.d.ts
+++ types/react-native-canvas/index.d.ts
@@ -7,10 +7,96 @@
 import * as React from "react";
 import { StyleProp, ViewStyle } from "react-native";
 
+export interface IImageData {
+    data: number[];
+    height: number;
+    width: number;
+}
+
+export interface CanvasRenderingContext2D {
+    fillStyle: string;
+    font: string;
+    globalAlpha: number;
+    globalCompositionOperation: string;
+    linecap: string;
+    lineDashOffset: number;
+    lineJoin: string;
+    lineWidth: number;
+    miterLimit: number;
+    shadowBlur: number;
+    shadowColor: string;
+    shadowOffsetX: number;
+    shadowOffsetY: number;
+    strokeStyle: string;
+    textAlign: string;
+    arc: (
+        x: number,
+        y: number,
+        r: number,
+        sAngle: number,
+        eAngle: number,
+        counterClockwise?: number
+    ) => void;
+    arcTo: (x1: number, y1: number, x2: number, y2: number, r: number) => void;
+    beginPath: () => void;
+    bezierCurveTo: (
+        cp1x: number,
+        cp1y: number,
+        cp2x: number,
+        cp2y: number,
+        x: number,
+        y: number
+    ) => void;
+    clearRect: (x: number, y: number, width: number, height: number) => void;
+    clip: () => void;
+    closePath: () => void;
+    createImageData: (
+        width: number,
+        height: number,
+        imageData: IImageData
+    ) => void;
+    createLinearGradient;
+    createPattern;
+    createRadialGradient;
+    drawFocusIfNeeded;
+    drawImage;
+    drawWidgetAsOnScreen;
+    drawWindow;
+    fill;
+    fillRect: (x: number, y: number, width: number, height: number) => void;
+    fillText;
+    getImageData;
+    getLineDash;
+    isPointInPath;
+    isPointInStroke;
+    lineTo;
+    measureText;
+    moveTo;
+    putImageData;
+    quadraticCurveTo;
+    rect;
+    restore;
+    rotate;
+    save;
+    scale;
+    setLineDash;
+    setTransform;
+    stroke;
+    strokeRect;
+    strokeText;
+    transform;
+    translate;
+}
+
+export interface ICanvas {
+    getContext: (context: string) => CanvasRenderingContext2D;
+}
+
 export interface CanvasProps {
-    style: StyleProp<ViewStyle>;
-    baseUrl: string;
-    originWhitelist: string[];
+    baseUrl?: string;
+    originWhitelist?: string[];
+    ref: (canvas: ICanvas) => void;
+    style?: StyleProp<ViewStyle>;
 }
 
 export default class Canvas extends React.Component<CanvasProps> {}
--- types/react-native-canvas/react-native-canvas-tests.tsx
+++ types/react-native-canvas/react-native-canvas-tests.tsx
@@ -1,6 +1,7 @@
 import * as React from "react";
 import { View } from "react-native";
 import Canvas, {
+    ICanvas,
     Image as CanvasImage,
     Path2D,
     ImageData
@@ -15,5 +16,29 @@ class CanvasTest extends React.Component {
         );
     }
 
-    handleCanvas = (canvas: Canvas) => {};
+    handleCanvas = (canvas: ICanvas) => {
+        canvas.width = 100;
+        canvas.height = 100;
+
+        const context = canvas.getContext("2d");
+        context.fillStyle = "purple";
+        context.fillRect(0, 0, 100, 100);
+
+        const ellipse = new Path2D(canvas);
+        ellipse.ellipse(50, 50, 25, 35, (45 * Math.PI) / 180, 0, 2 * Math.PI);
+        context.fillStyle = "purple";
+        context.fill(ellipse);
+
+        context.getImageData(0, 0, 100, 100).then((imageData: ImageData) => {
+            const data = Object.values(imageData.data);
+            const length = Object.keys(data).length;
+            for (let i = 0; i < length; i += 4) {
+                data[i] = 0;
+                data[i + 1] = 0;
+                data[i + 2] = 0;
+            }
+            const imgData = new ImageData(canvas, data, 100, 100);
+            context.putImageData(imgData, 0, 0);
+        });
+    };
 }
