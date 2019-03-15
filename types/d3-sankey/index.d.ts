0a90d3ca198db1b2157c260984d27a2da9b22661
Jarrett Meyer <meyer_jarrett@lilly.com>
2019-03-15 13:53:21 -0400

    Fixes version in index.d.ts header

--- types/d3-sankey/index.d.ts
+++ types/d3-sankey/index.d.ts
@@ -1,10 +1,10 @@
-// Type definitions for D3JS d3-sankey module 0.11.0
+// Type definitions for D3JS d3-sankey module 0.11
 // Project: https://github.com/d3/d3-sankey/
 // Definitions by: Tom Wanzek <https://github.com/tomwanzek>, Alex Ford <https://github.com/gustavderdrache>
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
 // TypeScript Version: 2.3
 
-// Last module patch version validated against: 0.11.0
+// Last module patch version validated against: 0.11
 
 import { Link } from 'd3-shape';
 
