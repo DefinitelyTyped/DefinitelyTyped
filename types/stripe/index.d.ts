2110f7b4236f696cf980a02c272f07efffd9eb90
Raúl Acuña <reacuna@crac.cl>
2019-03-14 22:41:28 -0300

    Add created property

--- types/stripe/index.d.ts
+++ types/stripe/index.d.ts
@@ -3906,6 +3906,12 @@ declare namespace Stripe {
              * Only return products of this type
              */
             type?: string;
+
+            /**
+             * A filter on the list based on the object created field. The value can be a string with an integer Unix timestamp,
+             * or it can be a dictionary with the following options:
+             */
+            created?: IDateFilter;
         }
 
         /**
