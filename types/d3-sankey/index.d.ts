49311e0283a9859a79d2cdd70ed52163204a30a3
Jarrett Meyer <jarrettmeyer@gmail.com>
2019-03-15 10:36:26 -0400

    Bump version in first line

--- types/d3-sankey/index.d.ts
+++ types/d3-sankey/index.d.ts
@@ -1,4 +1,4 @@
-// Type definitions for D3JS d3-sankey module 0.7
+// Type definitions for D3JS d3-sankey module 0.11.0
 // Project: https://github.com/d3/d3-sankey/
 // Definitions by: Tom Wanzek <https://github.com/tomwanzek>, Alex Ford <https://github.com/gustavderdrache>
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
