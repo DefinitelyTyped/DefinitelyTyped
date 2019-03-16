3005a2072189b898ab8054278b26772a64c4aade
Marko Harjula <marko.harjula@gmail.com>
2019-03-16 08:19:03 +0200

    added test

--- types/node-forge/node-forge-tests.ts
+++ types/node-forge/node-forge-tests.ts
@@ -343,3 +343,7 @@ if (forge.util.fillString('1', 5) !== '11111') throw Error('forge.util.fillStrin
     console.log('created TLS client and server, doing handshake...');
     client.handshake();
 }
+
+{
+    forge.pki.publicKeyToRSAPublicKeyPem(keypair.publicKey, 64);
+}
