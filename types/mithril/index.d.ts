03a179573d113c74ab2161c2412995f725fbf055
Zhen Wang <z@zmxv.com>
2019-03-16 15:00:43 +0800

    [types/mithril] add type definitions for mithril/ospec with tests

--- types/mithril/index.d.ts
+++ types/mithril/index.d.ts
@@ -1,6 +1,9 @@
 // Type definitions for Mithril 1.1
 // Project: https://mithril.js.org/, https://github.com/mithriljs/mithril.js
-// Definitions by: Mike Linkovich <https://github.com/spacejack>, András Parditka <https://github.com/andraaspar>, Isiah Meadows <https://github.com/isiahmeadows>
+// Definitions by: Mike Linkovich <https://github.com/spacejack>
+//                 András Parditka <https://github.com/andraaspar>
+//                 Isiah Meadows <https://github.com/isiahmeadows>
+//                 Zhen Wang <https://github.com/zmxv>
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
 // TypeScript Version: 2.3
 
