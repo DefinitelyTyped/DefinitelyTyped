f6aecc14f1ada885d870a525c73be6ddf83a0206
Johan Davidsson <johan.davidsson@trewit.se>
2019-03-18 12:19:49 +0100

    Add ParserOptions

--- types/sanitize-html/index.d.ts
+++ types/sanitize-html/index.d.ts
@@ -9,7 +9,7 @@
 //                 Johan Davidsson <https://github.com/johandavidson>
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
 
-import {Options} from "htmlparser2";
+import {Options,ParserOptions} from "htmlparser2";
 
 export = sanitize;
 
