cef82d6e7c1986d43a5fac69c377771fe684209a
Satya Rohith <gsatyarohith@gmail.com>
2019-03-18 21:11:31 +0530

    fix: use interface instead of object (#33972)

--- types/get-res/index.d.ts
+++ types/get-res/index.d.ts
@@ -6,9 +6,14 @@
 
 export = getRes;
 
+interface Resolution {
+    item: string;
+    percent: string;
+}
+
 /**
  * Get ten most popular screen resolutions
  *
  * @returns An array with the details of ten most popular screen resolutions
  */
-declare function getRes(): Promise<object[]>;
+declare function getRes(): Promise<Resolution[]>;
