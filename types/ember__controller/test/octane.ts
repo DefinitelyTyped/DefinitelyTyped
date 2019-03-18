59c441bc457f89cc5001da97c8c7ee6e8b2967fe
Mike North <michael.l.north@gmail.com>
2019-03-18 08:44:39 -0700

    [ember] ember-octane decorator types (#33963)
    * [@ember/object] types for @action and @computed decorators
    
    * [@ember/service] add support for ember-octane @inject decorator
    
    * [@ember/object] absolute import in tests
    
    * [@ember/controller] controller @inject decorator for ember-octane
    
    * [ember] lint fixes
    
    * [@ember/object] decorators for computed property macros in ember-octane

---
+++ types/ember__controller/test/octane.ts
@@ -0,0 +1,26 @@
+import Controller, { inject } from "@ember/controller";
+
+class FirstController extends Controller {
+    foo = "bar";
+    @inject second: InstanceType<typeof SecondController>;
+    @inject() otherSecond: InstanceType<typeof SecondController>;
+    @inject("second") moreSecond: InstanceType<typeof SecondController>;
+
+    first() {
+        return "";
+    }
+}
+const SecondController = Controller.extend({
+    foo: "bar",
+
+    second() {
+        return "";
+    }
+});
+
+declare module "@ember/controller" {
+    interface Registry {
+        first: FirstController;
+        second: InstanceType<typeof SecondController>;
+    }
+}
