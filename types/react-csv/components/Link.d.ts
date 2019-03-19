1f8ed6f5ad779578da893f39af5bdb2b28c9a73a
James Lismore <jlismore@gmail.com>
2019-03-19 16:48:21 -0400

    Extend anchor props

--- types/react-csv/components/Link.d.ts
+++ types/react-csv/components/Link.d.ts
@@ -1,5 +1,15 @@
 import { Component } from "react";
 import { CommonPropTypes } from "./CommonPropTypes";
 
-export default class Link extends Component<CommonPropTypes> {
-}
+type Omit<T, K extends keyof T> = Pick<T, Exclude<keyof T, K>>;
+
+interface LinkProps
+    extends CommonPropTypes,
+        Omit<
+            React.DetailedHTMLProps<
+                React.AnchorHTMLAttributes<HTMLAnchorElement>,
+                HTMLAnchorElement
+            >,
+            "onClick"
+        > {}
+export default class Link extends Component<LinkProps> {}
