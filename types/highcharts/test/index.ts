0f7add13ba328dabad5af4f841417fce64a96213
arunksan <arunksan@cisco.com>
2019-03-20 13:23:59 -0700

    @types/highcharts: dataLabels support Highcharts.ganttChart()

--- types/highcharts/test/index.ts
+++ types/highcharts/test/index.ts
@@ -2886,3 +2886,46 @@ function test_GanttChart() {
 		}],
 	});
 }
+
+// Tests DataLabels types for Highcharts.ganttChart, which uses the Gantt module
+function test_GanttChartDataLabels() {
+	Highcharts.ganttChart('ganttChartContainer', {
+		xAxis: {
+			min: Date.UTC(2014, 9, 18),
+			max: Date.UTC(2014, 12, 20)
+		},
+		series: [{
+			name: 'Project 1',
+			data: [
+				{
+					name: 'Lemon Tea',
+					start: Date.UTC(2014, 10, 18),
+					end: Date.UTC(2014, 11, 20)
+				},
+				{
+					name: 'Stapler',
+					start: Date.UTC(2014, 11, 18),
+					end: Date.UTC(2014, 12, 20)
+				},
+				{
+					name: 'Sierra',
+					start: Date.UTC(2014, 12, 18),
+					end: Date.UTC(2014, 12, 20)
+				},
+            ],
+            dataLabels: [{
+                enabled: true,
+                format: '<span>Data Label Left</span>',
+                useHTML: true,
+                align: 'left',
+                allowOverlap: true,
+              }, {
+                enabled: true,
+                format: '<span>Data Label Right</span>',
+                useHTML: true,
+                align: 'right',
+                allowOverlap: true,
+              }]
+		}],
+	});
+}
