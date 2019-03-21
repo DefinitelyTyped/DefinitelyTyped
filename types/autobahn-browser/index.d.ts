0612e5428cf05c89422cc53544b4745867d5fe71
Attila Papai <attilapapai@glorfindel>
2019-03-21 15:51:34 +0100

    Fix dt-header version issue

--- types/autobahn-browser/index.d.ts
+++ types/autobahn-browser/index.d.ts
@@ -1,4 +1,4 @@
-// Type definitions for AutobahnJS 18.10 browser build
+// Type definitions for AutobahnJS Browser Build 18.10
 // Project: https://crossbar.io/autobahn/, https://github.com/crossbario/autobahn-js-browser
 // Definitions by: Elad Zelingher <https://github.com/darkl>, Andy Hawkins <https://github.com/a904guy>, Wladimir Totino <https://github.com/valepu>, Mathias Teier <https://github.com/glenroy37>, Fran Rodriguez <https://github.com/spcfran>
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
