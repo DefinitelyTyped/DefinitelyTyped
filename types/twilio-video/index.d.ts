b775888cb28b7d4e737367c94c38a6564647a264
ktsn <ktsn55@gmail.com>
2019-03-21 14:19:39 +0800

    Update definitions by on twilio-video

--- types/twilio-video/index.d.ts
+++ types/twilio-video/index.d.ts
@@ -1,6 +1,8 @@
 // Type definitions for twilio-video 2.0
 // Project: https://twilio.com/video, https://twilio.com
-// Definitions by: MindDoc <https://github.com/minddocdev>, Darío Blanco <https://github.com/darioblanco>
+// Definitions by: MindDoc <https://github.com/minddocdev>
+//                 Darío Blanco <https://github.com/darioblanco>
+//                 katashin <https://github.com/ktsn>
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
 // TypeScript Version: 2.3
 
