bd5ed90116571b2fe2ddeee1180875901c356f4f
Leon Thorne <ldthorne96@gmail.com>
2019-03-21 13:59:50 -0400

    Remove www subdomain in contributor URL

--- types/zeit__next-source-maps/index.d.ts
+++ types/zeit__next-source-maps/index.d.ts
@@ -1,6 +1,6 @@
 // Type definitions for @zeit/next-source-maps 0.0
 // Project: https://github.com/zeit/next-plugins/tree/master/packages/next-source-maps, https://github.com/zeit/next-plugins
-// Definitions by: ldthorne <https://www.github.com/ldthorne>
+// Definitions by: ldthorne <https://github.com/ldthorne>
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
 // TypeScript Version: 3.3
 
