e346a41f6e13975eaac2ba115b7a0f17dff671ed
Leon Thorne <ldthorne96@gmail.com>
2019-03-21 12:27:45 -0400

    Add types for @zeit/next-source-maps

---
+++ types/zeit__next-source-maps/index.d.ts
@@ -0,0 +1,14 @@
+// Type definitions for @zeit/next-typescript 0.0.4-canary.1
+// Project: https://github.com/zeit/next-plugins/tree/master/packages/next-source-maps, https://github.com/zeit/next-plugins
+// Definitions by: ldthorne <https://www.github.com/ldthorne>
+// Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
+// TypeScript Version: 3.3
+
+import { ServerConfig } from 'next';
+
+declare function withSourceMaps(
+    /** @default {} */
+    nextConfig?: ServerConfig
+): ServerConfig;
+
+export = withSourceMaps;
