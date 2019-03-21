d57932f468726cdece8e7957aa90fd81139772c6
Leon Thorne <ldthorne96@gmail.com>
2019-03-21 13:46:41 -0400

    Remove canary version

--- types/zeit__next-source-maps/index.d.ts
+++ types/zeit__next-source-maps/index.d.ts
@@ -1,4 +1,4 @@
-// Type definitions for @zeit/next-source-maps 0.0.4-canary.1
+// Type definitions for @zeit/next-source-maps 0.0.4
 // Project: https://github.com/zeit/next-plugins/tree/master/packages/next-source-maps, https://github.com/zeit/next-plugins
 // Definitions by: ldthorne <https://www.github.com/ldthorne>
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
