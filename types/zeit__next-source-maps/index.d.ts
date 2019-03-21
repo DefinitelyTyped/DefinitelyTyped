615a24f5c972bc82a001a21d9262054c23c5052e
Leon Thorne <ldthorne96@gmail.com>
2019-03-21 13:56:10 -0400

    Remove patch version

--- types/zeit__next-source-maps/index.d.ts
+++ types/zeit__next-source-maps/index.d.ts
@@ -1,4 +1,4 @@
-// Type definitions for @zeit/next-source-maps 0.0.4
+// Type definitions for @zeit/next-source-maps 0.0
 // Project: https://github.com/zeit/next-plugins/tree/master/packages/next-source-maps, https://github.com/zeit/next-plugins
 // Definitions by: ldthorne <https://www.github.com/ldthorne>
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
