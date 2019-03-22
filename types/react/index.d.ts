61c8fdb680bc338c286c63bd83c5cb953ee889d8
ZHAO Jinxiang <xiaoxiangmoe@gmail.com>
2019-03-22 18:00:37 +0800

    using PropsWithChildren in Component.props

--- types/react/index.d.ts
+++ types/react/index.d.ts
@@ -426,7 +426,7 @@ declare namespace React {
         // always pass children as variadic arguments to `createElement`.
         // In the future, if we can define its call signature conditionally
         // on the existence of `children` in `P`, then we should remove this.
-        readonly props: Readonly<P> & Readonly<{ children?: ReactNode }>;
+        readonly props: Readonly<PropsWithChildren<P>>;
         state: Readonly<S>;
         /**
          * @deprecated
