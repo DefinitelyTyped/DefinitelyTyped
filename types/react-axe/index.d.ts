800807486ce3619a4ac2f447315ed0a52fa4a83a
Akshay Karthik <akshay.karthik@gmail.com>
2019-03-22 10:21:25 -0400

    Remove the v from the version header

--- types/react-axe/index.d.ts
+++ types/react-axe/index.d.ts
@@ -1,4 +1,4 @@
-// Type definitions for react-axe v3.1
+// Type definitions for react-axe 3.1
 // Project: https://github.com/dequelabs/react-axe
 // Definitions by: Akshay Karthik <https://github.com/akshaykarthik>
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
