d7a1bc51f83cde356a84c02109dcf820bfd81941
Haseeb Majid <hmajid2301@gmail.com>
2019-03-22 19:24:39 +0000

    Added Changes

--- types/react-native-uuid/index.d.ts
+++ types/react-native-uuid/index.d.ts
@@ -2,34 +2,27 @@
 // Project: https://github.com/eugenehp/react-native-uuid
 // Definitions by: hmajid2301 <https://github.com/hmajid2301>
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
-
+// TypeScript Version: 3.1
 
 interface v1 {
-    options?: {
-        node: number[],
-        clockseq: number,
-        msec: number | Date,
-        nsec: number
-    },
-    buffer?: ArrayBuffer,
-    offset?: number
+    node: number[];
+    clockseq: number;
+    msecs: number | Date;
+    nsecs: number;
 }
 
 interface v4 {
-    options?: {
-        random: number[],
-        rng: (...args: any) => void,
-    },
-    buffer?: ArrayBuffer,
-    offset?: number
+    random?: number[];
+    rng?: (...args: any) => void;
 }
 
-interface UUID {
-    v1: (options: v1) => string;
-    v4: (options: v4) => string;
+export interface uuid {
+    v1: (options?: v1, buffer?: ArrayBuffer, offset?: number) => string;
+    v4: (options?: v4, buffer?: ArrayBuffer, offset?: number) => string;
     parse: (id: string, buffer?: ArrayBuffer, offset?: number) => ArrayBuffer;
-    unparse: (id: string, buffer?: ArrayBuffer, offset?: number) => string;
-    noConflict: () => void;
+    unparse: (buffer?: ArrayBuffer, offset?: number) => string;
+    noConflict: () => string;
 }
 
-export const uuid: UUID;
+declare const uuid: uuid;
+export default uuid;
