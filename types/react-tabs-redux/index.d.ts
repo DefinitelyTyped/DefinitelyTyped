26ee14fa13ecf40bdc52cdc962bdd858dfd5c4c6
Anton Bortnikov <6088046+ntnbrtnkv@users.noreply.github.com>
2019-03-23 16:49:51 +0500

    feat: add react-tabs-redux types

---
+++ types/react-tabs-redux/index.d.ts
@@ -0,0 +1,8 @@
+// Type definitions for react-tabs-redux 4.0
+// Project: https://github.com/patrik-piskay/react-tabs-redux#readme
+// Definitions by: Anton Bortnikov <https://github.com/ntnbrtnkv>
+// Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
+
+export {Tabs, TabsProps} from './Tabs';
+export {TabLink, TabLinkProps} from './TabLink';
+export {TabContent, TabContentProps} from './TabContent';
