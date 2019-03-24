1efbc96aec58b215f54da629f859acc11a976822
Eugene Zaretskiy <eugene.zar@gmail.com>
2019-03-24 01:01:09 -0700

    Adding attribution

--- types/seedrandom/index.d.ts
+++ types/seedrandom/index.d.ts
@@ -1,6 +1,6 @@
 // Type definitions for seedrandom 2.4.2
 // Project: https://github.com/davidbau/seedrandom
-// Definitions by: Kern Handa <https://github.com/kernhanda>
+// Definitions by: Kern Handa <https://github.com/kernhanda>, Eugene Zaretskiy <https://github.com/EugeneZ>
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
 
 declare namespace seedrandom {
