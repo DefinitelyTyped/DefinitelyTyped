771d4224e06c88b640742542d4bbbad0735f3b10
Claas Ahlrichs <claasahl@gmail.com>
2019-03-24 18:20:43 +0100

    drafted types for align-text

--- types/align-text/index.d.ts
+++ types/align-text/index.d.ts
@@ -1,15 +1,35 @@
 // Type definitions for align-text 1.0
 // Project: https://github.com/jonschlinkert/align-text
-// Definitions by: My Self <https://github.com/me>
+// Definitions by: Claas Ahlrichs <https://github.com/claasahl>
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
 
 export = align_text;
 
-declare function align_text(val: any, fn: any): any;
-
-declare namespace align_text {
-    const prototype: {
-    };
+interface TransformResult {
+    /**
+     * the amount of indentation to use. Default is 0 when an object is returned.
+     */
+    indent: number
+    /**
+     * the character to use for indentation. Default is '' (empty string) when an object is returned.
+     */
+    character: string
+    /**
+     * leading characters to use at the beginning of each line. '' (empty string) when an object is returned.
+     */
+    prefix: string
 
 }
 
+interface Callback {
+    /**
+     * @param len the length of the "current" line
+     * @param longest the length of the longest line
+     * @param line the current line (string) being aligned
+     * @param lines the array of all lines
+     */
+    (len: number, longest: number, line: string, lines: string[]): number | TransformResult
+}
+
+declare function align_text(text: string, fn: number | Callback): string;
+declare function align_text(text: string[], fn: number | Callback): string[];
