508656ca28ac2bb4dfff6f83a1fc9591d7029254
Claas Ahlrichs <claasahl@gmail.com>
2019-03-24 18:10:18 +0100

    npx dts-gen -m align-text --dt

---
+++ types/align-text/index.d.ts
@@ -0,0 +1,15 @@
+// Type definitions for align-text 1.0
+// Project: https://github.com/jonschlinkert/align-text
+// Definitions by: My Self <https://github.com/me>
+// Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
+
+export = align_text;
+
+declare function align_text(val: any, fn: any): any;
+
+declare namespace align_text {
+    const prototype: {
+    };
+
+}
+
