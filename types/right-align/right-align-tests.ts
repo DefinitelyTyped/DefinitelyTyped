6049d5cd126cd8ba01bedf5b3d28153808881c5d
Claas Ahlrichs <claasahl@gmail.com>
2019-03-25 20:44:11 +0100

    npx prettier --write .\types\right-align\**

--- types/right-align/right-align-tests.ts
+++ types/right-align/right-align-tests.ts
@@ -1,12 +1,14 @@
 import rightAlign from "right-align";
 
 // multi-line sample
-const lines = ["Lorem ipsum dolor sit amet,",
-"consectetur adipiscing",
-"elit, sed do eiusmod tempor incididunt",
-"ut labore et dolore",
-"magna aliqua. Ut enim ad minim",
-"veniam, quis"];
+const lines = [
+    "Lorem ipsum dolor sit amet,",
+    "consectetur adipiscing",
+    "elit, sed do eiusmod tempor incididunt",
+    "ut labore et dolore",
+    "magna aliqua. Ut enim ad minim",
+    "veniam, quis"
+];
 rightAlign(lines);
 
 // single-line sample
