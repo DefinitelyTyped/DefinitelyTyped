60a51581cb4aaad77cd9ea2e5db86a4af867e3f0
Claas Ahlrichs <claasahl@gmail.com>
2019-03-25 20:51:07 +0100

    npx dts-gen -m center-align --dt

---
+++ types/center-align/index.d.ts
@@ -0,0 +1,15 @@
+// Type definitions for center-align 1.0
+// Project: https://github.com/jonschlinkert/center-align
+// Definitions by: My Self <https://github.com/me>
+// Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
+
+export = center_align;
+
+declare function center_align(val: any, width: any): any;
+
+declare namespace center_align {
+    const prototype: {
+    };
+
+}
+
