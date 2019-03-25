6be6d2279808874fb5ffdf650d49ca36a6054f7d
Claas Ahlrichs <claasahl@gmail.com>
2019-03-25 20:43:30 +0100

    drafted types for right-align

--- types/right-align/index.d.ts
+++ types/right-align/index.d.ts
@@ -1,15 +1,9 @@
 // Type definitions for right-align 0.1
 // Project: https://github.com/jonschlinkert/right-align
-// Definitions by: My Self <https://github.com/me>
+// Definitions by: Claas Ahlrichs <https://github.com/claasahl>
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
 
 export = right_align;
 
-declare function right_align(val: any): any;
-
-declare namespace right_align {
-    const prototype: {
-    };
-
-}
-
+declare function right_align(val: string): string;
+declare function right_align(val: string[]): string[];
