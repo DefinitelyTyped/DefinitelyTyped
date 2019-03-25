11cf4763d781785347f85baed4dea8640ef3afe2
Claas Ahlrichs <claasahl@gmail.com>
2019-03-25 20:46:39 +0100

    fixed linting issues

--- types/right-align/index.d.ts
+++ types/right-align/index.d.ts
@@ -2,8 +2,9 @@
 // Project: https://github.com/jonschlinkert/right-align
 // Definitions by: Claas Ahlrichs <https://github.com/claasahl>
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
-
-export = right_align;
+// TypeScript Version: 2.7
 
 declare function right_align(val: string): string;
 declare function right_align(val: string[]): string[];
+
+export = right_align;
