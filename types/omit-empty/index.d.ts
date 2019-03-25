e9d1f56d311ea73e6c358b6e241fc0440e6a4308
Shubham Kanodia <skanodia@atlassian.com>
2019-03-25 16:42:35 +0530

    Added types for omit-empty

---
+++ types/omit-empty/index.d.ts
@@ -0,0 +1,11 @@
+// Type definitions for omit-empty 1.0.0
+// Project: https://github.com/jonschlinkert/omit-empty
+// Definitions by: Shubham Kanodia <https://github.com/pastelsky>
+// Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
+
+type OmitOptions = {
+    omitZero?: boolean,
+}
+
+export = omitEmpty
+declare function omitEmpty(obj: object, options?: OmitOptions): object;
