1b178ad57f81f127a124cc37099a58ed3fe870c9
Claas Ahlrichs <claasahl@gmail.com>
2019-03-25 20:36:44 +0100

    npx dts-gen -m right-align --dt

---
+++ types/right-align/index.d.ts
@@ -0,0 +1,15 @@
+// Type definitions for right-align 0.1
+// Project: https://github.com/jonschlinkert/right-align
+// Definitions by: My Self <https://github.com/me>
+// Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
+
+export = right_align;
+
+declare function right_align(val: any): any;
+
+declare namespace right_align {
+    const prototype: {
+    };
+
+}
+
