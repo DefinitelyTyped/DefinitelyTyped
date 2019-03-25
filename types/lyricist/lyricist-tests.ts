e81d6013b401555d6783150760a9d1b96d60c773
Aleksey Klimenko <ralict@mail.ru>
2019-03-26 00:08:55 +0300

    [lyricist] removed additional tslint rules, updated export
    (cherry picked from commit ab214e91bdd8c47a14d3aaa41c28c9e5cc17b205)

--- types/lyricist/lyricist-tests.ts
+++ types/lyricist/lyricist-tests.ts
@@ -1,10 +1,10 @@
-// Type definitions for lyricist 2.2.2
 // Project: https://github.com/scf4/lyricist, https://www.npmjs.com/package/lyricist
 // Definitions by: Aleksei Klimenko <https://github.com/DadUndead>
 // Definitions: https://github.com/DefinitelyTyped/
-// TypeScript Version: 2.7
 
-import Lyricist, { IAlbum, IArtist, ISearchResult, ISong, ISongByArtist, LyricistTextFormat } from 'lyricist';
+import { Album, Artist, SearchResult, Song, SongByArtist, LyricistTextFormat } from 'lyricist';
+
+import Lyricist = require('lyricist');
 
 const lyricist = new Lyricist('access_token');
 
@@ -18,12 +18,12 @@ Promise
         lyricist.songsByArtist(1, { page: 1, perPage: 10, sort: 'asc' }),
     ])
     .then((results) => {
-        const album: IAlbum = results[0];
-        const artist: IArtist = results[1];
-        const artistByName: IArtist = results[2];
-        const searchResult: ISearchResult[] = results[3];
-        const song: ISong = results[4];
-        const songsByArtist: ISongByArtist[] = results[5];
+        const album: Album = results[0];
+        const artist: Artist = results[1];
+        const artistByName: Artist = results[2];
+        const searchResult: SearchResult[] = results[3];
+        const song: Song = results[4];
+        const songsByArtist: SongByArtist[] = results[5];
 
         console.log('album', album.name);
         console.log('artist', artist.name);
@@ -32,7 +32,3 @@ Promise
         console.log('song', song.title);
         console.log('songsByArtist length', songsByArtist.length);
     });
-
-
-
-
