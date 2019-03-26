96f3e2a0e9d80d1505b7034f7bf9223691d20229
Leonardo Donelli <learts92@gmail.com>
2019-03-26 16:45:15 +0100

    Linting fixes

--- types/strophejs-plugin-roster/index.d.ts
+++ types/strophejs-plugin-roster/index.d.ts
@@ -3,53 +3,55 @@
 // Definitions by: LeartS <https://github.com/LeartS>
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
 
-import { Strophe } from 'strophe.js'
+import { Strophe } from 'strophe.js';
 
-type IqCallback = (stanza: Element) => any
-type IqID = string
-type RosterSubscriptionState = 'none' | 'to' | 'from' | 'both' | 'remove'
-type PresenceSubscriptionType = 'subscribe' | 'unsubscribe' | 'subscribed' | 'unsubscribed'
+export {};
+
+export type IqCallback = (stanza: Element) => any;
+export type IqID = string;
+export type RosterSubscriptionState = 'none' | 'to' | 'from' | 'both' | 'remove';
+export type PresenceSubscriptionType = 'subscribe' | 'unsubscribe' | 'subscribed' | 'unsubscribed';
 
 export interface RosterResource {
-    priority: string
-    show: string
-    status: string
+    priority: string;
+    show: string;
+    status: string;
 }
 
 export interface RosterItem {
-    name: string
-    jid: string
-    subscription: RosterSubscriptionState
-    ask: string
-    groups: string[]
-    resources: {[resourceId: string]: RosterResource}
+    name: string;
+    jid: string;
+    subscription: RosterSubscriptionState;
+    ask: string;
+    groups: string[];
+    resources: {[resourceId: string]: RosterResource};
 }
 
-type RosterUpdateCallback =
-(items: RosterItem[], item: RosterItem, previousItem: RosterItem) => any
-type PresenceRequestCallback = (from: string) => any
+export type RosterUpdateCallback =
+(items: RosterItem[], item: RosterItem, previousItem: RosterItem) => any;
+export type PresenceRequestCallback = (from: string) => any;
 
 interface StropheRosterPlugin {
-    supportVersioning (): boolean
-    get (userCallback: IqCallback, ver?: string, items?: string[]): IqID
-    registerCallback (callback: RosterUpdateCallback): void
-    registerRequestCallback (callback: PresenceRequestCallback): void
-    findItem (jid: string): RosterItem | false
-    removeItem (jid: string): boolean
-    subscribe (jid: string, message?: string, nick?: string): void
-    unsubscribe (jid: string, message?: string): void
-    authorize (jid: string, message?: string): void
-    unauthorize (jid: string, message?: string): void
-    add (jid: string, name: string, groups: string[], call_back?: IqCallback): IqID
-    update (jid: string, name?: string, groups?: string[], call_back?: IqCallback): IqID
-    remove (jid: string, call_back?: IqCallback): void
+    supportVersioning(): boolean;
+    get(userCallback: IqCallback, ver?: string, items?: string[]): IqID;
+    registerCallback(callback: RosterUpdateCallback): void;
+    registerRequestCallback(callback: PresenceRequestCallback): void;
+    findItem(jid: string): RosterItem | false;
+    removeItem(jid: string): boolean;
+    subscribe(jid: string, message?: string, nick?: string): void;
+    unsubscribe(jid: string, message?: string): void;
+    authorize(jid: string, message?: string): void;
+    unauthorize(jid: string, message?: string): void;
+    add(jid: string, name: string, groups: string[], call_back?: IqCallback): IqID;
+    update(jid: string, name?: string, groups?: string[], call_back?: IqCallback): IqID;
+    remove(jid: string, call_back?: IqCallback): void;
 }
 
 /*~ Here, declare the same module as the one you imported above */
 declare module 'strophe.js' {
     namespace Strophe {
-        export interface Connection {
-            roster: StropheRosterPlugin
+        interface Connection {
+            roster: StropheRosterPlugin;
         }
     }
 }
--- types/strophejs-plugin-roster/strophejs-plugin-roster-tests.ts
+++ types/strophejs-plugin-roster/strophejs-plugin-roster-tests.ts
@@ -6,7 +6,7 @@ interface AugmentedRosterItem extends RosterItem {
     customField: string;
 }
 
-function onSubscribeRequest (from: string) {
+function onSubscribeRequest(from: string) {
   // Automatically accept and also send "inverse" subscribe request so that
   // both users will be in each other rosters with subscription="both"
   connection.roster.authorize(from);
@@ -16,18 +16,18 @@ function onSubscribeRequest (from: string) {
   return true;
 }
 
-function onRosterUpdate (items: RosterItem[], updatedItem?: RosterItem): void {
+function onRosterUpdate(items: RosterItem[], updatedItem?: RosterItem): void {
   if (updatedItem && updatedItem.subscription === 'remove') {
     // This is the case where we (or another of our resources)
     // deleted a roster item using a roster set
     // and this is the server's resulting roster push.
-    console.log(`Successfully removed ${updatedItem.jid} from roster`)
+    console.log(`Successfully removed ${updatedItem.jid} from roster`);
   }
 }
 
 const connection = new Strophe.Connection('someservice');
 
-function onRosterAdd (stanza: Element) {
+function onRosterAdd(stanza: Element) {
     console.log(stanza.tagName);
 }
 
@@ -36,10 +36,8 @@ connection.roster.get(() => console.log('Sent initial roster request'));
 connection.roster.registerRequestCallback(onSubscribeRequest);
 connection.roster.registerCallback(onRosterUpdate);
 
-connection.roster.add('node@domain/resource', 'Contact One', ['group1'], onRosterAdd)
+connection.roster.add('node@domain/resource', 'Contact One', ['group1'], onRosterAdd);
 connection.roster.update('node@domain/resource', undefined, ['group1', 'group2']);
 const contactOne: RosterItem = (connection.roster.findItem('node@domain/resource') || undefined)!;
 contactOne.groups.length === 2;
 connection.roster.remove(contactOne.jid);
-
-
