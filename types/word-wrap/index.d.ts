b2b0bbcd212149262e3c055af0a0ce6b373c7adf
Claas Ahlrichs <claasahl@gmail.com>
2019-03-26 18:33:14 +0100

    npx dts-gen -m word-wrap --dt

---
+++ types/word-wrap/index.d.ts
@@ -0,0 +1,15 @@
+// Type definitions for word-wrap 1.2
+// Project: https://github.com/jonschlinkert/word-wrap
+// Definitions by: My Self <https://github.com/me>
+// Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
+
+export = word_wrap;
+
+declare function word_wrap(str: any, options: any): any;
+
+declare namespace word_wrap {
+    const prototype: {
+    };
+
+}
+
