8f5fafd01ed30d088320e9f91f9298d0a3c6586b
timolinn <timothyonyiuke@gmail.com>
2019-03-26 19:01:51 +0100

    fix: correct versioning

--- types/burns/index.d.ts
+++ types/burns/index.d.ts
@@ -1,18 +1,18 @@
-// Type definitions for burns version 2.0.1
-// Project: https://github.com/shalvah/burns
+// Type definitions for burns 2.0
+// Project: https://github.com/shalvah/burns#readme
 // Definitions by: Timothy Onyiuke <https://github.com/timolinn>
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
 
 declare const _Burns: Burns;
 
+export interface ObjectLiteral {
+    [key: string]: any;
+}
+
 export interface Config {
     defaultHandler?: any;
     broadcaster?: any;
     pusher?: any;
-}
-
-export interface ObjectLiteral {
-    [key: string]: any
 }
 
 declare class Burns {
