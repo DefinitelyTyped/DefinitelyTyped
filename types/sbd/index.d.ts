6f30dd4d9528442f1ba5cc0120d61b5c560d4566
Brian Cort <brian@pienso.com>
2019-03-26 17:43:05 -0400

    Type definition for sbd library

--- types/sbd/index.d.ts
+++ types/sbd/index.d.ts
@@ -1,16 +1,16 @@
-// Type definitions for sbd 1.0.15
+// Type definitions for sbd v1.0.15
 // Project: https://github.com/Tessmore/sbd
 // Definitions by: Brian Cort <https://github.com/thatcort>
 
 export as namespace sbd;
 
 export interface Options {
-  newline_boundaries?: boolean;
-  html_boundaries?: boolean;
-  sanitize?: boolean;
-  allowed_tags?: false | string[];
-  preserve_whitespace?: boolean;
-  abbreviations?: string[];
+    newline_boundaries?: boolean;
+    html_boundaries?: boolean;
+    sanitize?: boolean;
+    allowed_tags?: false | string[];
+    preserve_whitespace?: boolean;
+    abbreviations?: string[];
 }
 
 export function sentences(text: string, options?: Options): string[];
