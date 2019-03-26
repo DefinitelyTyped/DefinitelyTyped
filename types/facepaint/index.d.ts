1b03b9d5a807252456ace2d06371af558ffdcffa
Anton Samper Rivaya <anton@aeser.co.uk>
2019-03-26 22:55:08 +0000

    lint fixes

--- types/facepaint/index.d.ts
+++ types/facepaint/index.d.ts
@@ -3,6 +3,7 @@
 // Definitions by: Ciarán Curley <https://github.com/DogPawHat>
 //                 Anton Samper Rivaya <https://github.com/antonsamper>
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
+// TypeScript Version: 2.2
 
 declare module 'facepaint' {
     type BaseArg = object | object[];
