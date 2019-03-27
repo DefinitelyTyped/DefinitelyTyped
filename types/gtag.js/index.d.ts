16529b8461086c8ede7d6a28023502fd368fb0b8
Junyoung Choi <fluke8259@gmail.com>
2019-03-27 11:32:52 +0900

    Fix type definition header of gtag.js

--- types/gtag.js/index.d.ts
+++ types/gtag.js/index.d.ts
@@ -1,6 +1,6 @@
 // Type definitions for Google gtag.js API
 // Project: https://developers.google.com/gtagjs
-// Definitions by:  Junyoung Choi <https://github.com/rokt33r>,
+// Definitions by:  Junyoung Choi <https://github.com/rokt33r>
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
 
 declare var gtag: Gtag.Gtag;
