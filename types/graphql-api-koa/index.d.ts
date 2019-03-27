524db8f3c6884682bb3b2ac63017ad77a8dd6d9e
Mike Marcacci <mike.marcacci@gmail.com>
2019-03-27 11:54:49 -0700

    Add graphql-api-koa

---
+++ types/graphql-api-koa/index.d.ts
@@ -0,0 +1,26 @@
+// Type definitions for graphql-api-koa 2.0
+// Project: https://github.com/jaydenseric/graphql-api-koa#readme
+// Definitions by: Mike Marcacci <https://github.com/mike-marcacci>
+// Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
+// TypeScript Version: 2.6
+
+import { GraphQLSchema } from "graphql";
+import { Middleware, ParameterizedContext } from "koa";
+
+interface ExecuteOptions {
+  schema?: GraphQLSchema;
+  rootValue?: any;
+  contextValue?: any;
+  fieldResolver?: any;
+}
+
+export const errorHandler: <StateT = any, CustomT = {}>() => Middleware<
+  StateT,
+  CustomT
+>;
+
+export const execute: <StateT = any, CustomT = {}>(
+  options: ExecuteOptions & {
+    override?: (ctx: ParameterizedContext<StateT, CustomT>) => ExecuteOptions;
+  }
+) => Middleware<StateT, CustomT>;
