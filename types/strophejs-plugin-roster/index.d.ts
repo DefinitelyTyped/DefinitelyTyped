1bd3762a1e24a59e1c4f460dd495717e47d5724a
Leonardo Donelli <learts92@gmail.com>
2019-03-27 10:20:06 +0100

    Add typescript version, same as strophe.js

--- types/strophejs-plugin-roster/index.d.ts
+++ types/strophejs-plugin-roster/index.d.ts
@@ -2,6 +2,7 @@
 // Project: https://github.com/strophe/strophejs-plugin-roster#readme
 // Definitions by: LeartS <https://github.com/LeartS>
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
+// TypeScript Version: 2.1
 
 import { Strophe } from 'strophe.js';
 
