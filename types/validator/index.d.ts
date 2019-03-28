b34a06c990326d656ebab2bd0b85f2e68181bfce
keatz55 <keatz55@gmail.com>
2019-03-27 22:24:24 -0600

    Increases the version number in the header

--- types/validator/index.d.ts
+++ types/validator/index.d.ts
@@ -1,4 +1,4 @@
-// Type definitions for validator.js v10.9.0
+// Type definitions for validator.js v10.11.0
 // Project: https://github.com/chriso/validator.js
 // Definitions by: tgfjt <https://github.com/tgfjt>
 //                 Ilya Mochalov <https://github.com/chrootsu>
