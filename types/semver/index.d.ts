8f2db9e73ff17e01c4ba8191645e3cbf227a6514
Klaus Meinhardt <klaus.meinhardt1@gmail.com>
2019-03-28 16:55:25 +0100

    add definition author

--- types/semver/index.d.ts
+++ types/semver/index.d.ts
@@ -3,6 +3,7 @@
 // Definitions by: Bart van der Schoor <https://github.com/Bartvds>
 //                 BendingBender <https://github.com/BendingBender>
 //                 Lucian Buzzo <https://github.com/LucianBuzzo>
+//                 Klaus Meinhardt <https://github.com/ajafff>
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped/tree/master/semver
 
 export const SEMVER_SPEC_VERSION: "2.0.0";
