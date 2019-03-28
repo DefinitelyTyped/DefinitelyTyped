b47f8313ae191cb7c05c453222efa256dae280d1
Sam Alexander <alexander.sam@me.com>
2019-03-28 08:57:02 +0000

    Fix lint errors

--- types/html5-to-pdf/index.d.ts
+++ types/html5-to-pdf/index.d.ts
@@ -13,24 +13,21 @@ declare class HTML5ToPDF {
 
     close(): Promise<void>;
 
-    includeAssets(): Promise<void[]>;
+    includeAssets(): Promise<void>;
 
     parseOptions(options: HTML5ToPDF.Options): HTML5ToPDF.ParsedOptions;
 
     start(): Promise<Page>;
-
 }
 
 declare namespace HTML5ToPDF {
     interface FileDef {
         /**
          * File type
-         * @type {("css" | "js")}
          */
         type: "css" | "js";
         /**
          * File path
-         * @type {string}
          */
         filePath: string;
     }
@@ -49,13 +46,11 @@ declare namespace HTML5ToPDF {
         /**
          * [COMPATIBLE]\
          * Page size
-         * @type {("A3" | "A4" | "Legal" | "Tabloid")}
          */
         pageSize?: "A3" | "A4" | "Legal" | "Tabloid";
         /**
          * [COMPATIBLE]\
          * True for landscape, false for portrait.
-         * @type {boolean}
          */
         landscape?: boolean;
         /**
@@ -63,13 +58,11 @@ declare namespace HTML5ToPDF {
          * 0 - default\
          * 1 - none\
          * 2 - minimum
-         * @type {number}
          */
         marginsType?: number;
         /**
          * [COMPATIBLE]\
          * Whether to print CSS backgrounds.
-         * @type {boolean}
          */
         printBackground?: boolean;
     }
@@ -77,54 +70,44 @@ declare namespace HTML5ToPDF {
     interface Options {
         /**
          * Path to the input HTML.
-         * @type {string}
          */
         inputPath?: string;
         /**
          * Path to the input html as a String, or Buffer. If specified this will override inputPath.
-         * @type {string | Buffer}
          */
         inputBody?: string | Buffer;
         /**
          * Path to the output pdf file.
-         * @type {string}
-        */
+         */
         outputPath?: string;
         /**
          * Delay in milliseconds before rendering the PDF (give HTML and CSS a chance to load).
-         * @type {number}
          */
         rendererDelay?: number;
         /**
          * A list of CSS or JS assets to include.
-         * @type {Array<string | FileDef>}
          */
-        include?: Array<string | FileDef>
+        include?: Array<string | FileDef>;
         /**
          * The template to use when rendering the html.
-         * @type {string}
          */
         template?: string;
         /**
          * The template to use for rendering the html. If this is set, it will use this instead of the template path.
-         * @type {string}
          */
         templateUrl?: string;
         /**
          * This object will be passed directly to `puppeteer`.
-         * @type {PDFOptions}
          */
         pdf?: PDFOptions;
         /**
          * This object will be passed directly to `puppeteer`.
-         * @type {LaunchOptions}
          */
         launchOptions?: LaunchOptions;
         /**
          * @deprecated Legacy Options.
          * See `options.pdf` for pdf options. Since some of these options are converted over to work with `puppeteer`,
          * this is automatically done if `options.pdf` is left empty.
-         * @type {LegacyOptions}
          */
         options?: LegacyOptions;
     }
