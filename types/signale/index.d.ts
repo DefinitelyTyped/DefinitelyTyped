d6b558d753f914c28d194821690df5f6a468e5bf
Jason Etcovitch <jasonetco@github.com>
2019-03-28 21:57:37 -0400

    Add class methods

--- types/signale/index.d.ts
+++ types/signale/index.d.ts
@@ -122,6 +122,30 @@ declare namespace signale {
             label?: string,
             span?: number
         ): { label: string; span?: number };
+        /**
+         * Disables the logging functionality of all loggers belonging to a specific instance.
+         */
+        disable(): void;
+        /**
+         * Enables the logging functionality of all loggers belonging to a specific instance.
+         */
+        enable(): void;
+        /**
+         * Checks whether the logging functionality of a specific instance is enabled.
+         *
+         * @returns a boolean that describes whether or not the logger is enabled.
+         */
+        isEnabled(): boolean;
+        /**
+         * Adds new secrets/sensitive-information to the targeted Signale instance.
+         *
+         * @param secrets Array holding the secrets/sensitive-information to be filtered out.
+         */
+        addSecrets(secrets: string[] | number[]): void;
+        /**
+         * Removes all secrets/sensitive-information from the targeted Signale instance.
+         */
+        clearSecrets(): void;
     }
 
     type LoggerFunc = (message?: any, ...optionalArgs: any[]) => void;
