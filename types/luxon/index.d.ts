0e827696ed412a138d45ab74f771522c69546a08
Miles Johnson <miles.johnson@airbnb.com>
2019-03-29 16:40:58 -0700

    Bump version.

--- types/luxon/index.d.ts
+++ types/luxon/index.d.ts
@@ -1,4 +1,4 @@
-// Type definitions for luxon 1.11
+// Type definitions for luxon 1.12
 // Project: https://github.com/moment/luxon#readme
 // Definitions by: Colby DeHart <https://github.com/colbydehart>
 //                 Hyeonseok Yang <https://github.com/FourwingsY>
