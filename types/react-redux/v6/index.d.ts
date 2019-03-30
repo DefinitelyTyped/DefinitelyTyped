0617f9dd7d821539802afaecdd1fbe06f52ce902
Wesley Wigham <wwigham@gmail.com>
2019-03-29 18:55:36 -0700

    Same as #34335 but for react-redux v6 (#34339)

--- types/react-redux/v6/index.d.ts
+++ types/react-redux/v6/index.d.ts
@@ -84,7 +84,7 @@ export type Matching<InjectedProps, DecorationTargetProps> = {
  */
 export type Shared<
     InjectedProps,
-    DecorationTargetProps extends Shared<InjectedProps, DecorationTargetProps>
+    DecorationTargetProps
     > = {
         [P in Extract<keyof InjectedProps, keyof DecorationTargetProps>]?: InjectedProps[P] extends DecorationTargetProps[P] ? DecorationTargetProps[P] : never;
     };
