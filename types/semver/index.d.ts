d5d98f6f971aaa11b16d4a51729181d8bd89ebfc
Klaus Meinhardt <klaus.meinhardt1@gmail.com>
2019-03-30 22:51:57 +0100

    v6.0

--- types/semver/index.d.ts
+++ types/semver/index.d.ts
@@ -1,4 +1,4 @@
-// Type definitions for semver 5.7
+// Type definitions for semver 6.0
 // Project: https://github.com/npm/node-semver
 // Definitions by: Bart van der Schoor <https://github.com/Bartvds>
 //                 BendingBender <https://github.com/BendingBender>
