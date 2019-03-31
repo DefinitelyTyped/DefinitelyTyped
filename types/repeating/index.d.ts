cb8b1195e1835f5d585ca17150ff1f18441ccf67
Claas Ahlrichs <claasahl@gmail.com>
2019-03-31 11:03:18 +0200

    npx dts-gen -m repeating --dt

---
+++ types/repeating/index.d.ts
@@ -0,0 +1,9 @@
+// Type definitions for repeating 3.0
+// Project: https://github.com/sindresorhus/repeating#readme
+// Definitions by: My Self <https://github.com/me>
+// Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
+
+export = repeating;
+
+declare function repeating(n: any, str: any): any;
+
