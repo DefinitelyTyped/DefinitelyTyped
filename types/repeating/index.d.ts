ab21c22e8a48a2e9373d775450c38cbcbc5b27f0
Claas Ahlrichs <claasahl@gmail.com>
2019-03-31 11:11:13 +0200

    fixed linting issues

--- types/repeating/index.d.ts
+++ types/repeating/index.d.ts
@@ -2,6 +2,7 @@
 // Project: https://github.com/sindresorhus/repeating#readme
 // Definitions by: Claas Ahlrichs <https://github.com/claasahl>
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
+// TypeScript Version: 2.7
 
 export = repeating;
 
