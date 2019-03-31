7b3781ebfa0d6036675f681df541c0f01467ab81
Peter Kimberley <peter@gradata.com.au>
2019-03-31 13:52:06 +1100

    Update supported package version comment

--- types/force-graph/index.d.ts
+++ types/force-graph/index.d.ts
@@ -1,4 +1,4 @@
-// Type definitions for force-graph >= 1.14
+// Type definitions for force-graph 1.14.4
 // Project: https://github.com/vasturiano/force-graph
 // Definitions by: Peter Kimberley <https://github.com/p-kimberley>
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
