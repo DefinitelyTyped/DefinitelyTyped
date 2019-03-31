752f796dbf0ebf183fc4b2084ee6a130f02522ec
Bert Verhelst <verhelstbert@gmail.com>
2019-03-31 20:09:34 +0200

    Increase minor version instead of patch version

--- types/async-retry/index.d.ts
+++ types/async-retry/index.d.ts
@@ -1,4 +1,4 @@
-// Type definitions for async-retry 1.2.1
+// Type definitions for async-retry 1.3
 // Project: https://github.com/zeit/async-retry#readme
 // Definitions by: Albert Wu <https://github.com/albertywu>
 //                 Pablo Rodríguez <https://github.com/MeLlamoPablo>
