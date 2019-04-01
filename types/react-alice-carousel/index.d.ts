a22dcaeacb7ad4875a012e66109a0527a52217a6
maxmarinich <max.marinich@gmail.com>
2019-04-01 11:24:46 +0300

    test fixes

--- types/react-alice-carousel/index.d.ts
+++ types/react-alice-carousel/index.d.ts
@@ -4,40 +4,40 @@
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
 // TypeScript Version: 2.8
 
-import * as React from 'react'
+import * as React from "react";
 
 export interface EventObject {
-  item: number
-  slide: number
-  itemsInSlide: number
+  item: number;
+  slide: number;
+  itemsInSlide: number;
 }
 
 export interface Props {
-  items?: Array<{}>
-  onSlideChange?: (e: EventObject) => void
-  onSlideChanged?: (e: EventObject) => void
-  onInitialized?: (e: EventObject) => void
-  onResized?: (e: EventObject) => void
-  keysControlDisabled?: boolean
-  playButtonEnabled?: boolean
-  buttonsDisabled?: boolean
-  dotsDisabled?: boolean
-  swipeDisabled?: boolean
-  responsive?: {}
-  stagePadding?: {}
-  duration?: number
-  startIndex?: number
-  slideToIndex?: number
-  autoPlay?: boolean
-  infinite?: boolean
-  mouseDragEnabled?: boolean
-  fadeOutAnimation?: boolean
-  autoPlayInterval?: number
-  autoPlayDirection?: string
-  disableAutoPlayOnAction?: boolean
-  stopAutoPlayOnHover?: boolean
-  showSlideInfo?: false
-  preventEventOnTouchMove?: false
+  items?: Array<{}>;
+  onSlideChange?: (e: EventObject) => void;
+  onSlideChanged?: (e: EventObject) => void;
+  onInitialized?: (e: EventObject) => void;
+  onResized?: (e: EventObject) => void;
+  keysControlDisabled?: boolean;
+  playButtonEnabled?: boolean;
+  buttonsDisabled?: boolean;
+  dotsDisabled?: boolean;
+  swipeDisabled?: boolean;
+  responsive?: {};
+  stagePadding?: {};
+  duration?: number;
+  startIndex?: number;
+  slideToIndex?: number;
+  autoPlay?: boolean;
+  infinite?: boolean;
+  mouseDragEnabled?: boolean;
+  fadeOutAnimation?: boolean;
+  autoPlayInterval?: number;
+  autoPlayDirection?: string;
+  disableAutoPlayOnAction?: boolean;
+  stopAutoPlayOnHover?: boolean;
+  showSlideInfo?: false;
+  preventEventOnTouchMove?: false;
 }
 
 export default class Carousel extends React.PureComponent<Props> {}
