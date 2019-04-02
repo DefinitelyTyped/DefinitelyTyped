30a2ddda990f6fcd99c01aba009b350e46de9c98
Emily Marigold Klassen <forivall@gmail.com>
2019-04-02 12:31:18 -0700

    fix homepage url

--- types/follow-redirects/index.d.ts
+++ types/follow-redirects/index.d.ts
@@ -1,5 +1,5 @@
 // Type definitions for follow-redirects 1.5
-// Project: https://github.com/olalonde/follow-redirects
+// Project: https://github.com/follow-redirects/follow-redirects
 // Definitions by: Emily Klassen <https://github.com/forivall>
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
 // TypeScript Version: 2.8
