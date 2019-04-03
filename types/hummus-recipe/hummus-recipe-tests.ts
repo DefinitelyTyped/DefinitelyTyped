6921ab60c420524e3fd0a130e29062b02983318d
Erik Berreßem <erik.berressem@gmail.com>
2019-04-03 13:29:29 +0200

    :label: Add typings for 'hummus-recipe'

---
+++ types/hummus-recipe/hummus-recipe-tests.ts
@@ -0,0 +1,21 @@
+import * as HummusRecipe from 'hummus-recipe';
+
+// $ExpectType HummusRecipe
+const pdf = new HummusRecipe('new', 'test.pdf', {
+  version: 1.6,
+  author: 'John Doe',
+  title: 'Hummus Recipe',
+  subject: 'A brand new PDF',
+});
+
+// $ExpectType HummusRecipe
+pdf.createPage(595, 842)
+  .text('Memento Mori', 100, 100)
+  .endPage()
+  .endPDF();
+
+// $ExpectError
+pdf.createPage('A4')
+  .text('Memento Mori', 100, 100)
+  .endPage()
+  .endPDF();
