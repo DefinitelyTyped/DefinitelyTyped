63a045db890ee4c430a0e94591f9ad0c75c6d77c
German Pineda <germanp173@gmail.com>
2019-04-02 23:23:51 -0700

    Add myself to Definitions By

--- types/react-burger-menu/index.d.ts
+++ types/react-burger-menu/index.d.ts
@@ -2,6 +2,7 @@
 // Project: https://github.com/negomi/react-burger-menu
 // Definitions by: Rajab Shakirov <https://github.com/radziksh>
 //                 David Acevedo <https://github.com/dacevedo12>
+//                 German Pineda <https://github.com/germanp173>
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
 // TypeScript Version: 2.8
 
