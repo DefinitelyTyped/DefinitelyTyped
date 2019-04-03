ca6228921530c30b49818a16e8228a82b669529b
syncfusion-javascript <packages@syncfusion.com>
2019-04-03 11:35:26 +0530

    changed UTF to 8

--- types/ej.web.all/index.d.ts
+++ types/ej.web.all/index.d.ts
@@ -1,4 +1,4 @@
-﻿// Type definitions for non-npm package ej.web.all 17.1
+// Type definitions for non-npm package ej.web.all 17.1
 // Project: http://help.syncfusion.com/js/typescript
 // Definitions by: Syncfusion <https://github.com/syncfusion>
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
