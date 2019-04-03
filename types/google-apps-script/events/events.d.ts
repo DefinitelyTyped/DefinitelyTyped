19f60bc0fa344f6c2c4cdd6a1f1006d14bd3abfc
Grant Timmerman <timmerman+devrel@google.com>
2019-04-02 17:13:42 -0700

    Rename event type to AppsScriptEvent

--- types/google-apps-script/events/events.d.ts
+++ types/google-apps-script/events/events.d.ts
@@ -1,6 +1,6 @@
-// Type definitions for Google Apps Script 2019-02-27
+// Type definitions for Google Apps Script 2019-04-02
 // Project: https://developers.google.com/apps-script/
-// Definitions by: motemen <https://github.com/motemen/>
+// Definitions by: grant <https://github.com/grant/>
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
 
 /// <reference path="../google-apps-script.script.d.ts" />
@@ -14,49 +14,49 @@ declare namespace GoogleAppsScript {
    */
   export module Events {
     // Internal interfaces
-    interface GSuiteEvent {
+    interface AppsScriptEvent {
       authMode: Script.AuthMode,
       triggerUid: string,
       user: Base.User,
     }
 
     // External interfaces
-    export interface SheetsOnOpen extends GSuiteEvent{
+    export interface SheetsOnOpen extends AppsScriptEvent {
       source: Spreadsheet.Spreadsheet,
     }
-    
+
     enum SheetsOnChangeChangeType { EDIT, INSERT_ROW, INSERT_COLUMN, REMOVE_ROW, REMOVE_COLUMN, INSERT_GRID, REMOVE_GRID, FORMAT, OTHER }
-    export interface SheetsOnChange extends GSuiteEvent {
+    export interface SheetsOnChange extends AppsScriptEvent {
       changeType: SheetsOnChangeChangeType,
     }
 
-    export interface SheetsOnEdit extends GSuiteEvent {
+    export interface SheetsOnEdit extends AppsScriptEvent {
       oldValue: string,
       range: Spreadsheet.Range,
       source: Spreadsheet.Spreadsheet,
       value: string,
     }
 
-    export interface FormsOnSubmit extends GSuiteEvent {
-      namedValues: { [key:string]:string[]; },
+    export interface FormsOnSubmit extends AppsScriptEvent {
+      namedValues: { [key: string]: string[]; },
       range: Spreadsheet.Range,
       values: string[],
     }
 
-    export interface DocsOnOpen extends GSuiteEvent{
+    export interface DocsOnOpen extends AppsScriptEvent {
       source: Document.Document,
     }
 
-    export interface SlidesOnOpen extends GSuiteEvent {
+    export interface SlidesOnOpen extends AppsScriptEvent {
       source: Slides.Presentation,
     }
 
-    export interface FormsOnOpen extends GSuiteEvent {
+    export interface FormsOnOpen extends AppsScriptEvent {
       source: Forms.Form,
     }
 
     // TODO: Is there a `user` attribute?
-    export interface CalendarEventUpdated extends GSuiteEvent {
+    export interface CalendarEventUpdated extends AppsScriptEvent {
       calendarId: string,
     }
 
