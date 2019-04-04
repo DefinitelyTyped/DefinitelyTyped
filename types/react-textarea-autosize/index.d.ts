3249f0f900f52dea1e5aec7b33344a9e207bf838
Rahul-Sagore <rahul.sagore@gmail.com>
2019-04-04 12:43:56 +0530

    Update Definitions by section

--- types/react-textarea-autosize/index.d.ts
+++ types/react-textarea-autosize/index.d.ts
@@ -1,6 +1,8 @@
 // Type definitions for react-textarea-autosize 4.3.0
 // Project: https://github.com/andreypopp/react-textarea-autosize
-// Definitions by: Alexey Svetliakov <https://github.com/asvetliakov>, Jerry Zou <https://github.com/zry656565>
+// Definitions by: Alexey Svetliakov <https://github.com/asvetliakov>,
+//                 Jerry Zou <https://github.com/zry656565>
+//                 Rahul Sagore <https://github.com/Rahul-Sagore>
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
 // TypeScript Version: 2.8
 
