af68d2616c56b06c3a438fef6ddb3610c6439816
Ajil Oommen <ajilo297@gmail.com>
2019-04-04 09:48:07 +0530

    Added TypeDefinitions for project-name

---
+++ types/project-name/index.d.ts
@@ -0,0 +1,10 @@
+// Type definitions for project-name
+// Project: https://github.com/jonschlinkert/project-name
+// Definitions by: Ajil Oommen <https://github.com/ajilo297/>
+// Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
+
+/**
+ * Type Definition for project-name
+ * @param cwd The Base Current Working Directory of project 
+ */
+export default function (cwd: string): string;
---
+++ types/project-name/project-name-tests.ts
@@ -0,0 +1,3 @@
+import name from 'project-name';
+
+name('d'); // $ExpectType string
