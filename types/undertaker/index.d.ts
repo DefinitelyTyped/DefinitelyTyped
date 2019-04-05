432e0027808177da01fd3971e213c4ef49cd4b2a
Evan Yamanishi <yamanishi1@gmail.com>
2019-04-05 13:18:51 -0400

    chore(undertaker): update attribution

--- types/undertaker/index.d.ts
+++ types/undertaker/index.d.ts
@@ -1,6 +1,8 @@
 // Type definitions for undertaker 1.2
 // Project: https://github.com/gulpjs/undertaker
-// Definitions by: Qubo <https://github.com/tkqubo>, Giedrius Grabauskas <https://github.com/GiedriusGrabauskas>
+// Definitions by: Qubo <https://github.com/tkqubo>
+//                 Giedrius Grabauskas <https://github.com/GiedriusGrabauskas>
+//                 Evan Yamanishi <https://github.com/sh0ji>
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
 
 import * as Registry from "undertaker-registry";
