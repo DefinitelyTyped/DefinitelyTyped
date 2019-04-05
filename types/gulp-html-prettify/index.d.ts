926ac9763d28e883f021109e887674795ee720d1
bokovgleb <bokovgleb@team-lab.com>
2019-04-05 13:00:38 +0900

    clear to test

--- types/gulp-html-prettify/index.d.ts
+++ types/gulp-html-prettify/index.d.ts
@@ -1,19 +1,18 @@
-// Type definitions for npm i gulp-html-prettify
+// Type definitions for npm i gulp-html-prettify 0.0
 // Project: https://www.npmjs.com/package/gulp-html-prettify
 // Definitions by: Takesi Tokugawa <https://github.com/TokugawaTakesi>
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
 
 /// <reference types="node"/>
 
-
-interface HtmlPrettifyOptions {
+interface GulpHtmlPrettifyOptions {
     indent_char?: string;
     indent_size?: number;
 }
 
-interface HtmlPrettify {
-    (options?: HtmlPrettifyOptions): NodeJS.ReadWriteStream;
+interface GulpHtmlPrettify {
+    (options?: GulpHtmlPrettifyOptions): NodeJS.ReadWriteStream;
 }
 
-declare var _htmlPrettify: HtmlPrettify;
-export = _htmlPrettify;
+declare var gulpHtmlPrettify: GulpHtmlPrettify;
+export = gulpHtmlPrettify;
