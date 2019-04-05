0895e31ab1166bf4ead2e46aff5a40a79663e69c
hamid_mayeli_geeksltd <hamid.mayeli@geeks.ltd.uk>
2019-04-05 12:19:23 +0100

    change the project

--- types/jquery-typeahead/index.d.ts
+++ types/jquery-typeahead/index.d.ts
@@ -1,5 +1,5 @@
 // Type definitions for jquery-typeahead
-// Project: https://github.com/running-coder/jquery-typeahead
+// Project: http://www.runningcoder.org/jquerytypeahead
 // Definitions by: Hamid Mayeli <https://github.com/HamidTheGeek>
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
 // TypeScript Version: 2.3
