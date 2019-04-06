446bc4263148f37f9c552157767e88719dfecd43
wessberg <frederikwessberg@gmail.com>
2019-04-06 12:36:57 +0200

    Updated workbox-sw tests

--- types/workbox-sw/workbox-sw-tests.ts
+++ types/workbox-sw/workbox-sw-tests.ts
@@ -1,7 +1,8 @@
 import WorkboxSW = require("workbox-sw");
 
-
 WorkboxSW.routing.registerRoute("/", new WorkboxSW.strategies.NetworkFirst()); // $ExpectType Route
+
+WorkboxSW.routing.registerRoute("/", WorkboxSW.strategies.networkFirst()); // $ExpectType Route
 
 // $ExpectError
 WorkboxSW.precaching.precacheAndRoute(/foo/);
