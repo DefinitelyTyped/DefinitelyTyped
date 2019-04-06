8afbe8680ce7ca3f3c17c5e0e66fb1009b9b1ca3
Dimitri Benin <dimitri.benin@gmail.com>
2019-04-06 16:37:30 +0200

    Deprecate types for packages that provide their own types

--- types/p-times/p-times-tests.ts
+++
@@ -1,6 +0,0 @@
-import pTimes = require('p-times');
-
-// $ExpectType Promise<string[]>
-pTimes(5, i => `🦄-${i + 1}`);
-// $ExpectType Promise<string[]>
-pTimes(5, i => `🦄-${i + 1}`, { concurrency: 1 });
--- types/sdbm/index.d.ts
+++
@@ -1,8 +0,0 @@
-// Type definitions for sdbm 1.0
-// Project: https://github.com/sindresorhus/sdbm#readme
-// Definitions by: BendingBender <https://github.com/BendingBender>
-// Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
-
-export = sdbm;
-
-declare function sdbm(string: string): number;
