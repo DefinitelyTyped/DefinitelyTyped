35e07a23ba6738ee160cd6ee9ac20e3834c9fd5f
Claas Ahlrichs <claasahl@gmail.com>
2019-04-07 11:30:11 +0200

    npx dts-gen -m isarray --dt

---
+++ types/isarray/index.d.ts
@@ -0,0 +1,9 @@
+// Type definitions for isarray 2.0
+// Project: https://github.com/juliangruber/isarray
+// Definitions by: My Self <https://github.com/me>
+// Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
+
+export = isarray;
+
+declare function isarray(p0: any): any;
+
