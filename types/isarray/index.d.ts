6bf5c23861754247117caac565ba71d1e5d85723
Claas Ahlrichs <claasahl@gmail.com>
2019-04-07 11:35:46 +0200

    drafted types for isarray

--- types/isarray/index.d.ts
+++ types/isarray/index.d.ts
@@ -1,9 +1,9 @@
 // Type definitions for isarray 2.0
 // Project: https://github.com/juliangruber/isarray
-// Definitions by: My Self <https://github.com/me>
+// Definitions by: Claas Ahlrichs <https://github.com/claasahl>
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
+// TypeScript Version: 2.7
 
 export = isarray;
 
-declare function isarray(p0: any): any;
-
+declare function isarray(p0: any): boolean;
