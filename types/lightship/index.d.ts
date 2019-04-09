669776892f7eb06a4c888b315df3462e05cf5f03
Scott Chang <purmac@gmail.com>
2019-04-09 14:47:43 -0400

    Correct Lightship version

--- types/lightship/index.d.ts
+++ types/lightship/index.d.ts
@@ -1,4 +1,4 @@
-// Type definitions for lightship 1.0
+// Type definitions for lightship 3.0
 // Project: https://github.com/gajus/lightship#readme
 // Definitions by: Scott Chang <https://github.com/purmac>
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
