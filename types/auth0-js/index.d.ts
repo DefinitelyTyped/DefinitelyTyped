149903af27c4de8cfa2ae66f78d8de3296ce6250
Mark Nelissen <mark.nelissen@gemsotec.com>
2019-04-09 14:48:14 +0200

    update version and contributor

--- types/auth0-js/index.d.ts
+++ types/auth0-js/index.d.ts
@@ -1,9 +1,10 @@
-// Type definitions for Auth0.js 8.11
+// Type definitions for Auth0.js 9.10
 // Project: https://github.com/auth0/auth0.js
 // Definitions by: Adrian Chia <https://github.com/adrianchia>
 //                 Matt Durrant <https://github.com/mdurrant>
 //                 Peter Blazejewicz <https://github.com/peterblazejewicz>
 //                 Bartosz Kotrys <https://github.com/bkotrys>
+//                 Mark Nelissen <https://github.com/marknelissen>
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
 // TypeScript Version: 2.3
 
