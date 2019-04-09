805bc861a6e542c42643dc56607486ff773c7821
TokugawaTakesi <tokugawa.takesi@gmail.com>
2019-04-09 10:59:47 +0900

    fix tslint

--- types/gulp-postcss/index.d.ts
+++ types/gulp-postcss/index.d.ts
@@ -1,5 +1,5 @@
 // Type definitions for gulp-postcss 8.0
-// Project: https://www.npmjs.com/package/gulp-postcss
+// Project: https://github.com/postcss/gulp-postcss
 // Definitions by: Takesi Tokugawa <https://github.com/TokugawaTakesi>
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
 // TypeScript Version: 2.8
