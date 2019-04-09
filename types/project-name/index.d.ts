c955b7631e912f8525231e008c469ccb1244dd26
Ajil Oommen <ajilo297@gmail.com>
2019-04-09 09:55:00 +0530

    Additional Fixes

--- types/project-name/index.d.ts
+++ types/project-name/index.d.ts
@@ -1,6 +1,6 @@
-// Type definitions for project-name
+// Type definitions for project-name 1.0
 // Project: https://github.com/jonschlinkert/project-name
-// Definitions by: Ajil Oommen <https://github.com/ajilo297/>
+// Definitions by: Ajil Oommen <https://github.com/ajilo297>
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
 
 export = index;
@@ -8,4 +8,4 @@ export = index;
  * Type Definition for project-name
  * @param cwd The Base Current Working Directory of project
  */
-declare function index(cwd: string): string;
+declare function index(cwd?: string): string;
