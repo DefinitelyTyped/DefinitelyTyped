c6053ebfd4221c16a291b4d4349225a81d85f6b7
Kai Volland <kaivolland@yahoo.de>
2019-04-10 14:23:18 +0200

    Extends version number with patch version

--- types/shpjs/index.d.ts
+++ types/shpjs/index.d.ts
@@ -1,4 +1,4 @@
-// Type definitions for shpjs 3.4
+// Type definitions for shpjs 3.4.3
 // Project: https://github.com/calvinmetcalf/shapefile-js#readme
 // Definitions by: Hsiao-Ting Yu <https://github.com/littlebtc>
 //                 Kai Volland <https://github.com/kaivolland>  
