dd67b0c76ece62bb3da722ee389b5abc859cb616
Luis Herranz <luisherranz@gmail.com>
2019-04-10 13:43:59 +0200

    increase version number (minor)

--- types/loadable__server/index.d.ts
+++ types/loadable__server/index.d.ts
@@ -1,4 +1,4 @@
-// Type definitions for @loadable/server 5.2
+// Type definitions for @loadable/server 5.3
 // Project: https://github.com/smooth-code/loadable-components
 // Definitions by: Martynas Kadiša <https://github.com/martynaskadisa>
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
