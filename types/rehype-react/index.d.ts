61d65d04000dedb04eea3e530641a410934de056
Adrian Kremer <adrian.kremer@namics.com>
2019-04-10 16:10:34 +0200

    fix: definitions reference

--- types/rehype-react/index.d.ts
+++ types/rehype-react/index.d.ts
@@ -1,7 +1,7 @@
 // Type definitions for rehype-react 2.0
 // Project: https://github.com/rhysd/rehype-react
 // Definitions by: Adrian Kremer <https://github.com/adriankremer>
-// Definitions: https://github.com/adriankremer/DefinitelyTyped
+// Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
 // TypeScript Version: 2.8
 
 import React from "react";
