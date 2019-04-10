a82c20628b7aa49282ffa93cc0f775cd6cce5a1e
Adrian Kremer <adrian.kremer@namics.com>
2019-04-10 17:40:38 +0200

    fix: used flat export like module

--- types/rehype-react/index.d.ts
+++ types/rehype-react/index.d.ts
@@ -1,20 +1,22 @@
-// Type definitions for rehype-react 2.0
+// Type definitions for rehype-react 3.1
 // Project: https://github.com/rhysd/rehype-react
 // Definitions by: Adrian Kremer <https://github.com/adriankremer>
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
 // TypeScript Version: 2.8
 
-import React from "react";
+import * as React from "react";
 
-export interface Options {
-    createElement: typeof React.createElement,
+interface Options {
+    createElement: typeof React.createElement;
     components: {
-        [tagName: string]: React.ComponentType<any>
-    },
-    prefix?: string
+        [tagName: string]: React.ComponentType<any>;
+    };
+    prefix?: string;
 }
 
-export default class RehypeReact {
+declare class RehypeReact {
     constructor(options: Options)
-    Compiler: (node: any) => any
+    Compiler: (node: any) => any;
 }
+
+export = RehypeReact;
