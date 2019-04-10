4b3d5689ded35ea92df050a4db6be383e7f07d99
Haseeb Majid <hmajid2301@gmail.com>
2019-04-10 20:22:34 +0100

    Fixed some bugs

--- types/react-native-dotenv/index.d.ts
+++ types/react-native-dotenv/index.d.ts
@@ -2,9 +2,12 @@
 // Project: https://github.com/zetachang/react-native-dotenv
 // Definitions by: hmajid2301 <https://github.com/hmajid2301>
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
+// TypeScript Version: 3.0
 
 interface Env {
     [name: string]: string;
 }
 
-export const env: Env;
+declare const env: Env;
+
+export { env };
