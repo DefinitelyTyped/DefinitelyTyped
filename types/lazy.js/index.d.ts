8bc3117bd7735f5f235111707463686f51e4f0de
gablorquet <g.lorquet@geotraceability.com>
2019-04-10 11:29:47 -0400

    Updated file header

--- types/lazy.js/index.d.ts
+++ types/lazy.js/index.d.ts
@@ -1,6 +1,8 @@
-// Type definitions for Lazy.js 0.3.4
+// Type definitions for Lazy.js 0.5.1
 // Project: https://github.com/dtao/lazy.js/
-// Definitions by: Bart van der Schoor <https://github.com/Bartvds>, Mike Doughty <https://github.com/miso440>
+// Definitions by: Bart van der Schoor <https://github.com/Bartvds>
+//                 Mike Doughty <https://github.com/miso440>
+//                 Gabriel Lorquet <https://github.com/gablorquet>
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
 
 declare namespace LazyJS {
