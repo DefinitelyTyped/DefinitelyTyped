b766a9b8b7650d1399a7d07cd7bd04617e8e28be
Luis Herranz <luisherranz@gmail.com>
2019-04-10 15:37:06 +0200

    add myself in definitions

--- types/loadable__server/index.d.ts
+++ types/loadable__server/index.d.ts
@@ -1,6 +1,7 @@
 // Type definitions for @loadable/server 5.3
 // Project: https://github.com/smooth-code/loadable-components
 // Definitions by: Martynas Kadiša <https://github.com/martynaskadisa>
+//                 Luis Herranz <https://github.com/luisherranz>
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
 // TypeScript Version: 2.8
 
