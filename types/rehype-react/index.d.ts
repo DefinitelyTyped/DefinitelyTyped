454b252dbe6425b81f032209362abb220eeb9763
Adrian Kremer <adrian.kremer@namics.com>
2019-04-10 13:17:46 +0200

    rehype-react typings

---
+++ types/rehype-react/index.d.ts
@@ -0,0 +1,20 @@
+// Type definitions for rehype-react 2.0
+// Project: https://github.com/rhysd/rehype-react
+// Definitions by: Adrian Kremer <https://github.com/adriankremer>
+// Definitions: https://github.com/adriankremer/DefinitelyTyped
+/* tslint:disable:no-unnecessary-generics */
+
+import React from "react";
+
+export interface Options {
+    createElement: typeof React.createElement,
+    components: {
+        [tagName: string]: React.ComponentType<any>
+    },
+    prefix?: string
+}
+
+export default class RehypeReact {
+    constructor(options: Options)
+    Compiler: (node: any) => any
+}
