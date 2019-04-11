fcedb2754c99c3241c3aa21501c6d4d1f9c3d4f3
Lenart Bezek <lenart.bezek@erpium.com>
2019-04-11 10:30:34 +0200

    Add mapReduce test.

--- types/mongodb/mongodb-tests.ts
+++ types/mongodb/mongodb-tests.ts
@@ -317,3 +317,29 @@ mongodb.connect(connectionString).then((client) => {
         runTransactionWithRetry(updateEmployeeInfo, client, session)
     );
 });
+
+// https://docs.mongodb.com/manual/core/map-reduce/
+
+// Declare emit function to be called inside map function
+declare function emit(key: any, value: any): void;
+
+interface ITestMapReduceSchema {
+    cust_id: string;
+    amount: number;
+    status: string;
+}
+
+function testCollectionMapFunction(this: ITestMapReduceSchema) {
+    emit(this.cust_id, this.amount);
+}
+
+function testCollectionReduceFunction(_key: string, values: number[]): number {
+    return values.reduce((a, v) => a + v, 0);
+}
+
+mongodb.connect(connectionString).then((client) => {
+    client.db("test").collection<ITestMapReduceSchema>('test-mapReduce-collection').mapReduce(
+        testCollectionMapFunction,
+        testCollectionReduceFunction
+    );
+});
