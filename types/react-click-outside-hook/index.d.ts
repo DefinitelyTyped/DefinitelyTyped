01b4d030bd8828e90461a31fb495f4ce67652b37
Bryan Deloeste <bryandeloeste@gmail.com>
2019-04-11 14:27:01 -0700

    add ts version

--- types/react-click-outside-hook/index.d.ts
+++ types/react-click-outside-hook/index.d.ts
@@ -2,7 +2,7 @@
 // Project: https://github.com/bdeloeste/react-click-outside-hook#readme
 // Definitions by: Bryan Deloeste <https://github.com/bdeloeste>
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
-
+// TypeScript Version: 2.8
 import { MutableRefObject } from "react";
 
 export function useClickOutside<T>(): [MutableRefObject<T>, boolean];
