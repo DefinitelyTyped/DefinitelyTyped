0a8e40f85a5a0edcedf2f19ea47be0816984a9df
gablorquet <g.lorquet@geotraceability.com>
2019-04-11 12:16:54 -0400

    Typescript goes under the header

--- types/lazy.js/index.d.ts
+++ types/lazy.js/index.d.ts
@@ -1,10 +1,10 @@
-// TypeScript Version: 2.1
 // Type definitions for Lazy.js 0.5.1
 // Project: https://github.com/dtao/lazy.js/
 // Definitions by: Bart van der Schoor <https://github.com/Bartvds>
 //                 Mike Doughty <https://github.com/miso440>
 //                 Gabriel Lorquet <https://github.com/gablorquet>
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
+// TypeScript Version: 2.1
 
 declare namespace LazyJS {
     interface LazyStatic {
