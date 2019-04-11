156e6916d5dfd10b74ead00645c137a90e0dd9ba
Jason Cordial <jpcordial@gmail.com>
2019-04-10 21:04:23 -0400

    Adds small changes to cookie-sessions types.

--- types/cookie-session/index.d.ts
+++ types/cookie-session/index.d.ts
@@ -1,8 +1,8 @@
 // Type definitions for cookie-session 2.0
 // Project: https://github.com/expressjs/cookie-session
-// Definitions by: Borislav Zhivkov <https://github.com/borislavjivkov>
+// Definitions by:  Borislav Zhivkov <https://github.com/borislavjivkov>, Jason Cordial <https://github.com/jcordial>
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
-// TypeScript Version: 2.2
+// TypeScript Version: 2.9
 
 /// <reference types="express" />
 /// <reference types="keygrip" />
