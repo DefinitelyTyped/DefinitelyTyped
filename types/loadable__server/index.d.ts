ab54e2bb19243722ef4e65001d3cd29783a84c6b
Luis Herranz <luisherranz@gmail.com>
2019-04-11 19:46:21 +0200

    remove patch from version

--- types/loadable__server/index.d.ts
+++ types/loadable__server/index.d.ts
@@ -1,4 +1,4 @@
-// Type definitions for @loadable/server 5.8.0
+// Type definitions for @loadable/server 5.8
 // Project: https://github.com/smooth-code/loadable-components
 // Definitions by: Martynas Kadiša <https://github.com/martynaskadisa>
 //                 Luis Herranz <https://github.com/luisherranz>
