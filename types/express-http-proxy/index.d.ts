a9f19f17074e114d364253f9c86e1dadd8a5ccd2
Lindsay Wardell <three060@gmail.com>
2019-04-10 19:06:33 -0700

    Fixed version number for Travis

--- types/express-http-proxy/index.d.ts
+++ types/express-http-proxy/index.d.ts
@@ -1,4 +1,4 @@
-// Type definitions for express-http-proxy 1.5.1
+// Type definitions for express-http-proxy 1.5
 // Project: https://github.com/villadora/express-http-proxy#readme
 // Definitions by:  ulrichb <https://github.com/ulrichb>
 //                  Daniel Schopf <https://github.com/Danscho>
