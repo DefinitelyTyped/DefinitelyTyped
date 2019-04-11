dcceb8d800c34f28c81d0684d73b55d9afcc6634
Sascha Zarhuber <sascha.zarhuber@gmail.com>
2019-04-11 21:07:34 +0200

    fix(react-sketchapp): reset typescript version & tsconfig to initial state

--- types/react-sketchapp/index.d.ts
+++ types/react-sketchapp/index.d.ts
@@ -4,7 +4,7 @@
 //                 DomiR <https://github.com/DomiR>
 //                 Sascha Zarhuber <https://github.com/saschazar21>
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
-// TypeScript Version: 3.3
+// TypeScript Version: 2.8
 
 import * as React from 'react';
 
