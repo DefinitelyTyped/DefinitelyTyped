6d8ac2682d4ac181e26b65d15c900bfbe9da2eba
Saeed Tabrizi <saeed.tabrizi@gmail.com>
2019-04-11 17:03:07 +0430

    Update index.d.ts

--- types/orientjs/index.d.ts
+++ types/orientjs/index.d.ts
@@ -2,11 +2,10 @@
 // Project: https://github.com/orientechnologies/orientjs
 // Definitions by: [Saeed Tabrizi] <https://github.com/saeedtabrizi>
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
-
+// TypeScript Version: 2.8
 // Last Update  : 11-4-2019
 // Compatible with Orientdb >= 2.2.x and orientjs <= 3.0.x features 
 // Developed with love in www.nowcando.com
-//
 
 /// <reference types="node" />
 
