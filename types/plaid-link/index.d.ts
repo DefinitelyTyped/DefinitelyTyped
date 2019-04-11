468972f245df8ff4194ea1dd72f3e95a0c80831c
afholderman <aaron@pieinsurance.com>
2019-04-11 11:56:29 -0600

    delete comments

--- types/plaid-link/index.d.ts
+++ types/plaid-link/index.d.ts
@@ -4,11 +4,6 @@
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped/
 // TypeScript Version: 2.4
 
-/*~ If this module is a UMD module that exposes a global variable 'myLib' when
- *~ loaded outside a module loader environment, declare that global here.
- *~ Otherwise, delete this declaration.
- */
-
 declare global {
     interface Window {
         Plaid: {
