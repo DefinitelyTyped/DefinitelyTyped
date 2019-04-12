522c720b8508b844b93a963a0d2247243002225b
vikram-gsu <vpareddy1@student.gsu.edu>
2019-04-12 07:45:02 -0400

    Fixed linting bugs and updated to React.ReactElement

--- types/react-leaflet-sidebarv2/index.d.ts
+++ types/react-leaflet-sidebarv2/index.d.ts
@@ -1,4 +1,4 @@
-// Type definitions for react-leaflet-sidebarv2
+// Type definitions for react-leaflet-sidebarv2 0.6
 // Project: https://github.com/condense/react-leaflet-sidebarv2
 // Definitions by: Vikram Pareddy <https://github.com/vikram-gsu>
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
@@ -10,20 +10,21 @@ type Icon = string | React.ElementType;
 type Anchor = 'top' | 'bottom';
 type Position = 'left' | 'right';
 
-interface TabProps{
+interface TabProps {
   id: string;
   header: string;
-  icon:Icon;
+  icon: Icon;
   anchor?: Anchor;
   disabled?: boolean;
   onClose?: () => void;
   closeIcon?: Icon;
   position?: Position;
-  active?: boolean
-};
-declare class Tab extends React.Component<TabProps, any>{};
+  active?: boolean;
+}
 
-type TabType =  React.Element<Tab> | Array<React.Element<Tab>>;
+declare class Tab extends React.Component<TabProps, any> {}
+
+type TabType = React.ReactElement<Tab> | Array<React.ReactElement<Tab>>;
 
 interface SidebarProps {
   id: string;
@@ -31,11 +32,11 @@ interface SidebarProps {
   position: Position;
   selected: string;
   closeIcon?: Icon;
-  onClose?:() => void;
+  onClose?: () => void;
   onOpen?: () => void;
-  children: TabType
-};
+  children: TabType;
+}
 
-declare class Sidebar extends React.Component<SidebarProps, any>{};
+declare class Sidebar extends React.Component<SidebarProps, any>{}
 
-export {Tab, Sidebar};
+export { Tab, Sidebar };
--- types/react-leaflet-sidebarv2/react-leaflet-sidebarv2-tests.tsx
+++ types/react-leaflet-sidebarv2/react-leaflet-sidebarv2-tests.tsx
@@ -2,22 +2,22 @@ import * as React from 'react';
 import { Sidebar, Tab } from 'react-leaflet-sidebarv2';
 
 const MySidebar = (
-  <Sidebar 
-    id='sidebar' 
-    collapsed={false} 
-    selected={'home'} 
-    closeIcon='fa' 
+  <Sidebar
+    id='sidebar'
+    collapsed={false}
+    selected={'home'}
+    closeIcon='fa'
     position='right'>
-    <Tab 
-      id='home' 
-      header='Home' 
+    <Tab
+      id='home'
+      header='Home'
       icon='fa fa-home'>
       No place like home!
     </Tab>
-    <Tab 
-      id='settings' 
-      header='Settings' 
-      icon='fa fa-cog' 
+    <Tab
+      id='settings'
+      header='Settings'
+      icon='fa fa-cog'
       anchor='bottom'>
       In Settings.
     </Tab>
