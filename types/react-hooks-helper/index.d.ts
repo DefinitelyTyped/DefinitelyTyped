8124fdf3f119d6d60f112db28bd920a7209192b2
jedmundo <joao.a.edmundo@gmail.com>
2019-04-13 14:10:37 +0200

    fix: react hooks helper test

--- types/react-hooks-helper/index.d.ts
+++ types/react-hooks-helper/index.d.ts
@@ -1,10 +1,7 @@
-// Type definitions for react-hooks-helper 1.6.0
-// Project: https://github.com/revelcw/react-hooks-helper
+// Type definitions for react-hooks-helper 1.6
+// Project: https://github.com/revelcw/react-hooks-helper#readme
 // Definitions by: Joao Edmundo <https://github.com/jedmundo>
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
-// TypeScript Version: 3.0
-
-///<reference types="react"/>
 
 declare module 'react-hooks-helper' {
 
