53190d2c684b902cfffa4c6336e39d418f56310a
jedmundo <joao.a.edmundo@gmail.com>
2019-04-13 14:10:37 +0200

    fix: react hooks helper test

--- types/react-hooks-helper/react-hooks-helper-tests.tsx
+++ types/react-hooks-helper/react-hooks-helper-tests.tsx
@@ -1,24 +1,22 @@
 import * as React from 'react';
 import { useStep, useForm } from 'react-hooks-helper';
 
-export class ReactHooksHelperTest extends React.Component {
-    public render() {
-        const { index, navigation } = useStep({ steps: 3 })
-        const [formData, setForm] = useForm({ name: '' , city: ''})
-        return (
-            <div>
-                <div id="step-1">
-                    Step: {index}
-                    <input name="name" onChange={setForm} />
-                    <button onCLick={navigation.next}>Next</button>
-                </div>
-                <div id="step-2">
-                    Step: {index}
-                    <input name="city" onChange={setForm} />
-                    <button onCLick={navigation.previous}>Back</button>
-                </div>
-                { JSON.stringify(formData) }
+export const ReactHooksHelperTest: React.FunctionComponent = () => {
+    const { index, navigation } = useStep({ steps: 3 })
+    const [formData, setForm] = useForm({ name: '', city: '' })
+    return (
+        <div>
+            <div id="step-1">
+                Step: {index}
+                <input name="name" onChange={setForm} />
+                <button type="button" onCLick={navigation.next}>Next</button>
             </div>
-        );
-    }
+            <div id="step-2">
+                Step: {index}
+                <input name="city" onChange={setForm} />
+                <button onCLick={navigation.previous}>Back</button>
+            </div>
+            {JSON.stringify(formData)}
+        </div>
+    )
 }
