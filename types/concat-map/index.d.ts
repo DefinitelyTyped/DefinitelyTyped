d1610f6dedcd5654d883a02dd1db496c1f9409fd
Claas Ahlrichs <claasahl@gmail.com>
2019-04-14 14:36:06 +0200

    npx dts-gen -m concat-map --dt

---
+++ types/concat-map/index.d.ts
@@ -0,0 +1,15 @@
+// Type definitions for concat-map 0.0
+// Project: https://github.com/substack/node-concat-map
+// Definitions by: My Self <https://github.com/me>
+// Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
+
+export = concat_map;
+
+declare function concat_map(xs: any, fn: any): any;
+
+declare namespace concat_map {
+    const prototype: {
+    };
+
+}
+
