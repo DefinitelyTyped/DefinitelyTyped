b181724e6d72c91456b162955e20e84283692444
Alexandre Paré <hericendre@live.com>
2019-04-13 21:21:19 -0400

    Fix TypeScript version depending on JQuery

--- types/bootstrap-menu/index.d.ts
+++ types/bootstrap-menu/index.d.ts
@@ -2,6 +2,7 @@
 // Project: https://github.com/dgoguerra/bootstrap-menu
 // Definitions by: Alexandre Paré <https://github.com/superheri>
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
+// TypeScript Version: 2.3
 
 /// <reference types="jquery" />
 
