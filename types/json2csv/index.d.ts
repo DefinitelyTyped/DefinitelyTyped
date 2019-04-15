02279fbd13be43528ee726a5b33e967f3cb6e50c
dangoo <daniel.gooss@sinnerschrader.com>
2019-04-15 13:17:38 +0200

    fix: restore original authors name

--- types/json2csv/index.d.ts
+++ types/json2csv/index.d.ts
@@ -1,6 +1,7 @@
 // Type definitions for json2csv 4.4
 // Project: https://github.com/zemirco/json2csv
-// Definitions by: Daniel Gooß <https://github.com/dangoo>
+// Definitions by: Juanjo Diaz <https://github.com/juanjoDiaz>
+//                 Daniel Gooß <https://github.com/dangoo>
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
 // TypeScript Version: 2.2
 
