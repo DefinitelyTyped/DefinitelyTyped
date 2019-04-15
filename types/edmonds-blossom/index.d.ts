5dc146d249d1817085765c906b1ec8af551185a9
John <37978984+johnridesabike@users.noreply.github.com>
2019-04-15 09:16:13 -0400

    Added Edmonds-Blossom type

---
+++ types/edmonds-blossom/index.d.ts
@@ -0,0 +1,6 @@
+// Type definitions for edmonds-blossom 1.0
+// Project: https://github.com/mattkrick/EdmondsBlossom
+// Definitions by: John Jackson <https://github.com/johnridesabike>
+// Definitions: https://github.com/DefinitelyTyped/edmonds-blossom
+export = blossom;
+declare function blossom(edges: number[][], maxCardinality?: number): number[];
