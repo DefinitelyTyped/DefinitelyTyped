0841b6df7973c3a2b6b6cf33c6c64b050ef6da9f
Miles Johnson <miles.johnson@airbnb.com>
2019-04-15 10:02:04 -0700

    Bump to v5.

--- types/storybook__addon-actions/index.d.ts
+++ types/storybook__addon-actions/index.d.ts
@@ -1,4 +1,4 @@
-// Type definitions for @storybook/addon-actions 3.4
+// Type definitions for @storybook/addon-actions 5.0
 // Project: https://github.com/storybooks/storybook, https://github.com/storybooks/storybook/tree/master/addons/actions
 // Definitions by: Joscha Feth <https://github.com/joscha>, June <https://github.com/jicjjang>
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
