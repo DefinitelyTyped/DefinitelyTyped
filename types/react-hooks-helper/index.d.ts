ba9c98bc9e352ee515daff8aaaa434f2c164e76d
jedmundo <joao.a.edmundo@gmail.com>
2019-04-15 07:40:00 +0200

    fix: removed module declaration

--- types/react-hooks-helper/index.d.ts
+++ types/react-hooks-helper/index.d.ts
@@ -2,34 +2,33 @@
 // Project: https://github.com/revelcw/react-hooks-helper#readme
 // Definitions by: Joao Edmundo <https://github.com/jedmundo>
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
+// TypeScript Version: 2.8
 
-declare module 'react-hooks-helper' {
-
-    export interface NavigationProps {
-        next: () => void
-        previous?: () => void
-        go?: (step: number | string) => void
-        play?: () => void
-        pause?: () => void
-    }
-
-    export interface UseStepParams {
-        initialStep?: number
-        autoAdvanceDuration?: number
-        steps: string[] | number
-    }
-
-    export interface UseStepResponse {
-        autoAdvanceDuration: number
-        isPaused: boolean
-        index: number
-        step: number
-        navigation: NavigationProps
-    }
-
-    export function useStep(params: UseStepParams): UseStepResponse
-
-    export type UseFormResponse = [any, any]
-
-    export function useForm(params: any): UseFormResponse
+export interface NavigationProps {
+    next: () => void
+    previous?: () => void
+    go?: (step: number | string) => void
+    play?: () => void
+    pause?: () => void
 }
+
+export interface UseStepParams {
+    initialStep?: number
+    autoAdvanceDuration?: number
+    steps: string[] | number
+}
+
+export interface UseStepResponse {
+    autoAdvanceDuration: number
+    isPaused: boolean
+    index: number
+    step: number
+    navigation: NavigationProps
+}
+
+export function useStep(params: UseStepParams): UseStepResponse
+
+export type UseFormResponse = [any, any]
+
+export function useForm(params: any): UseFormResponse
+
--- types/react-hooks-helper/react-hooks-helper-tests.tsx
+++ types/react-hooks-helper/react-hooks-helper-tests.tsx
@@ -5,11 +5,11 @@ export const ReactHooksHelperTest: React.FunctionComponent = () => {
     const { index, navigation } = useStep({ steps: 3 })
     const [formData, setForm] = useForm({ name: '', city: '' })
     return (
-        <div>
+        <>
             <div id="step-1">
                 Step: {index}
                 <input name="name" onChange={setForm} />
-                <button type="button" onCLick={navigation.next}>Next</button>
+                <button onCLick={navigation.next}>Next</button>
             </div>
             <div id="step-2">
                 Step: {index}
@@ -17,6 +17,6 @@ export const ReactHooksHelperTest: React.FunctionComponent = () => {
                 <button onCLick={navigation.previous}>Back</button>
             </div>
             {JSON.stringify(formData)}
-        </div>
+        </>
     )
 }
