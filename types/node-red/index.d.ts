84f336ba42f6799ff31c63371ebdc8b54cc5fdf7
Bernardo Belchior <bernardo.belchior1@gmail.com>
2019-04-16 12:03:18 +0100

    [node-red] Update contributors.

--- types/node-red/index.d.ts
+++ types/node-red/index.d.ts
@@ -1,9 +1,10 @@
-// Type definitions for node-red 0.17
+// Type definitions for node-red 0.20
 // Project: http://nodered.org
 // Definitions by: Anders E. Andersen <https://github.com/andersea>
 //                 Thomas B. Mørch <https://github.com/tbowmo>
+//                 Bernardo Belchior <https://github.com/bernardobelchior>
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
-// TypeScript Version: 2.2
+// TypeScript Version: 3.4
 
 /// <reference types="node" />
 
