8daaec30aa61fd59680d5a99d701dab3a890cb61
Josh McCullough <joshua.mccullough@gmail.com>
2019-04-16 06:01:56 -0400

    export types

--- types/classnames/index.d.ts
+++ types/classnames/index.d.ts
@@ -10,15 +10,15 @@
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
 // TypeScript Version: 2.3
 
-type ClassValue = string | number | ClassDictionary | ClassArray | undefined | null | boolean;
+export type ClassValue = string | number | ClassDictionary | ClassArray | undefined | null | boolean;
 
-interface ClassDictionary {
+export interface ClassDictionary {
 	[id: string]: any;
 }
 
 // This is the only way I found to break circular references between ClassArray and ClassValue
 // https://github.com/Microsoft/TypeScript/issues/3496#issuecomment-128553540
-interface ClassArray extends Array<ClassValue> { } // tslint:disable-line no-empty-interface
+export interface ClassArray extends Array<ClassValue> { } // tslint:disable-line no-empty-interface
 
 type ClassNamesFn = (...classes: ClassValue[]) => string;
 
@@ -26,5 +26,5 @@ type ClassNamesExport = ClassNamesFn & { default: ClassNamesFn };
 
 declare const classNames: ClassNamesExport;
 
-export = classNames;
+export default classNames;
 export as namespace classNames;
