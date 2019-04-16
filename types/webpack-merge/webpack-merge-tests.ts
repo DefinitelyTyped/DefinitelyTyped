a279560e73edbfe092df14df4dc16e4a1c178e79
yinz <yinzhong.enjoying@bytedance.com>
2019-04-16 13:43:18 +0800

    style: single queto

--- types/webpack-merge/webpack-merge-tests.ts
+++ types/webpack-merge/webpack-merge-tests.ts
@@ -1,53 +1,60 @@
-import { Configuration, HotModuleReplacementPlugin, Plugin } from "webpack";
-import webpackMerge = require("webpack-merge");
+import { Configuration, HotModuleReplacementPlugin, Plugin } from 'webpack';
+import webpackMerge = require('webpack-merge');
 
 const a: Configuration = {
-    entry: "test.js"
+    entry: 'test.js'
 };
 const b: Configuration = {
-    devtool: "source-map"
+    devtool: 'source-map'
 };
 
 const c: Configuration = webpackMerge(a, b);
 const d: Configuration = webpackMerge.smart(a, b);
-const e: Configuration[] = webpackMerge.multiple({ a, b }, { a: c, b: d });
-const f: Configuration = webpackMerge({
-    customizeArray(x: any[], y: any[], key: string): any[] | undefined {
-        if (key === "extensions") {
-            return Array.from(new Set([...x, ...y]));
-        }
-        // Fall back to default merging
-        return undefined;
-    },
-    customizeObject(x: {}, y: {}, key: string): {} | undefined {
-        if (key === "module") {
-            // Custom merging
-            return { ...x, ...y };
-        }
+const e: Configuration[] = webpackMerge.multiple({a, b});
+const f: Configuration = webpackMerge(
+    {
+        customizeArray(x: any[], y: any[], key: string): any[] | undefined {
+            if (key === 'extensions') {
+                return Array.from(new Set([...x, ...y]));
+            }
+            // Fall back to default merging
+            return undefined;
+        },
+        customizeObject(x: {}, y: {}, key: string): {} | undefined {
+            if (key === 'module') {
+                // Custom merging
+                return { ...x, ...y };
+            }
 
-        // Fall back to default merging
-        return undefined;
+            // Fall back to default merging
+            return undefined;
+        }
     }
-})(a, b);
+)(a, b);
 const g: Configuration = webpackMerge({
     customizeArray: webpackMerge.unique(
-        "plugins",
-        ["HotModuleReplacementPlugin"],
+        'plugins',
+        ['HotModuleReplacementPlugin'],
         (plugin: Plugin) => plugin.constructor && plugin.constructor.name
     )
-})(
+})({
+    plugins: [
+        new HotModuleReplacementPlugin()
+    ]
+}, {
+    plugins: [
+        new HotModuleReplacementPlugin()
+    ]
+});
+const h: Configuration = webpackMerge.strategy(
     {
-        plugins: [new HotModuleReplacementPlugin()]
-    },
-    {
-        plugins: [new HotModuleReplacementPlugin()]
+        entry: 'prepend', // or 'replace', defaults to 'append'
+        'module.loaders': 'prepend'
     }
-);
-const h: Configuration = webpackMerge.strategy({
-    entry: "prepend", // or 'replace', defaults to 'append'
-    "module.loaders": "prepend"
-})(a, b);
-const i: Configuration = webpackMerge.smartStrategy({
-    entry: "prepend", // or 'replace'
-    "module.loaders": "prepend"
-})(a, b);
+)(a, b);
+const i: Configuration = webpackMerge.smartStrategy(
+    {
+        entry: 'prepend', // or 'replace'
+        'module.loaders': 'prepend'
+    }
+)(a, b);
