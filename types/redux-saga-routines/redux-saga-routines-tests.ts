7fb9d7e5ccf49d44317e011568418d99cb416790
Alexey Pelykh <alexey.pelykh@gmail.com>
2019-04-16 14:22:02 +0300

    [IMP] redux-saga-routines: support 3.1

--- types/redux-saga-routines/redux-saga-routines-tests.ts
+++ types/redux-saga-routines/redux-saga-routines-tests.ts
@@ -9,6 +9,13 @@ import {
     createRoutine,
 } from 'redux-saga-routines';
 
+import {
+    ActionFunction0,
+    ActionFunction1,
+    ActionFunctionAny,
+    Action
+} from 'redux-actions';
+
 const routine_anyargs = createRoutine('ANYARGS');
 routine_anyargs();
 routine_anyargs('string');
@@ -16,18 +23,37 @@ routine_anyargs(42);
 routine_anyargs.trigger();
 routine_anyargs.trigger('string');
 routine_anyargs.trigger(42);
-routine_anyargs.request(0, '1');
+routine_anyargs.request();
 
 const routine_noargs = createRoutine('NOARGS', () => {});
 routine_noargs();
 routine_noargs.trigger();
 routine_noargs.trigger('string'); // $ExpectError
 routine_noargs.trigger(42); // $ExpectError
-routine_noargs.request(0, '1');
+routine_noargs.request();
 
 const routine_strarg = createRoutine('STRARG', (x: string) => {});
 routine_strarg(); // $ExpectError
 routine_strarg.trigger(); // $ExpectError
 routine_strarg.trigger('string');
 routine_strarg.trigger(42); // $ExpectError
-routine_strarg.request(0, '1');
+routine_strarg.request(); // $ExpectError
+routine_strarg.request('string');
+
+const routine_multiple_creators = createRoutine('MULTIPLE_CREATORS', {
+    trigger: () => {},
+    REQUEST: (arg: number) => arg,
+    success: (a: object, b: object, c: object, d: object, e: number) => e,
+    fulfill: (result: string, content: any) => {},
+});
+routine_multiple_creators();
+routine_multiple_creators.trigger('string'); // $ExpectError
+routine_multiple_creators.request(); // $ExpectError
+routine_multiple_creators.request('string'); // $ExpectError
+routine_multiple_creators.request(42);
+routine_multiple_creators.success();
+routine_multiple_creators.success('string');
+routine_multiple_creators.success(42);
+routine_multiple_creators.fulfill(); // $ExpectError
+routine_multiple_creators.fulfill(1, 'string'); // $ExpectError
+routine_multiple_creators.fulfill('string', {});
