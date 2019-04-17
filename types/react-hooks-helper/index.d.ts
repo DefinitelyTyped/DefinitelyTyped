64fa38f03afb13d00ab8866e4d90d3ef6ef4f6d3
jedmundo <joao.a.edmundo@gmail.com>
2019-04-17 08:44:20 +0200

    fix: export module to umd

--- types/react-hooks-helper/index.d.ts
+++ types/react-hooks-helper/index.d.ts
@@ -4,6 +4,8 @@
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
 // TypeScript Version: 2.8
 
+export as namespace ReactHooksHelper;
+
 export interface NavigationProps {
     next: () => void;
     previous?: () => void;
