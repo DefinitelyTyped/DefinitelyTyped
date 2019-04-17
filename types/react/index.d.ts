4e5d52015238689acdb90aee0e894031637821c4
Matt Tingen <matt.tingen@gmail.com>
2019-04-17 11:06:59 -0400

    Simplify distribution

--- types/react/index.d.ts
+++ types/react/index.d.ts
@@ -2760,7 +2760,6 @@ declare namespace React {
     }
 }
 
-type DistributivePick<T, K extends keyof T> = T extends any ? Pick<T, K> : never;
 // naked 'any' type in a conditional type will short circuit and union both the then/else branches
 // so boolean is only resolved for T = any
 type IsExactlyAny<T> = boolean extends (T extends never ? true : false) ? true : false;
@@ -2770,16 +2769,19 @@ type NotExactlyAnyPropertyKeys<T> = Exclude<keyof T, ExactlyAnyPropertyKeys<T>>;
 
 // Try to resolve ill-defined props like for JS users: props can be any, or sometimes objects with properties of type any
 type MergePropTypes<P, T> =
-    // If props is type any, use propTypes definitions
-    IsExactlyAny<P> extends true ? T :
-        // If declared props have indexed properties, ignore inferred props entirely as keyof gets widened
-        string extends keyof P ? P :
-            // Prefer declared types which are not exactly any
-            & DistributivePick<P, NotExactlyAnyPropertyKeys<P>>
-            // For props which are exactly any, use the type inferred from propTypes if present
-            & DistributivePick<T, Exclude<keyof T, NotExactlyAnyPropertyKeys<P>>>
-            // Keep leftover props not specified in propTypes
-            & DistributivePick<P, Exclude<keyof P, keyof T>>;
+    // Distribute over P in case it is a union type
+    P extends any
+        // If props is type any, use propTypes definitions
+        ? IsExactlyAny<P> extends true ? T :
+            // If declared props have indexed properties, ignore inferred props entirely as keyof gets widened
+            string extends keyof P ? P :
+                // Prefer declared types which are not exactly any
+                & Pick<P, NotExactlyAnyPropertyKeys<P>>
+                // For props which are exactly any, use the type inferred from propTypes if present
+                & Pick<T, Exclude<keyof T, NotExactlyAnyPropertyKeys<P>>>
+                // Keep leftover props not specified in propTypes
+                & Pick<P, Exclude<keyof P, keyof T>>
+        : never;
 
 // Any prop that has a default prop becomes optional, but its type is unchanged
 // Undeclared default props are augmented into the resulting allowable attributes
