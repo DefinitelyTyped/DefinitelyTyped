2a307709502517e9d7fbf15f16eb2decc4bd910b
Merijn Wijngaard <mwijngaard@gmail.com>
2019-04-17 18:56:09 +0200

    Add all current overloads of setInputAction and getInputAction

--- types/cesium/index.d.ts
+++ types/cesium/index.d.ts
@@ -1393,8 +1393,18 @@ declare namespace Cesium {
 
     class ScreenSpaceEventHandler {
         constructor(element?: HTMLCanvasElement);
-        setInputAction(action: (click: { position: Cartesian2 }) => void, type: number, modifier?: number): void;
-        getInputAction(type: number, modifier?: number): () => void;
+        setInputAction(action: (event: { position: Cartesian2 }) => void, type: ScreenSpaceEventType.LEFT_DOWN | ScreenSpaceEventType.LEFT_UP | ScreenSpaceEventType.LEFT_CLICK | ScreenSpaceEventType.LEFT_DOUBLE_CLICK | ScreenSpaceEventType.RIGHT_DOWN | ScreenSpaceEventType.RIGHT_UP | ScreenSpaceEventType.RIGHT_CLICK | ScreenSpaceEventType.MIDDLE_DOWN | ScreenSpaceEventType.MIDDLE_UP | ScreenSpaceEventType.MIDDLE_CLICK, modifier?: number): void;
+        setInputAction(action: (event: { startPosition: Cartesian2, endPosition: Cartesian2 }) => void, type: ScreenSpaceEventType.MOUSE_MOVE, modifier?: number): void;
+        setInputAction(action: (delta: number) => void, type: ScreenSpaceEventType.WHEEL, modifier?: number): void;
+        setInputAction(action: (event: { position1: Cartesian2, position2: Cartesian2 }) => void, type: ScreenSpaceEventType.PINCH_START, modifier?: number): void;
+        setInputAction(action: () => void, type: ScreenSpaceEventType.PINCH_END, modifier?: number): void;
+        setInputAction(action: (event: { distance: { startPosition: Cartesian2, endPosition: Cartesian2 }, angleAndHeight: { startPosition: Cartesian2, endPosition: Cartesian2 }}) => void, type: ScreenSpaceEventType.PINCH_MOVE, modifier?: number): void;
+        getInputAction(type: ScreenSpaceEventType.LEFT_DOWN | ScreenSpaceEventType.LEFT_UP | ScreenSpaceEventType.LEFT_CLICK | ScreenSpaceEventType.LEFT_DOUBLE_CLICK | ScreenSpaceEventType.RIGHT_DOWN | ScreenSpaceEventType.RIGHT_UP | ScreenSpaceEventType.RIGHT_CLICK | ScreenSpaceEventType.MIDDLE_DOWN | ScreenSpaceEventType.MIDDLE_UP | ScreenSpaceEventType.MIDDLE_CLICK, modifier?: number): () => (event: { position: Cartesian2 }) => void;
+        getInputAction(type: ScreenSpaceEventType.MOUSE_MOVE, modifier?: number): () => (event: { startPosition: Cartesian2, endPosition: Cartesian2 }) => void;
+        getInputAction(type: ScreenSpaceEventType.WHEEL, modifier?: number): () => (delta: number) => void;
+        getInputAction(type: ScreenSpaceEventType.PINCH_START, modifier?: number): () => (event: { position1: Cartesian2, position2: Cartesian2 }) => void;
+        getInputAction(type: ScreenSpaceEventType.PINCH_END, modifier?: number): () => () => void;
+        getInputAction(type: ScreenSpaceEventType.PINCH_MOVE, modifier?: number): () => (event: { distance: { startPosition: Cartesian2, endPosition: Cartesian2 }, angleAndHeight: { startPosition: Cartesian2, endPosition: Cartesian2 }}) => void;
         removeInputAction(type: number, modifier?: number): void;
         isDestroyed(): boolean;
         destroy(): void;
@@ -4847,23 +4857,21 @@ declare namespace Cesium {
     }
 
     enum ScreenSpaceEventType {
-        LEFT_DOWN,
-        LEFT_UP,
-        LEFT_CLICK,
-        LEFT_DOUBLE_CLICK,
-        RIGHT_DOWN,
-        RIGHT_UP,
-        RIGHT_CLICK,
-        RIGHT_DOUBLE_CLICK,
-        MIDDLE_DOWN,
-        MIDDLE_UP,
-        MIDDLE_CLICK,
-        MIDDLE_DOUBLE_CLICK,
-        MOUSE_MOVE,
-        WHEEL,
-        PINCH_START,
-        PINCH_END,
-        PINCH_MOVE,
+        LEFT_DOWN = 0,
+        LEFT_UP = 1,
+        LEFT_CLICK = 2,
+        LEFT_DOUBLE_CLICK = 3,
+        RIGHT_DOWN = 5,
+        RIGHT_UP = 6,
+        RIGHT_CLICK = 7,
+        MIDDLE_DOWN = 10,
+        MIDDLE_UP = 11,
+        MIDDLE_CLICK = 12,
+        MOUSE_MOVE = 15,
+        WHEEL = 16,
+        PINCH_START = 17,
+        PINCH_END = 18,
+        PINCH_MOVE = 19,
     }
 
     namespace Simon1994PlanetaryPositions {
