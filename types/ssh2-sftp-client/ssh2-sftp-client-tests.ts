9b1eb526391417f3caebe876e1dd517e44ba9802
Viacheslav Kasikhin <sonwol@yandex.ru>
2019-04-17 16:42:51 +0300

    fix

--- types/ssh2-sftp-client/ssh2-sftp-client-tests.ts
+++ types/ssh2-sftp-client/ssh2-sftp-client-tests.ts
@@ -3,14 +3,12 @@ import * as fs from 'fs';
 
 const client = new Client();
 
-client
-    .connect({
-        host: 'asdb',
-        port: 1234,
-        privateKey: 'my private key rsa in openssh format',
-        readyTimeout: 1000
-    })
-    .then(() => null);
+client.connect({
+    host: 'asdb',
+    port: 1234,
+    privateKey: 'my private key rsa in openssh format',
+    readyTimeout: 1000,
+}).then(() => null);
 
 client.list('/remote/path').then(() => null);
 
@@ -19,6 +17,8 @@ client.exists('/remote/path').then(() => null);
 client.stat('/remote/path').then(() => null);
 
 client.get('/remote/path').then(() => null);
+
+client.fastGet('/remote/path', 'local/path').then(() => null);
 
 client.put('/local/path', '/remote/path').then(() => null);
 client.put(new Buffer('content'), '/remote/path').then(() => null);
