ce5419477781589d73ded6b540e15e92c8608e42
Zhibin Liu <ljqx@live.cn>
2019-04-18 16:53:33 +0800

    update authors

--- types/urijs/index.d.ts
+++ types/urijs/index.d.ts
@@ -1,6 +1,9 @@
 // Type definitions for URI.js 1.19.1
 // Project: https://github.com/medialize/URI.js
-// Definitions by: RodneyJT <https://github.com/RodneyJT>, Brian Surowiec <https://github.com/xt0rted>, Pete Johanson <https://github.com/petejohanson>
+// Definitions by: RodneyJT <https://github.com/RodneyJT>
+//                 Brian Surowiec <https://github.com/xt0rted>
+//                 Pete Johanson <https://github.com/petejohanson>
+//                 Zhibin Liu <https://github.com/ljqx>
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
 // TypeScript Version: 2.3
 
