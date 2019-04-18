38acb2b7b349632a9c0a38fc1b55f461c2a4b457
Elizabeth Samuel <elizs@microsoft.com>
2019-04-18 15:56:44 -0700

    [office-js-preview] (Outlook) Update CategoryDetails description (#34807)
    * [office-js-preview] (Outlook) Update CategoryDetails description
    
    * Add newline

--- types/office-js-preview/index.d.ts
+++ types/office-js-preview/index.d.ts
@@ -10753,6 +10753,7 @@ declare namespace Office {
          * @remarks
          * 
          * **{@link https://docs.microsoft.com/outlook/add-ins/understanding-outlook-add-in-permissions | Minimum permission level}**: ReadItem
+         * 
          * **{@link https://docs.microsoft.com/outlook/add-ins/#extension-points | Applicable Outlook mode}**: Appointment Attendee
          *
          * **Note**: Certain types of files are blocked by Outlook due to potential security issues and are therefore not returned. For more information, see 
@@ -15387,7 +15388,7 @@ declare namespace Office {
     }
 
     /**
-     * Represents an item's category details like name and associated color.
+     * Represents a category's details like name and associated color.
      *
      * [Api set: Mailbox Preview]
      *
