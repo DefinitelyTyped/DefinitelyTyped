9d88a794dc55ea2e644676d514bc9c7ca03b81a8
Claas Ahlrichs <claasahl@gmail.com>
2019-04-19 14:35:03 +0200

    npx dts-gen -m kind-of --dt

---
+++ types/kind-of/index.d.ts
@@ -0,0 +1,15 @@
+// Type definitions for kind-of 6.0
+// Project: https://github.com/jonschlinkert/kind-of
+// Definitions by: My Self <https://github.com/me>
+// Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
+
+export = kind_of;
+
+declare function kind_of(val: any): any;
+
+declare namespace kind_of {
+    const prototype: {
+    };
+
+}
+
