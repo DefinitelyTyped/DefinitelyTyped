8c3315f9aad667f9a558986d92b0a9f8398ef02b
Vladislav Polyakov <39828645+polRk@users.noreply.github.com>
2019-04-19 21:16:11 +0300

    Add sign

--- types/listr/index.d.ts
+++ types/listr/index.d.ts
@@ -1,11 +1,13 @@
 // Type definitions for listr 0.13
 // Project: https://github.com/samverschueren/listr#readme
 // Definitions by: Dusan Radovanovic <https://github.com/durad>
+// Definitions by: Vladislav Polyakov <https://github.com/polRk>
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
 
 /// <reference types="node" />
 
 import * as stream from "stream";
+import {Observable} from "rxjs";
 
 declare namespace Listr {
     interface ListrRenderer {
@@ -24,9 +26,9 @@ declare namespace Listr {
     interface ListrTask {
         title: string;
         output?: string;
-        task: (ctx: any, task: ListrTaskWrapper) => void | string | Promise<any> | stream.Readable | Listr;
-        skip?: (ctx: any, task: ListrTaskWrapper) => boolean | Promise<boolean> | string | void;
-        enabled?: (ctx: any, task: ListrTaskWrapper) => boolean | Promise<boolean>;
+        task: (ctx: any, task: ListrTaskWrapper) => void | string | Promise<any> | stream.Readable | Listr | Observable<any>;
+        skip?: (ctx: any, task: ListrTaskWrapper) => boolean | Promise<boolean> | string | void | Observable<boolean>;
+        enabled?: (ctx: any, task: ListrTaskWrapper) => boolean | Promise<boolean> | Observable<boolean>;
     }
 
     interface ListrTaskWrapper {
