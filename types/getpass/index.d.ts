b84f74691e7cac054515881e057d03d828711ee7
Claas Ahlrichs <claasahl@gmail.com>
2019-04-19 14:04:29 +0200

    npx dts-gen -m getpass --dt

---
+++ types/getpass/index.d.ts
@@ -0,0 +1,13 @@
+// Type definitions for getpass 0.1
+// Project: https://github.com/arekinath/node-getpass#readme
+// Definitions by: My Self <https://github.com/me>
+// Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
+
+export function getPass(opts: any, cb: any): void;
+
+export namespace getPass {
+    const prototype: {
+    };
+
+}
+
