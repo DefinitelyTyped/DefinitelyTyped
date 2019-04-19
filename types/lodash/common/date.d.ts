6aadfc4dcacbda167626020e52c365ab50323573
Nathan Shively-Sanders <293473+sandersn@users.noreply.github.com>
2019-04-19 09:23:37 -0700

    MINIFY 5

--- types/lodash/common/date.d.ts
+++ types/lodash/common/date.d.ts
@@ -1,27 +1,12 @@
 import _ = require("../index");
 declare module "../index" {
-    // now
-
     interface Stat {
-        /**
-         * Gets the number of milliseconds that have elapsed since the Unix epoch (1 January 1970 00:00:00 UTC).
-         *
-         * @return The number of milliseconds.
-         */
         now(): number;
     }
-
     interface Imp<TValue> {
-        /**
-         * @see _.now
-         */
         now(): number;
     }
-
     interface Exp<TValue> {
-        /**
-         * @see _.now
-         */
         now(): Exp<number>;
     }
 }
