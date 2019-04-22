919d8426e2efd1c558252f75d3a292b44e77d5e9
Xiao Liang <yxliang01@users.noreply.github.com>
2019-04-22 21:21:16 +0800

    fix(treeify): types
    - Fixed previously only `object` can be supplied as argument for printing
    - Made `TreeObject` attributes `readonly`
    - Fixed `asTree` `hideFunctions` argument that it should be optional

--- types/treeify/index.d.ts
+++ types/treeify/index.d.ts
@@ -1,27 +1,29 @@
-// Type definitions for treeify 1.0
+// Type definitions for treeify 1.1.0
 // Project: https://github.com/notatestuser/treeify
 // Definitions by: Mike North <https://github.com/mike-north>
+// Definitions by: Xiao Liang <https://github.com/yxliang01>
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
 // TypeScript Version: 2.1
 
 export interface TreeObject {
-    [k: string]: TreeValue;
+    readonly [k: string]: TreeValue;
 }
-export type TreeValue = string | TreeObject;
+export type TreeValue = string | PrintObject;
+export type PrintObject = TreeObject | ReadonlyArray<any>;
 
 export function asTree(
-    treeObj: TreeObject,
+    printObj: PrintObject,
     showValues: boolean,
-    hideFunctions: boolean
+    hideFunctions?: boolean
 ): string;
 
 export function asLines(
-    treeObj: TreeObject,
+    printObj: PrintObject,
     showValues: boolean,
     lineCallback: (line: string) => void
 ): string;
 export function asLines(
-    treeObj: TreeObject,
+    printObj: PrintObject,
     showValues: boolean,
     hideFunctions: boolean,
     lineCallback: (line: string) => void
