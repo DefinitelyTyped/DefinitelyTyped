ed2fc143a697ddea9ca2056f43ce0b189e77e771
Von Waters <von@ideoclick.com>
2019-04-22 17:03:42 -0700

    added name and updated versions to most recent

--- types/mui-datatables/index.d.ts
+++ types/mui-datatables/index.d.ts
@@ -1,9 +1,10 @@
-// Type definitions for mui-datatables 2.0
+// Type definitions for mui-datatables 2.0.0-beta.59
 // Project: https://github.com/gregnb/mui-datatables
 // Definitions by: Jeroen "Favna" Claassens <https://github.com/favna>
 //                 Ankith Konda <https://github.com/ankithkonda>
+//                 Herman "Von" Waters IV <https://github.com/hwatersiv>
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
-// TypeScript Version: 3.1
+// TypeScript Version: 3.4.4
 
 import * as React from 'react';
 
