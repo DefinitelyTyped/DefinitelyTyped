e39d217dde6e29507f7ab55710a08b445e68b54d
Claas Ahlrichs <claasahl@gmail.com>
2019-04-23 19:37:58 +0200

    clarified edge-cases

--- types/kind-of/index.d.ts
+++ types/kind-of/index.d.ts
@@ -12,8 +12,8 @@ declare function kind_of(thing: undefined): "undefined";
 declare function kind_of(thing: null): "null";
 declare function kind_of(thing: boolean): "boolean";
 declare function kind_of(thing: Buffer): "buffer";
-declare function kind_of(thing: number): "number";
-declare function kind_of(thing: string): "string";
+declare function kind_of(thing: number | Number): "number";
+declare function kind_of(thing: string | String): "string";
 declare function kind_of(thing: typeof arguments): "arguments";
 declare function kind_of(thing: Date): "date";
 declare function kind_of(thing: any[]): "array";
@@ -30,7 +30,7 @@ declare function kind_of(
 declare function kind_of(
     thing: (...args: any[]) => any
 ): "function" | "generatorfunction";
-declare function kind_of(thing: symbol): "symbol";
+declare function kind_of(thing: symbol | Symbol): "symbol";
 declare function kind_of(thing: Promise<any>): "promise";
 declare function kind_of(thing: Map<any, any>): "map";
 declare function kind_of(thing: WeakMap<any, any>): "weakmap";
@@ -45,5 +45,5 @@ declare function kind_of(thing: Int32Array): "int32array";
 declare function kind_of(thing: Uint32Array): "uint32array";
 declare function kind_of(thing: Float32Array): "float32array";
 declare function kind_of(thing: Float64Array): "float64array";
-declare function kind_of(thing: any): "object" | string;
+declare function kind_of(thing: object | Object): "object";
 declare function kind_of(thing: any): string;
