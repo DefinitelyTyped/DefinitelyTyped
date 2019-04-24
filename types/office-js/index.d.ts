cb7f176a234e47013a09bf51eea6e74f57d30429
Alex Jerabek <aljerabe@microsoft.com>
2019-04-24 15:05:04 -0700

    Adding version numbers

--- types/office-js/index.d.ts
+++ types/office-js/index.d.ts
@@ -1,4 +1,4 @@
-// Type definitions for Office.js
+// Type definitions for Office.js 1.0
 // Project: https://github.com/OfficeDev/office-js
 // Definitions by: OfficeDev <https://github.com/OfficeDev>, Lance Austin <https://github.com/LanceEA>, Michael Zlatkovsky <https://github.com/Zlatkovsky>, Kim Brandl <https://github.com/kbrandl>, Ricky Kirkham <https://github.com/Rick-Kirkham>, Alex Jerabek <https://github.com/AlexJerabek>, Elizabeth Samuel <https://github.com/ElizabethSamuel-MSFT>
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
