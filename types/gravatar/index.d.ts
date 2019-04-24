59edb4e2197ae234ee3f5c338da7caf2baf298cf
April Arcus <april.arcus@looker.com>
2019-04-24 13:31:10 -0700

    [node-gravatar] add string enums and profile_url()

--- types/gravatar/index.d.ts
+++ types/gravatar/index.d.ts
@@ -1,23 +1,35 @@
 // Type definitions for gravatar v1.8.0
 // Project: https://github.com/emerleite/node-gravatar
 // Definitions by: Denis Sokolov <https://github.com/denis-sokolov>
+//                 April Arcus <https://github.com/aprilarcus>
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
 
-declare namespace GravatarModule {
-  type Options = {
-    d?: string
-    default?: string
-    f?: string
-    forcedefault?: string
-    format?: string
-    protocol?: string
-    r?: string
-    rating?: string
-    s?: string
-    size?: string
+declare module 'gravatar' {
+
+  type URL = string
+
+  type Default = URL | '404' | 'mp' | 'identicon' | 'monsterid' | 'wavatar' | 'retro' | 'robohash' | 'blank'
+
+  type Rating = 'g' | 'pg' | 'r' | 'x'
+
+  interface Options {
+    size?: number,
+    s?: number,
+    default?: Default,
+    d?: Default,
+    rating?: Rating,
+    r?: Rating,
+    forcedefault?: 'y',
+    f?: 'y',
+    protocol?: boolean | 'http' | 'https',
+    cdn?: string
   }
 
-  function url(email: string, options?: Options, forceProtocol?: boolean): string;
-}
+  interface ProfileOptions extends Options {
+    format?: 'json' | 'xml' | 'php' | 'vcf' | 'qr'
+  }
 
-export = GravatarModule;
+  export function url (email: string, options?: Options, https?: boolean): string
+  export function profile_url (email: string, options?: ProfileOptions, https?: boolean): string
+
+}
