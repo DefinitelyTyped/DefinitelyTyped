a7f33feeaa9724593244792c78541605cbb4e6c0
Rigoberto Molina <rigothedev@gmail.com>
2019-04-25 00:33:03 -0700

    Add myself to authors

--- types/react-flatpickr/index.d.ts
+++ types/react-flatpickr/index.d.ts
@@ -2,6 +2,7 @@
 // Project: https://github.com/coderhaoxin/react-flatpickr
 // Definitions by: begincalendar <https://github.com/begincalendar>
 //                 snaveevans <https://github.com/snaveevans>
+//                 rigothedev <https://github.com/rigothedev>
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
 // TypeScript Version: 2.8
 
