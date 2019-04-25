c9300e0c9b0cc535067ac614c4046b7d96398a9c
Vladislav Polyakov <39828645+polRk@users.noreply.github.com>
2019-04-25 10:31:43 +0300

    Update version number

--- types/listr/index.d.ts
+++ types/listr/index.d.ts
@@ -1,4 +1,4 @@
-// Type definitions for listr 0.13
+// Type definitions for listr 0.14
 // Project: https://github.com/samverschueren/listr#readme
 // Definitions by: Dusan Radovanovic <https://github.com/durad>
 // Definitions by: Vladislav Polyakov <https://github.com/polRk>
