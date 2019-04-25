241928599039bce0406a355c1d5505003705a254
Jirka Svoboda <svobik7@gmail.com>
2019-04-25 08:49:28 +0200

    Added own signature

--- types/react-lazyload/index.d.ts
+++ types/react-lazyload/index.d.ts
@@ -1,6 +1,7 @@
 // Type definitions for react-lazyload ver 2.5
 // Project: https://github.com/jasonslyvia/react-lazyload
 // Definitions by: m0a <https://github.com/m0a>
+//                 svobik7 <https://github.com/svobik7>
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
 // TypeScript Version: 2.8
 
