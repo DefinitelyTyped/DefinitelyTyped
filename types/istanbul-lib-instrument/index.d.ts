8b4ae33bfaef4d310138d0a1cc3b71b26746c395
Nathan Shively-Sanders <293473+sandersn@users.noreply.github.com>
2019-04-25 16:05:15 -0700

    Cleanup 20190425 (#34995)
    * Update project urls
    
    * remove jquery-notifier, update react-test-renderer
    
    1. jquery-notifier was types for a type-only package.
    2. react-test-renderer's test incorrectly assumed that <t> was an html
    tag. This is now an error. I updated the test to use <div>, since that's
    a tag I've heard of before.
    
    * Fix space lint
    
    * Use typescript for removing non-npm package
    
    * Reorder names in notNeededpackages.json
    
    * Point to real version of typescript

--- types/istanbul-lib-instrument/index.d.ts
+++ types/istanbul-lib-instrument/index.d.ts
@@ -1,5 +1,5 @@
 // Type definitions for istanbul-lib-instrument 1.7
-// Project: https://github.com/istanbuljs/istanbuljs
+// Project: https://istanbul.js.org, https://github.com/istanbuljs/istanbuljs
 // Definitions by: Jason Cheatham <https://github.com/jason0x43>
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
 // TypeScript Version: 2.8
--- types/istanbul-lib-source-maps/index.d.ts
+++ types/istanbul-lib-source-maps/index.d.ts
@@ -1,5 +1,5 @@
 // Type definitions for istanbul-lib-source-maps 1.2
-// Project: https://github.com/istanbuljs/istanbuljs
+// Project: https://istanbul.js.org, https://github.com/istanbuljs/istanbuljs
 // Definitions by: Jason Cheatham <https://github.com/jason0x43>
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
 // TypeScript Version: 2.4
