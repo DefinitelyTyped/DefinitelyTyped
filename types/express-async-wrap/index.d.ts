91cab8661c9503d7ef46fb4adda1c36df0602984
Remy Oudemans <remy.oudemans@digib.com>
2019-04-26 20:58:50 +0200

    Adds typescript version to express-async-wrap

--- types/express-async-wrap/index.d.ts
+++ types/express-async-wrap/index.d.ts
@@ -2,6 +2,7 @@
 // Project: https://github.com/Greenfields/express-async-wrap#readme
 // Definitions by: Remy Oudemans <https://github.com/remyoudemans>
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
+// TypeScript Version: 2.7
 
 import { NextFunction } from 'connect';
 import { Response, RequestHandler, ErrorRequestHandler, Request } from 'express-serve-static-core';
