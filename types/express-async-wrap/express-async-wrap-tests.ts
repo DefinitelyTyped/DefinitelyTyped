268c9994f373c76d7aa4a175331e29bcfc05885a
Remy Oudemans <remy.oudemans@digib.com>
2019-04-26 20:56:19 +0200

    Adds express-async-wrap properly

--- types/express-async-wrap/express-async-wrap-tests.ts
+++ types/express-async-wrap/express-async-wrap-tests.ts
@@ -1,18 +1,18 @@
 import asyncWrap = require('express-async-wrap');
-import * as express from 'express';
+import express = require('express');
+// import * as express from 'express';
 
-const app = express()
+const app = express();
 
 const promise = new Promise(resolve => {
   setTimeout(() => {
     resolve("Success");
   }, 250);
-})
+});
 
 const asyncHandler = async (_: express.Request, res: express.Response) => {
   await promise;
   res.send('promise awaited');
-}
+};
 
 app.get('/', asyncWrap(asyncHandler));
-
--- types/express-async-wrap/index.d.ts
+++ types/express-async-wrap/index.d.ts
@@ -1,20 +1,17 @@
-// Type definitions for express-async-wrap 1.0.0
-// Project: https://github.com/Greenfields/express-async-wrap
+// Type definitions for express-async-wrap 1.0
+// Project: https://github.com/Greenfields/express-async-wrap#readme
 // Definitions by: Remy Oudemans <https://github.com/remyoudemans>
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
-// TypeScript Version: 2.8
-
-/// <reference types="express-serve-static-core" />
 
 import { NextFunction } from 'connect';
 import { Response, RequestHandler, ErrorRequestHandler, Request } from 'express-serve-static-core';
 
 declare function asyncWrap<ReqType extends Request, ResType extends Response>(
-    handler: (req: ReqType, res: ResType, next: NextFunction) => Promise<void>
-  ): RequestHandler;
+  handler: (req: ReqType, res: ResType, next: NextFunction) => Promise<void>
+): RequestHandler;
 
 declare function asyncWrap<ReqType extends Request, ResType extends Response>(
-    handler: (err: any, req: ReqType, res: ResType, next: NextFunction) => Promise<void>,
-  ): ErrorRequestHandler;
+  handler: (err: any, req: ReqType, res: ResType, next: NextFunction) => Promise<void>,
+): ErrorRequestHandler;
 
 export = asyncWrap;
