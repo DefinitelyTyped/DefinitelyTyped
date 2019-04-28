b7d013afb3b0a9ff3e3c6b12b0951867a400b2a8
coppy <zheqingli@icloud.com>
2019-04-28 10:38:26 +0800

    Update index.d.ts
    为 Page 的 page 参数 提供默认类型
    -----------------------------
    set the default type for the page param

--- types/weixin-app/index.d.ts
+++ types/weixin-app/index.d.ts
@@ -4560,8 +4560,8 @@ declare function Behavior<
  * Page() 函数用来注册一个页面。
  * 接受一个 object 参数，其指定页面的初始数据、生命周期函数、事件处理函数等。
  */
-declare function Page<T extends wx.PageOptions & object>(
-	page: T & ThisType<T & wx.Page>
+declare function Page<T>(
+	page: T & wx.PageOptions & object & ThisType<T & wx.Page>
 ): void;
 /**
  * getCurrentPages() 函数用于获取当前页面栈的实例，
