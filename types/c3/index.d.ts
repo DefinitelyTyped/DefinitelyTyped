ab8e7eb25d007ab2325b9a168815ccdff57abad2
Nate Mara <natemara@gmail.com>
2019-04-28 09:26:43 -0400

    update version and add my name to author list

--- types/c3/index.d.ts
+++ types/c3/index.d.ts
@@ -1,10 +1,11 @@
-// Type definitions for C3js 0.6
+// Type definitions for C3js 0.7
 // Project: http://c3js.org/, https://github.com/c3js/c3
 // Definitions by: Marc Climent <https://github.com/mcliment>
 //                 Gerin Jacob <https://github.com/gerinjacob>
 //                 Bernd Hacker <https://github.com/denyo>
 //                 Dzmitry Shyndzin <https://github.com/dmitryshindin>
 //                 Tim Niemueller <https://github.com/timn>
+//                 Nate Mara <https://github.com/natemara>
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
 // TypeScript Version: 2.3
 
