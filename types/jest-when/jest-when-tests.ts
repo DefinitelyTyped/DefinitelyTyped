6fed0204746d17a2d1a9013b1d95e13894d5b3f3
Alex Bolenok <quassnoi@gmail.com>
2019-04-30 00:04:27 +0300

    Prettier

--- types/jest-when/jest-when-tests.ts
+++ types/jest-when/jest-when-tests.ts
@@ -1,110 +1,135 @@
-import { when } from 'jest-when';
+import { when } from "jest-when";
 
-describe('mock-when test', () => {
-  it('basic usage:', () => {
-    const fn = jest.fn();
-    when(fn).calledWith(1).mockReturnValue('yay!');
+describe("mock-when test", () => {
+    it("basic usage:", () => {
+        const fn = jest.fn();
+        when(fn)
+            .calledWith(1)
+            .mockReturnValue("yay!");
 
-    const result = fn(1);
-    expect(result).toEqual('yay!');
-  });
+        const result = fn(1);
+        expect(result).toEqual("yay!");
+    });
 
-  it('Supports multiple args:', () => {
-    const fn = jest.fn();
-    when(fn).calledWith(1, true, 'foo').mockReturnValue('yay!');
+    it("Supports multiple args:", () => {
+        const fn = jest.fn();
+        when(fn)
+            .calledWith(1, true, "foo")
+            .mockReturnValue("yay!");
 
-    const result = fn(1, true, 'foo');
-    expect(result).toEqual('yay!');
-  });
+        const result = fn(1, true, "foo");
+        expect(result).toEqual("yay!");
+    });
 
-  it('Supports jest matchers:', () => {
-    const fn = jest.fn();
-    when(fn).calledWith(
-      expect.anything(),
-      expect.any(Number),
-      expect.arrayContaining([false])
-    ).mockReturnValue('yay!');
+    it("Supports jest matchers:", () => {
+        const fn = jest.fn();
+        when(fn)
+            .calledWith(expect.anything(), expect.any(Number), expect.arrayContaining([false]))
+            .mockReturnValue("yay!");
 
-    const result = fn('whatever', 100, [true, false]);
-    expect(result).toEqual('yay!');
-  });
+        const result = fn("whatever", 100, [true, false]);
+        expect(result).toEqual("yay!");
+    });
 
-  it('Supports compound declarations:', () => {
-    const fn = jest.fn();
-    when(fn).calledWith(1).mockReturnValueOnce('no').mockReturnValue('yes');
-    when(fn).calledWith(2).mockReturnValue('way?');
-    when(fn).calledWith(3).mockResolvedValueOnce('no');
-    when(fn).calledWith(3).mockResolvedValue('yes');
-    when(fn).calledWith(4).mockRejectedValueOnce('no');
-    when(fn).calledWith(4).mockRejectedValue('yes');
+    it("Supports compound declarations:", () => {
+        const fn = jest.fn();
+        when(fn)
+            .calledWith(1)
+            .mockReturnValueOnce("no")
+            .mockReturnValue("yes");
+        when(fn)
+            .calledWith(2)
+            .mockReturnValue("way?");
+        when(fn)
+            .calledWith(3)
+            .mockResolvedValueOnce("no");
+        when(fn)
+            .calledWith(3)
+            .mockResolvedValue("yes");
+        when(fn)
+            .calledWith(4)
+            .mockRejectedValueOnce("no");
+        when(fn)
+            .calledWith(4)
+            .mockRejectedValue("yes");
 
-    expect(fn(1)).toEqual('no');
-    expect(fn(1)).toEqual('yes');
-    expect(fn(2)).toEqual('way?');
-    expect(fn(3)).resolves.toEqual('no');
-    expect(fn(3)).resolves.toEqual('yes');
-    expect(fn(4)).rejects.toEqual('no');
-    expect(fn(4)).rejects.toEqual('yes');
-  });
+        expect(fn(1)).toEqual("no");
+        expect(fn(1)).toEqual("yes");
+        expect(fn(2)).toEqual("way?");
+        expect(fn(3)).resolves.toEqual("no");
+        expect(fn(3)).resolves.toEqual("yes");
+        expect(fn(4)).rejects.toEqual("no");
+        expect(fn(4)).rejects.toEqual("yes");
+    });
 
-  it('Supports chained calls:', () => {
-    const fn = jest.fn();
-    when(fn)
-      .calledWith(1).mockReturnValue('no')
-      .calledWith(2).mockReturnValue('way?')
-      .calledWith(3).mockReturnValue('yes')
-      .calledWith(4).mockReturnValue('way!');
+    it("Supports chained calls:", () => {
+        const fn = jest.fn();
+        when(fn)
+            .calledWith(1)
+            .mockReturnValue("no")
+            .calledWith(2)
+            .mockReturnValue("way?")
+            .calledWith(3)
+            .mockReturnValue("yes")
+            .calledWith(4)
+            .mockReturnValue("way!");
 
-    expect(fn(1)).toEqual('no');
-    expect(fn(2)).toEqual('way?');
-    expect(fn(3)).toEqual('yes');
-    expect(fn(4)).toEqual('way!');
-    expect(fn(5)).toEqual(undefined);
-  });
+        expect(fn(1)).toEqual("no");
+        expect(fn(2)).toEqual("way?");
+        expect(fn(3)).toEqual("yes");
+        expect(fn(4)).toEqual("way!");
+        expect(fn(5)).toEqual(undefined);
+    });
 
-  it('Supports chained calls with defaults:', () => {
-    const fn = jest.fn();
-    when(fn)
-      .mockReturnValue('nice')
-      .calledWith(1).mockReturnValue('no')
-      .calledWith(2).mockReturnValue('way?')
-      .calledWith(3).mockReturnValue('yes')
-      .calledWith(4).mockReturnValue('way!');
+    it("Supports chained calls with defaults:", () => {
+        const fn = jest.fn();
+        when(fn)
+            .mockReturnValue("nice")
+            .calledWith(1)
+            .mockReturnValue("no")
+            .calledWith(2)
+            .mockReturnValue("way?")
+            .calledWith(3)
+            .mockReturnValue("yes")
+            .calledWith(4)
+            .mockReturnValue("way!");
 
-    expect(fn(1)).toEqual('no');
-    expect(fn(2)).toEqual('way?');
-    expect(fn(3)).toEqual('yes');
-    expect(fn(4)).toEqual('way!');
-    expect(fn(5)).toEqual('nice');
-  });
+        expect(fn(1)).toEqual("no");
+        expect(fn(2)).toEqual("way?");
+        expect(fn(3)).toEqual("yes");
+        expect(fn(4)).toEqual("way!");
+        expect(fn(5)).toEqual("nice");
+    });
 
-  it('Assert the args:', () => {
-    const fn = jest.fn();
-    when(fn).expectCalledWith(1).mockReturnValue('x');
-    let testPassed = false;
-    try {
-      fn(2); // Will throw a helpful jest assertion error with args diff
-    } catch (e) {
-      testPassed = true;
-    }
-    expect(testPassed).toBeTruthy();
-  });
+    it("Assert the args:", () => {
+        const fn = jest.fn();
+        when(fn)
+            .expectCalledWith(1)
+            .mockReturnValue("x");
+        let testPassed = false;
+        try {
+            fn(2); // Will throw a helpful jest assertion error with args diff
+        } catch (e) {
+            testPassed = true;
+        }
+        expect(testPassed).toBeTruthy();
+    });
 
-  it('Accepts a spied method:', () => {
-    class TheClass {
-      theMethod(theArgument: number) {
-        return "real";
-      }
-    }
+    it("Accepts a spied method:", () => {
+        class TheClass {
+            theMethod(theArgument: number) {
+                return "real";
+            }
+        }
 
-    const theInstance = new TheClass();
+        const theInstance = new TheClass();
 
-    const theSpiedMethod = jest.spyOn(theInstance, 'theMethod');
-    when(theSpiedMethod)
-      .calledWith(1)
-      .mockReturnValue('mock');
-    const returnValue = theInstance.theMethod(1);
-    expect(returnValue).toBe('mock');
-    expect(theSpiedMethod).toHaveReturnedWith('mock');
-  });
+        const theSpiedMethod = jest.spyOn(theInstance, "theMethod");
+        when(theSpiedMethod)
+            .calledWith(1)
+            .mockReturnValue("mock");
+        const returnValue = theInstance.theMethod(1);
+        expect(returnValue).toBe("mock");
+        expect(theSpiedMethod).toHaveReturnedWith("mock");
+    });
 });
