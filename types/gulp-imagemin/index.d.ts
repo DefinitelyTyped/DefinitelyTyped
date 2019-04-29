7ffcada87e9112d3c3e6f033f6f57971f406a66f
Takesi Tokugawa <tokugawa.takesi@gmail.com>
2019-04-29 10:39:12 +0900

    version changed

--- types/gulp-imagemin/index.d.ts
+++ types/gulp-imagemin/index.d.ts
@@ -1,4 +1,4 @@
-// Type definitions for gulp-imagemin 4.1
+// Type definitions for gulp-imagemin 7.0
 // Project: https://github.com/sindresorhus/gulp-imagemin#readme
 // Definitions by: Romain Faust <https://github.com/romain-faust>
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
