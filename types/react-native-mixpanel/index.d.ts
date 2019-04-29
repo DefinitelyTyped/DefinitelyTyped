cf44e9935374cd05f9630fc3e5031635cc07c267
Jake Bloom <jake@steady.health>
2019-04-29 15:52:18 -0700

    Updated types for react-native-mixpanel

--- types/react-native-mixpanel/index.d.ts
+++ types/react-native-mixpanel/index.d.ts
@@ -1,25 +1,76 @@
-// Type definitions for react-native-mixpanel 0.1
+// Type definitions for react-native-mixpanel 1.1.1
 // Project: https://github.com/davodesign84/react-native-mixpanel#readme
 // Definitions by: Andrew Makarov <https://github.com/r3nya>
+//                 Jake Bloom     <https://github.com/jakebloom>
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
 // TypeScript Version: 2.2
 
-export function sharedInstanceWithToken(token: string): void;
-export function track(event: string): void;
-export function trackWithProperties(event: string, property: object): void;
-export function createAlias(id: string): void;
-export function identify(id: string): void;
-export function set(property: object): void;
-export function setOnce(property: object): void;
-export function timeEvent(event: string): void;
-export function registerSuperProperties(property: object): void;
-export function registerSuperPropertiesOnce(property: object): void;
-export function trackCharge(id: number): void;
-export function trackChargeWithProperties(id: number, property: object): void;
-export function increment(property: string, value: number): void;
-export function setPushRegistrationId(token: string): void;
-export function initPushHandling(token: string): void;
-export function clearPushRegistrationId(): void;
-export function addPushDeviceToken(token: string): void;
-export function reset(): void;
-export function getDistinctId(handler: (id: string) => void): void;
+export class MixpanelInstance {
+  constructor(apiToken?: string)
+
+  initialize(): Promise<void>;
+  getDistinctId(): Promise<string>;
+  getSuperProperty(propertyName: string): Promise<any>;
+  track(event: string, properties?: object): Promise<void>;
+  flush(): Promise<void>;
+  disableIpAddressGeolocalization(): Promise<void>;
+  alias(alias: string): Promise<void>;
+  identify(userId: string): Promise<void>;
+  timeEvent(event: string): Promise<void>;
+  registerSuperProperties(properties: object): Promise<void>;
+  registerSuperPropertiesOnce(properties: object): Promise<void>;
+  initPushHandling(token: string): Promise<void>;
+  set(properties: object): Promise<void>;
+  setOnce(properties: object): Promise<void>;
+  trackCharge(charge: number): Promise<void>;
+  trackChargeWithProperties(charge: number, properties: object): Promise<void>;
+  increment(property: string, by: number): Promise<void>;
+  union(name: string, properties: any[]): Promise<void>;
+  removePushDeviceToken(deviceToken: object): Promise<void>;
+  removeAllPushDeviceTokens(): Promise<void>;
+  addPushDeviceToken(token: string): Promise<void>;
+
+  // android only
+  setPushRegistrationId(token: string): Promise<void>;
+
+  // android only
+  clearPushRegistrationId(): Promise<void>;
+
+  reset(): Promise<void>;
+}
+
+interface MixpanelAPI {
+  sharedInstanceWithToken(apiToken: string): Promise<void>;
+  getDistinctId(callback: (id?: string) => void): void;
+  getSuperProperty(propertyName: string, callback: (value: any) => void): void;
+  track(event: string): void;
+  trackWithProperties(event: string, properties: object): void;
+  flush(): void;
+  disableIpAddressGeolocalization(): void;
+  createAlias(alias: string): void;
+  identify(userId: string): void;
+  timeEvent(event: string): void;
+  registerSuperProperties(properties: object): void;
+  registerSuperPropertiesOnce(properties: object): void;
+  initPushHandling(token: string): void;
+  set(properties: object): void;
+  setOnce(properties: object): void;
+  removePushDeviceToken(deviceToken: object): void;
+  removeAllPushDeviceTokens(): void;
+  trackCharge(charge: number): void;
+  trackChargeWithProperties(charge: number, properties: object): void;
+  increment(property: string, by: number): void;
+  union(name: string, properties: any[]): void;
+  addPushDeviceToken(token: string): void;
+
+  // android only
+  setPushRegistrationId(token: string): void;
+
+  // android only
+  clearPushRegistrationId(): void;
+
+  reset(): void;
+}
+
+declare const mixpanelApi: MixpanelAPI;
+export default mixpanelApi;
--- types/react-native-mixpanel/react-native-mixpanel-tests.ts
+++ types/react-native-mixpanel/react-native-mixpanel-tests.ts
@@ -1,4 +1,4 @@
-import * as Mixpanel from 'react-native-mixpanel';
+import Mixpanel, { MixpanelInstance } from 'react-native-mixpanel';
 
 Mixpanel.sharedInstanceWithToken('1234567890');
 Mixpanel.track('Event name');
@@ -20,3 +20,47 @@ Mixpanel.clearPushRegistrationId();
 Mixpanel.addPushDeviceToken('1234567890abc');
 Mixpanel.reset();
 Mixpanel.getDistinctId((id) => (id));
+
+const instance = new MixpanelInstance();
+
+instance.initialize().then().catch();
+
+instance.getDistinctId().then().catch();
+
+instance.getSuperProperty('propName').then().catch();
+
+instance.track('event').then().catch();
+
+instance.flush().then().catch();
+
+instance.disableIpAddressGeolocalization().then().catch();
+
+instance.alias('alias').then().catch();
+
+instance.identify('userId').then().catch();
+
+instance.timeEvent('event').then().catch();
+
+instance.registerSuperProperties({properties: 1}).then().catch();
+
+instance.registerSuperPropertiesOnce({properties: 1}).then().catch();
+
+instance.initPushHandling('token').then().catch();
+
+instance.set({properties: 1}).then().catch();
+
+instance.setOnce({properties: 1}).then().catch();
+
+instance.trackCharge(1).then().catch();
+
+instance.trackChargeWithProperties(1, {properties: 1}).then().catch();
+
+instance.increment('property', 1).then().catch();
+
+instance.union('name', [1, 'f', {}]).then().catch();
+
+instance.removePushDeviceToken({token: 'token'}).then().catch();
+
+instance.removeAllPushDeviceTokens().then().catch();
+
+instance.addPushDeviceToken('token').then().catch();
