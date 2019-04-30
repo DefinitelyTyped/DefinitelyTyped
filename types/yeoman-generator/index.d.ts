d14da55db1a64b53dc117bf9f8dda5023895cc7a
devid.farinelli@gmail.com <devid.farinelli@gmail.com>
2019-04-30 17:48:51 +0200

    [yeoman-generator] add async and env.adapter

--- types/yeoman-generator/index.d.ts
+++ types/yeoman-generator/index.d.ts
@@ -6,12 +6,14 @@
 //                 Joshua Cherry <https://github.com/tasadar2>
 //                 Arthur Corenzan <https://github.com/haggen>
 //                 Richard Lea <https://github.com/chigix>
+//                 Devid Farinelli <https://github.com/misterdev>
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
 // TypeScript Version: 2.8
 
 import { EventEmitter } from 'events';
 import * as inquirer from 'inquirer';
 import { Observable } from 'rxjs';
+import { DiffieHellman } from 'crypto';
 
 type Callback = (err: any) => void;
 
@@ -87,7 +89,10 @@ declare class Generator extends EventEmitter {
     constructor(args: string|string[], options: {});
 
     env: {
-        error(...e: Error[]): void
+        error(...e: Error[]): void;
+        adapter: {
+            promptModule: inquirer.PromptModule;
+        };
     };
     args: {};
     resolved: string;
@@ -114,6 +119,7 @@ declare class Generator extends EventEmitter {
 
     // actions/help mixin
     argumentsHelp(): string;
+    async(): () => {};
     desc(description: string): this;
     help(): string;
     optionsHelp(): string;
