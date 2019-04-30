8c300ac9a5de16b31f701a614757f73d3dc32c25
kan_a_17 <39461306+Kan-Artem@users.noreply.github.com>
2019-04-30 21:53:52 +0500

    Fix typos: Cursor#max and Cursor#min
    Referring to the documentation I updated types. See [Cursor#min](http://mongodb.github.io/node-mongodb-native/3.1/api/Cursor.html#min) and [Cursor#max](http://mongodb.github.io/node-mongodb-native/3.1/api/Cursor.html#max)

--- types/mongodb/index.d.ts
+++ types/mongodb/index.d.ts
@@ -1726,7 +1726,7 @@ export class Cursor<T = Default> extends Readable {
     /** http://mongodb.github.io/node-mongodb-native/3.1/api/Cursor.html#map */
     map<U>(transform: (document: T) => U): Cursor<U>;
     /** http://mongodb.github.io/node-mongodb-native/3.1/api/Cursor.html#max */
-    max(max: number): Cursor<T>;
+    max(max: object): Cursor<T>;
     /** http://mongodb.github.io/node-mongodb-native/3.1/api/Cursor.html#maxAwaitTimeMS */
     maxAwaitTimeMS(value: number): Cursor<T>;
     /** http://mongodb.github.io/node-mongodb-native/3.1/api/Cursor.html#maxScan */
@@ -1734,7 +1734,7 @@ export class Cursor<T = Default> extends Readable {
     /** http://mongodb.github.io/node-mongodb-native/3.1/api/Cursor.html#maxTimeMS */
     maxTimeMS(value: number): Cursor<T>;
     /** http://mongodb.github.io/node-mongodb-native/3.1/api/Cursor.html#min */
-    min(min: number): Cursor<T>;
+    min(min: object): Cursor<T>;
     /** http://mongodb.github.io/node-mongodb-native/3.1/api/Cursor.html#next */
     next(): Promise<T | null>;
     next(callback: MongoCallback<T | null>): void;
