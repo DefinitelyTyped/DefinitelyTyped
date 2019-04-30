b2dcb9d76cb312ddc1039a9990c2fc84b70d23d6
yisraelx <yisraelx@gmail.com>
2019-04-30 11:25:48 +0300

    docs(ShowdownOptions): add and fix

--- types/showdown/index.d.ts
+++ types/showdown/index.d.ts
@@ -71,32 +71,80 @@ declare namespace Showdown {
         [meta: string]: string;
     }
 
+    /**
+     * Showdown options
+     *
+     * @see https://github.com/showdownjs/showdown#valid-options
+     * @see https://github.com/showdownjs/showdown/wiki/Showdown-options
+     * @see https://github.com/showdownjs/showdown/blob/master/src/options.js
+     */
     interface ShowdownOptions {
         /**
-         * Omit the trailing newline in a code block. Ex:
+         * Omit the trailing newline in a code block.
+         * By default, showdown adds a newline before the closing tags in code blocks. By enabling this option, that newline is removed.
+         * This option affects both indented and fenced (gfm style) code blocks.
          *
-         * This:
-         *   <code><pre>var foo = 'bar';
-         *   </pre></code>
+         * @example
          *
-         * Becomes this:
-         *   <code><pre>var foo = 'bar';</pre></code>
+         * **input**:
          *
+         * ```md
+         * var foo = 'bar';
+         * ```
+         *
+         * **omitExtraWLInCodeBlocks** = false:
+         *
+         * ```html
+         * <code><pre>var foo = 'bar';
+         * </pre></code>
+         * ```
+         * **omitExtraWLInCodeBlocks** = true:
+         *
+         * ```html
+         * <code><pre>var foo = 'bar';</pre></code>
+         * ```
          * @default false
+         * @since 1.0.0
          */
         omitExtraWLInCodeBlocks?: boolean;
 
         /**
-         * Disable the automatic generation of header ids. Setting to true overrides <strong>prefixHeaderId</strong>.
+         * Disable the automatic generation of header ids.
+         * Showdown generates an id for headings automatically. This is useful for linking to a specific header.
+         * This behavior, however, can be disabled with this option.
          *
+         * @example
+         * **input**:
+         *
+         * ```md
+         * # This is a header
+         * ```
+         *
+         * **noHeaderId** = false
+         *
+         * ```html
+         * <h1 id="thisisaheader">This is a header</h1>
+         * ```
+         *
+         * **noHeaderId** = true
+         *
+         * ```html
+         * <h1>This is a header</h1>
+         * ```
          * @default false
+         * @since 1.1.0
          */
         noHeaderId?: boolean;
 
         /**
          * Use text in curly braces as header id.
          *
+         * @example
+         *  ```md
+         *   ## Sample header {real-id}     will use real-id as id
+         *  ```
          * @default false
+         * @since 1.7.0
          */
         customizedHeaderId?: boolean;
 
@@ -104,7 +152,26 @@ declare namespace Showdown {
          * Generate header ids compatible with github style (spaces are replaced
          * with dashes and a bunch of non alphanumeric chars are removed).
          *
+         * @example
+         * **input**:
+         *
+         * ```md
+         * # This is a header with @#$%
+         * ```
+         *
+         * **ghCompatibleHeaderId** = false
+         *
+         * ```html
+         * <h1 id="thisisaheader">This is a header</h1>
+         * ```
+         *
+         * **ghCompatibleHeaderId** = true
+         *
+         * ```html
+         * <h1 id="this-is-a-header-with-">This is a header with @#$%</h1>
+         * ```
          * @default false
+         * @since 1.5.5
          */
         ghCompatibleHeaderId?: boolean;
 
@@ -114,46 +181,101 @@ declare namespace Showdown {
          * Setting to true will add a generic 'section' prefix.
          *
          * @default false
+         * @since 1.0.0
          */
         prefixHeaderId?: string | boolean;
 
         /**
          * Enable support for setting image dimensions from within markdown syntax.
-         * Examples:
-         *
-         *   ![foo](foo.jpg =100x80)     simple, assumes units are in px
-         *   ![bar](bar.jpg =100x*)      sets the height to "auto"
-         *   ![baz](baz.jpg =80%x5em)  Image with width of 80% and height of 5em
-         *
+         * 
+         * @example
+         * ```md
+         * ![foo](foo.jpg =100x80)   simple, assumes units are in px
+         * ![bar](bar.jpg =100x*)    sets the height to "auto"
+         * ![baz](baz.jpg =80%x5em)  Image with width of 80% and height of 5em
+         * ```
          * @default false
+         * @since 1.1.0
          */
         parseImgDimensions?: boolean;
 
         /**
          * Set the header starting level. For instance, setting this to 3 means that
+         * 
+         * @example
+         *  **input**:
          *
-         *   # foo
+         * ```md
+         * # header
+         * ```
          *
-         * will be parsed as
+         * **headerLevelStart** = 1
          *
-         *   <h3>foo</h3>
+         * ```html
+         * <h1>header</h1>
+         * ```
          *
+         * **headerLevelStart** = 3
+         *
+         * ```html
+         * <h3>header</h3>
+         * ```
          * @default 1
+         * @since 1.1.0
          */
         headerLevelStart?: number;
 
         /**
          * Turning this option on will enable automatic linking to urls.
          *
+         * @example
+         * **input**:
+         *
+         * ```md
+         * some text www.google.com
+         * ```
+         *
+         * **simplifiedAutoLink** = false
+         *
+         * ```html
+         * <p>some text www.google.com</p>
+         * ```
+         *
+         * **simplifiedAutoLink** = true
+         *
+         * ```html
+         * <p>some text <a href="www.google.com">www.google.com</a></p>
+         * ```
          * @default false
+         * @since 1.2.0
          */
         simplifiedAutoLink?: boolean;
 
         /**
          * This option excludes trailing punctuation from autolinking urls.
-         * Punctuation excluded: . ! ? ( ). Only applies if simplifiedAutoLink option is set to true.
+         * Punctuation excluded: . ! ? ( ).
          *
+         * @remarks This option only applies to links generated by {@link Showdown.ShowdownOptions.simplifiedAutoLink}.
+         * @example
+         * **input**:
+         *
+         * ```md
+         *    check this link www.google.com.
+         * ```
+         *
+         * **excludeTrailingPunctuationFromURLs** = false
+         *
+         * ```html
+         * <p>check this link <a href="www.google.com">www.google.com.</a></p>
+         * ```
+         *
+         * **excludeTrailingPunctuationFromURLs** = true
+         *
+         * ```html
+         * <p>check this link <a href="www.google.com">www.google.com</a>.</p>
+         * ```
          * @default false
+         * @since 1.5.1
          */
         excludeTrailingPunctuationFromURLs?: boolean;
 
@@ -161,67 +283,108 @@ declare namespace Showdown {
          * Turning this on will stop showdown from interpreting underscores in the middle of
          * words as <em> and <strong> and instead treat them as literal underscores.
          *
-         * Example:
+         * @example
+         * **input**:
          *
-         *   some text with__underscores__in middle
+         * ```md
+         * some text with__underscores__in middle
+         * ```
          *
-         * will be parsed as
+         * **literalMidWordUnderscores** = false
          *
-         *   <p>some text with__underscores__in middle</p>
+         * ```html
+         * <p>some text with<strong>underscores</strong>in middle</p>
+         * ```
          *
+         * **literalMidWordUnderscores** = true
+         *
+         * ```html
+         * <p>some text with__underscores__in middle</p>
+         * ```
          * @default false
+         * @since 1.2.0
          */
         literalMidWordUnderscores?: boolean;
 
         /**
          * Enable support for strikethrough syntax.
-         * `~~strikethrough~~` as `<del>strikethrough</del>`.
          *
+         * @example
+         *  **syntax**:
+         *
+         * ```md
+         * ~~strikethrough~~
+         * ```
+         *
+         * ```html
+         * <del>strikethrough</del>
+         * ```
          * @default false
+         * @since 1.2.0
          */
         strikethrough?: boolean;
 
         /**
-         * Enable support for tables syntax. Example:
+         * Enable support for tables syntax.
          *
-         *   | h1    |    h2   |      h3 |
-         *   |:------|:-------:|--------:|
-         *   | 100   | [a][1]  | ![b][2] |
-         *   | *foo* | **bar** | ~~baz~~ |
+         * @example
+         *  **syntax**:
          *
-         * See the wiki for more info
-         *
+         * ```md
+         * | h1    |    h2   |      h3 |
+         * |:------|:-------:|--------:|
+         * | 100   | [a][1]  | ![b][2] |
+         * | *foo* | **bar** | ~~baz~~ |
+         * ```
          * @default false
+         * @since 1.2.0
          */
         tables?: boolean;
 
         /**
          * If enabled adds an id property to table headers tags.
          *
+         * @remarks This options only applies if **[tables][]** is enabled.
          * @default false
+         * @since 1.2.0
          */
         tablesHeaderId?: boolean;
 
         /**
-         * Enable support for GFM code block style.
+         * Enable support for GFM code block style syntax (fenced codeblocks).
          *
+         * @example
+         * **syntax**:
+         *
+         *     ```md
+         *        some code here
+         *        ```
          * @default true
+         * @since 1.2.0
          */
         ghCodeBlocks?: boolean;
 
         /**
-         * Enable support for GFM takslists. Example:
+         * Enable support for GFM takslists.
          *
-         *   - [x] This task is done
-         *   - [ ] This is still pending
+         * @example
+         * **syntax**:
          *
+         * ```md
+         *  - [x] This task is done
+         *  - [ ] This is still pending
+         * ```
          * @default false
+         * @since 1.2.0
          */
         tasklists?: boolean;
 
         /**
          * Prevents weird effects in live previews due to incomplete input.
-         *
+         * 
+         * @example
+         * ![awkward effect](http://i.imgur.com/YQ9iHTL.gif)
+         * You can prevent this by enabling this option.
          * @default false
          */
         smoothLivePreview?: boolean;
@@ -231,6 +394,7 @@ declare namespace Showdown {
          * strings in the midst of indented code.
          *
          * @default false
+         * @since 1.4.2
          */
         smartIndentationFix?: boolean;
 
@@ -238,59 +402,226 @@ declare namespace Showdown {
          * Disables the requirement of indenting sublists by 4 spaces for them to be nested,
          * effectively reverting to the old behavior where 2 or 3 spaces were enough.
          *
+         * @example
+         * **input**:
+         *
+         * ```md
+         * - one
+         *   - two
+         *
+         * ...
+         *
+         * - one
+         *     - two
+         * ```
+         *
+         * **disableForced4SpacesIndentedSublists** = false
+         *
+         * ```html
+         * <ul>
+         * <li>one</li>
+         * <li>two</li>
+         * </ul>
+         * <p>...</p>
+         * <ul>
+         * <li>one
+         *    <ul>
+         *        <li>two</li>
+         *    </ul>
+         * </li>
+         * </ul>
+         * ```
+         *
+         * **disableForced4SpacesIndentedSublists** = true
+         *
+         * ```html
+         * <ul>
+         * <li>one
+         *    <ul>
+         *        <li>two</li>
+         *    </ul>
+         * </li>
+         * </ul>
+         * <p>...</p>
+         * <ul>
+         * <li>one
+         *    <ul>
+         *        <li>two</li>
+         *    </ul>
+         * </li>
+         * </ul>
+         * ```
          * @default false
+         * @since 1.5.0
          */
         disableForced4SpacesIndentedSublists?: boolean;
 
         /**
          * Parses line breaks as like GitHub does, without needing 2 spaces at the end of the line.
          *
+         * @example
+         * **input**:
+         *
+         * ```md
+         * a line
+         * wrapped in two
+         * ```
+         *
+         * **simpleLineBreaks** = false
+         *
+         * ```html
+         * <p>a line
+         * wrapped in two</p>
+         * ```
+         *
+         * **simpleLineBreaks** = true
+         *
+         * ```html
+         * <p>a line<br>
+         * wrapped in two</p>
+         * ```
          * @default false
+         * @since 1.5.1
          */
         simpleLineBreaks?: boolean;
 
         /**
          * Makes adding a space between # and the header text mandatory.
          *
+         * @example
+         * **input**:
+         *
+         * ```md
+         * #header
+         * ```
+         *
+         * **requireSpaceBeforeHeadingText** = false
+         *
+         * ```html
+         * <h1 id="header">header</h1>
+         * ```
+         *
+         * **simpleLineBreaks** = true
+         *
+         * ```html
+         * <p>#header</p>
+         * ```
+         *
          * @default false
+         * @since 1.5.3
          */
         requireSpaceBeforeHeadingText?: boolean;
 
         /**
-         * Enables github @mentions, which link to the username mentioned
+         * Enables support for github @mentions, which links to the github profile page of the username mentioned.
          *
+         * @example
+         * **input**:
+         *
+         * ```md
+         * hello there @tivie
+         * ```
+         *
+         * **ghMentions** = false
+         *
+         * ```html
+         * <p>hello there @tivie</p>
+         * ```
+         *
+         * **ghMentions** = true
+         *
+         * ```html
+         * <p>hello there <a href="https://www.github.com/tivie>@tivie</a></p>
+         * ```
          * @default false
+         * @since 1.6.0
          */
         ghMentions?: boolean;
 
         /**
-         * Changes the link generated by @mentions. Showdown will replace {u}
-         * with the username. Only applies if ghMentions option is enabled.
-         * Example: @tivie with ghMentionsOption set to //mysite.com/{u}/profile will
-         * result in <a href="//mysite.com/tivie/profile">@tivie</a>
+         * Changes the link generated by @mentions. `{u}` is replaced by the text of the mentions. Only applies if **[ghMentions][]** is enabled.
          *
+         * @example
+         * **input**:
+         *
+         * ```md
+         * hello there @tivie
+         * ```
+         *
+         * **ghMentionsLink** = https://github.com/{u}
+         *
+         * ```html
+         * <p>hello there <a href="https://www.github.com/tivie>@tivie</a></p>
+         * ```
+         *
+         * **ghMentionsLink** = http://mysite.com/{u}/profile
+         *
+         * ```html
+         * <p>hello there <a href="//mysite.com/tivie/profile">@tivie</a></p>
+         * ```
          * @default https://github.com/{u}
+         * @since 1.6.2
          */
         ghMentionsLink?: string;
 
         /**
          * Open all links in new windows (by adding the attribute target="_blank" to <a> tags).
          *
+         * @example
+         * **input**:
+         *
+         * ```md
+         * [Showdown](http://showdownjs.com)
+         * ```
+         *
+         * **openLinksInNewWindow** = false
+         * ```html
+         * <p><a href="http://showdownjs.com">Showdown</a></p>
+         * ```
+         * 
+         * **openLinksInNewWindow** = true
+         * ```html
+         * <p><a href="http://showdownjs.com" target="_blank">Showdown</a></p>
+         * ```
          * @default false
+         * @since 1.7.0
          */
         openLinksInNewWindow?: boolean;
 
         /**
-         * Support for HTML Tag escaping. ex: \<div>foo\</div>.
+         * Support for HTML Tag escaping.
          *
+         * @example
+         * **input**:
+         *
+         * ```md
+         * \<div>foo\</div>.
+         * ```
+         *
+         * **backslashEscapesHTMLTags** = false
+         * ```html
+         * <p>\<div>foo\</div></p>
+         * ```
+         * 
+         * **backslashEscapesHTMLTags** = true
+         * ```html
+         * <p>&lt;div&gt;foo&lt;/div&gt;</p>
+         * ```
          * @default false
+         * @since 1.7.2
          */
         backslashEscapesHTMLTags?: boolean;
 
         /**
-         * Enable emoji support. Ex: `this is a :smile: emoji.
-         *
+         * Enable emoji support.
+         * 
+         * @example
+         * ```md
+         * this is a :smile: emoji
+         * ```
          * @default false
+         * @see https://github.com/showdownjs/showdown/wiki/Emojis
+         * @since 1.8.0
          */
         emoji?: boolean;
 
@@ -298,28 +629,78 @@ declare namespace Showdown {
          * Enable support for underline. Syntax is double or triple underscores: `__underline word__`. With this option enabled,
          * underscores no longer parses into `<em>` and `<strong>`
          *
+         * @example
+         * **input**:
+         *
+         * ```md
+         * __underline word__
+         * ```
+         *
+         * **underline** = false
+         * ```html
+         * <p><strong>underlined word</strong></p>
+         * ```
+         * 
+         * **underline** = true
+         * ```html
+         * <p><u>underlined word</u></p>
+         * ```
          * @default false
+         * @since 1.8.0
          */
         underline?: boolean;
 
         /**
-         * Outputs a complete html document, including `<html>`, `<head>` and `<body>` tags
+         * Outputs a complete html document, including <html>, <head> and <body> tags' instead of an HTML fragment.
          *
+         * @example
+         * **input**:
+         *
+         * ```md
+         * # Showdown
+         * ```
+         *
+         * **completeHTMLDocument** = false
+         * ```html
+         * <p><strong>Showdown</strong></p>
+         * ```
+         * 
+         * **completeHTMLDocument** = true
+         * ```html
+         * <!DOCTYPE HTML>
+         * <html>
+         * <head>
+         * <meta charset="utf-8">
+         * </head>
+         * <body>
+         * <p><strong>Showdown</strong></p>
+         * </body>
+         * </html>
+         * ```
          * @default false
+         * @since 1.8.5
          */
         completeHTMLDocument?: boolean;
 
         /**
-         * Outputs a complete html document, including `<html>`, `<head>` and `<body>` tags
-         *
+         * Enable support for document metadata (defined at the top of the document
+         * between `«««` and `»»»` or between `---` and `---`).
+         * 
+         * @example
+         *  ```js
+         *  var conv = new showdown.Converter({metadata: true});
+         *  var html = conv.makeHtml(someMd);
+         *  var metadata = conv.getMetadata(); // returns an object with the document metadata
+         * ```
          * @default false
+         * @since 1.8.5
          */
         metadata?: boolean;
 
         /**
-         * Split adjacent blockquote blocks
+         * Split adjacent blockquote blocks.
          *
-         * @default false
+         * @since 1.8.6
          */
         splitAdjacentBlockquotes?: boolean;
 
