ff4433084cd7058408cb8404e7a05d4c2e62cc6e
Yamatomo <mazda13bturbo@gmail.com>
2019-05-01 15:46:22 +0900

    chore: add author

--- types/hard-source-webpack-plugin/index.d.ts
+++ types/hard-source-webpack-plugin/index.d.ts
@@ -1,6 +1,7 @@
 // Type definitions for hard-source-webpack-plugin 1.0
 // Project: https://github.com/mzgoddard/hard-source-webpack-plugin#readme
 // Definitions by: woitechen <https://github.com/woitechen>
+//                 Yama-Tomo <https://github.com/Yama-Tomo>
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
 // TypeScript Version: 2.3
 
