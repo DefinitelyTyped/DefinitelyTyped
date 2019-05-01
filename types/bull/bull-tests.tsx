3269817850fbc99abdb3d620af8cfeaaa7e45187
Julian Hundeloh <jaulz@users.noreply.github.com>
2019-05-01 17:12:56 +0200

    fix: add test

--- types/bull/bull-tests.tsx
+++ types/bull/bull-tests.tsx
@@ -16,6 +16,9 @@ videoQueue.process((job, done) => {
     // job.data contains the custom data passed when the job was created
     // job.jobId contains id of this job.
 
+    // job.opts contains the options the job was created with
+    job.opts;
+
     // transcode video asynchronously and report progress
     job.progress(42);
 
