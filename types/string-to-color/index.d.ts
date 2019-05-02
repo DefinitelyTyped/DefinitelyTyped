8c32cb34bbc04e3e1411d173748f851e14b946f5
Bleser <bleser92@gmail.com>
2019-05-02 16:46:51 +0500

    fix travis errors

--- types/string-to-color/index.d.ts
+++ types/string-to-color/index.d.ts
@@ -1,6 +1,6 @@
 // Type definitions for string-to-color 2.0.1
 // Project: https://github.com/Gustu/string-to-color
-// Definitions by: Bleser https://github.com/Bleser
+// Definitions by: Bleser <https://github.com/Bleser>
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
 
 
