f0b9e11ff656ad7b269b0c3fd3ddf3c345b3a9cf
Bleser <bleser92@gmail.com>
2019-05-02 15:46:07 +0500

    add definition for string-to-color

---
+++ types/string-to-color/index.d.ts
@@ -0,0 +1,10 @@
+// Type definitions for string-to-color 2.0.1
+// Project: https://github.com/Gustu/string-to-color
+// Definitions by: Bleser https://github.com/Bleser
+// Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
+
+
+
+declare module "string-to-color" {
+    export default function generate(obj:any): string;
+}
