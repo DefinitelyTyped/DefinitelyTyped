db8cd7a9ec44ea2f7d73df50d213d89b52f989e5
Andi Pätzold <andipaetzold@users.noreply.github.com>
2019-05-04 00:58:09 +0200

    fix(pdfmake) add myself as author

--- types/pdfmake/index.d.ts
+++ types/pdfmake/index.d.ts
@@ -3,6 +3,7 @@
 // Definitions by: Milen Stefanov <https://github.com/m1llen1um>
 //                 Rajab Shakirov <https://github.com/radziksh>
 //                 Enzo Volkmann <https://github.com/evolkmann>
+//                 Andi Pätzold <https://github.com/andipaetzold>
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
 // TypeScript Version: 2.4
 
