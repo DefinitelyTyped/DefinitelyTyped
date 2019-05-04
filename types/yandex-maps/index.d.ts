364e070004e2276213f4caaf6aa9195a037533b9
Alexander Kachkaev <alexander@kachkaev.ru>
2019-05-04 19:27:02 +0100

    Try fixing CI

--- types/yandex-maps/index.d.ts
+++ types/yandex-maps/index.d.ts
@@ -1385,7 +1385,7 @@ declare namespace ymaps {
 
 				add(object: object): this;
 
-				each(callback: (layer: Layer) => void, context?: object): void;
+				each(callback: (layer: ILayer) => void, context?: object): void;
 
 				getIterator(): IIterator;
 
