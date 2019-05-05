f316a530588aa1302435d1fdc25d1e1abd831640
Ben Carp <carpben@users.noreply.github.com>
2019-05-05 13:37:31 +0300

    Adding DeepReadonly generic

--- types/react/index.d.ts
+++ types/react/index.d.ts
@@ -3005,4 +3005,21 @@ declare global {
             view: React.SVGProps<SVGViewElement>;
         }
     }
+                    
+    
+    export type DeepReadonly<T> =
+    T extends Function ? T :
+    T extends ReadonlyArray<infer R> ? IDRArray<R> :
+    T extends Map<infer K, infer V> ? IDRMap<K, V> :
+    T extends object ? DRObject<T> :
+    T
+
+    interface IDRArray<T> extends ReadonlyArray<DeepReadonly<T>> {}
+
+    type DRObject<T> = {
+    readonly [P in keyof T]: DeepReadonly<T[P]>;
+    }
+
+    interface IDRMap<K, V> extends ReadonlyMap<DeepReadonly<K>, DeepReadonly<V>> {}
+              
 }
