4007cc81152968be986e619e54a733306cd9f935
Jason Cordial <jpcordial@gmail.com>
2019-05-06 12:08:13 -0400

    Pump signature now returns a single stream instead of an array of streams.

--- types/pump/index.d.ts
+++ types/pump/index.d.ts
@@ -5,10 +5,10 @@
 
 /// <reference types="node" />
 
-declare function pump(streams: pump.Stream[], callback?: pump.Callback): pump.Stream[];
+declare function pump(streams: pump.Stream[], callback?: pump.Callback): pump.Stream;
 
 // callback have to be passed as last argument
-declare function pump(...streams: Array<pump.Stream | pump.Callback>): pump.Stream[];
+declare function pump(...streams: Array<pump.Stream | pump.Callback>): pump.Stream;
 
 declare namespace pump {
     type Callback = (err: Error) => any;
