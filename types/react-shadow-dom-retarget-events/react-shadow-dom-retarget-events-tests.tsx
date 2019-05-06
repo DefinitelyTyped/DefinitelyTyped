f3fd6479a79f6971fe37482d7d70e4e078f2f44c
Remco Haszing <remcohaszing@gmail.com>
2019-05-06 16:08:40 +0200

    Use esModuleInterop for react-shadow-dom-retarget-events

--- types/react-shadow-dom-retarget-events/react-shadow-dom-retarget-events-tests.tsx
+++ types/react-shadow-dom-retarget-events/react-shadow-dom-retarget-events-tests.tsx
@@ -1,6 +1,6 @@
 import * as React from 'react';
 import * as ReactDOM from 'react-dom';
-import * as retargetEvents from 'react-shadow-dom-retarget-events';
+import retargetEvents from 'react-shadow-dom-retarget-events';
 
 class App extends React.Component {
     render() {
