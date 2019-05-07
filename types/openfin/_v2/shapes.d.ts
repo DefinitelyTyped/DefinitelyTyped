ba1ff186c14e3c6188c84f3081b8c60cdbb3509e
licui <li@openfin.co>
2019-05-07 18:53:38 -0400

    delete unused file

--- types/openfin/_v2/shapes.d.ts
+++
@@ -1,9 +0,0 @@
-export declare type AnchorType = 'top-left' | 'top-right' | 'bottom-left' | 'bottom-right';
-export interface Bounds {
-    height: number;
-    width: number;
-    top: number;
-    left: number;
-    right?: number;
-    bottom?: number;
-}
