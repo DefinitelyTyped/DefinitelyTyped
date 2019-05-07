15d7711fc2e512ac7de2779a31af7f33c7f75efa
licui <li@openfin.co>
2019-05-07 17:49:34 -0400

    delete unused file

--- types/openfin/_v2/launcher/services.d.ts
+++
@@ -1,7 +0,0 @@
-import { ServiceConfig } from '../transport/wire';
-export interface AppDirectoryEntry {
-    manifest_url: string;
-}
-export declare const lookupServiceUrl: (serviceName: string) => Promise<any>;
-export declare const launchService: (service: ServiceConfig) => Promise<void>;
-export declare const startServices: (services: ServiceConfig[]) => Promise<void>;
