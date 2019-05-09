20b213b9c902c27fef03c0d32e1df8a152167176
Alex Jerabek <38896772+AlexJerabek@users.noreply.github.com>
2019-05-08 17:28:56 -0700

    Removing links (#35263)

--- types/office-js-preview/index.d.ts
+++ types/office-js-preview/index.d.ts
@@ -17356,7 +17356,7 @@ declare namespace OfficeExtension {
 
     /**
      * A Promise object that represents a deferred interaction with the host Office application. 
-     * The publicly-consumable {@link Office.OfficeExtension.Promise} is available starting in ExcelApi 1.2 and WordApi 1.2. 
+     * The publicly-consumable OfficeExtension.Promise is available starting in ExcelApi 1.2 and WordApi 1.2. 
      * Promises can be chained via ".then", and errors can be caught via ".catch". 
      * Remember to always use a ".catch" on the outer promise, and to return intermediary promises so as not to break the promise chain. 
      * When a browser-provided native Promise implementation is available, OfficeExtension.Promise will switch to use the native Promise instead.
