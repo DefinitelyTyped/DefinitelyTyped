0ca38c9224f11d231e192dffb93071b044580f0f
Andrew Kaiser <kaisea.rpi@gmail.com>
2019-05-09 17:16:18 -0400

    bump better-sqlite3 version

--- types/better-sqlite3/index.d.ts
+++ types/better-sqlite3/index.d.ts
@@ -1,4 +1,4 @@
-// Type definitions for better-sqlite3 5.2
+// Type definitions for better-sqlite3 5.4
 // Project: http://github.com/JoshuaWise/better-sqlite3
 // Definitions by: Ben Davies <https://github.com/Morfent>
 //                 Mathew Rumsey <https://github.com/matrumz>
