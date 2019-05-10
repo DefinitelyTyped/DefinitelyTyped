c643a240b7530e610e9b43225237094d259466d4
Robert Brownstein <rbrownstein@rbrownstein-m1.cvent.net>
2019-05-10 10:12:16 -0400

    update tests

--- types/cryptex/cryptex-tests.ts
+++ types/cryptex/cryptex-tests.ts
@@ -1,6 +1,14 @@
 import * as cryptex from "cryptex";
 
-cryptex.decrypt("") // $ExpectType Promise<string>
-cryptex.encrypt("") // $ExpectType Promise<string>
-cryptex.getSecret("") // $ExpectType Promise<string>
-cryptex.getSecrets([""]) // $ExpectType Promise<Array<string>>
+cryptex.update({
+    config: {
+        keySource: "none",
+        algorithm: "plaintext",
+        secretEncoding: "binary",
+        secrets: {
+            "foo": "bar"
+        }
+    }
+});
+
+const value: Promise<string> = cryptex.getSecret("foo");
