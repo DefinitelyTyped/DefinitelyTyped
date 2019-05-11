0b045832fc8fbd5d35161b21f448054a07c5eed6
Ulrich Buchgraber <ulrich.b@gmx.at>
2019-05-12 01:02:58 +0200

    Increase the version number

--- types/proper-lockfile/index.d.ts
+++ types/proper-lockfile/index.d.ts
@@ -1,4 +1,4 @@
-// Type definitions for proper-lockfile 3.0
+// Type definitions for proper-lockfile 4.1
 // Project: https://github.com/moxystudio/node-proper-lockfile
 // Definitions by: Nikita Volodin <https://github.com/qlonik>
 //                 Linus Unnebäck <https://github.com/LinusU>
