9be8e0ad6f7717329bad31b795697ac316a84b86
ExE Boss <3889017+ExE-Boss@users.noreply.github.com>
2019-05-12 16:00:00 +0200

    docs(yargs): Add note about returning `void` from command builder
    Co-Authored-By: Emily Marigold Klassen <forivall@gmail.com>

--- types/yargs/index.d.ts
+++ types/yargs/index.d.ts
@@ -131,6 +131,8 @@ declare namespace yargs {
          * Set `description` to false to create a hidden command. Hidden commands don't show up in the help output and aren't available for completion.
          * @param [builder] Object to give hints about the options that your command accepts.
          * Can also be a function. This function is executed with a yargs instance, and can be used to provide advanced command specific help.
+         *
+         * Note that when `void` is returned, the handler `argv` object type will not include command-specific arguments.
          * @param [handler] Function, which will be executed with the parsed `argv` object.
          */
         command<U = T>(command: string | ReadonlyArray<string>, description: string, builder?: (args: Argv<T>) => Argv<U> | void, handler?: (args: Arguments<U>) => void): Argv<T>;
