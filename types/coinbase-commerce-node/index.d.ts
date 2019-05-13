32ced3cd846e51b6aee5194e82ce040390b4e1cd
Jørgen Vatle <Jorgen@WcAServices.net>
2019-05-13 14:20:49 +0300

    Define CoinbaseCommerceNode Charge resource.

--- types/coinbase-commerce-node/index.d.ts
+++ types/coinbase-commerce-node/index.d.ts
@@ -22,6 +22,79 @@ interface Options {
 }
 
 /**
+ * Key-value object.
+ */
+interface KeyVal {
+    [key: string]: any;
+}
+
+/**
+ * Node callback
+ */
+type Callback = (error: any, response: any) => void;
+
+/**
+ * Fiat currency.
+ */
+type FiatCurrency = 'USD' | 'GBP' | 'EUR' | string;
+
+/**
+ * Crypto currency.
+ */
+type CryptoCurrency = 'BTC' | 'ETH' | 'ETC' | 'BCH' | 'LTC';
+
+/**
+ * Pricing type.
+ */
+type PricingType = 'no_price' | 'fixed_price'
+
+
+/**
+ * Create a charge.
+ *
+ * @link https://commerce.coinbase.com/docs/api/#charge-resource
+ */
+export interface CreateACharge {
+
+    /**
+     * Charge name.
+     * 100 characters or less.
+     */
+    name: string,
+
+    /**
+     * More detailed description of the charge.
+     * 200 characters or less.
+     */
+    description: string,
+
+    /**
+     * Charge pricing type.
+     */
+    pricing_type: PricingType,
+
+    /**
+     * Price in local fiat currency.
+     */
+    local_price?: FiatCurrency;
+
+    /**
+     * Optional key value pairs for your own use.
+     */
+    metadata?: KeyVal,
+
+    /**
+     * Redirect the user to this URL on completion.
+     */
+    redirect_url?: string,
+
+    /**
+     * Redirect the user to this URL on cancel.
+     */
+    cancel_url?: string;
+}
+
+/**
  * Coinbase-Commerce-Node entry point.
  *
  * @link https://github.com/JorgenVatle/coinbase-commerce-node#usage
@@ -32,5 +105,33 @@ export class Client {
      * Setup client.
      */
     public init(apiKey: string, baseApiUrl?: string, apiVersion?: string, timeout?: number): Options;
+
+}
+
+export module resources {
+
+    /**
+     * Charge class extends CreateACharge's properties.
+     */
+    interface Charge extends CreateACharge {}
+
+    /**
+     * Create a charge.
+     *
+     * @link https://commerce.coinbase.com/docs/api/#create-a-charge
+     */
+    class Charge {
+
+        /**
+         * Charge constructor.
+         */
+        public constructor(data: CreateACharge);
+
+        /**
+         * Save Charge to Coinbase Commerce's servers.
+         */
+        save(callback: Callback): Promise<any>;
+
+    }
 
 }
