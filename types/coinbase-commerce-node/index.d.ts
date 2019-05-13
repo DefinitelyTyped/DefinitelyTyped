ee6987479f234bb64594d49cdeb56677b9625a4f
Jørgen Vatle <Jorgen@WcAServices.net>
2019-05-13 13:34:48 +0300

    Add coinbase-commerce-node type index.

---
+++ types/coinbase-commerce-node/index.d.ts
@@ -0,0 +1,5 @@
+// Type definitions for coinbase-commerce-node 1.0.0
+// Project: https://github.com/coinbase/coinbase-commerce-node
+// Definitions by: Jørgen Vatle <https://github.com/JorgenVatle>
+// Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
+
