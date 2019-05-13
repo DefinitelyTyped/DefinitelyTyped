b35c7d98109e36b4edda8ab3b89d15507d31a6ea
Jørgen Vatle <Jorgen@WcAServices.net>
2019-05-13 14:57:04 +0300

    Correct GitHub link for ChargeResource JSDoc in CoinbaseCommerceNode.

--- types/coinbase-commerce-node/index.d.ts
+++ types/coinbase-commerce-node/index.d.ts
@@ -96,6 +96,8 @@ export interface CreateACharge {
 
 /**
  * Charge creation response.
+ *
+ * @link https://commerce.coinbase.com/docs/api/#charge-resource
  */
 type CreateAChargeResponse = any;
 
