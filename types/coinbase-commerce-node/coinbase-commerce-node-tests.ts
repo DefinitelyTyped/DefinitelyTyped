4bb1a6c7a86c9eb78f17176e90a8f634eed037b0
Jørgen Vatle <Jorgen@WcAServices.net>
2019-05-13 21:09:14 +0300

    Add Event tests to CoinbaseCommerceNode.

--- types/coinbase-commerce-node/coinbase-commerce-node-tests.ts
+++ types/coinbase-commerce-node/coinbase-commerce-node-tests.ts
@@ -4,12 +4,14 @@ import {
     CheckoutResource,
     CreateCheckout,
     Client,
+    EventResource,
     CreateCharge,
     resources as Resource
 } from 'coinbase-commerce-node';
 
 const Checkout = Resource.Checkout;
 const Charge = Resource.Charge;
+const Event = Resource.Event;
 
 /**
  * Initialize client.
@@ -251,6 +253,52 @@ Checkout.all({}).then((list) => {
         const id: string = entry.id;
         const resource: 'checkout' = entry.resource;
     });
+});
+
+/**
+ * Event resource.
+ *
+ * @link https://commerce.coinbase.com/docs/api/#event-resource
+ */
+const eventResponseExample: EventResource = {
+    id: '24934862-d980-46cb-9402-43c81b0cdba6',
+    resource: 'event',
+    type: 'charge:created',
+    api_version: '2018-03-22',
+    created_at: '2017-01-31T20:49:02Z',
+    data: {
+        code: '66BEOV2A',
+        name: 'The Sovereign Individual',
+        description: 'Mastering the Transition to the Information Age',
+        hosted_url: 'https://commerce.coinbase.com/charges/66BEOV2A',
+        created_at: '2017-01-31T20:49:02Z',
+        expires_at: '2017-01-31T21:49:02Z',
+        timeline: [
+            {
+                time: '2017-01-31T20:49:02Z',
+                status: 'NEW'
+            }
+        ],
+        metadata: {},
+        pricing_type: 'no_price',
+        payments: [],
+        addresses: {
+            bitcoin: 'mymZkiXhQNd6VWWG7VGSVdDX9bKmviti3U',
+            ethereum: '0x419f91df39951fd4e8acc8f1874b01c0c78ceba6'
+        }
+    }
+};
+
+Event.retrieve(eventResponseExample.id).then((event) => {
+    const type: 'event' = event.resource;
+});
+
+Event.list({}).then((events) => {
+    const type: 'event' = events[0].resource;
+});
+
+Event.all({}).then((events) => {
+    const type: 'event' = events[0].resource;
 });
 
 /**
