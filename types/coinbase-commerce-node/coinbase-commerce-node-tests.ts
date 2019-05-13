6d05f28fca07887ab54d600c6dfef838fac16185
Jørgen Vatle <Jorgen@WcAServices.net>
2019-05-13 21:35:34 +0300

    Update eventResponseExample object in CoinbaseCommerceNode tests.

--- types/coinbase-commerce-node/coinbase-commerce-node-tests.ts
+++ types/coinbase-commerce-node/coinbase-commerce-node-tests.ts
@@ -266,27 +266,7 @@ const eventResponseExample: EventResource = {
     type: 'charge:created',
     api_version: '2018-03-22',
     created_at: '2017-01-31T20:49:02Z',
-    data: {
-        code: '66BEOV2A',
-        name: 'The Sovereign Individual',
-        description: 'Mastering the Transition to the Information Age',
-        hosted_url: 'https://commerce.coinbase.com/charges/66BEOV2A',
-        created_at: '2017-01-31T20:49:02Z',
-        expires_at: '2017-01-31T21:49:02Z',
-        timeline: [
-            {
-                time: '2017-01-31T20:49:02Z',
-                status: 'NEW'
-            }
-        ],
-        metadata: {},
-        pricing_type: 'no_price',
-        payments: [],
-        addresses: {
-            bitcoin: 'mymZkiXhQNd6VWWG7VGSVdDX9bKmviti3U',
-            ethereum: '0x419f91df39951fd4e8acc8f1874b01c0c78ceba6'
-        }
-    }
+    data: chargeResponseExample,
 };
 
 /**
