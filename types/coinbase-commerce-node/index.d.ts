7f9ee06dcfb5a4f5528ebe00936f5db5b3c1ad48
Jørgen Vatle <Jorgen@WcAServices.net>
2019-05-13 13:57:17 +0300

    Define init() method of CoinbaseCommerceNode client.

--- types/coinbase-commerce-node/index.d.ts
+++ types/coinbase-commerce-node/index.d.ts
@@ -3,3 +3,29 @@
 // Definitions by: Jørgen Vatle <https://github.com/JorgenVatle>
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
 
+/**
+ * Client request options.
+ */
+interface Options {
+    url: string;
+    body: string;
+    method: 'GET' | 'POST' | 'DELETE' | 'PUT',
+    timeout: number,
+    headers: {
+        [key: string]: any,
+        'Content-Type': 'application/json',
+        'Accept': 'application/json',
+        'User-Agent': string,
+        'X-CC-Api-Key': string,
+        'X-CC-Version': string,
+    }
+}
+
+export class Client {
+
+    /**
+     * Setup client.
+     */
+    init(apiKey: string, baseApiUrl?: string, apiVersion?: string, timeout?: number): Options;
+
+}
