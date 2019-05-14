ba967e9cda530d058de4a8cd0d2b3323f8e80f8f
jsinglet <jsinglet@gmail.com>
2019-05-14 17:40:15 -0400

    - adding option for parseReqBody - making ProxyOptions visible

--- types/express-http-proxy/index.d.ts
+++ types/express-http-proxy/index.d.ts
@@ -10,20 +10,23 @@
 import { RequestHandler, Request, Response, NextFunction } from "express";
 import { RequestOptions, IncomingHttpHeaders, OutgoingHttpHeaders } from "http";
 
-interface ProxyOptions {
-    /**
-     * The byte limit of the body. This is the number of bytes or any string
-     * format supported by `bytes`, for example `1000`, `'500kb'` or `'3mb'`.
-     * See https://github.com/stream-utils/raw-body/blob/master/index.d.ts
-     */
-    limit?: number | string;
-    proxyErrorHandler?: (err: any, res: Response, next: NextFunction) => any;
-    proxyReqPathResolver?: (req: Request) => string;
-    proxyReqOptDecorator?: (proxyReqOpts: RequestOptions, srcReq: Request) => RequestOptions;
-    userResHeaderDecorator?: (headers: IncomingHttpHeaders, userReq: Request, userRes: Response, proxyReq: Request, proxyRes: Response) => OutgoingHttpHeaders;
-    preserveHostHdr?: boolean;
+declare namespace proxy {
+    interface ProxyOptions {
+        /**
+         * The byte limit of the body. This is the number of bytes or any string
+         * format supported by `bytes`, for example `1000`, `'500kb'` or `'3mb'`.
+         * See https://github.com/stream-utils/raw-body/blob/master/index.d.ts
+         */
+        limit?: number | string;
+        proxyErrorHandler?: (err: any, res: Response, next: NextFunction) => any;
+        proxyReqPathResolver?: (req: Request) => string;
+        proxyReqOptDecorator?: (proxyReqOpts: RequestOptions, srcReq: Request) => RequestOptions;
+        userResHeaderDecorator?: (headers: IncomingHttpHeaders, userReq: Request, userRes: Response, proxyReq: Request, proxyRes: Response) => OutgoingHttpHeaders;
+        preserveHostHdr?: boolean;
+        parseReqBody?: boolean;
+    }
 }
 
-declare function proxy(host: string|((req: Request) => string), options?: ProxyOptions): RequestHandler;
+declare function proxy(host: string|((req: Request) => string), options?: proxy.ProxyOptions): RequestHandler;
 
 export = proxy;
