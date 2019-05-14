2c58b69ae367ab4765d80e9da9cce6142a14632d
orblazer <orblazer.minecraft@gmail.com>
2019-05-14 15:32:20 +0200

    Add lint addon

---
+++ types/codemirror/addon/lint/html-lint.d.ts
@@ -0,0 +1,16 @@
+// Type definitions for CodeMirror
+// Project: https://github.com/codemirror/CodeMirror
+// Definitions by: orblazer <https://github.com/orblazer>
+// Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
+
+// See docs https://codemirror.net/doc/manual.html#active-line
+
+import * as CodeMirror from "codemirror";
+
+declare module "codemirror" {
+    interface Lint {
+        html: Linter;
+    }
+
+    var lint: Lint;
+}
