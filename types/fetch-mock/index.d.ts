8fce723803ac8b01d2ef51d44647bf2171c1c87d
Gregor Martynus <gregor@martynus.net>
2019-05-15 08:52:38 -0700

    fetch-mock: allow headers[name] to be set to number (#35462)

--- types/fetch-mock/index.d.ts
+++ types/fetch-mock/index.d.ts
@@ -82,7 +82,7 @@ declare namespace fetchMock {
         /**
          * Set the response headers.
          */
-        headers?: { [key: string]: string };
+        headers?: { [key: string]: string | number };
 
         /**
          * If this property is present then a Promise rejected with the value
@@ -141,7 +141,7 @@ declare namespace fetchMock {
         /**
          * key/value map of headers to match
          */
-        headers?: { [key: string]: string };
+        headers?: { [key: string]: string | number };
 
         /**
          * key/value map of query strings to match, in any order
