76b7ba68a15e83778935bb3a0a39b504fb00d0ee
Jørgen Vatle <Jorgen@WcAServices.net>
2019-05-15 05:05:16 +0300

    Define minimal compatible TypeScript version for log-process-errors

--- types/log-process-errors/index.d.ts
+++ types/log-process-errors/index.d.ts
@@ -2,6 +2,7 @@
 // Project: https://git.io/fhSGY
 // Definitions by: Jørgen Vatle <https://github.com/JorgenVatle>
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
+// TypeScript Version: 2.1
 
 /**
  * Log level
