84a51fdcd959d2cd92b3ddd4934829f6a70a3745
Guilherme Camillo <guilherme.camillo@booking.com>
2019-05-17 13:36:43 +0200

    Adds tests for <Redirect />

---
+++ types/react-router/test/Redirect.tsx
@@ -0,0 +1,34 @@
+import * as React from "react";
+import { Redirect } from "react-router";
+
+interface State {
+    foo: boolean;
+}
+
+const RedirectWithoutTypedState = () => {
+    return (
+        <Redirect
+            to={{
+                pathname: "/somewhere",
+                state: {
+                    foo: "bar"
+                }
+            }}
+        />
+    );
+};
+
+const RedirectWithTypedState = () => {
+    return (
+        <Redirect<State>
+            to={{
+                pathname: "/somewhere",
+                state: {
+                    foo: true
+                }
+            }}
+        />
+    );
+};
+
+export { RedirectWithoutTypedState, RedirectWithTypedState };
