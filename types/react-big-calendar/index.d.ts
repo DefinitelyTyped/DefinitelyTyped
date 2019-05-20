5b34b818f19dce02978f8d95210ce8cf6e51e9fb
Andy <andyaltepeter@invenshure.com>
2019-05-20 12:06:08 -0500

    React-Big-Calendar: improve typing for Views, specify type for Event, and remove 'extends' for BigCalendarProps which isn't needed anymore (#35517)

--- types/react-big-calendar/index.d.ts
+++ types/react-big-calendar/index.d.ts
@@ -23,15 +23,21 @@ export type stringOrDate = string | Date;
 export type ViewKey = 'MONTH' | 'WEEK' | 'WORK_WEEK' | 'DAY' | 'AGENDA';
 export type View = 'month' | 'week' | 'work_week' | 'day' | 'agenda';
 export type Views = View[] | {
-    day: boolean | React.SFC | React.Component,
-    agenda: boolean | React.SFC | React.Component,
-    month: boolean | React.SFC | React.Component,
-    week: boolean | React.SFC | React.Component,
-    work_week: boolean | React.SFC | React.Component
+    work_week?: boolean | React.SFC | React.Component,
+    day?: boolean | React.SFC | React.Component,
+    agenda?: boolean | React.SFC | React.Component,
+    month?: boolean | React.SFC | React.Component,
+    week?: boolean | React.SFC | React.Component
 };
 export type Navigate = 'PREV' | 'NEXT' | 'TODAY' | 'DATE';
 
-export type Event = object;
+export interface Event {
+    allDay?: boolean;
+    title: string;
+    start: Date;
+    end: Date;
+    resource?: any;
+}
 export interface DateRange {
     start: Date;
     end: Date;
