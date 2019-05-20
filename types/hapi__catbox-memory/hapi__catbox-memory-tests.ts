5d53f6d87eac44c07da3e6a2d7968d22863b73b2
Christopher Crewdson <Christopher.Crewdson@walmartlabs.com>
2019-05-20 16:45:56 -0700

    Add more tests

--- types/hapi__catbox-memory/hapi__catbox-memory-tests.ts
+++ types/hapi__catbox-memory/hapi__catbox-memory-tests.ts
@@ -1,4 +1,5 @@
 import * as CatboxMemory from '@hapi/catbox-memory';
+import { Client } from '@hapi/catbox';
 
 const client = new CatboxMemory<string>({
     allowMixedContent: true,
@@ -8,3 +9,12 @@ const client = new CatboxMemory<string>({
 });
 
 const client2 = new CatboxMemory<string>();
+
+const catboxMemoryOptions: CatboxMemory.Options = {
+    allowMixedContent: true,
+    cloneBuffersOnGet: false,
+    maxByteSize: 1024,
+    minCleanupIntervalMsec: 1000,
+};
+
+const client3 = new Client<string>(CatboxMemory, catboxMemoryOptions);
