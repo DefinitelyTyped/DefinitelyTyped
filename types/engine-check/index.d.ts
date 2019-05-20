2410b43e9104ceac707a229723f4dae379311edd
jgeth <john@gethoefer.com>
2019-05-20 15:37:04 -0700

    Updates engine-check definition for default module export (#35605)
    checkVersion() is the module export from source package
    Updates type definition accordingly and namespaces the config option interface
    Updates tests to reference new definition
    
    Updates version to 1.1

--- types/engine-check/index.d.ts
+++ types/engine-check/index.d.ts
@@ -1,11 +1,14 @@
-// Type definitions for engine-check 1.0
+// Type definitions for engine-check 1.1
 // Project: https://github.com/ppvg/node-engine-check
 // Definitions by: jgeth <https://github.com/jgeth>
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
 
-export interface EngineCheckOptions {
-    searchRoot?: string;
-    silent?: boolean;
-    debug?: boolean;
+declare namespace engineCheck {
+    interface Options {
+        searchRoot?: string;
+        silent?: boolean;
+        debug?: boolean;
+    }
 }
-export function checkVersion(options?: EngineCheckOptions): void;
+declare function engineCheck(options?: engineCheck.Options): void;
+export = engineCheck;
