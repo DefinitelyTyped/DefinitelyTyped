d99b21dd6ff544e07798b611d1199e47105f4856
Marc <marcveens@hotmail.com>
2019-05-21 11:38:22 +0200

    Added types for react-gtm-module

---
+++ types/react-gtm-module/index.d.ts
@@ -0,0 +1,43 @@
+// Type definitions for React Google Tag Manager Module 2.0.4
+// Project: https://github.com/alinemorelli/react-gtm
+// Definitions by: Marc Veens <https://github.com/marcveens>
+// Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
+
+declare module 'react-gtm-module' {
+    const TagManager: {
+        dataLayer: (dataLayerArgs: DataLayerArgs) => void;
+        initialize: (tagManagerArgs: TagManagerArgs) => void;
+    };
+
+    export interface TagManagerArgs extends DataLayerArgs {
+        /**
+         * GTM id, must be something like GTM-000000.
+         */
+        gtmId: string;
+        /**
+         * Additional events such as 'gtm.start': new Date().getTime(),event:'gtm.js'.
+         */
+        events?: object;
+        /**
+         * Used to set environments.
+         */
+        auth?: string;
+        /**
+         * Used to set environments, something like env-00.
+         */
+        preview?: string;
+    }
+
+    export interface DataLayerArgs {
+        /**
+         * Object that contains all of the information that you want to pass to Google Tag Manager.
+         */
+        dataLayer?: object;
+        /**
+         * Custom name for dataLayer object.
+         */
+        dataLayerName?: string;
+    }
+
+    export default TagManager;
+}
