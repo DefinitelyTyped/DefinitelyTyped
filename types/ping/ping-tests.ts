ac6dcc1ebb776fbe5f3594206f1c35f63974a4db
Richard Honor <me@r-honor.uk>
2019-05-21 10:03:05 +0100

    Add definitions for node-ping

---
+++ types/ping/ping-tests.ts
@@ -0,0 +1,40 @@
+import {
+  PingConfig,
+  promise,
+  sys,
+} from 'ping';
+
+const config: PingConfig = {
+    numeric: false,
+    timeout: 1000,
+    min_reply: 5,
+    v6: false,
+    sourceAddr: 'localhost',
+    extra: [],
+};
+
+sys.probe('test', (isAlive, err) => {
+    if (isAlive === true) {
+
+    }
+});
+
+async function test() {
+    const res = await promise.probe('test', config);
+
+    const alive: boolean = res.alive;
+    const host: string = res.host;
+    const output: string = res.output;
+    const min: string = res.min;
+    const max: string = res.max;
+    const avg: string = res.avg;
+    const stddev: string = res.stddev;
+
+    if (res.time !== 'unknown') {
+      const time: number = res.time;
+    }
+
+    if (res.numeric_host) {
+        const numHost: string = res.numeric_host;
+    }
+}
