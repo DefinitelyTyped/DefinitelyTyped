841817cc6877d6c11df88e9d4ba5950f1f5f4da4
Manuel Thalmann <m@nuth.ch>
2019-05-21 10:47:23 +0200

    Remove colliding require-declaration

--- types/requirejs/index.d.ts
+++ types/requirejs/index.d.ts
@@ -419,5 +419,4 @@ interface RequireDefine {
 
 // Ambient declarations for 'require' and 'define'
 declare var requirejs: Require;
-declare var require: Require;
 declare var define: RequireDefine;
