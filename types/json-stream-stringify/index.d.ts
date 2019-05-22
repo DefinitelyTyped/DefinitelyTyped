7a0c206df3feb86d5039f3f6fc9c91025daddc9a
segayuu <segayuu@gmail.com>
2019-05-22 20:42:39 +0900

    Define type JsonStreamStringify class

--- types/json-stream-stringify/index.d.ts
+++ types/json-stream-stringify/index.d.ts
@@ -2,41 +2,10 @@
 // Project: https://github.com/Faleij/json-stream-stringify#readme
 // Definitions by: My Self <https://github.com/me>
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
+/// <reference types="node" />
+import { Readable } from "stream";
 
-/*~ Note that ES6 modules cannot directly export class objects.
- *~ This file should be imported using the CommonJS-style:
- *~   import x = require('someLibrary');
- *~
- *~ Refer to the documentation to understand common
- *~ workarounds for this limitation of ES6 modules.
- */
-
-/*~ If this module is a UMD module that exposes a global variable 'myClassLib' when
- *~ loaded outside a module loader environment, declare that global here.
- *~ Otherwise, delete this declaration.
- */
-export as namespace myClassLib;
-
-/*~ This declaration specifies that the class constructor function
- *~ is the exported object from the file
- */
-export = MyClass;
-
-/*~ Write your module's methods and properties in this class */
-declare class MyClass {
-    constructor(someParam?: string);
-
-    someProperty: string[];
-
-    myMethod(opts: MyClass.MyClassMethodOptions): number;
-}
-
-/*~ If you want to expose types from your module as well, you can
- *~ place them in this block.
- */
-declare namespace MyClass {
-    export interface MyClassMethodOptions {
-        width?: number;
-        height?: number;
-    }
+export default class JsonStreamStringify extends Readable {
+    constructor(value: any, replacer?: ((key: any, value: any) => any) | any[], spaces?: string | number, cycle?: boolean);
+    path(): [string, number];
 }
