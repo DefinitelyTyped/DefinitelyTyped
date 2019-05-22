b856c0ded331f591b54fdd65b56932b15f30b60a
kirick1 <kyrychenko.vladyslav@gmail.com>
2019-05-23 01:52:56 +0300

    fix + tests

---
+++ types/bootbot/bootbot-tests.ts
@@ -0,0 +1,13 @@
+import BootBot from "bootbot";
+
+const bot = new BootBot({
+    accessToken: '0123456789',
+    verifyToken: '0123456789',
+    appSecret: '0123456789'
+})
+
+bot.on('message', (payload, chat) => chat.say(payload.message.text))
+
+bot.start()
+
+setTimeout(() => process.kill(0), 100)
