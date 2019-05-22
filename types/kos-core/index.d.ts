b3ba6a22299ac59d3cf9da9f4596b20403727139
lizhen <lz@choicesoft.com.cn>
2019-05-22 16:34:49 +0800

    Update definitions to fit new version.

--- types/kos-core/index.d.ts
+++ types/kos-core/index.d.ts
@@ -1,32 +1,33 @@
-// Type definitions for kos-core 0.4
+// Type definitions for kos-core 0.6
 // Project: https://github.com/ali-Kos/Kos
 // Definitions by: alibaba ali-Kos <https://github.com/ali-Kos>
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
-// TypeScript Version: 2.8
-import * as React from 'react';
+// TypeScript Version: 3.4
+import * as React from "react";
 
 type ReactComponent<P = any, S = any> = React.ComponentClass<P, S>;
 
 interface Util {
-    getActionType: (action: string) => { namespace: string | null; type: string };
+  getActionType: (action: string) => { namespace: string | null; type: string };
+  getParam: () => any;
 }
 
 interface WrapperConfig {
-    model: KosModel;
-    autoLoad?: boolean;
-    autoReset?: boolean;
-    namespace?: string;
+  model: KosModel;
+  autoLoad?: boolean;
+  autoReset?: boolean;
+  namespace?: string;
 }
 
 interface Action<T = any> {
-    type: string;
-    payload?: Partial<T> & { [x: string]: any };
+  type: string;
+  payload?: Partial<T> & { [x: string]: any };
 }
 
 export interface KosProps<T = any> {
-    dispatch?: (action: Action<T>) => void;
-    getParam?: () => string;
-    getNamespace?: () => string;
+  dispatch?: (action: Action<T>) => void;
+  getParam?: () => any;
+  getNamespace?: () => string;
 }
 
 export type KosDispatch = (action: Action) => void;
@@ -34,30 +35,26 @@ export type KosDispatch = (action: Action) => void;
 export type GetKosState<T = any> = () => T;
 
 export interface KosModel<T = any> {
-    namespace: string;
-    initial: T;
-    reducers: {
-        [key: string]: (state: T, { payload }: { payload: T }) => void;
-    };
-    asyncs: {
-        [key: string]: (dispatch?: KosDispatch, getState?: GetKosState<T>, action?: { payload: T }) => void;
-    };
-    setup?: (dispatch: KosDispatch, getState: GetKosState<T>) => void;
-    getAsync?: (
-        key: string
-    ) => (dispatch: KosDispatch, getState?: GetKosState) => void;
+  namespace: string;
+  initial: T;
+  reducers: {
+    [key: string]: (state: T, { payload }: { payload: T }) => void;
+  };
+  asyncs: {
+    [key: string]: (dispatch?: KosDispatch, getState?: GetKosState<T>, action?: { payload: T }) => void;
+  };
+  setup?: (dispatch: KosDispatch, getState: GetKosState<T>, action: { payload: { param: any } }) => void;
+  getAsync?: (key: string) => (dispatch: KosDispatch, getState?: GetKosState) => void;
 }
 
 interface Kos {
-    Util: Util;
-    registeModel: (model: any) => void;
-    removeModel: (namespace: string) => void;
-    getModel: (namespace: string) => KosModel;
-    use: (middleware: (api: { dispatch: any; getState: any }) => void) => void;
-    Wrapper: (
-        config: WrapperConfig
-    ) => (component: ReactComponent<any & KosProps>) => any;
-    start: (Layout: ReactComponent, container?: string) => void;
+  Util: Util;
+  registeModel: (model: any) => void;
+  removeModel: (namespace: string) => void;
+  getModel: (namespace: string) => KosModel;
+  use: (middleware: (api: { dispatch: any; getState: any }) => void) => void;
+  Wrapper: (config: WrapperConfig) => (component: ReactComponent<any & KosProps>) => any;
+  start: (Layout: ReactComponent, container?: string) => void;
 }
 
 declare let KOS: Kos;
