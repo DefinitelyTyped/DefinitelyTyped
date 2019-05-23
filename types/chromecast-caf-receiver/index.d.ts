d5ca8351334dbf029aef9175664ae4f27ea8d3ce
Sergio Arbeo <sarbeo@apple.com>
2019-05-23 21:47:53 +0200

    Bump version

--- types/chromecast-caf-receiver/index.d.ts
+++ types/chromecast-caf-receiver/index.d.ts
@@ -2,7 +2,7 @@
 // Project: https://github.com/googlecast
 // Definitions by: Craig Bruce <https://github.com/craigrbruce>
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
-// TypeScript Version: 2.1
+// TypeScript Version: 3.0
 
 /// <reference path="./cast.framework.d.ts" />
 /// <reference path="./cast.framework.breaks.d.ts" />
