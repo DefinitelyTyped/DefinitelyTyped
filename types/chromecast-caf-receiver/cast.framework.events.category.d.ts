848472b6f80032d44519aecf7846d99677061c9a
Sergio Arbeo <sarbeo@apple.com>
2019-05-23 21:39:44 +0200

    Lint

--- types/chromecast-caf-receiver/cast.framework.events.category.d.ts
+++ types/chromecast-caf-receiver/cast.framework.events.category.d.ts
@@ -5,16 +5,16 @@ export as namespace category;
 /**
  * Includes high-level events. These events fire less frequently and are the most likely to be useful for debugging.
  */
-declare const CORE: EventType[];
+export const CORE: EventType[];
 /**
  * Includes low-level events. These events fire somewhat frequently, and usually indicate clip-level loading / ready statuses.
  */
-declare const DEBUG: EventType[];
+export const DEBUG: EventType[];
 /**
  * Includes low-level events that trigger frequently.
  */
-declare const FINE: EventType[];
+export const FINE: EventType[];
 /**
  * Includes all events that represent a request made to the receiver.
  */
-declare const REQUEST: EventType[];
+export const REQUEST: EventType[];
