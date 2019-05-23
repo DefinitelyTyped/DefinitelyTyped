126e906bcf193e204c3eacc94672b3e558e6cc70
Chris Shaffer <chris@methodbox.io>
2019-05-23 12:55:12 -0500

    Corrected jsdoc alignment

--- types/node/querystring.d.ts
+++ types/node/querystring.d.ts
@@ -20,12 +20,12 @@ declare module "querystring" {
     function stringify(obj?: ParsedUrlQueryInput, sep?: string, eq?: string, options?: StringifyOptions): string;
     function parse(str: string, sep?: string, eq?: string, options?: ParseOptions): ParsedUrlQuery;
     /**
-    * The querystring.encode() function is an alias for querystring.stringify().
-    */
+     * The querystring.encode() function is an alias for querystring.stringify().
+     */
     function encode(obj?: ParsedUrlQueryInput, sep?: string, eq?: string, options?: StringifyOptions): string;
     /**
-    * The querystring.encode() function is an alias for querystring.stringify().
-    */
+     * The querystring.encode() function is an alias for querystring.stringify().
+     */
     function decode(str: string, sep?: string, eq?: string, options?: ParseOptions): ParsedUrlQuery;
     function escape(str: string): string;
     function unescape(str: string): string;
