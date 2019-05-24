7c68041fa2038840380ae8d93d4617f82b88d5b0
jh3y <jh3y@users.noreply.github.com>
2019-05-24 19:37:25 +0100

    [@types/styletron-engine-atomic] foundation for styletron-engine-atomic typings

---
+++ types/styletron-engine-atomic/index.d.ts
@@ -0,0 +1,45 @@
+// Type definitions for styletron-engine-atomic 1.1
+// Project: https://github.com/styletron/styletron
+// Definitions by: Jhey Tompkins <https://github.com/jh3y>
+// Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
+
+import { KeyframesObject, FontFace as FontFaceObject, StandardEngine } from 'styletron-standard'
+
+export = StyletronEngineAtomic
+
+type hydrateType = HTMLCollectionOf<HTMLStyleElement> | Array<HTMLStyleElement> | NodeListOf<HTMLStyleElement>
+type sheetType = {
+  css: string,
+  attrs: { [key: string]: string }
+}
+
+interface ClientOptions {
+  hydrate?: hydrateType;
+  container?: Element;
+  prefix?: string;
+}
+
+interface ServerOptions {
+  prefix?: string;
+}
+
+declare namespace StyletronEngineAtomic {
+  class Client implements StandardEngine {
+    constructor(opts?: ClientOptions);
+    styleElements: { [key: string]: HTMLStyleElement };
+    fontFaceSheet: HTMLStyleElement;
+    keyframesSheet: HTMLStyleElement;
+    styleCache: MultiCache<{pseudo: string, block: string}>;
+    keyframesCache: Cache<KeyframesObject>;
+    fontFaceCache: Cache<FontFaceObject>;
+  }
+  class Server implements StandardEngine {
+    constructor(opts?: ServerOptions)
+    styleRules: { [key: string]: string };
+    keyframesRules: string;
+    fontFaceRules: string;
+    getStylesheets(): Array<sheetType>;
+    getStylesheetsHtml(className?: string): string;
+    getCss(): string;
+  }
+}
