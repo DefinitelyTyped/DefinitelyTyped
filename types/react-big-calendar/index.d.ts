dad14da7ea8882a8661c9c020148ea913e0ae1af
Mark Nelissen <mark.nelissen@gmail.com>
2019-05-24 22:57:14 +0200

    react big calender event fields not mandatory (#35721)
    * react big calender event fields not mandatory
    
    * add name

--- types/react-big-calendar/index.d.ts
+++ types/react-big-calendar/index.d.ts
@@ -11,6 +11,7 @@
 //                 Tomas Hubelbauer <https://github.com/TomasHubelbauer>
 //                 Lucas Silva Souza <https://github.com/lksilva>
 //                 Siarhey Belofost <https://github.com/SergeyBelofost>
+//                 Mark Nelissen <https://github.com/marknelissen>
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
 // TypeScript Version: 2.8
 import { Validator } from 'prop-types';
@@ -33,9 +34,9 @@ export type Navigate = 'PREV' | 'NEXT' | 'TODAY' | 'DATE';
 
 export interface Event {
     allDay?: boolean;
-    title: string;
-    start: Date;
-    end: Date;
+    title?: string;
+    start?: Date;
+    end?: Date;
     resource?: any;
 }
 export interface DateRange {
