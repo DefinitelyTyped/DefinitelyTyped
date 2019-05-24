ebdd053dca456b49b23a35ba1df2689f2a852840
Christian Scheja <christian@scheja.me>
2019-05-24 23:49:33 +0200

    fix: npm test issues

--- types/yeast/index.d.ts
+++ types/yeast/index.d.ts
@@ -1,4 +1,4 @@
-// Type definitions for yeast 0.1.2
+// Type definitions for yeast 0.1
 // Project: https://github.com/unshiftio/yeast
 // Definitions by: Christian Scheja <https://github.com/schmollmolch>
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
@@ -9,33 +9,20 @@ export = yeast;
 export as namespace yeast;
 
 declare namespace yeast {
-    interface Yeast {
+	interface Yeast {
 		/**
-        * Yeast: A tiny growing id generator.
-        *
-        * @returns {String} A unique id.
-        * @api public
-        */
-        (): string;
-
+		 * Yeast: A tiny growing id generator.
+		 */
+		(): string;
 
 		/**
 		 * Return a string representing the specified number.
-		 *
-		 * @param {Number} num The number to convert.
-		 * @returns {String} The string representation of the number.
-		 * @api public
 		 */
-        encode: (num: number) => string;
+		encode: (num: number) => string;
 
 		/**
- 		* Return the integer value specified by the given string.
- 		*
- 		* @param {String} str The string to convert.
- 		* @returns {Number} The integer value represented by the string.
- 		* @api public
- 		*/
-        decode: (str: string) => number;
-    }
-
+		 * Return the integer value specified by the given string.
+		 */
+		decode: (str: string) => number;
+	}
 }
