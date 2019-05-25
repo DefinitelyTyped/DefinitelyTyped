ef95cf1ca687943f7b8edc2baea19dda246a8a64
Christian Scheja <christian@scheja.me>
2019-05-25 21:01:04 +0200

    fix: declaration as UMD module

--- types/yeast/index.d.ts
+++ types/yeast/index.d.ts
@@ -3,26 +3,20 @@
 // Definitions by: Christian Scheja <https://github.com/schmollmolch>
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
 
-declare var yeast: yeast.Yeast;
-
 export = yeast;
-export as namespace yeast;
+/**
+ * Yeast: A tiny growing id generator.
+ */
+declare function yeast(): string;
 
 declare namespace yeast {
-	interface Yeast {
-		/**
-		 * Yeast: A tiny growing id generator.
-		 */
-		(): string;
+	/**
+	 * Return a string representing the specified number.
+	 */
+	function encode(num: number): string;
 
-		/**
-		 * Return a string representing the specified number.
-		 */
-		encode: (num: number) => string;
-
-		/**
-		 * Return the integer value specified by the given string.
-		 */
-		decode: (str: string) => number;
-	}
+	/**
+	 * Return the integer value specified by the given string.
+	 */
+	function decode(str: string): number;
 }
