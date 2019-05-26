cd8ed3a501d867a88183353aa399f31e75c36ff0
KuhnChris <kuhnchris@kuhnchris.eu>
2019-05-26 13:41:27 +0200

    LINT'd

--- types/3box/3box-tests.ts
+++ types/3box/3box-tests.ts
@@ -3,4 +3,3 @@ import Box = require("3box");
 Box.getProfile("0x28F4a17f8A99AB90c1a401b85D694B2C0eA40C4b").then((ret: any) => {
     console.log(ret.name);
 });
-
--- types/3box/index.d.ts
+++ types/3box/index.d.ts
@@ -1,107 +1,111 @@
-declare module "3box"{    
-    interface BoxVerified {
-        Verified: () => BoxVerified,
-        DID: () => string,
-        github: () => any,
-        addGithub: (gistUrl: string) => any,
-        twitter: () => any,
-        addTwitter: (claim: string) => any,
-        email: () => any,
-        addEmail: (claim: string) => any
-    }
-    interface BoxThreadOpts_getPosts {
-        gt?: string,
-        gte?: string,
-        lt?: string,
-        lte?: string,
-        limit?: number,
-        reverse?: boolean
-    }
-    interface BoxThread {
-        BoxThread: () => BoxThread,
-        post: (message: string) => string,
-        getPosts: (opts?: BoxThreadOpts_getPosts) => Array<any>
-        onNewPost: (newPostFn: () => undefined) => undefined
-    }
-    interface BoxSpaceOpts_joinThread {
-        noAutoSub?: boolean
-    }
-    interface BoxSpace {
-        public: BoxKeyValueStore,
-        private: BoxKeyValueStore,
-        joinThread: (name: string, opts?: BoxSpaceOpts_joinThread) => BoxThread,
-        subscribeThread: (name: string) => undefined,
-        unsubscribeThread: (name: string) => undefined,
-        subscribedThreads: () => Array<string>
-        BoxSpace: () => BoxSpace
-    }
-    interface BoxKeyValueStoreMetadata { }
-
-    interface BoxKeyValueStore {
-        BoxKeyValueStore: () => BoxKeyValueStore,
-        log: Array<any>,
-        get: (key: any) => any,
-        getMetadata: (key: any) => BoxKeyValueStoreMetadata | undefined, 
-        set: (key: any, value: any) => boolean,
-        remove: (key: any) => boolean
-    }
-    class BoxStaticIdUtils {
-        verifyClaim: () => any;
-        isMuportDID: (address: any) => any | boolean;
-        isClaim: (claim: any, opts: any) => Promise<boolean>;
-    }
-    interface BoxObjectOpts_OpenSpace {
-        consentCallback?: () => any
-        onSyncDone?: () => any
-    }
-    interface BoxObjectOpts_GetProfile {
-        addressServer?: string,
-        ipfs?: any, //ipfs object
-        useCacheService?: boolean,
-        profileServer?: string 
-    }
-    interface BoxObjectOpts_GetProfiles {
-        profileServer?: string 
-    }
-    interface BoxObjectOpts_GetSpace {
-        profileServer?: string, 
-        metadata?: string
-    }
-    interface BoxObjectOpts_GetThread {
-        profileServer?: string 
-    }
-    interface BoxObjectOpts_ListSpaces {
-        profileServer?: string 
-    }
-    interface BoxObjectOpts_profileGraphQL {
-        graphqlServer?: string 
-    }
-    interface BoxObjectOpts_openBox {
-        consentCallback?: () => any,
-        pinningNode?: string,
-        ipfs?: any, // ipfsobj
-        addressServer?: string
-    }
-    
-    class BoxInstance {
-        public: BoxKeyValueStore;
-        private: BoxKeyValueStore;
-        verified: BoxVerified;
-        spaces: any;
-        openSpace: (name: string, opts?: BoxObjectOpts_OpenSpace) => any;
-        onSyncDone: (cb: () => undefined) => null;
-        logout: () => any;
-    }
-
-    const idUtils: BoxStaticIdUtils;
-    const getProfile: (address: any, opts?: BoxObjectOpts_GetProfile) => Promise<any>;
-    const getProfiles: (address: Array<any>, opts?: BoxObjectOpts_GetProfiles) => undefined;
-    const getSpace: (address: any, name: any, opts?: BoxObjectOpts_GetSpace) => undefined;
-    const getThread: (space: any, name: any, opts?: BoxObjectOpts_GetThread) => Array<string>;
-    const listSpaces: (address: any, opts?: BoxObjectOpts_ListSpaces) => undefined;
-    const profileGraphQL: (query: any, opts?: BoxObjectOpts_profileGraphQL) => undefined;
-    const getVerifiedAccounts: (profile: any) => undefined;
-    const openBox: (address: any, ethereumProvider: any, opts?: BoxObjectOpts_openBox) => BoxInstance;
-    const isLoggedIn: (address: any) => undefined;
+export interface BoxVerified {
+    Verified: () => BoxVerified;
+    DID: () => string;
+    github: () => any;
+    addGithub: (gistUrl: string) => any;
+    twitter: () => any;
+    addTwitter: (claim: string) => any;
+    email: () => any;
+    addEmail: (claim: string) => any;
+}
+export interface BoxThreadOpts_getPosts {
+    gt?: string;
+    gte?: string;
+    lt?: string;
+    lte?: string;
+    limit?: number;
+    reverse?: boolean;
+}
+export interface BoxThread {
+    BoxThread: () => BoxThread;
+    post: (message: string) => string;
+    getPosts: (opts?: BoxThreadOpts_getPosts) => any[];
+    onNewPost: (newPostFn: () => undefined) => undefined;
+}
+export interface BoxSpaceOpts_joinThread {
+    noAutoSub?: boolean;
+}
+export interface BoxSpace {
+    public: BoxKeyValueStore;
+    private: BoxKeyValueStore;
+    joinThread: (name: string, opts?: BoxSpaceOpts_joinThread) => BoxThread;
+    subscribeThread: (name: string) => undefined;
+    unsubscribeThread: (name: string) => undefined;
+    subscribedThreads: () => string[];
+    BoxSpace: () => BoxSpace;
+}
+export interface BoxKeyValueStoreMetadata {
+    // missing definition
+    dummy: boolean;
 }
 
+export interface BoxKeyValueStore {
+    BoxKeyValueStore: () => BoxKeyValueStore;
+    log: any[];
+    get: (key: any) => any;
+    getMetadata: (key: any) => BoxKeyValueStoreMetadata | undefined;
+    set: (key: any, value: any) => boolean;
+    remove: (key: any) => boolean;
+}
+export interface BoxIsMuportDIDReturn {
+    // missing definition
+    dummy: boolean;
+}
+export class BoxStaticIdUtils {
+    verifyClaim: () => any;
+    isMuportDID: (address: any) => BoxIsMuportDIDReturn | boolean;
+    isClaim: (claim: any, opts: any) => Promise<boolean>;
+}
+export interface BoxObjectOpts_OpenSpace {
+    consentCallback?: () => any;
+    onSyncDone?: () => any;
+}
+export interface BoxObjectOpts_GetProfile {
+    addressServer?: string;
+    ipfs?: any; // ipfs object
+    useCacheService?: boolean;
+    profileServer?: string;
+}
+export interface BoxObjectOpts_GetProfiles {
+    profileServer?: string;
+}
+export interface BoxObjectOpts_GetSpace {
+    profileServer?: string;
+    metadata?: string;
+}
+export interface BoxObjectOpts_GetThread {
+    profileServer?: string;
+}
+export interface BoxObjectOpts_ListSpaces {
+    profileServer?: string;
+}
+export interface BoxObjectOpts_profileGraphQL {
+    graphqlServer?: string;
+}
+export interface BoxObjectOpts_openBox {
+    consentCallback?: () => any;
+    pinningNode?: string;
+    ipfs?: any; // ipfsobj
+    addressServer?: string;
+}
+
+export class BoxInstance {
+    public: BoxKeyValueStore;
+    private: BoxKeyValueStore;
+    verified: BoxVerified;
+    spaces: any;
+    openSpace: (name: string, opts?: BoxObjectOpts_OpenSpace) => any;
+    onSyncDone: (cb: () => undefined) => null;
+    logout: () => any;
+}
+
+export const idUtils: BoxStaticIdUtils;
+export function getProfile(address: any, opts?: BoxObjectOpts_GetProfile): Promise<any>;
+export function getProfiles(address: any[], opts?: BoxObjectOpts_GetProfiles): undefined;
+export function getSpace(address: any, name: any, opts?: BoxObjectOpts_GetSpace): undefined;
+export function getThread(space: any, name: any, opts?: BoxObjectOpts_GetThread): string[];
+export function listSpaces(address: any, opts?: BoxObjectOpts_ListSpaces): undefined;
+export function profileGraphQL(query: any, opts?: BoxObjectOpts_profileGraphQL): undefined;
+export function getVerifiedAccounts(profile: any): undefined;
+export function openBox(address: any, ethereumProvider: any, opts?: BoxObjectOpts_openBox): BoxInstance;
+export function isLoggedIn(address: any): undefined;
