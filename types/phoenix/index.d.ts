67fec0ed638b94f2011f993c6c2f1db6a369780e
Christian Wesselhoeft <hi@xtian.us>
2019-05-27 11:24:22 -0600

    phoenix/index.d.ts: Wrap attributions

--- types/phoenix/index.d.ts
+++ types/phoenix/index.d.ts
@@ -1,6 +1,8 @@
 // Type definitions for phoenix 1.4
 // Project: https://github.com/phoenixframework/phoenix
-// Definitions by: Mirosław Ciastek <https://github.com/mciastek>, John Goff <https://github.com/John-Goff>, Po Chen <https://github.com/princemaple>
+// Definitions by: Mirosław Ciastek <https://github.com/mciastek>
+//                 John Goff <https://github.com/John-Goff>
+//                 Po Chen <https://github.com/princemaple>
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
 // TypeScript Version: 2.4
 
