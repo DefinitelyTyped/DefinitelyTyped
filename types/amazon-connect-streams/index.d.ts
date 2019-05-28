7b886418335e2207d7b5c4e1f26b6711ea67263c
Andy Hopper <andyhopp@amazon.com>
2019-05-28 18:13:47 -0400

    Updated Project header to URL for Amazon Connect Streams

--- types/amazon-connect-streams/index.d.ts
+++ types/amazon-connect-streams/index.d.ts
@@ -1,5 +1,5 @@
 // Type definitions for non-npm package Amazon Connect Streams API 1.3
-// Project: amazon-connect-streams
+// Project: https://github.com/aws/amazon-connect-streams
 // Definitions by: Andy Hopper <https://github.com/andyhopp>
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
 // TypeScript Version: 2.4
