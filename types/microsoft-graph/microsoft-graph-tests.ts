7d93f9580a0b2fac9e832d42f310ce0e06ecbc40
Muthurathinam <muthurathinam@zoho.com>
2019-05-28 15:31:09 +0530

    Added more test for types

--- types/microsoft-graph/microsoft-graph-tests.ts
+++ types/microsoft-graph/microsoft-graph-tests.ts
@@ -1,5 +1,32 @@
-import { User } from "microsoft-graph";
+// Microsoft Graph tests
+// Project: https://github.com/microsoftgraph/msgraph-typescript-typings
+
+
+import { User, Event, DateTimeTimeZone, ItemBody, BodyType } from "microsoft-graph";
 
 const user: User = {
-    officeLocation: "Redmond"
+    officeLocation: "Bengaluru",
+    companyName: "Microsoft",
+    country: "India",
+    displayName: "Muthu"
+};
+
+const startTime: DateTimeTimeZone = {
+    dateTime: "2019-05-29T04:00:00.0000000"
+};
+
+const endTime: DateTimeTimeZone = {
+    dateTime: "2019-05-29T05:00:00.0000000",
+};
+
+const bodyText: ItemBody = {
+    contentType: "text",
+    content: "Hi, Shall we meet for a cup of coffee"
+};
+
+const event: Event = {
+    subject: "Meet for a coffee",
+    body: bodyText,
+    start: startTime,
+    end: endTime
 };
