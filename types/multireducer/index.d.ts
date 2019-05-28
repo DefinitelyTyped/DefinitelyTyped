a04d551f1c82666987391ccdf89b0033cd5fc504
MinGu Lee <mglee@whatap.io>
2019-05-28 19:36:58 +0900

    added type definitions for multireducer fixed issues

--- types/multireducer/index.d.ts
+++ types/multireducer/index.d.ts
@@ -1,22 +1,20 @@
-// Type definitions for multireducer v3.1.2
+// Type definitions for multireducer 3.1
 // Project: https://github.com/erikras/multireducer
-// Definitions by: Anthony M. Lee <https://github.com/immigration9/>
+// Definitions by: Anthony M. Lee <https://github.com/immigration9>
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
+// TypeScript Version: 2.1
 
-interface MetaObject {
+export interface MetaObject {
   [key: string]: string;
 }
 
-interface ActionObject {
+export interface ActionObject {
   meta: MetaObject;
   [rest: string]: any;
 }
-
-type Reducer = { [key: string]: any }
 
 export function bindActionCreators(actions: any, dispatch: any, reducerKey: string): any;
 export function wrapDispatch(dispatch: any, reducerKey: string): any;
 export function wrapAction(action: any, reducerKey: string): ActionObject;
 
-export default function multireducer(reducers: Reducer): (state: any, action: any) => any;
-export default function multireducer(reducers: Reducer, reducerKey: string): (state: any, action: any) => any;
+export default function multireducer(reducers: { [key: string]: any }, reducerKey?: string): (state: any, action: any) => any;
