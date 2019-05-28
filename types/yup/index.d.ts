b4bf0cc340fbe3942b0adabf4d3f00acc94a7134
Maurice de Beijer <maurice.de.beijer@gmail.com>
2019-05-28 20:19:00 +0200

    Definitions by

--- types/yup/index.d.ts
+++ types/yup/index.d.ts
@@ -10,6 +10,7 @@
 //                 Yusuke Sato <https://github.com/sat0yu>
 //                 Dan Rumney <https://github.com/dancrumb>
 //                 Desmond Koh <https://github.com/deskoh>
+//                 Maurice de Beijer <https://github.com/mauricedb>
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
 // TypeScript Version: 2.8
 
@@ -245,7 +246,11 @@ export interface WhenOptionsBuilder<T> {
 
 export type WhenOptions<T> =
     | WhenOptionsBuilder<T>
-    | { is: boolean | ((...values: any[]) => boolean); then: any; otherwise: any }
+    | {
+          is: boolean | ((...values: any[]) => boolean);
+          then: any;
+          otherwise: any;
+      }
     | object;
 
 export interface TestContext {
