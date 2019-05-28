11340cad8e21b5c208eaf546de0a198713b2680b
s8kggu_zlatkoa <zlatko.andonovski@alite-international.com>
2019-05-28 11:41:24 +0200

    Additional linting fixes

--- types/dynamic-time-warping/index.d.ts
+++ types/dynamic-time-warping/index.d.ts
@@ -7,7 +7,7 @@ declare class DynamicTimeWarping<T> {
     constructor(ser1: ReadonlyArray<T>, ser2: ReadonlyArray<T>, distFunc: (a: T, b: T) => number);
 
     getDistance(): number;
-    getPath(): [number, number][]   
+    getPath(): Array<[number, number]>;
 }
 
 export = DynamicTimeWarping;
