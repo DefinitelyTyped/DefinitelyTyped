e78defc2012144a1ebf955e355c204812b73fabb
Vincenzo Chianese <vincenz.chianese@icloud.com>
2019-05-29 09:53:54 +0200

    Update index.d.ts

--- types/swagger-schema-official/index.d.ts
+++ types/swagger-schema-official/index.d.ts
@@ -1,5 +1,5 @@
 // Type definitions for swagger-schema-official 2.0
-// Project: http://io/specification/
+// Project: https://swagger.io/specification/
 // Definitions by: Mohsen Azimi <https://github.com/mohsen1>, Ben Southgate <https://github.com/bsouthga>, Nicholas Merritt <https://github.com/nimerritt>, Mauri Edo <https://github.com/mauriedo>
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
 
