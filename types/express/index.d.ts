3c1663bd6948cc89dc9f2e79fdcea1ab461b7c39
Gady Piazza <gady90@gmail.com>
2019-05-29 13:36:35 +0300

    Don't use patch number

--- types/express/index.d.ts
+++ types/express/index.d.ts
@@ -1,4 +1,4 @@
-// Type definitions for Express 4.17.1
+// Type definitions for Express 4.17
 // Project: http://expressjs.com
 // Definitions by: Boris Yankov <https://github.com/borisyankov>
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
