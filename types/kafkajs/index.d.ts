f74e1296dbd54cb8fc37fddf425665e936aa6670
Jose Camara <jose.camara@coosto.com>
2019-05-30 14:29:21 +0200

    Updated version of `kafkajs` to 1.8

--- types/kafkajs/index.d.ts
+++ types/kafkajs/index.d.ts
@@ -1,4 +1,4 @@
-// Type definitions for kafkajs 1.4
+// Type definitions for kafkajs 1.8
 // Project: https://github.com/tulios/kafkajs, https://kafka.js.org
 // Definitions by: Michal Kaminski <https://github.com/michal-b-kaminski>
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
