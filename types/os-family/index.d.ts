4c3fc605faa47891b70e5e327b1c4016b7dbd60a
LavrovArtem <artjomlav@gmail.com>
2019-05-30 17:10:15 +0300

    Create os-family

---
+++ types/os-family/index.d.ts
@@ -0,0 +1,10 @@
+// Type definitions for os-family 1.1
+// Project: https://github.com/inikulin/os-family
+// Definitions by: Artem Lavrov <https://github.com/LavrovArtem>
+// Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
+// TypeScript Version: 2.2
+
+export const win: boolean;
+export const linux: boolean;
+export const mac: boolean;
+export const bsd: boolean;
