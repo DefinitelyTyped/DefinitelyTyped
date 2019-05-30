517c0c0c050db6556735e95dad1c7d09b39542a5
Shavkat Aynurin <shavkat@ainurin.com>
2019-05-29 18:11:24 -0700

    Regenerated type defintions for dir-walker-gen using dts-gen

--- types/dir-walker-gen/index.d.ts
+++ types/dir-walker-gen/index.d.ts
@@ -3,39 +3,36 @@
 // Definitions by: Shavkat Aynurin <https://github.com/aynurin/>
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
 
-declare module "dir-walker-gen" {
-    function DirGen(
-        options: {
-            /**
-             * (Required) List of starting folders
-             */
-            folders: string[],
-            /**
-             * Does not show console warning when directories do not exist
-             * @default {false}
-             */
-            silent?: boolean,
-            /**
-             * Ignores directories that start with a dot (e.g. .git, .vscode, etc)
-             * @default {false}
-             */
-            ignoreDotDir?: boolean,
-            /**
-             * Exclude all folder that ends with any of the given strings
-             * @default []
-             */
-            excludeFolders?: string[],
-            /**
-             * List of extensions to ignore
-             * @default []
-             */
-            excludeExtensions?: string[],
-            /**
-             * List of extensions to scan (all other extensions are ignored)
-             * @default []
-             */
-            includeExtensions?: string[]
-        },
-    ): IterableIterator<string>;
-    export default DirGen;
-}
+export default function DirGen(
+    options: {
+        /**
+         * (Required) List of starting folders
+         */
+        folders: string[],
+        /**
+         * Does not show console warning when directories do not exist
+         * @default {false}
+         */
+        silent?: boolean,
+        /**
+         * Ignores directories that start with a dot (e.g. .git, .vscode, etc)
+         * @default {false}
+         */
+        ignoreDotDir?: boolean,
+        /**
+         * Exclude all folder that ends with any of the given strings
+         * @default []
+         */
+        excludeFolders?: string[],
+        /**
+         * List of extensions to ignore
+         * @default []
+         */
+        excludeExtensions?: string[],
+        /**
+         * List of extensions to scan (all other extensions are ignored)
+         * @default []
+         */
+        includeExtensions?: string[]
+    },
+): IterableIterator<string>;
