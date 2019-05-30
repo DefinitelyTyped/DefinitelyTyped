43788ece58254014cd9b53d83a43ed65a74ebe5c
s8kggu_zlatkoa <zlatko.andonovski@alite-international.com>
2019-05-30 13:04:55 +0200

    Removed trailing whitespace

--- types/dynamic-time-warping/index.d.ts
+++ types/dynamic-time-warping/index.d.ts
@@ -6,7 +6,7 @@
 declare global {
     class DynamicTimeWarping<T> {
         constructor(ser1: ReadonlyArray<T>, ser2: ReadonlyArray<T>, distFunc: (a: T, b: T) => number);
-    
+
         getDistance(): number;
         getPath(): Array<[number, number]>;
     }
