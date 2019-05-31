95a3843eb51b9a257f5bf8cbeb3db57204ab5d4b
Alexander Ivanov <oshli.a.er@gmail.com>
2019-05-31 06:14:09 +0300

    Removing formatting on the current iteration
    Signed-off-by: Alexander Ivanov <oshli.a.er@gmail.com>

--- types/google-apps-script/google-apps-script-events.d.ts
+++ types/google-apps-script/google-apps-script-events.d.ts
@@ -16,17 +16,17 @@ declare namespace GoogleAppsScript {
   export module Events {
     // Internal interfaces
     interface AppsScriptEvent {
-      authMode: Script.AuthMode;
-      triggerUid: string;
-      user: Base.User;
+      authMode: Script.AuthMode,
+      triggerUid: string,
+      user: Base.User,
     }
 
     interface AppsScriptHttpRequestEvent {
-      parameter: object;
-      contextPath: string;
-      contentLength: number;
-      queryString: string;
-      parameters: object;
+      parameter: object,
+      contextPath: string,
+      contentLength: number,
+      queryString: string,
+      parameters: object,
     }
 
     interface AppsScriptHttpRequestEventPostData {
@@ -38,52 +38,52 @@ declare namespace GoogleAppsScript {
 
     // External interfaces
     export interface SheetsOnOpen extends AppsScriptEvent {
-      source: Spreadsheet.Spreadsheet;
+      source: Spreadsheet.Spreadsheet,
     }
 
     enum SheetsOnChangeChangeType { EDIT, INSERT_ROW, INSERT_COLUMN, REMOVE_ROW, REMOVE_COLUMN, INSERT_GRID, REMOVE_GRID, FORMAT, OTHER }
     export interface SheetsOnChange extends AppsScriptEvent {
-      changeType: SheetsOnChangeChangeType;
+      changeType: SheetsOnChangeChangeType,
     }
 
     export interface SheetsOnEdit extends AppsScriptEvent {
-      oldValue: string;
-      range: Spreadsheet.Range;
-      source: Spreadsheet.Spreadsheet;
-      value: string;
+      oldValue: string,
+      range: Spreadsheet.Range,
+      source: Spreadsheet.Spreadsheet,
+      value: string,
     }
 
     export interface FormsOnSubmit extends AppsScriptEvent {
-      namedValues: { [key: string]: string[] };
-      range: Spreadsheet.Range;
-      values: string[];
+      namedValues: { [key: string]: string[]; },
+      range: Spreadsheet.Range,
+      values: string[],
     }
 
     export interface DocsOnOpen extends AppsScriptEvent {
-      source: Document.Document;
+      source: Document.Document,
     }
 
     export interface SlidesOnOpen extends AppsScriptEvent {
-      source: Slides.Presentation;
+      source: Slides.Presentation,
     }
 
     export interface FormsOnOpen extends AppsScriptEvent {
-      source: Forms.Form;
+      source: Forms.Form,
     }
 
     // TODO: Is there a `user` attribute?
     export interface CalendarEventUpdated extends AppsScriptEvent {
-      calendarId: string;
+      calendarId: string,
     }
 
     export interface AddonOnInstall {
-      authMode: Script.AuthMode;
+      authMode: Script.AuthMode,
     }
 
     export interface DoGet extends AppsScriptHttpRequestEvent {}
 
     export interface DoPost extends AppsScriptHttpRequestEvent {
-      postData: AppsScriptHttpRequestEventPostData;
+      postData: AppsScriptHttpRequestEventPostData,
     }
   }
 }
