06c371e17cc5d6c20786fb5377102c9c5a74cf38
Nathan Shively-Sanders <293473+sandersn@users.noreply.github.com>
2019-05-31 10:44:13 -0700

    Cleanup 2019-05-31 (#35868)
    * Cleanup 2019-05-31
    
    1. Downstream lint rule failure now only caught overnight.
    2. update intl-relativeformat project url to monorepo url.
    
    * Require 2.8 for all featherjs packages

--- types/feathersjs__socketio-client/feathersjs__socketio-client-tests.ts
+++ types/feathersjs__socketio-client/feathersjs__socketio-client-tests.ts
@@ -4,7 +4,7 @@ import feathersSocketIOClient from '@feathersjs/socketio-client';
 import * as io from 'socket.io-client';
 
 const socket = io();
-const app: Application<{}> = feathers();
+const app: Application = feathers();
 
 app.configure(feathersSocketIOClient(socket));
 app.configure(feathersSocketIOClient(socket, { timeout: 1337 }));
