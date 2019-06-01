f34248d96b70dd254b9574e2264cfff3fe0236e9
aereal <aereal@aereal.org>
2019-06-01 14:44:22 +0900

    [express-serve-static-core] listen's callback can take some parameters

--- types/express-serve-static-core/express-serve-static-core-tests.ts
+++ types/express-serve-static-core/express-serve-static-core-tests.ts
@@ -2,3 +2,6 @@ import * as express from 'express-serve-static-core';
 
 const app: express.Application = {} as any;
 app.listen(3000);
+app.listen(3000, (err: any) => {
+    // no-op error callback
+});
