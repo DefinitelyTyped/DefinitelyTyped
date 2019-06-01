5104c9a4feeaa285657b1456c94571ed46c66dcd
Derek Sifford <dereksifford@gmail.com>
2019-05-31 21:26:43 -0400

    remove typescript version constraint since this is a simple module

--- types/wordpress__dom-ready/index.d.ts
+++ types/wordpress__dom-ready/index.d.ts
@@ -2,6 +2,5 @@
 // Project: https://github.com/WordPress/gutenberg/tree/master/packages/dom-ready/README.md
 // Definitions by: Derek Sifford <https://github.com/dsifford>
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
-// TypeScript Version: 3.4
 
 export default function domReady(callback: () => void): void;
