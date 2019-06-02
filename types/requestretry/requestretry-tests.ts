1896040cdbab011e76932adc8af4ad76c04ccbf8
Aniket Patel <anipatel@cimpress.com>
2019-06-02 23:15:05 +0530

    Add typings for promises in requestretry module

--- types/requestretry/requestretry-tests.ts
+++ types/requestretry/requestretry-tests.ts
@@ -1,6 +1,21 @@
 import request = require('requestretry');
 import http = require('http');
 
+// Promisified request function
+request({
+	url: 'https://api.example.com/v1/a/b',
+	json: true,
+	// The below parameters are specific to request-retry
+	// (default) try 5 times
+	maxAttempts: 5,
+	// (default) wait for 5s before trying again
+	retryDelay: 5000,
+	// (default) retry on 5xx or network errors
+	retryStrategy: request.RetryStrategies.HTTPOrNetworkError
+}).then(res => {
+	// Body
+});
+
 // HTTPOrNetworkError strategy.
 request({
 	url: 'https://api.example.com/v1/a/b',
