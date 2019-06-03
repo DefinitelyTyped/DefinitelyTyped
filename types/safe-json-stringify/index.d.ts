c1c5669fd26a38d7418186b92a48a143b0e3fde9
Tony Wooster <tony.wooster@contentful.com>
2019-06-03 10:35:04 +0200

    Improve types of safe-json-stringify
    `safe-json-stringify` >= 1.1 accepts the same parameters as
    JSON.stringify.  This commit changes the definitions to match the
    signature defined for JSON.stringify in the `lib.es5.d.ts`
    definitions.
    
    Also updated the definitions of ensureProperties to be a bit
    more powerful and correct.

--- types/safe-json-stringify/index.d.ts
+++ types/safe-json-stringify/index.d.ts
@@ -1,13 +1,19 @@
-// Type definitions for safe-json-stringify 1.0
+// Type definitions for safe-json-stringify 1.1
 // Project: https://github.com/debitoor/safe-json-stringify
 // Definitions by: Eric Byers <https://github.com/ericbyers>
-// Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped"
+//                 Tony Wooster <https://github.com/twooster>
+// Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
 // TypeScript Version: 2.3
 
 declare namespace safeJsonStringify {
-	function ensureProperties(obj: any): object;
+    function ensureProperties(value: object): object;
+    function ensureProperties<T>(value: T): T;
 }
 
-declare function safeJsonStringify(data: object): string;
+declare function safeJsonStringify(
+    value: any,
+    replacer?: ((this: any, key: string, value: any) => any) | Array<number | string> | null,
+    space?: string | number
+): string;
 
 export = safeJsonStringify;
--- types/safe-json-stringify/safe-json-stringify-tests.ts
+++ types/safe-json-stringify/safe-json-stringify-tests.ts
@@ -1,5 +1,32 @@
 import safeJsonStringify = require('safe-json-stringify');
 
 const obj1 = {foo: 'bar'};
+// $ExpectType string
 safeJsonStringify(obj1);
+
+class A {
+  a() { }
+}
+
+// Ensure that concrete classes are widened to `object`
+// $ExpectError
+const a: A = safeJsonStringify.ensureProperties(new A());
+// $ExpectType object
+safeJsonStringify.ensureProperties(new A());
+// $ExpectType object
 safeJsonStringify.ensureProperties(obj1);
+// $ExpectType true
+safeJsonStringify.ensureProperties(true);
+// $ExpectType null
+safeJsonStringify.ensureProperties(null);
+// $ExpectType 10
+safeJsonStringify.ensureProperties(10);
+// $ExpectType "wat"
+safeJsonStringify.ensureProperties('wat');
+
+safeJsonStringify(obj1, null, 2);
+safeJsonStringify(obj1, null, ' ');
+safeJsonStringify(obj1, (_k: string, v) => 'wat', 2);
+safeJsonStringify(obj1, (_k: string, v: any) => v, '  ');
+safeJsonStringify(obj1, ['foo'], 2);
+safeJsonStringify(obj1, ['foo', 0], '  ');
