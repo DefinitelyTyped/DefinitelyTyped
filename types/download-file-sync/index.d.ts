afb00ba333c0b0c5d5c91bb3a8075df05e22d12a
Nathan Shively-Sanders <293473+sandersn@users.noreply.github.com>
2019-06-05 14:46:52 -0700

    Remove download-file-async (#35980)
    HA HA packages with the word 'download' in them are spam according to
    npm.

--- types/download-file-sync/index.d.ts
+++
@@ -1,6 +0,0 @@
-// Type definitions for download-file-sync 1.0
-// Project: https://github.com/vjeux/download-file-sync
-// Definitions by: Nathan Shively-Sanders <https://github.com/sandersn>
-// Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
-declare function f(url: string): string;
-export = f;
