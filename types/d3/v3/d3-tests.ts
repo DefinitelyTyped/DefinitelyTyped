0cf0a4d511aba0045bbb379b519e0589ecc1c8f0
vbinithyanandamv <nithyanandamv@visualbi.com>
2019-06-06 17:43:12 +0530

    Index updated

--- types/d3/v3/d3-tests.ts
+++ types/d3/v3/d3-tests.ts
@@ -786,7 +786,7 @@ function populationPyramid() {
         .scale(y)
         .orient("right")
         .tickSize(-width)
-        .tickFormat(function (d) { return Math.round(d / 1e6) + "M"; } );
+        .tickFormat(function (d,i) { return Math.round(d / 1e6) + "M"; } );
 
     // An SVG element with a bottom-right origin.
     var svg = d3.select("body").append("svg")
