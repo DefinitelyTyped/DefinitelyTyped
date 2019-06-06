7f887effff9ec11a69349ffea4cdf8ca9a728b9b
Johann Wolf <jw@empatica.com>
2019-06-06 11:15:59 +0200

    mention myself in definition

--- types/selenium-webdriver/index.d.ts
+++ types/selenium-webdriver/index.d.ts
@@ -6,6 +6,7 @@
 //   Simon Gellis <https://github.com/SupernaviX>,
 //   Ben Dixon <https://github.com/bendxn>,
 //   Ziyu <https://github.com/oddui>
+//   Johann Wolf <https://github.com/beta-vulgaris>
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
 // TypeScript Version: 2.4
 
