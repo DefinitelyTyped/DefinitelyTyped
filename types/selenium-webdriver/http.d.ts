578dc38854c2586a03a553bc32f837effcb4690e
Johann Wolf <jw@empatica.com>
2019-06-06 11:14:29 +0200

    move definition of 'defineCommand' for Executor class

--- types/selenium-webdriver/http.d.ts
+++ types/selenium-webdriver/http.d.ts
@@ -105,21 +105,6 @@ export class Executor {
    */
   constructor(client: HttpClient|Promise<HttpClient>);
 
-  /**
-   * Defines a new command for use with this executor. When a command is sent,
-   * the {@code path} will be preprocessed using the command's parameters; any
-   * path segments prefixed with ':' will be replaced by the parameter of the
-   * same name. For example, given '/person/:name' and the parameters
-   * '{name: 'Bob'}', the final command path will be '/person/Bob'.
-   *
-   * @param {string} name The command name.
-   * @param {string} method The HTTP method to use when sending this command.
-   * @param {string} path The path to send the command to, relative to
-   *     the WebDriver server's command root and of the form
-   *     '/path/:variable/segment'.
-   */
-  defineCommand(name: string, method: string, path: string): void;
-
   /** @override */
   execute(command: any): any;
 }
