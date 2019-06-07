5276ad8119586f6bdf7fb71f6a195582d13fd130
Kirby Kuindersma <kkuinder@users.noreply.github.com>
2019-06-07 09:15:36 -0400

    Tabs, not spaces.

--- types/react-currency-formatter/index.d.ts
+++ types/react-currency-formatter/index.d.ts
@@ -14,7 +14,7 @@ interface CurrencyFormatterProps {
 	pattern?: string;
 	decimal?: string;
 	group?: string;
-    symbol?: string;
+	symbol?: string;
 }
 
 declare const reactCurrencyFormatter: React.ComponentClass<CurrencyFormatterProps>;
