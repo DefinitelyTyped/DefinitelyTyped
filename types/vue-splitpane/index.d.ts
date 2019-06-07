c864db575e088dbdd3b592abaa3919f54418b888
teenhe <teenhe@tencent.com>
2019-06-07 12:49:03 +0800

    Add types of [@types/vue-splitpane] package

---
+++ types/vue-splitpane/index.d.ts
@@ -0,0 +1,50 @@
+// Type definitions for vue-splitpane 1.0.4
+// Project: https://github.com/PanJiaChen/vue-split-pane/
+// Definitions by: teenhe <https://github.com/noonhorse>
+// Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
+// TypeScript Version: 3.5
+import { VueConstructor } from 'vue'
+
+export interface SplitpaneProps {
+  minPercent: number
+  defaultPercent: number
+  split: string[] // = ['vertical', 'horizontal']
+  className: string
+}
+
+export interface SplitpaneData {
+  active: boolean
+  hasMoved: boolean
+  height: any // null number
+  percent: number
+  type: string
+  resizeType: string
+}
+
+export interface SplitpaneWatch {
+  defaultPercent: (val: any) => void
+}
+
+export interface SplitpaneMethods {
+  onClick: () => void
+  onMouseDown: () => void
+  onMouseUp: () => void
+  onMouseMove: (e: Event) => void
+}
+
+export interface SplitpaneComputed {
+  userSelect: () => string
+  cursor: () => string
+}
+
+export interface SplitpaneConstructor extends VueConstructor {
+  props: SplitpaneProps
+  data: () => SplitpaneData
+  watch: SplitpaneWatch
+  methods: SplitpaneMethods
+  computed: SplitpaneComputed
+}
+
+export const Splitpane: SplitpaneConstructor
+
+export default Splitpane
---
+++ types/vue-splitpane/vue-splitpane-tests.ts
@@ -0,0 +1,18 @@
+import Vue from 'vue'
+import splitPane from 'vue-splitpane'
+
+new Vue({
+  el: '#app',
+  data: {},
+  components: {
+    'vue-tel-input': splitPane,
+  },
+  template: `<split-pane v-on:resize="resize" :min-percent='20' :default-percent='30' split="vertical">
+    <template slot="paneL">
+      A
+    </template>
+    <template slot="paneR">
+      B
+    </template>
+  </split-pane>`,
+})
