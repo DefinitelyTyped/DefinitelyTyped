aa67d18954cae3cc219afb09bd1f4b2ea75b5787
Derek Sifford <dereksifford@gmail.com>
2019-06-07 12:57:01 -0400

    [@wordpress/dependency-extraction-webpack-plugin] add new definitions

---
+++ types/wordpress__dependency-extraction-webpack-plugin/index.d.ts
@@ -0,0 +1,18 @@
+// Type definitions for @wordpress/dependency-extraction-webpack-plugin 1.0
+// Project: https://github.com/WordPress/gutenberg/tree/master/packages/dependency-extraction-webpack-plugin/README.md
+// Definitions by: Derek Sifford <https://github.com/dsifford>
+// Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
+// TypeScript Version: 3.4
+
+import { Plugin } from "webpack";
+
+export interface Options {
+    useDefaults?: boolean;
+    injectPolyfill?: boolean;
+    requestToExternal?(request: string): string | string[] | void;
+    requestToHandle?(request: string): string | void;
+}
+
+export default class DependencyExtractionWebpackPlugin extends Plugin {
+    constructor(options?: Options);
+}
