b40a7186caa3f446dea5e2b14784f48696325a8f
Manuel Thalmann <m@nuth.ch>
2019-06-07 23:43:18 +0200

    Fix the code according to tslint

--- types/npm-run/index.d.ts
+++ types/npm-run/index.d.ts
@@ -2,14 +2,14 @@
 // Project: https://github.com/timoxley/npm-run
 // Definitions by: Manuel Thalmann <https://github.com/manuth>
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
+// TypeScript Version: 2.2
 /// <reference types="node" />
 
 import { exec, execSync, spawnSync, spawn } from "child_process";
 
 type ExecFunction = typeof exec;
 
-interface Runner extends ExecFunction
-{
+interface Runner extends ExecFunction {
     exec: ExecFunction;
     sync: typeof execSync;
     execSync: typeof execSync;
