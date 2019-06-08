b544f4a384fc3f91b97ccf81900e26c7f5299be2
gossi <gossi@users.noreply.github.com>
2019-06-08 10:33:35 +0200

    increase version number

--- types/ember__test-helpers/index.d.ts
+++ types/ember__test-helpers/index.d.ts
@@ -1,4 +1,4 @@
-// Type definitions for @ember/test-helpers 0.7
+// Type definitions for @ember/test-helpers 0.8
 // Project: https://github.com/emberjs/ember-test-helpers
 // Definitions by: Dan Freeman <https://github.com/dfreeman>
 //                 James C. Davis <https://github.com/jamescdavis>
