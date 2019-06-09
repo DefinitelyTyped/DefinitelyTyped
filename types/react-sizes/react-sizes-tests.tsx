0085f08340d2023eca0451ec4a7a2c7377acb97f
micahstubbs <micah.stubbs@gmail.com>
2019-06-09 15:37:03 -0700

    fix example in test

--- types/react-sizes/react-sizes-tests.tsx
+++ types/react-sizes/react-sizes-tests.tsx
@@ -2,25 +2,23 @@ import * as React from "react";
 import { withSizes, Sizes } from "react-sizes";
 
 interface TestProps {
-    foo?: string;
+    foo: string;
+    width: number;
+    height: number;
 }
 
-interface TestInnerProps {
-    TestProps,
-    Sizes
-} 
-
 const mapSizesToProps = ({ width, height }: Sizes) => ({
+    foo: 'foo',
     width,
     height
 });
 
-const TestComponent: React.ComponentType<TestInnerProps> = ({
+const TestComponent: React.ComponentType<TestProps> = ({
     foo,
     width,
     height
 }) => {
-    foo; // $ExpectType string | undefined
+    foo; // $ExpectType string 
     width; // $ExpectType number
     height; // $ExpectType number
     return (
