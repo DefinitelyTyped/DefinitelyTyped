1ae9964b16332a774f6fb7b55fc4814185f3dc9e
ben-thinkpad-e480 <ben@givingway.com>
2019-06-09 20:40:05 +0300

    more effecient DeepReadonly

--- types/react/index.d.ts
+++ types/react/index.d.ts
@@ -3009,20 +3009,39 @@ declare global {
             view: React.SVGProps<SVGViewElement>;
         }
     }
-                    
+     
+    
+    type DR<T> = DeepReadonly<T>
     
     type DeepReadonly<T> =
-    T extends Function ? T :
-    T extends ReadonlyArray<infer R> ? DRArray<R> :
-    T extends Map<infer K, infer V> ? DRMap<K, V> :
-    T extends object ? DRObject<T> :
-    T;
-
-    interface DRArray<T> extends ReadonlyArray<DeepReadonly<T>> {}
-
+        T extends  AnyFunction | Primitive ? T :
+        T extends ReadonlyArray<infer R> ? IDRArray<R> :
+        T extends ReadonlyMap<infer K, infer V> ? IDRMap<K, V> :
+        T extends ReadonlySet<infer ItemType>? ReadonlySetDeep<ItemType>:
+        T extends object ? DRObject<T> :
+        T
+    
+    interface IDRArray<T> extends ReadonlyArray<DeepReadonly<T>> {}
+    
     type DRObject<T> = {
         readonly [P in keyof T]: DeepReadonly<T[P]>;
-    };
+    }
+    
+    interface IDRMap<K, V> extends ReadonlyMap<DeepReadonly<K>, DeepReadonly<V>> {}
+    
+    interface ReadonlySetDeep<ItemType>
+        extends ReadonlySet<DeepReadonly<ItemType>> {}
 
-    interface DRMap<K, V> extends ReadonlyMap<DeepReadonly<K>, DeepReadonly<V>> {}
+    
+    export type Primitive =
+    | null
+    | undefined
+    | string
+    | number
+    | boolean
+    | symbol
+    | bigint
+
+    type AnyFunction = (...args: any[]) => any
+    
 }
