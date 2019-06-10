ffdd9cf5fc8f1046ed9fb7e5d85a4ef9d3b18ba4
John Gethoefer <john@gethoefer.com>
2019-06-10 12:30:25 -0700

    Adds type definitions for 'promise-nodeify'
    Adds type definitions and tests
    Adds tsconfig and tslint config

---
+++ types/promise-nodeify/index.d.ts
@@ -0,0 +1,25 @@
+// Type definitions for promise-nodeify 3.0
+// Project: https://github.com/kevinoid/promise-nodeify
+// Definitions by: John Gethoefer <https://github.com/me>
+// Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
+
+export as namespace promiseNodeify;
+
+declare const promiseNodeify: {
+    <T>(
+        promise: Promise<T>,
+        callback: (error: Error | undefined, value: T | undefined) => any
+    ): Promise<T> | undefined;
+
+    delegated<T>(
+        promise: Promise<T>,
+        callback: (error: Error | undefined, value: T | undefined) => any
+    ): Promise<T> | undefined;
+
+    nodeifyThis<T>(
+        promise: Promise<T>,
+        callback: (error: Error | undefined, value: T | undefined) => any
+    ): Promise<T> | undefined;
+};
+
+export = promiseNodeify;
