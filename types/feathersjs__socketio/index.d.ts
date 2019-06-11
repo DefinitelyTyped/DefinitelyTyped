f7daabaa3aaff7326d3554f754baab2971b43522
deskoh <deskoh.sg@gmail.com>
2019-06-11 13:27:25 +0800

    Updated header.

--- types/feathersjs__socketio/index.d.ts
+++ types/feathersjs__socketio/index.d.ts
@@ -1,6 +1,7 @@
 // Type definitions for @feathersjs/socketio 3.0
 // Project: https://feathersjs.com
 // Definitions by: Jan Lohage <https://github.com/j2L4e>
+//                 Desmond Koh <https://github.com/deskoh>
 // Definitions: https://github.com/feathersjs-ecosystem/feathers-typescript
 // TypeScript Version: 2.8
 
