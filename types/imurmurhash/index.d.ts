ae7be0603d208262d9754500e38112e39d9e43bb
Jiayu Liu <etareduce@gmail.com>
2019-06-11 18:11:37 +0800

    add types

--- types/imurmurhash/index.d.ts
+++ types/imurmurhash/index.d.ts
@@ -1,39 +1,15 @@
 // Type definitions for imurmurhash 0.1
 // Project: https://github.com/jensyt/imurmurhash-js
-// Definitions by: Jiayu Liu <https://github.com/DefinitelyTyped>
+// Definitions by: Jiayu Liu <jimexist@gmail.com>
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
 
-/*~ If this module is a UMD module that exposes a global variable 'myLib' when
- *~ loaded outside a module loader environment, declare that global here.
- *~ Otherwise, delete this declaration.
- */
-export as namespace myLib;
+export interface IMurmurHash3 {
+    result(): number;
 
-/*~ If this module has methods, declare them as functions like so.
- */
-export function myMethod(a: string): string;
-export function myOtherMethod(a: number): number;
+    reset(seed?: number): IMurmurHash3;
 
-/*~ You can declare types that are available via importing the module */
-export interface someType {
-    name: string;
-    length: number;
-    extras?: string[];
+    hash(value: string): IMurmurHash3;
 }
 
 /*~ You can declare properties of the module using const, let, or var */
-export const myField: number;
-
-/*~ If there are types, properties, or methods inside dotted names
- *~ of the module, declare them inside a 'namespace'.
- */
-export namespace subProp {
-    /*~ For example, given this definition, someone could write:
-     *~   import { subProp } from 'yourModule';
-     *~   subProp.foo();
-     *~ or
-     *~   import * as yourMod from 'yourModule';
-     *~   yourMod.subProp.foo();
-     */
-    function foo(): void;
-}
+export default function MurmurHash3(text?: string, seed?: number): IMurmurHash3;
