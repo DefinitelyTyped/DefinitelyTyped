32ee69a015907a64055d82efd351ad81ce91c6c3
Mike Martin <2hip2carebear@gmail.com>
2019-06-11 13:30:47 -0700

    Genericize ReactTimeago by ComponentType
    This allows properties from `component` to be added to `TimeAgo` without throwing an error.
    e.g.
    ```
    <TimeAgo<typeof Text>
      component={Text}
      style={[styles.lastActiveText, { fontSize: activeBubbleSize * 0.588 }]}
      date={lastActiveAt}
    />
    ```
    Type inferencing not working yet, so must specify generic in order to add properties from `component`

--- types/react-timeago/react-timeago-tests.tsx
+++ types/react-timeago/react-timeago-tests.tsx
@@ -1,16 +1,20 @@
 import ReactTimeago, { Unit, Suffix } from "react-timeago";
 import * as React from "react";
+import { Text } from "react-native";
 
 const ReactTimeagoRequiredOptions: JSX.Element = (
-    <ReactTimeago
-        date={new Date()}
-    />
+    <ReactTimeago date={new Date()} />
 );
 
-const customFormatter = (value: number, unit: Unit, suffix: Suffix, epochMiliseconds: number) => {
-    return (
-        <div />
-    );
+const customFormatter = (
+    value: number,
+    unit: Unit,
+    suffix: Suffix,
+    epochMiliseconds: number
+) => {
+    return epochMiliseconds > 300000
+        ? `${value}${unit[0]} ${suffix}`
+        : "a really long time ago";
 };
 
 const ReactTimeagoAllOptions: JSX.Element = (
@@ -22,6 +26,17 @@ const ReactTimeagoAllOptions: JSX.Element = (
         component={() => <div />}
         title="Timeago"
         formatter={customFormatter}
+    />
+);
 
+const ReactTimeagoCustomComponent: JSX.Element = (
+    <ReactTimeago<typeof Text>
+        date={new Date()}
+        component={Text}
+        // props passed down to Text
+        style={[{ textAlign: "center" }, { fontSize: 24 }]}
+        numberOfLines={2}
+        ellipsizeMode="middle"
+        allowFontScaling
     />
 );
