689624361ba6b8b5657cefb427de818327c61a7a
Jonathan Ziller <jonathan.ziller@gmail.com>
2019-06-12 15:57:39 +0200

    react-redux: add types for hooks which will be added in version 7.1 (#34913)
    * react-redux: add types for hooks which will be added in version 7.1
    
    * react-redux: fix linting error due to redundant type declaration in JSDoc comment
    
    * react-redux: convert action creator return type to any in useActions and useRedux
    
    * react-redux: fix linting errors
    
    * react-redux: fix mistake in `useActions` hook example code
    
    * react-redux: add proper type annotations in hooks example code comments
    
    * react-redux: support thunks in `useActions`, `useDispatch`, and `useRedux`; add tests for hooks; bump TypeScript required version to 3.1 (implemented by @threehams)
    
    * react-redux: remove obsolete comment
    
    * react-redux: adjust thunk array handling to work with TypeScript 3.0
    
    * react-redux: Remove useRedux hook
    
    With the release of react-redux@7.1.0-alpha.3, `useRedux` has been
    removed, so removing it from the types.
    
    * update typings for latest react-redux hooks API changes + add overload for `useDispatch` that allows customizing the type of the returned dispatch function
    
    * react-redux: add equalityFn argument to useSelector
    
    * react-redux: add newly exported shallowEqual function; add tests for shallowEqual and equalityFn parameter of useSelector
    
    * react-redux: add interface that allows to easily create a typed useSelector hook

--- types/react-redux/react-redux-tests.tsx
+++ types/react-redux/react-redux-tests.tsx
@@ -1,7 +1,32 @@
 import * as React from 'react';
 import * as ReactDOM from 'react-dom';
-import { Store, Dispatch, AnyAction, ActionCreator, createStore, bindActionCreators, ActionCreatorsMapObject, Reducer } from 'redux';
-import { Connect, connect, Provider, DispatchProp, MapStateToProps, Options, ReactReduxContext, ReactReduxContextValue, Selector, MapDispatchToProps } from 'react-redux';
+import {
+    Store,
+    Dispatch,
+    AnyAction,
+    ActionCreator,
+    createStore,
+    bindActionCreators,
+    ActionCreatorsMapObject,
+    Reducer,
+} from 'redux';
+import {
+    Connect,
+    connect,
+    Provider,
+    DispatchProp,
+    MapStateToProps,
+    Options,
+    ReactReduxContext,
+    ReactReduxContextValue,
+    Selector,
+    shallowEqual,
+    MapDispatchToProps,
+    useDispatch,
+    useSelector,
+    useStore,
+    TypedUseSelectorHook,
+} from 'react-redux';
 import objectAssign = require('object-assign');
 
 //
@@ -82,7 +107,7 @@ function MapDispatch() {
 
     class TestComponent extends React.Component<OwnProps & DispatchProps> { }
 
-    const mapDispatchToProps = ({ onClick: () => {} });
+    const mapDispatchToProps = ({ onClick: () => { } });
 
     const TestNull = connect(
         null,
@@ -1273,4 +1298,105 @@ function TestSelector() {
     notSimpleSelect(state, ownProps);
     simpleSelect(state, ownProps); // $ExpectError
     notSimpleSelect(state); // $ExpectError
+}
+
+function testShallowEqual() {
+    shallowEqual(); // $ExpectError
+    shallowEqual('a'); // $ExpectError
+    shallowEqual('a', 'a');
+    shallowEqual({ test: 'test' }, { test: 'test' });
+    shallowEqual({ test: 'test' }, 'a');
+    const x: boolean = shallowEqual('a', 'a');
+}
+
+function testUseDispatch() {
+    const actionCreator = (selected: boolean) => ({
+        type: "ACTION_CREATOR",
+        payload: selected,
+    });
+    const thunkActionCreator = (selected: boolean) => {
+        return (dispatch: Dispatch) => {
+            return dispatch(actionCreator(selected));
+        };
+    };
+
+    const dispatch = useDispatch();
+    dispatch(actionCreator(true));
+    dispatch(thunkActionCreator(true));
+    dispatch(true);
+
+    type ThunkAction<TReturnType> = (dispatch: Dispatch) => TReturnType;
+    type ThunkDispatch = <TReturnType>(action: ThunkAction<TReturnType>) => TReturnType;
+    // tslint:disable-next-line:no-unnecessary-callback-wrapper (required for the generic parameter)
+    const useThunkDispatch = () => useDispatch<ThunkDispatch>();
+    const thunkDispatch = useThunkDispatch();
+    const result: ReturnType<typeof actionCreator> = thunkDispatch(thunkActionCreator(true));
+}
+
+function testUseSelector() {
+    interface State {
+        counter: number;
+        active: string;
+    }
+
+    const selector = (state: State) => {
+        return {
+            counter: state.counter,
+            active: state.active,
+        };
+    };
+    const { counter, active } = useSelector(selector);
+    counter === 1;
+    counter === "321"; // $ExpectError
+    active === "hi";
+    active === {}; // $ExpectError
+
+    const { extraneous } = useSelector(selector); // $ExpectError
+    useSelector(selector);
+
+    useSelector(selector, 'a'); // $ExpectError
+    useSelector(selector, (l, r) => l === r);
+    useSelector(selector, (l, r) => {
+        // $ExpectType { counter: number; active: string; }
+        l;
+        return l === r;
+    });
+
+    useSelector(selector, shallowEqual);
+    const compare = (_l: number, _r: number) => true;
+    useSelector(() => 1, compare);
+    const compare2 = (_l: number, _r: string) => true;
+    useSelector(() => 1, compare2); // $ExpectError
+
+    interface RootState {
+        property: string;
+    }
+
+    const useTypedSelector: TypedUseSelectorHook<RootState> = useSelector;
+
+     // $ExpectType string
+    const r = useTypedSelector(state => {
+        // $ExpectType RootState
+        state;
+        return state.property;
+    });
+}
+
+function testUseStore() {
+    interface TypedState {
+        counter: number;
+        active: boolean;
+    }
+    interface TypedAction {
+        type: "SET_STATE";
+    }
+
+    const untypedStore = useStore();
+    const state = untypedStore.getState();
+    state.things.stuff.anything; // any by default
+
+    const typedStore = useStore<TypedState, TypedAction>();
+    const typedState = typedStore.getState();
+    typedState.counter;
+    typedState.things.stuff; // $ExpectError
 }
