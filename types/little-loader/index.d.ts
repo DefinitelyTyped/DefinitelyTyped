5c3bd7a816a18d172757fbddd2e15f8a6fb2a1da
Chris Drackett <chrisdrackett@users.noreply.github.com>
2019-06-11 21:12:08 -0700

    going back to original. giving up.

--- types/little-loader/index.d.ts
+++ types/little-loader/index.d.ts
@@ -1,12 +1,14 @@
-// Type definitions for little-loader 0.2
-// Project: https://github.com/walmartlabs/little-loader#readme
+// Type definitions for little-loader
+// Project: https://github.com/walmartlabs/little-loader
 // Definitions by: Chris Drackett <https://github.com/chrisdrackett>
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
 
-declare function load(
-  module: string,
-  callBack: (err: string) => void,
-  context: any,
-): void
+declare module 'little-loader' {
+  function load(
+    module: string,
+    callBack: (err: string) => void,
+    context: any,
+  ): void
 
-export default load
+  export default load
+}
