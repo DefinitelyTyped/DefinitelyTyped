6fffa7da4904eb1466ef1b51794961bd56c894a8
gstamac <1668205+gstamac@users.noreply.github.com>
2019-06-12 21:23:22 +0200

    [dirty-chai] fix 'esModuleInterop' requirement

--- types/dirty-chai/index.d.ts
+++ types/dirty-chai/index.d.ts
@@ -1,28 +1,30 @@
 // Type definitions for dirty-chai 2.0
 // Project: https://github.com/prodatakey/dirty-chai
 // Definitions by: Piotr Roszatycki <https://github.com/dex4er>
+//                 Gregor Stamać <https://github.com/gstamac>
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
 
 /// <reference types="chai" />
 /// <reference types="chai-as-promised" />
 
-declare global {
-    namespace Chai {
-        interface LanguageChains {
-            always: Assertion;
-        }
+import Assertion = Chai.Assertion;
 
-        interface Assertion {
-            (message?: string): Assertion;
-            ensure: Assertion;
-        }
+declare module 'dirty-chai' {
+    function dirtyChai(chai: any, utils: any): void;
 
-        interface PromisedAssertion extends Eventually, PromiseLike<any> {
-            (message?: string): Assertion;
-            ensure: Assertion;
-        }
-    }
+    namespace dirtyChai { }
+
+    export = dirtyChai;
 }
 
-declare function dirtyChai(chai: any, utils: any): void;
-export = dirtyChai;
+declare namespace Chai {
+    interface Assertion {
+        (message?: string): Assertion;
+        ensure(): Assertion;
+    }
+
+    interface PromisedAssertion extends Eventually, PromiseLike<any> {
+        (message?: string): Assertion;
+        ensure(): Assertion;
+    }
+}
