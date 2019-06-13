b1c84d7a87f83e6a037b9f3287ff440fd283dc39
Jiayu Liu <etareduce@gmail.com>
2019-06-13 14:50:57 +0800

    fix per comment

--- types/imurmurhash/index.d.ts
+++ types/imurmurhash/index.d.ts
@@ -1,17 +1,17 @@
 // Type definitions for imurmurhash 0.1
 // Project: https://github.com/jensyt/imurmurhash-js
-// Definitions by: Jiayu Liu <jimexist@gmail.com>
+// Definitions by: Jiayu Liu <https://github.com/Jimexist>
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
+// TypeScript Version: 2.7
 
-declare module "imurmurhash" {
-    interface MurmurHash3 {
-        result(): number;
+interface MurmurHash3 {
+    result(): number;
 
-        reset(seed?: number): MurmurHash3;
+    reset(seed?: number): MurmurHash3;
 
-        hash(value: string): MurmurHash3;
-    }
-    function MurmurHash(text?: string, seed?: number): MurmurHash3;
-
-    export = MurmurHash;
+    hash(value: string): MurmurHash3;
 }
+
+declare function MurmurHash(text?: string, seed?: number): MurmurHash3;
+
+export = MurmurHash;
