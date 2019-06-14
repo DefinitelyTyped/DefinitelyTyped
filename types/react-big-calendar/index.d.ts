612308fa5c58a0d2684c519515e99a493cbb5b67
Eric Kenney <e.kenney@contactelevation.com>
2019-06-13 23:39:36 -0600

    Use [] array syntax in react-big-calendar

--- types/react-big-calendar/index.d.ts
+++ types/react-big-calendar/index.d.ts
@@ -262,7 +262,7 @@ export interface BigCalendarProps<TEvent extends Event = Event, TResource extend
     onDoubleClickEvent?: (event: TEvent, e: React.SyntheticEvent<HTMLElement>) => void;
     onSelectEvent?: (event: TEvent, e: React.SyntheticEvent<HTMLElement>) => void;
     onSelecting?: (range: { start: stringOrDate, end: stringOrDate }) => boolean | undefined | null;
-    onRangeChange?: (range: Array<Date> | { start: stringOrDate, end: stringOrDate }) => void;
+    onRangeChange?: (range: Date[] | { start: stringOrDate, end: stringOrDate }) => void;
     selected?: any;
     views?: Views;
     drilldownView?: View | null;
