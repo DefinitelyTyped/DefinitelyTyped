9247916424e8b76c21bad4607e8eece83112c433
Levi Bostian <levi.bostian@gmail.com>
2019-06-14 14:39:44 -0500

    Add typings for npm module uid2

---
+++ types/uid2/index.d.ts
@@ -0,0 +1,6 @@
+// Type definitions for uid2 0.3.3
+// Project: https://github.com/coreh/uid2
+// Definitions by: Levi Bostian <https://github.com/levibostian>
+// Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
+
+export default function(length: number): string;
---
+++ types/uid2/uid2-tests.ts
@@ -0,0 +1,3 @@
+import uid2 from 'uid2';
+
+uid2(20); // $ExpectType string
