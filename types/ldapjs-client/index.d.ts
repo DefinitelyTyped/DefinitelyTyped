87c95ffdb991e19c6f90f295e05c34001c334640
colthreepv <valerio.coltre@gmail.com>
2019-06-17 18:46:35 +0200

    fix tsconfig and typescript version. lint should go green

--- types/ldapjs-client/index.d.ts
+++ types/ldapjs-client/index.d.ts
@@ -2,6 +2,7 @@
 // Project: https://github.com/zont/ldapjs-client#readme
 // Definitions by: Valerio Coltrè <https://github.com/colthreepv>
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
+// TypeScript Version: 2.2
 
 export class Filter {
     matches(obj: any): boolean;
