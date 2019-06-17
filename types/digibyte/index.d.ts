9d1b4de5527fc88da9de16493a033b92be110745
Daniel Rosenwasser <DanielRosenwasser@users.noreply.github.com>
2019-06-17 00:09:00 -0700

    Update index.d.ts

--- types/digibyte/index.d.ts
+++ types/digibyte/index.d.ts
@@ -1,4 +1,4 @@
-// Type definitions for digibyte 0.15
+// Type definitions for digibyte 0.14
 // Project: https://github.com/digicontributer/digibyte-js
 // Definitions by: Lautaro Dragan <https://github.com/lautarodragan>
 //                 Adam Wolfe <https://github.com/werewolfe>
