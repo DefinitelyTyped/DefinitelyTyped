6743fc1536d1bad3aa3cc27d7e0b0d71afef2311
Alex Curtis <alex.curtis@ft.com>
2019-06-18 12:03:34 +0100

    Specify TS version

--- types/targz/index.d.ts
+++ types/targz/index.d.ts
@@ -2,6 +2,7 @@
 // Project: https://github.com/miskun/targz#readme
 // Definitions by: Alexander Curtis <https://github.com/alexandercurtis>
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
+// TypeScript Version: 2.6
 
 declare module 'targz' {
     import * as tar from 'tar-fs';
