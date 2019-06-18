47ce9e3358b24adc1d71cbc15b85be63e6070b20
Scott Cooper <scttcper@gmail.com>
2019-06-17 22:12:21 -0700

    fix: support knex 0.17.x and mock-knex 0.4

--- types/mock-knex/index.d.ts
+++ types/mock-knex/index.d.ts
@@ -1,4 +1,4 @@
-// Type definitions for mock-knex 0.3
+// Type definitions for mock-knex 0.4
 // Project: https://github.com/colonyamerican/mock-knex
 // Definitions by: Jesse Zhang <https://github.com/jessezhang91>
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
