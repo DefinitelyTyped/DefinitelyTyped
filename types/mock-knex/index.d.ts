4b0c04c5daf1a585d445a2bed612cf8aea9f8b0c
Scott Cooper <scttcper@gmail.com>
2019-06-17 22:20:22 -0700

    requires typescript 3 for knex 0.17.x unknown type use

--- types/mock-knex/index.d.ts
+++ types/mock-knex/index.d.ts
@@ -2,7 +2,7 @@
 // Project: https://github.com/colonyamerican/mock-knex
 // Definitions by: Jesse Zhang <https://github.com/jessezhang91>
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
-// TypeScript Version: 2.8
+// TypeScript Version: 3.0
 
 /// <reference types="node" />
 
