9cf5c98aa8c9edc65e70dd078e0f94d72d4aa6d1
Regev Brody <regevbr@gmail.com>
2019-06-19 20:04:01 +0300

    underscore - add typing to pick (#36230)
    * underscore - add typing to pick
    
    * underscore - add typing to pick
    
    * underscore - add typing to pick
    
    * underscore - add typing to pick

--- types/backbone-relational/index.d.ts
+++ types/backbone-relational/index.d.ts
@@ -1,9 +1,9 @@
-// Type definitions for Backbone-relational 0.10.0
+// Type definitions for Backbone-relational 0.10
 // Project: http://backbonerelational.org/
 // Definitions by: Eirik Hoem <https://github.com/eirikhm>
 //                 Julian Gonggrijp <https://github.com/jgonggrijp>
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
-// TypeScript Version: 2.3
+// TypeScript Version: 2.8
 
 /// <reference types="jquery" />
 
