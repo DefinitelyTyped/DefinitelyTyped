b8aa6318b44c33172cf9a03f7e4b34d3741b2d04
aleccool213 <alec@alec.coffee>
2019-06-19 11:33:36 -0400

    Add definitions for cypress-cucumber-preprocessor
    Add tests for cypress-cucumber-preprocessor

---
+++ types/cypress-cucumber-preprocessor/index.d.ts
@@ -0,0 +1,6 @@
+// Type definitions for cypress-cucumber-preprocessor 1.12
+// Project: https://github.com/TheBrainFamily/cypress-cucumber-preprocessor
+// Definitions by: Alec Brunelle <https://github.com/aleccool213>
+// Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
+
+export {};
---
+++ types/cypress-cucumber-preprocessor/steps/index.d.ts
@@ -0,0 +1,2 @@
+export function Given(expression: RegExp | string, implementation: (...args: any[]) => void): void;
+export function Then(expression: RegExp | string, implementation: (...args: any[]) => void): void;
