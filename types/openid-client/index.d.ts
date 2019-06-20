38d0a27f65fd482772e4715fb74fb5a407f51968
Brandon <brandon@pagedip.com>
2019-06-20 15:38:53 -0600

    fix Issuer constructor; improve object typing and return types

--- types/openid-client/index.d.ts
+++ types/openid-client/index.d.ts
@@ -1,20 +1,42 @@
 // Type definitions for openid-client 3.1
 // Project: https://github.com/panva/node-openid-client
 // Definitions by: ulrichb <https://github.com/ulrichb>
+//                 Brandon Shelton <https://github.com/YangusKhan>
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
 // TypeScript Version: 3.0
 
 /// <reference types="node" />
 
-import { IncomingMessage } from "http";
+import { IncomingMessage } from 'http';
 
 // https://github.com/panva/node-openid-client/tree/master/docs#issuer
 
 export interface IssuerMetadata {
     readonly issuer?: string;
+    readonly authorization_endpoint?: string;
+    readonly userinfo_endpoint?: string;
+    readonly token_endpoint?: string;
+    readonly jwks_uri: string;
+    readonly revocation_endpoint?: string;
+    readonly introspection_endpoint?: string;
+    readonly end_session_endpoint?: string;
+    readonly registration_endpoint?: string;
+    readonly token_endpoint_auth_methods_supported?: ReadonlyArray<string>;
+    readonly token_endpoint_auth_signing_alg_values_supported?: ReadonlyArray<string>;
+    readonly introspection_endpoint_auth_methods_supported?: ReadonlyArray<string>;
+    readonly introspection_endpoint_auth_signing_alg_values_supported?: ReadonlyArray<string>;
+    readonly revocation_endpoint_auth_methods_supported?: ReadonlyArray<string>;
+    readonly revocation_endpoint_auth_signing_alg_values_supported?: ReadonlyArray<string>;
+    readonly mtls_endpoint_aliases?: {
+        token_endpoint?: string;
+        userinfo_endpoint?: string;
+        revocation_endpoint?: string;
+        introspection_endpoint?: string;
+    };
 }
 
 export class Issuer {
+    constructor(metadata?: IssuerMetadata);
     static discover(issuer: string): Promise<Issuer>;
 
     readonly metadata: IssuerMetadata;
@@ -51,7 +73,7 @@ export interface EndSessionUrlParameters {
 }
 
 export class Client {
-    constructor(metadata: ClientMetadata);
+    constructor(metadata?: ClientMetadata);
 
     readonly metadata: ClientMetadata;
 
@@ -59,25 +81,32 @@ export class Client {
 
     endSessionUrl(parameters?: EndSessionUrlParameters): string;
 
-    callbackParams(input: string | IncomingMessage): {};
+    callbackParams(input: string | IncomingMessage): object;
 
-    callback(redirectUri: string, parameters: {}, checks?: {
-        readonly response_type?: string;
-        readonly state?: string;
-        readonly nonce?: string;
-        readonly code_verifier?: string;
-        readonly max_age?: number;
-    }): Promise<TokenSet>;
+    callback(
+        redirectUri: string,
+        parameters: object,
+        checks?: {
+            readonly response_type?: string;
+            readonly state?: string;
+            readonly nonce?: string;
+            readonly code_verifier?: string;
+            readonly max_age?: number;
+        }
+    ): Promise<TokenSet>;
 
-    userinfo(accessToken: string | TokenSet): Promise<{ readonly [name: string]: {} | null | undefined }>;
+    userinfo(accessToken: string | TokenSet): Promise<{ readonly [name: string]: any }>;
 
     grant(body: {
-        readonly grant_type: "authorization_code" | "client_credentials" | "password" | "refresh_token" | string;
+        readonly grant_type: 'authorization_code' | 'client_credentials' | 'password' | 'refresh_token' | string;
         readonly [name: string]: string | undefined;
     }): Promise<TokenSet>;
 
-    introspect(token: string, tokenTypeHint?: string, extras?: { readonly introspectBody?: {} }):
-        Promise<{ readonly [name: string]: {} | null | undefined }>;
+    introspect(
+        token: string,
+        tokenTypeHint?: string,
+        extras?: { readonly introspectBody?: object }
+    ): Promise<{ readonly [name: string]: any }>;
 }
 
 export class TokenSet {
@@ -88,7 +117,7 @@ export class TokenSet {
 
     expired(): boolean;
 
-    claims(): { readonly [name: string]: {} | null | undefined };
+    claims(): { readonly [name: string]: any };
 }
 
 export namespace generators {
