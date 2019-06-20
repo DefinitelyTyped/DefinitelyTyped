ac28883ff40e22637dd019a609ca06030f9c095b
rhysd <lin90162@yahoo.co.jp>
2019-06-21 08:10:10 +0900

    Improve types of `ccall` and `cwrap` functions

--- types/emscripten/index.d.ts
+++ types/emscripten/index.d.ts
@@ -17,6 +17,8 @@ declare namespace Emscripten {
 
 declare namespace Module {
     type EnvironmentType = "WEB" | "NODE" | "SHELL" | "WORKER";
+    type ValueType = "number" | "string" | "array" | "boolean";
+    type TypeCompatibleWithC = number | string | Array<any> | boolean;
 
     type WebAssemblyImports =  Array<{
         name: string;
@@ -54,8 +56,8 @@ declare namespace Module {
 
     var Runtime: any;
 
-    function ccall(ident: string, returnType: string | null, argTypes: string[], args: any[]): any;
-    function cwrap(ident: string, returnType: string | null, argTypes: string[]): any;
+    function ccall(ident: string, returnType: ValueType | null, argTypes: ValueType[], args: TypeCompatibleWithC[]): any;
+    function cwrap(ident: string, returnType: ValueType | null, argTypes: ValueType[]): (...any[]) => any;
 
     function setValue(ptr: number, value: any, type: string, noSafe?: boolean): void;
     function getValue(ptr: number, type: string, noSafe?: boolean): number;
