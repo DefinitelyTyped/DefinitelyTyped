5779bedbe2547c30fb8ea39d426864d6c80a61b1
Martin Jurča <martin.jurca@firma.seznam.cz>
2019-06-20 11:19:41 +0200

    code style fix

--- types/promise.allsettled/auto.d.ts
+++ types/promise.allsettled/auto.d.ts
@@ -1,5 +1,5 @@
 // This file exists only to reflect the existence of the auto.js file in the source package, which has a side-effect
 // when imported.
 
-declare const exports: {}
+declare const exports: {};
 export = exports;
