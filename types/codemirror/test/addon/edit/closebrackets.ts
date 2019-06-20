9c3d8b7af8d90ff073d4b3848bd21b68a9cc9277
xemlock <xemlock@gmail.com>
2019-06-20 15:03:58 +0200

    [@types/codemirror] Add boolean type to autoCloseBrackets option
    Currently definition for autoCloseBrackets doesn't allow boolean values, which
    is wrong. Setting autoCloseBrackets to true enables autoclosing feature with
    the default settings, as is visible in the source code of a demo on the
    CodeMirror's official website: https://codemirror.net/demo/closebrackets.html
    
    var editor = CodeMirror.fromTextArea(document.getElementById("code"), {autoCloseBrackets: true});

--- types/codemirror/test/addon/edit/closebrackets.ts
+++ types/codemirror/test/addon/edit/closebrackets.ts
@@ -4,3 +4,7 @@
 var myCodeMirror: CodeMirror.Editor = CodeMirror(document.body, {
     autoCloseBrackets: "()[]{}''\"\""
 });
+
+var myCodeMirror2: CodeMirror.Editor = CodeMirror(document.body, {
+    autoCloseBrackets: true
+});
