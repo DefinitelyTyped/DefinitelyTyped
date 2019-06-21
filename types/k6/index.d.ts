5d6c4bb5b87e301e913cf1e93da1e23acca21930
Book Moons <35854232+bookmoons@users.noreply.github.com>
2019-06-21 15:26:27 -0400

    k6: Add author

--- types/k6/index.d.ts
+++ types/k6/index.d.ts
@@ -1,6 +1,7 @@
 // Type definitions for k6 0.0
 // Project: https://github.com/loadimpact/k6#readme
 // Definitions by: MajorBreakfast <https://github.com/MajorBreakfast>
+//                 Book Moons <https://github.com/bookmoons>
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
 // TypeScript Version: 2.2
 
