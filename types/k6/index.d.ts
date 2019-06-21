ef49a2f20f3f680fde0e7021adea5f16909f5e69
Book Moons <35854232+bookmoons@users.noreply.github.com>
2019-06-21 17:35:48 -0400

    k6: Document version 1.0

--- types/k6/index.d.ts
+++ types/k6/index.d.ts
@@ -1,4 +1,4 @@
-// Type definitions for k6 0.0
+// Type definitions for k6 1.0
 // Project: https://github.com/loadimpact/k6#readme
 // Definitions by: MajorBreakfast <https://github.com/MajorBreakfast>
 //                 Book Moons <https://github.com/bookmoons>
