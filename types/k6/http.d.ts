652bc4e1abd6f4879d8ae047bf4e262dbad9bbc1
Book Moons <35854232+bookmoons@users.noreply.github.com>
2019-06-21 18:04:19 -0400

    k6: Normalize formatting
    Uses the repo formatter to normalize code format.

--- types/k6/http.d.ts
+++ types/k6/http.d.ts
@@ -42,49 +42,46 @@ export interface Response {
     error: string;
     headers: { [key: string]: string };
     ocsp: {
-        produced_at: number,
-        this_update: number,
-        next_update: number,
-        revocation_reason: string,
-        revoked_at: number,
-        status: string
+        produced_at: number;
+        this_update: number;
+        next_update: number;
+        revocation_reason: string;
+        revoked_at: number;
+        status: string;
     };
     proto: string;
     remote_ip: string;
     remote_port: number;
     request: {
-        body: string,
-        cookies: object,
-        headers: object,
-        method: string,
-        url: string
+        body: string;
+        cookies: object;
+        headers: object;
+        method: string;
+        url: string;
     };
     status: number;
     timings: {
-        blocked: number,
-        looking_up: number,
-        connecting: number,
-        tls_handshaking: number,
-        sending: number,
-        waiting: number,
-        receiving: number,
-        duration: number
+        blocked: number;
+        looking_up: number;
+        connecting: number;
+        tls_handshaking: number;
+        sending: number;
+        waiting: number;
+        receiving: number;
+        duration: number;
     };
     tls_cipher_suite: string;
     tls_version: string;
     url: string;
-    clickLink: (params?: {
-        selector?: string,
-        params?: RequestParams
-    }) => Response;
+    clickLink: (params?: { selector?: string; params?: RequestParams }) => Response;
     html: (selector?: string) => any;
     json: () => any;
     submitForm: (params?: {
-        formSelector?: string,
-        fields?: object,
-        submitSelector?: string,
-        params?: RequestParams
-    }) =>  Response;
+        formSelector?: string;
+        fields?: object;
+        submitSelector?: string;
+        params?: RequestParams;
+    }) => Response;
 }
 
 export function file(data: string | bytes, filename?: string, contentType?: string): FileData;
