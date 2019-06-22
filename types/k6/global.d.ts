d6f9826142fc2f4a9c03318512d3b12d1db818cd
Book Moons <35854232+bookmoons@users.noreply.github.com>
2019-06-22 15:56:22 -0400

    k6: Rename init global

--- types/k6/global.d.ts
+++ types/k6/global.d.ts
@@ -1,8 +1,8 @@
 import { bytes } from '.';
 
 // Available without importing
-// Only available in init context
 declare global {
+    // Init context only
     function open(filePath: string): string;
     function open(filePath: string, mode: 'b'): bytes;
 }
