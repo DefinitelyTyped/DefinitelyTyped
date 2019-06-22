30ce3cdbc82f86451340a76135b0071786093ba7
HarikiRito <35214929+HarikiRito@users.noreply.github.com>
2019-06-22 10:41:26 +0700

    Update callback_api.d.ts

--- types/amqplib/callback_api.d.ts
+++ types/amqplib/callback_api.d.ts
@@ -71,3 +71,4 @@ export const credentials: {
 export function connect(callback: (err: any, connection: Connection) => void): void;
 export function connect(url: string, callback: (err: any, connection: Connection) => void): void;
 export function connect(url: string, socketOptions: any, callback: (err: any, connection: Connection) => void): void;
+export function connect(url: string | Options.Connect, socketOptions?: any): Promise<Connection>;
