94035594016a4be19010914c4b71dae90fce249c
teenhe <teenhe@tencent.com>
2019-06-23 16:08:02 +0800

    fix eslint

--- types/splitpanes/index.d.ts
+++ types/splitpanes/index.d.ts
@@ -3,84 +3,84 @@
 // Definitions by: teenhe <https://github.com/noonhorse>
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
 // TypeScript Version: 3.5
-import { VueConstructor } from 'vue'
+import { VueConstructor } from 'vue';
 
 export interface positionTypes {
-  x: number
-  y: number
+  x: number;
+  y: number;
 }
 export interface containerTypes {
-  vnode: any
-  offsetLeft: null | number
-  offsetTop: null | number
+  vnode: any;
+  offsetLeft: null | number;
+  offsetTop: null | number;
 }
 export interface touchTypes {
-  mouseDown: boolean
-  dragging: boolean
-  activeSplitter: null | number
+  mouseDown: boolean;
+  dragging: boolean;
+  activeSplitter: null | number;
 }
 
 export interface splitterTapsTypes {
-  splitter: null | number
-  timeoutId: null | object
+  splitter: null | number;
+  timeoutId: null | object;
 }
 
 export interface splitpanesProps {
-  watchSlots: boolean // 是否监控插槽 false
-  horizontal: boolean // 是否水平 false
-  pushOtherPanes: boolean // 支持添加 true
-  dblClickSplitter: boolean // 双击分离 true
+  watchSlots: boolean; // 是否监控插槽 false
+  horizontal: boolean; // 是否水平 false
+  pushOtherPanes: boolean; // 支持添加 true
+  dblClickSplitter: boolean; // 双击分离 true
 }
 
 export interface splitpanesData {
-  container: containerTypes
-  slotsCount: number
-  vnodes: any[]
-  panes: any[]
-  splitters: any[]
-  touch: touchTypes
+  container: containerTypes;
+  slotsCount: number;
+  vnodes: any[];
+  panes: any[];
+  splitters: any[];
+  touch: touchTypes;
   // Detect double click on touch devices.
-  splitterTaps: splitterTapsTypes
-  slotsCopy: string
+  splitterTaps: splitterTapsTypes;
+  slotsCopy: string;
 }
 
 export interface splitpanesWatch {
-  defaultPercent: (val: any) => void
+  defaultPercent: (val: any) => void;
 }
 
 export interface splitpanesMethods {
-  bindEvents: () => void
-  onMouseDown: (e: Event, splitterIndex: number) => void
-  onMouseMove: (e: Event) => void
-  onMouseUp: () => void
-  onSplitterClick: (e: Event, splitterIndex: number) => void
-  onSplitterDblClick: (e: Event, splitterIndex: number) => void
-  getCurrentMouseDrag: (e: Event) => positionTypes
-  getCurrentDragPercentage: (drag: object) => number
-  calculatePanesSize: (drag: object) => boolean | void
-  doPushOtherPanes: (sums: object, dragPercentage: number) => any
-  sumPrevPanesSize: (splitterIndex: number) => object
-  sumNextPanesSize: (splitterIndex: number) => object
-  findPrevExpandedPane: (splitterIndex: number) => object
-  findNextExpandedPane: (splitterIndex: number) => object
+  bindEvents: () => void;
+  onMouseDown: (e: Event, splitterIndex: number) => void;
+  onMouseMove: (e: Event) => void;
+  onMouseUp: () => void;
+  onSplitterClick: (e: Event, splitterIndex: number) => void;
+  onSplitterDblClick: (e: Event, splitterIndex: number) => void;
+  getCurrentMouseDrag: (e: Event) => positionTypes;
+  getCurrentDragPercentage: (drag: object) => number;
+  calculatePanesSize: (drag: object) => boolean | void;
+  doPushOtherPanes: (sums: object, dragPercentage: number) => any;
+  sumPrevPanesSize: (splitterIndex: number) => object;
+  sumNextPanesSize: (splitterIndex: number) => object;
+  findPrevExpandedPane: (splitterIndex: number) => object;
+  findNextExpandedPane: (splitterIndex: number) => object;
 }
 
 export interface splitpanesComputed {
-  defaultWidth: () => number
+  defaultWidth: () => number;
 }
 
 export interface splitpanesConstructor extends VueConstructor {
-  props: splitpanesProps
-  data: () => splitpanesData
-  watch: splitpanesWatch
-  methods: splitpanesMethods
-  computed: splitpanesComputed
-  mounted: () => void
-  beforeDestroy: () => void
-  beforeUpdate: () => void
-  render: (createEl: any) => any
+  props: splitpanesProps;
+  data: () => splitpanesData;
+  watch: splitpanesWatch;
+  methods: splitpanesMethods;
+  computed: splitpanesComputed;
+  mounted: () => void;
+  beforeDestroy: () => void;
+  beforeUpdate: () => void;
+  render: (createEl: any) => any;
 }
 
-export const splitpanes: splitpanesConstructor
+export const splitpanes: splitpanesConstructor;
 
-export default splitpanes
+export default splitpanes;
