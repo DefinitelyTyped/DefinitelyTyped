84ee07cf2b9fc5c37ccf741e832a11be518c71c5
Book Moons <35854232+bookmoons@users.noreply.github.com>
2019-06-23 16:24:07 -0400

    k6: Document branding

--- types/k6/index.d.ts
+++ types/k6/index.d.ts
@@ -5,6 +5,19 @@
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
 // TypeScript Version: 3.4
 
+/*
+ * Uses a branding pattern throughout to reflect the custom classes exposed by
+ * the k6 runtime. These classes have internal state not visible in JavaScript.
+ * The object passed back to k6 must be an actual instance created on the
+ * golang side. Duck typed objects are not safe.
+ *
+ * Each class includes a private property __brand. The brand breaks duck typing
+ * for noninstances with an identical public shape.
+ *
+ * This follows the pattern used in the TypeScript compiler:
+ * https://github.com/Microsoft/TypeScript/blob/7b48a182c05ea4dea81bab73ecbbe9e013a79e99/src/compiler/types.ts#L693-L698
+ */
+
 export function check<T>(val: T, sets: Checkers<T>, tags?: object): boolean;
 export function fail(err?: string): never;
 export function group<T>(name: string, fn: () => T): T;
