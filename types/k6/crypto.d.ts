d135ec42ed2311cce8cbe84e32075c43562398f0
Book Moons <35854232+bookmoons@users.noreply.github.com>
2019-06-22 21:28:55 -0400

    k6/crypto: Support all string encodings

--- types/k6/crypto.d.ts
+++ types/k6/crypto.d.ts
@@ -1,19 +1,19 @@
-export type OutputEncoding = 'hex' | 'base64';
+export type StringEncoding = 'hex' | 'base64' | 'base64url' | 'base64rawurl';
 
-export function hmac(algorithm: string, secret: string, data: string, outputEncoding: OutputEncoding): string;
-export function md4(input: string, outputEncoding: OutputEncoding): string;
-export function md5(input: string, outputEncoding: OutputEncoding): string;
-export function sha1(input: string, outputEncoding: OutputEncoding): string;
-export function sha256(input: string, outputEncoding: OutputEncoding): string;
-export function sha384(input: string, outputEncoding: OutputEncoding): string;
-export function sha512(input: string, outputEncoding: OutputEncoding): string;
-export function sha512_224(input: string, outputEncoding: OutputEncoding): string;
-export function sha512_256(input: string, outputEncoding: OutputEncoding): string;
-export function ripemd160(input: string, outputEncoding: OutputEncoding): string;
+export function hmac(algorithm: string, secret: string, data: string, outputEncoding: StringEncoding): string;
+export function md4(input: string, outputEncoding: StringEncoding): string;
+export function md5(input: string, outputEncoding: StringEncoding): string;
+export function sha1(input: string, outputEncoding: StringEncoding): string;
+export function sha256(input: string, outputEncoding: StringEncoding): string;
+export function sha384(input: string, outputEncoding: StringEncoding): string;
+export function sha512(input: string, outputEncoding: StringEncoding): string;
+export function sha512_224(input: string, outputEncoding: StringEncoding): string;
+export function sha512_256(input: string, outputEncoding: StringEncoding): string;
+export function ripemd160(input: string, outputEncoding: StringEncoding): string;
 export function createHash(algorithm: string): Hasher;
 export function createHMAC(algorithm: string, secret: string): Hasher;
 
 export interface Hasher {
     update: (input: string) => void;
-    digest: (outputEncoding: OutputEncoding) => string;
+    digest: (outputEncoding: StringEncoding) => string;
 }
