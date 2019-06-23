bca27b3eee9712b60336568818117d7b2a042287
Book Moons <35854232+bookmoons@users.noreply.github.com>
2019-06-23 15:37:40 -0400

    k6/encoding: Order types to bottom

--- types/k6/encoding.d.ts
+++ types/k6/encoding.d.ts
@@ -1,4 +1,4 @@
-export type Base64Variant = 'std' | 'rawstd' | 'url' | 'rawurl';
-
 export function b64decode(input: string, encoding?: Base64Variant): string;
 export function b64encode(input: string, encoding?: Base64Variant): string;
+
+export type Base64Variant = 'std' | 'rawstd' | 'url' | 'rawurl';
