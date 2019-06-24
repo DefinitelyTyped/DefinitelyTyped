f6ef22330194caa059fa5ecb0df2fbe69bf128e6
Daniel Rosenwasser <DanielRosenwasser@users.noreply.github.com>
2019-06-24 12:54:58 -0700

    Align

--- types/baseui/index.d.ts
+++ types/baseui/index.d.ts
@@ -1,11 +1,11 @@
 // Type definitions for baseui 7.3
 // Project: https://github.com/uber-web/baseui#readme
 // Definitions by: Jhey Tompkins <https://github.com/jh3y>
-//                Gergely Nemeth <https://github.com/gergelyke>
-//                Chase Starr <https://github.com/chasestarr>
-//                Nadiia <https://github.com/nadiia>
-//                Vojtech Miksu <https://github.com/tajo>
-//                Graham Murdoch <https://github.com/sandgraham>
+//                 Gergely Nemeth <https://github.com/gergelyke>
+//                 Chase Starr <https://github.com/chasestarr>
+//                 Nadiia <https://github.com/nadiia>
+//                 Vojtech Miksu <https://github.com/tajo>
+//                 Graham Murdoch <https://github.com/sandgraham>
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
 // TypeScript Version: 3.1
 /// <reference path="theme.d.ts"/>
