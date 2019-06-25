da33835f5249b15b415997dfc1335df822fb28c7
Maël Nison <nison.mael@gmail.com>
2019-06-25 18:06:27 +0200

    More linting;

--- types/pnpapi/index.d.ts
+++ types/pnpapi/index.d.ts
@@ -2,25 +2,26 @@
 // Project: https://yarnpkg.github.io/berry/advanced/pnpapi
 // Definitions by: Maël Nison <https://github.com/arcanis>
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
+// TypeScript Version: 2.1
 
 export interface PhysicalPackageLocator {
-    name: string,
-    reference: string,
-};
+    name: string;
+    reference: string;
+}
 
 export interface TopLevelPackageLocator {
-    name: null,
-    reference: null,
-};
+    name: null;
+    reference: null;
+}
 
 export type PackageLocator =
     | PhysicalPackageLocator
     | TopLevelPackageLocator;
 
 export interface PackageInformation {
-    packageLocation: string,
-    packageDependencies: Map<string, string | [string, string]>,
-};
+    packageLocation: string;
+    packageDependencies: Map<string, string | [string, string]>;
+}
 
 export const VERSIONS: {std: number, [key: string]: number};
 
@@ -30,7 +31,11 @@ export function getPackageInformation(locator: PackageLocator): PackageInformati
 export function findPackageLocator(location: string): PackageLocator | null;
 
 export function resolveToUnqualified(request: string, issuer: string | null, opts?: {considerBuiltins?: boolean}): string | null;
-export function resolveUnqualified(unqualified: string, opts?: {extensions?: string[]}): string;
-export function resolveRequest(request: string, issuer: string | null, opts?: {considerBuiltins?: boolean, extensions?: string[]}): string | null;
+export function resolveUnqualified(unqualified: string, opts?: { extensions?: string[] }): string;
+export function resolveRequest(
+    request: string,
+    issuer: string | null,
+    opts?: { considerBuiltins?: boolean; extensions?: string[] }
+): string | null;
 
 export function setup(): void;
