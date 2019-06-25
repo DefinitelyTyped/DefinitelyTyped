46ae3058079c4cb32156aaf8000bfcc55c91e697
jituanlin <linjituan@guorou.tv>
2019-06-25 11:16:26 +0800

    add test for `chain`

--- types/ramda/ramda-tests.ts
+++ types/ramda/ramda-tests.ts
@@ -723,6 +723,11 @@ R.times(i, 5);
 
     R.chain(duplicate, [1, 2, 3]); // => [1, 1, 2, 2, 3, 3]
     R.chain(duplicate)([1, 2, 3]); // => [1, 1, 2, 2, 3, 3]
+    const result1: number[] = R.chain<number, number[], number[]>(R.append, R.head)([
+        1,
+        2,
+        3
+    ]); // => [1, 2, 3, 1]
 };
 
 () => {
