0778f06293d0682b0e9e1a00787bcedc7364aa2c
uhyo <uhyo@uhy.ooo>
2019-06-25 17:11:19 +0900

    same default export trick for classnames/bind

--- types/classnames/bind.d.ts
+++ types/classnames/bind.d.ts
@@ -3,6 +3,9 @@ import { ClassNamesFn } from './types';
 interface ClassNamesBind extends ClassNamesFn {
     bind(styles: Record<string, string>): ClassNamesFn;
 }
-declare const classNamesBind: ClassNamesBind;
+type ClassNamesBindExport = ClassNamesBind & {
+    default: ClassNamesBind;
+};
+declare const classNamesBind: ClassNamesBindExport;
 
 export = classNamesBind;
