57b524e6f14b1fbd403d8c044c8a39760ff99c7e
Weyert de Boer <weyert.deboer@investec.co.uk>
2019-06-25 14:48:26 +0100

    just make changes to testing-library__user-event

--- types/testing-library__user-event/index.d.ts
+++ types/testing-library__user-event/index.d.ts
@@ -1,4 +1,4 @@
-// Type definitions for @testing-library/user-event 4.0
+// Type definitions for @testing-library/user-event 4.1.0
 // Project: https://github.com/testing-library/user-event
 // Definitions by: Wu Haotian <https://github.com/whtsky>
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
@@ -8,14 +8,12 @@ export interface IUserOptions {
     delay?: number;
 }
 
-type TargetElement = Element | Window;
-
 declare const userEvent: {
-    click: (element: TargetElement) => void;
-    dblClick: (element: TargetElement) => void;
-    selectOptions: (element: TargetElement, values: string | string[]) => void;
+    click: (element: Element | Window) => void;
+    dblClick: (element: Element | Window) => void;
+    selectOptions: (element: Element | Window, values: string | string[]) => void;
     type: (
-        element: TargetElement,
+        element: Element | Window,
         text: string,
         userOpts?: IUserOptions
     ) => Promise<void>;
