7cf6ed21c2414d059d066633f92e882df974f361
Book Moons <35854232+bookmoons@users.noreply.github.com>
2019-06-24 23:46:10 -0400

    k6/html: Specify optional return types

--- types/k6/html.d.ts
+++ types/k6/html.d.ts
@@ -1,5 +1,58 @@
 export function parseHTML(html: string): Selection;
 
+// Selection
 export abstract class Selection {
+    protected __brand: never;
+    attr(name: string): string | undefined;
+    children(selector?: string): Selection;
+    closest(selector: string): Selection;
+    contents(): Selection;
+    data(key?: string): string | undefined;
+    each(handler: Handler): void;
+    eq(index: number): Selection;
+    filter(selector: string | Tester | Selection): Selection;
+    find(selector: string): Selection;
+    first(): Selection;
+    get(index: number): Element;
+    has(selector: string): Selection;
+    html(): string | undefined;
+    is(selector: string | Tester | Selection): Selection;
+    last(): Selection;
+    map(mapper: Mapper): unknown[];
+    next(selector?: string): Selection;
+    nextAll(selector?: string): Selection;
+    nextUntil(selector?: string): Selection;
+    not(selector: string | Tester): Selection;
+    parent(selector?: string): Selection;
+    parents(selector?: string): Selection;
+    parentsUntil(selector?: string): Selection;
+    prev(selector?: string): Selection;
+    prevAll(selector?: string): Selection;
+    prevUntil(selector?: string): Selection;
+    serialize(): string;
+    serializeArray(): FormValue[];
+    serializeObject(): { [name: string]: string };
+    size(): number;
+    slice(start: number, end?: number): Selection;
+    text(): string;
+    toArray(): Selection[];
+    val(): string | undefined;
+}
+export interface FormValue {
+    name: string;
+    value: string;
+}
+export interface Tester {
+    (index: number, element: Element): boolean;
+}
+export interface Handler {
+    (index: number, element: Element): void;
+}
+export interface Mapper {
+    (index: number, element: Element): unknown;
+}
+
+// Element
+export abstract class Element {
     protected __brand: never;
 }
--- types/k6/test/html.ts
+++ types/k6/test/html.ts
@@ -1,9 +1,60 @@
-import { Selection, parseHTML } from 'k6/html';
+import { Element, FormValue, Selection, parseHTML } from 'k6/html';
+
+const handler = (index: number, element: Element) => {};
+const tester = (index: number, element: Element) => true;
+const mapper = (index: number, element: Element) => null;
 
 let selection: Selection;
+let derived: Selection;
+let selections: Selection[];
+let element: Element;
+let formValues: FormValue[];
 
 // parseHTML
 parseHTML(); // $ExpectError
 parseHTML(5); // $ExpectError
 selection = parseHTML('<html></html>');
 parseHTML('<html></html>', 5); // $ExpectError
+
+// Selection
+selection = parseHTML('<html></html>');
+selection.attr('name'); // $ExpectType string | undefined
+derived = selection.children('.item');
+derived = selection.closest('li');
+derived = selection.contents();
+selection.data('email'); // $ExpectType string | undefined
+selection.each(handler); // $ExpectType void
+derived = selection.eq(7);
+derived = selection.filter('.item');
+derived = selection.filter(tester);
+derived = selection.filter(selection);
+derived = selection.find('.item');
+derived = selection.first();
+element = selection.get(7);
+derived = selection.has('.item');
+selection.html(); // $ExpectType string | undefined
+derived = selection.is('.item');
+derived = selection.is(tester);
+derived = selection.is(selection);
+derived = selection.last();
+selection.map(mapper); // $ExpectType unknown[]
+derived = selection.next('span');
+derived = selection.nextAll('span');
+derived = selection.nextUntil('span');
+derived = selection.not('.item');
+derived = selection.not(tester);
+derived = selection.parent('.item');
+derived = selection.parents('.item');
+derived = selection.parentsUntil('.item');
+derived = selection.prev('.item');
+derived = selection.prevAll('.item');
+derived = selection.prevUntil('.item');
+selection.serialize(); // $ExpectType string
+formValues = selection.serializeArray();
+selection.serializeObject(); // $ExpectType { [name: string]: string; }
+selection.size(); // $ExpectType number
+derived = selection.slice(0);
+derived = selection.slice(7, 9);
+selection.text(); // $ExpectType string
+selections = selection.toArray();
+selection.val(); // $ExpectType string | undefined
