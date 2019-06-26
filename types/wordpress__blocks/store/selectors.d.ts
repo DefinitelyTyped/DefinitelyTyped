81d2311797dca4270294e3944db1cf937591164d
Derek Sifford <dereksifford@gmail.com>
2019-06-26 14:16:33 -0400

    [@wordpress/blocks] add new definitions (#36420)
    * [@wordpress/blocks] add new definitions
    
    * forgot to uncomment file includes
    
    * "stores" directory should be called "store"

---
+++ types/wordpress__blocks/store/selectors.d.ts
@@ -0,0 +1,34 @@
+import { Block, BlockStyle } from '../';
+
+export {
+    getBlockSupport,
+    getBlockType,
+    getBlockTypes,
+    getCategories,
+    getChildBlockNames,
+    getDefaultBlockName,
+    hasBlockSupport,
+    hasChildBlocks,
+    hasChildBlocksWithInserterSupport,
+} from '../';
+
+/**
+ * Returns block styles by block name.
+ */
+export function getBlockStyles(blockName: string): readonly BlockStyle[] | undefined;
+
+/**
+ * Returns the name of the block for handling non-block content.
+ */
+export function getFreeformFallbackBlockName(): string | undefined;
+
+/**
+ * Returns the name of the block for handling unregistered blocks.
+ */
+export function getUnregisteredFallbackBlockName(): string | undefined;
+
+/**
+ * Returns true if the block type by the given name or object value matches a
+ * search term, or false otherwise.
+ */
+export function isMatchingSearchTerm(nameOrType: string | Block<any>, searchTerm: string): boolean;
