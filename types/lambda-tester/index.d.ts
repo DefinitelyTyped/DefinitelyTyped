d5974d578e7d19e2e69b5f89202f447d20a4ffc2
Suntharesan Mohan <suntharesan.mohan@gmail.com>
2019-06-26 22:09:41 -0400

    Apply typing based on `aws-lambda` handler type

--- types/lambda-tester/index.d.ts
+++ types/lambda-tester/index.d.ts
@@ -1,34 +1,45 @@
-// Type definitions for lambda-tester 3.5
+// Type definitions for lambda-tester 3.6
 // Project: https://github.com/vandium-io/lambda-tester#readme
 // Definitions by: Ivan Kerin <https://github.com/ivank>
 //                 Hajo Aho-Mantila <https://github.com/HajoAhoMantila>
+//                 Suntharesan Mohan <https://github.com/msuntharesan>
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
-// TypeScript Version: 2.3
+// TypeScript Version: 3.1
 
-import { Context, ClientContext, CognitoIdentity, Handler } from "aws-lambda";
+/* tslint:disable:no-unnecessary-generics */
+
+import { Context, ClientContext, CognitoIdentity, Handler, Callback } from 'aws-lambda';
 
 declare namespace lambdaTester {
-    type Verifier = (result: any) => void;
+    type HandlerEvent<T extends Handler> = T extends Handler<infer TEvent> ? TEvent : never;
+    type HandlerResult<T extends Handler> = T extends Handler<any, infer TResult> ? TResult : never;
+    type HandlerError<T extends Handler> = T extends Handler<any, infer TResult>
+        ? NonNullable<Parameters<Callback<TResult>>['0']>
+        : never;
 
-    class LambdaTester {
-        event(event: any): this;
+    interface Verifier<S> {
+        (result: S, additional?: any): void | Promise<void>;
+        (result: S, additional?: any, done?: () => {}): void;
+    }
+
+    // type Verifier<T> = (result: T, additional?: any, done?: () => {}) => void | Promise<void>;
+
+    class LambdaTester<T extends Handler> {
+        event(event: HandlerEvent<T>): this;
         context(context: Context): this;
         clientContext(clientContext: ClientContext): this;
-        identity(
-            cognitoIdentityId: string,
-            cognitoIdentityPoolId: string
-        ): this;
+        identity(cognitoIdentityId: string, cognitoIdentityPoolId: string): this;
         timeout(seconds: number): this;
         xray(): this;
-        expectSucceed(verifier: Verifier): Promise<any>;
-        expectFail(verifier: Verifier): Promise<any>;
-        expectError(verifier: Verifier): Promise<any>;
-        expectResult(verifier: Verifier): Promise<any>;
-        expectReject(verifier: Verifier): Promise<any>;
-        expectResolve(verifier: Verifier): Promise<any>;
+        expectSucceed<S extends HandlerResult<T> = HandlerResult<T>>(verifier: Verifier<S>): Promise<any>;
+        expectFail<S = HandlerError<T>>(verifier: Verifier<S>): Promise<any>;
+        expectResult<S extends HandlerResult<T> = HandlerResult<T>>(verifier: Verifier<S>): Promise<any>;
+        expectError<S = HandlerError<T>>(verifier: Verifier<S>): Promise<any>;
+        expectResolve<S extends HandlerResult<T> = HandlerResult<T>>(verifier: Verifier<S>): Promise<any>;
+        expectReject<S = HandlerError<T>>(verifier: Verifier<S>): Promise<any>;
     }
 }
 
-declare function lambdaTester(handler: Handler): lambdaTester.LambdaTester;
+declare function lambdaTester<T extends Handler = Handler>(handler: T): lambdaTester.LambdaTester<T>;
 
 export = lambdaTester;
