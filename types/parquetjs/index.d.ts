68cf5f1b2a2736a7aecb415cba1d5ff2afdd689a
jon-fearer <fearer.jon@gmail.com>
2019-06-28 22:21:23 -0600

    remove unnecessary namesplace declaration

--- types/parquetjs/index.d.ts
+++ types/parquetjs/index.d.ts
@@ -8,5 +8,3 @@ export { ParquetEnvelopeReader, ParquetReader } from './lib/reader';
 export { ParquetEnvelopeWriter, ParquetWriter, ParquetTransformer } from './lib/writer';
 export { ParquetSchema } from './lib/schema';
 export { shredder as ParquetShredder } from './lib/shred';
-
-declare namespace parquet { }
