a5e719da13950005cb187f7b4af6bbf20ef7edb0
Oscar Busk <oscar.busk@gmail.com>
2019-07-01 17:46:11 +0200

    Add typescript version to @types/load-google-maps-api

--- types/load-google-maps-api/index.d.ts
+++ types/load-google-maps-api/index.d.ts
@@ -2,6 +2,7 @@
 // Project: https://github.com/yuanqing/load-google-maps-api#readme
 // Definitions by: Oscar Busk <https://github.com/oBusk>
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
+// TypeScript Version: 2.7
 
 /// <reference types="googlemaps" />
 
