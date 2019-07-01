10b9f9000d3af01a7c122113bf6425811f63a045
James Whitmarsh <jwhitmarsh@dresources.com>
2019-07-01 11:40:13 +0100

    Update knex version to bring in line with bookshelf 0.15.1

--- types/bookshelf/index.d.ts
+++ types/bookshelf/index.d.ts
@@ -1,4 +1,4 @@
-// Type definitions for bookshelfjs v0.13.0
+// Type definitions for bookshelfjs v0.15.1
 // Project: http://bookshelfjs.org/
 // Definitions by: Andrew Schurman <https://github.com/arcticwaters>, Vesa Poikajärvi <https://github.com/vesse>
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
