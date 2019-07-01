ea69da07defda55621d246fc7ff70835c08c2393
Aurelijus Rožėnas <aurelijus.rozenas@gmail.com>
2019-07-01 07:05:23 +0300

    Updated bazinga-translator to accept null for some parameters.

--- types/bazinga-translator/bazinga-translator-tests.ts
+++ types/bazinga-translator/bazinga-translator-tests.ts
@@ -1,10 +1,12 @@
 Translator.fallback = 'en';
 Translator.defaultDomain = 'messages';
 
-Translator.add("test", "it work", "frontend", "en");
+Translator.add('test', "it work's", 'frontend', 'en');
+Translator.add('test', 'message with default domain and local');
 
 Translator.trans('key', {}, 'frontend');
 Translator.trans('key', {"foo": "bar"}, 'DOMAIN_NAME');
+Translator.trans('key');
 
 Translator.transChoice('key', 1, {}, 'frontend');
-Translator.transChoice('key', 123, {"foo": "bar"}, 'DOMAIN_NAME');
+Translator.transChoice('key', 123, { foo: 'bar' }, 'DOMAIN_NAME');
