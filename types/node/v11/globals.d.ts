2ebd088b1f72ae737f887ceb0ed5c98f0fe265d4
Martin Trobäck <martintk@axis.com>
2019-07-02 13:24:39 +0200

    Update types/node/v11
    * Fixes documentation for `concat`
    * Adds `Buffer | Uint8Array` union to `concat`,
      `compare`, `equals` and `copy`
    
    Solves #36253.
    
    Change-Id: I4dcb4b497cf48668e0f2549749c841a95ea0ad84

--- types/node/v11/globals.d.ts
+++ types/node/v11/globals.d.ts
@@ -241,9 +241,10 @@ interface Buffer extends Uint8Array {
     write(string: string, offset: number, length: number, encoding?: BufferEncoding): number;
     toString(encoding?: string, start?: number, end?: number): string;
     toJSON(): { type: 'Buffer', data: number[] };
-    equals(otherBuffer: Uint8Array): boolean;
-    compare(otherBuffer: Uint8Array, targetStart?: number, targetEnd?: number, sourceStart?: number, sourceEnd?: number): number;
-    copy(targetBuffer: Uint8Array, targetStart?: number, sourceStart?: number, sourceEnd?: number): number;
+    equals(otherBuffer: Buffer | Uint8Array): boolean;
+    compare(otherBuffer: Buffer | Uint8Array, targetStart?: number, targetEnd?: number, sourceStart?: number, sourceEnd?: number): number;
+    concat(list: Buffer[] | Uint8Array[], totalLength?: number): Buffer;
+    copy(targetBuffer: Buffer | Uint8Array, targetStart?: number, sourceStart?: number, sourceEnd?: number): number;
     slice(start?: number, end?: number): Buffer;
     subarray(begin: number, end?: number): Buffer;
     writeUIntLE(value: number, offset: number, byteLength: number): number;
@@ -398,18 +399,17 @@ declare const Buffer: {
      * Returns a buffer which is the result of concatenating all the buffers in the list together.
      *
      * If the list has no items, or if the totalLength is 0, then it returns a zero-length buffer.
-     * If the list has exactly one item, then the first item of the list is returned.
-     * If the list has more than one item, then a new Buffer is created.
+     * If the list has one or more items, then a new Buffer is created.
      *
      * @param list An array of Buffer objects to concatenate
      * @param totalLength Total length of the buffers when concatenated.
      *   If totalLength is not provided, it is read from the buffers in the list. However, this adds an additional loop to the function, so it is faster to provide the length explicitly.
      */
-    concat(list: Uint8Array[], totalLength?: number): Buffer;
+    concat(list: Buffer[] | Uint8Array[], totalLength?: number): Buffer;
     /**
      * The same as buf1.compare(buf2).
      */
-    compare(buf1: Uint8Array, buf2: Uint8Array): number;
+    compare(buf1: Buffer | Uint8Array, buf2: Buffer | Uint8Array): number;
     /**
      * Allocates a new buffer of {size} octets.
      *
