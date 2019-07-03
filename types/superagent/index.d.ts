94300fed8404c3a729c084c879c322fbdd3c0cb5
Pieter Van der Haegen <pieter@datacamp.com>
2019-07-03 18:39:25 +0200

    Include "dom" via triple-slash reference in @types/superagent: Fixes #12044 (#36282)
    * Include "dom" via /// reference in @types/superagent: Fixes #12044
    
    * Update TS version of promisify-supertest
    
    * Update TS version of simple-cw-node
    
    * Update TS version of superagent-bunyan
    
    * Update TS version of superagent-no-cache
    
    * Update TS version of superagent-prefix
    
    * Update TS version of supertest

--- types/superagent/index.d.ts
+++ types/superagent/index.d.ts
@@ -8,10 +8,12 @@
 //                 Adam Haglund <https://github.com/beeequeue>
 //                 Lukas Elmer <https://github.com/lukaselmer>
 //                 Jesse Rogers <https://github.com/theQuazz>
+//                 Chris Arnesen <https://github.com/carnesen>
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
-// TypeScript Version: 2.2
+// TypeScript Version: 3.0
 
 /// <reference types="node" />
+/// <reference lib="dom" />
 
 import * as fs from 'fs';
 import * as https from 'https';
