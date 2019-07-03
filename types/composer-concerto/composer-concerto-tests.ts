75bf358e43b0a9713be9230cec374a15f226e42f
Giacomo Minighin <jamesbomb@localhost.localdomain>
2019-07-03 10:49:47 +0200

    composer-concerto tests file

--- types/composer-concerto/composer-concerto-tests.ts
+++ types/composer-concerto/composer-concerto-tests.ts
@@ -1,22 +1,21 @@
-import {
-  ModelManager, Factory, Serializer
-} from 'composer-concerto';
+import { ModelManager, Factory, Serializer } from 'composer-concerto';
 
 const modelManager = new ModelManager();
 modelManager.addModelFile(
-  `namespace org.acme.address
-
-  /**
-   * This is a concept
-   */
-  concept PostalAddress {
+    `namespace org.acme.address
+/**
+ * This is a concept
+ */
+concept PostalAddress {
     o String streetAddress optional
     o String postalCode optional
     o String postOfficeBoxNumber optional
     o String addressRegion optional
     o String addressLocality optional
     o String addressCountry optional
-  }`, 'filename.cto');
+}`,
+    'filename.cto'
+);
 
 const factory = new Factory(modelManager);
 const postalAddress = factory.newConcept('org.acme.address', 'PostalAddress');
@@ -26,3 +25,4 @@ const serializer = new Serializer(factory, modelManager);
 const plainJsObject = serializer.toJSON(postalAddress);
 
 const sPostalAddress = serializer.fromJSON(plainJsObject);
+let streetAddress: string = sPostalAddress.streetAddress;
