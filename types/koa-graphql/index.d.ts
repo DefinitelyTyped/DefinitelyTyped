2ef9631057b157e8931e2acec4a1215dc6fbb5ba
PlayMa256 <matheus.g.silva@hotmail.com>
2019-07-03 09:28:25 -0300

    fix ts version

--- types/koa-graphql/index.d.ts
+++ types/koa-graphql/index.d.ts
@@ -2,7 +2,7 @@
 // Project: https://github.com/chentsulin/koa-graphql
 // Definitions by: Matheus Gonçalves da Silva <https://github.com/PlayMa256>
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
-// TypeScript Version: 2.3
+// TypeScript Version: 2.6
 /// <reference types="node" />
 
 import { Context, Request, Response, Middleware } from "koa";
