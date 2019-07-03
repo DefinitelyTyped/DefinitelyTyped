aa5f1d5750d7a1b7bc5467e4b1cd87a92ecbd76a
Ihor Banadiga <banadiga@gmail.com>
2019-07-03 16:45:27 +0300

    change after code review

--- types/sumologic/index.d.ts
+++ types/sumologic/index.d.ts
@@ -4,8 +4,6 @@
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
 // TypeScript Version: 2.2
 
-declare namespace SumoLogger {}
-
 declare class SumoLogger {
     constructor(collectorCode: string, opts?: object);
 
