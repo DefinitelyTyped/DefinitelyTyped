1e568ca2b12cbe23f4374c3aeb308f48b26850a9
Luke Rielley <luke.rielley@gmail.com>
2019-07-04 20:24:52 +1000

    Updated contributors list for falcor-http-datasource

--- types/falcor-http-datasource/index.d.ts
+++ types/falcor-http-datasource/index.d.ts
@@ -1,6 +1,6 @@
 // Type definitions for falcor-http-datasource 0.1.3
 // Project: https://github.com/Netflix/falcor-http-datasource
-// Definitions by: Quramy <https://github.com/Quramy>
+// Definitions by: Quramy <https://github.com/Quramy>, LukeRielley <https://github.com/lukerielley>
 // Definitions: https://github.com/borisyankov/DefinitelyTyped
 // TypeScript Version: 2.4
 
