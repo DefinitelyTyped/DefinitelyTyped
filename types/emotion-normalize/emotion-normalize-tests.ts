ff1f70343b22d52c21d4709311462e66edce9c8d
Krzysztof Żuraw <krzysztofzuraw@fastmail.com>
2019-07-06 17:22:25 +0200

    add emotion-normalize types

---
+++ types/emotion-normalize/emotion-normalize-tests.ts
@@ -0,0 +1,1 @@
+import emotionNormalize from 'emotion-normalize';
---
+++ types/emotion-normalize/index.d.ts
@@ -0,0 +1,7 @@
+// Type definitions for emotion-normalize 10.0
+// Project: https://github.com/infinum/emotion-normalize#readme
+// Definitions by: Krzysztof Żuraw <https://github.com/krzysztofzuraw>
+// Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
+
+type emotionNormalize = TemplateStringsArray;
+export default emotionNormalize;
