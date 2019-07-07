9726f311cfa4e26fb759df22fd9fc822cc055f63
Manuel Thalmann <m@nuth.ch>
2019-07-07 15:07:07 +0200

    Add the password-prompt

--- types/inquirer/lib/prompts/number.d.ts
+++ types/inquirer/lib/prompts/number.d.ts
@@ -1,5 +1,6 @@
 import InputPrompt = require("./input");
 import inquirer = require("../..");
+import { Interface as ReadlineInterface } from "readline";
 
 /**
  * The question for the `NumberPrompt<T>`.
@@ -9,6 +10,20 @@ type Question = inquirer.poll.NumberQuestionOptions<inquirer.poll.Answers>;
 /**
  * Provides a prompt which allows the user to type a number as answer.
  */
-declare class NumberPrompt<TQuestion extends Question = Question> extends InputPrompt<TQuestion> { }
+declare class NumberPrompt<TQuestion extends Question = Question> extends InputPrompt<TQuestion> {
+    /**
+     * Initializes a new instance of the `NumberPrompt<T>` class.
+     *
+     * @param question
+     * The question to prompt the user to answer.
+     *
+     * @param readLine
+     * An object for performing read from and write to the console.
+     *
+     * @param answers
+     * The answer-object.
+     */
+    public constructor(question: TQuestion, readLine: ReadlineInterface, answers: inquirer.poll.Answers);
+}
 
 export = NumberPrompt;
