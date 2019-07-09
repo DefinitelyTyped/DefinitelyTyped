ef3b21029c368c2a1c4acee8a178190192652d2d
AdrianFleming <48060111+AdrianFleming@users.noreply.github.com>
2019-07-09 13:18:24 +0100

    TypeScript definitions for react-transition-group's new SwitchTransition component

---
+++ types/react-transition-group/SwitchTransition.d.ts
@@ -0,0 +1,12 @@
+﻿import { Component } from "react";
+import { TransitionProps } from "react-transition-group/Transition";
+
+declare namespace SwitchTransition {
+    interface SwitchTransitionProps extends TransitionProps {
+        mode?: 'out-in' | 'in-out';
+    }
+}
+
+declare class SwitchTransition extends Component<SwitchTransition.SwitchTransitionProps> { }
+
+export = SwitchTransition;
