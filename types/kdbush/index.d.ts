57e4d9c67f6c3118c36c05e6c84044a3479576a1
Jamie Winder <Jamiewinder@gmail.com>
2019-07-09 10:34:08 +0100

    update types

--- types/kdbush/index.d.ts
+++ types/kdbush/index.d.ts
@@ -1,35 +1,28 @@
-// Type definitions for kdbush 1.0
+// Type definitions for kdbush 3.0.0
 // Project: https://github.com/mourner/kdbush
 // Definitions by: DenisCarriere <https://github.com/DenisCarriere>
 //                 Christian Scott <https://github.com/chrfrasco>
+//                 Jamie Winder <https://github.com/jamiewinder>
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
 
-type Points = number[][];
 type Get<T> = (point: T) => number;
-type ArrayType =
-  typeof Int8Array
-  | typeof Int16Array
-  | typeof Int32Array
-  | typeof Float32Array
-  | typeof Float64Array
-  | typeof Array;
 
-declare function kdbush(points: Points): kdbush.KDBush<Points>;
-declare function kdbush<T>(
-  points: T[],
-  getX: Get<T>,
-  getY: Get<T>,
-  nodeSize?: number,
-  ArrayType?: ArrayType
-): kdbush.KDBush<T>;
-declare namespace kdbush {
-  class KDBush<T> {
-    ids: number[];
-    coords: number[];
-    nodeSize: number;
-    points: T[];
-    range(minX: number, minY: number, maxX: number, maxY: number): number[];
-    within(x: number, y: number, r: number): number[];
-  }
+type ArrayType =
+    | typeof Int8Array
+    | typeof Int16Array
+    | typeof Int32Array
+    | typeof Float32Array
+    | typeof Float64Array
+    | typeof Array;
+
+declare class KDBush<T = readonly [number, number]> {
+    public constructor(points: Array<T>, getX?: Get<T>, getY?: Get<T>, arrayType?: ArrayType);
+
+    public readonly nodeSize: number;
+    public readonly points: ReadonlyArray<T>;
+
+    public range(minX: number, minY: number, maxX: number, maxY: number): Array<number>;
+    public within(x: number, y: number, r: number): Array<number>;
 }
-export = kdbush;
+
+export = KDBush;
