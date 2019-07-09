cb218e7bc2b6dd95259ba9a53f7d03fcabbb3ba1
AdrianFleming <48060111+AdrianFleming@users.noreply.github.com>
2019-07-09 11:43:05 +0100

    Update index.d.ts
    Remove SwitchTransition; created in the wrong branch

--- types/react-transition-group/index.d.ts
+++ types/react-transition-group/index.d.ts
@@ -1,4 +1,4 @@
-// Type definitions for react-transition-group 2.10
+// Type definitions for react-transition-group 2.9
 // Project: https://github.com/reactjs/react-transition-group
 // Definitions by: Karol Janyst <https://github.com/LKay>
 //                 Epskampie <https://github.com/Epskampie>
@@ -7,13 +7,11 @@
 // TypeScript Version: 2.8
 
 import CSSTransition = require("./CSSTransition");
-import SwitchTransition = require("./SwitchTransition");
 import Transition from "./Transition";
 import TransitionGroup = require("./TransitionGroup");
 
 export {
     CSSTransition,
-    SwitchTransition,
     Transition,
     TransitionGroup
 };
