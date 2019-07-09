9cad6dbdf02265f56ff3b57657e692325b4cf000
AdrianFleming <48060111+AdrianFleming@users.noreply.github.com>
2019-07-09 10:49:17 +0100

    Update index.d.ts
    Export TypeScript definition for new SwitchTransition component

--- types/react-transition-group/index.d.ts
+++ types/react-transition-group/index.d.ts
@@ -1,4 +1,4 @@
-// Type definitions for react-transition-group 2.9
+// Type definitions for react-transition-group 2.10
 // Project: https://github.com/reactjs/react-transition-group
 // Definitions by: Karol Janyst <https://github.com/LKay>
 //                 Epskampie <https://github.com/Epskampie>
@@ -7,11 +7,13 @@
 // TypeScript Version: 2.8
 
 import CSSTransition = require("./CSSTransition");
+import SwitchTransition = require("./SwitchTransition");
 import Transition from "./Transition";
 import TransitionGroup = require("./TransitionGroup");
 
 export {
     CSSTransition,
+    SwitchTransition,
     Transition,
     TransitionGroup
 };
