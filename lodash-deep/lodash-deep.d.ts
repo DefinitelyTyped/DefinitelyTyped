05153f862fd9ed76ed62451be1ef726c40782966
Uzunov <stanko.uzunov@broadcom.com>
2019-07-11 14:32:50 +0200

    Add basic types def for lodash-deep
    Signed-off-by: Uzunov <stanko.uzunov@broadcom.com>

---
+++ lodash-deep/lodash-deep.d.ts
@@ -0,0 +1,13 @@
+// Type definitions for lodash-deep v 2.x
+// Project: https://github.com/marklagendijk/lodash-deep
+// Definitions by: Remo uzust01 <https://github.com/uzust01/>
+// Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
+
+
+declare module "lodash-deep" {
+    export function deepMapValues(
+        object: any,
+        callback: any,
+        propertyPath?: any
+    ): any;
+}
