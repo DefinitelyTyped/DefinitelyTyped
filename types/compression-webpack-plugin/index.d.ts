1938b1b55a7adacaff6d487cd92a5dde029f7550
Chris3773 <22506071+Chris3773@users.noreply.github.com>
2019-07-11 15:24:09 -0400

    Update version to 2.0.1.

--- types/compression-webpack-plugin/index.d.ts
+++ types/compression-webpack-plugin/index.d.ts
@@ -1,4 +1,4 @@
-// Type definitions for compression-webpack-plugin 2.0
+// Type definitions for compression-webpack-plugin 2.0.1
 // Project: https://github.com/webpack-contrib/compression-webpack-plugin
 // Definitions by: Anton Kandybo <https://github.com/dublicator>
 //                 Rhys van der Waerden <https://github.com/rhys-vdw>
