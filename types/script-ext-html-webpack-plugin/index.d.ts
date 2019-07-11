3031ca27517591f25a210d06af9d673cc20fa213
Max Boguslavskiy <mboguslavskiy@aligntech.com>
2019-07-11 20:45:05 +0300

    corrected formatting

--- types/script-ext-html-webpack-plugin/index.d.ts
+++ types/script-ext-html-webpack-plugin/index.d.ts
@@ -4,7 +4,7 @@
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
 // TypeScript Version: 2.3
 
-import { Compiler, Plugin } from "webpack";
+import { Compiler, Plugin } from 'webpack';
 
 export = ScriptExtHtmlWebpackPlugin;
 
