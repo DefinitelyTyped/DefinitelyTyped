d394f296b774ef6144a096924545889e00beb07f
Martin Jurča <martin.jurca@firma.seznam.cz>
2019-07-11 08:36:10 +0200

    fixed code style

--- types/promise.allsettled/implementation.d.ts
+++ types/promise.allsettled/implementation.d.ts
@@ -1,7 +1,7 @@
 import { PromiseRejection, PromiseResolution, PromiseResult } from './types';
 
 type PromiseTuple<T extends [unknown, ...unknown[]]> = {[P in keyof T]: Promise<T[P]>};
-type PromiseResultTuple<T extends [unknown, ...unknown[]]> = {[P in keyof T]: PromiseResult<T[P], unknown>};
+type PromiseResultTuple<T extends [unknown, ...unknown[]]> = {[P in keyof T]: PromiseResult<T[P]>};
 
 declare function allSettled(): Promise<[]>;
 declare function allSettled<T extends [unknown, ...unknown[]]>(iterable: PromiseTuple<T>): Promise<PromiseResultTuple<T>>;
