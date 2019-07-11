bbccf6f42928f6060ec9a6452138f00e5759c949
Demian <thde@users.noreply.github.com>
2019-07-11 02:10:22 +0200

    [ghost-storage-base] add module types (#36751)
    * add ghost-storage-base types
    
    * add typescript version
    
    * fix linting errors
    
    * add default export
    
    * fix abstract class errors
    
    * del default export
    
    * fix optional targetDir
    
    * make baseDir optional

---
+++ types/ghost-storage-base/index.d.ts
@@ -0,0 +1,31 @@
+// Type definitions for ghost-storage-base 0.0
+// Project: https://github.com/TryGhost/Ghost-Storage-Base
+// Definitions by: Demian <https://github.com/thde>
+// Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
+// TypeScript Version: 2.3
+
+import { Handler } from 'express';
+
+export interface Image {
+    path: string;
+    name: string;
+    type: string;
+}
+
+export interface ReadOptions {
+    path: string;
+}
+
+export abstract class StorageBase {
+    constructor();
+
+    abstract exists(fileName: string, targetDir?: string): Promise<boolean>;
+    abstract save(image: Image, targetDir?: string): Promise<string>;
+    abstract serve(): Handler;
+    abstract delete(fileName: string, targetDir?: string): Promise<boolean>;
+    abstract read(options?: ReadOptions): Promise<Buffer>;
+
+    getTargetDir(baseDir?: string): string;
+    getUniqueFileName(image: Image, targetDir: string): string;
+    getSanitizedFileName(fileName: string): string;
+}
