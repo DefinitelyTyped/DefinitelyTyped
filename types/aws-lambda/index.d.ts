85229112e6f0b3916f9e39484b4b3fd0fde28c70
Jon Simpkins <jon.simpkins@gmail.com>
2019-07-11 12:15:27 -0500

    fix: typo in CodePipeline docs

--- types/aws-lambda/index.d.ts
+++ types/aws-lambda/index.d.ts
@@ -639,7 +639,7 @@ export interface CodePipelineEvent {
  * https://docs.aws.amazon.com/codepipeline/latest/userguide/detect-state-changes-cloudwatch-events.html
  *
  * The above CodePipelineEvent is when a lambda is invoked by a CodePipeline.
- * These events are when you subsribe to CodePipeline events in CloudWatch.
+ * These events are when you subscribe to CodePipeline events in CloudWatch.
  *
  * Their documentation says that detail.version is a string, but it is actually an integer
  */
