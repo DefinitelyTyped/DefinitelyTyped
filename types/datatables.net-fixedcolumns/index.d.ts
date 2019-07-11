61553c4b23935ce5ca1a053e043346c1ea888d93
Arik-neKrol <brightgenie@gmail.com>
2019-07-11 11:36:49 +0500

    formatting

--- types/datatables.net-fixedcolumns/index.d.ts
+++ types/datatables.net-fixedcolumns/index.d.ts
@@ -1,4 +1,5 @@
 // Type definitions for datatables.net-fixedcolumns 3.2
+
 // Project: https://datatables.net
 // Definitions by: Konstantin Kuznetsov <https://github.com/Arik-neKrol>
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
@@ -8,73 +9,77 @@
 /// <reference types="datatables.net"/>
 
 declare namespace DataTables {
-  interface Settings {
-    /*
-     * FixedColumns extension options
-     */
-    fixedColumns?: boolean | FixedColumnsSettings;
-  }
+    interface Settings {
+        /*
+         * FixedColumns extension options
+         */
+        fixedColumns?: boolean | FixedColumnsSettings;
+    }
 
-  interface FixedColumnsSettings {
-    /*
-     * The algorithm to use. This can be one of (see below for full description):
-     * 'none' | 'semiauto' | 'auto'
-     */
-    heightMatch?: string;
+    interface FixedColumnsSettings {
+        /*
+         * The algorithm to use. This can be one of (see below for full description):
+         * 'none' | 'semiauto' | 'auto'
+         */
+        heightMatch?: string;
+
+        /*
+         * The number of columns on the left hand side of the table to fix in place.
+         */
+        leftColumns?: number;
+
+        /*
+         * The number of columns on the right hand side of the table to fix in place.
+         */
+        rightColumns?: number;
+    }
+
+    interface Api {
+        fixedColumns: FixedColumnsMethodsModel;
+    }
+
+    interface FixedColumnsMethodsModel {
+        /*
+        * Get FixedColumns Api
+        */
+        fixedColumns(): FixedColumnsMethods;
+    }
+
+    interface FixedColumnsMethods extends Api {
+        /*
+        * Update the data shown in the FixedColumns
+        */
+        update(): void;
+
+        /*
+        * Redraw the fixed columns based on new table size
+        */
+        relayout(): void;
+
+        /*
+        * Get the row index of a row in a fixed column
+        */
+        rowIndex(): number;
+
+        /*
+        * Get the cell index of a cell in a fixed column
+        */
+        cellIndex(): CellIndex;
+    }
 
     /*
-     * The number of columns on the left hand side of the table to fix in place.
-     */
-    leftColumns?: number;
-
-    /*
-     * The number of columns on the right hand side of the table to fix in place.
-     */
-    rightColumns?: number;
-  }
-
-  interface Api {
-    fixedColumns: FixedColumnsMethodsModel;
-  }
-
-  interface FixedColumnsMethodsModel {
-    /*
-    * Get FixedColumns Api
     */
-    fixedColumns(): FixedColumnsMethods;
-  }
+    interface CellIndex {
+        row: number;
+        column: number;
+        columnVisible: number;
+    }
 
-  interface FixedColumnsMethods extends Api {
-    /*
-    * Update the data shown in the FixedColumns
-    */
-    update(): void;
-    /*
-    * Redraw the fixed columns based on new table size
-    */
-    relayout(): void;
-    /*
-    * Get the row index of a row in a fixed column
-    */
-    rowIndex(): number;
-    /*
-    * Get the cell index of a cell in a fixed column
-    */
-    cellIndex(): CellIndex;
-  }
+    interface RowsMethods {
+        /*
+         * Recalculate the height of one or more rows after a data change
+         */
+        recalcHeight(): void;
 
-  /*
-  */
-  interface CellIndex {
-    row: number;
-    column: number;
-    columnVisible: number;
-  }
-
-  interface RowsMethods {
-    /*
-     * Recalculate the height of one or more rows after a data change
-     */
-    recalcHeight(): void;
-  }
+    }
 }
