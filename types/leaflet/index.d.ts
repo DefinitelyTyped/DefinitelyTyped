2b5f8af009dbbb4702c5d0d171f58209bffb437d
Nikita Zhakov <zhakovnikita@gmail.com>
2019-07-14 19:36:58 +0300

    fix type for html property in DivIconOptions
    According https://leafletjs.com/reference-1.5.0.html#divicon-html we can use HTMLElement for html property. Update becouse of this comment https://github.com/DefinitelyTyped/DefinitelyTyped/pull/35920#pullrequestreview-247360014
    <<https://leafletjs.com/reference-1.5.0.html#divicon-html >>
    
    Please fill in this template.
    
    - [x] Use a meaningful title for the pull request. Include the name of the package modified.
    - [x] Test the change in your own code. (Compile and run.)
    - [x] Add or edit tests to reflect the change. (Run with `npm test`.)
    - [x] Follow the advice from the [readme](https://github.com/DefinitelyTyped/DefinitelyTyped/blob/master/README.md#make-a-pull-request).
    - [x] Avoid [common mistakes](https://github.com/DefinitelyTyped/DefinitelyTyped/blob/master/README.md#common-mistakes).
    - [x] Run `npm run lint package-name` (or `tsc` if no `tslint.json` is present).
    
    Select one of these and delete the others:
    
    
    If changing an existing definition:
    - [x] Provide a URL to documentation or source code which provides context for the suggested changes: <<url here>>
    - [x] Increase the version number in the header if appropriate.

--- types/leaflet/index.d.ts
+++ types/leaflet/index.d.ts
@@ -1511,7 +1511,7 @@ export namespace Icon {
 export function icon(options: IconOptions): Icon;
 
 export interface DivIconOptions extends BaseIconOptions {
-    html?: string | false;
+    html?: string | false | HTMLElement | SVGElement;
     bgPos?: PointExpression;
     iconSize?: PointExpression;
     iconAnchor?: PointExpression;
