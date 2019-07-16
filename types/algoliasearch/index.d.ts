3efeaf2eb8a4948cf4b67513024a2cde90e4ac66
Dan Grayson <yesmeansyes@gmail.com>
2019-07-15 22:53:09 -0700

    updated version and added me to definitions by list

--- types/algoliasearch/index.d.ts
+++ types/algoliasearch/index.d.ts
@@ -1,4 +1,4 @@
-// Type definitions for algoliasearch-client-js 3.30.1
+// Type definitions for algoliasearch-client-js 3.30.2
 // Project: https://github.com/algolia/algoliasearch-client-js
 // Definitions by: Baptiste Coquelle <https://github.com/cbaptiste>
 //                 Haroen Viaene <https://github.com/haroenv>
@@ -8,6 +8,7 @@
 //                 Antoine Rousseau <https://github.com/antoinerousseau>
 //                 Luca Pasquale <https://github.com/lucapasquale>
 //                 Alexandre Deve <https://github.com/adeve>
+//                 Dan Grayson <https://github.com/dan-grayson>
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
 // TypeScript Version: 2.8
 
