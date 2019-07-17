ad428896638a744469e11b5543fb9cec9f949af0
Uche Chilaka <uche@lar.city>
2019-07-17 06:58:43 -0400

    (apimocker:build) removed offending test

--- types/apimocker/apimocker-tests.ts
+++ types/apimocker/apimocker-tests.ts
@@ -6,9 +6,6 @@ const PORT = 7878;
 // $ExpectType ApiMocker
 const server = ApiMocker.createServer({ quiet: false });
 
-// $ExpectType ApiMocker
-server.setConfigFile(path.resolve(__dirname, 'apimocker-test.config'));
-
 server.start(PORT, () => {
     console.log(`API mocker started successfully @${PORT}`);
     server.stop(() => console.log('API mocker stopped successully'));
