d2b29c07cfea081b9d85576afa44e0b4700b213d
Uche Chilaka <uche@lar.city>
2019-07-17 05:05:57 -0400

    (apimocker) fixed sample code

--- types/apimocker/apimocker-tests.ts
+++ types/apimocker/apimocker-tests.ts
@@ -3,9 +3,7 @@ import * as path from 'path';
 
 const PORT = 7878;
 
-// const server = ApiMocker.createServer({ quiet: false }).setConfigFile(path.resolve(__dirname, 'apimocker-test.config'));
-
-const server = ApiMocker.createServer({ quiet: false });
+const server = ApiMocker.createServer({ quiet: false }).setConfigFile(path.resolve(__dirname, 'apimocker-test.config'));
 
 server.start(PORT, () => {
     console.log(`API mocker started successfully @${PORT}`);
