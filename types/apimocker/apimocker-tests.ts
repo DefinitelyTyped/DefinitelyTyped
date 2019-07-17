e1d0db0aab17ca4cd335e578da31ff2b5ede70cc
Uche Chilaka <uche@lar.city>
2019-07-17 07:05:20 -0400

    (apimocker:refactor) more build fixes 😭

--- types/apimocker/apimocker-tests.ts
+++ types/apimocker/apimocker-tests.ts
@@ -1,12 +1,14 @@
 import * as ApiMocker from 'apimocker';
-import * as path from 'path';
 
 const PORT = 7878;
 
 // $ExpectType ApiMocker
 const server = ApiMocker.createServer({ quiet: false });
 
+// $ExpectType ApiMocker
 server.start(PORT, () => {
     console.log(`API mocker started successfully @${PORT}`);
+
+    // $ExpectType ApiMocker
     server.stop(() => console.log('API mocker stopped successully'));
 });
--- types/apimocker/index.d.ts
+++ types/apimocker/index.d.ts
@@ -4,8 +4,39 @@
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
 // TypeScript Version: 3.5
 
-import { RequestHandler } from 'express';
-import { configOptions, ApiMocker } from './models';
+import { RequestHandler, Application } from 'express';
+
+export interface configOptions {
+    port?: string;
+    mockDirectory?: string;
+    allowedDomains?: string[];
+    allowedHeaders?: string[];
+    logRequestHeaders?: boolean;
+    allowAvoidPreFlight?: boolean;
+    useUploadFieldname?: boolean;
+    webServices?: any;
+    quiet?: boolean;
+}
+
+export interface ApiMocker {
+    express: Application;
+    middlewares: RequestHandler[];
+    setConfigFile: (file: string) => ApiMocker;
+    loadConfigFile: () => void;
+    setRoutes: (webServices: any) => void;
+    /**
+     * Set the route for express, in case it was not set yet
+     */
+    setRoute: (options: any) => void;
+    /**
+     * @TODO inspect Express.Application.start
+     */
+    start: (serverPort: string | number, callback?: () => void) => ApiMocker;
+    /**
+     * @TODO inspect Express.Application.close
+     */
+    stop: (callback?: () => void) => ApiMocker;
+}
 
 export const middlewares: RequestHandler[];
 
