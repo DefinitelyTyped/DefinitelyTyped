d755680f4246128a27f451cd079340302fcf11d6
Levi Bostian <levi.bostian@gmail.com>
2019-07-18 09:30:06 -0500

    Enable dt-header lint rule @types/uid2

--- types/uid2/index.d.ts
+++ types/uid2/index.d.ts
@@ -1,4 +1,4 @@
-// Type definitions for uid2 0.3.3
+// Type definitions for uid2 0.3
 // Project: https://github.com/coreh/uid2
 // Definitions by: Levi Bostian <https://github.com/levibostian>
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
