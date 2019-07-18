41832e121e31546249ba69e293e37df72a5e98ab
taoqf <tao_qiufeng@126.com>
2019-07-18 15:01:51 +0800

    react-native-easy-update: add typescript version

--- types/react-native-easy-upgrade/index.d.ts
+++ types/react-native-easy-upgrade/index.d.ts
@@ -2,6 +2,7 @@
 // Project: https://github.com/react-native-org/react-native-easy-upgrade#readme
 // Definitions by: taoqf <https://github.com/taoqf>
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
+// TypeScript Version: 2.8
 
 interface Options {
 	iOSAppId: string;
