284faa4426e08ce3dc9433b44d407cd3a68764a0
Lusun <chobits4@sina.com>
2019-07-19 15:25:38 +0800

    modify min-app-tests.ts

--- types/min-app/min-app-tests.ts
+++ types/min-app/min-app-tests.ts
@@ -1,26 +1,27 @@
 Page({
     data: {
-      msg: "hello world",
-      share: "share to my friends!"
+        msg: 'hello world',
+        share: 'share to my friends!',
     },
     onLoad() {
-      wx.showToast({
-        title: 'my_world',
-        icon: 'success',
-        duration: 1500
-      });
-      wx.setStorageSync('token', "e23o33xfsefeeooi2344");
+        wx.showToast({
+            title: 'my_world',
+            icon: 'success',
+            duration: 1500,
+        });
+        wx.setStorageSync('token', 'e23o33xfsefeeooi2344');
     },
     onReady() {
-      wx.getStorageSync('token');
+        wx.getStorageSync('token');
     },
     onShareAppMessage(e: Page.IShareAppMessageOption): any {
-      wx.showModal({
-        title: 'my_title',
-        content: 'this is content...',
-        success(res) {
-          console.log(res);
-        }
-      });
-    }
-  });
+        wx.showModal({
+            title: 'my_title',
+            content: 'this is content...',
+            success(res) {
+                console.log(res);
+            },
+        });
+    },
+});
+
