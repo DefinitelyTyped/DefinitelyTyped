0fdc4623255b4184abb781cc9e5b5864220ca612
Lusun <chobits4@sina.com>
2019-07-19 15:27:20 +0800

    modefy tsconfig

--- types/min-app/min-app-tests.ts
+++ types/min-app/min-app-tests.ts
@@ -4,15 +4,19 @@ Page({
         share: 'share to my friends!',
     },
     onLoad() {
+
         wx.showToast({
             title: 'my_world',
             icon: 'success',
             duration: 1500,
         });
         wx.setStorageSync('token', 'e23o33xfsefeeooi2344');
+
     },
     onReady() {
+
         wx.getStorageSync('token');
+
     },
     onShareAppMessage(e: Page.IShareAppMessageOption): any {
         wx.showModal({
@@ -24,4 +28,5 @@ Page({
         });
     },
 });
+
 
