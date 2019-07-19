3c807c54803c95763a7c32036e25c90c6bdc6cef
Lusun <chobits4@sina.com>
2019-07-19 15:08:32 +0800

    rebuild .d.ts files from  dts-gen

--- types/min-app/lib.wa.es6.d.ts
+++ types/min-app/lib.wa.es6.d.ts
@@ -1,17 +1,7 @@
-/*! *****************************************************************************
-Copyright (c) Microsoft Corporation. All rights reserved. 
-Licensed under the Apache License, Version 2.0 (the "License"); you may not use
-this file except in compliance with the License. You may obtain a copy of the
-License at http://www.apache.org/licenses/LICENSE-2.0  
- 
-THIS CODE IS PROVIDED ON AN *AS IS* BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
-KIND, EITHER EXPRESS OR IMPLIED, INCLUDING WITHOUT LIMITATION ANY IMPLIED
-WARRANTIES OR CONDITIONS OF TITLE, FITNESS FOR A PARTICULAR PURPOSE, 
-MERCHANTABLITY OR NON-INFRINGEMENT. 
- 
-See the Apache Version 2.0 License for specific language governing permissions
-and limitations under the License.
-***************************************************************************** */
+// Type definitions for min-app 1.0
+// Project: https://github.com/chobits4/min-app (Does not have to be to GitHub, but prefer linking to a source code repository rather than to a project website.)
+// Definitions by: My Self <https://github.com/chobits4>
+// Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
 
 
 
