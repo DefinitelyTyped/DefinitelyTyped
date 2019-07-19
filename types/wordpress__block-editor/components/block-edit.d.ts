51894b84726c8b28c8cf34015dbeae8199a08c30
Derek Sifford <dereksifford@gmail.com>
2019-07-19 11:13:43 -0400

    [@wordpress/block-editor] add new definitions

---
+++ types/wordpress__block-editor/components/block-edit.d.ts
@@ -0,0 +1,9 @@
+import { ComponentType } from '@wordpress/element';
+
+declare namespace BlockEdit {
+    // It is extremely unclear what props this component accepts.
+    type Props = any;
+}
+declare const BlockEdit: ComponentType<BlockEdit.Props>;
+
+export default BlockEdit;
