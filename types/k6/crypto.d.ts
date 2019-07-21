f114bea76ffc41cd651e1fc453ca1f86dd1a6786
Book Moons <35854232+bookmoons@users.noreply.github.com>
2019-07-20 23:36:54 -0400

    k6/crypto: Document sha512_224

--- types/k6/crypto.d.ts
+++ types/k6/crypto.d.ts
@@ -108,8 +108,19 @@ export function sha512<OE extends OutputEncoding>(
     outputEncoding: OE
 ): Output<OE>;
 
-export function sha512_224(input: string, outputEncoding: BinaryEncoding): bytes;
-export function sha512_224(input: string, outputEncoding: StringEncoding): string;
+/**
+ * Hash with SHA-512/224.
+ * https://docs.k6.io/docs/sha512_224-input-outputencoding
+ * @param input - Data to hash.
+ * @param outputEncoding - Output encoding.
+ * @returns SHA-512/224 digest.
+ * @public
+ */
+export function sha512_224<OE extends OutputEncoding>(
+    input: string,
+    outputEncoding: OE
+): Output<OE>;
+
 export function sha512_256(input: string, outputEncoding: BinaryEncoding): bytes;
 export function sha512_256(input: string, outputEncoding: StringEncoding): string;
 export function ripemd160(input: string, outputEncoding: BinaryEncoding): bytes;
