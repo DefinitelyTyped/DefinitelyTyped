e5e8c0d3f066e5f3f05002e8d64d055250fb4c2a
Book Moons <35854232+bookmoons@users.noreply.github.com>
2019-07-21 00:05:46 -0400

    k6/encoding: Document module

--- types/k6/encoding.d.ts
+++ types/k6/encoding.d.ts
@@ -1,3 +1,8 @@
+/*
+ * Encoding utilities.
+ * https://docs.k6.io/docs/k6encoding
+ */
+
 export function b64decode(input: string, encoding?: Base64Variant): string;
 export function b64encode(input: string, encoding?: Base64Variant): string;
 
