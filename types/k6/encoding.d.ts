a15745ae13374529079a7ffaf324b621a7389ff1
Book Moons <35854232+bookmoons@users.noreply.github.com>
2019-07-21 00:08:48 -0400

    k6/encoding: Document b64decode

--- types/k6/encoding.d.ts
+++ types/k6/encoding.d.ts
@@ -13,6 +13,14 @@
  */
 export function b64decode(input: string, encoding?: Base64Variant): string;
 
+/**
+ * Base64 decode a string.
+ * https://docs.k6.io/docs/b64decode-input-encoding
+ * @param input - Base64 encoded string.
+ * @param encoding - Base64 variant.
+ * @returns Decoded string.
+ * @public
+ */
 export function b64encode(input: string, encoding?: Base64Variant): string;
 
 export type Base64Variant = 'std' | 'rawstd' | 'url' | 'rawurl';
