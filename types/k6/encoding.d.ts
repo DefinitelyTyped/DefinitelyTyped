3b49d9f030603bb457b9f794bde9255283ea4dd5
Book Moons <35854232+bookmoons@users.noreply.github.com>
2019-07-21 00:09:21 -0400

    k6/encoding: Document Base64Variant

--- types/k6/encoding.d.ts
+++ types/k6/encoding.d.ts
@@ -23,4 +23,8 @@ export function b64decode(input: string, encoding?: Base64Variant): string;
  */
 export function b64encode(input: string, encoding?: Base64Variant): string;
 
+/**
+ * Base64 variant.
+ * @public
+ */
 export type Base64Variant = 'std' | 'rawstd' | 'url' | 'rawurl';
