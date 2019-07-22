be9bb0c3563383e9c18a6f9a706ea3c954f7f26d
Book Moons <35854232+bookmoons@users.noreply.github.com>
2019-07-21 21:27:52 -0400

    k6/html: Document UListElement

--- types/k6/html.d.ts
+++ types/k6/html.d.ts
@@ -1370,9 +1370,15 @@ export abstract class TrackElement extends Element {
     protected __brand: never;
 }
 
+/**
+ * HTML <ul> element.
+ * https://docs.k6.io/docs/element-k6html
+ * @public
+ */
 export abstract class UListElement extends Element {
     protected __brand: never;
 }
+
 export abstract class VideoElement extends MediaElement {
     protected __brand: never;
 }
