16c76cc5f2d81ffda7e94f948468ae7e780c0e5f
Barron Wei <barronwei@outlook.com>
2019-07-22 10:09:54 +0200

    Definitons By Update

--- types/paypal-rest-sdk/index.d.ts
+++ types/paypal-rest-sdk/index.d.ts
@@ -1,6 +1,6 @@
 // Type definitions for paypal-rest-sdk 1.7
 // Project: https://github.com/paypal/PayPal-node-SDK
-// Definitions by: Andrew Throener <https://github.com/trainerbill>
+// Definitions by: Andrew Throener <https://github.com/trainerbill>, Barron Wei <https://github.com/barronwei>
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
 // TypeScript Version: 2.1
 
