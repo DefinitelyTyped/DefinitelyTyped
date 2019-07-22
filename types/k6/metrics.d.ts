09537f424ae81fdc1de4d1331d4f194143986b5e
Book Moons <35854232+bookmoons@users.noreply.github.com>
2019-07-22 04:39:26 -0400

    k6/metrics: Document Metric

--- types/k6/metrics.d.ts
+++ types/k6/metrics.d.ts
@@ -3,9 +3,24 @@
  * https://docs.k6.io/docs/k6metrics
  */
 
+/**
+ * Custom metric.
+ * @public
+ */
 export abstract class Metric {
     protected __brand: never;
+
+    /**
+     * @param name - Metric name.
+     * @param isTime - Whether values are time values.
+     */
     constructor(name: string, isTime?: boolean);
+
+    /**
+     * Add value.
+     * @param value - Value to add.
+     * @param tags - Tags to attach to data point.
+     */
     add(value: number | boolean, tags?: { [name: string]: string }): void;
 }
 
