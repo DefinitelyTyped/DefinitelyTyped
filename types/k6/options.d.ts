958c7d05d0bd8476afb47c314911c9b709880cdd
Book Moons <35854232+bookmoons@users.noreply.github.com>
2019-07-22 05:44:21 -0400

    k6/options: Document Options

--- types/k6/options.d.ts
+++ types/k6/options.d.ts
@@ -3,38 +3,108 @@
  * https://docs.k6.io/docs/options
  */
 
+import { CipherSuite } from './http';
+
+/**
+ * Program options.
+ * https://docs.k6.io/docs/options
+ * @public
+ */
 export interface Options {
+    /** Maximum parallel `http.batch()` connections per VU. */
     batch: number;
+
+    /** Maximum parallel `http.batch()` host connections per VU. */
     batchPerHost: number;
+
+    /** Blacklist IP ranges from being called. */
     blacklistIPs: string[];
+
+    /** Discard response bodies. */
     discardResponseBodies: boolean;
+
+    /** Test duration. */
     duration: string;
+
+    /** Third party collector configuration. */
     ext: { [name: string]: CollectorOptions };
+
+    /** Static hostname mapping. */
     hosts: { [name: string]: string };
+
+    /** Log all HTTP requests and responses. */
     httpDebug: string;
+
+    /** Disable TLS verification. Insecure. */
     insecureSkipTLSVerify: boolean;
+
+    /** Iterations to execute. */
     iterations: number;
+
+    /** Persist the k6 process after test completion. */
     linger: boolean;
+
+    /** Maximum HTTP redirects to follow. */
     maxRedirects: number;
+
+    /** Minimum test iteration duration. */
     minIterationDuration: string;
+
+    /** Disable keepalive connections. */
     noConnectionReuse: boolean;
+
+    /** Disable usage reports. */
     noUsageReport: boolean;
+
+    /** Disable cross-VU TCP connection reuse. */
     noVUConnectionReuse: boolean;
+
+    /** Start test in paused state. */
     paused: boolean;
+
+    /** Maximum requests per second across all VUs. */
     rps: number;
+
+    /** Setup function timeout. */
     setupTimeout: string;
+
+    /** Test stage specifications. Program of target VU stages. */
     stages: Stage[];
+
+    /** Define stats for trend metrics. */
     summaryTrendStats: string[];
+
+    /** Which system tags to include in collected metrics. */
     systemTags: string[];
+
+    /** Tags to set test wide across all metrics. */
     tags: { [name: string]: string };
+
+    /** Teardown function timeout. */
     teardownTimeout: string;
+
+    /** Threshold specifications. Defines pass and fail conditions. */
     thresholds: { [name: string]: Threshold[] };
+
+    /** Throw error on failed HTTP request. */
     throw: boolean;
+
+    /** TLS client certificates. */
     tlsAuth: Certificate[];
-    tlsCipherSuites: string[];
+
+    /** Allowed TLS cipher suites. */
+    tlsCipherSuites: CipherSuite[];
+
+    /** Allowed TLS version. Use `http.SSL_*` `http.TLS_*` constants. */
     tlsVersion: string | { min: string; max: string };
+
+    /** User agent string to include in HTTP requests. */
     userAgent: string;
+
+    /** Number of VUs to run concurrently. */
     vus: number;
+
+    /** Maximum VUs. Preallocates VUs to enable faster scaling. */
     vusMax: number;
 }
 
