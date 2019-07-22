984f625745653e925c8171ad8a140c53401d5f34
Haroen Viaene <hello@haroen.me>
2019-07-22 23:13:34 +0200

    [airtable]: add extra functions
    I got a little lazy at the end trying to figure out what all the parameters are, but at least they're now there, another PR can improve them.
    
    Ref: https://github.com/Airtable/airtable.js/blob/master/lib/table.js#L23

--- types/airtable/index.d.ts
+++ types/airtable/index.d.ts
@@ -33,6 +33,12 @@ declare global {
 
         interface Table<TFields extends FieldSet> {
             select(opt?: SelectOptions): Query<TFields>;
+            find(id: string): Promise<Response<TFields>>;
+            create(record: TFields, opts?: { typecast: boolean }): Promise<Response<TFields>>;
+            create(records: Array<TFields>, opts?: { typecast: boolean }): Promise<Array<Response<TFields>>>;
+            update(...args: any[]): Promise<any>;
+            replace(...args: any[]): Promise<any>;
+            destroy(...args: any[]): Promise<any>;
         }
 
         interface SelectOptions {
