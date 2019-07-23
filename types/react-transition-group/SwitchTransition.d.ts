f6b084236c7ddc7d9e39a685201ae662d4dbf0b4
Setsun <setsun@setsun.io>
2019-07-22 21:05:55 -0400

    Update types/react-transition-group/SwitchTransition.d.ts
    Co-Authored-By: Masafumi Koba <473530+ybiquitous@users.noreply.github.com>

--- types/react-transition-group/SwitchTransition.d.ts
+++ types/react-transition-group/SwitchTransition.d.ts
@@ -10,7 +10,7 @@ declare namespace SwitchTransition {
     /**
      * Transition modes.
      * `out-in`: Current element transitions out first, then when complete, the new element transitions in.
-     * `in-out: New element transitions in first, then when complete, the current element transitions out.`
+     * `in-out`: New element transitions in first, then when complete, the current element transitions out.
      */
     mode?: 'out-in' | 'in-out';
 
