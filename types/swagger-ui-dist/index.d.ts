59858d9d646a1a417c4690eb7eb18a02705c6805
brycematheson1234 <bryce@canva.com>
2019-07-25 05:55:51 +1000

    swagger-ui-dist additional typing (#37067)
    * Initial commit
    
    * Making linter happy

--- types/swagger-ui-dist/index.d.ts
+++ types/swagger-ui-dist/index.d.ts
@@ -1,15 +1,10 @@
 // Type definitions for swagger-ui-dist 3.x
 // Project: https://github.com/swagger-api/swagger-ui#readme
 // Definitions by: Haowen <https://github.com/haowen737>
+//                 Bryce <https://github.com/brycematheson1234>
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
 
-/* =================== USAGE ===================
-
-    import * as SwaggerUIDist from "swagger-ui-dist"
-    const swaggerUiAssetPath = SwaggerUIDist.getAbsoluteFSPath()
-
- =============================================== */
-
+export {};
 export as namespace SwaggerUIDist;
 
 /**
@@ -18,5 +13,33 @@ export as namespace SwaggerUIDist;
 export function getAbsoluteFSPath(): string;
 export function absolutePath(): string;
 
-export const SwaggerUIBundle: any;
+interface SwaggerUIBundle {
+  (a: {
+    deepLinking: boolean;
+    dom_id: string;
+    presets: any[];
+    plugins: any;
+    urls: Array<[string, string]>;
+    layout: string;
+  }): any;
+  presets: any;
+  plugins: any;
+  [k: string]: any;
+
+  getConfigs(): SwaggerConfigs;
+}
+
+export const SwaggerUIBundle: SwaggerUIBundle;
+
+interface SwaggerConfigs {
+  requestInterceptor: ((request: SwaggerRequest) => SwaggerRequest);
+  [k: string]: any;
+}
+
+interface SwaggerRequest {
+  url: string;
+  credentials: string;
+  [k: string]: any;
+}
+
 export const SwaggerUIStandalonePreset: any;
