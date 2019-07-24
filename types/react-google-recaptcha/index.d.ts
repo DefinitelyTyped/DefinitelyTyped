953c753a6608cda5635b99a944c922105883031f
Meir <mrzkeller@gmail.com>
2019-07-25 01:05:07 +0300

    Added the `hl?: string;;` prop, with description sourced from

--- types/react-google-recaptcha/index.d.ts
+++ types/react-google-recaptcha/index.d.ts
@@ -1,8 +1,9 @@
-// Type definitions for React Google Recaptcha 1.0
+// Type definitions for React Google Recaptcha 1.1
 // Project: https://github.com/dozoisch/react-google-recaptcha
 // Definitions by: Koala Human <https://github.com/KoalaHuman>
 //                 Tom Sturge <https://github.com/tomsturge>
 //                 Max Bo <https://github.com/MaxwellBo>
+//                 Meir Keller <https://github.com/meirkl>
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
 // TypeScript Version: 2.8
 
@@ -79,6 +80,12 @@ export interface ReCAPTCHAProps {
 	 *  @see reCAPTCHA secure-token
 	 */
 	stoken?: string;
+	/**
+	 * Optional. Set the hl parameter, which allows the captcha to be used from different languages.
+	 *
+	 * @default "en"
+	 */
+	hl?: string;
 	/**
 	 *  Optional compact, normal or invisible. This allows you to change the size or do an invisible captcha
 	 */
