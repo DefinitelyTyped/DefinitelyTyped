ebc46786397ac683047f778ec29abc86803ea90c
Joel Brenstrum <joel.brenstrum@gmail.com>
2019-07-24 14:14:04 +1200

    allow functions to be called

--- types/deep-freeze/index.d.ts
+++ types/deep-freeze/index.d.ts
@@ -11,7 +11,9 @@ declare function deepFreeze<T extends Function>(f: T): T;
 declare function deepFreeze<T>(o: T): deepFreeze.DeepReadonly<T>;
 
 declare namespace deepFreeze {
-    type DeepReadonly<T> = {
-        readonly [P in keyof T]: DeepReadonly<T[P]>
-    };
+    type DeepReadonly<T> =
+        T extends (...args: any) => any
+            ? T 
+            : { readonly [P in keyof T]: DeepReadonly<T[P]> };
+   
 }
