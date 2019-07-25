a6c07e0051bae80af7ea7b9bbd1f0ceeda0fcc71
Lusun <chobits4@sina.com>
2019-07-25 10:51:59 +0800

    changed the project name to minapp-env

--- types/minapp-env/index.d.ts
+++ types/minapp-env/index.d.ts
@@ -1,5 +1,5 @@
-// Type definitions for min-app 1.0
-// Project: https://github.com/chobits4/min-app (Does not have to be to GitHub, but prefer linking to a source code repository rather than to a project website.)
+// Type definitions for minapp-env 1.0
+// Project: https://github.com/chobits4/minapp-env (Does not have to be to GitHub, but prefer linking to a source code repository rather than to a project website.)
 // Definitions by: My Self <https://github.com/chobits4>
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
 // TypeScript Version: 2.8
