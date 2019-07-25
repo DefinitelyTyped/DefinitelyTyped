58f6a5b5cefc624afd8f28129e0d224f3a63041f
Lusun <chobits4@sina.com>
2019-07-25 10:59:34 +0800

    changed the project name to minapp-env

--- types/min-app/index.d.ts
+++
@@ -1,8 +0,0 @@
-// Type definitions for min-app(So far the best preserved declaration files of typescript for weixin mini program)
-// Project: https://mp.weixin.qq.com/debug/wxadoc/dev/api/
-// Definitions by: My Self <https://github.com/chobits4>
-// Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
-// TypeScript Version: 2.8
-
-/// <reference path="./lib.wa.es6.d.ts" />
-/// <reference path="./wx/index.d.ts" />
