6abd6dccd47a6d48d59719c0d6e0f0173e84fea5
Haseeb Majid <hmajid2301@gmail.com>
2019-07-25 21:28:53 +0100

    Added React Native App Intro Slider
    Added react-native-app-intro-slider.

--- types/react-native-dotenv/index.d.ts
+++ types/react-native-dotenv/index.d.ts
@@ -2,7 +2,11 @@
 // Project: https://github.com/zetachang/react-native-dotenv
 // Definitions by: hmajid2301 <https://github.com/hmajid2301>
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
-// TypeScript Version: 3.1
+// TypeScript Version: 3.0
 
-declare const _a: any;
-export = _a;
+interface Env {
+    [name: string]: string;
+}
+
+declare const env: Env;
+export = env;
