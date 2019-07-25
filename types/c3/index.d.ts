0bab095ce37a3c71e326a69d7ef57cd5a36cec9c
Ian Sanders <isanders@chromalloy.com>
2019-07-25 15:42:29 -0400

    Add more specific individual axis interfaces

--- types/c3/index.d.ts
+++ types/c3/index.d.ts
@@ -151,7 +151,7 @@ export interface ChartConfiguration {
 
     data: Data;
 
-    axis?: Axis;
+    axis?: AxesOptions;
 
     grid?: Grid;
 
@@ -547,54 +547,79 @@ export interface Data {
     idConverter?(id: string): string;
 }
 
-export type Axis = {
-    [key in YAxisName]?: YAxisConfiguration;
-} & {
+export interface AxesOptions {
     /**
      * Switch x and y axis position.
      */
     rotated?: boolean;
+    /** x axis configuration. */
     x?: XAxisConfiguration;
-};
+    /** y axis configuration. */
+    y?: YAxisConfigurationWithTime;
+    /** y2 axis configuration. */
+    y2?: YAxisConfiguration;
+}
 
-export interface XAxisConfiguration {
+export interface AxisConfiguration {
     /**
-     * Show or hide x axis.
+     * Show or hide the axis.
      */
     show?: boolean;
     /**
-     * Set type of x axis (timeseries, category, indexed)
+     * Set padding for axis.
+     * If this option is set, the range of axis will increase/decrease according to the values. If no padding is needed in the range of axis, `0` should be set. On category axis, this option
+     * will be ignored.
      */
-    type?: string;
+    padding?: {
+        /** Left side padding. */
+        left?: number;
+        /** Right side padding. */
+        right?: number;
+    };
+    /**
+     * Set label on axis.
+     * Valid horizontal axis positions: inner-right (default), inner-center, inner-left, outer-right, outer-center, outer-left
+     * Valid vertical axis positions: inner-top, inner-middle, inner-bottom, outer-top, outer-middle, outer-bottom
+     */
+    label?: string | {
+        /** The label text to show. */
+        text: string;
+        /** The position of the label. */
+        position: "inner-right" | "inner-center" | "inner-left" | "outer-right" | "outer-center" | "outer-left" | "inner-top" | "inner-middle" | "inner-bottom" | "outer-top" | "outer-middle" | "outer-bottom";
+    };
+    /**
+     * Set max value of the axis.
+     */
+    max?: string | number | Date;
+    /**
+     * Set min value of the axis.
+     */
+    min?: string | number | Date;
+    /**
+     * Show the axis inside of the chart.
+     */
+    inner?: boolean;
+}
+
+export interface XAxisConfiguration extends AxisConfiguration {
+    /**
+     * Set type of x axis.
+     * Defaults to `"indexed"`.
+     */
+    type?: "timeseries" | "category" | "indexed";
     /**
      * Set how to treat the timezone of x values.
-     * If true, treat x value as localtime. If false, convert to UTC internally.
+     * If `true` (default), treat x value as localtime. If `false`, convert to UTC internally.
      */
     localtime?: boolean;
     /**
      * Set category names on category axis.
-     * This must be an array that includes category names in string. If category names are included in the date by data.x option, this is not required.
+     * This must be an array that includes category names in string. If category names are included in the date by `data.x` option, this is not required.
      */
     categories?: string[];
 
     tick?: XTickConfiguration;
-    /**
-     * Set max value of x axis range.
-     */
-    max?: string | number | Date;
-    /**
-     * Set min value of x axis range.
-     */
-    min?: string | number | Date;
-    /**
-     * Set padding for x axis.
-     * If this option is set, the range of x axis will increase/decrease according to the values. If no padding is needed in the ragen of x axis, 0 should be set. On category axis, this option
-     * will be ignored.
-     */
-    padding?: {
-        left?: number;
-        right?: number;
-    };
+
     /**
      * Set height of x axis.
      * The height of x axis can be set manually by this option. If you need more space for x axis, please use this option for that. The unit is pixel.
@@ -604,33 +629,10 @@ export interface XAxisConfiguration {
      * Set default extent for subchart and zoom. This can be an array or function that returns an array.
      */
     extent?: number[] | (() => number[]);
-    /**
-     * Set label on x axis.
-     * You can set x axis label and change its position by this option. string and object can be passed and we can change the poisiton by passing object that has position key. Available position
-     * differs according to the axis direction (vertical or horizontal). If string set, the position will be the default.
-     * Valid horizontal positions: inner-right (Default), inner-center, inner-left, outer-right, outer-center, outer-left
-     * Valid vertical positions: inner-top, inner-middle, inner-bottom, outer-top, outer-middle, outer-bottom
-     */
-    label?: string | { text: string; position: string };
+    selection?: unknown;
 }
 
-export interface YAxisConfiguration {
-    /**
-     * Show or hide y axis.
-     */
-    show?: boolean;
-    /**
-     * Show y axis inside of the chart.
-     */
-    inner?: boolean;
-    /**
-     * Set max value of y axis.
-     */
-    max?: number;
-    /**
-     * Set min value of y axis.
-     */
-    min?: number;
+export interface YAxisConfiguration extends AxisConfiguration {
     /**
      * Change the direction of y axis.
      * If true set, the direction will be from the top to the bottom.
@@ -640,52 +642,32 @@ export interface YAxisConfiguration {
      * Set center value of y axis.
      */
     center?: number;
-    /**
-     * Set label on y axis. This option works in the same way as axis.x.label.
-     * Valid horizontal positions: inner-right (Default), inner-center, inner-left, outer-right, outer-center, outer-left
-     * Valid vertical positions: inner-top, inner-middle, inner-bottom, outer-top, outer-middle, outer-bottom
-     */
-    label?: string | { text: string; position: string };
+
     tick?: YTickConfiguration;
-    /**
-     * Set padding for y axis.
-     * You can set padding for y axis to create more space on the edge of the axis. This option accepts object and it can include top and bottom. top, bottom will be treated as pixels.
-     */
-    padding?: {
-        top?: number;
-        bottom?: number;
-    };
+
     /**
      * Set default range of y axis. This option set the default value for y axis when there is no data on init.
      */
-    default?: number[];
+    default?: [number, number];
+
+    max?: number;
+    min?: number;
 }
 
-export interface XTickConfiguration {
-    /**
-     * Centerise ticks on category axis
-     */
-    centered?: boolean;
+export interface YAxisConfigurationWithTime extends YAxisConfiguration {
+    tick?: YTickConfigurationWithTime;
+    type?: unknown;
+}
+
+export interface TickConfiguration {
     /**
      * A function to format tick value. Format string is also available for timeseries data.
      */
     format?: string | ((x: number | Date) => string | number);
     /**
-     * Setting for culling ticks.
-     * If true is set, the ticks will be culled, then only limitted tick text will be shown. This option does not hide the tick lines. If false is set, all of ticks will be shown.
+     * Show x axis outer tick.
      */
-    culling?: boolean | CullingConfiguration;
-    /**
-     * The number of x axis ticks to show.
-     * This option hides tick lines together with tick text. If this option is used on timeseries axis, the ticks position will be determined precisely and not nicely positioned (e.g. it will
-     * have rough second value).
-     */
-    count?: number;
-    /**
-     * Fit x axis ticks.
-     * If true set, the ticks will be positioned nicely. If false set, the ticks will be positioned according to x value of the data points.
-     */
-    fit?: boolean;
+    outer?: boolean;
     /**
      * Set the x values of ticks manually.
      * If this option is provided, the position of the ticks will be determined based on those values. This option works with timeseries data and the x values will be parsed accoding to the type
@@ -697,42 +679,50 @@ export interface XTickConfiguration {
      */
     rotate?: number;
     /**
-     * Show x axis outer tick.
-     */
-    outer?: boolean;
-    /**
-     * Set width of x axis tick.
-     */
-    width?: number;
-    multiline?: boolean; // Undocumented
-}
-
-export interface YTickConfiguration {
-    /**
-     * Set formatter for y axis tick text.
-     * This option accepts d3.format object as well as a function you define.
-     */
-    format?(x: number): string;
-    /**
-     * Show or hide outer tick.
-     */
-    outer?: boolean;
-    /**
-     * Set the y values of ticks manually.
-     */
-    values?: number[];
-    /**
-     * The number of y axis ticks to show.
-     * The position of the ticks will be calculated precisely, so the values on the ticks will not be rounded nicely. In the case, axis.y.tick.format or axis.y.tick.values will be helpful.
+     * The number of x axis ticks to show.
+     * This option hides tick lines together with tick text. If this option is used on timeseries axis, the ticks position will be determined precisely and not nicely positioned (e.g. it will
+     * have rough second value).
      */
     count?: number;
 }
 
-export interface CullingConfiguration {
+export interface XTickConfiguration extends TickConfiguration {
     /**
-     * The number of tick texts will be adjusted to less than this value.
+     * Centerise ticks on category axis
      */
-    max: number;
+    centered?: boolean;
+    /**
+     * Setting for culling ticks.
+     * If `true` is set, the ticks will be culled, then only limitted tick text will be shown. This option does not hide the tick lines. If `false` is set, all of ticks will be shown.
+     */
+    culling?: boolean | {
+        /**
+         * The number of tick texts will be adjusted to less than this value.
+         */
+        max: number;
+    };
+    /**
+     * Fit x axis ticks.
+     * If `true` set, the ticks will be positioned nicely. If `false` set, the ticks will be positioned according to x value of the data points.
+     */
+    fit?: boolean;
+    /**
+     * Set width of x axis tick.
+     */
+    width?: number;
+    multiline?: boolean;
+    multilineMax?: number;
+}
+
+export interface YTickConfiguration extends TickConfiguration {
+    format: (x: number) => string;
+}
+
+export interface YTickConfigurationWithTime extends YTickConfiguration {
+    time?: {
+        type?: unknown;
+        interval?: unknown;
+    }
 }
 
 export interface Grid {
