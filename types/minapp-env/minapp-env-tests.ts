c8c9584e44792ccacfc79870287b144ab5e4610e
Lusun <chobits4@sina.com>
2019-07-25 19:09:24 +0800

    modified tslint v19

--- types/minapp-env/minapp-env-tests.ts
+++ types/minapp-env/minapp-env-tests.ts
@@ -1,3 +1,4 @@
+// Project: https://github.com/chobits4/minapp-env (Does not have to be to GitHub, but prefer linking to a source code repository rather than to a project website.)
 // TypeScript Version: 2.8
 
 Page({
--- types/minapp-env/wx/lib.wx.api.d.ts
+++ types/minapp-env/wx/lib.wx.api.d.ts
@@ -1,7 +1,4 @@
-// Type definitions for minapp-env 1.0
 // Project: https://github.com/chobits4/minapp-env (Does not have to be to GitHub, but prefer linking to a source code repository rather than to a project website.)
-// Definitions by: chobits4 <https://github.com/chobits4>
-// Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
 // TypeScript Version: 2.8
 
 declare namespace wx {
