6701625e0ec7402b69c4633f6fa395fe1dcdcba8
Eric <eric@canva.com>
2019-07-25 18:11:38 +1000

    .

--- types/parse-package-name/index.d.ts
+++ types/parse-package-name/index.d.ts
@@ -1,13 +1,12 @@
-// Type definitions for parse-package-name
+// Type definitions for parse-package-name 0.1.0
 // Project: https://github.com/egoist/parse-package-name
 // Definitions by: BendingBender <https://github.com/fa93hws>
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
 
-declare module 'parse-package-name' {
-  const parsePackageName: (path: string) => {
-    name: string;
-    path: string;
-    version: string;
-  }
-  export = parsePackageName;
-}
+export = parsePackageName;
+
+declare const parsePackageName: (path: string) => {
+  name: string;
+  path: string;
+  version: string;
+};
