2208275cac6af78ce6a5acebb6482ac51fcf902d
Lusun <chobits4@sina.com>
2019-07-25 18:55:01 +0800

    modified tslint v11

--- types/minapp-env/index.d.ts
+++ types/minapp-env/index.d.ts
@@ -2,6 +2,7 @@
 // Project: https://github.com/chobits4/minapp-env (Does not have to be to GitHub, but prefer linking to a source code repository rather than to a project website.)
 // Definitions by: chobits4 <https://github.com/chobits4>
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
+// TypeScript Version: 3.0
 // TypeScript version: 3.0
 
 /// <reference path="./lib.wa.es6.d.ts" />
