8e05c75bae5ff9bd19c4df73cc00af2d61e938fc
Eric <eric@canva.com>
2019-07-25 18:08:58 +1000

    add test

--- types/parse-package-name/parse-package-name-tests.ts
+++ types/parse-package-name/parse-package-name-tests.ts
@@ -1,10 +1,6 @@
 import * as parsePackageName from 'parse-package-name';
 
-function testType({ name, path, version }: {
-  name: string;
-  path: string;
-  version: string;
-}) {}
+function testType({ name, path, version }: { name: string; path: string; version: string }) {}
 
 const result = parsePackageName('');
 testType(result);
