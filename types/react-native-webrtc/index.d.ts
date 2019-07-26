b834fcf0165edc21ef2f2b123579a5a1b7d92a2a
CARLOS QUIROGA <cquirogab@k-lagan.com>
2019-07-26 13:35:19 +0200

    Fix typescript version

--- types/react-native-webrtc/index.d.ts
+++ types/react-native-webrtc/index.d.ts
@@ -2,6 +2,7 @@
 // Project: https://github.com/react-native-webrtc/react-native-webrtc
 // Definitions by: Carlos Quiroga <https://github.com/KarlosQ>
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
+// TypeScript Version: 2.8
 
 export type RTCSignalingState =
     | "stable"
