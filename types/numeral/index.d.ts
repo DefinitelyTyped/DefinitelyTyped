0db2cc49da5280836916cefa69f290fe8a87e251
CARLOS QUIROGA <cquirogab@k-lagan.com>
2019-07-26 13:29:57 +0200

    reverse

--- types/numeral/index.d.ts
+++ types/numeral/index.d.ts
@@ -3,7 +3,6 @@
 // Definitions by: Vincent Bortone <https://github.com/vbortone>
 //                 Behind The Math <https://github.com/BehindTheMath>
 //                 Kenneth Luján <https://github.com/klujanrosas>
-//				   Carlos Quiroga <https://github.com/KarlosQ>
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
 
 
@@ -23,10 +22,6 @@ interface NumeralJSLocale {
 	currency: {
 		symbol: string;
 	};
-}
-
-interface NumeralJSLocales {
-	[id: string]: NumeralJSLocale
 }
 
 interface NumeralJSOptions {
@@ -65,11 +60,6 @@ interface Numeral {
 	locale(key?: string): string;
 
 	/**
-	 * Object with all loaded locales
-	 */
-	locales: NumeralJSLocales;
-
-	/**
 	 * This function provides access to the loaded locale data.  If
 	 * no arguments are passed in, it will simply return the current
 	 * global locale object.
@@ -95,7 +85,7 @@ interface Numeral {
 	unformat(inputString: string): number;
 	value(): number;
 	valueOf(): number;
-	set(value: any): Numeral;
+	set (value: any): Numeral;
 	add(value: any): Numeral;
 	subtract(value: any): Numeral;
 	multiply(value: any): Numeral;
