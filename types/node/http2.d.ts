5bd18024fa721f09a5d9ff27b9ba141336c23299
chyzwar <schizek.marcin@gmail.com>
2019-07-27 21:13:55 +0100

    allow to extend http2 Http2ServerResponse, Http2ServerRequest

--- types/node/http2.d.ts
+++ types/node/http2.d.ts
@@ -583,7 +583,7 @@ declare module "http2" {
     }
 
     export class Http2ServerRequest extends stream.Readable {
-        private constructor();
+        public constructor();
 
         readonly aborted: boolean;
         readonly authority: string;
@@ -651,7 +651,7 @@ declare module "http2" {
     }
 
     export class Http2ServerResponse extends stream.Stream {
-        private constructor();
+        public constructor();
 
         addTrailers(trailers: OutgoingHttpHeaders): void;
         readonly connection: net.Socket | tls.TLSSocket;
