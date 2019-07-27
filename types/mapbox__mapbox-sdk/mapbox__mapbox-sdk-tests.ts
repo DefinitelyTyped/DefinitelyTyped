cba2e7ef4c2897d3a6691f7ebf3ddb97aae29f1d
Jeff Dye <jeffbdye@gmail.com>
2019-07-26 21:32:38 -0400

    Initial commit

---
+++ types/mapbox__mapbox-sdk/mapbox__mapbox-sdk-tests.ts
@@ -0,0 +1,25 @@
+import Directions, { DirectionsService, DirectionsResponse } from '@mapbox/mapbox-sdk/services/directions';
+import MapiClient, { SdkConfig } from '@mapbox/mapbox-sdk/lib/classes/mapi-client';
+import { MapiRequest } from '@mapbox/mapbox-sdk/lib/classes/mapi-request';
+import { MapiResponse } from '@mapbox/mapbox-sdk/lib/classes/mapi-response';
+
+const config: SdkConfig = {
+  accessToken: 'access-token'
+};
+const client = new MapiClient(config);
+
+const ds: DirectionsService = Directions(client);
+
+const mapiRequest: MapiRequest = ds.getDirections({
+  profile: 'walking',
+  waypoints: [{
+    coordinates: [1, 3]
+  }, {
+    coordinates: [2, 4]
+  }]
+});
+
+const response = mapiRequest.send().then((response: MapiResponse) => {
+  const body = response.body as DirectionsResponse;
+  const route = body.routes;
+});
