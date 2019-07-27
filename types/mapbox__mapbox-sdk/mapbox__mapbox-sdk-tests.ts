cae8f185f33fe14fe87742afd7ae8fd2a5852609
Jeff Dye <jeffbdye@gmail.com>
2019-07-26 21:33:01 -0400

    Prettier

--- types/mapbox__mapbox-sdk/mapbox__mapbox-sdk-tests.ts
+++ types/mapbox__mapbox-sdk/mapbox__mapbox-sdk-tests.ts
@@ -4,22 +4,25 @@ import { MapiRequest } from '@mapbox/mapbox-sdk/lib/classes/mapi-request';
 import { MapiResponse } from '@mapbox/mapbox-sdk/lib/classes/mapi-response';
 
 const config: SdkConfig = {
-  accessToken: 'access-token'
+    accessToken: 'access-token',
 };
 const client = new MapiClient(config);
 
 const ds: DirectionsService = Directions(client);
 
 const mapiRequest: MapiRequest = ds.getDirections({
-  profile: 'walking',
-  waypoints: [{
-    coordinates: [1, 3]
-  }, {
-    coordinates: [2, 4]
-  }]
+    profile: 'walking',
+    waypoints: [
+        {
+            coordinates: [1, 3],
+        },
+        {
+            coordinates: [2, 4],
+        },
+    ],
 });
 
 const response = mapiRequest.send().then((response: MapiResponse) => {
-  const body = response.body as DirectionsResponse;
-  const route = body.routes;
+    const body = response.body as DirectionsResponse;
+    const route = body.routes;
 });
