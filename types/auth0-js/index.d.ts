4f1f0efd010348e2fdec2fb4635c25e49868a92a
Sergey Fetiskin <fetis26@gmail.com>
2019-07-29 13:35:37 +0200

    removed trailing spaces

--- types/auth0-js/index.d.ts
+++ types/auth0-js/index.d.ts
@@ -835,7 +835,7 @@ export interface AuthorizeOptions {
     mode?: "login" | "signUp";
     accessType?: string;
     approvalPrompt?: string;
-    appState?: any;                         
+    appState?: any;
 }
 
 export interface CheckSessionOptions extends AuthorizeOptions {
