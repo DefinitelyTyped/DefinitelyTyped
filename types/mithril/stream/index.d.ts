3b9cf28f620071f05a21c02011bf794277eea4ff
spacejack <spacejack@gmail.com>
2019-07-28 22:52:21 -0400

    Remove unused generic param from merge

--- types/mithril/stream/index.d.ts
+++ types/mithril/stream/index.d.ts
@@ -29,7 +29,7 @@ declare namespace Stream {
 	/** Combines the values of one or more streams into a single stream that is updated whenever one or more of the sources are updated */
 	export function lift<S extends any[], T>(fn: (...values: S) => T, ...streams: {[I in keyof S]: Stream<S[I]>}): Stream<T>;
 	/** Creates a stream whose value is the array of values from an array of streams. */
-	export function merge<S extends any[], T>(streams: {[I in keyof S]: Stream<S[I]>}): Stream<{[I in keyof S]: S[I]}>; // tslint:disable-line no-unnecessary-generics
+	export function merge<S extends any[]>(streams: {[I in keyof S]: Stream<S[I]>}): Stream<{[I in keyof S]: S[I]}>;
 	/** Creates a new stream with the results of calling the function on every incoming stream with and accumulator and the incoming value. */
 	export function scan<T, U>(fn: (acc: U, value: T) => U, acc: U, stream: Stream<T>): Stream<U>;
 	/** Takes an array of pairs of streams and scan functions and merges all those streams using the given functions into a single stream. */
