f5a50ceae9d886d352d8482ea978e5d44a3e621a
Kevin Brown <kevin.brown@techin.site>
2019-07-31 08:22:19 +1000

    Make offset parameter optional as per docs. (#37229)
    The docs and comment above this method indicate that `offset` isn't a required parameter. This change brings the type declarations in sync with that.

--- types/react-native-snap-carousel/index.d.ts
+++ types/react-native-snap-carousel/index.d.ts
@@ -297,7 +297,7 @@ export interface CarouselStatic<T> extends React.ComponentClass<CarouselProps<T>
      * (see #238). Note that the offset parameter is not required and will default to either 1 or -1 depending
      * on the current scroll position
      */
-    triggerRenderingHack(offset: number): void;
+    triggerRenderingHack(offset?: number): void;
 }
 
 export type CarouselProperties<T> = ScrollViewProps & CarouselProps<T> & React.Props<CarouselStatic<T>>;
