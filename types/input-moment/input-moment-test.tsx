92337fd97e02dfa24759f2700edf6e419dcbc6a5
Tim Ittermann <tim.ittermann@gmail.com>
2019-07-30 10:04:02 +0200

    Add input-moment

---
+++ types/input-moment/input-moment-test.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import InputMoment from "input-moment";
+import moment from "moment";
+
+const moment = moment.Moment();
+
+const OnChange: (m: moment.Moment) => void = (m: moment.Moment) => console.log(m);
+const OnSave: () => void = () => console.log("Save");
+
+const InputMomentTest: React.SFC = () =>
+    <InputMoment moment={moment} onChange={OnChange} onSave={OnSave} />
+;
