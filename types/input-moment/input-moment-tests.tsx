467963c0f307235dc79d1776b690bc0bbfe7b1be
Tim Ittermann <tim.ittermann@gmail.com>
2019-07-30 10:35:50 +0200

    Package.json

--- types/input-moment/input-moment-tests.tsx
+++ types/input-moment/input-moment-tests.tsx
@@ -2,9 +2,9 @@ import React from 'react';
 import InputMoment from 'input-moment';
 import moment from 'moment';
 
-const moment = moment.Moment();
+const momentI = moment();
 
 const OnChange: (m: moment.Moment) => void = (m: moment.Moment) => console.log(m);
 const OnSave: () => void = () => console.log('Save');
 
-const InputMomentTest: React.SFC = () => <InputMoment moment={moment} onChange={OnChange} onSave={OnSave} />;
+const InputMomentTest: React.SFC = () => <InputMoment moment={momentI} onChange={OnChange} onSave={OnSave} />;
