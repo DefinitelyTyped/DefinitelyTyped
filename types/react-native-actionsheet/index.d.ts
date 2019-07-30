5c57afeb10e5bb0363af3be374285fc1a957df2a
krister <krister@sent.at>
2019-07-30 16:07:56 +0300

    [@types/react-native-actionsheet] Add missing ActionSheetCustom

--- types/react-native-actionsheet/index.d.ts
+++ types/react-native-actionsheet/index.d.ts
@@ -7,9 +7,19 @@
 import * as React from 'react';
 
 export interface ActionSheetProps {
-    options: React.ReactNode[];
+    options: string[];
     onPress: (index: number) => void;
     title?: string;
+    message?: string;
+    tintColor?: string;
+    cancelButtonIndex?: number;
+    destructiveButtonIndex?: number;
+}
+
+export interface ActionSheetCustomProps {
+    options: React.ReactNode[];
+    onPress: (index: number) => void;
+    title?: React.ReactNode;
     message?: string;
     tintColor?: string;
     cancelButtonIndex?: number;
@@ -18,5 +28,9 @@ export interface ActionSheetProps {
 }
 
 export default class ActionSheet extends React.Component<ActionSheetProps> {
+    show: () => void;
+}
+
+export class ActionSheetCustom extends React.Component<ActionSheetCustomProps> {
     show: () => void;
 }
