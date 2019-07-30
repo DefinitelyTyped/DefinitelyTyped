c1c24377b6f990c6fa5e7be7d92e61c0a9a06f00
Alejandro Corredor <aecorredor93@gmail.com>
2019-07-30 00:40:50 -0400

    Add myself to the contributors list.
    Forgot to add myself in the first commit.

--- types/graphql-resolvers/index.d.ts
+++ types/graphql-resolvers/index.d.ts
@@ -1,6 +1,7 @@
 // Type definitions for graphql-resolvers 0.2
 // Project: https://github.com/lucasconstantino/graphql-resolvers#readme
 // Definitions by: Mike Engel <https://github.com/mike-engel>
+// Definitions by: Alejandro Corredor <https://github.com/aecorredor>
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
 // TypeScript Version: 2.6
 
