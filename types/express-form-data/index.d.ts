e6444f3d6c0181e48fda33e747cc65ed397e3fdd
nomnes <nomnes1990@gmail.com>
2019-08-01 06:53:29 +0700

    update by rules

--- types/express-form-data/index.d.ts
+++ types/express-form-data/index.d.ts
@@ -4,34 +4,17 @@
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
 // TypeScript Version: 2.2
 
-// INFO: without disabling 'no-single-declare-module' and 'no-declare-current-package' it not working correctly
-// tslint:disable-next-line no-single-declare-module no-declare-current-package
-declare module "express-form-data" {
-    import { NextHandleFunction } from 'connect';
-    import { FormOptions } from 'multiparty';
+import { NextHandleFunction } from 'connect';
+import { FormOptions } from 'multiparty';
 
-    interface FormDataOptions extends FormOptions {
-        autoClean?: boolean;
-    }
-
-    interface FormDataOptions {
-        autoClean?: boolean;
-        autoFields?: boolean;
-        autoFiles?: boolean;
-        maxFields?: number;
-        maxFieldsSize?: number;
-        maxFilesSize?: number;
-        uploadDir?: string;
-        encoding?: string;
-
-        [key: string]: any;
-    }
-
-    function parse(options?: FormDataOptions): NextHandleFunction;
-
-    function format(): NextHandleFunction;
-
-    function stream(): NextHandleFunction;
-
-    function union(): NextHandleFunction;
+export interface FormDataOptions extends FormOptions {
+    autoClean?: boolean;
 }
+
+export function parse(options?: FormDataOptions): NextHandleFunction;
+
+export function format(): NextHandleFunction;
+
+export function stream(): NextHandleFunction;
+
+export function union(): NextHandleFunction;
