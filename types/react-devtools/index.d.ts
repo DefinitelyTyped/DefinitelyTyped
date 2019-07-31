3f1e548e8fac113ee3e955a0e0ca3c697442ca97
Paulo Andrade <pauloa@iqmetrix.com>
2019-07-31 10:14:47 -0600

    fixes
    done after running npm run lint react-devtools

--- types/react-devtools/index.d.ts
+++ types/react-devtools/index.d.ts
@@ -1,10 +1,7 @@
-// Type definitions for react-easy-chart v3.6.3
+// Type definitions for react-devtools 3.6
 // Project: https://github.com/facebook/react-devtools
 // Definitions by: Paulo Andrade <https://github.com/prma85>
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
-// TypeScript Version: 3.4.5
+// TypeScript Version: 2.4
 
-declare module 'react-devtools' {
-    var main: any;
-    export = main;
-}
+export function main(): any;
--- types/react-devtools/react-devtools-tests.ts
+++ types/react-devtools/react-devtools-tests.ts
@@ -1,5 +1,3 @@
-import * as React from 'react';
-
 async function init() {
     await import('react-devtools');
 }
