0202025e82a5fb39e136e55c59383da560cedb8d
Teodor Tanasoaia <28601907+Teoxoy@users.noreply.github.com>
2019-08-01 23:51:23 +0200

    remove patch version

--- types/dat.gui/index.d.ts
+++ types/dat.gui/index.d.ts
@@ -1,4 +1,4 @@
-// Type definitions for dat.GUI 0.7.4
+// Type definitions for dat.GUI 0.7
 // Project: https://github.com/dataarts/dat.gui
 // Definitions by: Satoru Kimura <https://github.com/gyohk>, ZongJing Lu <https://github.com/sonic3d>, Richard Roylance <https://github.com/rroylance>, Nahuel Scotti <https://github.com/singuerinc>, Teoxoy <https://github.com/teoxoy>
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
