003eb06bc0edbb0df093be79e9d0576f42cd60ff
Christian Stornowski <christian.stornowski@materna.de>
2019-08-01 19:16:36 +0200

    Updated to newest vue js and vue2-datepicker version Added test for vue-class-component with decorators

--- types/vue2-datepicker/vue2-datepicker-tests.ts
+++ types/vue2-datepicker/vue2-datepicker-tests.ts
@@ -1,5 +1,6 @@
 import Vue from 'vue';
-import DatePicker from "vue2-datepicker";
+import DatePicker from 'vue2-datepicker';
+import Component from 'vue-class-component';
 
 new Vue({
     el: '#app',
@@ -21,3 +22,23 @@ new Vue({
         return { dateOfBirth: new Date() };
     }
 });
+
+@Component({
+    components: {
+        DatePicker
+    },
+    template: `
+        <date-picker
+            :placeholder="placeholder"
+            v-model="dateOfBirth"
+            format="YYYY-MM-DD"
+            lang="en">
+            <template slot="calendar-icon">
+                <span class="input-datepicker__icon"></span>
+            </template>
+        </date-picker>
+    `
+})
+class App extends Vue {
+    dateOfBirth = new Date();
+}
