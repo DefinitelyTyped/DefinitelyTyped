b9da25b43218ecf52227242b6fabba970a829ed3
Jason <me@jasonharrison.us>
2019-08-03 01:52:46 -0400

    Add types for bayes-classifier

---
+++ types/bayes-classifier/index.d.ts
@@ -0,0 +1,26 @@
+// Type definitions for bayes-classifier 0.0
+// Project: https://github.com/miguelmota/bayes-classifier
+// Definitions by: Jason Harrison <https://github.com/jasonharrison>
+// Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
+// TypeScript Version: 3.5
+
+export interface Classifications {
+    label: string;
+    value: number;
+}
+
+export default class bayes_classifier {
+    constructor();
+
+    addDocument(doc: string, label: string): void;
+
+    addDocuments(docs: string[], label: string): void;
+
+    classify(doc: string): string;
+
+    getClassifications(doc: string): Classifications;
+
+    restore(classifier: any): void;
+
+    train(): void;
+}
