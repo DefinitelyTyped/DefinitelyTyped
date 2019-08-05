c5e9cdb48cff54a4697046dd8cc6325f58b446d8
Aimee Gamble-Milner <aimee-gm@users.noreply.github.com>
2019-08-05 12:58:47 +0000

    correct vector definition

--- types/integrate-adaptive-simpson/integrate-adaptive-simpson-tests.ts
+++ types/integrate-adaptive-simpson/integrate-adaptive-simpson-tests.ts
@@ -3,10 +3,10 @@ import integrateVector = require('integrate-adaptive-simpson/vector');
 
 const functionTotest = (x: number) => x * 4;
 
-integrate(functionTotest, 12, 0);
+const integral: number = integrate(functionTotest, 12, 0);
 integrate(functionTotest, 12, 0, 0.01);
 integrate(functionTotest, 12, 0, 0.01, 17);
 
-integrateVector(functionTotest, 12, 0);
+const ventorIntegral: number[] = integrateVector(functionTotest, 12, 0);
 integrateVector(functionTotest, 12, 0, 0.01);
 integrateVector(functionTotest, 12, 0, 0.01, 17);
--- types/integrate-adaptive-simpson/vector.d.ts
+++ types/integrate-adaptive-simpson/vector.d.ts
@@ -1,2 +1,5 @@
-import integrate = require('.');
+type Func = (val: number) => number;
+
+declare function integrate(f: Func, a: number, b: number, tol?: number, maxdepth?: number): number[];
+
 export = integrate;
