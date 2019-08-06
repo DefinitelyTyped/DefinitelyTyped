85b04343adf91f5f99b69433d074e59784cf5e1d
LeonMontealegre <leonm99@gmail.com>
2019-08-06 17:22:39 -0400

    Added myself to authors list

--- types/intro.js/index.d.ts
+++ types/intro.js/index.d.ts
@@ -1,6 +1,7 @@
 // Type definitions for intro.js 2.4
 // Project: https://github.com/usablica/intro.js
 // Definitions by: Maxime Fabre <https://github.com/anahkiasen>
+//                 Leon Montealegre <https://github.com/leonmontealegre>
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
 
 declare namespace IntroJs {
