d7993e6ca025f3ef74da25ac2af133bdded38a9a
ayZagen <ayZagen@users.noreply.github.com>
2019-08-06 13:12:31 +0300

    chore: updated version

--- types/email-templates/index.d.ts
+++ types/email-templates/index.d.ts
@@ -1,4 +1,4 @@
-// Type definitions for node-email-templates 3.5
+// Type definitions for node-email-templates 6.0
 // Project: https://github.com/niftylettuce/email-templates
 // Definitions by: Cyril Schumacher <https://github.com/cyrilschumacher>
 //                 Matus Gura <https://github.com/gurisko>
