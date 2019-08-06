408248938a6d079d9514097da6403375b47e672a
Dmitry Demensky <dmitry@demensky.com>
2019-08-06 20:18:10 +0300

    Enable & fix `dt-header` rule

--- types/googlemaps/index.d.ts
+++ types/googlemaps/index.d.ts
@@ -1,19 +1,17 @@
 // Type definitions for Google Maps JavaScript API 3.37
 // Project: https://developers.google.com/maps/
-// Definitions by:  Folia A/S <http://www.folia.dk>,
-//                  Chris Wrench <https://github.com/cgwrench>,
-//                  Kiarash Ghiaseddin
-//                  <https://github.com/Silver-Connection/DefinitelyTyped>,
-//                  Grant Hutchins <https://github.com/nertzy>,
-//                  Denis Atyasov <https://github.com/xaolas>,
-//                  Michael McMullin <https://github.com/mrmcnerd>,
-//                  Martin Costello <https://github.com/martincostello>,
-//                  Sven Kreiss <https://github.com/svenkreiss>
-//                  Umar Bolatov <https://github.com/bolatovumar>
-//                  Michael Gauthier <https://github.com/gauthierm>
-//                  Colin Doig <https://github.com/captain-igloo>
-//                  Dmitry Demensky <https://github.com/demensky>
-//                  Vladimir Dashukevich <https://github.com/life777>
+// Definitions by: Chris Wrench <https://github.com/cgwrench>,
+//                 Kiarash Ghiaseddin <https://github.com/Silver-Connection>,
+//                 Grant Hutchins <https://github.com/nertzy>,
+//                 Denis Atyasov <https://github.com/xaolas>,
+//                 Michael McMullin <https://github.com/mrmcnerd>,
+//                 Martin Costello <https://github.com/martincostello>,
+//                 Sven Kreiss <https://github.com/svenkreiss>
+//                 Umar Bolatov <https://github.com/bolatovumar>
+//                 Michael Gauthier <https://github.com/gauthierm>
+//                 Colin Doig <https://github.com/captain-igloo>
+//                 Dmitry Demensky <https://github.com/demensky>
+//                 Vladimir Dashukevich <https://github.com/life777>
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
 
 // TypeScript Version: 2.7
