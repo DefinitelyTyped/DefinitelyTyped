d3d1295490d13816d4b87de04f4203f67cb28d40
Nathan Hardy <nhardy@users.noreply.github.com>
2019-08-08 07:32:12 +1000

    react-pdf: Update some types to use pdfjs-dist for better type safety (#37424)

--- types/react-pdf/index.d.ts
+++ types/react-pdf/index.d.ts
@@ -3,7 +3,7 @@
 // Definitions by: CodeDaraW <https://github.com/CodeDaraW>
 //                 Nathan Hardy <https://github.com/nhardy>
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
-// TypeScript Version: 2.8
+// TypeScript Version: 3.0
 
 import Document from './dist/Document';
 import Page from './dist/Page';
