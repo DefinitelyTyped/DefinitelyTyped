af6ce078c7d4ea082a6b47d9fb1b76c97dddc570
Leon Montealegre <leonm99@gmail.com>
2019-08-07 09:00:13 -0400

    Small commit to force travis.ci to update
    'Cause the error seems to be a network issue and I can't trigger a refresh :(

--- types/intro.js/index.d.ts
+++ types/intro.js/index.d.ts
@@ -1,7 +1,7 @@
 // Type definitions for intro.js 2.4
 // Project: https://github.com/usablica/intro.js
 // Definitions by: Maxime Fabre <https://github.com/anahkiasen>
-//                 Leon Montealegre <https://github.com/leonmontealegre>
+//                 Leon Montealegre <https://github.com/LeonMontealegre>
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
 
 declare namespace IntroJs {
