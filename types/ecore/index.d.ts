66d56c3ddddac26972fa4a2281b194541ae1ad9d
michael-whi <michael_tugushev@mail.ru>
2019-08-08 11:43:03 +0300

    Test.

--- types/ecore/index.d.ts
+++ types/ecore/index.d.ts
@@ -39,14 +39,14 @@ export let Resource: Resource;
 export let EPackageRegistry: EPackageRegistry;
 export let EPackage: EPackage;
 
-interface Edit {
+export interface Edit {
     childTypes: (object: EObject, createDescriptor: any) => EObject[];
     siblingTypes: (object: EObject, createDescriptor: any) => EObject[];
     childDescriptors: (object: EObject) => EObject[];
     siblingDescriptors: (object: EObject) => EObject[];
     choiceOfValues: (owner: EObject, feature: EObject) => EObject[];
 }
-interface EObject {
+export interface EObject {
     setEClass: (eClass: EClass) => void;
     create: (attributes: any) => EObject;
     has: (name: string) => boolean;
@@ -67,7 +67,7 @@ interface EObject {
     _id: string;
     getEStructuralFeature: (feature: string | EObject) => any;
 }
-interface EList extends EObject {
+export interface EList extends EObject {
     add: (eObject: EObject) => EList;
     addAll: (args: EObject[] | EObject) => EList;
     remove: (eObject: EObject) => EList;
@@ -85,34 +85,34 @@ interface EList extends EObject {
     contains: (eObject: EObject) => boolean;
     indexOf: (eObject: EObject) => number;
 }
-type EString = EObject;
-type EInt = EObject;
-type EBoolean = EObject;
-type EDouble = EObject;
-type EDate = EObject;
-type EIntegerObject = EObject;
-type EFloatObject = EObject;
-type ELongObject = EObject;
-type EMap = EObject;
-type EDiagnosticChain = EObject;
-type JSObject = EObject;
-type EModelElement = EObject;
-type EAnnotation = EModelElement;
-type ENamedElement = EModelElement;
-type EClassifier = ENamedElement;
-type EClass = EClassifier;
-type EDataType = EClassifier;
-type ETypedElement = ENamedElement;
-type EStructuralFeature = ETypedElement;
-type EAttribute = EStructuralFeature;
-type EReference = EStructuralFeature;
-type EOperation = ETypedElement;
-type EParameter = ETypedElement;
-type EEnum = EDataType;
-type EEnumLiteral = ENamedElement;
-type EGenericType = EObject;
-type ETypeParameter = ENamedElement;
-interface Resource extends EObject {
+export type EString = EObject;
+export type EInt = EObject;
+export type EBoolean = EObject;
+export type EDouble = EObject;
+export type EDate = EObject;
+export type EIntegerObject = EObject;
+export type EFloatObject = EObject;
+export type ELongObject = EObject;
+export type EMap = EObject;
+export type EDiagnosticChain = EObject;
+export type JSObject = EObject;
+export type EModelElement = EObject;
+export type EAnnotation = EModelElement;
+export type ENamedElement = EModelElement;
+export type EClassifier = ENamedElement;
+export type EClass = EClassifier;
+export type EDataType = EClassifier;
+export type ETypedElement = ENamedElement;
+export type EStructuralFeature = ETypedElement;
+export type EAttribute = EStructuralFeature;
+export type EReference = EStructuralFeature;
+export type EOperation = ETypedElement;
+export type EParameter = ETypedElement;
+export type EEnum = EDataType;
+export type EEnumLiteral = ENamedElement;
+export type EGenericType = EObject;
+export type ETypeParameter = ENamedElement;
+export interface Resource extends EObject {
     add: (value: EObject) => void;
     addAll: (values: EObject[]) => EObject;
     clear: () => EList;
@@ -124,18 +124,18 @@ interface Resource extends EObject {
     load: (res: any) => void;
     to: () => any;
 }
-interface EPackage extends EObject {
+export interface EPackage extends EObject {
     getEPackage: (nsURI: string) => EPackage;
     register: (ePackage: EPackage) => void;
     ePackages: () => EPackage[];
     elements: (type: EObject) => EObject[];
     Registry: EPackageRegistry;
 }
-interface EPackageRegistry extends EObject {
+export interface EPackageRegistry extends EObject {
     register: (ePackage: EPackage) => void;
     ePackages: () => EPackage[];
 }
-interface ResourceSet extends EObject {
+export interface ResourceSet extends EObject {
     create: (uri: any) => Resource;
     getEObject: (uri: string) => EObject;
     parse: (data: EObject) => void;
