d10533e859da61183ecd362b416753d8e43c3be5
Wayne Brantley <waynebrantley@users.noreply.github.com>
2019-08-08 12:02:06 -0400

    Add errors property to submissionError

--- types/redux-form/lib/SubmissionError.d.ts
+++ types/redux-form/lib/SubmissionError.d.ts
@@ -2,6 +2,7 @@ import { FormErrors } from "../index";
 
 export declare class SubmissionError<FormData = {}, ErrorType = string> extends Error {
   constructor(errors?: FormErrors<FormData, ErrorType>);
+  public errors: FormErrors<FormData, ErrorType> | undefined;
 }
 
 export default SubmissionError;
