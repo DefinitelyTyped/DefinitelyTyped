7c791adc27d7c894b44b822bcfcc066fc0cd06ab
Emily Marigold Klassen <forivall@gmail.com>
2019-08-09 13:23:40 -0700

    Make toposort properly generic and add missing array definition

--- types/toposort/index.d.ts
+++ types/toposort/index.d.ts
@@ -2,7 +2,11 @@
 // Project: https://github.com/marcelklehr/toposort
 // Definitions by: Daniel Byrne <https://github.com/danwbyrne>
 //                 Prokop Simek <https://github.com/prokopsimek>
+//                 Emily Marigold Klassen <https://github.com/forivall>
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
 
-declare function toposort(graph: Array<[string, string | undefined]>): string[];
+declare function toposort<T = string>(edges: ReadonlyArray<readonly [T, T | undefined]>): T[];
+declare namespace toposort {
+    export function array<T = string>(nodes: ReadonlyArray<T>, edges: ReadonlyArray<readonly [T, T | undefined]>): T[]
+}
 export = toposort;
