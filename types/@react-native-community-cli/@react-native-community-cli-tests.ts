c0f2524e862a83b101249ee5c90094a6429638df
agathekieny <agkieny@gmail.com>
2019-08-09 15:42:56 +0200

    react-native-community/cli : add tests and types

--- types/@react-native-community-cli/@react-native-community-cli-tests.ts
+++ types/@react-native-community-cli/@react-native-community-cli-tests.ts
@@ -1,20 +1,17 @@
 import { UserDependencyConfigT } from '@react-native-community-cli';
 
-
-
-export const pluginConfig : UserDependencyConfigT = {
-    commands : [
-        { 
-            name : 'test-type',
-            func : () => new Promise((resolve) => resolve()),
-            options : 
-                [
-                    {
-                    name : '--option1',
-                    description : 'description of the command'}
-                ]
-            
-        }
-    ]
-}
+export const pluginConfig: UserDependencyConfigT = {
+  commands: [
+    {
+      name: 'test-type',
+      func: () => new Promise(resolve => resolve()),
+      options: [
+        {
+          name: '--option1',
+          description: 'description of the command',
+        },
+      ],
+    },
+  ],
+};
 
