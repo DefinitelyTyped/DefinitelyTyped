a7c1e19ddf69358a66337ea633a31bcf43660070
Matt <visionsofparadise@pm.me>
2019-08-10 22:52:31 +0100

    cleaned up tests

--- types/react-tag-input/react-tag-input-tests.tsx
+++ types/react-tag-input/react-tag-input-tests.tsx
@@ -1,6 +1,7 @@
 import * as React from "react";
 import * as ReactDOM from "react-dom";
 import { WithContext as ReactTags } from "react-tag-input";
+import { Tag } from './';
 
 const tags = Array({ id: "0", text: "test" }, { id: "1", text: "testing" });
 
@@ -13,14 +14,21 @@ ReactDOM.render(
         delimiters={[13, 8, 188]} // Enter, Tab and Comma
         placeholder="Some placeholder text"
         labelField="Some label"
-
-        handleAddition={(tag: { id: string, text: string }) => console.log("Add: " + tag.text)}
-        handleDelete={(i: number) => console.log("Delete: " + i)}
-        handleDrag={(tag: { id: string; text: string; }, currPos: number, newPos: number) => console.log("Drag: " + tag.text)}
-        handleInputChange={(value: string) => console.log("Changed to: ", value)}
-        handleFilterSuggestions={(textInputValue: string, possibleSuggestionsArray: Array<{ id: string, text: string }>) => suggestions}
-        handleInputBlur={() => console.log("Blured")}
-
+        handleAddition={(tag: Tag) =>
+            console.log('Add: ' + tag.text)
+        }
+        handleDelete={(i: number) => console.log('Delete: ' + i)}
+        handleDrag={(
+            tag: Tag,
+            currPos: number,
+            newPos: number,
+        ) => console.log('Drag: ' + tag.text)}
+        handleInputChange={(value: string) => console.log('Changed to: ', value)}
+        handleFilterSuggestions={(
+            textInputValue: string,
+            possibleSuggestionsArray: Tag[],
+        ) => suggestions}
+        handleInputBlur={() => console.log('Blurred')}
         autofocus={false}
         allowDeleteFromEmptyInput={false}
         minQueryLength={0}
@@ -29,11 +37,9 @@ ReactDOM.render(
         readOnly={false}
         maxLength={64}
         inputFieldPosition="top"
-        renderSuggestion={({ id, text }, query) => console.log('tag' + id, text)}
-
+        renderSuggestion={({ id, text }: Tag, query: string) => console.log('tag' + id, text)}
         name="react-tags-field"
         id="react-tags-field"
-
         classNames={{
             tags: 'tagsClass',
             tagInput: 'tagInputClass',
@@ -42,8 +48,8 @@ ReactDOM.render(
             tag: 'tagClass',
             remove: 'removeClass',
             suggestions: 'suggestionsClass',
-            activeSuggestion: 'activeSuggestionClass'
+            activeSuggestion: 'activeSuggestionClass',
         }}
     />,
-    document.getElementById("app")
+    document.getElementById('app'),
 );
