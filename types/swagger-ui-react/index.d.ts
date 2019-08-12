5b202eec4bdf228bd912b811f4e41fed49d16032
viki.green <viki.green@trulioo.com>
2019-08-12 14:08:33 -0700

    Typings for swagger-ui-react

---
+++ types/swagger-ui-react/index.d.ts
@@ -0,0 +1,22 @@
+// Type definitions for swagger-ui-react 3.23
+// Project: https://github.com/swagger-api/swagger-ui#readme
+// Definitions by: viki.green <https://github.com/VictoriaGreen93>
+// Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
+
+import * as React from 'react';
+
+type Request = { [k: string]: any };
+type Response = { [k: string]: any };
+type System = any;
+
+export interface SwaggerUIProps {
+    spec?: object;
+    url?: string;
+    onComplete?: (system: System) => void;
+    requestInterceptor?: (req: Request) => Request | Promise<Request>;
+    responseInterceptor?: (res: Response) => Response | Promise<Response>;
+    docExpansion: 'list' | 'full' | 'none';
+}
+
+declare class SwaggerUI extends React.PureComponent<SwaggerUIProps> {}
+export default SwaggerUI;
