4abc391bcd3483dee1d709c47b2c5fcb2769e7a8
viki.green <viki.green@trulioo.com>
2019-08-12 14:26:56 -0700

    Typings for swagger-ui-react

--- types/swagger-ui-react/index.d.ts
+++ types/swagger-ui-react/index.d.ts
@@ -2,6 +2,7 @@
 // Project: https://github.com/swagger-api/swagger-ui#readme
 // Definitions by: viki.green <https://github.com/VictoriaGreen93>
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
+// TypeScript Version: 2.8
 
 import * as React from 'react';
 
