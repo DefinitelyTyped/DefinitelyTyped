091d2bba0b8f92b01d22583c5a62a5e77aa446ae
Mike Deverell <mike@onepeloton.com>
2019-08-13 18:26:02 -0400

    add  assertions to add tests

--- types/ramda/tests/add.ts
+++ types/ramda/tests/add.ts
@@ -1,11 +1,16 @@
 import { add } from 'ramda';
 
+// $ExpectType (b: number) => number
 add(3);
 
+// $ExpectType number
 add(2, 3); // =>  5
 
+// $ExpectType number
 add(7)(10); // => 17
 
+// $ExpectType string
 add('Hello', ' World'); // =>  "Hello World"
 
+// $ExpectType string
 add('Hello')(' World'); // =>  "Hello World"
