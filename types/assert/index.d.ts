134e8dcd7eec25dac0436aa5ecf1e4160bc97c63
Zaoqi <zaomir@outlook.com>
2019-08-13 17:35:25 +0800

    @types/assert: add strict

--- types/assert/index.d.ts
+++ types/assert/index.d.ts
@@ -7,6 +7,46 @@
 declare function assert(value: any, message?: string): void;
 
 declare namespace assert {
+    function strict(value: any, message?: string): void;
+    export namespace strict{
+
+        function fail(actual?: any, expected?: any, message?: string, operator?: string): void;
+
+        function ok(value: any, message?: string): void;
+
+        function equal(actual: any, expected: any, message?: string): void;
+
+        function notEqual(actual: any, expected: any, message?: string): void;
+
+        function deepEqual(actual: any, expected: any, message?: string): void;
+
+        function notDeepEqual(actual: any, expected: any, message?: string): void;
+
+        function deepStrictEqual(actual: any, expected: any, message?: string): void;
+
+        function strictEqual(actual: any, expected: any, message?: string): void;
+
+        function notStrictEqual(actual: any, expected: any, message?: string): void;
+
+        function throws(block: () => void, message?: string): void;
+        function throws(block: () => void, error: (() => void) | ((err: any) => boolean) | RegExp, message?: string): void;
+
+        function doesNotThrow(block: () => void, message?: string): void;
+        function doesNotThrow(block: () => void, error: (() => void) | ((err: any) => boolean) | RegExp, message?: string): void;
+
+        function ifError(value: any): void;
+
+        class AssertionError implements Error {
+            name: string;
+            message: string;
+            actual: any;
+            expected: any;
+            operator: string;
+            generatedMessage: boolean;
+
+            constructor(options?: { message?: string; actual?: any; expected?: any; operator?: string; stackStartFunction?: () => void });
+        }
+    }
     function fail(actual?: any, expected?: any, message?: string, operator?: string): void;
 
     function ok(value: any, message?: string): void;
