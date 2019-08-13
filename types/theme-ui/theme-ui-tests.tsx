c14ed38e51e1bf132f89666c58043a441e78266a
Ifiok Jr <ifiokotung@gmail.com>
2019-08-13 19:33:50 +0100

    feat: update theme-ui types (#37578)

--- types/theme-ui/theme-ui-tests.tsx
+++ types/theme-ui/theme-ui-tests.tsx
@@ -1,10 +1,50 @@
 /** @jsx jsx */
-import { Flex, jsx } from 'theme-ui';
+import { Flex, jsx, InitializeColorMode, ColorMode, Styled } from 'theme-ui';
 
 export const Component = () => {
     return (
+      <>
+        <InitializeColorMode />
+        <ColorMode />
+        <Styled.a href="https://awesome.com">Awesome</Styled.a>
+        <Styled
+          as="article"
+          sx={{
+            ':hover': {
+              backgroundColor: 'red',
+            },
+          }}
+        >
+          Works
+        </Styled>
         <div sx={{ bg: 'red' }}>
-            <Flex sx={{ backgroundColor: 'pink' }} />
+          <Flex sx={{ backgroundColor: 'pink' }} />
         </div>
+      </>
     );
 };
+
+const Success = () => (
+  <>
+    <div
+      sx={{
+        'body > div': {
+          ':hover': {
+            color: 'red',
+          },
+        },
+      }}
+    />
+    <div
+      sx={{
+        '@media only screen and (max-width: 600px)': {
+          'body > div': {
+            ':hover': {
+              color: 'red',
+            },
+          },
+        },
+      }}
+    />
+  </>
+);
