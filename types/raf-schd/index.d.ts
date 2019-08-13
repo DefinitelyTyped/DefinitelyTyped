e083fd405661202cfaa22d1e3e7b0a34260509e1
Adam Bergman <adam@fransvilhelm.com>
2019-08-13 09:30:42 +0200

    [raf-schd] Add types for raf-schd

---
+++ types/raf-schd/index.d.ts
@@ -0,0 +1,14 @@
+// Type definitions for raf-schd 4.0
+// Project: https://github.com/alexreardon/raf-schd (Does not have to be to GitHub, but prefer linking to a source code repository rather than to a project website.)
+// Definitions by: Adam Bergman <https://github.com/adambrgmn>
+// Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
+// TypeScript Version: 3.1
+
+interface Schedule<T extends (...args: any[]) => any> {
+    (...args: Parameters<T>): void;
+    cancel(): void;
+}
+
+declare function rafSchd<T extends (...args: any[]) => void>(fn: T): Schedule<T>;
+
+export = rafSchd;
