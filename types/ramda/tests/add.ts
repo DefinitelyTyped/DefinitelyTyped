73886ee393b940befcfe7e71a9dd6e2516648527
Mike Deverell <mike@onepeloton.com>
2019-08-13 18:25:27 -0400

    reformat add tests

--- types/ramda/tests/add.ts
+++ types/ramda/tests/add.ts
@@ -1,12 +1,11 @@
-import * as R from 'ramda';
+import { add } from 'ramda';
 
-(() => {
-  const plus3 = R.add(3);
-})();
+add(3);
 
-() => {
-  R.add(2, 3); // =>  5
-  R.add(7)(10); // => 17
-  R.add('Hello', ' World'); // =>  "Hello World"
-  R.add('Hello')(' World'); // =>  "Hello World"
-};
+add(2, 3); // =>  5
+
+add(7)(10); // => 17
+
+add('Hello', ' World'); // =>  "Hello World"
+
+add('Hello')(' World'); // =>  "Hello World"
