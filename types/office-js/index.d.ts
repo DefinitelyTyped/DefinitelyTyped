c1af560bd0b0a0fe4978eb330e68e34bd96f3cdd
Alex Jerabek <aljerabe@microsoft.com>
2019-08-14 11:22:33 -0700

    Increased urgency around using strings in isSetSupported

--- types/office-js/index.d.ts
+++ types/office-js/index.d.ts
@@ -973,15 +973,24 @@ declare namespace Office {
     }
 
     /**
-     * Provides information about what Requirement Sets are supported in current environment.
+     * Provides information about which Requirement Sets are supported in current environment.
      */
     interface RequirementSetSupport {
         /**
         * Check if the specified requirement set is supported by the host Office application.
         * @param name - Set name; e.g., "MatrixBindings".
-        * @param minVersion - The minimum required version; e.g., "1.4". Note: String type is recommended data type for this parameter. The use of number type is deprecated and will not be compatible with recent requirement sets.
+        * @param minVersionString - The minimum required version (e.g., "1.4").
         */
-       isSetSupported(name: string, minVersion?: string | number): boolean;
+       isSetSupported(name: string, minVersion?: string): boolean;
+
+       
+        /**
+        * Check if the specified requirement set is supported by the host Office application.
+        * @param name - Set name; e.g., "MatrixBindings".
+        * @param minVersion - The minimum required version (e.g., 1.4). Warning: Your add-ins should use the string overload of `isSetSupported` instead. 
+        * The use of the number type is deprecated, as the JavaScript parser cannot tell the difference between `1.1` and `1.10`.
+        */
+       isSetSupported(name: string, minVersionNumber?: number): boolean;
     }
 
     /**
