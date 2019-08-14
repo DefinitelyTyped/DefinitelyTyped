955ebd0549fcb6fdebbac0f8fe637e0e8efe6c51
orangain <orangain@gmail.com>
2019-08-15 00:24:32 +0900

    [react-material-ui-form-validator] Change return type of ValidatorForm.isFormValid for v2.0.7
    react-form-validator-core: v0.6.2

--- types/react-material-ui-form-validator/index.d.ts
+++ types/react-material-ui-form-validator/index.d.ts
@@ -18,7 +18,7 @@ export interface ValidatorFormProps {
 }
 export class ValidatorForm extends React.Component<ValidatorFormProps> {
     static addValidationRule(name: string, callback: (value: any) => boolean): void;
-    isFormValid(dryRun: boolean): boolean;
+    isFormValid(dryRun: boolean): Promise<boolean>;
     resetValidations(): void;
 }
 
