745be742fac06bead00a33633f78e8fbcfd2423f
tkrahn <krahn@newmediacompany.de>
2019-08-14 11:44:01 +0200

    [chart.js] updated version

--- types/chart.js/index.d.ts
+++ types/chart.js/index.d.ts
@@ -1,4 +1,4 @@
-// Type definitions for Chart.js 2.7
+// Type definitions for Chart.js 2.8
 // Project: https://github.com/nnnick/Chart.js, https://www.chartjs.org
 // Definitions by: Alberto Nuti <https://github.com/anuti>
 //                 Fabien Lavocat <https://github.com/FabienLavocat>
