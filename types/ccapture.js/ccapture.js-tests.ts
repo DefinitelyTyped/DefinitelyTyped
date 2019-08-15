da1516817278f616e427d35e91d4981cc51f38f8
Sam Alexander <alexander.sam@me.com>
2019-08-15 12:35:23 +0100

    Add types for ccapture.js

---
+++ types/ccapture.js/ccapture.js-tests.ts
@@ -0,0 +1,8 @@
+import * as CCapture from "ccapture.js";
+
+const canvas = document.createElement('canvas');
+const capturer = new CCapture({ format: 'webm' });
+capturer.start(); // $ExpectType void
+capturer.capture(canvas); // $ExpectType void
+capturer.stop(); // $ExpectType void
+capturer.save(); // $ExpectType void
