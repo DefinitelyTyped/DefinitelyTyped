d1fd234771d4b05d4c54f5824c3405fa87a8a2c8
Mike Deverell <mike@onepeloton.com>
2019-08-15 23:00:19 -0400

    move 'both' tests to separate file

--- types/ramda/ramda-tests.ts
+++ types/ramda/ramda-tests.ts
@@ -2587,21 +2587,6 @@ class Rectangle {
  */
 
 () => {
-    function gt10(x: number) {
-        return x > 10;
-    }
-
-    function even(x: number) {
-        return x % 2 === 0;
-    }
-
-    const f = R.both(gt10, even);
-    const g = R.both(gt10)(even);
-    f(100); // => true
-    f(101); // => false
-};
-
-() => {
     function isEven(n: number) {
         return n % 2 === 0;
     }
