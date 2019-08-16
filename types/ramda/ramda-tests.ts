e43b5f83f9402701582d9e601bedb8c1daf74eeb
Mike Deverell <mike@onepeloton.com>
2019-08-16 17:58:04 -0400

    move 'innerJoin' tests to separate file

--- types/ramda/ramda-tests.ts
+++ types/ramda/ramda-tests.ts
@@ -1699,24 +1699,6 @@ type Pair = KeyValuePair<string, number>;
     R.symmetricDifferenceWith(eqL)(l1, l2); // => ['dddd', 'c']
 };
 
-() => {
-    const list1 = [
-        {id: 1, name: 'One'},
-        {id: 2, name: 'Two'},
-        {id: 3, name: 'Three'},
-        {id: 4, name: 'Four'},
-        {id: 5, name: 'Five'}
-    ];
-
-    const list2 = [5, 4, 6];
-    const matchId = (record: { id: number, name: string }, id: number): boolean => record.id === id;
-
-    R.innerJoin(matchId, list1, list2); // [{"id": 4, "name": "Four"}, {"id": 5, "name": "Five"}]
-
-    const innerJoinCurried = R.innerJoin(matchId);
-    innerJoinCurried(list1, list2); // [{"id": 4, "name": "Four"}, {"id": 5, "name": "Five"}]
-};
-
 /*****************************************************************
  * String category
  */
