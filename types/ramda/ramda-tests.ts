693dbcd80802ac626e8ed3d4a80d4997e4760ca3
Mike Deverell <mike@onepeloton.com>
2019-08-16 17:43:56 -0400

    move 'hasPath' tests to separate file

--- types/ramda/ramda-tests.ts
+++ types/ramda/ramda-tests.ts
@@ -1119,16 +1119,6 @@ class Rectangle {
 };
 
 () => {
-    R.hasPath(['a', 'b'], {a: {b: 2}});         // true
-    R.hasPath(['a', 'b'], {a: {b: undefined}}); // => true
-    R.hasPath(['a', 'b'], {a: {c: 2}});         // => false
-    R.hasPath(['a', 'b'], {});                  // => false
-
-    const hasPathCurried = R.hasPath(['a', 'b']);
-    hasPathCurried({a: {b: 2}}); // true
-};
-
-() => {
     const raceResultsByFirstName = {
         first : "alice",
         second: "jake",
