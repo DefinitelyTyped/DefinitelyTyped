76091df83b592e33742469996d1977b0de0ccc92
Evan Shortiss <evanshortiss@gmail.com>
2019-08-16 11:32:26 -0700

    address linter errors

--- types/pixl-xml/index.d.ts
+++ types/pixl-xml/index.d.ts
@@ -4,53 +4,52 @@
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
 // TypeScript Version: 2.3
 
-
 /**
  * Options that can be passed to parse function
  */
 export interface PixlParseOptions {
-    preserveAttributes?: boolean
-    lowerCase?: boolean
-    preserveDocumentNode?: boolean
-    preserveWhitespace?: boolean
-    forceArrays?: boolean
+    preserveAttributes?: boolean;
+    lowerCase?: boolean;
+    preserveDocumentNode?: boolean;
+    preserveWhitespace?: boolean;
+    forceArrays?: boolean;
 }
 
 /**
  * Parser class can be used for a more object oriented style API
  */
-export class Parser <OutputJsonInterface>{
-    constructor (xml: string, options?: PixlParseOptions)
+export class Parser <OutputJsonInterface> {
+    constructor(xml: string, options?: PixlParseOptions)
 
-    public attribsKey: string
-    public dataKey: string
-    public documentNodeName: string
-    public piNodeList: string[]
-    public dtdNodeList: string[]
-    public errors: any[]
+    attribsKey: string;
+    dataKey: string;
+    documentNodeName: string;
+    piNodeList: string[];
+    dtdNodeList: string[];
+    errors: any[];
 
     /**
      * The XML input that was provided to the constructor
      */
-    public text: string
+    text: string;
 
     /**
      * The resulting JSON from the parse operation
      */
-    public tree: OutputJsonInterface
+    tree: OutputJsonInterface;
 
     /**
      * Returns the parsed XML tree. This tree is mutable and can be modified
      * as necessary.
      */
-    public getTree (): OutputJsonInterface
+    getTree(): OutputJsonInterface;
 
     /**
      * Returns an XML string with formatting determined by the params provided
      * @param indentCharacter
      * @param eol
      */
-    public compose (indentCharacter?: string, eol?: string): string
+    compose(indentCharacter?: string, eol?: string): string;
 }
 
 /**
@@ -58,7 +57,7 @@ export class Parser <OutputJsonInterface>{
  * (<) and right-angle-bracket (>), into their XML-safe counterparts.
  * @param xml
  */
-export function encodeEntities (xml: string): string
+export function encodeEntities(xml: string): string;
 
 /**
  * Similar to encodeEntities, but it also includes encoding for single-quotes
@@ -66,7 +65,7 @@ export function encodeEntities (xml: string): string
  * composing into an attribute value.
  * @param xml
  */
-export function encodeAttribEntities (xml: string): string
+export function encodeAttribEntities(xml: string): string;
 
 /**
  * This function decodes all the standard XML entities back into their
@@ -74,7 +73,7 @@ export function encodeAttribEntities (xml: string): string
  * right-angle-bracket (>), single-quote (') and double-quote (").
  * @param xml
  */
-export function decodeEntities (xml: string): string
+export function decodeEntities(xml: string): string;
 
 /**
  * This function will wrap anything passed to it into an array and return the
@@ -82,48 +81,53 @@ export function decodeEntities (xml: string): string
  * simply returned verbatim.
  * @param input
  */
-export function alwaysArray <T>(input: T|T[]): T[]
+export function alwaysArray<T>(input: T|T[]): T[];
 
 /**
  * This function returns all the hash keys as an array. Useful for sorting and
  * then iterating over the sorted list.
  * @param input
  */
-export function hashKeysToArray (input: object): string[]
+export function hashKeysToArray(input: object): string[];
 
 /**
  * This function returns true if the provided argument is a hash (object),
  * false otherwise.
  * @param input
  */
-export function isaHash (input: any): boolean
+export function isaHash(input: any): boolean;
 
 /**
  * This function returns true if the provided argument is an array
  * (or is array-like), false otherwise.
  * @param input
  */
-export function isaArray (input: any): boolean
+export function isaArray(input: any): boolean;
 
 /**
  * his function returns the number of keys in the specified hash.
  * @param hash
  */
-export function numKeys (hash: object): number
+export function numKeys(hash: object): number;
 
 /**
  * This function returns the first key of the hash when iterating over it.
  * Note that hash keys are stored in an undefined order.
  * @param hash
  */
-export function firstKey (hash: object): string
+export function firstKey(hash: object): string;
 
 /**
  * Parse an XML string into a JSON Object.
  * Throws if a parsing error occurs.
+ *
+ * @example
+ *
+ *      pixl.parse(xmlstring) as SomeInterface
+ *
  * @param xml
  */
-export function parse<OutputJsonInterface>(xml: string, options?: PixlParseOptions): OutputJsonInterface
+export function parse(xml: string, options?: PixlParseOptions): {};
 
 /**
  * Composes XML Objects back to a string. Pass in your pre-parsed XML object,
@@ -137,4 +141,4 @@ export function parse<OutputJsonInterface>(xml: string, options?: PixlParseOptio
  * @param eolCharacter
  * @param preserveOrder
  */
-export function stringify(doc: Object, outerElName?: string, indentSize?: number, indentCharacter?: string, eolCharacter?: string, preserveOrder?: boolean): string
+export function stringify(doc: object, outerElName?: string, indentSize?: number, indentCharacter?: string, eolCharacter?: string, preserveOrder?: boolean): string;
