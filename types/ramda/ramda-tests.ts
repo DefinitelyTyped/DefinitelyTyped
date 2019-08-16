a064e9e2d883f5f78635b1eda1427763eb4cf67a
Mike Deverell <mike@onepeloton.com>
2019-08-15 22:28:09 -0400

    move 'allPass' tests to separate file

--- types/ramda/ramda-tests.ts
+++ types/ramda/ramda-tests.ts
@@ -2681,19 +2681,6 @@ class Rectangle {
 /*****************************************************************
  * Logic category
  */
-() => {
-    function gt10(x: number) {
-        return x > 10;
-    }
-
-    function even(x: number) {
-        return x % 2 === 0;
-    }
-
-    const f = R.allPass([gt10, even]);
-    f(11); // => false
-    f(12); // => true
-};
 
 () => {
     R.and(false, true); // => false
