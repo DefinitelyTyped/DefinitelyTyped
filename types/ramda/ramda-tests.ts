a104c6234e624c7cbc1719df00fc1ca4c1bfcb0f
Mike Deverell <mike@onepeloton.com>
2019-08-16 18:32:10 -0400

    move 'isEmpty' tests to separate file

--- types/ramda/ramda-tests.ts
+++ types/ramda/ramda-tests.ts
@@ -1674,15 +1674,6 @@ type Pair = KeyValuePair<string, number>;
 };
 
 () => {
-    R.isEmpty([1, 2, 3]); // => false
-    R.isEmpty([]); // => true
-    R.isEmpty(""); // => true
-    R.isEmpty(null); // => false
-    R.isEmpty({}); // =>true
-    R.isEmpty({a: 1}); // => false
-};
-
-() => {
     R.not(true); // => false
     R.not(false); // => true
     R.not(0); // => true
