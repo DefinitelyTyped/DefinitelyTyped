9b059238b8da6c7c90bfb616daf0e913300569eb
Mike Deverell <mike@onepeloton.com>
2019-08-16 10:22:15 -0400

    move 'eqBy' tests to separate file

--- types/ramda/ramda-tests.ts
+++ types/ramda/ramda-tests.ts
@@ -2128,10 +2128,6 @@ class Rectangle {
  * Logic category
  */
 
-(() => {
-    R.eqBy(Math.abs, 5, -5); // => true
-});
-
 () => {
     // Flatten all arrays in the list but leave other values alone.
     const flattenArrays = R.map(R.ifElse(Array.isArray, R.flatten, R.identity));
