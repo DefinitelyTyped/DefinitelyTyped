fe9a2d2831411decf708c0af07929f90467a3873
Mike Deverell <mike@onepeloton.com>
2019-08-16 17:48:01 -0400

    move 'identity' tests to separate file

--- types/ramda/ramda-tests.ts
+++ types/ramda/ramda-tests.ts
@@ -1505,14 +1505,6 @@ type Pair = KeyValuePair<string, number>;
  * Relation category
  */
 () => {
-    const a1 = R.identity(1); // => 1
-    const obj  = {};
-    const a2 = R.identity([1, 2, 3]);
-    const a3 = R.identity(["a", "b", "c"]);
-    const a4 = R.identity(obj) === obj; // => true
-};
-
-() => {
     const testPath = ["x", 0, "y"];
     const testObj  = {x: [{y: 2, z: 3}, {y: 4, z: 5}]};
 
