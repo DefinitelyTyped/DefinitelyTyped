b9675246b5120775a4fbd109c13f3fcc6b9e4043
Mike Deverell <mike@onepeloton.com>
2019-08-16 18:06:19 -0400

    move 'is' tests to separate file

--- types/ramda/ramda-tests.ts
+++ types/ramda/ramda-tests.ts
@@ -1725,28 +1725,6 @@ class Rectangle {
 };
 
 /*****************************************************************
- * Is category
- */
-() => {
-    R.is(Object, {}); // => true
-    R.is(Object)({}); // => true
-    R.is(Number, 1); // => true
-    R.is(Number)(1); // => true
-    R.is(Object, 1); // => false
-    R.is(Object)(1); // => false
-    R.is(String, "s"); // => true
-    R.is(String)("s"); // => true
-    R.is(String, new String("")); // => true
-    R.is(String)(new String("")); // => true
-    R.is(Object, new String("")); // => true
-    R.is(Object)(new String("")); // => true
-    R.is(Object, "s"); // => false
-    R.is(Object)("s"); // => false
-    R.is(Number, {}); // => false
-    R.is(Number)({}); // => false
-};
-
-/*****************************************************************
  * Logic category
  */
 
