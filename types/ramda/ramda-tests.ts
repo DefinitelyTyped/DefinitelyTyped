6b457cab5aecb115638deb8dd65eafc160ed6973
Mike Deverell <mike@onepeloton.com>
2019-08-16 17:28:09 -0400

    move 'groupBy' tests to separate file

--- types/ramda/ramda-tests.ts
+++ types/ramda/ramda-tests.ts
@@ -516,20 +516,6 @@ interface Obj {
 };
 
 () => {
-    const byGrade  = R.groupBy((student: { score: number; name: string }) => {
-        const score = student.score;
-        return score < 65 ? "F" :
-            score < 70 ? "D" :
-                score < 80 ? "C" :
-                    score < 90 ? "B" : "A";
-    });
-    const students = [{name: "Abby", score: 84},
-        {name: "Eddy", score: 58},
-        {name: "Jack", score: 69}];
-    byGrade(students);
-};
-
-() => {
     interface MyObject {
         id: string;
         quantity: number;
