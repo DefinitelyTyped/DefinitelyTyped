96d418d961dc4bd1fe6c57d8f18d2e14c60931c7
Yuri Drabik <yurist38@mail.ru>
2019-08-16 15:53:52 +0200

    Added types for module compress.js

---
+++ types/compress.js/index.d.ts
@@ -0,0 +1,46 @@
+// Type definitions for compress.js 1.1
+// Project: - (link to the repo is missing)
+// Definitions by: Yuri Drabik <https://github.com/yurist38>
+// Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
+
+export default class Compress {
+    attach(
+        el: string,
+        options: CompressOptions,
+    ): Promise<CompressResult[]>;
+
+    compress(
+        files: File[],
+        options: CompressOptions,
+    ): Promise<CompressResult[]>;
+
+    static convertBase64ToFile(
+        base64: string,
+        mime?: string,
+    ): File;
+}
+
+export interface CompressOptions {
+    quality?: number;
+    size?: number;
+    maxWidth?: number;
+    maxHeight?: number;
+    resize?: boolean;
+}
+
+export interface CompressResult {
+    data: string;
+    prefix: string;
+    elapsedTimeInSeconds: number;
+    alt: string;
+    initialSizeInMb: number;
+    endSizeInMb: number;
+    ext: string;
+    quality: number;
+    endWidthInPx: number;
+    endHeightInPx: number;
+    initialWidthInPx: number;
+    initialHeightInPx: number;
+    sizeReducedInPercent: number;
+    iterations: number;
+}
