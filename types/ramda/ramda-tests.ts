6679b584ebdef076e1ba0bcf96f648d18465e98f
Mike Deverell <mike@onepeloton.com>
2019-08-17 14:12:55 -0400

    move 'tryCatch' tests to separate file

--- types/ramda/ramda-tests.ts
+++ types/ramda/ramda-tests.ts
@@ -161,13 +161,6 @@ R.useWith(addAll, [double, square]);
 };
 
 () => {
-    const a: boolean  = R.tryCatch<boolean>(R.prop("x"), R.F)({x: true}); // => true
-    const a1: boolean = R.tryCatch(R.prop<"x", true>("x"), R.F)({x: true}); // => true
-    const b: boolean = R.tryCatch<boolean>(R.prop("x"), R.F)(null);      // => false
-    const c: boolean = R.tryCatch<boolean>(R.and, R.F)(true, true);      // => true
-};
-
-() => {
     R.uniq([1, 1, 2, 1]); // => [1, 2]
     R.uniq([{}, {}]);     // => [{}, {}]
     R.uniq([1, "1"]);     // => [1, '1']
