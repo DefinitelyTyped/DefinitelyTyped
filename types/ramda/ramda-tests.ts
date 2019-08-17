ae8446f4c00ba825ca6fac3339150d430b560e9b
Mike Deverell <mike@onepeloton.com>
2019-08-17 14:23:50 -0400

    move 'unfold' tests to separate file

--- types/ramda/ramda-tests.ts
+++ types/ramda/ramda-tests.ts
@@ -105,16 +105,6 @@ R.useWith(addAll, [double, square]);
     slashify("a/"); // => 'a/'
 })();
 
-(() => {
-    function f(n: number): false | [number, number] {
-        return n > 50 ? false : [-n, n + 10];
-    }
-
-    const a = R.unfold(f, 10); // => [-10, -20, -30, -40, -50]
-    const b = R.unfold(f); // => [-10, -20, -30, -40, -50]
-    const c = b(10);
-});
-
 /*********************
  * List category
  */
