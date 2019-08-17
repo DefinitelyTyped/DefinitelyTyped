ed0b705a5b63d4ee6caaee5fae439e43c2171fb9
Mike Deverell <mike@onepeloton.com>
2019-08-17 14:46:00 -0400

    move 'zipObj' tests to separate file

--- types/ramda/ramda-tests.ts
+++ types/ramda/ramda-tests.ts
@@ -62,13 +62,6 @@ class F2 {
  * List category
  */
 () => {
-    R.zipObj(["a", "b", "c"], [1, 2, 3]); // => {a: 1, b: 2, c: 3}
-    R.zipObj(["a", "b", "c"])([1, 2, 3]); // => {a: 1, b: 2, c: 3}
-    R.zipObj([1, 2, 3], ['a', 'b', 'c']); // => {1: 'a', 2: 'b', 3: 'c'}
-    R.zipObj([1, 2, 3])(['a', 'b', 'c']); // => {1: 'a', 2: 'b', 3: 'c'}
-};
-
-() => {
     function f(x: number, y: string) {
         // ...
     }
