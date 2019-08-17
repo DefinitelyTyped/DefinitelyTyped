5bf85a66ebd50655fa5af2bfc1895fb24ba7c00c
Mike Deverell <mike@onepeloton.com>
2019-08-16 21:36:51 -0400

    move 'pathSatisfies' tests to separate file

--- types/ramda/ramda-tests.ts
+++ types/ramda/ramda-tests.ts
@@ -821,12 +821,6 @@ type Pair = KeyValuePair<string, number>;
     R.set(xyLens, 4, testObj);          // => {x: [{y: 4, z: 3}, {y: 4, z: 5}]}
 };
 () => {
-    const a: boolean = R.pathSatisfies((x: number) => x > 0, ["x"], {x: 1, y: 2}); // => true
-    const b: boolean = R.pathSatisfies((x: number) => x > 0, ["x"])({x: 1, y: 2}); // => true
-    const c: boolean = R.pathSatisfies((x: number) => x > 0)(["x"])({x: 1, y: 2}); // => true
-};
-
-() => {
     function isPositive(n: number) {
         return n > 0;
     }
