a3e4fc88277c517f952fdaa3f27af8028bab2183
Mike Deverell <mike@onepeloton.com>
2019-08-16 20:06:41 -0400

    move 'mapAccumRight' tests to separate file

--- types/ramda/ramda-tests.ts
+++ types/ramda/ramda-tests.ts
@@ -541,18 +541,6 @@ interface Obj {
 };
 
 () => {
-    const digits = ["1", "2", "3", "4"];
-
-    function append(a: string, b: string): [string, string] {
-        return [a + b, a + b];
-    }
-
-    R.mapAccumRight(append, "0", digits); // => ['04321', ['04321', '0432', '043', '04']]
-    R.mapAccumRight(append)("0", digits); // => ['04321', ['04321', '0432', '043', '04']]
-    R.mapAccumRight(append, "0")(digits); // => ['04321', ['04321', '0432', '043', '04']]
-};
-
-() => {
     const sampleList = ['a', 'b', 'c', 'd', 'e', 'f'];
 
     R.move<string>(0, 2, sampleList); // => ['b', 'c', 'a', 'd', 'e', 'f']
