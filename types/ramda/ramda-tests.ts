ea4fb19ca5956a89c7b5b8b3494e11637337a1b3
Mike Deverell <mike@onepeloton.com>
2019-08-16 21:44:04 -0400

    move 'pickBy' tests to separate file

--- types/ramda/ramda-tests.ts
+++ types/ramda/ramda-tests.ts
@@ -875,36 +875,8 @@ class Rectangle {
 };
 
 () => {
-    function isPositive(n: number) {
-        return n > 0;
-    }
-
-    const a1 = R.pickBy(isPositive, {a: 1, b: 2, c: -1, d: 0, e: 5}); // => {a: 1, b: 2, e: 5}
-    function containsBackground(val: any) {
-        return val.bgcolor;
-    }
-
-    const colors = {1: {color: "read"}, 2: {color: "black", bgcolor: "yellow"}};
-    R.pickBy(containsBackground, colors); // => {2: {color: 'black', bgcolor: 'yellow'}}
-
-    function isUpperCase(val: number, key: string) {
-        return key.toUpperCase() === key;
-    }
-
-    R.pickBy(isUpperCase, {a: 1, b: 2, A: 3, B: 4}); // => {A: 3, B: 4}
-};
-
-() => {
     R.omit(["a", "d"], {a: 1, b: 2, c: 3, d: 4}); // => {b: 2, c: 3}
     R.omit(["a", "d"])({a: 1, b: 2, c: 3, d: 4}); // => {b: 2, c: 3}
-};
-
-() => {
-    function isUpperCase(val: number, key: string) {
-        return key.toUpperCase() === key;
-    }
-
-    R.pickBy(isUpperCase, {a: 1, b: 2, A: 3, B: 4}); // => {A: 3, B: 4}
 };
 
 () => {
