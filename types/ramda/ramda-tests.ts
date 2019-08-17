388845bbf7c8a0fdffeaad6610a377eb6fb1333b
Mike Deverell <mike@onepeloton.com>
2019-08-16 22:52:09 -0400

    move 'repeat' tests to separate file

--- types/ramda/ramda-tests.ts
+++ types/ramda/ramda-tests.ts
@@ -175,13 +175,6 @@ R.times(i, 5);
 };
 
 () => {
-    R.repeat("hi", 5); // => ['hi', 'hi', 'hi', 'hi', 'hi']
-    const obj          = {};
-    const repeatedObjs = R.repeat(obj, 5); // => [{}, {}, {}, {}, {}]
-    repeatedObjs[0] === repeatedObjs[1]; // => true
-};
-
-() => {
     R.reverse([1, 2, 3]);  // => [3, 2, 1]
     R.reverse([1, 2]);     // => [2, 1]
     R.reverse([1]);        // => [1]
