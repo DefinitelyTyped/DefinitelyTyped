efc3f47f31d7ce32263d703a69ba546753b26507
Mike Deverell <mike@onepeloton.com>
2019-08-17 07:45:43 -0400

    move 'sortBy' tests to separate file

--- types/ramda/ramda-tests.ts
+++ types/ramda/ramda-tests.ts
@@ -493,42 +493,6 @@ class Rectangle {
  * Relation category
  */
 () => {
-    const sortByAgeDescending = R.sortBy(R.compose<{}, number, number>(R.negate, R.prop("age")));
-    const alice               = {
-        name: "ALICE",
-        age : 101
-    };
-    const bob                 = {
-        name: "Bob",
-        age : -10
-    };
-    const clara               = {
-        name: "clara",
-        age : 314.159
-    };
-    const people              = [clara, bob, alice];
-    sortByAgeDescending(people); // => [alice, bob, clara]
-};
-
-() => {
-    const sortByNameCaseInsensitive = R.sortBy(R.compose<Record<'name', string>, string, string>(R.toLower, R.prop("name")));
-    const alice                     = {
-        name: "ALICE",
-        age : 101
-    };
-    const bob                       = {
-        name: "Bob",
-        age : -10
-    };
-    const clara                     = {
-        name: "clara",
-        age : 314.159
-    };
-    const people                    = [clara, bob, alice];
-    sortByNameCaseInsensitive(people); // => [alice, bob, clara]
-};
-
-() => {
     const a: number[][] = R.splitAt(1, [1, 2, 3]);        // => [[1], [2, 3]]
     const b: number[][] = R.splitAt(1)([1, 2, 3]);        // => [[1], [2, 3]]
     const c: string[]   = R.splitAt(5, "hello world");      // => ['hello', ' world']
