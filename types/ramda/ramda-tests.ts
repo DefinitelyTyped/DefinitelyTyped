927935a686fac1f093634386a860f1dde63ca624
Mike Deverell <mike@onepeloton.com>
2019-08-17 15:13:30 -0400

    move 'why' tests to separate file

--- types/ramda/ramda-tests.ts
+++ types/ramda/ramda-tests.ts
@@ -38,31 +38,3 @@ import * as R from "ramda";
   R.subtract(R.__, 5)(17); // 12
   R.subtract(R.__)(5, 17); // 12
 };
-
-/*****************************************************************
- * Logic category
- */
-
-class Why {
-    val: boolean;
-
-    constructor(val: boolean) {
-        this.val = val;
-    }
-
-    or(x: boolean) {
-        return this.val && x;
-    }
-}
-() => {
-    const x0: boolean        = R.or(false, true); // => false
-    const x1: number | any[] = R.or(0, []); // => []
-    const x2: number | any[] = R.or(0)([]); // => []
-    const x3: string | null  = R.or(null, ""); // => ''
-
-    const why = new Why(true);
-    why.or(true);
-    const x4: Why | boolean = R.or(why, false); // false
-};
-
-// Curry tests
