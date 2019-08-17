46665f4a57adfa3a8caf9416c55617681ae9b2f6
Mike Deverell <mike@onepeloton.com>
2019-08-16 20:50:48 -0400

    move 'minBy' tests to separate file

--- types/ramda/ramda-tests.ts
+++ types/ramda/ramda-tests.ts
@@ -1220,25 +1220,6 @@ type Pair = KeyValuePair<string, number>;
 };
 
 () => {
-    function cmp(obj: { x: R.Ord }) {
-        return obj.x;
-    }
-
-    const a = {x: 1};
-    const b = {x: 2};
-    const c = {x: 3};
-    const d = {x: "a"};
-    const e = {x: "z"};
-    const f = {x: new Date(0)};
-    const g = {x: new Date(60 * 1000)};
-    R.minBy(cmp, a, b); // => {x: 1}
-    R.minBy(cmp)(a, b); // => {x: 1}
-    R.minBy(cmp)(a)(c);
-    R.minBy(cmp, d, e);
-    R.minBy(cmp)(f)(g);
-};
-
-() => {
     R.modulo(17, 3); // => 2
     // JS behavior:
     R.modulo(-17, 3); // => -2
