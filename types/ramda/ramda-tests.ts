8900236859c89259e276b2ba2a71446868dde386
Mike Deverell <mike@onepeloton.com>
2019-08-17 09:34:15 -0400

    move 'sum' tests to separate file

--- types/ramda/ramda-tests.ts
+++ types/ramda/ramda-tests.ts
@@ -427,10 +427,6 @@ R.times(i, 5);
  * Relation category
  */
 () => {
-    R.sum([2, 4, 6, 8, 100, 1]); // => 121
-};
-
-() => {
     const a: number[] = R.symmetricDifference([1, 2, 3, 4], [7, 6, 5, 4, 3]); // => [1,2,7,6,5]
     const b: number[] = R.symmetricDifference([7, 6, 5, 4, 3])([1, 2, 3, 4]); // => [7,6,5,1,2]
 };
