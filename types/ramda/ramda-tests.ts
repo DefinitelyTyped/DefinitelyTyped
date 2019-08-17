a3dddb1a511e0975268b786aa6ea9f5dc00d8b0d
Mike Deverell <mike@onepeloton.com>
2019-08-17 09:46:22 -0400

    move 'test' tests to separate file

--- types/ramda/ramda-tests.ts
+++ types/ramda/ramda-tests.ts
@@ -166,11 +166,6 @@ R.times(i, 5);
 };
 
 () => {
-    const a: boolean = R.test(/^x/, "xyz"); // => true
-    const b: boolean = R.test(/^y/)("xyz"); // => false
-};
-
-() => {
     const x: number = R.thunkify(R.identity)(42)();
     const y: number = R.thunkify((a: number, b: number) => a + b)(25, 17)();
     const z: number = R.thunkify((a: number, b: number) => a + b)(25)(17)();
