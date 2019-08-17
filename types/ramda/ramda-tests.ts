fcc57bd604549d0dc22f7c0bf53b4d4395c5e112
Mike Deverell <mike@onepeloton.com>
2019-08-16 20:04:53 -0400

    move 'mapAccum' tests to separate file

--- types/ramda/ramda-tests.ts
+++ types/ramda/ramda-tests.ts
@@ -547,18 +547,6 @@ interface Obj {
         return [a + b, a + b];
     }
 
-    R.mapAccum(append, "0", digits); // => ['01234', ['01', '012', '0123', '01234']]
-    R.mapAccum(append)("0", digits); // => ['01234', ['01', '012', '0123', '01234']]
-    R.mapAccum(append, "0")(digits); // => ['01234', ['01', '012', '0123', '01234']]
-};
-
-() => {
-    const digits = ["1", "2", "3", "4"];
-
-    function append(a: string, b: string): [string, string] {
-        return [a + b, a + b];
-    }
-
     R.mapAccumRight(append, "0", digits); // => ['04321', ['04321', '0432', '043', '04']]
     R.mapAccumRight(append)("0", digits); // => ['04321', ['04321', '0432', '043', '04']]
     R.mapAccumRight(append, "0")(digits); // => ['04321', ['04321', '0432', '043', '04']]
