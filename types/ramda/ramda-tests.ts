17c13331abca28bea4411a42c2196eb087c8d4df
Mike Deverell <mike@onepeloton.com>
2019-08-16 20:45:26 -0400

    move 'mergeRight' tests to separate file

--- types/ramda/ramda-tests.ts
+++ types/ramda/ramda-tests.ts
@@ -969,14 +969,6 @@ class Rectangle {
 };
 
 () => {
-    R.mergeRight({ name: 'fred', age: 10 }, { age: 40 });
-    // => { 'name': 'fred', 'age': 40 }
-
-    const withDefaults = R.mergeRight({x: 0, y: 0});
-    withDefaults({y: 2}); // => {x: 0, y: 2}
-};
-
-() => {
     const a = R.mergeWith(R.concat,
         {a: true, values: [10, 20]},
         {b: true, values: [15, 35]});
