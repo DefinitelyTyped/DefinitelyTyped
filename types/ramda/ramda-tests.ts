8cc3b0ebc309ff5a133d35498a48d1a2b167c992
Mike Deverell <mike@onepeloton.com>
2019-08-16 21:26:09 -0400

    move 'partition' tests to separate file

--- types/ramda/ramda-tests.ts
+++ types/ramda/ramda-tests.ts
@@ -432,13 +432,6 @@ interface Obj {
 };
 
 () => {
-    R.partition(R.contains("s"), ["sss", "ttt", "foo", "bars"]);
-    R.partition(R.contains("s"))(["sss", "ttt", "foo", "bars"]);
-    R.partition((x: number) => x > 2, [1, 2, 3, 4]);
-    R.partition((x: number) => x > 2)([1, 2, 3, 4]);
-};
-
-() => {
     const a = R.pluck("a")([{a: 1}, {a: 2}]); // => [1, 2]
     const b = R.pluck(0)([[1, 2], [3, 4]]);   // => [1, 3]
 };
