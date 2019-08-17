f50836224a9a7da1e8779d1c8b398f4c2d5d0cb6
Mike Deverell <mike@onepeloton.com>
2019-08-16 20:48:03 -0400

    move 'mergeWithKey' tests to separate file

--- types/ramda/ramda-tests.ts
+++ types/ramda/ramda-tests.ts
@@ -969,15 +969,6 @@ class Rectangle {
 };
 
 () => {
-    const concatValues = (k: string, l: string, r: string) => k === "values" ? R.concat(l, r) : r;
-    R.mergeWithKey(concatValues,
-        {a: true, thing: "foo", values: [10, 20]},
-        {b: true, thing: "bar", values: [15, 35]});
-    const merge = R.mergeWithKey(concatValues);
-    merge({a: true, thing: "foo", values: [10, 20]}, {b: true, thing: "bar", values: [15, 35]});
-};
-
-() => {
     const orValue  = 11;
     const orValueStr = "str";
     const testPath = ["x", 0, "y"];
