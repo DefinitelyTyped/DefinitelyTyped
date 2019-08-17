21dce1490b92c707fb0f5f85f179dfb016169a23
Mike Deverell <mike@onepeloton.com>
2019-08-16 21:42:46 -0400

    move 'pickAll' tests to separate file

--- types/ramda/ramda-tests.ts
+++ types/ramda/ramda-tests.ts
@@ -841,13 +841,6 @@ type Pair = KeyValuePair<string, number>;
 };
 
 () => {
-    R.pickAll(["a", "d"], {a: 1, b: 2, c: 3, d: 4}); // => {a: 1, d: 4}
-    R.pickAll(["a", "d"])({a: 1, b: 2, c: 3, d: 4}); // => {a: 1, d: 4}
-    R.pickAll(["a", "e", "f"], {a: 1, b: 2, c: 3, d: 4}); // => {a: 1, e: undefined, f: undefined}
-    R.pickAll(["a", "e", "f"])({a: 1, b: 2, c: 3, d: 4}); // => {a: 1, e: undefined, f: undefined}
-};
-
-() => {
     function isUpperCase(val: number, key: string) {
         return key.toUpperCase() === key;
     }
