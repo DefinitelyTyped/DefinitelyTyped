cb5d2887d14a2d62fab9f3af094704b02adf33e6
Mike Deverell <mike@onepeloton.com>
2019-08-16 22:06:03 -0400

    move 'product' tests to separate file

--- types/ramda/ramda-tests.ts
+++ types/ramda/ramda-tests.ts
@@ -779,10 +779,6 @@ type Pair = KeyValuePair<string, number>;
 };
 
 () => {
-    R.product([2, 4, 6, 8, 100, 1]); // => 38400
-};
-
-() => {
     R.subtract(10, 8); // => 2
 
     const complementaryAngle = R.subtract(90);
