3fd7ff502dc70c2c4b1f1e4e0bd5b6af8039d75d
Mike Deverell <mike@onepeloton.com>
2019-08-17 13:58:08 -0400

    move 'toPairsIn' tests to separate file

--- types/ramda/ramda-tests.ts
+++ types/ramda/ramda-tests.ts
@@ -336,12 +336,6 @@ class Rectangle {
 };
 
 () => {
-    const f    = new F();
-    const a1 = R.toPairsIn(f); // => [['x','X'], ['y','Y']]
-    const a2 = R.toPairsIn<string>(f); // => [['x','X'], ['y','Y']]
-};
-
-() => {
     interface A {
         a: string;
         b: string;
