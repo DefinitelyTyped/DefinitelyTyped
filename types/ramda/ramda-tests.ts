14055c0c6f53ef7670c8622c746c4f1575c3cd0d
Mike Deverell <mike@onepeloton.com>
2019-08-16 22:52:55 -0400

    move 'replace' tests to separate file

--- types/ramda/ramda-tests.ts
+++ types/ramda/ramda-tests.ts
@@ -582,26 +582,6 @@ R.times(i, 5);
     R.symmetricDifferenceWith(eqL)(l1, l2); // => ['dddd', 'c']
 };
 
-/*****************************************************************
- * String category
- */
-() => {
-    R.replace("foo", "bar", "foo foo foo"); // => 'bar foo foo'
-    R.replace("foo", "bar")("foo foo foo"); // => 'bar foo foo'
-    R.replace("foo")("bar")("foo foo foo"); // => 'bar foo foo'
-    R.replace(/foo/, "bar", "foo foo foo"); // => 'bar foo foo'
-
-    // Use the "g" (global) flag to replace all occurrences:
-    R.replace(/foo/g, "bar", "foo foo foo"); // => 'bar bar bar'
-    R.replace(/foo/g, "bar")("foo foo foo"); // => 'bar bar bar'
-    R.replace(/foo/g)("bar")("foo foo foo"); // => 'bar bar bar'
-
-    // Using a function as the replacement value
-    R.replace(/([cfk])oo/g, (match, p1, offset) => `${p1}-${offset}`, "coo foo koo"); // => 'c0oo f4oo k8oo'
-    R.replace(/([cfk])oo/g, (match, p1, offset) => `${p1}-${offset}`)("coo foo koo"); // => 'c0oo f4oo k8oo'
-    R.replace(/([cfk])oo/g)((match, p1, offset) => `${p1}-${offset}`) ("coo foo koo"); // => 'c0oo f4oo k8oo'
-};
-
 () => {
     interface A {
         a: number;
