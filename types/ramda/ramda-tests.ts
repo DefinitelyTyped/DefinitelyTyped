2e099ee96e09b7f0479e1c4010bdc931d71569e4
Mike Deverell <mike@onepeloton.com>
2019-08-16 20:59:11 -0400

    re-remove 'move' and 'none' tests from ramda-tests.ts

--- types/ramda/ramda-tests.ts
+++ types/ramda/ramda-tests.ts
@@ -471,29 +471,6 @@ interface Obj {
 };
 
 () => {
-    const sampleList = ['a', 'b', 'c', 'd', 'e', 'f'];
-
-    R.move<string>(0, 2, sampleList); // => ['b', 'c', 'a', 'd', 'e', 'f']
-    R.move<string>(-1, 0, sampleList); // => ['f', 'a', 'b', 'c', 'd', 'e'] list rotation
-
-    const moveCurried1 = R.move(0, 2);
-    moveCurried1<string>(sampleList); // => ['b', 'c', 'a', 'd', 'e', 'f']
-
-    const moveCurried2 = R.move(0);
-    moveCurried2<string>(2, sampleList); // => ['b', 'c', 'a', 'd', 'e', 'f']
-
-    const moveCurried3 = R.move(0);
-    const moveCurried4 = moveCurried3(2);
-    moveCurried4<string>(sampleList); // => ['b', 'c', 'a', 'd', 'e', 'f']
-};
-
-() => {
-    R.none(Number.isNaN, [1, 2, 3]); // => true
-    R.none(Number.isNaN, [1, 2, 3, NaN]); // => false
-    R.none(Number.isNaN, [1, 2, 3, NaN]); // => false
-};
-
-() => {
     const list = ["foo", "bar", "baz", "quux"];
     R.nth(1, list); // => 'bar'
     R.nth(-1, list); // => 'quux'
