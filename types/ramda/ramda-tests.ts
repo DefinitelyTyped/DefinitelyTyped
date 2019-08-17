51c1a136845d0ac41371eaec4e49646806bbaa68
Mike Deverell <mike@onepeloton.com>
2019-08-17 14:36:26 -0400

    move 'when' tests to separate file

--- types/ramda/ramda-tests.ts
+++ types/ramda/ramda-tests.ts
@@ -50,15 +50,6 @@ class F2 {
   R.subtract(R.__)(5, 17); // 12
 };
 
-() => {
-    const truncate = R.when(
-        R.propSatisfies(R.flip(R.gt)(10), "length"),
-        R.pipe<string, string, string[], string>(R.take(10), R.append("…") as (wrong: any) => string[], R.join(""))
-    );
-    const a: string = truncate("12345");         // => '12345'
-    const b: string = truncate("0123456789ABC"); // => '0123456789…'
-};
-
 (() => {
     const addOneOnce = R.once((x: number) => x + 1);
     addOneOnce(10); // => 11
