cdbec7c1a0fc2b7065e0622c7e138c985f920a05
Mike Deverell <mike@onepeloton.com>
2019-08-17 09:36:35 -0400

    move 'symmetricDifferenceWith' tests to separate file

--- types/ramda/ramda-tests.ts
+++ types/ramda/ramda-tests.ts
@@ -525,23 +525,6 @@ class Rectangle {
     minus5(17); // => 12
 };
 
-() => {
-    const eqA = R.eqBy(R.prop("a"));
-    const l1  = [{a: 1}, {a: 2}, {a: 3}, {a: 4}];
-    const l2  = [{a: 3}, {a: 4}, {a: 5}, {a: 6}];
-    R.symmetricDifferenceWith(eqA, l1, l2); // => [{a: 1}, {a: 2}, {a: 5}, {a: 6}]
-    R.symmetricDifferenceWith(eqA)(l1, l2); // => [{a: 1}, {a: 2}, {a: 5}, {a: 6}]
-    // const c: (a: any[]) => any[] = R.symmetricDifferenceWith(eqA)(l1); // => [{a: 1}, {a: 2}, {a: 5}, {a: 6}]
-};
-
-() => {
-    const eqL = R.eqBy<string, number>(s => s.length);
-    const l1 = ['bb', 'ccc', 'dddd'];
-    const l2 = ['aaa', 'bb', 'c'];
-    R.symmetricDifferenceWith(eqL, l1, l2); // => ['dddd', 'c']
-    R.symmetricDifferenceWith(eqL)(l1, l2); // => ['dddd', 'c']
-};
-
 /*****************************************************************
  * Logic category
  */
