2b74e51f135faccbf4d0ec709e1a85ead6b0d376
Mike Deverell <mike@onepeloton.com>
2019-08-16 21:40:42 -0400

    move 'pick' tests to separate file

--- types/ramda/ramda-tests.ts
+++ types/ramda/ramda-tests.ts
@@ -841,13 +841,6 @@ type Pair = KeyValuePair<string, number>;
 };
 
 () => {
-    const a1 = R.pick(["a", "d"], {a: 1, b: 2, c: 3, d: 4}); // => {a: 1, d: 4}
-    const a2 = R.pick(["a", "e", "f"], {a: 1, b: 2, c: 3, d: 4}); // => {a: 1}
-    const a3 = R.pick(["a", "e", "f"])({a: 1, b: 2, c: 3, d: 4}); // => {a: 1}
-    const a4 = R.pick(["a", "e", "f"], [1, 2, 3, 4]); // => {a: 1}
-};
-
-() => {
     R.pickAll(["a", "d"], {a: 1, b: 2, c: 3, d: 4}); // => {a: 1, d: 4}
     R.pickAll(["a", "d"])({a: 1, b: 2, c: 3, d: 4}); // => {a: 1, d: 4}
     R.pickAll(["a", "e", "f"], {a: 1, b: 2, c: 3, d: 4}); // => {a: 1, e: undefined, f: undefined}
