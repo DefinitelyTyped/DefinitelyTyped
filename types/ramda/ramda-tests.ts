4c692c7b054dddcb1151a6209e38f52240dff8b4
Mike Deverell <mike@onepeloton.com>
2019-08-17 14:30:42 -0400

    move 'values' tests to separate file

--- types/ramda/ramda-tests.ts
+++ types/ramda/ramda-tests.ts
@@ -193,22 +193,6 @@ class Rectangle {
 };
 
 () => {
-    interface A {
-        a: string;
-        b: string;
-    }
-    const a1: A = { a: 'something', b: 'else' };
-    const v1 = R.values(a1);
-
-    const a = R.values({a: 1, b: 2, c: 3}); // => [1, 2, 3] (number[])
-    const addition = a[0] + a[1];
-
-    const b = R.values({a: 1, b: 'something'}); // b = (string|number)[]
-    const c = R.values({1: 3});
-    // const d = R.values('something');
-};
-
-() => {
     const f   = new F();
     const a = R.valuesIn(f); // => ['X', 'Y']
 };
