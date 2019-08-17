1790da89ac45176b7fba06dd0c29be6e5247ffef
Mike Deverell <mike@onepeloton.com>
2019-08-17 14:35:37 -0400

    move 'view' tests to separate file

--- types/ramda/ramda-tests.ts
+++ types/ramda/ramda-tests.ts
@@ -78,11 +78,6 @@ class F2 {
  * List category
  */
 () => {
-    const headLens = R.lensIndex(0);
-    R.view(headLens, ["a", "b", "c"]);            // => 'a'
-};
-
-() => {
     R.xprod([1, 2], ["a", "b"]); // => [[1, 'a'], [1, 'b'], [2, 'a'], [2, 'b']]
     R.xprod([1, 2])(["a", "b"]); // => [[1, 'a'], [1, 'b'], [2, 'a'], [2, 'b']]
 };
@@ -143,29 +138,6 @@ class Rectangle {
 () => {
     const square = new Rectangle(2, 2);
     R.flip(R.hasIn)(square)("area");  // => true
-};
-
-() => {
-    const xLens = R.lens(R.prop("x"), R.assoc("x"));
-    R.view(xLens, {x: 1, y: 2});            // => 1
-    R.view(xLens)({x: 1, y: 2});            // => 1
-};
-
-() => {
-    const headLens = R.lensIndex(0);
-    R.view(headLens, ["a", "b", "c"]);            // => 'a'
-};
-
-() => {
-    const xLens = R.lensProp("x");
-    R.view(xLens, {x: 1, y: 2});            // => 1
-};
-
-() => {
-    const xyLens  = R.lensPath(["x", 0, "y"]);
-    const testObj = {x: [{y: 2, z: 3}, {y: 4, z: 5}]};
-
-    R.view(xyLens, testObj);            // => 2
 };
 
 () => {
