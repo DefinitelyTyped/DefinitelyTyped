0df1736fd65a6dab772bcbd31c447a92f8aae45e
Mike Deverell <mike@onepeloton.com>
2019-08-17 14:10:35 -0400

    move 'transpose' tests to separate file

--- types/ramda/ramda-tests.ts
+++ types/ramda/ramda-tests.ts
@@ -161,12 +161,6 @@ R.useWith(addAll, [double, square]);
 };
 
 () => {
-    const a: Array<Array<number | string>> = R.transpose<number | string>([[1, "a"], [2, "b"], [3, "c"]]); // => [[1, 2, 3], ['a', 'b', 'c']]
-    const b: Array<Array<number | string>> = R.transpose<number | string>([[1, 2, 3], ["a", "b", "c"]]); // => [[1, 'a'], [2, 'b'], [3, 'c']]
-    const c: number[][]            = R.transpose([[10, 11], [20], [], [30, 31, 32]]); // => [[10, 20, 30], [11, 31], [32]]
-};
-
-() => {
     const of = Array.of;
     const fn = (x: number) => Array.of(x + 1);
     const list = [1, 2, 3];
