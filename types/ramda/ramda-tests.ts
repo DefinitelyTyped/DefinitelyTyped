04890e63e8b13846531b0b1d57b7d829fdbed768
Mike Deverell <mike@onepeloton.com>
2019-08-16 20:29:18 -0400

    move 'median' tests to separate file

--- types/ramda/ramda-tests.ts
+++ types/ramda/ramda-tests.ts
@@ -1384,11 +1384,6 @@ class Rectangle {
 };
 
 () => {
-    const a: number = R.median([7, 2, 10, 9]); // => 8
-    const b: number = R.median([]); // => NaN
-};
-
-() => {
     const x: number = R.min(9, 3); // => 3
     const y: string = R.min("a", "z"); // => 'a'
 };
