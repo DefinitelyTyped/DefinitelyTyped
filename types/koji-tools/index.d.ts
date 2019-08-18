944a50228d6b0c0bea71404864f2e2ffffdad533
Jeff <bdjeffyp@users.noreply.github.com>
2019-08-18 15:18:34 -0700

    Prettier

--- types/koji-tools/index.d.ts
+++ types/koji-tools/index.d.ts
@@ -8,22 +8,22 @@ export = Koji;
 export as namespace Koji;
 
 declare namespace Koji {
-  // index.js methods
-  /**
-   * Sets up Koji.config parameters for each client and handles communication
-   * between the Koji live preview iframe and your app.
-   * @param options ???
-   */
-  function pageLoad(options?: {}): void;
+    // index.js methods
+    /**
+     * Sets up Koji.config parameters for each client and handles communication
+     * between the Koji live preview iframe and your app.
+     * @param options ???
+     */
+    function pageLoad(options?: {}): void;
 
-  function getConfig(): any;
+    function getConfig(): any;
 
-  /**
-   * Registers a callback on a Koji event.
-   * @param event name of the event being called
-   * @param callback method to execute when the event occurs
-   */
-  function on(event: string, callback: () => void): void;
+    /**
+     * Registers a callback on a Koji event.
+     * @param event name of the event being called
+     * @param callback method to execute when the event occurs
+     */
+    function on(event: string, callback: () => void): void;
 
-  function callEvent(event: string, params?: []): void;
+    function callEvent(event: string, params?: []): void;
 }
