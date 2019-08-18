ae55441900f52a653606aa1a78b8775a04ff2353
Nico Flaig <nflaig@protonmail.com>
2019-08-18 22:29:55 +0200

    Add joi-password-complexity type definitions

---
+++ types/joi-password-complexity/index.d.ts
@@ -0,0 +1,23 @@
+// Type definitions for joi-password-complexity 3.1
+// Project: https://github.com/kamronbatman/joi-password-complexity
+// Definitions by: Nico Flaig <https://github.com/nflaig>
+// Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
+// TypeScript Version: 3.5
+
+declare class PasswordComplexity {
+    constructor(options?: PasswordComplexity.ComplexityOptions);
+}
+
+declare namespace PasswordComplexity {
+    interface ComplexityOptions {
+        min: number;
+        max: number;
+        lowerCase: number;
+        upperCase: number;
+        numeric: number;
+        symbol: number;
+        requirementCount: number;
+    }
+}
+
+export = PasswordComplexity;
