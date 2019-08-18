559f79337a5719948faeb0315af7ec14bba1553e
Brendan Early <mymindstorm1@gmail.com>
2019-08-18 09:51:41 -0500

    add missing types to argon2-browser

--- types/argon2-browser/index.d.ts
+++ types/argon2-browser/index.d.ts
@@ -1,21 +1,26 @@
 // Type definitions for argon2-browser 1.1
 // Project: https://github.com/antelle/argon2-browser#readme
 // Definitions by: Ivan Gabriele <https://github.com/ivangabriele>
+//                 Brendan Early <https://github.com/mymindstorm>
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
 // TypeScript Version: 2.1
 
-export namespace argon2 {
+export = argon2;
+export as namespace argon2;
+
+declare namespace argon2 {
+    // Argon2Error is provided on promise rejection
     function hash(options: Argon2BrowserHashOptions): Promise<Argon2BrowserHashResult>;
 
     interface Argon2BrowserHashOptions {
-        distPath?: string;
-        hashLen?: number;
-        mem?: number;
-        parallelism?: number;
-        pass: string;
-        salt: string;
+        pass: string | Uint8Array;
+        salt: string | Uint8Array;
         time?: number;
+        mem?: number;
+        hashLen?: number;
+        parallelism?: number;
         type?: ArgonType;
+        distPath?: string;
     }
 
     interface Argon2BrowserHashResult {
@@ -24,8 +29,23 @@ export namespace argon2 {
         hashHex: string;
     }
 
+    // Argon2Error provided on promise rejection
+    function verify(options: Argon2VerifyOptions): Promise<undefined>;
+
+    interface Argon2VerifyOptions {
+        pass: string;
+        encoded: string | Uint8Array;
+        type?: ArgonType;
+    }
+
+    interface Argon2Error {
+        message: string;
+        code: number;
+    }
+
     enum ArgonType {
         Argon2d = 0,
-        Argon2i = 1
+        Argon2i = 1,
+        Argon2id = 2
     }
 }
