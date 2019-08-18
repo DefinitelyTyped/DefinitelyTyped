1a364e5ec679858f64915aa808aaa3e63d670a8c
Oliver Joseph Ash <oliverjash@gmail.com>
2019-08-18 17:30:44 +0100

    Add tests

--- types/express-serve-static-core/express-serve-static-core-tests.ts
+++ types/express-serve-static-core/express-serve-static-core-tests.ts
@@ -5,3 +5,14 @@ app.listen(3000);
 app.listen(3000, (err: any) => {
     // no-op error callback
 });
+
+app.get('/:foo', (req, res) => {
+    req.params.foo; // $ExpectType string
+    req.params[0]; // $ExpectType string
+});
+
+// Params can used as an array
+app.get<express.ParamsArray>('/*', (req, res) => {
+    req.params[0]; // $ExpectType string
+    req.params.length; // $ExpectType number
+});
