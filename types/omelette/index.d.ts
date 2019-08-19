12ac4367c4d0ae625fb49710c371d81614027213
Kamontat Chantrachirathumrong <github@kamontat.net>
2019-08-19 12:12:09 +0700

    support ts2.7 more

--- types/omelette/index.d.ts
+++ types/omelette/index.d.ts
@@ -2,11 +2,7 @@
 // Project: https://github.com/f/omelette
 // Definitions by: Kamontat Chantrachirathumrong <https://github.com/kamontat>
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
-
-/*~ If this module is a UMD module that exposes a global variable 'myLib' when
- *~ loaded outside a module loader environment, declare that global here.
- *~ Otherwise, delete this declaration.
- */
+// TypeScript Version: 2.7
 
 declare var omelette: omelette.Omelette;
 
