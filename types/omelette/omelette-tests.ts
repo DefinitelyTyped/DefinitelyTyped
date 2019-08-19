04dfd86276e29ed054b4b994fac7d18223d512ed
Kamontat Chantrachirathumrong <github@kamontat.net>
2019-08-19 12:05:04 +0700

    Update omelette-tests.ts

--- types/omelette/omelette-tests.ts
+++ types/omelette/omelette-tests.ts
@@ -1,13 +1,13 @@
-import omelette, { Callback } from 'omelette';
+import omelette, { Callback } from "omelette";
 
 // Prepare environment value
 
-declare var process: any
-declare var console: any
-declare var fs: any
-declare var require: any
-declare var getFromRemote: any
-declare var fetch: any
+declare var process: any;
+declare var console: any;
+declare var fs: any;
+declare var require: any;
+declare var getFromRemote: any;
+declare var fetch: any;
 
 // END
 
@@ -16,18 +16,18 @@ declare var fetch: any
 // ------------------------------ //
 
 // simple initial omelette object: with array choices
-omelette`github ${['pull', 'push']} ${['origin', 'upstream']} ${['master', 'develop']}`.init()
+omelette`github ${["pull", "push"]} ${["origin", "upstream"]} ${["master", "develop"]}`.init();
 
 const firstArgument: Callback = ({ reply }) => {
-    reply(['beautiful', 'cruel', 'far'])
-}
+  reply(["beautiful", "cruel", "far"]);
+};
 
 const planet: Callback = ({ reply }) => {
-    reply(['world', 'mars', 'pluto'])
-}
+  reply(["world", "mars", "pluto"]);
+};
 
 // simple initial omelette object: with function callback
-omelette`hello|hi ${firstArgument} ${planet}`.init()
+omelette`hello|hi ${firstArgument} ${planet}`.init();
 
 // ------------------------------ //
 // SIMPLE EVENT BASED APIS        //
@@ -37,57 +37,51 @@ omelette`hello|hi ${firstArgument} ${planet}`.init()
 const completion = omelette(`githubber|gh <action> <user> <repo>`);
 
 // Bind events for every template part.
-completion.on('action', ({ reply }) => {
-    reply(['clone', 'update', 'push'])
-})
+completion.on("action", ({ reply }) => {
+  reply(["clone", "update", "push"]);
+});
 
-completion.on('user', ({ reply }) => {
-    reply(fs.readdirSync('/Users/'))
-})
+completion.on("user", ({ reply }) => {
+  reply(fs.readdirSync("/Users/"));
+});
 
-completion.on('repo', ({ before, reply }) => {
-    reply([
-        `http://github.com/${before}/helloworld`,
-        `http://github.com/${before}/blabla`
-    ])
-})
+completion.on("repo", ({ before, reply }) => {
+  reply([`http://github.com/${before}/helloworld`, `http://github.com/${before}/blabla`]);
+});
 
 // Initialize the omelette.
-completion.init()
+completion.init();
 
 // If you want to have a setup feature, you can use `omeletteInstance.setupShellInitFile()` function.
-if (~process.argv.indexOf('--setup')) {
-    completion.setupShellInitFile()
+if (~process.argv.indexOf("--setup")) {
+  completion.setupShellInitFile();
 
-    completion.setupShellInitFile("~/custom/.bashrc") // OR
+  completion.setupShellInitFile("~/custom/.bashrc"); // OR
 }
 
 // Similarly, if you want to tear down autocompletion, use `omeletteInstance.cleanupShellInitFile()`
-if (~process.argv.indexOf('--cleanup')) {
-    completion.cleanupShellInitFile()
+if (~process.argv.indexOf("--cleanup")) {
+  completion.cleanupShellInitFile();
 }
 
 // Rest is yours
-console.log("Your program's default workflow.")
-console.log(process.argv)
+console.log("Your program's default workflow.");
+console.log(process.argv);
 
 // ------------------------------ //
 // TEMPLATE LITERAL API FOR ES6   //
 // ------------------------------ //
 
 // Just pass a template literal to use super easy API.
-omelette`hello ${['cruel', 'nice']} ${['world', 'mars']}`.init()
+omelette`hello ${["cruel", "nice"]} ${["world", "mars"]}`.init();
 
 // Write your CLI template.
 omelette`
   githubber|gh
-  ${['clone', 'update', 'push']}
-  ${() => fs.readdirSync('/Users/')}
-  ${({ before }) => [
-        `http://github.com/${before}/helloworld`,
-        `http://github.com/${before}/blabla`,
-    ]}
-`.init()
+  ${["clone", "update", "push"]}
+  ${() => fs.readdirSync("/Users/")}
+  ${({ before }) => [`http://github.com/${before}/helloworld`, `http://github.com/${before}/blabla`]}
+`.init();
 
 // ------------------------------ //
 // ADVANCED TEMPLATE LITERAL      //
@@ -95,44 +89,49 @@ omelette`
 
 omelette`
   githubber|gh
-      ${['pull', 'push', 'star'] /* Direct command list */}
-      ${require('some/other/commands') /* Import from another file */}
-      ${getFromRemote('http://api.example.com/commands') /* Remote call at the beginning */}
-      ${({ reply }) => fetch('http://api.example.com/lazy-commands').then(reply) /* Fetch when argument <tab>bed */}
+      ${["pull", "push", "star"] /* Direct command list */}
+      ${require("some/other/commands") /* Import from another file */}
+      ${getFromRemote("http://api.example.com/commands") /* Remote call at the beginning */}
+      ${({ reply }) => fetch("http://api.example.com/lazy-commands").then(reply) /* Fetch when argument <tab>bed */}
       ${() => fs.readdirSync("/Users/") /* Access filesystem via Node */}
-      ${({ before }) => [ /* Use parameters like `before`, `line`, `fragment` or `reply` */
+      ${({ before }) => [
+        /* Use parameters like `before`, `line`, `fragment` or `reply` */
         `${before}/helloworld`,
-        `${before}/blabla`
-    ]}
-  `.init()
+        `${before}/blabla`,
+      ]}
+  `.init();
 
 // No extra configuration required.
 
-console.log("Your program's default workflow.")
-console.log(process.argv)
+console.log("Your program's default workflow.");
+console.log(process.argv);
 
 // ------------------------------ //
 // ASYNC APIS                     //
 // ------------------------------ //
 
-completion.onAsync('user', async ({ reply }) => {
-    reply(new Promise((resolve) => {
-        fs.readdir('/Users/', (err: any, users: any) => {
-            resolve(users)
-        })
-    }))
-})
+completion.onAsync("user", async ({ reply }) => {
+  reply(
+    new Promise(resolve => {
+      fs.readdir("/Users/", (err: any, users: any) => {
+        resolve(users);
+      });
+    }),
+  );
+});
 
 // Instead of running directly, you need to set an handler to run your main workflow.
 completion.next(() => {
-    console.log("Your program's default workflow.")
-    console.log(process.argv)
-})
+  console.log("Your program's default workflow.");
+  console.log(process.argv);
+});
 
 // .init must be called after defining .next
-completion.init()
+completion.init();
 
-omelette('hello').tree({
-    cruel: ['world', 'moon'],
-    beautiful: ['mars', 'pluto']
-}).init();
+omelette("hello")
+  .tree({
+    cruel: ["world", "moon"],
+    beautiful: ["mars", "pluto"],
+  })
+  .init();
