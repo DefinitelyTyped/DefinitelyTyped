7792bdb6d129a6a08fc92d9a80baad34bade13c0
skalma <38102328+skalma@users.noreply.github.com>
2019-08-19 19:41:22 +0200

    Update to reflect types for mui-datatables v2.8.0 (#37701)
    * Update index.d.ts
    
    Added 'custom' to type FilterType and added searchText as string to MUIDataTableOptions
    
    * Update to reflect types for mui-datatables v2.8.0
    
    - Added interface type MUIDataTableFilterOptions as: 
    export interface MUIDataTableFilterOptions {
        names?: string[];
        display?: (filterList:string[], onChange: any, index: number, column: any) => void;
        logic?: any;
    }
    - Changed type of filterOptions in MUIDataTableColumnOptions to MUIDataTableFilterOptions (breaking change if filteroptions is used)
    - Added customFilterListRender?: (value: any) => string; to MUIDataTableColumnOptions 
    - Added to MUIDataTableOptions: 
         - selectableRowsOnClick?: boolean;
         - rowsExpanded?: any[];
         - onSearchOpen?: () => void;
         - customSearchRender?: (searchText: string, handleSearch: any, hideSearch: any, options: any) => React.Component;
    
    * Added whitespace after colon in line 108, pos 27
    
    To correct travis build failure
    
    * Changed typing of logic in MUIDataTableFilterOptions
    
    As per review i changed the typing of logic in MUIDataTableFilterOptions from <any> to <logic?: (prop: string, filterValue: any[]) => boolean>

--- types/mui-datatables/index.d.ts
+++ types/mui-datatables/index.d.ts
@@ -103,12 +103,18 @@ export interface MUIDataTableTextLabels {
     selectedRows: MUIDataTableTextLabelsSelectedRows;
 }
 
+export interface MUIDataTableFilterOptions {
+    names?: string[];
+    display?: (filterList: string[], onChange: any, index: number, column: any) => void;
+    logic?: (prop: string, filterValue: any[]) => boolean;
+}
+
 export interface MUIDataTableColumnOptions {
     display?: Display;
     empty?: boolean;
     filter?: boolean;
     filterList?: string[];
-    filterOptions?: string[];
+    filterOptions?: MUIDataTableFilterOptions;
     filterType?: FilterType;
     sort?: boolean;
     searchable?: boolean;
@@ -117,6 +123,7 @@ export interface MUIDataTableColumnOptions {
     download?: boolean;
     viewColumns?: boolean;
     hint?: string;
+    customFilterListRender?: (value: any) => string;
     customHeadRender?: (columnMeta: MUIDataTableCustomHeadRenderer, updateDirection: (params: any) => any) => string | React.ReactNode;
     customBodyRender?: (value: any, tableMeta: MUIDataTableMeta, updateValue: (s: any, c: any, p: any) => any) => string | React.ReactNode;
     setCellProps?: (cellValue: string, rowIndex: number, columnIndex: number) => object;
@@ -134,6 +141,7 @@ export interface MUIDataTableOptions {
         changePage: number
     ) => React.ReactNode;
     customSearch?: (searchQuery: string, currentRow: any[], columns: any[]) => boolean;
+    customSearchRender?: (searchText: string, handleSearch: any, hideSearch: any, options: any) => React.Component;
     customSort?: (data: any[], colIndex: number, order: string) => any[];
     customToolbar?: () => React.ReactNode;
     customToolbarSelect?: (
@@ -172,6 +180,7 @@ export interface MUIDataTableOptions {
     onRowsDelete?: (rowsDeleted: any[]) => void;
     onRowsSelect?: (currentRowsSelected: any[], rowsSelected: any[]) => void;
     onSearchChange?: (searchText: string) => void;
+    onSearchOpen?: () => void;
     onTableChange?: (action: string, tableState: MUIDataTableState) => void;
     page?: number;
     pagination?: boolean;
@@ -183,9 +192,11 @@ export interface MUIDataTableOptions {
     rowsPerPage?: number;
     rowsPerPageOptions?: number[];
     rowsSelected?: any[];
+    rowsExpanded?: any[];
     search?: boolean;
     searchText?: string;
     selectableRows?: SelectableRows;
+    selectableRowsOnClick?: boolean;
     serverSide?: boolean;
     setRowProps?: (row: any[], rowIndex: number) => object;
     sort?: boolean;
