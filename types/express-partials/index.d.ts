3cd6ad7fa49791641e198f0b28592245b14cd825
Oliver Joseph Ash <oliverjash@gmail.com>
2019-08-19 01:47:36 +0100

    Express: use generics for params, default to dictionary (#37718)
    * Revert "Express: improve type of `Request['params']` aka `req.params` (#37502)"
    
    This reverts commit 9aa863ef23f3d549087339668dcebe3697f0a957.
    
    * Express: use generics for params, default to dictionary
    
    * Lint
    
    * Bump all dependants
    
    * Spacing
    
    * Bump dependants
    
    * Bump dependants
    
    * Bump dependants
    
    * Bump dependants
    
    * Bump dependants
    
    * Bump dependants
    
    * Bump dependants
    
    * Bump dependants
    
    * Bump dependants
    
    * Bump dependants
    
    * Bump dependants (via tests)
    
    * Bump dependants
    
    * Bump dependants (via tests)
    
    * Bump dependants
    
    * Simplify test
    
    * Hoist imports
    
    * Tidy test
    
    * Add tests
    
    * Add reasons
    
    * Remove redundant params
    
    * Add tests
    
    * Format
    
    * Remove redundant params
    
    * Add tests
    
    * Add JSDoc
    
    * Improve comment
    
    * Improve comment

--- types/express-partials/index.d.ts
+++ types/express-partials/index.d.ts
@@ -2,7 +2,7 @@
 // Project: https://github.com/publicclass/express-partials
 // Definitions by: jt000 <https://github.com/jt000>
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
-// TypeScript Version: 2.2
+// TypeScript Version: 2.3
 
 
 
