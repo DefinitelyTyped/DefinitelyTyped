870925f926f79a4b591a4f7e0b13cd1c40c1b6af
Juliette Regimbal <juliette.regimbal@gmail.com>
2019-08-20 12:47:35 -0400

    Add Settings definition.

--- types/diva.js/index.d.ts
+++ types/diva.js/index.d.ts
@@ -10,58 +10,101 @@
 export as namespace myLib;
 
 interface Options {
-    acceptHeader?: string,
-    adaptivePadding?: number,
-    arrowScrollAmount?: number,
-    blockMobileMove?: boolean,
+    acceptHeader: string,
+    adaptivePadding: number,
+    arrowScrollAmount: number,
+    blockMobileMove: boolean,
     objectData: object | string,
-    enableAutoTitle?: boolean,
-    enableFilename?: boolean,
-    enableFullscreen?: boolean,
-    enableGotoPage?: boolean,
-    enableGotoSuggestions?: boolean,
-    enableGridIcon?: boolean,
-    enableGridControls?: string,
-    enableImageTitles?: boolean,
-    enableIndexAsLabel?: boolean,
-    enableKeyScroll?: boolean,
-    enableLinkIcon?: boolean,
-    enableNonPagedVisibilityIcon?: boolean,
-    enableSpaceScroll?: boolean,
-    enableToolbar?: boolean,
-    enableZoomControls?: string,
-    fillParentHeight?: boolean,
-    fixedPadding?: number,
-    fixedHeightGrid?: boolean,
-    goDirectlyTo?: number,
-    hashParamSuffix?: string | null,
-    inFullscreen?: boolean,
-    inBookLayout?: boolean,
-    inGrid?: boolean,
-    maxPagesPerRow?: number,
-    maxZoomLevel?: number,
-    minPagesPerRow?: number,
-    minZoomLevel?: number,
-    onGotoSubmit?: Function,
-    pageAliases?: {},
-    pageAliasFunction?: Function,
-    pageLoadTimeout?: number,
-    pagesPerRow?: number,
-    showNonPagedPages?: boolean,
-    throbberTimeout?: number,
-    tileHeight?: number,
-    tileWidth?: number,
-    toolbarParentObject?: object,
-    verticallyOriented?: boolean;
-    viewportMargin?: number,
-    zoomLevel?: number
+    enableAutoTitle: boolean,
+    enableFilename: boolean,
+    enableFullscreen: boolean,
+    enableGotoPage: boolean,
+    enableGotoSuggestions: boolean,
+    enableGridIcon: boolean,
+    enableGridControls: string,
+    enableImageTitles: boolean,
+    enableIndexAsLabel: boolean,
+    enableKeyScroll: boolean,
+    enableLinkIcon: boolean,
+    enableNonPagedVisibilityIcon: boolean,
+    enableSpaceScroll: boolean,
+    enableToolbar: boolean,
+    enableZoomControls: string,
+    fillParentHeight: boolean,
+    fixedPadding: number,
+    fixedHeightGrid: boolean,
+    goDirectlyTo: number,
+    hashParamSuffix: string | null,
+    inFullscreen: boolean,
+    inBookLayout: boolean,
+    inGrid: boolean,
+    maxPagesPerRow: number,
+    maxZoomLevel: number,
+    minPagesPerRow: number,
+    minZoomLevel: number,
+    onGotoSubmit: Function,
+    pageAliases: {},
+    pageAliasFunction: Function,
+    pageLoadTimeout: number,
+    pagesPerRow: number,
+    showNonPagedPages: boolean,
+    throbberTimeout: number,
+    tileHeight: number,
+    tileWidth: number,
+    toolbarParentObject: object,
+    verticallyOriented: boolean;
+    viewportMargin: number,
+    zoomLevel: number
+}
+
+interface ViewerState {
+    currentPageIndices: number[],
+    activePageIndex: number,
+    horizontalOffset: number,
+    horizontalPadding: number,
+    ID: null | string,
+    initialKeyScroll: boolean,
+    initialSpaceScroll: boolean,
+    innerElement: HTMLElement,
+    innerObject: HTMLElement,
+    isActiveDiva: boolean,
+    isScrollable: boolean,
+    isZooming: boolean,
+    loaded: boolean,
+    manifest: object,
+    mobileWebkit: boolean,
+    numPages: number,
+    oldZoomLevel: number,
+    options: Options,
+    outerElement: HTMLElement,
+    outerObject: HTMLElement,
+    pageOverlays: object, // new PageOverlayManager(),
+    pageTools: object[], // plugins as page tools
+    parentObject: HTMLElement,
+    pendingManifestRequest: XMLHttpRequest,
+    pluginInstances: object[], // enabled plugins from the registry
+    renderer: object, // Renderer object
+    resizeTimer: number,
+    scrollbarWidth: number,
+    selector: string,
+    throbberTimeoutID: number,
+    toolbar: object,
+    verticalOffset: number,
+    verticalPadding: number,
+    viewHandler: object,
+    viewport: object,
+    viewportElement: HTMLElement,
+    viewportObject: HTMLElement,
+    zoomDuration: number
 }
 
 interface State {
     f: boolean; v: string; z: number; n: number; i: boolean | string; p: boolean | number; y: boolean | number; x: boolean | number;
 }
 
-export class Diva {
+type Settings = Options & ViewerState;
+
+export default class Diva {
     /**
      * @param element - The ID of an HTMLElement to attach an instance of Diva to.
      * @param options - Options to be set by the user for the instance of Diva.
@@ -440,7 +483,6 @@ export class Diva {
       * @returns False if it's at the minimum zoom.
       */
      zoomOut(): boolean;
-
 }
 
 /*~ If this module has methods, declare them as functions like so.
