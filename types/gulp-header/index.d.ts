dc925592bb841039d004d3e3f8e66c5a5f7df234
Max Fouquet <fouquet.max@gmail.com>
2019-08-20 17:45:05 +0200

    fix

--- types/gulp-header/index.d.ts
+++ types/gulp-header/index.d.ts
@@ -2,12 +2,10 @@
 // Project: https://github.com/tracker1/gulp-header
 // Definitions by: Asana <https://asana.com>
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
-
+// TypeScript Version: 2.2
 /// <reference types="node"/>
 
-
 declare function header(text: string, data?: object): NodeJS.ReadWriteStream;
-
 
 declare namespace header { }
 
