32677da884f98c19822a74455a1d0539adcce817
Max Fouquet <fouquet.max@gmail.com>
2019-08-20 17:59:53 +0200

    fix

--- types/gulp-header/index.d.ts
+++ types/gulp-header/index.d.ts
@@ -1,6 +1,6 @@
 // Type definitions for gulp-header 2.0
 // Project: https://github.com/tracker1/gulp-header
-// Definitions by: Asana <https://asana.com>
+// Definitions by: Max Fouquet <https://github.com/maxfouquet>
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
 // TypeScript Version: 2.2
 /// <reference types="node"/>
