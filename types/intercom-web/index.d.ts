992ab02959e7dd07345981fe6a6efed0ae6145ca
Chia-Lun Wu <bingo4508@gmail.com>
2019-08-20 10:01:15 +0200

    Fix and make IntercomSettings complete
    IntercomSettings.company.created_at should be optional.
    Added multiple missing attributes in IntercomSettings.

--- types/intercom-web/index.d.ts
+++ types/intercom-web/index.d.ts
@@ -9,27 +9,42 @@
 
 declare namespace Intercom_ {
   interface IntercomSettings {
-    alignment?: string;
+    // Messanger attributes
     app_id?: string;
-    email?: string;
-    created_at?: number;
+    alignment?: string;
+    custom_launcher_selector?: string;
     hide_default_launcher?: boolean;
+    horizontal_padding?: number;
+    session_duration?: number;
+    vertical_padding?: number;
+    action_color?: string;
+    background_color?: string;
+
+    // Data attributes
+    email?: string;
+    phone?: string;
+    created_at?: number;
     name?: string;
     user_id?: string;
     user_hash?: string;
-    widget?: {
-      activator?: string;
-    };
+    unsubscribed_from_emails?: boolean;
+    language_override?: string;
+    utm_campaign?: string;
+    utm_content?: string;
+    utm_medium?: string;
+    utm_source?: string;
+    utm_term?: string;
     company?: {
-      id: string | number,
-      name: string,
-      created_at: number,
-      plan?: string,
-      monthly_spend?: number,
-      [index: string]: any;
+      id: string | number;
+      name: string;
+      created_at?: number;
+      plan?: string;
+      monthly_spend?: number;
+      user_count?: number;
+      size?: number;
+      website?: string;
+      industry?: string;
     };
-    vertical_padding?: number;
-    horizontal_padding?: number;
   }
 
   type IntercomCommand = 'boot'
