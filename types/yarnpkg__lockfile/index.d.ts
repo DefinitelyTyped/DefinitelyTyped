941d1265fe5794b11737623dc83e06dad0f9f937
Eric <eric@canva.com>
2019-08-21 13:35:49 +1000

    .

--- types/yarnpkg__lockfile/index.d.ts
+++ types/yarnpkg__lockfile/index.d.ts
@@ -17,9 +17,7 @@ export interface LockFileObject {
   [packageName: string]: FirstLevelDependency | {};
 }
 
-export type ParseResult =
-  { type: 'success' | 'merge';object: LockFileObject; }
-  | { type: 'conflict'; object: {}; };
+export type ParseResult = { type: 'success' | 'merge'; object: LockFileObject } | { type: 'conflict'; object: {} };
 
 export function parse(file: string, fileLoc?: string): ParseResult;
 export function stringify(
