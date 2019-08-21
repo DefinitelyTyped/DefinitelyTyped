e505e4d1d78fcef1aa065b8888fec6ec426f060f
Julian Gonggrijp <j.gonggrijp@uu.nl>
2019-08-21 16:04:13 +0200

    Add myself to the list of jasmine-ajax contributors

--- types/jasmine-ajax/index.d.ts
+++ types/jasmine-ajax/index.d.ts
@@ -1,6 +1,7 @@
 // Type definitions for jasmine-ajax 3.3
 // Project: https://github.com/jasmine/jasmine-ajax
 // Definitions by: Louis Grignon <https://github.com/lgrignon>
+//                 Julian Gonggrijp <https://github.com/jgonggrijp>
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
 // TypeScript Version: 2.8
 
