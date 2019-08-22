7a4cf694bf15205d5959e704286e89998b835d68
Oleg Vaskevich <oleg@osv.im>
2019-08-22 05:24:51 +0000

    Use no-empty-interface locally

--- types/passport/index.d.ts
+++ types/passport/index.d.ts
@@ -12,7 +12,9 @@
 
 declare global {
     namespace Express {
+        // tslint:disable-next-line:no-empty-interface
         interface AuthInfo {}
+        // tslint:disable-next-line:no-empty-interface
         interface User {}
 
         interface Request {
