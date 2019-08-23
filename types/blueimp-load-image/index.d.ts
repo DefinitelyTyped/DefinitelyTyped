9bde773e0d708d98070d1ff1be8c9413e03f8a2e
Konstantin Lukaschenko <k.lukaschenko@googlemail.com>
2019-08-23 15:23:42 +0200

    formatting.

--- types/blueimp-load-image/index.d.ts
+++ types/blueimp-load-image/index.d.ts
@@ -20,20 +20,23 @@ export interface Iptc {
 }
 
 export interface ImageHead {
-  imageHead?: ArrayBuffer | Uint8Array;
+    imageHead?: ArrayBuffer | Uint8Array;
 }
 
 export interface MetaData extends ImageHead {
-  originalWidth?: number;
-  originalHeight?: number;
-  exif?: Exif;
-  iptc?: Iptc;
+    originalWidth?: number;
+    originalHeight?: number;
+    exif?: Exif;
+    iptc?: Iptc;
 }
 
 export interface BasicOptions {
-    maxWidth?: number; maxHeight?: number;
-    minWidth?: number; minHeight?: number;
-    contain?: boolean; cover?: boolean;
+    maxWidth?: number;
+    maxHeight?: number;
+    minWidth?: number;
+    minHeight?: number;
+    contain?: boolean;
+    cover?: boolean;
     crossOrigin?: string;
     noRevoke?: boolean;
 }
@@ -46,45 +49,72 @@ export type AspectRatio = number;
 // it automatically enables 'canvas' so in those cases,
 // 'canvas' cannot be false
 export interface CanvasTrueOptions {
-    canvas: true; sourceWidth?: number; sourceHeight?: number;
-    top?: number; right?: number; bottom?: number; left?: number;
-    pixelRatio?: number; downsamplingRatio?: number;
-    orientation?: Orientation; crop?: boolean;
+    canvas: true;
+    sourceWidth?: number;
+    sourceHeight?: number;
+    top?: number;
+    right?: number;
+    bottom?: number;
+    left?: number;
+    pixelRatio?: number;
+    downsamplingRatio?: number;
+    orientation?: Orientation;
+    crop?: boolean;
 }
-export interface CanvasFalseOptions { canvas?: false; }
+export interface CanvasFalseOptions {
+    canvas?: false;
+}
 export type CanvasOptions = CanvasTrueOptions | CanvasFalseOptions;
 
 // Setting 'aspectRatio' automatically enables 'crop', so setting 'crop' to
 // 'false' in that case is not valid
-export interface CropTrueOptions { crop?: true; aspectRatio?: AspectRatio; }
-export interface CropFalseOptions { crop?: false; }
+export interface CropTrueOptions {
+    crop?: true;
+    aspectRatio?: AspectRatio;
+}
+export interface CropFalseOptions {
+    crop?: false;
+}
 export type CropOptions = CanvasTrueOptions | CropFalseOptions;
 
 // Setting 'orientation' automatically sets 'meta' to true
 // so setting it to false is not valid in that case
-export interface MetaTrueOptions { meta?: true; orientation: Orientation; }
-export interface MetaFalseOptions { meta?: false; }
+export interface MetaTrueOptions {
+    meta?: true;
+    orientation: Orientation;
+}
+export interface MetaFalseOptions {
+    meta?: false;
+}
 export type MetaOptions = MetaTrueOptions | MetaFalseOptions;
 
 export interface ParseOptions {
-  // Defines the maximum number of bytes to parse.
-  maxMetaDataSize?: number;
+    // Defines the maximum number of bytes to parse.
+    maxMetaDataSize?: number;
 
-  // Disables creating the imageHead property.
-  disableImageHead?: boolean;
+    // Disables creating the imageHead property.
+    disableImageHead?: boolean;
 }
 
 export type LoadImageOptions = BasicOptions & CanvasOptions & CropOptions & MetaOptions;
 
 // loadImage is implemented as a callable object.
 type LoadImageType = {
-    (file: File | Blob | string, callback: LoadImageCallback, options: LoadImageOptions): HTMLImageElement | FileReader | false,
+    (file: File | Blob | string, callback: LoadImageCallback, options: LoadImageOptions):
+        | HTMLImageElement
+        | FileReader
+        | false;
 
     // Parses image meta data and calls the callback with the image head
-    parseMetaData: (file: File | Blob | string, callback: ParseMetaDataCallback, options?: ParseOptions, data?: ImageHead) => void,
+    parseMetaData: (
+        file: File | Blob | string,
+        callback: ParseMetaDataCallback,
+        options?: ParseOptions,
+        data?: ImageHead,
+    ) => void;
 
-    blobSlice: (file: Blob, start?: number, end?: number) => Blob,
-}
+    blobSlice: (file: Blob, start?: number, end?: number) => Blob;
+};
 
 declare const loadImage: LoadImageType;
 
