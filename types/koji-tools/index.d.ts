65c92aaf1ac73c4047d4dce07e0387233c8cc083
Jeff <bdjeffyp@users.noreply.github.com>
2019-08-23 14:37:10 -0700

    Remove namespace

--- types/koji-tools/index.d.ts
+++ types/koji-tools/index.d.ts
@@ -4,60 +4,52 @@
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
 // TypeScript Version: 3.0
 
-// tslint:disable-next-line:export-just-namespace
-export = Koji;
-export as namespace Koji;
+/**
+ * Server-side function that sets file watchers on all .koji customization
+ * files and allows for hot reloading of these properties.
+ */
+export function watch(): void;
 
-declare namespace Koji {
-    // index.js methods
+/**
+ * Sets up Koji.config parameters for each client and handles communication
+ * between the Koji live preview iframe and your app.
+ */
+export function pageLoad(options?: object): void;
 
-    /**
-     * Server-side function that sets file watchers on all .koji customization
-     * files and allows for hot reloading of these properties.
-     */
-    function watch(): void;
+/**
+ * Wrapper for fetch that takes objects from Koji.routes.
+ */
+export function request(route: object, params?: object): Promise<any>;
 
-    /**
-     * Sets up Koji.config parameters for each client and handles communication
-     * between the Koji live preview iframe and your app.
-     */
-    function pageLoad(options?: object): void;
+/**
+ * After the 'pwaPromptReady' event has fired, this function will make a popup
+ * installation prompt appear.
+ */
+export function pwaPrompt(): any;
 
-    /**
-     * Wrapper for fetch that takes objects from Koji.routes.
-     */
-    function request(route: object, params?: object): Promise<any>;
+/**
+ * An auto-generated list of all of the Koji Customization Controls (VCCs) your
+ * application has setup. When Koji.watch() is being used this list updates automatically.
+ */
+export const config: object;
 
-    /**
-     * After the 'pwaPromptReady' event has fired, this function will make a popup
-     * installation prompt appear.
-     */
-    function pwaPrompt(): any;
+/**
+ * A auto-generated list of routes based on koji.json files in your project that are
+ * used in Koji.request() to request the backend of your app.
+ */
+export const routes: object;
 
-    /**
-     * An auto-generated list of all of the Koji Customization Controls (VCCs) your
-     * application has setup. When Koji.watch() is being used this list updates automatically.
-     */
-    const config: object;
+export const pwa: any;
 
-    /**
-     * A auto-generated list of routes based on koji.json files in your project that are
-     * used in Koji.request() to request the backend of your app.
-     */
-    const routes: object;
+/**
+ * Registers a callback on a Koji event.
+ * @param event name of the event being called
+ * @param callback method to execute when the event occurs
+ */
+export function on(event: string, callback: () => void): void;
 
-    const pwa: any;
-
-    /**
-     * Registers a callback on a Koji event.
-     * @param event name of the event being called
-     * @param callback method to execute when the event occurs
-     */
-    function on(event: string, callback: () => void): void;
-
-    /**
-     * Resolve the value of a user's secret from the Koji Keystore. Secrets are used for
-     * values that are not intended to be read by other users when a project is remixed.
-     */
-    function resolveSecret(key: string): string | null;
-}
+/**
+ * Resolve the value of a user's secret from the Koji Keystore. Secrets are used for
+ * values that are not intended to be read by other users when a project is remixed.
+ */
+export function resolveSecret(key: string): string | null;
