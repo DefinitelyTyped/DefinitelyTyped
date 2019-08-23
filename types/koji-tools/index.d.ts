2ef3c6370e984bf68feb1f32239df213cddefd56
Jeff <bdjeffyp@users.noreply.github.com>
2019-08-23 14:08:21 -0700

    Add remaining exports

--- types/koji-tools/index.d.ts
+++ types/koji-tools/index.d.ts
@@ -10,14 +10,43 @@ export as namespace Koji;
 
 declare namespace Koji {
     // index.js methods
+
+    /**
+     * Server-side function that sets file watchers on all .koji customization
+     * files and allows for hot reloading of these properties.
+     */
+    function watch(): void;
+
     /**
      * Sets up Koji.config parameters for each client and handles communication
      * between the Koji live preview iframe and your app.
-     * @param options ???
      */
-    function pageLoad(options?: {}): void;
+    function pageLoad(options?: object): void;
 
-    function getConfig(): any;
+    /**
+     * Wrapper for fetch that takes objects from Koji.routes.
+     */
+    function request(route: object, params?: object): Promise<any>;
+
+    /**
+     * After the 'pwaPromptReady' event has fired, this function will make a popup
+     * installation prompt appear.
+     */
+    function pwaPrompt(): any;
+
+    /**
+     * An auto-generated list of all of the Koji Customization Controls (VCCs) your
+     * application has setup. When Koji.watch() is being used this list updates automatically.
+     */
+    const config: object;
+
+    /**
+     * A auto-generated list of routes based on koji.json files in your project that are
+     * used in Koji.request() to request the backend of your app.
+     */
+    const routes: object;
+
+    const pwa: any;
 
     /**
      * Registers a callback on a Koji event.
@@ -26,5 +55,9 @@ declare namespace Koji {
      */
     function on(event: string, callback: () => void): void;
 
-    function callEvent(event: string, params?: []): void;
+    /**
+     * Resolve the value of a user's secret from the Koji Keystore. Secrets are used for
+     * values that are not intended to be read by other users when a project is remixed.
+     */
+    function resolveSecret(key: string): string | null;
 }
--- types/koji-tools/koji-tools-tests.ts
+++ types/koji-tools/koji-tools-tests.ts
@@ -1,6 +1,11 @@
-import * as Koji from 'koji-tools';
+import * as Koji from "koji-tools";
 
+Koji.watch();
 Koji.pageLoad();
-Koji.getConfig();
-Koji.on("test", () => {});
-Koji.callEvent("test");
+Koji.request({ url: "www.test.org" });
+Koji.pwaPrompt();
+const myConfig = Koji.config;
+const myRoutes = Koji.routes;
+const myPwaInstaller = Koji.pwa;
+Koji.on("test", () => "Hello, world!");
+const mySecret = Koji.resolveSecret("verySecretKey");
