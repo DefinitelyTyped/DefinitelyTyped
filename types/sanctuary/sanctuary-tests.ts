6856be1fcf50154f3aa4255e4d2d68faf47b66c2
Michał Kaczanowicz <mj.kaczanowicz@gmail.com>
2019-08-23 22:03:03 +0200

    Add missing semicolons

--- types/sanctuary/sanctuary-tests.ts
+++ types/sanctuary/sanctuary-tests.ts
@@ -8,5 +8,5 @@ const S = create({checkTypes, env});
 assert.equal(S.map(S.concat('@'))(['foo', 'bar', 'baz']), ["@foo", "@bar", "@baz"]);
 assert.equal(S.reduce(S.add)(0)([1, 2, 3, 4, 5]), 15);
 assert.equal(S.flip(S.concat)('foo')('bar'), "barfoo");
-assert.equal(S.map(S.fst)(S.zip([1, 2])(['a'])), [1])
-assert.equal(S.zipWith(a => b => [a, b])([1, 2, 3])(['a', 'b', 'c']), [[1, 'a'], [2, 'b'], [3, 'c']])
+assert.equal(S.map(S.fst)(S.zip([1, 2])(['a'])), [1]);
+assert.equal(S.zipWith(a => b => [a, b])([1, 2, 3])(['a', 'b', 'c']), [[1, 'a'], [2, 'b'], [3, 'c']]);
