ed418026b8a0cf5688ebd557e146abcd14f7db5f
Eric Mulligan <eric.mulligan@fullscript.com>
2019-08-23 16:49:41 -0400

    Missed these changes.

--- types/sauronjs/core/component.d.ts
+++ types/sauronjs/core/component.d.ts
@@ -2,15 +2,15 @@ import { Observable } from 'rxjs/Observable';
 import { Subscription } from 'rxjs/Subscription';
 
 export class Component {
-    constructor(params: HTMLElement);
+  constructor(params: HTMLElement);
 
-    static _index: number;
+  static _index: number;
 
-    broadcast(event, data): void;
-    destroy(): void;
-    registerSubscription(subscriptions: Subscription | Subscription[]): void;
-    subscribe<T>(observables: Observable<T> | Array<Observable<T>>): void;
-    find(selector: string): HTMLElement;
-    findAll(selector): HTMLElement[];
-    attr(key: string, value?: string): string | void;
+  broadcast(event, data): void;
+  destroy(): void;
+  registerSubscription(subscriptions: Subscription | Subscription[]): void;
+  subscribe<T>(observables: Observable<T> | Array<Observable<T>>): void;
+  find(selector: string): HTMLElement;
+  findAll(selector): HTMLElement[];
+  attr(key: string, value?: string): string | void;
 }
