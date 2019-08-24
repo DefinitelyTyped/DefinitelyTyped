2ed565543a2b14a874e6b7f244130d79d157c350
wkl007 <499657357@qq.com>
2019-08-24 15:38:32 +0800

    Add types form good-storage

---
+++ types/good-storage/index.d.ts
@@ -0,0 +1,30 @@
+// Type definitions for good-storage
+// Project: https://github.com/ustbhuangyi/storage
+// Definitions by: Wang KaiLing <https://github.com/wkl007>
+// Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
+
+interface Storage {
+  session: Storage;
+
+  set (key: string, val?: any): any;
+
+  get (key: string, def?: any): any;
+
+  has (key: string): boolean;
+
+  remove (key: string): void;
+
+  clear (): void;
+
+  getAll (): any;
+
+  forEach (callback: () => void): void;
+}
+
+declare var storage: Storage
+
+declare module "good-storage" {
+
+  export = storage;
+
+}
