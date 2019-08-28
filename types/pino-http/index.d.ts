bd21b4bfc9a7d099450a6b0fc66fb872cfb21171
Jeremy Forsythe <jdforsythe@gmail.com>
2019-08-28 08:33:23 -0400

    bump version

--- types/pino-http/index.d.ts
+++ types/pino-http/index.d.ts
@@ -1,4 +1,4 @@
-// Type definitions for pino-http 4.0
+// Type definitions for pino-http 4.3
 // Project: https://github.com/pinojs/pino-http#readme
 // Definitions by: Christian Rackerseder <https://github.com/screendriver>
 //                 Jeremy Forsythe <https://github.com/jdforsythe>
