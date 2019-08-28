17f95c0c8e23e26a229d7753e81fa9c9067956fa
AJ Livingston <livingston.aj@gmail.com>
2019-08-28 17:32:52 -0400

    adds typings for react-aria-live

---
+++ types/react-aria-live/index.d.ts
@@ -0,0 +1,24 @@
+// Type definitions for react-aria-live 2.0
+// Project: https://github.com/AlmeroSteyn/react-aria-live#readme
+// Definitions by: AJ Livingston <https://github.com/ajliv>
+// Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
+
+import * as React from "react";
+
+export class LiveAnnouncer extends React.Component {
+    announcePolite(message: string, id?: string): void;
+    announceAssertive(message: string, id?: string): void;
+}
+
+export const LiveMessage: React.SFC<{
+    'aria-live': 'assertive' | 'polite';
+    clearOnUnmount?: boolean | 'true' | 'false';
+    message: string;
+}>;
+
+export const LiveMessenger: React.SFC<{
+    children(contextProps: {
+        announceAssertive(message: string, id?: string): void;
+        announcePolite(message: string, id?: string): void;
+    }): React.ReactElement<any> | null;
+}>;
