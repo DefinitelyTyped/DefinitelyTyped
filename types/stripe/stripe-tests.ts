fa9ed8432f3ae0343c0fd213359fa8dbebbc0e66
jeff <jeff@giftbit.com>
2019-08-27 17:37:13 -0700

    Added missing params to Charge, Customer and related create and update options.  Adding Customer Tax Id.

--- types/stripe/stripe-tests.ts
+++ types/stripe/stripe-tests.ts
@@ -830,6 +830,42 @@ stripe.customers.deleteSubscriptionDiscount("cus_5rfJKDJkuxzh5Q", "sub_5rfJxnBLG
 
 //#endregion
 
+//#region Customer Tax Ids
+// ##################################################################################
+
+stripe.customers.createTaxId(
+    'cus_FhdWgak8aeNfht',
+    {
+        type: 'eu_vat',
+        value: 'DE123456789',
+    },
+    function(err, taxId) {
+        // asynchronously called
+    }
+);
+
+stripe.customers.retrieveTaxId(
+    'cus_FhdWgak8aeNfht',
+    'txi_123456789',
+    function(err, taxId) {
+        // asynchronously called
+    }
+);
+
+stripe.customers.deleteTaxId(
+    'cus_FhdWgak8aeNfht',
+    'txi_123456789',
+    function(err, confirmation) {
+        // asynchronously called
+    }
+);
+
+stripe.customers.listTaxIds('cus_FhdWgak8aeNfht', function(err, taxIds) {
+    // asynchronously called
+});
+
+//#endregion
+
 //#region Disputes tests
 // ##################################################################################
 
