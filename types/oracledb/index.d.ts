0e9a8055fac30647dae61364bd775412e4f6a01f
Connor Fitzgerald <connorfitzgerald1995@aol.com>
2019-08-28 22:55:35 +0100

    Revert "Update header"
    This reverts commit b4f5169dfc5fb3d0c00c5b91dee310028d4d2d30.

--- types/oracledb/index.d.ts
+++ types/oracledb/index.d.ts
@@ -1,6 +1,7 @@
 // Type definitions for oracledb 4.0
 // Project: https://github.com/oracle/node-oracledb
-// Definitions by: Connor Fitzgerald <https://github.com/connorjayfitzgerald>
+// Definitions by: Richard Natal <https://github.com/Bigous>
+//                 Connor Fitzgerald <https://github.com/connorjayfitzgerald>
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
 // TypeScript Version: 3.2
 
