a462e43c4135ea8caea78cc4f179fc90b5e83f71
Elvis Wolcott <elvis.wolcott@pubnub.com>
2019-08-28 11:56:39 -0700

    bump minor version to latest

--- types/emoji-mart/index.d.ts
+++ types/emoji-mart/index.d.ts
@@ -1,7 +1,8 @@
-// Type definitions for emoji-mart 2.8
+// Type definitions for emoji-mart 2.11
 // Project: https://github.com/missive/emoji-mart
 // Definitions by: Jessica Franco <https://github.com/Jessidhia>
 //                 Nick Winans <https://github.com/Nicell>
+//                 Elvis Wolcott <https://github.com/elvis-wolcott>
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
 // TypeScript Version: 2.8
 
