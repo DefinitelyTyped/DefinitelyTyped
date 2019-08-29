b1dff2342d0e9583efcc3a5aca040d62ef5939e4
Simon Warta <simon@warta.it>
2019-08-29 09:06:57 +0200

    Run prettier on 'types/ledgerhq__hw-transport/*'

--- types/ledgerhq__hw-transport/index.d.ts
+++ types/ledgerhq__hw-transport/index.d.ts
@@ -10,17 +10,9 @@ declare class Transport<TDescriptor extends Descriptor = string> {
     constructor();
     static isSupported(): Promise<boolean>;
     static list(): Promise<ReadonlyArray<Descriptor>>;
-    static listen(
-        observer: Observer<DescriptorEvent<Descriptor>>
-    ): Subscription;
-    static open(
-        descriptor: Descriptor,
-        timeout?: number
-    ): Promise<Transport<typeof descriptor>>;
-    static create(
-        openTimeout?: number,
-        listenTimeout?: number
-    ): Promise<Transport<Descriptor>>;
+    static listen(observer: Observer<DescriptorEvent<Descriptor>>): Subscription;
+    static open(descriptor: Descriptor, timeout?: number): Promise<Transport<typeof descriptor>>;
+    static create(openTimeout?: number, listenTimeout?: number): Promise<Transport<Descriptor>>;
     exchange(apdu: Buffer): Promise<Buffer>;
     setScrambleKey(key: string): void;
     close(): Promise<void>;
@@ -34,14 +26,21 @@ declare class Transport<TDescriptor extends Descriptor = string> {
      * @param statusList A list of accepted status code (shorts). [0x9000] by default.
      * @return A Promise of the response Buffer
      */
-    send(cla: number, ins: number, p1: number, p2: number, data?: Buffer, statusList?: ReadonlyArray<number>): Promise<Buffer>;
+    send(
+        cla: number,
+        ins: number,
+        p1: number,
+        p2: number,
+        data?: Buffer,
+        statusList?: ReadonlyArray<number>,
+    ): Promise<Buffer>;
     decorateAppAPIMethods(self: any, methods: string[], scrambleKey: string): void;
 }
 
 export type Device = any;
 export type Descriptor = string; // ?
 export interface DescriptorEvent<Descriptor> {
-    type: "add" | "remove";
+    type: 'add' | 'remove';
     descriptor: Descriptor;
     device?: Device;
 }
@@ -54,6 +53,6 @@ export interface Subscription {
     readonly unsubscribe: () => void;
 }
 export function getAltStatusMessage(code: number): string;
-export const StatusCodes: {[k in string]: number};
+export const StatusCodes: { [k in string]: number };
 
 export default Transport;
