16f73b39a19efc27fcf0c53bb80759044730e52e
Michael Scott-Nelson <mscottnelson@gmail.com>
2019-08-29 13:11:49 -0400

    reintroduce version number tracking

--- types/ldapjs/index.d.ts
+++ types/ldapjs/index.d.ts
@@ -1,4 +1,4 @@
-// Type definitions for ldapjs
+// Type definitions for ldapjs 1.0
 // Project: http://ldapjs.org
 // Definitions by: Charles Villemure <https://github.com/cvillemure>, Peter Kooijmans <https://github.com/peterkooijmans>, Pablo Moleri <https://github.com/pmoleri>, Michael Scott-Nelson <https://github.com/mscottnelson>
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
