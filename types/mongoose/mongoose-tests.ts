5eed606ea4ddcb895abd1ad8705e50b03cddec71
Anton Kenikh <kenikh@me.com>
2019-09-01 22:24:12 +0300

    Update mongoose-tests.ts

--- types/mongoose/mongoose-tests.ts
+++ types/mongoose/mongoose-tests.ts
@@ -778,10 +778,10 @@ MyModel.bulkWrite([{foo:'bar'}]).then(r => {
   console.log(r.deletedCount);
 });
 MyModel.bulkWrite([], (err, res) => {
-  console.log(res.modifiedCount)
+  console.log(res.modifiedCount);
 });
 MyModel.bulkWrite([], { ordered: false }, (err, res) => {
-  console.log(res.modifiedCount)
+  console.log(res.modifiedCount);
 });
 doc.populate('path');
 doc.populate({path: 'hello'});
