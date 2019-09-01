82dd00605f9454d6ccd81c2c8dd7f6d3fafcc442
Dennis <realdennis777@gmail.com>
2019-09-01 17:43:31 +0800

    Update. The version of corresponding lib in header

--- types/react-share/index.d.ts
+++ types/react-share/index.d.ts
@@ -1,4 +1,4 @@
-// Type definitions for react-share 2.1
+// Type definitions for react-share 3.0.1
 // Project: https://github.com/nygardk/react-share#readme
 // Definitions by: icopp <https://github.com/icopp>
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
