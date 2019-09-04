d2814fba8e3880d1b3af76e6524373696d9dae98
Daniel Hritzkiv <daniel.hritzkiv@gmail.com>
2019-09-04 11:58:02 -0400

    Remove patch from version

--- types/node-geocoder/index.d.ts
+++ types/node-geocoder/index.d.ts
@@ -1,4 +1,4 @@
-// Type definitions for node-geocoder 3.24.0
+// Type definitions for node-geocoder 3.24
 // Project: https://github.com/nchaulet/node-geocoder#readme
 // Definitions by: Krzysztof Rosinski <https://github.com/rosek86>
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
