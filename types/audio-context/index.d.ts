ad7c768d79f50ecdffbb318212e53c30b098eb33
bdjeffyp <bdjeffyp@users.noreply.github.com>
2019-09-03 23:03:18 -0700

    Test fixes

--- types/audio-context/index.d.ts
+++ types/audio-context/index.d.ts
@@ -2,15 +2,16 @@
 // Project: https://github.com/audiojs/audio-context
 // Definitions by: Jeff Peterson <https://github.com/bdjeffyp>
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
+// TypeScript version 2.2
 
 /**
  * Options for your audio context:
-@param sampleRate if specified, will set the context sampleRate.
-@param latencyHint if specified, will control latency. One of 'balanced', 'playback', 'interaction' (default) or number.
-@param offline if specified, will create OfflineAudioContext.
-@param length if specified, will set number of frames for offline context.
-@param channels if specified, will set number of channels for offline context.
-@param contextAttributes any other options for the context.
+ * @param sampleRate if specified, will set the context sampleRate.
+ * @param latencyHint if specified, will control latency. One of 'balanced', 'playback', 'interaction' (default) or number.
+ * @param offline if specified, will create OfflineAudioContext.
+ * @param length if specified, will set number of frames for offline context.
+ * @param channels if specified, will set number of channels for offline context.
+ * @param contextAttributes any other options for the context.
  */
 export interface Options {
     sampleRate?: number;
--- types/audio-play/index.d.ts
+++ types/audio-play/index.d.ts
@@ -14,23 +14,23 @@ export = audioPlay;
  */
 declare function audioPlay(buffer: AudioBuffer, how: Options, cb: () => void): AudioPlayHandle;
 
-declare type AudioPlayHandle = {
+export interface AudioPlayHandle {
     play: () => any;
     pause: () => any;
 }
 
 /**
-* Various options for audio playback
-* @param start The timestamp at which to start the audio. Can be negative to start from end. (Default: 0)
-* @param end The timestamp the audio ends at. (Default: length of audio buffer)
-* @param autoplay Plays back the audio immediately upon loading. (Default: false)
-* @param loop Continuously loops the buffer until paused. (Default: false)
-* @param context Handle to an audio context. If not provided, one is provided for you.
-* @param volume (not implemented) Playback the audio at a percentage of full volume. (Default: 1)
-* @param detune (not implemented) Percentage of fine-tuning. (Default: 0)
-* @param rate (not implemented) Playback rate, in percent, of the audio. (Default: 1)
-*/
-declare interface Options {
+ * Various options for audio playback
+ * @param start The timestamp at which to start the audio. Can be negative to start from end. (Default: 0)
+ * @param end The timestamp the audio ends at. (Default: length of audio buffer)
+ * @param autoplay Plays back the audio immediately upon loading. (Default: false)
+ * @param loop Continuously loops the buffer until paused. (Default: false)
+ * @param context Handle to an audio context. If not provided, one is provided for you.
+ * @param volume (not implemented) Playback the audio at a percentage of full volume. (Default: 1)
+ * @param detune (not implemented) Percentage of fine-tuning. (Default: 0)
+ * @param rate (not implemented) Playback rate, in percent, of the audio. (Default: 1)
+ */
+export interface Options {
     start?: number;
     end?: number;
     autoplay?: boolean;
