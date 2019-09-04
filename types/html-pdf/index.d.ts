5b85b35d98b009ea23366f3d9c8a2dda89a5bafe
Mathieu Ghennassia <mathieu.ghennassia@gmail.com>
2019-09-04 11:51:23 +0200

    Update version

--- types/html-pdf/index.d.ts
+++ types/html-pdf/index.d.ts
@@ -1,4 +1,4 @@
-// Type definitions for html-pdf v2.1.0
+// Type definitions for html-pdf v2.2.0
 // Project: https://github.com/marcbachmann/node-html-pdf
 // Definitions by: Seth Westphal <https://github.com/westy92>
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
