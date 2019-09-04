f9d88e4e11156be888d99fe1bf6fec458e443736
Max Boguslavskiy <mboguslavskiy@aligntech.com>
2019-09-04 17:48:33 +0300

    added tests and type for html-webpack-tags-plugin

---
+++ types/html-webpack-tags-plugin/index.d.ts
@@ -0,0 +1,16 @@
+// Type definitions for html-webpack-tags-plugin 2.0
+// Project: https://github.com/jharris4/html-webpack-tags-plugin
+// Definitions by: // Definitions by: 贺师俊 <https://github.com/hax>, Vladimir Grenaderov <https://github.com/VladimirGrenaderov>, Max Boguslavskiy <https://github.com/maxbogus>
+// Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
+
+import { Plugin } from 'webpack';
+
+export interface Options {
+    tags: string | string[];
+    files?: string | string[];
+    append?: boolean;
+}
+
+export default class HtmlWebpackTagsPlugin extends Plugin {
+    constructor(options: Options);
+}
