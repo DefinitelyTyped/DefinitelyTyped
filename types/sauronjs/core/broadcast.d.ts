8d3ebeda844473e0e7a26f14a4f711d8ac990b00
Eric Mulligan <eric.mulligan@fullscript.com>
2019-09-05 16:05:50 -0400

    Updated prettier.

--- types/sauronjs/core/broadcast.d.ts
+++ types/sauronjs/core/broadcast.d.ts
@@ -2,7 +2,7 @@ import { Subscription } from 'rxjs/Subscription';
 
 import { Service } from './service';
 
-declare function next(channels: string[], event: string, data: any, id: string): void;
+declare function next(channels: ReadonlyArray<string>, event: string, data: any, id: string): void;
 declare function attachSubject(object: Service): Subscription;
 
 export { next, attachSubject };
--- types/sauronjs/core/component.d.ts
+++ types/sauronjs/core/component.d.ts
@@ -6,17 +6,17 @@ interface ComponentParameters {
 }
 
 declare class Component {
-constructor(params: ComponentParameters);
+    constructor(params: ComponentParameters);
 
-static _index: number;
+    static _index: number;
 
- broadcast(event: string, data: any): void;
-destroy(): void;
- registerSubscription(subscriptions: Subscription | Subscription[]): void;
-subscribe<T>(observables: Observable<T> | Array<Observable<T>>): void;
-  find(selector: string): HTMLElement;
-findAll(selector: string): HTMLElement[];
-  attr(key: string, value?: string): string | void;
+    broadcast(event: string, data: any): void;
+    destroy(): void;
+    registerSubscription(subscriptions: Subscription | Subscription[]): void;
+    subscribe<T>(observables: Observable<T> | Array<Observable<T>>): void;
+    find(selector: string): HTMLElement;
+    findAll(selector: string): HTMLElement[];
+    attr(key: string, value?: string): string | void;
 }
 
 export { Component };
