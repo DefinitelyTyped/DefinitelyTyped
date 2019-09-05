f0ffba1350bb6f5486349b34408c2f7d8f91f1a5
Conor Dockry <conordockry@gmail.com>
2019-09-05 14:19:10 -0400

    stripe checkout.sessions.create  (#38049)
    * Allow Partial subscription_data in sessions.create
    
    https://stripe.com/docs/payments/checkout/subscriptions/starting#client-server
    
    * use existing type for subscription creation

--- types/stripe/index.d.ts
+++ types/stripe/index.d.ts
@@ -2158,7 +2158,7 @@ declare namespace Stripe {
                 /**
                  * Use instead of @param line_items when using a subscription
                  */
-                subscription_data?: subscriptions.ISubscription;
+                subscription_data?: subscriptions.ISubscriptionCustCreationOptions;
             }
 
             interface ICheckoutLineItems {
