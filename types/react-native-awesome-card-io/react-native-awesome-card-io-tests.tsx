33570914067d930a634cb65ef2698ad95cb6f833
Onur Var <var.onur@hotmail.com>
2019-09-06 03:01:28 +0300

    Changes in tests

--- types/react-native-awesome-card-io/react-native-awesome-card-io-tests.tsx
+++ types/react-native-awesome-card-io/react-native-awesome-card-io-tests.tsx
@@ -1,21 +1,16 @@
 import { Component } from 'react';
-
-import {
-  CardIOView,
-  CardIOUtilities,
-  CardDetails,
-} from 'react-native-awesome-card-io';
+import { CardDetails, CardIOUtilities, CardIOView } from 'react-native-awesome-card-io';
 
 export default class CardIOExample extends Component {
-  componentWillMount() {
-    CardIOUtilities.preload();
-  }
+    componentWillMount() {
+        CardIOUtilities.preload();
+    }
 
-  didScanCard = (card: CardDetails) => {
-    console.log(card);
-  };
+    didScanCard = (card: CardDetails) => {
+        console.log(card);
+    };
 
-  render() {
-    return <CardIOView didScanCard={this.didScanCard} style={{ flex: 1 }} />;
-  }
+    render() {
+        return <CardIOView didScanCard={this.didScanCard} style={{ flex: 1 }} />;
+    }
 }
