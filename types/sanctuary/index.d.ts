588bc0970e826723940767f812d2679d5986b611
Michał Kaczanowicz <mj.kaczanowicz@gmail.com>
2019-09-07 14:52:10 +0200

    Make type parameter name consistent

--- types/sanctuary/index.d.ts
+++ types/sanctuary/index.d.ts
@@ -103,8 +103,8 @@ declare namespace Sanctuary {
     gte<A>(x: Ord<A>): (y: Ord<A>) => boolean;
     min<A>(x: Ord<A>): (y: Ord<A>) => A;
     max<A>(x: Ord<A>): (y: Ord<A>) => A;
-    clamp<T = string | number | Date>(a: T): (b: T) => (c: T) => T;
-    clamp<T extends Ord<T>>(a: T): (b: T) => (c: T) => T;
+    clamp<A = string | number | Date>(a: A): (b: A) => (c: A) => A;
+    clamp<A extends Ord<A>>(a: A): (b: A) => (c: A) => A;
     id<A>(p: TypeRep): Fn<A, A> | Category<any>;
     concat<A>(x: Semigroup<A>): (y: Semigroup<A>) => Semigroup<A>;
     concat<A>(x: ReadonlyArray<A>): (y: ReadonlyArray<A>) => Array<A>;
