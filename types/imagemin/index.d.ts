7efcaf936db844a41ac010d1fa282426b6deb805
Jeff Chan <hkjeffchan@gmail.com>
2019-09-08 00:14:49 +0800

    update to fix test and lint

--- types/imagemin/index.d.ts
+++ types/imagemin/index.d.ts
@@ -1,28 +1,29 @@
 // Type definitions for imagemin 7.0
 // Project: https://github.com/imagemin/imagemin#readme
-// Definitions by: Jeff Chan <https://github.com/hkjeffchan>
+// Definitions by: Romain Faust <https://github.com/romain-faust>
+//                 Jeff Chan <https://github.com/hkjeffchan>
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
 
 /// <reference types="node" />
 
-declare function imagemin(input: string[], options?: imagemin.Options): Promise<imagemin.Result[]>
+declare function imagemin(input: string[], options?: imagemin.Options): Promise<imagemin.Result[]>;
 
 declare namespace imagemin {
-  type Plugin = (input: Buffer) => Promise<Buffer>;
+    type Plugin = (input: Buffer) => Promise<Buffer>;
 
-  interface Options {
-    destination?: string;
-    plugins: ReadonlyArray<Plugin>;
-    glob?: boolean;
-  }
+    interface Options {
+        destination?: string;
+        plugins: ReadonlyArray<Plugin>;
+        glob?: boolean;
+    }
 
-  interface Result {
-    data: Buffer;
-    sourcePath: string;
-    destinationPath: string;
-  }
+    interface Result {
+        data: Buffer;
+        sourcePath: string;
+        destinationPath: string;
+    }
 
-  function buffer(buffer: Buffer, options?: Options): Promise<Buffer>;
+    function buffer(buffer: Buffer, options?: Options): Promise<Buffer>;
 }
 
 export = imagemin;
