19745141b42150dbb43821ed7d05d559fa3bafe6
Ladislav Durchánek <durchanek@gmail.com>
2019-09-07 15:39:37 +0200

    @types/stripe Updating the version number

--- types/stripe/index.d.ts
+++ types/stripe/index.d.ts
@@ -1,4 +1,4 @@
-// Type definitions for stripe 6.31
+// Type definitions for stripe 6.32
 // Project: https://github.com/stripe/stripe-node/
 // Definitions by: William Johnston <https://github.com/wjohnsto>
 //                 Peter Harris <https://github.com/codeanimal>
