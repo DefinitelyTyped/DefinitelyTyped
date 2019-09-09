5c16b0ae049d25118d9fe403e2019962b5ffe994
Michał Kaczanowicz <mj.kaczanowicz@gmail.com>
2019-09-09 15:16:53 +0200

    Add missing semicolons

--- types/sanctuary/index.d.ts
+++ types/sanctuary/index.d.ts
@@ -128,11 +128,11 @@ type Radix
   | 10 | 11 | 12 | 13 | 14 | 15 | 16 | 17
   | 18 | 19 | 20 | 21 | 22 | 23 | 24 | 25
   | 26 | 27 | 28 | 29 | 30 | 31 | 32 | 33
-  | 34 | 35 | 36
+  | 34 | 35 | 36;
 
 type RegExpFlags
   = '' | 'g' | 'i' | 'm' | 'gi'
-  | 'gm' | 'im' | 'gim'
+  | 'gm' | 'im' | 'gim';
 
 interface MatchObj {
   match: string;
