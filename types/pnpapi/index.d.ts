ebad7f2c80aeb797e31176f9fba790be0bda39b4
Maël Nison <nison.mael@gmail.com>
2019-09-09 16:15:07 +0200

    Adds process.versions.pnp

--- types/pnpapi/index.d.ts
+++ types/pnpapi/index.d.ts
@@ -4,38 +4,55 @@
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
 // TypeScript Version: 2.1
 
-export interface PhysicalPackageLocator {
-    name: string;
-    reference: string;
+declare namespace NodeJS {
+    interface ProcessVersions {
+        pnp?: string;
+    }
 }
 
-export interface TopLevelPackageLocator {
-    name: null;
-    reference: null;
+declare module 'pnpapi' {
+    export interface PhysicalPackageLocator {
+        name: string;
+        reference: string;
+    }
+
+    export interface TopLevelPackageLocator {
+      name: null;
+      reference: null;
+    }
+
+    export type PackageLocator =
+      | PhysicalPackageLocator
+      | TopLevelPackageLocator;
+
+    export interface PackageInformation {
+      packageLocation: string;
+      packageDependencies: Map<string, string | [string, string]>;
+    }
+
+    export const VERSIONS: { std: number; [key: string]: number };
+
+    export const topLevel: { name: null; reference: null };
+
+    export function getPackageInformation(
+      locator: PackageLocator,
+    ): PackageInformation;
+    export function findPackageLocator(location: string): PackageLocator | null;
+
+    export function resolveToUnqualified(
+      request: string,
+      issuer: string | null,
+      opts?: { considerBuiltins?: boolean },
+    ): string | null;
+    export function resolveUnqualified(
+      unqualified: string,
+      opts?: { extensions?: string[] },
+    ): string;
+    export function resolveRequest(
+      request: string,
+      issuer: string | null,
+      opts?: { considerBuiltins?: boolean; extensions?: string[] },
+    ): string | null;
+
+    export function setup(): void;
 }
-
-export type PackageLocator =
-    | PhysicalPackageLocator
-    | TopLevelPackageLocator;
-
-export interface PackageInformation {
-    packageLocation: string;
-    packageDependencies: Map<string, string | [string, string]>;
-}
-
-export const VERSIONS: {std: number, [key: string]: number};
-
-export const topLevel: {name: null, reference: null};
-
-export function getPackageInformation(locator: PackageLocator): PackageInformation;
-export function findPackageLocator(location: string): PackageLocator | null;
-
-export function resolveToUnqualified(request: string, issuer: string | null, opts?: {considerBuiltins?: boolean}): string | null;
-export function resolveUnqualified(unqualified: string, opts?: { extensions?: string[] }): string;
-export function resolveRequest(
-    request: string,
-    issuer: string | null,
-    opts?: { considerBuiltins?: boolean; extensions?: string[] }
-): string | null;
-
-export function setup(): void;
--- types/pnpapi/pnpapi-tests.ts
+++ types/pnpapi/pnpapi-tests.ts
@@ -1,15 +1,23 @@
 import * as pnp from 'pnpapi';
 
-const information = pnp.getPackageInformation(pnp.topLevel);
-
-const locator = pnp.findPackageLocator('/foo');
-if (locator !== null) {
+if (process.versions.pnp) {
+  const locator = pnp.findPackageLocator('/foo');
+  if (locator !== null) {
     pnp.getPackageInformation(locator).packageDependencies;
-}
+  }
 
-const resolution1 = pnp.resolveRequest('lodash', '/foo');
+  const resolution1 = pnp.resolveRequest('lodash', '/foo', {
+    considerBuiltins: false,
+    extensions: ['.ts', '.js'],
+  });
 
-let resolution2 = pnp.resolveToUnqualified('lodash', '/foo');
-if (resolution2 !== null) {
-    resolution2 = pnp.resolveUnqualified(resolution2);
+  let resolution2 = pnp.resolveToUnqualified('lodash', '/foo', {
+    considerBuiltins: false,
+  });
+
+  if (resolution2 !== null) {
+    resolution2 = pnp.resolveUnqualified(resolution2, {
+      extensions: ['.ts', '.js'],
+    });
+  }
 }
