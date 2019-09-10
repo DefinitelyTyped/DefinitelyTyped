d96328540e75ae4253519e95a8a8824e273f9534
Enzo Volkmann <enzo@exportarts.io>
2019-09-10 15:34:13 +0200

    Merged definitions with same signature, added dummy version number

--- types/ineum/index.d.ts
+++ types/ineum/index.d.ts
@@ -1,22 +1,17 @@
-// Type definitions for non-npm package ineum-browser, the Website Monitoring API from Instana (status: 2019-09-10)
+// Type definitions for non-npm package ineum-browser 1.0
 // Project: https://docs.instana.io/products/website_monitoring/api/
 // Definitions by: Enzo Volkmann <https://github.com/evolkmann>
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
 // TypeScript Version: 3.2
 
-declare function ineum(command: 'key', trackingKey: string): void;
-declare function ineum(command: 'reportingUrl', reportingUrl: string): void;
-declare function ineum(command: 'page', pageName: string): void;
+// Website Monitoring API from Instana (status: 2019-09-10)
+
+declare function ineum(command: 'key' | 'reportingUrl' | 'page' | 'traceId', value: string): void;
 declare function ineum(command: 'user', userId: string, userName: string, userEmail: string): void;
 declare function ineum(command: 'meta', key: string, value: string): void;
-declare function ineum(command: 'traceId', traceId: string): void;
-declare function ineum(command: 'ignoreUrls', ignoreUrls: RegExp[]): void;
-declare function ineum(command: 'autoClearResourceTimings', autoClearResourceTimings: boolean): void;
+declare function ineum(command: 'autoClearResourceTimings' | 'wrapEventHandlers' | 'wrapTimers', enable: boolean): void;
 declare function ineum(command: 'getPageLoadId'): string | undefined;
 declare function ineum(command: 'reportError', error: string, opts?: {
     componentStack: string
 }): void;
-declare function ineum(command: 'ignoreErrorMessages', ignoreErrorMessages: RegExp[]): void;
-declare function ineum(command: 'wrapEventHandlers', wrapEventHandlers: boolean): void;
-declare function ineum(command: 'wrapTimers', wrapTimers: boolean): void;
-declare function ineum(command: 'whitelistedOrigins', whitelistedOrigins: RegExp[]): void;
+declare function ineum(command: 'ignoreUrls' | 'ignoreErrorMessages' | 'whitelistedOrigins', values: RegExp[]): void;
