3ac573d5c3dfec6682061220699aa49b3a0ac908
Per Bergland <per@refapp.se>
2019-09-10 08:09:47 +0200

    Add docs and update props type with new properties
    Make test component functional

--- types/react-easy-crop/index.d.ts
+++ types/react-easy-crop/index.d.ts
@@ -4,7 +4,7 @@
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
 // TypeScript Version: 2.8
 
-import { CSSProperties, Component } from "react";
+import { Component } from 'react';
 
 export interface Size {
     width: number;
@@ -24,33 +24,121 @@ export interface Area {
 }
 
 export interface CropperProps {
+    /**
+     * The image to be cropped.
+     **/
     image: string;
+    /**
+     * Position of the image. { x: 0, y: 0 } will center the image under the cropper.
+     **/
     crop: Location;
+    /**
+     * Zoom of the image between minZoom and maxZoom. Defaults to 1.
+     **/
     zoom?: number;
+    /**
+     * Rotation of the image. Defaults to 0.
+     **/
+    rotation?: number;
+    /**
+     * Aspect of the cropper. The value is the ratio between its width and its height.
+     * The default value is 4/3
+     **/
     aspect?: number;
+    /**
+     * Minimum zoom of the image. Defaults to 1.
+     **/
     minZoom?: number;
+    /**
+     * Maximum zoom of the image. Defaults to 3.
+     **/
     maxZoom?: number;
-    cropShape?: "rect" | "round";
+    /**
+     * Shape of the crop area. Defaults to 'rect'.
+     **/
+    cropShape?: 'rect' | 'round';
+    /**
+     * Size of the crop area (in pixels).
+     * If you don't provide it, it will be computed automatically using the aspect prop and the image size.
+     * Warning: this cannot be changed once the component is displayed.
+     * If you need to change it (based on some user inputs for instance),
+     * you can force the component to be reset by using a key.
+     **/
     cropSize?: Size;
+    /**
+     * Whether to show or not the grid (third-lines). Defaults to true.
+     **/
     showGrid?: boolean;
+    /**
+     * Multiplies the value by which the zoom changes. Defaults to 1.
+     **/
     zoomSpeed?: number;
+    /**
+     * Allows setting the crossOrigin attribute on the image.
+     **/
     crossOrigin?: string;
+    /**
+     * Called everytime the crop is changed. Use it to update your crop state.
+     **/
     onCropChange: (location: Location) => void;
+    /**
+     * Called everytime the zoom is changed. Use it to update your zoom state.
+     **/
     onZoomChange?: (zoom: number) => void;
+    /**
+     * Called everytime the rotation is changed (with mobile gestures).
+     * Use it to update your rotation state.
+     **/
+    onRotationChange?: (rotation: number) => void;
+    /**
+     * Called when the user stops moving the image or stops zooming.
+     * It will be passed the corresponding cropped area on the image in percentages and pixels
+     **/
     onCropComplete?: (croppedArea: Area, croppedAreaPixels: Area) => void;
-    onImgError?: () => void;
+    /**
+     * Called when error occurs while loading an external image
+     **/
+    onImgError?: (event: React.ReactEventHandler<HTMLImageElement>) => void;
+    /**
+     * Custom styles to be used with the Cropper.
+     * Styles passed via the style prop are merged with the defaults.
+     **/
     style?: {
-        containerStyle: React.CSSProperties,
-        imageStyle: React.CSSProperties,
-        cropAreaStyle: React.CSSProperties
+        containerStyle: React.CSSProperties;
+        imageStyle: React.CSSProperties;
+        cropAreaStyle: React.CSSProperties;
     };
+    /**
+     * Custom class names to be used with the Cropper.
+     * Classes passed via the classes prop are merged with the defaults.
+     **/
     classes?: {
-        containerClassName: string,
-        imageClassName: string,
-        cropAreaClassName: string
+        containerClassName: string;
+        imageClassName: string;
+        cropAreaClassName: string;
     };
+    /**
+     * Whether the position of the image should be restricted to the boundaries of the cropper.
+     * Useful setting in case of zoom < 1 or if the cropper should preserve all image content
+     * while forcing a specific aspect ratio for image throughout the application.
+     * Example: https://codesandbox.io/s/1rmqky233q.
+     **/
     restrictPosition?: boolean;
+    /**
+     * Use this to set the initial crop position/zoom of the cropper
+     * (for example, when editing a previously cropped image).
+     * The value should be the same as the croppedAreaPixelspassed to
+     * onCropComplete Example: https://codesandbox.io/s/pmj19vp2yx.
+     **/
     initialCroppedAreaPixels?: Area;
+    /**
+     * Called everytime a user starts a wheel, touch or mousedown event.
+     **/
+    onInteractionStart?: () => void;
+    /**
+     * Called everytime a user ends a wheel, touch or mousedown event.
+     **/
+    onInteractionEnd?: () => void;
 }
 
 declare class Cropper extends Component<CropperProps> {}
--- types/react-easy-crop/react-easy-crop-tests.tsx
+++ types/react-easy-crop/react-easy-crop-tests.tsx
@@ -1,41 +1,14 @@
 import * as React from 'react';
 import * as ReactDOM from 'react-dom';
-import Cropper, { Area, Location, Size } from 'react-easy-crop';
+import Cropper, { Area, Location, CropperProps } from 'react-easy-crop';
 
-interface State {
-  imageSrc: string;
-  crop: Location;
-  zoom: number;
-  aspect: number;
-  minZoom?: number;
-  maxZoom?: number;
-  cropShape?: 'rect' | 'round';
-  cropSize?: Size;
-  showGrid?: boolean;
-  zoomSpeed?: number;
-  crossOrigin?: string;
-  style?: {
-    containerStyle: React.CSSProperties,
-    imageStyle: React.CSSProperties,
-    cropAreaStyle: React.CSSProperties
-  };
-  classes?: {
-    containerClassName: string,
-    imageClassName: string,
-    cropAreaClassName: string
-  };
-  restrictPosition?: boolean;
-  initialCroppedAreaPixels?: Area;
-}
-
-class App extends React.Component<{}, State> {
-  constructor(props: any) {
-    super(props);
-    this.state = {
-      imageSrc:
+const App = () => {
+  const [state, setState] = React.useState<Omit<CropperProps,"onCropChange">>({
+      image:
         'https://img.huffingtonpost.com/asset/5ab4d4ac2000007d06eb2c56.jpeg?cache=sih0jwle4e&ops=1910_1000',
       crop: { x: 0, y: 0 },
       zoom: 1,
+      rotation: 0,
       aspect: 4 / 3,
       minZoom: 1,
       maxZoom: 10,
@@ -61,58 +34,58 @@ class App extends React.Component<{}, State> {
           x: 10,
           y: 10
       }
-    };
-  }
+    });
+  
 
-  onCropChange = (crop: Location) => {
+  const onCropChange = (crop: Location) => {
     console.log(`onCropChange: { x: ${crop.x}, y: ${crop.y} }`);
-    this.setState({ crop });
+    setState(s=>({...s, crop }));
   }
 
-  onCropComplete = (croppedArea: Area, croppedAreaPixels: Area) => {
+  const onCropComplete = (croppedArea: Area, croppedAreaPixels: Area) => {
     console.log('onCropComplete:');
     console.log(croppedArea, croppedAreaPixels);
   }
 
-  onZoomChange = (zoom: number) => {
+  const onZoomChange = (zoom: number) => {
     console.log(`onZoomChange: ${zoom}`);
-    this.setState({ zoom });
+    setState(s=>({...s, zoom }));
   }
 
-  onImgError = () => {
+  const onImgError = () => {
     console.log(`onImgError`);
   }
 
-  render() {
-    return (
-      <div className="App">
-        <div className="crop-container">
-          <Cropper
-            image={this.state.imageSrc}
-            crop={this.state.crop}
-            zoom={this.state.zoom}
-            aspect={this.state.aspect}
-            minZoom={this.state.minZoom}
-            maxZoom={this.state.maxZoom}
-            cropShape={this.state.cropShape}
-            cropSize={this.state.cropSize}
-            showGrid={this.state.showGrid}
-            zoomSpeed={this.state.zoomSpeed}
-            crossOrigin={this.state.crossOrigin}
-            onCropChange={this.onCropChange}
-            onCropComplete={this.onCropComplete}
-            onZoomChange={this.onZoomChange}
-            onImgError={this.onImgError}
-            style={this.state.style}
-            classes={this.state.classes}
-            restrictPosition={this.state.restrictPosition}
-            initialCroppedAreaPixels={this.state.initialCroppedAreaPixels}
-          />
-        </div>
+  return (
+    <div className="App">
+      <div className="crop-container">
+        <Cropper
+          image={state.image}
+          crop={state.crop}
+          zoom={state.zoom}
+          rotation={state.rotation}
+          aspect={state.aspect}
+          minZoom={state.minZoom}
+          maxZoom={state.maxZoom}
+          cropShape={state.cropShape}
+          cropSize={state.cropSize}
+          showGrid={state.showGrid}
+          zoomSpeed={state.zoomSpeed}
+          crossOrigin={state.crossOrigin}
+          onCropChange={onCropChange}
+          onCropComplete={onCropComplete}
+          onZoomChange={onZoomChange}
+          onImgError={onImgError}
+          style={state.style}
+          classes={state.classes}
+          restrictPosition={state.restrictPosition}
+          initialCroppedAreaPixels={state.initialCroppedAreaPixels}
+        />
       </div>
-    );
-  }
-}
+    </div>
+  );
+};
+
 
 const AppFactory = React.createFactory(App);
 
