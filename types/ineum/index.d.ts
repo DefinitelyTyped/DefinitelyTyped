c1f2c058679ec2b32316b1f91e2ef977b30b8c03
Enzo Volkmann <enzo@exportarts.io>
2019-09-10 15:15:27 +0200

    Added tests and fixed header lint issue

--- types/ineum/index.d.ts
+++ types/ineum/index.d.ts
@@ -1,4 +1,4 @@
-// Type definitions for ineum, the Website Monitoring API from Instana (status: 2019-09-10)
+// Type definitions for non-npm package ineum-browser, the Website Monitoring API from Instana (status: 2019-09-10)
 // Project: https://docs.instana.io/products/website_monitoring/api/
 // Definitions by: Enzo Volkmann <https://github.com/evolkmann>
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
