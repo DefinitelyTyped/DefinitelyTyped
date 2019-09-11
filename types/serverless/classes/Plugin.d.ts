4954904c142a362a19d0c7c9a6b52e5582a7b3c2
timtrinidad <timtrinidad@gmail.com>
2019-09-11 16:22:41 -0700

    Update serverless types
    This fixes a couple of inconsistencies with the type declarations:
    * `Plugin.hooks` is a dictionary of functions that return a promise, not a dictionary of promises
    * `serverless.service.provider.compiledCloudFormationTemplate.Resources` is an object rather than an array

--- types/serverless/classes/Plugin.d.ts
+++ types/serverless/classes/Plugin.d.ts
@@ -2,7 +2,7 @@ import Serverless = require("../index");
 
 declare abstract class Plugin {
     hooks: {
-        [event: string]: Promise<any>;
+        [event: string]: () => Promise<any>;
     };
 
     constructor(serverless: Serverless, options: Serverless.Options)
