d207e01a74d1171c63b8211952ad73161ba3af5c
Max Boguslavsky <521205+maxbogus@users.noreply.github.com>
2019-09-12 21:39:26 +0300

    Update types/html-webpack-tags-plugin/index.d.ts
    Co-Authored-By: Ron Buckton <ron.buckton@microsoft.com>

--- types/html-webpack-tags-plugin/index.d.ts
+++ types/html-webpack-tags-plugin/index.d.ts
@@ -1,6 +1,8 @@
 // Type definitions for html-webpack-tags-plugin 2.0
 // Project: https://github.com/jharris4/html-webpack-tags-plugin
-// Definitions by: // Definitions by: 贺师俊 <https://github.com/hax>, Vladimir Grenaderov <https://github.com/VladimirGrenaderov>, Max Boguslavskiy <https://github.com/maxbogus>
+// Definitions by: 贺师俊 <https://github.com/hax>
+//                 Vladimir Grenaderov <https://github.com/VladimirGrenaderov>
+//                 Max Boguslavskiy <https://github.com/maxbogus>
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
 // TypeScript Version: 2.3
 
