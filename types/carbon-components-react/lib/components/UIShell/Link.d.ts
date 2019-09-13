f67ef5ea9f4f6d2514bb62e3970df064462e16b2
Kyle Albert <kyle.albert@us.ibm.com>
2019-09-13 16:29:49 -0400

    Fix Link type.

--- types/carbon-components-react/lib/components/UIShell/Link.d.ts
+++ types/carbon-components-react/lib/components/UIShell/Link.d.ts
@@ -1,11 +1,12 @@
 import * as React from "react";
 import { ReactAnchorAttr, ShapeOf } from "../../../typings/shared";
 
+type InnerElementProps<P> = Omit<P, "element">;
 export interface LinkPropsBase<P = any> {
-    element?: string | React.JSXElementConstructor<P>, // required but has default value
+    element?: string | React.JSXElementConstructor<InnerElementProps<P>>, // required but has default value
 }
 
-export type LinkProps<P extends object = ReactAnchorAttr, IP = P> = ShapeOf<LinkPropsBase<Exclude<IP, "element">>, P>;
+export type LinkProps<P extends object = ReactAnchorAttr, IP = P> = ShapeOf<LinkPropsBase<IP>, P>;
 
 declare function Link<P extends object = ReactAnchorAttr>(
     props: React.PropsWithChildren<LinkProps<P>>,
