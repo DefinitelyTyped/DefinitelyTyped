2c52e73c3e555057557860ef298525f345e2c976
sashko <7355623+a-pavlenko@users.noreply.github.com>
2019-09-14 10:12:15 +0300

    [serverless] extend plugin types

--- types/serverless/classes/Plugin.d.ts
+++ types/serverless/classes/Plugin.d.ts
@@ -1,11 +1,26 @@
-import Serverless = require("../index");
+import Serverless = require('../index');
 
 declare abstract class Plugin {
     hooks: {
-        [event: string]: Promise<any>;
+        [event: string]: (...rest: any[]) => any;
     };
 
-    constructor(serverless: Serverless, options: Serverless.Options)
+    commands?: {
+        [command: string]: {
+            usage?: string;
+            lifecycleEvents?: string[];
+            commands?: { [command: string]: {} };
+            options?: {
+                [option: string]: {
+                    usage?: string;
+                    required?: boolean;
+                    shortcut?: string;
+                };
+            };
+        };
+    };
+
+    constructor(serverless: Serverless, options: Serverless.Options);
 }
 
 export = Plugin;
