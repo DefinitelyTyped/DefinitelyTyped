30750093581818581c8b0286a345d8777c6a986e
sashko <7355623+a-pavlenko@users.noreply.github.com>
2019-09-14 11:11:57 +0300

    [serverless] fix identation

--- types/serverless/serverless-tests.ts
+++ types/serverless/serverless-tests.ts
@@ -2,36 +2,36 @@ import Serverless from 'serverless';
 import Plugin from 'serverless/classes/Plugin';
 
 const options: Serverless.Options = {
-  noDeploy: false,
-  stage: null,
-  region: '',
+    noDeploy: false,
+    stage: null,
+    region: '',
 };
 
 const serverless = new Serverless();
 
 class CustomPlugin extends Plugin {
-  commands = {
-    command: {
-      usage: 'description',
-      lifecycleEvents: ['start'],
-      options: {
-        option: {
-          usage: `description`,
-          required: true,
-          shortcut: 'o',
+    commands = {
+        command: {
+            usage: 'description',
+            lifecycleEvents: ['start'],
+            options: {
+                option: {
+                    usage: `description`,
+                    required: true,
+                    shortcut: 'o',
+                },
+            },
         },
-      },
-    },
-  };
-
-  customProp = {};
-
-  hooks: Plugin.Hooks;
-
-  constructor(serverless: Serverless, options: Serverless.Options) {
-    super(serverless, options);
-    this.hooks = {
-      'command:start': () => {},
     };
-  }
+
+    customProp = {};
+
+    hooks: Plugin.Hooks;
+
+    constructor(serverless: Serverless, options: Serverless.Options) {
+        super(serverless, options);
+        this.hooks = {
+            'command:start': () => {},
+        };
+    }
 }
