3133f5080c77a42284c1ed610c87c2f2a54112ef
Candid Dauth <cdauth@cdauth.eu>
2019-09-15 09:25:14 +0200

    Revert back to namespace approach

--- types/av/decoder.d.ts
+++ types/av/decoder.d.ts
@@ -1,35 +1,35 @@
-import { Bitstream } from "./bitstream";
-import { TypedArray } from "./baseTypes";
-import { Stream } from "./stream";
-import { Buffer } from "./buffer";
+/// <reference path="./baseTypes.d.ts" />
+/// <reference path="./bitstream.d.ts" />
 
-export abstract class Decoder {
-	static register(id: string, decoder: new () => Decoder): void;
-	static find(id: string): Decoder | null;
+declare namespace AV {
+	abstract class Decoder {
+		static register(id: string, decoder: new () => Decoder): void;
+		static find(id: string): Decoder | null;
 
-	stream: Stream;
-	bitstream: Bitstream;
-	receivedFinalBuffer: boolean;
+		stream: Stream;
+		bitstream: Bitstream;
+		receivedFinalBuffer: boolean;
 
-	decode(): boolean;
-	seek(timestamp: number): number;
+		decode(): boolean;
+		seek(timestamp: number): number;
 
-	abstract setCookie(cookie: Buffer): void;
-	abstract readChunk(): TypedArray;
+		abstract setCookie(cookie: Buffer): void;
+		abstract readChunk(): TypedArray;
 
-	on(event: "data", fn: (data: TypedArray, isLastBuffer: boolean) => void): void;
-	on(event: "end", fn: () => void): void;
-	on(event: "error", fn: (err: Error) => void): void;
+		on(event: "data", fn: (data: TypedArray, isLastBuffer: boolean) => void): void;
+		on(event: "end", fn: () => void): void;
+		on(event: "error", fn: (err: Error) => void): void;
 
-	off(event: "data", fn: (data: TypedArray, isLastBuffer: boolean) => void): void;
-	off(event: "end", fn: () => void): void;
-	off(event: "error", fn: (err: Error) => void): void;
+		off(event: "data", fn: (data: TypedArray, isLastBuffer: boolean) => void): void;
+		off(event: "end", fn: () => void): void;
+		off(event: "error", fn: (err: Error) => void): void;
 
-	once(event: "data", fn: (data: TypedArray, isLastBuffer: boolean) => void): void;
-	once(event: "end", fn: () => void): void;
-	once(event: "error", fn: (err: Error) => void): void;
+		once(event: "data", fn: (data: TypedArray, isLastBuffer: boolean) => void): void;
+		once(event: "end", fn: () => void): void;
+		once(event: "error", fn: (err: Error) => void): void;
 
-	emit(event: "data", fn: (data: TypedArray, isLastBuffer: boolean) => void): void;
-	emit(event: "end", fn: () => void): void;
-	emit(event: "error", fn: (err: Error) => void): void;
+		emit(event: "data", fn: (data: TypedArray, isLastBuffer: boolean) => void): void;
+		emit(event: "end", fn: () => void): void;
+		emit(event: "error", fn: (err: Error) => void): void;
+	}
 }
