a295b63192fa74c3d0e5957d9d140c23051a2a59
Auston Pramodh Barboza <austonpramodh@gmail.com>
2019-09-17 17:59:52 +0530

    added missing types for react--vertical-timeline-component(contentStyle,contentArrowStyle)

--- types/react-vertical-timeline-component/index.d.ts
+++ types/react-vertical-timeline-component/index.d.ts
@@ -1,10 +1,10 @@
 // Type definitions for react-vertical-timeline-component 2.3
 // Project: https://github.com/stephane-monnot/react-vertical-timeline, https://stephane-monnot.github.io/react-vertical-timeline
-// Definitions by: Stéphane Monnot <https://github.com/stephane-monnot>
+// Definitions by: Stéphane Monnot <https://github.com/stephane-monnot>, Auston Pramodh Barboza <https://github.com/austonpramodh>
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
 // TypeScript Version: 2.8
 
-import * as React from "react";
+import * as React from 'react';
 
 export interface VerticalTimelineProps {
     animate?: boolean;
@@ -22,7 +22,9 @@ export interface VerticalTimelineElementProps {
     position?: string;
     style?: React.CSSProperties;
     visibilitySensorProps?: any;
+    contentStyle?: React.CSSProperties;
+    contentArrowStyle?: React.CSSProperties;
 }
 
-export class VerticalTimeline extends React.Component<VerticalTimelineProps> { }
-export class VerticalTimelineElement extends React.Component<VerticalTimelineElementProps> { }
+export class VerticalTimeline extends React.Component<VerticalTimelineProps> {}
+export class VerticalTimelineElement extends React.Component<VerticalTimelineElementProps> {}
