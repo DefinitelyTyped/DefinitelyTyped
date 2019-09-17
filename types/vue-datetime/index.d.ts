db8fb3375579181e56356f9ccc6bfaa5ce84dea0
Olavo Rocha <olavo@exodus.eti.br>
2019-09-17 02:03:07 -0300

    npm test fixes for PR

--- types/vue-datetime/index.d.ts
+++ types/vue-datetime/index.d.ts
@@ -1,36 +1,25 @@
-
-
-// Type definitions for vue-datetime 1.0.0
+// Type definitions for vue-datetime 1.0
 // Project: https://github.com/mariomka/vue-datetime#readme
 // Definitions by: Olavo Rocha Neto <https://github.com/olavorn>
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
-// TypeScript Version: 3.6.2
+// TypeScript Version: 2.3
 
 import { VueConstructor } from 'vue';
-
 export const Datetime: VueDateTimeConstructor;
 export const DatetimePopup: VueDateTimeConstructor;
 export default Datetime;
 
 export interface VueDateTimeProps {
+    inputClass: any;
+    inputStyle: any;
 }
 
 export interface VueDateTimeData {
-}
-
-export interface VueDateTimeWatch {
-}
-
-export interface VueDateTimeMethods {
-}
-
-export interface VueDateTimeComputed {
+    value: string;
 }
 
 export interface VueDateTimeConstructor extends VueConstructor {
     props: VueDateTimeProps;
     data: () => VueDateTimeData;
-    watch: VueDateTimeWatch;
-    methods: VueDateTimeMethods;
-    computed: VueDateTimeComputed;
+    methods: any;
 }
