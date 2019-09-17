2404179caaec7d56680e7110468404afb9e645c5
dschmidt <dominik.schmidt@ametras.com>
2019-09-17 08:41:17 +0200

    Added vue the mask definitions

--- types/vue-the-mask/vue-the-mask-tests.ts
+++ types/vue-the-mask/vue-the-mask-tests.ts
@@ -1,14 +1,14 @@
 import Vue from 'vue';
-import {mask} from 'vue-the-mask';
+import { mask } from 'vue-the-mask';
 
 new Vue({
-    el: '#app',
-    data: {
-    },
-    directives: {
-        mask
-    },
-    template: `
+  el: '#app',
+  data: {},
+  directives: {
+    mask,
+  },
+  template: `
     <input mask="##.##.####" type="text">
-`
+`,
 });
+
