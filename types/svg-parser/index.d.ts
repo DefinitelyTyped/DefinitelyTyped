f381405208615e1de69abf9486625d66be70459b
JoelCodes <amnshinness@yahoo.com>
2019-09-18 19:31:21 -0700

    Adds types for new version of svg-parser

--- types/svg-parser/index.d.ts
+++ types/svg-parser/index.d.ts
@@ -1,14 +1,24 @@
-// Type definitions for svg-parser 1.0
+// Type definitions for svg-parser 2.0
 // Project: https://gitlab.com/Rich-Harris/svg-parser#README
 // Definitions by: mrmlnc <https://github.com/mrmlnc>
+//                 Joel Shinness <https://github.com/JoelCodes>
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
 // TypeScript Version: 2.1
 
-export interface Node {
-    name: string;
-    attributes: Record<string, string | number>;
-    children: Node[];
+export interface TextNode {
+    type: 'text';
+    value?: string|boolean|number;
+}
+
+export interface ElementNode {
+    type: string;
+    tagName?: string;
+    properties?: {};
+    children: Array<Node | string>;
+    value?: string;
     metadata?: string;
 }
 
-export function parse(content: string): Node;
+export type Node = TextNode | ElementNode;
+
+export function parse(source: string): Node;
