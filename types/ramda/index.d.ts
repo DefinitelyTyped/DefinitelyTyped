9e7c68ba0628428a5a1112fa7b866d91cba91756
Pierre-Antoine Mills <pierreantoine.urvoy@gmail.com>
2019-09-20 18:54:18 +0200

    fix: flatten & now works with unlimited nesting

--- types/ramda/index.d.ts
+++ types/ramda/index.d.ts
@@ -1398,7 +1398,11 @@ declare namespace R {
          * Returns a new list by pulling every item out of it (and all its sub-arrays) and putting
          * them in a new array, depth-first.
          */
-        flatten<T>(x: T[][] | T[][] | T[]): T[];
+        flatten<T extends any[]>(x: T): T extends Array<infer ST>
+                                        ? A.Extends<ST, any[]> extends 1
+                                          ? ST
+                                          : T
+                                        : never
 
         /**
          * Returns a new function much like the supplied one, except that the first two arguments'
