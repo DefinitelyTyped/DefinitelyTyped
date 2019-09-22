472e898fdc2bd691329ab84b97d796823d0027cb
Florin Asavoaie <FlorinAsavoaie@users.noreply.github.com>
2019-09-22 04:55:35 +0300

    aws-lambda: Added NoEcho parameter for CloudFormation Custom Resource Response
    According to https://docs.aws.amazon.com/AWSCloudFormation/latest/UserGuide/crpg-ref-responses.html

--- types/aws-lambda/index.d.ts
+++ types/aws-lambda/index.d.ts
@@ -383,6 +383,7 @@ export interface CloudFormationCustomResourceResponseCommon {
     Data?: {
         [Key: string]: any;
     };
+    NoEcho?: boolean;
 }
 
 export interface CloudFormationCustomResourceSuccessResponse extends CloudFormationCustomResourceResponseCommon {
