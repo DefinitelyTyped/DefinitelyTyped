514063b32b5a30769e41404a24f9edff70134546
Nathan Shively-Sanders <293473+sandersn@users.noreply.github.com>
2019-09-23 10:00:08 -0700

    Modify ramda tests to pass on typescript@next (#38547)
    Contextual signature instantation now catches the fact that thunkify
    would have produced an `any` during execution and now produces `unknown`
    instead. This breaks a test that expects the result of `thunkify` to be
    number. I changed the test to expect `unknown`, which is what `thunkify`
    produces when compiled with `typescript@next`.

--- types/ramda/ramda-tests.ts
+++ types/ramda/ramda-tests.ts
@@ -966,7 +966,7 @@ type Pair = KeyValuePair<string, number>;
 };
 
 () => {
-    const x: number = R.thunkify(R.identity)(42)();
+    const x: unknown = R.thunkify(R.identity)(42)();
     const y: number = R.thunkify((a: number, b: number) => a + b)(25, 17)();
     const z: number = R.thunkify((a: number, b: number) => a + b)(25)(17)();
 };
