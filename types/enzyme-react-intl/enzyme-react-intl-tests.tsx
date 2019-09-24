2e2f19350a0cfcbf5afd45b86f04653ee2f562b8
Mateusz Meller <mateusz.meller@axbit.com>
2019-09-24 08:56:02 +0200

    add test for shallowWithIntl/renderWithIntl/mountWithIntl using generic types

--- types/enzyme-react-intl/enzyme-react-intl-tests.tsx
+++ types/enzyme-react-intl/enzyme-react-intl-tests.tsx
@@ -7,9 +7,96 @@ class SampleComponent extends React.Component {
   }
 }
 
+interface Props {
+  id: number;
+}
+
+interface State {
+  constructed: boolean;
+}
+
+class PropsComponent extends React.Component<Props> {
+  render() {
+    return <div>{this.props.id}</div>;
+  }
+}
+
+class StateComponent extends React.Component<{}, State> {
+  constructor(props: {}) {
+    super(props);
+
+    this.state = {
+      constructed: true
+    };
+  }
+
+  render() {
+    return <div>{this.state.constructed ? 'Constructed' : 'Not constructed'}</div>;
+  }
+}
+
+class StatePropsComponent extends React.Component<Props, State> {
+  constructor(props: Props) {
+    super(props);
+
+    this.state = {
+      constructed: true
+    };
+  }
+
+  render() {
+    return (
+      <div>
+        <div>{this.state.constructed ? 'Constructed' : 'Not constructed'}</div>
+        <div>{this.props.id}</div>
+      </div>
+    );
+  }
+}
+
 intl.getLocale();
 intl.loadTranslation('en');
-intl.mountWithIntl(<SampleComponent />);
-intl.renderWithIntl(<SampleComponent />);
-intl.shallowWithIntl(<SampleComponent />);
+
+function mountWithIntlTests() {
+  intl.mountWithIntl(<SampleComponent />);
+
+  const propsComponentWrapper = intl.mountWithIntl<Props>(
+    <PropsComponent id={4} />,
+  );
+
+  propsComponentWrapper.props().id; // $ExpectType number
+
+  const stateComponentWrapper = intl.mountWithIntl<{}, State>(
+    <StateComponent />,
+  );
+
+  stateComponentWrapper.state().constructed; // $ExpectType boolean
+}
+
+function renderWithIntlTests() {
+  intl.renderWithIntl(<SampleComponent />);
+
+  const wrapper = intl.renderWithIntl<Props, State>(
+    <StatePropsComponent id={1} />,
+  );
+
+  wrapper.toggleClass('toggle');
+}
+
+function shallowWithIntlTests() {
+  intl.shallowWithIntl(<SampleComponent />);
+
+  const propsComponentWrapper = intl.shallowWithIntl<Props>(
+    <PropsComponent id={4} />,
+  );
+
+  propsComponentWrapper.props().id; // $ExpectType number
+
+  const stateComponentWrapper = intl.shallowWithIntl<{}, State>(
+    <StateComponent />,
+  );
+
+  stateComponentWrapper.state().constructed; // $ExpectType boolean
+}
+
 intl.setLocale('pl');
