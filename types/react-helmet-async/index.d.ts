26e78ebeef4f3325b53fb7fff28c6263ee9cb7a9
Dobes Vandermeer <dobesv@gmail.com>
2019-09-23 21:37:53 -0700

    react-helmet: remove default export
    This package does not have a default export.

--- types/react-helmet-async/index.d.ts
+++ types/react-helmet-async/index.d.ts
@@ -1,14 +1,14 @@
-// Type definitions for react-helmet-async 1.0
-// Project: https://github.com/staylor/react-helmet-async, https://github.com/nytimes/react-helmet-async
+// Type definitions for react-helmet-async 0.0
+// Project: https://github.com/staylor/react-helmet-async#readme
 // Definitions by: forabi <https://github.com/forabi>
-//                 Daniel Perez Alvarez <https://github.com/unindented>
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
-// TypeScript Version: 2.8
+// TypeScript Version: 2.6
 
-import * as React from "react";
-import { HelmetData } from "react-helmet";
+import * as React from 'react';
 
-export { default as Helmet } from "react-helmet";
+import { Helmet, HelmetData } from 'react-helmet';
+
+export { Helmet, HelmetData };
 
 export interface PopulatedContext {
     helmet: HelmetData;
