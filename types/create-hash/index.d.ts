0cc3ce307a1e16ab1faec4f4b8235376149864f9
Max Boguslavskiy <mboguslavskiy@aligntech.com>
2019-09-24 13:35:00 +0300

    updated signature for create Hash

--- types/create-hash/index.d.ts
+++ types/create-hash/index.d.ts
@@ -4,14 +4,6 @@
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
 // TypeScript Version: 2.1
 
-/// <reference types="node" />
-
-import { Hash } from 'crypto';
-
-export = createHash;
-
-declare function createHash(algo: createHash.Algorithm): Hash;
-
 declare namespace createHash {
     type Algorithm =
         | 'md5'
@@ -23,4 +15,15 @@ declare namespace createHash {
         | 'sha256'
         | 'sha384'
         | 'sha512';
+
+    type encoding = 'utf8' | 'hex' | 'base64';
+
+    interface HashAlgorithm {
+        update(data: string): void;
+        digest(target?: encoding): string;
+    }
 }
+
+declare function createHash(algo: createHash.Algorithm): createHash.HashAlgorithm;
+
+export = createHash;
