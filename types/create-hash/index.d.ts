630286145012023ff2b48b61d7ce681a36a6ea8f
Max Boguslavskiy <mboguslavskiy@aligntech.com>
2019-09-24 16:03:20 +0300

    updated credits

--- types/create-hash/index.d.ts
+++ types/create-hash/index.d.ts
@@ -1,6 +1,8 @@
 // Type definitions for create-hash 1.2
 // Project: https://github.com/crypto-browserify/createHash
-// Definitions by: BendingBender <https://github.com/BendingBender>
+// Definitions by: BendingBender <https://github.com/BendingBender>,
+//                 Konstantin Yuriev <https://github.com/gallowsmaker>
+//                 Max Boguslavskiy <https://github.com/maxbogus>
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
 // TypeScript Version: 2.1
 
