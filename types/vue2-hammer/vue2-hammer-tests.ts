110867d1b0da98a01197d2a282f041e0c1ab3284
Peter Tandler <p.github@tandlers.de>
2019-09-25 08:57:47 +0200

    Removed trailing whitespace in vue2-hammer-tests.ts

--- types/vue2-hammer/vue2-hammer-tests.ts
+++ types/vue2-hammer/vue2-hammer-tests.ts
@@ -12,7 +12,7 @@ new Vue({
     el: '#app',
     template: `
         <img
-			src="https://gist.githubusercontent.com/ptandler/5b676312b58242c801ac1cdabce07902/raw/cb9b8e2cd01f39836023c10c682028c153be4651/Enneagram.svg" 
+			src="https://gist.githubusercontent.com/ptandler/5b676312b58242c801ac1cdabce07902/raw/cb9b8e2cd01f39836023c10c682028c153be4651/Enneagram.svg"
 			 v-hammer:pinchstart="console.log('onPinchStart')"
 			 v-hammer:pinch="console.log('onPinch')"
 		/>
