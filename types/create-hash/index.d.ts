1c698914abdec0de2766e3281a66d7eb2bc3f1d7
Max Boguslavskiy <mboguslavskiy@aligntech.com>
2019-09-25 18:02:06 +0300

    rewrote definition to match both usages

--- types/create-hash/index.d.ts
+++ types/create-hash/index.d.ts
@@ -6,19 +6,7 @@
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
 // TypeScript Version: 2.1
 
-type encoding = 'utf8' | 'hex' | 'base64';
-type Algorithm =
-    | 'md5'
-    | 'rmd160'
-    | 'ripemd160'
-    | 'sha'
-    | 'sha1'
-    | 'sha224'
-    | 'sha256'
-    | 'sha384'
-    | 'sha512';
-
-export namespace createHash {
+declare namespace createHash {
     interface HashAlgorithm {
         update(data: string): void;
         digest(target?: encoding): string;
@@ -27,5 +15,19 @@ export namespace createHash {
         read(): void;
     }
 
-    export function createHash(algo: Algorithm): createHash.HashAlgorithm;
+    type encoding = 'utf8' | 'hex' | 'base64';
+    type algorithm =
+        | 'md5'
+        | 'rmd160'
+        | 'ripemd160'
+        | 'sha'
+        | 'sha1'
+        | 'sha224'
+        | 'sha256'
+        | 'sha384'
+        | 'sha512';
 }
+
+declare function createHash(algorithm: createHash.algorithm, options?: any): createHash.HashAlgorithm;
+
+export = createHash
