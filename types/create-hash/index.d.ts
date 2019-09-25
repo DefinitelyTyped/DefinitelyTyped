ab1091d091930e915c6216fea1bb7bfa17dc11be
Max Boguslavskiy <mboguslavskiy@aligntech.com>
2019-09-25 16:32:11 +0300

    fixed errors

--- types/create-hash/index.d.ts
+++ types/create-hash/index.d.ts
@@ -6,29 +6,27 @@
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
 // TypeScript Version: 2.1
 
-export declare namespace createHash {
-  type Algorithm =
-    | 'md5'
-    | 'rmd160'
-    | 'ripemd160'
-    | 'sha'
-    | 'sha1'
-    | 'sha224'
-    | 'sha256'
-    | 'sha384'
-    | 'sha512';
+export namespace createHash {
+    type Algorithm =
+        | 'md5'
+        | 'rmd160'
+        | 'ripemd160'
+        | 'sha'
+        | 'sha1'
+        | 'sha224'
+        | 'sha256'
+        | 'sha384'
+        | 'sha512';
 
-  type encoding = 'utf8' | 'hex' | 'base64';
+    type encoding = 'utf8' | 'hex' | 'base64';
 
-  interface HashAlgorithm {
-    update(data: string): void;
-    digest(target?: encoding): string;
-    write(data: string): void;
-    end(): void;
-    read(): void;
-  }
+    interface HashAlgorithm {
+        update(data: string): void;
+        digest(target?: encoding): string;
+        write(data: string): void;
+        end(): void;
+        read(): void;
+    }
 }
 
-export declare function createHash(
-  algo: createHash.Algorithm,
-): createHash.HashAlgorithm;
+export function createHash(algo: createHash.Algorithm): createHash.HashAlgorithm;
