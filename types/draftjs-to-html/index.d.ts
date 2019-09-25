d1b7b2e95eb15025c718255d78d647d360627b10
Ivan Zverev <ivan.zverev.ext@siemens.com>
2019-09-25 12:45:15 +0300

    Add types for draftjs-to-html

---
+++ types/draftjs-to-html/index.d.ts
@@ -0,0 +1,19 @@
+// Type definitions for draftjs-to-html 0.8
+// Project: https://github.com/jpuri/draftjs-to-html#readme
+// Definitions by: Ivan Zverev <https://github.com/1cheese>
+// Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
+// TypeScript Version: 2.9
+
+import { RawDraftContentState } from 'draft-js';
+
+export interface HashtagConfig {
+    trigger?: string;
+    separator?: string;
+}
+
+export default function draftToHtml(
+    editorContent: RawDraftContentState,
+    hashtagConfig?: HashtagConfig,
+    directional?: boolean,
+    customEntityTransform?: (...args: any[]) => any,
+): string;
