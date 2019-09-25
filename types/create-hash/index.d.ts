9ef6143e714a23033541a0eadb1eca8a3645086f
Max Boguslavskiy <mboguslavskiy@aligntech.com>
2019-09-25 17:07:32 +0300

    corrected signature

--- types/create-hash/index.d.ts
+++ types/create-hash/index.d.ts
@@ -6,27 +6,26 @@
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
 // TypeScript Version: 2.1
 
+type encoding = 'utf8' | 'hex' | 'base64';
+type Algorithm =
+    | 'md5'
+    | 'rmd160'
+    | 'ripemd160'
+    | 'sha'
+    | 'sha1'
+    | 'sha224'
+    | 'sha256'
+    | 'sha384'
+    | 'sha512';
+
 export namespace createHash {
-    type Algorithm =
-        | 'md5'
-        | 'rmd160'
-        | 'ripemd160'
-        | 'sha'
-        | 'sha1'
-        | 'sha224'
-        | 'sha256'
-        | 'sha384'
-        | 'sha512';
-
-    type encoding = 'utf8' | 'hex' | 'base64';
-
-    interface HashAlgorithm {
+    export interface HashAlgorithm {
         update(data: string): void;
         digest(target?: encoding): string;
         write(data: string): void;
         end(): void;
         read(): void;
     }
-}
 
-export function createHash(algo: createHash.Algorithm): createHash.HashAlgorithm;
+    export function createHash(algo: Algorithm): createHash.HashAlgorithm;
+}
