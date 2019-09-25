78679a081a639b5e47a579202c1eb4e1d42452e3
Max Boguslavskiy <mboguslavskiy@aligntech.com>
2019-09-25 16:28:54 +0300

    reworked exports

--- types/create-hash/index.d.ts
+++ types/create-hash/index.d.ts
@@ -7,26 +7,28 @@
 // TypeScript Version: 2.1
 
 export declare namespace createHash {
-    type Algorithm =
-        | 'md5'
-        | 'rmd160'
-        | 'ripemd160'
-        | 'sha'
-        | 'sha1'
-        | 'sha224'
-        | 'sha256'
-        | 'sha384'
-        | 'sha512';
+  type Algorithm =
+    | 'md5'
+    | 'rmd160'
+    | 'ripemd160'
+    | 'sha'
+    | 'sha1'
+    | 'sha224'
+    | 'sha256'
+    | 'sha384'
+    | 'sha512';
 
-    type encoding = 'utf8' | 'hex' | 'base64';
+  type encoding = 'utf8' | 'hex' | 'base64';
 
-    interface HashAlgorithm {
-        update(data: string): void;
-        digest(target?: encoding): string;
-        write(data: string): void;
-        end(): void;
-        read(): void;
-    }
+  interface HashAlgorithm {
+    update(data: string): void;
+    digest(target?: encoding): string;
+    write(data: string): void;
+    end(): void;
+    read(): void;
+  }
 }
 
-export declare function createHash(algo: createHash.Algorithm): createHash.HashAlgorithm;
+export declare function createHash(
+  algo: createHash.Algorithm,
+): createHash.HashAlgorithm;
