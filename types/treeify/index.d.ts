86aab67a0528761a60e0250410fdfd8f37ece4e7
Chris Arnesen <chris@alwaysai.co>
2019-09-26 08:23:41 -0700

    Loosen type signature for treeify#asTree
    Previously the type signature for asTree restricted the "treeObj" input be a string-indexed object with keys that were string or a `TreeObj`. This recursive type definition is unduly narrow because the implementation is far less restrictive; it'll stringify an array or an object whose values can be anything, even functions. In this update, I've typed the treeObj input as "object" which represents any non-primitive value. I've also renamed it to "obj" to be consistent with this module's documentation. I also made the showValues and hideFunctions arguments optional, as they are in the implementation. (undefined is treated as false per usual).

--- types/treeify/index.d.ts
+++ types/treeify/index.d.ts
@@ -1,27 +1,23 @@
-// Type definitions for treeify 1.0
+// Type definitions for treeify 1.1
 // Project: https://github.com/notatestuser/treeify
 // Definitions by: Mike North <https://github.com/mike-north>
+//                 Chris Arnesen <https://github.com/carnesen>
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
-// TypeScript Version: 2.1
-
-export interface TreeObject {
-    [k: string]: TreeValue;
-}
-export type TreeValue = string | TreeObject;
+// TypeScript Version: 2.2
 
 export function asTree(
-    treeObj: TreeObject,
-    showValues: boolean,
-    hideFunctions: boolean
+    obj: object,
+    showValues?: boolean,
+    hideFunctions?: boolean
 ): string;
 
 export function asLines(
-    treeObj: TreeObject,
+    obj: object,
     showValues: boolean,
     lineCallback: (line: string) => void
 ): string;
 export function asLines(
-    treeObj: TreeObject,
+    obj: object,
     showValues: boolean,
     hideFunctions: boolean,
     lineCallback: (line: string) => void
