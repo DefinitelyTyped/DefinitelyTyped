b435fd30fd864ba39588d519e82ad788bc6e09b4
Max Boguslavskiy <mboguslavskiy@aligntech.com>
2019-09-26 17:56:15 +0300

    added info

--- types/xml-c14n/index.d.ts
+++ types/xml-c14n/index.d.ts
@@ -1,3 +1,10 @@
+// Type definitions for xml-c14n 0.0.6
+// Project: https://github.com/deoxxa/xml-c14n
+// Definitions by: Konstantin Yuriev <https://github.com/gallowsmaker>
+//                 Max Boguslavskiy <https://github.com/maxbogus>
+// Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
+// TypeScript Version: 2.1
+
 declare module xml_c14n {
     type canonicaliseCb = (err: any, data: string) => void;
 
