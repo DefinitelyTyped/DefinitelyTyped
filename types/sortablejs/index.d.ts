2eb84edee01c010a710f67e8cb2fdfdbb07934d8
Wayne Van Son <waynevspersonal@gmail.com>
2019-09-27 13:34:39 +1000

    [docs] Updated version number for 1.7 to 1.10

--- types/sortablejs/index.d.ts
+++ types/sortablejs/index.d.ts
@@ -1,4 +1,4 @@
-// Type definitions for Sortable.js 1.7
+// Type definitions for Sortable.js 1.10
 // Project: https://github.com/RubaXa/Sortable
 // Definitions by: Maw-Fox <https://github.com/Maw-Fox>
 //                 Maarten Staa <https://github.com/maartenstaa>
