f10baa3b578c031a03aa93fc1804d664c3c105a8
Renan Machado <renan.mav@hotmail.com>
2019-09-27 17:28:27 -0300

    add fetchQuery

---
+++ types/entria__relay-experimental/entria__relay-experimental-tests.ts
@@ -0,0 +1,1 @@
+import { fetchQuery } from 'entria__relay-experimental';
--- types/entria__relay-experimental/index.d.ts
+++ types/entria__relay-experimental/index.d.ts
@@ -1,39 +1,7 @@
-// Type definitions for entria__relay-experimental 6.0.0
+// Type definitions for entria__relay-experimental 6.0
 // Project: https://github.com/baz/foo (Does not have to be to GitHub, but prefer linking to a source code repository rather than to a project website.)
 // Definitions by: Renan Machado <https://github.com/renanmav>
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
+// TypeScript Version: 3.0
 
-/*~ If this module is a UMD module that exposes a global variable 'myLib' when
- *~ loaded outside a module loader environment, declare that global here.
- *~ Otherwise, delete this declaration.
- */
-export as namespace myLib;
-
-/*~ If this module has methods, declare them as functions like so.
- */
-export function myMethod(a: string): string;
-export function myOtherMethod(a: number): number;
-
-/*~ You can declare types that are available via importing the module */
-export interface someType {
-    name: string;
-    length: number;
-    extras?: string[];
-}
-
-/*~ You can declare properties of the module using const, let, or var */
-export const myField: number;
-
-/*~ If there are types, properties, or methods inside dotted names
- *~ of the module, declare them inside a 'namespace'.
- */
-export namespace subProp {
-    /*~ For example, given this definition, someone could write:
-     *~   import { subProp } from 'yourModule';
-     *~   subProp.foo();
-     *~ or
-     *~   import * as yourMod from 'yourModule';
-     *~   yourMod.subProp.foo();
-     */
-    function foo(): void;
-}
+export { fetchQuery } from './lib/fetchQuery';
