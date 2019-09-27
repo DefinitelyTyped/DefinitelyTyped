0aed0b1e50233edd94b24f8eafd3c03811e8884e
Max Boguslavskiy <mboguslavskiy@aligntech.com>
2019-09-27 20:36:28 +0300

    added rollup-plugin-visualizer

---
+++ types/rollup-plugin-visualizer/index.d.ts
@@ -0,0 +1,22 @@
+// Type definitions for rollup-plugin-visualizer 2.6
+// Project: https://www.npmjs.com/package/rollup-plugin-visualizer
+// Definitions by: Nikolai Tsapkin <https://github.com/>
+//                 Max Boguslavskiy <https://github.com/maxbogus>
+// Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
+// TypeScript Version: 2.3
+
+/// <reference types="node" />
+import { Plugin } from 'rollup';
+
+interface PluginVisualizerOptions {
+    filename?: string;
+    title?: string;
+    sourcemap?: boolean;
+    open?: boolean;
+    template?: string;
+    bundlesRelative?: boolean;
+}
+
+declare function visualizer(options?: PluginVisualizerOptions): Plugin;
+
+export = visualizer;
---
+++ types/rollup-plugin-visualizer/rollup-plugin-visualizer-tests.ts
@@ -0,0 +1,13 @@
+import visualizer from 'rollup-plugin-visualizer';
+
+visualizer(); // $ExpectType Plugin
+const fullOptions = {
+    filename: 'filename',
+    title: 'title',
+    sourcemap: true,
+    open: true,
+    template: '',
+    bundlesRelative: true
+};
+visualizer({});
+visualizer(fullOptions);
