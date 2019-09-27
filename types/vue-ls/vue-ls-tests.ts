6bad437fb57ee4d861ef3852208a40d6475908c9
oleg.spakov <oleg.spakov@uta.fi>
2019-09-27 12:42:44 +0300

    authors corrected

--- types/vue-ls/vue-ls-tests.ts
+++ types/vue-ls/vue-ls-tests.ts
@@ -2,9 +2,9 @@ import Vue from 'vue';
 import VueStorage, { StorageOptions, StorageTypes } from 'vue-ls';
 
 const storageOptions: StorageOptions = {
-  namespace: 'my_project',
-  name: 'ls',
-  storage: StorageTypes.Local,
+    namespace: 'my_project',
+    name: 'ls',
+    storage: StorageTypes.Local,
 };
 
 Vue.use(VueStorage, storageOptions);
