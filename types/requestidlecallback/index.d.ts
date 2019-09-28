4505ae09b7d737133a5375885d11d7cdb987ccc0
teramotodaiki <i101323@gmail.com>
2019-09-28 15:44:21 +0900

    format

--- types/requestidlecallback/index.d.ts
+++ types/requestidlecallback/index.d.ts
@@ -12,15 +12,15 @@ export type IdleCallbackHandle = number;
 export type IdleRequestCallback = (deadline: IdleDeadline) => void;
 
 export interface IdleDeadline {
-	timeRemaining(): DOMHighResTimeStamp;
-	readonly didTimeout: boolean;
+    timeRemaining(): DOMHighResTimeStamp;
+    readonly didTimeout: boolean;
 }
 
 export interface IdleRequestOptions {
-	timeout: number;
+    timeout: number;
 }
 
 export interface Window {
-  requestIdleCallback(callback: IdleRequestCallback, options?: IdleRequestOptions): IdleCallbackHandle;
-  cancelIdleCallback(handle: number): void;
+    requestIdleCallback(callback: IdleRequestCallback, options?: IdleRequestOptions): IdleCallbackHandle;
+    cancelIdleCallback(handle: number): void;
 }
