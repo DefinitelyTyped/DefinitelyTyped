75bd28ef5f52ea8eeffb5d865c602ea7ffea3b36
teramotodaiki <i101323@gmail.com>
2019-09-28 15:36:32 +0900

    [requestidlecallback] export "request" instead
    aFarkas's requestIdleCallback polyfill does not export as "requestIdleCallback". It's "request" and "cancel".
    
    https://github.com/aFarkas/requestIdleCallback/blob/18e7d0036d970204e32451e6c4190003161176c0/index.js#L209-L212

--- types/requestidlecallback/index.d.ts
+++ types/requestidlecallback/index.d.ts
@@ -1,10 +1,10 @@
-// Type definitions for requestidlecallback 0.1
+// Type definitions for requestidlecallback 0.3
 // Project: https://w3c.github.io/requestidlecallback/, https://github.com/afarkas/requestidlecallback
-// Definitions by: 贺师俊 <https://github.com/hax>, Vladimir Grenaderov <https://github.com/VladimirGrenaderov>, Max Boguslavskiy <https://github.com/maxbogus>
+// Definitions by: 贺师俊 <https://github.com/hax>, Vladimir Grenaderov <https://github.com/VladimirGrenaderov>, Max Boguslavskiy <https://github.com/maxbogus>, Teramoto Daiki <https://github.com/teramotodaiki>
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
 
-export function requestIdleCallback(callback: IdleRequestCallback, options?: IdleRequestOptions): IdleCallbackHandle;
-export function cancelIdleCallback(handle: IdleCallbackHandle): void;
+export function request(callback: IdleRequestCallback, options?: IdleRequestOptions): IdleCallbackHandle;
+export function cancel(handle: IdleCallbackHandle): void;
 
 export type DOMHighResTimeStamp = number;
 export type IdleCallbackHandle = number;
