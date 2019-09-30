3dbe0db19a219c8105c8dbc65f5b173ca06855c0
Nathan Shively-Sanders <293473+sandersn@users.noreply.github.com>
2019-09-30 14:01:08 -0700

    Update to version 4.0

--- types/react-dnd-multi-backend/index.d.ts
+++ types/react-dnd-multi-backend/index.d.ts
@@ -1,4 +1,4 @@
-// Type definitions for react-dnd-multi-backend 3.0
+// Type definitions for react-dnd-multi-backend 4.0
 // Project: https://github.com/LouisBrunner/react-dnd-multi-backend, https://louisbrunner.github.io/dnd-multi-backend/packages/react-dnd-multi-backend
 // Definitions by: Janeene Beeforth <https://github.com/dawnmist>
 //                 Adam Haglund <https://github.com/beeequeue>
