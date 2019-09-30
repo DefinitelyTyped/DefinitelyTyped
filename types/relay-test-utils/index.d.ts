b445ce619c1d3de39e2e84ecad5689179d091cca
Renan Machado <renan.mav@hotmail.com>
2019-09-30 08:10:30 -0300

    create relay-test-utils package

---
+++ types/relay-test-utils/index.d.ts
@@ -0,0 +1,4 @@
+// Type definitions for relay-test-utils 6.0
+// Project: https://relay.dev
+// Definitions by: Renan Machado <https://github.com/renanmav>
+// Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
