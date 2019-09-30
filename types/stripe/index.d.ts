31092a33e7a1f15e1a560a915bb125a3350c1ca5
Stilborg <cb@nebulators.com>
2019-09-30 07:05:03 +0200

    Add requirements to IAccount

--- types/stripe/index.d.ts
+++ types/stripe/index.d.ts
@@ -216,6 +216,11 @@ declare namespace Stripe {
              * attached to this account
              */
             external_accounts?: IList<IExternalAccount>;
+
+            /**
+             * Information about the requirements for the account, including what information needs to be collected, and by when.
+             */
+            requirements?: IAccountRequirements;
         }
 
         interface IAccountCreationOptions extends IAccountUpdateOptions {
@@ -260,6 +265,39 @@ declare namespace Stripe {
              * This field is null unless business_type is set to individual.
              */
             individual?: IIndividualCreateUpdateOptions;
+        }
+
+        interface IAccountRequirements {
+            /**
+             * The date the fields in currently_due must be collected by to keep payouts enabled for the account.
+             * These fields might block payouts sooner if the next threshold is reached before these fields are collected.
+             */
+            current_deadline: number | null;
+
+            /**
+             * The fields that need to be collected to keep the account enabled. If not collected by the current_deadline, these fields appear in past_due as well, and the account is disabled.
+             */
+            currently_due?: string[];
+
+            /**
+             * If the account is disabled, this string describes why the account can’t create charges or receive payouts. Can be requirements.past_due, requirements.pending_verification, rejected.fraud, rejected.terms_of_service, rejected.listed, rejected.other, listed, under_review, or other.
+             */
+            disabled_reason?: string | null;
+
+            /**
+             * The fields that need to be collected assuming all volume thresholds are reached. As they become required, these fields appear in currently_due as well, and the current_deadline is set.
+             */
+            eventually_due?: string[];
+
+            /**
+             * The fields that weren’t collected by the current_deadline. These fields need to be collected to re-enable the account.
+             */
+            past_due?: string[];
+
+            /**
+             * Fields that may become required depending on the results of verification or review. An empty array unless an asynchronous verification is pending. If verification fails, the fields in this array become required and move to currently_due or past_due.
+             */
+            pending_verification?: string[];
         }
 
         interface IAccountShared {
