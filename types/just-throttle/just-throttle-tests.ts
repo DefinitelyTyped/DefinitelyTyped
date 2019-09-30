34d2312b648e7279c6a76c32fc56dde62daa2f4d
Dominik Rowicki <yzukonta@gmail.com>
2019-09-30 03:59:42 +0200

    Improve comments in tests slightly

--- types/just-throttle/just-throttle-tests.ts
+++ types/just-throttle/just-throttle-tests.ts
@@ -1,9 +1,11 @@
 import throttle = require('just-throttle');
 
-// returns a function that sets interval but doesn't do anything else by default
+// returns a function that sets an interval but doesn't do anything else by
+// default
 throttle(() => 'foo', 200); // $ExpectType () => void
 throttle(() => 'foo', 200, false); // $ExpectType () => void
 
-// immediately applies provided callback if third argument is true
+// if third argument is true, the returned function will immediately apply the
+// callback when called
 throttle(() => 'foo', 200, true); // $ExpectType () => string
 throttle((foo: string) => 42, 200, true); // $ExpectType (foo: string) => number
