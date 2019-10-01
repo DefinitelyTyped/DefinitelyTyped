11bd55f6abf79542148365f9f53062e80988ef78
Vladimir Alabov <vladimir.alabov@bsc-ideas.com>
2019-10-01 13:39:21 +0300

    [formol] Add types for formol package. Fix TS dependency version

--- types/formol/index.d.ts
+++ types/formol/index.d.ts
@@ -2,6 +2,7 @@
 // Project: https://github.com/Kozea/formol
 // Definitions by: today- <https://github.com/today->
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
+// TypeScript Version: 2.8
 
 import * as React from "react";
 
