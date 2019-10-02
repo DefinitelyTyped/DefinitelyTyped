6b40e7f4ca6c095dde70d53b376c519a6b64d41a
Divyun Raje Vaid <mail@divyun.com>
2019-10-03 03:06:20 +0530

    feat(pubnub): adds new typings for Object and signal
    also, consodidates the status interface.

--- types/pubnub/index.d.ts
+++ types/pubnub/index.d.ts
@@ -1,11 +1,12 @@
-// Type definitions for pubnub 4.0
+// Type definitions for pubnub 4.26
 // Project: https://github.com/pubnub/javascript
 // Definitions by:  bitbankinc <https://github.com/bitbankinc>,
 //                  rollymaduk <https://github.com/rollymaduk>,
 //                  vitosamson <https://github.com/vitosamson>,
 //                  FlorianDr <https://github.com/FlorianDr>,
 //                  danduh <https://github.com/danduh>,
-//                  ChristianBoehlke <https://github.com/ChristianBoehlke>
+//                  ChristianBoehlke <https://github.com/ChristianBoehlke>,
+//                  divyun <https://github.com/divyun>
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
 // @see https://www.pubnub.com/docs/web-javascript/api-reference-configuration
 // TypeScript Version: 2.2
@@ -33,21 +34,28 @@ declare class Pubnub {
 
     publish(
         params: Pubnub.PublishParameters,
-        callback: (status: Pubnub.PublishStatus, response: Pubnub.PublishResponse) => void,
+        callback: (status: Pubnub.PubnubStatus, response: Pubnub.PublishResponse) => void,
     ): void;
 
     publish(params: Pubnub.PublishParameters): Promise<Pubnub.PublishResponse>;
 
     fire(
         params: Pubnub.FireParameters,
-        callback: (status: Pubnub.PublishStatus, response: Pubnub.PublishResponse) => void,
+        callback: (status: Pubnub.PubnubStatus, response: Pubnub.PublishResponse) => void,
     ): void;
 
     fire(params: Pubnub.FireParameters): Promise<Pubnub.PublishResponse>;
 
+    signal(
+        params: Pubnub.SignalParameters,
+        callback: (status: Pubnub.PubnubStatus, response: Pubnub.SignalResponse) => void,
+    ): void;
+
+    signal(params: Pubnub.SignalParameters): Promise<Pubnub.SignalResponse>;
+
     history(
         params: Pubnub.HistoryParameters,
-        callback: (status: Pubnub.HistoryStatus, response: Pubnub.HistoryResponse) => void,
+        callback: (status: Pubnub.PubnubStatus, response: Pubnub.HistoryResponse) => void,
     ): void;
 
     subscribe(params: Pubnub.SubscribeParameters): void;
@@ -64,35 +72,161 @@ declare class Pubnub {
 
     hereNow(
         params: Pubnub.HereNowParameters,
-        callback: (status: Pubnub.HereNowStatus, response: Pubnub.HereNowResponse) => void,
+        callback: (status: Pubnub.PubnubStatus, response: Pubnub.HereNowResponse) => void,
     ): void;
 
     hereNow(params: Pubnub.HereNowParameters): Promise<Pubnub.HereNowResponse>;
 
     whereNow(
         params: Pubnub.WhereNowParameters,
-        callback: (status: Pubnub.WhereNowStatus, response: Pubnub.WhereNowResponse) => void,
+        callback: (status: Pubnub.PubnubStatus, response: Pubnub.WhereNowResponse) => void,
     ): void;
 
     whereNow(params: Pubnub.WhereNowParameters): Promise<Pubnub.WhereNowResponse>;
 
     getState(
         params: Pubnub.GetStateParameters,
-        callback: (status: Pubnub.GetStateStatus, state: Pubnub.GetStateResponse) => void,
+        callback: (status: Pubnub.PubnubStatus, state: Pubnub.GetStateResponse) => void,
     ): void;
 
     getState(params: Pubnub.GetStateParameters): Promise<Pubnub.GetStateResponse>;
 
     setState(
         params: Pubnub.SetStateParameters,
-        callback: (status: Pubnub.SetStateStatus, state: Pubnub.SetStateResponse) => void,
+        callback: (status: Pubnub.PubnubStatus, state: Pubnub.SetStateResponse) => void,
     ): void;
 
     setState(params: Pubnub.SetStateParameters): Promise<Pubnub.SetStateResponse>;
 
-    grant(params: Pubnub.GrantParameters, callback: (status: Pubnub.GrantStatus, response: {}) => void): void;
+    grant(params: Pubnub.GrantParameters, callback: (status: Pubnub.PubnubStatus, response: {}) => void): void;
 
     grant(params: Pubnub.GrantParameters): Promise<{}>;
+
+    createUser(
+        params: Pubnub.UserInputParameters,
+        callback: (status: Pubnub.PubnubStatus, response: Pubnub.GetUserResponse) => void,
+    ): void;
+
+    createUser(params: Pubnub.UserInputParameters): Promise<Pubnub.GetUserResponse>;
+
+    updateUser(
+        params: Pubnub.UserInputParameters,
+        callback: (status: Pubnub.PubnubStatus, response: Pubnub.GetUserResponse) => void,
+    ): void;
+
+    updateUser(params: Pubnub.UserInputParameters): Promise<Pubnub.GetUserResponse>;
+
+    deleteUser(
+        userId: string,
+        callback: (status: Pubnub.PubnubStatus, response: Pubnub.DeleteUserResponse) => void,
+    ): void;
+
+    deleteUser(userId: string): Promise<Pubnub.DeleteUserResponse>;
+
+    getUsers(
+        params: Pubnub.GetObjectsParameters,
+        callback: (status: Pubnub.PubnubStatus, response: Pubnub.GetUsersResponse) => void,
+    ): void;
+
+    getUsers(params: Pubnub.GetObjectsParameters): Promise<Pubnub.GetUsersResponse>;
+
+    getUser(
+        params: Pubnub.GetUserParameters,
+        callback: (status: Pubnub.PubnubStatus, response: Pubnub.GetUserResponse) => void,
+    ): void;
+
+    getUser(params: Pubnub.GetUserParameters): Promise<Pubnub.GetUserResponse>;
+
+    createSpace(
+        params: Pubnub.SpaceInputParameters,
+        callback: (status: Pubnub.PubnubStatus, response: Pubnub.GetSpaceResponse) => void,
+    ): void;
+
+    createSpace(params: Pubnub.SpaceInputParameters): Promise<Pubnub.GetSpaceResponse>;
+
+    updateSpace(
+        params: Pubnub.SpaceInputParameters,
+        callback: (status: Pubnub.PubnubStatus, response: Pubnub.GetSpaceResponse) => void,
+    ): void;
+
+    updateSpace(params: Pubnub.SpaceInputParameters): Promise<Pubnub.GetSpaceResponse>;
+
+    deleteSpace(
+        spaceId: string,
+        callback: (status: Pubnub.PubnubStatus, response: Pubnub.DeleteSpaceResponse) => void,
+    ): void;
+
+    deleteSpace(spaceId: string): Promise<Pubnub.DeleteSpaceResponse>;
+
+    getSpaces(
+        params: Pubnub.GetObjectsParameters,
+        callback: (status: Pubnub.PubnubStatus, response: Pubnub.GetSpacesResponse) => void,
+    ): void;
+
+    getSpaces(params: Pubnub.GetObjectsParameters): Promise<Pubnub.GetSpacesResponse>;
+
+    getSpace(
+        params: Pubnub.GetSpaceParameters,
+        callback: (status: Pubnub.PubnubStatus, response: Pubnub.GetSpaceResponse) => void,
+    ): void;
+
+    getSpace(params: Pubnub.GetSpaceParameters): Promise<Pubnub.GetSpaceResponse>;
+
+    getMemberships(
+        params: Pubnub.GetMembershipsParameters,
+        callback: (status: Pubnub.PubnubStatus, response: Pubnub.GetMembershipsResponse) => void,
+    ): void;
+
+    getMemberships(params: Pubnub.GetMembershipsParameters): Promise<Pubnub.GetMembershipsResponse>;
+
+    getMembers(
+        params: Pubnub.GetMembersParameters,
+        callback: (status: Pubnub.PubnubStatus, response: Pubnub.GetMembersResponse) => void,
+    ): void;
+
+    getMembers(params: Pubnub.GetMembersParameters): Promise<Pubnub.GetMembersResponse>;
+
+    joinSpaces(
+        params: Pubnub.MembershipsInputParameters,
+        callback: (status: Pubnub.PubnubStatus, response: Pubnub.GetMembershipsResponse) => void,
+    ): void;
+
+    joinSpaces(params: Pubnub.MembershipsInputParameters): Promise<Pubnub.GetMembershipsResponse>;
+
+    updateMemberships(
+        params: Pubnub.MembershipsInputParameters,
+        callback: (status: Pubnub.PubnubStatus, response: Pubnub.GetMembershipsResponse) => void,
+    ): void;
+
+    updateMemberships(params: Pubnub.MembershipsInputParameters): Promise<Pubnub.GetMembershipsResponse>;
+
+    leaveSpaces(
+        params: Pubnub.LeaveSpacesParameters,
+        callback: (status: Pubnub.PubnubStatus, response: Pubnub.GetMembershipsResponse) => void,
+    ): void;
+
+    leaveSpaces(params: Pubnub.LeaveSpacesParameters): Promise<Pubnub.GetMembershipsResponse>;
+
+    addMembers(
+        params: Pubnub.MembersInputParameters,
+        callback: (status: Pubnub.PubnubStatus, response: Pubnub.GetMembersResponse) => void,
+    ): void;
+
+    addMembers(params: Pubnub.MembersInputParameters): Promise<Pubnub.GetMembersResponse>;
+
+    updateMembers(
+        params: Pubnub.MembersInputParameters,
+        callback: (status: Pubnub.PubnubStatus, response: Pubnub.GetMembersResponse) => void,
+    ): void;
+
+    updateMembers(params: Pubnub.MembersInputParameters): Promise<Pubnub.GetMembersResponse>;
+
+    removeMembers(
+        params: Pubnub.RemoveMembersParameters,
+        callback: (status: Pubnub.PubnubStatus, response: Pubnub.GetMembersResponse) => void,
+    ): void;
+
+    removeMembers(params: Pubnub.RemoveMembersParameters): Promise<Pubnub.GetMembersResponse>;
 
     encrypt(data: string, customCipherKey?: string, options?: Pubnub.CryptoParameters): any;
 
@@ -177,6 +311,59 @@ declare namespace Pubnub {
         subscribedChannel: string;
     }
 
+    interface SignalEvent {
+        channel: string;
+        subscription: string;
+        timetoken: string;
+        message: any;
+        publisher: string;
+    }
+
+    interface UserEvent {
+        channel: string;
+        subscription: string;
+        timetoken: string;
+        message: {
+            event: string;
+            type: string;
+            data: UserData;
+        };
+        publisher: string;
+    }
+
+    interface SpaceEvent {
+        channel: string;
+        subscription: string;
+        timetoken: string;
+        message: {
+            event: string;
+            type: string;
+            data: SpaceData;
+        };
+        publisher: string;
+    }
+
+    interface MembershipData {
+        userId: string;
+        spaceId: string;
+        eTag: string;
+        created: string;
+        updated: string;
+        custom?: object | null;
+    }
+
+    interface MembershipEvent {
+        channel: string;
+        subscription: string;
+        timetoken: string;
+        message: {
+            event: string;
+            type: string;
+            data: MembershipData;
+        };
+        publisher: string;
+    }
+
     // publish
     interface PublishParameters {
         message: any;
@@ -188,6 +375,17 @@ declare namespace Pubnub {
     }
 
     interface PublishResponse {
+        timetoken: number;
+    }
+
+    // signal
+
+    interface SignalParameters {
+        message: any;
+        channel: string;
+    }
+
+    interface SignalResponse {
         timetoken: number;
     }
 
@@ -212,18 +410,12 @@ declare namespace Pubnub {
         messages: HistoryMessage[];
     }
 
-    interface HistoryStatus {
+    interface PubnubStatus {
         error: boolean;
+        category?: string; // see Pubnub.Categories
+        operation: string; // see Pubnub.Operations
+        statusCode: number;
         errorData?: Error;
-        operation: string; // see Pubnub.Operations
-        statusCode?: number;
-    }
-
-    interface PublishStatus {
-        operation: string; // see Pubnub.Operations
-        category: string; // see Pubnub.Categories;
-        error: boolean;
-        errorData: Error;
     }
 
     // fire
@@ -250,26 +442,26 @@ declare namespace Pubnub {
 
     // channelGroups
     interface ChannelGroups {
-        addChannels(params: AddChannelParameters, callback: (status: ChannelGroupStatus) => void): void;
+        addChannels(params: AddChannelParameters, callback: (status: PubnubStatus) => void): void;
 
         addChannels(params: AddChannelParameters): Promise<{}>;
 
-        removeChannels(params: RemoveChannelParameters, callback: (status: ChannelGroupStatus) => void): void;
+        removeChannels(params: RemoveChannelParameters, callback: (status: PubnubStatus) => void): void;
 
         removeChannels(params: RemoveChannelParameters): Promise<{}>;
 
         listChannels(
             params: ListChannelsParameters,
-            callback: (status: ChannelGroupStatus, response: ListChannelsResponse) => void,
+            callback: (status: PubnubStatus, response: ListChannelsResponse) => void,
         ): void;
 
         listChannels(params: ListChannelsParameters): Promise<ListChannelsResponse>;
 
-        listGroups(callback: (status: ChannelGroupStatus, response: ListAllGroupsResponse) => void): void;
+        listGroups(callback: (status: PubnubStatus, response: ListAllGroupsResponse) => void): void;
 
         listGroups(): Promise<ListAllGroupsResponse>;
 
-        deleteGroup(params: DeleteGroupParameters, callback: (status: ChannelGroupStatus) => void): void;
+        deleteGroup(params: DeleteGroupParameters, callback: (status: PubnubStatus) => void): void;
 
         deleteGroup(params: DeleteGroupParameters): Promise<{}>;
     }
@@ -300,13 +492,6 @@ declare namespace Pubnub {
         channels: string[];
     }
 
-    interface ChannelGroupStatus {
-        error: boolean;
-        errorData?: Error;
-        operation: string; // see Pubnub.Operations
-        statusCode?: number;
-    }
-
     // addListener
     interface ListenerParameters {
         status?(statusEvent: StatusEvent): void;
@@ -314,6 +499,14 @@ declare namespace Pubnub {
         message?(messageEvent: MessageEvent): void;
 
         presence?(presenceEvent: PresenceEvent): void;
+
+        signal?(signalEvent: SignalEvent): void;
+
+        user?(userEvent: UserEvent): void;
+
+        space?(spaceEvent: SpaceEvent): void;
+
+        membership?(membershipEvent: MembershipEvent): void;
     }
 
     // hereNow
@@ -339,21 +532,9 @@ declare namespace Pubnub {
         };
     }
 
-    interface HereNowStatus {
-        error: boolean;
-        operation: string; // see Pubnub.Operations;
-        statusCode: number;
-    }
-
     // whereNow
     interface WhereNowParameters {
         uuid?: string;
-    }
-
-    interface WhereNowStatus {
-        error: boolean;
-        operation: string;
-        statusCode: number;
     }
 
     interface WhereNowResponse {
@@ -367,12 +548,6 @@ declare namespace Pubnub {
         state?: any;
     }
 
-    interface SetStateStatus {
-        error: boolean;
-        operation: string;
-        statusCode: number;
-    }
-
     interface SetStateResponse {
         state: any;
     }
@@ -382,12 +557,6 @@ declare namespace Pubnub {
         uuid?: string;
         channels?: string[];
         channelGroups?: string[];
-    }
-
-    interface GetStateStatus {
-        error: boolean;
-        operation: string;
-        statusCode: number;
     }
 
     interface GetStateResponse {
@@ -407,10 +576,162 @@ declare namespace Pubnub {
         manage?: boolean;
     }
 
-    interface GrantStatus {
-        error: boolean;
-        operation: string;
-        statusCode: number;
+    // Object
+
+    interface ObjectData {
+        id: string;
+        eTag: string;
+        created: string;
+        updated: string;
+        custom?: object | null;
+    }
+
+    interface GetObjectsParameters {
+        limit?: number;
+        page?: {
+            next?: string;
+            prev?: string;
+        };
+        include?: {
+            customFields?: boolean;
+        };
+    }
+
+    interface DeleteObjectResponse {
+        status: number;
+        data: null;
+    }
+
+    // User
+    interface UserData extends ObjectData {
+        name: string;
+        externalId?: string | null;
+        profileUrl?: string | null;
+        email?: string | null;
+    }
+
+    interface UserInputParameters {
+        id: string;
+        name: string;
+        externalId?: string | null;
+        profileUrl?: string | null;
+        email?: string | null;
+        custom?: object | null;
+        include?: {
+            customFields?: boolean;
+        };
+    }
+
+    interface GetUsersResponse {
+        data: UserData[];
+        status: number;
+    }
+
+    interface DeleteUserResponse {
+        data: null;
+        status: number;
+    }
+
+    interface GetUserParameters {
+        userId: string;
+        include?: {
+            customFields?: boolean;
+        };
+    }
+
+    interface GetUserResponse {
+        data: UserData;
+        status: number;
+    }
+
+    // Space
+    interface SpaceData extends ObjectData {
+        id: string;
+        name: string;
+        description?: string | null;
+    }
+
+    interface SpaceInputParameters {
+        id: string;
+        name: string;
+        description?: string | null;
+        custom?: object | null;
+        include?: {
+            customFields?: boolean;
+        };
+    }
+
+    interface DeleteSpaceResponse {
+        status: number;
+    }
+
+    interface GetSpacesResponse {
+        data: SpaceData[];
+        prev?: string;
+        next?: string;
+        status: number;
+    }
+
+    interface GetSpaceParameters {
+        spaceId: string;
+        include?: {
+            customFields?: boolean;
+        };
+    }
+
+    interface GetSpaceResponse {
+        data: SpaceData;
+        status: number;
+    }
+
+    // Membership
+    interface GetMembershipsParameters extends GetObjectsParameters {
+        userId: string;
+    }
+
+    interface GetMembershipsResponse {
+        data: ObjectData[];
+        prev?: string;
+        next?: string;
+        status: number;
+    }
+
+    interface MembershipsInputParameters extends GetObjectsParameters {
+        userId: string;
+        spaces: Array<{
+            id: string;
+            custom?: object | null;
+        }>;
+    }
+
+    interface LeaveSpacesParameters extends GetObjectsParameters {
+        userId: string;
+        spaces: string[];
+    }
+
+    // Member
+    interface GetMembersParameters extends GetObjectsParameters {
+        spaceId: string;
+    }
+
+    interface GetMembersResponse {
+        data: ObjectData[];
+        prev?: string;
+        next?: string;
+        status: number;
+    }
+
+    interface MembersInputParameters extends GetObjectsParameters {
+        spaceId: string;
+        users: Array<{
+            id: string;
+            custom?: object | null;
+        }>;
+    }
+
+    interface RemoveMembersParameters extends GetObjectsParameters {
+        spaceId: string;
+        users: string[];
     }
 
     // encrypt & decrypt
@@ -461,6 +782,17 @@ declare namespace Pubnub {
         PNRemoveChannelsFromGroupOperation: string;
         PNAccessManagerGrant: string;
         PNAccessManagerAudit: string;
+        PNCreateUserOperation: string;
+        PNUpdateUserOperation: string;
+        PNDeleteUserOperation: string;
+        PNGetUsersOperation: string;
+        PNCreateSpaceOperation: string;
+        PNUpdateSpaceOperation: string;
+        PNDeleteSpaceOperation: string;
+        PNGetSpacesOperation: string;
+        PNGetMembershipsOperation: string;
+        PNGetMembersOperation: string;
+        PNUpdateMembershipsOperation: string;
     }
 }
 
