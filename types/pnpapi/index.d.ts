8963907c8c40d2ef17cf03cfdc3eb46bcf9af20b
Maël Nison <nison.mael@gmail.com>
2019-10-02 11:38:25 +0200

    Updates things

--- types/pnpapi/index.d.ts
+++ types/pnpapi/index.d.ts
@@ -3,6 +3,7 @@
 // Definitions by: Maël Nison <https://github.com/arcanis>
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
 // TypeScript Version: 2.1
+/// <reference types="node"/>
 
 declare namespace NodeJS {
     interface ProcessVersions {
@@ -26,28 +27,29 @@ declare module 'pnpapi' {
       | TopLevelPackageLocator;
 
     export interface PackageInformation {
-      packageLocation: string;
-      packageDependencies: Map<string, string | [string, string]>;
+        packageLocation: string;
+        packageDependencies: Map<string, null | string | [string, string]>;
+        linkType: 'HARD' | 'SOFT';
     }
 
     export const VERSIONS: { std: number; [key: string]: number };
 
     export const topLevel: { name: null; reference: null };
 
-    export function getPackageInformation(
-      locator: PackageLocator,
-    ): PackageInformation;
+    export function getDependencyTreeRoots(): PackageLocator[];
+
+    export function getPackageInformation(locator: PackageLocator): PackageInformation;
+
     export function findPackageLocator(location: string): PackageLocator | null;
 
     export function resolveToUnqualified(
-      request: string,
-      issuer: string | null,
-      opts?: { considerBuiltins?: boolean },
+        request: string,
+        issuer: string | null,
+        opts?: { considerBuiltins?: boolean },
     ): string | null;
-    export function resolveUnqualified(
-      unqualified: string,
-      opts?: { extensions?: string[] },
-    ): string;
+
+    export function resolveUnqualified(unqualified: string, opts?: { extensions?: string[] }): string;
+
     export function resolveRequest(
       request: string,
       issuer: string | null,
