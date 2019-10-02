14dd6dc145d8444adab7311cf12c06420e58ae0d
Maurice de Beijer <maurice.de.beijer@gmail.com>
2019-10-02 16:14:59 +0200

    Added new test case for the optional delay

--- types/scheduler/test/scheduler.ts
+++ types/scheduler/test/scheduler.ts
@@ -1,8 +1,9 @@
 import { unstable_scheduleCallback, unstable_cancelCallback, unstable_now, unstable_NormalPriority } from 'scheduler';
 
 // $ExpectType CallbackNode
-const callbackNode = unstable_scheduleCallback(unstable_NormalPriority, () => {}, { delay: 100 });
+const callbackNode = unstable_scheduleCallback(unstable_NormalPriority, () => {}, { timeout: 100 });
 unstable_cancelCallback(callbackNode);
+unstable_scheduleCallback(unstable_NormalPriority, () => {}, { delay: 100 });
 
 // $ExpectType number
 unstable_now();
