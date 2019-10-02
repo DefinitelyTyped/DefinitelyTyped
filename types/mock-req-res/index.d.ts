78bfae69a4b12368b2079af3b2de95144fd5e63f
Sandor Turanszky <sandor.turanszky@sandors-mbp.lan>
2019-10-02 15:48:58 +0200

    Add types for mock-req-res

---
+++ types/mock-req-res/index.d.ts
@@ -0,0 +1,53 @@
+
+// Type definitions for mock-req-res 1.1
+// Project: https://github.com/davesag/mock-req-res#readme
+// Definitions by: Sandor Turanszky <https://github.com/sandorTuranszky>
+// Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
+
+/// <reference types="express" />
+/// <reference types="sinon" />
+
+import { Request, Response } from "express";
+import { SinonStub, SinonSpy } from "sinon";
+
+declare namespace mockReqRes {
+	interface Dictionary<T> {
+		[key: string]: T;
+	}
+
+	type RequestPayload = Dictionary<any>;
+
+	type ResponsePayload = Dictionary<any>;
+
+	interface RequestOutput extends Request {
+		get: SinonStub;
+	}
+
+	interface ResponseOutput extends Response {
+		cookie: SinonSpy;
+		clearCookie: SinonSpy;
+		download: SinonSpy;
+		format: SinonSpy;
+		getHeader: SinonSpy;
+		json: SinonSpy;
+		jsonp: SinonSpy;
+		send: SinonSpy;
+		sendFile: SinonSpy;
+		sendStatus: SinonSpy;
+		setHeader: SinonSpy;
+		redirect: SinonSpy;
+		render: SinonSpy;
+		end: SinonSpy;
+		set: SinonSpy;
+		type: SinonSpy;
+		get: SinonStub;
+		status: SinonStub;
+		vary: SinonStub;
+	}
+
+	function mockRequest(options?: RequestPayload): RequestOutput;
+
+	function mockResponse(options?: ResponsePayload): ResponseOutput;
+}
+
+export = mockReqRes;
