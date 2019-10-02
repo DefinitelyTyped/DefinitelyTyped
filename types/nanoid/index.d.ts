20589de75a418ac05f89cdf7a97e7fa7c234cf5a
Sascha Zarhuber <sascha.zarhuber@gmail.com>
2019-10-02 14:25:39 +0200

    feat(nanoid): added newly introduced file declarations

--- types/nanoid/index.d.ts
+++ types/nanoid/index.d.ts
@@ -2,6 +2,7 @@
 // Project: https://github.com/ai/nanoid
 // Definitions by: Ruben Schmidmeister <https://github.com/bash>
 //                 Daniel Zhang <https://github.com/fadezhanger>
+//                 Sascha Zarhuber <https://github.com/saschazar21>
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
 
 declare function nanoid(size?: number): string;
