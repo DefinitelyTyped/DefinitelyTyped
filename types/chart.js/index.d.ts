2f2f2cbd4439e9b6171c34c9d750a39b075e7181
James Ah Yong <retrocraft314@gmail.com>
2019-10-03 18:46:45 -0400

    fix ChartLegendItem properties (#38746)

--- types/chart.js/index.d.ts
+++ types/chart.js/index.d.ts
@@ -152,6 +152,7 @@ declare namespace Chart {
         text?: string;
         fillStyle?: string;
         hidden?: boolean;
+        index?: number;
         lineCap?: 'butt' | 'round' | 'square';
         lineDash?: number[];
         lineDashOffset?: number;
@@ -162,7 +163,7 @@ declare namespace Chart {
     }
 
     interface ChartLegendLabelItem extends ChartLegendItem {
-        datasetIndex: number;
+        datasetIndex?: number;
     }
 
     interface ChartTooltipItem {
