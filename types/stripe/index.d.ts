6bb622a0f566b2fc6db68f002f6df1b66ad5e093
Jørgen <mail@jvik.no>
2019-10-03 22:33:26 +0200

    Fixed some alignment issues

--- types/stripe/index.d.ts
+++ types/stripe/index.d.ts
@@ -3834,11 +3834,11 @@ declare namespace Stripe {
             auto_advance?: boolean;
 
             /**
-             *@deprecated Use collection_method instead
+             * @deprecated Use collection_method instead
              */
-             billing?: 'charge_automatically' | 'send_invoice';
+            billing?: 'charge_automatically' | 'send_invoice';
 
-                /**
+            /**
              * Either `charge_automatically`, or `send_invoice`. When charging automatically, Stripe
              * will attempt to pay this invoice using the default source attached to the customer.
              * When sending an invoice, Stripe will email this invoice to the customer with payment
