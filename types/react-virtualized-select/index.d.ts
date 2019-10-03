193b84ff4a369a496ae23dba0c70a6da5fd2c81e
Adam Epling <adamepling@gmail.com>
2019-10-03 13:24:05 -0500

    Update index.d.ts
    Fix issue with callback

--- types/react-virtualized-select/index.d.ts
+++ types/react-virtualized-select/index.d.ts
@@ -24,7 +24,7 @@ export interface VirtualizedOptionRenderOptions<T> {
 
 export interface AdditionalVirtualizedSelectProps<TValue> {
     maxHeight?: number;
-    optionHeight?: number | (({ option: any }) => number);
+    optionHeight?: number | ((option) => number);
     optionRenderer?(options: VirtualizedOptionRenderOptions<TValue>): JSX.Element;
     selectComponent?: React.ComponentClass<any> | React.StatelessComponent<any>;
 }
