f2d05fe88281c7945d34ab00e11cf488c7c44642
Igor Zhukovich <igorzh@velmie.com>
2019-10-03 20:22:09 +0300

    fix(wellknow): parse result

--- types/wellknown/index.d.ts
+++ types/wellknown/index.d.ts
@@ -3,5 +3,8 @@
 // Definitions by: Yair Tawil <https://github.com/yairtawil>
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
 
-export function parse(input: string): {};
+export function parse(input: string): {
+    type: string,
+    coordinates: [ [ [number, number] ] ]
+};
 export function stringify(gj: {}): string;
