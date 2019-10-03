3b06302c51f93f33a2e182342d93897f0c766e24
Dobes Vandermeer <dobesv@gmail.com>
2019-10-03 11:40:53 -0700

    handleKeyCommand may return null
    Even some values of DraftEditorCommand may result in a null return
    from handleKeyCommand, for example 'move-selection-to-start-of-block'.

--- types/draft-js/index.d.ts
+++ types/draft-js/index.d.ts
@@ -935,7 +935,7 @@ declare namespace Draft {
                 static getCurrentBlockType(editorState: EditorState): string;
                 static getDataObjectForLinkURL(uri: URI): Object;
 
-                static handleKeyCommand(editorState: EditorState, command: DraftEditorCommand): EditorState;
+                static handleKeyCommand(editorState: EditorState, command: DraftEditorCommand): EditorState | null;
                 static handleKeyCommand(editorState: EditorState, command: string): null;
 
                 static insertSoftNewline(editorState: EditorState): EditorState;
