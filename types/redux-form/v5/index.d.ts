53fb3da947acbb62666f776fc93f1d85fe0b2133
Tim de Koning <git@king-management.nl>
2019-10-06 14:35:17 +0200

    Allow parse of types header

--- types/redux-form/v5/index.d.ts
+++ types/redux-form/v5/index.d.ts
@@ -1,11 +1,11 @@
 // Type definitions for redux-form 5.0
 // Project: https://github.com/erikras/redux-form
 // Definitions by: Tim de Koning <https://github.com/reggino>
-// Definitions based on work by: Daniel Lytkin <https://github.com/aikoven>
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
 // TypeScript Version: 3.6
 
 // tslint:disable:interface-name
+// Based on work by: Daniel Lytkin <https://github.com/aikoven>
 
 import * as React from 'react';
 import { Dispatch, Reducer } from 'redux';
