459e0f00a9df4ccd47b0ae7b2f29c84ed40ceb3a
ExE Boss <3889017+ExE-Boss@users.noreply.github.com>
2019-10-07 17:30:00 +0200

    fix(es‑to‑primitive): Add missing primitive types

--- types/es-to-primitive/ts3.2/es2015.d.ts
+++ types/es-to-primitive/ts3.2/es2015.d.ts
@@ -1,5 +1,5 @@
 declare function ToPrimitive(
 	input: any,
 	hint?: typeof String | typeof Number,
-): string | number | boolean | symbol | null | undefined;
+): string | number | bigint | boolean | symbol | null | undefined;
 export = ToPrimitive;
