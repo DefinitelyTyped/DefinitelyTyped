544636d859dd29ffe7e688fc9e9feb5e2e9701da
Florian Rohrer <Florian.Rohrer@vivaldi.net>
2019-10-07 17:22:24 +0200

    Update index.d.ts

--- types/grecaptcha/index.d.ts
+++ types/grecaptcha/index.d.ts
@@ -1,4 +1,4 @@
-// Type definitions for Google Recaptcha 2.0
+// Type definitions for Google Recaptcha 3.0
 // Project: https://www.google.com/recaptcha
 // Definitions by: Kristof Mattei <http://kristofmattei.be>
 //                 Martin Costello <https://martincostello.com/>
