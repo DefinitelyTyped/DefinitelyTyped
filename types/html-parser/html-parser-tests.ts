d333c85ee5bf1b763b5e359d891953f693413c43
Max Boguslavskiy <mboguslavskiy@aligntech.com>
2019-10-07 16:27:47 +0300

    corrected formatting

--- types/html-parser/html-parser-tests.ts
+++ types/html-parser/html-parser-tests.ts
@@ -1,10 +1,10 @@
 import { parse, parseFile, sanitize, RegExpOptions, CallbacksOption, Token } from 'html-parser';
 
-//test data
-const attributes = ( arg: string ) => arg;
-const elements = ( arg: string ) => arg;
-const comments = ( arg: boolean ) => arg;
-const docTypes = ( arg: boolean ) => arg;
+// test data
+const attributes = (arg: string) => arg;
+const elements = (arg: string) => arg;
+const comments = (arg: boolean) => arg;
+const docTypes = (arg: boolean) => arg;
 const attribute = (name: string, value: string) => {};
 const openElement = (name: string) => {};
 const closeOpenedElement = (name: string, token: Token, isUnary: boolean) => {};
@@ -16,7 +16,7 @@ const xmlProlog = () => {};
 const text = (name: string) => {
 };
 const emptyRegExpOptions: RegExpOptions = {};
-const filledRegExpOptions: RegExpOptions = { attribute: new RegExp( '' ), name: new RegExp( '' ) };
+const filledRegExpOptions: RegExpOptions = {attribute: new RegExp(''), name: new RegExp('')};
 const emptyCallbackOptions: CallbacksOption = {};
 const filled: CallbacksOption = {
     attribute,
@@ -31,15 +31,15 @@ const filled: CallbacksOption = {
 };
 
 // parse tests:
-parse( '' );
-parse( '', emptyCallbackOptions, emptyRegExpOptions );
-parse( '', filled, filledRegExpOptions );
+parse('');
+parse('', emptyCallbackOptions, emptyRegExpOptions);
+parse('', filled, filledRegExpOptions);
 
 // parseFile tests:
-parseFile( '', '', emptyCallbackOptions, attributes );
-parseFile( '', '', filled, attributes );
+parseFile('', '', emptyCallbackOptions, attributes);
+parseFile('', '', filled, attributes);
 
 // sanitize tests:
-sanitize( '' );
-sanitize( '', { attributes: [ 'hello' ], elements: [ 'hi' ], comments: true, docTypes: false } );
-sanitize( '', { attributes, elements, comments, docTypes } );
+sanitize('');
+sanitize('', {attributes: [ 'hello' ], elements: [ 'hi' ], comments: true, docTypes: false});
+sanitize('', {attributes, elements, comments, docTypes });
