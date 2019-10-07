c8543e92a3d2bf326ab5ff8b15577bc8db8b33f7
Max Boguslavskiy <mboguslavskiy@aligntech.com>
2019-10-07 21:34:38 +0300

    added typescript version

--- types/bufferhelper/index.d.ts
+++ types/bufferhelper/index.d.ts
@@ -3,6 +3,7 @@
 // Definitions by: Vladimir Grenaderov <https://github.com/VladimirGrenaderov>,
 //                 Max Boguslavskiy <https://github.com/maxbogus>
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
+// TypeScript Version: 2.7
 
 /// <reference types="node" />
 
