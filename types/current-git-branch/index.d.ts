c1379336622212db1e85e4b2c63e9b20fd136c21
Max Boguslavskiy <mboguslavskiy@aligntech.com>
2019-10-07 20:13:34 +0300

    fixed version

--- types/current-git-branch/index.d.ts
+++ types/current-git-branch/index.d.ts
@@ -3,7 +3,7 @@
 // Definitions by: Vladimir Grenaderov <https://github.com/VladimirGrenaderov>,
 //                 Max Boguslavskiy <https://github.com/maxbogus>
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
-// TypeScript Version: 2.2
+// TypeScript Version: 2.7
 
 declare namespace CurrentGitBranch {
     type CurrentGitBranchOptions = CurrentGitBranchOptionsObject | string[] | string;
