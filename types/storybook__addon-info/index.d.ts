2162217c136b00d00f6563cc80546c799326d4c5
Michaël De Boey <info@michaeldeboey.be>
2019-10-08 23:22:55 +0200

    Fix errors

--- types/storybook__addon-info/index.d.ts
+++ types/storybook__addon-info/index.d.ts
@@ -7,48 +7,48 @@
 // TypeScript Version: 3.1
 
 import { ComponentType, ReactElement } from 'react';
-import { RenderFunction, StoryDecorator } from '@storybook/react';
+import { DecoratorFunction, StoryFn } from '@storybook/react';
 
 export interface WrapStoryProps {
-  storyFn?: RenderFunction;
-  context?: object;
-  options?: object;
+    storyFn?: StoryFn;
+    context?: object;
+    options?: object;
 }
 
 export interface Options {
-    text?: string;
-    header?: boolean;
-    inline?: boolean;
-    source?: boolean;
-    propTables?: Array<ComponentType<any>> | false;
-    propTablesExclude?: Array<ComponentType<any>>;
-    styles?: object;
-    components?: {
-        [key: string]: ComponentType<any>;
-    };
-    marksyConf?: object;
-    maxPropsIntoLine?: number;
-    maxPropObjectKeys?: number;
-    maxPropArrayLength?: number;
-    maxPropStringLength?: number;
-    TableComponent?: ComponentType<{
-        propDefinitions: Array<{
-            property: string;
-            propType: object | string; // TODO: info about what this object is...
-            required: boolean;
-            description: string;
-            defaultValue: any;
-        }>;
+  text?: string;
+  header?: boolean;
+  inline?: boolean;
+  source?: boolean;
+  propTables?: Array<ComponentType<any>> | false;
+  propTablesExclude?: Array<ComponentType<any>>;
+  styles?: object;
+  components?: {
+    [key: string]: ComponentType<any>;
+  };
+  marksyConf?: object;
+  maxPropsIntoLine?: number;
+  maxPropObjectKeys?: number;
+  maxPropArrayLength?: number;
+  maxPropStringLength?: number;
+  TableComponent?: ComponentType<{
+    propDefinitions: Array<{
+      property: string;
+      propType: object | string; // TODO: info about what this object is...
+      required: boolean;
+      description: string;
+      defaultValue: any;
     }>;
-    excludedPropTypes?: string[];
+  }>;
+  excludedPropTypes?: string[];
 }
 
 // TODO: it would be better to use type inference for the parameters
 // type DecoratorParams = StoryDecorator extends (...a: infer A) => any ? A: never;
-export function withInfo(story: RenderFunction, context: { kind: string, story: string }): ReturnType<StoryDecorator>;
+export function withInfo(story: StoryFn, context: { kind: string; story: string }): ReturnType<DecoratorFunction>;
 // Legacy, but supported
 export function withInfo(
     textOrOptions?: string | Options,
-): (storyFn: RenderFunction) => (context?: object) => ReactElement<WrapStoryProps>;
+): (storyFn: StoryFn) => (context?: object) => ReactElement<WrapStoryProps>;
 
 export function setDefaults(newDefaults: Options): Options;
