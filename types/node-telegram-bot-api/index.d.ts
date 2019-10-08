440253e81b348ed7d0d33f6ac1031fddc2c6dab1
Aditya <thebeaditya@gmail.com>
2019-10-08 18:28:51 +0545

    Updated ChatMember interface
    - Rearranged ordering to match Telegram documentation
    - Added new property : is_member

--- types/node-telegram-bot-api/index.d.ts
+++ types/node-telegram-bot-api/index.d.ts
@@ -559,16 +559,18 @@ declare namespace TelegramBot {
         status: ChatMemberStatus;
         until_date?: number;
         can_be_edited?: boolean;
-        can_change_info?: boolean;
         can_post_messages?: boolean;
         can_edit_messages?: boolean;
         can_delete_messages?: boolean;
-        can_invite_users?: boolean;
         can_restrict_members?: boolean;
-        can_pin_messages?: boolean;
         can_promote_members?: boolean;
+        can_change_info?: boolean;
+        can_invite_users?: boolean;
+        can_pin_messages?: boolean;
+        is_member?: boolean;
         can_send_messages?: boolean;
         can_send_media_messages?: boolean;
+        can_send_polls: boolean;
         can_send_other_messages?: boolean;
         can_add_web_page_previews?: boolean;
     }
