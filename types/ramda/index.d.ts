e99d2d4e482b4a1f10523b7f6201dd413b33bcad
Saul-Mirone <xy610888871@gmail.com>
2019-10-08 10:12:57 +0800

    fix for the last reload

--- types/ramda/index.d.ts
+++ types/ramda/index.d.ts
@@ -2021,11 +2021,11 @@ declare namespace R {
          */
         o<T1, T2, R>(f: (x: T2) => R, g: (x: T1) => T2, v: T1): R;
         o<T1, T2, R>(f: (x: T2) => R, g: (x: T1) => T2): (v: T1) => R;
-        o<T1, T2, R>(
+        o<T2, R>(
             f: (x: T2) => R,
         ): {
-            (g: (x: T1) => T2, v: T1): R;
-            (g: (x: T1) => T2): (v: T1) => R;
+            <T1>(g: (x: T1) => T2, v: T1): R;
+            <T1>(g: (x: T1) => T2): (v: T1) => R;
         };
 
         /**
