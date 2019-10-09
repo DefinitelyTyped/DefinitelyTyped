595e29da14d2a343e4398b2000825f2922078885
Bill Parrott <bill@chimericdream.com>
2019-10-09 14:07:16 -0500

    [reactstrap] 'toggle()' and 'tag' prop updates (#38575)
    * [reactstrap] Update toggle() functions to use EventListeners instead of void functions
    
    * [reactstrap] update all tag props to also allow strings
    
    * [reactstrap] minor updates based on PR suggestions
    
    * [reactstrap] fix formatting from husky; address issues from PR

--- types/reactstrap/lib/ListGroup.d.ts
+++ types/reactstrap/lib/ListGroup.d.ts
@@ -2,11 +2,11 @@ import * as React from 'react';
 import { CSSModule } from '../index';
 
 export interface ListGroupProps extends React.HTMLAttributes<HTMLElement> {
-  [key: string]: any;
-  tag?: React.ReactType;
-  flush?: boolean;
-  className?: string;
-  cssModule?: CSSModule;
+    [key: string]: any;
+    tag?: string | React.ReactType;
+    flush?: boolean;
+    className?: string;
+    cssModule?: CSSModule;
 }
 
 declare class ListGroup<T = {[key: string]: any}> extends React.Component<ListGroupProps> {}
