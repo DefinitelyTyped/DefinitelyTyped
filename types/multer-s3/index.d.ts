9e020d2b9ecc689202ca7bf697e64ad395dcbf2a
abelkius <abelkius@gmail.com>
2019-10-09 12:50:01 +0200

    multer-s3 - extended definition to work with @koa/multer types

--- types/multer-s3/index.d.ts
+++ types/multer-s3/index.d.ts
@@ -7,17 +7,18 @@
 // TypeScript Version: 2.3
 
 import * as AWS from "aws-sdk";
+import { IncomingMessage } from 'http';
 import { StorageEngine } from "multer";
 
 interface Options {
     s3: AWS.S3;
-    bucket: ((req: Express.Request, file: Express.Multer.File, callback: (error: any, bucket?: string) => void) => void) | string;
-    key?(req: Express.Request, file: Express.Multer.File, callback: (error: any, key?: string) => void): void;
-    acl?: ((req: Express.Request, file: Express.Multer.File, callback: (error: any, acl?: string) => void) => void) | string;
-    contentType?(req: Express.Request, file: Express.Multer.File, callback: (error: any, mime?: string, stream?: NodeJS.ReadableStream) => void): void;
-    metadata?(req: Express.Request, file: Express.Multer.File, callback: (error: any, metadata?: any) => void): void;
-    cacheControl?: ((req: Express.Request, file: Express.Multer.File, callback: (error: any, cacheControl?: string) => void) => void) | string;
-    serverSideEncryption?: ((req: Express.Request, file: Express.Multer.File, callback: (error: any, serverSideEncryption?: string) => void) => void) | string;
+    bucket: ((req: Express.Request | IncomingMessage, file: Express.Multer.File | File, callback: (error: any, bucket?: string) => void) => void) | string;
+    key?(req: Express.Request | IncomingMessage, file: Express.Multer.File | File, callback: (error: any, key?: string) => void): void;
+    acl?: ((req: Express.Request | IncomingMessage, file: Express.Multer.File | File, callback: (error: any, acl?: string) => void) => void) | string;
+    contentType?(req: Express.Request | IncomingMessage, file: Express.Multer.File | File, callback: (error: any, mime?: string, stream?: NodeJS.ReadableStream) => void): void;
+    metadata?(req: Express.Request | IncomingMessage, file: Express.Multer.File | File, callback: (error: any, metadata?: any) => void): void;
+    cacheControl?: ((req: Express.Request | IncomingMessage, file: Express.Multer.File | File, callback: (error: any, cacheControl?: string) => void) => void) | string;
+    serverSideEncryption?: ((req: Express.Request | IncomingMessage, file: Express.Multer.File | File, callback: (error: any, serverSideEncryption?: string) => void) => void) | string;
 }
 
 declare global {
@@ -43,12 +44,12 @@ interface S3Storage {
     (options?: Options): StorageEngine;
 
     AUTO_CONTENT_TYPE(
-        req: Express.Request,
-        file: Express.Multer.File,
+        req: Express.Request | IncomingMessage,
+        file: Express.Multer.File | File,
         callback: (error: any, mime?: string, stream?: NodeJS.ReadableStream) => void): void;
     DEFAULT_CONTENT_TYPE(
-        req: Express.Request,
-        file: Express.Multer.File,
+        req: Express.Request | IncomingMessage,
+        file: Express.Multer.File | File,
         callback: (error: any, mime?: string) => void): void;
 }
 
