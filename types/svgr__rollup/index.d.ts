cf56d3dd881671faa50e1a9e0272e4051e26b9e8
Max Boguslavskiy <mboguslavskiy@aligntech.com>
2019-10-09 17:50:48 +0300

    added types for @types/svgr/rollup

---
+++ types/svgr__rollup/index.d.ts
@@ -0,0 +1,19 @@
+// Type definitions for @svgr/rollup 4.3
+// Project: https://github.com/smooth-code/svgr/tree/master/packages/rollup
+// Definitions by: Nick <https://github.com/fobdy>
+//                 Max Boguslavskiy <https://github.com/maxbogus>
+// Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
+
+import { Plugin } from 'rollup';
+
+declare namespace svgrRollup {
+    interface Options {
+        include?: string;
+        exclude: string;
+        babel: boolean;
+    }
+}
+
+declare function svgrRollup(options?: svgrRollup.Options): Plugin;
+
+export = svgrRollup;
