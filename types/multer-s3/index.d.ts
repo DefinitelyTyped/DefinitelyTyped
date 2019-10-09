7cd8d8035d4a04f82252f8099450baf5ef4d2614
abelkius <abelkius@gmail.com>
2019-10-09 13:00:41 +0200

    Added a union type for file and req used in Options and S3Storage

--- types/multer-s3/index.d.ts
+++ types/multer-s3/index.d.ts
@@ -3,6 +3,7 @@
 // Definitions by: KIM Jaesuck a.k.a. gim tcaesvk <https://github.com/tcaesvk>
 //                 Gal Talmor <https://github.com/galtalmor>
 //                 Matt Terski <https://github.com/terski>
+//                 Agata Belkius <https://github.com/abelkius>
 // Definitions: https://github.com/DefinitelyType/DefinitelyTyped
 // TypeScript Version: 2.3
 
@@ -10,15 +11,18 @@ import * as AWS from "aws-sdk";
 import { IncomingMessage } from 'http';
 import { StorageEngine } from "multer";
 
+type MulterRequest = Express.Request | IncomingMessage;
+type MulterFile = Express.Multer.File | File;
+
 interface Options {
     s3: AWS.S3;
-    bucket: ((req: Express.Request | IncomingMessage, file: Express.Multer.File | File, callback: (error: any, bucket?: string) => void) => void) | string;
-    key?(req: Express.Request | IncomingMessage, file: Express.Multer.File | File, callback: (error: any, key?: string) => void): void;
-    acl?: ((req: Express.Request | IncomingMessage, file: Express.Multer.File | File, callback: (error: any, acl?: string) => void) => void) | string;
-    contentType?(req: Express.Request | IncomingMessage, file: Express.Multer.File | File, callback: (error: any, mime?: string, stream?: NodeJS.ReadableStream) => void): void;
-    metadata?(req: Express.Request | IncomingMessage, file: Express.Multer.File | File, callback: (error: any, metadata?: any) => void): void;
-    cacheControl?: ((req: Express.Request | IncomingMessage, file: Express.Multer.File | File, callback: (error: any, cacheControl?: string) => void) => void) | string;
-    serverSideEncryption?: ((req: Express.Request | IncomingMessage, file: Express.Multer.File | File, callback: (error: any, serverSideEncryption?: string) => void) => void) | string;
+    bucket: ((req: MulterRequest, file: MulterFile, callback: (error: any, bucket?: string) => void) => void) | string;
+    key?(req: MulterRequest, file: MulterFile, callback: (error: any, key?: string) => void): void;
+    acl?: ((req: MulterRequest, file: MulterFile, callback: (error: any, acl?: string) => void) => void) | string;
+    contentType?(req: MulterRequest, file: MulterFile, callback: (error: any, mime?: string, stream?: NodeJS.ReadableStream) => void): void;
+    metadata?(req: MulterRequest, file: MulterFile, callback: (error: any, metadata?: any) => void): void;
+    cacheControl?: ((req: MulterRequest, file: MulterFile, callback: (error: any, cacheControl?: string) => void) => void) | string;
+    serverSideEncryption?: ((req: MulterRequest, file: MulterFile, callback: (error: any, serverSideEncryption?: string) => void) => void) | string;
 }
 
 declare global {
@@ -44,12 +48,12 @@ interface S3Storage {
     (options?: Options): StorageEngine;
 
     AUTO_CONTENT_TYPE(
-        req: Express.Request | IncomingMessage,
-        file: Express.Multer.File | File,
+        req: MulterRequest,
+        file: MulterFile,
         callback: (error: any, mime?: string, stream?: NodeJS.ReadableStream) => void): void;
     DEFAULT_CONTENT_TYPE(
-        req: Express.Request | IncomingMessage,
-        file: Express.Multer.File | File,
+        req: MulterRequest,
+        file: MulterFile,
         callback: (error: any, mime?: string) => void): void;
 }
 
