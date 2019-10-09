fafcedf5760874fab52ef649d5307936797468af
Max Boguslavskiy <mboguslavskiy@aligntech.com>
2019-10-09 16:14:36 +0300

    added tests to typing

--- types/webpack-blocks__core/webpack-blocks__core-tests.ts
+++ types/webpack-blocks__core/webpack-blocks__core-tests.ts
@@ -1,8 +1,21 @@
-import { createConfig, group, env, match, when } from '@webpack-blocks/core';
+import { createConfig, group, env, match, when, Block, Context, MatchOptions, Util } from '@webpack-blocks/core';
+import { Configuration } from '../webpack';
 
-createConfig();
-group();
-env();
-match();
-match();
-when();
+// test data
+const block = (config: Configuration): Block => {
+    return (context: Context, util: Util) => util.merge(config);
+};
+const config: Configuration = {};
+const blocks: Block[] = [block(config)];
+const emptyMatchOptions: MatchOptions = {};
+const filledMatchOptions: MatchOptions = {exclude: new RegExp(''), include: ''};
+
+// tests
+createConfig({webpack: null, webpackVersion: ''}, blocks);
+group([]);
+group(blocks);
+env('production', blocks);
+match('', blocks);
+match('', emptyMatchOptions, blocks);
+match('', filledMatchOptions, blocks);
+when(true, blocks);
