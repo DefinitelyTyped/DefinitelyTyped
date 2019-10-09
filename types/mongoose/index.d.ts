7d1ee0c34ac518764b3d2b1ee3ff9dc5c5a5ddce
Tom Yam <tomyam@tepez.co.il>
2019-10-10 02:39:02 +0300

    Mongoose type fixes: Mongoose.prototype.STATES and Schema.post('insertMany') (#37995)
    * [@types/mongoose] fix typing for Mongoose.prototype.STATES and its aliases
    
    4ef82c966a21c7f54f7c360eec5df4787c6d971e added the STATES object that is shared on:
    
    1. Mongoose.prototype.STATES
    https://mongoosejs.com/docs/api.html#mongoose_Mongoose-STATES
    
    2. Connection.prototype.STATES (undocumented)
    https://github.com/Automattic/mongoose/blob/5.6.11/lib/connection.js#L1082
    
    3. connection.states (undocumented)
    https://github.com/Automattic/mongoose/blob/5.6.11/lib/connection.js#L58
    
    STATES maps connection state names to numbers (also undocumented)
    https://github.com/Automattic/mongoose/blob/5.6.11/lib/connectionstate.js#L22
    
    (it also numbers back to strings but I don't think it's critical to support this)
    
    Before the commit - you'd get
    TS2339: Property 'connected' does not exist on type 'ConnectionStates' if you try to
    do connection.states.connected (or any other state)
    
    This commit allows it
    
    * [@types/mongoose] Add typings for Schema.post('insertMany')
    
    insertMany is different from other middlewares
    
    1. Since it's a "Model middleware":
    
    "Model middleware is supported for the following model functions.
    In model middleware functions, this refers to the model."
    
    From https://mongoosejs.com/docs/middleware.html#types-of-middleware
    
    2. It's order of arguments is different:
    
    Undocumented but found in the specs:
    https://github.com/Automattic/mongoose/blob/5.6.11/test/model.test.js#L4853

--- types/mongoose/index.d.ts
+++ types/mongoose/index.d.ts
@@ -31,6 +31,7 @@
 //                 Simon Driscoll <https://github.com/dinodeSimon>
 //                 Anton Kenikh <https://github.com/anthony-kenikh>
 //                 Chathu Vishwajith <https://github.com/iamchathu>
+//                 Tom Yam <https://github.com/tomyam1>
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
 // TypeScript Version: 3.0
 
@@ -97,7 +98,7 @@ declare module "mongoose" {
   export var SchemaTypes: typeof Schema.Types;
 
   /** Expose connection states for user-land */
-  export var STATES: ConnectionStates;
+  export var STATES: typeof ConnectionStates;
   /** The default connection of the mongoose module. */
   export var connection: Connection;
   /** An array containing all connections associated with this Mongoose instance. */
@@ -309,7 +310,7 @@ declare module "mongoose" {
     readyState: number;
 
     /** mapping of ready states */
-    states: ConnectionStates;
+    states: typeof ConnectionStates;
   }
 
   /**
@@ -411,7 +412,7 @@ declare module "mongoose" {
     startSession(options?: mongodb.SessionOptions, cb?: (err: any, session: mongodb.ClientSession) => void): Promise<mongodb.ClientSession>;
 
     /** Expose the possible connection states. */
-    static STATES: ConnectionStates;
+    static STATES: typeof ConnectionStates;
   }
 
   export enum ConnectionStates {
@@ -776,6 +777,21 @@ declare module "mongoose" {
      * @param method name of the method to hook
      * @param fn callback
      */
+    post<T extends Document>(method: 'insertMany', fn: (
+      this: Model<Document>,
+      error: mongodb.MongoError, docs: T[], next: (err?: NativeError) => void
+    ) => void): this;
+
+    post<T extends Document>(method: 'insertMany', fn: (
+      this: Model<Document>,
+      docs: T[], next: (err?: NativeError) => void
+    ) => void): this;
+
+    post<T extends Document>(method: 'insertMany', fn: (
+      this: Model<Document>,
+      docs: T[], next: (err?: NativeError) => Promise<any>
+    ) => void): this;
+
     post<T extends Document>(method: string | RegExp, fn: (
       doc: T, next: (err?: NativeError) => void
     ) => void): this;
