7187107bba30395e2c78c9c843d1d30fbfa6cbc5
Max Boguslavskiy <mboguslavskiy@aligntech.com>
2019-10-09 11:23:35 +0300

    created draft for @types/webpack-blocks__core

---
+++ types/webpack-blocks__core/index.d.ts
@@ -0,0 +1,52 @@
+// Type definitions for @webpack-blocks/core 2.0
+// Project: https://github.com/andywer/webpack-blocks
+// Definitions by: Vladimir Grenaderov <https://github.com/VladimirGrenaderov>,
+//                 Max Boguslavskiy <https://github.com/maxbogus>
+// Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
+// TypeScript Version: 2.7
+
+import { Configuration, Plugin, RuleSetRule } from 'webpack';
+
+export type ConfigSetter<T extends Context = Context> = (
+    context: T,
+    util: Util
+) => (config: Configuration) => Configuration;
+
+export interface Block<T extends Context = Context> extends ConfigSetter<T> {
+    pre?: ConfigSetter<T> | Array<ConfigSetter<T>>;
+    post?: ConfigSetter<T> | Array<ConfigSetter<T>>;
+}
+
+export interface Context {
+    match: {
+        test: RegExp | RegExp[];
+    };
+}
+
+export interface InitialContext {
+    webpack: any;
+    webpackVersion: string;
+}
+
+export interface MatchOptions {
+    include?: string;
+    exclude?: RegExp;
+}
+
+export interface Util {
+    addLoader(loaderDefinition: RuleSetRule): () => Configuration;
+    addPlugin(plugin: Plugin): () => Configuration;
+    merge(configSnippet: Configuration): () => Configuration;
+}
+
+export function createConfig(initialContext: InitialContext, configSetters: Block[]): Block;
+
+export function group(configSetters: Block[]): Block;
+
+export function env(envName: string, configSetters: Block[]): Block;
+
+export function match(test: string | string[], configSetters: Block[]): Block;
+
+export function match(test: string | string[], options: MatchOptions, configSetters: Block[] ): Block;
+
+export function when(condition: boolean, configSetters: Block[]): Block;
---
+++ types/webpack-blocks__core/webpack-blocks__core-tests.ts
@@ -0,0 +1,8 @@
+import { createConfig, group, env, match, when } from "@webpack-blocks/core";
+
+createConfig();
+group();
+env();
+match();
+match();
+when();
