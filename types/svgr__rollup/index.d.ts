7f7e83c87c1993e7115262a0211d605b4093c6b5
Max Boguslavskiy <mboguslavskiy@aligntech.com>
2019-10-09 17:54:24 +0300

    added typescript version

--- types/svgr__rollup/index.d.ts
+++ types/svgr__rollup/index.d.ts
@@ -3,6 +3,7 @@
 // Definitions by: Nick <https://github.com/fobdy>
 //                 Max Boguslavskiy <https://github.com/maxbogus>
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
+// TypeScript Version: 2.7'
 
 import { Plugin } from 'rollup';
 
