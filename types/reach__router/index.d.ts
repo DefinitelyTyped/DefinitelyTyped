35ccca18c10baa8bc97773ac2ee9f420d0e7b6cb
Ignat Prokopovich <ignatius.awwit@gmail.com>
2019-10-09 15:43:30 +0300

    Revert lib version

--- types/reach__router/index.d.ts
+++ types/reach__router/index.d.ts
@@ -1,4 +1,4 @@
-// Type definitions for @reach/router 1.2.1
+// Type definitions for @reach/router 1.2
 // Project: https://github.com/reach/router
 // Definitions by: Kingdaro <https://github.com/kingdaro>,
 //                 A.Mokhtar <https://github.com/xMokAx>,
