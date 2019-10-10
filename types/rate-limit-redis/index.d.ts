087b78b0e45654dc36ce6d753a97daee1fd28a68
Chris Suich <chris@stackforce.com>
2019-10-10 11:45:22 -0400

    Add typings for rate-limit-redis

---
+++ types/rate-limit-redis/index.d.ts
@@ -0,0 +1,18 @@
+// Type definitions for rate-limit-redis 1.6
+// Project: https://github.com/wyattjoh/rate-limit-redis#readme
+// Definitions by: Chris Suich <https://github.com/csuich2>
+// Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
+
+import { RedisClient } from 'redis';
+import { Store } from 'express-rate-limit';
+
+interface RedisStoreOptions {
+    expiry?: number;
+    prefix?: string;
+    resetExpiryOnChange?: boolean;
+    client?: RedisClient;
+}
+
+declare function RedisStore(options?: RedisStoreOptions): Store;
+
+export = RedisStore;
