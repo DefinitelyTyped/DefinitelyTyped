79f8b9d92b1c9933241c9b4c3f15a3efac204c15
Ronald Rey <reyronald@gmail.com>
2019-10-10 11:25:32 -0400

    fix(testing-library__dom): Rename `logDom` to `logDOM`
    The correct name of this function is `logDOM` as you can see here:
    https://github.com/testing-library/dom-testing-library/blob/13543a9762dff7e8f0cad8d57c1c02dc66052df6/src/pretty-dom.js#L62
    
    With the current definitions released it's not possible to import it
    without ignoring the TS error. I also updated the typings around the
    arguments to match `prettyDOM` and moved it to the `./pretty-dom.d.ts`
    file.

--- types/testing-library__dom/pretty-dom.d.ts
+++ types/testing-library__dom/pretty-dom.d.ts
@@ -1,3 +1,6 @@
 import { OptionsReceived } from 'pretty-format';
 
+type Parameters<T extends (...args: any) => any> = T extends (...args: infer P) => any ? P : never;
+
 export function prettyDOM(dom?: Element | HTMLDocument, maxLength?: number, options?: OptionsReceived): string | false;
+export function logDOM(...args: Parameters<typeof prettyDOM>): void;
