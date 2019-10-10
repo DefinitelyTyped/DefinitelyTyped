cee8c3cf8da273187a0972ecc68f6075ae9a94fd
Doma <leishenghao@126.com>
2019-10-10 21:04:09 +0800

    [redux-immutable] Fix typing for combineReducers()

--- types/redux-immutable/index.d.ts
+++ types/redux-immutable/index.d.ts
@@ -8,8 +8,8 @@
 // TypeScript Version: 2.3
 
 import { ReducersMapObject, Reducer, Action } from 'redux';
-import { Collection } from 'immutable';
+import { Collection, Map } from 'immutable';
 
-export declare function combineReducers<S, A extends Action, T>(reducers: ReducersMapObject<S, A>, getDefaultState?: () => Collection.Keyed<T, S>): Reducer<S, A>;
+export declare function combineReducers<S, A extends Action, T>(reducers: ReducersMapObject<S, A>, getDefaultState?: () => Collection.Keyed<T, S>): Reducer<Map<keyof S, any>, A>;
 export declare function combineReducers<S, A extends Action>(reducers: ReducersMapObject<S, A>, getDefaultState?: () => Collection.Indexed<S>): Reducer<S, A>;
 export declare function combineReducers<S>(reducers: ReducersMapObject<S, any>, getDefaultState?: () => Collection.Indexed<S>): Reducer<S>;
