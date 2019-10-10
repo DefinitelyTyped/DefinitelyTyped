c29c1fb7a8cb595e0b7f5459a71b7ec94785e354
John Meyer <0x326@users.noreply.github.com>
2019-10-09 22:23:34 -0400

    Add 'events.once' definition

---
+++ types/events.once/events.once-tests.ts
@@ -0,0 +1,27 @@
+// Taken from https://github.com/davidmarkclements/events.once/blob/master/readme.md
+
+import once from 'events.once';
+import { EventEmitter } from 'events';
+
+async function run() {
+    const ee = new EventEmitter();
+    process.nextTick(() => {
+        ee.emit('myevent', 42);
+    });
+
+    const [value] = await once(ee, 'myevent');
+    console.log(value);
+
+    const err = new Error('kaboom');
+    process.nextTick(() => {
+        ee.emit('error', err);
+    });
+
+    try {
+        await once(ee, 'myevent');
+    } catch (err) {
+        console.log('error happened', err);
+    }
+}
+
+run();
