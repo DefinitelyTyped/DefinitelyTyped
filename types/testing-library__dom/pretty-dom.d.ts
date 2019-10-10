890e0b36348da90d4156e3b5e188f0873e29de94
Ronald Rey <reyronald@gmail.com>
2019-10-10 12:12:36 -0400

    Do not use `Parameters` type

--- types/testing-library__dom/pretty-dom.d.ts
+++ types/testing-library__dom/pretty-dom.d.ts
@@ -1,6 +1,4 @@
 import { OptionsReceived } from 'pretty-format';
 
-type Parameters<T extends (...args: any) => any> = T extends (...args: infer P) => any ? P : never;
-
 export function prettyDOM(dom?: Element | HTMLDocument, maxLength?: number, options?: OptionsReceived): string | false;
-export function logDOM(...args: Parameters<typeof prettyDOM>): void;
+export function logDOM(dom?: Element | HTMLDocument, maxLength?: number, options?: OptionsReceived): void;
