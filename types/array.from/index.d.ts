47b545911a7a20f7f001d3e0e2ac5b62e7369bdd
ExE Boss <3889017+ExE-Boss@users.noreply.github.com>
2019-10-11 13:30:00 +0200

    docs(array.from): Improve TSDoc

--- types/array.from/index.d.ts
+++ types/array.from/index.d.ts
@@ -11,10 +11,20 @@ import shim = require('./shim');
  * Creates an array from an array-like or iterable object.
  * @param iterable An iterable object to convert to an array.
  * @param mapfn A mapping function to call on every element of the array.
- * @param thisArg Value of 'this' used to invoke the mapfn.
+ * @param thisArg Value of `this` used to invoke the mapfn.
  */
 declare const ArrayFrom: typeof implementation & {
+	/**
+	 * Gets the optimal `Array.from` implementation to use.
+	 */
 	readonly getPolyfill: typeof getPolyfill;
+
+	/**
+	 * Creates an array from an array-like or iterable object.
+	 * @param iterable An iterable object to convert to an array.
+	 * @param mapfn A mapping function to call on every element of the array.
+	 * @param thisArg Value of `this` used to invoke the mapfn.
+	 */
 	readonly implementation: typeof implementation;
 	readonly shim: typeof shim;
 };
