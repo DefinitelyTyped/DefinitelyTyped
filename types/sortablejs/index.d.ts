272d4c39e9e649f992a3871b332dcb5d287b5c2d
Wayne Van Son <waynevspersonal@gmail.com>
2019-10-11 17:11:11 +1100

    adds wayne as definition contributer

--- types/sortablejs/index.d.ts
+++ types/sortablejs/index.d.ts
@@ -2,6 +2,7 @@
 // Project: https://github.com/RubaXa/Sortable
 // Definitions by: Maw-Fox <https://github.com/Maw-Fox>
 //                 Maarten Staa <https://github.com/maartenstaa>
+//                 Wayne Van Son <https://github.com/waynevanson>
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
 // TypeScript Version: 2.1
 
