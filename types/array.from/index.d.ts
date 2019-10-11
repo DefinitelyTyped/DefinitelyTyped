503db5724392708870e9409e05103b16bc44e9e7
ExE Boss <3889017+ExE-Boss@users.noreply.github.com>
2019-10-11 13:20:00 +0200

    docs(array.from): Add TSDoc comments

--- types/array.from/index.d.ts
+++ types/array.from/index.d.ts
@@ -7,6 +7,12 @@ import implementation = require('./implementation');
 import getPolyfill = require('./polyfill');
 import shim = require('./shim');
 
+/**
+ * Creates an array from an array-like or iterable object.
+ * @param iterable An iterable object to convert to an array.
+ * @param mapfn A mapping function to call on every element of the array.
+ * @param thisArg Value of 'this' used to invoke the mapfn.
+ */
 declare const ArrayFrom: typeof implementation & {
 	readonly getPolyfill: typeof getPolyfill;
 	readonly implementation: typeof implementation;
