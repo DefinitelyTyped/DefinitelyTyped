0617a40cb9718a8ce6d74bd29999b78579dff7c5
Marco Beltempo <marco@toolbx.com>
2019-10-14 19:33:26 -0400

    Added index export for resources and helpers

---
+++ types/onfleet__node-onfleet/index.d.ts
@@ -0,0 +1,10 @@
+// Type definitions for @onfleet/node-onfleet 1.0
+// Project: https://github.com/TBD
+// Definitions by: Marco Beltempo <https://github.com/marcobeltempo>
+// Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
+// TypeScript Version: 2.1
+import { Onfleet } from './onfleet';
+export * from './error';
+export * from './metadata';
+export * from './Resources';
+export default Onfleet;
