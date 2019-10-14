ef108800438153a43df30723e8cbd1b0ad79d00a
Joe Mitchard <joe.mitchard@spafax.com>
2019-10-14 14:15:10 +0100

    fixed linting error for jest-matcher-one-of

--- types/jest-matcher-one-of/index.d.ts
+++ types/jest-matcher-one-of/index.d.ts
@@ -2,10 +2,9 @@
 // Project: https://github.com/d4nyll/jest-matcher-one-of#readme
 // Definitions by: Joe Mitchard <https://github.com/joemitchard>
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
+// TypeScript Version: 3.0
 
 /// <reference types="jest" />
-declare module 'jest-matcher-one-of';
-
 declare namespace jest {
     interface Matchers<R> {
         toBeOneOf(expected: any[]): R;
