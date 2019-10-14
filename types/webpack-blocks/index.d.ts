d800bc9ec92d0d582d2e4d33f3775cbde8a9e141
Max Boguslavskiy <mboguslavskiy@aligntech.com>
2019-10-14 15:30:31 +0300

    - implemented definitions for webpack-blocks package.

---
+++ types/webpack-blocks/index.d.ts
@@ -0,0 +1,28 @@
+// Type definitions for webpack-blocks 2.0
+// Project: https://github.com/andywer/webpack-blocks
+// Definitions by: Max Boguslavskiy <https://github.com/maxbogus>
+// Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
+// TypeScript Version: 2.7
+
+import assets from '@webpack-blocks/assets';
+import babel from '@webpack-blocks/babel';
+import devServer from '@webpack-blocks/dev-server';
+import extractText from '@webpack-blocks/extract-text';
+import postcss from '@webpack-blocks/postcss';
+import sass from '@webpack-blocks/sass';
+import typescript from '@webpack-blocks/typescript';
+import uglify from '@webpack-blocks/uglify';
+import webpack from '@webpack-blocks/webpack';
+
+// Export all components.
+export {
+    webpack,
+    assets,
+    babel,
+    devServer,
+    extractText,
+    postcss,
+    sass,
+    typescript,
+    uglify
+};
