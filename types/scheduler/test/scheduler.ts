5364a6e76f3d2b97cd4c9449457335fad8d73964
Maurice de Beijer <maurice.de.beijer@gmail.com>
2019-10-14 19:21:42 +0200

    Update tests with aditional undefined result type

--- types/scheduler/test/scheduler.ts
+++ types/scheduler/test/scheduler.ts
@@ -8,7 +8,7 @@ unstable_scheduleCallback(unstable_NormalPriority, () => {}, { delay: 100 });
 // $ExpectType number
 unstable_now();
 
-// $ExpectType number
+// $ExpectType number | undefined
 const nextNumberResult = unstable_next(() => 42);
-// $ExpectType string
+// $ExpectType string | undefined
 const nextStringResult = unstable_next(() => 'Hello');
