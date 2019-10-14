0309966b05b8bcf81fce40374aff38ae61f03d9f
Matthew Bryant <matthewbryant95@gmail.com>
2019-10-14 10:28:56 +0100

    Update TS version to 3.0 for 'unknown'

--- types/hig__button/index.d.ts
+++ types/hig__button/index.d.ts
@@ -2,7 +2,7 @@
 // Project: https://github.com/Autodesk/hig/tree/development/packages/button
 // Definitions by: Matthew Bryant <https://github.com/matthewbryant95>
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
-// TypeScript Version: 2.8
+// TypeScript Version: 3.0
 
 import * as React from 'react';
 
