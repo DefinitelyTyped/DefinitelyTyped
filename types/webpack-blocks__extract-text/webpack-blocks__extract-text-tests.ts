32029d0fd83d7048284cddb92cc325e579125560
Max Boguslavskiy <mboguslavskiy@aligntech.com>
2019-10-15 18:57:54 +0300

    - corrected formatting

--- types/webpack-blocks__extract-text/webpack-blocks__extract-text-tests.ts
+++ types/webpack-blocks__extract-text/webpack-blocks__extract-text-tests.ts
@@ -2,13 +2,6 @@ import { createConfig, match, env } from '@webpack-blocks/webpack';
 import extractText from '@webpack-blocks/extract-text';
 import { css } from '@webpack-blocks/assets';
 
-module.exports = createConfig([
-    extractText('path/to/output.file')
-]);
+module.exports = createConfig([extractText('path/to/output.file')]);
 
-module.exports = createConfig([
-    match('*.css', [
-        css(),
-        env('production', [extractText()])
-    ])
-]);
+module.exports = createConfig([match('*.css', [css(), env('production', [extractText()])])]);
