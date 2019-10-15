d84d95a46c48d9375488b4a4f78f0535478504ac
Max Boguslavskiy <mboguslavskiy@aligntech.com>
2019-10-15 19:32:46 +0300

    - corrected formatting.

--- types/webpack-blocks__postcss/webpack-blocks__postcss-tests.ts
+++ types/webpack-blocks__postcss/webpack-blocks__postcss-tests.ts
@@ -2,16 +2,11 @@ import { createConfig, match } from '@webpack-blocks/webpack';
 import { css } from '@webpack-blocks/assets';
 import postcss from '@webpack-blocks/postcss';
 
-createConfig([
-    match(['*.css', '!*node_modules*'], [
-        css(),
-        postcss()
-    ])
-]);
+createConfig([match(['*.css', '!*node_modules*'], [css(), postcss()])]);
 
 createConfig([
     css(),
     postcss({
-        plugins: []
-    })
+        plugins: [],
+    }),
 ]);
