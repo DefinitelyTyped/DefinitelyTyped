7125b813db1b660d865d564c650dbe59eb3dc18e
Marco Beltempo <marco@toolbx.com>
2019-10-15 00:49:31 -0400

    Set Typescript version to 3.5

--- types/onfleet__node-onfleet/index.d.ts
+++ types/onfleet__node-onfleet/index.d.ts
@@ -1,8 +1,8 @@
 // Type definitions for @onfleet/node-onfleet 1.0
-// Project: https://github.com/TBD
+// Project: https://github.com/onfleet/node-onfleet
 // Definitions by: Marco Beltempo <https://github.com/marcobeltempo>
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
-// TypeScript Version: 2.1
+// TypeScript Version: 3.5
 import { Onfleet } from './onfleet';
 export * from './error';
 export * from './metadata';
