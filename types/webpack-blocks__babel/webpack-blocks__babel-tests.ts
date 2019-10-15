2852e276b8983fb21ececfcc70f2bdce85ab3679
Max Boguslavskiy <mboguslavskiy@aligntech.com>
2019-10-15 13:25:38 +0300

    - updated tests for babel.

--- types/webpack-blocks__babel/webpack-blocks__babel-tests.ts
+++ types/webpack-blocks__babel/webpack-blocks__babel-tests.ts
@@ -1,14 +1,6 @@
 import { createConfig, match } from '@webpack-blocks/webpack';
 import babel from '@webpack-blocks/babel';
 
-createConfig([
-    match(['*.js', '!*node_modules*'], [
-        babel()
-    ])
-]);
+createConfig([match(['*.js', '!*node_modules*'], [babel()])]);
 
-createConfig([
-    match(['*.js', '!*node_modules*'], [
-        babel({presets: [''], cacheDirectory: false, plugins: ['']})
-    ])
-]);
+createConfig([match(['*.js', '!*node_modules*'], [babel({ presets: [''], cacheDirectory: false, plugins: [''] })])]);
