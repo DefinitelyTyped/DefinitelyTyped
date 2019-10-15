d9d4bb79e2c5a58dd1a9e9fb93a3c4df40b7de33
es <es@MacBook-Pro-mac.local>
2019-10-15 16:57:00 +0300

    Add ts version

--- types/ajv-async/index.d.ts
+++ types/ajv-async/index.d.ts
@@ -2,6 +2,7 @@
 // Project: https://github.com/epoberezkin/ajv-async#readme
 // Definitions by: es <https://github.com/dmitriismitnov>
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
+// TypeScript Version: 2.2
 
 import { Ajv } from 'ajv';
 
