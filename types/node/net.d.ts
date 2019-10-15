4d4265242d50ff5f025df714f6b3a263946c1419
Simon Schick <demwizzy@gmail.com>
2019-10-14 20:07:20 -0700

    fixup! fixup! fixup! fixup! feat(node): v12.11

--- types/node/net.d.ts
+++ types/node/net.d.ts
@@ -19,13 +19,13 @@ declare module "net" {
     }
 
     interface OnReadOpts {
-        buffer?: Uint8Array | (() => Uint8Array);
+        buffer: Uint8Array | (() => Uint8Array);
         /**
          * This function is called for every chunk of incoming data.
          * Two arguments are passed to it: the number of bytes written to buffer and a reference to buffer.
          * Return false from this function to implicitly pause() the socket.
          */
-        callback?(bytesWritten: number, buf: Uint8Array): boolean;
+        callback(bytesWritten: number, buf: Uint8Array): boolean;
     }
 
     interface ConnectOpts {
