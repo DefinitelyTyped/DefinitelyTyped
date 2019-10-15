c62171ae54c6f69d4bf1ed8ddacf4f34cb962e09
Max Boguslavskiy <mboguslavskiy@aligntech.com>
2019-10-15 19:43:59 +0300

    - corrected formatting.

--- types/webpack-blocks__sass/webpack-blocks__core-tests.ts
+++ types/webpack-blocks__sass/webpack-blocks__core-tests.ts
@@ -4,32 +4,10 @@ import sass from '@webpack-blocks/sass';
 import extractText from '@webpack-blocks/extract-text';
 import postcss from '@webpack-blocks/postcss';
 
-createConfig([
-    match('*.scss', [
-        css(),
-        sass(),
-        postcss({plugins: []})
-    ])
-]);
+createConfig([match('*.scss', [css(), sass(), postcss({ plugins: [] })])]);
 
-createConfig([
-    match('*.scss', [
-        css.modules(),
-        sass()
-    ])
-]);
+createConfig([match('*.scss', [css.modules(), sass()])]);
 
-createConfig([
-    match('*.scss', [
-        css(),
-        sass(),
-        env('production', [extractText()])
-    ])
-]);
+createConfig([match('*.scss', [css(), sass(), env('production', [extractText()])])]);
 
-createConfig([
-    match(['*.scss', '!*node_modules*'], [
-        css(),
-        sass()
-    ])
-]);
+createConfig([match(['*.scss', '!*node_modules*'], [css(), sass()])]);
