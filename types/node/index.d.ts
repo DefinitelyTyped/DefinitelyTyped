a7256c1065b1110e2184f9ca205467ab9892480e
anstarovoyt <andrey.starovoyt@jetbrains.com>
2019-10-17 18:21:43 +0300

    update version

--- types/node/index.d.ts
+++ types/node/index.d.ts
@@ -1,4 +1,4 @@
-// Type definitions for non-npm package Node.js 12.2
+// Type definitions for non-npm package Node.js 12.11
 // Project: http://nodejs.org/
 // Definitions by: Microsoft TypeScript <https://github.com/Microsoft>
 //                 DefinitelyTyped <https://github.com/DefinitelyTyped>
