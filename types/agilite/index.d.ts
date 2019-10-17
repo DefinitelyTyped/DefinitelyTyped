c05d69fb25c5ad9cfc596b705177b213f4d3415b
Armand Smit <armand@agilite.io>
2019-10-17 08:54:57 +0200

    Formatting fixes to agilite

--- types/agilite/index.d.ts
+++ types/agilite/index.d.ts
@@ -33,58 +33,58 @@ declare class Agilite {
 
 declare namespace Agilite {
   interface AgiliteConfig {
-    apiServerUrl ? : string;
-    apiKey ? : string;
-    teamId ? : string;
+    apiServerUrl?: string;
+    apiKey?: string;
+    teamId?: string;
   }
-  
+
   interface reqType {
-    GET: string,
-    POST: string,
-    PUT: string,
-    DELETE: string
+    GET: string;
+    POST: string;
+    PUT: string;
+    DELETE: string;
   }
-  
+
   interface appName {
-    MODULE_KEY_API_KEYS: string,
-    MODULE_KEY_KEYWORDS: string,
-    MODULE_KEY_NUMBERING: string,
-    MODULE_KEY_CONNECTORS: string,
-    MODULE_KEY_DATA_MAPPING: string,
-    MODULE_KEY_TEMPLATES: string,
-    MODULE_KEY_BPM: string,
-    MODULE_KEY_ROLES: string,
-    MODULE_KEY_BOT_BUILDER: string,
-    MODULE_KEY_FILES: string,
-    MODULE_KEY_UTILS: string,
-    MODULE_KEY_TIER_STRUCTURES: string
+    MODULE_KEY_API_KEYS: string;
+    MODULE_KEY_KEYWORDS: string;
+    MODULE_KEY_NUMBERING: string;
+    MODULE_KEY_CONNECTORS: string;
+    MODULE_KEY_DATA_MAPPING: string;
+    MODULE_KEY_TEMPLATES: string;
+    MODULE_KEY_BPM: string;
+    MODULE_KEY_ROLES: string;
+    MODULE_KEY_BOT_BUILDER: string;
+    MODULE_KEY_FILES: string;
+    MODULE_KEY_UTILS: string;
+    MODULE_KEY_TIER_STRUCTURES: string;
   }
-  
+
   interface sort {
-    ASC: string,
-    DESC: string,
-    ASC_VALUE: string,
-    DESC_VALUE: string
+    ASC: string;
+    DESC: string;
+    ASC_VALUE: string;
+    DESC_VALUE: string;
   }
-  
+
   interface outputFormat {
-    ARRAY: string,
-    JSON: string,
-    STRING: string
+    ARRAY: string;
+    JSON: string;
+    STRING: string;
   }
-  
+
   interface responseType {
-    ARRAY_BUFFER: string,
-    BLOB: string,
-    DOCUMENT: string,
-    JSON: string,
-    TEXT: string,
-    STREAM: string
+    ARRAY_BUFFER: string;
+    BLOB: string;
+    DOCUMENT: string;
+    JSON: string;
+    TEXT: string;
+    STREAM: string;
   }
 
   class ApiKeys {
-    constructor(config: Agilite.AgiliteConfig);
-  
+    constructor(config: AgiliteConfig);
+
     postData(data: any): any;
     getData(profileKeys: string[], recordIds: string[], slimResult: boolean): any;
     putData(recordId: string, data: any): any;
@@ -94,13 +94,13 @@ declare namespace Agilite {
     disableApiKey(recordId: string): any;
     enableApiKey(recordId: string): any;
   }
-  
+
   class Keywords {
     constructor(config: AgiliteConfig);
-  
+
     sort: sort;
     outputFormat: outputFormat;
-  
+
     postData(data: any): any;
     getData(profileKeys: string[], recordIds: string[], slimResult: boolean): any;
     putData(recordId: string, data: any): any;
@@ -110,12 +110,12 @@ declare namespace Agilite {
     getLabelByValue(profileKey: string, value: string, outputFormat: string): any;
     getValueByLabel(profileKey: string, label: string, outputFormat: string): any;
   }
-  
+
   class Numbering {
     constructor(config: AgiliteConfig);
-  
+
     outputFormat: outputFormat;
-  
+
     postData(data: any): any;
     getData(profileKeys: string[], recordIds: string[], slimResult: boolean): any;
     putData(recordId: string, data: any): any;
@@ -123,56 +123,57 @@ declare namespace Agilite {
     generate(profileKey: string, outputFormat: string, data: any): any;
     resetCounters(recordId: string): any;
   }
-  
+
   class Connectors {
     constructor(config: AgiliteConfig);
-  
+
     postData(data: any): any;
     getData(profileKeys: string[], recordIds: string[], slimResult: boolean): any;
     putData(recordId: string, data: any): any;
     deleteData(recordId: string): any;
     execute(profileKey: string, routeKey: string, data: any): any;
   }
-  
+
   class DataMappings {
     constructor(config: AgiliteConfig);
-  
+
     postData(data: any): any;
     getData(profileKeys: string[], recordIds: string[], slimResult: boolean): any;
     putData(recordId: string, data: any): any;
     deleteData(recordId: string): any;
     execute(profileKey: string, data: any): any;
   }
-  
+
   class Templates {
     constructor(config: AgiliteConfig);
-  
+
     postData(data: any): any;
     getData(profileKeys: string[], recordIds: string[], slimResult: boolean): any;
     putData(recordId: string, data: any): any;
     deleteData(recordId: string): any;
     execute(profileKey: string, data: any): any;
   }
-  
+
   class BPM {
     constructor(config: AgiliteConfig);
-  
+
     postData(data: any): any;
     getData(profileKeys: string[], recordIds: string[], slimResult: boolean): any;
     putData(recordId: string, data: any): any;
     deleteData(recordId: string): any;
     registerBPMRecord(processKey: string, currentUser: string): any;
     execute(processKey: string, bpmRecordId: string, optionSelected: string, currentUser: string, comments: string, data: any): any;
+    // tslint:disable-next-line max-line-length
     getRecordState(processKeys: string[], bpmRecordIds: string[], stepNames: string[], responsibleUsers: string[], relevantUsers: string[], includeHistory: boolean, includeStepOptions: boolean, includeVisibleObjects: boolean, page: any, pageLimit: any): any;
     getByProfileKey(profileKey: string): any;
     clearHistoryData(profileKey: string): any;
     getActiveSteps(processKey: string): any;
     getActiveUsers(processKey: string): any;
   }
-  
+
   class Roles {
     constructor(config: AgiliteConfig);
-  
+
     postData(data: any): any;
     getData(profileKeys: string[], recordIds: string[], slimResult: boolean): any;
     putData(recordId: string, data: any): any;
@@ -183,23 +184,23 @@ declare namespace Agilite {
     changeConditionalLevels(recordId: string, conditionalLevels: string[]): any;
     reAssignResponsibleUser(recordId: string, responsibleUser: string): any;
   }
-  
+
   class Files {
     constructor(config: AgiliteConfig);
-  
+
     responseType: responseType;
-  
+
     getFile(recordId: string, responseType: any): any;
     uploadFile(fileName: string, contentType: string, data: any): any;
     deleteFile(recordId: string): any;
     getFileName(recordId: string): any;
   }
-  
+
   class Utils {
     constructor(config: AgiliteConfig);
-  
+
     responseType: responseType;
-  
+
     encodeXML(data: string): any;
     decodeXML(data: string): any;
     XMLToJS(data: string): any;
@@ -213,13 +214,13 @@ declare namespace Agilite {
     exportData(includeModules: any): any;
     importData(fileId: string): any;
   }
-  
+
   class TierStructures {
     constructor(config: AgiliteConfig);
-  
+
     sort: sort;
     outputFormat: outputFormat;
-  
+
     postData(data: any): any;
     getData(profileKeys: string[], recordIds: string[], slimResult: boolean): any;
     putData(recordId: string, data: any): any;
