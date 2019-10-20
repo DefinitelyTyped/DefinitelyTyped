b5a0aa857a71b2d43885932f9351796698447ab2
Gary King <garyking@gmail.com>
2019-10-20 16:42:45 -0400

    finish emoji-strip/index.d.ts

--- types/emoji-strip/index.d.ts
+++ types/emoji-strip/index.d.ts
@@ -1,39 +1,16 @@
-// Type definitions for emoji-strip 1.0
-// Project: https://github.com/khalifenizar/emoji-strip
-// Definitions by: Gary King <https://github.com/DefinitelyTyped>
+// Type definitions for emoji-strip 1.0.1
+// Project: https://github.com/nizaroni/emoji-strip
+// Definitions by: Gary King <https://github.com/garyking>
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
 
-/*~ If this module is a UMD module that exposes a global variable 'myLib' when
- *~ loaded outside a module loader environment, declare that global here.
- *~ Otherwise, delete this declaration.
- */
-export as namespace myLib;
-
-/*~ If this module has methods, declare them as functions like so.
- */
-export function myMethod(a: string): string;
-export function myOtherMethod(a: number): number;
-
-/*~ You can declare types that are available via importing the module */
-export interface someType {
-    name: string;
-    length: number;
-    extras?: string[];
-}
-
-/*~ You can declare properties of the module using const, let, or var */
-export const myField: number;
-
-/*~ If there are types, properties, or methods inside dotted names
- *~ of the module, declare them inside a 'namespace'.
- */
-export namespace subProp {
-    /*~ For example, given this definition, someone could write:
-     *~   import { subProp } from 'yourModule';
-     *~   subProp.foo();
-     *~ or
-     *~   import * as yourMod from 'yourModule';
-     *~   yourMod.subProp.foo();
+declare module 'emoji-strip' {
+    /**
+     * Strip emojis from text.
+     *
+     * @param text The text from which to strip the emojis.
+     * @returns The text, with emojis removed.
      */
-    function foo(): void;
+    function emojiStrip(text: string): string;
+
+    export default emojiStrip;
 }
