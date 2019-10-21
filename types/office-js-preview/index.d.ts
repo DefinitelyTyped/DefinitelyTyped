ecb63f2af551b32fe259f459b2d5cafd0d111894
AlexJerabek <aljerabe@microsoft.com>
2019-10-21 08:50:46 -0700

    PR feedback

--- types/office-js-preview/index.d.ts
+++ types/office-js-preview/index.d.ts
@@ -3910,7 +3910,7 @@ declare namespace Office {
          * <table>
          *   <tr>
          *     <td>`Office.CoercionType.XmlSvg`</td>
-         *     <td>There is a 60kB size limitation for SVG insertions.</td>
+         *     <td>There is a 60KB size limitation for SVG insertions.</td>
          *   </tr>
          * </table>
          * 
@@ -4109,7 +4109,7 @@ declare namespace Office {
          * <table>
          *   <tr>
          *     <td>`Office.CoercionType.XmlSvg`</td>
-         *     <td>There is a 60kB size limitation for SVG insertions.</td>
+         *     <td>There is a 60KB size limitation for SVG insertions.</td>
          *   </tr>
          * </table>
          * 
