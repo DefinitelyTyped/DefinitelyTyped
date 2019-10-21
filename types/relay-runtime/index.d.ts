b5210edf39148c9fbc68e872c703d358c5c22c5e
Eloy Durán <eloy.de.enige@gmail.com>
2019-10-21 18:45:00 +0200

    [relay-runtime] Update fragment reference definitions (#39231)
    As of TS 3.6 an intersection of multiple unique symbols results in
    `never`, which means our fragment reference checking is now unsafe.

--- types/relay-runtime/index.d.ts
+++ types/relay-runtime/index.d.ts
@@ -192,13 +192,28 @@ export { RelayNetworkLoggerTransaction } from './lib/network/RelayNetworkLoggerT
 export { createRelayNetworkLogger } from './lib/network/createRelayNetworkLogger';
 export { deepFreeze } from './lib/util/deepFreeze';
 
-// These match the output of relay-compiler-language-typescript.
-export interface _RefType<T> {
-    ' $refType': T;
-}
-export interface _FragmentRefs<T> {
-    ' $fragmentRefs': T;
+/**
+ * relay-compiler-language-typescript support for fragment references
+ */
+
+/**
+ * @private
+ */
+export interface _RefType<Ref extends string> {
+    ' $refType': Ref;
 }
 
+/**
+ * @private
+ */
+export interface _FragmentRefs<Refs extends string> {
+    ' $fragmentRefs': FragmentRefs<Refs>;
+}
+
+// This is used in the actual artifacts to define the various fragment references a container holds.
+export type FragmentRefs<Refs extends string> = {
+    [ref in Refs]: true;
+};
+
 // This is a utility type for converting from a data type to a fragment reference that will resolve to that data type.
-export type FragmentRef<T> = T extends _RefType<infer U> ? _FragmentRefs<U> : never;
+export type FragmentRef<Fragment> = Fragment extends _RefType<infer U> ? _FragmentRefs<U> : never;
