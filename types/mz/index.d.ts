a9d0b3f81e03abe060ddb82c5e2e8473c723890b
ExE Boss <3889017+ExE-Boss@users.noreply.github.com>
2019-10-21 09:50:00 +0200

    fix(mz): Add myself to the contributors list

--- types/mz/index.d.ts
+++ types/mz/index.d.ts
@@ -1,6 +1,7 @@
 // Type definitions for mz 2.7
 // Project: https://github.com/normalize/mz
 // Definitions by: Thomas Hickman <https://github.com/ThomasHickman>
+//                 ExE Boss <https://github.com/ExE-Boss>
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
 
 // Modified from the node.js definitions.
