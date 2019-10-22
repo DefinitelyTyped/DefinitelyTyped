f2c94a555651e7aa8a9f344c26caae3a3020377e
ExE Boss <3889017+ExE-Boss@users.noreply.github.com>
2019-10-23 00:52:03 +0200

    fix(es‑abstract/es2016): Correct `SameValueNonNumber(…)` type
    Co-Authored-By: Jordan Harband <ljharb@gmail.com>

--- types/es-abstract/es2016.d.ts
+++ types/es-abstract/es2016.d.ts
@@ -7,7 +7,7 @@ interface ES2016 extends ES2015 {
 	 *
 	 * @throws {TypeError} If `x` or `y` is a `number` or they're different types.
 	 */
-	SameValueNonNumber(x: unknown, y: unknown): boolean;
+	SameValueNonNumber(x: object | null | undefined | string | boolean | symbol, y: object | null | undefined | string | boolean | symbol): boolean;
 
 	IterableToArrayLike<T extends Iterable<unknown> | ArrayLike<unknown>>(
 		items: T,
