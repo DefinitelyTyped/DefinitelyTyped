e7d4335a64c8605dea984fc734920ca507f08341
Daniel Cassidy <mail@danielcassidy.me.uk>
2019-10-22 06:53:17 +0100

    semantic-release: fix lint errors.

--- types/semantic-release/index.d.ts
+++ types/semantic-release/index.d.ts
@@ -6,197 +6,353 @@
 
 /// <reference types="node" />
 
-declare module SemanticRelease {
-	/** semantic-release options.
-	 * 
+declare namespace SemanticRelease {
+	/**
+	 * semantic-release options.
+	 *
 	 * Can be used to set any core option or plugin options.
 	 * Each option will take precedence over options configured in the
-	 * configuration file and shareable configurations. */
-	export type Options = Partial<GlobalConfig>;
+	 * configuration file and shareable configurations.
+	 */
+	type Options = Partial<GlobalConfig>;
 
-	/** semantic-release options, after normalization and defaults have been
-	 * applied. */
-	export interface GlobalConfig {
-		/** The branch on which releases should happen. */
+	/**
+	 * semantic-release options, after normalization and defaults have been
+	 * applied.
+	 */
+	interface GlobalConfig {
+		/**
+		 * The branch on which releases should happen.
+		 */
 		branch: string;
-		/** The Git repository URL, in any supported format. */
+
+		/**
+		 * The Git repository URL, in any supported format.
+		 */
 		repositoryUrl: string;
-		/** The Git tag format used by semantic-release to identify releases. */
+
+		/**
+		 * The Git tag format used by semantic-release to identify releases.
+		 */
 		tagFormat: string;
-		/** Specifies the list of plugins to use. Plugins will run in series, in
+
+		/**
+		 * Specifies the list of plugins to use. Plugins will run in series, in
 		 * the order specified.
-		 * 
+		 *
 		 * If this option is not specified, then semantic-release will use a
 		 * default list of plugins.
-		 * 
+		 *
 		 * Configuration options for each plugin can be defined by wrapping the
-		 * name and an options object in an array. */
+		 * name and an options object in an array.
+		 */
 		plugins: ReadonlyArray<PluginSpec>;
-		/** Dry-run mode, skip publishing, print next version and release notes. */
+
+		/**
+		 * Dry-run mode, skip publishing, print next version and release notes.
+		 */
 		dryRun?: boolean;
-		/** Set to false to skip Continuous Integration environment verifications.
-		 * This allows for making releases from a local machine. */
+
+		/**
+		 * Set to false to skip Continuous Integration environment verifications.
+		 * This allows for making releases from a local machine.
+		 */
 		ci?: boolean;
-		/** Any other options supported by plugins. */
+
+		/**
+		 * Any other options supported by plugins.
+		 */
 		[name: string]: any;
 	}
 
-	/** Specifies a plugin to use.
-	 * 
+	/**
+	 * Specifies a plugin to use.
+	 *
 	 * The plugin is specified by its module name.
-	 * 
+	 *
 	 * To pass options to a plugin, specify an array containing the plugin module
-	 * name and an options object. */
-	export type PluginSpec = string | [string, any];
+	 * name and an options object.
+	 */
+	type PluginSpec = string | [string, any];
 
 	/** semantic-release configuration specific for API usage. */
-	export interface Config {
-		/** The current working directory to use. It should be configured to
+	interface Config {
+		/**
+		 * The current working directory to use. It should be configured to
 		 * the root of the Git repository to release from.
-		 * 
+		 *
 		 * It allows to run semantic-release from a specific path without
 		 * having to change the local process cwd with process.chdir().
-		 * 
-		 * @default process.cwd */
+		 *
+		 * @default process.cwd
+		 */
 		cwd?: string;
-		/** The environment variables to use.
-		 * 
+
+		/**
+		 * The environment variables to use.
+		 *
 		 * It allows to run semantic-release with specific environment
 		 * variables without having to modify the local process.env.
-		 * 
-		 * @default process.env */
+		 *
+		 * @default process.env
+		 */
 		env?: { [name: string]: string };
-		/** The writable stream used to log information.
-		 * 
+
+		/**
+		 * The writable stream used to log information.
+		 *
 		 * It allows to configure semantic-release to write logs to a specific
 		 * stream rather than the local process.stdout.
-		 * 
-		 * @default process.stdout */
+		 *
+		 * @default process.stdout
+		 */
 		stdout?: NodeJS.WriteStream;
-		/** The writable stream used to log errors.
-		 * 
+
+		/**
+		 * The writable stream used to log errors.
+		 *
 		 * It allows to configure semantic-release to write errors to a
 		 * specific stream rather than the local process.stderr.
-		 * 
-		 * @default process.stderr */
+		 *
+		 * @default process.stderr
+		 */
 		stderr?: NodeJS.WriteStream;
 	}
 
-	export interface LastRelease {
-		/** The version name of the release */
+	interface LastRelease {
+		/**
+		 * The version name of the release.
+		 */
 		version: string;
-		/** The Git tag of the release. */
+
+		/**
+		 * The Git tag of the release.
+		 */
 		gitTag: string;
-		/** The Git checksum of the last commit of the release. */
+
+		/**
+		 * The Git checksum of the last commit of the release.
+		 */
 		gitHead: string;
 	}
 
-	export interface NextRelease extends LastRelease {
-		/** The semver type of the release. */
+	interface NextRelease extends LastRelease {
+		/**
+		 * The semver type of the release.
+		 */
 		type: "patch" | "minor" | "major";
-		/** The release notes of the next release. */
+
+		/**
+		 * The release notes of the next release.
+		 */
 		notes: string;
 	}
 
-	export interface Context {
-		/** The semantic release configuration itself. */
+	interface Context {
+		/**
+		 * The semantic release configuration itself.
+		 */
 		options?: GlobalConfig;
-		/** The previous release details. */
+
+		/**
+		 * The previous release details.
+		 */
 		lastRelease?: LastRelease;
-		/** The next release details. */
+
+		/**
+		 * The next release details.
+		 */
 		nextRelease?: NextRelease;
-		/** The shared logger instance of semantic release. */
+
+		/**
+		 * The shared logger instance of semantic release.
+		 */
 		logger: {
 			log: (message: string, ...vars: any[]) => void,
 			error: (message: string, ...vars: any[]) => void,
 		};
-		/** Environment variables. */
+
+		/**
+		 * Environment variables.
+		 */
 		env: {
 			[key: string]: string;
 		};
 	}
 
-	export interface Commit {
-		/** The commit abbreviated and full hash. */
+	interface Commit {
+		/**
+		 * The commit abbreviated and full hash.
+		 */
 		commit: {
-			/** The commit hash. */
+			/**
+			 * The commit hash.
+			 */
 			long: string;
-			/** The commit abbreviated hash. */
+
+			/**
+			 * The commit abbreviated hash.
+			 */
 			short: string;
 		};
-		/** The commit abbreviated and full tree hash. */
+
+		/**
+		 * The commit abbreviated and full tree hash.
+		 */
 		tree: {
-			/** The commit tree hash. */
+			/**
+			 * The commit tree hash.
+			 */
 			long: string;
-			/** The commit abbreviated tree hash. */
+
+			/**
+			 * The commit abbreviated tree hash.
+			 */
 			short: string;
 		};
-		/** The commit author information. */
+
+		/**
+		 * The commit author information.
+		 */
 		author: {
-			/** The commit author name. */
+			/**
+			 * The commit author name.
+			 */
 			name: string;
-			/** The commit author email. */
+
+			/**
+			 * The commit author email.
+			 */
 			email: string;
-			/** The commit author date. */
+
+			/**
+			 * The commit author date.
+			 */
 			short: string;
 		};
-		/** The committer information. */
+
+		/**
+		 * The committer information.
+		 */
 		committer: {
-			/** The committer name. */
+			/**
+			 * The committer name.
+			 */
 			name: string;
-			/** The committer email. */
+
+			/**
+			 * The committer email.
+			 */
 			email: string;
-			/** The committer date. */
+
+			/**
+			 * The committer date.
+			 */
 			short: string;
 		};
-		/** The commit subject. */
+
+		/**
+		 * The commit subject.
+		 */
 		subject: string;
-		/** The commit body. */
+
+		/**
+		 * The commit body.
+		 */
 		body: string;
-		/** The commit full message (subject and body). */
+
+		/**
+		 * The commit full message (subject and body).
+		 */
 		message: string;
-		/** The commit hash. */
+
+		/**
+		 * The commit hash.
+		 */
 		hash: string;
-		/** The committer date. */
+
+		/**
+		 * The committer date.
+		 */
 		committerDate: string;
 	}
 
-	/** Details of a release published by a publish plugin. */
-	export interface Release {
-		/** The release name, only if set by the corresponding publish plugin. */
+	/**
+	 * Details of a release published by a publish plugin.
+	 */
+	interface Release {
+		/**
+		 * The release name, only if set by the corresponding publish plugin.
+		 */
 		name?: string;
-		/** The release URL, only if set by the corresponding publish plugin. */
+
+		/**
+		 * The release URL, only if set by the corresponding publish plugin.
+		 */
 		url?: string;
-		/** The semver type of the release. */
+
+		/**
+		 * The semver type of the release.
+		 */
 		type: "patch" | "minor" | "major";
-		/** The version of the release. */
+
+		/**
+		 * The version of the release.
+		 */
 		version: string;
-		/** The sha of the last commit being part of the release. */
+
+		/**
+		 * The sha of the last commit being part of the release.
+		 */
 		gitHead: string;
-		/** The Git tag associated with the release. */
+
+		/**
+		 * The Git tag associated with the release.
+		 */
 		gitTag: string;
-		/** The release notes for the release. */
+
+		/**
+		 * The release notes for the release.
+		 */
 		notes: string;
-		/** The name of the plugin that published the release. */
+
+		/**
+		 * The name of the plugin that published the release.
+		 */
 		pluginName: string;
 	}
 
-	/** An object with details of the release if a release was published, or
-	 * false if no release was published. */
-	export type Result = false | {
-		/** Information related to the last release found. */
+	/**
+	 * An object with details of the release if a release was published, or
+	 * false if no release was published.
+	 */
+	type Result = false | {
+		/**
+		 * Information related to the last release found.
+		 */
 		lastRelease: LastRelease;
-		/** The list of commits included in the new release. */
+
+		/**
+		 * The list of commits included in the new release.
+		 */
 		commits: Commit[];
-		/** Information related to the newly published release. */
+
+		/**
+		 * Information related to the newly published release.
+		 */
 		nextRelease: NextRelease;
-		/** The list of releases published, one release per publish plugin. */
+
+		/**
+		 * The list of releases published, one release per publish plugin.
+		 */
 		releases: Release[];
-	}
+	};
 }
 
-/** Run semantic-release and returns a Promise that resolves to a Result
- * object. */
+/**
+ * Run semantic-release and returns a Promise that resolves to a Result
+ * object.
+ */
 declare function SemanticRelease(options: SemanticRelease.Options,
 	environment?: SemanticRelease.Config): Promise<SemanticRelease.Result>;
 
