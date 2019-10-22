0d7abc34b928c17ce9f3ebe5ed8915283c3ee52f
Chives <chivesrs@gmail.com>
2019-10-21 22:59:08 -0400

    Use multiline format for authors (for readability)

--- types/angular-material/index.d.ts
+++ types/angular-material/index.d.ts
@@ -1,6 +1,10 @@
 // Type definitions for angular-material 1.1
 // Project: https://github.com/angular/material, https://material.angularjs.org
-// Definitions by: Blake Bigelow <https://github.com/blbigelow>, Peter Hajdu <https://github.com/PeterHajdu>, Davide Donadello <https://github.com/Dona278>, Geert Jansen <https://github.com/geertjansen>, Edward Knowles <https://github.com/eknowles>
+// Definitions by: Blake Bigelow <https://github.com/blbigelow>
+//                 Peter Hajdu <https://github.com/PeterHajdu>
+//                 Davide Donadello <https://github.com/Dona278>
+//                 Geert Jansen <https://github.com/geertjansen>
+//                 Edward Knowles <https://github.com/eknowles>
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
 // TypeScript Version: 2.3
 
