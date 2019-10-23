1b4ee82cae371dd97ac290b210bf19fc7f597a9d
Reece Dunham <me@rdil.rocks>
2019-10-23 09:12:26 -0400

    made the things constant

--- types/js-roman-numerals/js-roman-numerals-tests.ts
+++ types/js-roman-numerals/js-roman-numerals-tests.ts
@@ -1,7 +1,7 @@
 import RomanNumeral = require('js-roman-numerals');
 
-let firstvar = new RomanNumeral("IV");
-let secondvar = new RomanNumeral(4);
+const firstvar = new RomanNumeral("IV");
+const secondvar = new RomanNumeral(4);
 
 let testIntThing: number = firstvar.toInt();
 let testStringThing: String = secondvar.toString();
