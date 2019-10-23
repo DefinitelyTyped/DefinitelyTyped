111b6c5576f422095ce9610b39294987239da024
Reece Dunham <me@rdil.rocks>
2019-10-23 09:11:58 -0400

    fixes

--- types/js-roman-numerals/index.d.ts
+++ types/js-roman-numerals/index.d.ts
@@ -1,12 +1,12 @@
-// Type definitions for js-roman-numerals
+// Type definitions for js-roman-numerals 1.1.0
 // Project: https://github.com/bcotrim/roman-numerals
 // Definitions by: Reece Dunham <https://rdil.rocks>
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
 // TypeScript Version: 3.6
 
 export namespace RomanNumeral {
-    constructor(value: number): void
-    constructor(value: String): void
-    toInt(): number
-    toString(): String
+    constructor(value: number): void;
+    constructor(value: String): void;
+    toInt(): number;
+    toString(): String;
 }
