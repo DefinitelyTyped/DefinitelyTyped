eebc3826d18f426072be2eafe64dd0e510fc1b9b
mcpar-land <john@mcpar.land>
2019-10-23 14:26:07 -0500

    Added vcf package definitions

--- types/vcf/vcf-tests.ts
+++ types/vcf/vcf-tests.ts
@@ -1,11 +1,6 @@
 import vCard from 'vcf';
 
-import * as fs from 'fs';
-import * as path from 'path';
-
-const card = new vCard().parse(
-	fs.readFileSync(path.join(__dirname + '/types/vcf/data/john-doe-1_basic.vcf'))
-);
+const card = new vCard();
 
 const json = card.toJSON();
 
