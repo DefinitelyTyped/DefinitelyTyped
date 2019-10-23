0d46af01c3214ccb8a30238b43b9044fdec3a383
Reece Dunham <me@rdil.rocks>
2019-10-23 08:58:01 -0400

    Add js roman numerals lib

---
+++ types/js-roman-numerals/index.d.ts
@@ -0,0 +1,10 @@
+// Type definitions for js-roman-numerals
+// Project: https://github.com/bcotrim/roman-numerals
+// Definitions by: Reece Dunham <https://rdil.rocks>
+// Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
+// TypeScript Version: 3.6
+
+export namespace RomanNumeral {
+    constructor(value: number): void
+    constructor(value: string): void
+}
