73a7b18d399484c185bcd80fcb288218492102c9
Armando Aguirre <armando.aguirre@microsoft.com>
2019-10-23 13:40:02 -0700

    Added node reference

--- types/azure-kusto-data/index.d.ts
+++ types/azure-kusto-data/index.d.ts
@@ -4,6 +4,7 @@
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
 // TypeScript Version: 2.1
 
+/// <reference types="node" />
 import { UserCodeInfo } from "adal-node";
 
 export class Client {
