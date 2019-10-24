6e1f0c53ec06458813fc292d4660705575bb9c82
Nasreddine Bac Ali <nasreddine.bacali@eonics.nl>
2019-10-24 23:15:41 +0200

    apply library file layout

---
+++ types/node-os-utils/lib/os.d.ts
@@ -0,0 +1,25 @@
+export default class Os {
+    oos(): () => Promise<string>;
+
+    platform(): Platform;
+
+    uptime(): number;
+
+    ip(): string;
+
+    hostname(): string;
+
+    type(): string;
+
+    arch(): string;
+}
+
+type Platform = 'aix'
+    | 'android'
+    | 'darwin'
+    | 'freebsd'
+    | 'linux'
+    | 'openbsd'
+    | 'sunos'
+    | 'win32'
+    | 'cygwin';
