efd55db9d811078531d58fda300f73ce44900ef1
Gary King <garyking@gmail.com>
2019-10-24 17:59:52 -0400

    Fix TS version.

--- types/promise-sequential/index.d.ts
+++ types/promise-sequential/index.d.ts
@@ -2,6 +2,7 @@
 // Project: https://github.com/russiann/promise-sequential#readme
 // Definitions by: Gary King <https://github.com/garyking>
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
+// TypeScript Version: 2.1
 
 type PromiseFunction = () => Promise<any>;
 
