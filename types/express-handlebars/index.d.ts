506bf36b2ebf795bc0080b136780892f60a6fe89
Dennis Bartlett <bartlett.dc.1@gmail.com>
2019-10-25 10:16:06 -0400

    Added self to Definitions By

--- types/express-handlebars/index.d.ts
+++ types/express-handlebars/index.d.ts
@@ -1,6 +1,8 @@
 // Type definitions for express-handlebars
 // Project: https://github.com/ericf/express-handlebars
-// Definitions by: Sam Saint-Pettersen <https://github.com/stpettersens>, Igor Dultsev <https://github.com/yhaskell>
+// Definitions by: Sam Saint-Pettersen <https://github.com/stpettersens>
+//                 Igor Dultsev <https://github.com/yhaskell>
+//                 Dennis Bartlett <https://github.com/dcbartlett>
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
 // TypeScript Version: 2.3
 
