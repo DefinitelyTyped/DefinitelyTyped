6820ac78d8f56b9b2c79407c97e82d3b7dcd290f
Florian Rohrer <Florian.Rohrer@vivaldi.net>
2019-10-25 15:48:02 +0200

    Update react-google-recaptcha-tests.tsx

--- types/react-google-recaptcha/react-google-recaptcha-tests.tsx
+++ types/react-google-recaptcha/react-google-recaptcha-tests.tsx
@@ -1,9 +1,22 @@
-import ReCAPTCHA from 'react-google-recaptcha';
+import ReCAPTCHA, {ReCAPTCHA as ReCAPTCHA2} from 'react-google-recaptcha';
 import * as React from 'react';
 
 const basicRecapchta = <ReCAPTCHA sitekey="xxx" onChange={a => a}/>;
 const invisibleRecaptcha: React.SFC = () => {
     const recaptchaRef = React.createRef<ReCAPTCHA>();
+
+    return (
+	    <ReCAPTCHA
+  	        ref={recaptchaRef}
+	        sitekey="xxx"
+	        size="invisible"
+	    />
+    );
+};
+
+const basicRecapchta2 = <ReCAPTCHA2 sitekey="xxx" onChange={a => a}/>;
+const invisibleRecaptcha2: React.SFC = () => {
+    const recaptchaRef = React.createRef<ReCAPTCHA2>();
 
     return (
 	    <ReCAPTCHA
