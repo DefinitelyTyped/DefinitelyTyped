2097306c76623346bfd0c50264abde901e93c434
Alexander <alexander.tarasyuk@outlook.com>
2019-10-26 01:02:20 +0300

    akumina-core: Provides its own types

--- types/akumina-core/interfaces/ILanguageRequest.d.ts
+++
@@ -1,5 +0,0 @@
-// tslint:disable-next-line interface-name
-export default interface ILanguageRequest {
-    languageCode?: string;
-    languageId: number;
-}
--- types/akumina-core/interfaces/ISavePageWidgetRequest.d.ts
+++
@@ -1,7 +0,0 @@
-import IPageWidget from "./IPageWidget";
-
-// tslint:disable-next-line interface-name
-export default interface ISavePageWidgetRequest {
-    pageId: string;
-    pageWidgets: IPageWidget[];
-}
