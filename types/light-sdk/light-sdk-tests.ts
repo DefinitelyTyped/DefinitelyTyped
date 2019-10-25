e3cc7747057e9f48ed11b18ae2d8ce301e08eaae
cklwblove <lwbhtml@163.com>
2019-10-25 14:56:14 +0800

    完善接口定义

--- types/light-sdk/light-sdk-tests.ts
+++ types/light-sdk/light-sdk-tests.ts
@@ -1,12 +1,19 @@
 import LightSDK = require('light-sdk');
 
-LightSDK.register((options) => {
-	// ...
+interface Options {
+    cid: string;
+    iid: string;
+}
+
+LightSDK.register((options: Options) => {
+    // ...
+    const cidNew = options.cid;
+    const iidNew = options.iid;
 });
 LightSDK.config.get(() => {
-	// ...
+    // ...
 });
 
 LightSDK.openAPI.token(() => {
-	// ...
+    // ...
 });
