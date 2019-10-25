628db2dfa1cb554f1fdab2646551f2761b97a514
Alexander <alexander.tarasyuk@outlook.com>
2019-10-26 01:13:34 +0300

    find-java-home: Provides its own types

--- types/find-java-home/find-java-home-tests.ts
+++
@@ -1,6 +0,0 @@
-import findJavaHome = require("find-java-home");
-import { promisify } from "util";
-
-findJavaHome(() => { }); // $ExpectType void
-findJavaHome({ allowJre: true }, () => { }); // $ExpectType void
-promisify(findJavaHome); // $ExpectType (options?: { allowJre?: boolean | undefined; } | undefined) => Promise<string>
