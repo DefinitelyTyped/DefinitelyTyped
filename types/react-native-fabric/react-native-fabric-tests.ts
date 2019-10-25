2863cec908904ded59520fbb7fbf3e509e83df52
Alexander <alexander.tarasyuk@outlook.com>
2019-10-26 01:35:46 +0300

    react-native-fabric: Provides its own types

--- types/react-native-fabric/react-native-fabric-tests.ts
+++
@@ -1,7 +0,0 @@
-import { Answers, Crashlytics } from 'react-native-fabric';
-
-Answers.logSearch('query');
-Answers.logCustom('event', { customKey: 0 });
-
-Crashlytics.crash();
-Crashlytics.setUserName('name');
