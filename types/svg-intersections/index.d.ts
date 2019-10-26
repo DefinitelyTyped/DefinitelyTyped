30b566a663207cad1dabca9becde79988a08e73d
William Bergeron-Drouin <william@inter-net.ca>
2019-10-26 16:07:10 -0400

    Generated svg-intersections files using npx

---
+++ types/svg-intersections/index.d.ts
@@ -0,0 +1,4 @@
+// Type definitions for svg-intersections 0.4
+// Project: https://github.com/effektif/svg-intersections#readme
+// Definitions by: xWiiLLz <https://github.com/xWiiLLz>
+// Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
