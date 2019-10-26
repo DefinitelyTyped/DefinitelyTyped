4a14573f3dabb8b55b73909a3efd6d17fd9db925
Kyle Chine <kyle@leismore.co>
2019-10-27 09:33:58 +1100

    syntax error fix

--- types/uid-generator/uid-generator-tests.ts
+++ types/uid-generator/uid-generator-tests.ts
@@ -1,18 +1,18 @@
-import {default as UIDGenerator} from 'uid-generator';
+import { default as UIDGenerator } from 'uid-generator';
 
 let generator = new UIDGenerator('abc'); // $ExpectType UIDGenerator
     generator = new UIDGenerator(128, UIDGenerator.BASE58); // $ExpectType UIDGenerator
 
 generator.generateSync(); // $ExpectType string
-generator.generate((error, uid)=>{
+generator.generate((error, uid) => {
   if (error)
-    { throw error; }
+  { throw error; }
   else
-    { uid; } // $ExpectType string
+  { uid; } // $ExpectType string
 });
 generator.generate().then(uid => {
   uid; // $ExpectType string
-}).catch( e=>{throw e;} );
+}).catch(e => { throw e; });
 
 generator.bitSize; // $ExpectType number
 generator.uidLength; // $ExpectType number
