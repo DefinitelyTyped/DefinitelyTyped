eab9f0ad26ee066d945df332ecd0fca71c95fe31
William Bergeron-Drouin <william@inter-net.ca>
2019-10-26 16:08:35 -0400

    Added base typings for shape method with conditional second argument type

--- types/svg-intersections/index.d.ts
+++ types/svg-intersections/index.d.ts
@@ -2,3 +2,39 @@
 // Project: https://github.com/effektif/svg-intersections#readme
 // Definitions by: xWiiLLz <https://github.com/xWiiLLz>
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
+
+
+export type SvgElements = 'line' | 'rect' | 'circle' | 'ellipse' | 'polygon' | 'polyline' | 'path';
+
+// Svg element properties
+export interface LineProps {
+    x1: number;
+    y1: number;
+    x2: number;
+    y2: number;
+}
+
+export interface RectProps {
+    rx: number;
+    ry: number;
+    x: number;
+    y: number;
+    width: number;
+    height: number;
+}
+
+export interface CircleProps {
+    cx: number;
+    cy: number;
+    r: number;
+}
+
+export type SvgProperties<T extends SvgElements> =
+    T extends 'line' ? LineProps :
+    T extends 'rect' ? RectProps :
+    T extends 'circle' ? CircleProps :
+    never;
+
+
+export function shape<T extends SvgElements>(svgElementName: T, svgProps: SvgProperties<typeof svgElementName>): any;
+
