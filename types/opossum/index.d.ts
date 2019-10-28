f936e683f29022e20bc75c1169c3efd01b91ce5a
Tom Jenkinson <tom@tjenkinson.me>
2019-10-28 20:12:33 +0000

    remove patch version

--- types/opossum/index.d.ts
+++ types/opossum/index.d.ts
@@ -1,4 +1,4 @@
-// Type definitions for opossum 4.1.0
+// Type definitions for opossum 4.1
 // Project: https://github.com/nodeshift/opossum, https://nodeshift.dev/opossum
 // Definitions by: Quinn Langille <https://github.com/quinnlangille>
 //                 Willy Zhang <https://github.com/merufm>
