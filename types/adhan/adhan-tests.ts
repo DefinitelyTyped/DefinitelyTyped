2d1fd21c05cc2003b24140bb33ba10a9a5ffd81b
Ikramullah Latif <32958839+skymunn@users.noreply.github.com>
2019-10-28 23:05:49 +0800

    Linter checking

--- types/adhan/adhan-tests.ts
+++ types/adhan/adhan-tests.ts
@@ -1,16 +1,17 @@
-import adhan from 'adhan'
+import adhan from 'adhan';
 
-// Get coordinates 
-const coordinates = new adhan.Coordinates(35.78056, -78.6389)
+// Get coordinates
+const coordinates = new adhan.Coordinates(35.78056, -78.6389);
 // Get date right now
-const date = new Date()
+const date = new Date();
 // Calculation paramaters
-const param = adhan.CalculationMethod.MuslimWorldLeague()
-param.madhab = adhan.Madhab.Shafi
-param.fajrAngle = 13
-param.highLatitudeRule = adhan.HighLatitudeRule.TwilightAngle
-param.adjustments.dhuhr = 4
+const param = adhan.CalculationMethod.MuslimWorldLeague();
+param.madhab = adhan.Madhab.Shafi;
+param.fajrAngle = 13;
+param.highLatitudeRule = adhan.HighLatitudeRule.TwilightAngle;
+param.adjustments.dhuhr = 4;
 
 // Get prayertimes
-const prayerTimes = new adhan.PrayerTimes(coordinates, date, param)
-adhan.Date.formattedTime(prayerTimes.fajr)
+const prayerTimes = new adhan.PrayerTimes(coordinates, date, param);
+adhan.Date.formattedTime(prayerTimes.fajr);
+adhan.Date.formattedTime(prayerTimes.asr, 20, '24h')
