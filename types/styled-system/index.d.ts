120b5191d7721555e089ae46bbceba186c42c271
craig.thompson <craig.thompson@hive.hr>
2019-10-28 11:49:11 +0000

    chore: fix version

--- types/styled-system/index.d.ts
+++ types/styled-system/index.d.ts
@@ -1,4 +1,4 @@
-// Type definitions for styled-system 5.1.3
+// Type definitions for styled-system 5.1
 // Project: https://github.com/jxnblk/styled-system#readme
 // Definitions by: Marshall Bowers <https://github.com/maxdeviant>
 //                 Ben McCormick <https://github.com/phobon>
