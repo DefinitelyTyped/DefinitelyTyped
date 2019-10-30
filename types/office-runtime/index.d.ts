d32e03040d255778da716494f5ccdc1b771af642
sumurthy <suramam@microsoft.com>
2019-10-30 11:11:23 -0700

    Fixes to comment alignment

--- types/office-runtime/index.d.ts
+++ types/office-runtime/index.d.ts
@@ -187,10 +187,10 @@ declare namespace OfficeRuntime {
    */
   interface ApiInformation {
     /**
-    * Check if the specified requirement set is supported by the host Office application.
-    * @param name - Set name; e.g., "MatrixBindings".
-    * @param minVersion - The minimum required version; e.g., "1.4".
-    */
+     * Check if the specified requirement set is supported by the host Office application.
+     * @param name - Set name; e.g., "MatrixBindings".
+     * @param minVersion - The minimum required version; e.g., "1.4".
+     */
     isSetSupported(name: string, minVersion?: string): boolean;
   }
 }
