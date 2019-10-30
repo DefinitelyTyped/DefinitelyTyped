30059a7578194e37c5b65a1f91ad148495d50b66
Matt Burstein <j.matthew.burstein@gmail.com>
2019-10-30 23:08:42 +0000

    [@types/nunjucks] Add tests for improved Environment function signiatures

--- types/nunjucks/index.d.ts
+++ types/nunjucks/index.d.ts
@@ -1,6 +1,7 @@
 // Type definitions for nunjucks 3.1
 // Project: http://mozilla.github.io/nunjucks/, https://github.com/mozilla/nunjucks
 // Definitions by: Ruben Slabbert <https://github.com/RubenSlabbert>
+//                 Matthew Burstein <https://github.com/MatthewBurstein>
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
 // TypeScript Version: 2.2
 
