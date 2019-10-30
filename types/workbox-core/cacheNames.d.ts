131ce75e938508b898867793d02a5a7eabf52f1c
Jason Kwok <JasonHK@users.noreply.github.com>
2019-10-30 10:44:49 +0800

    [workbox-core] Updated ts-doc

--- types/workbox-core/cacheNames.d.ts
+++ types/workbox-core/cacheNames.d.ts
@@ -8,5 +8,12 @@ export interface CacheNames {
 
 /**
  * Get the current cache names and prefix/suffix used by Workbox.
+ * 
+ * `cacheNames.precache` is used for precached assets, 
+ * `cacheNames.googleAnalytics` is used by `workbox-google-analytics` to store 
+ * `analytics.js`, and `cacheNames.runtime` is used for everything else.
+ *
+ * `cacheNames.prefix` can be used to retrieve just the current prefix value.
+ * `cacheNames.suffix` can be used to retrieve just the current suffix value.
  */
 export const cacheNames: CacheNames;
