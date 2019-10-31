e3deb09975206ec3dda28b663ee7fbfbb79c4072
Holger Jeromin <mailgithub@katur.de>
2019-10-31 14:08:40 +0100

    real Array<T> fix

--- types/jasmine/index.d.ts
+++ types/jasmine/index.d.ts
@@ -836,11 +836,11 @@ declare namespace jasmine {
         /** By chaining the spy with calls.count(), will return the number of times the spy was called */
         count(): number;
         /** By chaining the spy with calls.argsFor(), will return the arguments passed to call number index */
-        argsFor<T = any>(index: number): Array<T>;
+        argsFor<T = any>(index: number): T[];
         /** By chaining the spy with calls.allArgs(), will return the arguments to all calls */
-        allArgs<T = any>(): Array<T>;
+        allArgs<T = any>(): T[];
         /** By chaining the spy with calls.all(), will return the context (the this) and arguments passed all calls */
-        all<T = any>(): CallInfo<T>[];
+        all<T = any>(): Array<CallInfo<T>>;
         /** By chaining the spy with calls.mostRecent(), will return the context (the this) and arguments for the most recent call */
         mostRecent<T = any>(): CallInfo<T>;
         /** By chaining the spy with calls.first(), will return the context (the this) and arguments for the first call */
@@ -853,7 +853,7 @@ declare namespace jasmine {
         /** The context (the this) for the call */
         object: any;
         /** All arguments passed to the call */
-        args: Array<T>;
+        args: T[];
         /** The return value of the call */
         returnValue: any;
     }
