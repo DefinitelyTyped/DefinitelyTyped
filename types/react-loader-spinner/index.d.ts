493e6ed4a490915e1653d79265c80eb6428fc4b7
Rayhan Wirjowerdojo <rayhan.wirjowerdojo@gmail.com>
2019-11-01 22:48:50 +0800

    Added type definitions for react-loader-spinner

---
+++ types/react-loader-spinner/index.d.ts
@@ -0,0 +1,61 @@
+// Type definitions for react-loader-spinner
+// Project: https://github.com/mhnpd/react-loader-spinner
+// Definitions by: Rayhan Wirjowerdojo <https://github.com/rayhanw>
+// Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
+// TypeScript Version: 3.6.4
+
+import { FC } from 'react';
+
+/**
+ * Possible Types:
+ * Audio
+ * BallTriangle
+ * Bars
+ * Circles
+ * Grid
+ * Hearts
+ * Oval
+ * Puff
+ * Rings
+ * TailSpin
+ * ThreeDots
+ * Watch
+ * RevolvingDot
+ * Triangle
+ * Plane
+ * MutatingDots
+ * None (Will default to Audio)
+ * NotSpecified (Will default to Audio)
+ **/
+
+type Types =
+    | 'Audio'
+    | 'BallTriangle'
+    | 'Bars'
+    | 'Circles'
+    | 'Grid'
+    | 'Hearts'
+    | 'Oval'
+    | 'Puff'
+    | 'Rings'
+    | 'TailSpin'
+    | 'ThreeDots'
+    | 'Watch'
+    | 'RevolvingDot'
+    | 'Triangle'
+    | 'Plane'
+    | 'MutatingDots'
+    | 'None'
+    | 'NotSpecified';
+
+interface ILoaderProps {
+    type?: Types;
+    color?: string;
+    timeout?: number; // in milliseconds
+    height?: number;
+    width?: number;
+    visible?: boolean | string;
+}
+
+declare const Loader: FC<ILoaderProps>;
+export default Loader;
