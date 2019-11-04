2fd8a97042640b49b38ec1659cc8149e7f99643c
Mathias Lorenzen <mathias.lorenzen@broelstaerk.dk>
2019-11-04 08:54:06 +0100

    fixed tests.

--- types/alloy/alloy-tests.ts
+++ types/alloy/alloy-tests.ts
@@ -1,8 +1,10 @@
 function test_controller_on() {
+	var $: AlloyController = null as any;
 	$.on('foo', console.log);
 }
 
 function test_controller_create_style() {
+	var $: AlloyController = null as any;
 	$.createStyle({
 		foo: 'bar'
 	});
