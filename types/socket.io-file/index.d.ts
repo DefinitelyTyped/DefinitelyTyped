3198f912ccfdfea6d6f1bad1d1010199ad66cc5b
Dief Bell <DBell@prolec.co.uk>
2019-11-04 12:46:56 +0000

    Added types for socket.io-file package

--- types/socket.io-file/index.d.ts
+++ types/socket.io-file/index.d.ts
@@ -1,13 +1,21 @@
+<<<<<<< HEAD
 // Type definitions for socket.io-file 2.0
 // Project: https://github.com/rico345100/socket.io-file
 // Definitions by: Dief Bell <https://github.com/merrickking>
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
 // TypeScript Version: 2.3
+=======
+// Type definitions for socket.io-file 2.0.31
+// Project: https://github.com/rico345100/socket.io-file
+// Definitions by: Dief Bell <https://github.com/merrickking>
+// Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
+>>>>>>> Added types for socket.io-file package
 
 import { Socket } from 'socket.io';
 
 export = SocketIOFile;
 
+<<<<<<< HEAD
 declare class SocketIOFile {
 	constructor(socket: Socket, options: Options);
 	on(event: string, cb: (fileInfo: FileInfo) => void): void;
@@ -35,4 +43,36 @@ interface FileInfo {
 	estimated: number;
 	uploadId: string;
 	originalFileName: string;
+=======
+declare class SocketIOFile
+{
+	constructor (socket : Socket, options : IOptions);
+	on(event : string, cb : (fileInfo : IFileInfo) => void) : void;
+}
+
+interface IOptions
+{
+	uploadDir : string | { [dirId : string] : string };
+	maxFileSize? : number;
+	accepts? : string[];
+	chunkSize? : number;
+	transmissionDelay? : number;
+	overwrite? : boolean;
+	rename? : (fileName : string, fileInfo : IFileInfo) => string | string;
+	resume? : boolean;
+}
+
+interface IFileInfo
+{
+	name : string;
+	size : number;
+	path : string;
+	wrote : number;
+	uploadDir : string;
+	data : any[];
+	mime : string;
+	estimated : number;
+	uploadId : string;
+	originalFileName : string;
+>>>>>>> Added types for socket.io-file package
 }
