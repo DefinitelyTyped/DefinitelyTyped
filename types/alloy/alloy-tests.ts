7e0ef09af7b1f94edd7d78226984b4d01a02d7cd
Mathias Lorenzen <mathias.lorenzen@broelstaerk.dk>
2019-11-04 09:00:45 +0100

    fix for scoping.

--- types/alloy/alloy-tests.ts
+++ types/alloy/alloy-tests.ts
@@ -1,10 +1,10 @@
 function test_controller_on() {
-	var $: AlloyController = null as any;
+	const $: AlloyController = null as any;
 	$.on('foo', console.log);
 }
 
 function test_controller_create_style() {
-	var $: AlloyController = null as any;
+	const $: AlloyController = null as any;
 	$.createStyle({
 		foo: 'bar'
 	});
