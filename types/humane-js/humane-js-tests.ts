0f0d84477c8add01c2b79c06d279716a8bd00187
Christophe Coevoet <stof@notk.org>
2019-11-05 22:25:01 +0100

    [humane-js] Fix humane-js types (#40043)
    * Rename the humane types to humane-js to match the package name
    
    * Improve humane-js types
    
    - define the exported CommonJS API instead of documenting the global
      variable from the UMD pattern
    - fix the list of available options
    - use proper signatures for various functions instead of generic
      Function types
    - use optional parameters instead of function overload without extra
      params for proper types
    
    * Enable the strict linting rules for the humane-js types

--- types/humane-js/humane-js-tests.ts
+++ types/humane-js/humane-js-tests.ts
@@ -1,8 +1,8 @@
-
+import humane = require('humane-js');
 
 humane.log("Welcome Back");
-humane.log(["List","of","Items"]);
-humane.log("Callback when finished", function(){ document.body.style.backgroundColor="#a66000" });
+humane.log(["List", "of", "Items"]);
+humane.log("Callback when finished", () => { document.body.style.backgroundColor = "#a66000"; });
 humane.log("Options can be passed", { timeout: 4000, clickToClose: true, addnCls: 'humane-error' });
 
 humane.timeout = 5000;
@@ -14,14 +14,14 @@ humane.info = humane.spawn({ addnCls: 'humane-libnotify-info', timeout: 1000 });
 humane.info('Info Themed Notifier');
 humane.error = humane.spawn({ addnCls: 'humane-libnotify-error', timeout: 1000 });
 humane.error('Error Themed Notifier');
-humane.remove(function(){ alert('removed') });
+humane.remove(() => alert('removed'));
 
-var bigbox = humane.create({baseCls: 'humane-bigbox', timeout: 1000});
+const bigbox = humane.create({baseCls: 'humane-bigbox', timeout: 1000});
 bigbox.error = bigbox.spawn({addnCls: 'humane-bigbox-error'});
 bigbox.log('Oh!').error('No!');
 
-var libnotify = humane.create({baseCls: 'humane-libnotify', addnCls: 'humane-libnotify-info'});
+const libnotify = humane.create({baseCls: 'humane-libnotify', addnCls: 'humane-libnotify-info'});
 libnotify.log('Notified');
 
-var jacked = humane.create({baseCls: 'humane-jackedup', addnCls: 'humane-jackedup-success'});
+const jacked = humane.create({baseCls: 'humane-jackedup', addnCls: 'humane-jackedup-success'});
 jacked.log("What's up here!");
