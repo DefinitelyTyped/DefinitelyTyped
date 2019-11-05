ad420150cc76c0ad3b823ac35daffc0c784338a0
syncfusion-javascript <packages@syncfusion.com>
2019-11-05 18:46:53 +0530

    Error fixed

--- types/ej.web.all/index.d.ts
+++ types/ej.web.all/index.d.ts
@@ -1,4 +1,4 @@
-// Type definitions for ej.web.all 17.3
+// Type definitions for non-npm package ej.web.all 17.3
 // Project: http://help.syncfusion.com/js/typescript
 // Definitions by: Syncfusion <https://github.com/syncfusion>
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
