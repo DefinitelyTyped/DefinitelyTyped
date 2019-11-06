f5fd25dd9379af21d7c01eec72e657d74a5d8e0c
ExE Boss <3889017+ExE-Boss@users.noreply.github.com>
2019-11-06 01:15:00 +0100

    fix(es‑abstract/helpers): Correct `getIteratorMethod(…)` type

--- types/es-abstract/helpers/getIteratorMethod.d.ts
+++ types/es-abstract/helpers/getIteratorMethod.d.ts
@@ -1,12 +1,12 @@
 import { PropertyKey as ESPropertyKey } from '../index';
 
-declare function getIteratorMethod<T extends Iterable<unknown>>(
+declare function getIteratorMethod<T>(
 	ES: {
 		GetMethod(O: unknown, P: ESPropertyKey): (...args: unknown[]) => unknown | undefined;
 		IsArray?(O: unknown): boolean;
 		Type?(O: unknown): string | undefined;
 	},
 	iterable: T,
-): () => Iterator<T extends Iterable<infer U> ? U : unknown>;
+): T extends Iterable<unknown> ? () => Iterator<T extends Iterable<infer U> ? U : unknown> : undefined;
 
 export = getIteratorMethod;
--- types/es-abstract/test/helpers/getIteratorMethod.test.ts
+++ types/es-abstract/test/helpers/getIteratorMethod.test.ts
@@ -7,6 +7,9 @@ const never: never = undefined as never;
 getIteratorMethod(ES2015, [1]); // $ExpectType () => Iterator<number, any, undefined>
 getIteratorMethod(ES2015, ['2']); // $ExpectType () => Iterator<string, any, undefined>
 getIteratorMethod(ES2015, [1, '2']); // $ExpectType () => Iterator<string | number, any, undefined>
+getIteratorMethod(ES2015, '2'); // $ExpectType () => Iterator<string, any, undefined>
 
-getIteratorMethod<any>(ES2015, any); // $ExpectType () => Iterator<unknown, any, undefined>
-getIteratorMethod(ES2015, never); // $ExpectType () => Iterator<never, any, undefined>
+getIteratorMethod(ES2015, undefined); // $ExpectType undefined
+getIteratorMethod(ES2015, {}); // $ExpectType undefined
+getIteratorMethod<any>(ES2015, any); // $ExpectType (() => Iterator<unknown, any, undefined>) | undefined
+getIteratorMethod(ES2015, never); // $ExpectType never
