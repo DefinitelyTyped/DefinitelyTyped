a93f9e8710fec913a20c107bb1123f91f4598860
PopGoesTheWza <PopGoesTheWza@gmail.com>
2019-11-07 01:25:21 +0100

    linting

--- types/google-apps-script/google-apps-script.url-fetch.d.ts
+++ types/google-apps-script/google-apps-script.url-fetch.d.ts
@@ -30,60 +30,60 @@ declare namespace GoogleAppsScript {
         url: string;
       }
       interface URLFetchRequestOptions {
-          /**
-           * the content type (defaults to 'application/x-www-form-urlencoded'). Another example of content
-           * type is 'application/xml; charset=utf-8'.
-           */
-          contentType?: string;
+        /**
+         * the content type (defaults to 'application/x-www-form-urlencoded'). Another example of content
+         * type is 'application/xml; charset=utf-8'.
+         */
+        contentType?: string;
 
-          /**
-           * a JavaScript key/value map of HTTP headers for the request
-           */
-          headers?: HttpHeaders;
+        /**
+         * a JavaScript key/value map of HTTP headers for the request
+         */
+        headers?: HttpHeaders;
 
-          /**
-           * the HTTP method for the request: get, delete, patch, post, or put. The default is get.
-           */
-          method?: HttpMethod;
+        /**
+         * the HTTP method for the request: get, delete, patch, post, or put. The default is get.
+         */
+        method?: HttpMethod;
 
-          /**
-           * the payload (e.g. POST body) for the request. Certain HTTP methods (e.g. GET) do not accept a
-           * payload. It can be a string, a byte array, or a JavaScript object. A JavaScript object will be
-           * interpretted as a map of form field names to values, where the values can be either strings or blobs.
-           */
-          payload?: Payload;
+        /**
+         * the payload (e.g. POST body) for the request. Certain HTTP methods (e.g. GET) do not accept a
+         * payload. It can be a string, a byte array, or a JavaScript object. A JavaScript object will be
+         * interpretted as a map of form field names to values, where the values can be either strings or blobs.
+         */
+        payload?: Payload;
 
-          /**
-           * Deprecated. This instructs fetch to resolve the specified URL within the intranet linked to your
-           * domain through (deprecated) SDC
-           */
-          useIntranet?: boolean;
+        /**
+         * Deprecated. This instructs fetch to resolve the specified URL within the intranet linked to your
+         * domain through (deprecated) SDC
+         */
+        useIntranet?: boolean;
 
-          /**
-           * if this is set to false, the fetch will ignore any invalid certificates for HTTPS requests.
-           * The default is true.
-           */
-          validateHttpsCertificates?: boolean;
+        /**
+         * if this is set to false, the fetch will ignore any invalid certificates for HTTPS requests.
+         * The default is true.
+         */
+        validateHttpsCertificates?: boolean;
 
-          /**
-           * if this is set to false, the fetch not automatically follow HTTP redirects; it will return
-           * the original HTTP response. The default is true.
-           */
-          followRedirects?: boolean;
+        /**
+         * if this is set to false, the fetch not automatically follow HTTP redirects; it will return
+         * the original HTTP response. The default is true.
+         */
+        followRedirects?: boolean;
 
-          /**
-           * if this is set to true, the fetch will not throw an exception if the response code indicates
-           * failure, and will instead return the HTTPResponse (default: false)
-           */
-          muteHttpExceptions?: boolean;
+        /**
+         * if this is set to true, the fetch will not throw an exception if the response code indicates
+         * failure, and will instead return the HTTPResponse (default: false)
+         */
+        muteHttpExceptions?: boolean;
 
-          /**
-           * if this is set to false, reserved characters in the URL will not be escaped (default: true)
-           */
-          escaping?: boolean;
-        }
+        /**
+         * if this is set to false, reserved characters in the URL will not be escaped (default: true)
+         */
+        escaping?: boolean;
+      }
 
-      /**
+    /**
      * Fetch resources and communicate with other hosts over the Internet.
      *
      * This service allows scripts to communicate with other applications or access other resources
