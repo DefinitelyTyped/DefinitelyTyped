692e44020c4d7c015d4713639778ba2b9f2194d4
Pat Lillis <plillis@soundhound.com>
2019-11-07 13:12:24 -0700

    Fix linting

--- types/nuclear-js/nuclear-js-tests.ts
+++ types/nuclear-js/nuclear-js-tests.ts
@@ -10,7 +10,7 @@ import {
     createReactMixin,
 } from 'nuclear-js';
 
-Immutable.Map({ a: 1 }) as Immutable.Map<any, any>;
+Immutable.Map({ a: 1 });
 Immutable.fromJS([5]);
 
 // Callable with or without `new`.
@@ -21,21 +21,21 @@ Reactor({ debug: undefined });
 // Make sure that type checking succeeds with or without `new`.
 const r1 = new Reactor();
 const r2 = Reactor();
-r1.dispatch('FETCH_ENTITY_SUCCESS') as void;
-r1.dispatch('FETCH_ENTITY_SUCCESS', { data: 5 }) as void;
-r1.batch(() => null) as void;
+r1.dispatch('FETCH_ENTITY_SUCCESS');
+r1.dispatch('FETCH_ENTITY_SUCCESS', { data: 5 });
+r1.batch(() => null);
 r1.evaluate(['keyPath']);
 r1.evaluate([['keyPath'], (dep1: any) => 5]);
 r1.evaluateToJS(['keyPath']);
 r1.evaluateToJS([['keyPath'], (dep1: any) => 5]);
-r1.observe(() => null)() as void;
-r1.observe(['getter'], (x: any) => null)() as void;
-r1.observe(['getter'], () => null)() as void;
-r1.unobserve(['getter'], (x: any) => null) as void;
-r1.unobserve(['getter'], () => null) as void;
+r1.observe(() => null)();
+r1.observe(['getter'], (x: any) => null)();
+r1.observe(['getter'], () => null)();
+r1.unobserve(['getter'], (x: any) => null);
+r1.unobserve(['getter'], () => null);
 r1.serialize();
-r1.loadState({}) as void;
-r1.registerStores({}) as void;
+r1.loadState({});
+r1.registerStores({});
 r1.registerStores({
     numberStore: new Store<number>({
         getInitialState() {
@@ -43,8 +43,8 @@ r1.registerStores({
         },
         initialize() {},
     }),
-}) as void;
-r1.replaceStores({}) as void;
+});
+r1.replaceStores({});
 r1.replaceStores({
     numberStore: new Store<number>({
         getInitialState() {
@@ -52,29 +52,29 @@ r1.replaceStores({
         },
         initialize() {},
     }),
-}) as void;
+});
 r1.prevReactorState;
 r1.reactorState;
 r1.observerState;
-r1.ReactMixin.componentDidMount() as void;
-r1.ReactMixin.componentWillUnmount() as void;
+r1.ReactMixin.componentDidMount();
+r1.ReactMixin.componentWillUnmount();
 r1.ReactMixin.getInitialState();
-r2.reset() as void;
-r2.dispatch('FETCH_ENTITY_SUCCESS') as void;
-r2.dispatch('FETCH_ENTITY_SUCCESS', { data: 5 }) as void;
-r2.batch(() => null) as void;
+r2.reset();
+r2.dispatch('FETCH_ENTITY_SUCCESS');
+r2.dispatch('FETCH_ENTITY_SUCCESS', { data: 5 });
+r2.batch(() => null);
 r2.evaluate(['keyPath']);
 r2.evaluate([['keyPath'], (dep1: any) => 5]);
 r2.evaluateToJS(['keyPath']);
 r2.evaluateToJS([['keyPath'], (dep1: any) => 5]);
-r2.observe(() => null)() as void;
-r2.observe(['getter'], (x: any) => null)() as void;
-r2.observe(['getter'], () => null)() as void;
-r2.unobserve(['getter'], (x: any) => null) as void;
-r2.unobserve(['getter'], () => null) as void;
+r2.observe(() => null)();
+r2.observe(['getter'], (x: any) => null)();
+r2.observe(['getter'], () => null)();
+r2.unobserve(['getter'], (x: any) => null);
+r2.unobserve(['getter'], () => null);
 r2.serialize();
-r2.loadState({}) as void;
-r2.registerStores({}) as void;
+r2.loadState({});
+r2.registerStores({});
 r2.registerStores({
     numberStore: new Store<number>({
         getInitialState() {
@@ -82,8 +82,8 @@ r2.registerStores({
         },
         initialize() {},
     }),
-}) as void;
-r2.replaceStores({}) as void;
+});
+r2.replaceStores({});
 r2.replaceStores({
     numberStore: new Store<number>({
         getInitialState() {
@@ -91,13 +91,13 @@ r2.replaceStores({
         },
         initialize() {},
     }),
-}) as void;
-r2.reset() as void;
+});
+r2.reset();
 r2.prevReactorState;
 r2.reactorState;
 r2.observerState;
-r2.ReactMixin.componentDidMount() as void;
-r2.ReactMixin.componentWillUnmount() as void;
+r2.ReactMixin.componentDidMount();
+r2.ReactMixin.componentWillUnmount();
 r2.ReactMixin.getInitialState();
 
 // Callable with or without `new`.
@@ -125,30 +125,30 @@ const s2 = Store<string>({
         return '15';
     },
 });
-s1.getInitialState() as number;
-s1.initialize() as void;
-s1.handleReset(5) as number;
+s1.getInitialState();
+s1.initialize();
+s1.handleReset(5);
 s1.serialize(15);
-s1.deserialize({}) as number;
-s1.handle(51, 'FETCH_THING', {}) as number;
+s1.deserialize({});
+s1.handle(51, 'FETCH_THING', {});
 s1.on('FETCH_THING', (x: number, y: any) => 15);
-s2.getInitialState() as string;
-s2.initialize() as void;
-s2.handleReset('5') as string;
+s2.getInitialState();
+s2.initialize();
+s2.handleReset('5');
 s2.serialize('15');
-s2.deserialize({}) as string;
-s2.handle('51', 'FETCH_THING', {}) as string;
+s2.deserialize({});
+s2.handle('51', 'FETCH_THING', {});
 s2.on('FETCH_THING', (x: string, y: any) => '15');
 
-isKeyPath({}) as boolean;
-isKeyPath(['getter']) as boolean;
-isKeyPath('') as boolean;
-isKeyPath(5) as boolean;
+isKeyPath({});
+isKeyPath(['getter']);
+isKeyPath('');
+isKeyPath(5);
 
-isGetter({}) as boolean;
-isGetter(['getter']) as boolean;
-isGetter('') as boolean;
-isGetter(5) as boolean;
+isGetter({});
+isGetter(['getter']);
+isGetter('');
+isGetter(5);
 
 toJS({});
 toJS([]);
@@ -160,10 +160,10 @@ toImmutable([]);
 toImmutable('');
 toImmutable(5);
 
-isImmutable({}) as boolean;
-isImmutable([]) as boolean;
-isImmutable('') as boolean;
-isImmutable(5) as boolean;
+isImmutable({});
+isImmutable([]);
+isImmutable('');
+isImmutable(5);
 
 createReactMixin(r1);
 createReactMixin(r2);
