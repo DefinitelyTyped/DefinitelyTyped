117bc3d310276c1816e072ed286716c99c7aa368
Chris Wilkinson <c.wilkinson@elifesciences.org>
2019-11-08 10:15:48 +0000

    Update version

--- types/indefinite/index.d.ts
+++ types/indefinite/index.d.ts
@@ -1,4 +1,4 @@
-// Type definitions for indefinite 2.2
+// Type definitions for indefinite 2.3
 // Project: https://github.com/tandrewnichols/indefinite
 // Definitions by: omaishar <https://github.com/omaishr>
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
