889584678371e08a1b51cf581ce6f839239687c8
Chris Wilkinson <c.wilkinson@elifesciences.org>
2019-11-08 10:30:27 +0000

    Fix link

--- types/indefinite/index.d.ts
+++ types/indefinite/index.d.ts
@@ -1,6 +1,6 @@
 // Type definitions for indefinite 2.3
 // Project: https://github.com/tandrewnichols/indefinite
-// Definitions by: omaishar <https://github.com/omaishr>
+// Definitions by: omaishar <https://github.com/omaishar>
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
 
 export interface Options {
