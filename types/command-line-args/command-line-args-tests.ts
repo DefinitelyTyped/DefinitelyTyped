e40120939a995cf15f6b904d700cd12e529be6d3
Emanuele Stoppa <my.burning@gmail.com>
2019-11-08 11:53:47 +0000

    fix: command-line-args
    Possible now to pass an Option that extends the base type

--- types/command-line-args/command-line-args-tests.ts
+++ types/command-line-args/command-line-args-tests.ts
@@ -9,16 +9,44 @@ const optionDefinitions: commandLineArgs.OptionDefinition[] = [
         multiple: true,
         lazyMultiple: true,
         defaultOption: true,
-        group: 'one'
-    }
+        group: 'one',
+    },
+];
+
+interface CustomDefinition extends commandLineArgs.OptionDefinition {
+    description: string;
+}
+
+const customOptionDefinitions: CustomDefinition[] = [
+    {
+        name: 'something',
+        alias: 's',
+        type: String,
+        defaultValue: '1',
+        multiple: true,
+        lazyMultiple: true,
+        defaultOption: true,
+        group: 'one',
+        description: 'Desc',
+    },
 ];
 
 const options = commandLineArgs(optionDefinitions, {
-    argv: [ '--one', '1' ],
+    argv: ['--one', '1'],
     partial: true,
     stopAtFirstUnknown: true,
-    camelCase: true
+    camelCase: true,
+});
+
+const customOptions = commandLineArgs(customOptionDefinitions, {
+    argv: ['--one', '1'],
+    partial: true,
+    stopAtFirstUnknown: true,
+    camelCase: true,
 });
 
 const unknown = options._unknown;
 const something = options.something;
+
+const customUnknown = customOptions._unknown;
+const customSomething = customOptions.something;
--- types/command-line-args/index.d.ts
+++ types/command-line-args/index.d.ts
@@ -1,6 +1,7 @@
 // Type definitions for command-line-args 5.0
 // Project: https://github.com/75lb/command-line-args
 // Definitions by: Lloyd Brookes <https://github.com/75lb>
+// Definitions by: Emanuele Stoppa <https://github.com/ematipico>
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
 // TypeScript Version: 2.2
 
@@ -8,7 +9,10 @@
  * Returns an object containing option values parsed from the command line. By default it parses the global `process.argv` array.
  * Parsing is strict by default. To be more permissive, enable `partial` or `stopAtFirstUnknown` modes.
  */
-declare function commandLineArgs(optionDefinitions: commandLineArgs.OptionDefinition[], options?: commandLineArgs.ParseOptions): commandLineArgs.CommandLineOptions;
+declare function commandLineArgs<OptionDefinition = commandLineArgs.OptionDefinition>(
+    optionDefinitions: OptionDefinition[],
+    options?: commandLineArgs.ParseOptions,
+): commandLineArgs.CommandLineOptions;
 
 declare namespace commandLineArgs {
     interface CommandLineOptions {
