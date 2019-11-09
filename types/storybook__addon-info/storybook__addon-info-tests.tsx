a877f5d9bdc93b75ecbc8b0a0ef7b64694a8f29d
Gaëtan Maisse <gmaisse@solocal.com>
2019-11-09 15:25:19 +0100

    [storybook__addon-info]
     - Use types directly from `Storybook` repo as it is now written in TS
     - Update types to match version 5.2 of the addon and of Storybook (as they have to have the same version)

--- types/storybook__addon-info/storybook__addon-info-tests.tsx
+++ types/storybook__addon-info/storybook__addon-info-tests.tsx
@@ -1,77 +1,109 @@
-/// <reference types="storybook__react" />
+import React, { Component, FunctionComponent } from 'react';
+import { addDecorator, addParameters, storiesOf } from '@storybook/react';
+import { setDefaults, withInfo, TableComponentOptionProps } from '@storybook/addon-info';
 
-import * as React from 'react';
-import { addDecorator, storiesOf } from '@storybook/react';
-import { setDefaults, withInfo } from '@storybook/addon-info';
-
-const { Component } = React;
-
-const TableComponent = ({ propDefinitions }: { propDefinitions: Array<{
-    property: string;
-    propType: { [key: string]: any} | string;
-    required: boolean;
-    description: string;
-    defaultValue: any;
-}> }) => (
+const TableComponent: FunctionComponent<TableComponentOptionProps> = ({ propDefinitions }) => (
     <table>
         <thead>
-        <tr>
-            <th>property</th>
-            <th>propType</th>
-            <th>required</th>
-            <th>default</th>
-            <th>description</th>
-        </tr>
+            <tr>
+                <th>property</th>
+                <th>propType</th>
+                <th>required</th>
+                <th>default</th>
+                <th>description</th>
+            </tr>
         </thead>
         <tbody>
-        {propDefinitions.map(row => (
-            <tr key={row.property}>
-            <td>{row.property}</td>
-            <td>{row.required ? 'yes' : '-'}</td>
-            <td>
-                {row.defaultValue === undefined ? '-' : row.defaultValue}
-            </td>
-            <td>{row.description}</td>
-            </tr>
-        ))}
+            {propDefinitions.map(row => (
+                <tr key={row.property}>
+                    <td>{row.property}</td>
+                    <td>{row.required ? 'yes' : '-'}</td>
+                    <td>{row.defaultValue === undefined ? '-' : row.defaultValue}</td>
+                    <td>{row.description}</td>
+                </tr>
+            ))}
         </tbody>
     </table>
 );
 
+// `withInfo` used as global decorator
 addDecorator(withInfo);
 
+// define parameters of info addon as part of global parameters
+addParameters({
+    info: {
+        text: 'String or React Element with docs about my component',
+        inline: true,
+        header: true,
+        source: true,
+        propTables: false,
+        propTablesExclude: [TableComponent],
+        styles: {},
+        components: { table: TableComponent },
+        marksyConf: {},
+        maxPropObjectKeys: 1,
+        maxPropArrayLength: 2,
+        maxPropsIntoLine: 3,
+        maxPropStringLength: 4,
+        TableComponent,
+        excludedPropTypes: [],
+    },
+});
+
+// `withInfo` used as story decorator
+storiesOf('Component', module).addDecorator(withInfo);
+
+// Set parameters for multiple config
+storiesOf('Component', module)
+    .addParameters({
+        info: {
+            header: '',
+            // Your settings
+        },
+    })
+    .add('with some emoji', () => <Component />);
+
+storiesOf('Component', module)
+    .add(
+        'with some emoji',
+        () => <Component emoji />,
+        { info: { inline: true, header: false } }, // Make your component render inline with the additional info
+    )
+    .add(
+        'with no emoji',
+        () => <Component />,
+        { info: '☹️ no emojis' }, // Add additional info text directly
+    );
+
+// Check deprecated option functions
 setDefaults({
     inline: false,
-    propTables: false
+    propTables: false,
 });
 
 storiesOf('Component', module)
-  .add('no info',
-       withInfo()(() =>
-         <Component>Click the "?" mark at top-right to view the info.</Component>
-       )
-  )
-  .add('simple info',
-       withInfo('doc string about my component')(() =>
-         <Component>Click the "?" mark at top-right to view the info.</Component>
-       )
-  )
-  .add('using an options object',
-       withInfo({
-         text: 'String or React Element with docs about my component',
-         inline: true,
-         header: true,
-         source: true,
-         styles: {},
-         components: {},
-         marksyConf: {},
-         maxPropObjectKeys: 1,
-         maxPropArrayLength: 2,
-         maxPropsIntoLine: 3,
-         maxPropStringLength: 4,
-         TableComponent,
-         excludedPropTypes: [],
-       })(() =>
-         <Component>Click the "?" mark at top-right to view the info.</Component>
-       )
-  );
+    .add('no info', withInfo()(() => <Component>Click the "?" mark at top-right to view the info.</Component>))
+    .add(
+        'simple info',
+        withInfo('doc string about my component')(() => (
+            <Component>Click the "?" mark at top-right to view the info.</Component>
+        )),
+    )
+    .add(
+        'using an options object',
+        withInfo({
+            text: 'String or React Element with docs about my component',
+            inline: true,
+            header: true,
+            source: true,
+            styles: {},
+            components: {},
+            marksyConf: {},
+            maxPropObjectKeys: 1,
+            maxPropArrayLength: 2,
+            maxPropsIntoLine: 3,
+            maxPropStringLength: 4,
+            TableComponent,
+            excludedPropTypes: [],
+        })(() => <Component>Click the "?" mark at top-right to view the info.</Component>),
+    );
