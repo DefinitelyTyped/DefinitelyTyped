3cdebec32e0c1d388a93055a8101c101bb5588a9
Alex Jerabek <38896772+AlexJerabek@users.noreply.github.com>
2019-11-11 10:55:14 -0800

    [office-runtime] Adding code owners to office-runtime (#40139)
    * Adding code owners
    
    * Adding OfficeDev
    
    * Fixing whitespace
    
    * Removing whitespace

--- types/office-runtime/index.d.ts
+++ types/office-runtime/index.d.ts
@@ -1,12 +1,19 @@
 // Type definitions for non-npm package office-runtime 1.0
 // Project: https://github.com/OfficeDev/office-js
-// Definitions by: Michael Zlatskovsky <https://github.com/Zlatkovsky>, Michelle Scharlock <https://github.com/mscharlock>
+// Definitions by: OfficeDev <https://github.com/OfficeDev>,
+//                 Michael Zlatskovsky <https://github.com/Zlatkovsky>,
+//                 Michelle Scharlock <https://github.com/mscharlock>,
+//                 David Chesnut <https://github.com/davidchesnut>,
+//                 Alex Jerabek <https://github.com/AlexJerabek>,
+//                 Sudhi Ramamurthy <https://github.com/sumurthy>
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
 // Typescript Version: 2.4
+
 /*
 office-runtime
 Copyright (c) Microsoft Corporation
 */
+
 /**
  * Office runtime namespace.
  */
