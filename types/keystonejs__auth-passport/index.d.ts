6a7717578adcd446ebe644c24bfd00a374d5b14c
Kevin Brown <kevin.brown@techin.site>
2019-11-12 15:19:10 +1100

    Added rudimentary types for KeystoneJS

---
+++ types/keystonejs__auth-passport/index.d.ts
@@ -0,0 +1,24 @@
+// Type definitions for @keystonejs/auth-passport 5.1
+// Project: https://github.com/keystonejs/keystone
+// Definitions by: Kevin Brown <https://github.com/thekevinbrown>
+// Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
+// TypeScript Version: 3.5
+
+// Because this is a scoped package, without this line Typescript doesn't associate the
+// types with the right package.
+// tslint:disable-next-line:no-single-declare-module
+declare module '@keystonejs/auth-passport' {
+    import { Keystone } from '@keystonejs/keystone';
+
+    class GoogleAuthStrategy {}
+    class PassportAuthStrategy {
+        // TODO: Better types here: https://www.keystonejs.com/keystonejs/auth-passport/
+        constructor(
+            authType: string,
+            keystone: Keystone,
+            listKey: string,
+            config: any,
+            strategy: any
+        );
+    }
+}
