47aaebea0e74ec026189652a7b39248aeb96e6aa
Felix Robles <felix@fundstack.com>
2019-11-12 16:28:47 +0000

    add webhook endpoints resource to stripe

--- types/stripe/index.d.ts
+++ types/stripe/index.d.ts
@@ -33,6 +33,7 @@
 //                 Claus Stilborg <https://github.com/stilborg>
 //                 Jorgen Vik <https://github.com/jvik>
 //                 Richard Ward <https://github.com/richardwardza>
+//                 Felix Robles <https://github.com/findeton>
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
 // TypeScript Version: 2.8
 
@@ -122,6 +123,7 @@ declare class Stripe {
     setupIntents: Stripe.resources.SetupIntents;
     skus: Stripe.resources.SKUs;
     webhooks: Stripe.resources.WebHooks;
+    webhookEndpoints: Stripe.resources.WebhookEndpoints;
     ephemeralKeys: Stripe.resources.EphemeralKeys;
     usageRecords: Stripe.resources.UsageRecords;
     usageRecordSummaries: Stripe.resources.UsageRecordSummaries;
@@ -3677,6 +3679,157 @@ declare namespace Stripe {
     }
 
     namespace events {
+        type EventType =
+            | "*"
+            | "account.updated"
+            | "account.application.authorized"
+            | "account.application.deauthorized"
+            | "account.external_account.created"
+            | "account.external_account.deleted"
+            | "account.external_account.updated"
+            | "application_fee.created"
+            | "application_fee.refunded"
+            | "application_fee.refund.updated"
+            | "balance.available"
+            | "capability.updated"
+            | "charge.captured"
+            | "charge.expired"
+            | "charge.failed"
+            | "charge.pending"
+            | "charge.refunded"
+            | "charge.succeeded"
+            | "charge.updated"
+            | "charge.dispute.closed"
+            | "charge.dispute.created"
+            | "charge.dispute.funds_reinstated"
+            | "charge.dispute.funds_withdrawn"
+            | "charge.dispute.updated"
+            | "charge.refund.updated"
+            | "checkout.session.completed"
+            | "coupon.created"
+            | "coupon.deleted"
+            | "coupon.updated"
+            | "credit_note.created"
+            | "credit_note.updated"
+            | "credit_note.voided"
+            | "customer.created"
+            | "customer.deleted"
+            | "customer.updated"
+            | "customer.discount.created"
+            | "customer.discount.deleted"
+            | "customer.discount.updated"
+            | "customer.source.created"
+            | "customer.source.deleted"
+            | "customer.source.expiring"
+            | "customer.source.updated"
+            | "customer.subscription.created"
+            | "customer.subscription.deleted"
+            | "customer.subscription.trial_will_end"
+            | "customer.subscription.updated"
+            | "customer.tax_id.created"
+            | "customer.tax_id.deleted"
+            | "customer.tax_id.updated"
+            | "file.created"
+            | "invoice.created"
+            | "invoice.deleted"
+            | "invoice.finalized"
+            | "invoice.marked_uncollectible"
+            | "invoice.payment_action_required"
+            | "invoice.payment_failed"
+            | "invoice.payment_succeeded"
+            | "invoice.sent"
+            | "invoice.upcoming"
+            | "invoice.updated"
+            | "invoice.voided"
+            | "invoiceitem.created"
+            | "invoiceitem.deleted"
+            | "invoiceitem.updated"
+            | "issuing_authorization.created"
+            | "issuing_authorization.request"
+            | "issuing_authorization.updated"
+            | "issuing_card.created"
+            | "issuing_card.updated"
+            | "issuing_cardholder.created"
+            | "issuing_cardholder.updated"
+            | "issuing_dispute.created"
+            | "issuing_dispute.updated"
+            | "issuing_settlement.created"
+            | "issuing_settlement.updated"
+            | "issuing_transaction.created"
+            | "issuing_transaction.updated"
+            | "mandate.updated"
+            | "order.created"
+            | "order.payment_failed"
+            | "order.payment_succeeded"
+            | "order.updated"
+            | "order_return.created"
+            | "payment_intent.amount_capturable_updated"
+            | "payment_intent.canceled"
+            | "payment_intent.created"
+            | "payment_intent.payment_failed"
+            | "payment_intent.succeeded"
+            | "payment_method.attached"
+            | "payment_method.card_automatically_updated"
+            | "payment_method.detached"
+            | "payment_method.updated"
+            | "payout.canceled"
+            | "payout.created"
+            | "payout.failed"
+            | "payout.paid"
+            | "payout.updated"
+            | "person.created"
+            | "person.deleted"
+            | "person.updated"
+            | "plan.created"
+            | "plan.deleted"
+            | "plan.updated"
+            | "product.created"
+            | "product.deleted"
+            | "product.updated"
+            | "radar.early_fraud_warning.created"
+            | "radar.early_fraud_warning.updated"
+            | "recipient.created"
+            | "recipient.deleted"
+            | "recipient.updated"
+            | "reporting.report_run.failed"
+            | "reporting.report_run.succeeded"
+            | "reporting.report_type.updated"
+            | "review.closed"
+            | "review.opened"
+            | "setup_intent.canceled"
+            | "setup_intent.created"
+            | "setup_intent.setup_failed"
+            | "setup_intent.succeeded"
+            | "sigma.scheduled_query_run.created"
+            | "sku.created"
+            | "sku.deleted"
+            | "sku.updated"
+            | "source.canceled"
+            | "source.chargeable"
+            | "source.failed"
+            | "source.mandate_notification"
+            | "source.refund_attributes_required"
+            | "source.transaction.created"
+            | "source.transaction.updated"
+            | "subscription_schedule.aborted"
+            | "subscription_schedule.canceled"
+            | "subscription_schedule.completed"
+            | "subscription_schedule.created"
+            | "subscription_schedule.expiring"
+            | "subscription_schedule.released"
+            | "subscription_schedule.updated"
+            | "tax_rate.created"
+            | "tax_rate.updated"
+            | "topup.created"
+            | "topup.failed"
+            | "topup.reversed"
+            | "topup.succeeded"
+            | "transfer.created"
+            | "transfer.failed"
+            | "transfer.paid"
+            | "transfer.reversed"
+            | "transfer.updated";
+
         interface IEvent extends IResourceObject {
             /**
              * Value is "event"
@@ -9469,6 +9622,45 @@ declare namespace Stripe {
         }
     }
 
+    namespace webhookEndpoints {
+        interface IWebhookCreateOptions {
+            url: string;
+            enabled_events: string[];
+            api_version?: string;
+            connect?: boolean;
+        }
+
+        interface IWebhookEndpoint extends IObject {
+            /**
+             * Value is 'webhook_endpoint'
+             */
+            object: string;
+            id: string;
+            api_version: string;
+            application: string | null;
+            created: number;
+            enabled_events: events.EventType[];
+            livemode: boolean;
+            status: "enabled" | "disabled";
+            url: string;
+            secret?: string;
+            metadata?: object;
+        }
+
+        interface IWebhookUpdateOptions {
+            disabled?: boolean;
+            enabled_events?: events.EventType[];
+            url?: string;
+            metadata?: any;
+        }
+
+        interface IWebhookListOptions {
+            ending_before?: string;
+            limit?: number;
+            starting_after?: string;
+        }
+    }
+
     // tslint:disable-next-line:no-unnecessary-class
     class StripeResource {
         constructor(stripe: Stripe, urlData: any);
@@ -13893,6 +14085,66 @@ declare namespace Stripe {
              * Generates a header to be used for webhook mocking
              */
             generateTestHeaderString(options: IWebHookGenerateTestHeaderStringOptions): string;
+        }
+
+        class WebhookEndpoints {
+            /**
+             * Creates a new Webhook Endpoint
+             */
+            create(
+                data: webhookEndpoints.IWebhookCreateOptions,
+                options: HeaderOptions,
+                response?: IResponseFn<webhookEndpoints.IWebhookEndpoint>,
+            ): Promise<webhookEndpoints.IWebhookEndpoint>;
+            create(data: webhookEndpoints.IWebhookCreateOptions, response?: IResponseFn<webhookEndpoints.IWebhookEndpoint>): Promise<webhookEndpoints.IWebhookEndpoint>;
+
+            /**
+             * Retrieves the webhook endpoint with the given ID
+             */
+            retrieve(
+                webhookId: string,
+                data: IDataOptions,
+                options: HeaderOptions,
+                response?: IResponseFn<webhookEndpoints.IWebhookEndpoint>,
+            ): Promise<webhookEndpoints.IWebhookEndpoint>;
+            retrieve(webhookId: string, data: IDataOptions, response?: IResponseFn<webhookEndpoints.IWebhookEndpoint>): Promise<webhookEndpoints.IWebhookEndpoint>;
+            retrieve(webhookId: string, options: HeaderOptions, response?: IResponseFn<webhookEndpoints.IWebhookEndpoint>): Promise<webhookEndpoints.IWebhookEndpoint>;
+            retrieve(webhookId: string, response?: IResponseFn<webhookEndpoints.IWebhookEndpoint>): Promise<webhookEndpoints.IWebhookEndpoint>;
+
+            /**
+             * Updates the webhook endpoint
+             *
+             * You may edit the url, the list of enabled_events, and the status of your endpoint
+             */
+            update(
+                webhookId: string,
+                data: webhookEndpoints.IWebhookUpdateOptions,
+                options: HeaderOptions,
+                response?: IResponseFn<webhookEndpoints.IWebhookEndpoint>,
+            ): Promise<webhookEndpoints.IWebhookEndpoint>;
+            update(webhookId: string, data: webhookEndpoints.IWebhookUpdateOptions, response?: IResponseFn<webhookEndpoints.IWebhookEndpoint>): Promise<webhookEndpoints.IWebhookEndpoint>;
+
+            /**
+             * Returns a list of your webhook endpoints
+             */
+            list(
+                data: webhookEndpoints.IWebhookListOptions,
+                options: HeaderOptions,
+                response?: IResponseFn<IList<webhookEndpoints.IWebhookEndpoint>>,
+            ): IListPromise<webhookEndpoints.IWebhookEndpoint>;
+            list(data: webhookEndpoints.IWebhookListOptions, response?: IResponseFn<IList<webhookEndpoints.IWebhookEndpoint>>): IListPromise<webhookEndpoints.IWebhookEndpoint>;
+            list(options: HeaderOptions, response?: IResponseFn<IList<webhookEndpoints.IWebhookEndpoint>>): IListPromise<webhookEndpoints.IWebhookEndpoint>;
+            list(response?: IResponseFn<IList<webhookEndpoints.IWebhookEndpoint>>): IListPromise<webhookEndpoints.IWebhookEndpoint>;
+
+            /**
+             * You can also delete webhook endpoints via the webhook endpoint management page of the Stripe dashboard
+             */
+            del(
+                webhookId: string,
+                options: HeaderOptions,
+                response?: IResponseFn<IDeleteConfirmation>,
+            ): Promise<IDeleteConfirmation>;
+            del(webhookId: string, response?: IResponseFn<IDeleteConfirmation>): Promise<IDeleteConfirmation>;
         }
 
         interface IWebHookGenerateTestHeaderStringOptions {
