842e91e454e437350ceef3e7d04082c887df8e7c
axelfran <axelnaess@gmail.com>
2019-11-13 01:54:38 +0100

    [@types/stripe] Added "transfer_group" as property on stripe.ITransfer (#40278)
    * Add transfer_group to type definitions for Stripe update charge. Documentation: https://stripe.com/docs/api/charges/update
    
    * Added "transfer_group" as property on stripe.ITransfer
    
    * 'transfer_group' on ITransfer can be 'null'

--- types/stripe/index.d.ts
+++ types/stripe/index.d.ts
@@ -7677,7 +7677,12 @@ declare namespace Stripe {
             status: Statuses;
 
             /**
-             * Can be card, bank_account, or stripe_account.
+             * A string that identifies this transaction as part of a group.
+             */
+            transfer_group: string | null;
+
+            /**
+             *  Can be card, bank_account, or stripe_account.
              */
             type: 'card' | 'bank_account' | 'stripe_account';
         }
