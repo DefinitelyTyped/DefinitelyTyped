55b943acd1341b735cc5fd9cee3bdff90b07624f
Akhil Stanislavose <akhil.stanislavose@gmail.com>
2019-11-13 16:59:13 -0800

    Update version to 1.0.0-alpha
    Seems like the interface haven't changed at all with the bump to 1.0.0-alpha

--- types/react-linkify/index.d.ts
+++ types/react-linkify/index.d.ts
@@ -1,4 +1,4 @@
-// Type definitions for react-linkify 0.2
+// Type definitions for react-linkify 1.0.0-alpha
 // Project: http://tasti.github.io/react-linkify/
 // Definitions by: Michael James <https://github.com/majames>
 //                 Jacky Wang <https://github.com/jackywang529>
