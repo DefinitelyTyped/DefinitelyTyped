3124a7febc06362279de31c987f1f693895fe24b
geekley <38332912+geekley@users.noreply.github.com>
2019-11-14 20:58:28 -0300

    connect: improve API, add req.originalUrl

--- types/connect/index.d.ts
+++ types/connect/index.d.ts
@@ -2,28 +2,45 @@
 // Project: https://github.com/senchalabs/connect
 // Definitions by: Maxime LUCE <https://github.com/SomaticIT>
 //                 Evan Hahn <https://github.com/EvanHahn>
+//                 geekley <https://github.com/geekley>
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
 
 /// <reference types="node" />
 
 
 import * as http from "http";
+import connect = createServer;
 
 /**
  * Create a new connect server.
  * @public
  */
-declare function createServer(): createServer.Server;
+declare function createServer(): connect.Server;
 
 declare namespace createServer {
+    export interface Request extends http.IncomingMessage {
+        /**
+         * The request URL, altered to remove the route part. For example, if the handle function is used
+         * at the route `/foo`, the request for `/foo/bar` will invoke it with `req.url === '/bar'` and
+         * `req.originalUrl === '/foo/bar'`.
+         */
+        url: string;
+        /**
+         * The original URL of the request, unaltered.
+         */
+        originalUrl: string;
+    }
+
     export type ServerHandle = HandleFunction | http.Server;
 
     type NextFunction = (err?: any) => void;
 
-    export type SimpleHandleFunction = (req: http.IncomingMessage, res: http.ServerResponse) => void;
-    export type NextHandleFunction = (req: http.IncomingMessage, res: http.ServerResponse, next: NextFunction) => void;
-    export type ErrorHandleFunction = (err: any, req: http.IncomingMessage, res: http.ServerResponse, next: NextFunction) => void;
-    export type HandleFunction = SimpleHandleFunction | NextHandleFunction | ErrorHandleFunction;
+    /** @deprecated `next` is always available to the handler */ export type SimpleHandleFunction = NextHandleFunction;
+    export type NextHandleFunction =
+        (req: Request, res: http.ServerResponse, next: NextFunction) => void;
+    export type ErrorHandleFunction =
+        (err: any, req: Request, res: http.ServerResponse, next: NextFunction) => void;
+    export type HandleFunction = NextHandleFunction | ErrorHandleFunction;
 
     export interface ServerStackItem {
         route: string;
@@ -31,7 +48,7 @@ declare namespace createServer {
     }
 
     export interface Server extends NodeJS.EventEmitter {
-        (req: http.IncomingMessage, res: http.ServerResponse, next?: Function): void;
+        (req: http.IncomingMessage, res: http.ServerResponse, next?: NextFunction): void;
 
         route: string;
         stack: ServerStackItem[];
@@ -49,8 +66,10 @@ declare namespace createServer {
         *
         * @public
         */
-        use(fn: HandleFunction): Server;
-        use(route: string, fn: HandleFunction): Server;
+        use(fn: (req: Request, res: http.ServerResponse, next: NextFunction) => void): Server;
+        use(fn: (err: any, req: Request, res: http.ServerResponse, next: NextFunction) => void): Server;
+        use(route: string, fn: (req: Request, res: http.ServerResponse, next: NextFunction) => void): Server;
+        use(route: string, fn: (err: any, req: Request, res: http.ServerResponse, next: NextFunction) => void): Server;
 
         /**
         * Handle server requests, punting them down
@@ -58,7 +77,7 @@ declare namespace createServer {
         *
         * @private
         */
-        handle(req: http.IncomingMessage, res: http.ServerResponse, next: Function): void;
+        handle(req: http.IncomingMessage, res: http.ServerResponse, out?: NextFunction): void;
 
         /**
         * Listen for connections.
@@ -84,11 +103,11 @@ declare namespace createServer {
         *
         * @api public
         */
-        listen(port: number, hostname?: string, backlog?: number, callback?: Function): http.Server;
-        listen(port: number, hostname?: string, callback?: Function): http.Server;
-        listen(path: string, callback?: Function): http.Server;
-        listen(handle: any, listeningListener?: Function): http.Server;
+        listen(port: number, hostname?: string, backlog?: number, listeningListener?: () => void): http.Server;
+        listen(port: number, hostname?: string, listeningListener?: () => void): http.Server;
+        listen(path: string, listeningListener?: () => void): http.Server;
+        listen(handle: any, listeningListener?: () => void): http.Server;
     }
 }
 
-export = createServer;
+export = connect;
