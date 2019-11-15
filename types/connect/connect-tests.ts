c35a402b0917da4d6f4b11f8f54a1df29531fec8
geekley <38332912+geekley@users.noreply.github.com>
2019-11-15 01:41:49 -0300

    connect: API docs from README, tests

--- types/connect/connect-tests.ts
+++ types/connect/connect-tests.ts
@@ -1,45 +1,62 @@
+// npm i --no-save connect
+// tsc --noEmit false && node connect-tests.js
+
 import * as http from "http";
 import connect = require("connect");
 
 const app = connect();
 
 // log all requests
-app.use((req: http.IncomingMessage, res: http.ServerResponse, next: connect.NextFunction) => {
-    console.log(req, res);
+function logReq(req: connect.Request, res: http.ServerResponse, next: connect.NextFunction) {
+    console.log(`${req.method} ${req.originalUrl}`);
     next();
-});
-
-// "Throw" an Error
-app.use((req: http.IncomingMessage, res: http.ServerResponse, next: connect.NextFunction) => {
-    next(new Error("Something went wrong!"));
-});
-
-// "Throw" a number
-app.use((req: http.IncomingMessage, res: http.ServerResponse, next: connect.NextFunction) => {
-    next(404);
-});
+}
 
 // Stop on errors
-app.use((err: any, req: http.IncomingMessage, res: http.ServerResponse, next: connect.NextFunction) => {
+function stopOnErr(err: any, req: connect.Request, res: http.ServerResponse, next: connect.NextFunction) {
     if (err) {
-        return res.end(`Error: ${err}`);
+        res.statusCode = +err || 500;
+        return res.end(`ERROR at ${req.url}: ${err}`);
     }
-
     next();
+}
+
+app.use(logReq);
+
+// "Throw" a number
+app.use("/403", (req: connect.Request, res: http.ServerResponse, next: connect.NextFunction) => {
+    next(403);
 });
 
 // Use legacy `Function` for `next` parameter.
-app.use((req: http.IncomingMessage, res: http.ServerResponse, next: Function) => {
+app.use((req: connect.Request, res: http.ServerResponse, next: Function) => {
     next();
 });
 
 // respond to all requests
-app.use((req: http.IncomingMessage, res: http.ServerResponse) => {
+app.use((req: connect.Request, res: http.ServerResponse) => {
     res.end("Hello from Connect!\n");
 });
 
+app.use(stopOnErr);
+
 //create node.js http server and listen on port
 http.createServer(app).listen(3000);
+console.log("http://localhost:3000/");
+
+
+const app2 = connect();
+
+app2.use(logReq);
+
+// "Throw" an Error
+app2.use("/err", (req: http.IncomingMessage, res: http.ServerResponse, next: connect.NextFunction) => {
+    next(new Error("Something went wrong!"));
+});
+
+// Catch only on specific route
+app2.use("/err/catch", stopOnErr);
 
 //create node.js http server and listen on port using connect shortcut
-app.listen(3000);
+app2.listen(3001);
+console.log("http://localhost:3001/");
--- types/connect/index.d.ts
+++ types/connect/index.d.ts
@@ -12,8 +12,21 @@ import * as http from "http";
 import connect = createServer;
 
 /**
- * Create a new connect server.
- * @public
+ * When the `connect` module is required, a function is returned that will construct a new app when called.
+ * This app will store all the middleware added and is, itself, a function.
+ *
+ * The core of Connect is "using" middleware. Middleware are added as a "stack" where incoming
+ * requests will execute each middleware one-by-one until a middleware does not call `next()` within it.
+ *
+ * There are special cases of "error-handling" middleware. These are middleware where the
+ * function takes exactly 4 arguments. When a middleware passes an error to `next`, the app will
+ * proceed to look for the error middleware that was declared after that middleware and invoke it,
+ * skipping any error middleware above that middleware and any non-error middleware below.
+ *
+ * The last step is to actually use the Connect app in a server. The `.listen()` method is a
+ * convenience to start a HTTP server (and is identical to the `http.Server`'s `listen` method).
+ * The app itself is really just a function with three arguments, so it can also be handed to
+ * `.createServer()` in Node.js.
  */
 declare function createServer(): connect.Server;
 
@@ -33,7 +46,9 @@ declare namespace createServer {
 
     export type ServerHandle = HandleFunction | http.Server;
 
-    type NextFunction = (err?: any) => void;
+    type NextFunction = (
+        /** An optional error, to make the app skip to error-handling middleware. */ err?: any
+    ) => void;
 
     /** @deprecated `next` is always available to the handler */ export type SimpleHandleFunction = NextHandleFunction;
     export type NextHandleFunction =
@@ -48,61 +63,92 @@ declare namespace createServer {
     }
 
     export interface Server extends NodeJS.EventEmitter {
+        /**
+         * The `app` itself is a function. This is just an alias to `app.handle`.
+         */
         (req: http.IncomingMessage, res: http.ServerResponse, next?: NextFunction): void;
 
         route: string;
         stack: ServerStackItem[];
 
         /**
-        * Utilize the given middleware `handle` to the given `route`,
-        * defaulting to _/_. This "route" is the mount-point for the
-        * middleware, when given a value other than _/_ the middleware
-        * is only effective when that segment is present in the request's
-        * pathname.
-        *
-        * For example if we were to mount a function at _/admin_, it would
-        * be invoked on _/admin_, and _/admin/settings_, however it would
-        * not be invoked for _/_, or _/posts_.
-        *
-        * @public
-        */
-        use(fn: (req: Request, res: http.ServerResponse, next: NextFunction) => void): Server;
-        use(fn: (err: any, req: Request, res: http.ServerResponse, next: NextFunction) => void): Server;
-        use(route: string, fn: (req: Request, res: http.ServerResponse, next: NextFunction) => void): Server;
-        use(route: string, fn: (err: any, req: Request, res: http.ServerResponse, next: NextFunction) => void): Server;
+         * Use a function on the app, where the function represents a middleware. The function will be
+         * invoked for every request in the order that `app.use` is called.
+         * @param fn A plain function, a Node.js HTTP server instance or another Connect app instance.
+         */
+        use(fn: (
+            /** The Node.js http request object. */ req: Request,
+            /** The Node.js http response object. */ res: http.ServerResponse,
+            /** A function to call to invoke the next middleware. */ next: NextFunction
+        ) => void): Server;
 
         /**
-        * Handle server requests, punting them down
-        * the middleware stack.
-        *
-        * @private
-        */
+         * Use a function on the app, where the function represents a middleware. The function will be
+         * invoked for every request in which the URL (`req.url` property) starts with the given `route`
+         * string in the order that `app.use` is called.
+         * @param route An optional path string that is matched against the beginning of the incoming request URL.
+         * This allows for basic routing.
+         *
+         * The route is always terminated at a path separator (`/`) or a dot (`.`) character. This means the
+         * given routes `/foo/` and `/foo` are the same and both will match requests with the URLs
+         * `/foo`, `/foo/`, `/foo/bar`, and `/foo.bar`, but not match a request with the URL `/foobar`.
+         *
+         * The route is matched in a case-insensitive manner.
+         * @param fn A plain function, a Node.js HTTP server instance or another Connect app instance.
+         */
+        use(route: string, fn: (
+            /** The Node.js http request object. */ req: Request,
+            /** The Node.js http response object. */ res: http.ServerResponse,
+            /** A function to call to invoke the next middleware. */ next: NextFunction
+        ) => void): Server;
+
+        /**
+         * Register an "error-handling" middleware. These are middleware where the
+         * function takes exactly 4 arguments. When a middleware passes an error to `next`, the app will
+         * proceed to look for the error middleware that was declared after that middleware and invoke it,
+         * skipping any error middleware above that middleware and any non-error middleware below.
+         * @param fn A plain function, a Node.js HTTP server instance or another Connect app instance.
+         */
+        use(fn: (
+            /** The error that occurred. */ err: any,
+            /** The Node.js http request object. */ req: Request,
+            /** The Node.js http response object. */ res: http.ServerResponse,
+            /** A function to call to invoke the next middleware. */ next: NextFunction
+        ) => void): Server;
+
+        /**
+         * Mount an "error-handling" middleware on the specified route.
+         * @param route An optional path string that is matched against the beginning of the incoming request URL.
+         * This allows for basic routing.
+         *
+         * The route is always terminated at a path separator (`/`) or a dot (`.`) character. This means the
+         * given routes `/foo/` and `/foo` are the same and both will match requests with the URLs
+         * `/foo`, `/foo/`, `/foo/bar`, and `/foo.bar`, but not match a request with the URL `/foobar`.
+         *
+         * The route is matched in a case-insensitive manner.
+         * @param fn A plain function, a Node.js HTTP server instance or another Connect app instance.
+         */
+        use(route: string, fn: (
+            /** The error that occurred. */ err: any,
+            /** The Node.js http request object. */ req: Request,
+            /** The Node.js http response object. */ res: http.ServerResponse,
+            /** A function to call to invoke the next middleware. */ next: NextFunction
+        ) => void): Server;
+
+        /**
+         * Calling the function will run the middleware stack against the given Node.js http request (`req`)
+         * and response (`res`) objects. An optional function `out` can be provided that will be called if the
+         * request (or error) was not handled by the middleware stack.
+         */
         handle(req: http.IncomingMessage, res: http.ServerResponse, out?: NextFunction): void;
 
         /**
-        * Listen for connections.
-        *
-        * This method takes the same arguments
-        * as node's `http.Server#listen()`.
-        *
-        * HTTP and HTTPS:
-        *
-        * If you run your application both as HTTP
-        * and HTTPS you may wrap them individually,
-        * since your Connect "server" is really just
-        * a JavaScript `Function`.
-        *
-        *      var connect = require('connect')
-        *        , http = require('http')
-        *        , https = require('https');
-        *
-        *      var app = connect();
-        *
-        *      http.createServer(app).listen(80);
-        *      https.createServer(options, app).listen(443);
-        *
-        * @api public
-        */
+         * Start the app listening for requests. This method will internally create a Node.js HTTP server and
+         * call `.listen()` on it.
+         *
+         * This is an alias to the `server.listen()` method in the version of Node.js running, so consult
+         * the Node.js documentation for all the different variations. The most common signature is `app.listen(port)`.
+         */
         listen(port: number, hostname?: string, backlog?: number, listeningListener?: () => void): http.Server;
         listen(port: number, hostname?: string, listeningListener?: () => void): http.Server;
         listen(path: string, listeningListener?: () => void): http.Server;
