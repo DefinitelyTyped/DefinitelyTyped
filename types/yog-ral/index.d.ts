cadfd0efd1e9180f15049c00c331b88deb56e7e8
ssddi456 <torrent88410@gmail.com>
2019-11-16 00:25:41 +0800

    Update index.d.ts

--- types/yog-ral/index.d.ts
+++ types/yog-ral/index.d.ts
@@ -1,4 +1,4 @@
-// Type definitions for yog-ral 0.20.0
+// Type definitions for yog-ral 0.20
 // Project: https://gitlab.baidu.com/fex/node-ral
 // Definitions by: ssddi456 <https://github.com/ssddi456>
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
