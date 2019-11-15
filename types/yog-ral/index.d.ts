a9212edcf4cc2d656f6d3544e696fbd21bebb55c
ssddi456 <torrent88410@gmail.com>
2019-11-16 00:20:17 +0800

    Update index.d.ts

--- types/yog-ral/index.d.ts
+++ types/yog-ral/index.d.ts
@@ -1,4 +1,5 @@
 // Type definitions for yog-ral 0.20.0
+// Project: https://gitlab.baidu.com/fex/node-ral
 // Definitions by: ssddi456 <https://github.com/ssddi456>
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
 // TypeScript Version: 3.7
