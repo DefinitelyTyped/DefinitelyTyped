2f0da43a893716e89d3b96fecafeae001231ba81
Owen Hall <owen.hall@red-gate.com>
2019-11-15 14:37:48 +0000

    underscore - Be more explicit that sortBy returns an array.
    That's its implementation behaviour, even if not provided with an array to begin with.

--- types/underscore/index.d.ts
+++ types/underscore/index.d.ts
@@ -5306,13 +5306,13 @@ declare module _ {
         * Wrapped type `any[]`.
         * @see _.sortBy
         **/
-        sortBy(iterator?: _.ListIterator<T, any>, context?: any): _Chain<T, V>;
+        sortBy<TSort>(iterator?: _.ListIterator<T, TSort>, context?: any): _Chain<T, T[]>;
 
         /**
         * Wrapped type `any[]`.
         * @see _.sortBy
         **/
-        sortBy(iterator: string, context?: any): _Chain<T, V>;
+        sortBy(iterator: string, context?: any): _Chain<T, T[]>;
 
         /**
         * Wrapped type `any[]`.
