9e2fcf88fd2aba30b2075f2d0a161f6a092f1626
Forzoom <forzoom@163.com>
2019-11-16 20:26:29 +0800

    add Definitions by

--- types/wx-js-sdk-dt/index.d.ts
+++ types/wx-js-sdk-dt/index.d.ts
@@ -1,6 +1,7 @@
 // Type definitions for non-npm package wx-js-sdk 1.4
 // Project: https://mp.weixin.qq.com/wiki?t=resource/res_main&id=mp1421141115
 // Definitions by: Bian Zhongjie <https://github.com/agasbzj>
+//                 Ming Huang <https://github.com/forzoom>
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
 
 declare namespace wx {
