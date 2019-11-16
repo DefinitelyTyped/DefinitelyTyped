0b20255f2bee546803c8854ad1f8c02103e97483
Max Boguslavskiy <max.bogus@gmail.com>
2019-11-16 15:21:23 +0300

    reworked ffmpeg

--- types/ffmpeg.js/index.d.ts
+++ types/ffmpeg.js/index.d.ts
@@ -5,58 +5,62 @@
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
 // TypeScript Version: 3.0
 
-export interface Options {
-    arguments: string[];
-    MEMFS?: Video[];
-    print?(data: any): void;
-    printErr?(data: any): void;
-    onExit?(code: unknown): void;
-    stdin?(data: any): void;
-    mounts?: Mount[];
-    TOTAL_MEMORY?: number;
-}
-
-export interface Opts {
-    root: string;
-}
-
-export interface Mount {
-    type: string;
-    opts: Opts;
-    mountpoint: string;
-}
-
-export interface Result {
-    MEMFS: Video[];
-}
-
-export interface Video {
-    data: Uint8Array;
-    name: string;
-}
-
-export namespace Worker {
-    interface Data {
-        type: string;
-        data: string;
-    }
-
-    interface PostMessageOptions {
-        type: string;
+declare namespace ffmpeg {
+    interface Options {
         arguments: string[];
+        MEMFS?: Video[];
+        print?(data: any): void;
+        printErr?(data: any): void;
+        onExit?(code: unknown): void;
+        stdin?(data: any): void;
+        mounts?: Mount[];
+        TOTAL_MEMORY?: number;
     }
-
-    interface OnMessageOptions {
-        data: Data;
+    
+    interface Opts {
+        root: string;
+    }
+    
+    interface Mount {
+        type: string;
+        opts: Opts;
+        mountpoint: string;
+    }
+    
+    interface Result {
+        MEMFS: Video[];
+    }
+    
+    interface Video {
+        data: Uint8Array;
+        name: string;
+    }
+    
+    namespace Worker {
+        interface Data {
+            type: string;
+            data: string;
+        }
+    
+        interface PostMessageOptions {
+            type: string;
+            arguments: string[];
+        }
+    
+        interface OnMessageOptions {
+            data: Data;
+        }
+    }
+    
+    function ffmpeg(opts: Options): Result;
+    
+    class Worker {
+        constructor(someParam?: string);
+    
+        onmessage(opts: Worker.OnMessageOptions): void;
+        postMessage(opts: Worker.PostMessageOptions): void;
+        terminate(): void;
     }
 }
 
-export function ffmpeg(opts: Options): Result;
-
-export class Worker {
-    constructor(someParam?: string);
-
-    onmessage(opts: Worker.OnMessageOptions): void;
-    postMessage(opts: Worker.PostMessageOptions): void;
-    terminate(): void;
-}
+export = ffmpeg;
