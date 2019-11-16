3f2372fc8ea2626bbbe6c013b7c8f2676c5a8edf
ExE Boss <3889017+ExE-Boss@users.noreply.github.com>
2019-11-16 01:45:00 +0100

    fix(es‑abstract/helpers): `forEach` can also iterate over objects

--- types/es-abstract/helpers/forEach.d.ts
+++ types/es-abstract/helpers/forEach.d.ts
@@ -1,2 +1,12 @@
-declare function forEach<T>(array: T[], callback: (value: T, index: number, array: T[]) => void): void;
+declare function forEach<A extends ArrayLike<unknown>, THIS_TYPE = undefined>(
+	array: A,
+	callback: (this: THIS_TYPE, value: A extends ArrayLike<infer T> ? T : unknown, index: number, array: A) => void,
+	thisArg?: THIS_TYPE,
+): void;
+declare function forEach<O extends object, THIS_TYPE = undefined>(
+	obj: O,
+	callback: (this: THIS_TYPE, value: O[keyof O], key: string, obj: O) => void,
+	thisArg?: THIS_TYPE,
+): void;
+
 export = forEach;
