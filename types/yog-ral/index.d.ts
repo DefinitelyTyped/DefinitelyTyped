90158f7559be09742bdfa0a3942e6701df7d0542
ssddi456 <torrent88410@gmail.com>
2019-11-18 11:05:11 +0800

    Update index.d.ts

--- types/yog-ral/index.d.ts
+++ types/yog-ral/index.d.ts
@@ -2,6 +2,6 @@
 // Project: https://gitlab.baidu.com/fex/node-ral
 // Definitions by: ssddi456 <https://github.com/ssddi456>
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
-// TypeScript Version: 3.7
+// TypeScript Version: 2.3
 
 export * from 'node-ral';
