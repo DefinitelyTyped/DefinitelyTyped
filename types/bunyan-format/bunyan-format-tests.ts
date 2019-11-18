5cc9d512c6c1d288593e70e068fb29fc5d2ebcc7
Piotr Roszatycki <piotr.roszatycki@gmail.com>
2019-11-18 22:45:41 +0100

    bunyan-format: reverted back to old syntax (#40455)
    * bunyan-format: reverted back to old syntax
    
    * bunyan-format: tslint:disable-next-line

--- types/bunyan-format/bunyan-format-tests.ts
+++ types/bunyan-format/bunyan-format-tests.ts
@@ -1,9 +1,10 @@
 import BunyanFormatWritable = require('bunyan-format');
+import { Writable } from 'stream';
 
-const formatOut = new BunyanFormatWritable({ outputMode: 'short' });
+const formatOut: Writable = new BunyanFormatWritable({ outputMode: 'short' });
 
-const formatOut2 = new BunyanFormatWritable({ outputMode: 'bunyan', levelInString: true });
+const formatOut2: NodeJS.WritableStream = new BunyanFormatWritable({ outputMode: 'bunyan', levelInString: true });
 
-const formatOut3 = BunyanFormatWritable({ outputMode: 'short' });
+const formatOut3: Writable = BunyanFormatWritable({ outputMode: 'short' });
 
-const formatOut4 = BunyanFormatWritable({ outputMode: 'bunyan', levelInString: true });
+const formatOut4: NodeJS.WritableStream = BunyanFormatWritable({ outputMode: 'bunyan', levelInString: true });
