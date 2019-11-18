5e1b17ed5ffb952adc457e6632beb176946d8051
bjoluc <25661029+bjoluc@users.noreply.github.com>
2019-11-18 12:33:03 +0100

    update qrcode.react version in header

--- types/qrcode.react/index.d.ts
+++ types/qrcode.react/index.d.ts
@@ -1,4 +1,4 @@
-// Type definitions for qrcode.react 0.9
+// Type definitions for qrcode.react 1.0.0
 // Project: https://github.com/zpao/qrcode.react, http://zpao.github.io/qrcode.react
 // Definitions by: Mleko <https://github.com/mleko>,
 //                 Yonas <https://github.com/yonasadiel>,
