e601b533ed8115faa76d6b68bdc66d6dc269a296
Manuel <github@mrspoocy.com>
2019-11-19 15:09:23 +0100

    add test for importType and modulesFromFile

--- types/webpack-node-externals/webpack-node-externals-tests.ts
+++ types/webpack-node-externals/webpack-node-externals-tests.ts
@@ -19,3 +19,53 @@ const c: webpack.Configuration = {
         })
     ]
 };
+const d: webpack.Configuration = {
+    entry: 'test.js',
+    externals: [
+        webpackNodeExternals({
+            importType: function (moduleName) {
+                return 'commonjs';
+            }
+        })
+    ]
+};
+const e: webpack.Configuration = {
+    entry: 'test.js',
+    externals: [
+        webpackNodeExternals({
+            modulesFromFile: {
+                exclude: 'devDependencies'
+            }
+        })
+    ]
+}
+const f: webpack.Configuration = {
+    entry: 'test.js',
+    externals: [
+        webpackNodeExternals({
+            modulesFromFile: {
+                exclude: ['devDependencies']
+            }
+        })
+    ]
+}
+const g: webpack.Configuration = {
+    entry: 'test.js',
+    externals: [
+        webpackNodeExternals({
+            modulesFromFile: {
+                include: 'dependencies'
+            }
+        })
+    ]
+}
+const h: webpack.Configuration = {
+    entry: 'test.js',
+    externals: [
+        webpackNodeExternals({
+            modulesFromFile: {
+                include: ['dependencies']
+            }
+        })
+    ]
+}
