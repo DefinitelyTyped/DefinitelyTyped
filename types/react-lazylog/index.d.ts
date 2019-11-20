8e31f2cadd0d3cd37221f7d10a992fad0bb0eb0c
Kun Yan <kyan@redhat.com>
2019-11-20 10:18:49 +0800

    Update index.d.ts

--- types/react-lazylog/index.d.ts
+++ types/react-lazylog/index.d.ts
@@ -1,8 +1,14 @@
-// Type definitions for react-lazylog 3.1
+// Type definitions for react-lazylog 4.4.1
 // Project: https://github.com/mozilla-frontend-infra/react-lazylog
 // Definitions by: Benjamin Romano <https://github.com/benjaminRomano>
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
 // TypeScript Version: 2.8
 
-export * from "./build/LazyLog";
-export * from "./build/ScrollFollow";
+export * from './LazyLog';
+export * from './Line';
+export * from './LineContent';
+export * from './LineNumber';
+export * from './LinePart';
+export * from './Loading';
+export * from './ScrollFollow';
+export * from './SearchBar';
