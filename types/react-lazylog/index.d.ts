1e958c052ee1303c078fa5e567a6e10e58a54c2c
Kun Yan <kyan@redhat.com>
2019-11-20 10:20:32 +0800

    Update index.d.ts

--- types/react-lazylog/index.d.ts
+++ types/react-lazylog/index.d.ts
@@ -4,11 +4,11 @@
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
 // TypeScript Version: 2.8
 
-export * from './build/LazyLog';
-export * from './build/Line';
-export * from './build/LineContent';
-export * from './build/LineNumber';
-export * from './build/LinePart';
-export * from './build/Loading';
-export * from './build/ScrollFollow';
-export * from './build/SearchBar';
+export * from "./build/LazyLog";
+export * from "./build/Line";
+export * from "./build/LineContent";
+export * from "./build/LineNumber";
+export * from "./build/LinePart";
+export * from "./build/Loading";
+export * from "./build/ScrollFollow";
+export * from "./build/SearchBar";
