6076aad9039c76173b0e2aea3f19b03e187a8734
Deividi <deividi.cavarzan@gmail.com>
2019-11-21 00:05:43 -0300

    fix lint

--- types/react-native-material-textfield/index.d.ts
+++ types/react-native-material-textfield/index.d.ts
@@ -1,4 +1,4 @@
-// Type definitions for react-native-material-textfield 0.15.1
+// Type definitions for react-native-material-textfield 0.16
 // Project: https://github.com/n4kz/react-native-material-textfield
 // Definitions by: Ville Venäläinen <https://github.com/mindhivefi>
 //                 Kyle Roach <https://github.com/iRoachie>
