e670ba049fec09f8f9731e22a6e96b68c205f6ae
Rifa Achrinza <25147899+achrinza@users.noreply.github.com>
2019-11-22 01:42:16 +0800

    feat(ibm-openapi-validator): added initial typings
    Signed-off-by: Rifa Achrinza <25147899+achrinza@users.noreply.github.com>

---
+++ types/ibm-openapi-validator/index.d.ts
@@ -0,0 +1,28 @@
+// Type definitions for ibm-openapi-validator 0.15
+// Project: https://github.com/IBM/openapi-validator#readme
+// Definitions by: Rifa Achrinza <https://github.com/DefinitelyTyped>
+// Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
+// TypeScript Version: 3.0
+
+export interface validatorResultItem {
+    path: string;
+    message: string;
+}
+
+export interface validatorResult {
+    errors: [] | [ validatorResultItem ];
+    warnings: [] | [ validatorResultItem ];
+}
+
+/**
+ * @default false
+ */
+export type validatorParameterDefaultMode = boolean;
+
+/**
+ * OpenAPI document validator
+ * @param openApiDoc - OpenAPI document object
+ * @param defaultMode - If set to true, the validator will ignore the .validaterc file and will use the [configuration defaults](https://github.com/IBM/openapi-validator#default-values).
+ * @returns Validation results
+ */
+export function validator(openApiDoc: {[property: string]: any}, defaultMode?: validatorParameterDefaultMode): Promise<validatorResult>;
