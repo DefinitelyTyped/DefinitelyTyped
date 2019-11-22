b8781b434fb3adec7c8e51fd6a42a84c7e4f3ded
reflector-six <56890998+reflector-six@users.noreply.github.com>
2019-11-22 11:52:36 -0500

    Increment version number

--- types/react-outside-click-handler/index.d.ts
+++ types/react-outside-click-handler/index.d.ts
@@ -1,4 +1,4 @@
-// Type definitions for react-outside-click-handler 1.2
+// Type definitions for react-outside-click-handler 1.3
 // Project: https://github.com/airbnb/react-outside-click-handler
 // Definitions by: Ivan Zub <https://github.com/zubivan>
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
