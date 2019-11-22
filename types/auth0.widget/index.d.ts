0167b4df50b11a5e3068b21d053f6853feef5038
Nathan Shively-Sanders <293473+sandersn@users.noreply.github.com>
2019-11-21 16:00:05 -0800

    require 2.9 for auth0.widget

--- types/auth0.widget/index.d.ts
+++ types/auth0.widget/index.d.ts
@@ -2,6 +2,7 @@
 // Project: http://auth0.com
 // Definitions by: Robert McLaws <https://github.com/advancedrei>
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
+// TypeScript Version: 2.9
 
 /// <reference types="auth0-js" />
 
