a9aaaf856bd5aa21da2c1163b831044721ec1964
reflector-six <56890998+reflector-six@users.noreply.github.com>
2019-11-22 11:48:04 -0500

    Allow all supported values for display prop

--- types/react-outside-click-handler/index.d.ts
+++ types/react-outside-click-handler/index.d.ts
@@ -9,7 +9,7 @@ import * as React from "react";
 export interface DefaultProps {
     disabled: boolean;
     useCapture: boolean;
-    display: "block" | "flex" | "inline-block";
+    display: "block" | "flex" | "inline" | "inline-block" | "contents";
 }
 
 // TODO: Remove partial once DT support TS 3.0, so skipping mandatory listed in default props won't result in compile error
