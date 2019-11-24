d9db93c0c2b96a27602e6bd450c46b9b61428135
Mike Moore <kgmihael@gmail.com>
2019-11-24 17:07:31 +0200

    add reHex, reHSLa, reRGBa

--- types/fabric/fabric-impl.d.ts
+++ types/fabric/fabric-impl.d.ts
@@ -528,6 +528,27 @@ export class Color {
 	 * Returns new color object, when given color in array representation (ex: [200, 100, 100, 0.5])
 	 */
 	static fromSource(source: number[]): Color;
+	/**
+	 * Regex matching color in HEX format (ex: #FF5544CC, #FF5555, 010155, aff)
+	 * @static
+	 * @field
+	 * @memberOf fabric.Color
+	 */
+	static reHex: RegExp
+	/**
+	 * Regex matching color in HSL or HSLA formats (ex: hsl(200, 80%, 10%), hsla(300, 50%, 80%, 0.5), hsla( 300 , 50% , 80% , 0.5 ))
+	 * @static
+	 * @field
+	 * @memberOf fabric.Color
+	 */
+	static reHSLa: RegExp
+	/**
+	 * Regex matching color in RGB or RGBA formats (ex: rgb(0, 0, 0), rgba(255, 100, 10, 0.5), rgba( 255 , 100 , 10 , 0.5 ), rgb(1,1,1), rgba(100%, 60%, 10%, 0.5))
+	 * @static
+	 * @field
+	 * @memberOf fabric.Color
+	 */
+	static reRGBa: RegExp
 }
 
 interface IGradientOptionsCoords {
