1e4bd518d7098ae0dbbd0d74d325e02805e0ea46
Mike Moore <kgmihael@gmail.com>
2019-11-24 16:48:31 +0200

    OGradientOptions refactor to interface

--- types/fabric/fabric-impl.d.ts
+++ types/fabric/fabric-impl.d.ts
@@ -530,11 +530,19 @@ export class Color {
 	static fromSource(source: number[]): Color;
 }
 
-type IGradientOptionsCoords = {x1?: number, y1?: number, x2?: number, y2?: number, r1?: number, r2?: number};
-type IGradientOptionsColorStops = {
+interface IGradientOptionsCoords {
+	x1?: number;
+	y1?: number;
+	x2?: number;
+	y2?: number;
+	r1?: number;
+	r2?: number;
+}
+
+type IGradientOptionsColorStops = Array<{
 	offset: string,
 	color: string,
-}[];
+}>;
 
 interface IGradientOptions {
 	/**
