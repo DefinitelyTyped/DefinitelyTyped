d4de4b7314f47cb7c44262949fdb4100fdb955a9
Corbin Crutchley <crutchcorn@gmail.com>
2019-11-25 11:29:27 -0800

    Remove un-needed comma

--- types/summernote/index.d.ts
+++ types/summernote/index.d.ts
@@ -1,7 +1,7 @@
 // Type definitions for Summernote 0.8
 // Project: https://github.com/summernote/summernote#readme
 // Definitions by: Wouter Staelens <https://github.com/wstaelens>
-//                 Denny Harijanto <https://github.com/nusantara-cloud>,
+//                 Denny Harijanto <https://github.com/nusantara-cloud>
 //                 Corbin Crutchley <https://github.com/crutchcorn>
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
 // TypeScript Version: 2.7
