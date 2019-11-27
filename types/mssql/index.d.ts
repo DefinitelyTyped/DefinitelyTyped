78f4ee8891c10a6437a6f7655440739b5aba505d
Rifa Achrinza <25147899+achrinza@users.noreply.github.com>
2019-11-27 18:40:55 +0800

    docs(mssql): removed contributor
    Remvoed @JaminFarr as a contributor as per his request
    
    ref: https://github.com/DefinitelyTyped/DefinitelyTyped/pull/40645#discussion_r351201290
    
    Signed-off-by: Rifa Achrinza <25147899+achrinza@users.noreply.github.com>

--- types/mssql/index.d.ts
+++ types/mssql/index.d.ts
@@ -1,7 +1,6 @@
 // Type definitions for mssql 6.0.0
 // Project: https://www.npmjs.com/package/mssql
 // Definitions by: COLSA Corporation <http://www.colsa.com/>
-//                 Ben Farr <https://github.com/jaminfarr>
 //                 Vitor Buzinaro <https://github.com/buzinas>
 //                 Matt Richardson <https://github.com/mrrichar>
 //                 Jørgen Elgaard Larsen <https://github.com/elhaard>
