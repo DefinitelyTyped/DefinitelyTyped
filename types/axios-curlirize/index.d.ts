ec603364e29b6aa99734d740c4c23da6782dbc32
Steve Hankin <steven.hankin@gmail.com>
2019-11-27 23:29:32 +0000

    docs: corrected author url

--- types/axios-curlirize/index.d.ts
+++ types/axios-curlirize/index.d.ts
@@ -1,6 +1,6 @@
 // Type definitions for axios-curlirize 1.3
 // Project: https://github.com/delirius325/axios-curlirize#readme
-// Definitions by: Steven Hankin <https://github.com/me>
+// Definitions by: Steven Hankin <https://github.com/stevenhankin>
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
 
 export = AxiosCurlirize;
