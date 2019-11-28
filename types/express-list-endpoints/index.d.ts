f1b5bf6b39a66a64e4269ab7a7e8cf864bce31b8
sjoseph7 <17250762+sjoseph7@users.noreply.github.com>
2019-11-27 21:50:11 -0800

    fix: update index.d.ts
    I followed in express-less's footsteps

--- types/express-list-endpoints/index.d.ts
+++ types/express-list-endpoints/index.d.ts
@@ -1,13 +1,17 @@
 // Type definitions for express-list-endpoints 4.0
-// Project: https://github.com/AlbertoFdzM/express-list-endpoints
+// Project: express-list-endpoints
 // Definitions by: S Joseph <https://github.com/sjoseph7>
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
 
 import express = require('express');
 
-export function getEndpoints(app: express.Express): Endpoint[];
+declare function getEndpoints(app: express.Express): getEndpoints.Endpoint[];
 
-export interface Endpoint {
-    path: string;
-    methods: string[];
+declare namespace getEndpoints {
+    export interface Endpoint {
+        path: string;
+        methods: string[];
+    }
 }
+
+export = getEndpoints;
