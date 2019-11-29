05c5d502a0c9dd5a00890442680686e9106a33f8
Pierre-Antoine Mills <pierreantoine.urvoy@gmail.com>
2019-11-30 00:39:09 +0200

    chore: replace `Ensure` with `Clean` alias

--- types/ramda/index.d.ts
+++ types/ramda/index.d.ts
@@ -1840,10 +1840,10 @@ declare namespace R {
          *
          * @deprecated since 0.26 in favor of mergeRight
          */
-        merge<O2 extends object>(__: Placeholder, b: O2): <O1 extends object>(a: O1) => O.Ensure<O.MergeUp<O2, O1>>;
-        merge(__: Placeholder): <O1 extends object, O2 extends object>(b: O2, a: O1) => O.Ensure<O.MergeUp<O2, O1>>;
-        merge<O1 extends object, O2 extends object>(a: O1, b: O2): O.Ensure<O.MergeUp<O2, O1>>;
-        merge<O1 extends object>(a: O1): <O2 extends object>(b: O2) => O.Ensure<O.MergeUp<O2, O1>>;
+        merge<O2 extends object>(__: Placeholder, b: O2): <O1 extends object>(a: O1) => O.Clean<O.MergeUp<O2, O1>>;
+        merge(__: Placeholder): <O1 extends object, O2 extends object>(b: O2, a: O1) => O.Clean<O.MergeUp<O2, O1>>;
+        merge<O1 extends object, O2 extends object>(a: O1, b: O2): O.Clean<O.MergeUp<O2, O1>>;
+        merge<O1 extends object>(a: O1): <O2 extends object>(b: O2) => O.Clean<O.MergeUp<O2, O1>>;
 
         /**
          * Merges a list of objects together into one object.
@@ -1851,7 +1851,7 @@ declare namespace R {
         mergeAll<Os extends readonly object[]>(list: Os): O.AssignUp<{}, Os> extends infer M
                                                           ? {} extends M        // nothing merged bcs no `as const`
                                                             ? T.UnionOf<Os>     // so we output the default types
-                                                            : O.Ensure<M & {}>  // otherwise, we can compute `M`
+                                                            : O.Clean<M & {}>  // otherwise, we can compute `M`
                                                           : never;
 
         /**
@@ -1860,8 +1860,8 @@ declare namespace R {
          * and both values are objects, the two values will be recursively merged
          * otherwise the value from the first object will be used.
          */
-        mergeDeepLeft<O1 extends object, O2 extends object>(o1: O1, o2: O2): O.Ensure<O.MergeUp<O1, O2, 'deep'>>;
-        mergeDeepLeft<O1 extends object>(o1: O1): <O2 extends object>(o2: O2) => O.Ensure<O.MergeUp<O1, O2, 'deep'>>;
+        mergeDeepLeft<O1 extends object, O2 extends object>(o1: O1, o2: O2): O.Clean<O.MergeUp<O1, O2, 'deep'>>;
+        mergeDeepLeft<O1 extends object>(o1: O1): <O2 extends object>(o2: O2) => O.Clean<O.MergeUp<O1, O2, 'deep'>>;
 
         /**
          * Creates a new object with the own properties of the first object merged with the own properties of the second object.
@@ -1869,8 +1869,8 @@ declare namespace R {
          * and both values are objects, the two values will be recursively merged
          * otherwise the value from the second object will be used.
          */
-        mergeDeepRight<O1 extends object, O2 extends object>(o1: O1, o2: O2): O.Ensure<O.MergeUp<O2, O1, 'deep'>>;
-        mergeDeepRight<O1 extends object>(a: O1): <O2 extends object>(o2: O2) => O.Ensure<O.MergeUp<O2, O1, 'deep'>>;
+        mergeDeepRight<O1 extends object, O2 extends object>(o1: O1, o2: O2): O.Clean<O.MergeUp<O2, O1, 'deep'>>;
+        mergeDeepRight<O1 extends object>(a: O1): <O2 extends object>(o2: O2) => O.Clean<O.MergeUp<O2, O1, 'deep'>>;
 
         /**
          * Creates a new object with the own properties of the two provided objects. If a key exists in both objects:
@@ -1897,15 +1897,15 @@ declare namespace R {
          * Create a new object with the own properties of the first object merged with the own properties of the second object.
          * If a key exists in both objects, the value from the first object will be used.
          */
-        mergeLeft<O1 extends object, O2 extends object>(a: O1, b: O2): O.Ensure<O.MergeUp<O1, O2>>;
-        mergeLeft<O1 extends object>(a: O1): <O2 extends object>(b: O2) => O.Ensure<O.MergeUp<O1, O2>>;
+        mergeLeft<O1 extends object, O2 extends object>(a: O1, b: O2): O.Clean<O.MergeUp<O1, O2>>;
+        mergeLeft<O1 extends object>(a: O1): <O2 extends object>(b: O2) => O.Clean<O.MergeUp<O1, O2>>;
 
         /**
          * Create a new object with the own properties of the first object merged with the own properties of the second object.
          * If a key exists in both objects, the value from the second object will be used.
          */
-        mergeRight<O1 extends object, O2 extends object>(a: O1, b: O2): O.Ensure<O.MergeUp<O2, O1>>;
-        mergeRight<O1 extends object>(a: O1): <O2 extends object>(b: O2) => O.Ensure<O.MergeUp<O2, O1>>;
+        mergeRight<O1 extends object, O2 extends object>(a: O1, b: O2): O.Clean<O.MergeUp<O2, O1>>;
+        mergeRight<O1 extends object>(a: O1): <O2 extends object>(b: O2) => O.Clean<O.MergeUp<O2, O1>>;
 
         /**
          * Creates a new object with the own properties of the two provided objects. If a key exists in both objects,
