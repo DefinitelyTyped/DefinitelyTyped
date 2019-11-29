cbe5a06d5c8eed0344719636345ade3af0af1919
kubk <ya.7gorbachev2016@yandex.ru>
2019-11-29 23:46:39 +0300

    Fix library version

--- types/wavesurfer.js/index.d.ts
+++ types/wavesurfer.js/index.d.ts
@@ -1,4 +1,4 @@
-// Type definitions for wavesurfer.js 3.2.0
+// Type definitions for wavesurfer.js 3.2
 // Project: https://github.com/katspaugh/wavesurfer.js
 // Definitions by: Yusuke Higuchi <https://github.com/higuri>, Egor Gorbachev <https://github.com/kubk>
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
