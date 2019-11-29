9d0aea4825a7b110035139c9d7aaee9c32c761bf
falsandtru <falsandtru@users.noreply.github.com>
2019-11-29 19:46:46 +0900

    power-assert: format

--- types/power-assert/index.d.ts
+++ types/power-assert/index.d.ts
@@ -13,54 +13,42 @@ import * as empower from "empower";
 export = assert;
 export as namespace assert;
 
-declare function assert(value:any, message?:string):void;
+declare function assert(value: any, message?: string): void;
 declare namespace assert {
     export class AssertionError implements Error {
-        name:string;
-        message:string;
-        actual:any;
-        expected:any;
-        operator:string;
-        generatedMessage:boolean;
+        name: string;
+        message: string;
+        actual: any;
+        expected: any;
+        operator: string;
+        generatedMessage: boolean;
 
-        constructor(options?:{message?: string; actual?: any; expected?: any; operator?: string; stackStartFunction?: Function});
+        constructor(options?: { message?: string; actual?: any; expected?: any; operator?: string; stackStartFunction?: Function });
     }
 
-    export function fail(actual?:any, expected?:any, message?:string, operator?:string):void;
-
-    export function ok(value:any, message?:string):void;
-
-    export function equal(actual:any, expected:any, message?:string):void;
-
-    export function notEqual(actual:any, expected:any, message?:string):void;
-
-    export function deepEqual(actual:any, expected:any, message?:string):void;
-
-    export function notDeepEqual(acutal:any, expected:any, message?:string):void;
-
-    export function strictEqual(actual:any, expected:any, message?:string):void;
-
-    export function notStrictEqual(actual:any, expected:any, message?:string):void;
-
-    export function deepStrictEqual(actual:any, expected:any, message?:string):void;
-
-    export function notDeepStrictEqual(actual:any, expected:any, message?:string):void;
-
-    export var throws:{
-        (block:Function, message?:string): void;
-        (block:Function, error:Function, message?:string): void;
-        (block:Function, error:RegExp, message?:string): void;
-        (block:Function, error:(err:any) => boolean, message?:string): void;
+    export function fail(actual?: any, expected?: any, message?: string, operator?: string): void;
+    export function ok(value: any, message?: string): void;
+    export function equal(actual: any, expected: any, message?: string): void;
+    export function notEqual(actual: any, expected: any, message?: string): void;
+    export function deepEqual(actual: any, expected: any, message?: string): void;
+    export function notDeepEqual(acutal: any, expected: any, message?: string): void;
+    export function strictEqual(actual: any, expected: any, message?: string): void;
+    export function notStrictEqual(actual: any, expected: any, message?: string): void;
+    export function deepStrictEqual(actual: any, expected: any, message?: string): void;
+    export function notDeepStrictEqual(actual: any, expected: any, message?: string): void;
+    export var throws: {
+        (block: Function, message?: string): void;
+        (block: Function, error: Function, message?: string): void;
+        (block: Function, error: RegExp, message?: string): void;
+        (block: Function, error: (err: any) => boolean, message?: string): void;
     };
-
-    export var doesNotThrow:{
-        (block:Function, message?:string): void;
-        (block:Function, error:Function, message?:string): void;
-        (block:Function, error:RegExp, message?:string): void;
-        (block:Function, error:(err:any) => boolean, message?:string): void;
+    export var doesNotThrow: {
+        (block: Function, message?: string): void;
+        (block: Function, error: Function, message?: string): void;
+        (block: Function, error: RegExp, message?: string): void;
+        (block: Function, error: (err: any) => boolean, message?: string): void;
     };
-
-    export function ifError(value:any):void;
+    export function ifError(value: any): void;
 
     export const strict: typeof assert;
 
@@ -69,5 +57,5 @@ declare namespace assert {
         output?: powerAssertFormatter.Options;
     }
 
-    export function customize(options:Options):typeof assert;
+    export function customize(options: Options): typeof assert;
 }
