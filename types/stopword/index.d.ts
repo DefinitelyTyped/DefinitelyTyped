bd04e9f78cc7d3a0e410f5432a3bf90591bd0a57
Rico Sandyca Novenza <ricosandyca.if@gmail.com>
2019-12-01 01:05:32 +0700

    Fix lint's rule

--- types/stopword/index.d.ts
+++ types/stopword/index.d.ts
@@ -3,55 +3,30 @@
 // Definitions by: Rico Sandyca Novenza <https://github.com/ricosandyca>
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
 
-export = stopword;
-
-declare namespace stopword {
+declare namespace Stopword {
     type TStopwords = string[];
 
-    /**
-      * Stopword removal
-      *
-      * @param text Array string of words
-      * @param stopwords Array string of your custom stopwords (default: English stopwords | .en)
-      */
-    function removeStopwords(text: string[], stopwords?: string[]): string;
+     const IStopword: {
+      /**
+       * Stopword removal
+       *
+       * @param text Array string of words
+       * @param stopwords Array string of your custom stopwords (default: English stopwords | .en)
+       */
+      removeStopwords(text: string[], stopwords?: string[]): string;
 
-    /**
-      * Get array of stopwords according by language code
-      * Also can uses for stopwords parameter on removeStopwords
-      */
-    const af: TStopwords;
-    const ar: TStopwords;
-    const bn: TStopwords;
-    const br: TStopwords;
-    const da: TStopwords;
-    const de: TStopwords;
-    const en: TStopwords;
-    const es: TStopwords;
-    const fa: TStopwords;
-    const fr: TStopwords;
-    const fi: TStopwords;
-    const ha: TStopwords;
-    const he: TStopwords;
-    const hi: TStopwords;
-    const id: TStopwords;
-    const it: TStopwords;
-    const ja: TStopwords;
-    const lgg: TStopwords;
-    const lggo: TStopwords;
-    const my: TStopwords;
-    const nl: TStopwords;
-    const no: TStopwords;
-    const pa: TStopwords;
-    const pl: TStopwords;
-    const pt: TStopwords;
-    const ru: TStopwords;
-    const so: TStopwords;
-    const st: TStopwords;
-    const sv: TStopwords;
-    const sw: TStopwords;
-    const vi: TStopwords;
-    const yo: TStopwords;
-    const zh: TStopwords;
-    const zu: TStopwords;
+      /**
+       * Get array of stopwords according by language code
+       * Also can uses for stopwords parameter on removeStopwords
+       */
+       af: TStopwords; ar: TStopwords; bn: TStopwords; br: TStopwords; da: TStopwords;
+       de: TStopwords; en: TStopwords; es: TStopwords; fa: TStopwords; fr: TStopwords;
+       fi: TStopwords; ha: TStopwords; he: TStopwords; hi: TStopwords; id: TStopwords;
+       it: TStopwords; ja: TStopwords; lgg: TStopwords; lggo: TStopwords; my: TStopwords;
+       nl: TStopwords; no: TStopwords; pa: TStopwords; pl: TStopwords; pt: TStopwords;
+       ru: TStopwords; so: TStopwords; st: TStopwords; sv: TStopwords; sw: TStopwords;
+       vi: TStopwords; yo: TStopwords; zh: TStopwords; zu: TStopwords;
+    };
 }
+
+export = Stopword.IStopword;
--- types/stopword/stopword-tests.ts
+++ types/stopword/stopword-tests.ts
@@ -1,5 +1,7 @@
 import stopword = require('stopword');
 
 const oldString = 'you can even roll your own custom stopword list'.split(' ');
-const stopwordLang = stopword.en;
-stopword.removeStopwords(oldString, stopwordLang); // it's working
+const stopwordLangId = stopword.id;
+
+stopword.removeStopwords(oldString, stopwordLangId); // with custom stopwords
+stopword.removeStopwords(oldString); // default stopwords (en)
