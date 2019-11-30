2e03388d707faace2c76c693757d1b46710843e6
Tushar Mathur <tusharmath@gmail.com>
2019-11-30 15:18:54 +0530

    Update index.d.ts

--- types/chai/index.d.ts
+++ types/chai/index.d.ts
@@ -385,7 +385,7 @@ declare namespace Chai {
          * @param expression    Expression to test for truthiness.
          * @param message    Message to display on error.
          */
-        (expression: any, message?: string): void;
+        (expression: any, message?: string): asserts expression;
 
         /**
          * Throws a failure.
