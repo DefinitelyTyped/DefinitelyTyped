6cf2ee38e45d7823d51c5d34f3967d314cc6fc50
Nicholas Bardy <nicholasbardy@gmail.com>
2019-12-01 15:16:12 -0800

    Make map type optional
    Fix complexFilter type to be optional and accept string as well as string arrays.

--- types/fluent-ffmpeg/index.d.ts
+++ types/fluent-ffmpeg/index.d.ts
@@ -1,4 +1,4 @@
-// Type definitions for node-fluent-ffmpeg 2.1
+p// Type definitions for node-fluent-ffmpeg 2.1
 // Project: https://github.com/fluent-ffmpeg/node-fluent-ffmpeg
 // Definitions by: KIM Jaesuck a.k.a. gim tcaesvk <https://github.com/tcaesvk>
 //                 DingWeizhe <https://github.com/DingWeizhe>
@@ -352,8 +352,8 @@ declare namespace Ffmpeg {
         outputOption(...options: string[]): FfmpegCommand;
         outputOptions(options: string[]): FfmpegCommand;
         outputOptions(...options: string[]): FfmpegCommand;
-        filterGraph(spec: string | FilterSpecification | Array<string | FilterSpecification>, map: string[]): FfmpegCommand;
-        complexFilter(spec: string | FilterSpecification | Array<string | FilterSpecification>, map: string[]): FfmpegCommand;
+        filterGraph(spec: string | FilterSpecification | Array<string | FilterSpecification>, map?: string[] | string): FfmpegCommand;
+        complexFilter(spec: string | FilterSpecification | Array<string | FilterSpecification>, map?: string[] | string): FfmpegCommand;
 
         // options/misc
         usingPreset(proset: string | GetPreset): FfmpegCommand;
