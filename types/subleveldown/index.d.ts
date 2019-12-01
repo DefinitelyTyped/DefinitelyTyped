e1df5a790f57ccdd58934a1c9a159fb60e75ab80
Carson Farmer <carson.farmer@gmail.com>
2019-11-30 22:02:22 -0800

    fix: use merging to do default export
    Signed-off-by: Carson Farmer <carson.farmer@gmail.com>

--- types/subleveldown/index.d.ts
+++ types/subleveldown/index.d.ts
@@ -6,24 +6,28 @@
 import { LevelUp } from 'levelup';
 import { ErrorValueCallback, ErrorCallback, AbstractGetOptions } from 'abstract-leveldown';
 
-export interface Options {
-    separator?: string;
-    open?: ((callback?: ErrorCallback) => any);
-    [key: string]: any;
+declare namespace sub {
+    interface Options {
+        separator?: string;
+        open?: ((callback?: ErrorCallback) => any);
+        [key: string]: any;
+    }
+
+    type LevelUpClear<V, O> =
+        ((callback: ErrorValueCallback<V>) => void) &
+        ((options: O, callback: ErrorValueCallback<V>) => void) &
+        ((options?: O) => Promise<V>);
+
+    type InferDBClear<DB> =
+        DB extends { get: (options: infer O, callback: ErrorValueCallback<infer V>) => void } ?
+        LevelUpClear<V, O> :
+        LevelUpClear<any, AbstractGetOptions>;
+
+    interface SublevelDown<DB, Iterator> extends LevelUp<DB, Iterator> {
+        clear: InferDBClear<DB>;
+    }
 }
 
-type LevelUpClear<V, O> =
-    ((callback: ErrorValueCallback<V>) => void) &
-    ((options: O, callback: ErrorValueCallback<V>) => void) &
-    ((options?: O) => Promise<V>);
+declare function sub<DB, Iterator>(db: LevelUp < DB, Iterator >, prefix ?: string, options ?: sub.Options): sub.SublevelDown<DB, Iterator>;
 
-type InferDBClear<DB> =
-    DB extends { get: (options: infer O, callback: ErrorValueCallback<infer V>) => void } ?
-    LevelUpClear<V, O> :
-    LevelUpClear<any, AbstractGetOptions>;
-
-export interface SublevelDown<DB, Iterator> extends LevelUp<DB, Iterator>  {
-    clear: InferDBClear<DB>;
-}
-
-export default function sub<DB, Iterator>(db: LevelUp<DB, Iterator>, prefix?: string, options?: Options): SublevelDown<DB, Iterator>;
+export = sub;
--- types/subleveldown/subleveldown-tests.ts
+++ types/subleveldown/subleveldown-tests.ts
@@ -1,5 +1,5 @@
 import levelup from 'levelup';
-import sub from 'subleveldown';
+import * as sub from 'subleveldown';
 import { AbstractLevelDOWN } from 'abstract-leveldown';
 
 interface StringEncoding {
