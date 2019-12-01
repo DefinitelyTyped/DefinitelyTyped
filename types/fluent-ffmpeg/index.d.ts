13b55f68ffbafaa59670357401a0ea95dfcfda82
Nicholas Bardy <nicholasbardy@gmail.com>
2019-12-01 15:18:54 -0800

    Update index.d.ts

--- types/fluent-ffmpeg/index.d.ts
+++ types/fluent-ffmpeg/index.d.ts
@@ -1,4 +1,4 @@
-p// Type definitions for node-fluent-ffmpeg 2.1
+// Type definitions for node-fluent-ffmpeg 2.1
 // Project: https://github.com/fluent-ffmpeg/node-fluent-ffmpeg
 // Definitions by: KIM Jaesuck a.k.a. gim tcaesvk <https://github.com/tcaesvk>
 //                 DingWeizhe <https://github.com/DingWeizhe>
