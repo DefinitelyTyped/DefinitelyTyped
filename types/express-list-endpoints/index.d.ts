e3a00ede18457202f4e30ec6022c69d5f97dc0fc
sjoseph7 <17250762+sjoseph7@users.noreply.github.com>
2019-12-03 14:14:05 -0800

    docs: update header

--- types/express-list-endpoints/index.d.ts
+++ types/express-list-endpoints/index.d.ts
@@ -1,5 +1,5 @@
 // Type definitions for express-list-endpoints 4.0
-// Project: express-list-endpoints
+// Project: https://github.com/AlbertoFdzM/express-list-endpoints
 // Definitions by: S Joseph <https://github.com/sjoseph7>
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
 
