58769de239200cebd7f42dd8fcf36c43d00c5065
Nathan Brown <nathan@nkbrown.us>
2019-12-03 15:25:51 -0700

    [icepick] apply strict-export-declare-modifiers rule

--- types/icepick/index.d.ts
+++ types/icepick/index.d.ts
@@ -3,14 +3,14 @@
 // Definitions by: Nathan Brown <https://github.com/ngbrown>, Tobias Cohen <https://github.com/tobico>
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
 
-export declare function freeze<T>(collection: T): T;
-export declare function thaw<T>(collection: T): T;
-export declare function assoc<T>(collection: T, key: number | string, value: any): T;
-export declare function dissoc<T>(collection: T, key: number | string): T;
-export declare function dissocIn<T>(collection: T, path: ReadonlyArray<number | string>): T;
-export declare function assocIn<T>(collection: T, path: ReadonlyArray<number | string>, value: any): T;
-export declare function getIn(collection: any, path: ReadonlyArray<number | string>): any;
-export declare function updateIn<T>(
+export function freeze<T>(collection: T): T;
+export function thaw<T>(collection: T): T;
+export function assoc<T>(collection: T, key: number | string, value: any): T;
+export function dissoc<T>(collection: T, key: number | string): T;
+export function dissocIn<T>(collection: T, path: ReadonlyArray<number | string>): T;
+export function assocIn<T>(collection: T, path: ReadonlyArray<number | string>, value: any): T;
+export function getIn(collection: any, path: ReadonlyArray<number | string>): any;
+export function updateIn<T>(
     collection: T,
     path: ReadonlyArray<number | string>,
     callback: (value: any) => any,
@@ -21,27 +21,27 @@ export { assocIn as setIn };
 export { dissoc as unset };
 export { dissocIn as unsetIn };
 
-export declare function assign<T>(target: T): T;
-export declare function assign<T, S1>(target: T, source1: S1): T & S1;
-export declare function assign<T, S1, S2>(target: T, s1: S1, s2: S2): T & S1 & S2;
-export declare function assign<T, S1, S2, S3>(target: T, s1: S1, s2: S2, s3: S3): T & S1 & S2 & S3;
-export declare function assign<T, S1, S2, S3, S4>(target: T, s1: S1, s2: S2, s3: S3, s4: S4): T & S1 & S2 & S3 & S4;
+export function assign<T>(target: T): T;
+export function assign<T, S1>(target: T, source1: S1): T & S1;
+export function assign<T, S1, S2>(target: T, s1: S1, s2: S2): T & S1 & S2;
+export function assign<T, S1, S2, S3>(target: T, s1: S1, s2: S2, s3: S3): T & S1 & S2 & S3;
+export function assign<T, S1, S2, S3, S4>(target: T, s1: S1, s2: S2, s3: S3, s4: S4): T & S1 & S2 & S3 & S4;
 
 export { assign as extend };
 
-export declare function merge<T, S1>(target: T, source: S1): T & S1;
+export function merge<T, S1>(target: T, source: S1): T & S1;
 
-export declare function push<T>(array: ReadonlyArray<T>, element: T): T[];
-export declare function pop<T>(array: ReadonlyArray<T>): T[];
-export declare function shift<T>(array: ReadonlyArray<T>): T[];
-export declare function unshift<T>(array: ReadonlyArray<T>, element: T): T[];
-export declare function reverse<T>(array: ReadonlyArray<T>): T[];
-export declare function sort<T>(array: ReadonlyArray<T>, compareFunction?: (a: T, b: T) => number): T[];
-export declare function splice<T>(array: ReadonlyArray<T>, start: number, deleteCount: number, ...items: T[]): T[];
-export declare function slice<T>(array: ReadonlyArray<T>, begin?: number, end?: number): T[];
+export function push<T>(array: ReadonlyArray<T>, element: T): T[];
+export function pop<T>(array: ReadonlyArray<T>): T[];
+export function shift<T>(array: ReadonlyArray<T>): T[];
+export function unshift<T>(array: ReadonlyArray<T>, element: T): T[];
+export function reverse<T>(array: ReadonlyArray<T>): T[];
+export function sort<T>(array: ReadonlyArray<T>, compareFunction?: (a: T, b: T) => number): T[];
+export function splice<T>(array: ReadonlyArray<T>, start: number, deleteCount: number, ...items: T[]): T[];
+export function slice<T>(array: ReadonlyArray<T>, begin?: number, end?: number): T[];
 
-export declare function map<T, U>(fn: (value: T) => U, array: ReadonlyArray<T>): U[];
-export declare function filter<T>(fn: (value: T) => boolean, array: ReadonlyArray<T>): T[];
+export function map<T, U>(fn: (value: T) => U, array: ReadonlyArray<T>): U[];
+export function filter<T>(fn: (value: T) => boolean, array: ReadonlyArray<T>): T[];
 
 interface IcepickWrapper<T> {
     value(): T;
@@ -76,4 +76,4 @@ interface IcepickWrapper<T> {
     merge<S1>(source: S1): IcepickWrapper<T & S1>;
 }
 
-export declare function chain<T>(target: T): IcepickWrapper<T>;
+export function chain<T>(target: T): IcepickWrapper<T>;
