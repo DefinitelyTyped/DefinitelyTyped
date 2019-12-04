39d12408114001a43469930f4f9f85e7d2393f79
Armando Ortiz <armando@solairo.co.jp>
2019-12-04 17:29:01 +0900

    comment at top

--- types/muvjs/index.d.ts
+++ types/muvjs/index.d.ts
@@ -1,4 +1,5 @@
 // Type definitions for non-npm package MUVjs v2.3.0
+// Project: https://github.com/yoyomo/muvjs
 
 /// <reference path="muv/index.d.ts" />
 /// <reference path="muv-dom/index.d.ts" />
