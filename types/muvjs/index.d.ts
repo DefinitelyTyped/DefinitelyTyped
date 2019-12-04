f002ff81e8da015c9ad3638588219b9dae17f2df
Armando Ortiz <armando@solairo.co.jp>
2019-12-04 17:48:33 +0900

    MAJOR.MINOR

--- types/muvjs/index.d.ts
+++ types/muvjs/index.d.ts
@@ -1,4 +1,4 @@
-// Type definitions for MUVjs v2.3.0
+// Type definitions for MUVjs 2.3
 // Project: https://github.com/yoyomo/muvjs
 // Definitions by: Armando J Ortiz <https://github.com/yoyomo>
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
