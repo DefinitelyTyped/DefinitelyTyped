3c2e21cfc642d99729196cc1d8b3b0c8bbe2c22c
sjoseph520 <17250762+sjoseph7@users.noreply.github.com>
2019-12-03 16:53:35 -0800

    fix: remove patch on type definition version

--- types/express-list-endpoints/index.d.ts
+++ types/express-list-endpoints/index.d.ts
@@ -1,4 +1,4 @@
-// Type definitions for express-list-endpoints 4.0.1
+// Type definitions for express-list-endpoints 4.0
 // Project: https://github.com/AlbertoFdzM/express-list-endpoints
 // Definitions by: S Joseph <https://github.com/sjoseph7>
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
