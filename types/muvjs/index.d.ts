9598db5b903d00ad52fc34d4e0abd168eec1ae0f
Armando Ortiz <armando@solairo.co.jp>
2019-12-04 17:55:51 +0900

    wip

--- types/muvjs/index.d.ts
+++ types/muvjs/index.d.ts
@@ -3,3 +3,6 @@
 // Definitions by: Armando J Ortiz <https://github.com/yoyomo>
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
 // TypeScript Version: 3.4
+
+/// <reference path="muv.d.ts" />
+/// <reference path="muv-dom.d.ts" />
