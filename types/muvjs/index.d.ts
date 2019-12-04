919d6c3765165e6320611c4fec99013394518e66
Armando Ortiz <armando@solairo.co.jp>
2019-12-04 17:46:28 +0900

    npm package

--- types/muvjs/index.d.ts
+++ types/muvjs/index.d.ts
@@ -1,4 +1,4 @@
-// Type definitions for non-npm package MUVjs v2.3.0
+// Type definitions for MUVjs v2.3.0
 // Project: https://github.com/yoyomo/muvjs
 // Definitions by: Armando J Ortiz <https://github.com/yoyomo>
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
