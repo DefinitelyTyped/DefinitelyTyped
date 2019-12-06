e343962679b0521029f1d634e749c000b1417cb2
blurfx <iam@xo.dev>
2019-12-06 22:40:51 +0900

    Fix optional function parameters

--- types/php-serialize/index.d.ts
+++ types/php-serialize/index.d.ts
@@ -6,6 +6,6 @@
 
 export function isSerialized(givenItem: any, strict: any): any;
 
-export function serialize(item: any, scope: any, givenOptions: any): any;
+export function serialize(item: any, scope?: any, givenOptions?: any): any;
 
-export function unserialize(item: any, scope: any, givenOptions: any): any;
+export function unserialize(item: any, scope?: any, givenOptions?: any): any;
