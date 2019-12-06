2b4ef8297add6c2a633c20d42362bfa7a73abd95
blurfx <iam@xo.dev>
2019-12-06 22:18:16 +0900

    Add missing type definitions header

--- types/php-serialize/index.d.ts
+++ types/php-serialize/index.d.ts
@@ -1,6 +1,11 @@
+// Type definitions for php-serialize 3.0
+// Project: https://github.com/steelbrain/php-serialize
+// Definitions by: Changhui Lee <https://github.com/blurfx>
+// Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
+// TypeScript Version: 3.7
+
 export function isSerialized(givenItem: any, strict: any): any;
 
 export function serialize(item: any, scope: any, givenOptions: any): any;
 
 export function unserialize(item: any, scope: any, givenOptions: any): any;
-
