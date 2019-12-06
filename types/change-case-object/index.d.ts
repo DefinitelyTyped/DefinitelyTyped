3af4077f6837b43d7579ffec81180e458c74905e
James Chao <jameschao@gmail.com>
2019-12-06 12:14:57 +0800

    Fixed issue with typing array params

--- types/change-case-object/index.d.ts
+++ types/change-case-object/index.d.ts
@@ -4,25 +4,31 @@
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
 
 export function camelCase(s: string): string;
-export function camelCase<T>(arr: ReadonlyArray<T>): T[];
+export function camelCase(arr: ReadonlyArray<string>): string[];
+export function camelCase(arr: ReadonlyArray<object>): object[];
 export function camelCase(obj: object): object;
 
 export function snakeCase(s: string): string;
-export function snakeCase<T>(arr: ReadonlyArray<T>): T[];
+export function snakeCase(arr: ReadonlyArray<string>): string[];
+export function snakeCase(arr: ReadonlyArray<object>): object[];
 export function snakeCase(obj: object): object;
 
 export function paramCase(s: string): string;
-export function paramCase<T>(arr: ReadonlyArray<T>): T[];
+export function paramCase(arr: ReadonlyArray<string>): string[];
+export function paramCase(arr: ReadonlyArray<object>): object[];
 export function paramCase(obj: object): object;
 
 export function camel(s: string): string;
-export function camel<T>(arr: ReadonlyArray<T>): T[];
+export function camel(arr: ReadonlyArray<string>): string[];
+export function camel(arr: ReadonlyArray<object>): object[];
 export function camel(obj: object): object;
 
 export function snake(s: string): string;
-export function snake<T>(arr: ReadonlyArray<T>): T[];
+export function snake(arr: ReadonlyArray<string>): string[];
+export function snake(arr: ReadonlyArray<object>): object[];
 export function snake(obj: object): object;
 
 export function param(s: string): string;
-export function param<T>(arr: ReadonlyArray<T>): T[];
+export function param(arr: ReadonlyArray<string>): string[];
+export function param(arr: ReadonlyArray<object>): object[];
 export function param(obj: object): object;
