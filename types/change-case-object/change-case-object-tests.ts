0523ee5e3dbf4b7c30edecc97b3ee1cc3abe77ca
James Chao <jameschao@gmail.com>
2019-12-06 12:30:31 +0800

    Updated tests

--- types/change-case-object/change-case-object-tests.ts
+++ types/change-case-object/change-case-object-tests.ts
@@ -4,40 +4,52 @@ import changeCaseObject = require('change-case-object');
 changeCaseObject.camelCase({ hello_world: 'hi' });
 // $ExpectType string
 changeCaseObject.camelCase('hello_world');
-// $ExpectType ReadonlyArray<string>
+// $ExpectType string[]
 changeCaseObject.camelCase(['hello_world']);
+// $ExpectType object[]
+changeCaseObject.camelCase([{ hello_world: 'hi' }]);
 
 // $ExpectType object
 changeCaseObject.snakeCase({ helloWorld: 'hi' });
 // $ExpectType string
 changeCaseObject.snakeCase('helloWorld');
-// $ExpectType ReadonlyArray<string>
+// $ExpectType string[]
 changeCaseObject.snakeCase(['helloWorld']);
+// $ExpectType object[]
+changeCaseObject.snakeCase([{ hello_world: 'hi' }]);
 
 // $ExpectType object
 changeCaseObject.paramCase({ helloWorld: 'hi' });
 // $ExpectType string
 changeCaseObject.paramCase('helloWorld');
-// $ExpectType ReadonlyArray<string>
+// $ExpectType string[]
 changeCaseObject.paramCase(['helloWorld']);
+// $ExpectType object[]
+changeCaseObject.paramCase([{ hello_world: 'hi' }]);
 
 // $ExpectType object
 changeCaseObject.camel({ hello_world: 'hi' });
 // $ExpectType string
 changeCaseObject.camel('hello_world');
-// $ExpectType ReadonlyArray<string>
+// $ExpectType string[]
 changeCaseObject.camel(['hello_world']);
+// $ExpectType object[]
+changeCaseObject.camel([{ hello_world: 'hi' }]);
 
 // $ExpectType object
 changeCaseObject.snake({ helloWorld: 'hi' });
 // $ExpectType string
 changeCaseObject.snake('helloWorld');
-// $ExpectType ReadonlyArray<string>
+// $ExpectType string[]
 changeCaseObject.snake(['helloWorld']);
+// $ExpectType object[]
+changeCaseObject.snake([{ hello_world: 'hi' }]);
 
 // $ExpectType object
 changeCaseObject.param({ helloWorld: 'hi' });
 // $ExpectType string
 changeCaseObject.param('helloWorld');
-// $ExpectType ReadonlyArray<string>
+// $ExpectType string[]
 changeCaseObject.param(['helloWorld']);
+// $ExpectType object[]
+changeCaseObject.param([{ hello_world: 'hi' }]);
