d0e09fa347389f71cd86eb13160fa0ec97a16e2d
Geoff Garbers <geoff@garbers.co.za>
2019-12-09 11:11:12 +0200

    Add recommendations from linter.

--- types/purifycss-webpack/index.d.ts
+++ types/purifycss-webpack/index.d.ts
@@ -2,27 +2,27 @@
 // Project: https://github.com/webpack-contrib/purifycss-webpack
 // Definitions by: Geoff Garbers <https://github.com/garbetjie>
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
-///<reference types="webpack"/>
+/// <reference types="webpack"/>
 
-import {Plugin} from "webpack";
+import * as webpack from 'webpack';
 
-declare class PurifyPlugin extends Plugin {
+declare class PurifyPlugin extends webpack.Plugin {
     constructor(options?: PurifyOptions);
 }
 
-declare type PurifyOptions = {
-    styleExtensions?: string[],
-    moduleExtensions?: string[],
-    minimize?: boolean,
-    paths?: object|string[]
+interface PurifyOptions {
+    styleExtensions?: string[];
+    moduleExtensions?: string[];
+    minimize?: boolean;
+    paths?: object | string[];
     purifyOptions?: {
         minify?: boolean,
         output?: string | boolean,
         info?: boolean,
         rejected?: boolean,
         whitelist?: string[]
-    },
-    verbose?: boolean
+    };
+    verbose?: boolean;
 }
 
 export = PurifyPlugin;
