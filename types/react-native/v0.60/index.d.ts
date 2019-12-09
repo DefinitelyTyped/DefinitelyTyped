503a17a88d62445fdece2ab1bb08eebb154d35ef
REDMOND\acoates <acoates@microsoft.com>
2019-12-09 11:03:34 -0800

    Create seperate versions for different RN versions

--- types/react-native/v0.60/index.d.ts
+++ types/react-native/v0.60/index.d.ts
@@ -1,4 +1,4 @@
-// Type definitions for react-native 0.62
+// Type definitions for react-native 0.60
 // Project: https://github.com/facebook/react-native
 // Definitions by: Eloy Durán <https://github.com/alloy>
 //                 HuHuanming <https://github.com/huhuanming>
