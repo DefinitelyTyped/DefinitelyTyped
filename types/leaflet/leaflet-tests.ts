b3f4f638f1c4eb8f57cd26b1c1e5540114a25cfe
kygoh <gohkengyew@gmail.com>
2019-12-10 23:06:52 +0800

    leaflet stopPropagation and stop new parameter tests
    added new parameter type tests for stopPropagation and stop

--- types/leaflet/leaflet-tests.ts
+++ types/leaflet/leaflet-tests.ts
@@ -336,6 +336,9 @@ videoOverlay = L.videoOverlay(videoElement, videoOverlayBounds, {
 });
 
 const eventHandler = () => { };
+const leafletMouseEvent: LeafletMouseEvent = {} as LeafletMouseEvent;
+const leafletKeyboardEvent: LeafletKeyboardEvent = {} as LeafletKeyboardEvent;
+const leafletEvent: LeafletEvent = {} as LeafletEvent;
 const domEvent: Event = {} as Event;
 L.DomEvent
 	.on(htmlElement, 'click', eventHandler)
@@ -346,10 +349,16 @@ L.DomEvent
 	.addListener(htmlElement, { click: eventHandler })
 	.off(htmlElement, { click: eventHandler }, eventHandler)
 	.removeListener(htmlElement, { click: eventHandler }, eventHandler)
+	.stopPropagation(leafletMouseEvent)
+	.stopPropagation(leafletKeyboardEvent)
+	.stopPropagation(leafletEvent)
 	.stopPropagation(domEvent)
 	.disableScrollPropagation(htmlElement)
 	.disableClickPropagation(htmlElement)
 	.preventDefault(domEvent)
+	.stop(leafletMouseEvent)
+	.stop(leafletKeyboardEvent)
+	.stop(leafletEvent)
 	.stop(domEvent);
 point = L.DomEvent.getMousePosition(domEvent as MouseEvent);
 point = L.DomEvent.getMousePosition(domEvent as MouseEvent, htmlElement);
