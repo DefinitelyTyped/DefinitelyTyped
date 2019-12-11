cc76d63a7bbf3cc8084ad18468a68b8f968527d1
Oliver Joseph Ash <oliverjash@gmail.com>
2019-12-11 11:36:02 +0000

    Add working test

--- types/node/node-tests.ts
+++ types/node/node-tests.ts
@@ -21,10 +21,6 @@ import Module = require("module");
     {
         url.format(url.parse('http://www.example.com/xyz'));
 
-        // TODO: why won't this work?
-        // // $ExpectError
-        // url.format({ path: '/foo' });
-
         url.format('http://www.example.com/xyz');
 
         // https://google.com/search?q=you're%20a%20lizard%2C%20gary
