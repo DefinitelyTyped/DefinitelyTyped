d807f280d48d79ce7188e3e4bd419cddb4dea7a3
Carlos Anoceto <43271580+canoceto@users.noreply.github.com>
2019-12-11 17:38:19 -0500

    Update index.d.ts

--- types/chart.js/index.d.ts
+++ types/chart.js/index.d.ts
@@ -21,7 +21,7 @@
 //                 Takuya Uehara <https://github.com/indigolain>
 //                 Ricardo Mello <https://github.com/ricardo-mello>
 //                 Ray Nicholus <https://github.com/rnicholus>
-//                 Carlos Anoceto <https://github.com/canoceto> 
+//                 Carlos Anoceto <https://github.com/canoceto>
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
 // TypeScript Version: 2.3
 
