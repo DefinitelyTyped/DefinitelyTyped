b20c7398f21582fc90f634c103242223fe26c6b0
anault <nault.alex@gmail.com>
2019-12-12 11:09:44 -0500

    Fix semicolon separator

--- types/slate-plain-serializer/index.d.ts
+++ types/slate-plain-serializer/index.d.ts
@@ -11,7 +11,7 @@ export interface DeserializeOptions {
     toJson?: boolean;
     defaultBlock?: BlockProperties;
     defaultMarks?: MarkProperties[] | Set<MarkProperties>;
-    delimiter?: string,
+    delimiter?: string;
 }
 
 export interface SerializeOptions {
