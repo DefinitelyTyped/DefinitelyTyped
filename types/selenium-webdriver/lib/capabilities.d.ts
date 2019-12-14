2c9a82c077946c44d1de778a7ebcdcd2b03fbff6
Ziyu Wang <odduid@gmail.com>
2019-12-14 14:16:48 +1100

    update capabilities to match implementation

--- types/selenium-webdriver/lib/capabilities.d.ts
+++ types/selenium-webdriver/lib/capabilities.d.ts
@@ -1,3 +1,4 @@
+import Symbols from './symbols';
 import { logging, ProxyConfig } from '../';
 
 /**
@@ -25,38 +26,96 @@ export interface IBrowser {
 export const Browser: IBrowser;
 
 /**
+ * Common platform names. These platforms are not explicitly defined by the
+ * WebDriver spec, however, their use is encouraged for interoperability.
+ *
+ * @enum {string}
+ * @see <https://w3c.github.io/webdriver/webdriver-spec.html>
+ */
+export interface IPlatform {
+  LINUX: string;
+  MAC: string;
+  WINDOWS: string;
+}
+
+export const Platform: IPlatform;
+
+/**
+ * Strategies for waiting for [document readiness] after a navigation event.
+ *
+ * [document readiness]: https://html.spec.whatwg.org/#current-document-readiness
+ *
+ * @enum {string}
+ */
+export interface IPageLoadStrategy {
+  /**
+   * Indicates WebDriver should not wait on the document readiness state after a
+   * navigation event.
+   */
+  NONE: string;
+
+  /**
+   * Indicates WebDriver should wait for the document readiness state to
+   * become "interactive" after navigation.
+   */
+  EAGER: string;
+
+  /**
+   * Indicates WebDriver should wait for the document readiness state to
+   * be "complete" after navigation. This is the default page loading strategy.
+   */
+  NORMAL: string;
+}
+
+export const PageLoadStrategy: IPageLoadStrategy;
+
+/**
+ * The possible default actions a WebDriver session can take to respond to
+ * unhandled user prompts (`window.alert()`, `window.confirm()`, and
+ * `window.prompt()`).
+ *
+ * @enum {string}
+ */
+export interface IUserPromptHandler {
+  /** All prompts should be silently accepted. */
+  ACCEPT: string;
+  /** All prompts should be silently dismissed. */
+  DISMISS: string;
+  /**
+   * All prompts should be automatically accepted, but an error should be
+   * returned to the next (or currently executing) WebDriver command.
+   */
+  ACCEPT_AND_NOTIFY: string;
+  /**
+   * All prompts should be automatically dismissed, but an error should be
+   * returned to the next (or currently executing) WebDriver command.
+   */
+  DISMISS_AND_NOTIFY: string;
+  /** All prompts should be left unhandled. */
+  IGNORE: string;
+}
+
+export const UserPromptHandler: IUserPromptHandler;
+
+/**
  * Common webdriver capability keys.
  * @enum {string}
  */
 export interface ICapability {
   /**
-   * Indicates whether a driver should accept all SSL certs by default. This
-   * capability only applies when requesting a new session. To query whether
-   * a driver can handle insecure SSL certs, see
-   * {@link Capability.SECURE_SSL}.
+   * Indicates whether a WebDriver session implicitly trusts otherwise untrusted
+   * and self-signed TLS certificates during navigation.
    */
-  ACCEPT_SSL_CERTS: string;
+  ACCEPT_INSECURE_TLS_CERTS: string;
 
   /**
    * The browser name. Common browser names are defined in the
-   * {@link Browser} enum.
+   * {@link ./capabilities.Browser Browser} enum.
    */
   BROWSER_NAME: string;
 
-  /**
-   * Defines how elements should be scrolled into the viewport for interaction.
-   * This capability will be set to zero (0) if elements are aligned with the
-   * top of the viewport, or one (1) if aligned with the bottom. The default
-   * behavior is to align with the top of the viewport.
-   */
-  ELEMENT_SCROLL_BEHAVIOR: string;
-
-  /**
-   * Whether the driver is capable of handling modal alerts (e.g. alert,
-   * confirm, prompt). To define how a driver <i>should</i> handle alerts,
-   * use {@link Capability.UNEXPECTED_ALERT_BEHAVIOR}.
-   */
-  HANDLES_ALERTS: string;
+  /** Identifies the browser version. */
+  BROWSER_VERSION: string;
 
   /**
    * Key for the logging driver logging preferences.
@@ -64,56 +123,47 @@ export interface ICapability {
   LOGGING_PREFS: string;
 
   /**
-   * Whether this session generates native events when simulating user input.
+   * Defines the session's
+   * {@linkplain ./capabilities.PageLoadStrategy page loading strategy}.
    */
-  NATIVE_EVENTS: string;
+  PAGE_LOAD_STRATEGY: string;
 
   /**
-   * Describes the platform the browser is running on. Will be one of
-   * ANDROID, IOS, LINUX, MAC, UNIX, or WINDOWS. When <i>requesting</i> a
-   * session, ANY may be used to indicate no platform preference (this is
-   * semantically equivalent to omitting the platform capability).
+   * Identifies the operating system of the endpoint node. Common values
+   * recognized by the most WebDriver server implementations are predefined in
+   * the {@link ./capabilities.Platform Platform} enum.
    */
-  PLATFORM: string;
+  PLATFORM_NAME: string;
 
   /**
    * Describes the proxy configuration to use for a new WebDriver session.
    */
   PROXY: string;
 
-  /** Whether the driver supports changing the brower's orientation. */
-  ROTATABLE: string;
+  /**
+   * Indicates whether the remote end supports all of the window resizing and
+   * positioning commands:
+   *
+   * -  {@linkplain ./webdriver.Window#getRect Window.getRect()}
+   * -  {@linkplain ./webdriver.Window#setRect Window.setRect()}
+   * -  {@linkplain ./webdriver.Window#maximize Window.maximize()}
+   * -  {@linkplain ./webdriver.Window#minimize Window.minimize()}
+   * -  {@linkplain ./webdriver.Window#fullscreen Window.fullscreen()}
+   */
+  SET_WINDOW_RECT: string;
 
   /**
-   * Whether a driver is only capable of handling secure SSL certs. To request
-   * that a driver accept insecure SSL certs by default, use
-   * {@link Capability.ACCEPT_SSL_CERTS}.
+   * Describes the {@linkplain ./capabilities.Timeouts timeouts} imposed on
+   * certain session operations.
    */
-  SECURE_SSL: string;
-
-  /** Whether the driver supports manipulating the app cache. */
-  SUPPORTS_APPLICATION_CACHE: string;
-
-  /** Whether the driver supports locating elements with CSS selectors. */
-  SUPPORTS_CSS_SELECTORS: string;
-
-  /** Whether the browser supports JavaScript. */
-  SUPPORTS_JAVASCRIPT: string;
-
-  /** Whether the driver supports controlling the browser's location info. */
-  SUPPORTS_LOCATION_CONTEXT: string;
-
-  /** Whether the driver supports taking screenshots. */
-  TAKES_SCREENSHOT: string;
+  TIMEOUTS: string;
 
   /**
-   * Defines how the driver should handle unexpected alerts. The value should
-   * be one of 'accept', 'dismiss', or 'ignore.
+   * Defines how a WebDriver session should
+   * {@linkplain ./capabilities.UserPromptHandler respond} to unhandled user
+   * prompts.
    */
-  UNEXPECTED_ALERT_BEHAVIOR: string;
-
-  /** Defines the browser version. */
-  VERSION: string;
+  UNHANDLED_PROMPT_BEHAVIOR: string;
 }
 
 /**
@@ -128,114 +178,14 @@ export class Capabilities {
   // region Constructors
 
   /**
-   * @param {(Capabilities|Object)=} opt_other Another set of
-   *     capabilities to merge into this instance.
-   * @constructor
+   * @param {(Capabilities|Map<string, ?>|Object)=} other Another set of
+   *     capabilities to initialize this instance from.
    */
-  constructor(opt_other?: Capabilities|{});
-
-  // endregion
-
-  // region Methods
-
-  /** @return {!Object} The JSON representation of this instance. */
-  toJSON(): any;
-
-  /**
-   * Merges another set of capabilities into this instance. Any duplicates in
-   * the provided set will override those already set on this instance.
-   * @param {!(Capabilities|Object)} other The capabilities to
-   *     merge into this instance.
-   * @return {!Capabilities} A self reference.
-   */
-  merge(other: Capabilities|{}): Capabilities;
-
-  /**
-   * @param {string} key The capability to set.
-   * @param {*} value The capability value.  Capability values must be JSON
-   *     serializable. Pass {@code null} to unset the capability.
-   * @return {!Capabilities} A self reference.
-   */
-  set(key: string, value: any): Capabilities;
-
-  /**
-   * Sets the name of the target browser.
-   *
-   * @param {(Browser|string)} name the browser name.
-   * @return {!Capabilities} a self reference.
-   */
-  setBrowserName(name: string): Capabilities;
-
-  /**
-   * Sets the desired version of the target browser.
-   *
-   * @param {string} version the desired version.
-   * @return {!Capabilities} a self reference.
-   */
-  setBrowserVersion(version: string): Capabilities;
-
-  /**
-   * Sets the logging preferences. Preferences may be specified as a
-   * {@link logging.Preferences} instance, or a as a map of log-type to
-   * log-level.
-   * @param {!(logging.Preferences|Object.<string, string>)} prefs The
-   *     logging preferences.
-   * @return {!Capabilities} A self reference.
-   */
-  setLoggingPrefs(prefs: logging.Preferences|{}): Capabilities;
-
-  /**
-   * Sets the proxy configuration for this instance.
-   * @param {ProxyConfig} proxy The desired proxy configuration.
-   * @return {!Capabilities} A self reference.
-   */
-  setProxy(proxy: ProxyConfig): Capabilities;
-
-  /**
-   * Sets whether native events should be used.
-   * @param {boolean} enabled Whether to enable native events.
-   * @return {!Capabilities} A self reference.
-   */
-  setEnableNativeEvents(enabled: boolean): Capabilities;
-
-  /**
-   * Sets how elements should be scrolled into view for interaction.
-   * @param {number} behavior The desired scroll behavior: either 0 to align
-   *     with the top of the viewport or 1 to align with the bottom.
-   * @return {!Capabilities} A self reference.
-   */
-  setScrollBehavior(behavior: number): Capabilities;
-
-  /**
-   * Sets the default action to take with an unexpected alert before returning
-   * an error.
-   * @param {string} behavior The desired behavior; should be 'accept',
-   *     'dismiss', or 'ignore'. Defaults to 'dismiss'.
-   * @return {!Capabilities} A self reference.
-   */
-  setAlertBehavior(behavior?: string): Capabilities;
-
-  /**
-   * @param {string} key The capability to return.
-   * @return {*} The capability with the given key, or {@code null} if it has
-   *     not been set.
-   */
-  get(key: string): any;
-
-  /**
-   * @param {string} key The capability to check.
-   * @return {boolean} Whether the specified capability is set.
-   */
-  has(key: string): boolean;
+  constructor(other?: Capabilities|Map<string, any>|{});
 
   // endregion
 
   // region Static Methods
-
-  /**
-   * @return {!Capabilities} A basic set of capabilities for Android.
-   */
-  static android(): Capabilities;
 
   /**
    * @return {!Capabilities} A basic set of capabilities for Chrome.
@@ -259,74 +209,195 @@ export class Capabilities {
   static ie(): Capabilities;
 
   /**
-   * @return {!Capabilities} A basic set of capabilities for iPad.
-   */
-  static ipad(): Capabilities;
-
-  /**
-   * @return {!Capabilities} A basic set of capabilities for iPhone.
-   */
-  static iphone(): Capabilities;
-
-  /**
-   * @return {!Capabilities} A basic set of capabilities for Opera.
-   */
-  static opera(): Capabilities;
-
-  /**
-   * @return {!Capabilities} A basic set of capabilities for
-   *     PhantomJS.
-   */
-  static phantomjs(): Capabilities;
-
-  /**
    * @return {!Capabilities} A basic set of capabilities for Safari.
    */
   static safari(): Capabilities;
 
-  /**
-   * @return {!Capabilities} A basic set of capabilities for HTMLUnit.
-   */
-  static htmlunit(): Capabilities;
+  // endregion
+
+  // region Methods
 
   /**
-   * @return {!Capabilities} A basic set of capabilities for HTMLUnit
-   *                                   with enabled Javascript.
+   * @return {!Object<string, ?>} The JSON representation of this instance.
+   *     Note, the returned object may contain nested promised values.
+   * @suppress {checkTypes} Suppress [] access on a struct (state inherited from
+   *     Map).
    */
-  static htmlunitwithjs(): Capabilities;
+  [Symbols.serialize](): {}
+
+  /**
+   * @param {string} key The capability to return.
+   * @return {*} The capability with the given key, or {@code null} if it has
+   *     not been set.
+   */
+  get(key: string): any;
+
+  /**
+   * @param {string} key The capability to check.
+   * @return {boolean} Whether the specified capability is set.
+   */
+  has(key: string): boolean;
+
+  /**
+   * @return {!Iterator<string>} an iterator of the keys set.
+   */
+  keys(): IterableIterator<string>;
+
+  /**
+   * Merges another set of capabilities into this instance.
+   * @param {!(Capabilities|Map<String, ?>|Object<string, ?>)} other The other
+   *     set of capabilities to merge.
+   * @return {!Capabilities} A self reference.
+   */
+  merge(other: Capabilities|Map<string, any>|{}): Capabilities;
+
+  /**
+   * Deletes an entry from this set of capabilities.
+   *
+   * @param {string} key the capability key to delete.
+   */
+  delete(key: string): boolean;
+
+  /**
+   * @param {string} key The capability key.
+   * @param {*} value The capability value.
+   * @return {!Capabilities} A self reference.
+   * @throws {TypeError} If the `key` is not a string.
+   */
+  set(key: string, value: any): Capabilities;
+
+  /**
+   * Sets whether a WebDriver session should implicitly accept self-signed, or
+   * other untrusted TLS certificates on navigation.
+   *
+   * @param {boolean} accept whether to accept insecure certs.
+   * @return {!Capabilities} a self reference.
+   */
+  setAcceptInsecureCerts(accept: boolean): Capabilities;
+
+  /**
+   * @return {boolean} whether the session is configured to accept insecure
+   *     TLS certificates.
+   */
+  getAcceptInsecureCerts(): boolean;
+
+  /**
+   * Sets the name of the target browser.
+   *
+   * @param {(Browser|string)} name the browser name.
+   * @return {!Capabilities} a self reference.
+   */
+  setBrowserName(name: string): Capabilities;
+
+  /**
+   * @return {(string|undefined)} the configured browser name, or undefined if
+   *     not set.
+   */
+  getBrowserName(): string|undefined;
+
+  /**
+   * Sets the desired version of the target browser.
+   *
+   * @param {string} version the desired version.
+   * @return {!Capabilities} a self reference.
+   */
+  setBrowserVersion(version: string): Capabilities;
+
+  /**
+   * @return {(string|undefined)} the configured browser version, or undefined
+   *     if not set.
+   */
+  getBrowserVersion(): string|undefined;
+
+  /**
+   * Sets the desired page loading strategy for a new WebDriver session.
+   *
+   * @param {PageLoadStrategy} strategy the desired strategy.
+   * @return {!Capabilities} a self reference.
+   */
+  setPageLoadStrategy(strategy: string): Capabilities;
+
+  /**
+   * Returns the configured page load strategy.
+   *
+   * @return {(string|undefined)} the page load strategy.
+   */
+  getPageLoadStrategy(): string|undefined;
+
+  /**
+   * Sets the target platform.
+   *
+   * @param {(Platform|string)} platform the target platform.
+   * @return {!Capabilities} a self reference.
+   */
+  setPlatform(platform: string): Capabilities;
+
+  /**
+   * @return {(string|undefined)} the configured platform or undefined if not
+   *     set.
+   */
+  getPlatform(): string|undefined;
+
+  /**
+   * Sets the logging preferences. Preferences may be specified as a
+   * {@link ./logging.Preferences} instance, or as a map of log-type to
+   * log-level.
+   * @param {!(./logging.Preferences|Object<string>)} prefs The logging
+   *     preferences.
+   * @return {!Capabilities} A self reference.
+   */
+  setLoggingPrefs(prefs: logging.Preferences|{}): Capabilities;
+
+  /**
+   * Sets the proxy configuration for this instance.
+   * @param {proxy.Config} proxy The desired proxy configuration.
+   * @return {!Capabilities} A self reference.
+   */
+  setProxy(proxy: ProxyConfig): Capabilities;
+
+  /**
+   * @return {(proxy.Config|undefined)} the configured proxy settings, or
+   *     undefined if not set.
+   */
+  getProxy(): ProxyConfig|undefined;
+
+  /**
+   * Sets the default action to take with an unexpected alert before returning
+   * an error. If unspecified, WebDriver will default to
+   * {@link UserPromptHandler.DISMISS_AND_NOTIFY}.
+   *
+   * @param {?UserPromptHandler} behavior The way WebDriver should respond to
+   *     unhandled user prompts.
+   * @return {!Capabilities} A self reference.
+   */
+  setAlertBehavior(behavior: string): Capabilities;
+
+  /**
+   * @return {(UserPromptHandler|undefined)} the behavior pattern for responding
+   *     to unhandled user prompts, or undefined if not set.
+   */
+  getAlertBehavior(): string|undefined;
 
   // endregion
 }
 
 export interface ITimeouts {
   /**
-   * Sets the amount of time to wait, in milliseconds, for an asynchronous
-   * script to finish execution before returning an error. If the timeout is
-   * less than or equal to 0, the script will be allowed to run indefinitely.
+   * Defines when, in milliseconds, to interrupt a script that is being
+   * {@linkplain ./webdriver.IWebDriver#executeScript evaluated}.
    */
   script?: number;
+
   /**
-   * Sets the amount of time to wait, in milliseconds, for a page load to
-   * complete before returning an error.  If the timeout is negative,
-   * page loads may be indefinite.
+   * The timeout, in milliseconds, to apply to navigation events along with the
+   * {@link PageLoadStrategy}.
    */
   pageLoad?: number;
+
   /**
-   * Specifies the amount of time in milliseconds the driver should wait when
-   * searching for an element if it is not immediately present.
-   * <p/>
-   * When searching for a single element, the driver should poll the page
-   * until the element has been found, or this timeout expires before failing
-   * with a {@code bot.ErrorCode.NO_SUCH_ELEMENT} error. When searching
-   * for multiple elements, the driver should poll the page until at least one
-   * element has been found or this timeout has expired.
-   * <p/>
-   * Setting the wait timeout to 0 (its default value), disables implicit
-   * waiting.
-   * <p/>
-   * Increasing the implicit wait timeout should be used judiciously as it
-   * will have an adverse effect on test run time, especially when used with
-   * slower location strategies like XPath.
+   * The maximum amount of time, in milliseconds, to spend attempting to
+   * {@linkplain ./webdriver.IWebDriver#findElement locate} an element on the
+   * current page.
    */
   implicit?: number;
 }
