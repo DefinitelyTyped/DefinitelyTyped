f1213e44384cd41740a2f5b5838b1438ebfc5756
hasparus <hasparus@gmail.com>
2019-12-15 19:38:51 +0100

    Stub definitions for @theme-ui/components

---
+++ types/theme-ui__components/index.d.ts
@@ -0,0 +1,42 @@
+// Type definitions for @theme-ui/components 0.2.50
+// Project: https://github.com/system-ui/theme-ui
+// Definitions by: Piotr Monwid-Olechnowicz <https://github.com/hasparus>
+// Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
+// TypeScript Version: 3.7
+
+import * as React from 'react';
+
+export const Box: React.FC;
+export const Flex: React.FC;
+export const Grid: React.FC;
+export const Button: React.FC;
+export const Link: React.FC;
+export const Text: React.FC;
+export const Heading: React.FC;
+export const Image: React.FC;
+export const Card: React.FC;
+export const Label: React.FC;
+export const Input: React.FC;
+export const Select: React.FC;
+export const Textarea: React.FC;
+export const Radio: React.FC;
+export const Checkbox: React.FC;
+export const Slider: React.FC;
+export const Field: React.FC;
+export const Progress: React.FC;
+export const Donut: React.FC;
+export const Spinner: React.FC;
+export const Avatar: React.FC;
+export const Badge: React.FC;
+export const Close: React.FC;
+export const Alert: React.FC;
+export const Divider: React.FC;
+export const Embed: React.FC;
+export const AspectRatio: React.FC;
+export const AspectImage: React.FC;
+export const Container: React.FC;
+export const NavLink: React.FC;
+export const Message: React.FC;
+export const IconButton: React.FC;
+export const MenuButton: React.FC;
+
