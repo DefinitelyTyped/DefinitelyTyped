075197ad5fde20ba7d38aec18d7fee133b7c54dd
Chris Wilkinson <c.wilkinson@elifesciences.org>
2019-12-17 14:18:05 +0000

    Add header

--- types/rdfjs__namespace/index.d.ts
+++ types/rdfjs__namespace/index.d.ts
@@ -1,3 +1,8 @@
+// Type definitions for @rdfjs/namespace 1.1
+// Project: https://github.com/rdfjs-base/namespace
+// Definitions by: Chris Wilkinson <https://github.com/thewilkybarkid>
+// Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
+
 import { DataFactory, NamedNode } from 'rdf-js';
 
 declare function namespace(baseIRI: string, options?: namespace.BuilderOptions): namespace.NamespaceBuilder;
