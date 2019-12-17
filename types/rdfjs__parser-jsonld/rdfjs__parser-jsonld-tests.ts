c0e7eeab137bcd083cba79ac3bad6b567b53eee9
Chris Wilkinson <c.wilkinson@elifesciences.org>
2019-12-17 15:24:29 +0000

    More type tests

--- types/rdfjs__parser-jsonld/rdfjs__parser-jsonld-tests.ts
+++ types/rdfjs__parser-jsonld/rdfjs__parser-jsonld-tests.ts
@@ -1,6 +1,7 @@
 import Parser = require('@rdfjs/parser-jsonld');
+import { EventEmitter } from 'events';
 import { Context } from 'jsonld/jsonld-spec';
-import { DataFactory, Stream } from 'rdf-js';
+import { DataFactory, Sink, Stream } from 'rdf-js';
 
 const baseIRI = '';
 const context: Context = {} as any;
@@ -11,6 +12,8 @@ const parser1 = new Parser();
 const parser2 = new Parser({});
 const parser3 = new Parser({ baseIRI, context, factory });
 
-const eventEmitter1 = parser1.import(stream);
-const eventEmitter2 = parser1.import(stream, {});
-const eventEmitter3 = parser1.import(stream, { baseIRI, context, factory });
+const sink: Sink = parser1;
+
+const eventEmitter1: EventEmitter = parser1.import(stream);
+const eventEmitter2: EventEmitter = parser1.import(stream, {});
+const eventEmitter3: EventEmitter = parser1.import(stream, { baseIRI, context, factory });
