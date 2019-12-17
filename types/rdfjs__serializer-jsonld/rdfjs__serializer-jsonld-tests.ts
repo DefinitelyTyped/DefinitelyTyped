419eb1a7f8f1474c1949e1697c336cf675caa666
Chris Wilkinson <c.wilkinson@elifesciences.org>
2019-12-17 15:09:52 +0000

    Add @rdfjs/serializer-jsonld

---
+++ types/rdfjs__serializer-jsonld/rdfjs__serializer-jsonld-tests.ts
@@ -0,0 +1,15 @@
+import Serializer = require('@rdfjs/serializer-jsonld');
+import { EventEmitter } from 'events';
+import { Stream } from 'rdf-js';
+
+const stream: Stream = {} as any;
+
+const serializer1 = new Serializer();
+const serializer2 = new Serializer({});
+const serializer3 = new Serializer({ encoding: 'string' });
+const serializer4 = new Serializer({ encoding: 'object' });
+
+const eventEmitter1: EventEmitter = serializer1.import(stream);
+const eventEmitter2: EventEmitter = serializer1.import(stream, {});
+const eventEmitter3: EventEmitter = serializer1.import(stream, { encoding: 'string' });
+const eventEmitter4: EventEmitter = serializer1.import(stream, { encoding: 'object' });
