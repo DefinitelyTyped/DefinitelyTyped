480280eefe88d48a5e463ff8e6096cac00adbbc2
Phil Jones <philip.graham.jones@googlemail.com>
2019-12-17 13:10:37 +0000

    recharts: Add ReferenceLine position prop
    Recharts' ReferenceLines have gained the ability for a position
    to be specified, https://github.com/recharts/recharts/pull/1685/files.

--- types/recharts/index.d.ts
+++ types/recharts/index.d.ts
@@ -55,6 +55,7 @@ export type LineType =
 export type IfOverflowType = 'hidden' | 'visible' | 'discard' | 'extendDomain';
 export type AxisInterval = number | 'preserveStart' | 'preserveEnd' | 'preserveStartEnd';
 export type BaseValueType = number | 'auto' | 'dataMin' | 'dataMax';
+export type ReferenceLinePosition = 'start' | 'middle' | 'end';
 
 export type PickedCSSStyleDeclarationKeys =
     'alignmentBaseline' | 'baselineShift' | 'clip' | 'clipPath' | 'clipRule' | 'color' |
@@ -735,6 +736,7 @@ export interface ReferenceLineProps extends Partial<PresentationAttributes<numbe
         & Partial<PresentationAttributes<number | string, number | string>>
         & { x1: number; y1: number; x2: number; y2: number; }
     > | React.ReactElement;
+    position?: ReferenceLinePosition;
 }
 
 export class ReferenceLine extends React.Component<ReferenceLineProps> { }
