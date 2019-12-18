4e4e3afe30d7c8293af1804e20f111493923df1e
wrumsby <walter@rumsby.net>
2019-12-19 09:42:42 +1300

    Add typings for @newrelic/winston-enricher

---
+++ types/newrelic__winston-enricher/index.d.ts
@@ -0,0 +1,9 @@
+// Type definitions for @newrelic/winston-enricher 0.1
+// Project: https://github.com/newrelic/newrelic-winston-logenricher-node
+// Definitions by: wrumsby <https://github.com/wrumsby>
+// Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
+import * as winston from 'winston';
+
+desclare function newrelicFormatter(opts?: any): winston.format;
+
+export = newrelicFormatter;
---
+++ types/newrelic__winston-enricher/newrelic__winston-enricher-tests.ts
@@ -0,0 +1,3 @@
+import newrelicFormatter from '@newrelic/winston-enricher';
+
+const formatter = newrelicFormatter();
