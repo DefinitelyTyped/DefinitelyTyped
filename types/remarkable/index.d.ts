9dd42895b8601198d07b468f6c9f8129ad2f1467
Herberth <herberthobregon@gmail.com>
2019-12-18 00:05:10 -0600

    Update index.d.ts for v2.0.0

--- types/remarkable/index.d.ts
+++ types/remarkable/index.d.ts
@@ -1,12 +1,12 @@
-// Type definitions for remarkable 1.7
+// Type definitions for remarkable 2.0.0
 // Project: https://github.com/jonschlinkert/remarkable
 // Definitions by: makepost <https://github.com/makepost>
 //                 Richard Lea <https://github.com/chigix>
 //                 Lilian Saget-Lethias <https://github.com/bios21>
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
-// TypeScript Version: 2.3
+// TypeScript Version: 2.7.2
 
 import Remarkable = require("./lib");
 
-export = Remarkable;
+export { Remarkable };
 export as namespace Remarkable;
