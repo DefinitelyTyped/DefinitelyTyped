c86991afc71de57c0f31688abc1a8f7c539acc6e
Bennnsen <benjamin.vogt87@gmail.com>
2019-12-18 12:56:06 +0100

    fixed typings
    added KeyPair properties, CleartextMessage.fromText now has CleartextMessage as return type

--- types/openpgp/ts3.2/index.d.ts
+++ types/openpgp/ts3.2/index.d.ts
@@ -95,7 +95,7 @@ export namespace cleartext {
      * Creates a new CleartextMessage object from text
      * @param text
      */
-    function fromText(text: string): void;
+    function fromText(text: string): CleartextMessage;
 }
 
 /**
@@ -774,6 +774,10 @@ export namespace crypto {
 
             namespace key {
                 class KeyPair {
+                    privateKeyArmored: string;
+                    publicKeyArmored: string;
+                    key?: key.Key;
+                    revocationCertificate?: string;
                 }
             }
         }
