fc5f37423c60ab38a5459c7d5a2f9a5f23d9f648
James Hill <me@jhdev.uk>
2019-12-18 15:50:52 +0000

    Added test file for definitions

--- types/kurento-client/kurento-client-tests.ts
+++ types/kurento-client/kurento-client-tests.ts
@@ -1,5 +1,28 @@
 import kurento from 'kurento-client';
 
 async () => {
+    const sdpOffer = 'offer';
+    const candidate = new RTCIceCandidate();
+
     const client = await kurento('//server');
+    const pipeline = await client.create('MediaPipeline');
+    const endpoint = await pipeline.create('WebRtcEndpoint');
+
+    endpoint.addIceCandidate(candidate);
+
+    endpoint.on('OnIceCandidate', ({ candidate }) => {
+        const value = kurento.getComplexType('IceCandidate')(
+            candidate
+        );
+
+        endpoint.addIceCandidate(value);
+    });
+
+    const sdpAnswer = await endpoint.processOffer(sdpOffer);
+
+    endpoint.gatherCandidates(error => {
+        // ok
+    });
+
+    return { sdpAnswer };
 };
