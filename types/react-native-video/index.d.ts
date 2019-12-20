f4aa26fd4ae6aa6bdcd68150199fb79b99395343
Valérian Cubero <nekith@errant-works.com>
2019-12-20 14:26:06 +0100

    [react-native-video] update version in header comments

--- types/react-native-video/index.d.ts
+++ types/react-native-video/index.d.ts
@@ -1,4 +1,4 @@
-// Type definitions for react-native-video 3.1
+// Type definitions for react-native-video 5.0.2
 // Project: https://github.com/react-native-community/react-native-video, https://github.com/brentvatne/react-native-video
 // Definitions by: HuHuanming <https://github.com/huhuanming>
 //                 Nekith <https://github.com/Nekith>
