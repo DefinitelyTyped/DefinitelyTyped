8ebde01323a86817f6a41089744cb248167d42d1
Amaimersion <amatsukine@gmail.com>
2019-12-22 17:27:14 +0300

    Add tests

---
+++ types/remove-files-webpack-plugin/remove-files-webpack-plugin-tests.ts
@@ -0,0 +1,38 @@
+import RemovePlugin = require("remove-files-webpack-plugin");
+
+
+new RemovePlugin({
+    before: {
+        root: ".",
+        include: [
+            "./test"
+        ],
+        exclude: [
+            "./test/test.txt"
+        ],
+        trash: false,
+        log: false,
+        emulate: true,
+        allowRootAndOutside: true
+    },
+    after: {
+        test: [
+            {
+                folder: "./",
+                method: (filePath) => {
+                    return filePath.includes('.map');
+                }
+            },
+            {
+                folder: "./test",
+                method: (filePath) => {
+                    return filePath.includes('.map');
+                },
+                recursive: true
+            }
+        ],
+        logWarning: true,
+        logError: true,
+        logDebug: true
+    }
+});
