acb2d87605b004f31b4d11c79031dc9eabd9d164
Matteo Frana <f2@f2.net>
2019-12-23 11:29:28 +0100

    Removed patch version number

--- types/react-query/index.d.ts
+++ types/react-query/index.d.ts
@@ -1,4 +1,4 @@
-// Type definitions for react-query 0.3.23
+// Type definitions for react-query 0.3
 // Project: https://github.com/tannerlinsley/react-query
 // Definitions by: Lukasz Fiszer <https://github.com/lukaszfiszer>
 //                 Jace Hensley <https://github.com/jacehensley>
