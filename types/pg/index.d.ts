da9a21ba5754757c4baef7d8aa616f536187df65
Eugeny Schibrikov <eugeny.schibrikov@paxport.net>
2019-12-23 20:58:34 +0500

    [pg] Updated Client and Pool config types according to docs (#40728)
    * Fixed Client and Pool config types for pg
    
    * Add ConnectionConfig alias

--- types/pg/index.d.ts
+++ types/pg/index.d.ts
@@ -1,4 +1,4 @@
-// Type definitions for pg 7.11
+// Type definitions for pg 7.14
 // Project: http://github.com/brianc/node-postgres
 // Definitions by: Phips Peter <https://github.com/pspeter3>, Ravi van Rooijen <https://github.com/HoldYourWaffle>
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
@@ -10,7 +10,9 @@ import events = require("events");
 import stream = require("stream");
 import pgTypes = require("pg-types");
 
-export interface ConnectionConfig {
+import { ConnectionOptions } from "tls";
+
+export interface ClientConfig {
     user?: string;
     database?: string;
     password?: string;
@@ -20,22 +22,19 @@ export interface ConnectionConfig {
     keepAlive?: boolean;
     stream?: stream.Duplex;
     statement_timeout?: false | number;
-    connectionTimeoutMillis?: number;
+    ssl?: boolean | ConnectionOptions;
+    query_timeout?: number;
     keepAliveInitialDelayMillis?: number;
 }
 
-export interface Defaults extends ConnectionConfig {
+export type ConnectionConfig = ClientConfig;
+
+export interface Defaults extends ClientConfig {
     poolSize?: number;
     poolIdleTimeout?: number;
     reapIntervalMillis?: number;
     binary?: boolean;
     parseInt8?: boolean;
-}
-
-import { ConnectionOptions } from "tls";
-
-export interface ClientConfig extends ConnectionConfig {
-    ssl?: boolean | ConnectionOptions;
 }
 
 export interface PoolConfig extends ClientConfig {
