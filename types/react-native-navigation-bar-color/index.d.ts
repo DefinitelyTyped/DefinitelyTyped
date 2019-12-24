2618590886a8c7319933945118660814b9f72b53
Haseeb Majid <hmajid2301@gmail.com>
2019-12-24 11:15:13 +0000

    Added react-native-navigation-bar-color
    Added react-native-navigation-bar-color.

---
+++ types/react-native-navigation-bar-color/index.d.ts
@@ -0,0 +1,8 @@
+// Type definitions for react-native-navigation-bar-color 1.0
+// Project: https://github.com/thebylito/react-native-navigation-bar-color#readme
+// Definitions by: Haseeb Majid <https://github.com/hmajid2301>
+// Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
+
+export default function changeNavigationBarColor(color: string, light?: boolean, animated?: boolean): Promise<void>;
+export function HideNavigationBar(): void;
+export function ShowNavigationBar(): void;
---
+++ types/react-native-navigation-bar-color/react-native-navigation-bar-color-tests.tsx
@@ -0,0 +1,57 @@
+import * as React from 'react';
+import { View, Text, Button } from 'react-native';
+import changeNavigationBarColor, { HideNavigationBar, ShowNavigationBar } from 'react-native-navigation-bar-color';
+
+export default class Mynewapp extends React.Component {
+    setNavigationColor = (color: string) => {
+        changeNavigationBarColor(color);
+    };
+
+    setNavBar = (color: string) => {
+        changeNavigationBarColor(color, true, true);
+    };
+
+    hideNavigation() {
+        HideNavigationBar();
+    }
+
+    showNavigation() {
+        ShowNavigationBar();
+    }
+
+    render() {
+        return (
+            <View
+                style={{
+                    flex: 1,
+                    justifyContent: 'space-around',
+                    alignContent: 'center',
+                    alignItems: 'center',
+                    backgroundColor: 'white',
+                }}
+            >
+                <Button
+                    title="Set color red"
+                    onPress={() => {
+                        this.setNavigationColor('red');
+                    }}
+                />
+                <Button
+                    title="Set color blue"
+                    onPress={() => {
+                        this.setNavigationColor('blue');
+                    }}
+                />
+                <Button
+                    title="Set color ligth"
+                    onPress={() => {
+                        changeNavigationBarColor('#ffffff', true);
+                    }}
+                />
+                <Button title="Hide bar" onPress={this.hideNavigation} />
+                <Button title="Show bar" onPress={this.showNavigation} />
+                <Text>Hello Word!</Text>
+            </View>
+        );
+    }
+}
