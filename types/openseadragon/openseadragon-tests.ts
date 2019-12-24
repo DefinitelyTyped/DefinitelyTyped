b599168b1ab21c2586325b317f5d882ef6171779
Kristin Ruben <kristin.ruben@pathai.com>
2019-12-24 11:16:25 -0500

    Augment test file, clean up tsconfig

--- types/openseadragon/openseadragon-tests.ts
+++ types/openseadragon/openseadragon-tests.ts
@@ -1,3 +1,11 @@
-import Viewer from 'openseadragon';
+import Viewer, { Drawer, Viewport, MouseTracker } from 'openseadragon';
 
 const viewer = new Viewer({ id: "viewerid" });
+
+const viewport = new Viewport({ margins: {} });
+
+const element = new Element();
+
+const drawer = new Drawer({ viewer, viewport, element });
+
+const mouseTracker = new MouseTracker({ element });
