598a382e56fd057ba8fc00124a30139b619b01b5
Dimitar <dim>
2019-12-25 17:16:23 +0200

    Adds contributor

--- types/redux-saga-tester/index.d.ts
+++ types/redux-saga-tester/index.d.ts
@@ -1,6 +1,8 @@
 // Type definitions for redux-saga-tester 1.0
 // Project: https://github.com/wix/redux-saga-tester#readme
-// Definitions by: Ben Lorantfy <https://github.com/BenLorantfy>, Law Smith <https://github.com/lawsumisu>
+// Definitions by: Ben Lorantfy <https://github.com/BenLorantfy>, 
+//                 Law Smith <https://github.com/lawsumisu>,
+//                 Dimitar Mitov <https://github.com/dmitov>
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
 // TypeScript Version: 2.4
 
