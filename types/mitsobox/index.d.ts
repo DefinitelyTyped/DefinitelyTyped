a5bc0a08743db901c460fecb04ad235719148ed7
Clément PONT <5255256+mrclemds@users.noreply.github.com>
2019-12-26 17:20:40 +0100

    Add mitsobox (#41121)
    * Add mitsobox
    
    * Apply correction requested by andrewbranch

---
+++ types/mitsobox/index.d.ts
@@ -0,0 +1,31 @@
+// Type definitions for mitsobox 0.1
+// Project: https://github.com/jimfilippou/mitsobox
+// Definitions by: MrClemds <https://github.com/MrClemds>
+// Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
+
+/**
+ * Create a win32 messageBox ok dialog
+ *
+ * @param title The title of the dialog
+ * @param message The message shown in this box
+ * @returns A promise witch resolve when the dialog is closed
+ */
+export function ok(title: string, message: string): Promise<void>;
+
+/**
+ * Create a win32 messageBox okCancel dialog
+ *
+ * @param title The title of the dialog
+ * @param message The message shown in this box
+ * @returns A promise witch resolve when the dialog is closed and gives you the button clicked
+ */
+export function okCancel(title: string, message: string): Promise<'OK' | 'CANCEL'>;
+
+/**
+ * Create a win32 messageBox abortRetryIgnore dialog
+ *
+ * @param title The title of the dialog
+ * @param message The message shown in this box
+ * @returns A promise witch resolve when the dialog is closed and gives you the button clicked
+ */
+export function abortRetryIgnore(title: string, message: string): Promise<'ABORT' | 'RETRY' | 'IGNORE'>;
