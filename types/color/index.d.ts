e9fb19757107b10248c048e0fc4930f3a8a46b32
Adam <adam.lj.haglund@gmail.com>
2019-12-26 21:57:25 +0100

    add to contributors

--- types/color/index.d.ts
+++ types/color/index.d.ts
@@ -1,6 +1,8 @@
 // Type definitions for color 3.0
 // Project: https://github.com/qix-/color#readme
-// Definitions by: Junyoung Clare Jang <https://github.com/Airlun>, James W. Lane <https://github.com/jameswlane>
+// Definitions by: Junyoung Clare Jang <https://github.com/Airlun>
+//                 James W. Lane <https://github.com/jameswlane>
+//                 Adam Haglund <https://github.com/BeeeQueue>
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
 // TypeScript Version: 2.3
 
