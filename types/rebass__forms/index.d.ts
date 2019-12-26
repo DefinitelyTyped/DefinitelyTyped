ab0604bea76423f8403d077dd15910569303e4e9
Jinho Jeong <jjinho0203@gmail.com>
2019-12-26 18:45:57 +0900

    Add @rebass/forms path mappings

--- types/rebass__forms/index.d.ts
+++ types/rebass__forms/index.d.ts
@@ -8,42 +8,46 @@ import * as React from 'react';
 import * as Rebass from 'rebass';
 import * as StyledSystem from 'styled-system';
 
-declare module '@rebass/forms' {
-    interface BoxKnownProps
-        extends Rebass.BaseProps,
-            StyledSystem.SpaceProps,
-            StyledSystem.LayoutProps,
-            StyledSystem.FontSizeProps,
-            StyledSystem.ColorProps,
-            StyledSystem.FlexProps,
-            StyledSystem.OrderProps,
-            StyledSystem.AlignSelfProps,
-            Rebass.SxProps {
-        variant?: StyledSystem.ResponsiveValue<string>;
-        tx?: string;
-    }
+interface BoxKnownProps
+    extends Rebass.BaseProps,
+        StyledSystem.SpaceProps,
+        StyledSystem.LayoutProps,
+        StyledSystem.FontSizeProps,
+        StyledSystem.ColorProps,
+        StyledSystem.FlexProps,
+        StyledSystem.OrderProps,
+        StyledSystem.AlignSelfProps,
+        Rebass.SxProps {
+    variant?: StyledSystem.ResponsiveValue<string>;
+    tx?: string;
+}
 
-    interface LabelProps extends BoxKnownProps, Omit<React.HTMLProps<HTMLLabelElement>, keyof BoxKnownProps> {}
+declare module '@rebass/forms' {
+    export interface LabelProps extends BoxKnownProps, Omit<React.HTMLProps<HTMLLabelElement>, keyof BoxKnownProps> {}
 
     export const Label: React.FunctionComponent<LabelProps>;
 
-    interface InputProps extends BoxKnownProps, Omit<React.HTMLProps<HTMLInputElement>, keyof BoxKnownProps> {}
+    export interface InputProps extends BoxKnownProps, Omit<React.HTMLProps<HTMLInputElement>, keyof BoxKnownProps> {}
 
     export const Input: React.FunctionComponent<InputProps>;
 
-    interface SelectProps extends BoxKnownProps, Omit<React.HTMLProps<HTMLSelectElement>, keyof BoxKnownProps> {}
+    export interface SelectProps extends BoxKnownProps, Omit<React.HTMLProps<HTMLSelectElement>, keyof BoxKnownProps> {}
 
     export const Select: React.FunctionComponent<SelectProps>;
 
-    interface TextareaProps extends BoxKnownProps, Omit<React.HTMLProps<HTMLTextAreaElement>, keyof BoxKnownProps> {}
+    export interface TextareaProps
+        extends BoxKnownProps,
+            Omit<React.HTMLProps<HTMLTextAreaElement>, keyof BoxKnownProps> {}
 
     export const Textarea: React.FunctionComponent<TextareaProps>;
 
-    interface RadioProps extends BoxKnownProps, Omit<React.HTMLProps<HTMLInputElement>, keyof BoxKnownProps> {}
+    export interface RadioProps extends BoxKnownProps, Omit<React.HTMLProps<HTMLInputElement>, keyof BoxKnownProps> {}
 
     export const Radio: React.FunctionComponent<RadioProps>;
 
-    interface CheckboxProps extends BoxKnownProps, Omit<React.HTMLProps<HTMLInputElement>, keyof BoxKnownProps> {}
+    export interface CheckboxProps
+        extends BoxKnownProps,
+            Omit<React.HTMLProps<HTMLInputElement>, keyof BoxKnownProps> {}
 
     export const Checkbox: React.FunctionComponent<CheckboxProps>;
 }
