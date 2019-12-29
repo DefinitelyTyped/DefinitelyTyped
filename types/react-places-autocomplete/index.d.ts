f9bbc02f72ab5d6a82eed85732b60a4377706575
Mark Nelissen <mark.nelissen@gemsotec.com>
2019-12-29 11:05:44 +0100

    update typescript version

--- types/react-places-autocomplete/index.d.ts
+++ types/react-places-autocomplete/index.d.ts
@@ -6,7 +6,7 @@
 //                 Aziz Khambati <https://github.com/azizhk>
 //                 Mark Nelissen <https://github.com/marknelissen>
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
-// TypeScript Version: 2.8
+// TypeScript Version: 3.7
 //
 /// <reference types="googlemaps" />
 import * as React from 'react';
