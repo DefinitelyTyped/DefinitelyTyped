807a4d8719a879f513b44dd2167b8828d1007d3a
Robin Giese <robin@grumpycorp.com>
2019-12-29 12:42:46 -0800

    Fix typings for Flatbuffers v1.10+ release

--- types/flatbuffers/index.d.ts
+++ types/flatbuffers/index.d.ts
@@ -1,6 +1,7 @@
 // Type definitions for flatbuffers 1.9
 // Project: http://google.github.io/flatbuffers/index.html
 // Definitions by: Kamil Rojewski <kamil.rojewski@gmail.com>
+//                 Robin Giese <robin@grumpycorp.com>
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
 
 export { flatbuffers };
@@ -17,6 +18,7 @@ declare global {
     const SIZEOF_SHORT: number;
     const SIZEOF_INT: number;
     const FILE_IDENTIFIER_LENGTH: number;
+    const SIZE_PREFIX_LENGTH: number;
 
     enum Encoding { UTF8_BYTES, UTF16_STRING }
 
@@ -167,7 +169,8 @@ declare global {
        */
       endObject(): Offset;
 
-      finish(root_table: Offset, file_identifier?: string): void;
+      finish(root_table: Offset, file_identifier?: string, size_prefix?: boolean): void;
+      finishSizePrefixed(root_table: Offset, file_identifier?: string): void;
 
       /**
        * This checks a required field has been set in a given table that has
@@ -205,7 +208,7 @@ declare global {
       createString(s: string|Uint8Array): Offset;
 
       /**
-       * Conveniance function for creating Long objects.
+       * Convenience function for creating Long objects.
        */
       createLong(low: number, high: number): Long;
     }
@@ -293,7 +296,7 @@ declare global {
       __has_identifier(ident: string): boolean;
 
       /**
-       * Conveniance function for creating Long objects.
+       * Convenience function for creating Long objects.
        */
       createLong(low: number, high: number): Long;
     }
