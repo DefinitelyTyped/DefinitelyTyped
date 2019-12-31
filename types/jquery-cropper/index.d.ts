6c2b992865564747a0e2a71ae4d6846251fe1ec3
Mustafa Salaheldin <mustafa.salaheldin@yahoo.com>
2019-12-31 10:41:53 +0400

    commit jquery-croper type definitions
    commit jquery-croper type definitions

---
+++ types/jquery-cropper/index.d.ts
@@ -0,0 +1,60 @@
+// Type definitions for jquery-cropper 0.9
+// Project: https://github.com/davidstutz/jquery-cropper
+// Definitions by: Mustafa Salaheldin <https://github.com/mustafasalahuldin>
+// Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
+// TypeScript Version: 2.3
+
+/// <reference types="jquery"/>
+
+/**
+ * See {@link https://davidstutz.github.io/jquery-cropper/#configuration-options|https://davidstutz.github.io/jquery-cropper/#configuration-options}
+ */
+interface ICropperOptions {
+    viewMode?: number;
+    dragMode?: string;
+    initialAspectRatio?: number;
+    aspectRatio?: number;
+    data?: any;
+    preview?: any;
+    responsive?: boolean;
+    restore?: boolean;
+    checkCrossOrigin?: boolean;
+    checkOrientation?: string;
+    modal?: boolean;
+    guides?: boolean;
+    center?: boolean;
+    highlight?: boolean;
+    background?: boolean;
+    autoCrop?: boolean;
+    autoCropArea?: number;
+    movable?: boolean;
+    rotatable?: boolean;
+    scalable?: boolean;
+    zoomable?: boolean;
+    zoomOnTouch?: boolean;
+    zoomOnWheel?: boolean;
+    wheelZoomRatio?: number;
+    cropBoxMovable?: number;
+    cropBoxResizable?: number;
+    toggleDragModeOnDblclick?: number;
+    minContainerWidth?: number;
+    minContainerHeight?: number;
+    minCanvasWidth?: number;
+    minCanvasHeight?: number;
+    minCropBoxWidth?: number;
+    minCropBoxHeight?: number;
+    ready?(): any;
+    cropstart?(): any;
+    cropmove?(): any;
+    cropend?(): any;
+    crop?(data?: any): void;
+    zoom?(): any;
+}
+
+interface JQuery {
+    cropper(options?: ICropperOptions): JQuery;
+    cropper(method: string, ...arguments: any[]): JQuery;
+}
+
+declare module "jquery-cropper" {
+}
