ca73215c062b8216ab3a53c8b49f2297d1425202
Piotr Błażejewicz (Peter Blazejewicz) <peterblazejewicz@users.noreply.github.com>
2020-01-02 18:46:49 +0100

    feat(react-stripe-elements): `handleCardAction`. Closes #41281 (#41285)
    https://stripe.com/docs/js/payment_intents/handle_card_action
    https://git.io/JeNlO
    
    Thanks!

--- types/react-stripe-elements/index.d.ts
+++ types/react-stripe-elements/index.d.ts
@@ -50,6 +50,12 @@ export namespace ReactStripeElements {
             paymentMethodType: stripe.paymentMethod.paymentMethodType,
             data?: stripe.CreatePaymentMethodOptions,
         ): Promise<stripe.PaymentMethodResponse>;
+        /**
+         * Use `stripe.handleCardAction` in the Payment Intents API manual confirmation flow
+         * to handle a PaymentIntent with the requires_action status.
+         * It will throw an error if the PaymentIntent has a different status.
+         */
+        handleCardAction(clientSecret: string): Promise<stripe.PaymentIntentResponse>;
         handleCardPayment(
             clientSecret: string,
             options?: stripe.HandleCardPaymentWithoutElementsOptions,
