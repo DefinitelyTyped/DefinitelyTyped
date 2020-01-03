0e67446c817a936a479951c66b76701a9ca5c0e3
Richie Bendall <richiebendall@gmail.com>
2020-01-03 15:25:57 +1300

    chore: Update expected types
    Signed-off-by: Richie Bendall <richiebendall@gmail.com>

--- types/nice-try/nice-try-tests.ts
+++ types/nice-try/nice-try-tests.ts
@@ -1,6 +1,6 @@
 import niceTry = require("nice-try");
 
-niceTry(() => true); // $ExpectType boolean | void
-niceTry(() => (1).toString(999)); // $ExpectType string | void
-niceTry(); // $ExpectType void
-niceTry(true); // $ExpectType void
+niceTry(() => true); // $ExpectType boolean | undefined
+niceTry(() => (1).toString(999)); // $ExpectType string | undefined
+niceTry(); // $ExpectType undefined
+niceTry(true); // $ExpectType undefined
