c377441c7d205d5c3acaeadd654dbc2aac2c37f9
Caleb <calebegg@google.com>
2020-01-03 10:24:23 -0500

    Update comment

--- types/webmidi/index.d.ts
+++ types/webmidi/index.d.ts
@@ -1,6 +1,5 @@
 // Type definitions for Web MIDI API 2.0
-// Project: http://www.w3.org/TR/webmidi/, https://github.com/djipco/webmidi
-// Definitions by: six a <https://github.com/lostfictions>
+// Project: http://www.w3.org/TR/webmidi/
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
 
 interface Navigator {
