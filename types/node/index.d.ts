98f56b49362fb6c179ad986219af8d240616339e
Andrew Neitsch <andrew@neitsch.ca>
2020-01-03 16:22:29 -0700

    Fix typo (#41349)

--- types/node/index.d.ts
+++ types/node/index.d.ts
@@ -59,7 +59,7 @@
 
 // Forward-declarations for needed types from es2015 and later (in case users are using `--lib es5`)
 // Empty interfaces are used here which merge fine with the real declarations in the lib XXX files
-// just to ensure the names are known and node typings can be sued without importing these libs.
+// just to ensure the names are known and node typings can be used without importing these libs.
 // if someone really needs these types the libs need to be added via --lib or in tsconfig.json
 interface AsyncIterable<T> { }
 interface IterableIterator<T> { }
