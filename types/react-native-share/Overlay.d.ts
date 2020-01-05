e44ee7b1d26dac8f53ed315daddb0ada41ee7dd9
Jesse Katsumata <jesse.katsumata@gmail.com>
2020-01-05 10:59:44 +0900

    react-native-share: apply lint

--- types/react-native-share/Overlay.d.ts
+++ types/react-native-share/Overlay.d.ts
@@ -1,20 +1,20 @@
-import {Component, ReactNode } from 'react'
-import { Animated, ViewProps ,StyleProp} from 'react-native'
-export type OverlayState ={
-  fadeAnim: Animated.Value
-  overlayStyle: StyleProp<ViewProps>
-}
+import { Component, ReactNode } from 'react';
+import { Animated, ViewProps, StyleProp } from 'react-native';
+export type OverlayState = {
+    fadeAnim: Animated.Value;
+    overlayStyle: StyleProp<ViewProps>;
+};
 
 export type OverlayProps = {
-  visible: boolean,
-  onCancel: ()=>void,
-  children: ReactNode
+    visible: boolean;
+    onCancel: () => void;
+    children: ReactNode;
+};
+
+declare class Overlay extends Component<OverlayProps, OverlayState> {
+    onAnimatedEnd(): void;
+    UNSAFE_componentWillReceiveProps(newProps: OverlayProps): void;
+    render(): JSX.Element;
 }
 
-declare class Overlay extends Component<OverlayProps, OverlayState>{
-  onAnimatedEnd(): void;
-  UNSAFE_componentWillReceiveProps(newProps: OverlayProps): void;
-  render(): JSX.Element;
-}
-
-export default Overlay
+export default Overlay;
--- types/react-native-share/Sheet.d.ts
+++ types/react-native-share/Sheet.d.ts
@@ -1,18 +1,18 @@
-import {Component, ReactNode } from 'react'
-import { Animated, ViewProps } from 'react-native'
+import { Component, ReactNode } from 'react';
+import { Animated, ViewProps } from 'react-native';
 
 export type SheetProps = {
-  visible: boolean,
-  children: ReactNode
+    visible: boolean;
+    children: ReactNode;
+};
+
+export type SheetState = {
+    bottom: Animated.Value;
+};
+
+declare class Sheet extends Component<SheetProps, SheetState> {
+    UNSAFE_componentWillReceiveProps(newProps: SheetProps): void;
+    render(): JSX.Element;
 }
 
-export type SheetState ={
-  bottom: Animated.Value
-  }
-
-declare class Sheet extends Component<SheetProps, SheetState>{
-  UNSAFE_componentWillReceiveProps(newProps: SheetProps): void;
-  render(): JSX.Element;
-}
-
-export default Sheet
+export default Sheet;
