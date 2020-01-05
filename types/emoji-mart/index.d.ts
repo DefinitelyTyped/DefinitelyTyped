9e1c31cb61e3990ae8a1a9b33fd8408b5963be82
seoyunho <yunho101775@gmail.com>
2020-01-05 17:52:00 +0900

    update getEmojiDataFromNative util type

--- types/emoji-mart/index.d.ts
+++ types/emoji-mart/index.d.ts
@@ -3,6 +3,7 @@
 // Definitions by: Jessica Franco <https://github.com/Jessidhia>
 //                 Nick Winans <https://github.com/Nicell>
 //                 Elvis Wolcott <https://github.com/elvis-wolcott>
+//                 Yunho Seo <https://github.com/seoyunho>
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
 // TypeScript Version: 2.9
 
