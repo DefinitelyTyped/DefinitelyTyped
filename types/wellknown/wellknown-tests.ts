78b4a727fe456ca53114900967d05fac9f6d260b
Davide <scalzodavide@yahoo.it>
2020-01-06 12:43:46 +0000

    fixed types for mapbox/wellknown

--- types/wellknown/wellknown-tests.ts
+++ types/wellknown/wellknown-tests.ts
@@ -1,6 +1,6 @@
 import * as wellknown from 'wellknown';
 wellknown.parse("POINT(1 2)");
-const geoJson: {} = {
+const geoJson: wellknown.GeoJSONGeometry = {
     coordinates: [1, 2],
     type: "Point"
 };
