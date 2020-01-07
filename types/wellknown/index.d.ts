ccb4930b0489b26d1c7b6565a5f016c085f57d3e
Davide <scalzodavide@yahoo.it>
2020-01-07 10:49:05 +0000

    added author, added null return type and test

--- types/wellknown/index.d.ts
+++ types/wellknown/index.d.ts
@@ -1,24 +1,26 @@
 // Type definitions for wellknown 0.5
 // Project: https://github.com/mapbox/wellknown#readme
 // Definitions by: Davide Scalzo <https://github.com/davodesign84>
+//                 Yair Tawil <https://github.com/yairtawil>
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
 
 export type GeoJSONPosition = [number, number] | [number, number, number];
+
 export interface Geometry<T, C> {
     type: T;
     coordinates: C;
 }
 
-export type GeoJSONPoint = Geometry<'Point', GeoJSONPosition>;
-export type GeoJSONMultiPoint = Geometry<'MultiPoint', GeoJSONPosition[]>;
+export type GeoJSONPoint = Geometry<"Point", GeoJSONPosition>;
+export type GeoJSONMultiPoint = Geometry<"MultiPoint", GeoJSONPosition[]>;
 
-export type GeoJSONLineString = Geometry<'LineString', GeoJSONPosition[]>;
+export type GeoJSONLineString = Geometry<"LineString", GeoJSONPosition[]>;
 
-export type GeoJSONMultiLineString = Geometry<'MultiLineString', GeoJSONPosition[][]>;
+export type GeoJSONMultiLineString = Geometry<"MultiLineString", GeoJSONPosition[][]>;
 
-export type GeoJSONPolygon = Geometry<'Polygon', GeoJSONPosition[][]>;
+export type GeoJSONPolygon = Geometry<"Polygon", GeoJSONPosition[][]>;
 
-export type GeoJSONMultiPolygon = Geometry<'MultiPolygon', GeoJSONPosition[][][]>;
+export type GeoJSONMultiPolygon = Geometry<"MultiPolygon", GeoJSONPosition[][][]>;
 
 export type GeoJSONGeometry =
     | GeoJSONPoint
@@ -28,5 +30,5 @@ export type GeoJSONGeometry =
     | GeoJSONPolygon
     | GeoJSONMultiPolygon;
 
-export function parse(input: string): GeoJSONGeometry;
+export function parse(input: string): GeoJSONGeometry | null;
 export function stringify(gj: GeoJSONGeometry): string;
--- types/wellknown/wellknown-tests.ts
+++ types/wellknown/wellknown-tests.ts
@@ -1,7 +1,10 @@
 import * as wellknown from 'wellknown';
+
+// $ExpectType GeoJSONGeometry
 wellknown.parse("POINT(1 2)");
+
 const geoJson: wellknown.GeoJSONGeometry = {
     coordinates: [1, 2],
     type: "Point"
 };
-wellknown.stringify(geoJson);
+wellknown.stringify(geoJson); // $ExpectType string
