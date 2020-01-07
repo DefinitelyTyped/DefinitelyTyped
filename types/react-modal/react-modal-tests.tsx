d8748655591bd23631b0d962461afbb4280d5660
Juwan Wheatley <datwheat@gmail.com>
2020-01-07 14:40:24 -0800

    [react-modal] revert prettier styling changes

--- types/react-modal/react-modal-tests.tsx
+++ types/react-modal/react-modal-tests.tsx
@@ -1,86 +1,86 @@
-import * as React from 'react';
+import * as React from "react";
 import ReactModal = require('react-modal');
 
 // tests for static method
 // string
-ReactModal.setAppElement('#main');
+ReactModal.setAppElement("#main");
 // HTMLElement
-ReactModal.setAppElement(document.getElementById('#main'));
+ReactModal.setAppElement(document.getElementById("#main"));
 
 class ExampleOfUsingReactModal extends React.Component {
-    contentRef: HTMLDivElement;
-    overlayRef: HTMLDivElement;
-    render() {
-        const onAfterOpenFn = () => {};
-        const onAfterCloseFn = () => {};
-        const onRequestCloseFn = (event: React.MouseEvent | React.KeyboardEvent) => {};
-        const customStyle: { overlay: React.CSSProperties; content: React.CSSProperties } = {
-            overlay: {
-                position: 'fixed',
-                top: 0,
-                left: 0,
-                right: 0,
-                bottom: 0,
-                backgroundColor: 'rgba(255, 255, 255, 0.75)',
-            },
-            content: {
-                position: 'absolute',
-                top: '40px',
-                left: '40px',
-                right: '40px',
-                bottom: '40px',
-                border: '1px solid #ccc',
-                background: '#fff',
-                overflow: 'auto',
-                WebkitOverflowScrolling: 'touch',
-                borderRadius: '4px',
-                outline: 'none',
-                padding: '20px',
-            },
-        };
-        const customClasses = {
-            afterOpen: 'afterOpen',
-            base: 'base',
-            beforeClose: 'beforeClose',
-        };
-        const customOverlayClasses = {
-            afterOpen: 'afterOpen',
-            base: 'base',
-            beforeClose: 'beforeClose',
-        };
-        const customAriaVariables = {
-            labelledby: 'labelledby',
-            describedby: 'describedby',
-            modal: true,
-        };
-        const customDataVariables = {
-            dataOne: 'one',
-            dataTwo: 'two',
-        };
-        return (
-            <ReactModal
-                isOpen={true}
-                onAfterOpen={onAfterOpenFn}
-                onAfterClose={onAfterCloseFn}
-                onRequestClose={onRequestCloseFn}
-                contentLabel="demo label"
-                closeTimeoutMS={1000}
-                style={customStyle}
-                className={customClasses}
-                overlayClassName={customOverlayClasses}
-                bodyOpenClassName={'bodyOpenClassName'}
-                htmlOpenClassName={'htmlOpenClassName'}
-                aria={customAriaVariables}
-                data={customDataVariables}
-                contentRef={instance => (this.contentRef = instance)}
-                overlayRef={instance => (this.overlayRef = instance)}
-                testId="modal-content"
-            >
-                <h1>Modal Content</h1>
-                <p>Etc.</p>
-            </ReactModal>
-        );
-    }
+  contentRef: HTMLDivElement;
+  overlayRef: HTMLDivElement;
+  render() {
+    const onAfterOpenFn = () => { };
+    const onAfterCloseFn = () => { };
+    const onRequestCloseFn = (event: React.MouseEvent | React.KeyboardEvent) => { };
+    const customStyle: { overlay: React.CSSProperties, content: React.CSSProperties } = {
+      overlay: {
+        position: 'fixed',
+        top: 0,
+        left: 0,
+        right: 0,
+        bottom: 0,
+        backgroundColor: 'rgba(255, 255, 255, 0.75)'
+      },
+      content: {
+        position: 'absolute',
+        top: '40px',
+        left: '40px',
+        right: '40px',
+        bottom: '40px',
+        border: '1px solid #ccc',
+        background: '#fff',
+        overflow: 'auto',
+        WebkitOverflowScrolling: 'touch',
+        borderRadius: '4px',
+        outline: 'none',
+        padding: '20px'
+      }
+    };
+    const customClasses = {
+      afterOpen: 'afterOpen',
+      base: 'base',
+      beforeClose: 'beforeClose'
+    };
+    const customOverlayClasses = {
+      afterOpen: 'afterOpen',
+      base: 'base',
+      beforeClose: 'beforeClose'
+    };
+    const customAriaVariables = {
+      labelledby: 'labelledby',
+      describedby: 'describedby',
+      modal: true,
+    };
+    const customDataVariables = {
+      dataOne: 'one',
+      dataTwo: 'two'
+    };
+    return (
+      <ReactModal
+        isOpen={true}
+        onAfterOpen={onAfterOpenFn}
+        onAfterClose={onAfterCloseFn}
+        onRequestClose={onRequestCloseFn}
+        contentLabel="demo label"
+        closeTimeoutMS={1000}
+        style={customStyle}
+        className={customClasses}
+        overlayClassName={customOverlayClasses}
+        bodyOpenClassName={'bodyOpenClassName'}
+        htmlOpenClassName={'htmlOpenClassName'}
+        aria={customAriaVariables}
+        data={customDataVariables}
+        contentRef={instance => this.contentRef = instance}
+        overlayRef={instance => this.overlayRef = instance}
+        testId="modal-content"
+        >
+        <h1>Modal Content</h1>
+        <p>Etc.</p>
+      </ReactModal>
+    );
+  }
 }
 
 const MyWrapperComponent: React.FC = () => {
@@ -90,9 +90,5 @@ const MyWrapperComponent: React.FC = () => {
         reactModaRef.current.portal.content.focus();
     });
 
-    return (
-        <ReactModal isOpen ref={reactModaRef}>
-            Hello, World!
-        </ReactModal>
-    );
+    return <ReactModal isOpen ref={reactModaRef}>Hello, World!</ReactModal>;
 };
