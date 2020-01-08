02dea6062f6d2e8b875639e56bcfa16972efe242
ExE Boss <3889017+ExE-Boss@users.noreply.github.com>
2020-01-08 01:58:23 +0100

    feat: Add `domexception` (#41390)
    * feat: Add `domexception`
    
    * fix(domexception): Hide internal implementation details

---
+++ types/domexception/index.d.ts
@@ -0,0 +1,7 @@
+// Type definitions for domexception 2.0
+// Project: https://github.com/jsdom/domexception#readme
+// Definitions by: ExE Boss <https://github.com/ExE-Boss>
+// Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
+// TypeScript Version: 3.0
+
+export = DOMException;
