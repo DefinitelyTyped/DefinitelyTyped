90a230126914c361c1b0fffe7426b5acd2de3a88
Crocsx <Crocsx.gaming@gmail.com>
2020-01-08 15:12:42 +0900

    version number

--- types/cesium/index.d.ts
+++ types/cesium/index.d.ts
@@ -1,4 +1,4 @@
-// Type definitions for cesium 1.59
+// Type definitions for cesium 1.65
 // Project: http://cesiumjs.org
 // Definitions by: Aigars Zeiza <https://github.com/Zuzon>
 //                 Harry Nicholls <https://github.com/hnipps>
