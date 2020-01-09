10adbaa1b1c71c68cbe97937f31110c01159c022
Robby Helms <robby@pocketprep.com>
2020-01-09 15:22:12 -0500

    [parse] Improve intellisense for UserConstructor

--- types/parse/index.d.ts
+++ types/parse/index.d.ts
@@ -825,7 +825,7 @@ namespace Parse {
     }
     interface UserConstructor extends ObjectStatic {
         new <T extends Attributes>(attributes: T): User<T>;
-        new(): User;
+        new(attributes?: Attributes): User;
 
         allowCustomUserClass(isAllowed: boolean): void;
         become(sessionToken: string, options?: UseMasterKeyOption): Promise<User>;
