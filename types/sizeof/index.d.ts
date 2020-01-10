17cb4a9032591ac2c47e2c9ea97aa12f9ebc761a
Daniel Perez Alvarez <unindented@gmail.com>
2020-01-10 15:04:57 -0800

    Improve type definitions for sizeof (#41517)

--- types/sizeof/index.d.ts
+++ types/sizeof/index.d.ts
@@ -3,5 +3,6 @@
 // Definitions by: Daniel Perez Alvarez <https://github.com/unindented>
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
 
-export function sizeof(object: object, pretty?: boolean): number | string;
+export function sizeof(object: any, pretty?: false): number;
+export function sizeof(object: any, pretty: true): string;
 export function format(bytes: number): string;
