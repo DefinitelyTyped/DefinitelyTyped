c165a32207316b0d14329d361fc901b7f256c90e
ksocha <kamil.socha@interia.eu>
2020-01-10 23:12:13 +0100

    Add lazy-brush definitions

---
+++ types/lazy-brush/lazy-brush-tests.ts
@@ -0,0 +1,39 @@
+import { LazyBrush, Point, LazyPoint, Coordinates } from 'lazy-brush';
+
+const point = new Point(1, 2);
+const pointX: number = point.x;
+const pointY: number = point.y;
+
+const lazyPoint = new LazyPoint(1, 2);
+const lazyPointX: number = lazyPoint.x;
+const lazyPointY: number = lazyPoint.y;
+const equalsToResult: boolean = lazyPoint.equalsTo({ x: 1, y: 4 });
+const getAngleToResult: number = lazyPoint.getAngleTo(point);
+const getDifferenceToResult: Point = lazyPoint.getDifferenceTo(point);
+const getDistanceToResult: number = lazyPoint.getDistanceTo(point);
+lazyPoint.moveByAngle(45, 20);
+const toObjectResult: Coordinates = lazyPoint.toObject();
+lazyPoint.update({ x: 1, y: 7 });
+
+new LazyBrush();
+new LazyBrush({ radius: 20 });
+const lazy = new LazyBrush({
+    radius: 30,
+    enabled: true,
+    initialPoint: { x: 0, y: 0 },
+});
+
+const updateResult: boolean = lazy.update({ x: 50, y: 0 });
+const updateResult2: boolean = lazy.update({ x: 50, y: 0 }, { both: true });
+const brushHasMovedResult: boolean = lazy.brushHasMoved();
+lazy.disable();
+lazy.enable();
+const isEnabledResult: boolean = lazy.isEnabled();
+const getAngleResult: number = lazy.getAngle();
+const getBrushResult: LazyPoint = lazy.getBrush();
+const getBrushCoordinatesResult: Coordinates = lazy.getBrushCoordinates();
+const getDistanceResult: number = lazy.getDistance();
+const getPointerResult: LazyPoint = lazy.getPointer();
+const getPointerCoordinatesResult: Coordinates = lazy.getPointerCoordinates();
+const getRadiusResult: number = lazy.getRadius();
+lazy.setRadius(12);
