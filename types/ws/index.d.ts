b2b339913d417b298059f7263287f4046b26c9aa
Max Sirenev <34866475+teidesu@users.noreply.github.com>
2020-01-11 16:17:54 +0300

    fixed event name

--- types/ws/index.d.ts
+++ types/ws/index.d.ts
@@ -234,13 +234,13 @@ declare namespace WebSocket {
         on(event: 'connection', cb: (this: WebSocket, socket: WebSocket, request: http.IncomingMessage) => void): this;
         on(event: 'error', cb: (this: WebSocket, error: Error) => void): this;
         on(event: 'headers', cb: (this: WebSocket, headers: string[], request: http.IncomingMessage) => void): this;
-        on(event: 'closed' | 'listening', cb: (this: WebSocket) => void): this;
+        on(event: 'close' | 'listening', cb: (this: WebSocket) => void): this;
         on(event: string | symbol, listener: (this: WebSocket, ...args: any[]) => void): this;
 
         addListener(event: 'connection', cb: (client: WebSocket) => void): this;
         addListener(event: 'error', cb: (err: Error) => void): this;
         addListener(event: 'headers', cb: (headers: string[], request: http.IncomingMessage) => void): this;
-        addListener(event: 'closed' | 'listening', cb: () => void): this;
+        addListener(event: 'close' | 'listening', cb: () => void): this;
         addListener(event: string | symbol, listener: (...args: any[]) => void): this;
     }
 
