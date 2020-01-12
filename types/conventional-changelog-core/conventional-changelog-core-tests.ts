c176934e4195626ffc1fe4042acca9e26dd2d774
Jason Kwok <JasonHK@users.noreply.github.com>
2020-01-12 14:30:57 +0800

    [conventional-changelog-core] Relocated `// $ExpectType`; altered the name of the namespace

--- types/conventional-changelog-core/conventional-changelog-core-tests.ts
+++ types/conventional-changelog-core/conventional-changelog-core-tests.ts
@@ -3,17 +3,24 @@
 
 import conventionalChangelogCore from "conventional-changelog-core";
 
-namespace Test {
+namespace Module {
     declare const context: conventionalChangelogCore.Context;
     declare const gitRawCommitsOpts: conventionalChangelogCore.GitRawCommitsOptions;
     declare const options: conventionalChangelogCore.Options;
     declare const parserOpts: conventionalChangelogCore.ParserOptions;
     declare const writerOpts: conventionalChangelogCore.WriterOptions;
 
-    conventionalChangelogCore(); // $ExpectType Readable
-    conventionalChangelogCore(options); // $ExpectType Readable
-    conventionalChangelogCore(options, context); // $ExpectType Readable
-    conventionalChangelogCore(options, context, gitRawCommitsOpts); // $ExpectType Readable
-    conventionalChangelogCore(options, context, gitRawCommitsOpts, parserOpts); // $ExpectType Readable
-    conventionalChangelogCore(options, context, gitRawCommitsOpts, parserOpts, writerOpts); // $ExpectType Readable
+    // $ExpectType Readable
+    conventionalChangelogCore();
+    // $ExpectType Readable
+    conventionalChangelogCore(options);
+    // $ExpectType Readable
+    conventionalChangelogCore(options, context);
+    // $ExpectType Readable
+    conventionalChangelogCore(options, context, gitRawCommitsOpts);
+    // $ExpectType Readable
+    conventionalChangelogCore(options, context, gitRawCommitsOpts, parserOpts);
+    // $ExpectType Readable
+    conventionalChangelogCore(options, context, gitRawCommitsOpts, parserOpts, writerOpts);
+    // $ExpectType Readable
 }
