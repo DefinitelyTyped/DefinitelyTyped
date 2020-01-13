d90c2050a9f73ef89adc998f83f952bb0a0053fe
Carson Farmer <carson.farmer@gmail.com>
2020-01-13 15:47:32 -0800

    fix: uses globally exported namespace in tests
    Signed-off-by: Carson Farmer <carson.farmer@gmail.com>

--- types/multicodec/multicodec-tests.ts
+++ types/multicodec/multicodec-tests.ts
@@ -1,56 +1,56 @@
-import multicodec from 'multicodec';
+// import Multicodec from 'multicodec';
 
 const buf = Buffer.from('hey');
 
-const prefixedBuf1 = multicodec.addPrefix('protobuf', buf);
-console.log(multicodec.getCodec(prefixedBuf1));
+const prefixedBuf1 = Multicodec.addPrefix('protobuf', buf);
+console.log(Multicodec.getCodec(prefixedBuf1));
 // protobuf
-console.log(multicodec.rmPrefix(prefixedBuf1) === buf);
+console.log(Multicodec.rmPrefix(prefixedBuf1) === buf);
 // true
 
-const prefixedBuf2 = multicodec.addPrefix(Buffer.from('70', 'hex'), buf);
-console.log(multicodec.getCodec(prefixedBuf2));
+const prefixedBuf2 = Multicodec.addPrefix(Buffer.from('70', 'hex'), buf);
+console.log(Multicodec.getCodec(prefixedBuf2));
 // dag-pb
-console.log(multicodec.rmPrefix(prefixedBuf2) === buf);
+console.log(Multicodec.rmPrefix(prefixedBuf2) === buf);
 // true
 
-const prefixedBuf3 = multicodec.addPrefix('eth-block', buf);
-console.log(multicodec.getCodec(prefixedBuf3));
+const prefixedBuf3 = Multicodec.addPrefix('eth-block', buf);
+console.log(Multicodec.getCodec(prefixedBuf3));
 // eth-block
-console.log(multicodec.rmPrefix(prefixedBuf2) === buf);
+console.log(Multicodec.rmPrefix(prefixedBuf2) === buf);
 // true
 
-const code1 = multicodec.getCodeVarint('keccak-256');
+const code1 = Multicodec.getCodeVarint('keccak-256');
 console.log(code1); // Equal to Buffer.from('1b', 'hex')
 
-const prefixedBuf4 = multicodec.addPrefix('dag-cbor', buf);
-const code2 = multicodec.getCode(prefixedBuf4);
-console.log(code2 === multicodec.DAG_CBOR);
+const prefixedBuf4 = Multicodec.addPrefix('dag-cbor', buf);
+const code2 = Multicodec.getCode(prefixedBuf4);
+console.log(code2 === Multicodec.DAG_CBOR);
 
-const code3 = multicodec.getVarint(multicodec.KECCAK_256);
+const code3 = Multicodec.getVarint(Multicodec.KECCAK_256);
 console.log(code3);
 // [0x1b]
 
-multicodec.getName(144); // 'eth-block'
-multicodec.getName(112); // 'dag-pb'
-multicodec.getName(0xb201); // 'blake2b-8'
+Multicodec.getName(144); // 'eth-block'
+Multicodec.getName(112); // 'dag-pb'
+Multicodec.getName(0xb201); // 'blake2b-8'
 
-multicodec.getNumber('eth-block'); // 144
-multicodec.getNumber('dag-pb'); // 112
+Multicodec.getNumber('eth-block'); // 144
+Multicodec.getNumber('dag-pb'); // 112
 
-multicodec.ETH_BLOCK; // $ExpectType 144
-multicodec.DAG_PB; // $ExpectType 112
-multicodec.BLAKE2B_8; // $ExpectType 45569
+Multicodec.ETH_BLOCK; // $ExpectType 144
+Multicodec.DAG_PB; // $ExpectType 112
+Multicodec.BLAKE2B_8; // $ExpectType 45569
 
-multicodec.print[144]; // $ExpectType string
-multicodec.print[112]; // $ExpectType string
-multicodec.print[0x0111]; // $ExpectType string
-multicodec.print[0xb201]; // $ExpectType string
+Multicodec.print[144]; // $ExpectType string
+Multicodec.print[112]; // $ExpectType string
+Multicodec.print[0x0111]; // $ExpectType string
+Multicodec.print[0xb201]; // $ExpectType string
 
-multicodec.print[multicodec.ETH_BLOCK]; // 'eth-block'
-multicodec.print[multicodec.DAG_PB]; // 'dag-pb'
-multicodec.print[multicodec.UDP]; // 'udp'
-multicodec.print[multicodec.BLAKE2B_8]; // 'blake2b-8'
+Multicodec.print[Multicodec.ETH_BLOCK]; // 'eth-block'
+Multicodec.print[Multicodec.DAG_PB]; // 'dag-pb'
+Multicodec.print[Multicodec.UDP]; // 'udp'
+Multicodec.print[Multicodec.BLAKE2B_8]; // 'blake2b-8'
 
 // `ipfs` and `p2p` are assigned to `0x01a5`, `ipfs` is deprecated
-multicodec.print[0x01a5]; // 'p2p'
+Multicodec.print[0x01a5]; // 'p2p'
