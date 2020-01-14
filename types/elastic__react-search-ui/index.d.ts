2430ea9ad5f3d44b182838dc86c7a70aceee7cb5
Alexander Atallah <alexander.atallah@gmail.com>
2020-01-14 12:48:46 -0500

    init react-search-ui types from template

---
+++ types/elastic__react-search-ui/index.d.ts
@@ -0,0 +1,33 @@
+// Type definitions for @elastic/react-search-ui 1.3
+// Project: https://github.com/elastic/search-ui#readme
+// Definitions by: Alex Atallah <https://github.com/alexanderatallah>
+// Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
+
+/*~ If this module has methods, declare them as functions like so.
+ */
+export function myMethod(a: string): string;
+export function myOtherMethod(a: number): number;
+
+/*~ You can declare types that are available via importing the module */
+export interface someType {
+    name: string;
+    length: number;
+    extras?: string[];
+}
+
+/*~ You can declare properties of the module using const, let, or var */
+export const myField: number;
+
+/*~ If there are types, properties, or methods inside dotted names
+ *~ of the module, declare them inside a 'namespace'.
+ */
+export namespace subProp {
+    /*~ For example, given this definition, someone could write:
+     *~   import { subProp } from 'yourModule';
+     *~   subProp.foo();
+     *~ or
+     *~   import * as yourMod from 'yourModule';
+     *~   yourMod.subProp.foo();
+     */
+    function foo(): void;
+}
