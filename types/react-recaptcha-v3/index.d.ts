72c67b409885fe8ed7b69ae0b238664e65f8bba9
Alessandro Rabitti <axel83.beta@gmail.com>
2020-01-14 14:32:11 +0100

    Fix react-recaptcha-v3 type definition comment in index.d.ts

--- types/react-recaptcha-v3/index.d.ts
+++ types/react-recaptcha-v3/index.d.ts
@@ -1,4 +1,4 @@
-// Type definitions for react-recaptcha 3
+// Type definitions for react-recaptcha-v3 1.1.0
 // Project: https://github.com/codeep/react-recaptcha-v3
 // Definitions by: Alessandro Rabitti <https://github.com/silversonicaxel>
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
