2339d46666630dc65c1136fc6a4359482ff8eac5
Johan Fagerberg <johanringmann@gmail.com>
2020-01-14 23:10:52 +0100

    [rehype-react] Add Fragment option
    Usually this will be `React.Fragment`, but it accepts any component.

--- types/rehype-react/index.d.ts
+++ types/rehype-react/index.d.ts
@@ -8,6 +8,7 @@ import * as React from "react";
 
 interface Options {
     createElement: typeof React.createElement;
+    Fragment: React.ComponentType<{ children?: React.ReactNode }>;
     components: {
         [tagName: string]: React.ComponentType<any>;
     };
