df66b7e6d73909599f048d24184d55e6cc1b77da
Johan Fagerberg <johanringmann@gmail.com>
2020-01-14 23:13:50 +0100

    [rehype-react] Update which options are optional
    Most options have healthy defaults. Only `createElement` is required.

--- types/rehype-react/index.d.ts
+++ types/rehype-react/index.d.ts
@@ -8,8 +8,8 @@ import * as React from "react";
 
 interface Options {
     createElement: typeof React.createElement;
-    Fragment: React.ComponentType<{ children?: React.ReactNode }>;
-    components: {
+    Fragment?: React.ComponentType<{ children?: React.ReactNode }>;
+    components?: {
         [tagName: string]: React.ComponentType<any>;
     };
     prefix?: string;
