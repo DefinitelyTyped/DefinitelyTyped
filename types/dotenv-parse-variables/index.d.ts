717a19c4c7a82f1497590dd7ad10e195f4137ff6
Gary King <garyking@gmail.com>
2020-01-16 15:35:18 -0500

    minimum ts version

--- types/dotenv-parse-variables/index.d.ts
+++ types/dotenv-parse-variables/index.d.ts
@@ -2,6 +2,7 @@
 // Project: https://github.com/niftylettuce/dotenv-parse-variables
 // Definitions by: Gary King <https://github.com/garyking>
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
+// Minimum TypeScript Version: 3.0
 
 declare namespace dotenvParseVariables {
     type Parsed = Record<string, string>;
