f99ce00f8d9621760398ec5e4b4d5c5ea85f91be
Linus Unnebäck <linus@folkdatorn.se>
2020-01-16 22:32:32 +0000

    [seed-random] Remove dependency on assert

--- types/seed-random/seed-random-tests.ts
+++ types/seed-random/seed-random-tests.ts
@@ -1,26 +1,21 @@
 /// <reference types="node" />
 
-import * as assert from 'assert';
 import * as seed from 'seed-random';
 
-const trueRandomA = seed();
-const trueRandomB = seed();
-assert(trueRandomA() !== trueRandomB());
+const trueRandomA = seed(); // $ExpectType () => number
+const trueRandomB = seed(); // $ExpectType () => number
 
-const fakeRandomA = seed('foo');
-const fakeRandomB = seed('foo');
-assert(fakeRandomA() === fakeRandomB());
+const fakeRandomA = seed('foo'); // $ExpectType () => number
+const fakeRandomB = seed('foo'); // $ExpectType () => number
 
-const fakeRandomC = seed('foo', {entropy: true});
-const fakeRandomD = seed('foo', {entropy: true});
-assert(fakeRandomC() !== fakeRandomD());
+const fakeRandomC = seed('foo', {entropy: true}); // $ExpectType () => number
+const fakeRandomD = seed('foo', {entropy: true}); // $ExpectType () => number
 
 // override global Math.random
 seed('foo', {global: true});
-const numA = Math.random();
+const numA = Math.random(); // $ExpectType number
 seed('foo', {global: true});
-const numB = Math.random();
-assert(numA === numB);
+const numB = Math.random(); // $ExpectType number
 
 // reset to default Math.random
 seed.resetGlobal();
