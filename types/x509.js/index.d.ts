5f219672027ebabb43fdd99a74060624c64e94c5
Stephane Moser <moser.sts@gmail.com>
2020-01-19 12:14:42 +0000

    Fix lint issues

--- types/x509.js/index.d.ts
+++ types/x509.js/index.d.ts
@@ -3,30 +3,26 @@
 // Definitions by: Stephane Moser <https://github.com/Moser-ss>
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
 
-declare module 'x509.js' {
-	interface Certificate {
-		publicModulus: string;
-		publicExponent: string;
-		subject: {
-			commonName: string;
-		};
-		issuer: {
-			commonName: string;
-		};
-		serial: string;
-		notBefore: string;
-		notAfter: string;
-		altNames: string[];
-		ocspList: string[];
-	}
-	interface Key {
-		publicExponent: string;
-		publicModulus: string;
-	}
-
-	function parseCert(certificate: string): Certificate;
-	function parseKey(key: string): Key;
-	function info(): number;
-
-	export { parseCert, parseKey, info };
+export interface Certificate {
+    publicModulus: string;
+    publicExponent: string;
+    subject: {
+        commonName: string;
+    };
+    issuer: {
+        commonName: string;
+    };
+    serial: string;
+    notBefore: string;
+    notAfter: string;
+    altNames: string[];
+    ocspList: string[];
 }
+export interface Key {
+    publicExponent: string;
+    publicModulus: string;
+}
+
+export function parseCert(certificate: string): Certificate;
+export function parseKey(key: string): Key;
+export function info(): number;
