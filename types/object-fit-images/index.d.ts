40b70465d52e5ca08faf63f385e3e5e90a0834b8
Ranjit Singh <rann2604@gmail.com>
2020-01-20 22:42:19 +0100

    [@types/object-fit-images] added new type definitions

---
+++ types/object-fit-images/index.d.ts
@@ -0,0 +1,11 @@
+// Type definitions for object-fit-images 3.2
+// Project: https://github.com/bfred-it/object-fit-images#readme
+// Definitions by: Ranjit Singh <https://github.com/rann91>
+// Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
+
+declare function objectFillImages(
+    images: string | HTMLElement | HTMLElement[] | NodeList | null,
+    options?: { watchMQ: boolean },
+): void;
+
+export default objectFillImages;
