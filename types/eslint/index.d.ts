c0808ee4a61e09fd330ad98f88f3a2e9a736949c
Jozef Sovcik <jozef@sovcik.net>
2020-01-21 13:51:51 +0100

    adding definition for optional formatter data argument

--- types/eslint/index.d.ts
+++ types/eslint/index.d.ts
@@ -538,6 +538,12 @@ export namespace CLIEngine {
         source?: string;
     }
 
+    interface LintResultData {
+        rulesMeta : {
+            [ruleId: string]: Rule.RuleMetaData;
+        }
+    }
+
     interface LintReport {
         results: LintResult[];
         errorCount: number;
@@ -546,7 +552,7 @@ export namespace CLIEngine {
         fixableWarningCount: number;
     }
 
-    type Formatter = (results: LintResult[]) => string;
+    type Formatter = (results: LintResult[], data?: LintResultData) => string;
 }
 
 //#endregion
