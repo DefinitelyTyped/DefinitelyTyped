49291cb5a95f5325667acdc4608f5facb11b3214
kikuchy <kikuchy.h@gmail.com>
2020-01-21 20:42:25 +0900

    fix verioning mistake

--- types/in-app-purchase/index.d.ts
+++ types/in-app-purchase/index.d.ts
@@ -1,4 +1,4 @@
-// Type definitions for in-app-purchase 1.11.4
+// Type definitions for in-app-purchase 1.11
 // Project: https://github.com/voltrue2/in-app-purchase#readme
 // Definitions by: Jonas Lochmann <https://github.com/l-jonas>
 //                 Dennis Kugelmann <https://github.com/IchordeDionysos>
