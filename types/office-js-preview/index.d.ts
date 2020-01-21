bbb122d8cb61e605c827d9c1b39961c08199552e
Elizabeth Samuel <elizs@microsoft.com>
2020-01-21 09:39:41 -0800

    [office-js] [office-js-preview] (Outlook) Remove SourceProperty

--- types/office-js-preview/index.d.ts
+++ types/office-js-preview/index.d.ts
@@ -8844,16 +8844,6 @@ declare namespace Office {
     interface CoercionTypeOptions {
         coercionType?: Office.CoercionType | string;
     }
-    enum SourceProperty {
-        /**
-         * The source of the data is from the body of the message.
-         */
-        Body,
-        /**
-         * The source of the data is from the subject of the message.
-         */
-        Subject
-    }
     /**
      * The subclass of {@link Office.Item | Item} dealing with appointments.
      * 
