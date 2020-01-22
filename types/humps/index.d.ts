f57acfb60bb510ce6b6802f5dd0072f4e95bb5cf
woody <woody@triple-corp.com>
2020-01-22 10:06:19 +0900

    Fix: re-enable dt-header rule

--- types/humps/index.d.ts
+++ types/humps/index.d.ts
@@ -1,4 +1,4 @@
-// Type definitions for humps v1.1.0
+// Type definitions for humps 1.1
 // Project: https://github.com/domchristie/humps
 // Definitions by: Niklas Mollenhauer <https://github.com/nikeee>
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
