d19629db37bce6be1d085c494344281da78074a5
Renan Machado <renan.mav@hotmail.com>
2020-01-24 10:02:23 -0300

    Merge branch 'update-relay-compiler-v8' of github.com:thicodes/DefinitelyTyped into feat/relay-8

--- types/relay-compiler/lib/reporters/MultiReporter.d.ts
+++ types/relay-compiler/lib/reporters/MultiReporter.d.ts
@@ -1,9 +1,9 @@
-import { GraphQLReporter } from './GraphQLReporter';
+import { Reporter } from './Reporter';
 
-declare class GraphQLMultiReporter implements GraphQLReporter {
+declare class MultiReporter implements Reporter {
     reportMessage(message: string): void;
     reportTime(name: string, ms: number): void;
     reportError(caughtLocation: string, error: Error): void;
 }
 
-export = GraphQLMultiReporter;
+export = MultiReporter;
