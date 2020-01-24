06dd435d8687b0304fd1bafd972a1ec3ed74d0ac
harry <harry@blend.com>
2020-01-24 09:51:11 -0800

    harry/fix-getFormError: fix

--- types/redux-form/lib/selectors.d.ts
+++ types/redux-form/lib/selectors.d.ts
@@ -1,7 +1,8 @@
 import { FormErrors, GetFormState } from "../index";
 
 export type DataSelector<FormData = {}, State = {}> = (formName: string, getFormState?: GetFormState) => (state: State) => FormData;
-export type ErrorSelector<State = {}> = (formName: string, getFormState?: GetFormState) => (state: State) => string;
+export type ErrorSelector<FormData = {}, State = {}, ErrorType = string> = (formName: string, getFormState?: GetFormState) => (state: State) => FormErrors<FormData, ErrorType>;
+export type GetFormErrorSelector<State = {}> = (formName: string, getFormState?: GetFormState) => (state: State) => any;
 export type BooleanSelector<State = {}> = (formName: string, getFormState?: GetFormState) => (state: State) => boolean;
 export type NamesSelector<State = {}> = (getFormState?: GetFormState) => (state: State) => string[];
 export type FormOrFieldsBooleanSelector<State = {}> = (formName: string, getFormState?: GetFormState) => (state: State, ...fields: string[]) => boolean;
@@ -13,7 +14,7 @@ export const getFormMeta: DataSelector;
 export const getFormAsyncErrors: ErrorSelector;
 export const getFormSyncWarnings: ErrorSelector;
 export const getFormSubmitErrors: ErrorSelector;
-export const getFormError: ErrorSelector;
+export const getFormError: GetFormErrorSelector;
 export const getFormNames: NamesSelector;
 export const isDirty: FormOrFieldsBooleanSelector;
 export const isPristine: FormOrFieldsBooleanSelector;
