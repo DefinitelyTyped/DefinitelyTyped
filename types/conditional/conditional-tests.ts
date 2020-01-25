cf33952f407ad799d09bcfe76dbddbc93fc79c46
Richard Wang <richardwa@google.com>
2020-01-25 15:38:31 +0900

    Expect void not undefined

--- types/conditional/conditional-tests.ts
+++ types/conditional/conditional-tests.ts
@@ -7,72 +7,72 @@ function callback<T extends Error>(err: T | null) {
     err?.message;
 };
 
-conditional.checkArgument(true); // $ExpectType undefined
-conditional.checkArgument(true, 'message'); // $ExpectType undefined
-conditional.checkArgument(true, 'message and callback', err => callback<conditional.IllegalArgumentError>(err)); // $ExpectType undefined
-conditional.checkArgument(true, err => callback<conditional.IllegalArgumentError>(err)); // $ExpectType undefined
+conditional.checkArgument(true); // $ExpectType void
+conditional.checkArgument(true, 'message'); // $ExpectType void
+conditional.checkArgument(true, 'message and callback', err => callback<conditional.IllegalArgumentError>(err)); // $ExpectType void
+conditional.checkArgument(true, err => callback<conditional.IllegalArgumentError>(err)); // $ExpectType void
 
-conditional.checkState(true); // $ExpectType undefined
-conditional.checkState(true, 'message'); // $ExpectType undefined
-conditional.checkState(true, 'message and callback', err => callback<conditional.IllegalStateError>(err)); // $ExpectType undefined
-conditional.checkState(true, err => callback<conditional.IllegalStateError>(err)); // $ExpectType undefined
+conditional.checkState(true); // $ExpectType void
+conditional.checkState(true, 'message'); // $ExpectType void
+conditional.checkState(true, 'message and callback', err => callback<conditional.IllegalStateError>(err)); // $ExpectType void
+conditional.checkState(true, err => callback<conditional.IllegalStateError>(err)); // $ExpectType void
 
-conditional.checkNumberType(true); // $ExpectType undefined
-conditional.checkNumberType(true, 'message'); // $ExpectType undefined
-conditional.checkNumberType(true, 'message and callback', err => callback<conditional.InvalidTypeError>(err)); // $ExpectType undefined
-conditional.checkNumberType(true, err => callback<conditional.InvalidTypeError>(err)); // $ExpectType undefined
+conditional.checkNumberType(true); // $ExpectType void
+conditional.checkNumberType(true, 'message'); // $ExpectType void
+conditional.checkNumberType(true, 'message and callback', err => callback<conditional.InvalidTypeError>(err)); // $ExpectType void
+conditional.checkNumberType(true, err => callback<conditional.InvalidTypeError>(err)); // $ExpectType void
 
-conditional.checkNotNumberType(true); // $ExpectType undefined
-conditional.checkNotNumberType(true, 'message'); // $ExpectType undefined
-conditional.checkNotNumberType(true, 'message and callback', err => callback<conditional.InvalidTypeError>(err)); // $ExpectType undefined
-conditional.checkNotNumberType(true, err => callback<conditional.InvalidTypeError>(err)); // $ExpectType undefined
+conditional.checkNotNumberType(true); // $ExpectType void
+conditional.checkNotNumberType(true, 'message'); // $ExpectType void
+conditional.checkNotNumberType(true, 'message and callback', err => callback<conditional.InvalidTypeError>(err)); // $ExpectType void
+conditional.checkNotNumberType(true, err => callback<conditional.InvalidTypeError>(err)); // $ExpectType void
 
-conditional.checkContains('y', ['y', 'n']); // $ExpectType undefined
-conditional.checkContains('y', ['y', 'n'], 'message'); // $ExpectType undefined
-conditional.checkContains('y', ['y', 'n'], 'message and callback', err => callback<conditional.UnknownValueError>(err)); // $ExpectType undefined
-conditional.checkContains('y', ['y', 'n'], err => callback<conditional.UnknownValueError>(err)); // $ExpectType undefined
+conditional.checkContains('y', ['y', 'n']); // $ExpectType void
+conditional.checkContains('y', ['y', 'n'], 'message'); // $ExpectType void
+conditional.checkContains('y', ['y', 'n'], 'message and callback', err => callback<conditional.UnknownValueError>(err)); // $ExpectType void
+conditional.checkContains('y', ['y', 'n'], err => callback<conditional.UnknownValueError>(err)); // $ExpectType void
 
-conditional.checkDoesNotContain('y', ['y', 'n']); // $ExpectType undefined
-conditional.checkDoesNotContain('y', ['y', 'n'], 'message'); // $ExpectType undefined
-conditional.checkDoesNotContain('y', ['y', 'n'], 'message and callback', err => callback<conditional.UnknownValueError>(err)); // $ExpectType undefined
-conditional.checkDoesNotContain('y', ['y', 'n'], err => callback<conditional.UnknownValueError>(err)); // $ExpectType undefined
+conditional.checkDoesNotContain('y', ['y', 'n']); // $ExpectType void
+conditional.checkDoesNotContain('y', ['y', 'n'], 'message'); // $ExpectType void
+conditional.checkDoesNotContain('y', ['y', 'n'], 'message and callback', err => callback<conditional.UnknownValueError>(err)); // $ExpectType void
+conditional.checkDoesNotContain('y', ['y', 'n'], err => callback<conditional.UnknownValueError>(err)); // $ExpectType void
 
-conditional.checkEquals('a', 'b'); // $ExpectType undefined
-conditional.checkEquals('a', 'b', 'message'); // $ExpectType undefined
-conditional.checkEquals('a', 'b', 'message and callback', err => callback<conditional.UnknownValueError>(err)); // $ExpectType undefined
-conditional.checkEquals('a', 'b', err => callback<conditional.UnknownValueError>(err)); // $ExpectType undefined
+conditional.checkEquals('a', 'b'); // $ExpectType void
+conditional.checkEquals('a', 'b', 'message'); // $ExpectType void
+conditional.checkEquals('a', 'b', 'message and callback', err => callback<conditional.UnknownValueError>(err)); // $ExpectType void
+conditional.checkEquals('a', 'b', err => callback<conditional.UnknownValueError>(err)); // $ExpectType void
 
-conditional.checkDoesNotEqual('a', 'b'); // $ExpectType undefined
-conditional.checkDoesNotEqual('a', 'b', 'message'); // $ExpectType undefined
-conditional.checkDoesNotEqual('a', 'b', 'message and callback', err => callback<conditional.UnknownValueError>(err)); // $ExpectType undefined
-conditional.checkDoesNotEqual('a', 'b', err => callback<conditional.UnknownValueError>(err)); // $ExpectType undefined
+conditional.checkDoesNotEqual('a', 'b'); // $ExpectType void
+conditional.checkDoesNotEqual('a', 'b', 'message'); // $ExpectType void
+conditional.checkDoesNotEqual('a', 'b', 'message and callback', err => callback<conditional.UnknownValueError>(err)); // $ExpectType void
+conditional.checkDoesNotEqual('a', 'b', err => callback<conditional.UnknownValueError>(err)); // $ExpectType void
 
-conditional.checkDefined(null); // $ExpectType undefined
-conditional.checkDefined(null, 'message'); // $ExpectType undefined
-conditional.checkDefined(null, 'message and callback', err => callback<conditional.UndefinedValueError>(err)); // $ExpectType undefined
-conditional.checkDefined(null, err => callback<conditional.UndefinedValueError>(err)); // $ExpectType undefined
+conditional.checkDefined(null); // $ExpectType void
+conditional.checkDefined(null, 'message'); // $ExpectType void
+conditional.checkDefined(null, 'message and callback', err => callback<conditional.UndefinedValueError>(err)); // $ExpectType void
+conditional.checkDefined(null, err => callback<conditional.UndefinedValueError>(err)); // $ExpectType void
 
-conditional.checkUndefined(null); // $ExpectType undefined
-conditional.checkUndefined(null, 'message'); // $ExpectType undefined
-conditional.checkUndefined(null, 'message and callback', err => callback<conditional.UndefinedValueError>(err)); // $ExpectType undefined
-conditional.checkUndefined(null, err => callback<conditional.UndefinedValueError>(err)); // $ExpectType undefined
+conditional.checkUndefined(null); // $ExpectType void
+conditional.checkUndefined(null, 'message'); // $ExpectType void
+conditional.checkUndefined(null, 'message and callback', err => callback<conditional.UndefinedValueError>(err)); // $ExpectType void
+conditional.checkUndefined(null, err => callback<conditional.UndefinedValueError>(err)); // $ExpectType void
 
-conditional.checkEmpty(null); // $ExpectType undefined
-conditional.checkEmpty(null, 'message'); // $ExpectType undefined
-conditional.checkEmpty(null, 'message and callback', err => callback<conditional.IllegalValueError>(err)); // $ExpectType undefined
-conditional.checkEmpty(null, err => callback<conditional.IllegalValueError>(err)); // $ExpectType undefined
+conditional.checkEmpty(null); // $ExpectType void
+conditional.checkEmpty(null, 'message'); // $ExpectType void
+conditional.checkEmpty(null, 'message and callback', err => callback<conditional.IllegalValueError>(err)); // $ExpectType void
+conditional.checkEmpty(null, err => callback<conditional.IllegalValueError>(err)); // $ExpectType void
 
-conditional.checkNotEmpty(null); // $ExpectType undefined
-conditional.checkNotEmpty(null, 'message'); // $ExpectType undefined
-conditional.checkNotEmpty(null, 'message and callback', err => callback<conditional.IllegalValueError>(err)); // $ExpectType undefined
-conditional.checkNotEmpty(null, err => callback<conditional.IllegalValueError>(err)); // $ExpectType undefined
+conditional.checkNotEmpty(null); // $ExpectType void
+conditional.checkNotEmpty(null, 'message'); // $ExpectType void
+conditional.checkNotEmpty(null, 'message and callback', err => callback<conditional.IllegalValueError>(err)); // $ExpectType void
+conditional.checkNotEmpty(null, err => callback<conditional.IllegalValueError>(err)); // $ExpectType void
 
-conditional.checkNull(null); // $ExpectType undefined
-conditional.checkNull(null, 'message'); // $ExpectType undefined
-conditional.checkNull(null, 'message and callback', err => callback<conditional.IllegalValueError>(err)); // $ExpectType undefined
-conditional.checkNull(null, err => callback<conditional.IllegalValueError>(err)); // $ExpectType undefined
+conditional.checkNull(null); // $ExpectType void
+conditional.checkNull(null, 'message'); // $ExpectType void
+conditional.checkNull(null, 'message and callback', err => callback<conditional.IllegalValueError>(err)); // $ExpectType void
+conditional.checkNull(null, err => callback<conditional.IllegalValueError>(err)); // $ExpectType void
 
-conditional.checkNotNull(null); // $ExpectType undefined
-conditional.checkNotNull(null, 'message'); // $ExpectType undefined
-conditional.checkNotNull(null, 'message and callback', err => callback<conditional.IllegalValueError>(err)); // $ExpectType undefined
-conditional.checkNotNull(null, err => callback<conditional.IllegalValueError>(err)); // $ExpectType undefined
+conditional.checkNotNull(null); // $ExpectType void
+conditional.checkNotNull(null, 'message'); // $ExpectType void
+conditional.checkNotNull(null, 'message and callback', err => callback<conditional.IllegalValueError>(err)); // $ExpectType void
+conditional.checkNotNull(null, err => callback<conditional.IllegalValueError>(err)); // $ExpectType void
