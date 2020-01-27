b03426cd7f3d904affb68e88fa775989f62c320d
Offir Golan <offirgolan@gmail.com>
2020-01-27 11:08:13 -0800

    [@pollyjs] Add missing adapter and persister packages

---
+++ types/pollyjs__persister-local-storage/index.d.ts
@@ -0,0 +1,9 @@
+// Type definitions for @pollyjs/persister-local-storage 4.0
+// Project: https://github.com/netflix/pollyjs/tree/master/packages/@pollyjs/persister-local-storage
+// Definitions by: offirgolan <https://github.com/offirgolan>
+// Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
+// TypeScript Version: 2.4
+
+import Persister from '@pollyjs/persister';
+
+export default class LocalStoragePersister extends Persister {}
