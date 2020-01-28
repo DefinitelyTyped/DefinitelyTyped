baabf692f8c8c9ef77071b41a176a7bcc3f921ab
Ben Lichtman <uniqueiniquity@users.noreply.github.com>
2020-01-28 09:35:04 -0800

    Revert "[assert] Add support for TypeScript's Assertion Functions (#41616)"
    This reverts commit 1eb97992fe9fd91a678e042a1ff178c8d5eb4310.

--- types/xmlpoke/xmlpoke-tests.ts
+++ types/xmlpoke/xmlpoke-tests.ts
@@ -1,70 +1,68 @@
 import xmlpoke = require('xmlpoke');
-
-// Declaring shims removes assert dependency. These tests are never executed, only typechecked, so this is fine.
-declare function assertEqual<T>(actual: T, expected: T): void;
+import assert = require('assert');
 
 let result: string;
 
 // add with xpath, value
 result = xmlpoke('<a/>', xml => xml.add('/a/b', 'c'));
-assertEqual(result, '<a><b>c</b></a>');
+assert.equal(result, '<a><b>c</b></a>');
 
 // add with xpath, Transform
 const addfn: XmlPoke.Transform = (node, value) => 'c';
 result = xmlpoke('<a/>', xml => xml.add('/a/b', addfn));
-assertEqual(result, '<a><b>c</b></a>');
+assert.equal(result, '<a><b>c</b></a>');
 
 // add with xpath, CDataValue
 const cdataval: XmlPoke.CDataValue = new xmlpoke.CDataValue('c');
 result = xmlpoke('<a/>', xml => xml.add('/a/b', cdataval));
-assertEqual(result, '<a><b><![CDATA[c]]></b></a>');
+assert.equal(result, '<a><b><![CDATA[c]]></b></a>');
 
 // add with xpath, XMLVal
 const xmlval = new xmlpoke.XmlString('<c/>');
 result = xmlpoke('<a/>', xml => xml.add('/a/b', xmlval));
-assertEqual(result, '<a><b><c/></b></a>');
+assert.equal(result, '<a><b><c/></b></a>');
 
 // add with map
 result = xmlpoke('<a/>', xml => xml.add({
     '/a/b': 'c'
 }));
-assertEqual(result, '<a><b>c</b></a>');
+assert.equal(result, '<a><b>c</b></a>');
 
 // set with xpath, value
 result = xmlpoke('<a>b</a>', xml => xml.set('/a', 'c'));
-assertEqual(result, '<a>c</a>');
+assert.equal(result, '<a>c</a>');
 
 // set with map
 result = xmlpoke('<a>b</a>', xml => xml.set({
     '/a': 'c'
 }));
-assertEqual(result, '<a>c</a>');
+assert.equal(result, '<a>c</a>');
 
 // set with xpath that doesn't exist (no-op)
 result = xmlpoke('<a><b>bval</b></a>', xml => xml.set('/a/c', 'cval'));
-assertEqual(result, '<a><b>bval</b></a>');
+assert.equal(result, '<a><b>bval</b></a>');
 
 // setOrAdd with xpath, value
 result = xmlpoke('<a/>', xml => xml.setOrAdd('/a/b', 'c'));
-assertEqual(result, '<a><b>c</b></a>');
+assert.equal(result, '<a><b>c</b></a>');
 
 // setOrAdd with map
 result = xmlpoke('<a/>', xml => xml.setOrAdd({
     '/a/b': 'c'
 }));
-assertEqual(result, '<a><b>c</b></a>');
+assert.equal(result, '<a><b>c</b></a>');
 
 // setOrAdd with xpath that doesn't exist: add
 result = xmlpoke('<a><b>bval</b></a>', xml => xml.setOrAdd('/a/c', 'cval'));
-assertEqual(result, '<a><b>bval</b><c>cval</c></a>');
+assert.equal(result, '<a><b>bval</b><c>cval</c></a>');
 
 // remove
 result = xmlpoke('<a><b/></a>', xml => xml.remove('//b'));
-assertEqual(result, '<a/>');
+assert.equal(result, '<a/>');
 
 // clear
 result = xmlpoke('<a><b/></a>', xml => xml.clear('/a'));
-assertEqual(result, '<a/>');
+assert.equal(result, '<a/>');
 
 // withBasePath, addNamespace, errorOnNoMatches
 result = xmlpoke('<test><x><![CDATA[hello]]></x></test>', xml =>
@@ -72,13 +70,13 @@ result = xmlpoke('<test><x><![CDATA[hello]]></x></test>', xml =>
        .addNamespace('x', 'http://example.com/x')
        .errorOnNoMatches()
        .set('/x', (node, value) => {
-            assertEqual(typeof node, 'object');
-            assertEqual((node.constructor as any).name, 'Element');
-            assertEqual(value, 'hello');
+            assert.equal(typeof node, 'object');
+            assert.equal((node.constructor as any).name, 'Element');
+            assert.equal(value, 'hello');
             return 'y';
        }));
-assertEqual(result, '<test><x>y</x></test>');
+assert.equal(result, '<test><x>y</x></test>');
 
 // ensure
 result = xmlpoke('</a>', xml => xml.ensure('a/b'));
-assertEqual(result, '<a><b/></a>');
+assert.equal(result, '<a><b/></a>');
