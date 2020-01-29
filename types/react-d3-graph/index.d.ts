512295b8120fbab93b6780c7ff660f86d70dcaa1
hrngoode <hgoode@scottlogic.com>
2020-01-29 16:09:16 +0000

    react-d3-graph types
    This is our first time submitting something to this git repo.
    This is the type definition for the node module 'react-d3-graph'.

---
+++ types/react-d3-graph/index.d.ts
@@ -0,0 +1,36 @@
+// Type definitions for react-d3-graph 2.3
+// Project: https://github.com/danielcaldas/react-d3-graph#readme
+// Definitions by: hrngoode <https://github.com/me>
+// Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
+
+import {Component} from "react";
+
+export class Graph extends Component<any, any>{
+    constructor(props: any, ...args: any[]);
+
+    UNSAFE_componentWillReceiveProps(nextProps: any): any;
+
+    componentDidMount(): void;
+
+    componentDidUpdate(): void;
+
+    componentWillUnmount(): void;
+
+    render(): any;
+
+}
+
+export class Link {
+    constructor(...args: any[]);
+
+    render(): any;
+
+}
+
+export class Node {
+    constructor(...args: any[]);
+
+    render(): any;
+
+}
+
