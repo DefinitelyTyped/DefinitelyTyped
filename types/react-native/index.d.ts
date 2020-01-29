347fd08f608dfba2c1a30a5c876d308b62da745f
Shinpei <react-native@kakao.com>
2020-01-29 18:52:55 +0900

    Update index.d.ts

--- types/react-native/index.d.ts
+++ types/react-native/index.d.ts
@@ -8474,7 +8474,7 @@ export namespace Animated {
     /**
      * Make any React component Animatable.  Used to create `Animated.View`, etc.
      */
-    export function createAnimatedComponent<T>(component: React.FC<T> | typeof React.Component): AnimatedComponent<T>;
+    export function createAnimatedComponent<T extends React.FC | typeof React.Component>(component: T): AnimatedComponent<T>;
 
     /**
      * Animated variants of the basic native views. Accepts Animated.Value for
