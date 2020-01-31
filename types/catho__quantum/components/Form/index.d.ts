3c071a312ee9d3eeedc5a6b7183b42635f14ab29
mutterpedro <pedro.mutter@catho.com>
2020-01-31 11:17:51 -0300

    Fix lint errors

--- types/catho__quantum/components/Form/index.d.ts
+++ types/catho__quantum/components/Form/index.d.ts
@@ -10,17 +10,17 @@ export interface FormProps {
 export class Form extends React.Component<FormProps> {}
 
 export namespace Validations {
-    export function Required(params: { value?: string }): string;
+    function Required(params: { value?: string }): string;
 
-    export function CPF(params: { value?: string }, cpf?: string): string;
+    function CPF(params: { value?: string }, cpf?: string): string;
 
-    export function CEP(params: { value?: string }): string;
+    function CEP(params: { value?: string }): string;
 
-    export function Date(params: { value?: string }): string;
+    function Date(params: { value?: string }): string;
 
-    export function MinLength(params: { value?: string; minLength?: string | number }): string;
+    function MinLength(params: { value?: string; minLength?: string | number }): string;
 
-    export function MaxLength(params: { value?: string; maxLength?: string | number }): string;
+    function MaxLength(params: { value?: string; maxLength?: string | number }): string;
 
-    export function Email(params: { value?: string }): string;
+    function Email(params: { value?: string }): string;
 }
