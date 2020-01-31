d17ed74c2fd9528ff5822c7658e6d7f09c96ea08
Simcha Wood <truthfulthomas@gmail.com>
2020-01-31 13:00:16 +0000

    Fixed tsconfig.json and adjusted exports

--- types/winston-loggly-bulk/index.d.ts
+++ types/winston-loggly-bulk/index.d.ts
@@ -1,39 +1,37 @@
-// Type definitions for winston-loggly-bulk 3.0.1
+// Type definitions for winston-loggly-bulk 3.0
 // Project: https://github.com/loggly/winston-loggly-bulk
 // Definitions by: Simcha Wood <https://github.com/SimchaWood>
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
 
+/// <reference types="node" />
+
 import { Url } from 'url';
-import Transport from 'winston-transport';
+import Transport = require('winston-transport');
 
-export namespace Loggly {
-    interface BufferOptions {
-        size: number;
-        retriesInMilliseconds: number;
-    }
+export interface BufferOptions {
+    size: number;
+    retriesInMilliseconds: number;
+}
 
-    interface LogglyOptions extends Transport.TransportStreamOptions {
-        auth?: {
-            username: string;
-            password: string;
-        } | null;
-        bufferOptions?: BufferOptions;
-        isBulk?: boolean;
-        json?: boolean;
-        networkErrorOnConsole?: boolean;
-        proxy?: null | string | Url;
-        stripColors?: boolean;
-        subdomain: string;
-        tags?: string[];
-        timestamp?: boolean;
-        token: string;
-    }
+export interface LogglyOptions extends Transport.TransportStreamOptions {
+    auth?: {
+        username: string;
+        password: string;
+    } | null;
+    bufferOptions?: BufferOptions;
+    isBulk?: boolean;
+    json?: boolean;
+    networkErrorOnConsole?: boolean;
+    proxy?: null | string | Url;
+    stripColors?: boolean;
+    subdomain: string;
+    tags?: string[];
+    timestamp?: boolean;
+    token: string;
 }
 
 export class Loggly extends Transport {
-    constructor(options?: Loggly.LogglyOptions);
+    constructor(options?: LogglyOptions);
 }
 
 export function flushLogsAndExit(): void;
-
-export {};
