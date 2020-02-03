eafc88ddb4bdd91c1a8dbae5f8b8f0465342f337
corno <github.a@schraverus.com>
2020-02-03 22:50:19 +0100

    Added declarations for 'clarinet'

---
+++ types/clarinet/index.d.ts
@@ -0,0 +1,48 @@
+// Type definitions for clarinet 0.12.4
+// Project: https://github.com/dscape/clarinet
+// Definitions by: corno <https://github.com/corno>
+// Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
+
+import { Stream } from "stream"
+
+export function parser(opt?: any): CParser
+export function createStream(opt?: any): CStream
+
+export declare type Options = {
+    trim?: boolean
+    normalize?: boolean
+}
+
+export declare class CParser {
+    line: number
+    column: number
+    position: number
+    onvalue: (value: string | boolean | null) => void
+    onkey: (key: string) => void
+    onopenobject: (key: string) => void
+    oncloseobject: () => void
+    onopenarray: () => void
+    onclosearray: () => void
+    onend: () => void
+    onerror: (e: Error) => void
+    onready: () => void
+    constructor(opt?: Options)
+    end(): void
+    write(chunk: string): void
+    resume(): void
+    close(): void
+}
+
+export declare class CStream extends Stream {
+    _parser: CParser
+    constructor(opt?: Options)
+    destroy(): void
+    end(chunk: string): void
+    on (ev: string, handler: (value?: string) => void): void
+}
+
+export declare let MAX_BUFFER_LENGTH: number
+export declare let DEBUG: boolean
+export declare let INFO: boolean
+export declare const EVENTS: Array<string>
+//export declare const STATE: {[key: number | string]: number | string}
