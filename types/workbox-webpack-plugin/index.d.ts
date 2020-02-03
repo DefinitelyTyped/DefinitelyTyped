787794ccedccc1223b20d3c73494b9b5e610cdf4
Preston Richey <prestonleerichey@gmail.com>
2020-02-03 17:19:19 -0500

    Fix lint errors

--- types/workbox-webpack-plugin/index.d.ts
+++ types/workbox-webpack-plugin/index.d.ts
@@ -1,6 +1,6 @@
 // Type definitions for workbox-webpack-plugin 5.0
 // Project: https://github.com/GoogleChrome/workbox/blob/master/packages/workbox-webpack-plugin, https://github.com/googlechrome/workbox
-// Definitions by: Preston Richey <https://github.com/prichey>
+// Definitions by: Kevin Groat <https://github.com/kgroat>, Preston Richey <https://github.com/prichey>
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
 
 export * from './generate-sw';
