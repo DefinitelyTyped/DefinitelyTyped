4c25d32566f3e20e284a6268c47dd25451ff72c0
Karl Horky <karl.horky@gmail.com>
2020-02-05 13:02:21 +0100

    Remove module again

--- types/passport-auth0/index.d.ts
+++ types/passport-auth0/index.d.ts
@@ -10,70 +10,68 @@
 import passport = require('passport');
 import express = require('express');
 
-declare module 'passport-auth0' {
-    export interface Profile extends passport.Profile {
-        id: string;
-        displayName: string;
-        gender?: string;
-        ageRange?: {
-            min: number;
-            max?: number;
-        };
-        profileUrl?: string;
-        username?: string;
-        birthday: string;
+export interface Profile extends passport.Profile {
+    id: string;
+    displayName: string;
+    gender?: string;
+    ageRange?: {
+        min: number;
+        max?: number;
+    };
+    profileUrl?: string;
+    username?: string;
+    birthday: string;
 
-        _raw: string;
-        _json: any;
-    }
-
-    export interface AuthenticateOptions extends passport.AuthenticateOptions {
-        authType?: string;
-    }
-
-    export interface StrategyOption {
-        clientID: string;
-        clientSecret: string;
-        callbackURL: string;
-        domain: string;
-        scopeSeparator?: string;
-        enableProof?: boolean;
-        profileFields?: string[];
-        state?: boolean;
-    }
-
-    export interface StrategyOptionWithRequest extends StrategyOption {
-        passReqToCallback: true;
-    }
-    export interface ExtraVerificationParams {
-        audience?: string;
-        connection?: string;
-        prompt?: string;
-    }
-
-    export type VerifyFunction = (
-        accessToken: string,
-        refreshToken: string,
-        extraParams: ExtraVerificationParams,
-        profile: Profile,
-        done: (error: any, user?: any, info?: any) => void,
-    ) => void;
-
-    export type VerifyFunctionWithRequest = (
-        req: express.Request,
-        accessToken: string,
-        refreshToken: string,
-        profile: Profile,
-        done: (error: any, user?: any, info?: any) => void,
-    ) => void;
-
-    export class Strategy extends passport.Strategy {
-        constructor(options: StrategyOptionWithRequest, verify: VerifyFunctionWithRequest);
-        constructor(options: StrategyOption, verify: VerifyFunction);
-
-        name: string;
-        authenticate(req: express.Request, options?: object): void;
-    }
-
-    export default Strategy;
+    _raw: string;
+    _json: any;
 }
+
+export interface AuthenticateOptions extends passport.AuthenticateOptions {
+    authType?: string;
+}
+
+export interface StrategyOption {
+    clientID: string;
+    clientSecret: string;
+    callbackURL: string;
+    domain: string;
+    scopeSeparator?: string;
+    enableProof?: boolean;
+    profileFields?: string[];
+    state?: boolean;
+}
+
+export interface StrategyOptionWithRequest extends StrategyOption {
+    passReqToCallback: true;
+}
+export interface ExtraVerificationParams {
+    audience?: string;
+    connection?: string;
+    prompt?: string;
+}
+
+export type VerifyFunction = (
+    accessToken: string,
+    refreshToken: string,
+    extraParams: ExtraVerificationParams,
+    profile: Profile,
+    done: (error: any, user?: any, info?: any) => void,
+) => void;
+
+export type VerifyFunctionWithRequest = (
+    req: express.Request,
+    accessToken: string,
+    refreshToken: string,
+    profile: Profile,
+    done: (error: any, user?: any, info?: any) => void,
+) => void;
+
+export class Strategy extends passport.Strategy {
+    constructor(options: StrategyOptionWithRequest, verify: VerifyFunctionWithRequest);
+    constructor(options: StrategyOption, verify: VerifyFunction);
+
+    name: string;
+    authenticate(req: express.Request, options?: object): void;
+}
+
+export default Strategy;
