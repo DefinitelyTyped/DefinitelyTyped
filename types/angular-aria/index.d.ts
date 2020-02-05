4c101ca873d267c7c1ce25e2fee83d00537efb80
Chives <chivesrs@gmail.com>
2020-02-05 03:45:37 -0500

    Add definition for AngularJS $aria service.

---
+++ types/angular-aria/index.d.ts
@@ -0,0 +1,19 @@
+// Type definitions for angular-aria 1.7
+// Project: http://angularjs.org, https://docs.angularjs.org/api/ngAria
+// Definitions by: Chives <https://github.com/chivesrs>
+// Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
+
+import * as angular from 'angular';
+
+declare module 'angular' {
+    namespace aria {
+        type IAriaAttribute = 'ariaHidden'|'ariaChecked'|'ariaReadonly'|'ariaDisabled'|'ariaRequired'|'ariaInvalid'|'ariaValue'|'tabindex'|'bindKeydown'|'bindRoleForClick';
+
+        /**
+         * $aria service.
+         */
+        interface IAriaService {
+            config(attribute: IAriaAttribute): boolean;
+        }
+    }
+}
