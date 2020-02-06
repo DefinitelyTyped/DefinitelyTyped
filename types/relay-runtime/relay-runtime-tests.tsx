cbcfdd4b12b863a82f5f1903c47fe22fa3b6975c
Martin Zlámal <mrtnzlml@gmail.com>
2020-02-06 11:21:59 +0100

    Relay runtime: add a new 'log' interface (#41807)

--- types/relay-runtime/relay-runtime-tests.tsx
+++ types/relay-runtime/relay-runtime-tests.tsx
@@ -10,10 +10,8 @@ import {
     RecordProxy,
     RecordSource,
     RecordSourceSelectorProxy,
-    RelayNetworkLoggerTransaction,
     Store,
     commitLocalUpdate,
-    createRelayNetworkLogger,
 } from 'relay-runtime';
 
 const source = new RecordSource();
@@ -36,10 +34,8 @@ function fetchQuery(operation: any, variables: { [key: string]: string }, cacheC
     });
 }
 
-const RelayNetworkLogger = createRelayNetworkLogger(RelayNetworkLoggerTransaction);
-
 // Create a network layer from the fetch function
-const network = Network.create(RelayNetworkLogger.wrapFetch(fetchQuery));
+const network = Network.create(fetchQuery);
 
 // Create a cache for storing query responses
 const cache = new QueryResponseCache({ size: 250, ttl: 60000 });
@@ -81,6 +77,19 @@ const environment = new Environment({
             kind: 'linked',
         },
     ],
+    log: (logEvent) => {
+        switch (logEvent.name) {
+            case 'execute.start':
+            case 'execute.next':
+            case 'execute.error':
+            case 'execute.info':
+            case 'execute.complete':
+            case 'execute.unsubscribe':
+            case 'queryresource.fetch':
+            default:
+                break;
+        }
+    }
 });
 
 // ~~~~~~~~~~~~~~~~~~~~~
