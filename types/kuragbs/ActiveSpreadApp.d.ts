32cd2e4057b7376ab653fec099d4e62ef9ed6f1e
Brian Phung <phunglesonhai.code1@gmail.com>
2020-02-07 10:56:59 +0700

    Update next

--- types/kuragbs/ActiveSpreadApp.d.ts
+++ types/kuragbs/ActiveSpreadApp.d.ts
@@ -1,4 +1,4 @@
-// Type definitions for non-npm package kuragbs 1.0
+// Type definitions kuragbs 1.0
 // Project: Does not have to be to GitHub, but prefer linking to a source code repository rather than to a project website.
 // Definitions by: Brian Phung <https://github.com/brian-phung>
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
@@ -6,11 +6,14 @@
 /// <reference types="google-apps-script" />
 
 declare namespace KuraGBS{
+
     namespace GSheets{
+
         type Sheet = GoogleAppsScript.Spreadsheet.Sheet;
         type Spreadsheet = GoogleAppsScript.Spreadsheet.Spreadsheet;
 
         class ActiveSpreadApp {
+
             id: string | undefined;
             spread: Spreadsheet;
             currentSheet: Sheet | undefined;            
@@ -22,4 +25,5 @@ declare namespace KuraGBS{
 }
 
 declare var ActiveSpreadApp: KuraGBS.GSheets.ActiveSpreadApp;
+
 
