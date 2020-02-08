a86ccb2decd642179052ba1bdaa2fbab49d7d154
Korolev <10618672@ext.de.uhde.org>
2020-02-08 22:42:26 +0100

    version update

--- types/react-adal/index.d.ts
+++ types/react-adal/index.d.ts
@@ -1,4 +1,4 @@
-// Type definitions for react-adal 0.4
+// Type definitions for react-adal 0.4.2
 // Project: https://github.com/salvoravida/react-adal#readme
 // Definitions by: Dmitry Korolev <https://github.com/dkorolev1>
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
