ee2d267fb03f061cc6f63865f646584730bd0379
Justin Beckwith <beckwith@google.com>
2020-02-09 09:40:03 -0800

    pr feedback

--- types/mkdirp/index.d.ts
+++ types/mkdirp/index.d.ts
@@ -10,9 +10,9 @@ import fs = require('fs');
 
 /**
  * Create a new directory and any necessary subdirectories at dir with octal
- * permission string opts.mode. If opts is a string or number, it will be
- * treated as the opts.mode. If opts.mode isn't specified, it defaults to
- * 0o777 & (~process.umask()).
+ * permission string `opts.mode`. If opts is a string or number, it will be
+ * treated as the `opts.mode`. If opts.mode isn't specified, it defaults to
+ * 0o777 & (~`process.umask()`).
  *
  * Promise resolves to first directory made that had to be created, or
  * undefined if everything already exists. Promise rejects if any errors are
@@ -21,17 +21,17 @@ import fs = require('fs');
  * You can optionally pass in an alternate fs implementation by passing in
  * opts.fs.
  *
- * Your implementation should have opts.fs.mkdir(path, opts, cb) and
- * opts.fs.stat(path, cb).
+ * Your implementation should have `opts.fs.mkdir(path, opts, cb)` and
+ * `opts.fs.stat(path, cb)`.
  *
  * You can also override just one or the other of mkdir and stat by passing in
- * opts.stat or opts.mkdir, or providing an fs option that only overrides one
+ * `opts.stat` or `opts.mkdir`, or providing an fs option that only overrides one
  * of these.
  */
 declare function mkdirp(dir: string, opts?: mkdirp.Mode | mkdirp.Options): Promise<string|undefined>;
 
 declare namespace mkdirp {
-    type Mode = number | string | null;
+    type Mode = number | string | undefined;
 
     interface FsImplementation {
         mkdir: typeof fs.mkdir;
@@ -55,17 +55,17 @@ declare namespace mkdirp {
 
     /**
      * Synchronously create a new directory and any necessary subdirectories at
-     * dir with octal permission string opts.mode. If opts is a string or number,
-     * it will be treated as the opts.mode. If opts.mode isn't specified, it
-     * defaults to 0o777 & (~process.umask()).
+     * dir with octal permission string `opts.mode`. If opts is a string or number,
+     * it will be treated as the `opts.mode`. If `opts.mode` isn't specified, it
+     * defaults to 0o777 & (~`process.umask()`).
      * You can optionally pass in an alternate fs implementation by passing in
-     * opts.fs. Your implementation should have opts.fs.mkdirSync(path, mode)
-     * and opts.fs.statSync(path).  You can also override just one or the other
-     * of mkdirSync and statSync by passing in opts.statSync or opts.mkdirSync,
+     * `opts.fs`. Your implementation should have `opts.fs.mkdirSync(path, mode)`
+     * and `opts.fs.statSync(path)`. You can also override just one or the other
+     * of `mkdirSync` and `statSync` by passing in `opts.statSync` or `opts.mkdirSync`,
      * or providing an fs option that only overrides one of these.
      * @returns Returns the first directory that had to be created, or undefined if everything already exists.
      */
-    function sync(dir: string, opts?: Mode | Options): string|null;
+    function sync(dir: string, opts?: Mode | Options): string|undefined;
 
     /**
      * Use the manual implementation (not the native one). This is the default
@@ -79,7 +79,7 @@ declare namespace mkdirp {
      * when the native implementation is not available or the stat/mkdir
      * implementation is overridden.
      */
-    function manualSync(dir: string, opts?: Mode | Options): string|null;
+    function manualSync(dir: string, opts?: Mode | Options): string|undefined;
 
     /**
      * Use the native implementation (not the manual one). This is the default
@@ -93,6 +93,6 @@ declare namespace mkdirp {
      * when the native implementation is available and stat/mkdir are not
      * overridden.
      */
-    function nativeSync(dir: string, opts?: Mode | Options): string|null;
+    function nativeSync(dir: string, opts?: Mode | Options): string|undefined;
 }
 export = mkdirp;
