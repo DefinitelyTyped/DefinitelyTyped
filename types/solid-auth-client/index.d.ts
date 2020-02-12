277f2f451a650d2b2bf481a31a5d979faf7ef7f4
Vincent <git@vincenttunru.com>
2020-02-12 16:15:36 +0100

    Update API for 2.4 release
    Specifically, it added the stopTrackSession() method.

--- types/solid-auth-client/index.d.ts
+++ types/solid-auth-client/index.d.ts
@@ -1,4 +1,4 @@
-// Type definitions for solid-auth-client 2.3
+// Type definitions for solid-auth-client 2.4
 // Project: https://github.com/solid/solid-auth-client#readme
 // Definitions by: Vincent <https://github.com/Vinnl>
 //                 James <https://github.com/durandj>
@@ -26,6 +26,7 @@ export interface SolidAuthClient extends EventEmitter {
   fetch(input: RequestInfo, init?: RequestInit): Promise<Response>;
   currentSession(storage?: AsyncStorage): Promise<Session | undefined>;
   trackSession(callback: (session?: Session) => void): void;
+  stopTrackSession(callback: (session?: Session) => void): void;
   login(identityProvider: string, options?: LoginOptions): Promise<void>;
   logout(storage?: AsyncStorage): Promise<void>;
   popupLogin(params?: LoginOptions): Promise<Session>;
