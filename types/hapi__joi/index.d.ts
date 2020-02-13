5d0343e977894f48a55b0aa83280280828d8189b
Simcha Wood <truthfulthomas@gmail.com>
2020-02-13 19:05:35 +0000

    Fix ValidationErrorFunction parameter type

--- types/hapi__joi/index.d.ts
+++ types/hapi__joi/index.d.ts
@@ -22,6 +22,7 @@
 //                 David Recuenco <https://github.com/RecuencoJones>
 //                 Frederic Reisenhauer <https://github.com/freisenhauer>
 //                 Stefan-Gabriel Muscalu <https://github.com/legraphista>
+//                 Simcha Wood <https://github.com/SimchaWood>
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
 // TypeScript Version: 2.8
 
