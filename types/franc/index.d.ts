0d56ef337147563a9904fd512245d30844e73335
Yoo.gyoon James Chung <gyoon.dev@gmail.com>
2020-02-13 15:23:22 +0900

    Update type for Options interface in franc package
    As documented here:
    
    https://github.com/wooorm/franc/issues/80
    
    adding this type enables the use of `franc.all` additionally with an `only` languages array when using TypeScript.
    
    Thank you!

--- types/franc/index.d.ts
+++ types/franc/index.d.ts
@@ -13,6 +13,7 @@ interface Options {
     minLength?: number;
     whitelist?: ISO6393[];
     blacklist?: ISO6393[];
+    only?: Array<string>;
 }
 
 declare function detect(text: string, options?: Options): ISO6393;
