93bd00c5f6479dcd7fdf262d6810557037ca7906
Sebastian Silbermann <silbermann.sebastian@gmail.com>
2020-02-16 11:16:08 +0100

    chore(react): explain `never` types in RenderFunction

--- types/react/index.d.ts
+++ types/react/index.d.ts
@@ -525,6 +525,8 @@ declare namespace React {
     interface ForwardRefRenderFunction<T, P = {}> {
         (props: PropsWithChildren<P>, ref: Ref<T>): ReactElement | null;
         displayName?: string;
+        // explicit rejected with `never` required due to
+        // https://github.com/microsoft/TypeScript/issues/36826
         /**
          * defaultProps are not supported on render functions
          */
