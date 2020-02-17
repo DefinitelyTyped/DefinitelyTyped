2973538e5f55eeb6aadbe1a9c29b0d5082f276d3
Gareth Jones <jones258@gmail.com>
2020-02-17 18:43:13 +1300

    feat(node): Add support for TypeScript 3.7 assertion functions

---
+++ types/node/v10/ts3.7/index.d.ts
@@ -0,0 +1,8 @@
+// NOTE: These definitions support NodeJS and TypeScript 3.7.
+
+// Base definitions for all NodeJS modules that are not specific to any version of TypeScript:
+// tslint:disable-next-line:no-bad-reference
+/// <reference path="../base.d.ts" />
+
+// TypeScript 3.7-specific augmentations:
+/// <reference path="assert.d.ts" />
