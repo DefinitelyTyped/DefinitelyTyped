c2f4a10120eb8b6f69e803d31ec0c425a5a89ba6
Allan Pope <pope.allans@gmail.com>
2020-02-17 13:09:45 +0100

    export correct types for functions

--- types/theme-ui__color/index.d.ts
+++ types/theme-ui__color/index.d.ts
@@ -4,14 +4,26 @@
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
 // TypeScript Version: 3.5
 
-export * from 'polished';
-// A few polished functions have been renamed in theme-ui
-// If these are updated in polished, then the code below can be deleted
-import {
-    adjustHue as rotate,
-    rgba as alpha,
-    setHue as hue,
-    setLightness as lightness,
-    setSaturation as saturation,
-} from 'polished';
-export { alpha, hue, lightness, rotate, saturation };
+export function darken(color: string, number: number | string): string;
+export function lighten(color: string, number: number | string): string;
+export function rotate(color: string, degree: number | string): string;
+
+export function hue(color: string, hue: number | string): string;
+export function saturation(color: string, saturation: number | string): string;
+export function lightness(color: string, lightness: number | string): string;
+
+export function desaturate(color: string, number: number | string): string;
+export function saturate(color: string, number: number | string): string;
+
+export function shade(color: string, number: number | string): string;
+export function tint(color: string, number: number | string): string;
+
+export function transparentize(color: string, number: number | string): string;
+export function alpha(color: string, number: number | string): string;
+
+export function mix(color: string, otherColor: string, number?: number | string): string;
+
+export function complement(color: string): string;
+export function invert(color: string): string;
+
+export function grayscale(color: string): string;
