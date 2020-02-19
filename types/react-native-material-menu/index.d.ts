cd0848420d5302a6c55412848e774d1ba3fb1364
hyun <coderhyun476@gmail.com>
2020-02-19 12:58:00 +0900

    add DT header

--- types/react-native-material-menu/index.d.ts
+++ types/react-native-material-menu/index.d.ts
@@ -1,23 +1,30 @@
+// Type definitions for react-native-material-menu 1.0
+// Project: https://github.com/mxck/react-native-material-menu (Does not have to be to GitHub, but prefer linking to a source code repository rather than to a project website.)
+// Definitions by: hyun <https://github.com/KoreanThinker>
+// Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
+
+
 import { ComponentClass, ReactElement, Component } from 'react'
 import { TextStyle } from 'react-native'
 
-export type MenuProps = {
-    button?: ReactElement,
-    style?: StyleMedia,
-    onHidden?: Function,
-    animationDuration?: number,
+/*~ You can declare types that are available via importing the module */
+export interface MenuProps {
+    button?: ReactElement;
+    style?: StyleMedia;
+    onHidden?: Function;
+    animationDuration?: number;
 }
-export type MenuItemProps = {
-    disabled?: boolean,
-    disabledTextColor?: string,
-    ellipsizeMode?: 'clip' | 'tail',
-    onPress?: Function,
-    style?: StyleMedia,
-    textStyle?: TextStyle,
-    underlayColor?: string,
+export interface MenuItemProps {
+    disabled?: boolean;
+    disabledTextColor?: string;
+    ellipsizeMode?: 'clip' | 'tail';
+    onPress?: Function;
+    style?: StyleMedia;
+    textStyle?: TextStyle;
+    underlayColor?: string;
 }
-export type MenuDividerProps = {
-    color?: string
+export interface MenuDividerProps {
+    color?: string;
 }
 
 export class MenuDivider extends Component<MenuDividerProps> { }
