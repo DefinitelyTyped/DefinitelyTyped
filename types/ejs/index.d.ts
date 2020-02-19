33aa07168f95aead63696ef8798116a06b9454a6
Piotr Błażejewicz (Peter Blazejewicz) <peterblazejewicz@users.noreply.github.com>
2020-02-19 14:13:51 +0100

    Update authors as per PR comment

--- types/ejs/index.d.ts
+++ types/ejs/index.d.ts
@@ -2,6 +2,7 @@
 // Project: http://ejs.co/, https://github.com/mde/ejs
 // Definitions by: Ben Liddicott <https://github.com/benliddicott>
 //                 ExE Boss <https://github.com/ExE-Boss>
+//                 Piotr Błażejewicz <https://github.com/peterblazejewicz>
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
 // TypeScript Version: 2.4
 
