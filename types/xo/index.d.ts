242394508777be7abafe0a5d75db48ebf42f72d0
Piotr Błażejewicz (Peter Blazejewicz) <peterblazejewicz@users.noreply.github.com>
2020-02-21 16:07:51 +0100

    feat(xo): type definition for v0.26
    - definition file
    - tests
    
    https://github.com/xojs/xo/#config
    
    Thanks!

---
+++ types/xo/index.d.ts
@@ -0,0 +1,60 @@
+// Type definitions for xo 0.26
+// Project: https://github.com/xojs/xo#readme
+// Definitions by: Piotr Błażejewicz (Peter Blazejewicz) <https://github.com/peterblazejewicz>
+// Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
+
+import eslint = require('eslint');
+
+export {};
+/**
+ * From T pick a set of properties K
+ */
+type Pick<T, K extends keyof T> = {
+    [P in K]: T[P];
+};
+
+// eslint.CLIEngine.getErrorResults without modification
+// eslint.CLIEngine.getErrorResults redeclared to match input shape
+
+/**
+ * Can be used to filter out all the non error messages from the report object.
+ */
+export const getErrorResults: typeof eslint.CLIEngine.getErrorResults;
+/**
+ * Used to output fixes from report to disk.
+ * It does by looking for files that have an output property in their results
+ */
+export function outputFixes(report: ResultReport): void;
+
+export function lintText(text: string, options?: Options): ResultReport;
+export function lintFiles(patterns: string | string[], options?: Options): ResultReport | Promise<ResultReport>;
+
+type CLIEngineOptions = Pick<
+    eslint.CLIEngine.Options,
+    'baseConfig' | 'cwd' | 'envs' | 'extensions' | 'fix' | 'globals' | 'ignore' | 'parser' | 'plugins' | 'rules'
+>;
+type ESLintOptions = Pick<eslint.Linter.LintOptions, 'filename'>;
+type ESLintConfig = Pick<eslint.Linter.Config, 'extends' | 'settings'>;
+
+export type Options = {
+    /** Enforce ES2015+ rules. Disabling this will make it not enforce ES2015+ syntax and conventions */
+    esnext?: boolean;
+    /** Some paths are ignored by default, including paths in .gitignore and .eslintignore. Additional ignores can be added here */
+    ignores?: string[];
+    /** Enable rules specific to the Node.js versions within the configured range */
+    nodeVersion?: string | boolean;
+    /** Format code with Prettier */
+    prettier?: boolean;
+    /** Set it to false to enforce no-semicolon style. */
+    semicolon?: boolean;
+    /** Set it to true to get 2-space indentation or specify the number of spaces. */
+    space?: number | string;
+} & CLIEngineOptions &
+    ESLintConfig &
+    ESLintOptions;
+
+export interface ResultReport {
+    readonly errorCount: number;
+    readonly warningCount: number;
+    readonly results: eslint.CLIEngine.LintResult[];
+}
---
+++ types/xo/xo-tests.ts
@@ -0,0 +1,58 @@
+/// <reference types="node" />
+
+import xo = require('xo');
+import path = require('path');
+
+const glob = path.join(__dirname, '..', '*.md');
+
+const options: xo.Options = {};
+
+if (options.semicolon === false && !options.prettier) {
+    if (options.rules) {
+        options.rules.semi = ['error', 'never'];
+        options.rules['semi-spacing'] = [
+            'error',
+            {
+                before: false,
+                after: true,
+            },
+        ];
+        options.rules['node/no-unsupported-features/es-builtins'] = ['error', { version: options.nodeVersion }];
+        options.rules['node/no-unsupported-features/es-syntax'] = [
+            'error',
+            { version: options.nodeVersion, ignores: ['modules'] },
+        ];
+        options.rules['node/no-unsupported-features/node-builtins'] = ['error', { version: options.nodeVersion }];
+    }
+    if (options.plugins) {
+        options.plugins = options.plugins.concat('react');
+    }
+    if (options.prettier && options.plugins) {
+        options.plugins = options.plugins.concat('prettier');
+        if (options.baseConfig) {
+            options.baseConfig.extends = options.baseConfig.extends.concat('prettier');
+            options.baseConfig.extends = options.baseConfig.extends.concat('prettier/unicorn');
+        }
+    }
+}
+let result = xo.lintText("'use strict'\nconsole.log('unicorn');\n");
+result = xo.lintText("'use strict'\nconsole.log('unicorn');\n", {
+    filename: 'ignored/index.js',
+    ignores: ['ignored/**/*.js'],
+});
+xo.lintText("'use strict'\nconsole.log('unicorn');\n", {
+    filename: 'node_modules/ignored/index.js',
+});
+result.errorCount; // $ExpectType number
+result.warningCount; // $ExpectType number
+result.results; // LintResult[]
+
+(async () => {
+    const moreExtensionsResults = await xo.lintFiles(glob, { extensions: ['md'] });
+    const { errorCount, results, warningCount } = await xo.lintFiles('**/*', { cwd: 'fixtures/cwd' });
+    const report = await xo.lintFiles('**/*', { cwd: 'fixtures/cwd' });
+    // only get the error messages
+    const errorReport = xo.getErrorResults(result.results);
+    // output fixes to disk
+    xo.outputFixes(report);
+})();
