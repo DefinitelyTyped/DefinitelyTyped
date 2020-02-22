7c22775ed9f4c52ea173c92f1e7e2bf128916112
Eric Cornelisesn <ericornelissen@gmail.com>
2020-02-22 22:29:53 +0200

    Use `export =` instead of `export default`
    as suggested by @PranavSenthilnathan

--- types/simple-icons/index.d.ts
+++ types/simple-icons/index.d.ts
@@ -1,19 +1,19 @@
 // Type definitions for simple-icons 2.5
 // Project: https://www.simpleicons.org
-// Definitions by: Eric Cornelisesn <https://github.com/ericcornelissen>
+// Definitions by: Eric Cornelissen <https://github.com/ericcornelissen>
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
 
-export interface SimpleIcon {
-    title: string;
-    slug: string;
-    svg: string;
-    path: string;
-    source: string;
-    hex: string;
-}
-
 declare namespace SimpleIcons {
+    interface SimpleIcon {
+        title: string;
+        slug: string;
+        svg: string;
+        path: string;
+        source: string;
+        hex: string;
+    }
+    
     function get(name: string): SimpleIcon;
 }
 
-export default SimpleIcons;
+export = SimpleIcons;
