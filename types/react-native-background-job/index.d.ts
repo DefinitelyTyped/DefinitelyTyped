d1213894f526aac05683619c042cfa4f254f50a2
hyun <coderhyun476@gmail.com>
2020-02-24 17:31:10 +0900

    remove declare

--- types/react-native-background-job/index.d.ts
+++ types/react-native-background-job/index.d.ts
@@ -4,42 +4,41 @@
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
 
 
-declare module "react-native-background-job" {
-    export interface registerProps {
-        jobKey?: string;
-        job?: () => void;
-    }
 
-    export interface scheduleProps {
-        jobKey?: string;
-        timeout?: number;
-        period?: number;
-        persist?: boolean;
-        override?: boolean,
-        networkType?: number;
-        requiresCharging?: boolean;
-        requiresDeviceIdle?: boolean;
-        exact?: boolean;
-        allowWhileIdle?: boolean;
-        allowExecutionInForeground?: boolean;
-        notificationText?: string;
-        notificationTitle?: string;
-    }
-
-    export interface cancelProps {
-        jobKey: string
-    }
-
-
-    export interface BackgroundJob {
-        register: (props: registerProps) => void;
-        schedule: (props: scheduleProps) => Promise<any>;
-        cancel: (props: cancelProps) => Promise<any>;
-        cancelAll: () => Promise<any>;
-        setGlobalWarnings: (warn: boolean) => void;
-        isAppIgnoringBatteryOptimization: (callback?: (err: any, isIgnoring: any) => void) => Promise<any>;
-    }
-
-    const BackgroundJob: BackgroundJob
-    export default BackgroundJob
+export interface registerProps {
+    jobKey?: string;
+    job?: () => void;
 }
+
+export interface scheduleProps {
+    jobKey?: string;
+    timeout?: number;
+    period?: number;
+    persist?: boolean;
+    override?: boolean,
+    networkType?: number;
+    requiresCharging?: boolean;
+    requiresDeviceIdle?: boolean;
+    exact?: boolean;
+    allowWhileIdle?: boolean;
+    allowExecutionInForeground?: boolean;
+    notificationText?: string;
+    notificationTitle?: string;
+}
+
+export interface cancelProps {
+    jobKey: string
+}
+
+
+export interface BackgroundJob {
+    register: (props: registerProps) => void;
+    schedule: (props: scheduleProps) => Promise<any>;
+    cancel: (props: cancelProps) => Promise<any>;
+    cancelAll: () => Promise<any>;
+    setGlobalWarnings: (warn: boolean) => void;
+    isAppIgnoringBatteryOptimization: (callback?: (err: any, isIgnoring: any) => void) => Promise<any>;
+}
+
+declare const BackgroundJob: BackgroundJob
+export default BackgroundJob
