b18511bbea00cdc536370239360d70da8a5a9de2
OhYee <oyohyee@oyohyee.com>
2020-02-26 02:52:31 +0800

    [@types/react] Add referrerPolicy to img attrs in React (#42568)
    * [@types/react] Add referrerPolicy to img attrs in React
    
    * Update types/react/index.d.ts
    
    Co-Authored-By: Sebastian Silbermann <silbermann.sebastian@gmail.com>
    
    Co-authored-by: Sebastian Silbermann <silbermann.sebastian@gmail.com>

--- types/react/index.d.ts
+++ types/react/index.d.ts
@@ -2010,6 +2010,7 @@ declare namespace React {
         decoding?: "async" | "auto" | "sync";
         height?: number | string;
         loading?: "eager" | "lazy";
+        referrerPolicy?: "no-referrer" | "origin" | "unsafe-url";
         sizes?: string;
         src?: string;
         srcSet?: string;
