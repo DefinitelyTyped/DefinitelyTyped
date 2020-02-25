84a698b171c25c2f00e153d433e599c8d2a5a392
Elizabeth Samuel <elizs@microsoft.com>
2020-02-25 10:48:24 -0800

    [office-js] [office-js-preview] (Outlook) Link to article show… (#42621)

--- types/office-js-preview/index.d.ts
+++ types/office-js-preview/index.d.ts
@@ -16313,6 +16313,9 @@ declare namespace Office {
     /**
      * Represents the properties of an appointment or message in a shared folder, mailbox, or calendar.
      *
+     * For more information on how this object is used, see the
+     * {@link https://docs.microsoft.com/office/dev/add-ins/outlook/delegate-access | delegate access} article.
+     *
      * [Api set: Mailbox 1.8]
      *
      * @remarks
