159238d66d1a2b7e88a4f65dc133d9a46bea25cd
ExE Boss <3889017+ExE-Boss@users.noreply.github.com>
2020-02-25 19:50:23 +0100

    feat(node): Add back support for TypeScript 3.2 (#42500)
    * feat(node): Add back support for TypeScript 3.2
    
    * fix(node): Fix `typesVersions`
    
    * fix(node/ts3.2): Fix TypeScript version comments

--- types/node/ts3.2/index.d.ts
+++ types/node/ts3.2/index.d.ts
@@ -1,4 +1,4 @@
-// NOTE: These definitions support NodeJS and TypeScript 3.5.
+// NOTE: These definitions support NodeJS and TypeScript 3.2.
 
 // Reference required types from the default lib:
 /// <reference lib="es2018" />
@@ -8,7 +8,9 @@
 
 // Base definitions for all NodeJS modules that are not specific to any version of TypeScript:
 // tslint:disable-next-line:no-bad-reference
-/// <reference path="../ts3.2/index.d.ts" />
+/// <reference path="../base.d.ts" />
 
-// TypeScript 3.5-specific augmentations:
-/// <reference path="wasi.d.ts" />
+// TypeScript 3.2-specific augmentations:
+/// <reference path="fs.d.ts" />
+/// <reference path="util.d.ts" />
+/// <reference path="globals.d.ts" />
