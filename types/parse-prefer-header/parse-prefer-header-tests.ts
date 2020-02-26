f353a4406fc81bf11b607bc0c84a8f69730d0314
Marcell Toth <marcell@marcelltoth.net>
2020-02-26 10:56:37 +0100

    parse-prefer-header: Support ReadonlyArray argument

--- types/parse-prefer-header/parse-prefer-header-tests.ts
+++ types/parse-prefer-header/parse-prefer-header-tests.ts
@@ -1,7 +1,6 @@
 import parsePreferHeader = require('parse-prefer-header');
 
-parsePreferHeader(['respond-async, wait=100', 'handling=lenient']);
+parsePreferHeader(['respond-async, wait=100', 'handling=lenient'] as ReadonlyArray<string>);
 parsePreferHeader('');
-
 parsePreferHeader(null);
 parsePreferHeader(undefined);
