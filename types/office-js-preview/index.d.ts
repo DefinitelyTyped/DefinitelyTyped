fe57599c02f7b5b4099d16cdcaa846eb5a412bc8
Elizabeth Samuel <elizs@microsoft.com>
2020-02-26 11:28:21 -0800

    [office-js] [office-js-preview] (Outlook) Mailbox.restUrl should include link to SharedProperties.targetRestUrl for shared folders scenario

--- types/office-js-preview/index.d.ts
+++ types/office-js-preview/index.d.ts
@@ -12587,8 +12587,12 @@ declare namespace Office {
          *
          * Your app must have the `ReadItem` permission specified in its manifest to call the `restUrl` member in read mode.
          *
-         * In compose mode you must call the saveAsync method before you can use the `restUrl` member. 
+         * In compose mode you must call the `saveAsync` method before you can use the `restUrl` member. 
          * Your app must have `ReadWriteItem` permissions to call the `saveAsync` method.
+         *
+         * However, in shared folders, you should instead use the `targetRestUrl` property of the {@link Office.SharedProperties | SharedProperties}
+         * object. For more information about this scenario, see the
+         * {@link https://docs.microsoft.com/office/dev/add-ins/outlook/delegate-access | delegate access} article.
          *
          * [Api set: Mailbox 1.5]
          *
