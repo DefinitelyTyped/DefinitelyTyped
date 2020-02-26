d328f3b1d06124f039449d9f1b477bb6564b50e9
Piotr Błażejewicz (Peter Blazejewicz) <peterblazejewicz@users.noreply.github.com>
2020-02-26 21:56:35 +0100

    feat(find-cache-dir): update to v3.2 (#42550)
    - v2 for backward compatibility
    - migrate return type from `null` to `undefined`
    
    https://github.com/avajs/find-cache-dir#readme
    
    Thanks!

--- types/find-cache-dir/v2/index.d.ts
+++ types/find-cache-dir/v2/index.d.ts
@@ -1,19 +1,20 @@
-// Type definitions for find-cache-dir 3.2
+// Type definitions for find-cache-dir 2.0
 // Project: https://github.com/avajs/find-cache-dir#readme
 // Definitions by: BendingBender <https://github.com/BendingBender>
-//                 Piotr Błażejewicz <https://github.com/peterblazejewicz>
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
 
 export = findCacheDir;
 
 /**
- * Finds the cache directory using the supplied options.
- * The algorithm tries to find a `package.json` file, searching every parent directory of the `cwd` specified
- * (or implied from other options). It returns a `string` containing the absolute path to the cache directory,
- * or `undefined` if `package.json` was never found or if the `node_modules` directory is unwritable.
+ * Finds the cache directory using the supplied options. The algorithm tries to find a `package.json` file,
+ * searching every parent directory of the `cwd` specified (or implied from other options).
+ * @param options
+ * @returns A string containing the absolute path to the cache directory, or null if package.json was never found.
  */
-declare function findCacheDir(options: findCacheDir.OptionsWithThunk): ((...pathParts: string[]) => string) | undefined;
-declare function findCacheDir(options: findCacheDir.Options): string | undefined;
+declare function findCacheDir(
+    options: findCacheDir.OptionsWithThunk
+): ((...pathParts: string[]) => string) | null;
+declare function findCacheDir(options: findCacheDir.Options): string | null;
 
 declare namespace findCacheDir {
     interface Options {
