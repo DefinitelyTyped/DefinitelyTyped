c6daba05dbc579289d14506bf8ed19fdb5a7f9c1
Valikhan Akhmedov <>
2020-02-26 22:45:00 +0600

    revert original comments in v2

--- types/simplebar/v2/index.d.ts
+++ types/simplebar/v2/index.d.ts
@@ -1,6 +1,6 @@
 // Type definitions for simplebar.js 2.4
 // Project: https://github.com/Grsmto/simplebar, https://grsmto.github.io/simplebar
-// Definitions by: Valikhan Akhmedov <https://github.com/val-o>
+// Definitions by: Gregor Woiwode <https://github.com/gregonnet>, Leonard Thieu <https://github.com/leonard-thieu>
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
 // TypeScript Version: 2.1
 
