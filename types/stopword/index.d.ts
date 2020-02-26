071e648d2c6f92c4bb47650b48329216c1db354c
Rico Sandyca Novenza <ricosandyca.if@gmail.com>
2020-02-26 11:39:52 +0700

    Update index.d.ts

--- types/stopword/index.d.ts
+++ types/stopword/index.d.ts
@@ -3,28 +3,48 @@
 // Definitions by: Rico Sandyca Novenza <https://github.com/ricosandyca>
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
 
-declare namespace Stopword {
-     const IStopword: {
-      /**
-       * Stopword removal
-       *
-       * @param text Array string of words
-       * @param stopwords Array string of your custom stopwords (default: English stopwords | .en)
-       */
-      removeStopwords(text: string[], stopwords?: string[]): string[];
+/**
+ * Stopword removal
+ *
+ * @param text Array string of words
+ * @param stopwords Array string of your custom stopwords (default: English stopwords | .en)
+ */
+export function removeStopwords(text: string[], stopwords?: string[]): string[];
 
-      /**
-       * Get array of stopwords according by language code
-       * Also can uses for stopwords parameter on removeStopwords
-       */
-       af: string[]; ar: string[]; bn: string[]; br: string[]; da: string[];
-       de: string[]; en: string[]; es: string[]; fa: string[]; fr: string[];
-       fi: string[]; ha: string[]; he: string[]; hi: string[]; id: string[];
-       it: string[]; ja: string[]; lgg: string[]; lggo: string[]; my: string[];
-       nl: string[]; no: string[]; pa: string[]; pl: string[]; pt: string[];
-       ru: string[]; so: string[]; st: string[]; sv: string[]; sw: string[];
-       vi: string[]; yo: string[]; zh: string[]; zu: string[];
-    };
-}
-
-export = Stopword.IStopword;
+/**
+* Get array of stopwords according by language code
+*/
+export const af: string[];
+export const ar: string[];
+export const bn: string[];
+export const br: string[];
+export const da: string[];
+export const de: string[];
+export const en: string[];
+export const es: string[];
+export const fa: string[];
+export const fr: string[];
+export const fi: string[];
+export const ha: string[];
+export const he: string[];
+export const hi: string[];
+export const id: string[];
+export const it: string[];
+export const ja: string[];
+export const lgg: string[];
+export const lggo: string[];
+export const my: string[];
+export const nl: string[];
+export const no: string[];
+export const pa: string[];
+export const pl: string[];
+export const pt: string[];
+export const ru: string[];
+export const so: string[];
+export const st: string[];
+export const sv: string[];
+export const sw: string[];
+export const vi: string[];
+export const yo: string[];
+export const zh: string[];
+export const zu: string[];
