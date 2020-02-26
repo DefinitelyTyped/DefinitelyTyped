3e1164763981342f14c4a09ac68b9fb6784d6dfb
Vincenzo Chianese <vincenz.chianese@icloud.com>
2020-02-25 18:48:08 -0600

    correct the package name

--- types/parse-prefer-header/index.d.ts
+++ types/parse-prefer-header/index.d.ts
@@ -1,4 +1,4 @@
-// Type definitions for node-persist 1.0
+// Type definitions for parse-prefer-header 1.0
 // Project: https://github.com/ppaskaris/node-parse-prefer-header
 // Definitions by: Vincenzo Chianese <https://github.com/XVincentX>
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
