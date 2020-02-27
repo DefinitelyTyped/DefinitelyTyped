a91050d839f06068970bdf5fa5d53f6305fbf105
ed eustace <ed.eustace@gmail.com>
2020-02-27 21:06:43 +0000

    feat: add types for @rollup/plugin-virtual

---
+++ types/rollup__plugin-virtual/index.d.ts
@@ -0,0 +1,21 @@
+// Type definitions for @rollup/plugin-virtual 2.0
+// Project: https://github.com/rollup/plugins/tree/master/packages/virtual#readme
+// Definitions by: ed eustace <https://github.com/ahum>
+// Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
+
+/// <reference types="node" />
+
+declare module '@rollup/plugin-virtual' {
+    import { Plugin } from 'rollup';
+    namespace virtual {
+        interface Options {
+            [filePath: string]: string;
+        }
+    }
+
+    /**
+     * A Rollup plugin which loads virtual modules from memory.
+     */
+    function virtual(options?: virtual.Options): Plugin;
+    export = virtual;
+}
