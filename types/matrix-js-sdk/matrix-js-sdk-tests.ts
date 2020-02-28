27ca45732efcd95c9f027f1e94801399845f72dc
Huan LI <zixia@zixia.net>
2020-02-28 09:02:19 +0800

    linting

--- types/matrix-js-sdk/matrix-js-sdk-tests.ts
+++ types/matrix-js-sdk/matrix-js-sdk-tests.ts
@@ -4,15 +4,15 @@
  *
  * Huan <zixia@zixia.net> Feb 2020
  */
-import sdk from 'matrix-js-sdk'
+import sdk from 'matrix-js-sdk';
 
-const myUserId      = "@example:localhost"
-const myAccessToken = "QGV4YW1wbGU6bG9jYWxob3N0.qPEvLuYfNBjxikiCjP"
+const myUserId      = "@example:localhost";
+const myAccessToken = "QGV4YW1wbGU6bG9jYWxob3N0.qPEvLuYfNBjxikiCjP";
 
 const matrixClient = sdk.createClient({
   baseUrl: "http://localhost:8008",
   accessToken: myAccessToken,
   userId: myUserId,
-})
+});
 
-matrixClient.startClient(10)  // messages for each room.
+matrixClient.startClient(10);  // messages for each room.
