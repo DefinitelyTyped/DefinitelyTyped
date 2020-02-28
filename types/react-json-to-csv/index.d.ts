fb675aa15b72dfe6f60770cbdb2b3db4f1fa454b
keith alleman <keith.alleman@icloud.com>
2020-02-27 18:08:06 -0700

    re-created the folder using dt-gen. Still cannot lint.

--- types/react-json-to-csv/index.d.ts
+++ types/react-json-to-csv/index.d.ts
@@ -1,3 +1,8 @@
+// Type definitions for react-json-to-csv 1.0
+// Project: https://github.com/coston/react-json-to-csv
+// Definitions by: keith alleman <https://github.com/me>
+// Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
+
 import * as React from 'react';
 
 export interface CsvDownloadProps extends React.HTMLAttributes<HTMLDivElement> {
