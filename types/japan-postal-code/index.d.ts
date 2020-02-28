2923625300de17c4a20ed8437193be817719e218
Takesi Tokugawa <tokugawa.takesi@gmail.com>
2020-02-28 11:17:07 +0900

    Clear to tests

---
+++ types/japan-postal-code/index.d.ts
@@ -0,0 +1,32 @@
+// Type definitions for japan-postal-code 2.0
+// Project: https://github.com/mzp/japan-postal-code
+// Definitions by: Takesi Tokugawa <https://github.com/TokugawaTakesi>
+// Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
+
+
+export as namespace JapanPostalCodeLibrary;
+
+export = JapanPostalCode;
+
+declare abstract class JapanPostalCode {
+  static get: JapanPostalCode.FetchPostalCodeMethod; 
+}
+
+
+declare namespace JapanPostalCode {
+
+  export interface FetchPostalCodeMethod {
+    (stringifiedPostalCode: string, callback: Callback): void;
+  }
+
+  interface Callback {
+    (addressData: AddressData): void;
+  }
+
+  export interface AddressData {
+    prefecture: string;
+    city: string;
+    area: string;
+    street: string;
+  }
+}
