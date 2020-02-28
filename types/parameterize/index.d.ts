5e629a72a28564c10e288dc443771e0eb2ee7f8e
Eduardo Turiño <eturino@eturino.com>
2020-02-28 17:44:51 +0000

    feat(parameterize): add parameterize typings

---
+++ types/parameterize/index.d.ts
@@ -0,0 +1,8 @@
+// Type definitions for parameterize 1.0
+// Project: https://github.com/fyalavuz/node-parameterize#readme
+// Definitions by: Eduardo Turiño <https://github.com/me>
+// Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
+
+export = parameterize;
+
+declare function parameterize(s: string, num_chars?: number, delimiter?: string): string;
