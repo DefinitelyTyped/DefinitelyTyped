cf2fd19d2682b4acb57a647c61689dbbaa8e495b
captain328 <captainsuper328@gmail.com>
2020-02-29 19:58:00 +0200

    Added type definitions for react-text-marquee

---
+++ types/react-text-marquee/react-text-marquee-tests.tsx
@@ -0,0 +1,17 @@
+import * as React from "react";
+import Marquee from "react-text-marquee";
+
+class Test extends React.Component {
+
+    public render() {
+        return (
+            <Marquee
+                text="Loremipsum dolor sit amet, consectetur adipiscing elit"
+                hoverToStop={true}
+                loop={true}
+                leading={1}
+                trailing={1}
+            />
+        );
+    }
+}
