909e5b9e018516e215524779457a341afdd101f5
Rico Sandyca Novenza <ricosandyca.if@gmail.com>
2020-02-29 20:07:45 +0700

    Update human-date-tests.ts

--- types/human-date/human-date-tests.ts
+++ types/human-date/human-date-tests.ts
@@ -1,6 +1,6 @@
 import hdate = require('human-date');
 
 hdate.prettyPrint(new Date());
-hdate.relativeTime('12-11-1998')
-hdate.monthName(8)
-hdate.toUTC(new Date())
+hdate.relativeTime('12-11-1998');
+hdate.monthName(8);
+hdate.toUTC(new Date());
