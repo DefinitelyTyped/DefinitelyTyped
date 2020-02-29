252f14107c1d33f7a1000bb08d32ea81cfbb36c2
Bill Sorensen <bill@redoxengine.com>
2020-02-28 19:32:30 -0700

    Bump libxmljs version comment

--- types/libxmljs/index.d.ts
+++ types/libxmljs/index.d.ts
@@ -1,4 +1,4 @@
-// Type definitions for Libxmljs 0.18
+// Type definitions for Libxmljs 0.19
 // Project: https://github.com/libxmljs/libxmljs
 // Definitions by: François de Campredon <https://github.com/fdecampredon>
 //                 ComFreek <https://github.com/ComFreek>
