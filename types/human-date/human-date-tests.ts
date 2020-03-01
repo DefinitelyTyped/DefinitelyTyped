5bb38d0b0983e7b56d53d6a313a3b8ed92d44722
Rico Sandyca Novenza <ricosandyca.if@gmail.com>
2020-03-01 14:15:27 +0700

    Add files via upload

--- types/human-date/human-date-tests.ts
+++ types/human-date/human-date-tests.ts
@@ -1,6 +1,27 @@
 import hdate = require('human-date');
 
-hdate.prettyPrint(new Date());
-hdate.relativeTime('12-11-1998');
+// .prertyPrint method
+hdate.prettyPrint('8-16-1987');
+hdate.prettyPrint(new Date('8-16-1987'));
+hdate.prettyPrint(-6400);
+hdate.prettyPrint(new Date(1416448704578), { showTime: true });
+
+// .relativeTime method
+hdate.relativeTime(4);
+hdate.relativeTime(4, {futureSuffix: "in the future"});
+hdate.relativeTime("8-16-1987");
+hdate.relativeTime(new Date("8-16-1987"));
+hdate.relativeTime(new Date("8-16-1987"), { returnObject: true });
+hdate.relativeTime(75, { allUnits: true });
+
+// .monthName method
 hdate.monthName(8);
-hdate.toUTC(new Date());
+hdate.monthName("8-16-1987");
+hdate.monthName(new Date("8-16-1987"));
+
+// .toUTC method
+hdate.toUTC(1000000000000);
+hdate.toUTC(1000000000000).getFullYear();
+hdate.toUTC("8-16-1987");
+hdate.toUTC(new Date("8-16-1987"));
+hdate.toUTC(new Date("8-16-1987")).toISOString();
--- types/human-date/index.d.ts
+++ types/human-date/index.d.ts
@@ -4,23 +4,23 @@
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
 
 export interface PrettyPrintOptions {
-    showTime: boolean;
+    showTime?: boolean;
 }
 
 export interface RelativeTimeOptions {
-    futureSuffix: string;
-    pastSuffix: string;
-    presentText: string;
-    returnObject: boolean;
-    allUnits: boolean;
+    futureSuffix?: string;
+    pastSuffix?: string;
+    presentText?: string;
+    returnObject?: boolean;
+    allUnits?: boolean;
 }
 
 export interface RelativeTimeReturns {
-    seconds: number;
-    hours: number;
-    days: number;
-    years: number;
-    past: boolean;
+    seconds?: number;
+    hours?: number;
+    days?: number;
+    years?: number;
+    past?: boolean;
 }
 
 // prettyPrint
