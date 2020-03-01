8af8f55438192bb0055472c719e61ae5a670b70d
Jon Phenow <j.phenow@gmail.com>
2020-03-01 10:36:29 -0600

    Despecify

--- types/hubot/index.d.ts
+++ types/hubot/index.d.ts
@@ -5,7 +5,7 @@
 //                 Emil Marklund <https://github.com/eeemil>
 //                 Jon Phenow <https://github.com/jphenow>
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
-// TypeScript Version: 3.7.2
+// TypeScript Version: 3.7
 
 import { EventEmitter } from 'events';
 import { Options as HttpOptions, ScopedClient } from 'scoped-http-client';
