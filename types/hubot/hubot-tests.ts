57a6e4904f88a68c8ed3c4c879e6f31755850d40
Jon Phenow <j.phenow@gmail.com>
2020-03-01 09:41:08 -0600

    fix base tests

--- types/hubot/hubot-tests.ts
+++ types/hubot/hubot-tests.ts
@@ -1,18 +1,13 @@
-import * as Hubot from "hubot";
+import { Robot, Adapter, Brain } from 'hubot';
 
-const robot = new Hubot.Robot<{}>(
-  'src/adapters',
-  'slack',
-  false,
-  'hubot',
-);
+const robot = new Robot<Adapter>('src/adapters', 'slack', false, 'hubot');
 robot; // $ExpectType Robot<{}>
 robot.adapter; // $ExpectType {}
 robot.hear(/hello/, () => null); // $ExpectType void
 robot.on('test', () => null); // $ExpectType Robot<{}>
 robot.emit('test', 'arg'); // $ExpectType boolean
 
-const brain = new Hubot.Brain(robot);
+const brain = new Brain(robot);
 brain; // $ExpectType Brain<{}>
 brain.userForName('someone'); // $ExpectType User
 brain.get('test'); // $ExpectType any
