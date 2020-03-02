9bdb2611b08e1345f4ed3bf448a1c14a23cd95bc
Nader Al-Shamma <naderal.shamma@mitrefinch.co.uk>
2020-03-02 11:43:43 +0000

    update tests

--- types/fullcalendar__vue/fullcalendar__vue-tests.ts
+++ types/fullcalendar__vue/fullcalendar__vue-tests.ts
@@ -1,5 +1,5 @@
 import Vue from 'vue';
-import FullCalendar, { FullCalendarComponent } from '@fullcalendar/vue';
+import FullCalendar, { FullCalendarComponent } from 'fullcalendar__vue';
 
 new Vue({
     el: '#app',
