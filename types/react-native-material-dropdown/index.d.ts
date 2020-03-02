1eb6334f7e6d84251359d0ba8d6663adfd35090e
Nico Hernandez <nicoo.her@hotmail.com>
2020-03-02 15:37:28 -0300

    Add missing labelTextStyle

--- types/react-native-material-dropdown/index.d.ts
+++ types/react-native-material-dropdown/index.d.ts
@@ -68,6 +68,8 @@ export interface DropDownProps extends TouchableWithoutFeedbackProps {
   containerStyle?: StyleProp<ViewStyle>;
   /** Set input container styles */
   inputContainerStyle?: StyleProp<ViewStyle>;
+  /** Set the label styles */
+  labelTextStyle?: StyleProp<ViewStyle>;
   /** Set overlay styles */
   overlayStyle?: StyleProp<ViewStyle>;
   /** Set picker styles */
