fa360396e3cf76616d5586e2fcbb2fb463c8df89
tjalsdud89 <tjalsdud89@naver.com>
2020-03-03 21:17:33 +0900

    add fxjs

--- types/fxjs/fxjs-test.ts
+++
@@ -1,3 +0,0 @@
-import fxjs = require('fxjs');
-
-fxjs.go(1, (a: number) => a);
--- types/fxjs/index.d.ts
+++ types/fxjs/index.d.ts
@@ -1,9 +1,9 @@
 // Type definitions for fxjs
-// Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
 // Project: https://github.com/marpple/FxJS.git
 // Definitions by: minyeong <https://github.com/load28>
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
 // TypeScript Version: 3.8
-declare module "fxjs" {
+
+declare module 'fxjs' {
     export function go(..._: any[]): any[];
 }
