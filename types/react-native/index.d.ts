55c63598e0ba6528e38248c9adef901137eefebe
Eloy Durán <eloy.de.enige@gmail.com>
2020-03-04 19:15:49 +0100

    [react-native] Specify more detailed type of `hardwareBackPress` handler (#42828)
    * react-native: handleBackPress
    
    * react-native: remove void from hardwareBackPress

--- types/react-native/index.d.ts
+++ types/react-native/index.d.ts
@@ -7000,12 +7000,17 @@ export type BackPressEventName = 'hardwareBackPress';
  * Detect hardware back button presses, and programmatically invoke the
  * default back button functionality to exit the app if there are no
  * listeners or if none of the listeners return true.
- * Methods don't have more detailed documentation as of 0.25.
+* The event subscriptions are called in reverse order
+ * (i.e. last registered subscription first), and if one subscription
+ * returns true then subscriptions registered earlier
+ * will not be called.
+ *
+ * @see https://reactnative.dev/docs/backhandler.html
  */
 export interface BackHandlerStatic {
     exitApp(): void;
-    addEventListener(eventName: BackPressEventName, handler: () => void): NativeEventSubscription;
-    removeEventListener(eventName: BackPressEventName, handler: () => void): void;
+    addEventListener(eventName: BackPressEventName, handler: () => boolean | null | undefined): NativeEventSubscription;
+    removeEventListener(eventName: BackPressEventName, handler: () => boolean | null | undefined): void;
 }
 
 export interface ButtonProps {
