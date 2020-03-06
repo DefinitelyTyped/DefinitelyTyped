f7987d04b81333180d8395c36df11c5a21bcb97c
Pokai Chang <mail@zeta.vg>
2020-03-06 18:05:25 +0800

    [classnames] accept the "default" export in the argument for classnames/bind
    While using the `import * as styles from '[...].postcss';` way to import the CSS module, an additional `.default` property will be attached to the imported styles, which value will be an object, causing a type error if it's passed to classnames/bind.

--- types/classnames/bind.d.ts
+++ types/classnames/bind.d.ts
@@ -1,7 +1,15 @@
 import { ClassNamesFn } from './types';
 
+type CssModule = Record<
+        string,
+        | string
+        // To accept be the "default" export while using the
+        // `import * as styles from '[...].postcss';` way to import the CSS module.
+        | Record<string, string>
+    >;
+
 interface ClassNamesBind extends ClassNamesFn {
-    bind(styles: Record<string, string>): ClassNamesFn;
+    bind(styles: CssModule): ClassNamesFn;
 }
 type ClassNamesBindExport = ClassNamesBind & {
     default: ClassNamesBind;
