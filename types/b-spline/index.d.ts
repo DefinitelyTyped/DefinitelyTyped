b89a757ae9fb675737388da11262720ff16589c3
Ruby Quail <ruby@rubyquail.design>
2020-03-09 22:14:53 +1100

    fixed an error
    Signed-off-by: Ruby Quail <ruby@rubyquail.design>

--- types/b-spline/index.d.ts
+++ types/b-spline/index.d.ts
@@ -1,5 +1,7 @@
 // Type definitions for b-spline 2.0.1
+// Project: https://github.com/thibauts/b-spline
 // Definitions by: Ruby <https://github.com/Rupertofly>
+// Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
 declare function b_spline<T extends ArrayLike<number>>(t: number, degree: number, points: T[], knots?: number[], weights?: number[]): T;
 
 export default b_spline;
