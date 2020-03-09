0e7bef137703a999940589e03d1dd6f8c62c7413
Piotr Błażejewicz (Peter Blazejewicz) <peterblazejewicz@users.noreply.github.com>
2020-03-09 21:42:55 +0100

    update(uglify-js): update Compression options
    - add missing: `assignments`, `directives`, `functions`, `keep_fargs`,
    `objects`, `pure_funcs`
    - remove `warnings` options
    - refine JSDoc comments and add defaults
    
    https://github.com/mishoo/UglifyJS2#compress-options
    https://github.com/mishoo/UglifyJS2/issues/3394#issuecomment-488668042
    
    Thanks!

--- types/uglify-js/uglify-js-tests.ts
+++ types/uglify-js/uglify-js-tests.ts
@@ -35,6 +35,24 @@ const output = minify(code, {
     }
 });
 
+const compressOptions = {
+    booleans: true,
+    comparisons: true,
+    conditionals: true,
+    dead_code: true,
+    evaluate: true,
+    hoist_funs: false,
+    if_return: true,
+    join_vars: true,
+    keep_fargs: true,
+    loops: true,
+    side_effects: true,
+    unused: true,
+};
+minify(code, {
+    compress: compressOptions,
+});
+
 if (output.warnings) {
     output.warnings.filter(x => x === 'Dropping unused variable');
 }
