3d6bfb8332405ef0407bdffa4861f24cf58aa3d0
Ruby Quail <Rupertofly@users.noreply.github.com>
2020-03-09 22:12:33 +1100

    Update index.d.ts

--- types/b-spline/index.d.ts
+++ types/b-spline/index.d.ts
@@ -1,4 +1,4 @@
-// Type definitions for b-spline
+// Type definitions for b-spline 2.0.1
 // Definitions by: Ruby <https://github.com/Rupertofly>
 declare function b_spline<T extends ArrayLike<number>>(t: number, degree: number, points: T[], knots?: number[], weights?: number[]): T;
 
