dd33cb524c6061fb429d09caf69698d790dba3af
Piotr Błażejewicz (Peter Blazejewicz) <peterblazejewicz@users.noreply.github.com>
2020-03-09 23:27:01 +0100

    feat(karma-html-detailed-reporter): new type definition files
    - definition file
    - tests
    
    https://github.com/a11smiles/karma-html-detailed-reporter#options
    
    Thanks!

---
+++ types/karma-html-detailed-reporter/karma-html-detailed-reporter-tests.ts
@@ -0,0 +1,23 @@
+import karma = require('karma');
+
+// See https://github.com/karma-runner/karma-coverage/blob/v1.1.2/README.md#basic
+module.exports = (config: karma.Config) => {
+    config.set({
+        frameworks: ['jasmine'],
+        reporters: ['progress', 'htmlDetailed'],
+        browsers: ['Chrome', 'PhantomJS'],
+        plugins: ['karma-jasmine', 'karma-chrome-launcher', 'karma-html-detailed-reporter'],
+
+        // (test) configure the reporter
+        htmlDetailed: {
+            autoReload: true,
+            dir: './_reports',
+            refreshTimeout: 1000,
+            splitResults: true,
+            showSuccess: true,
+            showFailed: false,
+            showSkipped: true,
+            useHostedBootstrap: true,
+        },
+    });
+};
