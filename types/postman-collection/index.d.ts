56ca12e9997da4d6e93c71bdf9d88ae620920fa6
Vincenzo Chianese <vincenz.chianese@icloud.com>
2020-03-12 23:52:13 -0500

    chore: add me as author of the definition

--- types/postman-collection/index.d.ts
+++ types/postman-collection/index.d.ts
@@ -1,6 +1,7 @@
 // Type definitions for postman-collection 3.5
 // Project: https://github.com/postmanlabs/postman-collection
 // Definitions by: Kyle Buzby <https://github.com/kbuzby>
+// Definitions by: Vincenzo Chianese <https://github.com/XVincentX>
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
 // TypeScript Version: 2.4
 /// <reference types="node" />
