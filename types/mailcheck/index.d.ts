f1f5c4bb0ae553f56766882f6458d2e22baa87c7
Jeffrey Meng <jeffkmeng@gmail.com>
2020-03-14 17:11:35 -0700

    Remove patch from header

--- types/mailcheck/index.d.ts
+++ types/mailcheck/index.d.ts
@@ -1,4 +1,4 @@
-// Type definitions for Mailcheck 1.1.1
+// Type definitions for Mailcheck 1.1
 // Project: https://github.com/mailcheck/mailcheck
 // Definitions by: Paulo Cesar <https://github.com/pocesar>
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
