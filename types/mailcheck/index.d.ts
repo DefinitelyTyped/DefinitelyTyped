fc3968c884f46212f5a6f34f15f92059b59409d1
Jeffrey Meng <jeffkmeng@gmail.com>
2020-03-14 17:06:50 -0700

    Update header

--- types/mailcheck/index.d.ts
+++ types/mailcheck/index.d.ts
@@ -1,8 +1,8 @@
-// Type definitions for Mailcheck 1.1
+// Type definitions for Mailcheck 1.1.1
 // Project: https://github.com/mailcheck/mailcheck
 // Definitions by: Paulo Cesar <https://github.com/pocesar>
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
-// TypeScript Version: 2.3
+// TypeScript Version: 3.8
 
 /// <reference types="jquery" />
 
