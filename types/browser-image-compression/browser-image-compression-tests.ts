8c149ffd5a4909c9d2c85af470d2089efb01e853
Donald <donaldcwl@gmail.com>
2020-03-17 13:12:08 +0800

    update test case

--- types/browser-image-compression/browser-image-compression-tests.ts
+++ types/browser-image-compression/browser-image-compression-tests.ts
@@ -1,14 +1,22 @@
 import imageCompression = require('browser-image-compression');
 
-const file = new File([''], 'image.jpg', { type: 'image/jpeg' });
+async function handleImageUpload({ target }: { target: EventTarget & { files: FileList } }) {
+  const imageFile = target.files[0];
+  console.log('originalFile instanceof Blob', imageFile instanceof Blob); // true
+  console.log(`originalFile size ${imageFile.size / 1024 / 1024} MB`);
 
-(async () => {
-  const compressedFile = await imageCompression(file, {
+  const options = {
     maxSizeMB: 1,
     maxWidthOrHeight: 1920,
-    useWebWorker: false,
-    maxIteration: 10,
-    exifOrientation: -2,
-    onProgress: (p: number) => {}
-  });
-})();
+    useWebWorker: true
+  };
+  try {
+    const compressedFile = await imageCompression(imageFile, options);
+    console.log('compressedFile instanceof Blob', compressedFile instanceof Blob); // true
+    console.log(`compressedFile size ${compressedFile.size / 1024 / 1024} MB`); // smaller than maxSizeMB
+
+    // await uploadToServer(compressedFile); // write your own logic
+  } catch (error) {
+    console.log(error);
+  }
+}
