880d00c8567a33fc2451b321402e3e16b3c93208
Christoph Tavan <dev@tavan.de>
2020-03-17 17:01:46 +0100

    Add deprecated deep imports (#43090)
    Fixes #42634
    
    Added again for consistency with uuid@7.x

--- types/uuid/uuid-tests.ts
+++ types/uuid/uuid-tests.ts
@@ -2,6 +2,11 @@
 
 import { v1 as uuidv1, v4 as uuidv4, v3 as uuidv3, v5 as uuidv5 } from 'uuid';
 
+import v1 = require('uuid/v1');
+import v3 = require('uuid/v3');
+import v4 = require('uuid/v4');
+import v5 = require('uuid/v5');
+
 const randoms = [
     0x10, 0x91, 0x56, 0xbe, 0xc4, 0xfb, 0xc1, 0xea,
     0x71, 0xb4, 0xef, 0xe1, 0x67, 0x1c, 0x58, 0x36
@@ -61,3 +66,9 @@ uuidv4(null, g); // $ExpectType Buffer
 class CustomBuffer extends Uint8Array {}
 const h = new CustomBuffer(10);
 uuidv4(null, h); // $ExpectType CustomBuffer
+
+// Legacy deep import
+stringv1 = v1();
+stringv4 = v4();
+const l3: string = v3('hello', MY_NAMESPACE);
+const l5: string = v5('hello', MY_NAMESPACE);
