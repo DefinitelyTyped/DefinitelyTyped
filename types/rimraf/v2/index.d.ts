f6266329c4aa4986cb9d30f0cbececffc834a0dc
Peter Blazejewicz <peterblazejewicz@users.noreply.github.com>
2020-03-17 22:58:04 +0100

    update(rimraf): update to version 3.0
    - v2 created
    - removed BUSYTRIES_MAX and EMFILE_MAX
    - other minor changes
    
    https://github.com/isaacs/rimraf/blob/master/rimraf.js
    
    Thanks!

--- types/rimraf/v2/index.d.ts
+++ types/rimraf/v2/index.d.ts
@@ -1,11 +1,12 @@
-// Type definitions for rimraf 3.0
+// Type definitions for rimraf 2.0
 // Project: https://github.com/isaacs/rimraf
 // Definitions by: Carlos Ballesteros Velasco <https://github.com/soywiz>
 //                 e-cloud <https://github.com/e-cloud>
 //                 Ruben Schmidmeister <https://github.com/bash>
 //                 Oganexon <https://github.com/oganexon>
-//                 Piotr Błażejewicz <https://github.com/peterblazejewicz>
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
+
+// Imported from: https://github.com/soywiz/typescript-node-definitions/rimraf.d.ts
 
 /// <reference types="node" />
 
@@ -15,20 +16,13 @@ import fs = require('fs');
 declare function rimraf(path: string, options: rimraf.Options, callback: (error: Error) => void): void;
 declare function rimraf(path: string, callback: (error: Error) => void): void;
 declare namespace rimraf {
-    /**
-     * It can remove stuff synchronously, too.
-     * But that's not so good. Use the async API.
-     * It's better.
-     */
+    function __promisify__(path: string, options?: Options): Promise<void>;
     function sync(path: string, options?: Options): void;
-
-    /**
-     * see {@link https://github.com/isaacs/rimraf/blob/79b933fb362b2c51bedfa448be848e1d7ed32d7e/README.md#options}
-     */
+    let EMFILE_MAX: number;
+    let BUSYTRIES_MAX: number;
     interface Options {
         maxBusyTries?: number;
         emfileWait?: number;
-        /** @default false */
         disableGlob?: boolean;
         glob?: glob.IOptions | false;
 
@@ -38,6 +32,7 @@ declare namespace rimraf {
         lstat?: typeof fs.lstat;
         rmdir?: typeof fs.rmdir;
         readdir?: typeof fs.readdir;
+
         unlinkSync?: typeof fs.unlinkSync;
         chmodSync?: typeof fs.chmodSync;
         statSync?: typeof fs.statSync;
