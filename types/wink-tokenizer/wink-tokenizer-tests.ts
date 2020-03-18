d30fa337d87c0a07ae3c9375dd4539b1cbdd97a2
lars klein <lars.klein@rwth-aachen.de>
2020-03-18 19:07:01 +0100

    formatting

--- types/wink-tokenizer/wink-tokenizer-tests.ts
+++ types/wink-tokenizer/wink-tokenizer-tests.ts
@@ -13,15 +13,15 @@ tokenizer.getTokensFP();
 // samples taken from the official API docs:
 // https://winkjs.org/wink-tokenizer/Tokenizer.html#getTokensFP
 // Adding a regex for an existing tag
-tokenizer.addRegex( /\(oo\)/gi, 'emoticon' );
-tokenizer.tokenize( '(oo) Hi!' )
+tokenizer.addRegex(/\(oo\)/gi, 'emoticon');
+tokenizer.tokenize('(oo) Hi!')
 // -> [ { value: '(oo)', tag: 'emoticon' },
 //      { value: 'Hi', tag: 'word' },
 //      { value: '!', tag: 'punctuation' } ]
 
 // Adding a regex to parse a new token type
-tokenizer.addRegex( /hello/gi, 'greeting', 'g' );
-tokenizer.tokenize( 'hello, how are you?' );
+tokenizer.addRegex(/hello/gi, 'greeting', 'g');
+tokenizer.tokenize('hello, how are you?');
 // -> [ { value: 'hello', tag: 'greeting' },
 //      { value: ',', tag: 'punctuation' },
 //      { value: 'how', tag: 'word' },
@@ -31,8 +31,8 @@ tokenizer.tokenize( 'hello, how are you?' );
 // Notice how "hello" is now tagged as "greeting" and not as "word".
 
 // Using definConfig will reset the above!
-tokenizer.defineConfig( { word: true } );
-tokenizer.tokenize( 'hello, how are you?' );
+tokenizer.defineConfig({ word: true });
+tokenizer.tokenize('hello, how are you?');
 // -> [ { value: 'hello', tag: 'word' },
 //      { value: ',', tag: 'punctuation' },
 //      { value: 'how', tag: 'word' },
@@ -40,8 +40,8 @@ tokenizer.tokenize( 'hello, how are you?' );
 //      { value: 'you', tag: 'word' },
 //      { value: '?', tag: 'punctuation' } ]
 // Do not tokenize & tag @mentions.
-tokenizer.defineConfig( { mention: false } );
+tokenizer.defineConfig({ mention: false });
 // -> 13
 // Only tokenize words as defined above.
-tokenizer.defineConfig( {} );
+tokenizer.defineConfig({});
 // -> 0
