8e9641bd0a4f2af551725fcee2824fabaf281a9f
Vincenzo Chianese <vincenz.chianese@icloud.com>
2020-03-19 07:13:37 -0500

    Update index.d.ts

--- types/postman-collection/index.d.ts
+++ types/postman-collection/index.d.ts
@@ -1,6 +1,7 @@
 // Type definitions for postman-collection 3.5
 // Project: https://github.com/postmanlabs/postman-collection
 // Definitions by: Kyle Buzby <https://github.com/kbuzby>
+// Definitions by: Vincenzo Chianese <https://github.com/XVincentX>
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
 // TypeScript Version: 2.4
 /// <reference types="node" />
@@ -359,7 +360,7 @@ export class HeaderList extends PropertyList<Header> {
 
 export interface ItemDefinition extends PropertyDefinition {
   request?: RequestDefinition;
-  responses?: ResponseDefinition[];
+  response?: ResponseDefinition[];
   events?: EventDefinition[];
 }
 
