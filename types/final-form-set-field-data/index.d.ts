3a1c46063371f0e2b974cbca6fd707579abb0f98
hikiko4ern <hikiko4ern@gmail.com>
2020-03-20 22:27:51 +0700

    fix github link

--- types/final-form-set-field-data/index.d.ts
+++ types/final-form-set-field-data/index.d.ts
@@ -1,6 +1,6 @@
 // Type definitions for final-form-set-field-data 1.0
 // Project: https://github.com/final-form/final-form-set-field-data#readme
-// Definitions by: hikiko4ern <https://github.com/me>
+// Definitions by: hikiko4ern <https://github.com/hikiko4ern>
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
 
 import { Mutator } from 'final-form';
