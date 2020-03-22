be2d773f493d378ea80182ea3320abf4c5c4a2c5
Eric Liu <ericyl.us@gmail.com>
2020-03-22 06:39:26 -0700

    feat(types): upgrade carbon__pictograms-react types to 10.9.1 (#43306)

--- types/carbon__pictograms-react/carbon__pictograms-react-tests.tsx
+++ types/carbon__pictograms-react/carbon__pictograms-react-tests.tsx
@@ -2,4 +2,4 @@ import * as React from 'react';
 import { Airplane } from '@carbon/pictograms-react';
 
 <Airplane />; // $ExpectType Element
-<Airplane aria-label="label" tabIndex="0" fill="#fff" className="className" />; // $ExpectType Element
+<Airplane title="airplane" aria-label="label" tabIndex="0" fill="#fff" className="className" />; // $ExpectType Element
