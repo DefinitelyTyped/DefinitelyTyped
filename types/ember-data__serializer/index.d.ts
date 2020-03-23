489d61b21ec91cf5d05d6547f2f5908f27fb54d4
James C. Davis <jdavis@salsify.com>
2020-03-23 16:29:28 -0400

    chore: update github url for @chriskrycho

--- types/ember-data__serializer/index.d.ts
+++ types/ember-data__serializer/index.d.ts
@@ -1,6 +1,6 @@
 // Type definitions for @ember-data/serializer 3.15
 // Project: https://github.com/emberjs/data
-// Definitions by: Chris Krycho <https://github.com/me>
+// Definitions by: Chris Krycho <https://github.com/chriskrycho>
 //                 Dan Freeman <https://github.com/dfreeman>
 //                 James C. Davis <https://github.com/jamescdavis>
 //                 Mike North <https://github.com/mike-north>
