860c9b8341981233563b0382fb1ae6a76d435df9
Melvin Mupondori <melmups@outlook.com>
2020-03-24 22:18:38 +0200

    Applied fixes from linter

--- types/searchjs/index.d.ts
+++ types/searchjs/index.d.ts
@@ -1,16 +1,14 @@
-// Type definitions for searchjs 1.0.2
+// Type definitions for searchjs 1.0
 // Project: https://github.com/deitch/searchjs
 // Definitions by: Melvin Mupondori <https://github.com/iammerus>
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
 
-declare module 'searchjs' {
-    export function setDefaults(options: any): void;
+export function setDefaults(options: any): void;
 
-    export function resetDefaults(): void;
+export function resetDefaults(): void;
 
-    export function singleMatch(field: any, s: any, text: any, word: any, regexp: any, start: any, end: any): any;
+export function singleMatch(field: any, s: any, text: any, word: any, regexp: any, start: any, end: any): any;
 
-    export function matchArray(ary: any[], search: any): any;
+export function matchArray(ary: any[], search: any): any;
 
-    export function matchObject(obj: any, search: any): any;
-}
+export function matchObject(obj: any, search: any): any;
