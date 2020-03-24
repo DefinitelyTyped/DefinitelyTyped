66fec07a047ded695f2b5e2bac64ef24d302ee6e
Melvin Mupondori <melmups@outlook.com>
2020-03-24 22:11:16 +0200

    Reformatted code

--- types/searchjs/index.d.ts
+++ types/searchjs/index.d.ts
@@ -3,16 +3,14 @@
 // Definitions by: Melvin Mupondori <https://github.com/iammerus>
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
 
-declare module "searchjs" {
-  export function setDefaults(
-      options: any
-   ): void;
+declare module 'searchjs' {
+    export function setDefaults(options: any): void;
 
-   export function resetDefaults() : void;
+    export function resetDefaults(): void;
 
-   export function singleMatch(field: any, s: any, text: any, word: any, regexp: any, start: any, end: any): any;
+    export function singleMatch(field: any, s: any, text: any, word: any, regexp: any, start: any, end: any): any;
 
-   export function matchArray(ary: any[], search: any) : any;
+    export function matchArray(ary: any[], search: any): any;
 
-   export function matchObject(obj: any, search: any) : any;
+    export function matchObject(obj: any, search: any): any;
 }
