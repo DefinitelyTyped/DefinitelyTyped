f83f91e8bea34fd5e5824615ea32614afffcf3a2
Vladlen <negezor@gmail.com>
2020-03-25 02:22:26 +1100

    chore(tough-cookie): update tests for 4 version

--- types/tough-cookie/tough-cookie-tests.ts
+++ types/tough-cookie/tough-cookie-tests.ts
@@ -1,26 +1,49 @@
 import { Cookie, CookieJar, MemoryCookieStore } from 'tough-cookie';
 
 let header = '';
-const cb = () => { };
 
 const cookie = Cookie.parse(header)!;
 cookie.value = 'somethingdifferent';
 header = cookie.toString();
 
-const cookiejar = new CookieJar();
-cookiejar.setCookie(cookie, 'http://currentdomain.example.com/path', cb);
-// ...
-cookiejar.getCookies('http://example.com/otherpath', (err, cookies) => {
-    // res.headers['cookie'] = cookies.join('; ');
+const cookieJarWithOptions = new CookieJar(new MemoryCookieStore(), {
+	allowSpecialUseDomain: true,
+	prefixSecurity: 'silent'
 });
 
-// All option are optional.
-cookiejar.getCookies('http://example.com/otherpath', {}, () => {});
+const cookieJar = new CookieJar();
+cookieJar.setCookie(cookie, 'http://currentdomain.example.com/path', (err, cookie) => {});
+cookieJar.setCookie(cookie, 'http://currentdomain.example.com/path')
+	.then((cookie) => {});
+// ...
+cookieJar.getCookies('http://example.com/otherpath', (err, cookies) => {});
+cookieJar.getCookies('http://example.com/otherpath')
+	.then((cookies) => {});
 
-cookiejar.getCookies('http://example.com/otherpath', {
+// All option are optional.
+cookieJar.getCookies('http://example.com/otherpath', {}, () => {});
+cookieJar.getCookies('http://example.com/otherpath', {})
+	.then((cookies) => {});
+
+cookieJar.getCookies('http://example.com/otherpath', {
     now: new Date(),
     allPaths: true,
 }, () => {});
+cookieJar.getCookies('http://example.com/otherpath', {
+    now: new Date(),
+    allPaths: true,
+})
+	.then((cookies) => {});
 
 CookieJar.deserializeSync("test cookie with store", new MemoryCookieStore());
 CookieJar.deserializeSync("test cookie");
+
+const store = new MemoryCookieStore();
+
+store.findCookies('example.com', 'path', (cookies) => {});
+store.findCookies('example.com', 'path')
+	.then((cookies) => {});
+
+store.findCookies('example.com', 'path', false, (err, cookies) => {});
+store.findCookies('example.com', 'path', false)
+	.then((cookies) => {});
