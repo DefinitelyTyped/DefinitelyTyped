57a26ea55c5900dc99c4ccfad86b5a6ae072f854
Nathan Shively-Sanders <293473+sandersn@users.noreply.github.com>
2020-03-24 10:19:50 -0700

    jquery: Add Document to Node
    And restore it to the return type of contents() as well --
    DocumentFragment is not correct there.

--- types/jquery/misc.other.d.ts
+++ types/jquery/misc.other.d.ts
@@ -4,7 +4,7 @@
 
 declare namespace JQuery {
     type TypeOrArray<T> = T | T[];
-    type Node = Element | Text | Comment | DocumentFragment;
+    type Node = Element | Text | Comment | Document | DocumentFragment;
 
     /**
      * A string is designated htmlString in jQuery documentation when it is used to represent one or more DOM elements, typically to be created and inserted in the document. When passed as an argument of the jQuery() function, the string is identified as HTML if it starts with <tag ... >) and is parsed as such until the final > character. Prior to jQuery 1.9, a string was considered to be HTML if it contained <tag ... > anywhere within the string.
