50975f527198459d5092577f5993a710fa58aa2d
arindamdawn <arindamdawn3@gmail.com>
2020-03-25 15:00:29 +0530

    updated any type with an interface

--- types/node-browser-history/index.d.ts
+++ types/node-browser-history/index.d.ts
@@ -3,13 +3,20 @@
 // Definitions by: Arindam Dawn <https://github.com/arindamdawn>
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
 
-export function getAllHistory(historyTimeLength?: number): Promise<any[]>;
-export function getFirefoxHistory(historyTimeLength?: number): Promise<any[]>;
-export function getSeaMonkeyHistory(historyTimeLength?: number): Promise<any[]>;
-export function getChromeHistory(historyTimeLength?: number): Promise<any[]>;
-export function getOperaHistory(historyTimeLength?: number): Promise<any[]>;
-export function getTorchHistory(historyTimeLength?: number): Promise<any[]>;
-export function getSafariHistory(historyTimeLength?: number): Promise<any[]>;
-export function getMaxthonHistory(historyTimeLength?: number): Promise<any[]>;
-export function getVivaldiHistory(historyTimeLength?: number): Promise<any[]>;
-export function getIEHistory(historyTimeLength?: number): Promise<any[]>;
+export interface BrowserHistory {
+    browser: string;
+    title: string;
+    url: string;
+    utc_time: number;
+}
+
+export function getAllHistory(historyTimeLength?: number): Promise<BrowserHistory[]>;
+export function getFirefoxHistory(historyTimeLength?: number): Promise<BrowserHistory[]>;
+export function getSeaMonkeyHistory(historyTimeLength?: number): Promise<BrowserHistory[]>;
+export function getChromeHistory(historyTimeLength?: number): Promise<BrowserHistory[]>;
+export function getOperaHistory(historyTimeLength?: number): Promise<BrowserHistory[]>;
+export function getTorchHistory(historyTimeLength?: number): Promise<BrowserHistory[]>;
+export function getSafariHistory(historyTimeLength?: number): Promise<BrowserHistory[]>;
+export function getMaxthonHistory(historyTimeLength?: number): Promise<BrowserHistory[]>;
+export function getVivaldiHistory(historyTimeLength?: number): Promise<BrowserHistory[]>;
+export function getIEHistory(historyTimeLength?: number): Promise<BrowserHistory[]>;
