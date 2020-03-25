4badc810691a2a75fafcd44d92e5eba8dc04136c
Daniel Rosenwasser <DanielRosenwasser@users.noreply.github.com>
2020-03-24 17:48:01 -0700

    Update index.d.ts

--- types/relay-test-utils/index.d.ts
+++ types/relay-test-utils/index.d.ts
@@ -1,7 +1,7 @@
 // Type definitions for relay-test-utils 6.0
 // Project: https://relay.dev
 // Definitions by: Renan Machado <https://github.com/renanmav>
-//               : Stephen Pittman <https://github.com/Stephen2>
+//                 Stephen Pittman <https://github.com/Stephen2>
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
 // TypeScript Version: 3.3
 
