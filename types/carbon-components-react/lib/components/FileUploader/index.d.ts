9b24ab7a4938085cb4f183d0c910891218ee221f
Sebastien GREGOIRE <sebastien.gregoire@ibm.com>
2020-03-26 12:01:15 +0100

    Bumped to carbon 10.7 + added missing types

--- types/carbon-components-react/lib/components/FileUploader/index.d.ts
+++ types/carbon-components-react/lib/components/FileUploader/index.d.ts
@@ -1,3 +1,5 @@
 export * from "./FileUploader";
+export * from "./FileUploaderItem";
+export * from "./FileUploaderDropContainer";
 export * from "./FileUploader.Skeleton";
 export { default } from './FileUploader';
