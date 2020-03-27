633eb9e9d47191d29887adf9aca52569de7f1b8b
Hannes Kindströmmer <me@brolaugh.com>
2020-03-27 16:43:37 +0100

    [plaid-link] Add destroy method to plaidHandler definition (#43413)
    * Add destroy method to plaidHandler definition
    
    The plaid handler has a `destroy()` method which is referenced in their documentation: https://plaid.com/docs/#destroy-function
    
    * Add myself to definitions header

--- types/plaid-link/index.d.ts
+++ types/plaid-link/index.d.ts
@@ -1,6 +1,7 @@
 // Type definitions for non-npm package plaid-link-browser 2.0
 // Project: https://github.com/plaid/link (Does not have to be to GitHub, but prefer linking to a source code repository rather than to a project website.)
 // Definitions by: Aaron Holderman <https://github.com/afholderman>
+//                 Hannes Kindströmmer <https://github.com/brolaugh>
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped/
 // TypeScript Version: 2.4
 
@@ -44,6 +45,7 @@ export namespace Plaid {
     interface LinkHandler {
         open: () => void;
         exit: (options?: ExitOptions) => void;
+        destroy: () => void;
         institutions: Institution[];
     }
 
