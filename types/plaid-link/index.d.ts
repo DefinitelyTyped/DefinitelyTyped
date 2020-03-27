a3bd07ac0beb5bd98e077c3df9173cfaffc9cf18
Hannes Kindströmmer <hannes@kindstrommer.se>
2020-03-27 13:33:12 +0100

    Add myself to definitions header

--- types/plaid-link/index.d.ts
+++ types/plaid-link/index.d.ts
@@ -1,6 +1,7 @@
 // Type definitions for non-npm package plaid-link-browser 2.0
 // Project: https://github.com/plaid/link (Does not have to be to GitHub, but prefer linking to a source code repository rather than to a project website.)
 // Definitions by: Aaron Holderman <https://github.com/afholderman>
+//                 Hannes Kindströmmer <https://github.com/brolaugh>
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped/
 // TypeScript Version: 2.4
 
