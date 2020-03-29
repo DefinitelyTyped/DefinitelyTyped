4a40a286fa72623b8e0a275337b14848495641ae
Mahruan <sgtmattis@gmail.com>
2020-03-29 23:59:38 +0200

    Add files via upload

--- types/tictactoejs/index.d.ts
+++ types/tictactoejs/index.d.ts
@@ -5,82 +5,76 @@
 
 export class TicTacToe {
   /**
-     * The game's constructor
-     * @param requestSize the board size ~ default: 3x3
-     */
+   * The game's constructor
+   */
   constructor(requestSize?: number);
   /**
-     * Restart the game
-     * @param requestSize the board size ~ default: 3x3
-     */
+   * Restart the game
+   */
   reset(requestSize?: number): void;
   /**
-     * Get the current game's size
-     * @returns the game size
-     */
+   * Get the current game's size
+   * @returns the game size
+   */
   getSize(): number;
   /**
-     * Check, whether it's X' turn or O's
-     * @returns whose turn it is
-     */
+   * Check, whether it's X' turn or O's
+   * @returns whose turn it is
+   */
   turn(): "X" | "O";
   /**
-     * Get the current board
-     * @returns the current board as ascii
-     */
+   * Get the current board
+   * @returns the current board as ascii
+   */
   ascii(): string;
   /**
-     * Get the current board
-     * @returns the current board as ascii (alternative style)
-     */
+   * Get the current board
+   * @returns the current board as ascii (alternative style)
+   */
   ascii2(): string;
   /**
-     * Does a move (bottom left is 1|1, bottom right is 3|1)
-     * @param x the x value @param y the y value
-     * @returns whether the move was successful
-     */
+   * Does a move (bottom left is 1|1, bottom right is 3|1)
+   * @returns whether the move was successful
+   */
   move(x: number, y: number): boolean;
   /**
-     * Check whether the provided coordinates exist on the current board
-     * @param x the x value to check @param y the y value to check
-     * @returns whether the current coordinates exist
-     */
+   * Check whether the provided coordinates exist on the current board
+   * @returns whether the current coordinates exist
+   */
   exists(x: number, y: number): boolean;
   /**
-     * Check which player occupies the provided coordinates
-     * @param x the x value to check @param y the y value to check
-     * @returns the player occupying these coordinates
-     */
+   * Check which player occupies the provided coordinates
+   * @returns the player occupying these coordinates
+   */
   get(x: number, y: number): "X" | "O" | null;
   /**
-     * Do a move with array style coordinates (top left is 0|0, top right is 0|2)
-     * @param x the x value @param y the y value
-     * @returns whether the move was successful
-     */
+   * Do a move with array style coordinates (top left is 0|0, top right is 0|2)
+   * @returns whether the move was successful
+   */
   moveArray(row: number, col: number): boolean;
   /**
-     * Check the game's current state
-     * @returns the game state
-     */
+   * Check the game's current state
+   * @returns the game state
+   */
   status(): "X" | "O" | "draw" | "in progress";
   /**
-     * Check whether the game is over
-     * @returns the game state
-     */
+   * Check whether the game is over
+   * @returns the game state
+   */
   gameOver(): boolean;
   /**
-     * Check whether the game ended in a draw
-     * @returns the game state
-     */
+   * Check whether the game ended in a draw
+   * @returns the game state
+   */
   isDraw(): boolean;
   /**
-     * Check the valid moves (all unoccupied coordinates)
-     * @returns an array of objects with the x and y values
-     */
+   * Check the valid moves (all unoccupied coordinates)
+   * @returns an array of objects with the x and y values
+   */
   legalMoves(): [{ x: number; y: number }];
   /**
-     * Do a random move (no logic or AI involved, this is completely random)
-     * @returns an object with the x and y value of the random move
-     */
+   * Do a random move (no logic or AI involved, this is completely random)
+   * @returns an object with the x and y value of the random move
+   */
   randomMove(): { x: number; y: number };
 }
