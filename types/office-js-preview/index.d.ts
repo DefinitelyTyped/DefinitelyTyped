3b86ddaec15ded58dd225a72defe371bff4b7e56
AlexJerabek <aljerabe@microsoft.com>
2020-03-30 11:34:06 -0700

    Note about suspendScreenUpdatingUntilNextSync behavior

--- types/office-js-preview/index.d.ts
+++ types/office-js-preview/index.d.ts
@@ -21199,7 +21199,9 @@ declare namespace Excel {
          */
         suspendApiCalculationUntilNextSync(): void;
         /**
-         * Suspends sceen updating until the next "context.sync()" is called.
+         * Suspends screen updating until the next `context.sync()` is called.
+         * 
+         * **Note**: Don't call `suspendScreenUpdatingUntilNextSync` repeatedly (such as in a loop). Repeated calls will cause the Excel window to flicker.
          *
          * [Api set: ExcelApi 1.9]
          */
