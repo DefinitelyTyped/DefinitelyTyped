ad698d5383017c907c7f0df3c97bb0d337c87a62
Darius Calliet <darius@epharmix.com>
2020-03-30 10:07:22 -0500

    update zipcodes version number

--- types/zipcodes/index.d.ts
+++ types/zipcodes/index.d.ts
@@ -1,4 +1,4 @@
-// Type definitions for zipcodes 6.1
+// Type definitions for zipcodes 8.0
 // Project: https://github.com/davglass/zipcodes#readme
 // Definitions by: Brayden Lopez <https://github.com/headdetect>, Dobes Vandermeer <https://github.com/dobesv>
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
