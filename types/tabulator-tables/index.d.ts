ad45641482fdecb1f60cb33e01c5b8dd215f8b9e
Jojoshua <jojoshua@gmail.com>
2020-03-30 15:32:32 -0400

    update typescript version

--- types/tabulator-tables/index.d.ts
+++ types/tabulator-tables/index.d.ts
@@ -2,7 +2,7 @@
 // Project: http://tabulator.info
 // Definitions by: Josh Harris <https://github.com/jojoshua>
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
-// TypeScript Version: 2.3
+// TypeScript Version: 3.7
 
 // tslint:disable:max-line-length
 // tslint:disable:jsdoc-format
