e42f01cfb90843a2d93930dcf93889b02d26f634
Mustafa Salaheldin <mustafa.salaheldin@yahoo.com>
2020-03-31 11:18:03 +0400

    unifi signature
    unifi signature

--- types/bootstrap-filestyle/index.d.ts
+++ types/bootstrap-filestyle/index.d.ts
@@ -22,70 +22,9 @@ interface FilestyleOptions {
 interface JQuery {
     filestyle(options?: FilestyleOptions): JQuery;
 
-    /**
-     * Clear selected files.
-     */
-    filestyle(method: 'clear'): JQuery;
+    filestyle(method: 'clear' | 'destroy' | 'htmlIcon' | 'htmlInput' | 'pushNameFiles'): JQuery;
 
-    /**
-     * Destroy a filestyle completely.
-     */
-    filestyle(method: 'destroy'): JQuery;
+    filestyle(method: 'disabled' | 'buttonBefore' | 'icon' | 'input', value: boolean): JQuery;
 
-    /**
-     * Disabled button. Type: Bool, Default: false
-     */
-    filestyle(method: 'disabled', value: boolean): JQuery;
-
-    /**
-     * Button before. Type: Bool, Default: false
-     */
-    filestyle(method: 'buttonBefore', value: boolean): JQuery;
-
-    /**
-     * Enables or disables the button icon. Type: Boolean, Default: true
-     */
-    filestyle(method: 'icon', value: boolean): JQuery;
-
-    /**
-     * Enables or disables the input text. Type: Boolean, Default: true. 
-     */
-    filestyle(method: 'input', value: boolean): JQuery;
-
-    /**
-     * Change size bootstrap of the button and input. Type: String, Default: "nr"
-     */
-    filestyle(method: 'size', value: string): JQuery;
-
-    /**
-     * Setting placeholder attribute. Type: String, Default:""
-     */
-    filestyle(method: 'placeholder', value: string): JQuery;
-
-    /**
-     * Changes the button text. Type: String, Default: "Choose file"
-     */
-    filestyle(method: 'buttonText', value: string): JQuery;
-
-    /**
-     * Change classes bootstrap button. Type: String, Default: "btn-default"
-     */
-    filestyle(method: 'buttonName', value: string): JQuery;
-
-    /**
-     * Change classes bootstrap icons. Type: String, Default: "glyphicon-folder-open"
-     */
-    filestyle(method: 'iconName', value: string): JQuery;
-
-    /**
-     * Set or get the value of the icon option
-     */
-    filestyle(method: 'htmlIcon'): JQuery;
-
-    filestyle(method: 'htmlInput'): JQuery;
-    /**
-     * puts the name of the input files
-     * return files
-     */
-    filestyle(method: 'pushNameFiles'): JQuery;
+    filestyle(method: 'size' | 'placeholder' | 'buttonText' | 'buttonName' | 'iconName', value: string): JQuery;
 }
