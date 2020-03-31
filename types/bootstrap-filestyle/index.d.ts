4b83d068b18b82dc3c5f3a80b0c4251364cd4320
Mustafa Salaheldin <mustafa.salaheldin@yahoo.com>
2020-03-31 10:16:31 +0400

    adding comments
    adding comments

--- types/bootstrap-filestyle/index.d.ts
+++ types/bootstrap-filestyle/index.d.ts
@@ -1,14 +1,11 @@
-// Type definitions for bootstrap-filestyle 0.9
-// Project: https://github.com/davidstutz/bootstrap-filestyle
-// Definitions by: Makoto Schoppert <https://github.com/mak0t0san>
+// Type definitions for bootstrap-filestyle 1.0
+// Project: https://github.com/markusslima/bootstrap-filestyle
+// Definitions by: Mustafa Salaheldin <https://github.com/mustafasalahuldin>
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
 // TypeScript Version: 2.3
 
 /// <reference types="jquery"/>
 
-/**
- * See {@link https://davidstutz.github.io/bootstrap-filestyle/#configuration-options|https://davidstutz.github.io/bootstrap-filestyle/#configuration-options}
- */
 interface FilestyleOptions {
     buttonText?: string;
     iconName?: string;
@@ -26,90 +23,69 @@ interface JQuery {
     filestyle(options?: FilestyleOptions): JQuery;
 
     /**
-     * This method is used to destroy the plugin on the given element - meaning unbinding the plugin.
+     * Clear selected files.
      */
     filestyle(method: 'clear'): JQuery;
 
     /**
-     * This method is used to refresh the checked checkboxes based on the currently selected options within the select. 
-     * Click 'Select some options' to select some of the options. Then click refresh. 
-     * The plugin will update the checkboxes accordingly.
+     * Destroy a filestyle completely.
      */
     filestyle(method: 'destroy'): JQuery;
 
     /**
-     * Rebuilds the whole dropdown menu. All selected options will remain selected (if still existent!).
+     * Disabled button. Type: Bool, Default: false
      */
     filestyle(method: 'disabled', value: boolean): JQuery;
 
     /**
-     * Selects an option by its value. Works also using an array of values.
-     * @param triggerOnChange  If set to true, the method will manually trigger the onChange option.
+     * Button before. Type: Bool, Default: false
      */
     filestyle(method: 'buttonBefore', value: boolean): JQuery;
 
     /**
-     * Deselect an option by its value. Works also using an array of values.
-     * @param triggerOnChange  If set to true, the method will manually trigger the onChange option.
+     * Enables or disables the button icon. Type: Boolean, Default: true
      */
     filestyle(method: 'icon', value: boolean): JQuery;
 
     /**
-     * Selects all options. 
-     * @param justVisible If set to true or not provided, all visible options are selected (when using the filter), 
-     * otherwise (justVisible set to false) all options are selected.
+     * Enables or disables the input text. Type: Boolean, Default: true. 
      */
     filestyle(method: 'input', value: boolean): JQuery;
 
     /**
-     * Deselects all options. 
-     * @param justVisible If set to true or not provided, all visible options are deselected,  
-     * otherwise (justVisible set to false) all options are deselected.
+     * Change size bootstrap of the button and input. Type: String, Default: "nr"
      */
     filestyle(method: 'size', value: string): JQuery;
 
     /**
-     * When manually selecting/deselecting options and the corresponding checkboxes, this function updates the text and title of the button.
-     * 
-     * Note that usually this method is only needed when using .filestyle('selectAll', justVisible) or .filestyle('deselectAll', justVisible). In all other cases, .filestyle('refresh') should be used.
+     * Setting placeholder attribute. Type: String, Default:""
      */
     filestyle(method: 'placeholder', value: string): JQuery;
 
     /**
-     * Used to change configuration after initializing the filestyle. This may be useful in combination with {@link filestyle('rebuild')}.
-     * @example  
-     * $('#example-setOptions').filestyle('setOptions', options);
-     * $('#example-setOptions').filestyle('rebuild');
+     * Changes the button text. Type: String, Default: "Choose file"
      */
     filestyle(method: 'buttonText', value: string): JQuery;
 
     /**
-     * Disable both the underlying select and the dropdown button.
+     * Change classes bootstrap button. Type: String, Default: "btn-default"
      */
     filestyle(method: 'buttonName', value: string): JQuery;
 
     /**
-     * Enable both the underlying select and the dropdown button.
+     * Change classes bootstrap icons. Type: String, Default: "glyphicon-folder-open"
      */
     filestyle(method: 'iconName', value: string): JQuery;
 
     /**
-     * This method is used to provide options programmatically
+     * Set or get the value of the icon option
      */
     filestyle(method: 'htmlIcon'): JQuery;
 
-    /**
-     * This method is used to programmatically provide a new text to display in the button when all options are selected, at runtime.
-     */
     filestyle(method: 'htmlInput'): JQuery;
-
     /**
-     * This method is used to programmatically provide a new text to display in the button when all options are selected, at runtime.
+     * puts the name of the input files
+     * return files
      */
     filestyle(method: 'pushNameFiles'): JQuery;
-
-    /**
-     * This method is used to programmatically provide a new text to display in the button when all options are selected, at runtime.
-     */
-    //filestyle(method: 'constructor'): JQuery;
 }
