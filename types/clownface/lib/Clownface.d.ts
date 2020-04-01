4217777839d74ee9c8d949ae12eecc7406810194
Tomasz Pluskiewicz <tpluscode@users.noreply.github.com>
2020-04-01 18:55:10 +0200

    clownface: subject traversal only find blank and named nodes (#43538)
    * clownface: subject traversal only find blank and named nodes
    
    * fix(cownface): class did not implement interface correctly

--- types/clownface/lib/Clownface.d.ts
+++ types/clownface/lib/Clownface.d.ts
@@ -1,4 +1,4 @@
-import { BlankNode, DatasetCore, Literal, NamedNode, Quad_Graph, Term, Variable, DefaultGraph, Quad } from 'rdf-js';
+import { BlankNode, DatasetCore, Literal, NamedNode, Term } from 'rdf-js';
 import {
     Clownface as ClownfaceContract,
     ClownfaceInit,
@@ -18,9 +18,9 @@ import { Context } from './Context';
 declare class Clownface<T extends AnyContext = AnyContext, D extends DatasetCore = DatasetCore> implements ClownfaceContract<T, D> {
     constructor(options: ClownfaceInit<D> | ClownfaceInitWithTerms<Term | Term[], D> | ClownfaceInitWithValue<D> | ClownfaceInitWithValues<D>);
 
-    readonly term: T extends undefined ? undefined : T extends any[] ? undefined : T;
+    readonly term: T extends undefined ? undefined : T extends any[] ? undefined | T[0] : T;
     readonly terms: T extends undefined ? Term[] : T extends any[] ? T : [T];
-    readonly value: T extends undefined ? undefined : T extends any[] ? undefined : string;
+    readonly value: T extends undefined ? undefined : T extends any[] ? undefined | string[0] : string;
     readonly values: T extends undefined ? string[] : T extends any[] ? string[] : [string];
     readonly dataset: D;
     readonly datasets: D[];
@@ -51,10 +51,10 @@ declare class Clownface<T extends AnyContext = AnyContext, D extends DatasetCore
     namedNode(value: SingleOrOneElementArray<string | NamedNode>): Clownface<NamedNode, D>;
     namedNode(values: Array<string | NamedNode>): Clownface<NamedNode[], D>;
 
-    in(predicates?: SingleOrArrayOfTerms<Term>): Clownface<T extends undefined ? never : Term[], D>;
+    in(predicates?: SingleOrArrayOfTerms<Term>): Clownface<T extends undefined ? never : Array<NamedNode | BlankNode>, D>;
     out(predicates?: SingleOrArrayOfTerms<Term>): Clownface<T extends undefined ? never : Term[], D>;
 
-    has<X extends Term = Term>(predicates: SingleOrArrayOfTerms<Term>, objects?: SingleOrArrayOfTermsOrLiterals<X>): Clownface<X[], D>;
+    has(predicates: SingleOrArrayOfTerms<Term>, objects?: SingleOrArrayOfTermsOrLiterals<Term>): Clownface<Array<NamedNode | BlankNode>, D>;
 
     addIn(predicates: SingleOrArrayOfTerms<Term>, callback?: AddCallback<D, BlankNode>): Clownface<T, D>;
     addIn<X extends Term = Term>(predicates: SingleOrArrayOfTerms<Term>, objects: SingleOrArrayOfTermsOrLiterals<X>, callback?: AddCallback<D, X>): Clownface<T, D>;
