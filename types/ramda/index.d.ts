055c8fcfbf8808325a1cd4bab7ac0a3b7b8b9c37
Vanya Dymovsky <dymouskyia@gmail.com>
2020-04-01 19:43:03 +0300

    Ramda types: R.into add case of converting types: R.into<From, To>([], transducer, sourceList); (#43359)
    Co-authored-by: Ivan Dymousky <Ivan_Dymousky@epam.com>

--- types/ramda/index.d.ts
+++ types/ramda/index.d.ts
@@ -833,6 +833,7 @@ export function intersperse<T>(separator: T): (list: readonly T[]) => T[];
  * using an appropriate iterator function based on the accumulator type.
  */
 export function into<T>(acc: any, xf: (...a: readonly any[]) => any, list: readonly T[]): T[];
+export function into<T, R>(acc: any, xf: (...a: readonly any[]) => R[], list: readonly T[]): R[];
 export function into(acc: any, xf: (...a: readonly any[]) => any): <T>(list: readonly T[]) => T[];
 export function into(acc: any): <T>(xf: (...a: readonly any[]) => any, list: readonly T[]) => T[];
 
