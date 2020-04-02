d638063d006839ba0457b4ecdf0ec7c27140d3f4
nikewu <862751557@qq.com>
2020-04-02 13:30:28 +0800

    fix: fix lint error

--- types/feflow__cli/index.d.ts
+++ types/feflow__cli/index.d.ts
@@ -1,67 +1,65 @@
-// Type definitions for @feflow/cli 0.18.3
+// Type definitions for @feflow/cli 0.18
 // Project: https://github.com/Tencent/feflow/releases
 // Definitions by: nikewu <https://github.com/nikewu>
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
 
 /// <reference types="node"/>
- /**
-  *  feflow namespace
- **/
+/**
+ *  feflow namespace
+ */
 declare namespace fef {
-
     /**
-    * feflow context
-    */
+     * feflow context
+     */
     export interface FeflowCtx {
         /**
          * Feflow context params
-        **/
+         */
         args: {
-            _: (string | number)[]
+            _: Array<number | string>
         };
         /**
-         * Feflow configuration file in the current directory  
+         * Feflow configuration file in the current directory
          * ( .feflowrc.json/.feflowrc.js/.feflowrc.yml )
-        **/
+         */
         projectConfig: string;
         /**
          * Current project path
-        **/
+         */
         projectPath: string;
         /**
          * Feflow version
-        **/
+         */
         version: string;
         /**
          * Feflow logger includes: (trace、debug、info、warn、error、fatal)
-        **/
+         */
         logger: Logger;
         /**
          * Feflow command includes: (register)
-        **/
+         */
         commander: Command;
         /**
          * Feflow hook includes: (hook)
-        **/
+         */
         hook: Hook;
         /**
          * Feflow home directory path
-        **/
+         */
         root: string;
         /**
          * Feflow home directory package.json path
-        **/
+         */
         rootPkg: string;
         /**
          * feflow config
-        **/
+         */
         config: string;
         /**
          * feflow config path
-        **/
+         */
         configPath: string;
     }
-
 }
 
 interface Command {
@@ -71,7 +69,7 @@ interface Command {
      * @param desc Plugin command description eg： Feflow devtool for better develop a devkit or plugin
      * @param fn   Plugin callback function
      */
-    register(cmd: string, desc: string, fn: Function): void
+    register(cmd: string, desc: string, fn: () => void): void;
 }
 
 interface Hook {
@@ -81,7 +79,7 @@ interface Hook {
      * @param type  Hook name eg：report
      * @param fn  Hook callback function eg：report
      */
-    hook(type: string, fn: Function): void
+    hook(type: string, fn: () => void): void;
 }
 
 interface Logger {
@@ -90,36 +88,34 @@ interface Logger {
      * Print trace level log, The Color is gray
      * @param desc log detail 
      */
-    trace(desc: string): void
-
+    trace(desc: string): void;
     /**
      * Print trace level log, The Color is gray
      * @param desc log detail
      */
-    debug(desc: string): void
+    debug(desc: string): void;
 
     /**
      * Print trace level log, The Color is green
      * @param desc log detail
      */
-    info(desc: string): void
+    info(desc: string): void;
 
     /**
      * Print trace level log, The Color is yellow
      * @param desc log detail
      */
-    warn(desc: string): void
-
-
-    /**
-     * Print trace level log, The Color is red
-     * @param desc log detail
-     */
-    error(desc: string): void
+    warn(desc: string): void;
 
     /**
      * Print trace level log, The Color is red
      * @param desc log detail
      */
-    fatal(desc: string): void
+    error(desc: string): void;
+
+    /**
+     * Print trace level log, The Color is red
+     * @param desc log detail
+     */
+    fatal(desc: string): void;
 }
