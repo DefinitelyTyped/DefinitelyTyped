7d7a1ab609d93af9c62f4f7e1b971be2857a5da5
serinuntius <wasab1gj@gmail.com>
2020-04-02 18:22:26 +0900

    fixed to enum

--- types/node-apple-receipt-verify/index.d.ts
+++ types/node-apple-receipt-verify/index.d.ts
@@ -3,40 +3,42 @@
 // Definitions by: serinuntius <https://github.com/serinuntius>
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
 
-export interface ERROR_CODES {
+export enum ERROR_CODES {
     /** The receipt validated successfully. */
-    SUCCESS: 0;
+    SUCCESS = 0,
 
     /** The receipt is valid, but purchased nothing. */
-    VALID_BUT_EMPTY: 2;
+    VALID_BUT_EMPTY = 2,
 
     /** The App Store could not read the JSON object you provided. */
-    INVALID_JSON: 21000;
+    INVALID_JSON = 21000,
 
     /** The data in the receipt-data property was malformed or missing. */
-    INVALID_RECEIPT_DATA: 21002;
+    INVALID_RECEIPT_DATA = 21002,
 
     /** The receipt could not be authenticated. */
-    COULD_NOT_AUTHENTICATE: 21003;
+    COULD_NOT_AUTHENTICATE = 21003,
 
     /** The shared secret you provided does not match the shared secret on file for your account. */
-    INVALID_SECRET: 21004;
+    INVALID_SECRET = 21004,
 
     /** The receipt server is not currently available. */
-    UNAVAILABLE: 21005;
+    UNAVAILABLE = 21005,
 
-    /** This receipt is valid but the subscription has expired. When this status code is returned to your server, the receipt data is also decoded and returned as part of the response.
-     Only returned for iOS 6 style transaction receipts for auto-renewable subscriptions. */
-    EXPIRED_SUBSCRIPTION: 21006;
+    /**
+     * This receipt is valid but the subscription has expired. When this status code is returned to your server, the receipt data is also decoded and returned as part of the response.
+     * Only returned for iOS 6 style transaction receipts for auto-renewable subscriptions.
+     */
+    EXPIRED_SUBSCRIPTION = 21006,
 
     /** This receipt is from the test environment, but it was sent to the production environment for verification. Send it to the test environment instead. */
-    TEST_RECEIPT: 21007;
+    TEST_RECEIPT = 21007,
 
     /** This receipt is from the production environment, but it was sent to the test environment for verification. Send it to the production environment instead. */
-    PROD_RECEIPT: 21008;
+    PROD_RECEIPT = 21008,
 
     /** This receipt could not be authorized. Treat this the same as if a purchase was never made. */
-    COULD_NOT_AUTHORIZE: 21010;
+    COULD_NOT_AUTHORIZE = 21010
 }
 
 export interface ConfigOptions {
@@ -88,4 +90,4 @@ export function validate(options: ValidateOptions): Promise<PurchasedProducts[]>
 
 export function resetConfig(): void;
 
-export function extract(): Promise<{}[]>;
+export function extract(): Promise<Array<{}>>;
