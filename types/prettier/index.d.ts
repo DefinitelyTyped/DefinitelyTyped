f04a415b37327e4bf5c9edcd26b4765ef1d9643d
Sosuke Suzuki <aosukeke@gmail.com>
2020-04-02 19:37:41 +0900

    Fix version

--- types/prettier/index.d.ts
+++ types/prettier/index.d.ts
@@ -1,4 +1,4 @@
-// Type definitions for prettier 1.19
+// Type definitions for prettier 2.0
 // Project: https://github.com/prettier/prettier, https://prettier.io
 // Definitions by: Ika <https://github.com/ikatyang>,
 //                 Ifiok Jr. <https://github.com/ifiokjr>,
