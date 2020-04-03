3e31321ffc8ad7b54401647c93e0f626bc429cf3
Andrea Ascari <dev.ascariandrea@gmail.com>
2020-04-04 01:12:00 +0200

    feat: added node-pdftk definitions (#43602)

---
+++ types/node-pdftk/node-pdftk-tests.ts
@@ -0,0 +1,17 @@
+import * as PDFTK from 'node-pdftk';
+
+PDFTK.configure({
+    bin: '/usr/local/bin/pdftk',
+    Promise,
+    ignoreWarnings: true,
+    tempDir: './pdftk',
+});
+
+const pdftk = PDFTK.input('file'); // $ExpectType PDFTK
+
+pdftk.allow(['FillIn']).attachFiles(['./file1.pdf', './file2.pdf']); // $ExpectType PDFTK
+
+pdftk.allow(['FillIn']).compress().output('./fileoutput.pdf'); // $ExpectType Promise<Buffer>
+pdftk.allow(['FillIn']).compress().output('./fileoutput.pdf', './destination/folder'); // $ExpectType Promise<string>
+PDFTK.input('file').flatten().ignoreWarnings().inputPw('password').burst('page_%02d.pdf'); // $ExpectType Promise<string>
+PDFTK.input('file').flatten().ignoreWarnings().inputPw('password').burst(); // $ExpectType Promise<Buffer>
