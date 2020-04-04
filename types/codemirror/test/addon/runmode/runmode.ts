bbf9d4828a8b3a50102dcf353b5325e9c1a167c9
mtgto <hogerappa@gmail.com>
2020-04-04 23:38:36 +0900

    [codemirror] Add lacked arguments of runMode

--- types/codemirror/test/addon/runmode/runmode.ts
+++ types/codemirror/test/addon/runmode/runmode.ts
@@ -4,3 +4,4 @@ import "codemirror/addon/runmode/runmode";
 const query = "SELECT * FROM Table";
 
 CodeMirror.runMode(query, "text/x-sql", document.body);
+CodeMirror.runMode(query, "text/x-sql", (text, style, row, column, state) => {});
