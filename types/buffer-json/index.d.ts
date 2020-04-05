867d5ca00b3a56e2c7605b4f4c462c956ef652df
Ayyan Lewis <ayyanlewis@gmail.com>
2020-04-05 15:00:41 -0700

    Update index.d.ts

--- types/buffer-json/index.d.ts
+++ types/buffer-json/index.d.ts
@@ -4,11 +4,9 @@
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
 
 /**
- * Converts a JavaScript object or value to a JSON string, optionally replacing values if a replacer function is specified or optionally including only the specified properties if a replacer array is specified.
+ * Converts a JavaScript object or value to a JSON string.
  * @param value The value to convert to a JSON string.
  * @param space A String or Number object that's used to insert white space into the output JSON string for readability purposes.
- * If this is a Number, it indicates the number of space characters to use as white space; this number is capped at 10 (if it is greater, the value is just 10). Values less than 1 indicate that no space should be used.
- * If this is a String, the string (or the first 10 characters of the string, if it's longer than that) is used as white space. If this parameter is not provided (or is null), no white space is used.
  * @return A JSON string representing the given value.
  */
 export function stringify(value: any, space?: string | number): string;
