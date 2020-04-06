856d7b526a1686502cee01cb52223e9216c69f34
josh <joshua.hunt@vista.co>
2020-04-07 07:57:27 +1200

    Add text-mask-addons and text-mask-core types

---
+++ types/text-mask-addons/index.d.ts
@@ -0,0 +1,14 @@
+// Type definitions for text-mask-addons 3.8
+// Project: https://github.com/text-mask/text-mask/tree/master/addons/#readme
+// Definitions by: josh <https://github.com/huntjosh>
+// Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
+
+declare module 'text-mask-addons' {
+    import { Pipe } from 'text-mask-core';
+
+    export interface DatePipeYears {
+        minYear: number;
+        maxYear: number;
+    }
+    export function createAutoCorrectedDatePipe(dateFormat?: string, validYears?: DatePipeYears): Pipe;
+}
