92589048c21e5f1c18bd384174ef2641d0419892
Tomasz Pluskiewicz <tpluscode@users.noreply.github.com>
2020-04-06 20:08:40 +0200

    refacor: use interface merging to simplify class declarations (#43604)

--- types/sparql-http-client/Endpoint.d.ts
+++ types/sparql-http-client/Endpoint.d.ts
@@ -41,19 +41,12 @@ declare namespace Endpoint {
     }
 }
 
-declare class Endpoint implements Endpoint.Endpoint {
+// tslint:disable-next-line no-empty-interface
+interface Endpoint extends Endpoint.Endpoint {}
+
+// tslint:disable-next-line no-unnecessary-class
+declare class Endpoint {
     constructor(options: Endpoint.EndpointOptions);
-    endpointUrl: string;
-    fetch: typeof fetch;
-    headers: HeadersInit;
-    password?: string;
-    storeUrl?: string;
-    updateUrl?: string;
-    user?: string;
-    get(query: string, options?: Endpoint.RequestOptions): Promise<Response>;
-    postDirect(query: string, options?: Endpoint.RequestOptions): Promise<Response>;
-    postUrlencoded(query: string, options?: Endpoint.RequestOptions): Promise<Response>;
-    mergeHeaders(args?: HeadersInit): Headers;
 }
 
 export = Endpoint;
