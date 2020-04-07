68698763ee784ccf0a5b5ac050ca9cb03a2838d3
Andrea Ascari <dev.ascariandrea@gmail.com>
2020-04-07 17:53:18 +0200

    fix: nullable value for node-pdftk output method (#43672)

--- types/node-pdftk/node-pdftk-tests.ts
+++ types/node-pdftk/node-pdftk-tests.ts
@@ -13,5 +13,7 @@ pdftk.allow(['FillIn']).attachFiles(['./file1.pdf', './file2.pdf']); // $ExpectT
 
 pdftk.allow(['FillIn']).compress().output('./fileoutput.pdf'); // $ExpectType Promise<Buffer>
 pdftk.allow(['FillIn']).compress().output('./fileoutput.pdf', './destination/folder'); // $ExpectType Promise<string>
+pdftk.allow(['FillIn']).compress().output(); // $ExpectType Promise<Buffer>
+
 PDFTK.input('file').flatten().ignoreWarnings().inputPw('password').burst('page_%02d.pdf'); // $ExpectType Promise<string>
 PDFTK.input('file').flatten().ignoreWarnings().inputPw('password').burst(); // $ExpectType Promise<Buffer>
