029706d7552320e2c2de25017581710417e99b49
Robert Wettstädt <robert.wett@gmail.com>
2020-04-08 10:11:35 +0200

    [iv-viewer] Add missing class fields types

--- types/iv-viewer/index.d.ts
+++ types/iv-viewer/index.d.ts
@@ -12,8 +12,125 @@ interface Options {
     zoomOnMouseWheel?: boolean;
 }
 
+interface SliderOptions {
+    isSliderEnabled: () => boolean;
+    onStart: () => void;
+    onMove: () => void;
+    onEnd: () => void;
+}
+
+declare class Slider {
+    constructor(container: Element | null, options: SliderOptions);
+
+    container: Element | null;
+    isSliderEnabled: () => boolean;
+    onStart: () => void;
+    onMove: () => void;
+    onEnd: () => void;
+
+    startHandler(event: Event): void;
+    moveHandler(event: Event): void;
+    endHandler(): void;
+    removeListeners(): void;
+    init(): void;
+    destroy(): void;
+}
+
+interface Elements {
+    container: Element | null;
+    domElement: Element | null;
+    image?: Element | null;
+    imageWrap?: Element | null;
+    snapHandle?: Element | null;
+    snapImage?: Element | null;
+    snapImageWrap?: Element | null;
+    snapView?: Element | null;
+    zoomHandle?: Element | null;
+}
+
+type IVEvent = () => void;
+
+interface Events {
+    hiResImageLoad?: IVEvent;
+    imageLoad?: IVEvent;
+    mouseEnterSnapView?: IVEvent;
+    mouseLeaveSnapView?: IVEvent;
+    onWindowResize?: IVEvent;
+    pinchStart?: IVEvent;
+    snapViewOnMouseMove?: IVEvent;
+}
+
+interface Frames {
+    slideMomentumCheck?: number;
+    sliderMomentumFrame?: number;
+    snapViewTimeout?: number;
+    zoomFrame?: number;
+}
+
+interface Sliders {
+    imageSlider?: Slider;
+    snapSlider?: Slider;
+    zoomSlider?: Slider;
+}
+
+type IVImage = string | null;
+
+interface Images {
+    imageSrc: IVImage;
+    hiResImageSrc: IVImage;
+}
+
+interface Dim {
+    w: number;
+    h: number;
+}
+
+interface State {
+    containerDim?: Dim;
+    imageDim?: Dim;
+    loaded?: boolean;
+    snapHandleDim?: Dim;
+    snapImageDim?: Dim;
+    snapViewVisible?: boolean;
+    zooming?: boolean;
+    zoomSliderLength?: number;
+    zoomValue?: number;
+}
+
 declare class ImageViewer {
     constructor(element: Element | null, options?: Options);
+
+    protected _elements: Elements;
+    protected _events: Events;
+    protected _frames: Frames;
+    protected _images: Images;
+    protected _options: Options;
+    protected _sliders: Sliders;
+    protected _state: State;
+
+    protected _calculateDimensions(): void;
+    protected _doubleTapToZoom(): void;
+    protected _findContainerAndImageSrc(
+        element: string | Element | null,
+    ): {
+        container: Element | null;
+        domElement: Element | null;
+        imageSrc: IVImage;
+        hiResImageSrc: IVImage;
+    };
+    protected _getImageCurrentDim(): void;
+    protected _init(): void;
+    protected _initDom(): void;
+    protected _initEvents(): void;
+    protected _initImageSlider(): void;
+    protected _initSnapSlider(): void;
+    protected _initZoomSlider(): void;
+    protected _loadHighResImage(): void;
+    protected _loadImages(): void;
+    protected _pinchAndZoom(): void;
+    protected _scrollZoom(): void;
+    protected _snapViewEvents(): void;
+
     destroy(): void;
     hideSnapView(): void;
     load(imageSrc: string, hiResImageSrc?: string): void;
@@ -23,14 +140,40 @@ declare class ImageViewer {
     zoom(perc: number, point?: { x: number; y: number }): void;
 }
 
-declare namespace ImageViewer {
-    class FullScreenViewer extends ImageViewer {
-        constructor(options?: Options);
-        hide: () => void;
-        show: (imageSrc: string, hiResImageSrc?: string) => void;
-    }
+interface FullScreenElements extends Elements {
+    fullScreen: Element | null;
+}
 
-    export { ImageViewer, FullScreenViewer };
+interface FullScreenEvents extends Events {
+    onCloseBtnClick?: IVEvent;
+    onWindowResize?: IVEvent;
+}
+
+declare class FullScreenViewer extends ImageViewer {
+    constructor(options?: Options);
+
+    protected _elements: FullScreenElements;
+    protected _events: FullScreenEvents;
+
+    protected _initFullScreenEvents(): void;
+    hide(): void;
+    show(imageSrc: string, hiResImageSrc?: string): void;
+}
+
+declare namespace ImageViewer {
+    export {
+        Elements,
+        Events,
+        Frames,
+        FullScreenElements,
+        FullScreenEvents,
+        FullScreenViewer,
+        Images,
+        ImageViewer,
+        Options,
+        Sliders,
+        State,
+    };
 }
 
 export = ImageViewer;
--- types/iv-viewer/iv-viewer-tests.ts
+++ types/iv-viewer/iv-viewer-tests.ts
@@ -1,4 +1,4 @@
-import ImageViewer, { FullScreenViewer, ImageViewer as NestedImageViewer } from 'iv-viewer';
+import ImageViewer, { FullScreenViewer, ImageViewer as DestructuredImageViewer, Options } from 'iv-viewer';
 
 const iv = new ImageViewer(new Element());
 iv.destroy(); // $ExpectType void
@@ -10,16 +10,18 @@ iv.resetZoom(); // $ExpectType void
 iv.showSnapView(); // $ExpectType void
 iv.zoom(0); // $ExpectType void
 
-new NestedImageViewer(new Element());
+new DestructuredImageViewer(new Element());
 
-const fsv = new FullScreenViewer();
+const options: Options = {};
+
+const fsv = new FullScreenViewer(options);
+fsv.destroy(); // $ExpectType void
+fsv.destroy(); // $ExpectType void
 fsv.hide(); // $ExpectType void
-fsv.show(''); // $ExpectType void
-fsv.destroy(); // $ExpectType void
-fsv.destroy(); // $ExpectType void
 fsv.hideSnapView(); // $ExpectType void
 fsv.load(''); // $ExpectType void
 fsv.refresh(); // $ExpectType void
 fsv.resetZoom(); // $ExpectType void
+fsv.show(''); // $ExpectType void
 fsv.showSnapView(); // $ExpectType void
 fsv.zoom(0); // $ExpectType void
