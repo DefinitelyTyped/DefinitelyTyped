66e8ba1f4824ff3254dae5caf76aa957fe1dea08
Raphaël Léger <raphael@nodixo.com>
2020-04-08 21:55:38 +0200

    Run prettier on react-combine-reducers

--- types/react-combine-reducers/index.d.ts
+++ types/react-combine-reducers/index.d.ts
@@ -2,13 +2,10 @@
 // Project: https://github.com/ankita1010/react-combine-reducers#readme
 // Definitions by: Raphaël Léger <https://github.com/raphael-leger>
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
-import { Reducer, ReducerState } from "react";
+import { Reducer, ReducerState } from 'react';
 
 export default function combineReducers<R extends Reducer<any, any>>(
-reducers: {
-  [K in keyof ReducerState<R>]: [
-    Reducer<ReducerState<R>[K], any>,
-    ReducerState<R>[K]
-  ];
-}
+    reducers: {
+        [K in keyof ReducerState<R>]: [Reducer<ReducerState<R>[K], any>, ReducerState<R>[K]];
+    },
 ): [R, ReducerState<R>];
--- types/react-combine-reducers/react-combine-reducers-tests.ts
+++ types/react-combine-reducers/react-combine-reducers-tests.ts
@@ -1,78 +1,66 @@
-import combineReducers from "react-combine-reducers";
+import combineReducers from 'react-combine-reducers';
 
 type SimpleAction = { type: string; payload: any };
 
 type GreeniesState = {
-  fruit: 'apple' | 'pear' | 'kiwi';
-  vegetable: 'carrot' | 'eggplant';
+    fruit: 'apple' | 'pear' | 'kiwi';
+    vegetable: 'carrot' | 'eggplant';
 };
 
 type ProteinsState = {
-  similiCarne: 'tofu' | 'seitan';
-  vegetable: 'bean' | 'artichoke';
+    similiCarne: 'tofu' | 'seitan';
+    vegetable: 'bean' | 'artichoke';
 };
 
 type DrinkState = {
-  beverage: 'water' | 'juice';
-  numberOfGlasses: number;
+    beverage: 'water' | 'juice';
+    numberOfGlasses: number;
 };
 
 type DessertState = {
-  cake: 'chocolate' | 'cherry';
-  drink: 'tea' | 'coffee';
+    cake: 'chocolate' | 'cherry';
+    drink: 'tea' | 'coffee';
 };
 
 type FullMealState = {
-  proteins: ProteinsState;
-  greenies: GreeniesState;
-  dessert: DessertState;
-  drink: DrinkState;
-}
+    proteins: ProteinsState;
+    greenies: GreeniesState;
+    dessert: DessertState;
+    drink: DrinkState;
+};
 
 type FullMealReducer = (state: FullMealState, action: SimpleAction) => FullMealState;
 
-const proteinsReducer = (
-  proteinsState: ProteinsState,
-  action: SimpleAction
-) => {
-  switch (action) {
-    default:
-      return proteinsState;
-  }
-}
+const proteinsReducer = (proteinsState: ProteinsState, action: SimpleAction) => {
+    switch (action) {
+        default:
+            return proteinsState;
+    }
+};
 
-const drinkReducer = (
-  drinkState: DrinkState,
-  action: SimpleAction
-) => {
-  switch (action) {
-    default:
-      return drinkState;
-  }
-}
+const drinkReducer = (drinkState: DrinkState, action: SimpleAction) => {
+    switch (action) {
+        default:
+            return drinkState;
+    }
+};
 
-const dessertReducer = (
-  dessertState: DessertState,
-  action: SimpleAction
-) => {
-  switch (action) {
-    default:
-      return dessertState;
-  }
-}
+const dessertReducer = (dessertState: DessertState, action: SimpleAction) => {
+    switch (action) {
+        default:
+            return dessertState;
+    }
+};
 
-const greeniesReducer = (
-  greeniesState: GreeniesState,
-  action: SimpleAction
-) => {
-  switch (action) {
-    default:
-      return greeniesState;
-  }
-}
+const greeniesReducer = (greeniesState: GreeniesState, action: SimpleAction) => {
+    switch (action) {
+        default:
+            return greeniesState;
+    }
+};
 
 // $ExpectError
-combineReducers("");
+combineReducers('');
 
 // $ExpectError
 combineReducers<FullMealReducer>({});
@@ -82,69 +70,96 @@ combineReducers<FullMealReducer>();
 
 // $ExpectError
 combineReducers<FullMealReducer>({
-  books: ["The Book Thief"]
+    books: ['The Book Thief'],
 });
 
 // $ExpectError
 combineReducers<FullMealReducer>({
-  proteins: [100]
+    proteins: [100],
 });
 
 // $ExpectError
 combineReducers<FullMealReducer>({
-  proteins: [3, proteinsReducer],
+    proteins: [3, proteinsReducer],
 });
 
 // $ExpectError
 combineReducers<FullMealReducer>({
-  proteins: [proteinsReducer, 3],
+    proteins: [proteinsReducer, 3],
 });
 
 // $ExpectError
 combineReducers<FullMealReducer>({
-  proteins: [proteinsReducer, {
-    similiCarne: 'tofu',
-    vegetable: 'artichoke'
-  }],
+    proteins: [
+        proteinsReducer,
+        {
+            similiCarne: 'tofu',
+            vegetable: 'artichoke',
+        },
+    ],
 });
 
 // $ExpectError
 combineReducers<FullMealReducer>({
-  proteins: [proteinsReducer, {
-    similiCarne: 'tofu',
-    vegetable: 'artichoke'
-  }],
-  greenies: [proteinsReducer, {
-    fruit: 'kiwi',
-    vegetable: 'eggplant'
-  }],
-  drink: [proteinsReducer, {
-    beverage: 'juice',
-    numberOfGlasses: 2
-  }],
-  dessert: [proteinsReducer, {
-    cake: 'chocolate',
-    drink: 'tea',
-  }]
+    proteins: [
+        proteinsReducer,
+        {
+            similiCarne: 'tofu',
+            vegetable: 'artichoke',
+        },
+    ],
+    greenies: [
+        proteinsReducer,
+        {
+            fruit: 'kiwi',
+            vegetable: 'eggplant',
+        },
+    ],
+    drink: [
+        proteinsReducer,
+        {
+            beverage: 'juice',
+            numberOfGlasses: 2,
+        },
+    ],
+    dessert: [
+        proteinsReducer,
+        {
+            cake: 'chocolate',
+            drink: 'tea',
+        },
+    ],
 });
 
 const [fullMealReducer, initialMeal] = combineReducers<FullMealReducer>({
-  proteins: [proteinsReducer, {
-    similiCarne: 'tofu',
-    vegetable: 'artichoke'
-  }],
-  greenies: [greeniesReducer, {
-    fruit: 'kiwi',
-    vegetable: 'eggplant'
-  }],
-  drink: [drinkReducer, {
-    beverage: 'juice',
-    numberOfGlasses: 2
-  }],
-  dessert: [dessertReducer, {
-    cake: 'chocolate',
-    drink: 'tea',
-  }],
+    proteins: [
+        proteinsReducer,
+        {
+            similiCarne: 'tofu',
+            vegetable: 'artichoke',
+        },
+    ],
+    greenies: [
+        greeniesReducer,
+        {
+            fruit: 'kiwi',
+            vegetable: 'eggplant',
+        },
+    ],
+    drink: [
+        drinkReducer,
+        {
+            beverage: 'juice',
+            numberOfGlasses: 2,
+        },
+    ],
+    dessert: [
+        dessertReducer,
+        {
+            cake: 'chocolate',
+            drink: 'tea',
+        },
+    ],
 });
 
 initialMeal.dessert;
@@ -162,45 +177,51 @@ fullMealReducer(3, {});
 fullMealReducer({}, {});
 
 // $ExpectError
-fullMealReducer({
-  proteins: {
-    similiCarne: 'tofu',
-    vegetable: 'artichoke'
-  },
-  greenies:  {
-    fruit: 'kiwi',
-    vegetable: 'eggplant'
-  },
-  drink: {
-    beverage: 'juice',
-    numberOfGlasses: 2
-  },
-  dessert: {
-    cake: 'chocolate',
-    drink: 'tea',
-  },
-}, 67);
+fullMealReducer(
+    {
+        proteins: {
+            similiCarne: 'tofu',
+            vegetable: 'artichoke',
+        },
+        greenies: {
+            fruit: 'kiwi',
+            vegetable: 'eggplant',
+        },
+        drink: {
+            beverage: 'juice',
+            numberOfGlasses: 2,
+        },
+        dessert: {
+            cake: 'chocolate',
+            drink: 'tea',
+        },
+    },
+    67,
+);
 
-fullMealReducer({
-  proteins: {
-    similiCarne: 'tofu',
-    vegetable: 'artichoke'
-  },
-  greenies:  {
-    fruit: 'kiwi',
-    vegetable: 'eggplant'
-  },
-  drink: {
-    beverage: 'juice',
-    numberOfGlasses: 2
-  },
-  dessert: {
-    cake: 'chocolate',
-    drink: 'tea',
-  },
-}, {
-  type: 'eat',
-  payload: 'everything'
-});
+fullMealReducer(
+    {
+        proteins: {
+            similiCarne: 'tofu',
+            vegetable: 'artichoke',
+        },
+        greenies: {
+            fruit: 'kiwi',
+            vegetable: 'eggplant',
+        },
+        drink: {
+            beverage: 'juice',
+            numberOfGlasses: 2,
+        },
+        dessert: {
+            cake: 'chocolate',
+            drink: 'tea',
+        },
+    },
+    {
+        type: 'eat',
+        payload: 'everything',
+    },
+);
 
 export { fullMealReducer, initialMeal };
