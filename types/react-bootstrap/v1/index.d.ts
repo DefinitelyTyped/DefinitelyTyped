16eca1624e4621e6e0b3b8f69c1f102bec5386a6
Aaron Beall <contact@abeall.com>
2020-04-08 23:24:51 -0400

    Updated def header

--- types/react-bootstrap/v1/index.d.ts
+++ types/react-bootstrap/v1/index.d.ts
@@ -1,5 +1,6 @@
-// Type definitions for react-bootstrap 1.0.0
+// Type definitions for react-bootstrap 1.0
 // Project: https://github.com/react-bootstrap/react-bootstrap, https://react-bootstrap.github.io
+// Definitions by: Aaron Beall <https://github.com/aaronbeall>
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
 // TypeScript Version: 2.8
 
