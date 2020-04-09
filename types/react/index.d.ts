82f5b794c1086b1b75a394c32d8d0ca3d1565ac3
JongChan Choi <disjukr@naver.com>
2020-04-10 04:54:43 +0900

    [react] Add tangentialPressure, twist to PointerEvent (#43589)
    - https://github.com/facebook/react/pull/13374
    - https://developer.mozilla.org/en-US/docs/Web/API/PointerEvent

--- types/react/index.d.ts
+++ types/react/index.d.ts
@@ -23,6 +23,7 @@
 //                 Kyle Scully <https://github.com/zieka>
 //                 Cong Zhang <https://github.com/dancerphil>
 //                 Dimitri Mitropoulos <https://github.com/dimitropoulos>
+//                 JongChan Choi <https://github.com/disjukr>
 //                 Victor Magalhães <https://github.com/vhfmag>
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
 // TypeScript Version: 2.8
@@ -1180,8 +1181,10 @@ declare namespace React {
     interface PointerEvent<T = Element> extends MouseEvent<T, NativePointerEvent> {
         pointerId: number;
         pressure: number;
+        tangentialPressure: number;
         tiltX: number;
         tiltY: number;
+        twist: number;
         width: number;
         height: number;
         pointerType: 'mouse' | 'pen' | 'touch';
