8748e299381c82e9719e4c741738542a83e943e5
Liam Martens <hi@liammartens.com>
2020-04-10 17:37:11 +0200

    Updated with require

--- types/parse-svg-path/parse-svg-path-tests.ts
+++ types/parse-svg-path/parse-svg-path-tests.ts
@@ -1,4 +1,4 @@
-import parse from 'parse-svg-path';
+import parse = require('parse-svg-path');
 const parsed = parse('M 10 10 L 100 100'); // $ExpectType Array
 parsed.forEach(command => {
     const type = command[0]; // $ExpectType string
