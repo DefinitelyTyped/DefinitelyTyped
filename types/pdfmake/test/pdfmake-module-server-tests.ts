3bb99e1e002e9da36e1056353d050cf39e9f5adc
syffs <th3_on3@msn.com>
2020-04-10 12:22:48 +0200

    test(pdfmake): tableLayouts is an object

--- types/pdfmake/test/pdfmake-module-server-tests.ts
+++ types/pdfmake/test/pdfmake-module-server-tests.ts
@@ -3,6 +3,7 @@ import {
     BufferOptions,
     TDocumentDefinitions,
     TFontDictionary,
+    TableLayout,
 } from 'pdfmake/interfaces';
 
 const fonts: TFontDictionary = {
@@ -20,10 +21,17 @@ const dd: TDocumentDefinitions = {
     content: 'Hello world!',
 };
 
+const customTableLayouts: {[key: string]: TableLayout} = {
+    customLayout: {
+        hLineColor: 'red',
+        vLineColor: () => 'red',
+    }
+};
+
 const options: BufferOptions = {
     fontLayoutCache: true,
     bufferPages: true,
-    tableLayouts: 'noBorders',
+    tableLayouts: customTableLayouts,
     autoPrint: true,
     progressCallback: progress =>
         console.log('Creating pdf: ', progress * 100, '%...'),
