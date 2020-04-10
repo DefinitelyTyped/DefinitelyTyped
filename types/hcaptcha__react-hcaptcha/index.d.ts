7a601b5a0ee7a8c79d36c6fe39868b87dc34325c
Matt Sutkowski <msutkowski@gmail.com>
2020-04-09 18:22:31 -0700

    Fix version

--- types/hcaptcha__react-hcaptcha/index.d.ts
+++ types/hcaptcha__react-hcaptcha/index.d.ts
@@ -1,4 +1,4 @@
-// Type definitions for @hcaptcha/react-hcaptcha 1.0
+// Type definitions for @hcaptcha/react-hcaptcha 0.1
 // Project: https://github.com/hCaptcha/react-hcaptcha
 // Definitions by: Matt Sutkowski <https://github.com/msutkowski>
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
