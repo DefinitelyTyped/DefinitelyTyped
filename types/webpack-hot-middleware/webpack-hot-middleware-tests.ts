b6dcecae251fb4c92462a66af86b14cdc08a037a
Rodrigo Saboya <saboya@users.noreply.github.com>
2020-04-10 21:16:06 -0300

    Separating client and middleware options into 2 separate types (#43625)

--- types/webpack-hot-middleware/webpack-hot-middleware-tests.ts
+++ types/webpack-hot-middleware/webpack-hot-middleware-tests.ts
@@ -9,9 +9,26 @@ let webpackHotMiddlewareInstance = webpackHotMiddleware(compiler);
 webpackHotMiddlewareInstance = webpackHotMiddleware(compiler, {
 	log: console.log.bind(console),
 	path: '/__what',
-	heartbeat: 2000,
-	reload: false,
+	heartbeat: 2000
 });
+
+const clientOpts: webpackHotMiddleware.ClientOptions = {
+	reload: true,
+	name: '__webpack_hmr_custom_bundle_name',
+	timeout: 1000,
+	overlay: false,
+	noInfo: true,
+	quiet: true,
+	dynamicPublicPath: false,
+	autoConnect: true,
+	ansiColors: {
+		red: '00FF00'
+	},
+	overlayStyles: {
+		color: '#FF0000'
+	},
+	overlayWarnings: false
+};
 
 const app = express();
 app.use(webpackHotMiddlewareInstance);
