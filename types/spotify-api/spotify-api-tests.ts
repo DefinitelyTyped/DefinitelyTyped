c5b194fed1a467f90cd1b413831d21883827c6de
Basti Ortiz <ortizbasti16@gmail.com>
2020-04-12 15:45:19 +0800

    Meta: include name in the tests file

--- types/spotify-api/spotify-api-tests.ts
+++ types/spotify-api/spotify-api-tests.ts
@@ -2,6 +2,7 @@
 // Project: https://developer.spotify.com/web-api/
 // Definitions by: Niels Kristian Hansen Skovmand <https://github.com/skovmand>
 //                 Nils Måsén <https://github.com/piksel>
+//                 Basti Ortiz <https://github.com/Some-Dood>
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
 
 /*
