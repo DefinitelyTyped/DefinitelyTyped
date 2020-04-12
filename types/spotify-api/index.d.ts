2aa522698c7048b508adcccb3344cd7fef52d23a
Basti Ortiz <ortizbasti16@gmail.com>
2020-04-12 17:44:01 +0800

    Docs: include documentation for playlists
    Source: https://developer.spotify.com/documentation/web-api/reference/object-model/#playlist-object-full

--- types/spotify-api/index.d.ts
+++ types/spotify-api/index.d.ts
@@ -979,23 +979,61 @@ declare namespace SpotifyApi {
      * but needs to be made since the tracks types vary in the Full and Simplified versions.
      */
     interface PlaylistBaseObject extends ContextObject {
+        /**
+         * Returns `true` if context is not search and the owner allows other users to modify the playlist.
+         * Otherwise returns `false`.
+         */
         collaborative: boolean;
+        /**
+         * The [Spotify ID](https://developer.spotify.com/documentation/web-api/#spotify-uris-and-ids) for the playlist.
+         */
         id: string;
+        /**
+         * Images for the playlist. The array may be empty or contain up to three images.
+         * The images are returned by size in descending order.
+         * See [Working with Playlists](https://developer.spotify.com/documentation/general/guides/working-with-playlists/).
+         * Note: If returned, the source URL for the image (`url`) is temporary and will expire in less than a day.
+         */
         images: ImageObject[];
+        /**
+         * The name of the playlist.
+         */
         name: string;
+        /**
+         * The user who owns the playlist.
+         */
         owner: UserObjectPublic;
-        public: boolean;
+        /**
+         * The playlist’s public/private status:
+         * `true` the playlist is public,
+         * `false` the playlist is private,
+         * or `null` the playlist status is not relevant.
+         */
+        public: boolean | null;
+        /**
+         * The version identifier for the current playlist. Can be supplied in other requests to target a specific playlist version:
+         * see [Remove tracks from a playlist](https://developer.spotify.com/documentation/web-api/reference/playlists/remove-tracks-playlist/).
+         */
         snapshot_id: string;
         type: "playlist";
     }
 
     /**
      * Playlist Object Full
-     * [](https://developer.spotify.com/web-api/object-model/)
+     * [](https://developer.spotify.com/web-api/object-model/#playlist-object-full)
      */
     interface PlaylistObjectFull extends PlaylistBaseObject {
-        description: string;
+        /**
+         * The playlist description. Only returned for modified, verified playlists, otherwise null.
+         */
+        description: string | null;
+        /**
+         * Information about the followers of the playlist.
+         */
         followers: FollowersObject;
+        /**
+         * Information about the tracks of the playlist.
+         */
         tracks: PagingObject<PlaylistTrackObject>;
     }
 
