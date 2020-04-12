e6df77ced2b654400558c0b3ecbfa7a751bf38f4
Basti Ortiz <ortizbasti16@gmail.com>
2020-04-12 16:28:28 +0800

    Docs: include documentation for followers object
    This also includes a type fix. Currently, the Web API does not support
    the `href` field. It is temporarily there as a placeholder for the
    future feature.
    
    Source: https://developer.spotify.com/documentation/web-api/reference/object-model/#followers-object

--- types/spotify-api/index.d.ts
+++ types/spotify-api/index.d.ts
@@ -940,7 +940,14 @@ declare namespace SpotifyApi {
      * [](https://developer.spotify.com/web-api/object-model/)
      */
     interface FollowersObject {
-        href: string;
+        /**
+         * A link to the Web API endpoint providing full details of the followers; `null` if not available.
+         * Please note that this will always be set to `null`, as the Web API does not support it at the moment.
+         */
+        href: null;
+        /**
+         * The total number of followers.
+         */
         total: number;
     }
 
