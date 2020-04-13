275fd285c4117752ef5e5fc23ae484100037696f
Elizabeth Samuel <elizs@microsoft.com>
2020-04-13 14:33:30 -0700

    [office-js] [office-js-preview] (Excel) Add closing backtick (#43874)

--- types/office-js/index.d.ts
+++ types/office-js/index.d.ts
@@ -17301,7 +17301,7 @@ declare namespace Excel {
      * Provides information about the selection that raised the "SelectionChanged" event.
      * 
      * **Note**: If multiple, discontiguous cells are selected, 
-     * `Binding.onSelectionChanged only reports row and column information for one selection.
+     * `Binding.onSelectionChanged` only reports row and column information for one selection.
      * Use `Worksheet.onSelectionChanged` for multiple selected ranges.
      *
      * [Api set: ExcelApi 1.2]
@@ -22098,7 +22098,7 @@ declare namespace Excel {
          * Occurs when the selected content in the binding is changed.
          * 
          * **Note**: If multiple, discontiguous cells are selected, 
-         * `Binding.onSelectionChanged only reports row and column information for one selection. 
+         * `Binding.onSelectionChanged` only reports row and column information for one selection. 
          * Use `Worksheet.onSelectionChanged` for multiple selected ranges.
          *
          * [Api set: ExcelApi 1.2]
