8a5a2106f4c9a39939822d85d3f42a8ca8e96d33
Elizabeth Samuel <elizs@microsoft.com>
2020-04-13 13:48:16 -0700

    [office-js-preview] (Outlook) Fix broken link

--- types/office-js-preview/index.d.ts
+++ types/office-js-preview/index.d.ts
@@ -10825,7 +10825,7 @@ declare namespace Office {
          *
          * @remarks
          *
-         * **{@link https://docs.microsoft.com/office/dev/add-ins/outlook/understanding-outlook-add-in-permission | Minimum permission level}**:  `ReadItem`
+         * **{@link https://docs.microsoft.com/office/dev/add-ins/outlook/understanding-outlook-add-in-permissions | Minimum permission level}**:  `ReadItem`
          *
          * **{@link https://docs.microsoft.com/office/dev/add-ins/outlook/outlook-add-ins-overview#extension-points | Applicable Outlook mode}**:  Appointment Attendee
          * 
