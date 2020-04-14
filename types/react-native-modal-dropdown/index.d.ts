f1591ada3a9e477438c9d164d2d1e16e43d2b1d1
Ruslan Yurchenko <kort9591@gmail.com>
2020-04-14 12:15:29 +0300

    Rewrite with ES6 modules, update definitions

--- types/react-native-modal-dropdown/index.d.ts
+++ types/react-native-modal-dropdown/index.d.ts
@@ -1,45 +1,45 @@
-// Type definitions for react-native-modal-dropdown 0.6
+// Type definitions for react-native-modal-dropdown 0.7
 // Project: https://github.com/sohobloo/react-native-modal-dropdown
 // Definitions by: Carlos Li <https://github.com/echoulen>
+//                 Ruslan Yurchenko <https://github.com/rusyurchenko>
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
 // TypeScript Version: 2.8
+import * as React from 'react';
+import { StyleProp, TextStyle, ViewProps, ViewStyle, Constructor, NativeMethodsMixin } from 'react-native';
 
-import * as React from "react";
+export interface PositionStyle {
+    top?: number;
+    left?: number;
+    right?: number;
+    width?: number;
+    height?: number;
+}
 
-import ModalDropdown = RNModalDropdown.ModalDropdown;
-export = ModalDropdown;
+export interface ModalDropdownProps extends ViewProps {
+    disabled?: boolean;
+    defaultIndex?: number;
+    defaultValue?: string;
+    options?: any[];
+    animated?: boolean;
+    scrollEnabled?: boolean;
+    showsVerticalScrollIndicator?: boolean;
+    textStyle?: StyleProp<TextStyle>;
+    dropdownStyle?: StyleProp<ViewStyle>;
+    dropdownTextStyle?: StyleProp<TextStyle>;
+    dropdownTextHighlightStyle?: StyleProp<TextStyle>;
+    adjustFrame?: (positionStyle: PositionStyle) => PositionStyle;
+    renderRow?: (option: any, index: string, isSelected: boolean) => React.ReactNode;
+    renderSeparator?: (sectionID: string, index: string, adjacentRowHighlighted: boolean) => React.ReactNode;
+    renderButtonText?: (text: string) => string;
+    onDropdownWillShow?: () => void | boolean;
+    onDropdownWillHide?: () => void | boolean;
+    onSelect?: (index: string, option: any) => void | boolean;
+    accessible?: boolean;
+    keyboardShouldPersistTaps?: 'always' | 'never' | 'handled';
+}
 
-declare namespace RNModalDropdown {
-    interface PositionStyle {
-        left?: number;
-        right?: number;
-        width?: number;
-    }
-    interface ModalDropdownProps {
-        disabled?: boolean;
-        defaultIndex?: number;
-        defaultValue?: string;
-        options?: any[];
-        animated?: boolean;
-        scrollEnabled?: boolean;
-        showsVerticalScrollIndicator?: boolean;
-        style?: any;
-        textStyle?: any;
-        dropdownStyle?: any;
-        dropdownTextStyle?: any;
-        dropdownTextHighlightStyle?: any;
-        adjustFrame?: (positionStyle: PositionStyle) => void;
-        renderRow?: (option: any, index: number, isSelected: boolean) => any;
-        renderSeparator?: () => any;
-        renderButtonText?: (text: any) => any;
-        onDropdownWillShow?: () => any;
-        onDropdownWillHide?: () => any;
-        onSelect?: (index: number, option: any) => void;
-        accessible?: boolean;
-        keyboardShouldPersistTaps?: "always" | "never" | "handled";
-    }
-
-    class ModalDropdown extends React.Component<ModalDropdownProps> {
-        static default: typeof ModalDropdown;
-    }
+export default class ModalDropdown extends React.Component<ModalDropdownProps> {
+    show(): void;
+    hide(): void;
+    select(index: number): void;
 }
