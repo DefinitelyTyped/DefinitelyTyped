47b2c063eac4de13fe9b552148516cb3c9bdfeb0
Peter Blazejewicz <peterblazejewicz@users.noreply.github.com>
2020-04-14 22:27:36 +0200

    update(plotly.js): missing plot configuration options
    - `plotlyServerURL`
    https://github.com/plotly/plotly.js/blob/d37895bd64227433229f2b3f91c9e14fd6ec7b89/CHANGELOG.md#added-14
    - `showSendToCloud`
    https://github.com/plotly/plotly.js/blob/7ad4317c545538cb85e8a31d24c9524466be5adb/src/plot_api/plot_config.js#L263
    - `showEditInChartStudio`
    https://github.com/plotly/plotly.js/blob/d37895bd64227433229f2b3f91c9e14fd6ec7b89/CHANGELOG.md#added-4
    
    Thanks!

--- types/plotly.js/test/index-tests.ts
+++ types/plotly.js/test/index-tests.ts
@@ -154,6 +154,26 @@ const graphDiv = '#test';
 	const layout2 = { title: 'Revenue' };
 	Plotly.newPlot(graphDiv, data2, layout2);
 })();
+(() => {
+	const data: Array<Partial<PlotData>> = [
+		{
+			type: 'bar',
+			labels: ['Eve', 'Cain', 'Seth', 'Enos', 'Noam', 'Abel', 'Awan', 'Enoch', 'Azura'],
+			parents: ['', 'Eve', 'Eve', 'Seth', 'Seth', 'Eve', 'Eve', 'Awan', 'Eve'],
+			values: [65, 14, 12, 10, 2, 6, 6, 4, 4],
+			marker: { line: { width: 2 } },
+		},
+	];
+
+	const layout = {
+		margin: { l: 0, r: 0, b: 0, t: 0 },
+	};
+	Plotly.newPlot('myDiv', data, layout, {
+		plotlyServerURL: 'https://chart-studio.plotly.com/',
+		showSendToCloud: true,
+		showEditInChartStudio: true,
+	});
+})();
 
 //////////////////////////////////////////////////////////////////////
 
