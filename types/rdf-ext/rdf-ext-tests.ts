9a0c189a77690660bee3ace9412d5cd2ce1e6a5f
Lars Hupel <lars.hupel@mytum.de>
2020-04-15 10:55:51 +0200

    remove triples
    Triples are not specified in the RDF/JS standards.

--- types/rdf-ext/rdf-ext-tests.ts
+++ types/rdf-ext/rdf-ext-tests.ts
@@ -177,7 +177,7 @@ function static_Triple_fromBaseTerms(): Quad {
     const predicate: NamedNode = <any> {};
     const object: NamedNode = <any> {};
 
-    return rdf.triple(subject, predicate, object);
+    return rdf.quad(subject, predicate, object);
 }
 
 function instance_Quad_fromBaseTerms(): Quad {
@@ -196,7 +196,7 @@ function instance_Triple_fromBaseTerms(): Quad {
     const predicate: NamedNode = <any> {};
     const object: NamedNode = <any> {};
 
-    return factory.triple(subject, predicate, object);
+    return factory.quad(subject, predicate, object);
 }
 
 function Quad_toJSON(): boolean {
