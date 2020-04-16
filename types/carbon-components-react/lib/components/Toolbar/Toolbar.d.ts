1daf2a166e176993d91ad979b91396d92eba31e8
Kyle Albert <kyle.albert@us.ibm.com>
2020-04-16 10:49:00 -0400

    Update Toolbar and ToolbarSearch deprecation.

--- types/carbon-components-react/lib/components/Toolbar/Toolbar.d.ts
+++ types/carbon-components-react/lib/components/Toolbar/Toolbar.d.ts
@@ -5,8 +5,14 @@ import { ReactAttr, ReactDivAttr, ForwardRefReturn } from "../../../typings/shar
 
 interface ToolbarInheritedProps extends ReactDivAttr { }
 
+/**
+ * @deprecated
+ */
 export interface ToolbarProps extends ToolbarInheritedProps { }
 
+/**
+ * @deprecated
+ */
 declare const Toolbar: React.FC<ToolbarProps>;
 
 // ToolbarItem
@@ -15,19 +21,31 @@ interface ToolbarItemInheritedProps {
     children?: ReactAttr["children"],
 }
 
+/**
+ * @deprecated
+ */
 export interface ToolbarItemProps extends ToolbarItemInheritedProps {
     placeholderText?: string,
     type?: string,
 }
 
+/**
+ * @deprecated
+ */
 export declare const ToolbarItem: React.FC<ToolbarItemProps>;
 
 // ToolbarTitle
 
+/**
+ * @deprecated
+ */
 export interface ToolbarTitleProps {
     title?: string,
 }
 
+/**
+ * @deprecated
+ */
 export declare const ToolbarTitle: ForwardRefReturn<HTMLLIElement, ToolbarTitleProps>;
 
 // ToolbarOption
@@ -36,12 +54,21 @@ interface ToolbarOptionInheritedProps {
     children?: ReactAttr["children"],
 }
 
+/**
+ * @deprecated
+ */
 export interface ToolbarOptionProps extends ToolbarOptionInheritedProps { }
 
+/**
+ * @deprecated
+ */
 export declare const ToolbarOption: ForwardRefReturn<HTMLLIElement, ToolbarOptionProps>;
 
 // ToolbarDivider
 
+/**
+ * @deprecated
+ */
 export declare const ToolbarDivider: ForwardRefReturn<HTMLHRElement>;
 
 export default Toolbar;
