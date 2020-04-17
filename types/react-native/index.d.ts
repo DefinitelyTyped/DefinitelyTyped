1178153be4bc6770c4bb32c4b3e5894e643d8260
Jesse Katsumata <jesse.katsumata@gmail.com>
2020-04-17 11:09:59 +0900

    react-native: revert version for now

--- types/react-native/index.d.ts
+++ types/react-native/index.d.ts
@@ -1,4 +1,4 @@
-// Type definitions for react-native 0.63.0-rc.0
+// Type definitions for react-native 0.62.2
 // Project: https://github.com/facebook/react-native
 // Definitions by: Eloy Durán <https://github.com/alloy>
 //                 HuHuanming <https://github.com/huhuanming>
