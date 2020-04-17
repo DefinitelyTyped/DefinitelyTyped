d4518201a8d134f15931cbb45d1351e063b7b325
Tim Weise <tw@vi-arise.com>
2020-04-17 14:31:48 +0200

    add scrollmagic types

---
+++ types/scrollmagic/scrollmagic-tests.ts
@@ -0,0 +1,23 @@
+import ScrollMagic, { DestroyEvent, SceneProgressEvent } from 'scrollmagic';
+
+const myController = new ScrollMagic.Controller();
+
+function handleDestroy(e: DestroyEvent) {
+    console.log(e);
+}
+
+function handleBorderCrossing(e: SceneProgressEvent<'enter'> | SceneProgressEvent<'leave'>) {
+    console.log(e.state);
+}
+
+const myScene = new ScrollMagic.Scene({
+    duration: 300,
+    loglevel: 2,
+    triggerHook: 'onLeave',
+    triggerElement: '#trigger',
+})
+    .on('destroy', handleDestroy)
+    .on('enter leave', handleBorderCrossing)
+    .addTo(myController);
+
+myScene.destroy();
