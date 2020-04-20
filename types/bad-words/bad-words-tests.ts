e495ffab567b7370014cf163cc438bd5b068e71d
Tom Pridham <pridham.tom@gmail.com>
2020-04-20 15:57:51 -0600

    add void return type test

--- types/bad-words/bad-words-tests.ts
+++ types/bad-words/bad-words-tests.ts
@@ -11,8 +11,10 @@ new Filter({
     splitRegex: /a/,
 });
 const f = new Filter();
+// $ExpectType void
 f.addWords('a', 'b', 'c');
 const clean: string = f.clean('something dirty');
 const profane: boolean = f.isProfane('something dirty');
+// $ExpectType void
 f.removeWords('a', 'b', 'c');
 const replace: string = f.replaceWord('a');
