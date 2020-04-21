51707501b7a90dfe49973fa6be07596e957493d8
Auryn Engel <auryn.engel@sap.com>
2020-04-21 11:18:44 +0200

    add type for backaction on shellbar

--- types/fundamental-react/lib/Shellbar/Shellbar.d.ts
+++ types/fundamental-react/lib/Shellbar/Shellbar.d.ts
@@ -9,6 +9,7 @@ export type ShellbarProps = {
         notificationCount: number;
         callback?: (event: React.MouseEvent<HTMLButtonElement, MouseEvent>) => void;
     }>;
+    backButton?: () => void;
     className?: string;
     copilot?: boolean;
     disableStyles?: boolean;
