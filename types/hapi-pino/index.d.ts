9fea1c241b8c26b5505b75207c0901e906b79d03
Kyle <kgray1497@gmail.com>
2020-04-23 00:32:46 -0400

    chore(hapi-pino): missed copy pasta, replaced the name properly

--- types/hapi-pino/index.d.ts
+++ types/hapi-pino/index.d.ts
@@ -3,7 +3,7 @@
 // Definitions by: Rodrigo Saboya <https://github.com/saboya>
 //                 Todd Bealmear <https://github.com/todd>
 //                 Matt Jeanes <https://github.com/BlooJeans>
-//                 Matt Jeanes <https://github.com/GoPro16>
+//                 Kyle Gray <https://github.com/GoPro16>
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
 // TypeScript Version: 2.8
 
