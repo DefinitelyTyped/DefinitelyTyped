1fbde2ba8a9e0f273e0040b386765739a180ddff
Vladimir <vzh.inbox@gmail.com>
2020-04-24 11:57:14 +0600

    Improve type inference of useParams
    useParams loose param type:
    ```ts
    const { param1 } = useParams<{ param1: 'value1' | 'value2' }>();
    // param1 is just string, but should be 'value1' | 'value2'
    ```

--- types/react-router/index.d.ts
+++ types/react-router/index.d.ts
@@ -162,7 +162,7 @@ export function useHistory<HistoryLocationState = H.LocationState>(): H.History<
 
 export function useLocation<S = H.LocationState>(): H.Location<S>;
 
-export function useParams<Params extends { [K in keyof Params]?: string } = {}>(): { [p in keyof Params]: keyof Params[p] extends undefined ? string | undefined : string  };
+export function useParams<Params extends { [K in keyof Params]?: string } = {}>(): Params;
 
 export function useRouteMatch<Params extends { [K in keyof Params]?: string } = {}>(): match<Params>;
 export function useRouteMatch<Params extends { [K in keyof Params]?: string } = {}>(
