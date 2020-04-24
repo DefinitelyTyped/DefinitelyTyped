f02617b31989c54f3a6617cafbf5a2d4747aa7b8
Jack Bates <jack@nottheoilrig.com>
2020-04-24 08:01:24 -0700

    [graylog2] Add missing /// <reference types="node" />

--- types/graylog2/index.d.ts
+++ types/graylog2/index.d.ts
@@ -4,6 +4,7 @@
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
 // TypeScript Version: 2.1
 
+/// <reference types="node" />
 import { EventEmitter } from 'events';
 
 export type GraylogDeflate = 'optimal' | 'always' | 'never';
