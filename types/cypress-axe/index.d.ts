9bc666afdff23014a578dfc90c8e4a85699a8adf
Daniel Hayes <dhayes@meetup.com>
2020-04-28 13:58:11 -0400

    clean cypress-axe lint errors

--- types/cypress-axe/index.d.ts
+++ types/cypress-axe/index.d.ts
@@ -1,7 +1,7 @@
 // Type definitions for cypress-axe 0.8
 // Project: https://github.com/avanslaars/cypress-axe#readme
 // Definitions by: Justin Hall <https://github.com/wKovacs64>
-//                 Daniel Hayes <https://github.com/daniel-hayes/>
+//                 Daniel Hayes <https://github.com/daniel-hayes>
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
 // TypeScript Version: 2.3
 
@@ -23,7 +23,7 @@ declare global {
                 context?: ElementContext,
                 options?: Options,
                 violationCallback?: ViolationCallback,
-                skipFailures?: Boolean,
+                skipFailures?: boolean,
             ): void;
             configureAxe(options?: RunOptions): void;
         }
