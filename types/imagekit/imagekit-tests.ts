ff771d87cbc3e82e10945114d45842e363497230
kemicofa <einar.ase.91@gmail.com>
2020-04-28 23:28:14 +0200

    added initial types for imagekit

---
+++ types/imagekit/imagekit-tests.ts
@@ -0,0 +1,9 @@
+import ImageKit = require('imagekit');
+
+const imageKit = new ImageKit({
+    privateKey: 'private_1234',
+    publicKey: 'public_1234',
+    urlEndpoint: 'https://ik.imagekit.io/your_imagekit_id/'
+});
+
+// TODO
