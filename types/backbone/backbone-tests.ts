6f8f7c34f2b4ea55c021dd0c6e90ac25e05e8e2c
rkesters <robert.d.kesterson@leidos.com>
2020-04-29 13:11:03 -0400

    Update Backbone.model get and set

--- types/backbone/backbone-tests.ts
+++ types/backbone/backbone-tests.ts
@@ -96,6 +96,20 @@ class SettingDefaults extends Backbone.Model {
     // same patterns could be used for setting 'Router.routes' and 'View.events'
 }
 
+class FullyTyped extends Backbone.Model<{iLikeBacon: boolean, iLikeToast: boolean}> {
+    public getILikeBacon(): boolean {
+        return this.get('iLikeBacon')
+    }
+
+    public setILikeBacon(value: boolean) {
+        return this.set('iLikeBacon', value);
+    }
+
+    public setAll(values: {iLikeBacon: boolean, iLikeToast?: boolean}) {
+        return this.set(values);
+    }
+}
+
 class Sidebar extends Backbone.Model {
 
     promptColor() {
