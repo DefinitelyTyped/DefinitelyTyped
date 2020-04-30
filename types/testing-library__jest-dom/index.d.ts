5d89901408001bb3b70b328d15325f1b763588c0
Brent Erickson <berickson1@users.noreply.github.com>
2020-04-30 09:56:49 -0700

    [@testing-library/jest-dom] Add types for toHaveDisplayValue

--- types/testing-library__jest-dom/index.d.ts
+++ types/testing-library__jest-dom/index.d.ts
@@ -1,4 +1,4 @@
-// Type definitions for @testing-library/jest-dom 5.6
+// Type definitions for @testing-library/jest-dom 5.7
 // Project: https://github.com/testing-library/jest-dom
 // Definitions by: Ernesto García <https://github.com/gnapse>
 //                 John Gozde <https://github.com/jgoz>
@@ -253,7 +253,54 @@ declare namespace jest {
         toHaveClass(...classNames: string[]): R;
         /**
          * @description
-         * Assert whether an element has focus or not.
+         * Check whether the given form element has the specified value.
+         *
+         * Accepts `<input>`, `<select>`, and `<textarea>` elements with the exception of `<input type="checkbox">` and
+         * `<input type="radiobox">`, which can be matched only using
+         * [toBeChecked](https:github.com/testing-library/jest-dom#tobechecked) or
+         * [toHaveFormValues](https:github.com/testing-library/jest-dom#tohaveformvalues).
+         * @example
+         * <label for="input-example">First name</label>
+         * <input type="text" id="input-example" value="Luca" />
+         *
+         * <label for="textarea-example">Description</label>
+         * <textarea id="textarea-example">An example description here.</textarea>
+         *
+         * <label for="single-select-example">Fruit</label>
+         * <select id="single-select-example">
+         *   <option value="">Select a fruit...</option>
+         *   <option value="banana">Banana</option>
+         *   <option value="ananas">Ananas</option>
+         *   <option value="avocado">Avocado</option>
+         * </select>
+         *
+         * <label for="mutiple-select-example">Fruits</label>
+         * <select id="multiple-select-example" multiple>
+         *   <option value="">Select a fruit...</option>
+         *   <option value="banana" selected>Banana</option>
+         *   <option value="ananas">Ananas</option>
+         *   <option value="avocado" selected>Avocado</option>
+         * </select>
+         *
+         * const input = screen.getByLabelText('First name')
+         * const textarea = screen.getByLabelText('Description')
+         * const selectSingle = screen.getByLabelText('Fruit')
+         * const selectMultiple = screen.getByLabelText('Fruits')
+         *
+         * expect(input).toHaveDisplayValue('Luca')
+         * expect(textarea).toHaveDisplayValue('An example description here.')
+         * expect(selectSingle).toHaveDisplayValue('Select a fruit...')
+         * expect(selectMultiple).toHaveDisplayValue(['Banana', 'Avocado'])
+         *
+         * @see
+         * [testing-library/jest-dom#tohavedisplayvalue](https:github.com/testing-library/jest-dom#tohavedisplayvalue)
+         */
+        toHaveDisplayValue(value?: string | string[]): R;
+        /**
+         * @description
+         * This allows you to check whether the given form element has the specified displayed value (the one the
+         * end user will see). It accepts <input>, <select> and <textarea> elements with the exception of <input type="checkbox">
+         * and <input type="radio">, which can be meaningfully matched only using toBeChecked or toHaveFormValues.
          * @example
          * <div>
          *   <input type="text" data-testid="element-to-focus" />
