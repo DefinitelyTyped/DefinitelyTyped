42c7a9aa65ab86b97e4ebb8bb3c3be11edb5342e
Jonny Buchanan <jonathan.buchanan@gmail.com>
2020-05-02 01:01:19 +1000

    Add babel-plugin-react-html-attrs type extensions for React

---
+++ types/babel-plugin-react-html-attrs/babel-plugin-react-html-attrs-tests.tsx
@@ -0,0 +1,167 @@
+import * as React from 'react';
+
+const htmlAttributesTest = (
+    <html>
+        <head>
+            <meta charset="utf-8" http-equiv="test" />
+            <script nomodule />
+        </head>
+
+        <iframe allowfullscreen={true} frameborder="0" marginheight={2} marginwidth={2} srcdoc="test" />
+
+        <form enctype="utf-8" accept-charset="utf-8" autocomplete="false" novalidate>
+            <label for="input"></label>
+            <input id="input" type="text" autofocus maxlength={42} minlength={1} readonly />
+            <button formaction="test" formenctype="test" formmethod="test" formnovalidate={false} formtarget="test" />
+        </form>
+
+        <video
+            autoplay
+            controlslist="test"
+            crossorigin="test"
+            disablepictureinpicture={true}
+            mediagroup="test"
+            playsinline={true}
+        >
+            <track srclang="en" />
+        </video>
+
+        <table cellpadding={2} cellspacing={2}>
+            <tbody>
+                <tr>
+                    <td colspan={2} rowspan={2} />
+                </tr>
+            </tbody>
+        </table>
+
+        <object classid="test" />
+
+        <ins datetime="test" />
+
+        <img srcset="test" usemap="test" />
+
+        <a hreflang="test" referrerpolicy="test" />
+
+        <menu>
+            <menuitem radiogroup="group1" />
+        </menu>
+
+        <keygen name="name" challenge="challenge string" keytype="type" keyparams="pqg-params" />
+
+        <div
+            accesskey="test"
+            autocapitalize="test"
+            autocorrect="test"
+            autosave="test"
+            class="test"
+            contenteditable={true}
+            contextmenu="test"
+            http-equiv="test"
+            inputmode="text"
+            itemid="test"
+            itemprop="test"
+            itemref="test"
+            itemscope={true}
+            itemtype="test"
+            spellcheck={true}
+            tabindex={0}
+        />
+    </html>
+);
+
+const svgAttributesTest = (
+    <svg
+        accent-height="test"
+        alignment-baseline="auto"
+        arabic-form="initial"
+        baseline-shift="test"
+        cap-height="test"
+        clip-path="test"
+        clip-rule="test"
+        color-interpolation="test"
+        color-interpolation-filters="auto"
+        color-profile="test"
+        color-rendering="test"
+        dominant-baseline="test"
+        enable-background="test"
+        fill-opacity="test"
+        fill-rule="inherit"
+        flood-color="test"
+        flood-opacity="test"
+        font-family="test"
+        font-size="test"
+        font-size-adjust="test"
+        font-stretch="test"
+        font-style="test"
+        font-variant="test"
+        font-weight="test"
+        glyph-name="test"
+        glyph-orientation-horizontal="test"
+        glyph-orientation-vertical="test"
+        horiz-adv-x="test"
+        horiz-origin-x="test"
+        image-rendering="test"
+        letter-spacing="test"
+        lighting-color="test"
+        marker-end="test"
+        marker-mid="test"
+        marker-start="test"
+        overline-position="test"
+        overline-thickness="test"
+        paint-order="test"
+        panose-1="test"
+        pointer-events="test"
+        rendering-intent="test"
+        shape-rendering="test"
+        stop-color="test"
+        stop-opacity="test"
+        strikethrough-position="test"
+        strikethrough-thickness="test"
+        stroke-dasharray="test"
+        stroke-dashoffset="test"
+        stroke-linecap="inherit"
+        stroke-linejoin="inherit"
+        stroke-miterlimit="test"
+        stroke-opacity="test"
+        stroke-width="test"
+        text-anchor="test"
+        text-decoration="test"
+        text-rendering="test"
+        underline-position="test"
+        underline-thickness="test"
+        unicode-bidi="test"
+        unicode-range="test"
+        units-per-em="test"
+        v-alphabetic="test"
+        v-hanging="test"
+        v-ideographic="test"
+        v-mathematical="test"
+        vector-effect="test"
+        vert-adv-y="test"
+        vert-origin-x="test"
+        vert-origin-y="test"
+        word-spacing="test"
+        writing-mode="test"
+        x-height="test"
+    />
+);
+
+/*
+// TypeScript doesn't support XML namespaces in JSX yet
+// https://github.com/microsoft/TypeScript/issues/11833
+const svgNamespacesTest = (
+    <svg
+        xlink:actuate="test"
+        xlink:arcrole="test"
+        xlink:href="test"
+        xlink:role="test"
+        xlink:show="test"
+        xlink:title="test"
+        xlink:type="test"
+        xml:base="test"
+        xml:lang="test"
+        xml:space="test"
+        xmlns:xlink="test"
+    />
+);
+*/
---
+++ types/babel-plugin-react-html-attrs/index.d.ts
@@ -0,0 +1,345 @@
+// Type definitions for React 16.9 when using babel-plugin-react-html-attrs 3.0
+// Project: https://github.com/insin/babel-plugin-react-html-attrs
+// Definitions by: Jonny Buchanan <https://github.com/insin>
+// Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
+import 'react';
+
+// tslint:disable-next-line strict-export-declare-modifiers
+type Booleanish = boolean | 'true' | 'false';
+
+declare module 'react' {
+    interface HTMLAttributes<T> extends AriaAttributes, DOMAttributes<T> {
+        // Standard HTML Attributes
+        accesskey?: string;
+        class?: string;
+        spellcheck?: Booleanish;
+        tabindex?: number;
+
+        // Unknown
+        radiogroup?: string; // <command>, <menuitem>
+
+        // Non-standard Attributes
+        autocapitalize?: string;
+        autocorrect?: string;
+        autosave?: string;
+        contenteditable?: Booleanish | 'inherit';
+        contextmenu?: string;
+        itemid?: string;
+        itemprop?: string;
+        itemref?: string;
+        itemscope?: boolean;
+        itemtype?: string;
+
+        // Living Standard
+        /**
+         * Hints at the type of data that might be entered by the user while editing the element or its contents
+         * @see https://html.spec.whatwg.org/multipage/interaction.html#input-modalities:-the-inputmode-attribute
+         */
+        inputmode?: 'none' | 'text' | 'tel' | 'url' | 'email' | 'numeric' | 'decimal' | 'search';
+    }
+
+    interface AllHTMLAttributes<T> extends HTMLAttributes<T> {
+        // Standard HTML Attributes
+        'accept-charset'?: string;
+        allowfullscreen?: boolean;
+        allowtransparency?: boolean;
+        autocomplete?: string;
+        autofocus?: boolean;
+        autoplay?: boolean;
+        cellpadding?: number | string;
+        cellspacing?: number | string;
+        charset?: string;
+        class?: string;
+        classid?: string;
+        colspan?: number;
+        crossorigin?: string;
+        datetime?: string;
+        enctype?: string;
+        for?: string;
+        formaction?: string;
+        formenctype?: string;
+        formmethod?: string;
+        formnovalidate?: boolean;
+        formtarget?: string;
+        frameborder?: number | string;
+        hreflang?: string;
+        'http-equiv'?: string;
+        keyparams?: string;
+        keytype?: string;
+        marginheight?: number;
+        marginwidth?: number;
+        maxlength?: number;
+        mediagroup?: string;
+        minlength?: number;
+        novalidate?: boolean;
+        playsinline?: boolean;
+        readonly?: boolean;
+        rowspan?: number;
+        srcdoc?: string;
+        srclang?: string;
+        srcset?: string;
+        usemap?: string;
+    }
+
+    interface AnchorHTMLAttributes<T> extends HTMLAttributes<T> {
+        hreflang?: string;
+        referrerpolicy?: string;
+    }
+
+    interface ButtonHTMLAttributes<T> extends HTMLAttributes<T> {
+        autofocus?: boolean;
+        formaction?: string;
+        formenctype?: string;
+        formmethod?: string;
+        formnovalidate?: boolean;
+        formtarget?: string;
+    }
+
+    interface DelHTMLAttributes<T> extends HTMLAttributes<T> {
+        datetime?: string;
+    }
+
+    interface FormHTMLAttributes<T> extends HTMLAttributes<T> {
+        'accept-charset'?: string;
+        autocomplete?: string;
+        enctype?: string;
+        novalidate?: boolean;
+    }
+
+    interface IframeHTMLAttributes<T> extends HTMLAttributes<T> {
+        allowfullscreen?: boolean;
+        allowtransparency?: boolean;
+        frameborder?: number | string;
+        marginheight?: number;
+        marginwidth?: number;
+        referrerpolicy?: string;
+        srcdoc?: string;
+    }
+
+    interface ImgHTMLAttributes<T> extends HTMLAttributes<T> {
+        crossorigin?: 'anonymous' | 'use-credentials' | '';
+        referrerpolicy?: 'no-referrer' | 'origin' | 'unsafe-url';
+        srcset?: string;
+        usemap?: string;
+    }
+
+    interface InputHTMLAttributes<T> extends HTMLAttributes<T> {
+        autocomplete?: string;
+        autofocus?: boolean;
+        crossorigin?: string;
+        formaction?: string;
+        formenctype?: string;
+        formmethod?: string;
+        formnovalidate?: boolean;
+        formtarget?: string;
+        maxlength?: number;
+        minlength?: number;
+        readonly?: boolean;
+    }
+
+    interface InsHTMLAttributes<T> extends HTMLAttributes<T> {
+        datetime?: string;
+    }
+
+    interface KeygenHTMLAttributes<T> extends HTMLAttributes<T> {
+        autofocus?: boolean;
+        keyparams?: string;
+        keytype?: string;
+    }
+
+    interface LabelHTMLAttributes<T> extends HTMLAttributes<T> {
+        for?: string;
+    }
+
+    interface LinkHTMLAttributes<T> extends HTMLAttributes<T> {
+        crossorigin?: string;
+        hreflang?: string;
+        charset?: string;
+    }
+
+    interface MediaHTMLAttributes<T> extends HTMLAttributes<T> {
+        autoplay?: boolean;
+        controlslist?: string;
+        crossorigin?: string;
+        mediagroup?: string;
+        playsinline?: boolean;
+    }
+
+    interface MetaHTMLAttributes<T> extends HTMLAttributes<T> {
+        charset?: string;
+        'http-equiv'?: string;
+    }
+
+    interface ObjectHTMLAttributes<T> extends HTMLAttributes<T> {
+        classid?: string;
+        usemap?: string;
+    }
+
+    interface OutputHTMLAttributes<T> extends HTMLAttributes<T> {
+        for?: string;
+    }
+
+    interface ScriptHTMLAttributes<T> extends HTMLAttributes<T> {
+        charset?: string;
+        crossorigin?: string;
+        nomodule?: boolean;
+    }
+
+    interface SelectHTMLAttributes<T> extends HTMLAttributes<T> {
+        autocomplete?: string;
+        autofocus?: boolean;
+    }
+
+    interface SourceHTMLAttributes<T> extends HTMLAttributes<T> {
+        srcset?: string;
+    }
+
+    interface TableHTMLAttributes<T> extends HTMLAttributes<T> {
+        cellpadding?: number | string;
+        cellspacing?: number | string;
+    }
+
+    interface TdHTMLAttributes<T> extends HTMLAttributes<T> {
+        colspan?: number;
+        rowspan?: number;
+    }
+
+    interface TextareaHTMLAttributes<T> extends HTMLAttributes<T> {
+        autocomplete?: string;
+        autofocus?: boolean;
+        dirname?: string;
+        maxlength?: number;
+        minlength?: number;
+        readonly?: boolean;
+    }
+
+    interface ThHTMLAttributes<T> extends HTMLAttributes<T> {
+        colspan?: number;
+        rowspan?: number;
+    }
+
+    interface TimeHTMLAttributes<T> extends HTMLAttributes<T> {
+        datetime?: string;
+    }
+
+    interface TrackHTMLAttributes<T> extends HTMLAttributes<T> {
+        srclang?: string;
+    }
+
+    interface VideoHTMLAttributes<T> extends MediaHTMLAttributes<T> {
+        disablepictureinpicture?: boolean;
+        playsinline?: boolean;
+    }
+
+    interface SVGAttributes<T> extends AriaAttributes, DOMAttributes<T> {
+        // Attributes also defined in HTMLAttributes
+        class?: string;
+
+        // Other HTML properties supported by SVG elements in browsers
+        tabindex?: number;
+        crossorigin?: 'anonymous' | 'use-credentials' | '';
+
+        // SVG Specific attributes
+        'accent-height'?: number | string;
+        'alignment-baseline'?:
+            | 'auto'
+            | 'baseline'
+            | 'before-edge'
+            | 'text-before-edge'
+            | 'middle'
+            | 'central'
+            | 'after-edge'
+            | 'text-after-edge'
+            | 'ideographic'
+            | 'alphabetic'
+            | 'hanging'
+            | 'mathematical'
+            | 'inherit';
+        'arabic-form'?: 'initial' | 'medial' | 'terminal' | 'isolated';
+        'baseline-shift'?: number | string;
+        'cap-height'?: number | string;
+        'clip-path'?: string;
+        'clip-rule'?: number | string;
+        'color-interpolation'?: number | string;
+        'color-interpolation-filters'?: 'auto' | 'sRGB' | 'linearRGB' | 'inherit';
+        'color-profile'?: number | string;
+        'color-rendering'?: number | string;
+        'dominant-baseline'?: number | string;
+        'enable-background'?: number | string;
+        'fill-opacity'?: number | string;
+        'fill-rule'?: 'nonzero' | 'evenodd' | 'inherit';
+        'flood-color'?: number | string;
+        'flood-opacity'?: number | string;
+        'font-family'?: string;
+        'font-size'?: number | string;
+        'font-size-adjust'?: number | string;
+        'font-stretch'?: number | string;
+        'font-style'?: number | string;
+        'font-variant'?: number | string;
+        'font-weight'?: number | string;
+        'glyph-name'?: number | string;
+        'glyph-orientation-horizontal'?: number | string;
+        'glyph-orientation-vertical'?: number | string;
+        'horiz-adv-x'?: number | string;
+        'horiz-origin-x'?: number | string;
+        'image-rendering'?: number | string;
+        'letter-spacing'?: number | string;
+        'lighting-color'?: number | string;
+        'marker-end'?: string;
+        'marker-mid'?: string;
+        'marker-start'?: string;
+        'overline-position'?: number | string;
+        'overline-thickness'?: number | string;
+        'paint-order'?: number | string;
+        'panose-1'?: number | string;
+        'pointer-events'?: number | string;
+        'rendering-intent'?: number | string;
+        'shape-rendering'?: number | string;
+        'stop-color'?: string;
+        'stop-opacity'?: number | string;
+        'strikethrough-position'?: number | string;
+        'strikethrough-thickness'?: number | string;
+        'stroke-dasharray'?: string | number;
+        'stroke-dashoffset'?: string | number;
+        'stroke-linecap'?: 'butt' | 'round' | 'square' | 'inherit';
+        'stroke-linejoin'?: 'miter' | 'round' | 'bevel' | 'inherit';
+        'stroke-miterlimit'?: number | string;
+        'stroke-opacity'?: number | string;
+        'stroke-width'?: number | string;
+        'text-anchor'?: string;
+        'text-decoration'?: number | string;
+        'text-rendering'?: number | string;
+        'underline-position'?: number | string;
+        'underline-thickness'?: number | string;
+        'unicode-bidi'?: number | string;
+        'unicode-range'?: number | string;
+        'units-per-em'?: number | string;
+        'v-alphabetic'?: number | string;
+        'v-hanging'?: number | string;
+        'v-ideographic'?: number | string;
+        'v-mathematical'?: number | string;
+        'vector-effect'?: number | string;
+        'vert-adv-y'?: number | string;
+        'vert-origin-x'?: number | string;
+        'vert-origin-y'?: number | string;
+        'word-spacing'?: number | string;
+        'writing-mode'?: number | string;
+        'x-height'?: number | string;
+        'xlink:actuate'?: string;
+        'xlink:arcrole'?: string;
+        'xlink:href'?: string;
+        'xlink:role'?: string;
+        'xlink:show'?: string;
+        'xlink:title'?: string;
+        'xlink:type'?: string;
+        'xml:base'?: string;
+        'xml:lang'?: string;
+        'xml:space'?: string;
+        'xmlns:xlink'?: string;
+    }
+
+    interface WebViewHTMLAttributes<T> extends HTMLAttributes<T> {
+        allowfullscreen?: boolean;
+        autofocus?: boolean;
+    }
+}
