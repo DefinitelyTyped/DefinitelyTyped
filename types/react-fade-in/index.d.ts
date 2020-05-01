63cb6a1e99c5e3b34dae066da20f45c55124af2f
Laurens <l.maarschalkerweerd@gmail.com>
2020-05-01 13:59:19 +0200

    Add childClassName property
    Add new `childClassName` property introduced in v.0.1.8
    https://github.com/gkaemmer/react-fade-in/commit/a168810c9b3b49054a36220e0c7b20b4fe638889

--- types/react-fade-in/index.d.ts
+++ types/react-fade-in/index.d.ts
@@ -1,4 +1,4 @@
-// Type definitions for react-fade-in 0.1
+// Type definitions for react-fade-in 0.1.8
 // Project: github.com/gkaemmer/react-fade-in
 // Definitions by: Barry Michael Doyle <https://github.com/barrymichaeldoyle>
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
@@ -10,21 +10,27 @@ import { ComponentClass, ReactNode } from 'react';
  * Props for a FadeIn component.
  */
 export interface FadeInProps {
-    children?: ReactNode;
-    className?: string;
-    /**
-     * Default: 50. Delay between each child's animation in milliseconds.
-     */
-    delay?: number;
-    /**
-     * Default: 400. Duration of each child's animation in milliseconds.
-     */
-    transitionDuration?: number;
+  children?: ReactNode;
+  className?: string;
+  /**
+   * No default. Adds a `className` prop to each child div, allowing
+   * you to style the direct children of the `FadeIn` component.
+   */
+  childClassName?: string;
+  /**
+   * Default: 50. Delay between each child's animation in milliseconds.
+   */
+  delay?: number;
+  /**
+   * Default: 400. Duration of each child's animation in milliseconds.
+   */
+  transitionDuration?: number;
 }
 
 /**
  * Visually animates content on render with FadeIn.js
  */
 declare const FadeIn: ComponentClass<FadeInProps>;
+declare module 'react-fade-in';
 
 export default FadeIn;
