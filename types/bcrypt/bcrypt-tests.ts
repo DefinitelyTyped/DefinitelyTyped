39a5c36ef1a167cdb5e0fe4362bb1e66c9a87c3a
Peter Blazejewicz <peterblazejewicz@users.noreply.github.com>
2020-05-02 21:32:01 +0200

    update(bcrypt): version 4 update
    - v3 compatible definition created
    - minor changes to method definitions to reflect sync/asynchronous with
    callbacks/asynchronous wiht Promise support
    - JSDocs udpated
    - minor typos fixed
    - tests rewritten to cover:
      - sync
      - async with callback
      - async with Promise
      - async/await
    usage.
    - maintainer added
    
    https://github.com/kelektiv/node.bcrypt.js#usage
    https://github.com/kelektiv/node.bcrypt.js/compare/v3.0.8...v4.0.1
    
    Thanks!

--- types/bcrypt/bcrypt-tests.ts
+++ types/bcrypt/bcrypt-tests.ts
@@ -1,65 +1,34 @@
+import bcrypt = require('bcrypt');
 
-import bcrypt = require("bcrypt");
+const saltRounds = 10;
+const myPlaintextPassword = 's0//P4$$w0rD';
+const hash = 'fake-hash';
 
-let num: number;
-let str: string;
-let bool: boolean;
-let error: Error;
-
-str = bcrypt.genSaltSync();
-str = bcrypt.genSaltSync(num);
-str = bcrypt.genSaltSync(num, str);
-
-bcrypt.genSalt(num, (err: Error, salt: string): void => { error = err; str = salt; })
-      .then(salt => str = salt)
-      .catch(err => error = err);
-
-bcrypt.genSalt(num, str, (err: Error, salt: string): void => { error = err; str = salt; })
-      .then(salt => str = salt)
-      .catch(err => error = err);
-
-bcrypt.genSalt(num)
-      .then(salt => str = salt)
-      .catch(err => error = err);
-
-bcrypt.genSalt(num, str)
-      .then(salt => str = salt)
-      .catch(err => error = err);
-
-bcrypt.genSalt((err: Error, salt: string): void => { error = err; str = salt; })
-      .then(salt => str = salt)
-      .catch(err => error = err);
-
-bcrypt.genSalt()
-      .then(salt => str = salt)
-      .catch(err => error = err);
-
-
-str = bcrypt.hashSync(str, str);
-str = bcrypt.hashSync(str, num);
-
-bcrypt.hash(str, str, (err: Error, encrypted: string):void => { str = encrypted; })
-      .then(encrypted => str = encrypted)
-      .catch(err => error = err);
-bcrypt.hash(str, str)
-      .then(encrypted => str = encrypted)
-      .catch(err => error = err);
-
-bcrypt.hash(str, num, (err: Error, encrypted: string): void => { str = encrypted; })
-      .then(encrypted => str = encrypted)
-      .catch(err => error = err);
-bcrypt.hash(str, num)
-      .then(encrypted => str = encrypted)
-      .catch(err => error = err);
-
-
-bool = bcrypt.compareSync(str, str);
-
-bcrypt.compare(str, str, (err: Error, same: boolean): void => { bool = same; })
-      .then(same => bool = same)
-      .catch(err => error = err);
-bcrypt.compare(str, str)
-      .then(same => bool = same)
-      .catch(err => error = err);
-
-num = bcrypt.getRounds(str);
+// $ExpectType void
+bcrypt.genSalt(saltRounds, 'a', (err, salt): void => {
+    err; // $ExpectType Error
+    salt; // $ExpectType string
+});
+bcrypt.genSalt(saltRounds, 'b'); // $ExpectType Promise<string>
+// $ExpectType string
+bcrypt.genSaltSync(saltRounds);
+// $ExpectType void
+bcrypt.hash(myPlaintextPassword, saltRounds, (err, hash) => {
+    err; // $ExpectType Error
+    hash; // $ExpectType string
+});
+bcrypt.hash(myPlaintextPassword, saltRounds); // $ExpectType Promise<string>
+bcrypt.hashSync(myPlaintextPassword, saltRounds); // $ExpectType string
+// $ExpectType void
+bcrypt.compare(myPlaintextPassword, hash, (err, result) => {
+    err; // $ExpectType Error
+    result; // $ExpectType boolean
+});
+bcrypt.compare(myPlaintextPassword, hash); // $ExpectType Promise<boolean>
+bcrypt.compareSync(myPlaintextPassword, hash); // $ExpectType boolean
+// async/await https://github.com/kelektiv/node.bcrypt.js#with-promises
+(async () => {
+    await bcrypt.genSalt(saltRounds, 'b'); // $ExpectType string
+    await bcrypt.hash(myPlaintextPassword, saltRounds); // $ExpectType string
+    await bcrypt.compare(myPlaintextPassword, hash); // $ExpectType boolean
+})();
--- types/bcrypt/index.d.ts
+++ types/bcrypt/index.d.ts
@@ -1,69 +1,70 @@
-// Type definitions for bcrypt 3.0
+// Type definitions for bcrypt 4.0
 // Project: https://www.npmjs.org/package/bcrypt
-// Definitions by:  Peter Harris <https://github.com/codeanimal>
-//                  Ayman Nedjmeddine <https://github.com/IOAyman>
-//                  David Stapleton <https://github.com/dstapleton92>
+// Definitions by: Peter Harris <https://github.com/codeanimal>
+//                 Ayman Nedjmeddine <https://github.com/IOAyman>
+//                 David Stapleton <https://github.com/dstapleton92>
+//                 Piotr Błażejewicz <https://github.com/peterblazejewicz>
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
 
-
 /**
- * @param rounds    The cost of processing the data. Default 10.
- * @param minor     The minor version of bcrypt to use. Either 'a' or 'b'. Default 'b'.
+ * @param [rounds=10] The cost of processing the data.
+ * @param [minor=b] The minor version of bcrypt to use. Either `a` or `b`.
  */
-export declare function genSaltSync(rounds?: number, minor?: string): string;
+export function genSaltSync(rounds?: number, minor?: 'a' | 'b'): string;
 
 /**
- * @param rounds    The cost of processing the data. Default 10.
- * @param callback  A callback to be fire once the sald has been generated. Uses eio making it asynchronous.
+ * @param [rounds=10] The cost of processing the data..
+ * @param [minor=b] The minor version of bcrypt to use. Either `a` or `b`.
  * @return A promise to be either resolved with the generated salt or rejected with an Error
  */
-export declare function genSalt(rounds?: number, callback?: (err: Error, salt: string) => void): Promise<string>;
-
+export function genSalt(rounds?: number, minor?: 'a' | 'b'): Promise<string>;
 /**
- * @param rounds    The cost of processing the data. Default 10.
- * @param minor     The minor version of bcrypt to use. Either 'a' or 'b'. Default 'b'.
- * @param callback  A callback to be fire once the sald has been generated. Uses eio making it asynchronous.
- * @return A promise to be either resolved with the generated salt or rejected with an Error
+ * @param [rounds=10] The cost of processing the data..
+ * @param [minor=b] The minor version of bcrypt to use. Either `a` or `b`.
+ * @param [callback] A callback to be fire once the salt has been generated. Uses eio making it asynchronous.
  */
-export declare function genSalt(rounds?: number, minor?: string, callback?: (err: Error, salt: string) => void): Promise<string>;
+export function genSalt(rounds?: number, minor?: 'a' | 'b', callback?: (err: Error, salt: string) => void): void;
 
 /**
- * @param callback  A callback to be fire once the sald has been generated. Uses eio making it asynchronous.
- * @return A promise to be either resolved with the generated salt or rejected with an Error
+ * @param data The data to be encrypted.
+ * @param salt The salt to be used in encryption.
  */
-export declare function genSalt(callback?: (err: Error, salt: string) => void): Promise<string>;
+export function hashSync(data: any, saltOrRounds: string | number): string;
 
 /**
- * @param data  The data to be encrypted.
- * @param salt  The salt to be used in encryption.
+ * @param data The data to be encrypted.
+ * @param salt The salt to be used in encryption.
+ * @param [callback] A callback to be fired once the data has been encrypted. Uses eio making it asynchronous.
  */
-export declare function hashSync(data: any, saltOrRounds: string | number): string;
-
+export function hash(data: any, saltOrRounds: string | number, callback: (err: Error, encrypted: string) => void): void;
 /**
- * @param data      The data to be encrypted.
- * @param salt      The salt to be used in encryption.
- * @param callback  A callback to be fired once the data has been encrypted. Uses eio making it asynchronous.
+ * @param data The data to be encrypted.
+ * @param salt The salt to be used in encryption.
  * @return A promise to be either resolved with the encrypted data salt or rejected with an Error
  */
-export declare function hash(data: any, saltOrRounds: string | number, callback?: (err: Error, encrypted: string) => void): Promise<string>;
+export function hash(data: any, saltOrRounds: string | number): Promise<string>;
 
 /**
- * @param data      The data to be encrypted.
+ * @param data The data to be encrypted.
  * @param encrypted The data to be compared against.
  */
-export declare function compareSync(data: any, encrypted: string): boolean;
+export function compareSync(data: any, encrypted: string): boolean;
 
 /**
- * @param data      The data to be encrypted.
+ * @param data The data to be encrypted.
  * @param encrypted The data to be compared against.
- * @param callback  A callback to be fire once the data has been compared. Uses eio making it asynchronous.
- * @return A promise to be either resolved with the comparision result salt or rejected with an Error
+ * @param [callback] A callback to be fire once the data has been compared. Uses eio making it asynchronous.
  */
-export declare function compare(data: any, encrypted: string, callback?: (err: Error, same: boolean) => void): Promise<boolean>;
+export function compare(data: any, encrypted: string, callback: (err: Error, same: boolean) => void): void;
+/**
+ * @param data The data to be encrypted.
+ * @param encrypted The data to be compared against.
+ * @return A promise to be either resolved with the comparison result salt or rejected with an Error
+ */
+export function compare(data: any, encrypted: string): Promise<boolean>;
 
 /**
  * Return the number of rounds used to encrypt a given hash
- *
  * @param encrypted Hash from which the number of rounds used should be extracted.
  */
-export declare function getRounds(encrypted: string): number;
+export function getRounds(encrypted: string): number;
