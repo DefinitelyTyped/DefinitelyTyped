d6df74423c5b86c825bd3d8ef09438cf2f342351
Jonny Buchanan <jonathan.buchanan@gmail.com>
2020-05-02 16:24:12 +1000

    This is now a fork of @types/react in order to add full boolean and numeric HTML attribute support
    This had to be done as not possible to override existing imported types
    (e.g. so  boolean attributes can be `boolean | string` instead of
    `boolean`), which probably makes this type definition academic as it
    probably breaks when  you use other typedefs which import @types/react.

--- types/babel-plugin-react-html-attrs/babel-plugin-react-html-attrs-tests.tsx
+++ types/babel-plugin-react-html-attrs/babel-plugin-react-html-attrs-tests.tsx
@@ -1,45 +1,144 @@
-import * as React from 'react';
-
 const htmlAttributesTest = (
     <html>
         <head>
+            <link crossorigin="test" href="#" hreflang="en" charset="utf-8" />
+
             <meta charset="utf-8" http-equiv="test" />
-            <script nomodule />
+
+            <script charset="utf-8" crossorigin="test" nomodule />
+            <script async="" defer="" nomodule="" />
+
+            <style scoped="" />
         </head>
 
-        <iframe allowfullscreen frameborder="0" marginheight="2" marginwidth="2" srcdoc="test" />
+        <div
+            aria-colcount="2"
+            aria-colindex="2"
+            aria-colspan="2"
+            aria-level="2"
+            aria-posinset="2"
+            aria-rowcount="2"
+            aria-rowindex="2"
+            aria-rowspan="2"
+            aria-setsize="2"
+            aria-valuemax="2"
+            aria-valuemin="2"
+            aria-valuenow="2"
+        />
 
-        <form enctype="utf-8" accept-charset="utf-8" autocomplete="false" novalidate>
+        <a hreflang="test" referrerpolicy="test" />
+
+        <map name="infographic">
+            <area href="" hreflang="en" />
+        </map>
+
+        <details open="" />
+
+        <del datetime="test" />
+
+        <dialog open="" />
+
+        <fieldset disabled="" />
+
+        <form accept-charset="utf-8" autocomplete="false" enctype="utf-8" novalidate>
+            <button
+                autofocus
+                disabled
+                formaction="test"
+                formenctype="test"
+                formmethod="test"
+                formnovalidate
+                formtarget="test"
+            />
+            <button autofocus="" disabled="" formnovalidate="" />
+
+            <input
+                autocomplete="false"
+                autofocus
+                crossorigin="test"
+                formaction="test"
+                formenctype="test"
+                formmethod="test"
+                formnovalidate
+                formtarget=""
+                id="input"
+                type="text"
+                inputmode="tel"
+                max="42"
+                maxlength="42"
+                min="1"
+                minlength="1"
+                readonly
+                required
+                size="2"
+                step="2"
+            />
+            <input autofocus="" checked="" disabled="" formnovalidate="" multiple="" readonly="" required="" />
+
             <label for="input"></label>
-            <input id="input" type="text" autofocus maxlength="42" minlength="1" readonly />
-            <button formaction="test" formenctype="test" formmethod="test" formnovalidate formtarget="test" />
-        </form>
 
-        <video autoplay controlslist="test" crossorigin="test" disablepictureinpicture mediagroup="test" playsinline>
-            <track srclang="en" />
+            <select autocomplete="false" autofocus size="2">
+                <optgroup disabled="" />
+                <option disabled="" selected="" />
+            </select>
+            <select autocomplete="false" autofocus="" disabled="" multiple="" required="" />
+
+            <textarea autocomplete="false" autofocus cols="4" maxlength="42" minlength="1" readonly rows="42" />
+            <textarea autofocus="" disabled="" readonly="" required="" />
+        </form>
+        <form novalidate="" />
+
+        <iframe
+            allowfullscreen
+            allowtransparency
+            frameborder="0"
+            marginheight="2"
+            marginwidth="2"
+            referrerpolicy="test"
+            seamless
+            srcdoc="test"
+        />
+        <iframe allowfullscreen="" allowtransparency="" seamless="" />
+
+        <img crossorigin="anonymous" referrerpolicy="origin" srcset="test" usemap="test" tabindex="-1" />
+
+        <ins datetime="test" />
+
+        <keygen autofocus name="name" challenge="challenge string" keytype="type" keyparams="pqg-params" />
+        <keygen autofocus="" disabled="" />
+
+        <video autoplay controlslist="test" crossorigin="test" mediagroup="test" playsinline disablepictureinpicture>
+            <track default="" srclang="en" />
         </video>
+        <video autoplay="" controls="" disablepictureinpicture="" loop="" muted="" playsinline="" />
+
+        <meter high="42" low="1" min="200" max="500" optimum="123" />
+
+        <object classid="test" usemap="test" />
+
+        <ol reversed="" start="2" />
+
+        <output for="test" />
+
+        <progress max="42" />
 
         <table cellpadding="2" cellspacing="2">
+            <colgroup span="2">
+                <col span="2" />
+            </colgroup>
             <tbody>
                 <tr>
+                    <th colspan="2" rowspan="2" />
                     <td colspan="2" rowspan="2" />
                 </tr>
             </tbody>
         </table>
 
-        <object classid="test" />
-
-        <ins datetime="test" />
-
-        <img srcset="test" usemap="test" tabindex="-1" />
-
-        <a hreflang="test" referrerpolicy="test" />
+        <time datetime="test" />
 
         <menu>
             <menuitem radiogroup="group1" />
         </menu>
-
-        <keygen name="name" challenge="challenge string" keytype="type" keyparams="pqg-params" />
 
         <div
             accesskey="test"
@@ -49,16 +148,17 @@ const htmlAttributesTest = (
             class="test"
             contenteditable
             contextmenu="test"
+            hidden=""
             http-equiv="test"
-            inputmode="text"
             itemid="test"
             itemprop="test"
             itemref="test"
-            itemScope
+            itemscope
             itemtype="test"
             spellcheck
             tabindex="0"
         />
+        <div itemscope="itemscope" />
     </html>
 );
 
