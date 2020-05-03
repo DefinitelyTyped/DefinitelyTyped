769ab901f6684a9b2fd303ab02b2a60895d78179
Weslen Nascimento <weslenng@outlook.com>
2020-05-02 23:47:38 -0300

    Unnecessary namespace

--- types/node-uci/index.d.ts
+++ types/node-uci/index.d.ts
@@ -5,55 +5,51 @@
 
 import { EventEmitter } from 'events';
 
-declare namespace NodeUCI {
-    export type SearchOptions = {
-        searchmoves?: ReadonlyArray<string>;
-        ponder?: boolean;
-        wtime?: number;
-        btime?: number;
-        winc?: number;
-        binc?: number;
-        movestogo?: number;
-        depth?: number;
-        nodes?: number;
-        mate?: number;
-        movetime?: number;
-    };
+export type SearchOptions = {
+    searchmoves?: ReadonlyArray<string>;
+    ponder?: boolean;
+    wtime?: number;
+    btime?: number;
+    winc?: number;
+    binc?: number;
+    movestogo?: number;
+    depth?: number;
+    nodes?: number;
+    mate?: number;
+    movetime?: number;
+};
 
-    export type SearchResult = {
-        bestmove: string;
-        info: ReadonlyArray<string>;
-    };
+export type SearchResult = {
+    bestmove: string;
+    info: ReadonlyArray<string>;
+};
 
-    export class Engine {
-        constructor(enginePath: string);
-        getBufferUntil(fn: (str: string) => boolean): Promise<ReadonlyArray<string>>;
-        write(cmd: string): void;
-        chain(): EngineChain;
-        init(): Promise<Engine>;
-        quit(): Promise<Engine>;
-        isready(): Promise<Engine>;
-        sendCmd(cmd: string): Promise<Engine>;
-        setoption(name: string, value?: string): Promise<Engine>;
-        ucinewgame(): Promise<Engine>;
-        ponderhit(): Promise<Engine>;
-        position(fen: string, moves?: ReadonlyArray<string>): Promise<Engine>;
-        go(sp: SearchOptions): Promise<SearchResult>;
-        goInfinite(sp: SearchOptions): EventEmitter;
-        stop(): Promise<SearchResult>;
-    }
-
-    export class EngineChain {
-        constructor(engine: Engine);
-        init(): EngineChain;
-        setoption(name: string, value?: string): EngineChain;
-        isready(): EngineChain;
-        ucinewgame(): EngineChain;
-        quit(): EngineChain;
-        position(fen: string, moves?: ReadonlyArray<string>): EngineChain;
-        go(sp: SearchOptions): Promise<SearchResult>;
-        exec(): unknown;
-    }
+export class Engine {
+    constructor(enginePath: string);
+    getBufferUntil(fn: (str: string) => boolean): Promise<ReadonlyArray<string>>;
+    write(cmd: string): void;
+    chain(): EngineChain;
+    init(): Promise<Engine>;
+    quit(): Promise<Engine>;
+    isready(): Promise<Engine>;
+    sendCmd(cmd: string): Promise<Engine>;
+    setoption(name: string, value?: string): Promise<Engine>;
+    ucinewgame(): Promise<Engine>;
+    ponderhit(): Promise<Engine>;
+    position(fen: string, moves?: ReadonlyArray<string>): Promise<Engine>;
+    go(sp: SearchOptions): Promise<SearchResult>;
+    goInfinite(sp: SearchOptions): EventEmitter;
+    stop(): Promise<SearchResult>;
 }
 
-export = NodeUCI;
+export class EngineChain {
+    constructor(engine: Engine);
+    init(): EngineChain;
+    setoption(name: string, value?: string): EngineChain;
+    isready(): EngineChain;
+    ucinewgame(): EngineChain;
+    quit(): EngineChain;
+    position(fen: string, moves?: ReadonlyArray<string>): EngineChain;
+    go(sp: SearchOptions): Promise<SearchResult>;
+    exec(): unknown;
+}
