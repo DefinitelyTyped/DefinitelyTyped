0d0bd67c1a6304b2018544e6c8d6a8afa49d0b27
David Stephens <dev@dwrs.org>
2020-05-03 14:37:54 +0100

    Add myself as a contributor to the Redis types

--- types/redis/index.d.ts
+++ types/redis/index.d.ts
@@ -13,6 +13,7 @@
 //                 Adebayo Opesanya <https://github.com/OpesanyaAdebayo>
 //                 Ryo Ota <https://github.com/nwtgck>
 //                 Thomas de Barochez <https://github.com/tdebarochez>
+//                 David Stephens <https://github.com/dwrss>
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
 
 // Imported from: https://github.com/types/npm-redis
