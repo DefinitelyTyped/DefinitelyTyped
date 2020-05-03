2de3486518d4c919a626c7b71122a9b1d3a21ae7
Dmitry Demensky <dmitry@demensky.com>
2020-05-03 22:58:59 +0300

    [googlemaps] Extract elevation.d.ts

--- types/googlemaps/index.d.ts
+++ types/googlemaps/index.d.ts
@@ -1977,40 +1977,6 @@ declare namespace google.maps {
         TROLLEYBUS,
     }
 
-    class ElevationService {
-        getElevationAlongPath(
-            request: PathElevationRequest,
-            callback: (results: ElevationResult[], status: ElevationStatus) => void,
-        ): void;
-        getElevationForLocations(
-            request: LocationElevationRequest,
-            callback: (results: ElevationResult[], status: ElevationStatus) => void,
-        ): void;
-    }
-
-    interface LocationElevationRequest {
-        locations: LatLng[];
-    }
-
-    interface PathElevationRequest {
-        path?: LatLng[];
-        samples?: number;
-    }
-
-    interface ElevationResult {
-        elevation: number;
-        location: LatLng;
-        resolution: number;
-    }
-
-    enum ElevationStatus {
-        INVALID_REQUEST = 'INVALID_REQUEST',
-        OK = 'OK',
-        OVER_QUERY_LIMIT = 'OVER_QUERY_LIMIT',
-        REQUEST_DENIED = 'REQUEST_DENIED',
-        UNKNOWN_ERROR = 'UNKNOWN_ERROR',
-    }
-
     class DistanceMatrixService {
         getDistanceMatrix(
             request: DistanceMatrixRequest,
