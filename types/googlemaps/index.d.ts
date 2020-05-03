fd3677fe7e78b5b64afe28f051559b77931f87a3
Dmitry Demensky <dmitry@demensky.com>
2020-05-03 22:28:31 +0300

    [googlemaps] Extract image-overlay.d.ts

--- types/googlemaps/index.d.ts
+++ types/googlemaps/index.d.ts
@@ -47,6 +47,7 @@ THE SOFTWARE.
 /// <reference path="./reference/event.d.ts" />
 /// <reference path="./reference/control.d.ts" />
 /// <reference path="./reference/kml.d.ts" />
+/// <reference path="./reference/image-overlay.d.ts" />
 /// <reference path="./reference/drawing.d.ts" />
 /// <reference path="./reference/visualization.d.ts" />
 /// <reference path="./reference/max-zoom.d.ts" />
@@ -1849,22 +1850,6 @@ declare namespace google.maps {
         OUTSIDE = 2,
     }
 
-    class GroundOverlay extends MVCObject {
-        constructor(url: string, bounds: LatLngBounds | LatLngBoundsLiteral, opts?: GroundOverlayOptions);
-        getBounds(): LatLngBounds;
-        getMap(): Map;
-        getOpacity(): number;
-        getUrl(): string;
-        setMap(map: Map | null): void;
-        setOpacity(opacity: number): void;
-    }
-
-    interface GroundOverlayOptions {
-        clickable?: boolean;
-        map?: Map;
-        opacity?: number;
-    }
-
     class OverlayView extends MVCObject {
         draw(): void;
         getMap(): Map | StreetViewPanorama;
@@ -2475,72 +2460,9 @@ declare namespace google.maps {
         place?: Place;
     }
 
-    /***** Map Types *****/
-    interface MapType {
-        getTile(tileCoord: Point, zoom: number, ownerDocument: Document): Element;
-        releaseTile(tile: Element): void;
-        alt?: string;
-        maxZoom?: number;
-        minZoom?: number;
-        name?: string;
-        projection?: Projection;
-        radius?: number;
-        tileSize?: Size;
-    }
-
     class MapTypeRegistry extends MVCObject {
         constructor();
         set(id: string, mapType: MapType): void;
-    }
-
-    interface Projection {
-        fromLatLngToPoint(latLng: LatLng, point?: Point): Point;
-        fromPointToLatLng(pixel: Point, noWrap?: boolean): LatLng;
-    }
-
-    class ImageMapType extends MVCObject implements MapType {
-        constructor(opts: ImageMapTypeOptions);
-        getOpacity(): number;
-        getTile(tileCoord: Point, zoom: number, ownerDocument: Document): Element;
-        releaseTile(tile: Element): void;
-        setOpacity(opacity: number): void;
-        alt: string;
-        maxZoom: number;
-        minZoom: number;
-        name: string;
-        projection: Projection;
-        radius: number;
-        tileSize: Size;
-    }
-
-    interface ImageMapTypeOptions {
-        alt?: string;
-        getTileUrl(tileCoord: Point, zoom: number): string;
-        maxZoom?: number;
-        minZoom?: number;
-        name?: string;
-        opacity?: number;
-        tileSize: Size;
-    }
-
-    class StyledMapType extends MVCObject implements MapType {
-        constructor(styles: MapTypeStyle[], options?: StyledMapTypeOptions);
-        getTile(tileCoord: Point, zoom: number, ownerDocument: Document): Element;
-        releaseTile(tile: Element): void;
-        alt: string;
-        maxZoom: number;
-        minZoom: number;
-        name: string;
-        projection: Projection;
-        radius: number;
-        tileSize: Size;
-    }
-
-    interface StyledMapTypeOptions {
-        alt?: string;
-        maxZoom?: number;
-        minZoom?: number;
-        name?: string;
     }
 
     interface MapTypeStyle {
