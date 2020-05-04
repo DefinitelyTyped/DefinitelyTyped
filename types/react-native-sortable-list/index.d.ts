4bf6f901826d876ba63bca7331cd4f72aa3dac34
Attila Gazso <agazso@gmail.com>
2020-05-04 12:39:36 +0200

    react-native-sortable-list: changed version number to 0.0.24

--- types/react-native-sortable-list/index.d.ts
+++ types/react-native-sortable-list/index.d.ts
@@ -1,4 +1,4 @@
-// Type definitions for react-native-sortable-list 0.0
+// Type definitions for react-native-sortable-list 0.0.24
 // Project: https://github.com/gitim/react-native-sortable-list
 // Definitions by: Michael Sivolobov <https://github.com/sivolobov>
 //                 Vince Maiuri <https://github.com/RookY2K>
