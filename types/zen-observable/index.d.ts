365e50e56b3a4fb0f6a2f71319fde095a342375f
Benoît Zugmeyer <bzugmeyer@gmail.com>
2020-05-04 12:08:55 +0200

    [zen-observable] add zen-observable/esm module types
    This commit add support for importing "zen-observable/esm".  Contrary to importing "zen-observable" directly, it
    doesn't expose the global namespace ZenObservable.  Instead, it exports types directly.

--- types/zen-observable/index.d.ts
+++ types/zen-observable/index.d.ts
@@ -3,65 +3,22 @@
 // Definitions by: Kombu <https://github.com/aicest>
 //                 JounQin <https://github.com/JounQin>
 //                 Thomas <https://github.com/itomtom>
+//                 BenoitZugmeyer <https://github.com/BenoitZugmeyer>
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
 
+import * as esm from './esm';
+
 declare global {
-    interface SymbolConstructor {
-        readonly observable: symbol;
-    }
-
     namespace ZenObservable {
-        interface SubscriptionObserver<T> {
-            closed: boolean;
-            next(value: T): void;
-            error(errorValue: any): void;
-            complete(): void;
-        }
-
-        interface Subscription {
-            closed: boolean;
-            unsubscribe(): void;
-        }
-
-        interface Observer<T> {
-            start?(subscription: Subscription): any;
-            next?(value: T): void;
-            error?(errorValue: any): void;
-            complete?(): void;
-        }
-
-        type Subscriber<T> = (observer: SubscriptionObserver<T>) => void | (() => void) | Subscription;
-
-        interface ObservableLike<T> {
-            subscribe?: Subscriber<T>;
-            [Symbol.observable](): Observable<T> | ObservableLike<T>;
-        }
+        type SubscriptionObserver<T> = esm.SubscriptionObserver<T>;
+        type Subscription = esm.Subscription;
+        type Observer<T> = esm.Observer<T>;
+        type Subscriber<T> = esm.Subscriber<T>;
+        type ObservableLike<T> = esm.ObservableLike<T>;
     }
 }
 
-declare class Observable<T> {
-    constructor(subscriber: ZenObservable.Subscriber<T>);
-
-    subscribe(observer: ZenObservable.Observer<T>): ZenObservable.Subscription;
-    subscribe(
-        onNext: (value: T) => void,
-        onError?: (error: any) => void,
-        onComplete?: () => void,
-    ): ZenObservable.Subscription;
-
-    [Symbol.observable](): Observable<T>;
-
-    forEach(callback: (value: T) => void): Promise<void>;
-    map<R>(callback: (value: T) => R): Observable<R>;
-    filter(callback: (value: T) => boolean): Observable<T>;
-    reduce(callback: (previousValue: T, currentValue: T) => T, initialValue?: T): Observable<T>;
-    reduce<R>(callback: (previousValue: R, currentValue: T) => R, initialValue?: R): Observable<R>;
-    flatMap<R>(callback: (value: T) => ZenObservable.ObservableLike<R>): Observable<R>;
-    concat<R>(...observable: Array<Observable<R>>): Observable<R>;
-
-    static from<R>(observable: Observable<R> | ZenObservable.ObservableLike<R> | ArrayLike<R>): Observable<R>;
-    static of<R>(...items: R[]): Observable<R>;
-}
+declare class Observable<T> extends esm.Observable<T> {}
 
 declare namespace Observable {}
 
