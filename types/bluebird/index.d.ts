298bf0baaadf8aa927807c7898bdfd03ac360679
bluelovers <codelovers@users.sourceforge.net>
2020-05-06 02:41:54 +0800

    Update index.d.ts

--- types/bluebird/index.d.ts
+++ types/bluebird/index.d.ts
@@ -574,13 +574,6 @@ declare class Bluebird<R> implements PromiseLike<R>, Bluebird.Inspection<R> {
    */
   all(): Bluebird<never>;
 
-  static allSettled<T1, T2, T3, T4, T5>(values: [Resolvable<T1>, Resolvable<T2>, Resolvable<T3>, Resolvable<T4>, Resolvable<T5>]): Bluebird<ResultArrayToInspection<[T1, T2, T3, T4, T5]>>;
-  static allSettled<T1, T2, T3, T4>(values: [Resolvable<T1>, Resolvable<T2>, Resolvable<T3>, Resolvable<T4>]): Bluebird<ResultArrayToInspection<[T1, T2, T3, T4]>>;
-  static allSettled<T1, T2, T3>(values: [Resolvable<T1>, Resolvable<T2>, Resolvable<T3>]): Bluebird<ResultArrayToInspection<[T1, T2, T3]>>;
-  static allSettled<T1, T2>(values: [Resolvable<T1>, Resolvable<T2>]): Bluebird<ResultArrayToInspection<[T1, T2]>>;
-  static allSettled<T1>(values: [Resolvable<T1>]): Bluebird<ResultArrayToInspection<[T1]>>;
-  static allSettled<R>(values: Resolvable<Iterable<Resolvable<R>>>): Bluebird<Bluebird.Inspection<R>[]>;
-
   /**
    * Same as calling `Promise.props(thisPromise)`. With the exception that if this promise is bound to a value, the returned promise is bound to that value too.
    */
@@ -862,6 +855,13 @@ declare class Bluebird<R> implements PromiseLike<R>, Bluebird.Inspection<R> {
   static all<T1>(values: [Resolvable<T1>]): Bluebird<[T1]>;
   // array with values
   static all<R>(values: Resolvable<Iterable<Resolvable<R>>>): Bluebird<R[]>;
+
+  static allSettled<T1, T2, T3, T4, T5>(values: [Resolvable<T1>, Resolvable<T2>, Resolvable<T3>, Resolvable<T4>, Resolvable<T5>]): Bluebird<ResultArrayToInspection<[T1, T2, T3, T4, T5]>>;
+  static allSettled<T1, T2, T3, T4>(values: [Resolvable<T1>, Resolvable<T2>, Resolvable<T3>, Resolvable<T4>]): Bluebird<ResultArrayToInspection<[T1, T2, T3, T4]>>;
+  static allSettled<T1, T2, T3>(values: [Resolvable<T1>, Resolvable<T2>, Resolvable<T3>]): Bluebird<ResultArrayToInspection<[T1, T2, T3]>>;
+  static allSettled<T1, T2>(values: [Resolvable<T1>, Resolvable<T2>]): Bluebird<ResultArrayToInspection<[T1, T2]>>;
+  static allSettled<T1>(values: [Resolvable<T1>]): Bluebird<ResultArrayToInspection<[T1]>>;
+  static allSettled<R>(values: Resolvable<Iterable<Resolvable<R>>>): Bluebird<Bluebird.Inspection<R>[]>;
 
   /**
    * Like ``Promise.all`` but for object properties instead of array items. Returns a promise that is fulfilled when all the properties of the object are fulfilled.
