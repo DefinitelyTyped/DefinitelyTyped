72fc2af71157bf8189bac65ed3495ee70638d8f6
Oliver Joseph Ash <oliverjash@gmail.com>
2020-05-05 10:55:36 +0100

    Fix bad merge

--- types/express-serve-static-core/index.d.ts
+++ types/express-serve-static-core/index.d.ts
@@ -30,6 +30,8 @@ import { EventEmitter } from "events";
 import { Options as RangeParserOptions, Result as RangeParserResult, Ranges as RangeParserRanges } from "range-parser";
 import { ParsedQs } from "qs";
 
+export type Query = ParsedQs;
+
 export interface NextFunction {
     // tslint:disable-next-line callable-types (In ts2.1 it thinks the type alias has no call signatures)
     (err?: any): void;
