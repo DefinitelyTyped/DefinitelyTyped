d1cbe4329b747edcd44cad9d49718039c14b7283
Nathan Shively-Sanders <293473+sandersn@users.noreply.github.com>
2020-05-05 08:44:27 -0700

    try to read suggestions

--- dangerfile.ts
+++ dangerfile.ts
@@ -1,3 +1,15 @@
-import { message } from "danger"
+import fs = require('fs')
+import os = require('os')
+import { message, warn } from "danger"
+const suggestionsDir = [os.homedir(), ".dts", "suggestions"].join('/')
+let msg = "\n\n=== SUGGESTIONS ===\n"
+const suggestionLines: string[] = [];
+for (const suggestionFile of fs.readdirSync("foo")) {
+    msg += suggestionFile
+    const path = [suggestionsDir, suggestionFile].join('/');
+    const suggestions = fs.readFileSync(path, "utf8").split("\n");
+    suggestionLines.push(`"${suggestionFile}": [${suggestions.join(",")}]`);
+}
+message(msg)
+message(suggestionLines.join('\n'))
 
-message("It works")
