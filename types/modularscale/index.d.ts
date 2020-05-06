cd44356b90da6247179854b2e3e1d74b50c591c4
Cyril CHAPON <cyril@permettezmoideconstruire.fr>
2020-05-06 12:34:25 +0200

    Add modularscale definitions

---
+++ types/modularscale/index.d.ts
@@ -0,0 +1,25 @@
+// Type definitions for modularscale 2.0
+// Project: https://github.com/KyleAMathews/modularscale
+// Definitions by: Cyril CHAPON <https://github.com/me>
+// Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
+
+export type RatioLiteral =
+    | 'minor second'
+    | 'major second'
+    | 'minor third'
+    | 'major third'
+    | 'augmented fourth'
+    | 'perfect fifth'
+    | 'minor sixth'
+    | 'golden'
+    | 'phi'
+    | 'major sixth'
+    | 'minor seventh'
+    | 'major seventh'
+    | 'octave'
+    | 'major tenth'
+    | 'major eleventh'
+    | 'major twelfth'
+    | 'double octave';
+
+export default function modularscale(value: number, ratio?: number | RatioLiteral): number;
