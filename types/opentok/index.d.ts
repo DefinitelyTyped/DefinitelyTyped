ea12447749374f52bd39b4c8256c7f92a1323b28
Victor Alencar <victor.a.alencar@gmail.com>
2020-05-06 09:56:08 -0300

    Update opentok to include Broadcast and Signaling api

--- types/opentok/index.d.ts
+++ types/opentok/index.d.ts
@@ -3,6 +3,7 @@
 // Definitions by: Seth Westphal <https://github.com/westy92>
 //                 Anthony Messerschmidt <https://github.com/CatGuardian>
 //                 Andrej Mihajlov <https://github.com/pronebird>
+//                 Victor Alencar <https://github.com/valencar>
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
 
 declare module 'opentok' {
@@ -35,6 +36,7 @@ declare module 'opentok' {
       hasVideo?: boolean;
       outputMode?: OutputMode;
       layout?: ArchiveLayoutOptions;
+      resolution?: string;
     }
 
     export type ArchiveLayoutOptions = PredefinedArchiveLayoutOptions | CustomArchiveLayoutOptions;
@@ -77,18 +79,70 @@ declare module 'opentok' {
       offset?: number;
       sessionId?: string;
     }
+
+    export interface BroadcastLayoutOptions {
+      type: 'bestFit' | 'pip' | 'verticalPresentation' | 'horizontalPresentation' | 'custom';
+      stylesheet?: string
+    }
+
+    export interface BroadcastOutputOptionsRtmp {
+      id: string;
+      serverUrl: string;
+      streamName: string;
+      status?: string;
+    }
+
+    export interface BroadcastOutputOptions {
+      hls?: {};
+      rtmp?: BroadcastOutputOptionsRtmp[];
+    }
+
+    export interface BroadcastOptions {
+      outputs: BroadcastOutputOptions;
+      maxDuration?: number;
+      resolution?: string;
+      layout: BroadcastLayoutOptions;
+    }
+
+    export interface BroadcastUrlsResponse {
+      hls?: string;
+      rtmp?: BroadcastOutputOptionsRtmp[];
+    }
+
+    export interface Broadcast {
+      id: string;
+      status: string;
+      broadcastUrls: BroadcastUrlsResponse;
+    }
+
+    export interface BroadcastStopResponse {
+      id: string;
+      sessionId: string;
+      projectId: number;
+      createdAt: number;
+      updatedAt: number;
+      resolution: string;
+    }
+
+    export interface SignalOptions {
+      type: string;
+      data: any;
+    }
   }
 
   class OpenTok {
     constructor(apiKey: string, apiSecret: string);
 
-    public createSession(options: OpenTok.SessionOptions, callback: (error: Error | null, session?: OpenTok.Session) => void): void;
+    public createSession(options: OpenTok.SessionOptions, callback: (error: Error, session: OpenTok.Session) => void): void;
     public generateToken(sessionId: string, options: OpenTok.TokenOptions): OpenTok.Token;
-    public startArchive(sessionId: string, options: OpenTok.ArchiveOptions, callback: (error: Error | null, archive?: OpenTok.Archive) => void): void;
-    public stopArchive(archiveId: string, callback: (error: Error | null, archive?: OpenTok.Archive) => void): void;
+    public startArchive(sessionId: string, options: OpenTok.ArchiveOptions, callback: (error: Error, archive: OpenTok.Archive) => void): void;
+    public stopArchive(archiveId: string, callback: (error: Error, archive: OpenTok.Archive) => void): void;
     public getArchive(archiveId: string, callback: (error: Error | null, archive?: OpenTok.Archive) => void): void;
     public deleteArchive(archiveId: string, callback: (error: Error | null) => void): void;
     public listArchives(options: OpenTok.ListArchivesOptions, callback: (error: Error | null, archives?: OpenTok.Archive[], totalCount?: number) => void): void;
+    public startBroadcast(sessionId: string, options: OpenTok.BroadcastOptions, callback: (error: Error, broadcast: OpenTok.Broadcast) => void): void;
+    public stopBroadcast(broadcastId: string, callback: (error: Error, broadcast: OpenTok.BroadcastStopResponse) => void): void;
+    public signal(sessionId: string, connectionId: string | null, data: OpenTok.SignalOptions, callback: (error: Error) => void): void;
   }
 
   export = OpenTok;
