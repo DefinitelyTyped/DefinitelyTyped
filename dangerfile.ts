02bb184e1a2a6118bd49c8748cf61df802e55484
Nathan Shively-Sanders <293473+sandersn@users.noreply.github.com>
2020-05-06 16:02:04 -0700

    Better formatting
    Still not best

--- dangerfile.ts
+++ dangerfile.ts
@@ -3,14 +3,28 @@ import os = require('os')
 import { message, warn, markdown } from "danger"
 const suggestionsDir = [os.homedir(), ".dts", "suggestions"].join('/')
 const lines: string[] = []
+const missingProperty = /module exports a property named '(.+?)', which is missing/
 if (fs.existsSync(suggestionsDir)) {
     for (const suggestionFile of fs.readdirSync(suggestionsDir)) {
         const path = [suggestionsDir, suggestionFile].join('/')
         const suggestions = fs.readFileSync(path, "utf8").split('\n').map(x => JSON.parse(x)) as Array<{ fileName: string, ruleName: string, message: string }>
-        const name = suggestionFile.slice(0, suggestionFile.indexOf('.txt'))
-        lines.push("## " + name)
+        const packageName = suggestionFile.slice(0, suggestionFile.indexOf('.txt'))
+        const missingProperties: { [s: string]: string[] } = {}
+        lines.push("## " + packageName)
         for (const s of suggestions) {
-            lines.push("- " + s.fileName.slice(s.fileName.indexOf("types/" + name)) + " &mdash; " + s.message)
+            const fileName = s.fileName.slice(s.fileName.indexOf("types/" + packageName + "/") + ("types/" + packageName + "/").length)
+            const match = s.message.match(missingProperty)
+            const identifier = match ? match[1] : s.message
+            if (fileName in missingProperties) {
+                missingProperties[fileName].push(identifier)
+            }
+            else {
+                missingProperties[fileName] = [identifier]
+            }
+        }
+        for (const fileName in missingProperties) {
+            const properties = missingProperties[fileName]
+            lines.push("- " + fileName + " was missing the following properties: " + properties.join(','))
         }
     }
     markdown(lines.join('\n'))
