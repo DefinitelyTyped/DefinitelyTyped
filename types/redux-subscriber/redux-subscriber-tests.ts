919b1429a7dd4df4bf55ed83eea696931699cdf9
thisissami <sami@rubikon.io>
2020-05-06 22:55:38 -0700

    added definitions for redux-subscriber

---
+++ types/redux-subscriber/redux-subscriber-tests.ts
@@ -0,0 +1,5 @@
+import initSubscriber, { subscribe, Subscribe, Unsubscribe } from 'redux-subscriber';
+
+const subscribe2: Subscribe = initSubscriber({});
+const unsubscribe: Unsubscribe = subscribe('', state => {});
+const unsubscribe2: Unsubscribe = subscribe2('', state => {});
