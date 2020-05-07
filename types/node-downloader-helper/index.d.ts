2f7fdf586680ae237d3ed491c1b6d9b423a18754
Roberto Araujo <roberto.araujo.m@hotmail.com>
2020-05-07 09:01:00 -0300

    Upgrade to new version

--- types/node-downloader-helper/index.d.ts
+++ types/node-downloader-helper/index.d.ts
@@ -1,14 +1,15 @@
 // Type definitions for node-downloader-helper 1.0
 // Project: https://github.com/hgouveia/node-downloader-helper
-// Definitions by: Rémy Jeancolas <https://github.com/RemyJeancolas>
+// Definitions by: Rémy Jeancolas <https://github.com/RemyJeancolas>, Roberto Araujo <https://github.com/RemyJeancolas>
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
-// TypeScript Version: 2.4
+// TypeScript Version: 3.8
 
 /// <reference types="node" />
 
 import { EventEmitter } from 'events';
 import { RequestOptions as HttpRequestOptions, OutgoingHttpHeaders } from 'http';
 import { RequestOptions as HttpsRequestOptions } from 'https';
+import { Readable as StreamReadable } from 'stream';
 
 export interface Options {
     method?: string; // Request Method Verb
@@ -18,13 +19,35 @@ export interface Options {
     override?: boolean; // if true it will override the file, otherwise will append '(number)' to the end of file
     httpRequestOptions?: HttpRequestOptions; // Override the http request options
     httpsRequestOptions?: HttpsRequestOptions; // Override the https request options, ex: to add SSL Certs
+    retry?: {
+        maxRetries: number,
+        delay: number,
+    }; // { maxRetries: number, delay: number in ms } or false to disable (default)
+    removeOnStops?: boolean; // remove the file when is stopped (default:true)
+    removeOnFail?: boolean; // remove the file when fail (default:true)
 }
 
 export interface Stats {
     total: number; // total size that needs to be downloaded in bytes
+    name: string; // name of fille
     downloaded: number; // downloaded size in bytes
     progress: number; // progress porcentage 0-100%
     speed: number; // download speed in bytes
+}
+
+export interface DownloadInfo {
+    totalSize: number; // total file size got from the server
+    fileName: string; // assigned name
+    filePath: string; // download path
+    isResumed: boolean; // if the download is a resume,
+    downloadedSize: number; // the downloaded amount (only if is resumed otherwise always 0)
+}
+
+export interface FilePaths {
+    path: string;
+    fileName: string;
+    prevPath: string;
+    prevFileName: string;
 }
 
 export enum DH_STATES {
@@ -39,7 +62,11 @@ export enum DH_STATES {
 }
 
 export interface DownloaderHelper {
-    on(event: 'start' | 'download' | 'end' | 'pause' | 'resume' | 'stop' | string, callback: () => void): this;
+    on(event: 'start' | 'timeout' | 'pause' | 'stop' | string, callback: () => void): this;
+    on(event: 'download' | 'end', callback: (downloadInfo: DownloadInfo) => void): this;
+    on(event: 'resume', callback: (isResume: boolean) => void): this;
+    on(event: 'renamed', callback: (filePaths: FilePaths) => void): this;
+    on(event: 'retry', callback: (attempt: number, retryOpts: any) => void): this;
     on(event: 'progress', callback: (stats: Stats) => void): this;
     on(event: 'error', callback: (error: Error) => void): this;
     on(event: 'stateChanged', callback: (state: DH_STATES) => void): this;
@@ -51,4 +78,10 @@ export class DownloaderHelper extends EventEmitter {
     pause(): Promise<boolean>;
     resume(): Promise<boolean>;
     stop(): Promise<boolean>;
+    pipe(readableStream: StreamReadable, options?: any): StreamReadable; // Add pipes to the pipe list that will be applied later when the download starts
+    unpipe(readableStream?: StreamReadable): void; // Unpipe an stream , if a stream is not specified, then all pipes are detached.
+    getDownloadPath(): string;	// gets the full path where the file will be downloaded (available after the start phase)
+    isResumable(): boolean; // return tru/false if the download can be resumable (available after the start phase)
+    updateOptions(options: Options): void; // updates the options, can be use on pause/resume events
+    getStats(): Stats; // returns stats from the current download, these are the same stats sent via progress event
 }
