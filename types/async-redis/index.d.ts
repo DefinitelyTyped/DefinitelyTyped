d4f7f2ae0b4dd9c92dd2ab18efafd5a90f42e16e
philipp <philipp.sapronov@gmail.com>
2020-05-08 12:51:57 +0300

    add types for async-redis npm module

---
+++ types/async-redis/index.d.ts
@@ -0,0 +1,31 @@
+// Type definitions for async-redis 1.1.7
+// Definitions by: philipp-sapronov <https://github.com/https://github.com/philipp-sapronov>
+// Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
+// Minimum TypeScript Version: 3.1
+
+/// <reference types="reids" />
+
+type MethodsToPromisify = keyof Commands<boolean>;
+
+type Promisified<T = RedisClient> = {
+  [K in keyof T]: K extends MethodsToPromisify
+    ? T[K] extends (...args: infer A) => infer R
+      ? (...args: A) => Promise<R>
+      : T[K]
+    : T[K];
+};
+
+interface AsyncRedisConstructor {
+  new (port: number, host?: string, options?: ClientOpts): Promisified;
+  new (unix_socket: string, options?: ClientOpts): Promisified;
+  new (redis_url: string, options?: ClientOpts): Promisified;
+  new (options?: ClientOpts): Promisified;
+  decorate: (client: RedisClient) => Promisified;
+  createClient(port: number, host?: string, options?: ClientOpts): Promisified;
+  createClient(unix_socket: string, options?: ClientOpts): Promisified;
+  createClient(redis_url: string, options?: ClientOpts): Promisified;
+  createClient(options?: ClientOpts): Promisified;
+}
+
+declare const AsyncRedis: AsyncRedisConstructor;
+export = AsyncRedis;
