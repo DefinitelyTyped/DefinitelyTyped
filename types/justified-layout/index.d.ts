8e71a8b3092a66878fdb704869966b7f89c36ba5
Liau Jian Jie <liaujianjie@gmail.com>
2020-05-10 03:53:21 +0800

    Remove 'declare module'

--- types/justified-layout/index.d.ts
+++ types/justified-layout/index.d.ts
@@ -3,126 +3,124 @@
 // Definitions by: Alex Walter <https://github.com/ArgonAlex/>
 //                 Liau Jian Jie <https://github.com/liaujianjie/>
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
-declare module 'justified-layout' {
-    /**
-     * Item to be layout.
-     */
-    type JustifiedLayoutItem = number | { width: number; height: number };
+/**
+ * Item to be layout.
+ */
+type JustifiedLayoutItem = number | { width: number; height: number };
 
+/**
+ * Options for configuring the justified layout.
+ */
+type JustifiedLayoutOptions = {
     /**
-     * Options for configuring the justified layout.
+     * The width that boxes will be contained within irrelevant of padding.
+     * @default 1060
      */
-    type JustifiedLayoutOptions = {
-        /**
-         * The width that boxes will be contained within irrelevant of padding.
-         * @default 1060
-         */
-        containerWidth?: number;
-        /**
-         * Provide a single integer to apply padding to all sides or provide an object to apply
-         * individual values to each side.
-         * @default 10
-         */
-        containerPadding?: number | { top: number; right: number; left: number; bottom: number };
-        /**
-         * Provide a single integer to apply spacing both horizontally and vertically or provide an
-         * object to apply individual values to each axis.
-         * @default 10
-         */
-        boxSpacing?: number | { horizontal: number; vertical: number };
-        /**
-         * It's called a target because row height is the lever we use in order to fit everything in
-         * nicely. The algorithm will get as close to the target row height as it can.
-         * @default 320
-         */
-        targetRowHeight?: number;
-        /**
-         * How far row heights can stray from targetRowHeight. `0` would force rows to be the
-         * `targetRowHeight` exactly and would likely make it impossible to justify. The value must
-         * be between `0` and `1`.
-         * @default 0.25
-         */
-        targetRowHeightTolerance?: number;
-        /**
-         * Will stop adding rows at this number regardless of how many items still need to be laid
-         * out.
-         * @default Number.POSITIVE_INFINITY
-         */
-        maxNumRows?: number;
-        /**
-         * Provide an aspect ratio here to return everything in that aspect ratio. Makes the values
-         * in your input array irrelevant. The length of the array remains relevant.
-         * @default false
-         */
-        forceAspectRatio?: boolean | number;
-        /**
-         * If you'd like to insert a full width box every n rows you can specify it with this
-         * parameter. The box on that row will ignore the targetRowHeight, make itself as wide as
-         * `containerWidth - containerPadding` and be as tall as its aspect ratio defines. It'll
-         * only happen if that item has an aspect ratio >= 1. Best to have a look at the examples to
-         * see what this does.
-         * @default false
-         */
-        fullWidthBreakoutRowCadence?: boolean | number;
-        /**
-         * By default we'll return items at the end of a justified layout even if they don't make a
-         * full row. If false they'll be omitted from the output.
-         * @default true
-         */
-        showWidows?: boolean;
-        /**
-         * If widows are visible, how should they be laid out?
-         * @default "left"
-         */
-        widowLayoutStyle?: 'left' | 'justify' | 'center';
-    };
-
+    containerWidth?: number;
     /**
-     * Computed positional and sizing properties of a box in the layout.
+     * Provide a single integer to apply padding to all sides or provide an object to apply
+     * individual values to each side.
+     * @default 10
      */
-    type LayoutBox = {
-        /**
-         * Aspect ratio of the box.
-         */
-        aspectRatio: number;
-        /**
-         * Distance between the top side of the box and the top boundary of the justified layout.
-         */
-        top: number;
-        /**
-         * Width of the box in a justified layout.
-         */
-        width: number;
-        /**
-         * Height of the box in a justified layout.
-         */
-        height: number;
-        /**
-         * Distance between the left side of the box and the left boundary of the justified layout.
-         */
-        left: number;
-    };
-
+    containerPadding?: number | { top: number; right: number; left: number; bottom: number };
     /**
-     * Results from calculating the justified layout.
+     * Provide a single integer to apply spacing both horizontally and vertically or provide an
+     * object to apply individual values to each axis.
+     * @default 10
      */
-    type JustifiedLayoutResult = {
-        /**
-         * Height of the container containing the justified layout.
-         */
-        containerHeight: number;
-        /**
-         * Number of items that are in rows that aren't fully-packed.
-         */
-        widowCount: number;
-        /**
-         * Computed positional and sizing properties of a box in the justified layout.
-         */
-        boxes: LayoutBox[];
-    };
+    boxSpacing?: number | { horizontal: number; vertical: number };
+    /**
+     * It's called a target because row height is the lever we use in order to fit everything in
+     * nicely. The algorithm will get as close to the target row height as it can.
+     * @default 320
+     */
+    targetRowHeight?: number;
+    /**
+     * How far row heights can stray from targetRowHeight. `0` would force rows to be the
+     * `targetRowHeight` exactly and would likely make it impossible to justify. The value must
+     * be between `0` and `1`.
+     * @default 0.25
+     */
+    targetRowHeightTolerance?: number;
+    /**
+     * Will stop adding rows at this number regardless of how many items still need to be laid
+     * out.
+     * @default Number.POSITIVE_INFINITY
+     */
+    maxNumRows?: number;
+    /**
+     * Provide an aspect ratio here to return everything in that aspect ratio. Makes the values
+     * in your input array irrelevant. The length of the array remains relevant.
+     * @default false
+     */
+    forceAspectRatio?: boolean | number;
+    /**
+     * If you'd like to insert a full width box every n rows you can specify it with this
+     * parameter. The box on that row will ignore the targetRowHeight, make itself as wide as
+     * `containerWidth - containerPadding` and be as tall as its aspect ratio defines. It'll
+     * only happen if that item has an aspect ratio >= 1. Best to have a look at the examples to
+     * see what this does.
+     * @default false
+     */
+    fullWidthBreakoutRowCadence?: boolean | number;
+    /**
+     * By default we'll return items at the end of a justified layout even if they don't make a
+     * full row. If false they'll be omitted from the output.
+     * @default true
+     */
+    showWidows?: boolean;
+    /**
+     * If widows are visible, how should they be laid out?
+     * @default "left"
+     */
+    widowLayoutStyle?: 'left' | 'justify' | 'center';
+};
 
-    export default function createJustifiedLayout(
-        items: JustifiedLayoutItem[],
-        options?: JustifiedLayoutOptions,
-    ): JustifiedLayoutResult;
-}
+/**
+ * Computed positional and sizing properties of a box in the layout.
+ */
+type LayoutBox = {
+    /**
+     * Aspect ratio of the box.
+     */
+    aspectRatio: number;
+    /**
+     * Distance between the top side of the box and the top boundary of the justified layout.
+     */
+    top: number;
+    /**
+     * Width of the box in a justified layout.
+     */
+    width: number;
+    /**
+     * Height of the box in a justified layout.
+     */
+    height: number;
+    /**
+     * Distance between the left side of the box and the left boundary of the justified layout.
+     */
+    left: number;
+};
+
+/**
+ * Results from calculating the justified layout.
+ */
+type JustifiedLayoutResult = {
+    /**
+     * Height of the container containing the justified layout.
+     */
+    containerHeight: number;
+    /**
+     * Number of items that are in rows that aren't fully-packed.
+     */
+    widowCount: number;
+    /**
+     * Computed positional and sizing properties of a box in the justified layout.
+     */
+    boxes: LayoutBox[];
+};
+
+export default function createJustifiedLayout(
+    items: ReadonlyArray<JustifiedLayoutItem>,
+    options?: JustifiedLayoutOptions,
+): JustifiedLayoutResult;
---
+++ types/justified-layout/justified-layout-tests.ts
@@ -0,0 +1,36 @@
+import createJustifiedLayout from './';
+
+// Testcase 1: aspect ratios without options
+
+// $ExpectType JustifiedLayoutResult
+const layout1 = createJustifiedLayout([1, 2, 3]);
+
+// Testcase 2: sizes without options
+
+// $ExpectType JustifiedLayoutResult
+const layout2 = createJustifiedLayout([
+    { width: 1, height: 2 },
+    { width: 3, height: 4 },
+]);
+
+// Testcase 3: sizes with equidistant padding
+
+// $ExpectType JustifiedLayoutResult
+const layout3 = createJustifiedLayout(
+    [
+        { width: 1, height: 2 },
+        { width: 3, height: 4 },
+    ],
+    { containerPadding: 10 },
+);
+
+// Testcase 4: sizes with non-equidistant padding
+
+// $ExpectType JustifiedLayoutResult
+const layout4 = createJustifiedLayout(
+    [
+        { width: 1, height: 2 },
+        { width: 3, height: 4 },
+    ],
+    { containerPadding: { top: 1, bottom: 2, left: 3, right: 4 } },
+);
