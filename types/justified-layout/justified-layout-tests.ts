7416d80a9a2314bb089250b47c875f5d44b08a5b
Liau Jian Jie <liaujianjie@gmail.com>
2020-05-10 03:58:11 +0800

    Cut down test cases

--- types/justified-layout/justified-layout-tests.ts
+++ types/justified-layout/justified-layout-tests.ts
@@ -1,33 +1,7 @@
 import createJustifiedLayout from 'justified-layout';
 
-// Testcase 1: aspect ratios without options
-
 // $ExpectType JustifiedLayoutResult
-const layout1 = createJustifiedLayout([1, 2, 3]);
-
-// Testcase 2: sizes without options
-
-// $ExpectType JustifiedLayoutResult
-const layout2 = createJustifiedLayout([
-    { width: 1, height: 2 },
-    { width: 3, height: 4 },
-]);
-
-// Testcase 3: sizes with equidistant padding
-
-// $ExpectType JustifiedLayoutResult
-const layout3 = createJustifiedLayout(
-    [
-        { width: 1, height: 2 },
-        { width: 3, height: 4 },
-    ],
-    { containerPadding: 10 },
-);
-
-// Testcase 4: sizes with non-equidistant padding
-
-// $ExpectType JustifiedLayoutResult
-const layout4 = createJustifiedLayout(
+createJustifiedLayout(
     [
         { width: 1, height: 2 },
         { width: 3, height: 4 },
