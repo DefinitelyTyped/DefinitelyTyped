f2f8ca84bf813cae28a96e213375a5bceb4a933b
Rodrigo Ribeiro <ribeiro.rodrigo1989@gmail.com>
2020-05-10 17:36:36 -0300

    create getMemoryTotal

--- types/memmonit/index.d.ts
+++ types/memmonit/index.d.ts
@@ -18,3 +18,9 @@ export function memoryUsageTrunc(unit: string): number
  * @returns {number} Memory value used fixed
  */
 export function getMemoryUsageFixed(unit: string, value: number): number
+/**
+ * Returns the total memory used.
+ * @param {string} unit Return measurement unit. Support GB, MB or kB
+ * @returns {number} Memory value total
+ */
+export function getMemoryTotal(unit: string): number
