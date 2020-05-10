b53f7df577698608ba81055f7644b0cc839f981f
Rodrigo Ribeiro <ribeiro.rodrigo1989@gmail.com>
2020-05-10 17:35:07 -0300

    create memoryUsageTrunc

--- types/memmonit/index.d.ts
+++ types/memmonit/index.d.ts
@@ -6,3 +6,9 @@ declare module 'memmonit'
  * @returns {number} Memory Usage
  */
 export function getMemoryUsage(unit: string): number
+/**
+ * Returns the amount of memory used.
+ * @param {string} unit Return measurement unit. Support GB, MB or kB
+ * @returns {number} Memory value used truncated
+ */
+export function memoryUsageTrunc(unit: string): number
