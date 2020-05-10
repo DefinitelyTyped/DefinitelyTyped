fd73ff87b73aac7298632294e80e88e1010b429a
Patrick Smith <pgwsmith@gmail.com>
2020-05-11 09:36:34 +1000

    5.6 not 5.6.0

--- types/testing-library__jest-dom/index.d.ts
+++ types/testing-library__jest-dom/index.d.ts
@@ -1,4 +1,4 @@
-// Type definitions for @testing-library/jest-dom 5.6.0
+// Type definitions for @testing-library/jest-dom 5.6
 // Project: https://github.com/testing-library/jest-dom
 // Definitions by: Ernesto García <https://github.com/gnapse>
 //                 John Gozde <https://github.com/jgoz>
