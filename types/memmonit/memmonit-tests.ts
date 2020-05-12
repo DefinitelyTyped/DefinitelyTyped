96c9b875927008f71fa013cd08b0cb47339cd3ad
Rodrigo Ribeiro <ribeiro.rodrigo1989@gmail.com>
2020-05-11 21:16:47 -0300

    lint tests

--- types/memmonit/memmonit-tests.ts
+++ types/memmonit/memmonit-tests.ts
@@ -1,21 +1,22 @@
+
 import memmonit = require('memmonit')
 
-let memoryUsage = memmonit.getMemoryUsage("GB");
+const memoryUsage = memmonit.getMemoryUsage("GB");
 console.log(memoryUsage);    //2.015354681
 
-let memoryUsageTrunc = memmonit.memoryUsageTrunc("GB");
+const memoryUsageTrunc = memmonit.memoryUsageTrunc("GB");
 console.log(memoryUsageTrunc);   //2
 
-let memoryUsageFixed = memmonit.getMemoryUsageFixed("GB", 2);
+const memoryUsageFixed = memmonit.getMemoryUsageFixed("GB", 2);
 console.log(memoryUsageFixed);   //2.02
 
-let memoryTotal = memmonit.getMemoryTotal("GB");
+const memoryTotal = memmonit.getMemoryTotal("GB");
 console.log(memoryTotal);    //15.364312
 
-let memoryTotalTrunc = memmonit.getMemoryTotalTrunc("GB");
+const memoryTotalTrunc = memmonit.getMemoryTotalTrunc("GB");
 console.log(memoryTotalTrunc);   //15
 
-let memoryTotalFixed = memmonit.getMemoryTotalFixed("GB", 2);
+const memoryTotalFixed = memmonit.getMemoryTotalFixed("GB", 2);
 console.log(memoryTotalFixed);
 
 memmonit.registerMemoryUsage("./test.txt", "GB")
