1b38f437d3e29016924db5918dd361fc9ee80fb9
Gerhard Stoebich <18708370+Flarna@users.noreply.github.com>
2020-05-12 10:20:01 +0200

    node: add errorMonitor on EventEmitter
    The property errorMonitor is a static property on EventEmitter.
    This avoid the need to import errorMonitor seperate of EventEmitter.

--- types/node/events.d.ts
+++ types/node/events.d.ts
@@ -43,6 +43,16 @@ declare module "events" {
             /** @deprecated since v4.0.0 */
             static listenerCount(emitter: EventEmitter, event: string | symbol): number;
             static defaultMaxListeners: number;
+            /**
+             * This symbol shall be used to install a listener for only monitoring `'error'`
+             * events. Listeners installed using this symbol are called before the regular
+             * `'error'` listeners are called.
+             *
+             * Installing a listener using this symbol does not change the behavior once an
+             * `'error'` event is emitted, therefore the process will still crash if no
+             * regular `'error'` listener is installed.
+             */
+            static readonly errorMonitor: unique symbol;
         }
     }
 
