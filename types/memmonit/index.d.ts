447701df9dcad30bfe07b3146b8646bd745cde10
Rodrigo Ribeiro <ribeiro.rodrigo1989@gmail.com>
2020-05-11 21:26:09 -0300

    lint tests other corrections

--- types/memmonit/index.d.ts
+++ types/memmonit/index.d.ts
@@ -1,56 +1,59 @@
-declare module 'memmonit'
+// Type definitions for memmonit 1.1
+// Project: https://github.com/diegodamato/memmonit
+// Definitions by: Rodrigo Ribeiro <https://github.com/ribeiro-rodrigo>
+// Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
 
 /**
  * Returns the amount of memory used.
- * @param {string} unit Return measurement unit. Support GB, MB or kB
- * @returns {number} Memory Usage
+ * @param unit Return measurement unit. Support GB, MB or kB
+ * @returns Memory Usage
  */
 export function getMemoryUsage(unit: string): number
 /**
  * Returns the amount of memory used.
- * @param {string} unit Return measurement unit. Support GB, MB or kB
- * @returns {number} Memory value used truncated
+ * @param unit Return measurement unit. Support GB, MB or kB
+ * @returns Memory value used truncated
  */
 export function memoryUsageTrunc(unit: string): number
 /**
  * Returns the amount of memory used.
- * @param {string} unit Return measurement unit. Support GB, MB or kB
- * @returns {number} Memory value used fixed
+ * @param unit Return measurement unit. Support GB, MB or kB
+ * @returns Memory value used fixed
  */
 export function getMemoryUsageFixed(unit: string, value: number): number
 /**
  * Returns the total memory used.
- * @param {string} unit Return measurement unit. Support GB, MB or kB
- * @returns {number} Memory value total
+ * @param unit Return measurement unit. Support GB, MB or kB
+ * @returns Memory value total
  */
 export function getMemoryTotal(unit: string): number
 /**
  * Returns the total memory used.
- * @param {string} unit Return measurement unit. Support GB, MB or kB
- * @returns {number} Memory value total truncated
+ * @param unit Return measurement unit. Support GB, MB or kB
+ * @returns Memory value total truncated
  */
 export function getMemoryTotalTrunc(unit: string): number
 /**
  * Returns the total memory used.
- * @param {string} unit Return measurement unit. Support GB, MB or kB
- * @returns {number} Memory value total fixed
+ * @param unit Return measurement unit. Support GB, MB or kB
+ * @returns Memory value total fixed
  */
 export function getMemoryTotalFixed(unit: string, value: number): number
 /**
  * Writes the measurement to a file.
- * @param {string} file Recording file
- * @param {string} unit Return measurement unit. Support GB, MB or kB
+ * @param file Recording file
+ * @param unit Return measurement unit. Support GB, MB or kB
  */
 export function registerMemoryUsage(file: string, unit: string): void
 /**
  * Writes the measurement to a file.
- * @param {string} file Recording file
- * @param {string} unit Return measurement unit. Support GB, MB or kB
+ * @param file Recording file
+ * @param unit Return measurement unit. Support GB, MB or kB
  */
 export function registerMemoryUsageTrunc(file: string, unit: string): void
 /**
  * Writes the measurement to a file.
- * @param {string} file Recording file
- * @param {string} unit Return measurement unit. Support GB, MB or kB
+ * @param file Recording file
+ * @param unit Return measurement unit. Support GB, MB or kB
  */
 export function registerMemoryUsageFixed(file: string, unit: string): void 
--- types/memmonit/memmonit-tests.ts
+++ types/memmonit/memmonit-tests.ts
@@ -1,24 +1,29 @@
-
-import memmonit = require('memmonit')
+import memmonit = require('memmonit');
 
 const memoryUsage = memmonit.getMemoryUsage("GB");
-console.log(memoryUsage);    //2.015354681
+console.log(memoryUsage);    // 2.015354681
 
 const memoryUsageTrunc = memmonit.memoryUsageTrunc("GB");
-console.log(memoryUsageTrunc);   //2
+
+console.log(memoryUsageTrunc);   // 2
 
 const memoryUsageFixed = memmonit.getMemoryUsageFixed("GB", 2);
-console.log(memoryUsageFixed);   //2.02
+console.log(memoryUsageFixed);   // 2.02
 
 const memoryTotal = memmonit.getMemoryTotal("GB");
-console.log(memoryTotal);    //15.364312
+
+console.log(memoryTotal);    // 15.364312
 
 const memoryTotalTrunc = memmonit.getMemoryTotalTrunc("GB");
-console.log(memoryTotalTrunc);   //15
+
+console.log(memoryTotalTrunc);   // 15
 
 const memoryTotalFixed = memmonit.getMemoryTotalFixed("GB", 2);
+
 console.log(memoryTotalFixed);
 
-memmonit.registerMemoryUsage("./test.txt", "GB")
-memmonit.registerMemoryUsageTrunc("./test.txt", "GB")
-memmonit.registerMemoryUsageFixed("./test.txt", "GB")
+memmonit.registerMemoryUsage("./test.txt", "GB");
+
+memmonit.registerMemoryUsageTrunc("./test.txt", "GB");
+
+memmonit.registerMemoryUsageFixed("./test.txt", "GB");
