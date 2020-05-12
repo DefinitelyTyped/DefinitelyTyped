e62a2b9a75ce8ca998bfa88ebc36f6eab973c884
Rodrigo Ribeiro <ribeiro.rodrigo1989@gmail.com>
2020-05-11 21:27:29 -0300

    lint index.d.ts

--- types/memmonit/index.d.ts
+++ types/memmonit/index.d.ts
@@ -8,52 +8,52 @@
  * @param unit Return measurement unit. Support GB, MB or kB
  * @returns Memory Usage
  */
-export function getMemoryUsage(unit: string): number
+export function getMemoryUsage(unit: string): number;
 /**
  * Returns the amount of memory used.
  * @param unit Return measurement unit. Support GB, MB or kB
  * @returns Memory value used truncated
  */
-export function memoryUsageTrunc(unit: string): number
+export function memoryUsageTrunc(unit: string): number;
 /**
  * Returns the amount of memory used.
  * @param unit Return measurement unit. Support GB, MB or kB
  * @returns Memory value used fixed
  */
-export function getMemoryUsageFixed(unit: string, value: number): number
+export function getMemoryUsageFixed(unit: string, value: number): number;
 /**
  * Returns the total memory used.
  * @param unit Return measurement unit. Support GB, MB or kB
  * @returns Memory value total
  */
-export function getMemoryTotal(unit: string): number
+export function getMemoryTotal(unit: string): number;
 /**
  * Returns the total memory used.
  * @param unit Return measurement unit. Support GB, MB or kB
  * @returns Memory value total truncated
  */
-export function getMemoryTotalTrunc(unit: string): number
+export function getMemoryTotalTrunc(unit: string): number;
 /**
  * Returns the total memory used.
  * @param unit Return measurement unit. Support GB, MB or kB
  * @returns Memory value total fixed
  */
-export function getMemoryTotalFixed(unit: string, value: number): number
+export function getMemoryTotalFixed(unit: string, value: number): number;
 /**
  * Writes the measurement to a file.
  * @param file Recording file
  * @param unit Return measurement unit. Support GB, MB or kB
  */
-export function registerMemoryUsage(file: string, unit: string): void
+export function registerMemoryUsage(file: string, unit: string): void;
 /**
  * Writes the measurement to a file.
  * @param file Recording file
  * @param unit Return measurement unit. Support GB, MB or kB
  */
-export function registerMemoryUsageTrunc(file: string, unit: string): void
+export function registerMemoryUsageTrunc(file: string, unit: string): void;
 /**
  * Writes the measurement to a file.
  * @param file Recording file
  * @param unit Return measurement unit. Support GB, MB or kB
  */
-export function registerMemoryUsageFixed(file: string, unit: string): void 
+export function registerMemoryUsageFixed(file: string, unit: string): void;
