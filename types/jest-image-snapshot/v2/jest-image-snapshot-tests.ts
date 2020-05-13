88e445fb262155ab4a24821eb2868c3b7c8f24a6
Piotr Błażejewicz (Peter Blazejewicz) <peterblazejewicz@users.noreply.github.com>
2020-05-13 20:38:12 +0200

    feat(jest-image-snapshot): update to v3.1 (#44411)
    - v2 created
    - updated to version 3.1
    - maintainer added
    
    https://github.com/americanexpress/jest-image-snapshot/compare/v2.12.0...v3.1.0
    
    Thanks!

--- types/jest-image-snapshot/v2/jest-image-snapshot-tests.ts
+++ types/jest-image-snapshot/v2/jest-image-snapshot-tests.ts
@@ -9,14 +9,13 @@ it('should be able to use toMatchImageSnapshot in a test', () => {
 
 it('should be able to use configureToMatchImageSnapshot in a test', () => {
     const matchFn = configureToMatchImageSnapshot({
-        allowSizeMismatch: true,
         noColors: true,
         customDiffConfig: {
             threshold: 5,
-            includeAA: false,
+            includeAA: false
         },
         failureThreshold: 10,
-        failureThresholdType: 'percent',
+        failureThresholdType: 'percent'
     });
     expect.extend({ toMatchImageSnapshot: matchFn });
 
@@ -27,17 +26,16 @@ it('Should be able to use configuration directly in toMatchImageSnapshot', () =>
     expect.extend({ toMatchImageSnapshot });
 
     const options: MatchImageSnapshotOptions = {
-        allowSizeMismatch: false,
         noColors: true,
         customDiffConfig: {
             threshold: 5,
-            includeAA: false,
+            includeAA: false
         },
         customDiffDir: './diffs',
         diffDirection: 'vertical',
         updatePassedSnapshot: true,
         failureThreshold: 10,
-        failureThresholdType: 'percent',
+        failureThresholdType: 'percent'
     };
 
     expect('Me').toMatchImageSnapshot(options);
