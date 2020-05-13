10dbc6d630395c410dfa0801c187e400038fc7a3
Soc Sieng <socsieng@google.com>
2020-05-13 00:04:04 -0700

    Update version number

--- types/googlepay/index.d.ts
+++ types/googlepay/index.d.ts
@@ -1,4 +1,4 @@
-// Type definitions for non-npm package Google Pay API 0.3
+// Type definitions for non-npm package Google Pay API 0.4
 // Project: https://developers.google.com/pay/api/web/
 // Definitions by: Florian Luccioni <https://github.com/Fluccioni>,
 //                 Radu Raicea <https://github.com/Radu-Raicea>,
