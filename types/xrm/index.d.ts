82a1fe0eb5e67993fe6533fe15cd63db40800c10
Derek Finlinson <dcfinlinson@gmail.com>
2020-05-13 10:19:34 -0600

    Add entity record to Xrm.Navigation.navigateTo (#44552)
    * Fix ViewSelector methods
    
    * Add missing event handler methods
    
    * Add prevent default to stage change event args
    
    * Remove whitespace
    
    * Remove additional whitespace
    
    * Merge remote-tracking branch 'upstream/master'
    
    * Add entityrecord pageType to Xrm.Navigation.navigateTo
    
    * Make data optional

--- types/xrm/index.d.ts
+++ types/xrm/index.d.ts
@@ -4459,6 +4459,54 @@ declare namespace Xrm {
             roleType?: XrmEnum.RoleType;
         }
 
+        interface PageInputEntityRecord {
+            pageType: "entityrecord";
+            /**
+             * Logical name of the entity to display the form for.
+             * */
+            entityName: string;
+            /**
+             * ID of the entity record to display the form for. If you don't specify this value, the form will be opened in create mode.
+             * */
+            entityId?: string;
+            /**
+             * Designates a record that will provide default values based on mapped attribute values. The lookup object has the following String properties: entityType, id, and name (optional).
+             */
+            createFromEntity?: boolean;
+            /**
+             * A dictionary object that passes extra parameters to the form. Invalid parameters will cause an error.
+             */
+            data?: { [attributeName: string]: any };
+            /**
+             * ID of the form instance to be displayed.
+             */
+            formId?: string;
+            /**
+             * Indicates whether the form is navigated to from a different entity using cross-entity business process flow.
+             */
+            isCrossEntityNavigate?: boolean;
+            /**
+             * Indicates whether there are any offline sync errors.
+             */
+            isOfflineSyncError?: boolean;
+            /**
+             * ID of the business process to be displayed on the form.
+             */
+            processId?: string;
+            /**
+             * ID of the business process instance to be displayed on the form.
+             */
+            processInstanceId?: string;
+            /**
+             * Define a relationship object to display the related records on the form.
+             */
+            relationship?: Relationship;
+            /**
+             * ID of the selected stage in business process instance.
+             */
+            selectedStageId?: string;
+        }
+
         interface PageInputEntityList {
             pageType: "entitylist";
             /**
@@ -4533,7 +4581,7 @@ declare namespace Xrm {
          * @param pageInput Input about the page to navigate to. The object definition changes depending on the type of page to navigate to: entity list or HTML web resource.
          * @param navigationOptions Options for navigating to a page: whether to open inline or in a dialog. If you don't specify this parameter, page is opened inline by default.
          */
-        navigateTo(pageInput: Navigation.PageInputEntityList | Navigation.PageInputHtmlWebResource, navigationOptions?: Navigation.NavigationOptions): Async.PromiseLike<any>;
+        navigateTo(pageInput: Navigation.PageInputEntityRecord | Navigation.PageInputEntityList | Navigation.PageInputHtmlWebResource, navigationOptions?: Navigation.NavigationOptions): Async.PromiseLike<any>;
 
         /**
          * Displays an alert dialog containing a message and a button.
