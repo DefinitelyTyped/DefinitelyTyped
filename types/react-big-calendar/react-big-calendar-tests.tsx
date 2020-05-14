ea3a0b763235a24aa4125599d79277fce940247a
Tom Price <tom@codedinternet.com>
2020-05-14 01:06:31 +0100

    [@types/react-big-calendar] Add custom day layout algorithm functions.
    Define a type for the function and allow this type in the calendar props.

--- types/react-big-calendar/react-big-calendar-tests.tsx
+++ types/react-big-calendar/react-big-calendar-tests.tsx
@@ -17,8 +17,8 @@ import {
     EventProps,
     EventWrapperProps,
     NavigateAction,
-    Culture
-} from "react-big-calendar";
+    Culture, DayLayoutAlgorithm, DayLayoutFunction,
+} from 'react-big-calendar';
 import withDragAndDrop from "react-big-calendar/lib/addons/dragAndDrop";
 
 // Don't want to add this as a dependency, because it is only used for tests.
@@ -198,7 +198,7 @@ class CalendarResource {
                         const end = slotInfo.end;
                         return true;
                     }}
-                    dayLayoutAlgorithm="overlap"
+                    dayLayoutAlgorithm={customLayoutAlgorithm}
                     views={['day']}
                     toolbar={true}
                     popup={true}
@@ -332,6 +332,18 @@ const customGroupSlotPropGetter = () => {
     return {
         className: 'slot-group'
     };
+};
+
+const customLayoutAlgorithm: DayLayoutFunction<CalendarEvent> = (args: {
+    events: CalendarEvent[],
+    minimumStartDifference: any,
+    slotMetrics: any,
+    accessors: any,
+}) => {
+    // This is where the events would get styled in an actual algorithm, but for TS test we just want to confirm it returns
+    return args.events.map(e => {
+        return { event: e, style: {} };
+    });
 };
 
 function Event(props: EventProps<CalendarEvent>) {
