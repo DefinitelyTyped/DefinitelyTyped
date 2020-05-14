1afe9a65d2ee3937db1404576f829bcb5e654acf
Dor <36549796+Dor256@users.noreply.github.com>
2020-05-15 01:28:56 +0300

    Adds type definitions for `jest-circus` for Detox package. (#44495)
    * Adds type definitions to support jest-circus configuration
    
    * Adds type definitions to support jest-circus configuration for detox
    
    * Adds author
    
    * Updates version of detox matching typedefs

--- types/detox/runners/jest/specReporter.d.ts
+++ types/detox/runners/jest/specReporter.d.ts
@@ -1,4 +1,4 @@
-interface JasmineSpecReporter {
+interface SpecReporter extends Detox.CircusTestEventListenerBase {
     // These are not publicly used, but are defined in order to overlap with the jasmine.CustomReporter interface (which is a weak interface)
     suiteStarted: () => void;
     suiteDone: () => void;
@@ -6,6 +6,6 @@ interface JasmineSpecReporter {
     specStarted: () => void;
 }
 
-declare const specReporter: JasmineSpecReporter;
+declare const specReporter: SpecReporter;
 
 export = specReporter;
