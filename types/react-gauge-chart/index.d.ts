c240d940db542f07afc656889cc454eb1aa2c049
Meir Keller <mrzkeller@gmail.com>
2020-05-16 14:32:14 +0300

    Added new type definition for react-gauge-chart

---
+++ types/react-gauge-chart/index.d.ts
@@ -0,0 +1,29 @@
+// Type definitions for react-gauge-chart 0.2
+// Project: https://martin36.github.io/react-gauge-chart/
+// Definitions by: Meir Keller <https://github.com/meirkl>
+// Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
+
+import * as React from 'react';
+
+export interface GaugeChartProps {
+    id: string;
+    className?: string;
+    style?: React.CSSProperties;
+    marginInPercent?: number;
+    cornerRadius?: number;
+    nrOfLevels?: number;
+    percent?: number;
+    arcPadding?: number;
+    arcWidth?: number;
+    arcsLength?: number[];
+    colors?: string[];
+    textColor?: string;
+    needleColor?: string;
+    needleBaseColor?: string;
+    hideText?: boolean;
+    animate?: boolean;
+    animDelay?: number;
+    formatTextValue?: (value: string) => string;
+}
+
+export default function GaugeChart(props: GaugeChartProps): React.ReactElement;
