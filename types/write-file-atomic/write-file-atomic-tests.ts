cceac6a8bde8d417eea5ab74223e957c7b8596db
Peter Blazejewicz <peterblazejewicz@users.noreply.github.com>
2020-05-17 13:30:26 +0200

    feat(write-file-encoding): add support for encoding option
    The api supports string as an option This string option is the encodign
    of the data, defaulting to 'utf8'. As the Node types are referenced,
    simply BufferEncoding is beign used instead of string or string aliases
    to align with Node types.
    For backward compatiblity options 'encoding' is defined as:
    BufferEncoding | ''
    
    https://github.com/npm/write-file-atomic#write-file-atomic
    
    Thanks!

--- types/write-file-atomic/write-file-atomic-tests.ts
+++ types/write-file-atomic/write-file-atomic-tests.ts
@@ -6,9 +6,11 @@ writeFileAtomic('message.txt', 'Hello Node', {chown: {uid: 100, gid: 50}}, err =
 writeFileAtomic('message.txt', 'Hello Node', {encoding: ''}, err => {});
 writeFileAtomic('message.txt', 'Hello Node', {fsync: false}, err => {});
 writeFileAtomic('message.txt', 'Hello Node', {mode: 123}, err => {});
+writeFileAtomic('message.txt', 'Hello Node', 'utf8', err => {});
 
 writeFileAtomic('message.txt', 'Hello Node').then(() => {}).catch(() => {});
 writeFileAtomic('message.txt', 'Hello Node', {mode: 123}).then(() => {}).catch(() => {});
+writeFileAtomic('message.txt', 'Hello Node', 'utf8').then(() => {}).catch(() => {});
 
 writeFileAtomic.sync('message.txt', 'Hello Node');
 
@@ -16,3 +18,4 @@ writeFileAtomic.sync('message.txt', 'Hello Node', {chown: {uid: 100, gid: 50}});
 writeFileAtomic.sync('message.txt', 'Hello Node', {encoding: ''});
 writeFileAtomic.sync('message.txt', 'Hello Node', {fsync: false});
 writeFileAtomic.sync('message.txt', 'Hello Node', {mode: 123});
+writeFileAtomic.sync('message.txt', 'Hello Node', 'utf8');
