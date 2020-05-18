ef2a7fc474f7d10a734600eb1c42ca525f307167
Eitan Levi <eitan.levi@docusign.com>
2020-05-18 09:27:37 -0700

    [config] Add export for DeferredConfig class
    The node-config package's `deferConfig` function returns an instance of
    `DeferredConfig`. It later uses `instanceof DeferredConfig` to see if
    the value needs to be resolved. This change adds an export for
    `DeferredConfig` and also updates the `deferConfig` function to return
    that instance.

--- types/config/defer.d.ts
+++ types/config/defer.d.ts
@@ -1,8 +1,15 @@
-type DeferFunction<T,R> = (this: any, origValue: T) => R;
-
 /*
  * Allows a config value to be deferred for later. The original config object is passed
  * into func as "this".
  * See: https://github.com/lorenwest/node-config/wiki/Special-features-for-JavaScript-configuration-files#deferred-values-in-javascript-configuration-files
  */
-export function deferConfig<T, R>(func: DeferFunction<T,R>): R;
+type DeferFunction<T,R> = (this: any, origValue: T) => R;
+
+import { IConfig } from '.';
+
+export class DeferredConfig<T, R> {
+    prepare(config: IConfig, prop: T, property: keyof T): this;
+    resolve(): R;
+}
+
+export function deferConfig<T, R>(func: DeferFunction<T,R>): DeferredConfig<T, R>;
