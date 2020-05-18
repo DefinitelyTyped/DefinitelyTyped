ae6d003ad1339467cb5f7b753da47dbb9d02bce8
Kevin Wylder <wylderkevin@gmail.com>
2020-05-18 12:28:30 -0700

    Use dts initialization and add test

--- types/motion-sensors-polyfill/index.d.ts
+++ types/motion-sensors-polyfill/index.d.ts
@@ -1,6 +1,13 @@
+// Type definitions for motion-sensors-polyfill 0.3
+// Project: https://github.com/kenchris/lit-element
+// Definitions by: Kevin Wylder <https://github.com/me>
+// Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
+
+export {};
+
 type EventHandler = (event: Event) => void;
 
-declare class Sensor extends EventTarget {
+export class Sensor extends EventTarget {
     readonly activated: boolean;
     readonly hasReading: boolean;
     readonly timestamp?: DOMHighResTimeStamp;
@@ -12,44 +19,44 @@ declare class Sensor extends EventTarget {
 }
 
 interface SensorOptions {
-    frequency?: number
+    frequency?: number;
 }
 
 type AccelerometerLocalCoordinateSystem = "device" | "screen";
 
 interface AccelerometerSensorOptions extends SensorOptions {
-  referenceFrame?: AccelerometerLocalCoordinateSystem // defaults to "device". IDK how to type this
+  referenceFrame?: AccelerometerLocalCoordinateSystem; // defaults to "device". IDK how to type this
 }
 
-export declare class Accelerometer extends Sensor {
+export class Accelerometer extends Sensor {
     constructor(options?: AccelerometerSensorOptions);
     readonly x: number;
     readonly y: number;
     readonly z: number;
 }
 
-export declare class LinearAccelerationSensor extends Accelerometer {
-    constructor (options?: AccelerometerSensorOptions);
+export class LinearAccelerationSensor extends Accelerometer {
+    constructor(options?: AccelerometerSensorOptions);
 }
 
-export declare class GravitySensor extends Accelerometer {
-    constructor (options?: AccelerometerSensorOptions);
+export class GravitySensor extends Accelerometer {
+    constructor(options?: AccelerometerSensorOptions);
 }
 
 interface GyroscopeSensorOptions {
-    referenceFrame?: GyroscopeLocalCoordinateSystem // defauts to "device"
+    referenceFrame?: GyroscopeLocalCoordinateSystem; // defauts to "device"
 }
 
 type GyroscopeLocalCoordinateSystem = "device" | "screen";
 
-export declare class Gyroscope extends Sensor {
-    constructor (options: GyroscopeSensorOptions);
+export class Gyroscope extends Sensor {
+    constructor(options?: GyroscopeSensorOptions);
     readonly x: number;
     readonly y: number;
     readonly z: number;
 }
 
-type OrientationSensorLocalCoordinateSystem = "device" | "screen"
+type OrientationSensorLocalCoordinateSystem = "device" | "screen";
 
 interface OrientationSensorOptions extends SensorOptions {
     referenceFrame?: OrientationSensorLocalCoordinateSystem; // defaults to "device"
@@ -57,15 +64,15 @@ interface OrientationSensorOptions extends SensorOptions {
 
 type RotationMatrixType = Float32Array | Float64Array | DOMMatrix;
 
-declare class OrientationSensor extends Sensor {
-    readonly quaternion: [ number, number, number, number ]
-    populateMatrix(matrix: RotationMatrixType): void
+export class OrientationSensor extends Sensor {
+    readonly quaternion: [ number, number, number, number ];
+    populateMatrix(matrix: RotationMatrixType): void;
 }
 
-export declare class RelativeOrientationSensor extends OrientationSensor {
+export class RelativeOrientationSensor extends OrientationSensor {
     constructor(options?: OrientationSensorOptions);
 }
 
-export declare class AbsoluteOrientationSensor extends OrientationSensor {
-    constructor (options?: OrientationSensorOptions);
+export class AbsoluteOrientationSensor extends OrientationSensor {
+    constructor(options?: OrientationSensorOptions);
 }
