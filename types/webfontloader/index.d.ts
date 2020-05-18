971713cf705447bca19223acf8c972c2ccb98fa4
Jordi Talens <talensjr@gmail.com>
2020-05-18 11:57:14 +0200

    Undo formatting

--- types/webfontloader/index.d.ts
+++ types/webfontloader/index.d.ts
@@ -7,54 +7,55 @@ export = WebFont;
 export as namespace WebFont;
 
 declare namespace WebFont {
-    export function load(config: WebFont.Config): void;
+    export function load(config:WebFont.Config):void;
     export interface Config {
         /** Setting this to false will disable html classes (defaults to true) */
-        classes?: boolean;
+        classes?:boolean;
         /** Settings this to false will disable callbacks/events (defaults to true) */
-        events?: boolean;
+        events?:boolean;
         /** Time (in ms) until the fontinactive callback will be triggered (defaults to 5000) */
-        timeout?: number;
+        timeout?:number;
         /** This event is triggered when all fonts have been requested. */
-        loading?(): void;
+        loading?():void;
         /** This event is triggered when the fonts have rendered. */
-        active?(): void;
+        active?():void;
         /** This event is triggered when the browser does not support linked fonts or if none of the fonts could be loaded. */
-        inactive?(): void;
+        inactive?():void;
         /** This event is triggered once for each font that's loaded. */
-        fontloading?(familyName: string, fvd: string): void;
+        fontloading?(familyName:string, fvd:string):void;
         /** This event is triggered once for each font that renders. */
-        fontactive?(familyName: string, fvd: string): void;
+        fontactive?(familyName:string, fvd:string):void;
         /** This event is triggered if the font can't be loaded. */
-        fontinactive?(familyName: string, fvd: string): void;
+        fontinactive?(familyName:string, fvd:string):void;
 
         /** Child window or iframes to manage fonts for */
-        context?: Array<string>;
+        context?:Array<string>;
 
-        custom?: Custom;
-        google?: Google;
-        typekit?: Typekit;
-        fontdeck?: Fontdeck;
-        monotype?: Monotype;
+        custom?:Custom;
+        google?:Google;
+        typekit?:Typekit;
+        fontdeck?:Fontdeck;
+        monotype?:Monotype;
     }
     export interface Google {
         api?: string;
-        families: Array<string>;
+        families:Array<string>;
         text?: string;
     }
     export interface Typekit {
-        id?: string;
+        id?:string;
     }
     export interface Custom {
-        families?: Array<string>;
-        urls?: Array<string>;
-        testStrings?: { [fontFamily: string]: string };
+        families?:Array<string>;
+        urls?:Array<string>;
+        testStrings?:{[fontFamily:string]:string};
     }
     export interface Fontdeck {
-        id?: string;
+        id?:string;
     }
     export interface Monotype {
-        projectId?: string;
-        version?: number;
+        projectId?:string;
+        version?:number;
     }
+
 }
