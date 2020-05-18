66de8f9d32987c33588471b26fee9677218bd6d2
IdeFFiX <ideffix1337@gmail.com>
2020-05-18 19:07:36 +0200

    create types for pascalcase

---
+++ types/pascalcase/index.d.ts
@@ -0,0 +1,6 @@
+// Type definitions for pascalcase 1.0
+// Project: https://github.com/jonschlinkert/pascalcase
+// Definitions by: IdeFFiX <https://github.com/ideffix>
+// Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
+
+export default function(value: string): string;
