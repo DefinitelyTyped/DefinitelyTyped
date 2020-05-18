fdac9aec99db3026f8bd1416d3164a4b77e42174
Alexander Cerutti <cerutti.alexander@gmail.com>
2020-05-19 00:14:17 +0200

    Replaced tab indentation with spaces

--- types/react-color/lib/components/material/Material.d.ts
+++ types/react-color/lib/components/material/Material.d.ts
@@ -3,20 +3,20 @@ import { ColorPickerProps } from "../../..";
 import { Classes } from "reactcss";
 
 export interface MaterialPickerStylesProps {
-	material: CSSProperties;
-	HEXwrap: CSSProperties;
-	HEXinput: CSSProperties;
-	HEXlabel: CSSProperties;
-	Hex: CSSProperties;
-	RGBwrap: CSSProperties;
-	RGBinput: CSSProperties;
-	RGBlabel: CSSProperties;
-	split: CSSProperties;
-	third: CSSProperties;
+    material: CSSProperties;
+    HEXwrap: CSSProperties;
+    HEXinput: CSSProperties;
+    HEXlabel: CSSProperties;
+    Hex: CSSProperties;
+    RGBwrap: CSSProperties;
+    RGBinput: CSSProperties;
+    RGBlabel: CSSProperties;
+    split: CSSProperties;
+    third: CSSProperties;
 }
 
 export interface MaterialPickerProps extends ColorPickerProps<MaterialPicker> {
-	styles?: Partial<Classes<MaterialPickerStylesProps>>;
+    styles?: Partial<Classes<MaterialPickerStylesProps>>;
 }
 
 export default class MaterialPicker extends Component<MaterialPickerProps> { }
--- types/react-color/lib/components/slider/Slider.d.ts
+++ types/react-color/lib/components/slider/Slider.d.ts
@@ -3,12 +3,12 @@ import { ColorPickerProps } from "../../..";
 import { Classes } from "reactcss";
 
 export interface SliderPickerStylesProps {
-	hue: CSSProperties;
-	Hue: CSSProperties;
+    hue: CSSProperties;
+    Hue: CSSProperties;
 }
 
 export interface SliderPickerProps extends ColorPickerProps<SliderPicker> {
-	styles?: Partial<Classes<SliderPickerStylesProps>>;
+    styles?: Partial<Classes<SliderPickerStylesProps>>;
 }
 
 export default class SliderPicker extends Component<SliderPickerProps> { }
