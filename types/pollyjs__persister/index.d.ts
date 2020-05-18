c1d8af77cc12d848793e46f03885ae6e54eb6171
Offir Golan <offirgolan@gmail.com>
2020-05-18 10:54:00 -0700

    [@pollyjs] Update types for version 4.3.0

--- types/pollyjs__persister/index.d.ts
+++ types/pollyjs__persister/index.d.ts
@@ -1,12 +1,12 @@
-// Type definitions for @pollyjs/persister 2.0
+// Type definitions for @pollyjs/persister 4.3.0
 // Project: https://github.com/netflix/pollyjs/tree/master/packages/@pollyjs/persister
 // Definitions by: feinoujc <https://github.com/feinoujc>
 //                 silverchen <https://github.com/silverchen>
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
 
 export default class Persister {
-    static readonly name: string;
+    static readonly id: string;
     static readonly type: string;
-    readonly options: any;
+    readonly options: { [key: string]: any };
     persist(): Promise<void>;
 }
