ff24b8b5aa2238ec46cc96605eacc65fa918b527
ExE Boss <3889017+ExE-Boss@users.noreply.github.com>
2020-05-18 15:05:00 +0200

    feat(es‑abstract): Split `es2016.d.ts`

---
+++ types/es-abstract/2016/SameValueNonNumber.d.ts
@@ -0,0 +1,11 @@
+/**
+ * @param x
+ * @param y
+ *
+ * @throws {TypeError} If `x` or `y` is a `number` or they're different types.
+ */
+declare function SameValueNonNumber(
+    x: object | null | undefined | string | boolean | symbol,
+    y: object | null | undefined | string | boolean | symbol,
+): boolean;
+export = SameValueNonNumber;
