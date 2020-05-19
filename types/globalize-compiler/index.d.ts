a221345a59e97c597eb63848c3ce1583135e9d9d
Daniel Friesen <d@danf.ca>
2020-05-18 18:53:15 -0700

    [globalize-compiler] Fix project homepage

--- types/globalize-compiler/index.d.ts
+++ types/globalize-compiler/index.d.ts
@@ -1,5 +1,5 @@
 // Type definitions for globalize-compiler v0.2.0
-// Project: https://github.com/jquery-support/globalize-compiler
+// Project: https://github.com/globalizejs/globalize-compiler
 // Definitions by: Ian Clanton-Thuon <https://github.com/iclanton>
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
 
