4466dd0d2afb21c3d092a3f84fe1230d53f8f7e6
Kyle Albert <kyle.albert@us.ibm.com>
2020-05-19 17:45:34 -0400

    Fix more imports.

--- types/carbon-components-react/lib/components/Grid/Column.d.ts
+++ types/carbon-components-react/lib/components/Grid/Column.d.ts
@@ -1,4 +1,4 @@
-import * as React from '../../../../react/index';
+import * as React from 'react';
 import { ReactAttr, ReactDivAttr, JSXIntrinsicElementProps, FCReturn } from '../../../typings/shared';
 
 export interface ColumnSpanDetailed {
