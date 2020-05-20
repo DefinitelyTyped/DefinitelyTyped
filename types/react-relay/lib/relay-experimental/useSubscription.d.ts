d561c2fbd2e51c092aba2b191c46d969fd83eb25
Christian Ivicevic <christian.ivicevic@comsysto.com>
2020-05-20 12:53:27 +0200

    [react-relay] Fix wrong disable linter command

--- types/react-relay/lib/relay-experimental/useSubscription.d.ts
+++ types/react-relay/lib/relay-experimental/useSubscription.d.ts
@@ -1,7 +1,7 @@
 import { GraphQLSubscriptionConfig, OperationType, requestSubscription } from 'relay-runtime';
 
 // The actual subtype of OperationType is required to allow for type inferrence inside GraphQLSubscriptionConfig.
-// eslint-disable-next-line no-unnecessary-generics
+// tslint:disable-next-line:no-unnecessary-generics
 export function useSubscription<TSubscriptionPayload extends OperationType>(
     config: GraphQLSubscriptionConfig<TSubscriptionPayload>,
     requestSubscriptionFn?: typeof requestSubscription,
