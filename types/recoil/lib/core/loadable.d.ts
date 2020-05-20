01dd0d6e78e682e6131b9821b29631916629e8c9
Christian Santos <chrissantos1995@gmail.com>
2020-05-20 17:01:03 -0400

    Remove loadable accessors

--- types/recoil/lib/core/loadable.d.ts
+++ types/recoil/lib/core/loadable.d.ts
@@ -7,53 +7,11 @@ export type ResolvedLoadablePromiseInfo<T> = Readonly<{
 
 export type LoadablePromise<T> = Promise<ResolvedLoadablePromiseInfo<T>>;
 
-export type Accessors<T> = Readonly<{
-    /**
-     * Attemps to get the value. If state === 'hasError', that error will be thrown. If state === 'loading', the pending Promise will be thrown.
-     */
-    getValue: () => T;
-
-    /**
-     * Returns a Promise that resolves (or rejects) depending on the eventual fulfillment of the underlying Promise.
-     */
-    toPromise: () => LoadablePromise<T>;
-
-    /**
-     * Will return value if it exists. Otherwise will return undefined if value is in error or loading state.
-     */
-    valueMaybe: () => T | void;
-
-    /**
-     * Throws a generic error if state !== 'hasValue' (if it is in loading or error state)
-     */
-    valueOrThrow: () => T;
-
-    /**
-     * If state === 'hasError', returns that error. Otherwise, returns undefined.
-     */
-    errorMaybe: () => Error | void;
-
-    /**
-     * If state !== 'hasError', throw a generic error message. Otherwise, return the error.
-     */
-    errorOrThrow: () => Error;
-
-    /**
-     * If state === 'loading', return the Promise. Otherwise, return undefined.
-     */
-    promiseMaybe: () => Promise<T> | void;
-
-    /**
-     * If state !== 'loading', throw a generic error message. Otherwise, return the Promise.
-     */
-    promiseOrThrow: () => Promise<T>;
-}>;
-
 export type Loadable<T> =
-    | Readonly<Accessors<T> & { state: 'hasValue'; contents: T }>
-    | Readonly<Accessors<T> & { state: 'hasError'; contents: Error }>
+    | Readonly<{ state: 'hasValue'; contents: T }>
+    | Readonly<{ state: 'hasError'; contents: Error }>
     | Readonly<
-          Accessors<T> & {
+          {
               state: 'loading';
               contents: LoadablePromise<T>;
           }
