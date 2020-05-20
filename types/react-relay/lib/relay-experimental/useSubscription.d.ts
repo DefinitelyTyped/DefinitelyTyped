e184a46badcedd797ef6245db38ba75c7b369f3d
Christian Ivicevic <christian.ivicevic@comsysto.com>
2020-05-20 13:02:20 +0200

    [react-relay] Fix wrong location of the linter comment

--- types/react-relay/lib/relay-experimental/useSubscription.d.ts
+++ types/react-relay/lib/relay-experimental/useSubscription.d.ts
@@ -1,8 +1,8 @@
 import { GraphQLSubscriptionConfig, OperationType, requestSubscription } from 'relay-runtime';
 
-// The actual subtype of OperationType is required to allow for type inferrence inside GraphQLSubscriptionConfig.
-// tslint:disable-next-line:no-unnecessary-generics
 export function useSubscription<TSubscriptionPayload extends OperationType>(
+    // The actual subtype of OperationType is required to allow for type inferrence inside GraphQLSubscriptionConfig.
+    // tslint:disable-next-line:no-unnecessary-generics
     config: GraphQLSubscriptionConfig<TSubscriptionPayload>,
     requestSubscriptionFn?: typeof requestSubscription,
 ): void;
