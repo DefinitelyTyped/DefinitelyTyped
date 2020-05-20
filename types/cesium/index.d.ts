c1b71cb5c1c294ce04768c6fd7f750fb63e09cff
h_Davy <dave3068@gmail.com>
2020-05-20 20:00:43 +0800

    fix the constructor parameters of ClippingPlane and ClippingPlaneCollection
    @types/cesium fix the constructor parameters of ClippingPlane and ClippingPlaneCollection

--- types/cesium/index.d.ts
+++ types/cesium/index.d.ts
@@ -3730,10 +3730,7 @@ declare namespace Cesium {
     class ClippingPlane {
         normal: Cartesian3;
         distance: number;
-        constructor(option: {
-            normal: Cartesian3;
-            distance: number;
-        })
+        constructor(normal: Cartesian3, distance: number);
 
         static clone(clippingPlane: ClippingPlane, result?: ClippingPlane): ClippingPlane;
         static fromPlane(plane: Plane, result?: ClippingPlane): ClippingPlane;
@@ -3750,7 +3747,7 @@ declare namespace Cesium {
         readonly length: number;
 
         constructor(options?: {
-            planes?: ClippingPlane;
+            planes?: ClippingPlane[];
             enabled?: boolean;
             modelMatrix?: Matrix4;
             unionClippingRegions?: boolean;
