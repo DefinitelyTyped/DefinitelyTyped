3a2fdd62a173e7a068db3026f2cb5d3e4e4ac462
Michael Rutter <michael.rutter@deskpro.com>
2020-05-21 10:33:49 +0100

    fixed groupByFn type to match implementation

--- types/react-table/index.d.ts
+++ types/react-table/index.d.ts
@@ -547,7 +547,7 @@ export type UseGroupByOptions<D extends object> = Partial<{
     disableGroupBy: boolean;
     defaultCanGroupBy: boolean;
     aggregations: Record<string, AggregatorFn<D>>;
-    groupByFn: (rows: Array<Row<D>>, columnId: IdType<D>) => Record<string, Row<D>>;
+    groupByFn: (rows: Array<Row<D>>, columnId: IdType<D>) => Record<string, Array<Row<D>>>;
     autoResetGroupBy?: boolean;
 }>;
 
@@ -864,7 +864,7 @@ export function defaultOrderByFn<D extends object = {}>(
 export function defaultGroupByFn<D extends object = {}>(
     rows: Array<Row<D>>,
     columnId: IdType<D>,
-): Record<string, Row<D>>;
+): Record<string, Array<Row<D>>>;
 
 export function makePropGetter(hooks: Hooks, ...meta: any[]): any;
 
