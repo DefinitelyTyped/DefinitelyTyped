84bf7058fd34c00276fb0cbd65e1cbabaac7fdd0
Jason Kwok <4410086+JasonHK@users.noreply.github.com>
2020-05-21 14:14:50 +0800

    [window-or-global] Update types/window-or-global/index.d.ts
    Co-authored-by: ExE Boss <3889017+ExE-Boss@users.noreply.github.com>

--- types/window-or-global/index.d.ts
+++ types/window-or-global/index.d.ts
@@ -2,6 +2,7 @@
 // Project: https://github.com/purposeindustries/window-or-global
 // Definitions by: vvakame <https://github.com/vvakame>
 //                 Jason Kwok <https://github.com/JasonHK>
+//                 ExE Boss <https://github.com/ExE-Boss>
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
 
 /// <reference types="node" />
