21ecb0a486420079a1973fce895b473b9081516b
Jason Kwok <JasonHK@users.noreply.github.com>
2020-05-21 22:36:48 +0800

    [json-schema] Added type definition for JSON object

--- types/json-schema/json-schema-tests.ts
+++ types/json-schema/json-schema-tests.ts
@@ -14,27 +14,27 @@ import {
     validate,
     mustBeValid,
     checkPropertyChange
-} from 'json-schema'
+} from 'json-schema';
 
 /* JSON Schema 4 */
 
 // SimpleType
-() => {
-    const a: JSONSchema4TypeName = 'string'
-    const b: JSONSchema4TypeName = 'null'
-    const c: JSONSchema4TypeName = 'any'
+{
+    const a: JSONSchema4TypeName = 'string';
+    const b: JSONSchema4TypeName = 'null';
+    const c: JSONSchema4TypeName = 'any';
 }
 
 // Type
-() => {
-    const a: JSONSchema4Type = 'foo'
-    const b: JSONSchema4Type = null
-    const c: JSONSchema4Type = [1, 2]
+{
+    const a: JSONSchema4Type = 'foo';
+    const b: JSONSchema4Type = null;
+    const c: JSONSchema4Type = [1, 2];
 }
 
 // JSONSchema4
-() => {
-    const a: JSONSchema4 = {}
+{
+    const a: JSONSchema4 = {};
     const b: JSONSchema4 = {
         id: 'foo',
         $ref: 'foo/bar',
@@ -82,32 +82,31 @@ import {
         extends: 'foo',
         bar: 4,
         format: 'date-time'
-    }
+    };
 }
 
 // Class
-class Schema4 implements JSONSchema4 {
-}
+class Schema4 implements JSONSchema4 {}
 
 /* JSON Schema 6 */
 
 // SimpleType
-() => {
-    const a: JSONSchema6TypeName = 'string'
-    const b: JSONSchema6TypeName = 'null'
-    const c: JSONSchema6TypeName = 'any'
+{
+    const a: JSONSchema6TypeName = 'string';
+    const b: JSONSchema6TypeName = 'null';
+    const c: JSONSchema6TypeName = 'any';
 }
 
 // Type
-() => {
-    const a: JSONSchema6Type = 'foo'
-    const b: JSONSchema6Type = null
-    const c: JSONSchema6Type = [1, 2]
+{
+    const a: JSONSchema6Type = 'foo';
+    const b: JSONSchema6Type = null;
+    const c: JSONSchema6Type = [1, 2];
 }
 
 // JSONSchema6Definition
-() => {
-    const a: JSONSchema6Definition = {}
+{
+    const a: JSONSchema6Definition = {};
     const b: JSONSchema6Definition = {
         $id: 'foo',
         $ref: 'foo/bar',
@@ -157,33 +156,32 @@ class Schema4 implements JSONSchema4 {
         examples: [{}],
         propertyNames: {},
         format: 'date-time'
-    }
+    };
     const c: JSONSchema6Definition = false;
 }
 
 // Class
-class Schema6 implements JSONSchema6 {
-}
+class Schema6 implements JSONSchema6 {}
 
 /* JSON Schema 7 */
 
 // SimpleType
-() => {
-    const a: JSONSchema7TypeName = 'string'
-    const b: JSONSchema7TypeName = 'number'
-    const c: JSONSchema7TypeName = 'null'
+{
+    const a: JSONSchema7TypeName = 'string';
+    const b: JSONSchema7TypeName = 'number';
+    const c: JSONSchema7TypeName = 'null';
 }
 
 // Type
-() => {
-    const a: JSONSchema7Type = 'foo'
-    const b: JSONSchema7Type = null
-    const c: JSONSchema7Type = [1, 2]
+{
+    const a: JSONSchema7Type = 'foo';
+    const b: JSONSchema7Type = null;
+    const c: JSONSchema7Type = [1, 2];
 }
 
 // JSONSchema7Definition
-() => {
-    const a: JSONSchema7Definition = {}
+{
+    const a: JSONSchema7Definition = {};
     const b: JSONSchema7Definition = {
         $id: 'foo',
         $ref: 'foo/bar',
@@ -241,13 +239,12 @@ class Schema6 implements JSONSchema6 {
         else: {},
         if: {},
         then: {}
-    }
+    };
     const c: JSONSchema7Definition = false;
 }
 
 // Class
-class Schema7 implements JSONSchema7 {
-}
+class Schema7 implements JSONSchema7 {}
 
 let result = validate({}, { $id: 'schema-id' });
 mustBeValid(result);
