cc7d6a122f29f1233e73064f7ef20795eca3adfe
wchtommy20013 <wchtommy20013@gmail.com>
2020-05-22 14:06:24 +0800

    [@types/json-diff] add definition for json-diff

---
+++ types/json-diff/index.d.ts
@@ -0,0 +1,9 @@
+// Type definitions for json-diff
+// Project: https://github.com/andreyvit/json-diff
+// Definitions by: Tommy Wong <https://github.com/wchtommy20013/>
+// Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
+
+export function diff(obj1: any, obj2: any): any;
+export function diffString(obj1: any, obj2: any): any;
+
+
