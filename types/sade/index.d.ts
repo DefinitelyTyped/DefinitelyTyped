e164d37150e840ee930f6d38f498963c1061fe0f
Peter Blazejewicz <peterblazejewicz@users.noreply.github.com>
2020-05-23 21:10:47 +0200

    update(sade): v1.7 minor update
    - support for 'alias' added
    - tests amended
    - mainter added
    - version bump
    
    https://github.com/lukeed/sade/compare/v1.6.1...v1.7.3
    
    Thanks!

--- types/sade/index.d.ts
+++ types/sade/index.d.ts
@@ -1,14 +1,25 @@
-// Type definitions for sade 1.6
+// Type definitions for sade 1.7
 // Project: https://github.com/lukeed/sade#readme
 // Definitions by: Epimodev <https://github.com/Epimodev>
+//                 Piotr Błażejewicz <https://github.com/peterblazejewicz>
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
 
 import * as mri from 'mri';
 
 type Handler = (...args: any[]) => void;
 
+/**
+ * Sade is a small but powerful tool for building command-line interface (CLI) applications for Node.js that are fast, responsive, and helpful!
+ * It enables default commands, git-like subcommands, option flags with aliases, default option values with type-casting,
+ * required-vs-optional argument handling, command validation, and automated help text generation!
+ */
 declare namespace sade {
     interface CommandOptions {
+        /**
+         * Optionally define one or more aliases for the current Command.
+         * When declared, the `opts.alias` value is passed directly to the [`prog.alias`](#progaliasnames) method.
+         */
+        alias?: string | string[];
         default?: boolean;
     }
 
@@ -17,6 +28,10 @@ declare namespace sade {
     }
 
     interface Sade {
+        /**
+         * Define one or more aliases for the current Command.
+         */
+        alias(...names: string[]): Sade;
         command(str: string, desc?: string, opts?: Readonly<CommandOptions>): Sade;
         describe(str: string | ReadonlyArray<string>): Sade;
         option(str: string, desc: string, val?: string | number): Sade;
