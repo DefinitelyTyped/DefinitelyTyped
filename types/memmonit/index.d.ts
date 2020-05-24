20da7e4bff15a07a3758822a8ae79711765e837a
Rodrigo Ribeiro <ribeiro.rodrigo1989@gmail.com>
2020-05-24 03:47:39 -0300

    Inclusion of RegisterMonitoring and MemoryMonitoring namespaces

--- types/memmonit/index.d.ts
+++ types/memmonit/index.d.ts
@@ -3,57 +3,62 @@
 // Definitions by: Rodrigo Ribeiro <https://github.com/ribeiro-rodrigo>
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
 
-/**
- * Returns the amount of memory used.
- * @param unit Return measurement unit. Support GB, MB or kB
- * @returns Memory Usage
- */
-export function getMemoryUsage(unit: string): number;
-/**
- * Returns the amount of memory used.
- * @param unit Return measurement unit. Support GB, MB or kB
- * @returns Memory value used truncated
- */
-export function memoryUsageTrunc(unit: string): number;
-/**
- * Returns the amount of memory used.
- * @param unit Return measurement unit. Support GB, MB or kB
- * @returns Memory value used fixed
- */
-export function getMemoryUsageFixed(unit: string, value: number): number;
-/**
- * Returns the total memory used.
- * @param unit Return measurement unit. Support GB, MB or kB
- * @returns Memory value total
- */
-export function getMemoryTotal(unit: string): number;
-/**
- * Returns the total memory used.
- * @param unit Return measurement unit. Support GB, MB or kB
- * @returns Memory value total truncated
- */
-export function getMemoryTotalTrunc(unit: string): number;
-/**
- * Returns the total memory used.
- * @param unit Return measurement unit. Support GB, MB or kB
- * @returns Memory value total fixed
- */
-export function getMemoryTotalFixed(unit: string, value: number): number;
-/**
- * Writes the measurement to a file.
- * @param file Recording file
- * @param unit Return measurement unit. Support GB, MB or kB
- */
-export function registerMemoryUsage(file: string, unit: string): void;
-/**
- * Writes the measurement to a file.
- * @param file Recording file
- * @param unit Return measurement unit. Support GB, MB or kB
- */
-export function registerMemoryUsageTrunc(file: string, unit: string): void;
-/**
- * Writes the measurement to a file.
- * @param file Recording file
- * @param unit Return measurement unit. Support GB, MB or kB
- */
-export function registerMemoryUsageFixed(file: string, unit: string): void;
+export namespace MemoryMonitoring {
+    /**
+     * Returns the amount of memory used.
+     * @param unit Return measurement unit. Support GB, MB or kB
+     * @returns Memory Usage
+     */
+    function getMemoryUsage(unit: string): number;
+    /**
+     * Returns the amount of memory used.
+     * @param unit Return measurement unit. Support GB, MB or kB
+     * @returns Memory value used truncated
+     */
+    function memoryUsageTrunc(unit: string): number;
+    /**
+     * Returns the amount of memory used.
+     * @param unit Return measurement unit. Support GB, MB or kB
+     * @returns Memory value used fixed
+     */
+    function getMemoryUsageFixed(unit: string, value: number): number;
+    /**
+     * Returns the total memory used.
+     * @param unit Return measurement unit. Support GB, MB or kB
+     * @returns Memory value total
+     */
+    function getMemoryTotal(unit: string): number;
+    /**
+     * Returns the total memory used.
+     * @param unit Return measurement unit. Support GB, MB or kB
+     * @returns Memory value total truncated
+     */
+    function getMemoryTotalTrunc(unit: string): number;
+    /**
+     * Returns the total memory used.
+     * @param unit Return measurement unit. Support GB, MB or kB
+     * @returns Memory value total fixed
+     */
+    function getMemoryTotalFixed(unit: string, value: number): number;
+}
+
+export namespace RegisterMonitoring {
+    /**
+     * Writes the measurement to a file.
+     * @param file Recording file
+     * @param unit Return measurement unit. Support GB, MB or kB
+     */
+    function registerMemoryUsage(file: string, unit: string): void;
+    /**
+     * Writes the measurement to a file.
+     * @param file Recording file
+     * @param unit Return measurement unit. Support GB, MB or kB
+     */
+    function registerMemoryUsageTrunc(file: string, unit: string): void;
+    /**
+     * Writes the measurement to a file.
+     * @param file Recording file
+     * @param unit Return measurement unit. Support GB, MB or kB
+     */
+    function registerMemoryUsageFixed(file: string, unit: string): void;
+}
--- types/memmonit/memmonit-tests.ts
+++ types/memmonit/memmonit-tests.ts
@@ -1,29 +1,32 @@
 import memmonit = require('memmonit');
 
-const memoryUsage = memmonit.getMemoryUsage("GB");
+const memoryMonitoring = memmonit.MemoryMonitoring;
+const registerMonitoring = memmonit.RegisterMonitoring;
+
+const memoryUsage = memoryMonitoring.getMemoryUsage("GB");
 console.log(memoryUsage);    // 2.015354681
 
-const memoryUsageTrunc = memmonit.memoryUsageTrunc("GB");
+const memoryUsageTrunc = memoryMonitoring.memoryUsageTrunc("GB");
 
 console.log(memoryUsageTrunc);   // 2
 
-const memoryUsageFixed = memmonit.getMemoryUsageFixed("GB", 2);
+const memoryUsageFixed = memoryMonitoring.getMemoryUsageFixed("GB", 2);
 console.log(memoryUsageFixed);   // 2.02
 
-const memoryTotal = memmonit.getMemoryTotal("GB");
+const memoryTotal = memoryMonitoring.getMemoryTotal("GB");
 
 console.log(memoryTotal);    // 15.364312
 
-const memoryTotalTrunc = memmonit.getMemoryTotalTrunc("GB");
+const memoryTotalTrunc = memoryMonitoring.getMemoryTotalTrunc("GB");
 
 console.log(memoryTotalTrunc);   // 15
 
-const memoryTotalFixed = memmonit.getMemoryTotalFixed("GB", 2);
+const memoryTotalFixed = memoryMonitoring.getMemoryTotalFixed("GB", 2);
 
 console.log(memoryTotalFixed);
 
-memmonit.registerMemoryUsage("./test.txt", "GB");
+registerMonitoring.registerMemoryUsage("./test.txt", "GB");
 
-memmonit.registerMemoryUsageTrunc("./test.txt", "GB");
+registerMonitoring.registerMemoryUsageTrunc("./test.txt", "GB");
 
-memmonit.registerMemoryUsageFixed("./test.txt", "GB");
+registerMonitoring.registerMemoryUsageFixed("./test.txt", "GB");
