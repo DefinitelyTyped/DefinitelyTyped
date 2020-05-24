1989dfacb14629ca7dcb44729111b3b45ffa622e
David D <comxd@users.noreply.github.com>
2020-05-24 20:10:46 +0200

    dt-header fix

--- types/muibox/index.d.ts
+++ types/muibox/index.d.ts
@@ -1,6 +1,7 @@
-// Type definitions for muibox 1.3.0
+// Type definitions for muibox 1.3
 // Project: https://github.com/chunkai1312/muibox
 // Definitions by: Diego Mijelshon <https://github.com/diegose>
+// Definitions by: David DIVERRES <https://github.com/comxd>
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
 
 import { ReactNode } from "react";
