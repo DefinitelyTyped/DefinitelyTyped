df94e0c753fe84ef39591f2cf6a802892a20d585
Todd Dukart <tdukart@mechanicalninja.com>
2020-05-25 15:44:50 -0400

    Add browser-extname

---
+++ types/browser-extname/index.d.ts
@@ -0,0 +1,6 @@
+// Type definitions for browser-extname 1.0
+// Project: https://github.com/pcr910303/extname
+// Definitions by: Todd Dukart <https://github.com/tdukart>
+// Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
+
+export default function browser_extname(path: string): string;
