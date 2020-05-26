a71b79b432288364879fbc07aa850c57a8fb9690
Ryan Blackman <rblackman@mtd.org>
2020-05-26 10:16:26 -0500

    Add type definitions for levenshtein-edit-distance

---
+++ types/levenshtein-edit-distance/index.d.ts
@@ -0,0 +1,6 @@
+// Type definitions for levenshtein-edit-distance 2.0
+// Project: https://words.github.io/levenshtein-edit-distance/
+// Definitions by: Ryan Blackman <https://github.com/me>
+// Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
+
+export function levenshtein(value: string, other: string, insensitive?: boolean): number;
