a240b5a71fee4e700b5dcbf6d5173357983f66f6
ewan-m <ewanmorrrison@gmail.com>
2020-05-28 08:55:41 +0100

    Change tests to match example usage from svg2ttf readme

--- types/svg2ttf/svg2ttf-tests.ts
+++ types/svg2ttf/svg2ttf-tests.ts
@@ -1,22 +1,10 @@
-import  svg2ttf = require('svg2ttf');
+/// <reference types="node" />
+import fs = require('fs');
+import svg2ttf = require('svg2ttf');
 
-const dummySvgFontString = `
-<?xml version="1.0" standalone="no"?>
-<!DOCTYPE svg PUBLIC "-//W3C//DTD SVG 1.1//EN" "http://www.w3.org/Graphics/SVG/1.1/DTD/svg11.dtd">
-<svg xmlns="http://www.w3.org/2000/svg">
-    <defs>
-        <font id="test" horiz-adv-x="200" >
-        <font-face font-family="test"
-            units-per-em="1000"
-            ascent="800"
-            descent="-200"
-            alphabetic="0"
-        />
-        <glyph unicode="." glyph-name="fullstop" horiz-adv-x="300" d="M0,0 L10,10 L20,10"/>
-        </font>
-    </defs>
-</svg>`;
+const ttf = svg2ttf(fs.readFileSync('myfont.svg', 'utf8'), {});
 
-svg2ttf(dummySvgFontString); // $ExpectType MicroBuffer
+ttf; // $ExpectType MicroBuffer
+ttf.buffer; // $ExpectType Uint8Array
 
-svg2ttf(dummySvgFontString).buffer; // $ExpectType Uint8Array
+fs.writeFileSync('myfont.ttf', new Buffer(ttf.buffer));
