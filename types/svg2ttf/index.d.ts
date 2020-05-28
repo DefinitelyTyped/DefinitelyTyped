aad0b5458b6913359d4eb7423839db086e054afb
ewan-m <ewanmorrrison@gmail.com>
2020-05-28 09:52:22 +0100

    Disable the linter for the jsdoc comments

--- types/svg2ttf/index.d.ts
+++ types/svg2ttf/index.d.ts
@@ -3,6 +3,8 @@
 // Definitions by: Ewan Morrison <https://github.com/ewan-m>
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
 
+// tslint:disable:no-redundant-jsdoc-2
+
 /**
  * Converts SVG fonts to TTF format
  */
