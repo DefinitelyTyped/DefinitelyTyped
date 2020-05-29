c26311404b4a1f361d32cb58e064b49fc620b0f5
Alister Hatt <ahatt@ozxcorp.com>
2020-05-29 12:09:48 +1000

    IotData client definition for aws-greengrass-core-sdk Javascript library.

---
+++ types/aws-greengrass-core-sdk/index.d.ts
@@ -0,0 +1,27 @@
+// Type definitions for aws-greengrass-core-sdk 1.6
+// Project: https://github.com/aws/aws-greengrass-core-sdk-js#readme
+// Definitions by: Alister Hatt <https://github.com/ozxcorp>
+// Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
+
+/// <reference types="node" />
+
+export interface PublishParams {
+    topic: string;
+    payload: Buffer | Blob | string | ArrayLike<any>;
+    queueFullPolicy?: 'AllOrError' | 'BestEffort';
+}
+
+export interface IotCallbackData {
+    payload: Buffer | Blob | string | ArrayLike<any>;
+}
+
+export type IotCallback = (error: Error | null, data: IotCallbackData | null) => void;
+
+export interface IotDataClient {
+    publish(params: PublishParams, callback: IotCallback): void;
+}
+
+export class IotData implements IotDataClient {
+    constructor();
+    publish(params: PublishParams, callback: IotCallback): void;
+}
