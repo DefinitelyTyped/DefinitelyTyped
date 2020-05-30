110dcb4cd853891c1c7cd5dd1b4bdbde3ba77516
Ryan Ling <ryan@outlook.com.au>
2020-05-30 20:59:43 +1000

    concurrently: Revert declaration changes
    https://github.com/DefinitelyTyped/DefinitelyTyped/issues/44317#issuecomment-635019190

--- types/concurrently/index.d.ts
+++ types/concurrently/index.d.ts
@@ -7,52 +7,49 @@
 
 /// <reference types="node" />
 
-declare module 'concurrently' {
-    namespace concurrently {
-        interface CommandObj {
-            command: string;
-            env?: NodeJS.ProcessEnv;
-            name?: string;
-            prefixColor?: string;
-        }
-
-        interface Options {
-            /** the default input target when reading from `inputStream`. Default: `0`. */
-            defaultInputTarget?: number;
-            /** a Readable stream to read the input from, eg `process.stdin` */
-            inputStream?: NodeJS.ReadableStream;
-            /** an array of exiting conditions that will cause a process to kill others. Can contain any of success or failure. */
-            killOthers?: Array<'success' | 'failure'>;
-            /**
-             * how many processes should run at once
-             * @default 0
-             */
-            maxProcesses?: number;
-            /**  a Writable stream to write logs to. Default: `process.stdout` */
-            outputStream?: NodeJS.WritableStream;
-            /**
-             * the prefix type to use when logging processes output.
-             */
-            prefix?: 'index' | 'pid' | 'time' | 'command' | 'name' | 'none' | string;
-            /** how many characters to show when prefixing with `command`. Default: `10` */
-            prefixLength?: number;
-            /** whether raw mode should be used, meaning strictly process output will be logged, without any prefixes, colouring or extra stuff. */
-            raw?: boolean;
-            /** the condition to consider the run was successful. */
-            successCondition?: 'first' | 'last';
-            /** how many attempts to restart a process that dies will be made. Default: `0` */
-            restartTries?: number;
-            /** how many milliseconds to wait between process restarts. Default: 0 */
-            restartDelay?: number;
-            /** a date-fns format to use when prefixing with time. Default: `yyyy-MM-dd HH:mm:ss.ZZZ` */
-            timestampFormat?: string;
-        }
+declare namespace concurrently {
+    interface CommandObj {
+        command: string;
+        env?: NodeJS.ProcessEnv;
+        name?: string;
+        prefixColor?: string;
     }
-
-    function concurrently(
-        commands: Array<concurrently.CommandObj | string>,
-        options?: concurrently.Options,
-    ): Promise<null>;
-
-    export = concurrently;
+    interface Options {
+        /** the default input target when reading from `inputStream`. Default: `0`. */
+        defaultInputTarget?: number;
+        /** a Readable stream to read the input from, eg `process.stdin` */
+        inputStream?: NodeJS.ReadableStream;
+        /** an array of exiting conditions that will cause a process to kill others. Can contain any of success or failure. */
+        killOthers?: Array<'success' | 'failure'>;
+        /**
+         * how many processes should run at once
+         * @default 0
+         */
+        maxProcesses?: number;
+        /**  a Writable stream to write logs to. Default: `process.stdout` */
+        outputStream?: NodeJS.WritableStream;
+        /**
+         * the prefix type to use when logging processes output.
+         */
+        prefix?: 'index' | 'pid' | 'time' | 'command' | 'name' | 'none' | string;
+        /** how many characters to show when prefixing with `command`. Default: `10` */
+        prefixLength?: number;
+        /** whether raw mode should be used, meaning strictly process output will be logged, without any prefixes, colouring or extra stuff. */
+        raw?: boolean;
+        /** the condition to consider the run was successful. */
+        successCondition?: 'first' | 'last';
+        /** how many attempts to restart a process that dies will be made. Default: `0` */
+        restartTries?: number;
+        /** how many milliseconds to wait between process restarts. Default: 0 */
+        restartDelay?: number;
+        /** a date-fns format to use when prefixing with time. Default: `yyyy-MM-dd HH:mm:ss.ZZZ` */
+        timestampFormat?: string;
+    }
 }
+
+declare function concurrently(
+    commands: Array<concurrently.CommandObj | string>,
+    options?: concurrently.Options,
+): Promise<null>;
+
+export = concurrently;
