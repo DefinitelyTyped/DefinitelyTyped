cf490fcfa2a40f908813c69ec0de7019a1da5237
Andy Katz <katz12@illinois.edu>
2020-05-30 14:21:48 -0500

    implement types and add tests

--- types/react-relative-portal/index.d.ts
+++ types/react-relative-portal/index.d.ts
@@ -3,37 +3,21 @@
 // Definitions by: Andy Katz <https://github.com/katz12>
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
 
-/*~ If this module is a UMD module that exposes a global variable 'myLib' when
- *~ loaded outside a module loader environment, declare that global here.
- *~ Otherwise, delete this declaration.
- */
-export as namespace myLib;
+import * as React from 'react';
 
-/*~ If this module has methods, declare them as functions like so.
- */
-export function myMethod(a: string): string;
-export function myOtherMethod(a: number): number;
+declare class RelativePortal extends React.Component<RelativePortal.Props> {}
 
-/*~ You can declare types that are available via importing the module */
-export interface someType {
-    name: string;
-    length: number;
-    extras?: string[];
+declare namespace RelativePortal {
+    interface Props {
+        children: React.ReactNode;
+        className?: string;
+        component: string;
+        fullWidth?: boolean;
+        left?: number;
+        onOutClick?: () => void;
+        right?: number;
+        top?: number;
+    }
 }
 
-/*~ You can declare properties of the module using const, let, or var */
-export const myField: number;
-
-/*~ If there are types, properties, or methods inside dotted names
- *~ of the module, declare them inside a 'namespace'.
- */
-export namespace subProp {
-    /*~ For example, given this definition, someone could write:
-     *~   import { subProp } from 'yourModule';
-     *~   subProp.foo();
-     *~ or
-     *~   import * as yourMod from 'yourModule';
-     *~   yourMod.subProp.foo();
-     */
-    function foo(): void;
-}
+export = RelativePortal;
