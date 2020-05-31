077787d74a7b262f3712c751b6beebb21798c411
Enn Michael <enntheprogrammer@gmail.com>
2020-05-31 20:35:04 +0200

    Added stuff that was changed, but forgot to commit

--- types/streamsaver/index.d.ts
+++ types/streamsaver/index.d.ts
@@ -1,6 +1,8 @@
-// Type definitions for streamsaver 2.0.0
+// Type definitions for streamsaver 2.0
 // Project: https://github.com/jimmywarting/StreamSaver.js
 // Definitions by: Enn Michael <https://github.com/ennmichael/>
+// Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
+// Minimum TypeScript Version: 3.2
 
 export as namespace streamSaver;
 
@@ -9,61 +11,61 @@ export as namespace streamSaver;
  * @param filename File name for the download.
  * @param options Additional options.
  */
-export function createWriteStream<I = any, O = any>(
-    filename: string, options?: CreateWriteStreamOptions<I, O>): WritableStream;
+export function createWriteStream(
+    filename: string, options?: CreateWriteStreamOptions): WritableStream;
 
 export interface CreateWriteStreamOptions<I = any, O = any> {
     /**
      * Indicates the size of the streamed data and allows the browser to show progress while downloading.
      */
-    size?: number
+    size?: number;
     /**
      * URL to serve the stream from. This is the URL that the browser is going to request from the service worker.
      * You might need to provide this if you're using a custom service worker.
      */
-    pathname?: string
-    writableStrategy?: QueuingStrategy<I>
-    readableStrategy?: QueuingStrategy<O>
+    pathname?: string;
+    writableStrategy?: QueuingStrategy<I>;
+    readableStrategy?: QueuingStrategy<O>;
 }
 
 export interface Version {
-    full: string
-    major: number
-    minor: number
-    dot: number
+    full: string;
+    major: number;
+    minor: number;
+    dot: number;
 }
 
 /**
  * StreamSaver.js library version.
  */
-export const version: Version
+export const version: Version;
 
-export var supported: boolean
+export let supported: boolean;
 
 /**
  * URL to the man-in-the-middle page. By default, this is
  * https://jimmywarting.github.io/StreamSaver.js/mitm.html?version=2.0.0, but you can also serve this
  * file from your own server, or even provide a custom MITM file with a custom service worker.
  */
-export var mitm: string
+export let mitm: string;
 
 /**
  * The WritableStream class that will be used by StreamSaver.js. By default, this is window.WritableStream,
  * but you can also provide a ponyfill or a polyfill if your target browser does not support it natively.
  */
-export var WritableStream: {
+export let WritableStream: {
     prototype: WritableStream,
     new<W = any>(underlyingSink?: UnderlyingSink<W>, strategy?: QueuingStrategy<W>): WritableStream<W>;
-}
+};
 
 /**
  * The TransformStream class that will be used by StreamSaver.js. By default, this is the native TransformStream,
  * but you can also provide a ponyfill or a polyfill if your target browser does not support it natively.
  */
-export var TransformStream: {
+export let TransformStream: {
     prototype: TransformStream,
     new<I = any, O = any>(
         transformer: Transformer<I, O>,
         writableStrategy: QueuingStrategy<I>,
         readableStrategy: QueuingStrategy<O>): TransformStream<I, O>,
-}
+};
