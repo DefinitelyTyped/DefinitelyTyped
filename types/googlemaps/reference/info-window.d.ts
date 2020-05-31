9f1384e1d92093079f44435fc0900f914b0ca151
Dmitry Demensky <dmitry@demensky.com>
2020-05-31 21:27:28 +0300

    [googlemaps] JSDoc InfoWindowOptions

--- types/googlemaps/reference/info-window.d.ts
+++ types/googlemaps/reference/info-window.d.ts
@@ -35,44 +35,62 @@ declare namespace google.maps {
         setZIndex(zIndex: number): void;
     }
 
+    /**
+     * {@link InfoWindowOptions} object used to define the properties that can be set on a {@link InfoWindow}.
+     * @see {@link https://developers.google.com/maps/documentation/javascript/reference/info-window#InfoWindowOptions Maps JavaScript API}
+     * @see {@link InfoWindow#setOptions}
+     */
     interface InfoWindowOptions {
         /**
-         * Content to display in the InfoWindow. This can be an HTML element, a
-         * plain-text string, or a string containing HTML. The InfoWindow will be
-         * sized according to the content. To set an explicit size for the content,
-         * set content to be a HTML element with that size.
+         * Content to display in the {@link InfoWindow}. This can be an HTML element, a plain-text string, or a string
+         * containing HTML. The InfoWindow will be sized according to the content. To set an explicit size for the
+         * content, set content to be a HTML element with that size.
+         * @see {@link InfoWindow#getContent}
+         * @see {@link InfoWindow#setContent}
          */
         content?: string | Node;
+
         /**
-         * Disable auto-pan on open. By default, the info window will pan the map so
-         * that it is fully visible when it opens.
+         * Disable auto-pan on open. By default, the {@link InfoWindow} will pan the map so that it is fully visible
+         * when it opens.
+         * @see {@link https://developers.google.com/maps/documetntation/javascript/reference/info-window#InfoWindowOptions.disableAutoPan Maps JavaScript API}
          */
         disableAutoPan?: boolean;
+
         /**
-         * Maximum width of the infowindow, regardless of content's width.
-         * This value is only considered if it is set before a call to open.
-         * To change the maximum width when changing content, call close,
-         * setOptions, and then open.
+         * Maximum width of the {@link InfoWindow}, regardless of content's width. This value is only considered if it
+         * is set before a call to open. To change the maximum width when changing content, call close,
+         * {@link InfoWindow#setOptions setOptions}, and then open.
+         * @see {@link https://developers.google.com/maps/documentation/javascript/reference/info-window#InfoWindowOptions.maxWidth Maps JavaScript API}
          */
         maxWidth?: number;
+
         /**
-         * The offset, in pixels, of the tip of the info window from the point on
-         * the map at whose geographical coordinates the info window is anchored. If
-         * an InfoWindow is opened with an anchor, the pixelOffset will be
-         * calculated from the anchor's anchorPoint property.
+         * The offset, in pixels, of the tip of the info window from the point on the map at whose geographical
+         * coordinates the info window is anchored. If an {@link InfoWindow} is opened with an anchor, the
+         * {@link pixelOffset} will be calculated from the anchor's {@link MarkerOptions#anchorPoint anchorPoint}
+         * property.
+         * @see {@link https://developers.google.com/maps/documentation/javascript/reference/info-window#InfoWindowOptions.pixelOffset Maps JavaScript API}
          */
         pixelOffset?: Size;
+
         /**
-         * The LatLng at which to display this InfoWindow. If the InfoWindow is
-         * opened with an anchor, the anchor's position will be used instead.
+         * The {@link LatLng} at which to display this {@link InfoWindow}. If the {@link InfoWindow} is opened with an
+         * anchor, the anchor's position will be used instead.
+         * @see {@link https://developers.google.com/maps/documentation/javascript/reference/info-window#InfoWindowOptions.position Maps JavaScript API}
+         * @see {@link InfoWindow#getPosition}
+         * @see {@link InfoWindow#setPosition}
          */
         position?: LatLng | LatLngLiteral;
+
         /**
-         * All InfoWindows are displayed on the map in order of their zIndex,
-         * with higher values displaying in front of InfoWindows with lower values.
-         * By default, InfoWindows are displayed according to their latitude,
-         * with InfoWindows of lower latitudes appearing in front of InfoWindows at
-         * higher latitudes. InfoWindows are always displayed in front of markers.
+         * All {@link InfoWindow} are displayed on the map in order of their {@link zIndex}, with higher values
+         * displaying in front of {@link InfoWindow} with lower values. By default, {@link InfoWindow} are displayed
+         * according to their latitude, with {@link InfoWindow} of lower latitudes appearing in front of
+         * {@link InfoWindow} at higher latitudes. {@link InfoWindow} are always displayed in front of markers.
+         * @see {@link https://developers.google.com/maps/documentation/javascript/reference/info-window#InfoWindowOptions.zIndex Maps JavaScript API}
+         * @see {@link InfoWindow#getZIndex}
+         * @see {@link InfoWindow#setZIndex}
          */
         zIndex?: number;
     }
