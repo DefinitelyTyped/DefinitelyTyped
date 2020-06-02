0f0665e71be9f33264c0b4b69c47202ed4f9dec4
Randolf C <34705014+jun-sheaf@users.noreply.github.com>
2020-06-02 23:02:11 +0200

    Updated Version

--- types/howler/index.d.ts
+++ types/howler/index.d.ts
@@ -1,4 +1,4 @@
-// Type definitions for howler.js v2.1.1
+// Type definitions for howler.js v2.2.0
 // Project: https://github.com/goldfire/howler.js
 // Definitions by: Pedro Casaubon <https://github.com/xperiments>
 //                 Alexander Leon <https://github.com/alien35>
