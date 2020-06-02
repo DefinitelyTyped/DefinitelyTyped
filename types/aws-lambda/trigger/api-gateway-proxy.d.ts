87d3c5f12e15f7c80b979253995070a638b19521
Sachin Shekhar <noreply.git@sachinshekhar.com>
2020-06-02 05:45:20 +0530

    [aws-lambda] prettier fix

--- types/aws-lambda/trigger/api-gateway-proxy.d.ts
+++ types/aws-lambda/trigger/api-gateway-proxy.d.ts
@@ -105,33 +105,34 @@ export interface APIGatewayProxyEventV2 {
     headers: { [name: string]: string };
     queryStringParameters: { [name: string]: string };
     requestContext: {
-      accountId: string;
-      apiId: string;
-      authorizer: { jwt: {
-          claims: { [name: string]: string };
-          scopes: string[]
-          }
-      };
-      domainName: string;
-      domainPrefix: string;
-      http: {
-        method: string;
-        path: string;
-        protocol: string;
-        sourceIp: string;
-        userAgent: string;
-      };
-      requestId: string;
-      routeKey: string;
-      stage: string;
-      time: string;
-      timeEpoch: number;
+        accountId: string;
+        apiId: string;
+        authorizer: {
+            jwt: {
+                claims: { [name: string]: string };
+                scopes: string[];
+            };
+        };
+        domainName: string;
+        domainPrefix: string;
+        http: {
+            method: string;
+            path: string;
+            protocol: string;
+            sourceIp: string;
+            userAgent: string;
+        };
+        requestId: string;
+        routeKey: string;
+        stage: string;
+        time: string;
+        timeEpoch: number;
     };
     body: string;
     pathParameters: { [name: string]: string };
     isBase64Encoded: boolean;
     stageVariables: { [name: string]: string };
-    }
+}
 
 /**
  * Works with HTTP API integration Payload Format version 2.0
