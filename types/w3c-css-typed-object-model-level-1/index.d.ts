7fd236736e7452cb201de5e4ad897879ff6fbdc2
Ron Buckton <ron.buckton@microsoft.com>
2020-06-02 17:57:39 -0700

    Update types/w3c-css-typed-object-model-level-1/index.d.ts
    Co-authored-by: Piotr Błażejewicz (Peter Blazejewicz) <peterblazejewicz@users.noreply.github.com>

--- types/w3c-css-typed-object-model-level-1/index.d.ts
+++ types/w3c-css-typed-object-model-level-1/index.d.ts
@@ -1,6 +1,5 @@
 // Type definitions for non-npm package css-typed-object-model-level-1 20180410.0
 // Project: https://www.w3.org/TR/css-typed-om-1/
-// Definitions by: Dmitry Guketlev <https://github.com/yavanosta>
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
 // Minimum TypeScript Version: 3.9
 
