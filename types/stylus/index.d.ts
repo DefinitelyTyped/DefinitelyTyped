2aeec0e3c2b787f1d31eab3f4aaad931030e41e4
Luis Crespo <luis.crespo@grammarly.com>
2020-06-03 10:29:15 -0700

    Fix UrlFunction interface
    The options for stylus.url() are optional.

--- types/stylus/index.d.ts
+++ types/stylus/index.d.ts
@@ -605,7 +605,7 @@ declare namespace Stylus {
     }
 
     export interface UrlFunction {
-        (options: UrlOptions): LiteralFunction;
+        (options?: UrlOptions): LiteralFunction;
 
         mimes: {
             ".gif": string;
@@ -1421,7 +1421,7 @@ declare namespace Stylus {
     export type RenderCallback = (err: Error, css: string, js: string) => void;
 
     export interface UrlOptions {
-        limit?: string;
+        limit?: number;
         paths?: string[];
     }
 
