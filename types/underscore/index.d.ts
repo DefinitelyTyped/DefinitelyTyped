f1ab657b9863af66f73f1c39997463c611a412d2
Mike Ness <mness@clearcompany.com>
2020-06-04 22:49:07 -0700

    Moving the minimum supported TS version to 3.0 so unknown can be used instead of any since that's very close to being the minimum supported version at this time.

--- types/underscore/index.d.ts
+++ types/underscore/index.d.ts
@@ -10,7 +10,7 @@
 //                 Piotr Błażejewicz <https://github.com/peterblazejewicz>
 //                 Michael Ness <https://github.com/reubenrybnik>
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
-// TypeScript Version: 2.8
+// TypeScript Version: 3.0
 
 declare var _: _.UnderscoreStatic;
 export = _;
