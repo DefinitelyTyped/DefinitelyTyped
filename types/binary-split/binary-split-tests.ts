d72fc86bfcbb27a5786470f4b66138c34af8f339
Krisztian Balla <k.balla@gmx.at>
2020-06-07 21:22:22 +0200

    Fix test

--- types/binary-split/binary-split-tests.ts
+++ types/binary-split/binary-split-tests.ts
@@ -6,12 +6,10 @@ splitter.on('data', (chunk: Buffer) => {
     output.push(chunk);
 });
 splitter.on('end', () => {
-    // if (output.length !== 2 && output[0].toString('utf8') !== 'A' && output[1].toString('utf8') !== 'B') {
+    if (output.length !== 2 || output[0].toString('utf8') !== 'A' || output[1].toString('utf8') !== 'B') {
         throw new Error('Test failed');
-    // }
+    }
 });
-
-throw new Error('Test failed');
 
 const input = Buffer.from('A*snip*B');
 splitter.write(input);
