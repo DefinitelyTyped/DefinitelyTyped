779b87c28428f484227468c89d5c54207d682fd1
Krisztian Balla <k.balla@gmx.at>
2020-06-07 20:07:48 +0200

    Make test fail

--- types/binary-split/binary-split-tests.ts
+++ types/binary-split/binary-split-tests.ts
@@ -6,9 +6,9 @@ splitter.on('data', (chunk: Buffer) => {
     output.push(chunk);
 });
 splitter.on('end', () => {
-    if (output.length !== 2 && output[0].toString('utf8') !== 'A' && output[1].toString('utf8') !== 'B') {
+    // if (output.length !== 2 && output[0].toString('utf8') !== 'A' && output[1].toString('utf8') !== 'B') {
         throw new Error('Test failed');
-    }
+    // }
 });
 
 const input = Buffer.from('A*snip*B');
