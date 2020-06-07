9fcec07301fcaaf8bdc1b596d78de6ae1a33ee86
Krisztian Balla <k.balla@gmx.at>
2020-06-07 20:02:28 +0200

    Add types for binary-split

---
+++ types/binary-split/index.d.ts
@@ -0,0 +1,18 @@
+// Type definitions for binary-split 1.0
+// Project: https://github.com/maxogden/binary-split#readme
+// Definitions by: Krisztián Balla <https://github.com/krisztianb>
+// Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
+
+/// <reference types="node" />
+
+import { Transform } from 'stream';
+
+export = binary_split;
+
+/**
+ * Split streams of binary data.
+ * @param splitOn The matcher for the splitting points in the stream. Default: os.EOL
+ */
+declare function binary_split(
+    splitOn: number[] | BigInt[] | Buffer | Uint8Array | ArrayBuffer | SharedArrayBuffer | string | object,
+): Transform;
