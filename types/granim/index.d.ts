bf5af8e7779e15bab2e44dd98b9973058ed3daa6
Raiper34 <raipergm34@gmail.com>
2020-06-08 12:48:47 +0200

    Add granim typings

---
+++ types/granim/index.d.ts
@@ -0,0 +1,49 @@
+export declare class Granim {
+  constructor(options: Options);
+  play(): void;
+  pause(): void;
+  clear(): void;
+  changeState(stateName: string): void;
+  changeDirection(directionName: string): void;
+  changeBlendingMode(blendingModeName: string): void;
+  destroy(): void;
+}
+
+export interface OnGradientChangeObject {
+  activeState: string;
+  colorsFrom: [string, string];
+  colorsTo: [string, string];
+  isLooping: boolean;
+}
+
+export interface Options {
+  element: string | HTMLCanvasElement;
+  name?: string;
+  elToSetClassOn?: string;
+  direction?: 'diagonal' | 'left-right' | 'top-bottom' | 'radial' | 'custom';
+  customDirection?: {x0: string; y0: string; x1: string; y1: string};
+  isPausedWhenNotInView?: boolean;
+  scrollDebounceThreshold?: number;
+  stateTransitionSpeed?: number;
+  defaultStateName?: string;
+  states: {[stateName: string]: State};
+  image?: Image;
+  onStart?: () => void;
+  onGradientChange?: (colorDetails: OnGradientChangeObject) => void;
+  onEnd?: () => void;
+}
+
+export interface State {
+  gradients: string[][] | {color: string, pos: number}[][];
+  transitionSpeed?: number;
+  loop?: boolean;
+}
+
+type stretchMode = 'none' | 'stretch' | 'stretch-if-smaller' | 'stretch-if-bigger';
+
+export interface Image {
+  source: string;
+  position?: ['left' | 'center' | 'right', 'top' | 'center' | 'bottom'];
+  stretchMode?: [stretchMode, stretchMode];
+  blendingMode?: string;
+}
