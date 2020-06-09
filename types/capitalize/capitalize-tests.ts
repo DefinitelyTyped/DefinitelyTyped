a3f7ba0875f662c54bf65d65ffb5b7eb06798000
Peter Blazejewicz <peterblazejewicz@users.noreply.github.com>
2020-06-09 22:29:42 +0200

    update(capitalize): version 2 bump
    This updates definition to v2. As the changes are completely
    non-breaking (optional params added) the version is just updated without
    creating v1 at the same time.
    
    - optional `preserve` parameter added
    - version bump
    - maintainer added
    - minor changes to DT trivia (TS version no longer required, TSLint
    exclusion added inline for clariy, instead of in-file).
    
    https://github.com/grncdr/js-capitalize/compare/v1.0.0...v2.0.2#diff-168726dbe96b3ce427e7fedce31bb0bc
    
    Thanks!

--- types/capitalize/capitalize-tests.ts
+++ types/capitalize/capitalize-tests.ts
@@ -1,12 +1,14 @@
-import * as capitalize from "capitalize";
-import { words } from "capitalize";
+import * as capitalize from 'capitalize';
+// tslint:disable-next-line:no-duplicate-imports
+import { words } from 'capitalize';
 
-capitalize("united states");
+capitalize('united states');
+capitalize('uniTed staTes', true); // $ExpectType string
 
-capitalize.words("united states");
+capitalize.words('united states');
+capitalize.words('uniTed staTes', true); // $ExpectType string
 
 capitalize.words('hello-cañapolísas');
-
 capitalize.words("it's a nice day");
 
-words("united states");
+words('united states');
