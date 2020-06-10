72f346aae154eb0a4c6116975662e54be2b3c151
flakronkadriu <flakron.kadriu@shijigroup.com>
2020-06-10 12:42:12 +0200

    Version number update

--- types/react-datepicker/index.d.ts
+++ types/react-datepicker/index.d.ts
@@ -1,4 +1,4 @@
-// Type definitions for react-datepicker 2.11
+// Type definitions for react-datepicker 2.12
 // Project: https://github.com/Hacker0x01/react-datepicker
 // Definitions by: Rajab Shakirov <https://github.com/radziksh>,
 //                 Andrey Balokha <https://github.com/andrewBalekha>,
