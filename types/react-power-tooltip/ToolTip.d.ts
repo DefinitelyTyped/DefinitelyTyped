e08ea5a5a4cff704295c077c3e4898013bf77937
Karthik K <karthiknetworklink@gmail.com>
2020-06-11 17:46:20 +0530

    fix: some more errors

--- types/react-power-tooltip/ToolTip.d.ts
+++ types/react-power-tooltip/ToolTip.d.ts
@@ -5,22 +5,39 @@ declare class Tooltip extends React.Component<TooltipProps> {}
 export default Tooltip;
 
 export interface TooltipProps {
-    lineSeparated: boolean | string,
-    position: string,
-    hoverBackground: string,
-    backgroundColor: string,
-    arrowAlign: string,
-    moveDown: string,
-    moveRight: string,
-    moveLeft: string,
-    moveUp: string,
-    textAlign: string,
-    fontFamily: string,
-    fontWeight: string,
-    fontSize: string,
-    color: string,
-    animation: string,
-    zIndex: string,
-    show: boolean,
-    flat: boolean
+    lineSeparated?: boolean | string;
+    
+    position?: string;
+    
+    hoverBackground?: string;
+    
+    backgroundColor?: string;
+    
+    arrowAlign?: string;
+    
+    moveDown?: string;
+    
+    moveRight?: string;
+    
+    moveLeft?: string;
+    
+    moveUp?: string;
+    
+    textAlign?: string;
+    
+    fontFamily?: string;
+    
+    fontWeight?: string;
+    
+    fontSize?: string;
+    
+    color?: string;
+    
+    animation?: string;
+    
+    zIndex?: string;
+    
+    show: boolean;
+    
+    flat?: boolean;
 }
--- types/react-power-tooltip/react-power-tooltip-tests.tsx
+++ types/react-power-tooltip/react-power-tooltip-tests.tsx
@@ -27,5 +27,3 @@ class ToolTipTest extends React.Component {
         );
     }
 }
-
-
