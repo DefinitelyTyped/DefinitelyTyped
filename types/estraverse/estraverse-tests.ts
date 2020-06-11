9bc11af6dea5518df203ff45b17f1bf6f550f9d7
Jason Kwok <JasonHK@users.noreply.github.com>
2020-06-12 01:40:41 +0800

    [estraverse] updated test cases

--- types/estraverse/estraverse-tests.ts
+++ types/estraverse/estraverse-tests.ts
@@ -1,73 +1,70 @@
-import * as estraverse from 'estraverse';
-import * as estree from 'estree';
+import estraverse from 'estraverse';
+import estree from 'estree';
 
-let ast: estree.Node = {
-    "type": "Program",
-    "body": [
-        {
-            "type": "VariableDeclaration",
-            "declarations": [
-                {
-                    "type": "VariableDeclarator",
-                    "id": {
-                        "type": "Identifier",
-                        "name": "answer"
-                    },
-                    "init": {
-                        "type": "BinaryExpression",
-                        "operator": "*",
-                        "left": {
-                            "type": "Literal",
-                            "value": 6,
-                            "raw": "6"
-                        },
-                        "right": {
-                            "type": "Literal",
-                            "value": 7,
-                            "raw": "7"
-                        }
-                    }
-                }
-            ],
-            "kind": "var"
-        }
-    ],
-    "sourceType": "script"
-};
+declare const NODE: estree.Node;
+declare const NODE_VISITOR: estraverse.Visitor;
 
-estraverse.traverse(ast, {
-    enter(node: estree.Node, parentNode: estree.Node | null) {
-        if (node.type === 'Identifier') {
-            return estraverse.VisitorOption.Skip;
-        }
-    },
-    leave: (node: estree.Node, parentNode: estree.Node | null) => {},
-    fallback: 'iteration',
-    keys: {
-        TestExpression: ['argument']
-    }
-});
+declare const FLAG: estraverse.VisitorOption;
 
-estraverse.replace(ast, {
-    enter: (node: estree.Node, parentNode: estree.Node | null) => {
-        return node;
-    },
-    leave: (node: estree.Node, parentNode: estree.Node | null) => {
-        return node;
-    }
-});
-
+// Test cases for `Visitor`
 {
     const visitor: estraverse.Visitor = {
         enter(node, parent) {
-            this.break(); // $ExpectType void
-            this.current(); // $ExpectType Node
-            this.notify(estraverse.VisitorOption.Break); // $ExpectType void
-            this.parents(); // $ExpectType Node[]
-            this.path(); // $ExpectType (string | number)[] | null
-            this.remove(); // $ExpectType void
-            this.skip(); // $ExpectType void
-            this.type(); // $ExpectType string
+            // Test cases for `this`
+            {
+                // $ExpectType Controller
+                this;
+                this.break(); // $ExpectType void
+                this.current(); // $ExpectType Node
+                this.notify(FLAG); // $ExpectType void
+                this.parents(); // $ExpectType Node[]
+                this.path(); // $ExpectType (string | number)[] | null
+                this.remove(); // $ExpectType void
+                this.skip(); // $ExpectType void
+                this.type(); // $ExpectType string
+            }
         },
+        leave(node, parent) {
+            // Test cases for `this
+            {
+                // $ExpectType Controller
+                this;
+                this.break(); // $ExpectType void
+                this.current(); // $ExpectType Node
+                this.notify(FLAG); // $ExpectType void
+                this.parents(); // $ExpectType Node[]
+                this.path(); // $ExpectType (string | number)[] | null
+                this.remove(); // $ExpectType void
+                this.skip(); // $ExpectType void
+                this.type(); // $ExpectType string
+            }
+        },
+        fallback(node) {
+           // Test cases for `this
+            {
+                // $ExpectType Controller
+                this;
+                this.break(); // $ExpectType void
+                this.current(); // $ExpectType Node
+                this.notify(FLAG); // $ExpectType void
+                this.parents(); // $ExpectType Node[]
+                this.path(); // $ExpectType (string | number)[] | null
+                this.remove(); // $ExpectType void
+                this.skip(); // $ExpectType void
+                this.type(); // $ExpectType string
+            }
+
+            return [];
+        }
     };
+}
+
+// Test cases for `traverse`
+{
+    estraverse.traverse(NODE, NODE_VISITOR); // $ExpectType void
+}
+
+// Test cases for `replace`
+{
+    estraverse.replace(NODE, NODE_VISITOR); // $ExpectType Node
 }
