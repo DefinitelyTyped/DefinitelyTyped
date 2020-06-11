650281fe473c3766c51d392eb85c4d08d83c4a71
Karthik K <karthiknetworklink@gmail.com>
2020-06-11 18:09:11 +0530

    fix: errors

--- types/react-power-tooltip/react-power-tooltip-tests.tsx
+++ types/react-power-tooltip/react-power-tooltip-tests.tsx
@@ -3,18 +3,17 @@ import ToolTip from 'react-power-tooltip';
 
 // Tooltip test
 const ToolTipTest = () => {
-    const [state, setState] = useState(false);
+    const [visible, setVisible] = useState(false);
     return (
             <div>
-                <p id="text" onMouseEnter={()=>setState(true)} onMouseLeave={()=>setState(false)}>
+                <p id="text" onMouseEnter={() => setVisible(true)} onMouseLeave={() => setVisible(false)}>
                     This is a cool component
                 </p>
-                <ToolTip show={this.state.isTooltipActive} position="right center" arrowAlign="center">
+                <ToolTip show={visible} position="right center" arrowAlign="center">
                     <div>
                         <p>This is the content of the tooltip</p>
                     </div>
                 </ToolTip>
             </div>
         );
-
-}
+};
