0229e667645d95370dd757857e58d4d0734d437e
Benjin Dubishar <benjin.dubishar@gmail.com>
2020-06-11 14:52:45 -0700

    Bump minor version

--- types/xml-formatter/index.d.ts
+++ types/xml-formatter/index.d.ts
@@ -1,4 +1,4 @@
-// Type definitions for xml-formatter 1.1
+// Type definitions for xml-formatter 1.2
 // Project: https://github.com/chrisbottin/xml-formatter/
 // Definitions by: Joachim Holwech <https://github.com/holwech>
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
