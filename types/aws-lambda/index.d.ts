67401e93f5a4cc1a69ebe2decebb56b9a89ccc42
ivanmartos <ivan.martos@crowdhouse.ch>
2020-06-17 10:44:00 +0200

    removed unnecessary general codebuild types

--- types/aws-lambda/index.d.ts
+++ types/aws-lambda/index.d.ts
@@ -53,7 +53,6 @@ export * from "./trigger/cloudfront-response";
 export * from "./trigger/cloudwatch-events";
 export * from "./trigger/cloudwatch-logs";
 // TODO: export * from "./trigger/codecommit";
-export * from "./trigger/codebuild-cloudwatch";
 export * from "./trigger/codebuild-cloudwatch-state";
 export * from "./trigger/codepipeline";
 export * from "./trigger/codepipeline-cloudwatch";
--- types/aws-lambda/trigger/codebuild-cloudwatch.d.ts
+++
@@ -1,3 +0,0 @@
-import { CodeBuildCloudWatchStateEvent } from './codebuild-cloudwatch-state';
-
-export type CodeBuildCloudWatchEvent = CodeBuildCloudWatchStateEvent;
