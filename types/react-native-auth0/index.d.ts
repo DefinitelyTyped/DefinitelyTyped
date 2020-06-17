60935f54bbf7ba421ea72d4f7a497298ca46cf74
Conor Cussell <conor.cussell@babylonhealth.com>
2020-06-17 16:17:58 +0100

    Remove patch version from header

--- types/react-native-auth0/index.d.ts
+++ types/react-native-auth0/index.d.ts
@@ -1,4 +1,4 @@
-// Type definitions for react-native-auth0 2.5.0
+// Type definitions for react-native-auth0 2.5
 // Project: https://github.com/auth0/react-native-auth0
 // Definitions by: Andrea Ascari <https://github.com/ascariandrea>
 //                 Mark Nelissen <https://github.com/marknelissen>
