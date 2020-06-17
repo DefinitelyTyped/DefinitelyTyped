c8159bd0d46ab0a6bb8bd813e26f55531df8c813
morrys <lorenzo.digiacomo85@gmail.com>
2020-06-17 13:03:35 +0200

    added morrys as maintainer

--- types/relay-runtime/index.d.ts
+++ types/relay-runtime/index.d.ts
@@ -7,6 +7,7 @@
 //                 Stephen Pittman <https://github.com/Stephen2>
 //                 Martin Zlámal <https://github.com/mrtnzlml>
 //                 Christian Ivicevic <https://github.com/ChristianIvicevic>
+//                 Lorenzo Di Giacomo <https://github.com/morrys>
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
 // TypeScript Version: 3.0
 
