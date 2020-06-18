a9218ee660d16f0e512f509a0e918ac1d2f05de7
Mark Dreyer <dreyguy@gmail.com>
2020-06-18 04:38:22 -0500

    refactor: add my name to author list for notifications

--- types/readable-stream/index.d.ts
+++ types/readable-stream/index.d.ts
@@ -1,6 +1,7 @@
 // Type definitions for readable-stream 2.3
 // Project: https://github.com/nodejs/readable-stream
 // Definitions by: TeamworkGuy2 <https://github.com/TeamworkGuy2>
+//                   markdreyer <https://github.com/markdreyer>
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
 // TypeScript Version: 2.3
 
