2f616938b7603258395bdc25667ebe4f6e482b34
Mike Deverell <mike@onepeloton.com>
2020-06-17 23:24:09 -0400

    move 'into' tests to test/into-tests.ts

--- types/ramda/ramda-tests.ts
+++ types/ramda/ramda-tests.ts
@@ -3,23 +3,3 @@ import * as R from "ramda";
 /** R.__ */
 () => {
 };
-
-() => {
-    interface A {
-        a: number;
-    }
-
-    interface B {
-        b: number;
-    }
-
-    const As = [{ a: 1 }, { a: 2 }];
-    const AsToBs: (a: A[]) => B[] = R.map((a: A): B => ({
-        b: a.a,
-    }));
-
-    function test(): B[] {
-        return R.into<A, B>([], AsToBs, As);
-    }
-    test(); // => [{ b: 1 }, { b: 2 }]
-};
