e6b9cf980b00a904ed6bb5e3a7efb4e13384ca91
manuc66 <manuc66@gmail.com>
2020-06-18 23:41:42 +0200

    Add typing for react-cytoscapejs

---
+++ types/react-cytoscapejs/index.d.ts
@@ -0,0 +1,39 @@
+// Type definitions for react-cytoscapejs 4.0
+// Project: https://github.com/plotly/react-cytoscapejs
+// Definitions by:  Emmanuel COunasse <https://github.com/manuc66/>
+// Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
+// TypeScript Version: 2.8
+
+import cytoscape from 'cytoscape';
+import { Stylesheet, LayoutOptions, ElementDefinition } from 'cytoscape';
+import { Component, CSSProperties } from 'react';
+
+export interface CytoscapeComponentProps {
+    id?: string;
+    cy?: (cy: cytoscape.Core) => void;
+    style?: CSSProperties;
+    elements: ElementDefinition[];
+    layout?: LayoutOptions;
+    stylesheet?: Stylesheet | Stylesheet[] | string;
+    className?: string;
+    zoom?: number;
+    pan?: Position;
+    minZoom?: number;
+    maxZoom?: number;
+    zoomingEnabled?: boolean;
+    userZoomingEnabled?: boolean;
+    boxSelectionEnabled?: boolean;
+    autoungrabify?: boolean;
+    autounselectify?: boolean;
+}
+
+export default class CytoscapeComponent extends Component<CytoscapeComponentProps> {
+    static normalizeElements(
+        data:
+            | {
+                  nodes: ElementDefinition[];
+                  edges: ElementDefinition[];
+              }
+            | ElementDefinition[],
+    ): ElementDefinition[];
+}
