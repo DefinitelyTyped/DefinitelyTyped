69bf7f68d5a0951e191b01f0e57a0fc8a8a98c6d
Rick Wilson <me@richardawilson.com>
2020-06-20 00:54:04 -0400

    Added Types for react-compass

---
+++ types/react-compass/index.d.ts
@@ -0,0 +1,16 @@
+// Type definitions for react-compass 0.1
+// Project: https://github.com/virtyaluk/react-compass
+// Definitions by: Rick Wilson <https://github.com/rwilson504>
+// Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
+
+import * as React from "react";
+
+export interface ReactCompassProps {
+    direction: number;
+}
+
+export function directionName(dir: number): string;
+export function normalizeAngle(direction: number): number;
+
+export class ReactCompass extends React.Component<ReactCompassProps, any> {}
+export default ReactCompass;
