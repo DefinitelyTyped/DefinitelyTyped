82c020acde8d88fa0e5f98d5490126d8a0004d4e
Sean S. LeBlanc <sean.s.leblanc@gmail.com>
2020-06-20 16:31:56 -0400

    feat(dotenv-load): new definition

---
+++ types/dotenv-load/index.d.ts
@@ -0,0 +1,6 @@
+// Type definitions for dotenv-load 2.0
+// Project: https://github.com/formatlos/dotenv-load
+// Definitions by: Sean S. LeBlanc <https://github.com/seleb>
+// Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
+
+export default function(preloadEnv?: string): void;
