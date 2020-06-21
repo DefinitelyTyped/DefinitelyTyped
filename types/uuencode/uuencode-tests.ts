58b9a595e92116a6d27177f6c2beed2f44cc7a3a
Seth Niimi <seth@synaestheory.online>
2020-06-21 11:37:36 -0700

    add test for decode with buffer.

--- types/uuencode/uuencode-tests.ts
+++ types/uuencode/uuencode-tests.ts
@@ -1,5 +1,6 @@
 /// <reference types="node"/>
 import * as uuencode from 'uuencode';
 const encoded: string = uuencode.encode('$#%');
-const decoded: string = uuencode.decode('');
-const encodedBuffer: string = uuencode.encode(new Buffer(encoded))
+const decoded: string = uuencode.decode('$#%');
+const encodedBuffer: string = uuencode.encode(new Buffer('$#%'));
+const decodedBuffer: string = uuencode.decode(new Buffer('$#%'));
