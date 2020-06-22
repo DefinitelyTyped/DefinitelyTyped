65c419cb993bb5c2f8f7347cd22b93c6db8b1cbd
John Dengis <jadengis@gmail.com>
2020-06-21 23:49:00 -0400

    update version number in header

--- types/stream-json/index.d.ts
+++ types/stream-json/index.d.ts
@@ -1,4 +1,4 @@
-// Type definitions for stream-json 1.0
+// Type definitions for stream-json 1.5
 // Project: http://github.com/uhop/stream-json
 // Definitions by: Eugene Lazutkin <https://github.com/uhop>
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
