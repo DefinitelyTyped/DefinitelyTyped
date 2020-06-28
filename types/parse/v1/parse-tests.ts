8da219c593e185b059ded4e569e740489cbcf014
Mike Ness <miken7@hotmail.com>
2020-06-28 15:52:21 -0700

    Revert "Fixing a test for @types/parse that breaks with these changes."
    This reverts commit 1955fc1a00aa884e088c07a2d02dd7b3bc08c2fa.

--- types/parse/v1/parse-tests.ts
+++ types/parse/v1/parse-tests.ts
@@ -148,7 +148,7 @@ function test_query() {
     const testQuery = Parse.Query.or(query, query);
 }
 
-class TestCollection extends Parse.Collection<Parse.Object> {
+class TestCollection extends Parse.Collection<Object> {
 
     constructor(models?: Parse.Object[]) {
 
