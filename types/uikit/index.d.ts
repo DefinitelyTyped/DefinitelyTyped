467526a2af12862ef5fd139ab8c17724be3e21f8
Johanns Gregorian <io@jsg.io>
2020-06-29 12:32:16 -0700

    UIkit: Fixed header typo

--- types/uikit/index.d.ts
+++ types/uikit/index.d.ts
@@ -3,7 +3,7 @@
 // Definitions by: Giovanni Silva <https://github.com/giovannicandido>
 //                 Ivo Senner <https://github.com/s0x>
 //                 Weiyu Weng <https://github.com/pcdotfan>
-//                 Johanns Gregorian <<https://github.com/johanns>
+//                 Johanns Gregorian <https://github.com/johanns>
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
 // TypeScript Version: 2.3
 
