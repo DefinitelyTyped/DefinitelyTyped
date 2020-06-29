85b241feb458302d7ca356265b1a17ea8d44ed26
Oliver Joseph Ash <oliverjash@gmail.com>
2020-06-29 11:44:39 +0100

    Bump

--- types/http-errors/index.d.ts
+++ types/http-errors/index.d.ts
@@ -1,4 +1,4 @@
-// Type definitions for http-errors 1.6
+// Type definitions for http-errors 1.8
 // Project: https://github.com/jshttp/http-errors
 // Definitions by: Tanguy Krotoff <https://github.com/tkrotoff>
 //                 BendingBender <https://github.com/BendingBender>
