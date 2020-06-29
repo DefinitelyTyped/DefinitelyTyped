cbe4e306db39e915e2023acd38d28970c25540c7
Anton Rieder <aried3r@gmail.com>
2020-06-29 17:32:18 +0200

    [string-natural-compare] Add type definition

---
+++ types/string-natural-compare/index.d.ts
@@ -0,0 +1,10 @@
+// Type definitions for string-natural-compare 3.0
+// Project: https://github.com/nwoltman/string-natural-compare
+// Definitions by: Anton Rieder <https://github.com/DefinitelyTyped>
+// Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
+
+export default function naturalCompare(
+    a: string,
+    b: string,
+    options?: { caseInsensitive?: boolean; alphabet?: string },
+): number;
