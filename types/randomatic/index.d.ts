21e7ddd176771786014357e41cdd080f9980eb59
Amir Fuhaira <amirfuhairah@gmail.com>
2020-07-01 17:42:18 +0800

    Include definition author for randomatic

--- types/randomatic/index.d.ts
+++ types/randomatic/index.d.ts
@@ -1,5 +1,6 @@
 // Type definitions for randomatic 3.1
 // Project: https://github.com/jonschlinkert/randomatic
+// Definitions by: Frelia <https://github.com/execfera>
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
 
 declare function randomatic(p: string, l?: number, options?: {
