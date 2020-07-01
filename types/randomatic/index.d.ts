fed02ee4313eaa70fd7bc95ea3c7287db0351796
Amir Fuhaira <amirfuhairah@gmail.com>
2020-07-01 17:28:50 +0800

    Add types to randomatic

---
+++ types/randomatic/index.d.ts
@@ -0,0 +1,14 @@
+// Type definitions for randomatic 3.1
+// Project: https://github.com/jonschlinkert/randomatic
+// Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
+
+declare function randomatic(p: string, l?: number, options?: {
+  chars?: string;
+  exclude?: string | string[];
+}): string;
+
+declare namespace randomatic {
+  const isCrypto: boolean;
+}
+
+export = randomatic;
