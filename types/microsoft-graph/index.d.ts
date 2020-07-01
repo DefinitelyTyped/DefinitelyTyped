12a42a6345d14ec5d58fe010cb58d99acd370b29
Michael Mainer <mmainer@microsoft.com>
2020-07-01 13:24:28 -0700

    Fixed linting errors

--- types/microsoft-graph/index.d.ts
+++ types/microsoft-graph/index.d.ts
@@ -1,4 +1,4 @@
-// Type definitions for non-npm package microsoft-graph 1.13.0
+// Type definitions for non-npm package microsoft-graph 1.13
 // Project: https://github.com/microsoftgraph/msgraph-typescript-typings
 // Definitions by: Microsoft Graph Team <https://github.com/microsoftgraph>
 //                 Michael Mainer <https://github.com/MIchaelMainer>
