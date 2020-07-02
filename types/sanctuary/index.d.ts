ddf78235b03be4db3d3fdc3cec6fa5ab286da82d
Leonardo Farroco <leonardo.farroco@gmail.com>
2020-07-02 15:03:35 -0300

    Sanctuary - Adds Pair declarations (#45698)
    * Adds Pair declarations
    
    * whitespace fixes
    
    * removing single-use variables
    
    * replacing with any
    
    * adds missing whitespaces
    
    * changes interface for Pair - no longer uses array
    
    * updates name
    
    * Update tslint.json
    
    * Update index.d.ts
    
    * Update index.d.ts
    
    * Update tslint.json
    
    * Update index.d.ts

--- types/sanctuary/index.d.ts
+++ types/sanctuary/index.d.ts
@@ -12,7 +12,9 @@ export as namespace S;
 
 type Nullable<A> = A | null;
 
-type Pair<A, B> = [A, B];
+interface Pair<A, B> {
+  '@@type': 'sanctuary/Pair';
+}
 
 type Thunk<A> = () => A;
 
@@ -31,15 +33,11 @@ type Predicate<A> = (a: A) => boolean;
 interface StrMap<A> { [k: string]: A; }
 
 interface Maybe<A> {
-  constructor: {
-    '@@type': 'sanctuary/Maybe';
-  };
+  '@@type': 'sanctuary/Maybe';
 }
 
 interface Either<A, B> {
-  constructor: {
-    '@@type': 'sanctuary/Either';
-  };
+  '@@type': 'sanctuary/Either';
 }
 
 type ValidNumber            = number;
@@ -177,6 +175,12 @@ declare namespace Sanctuary {
     I<A>(x: A): A;
     K<A>(x: A): (y: any) => A;
     T<A>(x: A): <B>(f: Fn<A, B>) => B;
+    // Pair
+    Pair<A>(a: A): <B>(b: B) => Pair<A, B>;
+    pair<A, B, C>(f: Fn2<A, B, C>): (p: Pair<A, B>) => C;
+    fst<A, B>(p: Pair<A, B>): A;
+    snd<A, B>(p: Pair<A, B>): B;
+    swap<A, B>(p: Pair<A, B>): Pair<B, A>;
     //  Function
     curry2<A, B, C>(f: Fn2_<A, B, C>): Fn2<A, B, C>;
     curry3<A, B, C, D>(f: Fn3_<A, B, C, D>): Fn3<A, B, C, D>;
