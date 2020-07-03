d539d72da0609b5ac414c75aa18eb547bfcda893
Daniel Eden <dan.eden@me.com>
2020-07-03 21:28:58 +0100

    Turn off export to fix lint error

--- types/cxs/index.d.ts
+++ types/cxs/index.d.ts
@@ -9,7 +9,7 @@ declare namespace cxs {
     type CSSProperties = CSS.Properties<string | number>;
     type CSSPseudos = { [K in CSS.Pseudos]?: CSSObject };
 
-    export interface CSSObject extends CSSProperties, CSSPseudos {
+    interface CSSObject extends CSSProperties, CSSPseudos {
         [key: string]: CSSObject | string | number | undefined;
     }
 }
