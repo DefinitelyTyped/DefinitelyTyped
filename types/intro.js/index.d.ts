49cec210457c3beb07755af5fbb7b0531ecb69e8
Igor Timoshenkov <igor.timoshenkov@gmail.com>
2020-07-03 16:38:18 +0300

    Add missing methods to type declaration
    Update @types/intro.js to latest version of API in documentation

--- types/intro.js/index.d.ts
+++ types/intro.js/index.d.ts
@@ -81,6 +81,18 @@ declare namespace introJs {
         onafterchange(callback: (element: HTMLElement) => any): IntroJs;
         onchange(callback: (element: HTMLElement) => any): IntroJs;
         oncomplete(callback: Function): IntroJs;
+        
+        /**
+		 * @link https://introjs.com/docs/intro/api/#introjsaddstepoptions
+		 * @param {Step} options
+		 */
+		addStep(options: Step): IntroJs;
+        
+        /**
+		 * @link https://introjs.com/docs/intro/api/#introjsaddstepssteps
+		 * @param {Step[]} steps
+		 */
+		addSteps(steps: Step[]): IntroJs;
 
         addHints(): IntroJs;
         showHint(stepId: number): IntroJs;
