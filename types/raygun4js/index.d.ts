5279ffc3279191be167ebe75f60f4a2f55d07307
Krishna Kapadia <kapadikris@myvuw.ac.nz>
2020-07-06 11:23:03 +1200

    Fix: linting errors

--- types/raygun4js/index.d.ts
+++ types/raygun4js/index.d.ts
@@ -1,6 +1,9 @@
 // Type definitions for raygun4js 2.13
 // Project: https://github.com/MindscapeHQ/raygun4js
-// Definitions by: Krishna Kapadia <https://github.com/krishnakapadia>, Benjamin Harding <https://github.com/BenjaminHarding>, Taylor Lodge <https://github.com/UberMouse>, Brian Surowiec <https://github.com/xt0rted>
+// Definitions by: Krishna Kapadia <https://github.com/krishnakapadia>,
+//                 Benjamin Harding <https://github.com/BenjaminHarding>,
+//                 Taylor Lodge <https://github.com/UberMouse>,
+//                 Brian Surowiec <https://github.com/xt0rted>
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
 // TypeScript Version: 2.4
 
@@ -399,11 +402,11 @@ interface RaygunV2 {
         message:
             | string
             | {
-                  message: string;
-                  metadata: any;
-                  level: BreadcrumbLevel;
-                  location: string;
-              },
+                message: string;
+                metadata: any;
+                level: BreadcrumbLevel;
+                location: string;
+            },
         metadata: object
     ): void;
 }
