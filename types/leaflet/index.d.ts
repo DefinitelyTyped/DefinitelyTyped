6a6dd4ff8dcbb1ca1c4a51f7e6dbb5cdac502703
DerZade <DerZade@gmail.com>
2020-07-06 23:59:06 +0200

    Update index.d.ts (#45804)

--- types/leaflet/index.d.ts
+++ types/leaflet/index.d.ts
@@ -1628,7 +1628,7 @@ export interface LayersControlEvent extends LayerEvent {
 
 export interface TileEvent extends LeafletEvent {
     tile: HTMLImageElement;
-    coords: Point; // apparently not a normal point, since docs say it has z (zoom)
+    coords: Coords;
 }
 
 export interface TileErrorEvent extends TileEvent {
