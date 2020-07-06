7a9a764c03fd9fdfffca0dd28d8b21c2cfa027e0
Shengjie Pan <jessepan@umich.edu>
2020-07-06 12:32:09 -0700

    Add type definitions for nanoid-dictionary

---
+++ types/nanoid-dictionary/index.d.ts
@@ -0,0 +1,13 @@
+// Type definitions for nanoid-dictionary 3.0
+// Project: https://github.com/CyberAP/nanoid-dictionary#readme
+// Definitions by: Shengjie Pan <https://github.com/kenelm007>
+// Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
+
+/* Lowercase English letters */
+export const lowercase: string;
+/* Uppercase English letters */
+export const uppercase: string;
+/* Numbers from 0 to 9 */
+export const numbers: string;
+/* Numbers and english alphabet without lookalikes */
+export const nolookalikes: string;
---
+++ types/nanoid-dictionary/nanoid-dictionary-tests.ts
@@ -0,0 +1,6 @@
+import { lowercase, uppercase, numbers, nolookalikes } from 'nanoid-dictionary';
+
+lowercase; // $ExpectType string
+uppercase; // $ExpectType string
+numbers; // $ExpectType string
+nolookalikes; // $ExpectType string
