297384e37c0b14427c47c84f09872561df421824
Jack Tomaszewski <jacek@jtom.me>
2020-07-06 22:57:45 +0200

    fix(jest): Fix usage example of `jest.MockedClass` (#45840)
    So it reflects of how it should be done in [reality](https://github.com/DefinitelyTyped/DefinitelyTyped/blob/15fe0696be464ec1f7cc97ed42f1ec35bc4476b5/types/jest/jest-tests.ts#L613)

--- types/jest/index.d.ts
+++ types/jest/index.d.ts
@@ -1099,7 +1099,7 @@ declare namespace jest {
      *  import { MyClass } from "./libary";
      *  jest.mock("./library");
      *
-     *  const mockedMyClass = MyClass as jest.MockedClass<MyClass>;
+     *  const mockedMyClass = MyClass as jest.MockedClass<typeof MyClass>;
      *
      *  expect(mockedMyClass.mock.calls[0][0]).toBe(42); // Constructor calls
      *  expect(mockedMyClass.prototype.myMethod.mock.calls[0][0]).toBe(42); // Method calls
