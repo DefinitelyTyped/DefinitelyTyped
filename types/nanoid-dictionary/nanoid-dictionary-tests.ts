65d6917fc832912017c2eb03746a1ac600465a94
Shengjie Pan <jessepan@umich.edu>
2020-07-06 16:41:13 -0700

    Support use specific dictionary only
    As per comment of peterblazejewicz https://github.com/DefinitelyTyped/DefinitelyTyped/pull/45918

--- types/nanoid-dictionary/nanoid-dictionary-tests.ts
+++ types/nanoid-dictionary/nanoid-dictionary-tests.ts
@@ -1,4 +1,13 @@
-import { lowercase, uppercase, numbers, nolookalikes } from 'nanoid-dictionary';
+import dict = require('nanoid-dictionary');
+import lowercase = require('nanoid-dictionary/lowercase');
+import uppercase = require('nanoid-dictionary/uppercase');
+import numbers = require('nanoid-dictionary/numbers');
+import nolookalikes = require('nanoid-dictionary/nolookalikes');
+
+dict.lowercase; // $ExpectType string
+dict.uppercase; // $ExpectType string
+dict.numbers; // $ExpectType string
+dict.nolookalikes; // $ExpectType string
 
 lowercase; // $ExpectType string
 uppercase; // $ExpectType string
