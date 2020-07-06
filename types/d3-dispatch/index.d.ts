3a969b7c039d6789a444e76ff22346f9f2a3abd4
Deepak Kumar <deepak@kreatio.com>
2020-07-07 00:27:20 +0530

    Relax template parameter to be any object - it need not be an `EventTarget`.

--- types/d3-dispatch/index.d.ts
+++ types/d3-dispatch/index.d.ts
@@ -8,7 +8,7 @@
 
 // Last module patch version validated against: 1.0.3
 
-export interface Dispatch<T> {
+export interface Dispatch<T extends object> {
     /**
      * Like `function.apply`, invokes each registered callback for the specified type,
      * passing the callback the specified arguments, with `that` as the `this` context.
@@ -71,4 +71,4 @@ export interface Dispatch<T> {
  * @param types The event types.
  * @throws "illegal type" on empty string or duplicated event types.
  */
-export function dispatch<T>(...types: string[]): Dispatch<T>;
+export function dispatch<T extends object>(...types: string[]): Dispatch<T>;
