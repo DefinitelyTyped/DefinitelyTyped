c9c36ca4a98005c73525dbdcbfa118be866fad4e
Trevor <trevorhreed@users.noreply.github.com>
2020-07-06 18:08:29 -0600

    Update index.d.ts (#45919)

--- types/cheerio/index.d.ts
+++ types/cheerio/index.d.ts
@@ -8,7 +8,6 @@
 //                 Chennakrishna <https://github.com/chennakrishna8>
 //                 AzSiAz <https://github.com/AzSiAz>
 //                 Ryo Ota <https://github.com/nwtgck>
-//                 Trevor Reed <https://github.com/trevorhreed>
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
 
 /// <reference types="node" />
