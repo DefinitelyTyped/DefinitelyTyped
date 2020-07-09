9ae031ecc7d4aa16beb5bf0cd6343fe8486cf578
장세중/S/W Platform Lab(VD)/Engineer/삼성전자 <sejung.chang@samsung.com>
2020-07-09 12:12:11 +0900

    Resolve the lint errors

--- types/tizen-common-web/test/application-test.ts
+++ types/tizen-common-web/test/application-test.ts
@@ -2,8 +2,8 @@ import { application } from 'tizen-common-web';
 import { ApplicationControlLaunchMode, ApplicationInformation, ApplicationControl } from 'application';
 import { WebAPIError } from 'tizen';
 
-var app = application.getCurrentApplication();
-var watchId = app.addEventListener({ appId: app.appInfo.id, name: 'custom_user_event' }, function (event, data) {
+const app = application.getCurrentApplication();
+const watchId = app.addEventListener({ appId: app.appInfo.id, name: 'custom_user_event' }, (event, data) => {
     console.log('Data: ' + JSON.stringify(data));
     // Do something.
 });
@@ -11,13 +11,13 @@ var watchId = app.addEventListener({ appId: app.appInfo.id, name: 'custom_user_e
 ApplicationControlLaunchMode.GROUP;
 ApplicationControlLaunchMode.SINGLE;
 
-var appControl = new ApplicationControl('http://tizen.org/appcontrol/operation/pick', 'null', 'image/jpeg', 'null');
+const appControl = new ApplicationControl('http://tizen.org/appcontrol/operation/pick', 'null', 'image/jpeg', 'null');
 
 function successCB(appInfos: ApplicationInformation[], appControl: ApplicationControl) {
     // appControl is same object with the value passed as first parameter to findAppControl().
-    var appControlReplyCallback = {
+    const appControlReplyCallback = {
         // Callee sent a reply.
-        onsuccess: function () {
+        onsuccess: () => {
             // for (var i = 0; i < data.length; i++) {
             //     if (
             //         data[i].key ==
@@ -31,20 +31,20 @@ function successCB(appInfos: ApplicationInformation[], appControl: ApplicationCo
             console.log('[222]success');
         },
         // Callee returned failure.
-        onfailure: function () {
+        onfailure: () => {
             console.log('The launch application control failed');
         },
     };
 
-    var appId = appInfos[0].id; // Select first app's id.
+    const appId = appInfos[0].id; // Select first app's id.
 
     application.launchAppControl(
         appControl,
         appId,
-        function () {
+        () => {
             console.log('Launch application control succeed');
         },
-        function (e: WebAPIError) {
+        (e: WebAPIError) => {
             console.log('Launch application control failed, reason: ' + e.message);
         },
         appControlReplyCallback,
