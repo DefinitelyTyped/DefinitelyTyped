d7eff65c68c02b75e215e33faf9f026ad93b6e56
Alex Kondratyuk <lynxtaa@gmail.com>
2020-07-10 15:30:21 +0300

    Update extract-files typings

--- types/extract-files/extract-files-tests.ts
+++ types/extract-files/extract-files-tests.ts
@@ -1,9 +1,21 @@
-import { ReactNativeFile, ReactNativeFileOptions } from "extract-files";
+import {
+    ReactNativeFile,
+    ReactNativeFileOptions,
+    extractFiles,
+    isExtractableFile,
+    ExtractableFileMatcher,
+} from 'extract-files';
 
 const options: ReactNativeFileOptions = {
-    name: "",
-    type: "",
-    uri: "",
+    name: '',
+    type: '',
+    uri: '',
 };
 
 new ReactNativeFile(options);
+
+const isFile: ExtractableFileMatcher = (value: any) => isExtractableFile(value) || true;
+
+extractFiles({}, '', isFile);
+
+extractFiles({});
--- types/extract-files/index.d.ts
+++ types/extract-files/index.d.ts
@@ -1,20 +1,32 @@
-// Type definitions for extract-files 3.1
+// Type definitions for extract-files 8.1
 // Project: https://github.com/jaydenseric/extract-files#readme
 // Definitions by: Edward Sammut Alessi <https://github.com/Slessi>
+//                 Alex K <https://github.com/lynxtaa>
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
+
+export type ExtractableFileMatcher = (value: any) => boolean;
+
+export const isExtractableFile: ExtractableFileMatcher;
+
+export function extractFiles<T>(
+    value: T,
+    path?: string,
+    isExtractableFile?: ExtractableFileMatcher,
+): {
+    clone: T;
+    files: Map<any, string[]>;
+};
 
 export interface ReactNativeFileOptions {
     uri: string;
-    type: string;
-    name: string;
+    type?: string;
+    name?: string;
 }
 
 export class ReactNativeFile {
     uri: string;
-    type: string;
-    name: string;
+    type?: string;
+    name?: string;
 
     constructor(options: ReactNativeFileOptions);
-
-    static list(files: ReactNativeFileOptions[]): ReactNativeFile[];
 }
