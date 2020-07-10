132b810e1e347b743c72c1a4defc97cd56919aa1
praveen-me <praveenkrsaini0@gmail.com>
2020-07-10 21:37:59 +0530

    Added tests for workerb

--- types/workerb/workerb-tests.ts
+++ types/workerb/workerb-tests.ts
@@ -10,30 +10,97 @@ import {
     readTable,
     readUrl,
     select,
+    notify,
+    type,
+    log,
+    read,
+    readAll,
+    runInTab,
+    submit,
+    tab,
+    logging,
+    httpPut,
 } from 'workerb';
 
+open('https://google.com'); // should pass
+
 click('#div', {
-    method: 'by_xpath',
+    method: 'by_query_selector',
     retryDuration: 10,
     expectReload: true,
+    numberOfTries: 10,
+}); // should pass
+
+notify('Workerb is cool.', 'success', 200); // should pass
+
+type('Some text', '#form-input', {
+    numberOfTries: 10,
+    method: 'by_label',
+}); // should pass
+
+download('Download Pdf', '.download', '.pdf');
+
+event(
+    '#div',
+    {
+        eventType: 'keydown',
+    },
+    { method: 'by_query_selector', numberOfTries: 10 },
+);
+
+prompt('Enter your name');
+
+log('logging...');
+
+read('#input', {
+    method: 'by_query_selector',
 });
 
-download('ahg', 'nfhgfhj', '.xml');
+readAll('.boards', {
+    retryDuration: 10,
+});
 
-event('#div', { eventType: 'keydown' });
+readTable('#table', {
+    method: 'by_query_selector',
+    numberOfTries: 10,
+});
 
-getAttribute(['href'], 'a');
+readUrl('https://google.com');
 
-httpDelete('hfgfh');
+runInTab(function () {
+    open('https://cnn.com');
+    click('#searchInput', { method: 'by_query_selector' });
+    type('steve jobs', '#searchInput', { method: 'by_query_selector' });
+    submit('#searchInput', { expectReload: true });
+}, true);
 
-httpGet('gdfgh');
+select('2', '#.access-me', {
+    selectBy: 'label',
+    method: 'by_query_selector',
+    retryDuration: 10,
+});
 
-httpPost('hgj');
+submit('#workerB_Input', {
+    method: 'by_query_selector',
+    retryDuration: 10,
+});
 
-httpDelete('hgj');
+tab('https://google.com');
 
-readTable('hgjgj');
+getAttribute(['href'], 'a', {
+    method: 'by_query_selector_all',
+    retryDuration: 10,
+    numberOfTries: 10,
+});
 
-select('2', '.div', { selectBy: 'ng' });
+logging('on');
 
-const url = readUrl();
+const { response, status } = httpGet('https://api.github.com/userss/', {});
+
+httpDelete('https://www.google.com', JSON.stringify({ name: 'workerB' }), { 'content-type': 'application/json' });
+
+httpDelete('https://api.delete.com/posts/1');
+
+httpPost('https://www.google.com', JSON.stringify({ name: 'workerB' }), { 'content-type': 'application/json' });
+
+httpPut('https://www.google.com', JSON.stringify({ name: 'workerB' }), { 'content-type': 'application/json' });
