b8c391f1fd31291390c0aad4e65ec1afa0c79149
Jacob Fischer <jacob.t.fischer@gmail.com>
2020-07-14 19:14:24 -0500

    fix(html-docx-js): correct url to my authorship of html-docx-js types

--- types/html-docx-js/index.d.ts
+++ types/html-docx-js/index.d.ts
@@ -1,6 +1,6 @@
 // Type definitions for html-docx-js 0.3
 // Project: https://github.com/evidenceprime/html-docx-js#readme
-// Definitions by: Jacob Fischer <https://github.com/me>
+// Definitions by: Jacob Fischer <https://github.com/JacobFischer>
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
 
 /// <reference types="node" />
