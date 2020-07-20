c8c56b462758b37661d362a6f63c51e2e19f1e34
Pierre-Antoine Mills <pierreantoine.urvoy@gmail.com>
2020-07-20 19:54:40 +0200

    fix(mergeall): wrong type param

--- types/ramda/tools.d.ts
+++ types/ramda/tools.d.ts
@@ -292,7 +292,7 @@ export type Merge<O1 extends object, O2 extends object, Depth extends 'flat' | '
  * <created by @pirix-gh>
  */
 export type MergeAll<Os extends readonly object[]> =
-    O.AssignUp<{}, Os, 1> extends infer M
+    O.AssignUp<{}, Os, 'flat', 1> extends infer M
     ? {} extends M    // nothing merged => bcs no `as const`
       ? T.UnionOf<Os> // so we output the approximate types
       : M             // otherwise, we can get accurate types
