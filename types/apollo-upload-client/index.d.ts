35d1c2e5b3b189afe46ee8e22a846d2318225f2c
Jordan Overbye <jordanoverbye@gmail.com>
2020-07-20 16:41:10 +1000

    Update type definitions for apollo client v3

--- types/apollo-upload-client/index.d.ts
+++ types/apollo-upload-client/index.d.ts
@@ -1,22 +1,26 @@
-// Type definitions for apollo-upload-client 8.1
+// Type definitions for apollo-upload-client 14.0.0
 // Project: https://github.com/jaydenseric/apollo-upload-client#readme
 // Definitions by: Edward Sammut Alessi <https://github.com/Slessi>
+//                 Jordan Overbye <https://github.com/jordanoverbye>
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
 // TypeScript Version: 3.1
 
-import { ApolloLink } from "apollo-link";
-import { HttpOptions } from "apollo-link-http-common";
-
-export { ReactNativeFile } from "extract-files";
+import { ApolloLink, HttpOptions } from '@apollo/client';
+export { ReactNativeFile } from 'extract-files';
 
 declare global {
     interface GlobalFetch {
-        fetch: WindowOrWorkerGlobalScope["fetch"];
+        fetch: WindowOrWorkerGlobalScope['fetch'];
     }
 }
 
-/**
- * `createUploadLink` options match `createHttpLink` options
- * @param linkOptions `HttpOptions`
- */
-export function createUploadLink(linkOptions?: HttpOptions): ApolloLink;
+export function createUploadLink(
+    linkOptions?: HttpOptions & {
+        // Customizes how files are matched in the GraphQL operation for extraction.
+        isExtractableFile?: (value: any) => boolean;
+        // FormData implementation to use, defaulting to the FormData global.
+        FormData?: any;
+        // Customizes how extracted files are appended to the FormData instance.
+        formDataAppendFile?: (formData: FormData, fieldName: string, file: File) => void;
+    },
+): ApolloLink;
