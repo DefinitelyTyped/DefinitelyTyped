d79f6eb097194eb87b8e986fa9bdc1b23e50427d
Felix <felix.mattick@gmail.com>
2020-07-21 14:58:02 -0400

    Fix mistakes

--- types/deta/deta-tests.ts
+++ types/deta/deta-tests.ts
@@ -31,3 +31,5 @@ async function test() {
         key6: db.util.trim()
     }, 'key');
 }
+
+test();
--- types/deta/index.d.ts
+++ types/deta/index.d.ts
@@ -3,46 +3,37 @@
 // Definitions by: Felix Mattick <https://github.com/kognise>
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
 
-declare namespace Deta {
-    export type Serializable = { [key: string]: Serializable } | string | number | boolean | Serializable[];
+export type Serializable = { [key: string]: any } | string | number | boolean | ReadonlyArray<any>;
 
-    export interface DetaInstance {
-        Base(db: string): DetaBase;
-    }
-
-    export interface TrimUpdate {}
-    export interface IncrementUpdate {}
-    export interface AppendUpdate {}
-    export interface PrependUpdate {}
-
-    export interface DetaBaseUpdates {
-        [key: string]: Serializable | TrimUpdate | AppendUpdate | PrependUpdate | IncrementUpdate;
-    }
-
-    export interface DetaBaseQuery {
-        [key: string]: string | number | boolean | DetaBaseQuery;
-    }
-
-    export interface DetaBaseUtil {
-        trim(): TrimUpdate;
-        increment(amount?: number): IncrementUpdate;
-        append(value: Serializable): AppendUpdate;
-        prepend(value: Serializable): PrependUpdate;
-    }
-
-    export interface DetaBase {
-        put<Data = Serializable>(data: Data, key?: string): Promise<Data>;
-        putMany<Items = Serializable[]>(items: Items): Promise<Items>;
-        insert<Data = Serializable>(data: Data, key: string): Promise<Data>;
-        get(key: string): Promise<Serializable | null>;
-        delete(key: string): Promise<void>;
-        fetch(query?: DetaBaseQuery, limit?: number, buffer?: number): Promise<AsyncIterable<Serializable[]>>;
-        update(updates: DetaBaseUpdates, key: string): Promise<void>;
-
-        util: DetaBaseUtil;
-    }
-
-    export function Deta(projectKey: string): Deta.DetaInstance;
+export interface DetaInstance {
+    Base(db: string): DetaBase;
 }
 
-export = Deta;
+export interface DetaBaseUpdates {
+    [key: string]: Serializable | {};
+}
+
+export interface DetaBaseQuery {
+    [key: string]: string | number | boolean | DetaBaseQuery;
+}
+
+export interface DetaBaseUtil {
+    trim(): {};
+    increment(amount?: number): {};
+    append(value: Serializable): {};
+    prepend(value: Serializable): {};
+}
+
+export interface DetaBase {
+    put<Data = Serializable>(data: Data, key?: string): Promise<Data>;
+    putMany<Items = ReadonlyArray<Serializable>>(items: Items): Promise<Items>;
+    insert<Data = Serializable>(data: Data, key: string): Promise<Data>;
+    get(key: string): Promise<Serializable | null>;
+    delete(key: string): Promise<void>;
+    fetch(query?: DetaBaseQuery, limit?: number, buffer?: number): Promise<AsyncIterable<ReadonlyArray<Serializable>>>;
+    update(updates: DetaBaseUpdates, key: string): Promise<void>;
+
+    util: DetaBaseUtil;
+}
+
+export function Deta(projectKey: string): DetaInstance;
