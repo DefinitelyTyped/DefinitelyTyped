90741f083774d563a411843f4cbf5a24e7619707
nadchif <nadchif@gmail.com>
2020-07-21 17:32:16 +0200

    added types for img-diff-js

---
+++ types/img-diff-js/index.d.ts
@@ -0,0 +1,28 @@
+// Type definitions for img-diff-js 0.4
+// Project: https://github.com/reg-viz/img-diff-js#readme
+// Definitions by: Daniel Chif <https://github.com/nadchif>
+// Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
+
+export interface ImgDiffOptions {
+    actualFilename: string;
+    expectedFilename: string;
+    diffFilename?: string;
+    generateOnlyDiffFile?: boolean; // default false
+    options?: {
+        threshold?: number; // default 0.1
+        includeAA?: boolean; // default false
+    };
+}
+
+export interface ImgDiffResult {
+    width: number;
+    height: number;
+    imagesAreSame: boolean;
+    diffCount: number;
+}
+/**
+ * Create image differential between two images.
+ */
+export function imgDiff(opt: ImgDiffOptions): Promise<ImgDiffResult>;
+
+export function registerDecoder(extensions: any, decoder: any): void;
