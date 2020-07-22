214bcb8d2083bf0d005353231f385e8d54867296
Pranay Prakash <pranay.gp@gmail.com>
2020-07-22 13:38:35 -0700

    add tests

--- types/death/death-tests.ts
+++ types/death/death-tests.ts
@@ -1,18 +1,54 @@
 import ON_DEATH = require("death");
 
-const unsub: () => void = ON_DEATH(
+const unsub1: () => void = ON_DEATH(
     (value: "SIGINT" | "SIGTERM" | "SIGQUIT") => {}
 );
 
-const otherUnsub: () => void = ON_DEATH({
+const unsub2: () => void = ON_DEATH({
+    debug: true,
+    SIGINT: true,
+    SIGTERM: true,
+    SIGQUIT: true,
+})(
+    (
+        value: "SIGINT" | "SIGTERM" | "SIGQUIT"
+    ) => {}
+);
+
+const unsub3: () => void = ON_DEATH({
     debug: true,
     uncaughtException: true,
     SIGINT: true,
     SIGTERM: true,
     SIGQUIT: true,
-    SIGHUP: true
 })(
     (
-        value: "uncaughtException" | "SIGINT" | "SIGTERM" | "SIGQUIT" | "SIGHUP"
+        value: "SIGINT" | "SIGTERM" | "SIGQUIT" | Error
     ) => {}
+);
+
+const unsub4: () => void = ON_DEATH({
+    debug: true,
+    SIGINT: true,
+    SIGTERM: true,
+    SIGQUIT: true,
+})(
+    (
+        value: "SIGINT" | "SIGTERM" | "SIGQUIT"
+    ) => {
+    }
+);
+
+const unsub5: () => void = ON_DEATH({
+    debug: true,
+    uncaughtException: true,
+    SIGINT: true,
+    SIGTERM: true,
+    SIGQUIT: true,
+})(
+    // $ExpectError
+    (
+        value: "SIGINT" | "SIGTERM" | "SIGQUIT"
+    ) => {
+    }
 );
