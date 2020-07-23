f831391fd9a0b42c521ff200ce276f4c78b95af6
Ulrich Block <ulblock@gmx.de>
2020-07-23 16:46:27 +0200

    address linter errors and add testing comments

--- types/nspell/index.d.ts
+++ types/nspell/index.d.ts
@@ -11,10 +11,10 @@
  * @param - forbidden - Whether word is actually correct, but forbidden from showing up as such (often by the users wish)
  * @param warn - Whether word is correct, but should trigger a warning (rarely used in dictionaries)
  */
-interface ISpellCheck {
-    correct: boolean
-    forbidden: boolean
-    warn: boolean
+interface SpellCheck {
+    correct: boolean;
+    forbidden: boolean;
+    warn: boolean;
 }
 
 /**
@@ -27,76 +27,67 @@ export interface NSpell {
      * If a model is given, word will be handled the same as model.
      * @returns Operated on instance.
      */
-    add(word: string, model?: string): NSpell
+    add(word: string, model?: string): NSpell;
 
     /**
      * Check if word is correctly spelled
      * @returns Whether word is correctly spelled.
      */
-    correct(word: string): boolean
+    correct(word: string): boolean;
 
     /**
      * Add an extra dictionary to the spellchecker.
      * @returns Operated on instance.
      */
-    dictionary(words: string | Buffer): NSpell
+    dictionary(words: string | Buffer): NSpell;
 
     /**
      * Add a personal dictionary.
      * @returns Operated on instance.
      */
-    personal(words: string | Buffer): NSpell
+    personal(words: string | Buffer): NSpell;
 
     /**
      * Remove word from the known words.
      * @returns  Operated on instance.
      */
-    remove(word: string): NSpell
+    remove(word: string): NSpell;
 
     /**
      * Suggest correctly spelled words close to word.
      * @returns List with zero or more suggestions.
      */
-    suggest(word: string): string[]
+    suggest(word: string): string[];
 
     /**
      * Get spelling information for word.
      * @returns Object, with the properties correct, forbidden and warn
      */
-    spell(word: string): ISpellCheck
+    spell(word: string): SpellCheck;
 
     /**
      * Get extra word characters defined by the loaded affix file.
      * Most affix files don’t set these, but for example the en-US dictionary sets 0123456789.
      * @returns Defined word characters, if any.
      */
-    wordCharacters(): string | undefined
+    wordCharacters(): string | undefined;
 }
 
-export interface IDictionary {
-    aff: Buffer | string
-    dic?: Buffer | string
+export interface Dictionary {
+    aff: Buffer | string;
+    dic?: Buffer | string;
 }
 
-/**
- * @param aff - Affix document to use. Must be in UTF-8 when buffer
- * @param dic - Dictionary document to use. Must be in UTF-8 when buffer
- */
-declare function nspell(aff: Buffer | string, dic?: Buffer | string): NSpell
-
-/**
- * @param dictionary - Object with aff (required) and dic (optional) properties
- */
-declare function nspell(dictionary: IDictionary): NSpell
-
-/**
- * @param dictionaries - List of dictionary objects. The first must have an aff key, other aff keys are ignored
- */
-declare function nspell(dictionaries: IDictionary[]): NSpell
+declare function nspell(aff: Buffer | string, dic?: Buffer | string): NSpell;
+declare function nspell(dictionary: Dictionary | Dictionary[]): NSpell;
 
 /**
  * Create a new spell checker. Passing an affix document is required, through any of the below mentioned signatures.
  * nspell is useless without at least one dic passed:
  * make sure to pass one either in the constructor or to NSpell.dictionary
+ * @param aff - Affix document to use. Must be in UTF-8 when buffer
+ * @param dic - Dictionary document to use. Must be in UTF-8 when buffer
+ * @param dictionary {Dictionary} - Object with aff (required) and dic (optional) properties
+ * @param dictionary {Dictionary[]} - List of dictionary objects. The first must have an aff key, other aff keys are ignored
  */
-export default nspell
+export default nspell;
--- types/nspell/nspell-tests.ts
+++ types/nspell/nspell-tests.ts
@@ -1,19 +1,34 @@
-import nspell = require('nspell')
+import nspell from 'nspell';
 
-nspell('')
-nspell(new Buffer(''))
-nspell(new Buffer(''), new Buffer(''))
+// $ExpectType NSpell
+nspell('');
+// $ExpectType NSpell
+nspell(new Buffer(''));
+// $ExpectType NSpell
+nspell(new Buffer(''), new Buffer(''));
+// $ExpectType NSpell
 nspell({ aff: new Buffer(''), dic: new Buffer('') });
+// $ExpectType NSpell
 nspell([{ aff: new Buffer('') }, { aff: new Buffer('') }]);
 
-const spell = nspell('', '')
-spell.correct('')
-spell.suggest('')
-spell.spell('')
-spell.add('')
-spell.remove('')
-spell.wordCharacters()
-spell.dictionary('')
-spell.dictionary(new Buffer(''))
-spell.personal('')
-spell.personal(new Buffer(''))
+const spell = nspell('', '');
+// $ExpectType boolean
+spell.correct('');
+// $ExpectType string[]
+spell.suggest('');
+// $ExpectType SpellCheck
+spell.spell('');
+// $ExpectType NSpell
+spell.add('');
+// $ExpectType NSpell
+spell.remove('');
+// $ExpectType string | undefined
+spell.wordCharacters();
+// $ExpectType NSpell
+spell.dictionary('');
+// $ExpectType NSpell
+spell.dictionary(new Buffer(''));
+// $ExpectType NSpell
+spell.personal('');
+// $ExpectType NSpell
+spell.personal(new Buffer(''));
