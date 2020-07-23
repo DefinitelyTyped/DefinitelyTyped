3f9a71f0deb7e369fc7795c9d5e37a998b4c5d39
Raiper34 <raipergm34@gmail.com>
2020-07-23 13:29:00 +0200

    Add granim typings

--- types/granim/index.d.ts
+++ types/granim/index.d.ts
@@ -3,52 +3,51 @@
 // Definitions by: Raiper34 <https://github.com/Raiper34>
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
 
-export declare class Granim {
-  constructor(options: Options);
-  play(): void;
-  pause(): void;
-  clear(): void;
-  changeState(stateName: string): void;
-  changeDirection(directionName: string): void;
-  changeBlendingMode(blendingModeName: string): void;
-  destroy(): void;
+export class Granim {
+    constructor(options: Options);
+    play(): void;
+    pause(): void;
+    clear(): void;
+    changeState(stateName: string): void;
+    changeDirection(directionName: string): void;
+    changeBlendingMode(blendingModeName: string): void;
+    destroy(): void;
 }
-
 export interface OnGradientChangeObject {
-  activeState: string;
-  colorsFrom: [string, string];
-  colorsTo: [string, string];
-  isLooping: boolean;
+    activeState: string;
+    colorsFrom: [string, string];
+    colorsTo: [string, string];
+    isLooping: boolean;
 }
-
 export interface Options {
-  element: string | HTMLCanvasElement;
-  name?: string;
-  elToSetClassOn?: string;
-  direction?: 'diagonal' | 'left-right' | 'top-bottom' | 'radial' | 'custom';
-  customDirection?: {x0: string; y0: string; x1: string; y1: string};
-  isPausedWhenNotInView?: boolean;
-  scrollDebounceThreshold?: number;
-  stateTransitionSpeed?: number;
-  defaultStateName?: string;
-  states: {[stateName: string]: State};
-  image?: Image;
-  onStart?: () => void;
-  onGradientChange?: (colorDetails: OnGradientChangeObject) => void;
-  onEnd?: () => void;
+    element: string | HTMLCanvasElement;
+    name?: string;
+    elToSetClassOn?: string;
+    direction?: 'diagonal' | 'left-right' | 'top-bottom' | 'radial' | 'custom';
+    customDirection?: { x0: string; y0: string; x1: string; y1: string };
+    isPausedWhenNotInView?: boolean;
+    scrollDebounceThreshold?: number;
+    stateTransitionSpeed?: number;
+    defaultStateName?: string;
+    states: { [stateName: string]: State };
+    image?: Image;
+    onStart?: () => void;
+    onGradientChange?: (colorDetails: OnGradientChangeObject) => void;
+    onEnd?: () => void;
 }
-
+export interface Gradient {
+    color: string;
+    pos: number;
+}
 export interface State {
-  gradients: string[][] | {color: string, pos: number}[][];
-  transitionSpeed?: number;
-  loop?: boolean;
+    gradients: string[][] | Gradient[][];
+    transitionSpeed?: number;
+    loop?: boolean;
 }
-
-type stretchMode = 'none' | 'stretch' | 'stretch-if-smaller' | 'stretch-if-bigger';
-
+export type stretchMode = 'none' | 'stretch' | 'stretch-if-smaller' | 'stretch-if-bigger';
 export interface Image {
-  source: string;
-  position?: ['left' | 'center' | 'right', 'top' | 'center' | 'bottom'];
-  stretchMode?: [stretchMode, stretchMode];
-  blendingMode?: string;
+    source: string;
+    position?: ['left' | 'center' | 'right', 'top' | 'center' | 'bottom'];
+    stretchMode?: [stretchMode, stretchMode];
+    blendingMode?: string;
 }
