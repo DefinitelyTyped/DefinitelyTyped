5179f1da548e6f3b3a1f35fca34a11a5359e79a7
Alex Kondov <alexkondov38@gmail.com>
2020-07-23 13:10:59 +0300

    Add type definitions for ft-poller

---
+++ types/ft-poller/ft-poller-tests.ts
@@ -0,0 +1,10 @@
+import Poller from 'ft-poller'
+
+type Data = {
+    [key: string]: string
+}
+
+const poller = new Poller<Data>({ url: 'example.com' })
+
+poller.start({}) // $ExpectType Promise<void | Data>
+poller.getData() // $ExpectType Data
---
+++ types/ft-poller/index.d.ts
@@ -0,0 +1,27 @@
+// Type definitions for ft-poller 3.0
+// Project: https://github.com/Financial-Times/ft-poller#readme
+// Definitions by: Alex Kondov <https://github.com/DefinitelyTyped>
+// Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
+
+declare module 'ft-poller' {
+    interface Config<PollerData, ExternalData> {
+        url: string;
+        defaultData?: PollerData;
+        options?: object;
+        refreshInterval?: number;
+        autostart?: boolean;
+        parseData?: (data: ExternalData) => PollerData;
+    }
+
+    class Poller<PollerData, ExternalData = any> {
+        constructor(config: Config<PollerData, ExternalData>)
+        start(options?: object): Promise<PollerData | void>
+        stop(): boolean;
+        retry(): void;
+        fetch(): void;
+        getData(): PollerData;
+        isRunning(): boolean;
+    }
+
+    export default Poller
+}
