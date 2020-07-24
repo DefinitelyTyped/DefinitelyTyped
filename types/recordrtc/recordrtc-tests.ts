1669b9f9d0c26302273481cd2117235f059c7d98
Kyle Hensel <k.y.l.e@outlook.co.nz>
2020-07-25 10:53:57 +1200

    add typedefs for RecordRTC package

---
+++ types/recordrtc/recordrtc-tests.ts
@@ -0,0 +1,26 @@
+import RecordRTC from 'recordrtc';
+
+navigator.getUserMedia(
+    { audio: true, video: true },
+    stream => {
+        const instance = new RecordRTC(stream, {
+            type: 'video',
+            disableLogs: true,
+            bufferSize: 2048,
+        });
+
+        instance.stopRecording(() => {
+            const blob = instance.getBlob();
+        });
+    },
+    console.error,
+);
+
+const canvas = document.querySelector('canvas')!;
+
+const instance2 = new RecordRTC(canvas, {
+    type: 'canvas',
+});
+instance2.stopRecording();
+
+console.log(RecordRTC.version);
