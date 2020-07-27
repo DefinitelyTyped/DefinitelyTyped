f308cf6d8383e439a55cce749e2f4ca153b6c50c
SSen <i@ssen.name>
2020-07-27 13:07:58 +0900

    fix: update version number from 3.3 to 3.4

--- types/testing-library__react-hooks/index.d.ts
+++ types/testing-library__react-hooks/index.d.ts
@@ -1,4 +1,4 @@
-// Type definitions for @testing-library/react-hooks 3.3
+// Type definitions for @testing-library/react-hooks 3.4
 // Project: https://github.com/testing-library/react-hooks-testing-library
 // Definitions by: Michael Peyper <https://github.com/mpeyper>
 //                Sarah Dayan <https://github.com/sarahdayan>
