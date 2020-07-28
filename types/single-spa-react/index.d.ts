4787ff10de17b5b6a76f2ff3a274c79715040030
Lasse Linkola <lasse.linkola@enersoft.fi>
2020-07-28 14:00:14 +0300

    add renderType to SingleSpaReact Options, updated single-spa-react version

--- types/single-spa-react/index.d.ts
+++ types/single-spa-react/index.d.ts
@@ -1,4 +1,4 @@
-// Type definitions for single-spa-react 2.8
+// Type definitions for single-spa-react 3.0.1
 // Project: https://github.com/CanopyTax/single-spa-react, https://github.com/joeldenning/single-spa-react
 // Definitions by: Garrett Smith <https://github.com/Garrett-Smith-iq>
 //                 Chris Dopuch <https://github.com/chrisdopuch>
@@ -23,6 +23,7 @@ declare namespace SingleSpaReact {
         domElementGetter?: () => Element;
         suppressComponentDidCatchWarning?: boolean;
         parcelCanUpdate?: boolean;
+        renderType?: 'render' | 'hydrate' | 'createRoot' | 'createBlockingRoot';
     }
 
     interface Lifecycles {
