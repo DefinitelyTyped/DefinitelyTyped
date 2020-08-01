58871f5300d7a047cf15d6a5dd1d78e2acd3ca42
pole <pole1419@hotmail.com>
2020-08-01 16:51:09 +0800

    feat: add aladdin type

---
+++ types/aladdin/aladdin-tests.ts
@@ -0,0 +1,2 @@
+aladdin.count(1); // $ExpectType number
+aladdin.copy(''); // $ExpectType string
---
+++ types/aladdin/idnex.d.ts
@@ -0,0 +1,4 @@
+export function count(n: number): number
+export function copy(n: string): string
+
+export as namespace aladdin
