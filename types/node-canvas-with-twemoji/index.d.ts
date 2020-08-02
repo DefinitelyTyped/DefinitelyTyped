0354b3f2c2b3e383634a1667824e3163bdab820c
Sfue <50198413+abh80@users.noreply.github.com>
2020-08-02 11:58:59 +0530

    Add files via upload

---
+++ types/node-canvas-with-twemoji/index.d.ts
@@ -0,0 +1,8 @@
+// Type definitions for node-canvas-with-twemoji 0.1
+// Project: https://github.com/cagpie/node-canvas-with-twemoj
+// Definitions by: Abh80 <https://github.com/abh80/>
+// Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
+/// <reference types="node" />
+
+declare function fillTextWithTwemoji(context:object,text:string,x:number,y:number): void;
+export = fillTextWithTwemoji
---
+++ types/node-canvas-with-twemoji/node-canvas-with-twemoji-test.ts
@@ -0,0 +1,9 @@
+import fillTextWithTwemoji = require('node-canvas-with-twemoji')
+import {createCanvas} from 'canvas'
+const canvas = createCanvas(200,200)
+const ctx = canvas.getContext('2d')
+async function GenerateCanvas(){
+    await fillTextWithTwemoji(ctx,'This is a test',20,20)
+    return canvas.toBuffer()
+}
+GenerateCanvas()
