0b66c1f85d004b632df16ed7c6cc6ccaa5f20e45
Sfue <50198413+abh80@users.noreply.github.com>
2020-08-02 11:17:21 +0530

    Add files via upload

---
+++ types/node-canvas-with-twemoji/node-canvas-with-twemoji-test.ts
@@ -0,0 +1,8 @@
+import {fillTextWithTwemoji} from 'node-canvas-with-twemoji'
+import {createCanvas} from 'canvas'
+const canvas = createCanvas(200,200)
+const ctx = canvas.getContext('2d')
+async function GenerateCanvas(){
+    await fillTextWithTwemoji(ctx,'This is a test',20,20)
+}
+GenerateCanvas()
