45816108d607902d92f5fbee3b9e5a77a5a80dce
pole <pole1419@hotmail.com>
2020-08-02 21:22:34 +0800

    chore: update config

--- types/pa-aladdin/index.d.ts
+++ types/pa-aladdin/index.d.ts
@@ -1,3 +1,8 @@
+// Type definitions for pa-aladdin
+// Project: https://github.com/pole1419/pa-aladdin
+// Definitions by: pole1419 <https://github.com/pole1419>
+// Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
+
 declare namespace Aladdin {
     interface Env {
         isPro: boolean
