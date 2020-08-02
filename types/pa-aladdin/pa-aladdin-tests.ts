6a2fdf3450759e8b169ebecbc778fc884359794c
pole <pole1419@hotmail.com>
2020-08-02 22:07:57 +0800

    chore: update config

--- types/pa-aladdin/pa-aladdin-tests.ts
+++ types/pa-aladdin/pa-aladdin-tests.ts
@@ -1,5 +1,1 @@
 import Aladdin from 'pa-aladdin';
-
-const isPro: Aladdin.Env.isPro = true; // $ExpectType boolean
-const isDev: Aladdin.Env.isDev = false; // $ExpectType boolean
-const countFn: Aladdin.count = (n => n * 2); // $ExpectType number
