89d6c2468912ad56bce042e03ca38152b0249e12
tpluscode <tpluscode@users.noreply.github.com>
2020-08-04 10:20:10 +0200

    lint

--- types/rdfjs__serializer-ntriples/index.d.ts
+++ types/rdfjs__serializer-ntriples/index.d.ts
@@ -6,7 +6,6 @@
 import { EventEmitter } from 'events';
 import { Sink, Stream, BaseQuad, Quad } from 'rdf-js';
 
-
 declare class Serializer<Q extends BaseQuad = Quad> implements Sink<Stream<Q>, EventEmitter> {
     import(stream: Stream<Q>): EventEmitter;
 }
