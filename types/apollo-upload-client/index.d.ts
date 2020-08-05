ca4838ce5ea2bda09e19dc46d713506b965d7fdb
Jordan Overbye <jordanoverbye@gmail.com>
2020-08-06 08:36:07 +1000

    Extend interface and add Simen Bekkhus

--- types/apollo-upload-client/index.d.ts
+++ types/apollo-upload-client/index.d.ts
@@ -2,6 +2,7 @@
 // Project: https://github.com/jaydenseric/apollo-upload-client#readme
 // Definitions by: Edward Sammut Alessi <https://github.com/Slessi>
 //                 Jordan Overbye <https://github.com/jordanoverbye>
+//                 Simen Bekkhus <https://github.com/SimenB>
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
 // TypeScript Version: 3.1
 
@@ -15,13 +16,13 @@ declare global {
     }
 }
 
-export function createUploadLink(
-    linkOptions?: HttpOptions & {
-        // Function that checks if a value is an extractable file
-        isExtractableFile?: (value: any) => boolean;
-        // FormData implementation to use, defaulting to the FormData global.
-        FormData?: any;
-        // Customizes how extracted files are appended to the FormData instance.
-        formDataAppendFile?: (formData: FormData, fieldName: string, file: any) => void;
-    },
-): ApolloLink;
+interface UploadHttpOptions extends HttpOptions {
+    // Function that checks if a value is an extractable file
+    isExtractableFile?: (value: any) => boolean;
+    // FormData implementation to use, defaulting to the FormData global.
+    FormData?: any;
+    // Customizes how extracted files are appended to the FormData instance.
+    formDataAppendFile?: (formData: FormData, fieldName: string, file: any) => void;
+}
+
+export function createUploadLink(linkOptions?: UploadHttpOptions): ApolloLink;
