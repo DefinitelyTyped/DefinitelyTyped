f4308a06dd529e5f6227f12aeca938bcbb464854
Michael Morelock <mmorelock@talroo.com>
2020-08-05 15:33:37 -0500

    Added  type definitions and test

--- types/text-mask-addons/index.d.ts
+++ types/text-mask-addons/index.d.ts
@@ -3,10 +3,25 @@
 // Definitions by: josh <https://github.com/huntjosh>
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
 
-import { Pipe } from 'text-mask-core';
+import { Mask, Pipe } from 'text-mask-core';
 
 export interface DatePipeYears {
     minYear: number;
     maxYear: number;
 }
 export function createAutoCorrectedDatePipe(dateFormat?: string, validYears?: DatePipeYears): Pipe;
+
+export interface NumberMaskOptions {
+    prefix: string;
+    suffix: string;
+    includeThousandsSeparator: boolean;
+    thousandsSeparatorSymbol: string;
+    allowDecimal: boolean;
+    decimalSymbol: string;
+    decimalLimit: number;
+    requireDecimal: boolean;
+    allowNegative: boolean;
+    allowLeadingZeroes: boolean;
+    integerLimit: number | null;
+}
+export function createNumberMask(maskOptions?: Partial<NumberMaskOptions>): Mask;
