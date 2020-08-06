6b457ce29079f89136f4ea85ba47151441009af6
Nigecat <nigel.quick@carey.com.au>
2020-08-06 13:45:13 +1000

    Add types for urban-dictionary

---
+++ types/urban-dictionary/urban-dictionary-tests.ts
@@ -0,0 +1,7 @@
+import methods = require("urban-dictionary");
+
+methods.defid(10085618); // $ExpectType Promise<WordDefinition>
+
+methods.random(); // $ExpectType Promise<WordDefinition>
+
+methods.term("nodejs"); // $ExpectType Promise<{ entries: WordDefinition[]; tags: string[]; sounds: string[]; }>
