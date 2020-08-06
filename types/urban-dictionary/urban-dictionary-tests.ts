33601e774fedac5354e39a20d938d90dfda2ac9c
Nigecat <nigel.quick@carey.com.au>
2020-08-06 13:56:39 +1000

    Add tests

--- types/urban-dictionary/urban-dictionary-tests.ts
+++ types/urban-dictionary/urban-dictionary-tests.ts
@@ -5,3 +5,7 @@ methods.defid(10085618); // $ExpectType Promise<WordDefinition>
 methods.random(); // $ExpectType Promise<WordDefinition>
 
 methods.term("nodejs"); // $ExpectType Promise<{ entries: WordDefinition[]; tags: string[]; sounds: string[]; }>
+
+methods.random((error, object) => {
+    object; // $ExpectType WordDefinition
+});
