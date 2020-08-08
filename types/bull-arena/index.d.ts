a64536bccedc189e3397fa4db43f8603b9dfbcaa
Eli Skeggs <eli@mixmax.com>
2020-08-07 21:40:21 -0700

    style: apply prettier changes

--- types/bull-arena/index.d.ts
+++ types/bull-arena/index.d.ts
@@ -4,53 +4,50 @@
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
 // TypeScript Version: 2.8
 
-import { RequestHandler } from "express";
-import { ClientOpts } from "redis";
+import { RequestHandler } from 'express';
+import { ClientOpts } from 'redis';
 
 declare function Arena(
-  options: BullArena.MiddlewareOptions,
-  listenOptions?: BullArena.MiddlewareListenOptions
+    options: BullArena.MiddlewareOptions,
+    listenOptions?: BullArena.MiddlewareListenOptions,
 ): RequestHandler;
 
 declare namespace BullArena {
-  interface MiddlewareOptions {
-    queues: Array<QueueOptions & ConnectionOptions>;
-  }
+    interface MiddlewareOptions {
+        queues: Array<QueueOptions & ConnectionOptions>;
+    }
 
-  interface MiddlewareListenOptions {
-    port?: number;
-    host?: string;
-    basePath?: string;
-    disableListen?: boolean;
-    useCdn?: boolean;
-  }
+    interface MiddlewareListenOptions {
+        port?: number;
+        host?: string;
+        basePath?: string;
+        disableListen?: boolean;
+        useCdn?: boolean;
+    }
 
-  interface QueueOptions {
-    name: string;
-    hostId?: string;
-    type?: "bull" | "bee";
-    prefix?: "bull" | "bq" | string;
-  }
+    interface QueueOptions {
+        name: string;
+        hostId?: string;
+        type?: 'bull' | 'bee';
+        prefix?: 'bull' | 'bq' | string;
+    }
 
-  type ConnectionOptions =
-    | PortHostConnectionOptions
-    | RedisUrlConnectionOptions
-    | RedisClientConnectionOptions;
+    type ConnectionOptions = PortHostConnectionOptions | RedisUrlConnectionOptions | RedisClientConnectionOptions;
 
-  interface PortHostConnectionOptions {
-    host: string;
-    port?: number;
-    password?: string;
-    db?: string;
-  }
+    interface PortHostConnectionOptions {
+        host: string;
+        port?: number;
+        password?: string;
+        db?: string;
+    }
 
-  interface RedisUrlConnectionOptions {
-    url: string;
-  }
+    interface RedisUrlConnectionOptions {
+        url: string;
+    }
 
-  interface RedisClientConnectionOptions {
-    redis: ClientOpts;
-  }
+    interface RedisClientConnectionOptions {
+        redis: ClientOpts;
+    }
 }
 
 export = Arena;
