bb4ea6c93293d1d363caec93d0b40e61a7466600
Lex Webb <lexwebb@gmail.com>
2020-08-11 00:14:57 +0100

    Added more tests for new typing of getContextStorage

--- types/emartech__cls-adapter/emartech__cls-adapter-tests.ts
+++ types/emartech__cls-adapter/emartech__cls-adapter-tests.ts
@@ -1,5 +1,10 @@
 import ClsAdapter = require('@emartech/cls-adapter');
 
+interface ContextDef {
+    extraKey: string;
+    boolKey: boolean;
+}
+
 ClsAdapter.createNamespace();
 
 ClsAdapter.getKoaMiddleware();
@@ -10,12 +15,14 @@ ClsAdapter.setOnContext('key', 'myValue');
 
 ClsAdapter.getRequestId();
 
-ClsAdapter.getContextStorage().requestId;
+ClsAdapter.getContextStorage<ContextDef>().requestId;
+ClsAdapter.getContextStorage<ContextDef>().extraKey;
+ClsAdapter.getContextStorage<ContextDef>().boolKey;
 
-ClsAdapter.addContextStorageToInput()({key: 'myValue'}).requestId;
-ClsAdapter.addContextStorageToInput()({key: 'myValue'}).key;
+ClsAdapter.addContextStorageToInput()({ key: 'myValue' }).requestId;
+ClsAdapter.addContextStorageToInput()({ key: 'myValue' }).key;
 
-ClsAdapter.addRequestIdToInput()({key: 'myValue'}).requestId;
-ClsAdapter.addRequestIdToInput()({key: 'myValue'}).key;
+ClsAdapter.addRequestIdToInput()({ key: 'myValue' }).requestId;
+ClsAdapter.addRequestIdToInput()({ key: 'myValue' }).key;
 
 ClsAdapter.destroyNamespace();
