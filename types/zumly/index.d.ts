e3be6d77539d418a5f736e6e364e5be4068d92b1
Edwin Narusberg <edwin.narusberg@systematicinc.com>
2020-08-13 07:20:50 -0400

    Fix exports to correspond with UMD

--- types/zumly/index.d.ts
+++ types/zumly/index.d.ts
@@ -3,61 +3,61 @@
 // Definitions by: edwinork <https://github.com/edwinork>
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
 
-declare global {
+/**
+ * Zumly powers your apps with a zoomable user interface (ZUI) taste.
+ */
+declare class zumly {
     /**
-     * Zumly powers your apps with a zoomable user interface (ZUI) taste.
+     * Creates a Zumly instance
+     * @params options
+     * @example
+     *  new Zumly({
+     *  mount: '.mount',
+     *  initialView: 'home',
+     *  views: {
+     *   home,
+     *   contact,
+     *   ...
+     *  }
+     *
      */
-    class zumly {
-        /**
-         * Creates a Zumly instance
-         * @params options
-         * @example
-         *  new Zumly({
-         *  mount: '.mount',
-         *  initialView: 'home',
-         *  views: {
-         *   home,
-         *   contact,
-         *   ...
-         *  }
-         *
-         */
-        constructor(options: Zumly.Options);
+    constructor(options: zumly.Options);
 
-        init(): Promise<void>;
+    init(): Promise<void>;
 
-        zoomLevel(): number;
+    zoomLevel(): number;
 
-        zoomIn(el: Element): Promise<void>;
+    zoomIn(el: Element): Promise<void>;
 
-        zoomOut(): void;
-    }
+    zoomOut(): void;
+}
 
-    namespace Zumly {
-        type WithRender = Record<string, unknown> & { render(): Promise<string> };
+declare namespace zumly {
+    type WithRender = Record<string, unknown> & { render(): Promise<string> };
 
-        interface Options {
-            // Mount DOM Element
-            mount: string;
-            // First rendered view name
-            initialView: string;
-            // Store of all view objects
-            views: Record<string, WithRender | string>;
-            // Custom transitions
-            transitions?: {
-                // Effects for background views
-                effects: Array<'blur' | 'sepia' | 'saturate'>;
-                // How new injected view is adapted. String. Default 'width'
-                cover: 'height' | 'width';
-                // Transition duration. Default '1s'
-                duration: string;
-                // Transition ease. Default 'ease-in-out'
-                ease: string;
-            };
-            // Activate debug notifications
-            debug?: boolean;
-        }
+    interface Options {
+        // Mount DOM Element
+        mount: string;
+        // First rendered view name
+        initialView: string;
+        // Store of all view objects
+        views: Record<string, WithRender | string>;
+        // Custom transitions
+        transitions?: {
+            // Effects for background views
+            effects: Array<'blur' | 'sepia' | 'saturate'>;
+            // How new injected view is adapted. String. Default 'width'
+            cover: 'height' | 'width';
+            // Transition duration. Default '1s'
+            duration: string;
+            // Transition ease. Default 'ease-in-out'
+            ease: string;
+        };
+        // Activate debug notifications
+        debug?: boolean;
     }
 }
 
-export default zumly;
+export = zumly;
+
+export as namespace zumly;
