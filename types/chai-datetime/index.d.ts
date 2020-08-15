6988953c92ac8679ee9da0b625abcd96a5855020
Oleksiy Pyltsov <valar999@gmail.com>
2020-08-15 18:01:26 +0300

    Add closeToTime()

--- types/chai-datetime/index.d.ts
+++ types/chai-datetime/index.d.ts
@@ -20,6 +20,7 @@ declare global {
             afterTime(date: Date): Assertion;
             beforeTime(date: Date): Assertion;
             equalTime(date: Date): Assertion;
+            closeToTime(date: Date, delta: number): Assertion;
             withinTime(dateFrom: Date, dateTo: Date): Assertion;
         }
 
