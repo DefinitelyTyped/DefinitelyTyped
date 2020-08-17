91ce3bab390b82e8968f6334b4dcd61e5734d05b
Nathan Shively-Sanders <293473+sandersn@users.noreply.github.com>
2020-08-17 16:25:59 -0700

    remove react-event-listener <3.1

--- types/react-event-listener/react-event-listener-tests.tsx
+++ types/react-event-listener/react-event-listener-tests.tsx
@@ -5,3 +5,5 @@ import EventListener, { withOptions } from "react-event-listener";
 <EventListener target={window} onResize={withOptions((ev: UIEvent) => { }, { passive: true, capture: true })} />;
 <EventListener target="window" onResize={() => { }}/>;
 <EventListener target={document} onSelectionChange={ev => { }} />;
+<EventListener target="document" onError={function() { this.documentElement; }} />;
+<EventListener target={window} onBlurCapture={function() { this.pageXOffset; }} />;
