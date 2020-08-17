3c12b03a276bf358440af9e7e66a717e5ee1f678
Nathan Bierema <nbierema@gmail.com>
2020-08-17 09:37:55 -0400

    Update Control.d.ts

--- types/react-select/src/components/Control.d.ts
+++ types/react-select/src/components/Control.d.ts
@@ -3,24 +3,24 @@ import { ComponentType, ReactNode, Ref as ElementRef } from 'react';
 import { borderRadius, colors, spacing } from '../theme';
 import { CommonProps, OptionTypeBase, PropsWithStyles } from '../types';
 
-interface State {
-  /** Whether the select is disabled. */
-  isDisabled: boolean;
-  /** Whether the select is focused. */
-  isFocused: boolean;
-}
-
 export type ControlProps<OptionType extends OptionTypeBase> = CommonProps<OptionType> &
-  PropsWithStyles &
-  State & {
-    /** Children to render. */
-    children: ReactNode,
-    innerRef: ElementRef<any>,
+  PropsWithStyles & {
+    className?: string;
+    innerRef: RefCallback<HTMLDivElement>;
     /** The mouse down event and the innerRef to pass down to the controller element. */
     innerProps: {
-      onMouseDown: (event: React.MouseEvent<HTMLElement>) => void,
-    },
-  };
+      onMouseDown: MouseEventHandler<HTMLDivElement>;
+      onTouchEnd: TouchEventHandler<HTMLDivElement>;
+    };
+    /** Whether the select is disabled. */
+    isDisabled: boolean;
+    /** Whether the select is focused. */
+    isFocused: boolean;
+    /** Whether the select is expanded. */
+    menuIsOpen: boolean;
+    /** Children to render. */
+    children: ReactNode;
+}
 
 export function css(state: State): React.CSSProperties;
 
