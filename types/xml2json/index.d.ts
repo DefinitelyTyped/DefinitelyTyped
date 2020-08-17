aadb41f73611cbaf61499f0195485c7beb4cbe1c
Igor Strebezhev <xamgore@users.noreply.github.com>
2020-08-17 16:58:14 +0300

    Pass a generic type argument to "toJSON" method
    xml2json.toJson(content) as Person // before
    xml2json.toJson<Person>(content)   // now, preferred

--- types/xml2json/index.d.ts
+++ types/xml2json/index.d.ts
@@ -7,7 +7,7 @@
 /// <reference types="node" />
 
 export function toJson(xml: string | Buffer, options?: { object?: false } & JsonOptions): string;
-export function toJson(xml: string | Buffer, options?: { object: true } & JsonOptions): { [key: string]: unknown };
+export function toJson<T extends { [key: string]: unknown }>(xml: string | Buffer, options?: { object: true } & JsonOptions): T;
 
 export function toXml(json: { [key: string]: unknown } | string | Buffer, options?: XmlOptions): string;
 
