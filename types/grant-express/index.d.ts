d9806c76d2c989c17161ca99643e7a568b7fcbf8
Beeno Tung <aabbcc1241@yahoo.com.hk>
2020-08-19 10:34:37 +0800

    update config files from `dts-gen --dt`

--- types/grant-express/index.d.ts
+++ types/grant-express/index.d.ts
@@ -1,9 +1,8 @@
-// Type definitions for grant-express 5.2.0
+// Type definitions for grant-express 5.3
 // Project: https://www.npmjs.org/package/grant-express
 // Definitions by:  Gordon Lau <https://gitlab.com/gordon-tecky>
 //                  Beeno Tung <https://github.com/beenotung>
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
-// TypeScript Version: 3.9
 
 import express = require('express');
 
