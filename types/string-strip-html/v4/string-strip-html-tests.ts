29958021f19e3a45af24a1df847c8b48e6b303a6
Brian Crosby <bcrosby@seek.com.au>
2020-08-19 17:40:36 +1000

    [string-strip-html] add v5 type definitions

--- types/string-strip-html/v4/string-strip-html-tests.ts
+++ types/string-strip-html/v4/string-strip-html-tests.ts
@@ -3,5 +3,3 @@ import stringStripHtml = require("string-strip-html");
 stringStripHtml("");
 stringStripHtml("", {});
 stringStripHtml("", { dumpLinkHrefsNearby: {} });
-
-stringStripHtml("Some text <b>and</b> text.").result;
