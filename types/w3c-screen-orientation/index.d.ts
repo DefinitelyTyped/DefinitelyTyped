a75d16605f1a15eedda54dc5cd21f69fa4a67faa
Nathan Shively-Sanders <293473+sandersn@users.noreply.github.com>
2020-08-19 08:48:09 -0700

    restore deleted DT header to w3c-screen-orientation

--- types/w3c-screen-orientation/index.d.ts
+++ types/w3c-screen-orientation/index.d.ts
@@ -1,3 +1,8 @@
+// Type definitions for non-npm package W3C Screen Orientation API 1.0
+// Project: https://www.w3.org/TR/screen-orientation/
+// Definitions by: Kenneth Rohde Christiansen <https://github.com/kenchris>
+// Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
+
 type ScreenOrientationLockType = "any" | "natural" | "landscape" | "portrait" | "portrait-primary" | "portrait-secondary" | "landscape-primary" | "landscape-secondary";
 
 interface ScreenOrientation extends EventTarget {
