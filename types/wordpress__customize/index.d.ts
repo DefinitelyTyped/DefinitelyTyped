a6bdbfad69f75eeddf8be4ba58bcc32a3027b5ee
Marek Dědič <developer@dedic.eu>
2020-08-19 09:45:35 +0200

    [wordpress__customize] Fixed index.d.ts errors

--- types/wordpress__customize/index.d.ts
+++ types/wordpress__customize/index.d.ts
@@ -1,10 +1,12 @@
-// Type definitions for @wordpress/customize
+// Type definitions for non-npm package @wordpress/customize-browser 5.5
 // Project: https://core.trac.wordpress.org/browser/trunk/src/js/_enqueues/wp/customize
 // Definitions by: Marek Dědič <https://github.com/marekdedic>
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
 // TypeScript Version: 3.9
 
-import {Customize} from './Customize';
+/// <reference types="jquery" />
+
+import { Customize } from './Customize';
 
 declare const customize: Customize;
 
