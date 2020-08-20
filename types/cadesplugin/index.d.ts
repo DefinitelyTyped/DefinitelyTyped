3edee7b28995ba889fe83999751d887c28b6cd02
Nathan Shively-Sanders <293473+sandersn@users.noreply.github.com>
2020-08-20 15:26:56 -0700

    restore header

--- types/cadesplugin/index.d.ts
+++ types/cadesplugin/index.d.ts
@@ -1,3 +1,9 @@
+// Type definitions for cadesplugin 2.1
+// Project: https://www.npmjs.com/package/cadesplugin
+// Definitions by: Delagen <https://github.com/Delagen>,
+//                 kvmi <https://github.com/kmvi>
+// Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
+
 ///<reference path="./common.d.ts"/>
 
 ///<reference path="./capicom.d.ts"/>
