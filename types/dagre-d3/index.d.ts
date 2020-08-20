5687cb2d13c94429a7c76e1ae61bc6a5a42fa53d
Matthew Simmons <simmonmt@google.com>
2020-08-20 07:48:52 -0700

    Use major.minor version instead of patch version

--- types/dagre-d3/index.d.ts
+++ types/dagre-d3/index.d.ts
@@ -1,4 +1,4 @@
-// Type definitions for dagre-d3.core.js 0.6.1
+// Type definitions for dagre-d3.core.js 0.6
 // Project: https://github.com/dagrejs/dagre-d3
 // Definitions by: Mark Wong Siang Kai <https://github.com/markwongsk>
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
