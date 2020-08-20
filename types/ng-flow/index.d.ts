86834d105d402156f16724ea159014222d11a6bb
Nathan Shively-Sanders <293473+sandersn@users.noreply.github.com>
2020-08-20 16:01:05 -0700

    restore header

--- types/ng-flow/index.d.ts
+++ types/ng-flow/index.d.ts
@@ -1,3 +1,8 @@
+// Type definitions for ng-flow
+// Project: https://github.com/flowjs/ng-flow
+// Definitions by: Ryan McNamara <https://github.com/ryan10132>
+//                 Martin Nuc <https://github.com/martinnuc>
+// Definitions: https://github.com/DefinitelyTyped/DefinitelyTypedi
 /// <reference types="flowjs" />
 
 import * as angular from 'angular';
