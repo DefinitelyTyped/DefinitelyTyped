8ad9b708b5399335225df21fe692b5e35d377758
Pete Gonzalez <4673363+octogonz@users.noreply.github.com>
2020-08-20 16:51:48 -0700

    Add types for heft-jest package

---
+++ types/heft-jest/heft-jest-tests.ts
@@ -0,0 +1,5 @@
+/// <reference types=".">
+
+class X { }
+
+mocked(X).mockClear();
---
+++ types/heft-jest/index.d.ts
@@ -0,0 +1,19 @@
+// Type definitions for heft-jest 1.0
+// Project: https://github.com/octogonz/heft-jest
+// Definitions by: Pete Gonzalez <https://github.com/octogonz>
+// Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
+
+/// <reference types="jest" />
+
+
+declare namespace mocked {
+  // Todo - fill out this definition in the next release.
+  type Mocked<T> = any;
+}
+
+/**
+ * This is a global equivalent of the mocked() API from ts-jest:
+ * https://kulshekhar.github.io/ts-jest/user/test-helpers
+ */
+declare function mocked<T>(item: T, deep?: false): mocked.Mocked<T>;
+declare function mocked<T>(item: T, deep: true): mocked.Mocked<T>;
