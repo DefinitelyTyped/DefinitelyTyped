5efab71a4aefffd6418be30c6985333faa6b9768
kemicofa <einar.ase.91@gmail.com>
2020-08-21 21:01:19 +0200

    added first

--- types/imagekit/imagekit-tests.ts
+++ types/imagekit/imagekit-tests.ts
@@ -4,6 +4,4 @@ const imageKit = new ImageKit({
     privateKey: 'private_1234',
     publicKey: 'public_1234',
     urlEndpoint: 'https://ik.imagekit.io/your_imagekit_id/'
-});
-
-// TODO
+}); // $ExpectType ImageKit
