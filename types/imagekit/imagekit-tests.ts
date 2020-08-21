e2dadcdcab003be63a4256d4089822836920718e
kemicofa <einar.ase.91@gmail.com>
2020-08-21 21:38:36 +0200

    resolved lint errors

--- types/imagekit/imagekit-tests.ts
+++ types/imagekit/imagekit-tests.ts
@@ -4,4 +4,25 @@ const imageKit = new ImageKit({
     privateKey: 'private_1234',
     publicKey: 'public_1234',
     urlEndpoint: 'https://ik.imagekit.io/your_imagekit_id/'
-}); // $ExpectType ImageKit
+});
+
+imageKit.url({
+    path: '/some/path',
+    transformationPosition: 'path'
+});
+
+imageKit.url({
+    src: '/some/src',
+    transformationPosition: 'path'
+});
+
+imageKit.url({
+    path: '/some/path',
+    src: '/some/src',
+    transformationPosition: 'path'
+});
+
+imageKit.bulkDeleteFiles(['1', '2', '3'], (error, bulkDeleteFilesResponse) => {});
+
+imageKit.bulkDeleteFiles(['1', '2', '3'])
+.then((bulkDeleteFilesResponse) => {});
