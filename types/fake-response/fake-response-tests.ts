a00b1ff2a773ca1e2f23c8bcb5401c3ef7cb8f38
Siva <Mycount.siva@gmail.com>
2020-08-21 23:21:19 +0530

    Update types/fake-response/fake-response-tests.ts
    Ya Pretty good
    
    Co-authored-by: Piotr Błażejewicz (Peter Blazejewicz) <peterblazejewicz@users.noreply.github.com>

--- types/fake-response/fake-response-tests.ts
+++ types/fake-response/fake-response-tests.ts
@@ -1,11 +1,42 @@
 import { FakeResponse } from 'fake-response';
+import { Config, Injectors } from 'fake-response/model';
 
 const db = {
     'hello,world': 'Hello World',
 };
 
-const config = {
-    port: 4000,
+const config: Config = {
+    port: 3000,
+    rootPath: './', // root path of the db to get the absolute path of the relative assets
+    middleware: {
+        func: ({ next }) => {
+            next();
+        },
+        excludeRoutes: [],
+        override: false, // if true common middleware runs first before the specific middleware
+    },
+    delay: {
+        time: 0, // must give in milliseconds
+        excludeRoutes: [],
+        override: false, // if true overrides the specific delay with the common delay
+    },
+    env: '',
+    proxy: {
+        hello: 'helloWorld',
+    },
+    excludeRoutes: [],
 };
 
-new FakeResponse(db, config).launchServer();
+const globals = {
+    value: false, // store the default value here
+};
+
+const Injectors: Injectors[] = [
+    {
+        middleware: ({ res }) => res.send('This is an injected response  with 300ms delay'),
+        delay: 300,
+        routes: ['/injector'],
+    },
+];
+
+new FakeResponse(db, config, globals).launchServer();
