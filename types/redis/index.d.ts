0f2514fb6d9f6767549c03b5fab335bfba4ec523
Nathan Shively-Sanders <293473+sandersn@users.noreply.github.com>
2020-08-21 09:27:22 -0700

    Make ts3.1 the default for redis (#46956)
    * Make ts3.1 the default
    
    * remove trailing space

--- types/redis/index.d.ts
+++ types/redis/index.d.ts
@@ -30,7 +30,7 @@ export interface RetryStrategyOptions {
     attempt: number;
 }
 
-export type RetryStrategy = (options: RetryStrategyOptions) => number | Error | undefined;
+export type RetryStrategy = (options: RetryStrategyOptions) => number | Error | unknown;
 
 export interface ClientOpts {
     host?: string;
@@ -105,6 +105,7 @@ export interface OverloadedSetCommand<T, U, R> {
     (key: string, arg1: T, arg2: T, cb?: Callback<U>): R;
     (key: string, arg1: T | { [key: string]: T } | T[], cb?: Callback<U>): R;
     (key: string, ...args: Array<T | Callback<U>>): R;
+    (args: [string, ...T[]], cb?: Callback<U>): R;
 }
 
 export interface OverloadedLastCommand<T1, T2, U, R> {
@@ -482,14 +483,14 @@ export interface Commands<R> {
     /**
      * Get the value of a hash field.
      */
-    hget(key: string, field: string, cb?: Callback<string | null>): R;
-    HGET(key: string, field: string, cb?: Callback<string | null>): R;
+    hget(key: string, field: string, cb?: Callback<string>): R;
+    HGET(key: string, field: string, cb?: Callback<string>): R;
 
     /**
      * Get all fields and values in a hash.
      */
-    hgetall(key: string, cb?: Callback<{ [key: string]: string } | null>): R;
-    HGETALL(key: string, cb?: Callback<{ [key: string]: string } | null>): R;
+    hgetall(key: string, cb?: Callback<{ [key: string]: string }>): R;
+    HGETALL(key: string, cb?: Callback<{ [key: string]: string }>): R;
 
     /**
      * Increment the integer value of a hash field by the given number.
@@ -1257,15 +1258,17 @@ export function createClient(options?: ClientOpts): RedisClient;
 
 export function print(err: Error | null, reply: any): void;
 
-export class RedisError extends Error { }
+export class RedisError extends Error {
+    name: string;
+}
 export class ReplyError extends RedisError {
     command: string;
-    args?: any[];
+    args?: unknown[];
     code: string;
 }
 export class AbortError extends RedisError {
     command: string;
-    args?: any[];
+    args?: unknown[];
     code?: string;
 }
 export class ParserError extends RedisError {
