57da660f18741e6779aa69e4562f705a4a9f2036
Dmitry Demensky <dmitry@demensky.com>
2020-08-21 10:30:26 +0300

    [twitch-ext] Add to "Definitions by"

--- types/twitch-ext/index.d.ts
+++ types/twitch-ext/index.d.ts
@@ -2,6 +2,7 @@
 // Project: https://dev.twitch.tv/docs/extensions/reference/#javascript-helper
 // Definitions by: Benedict Etzel <https://github.com/beheh>
 //                 Federico Della Rovere <https://github.com/FedeDR>
+//                 Dmitry Demensky <https://github.com/demensky>
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
 // TypeScript Version: 2.4
 
