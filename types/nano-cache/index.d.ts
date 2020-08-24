b7540b887a07d52f748e04582d21abcb8e0051e8
Ross Coundon <ross.coundon@leadent.digital>
2020-08-24 19:16:17 +0100

    Add types for nano-cache

---
+++ types/nano-cache/index.d.ts
@@ -0,0 +1,57 @@
+// Type definitions for nano-cache
+// Project: https://github.com/akhoury/nano-cache
+// Definitions by: Ross Coundon <https://github.com/rcoundon>
+// Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
+
+interface SIZE {
+    GB: number;
+    MB: number;
+    KB: number;
+}
+type Info = {
+    key: string;
+    hits: number;
+    accessed: number;
+    updated: number;
+    expires: number;
+    value: unknown;
+    bytes: number;
+    ttl: number;
+    compressed: boolean;
+    cost: number;
+    limit: number;
+};
+type Stats = {
+    count: number;
+    age: number;
+    hits: number;
+    evictions: number;
+    misses: number;
+    bytes: number;
+};
+type Options = {
+    ttl?: number;
+    limit?: number;
+    bytes?: number;
+    compress?: boolean;
+    minFreeMem?: number;
+    maxEvictBytes?: number;
+};
+
+declare class NanoCache {
+    constructor(options?: Options);
+    get<T>(key: T): unknown;
+    set<T>(key: T, value: unknown, options?: Options): void;
+    delete<T>(key: T): unknown;
+    clear(): void;
+    clearExpired(): void;
+    isTTLExpired<T>(key: T): boolean;
+    isLimitReached<T>(key: T): boolean;
+    info<T>(key: T): Info;
+    stats(): Stats;
+    static SIZE: SIZE;
+}
+
+declare module 'nano-cache' {
+    export = NanoCache;
+}
