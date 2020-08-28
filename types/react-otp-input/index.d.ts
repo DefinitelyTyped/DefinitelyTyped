c66217e60e5958e1c8b6a0577adc1e90cc66e11c
Anup Bhatkhande <anupb239@gmail.com>
2020-08-28 22:05:51 +0530

    Remove minor version from top definitions

--- types/react-otp-input/index.d.ts
+++ types/react-otp-input/index.d.ts
@@ -1,29 +1,23 @@
-// Type definitions for react-otp-input 2.0.2
+// Type definitions for react-otp-input 2.0
 // Project: https://github.com/devfolioco/react-otp-input
 // Definitions by: Anup Bhatkhande <https://github.com/anupbui>
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
-// TypeScript Version: 3.7
-
 import * as React from 'react';
-import { Dispatch } from 'react';
-import { SetStateAction } from 'react';
 
-declare namespace ReactOTPInput {
-    interface OTPInputProps {
-        value: number | string;
-        onChange: Dispatch<SetStateAction<string | number>>;
-        numInputs: number;
-        separator?: JSX.Element;
-        isDisabled?: boolean;
-        shouldAutoFocus?: boolean;
-        hasErrored?: boolean;
-        isInputNum?: boolean;
-        containerStyle?: string | React.CSSProperties;
-        inputStyle?: string | React.CSSProperties;
-        focusStyle?: string | React.CSSProperties;
-        disabledStyle?: string | React.CSSProperties;
-        errorStyle?: string | React.CSSProperties;
-    }
+export interface OTPInputProps {
+    value: number | string;
+    onChange: any;
+    numInputs: number;
+    separator?: JSX.Element;
+    isDisabled?: boolean;
+    shouldAutoFocus?: boolean;
+    hasErrored?: boolean;
+    isInputNum?: boolean;
+    containerStyle?: string | React.CSSProperties;
+    inputStyle?: string | React.CSSProperties;
+    focusStyle?: string | React.CSSProperties;
+    disabledStyle?: string | React.CSSProperties;
+    errorStyle?: string | React.CSSProperties;
 }
 
-export default class extends React.Component<ReactOTPInput.OTPInputProps> { }
+export default class extends React.Component<OTPInputProps> { }
