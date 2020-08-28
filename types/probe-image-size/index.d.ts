35af0aad68d915d4e72153c4e51067ad013a30f4
Piotr Błażejewicz <peterblazejewicz@users.noreply.github.com>
2020-08-28 22:36:20 +0200

    update(probe-image-size): support for older api and minor changes
    - support for still supported fallback singatures
    - new methods and types added
    - tests amended
    - maintainer added
    
    https://github.com/nodeca/probe-image-size#api
    
    Thanks!

--- types/probe-image-size/index.d.ts
+++ types/probe-image-size/index.d.ts
@@ -1,9 +1,17 @@
 // Type definitions for probe-image-size 5.0
 // Project: https://github.com/nodeca/probe-image-size#readme
 // Definitions by: Jinesh Shah <https://github.com/jineshshah36>
+//                 Piotr Błażejewicz <https://github.com/peterblazejewicz>
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
+/// <reference types="node" />
 
+/**
+ * Get image size without full download. Supported image types: JPG, GIF, PNG, WebP, BMP, TIFF, SVG, PSD.
+ */
+declare function probe(source: string, opts: probe.ProbeOptions, callback: probe.ProbeCallback): void;
 declare function probe(source: string, opts?: probe.ProbeOptions): Promise<probe.ProbeResult>;
+declare function probe(source: string | NodeJS.ReadableStream, callback: probe.ProbeCallback): void;
+declare function probe(source: NodeJS.ReadableStream): Promise<probe.ProbeResult>;
 
 declare namespace probe {
     interface ProbeResult {
@@ -21,6 +29,15 @@ declare namespace probe {
         retries?: number;
         timeout?: number;
     }
+
+    interface ProbeError extends Error {
+        code?: 'ECONTENT';
+        status?: number;
+    }
+
+    type ProbeCallback = (err: ProbeError | null, result: ProbeResult) => void;
+
+    function sync(data: Buffer): ProbeResult | null;
 }
 
 export = probe;
