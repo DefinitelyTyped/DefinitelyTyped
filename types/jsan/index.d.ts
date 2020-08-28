6b2536c4680647b8d36d7549def3a69974d16490
Nathan Bierema <nbierema@gmail.com>
2020-08-28 14:52:45 -0400

    [jsan] Add jsan types

---
+++ types/jsan/index.d.ts
@@ -0,0 +1,42 @@
+// Type definitions for jsan 3.1
+// Project: https://github.com/kolodny/jsan
+// Definitions by: Nathan Bierema <https://github.com/me>
+// Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
+
+interface Options {
+    date?: boolean;
+    function?: boolean;
+    regex?: boolean;
+    undefined?: boolean;
+    error?: boolean;
+    symbol?: boolean;
+    map?: boolean;
+    set?: boolean;
+    nan?: boolean;
+    infinity?: boolean;
+    refs?: boolean;
+}
+
+declare function stringify(
+    value: unknown,
+    replacer?: (key: string, value: unknown) => unknown,
+    space?: string | number,
+    _options?: Options | boolean
+): string;
+declare function stringify(
+    value: unknown,
+    replacer?: (number | string)[] | null,
+    space?: string | number,
+    _options?: Options | boolean
+): string;
+
+declare function parse(
+    text: string,
+    reviver?: (key: string, value: unknown) => unknown
+): unknown;
+
+declare const _default: {
+    stringify: typeof stringify,
+    parse: typeof parse,
+};
+export = _default;
