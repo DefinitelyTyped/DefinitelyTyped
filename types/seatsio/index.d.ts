5b7f2a8512a1057422dd20e6a9fdf345993fbea7
mroloux <matti@seats.io>
2020-08-28 10:01:23 +0200

    Tweaked authors

--- types/seatsio/index.d.ts
+++ types/seatsio/index.d.ts
@@ -1,7 +1,7 @@
 // Type definitions for seatsio 1.0
 // Project: https://www.seats.io
 // Definitions by: seatsio <https://github.com/seatsio>
-//                 Peter <https://github.com/PeterDeKok>
+//                 Peter de Kok <https://github.com/PeterDeKok>
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
 
 declare var seatsio: Seatsio;
