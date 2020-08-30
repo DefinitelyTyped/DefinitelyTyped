fd5862dab139132685d3eb93f1831e5981598b53
Nikolas Papaioannou <nikolas.papaioannou@gmail.com>
2020-08-30 21:41:34 +0200

    definition updated, need to checkout the tests closer

--- types/splitpanes/splitpanes-tests.ts
+++ types/splitpanes/splitpanes-tests.ts
@@ -1,17 +1,22 @@
 import Vue from 'vue';
-import splitPanes from 'splitpanes';
+import { Splitpane, Pane } from 'splitpanes';
+
 new Vue({
-  el: '#app',
-  data: {},
-  components: {
-    'split-panes': splitPanes,
-  },
-  template: `<split-panes class="default-theme" horizontal @resize="horizontalResize($event)" :dblClickSplitter="false" :push-other-panes="false">
-    <span splitpanes-min="2" splitpanes-size="35" splitpanes-max="85">
-        A
-     </span>
-    <span splitpanes-min="2" splitpanes-size="65" splitpanes-max="85">
-        B
-     </span>
-  </split-panes>`,
+    el: '#app',
+    data: {},
+    components: {
+        Splitpane,
+        Pane,
+    },
+    template: `
+        <Splitpane class="default-theme" horizontal @resize="horizontalResize($event)" :dblClickSplitter="false"
+                   :push-other-panes="false">
+        <Pane splitpanes-min="2" splitpanes-size="35" splitpanes-max="85">
+            A
+        </Pane>
+        <Pane splitpanes-min="2" splitpanes-size="65" splitpanes-max="85">
+            B
+        </Pane>
+        </Splitpane>
+    `,
 });
