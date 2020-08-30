a7bee7b0a6fa3c533bb5d0673c779a952d740734
pofider <honza.pofider@seznam.cz>
2020-08-30 21:14:44 +0200

    change definitions by url to user pofider

--- types/jsreport/index.d.ts
+++ types/jsreport/index.d.ts
@@ -1,6 +1,6 @@
 // Type definitions for jsreport 2.9
 // Project: https://github.com/jsreport/jsreport
-// Definitions by: pofider <https://github.com/jsreport>
+// Definitions by: pofider <https://github.com/pofider>
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
 
 // import all availible types for jsreport included extensions
