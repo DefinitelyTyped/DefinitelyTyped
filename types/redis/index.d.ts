8cbda3cd092a43c6231c00f90d7307f8e7bd247b
mihai.pruteanu <mihai.pruteanu@accenture.com>
2020-08-30 22:28:29 +0200

    Fixed lint errors

--- types/redis/index.d.ts
+++ types/redis/index.d.ts
@@ -187,26 +187,26 @@ export interface ClientOpts {
      * @default function
      * @see interface `RetryStrategyOptions`
      * @example
-        const client = redis.createClient({
-            retry_strategy: function(options) {
-            if (options.error && options.error.code === "ECONNREFUSED") {
-                // End reconnecting on a specific error and flush all commands with
-                // a individual error
-                return new Error("The server refused the connection");
-            }
-            if (options.total_retry_time > 1000 * 60 * 60) {
-                // End reconnecting after a specific timeout and flush all commands
-                // with a individual error
-                return new Error("Retry time exhausted");
-            }
-            if (options.attempt > 10) {
-                // End reconnecting with built in error
-                return undefined;
-            }
-            // reconnect after
-            return Math.min(options.attempt * 100, 3000);
-            }
-        });
+     * const client = redis.createClient({
+     *   retry_strategy: function(options) {
+     *   if (options.error && options.error.code === "ECONNREFUSED") {
+     *     // End reconnecting on a specific error and flush all commands with
+     *     // a individual error
+     *     return new Error("The server refused the connection");
+     *   }
+     *   if (options.total_retry_time > 1000 * 60 * 60) {
+     *     // End reconnecting after a specific timeout and flush all commands
+     *     // with a individual error
+     *     return new Error("Retry time exhausted");
+     *   }
+     *   if (options.attempt > 10) {
+     *     // End reconnecting with built in error
+     *     return undefined;
+     *   }
+     *   // reconnect after
+     *   return Math.min(options.attempt * 100, 3000);
+     *   }
+     * });
      */
     retry_strategy?: RetryStrategy;
 }
