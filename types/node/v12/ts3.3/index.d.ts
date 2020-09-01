829d4f84be4b4b8f723c68f6fdc9ca1890920614
Nathan Shively-Sanders <293473+sandersn@users.noreply.github.com>
2020-09-01 09:11:15 -0700

    Put current version of node in the root
    Previously, the code that supports the oldest version of Typescript was
    in the root: 3.1 and below. Newer versions were is ts*/ subdirectories.
    
    This PR puts the newest version in the root, and older versions --
    usually 3.7 and below -- in ts*/ subdirectories. This is possible
    because all supported versions of the Typescript now understand the
    typesVersions property in package.json.
    
    This PR needs a new version of DefinitelyTyped-tools and dtslint, which
    I will ship soon. It also needs to be brought up to date with master.

---
+++ types/node/v12/ts3.3/index.d.ts
@@ -0,0 +1,12 @@
+// NOTE: These definitions support NodeJS and TypeScript 3.2.
+// This is requried to enable globalThis support for global in ts3.4 without causing errors
+// This is requried to enable typing assert in ts3.7 without causing errors
+// Typically type modifiations should be made in base.d.ts instead of here
+
+/// <reference path="base.d.ts" />
+
+// tslint:disable-next-line:no-bad-reference
+/// <reference path="../ts3.1/assert.d.ts" />
+
+// tslint:disable-next-line:no-bad-reference
+/// <reference path="../ts3.1/globals.global.d.ts" />
