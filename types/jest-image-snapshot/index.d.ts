ff86844d741cca9bbc72564f44ac84a29cdf4a2d
Wessel Kuipers <wesselkuipers@gmail.com>
2020-09-01 15:28:32 +0200

    Fix type definition comment version

--- types/jest-image-snapshot/index.d.ts
+++ types/jest-image-snapshot/index.d.ts
@@ -1,4 +1,4 @@
-// Type definitions for jest-image-snapshot 4.1.0
+// Type definitions for jest-image-snapshot 4.1
 // Project: https://github.com/americanexpress/jest-image-snapshot#readme
 // Definitions by: Janeene Beeforth <https://github.com/dawnmist>
 //                 erbridge <https://github.com/erbridge>
