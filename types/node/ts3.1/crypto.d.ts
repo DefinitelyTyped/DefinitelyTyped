2cc01a442e1e682534ab9b4f59cfbfba02908cd4
fuzzc0re <60238448+fuzzc0re@users.noreply.github.com>
2020-09-02 21:49:24 +0000

    Fixed trailing whitespace

--- types/node/ts3.1/crypto.d.ts
+++ types/node/ts3.1/crypto.d.ts
@@ -682,8 +682,8 @@ declare module "crypto" {
     function verify(algorithm: string | null | undefined, data: NodeJS.ArrayBufferView, key: KeyLike | VerifyKeyWithOptions, signature: NodeJS.ArrayBufferView): boolean;
 
     /**
-     * Computes the Diffie-Hellman secret based on a privateKey and a publicKey. 
-     * Both keys must have the same asymmetricKeyType, which must be one of 
+     * Computes the Diffie-Hellman secret based on a privateKey and a publicKey.
+     * Both keys must have the same asymmetricKeyType, which must be one of
      * 'dh' (for Diffie-Hellman), 'ec' (for ECDH), 'x448', or 'x25519' (for ECDH-ES).
      */
     function diffieHellman(options: {
