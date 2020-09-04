a8f5877033d44b7680a4dcbd9e99a22fdd9809f3
Elizabeth Samuel <elizs@microsoft.com>
2020-09-04 09:41:26 -0700

    [custom-functions-runtime] (Test) Test

--- types/custom-functions-runtime/custom-functions-runtime-tests.ts
+++ types/custom-functions-runtime/custom-functions-runtime-tests.ts
@@ -64,3 +64,32 @@ function stockPriceStream(ticker: string,
 }
 
 declare function pause(ms: number): Promise<undefined>;
+
+function throwErrors(parameter: any) {
+    const customError = new CustomFunctions.Error(CustomFunctions.ErrorCode.invalidValue, "The parameter can only contain lowercase characters.");
+    switch (parameter) {
+        case (parameter === 0):
+          return CustomFunctions.ErrorCode.invalidValue;
+          break;
+        case (parameter === 1):
+          throw CustomFunctions.ErrorCode.notAvailable;
+          break;
+        case (parameter === 2):
+          throw CustomFunctions.ErrorCode.nullReference;
+          break;
+        case (parameter === 3):
+          throw CustomFunctions.ErrorCode.invalidReference;
+          break;
+        case (parameter === 4):
+          throw CustomFunctions.ErrorCode.invalidNumber;
+          break;
+        case (parameter === 5):
+          throw CustomFunctions.ErrorCode.invalidName;
+          break;
+        case (parameter === 6):
+          throw CustomFunctions.ErrorCode.divisionByZero;
+          break;
+        case (parameter === "A"):
+          return customError;
+    }
+}
--- types/custom-functions-runtime/index.d.ts
+++ types/custom-functions-runtime/index.d.ts
@@ -1,4 +1,4 @@
-// Type definitions for non-npm package Custom Functions 1.5
+// Type definitions for non-npm package custom-functions-runtime 1.5
 // Project: https://github.com/OfficeDev/office-js
 // Definitions by: OfficeDev <https://github.com/OfficeDev>,
 //                 Adam Krantz <https://github.com/akrantz>,
@@ -9,7 +9,7 @@
 // TypeScript Version: 2.4
 
 /*
-office-js
+custom-functions-runtime
 Copyright (c) Microsoft Corporation
 */
 
@@ -27,6 +27,24 @@ declare namespace CustomFunctions {
     function associate(mappings: { [key: string]: Function }): void;
 
     /**
+     * Use this class to handle errors and write custom error messages.
+     * [Api set: CustomFunctionsRuntime 1.2]
+     */
+    class Error {
+        constructor(errorCode: ErrorCode, errorMessage?: string);
+         /**
+          * The error code returned by your custom function.
+          * [Api set: CustomFunctionsRuntime 1.2]
+          */
+        errorCode: ErrorCode;
+         /**
+          * Your custom error message, such as "This stock price is unavailable". Custom messages are only available with certain error codes.
+          * [Api set: CustomFunctionsRuntime 1.2]
+          */
+        errorMessage?: string;
+    }
+
+    /**
      * Provides information about the invocation of a custom function.
      */
     interface Invocation {
@@ -37,8 +55,19 @@ declare namespace CustomFunctions {
          * `{ "requiresAddress": true }`
          *
          * If the metadata JSON file is being generated from JSDoc comments, include the tag `@requiresAddress`.
+         * [Api set: CustomFunctionsRuntime 1.1]
          */
         address?: string;
+        /**
+         * The range addresses where the function parameters are located, if requested, otherwise undefined.
+         *
+         * To request the parameter addresses for the function, in the metadata JSON file, the function options should specify:
+         * `{ "requiresParameterAddresses": true }`
+         *
+         * If the metadata JSON file is being generated from JSDoc comments, include the tag `@requiresParameterAddresses`.
+         * [Api set: CustomFunctionsRuntime 1.1]
+         */
+        parameterAddresses?: string;
     }
 
     /**
@@ -53,6 +82,7 @@ declare namespace CustomFunctions {
     interface CancelableInvocation extends Invocation {
         /**
          * Event handler called when the custom function is canceled.
+         * [Api set: CustomFunctionsRuntime 1.1]
          */
         onCanceled: () => void;
     }
@@ -67,7 +97,63 @@ declare namespace CustomFunctions {
     interface StreamingInvocation<ResultType> extends CancelableInvocation {
         /**
          * Set the result for the custom function. May be called more than once.
+         * [Api set: CustomFunctionsRuntime 1.1]
          */
         setResult: (value: ResultType | Error) => void;
+    }
+
+    /**
+     * Error codes for custom functions. These errors will appear in the cell that invoked the function.
+     */
+    enum ErrorCode {
+        /**
+         *
+         * This error code indicates that a value used in the function is of the wrong data type.
+         * A custom error message can also be used in place of the error code, if desired.
+         * [Api set: CustomFunctionsRuntime 1.3]
+         */
+        invalidValue = "#VALUE!",
+        /**
+         *
+         * This error code indicates that the function or service isn't available.
+         * A custom error message can also be used in place of the error code, if desired.
+         * [Api set: CustomFunctionsRuntime 1.3]
+         */
+        notAvailable = "#N/A",
+        /**
+         *
+         * This error code indicates that the function used is dividing by zero or empty cells.
+         * A custom error message can't be used.
+         * [Api set: CustomFunctionsRuntime 1.3]
+         */
+        divisionByZero = "#DIV/0!",
+        /**
+         *
+         * There is a problem with a number used in the function.
+         * A custom error message can't be used.
+         * [Api set: CustomFunctionsRuntime 1.3]
+         */
+        invalidNumber = "#NUM!",
+        /**
+         *
+         * The ranges in the function don't intersect.
+         * A custom error message can't be used.
+         * [Api set: CustomFunctionsRuntime 1.3]
+         */
+        nullReference = "#NULL!",
+        /**
+         *
+         * There is a typo in the function name.
+         * A custom error message can't be used.
+         * [Api set: CustomFunctionsRuntime 1.3]
+         */
+        invalidName = "#NAME?",
+        /**
+         *
+         * The function refers to an invalid cell.
+         * A custom error message can't be used.
+         * [Api set: CustomFunctionsRuntime 1.3]
+         */
+        invalidReference = "#REF!"
     }
 }
