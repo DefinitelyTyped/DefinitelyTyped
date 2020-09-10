d708dc44c2c863924b4ab28002056bf802886b26
Jeremy Hull <sourdrums@gmail.com>
2020-09-10 01:03:01 -0700

    Update file information

--- types/koa-pino-logger/index.d.ts
+++ types/koa-pino-logger/index.d.ts
@@ -1,6 +1,7 @@
-// Type definitions for koa-pino-logger 2.1
+// Type definitions for koa-pino-logger 3.0
 // Project: https://github.com/pinojs/koa-pino-logger, https://github.com/davidmarkclements/koa-pino-logger
 // Definitions by: Cameron Yan <https://github.com/khell>
+//                 Jeremy Hull <https://github.com/sourrust>
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
 // TypeScript Version: 2.7
 
