da32bb9d3ba70ec99edba5ff830b251f1ea700a9
A O <alex.oshchepkov@gmail.com>
2020-09-10 19:35:09 +0500

    add test for partial record case

--- types/ramda/test/mapObjIndexed-tests.ts
+++ types/ramda/test/mapObjIndexed-tests.ts
@@ -1,13 +1,13 @@
 import * as R from 'ramda';
 
 () => {
-  const values = { x: 1, y: 2, z: 3 };
+    const values = { x: 1, y: 2, z: 3 };
 
-  function prependKeyAndDouble(num: number, key: string, obj: any) {
-    return key + num * 2;
-  }
+    function prependKeyAndDouble(num: number, key: string, obj: any) {
+        return key + num * 2;
+    }
 
-  R.mapObjIndexed(prependKeyAndDouble, values); // => { x: 'x2', y: 'y4', z: 'z6' }
+    R.mapObjIndexed(prependKeyAndDouble, values); // => { x: 'x2', y: 'y4', z: 'z6' }
 };
 
 () => {
@@ -52,6 +52,39 @@ import * as R from 'ramda';
     }, testObject);
 
     // keys of testObject should be detected, and their values should be `number`.
-    const helloResult = mapped.hello.toFixed();
+    const helloResult = mapped.hello.toFixed();    
+    console.log(mapped, helloResult);
+};
+
+() => {
+    type PartialRecord<K extends keyof any, T> = {
+        [P in K]?: T;
+    };
+    type TestKeys = 'hello' | 'test';
+    const testObject: PartialRecord<TestKeys, string> = {
+        hello: 'world',
+    };
+
+    const mapped = R.mapObjIndexed(function test(value, key) {
+        function unreachableCase(v: never): never {
+            throw new Error('not possible');
+        }
+
+        let returnValue: number;
+        // type of key should be inferred as `TestKeys` (not `string`).
+        if (key === 'hello') {
+            returnValue = 42;
+        } else if (key === 'test') {
+            returnValue = 13;
+        } else {
+            return unreachableCase(key);
+        }
+
+        const test1 = value.charAt(0);
+        return returnValue;
+    }, testObject);
+
+    // keys of testObject should be detected, and their values should be `number`.
+    const helloResult = mapped.hello?.toFixed();    
     console.log(mapped, helloResult);
 };
