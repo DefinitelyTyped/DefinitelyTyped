5d1bc3970a2522555f2be46492a268b2d33eace3
dalofeco <me@dalofeco.com>
2020-09-09 22:43:33 -0400

    Add onChange selection typing and enforced required prop for ContentSwitcher

--- types/carbon-components-react/lib/components/ContentSwitcher/ContentSwitcher.d.ts
+++ types/carbon-components-react/lib/components/ContentSwitcher/ContentSwitcher.d.ts
@@ -3,9 +3,16 @@ import { ReactDivAttr } from "../../../typings/shared";
 
 interface InheritedProps extends Omit<ReactDivAttr, "role"> { }
 
+interface ContentSwitcherOnChangeSelection {
+    index: number;
+    name: string;
+    text: string;
+}
+
 export interface ContentSwitcherProps extends InheritedProps {
     selectedIndex?: number,
     selectionMode?: "automatic" | "manual";
+    onChange: (selection: ContentSwitcherOnChangeSelection) => void;
 }
 
 declare class ContentSwitcher extends React.Component<ContentSwitcherProps> {}
