fe88c8c7f45bf37684417102fdb652184db0fe2d
Rumon <crash.msr@gmail.com>
2020-09-11 23:47:38 +0600

    [@paypal/payouts-sdk] Added type definition

---
+++ types/paypal__payouts-sdk/paypal__payouts-sdk-tests.ts
@@ -0,0 +1,56 @@
+import { Core, Payouts } from 'paypal__payouts-sdk';
+
+declare const id: string;
+declare const secret: string;
+
+declare const currency: string;
+declare const value: string;
+declare const receiver: string;
+
+const envLive = new Core.LiveEnvironment(id, secret);
+const client = new Core.PayPalHttpClient(envLive);
+
+const reqPost = new Payouts.PayoutsPostRequest();
+reqPost.headers.Authorization = envLive.authorizationString();
+reqPost.requestBody({ sender_batch_header: {}, items: [{ amount: { currency, value }, receiver }] });
+client
+    .execute(reqPost)
+    .then(res => {
+        const result = res.result;
+        const batchId = result?.batch_header?.payout_batch_id;
+        if (batchId) {
+            const reqGetBatch = new Payouts.PayoutsGetRequest(batchId);
+            reqGetBatch.headers.Authorization = envLive.authorizationString();
+            return client.execute(reqGetBatch);
+        }
+    })
+    .then(res => {
+        const batchStatus = res?.result?.batch_header?.batch_status;
+        if (batchStatus === 'SUCCESS') {
+            // payout is successful
+        }
+        const batchItems = res?.result?.items;
+        if (batchItems?.length) {
+            const [firstItem] = batchItems;
+            const reqItems = new Payouts.PayoutsItemGetRequest(firstItem.payout_item_id);
+            reqItems.headers.Authorization = envLive.authorizationString();
+            return client.execute(reqItems);
+        }
+    })
+    .then(res => {
+        const txStatus = res?.result?.transaction_status;
+        if (txStatus !== 'SUCCESS') {
+            const itemId = res?.result?.payout_item_id;
+            if (itemId) {
+                const reqCancel = new Payouts.PayoutsItemCancelRequest(itemId);
+                reqCancel.headers.Authorization = envLive.authorizationString();
+                return client.execute(reqCancel);
+            }
+        }
+    })
+    .then(res => {
+        // handle cancellation response
+    })
+    .catch(error => {
+        // do something with the error
+    });
