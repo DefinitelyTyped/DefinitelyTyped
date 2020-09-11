ce6e032e644ac9d17b4dae23a7d14b6ced7ddbbc
Sara Marcondes <saram@fastmail.com>
2020-09-11 07:57:45 -0700

    Fix patch version
    Co-authored-by: Jon Surrell <jon.surrell@automattic.com>

--- types/wordpress__components/index.d.ts
+++ types/wordpress__components/index.d.ts
@@ -1,4 +1,4 @@
-// Type definitions for @wordpress/components 10.0.0
+// Type definitions for @wordpress/components 9.8
 // Project: https://github.com/WordPress/gutenberg/tree/master/packages/components/README.md
 // Definitions by: Derek Sifford <https://github.com/dsifford>
 //                 Jon Surrell <https://github.com/sirreal>
