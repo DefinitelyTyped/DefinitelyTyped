c70625282295def8fb7d6a3aaa18831bb3987080
cgu <cgu@pinterest.com>
2020-09-14 11:20:48 -0400

    [gestalt-datepicker] Add type definitions

---
+++ types/gestalt-datepicker/index.d.ts
@@ -0,0 +1,38 @@
+// Type definitions for gestalt-datepicker 13.4
+// Project: https://gestalt.netlify.app/
+// Definitions by: cgu <https://github.com/czgu>
+// Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
+// Minimum TypeScript Version: 3.4
+
+import * as React from 'react';
+import { Locale } from 'date-fns';
+
+/*
+DatePicker Props Interface
+https://gestalt.netlify.app/DatePicker
+*/
+
+export interface DatePickerProps {
+    id: string;
+    onChange: (args: { event: React.SyntheticEvent<HTMLInputElement>; value: Date }) => void;
+    disabled?: boolean;
+    errorMessage?: string;
+    excludeDates?: ReadonlyArray<Date>;
+    helperText?: string;
+    idealDirection?: 'up' | 'right' | 'down' | 'left';
+    includeDates?: ReadonlyArray<Date>;
+    label?: string;
+    localeData?: Locale;
+    maxDate?: Date;
+    minDate?: Date;
+    nextRef?: React.Ref<any>;
+
+    placeholder?: string;
+    rangeEndDate?: Date;
+    rangeSelector?: 'start' | 'end';
+    rangeStartDate?: Date;
+    ref?: React.Ref<any>;
+    value?: Date;
+}
+
+export class DatePicker extends React.Component<DatePickerProps, any> {}
