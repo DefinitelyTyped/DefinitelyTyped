381d423eb2ec3918fe7022d92c0f3eeae32417c2
Gavin Rehkemper <gavinr@users.noreply.github.com>
2020-09-14 11:15:05 -0500

    Update types/terraformer__arcgis/index.d.ts
    Co-authored-by: Linn Dahlgren <admin@opposite.biz>

--- types/terraformer__arcgis/index.d.ts
+++ types/terraformer__arcgis/index.d.ts
@@ -8,5 +8,5 @@ import * as GeoJSON from 'geojson';
 import * as ArcGIS from 'arcgis-rest-api';
 
 // tslint:disable-next-line [no-unnecessary-generics]
-export function arcgisToGeoJSON(arcgis: ArcGIS.Geometry): GeoJSON.GeometryObject;
-export function geojsonToArcGIS(geojson: GeoJSON.GeometryObject): ArcGIS.Geometry;
+export function arcgisToGeoJSON(arcgis: ArcGIS.Geometry, idAttribute?: string): GeoJSON.GeometryObject;
+export function geojsonToArcGIS(geojson: GeoJSON.GeometryObject, idAttribute?: string): ArcGIS.Geometry;
