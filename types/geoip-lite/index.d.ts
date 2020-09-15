5d9e9905238c51d151960b35958552cef742697f
a.kuzmenko <1782530+witem@users.noreply.github.com>
2020-09-15 13:08:36 +0300

    master | update version in header
    Signed-off-by: a.kuzmenko <1782530+witem@users.noreply.github.com>

--- types/geoip-lite/index.d.ts
+++ types/geoip-lite/index.d.ts
@@ -1,4 +1,4 @@
-// Type definitions for GeoIP-lite 1.1.6
+// Type definitions for GeoIP-lite 1.4.2
 // Project: https://github.com/bluesmoon/node-geoip
 // Definitions by: Yuce Tekol <http://yuce.me/>
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
