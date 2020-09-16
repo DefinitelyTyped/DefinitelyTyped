ba9c8d6368ed4c8e2194e753580abba302c1eea1
Peter Kupietz <kuuup-at-work@users.noreply.github.com>
2020-09-16 11:02:43 +0200

    Fixed version

--- types/yesql/index.d.ts
+++ types/yesql/index.d.ts
@@ -1,4 +1,4 @@
-// Type definitions for yesql 4.1.0
+// Type definitions for yesql 4.1
 // Project: https://github.com/pihvi/yesql#readme
 // Definitions by: Lluís Ulzurrun de Asanza Sáez <https://github.com/Sumolari>
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
