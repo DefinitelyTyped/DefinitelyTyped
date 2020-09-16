82153f9531d4ef0d57b3c1d47ac01331d104f831
Peter Kupietz <p.kupietz@socoto.com>
2020-09-16 09:57:36 +0200

    Added yesql options parameter

--- types/yesql/index.d.ts
+++ types/yesql/index.d.ts
@@ -14,11 +14,16 @@ declare function readSqlFiles(
 
 declare namespace readSqlFiles {
     type AnyParams = Record<string, any>;
+    interface Options {
+        useNullForMissing?: boolean;
+    }
     function pg<TParams extends object = AnyParams>(
         query: string,
+        options?: Options
     ): (params: TParams) => { text: string; values: Array<TParams[keyof TParams]> };
     function mysql<TParams extends object = AnyParams>(
         query: string,
+        options?: Options
     ): (params: TParams) => { sql: string; values: Array<TParams[keyof TParams]> };
 }
 
