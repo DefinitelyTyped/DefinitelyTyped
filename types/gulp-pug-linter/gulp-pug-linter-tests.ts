cb5013ede4633158957448a2735e33961f1d8ffa
Takesi Tokugawa <tokugawa.takesi@gmail.com>
2020-09-17 18:51:39 +0900

    fixed issues

--- types/gulp-pug-linter/gulp-pug-linter-tests.ts
+++ types/gulp-pug-linter/gulp-pug-linter-tests.ts
@@ -1,7 +1,13 @@
 import gulp = require('gulp');
 import * as gulpPugLinter from "gulp-pug-linter";
 
-gulp.task('lint:template', () => (
+gulp.task('lint:template__default', () => (
+    gulp
+        .src('./**/*.pug')
+        .pipe(gulpPugLinter({ failAfterError: true }))
+));
+
+gulp.task('lint:template__reporter', () => (
     gulp
         .src('./**/*.pug')
         .pipe(gulpPugLinter({ reporter: 'default' }))
--- types/gulp-pug-linter/index.d.ts
+++ types/gulp-pug-linter/index.d.ts
@@ -1,4 +1,4 @@
-// Type definitions for gulp-pug-linter 1.4.0
+// Type definitions for gulp-pug-linter 1.4
 // Project: https://github.com/ilyakam/gulp-pug-linter
 // Definitions by: Takesi Tokugawa <https://github.com/TokugawaTakesi>
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
