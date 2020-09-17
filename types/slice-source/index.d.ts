9c4abfd50a8c2c30a40a4b59201cfb1808d7a728
Björn Harrtell <bjorn@wololo.org>
2020-09-17 09:24:42 +0200

    Fix lint

--- types/slice-source/index.d.ts
+++ types/slice-source/index.d.ts
@@ -3,15 +3,17 @@
 // Definitions by: Björn Harrtell <https://github.com/bjornharrtell>
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
 
-export default function slice(source: ReadableStream | ReadableStreamReader | SliceSource): SliceSource;
+declare function slice(source: ReadableStream | ReadableStreamReader | SliceSource): SliceSource;
 
-export interface SliceChunk {
+interface SliceChunk {
     value: Uint8Array;
     done: boolean;
 }
 
-export interface SliceSource {
+interface SliceSource {
     slice(length: number): Promise<Uint8Array>;
     read(): Promise<SliceChunk>;
     cancel(): Promise<void>;
 }
+
+export = slice;
