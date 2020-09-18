fd54833bbd3350644c93617a9701fb9ebe5a18d8
Kasper <kasper@sitepark.com>
2020-09-18 22:06:24 +0200

    typing to export equals convention

--- types/preact-custom-element/index.d.ts
+++ types/preact-custom-element/index.d.ts
@@ -4,10 +4,12 @@
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
 // TypeScript Version: 3.9
 
-import { ComponentClass, FunctionComponent, FunctionalComponent, h } from 'preact';
-export default function register(
+import { ComponentClass, FunctionComponent, FunctionalComponent } from 'preact';
+declare function register(
     componentDefinition: FunctionComponent<any> | ComponentClass<any> | FunctionalComponent<any>,
     tagName: string,
     observedAttributes?: string[],
     options?: { shadow: boolean },
 ): void;
+
+export = register;
