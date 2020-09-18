272580136948a405fcc164822d1e746a30805232
Ricky Kirkham <rickki@microsoft.com>
2020-09-17 21:11:18 -0700

    fix build error

--- types/office-js-preview/index.d.ts
+++ types/office-js-preview/index.d.ts
@@ -1482,7 +1482,7 @@ declare namespace Office {
         /**
          * The handler to be removed. If not specified all handlers for the specified event type are removed.
          */
-        handler?: (eventArgs?: Office.BindingDataChangedEventArgs | Office.BindingSelectionChangedEventArgs) => void;
+        handler?: (eventArgs?: Office.BindingDataChangedEventArgs | Office.BindingSelectionChangedEventArgs) => any
         /**
          * A user-defined item of any type that is returned, unchanged, in the asyncContext property of the AsyncResult object that is passed to a callback.
          */
