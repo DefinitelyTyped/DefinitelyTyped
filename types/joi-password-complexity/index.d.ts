803c973c0dab2f4dd3cb4470a8c7045417430b56
Alan Morel <alan@alanmorel.com>
2020-09-20 22:35:34 -0400

    make improvements after running tests

--- types/joi-password-complexity/index.d.ts
+++ types/joi-password-complexity/index.d.ts
@@ -4,51 +4,48 @@
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
 // TypeScript Version: 4.0
 
-// tslint:disable-next-line:no-unnecessary-class
-declare module 'joi-password-complexity' {
-    import { ValidationOptions, Context } from 'joi';
+import { ValidationOptions, Context } from 'joi';
 
-    export type ComplexityOptions = {
-        min?: number,
-        max?: number,
-        lowerCase?: number,
-        upperCase?: number,
-        numeric?: number,
-        symbol?: number,
-        requirementCount?: number,
-    };
+interface ComplexityOptions = {
+    min?: number,
+    max?: number,
+    lowerCase?: number,
+    upperCase?: number,
+    numeric?: number,
+    symbol?: number,
+    requirementCount?: number,
+};
 
-    interface ValidationErrorItem {
-        message: string;
-        type: string;
-        path: string[];
-        options?: ValidationOptions;
-        context?: Context;
-    }
-
-    interface ValidationError extends Error {
-        details: ValidationErrorItem[];
-        annotate(): string;
-        _object: any;
-    }
-
-    export type ComplexityObject = {
-        type: string,
-        base: string,
-        messages: {
-            'passwordComplexity.tooShort': string,
-            'passwordComplexity.tooLong': string,
-            'passwordComplexity.lowercase': string,
-            'passwordComplexity.uppercase': string,
-            'passwordComplexity.numeric': string,
-            'passwordComplexity.symbol': string,
-            'passwordComplexity.requirementCount': string,
-        },
-        validate: (value: string) => {
-            value: string
-            error?: ValidationError,
-        };
-    };
-
-    export default function (options: ComplexityOptions, label?: string): ComplexityObject;
+interface ValidationErrorItem {
+    message: string;
+    type: string;
+    path: string[];
+    options?: ValidationOptions;
+    context?: Context;
 }
+
+interface ValidationError extends Error {
+    details: ValidationErrorItem[];
+    annotate(): string;
+    _object: any;
+}
+
+interface ComplexityObject = {
+    type: string,
+    base: string,
+    messages: {
+        'passwordComplexity.tooShort': string,
+        'passwordComplexity.tooLong': string,
+        'passwordComplexity.lowercase': string,
+        'passwordComplexity.uppercase': string,
+        'passwordComplexity.numeric': string,
+        'passwordComplexity.symbol': string,
+        'passwordComplexity.requirementCount': string,
+    },
+    validate: (value: string) => {
+        value: string
+        error?: ValidationError,
+    };
+};
+
+export default function(options: ComplexityOptions, label?: string): ComplexityObject;
