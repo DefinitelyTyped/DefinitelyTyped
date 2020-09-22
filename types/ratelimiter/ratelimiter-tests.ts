e93b6ad6d9ca55403eebd43032c8371ed6fb7354
Piotr Błażejewicz <peterblazejewicz@users.noreply.github.com>
2020-09-22 22:05:23 +0200

    update(ratelimiter): version 3.4 update and other changes
    This introduces minor updates from 3.4, while at the same time exporting
    types from the module:
    - new creation options
    - new properties in results
    - version bump
    - export types properly
    - tests amended
    - mantainer added
    
    https://github.com/tj/node-ratelimiter/compare/v2.1.1...v3.3.1#diff-168726dbe96b3ce427e7fedce31bb0bc
    
    Thanks!

--- types/ratelimiter/ratelimiter-tests.ts
+++ types/ratelimiter/ratelimiter-tests.ts
@@ -3,12 +3,19 @@ import Limiter = require('ratelimiter');
 
 declare let id: string;
 declare let db: redis.RedisClient;
-let limit = new Limiter({ id: id, db: db });
 
-const str: string = limit.inspect();
+const limit = new Limiter({ id, db, duration: 3_600, max: 1_000 });
 
-limit.get((err, limit): void => {
-    const total: number = limit.total;
-    const remaining: number = limit.remaining;
-    const reset: number = limit.reset;
+limit.inspect(); // $ExpectType string
+
+limit.get((error, limit): void => {
+    if (error) {
+        console.error(error);
+        return;
+    }
+
+    limit.total; // $ExpectType number
+    limit.remaining; // $ExpectType number
+    limit.reset; // $ExpectType number
+    limit.resetMs; // $ExpectType number
 });
