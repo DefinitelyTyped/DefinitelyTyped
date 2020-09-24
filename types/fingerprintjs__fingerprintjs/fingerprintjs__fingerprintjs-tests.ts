ef7aa1ff22d07f64ddce74dee97c7fcb177c69e7
Luke Bennett <LukeeeeBennettPlus@gmail.com>
2020-09-24 14:35:59 +0100

    test: fix fingerprintjs__fingerprintjs test import

--- types/fingerprintjs__fingerprintjs/fingerprintjs__fingerprintjs-tests.ts
+++ types/fingerprintjs__fingerprintjs/fingerprintjs__fingerprintjs-tests.ts
@@ -1,51 +1,51 @@
-import * as fingerprint2 from 'fingerprintjs2';
+import * as Fingerprint2 from '@fingerprintjs/fingerprintjs';
 
-function defaultCallback(components: fingerprint2.Component[]) {}
-function v18Callback(murmur: string, components: fingerprint2.V18Component[]) {}
+function defaultCallback(components: Fingerprint2.Component[]) {}
+function v18Callback(murmur: string, components: Fingerprint2.V18Component[]) {}
 
 function test_x64hash128() {
     // $ExpectType string
-    fingerprint2.x64hash128('abc', 99);
+    Fingerprint2.x64hash128('abc', 99);
 }
 
 function test_get_default_settings() {
-    fingerprint2.get(defaultCallback);
+    Fingerprint2.get(defaultCallback);
 }
 
 function test_get_with_options() {
-    fingerprint2.get({ excludes: { userAgent: true } }, defaultCallback);
+    Fingerprint2.get({ excludes: { userAgent: true } }, defaultCallback);
 }
 
 function test_getPromise_default_settings() {
     // $ExpectType Promise<Component[]>
-    fingerprint2.getPromise();
+    Fingerprint2.getPromise();
 }
 
 function test_getPromise_with_options() {
     // $ExpectType Promise<Component[]>
-    fingerprint2.getPromise({ excludes: { userAgent: true } });
+    Fingerprint2.getPromise({ excludes: { userAgent: true } });
 }
 
 function test_getV18_default_settings() {
-    fingerprint2.getV18(v18Callback);
+    Fingerprint2.getV18(v18Callback);
 }
 
 function test_getV18_with_options() {
-    fingerprint2.getV18({ excludes: { userAgent: true } }, v18Callback);
+    Fingerprint2.getV18({ excludes: { userAgent: true } }, v18Callback);
 }
 
 function test_get_audio_options() {
-    const options: fingerprint2.Options = {
+    const options: Fingerprint2.Options = {
         audio: {
             timeout: 10,
             excludeIOS11: true,
         },
     };
-    fingerprint2.get(options, defaultCallback);
+    Fingerprint2.get(options, defaultCallback);
 }
 
 function test_get_fonts_options() {
-    const options: fingerprint2.Options = {
+    const options: Fingerprint2.Options = {
         fonts: {
             swfContainerId: 'swfContainerId',
             swfPath: 'pathToSwf',
@@ -53,30 +53,30 @@ function test_get_fonts_options() {
             extendedJsFonts: true,
         },
     };
-    fingerprint2.get(options, defaultCallback);
+    Fingerprint2.get(options, defaultCallback);
 }
 
 function test_get_screen_options() {
-    const options: fingerprint2.Options = {
+    const options: Fingerprint2.Options = {
         screen: {
             detectScreenOrientation: true,
         },
     };
-    fingerprint2.get(options, defaultCallback);
+    Fingerprint2.get(options, defaultCallback);
 }
 
 function test_get_plugins_options() {
-    const options: fingerprint2.Options = {
+    const options: Fingerprint2.Options = {
         plugins: {
             sortPluginsFor: [/foo/i],
             excludeIE: true,
         },
     };
-    fingerprint2.get(options, defaultCallback);
+    Fingerprint2.get(options, defaultCallback);
 }
 
 function test_get_extraComponents_options() {
-    const options: fingerprint2.Options = {
+    const options: Fingerprint2.Options = {
         extraComponents: [
             {
                 key: 'foo',
@@ -92,11 +92,11 @@ function test_get_extraComponents_options() {
             },
         ],
     };
-    fingerprint2.get(options, defaultCallback);
+    Fingerprint2.get(options, defaultCallback);
 }
 
 function test_get_excludes_options() {
-    let options: fingerprint2.Options;
+    let options: Fingerprint2.Options;
 
     options = { excludes: { userAgent: true } };
     options = { excludes: { language: true } };
@@ -131,5 +131,5 @@ function test_get_excludes_options() {
     options = { excludes: { audio: true } };
     options = { excludes: { enumerateDevices: true } };
 
-    fingerprint2.get(options, defaultCallback);
+    Fingerprint2.get(options, defaultCallback);
 }
