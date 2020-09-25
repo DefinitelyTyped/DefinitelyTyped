e07c4f9e035bb7735ad712052c9ed895462374bc
Liam Read <liam@liam.pro>
2020-09-25 15:46:47 +0200

    Adding back declare

--- types/onfido-sdk-ui/index.d.ts
+++ types/onfido-sdk-ui/index.d.ts
@@ -4,59 +4,59 @@
 //                 aktary <https://github.com/aktary>
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
 
-interface iOnfidoResponse {
-    document_front: {
-        id: string;
-        type: string;
-        side: string;
-    };
-    face: {
-        id: string;
-        variant: string;
-    };
+declare module onfidosdkui {
+    interface iOnfidoResponse {
+        document_front: {
+            id: string;
+            type: string;
+            side: string;
+        };
+        face: {
+            id: string;
+            variant: string;
+        };
+    }
+
+    interface iOnfidoConfig {
+        token?: string;
+        containerId?: string;
+        onComplete?: (data: iOnfidoResponse) => null;
+        onModalRequestClose?: () => null;
+        isModalOpen?: boolean;
+        useModal?: boolean;
+
+        steps?: Array<| string
+            | {
+            type: string;
+            options: {
+                title?: string;
+                descriptions?: [string];
+                nextButton?: string;
+                country?: string;
+                documentTypes?: {
+                    passport?: boolean;
+                    driving_licence?: boolean;
+                    national_identity_card?: boolean;
+                    bank_building_society_statement?: boolean;
+                    utility_bill?: boolean;
+                    council_tax?: boolean; // GBR only
+                    benefit_letters?: boolean; // GBR only
+                    government_letter?: boolean; // non-GBR only
+                };
+                forceCrossDevice?: boolean;
+                requestedVariant?: 'standard' | 'video';
+                uploadFallback?: false;
+                message?: string;
+                submessage?: string;
+            };
+        }>;
+    }
+
+    interface iOnfidoHandle {
+        tearDown(): null;
+
+        setOptions(opts: iOnfidoConfig): null;
+    }
+
+    function init(e: any): iOnfidoHandle;
 }
-
-interface iOnfidoConfig {
-    token?: string;
-    containerId?: string;
-    onComplete?: (data: iOnfidoResponse) => null;
-    onModalRequestClose?: () => null;
-    isModalOpen?: boolean;
-    useModal?: boolean;
-
-    steps?: Array<
-        | string
-        | {
-              type: string;
-              options: {
-                  title?: string;
-                  descriptions?: [string];
-                  nextButton?: string;
-                  country?: string;
-                  documentTypes?: {
-                      passport?: boolean;
-                      driving_licence?: boolean;
-                      national_identity_card?: boolean;
-                      bank_building_society_statement?: boolean;
-                      utility_bill?: boolean;
-                      council_tax?: boolean; // GBR only
-                      benefit_letters?: boolean; // GBR only
-                      government_letter?: boolean; // non-GBR only
-                  };
-                  forceCrossDevice?: boolean;
-                  requestedVariant?: 'standard' | 'video';
-                  uploadFallback?: false;
-                  message?: string;
-                  submessage?: string;
-              };
-          }
-    >;
-}
-
-interface iOnfidoHandle {
-    tearDown(): null;
-
-    setOptions(opts: iOnfidoConfig): null;
-}
-
-function init(e: any): iOnfidoHandle;
