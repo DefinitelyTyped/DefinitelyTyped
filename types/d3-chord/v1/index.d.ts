0e5d044f92b87ed1e12c6b300607dcb6528c7b59
Nathan Bierema <nbierema@gmail.com>
2020-09-26 09:20:44 -0400

    Bump old patch version

--- types/d3-chord/v1/index.d.ts
+++ types/d3-chord/v1/index.d.ts
@@ -7,7 +7,7 @@
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
 // TypeScript Version: 2.3
 
-// Last module patch version validated against: 1.0.3
+// Last module patch version validated against: 1.0.6
 
 // ---------------------------------------------------------------------
 // Chord
