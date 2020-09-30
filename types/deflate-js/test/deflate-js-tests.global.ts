e09a4580e77f306f50af5846b68267f8d0844126
Seb Renauld <seb.renauld@gmail.com>
2020-10-01 00:51:30 +0200

    Very good catch - it isn't usable as a global

--- types/deflate-js/test/deflate-js-tests.global.ts
+++
@@ -1,3 +0,0 @@
-deflatejs.deflate(new Uint8Array([1, 2, 3, 4]), 3); // $ExpectType Uint8Array
-deflatejs.deflate(new Uint8Array([1, 2, 3, 4])); // $ExpectType Uint8Array
-deflatejs.inflate(new Uint8Array([1, 2, 3, 4])); // $ExpectType Uint8Array
