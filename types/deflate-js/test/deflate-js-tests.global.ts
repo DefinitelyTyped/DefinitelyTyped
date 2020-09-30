fab424bee8e177be684cb3ddd0d3d587a42b60b3
Seb Renauld <seb.renauld@gmail.com>
2020-10-01 00:51:30 +0200

    deflate-js types

---
+++ types/deflate-js/test/deflate-js-tests.global.ts
@@ -0,0 +1,3 @@
+deflatejs.deflate(new Uint8Array([1, 2, 3, 4]), 3); // $ExpectType Uint8Array
+deflatejs.deflate(new Uint8Array([1, 2, 3, 4])); // $ExpectType Uint8Array
+deflatejs.inflate(new Uint8Array([1, 2, 3, 4])); // $ExpectType Uint8Array
