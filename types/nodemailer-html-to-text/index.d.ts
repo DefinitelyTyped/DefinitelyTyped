a7d73993ba980834b6795b5cf398c3f7326c0e59
Vinícius Fagundes <mvlacerda@gmail.com>
2020-09-30 15:06:43 -0300

    add typings for nodemailer-html-to-text

---
+++ types/nodemailer-html-to-text/index.d.ts
@@ -0,0 +1,12 @@
+// Type definitions for nodemailer-html-to-text 3.1
+// Project: https://github.com/andris9/nodemailer-html-to-text
+// Definitions by: Vinícius Fagundes <https://github.com/fagundes>
+// Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
+
+import Mail = require('nodemailer/lib/mailer');
+import HtmlToText = require('html-to-text');
+
+/**
+ * Takes options for the html-to-text converter and returns a nodemailer plugin function
+ */
+export function htmlToText(options?: HtmlToText.HtmlToTextOptions): Mail.PluginFunction;
