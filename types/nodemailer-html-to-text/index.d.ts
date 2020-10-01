59805b3ff488aa79602cdeaa0cb452b546a0cc7c
Vinícius Fagundes <mvlacerda@gmail.com>
2020-09-30 23:39:19 -0300

    include typescript required version

--- types/nodemailer-html-to-text/index.d.ts
+++ types/nodemailer-html-to-text/index.d.ts
@@ -2,6 +2,7 @@
 // Project: https://github.com/andris9/nodemailer-html-to-text
 // Definitions by: Vinícius Fagundes <https://github.com/fagundes>
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
+// TypeScript Version: 3.3
 
 import Mail = require('nodemailer/lib/mailer');
 import HtmlToText = require('html-to-text');
