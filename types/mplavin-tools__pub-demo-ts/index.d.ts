95c35d1cb1f220010395da66df48b303a78a343b
mplavin <mplavin@softserveinc.com>
2020-10-01 20:24:04 +0300

    remove test file
    add header
    
    remove typescript version from the header
    
    set noEmit:true
    
    add compiler options
    
    remove redundant options
    
    remove file
    
    more rules
    
    remove module declare
    
    fix errros
    
    fix errors
    
    fix errors

--- types/mplavin-tools__pub-demo-ts/index.d.ts
+++ types/mplavin-tools__pub-demo-ts/index.d.ts
@@ -1,28 +1,35 @@
-declare module '@mplavin-tools/pub-demo-ts' {
-    export interface IRound {
+// Type definitions for @mplavin-tools/pub-demo-ts 0.1
+// Project: https://gitlab.com/mplavin/pub-demo-ts
+// Definitions by: Max Plavinskiy <https://github.com/MaxPlav>
+// Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
+
+    export interface RoundInterface {
         radius: number;
         square: () => number;
         getRadius: () => number;
     }
-    export interface IColor {
+    export interface Color {
+        value: number;
     }
-    export interface IShape {
+    export interface Shape {
+        value: number;
     }
-    export const sum: (a: number, b: number) => number;
-    export class Circle implements IRound {
+    export function sum(): (a: number, b: number) => number;
+    export class Circle implements RoundInterface {
         radius: number;
         constructor(radius: number);
         square(): number;
         getRadius(): number;
     }
-    export class Red implements IColor {
+    export class Red implements Color {
         private color;
+        value: number;
         constructor(color: string);
         getColor(): string;
     }
-    export class Round implements IShape {
+    export class Round implements Shape {
+        value: number;
         private shape;
         constructor(shape: string);
         getShape(): string;
     }
-}
--- types/mplavin-tools__pub-demo-ts/mplavin-tools__pub-demo-ts-tests.ts
+++
@@ -1,3 +0,0 @@
-import lib = require('@mplavin-tools/pub-demo-ts');
-
-const circle = new lib.Circle(5)
