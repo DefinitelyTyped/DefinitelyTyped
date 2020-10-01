dcb403275ba3a2712a9815d141735b46003f1ff1
Glenn Reyes <glenn@glennreyes.com>
2020-10-01 11:50:03 +0200

    Fix test

--- types/express-brute-redis/index.d.ts
+++ types/express-brute-redis/index.d.ts
@@ -19,7 +19,7 @@ declare class RedisStore {
             client?: RedisClient;
             prefix?: string;
             host?: string;
-            port?: string;
+            port?: number | string;
         },
         ...args: any[]
     );
