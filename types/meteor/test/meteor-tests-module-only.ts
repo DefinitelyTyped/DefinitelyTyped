7dff346a0e6476d8fed07933508633a367947ceb
Radosław Miernik <radekmie@gmail.com>
2020-10-01 12:36:58 +0200

    Added meteor/promise.

--- types/meteor/test/meteor-tests-module-only.ts
+++ types/meteor/test/meteor-tests-module-only.ts
@@ -1,7 +1,32 @@
 // This file is for tests that involve modules that don't have corresponding
 // globals.
 
+import { Promise as MeteorPromise } from "meteor/promise";
 import { ServiceConfiguration } from "meteor/service-configuration";
 import { WebApp } from "meteor/webapp";
 
-// We have no actual tests beyond the imports yet.
+function foo(x: number) {
+    return `${x}`;
+}
+
+// $ExpectType MeteorPromise<string>
+MeteorPromise.async(foo, null)(1);
+
+// $ExpectType string
+MeteorPromise.async(foo, null)(1).await();
+
+// $ExpectType MeteorPromise<string>
+MeteorPromise.asyncApply(foo, null, [1]);
+
+// $ExpectType string
+MeteorPromise.asyncApply(foo, null, [1]).await();
+
+async function bar() {
+    return 1;
+}
+
+// $ExpectType number
+MeteorPromise.await(bar());
+
+// $ExpectType [number]
+MeteorPromise.awaitAll([bar()]);
