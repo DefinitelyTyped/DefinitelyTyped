e5f8b91fcf4832ba760bce5af56ae03f48c0126c
Nathan Bierema <nbierema@gmail.com>
2020-10-01 00:40:34 -0400

    Prepare

--- types/d3-transition/v1/index.d.ts
+++ types/d3-transition/v1/index.d.ts
@@ -1,4 +1,4 @@
-// Type definitions for D3JS d3-transition module 2.0
+// Type definitions for D3JS d3-transition module 1.3
 // Project: https://github.com/d3/d3-transition/, https://d3js.org/d3-transition
 // Definitions by: Tom Wanzek <https://github.com/tomwanzek>
 //                 Alex Ford <https://github.com/gustavderdrache>
@@ -8,7 +8,7 @@
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
 // TypeScript Version: 2.3
 
-// Last module patch version validated against: 2.0.0
+// Last module patch version validated against: 1.3.2
 
 import { ArrayLike, BaseType, Selection, ValueFn } from 'd3-selection';
 
