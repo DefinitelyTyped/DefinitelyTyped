92896bf4f243a282974321d25497740c333408e3
SHIRSH ZIBBU <zibbu.shirsh@gmail.com>
2020-10-01 17:39:49 +0530

    remove patch version from header

--- types/react-native-aws3/index.d.ts
+++ types/react-native-aws3/index.d.ts
@@ -1,4 +1,4 @@
-// Type definitions for react-native-aws3 0.0.9
+// Type definitions for react-native-aws3 0.0
 // Project: https://github.com/benjreinhart/react-native-aws3#readme
 // Definitions by: Shirsh Zibbu <https://github.com/zhirzh>
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
