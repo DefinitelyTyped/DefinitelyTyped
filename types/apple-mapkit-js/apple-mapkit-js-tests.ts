f73769691e281037d48e599f11b482bd1ab9c960
Moritz Sternemann <moritz@strnmn.me>
2020-10-04 22:41:16 +0200

    Format type definitions and tests

--- types/apple-mapkit-js/apple-mapkit-js-tests.ts
+++ types/apple-mapkit-js/apple-mapkit-js-tests.ts
@@ -1,28 +1,25 @@
 mapkit.init({
-  language: 'en',
-  authorizationCallback: done => {
-    done('my-map-token');
-  },
+    language: 'en',
+    authorizationCallback: done => {
+        done('my-map-token');
+    },
 });
 
-const map: mapkit.Map = new mapkit.Map(
-  document.querySelector<HTMLElement>('.test'),
-  {
+const map: mapkit.Map = new mapkit.Map(document.querySelector<HTMLElement>('.test'), {
     isZoomEnabled: true,
     isRotationEnabled: false,
     tintColor: 'green',
-  },
-);
+});
 
 const colors = mapkit.Map.ColorSchemes.Dark;
 
 const coordinate = new mapkit.Coordinate(1000, 1000);
 const factory = (coordinate: mapkit.Coordinate) => {
-  const el = document.createElement('div');
+    const el = document.createElement('div');
 
-  el.textContent = `${coordinate.longitude}, ${coordinate.latitude}`;
+    el.textContent = `${coordinate.longitude}, ${coordinate.latitude}`;
 
-  return el;
+    return el;
 };
 
 map.addAnnotation(new mapkit.Annotation(coordinate, factory));
@@ -44,13 +41,17 @@ let markerAnnotation: mapkit.MarkerAnnotation;
 let circleOverlay: mapkit.CircleOverlay;
 let itemCollection: mapkit.ItemCollection;
 map.addItems([markerAnnotation, circleOverlay, itemCollection]);
-const addResult: Array<mapkit.Annotation | mapkit.Overlay | mapkit.ItemCollection> | mapkit.ItemCollection = map.addItems(itemCollection);
+const addResult:
+    | Array<mapkit.Annotation | mapkit.Overlay | mapkit.ItemCollection>
+    | mapkit.ItemCollection = map.addItems(itemCollection);
 map.removeItems([markerAnnotation, circleOverlay, itemCollection]);
-const removeResult: Array<mapkit.Annotation | mapkit.Overlay | mapkit.ItemCollection> | mapkit.ItemCollection = map.removeItems(itemCollection);
+const removeResult:
+    | Array<mapkit.Annotation | mapkit.Overlay | mapkit.ItemCollection>
+    | mapkit.ItemCollection = map.removeItems(itemCollection);
 
 const cameraBoundaryDescription: mapkit.CameraBoundaryDescription = {
     mapRect: new mapkit.MapRect(0, 0, 0, 0),
-    region: new mapkit.CoordinateRegion(new mapkit.Coordinate(0, 0), new mapkit.CoordinateSpan(0, 0))
+    region: new mapkit.CoordinateRegion(new mapkit.Coordinate(0, 0), new mapkit.CoordinateSpan(0, 0)),
 };
 
 let coordinateRegion: mapkit.CoordinateRegion;
@@ -71,7 +72,7 @@ const searchAutocompleteOptions: mapkit.SearchAutocompleteOptions = {
     includePointsOfInterest: false,
     includeQueries: false,
     pointOfInterestFilter: mapkit.PointOfInterestFilter.filterIncludingAllCategories,
-    limitToCountries: ''
+    limitToCountries: '',
 };
 
 let filter: mapkit.PointOfInterestFilter;
@@ -96,7 +97,7 @@ const pointOfInterestSearchOptions: mapkit.PointsOfInterestSearchOptions = {
     center: new mapkit.Coordinate(0, 0),
     radius: 0,
     region: new mapkit.CoordinateRegion(new mapkit.Coordinate(0, 0), new mapkit.CoordinateSpan(0, 0)),
-    pointOfInterestFilter: mapkit.PointOfInterestFilter.filterIncludingAllCategories
+    pointOfInterestFilter: mapkit.PointOfInterestFilter.filterIncludingAllCategories,
 };
 
 let pointsOfInterestSearchDelegate: mapkit.PointsOfInterestSearchDelegate;
@@ -104,14 +105,14 @@ pointsOfInterestSearchDelegate.searchDidComplete({ places: [] });
 pointsOfInterestSearchDelegate.searchDidError(new Error());
 
 const pointsOfInterestSearchResponse: mapkit.PointsOfInterestSearchResponse = {
-    places: []
+    places: [],
 };
 
 const etaRequestOptions: mapkit.EtaRequestOptions = {
     origin: new mapkit.Coordinate(0, 0),
     destinations: [],
     transportType: mapkit.Directions.Transport.Automobile,
-    departureDate: new Date()
+    departureDate: new Date(),
 };
 
 const directions = new mapkit.Directions();
@@ -119,17 +120,17 @@ const directionsResult: number = directions.eta(etaRequestOptions, (error: Error
 
 const etaResponse: mapkit.EtaResponse = {
     request: etaRequestOptions,
-    etas: []
+    etas: [],
 };
 
 const etaResult: mapkit.EtaResult = {
     transportType: mapkit.Directions.Transport.Automobile,
     distance: 0,
     expectedTravelTime: 0,
-    staticTravelTime: 0
+    staticTravelTime: 0,
 };
 
 let newCameraZoomRange = new mapkit.CameraZoomRange(0, 0);
-newCameraZoomRange = new mapkit.CameraZoomRange({ minCameraDistance: 0, maxCameraDistance: 0});
+newCameraZoomRange = new mapkit.CameraZoomRange({ minCameraDistance: 0, maxCameraDistance: 0 });
 const minCameraDistance: number = newCameraZoomRange.minCameraDistance;
 const maxCameraDistance: number = newCameraZoomRange.maxCameraDistance;
