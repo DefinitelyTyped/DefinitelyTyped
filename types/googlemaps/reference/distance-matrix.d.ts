0bd6c1686cc341bc2219968d5fa26a2ff3a94c5a
Dmitry Demensky <dmitry@demensky.com>
2020-10-06 01:15:41 +0300

    [googlemaps] distance-matrix: Add DistanceMatrixStatus jsdoc

--- types/googlemaps/reference/distance-matrix.d.ts
+++ types/googlemaps/reference/distance-matrix.d.ts
@@ -37,13 +37,52 @@ declare namespace google.maps {
         status: DistanceMatrixElementStatus;
     }
 
+    /**
+     * The top-level status about the request in general returned by the {@link DistanceMatrixService} upon completion
+     * of a distance matrix request. Specify these by value, or by using the constant's name.
+     * @see {@link https://developers.google.com/maps/documentation/javascript/reference/distance-matrix#DistanceMatrixStatus Maps JavaScript API}
+     */
     enum DistanceMatrixStatus {
+        /**
+         * The provided request was invalid.
+         * @see {@link https://developers.google.com/maps/documentation/javascript/reference/distance-matrix#DistanceMatrixStatus.INVALID_REQUEST Maps JavaScript API}
+         */
         INVALID_REQUEST = 'INVALID_REQUEST',
+
+        /**
+         * The request contains more than 25 origins, or more than 25 destinations.
+         * @see {@link https://developers.google.com/maps/documentation/javascript/reference/distance-matrix#DistanceMatrixStatus.MAX_DIMENSIONS_EXCEEDED Maps JavaScript API}
+         */
         MAX_DIMENSIONS_EXCEEDED = 'MAX_DIMENSIONS_EXCEEDED',
+
+        /**
+         * The product of origins and destinations exceeds the per-query limit.
+         * @see {@link https://developers.google.com/maps/documentation/javascript/reference/distance-matrix#DistanceMatrixStatus.MAX_ELEMENTS_EXCEEDED Maps JavaScript API}
+         */
         MAX_ELEMENTS_EXCEEDED = 'MAX_ELEMENTS_EXCEEDED',
+
+        /**
+         * The response contains a valid result.
+         * @see {@link https://developers.google.com/maps/documentation/javascript/reference/distance-matrix#DistanceMatrixStatus.OK Maps JavaScript API}
+         */
         OK = 'OK',
+
+        /**
+         * Too many elements have been requested within the allowed time period. The request should succeed if you try again after some time.
+         * @see {@link https://developers.google.com/maps/documentation/javascript/reference/distance-matrix#DistanceMatrixStatus.OVER_QUERY_LIMIT Maps JavaScript API}
+         */
         OVER_QUERY_LIMIT = 'OVER_QUERY_LIMIT',
+
+        /**
+         * The service denied use of the Distance Matrix service by your web page.
+         * @see {@link https://developers.google.com/maps/documentation/javascript/reference/distance-matrix#DistanceMatrixStatus.REQUEST_DENIED Maps JavaScript API}
+         */
         REQUEST_DENIED = 'REQUEST_DENIED',
+
+        /**
+         * A Distance Matrix request could not be processed due to a server error. The request may succeed if you try again.
+         * @see {@link https://developers.google.com/maps/documentation/javascript/reference/distance-matrix#DistanceMatrixStatus.UNKNOWN_ERROR Maps JavaScript API}
+         */
         UNKNOWN_ERROR = 'UNKNOWN_ERROR',
     }
 
