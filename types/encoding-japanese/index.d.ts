88134875f6610ab3b71ac76a052d7ba03e5ba69a
rhysd <lin90162@yahoo.co.jp>
2020-10-06 13:23:26 +0900

    Better return type inference from argument type of convert() in encoding-japanese package

--- types/encoding-japanese/index.d.ts
+++ types/encoding-japanese/index.d.ts
@@ -18,38 +18,58 @@ export type Encoding =
     | 'SJIS'
     | 'UNICODE'
     | 'AUTO';
-type IntArrayType = ReadonlyArray<number> | Uint8Array | Uint16Array | Int8Array | Int16Array | Int32Array;
+type IntArrayType =
+    | ReadonlyArray<number>
+    | Uint8Array
+    | Uint16Array
+    | Uint32Array
+    | Int8Array
+    | Int16Array
+    | Int32Array;
 type RawType = IntArrayType | ReadonlyArray<number> | Buffer;
+type EncodingDetection = Encoding | false;
 
-export type ConvertOptions = ConvertStringOptions | ConvertArrayBufferOptions | ConvertArrayOptions;
+export type ConvertOptions =
+    | ConvertStringOptions
+    | ConvertArrayBufferOptions
+    | ConvertArrayOptions
+    | ConvertUnknownOptions;
 
 export interface ConvertStringOptions {
     to: Encoding;
     from?: Encoding;
-    type?: 'string';
+    type: 'string';
     bom?: boolean | string;
 }
 
 export interface ConvertArrayBufferOptions {
     to: Encoding;
     from?: Encoding;
-    type?: 'arraybuffer';
+    type: 'arraybuffer';
     bom?: boolean | string;
 }
 
 export interface ConvertArrayOptions {
     to: Encoding;
     from?: Encoding;
-    type?: 'array';
+    type: 'array';
     bom?: boolean | string;
 }
 
-export declare function detect(data: RawType | string, encodings?: Encoding | Encoding[]): Encoding;
+export interface ConvertUnknownOptions {
+    to: Encoding;
+    from?: Encoding;
+    bom?: boolean | string;
+}
+
+export declare function detect(data: RawType | string, encodings?: Encoding | Encoding[]): EncodingDetection;
 export declare function convert(data: RawType, to: Encoding, from?: Encoding): number[];
 export declare function convert(data: string, to: Encoding, from?: Encoding): string;
 export declare function convert(data: RawType | string, options: ConvertStringOptions): string;
 export declare function convert(data: RawType | string, options: ConvertArrayBufferOptions): ArrayBuffer;
 export declare function convert(data: RawType | string, options: ConvertArrayOptions): number[];
+export declare function convert(data: string, options: ConvertUnknownOptions): string;
+export declare function convert(data: RawType, options: ConvertUnknownOptions): number[];
 export declare function urlEncode(data: IntArrayType): string;
 export declare function urlDecode(data: string): number[];
 export declare function base64Encode(data: IntArrayType): string;
