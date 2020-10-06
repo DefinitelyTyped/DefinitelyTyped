d4b8c0dabc1181e6bf54aee37e4ccd8288c35bb9
Domien Bakker <domien.bakker@coas.com>
2020-10-06 10:28:33 +0200

    Added new type for the jsbarcode library.

---
+++ types/jsbarcode/index.d.ts
@@ -0,0 +1,46 @@
+// Type definitions for jsbarcode 3.11.3
+// Project: https://github.com/lindell/JsBarcode
+// Definitions by: Domien Bakker <https://github.com/domienbakker>
+// Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
+
+/// <reference types="node" />
+
+declare namespace JsBarcode {
+    interface JsBarcodeOptions {
+        format: string;
+        width?: number;
+        height?: number;
+        displayValue?: boolean;
+        text?: string;
+        fontOptions?: string;
+        font?: string;
+        textAlign?: string;
+        textPosition?: string;
+        textMargin?: number;
+        fontSize?: number;
+        background?: string;
+        lineColor?: string;
+        margin?: number;
+        marginTop?: number;
+        marginBottom?: number;
+        marginLeft?: number;
+        marginRight?: number;
+        flat?: boolean;
+        valid?(valid: boolean): void;
+    }
+}
+
+/**
+ * The Browser version of the library's functionality, which makes use of an HTMLCanvasElement, HTMLImageElement or SVGElement
+ * for rendering.
+ * @param svg ID string or HTML element of the target to render within. 
+ * @param barcodeText Text to be rendered.
+ * @param barcodeOptions Options used for rendering.
+ */
+declare function JsBarcode(
+    svg: string | HTMLCanvasElement | HTMLImageElement | SVGElement,
+    barcodeText: string,
+    barcodeOptions?: JsBarcode.JsBarcodeOptions
+): void;
+
+export = JsBarcode;
