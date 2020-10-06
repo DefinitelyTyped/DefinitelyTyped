8144b9c91e61d87b44006e11973bc503e604bd9c
KyiMoeMin <kyimoemin0@gmail.com>
2020-10-06 17:20:36 +0630

    fix: test errors

--- types/react-native-bcrypt/index.d.ts
+++ types/react-native-bcrypt/index.d.ts
@@ -8,7 +8,6 @@
  *~ Otherwise, delete this declaration.
  */
 
-
 /**
  * *All the doc are same as original doc
  */
@@ -17,7 +16,7 @@ interface ReactNativeBcrypt {
      * Sets the pseudo random number generator to use as a fallback if neither node's `crypto` module nor the Web Crypto
      *  API is available. Please note: It is highly important that the PRNG used is cryptographically secure and that it
      *  is seeded properly!
-     * @param {?function(number):!Array.<number>} random Function taking the number of bytes to generate as its
+     * @param random Function taking the number of bytes to generate as its
      *  sole argument, returning the corresponding array of cryptographically secure random byte values.
      * @see http://nodejs.org/api/crypto.html
      * @see http://www.w3.org/TR/WebCryptoAPI/
@@ -26,20 +25,18 @@ interface ReactNativeBcrypt {
 
     /**
      * Synchronously generates a salt.
-     * @param {number=} rounds Number of rounds to use, defaults to 10 if omitted
-     * @param {number=} seed_length Not supported.
-     * @returns {string} Resulting salt
-     * @throws {Error} If a random fallback is required but not set
-     * @expose
+     * @param  rounds Number of rounds to use, defaults to 10 if omitted
+     * @param  seed_length Not supported.
+     * @returns Resulting salt
+     * @throws If a random fallback is required but not set
      */
     genSaltSync(round?: number, seed_length?: number): string;
 
     /**
      * Asynchronously generates a salt.
-     * @param {(number|function(Error, string=))=} rounds Number of rounds to use, defaults to 10 if omitted
-     * @param {(number|function(Error, string=))=} seed_length Not supported.
-     * @param {function(Error, string=)=} callback Callback receiving the error, if any, and the resulting salt
-     * @expose
+     * @param rounds Number of rounds to use, defaults to 10 if omitted
+     * @param seed_length Not supported.
+     * @param callback Callback receiving the error, if any, and the resulting salt
      */
     genSalt(
         rounds?: number | ((error: Error, string?: string) => any),
@@ -49,81 +46,65 @@ interface ReactNativeBcrypt {
 
     /**
      * Synchronously generates a hash for the given string.
-     * @param {string} s String to hash
-     * @param {(number|string)=} salt Salt length to generate or salt to use, default to 10
-     * @returns {string} Resulting hash
-     * @expose
+     * @param s String to hash
+     * @param salt Salt length to generate or salt to use, default to 10
+     * @returns Resulting hash
      */
     hashSync(s: string, salt?: number | string): string;
 
     /**
      * Asynchronously generates a hash for the given string.
-     * @param {string} s String to hash
-     * @param {number|string} salt Salt length to generate or salt to use
-     * @param {function(Error, string=)} callback Callback receiving the error, if any, and the resulting hash
-     * @param {function(number)=} progressCallback Callback successively called with the percentage of rounds completed
+     * @para s String to hash
+     * @param salt Salt length to generate or salt to use
+     * @param callback Callback receiving the error, if any, and the resulting hash
+     * @param progressCallback Callback successively called with the percentage of rounds completed
      *  (0.0 - 1.0), maximally once per `MAX_EXECUTION_TIME = 100` ms.
-     * @expose
      */
     hash(s: string, salt: number | string, callback: ((error: Error, string?: string) => any), progressCallback?: (number: number) => void): void;
 
     /**
      * Synchronously tests a string against a hash.
-     * @param {string} s String to compare
-     * @param {string} hash Hash to test against
-     * @returns {boolean} true if matching, otherwise false
-     * @throws {Error} If an argument is illegal
-     * @expose
+     * @param s String to compare
+     * @param hash Hash to test against
+     * @returns true if matching, otherwise false
      */
     compareSync(s: string, hash: string): boolean;
 
     /**
      * Asynchronously compares the given data against the given hash.
-     * @param {string} s Data to compare
-     * @param {string} hash Data to be compared to
-     * @param {function(Error, boolean)} callback Callback receiving the error, if any, otherwise the result
-     * @param {function(number)=} progressCallback Callback successively called with the percentage of rounds completed
+     * @param s Data to compare
+     * @param hash Data to be compared to
+     * @param callback Callback receiving the error, if any, otherwise the result
+     * @param progressCallback Callback successively called with the percentage of rounds completed
      *  (0.0 - 1.0), maximally once per `MAX_EXECUTION_TIME = 100` ms.
-     * @throws {Error} If the callback argument is invalid
-     * @expose
      */
-    compare(s: string, hash: string, callback: ((error: Error, boolean: boolean) => any), progressCallback?: (number: number) => any): void
+    compare(s: string, hash: string, callback: ((error: Error, boolean: boolean) => any), progressCallback?: (number: number) => any): void;
 
     /**
      * Gets the number of rounds used to encrypt the specified hash.
-     * @param {string} hash Hash to extract the used number of rounds from
-     * @returns {number} Number of rounds used
-     * @throws {Error} If hash is not a string
-     * @expose
+     * @param hash Hash to extract the used number of rounds from
+     * @returns Number of rounds used
      */
     getRounds(hash: string): number;
 
     /**
      * Gets the salt portion from a hash. Does not validate the hash.
-     * @param {string} hash Hash to extract the salt from
-     * @returns {string} Extracted salt part
-     * @throws {Error} If `hash` is not a string or otherwise invalid
-     * @expose
+     * @param hash Hash to extract the salt from
+     * @returns Extracted salt part
      */
     getSalt(hash: string): string;
 
     /**
      * Encodes a byte array to base64 with up to len bytes of input, using the custom bcrypt alphabet.
-     * @function
-     * @param {!Array.<number>} b Byte array
-     * @param {number} len Maximum input length
-     * @returns {string}
-     * @expose
+     * @param b Byte array
+     * @param len Maximum input length
      */
     encodeBase64(b: number[], len: number): string;
 
     /**
      * Decodes a base64 encoded string to up to len bytes of output, using the custom bcrypt alphabet.
-     * @function
-     * @param {string} s String to decode
-     * @param {number} len Maximum output length
-     * @returns {!Array.<number>}
-     * @expose
+     * @param s String to decode
+     * @param len Maximum output length
      */
     decodeBase64(s: string, len: number): number[];
 }
