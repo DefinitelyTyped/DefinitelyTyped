c71fd2031d6218ec443ef01bae80ffacd58343a5
Ricardo Souza <ricardo@ridedott.com>
2020-10-07 09:39:14 +0200

    Add types for @adobe/node-fetch-retry

---
+++ types/adobe__node-fetch-retry/index.d.ts
@@ -0,0 +1,50 @@
+// Type definitions for @adobe/node-fetch-retry 1.0
+// Project: https://github.com/adobe/node-fetch-retry#readme
+// Definitions by: Ricardo Souza <https://github.com/ricardoatsouza>
+// Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
+
+import { Request, RequestInit, Response } from 'node-fetch';
+
+/**
+ * options for fetch-retry
+ */
+export interface Options extends RequestInit {
+    /**
+     * options for retry or false if want to disable retry
+     * ... other options for fetch call (method, headers, etc...)
+     */
+    retryOptions?: RetryOptions;
+}
+
+/**
+ * options for retry or false if want to disable retry
+ */
+export interface RetryOptions {
+    /**
+     * time (in milliseconds) to retry until throwing an error
+     */
+    retryMaxDuration?: number;
+    /**
+     * time to wait between retries in milliseconds
+     */
+    retryInitialDelay?: number;
+    /**
+     * a function determining whether to retry on a specific HTTP code
+     */
+    retryOnHttpResponse?: (response: Response) => boolean;
+    /**
+     * backoff factor for wait time between retries (defaults to 2.0)
+     */
+    retryBackoff?: number;
+    /**
+     * Optional socket timeout in milliseconds (defaults to 60000ms)
+     */
+    socketTimeout?: number;
+    /**
+     * If true, socket timeout will be forced to use `socketTimeout` property declared (defaults to false)
+     */
+    forceSocketTimeout?: boolean;
+}
+
+declare function fetchRetry(url: string | Request, options: Options): Promise<Response>;
+export default fetchRetry;
