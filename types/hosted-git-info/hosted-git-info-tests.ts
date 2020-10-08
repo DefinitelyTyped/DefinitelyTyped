b667ff19e196c4b62492f9311c796658d24f0b04
Daniel Cassidy <mail@danielcassidy.me.uk>
2020-10-08 06:37:32 +0100

    hosted-git-info: Fix tests for typescript <=3.6.

--- types/hosted-git-info/hosted-git-info-tests.ts
+++ types/hosted-git-info/hosted-git-info-tests.ts
@@ -1,28 +1,28 @@
 import HostedGitInfo = require('hosted-git-info');
 
 HostedGitInfo.fromUrl('');
-const result = HostedGitInfo.fromUrl('', {});
-
-result?.hashformat('');
-result?.hash();
-result?.ssh({});
-result?.sshurl({});
-result?.browse('', '', {});
-result?.browse('', {});
-result?.browse({});
-result?.docs({});
-result?.bugs({});
-result?.https({});
-result?.git({});
-result?.shortcut({});
-result?.path({});
-result?.tarball({});
-result?.file('', {});
-result?.getDefaultRepresentation();
-result?.toString({});
+HostedGitInfo.fromUrl('', {});
 
 new HostedGitInfo('github', 'DefinitelyTyped', undefined, 'DefinitelyTyped');
 
 new HostedGitInfo('github', 'webpack-contrib', undefined, 'copy-webpack-plugin', '40e6ae58', 'https');
 
-new HostedGitInfo('gitlab', 'bagrounds', undefined, 'monad-id', '6d3c759b', 'docs');
+const info = new HostedGitInfo('gitlab', 'bagrounds', undefined, 'monad-id', '6d3c759b', 'docs');
+
+info.hashformat('');
+info.hash();
+info.ssh({});
+info.sshurl({});
+info.browse('', '', {});
+info.browse('', {});
+info.browse({});
+info.docs({});
+info.bugs({});
+info.https({});
+info.git({});
+info.shortcut({});
+info.path({});
+info.tarball({});
+info.file('', {});
+info.getDefaultRepresentation();
+info.toString({});
