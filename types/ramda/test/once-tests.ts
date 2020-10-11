52b2a31d277a440ead85f3363385962c81a0089e
Cotton Hou <himcotton@gmail.com>
2020-10-11 20:12:04 +0800

    [ramda] infer types of once

--- types/ramda/test/once-tests.ts
+++ types/ramda/test/once-tests.ts
@@ -1,9 +1,12 @@
 import * as R from 'ramda';
 
-(() => {
+() => {
+  // $ExpectType (x: number) => number
   const addOneOnce = R.once((x: number) => x + 1);
+  // $ExpectType number
   addOneOnce(10); // => 11
   addOneOnce(addOneOnce(50)); // => 11
 
-  const str = R.once<string>(() => 'test')();
-})();
+  // $ExpectType string
+  const str = R.once(() => 'test')();
+};
