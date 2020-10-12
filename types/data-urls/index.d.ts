160f43ae6852c4eefec2641e54cff96dd7b63488
Marmota <40345645+jaimeadf@users.noreply.github.com>
2020-10-12 13:44:39 -0300

    Add typings support for data-urls (#48456)

---
+++ types/data-urls/index.d.ts
@@ -0,0 +1,22 @@
+// Type definitions for data-urls 2.0
+// Project: https://github.com/jsdom/data-urls#readme
+// Definitions by: Jaime Filho <https://github.com/jaimeadf>
+// Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
+
+/// <reference types="node" />
+
+import MIMEType = require('whatwg-mimetype');
+import { URLRecord } from 'whatwg-url';
+
+declare function parseDataURL(stringInput: string): parseDataURL.DataURL | null;
+
+declare namespace parseDataURL {
+    interface DataURL {
+        mimeType: MIMEType;
+        body: Buffer;
+    }
+
+    function fromURLRecord(urlRecord: URLRecord): DataURL | null;
+}
+
+export = parseDataURL;
