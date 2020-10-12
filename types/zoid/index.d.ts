22a11edf4d80ad9dba26b050bd7f186a3a404f69
Nana Adjei Manu <claeusdev@gmail.com>
2020-10-12 03:14:51 +0000

    add zoid

---
+++ types/zoid/index.d.ts
@@ -0,0 +1,18 @@
+// Type definitions for zoid 9.0
+// Project: https://github.com/krakenjs/zoid#readme
+// Definitions by: Nana Adjei Manu <https://github.com/me>
+// Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
+
+
+export declare type DimensionsType = {
+    width: number;
+    height: number;
+};
+export declare type CssDimensionsType = {
+    width: string;
+    height: string;
+};
+export declare type CancelableType = {
+    cancel: () => void;
+};
+export declare type StringMatcherType = string | ReadonlyArray<string>;
---
+++ types/zoid/zoid-tests.ts
@@ -0,0 +1,1 @@
+import zoid from 'zoid';
