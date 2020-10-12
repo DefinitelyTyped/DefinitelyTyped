1c11090494bb87c2ba2a38efd542ac3af6b97473
Emmanuel Gautier <git@emmanuelgautier.fr>
2020-10-12 21:57:16 +0200

    chore(mongodb): update mongodb package version

--- types/mongodb/index.d.ts
+++ types/mongodb/index.d.ts
@@ -1,4 +1,4 @@
-// Type definitions for MongoDB 3.5
+// Type definitions for MongoDB 3.6
 // Project: https://github.com/mongodb/node-mongodb-native
 //          https://github.com/mongodb/node-mongodb-native/tree/3.1
 // Definitions by: Federico Caselli <https://github.com/CaselIT>
