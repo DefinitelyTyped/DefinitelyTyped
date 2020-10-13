637df5662973d91ea2393807ef00024030747c69
Svensson <Per.Svensson@dnvgl.com>
2020-10-13 15:38:12 +0200

    lint

--- types/wojtekmaj__react-datetimerange-picker/wojtekmaj__react-datetimerange-picker-tests.tsx
+++ types/wojtekmaj__react-datetimerange-picker/wojtekmaj__react-datetimerange-picker-tests.tsx
@@ -1,9 +1,8 @@
-import { useState } from 'react';
 import * as React from 'react';
 import DateTimeRangePicker from '@wojtekmaj/react-datetimerange-picker';
 
 function MyApp() {
-    const [value, onChange] = useState([new Date(), new Date()]);
+    const [value, onChange] = React.useState([new Date(), new Date()]);
 
     return (
         <div>
