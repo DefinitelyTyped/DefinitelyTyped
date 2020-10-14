a82da2660ae30c147b643337095fb0bb2d16731a
tpluscode <tpluscode@users.noreply.github.com>
2020-10-14 17:04:53 +0200

    types for @rdfjs/serializer-rdfjs

---
+++ types/rdfjs__serializer-rdfjs/rdfjs__serializer-rdfjs-tests.ts
@@ -0,0 +1,25 @@
+import Serializer = require('@rdfjs/serializer-rdfjs');
+import { Quad, Stream } from 'rdf-js';
+import { EventEmitter } from 'events';
+
+const quads: Quad[] = <any> {};
+const quadStream: Stream = <any> {};
+
+const serializer = new Serializer();
+const serializer2 = new Serializer({});
+const serializerTypescript = new Serializer({
+    module: 'ts'
+});
+const serializerEsm = new Serializer({
+    module: 'esm'
+});
+const serializerAny = new Serializer({
+    module: 'custom'
+});
+
+const code: string = serializer.transform(quads);
+
+const stream: EventEmitter = serializer.import(quadStream);
+const typescriptStream: EventEmitter = serializer.import(quadStream, {
+    module: 'ts'
+});
