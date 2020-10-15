e677bf9053931fb37cc7af37fdf0b80c28a62476
NthMetal <832529@gmail.com>
2020-10-15 10:48:03 -0400

    Add clamav.js types (#48770)
    * added clamav.js
    
    * ran prettier

---
+++ types/clamav.js/index.d.ts
@@ -0,0 +1,48 @@
+// Type definitions for clamav.js 0.12
+// Project: https://github.com/yongtang/clamav.js
+// Definitions by: NthMetal <https://github.com/NthMetal>
+// Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
+
+import { ReadStream } from 'fs';
+import { Stream } from 'stream';
+import * as glob from 'glob';
+
+export interface ClamScanner {
+    port: 3310 | number;
+    host: string | 'localhost';
+    scan: (object: ReadStream | Stream | string, callback: (error: Error, object: any, result: string) => any) => void;
+}
+export function createScanner(port: number, host: string): ClamScanner;
+export function ping(port: number, host: string, timeout: number, callback: (error?: Error) => any): void;
+export function version(
+    port: number,
+    host: string,
+    timeout: number,
+    callback: (error?: Error, status?: string) => any,
+): void;
+
+export class ClamAVChannel {
+    _transform(chunk: number, encoding: any, callback: () => any): void;
+    _flush(callback: () => any): void;
+}
+
+export function clamavstreamscan(
+    port: number,
+    host: string,
+    stream: Stream | ReadStream,
+    complete: (stream: Stream | ReadStream) => any,
+    object: any,
+    callback: (error: Error, object: any, result: string) => any,
+): void;
+export function clamavfilescan(
+    port: number,
+    host: string,
+    filename: string,
+    callback: (error: Error, object: any, result: string) => any,
+): void;
+export function clamavpathscan(
+    port: number,
+    host: string,
+    pathname: string,
+    callback: (error: Error, object: any, result: string) => any,
+): void;
