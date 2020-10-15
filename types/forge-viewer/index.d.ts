cc3c8c19a125c986522e60bae63ce791f502b06a
Petr Broz <ipetrbroz@gmail.com>
2020-10-15 12:05:33 +0200

    Adding myself to forge-viewer maintainers.

--- types/forge-viewer/index.d.ts
+++ types/forge-viewer/index.d.ts
@@ -1,6 +1,6 @@
 // Type definitions for non-npm package Forge Viewer 7.5
 // Project: https://forge.autodesk.com/en/docs/viewer/v7/reference/javascript/viewer3d/
-// Definitions by: Autodesk Forge Partner Development <https://github.com/Autodesk-Forge>, Alan Smith <https://github.com/alansmithnbs>, Jan Liska <https://github.com/liskaj>
+// Definitions by: Autodesk Forge Partner Development <https://github.com/Autodesk-Forge>, Alan Smith <https://github.com/alansmithnbs>, Jan Liska <https://github.com/liskaj>, Petr Broz <https://github.com/petrbroz>
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
 // TypeScript Version: 2.8
 
