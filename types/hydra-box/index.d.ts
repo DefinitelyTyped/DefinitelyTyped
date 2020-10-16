aee4f1c381dada5e4a74406f11b8ee5a34cae6d4
tpluscode <tpluscode@users.noreply.github.com>
2020-10-16 20:29:25 +0200

    feat: types for hydra-box

---
+++ types/hydra-box/index.d.ts
@@ -0,0 +1,49 @@
+// Type definitions for hydra-box 0.5
+// Project: https://github.com/zazuko/hydra-box
+// Definitions by: tpluscode <https://github.com/tpluscode>
+// Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
+// TypeScript Version: 3.4
+
+import express = require('express');
+import * as RDF from 'rdf-js';
+import { GraphPointer } from 'clownface';
+import middleware = require('./middleware');
+import Api = require('./Api');
+
+declare namespace HydraBox {
+    interface HydraBox {
+        api: Api;
+        term: RDF.NamedNode;
+        resource: Resource;
+        operation: GraphPointer;
+    }
+
+    interface Resource {
+        term: RDF.NamedNode;
+        dataset: RDF.DatasetCore;
+        types: Set<RDF.NamedNode>;
+    }
+
+    interface PropertyResource extends Resource {
+        property: RDF.Quad_Predicate;
+        object: RDF.NamedNode;
+    }
+
+    interface ResourceLoader {
+        forClassOperation(term: RDF.NamedNode, req: express.Request): Promise<Resource[]>;
+        forPropertyOperation(term: RDF.NamedNode, req: express.Request): Promise<PropertyResource[]>;
+    }
+}
+
+declare module 'express-serve-static-core' {
+    interface Request {
+        hydra: HydraBox.HydraBox;
+    }
+}
+
+declare const HydraBox: {
+    middleware: typeof middleware;
+    Api: typeof Api;
+};
+
+export = HydraBox;
