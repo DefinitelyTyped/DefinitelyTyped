281c67c68024fdf2725519167adf624cc23d966c
Piotr Błażejewicz <peterblazejewicz@users.noreply.github.com>
2020-10-16 21:21:52 +0200

    update(serve-favicon): correct `maxAge` type and other minor updates
    - the `maxAge` can be a string:
    https://github.com/expressjs/serve-favicon/blob/master/test/test.js#L67
    https://github.com/expressjs/serve-favicon#maxage
    - amend tests
    - add mantainer
    - DT lint fixes
    
    Thanks!

--- types/serve-favicon/serve-favicon-tests.ts
+++ types/serve-favicon/serve-favicon-tests.ts
@@ -1,8 +1,22 @@
-
 import express = require('express');
 import favicon = require('serve-favicon');
-var app = express();
 
-app.use(favicon(__dirname + '/public/favicon.ico', {
-    maxAge: 86400000
-}));
+const app = express();
+
+app.use(
+    favicon(__dirname + '/public/favicon.ico', {
+        maxAge: 86400000,
+    }),
+);
+
+app.use(
+    favicon(__dirname + '/public/favicon.ico', {
+        maxAge: '2 days',
+    }),
+);
+
+app.use(
+    favicon(__dirname + '/public/favicon.ico', {
+        maxAge: '2d',
+    }),
+);
