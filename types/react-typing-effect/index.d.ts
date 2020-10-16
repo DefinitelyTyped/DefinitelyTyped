5f2943bdef2a091a864c24a573c202377fabe4c6
Ross Dakin <rossdakin@gmail.com>
2020-10-16 19:34:50 -0400

    Remove patch number
    Per failed test guidance: https://github.com/DefinitelyTyped/DefinitelyTyped/commit/7e71210e95867c6384e3c49e5e1ca9d47eb5c518/checks\?check_suite_id\=1349276948

--- types/react-typing-effect/index.d.ts
+++ types/react-typing-effect/index.d.ts
@@ -1,4 +1,4 @@
-// Type definitions for react-typing-effect 2.0.4
+// Type definitions for react-typing-effect 2.0
 // Project: https://github.com/lamyfarai/react-typing-effect#readme
 // Definitions by: Debananda <https://github.com/Debananda>
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
