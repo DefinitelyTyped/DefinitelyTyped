9ff0043dbc2c2153e3a5a798c5110bf54b250165
Nick Amoscato <nick@amoscato.com>
2020-10-17 17:48:24 -0400

    Update react-copy-to-clipboard

--- types/react-copy-to-clipboard/react-copy-to-clipboard-tests.tsx
+++ types/react-copy-to-clipboard/react-copy-to-clipboard-tests.tsx
@@ -1,24 +1,22 @@
-import * as React from "react";
-import CopyToClipboard = require("react-copy-to-clipboard");
+import * as React from 'react';
+import { CopyToClipboard } from 'react-copy-to-clipboard';
 
-export class OnlyRequiredProps extends React.Component {
-    render() {
-        return (
-          <CopyToClipboard text={"Hello World"}>
-              <button>Copy to clipboard with button</button>
-          </CopyToClipboard>
-        );
-    }
+function OnlyRequiredProps() {
+    return (
+        <CopyToClipboard text={'Hello World'}>
+            <button>Copy to clipboard with button</button>
+        </CopyToClipboard>
+    );
 }
 
-export class AllProps extends React.Component {
-    render() {
-        return (
-          <CopyToClipboard text={"Hello World"}
-                onCopy={() => {}}
-                options={{debug: true, message: "message", format: "text/plain"}}>
+function AllProps() {
+    return (
+        <CopyToClipboard
+            text={'Hello World'}
+            onCopy={() => {}}
+            options={{ debug: true, message: 'message', format: 'text/plain' }}
+        >
             <span>Copy to clipboard with span</span>
-          </CopyToClipboard>
-        );
-    }
+        </CopyToClipboard>
+    );
 }
