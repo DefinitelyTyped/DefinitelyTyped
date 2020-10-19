a8c13228572a0e446562995f91ede6d850b47c30
Stuart Long <stuartlo@microsoft.com>
2020-10-19 11:46:41 -0700

    Add typings for url-regex-safe

---
+++ types/url-regex-safe/url-regex-safe-tests.ts
@@ -0,0 +1,5 @@
+import createUrlRegex = require("url-regex-safe");
+
+const regex: RegExp = createUrlRegex();
+
+const regexWithOptions: RegExp = createUrlRegex({ auth: true, tlds: [ ".domain" ] });
