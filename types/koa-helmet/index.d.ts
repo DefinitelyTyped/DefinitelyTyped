8a56b13c362f61e3aa6934000bfa95589ac2f793
Ben Salili-James <benhjames@sky.com>
2020-10-19 11:14:20 +0100

    Fix other packages that rely on removed helmet definitions

--- types/koa-helmet/index.d.ts
+++ types/koa-helmet/index.d.ts
@@ -15,8 +15,6 @@ declare namespace koaHelmet {
 
     type KoaHelmetCspDirectiveValue = string | KoaHelmetContentSecurityPolicyDirectiveFunction;
 
-    type KoaHelmetFeaturePolicyDirectiveValue = string;
-
     interface KoaHelmetContentSecurityPolicyDirectives {
         baseUri?: KoaHelmetCspDirectiveValue[];
         childSrc?: KoaHelmetCspDirectiveValue[];
@@ -36,52 +34,8 @@ declare namespace koaHelmet {
         styleSrc?: KoaHelmetCspDirectiveValue[];
     }
 
-    interface KoaHelmetFeaturePolicyDirectives {
-        accelerometer?: KoaHelmetFeaturePolicyDirectiveValue[];
-        ambientLightSensor?: KoaHelmetFeaturePolicyDirectiveValue[];
-        autoplay?: KoaHelmetFeaturePolicyDirectiveValue[];
-        camera?: KoaHelmetFeaturePolicyDirectiveValue[];
-        documentDomain?: KoaHelmetFeaturePolicyDirectiveValue[];
-        documentWrite?: KoaHelmetFeaturePolicyDirectiveValue[];
-        encryptedMedia?: KoaHelmetFeaturePolicyDirectiveValue[];
-        fontDisplayLateSwap?: KoaHelmetFeaturePolicyDirectiveValue[];
-        fullscreen?: KoaHelmetFeaturePolicyDirectiveValue[];
-        geolocation?: KoaHelmetFeaturePolicyDirectiveValue[];
-        gyroscope?: KoaHelmetFeaturePolicyDirectiveValue[];
-        layoutAnimations?: KoaHelmetFeaturePolicyDirectiveValue[];
-        legacyImageFormats?: KoaHelmetFeaturePolicyDirectiveValue[];
-        loadingFrameDefaultEager?: KoaHelmetFeaturePolicyDirectiveValue[];
-        magnetometer?: KoaHelmetFeaturePolicyDirectiveValue[];
-        microphone?: KoaHelmetFeaturePolicyDirectiveValue[];
-        midi?: KoaHelmetFeaturePolicyDirectiveValue[];
-        oversizedImages?: KoaHelmetFeaturePolicyDirectiveValue[];
-        payment?: KoaHelmetFeaturePolicyDirectiveValue[];
-        pictureInPicture?: KoaHelmetFeaturePolicyDirectiveValue[];
-        serial?: KoaHelmetFeaturePolicyDirectiveValue[];
-        speaker?: KoaHelmetFeaturePolicyDirectiveValue[];
-        syncScript?: KoaHelmetFeaturePolicyDirectiveValue[];
-        syncXhr?: KoaHelmetFeaturePolicyDirectiveValue[];
-        unoptimizedImages?: KoaHelmetFeaturePolicyDirectiveValue[];
-        unoptimizedLosslessImages?: KoaHelmetFeaturePolicyDirectiveValue[];
-        unoptimizedLossyImages?: KoaHelmetFeaturePolicyDirectiveValue[];
-        unsizedMedia?: KoaHelmetFeaturePolicyDirectiveValue[];
-        usb?: KoaHelmetFeaturePolicyDirectiveValue[];
-        verticalScroll?: KoaHelmetFeaturePolicyDirectiveValue[];
-        vibrate?: KoaHelmetFeaturePolicyDirectiveValue[];
-        vr?: KoaHelmetFeaturePolicyDirectiveValue[];
-        wakeLock?: KoaHelmetFeaturePolicyDirectiveValue[];
-        xr?: KoaHelmetFeaturePolicyDirectiveValue[];
-    }
-
-    interface KoaHelmetFeaturePolicyConfiguration {
-        features: KoaHelmetFeaturePolicyDirectives;
-    }
-
     interface KoaHelmetContentSecurityPolicyConfiguration {
         reportOnly?: boolean;
-        setAllHeaders?: boolean;
-        disableAndroid?: boolean;
-        browserSniff?: boolean;
         directives?: KoaHelmetContentSecurityPolicyDirectives;
     }
 
@@ -89,17 +43,15 @@ declare namespace koaHelmet {
         (options?: HelmetOptions): Middleware;
         contentSecurityPolicy(options?: KoaHelmetContentSecurityPolicyConfiguration): Middleware;
         dnsPrefetchControl(options?: HelmetOptions['dnsPrefetchControl']): Middleware;
+        expectCt(options?: HelmetOptions['expectCt']): Middleware;
         frameguard(options?: HelmetOptions['frameguard']): Middleware;
+        hidePoweredBy(options?: HelmetOptions['hidePoweredBy']): Middleware;
         hsts(options?: HelmetOptions['hsts']): Middleware;
-        ieNoOpen(): Middleware;
-        noCache(options?: any): Middleware;
-        noSniff(): Middleware;
+        ieNoOpen(options?: HelmetOptions['ieNoOpen']): Middleware;
+        noSniff(options?: HelmetOptions['noSniff']): Middleware;
+        permittedCrossDomainPolicies(options?: HelmetOptions['permittedCrossDomainPolicies']): Middleware;
         referrerPolicy(options?: HelmetOptions['referrerPolicy']): Middleware;
         xssFilter(options?: HelmetOptions['xssFilter']): Middleware;
-        hidePoweredBy(options?: HelmetOptions['hidePoweredBy']): Middleware;
-        permittedCrossDomainPolicies(options?: HelmetOptions['permittedCrossDomainPolicies']): Middleware;
-        featurePolicy(options: KoaHelmetFeaturePolicyConfiguration): Middleware;
-        expectCt(options?: HelmetOptions['expectCt']): Middleware;
     }
 }
 
