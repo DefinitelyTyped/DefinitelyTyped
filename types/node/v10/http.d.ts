fd2be8f09fcc00b629ebfdce89c27670c076db22
Jostein Kjønigsen <jostein@kjonigsen.net>
2020-10-19 15:58:03 +0200

    Remove set-cookie (v10)

--- types/node/v10/http.d.ts
+++ types/node/v10/http.d.ts
@@ -51,7 +51,6 @@ declare module "http" {
         'range'?: string;
         'referer'?: string;
         'retry-after'?: string;
-        'set-cookie'?: string[];
         'strict-transport-security'?: string;
         'tk'?: string;
         'trailer'?: string;
