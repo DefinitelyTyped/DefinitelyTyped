3895abed2f303056307fc775557bf323e7dfa2ac
Jeremy Jeanson <jeremy.jeanson@live.fr>
2020-10-20 23:44:11 +0200

    Add // Minimum TypeScript Version: 3.8

--- types/fitbit-asap/index.d.ts
+++ types/fitbit-asap/index.d.ts
@@ -2,6 +2,7 @@
 // Project: https://github.com/dillpixel/fitbit-asap
 // Definitions by: Jérémy Jeanson <https://github.com/JeremyJeanson>
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
+// Minimum TypeScript Version: 3.8
 
 export * as app from "./app";
 export * as companion from "./companion";
