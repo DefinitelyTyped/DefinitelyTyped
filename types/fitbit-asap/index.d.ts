13d4b11e857c0f5e6798032e3d9e4fae1d673b8e
Jeremy Jeanson <jeremy.jeanson@live.fr>
2020-10-20 09:35:32 +0200

    Add fitbit-asap for Fitbit OS

---
+++ types/fitbit-asap/index.d.ts
@@ -0,0 +1,7 @@
+// Type definitions for fitbit-asap 1.2
+// Project: https://github.com/dillpixel/fitbit-asap
+// Definitions by: Jérémy Jeanson <https://github.com/JeremyJeanson>
+// Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
+
+export * from "./app";
+export { Asap, Options } from "./companion";
