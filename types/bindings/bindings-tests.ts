9a5f9edde6a7dabe69f87813f0a376972eec3051
Boris Gulay <boris@gulay.name>
2020-10-20 10:24:33 +0300

    Update definitions for bindings package

--- types/bindings/bindings-tests.ts
+++ types/bindings/bindings-tests.ts
@@ -1,4 +1,7 @@
-import bindings = require('bindings');
+import bindings from 'bindings';
 
 // Use your bindings defined in your C files
 const result = bindings('binding.node').your_c_function();
+
+// The same with configuration object
+const result1 = bindings({ bindings: 'bindings.node' }).your_c_function();
--- types/bindings/index.d.ts
+++ types/bindings/index.d.ts
@@ -1,13 +1,65 @@
 // Type definitions for bindings 1.3
 // Project: https://github.com/TooTallNate/node-bindings
-// Definitions by: Daniel Perez Alvarez <https://github.com/unindented>
+// Definitions by: Daniel Perez Alvarez <https://github.com/unindented>, Boris Gulay <https://github.com/BoresXP>
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
+
+/// <reference types="node" />
+
+export interface BindingOptions {
+	/**
+	 * Arrow symbol used in error text on failed loading (default: process.env.NODE_BINDINGS_ARROW || ' → ').
+	 */
+	arrow?: string;
+
+	/**
+	 * Name of directory with compiled modules (default: process.env.NODE_BINDINGS_COMPILED_DIR || 'compiled').
+	 */
+	compiled?: string;
+
+	/**
+	 * Platform of running process (default: process.platform).
+	 */
+	platform?: NodeJS.Platform;
+
+	/**
+	 * Architectore of the process (default: process.arch).
+	 */
+	arch?: string;
+
+	/**
+	 * Name of directory used by NodeGyp (default: 'node-v' + process.versions.modules + '-' + process.platform + '-' + process.arch).
+	 */
+	nodePreGyp?: string;
+
+	/**
+	 * Version of NodeJS running script (default: process.versions.node).
+	 */
+	version?: string;
+
+	/**
+	 * Name of native module to be loaded (default: 'bindings.node').
+	 */
+	bindings?: string;
+
+	/**
+	 * Array of arrays of strings with path components. Used to build full paths to modules to be loaded.
+	 * Can contain keywords, that will be translated during build of result path:
+	 * - module_root - root directory of current module
+	 * - bindings - BindingOptions.bindings
+	 * - compiled - BindingOptions.compiled
+	 * - platform - BindingOptions.platform
+	 * - arch - BindingOptions.arch
+	 * - nodePreGyp - BindingOptions.nodePreGyp
+	 * - version - BindingOptions.version
+	 */
+    try?: string[][];
+}
 
 /**
  * The main `bindings()` function loads the compiled bindings for a given module.
  * It uses V8's Error API to determine the parent filename that this function is
  * being invoked from, which is then used to find the root directory.
+ * @param mod Module name or loading configuration
+ * @returns loaded module or undefined
  */
-declare function bindings(mod: string): any;
-
-export = bindings;
+export default function bindings(mod: string | BindingOptions): any;
