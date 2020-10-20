759b4cb2339ff12a22c8101f143a7a68ed535c70
Maxim Mazurok <maxim@mazurok.com>
2020-10-20 11:25:36 +0000

    automatic gapi.client.calendar update @ Tue, 20 Oct 2020 11:25:36 GMT

--- types/gapi.client.calendar/gapi.client.calendar-tests.ts
+++ types/gapi.client.calendar/gapi.client.calendar-tests.ts
@@ -4,7 +4,7 @@
 * In case of any problems please post issue to https://github.com/Maxim-Mazurok/google-api-typings-generator
 **/
 
-// Revision: 20200917
+// Revision: 20201015
 
 gapi.load('client', () => {
     /** now we can use gapi.client */
