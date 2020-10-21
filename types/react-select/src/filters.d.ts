bbcdf0c5262a132dfe87fb8464c9e0fa7019dcf9
Steve Dignam <steve@dignam.xyz>
2020-10-21 18:13:40 -0400

    make the linter happy?

--- types/react-select/src/filters.d.ts
+++ types/react-select/src/filters.d.ts
@@ -10,7 +10,7 @@ import { stripDiacritics } from './diacritics';
 
 export interface Option { label: string; value: string; data: any; }
 
-export function createFilter<T = Option>(config: Config | null): (
+export function createFilter<T extends { value: any; label: any } = Option>(config: Config | null): (
   option: T,
   rawInput: string
 ) => boolean;
