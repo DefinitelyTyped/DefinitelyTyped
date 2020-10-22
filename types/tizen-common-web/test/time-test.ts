30347f889aaa1c7a7d9f848e062400af634a0467
Egor Shulga <egorshulga@outlook.com>
2020-10-22 11:05:42 +0300

    revert time changes

--- types/tizen-common-web/test/time-test.ts
+++ types/tizen-common-web/test/time-test.ts
@@ -1,4 +1,4 @@
-import { TZDate } from "tizen-common-web/time";
+import { TimeDurationUnit, TZDate } from "tizen-common-web/time";
 
 const timezone: string = tizen.time.getLocalTimezone();
 
@@ -7,7 +7,7 @@ const isLeapYear: boolean = tizen.time.isLeapYear(2020);
 let datetime: TZDate = tizen.time.getCurrentDateTime();
 datetime.equalsTo(tizen.time.getCurrentDateTime());
 const hours: number = datetime.getHours();
-datetime = datetime.addDuration(new tizen.TimeDuration(1, "DAYS"));
+datetime = datetime.addDuration(new tizen.TimeDuration(1, TimeDurationUnit.DAYS));
 
 const now = new tizen.TZDate(new Date(), "Asia/Manila");
 console.log(now.toString());
