ffbb2e433920f7f45eaebc1a3f510daeec46cf72
Haroen Viaene <hello@haroen.me>
2020-10-22 09:48:34 +0200

    prevent wrong lint

--- types/react-instantsearch-core/index.d.ts
+++ types/react-instantsearch-core/index.d.ts
@@ -190,8 +190,8 @@ export interface AutocompleteExposed {
   defaultRefinement?: string;
 }
 
-// tslint:disable-next-line:no-unnecessary-generics
 export function connectAutoComplete<TDoc = BasicDoc>(
+  // tslint:disable-next-line:no-unnecessary-generics
   stateless: React.FunctionComponent<AutocompleteProvided<TDoc>>
 ): React.ComponentClass<AutocompleteExposed>;
 export function connectAutoComplete<Props extends AutocompleteProvided<TDoc>, TDoc = BasicDoc>(
@@ -345,8 +345,8 @@ export interface HitsProvided<THit> {
  *
  * https://community.algolia.com/react-instantsearch/connectors/connectHits.html
  */
-// tslint:disable-next-line:no-unnecessary-generics
 export function connectHits<THit = BasicDoc>(
+  // tslint:disable-next-line:no-unnecessary-generics
   stateless: React.FunctionComponent<HitsProvided<THit>>
 ): React.ComponentClass;
 export function connectHits<TProps extends HitsProvided<THit>, THit>(
