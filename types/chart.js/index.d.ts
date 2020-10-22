c56edc9cfab61d49d5b6c678bab3637dbeb36689
leapwill <leapwill@users.noreply.github.com>
2020-10-22 12:36:52 -0500

    Use correct type according to chart.js owner

--- types/chart.js/index.d.ts
+++ types/chart.js/index.d.ts
@@ -729,7 +729,7 @@ declare namespace Chart {
         type?: ScaleType | string;
         display?: boolean | string;
         id?: string;
-        labels?: Array<string | string[] | number | number[] | Date | Date[] | Moment | Moment[]>;
+        labels?: string[];
         stacked?: boolean;
         position?: string;
         ticks?: TickOptions;
