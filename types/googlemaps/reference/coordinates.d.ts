5ff6b4562d392bcee6afdbcbbf299126e52ac914
Dmitry Demensky <dmitry@demensky.com>
2020-10-23 23:25:17 +0300

    [googlemaps] coordinates: Add LatLngBoundsLiteral JSDoc

--- types/googlemaps/reference/coordinates.d.ts
+++ types/googlemaps/reference/coordinates.d.ts
@@ -153,13 +153,42 @@ declare namespace google.maps {
         union(other: LatLngBounds | LatLngBoundsLiteral): LatLngBounds;
     }
 
+    /**
+     * Object literals are accepted in place of {@link LatLngBounds} objects throughout the API. These are automatically
+     * converted to {@link LatLngBounds} objects. All {@link LatLngBoundsLiteral#south south},
+     * {@link LatLngBoundsLiteral#west west}, {@link LatLngBoundsLiteral#north north} and
+     * {@link LatLngBoundsLiteral#east east} must be set, otherwise an exception is thrown.
+     * @see {@link https://developers.google.com/maps/documentation/javascript/reference/coordinates#LatLngBoundsLiteral Maps JavaScript API}
+     */
     interface LatLngBoundsLiteral {
+        /**
+         * East longitude in degrees. Values outside the range [-180, 180] will be wrapped to the range [-180, 180]. For
+         * example, a value of -190 will be converted to 170. A value of 190 will be converted to -170. This reflects
+         * the fact that longitudes wrap around the globe.
+         * @see {@link https://developers.google.com/maps/documentation/javascript/reference/coordinates#LatLngBoundsLiteral.east Maps JavaScript API}
+         */
         east: number;
 
+        /**
+         * North latitude in degrees. Values will be clamped to the range [-90, 90]. This means that if the value
+         * specified is less than -90, it will be set to -90. And if the value is greater than 90, it will be set to 90.
+         * @see {@link https://developers.google.com/maps/documentation/javascript/reference/coordinates#LatLngBoundsLiteral.north Maps JavaScript API}
+         */
         north: number;
 
+        /**
+         * South latitude in degrees. Values will be clamped to the range [-90, 90]. This means that if the value
+         * specified is less than -90, it will be set to -90. And if the value is greater than 90, it will be set to 90.
+         * @see {@link https://developers.google.com/maps/documentation/javascript/reference/coordinates#LatLngBoundsLiteral.south Maps JavaScript API}
+         */
         south: number;
 
+        /**
+         * West longitude in degrees. Values outside the range [-180, 180] will be wrapped to the range [-180, 180]. For
+         * example, a value of -190 will be converted to 170. A value of 190 will be converted to -170. This reflects
+         * the fact that longitudes wrap around the globe.
+         * @see {@link https://developers.google.com/maps/documentation/javascript/reference/coordinates#LatLngBoundsLiteral.west Maps JavaScript API}
+         */
         west: number;
     }
 
