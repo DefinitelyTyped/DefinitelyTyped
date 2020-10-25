c8acc4bccdc5ebcbce438de47c87655f2a5e17c0
Itai Spector <spector37@gmail.com>
2020-10-25 13:20:19 +0200

    added annotation optional property

--- types/chart.js/index.d.ts
+++ types/chart.js/index.d.ts
@@ -308,6 +308,7 @@ declare namespace Chart {
         rotation?: number;
         devicePixelRatio?: number;
         plugins?: ChartPluginsOptions;
+        annotation?: Object;
     }
 
     interface ChartFontOptions {
