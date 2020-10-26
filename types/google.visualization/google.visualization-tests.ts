95b7d0daa24a3e72c0d10009fbc3396323074e54
Dan Manastireanu <498419+danmana@users.noreply.github.com>
2020-10-26 15:14:15 +0200

    feat: Add Gauge chart to @types/google.visualization. Closes #35516

--- types/google.visualization/google.visualization-tests.ts
+++ types/google.visualization/google.visualization-tests.ts
@@ -761,3 +761,31 @@ function test_chartChart() {
         }
     }
 }
+
+function test_GaugeChart() {
+    var data = google.visualization.arrayToDataTable([
+        ['Label', 'Value'],
+        ['Memory', 80],
+        ['CPU', 55],
+        ['Network', 68]
+    ]);
+
+    var options = {
+        title: 'Company Performance'
+    };
+
+    var container = document.getElementById('chart_div');
+    if (container) {
+        var chart = new google.visualization.Gauge(container);
+
+        chart.draw(data, {
+            width: 400,
+            height: 120,
+            redFrom: 90,
+            redTo: 100,
+            yellowFrom: 75,
+            yellowTo: 90,
+            minorTicks: 5
+        });
+    }
+}
