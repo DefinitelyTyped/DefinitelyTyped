0559b158714c26041a0d3c9ef4d977c301272f16
Enmanuel Veras <enmaveras07@gmail.com>
2020-10-26 14:25:09 -0400

    Fixes react-time-picker props adding typescript 3.5 requirement

--- types/react-time-picker/index.d.ts
+++ types/react-time-picker/index.d.ts
@@ -2,6 +2,7 @@
 // Project: https://github.com/wojtekmaj/react-time-picker
 // Definitions by: Enmanuel Veras <https://github.com/everas7>
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
+// TypeScript Version: 3.5
 
 import { ClockProps } from 'react-clock';
 
