f1c457c255758a8e4269b45c1b4bc200b5b0ca1a
Kyle Albert <5498623+kalbert312@users.noreply.github.com>
2020-10-26 09:58:26 -0400

    Update DatePickerSkeleton.

--- types/carbon-components-react/lib/components/DatePicker/DatePicker.Skeleton.d.ts
+++ types/carbon-components-react/lib/components/DatePicker/DatePicker.Skeleton.d.ts
@@ -1,5 +1,5 @@
 import * as React from "react";
-import { ReactAttr, ReactDivAttr } from "../../../typings/shared";
+import { ReactDivAttr } from "../../../typings/shared";
 
 interface InheritedProps extends ReactDivAttr { }
 
