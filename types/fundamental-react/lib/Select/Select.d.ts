49180264a0408eed2e0fc17e2a09ede78c26594b
Auryn Engel <auryn.engel@sap.com>
2020-10-27 13:33:20 +0100

    update select to fundamental-react 0.13

--- types/fundamental-react/lib/Select/Select.d.ts
+++ types/fundamental-react/lib/Select/Select.d.ts
@@ -1,21 +1,33 @@
-import * as React from "react";
+import * as React from 'react';
 
 export interface SelectProps {
     className?: string;
     compact?: boolean;
     disabled?: boolean;
     disableStyles?: boolean;
+    emptyAriaLabel?: string;
     id?: string;
+    includeEmptyOption?: boolean;
+    listClassName?: string;
+    listItemClassName?: string;
+    listItemTextClassName?: string;
     placeholder?: string;
+    readOnly?: boolean;
+    selectedKey?: string;
+    textContentClassName?: string;
+    triggerClassName?: string;
     validationState?: {
         state?: 'error' | 'warning' | 'information' | 'success';
         text?: string;
     };
+    options?: { key: string; text: string }[];
     onClick?: (...args: any[]) => any;
+    onBlur?: (...args: any[]) => any;
+    onSelect?: (...args: any[]) => any;
 }
 
 declare const Select: React.FunctionComponent<SelectProps> & {
-    displayName: "Select";
+    displayName: 'Select';
 };
 
 export default Select;
