9607fe411cb8e0d7610d92e615dce814375d118b
Shikanime Deva <deva.shikanime@protonmail.com>
2020-10-27 20:24:54 +0100

    Add minimum 3box test

---
+++ types/3box/3box-min-tests.ts
@@ -0,0 +1,14 @@
+import Box = require('3box');
+
+declare global {
+    interface Window {
+        ethereum: any;
+    }
+}
+
+(async () => {
+    const box = await Box.openBox('foo', window.ethereum);
+    const space = await box.openSpace('bar');
+    const thread = await space.joinThread('baz');
+    await thread.post('heelo 3box');
+})();
