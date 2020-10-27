69bfa1e4eab2f6e928d5297710c49241bc93abbd
Steve Dignam <steve@dignam.xyz>
2020-10-27 14:44:17 -0400

    update create filter

--- types/react-select/src/filters.d.ts
+++ types/react-select/src/filters.d.ts
@@ -11,7 +11,7 @@ import { stripDiacritics } from './diacritics';
 export interface Option { label: string; value: string; data: any; }
 
 /* tslint:disable:no-unnecessary-generics */
-export function createFilter<T extends { value: any; label: any } = Option>(config: Config | null): (
+export function createFilter<T extends { label: any; value: string; data?: any; }>(config: Config | null): (
   option: T,
   rawInput: string
 ) => boolean;
