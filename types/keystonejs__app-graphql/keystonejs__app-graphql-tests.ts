fd2110b1b0b36310d14b3893062910af05fd45d7
Kevin Brown <kevin.brown@techin.site>
2020-10-28 11:57:13 +1100

    Added tests, removed unused import.

--- types/keystonejs__app-graphql/keystonejs__app-graphql-tests.ts
+++ types/keystonejs__app-graphql/keystonejs__app-graphql-tests.ts
@@ -1,5 +1,34 @@
+import { Keystone } from '@keystonejs/keystone';
+import { KnexAdapter } from '@keystonejs/adapter-knex';
 import { GraphQLApp, validation } from '@keystonejs/app-graphql';
 
+const keystone = new Keystone({
+    name: 'test',
+    adapter: new KnexAdapter(),
+});
+
 new GraphQLApp();
+const app = new GraphQLApp({
+    apiPath: 'anything',
+    graphiqlPath: 'anything',
+    schemaName: 'anything',
+    apollo: {
+        validationRules: [],
+        introspection: true,
+    },
+});
+
+app.build();
+
+// $ExpectType Express
+app.prepareMiddleware({
+    keystone,
+    dev: true,
+});
+
+// $ExpectType Express
+app.prepareMiddleware({
+    keystone,
+});
 
 validation.depthLimit(1);
