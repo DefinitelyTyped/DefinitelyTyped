46e1c137fdaa29afc5ad86c440e9abb3026a014d
Niels Kersic <hi@niels.codes>
2020-10-28 23:54:55 +0100

    Added definitions for canvas-txt

--- types/canvas-txt/index.d.ts
+++ types/canvas-txt/index.d.ts
@@ -1,9 +1,10 @@
 // Type definitions for canvas-txt 3.0
 // Project: https://canvas-txt.geongeorge.com
-// Definitions by: Niels Kersic <https://github.com/nielskersic> (https://niels.codes)
+// Definitions by: Niels Kersic <https://github.com/nielskersic>
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
 
-/** Draws text on a 2D canvas context
+/**
+ * Draws text on a 2D canvas context
  * @param ctx 2D canvas context
  * @param text Text to draw
  * @param x X-axis offset
@@ -24,63 +25,63 @@ export function drawText(
  * Shows the border and align gravity for debugging purposes
  * @default false
  */
-export var debug: boolean;
+export let debug: boolean;
 
 /**
  * Horizontal text alignment
  * @default 'center'
  */
-export var align: 'center' | 'left' | 'right';
+export let align: 'center' | 'left' | 'right';
 
 /**
  * Vertical text alignment
  * @default 'middle'
  */
-export var vAlign: 'middle' | 'top' | 'bottom';
+export let vAlign: 'middle' | 'top' | 'bottom';
 
 /**
  * Font size of the text in pixels
  * @default 14
  */
-export var fontSize: number;
+export let fontSize: number;
 
 /**
  * Font family of the text
  * @default 'Arial'
  */
-export var font: string;
+export let font: string;
 
 /**
  * Font style. Same as CSS `font-style`
  * @example 'italic'
  * @example 'oblique 40deg'
  */
-export var fontStyle: string;
+export let fontStyle: string;
 
 /**
- * Font variant. Same as CSS `font-variant`
+ * Font letiant. Same as CSS `font-letiant`
  * @example 'small-caps'
  * @example 'slashed-zero'
  */
-export var fontVariant: string;
+export let fontVariant: string;
 
 /**
  * Font weight. Same as CSS `font-weight`
  * @example 'bold'
  * @example 100
  */
-export var fontWeight: string | number;
+export let fontWeight: string | number;
 
 /**
  * Line height of the text.
  * If set to `null`, canvas-txt will attempt to auto-detect the line height.
  * @default null
  */
-export var lineHeight: number | null;
+export let lineHeight: number | null;
 
 /**
  * Justify text if `true`.
  * Will insert spaces between words when necessary
  * @default false
  */
-export var justify: boolean;
+export let justify: boolean;
