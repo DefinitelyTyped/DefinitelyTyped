ffec328a7915ddfa6e201f5843cd3a1f01cbbfc0
sly <khmedj@gmail.com>
2020-10-28 18:10:51 -0700

    [belter] add new typing

---
+++ types/belter/belter-tests.ts
@@ -0,0 +1,4 @@
+import belter = require('belter');
+
+// String have a trim function
+belter.getUserAgent().trim();
---
+++ types/belter/index.d.ts
@@ -0,0 +1,8 @@
+// Type definitions for belter 1.0
+// Project: https://github.com/krakenjs/belter#readme
+// Definitions by: sly <https://github.com/SlyBouhafs>
+// Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
+
+export {};
+
+export function getUserAgent(): string;
