cfa093e1ba337b00cb26c27e595617b09169d28c
Jack Works <jackworks@protonmail.com>
2020-10-29 23:58:32 +0800

    Update index.d.ts

--- types/snowpack-env/index.d.ts
+++ types/snowpack-env/index.d.ts
@@ -37,6 +37,7 @@ interface ImportMetaHot {
 
 interface ImportMeta {
     readonly url: string;
+    readonly SSR?: boolean;
     readonly hot?: ImportMetaHot;
     readonly env: {
         readonly [key: string]: any;
