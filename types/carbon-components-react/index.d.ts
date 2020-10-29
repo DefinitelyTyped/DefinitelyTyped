d9f0e3c8d907af108cc44857efe6beb59be7dbc7
Kyle Albert <5498623+kalbert312@users.noreply.github.com>
2020-10-29 16:09:40 -0400

    Update to 7.23.

--- types/carbon-components-react/index.d.ts
+++ types/carbon-components-react/index.d.ts
@@ -1,4 +1,4 @@
-// Type definitions for carbon-components-react 7.22
+// Type definitions for carbon-components-react 7.23
 // Project: https://github.com/carbon-design-system/carbon/tree/master/packages/react
 // Definitions by: Kyle Albert <https://github.com/kalbert312>
 //                 Sebastien Gregoire <https://github.com/sgregoire>
