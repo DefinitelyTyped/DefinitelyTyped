53299966e30129f3f80387f4d58ceff443fc0f9a
Eggbertx <eggbertx@users.noreply.github.com>
2020-10-31 04:45:26 -0700

    Add required tsconfig keys and documentation URLs

--- types/sphere-engine/index.d.ts
+++ types/sphere-engine/index.d.ts
@@ -5,3 +5,7 @@
 
 /// <reference path="sphere1-api.d.ts" />
 /// <reference path="sphere2-api.d.ts" />
+
+
+// documentation for Sphere 2 API: https://github.com/fatcerberus/miniSphere/tree/master/docs
+// documentation for Sphere 1.x API: https://github.com/sphere-group/sphere/tree/master/sphere/docs/development
