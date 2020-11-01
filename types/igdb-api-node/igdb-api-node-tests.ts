6100a545593a688cee29cfaaf1a040539f9f200d
Melanie Seltzer <melleh11@gmail.com>
2020-10-31 20:10:01 -0700

    Update types for igdb-api-node v5.0

--- types/igdb-api-node/igdb-api-node-tests.ts
+++ types/igdb-api-node/igdb-api-node-tests.ts
@@ -7,9 +7,9 @@ getTagNumber(5, 1234);
 // $ExpectType Apicalypse
 igdb();
 // $ExpectType Apicalypse
-igdb('test-api-key');
+igdb('test-client-id', 'test-app-access-token');
 // $ExpectType Apicalypse
-igdb('test-api-key', {
+igdb('test-client-id', 'test-app-access-token', {
     queryMethod: 'url',
     method: 'get',
 });
