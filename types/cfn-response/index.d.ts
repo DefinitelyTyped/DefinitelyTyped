9e3cd5a93b13cf0d63bb5f756cf1bf3c36793278
arturovt <arthurandrosovich@gmail.com>
2020-11-02 15:41:14 +0200

    update(cfn-response): update types

--- types/cfn-response/index.d.ts
+++ types/cfn-response/index.d.ts
@@ -5,11 +5,11 @@
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
 // TypeScript Version: 3.0
 
-import { CloudFormationCustomResourceEvent, Context } from "aws-lambda";
+import { CloudFormationCustomResourceEvent, Context } from 'aws-lambda';
 
-export type ResponseStatus = "SUCCESS" | "FAILED";
-export const SUCCESS: ResponseStatus;
-export const FAILED: ResponseStatus;
+export const SUCCESS: 'SUCCESS';
+export const FAILED: 'FAILED';
+export type ResponseStatus = typeof SUCCESS | typeof FAILED;
 
 export function send(
     event: CloudFormationCustomResourceEvent,
