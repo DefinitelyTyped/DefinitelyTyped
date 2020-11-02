459a8e7f78adb4ce6f5a9b303d5957c7b36edbe4
arturovt <arthurandrosovich@gmail.com>
2020-11-02 02:47:35 +0200

    feat(brotli): new definition

---
+++ types/brotli/decompress.d.ts
@@ -0,0 +1,5 @@
+/// <reference types="node" />
+
+declare function decompress(buffer: Buffer, outputSize?: number): Uint8Array;
+
+export = decompress;
---
+++ types/brotli/index.d.ts
@@ -0,0 +1,10 @@
+// Type definitions for brotli 1.3
+// Project: https://github.com/foliojs/brotli.js
+// Definitions by: Artur Androsovych <https://github.com/arturovt>
+// Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
+// TypeScript Version: 3.0
+
+import compress = require('./compress');
+import decompress = require('./decompress');
+
+export { compress, decompress };
