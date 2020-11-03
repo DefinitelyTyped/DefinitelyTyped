70456fd7f2b601cf7da1de1dd6c3259e8899c68a
Eric Liu <ericyl.us@gmail.com>
2020-11-03 12:16:44 -0800

    fix: remove removed-component.d.ts

--- types/carbon-components/components/removed-component.d.ts
+++
@@ -1,8 +0,0 @@
-declare const removedComponent: (name: any) => {
-    new (): {};
-    create(): void;
-    init(): void;
-    components: WeakMap<object, any>;
-    options: {};
-};
-export default removedComponent;
