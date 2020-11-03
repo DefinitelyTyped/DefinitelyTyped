0e3a54786067756d007baba678c17457bd391187
Eric Liu <ericyl.us@gmail.com>
2020-11-03 12:12:05 -0800

    fix: remove unused config declarations

--- types/carbon-components/components/copy-button/copy-button.config.d.ts
+++
@@ -1,6 +0,0 @@
-declare const _default: {
-    context: {
-        prefix: any;
-    };
-};
-export default _default;
--- types/carbon-components/components/data-table-v2/data-table-v2.config.d.ts
+++
@@ -1,4 +0,0 @@
-declare const _default: {
-    hidden: boolean;
-};
-export default _default;
