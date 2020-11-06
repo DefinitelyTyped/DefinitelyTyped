72b73a9ac404b75d0f4fdcfa8b1a058d657c5e2a
Eric Liu <ericyl.us@gmail.com>
2020-11-06 12:35:19 -0800

    fix(ci): remove es/lib imports from test file

--- types/carbon__icons-react/carbon__icons-react-tests.tsx
+++ types/carbon__icons-react/carbon__icons-react-tests.tsx
@@ -13,13 +13,7 @@ import {
     ChartColumn16,
     LogoKeybase16,
 } from '@carbon/icons-react';
-import ES__Flag16 from "@carbon/icons-react/lib/flag/16";
-import ES__Flag24 from "@carbon/icons-react/es/flag/24";
-import ES__WatsonHealth_3D_Cursor24 from "@carbon/icons-react/es/watson-health/3D-Cursor/24";
 
-<ES__Flag16 aria-label='4'/>; // $ExpectType Element
-<ES__Flag24 aria-label='4'/>; // $ExpectType Element
-<ES__WatsonHealth_3D_Cursor24 aria-label='4'/>; // $ExpectType Element
 <AccessibilityAlt16 />; // $ExpectType Element
 <Add16><title>Icon title</title></Add16>; // $ExpectType Element
 <Add24 aria-label="Add" />; // $ExpectType Element
