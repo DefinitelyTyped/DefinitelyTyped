e600770dd8f38edcc5a28b0f21f67f6678c9eb66
Moritz Mahringer <me@moritzmahringer.de>
2020-11-06 05:17:23 +0100

    [text-to-svg] use class template structure

--- types/text-to-svg/index.d.ts
+++ types/text-to-svg/index.d.ts
@@ -5,81 +5,9 @@
 
 import { Font } from 'opentype.js';
 
-export type Anchor =
-    | 'left baseline'
-    | 'left top'
-    | 'left bottom'
-    | 'left middle'
-    | 'center baseline'
-    | 'center top'
-    | 'center bottom'
-    | 'center middle'
-    | 'right baseline'
-    | 'right top'
-    | 'right bottom'
-    | 'right middle';
+export = TextToSVG;
 
-export interface Metrics {
-    x: number;
-    y: number;
-    baseline: number;
-    width: number;
-    height: number;
-    ascender: number;
-    descender: number;
-}
-
-export interface FontOptions {
-    /**
-     * Horizontal position of the beginning of the text.
-     * @default 0
-     */
-    x?: number | null;
-
-    /**
-     * Vertical position of the baseline of the text.
-     * @default 0
-     */
-    y?: number | null;
-
-    /**
-     * Size of the text.
-     * @default 72
-     */
-    fontSize?: number | null;
-
-    /**
-     * If true takes kerning information into account.
-     * @default true
-     */
-    kerning?: boolean | null;
-
-    /**
-     * Letter-spacing value in em.
-     */
-    letterSpacing?: number | null;
-
-    /**
-     * Tracking value in (em / 1000).
-     */
-    tracking?: number | null;
-
-    /**
-     * @default "left baseline"
-     */
-    anchor?: Anchor | null;
-}
-
-export interface GenerationOptions extends FontOptions {
-    /**
-     * Key-value pairs of attributes for `<path>` element.
-     */
-    attributes?: { [key: string]: string } | null;
-}
-
-export type LoadCallback = (error: Error | null, textToSVG: TextToSVG | null) => void;
-
-export default class TextToSVG {
+declare class TextToSVG {
     /**
      * Create an instance of the SVG generator, using an already parsed font file.
      *
@@ -106,7 +34,7 @@ export default class TextToSVG {
      * @param url of font to load
      * @param callback called, when instance has been created
      */
-    static load(url: string, callback: LoadCallback): void;
+    static load(url: string, callback: TextToSVG.LoadCallback): void;
 
     /**
      * Measure the width of the specified text.
@@ -115,7 +43,7 @@ export default class TextToSVG {
      * @param options font options
      * @returns width of the text
      */
-    getWidth(text: string, options?: FontOptions | null): number;
+    getWidth(text: string, options?: TextToSVG.FontOptions | null): number;
 
     /**
      * Measure the height of the font.
@@ -131,7 +59,7 @@ export default class TextToSVG {
      * @param text to measure
      * @param options font options
      */
-    getMetrics(text: string, options?: FontOptions | null): Metrics;
+    getMetrics(text: string, options?: TextToSVG.FontOptions | null): TextToSVG.Metrics;
 
     /**
      * Generate SVG as a string with text converted to paths.
@@ -140,7 +68,7 @@ export default class TextToSVG {
      * @param options font options and attributes
      * @returns SVG string
      */
-    getSVG(text: string, options?: GenerationOptions | null): string;
+    getSVG(text: string, options?: TextToSVG.GenerationOptions | null): string;
 
     /**
      * Generate SVG `<path>` from text.
@@ -149,7 +77,7 @@ export default class TextToSVG {
      * @param options font options and attributes
      * @returns SVG path element string
      */
-    getPath(text: string, options?: GenerationOptions | null): string;
+    getPath(text: string, options?: TextToSVG.GenerationOptions | null): string;
 
     /**
      * Generate SVG `<path>` `d` value.
@@ -158,5 +86,81 @@ export default class TextToSVG {
      * @param options font options and attributes
      * @returns SVG path d attribute value
      */
-    getD(text: string, options?: GenerationOptions | null): string;
+    getD(text: string, options?: TextToSVG.GenerationOptions | null): string;
+}
+
+declare namespace TextToSVG {
+    type Anchor =
+        | 'left baseline'
+        | 'left top'
+        | 'left bottom'
+        | 'left middle'
+        | 'center baseline'
+        | 'center top'
+        | 'center bottom'
+        | 'center middle'
+        | 'right baseline'
+        | 'right top'
+        | 'right bottom'
+        | 'right middle';
+
+    interface Metrics {
+        x: number;
+        y: number;
+        baseline: number;
+        width: number;
+        height: number;
+        ascender: number;
+        descender: number;
+    }
+
+    interface FontOptions {
+        /**
+         * Horizontal position of the beginning of the text.
+         * @default 0
+         */
+        x?: number | null;
+
+        /**
+         * Vertical position of the baseline of the text.
+         * @default 0
+         */
+        y?: number | null;
+
+        /**
+         * Size of the text.
+         * @default 72
+         */
+        fontSize?: number | null;
+
+        /**
+         * If true takes kerning information into account.
+         * @default true
+         */
+        kerning?: boolean | null;
+
+        /**
+         * Letter-spacing value in em.
+         */
+        letterSpacing?: number | null;
+
+        /**
+         * Tracking value in (em / 1000).
+         */
+        tracking?: number | null;
+
+        /**
+         * @default "left baseline"
+         */
+        anchor?: Anchor | null;
+    }
+
+    interface GenerationOptions extends FontOptions {
+        /**
+         * Key-value pairs of attributes for `<path>` element.
+         */
+        attributes?: { [key: string]: string } | null;
+    }
+
+    type LoadCallback = (error: Error | null, textToSVG: TextToSVG | null) => void;
 }
