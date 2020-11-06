b3f7c55a1b046d336393b2c07d736a8ee57f253c
Wilson Gramer <wilson@gramer.dev>
2020-11-05 19:43:16 -0500

    Fix dependencies using DT typings for old versions of highlight.js

--- types/highlightjs/index.d.ts
+++ types/highlightjs/index.d.ts
@@ -1,7 +1,8 @@
-// Type definitions for highlightjs 10.1
+// Type definitions for highlight.js 10.1
 // Project: https://github.com/isagalaev/highlight.js
 // Definitions by: Florian Keller <https://github.com/ffflorian>
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
+// TypeScript Version: 3.5
 
 import highlightjs = require('highlight.js');
 
