4e9f2355d48ae53365b6ea7a7a983b5d6b832a92
tuuling <reedik.tuuling@gmail.com>
2020-11-07 21:11:46 +0200

    Bump version from 2.6 to 2.7

--- types/d3-array/index.d.ts
+++ types/d3-array/index.d.ts
@@ -1,4 +1,4 @@
-// Type definitions for D3JS d3-array module 2.6
+// Type definitions for D3JS d3-array module 2.7
 // Project: https://github.com/d3/d3-array, https://d3js.org/d3-array
 // Definitions by: Alex Ford <https://github.com/gustavderdrache>
 //                 Boris Yankov <https://github.com/borisyankov>
@@ -9,7 +9,7 @@
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
 // TypeScript Version: 2.3
 
-// Last module patch version validated against: 2.6.0
+// Last module patch version validated against: 2.7.0
 
 // --------------------------------------------------------------------------
 // Shared Types and Interfaces
