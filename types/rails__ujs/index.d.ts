a0f4fe403c5d90b1b7fcd1e6df6da43668d16fd7
fsubal <fsubal@users.noreply.github.com>
2020-11-07 17:10:32 +0900

    add abcang as a owner
    Co-authored-by: abcang <abcang1015@gmail.com>

--- types/rails__ujs/index.d.ts
+++ types/rails__ujs/index.d.ts
@@ -1,6 +1,6 @@
 // Type definitions for @rails/ujs 6.0
 // Project: http://rubyonrails.org/
-// Definitions by: fsubal <https://github.com/fsubal>
+// Definitions by: fsubal <https://github.com/fsubal>, abcang <https://github.com/abcang>
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
 
 import { SelectorObject } from './utils/dom';
