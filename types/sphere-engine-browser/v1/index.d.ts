e8cbd74c7fc6f4ac4ffe238b78f588fdd3e10e76
Eggbertx <eggbertx@users.noreply.github.com>
2020-11-09 11:33:46 -0800

    Move 1.x API into subdirectory

--- types/sphere-engine-browser/v1/index.d.ts
+++ types/sphere-engine-browser/v1/index.d.ts
@@ -1,40 +1,13 @@
-/**
- *  Sphere 1.x API declaration
- *  Copyright (c) 2019, Eggbertx
- *  All rights reserved.
- *
- *  Redistribution and use in source and binary forms, with or without
- *  modification, are permitted provided that the following conditions are met:
- *
- *  * Redistributions of source code must retain the above copyright notice,
- *    this list of conditions and the following disclaimer.
- *
- *  * Redistributions in binary form must reproduce the above copyright notice,
- *    this list of conditions and the following disclaimer in the documentation
- *    and/or other materials provided with the distribution.
- *
- *  * Neither the name of miniSphere nor the names of its contributors may be
- *    used to endorse or promote products derived from this software without
- *    specific prior written permission.
- *
- *  THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
- *  AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
- *  IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
- *  ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE
- *  LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
- *  CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
- *  SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
- *  INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
- *  CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
- *  ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
- *  POSSIBILITY OF SUCH DAMAGE.
- */
-
-// Most of the JSDoc comments have been copied/adapted from the old Sphere documentation
-// https://github.com/sphere-group/sphere/blob/master/sphere/docs/development/api.txt
+// Type definitions for the non-npm package sphere-engine-browser/v1 1.5
+// Project: https://github.com/fatcerberus/miniSphere
+// Definitions by: Eggbertx <https://github.com/Eggbertx>
+// Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
+// Minimum TypeScript Version: 3.4
 
 /*
  * Deprecated Sphere 1.x objects necessary for some functions
+ * Most of the JSDoc comments have been copied/adapted from the old Sphere documentation
+ * https://github.com/sphere-group/sphere/blob/master/sphere/docs/development/api.txt
  */
 
 declare const COMMAND_WAIT = 0;
