c5f430aa1f28067ddd316bc31f75858c81fa82c0
strozw <606372+strozw@users.noreply.github.com>
2020-11-10 13:55:42 +0900

    change BoxProps to using SystemProps

--- types/xstyled__styled-components/index.d.ts
+++ types/xstyled__styled-components/index.d.ts
@@ -11,6 +11,7 @@ import _styled, {
     DefaultTheme,
     FlattenSimpleInterpolation,
 } from 'styled-components';
+import { SystemProps } from '@xstyled/system'
 export * from 'styled-components';
 
 export interface Breakpoints {
@@ -51,110 +52,7 @@ export function useUp(_bereakpointName: BreakpointName): boolean;
 
 export function useDown(_bereakpointName: BreakpointName): boolean;
 
-interface BoxPropsBase {
-    /* See props documentation at:
-     * https://www.smooth-code.com/open-source/smooth-ui/docs/box/#box-2
-     */
-    background: number | string;
-    backgroundColor: number | string;
-    backgroundImage: number | string;
-    backgroundSize: number | string;
-    backgroundPosition: number | string;
-    backgroundRepeat: number | string;
-    opacity: number | string;
-    overflow: number | string;
-    transition: number | string;
-    border: number | string;
-    borderTop: number | string;
-    borderTopColor: number | string;
-    borderRight: number | string;
-    borderRightColor: number | string;
-    borderBottom: number | string;
-    borderBottomColor: number | string;
-    borderLeft: number | string;
-    borderLeftColor: number | string;
-    borderColor: number | string;
-    borderWidth: number | string;
-    borderStyle: number | string;
-    borderRadius: number | string;
-    display: number | string;
-    alignItems: number | string;
-    alignContent: number | string;
-    justifyContent: number | string;
-    justifyItems: number | string;
-    flexWrap: number | string;
-    flexBasis: number | string;
-    flexDirection: number | string;
-    flex: number | string;
-    justifySelf: number | string;
-    alignSelf: number | string;
-    order: number | string;
-    gridGap: number | string;
-    gridColumnGap: number | string;
-    gridRowGap: number | string;
-    gridColumn: number | string;
-    gridRow: number | string;
-    gridAutoFlow: number | string;
-    gridAutoColumns: number | string;
-    gridAutoRows: number | string;
-    gridTemplateColumns: number | string;
-    gridTemplateRows: number | string;
-    gridTemplateAreas: number | string;
-    gridArea: number | string;
-    width: number | string;
-    height: number | string;
-    maxWidth: number | string;
-    maxHeight: number | string;
-    minWidth: number | string;
-    minHeight: number | string;
-    size: number | string;
-    verticalAlign: number | string;
-    position: number | string;
-    zIndex: number | string;
-    top: number | string;
-    right: number | string;
-    bottom: number | string;
-    left: number | string;
-    boxShadow: number | string;
-    textShadow: number | string;
-    margin: number | string;
-    m: number | string;
-    marginTop: number | string;
-    mt: number | string;
-    marginRight: number | string;
-    mr: number | string;
-    marginBottom: number | string;
-    mb: number | string;
-    marginLeft: number | string;
-    ml: number | string;
-    mx: number | string;
-    my: number | string;
-    padding: number | string;
-    p: number | string;
-    paddingTop: number | string;
-    pt: number | string;
-    paddingRight: number | string;
-    pr: number | string;
-    paddingBottom: number | string;
-    pb: number | string;
-    paddingLeft: number | string;
-    pl: number | string;
-    px: number | string;
-    py: number | string;
-    fontFamily: number | string;
-    fontSize: number | string;
-    lineHeight: number | string;
-    fontWeight: number | string;
-    textAlign: number | string;
-    letterSpacing: number | string;
-    color: number | string;
-    textTransform: number | string;
-    row: number | string;
-    col: number | string;
-}
-
-/* adds support for { xs: arg } and makes all props optional */
-export type BoxProps = WithBreakpointArgs<BoxPropsBase>;
+export type BoxProps = WithBreakpointArgs<SystemProps>;
 
 // TODO: If styled-components default tags are overridden,
 // these will work
