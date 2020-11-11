078441617434935f3949915b183b9b47495e676a
Cedric Lisima <7251423+opii972@users.noreply.github.com>
2020-11-11 17:31:55 +0100

    feat: add types for react-phone-number-input 3.1.5

--- types/react-phone-number-input/react-phone-number-input-tests.tsx
+++ types/react-phone-number-input/react-phone-number-input-tests.tsx
@@ -1,5 +1,11 @@
 import * as React from 'react';
-import PhoneInput, { parsePhoneNumber, PhoneNumber } from 'react-phone-number-input';
+import PhoneInput, {
+    parsePhoneNumber,
+    PhoneNumber,
+    getCountries,
+    getCountryCallingCode,
+    CountrySelectComponentProps,
+} from 'react-phone-number-input';
 
 const phoneNumber: PhoneNumber | undefined = parsePhoneNumber('+12025550112');
 
@@ -35,6 +41,44 @@ const test2 = (
         onChange={(value: string) => {
             console.log(value);
         }}
+        addInternationalOption
+        countryOptionsOrder={['US', 'CA', 'AU', '|', '...']}
+        disabled
+        inputComponent={InputComponent}
+        numberInputProps={{ type: 'tel' }}
+        smartCaret={false}
+    />
+);
+
+const CountrySelect = ({
+   value,
+   onChange,
+   ...rest
+}: CountrySelectComponentProps) => (
+    <select
+        {...rest}
+        value={value}
+        onChange={(event) => {
+            if (onChange) {
+                onChange(event.target.value || undefined);
+            }
+        }}
+    >
+        {getCountries().map((country: string) => (
+            <option key={country} value={country}>
+                {country} +{getCountryCallingCode(country)}
+            </option>
+        ))}
+    </select>
+);
+
+const test3 = (
+    <PhoneInput
+        value={'+64271231234'}
+        onChange={(value: string) => {
+            console.log(value);
+        }}
+        countrySelectComponent={CountrySelect}
         addInternationalOption
         countryOptionsOrder={['US', 'CA', 'AU', '|', '...']}
         disabled
