e6f5d88dbff828dda98bc53cf6cb33528f49cf61
Keana Delmotte <keana.delmotte@gmail.com>
2020-11-12 14:52:00 +0200

    Added tests and rewrote to make tests pass

--- types/react-native-wol/index.d.ts
+++ types/react-native-wol/index.d.ts
@@ -3,12 +3,16 @@
 // Definitions by: Keana Delmotte <https://github.com/KeanaDelmotte>
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
 
-declare module 'react-native-wol' {
-    /** Send a Wake on LAN packet for the provided MAC address.*/
-    export function send(
-        ipAddress: string,
-        macAddress: string,
-        /** The callback will be passed true if the packet was sent without error. */
-        callback?: (result: boolean, message: string) => void,
-    ): void;
-}
+/**
+ * Send a Wake on LAN packet for the provided MAC address.
+ */
+declare function send(
+    ipAddress: string,
+    macAddress: string,
+    /** The callback will be passed true if the packet was sent without error. */
+    callback?: (result: boolean, message: string) => void,
+): void;
+
+declare const wol: { send: typeof send };
+
+export default wol;
