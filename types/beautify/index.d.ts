127d88705fbb3ec68ee0df5bd121087af3b8d949
nzhang4 <nzhang4@stubhub.com>
2020-11-12 12:07:50 +0800

    update
    seems should be like this

--- types/beautify/index.d.ts
+++ types/beautify/index.d.ts
@@ -3,9 +3,10 @@
 // Definitions by: Zhang Nan <https://github.com/anyone-developer>
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
 
-export default function beautify(content: string, options: Options): string;
+export = beautify;
 
-// format could be 'css', 'json', 'js', 'html', 'xml'
-export interface Options {
+declare function beautify(content: string, options: Options): string;
+
+interface Options {
     format: 'css' | 'json' | 'js' | 'html' | 'xml';
 }
