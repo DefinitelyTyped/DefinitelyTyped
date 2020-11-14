3176c2784b1e86567769c3acb538bee3398e623e
vijexa <nuzhdinvitja@gmail.com>
2020-11-14 21:45:59 +0200

    add type definition for hsl-to-hex

---
+++ types/hsl-to-hex/index.d.ts
@@ -0,0 +1,14 @@
+// Type definitions for hsl-to-hex 1.0
+// Project: https://github.com/davidmarkclements/hsl-to-hex#readme
+// Definitions by: vijexa <https://github.com/vijexa>
+// Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
+
+declare module 'hsl-to-hex' {
+    /**
+     * Convert hue, saturation and luminosity to hex string
+     * 
+     * e.g. hsl(212, 32, 65) to '#89a4c2'
+     */
+    function hslToHex (hue: number, saturation: number, luminosity: number): string
+    export = hslToHex
+}
