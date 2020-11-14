b77a9d9fa52734824bc63de775352e40798d327e
Griko Nibras <git@griko.id>
2020-11-14 10:18:33 +0700

    Create entry.nostyle.d.ts

---
+++ types/react-time-picker/dist/entry.nostyle.d.ts
@@ -0,0 +1,8 @@
+// Type definitions for react-time-picker 4.0
+// Project: https://github.com/wojtekmaj/react-time-picker
+// Definitions by: Enmanuel Veras <https://github.com/everas7>
+// Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
+// TypeScript Version: 3.5
+
+export { default } from "..";
+export * from "..";
