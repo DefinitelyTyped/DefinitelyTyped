11cf2e09f67ccba740c06a81be7905ad8e80591d
vijexa <nuzhdinvitja@gmail.com>
2020-11-14 22:12:27 +0200

    fixed linting issues

--- types/hsl-to-hex/index.d.ts
+++ types/hsl-to-hex/index.d.ts
@@ -3,12 +3,10 @@
 // Definitions by: vijexa <https://github.com/vijexa>
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
 
-declare module 'hsl-to-hex' {
-    /**
-     * Convert hue, saturation and luminosity to hex string
-     * 
-     * e.g. hsl(212, 32, 65) to '#89a4c2'
-     */
-    function hslToHex (hue: number, saturation: number, luminosity: number): string
-    export = hslToHex
-}
+/**
+ * Convert hue, saturation and luminosity to hex string
+ *
+ * e.g. hsl(212, 32, 65) to '#89a4c2'
+ */
+declare function hslToHex(hue: number, saturation: number, luminosity: number): string;
+export = hslToHex;
