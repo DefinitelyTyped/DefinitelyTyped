1a5f2891640041c858f05281a941d5284532e586
Lloyd Evans <contact@lloydevans.co.uk>
2020-11-15 12:07:14 +0000

    Add test

--- types/tweenjs/tweenjs-tests.ts
+++ types/tweenjs/tweenjs-tests.ts
@@ -9,3 +9,6 @@ createjs.Tween.get(target).wait(500, false).to({ alpha: 0, visible: false }, 100
 function onComplete() {
     //Tween complete
 }
+
+// $ExpectError
+createjs.Tween.get(target).wait(500, false).to({ unknown: 0 }, 1000);
