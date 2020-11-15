316519472409a955f1bd5fd02f41835a65dbda60
Noah Overcash <me@ncovercash.dev>
2020-11-15 04:54:47 -0600

    Fix typo in my username

--- types/fg-loadcss/index.d.ts
+++ types/fg-loadcss/index.d.ts
@@ -1,6 +1,6 @@
 // Type definitions for fg-loadcss 3.1
 // Project: https://github.com/filamentgroup/loadCSS
-// Definitions by: Noah Overcash <https://github.com/me>
+// Definitions by: Noah Overcash <https://github.com/ncovercash>
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
 
 export function loadCSS(href: string, before?: HTMLElement, media?: string, attributes?: Record<string, string>): HTMLLinkElement;
