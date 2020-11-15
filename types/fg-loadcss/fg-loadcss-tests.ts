7b9bb976b0538fb64a110a6fc4aab738b153a18e
Noah Overcash <me@ncovercash.dev>
2020-11-15 04:52:08 -0600

    Add fg-loadcss definitions

---
+++ types/fg-loadcss/fg-loadcss-tests.ts
@@ -0,0 +1,10 @@
+import { loadCSS, onloadCSS } from "fg-loadcss";
+
+loadCSS("example.css");
+loadCSS("example.css", document.body);
+loadCSS("example.css", document.body, "print");
+const foo = loadCSS("example.css", document.body, "print", { integrity: "sha123-abc" });
+
+onloadCSS(foo, () => {
+	console.log("Loaded!");
+});
---
+++ types/fg-loadcss/index.d.ts
@@ -0,0 +1,8 @@
+// Type definitions for fg-loadcss 3.1
+// Project: https://github.com/filamentgroup/loadCSS
+// Definitions by: Noah Overcash <https://github.com/me>
+// Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
+
+export function loadCSS(href: string, before?: HTMLElement, media?: string, attributes?: Record<string, string>): HTMLLinkElement;
+
+export function onloadCSS(stylesheet: HTMLLinkElement, callback: () => void): void;
