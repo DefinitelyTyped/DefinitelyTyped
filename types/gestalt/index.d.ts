45b84805dbc8cf9335d9dc495a0b79d6ed616dd4
Juan J. Alvarez <jalvarez5241997@gmail.com>
2020-11-16 17:24:54 -0500

    Removed patch version

--- types/gestalt/index.d.ts
+++ types/gestalt/index.d.ts
@@ -1,4 +1,4 @@
-// Type definitions for gestalt 14.28.1
+// Type definitions for gestalt 14.28
 // Project: https://github.com/pinterest/gestalt, https://pinterest.github.io/gestalt
 // Definitions by: Nicolás Serrano Arévalo <https://github.com/serranoarevalo>
 //                 Josh Gachnang <https://github.com/joshgachnang>
