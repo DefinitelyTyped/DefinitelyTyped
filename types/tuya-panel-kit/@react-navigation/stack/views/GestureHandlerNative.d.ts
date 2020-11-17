009ce433bcd8ac4f51cc724ac83522e8fdbdb7e8
youngjuning <youngjuning@aliyun.com>
2020-11-17 16:51:34 +0800

    remove unused file

--- types/tuya-panel-kit/@react-navigation/stack/views/GestureHandlerNative.d.ts
+++
@@ -1,4 +0,0 @@
-/// <reference types="react" />
-import { PanGestureHandlerProperties } from 'react-native-gesture-handler';
-export declare function PanGestureHandler(props: PanGestureHandlerProperties): JSX.Element;
-export { GestureHandlerRootView, State as GestureState, PanGestureHandlerGestureEvent, } from 'react-native-gesture-handler';
