ce1c5bdf6ef4e2aca7fe7e0a8fec3bb16c80c76e
Jack Bates <jack@nottheoilrig.com>
2020-11-17 16:15:34 -0700

    [node] Clarify assert.strict type (#49469)

--- types/node/assert.d.ts
+++ types/node/assert.d.ts
@@ -93,26 +93,30 @@ declare module 'assert' {
 
         const strict: Omit<
             typeof assert,
-            | 'strict'
-            | 'deepEqual'
-            | 'notDeepEqual'
             | 'equal'
             | 'notEqual'
+            | 'deepEqual'
+            | 'notDeepEqual'
             | 'ok'
             | 'strictEqual'
             | 'deepStrictEqual'
             | 'ifError'
+            | 'strict'
         > & {
             (value: any, message?: string | Error): asserts value;
-            strict: typeof strict;
-            deepEqual: typeof deepStrictEqual;
-            notDeepEqual: typeof notDeepStrictEqual;
             equal: typeof strictEqual;
             notEqual: typeof notStrictEqual;
-            ok(value: any, message?: string | Error): asserts value;
-            strictEqual<T>(actual: any, expected: T, message?: string | Error): asserts actual is T;
-            deepStrictEqual<T>(actual: any, expected: T, message?: string | Error): asserts actual is T;
-            ifError(value: any): asserts value is null | undefined;
+            deepEqual: typeof deepStrictEqual;
+            notDeepEqual: typeof notDeepStrictEqual;
+
+            // Mapped types and assertion functions are incompatible?
+            // TS2775: Assertions require every name in the call target
+            // to be declared with an explicit type annotation.
+            ok: typeof ok;
+            strictEqual: typeof strictEqual;
+            deepStrictEqual: typeof deepStrictEqual;
+            ifError: typeof ifError;
+            strict: typeof strict;
         };
     }
 
