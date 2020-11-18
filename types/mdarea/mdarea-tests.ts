586c6347bcf66f99e8f696fd8979ebb58a66d67c
Antoine Laffargue <antoine.laffargue@gmail.com>
2020-11-18 11:59:30 +0100

    lint

--- types/mdarea/mdarea-tests.ts
+++ types/mdarea/mdarea-tests.ts
@@ -1,21 +1,21 @@
-import MarkdownArea from 'mdarea'
+import MarkdownArea from 'mdarea';
 
-const elem = new HTMLTextAreaElement()
+const elem = new HTMLTextAreaElement();
 
-new MarkdownArea(elem)
+new MarkdownArea(elem);
 const editor = new MarkdownArea(elem, {
-  indent: '',
-  keyMap: {
-    enter: '',
-    indent: [],
-    inline: ['*', '`'],
-    outdent: ''
-  }
-})
+    indent: '',
+    keyMap: {
+        enter: '',
+        indent: [],
+        inline: ['*', '`'],
+        outdent: '',
+    },
+});
 
-const elem2 = editor.getElement()
-const value = editor.getValue()
-const same = elem2.value === value
-editor.setElement(new HTMLTextAreaElement())
-editor.setValue('')
-editor.destroy()
+const elem2 = editor.getElement();
+const value = editor.getValue();
+const same = elem2.value === value;
+editor.setElement(new HTMLTextAreaElement());
+editor.setValue('');
+editor.destroy();
