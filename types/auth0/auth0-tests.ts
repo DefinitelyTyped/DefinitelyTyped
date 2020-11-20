0aa9a4ab6cdc80fd491308ab2ee97185a0b1bcfd
cviquez <cviquez@samasource.org>
2020-11-20 16:14:49 -0600

    Change auth0 spec file. getEnrollments() return a list of enrollment so it should be plural

--- types/auth0/auth0-tests.ts
+++ types/auth0/auth0-tests.ts
@@ -635,8 +635,8 @@ management.deleteCustomDomain({ id: 'cd_0000000000000001' }).then(() => console.
 management.deleteCustomDomain({ id: 'cd_0000000000000001' }, err => console.log('deleted'));
 
 // User enrollment
-management.getGuardianEnrollments({ id: 'cd_0000000000000001' }).then((enrollment) => console.log(enrollment))
-management.getGuardianEnrollments({ id: 'cd_0000000000000001' },(err, enrollment) => console.log(enrollment))
+management.getGuardianEnrollments({ id: 'cd_0000000000000001' }).then((enrollments) => console.log(enrollments))
+management.getGuardianEnrollments({ id: 'cd_0000000000000001' },(err, enrollments) => console.log(enrollments))
 
 management.deleteGuardianEnrollment({ id: 'cd_0000000000000001' }).then(() => console.log('deleted'));
 management.deleteGuardianEnrollment({ id: 'cd_0000000000000001' }, err => console.log('deleted error'));
