3b531f55fdef0b4abb66096a777c9cb27a57d24c
Kaspar Vollenweider <vok@panter.ch>
2020-11-22 01:43:36 +0100

    fix(ttf2woff): add missing options metadata types and description

--- types/ttf2woff/index.d.ts
+++ types/ttf2woff/index.d.ts
@@ -4,6 +4,16 @@
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
 /// <reference types="node" />
 
-declare function ttf2woff(ttf: Uint8Array): Buffer;
+declare function ttf2woff(
+    ttf: Uint8Array,
+    options?: {
+        /**
+         * Woff Extended Metadata Block
+         *
+         * See https://www.w3.org/TR/WOFF/#Metadata
+         */
+        metadata?: string;
+    },
+): Buffer;
 
 export = ttf2woff;
--- types/ttf2woff/ttf2woff-tests.ts
+++ types/ttf2woff/ttf2woff-tests.ts
@@ -1,4 +1,6 @@
 import ttf2woff = require('ttf2woff');
 
 const input = new Uint8Array(10);
-const result: Buffer = ttf2woff(input);
+let result: Buffer = ttf2woff(input);
+result = ttf2woff(input, {});
+result = ttf2woff(input, { metadata: 'string meta data' });
