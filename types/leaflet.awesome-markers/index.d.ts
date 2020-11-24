ca3ee0949fa08627b5a3ca364c033417f02b11c9
Willian Luis Zuqui <willianluiszuqui@gmail.com>
2020-11-24 09:55:32 -0300

    Update index.d.ts

--- types/leaflet.awesome-markers/index.d.ts
+++ types/leaflet.awesome-markers/index.d.ts
@@ -6,46 +6,66 @@
 
 import * as Leaflet from "leaflet";
 
-declare module 'leaflet' {
-    namespace AwesomeMarkers {
-        const version: string;
+declare module "leaflet" {
+  namespace AwesomeMarkers {
+    const version: string;
 
-        interface AwesomeMarkersIconOptions extends BaseIconOptions {
-            /**
-             * Name of the icon. See glyphicons or font-awesome.
-             */
-            icon?: string;
+    interface AwesomeMarkersIconOptions extends BaseIconOptions {
+      /**
+       * Name of the icon. See glyphicons or font-awesome.
+       */
+      icon?: string;
 
-            /**
-             * Select de icon library. 'fa' for font-awesome or 'glyphicon' for bootstrap 3.
-             */
-            prefix?: 'fa' | 'glyphicon';
+      /**
+       * Select de icon library. 'fa' for font-awesome or 'glyphicon' for bootstrap 3.
+       */
+      prefix?: "fa" | "glyphicon";
 
-            /**
-             * Color of the marker
-             */
-            markerColor?: 'white' | 'red' |'darkred' | 'lightred' | 'orange' | 'beige' | 'green' | 'darkgreen' | 'lightgreen' | 'blue' | 'darkblue' | 'lightblue' | 'purple' | 'darkpurple' | 'pink' | 'cadetblue' | 'white' | 'gray' | 'lightgray' | 'black';
+      /**
+       * Color of the marker
+       */
+      markerColor?:
+        | "white"
+        | "red"
+        | "darkred"
+        | "lightred"
+        | "orange"
+        | "beige"
+        | "green"
+        | "darkgreen"
+        | "lightgreen"
+        | "blue"
+        | "darkblue"
+        | "lightblue"
+        | "purple"
+        | "darkpurple"
+        | "pink"
+        | "cadetblue"
+        | "white"
+        | "gray"
+        | "lightgray"
+        | "black";
 
-            /**
-             * Color of the icon. 'white', 'black' or css code (hex, rgba etc).
-             */
-            iconColor?: 'white' | 'black' | string;
+      /**
+       * Color of the icon. 'white', 'black' or css code (hex, rgba etc).
+       */
+      iconColor?: "white" | "black" | string;
 
-            /**
-             * Make the icon spin. true or false. Font-awesome required
-             */
-            spin?: boolean;
+      /**
+       * Make the icon spin. true or false. Font-awesome required
+       */
+      spin?: boolean;
 
-            /**
-             * Additional classes in the created tag
-             */
-            extraClasses?: string;
-        }
-
-        function icon(options: AwesomeMarkersIconOptions): Icon;
-
-        class Icon extends Leaflet.Icon<AwesomeMarkersIconOptions> {
-            constructor(options?: AwesomeMarkersIconOptions);
-        }
+      /**
+       * Additional classes in the created tag
+       */
+      extraClasses?: string;
     }
+
+    function icon(options: AwesomeMarkersIconOptions): Icon;
+
+    class Icon extends Leaflet.Icon<AwesomeMarkersIconOptions> {
+      constructor(options?: AwesomeMarkersIconOptions);
+    }
+  }
 }
