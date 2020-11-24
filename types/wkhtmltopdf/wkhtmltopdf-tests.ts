575a6c82043c1720baf9d86aba931f7c55fb6527
Jasper Jansen <jansen@voltolabs.com>
2020-11-24 11:26:24 +0100

    Fix return types and adds debug function call

--- types/wkhtmltopdf/wkhtmltopdf-tests.ts
+++ types/wkhtmltopdf/wkhtmltopdf-tests.ts
@@ -7,10 +7,10 @@ wkhtmltopdf("http://google.com/", { pageSize: "Letter" }); // $ExpectType NodeJS
 wkhtmltopdf("<h1>Test</h1><p>Hello world</p>"); // $ExpectType ReadWriteStream
 
 // output to a file directly
-wkhtmltopdf("http://apple.com/", { output: "out.pdf" }); // $ExpectType void
+wkhtmltopdf("http://apple.com/", { output: "out.pdf" }); // $ExpectType ReadWriteStream
 
 // Optional callback
-wkhtmltopdf("http://google.com/", { pageSize: "Letter" }, (err, stream) => { }); // $ExpectType void
+wkhtmltopdf("http://google.com/", { pageSize: "Letter" }, (err: Error, stream: NodeJS.ReadWriteStream) => { }); // $ExpectType void
 
 // Repeatable options
 wkhtmltopdf("http://google.com/", { // $ExpectType NodeJS.ReadWriteStream
@@ -22,15 +22,34 @@ wkhtmltopdf("http://google.com/", { // $ExpectType NodeJS.ReadWriteStream
 });
 
 // Ignore warning strings
-wkhtmltopdf("http://apple.com/", { // $ExpectType void
+wkhtmltopdf("http://apple.com/", { // $ExpectType ReadWriteStream
   output: "out.pdf",
   ignore: ["QFont::setPixelSize: Pixel size <= 0 (0)"]
 });
 
 // RegExp also acceptable
-wkhtmltopdf("http://apple.com/", { // $ExpectType void
+wkhtmltopdf("http://apple.com/", { // $ExpectType ReadWriteStream
   output: "out.pdf",
   ignore: [/QFont::setPixelSize/]
+});
+
+// Test debug types
+wkhtmltopdf("http://apple.com/", { // $ExpectType ReadWriteStream
+  debug: true
+});
+
+wkhtmltopdf("http://apple.com/", { // $ExpectType ReadWriteStream
+  output: "test.pdf",
+  debug: (data: Buffer) => {
+    const dataString = data.toString();
+    const stageMatch = dataString.match(/\((\d*)\/(\d*)\)/);
+    const percentMatch = dataString.match(/(\d*)%/);
+    if (stageMatch) {
+        console.log(`Pdf at ${stageMatch[1]} out of ${stageMatch[2]} stages`);
+    } else if (percentMatch) {
+        console.log(`Pdf at ${percentMatch[1]}%`);
+    }
+  }
 });
 
 wkhtmltopdf.shell; // $ExpectType string
