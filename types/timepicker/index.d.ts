7750250a4f132e880661907f3c7cb7eb49d9f62b
Dieter Oberkofler <1009585+doberkofler@users.noreply.github.com>
2020-11-25 00:18:48 +0100

    Changed TypeScript Version to 2.7

--- types/timepicker/index.d.ts
+++ types/timepicker/index.d.ts
@@ -2,7 +2,7 @@
 // Project: <https://github.com/jonthornton/jquery-timepicker>
 // Definitions by: doberkofler <https://github.com/doberkofler>
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
-// TypeScript Version: 2.3
+// TypeScript Version: 2.7
 
 /// <reference types="jquery"/>
 
