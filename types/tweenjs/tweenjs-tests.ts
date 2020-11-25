aa5c590c18b78ec62ac3a989c4a2d2276cc67c78
Lloyd Evans <contact@lloydevans.co.uk>
2020-11-25 15:11:01 +0000

    Add call test

--- types/tweenjs/tweenjs-tests.ts
+++ types/tweenjs/tweenjs-tests.ts
@@ -12,3 +12,5 @@ function onComplete() {
 
 // $ExpectError
 createjs.Tween.get(target).wait(500, false).to({ unknown: 0 }, 1000);
+
+createjs.Tween.get(target).wait(1).call((value: number) => console.log(value), [1]);
