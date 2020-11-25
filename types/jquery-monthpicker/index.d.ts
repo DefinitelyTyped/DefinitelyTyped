bc8d438e2c5850cba7f8a5f06437572cc9ed8293
Dieter Oberkofler <1009585+doberkofler@users.noreply.github.com>
2020-11-25 20:46:52 +0100

    Added jsdoc comments to the methods

--- types/jquery-monthpicker/index.d.ts
+++ types/jquery-monthpicker/index.d.ts
@@ -50,16 +50,39 @@ interface JQueryMonthPickerOptions {
 interface JQuery {
     MonthPicker(options: JQueryMonthPickerOptions): JQuery;
 
+    /** Clears the state of all input and validation warnings. */
     MonthPicker(methodName: 'Clear'): void;
+
+    /** Disables all previously assigned event callbacks listed in the Events tab. */
     MonthPicker(methodName: 'ClearAllCallbacks'): void; // tslint:disable-line:unified-signatures
+
+    /** Closes the month picker if it's already open. */
     MonthPicker(methodName: 'Close'): void; // tslint:disable-line:unified-signatures
+
+    /** Disables the MonthPicker and its associated elements. */
     MonthPicker(methodName: 'Disable'): void; // tslint:disable-line:unified-signatures
+
+    /** Enables the MonthPicker and its associated elements. */
     MonthPicker(methodName: 'Enable'): void; // tslint:disable-line:unified-signatures
+
+    /** Returns the selected month as a Date object. */
     MonthPicker(methodName: 'GetSelectedDate'): Date;
-    MonthPicker(methodName: 'GetSelectedMonth'): number;
+
+    /** Validates the selected month/year and returns the selected value as a string (for example '1/2015') if it is a valid date. */
     MonthPicker(methodName: 'GetSelectedMonthYear'): string|null;
+
+    /** Returns only the month portion of the selected date as an integer. Returns NaN if there is no valid date. */
+    MonthPicker(methodName: 'GetSelectedMonth'): number;
+
+    /** Returns only the year portion of the selected date as an integer. Returns NaN if there is no valid date. */
     MonthPicker(methodName: 'GetSelectedYear'): number; // tslint:disable-line:unified-signatures
+
+    /** Opens the month picker menu and keeps it open if it's already open, see the OnBeforeMenuClose event to prevent the menu from closing on click (or other) events. */
     MonthPicker(methodName: 'Open'): void; // tslint:disable-line:unified-signatures
+
+    /** Opens the month picker menu or closes the menu if it's already open, see the OnBeforeMenuClose event to prevent the menu from closing on click (or other) events.  */
     MonthPicker(methodName: 'Toggle'): void; // tslint:disable-line:unified-signatures
+
+    /** Validates the selected month/year and returns the selected value as a Date object if it is a valid date. */
     MonthPicker(methodName: 'Validate'): void; // tslint:disable-line:unified-signatures
 }
