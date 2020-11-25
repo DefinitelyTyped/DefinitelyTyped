8babc53ea095d9b40a75470e1e09614719cae48e
Ben Limmer <ben@benlimmer.com>
2020-11-25 13:20:05 -0700

    Create package truncate-utf8-bytes

---
+++ types/truncate-utf8-bytes/index.d.ts
@@ -0,0 +1,11 @@
+// Type definitions for truncate-utf8-bytes 1.0
+// Project: https://github.com/parshap/truncate-utf8-bytes#readme
+// Definitions by: Ben Limmer <https://github.com/blimmer>
+// Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
+
+export as namespace truncateUtf8Bytes;
+
+/** Returns string truncated to at most length bytes in length. */
+declare function truncate(string: string, byteLength: number): string;
+
+export = truncate;
