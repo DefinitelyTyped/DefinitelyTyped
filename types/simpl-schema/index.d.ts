8e7541b2c1c8a37bda648f2b89ea3b3d803f8739
OlivierChirouze <olivier.chirouze+github@gmail.com>
2020-11-29 15:22:07 +0100

    Update index.d.ts
    Methods field() and siblingField() both take a mandatory field name.

--- types/simpl-schema/index.d.ts
+++ types/simpl-schema/index.d.ts
@@ -41,13 +41,13 @@ interface CustomValidationContext {
      * (non-generic schema key) as the only argument. The return object will
      * have isSet, value, and operator properties for that field.
      */
-    field(): any;
+    field(name: string): any;
     /**
      * Use this method to get information about other fields that have the same
      * parent object. Works the same way as field(). This is helpful when you use
      * sub-schemas or when you're dealing with arrays of objects.
      */
-    siblingField(): any;
+    siblingField(name: string): any;
 
     /**
      * Call this to add validation errors for any key. In general, you should use
