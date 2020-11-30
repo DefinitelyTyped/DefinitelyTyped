10048f4bd896e1d68612dd60511749e1bcbb4d87
Alexandre Asselin <alexandre.asselin@gsoft.com>
2020-11-30 15:24:52 -0500

    Commit to re-trigger the CI

--- types/orbit-ui__react-components/orbit-ui__react-components-tests.tsx
+++ types/orbit-ui__react-components/orbit-ui__react-components-tests.tsx
@@ -1,7 +1,7 @@
 import * as React from "react";
 import { Button } from "@orbit-ui/react-components";
 
-const MyComponent = () => {
+const MyButton = () => {
     return (
         <>
             <Button circular={true} />
