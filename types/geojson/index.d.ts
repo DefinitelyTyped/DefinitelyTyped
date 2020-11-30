bddced95d38a7580754eb047f273675e2538e0c2
Mathieu Arnold <mat@mat.cc>
2020-11-30 23:48:26 +0100

    Add CoordinateReferenceSystem to the generic GeoJsonObject.

--- types/geojson/index.d.ts
+++ types/geojson/index.d.ts
@@ -41,6 +41,76 @@ export type BBox = [number, number, number, number] | [number, number, number, n
 export type Position = number[]; // [number, number] | [number, number, number];
 
 /**
+ * A CRS object may indicate a coordinate reference system by name. In this
+ * case, the value of its "type" member must be the string "name". The value of
+ * its "properties" member must be an object containing a "name" member. The
+ * value of that "name" member must be a string identifying a coordinate
+ * reference system. OGC CRS URNs such as "urn:ogc:def:crs:OGC:1.3:CRS84" shall
+ * be preferred over legacy identifiers such as "EPSG:4326".
+ */
+
+export interface NamedCoordinateReferenceSystemProperties {
+    name: string;
+}
+
+export interface NamedCoordinateReferenceSystem {
+    type: 'name';
+    properties: NamedCoordinateReferenceSystemProperties;
+}
+
+/**
+ * A link object has one required member: "href", and one optional member:
+ * "type".
+ *
+ * The value of the required "href" member must be a dereferenceable URI.
+ *
+ * The value of the optional "type" member must be a string that hints at the
+ * format used to represent CRS parameters at the provided URI. Suggested
+ * values are: "proj4", "ogcwkt", "esriwkt", but others can be used.
+ *
+ * Relative links may be used to direct processors to CRS parameters in an
+ * auxiliary file.
+ */
+
+export interface LinkedCoordinateReferenceSystemProperties {
+    href: string;
+    type?: string;
+}
+
+export interface LinkedCoordinateReferenceSystem {
+    type: 'link';
+    properties: LinkedCoordinateReferenceSystemProperties;
+}
+
+/**
+ * The coordinate reference system (CRS) of a GeoJSON object is determined by
+ * its "crs" member (referred to as the CRS object below). If an object has no
+ * crs member, then its parent or grandparent object’s crs member may be
+ * acquired.  If no crs member can be so acquired, the default CRS shall apply
+ * to the GeoJSON object.
+ *
+ * - The default CRS is a geographic coordinate reference system, using the
+ *   WGS84 datum, and with longitude and latitude units of decimal degrees.
+ *
+ * - The value of a member named "crs" must be a JSON object (referred to as
+ *   the CRS object below) or JSON null. If the value of CRS is null, no CRS
+ *   can be assumed.
+ *
+ * - The crs member should be on the top-level GeoJSON object in a hierarchy
+ *   (in feature collection, feature, geometry order) and should not be
+ *   repeated or overridden on children or grandchildren of the object.
+ *
+ * - A non-null CRS object has two mandatory members: "type" and "properties".
+ *
+ * - The value of the type member must be a string, indicating the type of CRS
+ *   object.
+ *
+ * - The value of the properties member must be an object.
+ */
+
+export type CoordinateReferenceSystem = NamedCoordinateReferenceSystem | LinkedCoordinateReferenceSystem;
+
+/**
  * The base GeoJSON object.
  * https://tools.ietf.org/html/rfc7946#section-3
  * The GeoJSON specification also allows foreign members
@@ -65,6 +135,7 @@ export interface GeoJsonObject {
      * https://tools.ietf.org/html/rfc7946#section-5
      */
     bbox?: BBox;
+    crs?: CoordinateReferenceSystem;
 }
 
 /**
