89c620a6f091e99342efa3255aede91c62c6fdb5
Darron Park <darron1217@gmail.com>
2020-12-02 11:03:08 +0900

    Update version comment of react-native-calendars

--- types/react-native-calendars/index.d.ts
+++ types/react-native-calendars/index.d.ts
@@ -1,4 +1,4 @@
-// Type definitions for react-native-calendars 1.20
+// Type definitions for react-native-calendars 1.505
 // Project: https://github.com/wix/react-native-calendars#readme
 // Definitions by: Tyler Zhang <https://github.com/Tyler-Zhang>
 //                 David Noreña <https://github.com/DavidNorena>
