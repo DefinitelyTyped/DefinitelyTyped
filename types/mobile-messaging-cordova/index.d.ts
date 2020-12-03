e1d4420e08da7df79b2120476d088d63ad93d454
Olga Koroleva <Olga.Koroleva@infobip.com>
2020-12-03 12:16:08 +0300

    version of plugin changed

--- types/mobile-messaging-cordova/index.d.ts
+++ types/mobile-messaging-cordova/index.d.ts
@@ -1,4 +1,4 @@
-// Type definitions for non-npm package mobile-messaging-cordova-plugin 1.3
+// Type definitions for non-npm package mobile-messaging-cordova-plugin 1.7
 // Project: https://github.com/infobip/mobile-messaging-cordova-plugin
 // Definitions by: kostap13 <https://github.com/kostap13>,
 //                 tjuric <https://github.com/tjuric>
