07069a861c6dba3e8c41738c1cd979dfc5182e9b
UNIDY2002 <UNIDY2002@outlook.com>
2020-12-05 14:46:29 +0800

    Bump dependency `highlight.js` of `types/markdown-it` from 9.7.0 to 10.4.1

--- types/markdown-it/index.d.ts
+++ types/markdown-it/index.d.ts
@@ -1,10 +1,10 @@
-// Type definitions for markdown-it v10.0.0
+// Type definitions for markdown-it v12.0.2
 // Project: https://github.com/markdown-it/markdown-it
 // Definitions by: York Yao <https://github.com/plantain-00>
 //                 Robert Coie <https://github.com/rapropos>
 //                 duduluu <https://github.com/duduluu>
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
-// TypeScript Version: 2.0
+// TypeScript Version: 3.6
 
 import MarkdownIt = require('./lib');
 
