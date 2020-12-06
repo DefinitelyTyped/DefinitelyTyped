f31cdf09f0f709a8d80bbdfdb53ed149f4a42c17
Yu-Jack <jk82421@gmail.com>
2020-12-06 09:48:24 +0800

    fix: change project url to official documentation

--- types/tpdirect/index.d.ts
+++ types/tpdirect/index.d.ts
@@ -1,5 +1,5 @@
 // Type definitions for non-npm package tpdirect-browser 1.0
-// Project: https://github.com/Yu-Jack/DefinitelyTyped
+// Project: https://docs.tappaysdk.com/tutorial/en/home.html
 // Definitions by: Yu-Jack <https://github.com/Yu-Jack>
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
 /// <reference path="tpdirect.d.ts" />
