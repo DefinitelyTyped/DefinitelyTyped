98466db5a29d20f6cc5ce59c8849587cdc100d80
Yaroslav Kiliba <om.dattaya@gmail.com>
2020-12-06 22:16:42 +0200

    [lqip-modern] new package

---
+++ types/lqip-modern/index.d.ts
@@ -0,0 +1,43 @@
+// Type definitions for lqip-modern 1.1
+// Project: https://github.com/transitive-bullshit/lqip-modern/
+// Definitions by: Yaroslav Kiliba <https://github.com/Dattaya>
+// Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
+// Minimum TypeScript Version: 3.2
+
+/// <reference types="node" />
+
+import sharp = require("sharp");
+
+declare namespace lqip {
+    export interface LqipResult {
+        content: Buffer;
+        metadata: {
+            originalWidth: number;
+            originalHeight: number;
+            width: number;
+            height: number;
+            type: string;
+            dataURIBase64: string;
+        };
+    }
+
+    type OutputFormat = 'webp' | 'jpeg' | 'jpg';
+
+    interface OutputOptions {
+        'webp': sharp.WebpOptions;
+        'jpeg': sharp.JpegOptions;
+        'jpg': sharp.JpegOptions;
+    }
+
+    export interface LqipOptions<T extends OutputFormat> {
+        concurrency?: number;
+        outputFormat?: T;
+        outputOptions?: OutputOptions[T];
+        resize?: number | any[];
+    }
+}
+
+declare function lqip<T extends lqip.OutputFormat = 'webp'>(input: string | Buffer, options?: lqip.LqipOptions<T>): Promise<lqip.LqipResult>;
+declare function lqip<T extends lqip.OutputFormat = 'webp'>(input: string[] | Buffer[], options?: lqip.LqipOptions<T>): Promise<lqip.LqipResult[]>;
+
+export = lqip;
---
+++ types/lqip-modern/lqip-modern-tests.ts
@@ -0,0 +1,10 @@
+import lqip = require('lqip-modern');
+
+lqip(Buffer.from(''));  // $ExpectType Promise<LqipResult>
+lqip(['tst']);  // $ExpectType Promise<LqipResult[]>
+
+lqip('tst', { outputFormat: 'jpg', outputOptions: { loop: 10 } }); // $ExpectError
+lqip('tst', { outputFormat: 'webp', outputOptions: { loop: 10 } });
+
+lqip('tst', { outputFormat: 'jpg', outputOptions: { quantisationTable: 1 } });
+lqip('tst', { outputFormat: 'webp', outputOptions: { quantisationTable: 1 } }); // $ExpectError
