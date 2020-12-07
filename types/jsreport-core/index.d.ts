abfd449e4e449223f647778281e6819fd050057c
Keith Kikta <keith.kikta@tevpro.com>
2020-12-07 01:38:53 -0600

    expanding jsreport-core type def

--- types/jsreport-core/index.d.ts
+++ types/jsreport-core/index.d.ts
@@ -7,6 +7,8 @@
 
 /// <reference types="node" />
 
+import * as fs from 'fs';
+
 declare namespace JsReport {
     type Helpers = string | { [fun: string]: (...args: any[]) => any };
 
@@ -26,9 +28,20 @@ declare namespace JsReport {
     }
 
     interface Request {
-        template: Partial<Template>;
+        value?: boolean;
+        writable?: boolean;
+        configurable?: boolean;
+        enumerable?: boolean;
+        template?: Partial<Template>;
         options?: Options;
-        data: any;
+        context?: Context;
+        data?: any;
+    }
+
+    interface Context {
+        shared?: any;
+        originalInputDataIsEmpty?: boolean;
+        isChildRequest?: boolean;
     }
 
     interface Response {
@@ -70,21 +83,44 @@ declare namespace JsReport {
     }
 
     interface Reporter {
-        afterRenderListeners: ListenerCollection;
-        afterTemplatingEnginesExecutedListeners: ListenerCollection;
-        beforeRenderListeners: ListenerCollection;
-        documentStore: DocumentStore;
-        initializeListeners: ListenerCollection;
+        defaults?: any;
+        options?: any;
+        afterRenderListeners?: ListenerCollection;
+        afterTemplatingEnginesExecutedListeners?: ListenerCollection;
+        beforeRenderListeners?: ListenerCollection;
+        closeListeners?: ListenerCollection;
+        documentStore?: DocumentStore;
+        initializeListeners?: ListenerCollection;
+        renderErrorListeners?: ListenerCollection;
         // it would be nice to add winston.LoggerInstance here
         // however adding import winston = require('winston') breaks exported enums
         logger: any;
         validateRenderListeners: ListenerCollection;
-        version: string;
+        version?: string;
+        settings?: Settings;
         init(): Promise<Reporter>;
         render(options: Partial<Request>): Promise<Response>;
         discover(): Reporter;
         createListenerCollection(): ListenerCollection;
         close(): Promise<void>;
+        optionsValidator?: SchemaValidator;
+        entityTypeValidator?: SchemaValidator;
+        extensionsLoad(opts: any): Promise<Reporter>;
+        checkValidEntityName(c: string, doc: any, req: Request): Promise<void>;
+        createError(message: string, options?: any): any;
+        getAvailableRenderTimeout(req: Request, defaultValue: any): any;
+        executeScript(inputs: any, options: any, req: Request): Promise<any>;
+        afterConfigLoaded(cb: Function): Reporter;
+        addRequestContextMetaConfig(property: any, options: any): void;
+        getRequestContextMetaConfig(property: any): any;
+        addPathToWatchForAutoCleanup(customPath: string): void;
+        getPathsToWatchForAutoCleanup(): any;
+        ensureTempDirectoryExists(): Promise<TempDirectoryResult>;
+        readTempFile(filename: string, opts: any): Promise<ReadTempFileResult>;
+        writeTempFile(filenameFn: Function, content: any, opts: any): Promise<WriteFileResult>;
+        readTempFileStream(filename: string, opts: any): Promise<ReadTempFileStreamResult>;
+        writeTempFileStream(filenameFn: Function, opts: any): Promise<WriteFileStreamResult>;
+        silentLogs(logger: any): void;
     }
 
     interface Configuration {
@@ -104,6 +140,59 @@ declare namespace JsReport {
             strategy: "dedicated-process" | "http-server" | "in-process" | string;
         }>;
         tempDirectory: string;
+    }
+
+    interface Settings {
+        add(key: string, value: string | any, req: Request): any;
+        get(key: string): [SettingItem];
+        findValue(key: string, req: Request): any;
+        set(key: string, avalue: any, req: Request): any;
+        addOrSet(key: string, avalue: any, req: Request): number | null;
+        init(documentStore: any, authorization: any): any;
+        _collection: [SettingItem];
+        documentStore: any;
+        registerEntity(documentStore: any): void;
+    }
+
+    interface SettingItem {
+        key: string;
+        value: any;
+    }
+
+    interface SchemaValidator {
+        schemaVersion: string;
+        rootSchema: any;
+        getRootSchema(): any;
+        validateRoot(data: any, schema?: any): any;
+        addSchema(name: string, _schema: any, replace: boolean): void;
+        validate(name: string, data: any, schema?: any): any;
+        getSchema(name: string): any;
+    }
+
+    interface TempDirectoryResult {
+        directoryPath: string;
+    }
+
+    interface WriteFileResult {
+        pathToFile: string;
+        filename: string;
+    }
+    interface WriteFileStreamResult {
+        pathToFile: string;
+        filename: string;
+        stream: fs.WriteStream;
+    }
+
+    interface ReadTempFileResult {
+        pathToFile: string;
+        filename: string;
+        context: string;
+    }
+
+    interface ReadTempFileStreamResult {
+        pathToFile: string;
+        filename: string;
+        stream: fs.ReadStream;
     }
 }
 
