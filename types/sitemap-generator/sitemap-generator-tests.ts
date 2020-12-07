c7954ffd145c0edb39f9c5aed0819c03cdf67a03
grgr-dkrk <shayashi513@gmail.com>
2020-12-07 23:14:41 +0900

    [sitemap-generator]add definition

---
+++ types/sitemap-generator/sitemap-generator-tests.ts
@@ -0,0 +1,46 @@
+import SitemapGenerator from 'sitemap-generator';
+
+// @ts-expect-error - no url
+SitemapGenerator();
+
+SitemapGenerator('https://example.com/');
+
+const generator = SitemapGenerator('https://example.com/', {
+    changeFreq: 'always',
+    filepath: './sitemap.xml',
+    ignore: url => /foo/g.test(url),
+    ignoreAMP: true,
+    lastMod: false,
+    maxEntriesPerFile: 1000,
+    priorityMap: [0.0, 0.4, 1.0],
+    // simplecrawler props
+    userAgent: 'Node/SitemapGenerator',
+    stripQuerystring: false,
+    // @ts-expect-error - exclude simplecrawler methods
+    wait: () => {},
+});
+
+generator.on('add', () => {});
+generator.on('done', () => {});
+generator.on('ignore', () => {});
+generator.on('error', error => {
+    error.message;
+});
+// @ts-expect-error - invalid event
+generator.on('bar', () => {});
+// @ts-expect-error - invalid arg
+generator.on('add', error => {
+    error.message;
+});
+
+generator.start();
+generator.stop();
+
+const crawler = generator.getCrawler();
+const sitemap = generator.getSitemap();
+
+crawler.on('crawlstart', () => {
+    sitemap.addURL('/my/static/url');
+});
+
+generator.queueURL('https://example.com/');
