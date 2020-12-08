979b77abb94f1611c48b83c1bca5fd1c5a2f3984
Yaroslav Kiliba <om.dattaya@gmail.com>
2020-12-08 23:02:10 +0200

    [lqip-modern] it should not error when passing only part of `LqipOptions`

--- types/lqip-modern/lqip-modern-tests.ts
+++ types/lqip-modern/lqip-modern-tests.ts
@@ -1,10 +1,17 @@
 import lqip = require('lqip-modern');
 
+// it should return the necessary type depending on `input`
 lqip(Buffer.from('')); // $ExpectType Promise<LqipResult>
 lqip('tst'); // $ExpectType Promise<LqipResult>
 lqip([Buffer.from('')]); // $ExpectType Promise<LqipResult[]>
 lqip(['tst']); // $ExpectType Promise<LqipResult[]>
 
+// it should not error when passing only part of `LqipOptions`
+lqip('tst', { resize: 32 });
+lqip('tst', { outputFormat: 'webp' });
+lqip('tst', { outputOptions: { loop: 10 } });
+
+// it should validate `outputOptions` depending on `outputFormat`
 lqip('tst', { outputFormat: 'jpg', outputOptions: { loop: 10 } }); // $ExpectError
 lqip('tst', { outputFormat: 'jpeg', outputOptions: { loop: 10 } }); // $ExpectError
 lqip('tst', { outputFormat: 'webp', outputOptions: { loop: 10 } });
