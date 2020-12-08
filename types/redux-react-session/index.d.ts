5ff5d0769f119c9912c8a4c5081c7810fbbe78fa
João Neto <joao@neto.pt>
2020-12-08 22:25:17 +0100

    proper type definitions

--- types/redux-react-session/index.d.ts
+++ types/redux-react-session/index.d.ts
@@ -1,42 +1,17 @@
-// Type definitions for redux-react-session 2.6.1
+// Type definitions for redux-react-session 2.6
 // Project: https://github.com/bernabe9/redux-react-session
 // Definitions by: João Neto <https://github.com/joaomlneto>
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
 
-import { Reducer, Dispatch, Action, Store } from 'redux';
-
-const GET_SESSION_SUCCESS = '@@redux-react-session/GET_SESSION_SUCCESS';
-const GET_SESSION_ERROR = '@@redux-react-session/GET_SESSION_ERROR';
-const GET_USER_SESSION_SUCCESS = '@@redux-react-session/GET_USER_SESSION_SUCCESS';
-const GET_USER_SESSION_ERROR = '@@redux-react-session/GET_USER_SESSION_ERROR';
-const INVALID_SESSION = '@@redux-react-session/INVALID_SESSION';
-
-interface StoreType {
-    dispatch: Dispatch;
-}
-
-type CookiesType = any;
-
-interface GetSessionSuccessAction extends Action {
-    type: typeof GET_SESSION_SUCCESS;
-}
-
-interface GetSessionErrorAction extends Action {
-    type: typeof GET_SESSION_ERROR;
-}
-
-interface GetUserSessionSuccessAction<UserType> extends Action {
-    type: typeof GET_USER_SESSION_SUCCESS;
-    user: UserType;
-}
-
-interface GetUserSessionErrorAction extends Action {
-    type: typeof GET_USER_SESSION_ERROR;
-}
-
-interface InvalidSessionAction extends Action {
-    type: typeof INVALID_SESSION;
-}
+import { Reducer, Action, Store } from 'redux';
+import {
+    GetSessionSuccessAction,
+    GetSessionErrorAction,
+    GetUserSessionSuccessAction,
+    GetUserSessionErrorAction,
+    InvalidSessionAction,
+} from './actions';
+import { SessionState } from './reducer';
 
 export type ActionTypes<UserType> =
     | GetSessionSuccessAction
@@ -45,43 +20,28 @@ export type ActionTypes<UserType> =
     | GetUserSessionErrorAction
     | InvalidSessionAction;
 
-export interface ReduxReactSessionState<UserType> {
-    authenticated: boolean;
-    checked: boolean;
-    invalid: boolean;
-    user: UserType;
-}
-
-export const initialState: ReduxReactSessionState = {
-    authenticated: false,
-    checked: false,
-    invalid: false,
-    user: {},
-};
-
 export interface SessionServiceOptions {
     driver: any;
-    validateSession: Function;
+    validateSession: (session: any) => boolean | Promise<boolean>;
     refreshOnCheckAuth: boolean;
     expires: number;
 }
 
-export class sessionService<SessionType, UserType extends Object>
-    implements Reducer<ReduxReactSessionState, AnyAction> {
-    constructor(store: StoreType, options?: SessionServiceOptions);
-    static setOptions(store: StoreType, options?: SessionServiceOptions): void;
-    static initSessionService(store: StoreType, options?: SessionServiceOptions): Promise;
-    static saveFromClient(cookies: CookiesType);
+export class sessionService /*implements Reducer<SessionState, Action>*/ {
+    constructor(store: Store, options?: SessionServiceOptions);
+    static setOptions(store: Store, options?: SessionServiceOptions): void;
+    static initSessionService(store: Store, options?: SessionServiceOptions): Promise<any>;
+    static saveFromClient(cookies: any): any;
     static invalidateSession(): void;
-    static attemptLoadUser(): Promise;
-    static refreshFromLocalStorage(): Promise;
+    static attemptLoadUser(): Promise<any>;
+    static refreshFromLocalStorage(): Promise<any>;
     static checkAuth(nextState: any, replace: any, next: any): void;
-    static saveSession(session: SessionType): Promise;
-    static loadSession(): Promise;
-    static deleteSession(): Promise;
-    static saveUser(user: UserType): Promise;
-    static loadUser(): Promise;
-    static deleteUser(): Promise;
+    static saveSession(session: any): Promise<any>;
+    static loadSession(): Promise<any>;
+    static deleteSession(): Promise<any>;
+    static saveUser(user: any): Promise<any>;
+    static loadUser(): Promise<any>;
+    static deleteUser(): Promise<any>;
 }
 
-export function sessionReducer<UserType>(state: ReduxReactSessionState<UserType> = initialState, action: Action): any;
+export function sessionReducer(state: SessionState, action: Action): any;
