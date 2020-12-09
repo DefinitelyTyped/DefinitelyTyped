c630c1dd8b1ced33987f37150a29db0c9eecb39f
Alessandro Rabitti <axel83.beta@gmail.com>
2020-12-09 20:38:17 +0100

    Add bero type definitions

---
+++ types/bero/index.d.ts
@@ -0,0 +1,21 @@
+// Type definitions for bero 0.1.3
+// Project: https://github.com/ZER0/bero
+// Definitions by: Alessandro Rabitti <https://github.com/silversonicaxel>
+// Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
+// TypeScript Version: 4.1
+
+type Block = string;
+
+type Element = string | { [index: string]: any } | (string | undefined)[];
+
+type Modifier = { [index: string]: any } | (string | undefined)[];
+
+type Bemmed = (arg1?: Element | Modifier, arg2?: Modifier) => string;
+
+type Joiner = string[];
+
+type Joined = string;
+
+export default function bem(block?: string, element?: Element, modifier?: Modifier): Bemmed;
+
+export function join(...arguments: Joiner): Joined;
