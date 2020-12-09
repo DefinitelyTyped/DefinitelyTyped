0182d4edb587e6ca8e15efabd000a5f8dfb3242a
Yu-Jack <jk82421@gmail.com>
2020-12-09 09:27:11 +0800

    feature: remove README and change major version in the header of index.d.ts

--- types/tpdirect/README.md
+++
@@ -1,5 +0,0 @@
-# TPDirect Usage Note
-
-This currenct version match the [TapPay Docs SDK v5.5](https://docs.tappaysdk.com/tutorial/en/home.html).  
-TapPay doesn't prodive the npm installation, but provide the global variable `TPDirect`. 
-So, you could use this for typing and hint.  
--- types/tpdirect/index.d.ts
+++ types/tpdirect/index.d.ts
@@ -1,4 +1,4 @@
-// Type definitions for non-npm package tpdirect-browser 1.0
+// Type definitions for non-npm package tpdirect-browser 5.5
 // Project: https://docs.tappaysdk.com/tutorial/en/home.html
 // Definitions by: Yu-Jack <https://github.com/Yu-Jack>
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
