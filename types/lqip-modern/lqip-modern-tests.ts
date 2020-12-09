065ca3f4e66907f433fa21634e9601e3b104afd5
Yaroslav Kiliba <om.dattaya@gmail.com>
2020-12-09 11:54:06 +0200

    test `JpegOptions` and `WebpOptions` w/ `progressive` and `lossless`

--- types/lqip-modern/lqip-modern-tests.ts
+++ types/lqip-modern/lqip-modern-tests.ts
@@ -9,13 +9,13 @@ lqip(['tst']); // $ExpectType Promise<LqipResult[]>
 // it should not error when passing only part of `LqipOptions`
 lqip('tst', { resize: 32 });
 lqip('tst', { outputFormat: 'webp' });
-lqip('tst', { outputOptions: { loop: 10 } });
+lqip(['tst'], { resize: 32, outputOptions: { lossless: true } });
 
 // it should validate `outputOptions` depending on `outputFormat`
-lqip('tst', { outputFormat: 'jpg', outputOptions: { loop: 10 } }); // $ExpectError
-lqip('tst', { outputFormat: 'jpeg', outputOptions: { loop: 10 } }); // $ExpectError
-lqip('tst', { outputFormat: 'webp', outputOptions: { loop: 10 } });
+lqip('tst', { outputFormat: 'jpg', outputOptions: { lossless: true } }); // $ExpectError
+lqip('tst', { outputFormat: 'jpeg', outputOptions: { lossless: true } }); // $ExpectError
+lqip('tst', { outputFormat: 'webp', outputOptions: { lossless: true } });
 
-lqip('tst', { outputFormat: 'jpg', outputOptions: { quantisationTable: 1 } });
-lqip('tst', { outputFormat: 'jpeg', outputOptions: { quantisationTable: 1 } });
-lqip('tst', { outputFormat: 'webp', outputOptions: { quantisationTable: 1 } }); // $ExpectError
+lqip('tst', { outputFormat: 'jpg', outputOptions: { progressive: true } });
+lqip('tst', { outputFormat: 'jpeg', outputOptions: { progressive: true } });
+lqip('tst', { outputFormat: 'webp', outputOptions: { progressive: true } }); // $ExpectError
