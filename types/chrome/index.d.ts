f2ea13c5bc64466b60bcd624cd6b5fd4fca5ed0e
gyeongwoopark <geewoo94@gmail.com>
2020-12-10 13:37:53 +0900

    update: chrome.search API

--- types/chrome/index.d.ts
+++ types/chrome/index.d.ts
@@ -5353,6 +5353,33 @@ declare namespace chrome.proxy {
 }
 
 ////////////////////
+// Search
+////////////////////
+/**
+ * Use the chrome.search API to search via the default provider.
+ * Permissions:  "search"
+ */
+
+declare namespace chrome.search {
+    export interface QueryInfo {
+        /** Location where search results should be displayed. CURRENT_TAB is the default.  */
+        disposition?: "CURRENT_TAB" | "NEW_TAB" | "NEW_WINDOW";
+        /** Location where search results should be displayed. tabIdcannot be used with disposition. */
+        tabId?: number;
+        /** String to query with the default search provider. */
+        text?: string;
+    }
+
+    /**
+     * Used to query the default search provider. In case of an error, runtime.lastError will be set.
+     * @param options search configuration options.
+     * @param callback The callback parameter should be a function that looks like this:
+     * function() => {...}
+     */
+    export function query(options: QueryInfo, callback: () => void): void;
+}
+
+////////////////////
 // Serial
 ////////////////////
 /**
