ccc70f4bf8772324ea81fb7017d094f6424e98cd
WoLewicki <wojciech.lewicki@swmansion.com>
2020-12-11 12:53:45 +0100

    fix: add another static

--- types/react-native/test/index.tsx
+++ types/react-native/test/index.tsx
@@ -388,6 +388,11 @@ export class TouchableNativeFeedbackTest extends React.Component {
                         <Text style={{ margin: 30 }}>Button</Text>
                     </View>
                 </TouchableNativeFeedback>
+                <TouchableNativeFeedback background={TouchableNativeFeedback.SelectableBackground()}>
+                    <View style={{ width: 150, height: 100, backgroundColor: 'red' }}>
+                        <Text style={{ margin: 30 }}>Button</Text>
+                    </View>
+                </TouchableNativeFeedback>
                 <TouchableNativeFeedback background={TouchableNativeFeedback.SelectableBackgroundBorderless()}>
                     <View style={{ width: 150, height: 100, backgroundColor: 'red' }}>
                         <Text style={{ margin: 30 }}>Button</Text>
