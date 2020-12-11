bffc888e073e587f7f78992394515b5a255889cf
Fay Hu <fay_hu@infosys.com>
2020-12-11 15:41:45 +0800

    bypass unused files

--- types/@pnp/index.d.ts
+++ types/@pnp/index.d.ts
@@ -5,13 +5,4 @@
 
 /// <reference types="node" />
 
-/// <reference path="adaljsclient/index.d.ts" />
-/// <reference path="common/index.d.ts" />
-/// <reference path="config-store/index.d.ts" />
-/// <reference path="graph/index.d.ts" />
-/// <reference path="logging/index.d.ts" />
-/// <reference path="msaljsclient/index.d.ts" />
-/// <reference path="nodejs/index.d.ts" />
-/// <reference path="odata/index.d.ts" />
-/// <reference path="sp-addinhelpers/index.d.ts" />
-/// <reference path="sp/index.d.ts" />
+/// <reference path="pnpjs/index.d.ts" />
