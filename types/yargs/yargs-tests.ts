c9da64641d6f9b6504ead389bd2c6501e685b607
Sergey Korolev <korolev.srg@gmail.com>
2020-12-11 21:59:00 +0300

    Added strictCommands typing

--- types/yargs/yargs-tests.ts
+++ types/yargs/yargs-tests.ts
@@ -1302,3 +1302,8 @@ function Argv$strictOptions() {
     .strictOptions()
     .argv;
 }
+
+function Argv$strictCommands() {
+    const argv1 = yargs.strictCommands().command('foo', 'foo command').argv;
+    const argv2 = yargs.strictCommands(true).command('foo', 'foo command').argv;
+}
