5e9cc6650ddeb4d77bb1d2005573be86eca09dbe
tqma <tqma@trip.com>
2020-12-15 16:44:04 +0800

    format

--- types/gulp-clean-css/gulp-clean-css-tests.ts
+++ types/gulp-clean-css/gulp-clean-css-tests.ts
@@ -1,48 +1,48 @@
-import * as gulpCleanCSS from './index'
-import * as gulp from 'gulp'
+import * as gulpCleanCSS from './index';
+import * as gulp from 'gulp';
 
 // gulp
-gulp.src('/foo').pipe(gulpCleanCSS())
+gulp.src('/foo').pipe(gulpCleanCSS());
 
 // default
-gulpCleanCSS()
+gulpCleanCSS();
 
 const pathToOutputDirectory = 'path';
 
 // with props
-gulpCleanCSS({ sourceMap: true, rebaseTo: pathToOutputDirectory })
+gulpCleanCSS({ sourceMap: true, rebaseTo: pathToOutputDirectory });
 
 // with props - promise resolution
-gulpCleanCSS({ returnPromise: true })
+gulpCleanCSS({ returnPromise: true });
 
-gulpCleanCSS({ returnPromise: false })
+gulpCleanCSS({ returnPromise: false });
 
-gulpCleanCSS({ returnPromise: true, rebaseTo: pathToOutputDirectory })
+gulpCleanCSS({ returnPromise: true, rebaseTo: pathToOutputDirectory });
 
-gulpCleanCSS({ returnPromise: true, sourceMap: true })
+gulpCleanCSS({ returnPromise: true, sourceMap: true });
 
 gulpCleanCSS({
-  format: {
-      semicolonAfterLastProperty: true
-  }
-})
+    format: {
+        semicolonAfterLastProperty: true,
+    },
+});
 
 // callback
-gulpCleanCSS({}, (details) => {
-  console.log(details)
-  console.log(details.errors)
-  console.log(details.name)
-  console.log(details.path)
-  console.log(details.stats)
-  console.log(details.stats.efficiency)
-  console.log(details.stats.minifiedSize)
-  console.log(details.stats.originalSize)
-  console.log(details.stats.timeSpent)
-  console.log(details.warnings)
+gulpCleanCSS({}, details => {
+    console.log(details);
+    console.log(details.errors);
+    console.log(details.name);
+    console.log(details.path);
+    console.log(details.stats);
+    console.log(details.stats.efficiency);
+    console.log(details.stats.minifiedSize);
+    console.log(details.stats.originalSize);
+    console.log(details.stats.timeSpent);
+    console.log(details.warnings);
 
-  // $ExpectType string
-  details.name
+    // $ExpectType string
+    details.name;
 
-  // $ExpectType string
-  details.path
-})
+    // $ExpectType string
+    details.path;
+});
--- types/gulp-clean-css/index.d.ts
+++ types/gulp-clean-css/index.d.ts
@@ -5,18 +5,21 @@
 
 /// <reference types="node" />
 
-import { Options, Output } from 'clean-css'
+import { Options, Output } from 'clean-css';
 
 declare namespace GulpCleanCss {
     interface Details {
-        stats: Output['stats'],
-        errors: Output['errors'],
-        warnings: Output['warnings'],
-        path: string,
-        name: string
+        stats: Output['stats'];
+        errors: Output['errors'];
+        warnings: Output['warnings'];
+        path: string;
+        name: string;
     }
 }
 
-declare function GulpCleanCss(options?: Options, callback?: (details: GulpCleanCss.Details) => void): NodeJS.ReadWriteStream
+declare function GulpCleanCss(
+    options?: Options,
+    callback?: (details: GulpCleanCss.Details) => void,
+): NodeJS.ReadWriteStream;
 
 export = GulpCleanCss;
