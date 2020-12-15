0b4255425e5cf0398632b32a44353a8210783f2b
Florent Jaby <florent.jaby@gmail.com>
2020-12-15 19:25:59 +0100

    Prettier formatting

--- types/jest-retries/jest-retries-tests.ts
+++ types/jest-retries/jest-retries-tests.ts
@@ -1,13 +1,18 @@
-import retries from "jest-retries";
+import retries from 'jest-retries';
 
 retries('some flaky test', 5, () => {
-  if (Math.random() > 0.2) {
-    throw Error('failed');
-  }
+    if (Math.random() > 0.2) {
+        throw Error('failed');
+    }
 });
 
-retries('some flaky test with timeout', 5, () => {
-  if (Math.random() > 0.2) {
-    throw Error('failed');
-  }
-}, 10000);
+retries(
+    'some flaky test with timeout',
+    5,
+    () => {
+        if (Math.random() > 0.2) {
+            throw Error('failed');
+        }
+    },
+    10000,
+);
