674cc9f9090739636720d7738d118282cce819f5
Chris Midgley <chris.midgley@newredo.com>
2020-12-15 16:13:18 +0000

    xml-encryption: create typings

---
+++ types/xml-encryption/index.d.ts
@@ -0,0 +1,34 @@
+// Type definitions for xml-encryption 1.2
+// Project: https://github.com/auth0/node-xml-encryption#readme
+// Definitions by: Chris Midgley <https://github.com/midgleyc>
+// Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
+/// <reference types="node" />
+
+export interface EncryptOptions {
+    rsa_pub: string | Buffer;
+    pem: string | Buffer;
+    keyEncryptionAlgorithm: string;
+    encryptionAlgorithm?: string;
+    disallowEncryptionWithInsecureAlgorithm?: boolean;
+    warnInsecureAlgorithm?: boolean;
+    input_encoding?: string;
+}
+
+export interface DecryptOptions {
+    key: string | Buffer;
+    disallowDecryptionWithInsecureAlgorithm?: boolean;
+    warnInsecureAlgorithm?: boolean;
+}
+
+export function decrypt(xml: string, options: DecryptOptions, callback: (error: Error, result: string) => void): void;
+export function decryptKeyInfo(doc: string, options: DecryptOptions): Buffer;
+export function encrypt(
+    content: string,
+    options: EncryptOptions,
+    callback: (error: Error, result: string) => void,
+): void;
+export function encryptKeyInfo(
+    symmetricKey: string | Buffer,
+    options: EncryptOptions,
+    callback: (error: Error, result: string) => void,
+): void;
