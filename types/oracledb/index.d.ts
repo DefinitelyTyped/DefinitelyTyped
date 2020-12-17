9896275928502c58088f2999d43b3e94e0089d1d
Connor Fitzgerald <connor.jay.fitzgerald@outlook.com>
2020-12-17 18:33:00 +0000

    remove inactive maintainer

--- types/oracledb/index.d.ts
+++ types/oracledb/index.d.ts
@@ -1,7 +1,6 @@
 // Type definitions for oracledb 5.1
 // Project: https://github.com/oracle/node-oracledb
-// Definitions by: Richard Natal <https://github.com/Bigous>
-//                 Connor Fitzgerald <https://github.com/connorjayfitzgerald>
+// Definitions by: Connor Fitzgerald <https://github.com/connorjayfitzgerald>
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
 // TypeScript Version: 3.5
 
