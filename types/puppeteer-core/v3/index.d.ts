f5f30723adb9ecd48b4cb177ba60384cdb0eeae9
SirDiR <phddir@gmail.com>
2020-12-17 22:35:56 +0100

    [puppeteer-core] upd to latest 5.4 - add support for legacy versions v1, v2, v3, v4

--- types/puppeteer-core/v3/index.d.ts
+++ types/puppeteer-core/v3/index.d.ts
@@ -1,4 +1,4 @@
-// Type definitions for puppeteer-core 5.4
+// Type definitions for puppeteer-core 5.3
 // Project: https://github.com/GoogleChrome/puppeteer#readme
 // Definitions by: Fumiaki Matsushima <https://github.com/mtsmfm>
 //                 Tymur Kubai <https://github.com/sirdir>
