86bc25bae8b0357c08130295f6703b0115e5c3ec
Arvin Wei <clovery.org@gmail.com>
2020-12-19 22:44:30 +0800

    add textEditor test case

--- types/ipaiva/ipaiva-tests.ts
+++ types/ipaiva/ipaiva-tests.ts
@@ -3,3 +3,5 @@ import * as ipaiva from 'ipaiva';
 ipaiva.library.onDidPick(({ url }) => {
     ipaiva.window.showInformationMessage(url);
 });
+
+ipaiva.textEditor.create(document.createElement('div'));
