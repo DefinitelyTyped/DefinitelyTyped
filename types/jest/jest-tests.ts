be7e5b89bbe3a5c1c8c9ea4d6042bd2347d1c773
Gerkin <nihilivin@gmail.com>
2021-01-04 00:11:23 +0100

    Update tests

--- types/jest/jest-tests.ts
+++ types/jest/jest-tests.ts
@@ -528,7 +528,7 @@ class TestMocked {
 
 const mocked: jest.Mocked<TestMocked> = new TestMocked() as any;
 mocked.test1.mockImplementation(() => Promise.resolve({ a: 1 }));
-// $ExpectType (x: Type1) => Promise<Type1> | undefined
+// $ExpectType ((x: Type1) => Promise<Type1>) | undefined
 mocked.test1.getMockImplementation();
 mocked.test1.mockReturnValue(Promise.resolve({ a: 1 }));
 // $ExpectType MockInstance<Promise<Type1>, [Type1]> & ((x: Type1) => Promise<Type1>) || MockInstance<Promise<Type1>, [x: Type1]> & ((x: Type1) => Promise<Type1>)
