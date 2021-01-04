a2593a29e7c6868b82a16ce8940338ac2af0c230
Pepe Cano <825430+ppcano@users.noreply.github.com>
2021-01-05 00:36:47 +0100

    k6: set version 0.29

--- types/k6/index.d.ts
+++ types/k6/index.d.ts
@@ -1,4 +1,4 @@
-// Type definitions for k6 0.28
+// Type definitions for k6 0.29
 // Project: https://k6.io/docs
 // Definitions by: MajorBreakfast <https://github.com/MajorBreakfast>
 //                 Book Moons <https://github.com/bookmoons>
