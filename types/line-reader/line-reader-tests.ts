d18baf0365474c149f1977f439be963460a7ff34
Arif Mahmud Rana <arif_mahmud_rana@hotmail.com>
2021-01-04 12:37:07 +0600

    Fix user define promisify definition for line-reader

--- types/line-reader/line-reader-tests.ts
+++ types/line-reader/line-reader-tests.ts
@@ -2,6 +2,31 @@
 
 import lineReader = require('line-reader');
 
+const eachLine = (
+    filename: string | NodeJS.ReadableStream,
+    options: LineReaderOptions,
+    iteratee: (line: string, last?: boolean, cb?: Function) => void,
+): Promise<void> =>
+    new Promise((resolve, reject) => {
+        lineReader.eachLine(filename, options, iteratee, (err: Error) => {
+            if (err) {
+                reject(err);
+            } else {
+                resolve();
+            }
+        });
+    });
+
+eachLine('line-reader-tests.ts', {}, function(line) {
+    console.log(line);
+})
+    .then(function() {
+        console.log('done');
+    })
+    .catch(function(err: Error) {
+        console.error(err);
+    });
+
 lineReader.open('line-reader-tests.ts', function(err: Error, reader: LineReader) {
     if (err) throw err;
     if (reader.hasNextLine()) {
