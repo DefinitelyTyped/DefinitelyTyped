1d794feb003609f9697bd5980950b35406ea6bee
Peter Savnik <peter@savnik.dk>
2021-01-04 20:30:11 +0100

    Adding comments and new line

--- types/aws-lambda/trigger/iot.d.ts
+++ types/aws-lambda/trigger/iot.d.ts
@@ -4,5 +4,6 @@ export type IoTHandler = Handler<IoTEvent, void>;
 
 // IoT
 // https://docs.aws.amazon.com/lambda/latest/dg/services-iot.html
+// IoT payload is not restriced to JSON, but JSON is highly recommended. Types as string, number or array are possible to use.
 
 export type IoTEvent = string | number | { [key: string]: any } | any[];
