9a049dd73b45f364dbd58cb0a21665a2d827a014
Peter Savnik <peter@savnik.dk>
2021-01-05 07:49:13 +0100

    Added tests

---
+++ types/aws-lambda/test/iot-tests.ts
@@ -0,0 +1,19 @@
+import { IoTEvent, IoTHandler } from "aws-lambda";
+
+// TODO: Update test to read all event properties, like the user will.
+
+const handler: IoTHandler = async (event, context, callback) => { };
+
+// See https://docs.aws.amazon.com/lambda/latest/dg/eventsources.html#eventsources-sqs
+const eventObject: IoTEvent = {
+  "row": "10",
+  "pos": "23",
+  "moisture": "75"
+};
+
+const eventString: IoTEvent = "AWS Lambda IoT Event";
+
+const eventNumber: IoTEvent = 100;
+
+const eventArray: IoTEvent = [eventObject, eventString, eventNumber];
+
