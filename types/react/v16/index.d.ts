51cff9182b2cebfe9e1a1236b3876dd3039cc2bb
AlexUzan <41793098+AlexUzan@users.noreply.github.com>
2021-01-06 16:48:07 +0100

    Add children to forwardRef component props

--- types/react/v16/index.d.ts
+++ types/react/v16/index.d.ts
@@ -802,7 +802,7 @@ declare namespace React {
         propTypes?: WeakValidationMap<P>;
     }
 
-    function forwardRef<T, P = {}>(render: ForwardRefRenderFunction<T, P>): ForwardRefExoticComponent<PropsWithoutRef<P> & RefAttributes<T>>;
+    function forwardRef<T, P = {}>(render: ForwardRefRenderFunction<T, P>): ForwardRefExoticComponent<PropsWithoutRef<PropsWithChildren<P>> & RefAttributes<T>>;
 
     /** Ensures that the props do not include ref at all */
     type PropsWithoutRef<P> =
