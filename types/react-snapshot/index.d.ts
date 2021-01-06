f18d837dd13af39d36ab3d300baccbf539c15b6c
Evan Gubarev <evan@evangubarev.com>
2021-01-06 00:40:32 -0800

    fix

--- types/react-snapshot/index.d.ts
+++ types/react-snapshot/index.d.ts
@@ -3,6 +3,50 @@
 // Definitions by: Evan Gubarev <https://github.com/gubareve>
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
 
-import * as ReactDOM from 'react-dom';
+import * as React from 'react';
 
-export const render: ReactDOM.Renderer;
+export interface Renderer {
+    <T extends Element>(
+        element: React.DOMElement<React.DOMAttributes<T>, T>,
+        container: Element | DocumentFragment | null,
+        callback?: () => void
+    ): T;
+
+    (
+        element: Array<React.DOMElement<React.DOMAttributes<any>, any>>,
+        container: Element | DocumentFragment | null,
+        callback?: () => void
+    ): Element;
+
+    (
+        element: React.SFCElement<any> | Array<React.SFCElement<any>>,
+        container: Element | DocumentFragment | null,
+        callback?: () => void
+    ): void;
+
+    <P, T extends React.Component<P, React.ComponentState>>(
+        element: React.CElement<P, T>,
+        container: Element | DocumentFragment | null,
+        callback?: () => void
+    ): T;
+
+    (
+        element: Array<React.CElement<any, React.Component<any, React.ComponentState>>>,
+        container: Element | DocumentFragment | null,
+        callback?: () => void
+    ): React.Component<any, React.ComponentState>;
+
+    <P>(
+        element: React.ReactElement<P>,
+        container: Element | DocumentFragment | null,
+        callback?: () => void
+    ): React.Component<P, React.ComponentState> | Element | void;
+
+    (
+        element: React.ReactElement[],
+        container: Element | DocumentFragment | null,
+        callback?: () => void
+    ): React.Component<any, React.ComponentState> | Element | void;
+}
+
+export const render: Renderer;
