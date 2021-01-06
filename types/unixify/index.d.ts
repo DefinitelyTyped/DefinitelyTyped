296fb315aaccd22bae688782e720237c06527b96
Karl Horky <karl.horky@gmail.com>
2021-01-06 16:11:49 +0100

    Fix version

--- types/unixify/index.d.ts
+++ types/unixify/index.d.ts
@@ -1,4 +1,4 @@
-// Type definitions for unixify 3.0
+// Type definitions for unixify 1.0
 // Project: https://github.com/jonschlinkert/unixify
 // Definitions by: Karl Horky <https://github.com/karlhorky>
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
