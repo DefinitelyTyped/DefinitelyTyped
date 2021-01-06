7c64df710f7b2cfe64df9a31d3fb95d294fcbee5
Evan Gubarev <evan@evangubarev.com>
2021-01-06 11:19:53 -0800

    Fix typings

--- types/react-snapshot/index.d.ts
+++ types/react-snapshot/index.d.ts
@@ -5,48 +5,4 @@
 
 import * as React from 'react';
 
-export interface Renderer {
-    <T extends Element>(
-        element: React.DOMElement<React.DOMAttributes<T>, T>,
-        container: Element | DocumentFragment | null,
-        callback?: () => void
-    ): T;
-
-    (
-        element: Array<React.DOMElement<React.DOMAttributes<any>, any>>,
-        container: Element | DocumentFragment | null,
-        callback?: () => void
-    ): Element;
-
-    (
-        element: React.SFCElement<any> | Array<React.SFCElement<any>>,
-        container: Element | DocumentFragment | null,
-        callback?: () => void
-    ): void;
-
-    <P, T extends React.Component<P, React.ComponentState>>(
-        element: React.CElement<P, T>,
-        container: Element | DocumentFragment | null,
-        callback?: () => void
-    ): T;
-
-    (
-        element: Array<React.CElement<any, React.Component<any, React.ComponentState>>>,
-        container: Element | DocumentFragment | null,
-        callback?: () => void
-    ): React.Component<any, React.ComponentState>;
-
-    <P>(
-        element: React.ReactElement<P>,
-        container: Element | DocumentFragment | null,
-        callback?: () => void
-    ): React.Component<P, React.ComponentState> | Element | void;
-
-    (
-        element: React.ReactElement[],
-        container: Element | DocumentFragment | null,
-        callback?: () => void
-    ): React.Component<any, React.ComponentState> | Element | void;
-}
-
-export const render: Renderer;
+export function render(rootComponent: React.Component | JSX.Element, domElement: HTMLElement | null): void;
--- types/react-snapshot/react-snapshot-tests.tsx
+++ types/react-snapshot/react-snapshot-tests.tsx
@@ -1,10 +1,14 @@
 import * as React from 'react';
 import { render } from 'react-snapshot';
 
-function Test() {
-    return (
+class Test extends React.Component {
+    render() {
+        return (
         <h1>Test!</h1>
-    );
+        );
+    }
 }
 
 render(<Test/>, document.getElementById('test'));
+render(<div><Test/></div>, document.getElementById('test'))
+
