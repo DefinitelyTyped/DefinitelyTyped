7adb5ab79bfb30bc10d334de9630c23de136ba57
Luiz Jaccao <luiz.jaccao@catho.com>
2021-01-06 21:16:52 -0300

    fix test file

--- types/css-to-react-native/index.d.ts
+++ types/css-to-react-native/index.d.ts
@@ -1,22 +1,23 @@
-// Type definitions for css-to-react-native 3.0.0
+// Type definitions for css-to-react-native 3.0
 // Project: https://github.com/styled-components/css-to-react-native
 // Definitions by: Luiz F. Jacção <https://github.com/jaccao>
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
 
-export type StyleTuple = [string, string]
+export type StyleTuple = [string, string];
 
 export interface Style {
-  [key: string]: string | number | Style
+  [key: string]: string | number | Style;
 }
 
-export function getPropertyName(name: string): string
+export function getPropertyName(name: string): string;
+
 export function getStylesForProperty(
   name: string,
   value: string,
   allowShorthand?: boolean
-): Style
+): Style;
 
 export default function transform(
   styleTuples: StyleTuple[],
   shorthandBlacklist?: string[]
-): Style
+): Style;
