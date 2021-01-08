557f3ff19dac51e51d5abece153e7cfee2bbd6c0
Ceci Woodward <ceci@salte.io>
2021-01-08 14:05:56 -0600

    chore(parcel-bundler): update to new name / username (#50490)

--- types/parcel-bundler/index.d.ts
+++ types/parcel-bundler/index.d.ts
@@ -1,7 +1,7 @@
 // Type definitions for parcel-bundler 1.12
 // Project: https://github.com/parcel-bundler/parcel#readme
 // Definitions by: pinage404 <https://github.com/pinage404>
-//                 Nick Woodward <https://github.com/nick-woodward>
+//                 Ceci Woodward <https://github.com/ceci-woodward>
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
 // TypeScript Version: 2.3
 
