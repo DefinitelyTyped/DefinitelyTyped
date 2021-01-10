331516bde28586bc933bbcb9333fff6000747c63
Marton Lederer <marton@lederer.hu>
2021-01-10 13:33:39 +0100

    Add types for @limestonefi/api

---
+++ types/limestonefi__api/index.d.ts
@@ -0,0 +1,18 @@
+// Type definitions for @limestonefi/api 0.1.2
+// Project: https://github.com/limestone-finance/limestone-api
+// Definitions by: Marton Lederer <https://github.com/martonlederer>
+// Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
+// TypeScript Version: 4.1.2
+
+interface PriceData {
+  price: number;
+  updated: Date;
+}
+
+/** 
+ * Get the price of a supported token 
+ * 
+ * @param token Token symbol
+ * @returns Price data
+*/
+export async function getPrice(token: string): Promise<PriceData>;
