83af071bd894377cc084d547272ed7def7f6adbd
Jaakko Nurkka <jaakkonurkka@gmail.com>
2021-01-11 17:21:08 +0100

    Add test for check option

--- types/simpl-schema/simpl-schema-tests.ts
+++ types/simpl-schema/simpl-schema-tests.ts
@@ -1,4 +1,5 @@
 import SimpleSchema, { SimpleSchemaDefinition, SchemaDefinition } from 'simpl-schema';
+import { check } from 'meteor/check';
 
 const schema: SimpleSchemaDefinition = {
     basicString: {
@@ -106,7 +107,8 @@ const StringSchemaWithOptions = new SimpleSchema({
         trimStrings: true,
         getAutoValues: true,
         removeNullsFromArrays: true,
-    }
+    },
+    check,
 });
 
 new SimpleSchema({
