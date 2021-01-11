bd54ceecc0abdac430ff65ca5154985f92674d3c
Sarsa Murmu <sarsamurmu05@gmail.com>
2021-01-11 18:37:27 +0530

    Fix typo

--- types/estree/index.d.ts
+++ types/estree/index.d.ts
@@ -13,7 +13,7 @@
 // to the interfaces that extend them (like Identifier or
 // ArrowFunctionExpression), but you can't extend a type union or enforce
 // common fields on them. So we've split the high level interfaces into two
-// types, a base type which passes down inhereted fields, and a type union of
+// types, a base type which passes down inherited fields, and a type union of
 // all types which extend the base type. Only the type union is exported, and
 // the union is how other types refer to the collection of inheriting types.
 //
