0f3daf13d3a8666cf5f4f69f1ecc53cbf25dddce
Gita Vahdatinia <gita@coda.io>
2021-01-11 17:04:26 -0800

    Match new type defn for webpack v5 for outputfilesystem

--- types/memory-fs/index.d.ts
+++ types/memory-fs/index.d.ts
@@ -50,21 +50,29 @@ declare class MemoryFileSystem {
         }
     ): any;
 
+
+
+
+
     createWriteStream(path: string, options?: any): any;
+
+    dirname?(arg0: string): string;
 
     exists(path: string, callback: (isExist: boolean) => void): void;
 
     writeFile(path: string, content: string | Buffer, callback: (err: Error | undefined) => void): void;
-
     writeFile(path: string, content: string | Buffer, encoding: string, callback: (err: Error | undefined) => void): void;
+    writeFile(arg0: string,arg1: string | Buffer,arg2: (arg0?: NodeJS.ErrnoException) => void): void;
 
     join(path: string, request: string): string;
+  	join?(arg0: string, arg1: string): string;
 
     pathToArray(path: string): string[];
 
     normalize(path: string): string;
 
     stat(path: string, callback: (err: Error | null, result?: any) => void): void;
+    stat(arg0: string,arg1: (arg0?: NodeJS.ErrnoException, arg1?: FsStats) => void): void;
 
     readdir(path: string, callback: (err: Error | null, result?: any) => void): void;
 
@@ -78,9 +86,14 @@ declare class MemoryFileSystem {
 
     mkdir(path: string, callback: (err: Error | null) => void): void;
     mkdir(path: string, optArg: {}, callback: (err: Error | null, result?: any) => void): void;
+    mkdir(arg0: string, arg1: (arg0?: ErrnoException | undefined) => void): void;
 
     readFile(path: string, callback: (err: Error | null, result?: any) => void): void;
     readFile(path: string, optArg: {}, callback: (err: Error | null, result?: any) => void): void;
+    readFile(arg0: string, arg1: (arg0?: NodeJS.ErrnoException, arg1?: Buffer) => void): void;
+
+    relative(arg0: string, arg1: string): string;
+
 }
 
 export = MemoryFileSystem;
