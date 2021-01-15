cdd9e79fcdf878f216515c63f5d475ed3ee80b43
Peter Wagenet <peter.wagenet@gmail.com>
2021-01-15 11:08:03 -0800

    [ember-data] Improve Transform handling
    * Support attr defaultValue that is a primitive value, not a callback
    * Corrects return type of attrs with transforms to be transform
      return type, not the transform itself.
    * Fixes handling of non-transforms in TransformRegistry for
      backwards compatibility

--- types/ember-data/test/model.ts
+++ types/ember-data/test/model.ts
@@ -14,12 +14,16 @@ const Person = DS.Model.extend({
         return `${this.get('firstName')} ${this.get('lastName')}`;
     }),
 
-    pointOne: DS.attr('point', { defaultValue: () => Point.create({ x: 1, y: 2 })}),
-    pointTwo: DS.attr('point', { defaultValue: Point.create({ x: 1, y: 2 }) }),
+    point: DS.attr('point', { defaultValue: () => Point.create({ x: 1, y: 2 })}),
+    oldPoint: DS.attr('oldPoint', { defaultValue: () => Point.create({ x: 1, y: 2 })}),
+
+    // Can't have a non-primitive as default
+    anotherPoint: DS.attr('point', { defaultValue: Point.create({ x: 1, y: 2 })}) // $ExpectError
 });
 
 const person = Person.create();
-assertType<Point>(person.get('pointOne'));
+assertType<Point>(person.get('point'));
+assertType<Point>(person.get('oldPoint'));
 
 const User = DS.Model.extend({
     username: DS.attr('string'),
