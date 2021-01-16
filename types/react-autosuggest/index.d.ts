28b41bce31f6aa2bed3f86b391b81d3332499fa9
Arthur Fücher <arthurfucher@gmail.com>
2021-01-16 13:38:33 -0300

    remove patch version from header

--- types/react-autosuggest/index.d.ts
+++ types/react-autosuggest/index.d.ts
@@ -1,4 +1,4 @@
-// Type definitions for react-autosuggest 10.1.0
+// Type definitions for react-autosuggest 10.1
 // Project: http://react-autosuggest.js.org/, https://github.com/moroshko/react-autosuggest
 // Definitions by: Nicolas Schmitt <https://github.com/nicolas-schmitt>
 //                 Philip Ottesen <https://github.com/pjo256>
