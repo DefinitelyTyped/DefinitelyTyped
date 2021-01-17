e722c020eba24fd8d6949ec7a7d862a09271b789
Scott Willeke <scott@willeke.com>
2021-01-17 14:08:33 -0800

    chore: adding @ryanblock as a definition owner per his request in slack.
    * Ryan's request in slack https://architecture-as-text.slack.com/archives/C6BGT0D08/p1610920330030000?thread_ts=1593229503.429300&cid=C6BGT0D08
    * Ryan is also an active contributor to the package: https://github.com/architect/functions/commits?author=ryanblock

--- types/architect__functions/index.d.ts
+++ types/architect__functions/index.d.ts
@@ -1,6 +1,7 @@
 // Type definitions for @architect/functions 3.13
 // Project: https://github.com/architect/functions
-// Definitions by: Scott Willeke <https://github.com/DefinitelyTyped>
+// Definitions by: Scott Willeke <https://github.com/activescott>
+//                 Ryan Block <https://github.com/ryanblock>
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
 import { ArcHttp } from './http';
 import { ArcTables } from './tables';
