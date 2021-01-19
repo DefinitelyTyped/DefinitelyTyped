feef401c63ddc71a3e4391e407a0c9de85730f74
Corné Dorrestijn <cdorrestijn@emico.nl>
2021-01-19 13:30:44 +0100

    Ran prettier on the test file

--- types/react-scroll-sync/react-scroll-sync-tests.tsx
+++ types/react-scroll-sync/react-scroll-sync-tests.tsx
@@ -2,18 +2,8 @@ import * as React from 'react';
 import { ScrollSync, ScrollSyncPane } from 'react-scroll-sync';
 
 () => (
-    <ScrollSync
-        onSync={e => e.childNodes}
-        proportional
-        vertical
-        horizontal
-        enabled
-    >
-        <ScrollSyncPane
-            enabled
-            group="one"
-            attachTo={document.body}
-        >
+    <ScrollSync onSync={e => e.childNodes} proportional vertical horizontal enabled>
+        <ScrollSyncPane enabled group="one" attachTo={document.body}>
             <div></div>
         </ScrollSyncPane>
     </ScrollSync>
