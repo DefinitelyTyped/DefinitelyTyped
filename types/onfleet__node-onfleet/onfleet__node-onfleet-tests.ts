5544a53994ed51a0dc8da67a31be9d9bd635b17a
Julien M <julien.malaval@gokepler.fr>
2021-01-22 15:08:18 +0100

    Add some tests

--- types/onfleet__node-onfleet/onfleet__node-onfleet-tests.ts
+++ types/onfleet__node-onfleet/onfleet__node-onfleet-tests.ts
@@ -16,7 +16,7 @@ async function testTasks(onfleet: Onfleet) {
     destination: 'fake_destination_id',
   });
 
-  await onfleet.tasks.create({
+  const taskCreated = await onfleet.tasks.create({
     recipients: [
       '<recipient-id-1>',
       '<recipient-id-2>',
@@ -36,14 +36,22 @@ async function testTasks(onfleet: Onfleet) {
     }
   });
 
+  if (taskCreated.container.type === 'TEAM') {
+    taskCreated.container.team === 'teamId';
+  }
+
   // test tasks.update
-  await onfleet.tasks.update(dummyTask.id, {
+  const taskUpdated = await onfleet.tasks.update(dummyTask.id, {
     notes: 'Some test task notes',
     container: {
       type: 'WORKER',
       worker: 'workerId'
     }
   });
+
+  if (taskUpdated.container.type === 'WORKER') {
+    taskUpdated.container.worker === 'workerId';
+  }
 
   // test tasks.update barcodes
   await onfleet.tasks.update(dummyTask.id, {
