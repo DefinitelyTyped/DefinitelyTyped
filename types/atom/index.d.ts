070db17de3df45d40972c7278866d0f566eb8728
Amin Yahyaabadi <aminyahyaabadi74@gmail.com>
2021-01-23 02:26:35 -0600

    Make color a separate file

--- types/atom/index.d.ts
+++ types/atom/index.d.ts
@@ -303,17 +303,7 @@ export * from "./src/config";
 
 export * from "./src/config-schema";
 
-/**
- *  A simple color class returned from Config::get when the value at the key path is
- *  of type 'color'.
- */
-export interface Color {
-    /** Returns a string in the form '#abcdef'. */
-    toHexString(): string;
-
-    /** Returns a string in the form 'rgba(25, 50, 75, .9)'. */
-    toRGBAString(): string;
-}
+export * from "./src/color";
 
 export interface CommandRegistryTargetMap extends HTMLElementTagNameMap {
   [key: string]: EventTarget;
