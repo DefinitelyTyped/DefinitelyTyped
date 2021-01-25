1d9616a1fd887be845be1d6cab75253383c834cd
Nathan Bierema <nbierema@gmail.com>
2021-01-25 09:04:59 -0500

    var => const

--- types/commonmark-react-renderer/commonmark-react-renderer-tests.ts
+++ types/commonmark-react-renderer/commonmark-react-renderer-tests.ts
@@ -1,9 +1,9 @@
 import * as CommonMark from 'commonmark';
 import * as ReactRenderer from 'commonmark-react-renderer';
 
-var parser = new CommonMark.Parser();
-var renderer = new ReactRenderer();
+const parser = new CommonMark.Parser();
+const renderer = new ReactRenderer();
 
-var input = '# This is a header\n\nAnd this is a paragraph';
-var ast = parser.parse(input);
-var result = renderer.render(ast);
+const input = '# This is a header\n\nAnd this is a paragraph';
+const ast = parser.parse(input);
+const result = renderer.render(ast);
