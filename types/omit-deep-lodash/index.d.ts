1718489c1165b23863f43fef16c8fb3ac74c3e20
Dmitrii Kartashev <dmitrii@freightpath.io>
2021-01-25 10:39:40 -0500

    [@types/omit-deep-lodash] Better types

--- types/omit-deep-lodash/index.d.ts
+++ types/omit-deep-lodash/index.d.ts
@@ -4,7 +4,31 @@
 //                 Bartosz Kopciuch <https://github.com/ideffix>
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
 
-declare function omitDeep(input: object, ...props: string[]): object;
-declare function omitDeep(input: object, props: string[]): object;
+// tslint:disable-next-line:ban-types
+type Primitive = string | Function | number | boolean | symbol | undefined | null;
+type DeepOmitArray<T extends any[], K> = {
+    [P in keyof T]: DeepOmit<T[P], K>;
+};
+
+type DeepOmit<Input, OmittedKeys> = Input extends Primitive
+    ? Input
+    : {
+          [P in Exclude<keyof Input, OmittedKeys>]: Input[P] extends infer TP
+              ? TP extends Primitive
+                  ? TP
+                  : TP extends any[]
+                  ? DeepOmitArray<TP, OmittedKeys>
+                  : DeepOmit<TP, OmittedKeys>
+              : never;
+      };
+
+declare function omitDeep<Input, OmittedKeys extends string | string[]>(
+    input: Input,
+    props: OmittedKeys,
+): DeepOmit<Input, OmittedKeys>;
+declare function omitDeep<Input, OmittedKeys extends string[]>(
+    input: Input,
+    ...props: OmittedKeys
+): DeepOmit<Input, OmittedKeys>;
 
 export = omitDeep;
