064bc1832bd933d2eff99774fa92bf37def15c35
Sebastian Krüger <2pi_r2@gmx.de>
2021-01-25 10:27:02 +0100

    update tests

--- types/ackee-tracker/ackee-tracker-tests.ts
+++ types/ackee-tracker/ackee-tracker-tests.ts
@@ -3,25 +3,18 @@ import * as ackeeTracker from 'ackee-tracker';
 ackeeTracker.attributes();
 ackeeTracker.attributes(true);
 
-const instance1 = ackeeTracker.create({
-    server: 'https://example.com',
-    domainId: 'hd11f820-68a1-11e6-8047-79c0c2d9bce0',
-});
+const instance1 = ackeeTracker.create('https://example.com');
 
-const { stop } = instance1.record();
+const { stop } = instance1.record('hd11f820-68a1-11e6-8047-79c0c2d9bce0');
 stop();
 
 ackeeTracker.detect();
 
-const instance2 = ackeeTracker.create(
-    {
-        server: 'https://example.com',
-        domainId: 'hd11f820-68a1-11e6-8047-79c0c2d9bce0',
-    },
-    {
+const instance2 = ackeeTracker.create('https://example.com', {
         ignoreLocalhost: true,
         detailed: false,
+        ignoreOwnVisits: false
     },
 );
 
-instance2.record(ackeeTracker.attributes(true));
+instance2.record('hd11f820-68a1-11e6-8047-79c0c2d9bce0', ackeeTracker.attributes(true));
