7f193268a6a64be926b8957cfdc29b0644cf4483
Chris Barth <chrisjbarth@hotmail.com>
2021-01-26 18:52:53 -0500

    Update author

--- types/jsonpath-plus/index.d.ts
+++ types/jsonpath-plus/index.d.ts
@@ -1,6 +1,6 @@
 // Type definitions for jsonpath-plus 5.0
 // Project: https://github.com/s3u/JSONPath
-// Definitions by: My Self <https://github.com/DefinitelyTyped>
+// Definitions by: Chris Barth <https://github.com/cjbarth>
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
 
 export as namespace JSONPath;
