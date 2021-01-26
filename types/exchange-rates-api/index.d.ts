65ece6690308d2d8146c437b01bc24714676de62
Marton Lederer <30638105+martonlederer@users.noreply.github.com>
2021-01-26 09:01:50 +0100

    feat: add "Minimum TypeScript Version: 3.6"

--- types/exchange-rates-api/index.d.ts
+++ types/exchange-rates-api/index.d.ts
@@ -2,6 +2,7 @@
 // Project: https://github.com/over-engineer/exchange-rates
 // Definitions by: Marton Lederer <https://github.com/martonlederer>
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
+// Minimum TypeScript Version: 3.6
 
 /**
  * Convert the given amount from one currency to another using the exchange rate of the given date
