4b7c892f6871239290a704f461e7e4d582002f32
Iva <ikopraleva@despark.com>
2021-01-27 16:16:08 +0200

    Rename types

--- types/gatsby-plugin-breakpoints/index.d.ts
+++ types/gatsby-plugin-breakpoints/index.d.ts
@@ -3,31 +3,28 @@
 // Definitions by: Iva Kop <https://github.com/IvaKop>
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
 
-export type Breakpoints = Record<string, boolean>;
-
-export type Queries = Record<string, string>;
-
-export interface BreakpointProps {
-    breakpoints: Breakpoints;
-}
-
-export interface BreakpointOptions {
-    queries?: Queries;
-}
-
 /**
  * @see https://www.gatsbyjs.com/plugins/gatsby-plugin-breakpoints/
  */
 
+export type BreakpointsObject = Record<string, boolean>;
+
+export type QueriesObject = Record<string, string>;
+export interface BreakpointProps {
+    breakpoints: BreakpointsObject;
+}
+export interface BreakpointOptions {
+    queries?: QueriesObject;
+}
 export interface BreakpointConfig {
     resolve: 'gatsby-plugin-breakpoints';
     options?: BreakpointOptions;
 }
 
-export function useBreakpoint(): Breakpoints;
+export function useBreakpoint(): BreakpointsObject;
 
 export function withBreakpoints<P extends BreakpointProps>(Component: React.ComponentType<P>): React.ComponentType<P>;
 
-export const BreakpointContext: React.Context<Queries>;
+export const BreakpointContext: React.Context<QueriesObject>;
 
-export const BreakpointProvider: React.Provider<Queries>;
+export const BreakpointProvider: React.Provider<QueriesObject>;
