21b16f457cae086f528f720ee5d25e91feca637e
Chris Barth <chrisjbarth@hotmail.com>
2021-01-27 18:07:11 -0500

    Add more types to jsonpath-plus

--- types/jsonpath-plus/index.d.ts
+++ types/jsonpath-plus/index.d.ts
@@ -4,4 +4,57 @@
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
 
 export as namespace JSONPath;
-export function JSONPath(args: { path: string; json: object }): object;
+
+type pathType = string | string[];
+type jsonType = null | boolean | number | string | object | [];
+type resultType =
+    | 'VALUE'
+    | 'Value'
+    | 'value'
+    | 'PATH'
+    | 'Path'
+    | 'path'
+    | 'POINTER'
+    | 'Pointer'
+    | 'pointer'
+    | 'PARENT'
+    | 'Parent'
+    | 'parent'
+    | 'PARENTPROPERTY'
+    | 'ParentProperty'
+    | 'parentProperty'
+    | 'parentproperty';
+type sandboxType = {
+    [key in string]: string;
+};
+type callback = (payloadValue: any, type: 'value' | 'property', fullPayload: any) => void;
+type otherCallback = (value: any, path: string, parent: object, parentPropertyName: any) => boolean;
+
+interface options {
+    path?: pathType;
+    json?: jsonType;
+    autostart?: boolean;
+    flatten?: boolean;
+    resultType?: resultType;
+    sandbox?: sandboxType;
+    wrap?: boolean;
+    preventEval?: boolean;
+    parent?: object;
+    parentProperty?: object;
+    callback?: callback;
+    otherTypeCallback?: otherCallback;
+}
+
+export function JSONPath(
+    options?: options,
+    path?: pathType,
+    json?: jsonType,
+    callback?: callback,
+    otherTypeCallback?: otherCallback,
+): object | object[];
+export function JSONPath(
+    path?: pathType,
+    json?: jsonType,
+    callback?: callback,
+    otherTypeCallback?: otherCallback,
+): object | object[];
--- types/jsonpath-plus/test/jsonpath-plus-module-tests.ts
+++ types/jsonpath-plus/test/jsonpath-plus-module-tests.ts
@@ -1,6 +1,44 @@
-import { JSONPath } from 'jsonpath-plus';
+import * as JSONPath from 'jsonpath-plus';
 
-const moduleResult = JSONPath({
-    path: "someXPath",
-    json: {},
-  });
+let path: JSONPath.pathType = '.';
+path = ['.', '//'];
+
+let json: JSONPath.jsonType = null;
+json = true;
+json = 1;
+json = 'test';
+json = { hi: 'there' };
+json = [1, 2];
+
+let result: JSONPath.resultType = 'PARENT';
+result = 'parent';
+result = 'Parent';
+
+let sandboxType: JSONPath.sandboxType = {
+    yes: 'man',
+};
+
+let callback: JSONPath.callback = (payloadValue: any, type: string, fullPayload: any): void => {};
+
+let otherCallback = (value: any, path: string, parent: object, parentPropertyName: any): boolean => {
+    return false;
+};
+
+let config: JSONPath.options = {
+    path: path,
+    json: json,
+    autostart: true,
+    flatten: true,
+    resultType: result,
+    sandbox: sandboxType,
+    wrap: true,
+    preventEval: true,
+    parent: {},
+    parentProperty: {},
+    callback: callback,
+    otherTypeCallback: otherCallback,
+};
+
+let moduleResult = JSONPath.JSONPath(config, path, json, callback, otherCallback);
+
+moduleResult = JSONPath.JSONPath(path, json);
