6b5664e1982b2b0fbbecdb2a098fc3aa3d909c7c
Dalibor Kundrat <54806834+damikun@users.noreply.github.com>
2021-01-27 10:29:22 +0100

    Remove whitespace

--- types/relay-runtime/relay-runtime-tests.tsx
+++ types/relay-runtime/relay-runtime-tests.tsx
@@ -202,7 +202,7 @@ function storeUpdaterWithTypes(store: RecordSourceSelectorProxy<SendConversation
 // Source
 // ~~~~~~~~~~~~~~~~~~~~~
 
-store.publish(source);   
+store.publish(source);
 const get_store_item = store.getSource().get("someDataId");
 const get_store_item_typed = store.getSource().get<TConversation>("someDataId");
 
