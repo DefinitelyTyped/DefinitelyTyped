b85c83af8ad1a7407a2a646e8c302f01276dfc8b
Dalibor Kundrat <54806834+damikun@users.noreply.github.com>
2021-01-27 10:03:17 +0100

    Add test for get<T> and get of store Record..

--- types/relay-runtime/relay-runtime-tests.tsx
+++ types/relay-runtime/relay-runtime-tests.tsx
@@ -203,6 +203,11 @@ function storeUpdaterWithTypes(store: RecordSourceSelectorProxy<SendConversation
 // ~~~~~~~~~~~~~~~~~~~~~
 
 store.publish(source);
+        
+const get_store_item = store.getSource().get("someDataId");
+
+const get_store_item_typed = store.getSource().get<TConversation>("someDataId");
+
 
 // ~~~~~~~~~~~~~~~~~~~~~
 // commitLocalUpdate
