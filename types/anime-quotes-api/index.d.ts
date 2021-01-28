19ea1ebfe64395aa575a7e98f089c83485c48a72
1chi <75403863+1chiSensei@users.noreply.github.com>
2021-01-28 22:57:26 +0800

    use spaces instead of tabs

--- types/anime-quotes-api/index.d.ts
+++ types/anime-quotes-api/index.d.ts
@@ -1,5 +1,5 @@
 // Type definitions for anime-quotes-api 1.2
-// Project: https://github.com/anjannair/anime-quotes-api#readme
+// Project: https://github.com/anjannair/anime-quotes-api
 // Definitions by: 1chiSensei <https://github.com/1chiSensei>
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
 
