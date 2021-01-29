a03f96e3976b06a26d0c0c6e9d387f732f1e9814
Luis Durão <luis.p.durao@hotmail.com>
2021-01-29 00:21:43 +0000

    Upgraded the type's version to the latest package version, 2.1.0 (#50898)
    Signed-off-by: TheDSCPL <luis.p.durao@hotmail.com>

--- types/react-router-transition/index.d.ts
+++ types/react-router-transition/index.d.ts
@@ -1,4 +1,4 @@
-// Type definitions for react-router-transition 2.0
+// Type definitions for react-router-transition 2.1
 // Project: https://github.com/maisano/react-router-transition, https://www.npmjs.com/package/react-router-transition
 // Definitions by: TheDSCPL <https://github.com/TheDSCPL>
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
