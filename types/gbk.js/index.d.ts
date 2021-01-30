5278e7c07ca3da640a71bed29adf4b2194dab8ac
mkchung <mk.chung@1dim.cn>
2021-01-30 11:45:56 +0800

    fixed export for gbk.js angin

--- types/gbk.js/index.d.ts
+++ types/gbk.js/index.d.ts
@@ -3,15 +3,11 @@
 // Definitions by: mkchung <https://github.com/mkchung>
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
 
-declare namespace gbkjs {
-    function encode(str: string): number[];
-    function decode(bytes: number[]): string;
-    namespace URI {
-      function encodeURI(str: string): string;
-      function decodeURI(str: string): string;
-      function encodeURIComponent(str: string): string;
-      function decodeURIComponent(str: string): string;
-    }
+export function encode(str: string): number[];
+export function decode(bytes: number[]): string;
+export namespace URI {
+    function encodeURI(str: string): string;
+    function decodeURI(str: string): string;
+    function encodeURIComponent(str: string): string;
+    function decodeURIComponent(str: string): string;
 }
-
-export = gbkjs;
