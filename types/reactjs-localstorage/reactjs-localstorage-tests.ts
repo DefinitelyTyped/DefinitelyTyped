69c763593ca30449c0918ddfb2540a4cc23bed69
Rick Wilson <me@richardawilson.com>
2021-01-30 16:46:01 -0500

    martin-badin updates

--- types/reactjs-localstorage/reactjs-localstorage-tests.ts
+++ types/reactjs-localstorage/reactjs-localstorage-tests.ts
@@ -1,10 +1,10 @@
 import { reactLocalStorage } from "reactjs-localstorage";
 
-reactLocalStorage.set("var", "test"); // $ExpectType any
-const resultKey: any = reactLocalStorage.get("var"); // $ExpectType any
+reactLocalStorage.set("var", "test"); // $ExpectType string
+const resultKey: string = reactLocalStorage.get("var"); // $ExpectType string
 
-reactLocalStorage.setObject("var", { test: "test" }); // $ExpectType any
-const resultKeyObject: any = reactLocalStorage.getObject("var"); // $ExpectType any
+reactLocalStorage.setObject("var", { test: "test" }); // $ExpectType string
+const resultKeyObject: object = reactLocalStorage.getObject("var"); // $ExpectType object
 
 reactLocalStorage.remove("var"); // $ExpectType void
 reactLocalStorage.clear(); // $ExpectType void
