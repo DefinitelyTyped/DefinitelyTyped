91c82986af5dfae9baa36d1dc3a0d02edc095ace
Josh Ellis <joshua.ellis18@gmail.com>
2021-01-30 19:49:58 +0000

    begin adding geom tests

---
+++ types/three/tests/src/geometries/ConeGeometry.ts
@@ -0,0 +1,32 @@
+import { CylinderGeometry, CylinderParameters } from './CylinderGeometry.js';
+
+class ConeGeometry extends CylinderGeometry {
+    parameters: CylinderParameters & {
+        radius: number;
+    };
+    constructor(
+        radius = 1,
+        height = 1,
+        radialSegments = 8,
+        heightSegments = 1,
+        openEnded = false,
+        thetaStart = 0,
+        thetaLength = Math.PI * 2,
+    ) {
+        super(0, radius, height, radialSegments, heightSegments, openEnded, thetaStart, thetaLength);
+
+        this.type = 'ConeGeometry';
+
+        this.parameters = {
+            radius: radius,
+            height: height,
+            radialSegments: radialSegments,
+            heightSegments: heightSegments,
+            openEnded: openEnded,
+            thetaStart: thetaStart,
+            thetaLength: thetaLength,
+        };
+    }
+}
+
+export { ConeGeometry, ConeGeometry as ConeBufferGeometry };
