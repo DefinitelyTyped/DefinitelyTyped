7796fd78445bd51022902fac909b889122aea8f3
shekari <me@shekari-ah.ir>
2021-01-31 10:32:00 +0330

    Add types for react-native-sha1

--- types/react-native-sha1/index.d.ts
+++ types/react-native-sha1/index.d.ts
@@ -1,5 +1,6 @@
-// Definitions by: Amir Hossein Shekari <https://github.com/shekari-ah>
+// Type definitions for react-native-sha1 1.2
+// Project: https://github.com/itinance/react-native-sha1
+// Definitions by: shekari <https://github.com/shekari-ah>
+// Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
 
-
-
-export function sha1(input: string): Promise<string>;
+export function sha1(data: string): Promise<string>;
--- types/react-native-sha1/react-native-sha1-test.ts
+++
@@ -1,5 +0,0 @@
-import {sha1} from 'react-native-sha1';
-
-sha1("").then((hash) => {
-    // Do something
-});
