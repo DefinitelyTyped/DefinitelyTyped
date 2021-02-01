18db419d9b36766d56582bc650d4cdb99de9330f
Alex Chaniotis <achaniotis@atlassian.com>
2021-01-31 23:03:28 -0600

    Update version number in header

--- types/pino-http/index.d.ts
+++ types/pino-http/index.d.ts
@@ -1,4 +1,4 @@
-// Type definitions for pino-http 5.0
+// Type definitions for pino-http 5.4
 // Project: https://github.com/pinojs/pino-http#readme
 // Definitions by: Christian Rackerseder <https://github.com/screendriver>
 //                 Jeremy Forsythe <https://github.com/jdforsythe>
