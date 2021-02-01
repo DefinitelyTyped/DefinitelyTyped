55e45e2ca2c34b40b665a77d76fa9f3b8dbd7cb8
Ashes <792634614@qq.com>
2021-02-01 18:45:57 +0800

    Fix test of CL

--- types/echarts/echarts-tests.ts
+++ types/echarts/echarts-tests.ts
@@ -24,10 +24,11 @@ seriesGraph.autoCurveness;
 seriesGraph.autoCurveness = 10;
 seriesGraph.autoCurveness = [1, 2, 3, 4];
 // $ExpectError
-seriesGraph.autoCurveness = 'error';
+seriesGraph.autoCurveness = 'error'; // prettier-ignore
 
 // enumerate render item return type
-type renderItemReturnType = echarts.EChartOption.SeriesCustom.RenderItemReturnGroup
+type RenderItemReturnType =
+    | echarts.EChartOption.SeriesCustom.RenderItemReturnGroup
     | echarts.EChartOption.SeriesCustom.RenderItemReturnPath
     | echarts.EChartOption.SeriesCustom.RenderItemReturnImage
     | echarts.EChartOption.SeriesCustom.RenderItemReturnText
@@ -39,86 +40,107 @@ type renderItemReturnType = echarts.EChartOption.SeriesCustom.RenderItemReturnGr
     | echarts.EChartOption.SeriesCustom.RenderItemReturnPolygon
     | echarts.EChartOption.SeriesCustom.RenderItemReturnPolyline
     | echarts.EChartOption.SeriesCustom.RenderItemReturnLine
-    | echarts.EChartOption.SeriesCustom.RenderItemReturnBezierCurve
+    | echarts.EChartOption.SeriesCustom.RenderItemReturnBezierCurve;
 
-
-// test the property `context` of renderItem parameter `param` dynamic type 
-const renderItem: echarts.EChartOption.SeriesCustom.RenderItem = (param) => {
+// test the property `context` of renderItem parameter `param` dynamic type
+const renderItem: echarts.EChartOption.SeriesCustom.RenderItem = param => {
     const { context } = param;
 
-    type CacheObjType = { customKey: number; custAccessKey: string; };
+    type CacheObjType = { customKey: number; custAccessKey: string };
 
-    const cacheObj:CacheObjType = {
+    const cacheObj: CacheObjType = {
         customKey: 1,
-        custAccessKey: 'diff CustomParams'
+        custAccessKey: 'diff CustomParams',
     };
 
     // prettier-ignore
-    context.customKey = 'error';    // $ExpectError
-
-    
-
+    context.customKey = 'error'; // $ExpectError
     if (context) {
         // $ExpectType { [key: string]: any; } & CacheObjType
-        Object.assign(context,cacheObj)
-    }else{
+        Object.assign(context, cacheObj);
+    } else {
         // $ExpectType undefined
-        context
+        context;
     }
-
-    const result: renderItemReturnType = {};
-    //$ExpectType 'image' | 'text' | 'circle' | 'sector' | 'ring' | 'polygon' | 'polyline' | 'rect' | 'line' | 'bezierCurve' | 'arc' | 'group'
+    const result: RenderItemReturnType = {};
+    // $ExpectType "image"
     result.type = 'image';
+    // $ExpectType "text"
     result.type = 'text';
+    // $ExpectType "circle"
     result.type = 'circle';
+    // $ExpectType "sector"
     result.type = 'sector';
+    // $ExpectType "ring"
     result.type = 'ring';
+    // $ExpectType "polygon"
     result.type = 'polygon';
+    // $ExpectType "polyline"
     result.type = 'polyline';
+    // $ExpectType "rect"
     result.type = 'rect';
+    // $ExpectType "line"
     result.type = 'line';
+    // $ExpectType "bezierCurve"
     result.type = 'bezierCurve';
+    // $ExpectType "arc"
     result.type = 'arc';
+    // $ExpectType "group"
     result.type = 'group';
+    // $ExpectError
+    result.type = 'error'; // prettier-ignore
     return result;
-}
+};
 
 type CustomParams = {
     customKey: string;
     customErrorKey?: number;
-}
+};
 
-// test the key `context` of renderItem parameter `param` dynamic type 
-const renderItemWithTypeParams: echarts.EChartOption.SeriesCustom.RenderItem<CustomParams> = (param) => {
+// test the key `context` of renderItem parameter `param` dynamic type
+const renderItemWithTypeParams: echarts.EChartOption.SeriesCustom.RenderItem<CustomParams> = param => {
     const { context } = param;
 
     // $ExpectError
-    context.customKey = 'error';
-
-
+    context.customKey = 'error'; // prettier-ignore
 
     if (context) {
+        const contextParams: CustomParams = {
+            customKey: 'cache content',
+        };
+
         // $ExpectType CustomParams
-        Object.assign(context,{
-            customKey: 'cache content'
-        } as CustomParams)
+        Object.assign(context, contextParams);
         // $ExpectError
-        context.customErrorKey = ''
+        context.customErrorKey = '' // prettier-ignore
     }
 
-    const result: renderItemReturnType = {};
-    //$ExpectType 'image' | 'text' | 'circle' | 'sector' | 'ring' | 'polygon' | 'polyline' | 'rect' | 'line' | 'bezierCurve' | 'arc' | 'group'
+    const result: RenderItemReturnType = {};
+    // $ExpectType "image"
     result.type = 'image';
+    // $ExpectType "text"
     result.type = 'text';
+    // $ExpectType "circle"
     result.type = 'circle';
+    // $ExpectType "sector"
     result.type = 'sector';
+    // $ExpectType "ring"
     result.type = 'ring';
+    // $ExpectType "polygon"
     result.type = 'polygon';
+    // $ExpectType "polyline"
     result.type = 'polyline';
+    // $ExpectType "rect"
     result.type = 'rect';
+    // $ExpectType "line"
     result.type = 'line';
+    // $ExpectType "bezierCurve"
     result.type = 'bezierCurve';
+    // $ExpectType "arc"
     result.type = 'arc';
+    // $ExpectType "group"
     result.type = 'group';
+    // $ExpectError
+    result.type = 'error'; // prettier-ignore
     return result;
-} 
+};
