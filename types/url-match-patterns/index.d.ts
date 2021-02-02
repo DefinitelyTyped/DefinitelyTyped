9387f038b2d6320a968a176f482b4a575047837e
lionel-rowe <lionel.rowe@gmail.com>
2021-02-02 21:47:05 +0000

    Fix no-single-declare-module linting error

--- types/url-match-patterns/index.d.ts
+++ types/url-match-patterns/index.d.ts
@@ -3,18 +3,15 @@
 // Definitions by: Lionel Rowe <https://github.com/lionel-rowe>
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
 
-// tslint:disable-next-line:no-single-declare-module no-declare-current-package
-declare module 'url-match-patterns' {
-    /**
-     * Checks if a pattern matches against a URL
-     *
-     * @param pattern The pattern to match against
-     * @param url The URL to match
-     * @returns Whether the URL matched the pattern
-     * @throws {AssertionError} Invalid pattern
-     */
-    function match(pattern: string): (url: string) => boolean;
-    function match(pattern: string, url: string): boolean;
+/**
+ * Checks if a pattern matches against a URL
+ *
+ * @param pattern The pattern to match against
+ * @param url The URL to match
+ * @returns Whether the URL matched the pattern
+ * @throws {AssertionError} Invalid pattern
+ */
+declare function match(pattern: string): (url: string) => boolean;
+declare function match(pattern: string, url: string): boolean;
 
-    export default match;
-}
+export default match;
