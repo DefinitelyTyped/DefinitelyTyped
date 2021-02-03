7d3f572df4dd972f4a72a00156af24fe0c61c173
Nathan <regdit47@gmail.com>
2021-02-04 00:07:29 +0100

    import and syntax fixes

--- types/rsmq-promise-native/index.d.ts
+++ types/rsmq-promise-native/index.d.ts
@@ -1,39 +1,37 @@
-// Type definitions for rsmq-promise-native 0.0.4
-// Project: https://github.com/smrchy/rsmq
+// Type definitions for rsmq-promise-native 0.0
+// Project: https://github.com/scottsloane/rsmq-promise-native
 // Definitions by: Nathan Němec <https://github.com/dubblen>
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
 // TypeScript Version: 3.2
 
-/// <reference types="rsmq-promise-native" />
+import RedisSMQ = require("rsmq");
 
-declare module 'rsmq-promise-native' {
-    import RedisSMQ from "rsmq";
+declare class RedisSMQPromise {
+    rsmq: RedisSMQ;
 
-    export default class RedisSMQPromise {
-        rsmq : RedisSMQ;
+    constructor(props: RedisSMQ.ConstructorOptions);
 
-        constructor(props: RedisSMQ.ConstructorOptions);
+    listQueues(): Promise<string[]>;
 
-        listQueues() : Promise<string[]>;
+    changeMessageVisibility(opts: RedisSMQ.ChangeMessageVisibilityOptions): Promise<(1 | 0)>;
 
-        changeMessageVisibility(opts : RedisSMQ.ChangeMessageVisibilityOptions) : Promise<(1 | 0)>;
+    createQueue(opts: RedisSMQ.CreateQueueOptions): Promise<1>;
 
-        createQueue(opts : RedisSMQ.CreateQueueOptions) : Promise<1>;
+    setQueueAttributes(opts: RedisSMQ.SetQueueAttributesOptions): Promise<RedisSMQ.QueueAttributes>;
 
-        setQueueAttributes(opts: RedisSMQ.SetQueueAttributesOptions) : Promise<RedisSMQ.QueueAttributes>;
+    getQueueAttributes(opts: RedisSMQ.GetQueueAttributesOptions): Promise<RedisSMQ.QueueAttributes>;
 
-        getQueueAttributes(opts: RedisSMQ.GetQueueAttributesOptions) : Promise<RedisSMQ.QueueAttributes>;
+    deleteQueue(opts: RedisSMQ.DeleteQueueOptions): Promise<1>;
 
-        deleteQueue(opts: RedisSMQ.DeleteQueueOptions) : Promise<1>;
+    sendMessage(opts: RedisSMQ.SendMessageOptions): Promise<string>;
 
-        sendMessage(opts: RedisSMQ.SendMessageOptions) : Promise<string>;
+    receiveMessage(opts: RedisSMQ.ReceiveMessageOptions): Promise<(RedisSMQ.QueueMessage | {})>;
 
-        receiveMessage(opts: RedisSMQ.ReceiveMessageOptions) : Promise<(RedisSMQ.QueueMessage | {})>;
+    deleteMessage(opts: RedisSMQ.DeleteMessageOptions): Promise<(0 | 1)>;
 
-        deleteMessage(opts: RedisSMQ.DeleteMessageOptions) : Promise<(0 | 1)>;
+    popMessage(opts: RedisSMQ.PopMessageOptions): Promise<(RedisSMQ.QueueMessage | {})>;
 
-        popMessage(opts: RedisSMQ.PopMessageOptions) : Promise<(RedisSMQ.QueueMessage | {})>;
-
-        quit() : Promise<void>;
-    }
+    quit(): Promise<void>;
 }
+
+export = RedisSMQPromise;
