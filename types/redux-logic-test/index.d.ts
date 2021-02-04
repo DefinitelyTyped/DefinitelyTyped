16d6863ca3927e9983a45198f05f220559917a3c
Felipe Plets <felipe.plets@zensurance.com>
2021-02-04 17:37:58 -0500

    chore: fix definition tests

--- types/redux-logic-test/index.d.ts
+++ types/redux-logic-test/index.d.ts
@@ -1,44 +1,35 @@
-// Type definitions for redux-logic-test 2.0.0
+// Type definitions for redux-logic-test 2.0
 // Project: https://github.com/jeffbski/redux-logic-test
 // Definitions by: Felipe Plets <https://github.com/felipeplets>
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
 
-declare module 'redux-logic-test' {
-    import { Middleware, Reducer, Store } from 'redux';
+import { Middleware, Reducer, Store } from 'redux';
 
-    import { Action, Logic, LogicMiddleware } from 'redux-logic';
+import { Action, Logic, LogicMiddleware } from 'redux-logic';
 
-    type Callback = () => void;
+export interface MockStore<State extends object, Actions extends Action> extends Store<State, Actions> {
+    resetActions(): void;
+    whenComplete(callback: () => void): ReturnType<LogicMiddleware['whenComplete']>;
 
-    export interface MockStore<State extends object, Actions extends Action> extends Store<State, Actions> {
-        resetActions(): void;
-        whenComplete(callback: Callback): ReturnType<LogicMiddleware['whenComplete']>;
-
-        actions: {
-            enumerable: true;
-            get(): Actions[];
-        };
-        logicMiddleware: LogicMiddleware;
-    }
-
-    export interface CreateMockStoreOptions<State extends object, Dependency extends object> {
-        initialState?: State;
-        reducer?: Reducer;
-        logic?: Logic<State, Dependency>[];
-        injectedDeps?: Dependency;
-        middleware?: Middleware[];
-    }
-
-    export interface CreateMockStore {
-        <State extends object, Dependency extends object, Actions extends Action>(
-            options?: CreateMockStoreOptions<State, Dependency>,
-        ): MockStore<State, Actions>;
-
-        <State extends object, Actions extends Action>(options?: CreateMockStoreOptions<State, {}>): MockStore<
-            State,
-            Actions
-        >;
-    }
-
-    export const createMockStore: CreateMockStore;
+    actions: {
+        enumerable: true;
+        get(): Actions[];
+    };
+    logicMiddleware: LogicMiddleware;
 }
+
+export interface CreateMockStoreOptions<State extends object, Dependency extends object> {
+    initialState?: State;
+    reducer?: Reducer;
+    logic?: Array<Logic<State, Dependency>>;
+    injectedDeps?: Dependency;
+    middleware?: Middleware[];
+}
+
+export interface CreateMockStore {
+    <State extends object>(
+        options?: CreateMockStoreOptions<State, object> | CreateMockStoreOptions<State, {}>,
+    ): MockStore<State, Action>;
+}
+
+export const createMockStore: CreateMockStore;
