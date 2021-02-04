3950eea46e269c651d55ceb72b354365e02dbe91
tpluscode <tpluscode@users.noreply.github.com>
2021-02-04 08:10:06 +0100

    feat(nanoid-dict): added alphanumeric dictionary

--- types/nanoid-dictionary/index.d.ts
+++ types/nanoid-dictionary/index.d.ts
@@ -9,13 +9,15 @@ import uppercase = require('./uppercase');
 import numbers = require('./numbers');
 import nolookalikes = require('./nolookalikes');
 import nolookalikesSafe = require('./nolookalikes-safe');
+import alphanumeric = require('./alphanumeric');
 
 declare const nanoidDictionary: {
     lowercase: typeof lowercase,
     uppercase: typeof uppercase,
     numbers: typeof numbers,
     nolookalikes: typeof nolookalikes,
-    nolookalikesSafe: typeof nolookalikesSafe
+    nolookalikesSafe: typeof nolookalikesSafe,
+    alphanumeric: typeof alphanumeric
 };
 
 export = nanoidDictionary;
