3629fec4712b649973e54cfb6c8533eed97d732b
Marton <marton@lederer.hu>
2021-02-04 12:51:39 +0100

    chore: weaveid declarations

---
+++ types/weaveid/index.d.ts
@@ -0,0 +1,25 @@
+// Type definitions for use-color-scheme 1.0
+// Project: https://github.com/instawallet/weaveid-js
+// Definitions by: Marton Lederer <https://github.com/martonlederer>
+// Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
+
+import { JWKInterface } from "arweave";
+
+declare global {
+  interface Window {
+    closeLoginModal: typeof closeModal;
+    openLoginModal: typeof openLoginModal;
+    getAddress: typeof getAddress;
+    getWallet: typeof getWallet;
+    WeaveID: any;
+    init: typeof init;
+  }
+}
+
+export function closeModal(): void;
+export function openLoginModal(e?: Event): Promise<string>;
+export function getAddress(): Promise<string>;
+export function getWallet(): Promise<JWKInterface>;
+export function init(): void;
+
+export {};
