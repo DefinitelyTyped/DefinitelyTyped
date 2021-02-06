17704eb1691a9e63034ad3f6c0f6b51e9cf4c323
Nathan <regdit47@gmail.com>
2021-02-06 01:59:42 +0100

    fix tests syntax

--- types/rsmq-promise-native/rsmq-promise-native-tests.ts
+++ types/rsmq-promise-native/rsmq-promise-native-tests.ts
@@ -4,26 +4,36 @@ const rsmq = new RedisSMQPromise({host: "127.0.0.1", port: 7001, ns: "rsmq"});
 
 rsmq.createQueue({qname: 'myqueue'})
     .then(done => {
-    	console.log('Queue created!');
-
 		rsmq.sendMessage({ qname: 'myqueue', message: 'my message!' })
-		    .then(result => console.log("Message sent. ID:", result))
-		    .catch(err => console.log(err));
+		    .then(result => result)
+		    .catch(err => {
+		        throw err;
+		    });
 
 		rsmq.receiveMessage({qname: 'myqueue'})
-		    .then(message => console.log(message))
-		    .catch(err => console.log(err));
+		    .then(message => message)
+		    .catch(err => {
+		        throw err;
+	        });
 
 		rsmq.deleteMessage({ qname: 'myqueue', id: 'dhoiwpiirm15ce77305a5c3a3b0f230c6e20f09b55'})
-		    .then(result => console.log("Message deleted."))
-		    .catch(err => console.log("Message not found."));
+		    .then(result => "Message deleted.")
+		    .catch(err => {
+		        throw err;
+	        });
 
 		rsmq.listQueues()
-		    .then(queues => console.log(queues))
-		    .catch(err => console.log(err));
+		    .then(queues => queues)
+		    .catch(err => {
+		        throw err;
+	        });
 
 		rsmq.quit()
-		    .then(success => console.log(success))
-		    .catch(err => console.log(err));
+		    .then(success => success)
+		    .catch(err => {
+		    	throw err;
+	    	});
    	})
-    .catch(err => console.log(err));
+    .catch(err => {
+        throw err;
+    });
