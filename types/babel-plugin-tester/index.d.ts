b4443fbc6b5b478fa4d34e5aa90f9bef93907248
Mathieu TUDISCO <oss@mathieutu.dev>
2021-02-07 18:19:37 +0100

    [babel-plugin-tester] Add missing pluginOptions

--- types/babel-plugin-tester/index.d.ts
+++ types/babel-plugin-tester/index.d.ts
@@ -1,6 +1,7 @@
 // Type definitions for babel-plugin-tester 9.0
 // Project: https://github.com/babel-utils/babel-plugin-tester#readme
 // Definitions by: Ifiok Jr. <https://github.com/ifiokjr>
+//                 Mathieu TUDISCO <https://github.com/mathieutu>
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
 // Minimum TypeScript Version: 3.4
 
@@ -152,6 +153,12 @@ export interface TestObject {
      *
      */
     babelOptions?: Babel.TransformOptions;
+    
+    /**
+     * This can be used to pass options into your plugin at transform time.
+     *
+     */
+    pluginOptions?: Babel.PluginOptions;
 }
 
 export type Omit<T, K extends keyof any> = Pick<T, Exclude<keyof T, K>>;
