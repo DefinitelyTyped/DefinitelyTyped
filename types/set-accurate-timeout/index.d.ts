5ba895fa13e805b7692dcc52355cf7262471bf9d
Priyanshu Rav <77456300+pr357@users.noreply.github.com>
2021-02-07 14:03:56 +0530

    Added type definitions for set-accurate-timeout

---
+++ types/set-accurate-timeout/index.d.ts
@@ -0,0 +1,8 @@
+// Type definitions for set-accurate-timeout 1.0
+// Project: https://gitlab.com/notacircle/set-accurate-timeout
+// Definitions by: Priyanshu Rav <https://github.com/pr357>
+// Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
+
+declare function setAccurateTimeout(callback: () => void, delay: number): void;
+
+export = setAccurateTimeout;
