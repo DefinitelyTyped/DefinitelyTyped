7c9d82ffc9dc87529829c920f95546901055cd42
Mikael COUZIC <mcouzic@solocal.com>
2021-02-08 16:56:45 +0100

    [ramda] fallback type for concat()

--- types/ramda/index.d.ts
+++ types/ramda/index.d.ts
@@ -375,6 +375,8 @@ export function concat<L1 extends any[]>(list1: L1): <L2 extends any[]>(list2: L
 export function concat<S1 extends string>(s1: S1): <S2 extends string>(s2: S2) => `${S1}${S2}`;
 export function concat<L1 extends any[], L2 extends any[]>(list1: L1, list2: L2): [...L1, ...L2];
 export function concat<S1 extends string, S2 extends string>(s1: S1, s2: S2): `${S1}${S2}`;
+export function concat(s1: string, s2: string): string;
+export function concat(s1: string): (s2: string) => string;
 
 /**
  * Returns a function, fn, which encapsulates if/else-if/else logic. R.cond takes a list of [predicate, transform] pairs.
