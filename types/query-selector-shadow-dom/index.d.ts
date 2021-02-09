1d6b6b0bc6b584feeb99fd8cf8c4dc29b512f2d3
David Alvarez Restrepo <dalvarezrestrepo@openwt.com>
2021-02-09 14:41:13 +0100

    Corrected headers

--- types/query-selector-shadow-dom/index.d.ts
+++ types/query-selector-shadow-dom/index.d.ts
@@ -1,4 +1,6 @@
+// Type definitions for query-selector-shadow-dom 1.0
+// Project: https://github.com/Georgegriff/query-selector-shadow-dom#readme
+// Definitions by: David Alvarez Restrepo <https://github.com/davidalvarezr>
+// Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
 export function querySelectorDeep(selector: string): any;
 export function querySelectorAllDeep(selector: string): any[];
-
-// Definitions by: David Alvarez Restrepo <https://github.com/davidalvarezr>
