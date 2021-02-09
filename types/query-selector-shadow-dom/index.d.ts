d852a9b501090cca29c06992a676d31af5b3be0f
David Alvarez Restrepo <dalvarezrestrepo@openwt.com>
2021-02-09 18:17:38 +0100

    Add second param in type definition (root)

--- types/query-selector-shadow-dom/index.d.ts
+++ types/query-selector-shadow-dom/index.d.ts
@@ -7,6 +7,7 @@
  * Finds first matching elements on the page that may be in a shadow root using a complex selector of n-depth
  * Don't have to specify all shadow roots to button, tree is travered to find the correct element
  * @param selector Simple selector to filter the elements by. e.g. 'a', 'div.main'
+ * @param root The root from which we apply the selector (default: document)
  * @returns the HTMLElement found or null if not found
  * @example
  * ```ts
@@ -15,16 +16,17 @@
  * ```
  * @license Apache-2.0
  */
-export function querySelectorDeep(selector: string): HTMLElement | null;
+export function querySelectorDeep(selector: string, root?: HTMLElement): HTMLElement | null;
 
 /**
  * Finds first matching elements on the page that may be in a shadow root using a complex selector of n-depth
  * Don't have to specify all shadow roots to button, tree is travered to find the correct element
  * @param selector Simple selector to filter the elements by. e.g. 'a', 'div.main'
+ * @param root The root from which we apply the selector (default: document)
  * @returns the HTMLElement's found or an empty array if none found
  * @license Apache-2.0
  */
-export function querySelectorAllDeep(selector: string): HTMLElement[];
+export function querySelectorAllDeep(selector: string, root?: HTMLElement): HTMLElement[];
 
 /**
  * Finds all elements on the page, inclusive of those within shadow roots
