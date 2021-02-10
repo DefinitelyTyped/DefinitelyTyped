3e9b5adbd7e83b915a2c631e17241192f0090339
Priyanshu Rav <77456300+pr357@users.noreply.github.com>
2021-02-10 19:10:32 +0530

    Added infinite arguments to callback type

--- types/set-accurate-timeout/index.d.ts
+++ types/set-accurate-timeout/index.d.ts
@@ -7,6 +7,6 @@
  * @param callback This callback is called when the specified delay is passed
  * @param delay The time (in milliseconds) to wait, before calling the callback
  */
-declare function setAccurateTimeout(callback: () => void, delay: number): void;
+declare function setAccurateTimeout(callback: (...args: any[]) => void, delay: number): void;
 
 export = setAccurateTimeout;
