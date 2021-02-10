89574af6aad86a0eb1f691ba6bd1e98ee08945fa
David Alvarez Restrepo <dalvarezrestrepo@openwt.com>
2021-02-10 09:54:55 +0100

    Now 100% compatible with JS lib

--- types/query-selector-shadow-dom/index.d.ts
+++ types/query-selector-shadow-dom/index.d.ts
@@ -8,31 +8,34 @@
  * Don't have to specify all shadow roots to button, tree is travered to find the correct element
  * @param selector Simple selector to filter the elements by. e.g. 'a', 'div.main'
  * @param root The root from which we apply the selector (default: document)
+ * @param cachedElements cached elements (default: null)
  * @returns the HTMLElement found or null if not found
  * @example
  * ```ts
  * // find first active download link element
- * querySelectorDeep('#downloads-list .is-active a[href^="https://"]');
+ * const firstActiveDownload = querySelectorDeep('#downloads-list .is-active a[href^="https://"]');
  * ```
  * @license Apache-2.0
  */
-export function querySelectorDeep(selector: string, root?: HTMLElement): HTMLElement | null;
+export function querySelectorDeep(selector: string, root?: Document | HTMLElement, cachedElements?: HTMLElement[] | null): HTMLElement | null;
 
 /**
  * Finds first matching elements on the page that may be in a shadow root using a complex selector of n-depth
  * Don't have to specify all shadow roots to button, tree is travered to find the correct element
  * @param selector Simple selector to filter the elements by. e.g. 'a', 'div.main'
  * @param root The root from which we apply the selector (default: document)
+ * @param cachedElements cached elements (default: null)
  * @returns the HTMLElement's found or an empty array if none found
  * @license Apache-2.0
  */
-export function querySelectorAllDeep(selector: string, root?: HTMLElement): HTMLElement[];
+export function querySelectorAllDeep(selector: string, root?: Document | HTMLElement, cachedElements?: HTMLElement[] | null): HTMLElement[];
 
 /**
  * Finds all elements on the page, inclusive of those within shadow roots
- * @param selector must be null
+ * @param selector
  * @param root must be document
+ * @param cachedElements cached elements (default: null)
  * @returns all elements on the page, inclusive of those within shadow roots.
  * @license Apache-2.0
  */
-export function collectAllElementsDeep(selector: null, root: Document): HTMLHtmlElement;
+export function collectAllElementsDeep(selector: string | null, root: Document | HTMLElement, cachedElements?: HTMLElement[] | null): HTMLHtmlElement[];
