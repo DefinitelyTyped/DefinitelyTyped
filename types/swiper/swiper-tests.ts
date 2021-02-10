e6b603ad5a6f3b2d4842f1d5b6f92b99b73b64c3
Daniel Hritzkiv <daniel.hritzkiv@gmail.com>
2021-02-10 16:00:53 -0500

    Update swiper-tests.ts
    Whitespace fix

--- types/swiper/swiper-tests.ts
+++ types/swiper/swiper-tests.ts
@@ -788,7 +788,7 @@ function slideableNavigation() {
                 }
             },
             lazyImageReady: (slideEl, imageEl) => {
-                // 
+                //
             }
         }
     });
