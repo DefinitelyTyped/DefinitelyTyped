d827523b19940a3aa693dd979566c122dac64c41
Sergei Serdyuk <sergei@redleafsoft.com>
2021-02-11 09:20:00 -0500

    Updated Oboe version number

--- types/oboe/index.d.ts
+++ types/oboe/index.d.ts
@@ -1,4 +1,4 @@
-// Type definitions for oboe v2.0.3
+// Type definitions for oboe v2.1.4
 // Project: https://github.com/jimhigson/oboe.js
 // Definitions by: Jared Klopper <https://github.com/optical>
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
