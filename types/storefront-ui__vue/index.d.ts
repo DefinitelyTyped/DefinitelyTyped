bf915f0f1403476295e92adf6e1e85e21d704312
Luïs De Zutter <luzedz@gmail.com>
2021-02-11 14:29:31 +0100

    test(storefront-ui): Add type checking tests
    Add a vue component to test types on the storefront-ui module

---
+++ types/storefront-ui__vue/index.d.ts
@@ -0,0 +1,4 @@
+// Type definitions for @storefront-ui 0.7.11
+// Project: https://github.com/vuestorefront/storefront-ui
+// Definitions by: Luïs De Zutter <https://github.com/ImLuze>
+// Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
---
+++ types/storefront-ui__vue/storefront-ui__vue-tests.ts
@@ -0,0 +1,227 @@
+import Vue from 'vue';
+import {
+    SfArrow,
+    SfBadge,
+    SfBreadcrumbs,
+    SfBullets,
+    SfButton,
+    SfChevron,
+    SfCimage,
+    SfCircleIcon,
+    SfColor,
+    SfDivider,
+    SfHeading,
+    SfIcon,
+    SfImage,
+    SfInput,
+    SfLink,
+    SfLoader,
+    SfOverlay,
+    SfPrice,
+    SfProperty,
+    SfQuantitySelector,
+    SfRating,
+    SfTextarea,
+    SfAddToCart,
+    SfAddressPicker,
+    SfAlert,
+    SfBanner,
+    SfBar,
+    SfBottomModal,
+    SfCallToAction,
+    SfCard,
+    SfCategoryCard,
+    SfCharacteristic,
+    SfCheckbox,
+    SfColorPicker,
+    SfComponentSelect,
+    SfDropdown,
+    SfFilter,
+    SfGallery,
+    SfMenuItem,
+    SfModal,
+    SfNotification,
+    SfPagination,
+    SfProductOption,
+    SfRadio,
+    SfReview,
+    SfScrollable,
+    SfSearchBar,
+    SfSection,
+    SfSelect,
+    SfSlidingSection,
+    SfSteps,
+    SfSticky,
+    SfTile,
+    SfAccordion,
+    SfBannerGrid,
+    SfBottomNavigation,
+    SfCarousel,
+    SfCollectedProduct,
+    SfContentPages,
+    SfFooter,
+    SfGroupedProduct,
+    SfHeader,
+    SfHero,
+    SfList,
+    SfMegaMenu,
+    SfProductCard,
+    SfProductCardHorizontal,
+    SfSidebar,
+    SfStoreLocator,
+    SfTable,
+    SfTabs,
+    SfTopBar
+} from '@storefront-ui/vue';
+
+new Vue({
+    el: '#app',
+    components: {
+        SfArrow,
+        SfBadge,
+        SfBreadcrumbs,
+        SfBullets,
+        SfButton,
+        SfChevron,
+        SfCimage,
+        SfCircleIcon,
+        SfColor,
+        SfDivider,
+        SfHeading,
+        SfIcon,
+        SfImage,
+        SfInput,
+        SfLink,
+        SfLoader,
+        SfOverlay,
+        SfPrice,
+        SfProperty,
+        SfQuantitySelector,
+        SfRating,
+        SfTextarea,
+        SfAddToCart,
+        SfAddressPicker,
+        SfAlert,
+        SfBanner,
+        SfBar,
+        SfBottomModal,
+        SfCallToAction,
+        SfCard,
+        SfCategoryCard,
+        SfCharacteristic,
+        SfCheckbox,
+        SfColorPicker,
+        SfComponentSelect,
+        SfDropdown,
+        SfFilter,
+        SfGallery,
+        SfMenuItem,
+        SfModal,
+        SfNotification,
+        SfPagination,
+        SfProductOption,
+        SfRadio,
+        SfReview,
+        SfScrollable,
+        SfSearchBar,
+        SfSection,
+        SfSelect,
+        SfSlidingSection,
+        SfSteps,
+        SfSticky,
+        SfTile,
+        SfAccordion,
+        SfBannerGrid,
+        SfBottomNavigation,
+        SfCarousel,
+        SfCollectedProduct,
+        SfContentPages,
+        SfFooter,
+        SfGroupedProduct,
+        SfHeader,
+        SfHero,
+        SfList,
+        SfMegaMenu,
+        SfProductCard,
+        SfProductCardHorizontal,
+        SfSidebar,
+        SfStoreLocator,
+        SfTable,
+        SfTabs,
+        SfTopBar
+    },
+    template: `
+    <SfArrow />
+    <SfBadge />
+    <SfBreadcrumbs />
+    <SfBullets />
+    <SfButton />
+    <SfChevron />
+    <SfCimage />
+    <SfCircleIcon />
+    <SfColor />
+    <SfDivider />
+    <SfHeading />
+    <SfIcon />
+    <SfImage />
+    <SfInput />
+    <SfLink />
+    <SfLoader />
+    <SfOverlay />
+    <SfPrice />
+    <SfProperty />
+    <SfQuantitySelector />
+    <SfRating />
+    <SfTextarea />
+    <SfAddToCart />
+    <SfAddressPicker />
+    <SfAlert />
+    <SfBanner />
+    <SfBar />
+    <SfBottomModal />
+    <SfCallToAction />
+    <SfCard />
+    <SfCategoryCard />
+    <SfCharacteristic />
+    <SfCheckbox />
+    <SfColorPicker />
+    <SfComponentSelect />
+    <SfDropdown />
+    <SfFilter />
+    <SfGallery />
+    <SfMenuItem />
+    <SfModal />
+    <SfNotification />
+    <SfPagination />
+    <SfProductOption />
+    <SfRadio />
+    <SfReview />
+    <SfScrollable />
+    <SfSearchBar />
+    <SfSection />
+    <SfSelect />
+    <SfSlidingSection />
+    <SfSteps />
+    <SfSticky />
+    <SfTile />
+    <SfAccordion />
+    <SfBannerGrid />
+    <SfBottomNavigation />
+    <SfCarousel />
+    <SfCollectedProduct />
+    <SfContentPages />
+    <SfFooter />
+    <SfGroupedProduct />
+    <SfHeader />
+    <SfHero />
+    <SfList />
+    <SfMegaMenu />
+    <SfProductCard />
+    <SfProductCardHorizontal />
+    <SfSidebar />
+    <SfStoreLocator />
+    <SfTable />
+    <SfTabs />
+    <SfTopBar />
+`
+});
