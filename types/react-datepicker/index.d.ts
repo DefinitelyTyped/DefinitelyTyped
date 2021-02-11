83d81b3eb5d395fb65b0abf1af9f4878bdad90cd
pinn3 <iam@lacking.education>
2021-02-11 03:06:39 +0100

    Format react-datepicker with prettier

--- types/react-datepicker/index.d.ts
+++ types/react-datepicker/index.d.ts
@@ -24,7 +24,12 @@ import { Locale } from 'date-fns';
 export function registerLocale(localeName: string, localeData: {}): void;
 export function setDefaultLocale(localeName: string): void;
 export function getDefaultLocale(): string;
-export function CalendarContainer(props: { className?: string, children?: React.ReactNode, showPopperArrow?: boolean, arrowProps?: { [propName: string]: any } }): React.ReactElement;
+export function CalendarContainer(props: {
+    className?: string;
+    children?: React.ReactNode;
+    showPopperArrow?: boolean;
+    arrowProps?: { [propName: string]: any };
+}): React.ReactElement;
 
 interface HighlightDates {
     [className: string]: Date[];
@@ -85,7 +90,10 @@ export interface ReactDatePickerProps {
     onBlur?(event: React.FocusEvent<HTMLInputElement>): void;
     onCalendarClose?(): void;
     onCalendarOpen?(): void;
-    onChange(date: Date | [Date, Date] /* for selectsRange */ | null, event: React.SyntheticEvent<any> | undefined): void;
+    onChange(
+        date: Date | [Date, Date] | /* for selectsRange */ null,
+        event: React.SyntheticEvent<any> | undefined,
+    ): void;
     onChangeRaw?(event: React.FocusEvent<HTMLInputElement>): void;
     onClickOutside?(event: React.MouseEvent<HTMLDivElement>): void;
     onDayMouseEnter?: (date: Date) => void;
