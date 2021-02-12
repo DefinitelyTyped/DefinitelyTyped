fe82e0628591b48e3091c8fcd33b2f76db218d9d
unknown <kieran6roberts@gmail.com>
2021-02-12 17:39:19 +0100

    fix test format errors

--- types/react-select-country-list/index.d.ts
+++ types/react-select-country-list/index.d.ts
@@ -1,44 +1,42 @@
 // Type definitions for react-select-country-list 2.2
 // Project: https://github.com/Chun-Lin/react-select-country-list
-// Definitions by: My Self <https://github.com/kieran6roberts>
+// Definitions by: Kieran Roberts <https://github.com/kieran6roberts>
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
 
 declare namespace ReactSelectCountries {
     interface CountryData {
         label: string;
-        value: string
+        value: string;
     }
-    
+
     interface LabelValueMap {
-        [key: string]: string
+        [key: string]: string;
     }
-    
+
     interface Countries {
-        data: Array<CountryData>;
+        data: CountryData[];
         labelMap: LabelValueMap;
         valueMap: LabelValueMap;
     }
 }
 
-declare module "react-select-country-list" {
-    class CountryList {
-        data: Array<ReactSelectCountries.CountryData>;
-        labelMap: ReactSelectCountries.LabelValueMap;
-        valueMap: ReactSelectCountries.LabelValueMap;
+declare class CountryList {
+    data: ReactSelectCountries.CountryData[];
+    labelMap: ReactSelectCountries.LabelValueMap;
+    valueMap: ReactSelectCountries.LabelValueMap;
 
-        getValue(label: string): string;
-        getLabel(value: string): string;
-        getValues(): Array<string>;
-        getLabels(): Array<string>;
-        getLabelList(): ReactSelectCountries.LabelValueMap;
-        getValueList(): ReactSelectCountries.LabelValueMap;
-        getData(): Array<ReactSelectCountries.CountryData>;
-        setLabel(value: string, label: string): ReactSelectCountries.Countries;
-        setEmpty(label: string): ReactSelectCountries.Countries;
-        native(): ReactSelectCountries.Countries;
-    }
-
-    const countryList: () => CountryList;
-
-    export default countryList;
+    getValue(label: string): string;
+    getLabel(value: string): string;
+    getValues(): string[];
+    getLabels(): string[];
+    getLabelList(): ReactSelectCountries.LabelValueMap;
+    getValueList(): ReactSelectCountries.LabelValueMap;
+    getData(): ReactSelectCountries.CountryData[];
+    setLabel(value: string, label: string): ReactSelectCountries.Countries;
+    setEmpty(label: string): ReactSelectCountries.Countries;
+    native(): ReactSelectCountries.Countries;
 }
+
+declare function countryList(): CountryList;
+
+export default countryList;
