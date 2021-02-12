759465db09e6c31d615a396aa9cc5d411634427a
Nick Hackman <snickhackman@gmail.com>
2021-02-11 21:49:04 -0500

    chore(react-blessed): version bumps
    Added NickHackman as a maintainer of @types/react-blesssed.

--- types/react-blessed/index.d.ts
+++ types/react-blessed/index.d.ts
@@ -1,18 +1,15 @@
-// Type definitions for react-blessed 0.3
+// Type definitions for react-blessed 0.7
 // Project: https://github.com/yomguithereal/react-blessed#readme
 // Definitions by: Century Guo <https://github.com/guoshencheng>
+//                 NickHackman <https://github.com/NickHackman>
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
-// TypeScript Version: 2.8
+// TypeScript Version: 4.2
 
 /// <reference path="./react.d.ts" />
 
 import * as React from 'react';
 import { Widgets, screen } from 'blessed';
 export type Callback = () => void | null | undefined;
-export type renderer = (
-  c: JSX.Element, s: Widgets.Screen, callback?: Callback,
-) => React.Component<any, any> | null;
-export function render(
-  c: JSX.Element, s: Widgets.Screen, callback?: Callback
-): React.Component<any, any> | null;
+export type renderer = (c: JSX.Element, s: Widgets.Screen, callback?: Callback) => React.Component<any, any> | null;
+export function render(c: JSX.Element, s: Widgets.Screen, callback?: Callback): React.Component<any, any> | null;
 export function createBlessedRenderer(bls: any): renderer;
