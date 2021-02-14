a7cd98e0faf1cfe718e736aacaca81fa050935ac
Dániel Tar <bdanee88@gmail.com>
2021-02-14 10:34:11 +0100

    Remove patch version from semver-sort

--- types/semver-sort/index.d.ts
+++ types/semver-sort/index.d.ts
@@ -1,4 +1,4 @@
-// Type definitions for semver-sort 0.0.4
+// Type definitions for semver-sort 0.0
 // Project: https://github.com/ragingwind/semver-sort
 // Definitions by: Leonid Logvinov <https://github.com/LogvinovLeon>, Dániel Tar <https://github.com/qcz>
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
