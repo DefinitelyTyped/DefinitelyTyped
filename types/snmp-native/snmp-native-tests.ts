4e423ce477134c2dae54a070b546c70cca3e3ac6
Matthew Wilson <matt@MattBook.lan>
2021-02-15 12:51:21 +0000

    Corrected linting errors and tsconfig.json

--- types/snmp-native/snmp-native-tests.ts
+++ types/snmp-native/snmp-native-tests.ts
@@ -1,15 +1,14 @@
+/// <reference types="node" />
 import snmp, { VarBind, Packet } from 'snmp-native';
-
 
 // snmp.defaultOptions
 
-const v_host: string = snmp.defaultOptions.host;
-const v_community: string = snmp.defaultOptions.community;
-const v_family: 'udp4' | 'udp6' = snmp.defaultOptions.family;
-const v_port: number = snmp.defaultOptions.port;
-const v_bindPort: number = snmp.defaultOptions.bindPort;
-const v_timeouts: number[] = snmp.defaultOptions.timeouts;
-
+const v_host: string | undefined = snmp.defaultOptions.host;
+const v_community: string | undefined = snmp.defaultOptions.community;
+const v_family: 'udp4' | 'udp6' | undefined = snmp.defaultOptions.family;
+const v_port: number | undefined = snmp.defaultOptions.port;
+const v_bindPort: number | undefined = snmp.defaultOptions.bindPort;
+const v_timeouts: number[] | undefined = snmp.defaultOptions.timeouts;
 
 // snmp.Session
 
@@ -20,27 +19,20 @@ const options = {
     timeouts: [1000, 2000, 3000],
 };
 
-const session = new snmp.Session( {
-
+const session = new snmp.Session({
     ...options,
-
     family: 'udp6',
     bindPort: 19840,
-
-    msgReceived ( message, rinfo ) {
-
+    msgReceived(message, rinfo) {
         const v_message: Buffer = message;
-        const { address, family, port, size } = rinfo;
 
+        const { address, family, port, size } = rinfo;
         const v_address: string = address;
         const v_family: 'IPv4' | 'IPv6' = family;
         const v_port: number = port;
         const v_size: number = size;
-
     },
-
-} );
-
+});
 
 // snmp.Session methods
 
@@ -48,56 +40,51 @@ const str_oid = '.1.3.6.1.4.1.1.2.3.4';
 const arr_oid = [1, 3, 6, 1, 4, 1, 1, 2, 3, 4];
 const oids = [str_oid, arr_oid];
 
-function callback ( err: Error | null, varbinds: VarBind[] ): void {
-
+function callback(err: Error | null, varbinds: VarBind[]): void {
     const vb = varbinds[0];
-
     const v_oid: number[] = vb.oid;
     const v_type: number = vb.type;
     const v_value: any = vb.value;
     const v_sendStamp: number = vb.sendStamp;
     const v_receiveStamp: number = vb.receiveStamp;
     const v_valueRaw: Buffer = vb.valueRaw;
-    const v_valueHex: string = vb.valueHex;
-
+    const v_valueHex: string | undefined = vb.valueHex;
 }
 
-session.get( { oid: str_oid, }, callback );
-session.get( { oid: arr_oid, }, callback );
-session.get( { oid: arr_oid, ...options, }, callback );
+session.get({ oid: str_oid, }, callback);
+session.get({ oid: arr_oid, }, callback);
+session.get({ oid: arr_oid, ...options, }, callback);
 
-session.getNext( { oid: str_oid, }, callback );
-session.getNext( { oid: arr_oid, }, callback );
-session.getNext( { oid: arr_oid, ...options, }, callback );
+session.getNext({ oid: str_oid, }, callback);
+session.getNext({ oid: arr_oid, }, callback);
+session.getNext({ oid: arr_oid, ...options, }, callback);
 
-session.getSubtree( { oid: str_oid, }, callback );
-session.getSubtree( { oid: arr_oid, }, callback );
-session.getSubtree( { oid: arr_oid, combinedTimeout: 10, }, callback );
-session.getSubtree( { oid: arr_oid, combinedTimeout: 10, ...options, }, callback );
+session.getSubtree({ oid: str_oid, }, callback);
+session.getSubtree({ oid: arr_oid, }, callback);
+session.getSubtree({ oid: arr_oid, combinedTimeout: 10, }, callback);
+session.getSubtree({ oid: arr_oid, combinedTimeout: 10, ...options, }, callback);
 
-session.getAll( { oids, }, callback );
-session.getAll( { oids, combinedTimeout: 10, }, callback );
-session.getAll( { oids, combinedTimeout: 10, abortOnError: true, }, callback );
-session.getAll( { oids, combinedTimeout: 10, abortOnError: true, ...options, }, callback );
+session.getAll({ oids, }, callback);
+session.getAll({ oids, combinedTimeout: 10, }, callback);
+session.getAll({ oids, combinedTimeout: 10, abortOnError: true, }, callback);
+session.getAll({ oids, combinedTimeout: 10, abortOnError: true, ...options, }, callback);
 
-session.set( { oid: str_oid, type: 2, value: 10, } );
-session.set( { oid: arr_oid, type: 2, value: 10, }, callback );
-session.set( { oid: arr_oid, type: 2, value: 10, }, callback );
-session.set( { oid: arr_oid, type: 2, value: 10, ...options, }, callback );
-session.set( { oid: arr_oid, value: null, }, callback );
-session.set( { oid: arr_oid, type: null, }, callback );
+session.set({ oid: str_oid, type: 2, value: 10, });
+session.set({ oid: arr_oid, type: 2, value: 10, }, callback);
+session.set({ oid: arr_oid, type: 2, value: 10, }, callback);
+session.set({ oid: arr_oid, type: 2, value: 10, ...options, }, callback);
+session.set({ oid: arr_oid, value: null, }, callback);
+session.set({ oid: arr_oid, type: null, }, callback);
 
 session.close();
 
-
 // snmp.compareOids
 
-const v_compare: -1 | 0 | 1 = snmp.compareOids( str_oid, arr_oid );
-
+const v_compare: -1 | 0 | 1 = snmp.compareOids(str_oid, arr_oid);
 
 // snmp.parse
 
-const vp: Packet = snmp.parse( Buffer.from( '' ) );
+const vp: Packet = snmp.parse(Buffer.from(''));
 const pdu = vp.pdu;
 
 const vp_version: 0 | 1 = vp.version;
@@ -108,23 +95,22 @@ const p_error: number = pdu.error;
 const p_errorIndex: number = pdu.errorIndex;
 const p_varbinds: VarBind[] = pdu.varbinds;
 
-
 // snmp.encode
 
-const packet = <Packet> {
+const varbinds: VarBind[] = [];
+const packet: Packet = {
     version: 0,
     community: 'public',
     pdu: {
+        varbinds,
         type: 1,
         reqid: 2,
         error: 3,
         errorIndex: 4,
-        varbinds: <VarBind[]>[],
     },
 };
 
-const v_encode: Buffer = snmp.encode( packet );
-
+const v_encode: Buffer = snmp.encode(packet);
 
 // snmp.DataTypes
 
@@ -145,14 +131,12 @@ const v_NoSuchInstance: number = snmp.DataTypes.NoSuchInstance;
 const v_EndOfMibView: number = snmp.DataTypes.EndOfMibView;
 const v_PDUBase: number = snmp.DataTypes.PDUBase;
 
-
 // snmp.PduTypes
 
 const v_GetRequestPDU: number = snmp.PduTypes.GetRequestPDU;
 const v_GetNextRequestPDU: number = snmp.PduTypes.GetNextRequestPDU;
 const v_GetResponsePDU: number = snmp.PduTypes.GetResponsePDU;
 const v_SetRequestPDU: number = snmp.PduTypes.SetRequestPDU;
-
 
 // snmp.Errors
 
