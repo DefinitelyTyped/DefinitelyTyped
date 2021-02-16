12646ede97704d4293cc542d839148023c6cb312
Peter Wagenet <peter.wagenet@gmail.com>
2021-02-16 08:22:58 -0800

    [ember-data] Improve PromiseArray and PromiseObject

--- types/ember-data/test/promises.ts
+++ types/ember-data/test/promises.ts
@@ -1,6 +1,7 @@
-import Ember from 'ember';
+import ArrayProxy from '@ember/array/proxy';
 import DS from 'ember-data';
 import RSVP from 'rsvp';
+
 import { assertType } from './lib/assert';
 
 declare const store: DS.Store;
@@ -21,6 +22,9 @@ promiseFind.content; // $ExpectType Person | undefined
 
 promiseFind.get('firstName');
 
-let promiseArray = DS.PromiseArray.create({ promise: RSVP.Promise.resolve(Ember.A([1])) });
+let promiseArray = DS.PromiseArray.create({ promise: RSVP.Promise.resolve([1]) });
 
 let promiseObject = DS.PromiseObject.create({ promise: RSVP.Promise.resolve({ value: 1 }) });
+
+let promiseManyArray = DS.PromiseManyArray.create({ promise: store.findAll('person') });
+assertType<RSVP.Promise<ArrayProxy<DS.Model>>>(promiseManyArray.promise);
