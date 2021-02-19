01d226e76bbc5f6ed022466b451db6ad9f9e8f73
jpoehnelt <jpoehnelt@users.noreply.github.com>
2021-02-19 00:52:50 +0000

    chore: sync updates to google-maps-web

--- types/google.maps/google.maps-tests.ts
+++ types/google.maps/google.maps-tests.ts
@@ -1,3 +1,3 @@
 // No tests required for generated types
-// Synced from: https://github.com/googlemaps/js-types/commit/f73d3ad95d04d136b6d3103b6058f138d3a1643f
+// Synced from: https://github.com/googlemaps/js-types/commit/39e6ee12caa1a85ac4c82102293896ea6cc12867
 google.maps.Map;
