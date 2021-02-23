8262d3f8cfc147df61e608cd746499b12e81854e
Federico <bpm230@gmail.com>
2021-02-23 10:00:14 -0300

    Add index.d.ts to root of package

--- types/ckeditor__ckeditor5-utils/index.d.ts
+++ types/ckeditor__ckeditor5-utils/index.d.ts
@@ -5,56 +5,29 @@
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
 // TypeScript Version: 3.6
 
-/// <reference path="./src/areconnectedthroughproperties.d.ts" />
-/// <reference path="./src/ckeditorerror.d.ts" />
-/// <reference path="./src/collection.d.ts" />
-/// <reference path="./src/comparearrays.d.ts" />
-/// <reference path="./src/config.d.ts" />
-/// <reference path="./src/count.d.ts" />
-/// <reference path="./src/diff.d.ts" />
-/// <reference path="./src/difftochanges.d.ts" />
-/// <reference path="./src/dom/createelement.d.ts" />
-/// <reference path="./src/dom/emittermixin.d.ts" />
-/// <reference path="./src/dom/getancestors.d.ts" />
-/// <reference path="./src/dom/getborderwidths.d.ts" />
-/// <reference path="./src/dom/getcommonancestor.d.ts" />
-/// <reference path="./src/dom/getdatafromelement.d.ts" />
-/// <reference path="./src/dom/getpositionedancestor.d.ts" />
-/// <reference path="./src/dom/global.d.ts" />
-/// <reference path="./src/dom/indexof.d.ts" />
-/// <reference path="./src/dom/insertat.d.ts" />
-/// <reference path="./src/dom/isnode.d.ts" />
-/// <reference path="./src/dom/isrange.d.ts" />
-/// <reference path="./src/dom/istext.d.ts" />
-/// <reference path="./src/dom/iswindow.d.ts" />
-/// <reference path="./src/dom/position.d.ts" />
-/// <reference path="./src/dom/rect.d.ts" />
-/// <reference path="./src/dom/remove.d.ts" />
-/// <reference path="./src/dom/resizeobserver.d.ts" />
-/// <reference path="./src/dom/scroll.d.ts" />
-/// <reference path="./src/dom/setdatainelement.d.ts" />
-/// <reference path="./src/dom/tounit.d.ts" />
-/// <reference path="./src/elementreplacer.d.ts" />
-/// <reference path="./src/emittermixin.d.ts" />
-/// <reference path="./src/env.d.ts" />
-/// <reference path="./src/eventinfo.d.ts" />
-/// <reference path="./src/fastdiff.d.ts" />
-/// <reference path="./src/first.d.ts" />
-/// <reference path="./src/focustracker.d.ts" />
-/// <reference path="./src/isiterable.d.ts" />
-/// <reference path="./src/keyboard.d.ts" />
-/// <reference path="./src/keystrokehandler.d.ts" />
-/// <reference path="./src/locale.d.ts" />
-/// <reference path="./src/mapsequal.d.ts" />
-/// <reference path="./src/mix.d.ts" />
-/// <reference path="./src/nth.d.ts" />
-/// <reference path="./src/objecttomap.d.ts" />
-/// <reference path="./src/observablemixin.d.ts" />
-/// <reference path="./src/priorities.d.ts" />
-/// <reference path="./src/spy.d.ts" />
-/// <reference path="./src/toarray.d.ts" />
-/// <reference path="./src/tomap.d.ts" />
-/// <reference path="./src/translation-service.d.ts" />
-/// <reference path="./src/uid.d.ts" />
-/// <reference path="./src/unicode.d.ts" />
-/// <reference path="./src/version.d.ts" />
+export { default as env } from "./src/env";
+export { default as diff } from "./src/diff";
+export { default as mix } from "./src/mix";
+export { default as EmitterMixin } from "./src/emittermixin";
+export { default as ObservableMixin } from "./src/observablemixin";
+export { default as ElementReplacer } from "./src/elementreplacer";
+export { default as createElement } from "./src/dom/createelement";
+export { default as DomEmitterMixin } from "./src/dom/emittermixin";
+export { default as global } from "./src/dom/global";
+export { default as getDataFromElement } from "./src/dom/getdatafromelement";
+export { default as Rect } from "./src/dom/rect";
+export { default as ResizeObserver } from "./src/dom/resizeobserver";
+export { default as setDataInElement } from "./src/dom/setdatainelement";
+export { default as toUnit } from "./src/dom/tounit";
+export * from "./src/keyboard";
+export { default as Locale } from "./src/locale";
+export { default as Collection } from "./src/collection";
+export { default as first } from "./src/first";
+export { default as FocusTracker } from "./src/focustracker";
+export { default as KeystrokeHandler } from "./src/keystrokehandler";
+export { default as toArray } from "./src/toarray";
+export { default as toMap } from "./src/tomap";
+export { default as priorities } from "./src/priorities";
+export { default as uid } from "./src/uid";
+export { default as version } from "./src/version";
+export { default as CKEditorError, logError, logWarning } from "./src/ckeditorerror";
