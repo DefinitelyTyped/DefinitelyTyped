2769fa63f30aeec1fe7640c6526b90482bc1c9f6
Amin Yahyaabadi <aminyahyaabadi74@gmail.com>
2021-02-23 14:23:16 -0600

    Prettier

--- types/atom/autocomplete-plus/autocomplete-plus-tests.ts
+++ types/atom/autocomplete-plus/autocomplete-plus-tests.ts
@@ -1,24 +1,29 @@
-import { SuggestionsRequestedEvent, Suggestion, Suggestions, SuggestionInsertedEvent, AutocompleteProvider } from "./index.d"
-import { TextEditor, Point } from "../index.d"
+import {
+    SuggestionsRequestedEvent,
+    Suggestion,
+    Suggestions,
+    SuggestionInsertedEvent,
+    AutocompleteProvider,
+} from './index.d';
+import { TextEditor, Point } from '../index.d';
 
 // https://github.com/atom/autocomplete-plus/wiki/Provider-API#suggestions
 const suggestion: Suggestion = {
-  text: 'someText',
-  snippet: 'someText(${1:myArg})',
-  displayText: 'someText',
-  replacementPrefix: 'so',
-  type: 'function',
-  leftLabel: '',
-  leftLabelHTML: '',
-  rightLabel: '',
-  rightLabelHTML: '',
-  className: '',
-  iconHTML: '',
-  description: '',
-  descriptionMoreURL: '',
-  // characterMatchIndices: [0, 1, 2], // TODO missing in the types
-}
-
+    text: 'someText',
+    snippet: 'someText(${1:myArg})',
+    displayText: 'someText',
+    replacementPrefix: 'so',
+    type: 'function',
+    leftLabel: '',
+    leftLabelHTML: '',
+    rightLabel: '',
+    rightLabelHTML: '',
+    className: '',
+    iconHTML: '',
+    description: '',
+    descriptionMoreURL: '',
+    // characterMatchIndices: [0, 1, 2], // TODO missing in the types
+};
 
 const suggestionsRequestedEvent: SuggestionsRequestedEvent = {
     editor: new TextEditor(),
@@ -26,45 +31,53 @@ const suggestionsRequestedEvent: SuggestionsRequestedEvent = {
     bufferPosition: new Point(),
 
     scopeDescriptor: {
-       getScopesArray() { return ["source.js"] }
+        getScopesArray() {
+            return ['source.js'];
+        },
     },
 
-    prefix: "",
+    prefix: '',
 
     activatedManually: false,
-}
+};
 
 // https://github.com/atom/autocomplete-plus/wiki/Provider-API#defining-a-provider
 const provider: AutocompleteProvider = {
-  selector: '.source.js, .source.coffee',
-  disableForSelector: '.source.js .comment',
+    selector: '.source.js, .source.coffee',
+    disableForSelector: '.source.js .comment',
 
-  inclusionPriority: 1,
-  excludeLowerPriority: true,
+    inclusionPriority: 1,
+    excludeLowerPriority: true,
 
-  suggestionPriority: 2,
+    suggestionPriority: 2,
 
-  filterSuggestions: true,
+    filterSuggestions: true,
 
-  getSuggestions({editor, bufferPosition, scopeDescriptor, prefix, activatedManually}: SuggestionsRequestedEvent): Promise<Suggestions> {
-    return new Promise(resolve => resolve([{text: 'something'}]));
-  },
+    getSuggestions({
+        editor,
+        bufferPosition,
+        scopeDescriptor,
+        prefix,
+        activatedManually,
+    }: SuggestionsRequestedEvent): Promise<Suggestions> {
+        return new Promise(resolve => resolve([{ text: 'something' }]));
+    },
 
-  getSuggestionDetailsOnSelect(suggestion) {
-    return new Promise(resolve => resolve(suggestion));
-  },
+    getSuggestionDetailsOnSelect(suggestion) {
+        return new Promise(resolve => resolve(suggestion));
+    },
 
-  onDidInsertSuggestion({editor, triggerPosition, suggestion}: SuggestionInsertedEvent) {},
+    onDidInsertSuggestion({ editor, triggerPosition, suggestion }: SuggestionInsertedEvent) {},
 
-  dispose() {}
+    dispose() {},
 };
 
 async function testFun() {
-  const suggestions = await provider.getSuggestions(suggestionsRequestedEvent)
+    const suggestions = await provider.getSuggestions(suggestionsRequestedEvent);
 
-  const suggestionInsertedEvent: SuggestionInsertedEvent = {
-    editor: new TextEditor(),
-    triggerPosition: new Point(),
-    suggestion: suggestions[0],
-  }
+    const suggestionInsertedEvent: SuggestionInsertedEvent = {
+        editor: new TextEditor(),
+        triggerPosition: new Point(),
+        suggestion: suggestions[0],
+    };
 }
