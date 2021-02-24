6dd07a2f325d4a3d35481f47e99f4e6e13689adb
Amin Yahyaabadi <aminyahyaabadi74@gmail.com>
2021-02-23 23:21:19 -0600

    Move text-editor-registry types to a separate file

--- types/atom/index.d.ts
+++ types/atom/index.d.ts
@@ -34,6 +34,7 @@ import { DecorationOptions } from "./src/decoration";
 import { NotificationManager } from "./src/notification-manager";
 import { TextEditor } from "./src/text-editor";
 import { TextEditorElement } from "./src/text-editor-element";
+import { TextEditorRegistry } from "./src/text-editor-registry";
 
 declare global {
     const atom: AtomEnvironment;
@@ -349,47 +350,7 @@ export * from "./src/text-editor-component";
 
 export * from "./src/text-editor-element";
 
-/** Experimental: This global registry tracks registered TextEditors. */
-export interface TextEditorRegistry {
-    // Managing Text Editors
-    /** Remove all editors from the registry. */
-    clear(): void;
-
-    /** Register a TextEditor. */
-    add(editor: TextEditor): Disposable;
-
-    /** Remove the given TextEditor from the registry. */
-    remove(editor: TextEditor): boolean;
-
-    /** Keep a TextEditor's configuration in sync with Atom's settings. */
-    maintainConfig(editor: TextEditor): Disposable;
-
-    /**
-     *  Set a TextEditor's grammar based on its path and content, and continue
-     *  to update its grammar as gramamrs are added or updated, or the editor's
-     *  file path changes.
-     */
-    maintainGrammar(editor: TextEditor): Disposable;
-
-    /**
-     *  Force a TextEditor to use a different grammar than the one that would
-     *  otherwise be selected for it.
-     */
-    setGrammarOverride(editor: TextEditor, scopeName: string): void;
-
-    /**
-     *  Retrieve the grammar scope name that has been set as a grammar override
-     *  for the given TextEditor.
-     */
-    getGrammarOverride(editor: TextEditor): string|null;
-
-    /** Remove any grammar override that has been set for the given TextEditor. */
-    clearGrammarOverride(editor: TextEditor): void;
-
-    // Event Subscription
-    /** Invoke the given callback with all the current and future registered TextEditors. */
-    observe(callback: (editor: TextEditor) => void): Disposable;
-}
+export * from "./src/text-editor-registry";
 
 export type TooltipPlacement =
     |"top"|"bottom"|"left"|"right"
