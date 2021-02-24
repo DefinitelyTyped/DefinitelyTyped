6ef531cf7ee39b958b2c629c63769d421b67d3f7
Amin Yahyaabadi <aminyahyaabadi74@gmail.com>
2021-02-23 23:15:14 -0600

    Make command-registry a separate file

--- types/atom/index.d.ts
+++ types/atom/index.d.ts
@@ -18,6 +18,7 @@ import { ChildProcess } from "child_process";
 // Imports ======================================================
 import { Config } from "./src/config";
 import { ConfigValues } from "./src/config-schema";
+import { CommandRegistry } from "./src/command-registry";
 
 
 declare global {
@@ -305,63 +306,7 @@ export * from "./src/config-schema";
 
 export * from "./src/color";
 
-export interface CommandRegistryTargetMap extends HTMLElementTagNameMap {
-  [key: string]: EventTarget;
-}
-
-export type CommandRegistryListener<TargetType extends EventTarget> = {
-    didDispatch(event: CommandEvent<TargetType>): void | Promise<void>,
-    displayName?: string,
-    description?: string,
-    hiddenInCommandPalette?: boolean,
-} | ((event: CommandEvent<TargetType>) => void | Promise<void>);
-
-/**
- *  Associates listener functions with commands in a context-sensitive way
- *  using CSS selectors.
- */
-export interface CommandRegistry {
-    /** Register a single command. */
-    add<T extends keyof CommandRegistryTargetMap>(
-        target: T, commandName: string,
-        listener: CommandRegistryListener<CommandRegistryTargetMap[T]>
-      ): Disposable;
-    /** Register a single command. */
-    add<T extends Node>(
-        target: T, commandName: string,
-        listener: CommandRegistryListener<T>
-      ): Disposable;
-
-    /** Register multiple commands. */
-    add<T extends keyof CommandRegistryTargetMap>(target: T, commands: {
-        [key: string]: CommandRegistryListener<CommandRegistryTargetMap[T]>
-    }): CompositeDisposable;
-    /** Register multiple commands. */
-    add<T extends Node>(target: T, commands: {
-        [key: string]: CommandRegistryListener<T>
-    }): CompositeDisposable;
-
-    /** Find all registered commands matching a query. */
-    findCommands(params: { target: string|Node }): Array<{
-        name: string,
-        displayName: string,
-        description?: string,
-        tags?: string[],
-    }>;
-
-    /**
-     *  Simulate the dispatch of a command on a DOM node.
-     *  @return Either a Promise that resolves after all handlers complete or null if
-     *  no handlers were matched.
-     */
-    dispatch(target: Node, commandName: string): Promise<void> | null;
-
-    /** Invoke the given callback before dispatching a command event. */
-    onWillDispatch(callback: (event: CommandEvent) => void): Disposable;
-
-    /** Invoke the given callback after dispatching a command event. */
-    onDidDispatch(callback: (event: CommandEvent) => void): Disposable;
-}
+export * from "./src/command-registry";
 
 /**
  *  An object that aggregates multiple Disposable instances together into a
