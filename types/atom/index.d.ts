a044ac9b7f31e4c5eb2f03f6aabafaaae154282a
Amin Yahyaabadi <aminyahyaabadi74@gmail.com>
2021-02-24 00:42:18 -0600

    Move buffered-process types to a seprate file

--- types/atom/index.d.ts
+++ types/atom/index.d.ts
@@ -13,7 +13,6 @@
 /// <reference types="node" />
 
 import { ReadStream, WriteStream } from "fs";
-import { ChildProcess } from "child_process";
 
 // Imports ======================================================
 import { Config } from "./src/config";
@@ -38,6 +37,7 @@ import { TextEditorRegistry } from "./src/text-editor-registry";
 import { TooltipManager } from "./src/tooltip-manager";
 import { ViewRegistry } from "./src/view-registry";
 import { Workspace } from "./src/workspace";
+import { BufferedProcess } from "./src/buffered-process";
 
 declare global {
     const atom: AtomEnvironment;
@@ -361,34 +361,9 @@ export * from "./src/workspace";
 
 export * from "./src/workspace-center";
 
+export * from "./src/buffered-process";
+
 // Extended Classes ===========================================================
-
-/**
- *  A wrapper which provides standard error/output line buffering for
- *  Node's ChildProcess.
- */
-export class BufferedProcess {
-    readonly process?: ChildProcess;
-
-    constructor(options: ProcessOptions);
-
-    // Event Subscription
-    /**
-     *  Will call your callback when an error will be raised by the process. Usually
-     *  this is due to the command not being available or not on the PATH. You can
-     *  call handle() on the object passed to your callback to indicate that you
-     *  have handled this error.
-     */
-    onWillThrowError(callback: (errorObject: HandleableErrorEvent) =>
-        void): Disposable;
-
-    // Helper Methods
-    /** Terminate the process. */
-    kill(): void;
-
-    /** Runs the process. */
-    start(): void;
-}
 
 /**
  *  Like BufferedProcess, but accepts a Node script as the command to run.
@@ -2682,69 +2657,9 @@ export interface MenuOptions {
     command?: string;
 }
 
-export interface NodeProcessOptions {
-    /** The command to execute. */
-    command: string;
-
-    /** The array of arguments to pass to the command. */
-    args?: ReadonlyArray<string>;
-
-    /** The options object to pass to Node's ChildProcess.spawn method. */
-    options?: SpawnProcessOptions;
-
-    /**
-     *  The callback that receives a single argument which contains the standard
-     *  output from the command.
-     */
-    stdout?(data: string): void;
-
-    /**
-     *  The callback that receives a single argument which contains the standard
-     *  error output from the command.
-     */
-    stderr?(data: string): void;
-
-    /** The callback which receives a single argument containing the exit status. */
-    exit?(code: number): void;
-}
-
-export interface ProcessOptions extends NodeProcessOptions {
-    /**
-     *  Whether the command will automatically start when this BufferedProcess is
-     *  created.
-     */
-    autoStart?: boolean;
-}
-
 export interface ReadonlyEditOptions {
     /** Whether the readonly protections on the text editor should be ignored. */
     bypassReadOnly?: boolean;
-}
-
-export interface SpawnProcessOptions {
-    /** Current working directory of the child process. */
-    cwd?: string;
-
-    /** Environment key-value pairs. */
-    env?: { [key: string]: string };
-
-    /** The child's stdio configuration. */
-    stdio?: string|Array<string|number>;
-
-    /** Prepare child to run independently of its parent process. */
-    detached?: boolean;
-
-    /** Sets the user identity of the process. */
-    uid?: number;
-
-    /** Sets the group identity of the process. */
-    gid?: number;
-
-    /**
-     *  If true, runs command inside of a shell. Uses "/bin/sh" on UNIX, and process.env.ComSpec
-     *  on Windows. A different shell can be specified as a string.
-     */
-    shell?: boolean | string;
 }
 
 export interface TextEditOptions {
