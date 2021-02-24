345de0c06030e4831d0e92ff6f979d64a321c2db
Hunter Caron <huntercaron@me.com>
2021-02-24 08:57:35 +0100

    commenting test

--- types/are.na/arena-tests.ts
+++ types/are.na/arena-tests.ts
@@ -2,10 +2,14 @@ import Arena from 'are.na';
 const arena = new Arena();
 
 const makeRequests = async () => {
+    // Channel request
     const channel = await arena.channel('arena-influences').get();
+    // Block request
     const block = await arena.block(3088).get();
-    const user = arena.user(16).get();
-    const search = arena.search('fruit').all();
+    // User request
+    const user = await arena.user(16).get();
+    // Search request
+    const search = await arena.search('fruit').all();
 
     return { channel, block, user, search };
 };
