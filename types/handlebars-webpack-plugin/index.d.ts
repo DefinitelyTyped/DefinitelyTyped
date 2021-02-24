b9ff5f20dc2053f1bcfc2547303b6032926f322c
odas0R <g.rosado@campus.fct.unl.pt>
2021-02-24 22:59:20 +0000

    Fixed errors

--- types/handlebars-webpack-plugin/index.d.ts
+++ types/handlebars-webpack-plugin/index.d.ts
@@ -20,19 +20,11 @@ declare class HandlebarsPlugin implements WebpackPluginInstance {
 
 declare namespace HandlebarsPlugin {
     /**
-     * Type for Handlebars helpers object
-     */
-    type Helpers = HelperDeclareSpec | { projectHelpers: string };
-
-    /**
-     * Type for HtmlWebpackPlugin Options
-     */
-    type HtmlWebpackOptions = Options | HtmlWebpackPlugin;
-
-    /**
      * Type for the object partials that the plugin creates in other to process all partials
      */
-    type PartialsMap = { [partial: string]: string };
+    interface PartialsMap {
+        [partial: string]: string;
+    }
 
     /**
      * Handlebars Webpack Plugin Options
@@ -69,71 +61,52 @@ declare namespace HandlebarsPlugin {
         /**
          * Register custom helpers. May be either a function or a glob-pattern
          */
-        helpers?: Helpers;
+        helpers?: HelperDeclareSpec | { projectHelpers: string };
 
         /**
          * Modify the default output path of each entry-template
-         * @param {String} filepath     - the source of the template
-         * @param {String} outputTemplate - the filepath template defined in `output`
-         * @param {String} rootFolder   - the filepaths rootFolder
-         * @return {String} final path, where the rendered html-file should be saved
          */
         getTargetFilepath?: (filepath: string, outputTemplate: string, rootFolder: string) => string | undefined;
 
         /**
          * Modify the hbs partial-id created for a loaded partial
-         * @param {String} filePath   - filePath to the loaded partial
-         * @return {String} hbs-partialId, per default folder/partialName is used
          */
         getPartialId?: (filePath: string) => string | undefined;
 
         /**
          * onBeforeSetup hook, runs before setup of the plugin
-         * @param {RuntimeOptions} Handlebars - runtime handlebar options
          */
         onBeforeSetup?: (Handlebars: RuntimeOptions) => any;
 
         /**
          * onBeforeAddPartials hook, runs before the partials addition to the .html files
-         * @param {RuntimeOptions} Handlebars - runtime handlebar options
-         * @param {PartialsMap} partialsMap - project partials
          */
         onBeforeAddPartials?: (Handlebars: RuntimeOptions, partialsMap: PartialsMap) => any;
 
         /**
          * onBeforeCompile hook, runs before the plugin compilation
-         * @param {RuntimeOptions} Handlebars - runtime handlebar options
-         * @param {String} templateContent - html of the template
          */
         onBeforeCompile?: (Handlebars: RuntimeOptions, templateContent: string) => any;
 
         /**
          * onBeforeRender hook, runs before rendering of the templates
-         * @param {RuntimeOptions} Handlebars - runtime handlebar options
-         * @param {Object} data - data passed to the template
-         * @param {String} filename - name of the file
          */
         onBeforeRender?: (Handlebars: RuntimeOptions, data: object, filename: string) => any;
 
         /**
          * onBeforeSave hook, runs before saving
-         * @param {RuntimeOptions} Handlebars - runtime handlebar options
-         * @param {String} resultHtml - the result html
-         * @param {String} filename - name of the file
          */
         onBeforeSave?: (Handlebars: RuntimeOptions, resultHtml: string, filename: string) => any;
 
         /**
          * onDone, runs before the final stages of the plugin
-         * @param {RuntimeOptions} Handlebars - runtime handlebar options
-         * @param {String} filename - name of the file
          */
         onDone?: (Handlebars: RuntimeOptions, filename: string) => any;
 
         /**
          * HtmlWebpackPlugin additional configurations
          */
-        htmlWebpackPlugin?: HtmlWebpackOptions;
+        htmlWebpackPlugin?: Options | HtmlWebpackPlugin;
     }
 }
 
