7f4645d3be254bfc0f5a6c3669fac274231e70df
Jason Kwok <JasonHK@users.noreply.github.com>
2021-02-24 13:33:49 +0800

    fix: apply changes suggested by @ljharb

--- types/iterate-value/index.d.ts
+++ types/iterate-value/index.d.ts
@@ -1,6 +1,7 @@
 // Type definitions for iterate-value 1.0
 // Project: https://github.com/ljharb/iterate-value#readme
 // Definitions by: Jason Kwok <https://github.com/JasonHK>
+//                 Jordan Harband <https://github.com/ljharb>
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
 
 declare function iterateValue<T>(iterator: Iterable<T>): T[];
