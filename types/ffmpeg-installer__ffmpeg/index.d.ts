251915d8711ae58b86a0b54413fe056d2f150d2e
Kyle Hensel <k.y.l.e@outlook.co.nz>
2021-02-26 18:33:41 +1300

    create @ffmpeg-installer/ffmpeg

---
+++ types/ffmpeg-installer__ffmpeg/index.d.ts
@@ -0,0 +1,11 @@
+// Type definitions for @ffmpeg-installer/ffmpeg 1.0
+// Project: https://github.com/kribblo/node-ffmpeg-installer#readme
+// Definitions by: Kyle Hensel <https://github.com/k-yle>
+// Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
+
+/** The path to the ffmpeg executable file */
+export const path: string;
+
+export const version: string;
+
+export const url: string;
