9d1410cb9fa9da8d0837f6a2b7fc2297fda6c6f7
JJPell <13407273+JJPell@users.noreply.github.com>
2021-02-27 09:21:01 +0000

    Defined messages

---
+++ types/google-ads-scripts/ads-app/ad-extensions/messages/account-message.d.ts
@@ -0,0 +1,23 @@
+// Type definitions for Google Ads Scripts 2021-02-27
+// Project: https://developers.google.com/google-ads/scripts
+// Definitions by: JJPell <https://github.com/JJPell>
+// Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
+
+/// <reference path="../../../base.d.ts" />
+/// <reference path="./message.d.ts" />
+
+declare namespace GoogleAdsScripts {
+    // TODO: Add doc comments
+    namespace AdsApp {
+        interface AccountMessage extends Message {}
+
+        interface AccountMessageIterator extends Base.Iterator<AccountMessage> {}
+
+        interface AcctountMessageSelector
+            extends Base.Selector<MessageIterator>,
+                Base.SelectorForDateRange,
+                Base.SelectorOrderBy,
+                Base.SelectorWithCondition,
+                Base.SelectorWithLimit {}
+    }
+}
