7957a7072cd31729ac9bd026591508e26d056420
JJPell <13407273+JJPell@users.noreply.github.com>
2021-02-27 11:05:08 +0000

    Defined mobile app

---
+++ types/google-ads-scripts/ads-app/ad-extensions/mobile-apps/ad-group-mobile-app.d.ts
@@ -0,0 +1,29 @@
+// Type definitions for Google Ads Scripts 2021-02-27
+// Project: https://developers.google.com/google-ads/scripts
+// Definitions by: JJPell <https://github.com/JJPell>
+// Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
+
+/// <reference path="../../../base.d.ts" />
+/// <reference path="./mobile-app.d.ts" />
+
+declare namespace GoogleAdsScripts {
+    // TODO: Add doc comments
+    namespace AdsApp {
+        interface AdGroupMobileApp extends MobileApp {
+            getAdGroup(): AdGroup;
+            getBaseAdGroup(): AdGroup;
+            getBaseCampaign(): Campaign;
+            getCampaign(): Campaign;
+        }
+  
+        interface AdGroupMobileAppIterator extends Base.Iterator<AdGroupMobileApp> {}
+  
+        interface AdGroupMobileAppSelector
+            extends Base.Selector<AdGroupMobileAppIterator>,
+                Base.SelectorForDateRange,
+                Base.SelectorOrderBy,
+                Base.SelectorWithCondition,
+                Base.SelectorWithLimit {}
+    }
+  }
+  
