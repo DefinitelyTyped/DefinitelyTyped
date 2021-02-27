9f0faa94ccfbae0efe0b7e25dafcc340ca820ba3
Amin Yahyaabadi <aminyahyaabadi74@gmail.com>
2021-02-27 17:43:51 -0600

    Add Panel to a separate file

--- types/atom/index.d.ts
+++ types/atom/index.d.ts
@@ -398,6 +398,8 @@ export * from "./src/package-manager";
 
 export * from "./src/pane";
 
+export * from "./src/panel";
+
 // Extended Classes ===========================================================
 
 /** Grammar that tokenizes lines of text. */
@@ -588,43 +590,6 @@ export interface GrammarRegistry {
      *  @param contents A string of text for that file path.
      */
     getGrammarScore(grammar: Grammar, filePath: string, contents: string): number;
-}
-
-/**
- *  A container representing a panel on the edges of the editor window. You
- *  should not create a Panel directly, instead use Workspace::addTopPanel and
- *  friends to add panels.
- */
-export interface Panel<T = object> {
-    /** Whether or not the Panel is visible. */
-    readonly visible: boolean;
-
-    // Construction and Destruction
-    /** Destroy and remove this panel from the UI. */
-    destroy(): void;
-
-    // Event Subscription
-    /** Invoke the given callback when the pane hidden or shown. */
-    onDidChangeVisible(callback: (visible: boolean) => void): Disposable;
-
-    /** Invoke the given callback when the pane is destroyed. */
-    onDidDestroy(callback: (panel: Panel<T>) => void): Disposable;
-
-    // Panel Details
-    /** Returns the panel's item. */
-    getItem(): T;
-
-    /** Returns a number indicating this panel's priority. */
-    getPriority(): number;
-
-    /** Returns a boolean true when the panel is visible. */
-    isVisible(): boolean;
-
-    /** Hide this panel. */
-    hide(): void;
-
-    /** Show this panel. */
-    show(): void;
 }
 
 /** Manage a subscription to filesystem events that occur beneath a root directory. */
