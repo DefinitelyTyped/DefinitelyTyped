16742017f16db96d18117b54545092c9d1e67313
JJPell <13407273+JJPell@users.noreply.github.com>
2021-02-27 11:05:08 +0000

    Defined mobile app

---
+++ types/google-ads-scripts/ads-app/ad-extensions/mobile-apps/account-mobile-app.d.ts
@@ -0,0 +1,23 @@
+// Type definitions for Google Ads Scripts 2021-02-27
+// Project: https://developers.google.com/google-ads/scripts
+// Definitions by: JJPell <https://github.com/JJPell>
+// Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
+
+/// <reference path="../../../base.d.ts" />
+/// <reference path="./MobileApp.d.ts" />
+
+declare namespace GoogleAdsScripts {
+    // TODO: Add doc comments
+    namespace AdsApp {
+        interface AccountMobileApp extends MobileApp {}
+
+        interface AccountMobileAppIterator extends Base.Iterator<AccountMobileApp> {}
+
+        interface AcctountMobileAppSelector
+            extends Base.Selector<AccountMobileAppIterator>,
+                Base.SelectorForDateRange,
+                Base.SelectorOrderBy,
+                Base.SelectorWithCondition,
+                Base.SelectorWithLimit {}
+    }
+}
---
+++ types/google-ads-scripts/ads-app/ad-extensions/mobile-apps/campaign-mobile-app.d.d.ts
@@ -0,0 +1,26 @@
+// Type definitions for Google Ads Scripts 2021-02-27
+// Project: https://developers.google.com/google-ads/scripts
+// Definitions by: JJPell <https://github.com/JJPell>
+// Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
+
+/// <reference path="../../../base.d.ts" />
+/// <reference path="./mobile-app.d.ts" />
+
+declare namespace GoogleAdsScripts {
+  // TODO: Add doc comments
+  namespace AdsApp {
+      interface CampaignMobileApp extends MobileApp {
+          getBaseCampaign(): Campaign;
+          getCampaign(): Campaign;
+      }
+
+      interface CampaignMobileAppIterator extends Base.Iterator<CampaignMobileApp> {}
+
+      interface CampaignMobileAppSelector
+          extends Base.Selector<CampaignMobileAppIterator>,
+              Base.SelectorForDateRange,
+              Base.SelectorOrderBy,
+              Base.SelectorWithCondition,
+              Base.SelectorWithLimit {}
+  }
+}
