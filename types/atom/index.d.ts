2fd61b304b0a1fd9ba123efda4dd12440fddec6b
Amin Yahyaabadi <aminyahyaabadi74@gmail.com>
2021-02-27 16:03:54 -0600

    Move PackageManager to a separate file

--- types/atom/index.d.ts
+++ types/atom/index.d.ts
@@ -42,6 +42,7 @@ import { GitRepository } from "./src/git-repository";
 import { HistoryManager } from "./src/history-manager";
 import { KeymapManager } from "./src/keymap-extensions";
 import { Package } from "./src/package";
+import { PackageManager } from "./src/package-manager";
 
 declare global {
     const atom: AtomEnvironment;
@@ -389,6 +390,8 @@ export * from "./src/keymap-extensions";
 
 export * from "./src/package";
 
+export * from "./src/package-manager";
+
 // Extended Classes ===========================================================
 
 /** Grammar that tokenizes lines of text. */
@@ -588,103 +591,6 @@ export interface MenuManager {
 
     /** Refreshes the currently visible menu. */
     update(): void;
-}
-
-/** Package manager for coordinating the lifecycle of Atom packages. */
-export interface PackageManager {
-    // Event Subscription
-    /** Invoke the given callback when all packages have been loaded. */
-    onDidLoadInitialPackages(callback: () => void): Disposable;
-
-    /** Invoke the given callback when all packages have been activated. */
-    onDidActivateInitialPackages(callback: () => void): Disposable;
-
-    /** Invoke the given callback when a package is activated. */
-    onDidActivatePackage(callback: (package: Package) => void): Disposable;
-
-    /** Invoke the given callback when a package is deactivated. */
-    onDidDeactivatePackage(callback: (package: Package) => void): Disposable;
-
-    /** Invoke the given callback when a package is loaded. */
-    onDidLoadPackage(callback: (package: Package) => void): Disposable;
-
-    /** Invoke the given callback when a package is unloaded. */
-    onDidUnloadPackage(callback: (package: Package) => void): Disposable;
-
-    /** Undocumented: invoke the given callback when an activation hook is triggered */
-    onDidTriggerActivationHook(hook: string, callback: () => void): Disposable;
-
-    // Package System Data
-    /** Get the path to the apm command. */
-    getApmPath(): string;
-
-    /** Get the paths being used to look for packages. */
-    getPackageDirPaths(): string[];
-
-    // General Package Data
-    /** Resolve the given package name to a path on disk. */
-    resolvePackagePath(name: string): string|undefined;
-
-    /** Is the package with the given name bundled with Atom? */
-    isBundledPackage(name: string): boolean;
-
-    // Enabling and Disabling Packages
-    /** Enable the package with the given name. */
-    enablePackage(name: string): Package|undefined;
-
-    /** Disable the package with the given name. */
-    disablePackage(name: string): Package|undefined;
-
-    /** Is the package with the given name disabled? */
-    isPackageDisabled(name: string): boolean;
-
-    // Accessing Active Packages
-    /** Get an Array of all the active Packages. */
-    getActivePackages(): Package[];
-
-    /** Get the active Package with the given name. */
-    getActivePackage(name: string): Package|undefined;
-
-    /** Is the Package with the given name active? */
-    isPackageActive(name: string): boolean;
-
-    /** Returns a boolean indicating whether package activation has occurred. */
-    hasActivatedInitialPackages(): boolean;
-
-    // Accessing Loaded Packages
-    /** Get an Array of all the loaded Packages. */
-    getLoadedPackages(): Package[];
-
-    /** Get the loaded Package with the given name. */
-    getLoadedPackage(name: string): Package|undefined;
-
-    /** Is the package with the given name loaded? */
-    isPackageLoaded(name: string): boolean;
-
-    /** Returns a boolean indicating whether package loading has occurred. */
-    hasLoadedInitialPackages(): boolean;
-
-    // Accessing Available Packages
-    /** Returns an Array of strings of all the available package paths. */
-    getAvailablePackagePaths(): string[];
-
-    /** Returns an Array of strings of all the available package names.  */
-    getAvailablePackageNames(): string[];
-
-    /** Returns an Array of strings of all the available package metadata. */
-    getAvailablePackageMetadata(): string[];
-
-    /** Activate a single package by name or path. */
-    activatePackage(nameOrPath: string): Promise<Package>;
-
-    /** Deactivate a single package by name or path. */
-    deactivatePackage(nameOrPath: string, suppressSerialization?: boolean): Promise<void>;
-
-    /** Triggers the given package activation hook. */
-    triggerActivationHook(hook: string): void;
-
-    /** Trigger all queued activation hooks immediately. */
-    triggerDeferredActivationHooks(): void;
 }
 
 /** A container for presenting content in the center of the workspace. */
