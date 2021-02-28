db4260b64ee11a310cdacbd16e2f621e91034b5b
JJPell <13407273+JJPell@users.noreply.github.com>
2021-02-28 21:12:07 +0000

    Defined snippets

---
+++ types/google-ads-scripts/ads-app/ad-extensions/snippets/ad-group-snippet.d.ts
@@ -0,0 +1,28 @@
+// Type definitions for Google Ads Scripts 2021-02-26
+// Project: https://developers.google.com/google-ads/scripts
+// Definitions by: JJPell <https://github.com/JJPell>
+// Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
+
+/// <reference path="../../../base.d.ts" />
+/// <reference path="./snippet.d.ts" />
+
+declare namespace GoogleAdsScripts {
+    // TODO: Add doc comments
+    namespace AdsApp {
+        interface AdGroupSnippet extends Snippet {
+            getAdGroup(): AdGroup;
+            getBaseAdGroup(): AdGroup;
+            getBaseCampaign(): Campaign;
+            getCampaign(): Campaign;
+        }
+
+        interface AdGroupSnippetIterator extends Base.Iterator<AdGroupSnippet> {}
+
+        interface AdGroupSnippetSelector
+            extends Base.Selector<AdGroupSnippetIterator>,
+                Base.SelectorForDateRange,
+                Base.SelectorOrderBy,
+                Base.SelectorWithCondition,
+                Base.SelectorWithLimit {}
+    }
+}
