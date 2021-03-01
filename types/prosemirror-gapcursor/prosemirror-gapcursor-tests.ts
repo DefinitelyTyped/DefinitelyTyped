39c93f670b6d8d7e430f4d3265de4459511a8f4a
ocavue <ocavue@users.noreply.github.com>
2021-03-02 03:04:28 +0800

    [prosemirror-gapcursor] Add NodeSpec.allowGapCursor

--- types/prosemirror-gapcursor/prosemirror-gapcursor-tests.ts
+++ types/prosemirror-gapcursor/prosemirror-gapcursor-tests.ts
@@ -1,4 +1,31 @@
 import { gapCursor } from 'prosemirror-gapcursor';
 import { Plugin } from 'prosemirror-state';
+import * as model from 'prosemirror-model';
 
 const gapCursorPlugin: Plugin = gapCursor();
+
+export const nodeSpec: model.NodeSpec = {
+    attrs: {
+        name: { default: '' },
+    },
+    parseDOM: [
+        {
+            tag: 'span[data-name]',
+            getAttrs(dom) {
+                if (dom instanceof HTMLElement) {
+                    return {
+                        name: dom.getAttribute('data-name')!,
+                    };
+                }
+            },
+        },
+    ],
+    toDOM(node) {
+        const { name } = node.attrs;
+        const attrs = {
+            'data-emoji-name': name,
+        };
+        return ['span', attrs, 0];
+    },
+    allowGapCursor: false,
+};
