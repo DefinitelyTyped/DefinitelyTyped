a1feb87999ac3d6fe3ed3dd94dc0b1c8458e05ea
David Fernando <30445601+david-fernando@users.noreply.github.com>
2021-03-01 11:35:14 -0300

    Update noloopfor-tests.ts

--- types/noloopfor/noloopfor-tests.ts
+++ types/noloopfor/noloopfor-tests.ts
@@ -2,7 +2,7 @@ import noloopfor = require('noloopfor');
 
 const number = 10;
 
-noloopfor.loop(number, 5, ()=> {});
-noloopfor.loopStep(number, 5, 1, ()=> {});
-noloopfor.decrementLoop(number, 5, ()=> {});
-noloopfor.decrementStep(number, 5, 1, ()=> {});
+noloopfor.loop(number, 5, ( )=> { });
+noloopfor.loopStep(number, 5, 1, ( )=> { });
+noloopfor.decrementLoop(number, 5, ( )=> { });
+noloopfor.decrementStep(number, 5, 1, ( )=> { });
