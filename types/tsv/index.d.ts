7bd9a9f9782c5c0159a993e52f12321e2bdf7eb9
s8kggu_zlatkoa <zlatko.andonovski@alite-international.com>
2021-03-02 17:01:14 +0100

    Definitions for tsv

---
+++ types/tsv/index.d.ts
@@ -0,0 +1,20 @@
+// Type definitions for tsv 0.2
+// Project: https://github.com/ricardobeat/TSV
+// Definitions by: Zlatko Andonovski <https://github.com/Goldsmith42>
+// Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
+
+export class Parser {
+    header: boolean;
+    sep: string;
+
+    constructor(sep: string, options?: { header?: boolean });
+
+    stringify(data: any[]): string;
+    parse(tsv: string): any[];
+}
+
+export const CSV: Parser;
+export const TSV: Parser;
+
+export function stringify(data: any[]): string;
+export function parse(tsv: string): any[];
