f350dd9f2efee37487fccc3b962ba7f0c66e2e2a
Masaki Hara <ackie.h.gmai@gmail.com>
2021-03-06 14:29:19 +0900

    feat(jest): Add ./global-types as an alias to ./index

---
+++ types/jest/global-types.d.ts
@@ -0,0 +1,11 @@
+// Starting from jest v24.3.0, the jest package provides its own type definition.
+// @types/jest now serves a different purpose than that;
+// it declares global variables injeced by jest, rather than jest's own API.
+//
+// Unfortunately, that led to `types: ["jest"]` being resolved to the jest package,
+// causing problems when `typeRoots` is configured.
+//
+// This file enables users to specify `types: ["jest/global-types"]` in tsconfig.json
+// to clarify their intention.
+
+import "./index";
