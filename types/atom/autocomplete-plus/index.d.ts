96b959d0976ac5f97df74f8a7988f5f1df1157a5
Amin Yahyaabadi <aminyahyaabadi74@gmail.com>
2021-03-07 03:15:57 -0600

    [atom] add tests for autocomplete-plus (#51402)
    * Add Suggestion type alias
    
    * Add tests for autocomplete-plus
    
    * Test getting suggestions
    
    * Add the tests to other files instead
    
    * Disable strict-export-declare-modifiers in tslint
    
    * Prettier

--- types/atom/autocomplete-plus/index.d.ts
+++ types/atom/autocomplete-plus/index.d.ts
@@ -113,6 +113,7 @@ export interface SnippetSuggestion extends SuggestionBase {
 }
 
 export type AnySuggestion = TextSuggestion|SnippetSuggestion;
+export type Suggestion =  AnySuggestion;
 export type Suggestions = AnySuggestion[];
 
 /** The interface that all Autocomplete+ providers must implement. */
