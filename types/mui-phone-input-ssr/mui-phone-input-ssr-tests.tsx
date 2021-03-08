2f3160bad92057ccbfc262eb9dd527708d39dc4b
Jonathan Jayet <jonathan@hublo.com>
2021-03-08 10:45:53 +0100

    Added types for mui-phone-input-ssr

---
+++ types/mui-phone-input-ssr/mui-phone-input-ssr-tests.tsx
@@ -0,0 +1,8 @@
+import { Country, MuiPhoneNumber } from 'mui-phone-input-ssr';
+import * as React from 'react';
+
+<MuiPhoneNumber title="French phone number" defaultCountry={Country.France} />;
+
+<MuiPhoneNumber title="French phone number" defaultCountry={Country['United States']} excludeCountries={[Country.Chile]} />;
+
+<MuiPhoneNumber title="French phone number" defaultCountry={Country['United States']} regions={['america']} />;
