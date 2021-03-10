48c7cde94f8746a918e58907628a8708b685ce6d
Oliver Joseph Ash <oliverjash@gmail.com>
2021-03-10 15:01:17 +0000

    `react-autosuggest`: `InputProps['onChange']`: restrict event type

--- types/react-autosuggest/react-autosuggest-tests.tsx
+++ types/react-autosuggest/react-autosuggest-tests.tsx
@@ -605,3 +605,12 @@ function testMultiSections() {
         renderSuggestion={suggestion => suggestion}
     />;
 }
+
+const testInputOnChange: Autosuggest.InputProps<{ foo: string }>['onChange'] = event => {
+    const element = event.target;
+    // `value` only exists on the input element, but sometimes this function is
+    // called with a `target` pointing to a non-input element. For example, when
+    // the user clicks on a suggestion. Reduced test case:
+    // https://stackblitz.com/edit/oliverjash-react-autosuggest-1lhfk3?file=index.tsx
+    element.value; // $ExpectError
+};
