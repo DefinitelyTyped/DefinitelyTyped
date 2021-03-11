bbfa9424a23dd6c53bd2088e686fd08528b6613b
eduardocecon <eduardocecon@gmail.com>
2021-03-11 15:50:46 -0300

    Added typedefinition to electron-push-receiver/src/constants

---
+++ types/electron-push-receiver/src/constants/index.d.ts
@@ -0,0 +1,5 @@
+export const START_NOTIFICATION_SERVICE: string;
+export const NOTIFICATION_SERVICE_STARTED: string;
+export const NOTIFICATION_SERVICE_ERROR: string;
+export const NOTIFICATION_RECEIVED: string;
+export const TOKEN_UPDATED: string;
