f4260ad76a80937dac91dca62f81d57a4868de36
eduardocecon <eduardocecon@gmail.com>
2021-03-11 16:10:42 -0300

    Implemented test

--- types/electron-push-receiver/src/constants/index.d.ts
+++ types/electron-push-receiver/src/constants/index.d.ts
@@ -1,3 +1,10 @@
+// Type definitions for electron-push-receiver 2.1.3
+// Project: https://github.com/MatthieuLemoine/electron-push-receiver
+// Definitions by: Cecon <https://github.com/cecon>
+// Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
+// TypeScript Version: 2.8
+
+
 export const START_NOTIFICATION_SERVICE: string;
 export const NOTIFICATION_SERVICE_STARTED: string;
 export const NOTIFICATION_SERVICE_ERROR: string;
