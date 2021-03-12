079185dab99800817e9459b15ba9cd28385d8e58
Piotr Błażejewicz <peterblazejewicz@users.noreply.github.com>
2021-03-12 21:40:27 +0100

    fix(git-branch-is): correct definition
    - missing method
    - correct invalid branch check function, should return string values
      always
    - update tests
    
    https://github.com/kevinoid/git-branch-is/blob/master/index.js
    
    Thanks!

--- types/git-branch-is/index.d.ts
+++ types/git-branch-is/index.d.ts
@@ -6,54 +6,82 @@
 /**
  * Checks that the current branch of a git repository has a given name
  */
-declare function gitBranchIs(branchNameOrTest: BranchNameOrTest, options: Options, callback?: Callback): void;
-declare function gitBranchIs(branchNameOrTest: BranchNameOrTest, options?: Options): Promise<boolean>;
-declare function gitBranchIs(branchNameOrTest: BranchNameOrTest, callback: Callback): void;
+declare function gitBranchIs(
+    branchNameOrTest: gitBranchIs.BranchNameOrTest,
+    options: gitBranchIs.GitBranchIsOptions,
+    callback?: gitBranchIs.Callback,
+): void;
+declare function gitBranchIs(
+    branchNameOrTest: gitBranchIs.BranchNameOrTest,
+    options?: gitBranchIs.GitBranchIsOptions,
+): Promise<string>;
+declare function gitBranchIs(branchNameOrTest: gitBranchIs.BranchNameOrTest, callback: gitBranchIs.Callback): void;
 
-/**
- * test function to apply to the branch name
- */
-type BranchNameTest = (branchName: string) => boolean | Promise<boolean>;
+declare namespace gitBranchIs {
+    /**
+     * Gets the name of the current (i.e. checked out) branch of a git repository.
+     *
+     * @param options Options.
+     * @param callback Callback function called
+     * with the current branch name, empty string if not on a branch, or
+     * <code>Error</code> if there was an error determining the branch name.
+     * @returns If <code>callback</code> is not given, a
+     * <code>Promise</code> with the current branch name, empty string if not on a
+     * branch, or <code>Error</code> if there was an error determining the branch
+     * name.
+     */
+    function getBranch(options: GitBranchIsOptions, callback: Callback): void;
+    function getBranch(options: GitBranchIsOptions): Promise<string>;
 
-/**
- * Expected name of current branch
- * or a test function to apply to the branch name
- */
-type BranchNameOrTest = string | BranchNameTest;
-/**
- * function called with the return value of <code>branchNameOrTest</code> if it is a function,
- * or the result of identity checking <code>branchNameOrTest</code> to the
- * current branch name
- */
-type Callback = (error: Error | null, result?: boolean) => void;
+    /**
+     * test function to apply to the branch name
+     */
+    interface BranchNameTest {
+        (branchName: string): boolean;
+    }
 
-/**
- * Options for {@link gitBranchIs}.
- */
-interface Options {
     /**
-     * Current working directory where the branch name is
-     * tested
-     * @default ''
+     * Expected name of current branch
+     * or a test function to apply to the branch name
      */
-    cwd?: string;
+    type BranchNameOrTest = string | BranchNameTest;
     /**
-     * Extra arguments to pass to git
-     * @default []
+     * function called with the return value of <code>branchNameOrTest</code> if it is a function,
+     * or the result of identity checking <code>branchNameOrTest</code> to the
+     * current branch name
      */
-    gitArgs?: string[];
+    interface Callback {
+        (error: Error | null, result: string): void;
+    }
+
     /**
-     * Path to the repository (i.e.
-     * <code>--git-dir=</code> option to <code>git</code>).
-     * @default ''
+     * Options for {@link gitBranchIs}.
      */
-    gitDir?: string;
-    /**
-     * Git binary name or path to use (default:
-     * <code>'git'</code>).
-     * @default 'git'
-     */
-    gitPath?: string;
+    interface GitBranchIsOptions {
+        /**
+         * Current working directory where the branch name is
+         * tested
+         * @default ''
+         */
+        cwd?: string;
+        /**
+         * Extra arguments to pass to git
+         * @default []
+         */
+        gitArgs?: string[];
+        /**
+         * Path to the repository (i.e.
+         * <code>--git-dir=</code> option to <code>git</code>).
+         * @default ''
+         */
+        gitDir?: string;
+        /**
+         * Git binary name or path to use (default:
+         * <code>'git'</code>).
+         * @default 'git'
+         */
+        gitPath?: string;
+    }
 }
 
 export = gitBranchIs;
