5fe96756b74cccb2815344a1afbc3ba364df8f6d
James Pell <13407273+JJPell@users.noreply.github.com>
2021-03-13 21:57:17 +0000

    Defined BiddingStrategy

---
+++ types/google-ads-scripts/ads-app/bidding-strategies/bidding-strategy.d.ts
@@ -0,0 +1,76 @@
+// Type definitions for Google Ads Scripts 2021-02-26
+// Project: https://developers.google.com/google-ads/scripts
+// Definitions by: JJPell <https://github.com/JJPell>
+// Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
+
+/// <reference path="../../base.d.ts" />
+/// <reference path="../common/stats.d.ts" />
+
+declare namespace GoogleAdsScripts {
+    namespace AdsApp {
+        /**
+         * Represents a flexible bidding strategy.
+         * Bidding strategies store shared bidding configuration data and are account-level objects.
+         * For more information, refer to Using flexible bid strategies.
+         */
+        interface BiddingStrategy {
+            /** Returns the selector of all ad groups that use this bidding strategy. */
+            adGroups(): AdGroupSelector;
+            /** Returns the selector of all campaigns that use this bidding strategy. */
+            campaigns(): CampaignSelector;
+            /** Returns the type of this entity as a String, in this case, "BiddingStrategy". */
+            getEntityType(): string;
+            /** Returns the ID of the bidding strategy. */
+            getId(): number;
+            /** Returns the name of the bidding strategy. */
+            getName(): string;
+            /** Returns stats for the specified date range. */
+            getStatsFor(): Stats;
+            /** Returns stats for the specified custom date range. */
+            getStatsFor(): Stats;
+            /** Returns the type of the bidding strategy. */
+            getType(): string;
+            /** Returns the selector of all keywords that use this bidding strategy. */
+            keywords(): KeywordSelector;
+            /** Returns the selector of all shopping ad groups that use this bidding strategy. */
+            shoppingAdGroups(): ShoppingAdGroupSelector;
+            /** Returns the selector of all shopping campaigns that use this bidding strategy. */
+            shoppingCampaigns(): ShoppingCampaignSelector;
+        }
+
+        /**
+         * An iterator of bidding strategies.
+         *
+         * Typical usage:
+         *
+         *      while (biddingStrategyIterator.hasNext()) {
+         *        var biddingStrategy = biddingStrategyIterator.next();
+         *      }
+         */
+        interface BiddingStrategyIterator extends Base.Iterator<BiddingStrategy> {}
+
+        /**
+         * Fetches bidding strategies. Supports filtering and sorting.
+         *
+         * Typical usage:
+         *
+         *      var biddingStrategySelector = AdsApp
+         *          .biddingStrategies()
+         *          .withCondition("Impressions > 100")
+         *          .forDateRange("LAST_MONTH")
+         *          .orderBy("Clicks DESC");
+         *
+         *      var biddingStrategyIterator = biddingStrategySelector.get();
+         *      while (biddingStrategyIterator.hasNext()) {
+         *        var biddingStrategy = biddingStrategyIterator.next();
+         *      }
+         */
+        interface BiddingStrategySelector
+            extends Base.Selector<BiddingStrategyIterator>,
+                Base.SelectorForDateRange,
+                Base.SelectorOrderBy,
+                Base.SelectorWithCondition,
+                Base.SelectorWithIds,
+                Base.SelectorWithLimit {}
+    }
+}
