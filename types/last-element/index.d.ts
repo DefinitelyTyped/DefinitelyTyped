b741c5e03fea756fc0d7ba037fe41c304412d85a
Rajas Paranjpe <paranjperajas@gmail.com>
2021-03-13 08:13:37 -0600

    Added definitions for last-element

---
+++ types/last-element/index.d.ts
@@ -0,0 +1,8 @@
+// Type definitions for last-element 1.0
+// Project: https://github.com/Prosen-Ghosh/last-element#readme
+// Definitions by: Rajas Paranjpe <https://github.com/ChocolateLoverRaj>
+// Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
+
+declare function last <T>(arr: T[]): T;
+
+export = last;
