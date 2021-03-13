a34f7db0c7c9470781b7a7214b43bc00017e1f7f
Eli Barzilay <eli@barzilay.org>
2021-03-13 15:48:53 -0500

    TEST PR -- IGNORE

---
+++ types/ignore-me/index.d.ts
@@ -0,0 +1,7 @@
+// Type definitions for non-npm package ignore-me-browser 1.0
+// Project: https://github.com/ignore-me-please
+// Definitions by: Eli Barzilay <https://github.com/elibarzilay>
+// Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
+
+export = ignoreMePlease;
+declare function ignoreMePlease(): void;
