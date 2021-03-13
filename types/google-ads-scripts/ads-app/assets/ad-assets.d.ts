7163c04f1dd5cf6d7f161d437a7c2402891e09ff
James Pell <13407273+JJPell@users.noreply.github.com>
2021-03-13 21:33:05 +0000

    Defined assets

---
+++ types/google-ads-scripts/ads-app/assets/ad-assets.d.ts
@@ -0,0 +1,22 @@
+// Type definitions for Google Ads Scripts 2021-02-26
+// Project: https://developers.google.com/google-ads/scripts
+// Definitions by: JJPell <https://github.com/JJPell>
+// Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
+
+/// <reference path="./asset.d.ts" />
+
+declare namespace GoogleAdsScripts {
+    namespace AdsApp {
+        /** Root object for fetching and creating asset objects. An asset can be an image, text, or video. Assets are deduplicated within a given customer account, so assets may be shared between different ads. */
+        interface AdAssets {
+            /** Returns a selector for all assets in the account. */
+            assets(): AssetSelector;
+            /** Returns a builder to upload an image asset. */
+            newImageAssetBuilder(): ImageAssetBuilder;
+            /** Returns a builder to upload a media bundle asset. */
+            newMediaBundleAssetBuilder(): MediaBundleAssetBuilder;
+            /** Returns a builder to upload a YouTube video asset. */
+            newYouTubeVideoAssetBuilder(): YouTubeVideoAssetBuilder;
+        }
+    }
+}
