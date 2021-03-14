d8b8c4a5ef50866afd0f133b3bcac28702b9d955
Robbie Nichols <23241278+defnotrobbie@users.noreply.github.com>
2021-03-14 16:17:14 -0400

    feat(react-blessed): add props from blessed, react, local defs

--- types/react-blessed/index.d.ts
+++ types/react-blessed/index.d.ts
@@ -1,18 +1,218 @@
-// Type definitions for react-blessed 0.3
+// Type definitions for react-blessed 0.7
 // Project: https://github.com/yomguithereal/react-blessed#readme
 // Definitions by: Century Guo <https://github.com/guoshencheng>
+//                 Robbie Nichols <https://github.com/defnotrobbie>
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
-// TypeScript Version: 2.8
+// Minimum TypeScript Version: 4.1
 
-/// <reference path="./react.d.ts" />
+import * as React from "react";
+import * as Blessed from "blessed";
 
-import * as React from 'react';
-import { Widgets, screen } from 'blessed';
+export {};
+
+/* RENDERER *****************************************************************/
+
 export type Callback = () => void | null | undefined;
-export type renderer = (
-  c: JSX.Element, s: Widgets.Screen, callback?: Callback,
-) => React.Component<any, any> | null;
-export function render(
-  c: JSX.Element, s: Widgets.Screen, callback?: Callback
-): React.Component<any, any> | null;
+export type renderer = (c: JSX.Element, s: Blessed.Widgets.Screen, callback?: Callback) => React.Component | null;
+export function render(c: JSX.Element, s: Blessed.Widgets.Screen, callback?: Callback): React.Component | null;
+
 export function createBlessedRenderer(bls: any): renderer;
+
+/* BLESSED ELEMENTS **********************************************************/
+
+export type Element = Blessed.Widgets.BlessedElement;
+export type ScrollableBoxElement = Blessed.Widgets.ScrollableBoxElement;
+export type ScrollableTextElement = Blessed.Widgets.ScrollableTextElement;
+export type BoxElement = Blessed.Widgets.BoxElement;
+export type TextElement = Blessed.Widgets.TextElement;
+export type LineElement = Blessed.Widgets.LineElement;
+export type BigTextElement = Blessed.Widgets.BigTextElement;
+export type ListElement = Blessed.Widgets.ListElement;
+export type FileManagerElement = Blessed.Widgets.FileManagerElement;
+export type ListTableElement = Blessed.Widgets.ListTableElement;
+export type ListbarElement = Blessed.Widgets.ListbarElement;
+export type InputElement = Blessed.Widgets.InputElement;
+export type TextareaElement = Blessed.Widgets.TextareaElement;
+export type TextboxElement = Blessed.Widgets.TextboxElement;
+export type ButtonElement = Blessed.Widgets.ButtonElement;
+export type CheckboxElement = Blessed.Widgets.CheckboxElement;
+export type RadioSetElement = Blessed.Widgets.RadioSetElement;
+export type RadioButtonElement = Blessed.Widgets.RadioButtonElement;
+export type TableElement = Blessed.Widgets.TableElement;
+export type PromptElement = Blessed.Widgets.PromptElement;
+export type QuestionElement = Blessed.Widgets.QuestionElement;
+export type MessageElement = Blessed.Widgets.MessageElement;
+export type LoadingElement = Blessed.Widgets.LoadingElement;
+export type LogElement = Blessed.Widgets.Log;
+export type ProgressBarElement = Blessed.Widgets.ProgressBarElement;
+export type TerminalElement = Blessed.Widgets.TerminalElement;
+export type LayoutElement = Blessed.Widgets.LayoutElement;
+
+/* EVENTS *******************************************************************/
+
+type Prefix<T extends string, P extends string> = `${T}${P}`;
+
+// create event handlers that map to 'blessed' events. see
+// https://github.com/Yomguithereal/react-blessed/blob/master/src/fiber/events.js
+type EventHandlerProp<T extends string, E extends (...args: never) => void> = {
+    [key in `on${Capitalize<T>}`]?: E;
+};
+
+// 'react-blessed' handles 'blessed' events by matching event names to
+// handlers by prefixing event names with with "on" and camelCasing the
+// result. this cannot be fully recreated in TS so we must manually map them
+// here.
+type ScreenElement = "click" | "mouseover" | "mouseout" | "mouseup";
+type ScreenElementPrefix = "element";
+type ExludedScreenEventNames = Prefix<ScreenElementPrefix, Prefix<" ", ScreenElement>>;
+type CamelCasedScreenEventNames = Prefix<ScreenElementPrefix, Capitalize<ScreenElement>>;
+
+type ScreenEventNames =
+    | Exclude<Blessed.Widgets.NodeScreenEventType, ExludedScreenEventNames>
+    | CamelCasedScreenEventNames;
+
+type GenericContentPrefix = "set" | "parsed";
+type GenericContent = "content";
+type ExludedGenericEventNames = Prefix<GenericContentPrefix, Prefix<" ", GenericContent>>;
+type CamelCasedGenericEventNames = Prefix<GenericContentPrefix, Capitalize<GenericContent>>;
+
+type GenericEventNames =
+    | Exclude<Blessed.Widgets.NodeGenericEventType, ExludedGenericEventNames>
+    | CamelCasedGenericEventNames;
+
+type ProgressBarEventNames = Parameters<Blessed.Widgets.ProgressBarElement["on"]>[0];
+
+type SpreadableArgs<T, K = T extends unknown[] ? T : [T]> = K;
+
+type EventHandler<T> = { (...args: SpreadableArgs<T>): void };
+
+// event args need to be manually typed because Blessed.Widgets.NodeWithEvents
+// overloads the event handlers and does not expose the event callbacks as
+// importable types
+
+// name these ScreenEvent or ScreenEventArg?
+export type ScreenEvent = Blessed.Widgets.Screen;
+export type ScreenEventHandler = EventHandler<ScreenEvent>;
+type ScreenEventProps = EventHandlerProp<ScreenEventNames, ScreenEventHandler>;
+
+export type MouseEvent = Blessed.Widgets.Events.IMouseEventArg;
+export type MouseEventHandler = EventHandler<MouseEvent>;
+type MouseEventProps = EventHandlerProp<Blessed.Widgets.NodeMouseEventType, MouseEventHandler>;
+
+export type GenericEvent = void;
+export type GenericEventHandler = EventHandler<GenericEvent>;
+type GenericEventProps = EventHandlerProp<GenericEventNames, GenericEventHandler>;
+
+export type KeyPressEvent = [key: any, event: Blessed.Widgets.Events.IKeyEventArg];
+export type KeyPressEventHandler = EventHandler<KeyPressEvent>;
+type KeyPressEventProps = EventHandlerProp<"keypress", KeyPressEventHandler>;
+
+export type WarningEvent = string;
+export type WarningEventHandler = EventHandler<WarningEvent>;
+type WarningEventProps = EventHandlerProp<"warning", WarningEventHandler>;
+
+export type ProgressBarEvent = void;
+export type ProgressBarEventHandler = EventHandler<ProgressBarEvent>;
+type ProgressBarEventProps = EventHandlerProp<ProgressBarEventNames, ProgressBarEventHandler>;
+
+/* BLESSED REACT JSX ********************************************************/
+
+interface BorderStyle {
+    type?: "line" | "bg";
+    ch?: string;
+    bg?: string;
+    fg?: string;
+    bold?: boolean;
+    underline?: boolean;
+}
+interface ItemStyle {
+    bg?: string;
+    fg?: string;
+    bold?: boolean;
+    underline?: boolean;
+    blink?: boolean;
+    inverse?: boolean;
+    invisible?: boolean;
+}
+interface ListStyle extends ElementStyle {
+    selected?: ItemStyle;
+    item?: ItemStyle;
+}
+interface ProgressBarStyle extends ElementStyle {
+    bar?: { bg?: string; fg?: string };
+}
+interface ElementStyle extends ItemStyle {
+    border?: BorderStyle;
+    focus?: { bg?: string; fg?: string };
+    hover?: { bg?: string; fg?: string };
+    transparent?: boolean;
+    scrollbar?: { bg?: string; fg?: string; track?: { bg?: string; fg?: string } };
+}
+
+interface EventProps
+    extends ScreenEventProps,
+        GenericEventProps,
+        MouseEventProps,
+        KeyPressEventProps,
+        WarningEventProps {}
+
+export type BlessedIntrinsicElements = {
+    box: DetailedBlessedProps<BoxElement>;
+    text: DetailedBlessedProps<TextElement>;
+    line: DetailedBlessedProps<LineElement>;
+    scrollablebox: DetailedBlessedProps<ScrollableBoxElement>;
+    scrollabletext: DetailedBlessedProps<ScrollableTextElement>;
+    bigtext: DetailedBlessedProps<BigTextElement>;
+    list: DetailedBlessedProps<ListElement>;
+    filemanager: DetailedBlessedProps<FileManagerElement>;
+    listtable: DetailedBlessedProps<ListTableElement>;
+    listbar: DetailedBlessedProps<ListbarElement>;
+    input: DetailedBlessedProps<InputElement>;
+    textarea: DetailedBlessedProps<TextareaElement>;
+    textbox: DetailedBlessedProps<TextboxElement>;
+    button: DetailedBlessedProps<ButtonElement>;
+    checkbox: DetailedBlessedProps<CheckboxElement>;
+    radioset: DetailedBlessedProps<RadioSetElement>;
+    radiobutton: DetailedBlessedProps<RadioButtonElement>;
+    table: DetailedBlessedProps<TableElement>;
+    prompt: DetailedBlessedProps<PromptElement>;
+    question: DetailedBlessedProps<QuestionElement>;
+    message: DetailedBlessedProps<MessageElement>;
+    loading: DetailedBlessedProps<LoadingElement>;
+    log: DetailedBlessedProps<LogElement>;
+    progressbar: DetailedBlessedProps<ProgressBarElement>;
+    terminal: DetailedBlessedProps<Blessed.Widgets.TerminalElement>;
+    layout: DetailedBlessedProps<Blessed.Widgets.LayoutElement>;
+    // escape: Blessed.escape is not an element
+    // program: Blessed.Widgets.Program is not an element
+};
+
+export type BlessedIntrinsicElementsPrefixed = {
+    [Key in keyof BlessedIntrinsicElements as Prefix<"blessed-", Key>]: BlessedIntrinsicElements[Key];
+};
+
+// remove indexers
+// https://stackoverflow.com/questions/58216298/how-to-omit-keystring-any-from-a-type-in-typescript
+type KnownKeys<T> = {
+    [K in keyof T]: string extends K ? never : number extends K ? never : K;
+} extends { [_ in keyof T]: infer U }
+    ? U
+    : never;
+
+type ClassProp<T, K = T | undefined | false | null> = T & { class?: K | K[] };
+type ProgressBarProps<T> = T extends ProgressBarElement ? ProgressBarEventProps & { style?: ProgressBarStyle } : {};
+type ListProps<T> = T extends ListElement ? ProgressBarEventProps & { style?: ListStyle; selected?: number } : {};
+// layout does not require prop "layout" in Blessed.Widgets.LayoutOptions--make it optional
+type LayoutProps<T> = T extends LayoutElement ? Partial<Blessed.Widgets.LayoutOptions> : {};
+
+// remove {[key: string]: any} indexer defined in Blessed.Widgets.IOptions.
+// blessed doesn't exist in a DOM so it probably doesn't make sense to allow any property
+type FilterOptions<T extends Record<any, any>> = Partial<Omit<Pick<T, KnownKeys<T>>, "style" | "children">>;
+
+type ModifiedBlessedOptions<T> = FilterOptions<T> & { children?: React.ReactNode; style?: ElementStyle } & EventProps;
+
+// mirrors react prop generation for DOM elements
+type BlessedAttributes<E extends Element> = ClassProp<
+    ModifiedBlessedOptions<E["options"]> & ProgressBarProps<E> & ListProps<E> & LayoutProps<E>
+>;
+type DetailedBlessedProps<E extends Element> = BlessedAttributes<E> & React.ClassAttributes<E>;
---
+++ types/react-blessed/jsx-runtime.d.ts
@@ -0,0 +1,16 @@
+import { BlessedIntrinsicElementsPrefixed, BlessedIntrinsicElements } from ".";
+import * as React from "react";
+
+// using jsx-runtime requires TS 4.1 and setting compiler option
+// "jsxImportSource" to "react-blessed". global JSX.IntrinsicElements will
+// still reflect the values set in @types/react/index.d.ts because they are
+// declared in the global namespace. JSX will otherwise work as expected for
+// "react-blessed" elements and will error for HTML elements.
+declare namespace JSX {
+    // copy React JSX, otherwise class refs won't type as expected
+    interface IntrinsicAttributes extends React.Attributes {}
+    interface IntrinsicClassAttributes<T> extends React.ClassAttributes<T> {}
+    // set IntrinsicElements to 'react-blessed' elements both with and without
+    // 'blessed-' prefix
+    interface IntrinsicElements extends BlessedIntrinsicElementsPrefixed, BlessedIntrinsicElements {}
+}
