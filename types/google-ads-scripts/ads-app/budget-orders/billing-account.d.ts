9494fbed23487297a896e4714fea2350d732ed5c
James Pell <13407273+JJPell@users.noreply.github.com>
2021-03-14 08:47:28 +0000

    Defined BudgetOrders

---
+++ types/google-ads-scripts/ads-app/budget-orders/billing-account.d.ts
@@ -0,0 +1,20 @@
+// Type definitions for Google Ads Scripts 2021-02-26
+// Project: https://developers.google.com/google-ads/scripts
+// Definitions by: JJPell <https://github.com/JJPell>
+// Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
+
+declare namespace GoogleAdsScripts {
+    namespace AdsApp {
+        /** Represents an account to which invoices are sent in consolidated billing. */
+        interface BillingAccount {
+            /** Returns the ID of the billing account. */
+            getId(): number;
+            /** Returns the name of the billing account. */
+            getName(): string;
+            /** Returns the primary billing ID. */
+            getPrimaryBillingId(): string;
+            /** Returns the secondary billing ID or null if there is no secondary billing account. */
+            getSecondaryBillingId(): string;
+        }
+    }
+}
---
+++ types/google-ads-scripts/ads-app/budget-orders/budget-order.d.ts
@@ -0,0 +1,58 @@
+// Type definitions for Google Ads Scripts 2021-02-26
+// Project: https://developers.google.com/google-ads/scripts
+// Definitions by: JJPell <https://github.com/JJPell>
+// Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
+
+/// <reference path="../../base.d.ts" />
+/// <reference path="../common/google-ads-date.d.ts" />
+/// <reference path="./billing-account.d.ts" />
+
+declare namespace GoogleAdsScripts {
+    namespace AdsApp {
+        /**
+         * Represents a Google Ads budget order. Budget orders are used for managing client budgets at the account level.
+         * Each budget order is associated with a billing account which can take invoices for consolidated billing.
+         * For details about budget orders and billing accounts, see Budget Order Service.
+         */
+        interface BudgetOrder {
+            /** Returns the associated billing account. */
+            getBillingAccount(): BillingAccount;
+            /** Returns the budget order's end date or null if there is no end date. */
+            getEndDateTime(): GoogleAdsDate;
+            /** Returns the ID of the budget order. */
+            getId(): number;
+            /** Returns the name of the budget order. */
+            getName(): string;
+            /** Returns the purchase order number. */
+            getPoNumber(): number;
+            /** Returns the effective spending limit in the account's currency, including all adjustments that have been applied to the budget order. */
+            getSpendingLimit(): number;
+            /** Returns the budget order's start date. */
+            getStartDateTime(): GoogleAdsDate;
+            /** Returns the total adjustments that have been applied to the spending limit, in the account's currency. */
+            getTotalAdjustments(): number;
+        }
+
+        /**
+         * An iterator of budgetOrders.
+         *
+         * Typical usage:
+         *
+         *      while (budgetOrderIterator.hasNext()) {
+         *        var budgetOrder = budgetOrderIterator.next();
+         *      }
+         */
+        interface BudgetOrderIterator extends Base.Iterator<BudgetOrder> {}
+
+        /**
+         * Fetches budget orders.
+         *
+         * Typical usage:
+         *
+         *      var budgetOrderIterator = AdsApp.budgetOrders()
+         *        .withCondition("Status = ACTIVE")
+         *        .get();
+         */
+        interface BudgetOrderSelector extends Base.Selector<BudgetOrderIterator>, Base.SelectorWithCondition {}
+    }
+}
