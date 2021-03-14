dbe75990d359ab3f35d48608ebe00f7f96ea887b
e271828- <e271828-@users.noreply.github.com>
2021-03-14 16:40:06 -0700

    Update index.d.ts

--- types/hcaptcha__react-hcaptcha/index.d.ts
+++ types/hcaptcha__react-hcaptcha/index.d.ts
@@ -1,4 +1,4 @@
-// Type definitions for @hcaptcha/react-hcaptcha 0.1
+// Type definitions for @hcaptcha/react-hcaptcha 0.3.5
 // Project: https://github.com/hCaptcha/react-hcaptcha
 // Definitions by: Matt Sutkowski <https://github.com/msutkowski>
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
