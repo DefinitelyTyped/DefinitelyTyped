236515814f7fdf8a1927b162560e96a01a21bcfa
Jean-Baptiste Zeller <esurnir@gmail.com>
2021-03-15 17:54:49 +0100

    Add types for Leaflet-freedraw (#51748)
    * Add type definitions for leaflet-freedraw
    
    * fix(index.d.ts) Use ReadonlyArray<LatLng> instead of LatLng[]
    
    Fixing a common mistake

---
+++ types/leaflet-freedraw/leaflet-freedraw-tests.ts
@@ -0,0 +1,22 @@
+import { Map } from 'leaflet';
+import FreeDraw, { CREATE, EDIT } from 'leaflet-freedraw';
+
+const mapNode = document.getElementById('foo')!;
+const map = new Map(mapNode);
+
+const freeDraw = new FreeDraw({
+    mode: CREATE | EDIT
+});
+
+map.addLayer(freeDraw);
+
+freeDraw.on('markers', event => {
+    console.log(event.latLngs);
+});
+
+document.addEventListener('keydown', event => {
+    // Cancel the current action when the escape key is pressed.
+    event.key === 'Escape' && freeDraw.cancel();
+});
+
+freeDraw.off('markers');
