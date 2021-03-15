0f50ab1f8d970e2cdb3f81a913c90c5fba22fd18
Nate Silva <nate@natesilva.com>
2021-03-15 11:27:01 -0700

    [quotesy] clean up doc comments

--- types/quotesy/index.d.ts
+++ types/quotesy/index.d.ts
@@ -4,20 +4,18 @@
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
 
 /**
- * returns an Array of Quote Objects
+ * Returns an Array of Quote Objects
  */
 export function parse_json(): Quote[];
 
 /**
- * returns a random quote
- * @returns a random quote from the list
+ * Returns a random quote from the list
  */
 export function random(): Quote;
 
 /**
- * returns a random quote for a specific tag
+ * Returns a random quote for a specific tag
  * @param tag the tag to search for
- * @returns a random quote from the list with the desired tag
  */
 export function random_by_tag(tag: string): Quote;
 
