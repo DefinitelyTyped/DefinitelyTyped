7933d9b5543b916de38b962242c4a8bdb0e1b0ef
Jeff Peirson <Jeff.Peirson@a2ius.com>
2021-03-16 15:30:33 -0400

    Added @default jsdocs, revisions from suggestions

--- types/idle-js/index.d.ts
+++ types/idle-js/index.d.ts
@@ -17,14 +17,14 @@ declare class IdleJs {
      *
      * @returns this instance for call chaining
      */
-    start(): IdleJs;
+    start(): this;
 
     /**
      * Stops all tracking.
      *
      * @returns this instance for call chaining
      */
-    stop(): IdleJs;
+    stop(): this;
 
     /**
      * Reset visible and idle state.
@@ -33,7 +33,7 @@ declare class IdleJs {
      * @param visible The new visibility state, or undefined to use the existing state.
      * @returns this instance for call chaining
      */
-    reset(idle?: boolean, visible?: boolean): IdleJs;
+    reset(idle?: boolean, visible?: boolean): this;
 
     /**
      * Updates idle tracking options.
@@ -47,11 +47,15 @@ declare namespace IdleJs {
      */
     interface Options {
         /**
-         * Idle time in ms. The default is 10000ms.
+         * Idle time in ms.
+         *
+         * @default 10000
          */
         idle?: number;
         /**
          * Events that will trigger the idle resetter.
+         *
+         * @default ['mousemove','keydown','mousedown','touchstart']
          */
         events?: string[];
         /**
@@ -72,15 +76,21 @@ declare namespace IdleJs {
         onShow?: () => void;
         /**
          * Set it to false of you want to track only once.
+         *
+         * @default true
          */
         keepTracking?: boolean;
         /**
          * Set it to true if you want to start in the idle state.
+         *
+         * @default false
          */
         startAtIdle?: boolean;
         /**
          * Set it to true if you want `onIdle` to be raised each time the idle period elapses, not just on idle state changes;
          * i.e.; set to true for `setInterval()` behavior, otherwise for `setTimeout()` behavior.
+         *
+         * @default false
          */
         recurIdleCall?: boolean;
     }
