f0269bd2d93be59e5b6927d7285fa9bed63862aa
Cameron Bernhardt <cambernhardt@me.com>
2021-03-16 18:48:29 -0700

    Expand test

--- types/memjs/memjs-tests.ts
+++ types/memjs/memjs-tests.ts
@@ -11,4 +11,6 @@ client.get('hello', (err, val) => {
 });
 client.get('hello'); // $ExpectType Promise<{ value: Buffer; flags: Buffer; }>
 
+// Can initialize clients with client and server options
 memjs.Client.create("12345", { username: "asdf" }); // $ExpectType Client
+memjs.Client.create("12345", { retries: 3 }); // $ExpectType Client
