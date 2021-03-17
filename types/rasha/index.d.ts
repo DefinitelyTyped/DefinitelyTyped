0135ff07fc8bb985a572204f1148de737bc8dd10
Takle <christian.takle@gmail.com>
2021-03-17 10:04:56 +0000

    typs/rasha: rm patch-version

--- types/rasha/index.d.ts
+++ types/rasha/index.d.ts
@@ -1,4 +1,4 @@
-// Type definitions for rasha 1.2.5
+// Type definitions for rasha 1.2
 // Project: https://git.coolaj86.com/coolaj86/rasha.js
 // Definitions by: Justin Baroux <https://github.com/Just1B>
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
