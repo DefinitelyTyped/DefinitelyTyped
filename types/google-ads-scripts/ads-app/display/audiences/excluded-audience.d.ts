bca84f73613a59bd75ca3dd87a768e1148b09fd9
James Pell <13407273+JJPell@users.noreply.github.com>
2021-03-18 16:12:47 +0000

    Defined ExcludedAudience

---
+++ types/google-ads-scripts/ads-app/display/audiences/excluded-audience.d.ts
@@ -0,0 +1,73 @@
+// Type definitions for Google Ads Scripts
+// Project: https://developers.google.com/google-ads/scripts
+// Definitions by: JJPell <https://github.com/JJPell>
+// Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
+
+/// <reference path="../../../base.d.ts" />
+/// <reference path="../../ad-groups/ad-group.d.ts" />
+/// <reference path="../../campaigns/campaign.d.ts" />
+/// <reference path="./audience.d.ts" />
+
+declare namespace GoogleAdsScripts {
+  namespace AdsApp {
+      /** Represents a Google Ads excludedAudience. */
+      interface ExcludedAudience extends Base.StatsFor {
+          /** Returns the ad group to which this excludedAudience belongs. */
+          getAdGroup(): AdGroup;
+          /** Returns the excludedAudience ID of the excludedAudience. */
+          getAudienceId(): number;
+          /** Returns the type of the excludedAudience, either USER_INTEREST or USER_LIST. */
+          getAudienceType(): AudienceTypeType;
+          /** Returns the base ad group to which this excludedAudience belongs. */
+          getBaseAdGroup(): AdGroup;
+          /** Returns the base campaign to which this excludedAudience belongs. */
+          getBaseCampaign(): Campaign;
+          /** Returns the campaign to which this excludedAudience belongs or null if it does not belong to a search or display campaign. */
+          getCampaign(): Campaign;
+          /** Returns the ID of the excludedAudience. */
+          getId(): number;
+          /** Removes the excludedAudience. */
+          remove(): void;
+      }
+
+      /**
+       * An iterator of excludedAudiences.
+       *
+       * Typical usage:
+       *
+       *      while (excludedAudienceIterator.hasNext()) {
+       *        var excludedAudience = excludedAudienceIterator.next();
+       *      }
+       */
+      interface ExcludedAudienceIterator extends Base.Iterator<ExcludedAudience> {}
+
+      /**
+       * An operation representing creation of a new excludedAudience.
+       */
+      interface ExcludedAudienceOperation extends Base.Operation<ExcludedAudience> {}
+
+      /**
+       * Fetches excludedAudiences. Supports filtering and sorting.
+       * 
+       * Typical usage:
+       * 
+       *  var excludedAudienceSelector = AdsApp.display()
+       *      .excludedAudiences()
+       *      .withCondition("Impressions > 100")
+       *      .forDateRange("LAST_MONTH")
+       *      .orderBy("Clicks DESC");
+       * 
+       *  var excludedAudienceIterator = excludedAudienceSelector.get();
+       *  while (excludedAudienceIterator.hasNext()) {
+       *    var excludedAudience = excludedAudienceIterator.next();
+       *  }
+       */
+      interface ExcludedAudienceSelector
+          extends Base.Selector<Audience>,
+              Base.SelectorForDateRange,
+              Base.SelectorOrderBy,
+              Base.SelectorWithCondition,
+              Base.SelectorWithIds,
+              Base.SelectorWithLimit {}
+  }
+}
