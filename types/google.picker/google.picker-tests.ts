03a156ce4c447f6c9678666c6b1f6160bb2cb111
Chives <chivesrs@gmail.com>
2021-03-18 22:39:37 -0400

    Run Prettier on the repo (no actual changes, all formatting)

--- types/google.picker/google.picker-tests.ts
+++ types/google.picker/google.picker-tests.ts
@@ -1,11 +1,9 @@
-
-
 var createPicker = () => {
     var picker = new google.picker.PickerBuilder()
         .addView(new google.picker.DocsUploadView())
         .setOAuthToken("accessToken")
         .setDeveloperKey("developerKey")
-        .setCallback((data:any) => {
+        .setCallback((data: any) => {
             var url = "nothing";
             if (data[google.picker.Response.ACTION] == google.picker.Action.PICKED) {
                 var doc = data[google.picker.Response.DOCUMENTS][0];
