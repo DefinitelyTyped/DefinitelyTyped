a22c2c4e2e3902bf46498fa8b9301c1c384c988a
Mikhail Vorobeychikov <mail@leequixxx.space>
2021-03-19 20:56:47 +0300

    Fix comment in multi-number-parse types

--- types/multi-number-parse/multi-number-parse-tests.ts
+++ types/multi-number-parse/multi-number-parse-tests.ts
@@ -5,7 +5,7 @@ let result: number;
 result = parse('2,543.56', '.'); // returns 2543.56
 result = parse('10 654.1234'); // returns 10654.1234
 result = parse('2.654$10'); // returns 2654.1
-result = parse('2,45EUR'); // extra suff at the end is stripped, returns 2.45
+result = parse('2,45EUR'); // extra stuff at the end is stripped, returns 2.45
 result = parse('-2,543.56'); // negative numbers are OK, returns -2543.56
 result = parse('10 345,234.21'); // returns NaN, too many different separators
 result = parse('1.123.234,534,234'); // returns NaN, impossible to detect the decimal separator
