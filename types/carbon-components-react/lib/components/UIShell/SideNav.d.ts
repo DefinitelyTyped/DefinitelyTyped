6158997b8365b27e9154e00f527aabb3caf1f474
Kyle Albert <5498623+kalbert312@users.noreply.github.com>
2021-03-19 15:07:04 -0400

    Update SideNav.

--- types/carbon-components-react/lib/components/UIShell/SideNav.d.ts
+++ types/carbon-components-react/lib/components/UIShell/SideNav.d.ts
@@ -1,15 +1,11 @@
 import * as React from "react";
-import { InternationalProps, ForwardRefReturn } from "../../../typings/shared";
+import { InternationalProps, ForwardRefReturn, ReactAttr } from "../../../typings/shared";
 
 export type SideNavTranslationKey = "carbon.sidenav.state.closed" | "carbon.sidenav.state.open";
 
-export interface SideNavProps extends InternationalProps<SideNavTranslationKey> {
+export interface SideNavProps extends ReactAttr, InternationalProps<SideNavTranslationKey> {
     addFocusListeners?: boolean;
     addMouseListeners?: boolean;
-    "aria-label"?: string,
-    "aria-labelledby"?: string,
-    children?: React.ReactNode,
-    className?: string,
     defaultExpanded?: boolean;
     expanded?: boolean;
     isChildOfHeader?: boolean;
