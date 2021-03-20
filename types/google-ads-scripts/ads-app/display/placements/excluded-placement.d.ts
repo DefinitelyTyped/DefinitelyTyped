08eca0c4c44b620a588802f0f3490b3a63006637
James Pell <13407273+JJPell@users.noreply.github.com>
2021-03-20 13:31:46 +0000

    Defined ExcludedPlacement

---
+++ types/google-ads-scripts/ads-app/display/placements/excluded-placement.d.ts
@@ -0,0 +1,68 @@
+// Type definitions for Google Ads Scripts
+// Project: https://developers.google.com/google-ads/scripts
+// Definitions by: JJPell <https://github.com/JJPell>
+// Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
+
+/// <reference path="../../../base.d.ts" />
+/// <reference path="../../ad-groups/ad-group.d.ts" />
+/// <reference path="../../campaigns/campaign.d.ts" />
+
+declare namespace GoogleAdsScripts {
+    namespace AdsApp {
+        /** Represents a Google Ads excluded placement. */
+        interface ExcludedPlacement {
+            /** Returns the ad group to which this audience belongs. */
+            getAdGroup(): AdGroup;
+            /** Returns the base ad group to which this audience belongs. */
+            getBaseAdGroup(): AdGroup;
+            /** Returns the base campaign to which this audience belongs. */
+            getBaseCampaign(): Campaign;
+            /** Returns the campaign to which this audience belongs or null if it does not belong to a search or display campaign. */
+            getCampaign(): Campaign;
+            /** Returns the ID of the audience. */
+            getId(): number;
+            /** Returns the URL of the excluded placement. */
+            getUrl(): string;
+            /** Removes the audience. */
+            remove(): void;
+        }
+
+        /**
+         * An iterator of excluded placements.
+         *
+         * Typical usage:
+         *
+         *      while (excludedPlacementIterator.hasNext()) {
+         *        var excludedPlacement = excludedPlacementIterator.next();
+         *      }
+         */
+        interface ExcludedPlacementIterator extends Base.Iterator<ExcludedPlacement> {}
+
+        /** An operation representing creation of a new excluded placement. */
+        interface ExcludedPlacementOperation extends Base.Operation<ExcludedPlacement> {}
+
+        /**
+         * Fetches excluded placements. Supports filtering and sorting.
+         *
+         * Typical usage:
+         *
+         *      var excludedPlacementSelector = AdsApp.display()
+         *          .keywords()
+         *          .withCondition("Impressions > 100")
+         *          .forDateRange("LAST_MONTH")
+         *          .orderBy("Clicks DESC");
+         *
+         *      var excludedPlacementIterator = excludedPlacementSelector.get();
+         *      while (excludedPlacementIterator.hasNext()) {
+         *        var excludedPlacement = excludedPlacementIterator.next();
+         *      }
+         */
+        interface ExcludedPlacementSelector
+            extends Base.Selector<ExcludedPlacementIterator>,
+                Base.SelectorForDateRange,
+                Base.SelectorOrderBy,
+                Base.SelectorWithCondition,
+                Base.SelectorWithIds,
+                Base.SelectorWithLimit {}
+    }
+}
