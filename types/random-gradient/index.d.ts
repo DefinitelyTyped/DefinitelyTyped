a4eba69cbffd3d634c1bf055ea565c37e916f068
dm1sh <me@dmitriy.icu>
2021-03-21 02:23:41 +0500

    Revert "Wrapped all definitions into named module"
    This reverts commit eafa7dcca217db9c0786f4057a87ca83dd5ff122.

--- types/random-gradient/index.d.ts
+++ types/random-gradient/index.d.ts
@@ -3,16 +3,10 @@
 // Definitions by: dm1sh <https://github.com/dm1sh>
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
 
-declare module "random-gradient" {
-    namespace randomGradient {
-      type GradientDirectionT = "diagonal" | "radial" | "horizontal" | "vertical";
-    }
-  
-    function randomGradient(
-      uuid: string,
-      type?: randomGradient.GradientDirectionT
-    ): string;
-  
-    export = randomGradient;
-  }
-  
+declare namespace randomGradient {
+    type GradientDirectionT = "diagonal" | "radial" | "horizontal" | "vertical";
+}
+
+declare function randomGradient(uuid: string, type?: randomGradient.GradientDirectionT): string;
+
+export = randomGradient;
