06495881772638bde34b17e65d467be3a016097a
Piotr Błażejewicz <peterblazejewicz@users.noreply.github.com>
2021-03-20 15:51:46 +0100

    update(tinycon): refine module definition, missing funcs
    - refine UDM like module exports
    - add missing functions exports
    - amend tests
    
    https://github.com/tommoor/tinycon/blob/master/tinycon.js
    
    Thanks!

--- types/tinycon/index.d.ts
+++ types/tinycon/index.d.ts
@@ -2,18 +2,47 @@
 // Project: https://github.com/tommoor/tinycon
 // Definitions by: Daniel Waxweiler <https://github.com/dwaxweiler>
 //                 Julian Hundeloh <https://github.com/jaulz>
+//                 Piotr Błażejewicz <https://github.com/peterblazejewicz>
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
 
-export function setBubble(count: number | string | null): void;
-
-export function setOptions(options: TinyconOptions): void;
-
-export interface TinyconOptions {
-    abbreviate?: boolean;
-    background?: string;
-    color?: string;
-    fallback?: boolean | 'force';
-    font?: string;
-    height?: number;
-    width?: number;
+declare namespace Tinycon {
+    interface TinyconStatic {
+        setBubble(label: number | string | null, color?: string): TinyconStatic;
+        setImage(url: string): TinyconStatic;
+        setOptions(custom: TinyconOptions): TinyconStatic;
+        reset: () => void;
+    }
+    interface TinyconOptions {
+        /**
+         * @default true
+         */
+        abbreviate?: boolean;
+        /**
+         * @default '#F03D25'
+         */
+        background?: string;
+        /**
+         * @default '#ffffff'
+         */
+        color?: string;
+        /**
+         * @default true
+         */
+        crossOrigin?: boolean;
+        fallback?: boolean | "force";
+        font?: string;
+        /**
+         * @default 8
+         */
+        height?: number;
+        /**
+         * @default 7
+         */
+        width?: number;
+    }
 }
+
+declare const Tinycon: Tinycon.TinyconStatic;
+
+export as namespace Tinycon;
+export = Tinycon;
