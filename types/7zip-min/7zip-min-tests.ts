18ca24fae5a21c1e736b2def042e1f1936622172
Piotr Błażejewicz <peterblazejewicz@users.noreply.github.com>
2021-03-20 12:15:45 +0100

    update(7zip-min): version 1.3 changes
    - new properties in results
    - version bump
    - tests amended
    
    https://github.com/onikienko/7zip-min/compare/v1.2.1...v1.3.0
    
    Thanks!

--- types/7zip-min/7zip-min-tests.ts
+++ types/7zip-min/7zip-min-tests.ts
@@ -4,4 +4,31 @@ _7z.pack("index.d.ts", "archive.7z", (err: any) => { });
 _7z.unpack("archive.7z", "./", (err: any) => { });
 _7z.unpack("archive.7z", (err: any) => { });
 
-_7z.list("archive.7z", (err: any, result: _7z.Result[]) => { });
+_7z.list("archive.7z", (err: any, result: _7z.Result[]) => {
+    if (err) {
+        return;
+    }
+
+    for (const item of result) {
+        // attr:'A'
+        // block:''
+        // compressed:'0'
+        // crc:''
+        // date:'2021-03-20'
+        // encrypted:'-'
+        // method:''
+        // name:'index.js'
+        // size:'0'
+        // time:'12:07:46'
+        item.attr; // $ExpectType string
+        item.block; // $ExpectType string
+        item.compressed; // $ExpectType string
+        item.crc; // $ExpectType string
+        item.date; // $ExpectType string
+        item.encrypted; // $ExpectType string
+        item.method; // $ExpectType string
+        item.name; // $ExpectType string
+        item.size; // $ExpectType string
+        item.time; // $ExpectType string
+    }
+});
