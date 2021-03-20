eafa7dcca217db9c0786f4057a87ca83dd5ff122
dm1sh <me@dmitriy.icu>
2021-03-21 02:18:27 +0500

    Wrapped all definitions into named module

--- types/random-gradient/index.d.ts
+++ types/random-gradient/index.d.ts
@@ -3,10 +3,16 @@
 // Definitions by: dm1sh <https://github.com/dm1sh>
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
 
-declare namespace randomGradient {
-    type GradientDirectionT = "diagonal" | "radial" | "horizontal" | "vertical";
-}
-
-declare function randomGradient(uuid: string, type?: randomGradient.GradientDirectionT): string;
-
-export = randomGradient;
+declare module "random-gradient" {
+    namespace randomGradient {
+      type GradientDirectionT = "diagonal" | "radial" | "horizontal" | "vertical";
+    }
+  
+    function randomGradient(
+      uuid: string,
+      type?: randomGradient.GradientDirectionT
+    ): string;
+  
+    export = randomGradient;
+  }
+  
