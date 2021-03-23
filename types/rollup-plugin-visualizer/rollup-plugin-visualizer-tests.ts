c2dee52aa2387c04fc8985703976e10d985b74cf
Nir Tamir <nirtamir2@gmail.com>
2021-03-23 12:29:20 +0200

    update types for rollup-plugin-visualizer 4.2.2

--- types/rollup-plugin-visualizer/rollup-plugin-visualizer-tests.ts
+++ types/rollup-plugin-visualizer/rollup-plugin-visualizer-tests.ts
@@ -1,13 +1,15 @@
-import visualizer, { PluginVisualizerOptions } from 'rollup-plugin-visualizer';
+import visualizer, { PluginVisualizerOptions } from "rollup-plugin-visualizer";
 
 visualizer(); // $ExpectType Plugin
+visualizer({});
 const fullOptions: PluginVisualizerOptions = {
-    filename: 'filename',
-    title: 'title',
+    filename: "filename",
+    title: "title",
     sourcemap: true,
     open: true,
-    template: '',
-    bundlesRelative: true,
+    template: "sunburst",
+    json: true,
+    gzipSize: true,
+    brotliSize: true,
 };
-visualizer({});
 visualizer(fullOptions);
