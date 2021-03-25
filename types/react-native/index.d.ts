eca2a905ef9e6ca63d2a0f7312b1162beee5a22e
Nelson Sousa <nelsonprsousa@gmail.com>
2021-03-25 12:53:06 +0000

    feat(react-native): add nativeID to image props

--- types/react-native/index.d.ts
+++ types/react-native/index.d.ts
@@ -4031,6 +4031,11 @@ export interface ImagePropsBase extends ImagePropsIOS, ImagePropsAndroid, Access
     testID?: string;
 
     /**
+     * Used to reference react managed images from native code.
+     */
+    nativeID?: string;
+
+    /**
      * A static image to display while downloading the final image off the network.
      */
     defaultSource?: ImageURISource | number;
