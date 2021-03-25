ede4d891ac70bd3ad6cfd04f6c9a67ec5be6c295
TANGUY Antoine <antoine.tanguy@sendinblue.com>
2021-03-25 16:37:05 +0100

    fix: apply PR feedback

--- types/smtp-client/index.d.ts
+++ types/smtp-client/index.d.ts
@@ -7,52 +7,66 @@
 
 export class SMTPClient {
     constructor(options?: SMTPClientOptions);
-    connect(options?: ConnectOptions): Promise<void>;
-    authPlain(options?: AuthPlainOptions): Promise<void>;
     authLogin(options?: AuthLoginOptions): Promise<void>;
-    close(options?: AuthLoginOptions): Promise<void>;
-    greet(options?: GreetOptions): Promise<void>;
-    mail(options?: MailOptions): Promise<void>;
-    rcpt(options?: RcptOptions): Promise<void>;
+    authPlain(options?: AuthPlainOptions): Promise<void>;
+    close(options?: TimeoutOptions): Promise<void>;
+    connect(options?: TimeoutOptions): Promise<void>;
     data(data: string | Buffer, options?: DataOptions): Promise<void>;
-    quit(options?: QuitOptions): Promise<void>;
+    mail(options?: MailOptions): Promise<void>;
+    quit(options?: TimeoutOptions): Promise<void>;
+    rcpt(options?: RcptOptions): Promise<void>;
+    greet(options?: GreetOptions): Promise<void>;
+    helo(options?: HeloOptions): Promise<void>;
+    ehlo(options?: EhloOptions): Promise<void>;
+    hasExtension(extensions: string[]): boolean;
+    getDataSizeLimit(): number;
+    getAuthMechanisms(): string[];
+    parseEnhancedReplyCode(line: string): string | null;
+    parseReplyText(line: string): string;
+    noop(options?: TimeoutOptions): Promise<void>;
+    secure(options?: TimeoutOptions): Promise<void>;
+}
+
+export interface TimeoutOptions {
+    timeout?: number;
+}
+
+export interface HeloOptions extends TimeoutOptions {
+    hostname?: string;
+}
+
+export interface EhloOptions extends TimeoutOptions {
+    hostname?: string;
 }
 
 export interface SMTPClientOptions {
     host?: string;
     port?: number;
 }
-export interface ConnectOptions {
-    timeout?: number;
-}
-export interface AuthPlainOptions {
+
+export interface AuthPlainOptions extends TimeoutOptions {
     username?: string;
     password?: string;
 }
-export interface AuthLoginOptions {
+
+export interface AuthLoginOptions extends TimeoutOptions {
     username?: string;
     password?: string;
 }
-export interface CloseOptions {
-    timeout?: number;
-}
-export interface GreetOptions {
+
+export interface GreetOptions extends TimeoutOptions {
     hostname?: string;
-    timeout?: number;
 }
-export interface MailOptions {
+
+export interface MailOptions extends TimeoutOptions {
     from?: string;
-    timeout?: number;
     utf8?: boolean;
 }
-export interface RcptOptions {
+
+export interface RcptOptions extends TimeoutOptions {
     to?: string;
-    timeout?: number;
 }
-export interface DataOptions {
+
+export interface DataOptions extends TimeoutOptions {
     sourceSize?: number;
-    timeout?: number;
-}
-export interface QuitOptions {
-    timeout?: number;
 }
