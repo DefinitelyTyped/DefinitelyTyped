0f0fa156cd48d74f822996f02af8263fb5605687
JJPell <13407273+JJPell@users.noreply.github.com>
2021-03-25 20:06:37 +0000

    Defined AdSchedule

---
+++ types/google-ads-scripts/ads-app/targeting/ad-schedules/ad-schedule.d.ts
@@ -0,0 +1,84 @@
+// Type definitions for Google Ads Scripts
+// Project: https://developers.google.com/google-ads/scripts
+// Definitions by: JJPell <https://github.com/JJPell>
+// Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
+
+/// <reference path="../../../base.d.ts" />
+/// <reference path="../../campaigns/campaign.d.ts" />
+/// <reference path="../../shopping/campaigns/shopping-campaign.d.ts" />
+
+declare namespace GoogleAdsScripts {
+    namespace AdsApp {
+        /** Represents one period of a Google Ads ad schedule. */
+        interface AdSchedule extends Base.StatsFor {
+            /** Returns the base campaign to which this ad schedule belongs. */
+            getBaseCampaign(): Campaign;
+            /** Returns the bid modifier for this ad schedule. */
+            getBidModifier(): number;
+            /** Returns the campaign to which this ad schedule belongs or null if it does not belong to a search or display campaign. */
+            getCampaign(): Campaign;
+            /** Returns the campaign type. */
+            getCampaignType(): string;
+            /** Returns the day of week this ad schedule applies to. */
+            getDayOfWeek(): string;
+            /** Returns the ending hour of this ad schedule. */
+            getEndHour(): number;
+            /** Returns the ending minute of this ad schedule. */
+            getEndMinute(): number;
+            /** Returns the type of this entity as a String, in this case, "AdSchedule". */
+            getEntityType(): string;
+            /** Returns the ID of the ad schedule. */
+            getId(): number;
+            /** Returns the shopping campaign to which this ad schedule belongs or null if it does not belong to a shopping campaign. */
+            getShoppingCampaign(): ShoppingCampaign;
+            /** Returns the starting hour of this ad schedule. */
+            getStartHour(): number;
+            /** Returns the starting minute of this ad schedule. */
+            getStartMinute(): number;
+            /** Returns the video campaign to which this ad schedule belongs or null if it does not belong to a video campaign. */
+            getVideoCampaign(): VideoCampaign;
+            /** Removes the ad schedule. */
+            remove(): void;
+            /** Sets the bid modifier for this ad schedule. */
+            setBidModifier(modifier: number): void;
+        }
+
+        /**
+         * An iterator of ad schedules.
+         *
+         * Typical usage:
+         *
+         *      while (adScheduleIterator.hasNext()) {
+         *        var adSchedule = adScheduleIterator.next();
+         *      }
+         */
+        interface AdScheduleIterator extends Base.Iterator<AdSchedule> {}
+
+        /** An operation representing creation of a new ad schedule. */
+        interface AdScheduleOperation extends Base.Operation<AdSchedule> {}
+
+        /**
+         * Fetches ad schedules. Supports filtering and sorting.
+         * 
+         * Typical usage:
+         * 
+         *      var adScheduleSelector = AdsApp.targeting()
+         *          .adSchedules()
+         *          .withCondition("Impressions > 100")
+         *          .forDateRange("LAST_MONTH")
+         *          .orderBy("Clicks DESC");
+         * 
+         *      var adScheduleIterator = adScheduleSelector.get();
+         *      while (adScheduleIterator.hasNext()) {
+         *        var adSchedule = adScheduleIterator.next();
+         *      }
+         */
+        interface AdScheduleSelector
+            extends Base.Selector<AdSchedule>,
+                Base.SelectorForDateRange,
+                Base.SelectorOrderBy,
+                Base.SelectorWithCondition,
+                Base.SelectorWithIds,
+                Base.SelectorWithLimit {}
+    }
+}
