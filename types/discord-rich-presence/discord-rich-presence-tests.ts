ed687ab8f961e3a7cc01de1b4ac333ca4691d7f7
Sv443 <sven.fehler@web.de>
2021-03-29 17:59:35 +0200

    added type definitions for "discord-rich-presence"

---
+++ types/discord-rich-presence/discord-rich-presence-tests.ts
@@ -0,0 +1,19 @@
+import DiscordRPC from "./";
+
+const client = DiscordRPC("180984871685062656");
+
+client.updatePresence({
+    state: "slithering",
+    details: "🐍",
+    startTimestamp: Date.now(),
+    endTimestamp: Date.now() + 1337,
+    largeImageKey: "snek_large",
+    smallImageKey: "snek_small",
+});
+
+client.on("error", err => console.log(`Error: ${err}`));
+
+client.on("connected", () => {
+    console.log("Connected.");
+    client.disconnect();
+});
