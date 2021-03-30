29658dfd80ff4b2a9bf52dd97d7e40c7d0899975
Piotr Błażejewicz <peterblazejewicz@users.noreply.github.com>
2021-03-30 22:30:51 +0200

    fix(headroom.js): export inline types properly
    - correct namespaced export
    - version bump
    
    Thanks!

--- types/headroom.js/headroom.js-tests.ts
+++ types/headroom.js/headroom.js-tests.ts
@@ -1,28 +1,29 @@
-import Headroom = require('headroom.js');
+import { HeadroomOptions } from "headroom.js";
+import Headroom = require("headroom.js");
 
-new Headroom(document.getElementById('siteHead')!);
+new Headroom(document.getElementById("siteHead")!);
 
-new Headroom(document.getElementsByClassName('siteHead')[0]);
+new Headroom(document.getElementsByClassName("siteHead")[0]);
 
-new Headroom(document.getElementsByClassName('siteHead')[0], {
+new Headroom(document.getElementsByClassName("siteHead")[0], {
     tolerance: 34,
 });
 
-new Headroom(document.getElementsByClassName('siteHead')[0], {
+new Headroom(document.getElementsByClassName("siteHead")[0], {
     classes: {
-        bottom: 'headroom--bottom',
-        frozen: 'headeroom--frozen',
-        initial: 'headroom',
-        notBottom: 'headroom--not-bottom',
-        notTop: 'headeroom--not-top',
-        pinned: 'headeroom--pinned',
-        top: 'headroom--top',
-        unpinned: 'headroom--unpinned',
+        bottom: "headroom--bottom",
+        frozen: "headeroom--frozen",
+        initial: "headroom",
+        notBottom: "headroom--not-bottom",
+        notTop: "headeroom--not-top",
+        pinned: "headeroom--pinned",
+        top: "headroom--top",
+        unpinned: "headroom--unpinned",
     },
     offset: 500,
 });
 
-const header = document.querySelector('header')!;
+const header = document.querySelector("header")!;
 
 const headroom = new Headroom(header);
 headroom.init();
@@ -31,3 +32,5 @@ headroom.pin();
 headroom.unpin();
 headroom.unfreeze();
 headroom.destroy();
+Headroom.options; // $ExpectType HeadroomOptions
+const options: Partial<HeadroomOptions> = { ...{}, ...Headroom.options };
