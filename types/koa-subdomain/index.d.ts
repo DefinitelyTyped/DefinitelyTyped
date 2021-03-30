30c4ef9e83aeddfe89341412cec6d13819290177
J. N <noehles@easybill.de>
2021-03-30 16:51:11 +0200

    Use template of npx dts-gen

--- types/koa-subdomain/index.d.ts
+++ types/koa-subdomain/index.d.ts
@@ -1,8 +1,7 @@
 // Type definitions for koa-subdomain 3.0
-// Project: https://github.com/keenwon/koa-subdomain
+// Project: https://github.com/keenwon/koa-subdomain#readme
 // Definitions by: Jan Nöhles <https://github.com/BolZer>
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
-// Minimum TypeScript Version: 3.0
 
 import { Middleware } from "koa";
 import { IMiddleware } from "koa-router";
