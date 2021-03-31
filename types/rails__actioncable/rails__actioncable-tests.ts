01b75ee6b6e10e26a4ab324e96596f8ff9736e55
aki <lala.akira@gmail.com>
2021-04-01 08:16:01 +0900

    feat(rails__actioncable): Support for enabled property of logger

--- types/rails__actioncable/rails__actioncable-tests.ts
+++ types/rails__actioncable/rails__actioncable-tests.ts
@@ -9,6 +9,7 @@ import {
     Subscriptions,
     MessageTypes,
     DisconnectReasons,
+    logger,
 } from '@rails/actioncable';
 
 INTERNAL.message_types.welcome;
@@ -22,6 +23,7 @@ INTERNAL.disconnect_reasons.invalid_request;
 INTERNAL.disconnect_reasons.server_restart;
 
 createWebSocketURL('url'); // $ExpectType string
+logger.enabled = true;
 
 /**
  * Consumer
