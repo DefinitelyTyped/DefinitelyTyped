5ea375c1c1c6f91e7fd8eedb283f8e3984d9b0cd
Josh Goebel <me@joshgoebel.com>
2021-03-31 16:40:01 -0400

    fix url

--- types/highlight.js/index.d.ts
+++ types/highlight.js/index.d.ts
@@ -1,5 +1,5 @@
 // Type definitions for highlight.js 10.1
-// Project: https://github.com/isagalaev/highlight.js
+// Project: https://github.com/highlightjs/highlight.js
 // Definitions by: Florian Keller <https://github.com/ffflorian>
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
 // TypeScript Version: 3.5
