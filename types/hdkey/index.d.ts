9886444c8a8382f1e6758b9c9c220876519685ac
J. Alberto <45895462+JAlbertoGonzalez@users.noreply.github.com>
2021-03-31 23:20:34 +0200

    update types for v2.0

--- types/hdkey/index.d.ts
+++ types/hdkey/index.d.ts
@@ -1,7 +1,8 @@
-// Type definitions for hdkey 0.7
+// Type definitions for hdkey 2.0
 // Project: https://github.com/cryptocoinjs/hdkey
 // Definitions by: Leonid Logvinov <https://github.com/LogvinovLeon>
 //                 Tvrtko Majstorovic <https://github.com/TvrtkoM>
+//                 Alberto Torre <https://github.com/JAlbertoGonzalez>
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
 
 /// <reference types="node"/>
@@ -15,5 +16,11 @@ declare class HDNode {
     derive(path: string): HDNode;
     toJSON(): { xpriv: string; xpub: string };
     static fromJSON(obj: { xpriv: string; xpub: string }): HDNode;
+    static fromExtendedKey(xpriv: string): HDNode;
+    sign(hash: Buffer): Buffer;
+    verify(hash: Buffer, signature: Buffer): boolean;
+    wipePrivateData(): HDNode;
+    privateExtendedKey: string;
+    publicExtendedKey: string;
 }
 export = HDNode;
