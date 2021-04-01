686819ef3e64f3ea3c16d7e4cbc8d6af33a6b60a
Andrew Branch <andrewbranch@users.noreply.github.com>
2021-04-01 11:34:39 -0700

    Revert "Correct library misnaming: highlight.js (#52120)"
    This reverts commit 745ec4d9d1cfe5e4020f6607ba2b8dba1fd397f3.

--- types/highlightjs/index.d.ts
+++ types/highlightjs/index.d.ts
@@ -1,6 +1,6 @@
 // Type definitions for highlight.js 10.1
-// Project: https://github.com/highlightjs/highlight.js
-// Definitions by: Florian Imdahl <https://github.com/ffflorian>
+// Project: https://github.com/isagalaev/highlight.js
+// Definitions by: Florian Keller <https://github.com/ffflorian>
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
 // TypeScript Version: 3.5
 
