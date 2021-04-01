9336e44b69c991862958e6e2ed906bca4fcd5996
Jamie Gerrard Lievesley <jamie@jamiegl.co.uk>
2021-04-01 23:24:32 +0100

    react-keyboard-event-handler

---
+++ types/react-keyboard-event-handler/index.d.ts
@@ -0,0 +1,6 @@
+// Type definitions for react-keyboard-event-handler 1.5
+// Project: https://github.com/linsight/react-keyboard-event-handler
+// Definitions by: Jamie Gerrard Lievesley <https://github.com/jamiegluk>
+// Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
+
+export { default } from "./KeyboardEventHandler";
