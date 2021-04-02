c190ace4d110fb052af7eadd4fe329d95d307167
shuta13 <hollystarsun@gmail.com>
2021-04-02 21:43:02 +0900

    refactor: tab size

--- types/gtag.js/gtag.js-tests.ts
+++ types/gtag.js/gtag.js-tests.ts
@@ -2,32 +2,32 @@ gtag('config', 'GA-TRACKING_ID');
 gtag('config', 'GA-TRACKING_ID', { send_page_view: false });
 
 gtag('event', 'login', {
-    method: 'Google',
+  method: 'Google',
 });
 
 gtag('set', 'user_properties', {
-    favorite_composer: 'Mahler',
-    favorite_instrument: 'double bass',
-    season_ticketholder: 'true',
+  favorite_composer: 'Mahler',
+  favorite_instrument: 'double bass',
+  season_ticketholder: 'true',
 });
 gtag('set', { currency: 'USD' });
 gtag('js', new Date());
 gtag('set', {
-    country: 'US',
-    currency: 'USD',
+  country: 'US',
+  currency: 'USD',
 });
 
 gtag('get', 'GA-TRACKING_ID', 'client_id', _clientId => {});
 
 gtag('consent', 'default', {
-    ad_storage: 'denied',
-    analytics_storage: 'denied',
-    wait_for_update: 500,
+  ad_storage: 'denied',
+  analytics_storage: 'denied',
+  wait_for_update: 500,
 });
 gtag('consent', 'default', {
-    ad_storage: 'denied',
-    region: ['ES', 'US-AK'],
+  ad_storage: 'denied',
+  region: ['ES', 'US-AK'],
 });
 gtag('consent', 'default', {
-    analytics_storage: 'denied',
+  analytics_storage: 'denied',
 });
