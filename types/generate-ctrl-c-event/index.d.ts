76c68c978373d8400271bcf5b2dcb250dc07c8a2
Khairul Azhar Kasmiran <kazarmy@gmail.com>
2021-04-02 19:20:22 +0800

    Add definitions

---
+++ types/generate-ctrl-c-event/index.d.ts
@@ -0,0 +1,17 @@
+// Type definitions for generate-ctrl-c-event 1.2
+// Project: https://github.com/zenflow/generate-ctrl-c-event
+// Definitions by: Matthew Francis Brunetti <https://github.com/zenflow>
+//                 Khairul Azhar Kasmiran <https://github.com/kazarmy>
+// Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
+
+declare namespace GenerateCtrlCEvent {
+  interface Funcs {
+    generateCtrlC(): boolean;
+    generateCtrlCAsync(): Promise<boolean>;
+    generateCtrlBreak(dwProcessGroupId?: number): boolean;
+    generateCtrlBreakAsync(dwProcessGroupId?: number): Promise<boolean>;
+  }
+}
+
+declare var funcs: GenerateCtrlCEvent.Funcs;
+export = funcs;
