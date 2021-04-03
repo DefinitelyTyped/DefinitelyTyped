b09238c715c302eaa9abeebde2f16c93a9b7b7ae
Philippe Beaudoin <philippe.beaudoin@gmail.com>
2021-04-03 19:22:49 -0400

    Update react-native-share-menu for 5.0 by @PhilBeaudoin

--- types/react-native-share-menu/index.d.ts
+++ types/react-native-share-menu/index.d.ts
@@ -1,12 +1,35 @@
-// Type definitions for react-native-share-menu 2.2
+// Type definitions for react-native-share-menu 5.0
 // Project: https://github.com/meedan/react-native-share-menu#readme
-// Definitions by: Haseeb Majid <https://github.com/hmajid2301>
+// Definitions by: Philippe Beaudoin <https://github.com/PhilBeaudoin>
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
+// Minimum TypeScript Version: 3.7
+
+export interface ShareData {
+    mimeType: string;
+    data: string;
+    extraData?: object;
+}
+
+export type ShareCallback = (share?: ShareData) => void;
+
+export interface ShareListener {
+    remove(): void;
+}
 
 interface ShareMenu {
-    getSharedText(callback: (text: string) => void): void;
+    getSharedText(callback: ShareCallback): void;
+    getInitialShare(callback: ShareCallback): void;
+    addNewShareListener(callback: ShareCallback): ShareListener;
     clearSharedText(): void;
 }
 
-export const ShareMenu: ShareMenu;
+interface ShareMenuReactView {
+    dismissExtension(error?: string): void;
+    openApp(): void;
+    continueInApp(extraData?: object): void;
+    data(): Promise<ShareData>;
+}
+
+export const ShareMenuReactView: ShareMenuReactView;
+declare const ShareMenu: ShareMenu;
 export default ShareMenu;
