a7c7da0c0643fe463413769215264411b23f8688
nokazn <nokazn@gmail.com>
2021-04-05 00:41:38 +0900

    Add new types for just-merge

---
+++ types/just-merge/index.d.ts
@@ -0,0 +1,8 @@
+// Type definitions for just-merge 1.1
+// Project: https://github.com/angus-c/just#readme
+// Definitions by: nokazn <https://github.com/nokazn>
+// Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
+
+declare function merge(obj1: object, ...objs: object[]): object;
+
+export = merge;
