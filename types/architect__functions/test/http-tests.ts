de6c49b7c54ba61aa3f34f38628422d9efc06651
Jake Champion <me@jakechampion.name>
2021-04-05 13:06:31 +0100

    Fix errors found by linting

--- types/architect__functions/test/http-tests.ts
+++ types/architect__functions/test/http-tests.ts
@@ -27,14 +27,14 @@ app.get('/cool', (req, res) => res.send('very cool'));
 
 ////////////////////
 // tests for arc.http.helpers: https://github.com/architect/functions/blob/master/src/http/index.js#L21-L26
-async function handlersession(req: arc.HttpRequest): Promise<arc.HttpResponse | undefined> {
+async function handlerhelpers(req: arc.HttpRequest): Promise<arc.HttpResponse | undefined> {
     req = arc.http.helpers.interpolate(req);
     const data = arc.http.helpers.bodyParser(req);
     const image = arc.http.helpers.static('test.jpg');
     const description = arc.http.helpers.static('test.txt', {stagePath: '/staging'});
     return {
         statusCode: 307,
-        headers: { 'location': arc.http.helpers.url('test.txt') },
+        headers: { location: arc.http.helpers.url('test.txt') },
     };
 }
 
