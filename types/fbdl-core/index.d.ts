7c8b5cec5b42c79beb535bdbd29ebccb54971a23
Yoshida Tomio <75403863+1chiSensei@users.noreply.github.com>
2021-04-05 14:55:14 +0800

    fix

--- types/fbdl-core/index.d.ts
+++ types/fbdl-core/index.d.ts
@@ -5,76 +5,75 @@
 
 /// <reference types="node" />
 
-import { Readable } from 'stream';
+import { Readable } from "stream";
 
-interface Response {
-    name: string;
-    title: string;
-    description: string;
-    rawVideo: string;
-    thumbnail: string;
-    uploadedAt: Date;
-    duration: string;
-    interactionCount: number;
-    streamURL: string;
-    publishedAt: Date;
-    width: number;
-    height: number;
-    nsfw: boolean;
-    genre: string;
-    keywords: any[];
-    comments: number;
-    size: string;
-    quality: string;
-    author: {
-        type: string;
+declare namespace FBDL {
+    interface Response {
         name: string;
+        title: string;
+        description: string;
+        rawVideo: string;
+        thumbnail: string;
+        uploadedAt: Date;
+        duration: string;
+        interactionCount: number;
+        streamURL: string;
+        publishedAt: Date;
+        width: number;
+        height: number;
+        nsfw: boolean;
+        genre: string;
+        keywords: any[];
+        comments: number;
+        size: string;
+        quality: string;
+        author: {
+            type: string;
+            name: string;
+            url: string;
+        };
+        publisher: {
+            type: string;
+            name: string;
+            url: string;
+            avatar: string;
+        };
         url: string;
-    };
-    publisher: {
-        type: string;
-        name: string;
-        url: string;
-        avatar: string;
-    };
-    url: string;
-    reactions: {
-        total: number;
-        like: number;
-        love: number;
-        care: number;
-        wow: number;
-        haha: number;
-        sad: number;
-        angry: number;
-    };
-    shares: string;
-    views: string;
+        reactions: {
+            total: number;
+            like: number;
+            love: number;
+            care: number;
+            wow: number;
+            haha: number;
+            sad: number;
+            angry: number;
+        };
+        shares: string;
+        views: string;
+    }
 }
 
-declare class FBDL {
+declare const FBDL: {
     /**
      * Validates facebook url
      */
-    static validateURL(url: string): boolean;
+    validateURL: (url: string) => boolean;
 
     /**
      * Downloads facebook video
      */
-    static download(url: string): Promise<Readable>;
+    download: (url: string) => Promise<Readable>;
 
     /**
      * Fetches facebook video info
      */
-    static getInfo(url: string): Promise<Response | null>;
+    getInfo: (url: string) => Promise<FBDL.Response | null>;
 
     /**
      * Parses time in ms
      */
-    static parseTime(duration: string): string;
-
-    /** @ignore */
-    private _parseHTML(url: string): Promise<string>;
-}
+    parseTime: (duration: string) => string;
+};
 
 export = FBDL;
