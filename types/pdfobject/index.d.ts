3df818682829a4db457555b56a2005c1e2be0b2b
Piotr Błażejewicz <peterblazejewicz@users.noreply.github.com>
2021-04-06 21:26:34 +0200

    update(pdfoject): v2.2
    - bump version
    - update module definition (flatten it down)
    - add support for UMD usage
    - wrap options into proper interface
    - add maintainer
    
    https://unpkg.com/pdfobject@2.2.5/pdfobject.js
    
    Thanks!

--- types/pdfobject/index.d.ts
+++ types/pdfobject/index.d.ts
@@ -1,16 +1,25 @@
-// Type definitions for PDFObject v2.0.201604172
+// Type definitions for PDFObject 2.2
 // Project: https://github.com/pipwerks/PDFObject
 // Definitions by: Niels Boogaard <https://github.com/nielsboogaard>
+//                 Piotr Błażejewicz <https://github.com/peterblazejewicz>
 // Definitions: https://github.com/borisyankov/DefinitelyTyped
+export as namespace PDFObject;
+export function embed(url: string, target?: any, options?: Options): HTMLElement;
+export const pdfobjectversion: "2.2.3";
+export const supportsPDFs: boolean;
 
-declare module "pdfobject" {
-    interface PDFObject {
-        embed(url:string, target?:any, options?:any): HTMLElement;
-        pdfobjectversion: string;
-        supportsPDFs: boolean;
-    }
-
-    var pdfObject: PDFObject;
-    export = pdfObject;
-
+export interface Options {
+    id?: string;
+    page?: boolean;
+    pdfOpenParams?: Record<string, string | number | boolean>;
+    fallbackLink?: boolean;
+    width?: string;
+    height?: string;
+    assumptionMode?: boolean;
+    forcePDFJS?: boolean;
+    supportRedirect?: boolean;
+    omitInlineStyles?: boolean;
+    suppressConsole?: boolean;
+    forceIframe?: boolean;
+    PDFJS_URL?: string;
 }
