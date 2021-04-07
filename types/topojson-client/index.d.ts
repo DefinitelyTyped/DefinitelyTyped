450da49728d8c3f95891732b234cbfaf1326e3d4
Ricardo Mello <rimello@squaretrade.com>
2021-04-07 11:20:31 -0300

    change my github name in definitions

--- types/topojson-client/index.d.ts
+++ types/topojson-client/index.d.ts
@@ -1,7 +1,7 @@
 // Type definitions for topojson-client 3.0
 // Project: https://github.com/topojson/topojson-client
 // Definitions by: denisname <https://github.com/denisname>
-//                 Ricardo Mello <https://github.com/ricardo-mello>
+//                 Ricardo Mello <https://github.com/ricmello>
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
 // TypeScript Version: 2.5
 
