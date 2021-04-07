892864b9beeb80475ce172cff4b971e31dd36a4b
Yoshida Tomio <mail@1chi.tk>
2021-04-07 08:12:30 +0800

    Update fbdl-core-tests.ts

--- types/fbdl-core/fbdl-core-tests.ts
+++ types/fbdl-core/fbdl-core-tests.ts
@@ -1,5 +1,6 @@
 import fbdl = require('fbdl-core');
+import { Readable } = require('stream');
 
-fbdl.download('https://www.facebook.com/alanwalkermusic/videos/277641643524720'); // $ExpectType Promise<internal.Readable>
+fbdl.download('https://www.facebook.com/alanwalkermusic/videos/277641643524720'); // $ExpectType Promise<Readable>
 
 fbdl.getInfo('https://example.com'); // $ExpectError
