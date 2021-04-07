79b71e9b2a5fadc45c7c63ab0b2199261c6b8b62
danielfx90 <daniel@wibson.org>
2021-04-07 14:18:36 -0300

    Revert of prettier formatting on Levelup

--- types/levelup/levelup-tests.ts
+++ types/levelup/levelup-tests.ts
@@ -1,5 +1,5 @@
-import levelup from "levelup";
-import { AbstractLevelDOWN } from "abstract-leveldown";
+import levelup from 'levelup';
+import { AbstractLevelDOWN } from 'abstract-leveldown';
 
 interface StringEncoding {
     encode(val: any): string;
@@ -10,50 +10,45 @@ interface StringEncoding {
 
 declare const stringEncoding: StringEncoding;
 
-const db = levelup(new AbstractLevelDOWN("here"), {
+const db = levelup(new AbstractLevelDOWN('here'), {
     keyEncoding: stringEncoding,
-    valueEncoding: stringEncoding,
+    valueEncoding: stringEncoding
 });
 
 db.open();
 db.close();
-db.open(error => {});
+db.open((error) => {
+});
 
-db.close(error => {});
+db.close((error) => {
+});
 
 db.put("key", {});
-db.put("key", {}, error => {});
-db.put("key", {}, { sync: true }, error => {});
+db.put("key", {}, (error) => {});
+db.put("key", {}, { sync: true}, (error) => {});
 
-db.get("key", { keyEncoding: "json" }, (error, val) => {});
-db.get("key", { fillCache: true }, (error, val) => {});
+db.get("key", {keyEncoding: "json"}, (error, val) => {});
+db.get("key", {fillCache: true}, (error, val) => {});
 db.get("key", (error, val) => {});
 
 db.del("key");
-db.del("key", error => {});
-db.del("key", { keyEncoding: "json" }, error => {});
-db.del("key", { sync: true }, error => {});
+db.del("key", (error) => {});
+db.del("key", {keyEncoding: "json"}, (error) => {});
+db.del("key", {sync: true}, (error) => {});
 
-db.batch(
-    [
-        {
-            type: "put",
-            key: [1, 2, 3],
-            value: { some: "json" },
-        },
-    ],
-    (error: Error | undefined) => {},
-);
+db.batch([{
+    type          : 'put'
+    , key           : ([1, 2, 3])
+    , value         : { some: 'json' }
+}], (error: Error | undefined) => {});
 
 db.batch()
-    .del("father")
-    .put("name", "Yuri Irsenovich Kim")
-    .put("dob", "16 February 1941")
-    .put("spouse", "Kim Young-sook")
-    .put("occupation", "Clown")
-    .write(() => {
-        console.log("Done!");
-    });
+    .del('father')
+    .put('name', 'Yuri Irsenovich Kim')
+    .put('dob', '16 February 1941')
+    .put('spouse', 'Kim Young-sook')
+    .put('occupation', 'Clown')
+    .write(() => { console.log('Done!'); });
 
 // $ExpectType boolean
 db.isOpen();
@@ -61,21 +56,23 @@ db.isOpen();
 db.isClosed();
 
 db.createReadStream()
-    .on("data", (data: any) => {
-        console.log(data.key, "=", data.value);
+    .on('data', (data: any) => {
+      console.log(data.key, '=', data.value);
     })
-    .on("error", (err: any) => {
-        console.log("Oh my!", err);
+    .on('error', (err: any) => {
+      console.log('Oh my!', err);
     })
-    .on("close", () => {
-        console.log("Stream closed");
+    .on('close', () => {
+      console.log('Stream closed');
     })
-    .on("end", () => {
-        console.log("Stream closed");
+    .on('end', () => {
+      console.log('Stream closed');
     });
 
-db.clear(error => {});
+db.clear((error) => {
+});
 
-db.clear({ gt: "hello" }, error => {});
+db.clear({ gt: 'hello' }, (error) => {
+});
 
-db.clear().then(() => console.log("cleared"));
+db.clear().then(() => console.log('cleared'));
