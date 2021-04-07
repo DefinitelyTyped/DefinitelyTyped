71d93fc68d71c5e302b0776f4e1c5ba8991f0943
danielfx90 <daniel@wibson.org>
2021-04-06 23:14:31 -0300

    Prettier formatting applied. Some changes required done

--- types/level/index.d.ts
+++ types/level/index.d.ts
@@ -3,16 +3,22 @@
 // Definitions by: danielfx90 <https://github.com/danielfx90>
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
 
-import { AbstractOptions, ErrorCallback } from 'abstract-leveldown';
+import { AbstractOptions, ErrorCallback } from "abstract-leveldown";
 
-import EncodingDown from 'encoding-down';
+import EncodingDown from "encoding-down";
 
-import { LevelUp } from 'levelup';
+import { LevelUp } from "levelup";
 
-export interface LevelDB<K = any, V = any> extends LevelUp<EncodingDown<K, V>> {
-  errors: /* typeof levelerrors */ any; // ? level-errors is not in DT
+declare namespace Level {
+    interface LevelDB<K = any, V = any> extends LevelUp<EncodingDown<K, V>> {
+        errors: /* typeof levelerrors */ any; // ? level-errors is not in DT
+    }
+    interface Constructor {
+        // tslint:disable-next-line:no-unnecessary-generics
+        <K = any, V = any>(location: string, options?: AbstractOptions, callback?: ErrorCallback): LevelDB<K, V>;
+    }
 }
 
-declare function level(location: string, options?: AbstractOptions, callback?: ErrorCallback): LevelDB;
+declare const Level: Level.Constructor;
 
-export = level;
+export = Level;
--- types/level/level-tests.ts
+++ types/level/level-tests.ts
@@ -1,24 +1,23 @@
-import level = require('level');
-import { LevelDB } from 'level';
+import level = require("level");
 
-const testString = (db: LevelDB<string, string>) => {
-  db.put("key1", "value");
-  db.put("key2", "value", { something: true }, (err?: Error) => { });
+const testString = (db: level.LevelDB<string, string>) => {
+    db.put("key1", "value");
+    db.put("key2", "value", { something: true }, (err?: Error) => {});
 
-  db.get("key1", (err?: Error) => { });
-  db.get("key2", { something: true }, (err: Error | undefined, value: any) => { });
+    db.get("key1", (err?: Error) => {});
+    db.get("key2", { something: true }, (err: Error | undefined, value: any) => {});
 };
 
-const testNumber = (db: LevelDB<number, string>) => {
-  db.put(1, "value");
-  db.put(2, "value", { something: true }, (err?: Error) => { });
+const testNumber = (db: level.LevelDB<number, string>) => {
+    db.put(1, "value");
+    db.put(2, "value", { something: true }, (err?: Error) => {});
 
-  db.get(1, (err?: Error) => { });
-  db.get(2, { something: true }, (err: Error | undefined, value: any) => { });
+    db.get(1, (err?: Error) => {});
+    db.get(2, { something: true }, (err: Error | undefined, value: any) => {});
 };
 
 // $ExpectType void
-testString(level('hereString', { something: true }, (err?: Error) => { }));
+testString(level("hereString", { something: true }, (err?: Error) => {}));
 
 // $ExpectType void
-testNumber(level('hereNumber', { something: true }, (err?: Error) => { }));
+testNumber(level("hereNumber", { something: true }, (err?: Error) => {}));
