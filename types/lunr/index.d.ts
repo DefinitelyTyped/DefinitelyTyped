c045ba0de9f8722812e8cff34dcee642c946d393
Derek Kershner <dkershner@cbtnuggets.com>
2021-04-07 08:25:55 -0700

    Added to Definitions By

--- types/lunr/index.d.ts
+++ types/lunr/index.d.ts
@@ -1,6 +1,7 @@
 // Type definitions for lunr.js 2.3
 // Project: https://github.com/olivernn/lunr.js, http://lunrjs.com
-// Definitions by: Sean Tan <https://github.com/seantanly>, Andrés Pérez <https://github.com/hokiegeek>
+// Definitions by: Sean Tan <https://github.com/seantanly>, Andrés Pérez <https://github.com/hokiegeek>,
+// Derek Kershner <https://github.com/dkershner6>
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
 // TypeScript Version: 2.3
 
