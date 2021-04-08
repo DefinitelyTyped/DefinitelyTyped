83b1e02b024e1c2f768ff841a25d2da478942c2a
beto <roblo53@hotmail.com>
2021-04-08 20:43:14 -0300

    Update index.d.ts

--- types/leaflet-offline/index.d.ts
+++ types/leaflet-offline/index.d.ts
@@ -6,24 +6,25 @@
 import * as L from 'leaflet';
 import * as LO from 'leaflet-offline';
 declare module 'leaflet' {  
+  export type DoneCallback = (error?: Error, tile?: HTMLElement) => void;
   export class TileLayerOffline {
-    constructor(urlTemplate: string, tilesDb: Object, options?: L.TileLayerOptions);
-    initialize(url: String, tilesDb: Object, options: Object): void;
-    createTile(coords: Object, done: Function): HTMLElement;
-    getTileUrl(coords: Object): String;
-    getTileUrls(bounds: Object, zoom: Number): Array<any>;
+    constructor(urlTemplate: string, tilesDb: object, options?: TileLayerOptions);
+    initialize(url: string, tilesDb: object, options: object): void;
+    createTile(coords: object, done: DoneCallback): HTMLElement;
+    getTileUrl(coords: object): string;
+    getTileUrls(bounds: object, zoom: Number): any[];
   }
-  export function tileLayer(urlTemplate: string, options?: L.TileLayerOptions): L.TileLayer;
+  export function tileLayer(urlTemplate: string, options?: TileLayerOptions): TileLayer;
   export namespace tileLayer {
-    function offline(url: String, tilesDb: Object, options: Object): TileLayerOffline;
+    function offline(url: string, tilesDb: object, options: object): TileLayerOffline;
   }
   export class ControlOffline {
-    constructor(baseLayer: Object, tilesDb: Object, options: Object);
-    initialize (baseLayer: Object, tilesDb: Object, options: Object): void;
-    onAdd(map: Object): HTMLElement;
+    constructor(baseLayer: object, tilesDb: object, options: object);
+    initialize (baseLayer: object, tilesDb: object, options: object): void;
+    onAdd(map: object): HTMLElement;
   }  
   export function control();
   export namespace control {
-    export function offline(baseLayer: Object, tilesDb: Object, options: Object): ControlOffline;
+    export function offline(baseLayer: object, tilesDb: object, options: object): ControlOffline;
   }
 } 
