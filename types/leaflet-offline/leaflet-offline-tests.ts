db5ba615da75358e666e066bf7b5936947fd9ef2
beto <roblo53@hotmail.com>
2021-04-08 19:28:49 -0300

    Update leaflet-offline-tests.ts

--- types/leaflet-offline/leaflet-offline-tests.ts
+++ types/leaflet-offline/leaflet-offline-tests.ts
@@ -1,65 +1,9 @@
-/// <reference path="../../../../../node_modules/@types/leaflet/index.d.ts"/>
-/// <reference path="../../../../../node_modules/@types/leaflet-offline/index.d.ts"/>
-import L from 'leaflet';
+import * as L from 'leaflet';
 import 'leaflet-offline';
 
-@Component({
-  selector: 'app-map-insert',
-  templateUrl: './map-insert.component.html',
-  //styleUrls: ['./map-insert.component.css']
-})
-export class MapInsertComponent {
- 
-  private map: Map;
-  private tilesDb: any;
-  private osmTileLayer;
-  private satelliteTileLayer;
+const map: L.Map = L.map('map-container');
 
-  constructor(
-  ) {
-    this.tilesDb = this.offlineMapStorageService;
-  }
+L.tileLayer('http://{s}.tile.osm.org/{z}/{x}/{y}.png', {
+    attribution: '&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+}).addTo(map);
 
-  initializeTileLayersOffline(): Observable<any> {
-    
-	try {
-      this.osmTileLayer = L.tileLayer.offline('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', this.tilesDb,
-        {
-          attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a>',
-          subdomains: 'abc',
-          minZoom: 13,
-          maxZoom: 19,
-          crossOrigin: true
-        })
-      this.satelliteTileLayer = L.tileLayer.offline('https://server.arcgisonline.com/ArcGIS/rest/services/World_Imagery/MapServer/tile/{z}/{y}/{x}', this.tilesDb,
-        {
-          attribution: 'Tiles © Esri — Source: Esri, i-cubed, USDA, USGS, AEX, GeoEye, Getmapping, Aerogrid, IGN, IGP, UPR-EGP, and the GIS User Community',
-          minZoom: 13,
-          maxZoom: 19,
-          crossOrigin: true
-        })
-  
-      // Define the layer to be shown on the map
-      let selectedLayer = this.osmTileLayer;
-
-      // Add the layer to the map
-      selectedLayer.addTo(this.map);
-
-      // Init layers
-      var baseLayers = {
-        "OSM": this.osmTileLayer,
-        "Satelliet": this.satelliteTileLayer
-      };
-
-      // Add control for switching between layers
-      L.control.layers(baseLayers).addTo(this.map);
-
-    }
-    catch (error) {
-	
-    }
-
-    return of({});
-
-  }
-}
