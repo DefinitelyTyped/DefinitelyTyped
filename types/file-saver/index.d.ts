dc2f48b6936687e9cce52397e31c6fb945fd1941
JounQin <admin@1stg.me>
2021-04-08 12:21:26 +0800

    fix(file-saver): disableAutoBOM is true by default

--- types/file-saver/index.d.ts
+++ types/file-saver/index.d.ts
@@ -27,7 +27,7 @@ declare namespace FileSaver {
      * FileSaver.js implements the saveAs() FileSaver interface in browsers that do not natively support it.
      * @param data - The actual file data blob or URL.
      * @param filename - The optional name of the file to be downloaded. If omitted, the name used in the file data will be used. If none is provided "download" will be used.
-     * @param disableAutoBOM - Optional & defaults to `false`. Set to `true` if you want FileSaver.js to automatically provide Unicode text encoding hints
+     * @param disableAutoBOM - Optional & defaults to `true`. Set to `false` if you want FileSaver.js to automatically provide Unicode text encoding hints
      * @deprecated use `{ autoBom: false }` as the third argument
      */
     // tslint:disable-next-line:unified-signatures
