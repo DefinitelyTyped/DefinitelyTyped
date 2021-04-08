f0071eb95d6015fdd5e6554c48edd03f12894734
Yoshida Tomio <mail@1chi.tk>
2021-04-08 15:14:14 +0800

    Update index.d.ts

--- types/paradisebotsapi.js/index.d.ts
+++ types/paradisebotsapi.js/index.d.ts
@@ -3,12 +3,8 @@
 // Definitions by: Yoshida Tomio <https://github.com/1chiSensei>
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
 
-declare class PBL {
-    constructor(id?: string, auth?: string);
+export class get {
+    constructor(id: string, auth: string);
 
-    post(server_count: number, shard_count: number): Promise<void>;
-    get(botID: string, response: (data: Record<string, unknown>) => void): Promise<void>;
-    getUser(userID: string, response: (data: Record<string, unknown>) => void): Promise<void>;
+    post(server_count: number, shard_count: number): Promise<void>
 }
-
-export = PBL;
