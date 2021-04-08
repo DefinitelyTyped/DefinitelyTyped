b7d8c96d911c39a4c360faf6505fc626b8259870
beto <roblo53@hotmail.com>
2021-04-08 20:19:45 -0300

    Update index.d.ts

--- types/leaflet-offline/index.d.ts
+++ types/leaflet-offline/index.d.ts
@@ -3,12 +3,9 @@
 // Definitions by: roblo53@hotmail.com <https://github.com/BETOXL>
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
 // TypeScript Version: 2.5
-
 import * as L from 'leaflet';
 import * as LO from 'leaflet-offline';
-
-declare module 'leaflet' {
-    
+declare module 'leaflet' {  
   export class TileLayerOffline {
     constructor(urlTemplate: string, tilesDb: Object, options?: L.TileLayerOptions);
     initialize(url: String, tilesDb: Object, options: Object): void;
@@ -16,23 +13,17 @@ declare module 'leaflet' {
     getTileUrl(coords: Object): String;
     getTileUrls(bounds: Object, zoom: Number): Array<any>;
   }
-    
   export function tileLayer(urlTemplate: string, options?: L.TileLayerOptions): L.TileLayer;
-    
   export namespace tileLayer {
     function offline(url: String, tilesDb: Object, options: Object): TileLayerOffline;
   }
-      
   export class ControlOffline {
     constructor(baseLayer: Object, tilesDb: Object, options: Object);
     initialize (baseLayer: Object, tilesDb: Object, options: Object): void;
     onAdd(map: Object): HTMLElement;
-  }
-      
+  }  
   export function control();
-      
   export namespace control {
     export function offline(baseLayer: Object, tilesDb: Object, options: Object): ControlOffline;
   }
-      
 } 
