2fc5aac62bd525ed8cf8b0c44c77397b61b980b0
beto <roblo53@hotmail.com>
2021-04-08 22:02:38 -0300

    fix

--- types/leaflet-offline/index.d.ts
+++ types/leaflet-offline/index.d.ts
@@ -5,22 +5,24 @@
 // TypeScript Version: 2.3
 import * as L from 'leaflet';
 declare module 'leaflet' {
-  class TileLayerOffline {
-    constructor(urlTemplate: string, tilesDb: object, options?: TileLayerOptions);
-    initialize(url: string, tilesDb: object, options: object): void;
-    createTile(coords: object, done: DoneCallback): HTMLElement;
-    getTileUrl(coords: object): string;
-    getTileUrls(bounds: object, zoom: number): any[];
-  }
-  namespace tileLayer {
-    function offline(url: string, tilesDb: object, options: object): TileLayerOffline;
-  }
-  class ControlOffline {
-    constructor(baseLayer: object, tilesDb: object, options: object);
-    initialize (baseLayer: object, tilesDb: object, options: object): void;
-    onAdd(map: object): HTMLElement;
-  }
-  namespace control {
-    function offline(baseLayer: object, tilesDb: object, options: object): ControlOffline;
-  }
+    class TileLayerOffline {
+        constructor(urlTemplate: string, tilesDb: object, options?: TileLayerOptions);
+        initialize(url: string, tilesDb: object, options: object): void;
+        createTile(coords: object, done: DoneCallback): HTMLElement;
+        getTileUrl(coords: object): string;
+        getTileUrls(bounds: object, zoom: number): any[];
+    }
+    function tileLayer(urlTemplate: string, options?: TileLayerOptions): TileLayer;
+    namespace tileLayer {
+        function offline(url: string, tilesDb: object, options: object): TileLayerOffline;
+    }
+    class ControlOffline {
+        constructor(baseLayer: object, tilesDb: object, options: object);
+        initialize(baseLayer: object, tilesDb: object, options: object): void;
+        onAdd(map: object): HTMLElement;
+    }
+    function control();
+    namespace control {
+        function offline(baseLayer: object, tilesDb: object, options: object): ControlOffline;
+    }
 }
