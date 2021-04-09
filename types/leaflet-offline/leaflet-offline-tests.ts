fa0a8a0b97acc99d078aaa88275f9acf5a7485e5
beto <roblo53@hotmail.com>
2021-04-08 22:08:53 -0300

    Update leaflet-offline-tests.ts

--- types/leaflet-offline/leaflet-offline-tests.ts
+++ types/leaflet-offline/leaflet-offline-tests.ts
@@ -1,9 +1,6 @@
 import * as L from 'leaflet';
 import 'leaflet-offline';
-
 const map: L.Map = L.map('map-container');
-
 L.tileLayer('http://{s}.tile.osm.org/{z}/{x}/{y}.png', {
     attribution: '&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
 }).addTo(map);
-
