489c100396ca8ca60fd333c607ba139110771b82
Krisztian Balla <k.balla@gmx.at>
2021-04-10 12:08:40 +0200

    Update tests

--- types/jbinary/jbinary-tests.ts
+++ types/jbinary/jbinary-tests.ts
@@ -1,22 +1,76 @@
-import jBinary = require('jbinary');
+import * as fs from "fs";
+import jBinary = require("jbinary");
 
-jBinary.loadData([0x05, 0x03, 0x7F, 0x1E]);
-jBinary.load([0x05, 0x03, 0x7F, 0x1E]);
+// #region Loading data
+(function () {
+    const title = '"loadData" (with Callback and file path)';
+    jBinary.loadData("./test.bin", (err, data) => {
+        if (err) {
+            console.log(`${title} => Error reading file: ${err.message}`);
+        } else if (data) {
+            console.log(`${title} => Loaded ${data.length} bytes of data.`);
+        } else {
+            console.log(`${title} => No error and no data.`);
+        }
+    });
+})();
 
-var originalData = [0x05, 0x03, 0x7F, 0x1E];
-var b1 = new jBinary(originalData);
-console.log(b1.readAll());
-console.log(b1.read('int8'));
+(function () {
+    const title = '"loadData" (with Promise and file path)';
+    jBinary
+        .loadData("./test.bin")
+        .then(data => {
+            console.log(`${title} => Loaded ${data.length} bytes of data.`);
+        })
+        .catch(reason => {
+            if (reason instanceof Error) {
+                console.log(`${title} => Error reading file: ${reason.message}`);
+            }
+        });
+})();
 
-b1.seek(4);
-console.log(b1.read('int8'));
+(function () {
+    const title = '"load" (with Callback and ReadableStream)';
+    const inputStream = fs.createReadStream("./test.bin");
+    jBinary.load(inputStream, undefined, (err, jb) => {
+        if (err) {
+            console.log(`${title} => Error reading file: ${err.message}`);
+        } else {
+            console.log(`${title} => Loaded ${jb.view.buffer.length} bytes of data.`);
+        }
+    });
+})();
 
-console.log(b1.tell());
+(function () {
+    const title = '"load" (with Promise and ReadableStream)';
+    const inputStream = fs.createReadStream("./test.bin");
+    jBinary
+        .load(inputStream)
+        .then(jb => {
+            console.log(`${title} => Loaded ${jb.view.buffer.length} bytes of data.`);
+        })
+        .catch(reason => {
+            if (reason instanceof Error) {
+                console.log(`${title} => Error reading file: ${reason.message}`);
+            }
+        });
+})();
+// #endregion Loading data
 
-b1.write('int8', 0x9A, 2);
-b1.writeAll(originalData);
+// var originalData = [0x05, 0x03, 0x7f, 0x1e];
+// var b1 = new jBinary(originalData);
+// console.log(b1.readAll());
+// console.log(b1.read("int8"));
 
-console.log(b1.slice(0, 2));
+// b1.seek(4);
+// console.log(b1.read("int8"));
 
-jBinary.saveAs('myfile.pdf', 'application/pdf');
-jBinary.toURI();
+// console.log(b1.tell());
+
+// b1.write("int8", 0x9a, 2);
+// b1.writeAll(originalData);
+
+// console.log(b1.slice(0, 2));
+
+// jBinary.saveAs("myfile.pdf", "application/pdf");
+// jBinary.toURI();
