0027e561bfeef84b2a095faf29756b477b16333b
Daniel Tschinder <231804+danez@users.noreply.github.com>
2021-04-12 12:16:01 +0000

    fix(picomatch): Better types when returnState equals true

--- types/picomatch/picomatch-tests.ts
+++ types/picomatch/picomatch-tests.ts
@@ -2,3 +2,7 @@ import pm = require('picomatch');
 
 // main function
 const isMatch = pm('*.!(*a)');
+
+// main function with state
+const isMatch2 = pm('*.!(*a)', {}, true);
+isMatch2.state.negated;
