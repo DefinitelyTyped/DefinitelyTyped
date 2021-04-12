4f7bfab19beb6c528920cfd359f7d8d4b600db5b
Birk Skyum <74932975+birkskyum@users.noreply.github.com>
2021-04-12 20:50:52 +0200

    Remove myself as maintainer

--- types/meteor/index.d.ts
+++ types/meteor/index.d.ts
@@ -4,7 +4,6 @@
 //                 Dave Allen <https://github.com/fullflavedave>
 //                 Olivier Refalo <https://github.com/orefalo>
 //                 Daniel Neveux <https://github.com/dagatsoin>
-//                 Birk Skyum <https://github.com/birkskyum>
 //                 Arda TANRIKULU <https://github.com/ardatan>
 //                 Stefan Holzapfel <https://github.com/stefanholzapfel>
 //                 Andrey Markeev <https://github.com/andrei-markeev>
