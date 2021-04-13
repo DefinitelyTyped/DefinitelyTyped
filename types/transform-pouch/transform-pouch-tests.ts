18e33456df5ff17b4cfd774e469b8cbd1818b1e1
Lucas Rainett <lucas@rainett.dev>
2021-04-13 12:40:29 +1200

    Include PR recomendations

--- types/transform-pouch/transform-pouch-tests.ts
+++ types/transform-pouch/transform-pouch-tests.ts
@@ -1,3 +1,5 @@
+/// <reference lib="dom" />
+
 import * as PouchDB from "pouchdb";
 import * as transform from "transform-pouch";
 PouchDB.plugin(transform);
@@ -5,10 +7,12 @@ PouchDB.plugin(transform);
 const db1 = new PouchDB<{example: number}>();
 db1.transform<{transformed: number}>({
     incoming(doc) {
+        // $ExpectType number
         doc.example;
         return {_id: doc._id, transformed: 1};
     },
     outgoing(doc) {
+        // $ExpectType number
         doc.transformed;
         return {_id: doc._id, example: 1};
     }
@@ -17,10 +21,12 @@ db1.transform<{transformed: number}>({
 const dbAsync = new PouchDB<{example: number}>();
 dbAsync.transform<{transformed: number}>({
     async incoming(doc) {
+        // $ExpectType number
         doc.example;
         return {_id: doc._id, transformed: 1};
     },
     async outgoing(doc) {
+        // $ExpectType number
         doc.transformed;
         return {_id: doc._id, example: 1};
     }
