d686ee77b79dbbfcc400b7d88fe2001a1c70e923
Виктор Тяжельников <vtyazhelnikov@swaxis.com>
2021-04-14 22:35:41 +0600

    bump version

--- types/google-spreadsheet/index.d.ts
+++ types/google-spreadsheet/index.d.ts
@@ -1,4 +1,4 @@
-// Type definitions for google-spreadsheet 3.0
+// Type definitions for google-spreadsheet 3.1
 // Project: https://github.com/theoephraim/node-google-spreadsheet
 // Definitions by: the-vampiire <https://github.com/the-vampiire>
 //                 Federico Grandi <https://github.com/EndBug>
