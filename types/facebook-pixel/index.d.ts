4e81e5b44880088f9423cab27e49eae0e9f38dc5
bcchoi <ninpeng@naver.com>
2021-04-14 18:48:16 +0900

    Add: definitions by modifier

--- types/facebook-pixel/index.d.ts
+++ types/facebook-pixel/index.d.ts
@@ -2,6 +2,7 @@
 // Project: https://developers.facebook.com/docs/ads-for-websites/tag-api/
 // Definitions by: Noctis Hsu <https://github.com/noctishsu>
 //                 Victor Hom <https://github.com/VictorHom>
+//                 BC Choi <https://github.com/ninpeng>
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
 declare var fbq:facebook.Pixel.Event;
 
