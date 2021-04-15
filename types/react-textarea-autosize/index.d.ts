f6d15b2363fb544531450471cf3bbfbc49a519ec
Adnan Asani <adnasa@users.noreply.github.com>
2021-04-15 18:10:26 +0200

    chore: update version on header

--- types/react-textarea-autosize/index.d.ts
+++ types/react-textarea-autosize/index.d.ts
@@ -1,4 +1,4 @@
-// Type definitions for react-textarea-autosize 4.3.0
+// Type definitions for react-textarea-autosize 4.3.1
 // Project: https://github.com/andreypopp/react-textarea-autosize
 // Definitions by: Alexey Svetliakov <https://github.com/asvetliakov>,
 //                 Jerry Zou <https://github.com/zry656565>
