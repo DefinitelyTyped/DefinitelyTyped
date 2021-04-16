95d34e46bf514776d2352038e5b7b65f469db7e1
Thibault-Pierre Gouin <gouin.thibault@gmail.com>
2021-04-16 16:19:02 +0200

    [page-flip] Create @types/page-flip
    - [x] Use a meaningful title for the pull request. Include the name of the package modified.
    - [x] Test the change in your own code. (Compile and run.)
    - [x] [Add or edit tests](https://github.com/DefinitelyTyped/DefinitelyTyped/blob/master/README.md#my-package-teststs) to reflect the change.
    - [x] Follow the advice from the [readme](https://github.com/DefinitelyTyped/DefinitelyTyped/blob/master/README.md#make-a-pull-request).
    - [x] Avoid [common mistakes](https://github.com/DefinitelyTyped/DefinitelyTyped/blob/master/README.md#common-mistakes).
    - [x] [Run `npm test <package to test>`](https://github.com/DefinitelyTyped/DefinitelyTyped/blob/master/README.md#running-tests).
    
    Select one of these and delete the others:
    
    If adding a new definition:
    - [x] The package does not already provide its own types, or cannot have its `.d.ts` files generated via `--declaration`
    - [x] If this is for an npm package, match the name. If not, do not conflict with the name of an npm package.
    - [x] Create it with `dts-gen --dt`, not by basing it on an existing project.
    - [x] Represents shape of module/library [correctly](https://www.typescriptlang.org/docs/handbook/declaration-files/library-structures.html)
    - [x] `tslint.json` [should contain](https://github.com/DefinitelyTyped/DefinitelyTyped/blob/master/README.md#linter-tslintjson) `{ "extends": "dtslint/dt.json" }`, and no additional rules.
    - [x] `tsconfig.json` [should have](https://github.com/DefinitelyTyped/DefinitelyTyped/blob/master/README.md#tsconfigjson) `noImplicitAny`, `noImplicitThis`, `strictNullChecks`, and `strictFunctionTypes` set to `true`.

---
+++ types/page-flip/page-flip-tests.ts
@@ -0,0 +1,80 @@
+import * as StPageFlip from "page-flip";
+
+//// Enumerations
+
+// $ExpectType typeof FlipCorner
+StPageFlip.FlipCorner;
+
+// $ExpectType typeof FlipDirection
+StPageFlip.FlipDirection;
+
+// $ExpectType typeof FlippingState
+StPageFlip.FlippingState;
+
+// $ExpectType typeof Orientation
+StPageFlip.Orientation;
+
+// $ExpectType typeof PageDensity
+StPageFlip.PageDensity;
+
+// $ExpectType typeof PageOrientation
+StPageFlip.PageOrientation;
+
+// $ExpectType typeof SizeType
+StPageFlip.SizeType;
+
+//// Classes
+
+// $ExpectType typeof HTMLPageCollection
+StPageFlip.HTMLPageCollection;
+
+// $ExpectType typeof ImagePageCollection
+StPageFlip.ImagePageCollection;
+
+// $ExpectType typeof PageCollection
+StPageFlip.PageCollection;
+
+// $ExpectType typeof EventObject
+StPageFlip.EventObject;
+
+// $ExpectType typeof Flip
+StPageFlip.Flip;
+
+// $ExpectType typeof FlipCalculation
+StPageFlip.FlipCalculation;
+
+// $ExpectType typeof Helper
+StPageFlip.Helper;
+
+// $ExpectType typeof HTMLPage
+StPageFlip.HTMLPage;
+
+// $ExpectType typeof ImagePage
+StPageFlip.ImagePage;
+
+// $ExpectType typeof Page
+StPageFlip.Page;
+
+// $ExpectType typeof CanvasRender
+StPageFlip.CanvasRender;
+
+// $ExpectType typeof HTMLRender
+StPageFlip.HTMLRender;
+
+// $ExpectType typeof Render
+StPageFlip.Render;
+
+// $ExpectType typeof CanvasUI
+StPageFlip.CanvasUI;
+
+// $ExpectType typeof HTMLUI
+StPageFlip.HTMLUI;
+
+// $ExpectType typeof UI
+StPageFlip.UI;
+
+// $ExpectType typeof PageFlip
+StPageFlip.PageFlip;
+
+// $ExpectType typeof Settings
+StPageFlip.Settings;
