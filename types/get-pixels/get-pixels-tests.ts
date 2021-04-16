31ac9e36375a91e22f860df17f4adbc0975c7a39
Don McCurdy <donmccurdy@google.com>
2021-04-16 14:56:39 -0700

    [get-pixels] Fixes for test + mimeType.

--- types/get-pixels/get-pixels-tests.ts
+++ types/get-pixels/get-pixels-tests.ts
@@ -2,9 +2,9 @@ import getPixels from "get-pixels";
 import ndarray from "ndarray";
 
 getPixels("path/to/image.png", callback);
-getPixels("path/to/image.png", "png", callback);
-getPixels(new Uint8Array(1000), "png", callback);
-getPixels(Buffer.from([0, 0, 0]), "png", callback);
+getPixels("path/to/image.png", "image/png", callback);
+getPixels(new Uint8Array(1000), "image/jpeg", callback);
+getPixels(Buffer.from([0, 0, 0]), "image/gif", callback);
 
 function callback(error: Error | null, array: ndarray | null) {
     if (error) {
