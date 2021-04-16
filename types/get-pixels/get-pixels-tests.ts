e623ca36be0769a9a7d5032d32234c3f5e6e7bf6
Don McCurdy <donmccurdy@google.com>
2021-04-16 00:14:12 -0700

    [get-pixels] Attempt to fix lint errors.

--- types/get-pixels/get-pixels-tests.ts
+++ types/get-pixels/get-pixels-tests.ts
@@ -1,9 +1,12 @@
+import getPixels from 'get-pixels';
+import * as ndarray from 'ndarray';
+
 getPixels('path/to/image.png', callback);
 getPixels('path/to/image.png', 'image/png', callback);
 getPixels(new Uint8Array(1000), 'image/png', callback);
 getPixels(Buffer.from([0, 0, 0]), 'image/png', callback);
 
-function callback (error, array) {
+function callback(error: string | null, array: ndarray | null) {
 	if (error) {
 		const errorMsg: string = error.toUpperCase();
 	}
