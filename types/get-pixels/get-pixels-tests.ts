fe1395b2702e109eebc91065d67ba59984744232
Don McCurdy <donmccurdy@google.com>
2021-04-16 14:54:47 -0700

    [get-pixels] Run prettier.

--- types/get-pixels/get-pixels-tests.ts
+++ types/get-pixels/get-pixels-tests.ts
@@ -1,16 +1,16 @@
-import getPixels from 'get-pixels';
-import ndarray from 'ndarray';
+import getPixels from "get-pixels";
+import ndarray from "ndarray";
 
-getPixels('path/to/image.png', callback);
-getPixels('path/to/image.png', 'png', callback);
-getPixels(new Uint8Array(1000), 'png', callback);
-getPixels(Buffer.from([0, 0, 0]), 'png', callback);
+getPixels("path/to/image.png", callback);
+getPixels("path/to/image.png", "png", callback);
+getPixels(new Uint8Array(1000), "png", callback);
+getPixels(Buffer.from([0, 0, 0]), "png", callback);
 
 function callback(error: Error | null, array: ndarray | null) {
-	if (error) {
-		const errorMsg: string = error.message.toUpperCase();
-	}
-	if (array) {
-		const numPixels: number = array.shape[0] * array.shape[1];
-	}
+    if (error) {
+        const errorMsg: string = error.message.toUpperCase();
+    }
+    if (array) {
+        const numPixels: number = array.shape[0] * array.shape[1];
+    }
 }
