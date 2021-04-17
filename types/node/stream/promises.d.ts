e871b0b17a954a21264e00fcbac4e05dccde3ece
Brian Crowell <brian@fluggo.com>
2021-04-17 16:16:22 -0500

    node: Add pipeline parameter inference to promise API

--- types/node/stream/promises.d.ts
+++ types/node/stream/promises.d.ts
@@ -3,20 +3,57 @@ declare module "node:stream/promises" {
 }
 
 declare module "stream/promises" {
-    import { FinishedOptions } from "stream";
+    import { FinishedOptions, PipelineSource, PipelineTransform,
+        PipelineDestination, PipelinePromise } from "stream";
 
     function finished(stream: NodeJS.ReadableStream | NodeJS.WritableStream | NodeJS.ReadWriteStream, options?: FinishedOptions): Promise<void>;
 
-    function pipeline(stream1: NodeJS.ReadableStream, stream2: NodeJS.WritableStream): Promise<void>;
-    function pipeline(stream1: NodeJS.ReadableStream, stream2: NodeJS.ReadWriteStream, stream3: NodeJS.WritableStream): Promise<void>;
-    function pipeline(stream1: NodeJS.ReadableStream, stream2: NodeJS.ReadWriteStream, stream3: NodeJS.ReadWriteStream, stream4: NodeJS.WritableStream): Promise<void>;
-    function pipeline(
-        stream1: NodeJS.ReadableStream,
-        stream2: NodeJS.ReadWriteStream,
-        stream3: NodeJS.ReadWriteStream,
-        stream4: NodeJS.ReadWriteStream,
-        stream5: NodeJS.WritableStream,
-    ): Promise<void>;
+    function pipeline<A extends PipelineSource<any>,
+        B extends PipelineDestination<A, any>>(
+        source: A,
+        destination: B
+    ): PipelinePromise<B>;
+    function pipeline<A extends PipelineSource<any>,
+        T1 extends PipelineTransform<A, any>,
+        B extends PipelineDestination<T1, any>>(
+        source: A,
+        transform1: T1,
+        destination: B
+    ): PipelinePromise<B>;
+    function pipeline<A extends PipelineSource<any>,
+        T1 extends PipelineTransform<A, any>,
+        T2 extends PipelineTransform<T1, any>,
+        B extends PipelineDestination<T2, any>>(
+        source: A,
+        transform1: T1,
+        transform2: T2,
+        destination: B
+    ): PipelinePromise<B>;
+    function pipeline<A extends PipelineSource<any>,
+        T1 extends PipelineTransform<A, any>,
+        T2 extends PipelineTransform<T1, any>,
+        T3 extends PipelineTransform<T2, any>,
+        B extends PipelineDestination<T3, any>>(
+        source: A,
+        transform1: T1,
+        transform2: T2,
+        transform3: T3,
+        destination: B
+    ): PipelinePromise<B>;
+    function pipeline<A extends PipelineSource<any>,
+        T1 extends PipelineTransform<A, any>,
+        T2 extends PipelineTransform<T1, any>,
+        T3 extends PipelineTransform<T2, any>,
+        T4 extends PipelineTransform<T3, any>,
+        B extends PipelineDestination<T4, any>>(
+        source: A,
+        transform1: T1,
+        transform2: T2,
+        transform3: T3,
+        transform4: T4,
+        destination: B
+    ): PipelinePromise<B>;
+
     function pipeline(streams: ReadonlyArray<NodeJS.ReadableStream | NodeJS.WritableStream | NodeJS.ReadWriteStream>): Promise<void>;
     function pipeline(
         stream1: NodeJS.ReadableStream,
