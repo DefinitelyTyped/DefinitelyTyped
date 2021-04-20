c479614a71c0c22ea55d965ed27772d04d7a734b
Elizabeth Samuel <elizs@microsoft.com>
2021-04-20 11:51:11 -0700

    [office-js] [office-js-preview] (Outlook) Categories restrictions in OWA (#52267)
    * [office-js] [office-js-preview] (Outlook) Categories restrictions in OWA
    
    * Add note to new overloads

--- types/office-js/index.d.ts
+++ types/office-js/index.d.ts
@@ -11742,6 +11742,8 @@ declare namespace Office {
          * Adds categories to an item. Each category must be in the categories master list on that mailbox and so must have a unique name
          * but multiple categories can use the same color.
          *
+         * **Important**: In Outlook on the web, you can't use the API to manage categories applied to a message or appointment item in Compose mode.
+         *
          * [Api set: Mailbox 1.8]
          *
          * @remarks
@@ -11765,6 +11767,8 @@ declare namespace Office {
          * Adds categories to an item. Each category must be in the categories master list on that mailbox and so must have a unique name
          * but multiple categories can use the same color.
          *
+         * **Important**: In Outlook on the web, you can't use the API to manage categories applied to a message or appointment item in Compose mode.
+         *
          * [Api set: Mailbox 1.8]
          *
          * @remarks
@@ -11785,8 +11789,12 @@ declare namespace Office {
         /**
          * Gets an item's categories.
          *
-         * **Important**: If there are no categories on the item, `null` or an empty array will be returned depending on the Outlook version
+         * **Important**:
+         *
+         * - If there are no categories on the item, `null` or an empty array will be returned depending on the Outlook version
          * so make sure to handle both cases.
+         *
+         * - In Outlook on the web, you can't use the API to manage categories applied to a message in Compose mode.
          *
          * [Api set: Mailbox 1.8]
          *
@@ -11805,8 +11813,12 @@ declare namespace Office {
         /**
          * Gets an item's categories.
          *
-         * **Important**: If there are no categories on the item, `null` or an empty array will be returned depending on the Outlook version
+         * **Important**:
+         *
+         * - If there are no categories on the item, `null` or an empty array will be returned depending on the Outlook version
          * so make sure to handle both cases.
+         *
+         * - In Outlook on the web, you can't use the API to manage categories applied to a message in Compose mode.
          *
          * [Api set: Mailbox 1.8]
          *
@@ -11822,6 +11834,8 @@ declare namespace Office {
         getAsync(callback: (asyncResult: Office.AsyncResult<CategoryDetails[]>) => void): void;
         /**
          * Removes categories from an item.
+         *
+         * **Important**: In Outlook on the web, you can't use the API to manage categories applied to a message in Compose mode.
          *
          * [Api set: Mailbox 1.8]
          *
@@ -11840,6 +11854,8 @@ declare namespace Office {
         removeAsync(categories: string[], options: Office.AsyncContextOptions, callback?: (asyncResult: Office.AsyncResult<void>) => void): void;
         /**
          * Removes categories from an item.
+         *
+         * **Important**: In Outlook on the web, you can't use the API to manage categories applied to a message in Compose mode.
          *
          * [Api set: Mailbox 1.8]
          *
