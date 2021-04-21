8b0877a7d3e0e2c0161c5f4fd1b2a630e34057ba
Axel Bocciarelli <axelboc@gmail.com>
2021-04-21 09:25:06 +0200

    (ndarray): Add contributor

--- types/ndarray/index.d.ts
+++ types/ndarray/index.d.ts
@@ -1,6 +1,8 @@
 // Type definitions for ndarray 1.0
 // Project: https://github.com/scijs/ndarray, https://github.com/mikolalysenko/ndarray
-// Definitions by: Giff Song <https://github.com/pawsong>, taoqf <https://github.com/taoqf>
+// Definitions by: Giff Song <https://github.com/pawsong>,
+//                 taoqf <https://github.com/taoqf>
+//                 Axel Bocciarelli <https://github.com/axelboc>
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
 // TypeScript Version: 2.3
 
