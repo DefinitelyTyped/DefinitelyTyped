1de566f00899ac1b0f950f092c6bf907ab17b3c5
duyutao <duyutao@pwrd.com>
2021-04-21 11:11:18 +0800

    modify slice params ; add findInde method in Fragment

--- types/prosemirror-model/index.d.ts
+++ types/prosemirror-model/index.d.ts
@@ -159,6 +159,12 @@ export class Fragment<S extends Schema = any> {
    * object with two separate positions is returned.
    */
   findDiffEnd(other: ProsemirrorNode<S>): { a: number; b: number } | null | undefined;
+
+  // : (number, ?number) → {index: number, offset: number}
+  // Find the index and inner offset corresponding to a given relative
+  // position in this fragment. The result object will be reused
+  // (overwritten) the next time the function is called. (Not public.)
+  findIndex(pos: number, round?: number ): { index: number; offset: number };
   /**
    * Return a debugging string that describes this fragment.
    */
@@ -599,7 +605,7 @@ declare class ProsemirrorNode<S extends Schema = any> {
    * Cut out the part of the document between the given positions, and
    * return it as a `Slice` object.
    */
-  slice(from: number, to?: number): Slice<S>;
+  slice(from: number, to?: number,includeParents?:boolean): Slice<S>;
   /**
    * Replace the part of the document between the given positions with
    * the given slice. The slice must 'fit', meaning its open sides
