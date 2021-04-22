0a55cb33d1cba5e7a577869b832f27b8afa35231
Cogi <dbwodlf3@naver.com>
2021-04-22 14:45:08 +0900

    fix some bugs

--- types/ip-to-int/index.d.ts
+++ types/ip-to-int/index.d.ts
@@ -1,9 +1,7 @@
-// Type definitions for ip-to-int 0.3.1
-// Project: https://github.com/joshuamarquez/node-ip-to-int
-// Definitions by: Ryu JaeIL <https://github.com/dbwodlf3>
+// Type definitions for ip-to-int 0.3
+// Project: https://github.com/joshuamarquez/node-ip-to-int#readme
+// Definitions by: Cogi <https://github.com/dbwodlf3>
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
-
-/// <reference types="node" />
 
 declare module 'ip-to-int' {
     function ipInt(ip: string): { 
