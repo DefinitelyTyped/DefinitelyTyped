556305b25434c65a9bb40de2e1b6288780f6c4f8
Linda Paiste <lindapaiste@gmail.com>
2021-04-21 19:34:44 -0500

    created type definitions for "smtpjs.com" script tag

---
+++ types/smtpjs.com/index.d.ts
@@ -0,0 +1,99 @@
+// Type definitions for SmtpJs v3
+// Project: https://smtpjs.com/
+// Definitions by: Linda Paiste <https://github.com/lindapaiste>
+
+/**
+ * The SmtpJS script exposes a variable `Email` on the global `window` object.
+ * Include the <script> tag in the HTML code of your website:
+ * <script src="https://smtpjs.com/v3/smtp.js"></script>
+ * Load this types package in order to access the `window.Email` variable with type safety.
+ */
+declare namespace Email {
+    /**
+     * One or more attachments can be added to the email in the following format.
+     * An attachment should have a `name` and can specify it's content through
+     * either a `path` property or a `data` property.
+     */
+    type Attachment = {
+        /**
+         * The file name of the attachment.
+         */
+        name: string;
+    } & ({
+        /**
+         * The URL of the file to attach, ie. "https://networkprogramming.files.wordpress.com/2017/11/smtpjs.png"
+         */
+        path: string;
+    } | {
+        /**
+         * A base-64 encoded representation of the file.
+         */
+        data: string;
+    });
+
+    /**
+     * These properties are required on email object regardless of authentication.
+     */
+    interface EmailContents {
+        /**
+         * The email address of the recipient, or an array of recipient email addresses.
+         */
+        To: string | string[];
+        /**
+         * The email address of the sender.
+         */
+        From: string;
+        /**
+         * The subject line of the email.
+         */
+        Subject: string;
+        /**
+         * The contents of the email, encoded to text.
+         */
+        Body: string;
+        /**
+         * OPTIONAL. An array of attachments to attach to the email.
+         */
+        Attachments?: Attachment[];
+    }
+
+    /**
+     * Emails can be authenticated using a username and password on a host domain.
+     */
+    interface UnsecureEmail extends EmailContents {
+        /**
+         * The host SMTP server, ie. "smtp.yourisp.com"
+         */
+        Host: string;
+        /**
+         * Login username.
+         */
+        Username: string;
+        /**
+         * Login password.
+         */
+        Password: string;
+    }
+
+    /**
+     * SMTP login credentials can be encrypted using a secure token.
+     */
+    interface SecureEmail extends EmailContents {
+        /**
+         * An alpha-numeric string, ie. "C973D7AD-F097-4B95-91F4-40ABC5567812"
+         */
+        SecureToken: string;
+    }
+
+    /**
+     * The only public method of the `window.Email` object is the `send` function, which is used to send an email.
+     *
+     * @param {UnsecureEmail | SecureEmail} email - The email object to send.
+     * @return {Promise<string>} - The send function returns a Promise which will resolve when the email has been sent
+     * or an error has occurred.  The resolved value of the Promise is a message of "OK" for success or an error
+     * message on failure.
+     */
+    function send(email: UnsecureEmail | SecureEmail): Promise<string>;
+}
+
+export default Email;
---
+++ types/smtpjs.com/smtpjs.com-tests.ts
@@ -0,0 +1,27 @@
+import Email from ".";
+
+const attachment: Email.Attachment = {
+    name: "smtpjs.png",
+    path: "https://networkprogramming.files.wordpress.com/2017/11/smtpjs.png"
+}
+
+const email: Email.UnsecureEmail = {
+    Host: "smtp.yourisp.com",
+    Username: "username",
+    Password: "password",
+    To: 'them@website.com',
+    From: "you@isp.com",
+    Subject: "This is the subject",
+    Body: "And this is the body"
+}
+
+Email.send(email).then(message => alert(message));
+Email.send({...email, Attachments: [attachment]}).then(message => alert(message));
+
+const promise: Promise<string> = Email.send({
+    SecureToken: "C973D7AD-F097-4B95-91F4-40ABC5567812",
+    To: 'them@website.com',
+    From: "you@isp.com",
+    Subject: "This is the subject",
+    Body: "And this is the body"
+});
