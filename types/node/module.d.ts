850e27cb293335ebe18360d3848c51d6c9ec4888
Victor Perin <me@victorperin.ninja>
2021-04-25 13:05:26 -0300

    feat(node/module): createRequireFromPath is now in End-of-Life

--- types/node/module.d.ts
+++ types/node/module.d.ts
@@ -42,10 +42,6 @@ declare module 'module' {
         static runMain(): void;
         static wrap(code: string): string;
 
-        /**
-         * @deprecated Deprecated since: v12.2.0. Please use createRequire() instead.
-         */
-        static createRequireFromPath(path: string): NodeRequire;
         static createRequire(path: string | URL): NodeRequire;
         static builtinModules: string[];
 
