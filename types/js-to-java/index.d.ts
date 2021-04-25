64ac47c5e0db4b52dae4d5f8ab937766d7583d1a
zhaoleipeng <zhaoleipeng@didiglobal.com>
2021-04-25 14:06:03 +0800

    feat: add definitions name

--- types/js-to-java/index.d.ts
+++ types/js-to-java/index.d.ts
@@ -1,6 +1,7 @@
 // Type definitions for js-to-java 2.7.0
 // Project: https://github.com/node-modules/js-to-java
 // Definitions by: skyitachi <https://github.com/skyitachi>
+//                 zhaoleipeng <https://github.com/zhaoleipeng>
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
 
 interface Currency {
