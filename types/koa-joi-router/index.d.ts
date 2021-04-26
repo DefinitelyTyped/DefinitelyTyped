3a1ba11a239d53b0c3f2ed0cc7b62d94ad937e74
Kevin Stolp <kevinstolp@gmail.com>
2021-04-26 12:50:25 -0700

    Update package version

--- types/koa-joi-router/index.d.ts
+++ types/koa-joi-router/index.d.ts
@@ -1,4 +1,4 @@
-// Type definitions for koa-joi-router 5.2
+// Type definitions for koa-joi-router 8.0
 // Project: https://github.com/koajs/joi-router
 // Definitions by: Matthew Bull <https://github.com/wingsbob>
 //                 Dave Welsh <https://github.com/move-zig>
