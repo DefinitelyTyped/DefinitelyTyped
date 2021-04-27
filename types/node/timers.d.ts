3128ebefdcfd7cd67591dfc3583acebe6ebf43dd
undefine <46276781+oadpoaw@users.noreply.github.com>
2021-04-27 10:21:48 +0800

    node: setTimeout, setInterval, setImmediate

--- types/node/timers.d.ts
+++ types/node/timers.d.ts
@@ -17,15 +17,15 @@ declare module 'timers' {
         signal?: AbortSignal;
     }
 
-    function setTimeout(callback: (...args: any[]) => void, ms?: number, ...args: any[]): NodeJS.Timeout;
+    function setTimeout<T extends any[]>(callback: (...args: T) => void, ms?: number, ...args: T): NodeJS.Timeout;
     namespace setTimeout {
         function __promisify__(ms: number): Promise<void>;
         function __promisify__<T>(ms: number, value: T, options?: TimerOptions): Promise<T>;
     }
     function clearTimeout(timeoutId: NodeJS.Timeout): void;
-    function setInterval(callback: (...args: any[]) => void, ms?: number, ...args: any[]): NodeJS.Timeout;
+    function setInterval<T extends any[]>(callback: (...args: T) => void, ms?: number, ...args: T): NodeJS.Timeout;
     function clearInterval(intervalId: NodeJS.Timeout): void;
-    function setImmediate(callback: (...args: any[]) => void, ...args: any[]): NodeJS.Immediate;
+    function setImmediate<T extends any[]>(callback: (...args: T) => void, ...args: T): NodeJS.Immediate;
     namespace setImmediate {
         function __promisify__(): Promise<void>;
         function __promisify__<T>(value: T, options?: TimerOptions): Promise<T>;
