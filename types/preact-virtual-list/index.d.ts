c7b4ca497a8f9ebcb066206eba3c66ba0919f589
Reece Berens <reece-berens1@gmail.com>
2021-04-28 17:47:43 -0500

    Fix props for class component

--- types/preact-virtual-list/index.d.ts
+++ types/preact-virtual-list/index.d.ts
@@ -7,16 +7,16 @@ import {Component} from "preact";
 
 interface Props {
     className?: string;
-    data: any;
+    data: any[];
     overscanCount?: number;
-    renderRow: (row: any) => Component<any>;
+    renderRow: (row: any) => any;
     rowHeight: number;
     sync?: boolean;
     [otherProps: string]: any;
 }
 
-declare class VirtualList extends Component<any> {
-    constructor(someParam?: string);
+declare class VirtualList extends Component<Props> {
+    constructor(props: Props);
 }
 
 export default VirtualList;
