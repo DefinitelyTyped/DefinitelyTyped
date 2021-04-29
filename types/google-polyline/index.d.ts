83cb25977cf31f1addf59a788c3fcfb14b630885
Babette Stam <babettestam@gmail.com>
2021-04-29 17:08:48 +0200

    New declaration for google-polyline

---
+++ types/google-polyline/index.d.ts
@@ -0,0 +1,8 @@
+// Type definitions for jMuxer
+// Definitions by: Babette Stam <https://github.com/Babettestam>
+// Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
+
+declare module "google-polyline" {
+    export function encode(latLng: [number, number][]): string;
+    export function decode(encoded: string): [number, number][];
+}
