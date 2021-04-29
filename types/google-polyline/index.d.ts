719230b8116c6211754c3a367cf258832dfb095c
Babette Stam <babettestam@gmail.com>
2021-04-29 17:20:34 +0200

    Fix testing warning

--- types/google-polyline/index.d.ts
+++ types/google-polyline/index.d.ts
@@ -1,8 +1,7 @@
-// Type definitions for google-polyline
+// Type definitions for google-polyline 1.0
+// Project: https://github.com/jhermsmeier/node-google-polyline
 // Definitions by: Babette Stam <https://github.com/Babettestam>
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
 
-declare module "google-polyline" {
-    export function encode(latLng: [number, number][]): string;
-    export function decode(encoded: string): [number, number][];
-}
+export function encode(latLng: Array<[number, number]>): string;
+export function decode(encoded: string): Array<[number, number]>;
