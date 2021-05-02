b37eebaba353328fd84a9ece6a1f260948b48300
Daniel Earwicker <dan@earwicker.com>
2021-05-02 19:40:29 +0100

    Fixes

--- types/transform-coordinates/index.d.ts
+++ types/transform-coordinates/index.d.ts
@@ -1,4 +1,4 @@
-// Type definitions for transform-coordinates 1.0.0
+// Type definitions for transform-coordinates 1.0
 // Project: https://github.com/derhuerst/transform-coordinates
 // Definitions by: Daniel Earwicker <https://github.com/danielearwicker>
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
