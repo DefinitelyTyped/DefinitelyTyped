7d270c73d17f76fb2e91c24142cbddb80dc322d9
ArvinWei <clovery.org@gmail.com>
2021-05-03 08:39:07 +0800

    publish version 0.2

--- types/ipaiva/index.d.ts
+++ types/ipaiva/index.d.ts
@@ -1,4 +1,4 @@
-// Type definitions for ipaiva 0.1
+// Type definitions for ipaiva 0.2
 // Project: https://github.com/ipaiva-studio
 // Definitions by: ipaiva <https://github.com/ipaiva-studio>
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
