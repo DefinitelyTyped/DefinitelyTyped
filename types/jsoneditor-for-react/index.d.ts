ca63c798078cc553bc83df4964724c7164a4b7a5
jayce-riderdash <62314301+jayce-riderdash@users.noreply.github.com>
2021-05-03 10:10:08 +0900

    Update index.d.ts

--- types/jsoneditor-for-react/index.d.ts
+++ types/jsoneditor-for-react/index.d.ts
@@ -10,6 +10,7 @@ import JSONEditor, { JSONEditorOptions } from 'jsoneditor';
 
 export interface ReactJsonEditorProps {
     values: {};
+    onChange: any;
 }
 
 export default class ReactJsoneditor extends React.Component<ReactJsonEditorProps> {
