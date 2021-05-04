a755bd4dd5078650cd231dc4bb9c4f96f32c5238
alioguzhan <aoguzhanyildiz@gmail.com>
2021-05-04 21:52:07 +0300

    update tests to increase performance

--- types/react-editext/react-editext-tests.tsx
+++ types/react-editext/react-editext-tests.tsx
@@ -1,20 +1,20 @@
-import EdiText from 'react-editext';
+import EdiText from "react-editext";
 
-import * as React from 'react';
-import { render } from 'react-dom';
+import * as React from "react";
+import { render } from "react-dom";
 
 declare const container: Element;
 
 render(
     <EdiText
-        type='text'
-        value={'hello'}
-        onSave={value => console.log(value)}
+        type="text"
+        value={"hello"}
+        onSave={v => v}
         renderValue={v => `${v} world`}
         hint="prefix the world"
         hideIcons={true}
         startEditingOnEnter
-        onValidationFail={alert}
+        onValidationFail={v => v}
         submitOnUnfocus
     />,
     container,
