646cebbc4ba81ff5590aba181c71ffed1aed0b03
Adam Voga <agov@users.noreply.github.com>
2021-05-04 10:03:15 +0200

    Remove myself as maintainer
    I can no longer properly validate pull requests

--- types/ibm_db/index.d.ts
+++ types/ibm_db/index.d.ts
@@ -1,6 +1,5 @@
 // Type definitions for ibm_db 2.0
 // Project: https://github.com/ibmdb/node-ibm_db
-// Definitions by: Adam Voga <https://github.com/agov>
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
 // TypeScript Version: 2.3
 
