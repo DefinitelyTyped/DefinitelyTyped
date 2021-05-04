0d44799f6fc2695b7e025d25c3c823b76a241524
Mark Skelton <mdskelton99@gmail.com>
2021-05-04 10:17:12 -0500

    Update index.d.ts

--- types/webpack-livereload-plugin/index.d.ts
+++ types/webpack-livereload-plugin/index.d.ts
@@ -4,7 +4,7 @@
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
 // TypeScript Version: 3.7
 import { Plugin, Stats } from 'webpack';
-import { ServerOptions } from 'https'
+import { ServerOptions } from 'https';
 import webpack = require('webpack');
 
 declare class LiveReloadPlugin extends Plugin {
