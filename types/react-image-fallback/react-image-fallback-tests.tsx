cb567014c6035082437e9caaa94fdad982c53a30
Martin Foakes <foakes.m@gmail.com>
2021-05-04 11:17:09 +0200

    Fix: tests, update class name

--- types/react-image-fallback/react-image-fallback-tests.tsx
+++ types/react-image-fallback/react-image-fallback-tests.tsx
@@ -9,7 +9,7 @@ class ReactImageFallbackSimple extends React.Component<ReactImageFallbackProps> 
     }
 }
 
-class ReactImageFallbackSimple extends React.Component<ReactImageFallbackProps> {
+class ReactImageFallbackWithUndefineds extends React.Component<ReactImageFallbackProps> {
     render() {
         return <ReactImageFallback
             src={undefined}
