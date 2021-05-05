96c485953abaeaab619e0003c5fc84d5a638618d
Alexander Staroselsky <Alexander.Staroselsky1@T-Mobile.com>
2021-05-05 08:13:33 -0600

    update version yet again

--- types/passport-spotify/index.d.ts
+++ types/passport-spotify/index.d.ts
@@ -1,4 +1,4 @@
-// Type definitions for passport-spotify 2.0
+// Type definitions for passport-spotify v2.0.0
 // Project: https://github.com/jmperez/passport-spotify#readme
 // Definitions by: Rishi Kodali <https://github.com/rishikodali>
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
