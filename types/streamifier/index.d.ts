883f221626d5208479225bba76a442e7642eb2e3
Idan Attias <idana@wix.com>
2021-05-06 17:12:56 +0300

    Add type definitions for streamifier

---
+++ types/streamifier/index.d.ts
@@ -0,0 +1,7 @@
+// Type definitions for streamifier 0.1
+// Project: https://github.com/gagle/node-streamifier
+// Definitions by: Idan Attias <https://github.com/idan-at>
+// Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
+import { Readable, ReadableOptions } from "stream";
+
+export function createReadStream(object: string | Buffer | Uint8Array, options?: ReadableOptions): Readable;
