6f77512e6248ca3eab67ed267b9fd0bf59817b0d
juliankrieger <julian.krieger@me.com>
2021-05-06 10:22:54 +0200

    create attributes now optional

--- types/scrivito/index.d.ts
+++ types/scrivito/index.d.ts
@@ -336,7 +336,7 @@ export class Obj {
 
     // Static methods
     static all(): any;
-    static create(attributes: CreateAttributes): Obj;
+    static create(attributes?: CreateAttributes): Obj;
     static createFromFile(file: File, attributes: CreateAttributes): Promise<Obj>;
     static get(id: string): Obj | null;
     static getByPath(path: string): Obj | null;
