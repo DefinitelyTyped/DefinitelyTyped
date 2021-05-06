a52940e7f4f14ef060f5240a35b7dbcaff9edf6e
Idan Attias <idana@wix.com>
2021-05-06 17:17:50 +0300

    add types reference

--- types/streamifier/index.d.ts
+++ types/streamifier/index.d.ts
@@ -2,6 +2,9 @@
 // Project: https://github.com/gagle/node-streamifier
 // Definitions by: Idan Attias <https://github.com/idan-at>
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
+
+/// <reference types="node" />
+
 import { Readable, ReadableOptions } from "stream";
 
 export function createReadStream(object: string | Buffer | Uint8Array, options?: ReadableOptions): Readable;
