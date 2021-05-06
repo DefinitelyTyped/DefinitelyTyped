cad54ce00dbf93c427e2a9aba5ef12bd1c15f027
sushilraje <39531904+sushilraje@users.noreply.github.com>
2021-05-06 00:28:10 -0700

    dimensions is an Array of MathNodes

--- types/mathjs/index.d.ts
+++ types/mathjs/index.d.ts
@@ -3083,7 +3083,7 @@ declare namespace math {
         comment?: string;
         content?: MathNode;
         condition?: MathNode;
-        dimensions?: MathNode;
+        dimensions?: MathNode[];
         op?: string;
         object?: MathNode;
         fn?: string;
