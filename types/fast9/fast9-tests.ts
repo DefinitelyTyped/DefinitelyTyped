159b4c9d88422f3e7328681befcf45f0f2820e91
Juan José González Giraldo <juanjose.eng@gmail.com>
2021-05-08 06:09:57 -0500

    Avoid using fs and use short base64 image instead
    What: Avoid reading from file and use inline base64
    Why: It was failing on benchmarks and test-all

--- types/fast9/fast9-tests.ts
+++ types/fast9/fast9-tests.ts
@@ -1,47 +1,32 @@
+/// <reference types='node' />
+
 import { detect } from 'fast9';
-import * as fs from 'fs';
-import { PNG } from 'pngjs';
 
-const pngfile = 'example.png';
+const pngImg = 'iVBORw0KGgoAAAANSUhEUgAAAAEAAAABCAYAAAAfFcSJAAAADUlEQVR42mN89Z/hPwAHrgLqdyqw1QAAAABJRU5ErkJggg==';
 
-fs.createReadStream(pngfile)
-	.pipe(new PNG({
-		filterType: -1,
-	}))
-	.on('parsed', function() {
-		const data = this.data;
-		const width = this.width;
-		const height = this.height;
+const data = Buffer.from(pngImg, 'base64');
+const width = 480;
+const height = 640;
 
-		// data is 'RGBA buffer'
+// data is 'RGBA buffer'
 
-		// make grayscale buffer (byte array)
-		const gs = new Uint8Array(width * height);
-		let gs_index = 0;
-		for (let y = 0; y < height; y++) {
-			for (let x = 0; x < width; x++) {
-				const idx = (width * y + x) << 2;
-				// data[idx] is red
-				// data[idx+1] is green
-				// data[idx+2] is blue
-				// data[idx+3] is alpha
-				const gray = Math.floor(data[idx] * 0.3 + data[idx + 1] * 0.6 + data[idx + 2] * 0.11);
-				gs[gs_index++] = gray;
-			}
-		}
+// make grayscale buffer (byte array)
+const gs = new Uint8Array(width * height);
+let gs_index = 0;
+for (let y = 0; y < height; y++) {
+	for (let x = 0; x < width; x++) {
+		const idx = (width * y + x) << 2;
+		// data[idx] is red
+		// data[idx+1] is green
+		// data[idx+2] is blue
+		// data[idx+3] is alpha
+		const gray = Math.floor(data[idx] * 0.3 + data[idx + 1] * 0.6 + data[idx + 2] * 0.11);
+		gs[gs_index++] = gray;
+	}
+}
 
-		const threshold = 20;
-		const corners = detect(gs, width, height, threshold, true);
+const threshold = 20;
+const corners = detect(gs, width, height, threshold, true);
 
-		// sort by score
-		// corners.sort((a,b)=>b.score-a.score); //lamda
-		corners.sort((a, b) => {
-			return b.score - a.score;
-		});
-
-		// print corners
-		console.log(corners.slice(0, 100));
-	})
-	.on('error', (e) => {
-		console.log(e);
-	});
+// sort by score
+corners.sort((a, b) => b.score - a.score);
