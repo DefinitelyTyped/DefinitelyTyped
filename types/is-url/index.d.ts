88e70d2d691e49bbc319930c5aa68d003c8238ea
Dimitri B <dimitri.benin@gmail.com>
2021-05-09 18:50:12 +0200

    [is-url] Add docs, improve tests, enable strictNull

--- types/is-url/index.d.ts
+++ types/is-url/index.d.ts
@@ -1,9 +1,12 @@
-// Type definitions for is-url v1.2.1
+// Type definitions for is-url 1.2
 // Project: https://github.com/segmentio/is-url
 // Definitions by: Ryota Murohoshi <https://github.com/RyotaMurohoshi>
+//                 BendingBender <https://github.com/BendingBender>
 // Definitions: https://github.com/RyotaMurohoshi/DefinitelyTyped
 
+export = isUrl;
 
-export = isUrl
-
+/**
+ * @returns Whether `string` is a URL.
+ */
 declare function isUrl(string: string): boolean;
