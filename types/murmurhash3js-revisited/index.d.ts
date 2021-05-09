30aba494593f8d0792cce049c750d98a7b8eafd7
Carlo Francisco <carlo@jcfrancisco.com>
2021-05-09 10:07:33 -0400

    [murmurhash3js-revisited] UMD export, not default export

--- types/murmurhash3js-revisited/index.d.ts
+++ types/murmurhash3js-revisited/index.d.ts
@@ -3,18 +3,19 @@
 // Definitions by: Carlo Francisco <https://github.com/me>
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
 
-interface X86 {
+export interface X86 {
     hash32: (bytes: Uint8Array) => number;
     hash128: (bytes: Uint8Array) => string;
 }
-interface X64 {
+export interface X64 {
     hash128: (bytes: Uint8Array) => string;
 }
 export const x86: X86;
 export const x64: X64;
 
-declare const MurmurHash3: {
+export const MurmurHash3: {
     x86: X86;
     x64: X64;
 };
-export default MurmurHash3;
+
+export as namespace MurmurHash3;
