86632a732197bef8fc4a8ed4500e9ae54b56818d
Will Tachau <will@metawork.com>
2021-05-11 14:27:28 -0700

    Update index.d.ts

--- types/keymaster/index.d.ts
+++ types/keymaster/index.d.ts
@@ -1,4 +1,4 @@
-// Type definitions for keymaster v1.6.2
+// Type definitions for keymaster v1.6.3
 // Project: https://github.com/madrobby/keymaster
 // Definitions by: Martin W. Kirst <https://github.com/nitram509>
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
