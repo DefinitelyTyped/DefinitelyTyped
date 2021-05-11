7a46059ab2b6d00af8b152adf884750f4540d339
hermanho <hermanho@users.noreply.github.com>
2021-05-11 12:13:44 +0800

    node-forge: add missing "sha-512" in hmac algorithm type

--- types/node-forge/index.d.ts
+++ types/node-forge/index.d.ts
@@ -756,7 +756,7 @@ declare module "node-forge" {
 
     namespace hmac {
 
-      type Algorithm = "sha1" | "md5" | "sha256";
+      type Algorithm = "sha1" | "md5" | "sha256" | "sha512";
 
       interface HMAC {
           digest(): util.ByteBuffer;
