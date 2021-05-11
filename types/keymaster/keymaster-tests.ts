83be6e4640d45b64d7694a3ca3835f5d275958dc
will <will@metawork.com>
2021-05-11 21:40:52 +0000

    fix tests

--- types/keymaster/keymaster-tests.ts
+++ types/keymaster/keymaster-tests.ts
@@ -7,6 +7,6 @@ key("shift+b", "aScope", (keyboardEvent, keymasterEvent) => {});
 // This re-sets the default filter method: https://www.npmjs.com/package/keymaster#filter-key-presses
 const element = new HTMLElement();
 key.filter = event => {
-    var tagName = (event.target || event.srcElement)!.tagName;
-    return !(tagName == "INPUT" || tagName == "SELECT" || tagName == "TEXTAREA");
+    const tagName = (event.target || event.srcElement)!.tagName;
+    return !(tagName === "INPUT" || tagName === "SELECT" || tagName === "TEXTAREA");
 };
