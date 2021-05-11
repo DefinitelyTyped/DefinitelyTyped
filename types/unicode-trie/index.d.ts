04937d7e819dc8462f971da652977a449a3f881e
Elijah Sheppard <esheppard@gmail.com>
2021-05-12 09:32:10 +1000

    .

--- types/unicode-trie/index.d.ts
+++ types/unicode-trie/index.d.ts
@@ -4,8 +4,6 @@
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
 
 /// <reference types="node" />
-/// <reference path="./builder.d.ts" />
-/// <reference path="./swap.d.ts" />
 
 declare class UnicodeTrie {
   constructor(data: Buffer | Uint8Array | { data: Uint32Array, highStart: number, errorValue: number });
