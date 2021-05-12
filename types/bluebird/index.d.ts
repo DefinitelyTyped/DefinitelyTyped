13bca8674292f1bcffda72ce55b85714ef9624f6
Adam Gastineau <adamg@cypress.io>
2021-05-12 12:06:06 -0700

    Properly typed promisifyAll

--- types/bluebird/index.d.ts
+++ types/bluebird/index.d.ts
@@ -40,6 +40,29 @@ type CatchFilter<E> = ((error: E) => boolean) | (object & E);
 type Resolvable<R> = R | PromiseLike<R>;
 type IterateFunction<T, R> = (item: T, index: number, arrayLength: number) => Resolvable<R>;
 
+type PromisifyAllKeys<T> = T extends string ? T | `${T}Async` : T
+type GetValueForKey<T, K> = K extends keyof T ? T[K] : never
+type WithoutLast<T extends any[]> = T extends [...infer A, any] ? A : []
+type Last<T extends any[]> = T extends [...any[], infer L] ? L : never
+type ExtractCallbackValueType<T> = T extends (error: any, ...data: infer D) => any ? D : never
+type ExtractAsyncMethod<T> = T extends (...args: infer A) => any ? (...arg: WithoutLast<A>) => Promise<ExtractCallbackValueType<Last<Required<A>>>[0]> : never
+type ValidPromiseOrNever<T> = ExtractAsyncMethod<T> extends (...args: any[]) => Promise<never> ? never : ExtractAsyncMethod<T>
+
+type PromisifyAllItems<T> = {
+  [Key in PromisifyAllKeys<keyof T>]: Key extends `${infer S}Async` ?
+    S extends keyof T ?
+      ValidPromiseOrNever<T[S]> :
+      GetValueForKey<T, Key> :
+    GetValueForKey<T, Key>
+}
+
+type NonNeverKeys<T> = ({
+  [Key in keyof T]: T[Key] extends never ? never : Key
+})[keyof T]
+
+// Drop `never` values
+type PromisifyAll<T> = Pick<PromisifyAllItems<T>, NonNeverKeys<PromisifyAllItems<T>>>
+
 declare class Bluebird<R> implements PromiseLike<R>, Bluebird.Inspection<R> {
   readonly [Symbol.toStringTag]: "Object";
 
@@ -775,7 +798,7 @@ declare class Bluebird<R> implements PromiseLike<R>, Bluebird.Inspection<R> {
    * if you `promisifyAll()` the node.js `fs` object use `fs.statAsync()` to call the promisified `stat` method.
    */
   // TODO how to model promisifyAll?
-  static promisifyAll<T extends object>(target: T, options?: Bluebird.PromisifyAllOptions<T>): T;
+  static promisifyAll<T extends object>(target: T, options?: Bluebird.PromisifyAllOptions<T>): PromisifyAll<T>;
 
   /**
    * Returns a promise that is resolved by a node style callback function.
