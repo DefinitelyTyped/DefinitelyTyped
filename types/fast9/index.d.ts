f4b5cbe3509bbb4bbf55d086c13c607a41dd6161
Juan José González Giraldo <juanjose.eng@gmail.com>
2021-05-12 16:40:03 -0500

    Add all exported functions of fast9 package
    What: Add 3 new functions missing from initial types
    Why: I completely miss them

--- types/fast9/index.d.ts
+++ types/fast9/index.d.ts
@@ -4,6 +4,9 @@
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
 
 export function detect(im: Uint8Array, width: number, height: number, threshold: number, nonmax: boolean): Corner[];
+export function fast9_detect(im: Uint8Array, xsize: number, ysize: number, b: number): Corner[];
+export function fast9_score(im: Uint8Array, xsize: number, ysize: number, corners: Corner[], b: number): number;
+export function nonmax_suppression(corners: Corner[], scores: number[]): Corner[];
 
 export interface Corner {
   x: number;
