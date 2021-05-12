be97cc449e7c597f10e1a8a4cf21514ee51db383
shenhaoliang <rolandshen@etop.com>
2021-05-12 19:23:18 +0800

    setting改为可选参数 修改test文件

--- types/watermark-dom/watermark-dom-tests.ts
+++ types/watermark-dom/watermark-dom-tests.ts
@@ -1,7 +1,6 @@
-import watermarkDom from "watermark-dom";
 // $ExpectType void
-watermarkDom.init({ watermark_txt: "测试水印，1021002301，测试水印，100101010111101" });
+watermark.init({ watermark_txt: "测试水印，1021002301，测试水印，100101010111101" });
 // $ExpectType void
-watermarkDom.load({ watermark_txt: "测试水印，1021002301，测试水印，100101010111101" });
+watermark.load({ watermark_txt: "测试水印，1021002301，测试水印，100101010111101" });
 // $ExpectType void
-watermarkDom.remove();
+watermark.remove();
