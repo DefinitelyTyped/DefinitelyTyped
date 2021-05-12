8ac3c5bb52ede93801823ea0b96f97f6a6ff1ecf
Adam Gastineau <adamg@cypress.io>
2021-05-12 12:53:27 -0700

    Linted tests

--- types/bluebird/index.d.ts
+++ types/bluebird/index.d.ts
@@ -2,7 +2,7 @@
 // Project: https://github.com/petkaantonov/bluebird
 // Definitions by: Leonard Hecker <https://github.com/lhecker>
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
-// Minimum TypeScript Version: 4.1
+// Minimum TypeScript Version: 4.2
 
 /*!
  * The code following this comment originates from:
@@ -40,28 +40,32 @@ type CatchFilter<E> = ((error: E) => boolean) | (object & E);
 type Resolvable<R> = R | PromiseLike<R>;
 type IterateFunction<T, R> = (item: T, index: number, arrayLength: number) => Resolvable<R>;
 
-type PromisifyAllKeys<T> = T extends string ? T | `${T}Async` : T
-type GetValueForKey<T, K> = K extends keyof T ? T[K] : never
-type WithoutLast<T extends any[]> = T extends [...infer A, any] ? A : []
-type Last<T extends any[]> = T extends [...any[], infer L] ? L : never
-type ExtractCallbackValueType<T> = T extends (error: any, ...data: infer D) => any ? D : never
-type ExtractAsyncMethod<T> = T extends (...args: infer A) => any ? (...arg: WithoutLast<A>) => Promise<ExtractCallbackValueType<Last<Required<A>>>[0]> : never
-type ValidPromiseOrNever<T> = ExtractAsyncMethod<T> extends (...args: any[]) => Promise<never> ? never : ExtractAsyncMethod<T>
+type PromisifyAllKeys<T> = T extends string ? T | `${T}Async` : T;
+type GetValueForKey<T, K> = K extends keyof T ? T[K] : never;
+type WithoutLast<T extends any[]> = T extends [...infer A, any] ? A : [];
+type Last<T extends any[]> = T extends [...any[], infer L] ? L : never;
+type ExtractCallbackValueType<T> = T extends (error: any, ...data: infer D) => any ? D : never;
+type ExtractAsyncMethod<T> = T extends (...args: infer A) => any
+    ? (...arg: WithoutLast<A>) => Promise<ExtractCallbackValueType<Last<Required<A>>>[0]>
+    : never;
+type ValidPromiseOrNever<T> = ExtractAsyncMethod<T> extends (...args: any[]) => Promise<never>
+    ? never
+    : ExtractAsyncMethod<T>;
 
 type PromisifyAllItems<T> = {
-  [Key in PromisifyAllKeys<keyof T>]: Key extends `${infer S}Async` ?
-    S extends keyof T ?
-      ValidPromiseOrNever<T[S]> :
-      GetValueForKey<T, Key> :
-    GetValueForKey<T, Key>
-}
+    [Key in PromisifyAllKeys<keyof T>]: Key extends `${infer S}Async`
+        ? S extends keyof T
+            ? ValidPromiseOrNever<T[S]>
+            : GetValueForKey<T, Key>
+        : GetValueForKey<T, Key>;
+};
 
-type NonNeverKeys<T> = ({
-  [Key in keyof T]: T[Key] extends never ? never : Key
-})[keyof T]
+type NonNeverKeys<T> = {
+    [Key in keyof T]: T[Key] extends never ? never : Key;
+}[keyof T];
 
 // Drop `never` values
-type PromisifyAll<T> = Pick<PromisifyAllItems<T>, NonNeverKeys<PromisifyAllItems<T>>>
+type PromisifyAll<T> = Pick<PromisifyAllItems<T>, NonNeverKeys<PromisifyAllItems<T>>>;
 
 declare class Bluebird<R> implements PromiseLike<R>, Bluebird.Inspection<R> {
   readonly [Symbol.toStringTag]: "Object";
