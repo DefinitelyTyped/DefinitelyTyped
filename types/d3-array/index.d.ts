1a12edbd44dc12d7c42e8bbdf22f5baf6cccc59b
Nathan Bierema <nbierema@gmail.com>
2021-05-12 21:04:51 -0400

    Unnecessary constructor

--- types/d3-array/index.d.ts
+++ types/d3-array/index.d.ts
@@ -1084,12 +1084,10 @@ export function thresholdSturges(values: ArrayLike<number | undefined>): number;
  * The InternMap class extends the native JavaScript Map class, allowing Dates and other non-primitive keys by bypassing the SameValueZero algorithm when determining key equality.
  */
 export class InternMap<K = any, V = any> extends Map<K, V> {
-    constructor(entries?: ReadonlyArray<readonly [K, V]> | null);
 }
 
 /**
  * The InternSet class extends the native JavaScript Set class, allowing Dates and other non-primitive keys by bypassing the SameValueZero algorithm when determining key equality.
  */
 export class InternSet<T = any> extends Set<T> {
-    constructor(values?: readonly T[] | null);
 }
