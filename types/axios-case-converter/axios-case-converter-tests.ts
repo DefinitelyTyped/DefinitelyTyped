9635b2c43fc7ed57f9cd953d3cd6f6fa6b000c76
Dimitri B <BendingBender@users.noreply.github.com>
2021-05-18 17:28:13 +0200

    [axios-case-converter] Remove, bundles its own types (#53022)

--- types/axios-case-converter/axios-case-converter-tests.ts
+++
@@ -1,6 +0,0 @@
-import axios from "axios";
-import applyConverters from "axios-case-converter";
-
-applyConverters(); // $ExpectError
-
-applyConverters(axios.create()); // $ExpectType AxiosInstance
