caa4e190b762d8ea9d748b88e73f96e15522d7fa
Federico Panico <fedemp@users.noreply.github.com>
2021-05-18 13:36:40 -0300

    New @ckeditor/ckeditor5-special-characters (#52970)

---
+++ types/ckeditor__ckeditor5-special-characters/src/specialcharacterstext.d.ts
@@ -0,0 +1,6 @@
+import { Plugin } from "@ckeditor/ckeditor5-core";
+
+export default class SpecialCharactersText extends Plugin {
+    static readonly pluginName: "SpecialCharactersText";
+    init(): void;
+}
