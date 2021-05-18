ceb68b609ab5b5ad90d556a0607e5a34c8b8c71e
Almeida <almeidx@pm.me>
2021-05-18 17:20:41 +0100

    fixed return function return types

--- types/color-thief-node/color-thief-node-tests.ts
+++ types/color-thief-node/color-thief-node-tests.ts
@@ -4,9 +4,11 @@ const imageUrl =
     "https://upload.wikimedia.org/wikipedia/commons/thumb/6/67/Firefox_Logo%2C_2017.svg/1024px-Firefox_Logo%2C_2017.svg.png";
 
 getColorFromURL(imageUrl).then(color => {
-    color.r; // $ExpectType number
-    color.g; // $ExpectType number
-    color.b; // $ExpectType number
+    // $ExpectType Palette
+    color;
 });
 
-getPaletteFromURL(imageUrl); // $ExpectType Promise<Color>
+getPaletteFromURL(imageUrl).then(color => {
+    // $ExpectType Palette
+    color[0];
+});
