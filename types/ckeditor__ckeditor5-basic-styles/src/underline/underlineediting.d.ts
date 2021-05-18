57ce096593aea5a416203f58eec93619ffd1b164
Federico Panico <fedemp@users.noreply.github.com>
2021-05-18 13:14:15 -0300

    New @ckeditor/ckeditor5-basic-styles v27 (#52974)

---
+++ types/ckeditor__ckeditor5-basic-styles/src/underline/underlineediting.d.ts
@@ -0,0 +1,6 @@
+import { Plugin } from "@ckeditor/ckeditor5-core";
+
+export default class UnderlineEditing extends Plugin {
+    static readonly pluginName: "UnderlineEditing";
+    init(): void;
+}
