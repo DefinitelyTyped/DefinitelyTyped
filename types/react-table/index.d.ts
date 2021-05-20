d02df438a99cfef69926345cef581d87cb198077
minhoe <minhoe1122@gmail.com>
2021-05-20 16:11:38 +0900

    add semicolon

--- types/react-table/index.d.ts
+++ types/react-table/index.d.ts
@@ -238,7 +238,7 @@ export interface UseTableHooks<D extends object> extends Record<string, any> {
 export interface UseTableColumnOptions<D extends object> {
     id?: IdType<D>;
     Header?: Renderer<HeaderProps<D>>;
-    Footer?: Renderer<FooterProps<D>>
+    Footer?: Renderer<FooterProps<D>>;
     width?: number | string;
     minWidth?: number;
     maxWidth?: number;
@@ -320,7 +320,7 @@ export type HeaderProps<D extends object> = TableInstance<D> & {
     column: ColumnInstance<D>;
 };
 
-export type FooterProps<D extends object> = TableInstance<D> & {}
+export type FooterProps<D extends object> = TableInstance<D> & {};
 
 export type CellProps<D extends object, V = any> = TableInstance<D> & {
     column: ColumnInstance<D>;
