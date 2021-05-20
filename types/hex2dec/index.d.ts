1135303d15d5528710cb34408a5a974dda3807fb
Joshua Hoeflich <josh@joshuahoeflich.com>
2021-05-20 17:52:28 -0500

    Fix a type error in the hex2dec definition

--- types/hex2dec/index.d.ts
+++ types/hex2dec/index.d.ts
@@ -1,13 +1,15 @@
-// Type definitions for hex2dec 1.1
+// Type definitions for hex2dec 2.0
 // Project: https://github.com/donmccurdy/hex2dec#readme
 // Definitions by: Adriaan Knapen <https://github.com/Addono>
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
 
 /**
- * Takes a hexadecimal string and returns it as a decimal string.
+ * Takes a hexadecimal string and, when the hexedecimal is valid,
+ * returns it as a decimal string.
  */
-export function hexToDec(hexStr: string): string;
+export function hexToDec(hexStr: string): string | null;
 /**
- * Takes a decimal string and returns it as a hexadecimal string.
+ * Takes a decimal string and, when the decimal is valid,
+ * returns it as a hexadecimal string.
  */
-export function decToHex(decStr: string, opts?: { prefix?: boolean }): string;
+export function decToHex(decStr: string, opts?: { prefix?: boolean }): string | null;
