efb82c55c14692388c44a04fe0fd7aa05afd6ea0
Alexandru Radovici <msg4alex@gmail.com>
2021-05-20 23:52:06 +0300

    Use Array<T>

--- types/node-red__registry/index.d.ts
+++ types/node-red__registry/index.d.ts
@@ -200,7 +200,7 @@ declare namespace registry {
          * Send a message to the nodes wired.
          * @param msg A message or array of messages to send
          */
-        send(msg?: NodeMessage | (NodeMessage | undefined | null)[]): void;
+        send(msg?: NodeMessage | Array<NodeMessage | undefined | null>): void;
         /**
          * Receive a message.
          *
