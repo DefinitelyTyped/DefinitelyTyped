da3001a68b232648e9552ada4b21968272b26f0a
MIYAZAKI Hiromu <miyazaki@osstech.co.jp>
2021-05-21 19:23:10 +0900

    fix argument type for verifyCertificateChain

--- types/node-forge/index.d.ts
+++ types/node-forge/index.d.ts
@@ -392,7 +392,13 @@ declare module "node-forge" {
 
         function createCaStore(certs?: ReadonlyArray<Certificate | pki.PEM>): CAStore;
 
-        function verifyCertificateChain(caStore: CAStore, chain: Certificate[], customVerifyCallback?: (verified: boolean | string, depth: number, chain: Certificate[]) => boolean): boolean;
+        function verifyCertificateChain(
+            caStore: CAStore,
+            chain: Certificate[],
+            options?: ((verified: boolean | string, depth: number, certs: Certificate[]) => boolean) | {
+                verify?: (verified: boolean | string, depth: number, certs: Certificate[]) => boolean,
+                validityCheckDate?: Date | null
+            }): boolean;
 
         function pemToDer(pem: PEM): util.ByteStringBuffer;
 
--- types/node-forge/node-forge-tests.ts
+++ types/node-forge/node-forge-tests.ts
@@ -229,6 +229,13 @@ if (forge.util.fillString('1', 5) !== '11111') throw Error('forge.util.fillStrin
         return true;
     });
 
+    forge.pki.verifyCertificateChain(caStore, [certificate], {
+        verify: (verified, depth, chain) => {
+            return true;
+        },
+        validityCheckDate: new Date()
+    });
+
     certificate.issued(certificate);
     certificate.isIssuer(certificate);
 }
