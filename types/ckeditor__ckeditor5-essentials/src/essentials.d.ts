e91858b444692313292500e3126e7932b005f90a
Federico Panico <federicopanico@gmail.com>
2021-05-23 16:40:01 -0300

    New @ckeditor/ckeditor5-select-all v27

--- types/ckeditor__ckeditor5-essentials/src/essentials.d.ts
+++ types/ckeditor__ckeditor5-essentials/src/essentials.d.ts
@@ -1,14 +1,13 @@
 import { Plugin } from "@ckeditor/ckeditor5-core";
 import { Enter, ShiftEnter } from "@ckeditor/ckeditor5-enter";
+import { SelectAll } from "@ckeditor/ckeditor5-select-all";
 import { Typing } from "@ckeditor/ckeditor5-typing";
 import { Undo } from "@ckeditor/ckeditor5-undo";
 // TODO: import {Clipboard} from "@ckeditor/ckeditor5-clipboard";
 type Clipboard = typeof Plugin;
-// TODO: import {SelectAll} from "@ckeditor/ckeditor5-select-all";
-type SelectAll = typeof Plugin;
 
 export default class Essentials extends Plugin {
-    static requires: [Clipboard, typeof Enter, SelectAll, typeof ShiftEnter, typeof Typing, typeof Undo];
+    static requires: [Clipboard, typeof Enter, typeof SelectAll, typeof ShiftEnter, typeof Typing, typeof Undo];
 
     static pluginName: "Essentials";
 }
