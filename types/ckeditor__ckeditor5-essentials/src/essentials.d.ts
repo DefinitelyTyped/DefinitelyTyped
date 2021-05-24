a1ac0de3fe1b0f6db435afe072c5ec02aea145ce
Federico Panico <fedemp@users.noreply.github.com>
2021-05-24 08:25:44 -0300

    New @ckeditor/ckeditor5-clipboard (#53350)

--- types/ckeditor__ckeditor5-essentials/src/essentials.d.ts
+++ types/ckeditor__ckeditor5-essentials/src/essentials.d.ts
@@ -1,13 +1,12 @@
+import { Clipboard } from "@ckeditor/ckeditor5-clipboard";
 import { Plugin } from "@ckeditor/ckeditor5-core";
 import { Enter, ShiftEnter } from "@ckeditor/ckeditor5-enter";
 import { SelectAll } from "@ckeditor/ckeditor5-select-all";
 import { Typing } from "@ckeditor/ckeditor5-typing";
 import { Undo } from "@ckeditor/ckeditor5-undo";
-// TODO: import {Clipboard} from "@ckeditor/ckeditor5-clipboard";
-type Clipboard = typeof Plugin;
 
 export default class Essentials extends Plugin {
-    static requires: [Clipboard, typeof Enter, typeof SelectAll, typeof ShiftEnter, typeof Typing, typeof Undo];
+    static requires: [typeof Clipboard, typeof Enter, typeof SelectAll, typeof ShiftEnter, typeof Typing, typeof Undo];
 
     static pluginName: "Essentials";
 }
