daa85c38ba839f7f635dd1d63830fd25e89987ab
Paito Anderson <pj.paito@gmail.com>
2021-05-24 16:57:24 -0400

    Finish services

--- types/chec__commerce.js/features/services.d.ts
+++ types/chec__commerce.js/features/services.d.ts
@@ -1,10 +1,20 @@
 import Commerce = require('@chec/commerce.js');
 
+type LocaleListCountriesResponse = {
+  countries: { [name: string]: string };
+  html: string;
+}
+
+type LocaleListSubdivisionsResponse = {
+  subdivisions: { [name: string]: string };
+  html: string;
+}
+
 export class Services {
   constructor(commerce: Commerce);
 
-  localeListCountries(): Promise<any>;
-  localeListShippingCountries(token: string): Promise<any>;
-  localeListShippingSubdivisions(token: string, countryCode: string): Promise<any>;
-  localeListSubdivisions(countryCode: string): Promise<any>;
+  localeListCountries(): Promise<LocaleListCountriesResponse>;
+  localeListShippingCountries(token: string): Promise<LocaleListCountriesResponse>;
+  localeListShippingSubdivisions(token: string, countryCode: string): Promise<LocaleListSubdivisionsResponse>;
+  localeListSubdivisions(countryCode: string): Promise<LocaleListSubdivisionsResponse>;
 }
--- types/chec__commerce.js/test/services.ts
+++ types/chec__commerce.js/test/services.ts
@@ -2,8 +2,13 @@ import Commerce = require('@chec/commerce.js');
 
 const commerce = new Commerce('{your_public_key}');
 
+const token = 'checkout_token_id';
 const country = 'US';
 
 commerce.services.localeListCountries().then(response => {});
+
+commerce.services.localeListShippingCountries(token).then(response => {});
+
+commerce.services.localeListShippingSubdivisions(token, country).then(response => {});
 
 commerce.services.localeListSubdivisions(country).then(response => {});
