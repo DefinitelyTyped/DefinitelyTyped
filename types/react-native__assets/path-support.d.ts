35f86a7b501e245ec590fe0c0dd119c9ded45497
Zihan Chen (MSFT) <53799235+ZihanChen-MSFT@users.noreply.github.com>
2021-05-25 11:48:20 -0700

    Format code

--- types/react-native__assets/path-support.d.ts
+++ types/react-native__assets/path-support.d.ts
@@ -1,5 +1,5 @@
-import { PackagerAsset } from "./registry"
+import { PackagerAsset } from './registry';
 
-export function getAndroidResourceFolderName(asset: PackagerAsset, scale: number): string
-export function getAndroidResourceIdentifier(asset: PackagerAsset): string
-export function getBasePath(asset: PackagerAsset): string
+export function getAndroidResourceFolderName(asset: PackagerAsset, scale: number): string;
+export function getAndroidResourceIdentifier(asset: PackagerAsset): string;
+export function getBasePath(asset: PackagerAsset): string;
--- types/react-native__assets/registry.d.ts
+++ types/react-native__assets/registry.d.ts
@@ -1,15 +1,14 @@
 export type PackagerAsset = {
-    __packager_asset: boolean
-    fileSystemLocation: string
-    httpServerLocation: string
-    width: null | undefined | number
-    height: null | undefined | number
-    scales: Array<number>
-    hash: string
-    name: string
-    type: string
-  }
-  
-  export function registerAsset(asset: PackagerAsset): number
-  export function getAssetByID(assetId: number): PackagerAsset
-  
+    __packager_asset: boolean;
+    fileSystemLocation: string;
+    httpServerLocation: string;
+    width: null | undefined | number;
+    height: null | undefined | number;
+    scales: Array<number>;
+    hash: string;
+    name: string;
+    type: string;
+};
+
+export function registerAsset(asset: PackagerAsset): number;
+export function getAssetByID(assetId: number): PackagerAsset;
