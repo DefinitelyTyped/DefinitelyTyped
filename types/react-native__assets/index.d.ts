cd3b715def13fe8ab13f5cb4084af050c1ece7d3
Zihan Chen (MSFT) <53799235+ZihanChen-MSFT@users.noreply.github.com>
2021-05-25 11:51:33 -0700

    Update index.d.ts

--- types/react-native__assets/index.d.ts
+++ types/react-native__assets/index.d.ts
@@ -2,3 +2,5 @@
 // Project: https://github.com/facebook/react-native#readme
 // Definitions by: Zihan Chen <https://github.com/ZihanChen-MSFT>
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
+
+// intended to leave blank
