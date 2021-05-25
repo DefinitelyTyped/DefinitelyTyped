7694f6e936a0c0ed55af3aef4b39603d2da0930c
Zihan Chen (MSFT) <53799235+ZihanChen-MSFT@users.noreply.github.com>
2021-05-25 11:45:40 -0700

    Initial commit for @react-native/assets

---
+++ types/react-native__assets/registry.d.ts
@@ -0,0 +1,15 @@
+export type PackagerAsset = {
+    __packager_asset: boolean
+    fileSystemLocation: string
+    httpServerLocation: string
+    width: null | undefined | number
+    height: null | undefined | number
+    scales: Array<number>
+    hash: string
+    name: string
+    type: string
+  }
+  
+  export function registerAsset(asset: PackagerAsset): number
+  export function getAssetByID(assetId: number): PackagerAsset
+  
