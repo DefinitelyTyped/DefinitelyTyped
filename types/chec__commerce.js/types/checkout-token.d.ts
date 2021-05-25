6d5ca3c19859fc7805658f5ecc6126aa2351d31f
Paito Anderson <pj.paito@gmail.com>
2021-05-24 20:14:44 -0400

    Fix Test

--- types/chec__commerce.js/types/checkout-token.d.ts
+++ types/chec__commerce.js/types/checkout-token.d.ts
@@ -11,94 +11,94 @@ export interface CheckoutToken {
   cart_id: string | null;
   created: number;
   expires: number;
-  analytics: any,
+  analytics: any;
   conditionals: {
-    collects_fullname: boolean,
-    collects_shipping_address: boolean,
-    collects_billing_address: boolean,
-    has_physical_delivery: boolean,
-    has_digital_delivery: boolean,
-    has_available_discounts: boolean,
-    has_pay_what_you_want: boolean,
-    collects_extra_fields: boolean,
-    is_cart_free: boolean
-  },
+    collects_fullname: boolean;
+    collects_shipping_address: boolean;
+    collects_billing_address: boolean;
+    has_physical_delivery: boolean;
+    has_digital_delivery: boolean;
+    has_available_discounts: boolean;
+    has_pay_what_you_want: boolean;
+    collects_extra_fields: boolean;
+    is_cart_free: boolean;
+  };
   collects: {
-    fullname: boolean,
-    shipping_address: boolean,
-    billing_address: boolean,
-    extra_fields: boolean
-  },
+    fullname: boolean;
+    shipping_address: boolean;
+    billing_address: boolean;
+    extra_fields: boolean;
+  };
   has: {
-    physical_delivery: boolean,
-    digital_delivery: boolean,
-    available_discounts: boolean,
-    pay_what_you_want: boolean,
-  },
+    physical_delivery: boolean;
+    digital_delivery: boolean;
+    available_discounts: boolean;
+    pay_what_you_want: boolean;
+  };
   is: {
     cart_free: boolean,
-  },
+  };
   line_items: CheckoutTokenLineItem[];
-  merchant: Merchant,
-  extra_fields: Extrafield[],
-  products: Product[],
+  merchant: Merchant;
+  extra_fields: Extrafield[];
+  products: Product[];
   gateways: {
     available: {
-      test_gateway: boolean,
-      stripe: boolean,
-      square: boolean,
-      paypal: boolean,
-      paymill: boolean,
-      razorpay: boolean,
-      manual: boolean,
-    },
-    available_count: number,
+      test_gateway: boolean;
+      stripe: boolean;
+      square: boolean;
+      paypal: boolean;
+      paymill: boolean;
+      razorpay: boolean;
+      manual: boolean;
+    };
+    available_count: number;
     test_gateway?: {
-      type: string,
-      settings: any,
-    },
+      type: string;
+      settings: any;
+    };
     stripe?: {
-      type: string,
+      type: string;
       settings: {
-        publishable_key: string,
-      },
-      cards_accepted: string[],
-    },
+        publishable_key: string;
+      };
+      cards_accepted: string[];
+    };
     square?: {
-      type: string,
+      type: string;
       settings: {
-        application_id: string,
-        square_merchant_id: string,
-      },
-      cards_accepted: string[],
-    },
+        application_id: string;
+        square_merchant_id: string;
+      };
+      cards_accepted: string[];
+    };
     paypal?: {
-      type: string,
+      type: string;
       settings: {
-        email: string,
+        email: string;
       },
     },
     paymill?: {
-      type: string,
+      type: string;
       settings: {
-        public_key: string,
+        public_key: string;
       },
-      cards_accepted: string[],
+      cards_accepted: string[];
     },
     razorpay?: {
-      type: string,
+      type: string;
       settings: {
-        key_id: string,
-      },
-    },
-    manual?: {
-      id: string,
-      name: string,
-      details: any,
-    }[],
-  },
-  shipping_methods: ShippingMethod[],
-  live: Live,
+        key_id: string;
+      };
+    };
+    manual?: Array<{
+      id: string;
+      name: string;
+      details: any;
+    }>;
+  };
+  shipping_methods: ShippingMethod[];
+  live: Live;
 }
 
 export interface CheckoutTokenLineItem {
@@ -107,10 +107,10 @@ export interface CheckoutTokenLineItem {
   name: string;
   image: string | null;
   description: string | null;
-  quantity: number,
-  price: Price,
-  subtotal: Price,
-  variants: SelectedVariant[],
+  quantity: number;
+  price: Price;
+  subtotal: Price;
+  variants: SelectedVariant[];
   conditionals: {
     is_active: boolean;
     is_free: boolean;
@@ -121,28 +121,28 @@ export interface CheckoutTokenLineItem {
     has_physical_delivery: boolean;
     has_images: boolean;
     has_video: boolean;
-    collects_fullname:boolean;
+    collects_fullname: boolean;
     collects_shipping_address: boolean;
     collects_billing_address: boolean;
     collects_extra_fields: boolean;
-  }
+  };
   is: {
     active: boolean;
     free: boolean;
     pay_what_you_want: boolean;
     inventory_managed: boolean;
     sold_out: boolean;
-  },
+  };
   has: {
       digital_delivery: boolean;
       physical_delivery: boolean;
       images: boolean;
       video: boolean;
-  },
+  };
   collects: {
       fullname: boolean;
       shipping_address: boolean;
       billing_address: boolean;
       extra_fields: boolean;
-  }
+  };
 }
