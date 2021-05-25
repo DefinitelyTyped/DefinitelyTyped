8a81eeadf00db3575d1a203642d910d78138dce5
Ruriko Araki <ruaraki@microsoft.com>
2021-05-25 11:41:22 -0700

    Add baseline files

---
+++ types/react-native/v63/test/stylesheet-flatten.tsx
@@ -0,0 +1,22 @@
+import * as React from 'react';
+import { View, Text, ViewStyle, StyleSheet } from 'react-native';
+
+interface Props {
+    style: ViewStyle;
+}
+
+export function App(props: Props) {
+    const backgroundColor = (StyleSheet.flatten([styles.container, props.style]) as ViewStyle).backgroundColor;
+
+    return (
+        <View style={[styles.container, props.style]}>
+            <Text>{backgroundColor}</Text>
+        </View>
+    );
+}
+
+const styles = StyleSheet.create({
+    container: {
+        backgroundColor: 'red',
+    },
+});
