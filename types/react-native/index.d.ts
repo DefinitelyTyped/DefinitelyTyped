a7a68fc62820e1323e28d830e559adffdb8d915e
Andrew Coates <30809111+acoates-ms@users.noreply.github.com>
2021-05-26 17:40:51 -0700

    Add missing localeIdentifier property on I18nManager (#53393)
    * Add missing localeIdentifier property on I18nManager
    
    * Update types/react-native/index.d.ts
    
    Co-authored-by: Christian Ost <hallo@christianost.de>
    
    Co-authored-by: Christian Ost <hallo@christianost.de>

--- types/react-native/index.d.ts
+++ types/react-native/index.d.ts
@@ -9170,6 +9170,7 @@ export interface I18nManagerStatic {
     getConstants: () => {
         isRTL: boolean;
         doLeftAndRightSwapInRTL: boolean;
+        localeIdentifier?: string | null;
     };
     allowRTL: (allowRTL: boolean) => void;
     forceRTL: (forceRTL: boolean) => void;
