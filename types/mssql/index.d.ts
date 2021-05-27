9b3bed3f27ebb7b16c022e997a542b0b5d134a85
Daniel Hensby <git@hens.by>
2021-05-27 18:51:16 +0100

    Add msnodesqlv8 dep

--- types/mssql/index.d.ts
+++ types/mssql/index.d.ts
@@ -18,6 +18,7 @@
 
 import events = require('events');
 import tds = require('tedious');
+import msnodesql = require('msnodesqlv8');
 import { Pool } from 'tarn';
 import { CallbackOrPromise, PoolOptions } from 'tarn/dist/Pool';
 export interface ISqlType {
@@ -72,7 +73,7 @@ export declare var Geography: ISqlTypeFactoryWithNoParams;
 export declare var Geometry: ISqlTypeFactoryWithNoParams;
 export declare var Variant: ISqlTypeFactoryWithNoParams;
 
-export type Connection = tds.Connection;
+export type Connection = tds.Connection | msnodesql.Connection;
 
 export declare var TYPES: {
     VarChar: ISqlTypeFactoryWithLength;
