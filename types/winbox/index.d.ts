cbf6e3c024875ff1a9ae21130d145815d287b283
cesco69 <francesco.bagnoli.69@gmail.com>
2021-05-28 12:13:14 +0200

    Update index.d.ts

--- types/winbox/index.d.ts
+++ types/winbox/index.d.ts
@@ -1,4 +1,4 @@
-// Type definitions for winbox 0.1
+// Type definitions for winbox 0.2
 // Project: https://nextapps-de.github.io/winbox/
 // Definitions by: Piotr Błażejewicz <https://github.com/peterblazejewicz>
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
