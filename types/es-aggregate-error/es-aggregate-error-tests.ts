c957f584bb9a96bbc29baa7a483db60b35166c9d
AverageHelper <1500092+AverageHelper@users.noreply.github.com>
2021-05-29 10:02:01 -0600

    Add typings for individual parts of es-aggregate-error

--- types/es-aggregate-error/es-aggregate-error-tests.ts
+++ types/es-aggregate-error/es-aggregate-error-tests.ts
@@ -1,17 +1,21 @@
-import AggregateError = require("es-aggregate-error");
+import AggregateError = require('es-aggregate-error');
+import 'es-aggregate-error/auto';
+import 'es-aggregate-error/implementation';
+import 'es-aggregate-error/polyfill';
+import 'es-aggregate-error/shim';
 
-const oneError = new RangeError("hi!");
-const otherError = new EvalError("oops");
+const oneError = new RangeError('hi!');
+const otherError = new EvalError('oops');
 
 new AggregateError(); // $ExpectError
-new AggregateError("this is not an error"); // $ExpectError
-AggregateError([oneError, otherError], "this is two kinds of errors"); // $ExpectError
+new AggregateError('this is not an error'); // $ExpectError
+AggregateError([oneError, otherError], 'this is two kinds of errors'); // $ExpectError
 
 new AggregateError([]); // $ExpectType AggregateError
 new AggregateError([oneError, otherError]); // $ExpectType AggregateError
 
 // $ExpectType AggregateError
-const error = new AggregateError([oneError, otherError], "this is two kinds of errors");
+const error = new AggregateError([oneError, otherError], 'this is two kinds of errors');
 
 AggregateError.shim; // $ExpectType () => void
 AggregateError.shim(); // $ExpectType: void
@@ -20,4 +24,4 @@ error.errors; // $ExpectType: Array<unknown>
 error.name; // $ExpectType: "AggregateError"
 error.message; // $ExpectType: string
 
-error.name = "something else"; // $ExpectError
+error.name = 'something else'; // $ExpectError
