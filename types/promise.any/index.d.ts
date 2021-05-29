9f479681a54248fbf462ed438ad0d9fc080813ab
AverageHelper <1500092+AverageHelper@users.noreply.github.com>
2021-05-29 10:48:41 -0600

    Fix lint errors

--- types/promise.any/index.d.ts
+++ types/promise.any/index.d.ts
@@ -2,10 +2,11 @@
 // Project: https://github.com/es-shims/Promise.any#readme
 // Definitions by: AverageHelper <https://github.com/AverageHelper>
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
+// Minimum TypeScript Version: 3.9
 
-import implementation = require("./implementation");
-import getPolyfill = require("./polyfill");
-import shim = require("./shim");
+import implementation = require('./implementation');
+import getPolyfill = require('./polyfill');
+import shim = require('./shim');
 
 type ExportedImplementationType = typeof implementation & {
     getPolyfill: typeof getPolyfill;
--- types/promise.any/promise.any-tests.ts
+++ types/promise.any/promise.any-tests.ts
@@ -1,5 +1,5 @@
-import assert = require("assert");
-import any = require("promise.any");
+import assert = require('assert');
+import any = require('promise.any');
 
 const resolved = Promise.resolve(42);
 const rejected = Promise.reject(-1);
@@ -9,22 +9,22 @@ any(); // $ExpectError
 
 any([]); // $ExpectType Promise<never>
 
-any([resolved, rejected, alsoRejected]).then(function (result) {
+any([resolved, rejected, alsoRejected]).then(result => {
     assert.strictEqual(result, 42);
 });
 
-any([rejected, alsoRejected]).catch(function (error) {
+any([rejected, alsoRejected]).catch(error => {
     assert.ok(error instanceof AggregateError);
     assert.strictEqual(error.errors, [-1, Infinity]);
 });
 
 any.shim();
 
-Promise.any([resolved, rejected, alsoRejected]).then(function (result) {
+Promise.any([resolved, rejected, alsoRejected]).then(result => {
     assert.strictEqual(result, 42);
 });
 
-Promise.any([rejected, alsoRejected]).catch(function (error) {
+Promise.any([rejected, alsoRejected]).catch(error => {
     assert.ok(error instanceof AggregateError);
     assert.strictEqual(error.errors, [-1, Infinity]);
 });
