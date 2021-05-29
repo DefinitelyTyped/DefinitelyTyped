4e26f85c5cf17e860b8af0373c6c79d950b1166d
Josh Dales <joshdales@pm.me>
2021-05-29 16:07:01 -0400

    Use 'in keyof' for useParam generics

--- types/reach__router/index.d.ts
+++ types/reach__router/index.d.ts
@@ -177,6 +177,6 @@ export function useLocation(): WindowLocation;
 
 export function useNavigate(): NavigateFn;
 
-export function useParams<TParams extends { [param: string]: string | undefined } = {}>(): TParams;
+export function useParams<TParams extends { [Param in keyof TParams]?: string } = {}>(): TParams;
 
 export function useMatch(pathname: string): null | { uri: string; path: string; [param: string]: string };
