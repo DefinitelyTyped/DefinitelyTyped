155f92b8d01ff5d6d51798dcbe7d28092bb6b251
Sarah Sporck <sporck@integreat-app.de>
2021-05-29 14:04:20 +0200

    remove conditional type

--- types/styled-components/index.d.ts
+++ types/styled-components/index.d.ts
@@ -255,9 +255,7 @@ export type StyledComponentInnerComponent<C extends React.ComponentType<any>> = 
     : C;
 export type StyledComponentPropsWithRef<
     C extends keyof JSX.IntrinsicElements | React.ComponentType<any>
-> = C extends React.ComponentType<infer I>
-    ? React.ComponentPropsWithRef<StyledComponentInnerComponent<React.ComponentType<I>>>
-    : React.ComponentPropsWithRef<C>;
+> = React.ComponentPropsWithRef<C>;
 export type StyledComponentInnerOtherProps<C extends AnyStyledComponent> = C extends StyledComponent<
     any,
     any,
