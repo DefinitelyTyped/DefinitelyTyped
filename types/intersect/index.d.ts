1ca46cfbaaad4cc8b68d4fc48a5c6381f4c499df
Noah Overcash <me@ncovercash.dev>
2021-05-30 02:28:07 -0400

    [intersect] Use read-only arrays

--- types/intersect/index.d.ts
+++ types/intersect/index.d.ts
@@ -8,17 +8,17 @@ declare const intersect: {
      * Find the intersection of arrays a and b.
      * The individual arrays are expected to be sets, i.e. there's no duplicate elements in them.
      */
-    <T>(a: T[], b: T[]): T[];
+    <T>(a: ReadonlyArray<T>, b: ReadonlyArray<T>): T[];
     /**
      * Find the intersection of all subarrays.
      * The individual arrays are expected to be sets, i.e. there's no duplicate elements in them.
      */
-    <T>(arrays: T[][]): T[];
+    <T>(arrays: ReadonlyArray<ReadonlyArray<T>>): T[];
     /**
      * Find the intersection of two large arrays.
      * May perform better than default when there are over approximately 125 elements
      */
-    big<T>(a: T[], b: T[]): T[];
+    big<T>(a: ReadonlyArray<T>, b: ReadonlyArray<T>): T[];
 };
 
 export = intersect;
