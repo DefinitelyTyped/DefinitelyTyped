d67888f8f9b241a81f32d769a4693750081d0b0e
Aaron Reisman <lifeiscontent@users.noreply.github.com>
2021-05-31 20:46:33 -0700

    Update index.d.ts
    Closes #28629

--- types/styled-components/index.d.ts
+++ types/styled-components/index.d.ts
@@ -311,9 +311,9 @@ type WithOptionalTheme<P extends { theme?: T }, T> = Omit<P, 'theme'> & {
 type AnyIfEmpty<T extends object> = keyof T extends never ? any : T;
 
 export interface ThemedStyledComponentsModule<T extends object, U extends object = T> {
-    default: ThemedStyledInterface<T>;
+    default: ThemedBaseStyledInterface<T>;
 
-    css: ThemedCssFunction<T>;
+    css: BaseThemedCssFunction<T>;
 
     // unfortunately keyframes can't interpolate props from the theme
     keyframes(strings: TemplateStringsArray | CSSKeyframes, ...interpolations: SimpleInterpolation[]): Keyframes;
@@ -323,7 +323,7 @@ export interface ThemedStyledComponentsModule<T extends object, U extends object
         ...interpolations: Array<Interpolation<ThemedStyledProps<P, T>>>
     ): GlobalStyleComponent<P, T>;
 
-    withTheme: WithThemeFnInterface<T>;
+    withTheme: BaseWithThemeFnInterface<T>;
     ThemeProvider: ThemeProviderComponent<T, U>;
     ThemeConsumer: React.Consumer<T>;
     ThemeContext: React.Context<T>;
