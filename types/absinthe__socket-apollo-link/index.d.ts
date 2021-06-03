ff23defe712901791ac9b2005bfe16200f2d4029
Dean Pienaar <dean@flickswitch.co.za>
2021-06-03 16:11:01 +0200

    @absinthe/socket-apollo-link: Fix Version and Remove Unneeded Types

--- types/absinthe__socket-apollo-link/index.d.ts
+++ types/absinthe__socket-apollo-link/index.d.ts
@@ -1,16 +1,14 @@
-// Type definitions for @absinthe/socket-apollo-link 0.2.1
+// Type definitions for @absinthe/socket-apollo-link 0.2
 // Project: https://github.com/absinthe-graphql/absinthe-socket/tree/master/packages/absinthe-phoenix-socket-apollo-link#readme
 // Definitions by: Maarten van Vliet <https://github.com/maartenvanvliet>
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
 // Minimum TypeScript Version: 3.8
 
-import { NextLink, Operation, Observable, FetchResult, ApolloLink } from '@apollo/client/core';
+import { ApolloLink } from '@apollo/client/link/core/ApolloLink';
 import { AbsintheSocket, Notifier } from '@absinthe/socket';
-
-export interface AbsintheSocketLink extends ApolloLink {}
 
 export function createAbsintheSocketLink(
     absintheSocket: AbsintheSocket,
     onError?: (error: Error) => any,
     onStart?: (notifier: Notifier) => any,
-): AbsintheSocketLink;
+): ApolloLink;
