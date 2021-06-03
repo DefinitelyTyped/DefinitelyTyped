c0c377d77207b5d95b3ad1575bbe89b5b0d65b20
Dean Pienaar <dean@flickswitch.co.za>
2021-06-03 15:44:27 +0200

    @abinsthe/socket-apollo-link: Correctly Extend ApolloLink Interface

--- types/absinthe__socket-apollo-link/index.d.ts
+++ types/absinthe__socket-apollo-link/index.d.ts
@@ -4,10 +4,10 @@
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
 // Minimum TypeScript Version: 3.8
 
-import { NextLink, Operation, Observable, FetchResult } from '@apollo/client/core';
+import { NextLink, Operation, Observable, FetchResult, ApolloLink } from '@apollo/client/core';
 import { AbsintheSocket, Notifier } from '@absinthe/socket';
 
-export interface AbsintheSocketLink {
+export interface AbsintheSocketLink extends ApolloLink {
     request: (operation: Operation, forward: NextLink | undefined) => Observable<FetchResult> | null;
 }
 
