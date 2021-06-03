c39cae4a9a7dfa19d0cf8c364e2f2f0e3478cdbe
Peter Hedenskog <peter@soulgalore.com>
2021-06-03 13:16:39 +0200

    Adding render blocking and LCP info
    In Browsertime and coming Chrome 92 we will have support for knowing if a request is render blocking or not. We also will be able to know if an image is the largest Contentful paint (or not).
    
    See https://github.com/micmro/PerfCascade/pull/278

--- types/har-format/index.d.ts
+++ types/har-format/index.d.ts
@@ -497,6 +497,10 @@ export interface Entry {
     _was_pushed?: number | string | null;
     /** _non-standard_  */
     _initialPriority?: string | null;
+    /** _non-standard_  */
+    _renderBlocking?: string | null;
+    /** _non-standard_  */
+    _isLCP?: boolean | null;
 }
 /**
  * This object contains detailed info about performed request.
