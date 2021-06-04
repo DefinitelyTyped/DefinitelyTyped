76e30faac65b813902cfff9f8e257b72880cab87
g.bado <g.bado@anoki.it>
2021-06-04 12:03:16 +0200

    Add cheet.js types

---
+++ types/cheet.js/cheet.js-tests.ts
@@ -0,0 +1,40 @@
+import { cheet } from 'cheet.js';
+
+const sequences = {
+    cross: 'up down left right',
+    circle: 'left up right down',
+};
+
+// Main object
+
+cheet('↑ ↑ ↓ ↓ ← → ← → b a');
+cheet('h a n d l e r', (str, seq) => {});
+cheet('m u l t i p l e h a n d l e r s', {
+    done(str, seq) {},
+    fail(str) {},
+    next(str, key, num, seq) {},
+});
+
+// Global handlers
+
+cheet(sequences.cross);
+cheet(sequences.circle);
+cheet.done(seq => {
+    if (seq === sequences.cross) {
+    } else {
+    }
+});
+cheet.fail(seq => {
+    if (seq === sequences.cross) {
+    } else {
+    }
+});
+cheet.next((str, key, num, seq) => {
+    if (seq.join(' ') === sequences.cross) {
+    } else {
+    }
+});
+
+// reset() and disable()
+cheet.reset(sequences.cross);
+cheet.disable(sequences.circle);
