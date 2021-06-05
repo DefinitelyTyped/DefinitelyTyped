59e252b78bcb24ac3ca37602416aeac2c47fd8ad
hebertcisco <mrrabbitomg@gmail.com>
2021-06-05 19:34:17 +0000

    fix expmidd_tests

--- types/expmidd/expmidd-tests.ts
+++ types/expmidd/expmidd-tests.ts
@@ -1,15 +1,31 @@
-import express = require('express');
-import { Request, Response } from 'express-serve-static-core';
+import * as http from 'http';
+//import express = require('express');
+
+//import { Request, Response } from 'express';
 //import Cors = require('cors');
 //import runMiddleware = require('expmidd');
 
 /*const cors = Cors({
     methods: ['GET'],
 });*/
-namespace expmidd_tests {
+/*namespace expmidd_tests {
     const app = express();
     app.get('/', async (req: Request, res: Response) => {
        // await runMiddleware(req, res, cors);
         res.send('hello world');
+    });
+}*/
+
+
+namespace expmidd_tests {
+    const host = 'localhost';
+    const port = 8000;
+    const requestListener = function (req, res) {
+        res.writeHead(200);
+        res.end("hello world");
+    };
+    const server = http.createServer(requestListener);
+    server.listen(port, host, () => {
+        console.log(`Server is running on http://${host}:${port}`);
     });
 }
