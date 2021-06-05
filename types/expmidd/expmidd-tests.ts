365290904f85a4e8611cbc2459f24b7ee8aad43c
hebertcisco <mrrabbitomg@gmail.com>
2021-06-05 18:59:16 +0000

    Fixing expmidd test

--- types/expmidd/expmidd-tests.ts
+++ types/expmidd/expmidd-tests.ts
@@ -1,13 +1,14 @@
-import express = require('express');
+import { Request, Response } from 'express';
 import runMiddleware from 'expmidd';
 import * as Cors from 'cors';
+import * as express from 'express';
 
 const cors = Cors({
     methods: ['GET'],
 });
 namespace expmidd_tests {
     const app = express();
-    app.get('/', async (req, res) => {
+    app.get('/', async (req: Request, res: Response) => {
         await runMiddleware(req, res, cors);
         res.send('hello world');
     });
