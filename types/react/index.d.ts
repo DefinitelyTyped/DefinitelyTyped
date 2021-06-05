76ea88ea9cbf38179428af719a92736c1590c09b
steven nguyen <nguyeste008@students.garlandisd.net>
2021-06-04 19:35:23 -0500

    Add `Partial<S>` to setState
    `Partial<S>` makes all properties of S optional: `type Partial<T> = { [P in keyof T]?: T[P] | undefined; }`
    `Pick<S, K>`, described as "from T, pick a set of properties in the union K" : `type Pick<T, K extends keyof T> = { [P in K]: T[P]; }`
    
    But wait, `setState` allows passing in an object that only has some properties, right?

--- types/react/index.d.ts
+++ types/react/index.d.ts
@@ -480,7 +480,7 @@ declare namespace React {
         // See: https://github.com/DefinitelyTyped/DefinitelyTyped/issues/18365#issuecomment-351013257
         // Also, the ` | S` allows intellisense to not be dumbisense
         setState<K extends keyof S>(
-            state: ((prevState: Readonly<S>, props: Readonly<P>) => (Pick<S, K> | S | null)) | (Pick<S, K> | S | null),
+            state: ((prevState: Readonly<S>, props: Readonly<P>) => (Pick<S, K> | Partial<S> | S | null)) | (Pick<S, K> | Partial<S> | S | null),
             callback?: () => void
         ): void;
 
