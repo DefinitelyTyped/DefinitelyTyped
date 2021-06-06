7da78fbcc087bbca552dabed6cb0efbe9a12d177
Brenek Harrison <brenekharrison@gmail.com>
2021-06-06 12:41:33 -0600

    Add type definition for sound-play

---
+++ types/sound-play/index.d.ts
@@ -0,0 +1,6 @@
+// Type definitions for sound-play 1.1
+// Project: https://github.com/leharry/sound-play
+// Definitions by: Brenek Harrison <https://github.com/BrenekH>
+// Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
+
+export function play(path: string, volume?: number): Promise<unknown>;
