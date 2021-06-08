20e2ef248fe6d255169c239192b7744f5aadd499
eps1lon <silbermann.sebastian@gmail.com>
2021-06-08 09:49:13 +0200

    Move unstable_useOpaqueIdentifier to alpha as well

--- types/react/test/alpha.tsx
+++ types/react/test/alpha.tsx
@@ -70,3 +70,23 @@ function suspenseTest() {
         );
     }
 }
+
+function Dialog() {
+    const nameId = React.unstable_useOpaqueIdentifier();
+
+    return (
+        <div role="dialog" aria-labelledby={nameId}>
+            <h2 id={nameId}></h2>
+        </div>
+    );
+}
+
+function InvalidOpaqueIdentifierUsage() {
+    const id = React.unstable_useOpaqueIdentifier();
+    // undesired, would warn in React should not type-check
+    const stringified1: string = id.toString();
+    // undesired, would warn in React should not type-check
+    const stringified2: string = id + '';
+
+    return null;
+}
