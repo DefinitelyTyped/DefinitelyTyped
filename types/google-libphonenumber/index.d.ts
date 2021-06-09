6f297f9e73b9c11f54547cf95f650655606e23e2
larsilus <lars.laegner@gmail.com>
2021-06-09 18:35:58 +0200

    Update index.ts googlelibphonenumber (missing numbers)
    Updated according to https://github.com/google/libphonenumber/blob/c6277266fba8223cfc610cfb1e999deb9f876d65/javascript/i18n/phonenumbers/phonenumberutil.js

--- types/google-libphonenumber/index.d.ts
+++ types/google-libphonenumber/index.d.ts
@@ -95,10 +95,31 @@ declare namespace libphonenumber {
 
     export module PhoneNumberUtil {
         export enum ValidationResult {
+          /** The number length matches that of valid numbers for this region. =0 */
             IS_POSSIBLE,
+          /** The number has an invalid country calling code. =1 */
             INVALID_COUNTRY_CODE,
+          /** The number is shorter than all valid numbers for this region. =2 */
             TOO_SHORT,
-            TOO_LONG
+          /** The number is longer than all valid numbers for this region. =3 */
+            TOO_LONG,
+          /**
+           * The number length matches that of local numbers for this region only (i.e.
+           * numbers that may be able to be dialled within an area, but do not have all
+           * the information to be dialled from anywhere inside or outside the country).
+           * =4
+           */
+          IS_POSSIBLE_LOCAL_ONLY,
+          /**
+           * The number is longer than the shortest valid numbers for this region,
+           * shorter than the longest valid numbers for this region, and does not itself
+           * have a number length that matches valid numbers for this region.
+           * This can also be returned in the case where
+           * isPossibleNumberForTypeWithReason was called, and there are no numbers of
+           * this type at all for this region.
+           * =5
+           */
+          INVALID_LENGTH
         }
 
         export enum MatchType {
