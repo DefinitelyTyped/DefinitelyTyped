e16ff44331ba57d03e2d3728ec52f1f82cdf4ea8
Richie Bendall <richiebendall@gmail.com>
2021-06-10 23:00:26 +1200

    Improve typings for `universalify`
    Signed-off-by: Richie Bendall <richiebendall@gmail.com>

--- types/universalify/universalify-tests.ts
+++ types/universalify/universalify-tests.ts
@@ -1,4 +1,15 @@
 import universalify = require("universalify");
 
-universalify.fromCallback(() => { }); // $ExpectType (...args: any[]) => void | Promise<any>
-universalify.fromPromise(() => { }); // $ExpectType (...args: any[]) => void | Promise<any>
+const callbackFunction = universalify.fromCallback((value: number, callback: (value: number) => void) => {
+    callback(value + 1);
+});
+
+const promiseFunction = universalify.fromPromise(async (value: number) => {
+    return value + 1;
+});
+
+callbackFunction(1, value => value); // $ExpectType void
+callbackFunction(1); // $ExpectType Promise<number>
+
+promiseFunction(1, value => value); // $ExpectType void
+promiseFunction(1); // $ExpectType Promise<number>
