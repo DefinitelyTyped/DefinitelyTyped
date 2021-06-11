84ab4e5c4d99474dafca8d43b2a37762fa23aec5
Yeoh, Ewe Seong <ewe-seong.yeoh@seb.se>
2021-06-11 15:25:25 +0800

    fix tslint missing semicolon

--- types/react-show-more-text/react-show-more-text-tests.tsx
+++ types/react-show-more-text/react-show-more-text-tests.tsx
@@ -16,4 +16,4 @@ import ShowMoreText from 'react-show-more-text';
     unchanged. It was popularised in the 1960s with the release of Letraset sheets containing
     Lorem Ipsum passages, and more recently with desktop publishing software like Aldus
     PageMaker including versions of Lorem Ipsum.
-</ShowMoreText>
+</ShowMoreText>;
