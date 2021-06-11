f075c216d1fa5fa22a8f2baa57dad683359e6533
Rami <rkhasawneh@bloombergindustry.com>
2021-06-11 00:19:22 -0400

    lint and tests

--- types/analytics-node/index.d.ts
+++ types/analytics-node/index.d.ts
@@ -8,10 +8,10 @@
 export = AnalyticsNode.Analytics;
 
 declare namespace AnalyticsNode {
-  type Identity = { 
-    userId?: string | number | undefined;
-    anonymousId?: string | number | undefined
-  };
+  interface Identity {
+    userId?: string | number;
+    anonymousId?: string | number;
+  }
 
   type Message = Identity & {
     type: string;
