8f7a4a1ff1089851b33dcc8faa2ff243ef757d49
Federico Panico <federicopanico@gmail.com>
2021-06-12 17:05:48 -0300

    New @ckeditor/ckeditor5-real-time-collaboration v27.1

---
+++ types/ckeditor__ckeditor5-real-time-collaboration/src/realtimecollaborativetrackchanges.d.ts
@@ -0,0 +1,5 @@
+import { Plugin } from '@ckeditor/ckeditor5-core';
+
+export default class RealTimeCollaborativeTrackChanges extends Plugin {
+    static readonly pluginName: 'RealTimeCollaborativeTrackChanges';
+}
