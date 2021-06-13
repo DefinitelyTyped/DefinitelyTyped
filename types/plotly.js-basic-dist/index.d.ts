0ae3f91756a80740b0e9f64de4c6d946aa51dc1f
Florian Dreier <dreier.florian@gmail.com>
2021-06-13 05:39:00 +0200

    chore: Update comment
    Co-authored-by: Piotr Błażejewicz (Peter Blazejewicz) <peterblazejewicz@users.noreply.github.com>

--- types/plotly.js-basic-dist/index.d.ts
+++ types/plotly.js-basic-dist/index.d.ts
@@ -1,6 +1,6 @@
 // Type definitions for plotly.js-basic-dist 1.54
 // Project: https://github.com/plotly/plotly.js#readme
-// Definitions by: Florian Dreier <https://github.com/me>
+// Definitions by: Florian Dreier <https://github.com/DreierF>
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
 
 import * as Plotly from 'plotly.js';
