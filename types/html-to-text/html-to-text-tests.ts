9115ec7f2991ed6f06a45cea143ee69c1bbddb6d
Chris. Webster <chris@webstech.net>
2021-06-12 21:34:45 -0700

    html-to-text: upgrade to v7
    A number of block-text-builder methods have added support for an
    options object in preference for options being separate parameters.
    
    This update adds support for the new interfaces and test cases to
    verify both the old and new interfaces work.
    
    Signed-off-by: Chris. Webster <chris@webstech.net>

--- types/html-to-text/html-to-text-tests.ts
+++ types/html-to-text/html-to-text-tests.ts
@@ -116,4 +116,54 @@ const fmtOptions: HtmlToTextOptions = {
         unorderedList: elementFormatter,
     },
 };
+console.log('Emit all elements with "--" wrapping');
 console.log(htmlToText(allElements, fmtOptions));
+
+const blockTextTestElements = '<a>a</a>\
+<blockquote>b</blockquote>\
+<p>1234567890123456789012345678901234567890</p>\
+<table></table>';
+
+const fmtOptionsDepr: HtmlToTextOptions = {
+    formatters: {
+        table: (elem, walk, builder, options) => {
+            builder.openBlock(options.leadingLineBreaks || 2);
+            builder.openTable();
+            builder.openTableRow();
+            builder.openTableCell(22);
+            builder.addInline("Cell 1 data", false);
+            builder.closeTableCell(2);
+            builder.openTableCell(6);
+            builder.addInline("Cell 2 data", false);
+            builder.closeTableCell(1);
+            builder.closeTableRow();
+            builder.closeTable(4);
+            // walk(elem.children, builder);
+            builder.closeBlock(options.trailingLineBreaks || 2);
+        },
+    },
+};
+console.log('Test deprecated table builder interfaces');
+console.log(htmlToText(blockTextTestElements, fmtOptionsDepr));
+
+const fmtOptionsTable: HtmlToTextOptions = {
+    formatters: {
+        table: (elem, walk, builder, options) => {
+            builder.openBlock({ leadingLineBreaks: options.leadingLineBreaks || 2 });
+            builder.openTable();
+            builder.openTableRow();
+            builder.openTableCell({ maxColumnWidth: 22});
+            builder.addInline("Cell 1 data", { noWordTransform: false });
+            builder.closeTableCell({ colspan: 2 });
+            builder.openTableCell({ maxColumnWidth: 6 });
+            builder.addInline("Cell 2 data", { noWordTransform: false });
+            builder.closeTableCell();
+            builder.closeTableRow();
+            builder.closeTable({ colSpacing: 4 });
+            // walk(elem.children, builder);
+            builder.closeBlock({ trailingLineBreaks: options.trailingLineBreaks || 2 });
+        },
+    },
+};
+console.log('Test current table builder interfaces');
+console.log(htmlToText(blockTextTestElements, fmtOptionsTable));
