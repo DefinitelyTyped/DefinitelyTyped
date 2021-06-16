e51d8690f0297e09cbe9168bd5824f6b9b4d1d6b
Nathan Bierema <nbierema@gmail.com>
2021-06-16 13:29:51 -0400

    Bump version number

--- types/d3-array/index.d.ts
+++ types/d3-array/index.d.ts
@@ -1,4 +1,4 @@
-// Type definitions for D3JS d3-array module 2.12
+// Type definitions for D3JS d3-array module 3.0
 // Project: https://github.com/d3/d3-array, https://d3js.org/d3-array
 // Definitions by: Alex Ford <https://github.com/gustavderdrache>
 //                 Boris Yankov <https://github.com/borisyankov>
@@ -8,7 +8,7 @@
 //                 Nathan Bierema <https://github.com/Methuselah96>
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
 
-// Last module patch version validated against: 2.12.1
+// Last module patch version validated against: 3.0.1
 
 // --------------------------------------------------------------------------
 // Shared Types and Interfaces
