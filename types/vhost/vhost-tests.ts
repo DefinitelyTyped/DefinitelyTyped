061a62088431844b0ece59a1be14c2843a11a3da
cambo <cambo@bure.com.au>
2021-06-16 21:25:32 +1000

    fix linting error in test

--- types/vhost/vhost-tests.ts
+++ types/vhost/vhost-tests.ts
@@ -3,6 +3,6 @@ import { Response, NextFunction } from 'express';
 
 // $ExpectType Handler
 vhost('hostname', (_req: Request, _res: Response, _next: NextFunction) => {
-    //check if vhost property exists on _req
+    // check if vhost property exists on _req
     if (!_req.vhost) throw new Error('missing vhost property in request argument');
 });
