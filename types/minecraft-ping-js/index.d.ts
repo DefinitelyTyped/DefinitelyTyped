e3f6985d0a23b37a521de9266cb9f462d3d0a74c
Honbra <54026315+HonbraDev@users.noreply.github.com>
2021-06-21 16:50:53 +0200

    Added minecraft-ping-js

---
+++ types/minecraft-ping-js/index.d.ts
@@ -0,0 +1,42 @@
+declare module "minecraft-ping-js" {
+    export function pingWithPromise (ip: string, port?: number): Promise<ServerInfo>
+    export function ping (ip: string, port: number, callback: (info: ServerInfo) => any): void
+  }
+  
+  export interface ServerInfo {
+    version: ServerVersion;
+    players: ServerPlayers;
+    description: ServerDescription;
+    favicon: ServerFavicon;
+    modinfo: ServerModInfo;
+    ping: ServerPing;
+  }
+  
+  export interface ServerVersion {
+    name: string;
+    protocol: number;
+  }
+  
+  export interface ServerPlayers {
+    online: number;
+    max: number;
+  }
+  
+  export interface ServerDescription {
+    text: string;
+  }
+  
+  export type ServerFavicon = string;
+  
+  export interface ServerModInfo {
+    type: string;
+    modList: ServerMod[];
+  }
+  
+  export interface ServerMod {
+    modid: string;
+    version: string;
+  }
+  
+  export type ServerPing = number;
+  
