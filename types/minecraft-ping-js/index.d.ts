ee0d2a1e4310c1000cd4025c0e0de47b1ccdbfdd
Honbra <54026315+HonbraDev@users.noreply.github.com>
2021-06-21 17:02:59 +0200

    Linted

--- types/minecraft-ping-js/index.d.ts
+++ types/minecraft-ping-js/index.d.ts
@@ -1,42 +1,39 @@
-declare module "minecraft-ping-js" {
-    export function pingWithPromise (ip: string, port?: number): Promise<ServerInfo>
-    export function ping (ip: string, port: number, callback: (info: ServerInfo) => any): void
-  }
-  
-  export interface ServerInfo {
+export function pingWithPromise(ip: string, port?: number): Promise<ServerInfo>;
+export function ping(ip: string, port: number, callback: (info: ServerInfo) => any): void;
+
+export interface ServerInfo {
     version: ServerVersion;
     players: ServerPlayers;
     description: ServerDescription;
     favicon: ServerFavicon;
     modinfo: ServerModInfo;
     ping: ServerPing;
-  }
-  
-  export interface ServerVersion {
+}
+
+export interface ServerVersion {
     name: string;
     protocol: number;
-  }
-  
-  export interface ServerPlayers {
+}
+
+export interface ServerPlayers {
     online: number;
     max: number;
-  }
-  
-  export interface ServerDescription {
+}
+
+export interface ServerDescription {
     text: string;
-  }
-  
-  export type ServerFavicon = string;
-  
-  export interface ServerModInfo {
+}
+
+export type ServerFavicon = string;
+
+export interface ServerModInfo {
     type: string;
     modList: ServerMod[];
-  }
-  
-  export interface ServerMod {
+}
+
+export interface ServerMod {
     modid: string;
     version: string;
-  }
-  
-  export type ServerPing = number;
-  
+}
+
+export type ServerPing = number;
