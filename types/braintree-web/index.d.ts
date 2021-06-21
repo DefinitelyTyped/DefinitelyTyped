434c419635320517678463d4c54841cbd1a9586e
Jack Clark <1485654+jackdclark@users.noreply.github.com>
2021-06-21 14:29:59 +0100

    chore: update package version

--- types/braintree-web/index.d.ts
+++ types/braintree-web/index.d.ts
@@ -1,4 +1,4 @@
-// Type definitions for Braintree-web 3.75
+// Type definitions for Braintree-web 3.78
 // Project: https://github.com/braintree/braintree-web
 // Definitions by: Guy Shahine <https://github.com/chlela>
 //                 Jason Buckner <https://github.com/jbuckner>
