38bc5db3f7e3fe3ede6a5ac57682cd5d3a01692f
Usama Ahsan <usama8800@gmail.com>
2021-06-23 23:36:34 +0500

    Fix PR comments

--- types/screenshot-desktop/index.d.ts
+++ types/screenshot-desktop/index.d.ts
@@ -1,33 +1,16 @@
 // Type definitions for screenshot-desktop 1.12
-// Project: https://github.com/baz/foo (Does not have to be to GitHub, but prefer linking to a source code repository rather than to a project website.)
+// Project: https://github.com/bencevans/screenshot-desktop (Does not have to be to GitHub, but prefer linking to a source code repository rather than to a project website.)
 // Definitions by: Usama Ahsan <https://github.com/usama8800>
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
 
-/// <reference types="../node" />
+/// <reference types="node" />
 
-/*~ Note that ES6 modules cannot directly export callable functions.
- *~ This file should be imported using the CommonJS-style:
- *~   import x = require('someLibrary');
- *~
- *~ Refer to the documentation to understand common
- *~ workarounds for this limitation of ES6 modules.
- */
+export = screenshotDesktop;
 
-/*~ This declaration specifies that the function
- *~ is the exported object from the file
- */
-export = ScreenshotDesktop;
+declare function screenshotDesktop(options?: { format?: screenshotDesktop.ImageFormat, screen?: screenshotDesktop.DisplayID }): Promise<Buffer>;
+declare function screenshotDesktop(options?: { filename: string, format?: screenshotDesktop.ImageFormat, screen?: screenshotDesktop.DisplayID }): Promise<string>;
 
-/*~ This example shows how to have multiple overloads for your function */
-declare function ScreenshotDesktop(options?: { format?: ScreenshotDesktop.ImageFormat, screen?: ScreenshotDesktop.DisplayID }): Promise<Buffer>;
-declare function ScreenshotDesktop(options?: { filename: string, format?: ScreenshotDesktop.ImageFormat, screen?: ScreenshotDesktop.DisplayID }): Promise<string>;
-
-/*~ If you want to expose types from your module as well, you can
- *~ place them in this block. Often you will want to describe the
- *~ shape of the return type of the function; that type should
- *~ be declared in here, as this example shows.
- */
-declare namespace ScreenshotDesktop {
+declare namespace screenshotDesktop {
     type DisplayID = number;
 
     type ImageFormat =
@@ -41,11 +24,6 @@ declare namespace ScreenshotDesktop {
         'tiff' |
         'wmf';
 
-    /*~ If the module also has properties, declare them here. For example,
-     *~ this declaration says that this code is legal:
-     *~   import f = require('myFuncLibrary');
-     *~   console.log(f.defaultName);
-     */
     const defaultName: string;
     let defaultLength: number;
     function listDisplays(): Promise<Array<{ id: DisplayID, name: string }>>;
