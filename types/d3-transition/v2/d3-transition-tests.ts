d2934ff04da1a3122b26855af896a26aa02c2dfb
Nathan Bierema <nbierema@gmail.com>
2021-06-25 19:48:28 -0400

    [d3-transition] Update types to v3

--- types/d3-transition/v2/d3-transition-tests.ts
+++ types/d3-transition/v2/d3-transition-tests.ts
@@ -225,20 +225,6 @@ paragraphsTransition = firstDivTransition.selectAll<HTMLParagraphElement, Paragr
     return this.querySelectorAll('p'); // 'this' type is HTMLElement, return type is HTMLParagraphElement
 });
 
-// selectChild() ---------------------------------------------------------------
-
-let one = document.querySelector("#one");
-let two = document.querySelector("#two");
-let t1 = selectAll([one, two]).data([1, 2]).transition().delay(function(d) { return d * 10; });
-let t2 = t1.selectChild("child");
-
-// selectChildren() ---------------------------------------------------------------
-
-one = document.querySelector("#one");
-two = document.querySelector("#two");
-t1 = selectAll([one, two]).data([1, 2]).transition().delay(function(d) { return d * 10; });
-t2 = t1.selectChildren("child");
-
 // filter () -----------------------------------------------------------------
 
 // Scenario 1: Filter retaining the element type of the select group (i.e. no type narrowing during filtering)
