3a617986994b4ccabacc3f7a11dad2c64d5ff9d0
Daniel Rose <danrose@gmx.de>
2021-06-25 09:58:27 +0200

    Remove unused import

--- types/socketcluster-client/index.d.ts
+++ types/socketcluster-client/index.d.ts
@@ -3,8 +3,6 @@
 // Definitions by: Daniel Rose <https://github.com/DanielRose>
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
 
-import { SCAuthEngine } from "sc-auth";
-
 export import factory = require("./lib/factory");
 export import AGClientSocket = require("./lib/clientsocket");
 
