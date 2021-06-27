5f63e73f2d2cb8714328746fd08471d40ecd5816
oof2win2 <honza.koco44@gmail.com>
2021-06-27 11:32:25 +0200

    :wrench: Run tests locally and fix them up

--- types/lsofi/index.d.ts
+++ types/lsofi/index.d.ts
@@ -1,6 +1,7 @@
-// Type definitions for lsofi 1.0.0
+// Type definitions for lsofi 1.0
 // Project: https://github.com/marionebl/lsofi
 // Definitions by: oof2win2 <https://github.com/oof2win2>
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
 
-export default function lsofi(port: number): Promise<number|null>;
+declare function lsofi(port: number): Promise<number|null>;
+export = lsofi;
