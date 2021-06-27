19231b86500f3e4cf2ff9ecb663aa582d7a6c55c
oof2win2 <honza.koco44@gmail.com>
2021-06-27 11:24:30 +0200

    :wrench: Proper metadata

--- types/lsofi/index.d.ts
+++ types/lsofi/index.d.ts
@@ -1,1 +1,6 @@
-export function lsofi(port: number): Promise<number|null>;
+// Type definitions for lsofi 1.0.0
+// Project: https://github.com/marionebl/lsofi
+// Definitions by: oof2win2 <https://github.com/oof2win2>
+// Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
+
+export default function lsofi(port: number): Promise<number|null>;
