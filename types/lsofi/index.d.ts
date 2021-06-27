2e487dc899f22116aae71e9fe40cb65b93f5feb7
oof2win2 <honza.koco44@gmail.com>
2021-06-27 10:54:48 +0200

    :wrench: Fixed tests

--- types/lsofi/index.d.ts
+++ types/lsofi/index.d.ts
@@ -1,8 +1,1 @@
-declare module "lsofi" {
-	/**
-	 * Check if a port is being used by a process or not
-	 * @param {number} port - The port to check
-	 * @returns {number|null} Process ID of the originating process or null if no process is listening to this port
-	 */
-	export default function lsofi(port: number): Promise<number|null>
-}
+export function lsofi(port: number): Promise<number|null>;
