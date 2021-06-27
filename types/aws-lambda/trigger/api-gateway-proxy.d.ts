b837f0672350a4e70596311459cd5dbce39a5d37
Carlos Yuste <carlos.yuste@col.vueling.com>
2021-06-27 10:30:49 +0200

    add iam auth

--- types/aws-lambda/trigger/api-gateway-proxy.d.ts
+++ types/aws-lambda/trigger/api-gateway-proxy.d.ts
@@ -133,10 +133,23 @@ export interface APIGatewayProxyEventV2 {
         accountId: string;
         apiId: string;
         authorizer?: {
-            jwt: {
+            jwt?: {
                 claims: { [name: string]: string | number | boolean | string[] };
                 scopes: string[];
-            };
+            },
+            iam?: {
+                accessKey: string,
+                accountId: string,
+                callerId: string,
+                cognitoIdentity: {
+                    amr: string[],
+                    identityId: string,
+                    identityPoolId: string,
+                },
+                principalOrgId: string,
+                userArn: string,
+                userId: string,
+            }
         };
         domainName: string;
         domainPrefix: string;
