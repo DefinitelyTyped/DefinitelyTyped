feeb3c661c5a3a5b51979274d01c700c0d9b030d
ToastHawaii <mar.pel@protonmail.com>
2021-06-28 23:43:55 +0200

    Fix generate-globals for meteor

---
+++ types/meteor/globals/fetch.d.ts
@@ -0,0 +1,4 @@
+declare export const Headers: typeof globalThis.Headers;
+declare export const Request: typeof globalThis.Request;
+declare export const Response: typeof globalThis.Response;
+declare export const fetch: typeof globalThis.fetch;
