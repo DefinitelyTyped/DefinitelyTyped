00a5d831e4f10df57552f566b42ed0d559580027
Nathan Shively-Sanders <293473+sandersn@users.noreply.github.com>
2021-06-29 15:39:38 -0700

    Inline IdleRequestCallback in requestidlecallback
    The type alias IdleRequestCallback can't be defined twice in 4.4, but inlining its definition allows older Typescripts to keep using requestidlecallback.

--- types/requestidlecallback/index.d.ts
+++ types/requestidlecallback/index.d.ts
@@ -21,12 +21,12 @@ interface IdleRequestOptions {
 type IdleCallbackHandle = number;
 
 interface Window {
-    requestIdleCallback(callback: IdleRequestCallback, options?: IdleRequestOptions): IdleCallbackHandle;
+    requestIdleCallback(callback: (deadline: IdleDeadline) => void, options?: IdleRequestOptions): IdleCallbackHandle;
     cancelIdleCallback(handle: number): void;
 }
 
-declare function requestIdleCallback(callback: IdleRequestCallback, options?: IdleRequestOptions): number;
+declare function requestIdleCallback(callback: (deadline: IdleDeadline) => void, options?: IdleRequestOptions): number;
 declare function cancelIdleCallback(handle: number): void;
 
-declare function request(callback: IdleRequestCallback, options?: IdleRequestOptions): IdleCallbackHandle;
+declare function request(callback: (deadline: IdleDeadline) => void, options?: IdleRequestOptions): IdleCallbackHandle;
 declare function cancel(handle: IdleCallbackHandle): void;
