e0c768b11700e7b9d5f3a7aa15694d9f35ccea35
Federico Panico <federicopanico@gmail.com>
2021-06-30 11:55:09 -0300

    Update @ckeditor/ckeditor5-basic-styles to v28

---
+++ types/ckeditor__ckeditor5-basic-styles/v27/src/bold.d.ts
@@ -0,0 +1,8 @@
+import { Plugin } from "@ckeditor/ckeditor5-core";
+import BoldEditing from "./bold/boldediting";
+import BoldUI from "./bold/boldui";
+
+export default class Bold extends Plugin {
+    static readonly requires: [typeof BoldEditing, typeof BoldUI];
+    static readonly pluginName: "Bold";
+}
