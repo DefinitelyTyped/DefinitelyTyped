5a062c6707e81fccf6f7a7ee2091684b2054ef76
Speedy <45142584+SpeedyCraftah@users.noreply.github.com>
2021-07-01 02:57:38 +0100

    Update async-exit-hook-tests.ts

--- types/async-exit-hook/async-exit-hook-tests.ts
+++ types/async-exit-hook/async-exit-hook-tests.ts
@@ -1,4 +1,4 @@
-import * as exitHook from "./";
+import exitHook = require("async-exit-hook");
 
 // you can add async hooks by accepting a callback
 exitHook(callback => {
@@ -28,21 +28,3 @@ exitHook.uncaughtExceptionHandler((err, callback) => {
 // Custom signal
 // Arguments are `signal, exitCode` (SIGBREAK is already handled, this is an example)
 exitHook.hookEvent('SIGBREAK', 21);
-
-// process event: `message` with a filter
-// filter gets all arguments passed to *handler*: `process.on(message, *handler*)`
-// Exits on process event `message` with msg `customShutdownMessage` only
-exitHook.hookEvent('message', 0, msg => msg !== 'customShutdownMessage');
-
-// All async hooks will work with uncaught errors when you have specified an uncaughtExceptionHandler
-throw new Error('awesome');
- 
-//=> // Sync uncaughtExcpetion hooks called and retun
-//=> '[Error: awesome]'
-//=> // Sync hooks called and retun
-//=> 'exiting'
-//=> 'exiting 2'
-//=> // Async uncaughtException hooks return
-//=> 'Sent error to cloud'
-//=> // Sync uncaughtException hooks return
-//=> 'exiting 3'
