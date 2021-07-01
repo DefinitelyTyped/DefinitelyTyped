239a7e133683dc78ce06f3ceddfaa0df05cac00f
Speedy <45142584+SpeedyCraftah@users.noreply.github.com>
2021-07-01 02:05:17 +0100

    commit files

---
+++ types/async-exit-hook/async-exit-hook-tests.ts
@@ -0,0 +1,48 @@
+import exitHook, { uncaughtExceptionHandler, hookEvent } from "./";
+
+// you can add async hooks by accepting a callback
+exitHook(callback => {
+    setTimeout(() => {
+        console.log("Waited 1 second, exiting...");
+        callback();
+    }, 1000);
+});
+
+// You can hook uncaught errors with uncaughtExceptionHandler(), consequently adding 
+// async support to uncaught errors (normally uncaught errors result in a synchronous exit).
+uncaughtExceptionHandler(err => {
+    console.error(err);
+});
+
+// You can add multiple uncaught error handlers
+// Add the second parameter (callback) to indicate async hooks
+uncaughtExceptionHandler((err, callback) => {
+    console.error(err);
+
+    setTimeout(() => {
+        console.log("Done:)");
+        callback();
+    }, 1000);
+});
+
+// Custom signal
+// Arguments are `signal, exitCode` (SIGBREAK is already handled, this is an example)
+hookEvent('SIGBREAK', 21);
+
+// process event: `message` with a filter
+// filter gets all arguments passed to *handler*: `process.on(message, *handler*)`
+// Exits on process event `message` with msg `customShutdownMessage` only
+hookEvent('message', 0, msg => msg !== 'customShutdownMessage');
+
+// All async hooks will work with uncaught errors when you have specified an uncaughtExceptionHandler
+throw new Error('awesome');
+ 
+//=> // Sync uncaughtExcpetion hooks called and retun
+//=> '[Error: awesome]'
+//=> // Sync hooks called and retun
+//=> 'exiting'
+//=> 'exiting 2'
+//=> // Async uncaughtException hooks return
+//=> 'Sent error to cloud'
+//=> // Sync uncaughtException hooks return
+//=> 'exiting 3'
