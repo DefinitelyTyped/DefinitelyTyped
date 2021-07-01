94c5dc74c4725cd302e502e540ef3955c8bfd366
illestomas <illestomi@azet.sk>
2021-07-01 23:42:24 +0200

    Added readonly modifier.

--- types/quick-picker/index.d.ts
+++ types/quick-picker/index.d.ts
@@ -28,169 +28,169 @@ declare module 'quick-picker' {
          * @default undefined
          * @platform both
          */
-        item?: T;
+        readonly item?: T;
 
         /**
          * Picker's items
          * @default [] // an empty array
          * @platform both
          */
-        items?: T[];
+        readonly items?: T[];
 
         /**
          * Callback function when an item is selected
          * @default undefined
          * @platform both
          */
-        onChange?: (item: T | Date) => void;
+        readonly onChange?: (item: T | Date) => void;
 
         /**
          * Callback function when Done/Ok button is pressed
          * @default undefined
          * @platform both
          */
-        onPressDone?: (item: T | Date) => void;
+        readonly onPressDone?: (item: T | Date) => void;
 
         /**
          * Callback function when Cancel button is pressed or when user taps Out
          * @default undefined
          * @platform both
          */
-        onTapOut?: () => void;
+        readonly onTapOut?: () => void;
 
         /**
          * Disable the picker top bar
          * @default false
          * @platform iOS
          */
-        disableTopRow?: boolean;
+        readonly disableTopRow?: boolean;
 
         /**
          * If you want to create your own custom top bar
          * @default undefined
          * @platform iOS
          */
-        topRow?: React.ReactNode;
+        readonly topRow?: React.ReactNode;
 
         /**
          * Done button text
          * @default "Done" (iOS) "Ok" (Android)
          * @platform both
          */
-        doneButtonText?: string;
+        readonly doneButtonText?: string;
 
         /**
          * Cancel button text
          * @default "Cancel"
          * @platform Android
          */
-        cancelButtonText?: string;
+        readonly cancelButtonText?: string;
 
         /**
          * Done (and Cancel on Android) button text style
          * @default undefined
          * @platform both
          */
-        doneButtonTextStyle?: TextStyle;
+        readonly doneButtonTextStyle?: TextStyle;
 
         /**
          * Android's modal view style
          * @default undefined
          */
-        androidModalStyle?: ViewStyle;
+        readonly androidModalStyle?: ViewStyle;
 
         /**
          * Android's modal items style
          * @default undefined
          */
-        androidItemStyle?: ViewStyle;
+        readonly androidItemStyle?: ViewStyle;
 
         /**
          * Android's modal items text style
          * @default undefined
          */
-        androidItemTextStyle?: TextStyle;
+        readonly androidItemTextStyle?: TextStyle;
 
         /**
          * Android's modal selected item text style
          * @default undefined
          */
-        androidSelectedItemStyle?: TextStyle;
+        readonly androidSelectedItemStyle?: TextStyle;
 
         /**
          * sets the picker to his normal mode or to time mode
          * @default "normal"
          * @platform both
          */
-        pickerType?: PickerType;
+        readonly pickerType?: PickerType;
 
         /**
          * mode if pickerType is set to "time"
          * @default undefined
          * @platform both
          */
-        mode?: PickerMode;
+        readonly mode?: PickerMode;
 
         /**
          * Defines the visual display of the picker for Android
          * @default undefined
          * @platform Android
          */
-        display?: PickerDisplayType;
+        readonly display?: PickerDisplayType;
 
         /**
          * Current selected date
          * @default new Date()
          */
-        date?: Date;
+        readonly date?: Date;
 
         /**
          * Maximum date
          * @default undefined
          * @platform both
          */
-        maximumDate?: Date;
+        readonly maximumDate?: Date;
 
         /**
          * Minimum date
          * @default undefined
          * @platform both
          */
-        minimumDate?: Date;
+        readonly minimumDate?: Date;
 
         /**
          * Allows changing of the timeZone of the date picker. By default it uses the device's time zone.
          * @default undefined
          * @platform both
          */
-        timeZoneOffsetInMinutes?: number;
+        readonly timeZoneOffsetInMinutes?: number;
 
         /**
          * Allows changing of the locale of the component. By default it uses the device's locale.
          * @default undefined
          * @platform iOS
          */
-        locale?: string;
+        readonly locale?: string;
 
         /**
          * Allows changing of the time picker to a 24 hour format.
          * @default undefined
          * @platform iOS
          */
-        is24Hour?: boolean;
+        readonly is24Hour?: boolean;
 
         /**
          * The interval at which minutes can be selected.
          * @default undefined
          * @platform iOS
          */
-        minuteInterval?: 1 | 2 | 6 | 5 | 4 | 3 | 10 | 12 | 15 | 20 | 30;
+        readonly minuteInterval?: 1 | 2 | 6 | 5 | 4 | 3 | 10 | 12 | 15 | 20 | 30;
 
         /**
          * Use native driver
          * @default true
          * @platform both
          */
-        useNativeDriver?: boolean;
+        readonly useNativeDriver?: boolean;
     }
 }
