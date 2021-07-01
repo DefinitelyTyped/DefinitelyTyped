944ceadbe26ab1b85480de00b53ada236cf8084a
joshuaharry <84721070+joshuaharry@users.noreply.github.com>
2021-07-01 09:53:08 -0500

    Fix more bugs with previous pull request

--- types/file-exists/file-exists-tests.ts
+++ types/file-exists/file-exists-tests.ts
@@ -1,11 +1,37 @@
 import fileExists = require('file-exists');
 
+// $ExpectType void
 fileExists('/index.html', (err, exists) => {
     err; // $ExpectType Error | null
     exists; // $ExpectType boolean
 });
 
+// $ExpectType void
 fileExists('/index.html', { root: '' }, (err, exists) => {
+    err; // $ExpectType Error | null
+    exists; // $ExpectType boolean
+});
+
+// $ExpectType void
+fileExists(Buffer.from('/tmp'), { root: '' }, (err, exists) => {
+    err; // $ExpectType Error | null
+    exists; // $ExpectType boolean
+});
+
+// $ExpectType void
+fileExists(Buffer.from('/tmp'), { anotherKey: 'this is fine' }, (err, exists) => {
+    err; // $ExpectType Error | null
+    exists; // $ExpectType boolean
+});
+
+// $ExpectType void
+fileExists(new URL('file:///etc/fstab'), { anotherKey: 'this is fine' }, (err, exists) => {
+    err; // $ExpectType Error | null
+    exists; // $ExpectType boolean
+});
+
+// $ExpectType void
+fileExists("tmp", { root: '/' }, (err, exists) => {
     err; // $ExpectType Error | null
     exists; // $ExpectType boolean
 });
@@ -16,6 +42,13 @@ fileExists('/index.html', { root: '' }); // $ExpectType Promise<boolean>
 fileExists(Buffer.from('/index.html')); // $ExpectType Promise<boolean>
 fileExists(new URL('file:///etc/fstab')); // $ExpectType Promise<boolean>
 
+fileExists(Buffer.from('/index.html'), { root: '' }); // $ExpectType Promise<boolean>
+fileExists(Buffer.from('/index.html'), {}); // $ExpectType Promise<boolean>
+fileExists(new URL('file:///etc/fstab'), { otherKey: '', thisIsFine: '/' }); // $ExpectType Promise<boolean>
+fileExists('index.html', { root: '/' }); // $ExpectType Promise<boolean>
+
 // $ExpectType boolean
 fileExists.sync('/index.html');
+
+// $ExpectType boolean
 fileExists.sync('/index.html', { root: '' });
--- types/file-exists/index.d.ts
+++ types/file-exists/index.d.ts
@@ -1,4 +1,4 @@
-// Type definitions for file-exists 4.0
+// Type definitions for file-exists 5.0
 // Project: https://github.com/scottcorgan/file-exists
 // Definitions by: BendingBender <https://github.com/BendingBender>
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
@@ -9,13 +9,25 @@ export = fileExists;
 
 type FilePathType = string | Buffer | URL;
 
+interface FalsyRoot {
+    root?: '';
+    [otherKey: string]: any;
+}
+
 // Promise APIs
-declare function fileExists(filepath: FilePathType, options?: fileExists.Options): Promise<boolean>;
+declare function fileExists(filepath: FilePathType): Promise<boolean>;
+declare function fileExists(filepath: FilePathType, options: FalsyRoot): Promise<boolean>;
+declare function fileExists(filepath: string, options: fileExists.Options): Promise<boolean>;
 
 // Callback APIs
 declare function fileExists(filepath: FilePathType, callback: (err: Error | null, exists: boolean) => void): void;
 declare function fileExists(
     filepath: FilePathType,
+    options: FalsyRoot,
+    callback: (err: Error | null, exists: boolean) => void,
+): void;
+declare function fileExists(
+    filepath: string,
     options: fileExists.Options,
     callback: (err: Error | null, exists: boolean) => void,
 ): void;
