1c91838047a75d04f39e56e248f0cfa7d87f1307
Nikita Parfenov <nktpfn@doubletapp.ai>
2021-07-03 00:45:02 +0500

    Add test example

--- types/react-text-mask/react-text-mask-tests.ts
+++ types/react-text-mask/react-text-mask-tests.ts
@@ -1,4 +1,18 @@
-import { conformToMask } from "react-text-mask";
+import MaskedInput, { conformToMask } from "react-text-mask";
 
 // $ExpectType conformToMaskResult
 conformToMask("123", ["(", /\d/, /\d/, ")"]);
+
+// playground: https://codesandbox.io/s/falling-sun-xdthu?file=/src/App.js:67-353
+function App() {
+    return (
+      <div className="App">
+        {/* throwing error after typing */}
+        <MaskedInput />
+        {/* throwing error after render */}
+        <MaskedInput mask={true} />
+        {/* works fine */}
+        <MaskedInput mask={false} />
+      </div>
+    );
+  }
