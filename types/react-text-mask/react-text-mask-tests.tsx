b3ec1ccb157b7399712387888f93c6a286828981
Nikita Parfenov <nktpfn@doubletapp.ai>
2021-07-03 01:36:31 +0500

    Add @ts-expect-error comments above previus types examples

--- types/react-text-mask/react-text-mask-tests.tsx
+++ types/react-text-mask/react-text-mask-tests.tsx
@@ -7,13 +7,15 @@ conformToMask("123", ["(", /\d/, /\d/, ")"]);
 // playground: https://codesandbox.io/s/falling-sun-xdthu?file=/src/App.js:67-353
 const Test: React.FC = () => {
     return (
-      <div className="App">
-        {/* throwing error after typing */}
-        <MaskedInput />
-        {/* throwing error after render */}
-        <MaskedInput mask={true} />
-        {/* works fine */}
-        <MaskedInput mask={false} />
-      </div>
+        <div className="App">
+            {/* throwing error after typing */}
+            {/* @ts-expect-error */}
+            <MaskedInput />
+            {/* throwing error after render */}
+            {/* @ts-expect-error */}
+            <MaskedInput mask={true} />
+            {/* works fine */}
+            <MaskedInput mask={false} />
+        </div>
     );
-  }
+};
