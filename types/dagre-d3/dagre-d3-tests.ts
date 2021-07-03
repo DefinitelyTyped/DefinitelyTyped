361d9242649aea2ea0a6583f8c6c12193be970f3
Chanwut (Mick) Kittivorawong <chanwutk@octoml.ai>
2021-07-02 17:52:49 -0700

    add types to all exports of dagre-d3; add corresponding tests

--- types/dagre-d3/dagre-d3-tests.ts
+++ types/dagre-d3/dagre-d3-tests.ts
@@ -1,15 +1,93 @@
-import * as graphlib from "graphlib";
-import { render } from "dagre-d3";
+import { dagre, intersect, render, util } from "dagre-d3";
 import * as d3 from "d3";
 
-const graph = new graphlib.Graph();
+const graph = new dagre.graphlib.Graph<{extraProp: string}>();
 
 // has graph methods from dagre.d.ts
 graph.setNode("a", {});
 
+const node: dagre.Node<{extraProp: string}> = graph.node("a");
+
+// has extraProp in Graph"s Generic type
+const _extraProp: string = node.extraProp;
+
+
+// intersect
+const point = {x: 0, y: 0};
+const _ = intersect.node(node, point);
+const {x: _xcircle, y: _ycircle} = intersect.circle(node, 0, point);
+const {x: _xellipse, y: _yellipse} = intersect.ellipse(node, 0, 0, point);
+const {x: _xpolygon, y: _ypolygon} = intersect.polygon(node, [point], point);
+const {x: _xrect, y: _yrect} = intersect.rect(node, point);
+
+// render
 const renderer = new render();
-renderer.arrows()["arrowType"] = (parent: d3.Selection<any, any, any, any>, id: string, edge: dagre.Edge, type: string) => { };
-renderer.shapes()["shapeName"] = (parent: d3.Selection<any, any, any, any>, bbox: any, node: dagre.Node) => { };
+
+const arrows = renderer.arrows();
+renderer.arrows({
+  ...arrows,
+  arrowType: (parent: d3.Selection<any, any, any, any>, _id: string, _edge: dagre.Edge, _type: string) => { }
+});
+
+const shapes = renderer.shapes();
+renderer.shapes({
+  ...shapes,
+  shapeName: (parent: d3.Selection<d3.BaseType, any, any, any>, _bbox: any, _node: dagre.Node) => parent.insert("rect"),
+});
 
 const svg = d3.select("svg");
 renderer(svg, graph);
+
+const createNodes = renderer.createNodes();
+renderer.createNodes((selection, _g, _shape) => selection.selectAll("g"))
+  .createNodes(createNodes);
+
+const createClusters = renderer.createClusters();
+renderer.createClusters((selection, _g) => selection.selectAll("g"))
+  .createClusters(createClusters);
+
+const createEdgeLabels = renderer.createEdgeLabels();
+renderer.createEdgeLabels((selection, _g) => selection.selectAll("g"))
+  .createEdgeLabels(createEdgeLabels);
+
+const createEdgePaths = renderer.createEdgePaths();
+renderer.createEdgePaths((selection, _g) => selection.selectAll("g"))
+  .createEdgePaths(createEdgePaths);
+
+// util
+const _isSubgraph: boolean = util.isSubgraph(graph, "a");
+
+const _id: string = util.edgeToId({v: "a", w: "b", name: "c"});
+
+util.applyStyle(
+  d3.select<d3.BaseType, string>("svg"),
+  function (
+    this: d3.BaseType,
+    _d: string,
+    _index: number,
+    _groups: d3.BaseType[] | d3.ArrayLike<d3.BaseType>,
+  ) {
+    return "fill: red";
+  }
+);
+
+util.applyClass(
+  d3.select<d3.BaseType, string>("svg"),
+  function (
+    this: d3.BaseType,
+    _d: string,
+    _index: number,
+    _groups: d3.BaseType[] | d3.ArrayLike<d3.BaseType>,
+  ) {
+    return "class-name";
+  },
+  'other-class-1 other-class-2'
+);
+
+const _applyTransition:
+  d3.Selection<d3.BaseType, string, HTMLElement, any> |
+  d3.Transition<d3.BaseType, string, HTMLElement, any> 
+= util.applyTransition(
+  d3.select<d3.BaseType, string>("svg"),
+  graph,
+);
--- types/dagre-d3/index.d.ts
+++ types/dagre-d3/index.d.ts
@@ -7,19 +7,133 @@
 
 import * as d3 from "d3";
 import * as dagre from "dagre";
-import * as graphlib from "graphlib";
 
 export as namespace dagreD3;
 
-export * from "dagre";
+export * as graphlib from "graphlib";
+
+export * as dagre from "dagre";
+
+export type Point = {
+  x: number,
+  y: number,
+};
+
+export const intersect: {
+  node: (node: dagre.Node, point: any) => any;
+
+  circle: (
+    node: dagre.Node,
+    rx: number,
+    point: Point,
+  ) => Point;
+
+  ellipse: (
+    node: dagre.Node,
+    rx: number,
+    ry: number,
+    point: Point,
+  ) => Point;
+
+  polygon: (
+    node: dagre.Node,
+    polyPoints: Point[],
+    point: Point,
+  ) => Point;
+
+  rect: (
+    node: dagre.Node,
+    point: Point,
+  ) => Point;
+};
+
 export const render: { new(): Render };
-export const intersect: { [shapeName: string]: (node: dagre.Node, points: Array<{}>, point: any) => void };
+
+export const util: {
+  isSubgraph: (g: dagre.graphlib.Graph<any>, v: string) => boolean,
+
+  edgeToId: (e: {v: string, w: string, name: string}) => string,
+
+  applyStyle: <GElement extends d3.BaseType, Datum>(
+    dom: d3.Selection<GElement, Datum, any, any>,
+    styleFn: d3.ValueFn<GElement, Datum, string | number | boolean | null>,
+  ) => void,
+
+  applyClass: <GElement extends d3.BaseType, Datum>(
+    dom: d3.Selection<GElement, Datum, any, any>,
+    classFn: d3.ValueFn<GElement, Datum, string | number | boolean | null>,
+    otherClasses: string,
+  ) => void,
+
+  applyTransition: <GElement extends d3.BaseType, Datum, PElement extends d3.BaseType, PDatum>(
+    selection: d3.Selection<GElement, Datum, PElement, PDatum>,
+    g: dagre.graphlib.Graph<any>,
+  ) => d3.Transition<GElement, Datum, PElement, PDatum> | d3.Selection<GElement, Datum, PElement, PDatum>,
+};
+
+export type CreateNodes = <GElement extends d3.BaseType>(
+  selection: d3.Selection<SVGGElement, string, any, any>,
+  g: dagre.graphlib.Graph<any>,
+  shapes: Shapes,
+) => d3.Selection<GElement, any, any, any>
+
+export type CreateClusters = <GElement extends d3.BaseType>(
+  selection: d3.Selection<SVGGElement, string, any, any>,
+  g: dagre.graphlib.Graph<any>,
+) => d3.Selection<GElement, any, any, any>
+
+export type CreateEdgeLabels = <GElement extends d3.BaseType>(
+  selection: d3.Selection<SVGGElement, string, any, any>,
+  g: dagre.graphlib.Graph<any>,
+) => d3.Selection<GElement , any, any, any>
+
+export type CreateEdgePaths = <GElement extends d3.BaseType>(
+  selection: d3.Selection<SVGGElement, string, any, any>,
+  g: dagre.graphlib.Graph<any>,
+  arrows: Arrows,
+) => d3.Selection<GElement, any, any, any>
+
+export type Arrow = (
+  parent: d3.Selection<any, any, any, any>,
+  id: string,
+  edge: dagre.Edge,
+  type: string,
+) => void;
+
+export type Arrows = {
+  [arrowStyleName: string]: Arrow;
+};
+
+export type Shape = <Datum, PElement extends d3.BaseType, PDatum>(
+  parent: d3.Selection<any, Datum, PElement, PDatum>,
+  bbox: any,
+  node: dagre.Node,
+) => d3.Selection<any, Datum, PElement, PDatum>;
+
+export type Shapes = {
+ [shapeStyleName: string]: Shape;
+};
 
 export interface Render {
   // see https://dagrejs.github.io/project/dagre-d3/latest/demo/user-defined.html for example usage
 
-  (selection: d3.Selection<any, any, any, any>, g: graphlib.Graph): void;
+  (selection: d3.Selection<any, any, any, any>, g: dagre.graphlib.Graph<any>): void;
 
-  arrows(): { [arrowStyleName: string]: (parent: d3.Selection<any, any, any, any>, id: string, edge: dagre.Edge, type: string) => void };
-  shapes(): { [shapeStyleName: string]: (parent: d3.Selection<any, any, any, any>, bbox: any, node: dagre.Node) => void };
+  createNodes(): CreateNodes;
+  createNodes(value: CreateNodes): Render;
+
+  createClusters(): CreateClusters;
+  createClusters(value: CreateClusters): Render;
+
+  createEdgeLabels(): CreateEdgeLabels;
+  createEdgeLabels(value: CreateEdgeLabels): Render;
+
+  createEdgePaths(): CreateEdgePaths;
+  createEdgePaths(value: CreateEdgePaths): Render;
+
+  arrows(): Arrows;
+  arrows(value: Arrows): Render;
+
+  shapes(): Shapes;
+  shapes(value: Shapes): Render;
 }
