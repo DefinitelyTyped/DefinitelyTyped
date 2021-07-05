6eb3afb5e378ebd6ed2fd1208973e97fb392dd24
Chris Wilkinson <c.wilkinson@elifesciences.org>
2021-07-05 11:22:14 +0100

    Lint

--- types/rdf-utils-dataset/index.d.ts
+++ types/rdf-utils-dataset/index.d.ts
@@ -3,9 +3,11 @@
 // Definitions by: Chris Wilkinson <https://github.com/thewilkybarkid>
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
 
-import { DatasetIndexed } from 'rdf-dataset-indexed/dataset'
-import { BaseQuad, DatasetCore } from 'rdf-js'
+import { DatasetIndexed } from 'rdf-dataset-indexed/dataset';
+import { BaseQuad, DatasetCore } from 'rdf-js';
 
 type OutQuad<T> = T extends DatasetCore<infer Q, any> ? Q : never;
 
-export function resource<D extends DatasetIndexed<BaseQuad>, Q extends OutQuad<D>>(input: D, subject: Q['subject']): D
+export function resource<D extends DatasetIndexed<BaseQuad>>(input: D, subject: OutQuad<D>['subject']): D;
+
+export {}
