1a9b2c3c4b1c0bba7c562c501dfdf61a65c9dd7a
Jon Musselwhite <35066367+FlippingBinary@users.noreply.github.com>
2021-07-05 15:42:52 -0400

    Add namePrependSlash property to ZipOptions interface in archiver (#1)
    * Add namePrependSlash property to ZipOptions
    
    Add namePrependSlash property to ZipOptions interface to reflect changes from recent [commit](https://github.com/archiverjs/node-archiver/commit/f6c9cc2fa6a167594c82428faf22dc59dbba7404#diff-097bdd53267cb87126c6ced763c4add78c4f04fdc00267e6f78875d0e9e69815) and match [current documentation](https://www.archiverjs.com/docs/archiver)
    
    * Add test for new namePrependSlash attribute
    
    * Update version

--- types/archiver/index.d.ts
+++ types/archiver/index.d.ts
@@ -1,4 +1,4 @@
-// Type definitions for archiver 5.1
+// Type definitions for archiver 5.3
 // Project: https://github.com/archiverjs/node-archiver
 // Definitions by:  Esri
 //                  Dolan Miu <https://github.com/dolanmiu>
@@ -125,6 +125,7 @@ declare namespace archiver {
         comment?: string;
         forceLocalTime?: boolean;
         forceZip64?: boolean;
+        namePrependSlash?: boolean;
         store?: boolean;
         zlib?: ZlibOptions;
     }
