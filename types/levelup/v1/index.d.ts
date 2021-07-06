cd29f1c657713344b002f8a87de631cb3e60989a
Nathan Shively-Sanders <293473+sandersn@users.noreply.github.com>
2021-07-06 14:36:44 -0700

    Add undefined to optional properties, Karma to Mimos (#54323)
    In preparation for exactOptionalPropertyTypes in Typescript 4.4, add undefined to all optional properties. #no-publishing-comment
    
    This PR covers widely used packages (more than 100,000 downloads per
    month) from karma-chrome-launcher -- mimos
    microsoft/dtslint#335

--- types/levelup/v1/index.d.ts
+++ types/levelup/v1/index.d.ts
@@ -26,23 +26,23 @@ declare namespace levelup {
         type: string;
         key: any;
         value?: any;
-        keyEncoding?: Encoding;
-        valueEncoding?: Encoding;
+        keyEncoding?: Encoding | undefined;
+        valueEncoding?: Encoding | undefined;
     }
 
     interface LevelUpBase<BatchType extends Batch> {
         open(callback ?: (error : any) => any): void;
         close(callback ?: (error : any) => any): void;
         put(key: any, value: any, callback ?: (error: any) => any): void;
-        put(key: any, value: any, options?: { sync?: boolean }, callback ?: (error: any) => any): void;
+        put(key: any, value: any, options?: { sync?: boolean | undefined }, callback ?: (error: any) => any): void;
         get(key: any, callback ?: (error: any, value: any) => any): void;
 
-        get(key: any, options ?: { keyEncoding?: Encoding; fillCache?: boolean }, callback ?: (error: any, value: any) => any): void;
+        get(key: any, options ?: { keyEncoding?: Encoding | undefined; fillCache?: boolean | undefined }, callback ?: (error: any, value: any) => any): void;
         del(key: any, callback ?: (error: any) => any): void;
-        del(key: any, options ?: { keyEncoding?: Encoding; sync?: boolean }, callback ?: (error: any) => any): void;
+        del(key: any, options ?: { keyEncoding?: Encoding | undefined; sync?: boolean | undefined }, callback ?: (error: any) => any): void;
 
 
-        batch(array: BatchType[], options?: { keyEncoding?: Encoding; valueEncoding?: Encoding; sync?: boolean }, callback?: (error?: any)=>any): void;
+        batch(array: BatchType[], options?: { keyEncoding?: Encoding | undefined; valueEncoding?: Encoding | undefined; sync?: boolean | undefined }, callback?: (error?: any)=>any): void;
         batch(array: BatchType[], callback?: (error?: any)=>any): void;
         batch():LevelUpChain;
         isOpen():boolean;
@@ -59,21 +59,21 @@ declare namespace levelup {
 
     interface LevelUpChain {
         put(key: any, value: any): LevelUpChain;
-        put(key: any, value: any, options?: { sync?: boolean }): LevelUpChain;
+        put(key: any, value: any, options?: { sync?: boolean | undefined }): LevelUpChain;
         del(key: any): LevelUpChain;
-        del(key: any, options ?: { keyEncoding?: Encoding; sync?: boolean }): LevelUpChain;
+        del(key: any, options ?: { keyEncoding?: Encoding | undefined; sync?: boolean | undefined }): LevelUpChain;
         clear(): LevelUpChain;
         write(callback?: (error?: any)=>any) : LevelUpChain;
     }
 
     interface levelupOptions {
-        createIfMissing?: boolean;
-        errorIfExists?: boolean;
-        compression?: boolean;
-        cacheSize?: number;
-        keyEncoding?: Encoding;
-        valueEncoding?: Encoding;
-        db?: leveldown.Constructor;
+        createIfMissing?: boolean | undefined;
+        errorIfExists?: boolean | undefined;
+        compression?: boolean | undefined;
+        cacheSize?: number | undefined;
+        keyEncoding?: Encoding | undefined;
+        valueEncoding?: Encoding | undefined;
+        db?: leveldown.Constructor | undefined;
     }
 
     interface LevelUpConstructor {
