44bf565983bf10c1aebf8465ac6d41a73799c3d9
Teryn Gull <tgull@10-4.com>
2021-07-06 11:56:25 -0600

    remove patch version

--- types/react-big-calendar/index.d.ts
+++ types/react-big-calendar/index.d.ts
@@ -1,4 +1,4 @@
-// Type definitions for react-big-calendar 0.33.5
+// Type definitions for react-big-calendar 0.33
 // Project: https://github.com/jquense/react-big-calendar
 // Definitions by: Piotr Witek <https://github.com/piotrwitek>
 //                 Austin Turner <https://github.com/paustint>
