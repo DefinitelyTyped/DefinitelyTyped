afb872f69486cb337b3efebaf2be64408956e533
lummish <harris@automotus.co>
2021-07-08 13:30:35 -0500

    fix: resolve lint errors for client

--- types/aws-greengrass-core-sdk/stream-manager/client.d.ts
+++ types/aws-greengrass-core-sdk/stream-manager/client.d.ts
@@ -22,8 +22,6 @@ export interface StreamManagerClientOptions {
 /**
  * Stream manager client
  *
- * @class
- * @memberOf aws-greengrass-core-sdk.StreamManager
  *
  * @example <caption>StreamManager Usage</caption>
  * const { StreamManagerClient } = require('aws-greengrass-core-sdk/stream-manager');
@@ -32,7 +30,7 @@ export interface StreamManagerClientOptions {
  *     // Do work with the client (c) here.
  * });
  */
-export declare class StreamManagerClient {
+export class StreamManagerClient {
   /**
    * Constructs a new Stream Manager client. Once connected, <tt>onConnectCb</tt> will be called and
    * the client can then be used.
@@ -50,17 +48,17 @@ export declare class StreamManagerClient {
    * Append a message into the specified message stream. Returns the sequence number of the message
    * if it was successfully appended.
    *
-   * @param streamName {string} The name of the stream to append to.
-   * @param data {Buffer} Buffer containing the data to be written.
-   * @returns {Promise<Number>}
+   * @param streamName The name of the stream to append to.
+   * @param data Buffer containing the data to be written.
+   * @returns
    */
   appendMessage(streamName: string, data: Buffer): Promise<number>;
 
   /**
    * Create a message stream with a given definition.
    *
-   * @param definition {aws-greengrass-core-sdk.StreamManager.MessageStreamDefinition}
-   * @returns {Promise<void>}
+   * @param definition
+   * @returns
    */
   createMessageStream(definition: MessageStreamDefinition): Promise<void>;
 
@@ -68,8 +66,8 @@ export declare class StreamManagerClient {
    * Updates a message stream with the new definition.
    * Minimum version requirements: StreamManager server version 1.1 (or AWS IoT Greengrass Core 1.11.0)
    *
-   * @param definition {aws-greengrass-core-sdk.StreamManager.MessageStreamDefinition}
-   * @returns {Promise<void>}
+   * @param definition
+   * @returns
    */
   updateMessageStream(definition: MessageStreamDefinition): Promise<void>;
 
@@ -77,8 +75,8 @@ export declare class StreamManagerClient {
    * Deletes a message stream based on its name. Nothing is returned if the request succeeds.
    * An error will be thrown if the request fails.
    *
-   * @param streamName {string} The name of the stream to be deleted.
-   * @returns {Promise<void>}
+   * @param streamName The name of the stream to be deleted.
+   * @returns
    */
   deleteMessageStream(streamName: string): Promise<void>;
 
@@ -86,8 +84,8 @@ export declare class StreamManagerClient {
    * Read message(s) from a chosen stream with options. If no options are specified it will try to read
    * 1 message from the stream.
    *
-   * @param streamName {string} The name of the stream to read from.
-   * @param options {aws-greengrass-core-sdk.StreamManager.ReadMessagesOptions?} Options used when reading from the stream of type {@linkcode aws-greengrass-core-sdk.StreamManager.ReadMessagesOptions}.
+   * @param streamName The name of the stream to read from.
+   * @param options.
    *     Defaults are:
    * <ul>
    *     <li>desiredStartSequenceNumber: 0</li>
@@ -101,14 +99,14 @@ export declare class StreamManagerClient {
    *     than the current beginning of the stream, returned messages will start
    *     at the beginning of the stream and not necessarily the desiredStartSequenceNumber.
    * </p>
-   * @returns {Promise<aws-greengrass-core-sdk.StreamManager.Message[]>} List of one or more messages.
+   * @returns  List of one or more messages.
    */
   readMessages(streamName: string, options?: ReadMessagesOptions | null): Promise<Message[]>;
 
   /**
    * List the streams in StreamManager. Returns a list of their names.
    *
-   * @returns {Promise<String[]>}
+   * @returns
    */
   listStreams(): Promise<string[]>;
 
@@ -116,20 +114,20 @@ export declare class StreamManagerClient {
    * Describe a message stream to get metadata including the stream's definition,
    * size, and exporter statuses.
    *
-   * @param streamName {string} The name of the stream to describe
-   * @returns {Promise<aws-greengrass-core-sdk.StreamManager.MessageStreamInfo>}
+   * @param streamName The name of the stream to describe
+   * @returns
    */
   describeMessageStream(streamName: string): Promise<MessageStreamInfo>;
 
   /**
    * Add a callback for when the client connects.
-   * @param f {function}
+   * @param f
    */
   onConnected(f: () => unknown): void;
 
   /**
    * Add a callback for when an error occurs.
-   * @param f {function}
+   * @param f
    */
   onError(f: (e: Error) => unknown): void;
 
