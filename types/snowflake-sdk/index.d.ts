0d026e4809eba1745832289d184487ca4dd0818b
Aaron-K-T-Berry <aaron.abm@gmail.com>
2021-07-09 16:13:46 +1000

    adding snowflake-sdk options for updated authentication options

--- types/snowflake-sdk/index.d.ts
+++ types/snowflake-sdk/index.d.ts
@@ -1,4 +1,4 @@
-// Type definitions for snowflake-sdk 1.5
+// Type definitions for snowflake-sdk 1.6
 // Project: https://github.com/snowflakedb/snowflake-connector-nodejs#readme
 // Definitions by: Hunter Tunnicliff <https://github.com/htunnicliff>
 //                 Mauricio Rojas <https://github.com/orellabac>
@@ -23,9 +23,10 @@ export interface ConnectionOptions {
     username: string;
 
     /**
-     * Password for the user.
+     * Password for the user. Set this option if you set the authenticator option to SNOWFLAKE or the Okta URL endpoint for your
+     * Okta account (e.g. https://<okta_account_name>.okta.com) or if you left the authenticator option unset.
      */
-    password: string;
+    password?: string;
 
     /**
      * @deprecated
@@ -85,6 +86,43 @@ export interface ConnectionOptions {
     clientSessionKeepAliveHeartbeatFrequency?: number | undefined;
 
     jsTreatIntegerAsBigInt?: boolean | undefined;
+
+    /**
+     * Specifies the authenticator to use for verifying user login credentials. You can set this to one of the following values:
+     *  - SNOWFLAKE: Use the internal Snowflake authenticator. You must also set the password option.
+     *  - EXTERNALBROWSER: Use your web browser to authenticate with Okta, ADFS, or any other SAML 2.0-compliant identity provider
+     *    (IdP) that has been defined for your account.
+     *  - https://<okta_account_name>.okta.com: Use Native SSO through Okta.
+     *  - OAUTH: Use OAuth for authentication. You must also set the token option to the OAuth token (see below).
+     *  - SNOWFLAKE_JWT: Use key pair authentication. See Using Key Pair Authentication & Key Pair Rotation.
+     * The default value is SNOWFLAKE.
+     * For more information on authentication, see {@link https://docs.snowflake.com/en/user-guide/admin-security-fed-auth-use.html Managing/Using Federated Authentication}
+     *  and {@link https://docs.snowflake.com/en/user-guide/admin-security-fed-auth-use.html OAuth with Clients, Drivers, and Connectors}.
+     */
+     authenticator?: string;
+
+     /**
+      * Specifies the OAuth token to use for authentication. Set this option if you set the authenticator option to OAUTH.
+      */
+     token?: string;
+
+     /**
+      * Specifies the private key (in PEM format) for key pair authentication.
+      * For details, see {@link https://docs.snowflake.com/en/user-guide/nodejs-driver-use.html#label-nodejs-key-pair-authentication Using Key Pair Authentication & Key Pair Rotation}.
+      */
+     privateKey?: string | Buffer;
+
+     /**
+      * Specifies the local path to the private key file (e.g. rsa_key.p8).
+      * For details, see {@link https://docs.snowflake.com/en/user-guide/nodejs-driver-use.html#label-nodejs-key-pair-authentication Using Key Pair Authentication & Key Pair Rotation}.
+      */
+      privateKeyPath?: string;
+
+     /**
+      * Specifies the passcode to decrypt the private key file, if the file is encrypted.
+      * For details, see {@link https://docs.snowflake.com/en/user-guide/nodejs-driver-use.html#label-nodejs-key-pair-authentication Using Key Pair Authentication & Key Pair Rotation}.
+      */
+      privateKeyPass?: string;
 }
 
 export interface Column {
