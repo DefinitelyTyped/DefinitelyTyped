b4b72f918b58123d5eca7fc58142feab9b775e96
Chris Wilkinson <c.wilkinson@elifesciences.org>
2021-07-09 12:38:42 +0100

    Widen InQuad type

--- types/rdf-utils-dataset/index.d.ts
+++ types/rdf-utils-dataset/index.d.ts
@@ -6,6 +6,6 @@
 import { DatasetIndexed } from 'rdf-dataset-indexed/dataset';
 import { BaseQuad, Term } from 'rdf-js';
 
-declare function resource<D extends DatasetIndexed<BaseQuad>>(input: D, subject: Term): D;
+declare function resource<D extends DatasetIndexed<BaseQuad, BaseQuad>>(input: D, subject: Term): D;
 
 export { resource };
