d757d68ad7e4f80e55e6259748e14d761af5136b
joshuaharry <84721070+joshuaharry@users.noreply.github.com>
2021-07-11 20:25:41 -0500

    Bump package version

--- types/point-in-polygon/index.d.ts
+++ types/point-in-polygon/index.d.ts
@@ -1,4 +1,4 @@
-// Type definitions for point-in-polygon 1.0
+// Type definitions for point-in-polygon 1.1
 // Project: https://github.com/substack/point-in-polygon
 // Definitions by: dyst5422 <https://github.com/dyst5422>
 //                 kogai <https://github.com/kogai>
