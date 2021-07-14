e7612063588133b2ffa0eae5ccdcf86a7e5c3404
Nathan Shively-Sanders <293473+sandersn@users.noreply.github.com>
2021-07-14 16:37:31 -0700

    Remove redundant undefined from parameters (#54530)
    * Remove redundant undefined from parameters
    
    These were probably exempted before the name of the rule changed.
    
    * Remove missed undefined

--- types/jsforce/api/chatter.d.ts
+++ types/jsforce/api/chatter.d.ts
@@ -36,8 +36,8 @@ export class Request<T> implements PromiseLike<T> {
 
     stream(): Stream;
 
-    then<TResult1, TResult2>(onfulfilled?: ((value: T) => (PromiseLike<TResult1> | TResult1)) | null | undefined,
-                             onrejected?: ((reason: any) => (PromiseLike<TResult2> | TResult2)) | null | undefined): Promise<TResult1 | TResult2>;
+    then<TResult1, TResult2>(onfulfilled?: ((value: T) => (PromiseLike<TResult1> | TResult1)) | null,
+                             onrejected?: ((reason: any) => (PromiseLike<TResult2> | TResult2)) | null): Promise<TResult1 | TResult2>;
 
     finally(onfinally?: () => void): Promise<T>;
 
