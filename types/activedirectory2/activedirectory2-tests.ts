4fa85bff80fc10d28e9106165c804ab8cbb9cb13
Pratik Gaikwad <noreplyfrompratik@gmail.com>
2021-07-15 11:33:59 -0700

    refactor(activeDirectory2): add promisified definitions for all the function calls

--- types/activedirectory2/activedirectory2-tests.ts
+++ types/activedirectory2/activedirectory2-tests.ts
@@ -12,3 +12,13 @@ ad.authenticate("test@mydomain.com", "yyyy111pw", (err, authed) => {
     console.log(err);
     console.log(authed);
 });
+
+async function Test() {
+    console.log("hello");
+    try {
+    const authed = await ad.authenticate("test@mydomain.com", "yyyy111pw");
+    console.log(authed);
+    } catch (err) {
+        console.log(err);
+    }
+}
--- types/activedirectory2/index.d.ts
+++ types/activedirectory2/index.d.ts
@@ -1,43 +1,13 @@
 // Type definitions for activedirectory2 1.2
 // Project: https://github.com/jsumners/node-activedirectory#readme
-// Definitions by: pas <https://github.com/pasthelod>
+// Definitions by: pas <https://github.com/pasthelod>, GaikwadPratik <https://github.com/GaikwadPratik>
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
 // TypeScript Version: 2.5
 
 import { Filter } from "ldapjs";
-
-interface ADProperties {
-    url: string;
-    baseDN: string;
-    username: string;
-    password: string;
-    pageSize?: 1000 | undefined;
-    entryParser?: ((entry: object, raw: string, cb: (entry: object) => void) => void) | undefined;
-    referrals?: {
-        enabled: false,
-        exclude: [
-            'ldaps?://ForestDnsZones\\..*/.*',
-            'ldaps?://DomainDnsZones\\..*/.*',
-            'ldaps?://.*/CN=Configuration,.*'
-        ]
-    } | undefined;
-    attributes?: {
-        user: [
-            'dn', 'distinguishedName',
-            'userPrincipalName', 'sAMAccountName', 'mail',
-            'lockoutTime', 'whenCreated', 'pwdLastSet', 'userAccountControl',
-            'employeeID', 'sn', 'givenName', 'initials', 'cn', 'displayName',
-            'comment', 'description'
-        ],
-        group: [
-            'dn', 'cn', 'description', 'distinguishedName', 'objectCategory'
-        ]
-    } | undefined;
-}
-
 interface LDAPjsReqProps {
     url: string;
-    tlsOptions: {
+    tlsOptions?: {
         host?: string | undefined;
         key?: string | undefined;
         cert?: string | undefined;
@@ -50,6 +20,11 @@ interface LDAPjsReqProps {
     idleTimeout?: number | undefined;
     connectionTimeout?: number | undefined;
     strictDN?: boolean | undefined;
+    queueSize?: number | undefined;
+    queueTimeout?: number | undefined;
+    queueDisable?: boolean | undefined;
+    bindDN?: string | undefined;
+    bindCredentials?: string | undefined;
 }
 
 type MembershipType = 'all'|'user'|'group';
@@ -101,6 +76,35 @@ interface FindResult {
     other: object[];
 }
 
+interface ADProperties extends LDAPjsReqProps {
+    url: string;
+    baseDN: string;
+    username: string;
+    password: string;
+    pageSize?: 1000 | undefined;
+    entryParser?: ((entry: object, raw: string, cb: (entry: object) => void) => void) | undefined;
+    referrals?: {
+        enabled: false,
+        exclude: [
+            'ldaps?://ForestDnsZones\\..*/.*',
+            'ldaps?://DomainDnsZones\\..*/.*',
+            'ldaps?://.*/CN=Configuration,.*'
+        ]
+    } | undefined;
+    attributes?: {
+        user: [
+            'dn', 'distinguishedName',
+            'userPrincipalName', 'sAMAccountName', 'mail',
+            'lockoutTime', 'whenCreated', 'pwdLastSet', 'userAccountControl',
+            'employeeID', 'sn', 'givenName', 'initials', 'cn', 'displayName',
+            'comment', 'description'
+        ],
+        group: [
+            'dn', 'cn', 'description', 'distinguishedName', 'objectCategory'
+        ]
+    } | undefined;
+}
+
 declare class ActiveDirectory {
     constructor(props: ADProperties);
     authenticate(
@@ -108,109 +112,132 @@ declare class ActiveDirectory {
         password: string,
         callback?: (err: string, authenticated: boolean) => void
     ): void;
+    authenticate(username: string, password: string): Promise<boolean>;
     isUserMemberOf(
         username: string,
         groupName: string,
         callback?: (err: object, res: boolean) => void
     ): void;
+    isUserMemberOf(username: string, groupName: string): Promise<boolean>;
     isUserMemberOf(
         opts: ReqProps,
         username: string,
         groupName: string,
         callback?: (err: object, res: boolean) => void
     ): void;
+    isUserMemberOf(opts: ReqProps, username: string, groupName: string): Promise<boolean>;
     find(callback?: (err: object, results: FindResult) => void): void;
     find(
         opts: string | ReqProps,
         callback?: (err: object, results: FindResult) => void
     ): void;
+    find(opts?: string | ReqProps): Promise<FindResult>;
     findDeletedObjects(callback?: (err: object, results: object[]) => void): void;
     findDeletedObjects(
         opts: string | ReqProps,
         callback?: (err: object, results: object[]) => void
     ): void;
+    findDeletedObjects(opts?: string | ReqProps): Promise<Array<object>>;
     findUser(
         username: string,
         callback?: (err: object, user: object) => void
     ): void;
+    findUser(username: string): Promise<object>;
     findUser(
         opts: string | ReqProps,
         username: string,
         callback?: (err: object, user: object) => void
     ): void;
+    findUser(opts: string | ReqProps, username: string): Promise<object>;
     findUsers(callback?: (err: object, users: object[]) => void): void;
     findUsers(
         opts: string | ReqProps,
         callback?: (err: object, users: object[]) => void
     ): void;
+    findUsers(opts?: string | ReqProps): Promise<Array<object>>;
     findGroup(
         groupName: string,
         callback?: (err: object, group: object) => void
     ): void;
+    findGroup(groupName: string): Promise<object>;
     findGroup(
         opts: string | ReqProps,
         groupName: string,
         callback?: (err: object, group: object) => void
     ): void;
+    findGroup(opts: string | ReqProps, groupName: string): Promise<object>;
     findGroups(
         groupName: string,
         callback?: (err: object, groups: object[]) => void
     ): void;
+    findGroups(groupName: string): Promise<Array<object>>;
     findGroups(
         opts: string | ReqProps,
         groupName: string,
         callback?: (err: object, groups: object[]) => void
     ): void;
+    findGroups(opts: string | ReqProps, groupName: string): Promise<Array<object>>;
     groupExists(
         groupName: string,
         callback?: (err: object, res: boolean) => void
     ): void;
+    groupExists(groupName: string): Promise<boolean>;
     groupExists(
         opts: string | ReqProps,
         groupName: string,
         callback?: (err: object, res: boolean) => void
     ): void;
+    groupExists(opts: string | ReqProps, groupName: string): Promise<boolean>;
     userExists(
         username: string,
         callback?: (err: object, res: boolean) => void
     ): void;
+    userExists(groupName: string): Promise<boolean>;
     userExists(
         opts: string | ReqProps,
         username: string,
         callback?: (err: object, res: boolean) => void
     ): void;
+    userExists(opts: string | ReqProps, groupName: string): Promise<boolean>;
     getGroupMembershipForGroup(
         groupName: string,
         callback?: (err: object, groups: object[]) => void
     ): void;
+    getGroupMembershipForGroup(groupName: string): Promise<Array<object>>;
     getGroupMembershipForGroup(
         opts: string | ReqProps,
         groupName: string,
         callback?: (err: object, groups: object[]) => void
     ): void;
+    getGroupMembershipForGroup(opts: string | ReqProps, groupName: string): Promise<Array<object>>;
     getGroupMembershipForUser(
         username: string,
         callback?: (err: object, groups: object[]) => void
     ): void;
+    getGroupMembershipForUser(username: string): Promise<Array<object>>;
     getGroupMembershipForUser(
         opts: string | ReqProps,
         username: string,
         callback?: (err: object, groups: object[]) => void
     ): void;
+    getGroupMembershipForUser( opts: string | ReqProps, username: string): Promise<Array<object>>;
     getUsersForGroup(
         groupName: string,
         callback?: (err: object, users: object[]) => void
     ): void;
+    getUsersForGroup(groupName: string): Promise<Array<object>>;
     getUsersForGroup(
         opts: string | ReqProps,
         groupName: string,
         callback?: (err: object, users: object[]) => void
     ): void;
+    getUsersForGroup(opts: string | ReqProps, groupName: string): Promise<Array<object>>;
     getRootDSE(
-        url: string,
-        attributes: string[],
+        url?: string,
+        attributes?: string[],
         callback?: (err: object, result: object) => void
     ): void;
+    getRootDSE(url?: string, attributes?: Array<string>): Promise<object>;
 }
 
 export = ActiveDirectory;
