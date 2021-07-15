1859ddadac1e9f38b54ba796204a2d36723d803c
Josh Bacon <josh.bacon@nexteer.com>
2021-07-15 12:09:32 -0400

    Add local-ipv4-address

---
+++ types/local-ipv4-address/index.d.ts
@@ -0,0 +1,5 @@
+declare module "local-ipv4-address" {
+    function main(): Promise<string>;
+    export default main;
+  }
+  
---
+++ types/local-ipv4-address/local-ipv4-address-tests.ts
@@ -0,0 +1,3 @@
+import ipv4Address from 'local-ipv4-address';
+
+ipv4Address().then((ip: string) => {})
