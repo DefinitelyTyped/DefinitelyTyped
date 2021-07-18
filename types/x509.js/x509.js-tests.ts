bfb234cf7919a5128baec8a21c3a92fd6c718cd4
joshuaharry <84721070+joshuaharry@users.noreply.github.com>
2021-07-18 16:20:23 -0500

    Bugfix: Add keys missing on the return object

--- types/x509.js/x509.js-tests.ts
+++ types/x509.js/x509.js-tests.ts
@@ -1,5 +1,13 @@
 import * as x509js from 'x509.js';
 
-x509js.parseCert('certSting');
 x509js.parseKey('keyString');
 x509js.info();
+const result = x509js.parseCert('certSting');
+result.subject.organizationalUnitName; // $ExpectType string
+result.issuer.localityName; // $ExpectType string
+result.issuer.countryName; // $ExpectType string
+result.issuer.organizationalUnitName; // $ExpectType string
+result.issuer.commonName; // $ExpectType string
+result.issuer.serialNumber; // $ExpectType string
+result.issuer.organizationName; // $ExpectType string
+result.issuer.stateOrProvinceName; // $ExpectType string
