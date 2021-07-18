30e9608609c15c1d5fc8878458907b459fecad61
joshuaharry <84721070+joshuaharry@users.noreply.github.com>
2021-07-18 14:41:21 -0500

    Bugfix: Handle null error case in ps-tree

--- types/ps-tree/index.d.ts
+++ types/ps-tree/index.d.ts
@@ -16,7 +16,7 @@ declare namespace ps_tree {
     };
 }
 
-declare function ps_tree(pid: number, callback: (error: Error, children: ReadonlyArray<ps_tree.PS>) => void): void;
+declare function ps_tree(pid: number, callback: (error: Error | null, children: ReadonlyArray<ps_tree.PS>) => void): void;
 
 export as namespace ps_tree;
 export = ps_tree;
