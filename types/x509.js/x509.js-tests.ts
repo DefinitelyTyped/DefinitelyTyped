e41ffff60885c534b6759e6baeeff5c9e864e205
joshuaharry <84721070+joshuaharry@users.noreply.github.com>
2021-07-20 14:43:42 -0500

    Bump test to reflect changes in type

--- types/x509.js/x509.js-tests.ts
+++ types/x509.js/x509.js-tests.ts
@@ -3,11 +3,11 @@ import * as x509js from 'x509.js';
 x509js.parseKey('keyString');
 x509js.info();
 const result = x509js.parseCert('certSting');
-result.subject.organizationalUnitName; // $ExpectType string
-result.issuer.localityName; // $ExpectType string
-result.issuer.countryName; // $ExpectType string
-result.issuer.organizationalUnitName; // $ExpectType string
+result.subject.organizationalUnitName; // $ExpectType string | undefined
+result.issuer.localityName; // $ExpectType string | undefined
+result.issuer.countryName; // $ExpectType string | undefined
+result.issuer.organizationalUnitName; // $ExpectType string | undefined
 result.issuer.commonName; // $ExpectType string
-result.issuer.serialNumber; // $ExpectType string
-result.issuer.organizationName; // $ExpectType string
-result.issuer.stateOrProvinceName; // $ExpectType string
+result.issuer.serialNumber; // $ExpectType string | undefined
+result.issuer.organizationName; // $ExpectType string | undefined
+result.issuer.stateOrProvinceName; // $ExpectType string | undefined
