6bbfea1d354cf47b91a0cb968c41b0af9042ded8
Paulo Andrade <paulo85br@gmail.com>
2021-07-22 15:19:53 -0600

    feat: add types for react-pay-icons

---
+++ types/react-pay-icons/react-pay-icons-tests.tsx
@@ -0,0 +1,10 @@
+import * as React from 'react';
+import { Visa } from './';
+
+class Test extends React.Component<object, object> {
+    render() {
+        return <Visa />;
+    }
+}
+
+export default Test;
