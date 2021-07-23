746d6b5654e5a7a73d773b22162a5120630d4c77
Alex Lavallee <73203142+lavalleeale@users.noreply.github.com>
2021-07-23 15:50:22 -0700

    Added Alexander Lavallee as Definition Co-owner

--- types/sshpk/index.d.ts
+++ types/sshpk/index.d.ts
@@ -1,6 +1,7 @@
 // Type definitions for sshpk 1.10
 // Project: https://github.com/arekinath/node-sshpk
 // Definitions by: Meno Abels <https://github.com/mabels>
+//                 Alexander Lavallee <https://github.com/lavalleeale>
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
 
 /// <reference types="node" />
