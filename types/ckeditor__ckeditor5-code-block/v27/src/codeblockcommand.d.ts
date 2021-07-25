abe710abda8e0eee361d8b54c216a5f94335b33e
Federico Panico <federicopanico@gmail.com>
2021-07-25 19:44:46 -0300

    Update @ckeditor/ckeditor5-code-block to v29

--- types/ckeditor__ckeditor5-code-block/v27/src/codeblockcommand.d.ts
+++ types/ckeditor__ckeditor5-code-block/v27/src/codeblockcommand.d.ts
@@ -2,5 +2,5 @@ import { Command } from '@ckeditor/ckeditor5-core';
 
 export default class CodeBlockCommand extends Command {
     refresh(): void;
-    execute(options?: { forceValue?: boolean; language?: string; usePreviousLanguageChoice?: boolean }): void;
+    execute(options?: { forceValue?: boolean, language?: string }): void;
 }
