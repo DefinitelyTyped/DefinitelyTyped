7ad2132eea16ef3bfdf75f008756aff1087aca62
Renan Araújo <renan@belvo.com>
2021-07-26 15:50:43 -0300

    Fix definition file linting

--- types/belvo/index.d.ts
+++ types/belvo/index.d.ts
@@ -3,7 +3,6 @@
 // Definitions by: Renan Araújo <https://github.com/renan-at-belvo>
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
 
-
 export default class Client {
     constructor(secretId: string, secretPassword: string, baseURL: string);
 
