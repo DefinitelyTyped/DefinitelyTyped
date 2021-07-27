9a10e7366eded5734312f20d8cae4e6ddc642595
Kushan Joshi <0o3ko0@gmail.com>
2021-07-27 16:06:25 -0700

    Update prosemirror-inputrules-tests.ts

--- types/prosemirror-inputrules/prosemirror-inputrules-tests.ts
+++ types/prosemirror-inputrules/prosemirror-inputrules-tests.ts
@@ -4,9 +4,7 @@ import { Transaction } from 'prosemirror-state';
 
 const nodeType = new NodeType();
 const rule1: inputrules.InputRule = inputrules.wrappingInputRule(/^\$/, nodeType);
-
-const rule2 = new inputrules.InputRule(/^$/, 'str');
+const rule2 = new inputrules.InputRule(/^$/, () => null);
 const regex2: RegExp = rule2.match;
-const handler2: string = rule2.handler;
-
+const handler2 = rule2.handler;
 const rule3 = new inputrules.InputRule(/^$/, () => null);
