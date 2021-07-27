c40dc1f0125176326c0d33e73e5d780ef39e6acc
Kagami Sascha Rosylight <saschanaz@outlook.com>
2021-07-27 23:07:42 +0200

    fix package error

--- types/web-ie11/index.d.ts
+++ types/web-ie11/index.d.ts
@@ -1,4 +1,4 @@
-// Type definitions for web-ie11 0.0
+// Type definitions for non-npm package web-ie11-browser
 // Project: https://docs.microsoft.com/en-us/lifecycle/announcements/internet-explorer-11-end-of-support
 // Definitions by: Kagami Sascha Rosylight <https://github.com/saschanaz>
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
