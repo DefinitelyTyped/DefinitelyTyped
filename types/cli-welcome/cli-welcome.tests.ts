5703ac73b19feaafa92db0df3aafdd0c9683928b
Max Programming <ui.sabuwala@gmail.com>
2021-07-27 10:31:31 +0530

    ✨ Type definitions for cli-welcome package

---
+++ types/cli-welcome/cli-welcome.tests.ts
@@ -0,0 +1,15 @@
+import welcome = require('cli-welcome');
+
+// Use it.
+welcome({ title: `Welcome CLI`, tagLine: `by Ahmad Awais` });
+
+// OR with all the options set like this:
+welcome({
+    title: `Welcome CLI`,
+    tagLine: `by Ahmad Awais`,
+    bgColor: `#FADC00`,
+    color: `#000000`,
+    bold: true,
+    clear: true,
+    version: `v1.0`,
+});
---
+++ types/cli-welcome/index.d.ts
@@ -0,0 +1,17 @@
+// Type definitions for cli-welcome 2.2.2
+// Project: https://github.com/ahmadawais/cli-welcome
+// Definitions by: Max Programming <https://github.com/max-programming>
+// Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
+
+interface WelcomeOptions {
+    title: string;
+    tagLine: string;
+    description?: string;
+    version?: string;
+    bgColor?: string;
+    color?: string;
+    clear?: boolean;
+    bold?: boolean;
+}
+
+export default function welcome(options: WelcomeOptions): void;
