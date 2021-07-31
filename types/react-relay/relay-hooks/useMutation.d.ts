26fd16fb07bf2757ba953601effcdc4a31d6ec6b
Oscar Beaumont <oscar@otbeaumont.me>
2021-07-31 15:09:47 +0800

    fixed error type on react-relay useMutation hook

--- types/react-relay/relay-hooks/useMutation.d.ts
+++ types/react-relay/relay-hooks/useMutation.d.ts
@@ -5,11 +5,36 @@ import {
     IEnvironment,
     MutationConfig,
     MutationParameters,
+    NormalizationOperation,
     PayloadError,
+    ReaderFragment,
+    RequestParameters,
     SelectorStoreUpdater,
     UploadableMap,
     VariablesOf,
 } from 'relay-runtime';
+
+export interface MutationOperation {
+    fragment: ReaderFragment;
+    kind: string;
+    operation: NormalizationOperation;
+    params: RequestParameters;
+    hash: string;
+}
+
+export interface MutationErrorSource<TMutation extends MutationParameters> {
+    errors: PayloadError[];
+    operation: MutationOperation;
+    variables: VariablesOf<TMutation>; // TODO: Does this work correctly?    [name: string]: any;
+}
+
+export interface MutationError<TMutation extends MutationParameters> {
+    name: string;
+    messageFormat: string;
+    messageParams: (string | number | boolean)[];
+    type: "fatal" | "error" | "warn" | "info";
+    source: MutationErrorSource<TMutation>; // TODO
+}
 
 export interface UseMutationConfig<TMutation extends MutationParameters> {
     variables: VariablesOf<TMutation>;
@@ -18,7 +43,7 @@ export interface UseMutationConfig<TMutation extends MutationParameters> {
     optimisticUpdater?: SelectorStoreUpdater<TMutation['response']> | null | undefined;
     optimisticResponse?: TMutation['rawResponse'] | undefined;
     configs?: DeclarativeMutationConfig[] | undefined;
-    onError?: ((error: Error) => void | null) | undefined;
+    onError?: ((error: MutationError<TMutation>) => void | null) | undefined;
     onCompleted?: ((response: TMutation['response'], errors: PayloadError[] | null) => void | null) | undefined;
     onUnsubscribe?: (() => void | null) | undefined;
 }
