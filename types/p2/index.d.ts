4215022be101f17db4715f82fd934c4dd8cd9cdc
Piotr Błażejewicz <peterblazejewicz@users.noreply.github.com>
2021-08-06 21:35:19 +0200

    update(p2): remove contributor handle
    As requested:
    https://github.com/DefinitelyTyped/DefinitelyTyped/pull/54961#issuecomment-893931714
    
    /cc @ alexandermbrown
    
    Thnaks!

--- types/p2/index.d.ts
+++ types/p2/index.d.ts
@@ -1,6 +1,6 @@
 // Type definitions for p2.js v0.7.1
 // Project: https://github.com/schteppe/p2.js/
-// Definitions by: Clark Stevenson <https://github.com/clark-stevenson>, Janne Ramstedt <https://github.com/jramstedt>, Alex Brown <https://github.com/alexandermbrown>
+// Definitions by: Clark Stevenson <https://github.com/clark-stevenson>, Janne Ramstedt <https://github.com/jramstedt>
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
 
 export = p2;
