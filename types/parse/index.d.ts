cd2bd01e35b4197fdd2f3a89673293ecf7d5bf94
Samuel Denis-D'Ortun <sam@sddproductions.com>
2021-08-06 23:22:18 -0400

    Add Query context and  validator 'required' field

--- types/parse/index.d.ts
+++ types/parse/index.d.ts
@@ -119,6 +119,10 @@ declare global {
             error?: Function | undefined;
         }
 
+        interface ContextOption {
+            context: {[key: string]: any};
+        }
+
         interface FullOptions {
             success?: Function | undefined;
             error?: Function | undefined;
@@ -528,6 +532,7 @@ declare global {
                     SuccessFailureOptions,
                     SilentOption,
                     ScopeOptions,
+                    ContextOption,
                     WaitOption {}
 
             interface SaveAllOptions extends BatchSizeOption, ScopeOptions {}
@@ -1310,6 +1315,7 @@ declare global {
                 default?: any;
                 options?: any[] | Function | undefined;
                 error?: String | undefined;
+                required?: boolean;
             }
             interface ValidatorFields {
                 [field: string]: ValidatorField;
@@ -1343,14 +1349,10 @@ declare global {
                 original?: T | undefined;
             }
 
-            interface AfterSaveRequest<T = Object> extends TriggerRequest<T> {
-                context: object;
-            }
+            interface AfterSaveRequest<T = Object> extends TriggerRequest<T>, ContextOption {}
             interface AfterDeleteRequest<T = Object> extends TriggerRequest<T> {} // tslint:disable-line no-empty-interface
             interface BeforeDeleteRequest<T = Object> extends TriggerRequest<T> {} // tslint:disable-line no-empty-interface
-            interface BeforeSaveRequest<T = Object> extends TriggerRequest<T> {
-                context: object;
-            }
+            interface BeforeSaveRequest<T = Object> extends TriggerRequest<T>, ContextOption {}
 
             interface FileTriggerRequest extends TriggerRequest<File> {
                 file: File;
