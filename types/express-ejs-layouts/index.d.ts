e81241624cf05895f1b2d2838831492c09c8417b
joshuaharry <84721070+joshuaharry@users.noreply.github.com>
2021-08-08 18:22:02 -0500

    Revert changes to express-ejs-layouts on this branch

--- types/express-ejs-layouts/index.d.ts
+++ types/express-ejs-layouts/index.d.ts
@@ -1,13 +1,20 @@
-// Type definitions for express-ejs-layouts 4.1
-// Project: https://github.com/Soarez/express-ejs-layouts
-// Definitions by: Erik Mavrinac <https://github.com/erikma>
+// Type definitions for express-form-data 2.0
+// Project: https://github.com/ortexx/express-form-data#readme
+// Definitions by: nomnes <https://github.com/NomNes>
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
 // TypeScript Version: 2.3
 
-/// <reference types="node" />
+import { NextHandleFunction } from 'connect';
+import { FormOptions } from 'multiparty';
 
-import { RequestHandler } from 'express';
+export interface FormDataOptions extends FormOptions {
+    autoClean?: boolean | undefined;
+}
 
-declare const expressEjsLayouts: RequestHandler;
+export function parse(options?: FormDataOptions): NextHandleFunction;
 
-export = expressEjsLayouts;
+export function format(): NextHandleFunction;
+
+export function stream(): NextHandleFunction;
+
+export function union(): NextHandleFunction;
