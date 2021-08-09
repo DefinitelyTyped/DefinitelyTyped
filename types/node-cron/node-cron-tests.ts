f0978770c3bb110b610efaabd2615c6f1923c8af
aseidma <alexander.seidmann@myredthread.de>
2021-08-09 16:09:00 +0200

    remove getStatus calls from test

--- types/node-cron/node-cron-tests.ts
+++ types/node-cron/node-cron-tests.ts
@@ -12,10 +12,14 @@ cron.schedule('1-5 * * * *', () => {
     log('running every minute to 1 from 5');
 });
 
-const task = cron.schedule('* * * * *', () => {
-    log('immediately started');
-    // because of manual call start method
-}, { scheduled: false });
+const task = cron.schedule(
+    '* * * * *',
+    () => {
+        log('immediately started');
+        // because of manual call start method
+    },
+    { scheduled: false },
+);
 
 task.start();
 
@@ -39,20 +43,21 @@ if (valid && !invalid) {
 }
 
 // check timezones are accepted from the string literal
-const task4 = cron.schedule('* * * * *', () => {
-    log('will execute every minute until stopped');
-}, { timezone: 'Europe/London' });
+const task4 = cron.schedule(
+    '* * * * *',
+    () => {
+        log('will execute every minute until stopped');
+    },
+    { timezone: 'Europe/London' },
+);
 
 task4.stop();
 
-const task5 = cron.schedule('* * * * *', () => {
-    log('will execute every minute until stopped');
+const task5 = cron.schedule('* * * * * *', () => {
+    log('will execute every second until stopped');
 });
 
-if (task5.getStatus() === 'scheduled') {
+task5.on('task-done', () => {
+    log('Task has been completed');
     task5.stop();
-}
-
-if (task5.getStatus() === 'destroyed') {
-    log('Task5 is destroyed!');
-}
+});
