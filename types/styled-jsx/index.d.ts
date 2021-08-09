1a46fb11706cb449249eb47edbc70c2540c45331
Jiachi Liu <inbox@huozhi.im>
2021-08-09 17:10:18 +0800

    revert version change

--- types/styled-jsx/index.d.ts
+++ types/styled-jsx/index.d.ts
@@ -1,8 +1,8 @@
-// Type definitions for styled-jsx 3.4.x
+// Type definitions for styled-jsx 2.2
 // Project: https://github.com/zeit/styled-jsx
 // Definitions by: R1ZZU <https://github.com/R1ZZU>
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
-// TypeScript Version: 2.8
+// TypeScript Version: 3.8
 
 import "react";
 
