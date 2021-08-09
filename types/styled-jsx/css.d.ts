3f7e7e1a07930ce4ac517a9b6ff386248789fd2a
Jiachi Liu <inbox@huozhi.im>
2021-08-09 16:48:52 +0800

    Align with styled-jsx 3.4.x

--- types/styled-jsx/css.d.ts
+++ types/styled-jsx/css.d.ts
@@ -1,6 +1,6 @@
-declare function css(chunks: TemplateStringsArray, ...args: any[]): string;
+declare function css(chunks: TemplateStringsArray, ...args: any[]): JSX.Element;
 declare namespace css {
-    function global(chunks: TemplateStringsArray, ...args: any[]): string;
-    function resolve(chunks: TemplateStringsArray, ...args: any[]): { className: string; styles: string };
+    function global(chunks: TemplateStringsArray, ...args: any[]): JSX.Element;
+    function resolve(chunks: TemplateStringsArray, ...args: any[]): { className: string; styles: JSX.Element };
 }
 export = css;
--- types/styled-jsx/index.d.ts
+++ types/styled-jsx/index.d.ts
@@ -1,13 +1,10 @@
-// Type definitions for styled-jsx 2.2
+// Type definitions for styled-jsx 3.4.x
 // Project: https://github.com/zeit/styled-jsx
 // Definitions by: R1ZZU <https://github.com/R1ZZU>
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
 // TypeScript Version: 2.8
 
 import "react";
-
-import * as css from "./css";
-import * as server from "./server";
 
 declare module "react" {
     interface StyleHTMLAttributes<T> extends HTMLAttributes<T> {
