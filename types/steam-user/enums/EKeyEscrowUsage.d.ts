d69f6f0bec4760ac3406bbb977863c2b41b57263
vanitasboi <joshua.jeschek@gmail.com>
2021-08-09 11:12:21 +0200

    reverted changes

--- types/steam-user/enums/EKeyEscrowUsage.d.ts
+++
@@ -1,8 +0,0 @@
-declare const EKeyEscrowUsage: {
-	"StreamingDevice": 0,
-
-	// Value-to-name mapping for convenience
-	"0": "StreamingDevice",
-};
-
-export = EKeyEscrowUsage;
--- types/steam-user/resources/EConnectionProtocol.d.ts
+++
@@ -1,9 +0,0 @@
-declare const EConnectionProtocol: {
-	"Auto": 0,
-
-	"TCP": 1,
-
-	"WebSocket": 2
-};
-
-export = EConnectionProtocol;
