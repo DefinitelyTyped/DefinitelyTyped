bd5a0e396bf282990d5170dab429759a489e0fc1
Federico Panico <federicopanico@gmail.com>
2021-08-10 16:09:13 -0300

    Update @ckeditor/ckeditor5-special-characters to v29

---
+++ types/ckeditor__ckeditor5-special-characters/v27/src/specialcharacterslatin.d.ts
@@ -0,0 +1,6 @@
+import { Plugin } from "@ckeditor/ckeditor5-core";
+
+export default class SpecialCharactersLatin extends Plugin {
+    static readonly pluginName: "SpecialCharactersLatin";
+    init(): void;
+}
