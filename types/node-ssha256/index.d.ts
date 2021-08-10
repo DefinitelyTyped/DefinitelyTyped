cc3db987b3d83acec86f54afd807836078a4bcb7
Chris Midgley <chris.midgley@dunecomputers.co.uk>
2021-08-10 08:02:31 +0100

    fix: update my github url

--- types/node-ssha256/index.d.ts
+++ types/node-ssha256/index.d.ts
@@ -1,6 +1,6 @@
 // Type definitions for node-ssha256 0.1
 // Project: https://github.com/henne-/node-ssha256
-// Definitions by: Chris Midgley <https://github.com/me>
+// Definitions by: Chris Midgley <https://github.com/midgleyc>
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
 
 export function check(hash: string, pw: string): boolean;
