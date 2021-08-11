c61155cea67a6ddbce41e355675222539136a55a
Federico Panico <federicopanico@gmail.com>
2021-08-11 10:01:35 -0300

    Update @ckeditor/ckedito5-media-embed to v29

--- types/ckeditor__ckeditor5-media-embed/v27/ckeditor__ckeditor5-media-embed-tests.ts
+++ types/ckeditor__ckeditor5-media-embed/v27/ckeditor__ckeditor5-media-embed-tests.ts
@@ -1,16 +1,10 @@
 import { Editor } from '@ckeditor/ckeditor5-core';
-import { DowncastWriter, Element, Model, Range, StylesProcessor } from '@ckeditor/ckeditor5-engine';
+import { DowncastWriter, Element, Model, StylesProcessor } from '@ckeditor/ckeditor5-engine';
 import Position from '@ckeditor/ckeditor5-engine/src/model/position';
 import ModelSelection from '@ckeditor/ckeditor5-engine/src/model/selection';
 import Document from '@ckeditor/ckeditor5-engine/src/view/document';
 import Selection from '@ckeditor/ckeditor5-engine/src/view/selection';
-import {
-    AutoMediaEmbed,
-    MediaEmbed,
-    MediaEmbedEditing,
-    MediaEmbedToolbar,
-    MediaEmbedUI,
-} from '@ckeditor/ckeditor5-media-embed';
+import ME from '@ckeditor/ckeditor5-media-embed';
 import * as converters from '@ckeditor/ckeditor5-media-embed/src/converters';
 import Command from '@ckeditor/ckeditor5-media-embed/src/mediaembedcommand';
 import MediaRegistry from '@ckeditor/ckeditor5-media-embed/src/mediaregistry';
@@ -21,27 +15,27 @@ import { Locale } from '@ckeditor/ckeditor5-utils';
 class MyEditor extends Editor {}
 const editor = new MyEditor();
 
-MediaEmbed.requires.map(Plugin => new Plugin(editor).init());
-new MediaEmbed(editor);
+ME.MediaEmbed.requires.map(Plugin => new Plugin(editor).init());
+new ME.MediaEmbed(editor);
 
-MediaEmbedUI.requires.map(Plugin => new Plugin(editor).init());
-new MediaEmbedUI(editor).init();
+ME.MediaEmbedUI.requires.map(Plugin => new Plugin(editor).init());
+new ME.MediaEmbedUI(editor).init();
 
-AutoMediaEmbed.requires.map(Plugin => new Plugin(editor));
-new AutoMediaEmbed(editor).init();
+ME.AutoMediaEmbed.requires.map(Plugin => new Plugin(editor));
+new ME.AutoMediaEmbed(editor).init();
 
-new MediaEmbedEditing(editor).init();
-new MediaEmbedEditing(editor).registry.getMediaViewElement(
+new ME.MediaEmbedEditing(editor).init();
+new ME.MediaEmbedEditing(editor).registry.getMediaViewElement(
     new DowncastWriter(new Document(new StylesProcessor())),
     '',
     { elementName: '', renderMediaPreview: true, renderForEditingView: true },
 );
-new MediaEmbedEditing(editor).registry.hasMedia('') === !!0;
-new MediaEmbedEditing(editor).registry.providerDefinitions?.forEach;
-new MediaEmbedEditing(editor).registry.locale.t('');
+new ME.MediaEmbedEditing(editor).registry.hasMedia('') === !!0;
+new ME.MediaEmbedEditing(editor).registry.providerDefinitions?.forEach;
+new ME.MediaEmbedEditing(editor).registry.locale.t('');
 
-MediaEmbedToolbar.requires.map(Plugin => new Plugin(editor).init());
-new MediaEmbedToolbar(editor).afterInit();
+ME.MediaEmbedToolbar.requires.map(Plugin => new Plugin(editor).init());
+new ME.MediaEmbedToolbar(editor).afterInit();
 
 converters.modelToViewUrlAttributeConverter(new MediaRegistry(new Locale(), {}), {
     elementName: '',
@@ -58,7 +52,7 @@ new View([() => ''], new Locale()).isValid() === !0;
 new View([() => ''], new Locale()).resetFormStatus();
 new View([() => ''], new Locale()).urlInputView.render();
 
-utils.insertMedia(new Model(), '', new Range(new Position(Element.fromJSON({ name: 'div' }), [4])));
+utils.insertMedia(new Model(), '', new Position(Element.fromJSON({ name: 'div' }), [4]));
 const emptyElement = new DowncastWriter(new Document(new StylesProcessor())).createEmptyElement('foo');
 utils.isMediaWidget(emptyElement) !== !0;
 utils.toMediaWidget(emptyElement, new DowncastWriter(new Document(new StylesProcessor())), '');
