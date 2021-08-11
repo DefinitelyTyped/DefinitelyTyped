a2bf70c2f094020bc816cd39d085f6b673aa2af4
Federico Panico <federicopanico@gmail.com>
2021-08-11 10:26:37 -0300

    Update @ckeditor/ckeditor5-list to v29

---
+++ types/ckeditor__ckeditor5-list/v27/src/checktodolistcommand.d.ts
@@ -0,0 +1,8 @@
+import { Command } from '@ckeditor/ckeditor5-core';
+import { Element } from '@ckeditor/ckeditor5-engine';
+
+export default class CheckTodoListCommand extends Command {
+    value: Element[];
+    refresh(): void;
+    execute(options?: { forceValue?: boolean | undefined }): void;
+}
