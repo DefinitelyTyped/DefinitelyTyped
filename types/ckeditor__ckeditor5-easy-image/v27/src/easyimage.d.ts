9eb627639e56f057f90ebc0bed78ecdd2856efa4
Federico Panico <federicopanico@gmail.com>
2021-08-11 14:45:50 -0300

    Update @ckeditor/ckeditor5-easy-image to v29

--- types/ckeditor__ckeditor5-easy-image/v27/src/easyimage.d.ts
+++ types/ckeditor__ckeditor5-easy-image/v27/src/easyimage.d.ts
@@ -1,9 +1,7 @@
 import { Plugin } from "@ckeditor/ckeditor5-core";
-import { ImageUpload } from "@ckeditor/ckeditor5-image";
 import CloudServicesUploadAdapter from "./cloudservicesuploadadapter";
 
 export default class EasyImage extends Plugin {
-    static requires: [typeof CloudServicesUploadAdapter, typeof ImageUpload];
+    static requires: [typeof CloudServicesUploadAdapter, "Image", "ImageUpload"];
     static pluginName: "EasyImage";
-    init(): void;
 }
