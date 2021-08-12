8b4f30be70f75dbd386d7805ee080ed73a7e216c
Andre Farzat <andrefarzat@gmail.com>
2021-08-12 16:12:45 -0300

    Adding plotly.js-dist-min package

---
+++ types/plotly.js-dist-min/index.d.ts
@@ -0,0 +1,8 @@
+// Type definitions for plotly.js-dist-min
+// Project: https://github.com/plotly/plotly.js#readme
+// Definitions forked from: plotly.js-basic-dist by Florian Dreier <https://github.com/DreierF>
+// Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
+
+import * as Plotly from 'plotly.js';
+
+export default Plotly;
