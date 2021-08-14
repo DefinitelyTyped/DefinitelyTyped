65316165a84389208ee5a51f28fdac7f49aadabb
Francis Gulotta <wizard@roborooter.com>
2021-08-13 20:02:41 -0400

    fix: port should be a number not a string

--- types/architect__sandbox/architect__sandbox-tests.ts
+++ types/architect__sandbox/architect__sandbox-tests.ts
@@ -3,7 +3,7 @@ import * as sandbox from '@architect/sandbox';
 sandbox.start().then(() => {});
 sandbox.start({}).then(() => {});
 sandbox.start({ quiet: true }).then(() => {});
-sandbox.start({ port: '4444' }).then(() => {});
+sandbox.start({ port: 4444 }).then(() => {});
 sandbox.start({ cwd: './idk/arc/' }).then(() => {});
 sandbox.start({ logLevel: 'normal' }).then(() => {});
 sandbox.start({ symlink: true }).then(() => {});
@@ -16,7 +16,7 @@ sandbox.end(() => {});
 sandbox.events.start().then(() => {});
 sandbox.events.start({}).then(() => {});
 sandbox.events.start({ quiet: true }).then(() => {});
-sandbox.events.start({ port: '4444' }).then(() => {});
+sandbox.events.start({ port: 4444 }).then(() => {});
 sandbox.events.start({}, () => {});
 
 sandbox.events.end().then(() => {});
@@ -26,7 +26,7 @@ sandbox.events.end(() => {});
 sandbox.http.start().then(() => {});
 sandbox.http.start({}).then(() => {});
 sandbox.http.start({ quiet: true }).then(() => {});
-sandbox.http.start({ port: '4444' }).then(() => {});
+sandbox.http.start({ port: 4444 }).then(() => {});
 sandbox.http.start({}, () => {});
 
 sandbox.http.end().then(() => {});
@@ -36,7 +36,7 @@ sandbox.http.end(() => {});
 sandbox.tables.start().then(() => {});
 sandbox.tables.start({}).then(() => {});
 sandbox.tables.start({ quiet: true }).then(() => {});
-sandbox.tables.start({ port: '4444' }).then(() => {});
+sandbox.tables.start({ port: 4444 }).then(() => {});
 sandbox.tables.start({}, () => {});
 
 sandbox.tables.end().then(() => {});
