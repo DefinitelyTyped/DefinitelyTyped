10d02c3eda812f7614ab5399fe7ec31085940753
pharrukh <farrukh.normuradov@gmail.com>
2021-08-14 10:57:14 +0200

    feat: add types for parse-multipart

---
+++ types/parse-multipart/parse-multipart-tests.ts
@@ -0,0 +1,7 @@
+/// <reference types="node" />
+
+import { Parse, getBoundary, DemoData } from 'parse-multipart';
+
+DemoData(); // $ExpectType Buffer
+getBoundary('multipart/form-data; boundary=----WebKitFormBoundaryvm5A9tzU1ONaGP5B'); // $ExpectType string
+Parse(Buffer.from('hello world', 'utf8'), ''); // $ExpectType ParsedFile[]
