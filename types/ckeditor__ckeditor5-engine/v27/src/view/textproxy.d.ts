e8b2bf00337ac973b3ffa7470fd71f0f12aadc75
Frankie Dintino <fdintino@gmail.com>
2021-08-16 15:14:16 -0400

    @ckeditor/ckeditor5-engine: type narrowing on view & model is() methods

--- types/ckeditor__ckeditor5-engine/v27/src/view/textproxy.d.ts
+++ types/ckeditor__ckeditor5-engine/v27/src/view/textproxy.d.ts
@@ -1,8 +1,19 @@
+import AttributeElement from "./attributeelement";
+import ContainerElement from "./containerelement";
 import Document from "./document";
 import DocumentFragment from "./documentfragment";
+import DocumentSelection from "./documentselection";
+import EditableElement from "./editableelement";
 import Element from "./element";
+import EmptyElement from "./emptyelement";
 import Node from "./node";
+import Position from "./position";
+import Range from "./range";
+import RawElement from "./rawelement";
+import RootEditableElement from "./rooteditableelement";
+import Selection from "./selection";
 import Text from "./text";
+import UIElement from "./uielement";
 
 export default class TextProxy {
     readonly data: string;
@@ -20,5 +31,53 @@ export default class TextProxy {
         includeSelf?: boolean | undefined;
         parentFirst?: boolean | undefined;
     }): Array<Element | DocumentFragment>;
-    is(type: string): boolean;
+    is(
+        type: "node" | "view:node",
+    ): this is
+        | Node
+        | Element
+        | Text
+        | ContainerElement
+        | EditableElement
+        | RootEditableElement
+        | AttributeElement
+        | UIElement
+        | RawElement
+        | EmptyElement;
+    is(type: "documentFragment" | "view:documentFragment"): this is DocumentFragment;
+    is(type: "position" | "view:position"): this is Position;
+    is(type: "range" | "view:range"): this is Range;
+    is(type: "selection" | "view:selection"): this is Selection | DocumentSelection;
+    is(type: "documentSelection" | "view:documentSelection"): this is DocumentSelection;
+    is(
+        type: "element" | "view:element",
+        name?: string | undefined,
+    ): this is
+        | Element
+        | ContainerElement
+        | EditableElement
+        | RootEditableElement
+        | AttributeElement
+        | UIElement
+        | RawElement
+        | EmptyElement;
+    is(type: "attributeElement" | "view:attributeElement", name?: string | undefined): this is AttributeElement;
+    is(
+        type: "containerElement" | "view:containerElement",
+        name?: string | undefined,
+    ): this is ContainerElement | EditableElement | RootEditableElement;
+    is(
+        type: "editableElement" | "view:editableElement",
+        name?: string | undefined,
+    ): this is EditableElement | RootEditableElement;
+    is(
+        type: "rootEditableElement" | "view:rootEditableElement",
+        name?: string | undefined,
+    ): this is RootEditableElement;
+    is(type: "uiElement" | "view:uiElement", name?: string | undefined): this is UIElement;
+    is(type: "rawElement" | "view:rawElement", name?: string | undefined): this is RawElement;
+    is(type: "emptyElement" | "view:emptyElement", name?: string | undefined): this is EmptyElement;
+    is(type: "$textProxy" | "view:$textProxy" | "textProxy" | "view:textProxy"): this is TextProxy;
+    is(type: "$text" | "view:$text" | "text" | "view:text"): this is Text;
+    is(type: string, name?: string | undefined): boolean;
 }
