eab83fbdbbcc1aa96896b6e4b79430299fe4632d
bitomic <35199700+bitomic@users.noreply.github.com>
2021-08-16 13:38:55 -0600

    Add an author

--- types/lua-json/index.d.ts
+++ types/lua-json/index.d.ts
@@ -1,6 +1,7 @@
 // Type definitions for lua-json 1.0
 // Project: https://github.com/kcwiki/lua-json#readme
 // Definitions by: bitomic <https://github.com/bitomic>
+//                 Tommy Josépovic <https://github.com/tommy-josepovic>
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
 
 /**
