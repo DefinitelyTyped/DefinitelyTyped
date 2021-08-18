b5b43581617c286a7805338a0a6a08322c7f8725
tsop14 <emma.milner@nytimes.com>
2021-08-18 22:50:29 +0100

    feat: ssh2-sftp-client v7

--- types/ssh2-sftp-client/ssh2-sftp-client-tests.ts
+++ types/ssh2-sftp-client/ssh2-sftp-client-tests.ts
@@ -10,6 +10,7 @@ import * as fs from 'fs';
             port: 1234,
             privateKey: 'my private key rsa in openssh format',
             readyTimeout: 1000,
+            tryKeyboard: false,
         })
         .then(() => null);
 
@@ -32,14 +33,57 @@ import * as fs from 'fs';
     client.realPath('/remote/path').then(() => null);
 
     client.get('/remote/path').then(() => null);
+    client.get('/remote/path', fs.createWriteStream('/local/path/copy.txt')).then(() => null);
+    client
+        .get('/remote/path', fs.createWriteStream('/local/path/copy.txt'), {
+            readStreamOptions: {
+                flags: 'r',
+                encoding: null,
+                handle: null,
+                mode: 0o666,
+                autoClose: true,
+            },
+            pipeOptions: {
+                end: false,
+            },
+        })
+        .then(() => null);
 
     client.fastGet('/remote/path', 'local/path').then(() => null);
+    client
+        .fastGet('/remote/path', 'local/path', {
+            concurrency: 64,
+            chunkSize: 32768,
+            step: (total_transferred, chunk, total) => null,
+        })
+        .then(() => null);
 
     client.put('/local/path', '/remote/path').then(() => null);
     client.put(new Buffer('content'), '/remote/path').then(() => null);
     client.put(fs.createReadStream('Hello World'), '/remote/path').then(() => null);
+    client
+        .put(fs.createReadStream('Hello World'), '/remote/path', {
+            writeStreamOptions: {
+                flags: 'w',
+                encoding: null,
+                mode: 0o666,
+                autoClose: true,
+            },
+            pipeOptions: {
+                end: false,
+            },
+        })
+        .then(() => null);
 
     client.fastPut('/remote/path', 'local/path').then(() => null);
+    client
+        .fastPut('/remote/path', 'local/path', {
+            concurrency: 64,
+            chunkSize: 32768,
+            mode: '0o755', // mixed. Integer or string representing the file mode to set
+            step: (total_transferred, chunk, total) => null,
+        })
+        .then(() => null);
 
     client.cwd().then(() => null);
 
@@ -51,8 +95,17 @@ import * as fs from 'fs';
     client.rename('/remote/from', '/remote/to').then(() => null);
 
     client.chmod('/remote/path', 777).then(() => null);
+    client.chmod('/remote/path', '777').then(() => null);
 
     client.end().then(() => null);
 
     client.on('event', () => null);
+
+    client.append(new Buffer('content'), 'remote/to');
+    client.append(new Buffer('content'), 'remote/to', {
+        flags: 'a',
+        encoding: null,
+        mode: 0o666,
+        autoClose: true,
+    });
 })();
