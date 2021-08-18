5e19743e2b6586383a74b26ee6debcfe1c7a12e2
darthf1 <darthf1>
2021-08-18 14:37:07 +0200

    added missing second optional parameter

--- types/bootstrap/js/dist/base-component.d.ts
+++ types/bootstrap/js/dist/base-component.d.ts
@@ -3,7 +3,7 @@
  * each plugin exposes a getInstance method. In order to retrieve it directly from an element, do this: bootstrap.Popover.getInstance(myPopoverEl)
  */
 export type GetInstanceFactory<T> = (element: string | Element) => T | null;
-export type GetOrCreateInstanceFactory<T> = (element: string | Element) => T;
+export type GetOrCreateInstanceFactory<T> = (element: string | Element, config?: Object) => T;
 
 /**
  * Base Component
