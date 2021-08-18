df692699641dd5fb6831b4ef616d679d0b743848
coderaiser <coderaiser@cloudcmd.io>
2021-08-18 19:51:45 +0300

    chore(tslint) disable rule

--- types/try-catch/index.d.ts
+++ types/try-catch/index.d.ts
@@ -1,8 +1,8 @@
-// Type definitions for try-catch 3.0.0
+// Type definitions for try-catch 3.0
 // Project: https://github.com/coderaiser/try-catch
 // Definitions by: Coderaiser <https://github.com/coderaiser>
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
 // Minimum TypeScript Version: 4.0
 
-declare function tryCatch (fn: Function, ...args: any[]): [error: Error, result?: any];
+declare function tryCatch(fn: Function, ...args: any[]): [error: Error, result?: any];
 export = tryCatch;
