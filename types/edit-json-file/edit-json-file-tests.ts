a569fe1d60d9114de20c6e1e4fabd1ea53207a0f
Max Amann <29060188+AmannMax@users.noreply.github.com>
2021-08-18 21:49:06 +0200

    Change ExpectType to JsonEditor

--- types/edit-json-file/edit-json-file-tests.ts
+++ types/edit-json-file/edit-json-file-tests.ts
@@ -4,7 +4,7 @@ const jsonEditor: editJsonFile.JsonEditor = editJsonFile('1.json'); // $ExpectTy
 jsonEditor.set('a', 2); // $ExpectType JsonEditor
 jsonEditor.set('b', []); // $ExpectType JsonEditor
 jsonEditor.append('a', 'b'); // $ExpectType JsonEditor
-jsonEditor.pop('a'); // $ExpectType any
+jsonEditor.pop('a'); // $ExpectType JsonEditor
 jsonEditor.unset('b'); // $ExpectType JsonEditor
 jsonEditor.get('a'); // $ExpectType any
 const options: editJsonFile.Options = { autosave: true };
