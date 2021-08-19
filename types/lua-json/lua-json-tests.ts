41f426a88227bda8d88bb0163af300d93aaee17a
bitomic <35199700+bitomic@users.noreply.github.com>
2021-08-18 21:30:26 -0600

    chore: update tests

--- types/lua-json/lua-json-tests.ts
+++ types/lua-json/lua-json-tests.ts
@@ -1,2 +1,8 @@
+interface Example {
+    a: number;
+    b: number;
+};
 luajson.format({ a: 1, b: 2 }); // $ExpectType string
-luajson.parse('return { a = 1, b = 2 }'); // $ExpectType Record<string | number, unknown>
+luajson.parse('return { a = 1, b = 2 }'); // $ExpectType object
+luajson.parse<Example>('return { a = 1, b = 2 }'); // $ExpectType Example
+luajson.parse<number>('return { a = 1, b = 2 }'); // $ExpectError
