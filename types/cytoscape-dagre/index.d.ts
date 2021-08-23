76b9cda20aa42a1ae84a2132a1b1012f47e4f185
Jan-Niclas Strüwer <j.n.struewer@gmail.com>
2021-08-23 10:38:32 +0200

    changed comment style to support display inside of tooltips

--- types/cytoscape-dagre/index.d.ts
+++ types/cytoscape-dagre/index.d.ts
@@ -19,23 +19,44 @@ declare namespace ext {
     interface DagreLayoutOptions extends cytoscape.ShapedLayoutOptions {
         name: 'dagre';
 
-        // the separation between adjacent nodes in the same rank, default: 50
+        /**
+         *  the separation between adjacent nodes in the same rank, default: 50
+         */
         nodeSep?: number | undefined;
-        // the separation between adjacent edges in the same rank, default: 10
+        /**
+         * the separation between adjacent edges in the same rank, default: 10
+         */
         edgeSep?: number | undefined;
-        // the separation between each rank in the layout, default: 50
+        /**
+         * the separation between each rank in the layout, default: 50
+         */
         rankSep?: number | undefined;
-        // 'TB' for top to bottom flow, 'LR' for left to right, default: 'TB'
+        /**
+         *  'TB' for top to bottom flow, 'LR' for left to right, default: 'TB'
+         */
         rankDir?: 'TB' | 'LR' | undefined;
-        // Type of algorithm to assign a rank to each node in the input graph. Possible values: 'network-simplex', 'tight-tree' or 'longest-path', default: 'network-simplex'
+        /**
+         * Type of algorithm to assign a rank to each node in the input graph.
+         * Possible values: 'network-simplex', 'tight-tree', or 'longest-path'.
+         * default: 'network-simplex'
+         */
         ranker?: 'network-simplex' | 'tight-tree' | 'longest-path' | undefined;
-        // number of ranks to keep between the source and target of the edge, default: 1 for all edges
+        /**
+         * number of ranks to keep between the source and target of the edge, default: 1 for all edges
+         */
         minLen?: EdgeAssessmentFunction | undefined;
-        // higher weight edges are generally made shorter and straighter than lower weight edges, default: 1 for all edges
+        /**
+         * higher weight edges are generally made shorter and straighter than lower weight edges,
+         * default: 1 for all edges
+         */
         edgeWeight?: EdgeAssessmentFunction | undefined;
-        // whether to animate specific nodes when animation is on; non-animated nodes immediately go to their final positions
+        /**
+         * whether to animate specific nodes when animation is on; non-animated nodes immediately go to their final positions
+         */
         animateFilter?: AnimationFilterFunction | undefined;
-        // a function that applies a transform to the final node position
+        /**
+         * a function that applies a transform to the final node position
+         */
         transform?: TransformFunction | undefined;
     }
 }
