1fb4760922f57b879d63e39ba4961fccb544e12b
Steven Hankin <steven.hankin@gmail.com>
2021-08-23 23:07:36 +0100

    Add ts version heading

--- types/react-split/index.d.ts
+++ types/react-split/index.d.ts
@@ -2,6 +2,7 @@
 // Project: https://split.js.org/
 // Definitions by: Steven Hankin <https://github.com/stevenhankin>
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
+// TypeScript Version: 3.5
 
 import * as React from 'react';
 
