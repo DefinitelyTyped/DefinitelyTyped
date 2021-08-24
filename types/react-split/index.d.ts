64011e6cb83b8f54720bf1e15ede96edc6482454
Steven Hankin <steven.hankin@gmail.com>
2021-08-24 20:16:08 +0100

    Tsconfig changes

--- types/react-split/index.d.ts
+++ types/react-split/index.d.ts
@@ -8,7 +8,7 @@ import * as React from 'react';
 
 declare class Split extends React.Component<SplitWrapper.SplitProps, any> {}
 
-export default Split
+export = Split;
 
 declare namespace SplitWrapper {
   interface SplitProps {
