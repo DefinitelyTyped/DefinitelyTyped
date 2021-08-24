016f04471f6ed04f9ea91058a9da617355435804
loic.mesieres <loic.mesieres@ubisoft.com>
2021-08-24 15:58:10 +0200

    fix version

--- types/opossum/index.d.ts
+++ types/opossum/index.d.ts
@@ -1,4 +1,4 @@
-// Type definitions for opossum 6.2.1
+// Type definitions for opossum 6.2
 // Project: https://github.com/nodeshift/opossum, https://nodeshift.dev/opossum
 // Definitions by: Quinn Langille <https://github.com/quinnlangille>
 //                 Willy Zhang <https://github.com/merufm>
