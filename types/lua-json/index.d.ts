073571393336aeb47d2cb2a13b2086aa2d066911
bitomic <35199700+bitomic@users.noreply.github.com>
2021-08-25 12:33:45 -0600

    revert: use object instead of package's specified type

--- types/lua-json/index.d.ts
+++ types/lua-json/index.d.ts
@@ -1,28 +1,25 @@
 // Type definitions for lua-json 1.0
-// Minimum Typescript Version: 3.7
 // Project: https://github.com/kcwiki/lua-json#readme
 // Definitions by: bitomic <https://github.com/bitomic>
 //                 Tommy Josépovic <https://github.com/tommy-josepovic>
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
 
-export type Json = null | boolean | number | string | Json[] | { [ _: string ]: Json };
-
 /**
  * Takes a JSON object and returns a Lua table as a string.
  */
 export function format(
-    value: Json,
+    value: object,
     options?: {
         eol?: string;
         singleQuote?: boolean;
-        spaces?: null | number | string;
+        spaces?: number;
     },
 ): string;
 
 /**
  * Takes a Lua table (string) and returns a JSON object.
  */
-export function parse(value: string): Json;
+export function parse(value: string): object;
 
 /**
  * lua-json provides a way to convert between JSON and Lua tables.
--- types/lua-json/lua-json-tests.ts
+++ types/lua-json/lua-json-tests.ts
@@ -1,3 +1,8 @@
-type Json = luajson.Json;
+interface Example {
+    a: number;
+    b: number;
+}
 luajson.format({ a: 1, b: 2 }); // $ExpectType string
-luajson.parse('return { a = 1, b = 2 }'); // $ExpectType Json
+luajson.parse('return { a = 1, b = 2 }'); // $ExpectType object
+luajson.parse('return { a = 1, b = 2 }') as Example; // $ExpectType Example
+luajson.parse('return { a = 1, b = 2 }') as number; // $ExpectError
