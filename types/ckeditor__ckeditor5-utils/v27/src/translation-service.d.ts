21eab50a6dff7a6ce208ecfdecb4d5f111f04389
Federico Panico <federicopanico@gmail.com>
2021-08-25 11:44:48 -0300

    Fix @ckeditor/utils Locale['t'] and constructor

--- types/ckeditor__ckeditor5-utils/v27/src/translation-service.d.ts
+++ types/ckeditor__ckeditor5-utils/v27/src/translation-service.d.ts
@@ -82,7 +82,7 @@ export function add(
  * The internationalization message interface. A message that implements this interface can be passed to the t() function to be translated to the target UI language.
  */
 export interface Message {
-    id: string;
-    plural: string;
+    id?: string;
+    plural?: string;
     string: string;
 }
