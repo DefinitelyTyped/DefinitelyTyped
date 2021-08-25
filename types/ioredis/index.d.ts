f28152817956d7728e446fc245740c87776f8a6d
Hannes Van De Vreken <vandevreken.hannes@gmail.com>
2021-08-25 17:27:42 +0200

    Updated ioredis version number

--- types/ioredis/index.d.ts
+++ types/ioredis/index.d.ts
@@ -1,4 +1,4 @@
-// Type definitions for ioredis 4.26
+// Type definitions for ioredis 4.27
 // Project: https://github.com/luin/ioredis
 // Definitions by: York Yao <https://github.com/plantain-00>
 //                 Christopher Eck <https://github.com/chrisleck>
