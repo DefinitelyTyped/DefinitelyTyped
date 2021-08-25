86da33c3b1fa552418781fe18e1606dc9bd12324
James Lismore <jlismore@gmail.com>
2021-08-25 16:28:30 -0400

    Add function factory export to joi-phone-number

--- types/joi-phone-number/joi-phone-number-tests.ts
+++ types/joi-phone-number/joi-phone-number-tests.ts
@@ -1,3 +1,21 @@
-import * as Joi from 'joi';
+import Joi = require('joi');
+import JoiPhone = require('joi-phone-number');
+
+// extend joi
+Joi.extend(JoiPhone);
 
 Joi.string().phoneNumber({ strict: true }).validate('+123456789');
+
+// no options
+Joi.string().phoneNumber().validate('+123456789');
+
+// empty options
+Joi.string().phoneNumber({}).validate('+123456789');
+
+// country array
+Joi.string()
+    .phoneNumber({ defaultCountry: ['US', 'UK'] })
+    .validate('+123456789');
+
+// all options
+Joi.string().phoneNumber({ defaultCountry: 'US', format: 'e164', strict: true }).validate('+123456789');
