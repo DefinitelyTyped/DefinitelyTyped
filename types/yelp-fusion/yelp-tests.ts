27e52dd30a51b9585d770243af99e338a382f0a2
yoranskiasu <you@example.com>
2021-08-25 23:47:38 -0700

    updating tests

--- types/yelp-fusion/yelp-tests.ts
+++ types/yelp-fusion/yelp-tests.ts
@@ -4,24 +4,3 @@ import yelp from 'yelp-fusion';
 yelp.LocationBusinessesSearchRequest({
     location: 'New York',
 });
-
-// // $ExpectType Builder
-// yelp.createBuilder('cn1', 'cn2', 'cn3');
-
-// // $ExpectType Builder
-// yelp.create('cn1', 'cn2', 'cn3');
-
-// // $ExpectType Builder
-// const builder = yelp.createBuilder('singleClassName');
-
-// // $ExpectType string
-// builder('test');
-
-// // $ExpectType string
-// builder('test', { foo: true });
-
-// // $ExpectType string
-// builder('test', { foo: 100, baz: 'stuff' });
-
-// // $ExpectType boolean
-// yelp.isBuilder(builder);
