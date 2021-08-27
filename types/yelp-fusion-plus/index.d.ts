8c79f4d1091d2653922ac5ab60bc24eb8107f047
yoranskiasu <you@example.com>
2021-08-26 19:13:55 -0700

    removing interfaces from namespace

--- types/yelp-fusion-plus/index.d.ts
+++ types/yelp-fusion-plus/index.d.ts
@@ -2,104 +2,114 @@
 // Project: https://www.npmjs.com/package/@kob490/yelp-fusion-plus
 // Definitions by: Kob490 <https://github.com/kob490>
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
+export {};
+
+// base interfaces\
+interface _BusinessesSearchRequest {
+    term?: string;
+    radius?: number;
+    categories?: string;
+    locale?: string;
+    limit?: number;
+    offset?: number;
+    sort_by?: string;
+    price?: string;
+    open_now?: boolean;
+    open_at?: number;
+    attributes?: string;
+}
+
+interface _BusinessesSearchResponse {
+    total: number;
+    region: Region;
+}
+
+interface Business {
+    id: string;
+    alias: string;
+    name: string;
+    image_url: string;
+    is_closed: boolean;
+    url: string;
+    review_count: number;
+    categories?: Category[] | null;
+    rating: number;
+    coordinates: Coordinates;
+    transactions?: Array<string | null> | null;
+    price?: string | null;
+    location: Location;
+    phone: string;
+    display_phone: string;
+    distance: number;
+}
+
+interface Category {
+    alias: string;
+    title: string;
+}
+
+interface Coordinates {
+    readonly latitude: number;
+    readonly longitude: number;
+}
+
+interface Location {
+    address1: string;
+    address2: string;
+    address3: string;
+    city: string;
+    zip_code: string;
+    country: string;
+    state: string;
+    display_address: string[];
+    cross_streets: string;
+}
+
+interface BusinessDetails extends Business {
+    is_claimed: boolean;
+    photos: string[];
+    hours: Hour[];
+}
+
+interface Hour {
+    open: Open[];
+    hours_type: string;
+    is_open_now: boolean;
+}
+
+interface Open {
+    is_overnight: boolean;
+    start: string;
+    end: string;
+    day: number;
+}
+
+interface Region {
+    center: Coordinates;
+}
+
+// exportables
+export interface LatLngBusinessesSearchRequest extends _BusinessesSearchRequest {
+    latitude: number;
+    longitude: number;
+}
+
+export interface LocationBusinessesSearchRequest extends _BusinessesSearchRequest {
+    location: string;
+}
+
+export interface BusinessesSearchResponse extends _BusinessesSearchResponse {
+    businesses?: Business[];
+}
+
+export interface BusinessesDetailsSearchResponse extends _BusinessesSearchResponse {
+    businesses?: BusinessDetails[];
+}
 
 export namespace YelpFusionPlus {
-    interface Business {
-        id: string;
-        alias: string;
-        name: string;
-        image_url: string;
-        is_closed: boolean;
-        url: string;
-        review_count: number;
-        categories?: Category[] | null;
-        rating: number;
-        coordinates: Coordinates;
-        transactions?: Array<string | null> | null;
-        price?: string | null;
-        location: Location;
-        phone: string;
-        display_phone: string;
-        distance: number;
-    }
+    function CreateBusiness(business: Business): Business;
 
-    interface Coordinates {
-        readonly latitude: number;
-        readonly longitude: number;
-    }
-
-    interface Category {
-        alias: string;
-        title: string;
-    }
-
-    interface Location {
-        address1: string;
-        address2: string;
-        address3: string;
-        city: string;
-        zip_code: string;
-        country: string;
-        state: string;
-        display_address: string[];
-        cross_streets: string;
-    }
-
-    interface BusinessDetails extends Business {
-        is_claimed: boolean;
-        photos: string[];
-        hours: Hour[];
-    }
-
-    interface Hour {
-        open: Open[];
-        hours_type: string;
-        is_open_now: boolean;
-    }
-
-    interface Open {
-        is_overnight: boolean;
-        start: string;
-        end: string;
-        day: number;
-    }
-
-    interface BusinessesSearchRequestBase {
-        term?: string;
-        radius?: number;
-        categories?: string;
-        locale?: string;
-        limit?: number;
-        offset?: number;
-        sort_by?: string;
-        price?: string;
-        open_now?: boolean;
-        open_at?: number;
-        attributes?: string;
-    }
-
-    interface LocationBusinessesSearchRequest extends BusinessesSearchRequestBase {
-        location: string;
-    }
-
-    interface BusinessesSearchResponse extends BusinessesSearchResponseBase {
-        businesses?: Business[];
-    }
-
-    interface BusinessesDetailsSearchResponse extends BusinessesSearchResponseBase {
-        businesses?: BusinessDetails[];
-    }
-
-    interface BusinessesSearchResponseBase {
-        total: number;
-        region: Region;
-    }
-
-    interface Region {
-        center: Coordinates;
-    }
-
-    function LocationBusinessesSearchRequest(
+    function CreateLocationBusinessesSearchRequest(
         locationBusinessesSearchRequest: LocationBusinessesSearchRequest,
     ): LocationBusinessesSearchRequest;
 }
