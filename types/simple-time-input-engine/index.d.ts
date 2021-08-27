c715297be881494e293e024a5edf6fb1ffa7043d
Chethan S <chethan.s@replicon.com>
2021-08-27 11:02:53 +0530

    lint issue fixes

--- types/simple-time-input-engine/index.d.ts
+++ types/simple-time-input-engine/index.d.ts
@@ -1,24 +1,16 @@
 // Type definitions for simple-time-input-engine 0.0
 // Project: https://github.com/Citifyd/simple-time-input-engine#readme
-// Definitions by: <https://github.com/repl-chethan-s>
+// Definitions by: Chethan S <https://github.com/repl-chethan-s>
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
 
-export function formatTimeForDisplay({
-    time,
-    clockMode
-  }: {
-    time: string;
-    clockMode: number;
-  }): string;
+export function formatTimeForDisplay({ time, clockMode }: { time: string; clockMode: number }): string;
 
- export function parseInputChange({
+export function parseInputChange({
     newValue,
     previousTime,
-    clockMode
-  }: {
+    clockMode,
+}: {
     newValue: string;
     previousTime: string;
     clockMode: number;
-  }): { time: string; valid: boolean };
-
-
+}): { time: string; valid: boolean };
