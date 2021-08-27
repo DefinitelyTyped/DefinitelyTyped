a2de4c6e0606f7b841ca1af79ebd9a904bb91acd
Tomas Valenta <valenta.and.thomas@gmail.com>
2021-08-27 20:11:05 +0200

    Update package version in the header

--- types/prosemirror-view/index.d.ts
+++ types/prosemirror-view/index.d.ts
@@ -1,4 +1,4 @@
-// Type definitions for prosemirror-view 1.18
+// Type definitions for prosemirror-view 1.19.3
 // Project: https://github.com/ProseMirror/prosemirror-view
 // Definitions by: Bradley Ayers <https://github.com/bradleyayers>
 //                 David Hahn <https://github.com/davidka>
