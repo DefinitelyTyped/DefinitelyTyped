263946e555279bfcfcbb54bf9d122b0d5cc272b5
Pika-Pool <33688622+Pika-Pool@users.noreply.github.com>
2021-08-27 19:57:51 +0530

    define overloads for `decode` function
    `decode` function acts as an **overloaded** function, where `encoding` can be either 2nd/3rd/4th argument. But ts throws if used that way, since encoding's `string` type cannot be assigned to `number|undefined` type
    
    So this PR defines the required overloads.
    
    Function defined at: https://github.com/webtorrent/node-bencode/blob/1bf04a25691f0118c3e456a020cdcf4ab9c553c8/lib/decode.js#L57

--- types/bencode/index.d.ts
+++ types/bencode/index.d.ts
@@ -8,6 +8,8 @@
 export function byteLength(value: any): number;
 export function encodingLength(value: any): number;
 export function encode(data: any, buffer?: Buffer, offset?: number): Buffer;
+export function decode(data: Buffer, encoding?: string): any;
+export function decode(data: Buffer, start?: number, encoding?: string): any;
 export function decode(
     data: Buffer,
     start?: number,
