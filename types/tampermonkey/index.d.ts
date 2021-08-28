ce73f1d6edc71b564d6253b93be9093d64b0890b
Luca <61409445+melusc@users.noreply.github.com>
2021-08-28 16:12:38 +0200

    Update index.d.ts

--- types/tampermonkey/index.d.ts
+++ types/tampermonkey/index.d.ts
@@ -9,12 +9,7 @@
 // TypeScript Version: 3.3
 
 declare namespace Tampermonkey {
-    type ValueChangeListener = (
-        name: string,
-        oldValue: any,
-        newValue: any,
-        remote: boolean
-    ) => void;
+    type ValueChangeListener = (name: string, oldValue: any, newValue: any, remote: boolean) => void;
 
     // Response
 
@@ -23,7 +18,7 @@ declare namespace Tampermonkey {
         Opened = 1,
         HeadersReceived = 2,
         Loading = 3,
-        Done = 4
+        Done = 4,
     }
 
     interface ResponseBase {
@@ -54,9 +49,7 @@ declare namespace Tampermonkey {
         readonly context: TContext;
     }
 
-    interface ProgressResponse<TContext>
-        extends Response<TContext>,
-            ProgressResponseBase {}
+    interface ProgressResponse<TContext> extends Response<TContext>, ProgressResponseBase {}
 
     // Request
 
@@ -64,10 +57,7 @@ declare namespace Tampermonkey {
         readonly [header: string]: string;
     }
 
-    type RequestEventListener<TResponse> = (
-        this: TResponse,
-        response: TResponse
-    ) => void;
+    type RequestEventListener<TResponse> = (this: TResponse, response: TResponse) => void;
 
     interface Request<TContext = object> {
         method?: 'GET' | 'HEAD' | 'POST' | undefined;
@@ -144,12 +134,7 @@ declare namespace Tampermonkey {
          * - `not_succeeded` - the download wasn't started or failed, the
          * details attribute may provide more information
          */
-        error:
-            | 'not_enabled'
-            | 'not_whitelisted'
-            | 'not_permitted'
-            | 'not_supported'
-            | 'not_succeeded';
+        error: 'not_enabled' | 'not_whitelisted' | 'not_permitted' | 'not_supported' | 'not_succeeded';
         /** Detail about that error */
         details?: string | undefined;
     }
@@ -239,6 +224,123 @@ declare namespace Tampermonkey {
     }
 
     type NotificationDetails = TextNotification | HighlightNotification;
+
+    interface GM_info {
+        downloadMode: 'native' | 'browser' | 'disabled';
+        isFirstPartyIsolation: boolean | undefined;
+        isIncognito: boolean;
+
+        /**
+         * `.. | null` means if it was not explicitely set in the metadata
+         * block it is null
+         */
+        script: {
+            antifeatures: Record<string, Record<string, string>>;
+            author: string | null;
+
+            /**
+             * Idk what this is, nothing I did changed this from any empty array
+             * and it's not documented anywhere
+             */
+            blockers: any[];
+
+            copyright: string | null;
+            description: string | null;
+            description_i18n: Record<string, string>;
+            downloadURL: string | null;
+            evilness: number;
+            excludes: string[];
+            grant: string[];
+            header: string;
+            homepage: string | null;
+            icon: string | null;
+            icon64: string | null;
+            includes: string[];
+            lastModified: number;
+            matches: string[];
+            name: string;
+            name_i18n: Record<string, string>;
+            namespace: string | null;
+
+            /**
+             * The options that the user of the userscript
+             * can override in the settings
+             */
+            options: {
+                check_for_updates: boolean;
+                comment: string | null;
+                compat_foreach: boolean;
+                compat_metadata: boolean;
+                compat_prototypes: boolean;
+                compat_wrappedjsobject: boolean;
+                compatopts_for_requires: boolean;
+                noframes: boolean | null;
+
+                /** This uses an underscore */
+                run_at: string;
+                override: {
+                    merge_connects: boolean;
+                    merge_excludes: boolean;
+                    merge_includes: boolean;
+                    merge_matches: boolean;
+                    orig_connects: string[];
+                    orig_excludes: string[];
+                    orig_includes: string[];
+                    orig_matches: string[];
+                    orig_noframes: string | null;
+
+                    /** This also uses an underscore */
+                    orig_run_at: string | null;
+                    use_blockers: string[];
+                    use_connects: string[];
+                    use_excludes: string[];
+                    use_includes: string[];
+                    use_matches: string[];
+                };
+            };
+
+            position: number;
+            resources: Array<{
+                name: string;
+                url: string;
+                content: string;
+                meta: string;
+            }>;
+
+            /**
+             * Never null, defaults to document-idle
+             *
+             * But this uses a hyphen in TM 4.13.6136 because
+             * that's how it is in the metadata
+             */
+            'run-at': string;
+
+            supportURL: string | null;
+            sync: {
+                imported: boolean;
+            };
+            unwrap: boolean;
+            updateURL: string | null;
+            uuid: string;
+            version: string;
+            webRequest: string[];
+        };
+
+        /**
+         * In tampermonkey it's "Tampermonkey"
+         * but I'll leave it as string so this can be used
+         * for other managers
+         */
+        scriptHandler: string;
+
+        scriptMetaStr: string;
+        scriptSource: string;
+        scriptUpdateURL: string | undefined;
+        scriptWillUpdate: boolean;
+
+        /** This refers to tampermonkey's version */
+        version: string;
+    }
 }
 
 /**
@@ -267,19 +369,13 @@ declare function GM_setValue(name: string, value: any): void;
  * different browser tabs to communicate with each other.
  * @param name Name of the observed variable
  */
-declare function GM_addValueChangeListener(
-    name: string,
-    listener: Tampermonkey.ValueChangeListener
-): number;
+declare function GM_addValueChangeListener(name: string, listener: Tampermonkey.ValueChangeListener): number;
 
 /** Removes a change listener by its ID */
 declare function GM_removeValueChangeListener(listenerId: number): void;
 
 /** Gets the value of 'name' from storage */
-declare function GM_getValue<TValue>(
-    name: string,
-    defaultValue?: TValue
-): TValue;
+declare function GM_getValue<TValue>(name: string, defaultValue?: TValue): TValue;
 
 /** Deletes 'name' from storage */
 declare function GM_deleteValue(name: string): void;
@@ -304,11 +400,7 @@ declare function GM_getResourceURL(name: string): string;
  * Register a menu to be displayed at the Tampermonkey menu at pages where this
  * script runs and returns a menu command ID.
  */
-declare function GM_registerMenuCommand(
-    name: string,
-    onClick: () => void,
-    accessKey?: string
-): number;
+declare function GM_registerMenuCommand(name: string, onClick: () => void, accessKey?: string): number;
 
 /**
  *  Unregister a menu command that was previously registered by
@@ -320,17 +412,12 @@ declare function GM_unregisterMenuCommand(menuCommandId: number): void;
 
 /** Makes an xmlHttpRequest */
 declare function GM_xmlhttpRequest<TContext = any>(
-    details: Tampermonkey.Request<TContext> // tslint:disable-line:no-unnecessary-generics
+    details: Tampermonkey.Request<TContext>, // tslint:disable-line:no-unnecessary-generics
 ): Tampermonkey.AbortHandle<void>;
 
 /** Downloads a given URL to the local disk */
-declare function GM_download(
-    details: Tampermonkey.DownloadRequest
-): Tampermonkey.AbortHandle<boolean>;
-declare function GM_download(
-    url: string,
-    name: string
-): Tampermonkey.AbortHandle<boolean>;
+declare function GM_download(details: Tampermonkey.DownloadRequest): Tampermonkey.AbortHandle<boolean>;
+declare function GM_download(url: string, name: string): Tampermonkey.AbortHandle<boolean>;
 
 // Tabs
 
@@ -341,11 +428,11 @@ declare function GM_saveTab(obj: object): void;
 declare function GM_getTab(callback: (obj: any) => void): void;
 
 /** Gets all tab objects as a hash to communicate with other script instances */
-declare function GM_getTabs(
-    callback: (tabsMap: { [tabId: number]: any }) => void
-): void;
+declare function GM_getTabs(callback: (tabsMap: { [tabId: number]: any }) => void): void;
 
 // Utils
+
+declare const GM_info: Tampermonkey.GM_info;
 
 /** Log a message to the console */
 declare function GM_log(...message: any[]): void;
@@ -366,10 +453,7 @@ declare function GM_log(...message: any[]): void;
  * @returns Object with the function `close`, the listener `onclosed` and a flag
  * called `closed`.
  */
-declare function GM_openInTab(
-    url: string,
-    options?: Tampermonkey.OpenTabOptions | boolean
-): Tampermonkey.OpenTabObject;
+declare function GM_openInTab(url: string, options?: Tampermonkey.OpenTabOptions | boolean): Tampermonkey.OpenTabObject;
 
 /**
  * Shows a HTML5 Desktop notification and/or highlight the current tab.
@@ -377,7 +461,7 @@ declare function GM_openInTab(
  */
 declare function GM_notification(
     details: Tampermonkey.NotificationDetails,
-    ondone?: Tampermonkey.NotificationOnDone
+    ondone?: Tampermonkey.NotificationOnDone,
 ): void;
 
 /**
@@ -390,7 +474,7 @@ declare function GM_notification(
     text: string,
     title?: string,
     image?: string,
-    onclick?: Tampermonkey.NotificationOnClick
+    onclick?: Tampermonkey.NotificationOnClick,
 ): void;
 
 /**
@@ -401,5 +485,5 @@ declare function GM_notification(
  */
 declare function GM_setClipboard(
     data: string,
-    info?: string | { type?: string | undefined; mimetype?: string | undefined }
+    info?: string | { type?: string | undefined; mimetype?: string | undefined },
 ): void;
