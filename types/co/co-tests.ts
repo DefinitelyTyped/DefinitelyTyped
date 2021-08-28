3525748c1e7b227a9773bdb42377ccadb2f4a82a
Yiming Li <yimingli.cs@gmail.com>
2021-08-28 13:08:01 +0800

    feat: refine types for co

--- types/co/co-tests.ts
+++ types/co/co-tests.ts
@@ -1,23 +1,41 @@
 import co = require('co');
 
-function* gen(num: number, str: string, arr: number[], obj: object, fun: () => void) {
+function* gen(
+    num: number,
+    str: string,
+    arr: number[],
+    obj: object,
+    fun: () => void,
+): Generator<undefined, number, undefined> {
     return num;
 }
 
 co(gen, 42, 'forty-two', [42], { value: 42 }, () => {})
-    .then((num: number) => {}, (err: Error) => {})
+    .then(
+        (num: number) => {},
+        (err: Error) => {},
+    )
     .catch((err: Error) => {});
 
 co.default(gen, 42, 'forty-two', [42], { value: 42 }, () => {})
-    .then((num: number) => {}, (err: Error) => {})
+    .then(
+        (num: number) => {},
+        (err: Error) => {},
+    )
     .catch((err: Error) => {});
 
 co.co(gen, 42, 'forty-two', [42], { value: 42 }, () => {})
-    .then((num: number) => {}, (err: Error) => {})
+    .then(
+        (num: number) => {},
+        (err: Error) => {},
+    )
     .catch((err: Error) => {});
 
 co.wrap(gen)(42, 'forty-two', [42], { value: 42 }, () => {})
-    .then((num: number) => {}, (err: Error) => {})
+    .then(
+        (num: number) => {},
+        (err: Error) => {},
+    )
     .catch((err: Error) => {});
 
 // $ExpectError
@@ -28,3 +46,15 @@ co.wrap(gen)();
 
 // $ExpectError
 co.wrap(gen)('forty-two');
+
+// example from https://github.com/tj/co/blob/master/Readme.md
+function* gen1(): Generator<Promise<boolean>, boolean, boolean> {
+    const result = yield Promise.resolve(true);
+    return result;
+}
+co.co(gen1)
+    .then(
+        (result: boolean) => {},
+        (err: Error) => {},
+    )
+    .catch((err: Error) => {});
