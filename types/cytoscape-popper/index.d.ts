dc8dd4c02d46c1a44a2ec54a5d8487170eddbb20
Felix <felix2510@gmx.net>
2021-08-28 10:13:35 +0200

    Added tests

--- types/cytoscape-popper/index.d.ts
+++ types/cytoscape-popper/index.d.ts
@@ -16,28 +16,34 @@ declare namespace cytoscapePopper {
         h: number;
     }
 
-    interface Options {
+    interface Options<Type> {
         /*
          * The HTML content of the popper. May be a DOM Element reference or a function that returns one.
          */
         content?: () => HTMLElement | HTMLElement | undefined;
         /*
-         * A function that can be used to override the [rendered](http://js.cytoscape.org/#notation/position) Cytoscape position of the Popper target. This option is mandatory when using Popper on the core. For an element, the centre of its bounding box is used by default.
+         * A function that can be used to override the [rendered](http://js.cytoscape.org/#notation/position) Cytoscape position of the Popper target.
+         * This option is mandatory when using Popper on the core.
+         * For an element, the centre of its bounding box is used by default.
          */
-        renderedPosition?: (el: cytoscape.SingularData | cytoscape.Core) => cytoscape.Position | undefined;
+        renderedPosition?: ((el: Type) => cytoscape.Position) | undefined;
         /*
-         * A function that can be used to override the [rendered](http://js.cytoscape.org/#notation/position) Cytoscape bounding box dimensions considered for the popper target (i.e. `cy` or `ele`).It defines only the effective `width` and `height` (`bb.w` and `bb.h`) of the Popper target. This option is more often useful for elements rather than for the core.
+         * A function that can be used to override the [rendered](http://js.cytoscape.org/#notation/position) Cytoscape bounding box dimensions
+         * considered for the popper target (i.e. `cy` or `ele`).
+         * It defines only the effective `width` and `height` (`bb.w` and `bb.h`) of the Popper target.
+         * This option is more often useful for elements rather than for the core.
          */
-        renderedDimensions?: (el: cytoscape.SingularData | cytoscape.Core) => Dimensions | undefined;
+        renderedDimensions?: ((el: Type) => Dimensions) | undefined;
         /*
-         * The [Popper options](https://popper.js.org/docs/v2/constructors/#options) object. You may use this to override Popper options.
+         * The [Popper options](https://popper.js.org/docs/v2/constructors/#options) object.
+         * You may use this to override Popper options.
          */
         popper?: popper.Options | undefined;
     }
 
-    type getPopperInstance = (opts?: Options) => popper.Instance;
+    type getPopperInstance<Type> = (opts?: Options<Type>) => popper.Instance;
 
-    type getPopperRef = (opts?: Options) => popper.VirtualElement;
+    type getPopperRef<Type> = (opts?: Options<Type>) => popper.VirtualElement;
 }
 
 declare global {
@@ -46,22 +52,24 @@ declare global {
             /*
              * Get a [Popper Instance](https://popper.js.org/docs/v2/constructors/) for the specified element.
              */
-            popper: cytoscapePopper.getPopperInstance;
+            popper: cytoscapePopper.getPopperInstance<SingularData>;
             /*
-             * Get a [Popper virtual element](https://popper.js.org/docs/v2/virtual-elements/) (aka Popper reference object in Popper v1) for the specified specified element.
+             * Get a [Popper virtual element](https://popper.js.org/docs/v2/virtual-elements/)
+             * (aka Popper reference object in Popper v1) for the specified specified element.
              */
-            popperRef: cytoscapePopper.getPopperRef;
+            popperRef: cytoscapePopper.getPopperRef<SingularData>;
         }
 
         interface Core {
             /*
              * Get a [Popper Instance](https://popper.js.org/docs/v2/constructors/) for the specified core Cytoscape instance.
              */
-            popper: cytoscapePopper.getPopperInstance;
+            popper: cytoscapePopper.getPopperInstance<Core>;
             /*
-             * Get a [Popper virtual element](https://popper.js.org/docs/v2/virtual-elements/) (aka Popper reference object in Popper v1) for the specified core Cytoscape instance.
+             * Get a [Popper virtual element](https://popper.js.org/docs/v2/virtual-elements/)
+             * (aka Popper reference object in Popper v1) for the specified core Cytoscape instance.
              */
-            popperRef: cytoscapePopper.getPopperRef;
+            popperRef: cytoscapePopper.getPopperRef<Core>;
         }
     }
 }
