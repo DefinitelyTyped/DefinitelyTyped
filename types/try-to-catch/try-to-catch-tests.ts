cd5f1f6135a3b58b9827125de34923c30abdb0f6
coderaiser <mnemonic.enemy@gmail.com>
2021-08-28 12:32:45 +0300

    Update types/try-to-catch/try-to-catch-tests.ts
    Co-authored-by: Karl Horky <karl.horky@gmail.com>

--- types/try-to-catch/try-to-catch-tests.ts
+++ types/try-to-catch/try-to-catch-tests.ts
@@ -1,9 +1,9 @@
 import tryToCatch = require("try-to-catch");
 
 const main = async () => {
-    await tryToCatch(() => 5); // $ExpectType [error: null, result: 5]
+    await tryToCatch(() => 5); // $ExpectType [error: Error] | [error: null, result: number]
 
-    await tryToCatch(async () => 5); // $ExpectType [error: null, result: 5]
+    await tryToCatch(async () => 5); // $ExpectType [error: Error] | [error: null, result: number]
 
-    await tryToCatch(() => throw Error("error"));  // $ExpectType [error: Error]
+    await tryToCatch(() => { throw new Error("error") });  // $ExpectType [error: Error] | [error: null, result: unknown]
 };
