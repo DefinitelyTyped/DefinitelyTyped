7c003ffa82695ade7be3a182fe428c6c6f8b69a5
Jiří Hulmák <jirka.hulmak@gmail.com>
2021-08-29 10:32:16 +0200

    converting resource interfaces to enum types

--- types/steamcommunity/index.d.ts
+++ types/steamcommunity/index.d.ts
@@ -8,12 +8,6 @@
 
 type SteamID = import('steamid');
 import { Request } from 'request';
-import { EResult } from './resources/EResult';
-import { EChatState } from './resources/EChatState';
-import { EConfirmationType } from './resources/EConfirmationType';
-import { EFriendRelationship } from './resources/EFriendRelationship';
-import { EPersonaState } from './resources/EPersonaState';
-import { EPersonaStateFlag } from './resources/EPersonaStateFlag';
 
 import CMarketItem = require('./classes/CMarketItem');
 import CSteamGroup = require('./classes/CSteamGroup');
@@ -33,8 +27,7 @@ import { TwoFactor } from './components/twofactor';
 import { Users } from './components/users';
 import { WebApi } from './components/webapi';
 
-interface SteamCommunity extends Chat, Confirmations, Groups, Help, Helpers, Http, InventoryHistory, Market, Profile, TwoFactor, Users, WebApi {
-}
+interface SteamCommunity extends Chat, Confirmations, Groups, Help, Helpers, Http, InventoryHistory, Market, Profile, TwoFactor, Users, WebApi {}
 
 declare class SteamCommunity {
     constructor(options?: SteamCommunity.Options);
@@ -277,32 +270,13 @@ declare class SteamCommunity {
     setCookies(cookies: string[]): void;
 
     steamID: SteamID;
-
-    static ConfirmationType: EConfirmationType;
-
-    static EFriendRelationship: EFriendRelationship;
-
-    static EResult: EResult;
-
-    /** The current state of our chat connection. One of the following values. */
-    static ChatState: EChatState;
-
-    static PersonaState: EPersonaState;
-
-    static PersonaStateFlag: EPersonaStateFlag;
-
-    static PrivacyState: {
-        'Private': 1,
-        'FriendsOnly': 2,
-        'Public': 3
-    };
 }
 
 declare namespace SteamCommunity {
     interface Options {
         /**
          * An instance of {@link https://www.npmjs.com/package/request|request} v2.x.x which will be used by `SteamCommunity` for its HTTP requests.
-         * `SteamCommunity` will create its own if omitted.
+         * SteamCommunity` will create its own if omitted.
          */
         request: Request;
 
@@ -341,6 +315,203 @@ declare namespace SteamCommunity {
     type packageid = number | string;
 
     type gid = SteamID | string;
+
+    // region Static enums
+
+    enum PrivacyState {
+        'Private' = 1,
+        'FriendsOnly' = 2,
+        'Public' = 3
+    }
+
+    /** The current state of our chat connection. One of the following values. */
+    enum ChatState {
+        'Offline' = 0,
+        'LoggingOn' = 1,
+        'LogOnFailed' = 2,
+        'LoggedOn' = 3
+    }
+
+    /**
+     * 1 is unknown, possibly "Invalid".
+     * 4 is opt-out or other like account confirmation?
+     */
+    enum ConfirmationType {
+        'Trade' = 2,
+        'MarketListing' = 3,
+    }
+
+    enum EFriendRelationship {
+        'None' = 0,
+        'Blocked' = 1,
+        'RequestRecipient' = 2,
+        'Friend' = 3,
+        'RequestInitiator' = 4,
+        'Ignored' = 5,
+        'IgnoredFriend' = 6,
+        'SuggestedFriend' = 7, // removed "was used by the original implementation of the facebook linking feature; but now unused."
+    }
+
+    enum PersonaState {
+        'Offline' = 0,
+        'Online' = 1,
+        'Busy' = 2,
+        'Away' = 3,
+        'Snooze' = 4,
+        'LookingToTrade' = 5,
+        'LookingToPlay' = 6,
+        'Invisible' = 7
+    }
+
+    enum PersonaStateFlag {
+        'HasRichPresence' = 1,
+        'InJoinableGame' = 2,
+        'Golden' = 4,
+        'RemotePlayTogether' = 8,
+        'OnlineUsingWeb' = 256,
+        'ClientTypeWeb' = 256,
+        'OnlineUsingMobile' = 512,
+        'ClientTypeMobile' = 512,
+        'OnlineUsingBigPicture' = 1024,
+        'ClientTypeTenfoot' = 1024,
+        'OnlineUsingVR' = 2048,
+        'ClientTypeVR' = 2048,
+        'LaunchTypeGamepad' = 4096,
+        'LaunchTypeCompatTool' = 8192
+    }
+
+    enum EResult {
+        'Invalid' = 0,
+        'OK' = 1,
+        'Fail' = 2,
+        'NoConnection' = 3,
+        'InvalidPassword' = 5,
+        'LoggedInElsewhere' = 6,
+        'InvalidProtocolVer' = 7,
+        'InvalidParam' = 8,
+        'FileNotFound' = 9,
+        'Busy' = 10,
+        'InvalidState' = 11,
+        'InvalidName' = 12,
+        'InvalidEmail' = 13,
+        'DuplicateName' = 14,
+        'AccessDenied' = 15,
+        'Timeout' = 16,
+        'Banned' = 17,
+        'AccountNotFound' = 18,
+        'InvalidSteamID' = 19,
+        'ServiceUnavailable' = 20,
+        'NotLoggedOn' = 21,
+        'Pending' = 22,
+        'EncryptionFailure' = 23,
+        'InsufficientPrivilege' = 24,
+        'LimitExceeded' = 25,
+        'Revoked' = 26,
+        'Expired' = 27,
+        'AlreadyRedeemed' = 28,
+        'DuplicateRequest' = 29,
+        'AlreadyOwned' = 30,
+        'IPNotFound' = 31,
+        'PersistFailed' = 32,
+        'LockingFailed' = 33,
+        'LogonSessionReplaced' = 34,
+        'ConnectFailed' = 35,
+        'HandshakeFailed' = 36,
+        'IOFailure' = 37,
+        'RemoteDisconnect' = 38,
+        'ShoppingCartNotFound' = 39,
+        'Blocked' = 40,
+        'Ignored' = 41,
+        'NoMatch' = 42,
+        'AccountDisabled' = 43,
+        'ServiceReadOnly' = 44,
+        'AccountNotFeatured' = 45,
+        'AdministratorOK' = 46,
+        'ContentVersion' = 47,
+        'TryAnotherCM' = 48,
+        'PasswordRequiredToKickSession' = 49,
+        'AlreadyLoggedInElsewhere' = 50,
+        'Suspended' = 51,
+        'Cancelled' = 52,
+        'DataCorruption' = 53,
+        'DiskFull' = 54,
+        'RemoteCallFailed' = 55,
+        'PasswordNotSet' = 56, // removed "renamed to PasswordUnset
+        'PasswordUnset' = 56,
+        'ExternalAccountUnlinked' = 57,
+        'PSNTicketInvalid' = 58,
+        'ExternalAccountAlreadyLinked' = 59,
+        'RemoteFileConflict' = 60,
+        'IllegalPassword' = 61,
+        'SameAsPreviousValue' = 62,
+        'AccountLogonDenied' = 63,
+        'CannotUseOldPassword' = 64,
+        'InvalidLoginAuthCode' = 65,
+        'AccountLogonDeniedNoMailSent' = 66, // removed "renamed to AccountLogonDeniedNoMail
+        'AccountLogonDeniedNoMail' = 66,
+        'HardwareNotCapableOfIPT' = 67,
+        'IPTInitError' = 68,
+        'ParentalControlRestricted' = 69,
+        'FacebookQueryError' = 70,
+        'ExpiredLoginAuthCode' = 71,
+        'IPLoginRestrictionFailed' = 72,
+        'AccountLocked' = 73, // removed "renamed to AccountLockedDown
+        'AccountLockedDown' = 73,
+        'AccountLogonDeniedVerifiedEmailRequired' = 74,
+        'NoMatchingURL' = 75,
+        'BadResponse' = 76,
+        'RequirePasswordReEntry' = 77,
+        'ValueOutOfRange' = 78,
+        'UnexpectedError' = 79,
+        'Disabled' = 80,
+        'InvalidCEGSubmission' = 81,
+        'RestrictedDevice' = 82,
+        'RegionLocked' = 83,
+        'RateLimitExceeded' = 84,
+        'AccountLogonDeniedNeedTwoFactorCode' = 85, // removed "renamed to AccountLoginDeniedNeedTwoFactor
+        'AccountLoginDeniedNeedTwoFactor' = 85,
+        'ItemOrEntryHasBeenDeleted' = 86, // removed "renamed to ItemDeleted
+        'ItemDeleted' = 86,
+        'AccountLoginDeniedThrottle' = 87,
+        'TwoFactorCodeMismatch' = 88,
+        'TwoFactorActivationCodeMismatch' = 89,
+        'AccountAssociatedToMultiplePlayers' = 90, // removed "renamed to AccountAssociatedToMultiplePartners
+        'AccountAssociatedToMultiplePartners' = 90,
+        'NotModified' = 91,
+        'NoMobileDeviceAvailable' = 92, // removed "renamed to NoMobileDevice
+        'NoMobileDevice' = 92,
+        'TimeIsOutOfSync' = 93, // removed "renamed to TimeNotSynced
+        'TimeNotSynced' = 93,
+        'SMSCodeFailed' = 94,
+        'TooManyAccountsAccessThisResource' = 95, // removed "renamed to AccountLimitExceeded
+        'AccountLimitExceeded' = 95,
+        'AccountActivityLimitExceeded' = 96,
+        'PhoneActivityLimitExceeded' = 97,
+        'RefundToWallet' = 98,
+        'EmailSendFailure' = 99,
+        'NotSettled' = 100,
+        'NeedCaptcha' = 101,
+        'GSLTDenied' = 102,
+        'GSOwnerDenied' = 103,
+        'InvalidItemType' = 104,
+        'IPBanned' = 105,
+        'GSLTExpired' = 106,
+        'InsufficientFunds' = 107,
+        'TooManyPending' = 108,
+        'NoSiteLicensesFound' = 109,
+        'WGNetworkSendExceeded' = 110,
+        'AccountNotFriends' = 111,
+        'LimitedUserAccount' = 112,
+        'CantRemoveItem' = 113,
+        'AccountHasBeenDeleted' = 114,
+        'AccountHasAnExistingUserCancelledLicense' = 115,
+        'DeniedDueToCommunityCooldown' = 116,
+        'NoLauncherSpecified' = 117,
+        'MustAgreeToSSA' = 118,
+        'ClientNoLongerSupported' = 119
+    }
+
+    // endregion Enums
 }
 
 export = SteamCommunity;
--- types/steamcommunity/resources/EConfirmationType.d.ts
+++
@@ -1,9 +0,0 @@
-export interface EConfirmationType {
-    // 1 is unknown, possibly "Invalid"
-    'Trade': 2;
-    'MarketListing': 3;
-    // 4 is opt-out or other like account confirmation?
-
-    '2': 'Trade';
-    '3': 'MarketListing';
-}
