b610de9688e91fde2720ab460a68ba017f6e58b9
Beeno Tung <aabbcc1241@yahoo.com.hk>
2021-08-30 17:05:57 +0800

    add test case for previous pull request
    add test case for https://github.com/DefinitelyTyped/DefinitelyTyped/pull/55133#issuecomment-898810232

--- types/node-forge/node-forge-tests.ts
+++ types/node-forge/node-forge-tests.ts
@@ -146,6 +146,21 @@ if (forge.util.fillString('1', 5) !== '11111') throw Error('forge.util.fillStrin
 }
 
 {
+    let count = 32;
+    let key = forge.random.getBytes(count, (err, bytes) => {
+        if (err) {
+            throw err;
+        }
+        if (bytes.length !== count) {
+            throw new Error('invalid length');
+        }
+        if (key !== bytes) {
+            throw new Error('return result and callback data not match');
+        }
+    });
+}
+
+{
     cert.publicKey = keypair.publicKey;
     cert.serialNumber = new Date().getTime() + '';
     cert.validity.notBefore = new Date();
