b175ed487589aa7eb3cb187c67ad5c7b578ba46a
Daniel Rose <danrose@gmx.de>
2021-08-30 12:18:06 +0200

    [ag-auth] Add new type definitions

---
+++ types/ag-auth/index.d.ts
@@ -0,0 +1,19 @@
+// Type definitions for ag-auth 1.0
+// Project: https://github.com/SocketCluster/ag-auth
+// Definitions by: Daniel Rose <https://github.com/DanielRose>
+// Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
+// Minimum TypeScript Version: 3.7
+
+import { VerifyOptions, Secret, SignOptions, Jwt } from 'jsonwebtoken';
+
+export interface AGAuthEngine {
+    verifyToken(signedToken: string | null, key: Secret, options?: VerifyOptions): Promise<Jwt>;
+    signToken(token: string | object | Buffer, key: Secret, options?: SignOptions): Promise<string | undefined>;
+}
+
+export class AuthEngine implements AGAuthEngine {
+    constructor();
+
+    verifyToken(signedToken: string | null, key: Secret, options?: VerifyOptions): Promise<Jwt>;
+    signToken(token: string | object | Buffer, key: Secret, options?: SignOptions): Promise<string | undefined>;
+}
