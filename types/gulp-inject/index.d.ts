2947d3a9ee450019916d61b49ce532a5c7454a11
joshuaharry <84721070+joshuaharry@users.noreply.github.com>
2021-08-29 21:22:02 -0500

    Bump version

--- types/gulp-inject/index.d.ts
+++ types/gulp-inject/index.d.ts
@@ -1,4 +1,4 @@
-// Type definitions for gulp-inject
+// Type definitions for gulp-inject 5.0
 // Project: https://github.com/klei/gulp-inject
 // Definitions by: Keita Kagurazaka <https://github.com/k-kagurazaka>
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
