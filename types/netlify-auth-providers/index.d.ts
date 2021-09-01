a3e59cb5d3cd3cf7f488271fff7a147a275c7447
Drazi Crendraven <drazisil@users.noreply.github.com>
2021-09-01 17:05:49 -0400

    chore: add types for netlify-auth-providers

---
+++ types/netlify-auth-providers/index.d.ts
@@ -0,0 +1,23 @@
+// Type definitions for netlify-auth-providers 1.0.0-alpha5
+// Project: https://www.npmjs.com/package/netlify-auth-providers
+// Definitions by: Drazi Crendraven <https://github.com/drazisil/>
+// Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
+declare module 'netlify-auth-providers' {
+  interface Config {
+    site_id?: string
+    base_url?: string
+  }
+
+  interface AuthenticatorConfig {
+    provider: string
+    scope?: string
+  }
+
+  class Authenticator {
+    constructor(config: Config)
+
+    authenticate(options: AuthenticatorConfig, cb: (err: Error, data: any) => void): void
+  }
+
+  export default Authenticator
+}
