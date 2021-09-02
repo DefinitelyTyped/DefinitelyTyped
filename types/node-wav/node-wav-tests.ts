da86d640eacec78cab637aff603331ef7fbc5a4d
jameswilddev <jameswilddev@outlook.com>
2021-09-02 21:46:34 +0100

    Replace test with @peterblazejewicz's implementation.

--- types/node-wav/node-wav-tests.ts
+++ types/node-wav/node-wav-tests.ts
@@ -1,35 +1,17 @@
-/**
- * Copyright 2016 Andreas Gal
- *
- * Permission is hereby granted, free of charge, to any person obtaining a copy
- * of this software and associated documentation files (the "Software"), to deal
- * in the Software without restriction, including without limitation the rights
- * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
- * copies of the Software, and to permit persons to whom the Software is
- * furnished to do so, subject to the following conditions:
- *
- * The above copyright notice and this permission notice shall be included in
- * all copies or substantial portions of the Software.
- *
- * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
- * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
- * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
- * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
- * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
- * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
- * SOFTWARE.
- */
+import wav = require('node-wav');
 
-// The following has been altered from the
-// [original version](https://github.com/andreasgal/node-wav/commit/9804a3fcbb251adb2766a5dad739b1ed033f3ba5?short_path=b335630#diff-b335630551682c19a781afebcf4d07bf978fb1f8ac04c6bf87428ed5106870f5)
-// to fit within DefinitelyTyped lint requirements.
+declare const buffer: Buffer;
 
-import * as fs from 'fs';
-import * as wav from 'node-wav';
-
-const buffer = fs.readFileSync('file.wav');
-const result = wav.decode(buffer);
-console.log(result.sampleRate);
-console.log(result.channelData); // array of Float32Arrays
-
-wav.encode(result.channelData, { sampleRate: result.sampleRate, float: true, bitDepth: 32 });
+// $ExpectType { readonly sampleRate: number; readonly channelData: readonly Float32Array[]; }
+const wavFileInfo = wav.decode(buffer);
+// $ExpectType Buffer
+wav.encode(wavFileInfo.channelData, {
+    sampleRate: wavFileInfo.sampleRate,
+    float: true,
+    bitDepth: 32,
+    floatingPoint: true,
+});
+// $ExpectType Buffer
+wav.encode(wavFileInfo.channelData, {});
+// $ExpectError
+wav.encode(wavFileInfo.channelData);
