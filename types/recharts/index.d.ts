3ea7bd4066db79bceeefb085286c840d8f507d92
Dmitriy Serdtsev <in19farkt@gmail.com>
2021-09-02 14:25:32 +0300

    [recharts] add stub definitions for v2

---
+++ types/recharts/index.d.ts
@@ -0,0 +1,31 @@
+// Type definitions for Recharts 2.0
+// Project: http://recharts.org/, https://github.com/recharts/recharts
+// Definitions by: Raphael Mueller <https://github.com/rapmue>
+//                 Roy Xue <https://github.com/royxue>
+//                 Zheyang Song <https://github.com/ZheyangSong>
+//                 Rich Baird <https://github.com/richbai90>
+//                 Dan Torberg <https://github.com/caspeco-dan>
+//                 Peter Keuter <https://github.com/pkeuter>
+//                 Jamie Saunders <https://github.com/jrsaunde>
+//                 Harry Cruse <https://github.com/crusectrl>
+//                 Andrew Palugniok <https://github.com/apalugniok>
+//                 Robert Stigsson <https://github.com/RobertStigsson>
+//                 Kosaku Kurino <https://github.com/kousaku-maron>
+//                 Leon Ng <https://github.com/iflp>
+//                 Dave Vedder <https://github.com/veddermatic>
+//                 Konstantin Azizov <https://github.com/g07cha>
+//                 Gonzalo Nicolas D'Elia <https://github.com/gndelia>
+//                 Dimitri Mitropoulos <https://github.com/dimitropoulos>
+//                 Eliot Ball <https://github.com/eliotball>
+//                 Ville Kentta <https://github.com/vkentta>
+//                 Fabien Caylus <https://github.com/fcaylus>
+//                 Samuel Weckstrom <https://github.com/samuelweckstrom>
+//                 George Cheng <https://github.com/Gerhut>
+//                 Haldun Anil <https://github.com/haldunanil>
+//                 Tobias Knapp <https://github.com/t-knapp>
+//                 Jay Lim <https://github.com/jlxc106>
+// Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
+// TypeScript Version: 2.8
+
+// Stub TypeScript definitions entry for recharts, which provides its own types definitions
+export {};
