b649eb20b8f711f9fedb2d966788237dff69d06a
Jan Litzenburger <jan.litzenburger@sap.com>
2021-09-03 17:35:47 +0200

    removed linter errors

--- types/magicmirror-module/index.d.ts
+++ types/magicmirror-module/index.d.ts
@@ -5,6 +5,36 @@
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
 // TypeScript Version: 3.0
 
-/// <reference path="Module.d.ts" />
-/// <reference path="node_helper.d.ts" />
-/// <reference path="config.d.ts" />
+declare namespace Module {
+    function register<T>(moduleName: string, moduleProperties: ModuleProperties<T>): void;
+ 
+    type ModuleProperties<T> = {
+     config?: T
+     defaults?: T
+     getDom?: () => HTMLElement
+     getHeader?: () => string
+     getStyles?: () => string[]
+     getTemplate?: () => string
+     getTemplateData?: () => object
+     getTranslations?: () => { [key: string]: string }
+     notificationReceived?: (notification: string, payload: any, sender: object) => void
+     resume?: () => void
+     socketNotificationReceived?: (notification: string, payload: any) => void
+     suspend?: () => void
+     [key: string]: any
+   } & ThisType<ModuleProperties<T>>;
+ }
+
+ declare namespace node_helper {
+    function create(object: NodeHelperModule): void;
+ 
+    type NodeHelperModule = {
+     init?(): void
+     start?(): void
+     stop?(): void
+     socketNotificationReceived?(notification: string, payload: any): void
+     sendSocketNotification?(notification: string, payload: any): void
+     [key: string]: any
+   } & ThisType<NodeHelperModule>;
+ }
+ 
