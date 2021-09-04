c8e1d9960d88754e503b7c1848027604447a715e
Jan Litzenburger <jan.litzenburger@sap.com>
2021-09-04 20:28:22 +0200

    added missing properties

--- types/magicmirror-module/index.d.ts
+++ types/magicmirror-module/index.d.ts
@@ -3,27 +3,54 @@
 // Definitions by: Jalibu <https://github.com/jalibu>
 //                 MichMich <https://github.com/MichMich>
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
-// TypeScript Version: 3.7
 
 declare namespace Module {
     /* tslint:disable:no-unnecessary-generics */
     function register<T>(moduleName: string, moduleProperties: ModuleProperties<T>): void;
 
+    class CoreModule<T> {
+        readonly name: string;
+        readonly identifier: string;
+        readonly hidden: boolean;
+        readonly data: { classes: string; file: string; path: string; header: string; position: string };
+        readonly lockStrings: string[];
+        config: T;
+        defaults: T;
+        requiresVersion: string;
+
+        // Subclassable methods
+        init: () => void;
+        loaded: (callback?: () => void) => void;
+        start: () => void;
+        getScripts: () => string[];
+        getStyles: () => string[];
+        getTranslations: () => { [key: string]: string };
+        getTemplate: () => string;
+        getTemplateData: () => any;
+        getDom: () => HTMLElement;
+        getHeader: () => string;
+        notificationReceived: (notification: string, payload: any, sender: ModuleProperties<any>) => void;
+        socketNotificationReceived: (notification: string, payload: any) => void;
+        suspend: () => void;
+        resume: () => void;
+
+        // Instance methods
+        readonly file: (filename: string) => string;
+        readonly updateDom: (speed: number) => void;
+        readonly sendNotification: (notification: string, payload: any) => void;
+        readonly sendSocketNotification: (notification: string, payload: any) => void;
+        readonly hide: NonNullable<(speed?: number, callback?: () => void, options?: { lockString: string }) => void>;
+        readonly show: (
+            speed?: number,
+            callback?: () => void,
+            options?: { lockString?: string; force?: boolean; onError?: () => void },
+        ) => void;
+        readonly translate: (identifier: string, variables?: any) => string;
+    }
+
     type ModuleProperties<T> = {
-        config?: T;
-        defaults?: T;
-        getDom?: () => HTMLElement;
-        getHeader?: () => string;
-        getStyles?: () => string[];
-        getTemplate?: () => string;
-        getTemplateData?: () => object;
-        getTranslations?: () => { [key: string]: string };
-        notificationReceived?: (notification: string, payload: any, sender: object) => void;
-        resume?: () => void;
-        socketNotificationReceived?: (notification: string, payload: any) => void;
-        suspend?: () => void;
         [key: string]: any;
-    } & ThisType<ModuleProperties<T>>;
+    } & ThisType<NonNullable<CoreModule<T>>> & Partial<CoreModule<T>>;
 }
 
 declare namespace node_helper {
--- types/magicmirror-module/magicmirror-module-tests.ts
+++ types/magicmirror-module/magicmirror-module-tests.ts
@@ -2,18 +2,21 @@ Module.register('test', {});
 interface TestConfig {
     bla: number;
     blubb: string;
+    locale?: string;
 }
 
 Module.register<TestConfig>('test', {
     defaults: {
         bla: 1,
         blubb: 'abc',
+        locale: config.locale || 'de',
     },
     config: {
         bla: 1,
         blubb: 'abc',
     },
     getHeader() {
+        this.name;
         return config.address;
     },
     getDom() {
@@ -30,6 +33,49 @@ Module.register<TestConfig>('test', {
     },
     getTranslations() {
         return { de: 'bla', en: 'blubb' };
+    },
+    getScripts() {
+        return ['one', 'two'];
+    },
+    requiresVersion: '1.2.3',
+    resume() {
+        console.log('resume');
+        this.translate('hello');
+        this.translate('hello', ['abc', 'def']);
+        this.config.bla;
+        this.config.blubb;
+        this.config.locale;
+        this.data.path;
+        this.file('abc');
+        this.sendNotification('test', {});
+        this.sendSocketNotification('test', {});
+        this.hide();
+        this.hide(100);
+        this.hide(100, () => {});
+        this.hide(100, () => {}, { lockString: this.identifier });
+        this.show();
+        this.show(100);
+        this.show(100, () => {});
+        this.show(100, () => {}, { lockString: this.identifier, force: true, onError: () => {} });
+    },
+    socketNotificationReceived(notification: string, payload: any) {
+        if (notification === 'test') {
+            console.log(payload);
+        }
+    },
+    notificationReceived(notification: string, payload: any) {
+        if (notification === 'test') {
+            console.log(payload);
+        }
+    },
+    start() {
+        console.log('started');
+    },
+    suspend() {
+        console.log('suspended');
+    },
+    loaded() {
+        console.log('loaded');
     },
 });
 
