a4c6823f1e925ca836f7ffcfe072a0661fa682ab
François Billioud <darksharcoux@hotmail.com>
2021-09-05 11:10:50 +0200

    Fix onPassword definition in react-pdf
    the type definition of this property is wrong
    
    the implementation: https://github.com/wojtekmaj/react-pdf/blob/a129be07b57dae74274f0b82af1e84eb8bbe77fd/src/Document.jsx#L118
    
    comes from: https://mozilla.github.io/pdf.js/api/draft/module-pdfjsLib-PDFDocumentLoadingTask.html

--- types/react-pdf/dist/Document.d.ts
+++ types/react-pdf/dist/Document.d.ts
@@ -90,7 +90,7 @@ export interface Props {
      * Function called when a password-protected PDF is loaded.
      * Defaults to a function that prompts the user for password.
      */
-    onPassword?: ((callback: (...args: any[]) => any) => void) | undefined;
+    onPassword?: ((callback: (...args: any[]) => any) => void, reason: string) | undefined;
 
     /**
      * Function called in case of an error while retrieving document source from `file` prop.
