3fe9ca4ef5bb21462d3ca467ac050fea76f2fc0e
tom-schier <schiertxs@gmail.com>
2021-09-06 19:32:24 +1000

    Renaming of some interfaces, removed redundant ones

--- types/consumer-data-standards/index.d.ts
+++ types/consumer-data-standards/index.d.ts
@@ -1,4 +1,4 @@
-// Type definitions for non-npm package consumer-data-standards-browser 1.0
+// Type definitions for non-npm package consumer-data-standards-browser 1.1
 // Project: https://github.com/ConsumerDataStandardsAustralia
 // Definitions by: CDR-API-Stream <https://github.com/ConsumerDataStandardsAustralia>
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
