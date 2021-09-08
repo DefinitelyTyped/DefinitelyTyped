34937f2ab824355f469a935013fb750cb856c8bd
Gil Michael Regalado <gil.regalado@netzontech.com>
2021-09-08 09:39:19 +0800

    add test case for this pr pattern from main repo - https://github.com/xiaolin/react-image-gallery/blob/bb7d7d03070d92bd5f69e3b07bc617deaab36562/example/app.js\#L31-L45

--- types/react-image-gallery/react-image-gallery-tests.tsx
+++ types/react-image-gallery/react-image-gallery-tests.tsx
@@ -23,15 +23,37 @@ class ImageGallery extends React.Component {
         );
     }
 
+    // condensed version of https://github.com/xiaolin/react-image-gallery/blob/bb7d7d03070d92bd5f69e3b07bc617deaab36562/example/app.js#L129-L166
+    _renderVideo(item: ReactImageGalleryItem) {
+        return <div>{item.embedUrl}</div>;
+    }
+
     render() {
-        const galleryItem: ReactImageGalleryItem = {
-            original: 'http://localhost/logo.jpg',
-            originalTitle: 'My Logo',
-            bulletClass: 'my-bullet-class-name',
-        };
+        // similar pattern to https://github.com/xiaolin/react-image-gallery/blob/bb7d7d03070d92bd5f69e3b07bc617deaab36562/example/app.js#L31-L45
+        const galleryItem: ReactImageGalleryItem[] = [
+            {
+                original: 'http://localhost/logo.jpg',
+                originalTitle: 'My Logo',
+                bulletClass: 'my-bullet-class-name',
+            },
+            {
+                thumbnail: `http://localhost/4v.jpg`,
+                original: `http://localhost/4v.jpg`,
+                embedUrl: 'https://www.youtube.com/embed/4pSzhZ76GdM?autoplay=1&showinfo=0',
+                description: 'Render custom slides (such as videos)',
+                renderItem: this._renderVideo.bind(this),
+            },
+            {
+                original: `http://localhost/1.jpg`,
+                thumbnail: `http://localhost/1t.jpg`,
+                originalClass: 'featured-slide',
+                thumbnailClass: 'featured-thumb',
+                description: 'Custom class for slides & thumbnails',
+            },
+        ];
 
         const props: ReactImageGalleryProps = {
-            items: [galleryItem],
+            items: galleryItem,
             autoPlay: false,
             showFullscreenButton: false,
             renderThumbInner: this.renderThumbInner,
