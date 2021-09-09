60b4780f67570d20facc6fea84e1c471a0173484
lntel <joeharris461767@gmail.com>
2021-09-09 13:34:56 +0100

    Added type tests

---
+++ types/byte-size/byte-size-tests.ts
@@ -0,0 +1,20 @@
+import { byteSize } from 'byte-size';
+
+const toStringFn = () => {};
+
+byteSize(256, {
+    locale: 'en-gb',
+    precision: 1,
+    toStringFn,
+    units: 'iec_octet',
+});
+
+byteSize(12, {
+    locale: 'en-gb',
+    precision: 2.5,
+    toStringFn,
+    units: 'test',
+    customUnits: {
+        test: 'sth',
+    },
+});
