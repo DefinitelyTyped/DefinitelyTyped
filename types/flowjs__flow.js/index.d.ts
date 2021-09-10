f32d14a839f1dd8aebaea0443243b7469d670777
gentoo90 <gentoo90@gmail.com>
2021-09-10 23:17:02 +0300

    Add `@flowjs/flow.js` package

---
+++ types/flowjs__flow.js/index.d.ts
@@ -0,0 +1,32 @@
+// Type definitions for @flowjs/flow.js 2.13
+// Project: https://github.com/flowjs/flow.js
+// Definitions by: gentoo90 <https://github.com/gentoo90>
+// Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
+
+import { Flow } from 'flowjs';
+export {
+    CatchAllCallbackArguments,
+    CompleteCallbackArguments,
+    ErrorCallbackArguments,
+    EventName,
+    FileAddedCallbackArguments,
+    FileErrorCallbackArguments,
+    FileProgressCallbackArguments,
+    FileRemovedCallbackArguments,
+    FileRetryCallbackArguments,
+    FilesAddedCallbackArguments,
+    FilesSubmittedCallbackArguments,
+    FileSuccessCallbackArguments,
+    Flow,
+    FlowChunk,
+    FlowChunkParams,
+    FlowEvent,
+    FlowEventFromEventName,
+    FlowEventMap,
+    FlowEventTypeFromFlowEvent,
+    FlowFile,
+    FlowOptions,
+    ProgressCallbackArguments,
+    UploadStartCallbackArguments,
+} from 'flowjs';
+export default Flow;
