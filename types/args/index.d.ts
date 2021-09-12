b9250fefaecc4782485c2afc809de5caa24b044e
joshuaharry <84721070+joshuaharry@users.noreply.github.com>
2021-09-12 08:52:58 -0500

    Bump version

--- types/args/index.d.ts
+++ types/args/index.d.ts
@@ -1,4 +1,4 @@
-// Type definitions for args 3.0
+// Type definitions for args 5.0
 // Project: https://github.com/leo/args#readme
 // Definitions by: Slessi <https://github.com/Slessi>
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
