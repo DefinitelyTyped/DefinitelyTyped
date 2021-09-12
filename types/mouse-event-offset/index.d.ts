5abde5cabcafb4de2bdce21050931306a5fcea20
joshuaharry <84721070+joshuaharry@users.noreply.github.com>
2021-09-12 15:29:21 -0500

    [mouse-event-offset] Adds second arg

--- types/mouse-event-offset/index.d.ts
+++ types/mouse-event-offset/index.d.ts
@@ -6,6 +6,6 @@
 
 import type { MouseEvent, TouchEvent } from 'react';
 
-declare function offset(e: MouseEvent | TouchEvent): [number, number];
+declare function offset(e: MouseEvent | TouchEvent, element?: HTMLElement): [number, number];
 
 export = offset;
