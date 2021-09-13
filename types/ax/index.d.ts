f37f01fdf29022d7f5bf05988adc1fb28f84db9d
kasumisk <kasumisk@163.com>
2021-09-13 16:30:05 +0800

    feat: test

--- types/ax/index.d.ts
+++ types/ax/index.d.ts
@@ -1,18 +1,19 @@
-// Type definitions for ax 0.0.1
+// Type definitions for ax 0.1
 // Project: https://github.com/AstroxNetwork/ax
 // Definitions by: Hsehoiki <https://github.com/AstroxNetwork>
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
 
 interface AxType {
     /**
+     * ax function
      */
-    setNavigationBarTitle: (text: string) => boolean,
-    setNavigationBarColor: (color: string) => boolean,
+    setNavigationBarTitle: (text: string) => boolean;
+    setNavigationBarColor: (color: string) => boolean;
 }
 
 /**
  * ax is global object
  */
-declare const ax: AxType
+declare const ax: AxType;
 
 export = ax;
