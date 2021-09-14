831a8ba2772d9ecb919b2c2c3d0cb7eb308e6b75
Totto16 <32566573+Totto16@users.noreply.github.com>
2021-09-14 14:47:26 +0200

    [nyaapi] updated definitions

--- types/nyaapi/index.d.ts
+++ types/nyaapi/index.d.ts
@@ -1,6 +1,6 @@
-// Type definitions for nyaapi 2.3
+// Type definitions for nyaapi 2.4
 // Project: https://github.com/Kylart/Nyaapi#readme
-// Definitions by: Christopher Schreiner <https://github.com/infanf>
+// Definitions by: Christopher Schreiner <https://github.com/infanf>, Totto16 <https://github.com/Totto16>
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
 
 export namespace si {
@@ -13,39 +13,39 @@ export namespace si {
     }
 
     enum Category {
-        'ANIME' = '1_0',
-        'ANIME_AMV' = '1_1',
-        'ANIME_ENGLISH' = '1_2',
-        'ANIME_NON_ENGLISH' = '1_3',
-        'ANIME_RAW' = '1_4',
-        'AUDIO' = '2_0',
-        'AUDIO_LOSSLESS' = '2_1',
-        'AUDIO_LOSSY' = '2_2',
-        'LITERATURE' = '3_0',
-        'LITERATURE_ENGLISH' = '3_1',
-        'LITERATURE_NON_ENGLISH' = '3_2',
-        'LITERATURE_RAW' = '3_3',
-        'LIVE_ACTION' = '4_0',
-        'LIVE_ACTION_ENGLISH' = '4_1',
-        'LIVE_ACTION_IDOL_PV' = '4_2',
-        'LIVE_ACTION_NON_ENGLISH' = '4_3',
-        'LIVE_ACTION_RAW' = '4_4',
-        'PICTURES' = '5_0',
-        'PICTURES_GRAPHICS' = '5_1',
-        'PICTURES_PHOTOS' = '5_2',
-        'SOFTWARE' = '6_0',
-        'SOFTWARE_APPS' = '6_1',
-        'SOFTWARE_GAMES' = '6_2',
+        '1_0' = 'ANIME',
+        '1_1' = 'ANIME_AMV',
+        '1_2' = 'ANIME_ENGLISH',
+        '1_3' = 'ANIME_NON_ENGLISH',
+        '1_4' = 'ANIME_RAW',
+        '2_0' = 'AUDIO',
+        '2_1' = 'AUDIO_LOSSLESS',
+        '2_2' = 'AUDIO_LOSSY',
+        '3_0' = 'LITERATURE',
+        '3_1' = 'LITERATURE_ENGLISH',
+        '3_2' = 'LITERATURE_NON_ENGLISH',
+        '3_3' = 'LITERATURE_RAW',
+        '4_0' = 'LIVE_ACTION',
+        '4_1' = 'LIVE_ACTION_ENGLISH',
+        '4_2' = 'LIVE_ACTION_IDOL_PV',
+        '4_3' = 'LIVE_ACTION_NON_ENGLISH',
+        '4_4' = 'LIVE_ACTION_RAW',
+        '5_0' = 'PICTURES',
+        '5_1' = 'PICTURES_GRAPHICS',
+        '5_2' = 'PICTURES_PHOTOS',
+        '6_0' = 'SOFTWARE',
+        '6_1' = 'SOFTWARE_APPS',
+        '6_2' = 'SOFTWARE_GAMES',
     }
 
     interface SearchOptions {
-        term?: string | undefined;
-        n?: number | undefined;
-        category?: Category | undefined;
-        p?: number | undefined;
-        sort?: string | undefined;
-        filter?: Filter | undefined;
-        direction?: 'asc' | 'desc' | undefined;
+        term?: string;
+        n?: number;
+        category?: keyof typeof Category;
+        p?: number;
+        sort?: string;
+        filter?: Filter;
+        direction?: 'asc' | 'desc';
     }
 
     interface SearchOptionsTerm extends SearchOptions {
@@ -67,19 +67,29 @@ export namespace si {
         leechers: string;
     }
 
-    interface UploadData {
-        credentials: string;
-        name: string;
-        category: string;
-        information: string;
-        description: string;
-        anonymous: boolean;
-        hidden: boolean;
-        complete: boolean;
-        remake: boolean;
-        trusted: boolean;
+    interface UploadOptions {
+        credentials: Credentials;
         torrent: string;
+        name: string;
+        category: keyof typeof Category;
+        information?: string;
+        description?: string;
+        anonymous?: boolean;
+        hidden?: boolean;
+        complete?: boolean;
+        remake?: boolean;
+        trusted?: boolean;
     }
+
+    interface Credentials {
+        username: string;
+        password: string;
+    }
+
+    const config: {
+        readonly url: string;
+        updateBaseUrl(url: string): void;
+    };
 
     function search(term: string | SearchOptionsTerm, n?: number, opts?: SearchOptions): Promise<Torrent[]>;
 
@@ -109,7 +119,7 @@ export namespace si {
         opts?: SearchOptions,
     ): Promise<Torrent[]>;
 
-    function upload(opts: UploadData): Promise<Torrent>;
+    function upload(opts: UploadOptions): Promise<Torrent>;
 }
 
 export namespace pantsu {
@@ -127,30 +137,30 @@ export namespace pantsu {
     }
 
     enum Category {
-        'ALL' = '_',
-        'ANIME' = '3_',
-        'ANIME_AMV' = '3_12',
-        'ANIME_ENGLISH' = '3_5',
-        'ANIME_NON_ENGLISH' = '3_13',
-        'ANIME_RAW' = '3_6',
-        'AUDIO' = '2_',
-        'AUDIO_LOSSLESS' = '2_3',
-        'AUDIO_LOSSY' = '2_4',
-        'LITERATURE' = '4_',
-        'LITERATURE_ENGLISH' = '4_7',
-        'LITERATURE_NON_ENGLISH' = '4_14',
-        'LITERATURE_RAW' = '4_8',
-        'LIVE_ACTION' = '5_',
-        'LIVE_ACTION_ENGLISH' = '5_9',
-        'LIVE_ACTION_IDOL_PV' = '5_10',
-        'LIVE_ACTION_NON_ENGLISH' = '5_18',
-        'LIVE_ACTION_RAW' = '5_11',
-        'PICTURES' = '6_',
-        'PICTURES_GRAPHICS' = '6_15',
-        'PICTURES_PHOTOS' = '6_16',
-        'SOFTWARE' = '1_',
-        'SOFTWARE_APPS' = '1_1',
-        'SOFTWARE_GAMES' = '1_2',
+        '_' = 'ALL',
+        '3_' = 'ANIME',
+        '3_12' = 'ANIME_AMV',
+        '3_5' = 'ANIME_ENGLISH',
+        '3_13' = 'ANIME_NON_ENGLISH',
+        '3_6' = 'ANIME_RAW',
+        '2_' = 'AUDIO',
+        '2_3' = 'AUDIO_LOSSLESS',
+        '2_4' = 'AUDIO_LOSSY',
+        '4_' = 'LITERATURE',
+        '4_7' = 'LITERATURE_ENGLISH',
+        '4_14' = 'LITERATURE_NON_ENGLISH',
+        '4_8' = 'LITERATURE_RAW',
+        '5_' = 'LIVE_ACTION',
+        '5_9' = 'LIVE_ACTION_ENGLISH',
+        '5_10' = 'LIVE_ACTION_IDOL_PV',
+        '5_18' = 'LIVE_ACTION_NON_ENGLISH',
+        '5_11' = 'LIVE_ACTION_RAW',
+        '6_' = 'PICTURES',
+        '6_15' = 'PICTURES_GRAPHICS',
+        '6_16' = 'PICTURES_PHOTOS',
+        '1_' = 'SOFTWARE',
+        '1_1' = 'SOFTWARE_APPS',
+        '1_2' = 'SOFTWARE_GAMES',
     }
 
     interface User {
@@ -173,23 +183,23 @@ export namespace pantsu {
     }
 
     interface SearchOptions {
-        term?: string | undefined;
-        n?: number | undefined;
-        c?: Category | undefined;
-        page?: number | undefined;
-        userID?: string | undefined;
-        fromID?: string | undefined;
-        s?: string | undefined;
-        maxage?: string | undefined;
-        toDate?: string | undefined;
-        fromDate?: string | undefined;
-        dateType?: string | undefined;
-        minSize?: string | undefined;
-        maxSize?: string | undefined;
-        sizeType?: 'b' | 'k' | 'm' | 'g' | undefined;
-        sort?: SortType | undefined;
-        order?: boolean | undefined;
-        lang?: string[] | undefined;
+        term?: string;
+        n?: number;
+        c?: Category ;
+        page?: number;
+        userID?: string;
+        fromID?: string ;
+        s?: string;
+        maxage?: string;
+        toDate?: string;
+        fromDate?: string;
+        dateType?: string;
+        minSize?: string;
+        maxSize?: string;
+        sizeType?: 'b' | 'k' | 'm' | 'g';
+        sort?: SortType ;
+        order?: boolean;
+        lang?: string[];
     }
 
     interface SearchOptionsTerm extends SearchOptions {
@@ -227,23 +237,20 @@ export namespace pantsu {
         file_list: string[];
     }
 
-    interface UploadData {
-        username: string;
+    interface UploadOptions {
+        token: string;
         name: string;
-        magnet: string;
-        c: Category;
-        remake: boolean;
-        description: string;
-        status: number;
-        hidden: boolean;
-        website_link: string;
-        languages: string[];
-        torrent: string;
-    }
-
-    interface LoginData {
         username: string;
-        password: string;
+        torrent?: string;
+        magnet?: string;
+        category?: Category;
+        information?: string;
+        description?: string;
+        anonymous?: boolean;
+        hidden?: boolean;
+        complete?: boolean;
+        remake?: boolean;
+        trusted?: boolean;
     }
 
     interface LoginResult {
@@ -263,22 +270,33 @@ export namespace pantsu {
         ok: boolean;
     }
 
-    interface UpdateData extends UploadData {
+    interface UpdateOptions extends UploadOptions {
         id: number;
     }
+
+    interface Credentials {
+            username: string;
+            password: string;
+    }
+
     function search(term: string | SearchOptionsTerm, n?: number, opts?: SearchOptions): Promise<Torrent[]>;
 
     function searchAll(term: string | SearchOptionsTerm, opts?: SearchOptions): Promise<Torrent[]>;
 
     function infoRequest(id: number): Promise<Torrent>;
 
-    function upload(opts: UploadData): Promise<Response<Torrent>>;
+    function upload(opts: UploadOptions): Promise<Response<Torrent>>;
 
-    function update(opts: UpdateData): Promise<Response<Torrent>>;
+    function update(opts: UpdateOptions): Promise<Response<Torrent>>;
 
-    function login(opts: LoginData): Promise<Response<LoginResult>>;
+    function login(opts: Credentials): Promise<Response<LoginResult>>;
 
     function checkUser(id: number): Promise<User[]>;
 
     function checkHeader(id: number): Promise<string>;
+
+    const config: {
+        readonly url: string;
+        updateBaseUrl(url: string): void;
+    };
 }
