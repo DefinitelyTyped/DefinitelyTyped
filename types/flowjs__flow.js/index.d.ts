bb01618f41d4c2ed4f055969ccdf0de760c9a38f
gentoo90 <gentoo90@gmail.com>
2021-09-15 23:03:41 +0300

    [@types/flowjs__flow.js] address NeedsExportEquals lint rule

--- types/flowjs__flow.js/index.d.ts
+++ types/flowjs__flow.js/index.d.ts
@@ -3,29 +3,34 @@
 // Definitions by: gentoo90 <https://github.com/gentoo90>
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
 
-import { Flow } from 'flowjs';
-export {
-    CatchAllCallbackArguments,
-    CompleteCallbackArguments,
-    ErrorCallbackArguments,
-    EventName,
-    FileAddedCallbackArguments,
-    FileErrorCallbackArguments,
-    FileProgressCallbackArguments,
-    FileRemovedCallbackArguments,
-    FileRetryCallbackArguments,
-    FilesAddedCallbackArguments,
-    FilesSubmittedCallbackArguments,
-    FileSuccessCallbackArguments,
-    FlowChunk,
-    FlowChunkParams,
-    FlowEvent,
-    FlowEventFromEventName,
-    FlowEventMap,
-    FlowEventTypeFromFlowEvent,
-    FlowFile,
-    FlowOptions,
-    ProgressCallbackArguments,
-    UploadStartCallbackArguments,
-} from 'flowjs';
-export default Flow;
+import flowjs = require('flowjs');
+
+declare class Flow extends flowjs.Flow {}
+
+declare namespace Flow {
+    type CatchAllCallbackArguments = flowjs.CatchAllCallbackArguments;
+    type CompleteCallbackArguments = flowjs.CompleteCallbackArguments;
+    type ErrorCallbackArguments = flowjs.ErrorCallbackArguments;
+    type EventName = flowjs.EventName;
+    type FileAddedCallbackArguments = flowjs.FileAddedCallbackArguments;
+    type FileErrorCallbackArguments = flowjs.FileErrorCallbackArguments;
+    type FileProgressCallbackArguments = flowjs.FileProgressCallbackArguments;
+    type FileRemovedCallbackArguments = flowjs.FileRemovedCallbackArguments;
+    type FileRetryCallbackArguments = flowjs.FileRetryCallbackArguments;
+    type FilesAddedCallbackArguments = flowjs.FilesAddedCallbackArguments;
+    type FilesSubmittedCallbackArguments = flowjs.FilesSubmittedCallbackArguments;
+    type FileSuccessCallbackArguments = flowjs.FileSuccessCallbackArguments;
+    type FlowChunk = flowjs.FlowChunk;
+    type FlowChunkParams = flowjs.FlowChunkParams;
+    type FlowEvent = flowjs.FlowEvent;
+    type FlowEventFromEventName<T extends EventName> = flowjs.FlowEventFromEventName<T>;
+    type FlowEventMap = flowjs.FlowEventMap;
+    type FlowEventTypeFromFlowEvent<T extends FlowEvent> = flowjs.FlowEventTypeFromFlowEvent<T>;
+    type FlowFile = flowjs.FlowFile;
+    type FlowOptions = flowjs.FlowOptions;
+    type ProgressCallbackArguments = flowjs.ProgressCallbackArguments;
+    type UploadStartCallbackArguments = flowjs.UploadStartCallbackArguments;
+}
+
+export = Flow;
+export as namespace Flow;
