e0d2fbe8672728c750ce4f556fe6aee34c5b4d22
Matthew Vernacchia <mvernacc@mit.edu>
2021-09-16 09:50:36 -0400

    Remove typescript version comments.

--- types/ml-levenberg-marquardt/index.d.ts
+++ types/ml-levenberg-marquardt/index.d.ts
@@ -2,7 +2,6 @@
 // Project: https://github.com/mljs/levenberg-marquardt#readme
 // Definitions by: m93a <https://github.com/m93a>
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
-// TypeScript Version: 2.1
 
 declare namespace LM {
     /**
