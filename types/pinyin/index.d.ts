a72a31a2036030c3743f4807d9b5077402cef765
joshuaharry <84721070+joshuaharry@users.noreply.github.com>
2021-09-20 09:12:30 -0500

    Version

--- types/pinyin/index.d.ts
+++ types/pinyin/index.d.ts
@@ -1,4 +1,4 @@
-// Type definitions for pinyin 2.8
+// Type definitions for pinyin 2.10
 // Project: https://github.com/hotoo/pinyin
 // Definitions by: AnJun Wang <https://github.com/wanganjun>
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
