d247fefbf448108c224b68e21575c8d0a20a5675
joshuaharry <84721070+joshuaharry@users.noreply.github.com>
2021-09-20 09:10:00 -0500

    Version

--- types/connect-livereload/index.d.ts
+++ types/connect-livereload/index.d.ts
@@ -1,4 +1,4 @@
-// Type definitions for connect-livereload v0.5.3
+// Type definitions for connect-livereload 0.6
 // Project: https://github.com/intesso/connect-livereload
 // Definitions by: Maxime LUCE <https://github.com/SomaticIT>
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
