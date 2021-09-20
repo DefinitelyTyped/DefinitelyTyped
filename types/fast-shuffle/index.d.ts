5072b93c8bf2e50391bcca1308a80bd9c08acb15
Dusan Djordjevic <dusan.djordjevic@MRSC02PF0S3G3QN.local>
2021-09-20 14:00:09 +0200

    Updating type definitions to match fast-shuffle 4.5

--- types/fast-shuffle/index.d.ts
+++ types/fast-shuffle/index.d.ts
@@ -1,6 +1,7 @@
-// Type definitions for fast-shuffle 1.0
+// Type definitions for fast-shuffle 4.5
 // Project: https://github.com/philihp/fast-shuffle
 // Definitions by: Piotr Roszatycki <https://github.com/dex4er>
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
 
-export default function shuffle<T>(deck: ReadonlyArray<T>, random?: () => number): T[];
+export function shuffle<T>(deck: ReadonlyArray<T>): T[];
+export default function _default<T>(randomSeed: number, deck: ReadonlyArray<T>): T[];
