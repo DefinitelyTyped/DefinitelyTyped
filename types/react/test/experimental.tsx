dc4f8f014158efb2eed460578e2c2e2af49b026f
Sebastian Silbermann <silbermann.sebastian@gmail.com>
2021-09-21 19:42:43 +0200

    feat(react): Add types for `useSyncExternalStore`  (#55701)
    * npx dts-gen --dt --name use-sync-external-store --template module
    
    * Implement types
    
    * Port to `react`
    
    * uSES is currently only exported from the experimental channel with an unstable_ prefix
    
    * Update types/use-sync-external-store/index.d.ts
    
    * Add types for getSeverSnapshot

--- types/react/test/experimental.tsx
+++ types/react/test/experimental.tsx
@@ -1,3 +1,43 @@
 /// <reference types="../experimental"/>
 
 import React = require('react');
+
+const { unstable_useSyncExternalStore: useSyncExternalStore } = React;
+// keep in sync with `use-sync-external-store-tests.ts`
+interface Store<State> {
+    getState(): State;
+    getServerState(): State;
+    subscribe(onStoreChange: () => void): () => void;
+}
+
+declare const numberStore: Store<number>;
+function useVersion(): number {
+    return useSyncExternalStore(numberStore.subscribe, numberStore.getState);
+}
+
+function useStoreWrong() {
+    useSyncExternalStore(
+        // no unsubscribe returned
+        // $ExpectError
+        () => {
+            return null;
+        },
+        () => 1,
+    );
+
+    // `string` is not assignable to `number`
+    // $ExpectError
+    const version: number = useSyncExternalStore(
+        () => () => {},
+        () => '1',
+    );
+}
+
+declare const objectStore: Store<{ version: { major: number; minor: number }; users: string[] }>;
+function useUsers(): string[] {
+    return useSyncExternalStore(
+        objectStore.subscribe,
+        () => objectStore.getState().users,
+        () => objectStore.getServerState().users,
+    );
+}
