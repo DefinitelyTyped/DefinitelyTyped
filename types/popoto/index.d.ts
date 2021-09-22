2f265ebf514a8d7ffe68851bf8dd962781b1d615
Omar Almalol <omar.alma3lol@gmail.com>
2021-09-22 16:33:13 -0400

    first commit to popoto package typing

---
+++ types/popoto/index.d.ts
@@ -0,0 +1,11 @@
+// Type definitions for popoto 3.0
+// Project: https://popotojs.com/
+// Definitions by: My Self <https://github.com/me>
+// Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
+
+/*~ If there are types, properties, or methods inside dotted names
+ *~ of the module, declare them inside a 'namespace'.
+ */
+export const Popoto = {
+    //
+}
