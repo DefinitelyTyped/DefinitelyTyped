12a8bf90e4ef170512f44a78ecb4ea5686bd37e1
Dasa Paddock <dpaddock@esri.com>
2021-09-22 15:02:15 -0700

    Update for ArcGIS API for JavaScript version 4.21

--- types/arcgis-js-api/arcgis-js-api-tests.ts
+++ types/arcgis-js-api/arcgis-js-api-tests.ts
@@ -34,3 +34,6 @@ import esriId = require("esri/identity/IdentityManager");
 
 esriId.enablePostMessageAuth();
 esriId.disablePostMessageAuth();
+
+import externalRenderers = require("esri/views/3d/externalRenderers");
+externalRenderers.forceWebGLContext(2);
