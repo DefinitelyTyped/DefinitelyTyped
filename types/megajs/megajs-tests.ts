a7b7aa9c17930b1ef0ffb6c73dd93606d5af0c8a
ChampionBuffalo <49675578+ChampionBuffalo1@users.noreply.github.com>
2021-09-22 23:15:25 +0530

    [megajs] Added event typings.

--- types/megajs/megajs-tests.ts
+++ types/megajs/megajs-tests.ts
@@ -1,9 +1,33 @@
+import fs from 'fs';
 import mega from 'megajs';
 
 // $ExpectType Storage
-mega({email: 'test@test.net', password: '1234'});
+mega({ email: 'test@test.net', password: '1234' });
 
 // tslint:disable-next-line no-duplicate-imports
 import { Storage } from 'megajs';
 
-const storage = new Storage({email: 'test@test.net', password: '1234'});
+const storage = new Storage({ email: 'test@test.net', password: '1234' });
+// $ExpectType MutableFile
+const folder = storage.root.children[0] // the first file in the root of the storage
+
+fs.createReadStream('myfile.txt').pipe(folder.upload('myfile.txt'))
+
+folder.upload('myfile.txt', 'Hello world!', (error, uploadedFile) => {});
+storage
+    .on('add', file => {
+        // File Added
+        console.log(file.name);
+    })
+    .on('move', (file, olddir) => {
+        // File moved
+        console.log(file.children, olddir.children);
+    })
+    .on('delete', file => {
+        // File Deleted
+        console.log(file.name);
+    })
+    .on('update', file => {
+        // File updated
+        console.log(file.name);
+    });
