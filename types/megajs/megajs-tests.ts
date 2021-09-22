8b02a9092621e2cbc5b7d1831f930b34b869d364
ChampionBuffalo <49675578+ChampionBuffalo1@users.noreply.github.com>
2021-09-22 23:22:11 +0530

    Update megajs-tests.ts

--- types/megajs/megajs-tests.ts
+++ types/megajs/megajs-tests.ts
@@ -1,4 +1,3 @@
-import fs from 'fs';
 import mega from 'megajs';
 
 // $ExpectType Storage
@@ -11,16 +10,15 @@ const storage = new Storage({ email: 'test@test.net', password: '1234' });
 // $ExpectType MutableFile
 const folder = storage.root.children[0] // the first file in the root of the storage
 
-fs.createReadStream('myfile.txt').pipe(folder.upload('myfile.txt'))
-
 folder.upload('myfile.txt', 'Hello world!', (error, uploadedFile) => {});
+
 storage
     .on('add', file => {
         // File Added
         console.log(file.name);
     })
     .on('move', (file, olddir) => {
-        // File moved
+        // File moved to a different directory
         console.log(file.children, olddir.children);
     })
     .on('delete', file => {
