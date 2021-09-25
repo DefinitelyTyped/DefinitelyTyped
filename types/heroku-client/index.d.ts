100304c81284d490b69900cc9c035bf6489a429e
Oleg Valter <o.a.philippov@gmail.com>
2021-09-25 21:49:21 +0300

    [heroku-client] added main module

---
+++ types/heroku-client/index.d.ts
@@ -0,0 +1,29 @@
+// Type definitions for heroku-client 3.1
+// Project: https://github.com/heroku/node-heroku-client
+// Definitions by: Oleg Valter <https://github.com/Oaphi>
+// Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
+// Minimum TypeScript Version: 3.8
+
+import Request, { RequestOptions } from './request';
+
+export type HerokuClientOptions = RequestOptions;
+
+declare class Heroku {
+    options: HerokuClientOptions;
+
+    constructor(options: HerokuClientOptions);
+
+    request(options: HerokuClientOptions): ReturnType<Request['request']>;
+
+    get(path: string, options?: HerokuClientOptions): ReturnType<Request['request']>;
+
+    put(path: string, options?: HerokuClientOptions): ReturnType<Request['request']>;
+
+    post(path: string, options?: HerokuClientOptions): ReturnType<Request['request']>;
+
+    patch(path: string, options?: HerokuClientOptions): ReturnType<Request['request']>;
+
+    delete(path: string, options?: HerokuClientOptions): ReturnType<Request['request']>;
+}
+
+export default Heroku;
