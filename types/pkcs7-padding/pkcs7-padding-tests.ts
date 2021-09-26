3616599beb5f0a130fce1bfd5945c68fd5ba56e8
joshuaharry <84721070+joshuaharry@users.noreply.github.com>
2021-09-26 15:47:49 -0500

    Makes size optional

--- types/pkcs7-padding/pkcs7-padding-tests.ts
+++ types/pkcs7-padding/pkcs7-padding-tests.ts
@@ -4,4 +4,5 @@ const paddedValue: string = pkcs7.pad('string to pad', 32);
 const paddedBufferValue: Buffer = pkcs7.pad(Buffer.from('string to pad'), 32);
 
 const unPaddedValue: string = pkcs7.pad(paddedValue, 32);
+const anotherValue: string = pkcs7.pad(paddedValue);
 const unPaddedBufferValue: Buffer = pkcs7.pad(paddedBufferValue, 32);
