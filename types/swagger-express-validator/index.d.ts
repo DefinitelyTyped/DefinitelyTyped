538c1cc6d4dbab074661011112193ac74e771628
joshuaharry <84721070+joshuaharry@users.noreply.github.com>
2021-09-26 16:05:03 -0500

    Version bump

--- types/swagger-express-validator/index.d.ts
+++ types/swagger-express-validator/index.d.ts
@@ -1,4 +1,4 @@
-// Type definitions for swagger-express-validator 0.0
+// Type definitions for swagger-express-validator 1.0
 // Project: https://github.com/gargol/swagger-express-validator
 // Definitions by: Pinguet62 <https://github.com/pinguet62>
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
