58f8355b83cf47b90bc05f0732156a6f972b1a7e
joshuaharry <84721070+joshuaharry@users.noreply.github.com>
2021-09-26 16:21:07 -0500

    Fix bug

--- types/nprogress/index.d.ts
+++ types/nprogress/index.d.ts
@@ -4,7 +4,7 @@
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
 // TypeScript Version: 2.1
 
-import JQuery from 'jquery';
+import JQuery = require('jquery');
 
 declare namespace nProgress {
     interface NProgressOptions {
