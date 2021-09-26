9d995794331c65c8cc56c5770063769ff92dbff2
Zane Hitchcox <zwhitchcox@gmail.com>
2021-09-26 19:41:04 -0400

    Allow `all` option in net.lookup

--- types/node/net.d.ts
+++ types/node/net.d.ts
@@ -16,7 +16,7 @@ declare module 'net' {
     import * as stream from 'node:stream';
     import { Abortable, EventEmitter } from 'node:events';
     import * as dns from 'node:dns';
-    type LookupFunction = (hostname: string, options: dns.LookupOneOptions, callback: (err: NodeJS.ErrnoException | null, address: string, family: number) => void) => void;
+    type LookupFunction = (hostname: string, options: dns.LookupOptions, callback: (err: NodeJS.ErrnoException | null, address: string, family: number) => void) => void;
     interface AddressInfo {
         address: string;
         family: string;
