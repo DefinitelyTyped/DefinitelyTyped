cbeaf3c4f50ee437b5e888c901019073c6a5ba61
joshuaharry <84721070+joshuaharry@users.noreply.github.com>
2021-09-26 16:03:30 -0500

    Version bump

--- types/fined/index.d.ts
+++ types/fined/index.d.ts
@@ -1,4 +1,4 @@
-// Type definitions for fined 1.1
+// Type definitions for fined 1.2
 // Project: https://github.com/gulpjs/fined
 // Definitions by: BendingBender <https://github.com/BendingBender>
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
