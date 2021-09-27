a75c35c277ed09ea962cbf743bdc384f3752295d
SNDST00M: M.U.N.I.N <82655227+SNDST00M@users.noreply.github.com>
2021-09-27 09:12:57 +0100

    Revert bump and use reference directive

--- types/jquery-mousewheel/index.d.ts
+++ types/jquery-mousewheel/index.d.ts
@@ -1,4 +1,4 @@
-// Type definitions for jquery-mousewheel v3.1.10
+// Type definitions for jquery-mousewheel v3.1.13
 // Project: https://github.com/jquery/jquery-mousewheel
 // Definitions by: Brian Surowiec <https://github.com/xt0rted>
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
