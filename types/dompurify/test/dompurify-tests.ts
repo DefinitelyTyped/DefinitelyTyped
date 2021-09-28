09a408510c95b3f3f5ecd2ee2a9f5a58dd705f4b
Piotr Błażejewicz <peterblazejewicz@users.noreply.github.com>
2021-09-28 23:06:31 +0200

    fix(dompurify): missing configuration option
    - `ADD_URI_SAFE_ATTR`
    https://github.com/cure53/DOMPurify/blob/f7fa358c3c4747993fcf3977915639cd32fd8aab/src/purify.js#L375
    - sort options in config for better devex
    - sort and cleanup tests grouping by similar area and outcome, testing
      with TSLint expectations
    
    Thanks!
    
    /cc @nicroto
    
    Fixes #56019

--- types/dompurify/test/dompurify-tests.ts
+++ types/dompurify/test/dompurify-tests.ts
@@ -1,59 +1,42 @@
 import DOMPurify = require('dompurify');
 
-DOMPurify.sanitize('<script>alert("hi")</script>'); // $ExpectType string
 DOMPurify.addHook('beforeSanitizeElements', (el, data, config) => undefined);
 
 // examples from the DOMPurify README
 const dirty = '<script>alert("hi")</script><p>Totally safe<p><p onerror="blowUp()">Totally not safe</p>';
-let str: string;
-let elem: HTMLElement;
 let frag: DocumentFragment;
 let trustedHtml: TrustedHTML;
-
-// allow only <b>
-str = DOMPurify.sanitize(dirty, { ALLOWED_TAGS: ['b'] });
-
-// allow only <b> and <q> with style attributes (for whatever reason)
-str = DOMPurify.sanitize(dirty, { ALLOWED_TAGS: ['b', 'q'], ALLOWED_ATTR: ['style'] });
 
 DOMPurify.sanitize('<div><b>preserve me</b></div><p><b>no not preserve me</b></p>', {
     FORBID_CONTENTS: ['p'],
     FORBID_TAGS: ['div', 'p'],
 });
 
-// leave all as it is but forbid <style>
-str = DOMPurify.sanitize(dirty, { FORBID_TAGS: ['style'] });
+DOMPurify.sanitize('<script>alert("hi")</script>'); // $ExpectType string
+DOMPurify.sanitize(dirty, { ADD_ATTR: ['my-attr'] }); // $ExpectType string
+DOMPurify.sanitize(dirty, { ADD_DATA_URI_TAGS: ['a', 'area'] }); // $ExpectType string
+DOMPurify.sanitize(dirty, { ADD_TAGS: ['my-tag'] }); // $ExpectType string
+DOMPurify.sanitize(dirty, { ADD_URI_SAFE_ATTR: ['my-attr'] }); // $ExpectType string
+DOMPurify.sanitize(dirty, { ALLOW_DATA_ATTR: false }); // $ExpectType string
+DOMPurify.sanitize(dirty, { ALLOWED_TAGS: ['b', 'q'], ALLOWED_ATTR: ['style'] }); // $ExpectType string
+DOMPurify.sanitize(dirty, { ALLOWED_TAGS: ['b'] }); // $ExpectType string
+DOMPurify.sanitize(dirty, { FORBID_ATTR: ['style'] }); // $ExpectType string
+DOMPurify.sanitize(dirty, { FORBID_TAGS: ['style'] }); // $ExpectType string
+DOMPurify.sanitize(dirty, { KEEP_CONTENT: false }); // $ExpectType string
+DOMPurify.sanitize(dirty, { NAMESPACE: 'http://www.w3.org/2000/svg' }); // $ExpectType string
+DOMPurify.sanitize(dirty, { PARSER_MEDIA_TYPE: 'text/html' }); // $ExpectType string
+DOMPurify.sanitize(dirty, { RETURN_DOM: false }); // $ExpectType string
+DOMPurify.sanitize(dirty, { SANITIZE_DOM: false }); // $ExpectType string
+DOMPurify.sanitize(dirty, { WHOLE_DOCUMENT: true }); // $ExpectType string
 
-// leave all as it is but forbid style attributes
-str = DOMPurify.sanitize(dirty, { FORBID_ATTR: ['style'] });
-
-// extend the existing array of allowed tags
-str = DOMPurify.sanitize(dirty, { ADD_TAGS: ['my-tag'] });
-
-// extend the existing array of attributes
-str = DOMPurify.sanitize(dirty, { ADD_ATTR: ['my-attr'] });
-
-// extend the existing array of tags that can use Data URIs
-
-str = DOMPurify.sanitize(dirty, { ADD_DATA_URI_TAGS: ['a', 'area'] });
-
-// prohibit HTML5 data attributes (default is true)
-str = DOMPurify.sanitize(dirty, { ALLOW_DATA_ATTR: false });
-
-str = DOMPurify.sanitize(dirty, { NAMESPACE: 'http://www.w3.org/2000/svg' });
-
-str = DOMPurify.sanitize(dirty, { PARSER_MEDIA_TYPE: 'text/html' });
-
-// return a DOM HTMLBodyElement instead of an HTML string (default is false)
-str = DOMPurify.sanitize(dirty, { RETURN_DOM: false });
-elem = DOMPurify.sanitize(dirty, { RETURN_DOM: true });
-elem = DOMPurify.sanitize(dirty, { RETURN_DOM: true, RETURN_DOM_FRAGMENT: false });
+DOMPurify.sanitize(dirty, { RETURN_DOM: true }); // $ExpectType HTMLElement
+DOMPurify.sanitize(dirty, { RETURN_DOM: true, RETURN_DOM_FRAGMENT: false }); // $ExpectType HTMLElement
 
 // return a DOM DocumentFragment instead of an HTML string (default is false)
-str = DOMPurify.sanitize(dirty, { RETURN_DOM_FRAGMENT: false });
-frag = DOMPurify.sanitize(dirty, { RETURN_DOM_FRAGMENT: true });
-frag = DOMPurify.sanitize(dirty, { RETURN_DOM_FRAGMENT: true, RETURN_DOM: false });
-frag = DOMPurify.sanitize(dirty, { RETURN_DOM_FRAGMENT: true, RETURN_DOM: true });
+DOMPurify.sanitize(dirty, { RETURN_DOM_FRAGMENT: false }); // $ExpectType string
+DOMPurify.sanitize(dirty, { RETURN_DOM_FRAGMENT: true }); // $ExpectType DocumentFragment
+DOMPurify.sanitize(dirty, { RETURN_DOM_FRAGMENT: true, RETURN_DOM: false }); // $ExpectType DocumentFragment
+DOMPurify.sanitize(dirty, { RETURN_DOM_FRAGMENT: true, RETURN_DOM: true }); // $ExpectType DocumentFragment
 
 // return a DOM DocumentFragment instead of an HTML string (default is false)
 // also import it into the current document (default is false).
@@ -63,15 +46,6 @@ frag = DOMPurify.sanitize(dirty, { RETURN_DOM_FRAGMENT: true, RETURN_DOM_IMPORT:
 
 // return a TrustHTML type instead of a HTML string
 trustedHtml = DOMPurify.sanitize(dirty, { RETURN_TRUSTED_TYPE: true });
-
-// return entire document including <html> tags (default is false)
-str = DOMPurify.sanitize(dirty, { WHOLE_DOCUMENT: true });
-
-// disable DOM Clobbering protection on output (default is true, handle with care!)
-str = DOMPurify.sanitize(dirty, { SANITIZE_DOM: false });
-
-// discard an element's content when the element is removed (default is true)
-str = DOMPurify.sanitize(dirty, { KEEP_CONTENT: false });
 
 // test createDOMPurify factory that accepts custom window (very helpful for nodejs)
 const createDOMPurify = DOMPurify;
