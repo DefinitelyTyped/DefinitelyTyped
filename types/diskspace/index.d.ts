bcc6cd064cb46254790528f5188e0faa21b35309
Piotr Błażejewicz (Peter Blazejewicz) <peterblazejewicz@users.noreply.github.com>
2021-09-29 14:53:29 +0200

    Apply suggestions from code review

--- types/diskspace/index.d.ts
+++ types/diskspace/index.d.ts
@@ -3,57 +3,56 @@
 // Definitions by: Vladislav <https://github.com/swedesjs>
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
 
-declare module "diskspace" {
-    /**
-     * @param err RU: Ошибка при выполнении  EN: Runtime error
-     * @param result RU: Результат проверки дискового пространства   EN: The result of the diskspace check
-     */
-    type Callback = (err: Error | null, result: Result) => void;
+/**
+ * @param err RU: Ошибка при выполнении  EN: Runtime error
+ * @param result RU: Результат проверки дискового пространства   EN: The result of the diskspace check
+ */
+type Callback = (err: Error | null, result: Result) => void;
 
-    type Result = {
-        /**
-         * RU: Это то, сколько диск имеет полностью.
-         *
-         * EN: Is how much the drive has totally.
-         */
-        total: string;
-        /**
-         * RU:
-         *
-         * Это указанная часть диска как используемая.
-         * На *nix это прямо из команды, в Windows это вычисляется из `df -kresult.total - result.free`
-         *
-         * EN:
-         *
-         * Is how much of the drive is reported as used.
-         * On *nix this is straight from the command, on Windows it's calculated from `df -kresult.total - result.free`
-         */
-        used: number;
-        /**
-         * RU: Это то, сколько свободного места у вас есть.
-         *
-         * EN: Is how much free space you have.
-         */
-        free: string;
-        /**
-         * RU:
-         *
-         * На самом деле не так полезен, если вы не хотите отлаживать.
-         * * NOTFOUND - Диск не найден, значения пространства будут 0
-         * * READY - Диск готов
-         * * NOTREADY - Диск не готов, значения пространства будут 0
-         * * STDERR - некоторая ошибка, вывод ее был записан на консоль.
-         *
-         * EN:
-         *
-         * Isn't really that useful unless you want to debug.
-         * * NOTFOUND - Disk was not found, the space values will be 0
-         * * READY - The drive is ready
-         * * NOTREADY - The drive isn't ready, the space values will be 0
-         * * STDERR - some error, the output of it was logged to the console.
-         */
+interface Result {
+    /**
+     * RU: Это то, сколько диск имеет полностью.
+     *
+     * EN: Is how much the drive has totally.
+     */
+    total: string;
+    /**
+     * RU:
+     *
+     * Это указанная часть диска как используемая.
+     * На *nix это прямо из команды, в Windows это вычисляется из `df -kresult.total - result.free`
+     *
+     * EN:
+     *
+     * Is how much of the drive is reported as used.
+     * On *nix this is straight from the command, on Windows it's calculated from `df -kresult.total - result.free`
+     */
+    used: number;
+    /**
+     * RU: Это то, сколько свободного места у вас есть.
+     *
+     * EN: Is how much free space you have.
+     */
+    free: string;
+    /**
+     * RU:
+     *
+     * На самом деле не так полезен, если вы не хотите отлаживать.
+     * * NOTFOUND - Диск не найден, значения пространства будут 0
+     * * READY - Диск готов
+     * * NOTREADY - Диск не готов, значения пространства будут 0
+     * * STDERR - некоторая ошибка, вывод ее был записан на консоль.
+     *
+     * EN:
+     *
+     * Isn't really that useful unless you want to debug.
+     * * NOTFOUND - Disk was not found, the space values will be 0
+     * * READY - The drive is ready
+     * * NOTREADY - The drive isn't ready, the space values will be 0
+     * * STDERR - some error, the output of it was logged to the console.
+     */
         status: "NOTFOUND" | "READY" | "NOTREADY" | "STDERR";
-    };
+}
 
     namespace diskspace {
         /**
