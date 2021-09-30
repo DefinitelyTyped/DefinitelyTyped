4afa40020d9bba8f187d6607335d3075e6634180
Andrew <vanillkay@gmail.com>
2021-09-30 17:35:42 +0300

    Add types for react-star-ratings

---
+++ types/react-star-ratings/README.md
@@ -0,0 +1,5 @@
+# Installation
+> `npm install --save @types/react-star-ratings`
+
+# Summary
+This package contains type definitions for react-star-ratings (https://github.com/ekeric13/react-star-ratings).
---
+++ types/react-star-ratings/index.d.ts
@@ -0,0 +1,20 @@
+import * as React from "react";
+
+interface StarRatingProps {
+    rating?: number;
+    numberOfStars?: number;
+    changeRating?: (rating: number) => void;
+    starRatedColor?: string;
+    starEmptyColor?: string;
+    starHoverColor?: string;
+    starDimension?: string;
+    starSpacing?: string;
+    gradientPathName?: string;
+    ignoreInlineStyles?: boolean;
+    svgIconPath?: string;
+    svgIconViewBox?: string;
+    name?: string;
+  }
+
+  export default class StarRatings extends React.Component<StarRatingProps> {}
+
