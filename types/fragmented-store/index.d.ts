66a8e63441b064dc2cf0297979d03435d380fa69
Lunuy <luminuy@gmail.com>
2021-09-30 12:20:11 +0900

    fragmented-store

---
+++ types/fragmented-store/index.d.ts
@@ -0,0 +1,23 @@
+// Type definitions for fragmented-store 0.2
+// Project: https://github.com/aralroca/fragmented-store
+// Definitions by: Lunuy <https://github.com/lunuy>
+// Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
+// Minimum TypeScript Version: 4.1
+
+import {
+    Dispatch,
+    SetStateAction,
+    ReactNode,
+    ExoticComponent
+} from 'react';
+
+export type StateHook<S> = () => [S, Dispatch<SetStateAction<S>>];
+
+export type StoreUtils<Store extends { [K: string]: any }> = Omit<{
+    [K in keyof Store as `use${Capitalize<string & K>}`]: StateHook<Store[K]>
+}, 'useStore'> & {
+    Provider: ExoticComponent<{ children: ReactNode }>,
+    useStore: StateHook<Store>
+};
+
+export default function createStore<Store extends { [K: string]: any }>(store: Store): StoreUtils<Store>;
