4226e09b8478f9d4f6b283cd6ecf526c50a554ed
smac89 <8305511+smac89@users.noreply.github.com>
2021-09-30 15:11:51 -0600

    Add files via upload
    Create files for react-animals

---
+++ types/react-animals/tests/react-animals-tests.tsx
@@ -0,0 +1,16 @@
+/// <reference types="react" />
+
+import Animal from 'react-animals';
+
+const MyComponent: React.FC = () => (
+    <div
+        style={{
+            borderRadius: '1rem',
+            padding: '1rem',
+        }}
+    >
+        <Animal name="badger" size={5} circle dance />
+    </div>
+);
+
+export default MyComponent;
