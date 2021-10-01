c7618956d6e89c7b77e94145b702e7794a942e1b
Alice Pote <alice.writes.wrongs@gmail.com>
2021-09-30 20:20:06 -0400

    add tests which cover the whole type?

--- types/url-assembler/url-assembler-tests.ts
+++ types/url-assembler/url-assembler-tests.ts
@@ -7,7 +7,54 @@ function printUrl(u: UrlAssembler) {
 }
 
 function checkQSConfig(u: UrlAssembler) {
-    return u.qsConfig({
+    u.qsConfig({
+        delimiter: "#",
+        strictNullHandling: true,
+        skipNulls: true,
+        encode: true,
+        filter: [1, 2, 3],
+        arrayFormat: 'indices',
+        indices: true,
         sort: (a: string, b: string) => a.localeCompare(b),
+        serializeDate: (d: Date) => d.toString(),
+        format: 'RFC1738',
+        encodeValuesOnly: true,
+        addQueryPrefix: true,
+        allowDots: true,
+        charset: 'utf-8',
+        charsetSentinel: true,
+    });
+
+    u.qsConfig({
+        // $ExpectError
+        delimiter: true
+        // $ExpectError
+        strictNullHandling: "boop",
+        // $ExpectError
+        skipNulls: "boop",
+        // $ExpectError
+        encode: "encode",
+        // $ExpectError
+        filter: true,
+        // $ExpectError
+        arrayFormat: 'uncool',
+        // $ExpectError
+        indices: "true",
+        // $ExpectError
+        sort: false
+        // $ExpectError
+        serializeDate: (d: Date) => d.toString(),
+        // $ExpectError
+        format: 'RFC1111',
+        // $ExpectError
+        encodeValuesOnly: "values",
+        // $ExpectError
+        addQueryPrefix: "yes please",
+        // $ExpectError
+        allowDots: "no dots",
+        // $ExpectError
+        charset: 'UtF-eight',
+        // $ExpectError
+        charsetSentinel: "sentinel?",
     });
 }
