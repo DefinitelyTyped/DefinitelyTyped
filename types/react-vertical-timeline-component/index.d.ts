d71a3a9d05e4415576d8a3d11ca005410b1ff4bf
ferreirix <ferreira.s.joao@gmail.com>
2021-10-01 15:55:16 +0200

    bump supported version

--- types/react-vertical-timeline-component/index.d.ts
+++ types/react-vertical-timeline-component/index.d.ts
@@ -1,4 +1,4 @@
-// Type definitions for react-vertical-timeline-component 3.0
+// Type definitions for react-vertical-timeline-component 3.3
 // Project: https://github.com/stephane-monnot/react-vertical-timeline, https://stephane-monnot.github.io/react-vertical-timeline
 // Definitions by: Stéphane Monnot <https://github.com/stephane-monnot>
 //                 Lukas Elmer <https://github.com/lukaselmer>
