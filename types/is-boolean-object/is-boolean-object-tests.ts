001eb3e1756353c41fafd90580ad4405b214be1c
Aaron Leopold <36278431+aaronleopold@users.noreply.github.com>
2021-10-02 15:35:54 -0700

    Update types/is-boolean-object/is-boolean-object-tests.ts
    Co-authored-by: Piotr Błażejewicz (Peter Blazejewicz) <peterblazejewicz@users.noreply.github.com>

--- types/is-boolean-object/is-boolean-object-tests.ts
+++ types/is-boolean-object/is-boolean-object-tests.ts
@@ -1,15 +1,15 @@
 import isBoolean = require('is-boolean-object');
 
-isBoolean(Boolean(42)); // $Expects true
-isBoolean(false); // $Expects true
-isBoolean(true); // $Expects true
+isBoolean(Boolean(42)); // $ExpectType boolean
+isBoolean(false); // $ExpectType boolean
+isBoolean(true); // $ExpectType boolean
 
-isBoolean(undefined); // $Expects false
-isBoolean(null); // $Expects false
-isBoolean([]); // $Expects false
-isBoolean({}); // $Expects false
-isBoolean(/a/g); // $Expects false
-isBoolean(new Date()); // $Expects false
-isBoolean(42); // $Expects false
-isBoolean(NaN); // $Expects false
-isBoolean(Infinity); // $Expects false
+isBoolean(undefined); // $ExpectType boolean
+isBoolean(null); // $ExpectType boolean
+isBoolean([]); // $ExpectType boolean
+isBoolean({}); // $ExpectType boolean
+isBoolean(/a/g); // $ExpectType boolean
+isBoolean(new Date()); // $ExpectType boolean
+isBoolean(42); // $ExpectType boolean
+isBoolean(NaN); // $ExpectType boolean
+isBoolean(Infinity); // $ExpectType boolean
