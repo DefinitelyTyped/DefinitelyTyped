0bed6393b358ccb1496a207245aaecfcbc9db8b5
Alice Pote <alice.writes.wrongs@gmail.com>
2021-10-03 10:58:20 -0400

    fix that

--- types/url-assembler/index.d.ts
+++ types/url-assembler/index.d.ts
@@ -1,4 +1,4 @@
-// Type definitions for url-assembler 2.1.0
+// Type definitions for url-assembler 2.1
 // Project: https://github.com/Floby/node-url-assembler
 // Definitions by: Wolfgang Faust <https://github.com/wolfgang42>
 //                 Alice Pote <https://github.com/alicewriteswrongs>
