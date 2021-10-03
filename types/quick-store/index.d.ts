41096ba87355a0d0fe3c245dd9622694d36b5f99
Promise <me@promise.solutions>
2021-10-03 09:50:17 +0200

    Use PascalCase and method overloading

--- types/quick-store/index.d.ts
+++ types/quick-store/index.d.ts
@@ -5,21 +5,21 @@
 
 export = QuickStore;
 
-interface dbContents { [key: string]: value; }
-type value = string | number | dbContents | value[] | boolean | null;
-type callback<value> = (data: value) => any;
-type callbackExists<optionalCallback, returnIfExists, returnIfNoExists> = optionalCallback extends callback<any> ? returnIfExists : returnIfNoExists;
+interface DatabaseContents { [key: string]: Value; }
+type Value = string | number | DatabaseContents | Value[] | boolean | null;
+type Callback<Value> = (data: Value) => void;
 
-declare function QuickStore(filename?: string, data?: dbContents): QuickStore.Database;
+declare function QuickStore(filename?: string, data?: DatabaseContents): QuickStore.Database;
 
 declare namespace QuickStore {
     interface Database {
-        put(data: dbContents, callback?: callback<dbContents>): void;
-        setItem(key: string, value: value, callback?: callback<dbContents>): void;
-        getItem(key: string, callback: callback<value>): void;
-        removeItem(key: string, callback?: callback<dbContents>): void;
-        clear(callback?: callback<dbContents>): void;
-        get<optionalCallback extends callback<dbContents> | undefined>(callback?: optionalCallback): callbackExists<optionalCallback, void, dbContents>;
+        put(data: DatabaseContents, callback?: Callback<DatabaseContents>): void;
+        setItem(key: string, value: Value, callback?: Callback<DatabaseContents>): void;
+        getItem(key: string, callback: Callback<Value>): void;
+        removeItem(key: string, callback?: Callback<DatabaseContents>): void;
+        clear(callback?: Callback<DatabaseContents>): void;
+        get(callback: Callback<DatabaseContents>): void;
+        get(): DatabaseContents;
         change(filename: string): void;
     }
 }
