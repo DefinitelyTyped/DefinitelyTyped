076e3f3563c7b167964c19fbcf175690d60b3ac1
João Pinto <jo.pinto08@gmail.com>
2021-10-04 16:37:43 -0400

    Formated and changed the variables to const

--- types/kafka-node-avro/kafka-node-avro-tests.ts
+++ types/kafka-node-avro/kafka-node-avro-tests.ts
@@ -1,56 +1,54 @@
-import * as KafkaAvro from 'kafka-node-avro';
+import * as KafkaAvro from "kafka-node-avro";
 
 KafkaAvro.init({
-    kafka: {
-        kafkaHost: 'kafka:9092',
-    },
-    schema: {
-        registry: 'http://schema-registry:8081',
-    },
+  kafka: {
+    kafkaHost: "kafka:9092",
+  },
+  schema: {
+    registry: "http://schema-registry:8081",
+  },
 }).then((client: KafkaAvro.Kafka) => {
-    client
-        .send({
-            topic: 'kafka-topic',
-            key: 'message-key',
-            messages: {
-                name: 'John',
-                lastName: 'Doe',
-                age: 53,
-            },
-        });
+  client.send({
+    topic: "kafka-topic",
+    key: "message-key",
+    messages: {
+      name: "John",
+      lastName: "Doe",
+      age: 53,
+    },
+  });
 });
 
 KafkaAvro.init({
-    kafka: {
-        kafkaHost: 'kafka:9092',
-    },
-    schema: {
-        registry: 'http://schema-registry:8081',
-    },
+  kafka: {
+    kafkaHost: "kafka:9092",
+  },
+  schema: {
+    registry: "http://schema-registry:8081",
+  },
 }).then((client: KafkaAvro.Kafka) => {
-    var producer = client.addProducer();
-    
-    producer.send({
-            topic: 'kafka-topic',
-            key: 'message-key',
-            messages: {
-                name: 'John',
-                lastName: 'Doe',
-                age: 53,
-            },
-        });
+  const producer = client.addProducer();
+
+  producer.send({
+    topic: "kafka-topic",
+    key: "message-key",
+    messages: {
+      name: "John",
+      lastName: "Doe",
+      age: 53,
+    },
+  });
 });
 
 KafkaAvro.init({
-    kafka: {
-        kafkaHost: 'kafka:9092',
-    },
-    schema: {
-        registry: 'http://schema-registry:8081',
-    },
+  kafka: {
+    kafkaHost: "kafka:9092",
+  },
+  schema: {
+    registry: "http://schema-registry:8081",
+  },
 }).then((client: KafkaAvro.Kafka) => {
-    var consumer = client.addConsumer('kafka-topic');
-    
-    consumer.on("message", console.log);
-});
+  const consumer = client.addConsumer("kafka-topic");
 
+  consumer.on("message", console.log);
+});
