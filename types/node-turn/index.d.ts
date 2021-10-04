0ac68d938d09f32b6304b04c0db7b6eca782d028
garzj <jojogarz123@gmail.com>
2021-10-04 10:53:30 +0200

    Add types for node-turn

---
+++ types/node-turn/index.d.ts
@@ -0,0 +1,35 @@
+// Type definitions for node-turn 0.0
+// Project: https://github.com/Atlantis-Software/node-turn#readme
+// Definitions by: Johannes Garz <https://github.com/garzj>
+// Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
+
+type TurnDebugLevel = 'OFF' | 'FATAL' | 'ERROR' | 'WARN' | 'INFO' | 'DEBUG' | 'TRACE' | 'ALL';
+
+type TurnAuthMech = 'none' | 'short-term' | 'long-term';
+
+interface TurnOptions {
+    listeningPort?: number;
+    listeningIps?: string[];
+    relayIps?: string[];
+    externalIps?: string | { [localIp: string]: string };
+    minPort?: number;
+    maxPort?: number;
+    authMech?: TurnAuthMech;
+    credentials?: { [user: string]: string };
+    real?: string;
+    debugLevel?: TurnDebugLevel;
+    maxAllocateLifetime?: number;
+    defaultAllocatetLifetime?: number;
+    debug?: (debugLevel: TurnDebugLevel, message: string) => void;
+}
+
+declare class Turn {
+    constructor(options: TurnOptions);
+
+    start(): void;
+    stop(): void;
+    addUser(username: string, password: string): void;
+    removeUser(username: string): void;
+}
+
+export = Turn;
