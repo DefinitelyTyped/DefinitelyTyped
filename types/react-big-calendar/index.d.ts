b6f57fdf02ff888f636e45c3aea620cc35ae0880
Ólafur Hólm Eyþórsson <olafur@beedle.co>
2021-10-04 20:24:47 +0000

    Removing patch version in header as it is not allowed

--- types/react-big-calendar/index.d.ts
+++ types/react-big-calendar/index.d.ts
@@ -1,4 +1,4 @@
-// Type definitions for react-big-calendar 0.36.0
+// Type definitions for react-big-calendar 0.36
 // Project: https://github.com/jquense/react-big-calendar
 // Definitions by: Piotr Witek <https://github.com/piotrwitek>
 //                 Austin Turner <https://github.com/paustint>
