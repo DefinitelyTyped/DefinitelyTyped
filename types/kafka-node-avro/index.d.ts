a029364cb7de94713423b3ec45a3c58f85809df8
João Pinto <jo.pinto08@gmail.com>
2021-10-04 16:37:07 -0400

    Fixed the identation and TS version

--- types/kafka-node-avro/index.d.ts
+++ types/kafka-node-avro/index.d.ts
@@ -2,46 +2,46 @@
 // Project: https://github.com/narcisoguillen/kafka-node-avro#readme
 // Definitions by: Gediminas Katilevicius <https://github.com/alfamegaxq>
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
-// TypeScript Version: 2.2
+// TypeScript Version: 4.4.3
 
 import { Consumer, HighLevelProducer } from "kafka-node";
 
 export interface SchemaSettings {
-    /** Kafka schema registry url */
-    registry: string;
+  /** Kafka schema registry url */
+  registry: string;
 }
 
 export interface KafkaSettings {
-    /** Kafka broker host name */
-    kafkaHost: string;
+  /** Kafka broker host name */
+  kafkaHost: string;
 }
 
 export interface Settings {
-    /** Kafka broker settings */
-    kafka: KafkaSettings;
-    /** Kafka schema registry settings */
-    schema: SchemaSettings;
+  /** Kafka broker settings */
+  kafka: KafkaSettings;
+  /** Kafka schema registry settings */
+  schema: SchemaSettings;
 }
 
 export interface SendOptions {
-    /** Kafka topic name to publish message */
-    topic: string;
-    /** Message key */
-    key: string;
-    /**
-     * Object to send to kafka.
-     * It will be automatically Avro encoded if schema registry finds a valid schema for topic
-     */
-    messages: object;
+  /** Kafka topic name to publish message */
+  topic: string;
+  /** Message key */
+  key: string;
+  /**
+   * Object to send to kafka.
+   * It will be automatically Avro encoded if schema registry finds a valid schema for topic
+   */
+  messages: object;
 }
 
 export interface Kafka {
-    /** Publishes message to Kafka */
-    send: (options: SendOptions) => Promise<{}>;
-    /** Creates a HighLevelProducer on demand */
-    addProducer: (options?, customPartitioner?) => Producer;
-    /** Creates a Consumer on demand */
-    addConsumer: (topics, options?) => Consumer;
+  /** Publishes message to Kafka */
+  send: (options: SendOptions) => Promise<{}>;
+  /** Creates a HighLevelProducer on demand */
+  addProducer: (options?, customPartitioner?) => Producer;
+  /** Creates a Consumer on demand */
+  addConsumer: (topics, options?) => Consumer;
 }
 
 /**
