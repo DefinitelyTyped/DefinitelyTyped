517a24b387ebc5235345697b638a8510469bc213
Lee Pender <lpender@gmail.com>
2021-10-05 05:52:45 -0700

    Update types/phoenix/index.d.ts

--- types/phoenix/index.d.ts
+++ types/phoenix/index.d.ts
@@ -39,7 +39,7 @@ export type ConnectionState = 'connecting' | 'open' | 'closing' | 'closed';
 export interface SocketConnectOption {
   binaryType: BinaryType;
   params: object | (() => object);
-  transport: new (endpoint: string) => Object;
+  transport: new (endpoint: string) => object;
   timeout: number;
   heartbeatIntervalMs: number;
   longpollerTimeout: number;
