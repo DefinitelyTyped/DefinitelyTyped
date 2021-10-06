c8ac97a008220fa0a9d1b0f3f2c0c971f8656956
Federico Panico <federicopanico@gmail.com>
2021-10-06 20:56:08 -0300

    @ckeditor/ckeditor5-utils Fix Emitter params
    Params for `on`, `once`, `listenTo` after the first one (which is
    always an `EventInfo`) depend on the listened event, and are different
    for each event. Only `dom/emittermixin` passes a `DomEventData`.

--- types/ckeditor__ckeditor5-engine/src/controller/datacontroller.d.ts
+++ types/ckeditor__ckeditor5-engine/src/controller/datacontroller.d.ts
@@ -1,24 +1,24 @@
-import { Emitter, EmitterMixinDelegateChain } from "@ckeditor/ckeditor5-utils/src/emittermixin";
-import EventInfo from "@ckeditor/ckeditor5-utils/src/eventinfo";
-import { BindChain, Observable } from "@ckeditor/ckeditor5-utils/src/observablemixin";
-import { PriorityString } from "@ckeditor/ckeditor5-utils/src/priorities";
-import DowncastDispatcher from "../conversion/downcastdispatcher";
-import Mapper from "../conversion/mapper";
-import UpcastDispatcher from "../conversion/upcastdispatcher";
-import { DataProcessor } from "../dataprocessor/dataprocessor";
-import HtmlDataProcessor from "../dataprocessor/htmldataprocessor";
-import DocumentFragment from "../model/documentfragment";
-import Element from "../model/element";
-import ViewElement from "../view/element";
-import Model from "../model/model";
-import { SchemaContextDefinition } from "../model/schema";
-import Document from "../view/document";
-import ViewDocumentFragment from "../view/documentfragment";
-import { MatcherPattern } from "../view/matcher";
-import DomEventData from "../view/observer/domeventdata";
-import { StylesProcessor } from "../view/stylesmap";
+import { Emitter, EmitterMixinDelegateChain } from '@ckeditor/ckeditor5-utils/src/emittermixin';
+import EventInfo from '@ckeditor/ckeditor5-utils/src/eventinfo';
+import { BindChain, Observable } from '@ckeditor/ckeditor5-utils/src/observablemixin';
+import { PriorityString } from '@ckeditor/ckeditor5-utils/src/priorities';
+import DowncastDispatcher from '../conversion/downcastdispatcher';
+import Mapper from '../conversion/mapper';
+import UpcastDispatcher from '../conversion/upcastdispatcher';
+import { DataProcessor } from '../dataprocessor/dataprocessor';
+import HtmlDataProcessor from '../dataprocessor/htmldataprocessor';
+import DocumentFragment from '../model/documentfragment';
+import Element from '../model/element';
+import ViewElement from '../view/element';
+import Model from '../model/model';
+import { SchemaContextDefinition } from '../model/schema';
+import Document from '../view/document';
+import ViewDocumentFragment from '../view/documentfragment';
+import { MatcherPattern } from '../view/matcher';
+import { StylesProcessor } from '../view/stylesmap';
 
-export default class DataController implements Emitter, Observable {
+export default class DataController implements Observable {
+    constructor(model: Model, stylesProcessor: StylesProcessor);
     readonly downcastDispatcher: DowncastDispatcher;
     readonly htmlProcessor: HtmlDataProcessor;
     readonly mapper: Mapper;
@@ -27,13 +27,11 @@ export default class DataController implements Emitter, Observable {
     readonly stylesProcessor: StylesProcessor;
     readonly upcastDispatcher: UpcastDispatcher;
     readonly viewDocument: Document;
-
-    constructor(model: Model, stylesProcessor: StylesProcessor);
     addStyleProcessorRules(
         callback: (stylesProcessor: StylesProcessor) => Record<string, Record<string, string> | string>,
     ): void;
     destroy(): void;
-    get(options?: { rootName?: string | undefined; trim?: "empty" | "none" | undefined }): string;
+    get(options?: { rootName?: string | undefined; trim?: 'empty' | 'none' | undefined }): string;
     init(data: string | Record<string, string>): Promise<void>;
     parse(data: string, context?: SchemaContextDefinition): DocumentFragment;
     registerRawContentMatcher(pattern: MatcherPattern): void;
@@ -44,32 +42,34 @@ export default class DataController implements Emitter, Observable {
     ): DocumentFragment;
     toView(modelElementOrFragment: Element | DocumentFragment, options?: Record<string, any>): ViewDocumentFragment;
 
-    set(option: Record<string, unknown>|string, options?: {
-        batchType?: "default" | "transparent"
-    }): void;
+    set(option: Record<string, unknown>): void;
+    set(name: string, value: unknown): void;
     bind(...bindProperties: string[]): BindChain;
     unbind(...unbindProperties: string[]): void;
     decorate(methodName: string): void;
-
-    on: (
-        event: string,
-        callback: (info: EventInfo, data: DomEventData) => void,
-        options?: { priority: PriorityString | number },
-    ) => void;
-    once(
-        event: string,
-        callback: (info: EventInfo, data: DomEventData) => void,
-        options?: { priority: PriorityString | number },
+    on<K extends string>(
+        event: K,
+        callback: (this: this, info: EventInfo<this, K>, ...args: any[]) => void,
+        options?: { priority?: number | PriorityString | undefined },
     ): void;
-    off(event: string, callback?: (info: EventInfo, data: DomEventData) => void): void;
-    listenTo(
-        emitter: Emitter,
-        event: string,
-        callback: (info: EventInfo, data: DomEventData) => void,
-        options?: { priority?: PriorityString | number | undefined },
+    once<K extends string>(
+        event: K,
+        callback: (this: this, info: EventInfo<this, K>, ...args: any[]) => void,
+        options?: { priority?: number | PriorityString | undefined },
     ): void;
-    stopListening(emitter?: Emitter, event?: string, callback?: (info: EventInfo, data: DomEventData) => void): void;
-    fire(eventOrInfo: string | EventInfo, ...args: any[]): any;
+    off<K extends string>(event: K, callback?: (this: this, info: EventInfo<this, K>, ...args: any[]) => void): void;
+    listenTo<P extends string, E extends Emitter>(
+        emitter: E,
+        event: P,
+        callback: (this: this, info: EventInfo<E, P>, ...args: any[]) => void,
+        options?: { priority?: number | PriorityString | undefined },
+    ): void;
+    stopListening<E extends Emitter, P extends string>(
+        emitter?: E,
+        event?: P,
+        callback?: (this: this, info: EventInfo<E, P>, ...args: any[]) => void,
+    ): void;
+    fire(eventOrInfo: string | EventInfo, ...args: any[]): unknown;
     delegate(...events: string[]): EmitterMixinDelegateChain;
     stopDelegating(event?: string, emitter?: Emitter): void;
 }
