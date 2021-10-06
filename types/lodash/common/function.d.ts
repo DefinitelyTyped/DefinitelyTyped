e1376c3550b031d4157befbfff2e8554b4615517
akcyp <pycka.bartosz@gmail.com>
2021-10-06 17:52:16 +0200

    Lodash debounce generic function fix

--- types/lodash/common/function.d.ts
+++ types/lodash/common/function.d.ts
@@ -368,18 +368,16 @@ declare module "../index" {
          */
         trailing?: boolean | undefined;
     }
-    interface DebouncedFunc<T extends (...args: any[]) => any> {
-        /**
-         * Call the original function, but applying the debounce rules.
-         *
-         * If the debounced function can be run immediately, this calls it and returns its return
-         * value.
-         *
-         * Otherwise, it returns the return value of the last invocation, or undefined if the debounced
-         * function was not invoked yet.
-         */
-        (...args: Parameters<T>): ReturnType<T> | undefined;
-
+    /**
+     * Call the original function, but applying the debounce rules.
+     *
+     * If the debounced function can be run immediately, this calls it and returns its return
+     * value.
+     *
+     * Otherwise, it returns the return value of the last invocation, or undefined if the debounced
+     * function was not invoked yet.
+     */
+    type DebouncedFunc<T extends (...args: any[]) => any> = (T | (() => undefined)) & {
         /**
          * Throw away any pending invocation of the debounced function.
          */
