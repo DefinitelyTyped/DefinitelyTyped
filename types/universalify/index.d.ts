63f5f163bbf67c9d306c9b4ad2cc6654fda21635
Ricardo Devis Agullo <mkredz@gmail.com>
2021-10-07 20:52:45 +0200

    improve types to mantain type on arguments

--- types/universalify/index.d.ts
+++ types/universalify/index.d.ts
@@ -3,5 +3,126 @@
 // Definitions by: Richie Bendall <https://github.com/Richienb>
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
 
+export function fromCallback(fn: (callback: (err?: unknown) => void) => void): {
+    (): Promise<void>;
+    (callback: (err?: unknown) => void): void;
+};
+export function fromCallback<T1, TResult>(
+    fn: (arg1: T1, callback: (err: unknown, result: TResult) => void) => void,
+): {
+    (arg1: T1): Promise<TResult>;
+    (arg1: T1, cb: (err: unknown, result: TResult) => void): void;
+};
+export function fromCallback<T1>(fn: (arg1: T1, callback: (err?: unknown) => void) => void): {
+    (arg1: T1): Promise<void>;
+    (arg1: T1, cb: (err?: unknown) => void): void;
+};
+export function fromCallback<T1, T2, TResult>(
+    fn: (arg1: T1, arg2: T2, callback: (err: unknown, result: TResult) => void) => void,
+): {
+    (arg1: T1, arg2: T2): Promise<TResult>;
+    (arg1: T1, arg2: T2, cb: (err: unknown, result: TResult) => void): void;
+};
+export function fromCallback<T1, T2>(
+    fn: (arg1: T1, arg2: T2, callback: (err?: unknown) => void) => void,
+): {
+    (arg1: T1, arg2: T2): Promise<void>;
+    (arg1: T1, arg2: T2, cb: (err: unknown) => void): void;
+};
+export function fromCallback<T1, T2, T3, TResult>(
+    fn: (arg1: T1, arg2: T2, arg3: T3, callback: (err: unknown, result: TResult) => void) => void,
+): {
+    (arg1: T1, arg2: T2, arg3: T3): Promise<TResult>;
+    (arg1: T1, arg2: T2, arg3: T3, cb: (err: unknown, result: TResult) => void): void;
+};
+export function fromCallback<T1, T2, T3>(
+    fn: (arg1: T1, arg2: T2, arg3: T3, callback: (err?: unknown) => void) => void,
+): {
+    (arg1: T1, arg2: T2, arg3: T3): Promise<void>;
+    (arg1: T1, arg2: T2, arg3: T3, cb: (err?: unknown) => void): void;
+};
+export function fromCallback<T1, T2, T3, T4, TResult>(
+    fn: (arg1: T1, arg2: T2, arg3: T3, arg4: T4, callback: (err: unknown, result: TResult) => void) => void,
+): {
+    (arg1: T1, arg2: T2, arg3: T3, arg4: T4): Promise<TResult>;
+    (arg1: T1, arg2: T2, arg3: T3, arg4: T4, cb: (err: unknown, result: TResult) => void): void;
+};
+export function fromCallback<T1, T2, T3, T4>(
+    fn: (arg1: T1, arg2: T2, arg3: T3, arg4: T4, callback: (err?: unknown) => void) => void,
+): {
+    (arg1: T1, arg2: T2, arg3: T3, arg4: T4): Promise<void>;
+    (arg1: T1, arg2: T2, arg3: T3, arg4: T4, cb: (err?: unknown) => void): void;
+};
+export function fromCallback<T1, T2, T3, T4, T5, TResult>(
+    fn: (arg1: T1, arg2: T2, arg3: T3, arg4: T4, arg5: T5, callback: (err: unknown, result: TResult) => void) => void,
+): {
+    (arg1: T1, arg2: T2, arg3: T3, arg4: T4, arg5: T5): Promise<TResult>;
+    (arg1: T1, arg2: T2, arg3: T3, arg4: T4, arg5: T5, cb: (err: unknown, result: TResult) => void): void;
+};
+export function fromCallback<T1, T2, T3, T4, T5>(
+    fn: (arg1: T1, arg2: T2, arg3: T3, arg4: T4, arg5: T5, callback: (err?: unknown) => void) => void,
+): {
+    (arg1: T1, arg2: T2, arg3: T3, arg4: T4, arg5: T5): Promise<void>;
+    (arg1: T1, arg2: T2, arg3: T3, arg4: T4, arg5: T5, cb: (err?: unknown) => void): void;
+};
 export function fromCallback(fn: (...args: any[]) => any): (...args: any[]) => Promise<any> | void;
+
+export function fromPromise<T1, TResult>(
+    fn: (arg1: T1) => Promise<TResult>,
+): {
+    (arg1: T1): Promise<TResult>;
+    (arg1: T1, cb: (err: unknown, result: TResult) => void): void;
+};
+export function fromPromise<T1>(fn: (arg1: T1) => Promise<void>): {
+    (arg1: T1): Promise<void>;
+    (arg1: T1, cb: (err?: unknown) => void): void;
+};
+export function fromPromise<T1, T2, TResult>(
+    fn: (arg1: T1, arg2: T2) => Promise<TResult>,
+): {
+    (arg1: T1, arg2: T2): Promise<TResult>;
+    (arg1: T1, arg2: T2, cb: (err: unknown, result: TResult) => void): void;
+};
+export function fromPromise<T1, T2>(
+    fn: (arg1: T1, arg2: T2) => Promise<void>,
+): {
+    (arg1: T1, arg2: T2): Promise<void>;
+    (arg1: T1, arg2: T2, cb: (err?: unknown) => void): void;
+};
+export function fromPromise<T1, T2, T3, TResult>(
+    fn: (arg1: T1, arg2: T2, arg3: T3) => Promise<TResult>,
+): {
+    (arg1: T1, arg2: T2, arg3: T3): Promise<TResult>;
+    (arg1: T1, arg2: T2, arg3: T3, cb: (err: unknown, result: TResult) => void): void;
+};
+export function fromPromise<T1, T2, T3>(
+    fn: (arg1: T1, arg2: T2, arg3: T3) => Promise<void>,
+): {
+    (arg1: T1, arg2: T2, arg3: T3): Promise<void>;
+    (arg1: T1, arg2: T2, arg3: T3, cb: (err?: unknown) => void): void;
+};
+export function fromPromise<T1, T2, T3, T4, TResult>(
+    fn: (arg1: T1, arg2: T2, arg3: T3, arg4: T4) => Promise<TResult>,
+): {
+    (arg1: T1, arg2: T2, arg3: T3, arg4: T4): Promise<TResult>;
+    (arg1: T1, arg2: T2, arg3: T3, arg4: T4, cb: (err: unknown, result: TResult) => void): void;
+};
+export function fromPromise<T1, T2, T3, T4>(
+    fn: (arg1: T1, arg2: T2, arg3: T3, arg4: T4) => Promise<void>,
+): {
+    (arg1: T1, arg2: T2, arg3: T3, arg4: T4): Promise<void>;
+    (arg1: T1, arg2: T2, arg3: T3, arg4: T4, cb: (err?: unknown) => void): void;
+};
+export function fromPromise<T1, T2, T3, T4, T5, TResult>(
+    fn: (arg1: T1, arg2: T2, arg3: T3, arg4: T4, arg: T5) => Promise<TResult>,
+): {
+    (arg1: T1, arg2: T2, arg3: T3, arg4: T4, arg: T5): Promise<TResult>;
+    (arg1: T1, arg2: T2, arg3: T3, arg4: T4, arg: T5, cb: (err: unknown, result: TResult) => void): void;
+};
+export function fromPromise<T1, T2, T3, T4, T5>(
+    fn: (arg1: T1, arg2: T2, arg3: T3, arg4: T4, arg: T5) => Promise<void>,
+): {
+    (arg1: T1, arg2: T2, arg3: T3, arg4: T4, arg: T5): Promise<void>;
+    (arg1: T1, arg2: T2, arg3: T3, arg4: T4, arg: T5, cb: (err?: unknown) => void): void;
+};
 export function fromPromise(fn: (...args: any[]) => any): (...args: any[]) => Promise<any> | void;
--- types/universalify/universalify-tests.ts
+++ types/universalify/universalify-tests.ts
@@ -1,4 +1,47 @@
-import universalify = require("universalify");
+import universalify = require('universalify');
 
-universalify.fromCallback(() => { }); // $ExpectType (...args: any[]) => void | Promise<any>
-universalify.fromPromise(() => { }); // $ExpectType (...args: any[]) => void | Promise<any>
+universalify.fromCallback(() => {}); // $ExpectType { (): Promise<void>; (callback: (err?: unknown) => void): void; }
+universalify.fromPromise(async () => {}); // $ExpectType { (arg1: unknown): Promise<void>; (arg1: unknown, cb: (err: unknown, result: void) => void): void; }
+
+declare const cb1: (p1: number, cb: (err: Error, data: string) => void) => void;
+universalify.fromCallback(cb1); // $ExpectType { (arg1: number): Promise<string>; (arg1: number, cb: (err: unknown, result: string) => void): void; }
+
+declare const p1: (p1: number) => Promise<string>;
+universalify.fromPromise(p1); // $ExpectType { (arg1: number): Promise<string>; (arg1: number, cb: (err: unknown, result: string) => void): void; }
+
+declare const cb2: (p1: number, p2: number, cb: (err: Error, data: string) => void) => void;
+universalify.fromCallback(cb2); // $ExpectType { (arg1: number, arg2: number): Promise<string>; (arg1: number, arg2: number, cb: (err: unknown, result: string) => void): void; }
+
+declare const p2: (p1: number, p2: number) => Promise<string>;
+universalify.fromPromise(p2); // $ExpectType { (arg1: number, arg2: number): Promise<string>; (arg1: number, arg2: number, cb: (err: unknown, result: string) => void): void; }
+
+declare const cb3: (p1: number, p2: number, p3: number, cb: (err: Error, data: string) => void) => void;
+universalify.fromCallback(cb3)(1, 2, 3); // $ExpectType Promise<string>
+
+declare const p3: (p1: number, p2: number, p3: number) => Promise<string>;
+universalify.fromPromise(p3)(1, 2, 3, (err, data) => {
+    data; // $ExpectType string
+});
+
+declare const cb4: (p1: number, p2: number, p3: number, p4: number, cb: (err: Error, data: string) => void) => void;
+universalify.fromCallback(cb4)(1, 2, 3, 4); // $ExpectType Promise<string>
+
+declare const p4: (p1: number, p2: number, p3: number, p4: number) => Promise<string>;
+universalify.fromPromise(p4)(1, 2, 3, 4, (err, data) => {
+    data; // $ExpectType string
+});
+
+declare const cb5: (
+    p1: number,
+    p2: number,
+    p3: number,
+    p4: number,
+    p5: number,
+    cb: (err: Error, data: string) => void,
+) => void;
+universalify.fromCallback(cb5)(1, 2, 3, 4, 5); // $ExpectType Promise<string>
+
+declare const p5: (p1: number, p2: number, p3: number, p4: number, p5: number) => Promise<string>;
+universalify.fromPromise(p5)(1, 2, 3, 4, 5, (err, data) => {
+    data; // $ExpectType string
+});
