a240da67ba438d67c5196a15f33c284079b43e98
steven nguyen <nguyeste008@students.garlandisd.net>
2021-10-07 12:00:02 -0500

    Use inline documentation for types
    See https://github.com/DefinitelyTyped/DefinitelyTyped/discussions/56297
    I didn't remove undocumented methods or add deprecated methods for now.
    
    For `addInjectionPoint` I'm not really sure what "syntax node" refers to. See https://github.com/atom/atom/discussions/23019
    https://github.com/atom/atom/commit/37a3ae149143e8275687ad3d8758b6ecf3695b7f might help, although the file has changed since then to only use 1 param: https://github.com/atom/atom/blame/v1.40.0/spec/tree-sitter-language-mode-spec.js. I can't find anything with "node.child".
    
    
    For `getAssignedLanguageId`, if you look in the code, it's just a `Map#get` call.
    Which could be undefined. Now if you look even closer, `assignLanguageMode` and `assignGrammar` also set values to null: https://github.com/atom/atom/blob/v1.40.0/src/grammar-registry.js#L137
    
    So `getAssignedLanguageId` actually returns `undefined | null | string`. But for now, I'm sticking to the documentation in the code
    
    `readGrammar` either calls the callback with `null, Grammar` or `error`. I guess the docs treat `undefined` as equivalent to `null`. But I'm changing the docs anyway.
    `getGrammars` docs say "options" but the param is actually called "params".

--- types/atom/src/grammar-registry.d.ts
+++ types/atom/src/grammar-registry.d.ts
@@ -1,5 +1,12 @@
 import { Disposable, Grammar, GrammarToken, TextBuffer } from '../index';
 
+type SyntaxNode = any;
+type InjectionPoint = {
+    type: string;
+    language: (node: SyntaxNode) => string;
+    content: (node: SyntaxNode) => SyntaxNode | SyntaxNode[];
+}
+
 /** Registry containing one or more grammars. */
 export interface GrammarRegistry {
     // Event Subscription
@@ -18,19 +25,16 @@ export interface GrammarRegistry {
      */
     onDidUpdateGrammar(callback: (grammar: Grammar) => void): Disposable;
 
-    /**
-     *  Invoke the given callback when a grammar is removed from the registry.
-     *  @param callback The callback to be invoked whenever a grammar is removed.
-     *  @return A Disposable on which `.dispose()` can be called to unsubscribe.
-     */
-    onDidRemoveGrammar(callback: (grammar: Grammar) => void): Disposable;
-
     // Managing Grammars
     /**
      *  Get all the grammars in this registry.
+     *  @param [params]
+     *  @param [params.includeTreeSitter] Whether to include
+     *  [Tree-sitter](https://github.blog/2018-10-31-atoms-new-parsing-system/)
+     *  grammars
      *  @return A non-empty array of Grammar instances.
      */
-    getGrammars(): Grammar[];
+    getGrammars(params?: { includeTreeSitter?: boolean }): Grammar[];
 
     /**
      *  Get a grammar with the given scope name.
@@ -66,14 +70,16 @@ export interface GrammarRegistry {
     /**
      *  Read a grammar synchronously but don't add it to the registry.
      *  @param grammarPath The absolute file path to a grammar.
-     *  @return The newly loaded Grammar.
+     *  @return A Grammar.
      */
     readGrammarSync(grammarPath: string): Grammar;
 
     /**
      *  Read a grammar asynchronously but don't add it to the registry.
      *  @param grammarPath The absolute file path to the grammar.
-     *  @param callback The function to be invoked once the Grammar has been read in.
+     *  @param callback The function to call when read with the following arguments:
+     *      - error: An error or null
+     *      - grammar: A grammar if an error has not occurred
      */
     readGrammar(grammarPath: string, callback: (error: Error | null, grammar?: Grammar) => void): void;
 
@@ -121,6 +127,22 @@ export interface GrammarRegistry {
     assignLanguageMode(buffer: TextBuffer, languageId: string): boolean;
 
     /**
+     *  Force a TextBuffer to use a different grammar than the one that would otherwise
+     *  be selected for it.
+     *  @param buffer The buffer whose grammar will be set.
+     *  @param grammar The desired Grammar
+     *  @return Returns a boolean that indicates whether the assignment was successful
+     */
+    assignGrammar(buffer: TextBuffer, grammar: Grammar): boolean;
+
+    /**
+     *  Get the `languageId` that has been explicity assigned to the given buffer, if
+     *  any.
+     *  @return Returns a string id of the language
+     */
+    getAssignedLanguageId(buffer: TextBuffer): string;
+
+    /**
      *  Remove any language mode override that has been set for the given TextBuffer.
      *  This will assign to the buffer the best language mode available.
      */
@@ -144,4 +166,20 @@ export interface GrammarRegistry {
      *  @param contents A string of text for that file path.
      */
     getGrammarScore(grammar: Grammar, filePath: string, contents: string): number;
+
+    /**
+     *  Specify a type of syntax node that may embed other languages
+     *  @param grammarId The string id of the parent language
+     *  @param injectionPoint An object with the following keys:
+     *  @param injectionPoint.type The string type of syntax node that may embed
+     *  other languages
+     *  @param injectionPoint.language A function that is called with syntax nodes
+     *  of the specified type and returns a string that will be tested against
+     *  other grammars' injectionRegex in order to determine what language should
+     *  be embedded.
+     *  @param injectionPoint.content A function that is called with syntax nodes
+     *  of the specified type and returns another syntax node or array of syntax
+     *  nodes that contain the embedded source code.
+     */
+    addInjectionPoint(grammarId: string, injectionPoint: InjectionPoint): Disposable;
 }
