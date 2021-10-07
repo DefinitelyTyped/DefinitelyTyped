bfcdf1c54844f94388dbaa95e75447adf3d96ce6
Jesper Sand Nielsen <jesper.sand.nielsen@gmail.com>
2021-10-07 11:17:41 +0200

    seatsio: add tests for descriminated bookableobjects

--- types/seatsio/seatsio-tests.ts
+++ types/seatsio/seatsio-tests.ts
@@ -1,10 +1,7 @@
 const seatingChartConfig: Seatsio.SeatingChartConfig = {
     divId: 'chart',
     workspaceKey: 'myWorkspaceKey',
-    selectionValidators: [
-        { type: 'consecutiveSeats' },
-        { type: 'noOrphanSeats' }
-    ]
+    selectionValidators: [{ type: 'consecutiveSeats' }, { type: 'noOrphanSeats' }],
 };
 const seatingChart = new seatsio.SeatingChart(seatingChartConfig).render();
 seatingChart.destroy();
@@ -13,7 +10,7 @@ const eventManager = new seatsio.EventManager({
     divId: 'chart',
     secretKey: 'aSecretKey',
     event: 'event1',
-    mode: 'manageChannels'
+    mode: 'manageChannels',
 }).render();
 eventManager.destroy();
 
@@ -21,13 +18,28 @@ const chartManager = new seatsio.ChartManager({
     divId: 'chart',
     secretKey: 'aSecretKey',
     chart: 'chart1',
-    mode: 'manageRulesets'
+    mode: 'manageRulesets',
 }).render();
 chartManager.destroy();
 
 const designer = new seatsio.SeatingChartDesigner({
     divId: 'chart',
     secretKey: 'aSecretKey',
-    chartKey: 'chart1'
+    chartKey: 'chart1',
 }).render();
 designer.destroy();
+
+const seatObject: Seatsio.BookableObject = {} as Seatsio.Seat;
+if (seatObject.objectType === 'Seat') {
+    seatObject.disabledBySocialDistancingRules;
+}
+
+const GeneralAdmissionAreaObject: Seatsio.BookableObject = {} as Seatsio.GeneralAdmissionArea;
+if (GeneralAdmissionAreaObject.objectType === 'GeneralAdmissionArea') {
+    GeneralAdmissionAreaObject.selectionPerTicketType;
+}
+
+const SectionObject: Seatsio.BookableObject = {} as Seatsio.Section;
+if (SectionObject.objectType === 'section') {
+    SectionObject.isInteractive;
+}
