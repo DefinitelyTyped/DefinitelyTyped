dc7e4ccadc96dde3fb0329b6cc03b8d028b5b3b1
Jesper Sand Nielsen <jesper.sand.nielsen@gmail.com>
2021-10-07 11:35:55 +0200

    seatsio: update saetsio-tests

--- types/seatsio/seatsio-tests.ts
+++ types/seatsio/seatsio-tests.ts
@@ -29,17 +29,134 @@ const designer = new seatsio.SeatingChartDesigner({
 }).render();
 designer.destroy();
 
-const seatObject: Seatsio.BookableObject = {} as Seatsio.Seat;
-if (seatObject.objectType === 'Seat') {
-    seatObject.disabledBySocialDistancingRules;
-}
+const testbookableObject = (object: Seatsio.BookableObject) => {
+    switch (object.objectType) {
+        case 'Seat':
+            object.disabledBySocialDistancingRules;
+            break;
+        case 'GeneralAdmissionArea':
+            object.selectionPerTicketType;
+            break;
+        case 'section':
+            object.isInteractive;
+            break;
+    }
+};
 
-const GeneralAdmissionAreaObject: Seatsio.BookableObject = {} as Seatsio.GeneralAdmissionArea;
-if (GeneralAdmissionAreaObject.objectType === 'GeneralAdmissionArea') {
-    GeneralAdmissionAreaObject.selectionPerTicketType;
-}
+const seatObject: Seatsio.Seat = {
+    objectType: 'Seat',
+    parent: {
+        type: 'row',
+    },
+    companionSeat: false,
+    restrictedView: false,
+    disabledBySocialDistancingRules: false,
+    accessible: false,
+    label: '',
+    labels: {
+        own: '',
+        parent: undefined,
+        section: undefined,
+    },
+    status: '',
+    extraData: undefined,
+    dataPerEvent: {},
+    forSale: false,
+    selectable: false,
+    selected: false,
+    inSelectableChannel: false,
+    select: (ticketType?: string): void => {
+        throw new Error('Function not implemented.');
+    },
+    deselect: (ticketType?: string): void => {
+        throw new Error('Function not implemented.');
+    },
+    pulse: (): void => {
+        throw new Error('Function not implemented.');
+    },
+    unpulse: (): void => {
+        throw new Error('Function not implemented.');
+    },
+};
+testbookableObject(seatObject);
 
-const SectionObject: Seatsio.BookableObject = {} as Seatsio.Section;
-if (SectionObject.objectType === 'section') {
-    SectionObject.isInteractive;
-}
+const generalAdmissionAreaObject: Seatsio.GeneralAdmissionArea = {
+    objectType: 'GeneralAdmissionArea',
+    capacity: 0,
+    numBooked: 0,
+    numFree: 0,
+    numSelected: 0,
+    selectionPerTicketType: {},
+    dataPerEvent: {},
+    accessible: false,
+    label: '',
+    labels: {
+        own: '',
+        parent: undefined,
+        section: undefined,
+    },
+    status: '',
+    extraData: undefined,
+    forSale: false,
+    selectable: false,
+    selected: false,
+    inSelectableChannel: false,
+    select: (ticketType?: string): void => {
+        throw new Error('Function not implemented.');
+    },
+    deselect: (ticketType?: string): void => {
+        throw new Error('Function not implemented.');
+    },
+    pulse: (): void => {
+        throw new Error('Function not implemented.');
+    },
+    unpulse: (): void => {
+        throw new Error('Function not implemented.');
+    },
+};
+testbookableObject(generalAdmissionAreaObject);
+
+const sectionObject: Seatsio.Section = {
+    objectType: 'section',
+    sectionCategory: {
+        accessible: false,
+        color: '',
+        key: 0,
+        label: '',
+        pricing: {
+            price: 0,
+            formattedPrice: '',
+        },
+    },
+    numberOfSelectableObjects: 0,
+    numberOfSelectedObjects: 0,
+    selectableCategories: [],
+    isInteractive: false,
+    accessible: false,
+    label: '',
+    labels: {
+        own: '',
+        parent: undefined,
+        section: undefined,
+    },
+    status: '',
+    extraData: undefined,
+    dataPerEvent: {},
+    forSale: false,
+    selectable: false,
+    selected: false,
+    inSelectableChannel: false,
+    select: (ticketType?: string): void => {
+        throw new Error('Function not implemented.');
+    },
+    deselect: (ticketType?: string): void => {
+        throw new Error('Function not implemented.');
+    },
+    pulse: (): void => {
+        throw new Error('Function not implemented.');
+    },
+    unpulse: (): void => {
+        throw new Error('Function not implemented.');
+    },
+};
+testbookableObject(sectionObject);
