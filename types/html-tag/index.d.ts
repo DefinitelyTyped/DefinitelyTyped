b3953d5e3f66268b566e89533cd7d163b7e5aa23
arnu515 <52203828+arnu515@users.noreply.github.com>
2021-10-08 17:14:47 +0530

    Update index.d.ts
    Add semicolon

--- types/html-tag/index.d.ts
+++ types/html-tag/index.d.ts
@@ -5,4 +5,4 @@
 
 declare function tag(name: string, text?: string | false): string;
 declare function tag(name: string, attribs?: Record<string, string | number | boolean>, text?: string | false): string;
-export = tag
+export = tag;
