cc15a3bac939e8b00721d02fb8115d2706e4d0b6
Omar Almalol <omar.alma3lol@gmail.com>
2021-10-10 23:10:20 -0400

    feat: adds Toolbar index.d.ts file

---
+++ types/popoto/toolbar/index.d.ts
@@ -0,0 +1,11 @@
+export namespace Toolbar {
+    export let TOOL_TAXONOMY: string;
+    export let TOOL_RELATION: string;
+    export let TOOL_CENTER: string;
+    export let TOOL_FULL_SCREEN: string;
+    export let TOOL_RESET: string;
+    export let TOOL_SAVE: string;
+    export let TOOL_FIT_TEXT: string;
+
+    export const render: (container: SVGGElement) => void;
+}
