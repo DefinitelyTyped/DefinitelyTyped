47061f6f9c4f37a4fb68ed592991515d704ed210
Omar Almalol <omar.alma3lol@gmail.com>
2021-10-10 23:41:27 -0400

    feat: adds missing driver to runner

--- types/popoto/runner/index.d.ts
+++ types/popoto/runner/index.d.ts
@@ -1,6 +1,8 @@
-import { Session, Result } from 'neo4j-driver-lite';
+import { Driver, Session, Result } from 'neo4j-driver-lite';
 
 export namespace Runner {
+    export let DRIVER: Driver;
+
     export let createSession: () => Session;
 
     export const run: (statements: { statement: string, parameters: any[] }[]) => void;
