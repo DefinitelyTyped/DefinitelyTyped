1cf4e54a573307e0ce90edb14b00f11fa9a36140
Yan-Luca <36674649+chaosfreak93@users.noreply.github.com>
2021-10-14 08:42:26 +0200

    Update index.d.ts

--- types/xmldoc/index.d.ts
+++ types/xmldoc/index.d.ts
@@ -1,10 +1,10 @@
-// Type definitions for xmldoc 1.1
+// Type definitions for xmldoc 1.1.2
 // Project: https://github.com/nfarina/xmldoc
 // Definitions by:  Xavier Stouder <https://github.com/Xstoudi>
 //                  Andrew Sheehan <https://github.com/ajsheehan>
 //                  Jordi Bunster <https://github.com/notlaforge>
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
-// TypeScript Version: 2.1
+// TypeScript Version: 4.4.4
 
 export class XmlDocument extends XmlElement {
     constructor(xml: string | Buffer);
