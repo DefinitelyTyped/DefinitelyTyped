7f05c673e14ea5e8f624a0f152af1fdad0716a33
Lee Zumstein <lee.zumstein@beam.dental>
2021-10-15 09:33:57 -0400

    fix: update react-native header version

--- types/react-native/index.d.ts
+++ types/react-native/index.d.ts
@@ -1,4 +1,4 @@
-// Type definitions for react-native 0.65
+// Type definitions for react-native 0.66
 // Project: https://github.com/facebook/react-native
 // Definitions by: Eloy Durán <https://github.com/alloy>
 //                 HuHuanming <https://github.com/huhuanming>
