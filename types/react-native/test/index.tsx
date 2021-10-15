25218d3a9a85b84a617186ffb0bfb3e5b50f3921
Lee Zumstein <lee.zumstein@beam.dental>
2021-10-15 09:19:56 -0400

    fix: remove Picker and PickerIOS components

--- types/react-native/test/index.tsx
+++ types/react-native/test/index.tsx
@@ -63,8 +63,6 @@ import {
     NativeScrollEvent,
     NativeSyntheticEvent,
     PermissionsAndroid,
-    Picker,
-    PickerIOS,
     Platform,
     PlatformColor,
     Pressable,
@@ -1308,13 +1306,6 @@ const DatePickerAndroidTest = () => {
         }
     });
 };
-
-const PickerTest = () => (
-    <Picker mode="dropdown" selectedValue="v1" onValueChange={(val: string) => {}}>
-        <Picker.Item label="Item1" value="v1" />
-        <Picker.Item label="Item2" value="v2" />
-    </Picker>
-);
 
 const NativeBridgedComponent = requireNativeComponent<{ nativeProp: string }>('NativeBridgedComponent'); // $ExpectType HostComponent<{ nativeProp: string; }>
 
