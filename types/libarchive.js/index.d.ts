1082dbe322e8295a101bae28bcbe8f04e7e4030c
Denis Malykh <finesunes@gmail.com>
2021-10-19 23:33:53 +0300

    add index.d.ts; remove main.d.ts and libarchive.js-tests.ts from tsconfig.json / files

--- types/libarchive.js/index.d.ts
+++ types/libarchive.js/index.d.ts
@@ -1,6 +1,13 @@
-// Type definitions for libarchive.js 1.3.0
+// Type definitions for libarchive.js 1.3
 // Project: https://github.com/nika-begiashvili/libarchivejs
 // Definitions by: Denis Malykh <https://github.com/finesunes>
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
 
-declare module "libarchive.js"
+import { Archive, FilesObject } from './main';
+import { CompressedFile } from './compressed-file';
+
+export {
+    Archive,
+    FilesObject,
+    CompressedFile,
+};
