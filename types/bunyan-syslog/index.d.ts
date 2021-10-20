48b6d129507d0e955a27d403a2bcedb1ca34f4ac
Naor Tedgi <naor.tedgi@ironsrc.com>
2021-10-20 11:56:57 +0300

    regenerate all files using  dts-gen

--- types/bunyan-syslog/index.d.ts
+++ types/bunyan-syslog/index.d.ts
@@ -1,49 +1,50 @@
-// Type definitions for bunyan-syslog
-// Project: https://github.com/joyent/node-bunyan-syslog
+// Type definitions for bunyan-syslog 0.3
+// Project: https://www.npmjs.com/package/bunyan-syslog
 // Definitions by: Naor Tedgi <https://github.com/ntedgi>
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
-/// <reference types="node" />
 
-import * as bunyan from "bunyan";
+declare namespace bsyslog {
+    type PrependNextNum<A extends unknown[]> = A['length'] extends infer T ? ((t: T, ...a: A) => void) extends ((...x: infer X) => void) ? X : never : never;
 
-type PrependNextNum<A extends unknown[]> = A['length'] extends infer T ? ((t: T, ...a: A) => void) extends ((...x: infer X) => void) ? X : never : never;
+    type EnumerateInternal<A extends unknown[], N extends number> = { 0: A, 1: EnumerateInternal<PrependNextNum<A>, N> }[N extends A['length'] ? 0 : 1];
 
-type EnumerateInternal<A extends unknown[], N extends number> = { 0: A, 1: EnumerateInternal<PrependNextNum<A>, N> }[N extends A['length'] ? 0 : 1];
+    type Enumerate<N extends number> = EnumerateInternal<[], N> extends Array<infer E> ? E : never;
 
-type Enumerate<N extends number> = EnumerateInternal<[], N> extends Array<infer E> ? E : never;
+    type Range<FROM extends number, TO extends number> = Exclude<Enumerate<TO>, Enumerate<FROM>>;
 
-type Range<FROM extends number, TO extends number> = Exclude<Enumerate<TO>, Enumerate<FROM>>;
+    type Facility = Range<0, 24>;
 
-export type Facility = Range<0, 24>;
+    type StreamType = | 'sys' | 'tcp' | 'udp';
 
-export type StreamType = | 'sys' | 'tcp' | 'udp';
+    const kern = 0;
+    const user = 1;
+    const mail = 2;
+    const daemon = 3;
+    const auth = 4;
+    const syslog = 5;
+    const lpr = 6;
+    const news = 7;
+    const uucp = 8;
+    const authpriv = 10;
+    const ftp = 11;
+    const cron = 15;
+    const local0 = 16;
+    const local1 = 17;
+    const local2 = 18;
+    const local3 = 19;
+    const local4 = 20;
+    const local5 = 21;
+    const local6 = 22;
+    const local7 = 23;
 
-export const kern = 0;
-export const user = 1;
-export const mail = 2;
-export const daemon = 3;
-export const auth = 4;
-export const syslog = 5;
-export const lpr = 6;
-export const news = 7;
-export const uucp = 8;
-export const authpriv = 10;
-export const ftp = 11;
-export const cron = 15;
-export const local0 = 16;
-export const local1 = 17;
-export const local2 = 18;
-export const local3 = 19;
-export const local4 = 20;
-export const local5 = 21;
-export const local6 = 22;
-export const local7 = 23;
+    interface StreamOptions {
+        type: StreamType;
+        facility: Facility;
+        host: string;
+        port: number;
+    }
 
-interface StreamOptions {
-    type: StreamType;
-    facility: Facility;
-    host: string;
-    port: number;
+    function createBunyanStream(options: StreamOptions): NodeJS.WritableStream;
 }
 
-export function createBunyanStream(options: StreamOptions): NodeJS.WritableStream;
+export default bsyslog;
