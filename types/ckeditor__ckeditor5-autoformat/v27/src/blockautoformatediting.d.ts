6271b0a82383a64d9bc48b3880fee2e914ae4b74
Federico Panico <federicopanico@gmail.com>
2021-10-22 15:28:01 -0300

    @ckeditor/ckeditor5-autoformat Update to v30

--- types/ckeditor__ckeditor5-autoformat/v27/src/blockautoformatediting.d.ts
+++
@@ -1,9 +0,0 @@
-import { Editor } from "@ckeditor/ckeditor5-core";
-import Autoformat from "./autoformat";
-
-export default function blockAutoformatEditing(
-    editor: Editor,
-    plugin: Autoformat,
-    pattern: RegExp,
-    callbackOrCommand: string | ((arg: ReturnType<RegExp["exec"]>) => void),
-): void;
