b673f58fb16ab377195421e466006ad142d38a4c
David Tanner <darthtanner@gmail.com>
2021-10-22 15:56:26 -0600

    Add types for pkginfo

---
+++ types/pkginfo/index.d.ts
@@ -0,0 +1,41 @@
+// Type definitions for pkginfo 0.4
+// Project: https://github.com/indexzero/node-pkginfo
+// Definitions by: David Tanner <https://github.com/DavidTanner>
+// Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
+
+declare namespace PkgInfo {
+    interface Options {
+        dir?: string;
+        include: string[];
+    }
+
+    interface FindResults {
+        dir?: string;
+        package: Record<string, any>;
+    }
+
+    interface PkgInfo {
+        (pmodule: NodeJS.Module, options?: Options | string[] | string, ...properties: string[]): PkgInfo;
+
+        //
+        // ### function find (dir)
+        // #### @pmodule {Module} Parent module to read from.
+        // #### @dir {string} **Optional** Directory to start search from.
+        // Searches up the directory tree from `dir` until it finds a directory
+        // which contains a `package.json` file.
+        //
+        read(
+          pmodule: NodeJS.Module,
+          dir?: string
+        ): FindResults;
+
+        find(
+          pmodule: NodeJS.Module,
+          dir?: string
+        ): Record<string, any>;
+    }
+}
+
+declare const pkgInfo: PkgInfo.PkgInfo & { version: string; };
+
+export = pkgInfo;
---
+++ types/pkginfo/pkginfo-tests.ts
@@ -0,0 +1,14 @@
+/// <reference types="node" />
+
+import pkginfo from 'pkginfo';
+
+pkginfo(module);
+pkginfo(module, 'version', 'author');
+pkginfo(module, ['version', 'author']);
+pkginfo(module, { include: [ 'version', 'author' ] });
+
+const pkg1 = pkginfo.find(module);
+const pkg2 = pkginfo.find(module, '.');
+
+const readRes1 = pkginfo.read(module);
+const readRes2 = pkginfo.read(module, '.');
