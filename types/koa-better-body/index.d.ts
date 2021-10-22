07d3ccc5908a47e0a72e76c343f3ba68296a2e43
David Tanner <darthtanner@gmail.com>
2021-10-22 12:50:18 -0600

    Test

--- types/koa-better-body/index.d.ts
+++ types/koa-better-body/index.d.ts
@@ -1,4 +1,4 @@
-// Type definitions for koa-better-body 3.3.9
+// Type definitions for koa-better-body 3.3
 // Project: https://github.com/tunnckoCore/koa-better-body
 // Definitions by: Daniel Byrne <https://github.com/danwbyrne>
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
@@ -6,12 +6,12 @@
 
 import { Context } from 'koa';
 
-declare function KoaBetterBody<Context>(
-  options?: KoaBetterBody.Options<Context>
+declare function KoaBetterBody(
+  options?: KoaBetterBody.Options,
 ): KoaBetterBody.Body;
 
 declare namespace KoaBetterBody {
-  interface Options<Context> {
+  interface Options {
     fields?: boolean | string;  // default: false
     files?: boolean | string;  // default: false
     multipart?: boolean;  // default: true
@@ -19,22 +19,22 @@ declare namespace KoaBetterBody {
     formLimit?: string;  // default: false
     jsonLimit?: string;  // default: false
     jsonStrict?: boolean;  // default: true
-    detectJSON?: boolean | ((ctx: Context) => boolean);
+    detectJSON?: (ctx: Context) => boolean;
     bufferLimit?: string;  // default: false
     buffer?: boolean;  // default: false
     strict?: boolean;  // default: true
 
     delimiter?: symbol; // default: '&'
-    decodeURIComponent?: typeof decodeURIComponent; // default: require('querystring').unescape
+    decodeURIComponent?: (query: string) => string; // default: require('querystring').unescape
     maxKeys?: number; // default: 1000
 
     urlencodedLimit?: string;
 
-    onError?: Function;
-    handler?: Function;
+    onError?: (err: any, ctx: Context) => void;
+    handler?: (ctx: Context, options: Options, next: () => any) => void;
   }
 
-  type Body = (context: Context, next: () => void) => Generator;
+  type Body = (next: any) => Generator;
 }
 
 export = KoaBetterBody;
