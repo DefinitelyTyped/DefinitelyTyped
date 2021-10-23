246c08d3eaeaf3b46c25f6bae23dfafe1a4782a4
Juzer Zarif <juzerapj@gmail.com>
2021-10-22 22:17:15 -0500

    add github link

--- types/cerner__terra-functional-testing/index.d.ts
+++ types/cerner__terra-functional-testing/index.d.ts
@@ -1,5 +1,5 @@
 // Type definitions for @cerner/terra-functional-testing 2.5
-// Project: https://github.com/baz/foo (Does not have to be to GitHub, but prefer linking to a source code repository rather than to a project website.)
+// Project: https://github.com/cerner/terra-toolkit/tree/main/packages/terra-functional-testing
 // Definitions by: Juzer Zarif <https://github.com/juzerzarif>
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
 
