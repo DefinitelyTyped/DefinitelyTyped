afb0d136078299b3f29f9458c8966d323c18799e
Dipu Zhang <81529060+dzhang-ssense@users.noreply.github.com>
2021-10-25 14:59:06 -0400

    Update Request to include rawBody property
    The `rawBody` property is a property which the restify team intend to support:
    https://github.com/restify/node-restify/issues/928
    https://github.com/restify/plugins/issues/6
    Having it available in the typing enables access to this property.

--- types/restify/index.d.ts
+++ types/restify/index.d.ts
@@ -637,6 +637,9 @@ export interface Request extends http.IncomingMessage {
 
     /** available when bodyParser plugin is used. */
     body?: any;
+    
+    /** available when bodyParser plugin is used. */
+    rawBody?: any;
 
     /** available when queryParser or bodyParser plugin is used with mapParams enabled. */
     params?: any;
