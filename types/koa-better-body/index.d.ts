1dfba40407aa039e507eda5deb4b3721ac5376b2
David Tanner <darthtanner@gmail.com>
2021-10-25 10:14:29 -0600

    Implement feedback, and take ownership.  More test definition

--- types/koa-better-body/index.d.ts
+++ types/koa-better-body/index.d.ts
@@ -1,6 +1,6 @@
 // Type definitions for koa-better-body 3.3
 // Project: https://github.com/tunnckoCore/opensource/tree/master/%40packages/koa-better-body
-// Definitions by: Daniel Byrne <https://github.com/danwbyrne>
+// Definitions by: David Tanner <https://github.com/DavidTanner>
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
 
 import { Context } from 'koa';
@@ -11,25 +11,83 @@ declare function KoaBetterBody(
 
 declare namespace KoaBetterBody {
   interface Options {
-    fields?: boolean | string;  // default: false
-    files?: boolean | string;  // default: false
-    multipart?: boolean;  // default: true
-    textLimit?: string;  // default: false
-    formLimit?: string;  // default: false
-    jsonLimit?: string;  // default: false
-    jsonStrict?: boolean;  // default: true
+    /**
+     * @default false
+     */
+    fields?: boolean | string;
+    /**
+     * @default false
+     */
+    files?: boolean | string;
+    /**
+     * @default true
+     */
+    multipart?: boolean;
+    /**
+     * @default false
+     */
+    textLimit?: string;
+    /**
+     * @default false
+     */
+    formLimit?: string;
+    /**
+     * @default false
+     */
+    jsonLimit?: string;
+    /**
+     * @default true
+     */
+    jsonStrict?: boolean;
+    /**
+     * @default () => false
+     */
     detectJSON?: (ctx: Context) => boolean;
-    bufferLimit?: string;  // default: false
-    buffer?: boolean;  // default: false
-    strict?: boolean;  // default: true
+    /**
+     * @default false
+     */
+    bufferLimit?: string;
+    /**
+     * @default false
+     */
+    buffer?: boolean;
+    /**
+     * @default true
+     */
+    strict?: boolean;
 
-    delimiter?: symbol; // default: '&'
+    /**
+     * @default '&'
+     */
+    delimiter?: string; // default: '&'
+    /**
+     * @default require('querystring').unescape
+     */
     decodeURIComponent?: (query: string) => string; // default: require('querystring').unescape
+    /**
+     * @default 1000
+     */
     maxKeys?: number; // default: 1000
 
+    /**
+     * @deprecated use formLimit instead
+     * @default config.formLimit
+     */
     urlencodedLimit?: string;
 
+    /**
+     * @deprecated use delimiter instead
+     * @default config.delimiter
+     */
+    sep?: string;
+
+    /**
+     * @default undefined
+     */
     onError?: (err: any, ctx: Context) => void;
+    /**
+     * @default undefined
+     */
     handler?: (ctx: Context, options: Options, next: () => any) => void;
   }
 
