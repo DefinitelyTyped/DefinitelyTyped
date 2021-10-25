507ba988383cf65b6b9d3155e4dd9f750d4da8e7
Piotr Błażejewicz (Peter Blazejewicz) <peterblazejewicz@users.noreply.github.com>
2021-10-25 19:11:09 +0200

    Update types/koa-better-body/koa-better-body-tests.ts
    Fix CI (missing semmicolon)

--- types/koa-better-body/koa-better-body-tests.ts
+++ types/koa-better-body/koa-better-body-tests.ts
@@ -2,7 +2,7 @@ import body = require('koa-better-body');
 
 // $ExpectType Body
 const parser1: body.Body = body();
-typeof parser1 === 'function'
+typeof parser1 === 'function';
 
 const testOptions: body.Options = {
   jsonLimit: '100mb',
