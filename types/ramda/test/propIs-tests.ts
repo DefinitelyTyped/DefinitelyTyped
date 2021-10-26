93095e1acb35479e77580c350f7baa02c6b65052
Nikita Balikhin <balikhin.n.ev@sberbank.ru>
2021-10-26 23:57:43 +0300

    [ramda] type guards implemented

--- types/ramda/test/propIs-tests.ts
+++ types/ramda/test/propIs-tests.ts
@@ -8,3 +8,45 @@ import * as R from 'ramda';
   R.propIs(Number, 'x', { x: 'foo' }); // => false
   R.propIs(Number, 'x', {}); // => false
 };
+
+() => {
+  const obj = {};
+
+  if (R.propIs(Number, "x", obj)) {
+    const number: number = obj.x;
+  }
+
+  if (R.propIs(Number, "x")(obj)) {
+    const number: number = obj.x;
+  }
+
+  if (R.propIs(Number)("x", obj)) {
+    const number: number = obj.x;
+  }
+
+  if (R.propIs(Number)("x")(obj)) {
+    const number: number = obj.x;
+  }
+};
+
+() => {
+  class Foo {}
+
+  const obj = {};
+
+  if (R.propIs(Foo, "foo", obj)) {
+    const foo: Foo = obj.foo;
+  }
+
+  if (R.propIs(Foo, "foo")(obj)) {
+    const foo: Foo = obj.foo;
+  }
+
+  if (R.propIs(Foo)("foo", obj)) {
+    const foo: Foo = obj.foo;
+  }
+
+  if (R.propIs(Foo)("foo")(obj)) {
+    const foo: Foo = obj.foo;
+  }
+};
