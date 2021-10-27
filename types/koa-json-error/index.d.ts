c091a580a754a8750e75619204e07092e8ddb9a7
ʀᴀʏ <nn_201312@163.com>
2021-10-27 21:28:33 +0800

    Correct the error type definition of koa-json-error

--- types/koa-json-error/index.d.ts
+++ types/koa-json-error/index.d.ts
@@ -6,21 +6,23 @@
 
 import * as Koa from "koa";
 
+type JSONError = Error & { status: number };
+
 interface JSONErrorOptions {
     /**
      * Perform some task before calling `options.format`. Must be a function with the original err as its only argument.
      */
-    preFormat?(err: Error): any;
+    preFormat?(err: JSONError): any;
 
     /**
      * Runs inmediatly after `options.preFormat`. It receives two arguments: the original `err` and the output of     `options.preFormat`. It should `return` a newly formatted error.
      */
-    format?(err: Error, obj: any): any;
+    format?(err: JSONError, obj: any): any;
 
     /**
      * Runs inmediatly after `options.format`. It receives two arguments: the original `err` and the output of   `options.format`.   It should `return` a newly formatted error.
      */
-    postFormat?(err: Error, obj: any): any;
+    postFormat?(err: JSONError, obj: any): any;
 }
 
 /**
