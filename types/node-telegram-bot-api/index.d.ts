e9870d53fa9b4664cdb1c4d501b9108f6b12b2fa
Dmitry Kochin <dco@mail.ru>
2021-10-29 12:13:23 +0300

    removed unnecessary comment to make the code clear

--- types/node-telegram-bot-api/index.d.ts
+++ types/node-telegram-bot-api/index.d.ts
@@ -76,7 +76,6 @@ declare namespace TelegramBot {
         'chat_invite_link' |
         'chat_member_updated';
 
-    // From https://core.telegram.org/bots/api#messageentity
     type MessageEntityType = 'mention' | 'hashtag' | 'cashtag' | 'bot_command' | 'url' | 'email' | 'phone_number' |
         'bold' | 'italic' | 'underline' | 'strikethrough' | 'code' | 'pre' | 'text_link' | 'text_mention';
 
