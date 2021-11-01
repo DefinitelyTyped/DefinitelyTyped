7dcf10b67205dc0e2ae67b8ca7a9722c6adca793
eps1lon <silbermann.sebastian@gmail.com>
2021-11-01 23:28:01 +0100

    [react] useOpaqueIdentifier -> useId

--- types/react/next.d.ts
+++ types/react/next.d.ts
@@ -151,24 +151,7 @@ declare module '.' {
      */
     export function startTransition(scope: TransitionFunction): void;
 
-    const opaqueIdentifierBranding: unique symbol;
-    /**
-     * WARNING: Don't use this as a `string`.
-     *
-     * This is an opaque type that is not supposed to type-check structurally.
-     * It is only valid if returned from React methods and passed to React e.g. `<button aria-labelledby={opaqueIdentifier} />`
-     */
-    // We can't create a type that would be rejected for string concatenation or `.toString()` calls.
-    // So in order to not have to add `string | OpaqueIdentifier` to every react-dom host prop we intersect it with `string`.
-    type OpaqueIdentifier = string & {
-        readonly [opaqueIdentifierBranding]: unknown;
-        // While this would cause `const stringified: string = opaqueIdentifier.toString()` to not type-check it also adds completions while typing.
-        // It would also still allow string concatenation.
-        // Unsure which is better. Not type-checking or not suggesting.
-        // toString(): void;
-    };
-
-    export function unstable_useOpaqueIdentifier(): OpaqueIdentifier;
+    export function useId(): string;
 
     /**
      * this should be an internal type
