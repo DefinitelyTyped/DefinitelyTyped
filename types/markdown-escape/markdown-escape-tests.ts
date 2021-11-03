c812d17557d09a0984988e6ac60b3a2b9fa63800
Christoph Thiede <christoph.thiede@outlook.de>
2021-11-03 22:14:49 +0000

    Fix imports

--- types/markdown-escape/markdown-escape-tests.ts
+++ types/markdown-escape/markdown-escape-tests.ts
@@ -1,5 +1,4 @@
 import * as markdownEscape from 'markdown-escape';
-import { CharacterSet } from 'markdown-escape';
 
 // $ExpectType string
 markdownEscape("#1! We're #1!");
@@ -7,5 +6,5 @@ markdownEscape("#1! We're #1!");
 // $ExpectType string
 markdownEscape('one (1)', ['parentheses']);
 
-const characterSet: CharacterSet = 'asterisks';
+const characterSet: markdownEscape.CharacterSet = 'asterisks';
 const string: string = characterSet;
