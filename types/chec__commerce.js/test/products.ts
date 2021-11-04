412225801e20fb5879a6a85ac7654fc8098f5a19
kvisca <kyle@viscadesign.ca>
2021-11-04 12:31:18 -0400

    Add attributes types

--- types/chec__commerce.js/test/products.ts
+++ types/chec__commerce.js/test/products.ts
@@ -173,6 +173,14 @@ const products: Products.ProductCollection = {
                 created_at: 1594413988,
                 updated_at: 1605921891,
             },
+            attributes: [
+                {
+                    id: 'attr_mL1vOoZW5Ra8Ze',
+                    meta: [],
+                    name: 'Weight',
+                    value: '2grams',
+                },
+            ],
             related_products: [],
         },
     ],
---
+++ types/chec__commerce.js/types/product-attribute-option.ts
@@ -0,0 +1,4 @@
+export interface ProductAttributeOption {
+    label: string;
+    key: string;
+}
