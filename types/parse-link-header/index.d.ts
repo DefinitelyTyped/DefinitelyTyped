e2626bd9ec3ce11af0ae5728405c653555c73e6e
Tomasz Pluskiewicz <tpluscode@users.noreply.github.com>
2021-11-04 18:07:50 +0100

    Update index.d.ts

--- types/parse-link-header/index.d.ts
+++ types/parse-link-header/index.d.ts
@@ -1,7 +1,6 @@
 // Type definitions for parse-link-header 1.0
 // Project: https://github.com/thlorenz/parse-link-header
-// Definitions by: Nick Zelei <https://github.com/zelein>
-//                 tpluscode <https://github.com/tpluscode>
+// Definitions by: Nick Zelei <https://github.com/nickzelei>
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
 
 declare namespace parseLinkHeader {
