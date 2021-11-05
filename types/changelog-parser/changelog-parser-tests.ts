5e2e6b56dd4b9bc2f16c4b5d8a059c2a23fba91a
Oliver Steele <steele@osteele.com>
2021-11-05 14:53:11 +0800

    [changelog-parser] Add missing option parameter (text); add interface for result type

--- types/changelog-parser/changelog-parser-tests.ts
+++ types/changelog-parser/changelog-parser-tests.ts
@@ -1,13 +1,33 @@
 import parseChangelog = require('changelog-parser');
 
+// Because the Changelog type is not exported, we need to inspect the
+// `parseChangelog` function to extract the type.
+//
+// The Changelog type is not exported because changelog-parser exports a default
+// export. The type declaration must therefore also export a default type, so it
+// is not possible for it to export additional types.
+type Changelog = Parameters<NonNullable<Parameters<typeof parseChangelog>[1]>>[1];
+
 const options = {
     filePath: 'path/to/CHANGELOG.md',
-    removeMarkdown: false
+    removeMarkdown: false,
 };
 
-const fn = (obj: object): void => {};
+const fn = (obj: Changelog): void => {
+    // verify that the changelog has the documented properties
+    const title: string = obj.title;
+    const description: string = obj.description;
 
-parseChangelog({filePath: 'path/to/CHANGELOG.md'}, (error, result) => {
+    const version = obj.versions[0];
+    const versionString: string | null = version.version;
+    const versionTitle: string = version.title;
+    const date: string | null = version.date;
+    const body: string = version.body;
+    const parsed: string[] = version.parsed['Fixes'];
+};
+
+// The callback should receive an object that contains the documented properties.
+parseChangelog({ filePath: 'path/to/CHANGELOG.md' }, (error, result) => {
     if (error) {
         throw error;
     }
@@ -15,10 +35,26 @@ parseChangelog({filePath: 'path/to/CHANGELOG.md'}, (error, result) => {
     fn(result);
 });
 
-parseChangelog(options, (error, result) => {});
+// The function should accept the documented options.
+parseChangelog(options, (error, result) => {
+    const title: string = result.title;
+});
 
-parseChangelog({filePath: 'path/to/CHANGELOG.md'}, (error) => {});
+parseChangelog({ filePath: 'path/to/CHANGELOG.md' }, error => {});
 
-parseChangelog(options).then((result) => {});
+// The function should return a promise.
+parseChangelog(options).then(result => {
+    const title: string = result.title;
+});
 
-parseChangelog('path/to/CHANGELOG.md').then((result) => {});
+// The first argument may be a string, in which case it is treated as the file
+// path.
+parseChangelog('path/to/CHANGELOG.md').then(result => {
+    const title: string = result.title;
+});
+
+// The options argument may contain a `text` property instead of a `filePath`
+// property..
+parseChangelog({ text: '# Change Log' }).then(result => {
+    const title: string = result.title;
+});
--- types/changelog-parser/index.d.ts
+++ types/changelog-parser/index.d.ts
@@ -1,14 +1,33 @@
-// Type definitions for changelog-parser 2.7
+// Type definitions for changelog-parser 2.8
 // Project: https://github.com/hypermodules/changelog-parser
-// Definitions by:  Adam Zerella <https://github.com/adamzerella>
+// Definitions by: Adam Zerella <https://github.com/adamzerella>
+//                 Oliver Steele <https://github.com/osteele>
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
-// TypeScript Version: 2.2
+// TypeScript Version: 4.4
 
+// Options must contain exactly one of filePath or text. This can be expressed
+// with a type, but not an interface. (See
+// <https://github.com/osteele/vscode-p5server/blob/fee241c06f/scripts/types/changelog-parser.d.ts>,
+// which contains the ambient interface that this type was extracted from.)
+//
+// The DefinitelyTyped lint settings don't allow type literals, so define an
+// interface here, and use it to construct a type expression in the argument.
+
+// This type can't be exported, because the module interface has a default
+// export.
+//
+// To use the type of this interface in your code, import `parseChangelog` and
+// define:
+//   type Options = Exclude<Parameters<typeof parseChangelog>[0], string>;
 interface Options {
     /**
      * Path to changelog file.
      */
     filePath: string;
+    /**
+     * Text of changelog file (you can use this instead of filePath).
+     */
+    text: string;
     /**
      * Removes the markdown markup from the changelog entries by default.
      * You can change its value to false to keep the markdown.
@@ -16,10 +35,34 @@ interface Options {
     removeMarkdown: boolean;
 }
 
+// This type can't be exported, because the module interface has a default
+// export.
+//
+// To use the type of this interface in your code, import `parseChangelog` and
+// define:
+//   type Changelog = Parameters<NonNullable<Parameters<typeof parseChangelog>[1]>>[1];
+interface Changelog {
+    title: string;
+    description: string;
+    versions: Array<{
+        version: string | null;
+        title: string;
+        date: string | null;
+        body: string;
+        parsed: Record<string, string[]>;
+    }>;
+}
+
 /**
  * Change log parser for node.
  */
-declare function parseChangelog(options: Partial<Options>|string,
-    callback?: (error: string|null, result: object) => void): Promise<object>;
+// The implementation returns a Promise whether or not a callback is specified.
+// This type declaration reflects this.
+declare function parseChangelog(
+    options:
+        | (Partial<Exclude<Options, 'filePath' | 'text'>> & (Pick<Options, 'filePath'> | Pick<Options, 'text'>))
+        | string,
+    callback?: (error: string | null, result: Changelog) => void,
+): Promise<Changelog>;
 
 export = parseChangelog;
