99e52959c85a4670e49ba4a6170d049bbaec9a21
Chris Midgley <chris.midgley@dunecomputers.co.uk>
2021-11-05 23:12:52 +0000

    fix version number

--- types/remote-git-tags/index.d.ts
+++ types/remote-git-tags/index.d.ts
@@ -1,4 +1,4 @@
-// Type definitions for remote-git-tags 4.0.0
+// Type definitions for remote-git-tags 4.0
 // Project: https://github.com/sindresorhus/remote-git-tags
 // Definitions by: Chris Midgley <https://github.com/midgleyc>
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
