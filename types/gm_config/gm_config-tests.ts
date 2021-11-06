0258d3ae93d45d0385c513bae4353be5e818dd3e
Adam Thompson-Sharpe <adamthompsonsharpe@gmail.com>
2021-11-06 17:47:06 -0400

    Add more comprehensive tests

--- types/gm_config/gm_config-tests.ts
+++ types/gm_config/gm_config-tests.ts
@@ -1,4 +1,4 @@
-// Adapted from https://github.com/sizzlemctwizzle/GM_config/wiki/#customizing-gm_config
+// Testing GM_config (and by extension GM_configStruct)
 GM_config.init({
     id: 'MyConfig',
     title: 'Script Settings',
@@ -12,13 +12,65 @@ GM_config.init({
     css: '#MyConfig_section_0 { display: none !important; }',
 });
 
-GM_config.open();
+new GM_configStruct({
+    id: 'MyConfig',
+    title: 'Script Settings',
+    fields: {
+        Name: {
+            label: 'Name',
+            type: 'text',
+            default: 'DefinitelyTyped',
+        },
+    },
+    css: '#MyConfig_section_0 { display: none !important; }',
+});
+
+GM_config.open(); // $ExpectType void
+GM_config.close(); // $ExpectType void
 
 const username = GM_config.get('Name');
-GM_config.set('Name', '@types/gm_config');
+GM_config.set('Name', '@types/gm_config'); // $ExpectType void
+
+GM_config.save(); // $ExpectType void
+
+GM_config.create(); // $ExpectType HTMLElement
+
+GM_config.center(); // $ExpectType void
+
+GM_config.remove(document.createElement('a')); // $ExpectType void
+
+GM_config.isGM; // $ExpectType boolean
+
+GM_config.setValue('Name', 'DefinitelyTyped'); // $ExpectType void | Promise<void>
+
+GM_config.stringify({ message: 'Hello, World!' }); // $ExpectType string
+
+GM_config.parser('{"message":"Hello, World!"}'); // $ExpectType void | object
+
+GM_config.log('Hello, World!'); // $ExpectType void
+
+GM_config.id; // $ExpectType string
+
+GM_config.title; // $ExpectType string
+
+GM_config.css.basic; // $ExpectType string[]
+GM_config.css.basicPrefix; // $ExpectType string
+GM_config.css.stylish; // $ExpectType string
+
+GM_config.frame; // $ExpectType HTMLElement | undefined
+
+GM_config.fields; // $ExpectType Record<string, Field<never>>
+
+GM_config.onInit; // $ExpectType (() => void) | undefined
+GM_config.onOpen; // $ExpectType ((document: Document, window: Window, frame: HTMLElement) => void) | undefined
+GM_config.onSave; // $ExpectType (() => void) | undefined
+GM_config.onClose; // $ExpectType (() => void) | undefined
+GM_config.onReset; // $ExpectType (() => void) | undefined
+
+GM_config.isOpen; // $ExpectType boolean
 
 // Testing GM_configField
-const someField = new GM_configField(
+const myField = new GM_configField(
     {
         label: 'My Field',
         type: 'text',
@@ -30,11 +82,29 @@ const someField = new GM_configField(
     'MyConfig',
 );
 
-document.body.appendChild(someField.toNode());
-GM_config.set('Name', someField.toValue()); // $ExpectError
+myField.settings; // $ExpectType Field<never>
 
-const value = someField.toValue();
-if (value) GM_config.set('Name', value);
+myField.id; // $ExpectType string
+
+myField.configId; // $ExpectType string
+
+myField.node; // $ExpectType Node | null
+
+myField.wrapper; // $ExpectType Node | null
+
+myField.save; // $ExpectType boolean
+
+myField.create(); // $ExpectType HTMLElement
+
+myField.toNode(); // $ExpectType Node
+
+myField.reset(); // $ExpectType void
+
+myField.remove(); // $ExpectType void
+
+myField.reload(); // $ExpectType void
+
+myField._checkNumberRange(42, '/!\\'); // $ExpectType true | null
 
 // Testing custom types
 const myType: CustomType = {
