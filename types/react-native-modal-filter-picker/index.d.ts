49e7a3f69c5de5666de931d1f085366801202605
vitalii-t <vitali.t.dev@gmail.com>
2021-11-14 16:22:26 +0200

    Update version

--- types/react-native-modal-filter-picker/index.d.ts
+++ types/react-native-modal-filter-picker/index.d.ts
@@ -1,4 +1,4 @@
-// Type definitions for react-native-modal-filter-picker 1.3
+// Type definitions for react-native-modal-filter-picker 2.1.0
 // Project: https://github.com/hiddentao/react-native-modal-filter-picker#readme
 // Definitions by: Chang Yanwei <https://github.com/ywchang>
 //                 Cheng Gibson <https://github.com/nossbigg>
