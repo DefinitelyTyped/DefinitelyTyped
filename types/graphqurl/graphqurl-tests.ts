a98ac077423330c6480b6a6eaf9e5d78fc05f1d9
Joseph Cagle <accounts@josephcagle.com>
2021-11-14 20:38:31 -0500

    Make sure graphqurl-tests.ts uses 4 spaces indent

--- types/graphqurl/graphqurl-tests.ts
+++ types/graphqurl/graphqurl-tests.ts
@@ -2,16 +2,16 @@ import { createClient } from 'graphqurl';
 
 // $ExpectType Client
 createClient({
-  endpoint: 'https://my-graphql-endpoint/graphql',
-  headers: {
-    Authorization: 'Bearer <token>'
-  },
-  websocket: {
     endpoint: 'https://my-graphql-endpoint/graphql',
-    shouldRetry: false,
-    parameters: { someData: 'abc123' },
-    onConnectionSuccess: () => {  },
-    onConnectionError: err => {  },
-    onConnectionKeepAlive: () => {  }
-  }
+    headers: {
+        Authorization: 'Bearer <token>'
+    },
+    websocket: {
+        endpoint: 'https://my-graphql-endpoint/graphql',
+        shouldRetry: false,
+        parameters: { someData: 'abc123' },
+        onConnectionSuccess: () => {  },
+        onConnectionError: err => {  },
+        onConnectionKeepAlive: () => {  }
+    }
 });
