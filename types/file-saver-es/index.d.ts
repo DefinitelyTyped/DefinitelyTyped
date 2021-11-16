329d0b5420c3d661b246f09b43c9fffb9ec3a82b
Dimitri B <dimitri.benin@gmail.com>
2021-11-16 11:12:01 +0100

    [file-saver-es] Fix project path

--- types/file-saver-es/index.d.ts
+++ types/file-saver-es/index.d.ts
@@ -1,5 +1,5 @@
 // Type definitions for file-saver-es 2.0
-// Project: https://github.com/eligrey/FileSaver.js/
+// Project: https://github.com/wobkenh/FileSaver.js
 // Definitions by: Cyril Schumacher <https://github.com/cyrilschumacher>
 //                 Daniel Roth <https://github.com/DaIgeb>
 //                 Chris Barr <https://github.com/chrismbarr>
