bbd45d68444adc98cfc79596dfa51e2599bd5c2d
Piotr Błażejewicz <peterblazejewicz@users.noreply.github.com>
2021-11-16 18:25:01 +0100

    prune(openfin): remove source for older versions
    This change intent is to ease CPU workload required to pass through any
    Node ecosystem PR on DT, which started to last for non acceptable time,
    hours on local dev machine (+30 minutes on CI).
    Removing older version of declaration files (already published to NPM),
    according to matrix from release notes:
    https://cdn.openfin.co/versions/?product=Runtime
    
    @chrisbarker for review
    
    Thanks!

--- types/openfin/v45/_v2/launcher/win-launch.d.ts
+++
@@ -1,4 +0,0 @@
-/// <reference types="node" />
-import { ChildProcess } from 'child_process';
-import { ConfigWithRuntime } from '../transport/wire';
-export default function launch(config: ConfigWithRuntime, manifestLocation: string, namedPipeName: string): Promise<ChildProcess>;
