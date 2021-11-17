2ded5a68e9e493f40f548143d24e5e4e4ef3a268
Paulo Moreno <paulocmoreno@gmail.com>
2021-11-17 09:24:45 -0300

    feat: update @nginstack/orm definitions

--- types/nginstack__orm/nginstack__orm-tests.ts
+++ types/nginstack__orm/nginstack__orm-tests.ts
@@ -25,4 +25,4 @@ entity.toJSONSchema(); // $ExpectType any
 function testMajorVersions(prior: number, current: number): boolean {
     return current > prior;
 }
-testMajorVersions(52, 53); // $ExpectType boolean
+testMajorVersions(53, 54); // $ExpectType boolean
