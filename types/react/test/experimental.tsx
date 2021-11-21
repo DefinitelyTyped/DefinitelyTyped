317faec0cdfd5ac6c908eb734a0eabc8838c6b45
eps1lon <silbermann.sebastian@gmail.com>
2021-11-21 17:28:51 +0100

    [react] SuspenseList was moved to experimental
    https://github.com/facebook/react/pull/22765

--- types/react/test/experimental.tsx
+++ types/react/test/experimental.tsx
@@ -51,3 +51,24 @@ function useExperimentalHooks() {
         return () => {};
     }, [toggle]);
 }
+
+// Unsupported `revealOrder` triggers a runtime warning
+// $ExpectError
+<React.SuspenseList revealOrder="something">
+    <React.Suspense fallback="Loading">Content</React.Suspense>
+</React.SuspenseList>;
+
+<React.SuspenseList revealOrder="backwards">
+    <React.Suspense fallback="Loading">A</React.Suspense>
+    <React.Suspense fallback="Loading">B</React.Suspense>
+</React.SuspenseList>;
+
+<React.SuspenseList revealOrder="forwards">
+    <React.Suspense fallback="Loading">A</React.Suspense>
+    <React.Suspense fallback="Loading">B</React.Suspense>
+</React.SuspenseList>;
+
+<React.SuspenseList revealOrder="together">
+    <React.Suspense fallback="Loading">A</React.Suspense>
+    <React.Suspense fallback="Loading">B</React.Suspense>
+</React.SuspenseList>;
