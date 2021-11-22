2e2cf29f9784c9e66c75c71af7e5f4ea4a04409a
Francis Gulotta <wizard@roborooter.com>
2021-11-22 10:02:33 -0500

    architect/functions websocket functions

--- types/architect__functions/test/web-socket-tests.ts
+++ types/architect__functions/test/web-socket-tests.ts
@@ -1,7 +1,13 @@
 import * as arc from '@architect/functions';
 
-function test() {
-    arc.ws.send({ id: 'anything', payload: { a: 4 }});
+async function test() {
+    await arc.ws.send({ id: 'anything', payload: { a: 4 }});
+    arc.ws.send({ id: 'anything', payload: { a: 4 }}, err => {});
+    await arc.ws.info({ id: 'anything'});
+    arc.ws.info({ id: 'anything'}, (err, data) => {});
+    await arc.ws.close({ id: 'anything'});
+    arc.ws.close({ id: 'anything'}, (err) => {});
+    console.log(arc.ws._api);
 }
 
 test();
--- types/architect__functions/web-socket.d.ts
+++ types/architect__functions/web-socket.d.ts
@@ -1,3 +1,12 @@
+import { ApiGatewayManagementApi } from 'aws-sdk';
+import { GetConnectionResponse } from "aws-sdk/clients/apigatewaymanagementapi";
+
 export interface ArcWebSocket {
-    send({ id, payload}: { id: string, payload: any}): Promise<void>;
+    send({ id, payload }: { id: string, payload: any }): Promise<void>;
+    send({ id, payload }: { id: string, payload: any }, callback: (error?: Error) => void): void;
+    close({ id }: { id: string }): Promise<void>;
+    close({ id }: { id: string }, callback: (error?: Error) => void): void;
+    info({ id }: { id: string }): Promise<GetConnectionResponse>;
+    info({ id }: { id: string }, callback: (error: Error | undefined, data: GetConnectionResponse) => void): void;
+    _api: ApiGatewayManagementApi;
 }
