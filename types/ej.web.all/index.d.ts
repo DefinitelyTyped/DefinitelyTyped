a1104ad6ab99cd5f3aec46725527e9c934c29a52
Syncfusion-JavaScript <Syncfusion-JavaScript@users.noreply.github.com>
2021-11-24 00:15:46 +0530

    syncfusion ej.web.all 19.3.53 added (#57120)
    * 19.3.53 added
    
    * Convert to UTF-8

--- types/ej.web.all/index.d.ts
+++ types/ej.web.all/index.d.ts
@@ -8,7 +8,7 @@
 
 /*!
 *  filename: ej.web.all.d.ts
-*  version : 19.3.0.43
+*  version : 19.3.0.53
 *  Copyright Syncfusion Inc. 2001 - 2021. All rights reserved.
 *  Use of this code is subject to the terms of our license.
 *  A copy of the current license can be obtained at any time by e-mailing
