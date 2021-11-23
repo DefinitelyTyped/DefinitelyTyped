ea30bf2ad1d9c830016bd3122a35bad843fa31ca
Brad Zacher <brad.zacher@gmail.com>
2021-11-23 15:56:25 -0800

    Update index.d.ts

--- types/react-mdl/index.d.ts
+++ types/react-mdl/index.d.ts
@@ -1,6 +1,6 @@
 // Type definitions for react-mdl 1.7.0
 // Project: https://github.com/tleunen/react-mdl
-// Definitions by: 
+// Definitions by: DefinitelyTyped <https://github.com/DefinitelyTyped>
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
 // TypeScript Version: 2.8
 
