ae7758656a4c74d068cba12f5c83d128b0234808
Brad Zacher <brad.zacher@gmail.com>
2021-11-23 15:30:43 -0800

    [react-mdl] remove me from the maintainer list
    I haven't touched or used this library in many, many years.
    I should not be listed as a maintainer

--- types/react-mdl/index.d.ts
+++ types/react-mdl/index.d.ts
@@ -1,6 +1,5 @@
 // Type definitions for react-mdl 1.7.0
 // Project: https://github.com/tleunen/react-mdl
-// Definitions by: Brad Zacher <https://github.com/bradzacher>
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
 // TypeScript Version: 2.8
 
