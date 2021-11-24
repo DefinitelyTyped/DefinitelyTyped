8dae47801e91c9008d11c77a9b94f2fbc5481b0f
wangzengdi <wangzengdi@meituan.com>
2021-11-24 19:25:41 +0800

    feat: comment must start with a space

--- types/ramda/test/unless-tests.ts
+++ types/ramda/test/unless-tests.ts
@@ -6,11 +6,12 @@ import * as R from 'ramda';
   const a: number[] = coerceArray([1, 2, 3]); // => [1, 2, 3]
   const b: number[] = coerceArray(1); // => [1]
 
+  // bodyTemperature :: (number) -> number | string
   const bodyTemperature = R.unless<number, string>(
     R.chain(R.equals, R.clamp(36.5, 37.5)),
     t => `abnormal: ${t}`
   );
 
-  const normal = bodyTemperature(37);   //=> 37
-  const abnormal = bodyTemperature(38); //=> 'abnormal: 38'
+  const normal = bodyTemperature(37); // => 37
+  const abnormal = bodyTemperature(38); // => 'abnormal: 38'
 };
