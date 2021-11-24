ba1389db8cc290aff41d35c7cde380edcdffcac9
wangzengdi <wangzengdi@meituan.com>
2021-11-24 09:57:40 +0800

    feat: add unless test

--- types/ramda/test/unless-tests.ts
+++ types/ramda/test/unless-tests.ts
@@ -5,4 +5,12 @@ import * as R from 'ramda';
   const coerceArray = R.unless(R.is(Array), R.of);
   const a: number[] = coerceArray([1, 2, 3]); // => [1, 2, 3]
   const b: number[] = coerceArray(1); // => [1]
+
+  const addOneIfNotNil = R.unless(
+    R.isNil,
+    R.add(1)
+  );
+
+  const nil: undefined = addOneIfNotNil(undefined);
+  const two: number = addOneIfNotNil(1);
 };
