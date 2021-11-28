16b19ae13f27fdd7da785b4ba4b9737c1411fecb
Remco Haszing <remcohaszing@gmail.com>
2021-11-28 21:48:15 +0100

    Add myself to postcss-url definition authors

--- types/postcss-url/index.d.ts
+++ types/postcss-url/index.d.ts
@@ -1,6 +1,7 @@
 // Type definitions for postcss-url 10.0
 // Project: https://github.com/postcss/postcss-url
 // Definitions by: Silas Rech <https://github.com/lenovouser>
+//                 Remco Haszing <https://github.com/remcohaszing>
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
 // TypeScript Version: 2.8
 
