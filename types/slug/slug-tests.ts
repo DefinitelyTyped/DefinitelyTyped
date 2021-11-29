ac4fdf8a1a0387f2cc93431cae70a6e8f7407460
Eddie Herbert <abeh78@gmail.com>
2021-11-29 15:30:42 -0500

    Update slug-tests.ts

--- types/slug/slug-tests.ts
+++ types/slug/slug-tests.ts
@@ -19,6 +19,7 @@ slug("string", {}); // $ExpectType string
 
 slug("string", "replacement"); // $ExpectType string
 
+// eslint-disable-next-line no-trailing-spaces
 slug("string something something ", { trim: false }); // $ExpectType string 
 
 slug.defaults.mode = "pretty";
