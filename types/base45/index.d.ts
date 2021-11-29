5982e1baa3176984e2bbf1a1939bf1dde8d0a195
Kyℓe Hensel <k-yle@users.noreply.github.com>
2021-11-29 14:56:46 +1300

    Update types/base45/index.d.ts
    Co-authored-by: Gareth Jones <Jones258@Gmail.com>

--- types/base45/index.d.ts
+++ types/base45/index.d.ts
@@ -1,5 +1,5 @@
 // Type definitions for base45 2.0
-// Project: https://github.com/baz/foo (Does not have to be to GitHub, but prefer linking to a source code repository rather than to a project website.)
+// Project: https://github.com/irony/base45
 // Definitions by: Kyle Hensel <https://github.com/k-yle>
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
 
