0fdf321f744ad1b556dffe75ae1c527e95f98ff7
Eric NICOLAS <ccjmne@gmail.com>
2021-11-30 23:23:42 +0100

    Update types/color-string/color-string-tests.ts
    Co-authored-by: Dimitri B. <BendingBender@users.noreply.github.com>

--- types/color-string/color-string-tests.ts
+++ types/color-string/color-string-tests.ts
@@ -21,12 +21,20 @@ color = colorString.get.hwb('hwb(60, 3%, 60%, 0.6)');
 
 color = colorString.get.rgb('invalid color string');
 
-function testNullableInput(input: string | null) {
-    color = colorString.get.rgb(input);
-}
+colorString.get.rgb(null); // $ExpectType null
+colorString.get.rgb('#FFF'); // $ExpectType Color
+colorString.get.rgb(null as string | null); // $ExpectType Color | null
+colorString.get.rgb(); // $ExpectError
 
-// $ExpectError
-color = colorString.get.rgb();
+colorString.get.hwb(null); // $ExpectType null
+colorString.get.hwb('hwb(60, 3%, 60%)'); // $ExpectType Color
+colorString.get.hwb(null as string | null); // $ExpectType Color | null
+colorString.get.hwb(); // $ExpectError
+
+colorString.get.hsl(null); // $ExpectType null
+colorString.get.hsl('hsl(360, 100%, 50%)'); // $ExpectType Color
+colorString.get.hsl(null as string | null); // $ExpectType Color | null
+colorString.get.hsl(); // $ExpectError
 
 let stringifiedColor: string;
 stringifiedColor = colorString.to.hex([255, 255, 255]);
