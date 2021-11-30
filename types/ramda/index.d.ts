21b2312191be31ba18f0a0ca41eb465db99a2e8f
wangzengdi <wangzengdi@meituan.com>
2021-11-30 18:32:28 +0800

    feat: change compose style to multiple lines

--- types/ramda/index.d.ts
+++ types/ramda/index.d.ts
@@ -294,8 +294,27 @@ export function complement<TArgs extends any[]>(pred: (...args: TArgs) => unknow
  * functions must be unary.
  */
 // tslint:disable:max-line-length
-export function compose<TArgs extends any[], R1, R2, R3, R4, R5, R6, R7, TResult>(...func: [fnLast: (a: any) => TResult, ...func: Array<(a: any) => any>, f7: (a: R6) => R7, f6: (a: R5) => R6, f5: (a: R4) => R5, f4: (a: R3) => R4, f3: (a: R2) => R3, f2: (a: R1) => R2, f1: (...args: TArgs) => R1]): (...args: TArgs) => TResult; // fallback overload if number of composed functions greater than 7
-export function compose<TArgs extends any[], R1, R2, R3, R4, R5, R6, R7, TResult>(f7: (a: R6) => R7, f6: (a: R5) => R6, f5: (a: R4) => R5, f4: (a: R3) => R4, f3: (a: R2) => R3, f2: (a: R1) => R2, f1: (...args: TArgs) => R1): (...args: TArgs) => R7;
+export function compose<TArgs extends any[], R1, R2, R3, R4, R5, R6, R7, TResult>(
+  ...func: [
+    fnLast: (a: any) => TResult,
+    ...func: Array<(a: any) => any>,
+    f7: (a: R6) => R7,
+    f6: (a: R5) => R6,
+    f5: (a: R4) => R5,
+    f4: (a: R3) => R4,
+    f3: (a: R2) => R3,
+    f2: (a: R1) => R2,
+    f1: (...args: TArgs) => R1
+  ]): (...args: TArgs) => TResult; // fallback overload if number of composed functions greater than 7
+export function compose<TArgs extends any[], R1, R2, R3, R4, R5, R6, R7, TResult>(
+  f7: (a: R6) => R7,
+  f6: (a: R5) => R6,
+  f5: (a: R4) => R5,
+  f4: (a: R3) => R4,
+  f3: (a: R2) => R3,
+  f2: (a: R1) => R2,
+  f1: (...args: TArgs) => R1
+  ): (...args: TArgs) => R7;
 export function compose<TArgs extends any[], R1, R2, R3, R4, R5, R6, R7>(f7: (a: R6) => R7, f6: (a: R5) => R6, f5: (a: R4) => R5, f4: (a: R3) => R4, f3: (a: R2) => R3, f2: (a: R1) => R2, f1: (...args: TArgs) => R1): (...args: TArgs) => R7;
 export function compose<TArgs extends any[], R1, R2, R3, R4, R5, R6>(f6: (a: R5) => R6, f5: (a: R4) => R5, f4: (a: R3) => R4, f3: (a: R2) => R3, f2: (a: R1) => R2, f1: (...args: TArgs) => R1): (...args: TArgs) => R6;
 export function compose<TArgs extends any[], R1, R2, R3, R4, R5>(f5: (a: R4) => R5, f4: (a: R3) => R4, f3: (a: R2) => R3, f2: (a: R1) => R2, f1: (...args: TArgs) => R1): (...args: TArgs) => R5;
