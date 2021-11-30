0ac83e95ba57e16f1d18b621cf3adcacbc9f13cd
Alexis Rico <sferadev@gmail.com>
2021-11-30 21:45:12 +0100

    Fix tests with path resolution
    Signed-off-by: Alexis Rico <sferadev@gmail.com>

--- types/dhis2__ui-constants/dhis2__ui-constants-tests.ts
+++ types/dhis2__ui-constants/dhis2__ui-constants-tests.ts
@@ -1,29 +1,29 @@
-import * as constants from '@dhis2/ui-constants';
+import { colors, elevations, layer, theme } from '@dhis2/ui-constants';
 
-constants.colors.blue050;
-constants.colors.blue100;
-constants.colors.blue200;
-constants.colors.blue300;
-constants.colors.blue400;
-constants.colors.blue500;
-constants.colors.blue600;
-constants.colors.blue700;
-constants.colors.blue800;
-constants.colors.blue900;
+colors.blue050;
+colors.blue100;
+colors.blue200;
+colors.blue300;
+colors.blue400;
+colors.blue500;
+colors.blue600;
+colors.blue700;
+colors.blue800;
+colors.blue900;
 
-constants.elevations.e100;
-constants.elevations.e200;
-constants.elevations.e300;
-constants.elevations.e400;
+elevations.e100;
+elevations.e200;
+elevations.e300;
+elevations.e400;
 
-constants.layer.alert;
-constants.layer.applicationTop;
-constants.layer.blocking;
+layer.alert;
+layer.applicationTop;
+layer.blocking;
 
-constants.theme.default;
-constants.theme.error;
-constants.theme.valid;
-constants.theme.warning;
-constants.theme.disabled;
-constants.theme.focus;
-constants.theme.fonts;
+theme.default;
+theme.error;
+theme.valid;
+theme.warning;
+theme.disabled;
+theme.focus;
+theme.fonts;
