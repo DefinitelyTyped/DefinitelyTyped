d323ec528ab5901f69b0ca0b3e56ed2ed1043e30
Ido Salomon <idosalomon@gmail.com>
2021-12-01 00:40:44 +0200

    add user to contributors list
    Forgot to add this in my PR https://github.com/DefinitelyTyped/DefinitelyTyped/pull/57437.

--- types/chrome/index.d.ts
+++ types/chrome/index.d.ts
@@ -1,8 +1,8 @@
 // Type definitions for Chrome extension development
 // Project: http://developer.chrome.com/extensions/
 // Definitions by: Matthew Kimber <https://github.com/matthewkimber>
-//                 otiai10 <https://github.com/otiai10>
-//                 couven92 <https://github.com/couven92>
+
+
 //                 RReverser <https://github.com/rreverser>
 //                 sreimer15 <https://github.com/sreimer15>
 //                 MatCarlson <https://github.com/MatCarlson>
@@ -16,6 +16,8 @@
 //                 userTim <https://github.com/usertim>
 //                 Idan Zeierman <https://github.com/idan315>
 //                 Nicolas Rodriguez <https://github.com/nicolas377>
+//                 Ido Salomon <https://github.com/idosal>
+
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
 // TypeScript Version: 2.4
 
