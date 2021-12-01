5a3f306cca9627c6d583b3626cc1c0118a3e4c59
yuriyg86@gmail.com <sakkarem0112>
2021-12-01 19:23:44 +0200

    Fixed declarations of danish-ssn after tests

--- types/danish-ssn/index.d.ts
+++ types/danish-ssn/index.d.ts
@@ -3,15 +3,14 @@
 // Definitions by: Yuriy Guzenko <https://github.com/yuriyg86>
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
 
+export function isValid(ssn: string): boolean;
+export function validate(ssn: string): string;
+export function validForDate(date: Date): string[];
+
 export interface DanishSsnStatus {
     cpr: string;
     valid: boolean;
     date: Date;
     sex: 'Female' | 'Male';
 }
-
 export default function(ssn: string): DanishSsnStatus;
-
-export function isValid(ssn: string): boolean;
-export function validate(ssn: string): null | string;
-export function validForDate(date: Date): string[];
