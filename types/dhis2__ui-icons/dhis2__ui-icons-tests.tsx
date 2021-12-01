cf645c011158c864522ce1cedb220cfa25880512
Alexis Rico <sferadev@gmail.com>
2021-12-01 08:37:26 +0100

    Rename test icon
    Signed-off-by: Alexis Rico <sferadev@gmail.com>

--- types/dhis2__ui-icons/dhis2__ui-icons-tests.tsx
+++ types/dhis2__ui-icons/dhis2__ui-icons-tests.tsx
@@ -1,6 +1,6 @@
 import { IconAdd16 } from '@dhis2/ui-icons';
 import * as React from "react";
 
-const emptyIcon = <IconAdd16 />;
+const icon = <IconAdd16 />;
 const iconWithColor = <IconAdd16 color={'white'} />;
 const iconWithInvalidProp = <IconAdd16 resize={true} />; // $ExpectError
