c5e347ecb0042f5b25148f1246710eb3c7f76d3c
Royson <roysondsilva@gmail.com>
2021-12-04 20:41:46 +0530

    add types for clevertap-web-sdk

---
+++ types/clevertap-web-sdk/index.d.ts
@@ -0,0 +1,9 @@
+// Type definitions for clevertap-web-sdk 1.1
+// Project: https://github.com/CleverTap/clevertap-web-sdk
+// Definitions by: Royson <https://github.com/droyson>
+// Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
+
+/// <reference path="./clevertap.d.ts" />
+
+export = clevertap;
+export as namespace clevertap;
