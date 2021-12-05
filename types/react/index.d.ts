81562399c63df4757b9b99b9ddba729ec4b1b55a
맹주영 <mjy9088@naver.com>
2021-12-05 23:42:26 +0900

    change React SetStateAction type
    Fix bug where no error occurs when S is a function and the argument is not wrapped in a function

--- types/react/index.d.ts
+++ types/react/index.d.ts
@@ -26,6 +26,7 @@
 //                 Victor Magalhães <https://github.com/vhfmag>
 //                 Dale Tan <https://github.com/hellatan>
 //                 Priyanshu Rav <https://github.com/priyanshurav>
+//                 Juyeong Maing <https://github.com/mjy9088>
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
 // TypeScript Version: 2.8
 
@@ -883,7 +884,7 @@ declare namespace React {
     // based on the code in https://github.com/facebook/react/pull/13968
 
     // Unlike the class component setState, the updates are not allowed to be partial
-    type SetStateAction<S> = S | ((prevState: S) => S);
+    type SetStateAction<S> = (S extends Function ? never : S) | ((prevState: S) => S);
     // this technically does accept a second argument, but it's already under a deprecation warning
     // and it's not even released so probably better to not define it.
     type Dispatch<A> = (value: A) => void;
