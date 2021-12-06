1380b42b9bf67da03b38681f9ba68d3644bbd2a9
wangzengdi <wangzengdi@meituan.com>
2021-12-06 08:03:04 +0800

    [ramda] restrict useWith

--- types/ramda/index.d.ts
+++ types/ramda/index.d.ts
@@ -48,6 +48,7 @@ import {
     Evolver,
     Find,
     Functor,
+    InputTypesOfFns,
     KeyValuePair,
     Lens,
     Merge,
@@ -2291,7 +2292,64 @@ export function update<T>(index: number, value: T): (list: readonly T[]) => T[];
  * need to be transformed, although you can ignore them, it's best to pass an identity function so
  * that the new function reports the correct arity.
  */
-export function useWith(fn: ((...a: readonly any[]) => any), transformers: Array<((...a: readonly any[]) => any)>): (...a: readonly any[]) => any;
+export function useWith<
+    TArg1, TR1, TArg2, TR2, TArg3, TR3, TArg4, TR4, TArg5, TR5, TArg6, TR6, TArg7, TR7,
+    TResult,
+    RestFunctions extends Array<(...args: any[]) => any>,
+    TArgs extends [TArg1, TArg2, TArg3, TArg4, TArg5, TArg6, TArg7, ...InputTypesOfFns<RestFunctions>]
+>(
+    fn: ((...args: [TR1, TR2, TR3, TR4, TR5, TR6, TR7, ...ReturnTypesOfFns<RestFunctions>]) => TResult),
+    transformers: [(arg: TArg1) => TR1, (arg: TArg2) => TR2, (arg: TArg3) => TR3, (arg: TArg4) => TR4, (arg: TArg5) => TR5, (arg: TArg6) => TR6, (arg: TArg7) => TR7, ...RestFunctions]
+): (...args: TArgs) => TResult;
+export function useWith<
+    TArg1, TR1, TArg2, TR2, TArg3, TR3, TArg4, TR4, TArg5, TR5, TArg6, TR6, TArg7, TR7,
+    TResult
+>(
+    fn: ((...args: [TR1, TR2, TR3, TR4, TR5, TR6, TR7] & { length: 7 }) => TResult),
+    transformers: [(arg: TArg1) => TR1, (arg: TArg2) => TR2, (arg: TArg3) => TR3, (arg: TArg4) => TR4, (arg: TArg5) => TR5, (arg: TArg6) => TR6, (arg: TArg7) => TR7]
+): (...args: [TArg1, TArg2, TArg3, TArg4, TArg5, TArg7]) => TResult;
+export function useWith<
+    TArg1, TR1, TArg2, TR2, TArg3, TR3, TArg4, TR4, TArg5, TR5, TArg6, TR6,
+    TResult
+>(
+    fn: ((...args: [TR1, TR2, TR3, TR4, TR5, TR6] & { length: 6 }) => TResult),
+    transformers: [(arg: TArg1) => TR1, (arg: TArg2) => TR2, (arg: TArg3) => TR3, (arg: TArg4) => TR4, (arg: TArg5) => TR5, (arg: TArg6) => TR6]
+): (...args: [TArg1, TArg2, TArg3, TArg4, TArg5, TArg6]) => TResult;
+export function useWith<
+    TArg1, TR1, TArg2, TR2, TArg3, TR3, TArg4, TR4, TArg5, TR5,
+    TResult
+>(
+    fn: ((...args: [TR1, TR2, TR3, TR4, TR5] & { length: 5 }) => TResult),
+    transformers: [(arg: TArg1) => TR1, (arg: TArg2) => TR2, (arg: TArg3) => TR3, (arg: TArg4) => TR4, (arg: TArg5) => TR5]
+): (...args: [TArg1, TArg2, TArg3, TArg4, TArg5]) => TResult;
+export function useWith<
+    TArg1, TR1, TArg2, TR2, TArg3, TR3, TArg4, TR4,
+    TResult
+>(
+    fn: ((...args: [TR1, TR2, TR3, TR4] & { length: 4 }) => TResult),
+    transformers: [(arg: TArg1) => TR1, (arg: TArg2) => TR2, (arg: TArg3) => TR3, (arg: TArg4) => TR4]
+): (...args: [TArg1, TArg2, TArg3, TArg4]) => TResult;
+export function useWith<
+    TArg1, TR1, TArg2, TR2, TArg3, TR3,
+    TResult
+>(
+    fn: ((...args: [TR1, TR2, TR3] & { length: 3 }) => TResult),
+    transformers: [(arg: TArg1) => TR1, (arg: TArg2) => TR2, (arg: TArg3) => TR3]
+): (...args: [TArg1, TArg2, TArg3]) => TResult;
+export function useWith<
+    TArg1, TR1, TArg2, TR2,
+    TResult
+>(
+    fn: ((...args: [TR1, TR2] & { length: 2 }) => TResult),
+    transformers: [(arg: TArg1) => TR1, (arg: TArg2) => TR2]
+): (...args: [TArg1, TArg2]) => TResult;
+export function useWith<
+    TArg1, TR1,
+    TResult
+>(
+    fn: ((...args: [TR1]) => TResult),
+    transformers: [(arg: TArg1) => TR1]
+): (...args: [TArg1]) => TResult;
 
 /**
  * Returns a list of all the enumerable own properties of the supplied object.
