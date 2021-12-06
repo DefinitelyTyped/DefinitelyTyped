8925acafefe7174db184fd9bdabc7df05af86914
robertmaier <robert.maier@redbull.com>
2021-12-06 17:42:52 +0100

    feat(sass-true): add types for v6.0

---
+++ types/sass-true/index.d.ts
@@ -0,0 +1,37 @@
+// Type definitions for sass-true 6.0
+// Project: https://github.com/oddbird/true
+// Definitions by: robertmaier <https://github.com/robertmaier>
+// Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
+import type { Options } from 'sass';
+
+export interface TrueOptions {
+    sass?: typeof import('sass');
+    describe: (description: string) => void;
+    it: (description: string) => void;
+}
+
+export interface Assertion {
+    description: string;
+    output: string;
+    assertionType: string;
+    expected: string;
+    details?: string;
+}
+
+export interface Module {
+    module: string;
+    tests: Test[];
+}
+
+export interface Test {
+    assertions: Assertion[];
+}
+
+export interface Assertion {
+    description: string;
+    passed: boolean;
+}
+
+export function runSass(sassOptions: Options, trueOptions: TrueOptions): void;
+export function formatFailureMessage(assertion: Readonly<Assertion>): string;
+export function parse(rawCss: Readonly<string>, ctxLines?: Readonly<number>): Module[];
