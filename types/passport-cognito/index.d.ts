bcb40389d608921c1dd135b545f03602c58ed5d3
Bert Verhelst <verhelstbert@gmail.com>
2021-12-10 18:37:43 +0100

    [passport-cognito] revert version change

--- types/passport-cognito/index.d.ts
+++ types/passport-cognito/index.d.ts
@@ -1,4 +1,4 @@
-// Type definitions for passport-cognito 0.2
+// Type definitions for passport-cognito 0.1
 // Project: https://github.com/kndt84/passport-cognito
 // Definitions by: Maksym Butsykin <https://github.com/mbutsykin>
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
