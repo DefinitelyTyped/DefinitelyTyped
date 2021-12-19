89959f4e19651d729ccbc06a06947b551e46cf90
martin-badin <martin.badin@gmail.com>
2021-12-19 16:38:12 +0100

    remove unused test cases
    Co-authored-by: Piotr Błażejewicz (Peter Blazejewicz) <peterblazejewicz@users.noreply.github.com>

--- types/paypal__paypalhttp/test/serializer-tests.ts
+++ types/paypal__paypalhttp/test/serializer-tests.ts
@@ -15,36 +15,3 @@ const req: paypal.HttpRequest = {
     formPart.headers; // $ExpectType HttpHeaders
     formPart.value; // $ExpectType any
 }
-
-// {
-//     const formEncoded = new paypal.FormEncoded();
-
-//     formEncoded.contentType(); // $ExpectType RegExp
-//     formEncoded.encode(req); // $ExpectType string
-//     formEncoded.decode(); // $ExpectType Error
-// }
-
-// {
-//     const json = new paypal.Json();
-
-//     json.contentType(); // $ExpectType RegExp
-//     json.encode(req); // $ExpectType string
-//     json.decode(JSON.stringify(req.body)); // $ExpectType object
-// }
-
-// {
-//     const text = new paypal.Text();
-
-//     text.contentType(); // $ExpectType RegExp
-//     text.encode(req); // $ExpectType string
-//     text.decode(''); // $ExpectType string
-// }
-
-// {
-//     const multipart = new paypal.Multipart();
-
-//     multipart.contentType(); // $ExpectType RegExp
-//     multipart.encode(req); // $ExpectType Buffer
-//     multipart.decode(); // $ExpectType Error
-//     multipart.formatHeaders(req.headers);
-// }
