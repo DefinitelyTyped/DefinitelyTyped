ecf8a1f507320eeeab66b296244d4968457cc9f2
Bert Verhelst <verhelstbert@gmail.com>
2021-12-20 12:31:47 +0100

    fix: use 4 spaces instead of 2 for indents

--- types/passport-cognito/index.d.ts
+++ types/passport-cognito/index.d.ts
@@ -8,35 +8,35 @@ import passport = require('passport');
 import express = require('express');
 
 export interface CognitoStrategyOptions {
-  userPoolId: string;
-  clientId: string;
-  region: string;
+    userPoolId: string;
+    clientId: string;
+    region: string;
 }
 
 export type CognitoVerifyFunction = (
-  accessToken: string,
-  idToken: string,
-  refreshToken: string,
-  user: object,
-  done: (error: any, user?: any) => void,
+    accessToken: string,
+    idToken: string,
+    refreshToken: string,
+    user: object,
+    done: (error: any, user?: any) => void,
 ) => any;
 
 export type CognitoVerifyFunctionWithSession = (
-  accessToken: string,
-  idToken: string,
-  refreshToken: string,
-  user: object,
-  session: {
-    getIdToken: () => {
-      getExpiration: () => number;
-    };
-  },
-  done: (error: any, user?: any) => void,
+    accessToken: string,
+    idToken: string,
+    refreshToken: string,
+    user: object,
+    session: {
+        getIdToken: () => {
+            getExpiration: () => number;
+        };
+    },
+    done: (error: any, user?: any) => void,
 ) => any;
 
 export class Strategy extends passport.Strategy {
-  constructor(options: CognitoStrategyOptions, verify: CognitoVerifyFunction | CognitoVerifyFunctionWithSession);
+    constructor(options: CognitoStrategyOptions, verify: CognitoVerifyFunction | CognitoVerifyFunctionWithSession);
 
-  name: string;
-  authenticate(req: express.Request, options?: object): void;
+    name: string;
+    authenticate(req: express.Request, options?: object): void;
 }
