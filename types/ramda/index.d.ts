3aab2e54ec0303907e36f7b1c1d9f5dd57719564
Kyle Hensel <k.y.l.e@outlook.co.nz>
2021-12-22 11:17:09 +1300

    fix lint

--- types/ramda/index.d.ts
+++ types/ramda/index.d.ts
@@ -1886,7 +1886,7 @@ export function remove<T>(start: number, count: number, list: readonly T[]): T[]
 export function remove<T>(start: number): {
     (count: number, list: readonly T[]): T[];
     (count: number): (list: readonly T[]) => T[];
-}
+};
 export function remove<T>(start: number, count: number): (list: readonly T[]) => T[];
 
 /**
