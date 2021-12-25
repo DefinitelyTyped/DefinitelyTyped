1123c03044fca2d6d48a96efd190114556d7df6c
Gray Zhang <otakustay@gmail.com>
2021-12-25 17:04:32 +0000

    republish dedent to compat with nodenext module resolution

--- types/dedent/index.d.ts
+++ types/dedent/index.d.ts
@@ -6,5 +6,7 @@
 export = dedent;
 
 declare function dedent(literals: string): string;
+
 declare function dedent(literals: TemplateStringsArray, ...placeholders: any[]): string;
+
 declare namespace dedent {}
