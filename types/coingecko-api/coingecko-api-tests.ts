677948705586c8a6c4166c7e1d8fd1f2234ea47a
wpoulin_ <williampoulin01@gmail.com>
2021-12-26 11:15:47 -0500

    added a test coins.fetchMarketChart without interval

--- types/coingecko-api/coingecko-api-tests.ts
+++ types/coingecko-api/coingecko-api-tests.ts
@@ -39,6 +39,8 @@ const client = new CoinGecko();
         tickers: false
     })).data.market_data.total_volume.cad;
 
+    (await client.coins.fetchMarketChart("bitcoin", {vs_currency: "usd", days: "1"})).data.market_caps[0][1];
+
     (await client.coins.fetchMarketChart("bitcoin", {vs_currency: "usd", days: "1", interval: "daily"})).data.market_caps[0][1];
 
     (await client.coins.fetchMarketChart("bitcoin", {vs_currency: "usd", days: "1", interval: "daily"})).data.prices[0][1];
