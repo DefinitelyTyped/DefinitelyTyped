83eba4f7b5ffaf0c5a0510626ab1c9736768d84b
Mateusz Turcza <xemlock@gmail.com>
2021-12-29 00:43:01 +0100

    Update version as suggested
    Co-authored-by: Piotr Błażejewicz (Peter Blazejewicz) <peterblazejewicz@users.noreply.github.com>

--- types/angular-scroll/index.d.ts
+++ types/angular-scroll/index.d.ts
@@ -1,4 +1,4 @@
-// Type definitions for angular-scroll 1.0.0
+// Type definitions for angular-scroll 1.0
 // Project: https://github.com/oblador/angular-scroll
 // Definitions by: Sam Herrmann <https://github.com/samherrmann>
 //                 xemlock <https://github.com/xemlock>
