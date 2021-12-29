900a38a2bbdbf595af261602a99935bc9ef08f87
andreiRUS39 <andrew_06_87@mail.ru>
2021-12-29 19:33:24 +0200

    Publish

--- types/fparser/index.d.ts
+++ types/fparser/index.d.ts
@@ -1,4 +1,4 @@
-// Type definitions for fparser
+// Type definitions for fparser 2.0.1
 // Project: https://github.com/bylexus/fparse
 // Definitions by: bylexus <https://github.com/bylexus>,
 //                 andrew0687 <https://github.com/andrew0687>
@@ -6,23 +6,23 @@
 
 export default class Formula {
     constructor(fStr: string, options?: object);
-    public setFormula(formulaString: string): this;
-    public enableMemoization(): void;
-    public disableMemoization(): void;
-    public splitFunctionParams(toSplit: string): string[];
-    public cleanupInputString(s: string): string;
-    public parse(str: string): any;
+    setFormula(formulaString: string): this;
+    enableMemoization(): void;
+    disableMemoization(): void;
+    splitFunctionParams(toSplit: string): string[];
+    cleanupInputString(s: string): string;
+    parse(str: string): any;
     private _do_parse(str: string): any;
-    public buildExpressionTree(expressions: any[]): any;
-    public isOperator(char: any): RegExpMatchArray | null;
-    public isOperatorExpr(expr: any): boolean;
-    public registerVariable(varName: string): void;
-    public getVariables(): string[];
-    public evaluate(valueObj: object | object[]): any;
-    public hashValues(valueObj: object): string;
-    public resultFromMemory(valueObj: object): any | null;
-    public storeInMemory(valueObj: object, value: any): void;
-    public getExpression(): any[];
-    public getExpressionString(): string;
+    buildExpressionTree(expressions: any[]): any;
+    isOperator(char: any): RegExpMatchArray | null;
+    isOperatorExpr(expr: any): boolean;
+    registerVariable(varName: string): void;
+    getVariables(): string[];
+    evaluate(valueObj: object | object[]): any;
+    hashValues(valueObj: object): string;
+    resultFromMemory(valueObj: object): any;
+    storeInMemory(valueObj: object, value: any): void;
+    getExpression(): any[];
+    getExpressionString(): string;
     static calc(formula: string, valueObj: object | object[], options?: object): any;
 }
