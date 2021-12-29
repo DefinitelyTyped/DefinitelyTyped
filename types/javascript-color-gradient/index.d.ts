d8c0a384a630cd45a5e2dadb3446d0266d344599
Wilson <wwc4618@ic.ac.uk>
2021-12-29 23:19:52 +0000

    Added type definitions for javascript-color-gradient

---
+++ types/javascript-color-gradient/index.d.ts
@@ -0,0 +1,14 @@
+// Type definitions for javascript-color-gradient 1.3
+// Project: https://github.com/Adrinlol/javascript-color-gradient
+// Definitions by: Wilson Chua <https://github.com/wilsoncwc/>
+// Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
+
+export = Gradient;
+
+declare class Gradient {
+    constructor(gradients?: string, maxNum?: number, color?: string[], intervals?: any[]);
+    setGradient(...args: string[]): Gradient;
+    getArray(): string[];
+    getColor(index: number): string;
+    setMidpoint(maxNumber: number): Gradient;
+}
