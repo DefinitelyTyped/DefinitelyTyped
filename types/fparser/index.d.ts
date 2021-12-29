21178ca6778042698f415f942bc53dbf01097476
andreiRUS39 <andrew_06_87@mail.ru>
2021-12-29 20:06:30 +0200

    Publish

--- types/fparser/index.d.ts
+++ types/fparser/index.d.ts
@@ -1,10 +1,10 @@
-// Type definitions for fparser 2.01
+// Type definitions for fparser 2.0
 // Project: https://github.com/bylexus/fparse
 // Definitions by: bylexus <https://github.com/bylexus>,
 //                 andrew0687 <https://github.com/andrew0687>
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
 
-export default class Formula {
+declare class Formula {
     constructor(fStr: string, options?: object);
     setFormula(formulaString: string): this;
     enableMemoization(): void;
@@ -26,3 +26,7 @@ export default class Formula {
     getExpressionString(): string;
     static calc(formula: string, valueObj: object | object[], options?: object): any;
 }
+
+declare const _default: Formula;
+
+export = _default;
