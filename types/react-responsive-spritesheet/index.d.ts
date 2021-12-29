5d98f9e6e939eb3b78d26d041868087da937c10d
Michael Liu <michael@triangularcube.com>
2021-12-28 16:52:51 -0800

    Remove namespace, export supplemental types

--- types/react-responsive-spritesheet/index.d.ts
+++ types/react-responsive-spritesheet/index.d.ts
@@ -6,7 +6,7 @@
 import * as React from 'react';
 import * as CSS from 'csstype';
 
-declare class ReactResponsiveSpritesheet extends React.Component<ReactResponsiveSpritesheet.Props> {
+export default class ReactResponsiveSpritesheet extends React.Component<Props> {
     play(): void;
     pause(): void;
     goToAndPlay(frame: number): void;
@@ -14,58 +14,54 @@ declare class ReactResponsiveSpritesheet extends React.Component<ReactResponsive
     setStartAt(frame: number): void;
     setEndAt(frame: number): void;
     setFps(fps: number): void;
-    setDirection(direction: ReactResponsiveSpritesheet.Direction): void;
-    getInfo(type: ReactResponsiveSpritesheet.NumberInfoType): number;
-    getInfo(type: ReactResponsiveSpritesheet.BooleanInfoType): boolean;
-    getInfo(type: 'direction'): ReactResponsiveSpritesheet.Direction;
+    setDirection(direction: Direction): void;
+    getInfo(type: NumberInfoType): number;
+    getInfo(type: BooleanInfoType): boolean;
+    getInfo(type: 'direction'): Direction;
 }
 
-declare namespace ReactResponsiveSpritesheet {
-    interface Props {
-        className?: string;
-        style?: CSS.Properties;
-        image: string;
-        widthFrame: number;
-        heightFrame: number;
-        steps: number;
-        fps: number;
-        direction: Direction;
-        timeout?: number;
-        autoplay?: boolean;
-        loop?: boolean;
-        isResponsive?: boolean;
-        startAt?: number;
-        endAt?: number;
-        background?: string;
-        backgroundSize?: CSS.Property.BackgroundSize;
-        backgroundRepeat?: CSS.Property.BackgroundRepeat;
-        backgroundPosition?: CSS.Property.BackgroundPosition;
-        getInstance?: Callback;
-        onClick?: Callback;
-        onDoubleClick?: Callback;
-        onMouseMove?: Callback;
-        onMouseEnter?: Callback;
-        onMouseLeave?: Callback;
-        onMouseOver?: Callback;
-        onMouseOut?: Callback;
-        onMouseDown?: Callback;
-        onMouseUp?: Callback;
-        onInit?: Callback;
-        onResize?: Callback;
-        onPlay?: Callback;
-        onPause?: Callback;
-        onLoopComplete?: Callback;
-        onEachFrame?: Callback;
-        onEnterFrame?: Array<{
-            frame: number;
-            callback: () => void;
-        }>;
-    }
-
-    type Callback = (spritesheet: ReactResponsiveSpritesheet) => void;
-    type Direction = 'forward' | 'rewind';
-    type NumberInfoType = 'frame' | 'fps' | 'steps' | 'width' | 'height' | 'scale' | 'completeLoopCicles';
-    type BooleanInfoType = 'isPlaying' | 'isPaused';
+export interface Props {
+    className?: string;
+    style?: CSS.Properties;
+    image: string;
+    widthFrame: number;
+    heightFrame: number;
+    steps: number;
+    fps: number;
+    direction: Direction;
+    timeout?: number;
+    autoplay?: boolean;
+    loop?: boolean;
+    isResponsive?: boolean;
+    startAt?: number;
+    endAt?: number;
+    background?: string;
+    backgroundSize?: CSS.Property.BackgroundSize;
+    backgroundRepeat?: CSS.Property.BackgroundRepeat;
+    backgroundPosition?: CSS.Property.BackgroundPosition;
+    getInstance?: Callback;
+    onClick?: Callback;
+    onDoubleClick?: Callback;
+    onMouseMove?: Callback;
+    onMouseEnter?: Callback;
+    onMouseLeave?: Callback;
+    onMouseOver?: Callback;
+    onMouseOut?: Callback;
+    onMouseDown?: Callback;
+    onMouseUp?: Callback;
+    onInit?: Callback;
+    onResize?: Callback;
+    onPlay?: Callback;
+    onPause?: Callback;
+    onLoopComplete?: Callback;
+    onEachFrame?: Callback;
+    onEnterFrame?: Array<{
+        frame: number;
+        callback: () => void;
+    }>;
 }
 
-export default ReactResponsiveSpritesheet;
+export type Callback = (spritesheet: ReactResponsiveSpritesheet) => void;
+export type Direction = 'forward' | 'rewind';
+export type NumberInfoType = 'frame' | 'fps' | 'steps' | 'width' | 'height' | 'scale' | 'completeLoopCicles';
+export type BooleanInfoType = 'isPlaying' | 'isPaused';
