18149f8d248ad1a6cbc319acd590f8f293fb6662
Tadeusz Wyrzykowski <tadev3@gmail.com>
2021-12-29 23:05:18 +0100

    Cleanup

--- types/abstract-things/index.d.ts
+++ types/abstract-things/index.d.ts
@@ -2,7 +2,6 @@
 // Project: https://github.com/thingbound/abstract-things
 // Definitions by: Tadeusz Wyrzykowski <https://github.com/Shaquu>
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
-// TypeScript Version: 2.2
 
 /// <reference types="node" />
 
