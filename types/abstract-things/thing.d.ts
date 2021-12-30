919d2f63d6487e0c92489c0edd508f7bb27916cd
Tadeusz Wyrzykowski <tadev3@gmail.com>
2021-12-30 22:39:23 +0100

    Update types/abstract-things/thing.d.ts
    Co-authored-by: Piotr Błażejewicz (Peter Blazejewicz) <peterblazejewicz@users.noreply.github.com>

--- types/abstract-things/thing.d.ts
+++ types/abstract-things/thing.d.ts
@@ -17,7 +17,7 @@ export class Thing {
     destroy(): Promise<void>;
     destroyCallback(): Promise<void>;
 
-    emitEvent(event: string, data: unknown, options?: EmitEventOptions): void;
+    emitEvent(event: string, data?: unknown, options?: EmitEventOptions): void;
     on: InstanceType<typeof EventEmitter>['on'];
     off: InstanceType<typeof EventEmitter>['off'];
     onAny: InstanceType<typeof EventEmitter>['onAny'];
