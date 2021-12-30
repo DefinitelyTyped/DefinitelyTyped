2b02a61859551b2ff452922a54dd5c1d4046ee9c
Jesse Wright <jesse.wright@anu.edu.au>
2021-12-30 12:55:38 +1100

    feat: re-track rdfjs__data-model changes

--- types/rdfjs__data-model/index.d.ts
+++ types/rdfjs__data-model/index.d.ts
@@ -4,12 +4,19 @@
 //                 Ruben Taelman <https://github.com/rubensworks>
 //                 Andy Davison  <https://github.com/andydavison>
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
+// Minimum TypeScript Version: 3.9
 
 import * as RDF from "@rdfjs/types";
 
-export default interface Factory<OutQuad extends RDF.BaseQuad = RDF.Quad, InQuad extends RDF.BaseQuad = OutQuad>
+interface Factory<OutQuad extends RDF.BaseQuad = RDF.Quad, InQuad extends RDF.BaseQuad = OutQuad>
   // Required is needed here because variable is optional in the spec
   extends Required<RDF.DataFactory<OutQuad, InQuad>> {
+  // Enable same behavior of version 1 where quads default to RDF.Quad (where possible)
+  quad<Q extends OutQuad = RDF.Quad extends OutQuad ? RDF.Quad : OutQuad>(
+      subject: Q['subject'], predicate: Q['predicate'], object: Q['object'], graph?: Q['graph']): Q;
   fromTerm<T extends RDF.Term>(value: T): T;
-  fromQuad<T extends RDF.BaseQuad>(value: T): T;
+  fromQuad<T extends OutQuad = RDF.Quad extends OutQuad ? RDF.Quad : OutQuad>(value: T): T;
 }
+
+declare const factory: Factory<RDF.BaseQuad, RDF.BaseQuad>;
+export default factory;
--- types/rdfjs__data-model/rdfjs__data-model-tests.ts
+++ types/rdfjs__data-model/rdfjs__data-model-tests.ts
@@ -1,48 +1,48 @@
-import factory from '@rdfjs/data-model';
-import * as RDF from '@rdfjs/types'
+import factory from "rdfjs__data-model";
+import * as RDF from "@rdfjs/types";
 
-const myQuad: RDF.Quad = factory.quad(
+const myQuad = factory.quad<RDF.Quad>(
   factory.namedNode('http://example.org/subject'),
   factory.namedNode('http://example.org/predicate'),
   factory.namedNode('http://example.org/object'),
-)
+);
 
-const myQuadWithLiteral: RDF.Quad = factory.quad(
+const myQuadWithLiteral: RDF.Quad = factory.quad<RDF.Quad>(
   factory.namedNode('http://example.org/subject'),
   factory.namedNode('http://example.org/predicate'),
-  factory.literal(34),
-)
+  factory.literal('34'),
+);
 
-const myQuadWithVariable: RDF.Quad = factory.quad(
+const myQuadWithVariable: RDF.Quad = factory.quad<RDF.Quad>(
   factory.namedNode('http://example.org/subject'),
   factory.namedNode('http://example.org/predicate'),
   factory.variable('?o'),
-)
+);
 
-const myQuadWithGraph: RDF.Quad = factory.quad(
+const myQuadWithGraph: RDF.Quad = factory.quad<RDF.Quad>(
   factory.namedNode('http://example.org/subject'),
   factory.namedNode('http://example.org/predicate'),
   factory.namedNode('http://example.org/object'),
   factory.defaultGraph()
-)
+);
 
 const myBaseQuad: RDF.BaseQuad = factory.quad<RDF.BaseQuad>(
   factory.namedNode('http://example.org/subject'),
   factory.blankNode('34'),
   factory.namedNode('http://example.org/object'),
-)
+);
 
-// @ts-expect-error
 const myBaseQuadBad: RDF.Quad = factory.quad<RDF.Quad>(
   factory.namedNode('http://example.org/subject'),
+  // @ts-expect-error
   factory.blankNode('34'),
   factory.namedNode('http://example.org/object'),
-)
+);
 
 const fromQuadValue: RDF.Quad = factory.fromQuad(myQuad);
 const fromBaseQuadValue: RDF.BaseQuad = factory.fromQuad(myBaseQuad);
 
 // @ts-expect-error
-factory.fromQuad(factory.variable('?o'))
+factory.fromQuad(factory.variable('?o'));
 
-const fromTermValue: RDF.Variable = factory.fromQuad(factory.variable('?o'));
+const fromTermValue: RDF.Variable = factory.fromTerm(factory.variable('?o'));
