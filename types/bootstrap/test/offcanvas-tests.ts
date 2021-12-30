755d8cd793da41dfb62bb7fce2a6cd0db6c4086e
darthf1 <darthf1>
2021-12-30 19:53:16 +0100

    fix: update bootstrap offcanvas options and methods

--- types/bootstrap/test/offcanvas-tests.ts
+++ types/bootstrap/test/offcanvas-tests.ts
@@ -6,25 +6,26 @@ const element = new Element();
 // $ExpectType Offcanvas
 new Offcanvas(element);
 
+// $ExpectType Offcanvas
+new Offcanvas(element, {
+    backdrop: true,
+    keyboard: true,
+    scroll: false,
+});
+
 // $ExpectType Offcanvas | null
 Offcanvas.getInstance(element);
 // $ExpectType Offcanvas
 Offcanvas.getOrCreateInstance(element);
+// $ExpectType Offcanvas
+Offcanvas.getOrCreateInstance(element, {
+    backdrop: true,
+    keyboard: true,
+    scroll: false,
+});
 
 // $ExpectType string
 Offcanvas.VERSION;
-
-element.addEventListener(Offcanvas.Events.click, event => {
-    // do something…
-});
-
-element.addEventListener(Offcanvas.Events.clickDismiss, event => {
-    // do something…
-});
-
-element.addEventListener(Offcanvas.Events.focusin, event => {
-    // do something…
-});
 
 element.addEventListener(Offcanvas.Events.hidden, event => {
     // do something…
