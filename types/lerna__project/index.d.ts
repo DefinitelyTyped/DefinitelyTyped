3bad0388eb763e5aaa87d20bf86b4db3ef1bbd1c
ò_ó <donmahallem@users.noreply.github.com>
2022-01-02 19:55:54 +0100

    fix typo
    Co-authored-by: Piotr Błażejewicz (Peter Blazejewicz) <peterblazejewicz@users.noreply.github.com>

--- types/lerna__project/index.d.ts
+++ types/lerna__project/index.d.ts
@@ -24,7 +24,7 @@ export class Project {
     /**
      * @param cwd defaults to process.cwd()
      */
-    static getPackges(cwd?: string): Promise<Package[]>;
+    static getPackages(cwd?: string): Promise<Package[]>;
     /**
      * @param cwd defaults to process.cwd()
      */
