c2473bb6c9d8852b7bace9f8a45c3240f68bf60a
andreiRUS39 <andrew_06_87@mail.ru>
2022-01-02 14:57:27 +0200

    Added test for UMD

--- types/fparser/index.d.ts
+++ types/fparser/index.d.ts
@@ -4,25 +4,6 @@
 //                 andrew0687 <https://github.com/andrew0687>
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
 
-export default class FormulaC {
-    constructor(fStr: string, options?: { memoization?: boolean; });
-    setFormula(formulaString: string): this;
-    enableMemoization(): void;
-    disableMemoization(): void;
-    splitFunctionParams(toSplit: string): string[];
-    cleanupInputString(s: string): string;
-    parse(str: string): any;
-    _do_parse(str: string): any;
-    buildExpressionTree(expressions: any[]): any;
-    isOperator(char: any): RegExpMatchArray | null;
-    isOperatorExpr(expr: any): boolean;
-    registerVariable(varName: string): void;
-    getVariables(): string[];
-    evaluate(valueObj: object | object[]): any;
-    hashValues(valueObj: object): string;
-    resultFromMemory(valueObj: object): any;
-    storeInMemory(valueObj: object, value: any): void;
-    getExpression(): any[];
-    getExpressionString(): string;
-    static calc(formula: string, valueObj: object | object[], options?: { memoization?: boolean; }): any;
-}
+///<reference path="./fparser.d.ts"/>
+
+export default FormulaC;
