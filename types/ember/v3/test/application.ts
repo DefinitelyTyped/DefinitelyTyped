16bf998ce948229a57de2ce696418e18580f1f83
Chris Krycho <hello@chriskrycho.com>
2022-01-03 16:45:05 -0700

    Ember: introduce v3 for 'ember' package

---
+++ types/ember/v3/test/application.ts
@@ -0,0 +1,41 @@
+import Ember from 'ember';
+import { assertType } from './lib/assert';
+
+const BaseApp = Ember.Application.extend({
+    modulePrefix: 'my-app',
+});
+
+BaseApp.initializer({
+    name: 'my-initializer',
+    initialize(app) {
+        app.register('foo:bar', Ember.Object.extend({ foo: 'bar' }));
+    },
+});
+
+BaseApp.instanceInitializer({
+    name: 'my-instance-initializer',
+    initialize(app) {
+        app.lookup('foo:bar').get('foo');
+    },
+});
+
+const App1 = BaseApp.create({
+    rootElement: '#app-one',
+    customEvents: {
+        paste: 'paste',
+    },
+});
+
+const App2 = BaseApp.create({
+    rootElement: '#app-two',
+    customEvents: {
+        mouseenter: null,
+        mouseleave: null,
+    },
+});
+
+const App3 = BaseApp.create();
+
+const App3Instance1 = App3.buildInstance();
+
+const App3Instance2 = App3.buildInstance({ foo: 'bar' });
