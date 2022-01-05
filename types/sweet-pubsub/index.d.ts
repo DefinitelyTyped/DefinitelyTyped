0aed336f4e78ceeb43a84e0b56ee719a988b5823
emilioivan12 <escobar.emilioivan@gmail.com>
2022-01-05 12:58:16 -0600

    Fix lint errors

--- types/sweet-pubsub/index.d.ts
+++ types/sweet-pubsub/index.d.ts
@@ -1,27 +1,42 @@
-// Type definitions for sweet-pubsub x.x
+// Type definitions for sweet-pubsub 1.2
 // Project: https://github.com/oldboyxx/sweet-pubsub
 // Definitions by: Emilio Escobar <https://github.com/emilioivan12>
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
+export function on<CBFn extends (...args: any[]) => unknown>(
+    topic: string,
+    unsubscribeLabel: string,
+    callback: CBFn,
+    priority?: number,
+): CBFn;
+export function on<CBFn extends (...args: any[]) => unknown>(topic: string, callback: CBFn, priority?: number): CBFn;
 
-export function on<CBFn extends (...args: any[]) => unknown>(topic: string, callback: CBFn): CBFn;
-export function on<CBFn extends (...args: any[]) => unknown>(topic: string, callback: CBFn, priority: number): CBFn;
-export function on<CBFn extends (...args: any[]) => unknown>(topic: string, unsubscribeLabel: string, callback: CBFn): CBFn;
-export function on<CBFn extends (...args: any[]) => unknown>(topic: string, unsubscribeLabel: string, callback: CBFn, priority: number): CBFn;
+export function sub<CBFn extends (...args: any[]) => unknown>(
+    topic: string,
+    unsubscribeLabel: string,
+    callback: CBFn,
+    priority?: number,
+): CBFn;
+export function sub<CBFn extends (...args: any[]) => unknown>(topic: string, callback: CBFn, priority?: number): CBFn;
 
-export function sub<CBFn extends (...args: any[]) => unknown>(topic: string, callback: CBFn): CBFn;
-export function sub<CBFn extends (...args: any[]) => unknown>(topic: string, callback: CBFn, priority: number): CBFn;
-export function sub<CBFn extends (...args: any[]) => unknown>(topic: string, unsubscribeLabel: string, callback: CBFn): CBFn;
-export function sub<CBFn extends (...args: any[]) => unknown>(topic: string, unsubscribeLabel: string, callback: CBFn, priority: number): CBFn;
+export function once<CBFn extends (...args: any[]) => unknown>(
+    topic: string,
+    unsubscribeLabel: string,
+    callback: CBFn,
+    priority?: number,
+): CBFn;
+export function once<CBFn extends (...args: any[]) => unknown>(topic: string, callback: CBFn, priority?: number): CBFn;
 
-export function once<CBFn extends (...args: any[]) => unknown>(topic: string, callback: CBFn): CBFn;
-export function once<CBFn extends (...args: any[]) => unknown>(topic: string, callback: CBFn, priority: number): CBFn;
-export function once<CBFn extends (...args: any[]) => unknown>(topic: string, unsubscribeLabel: string, callback: CBFn): CBFn;
-export function once<CBFn extends (...args: any[]) => unknown>(topic: string, unsubscribeLabel: string, callback: CBFn, priority: number): CBFn;
-
-export function subOnce<CBFn extends (...args: any[]) => unknown>(topic: string, callback: CBFn): CBFn;
-export function subOnce<CBFn extends (...args: any[]) => unknown>(topic: string, callback: CBFn, priority: number): CBFn;
-export function subOnce<CBFn extends (...args: any[]) => unknown>(topic: string, unsubscribeLabel: string, callback: CBFn): CBFn;
-export function subOnce<CBFn extends (...args: any[]) => unknown>(topic: string, unsubscribeLabel: string, callback: CBFn, priority: number): CBFn;
+export function subOnce<CBFn extends (...args: any[]) => unknown>(
+    topic: string,
+    unsubscribeLabel: string,
+    callback: CBFn,
+    priority?: number,
+): CBFn;
+export function subOnce<CBFn extends (...args: any[]) => unknown>(
+    topic: string,
+    callback: CBFn,
+    priority?: number,
+): CBFn;
 
 export function emit(topic: string, ...arguments: any[]): unknown;
 export function pub(topic: string, ...arguments: any[]): unknown;
@@ -30,11 +45,9 @@ export function emits(topic: string, ...arguments: any[]): unknown;
 export function pubs(topic: string, ...arguments: any[]): unknown;
 
 export function off(callback: (...args: any[]) => unknown): unknown[];
-export function off(topic: string, callback: (...args: any[]) => unknown): unknown[];
+export function off(topicOrUnsubscribeLabel: string, callback?: (...args: any[]) => unknown): unknown[];
 export function off(topic: string, unsubscribeLabel: string): unknown[];
-export function off(unsubscribeLabel: string): unknown[];
 
 export function unsub(callback: (...args: any[]) => unknown): unknown[];
-export function unsub(topic: string, callback: (...args: any[]) => unknown): unknown[];
+export function unsub(topicOrUnsubscribeLabel: string, callback?: (...args: any[]) => unknown): unknown[];
 export function unsub(topic: string, unsubscribeLabel: string): unknown[];
-export function unsub(unsubscribeLabel: string): unknown[];
