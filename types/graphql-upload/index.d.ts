b93dd1e07d8be9da16bdaf36ddc9b1202fb1825c
Aaron Laney <aaron.laney@adhocteam.us>
2022-01-07 09:16:13 -0600

    update typescript version

--- types/graphql-upload/index.d.ts
+++ types/graphql-upload/index.d.ts
@@ -2,49 +2,47 @@
 // Project: https://github.com/jaydenseric/graphql-upload#readme
 // Definitions by: Mike Marcacci <https://github.com/mike-marcacci>
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
-// TypeScript Version: 3.3
+// TypeScript Version: 4.0
 
 /* tslint:disable:no-unnecessary-generics */
 
-import { IncomingMessage, ServerResponse } from "http";
-import { GraphQLScalarType } from "graphql";
-import { RequestHandler } from "express";
-import { DefaultContext, DefaultState, Middleware } from "koa";
-import { ReadStream } from "fs-capacitor";
+import { IncomingMessage, ServerResponse } from 'http';
+import { GraphQLScalarType } from 'graphql';
+import { RequestHandler } from 'express';
+import { DefaultContext, DefaultState, Middleware } from 'koa';
+import { ReadStream } from 'fs-capacitor';
 
 export interface UploadOptions {
-  maxFieldSize?: number | undefined;
-  maxFileSize?: number | undefined;
-  maxFiles?: number | undefined;
+    maxFieldSize?: number | undefined;
+    maxFileSize?: number | undefined;
+    maxFiles?: number | undefined;
 }
 
 export interface GraphQLOperation {
-  query: string;
-  operationName?: null | string | undefined;
-  variables?: null | unknown | undefined;
+    query: string;
+    operationName?: null | string | undefined;
+    variables?: null | unknown | undefined;
 }
 
 export function processRequest(
-  request: IncomingMessage,
-  response: ServerResponse,
-  uploadOptions?: UploadOptions
+    request: IncomingMessage,
+    response: ServerResponse,
+    uploadOptions?: UploadOptions,
 ): Promise<GraphQLOperation | GraphQLOperation[]>;
 
-export function graphqlUploadExpress(
-  uploadOptions?: UploadOptions
-): RequestHandler;
+export function graphqlUploadExpress(uploadOptions?: UploadOptions): RequestHandler;
 
-export function graphqlUploadKoa <StateT = DefaultState, ContextT = DefaultContext>(
-  uploadOptions?: UploadOptions
+export function graphqlUploadKoa<StateT = DefaultState, ContextT = DefaultContext>(
+    uploadOptions?: UploadOptions,
 ): Middleware<StateT, ContextT>;
 
 export const GraphQLUpload: GraphQLScalarType;
 
 export interface FileUpload {
-  filename: string;
-  mimetype: string;
-  encoding: string;
-  createReadStream(): ReadStream;
+    filename: string;
+    mimetype: string;
+    encoding: string;
+    createReadStream(): ReadStream;
 }
 
 export type Upload = Promise<FileUpload>;
