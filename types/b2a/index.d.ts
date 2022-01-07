436108176c27b70efe655850c138653c90555826
Martin Rubas <martin.rubas@ataccama.com>
2022-01-07 23:02:06 +0100

    b2a: added base64url encode/decode function types (indent.fix)

--- types/b2a/index.d.ts
+++ types/b2a/index.d.ts
@@ -7,22 +7,22 @@
 /**
  * Converts base64 string back into original text
  */
- declare function atob(base64: string): string;
+declare function atob(base64: string): string;
 
  /**
   * Converts base64url string back into original text
   */
- declare function atobu(base64: string): string;
+declare function atobu(base64: string): string;
 
  /**
   * Converts text into base64 string
   */
- declare function btoa(text: string): string;
+declare function btoa(text: string): string;
 
  /**
   * Converts text into base64url string
   */
- declare function btoau(text: string): string;
+declare function btoau(text: string): string;
 
 export {
     atob,
