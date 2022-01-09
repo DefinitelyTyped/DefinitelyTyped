7afa0104be19370490e8a3cc4117ced28981f888
Payton Swick <payton@foolord.com>
2022-01-09 12:52:19 -0500

    Run Prettier

--- types/wordpress__components/guide/index.d.ts
+++ types/wordpress__components/guide/index.d.ts
@@ -1,19 +1,19 @@
 import type { ComponentType, ReactNode } from 'react';
 
 interface GuidePage {
-		content: ReactNode;
-		image?: ReactNode;
+    content: ReactNode;
+    image?: ReactNode;
 }
 
 interface GuideProps {
-		className?: string;
-		contentLabel?: string;
-		finishButtonText?: ReactNode;
-		onFinish?: () => void;
-		pages?: GuidePage[];
+    className?: string;
+    contentLabel?: string;
+    finishButtonText?: ReactNode;
+    onFinish?: () => void;
+    pages?: GuidePage[];
 
-		/* @deprecated */
-		children?: ReactNode;
+    /* @deprecated */
+    children?: ReactNode;
 }
 
 declare const Guide: ComponentType<GuideProps>;
