9278a7b6b2fd81982f0bfe9ac35a0f147ad2e3a5
Nick Papirniy <nikolaspapirniywork@gmail.com>
2022-01-11 20:49:00 +0200

    fix(hdkey): fixed spaces

--- types/hdkey/hdkey-tests.ts
+++ types/hdkey/hdkey-tests.ts
@@ -13,4 +13,4 @@ hdKey.chainCode;
 hdKey.toJSON();
 HDKey.fromJSON({ xpriv: '', xpub: '' });
 HDKey.fromMasterSeed(Buffer.from('hello world'));
-HDKey.fromMasterSeed(Buffer.from('hello world'), {private: 0x043587cf, public: 0x04358394});
+HDKey.fromMasterSeed(Buffer.from('hello world'), { private: 0x043587cf, public: 0x04358394 });
