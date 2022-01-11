4d772bb0a66c6bf095302a9717d31727d150f8a1
sciencefidelity <32623301+sciencefidelity@users.noreply.github.com>
2022-01-11 22:49:05 +0000

    refactor: add types for the Color Assistant added in Nova 5

--- types/nova-editor-node/index.d.ts
+++ types/nova-editor-node/index.d.ts
@@ -1,6 +1,7 @@
 // Type definitions for non-npm package nova-editor-node 4.1
 // Project: https://docs.nova.app/api-reference/
 // Definitions by: Cameron Little <https://github.com/apexskier>
+//                 Matt Cook <https://github.com/sciencefidelity>
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
 // Minimum TypeScript Version: 3.3
 
@@ -16,9 +17,13 @@ type WritableStream<T = any> = unknown;
 
 /// https://docs.nova.app/api-reference/assistants-registry/
 
-type AssistantsRegistrySelector = string | { syntax: string };
+type AssistantsRegistrySelector = string | string[] | { syntax: string } | { syntax: string }[];
 
 interface AssistantsRegistry {
+    registerColorAssistant(
+      selector: AssistantsRegistrySelector,
+      object: ColorAssistant,
+    ): Disposable;
     registerCompletionAssistant(
         selector: AssistantsRegistrySelector,
         object: CompletionAssistant,
@@ -33,6 +38,11 @@ interface AssistantsRegistry {
 }
 
 type AssistantArray<T> = ReadonlyArray<T> | Promise<ReadonlyArray<T>>;
+
+interface ColorAssistant {
+    provideColors(editor: TextEditor, context: ColorInformationContext): AssistantArray<ColorInformation>;
+    provideColorPresentations(color: Color, editor: TextEditor, context: ColorPresentationContext): AssistantArray<ColorPresentation>;
+}
 
 interface CompletionAssistant {
     provideCompletionItems(editor: TextEditor, context: CompletionContext): AssistantArray<CompletionItem>;
@@ -56,18 +66,19 @@ interface TaskAssistant {
 declare class Charset {
     constructor(characters?: string);
 
-    static alphanumeric: Charset;
-    static digits: Charset;
-    static letters: Charset;
-    static lower: Charset;
-    static newlines: Charset;
-    static symbols: Charset;
-    static upper: Charset;
-    static whitespace: Charset;
-    static whitespaceAndNewlines: Charset;
+    static alphanumeric: string;
+    static digits: string;
+    static letters: string;
+    static lower: string;
+    static newlines: string;
+    static symbols: string;
+    static upper: string;
+    static whitespace: string;
+    static whitespaceAndNewlines: string;
 
     concat(...charsets: Array<Charset>): Charset;
     intersect(...charsets: Array<Charset>): Charset;
+    invert(...charsets: Array<Charset>): Charset;
 }
 
 /// https://docs.nova.app/api-reference/clipboard/
@@ -79,14 +90,27 @@ declare interface Clipboard {
 
 /// https://docs.nova.app/api-reference/color/
 
-type ColorFormat = 'rgb' | 'hsl' | 'hsb' | 'p3' | 'hex';
-type ColorComponents = [number, number, number, number];
+declare enum ColorFormat {
+    rgb = 'rgb',
+    hsl = 'hsl',
+    hsb = 'hsb',
+    displayP3 = 'p3',
+}
+
+// type ColorComponents = [number, number, number, number];
 
 declare class Color {
-    constructor(format: ColorFormat, components: ColorComponents);
+    constructor(format: ColorFormat, components: number[]);
 
-    format: ColorFormat;
-    components: ColorComponents;
+    static rgb(red: number, green: number, blue: number, alpha?: number): any;
+    static hsl(hue: number, saturation: number, luminance: number, alpha?: number): any;
+    static hsb(hue: number, saturation: number, brightness: number, alpha?: number): any;
+    static displayP3(red: number, green: number, blue: number, alpha?: number): any;
+
+    convert(format: ColorFormat): Color;
+
+    readonly format: ColorFormat;
+    readonly components: number[];
 }
 
 /// https://docs.nova.app/api-reference/commands-registry/
@@ -108,6 +132,50 @@ interface CommandsRegistry {
     register<T>(name: string, callable: (this: T, ...params: any[]) => void, thisValue: T): Disposable;
     invoke(name: string, ...arguments: Transferrable[]): Promise<unknown>;
     invoke(name: string, textEditor: TextEditor, ...arguments: Transferrable[]): Promise<unknown>;
+}
+
+/// https://docs.nova.app/api-reference/color-information-context/
+
+interface ColorInformationContext {
+  readonly candidates: ColorCandidate[];
+}
+
+/// https://docs.nova.app/api-reference/color-candidate/
+
+interface ColorCandidate {
+  range: Range;
+  text: string;
+}
+
+/// https://docs.nova.app/api-reference/color-information/
+
+declare class ColorInformation {
+  constructor(range: Range, color: Color, kind?: string);
+
+  color: Color;
+  kind?: string;
+  range: Range;
+  usesFloats?: boolean;
+  format?: ColorFormat;
+}
+
+/// https://docs.nova.app/api-reference/color-presentation-context/
+
+interface ColorPresentationContext {
+  range: Range;
+}
+
+/// https://docs.nova.app/api-reference/color-presentation/
+
+declare class ColorPresentation {
+  constructor(label: string, kind?: any)
+
+  additionalTextEdits: TextEdit[];
+  format?: ColorFormat;
+  insertText?: string;
+  kind: string;
+  label: string;
+  usesFloats?: boolean;
 }
 
 /// https://docs.nova.app/api-reference/completion-context/
@@ -487,12 +555,7 @@ declare class LanguageClient {
     constructor(
         identifier: string,
         name: string,
-        serverOptions: {
-            type?: 'stdio' | 'socket' | 'pipe';
-            path: string;
-            args?: string[];
-            env?: { [key: string]: string };
-        },
+        serverOptions: ServerOptions,
         clientOptions: { initializationOptions?: any; syntaxes: string[] },
     );
 
@@ -507,6 +570,13 @@ declare class LanguageClient {
     sendNotification(method: string, parameters?: unknown): void;
     start(): void;
     stop(): void;
+}
+
+type ServerOptions = {
+  type?: 'stdio' | 'socket' | 'pipe';
+  path: string;
+  args?: string[];
+  env?: { [key: string]: string };
 }
 
 /// https://docs.nova.app/api-reference/notification-center/
