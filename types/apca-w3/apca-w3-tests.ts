294847a0808a5e72f727bd794876ba943779130e
Daniel Eden <dan.eden@me.com>
2022-01-11 15:31:04 +0000

    Fix tests

--- types/apca-w3/apca-w3-tests.ts
+++ types/apca-w3/apca-w3-tests.ts
@@ -1,6 +1,6 @@
 import { APCAcontrast, sRGBtoY } from 'apca-w3';
 
-const foreground = [240, 248, 255]; // $ExpectType [number]
-const background = [255, 0, 0]; // $ExpectType [number]
+const foreground = sRGBtoY([240, 248, 255]); // $ExpectType number
+const background = sRGBtoY([255, 0, 0]); // $ExpectType number
 
-APCAcontrast(sRGBtoY(foreground), sRGBtoY(background)); // $ExpectType number
+APCAcontrast(foreground, background); // $ExpectType string | number
