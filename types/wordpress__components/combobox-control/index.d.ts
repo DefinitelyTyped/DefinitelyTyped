fc7ed9d27d604c350d2da922c7bceb448315b98c
Payton Swick <payton@foolord.com>
2022-01-10 21:08:08 -0500

    Add types for ComboboxControl

---
+++ types/wordpress__components/combobox-control/index.d.ts
@@ -0,0 +1,19 @@
+import { ComponentType, ReactNode } from 'react';
+
+declare namespace ComboboxControl {
+    interface Props {
+        value: string | number;
+        label?: ReactNode | undefined;
+        options: { label: string | number; value: string }[];
+        onChange: (value: string | null) => void;
+        onFilterValueChange?: (value: string) => void;
+        hideLabelFromVision?: boolean | undefined;
+        help?: ReactNode | undefined;
+        allowReset?: boolean;
+        className?: string;
+        messages?: { selected: string };
+    }
+}
+declare const ComboboxControl: ComponentType<ComboboxControl.Props>;
+
+export default ComboboxControl;
