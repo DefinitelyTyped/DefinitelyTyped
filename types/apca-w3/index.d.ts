e7854a86ebee1f8b743cc8832302d20e6c782d68
Daniel Eden <dan.eden@me.com>
2022-01-12 10:55:08 +0000

    Apply suggestions from code review
    Co-authored-by: Piotr Błażejewicz (Peter Blazejewicz) <peterblazejewicz@users.noreply.github.com>

--- types/apca-w3/index.d.ts
+++ types/apca-w3/index.d.ts
@@ -11,7 +11,7 @@
  * @param bgY
  * The linear Y (luminance) of the background color. Must be between 0.0-1.1.
  *
- * @param places
+ * @param [places]
  * Number of places to round the value to. Defaults to -1, returning a signed
  * float. When set to 0 or more, rounds the result to that many decimal places.
  *
@@ -32,23 +32,11 @@ export function sRGBtoY(rgba: number[]): number;
 
 /**
  *
- * @param rgba
+ * @param [rgba]
  * An array of numbers representing the red, green, blue, and alpha components
  * of a color in the p3 display color space
  *
  * @returns
  * The linear Y (luminance) of the given rgba color (+0.0-1.1)
  */
-export function displayP3toY(rgba: number[]): number;
-
-/**
- * Convert a color to its rgba components
- *
- * @param colorIn
- * The color to convert. Can be a string (e.g. "#ffe4c4", "aliceblue"),
- * hexadecimal integer (0xFF0000), rgba array, or an Object with keys `r, g, b, a`
- * and number values.
- *
- * @returns
- * rgba components in a float array (`[r: number, g: number, b: number, a: number])
- */
+export function displayP3toY(rgba?: number[]): number;
