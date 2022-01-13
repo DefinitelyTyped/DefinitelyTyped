e0e391dff28bbed4c1b7e52b754ce2cbf53d0f86
unkonown <b1021091@fun.ac.jp>
2022-01-13 23:25:33 +0900

    bumped p5 version number from 1.3 to 1.4.0

--- types/p5/index.d.ts
+++ types/p5/index.d.ts
@@ -1,4 +1,4 @@
-// Type definitions for p5 1.3
+// Type definitions for p5 1.4.0
 // Project: https://github.com/processing/p5.js
 // Definitions by: p5-types <https://github.com/p5-types>
 //                 Pierre Krafft <https://github.com/Zalastax>
