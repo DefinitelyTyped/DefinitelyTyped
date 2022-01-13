a6e6bbd58bb8944fa3605309d56db52d24fc9202
unkonown <b1021091@fun.ac.jp>
2022-01-13 23:27:35 +0900

    fix typo "1.4.0"

--- types/p5/index.d.ts
+++ types/p5/index.d.ts
@@ -1,4 +1,4 @@
-// Type definitions for p5 1.4.0
+// Type definitions for p5 1.4
 // Project: https://github.com/processing/p5.js
 // Definitions by: p5-types <https://github.com/p5-types>
 //                 Pierre Krafft <https://github.com/Zalastax>
