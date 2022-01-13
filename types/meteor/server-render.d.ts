b5cd178f9e6cb4c7cda427a011784d4f4048dec1
Erik Demaine <edemaine@mit.edu>
2022-01-12 21:21:43 -0500

    Fix Meteor server-render method signatures
    Not optional, and more valid types for `html`

--- types/meteor/server-render.d.ts
+++ types/meteor/server-render.d.ts
@@ -1,22 +1,30 @@
 import * as http from 'http';
 declare module 'meteor/server-render' {
+    // NodeJS.ReadableStream only works on server.
+    // HTMLElement only works on client.
+    type Content = string | Content[] | NodeJS.ReadableStream | HTMLElement;
+
     interface Sink {
-        request?: http.IncomingMessage | undefined;
-        arch?: string | undefined;
-        head?: string | undefined;
-        body?: string | undefined;
-        htmlById?: { [key: string]: string } | undefined;
-        maybeMadeChanges?: boolean | undefined;
-        appendToHead?(html: string): void;
-        appendToBody?(html: string): void;
-        appendToElementById?(id: string, html: string): void;
-        renderIntoElementById?(id: string, html: string): void;
-        renderIntoElementById?(id: string, html: NodeJS.ReadableStream): void;
-        redirect?(location: string, code?: number): void;
-        setStatusCode?(code: number): void;
-        setHeader?(key: string, value: number | string | string[]): void;
-        getHeaders?(): http.IncomingHttpHeaders;
-        getCookies?(): { [key: string]: string };
+        // Server-only:
+        request?: http.IncomingMessage;
+        arch?: string;
+        head?: string;
+        body?: string;
+        htmlById?: { [key: string]: string };
+        maybeMadeChanges?: boolean;
+
+        // Client and server. Only client
+        appendToHead(html: Content): void;
+        appendToBody(html: Content): void;
+        appendToElementById(id: string, html: Content): void;
+        renderIntoElementById(id: string, html: Content): void;
+        redirect(location: string, code?: number): void;
+
+        // Server-only, but error-raising stubs provided to client:
+        setStatusCode(code: number): void;
+        setHeader(key: string, value: number | string | string[]): void;
+        getHeaders(): http.IncomingHttpHeaders;
+        getCookies(): { [key: string]: string };
     }
 
     type Callback = (sink: Sink) => Promise<any> | any;
