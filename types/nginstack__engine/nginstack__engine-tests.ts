19de28376a0dd766ff7aa33c23a6f9918bf4307c
Paulo Moreno <paulocmoreno@gmail.com>
2022-01-14 17:13:41 -0300

    feat: update @nginstack/engine definitions

--- types/nginstack__engine/nginstack__engine-tests.ts
+++ types/nginstack__engine/nginstack__engine-tests.ts
@@ -91,4 +91,4 @@ database.logout(); // $ExpectType void
 function testMajorVersions(prior: number, current: number): boolean {
     return current > prior;
 }
-testMajorVersions(53, 54); // $ExpectType boolean
+testMajorVersions(55, 56); // $ExpectType boolean
