a259d1977a6bf108473203897c9fa72fc246a739
Ray Martone <rmartone@gmail.com>
2022-01-13 20:24:45 -0500

    test

---
+++ types/typeflex/typeflex.tests.ts
@@ -0,0 +1,8 @@
+
+import * as Yoga from 'typeflex';
+const Node = Yoga.Node;
+const root = Node.create();
+root.setWidth(500);
+root.calculateLayout(500, 300, Yoga.DIRECTION_LTR);
+const layout = root.getComputedLayout()
+console.log(layout.width === 500);
