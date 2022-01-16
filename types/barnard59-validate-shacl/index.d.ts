fedfcd44da734957f9323fecffb6cb89781ae481
tpluscode <tpluscode@users.noreply.github.com>
2022-01-16 18:27:41 +0100

    feat(new types): barnard59-validate-shacl

---
+++ types/barnard59-validate-shacl/index.d.ts
@@ -0,0 +1,6 @@
+// Type definitions for barnard59-validate-shacl 0.3
+// Project: https://github.com/zazuko/barnard59-validate-shacl
+// Definitions by: tpluscode <https://github.com/tpluscode>
+// Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
+
+export * from './validate';
