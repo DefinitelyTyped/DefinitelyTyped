d27997d72a6e0e48801d81be8fe1f9d60c09c6df
Robrecht Meersman <robrechtmeersman@hotmail.com>
2022-01-18 17:36:44 +0100

    fix: restrict ActionSheetIOS userInterfaceStyle type
    reference: https://reactnative.dev/docs/actionsheetios#showactionsheetwithoptions

--- types/react-native/index.d.ts
+++ types/react-native/index.d.ts
@@ -6946,7 +6946,7 @@ export interface ActionSheetIOSOptions {
     message?: string | undefined;
     anchor?: number | undefined;
     tintColor?: ColorValue | ProcessedColorValue | undefined;
-    userInterfaceStyle?: string | undefined;
+    userInterfaceStyle?: 'light' | 'dark' | undefined;
     disabledButtonIndices?: number[] | undefined;
 }
 
--- types/react-native/v0.64/index.d.ts
+++ types/react-native/v0.64/index.d.ts
@@ -6934,7 +6934,7 @@ export interface ActionSheetIOSOptions {
     message?: string | undefined;
     anchor?: number | undefined;
     tintColor?: ColorValue | ProcessedColorValue | undefined;
-    userInterfaceStyle?: string | undefined;
+    userInterfaceStyle?: 'light' | 'dark' | undefined;
     disabledButtonIndices?: number[] | undefined;
 }
 
