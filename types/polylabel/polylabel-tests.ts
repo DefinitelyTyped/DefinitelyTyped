17c2c4ea16dfec6a911be409f2c2310304c6f3c2
FishOrBear <cay776@gmail.com>
2022-01-20 16:55:07 +0800

    Update polylabel-tests.ts

--- types/polylabel/polylabel-tests.ts
+++ types/polylabel/polylabel-tests.ts
@@ -1,6 +1,6 @@
 import polylabel from 'polylabel';
 
-const polygon:([number, number][])[] = [[[3116, 3071], [3118, 3068], [3108, 3102], [3751, 927]]];
+const polygon:Array<Array<[number, number]>> = [[[3116, 3071], [3118, 3068], [3108, 3102], [3751, 927]]];
 polylabel(polygon);
 polylabel(polygon, 1.0);
 polylabel(polygon, 1.0, true);
