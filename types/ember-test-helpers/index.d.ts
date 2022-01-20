c845c00f09b811f7d15fbcd942c96e6de4517ca5
Chris Krycho <hello@chriskrycho.com>
2022-01-20 09:32:47 -0700

    Ember v4 test infrastructure: fix relationships
    In 60fc4ff1, I incorrectly bumped the ember-test-helpers MSTSV, not
    remembering that that package itself is dead, replaced by a namespaced
    `@ember/test-helpers` version. This caused dependency hell: different
    versions of `ember-qunit` tried to resolve against `ember-test-helpers`
    at v1. This was fundamentally unresolveable, but happily was also
    just *wrong*, since `ember-qunit` v4 can point to `ember-test-helpers` v1 and *both* can resolve to `ember` v3.

--- types/ember-test-helpers/index.d.ts
+++ types/ember-test-helpers/index.d.ts
@@ -1,12 +1,12 @@
 // Type definitions for ember-test-helpers 1.0
 // Project: https://github.com/emberjs/ember-test-helpers#readme
-// Definitions by: Chris Krycho <https://github.com/chriskrycho>
+// Definitions by: Derek Wickern <https://github.com/dwickern>
+//                 Mike North <https://github.com/mike-north>
+//                 Chris Krycho <https://github.com/chriskrycho>
 //                 James C. Davis <https://github.com/jamescdavis>
 //                 Dan Freeman <https://github.com/dfreeman>
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
-// Minimum TypeScript Version: 4.4
-
-import EmberResolver from 'ember-resolver';
+// TypeScript Version: 3.7
 
 // NOTE: These types apply to ember-test-helper v0.7. The major
 // version had to be bumped for SemVer due to a breaking change
@@ -16,62 +16,96 @@ import EmberResolver from 'ember-resolver';
 // provides safety from breaking changes without bumping the major
 // version number
 
-import RSVP from "rsvp";
-import ApplicationInstance from '@ember/application/instance';
-import { TemplateFactory } from 'htmlbars-inline-precompile';
+/// <reference types="jquery" />
 
-export interface TestContext {
-    get(key: string): unknown;
-    getProperties<K extends string>(...keys: K[]): Pick<any, K>;
-    set<V>(key: string, value: V): V;
-    setProperties<P extends Record<string, unknown>>(hash: P): P;
-    render(template?: string | string[] | TemplateFactory): Promise<void>;
-    clearRender(): void;
-    factory(fullName: string): unknown;
-    // NOTE: this uses an intersection instead of something more normal because
-    // it is intentionally causing a collision between the two `factoryFor`
-    // definitions.
-    owner: ApplicationInstance & {
-        factoryFor(fullName: string, options?: {}): unknown;
-    };
-    pauseTest(): Promise<void>;
-    resumeTest(): void;
-    element: Element | Document;
+declare module 'ember-test-helpers' {
+    import Ember from 'ember';
+    import { TemplateFactory } from 'htmlbars-inline-precompile';
+    import RSVP from "rsvp";
+
+    interface ModuleCallbacks {
+        integration?: boolean | undefined;
+        unit?: boolean | undefined;
+        needs?: string[] | undefined;
+
+        beforeSetup?(assert?: any): void;
+        setup?(assert?: any): void;
+        teardown?(assert?: any): void;
+        afterTeardown?(assert?: any): void;
+
+        [key: string]: any;
+    }
+
+    interface TestContext {
+        get(key: string): any;
+        getProperties<K extends string>(...keys: K[]): Pick<any, K>;
+        set<V>(key: string, value: V): V;
+        setProperties<P extends { [key: string]: any }>(hash: P): P;
+        on(actionName: string, handler: (this: TestContext, ...args: any[]) => any): void;
+        send(actionName: string): void;
+        $: JQueryStatic;
+        subject(options?: {}): any;
+        render(template?: string | string[] | TemplateFactory): Promise<void>;
+        clearRender(): void;
+        registry: Ember.Registry;
+        container: Ember.Container;
+        dispatcher: Ember.EventDispatcher;
+        application: Ember.Application;
+        register(fullName: string, factory: any): void;
+        factory(fullName: string): any;
+        inject: {
+            controller(name: string, options?: { as: string }): any;
+            service(name: string, options?: { as: string }): any;
+        };
+        owner: Ember.ApplicationInstance & {
+            factoryFor(fullName: string, options?: {}): any;
+        };
+        pauseTest(): Promise<void>;
+        resumeTest(): void;
+        element: Element;
+    }
+
+    class TestModule {
+        constructor(name: string, callbacks?: ModuleCallbacks);
+        constructor(name: string, description?: string, callbacks?: ModuleCallbacks);
+
+        name: string;
+        subjectName: string;
+        description: string;
+        isIntegration: boolean;
+        callbacks: ModuleCallbacks;
+        context: TestContext;
+        resolver: Ember.Resolver;
+
+        setup(assert?: any): RSVP.Promise<void>;
+        teardown(assert?: any): RSVP.Promise<void>;
+        getContext(): TestContext;
+        setContext(context: TestContext): void;
+    }
+
+    class TestModuleForAcceptance extends TestModule {}
+    class TestModuleForIntegration extends TestModule {}
+    class TestModuleForComponent extends TestModule {}
+    class TestModuleForModel extends TestModule {}
+
+    function getContext(): TestContext | undefined;
+    function setContext(context: TestContext): void;
+    function unsetContext(): void;
+    function setResolver(resolver: Ember.Resolver): void;
 }
 
+declare module 'ember-test-helpers/wait' {
+    import RSVP from "rsvp";
 
-export interface ModuleCallbacks {
-    integration?: boolean | undefined;
-    unit?: boolean | undefined;
-    needs?: string[] | undefined;
+    interface WaitOptions {
+        waitForTimers?: boolean | undefined;
+        waitForAJAX?: boolean | undefined;
+        waitForWaiters?: boolean | undefined;
+    }
 
-    beforeSetup?(assert?: any): void;
-    setup?(assert?: any): void;
-    teardown?(assert?: any): void;
-    afterTeardown?(assert?: any): void;
-
-    [key: string]: any;
+    export default function wait(options?: WaitOptions): RSVP.Promise<void>;
 }
 
-export class TestModule {
-    constructor(name: string, callbacks?: ModuleCallbacks);
-    constructor(name: string, description?: string, callbacks?: ModuleCallbacks);
-
-    name: string;
-    subjectName: string;
-    description: string;
-    isIntegration: boolean;
-    callbacks: ModuleCallbacks;
-    context: TestContext;
-    resolver: EmberResolver;
-
-    setup(assert?: any): RSVP.Promise<void>;
-    teardown(assert?: any): RSVP.Promise<void>;
-    getContext(): TestContext;
-    setContext(context: TestContext): void;
+declare module 'ember-test-helpers/has-ember-version' {
+    export default function hasEmberVersion(major: number, minor: number): boolean;
 }
-
-export function getContext(): TestContext | undefined;
-export function setContext(context: TestContext): void;
-export function unsetContext(): void;
-export function setResolver(resolver: EmberResolver): void;
--- types/ember-test-helpers/wait.d.ts
+++
@@ -1,9 +0,0 @@
-import RSVP from "rsvp";
-
-export interface WaitOptions {
-    waitForTimers?: boolean | undefined;
-    waitForAJAX?: boolean | undefined;
-    waitForWaiters?: boolean | undefined;
-}
-
-export default function wait(options?: WaitOptions): RSVP.Promise<void>;
