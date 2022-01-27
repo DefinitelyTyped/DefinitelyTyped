5dcd856349dc7cb5f7ab2bab159d1968f6572351
Kenrick <kenrick95@users.noreply.github.com>
2022-01-28 04:54:48 +0800

    Add ForwardedRef type in React 16 too (#58441)

--- types/react/v16/index.d.ts
+++ types/react/v16/index.d.ts
@@ -557,8 +557,10 @@ declare namespace React {
         displayName?: string | undefined;
     }
 
+    type ForwardedRef<T> = ((instance: T | null) => void) | MutableRefObject<T | null> | null;
+
     interface ForwardRefRenderFunction<T, P = {}> {
-        (props: PropsWithChildren<P>, ref: ((instance: T | null) => void) | MutableRefObject<T | null> | null): ReactElement | null;
+        (props: PropsWithChildren<P>, ref: ForwardedRef<T>): ReactElement | null;
         displayName?: string | undefined;
         // explicit rejected with `never` required due to
         // https://github.com/microsoft/TypeScript/issues/36826
