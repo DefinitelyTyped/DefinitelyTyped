8d503b831fc1118df387b072469f041e8a385212
Keith Kikta <keith.kikta@tevpro.com>
2022-01-27 09:46:53 -0600

    feat(ethereum-events): event fetcher is not exposed

--- types/ethereum-events/lib/core/event-fetcher.d.ts
+++
@@ -1,9 +0,0 @@
-import { EthereumEventsConfig } from '../../config';
-import Web3 from 'web3';
-import { EthereumEvent } from '../models';
-
-export class EventFetcher {
-    constructor(web3: Web3, contracts: any, options?: EthereumEventsConfig);
-
-    getEvents(fromBlock: any, toBlock: any): Promise<EthereumEvent[]>;
-}
