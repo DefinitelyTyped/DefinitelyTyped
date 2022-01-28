9e506e7492cd50b9a8f68d5466a023292737d9f4
Keith Kikta <keith.kikta@tevpro.com>
2022-01-27 23:28:33 -0600

    test(ethereum-events): attempt to add tests

---
+++ types/ethereum-events/ethereum-events-tests.ts
@@ -0,0 +1,12 @@
+import EthereumEvents from 'ethereum-events';
+
+const web3 = {}
+const contract = { name: 'Something', address: '0x0000000000000000000000000000000000000000', abi: {}}
+const pollOptions = {}
+
+const listener = new EthereumEvents(web3, contract, pollOptions);
+listener.on('block', async (blockNumber, events, done) => {
+    blockNumber; // $ExpectedType  BlockNumber
+    events // $ExpectedType EthereumEvents[]
+    done();
+})
