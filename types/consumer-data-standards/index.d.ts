0124aa10d7462e1a90e8ecfaa35c60bce9e36914
tom-schier <schiertxs@gmail.com>
2022-01-29 11:22:57 +1100

    Changed tslint

--- types/consumer-data-standards/index.d.ts
+++ types/consumer-data-standards/index.d.ts
@@ -1,4 +1,4 @@
-// Type definitions for (non-npm package ) consumer-data-standards 2.0
+// Type definitions for consumer-data-standards 2.0
 // Project: https://github.com/ConsumerDataStandardsAustralia
 // Definitions by: Tomas Schier <https://github.com/tom-schier>
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
