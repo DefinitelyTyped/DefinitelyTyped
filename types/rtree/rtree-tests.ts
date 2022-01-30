5c8093ec7a7469421b930a305049f8376d2dadb4
Chris Cooper <chris@propelleraero.com.au>
2022-01-31 09:58:30 +1100

    an attempt at fromJSON

--- types/rtree/rtree-tests.ts
+++ types/rtree/rtree-tests.ts
@@ -10,3 +10,6 @@ let intersections = myRTree.search({ x: 0.5, y: 0.5, w: 1, h: 1 });
 intersections = myRTree.remove({ x: 0.5, y: 0.5, w: 1, h: 1 }, 'notTest!');
 
 intersections = myRTree.remove({ x: 0.5, y: 0.5, w: 1, h: 1 });
+
+const json = myRTree.toJSON(); // $ExpectType string
+const myOtherRTree = RTree.fromJSON(json); // $ExpectType RTree
