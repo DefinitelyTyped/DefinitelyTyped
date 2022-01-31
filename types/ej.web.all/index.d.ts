31d02309dc5d94312f58445a935db6a161e2eeae
syncfusion-javascript <packages@syncfusion.com>
2022-01-31 10:56:00 +0530

    19.4.48 added

--- types/ej.web.all/index.d.ts
+++ types/ej.web.all/index.d.ts
@@ -8,8 +8,8 @@
 
 /*!
 *  filename: ej.web.all.d.ts
-*  version : 19.4.0.38
-*  Copyright Syncfusion Inc. 2001 - 2021. All rights reserved.
+*  version : 19.4.0.48
+*  Copyright Syncfusion Inc. 2001 - 2022. All rights reserved.
 *  Use of this code is subject to the terms of our license.
 *  A copy of the current license can be obtained at any time by e-mailing
 *  licensing@syncfusion.com. Any infringement will be prosecuted under
