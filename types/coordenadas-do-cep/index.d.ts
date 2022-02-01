48e21c38b5fe4e7ee7cca31f6f78aa1ce057b7f0
Carlos Alencar <iuryalencar@poli.ufrj.br>
2022-02-01 13:46:56 -0300

    fix w/o module

--- types/coordenadas-do-cep/index.d.ts
+++ types/coordenadas-do-cep/index.d.ts
@@ -3,49 +3,47 @@
 // Definitions by: Carlos Alencar <https://github.com/cybalencar96>
 // Definitions: <https://github.com/DefinitelyTyped/DefinitelyTyped>
 
-declare module 'coordenadas-do-cep' {
-    export function getByEndereco(address: string): Coordinates;
-    export function getByCep(cep: string): CepWithCoordinates;
-    export function getInfoCep(cep: string): InfoCep;
-    export function getDistEntreCeps(cep1: string, cep2: string): number;
-    export function getDistEntreEnderecos(end1: string, end2: string): number;
-    export function getDistancia(coord1: Coordinates, coord2: Coordinates): number;
-    export function setOpcoes(options: Options): void;
+export function getByEndereco(address: string): Coordinates;
+export function getByCep(cep: string): CepWithCoordinates;
+export function getInfoCep(cep: string): InfoCep;
+export function getDistEntreCeps(cep1: string, cep2: string): number;
+export function getDistEntreEnderecos(end1: string, end2: string): number;
+export function getDistancia(coord1: Coordinates, coord2: Coordinates): number;
+export function setOpcoes(options: Options): void;
 
-    export interface CepWithCoordinates {
-        cep: string;
-        logradouro: string;
-        complemento: string;
-        bairro: string;
-        localidade: string;
-        uf: string;
-        unidade: string;
-        ibge: string;
-        gia: string;
-        lat: number;
-        lon: number;
-    }
+export interface CepWithCoordinates {
+    cep: string;
+    logradouro: string;
+    complemento: string;
+    bairro: string;
+    localidade: string;
+    uf: string;
+    unidade: string;
+    ibge: string;
+    gia: string;
+    lat: number;
+    lon: number;
+}
 
-    export interface InfoCep {
-        cep: string;
-        logradouro: string;
-        complemento: string;
-        bairro: string;
-        localidade: string;
-        uf: string;
-        unidade: string;
-        ibge: string;
-        gia: string;
-    }
+export interface InfoCep {
+    cep: string;
+    logradouro: string;
+    complemento: string;
+    bairro: string;
+    localidade: string;
+    uf: string;
+    unidade: string;
+    ibge: string;
+    gia: string;
+}
 
-    export interface Coordinates {
-        lat: number;
-        lon: number;
-    }
+export interface Coordinates {
+    lat: number;
+    lon: number;
+}
 
-    export interface Options {
-        busca_aproximada?: boolean;
-        precisao?: number;
-        casas_dec_dist?: number;
-    }
+export interface Options {
+    busca_aproximada?: boolean;
+    precisao?: number;
+    casas_dec_dist?: number;
 }
