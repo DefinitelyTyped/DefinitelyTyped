93a9d210544e4ea32a40bcb28d59c62af3fa0f24
Tomasz Pluskiewicz <tpluscode@users.noreply.github.com>
2022-02-04 13:20:41 +0100

    add maintainer

--- types/rdfjs__dataset/index.d.ts
+++ types/rdfjs__dataset/index.d.ts
@@ -1,6 +1,7 @@
 // Type definitions for @rdfjs/dataset 2.0
 // Project: https://github.com/rdfjs-base/dataset
 // Definitions by: tpluscode <https://github.com/tpluscode>
+//                 Jesse Wright <https://github.com/jeswr>
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
 
 import { BaseQuad, DataFactory, DefaultGraph, Quad } from 'rdf-js';
