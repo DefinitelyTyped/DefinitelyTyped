b57213703d7859a5d84aae61c3ab9fd08543579d
Tomasz Pluskiewicz <tpluscode@users.noreply.github.com>
2022-02-04 13:20:53 +0100

    add maintainer

--- types/rdfjs__term-set/index.d.ts
+++ types/rdfjs__term-set/index.d.ts
@@ -1,6 +1,7 @@
 // Type definitions for @rdfjs/term-set 2.0
 // Project: https://github.com/rdfjs-base/term-set
 // Definitions by: tpluscode <https://github.com/tpluscode>
+//                 Jesse Wright <https://github.com/jeswr>
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
 
 import { Term } from "rdf-js";
