9c3b23f4de7f795e1722cea4a7d127e11d3faf60
sapphi-red <green@sapphi.red>
2022-02-05 01:06:38 +0900

    use jsdoc comments with pica

--- types/pica/index.d.ts
+++ types/pica/index.d.ts
@@ -7,67 +7,141 @@
 
 declare namespace pica {
     interface PicaOptions {
-        // tile width/height.
-        // Images are processed by regions, to restrict peak memory use. Default 1024.
+        /**
+         * tile width/height.
+         *
+         * Images are processed by regions, to restrict peak memory use.
+         *
+         * @default 1024
+         */
         tile?: number | undefined;
-        // list of features to use.
-        // Default is [ 'js', 'wasm', 'ww' ]. Can be [ 'js', 'wasm', 'cib', 'ww' ] or [ 'all' ].
-        // Note, `cib` is buggy in Chrome and not supports default `mks2013` filter.
+        /**
+         * list of features to use.
+         *
+         * Note, `cib` is buggy in Chrome and not supports default `mks2013` filter.
+         *
+         * @default ['js', 'wasm', 'ww']
+         */
         features?: Array<'js' | 'wasm' | 'cib' | 'ww'> | ['all'] | undefined;
-        // cache timeout, ms. Webworkers create is not fast.
-        // This option allow reuse webworkers effectively. Default 2000.
+        /**
+         * cache timeout, ms. Webworkers create is not fast.
+         *
+         * This option allow reuse webworkers effectively.
+         *
+         * @default 2000
+         */
         idle?: number | undefined;
-        // max webworkers pool size. Default is autodetected CPU count, but not more than 4.
+        /**
+         * max webworkers pool size.
+         *
+         * Default is autodetected CPU count, but not more than 4.
+         */
         concurrency?: number | undefined;
-        // function which returns a new canvas, used internally by pica.
-        // Default returns a <canvas> element but this function could return an OffscreenCanvas
-        // instead (to run pica in a Service Worker).
+        /**
+         * function which returns a new canvas, used internally by pica.
+         *
+         * Default returns a <canvas> element but this function could return an OffscreenCanvas instead (to run pica in a Service Worker).
+         */
         createCanvas?(width: number, height: number): HTMLCanvasElement;
     }
 
     interface PicaResizeOptions {
-        // 0..3. deprecated, use `.filter` instead.
+        /**
+         * deprecated use `.filter` instead.
+         *
+         * @deprecated
+         */
         quality?: 0 | 1 | 2 | 3 | undefined;
-        // filter name. 'box' or 'hamming' or 'lanczos2' or 'lanczos3' or 'mks2013'. Default = 'mks2013'.
-        // `mks2013` does both resize and sharpening, it's optimal and not recommended to change.
+        /**
+         * filter name
+         *
+         * `mks2013` does both resize and sharpening, it's optimal and not recommended to change.
+         *
+         * @default 'mks2013'
+         */
         filter?: 'box' | 'hamming' | 'lanczos2' | 'lanczos3' | 'mks2013' | undefined;
-        // >=0, in percents. Default = 0 (off). Usually between 100 to 200 is good.
-        // Note, `mks2013` filter already does optimal sharpening.
+        /**
+         * in percents, >=0.
+         *
+         * Usually between 100 to 200 is good.
+         * Note, `mks2013` filter already does optimal sharpening.
+         *
+         * @default 0 (off)
+         */
         unsharpAmount?: number | undefined;
-        //  0.5..2.0. By default it's not set. Radius of Gaussian blur.
-        // If it is less than 0.5, Unsharp Mask is off. Big values are clamped to 2.0.
+        /**
+         * 0.5..2.0.
+         *
+         * By default it's not set. Radius of Gaussian blur.
+         * If it is less than 0.5, Unsharp Mask is off. Big values are clamped to 2.0.
+         */
         unsharpRadius?: number | undefined;
-        // 0..255. Default = 0. Threshold for applying unsharp mask.
+        /**
+         * 0..255
+         *
+         * Threshold for applying unsharp mask.
+         *
+         * @default 0
+         */
         unsharpThreshold?: number | undefined;
-        // Promise instance. If defined, current operation will be terminated on rejection.
+        /**
+         * If defined, current operation will be terminated on rejection.
+         */
         cancelToken?: Promise<unknown> | undefined;
     }
 
     interface PicaResizeBufferOptions {
-        // Uint8Array with source data.
+        /** Uint8Array with source data. */
         src: number[];
-        // src image width.
+        /** src image width. */
         width: number;
-        // src image height.
+        /** src image height. */
         height: number;
-        // output width, >=0, in pixels.
+        /** output width, >=0, in pixels. */
         toWidth: number;
-        // output height, >=0, in pixels.
+        /** output height, >=0, in pixels. */
         toHeigh: number;
-        // 0..3. deprecated, use `.filter` instead.
+        /**
+         * deprecated use `.filter` instead.
+         *
+         * @deprecated
+         */
         quality?: 0 | 1 | 2 | 3 | undefined;
-        // filter name. 'box' or 'hamming' or 'lanczos2' or 'lanczos3' or 'mks2013'. Default = 'mks2013'.
-        // `mks2013` does both resize and sharpening, it's optimal and not recommended to change.
+        /**
+         * filter name
+         *
+         * `mks2013` does both resize and sharpening, it's optimal and not recommended to change.
+         *
+         * @default 'mks2013'
+         */
         filter?: 'box' | 'hamming' | 'lanczos2' | 'lanczos3' | 'mks2013' | undefined;
-        // >=0, in percents. Default = 0 (off). Usually between 100 to 200 is good.
-        // Note, `mks2013` filter already does optimal sharpening.
+        /**
+         * in percents, >=0.
+         *
+         * Usually between 100 to 200 is good.
+         * Note, `mks2013` filter already does optimal sharpening.
+         *
+         * @default 0 (off)
+         */
         unsharpAmount?: number | undefined;
-        // 0.5..2.0. Radius of Gaussian blur.
-        // If it is less than 0.5, Unsharp Mask is off. Big values are clamped to 2.0.
+        /**
+         * 0.5..2.0.
+         *
+         * By default it's not set. Radius of Gaussian blur.
+         * If it is less than 0.5, Unsharp Mask is off. Big values are clamped to 2.0.
+         */
         unsharpRadius?: number | undefined;
-        // 0..255. Default = 0. Threshold for applying unsharp mask.
+        /**
+         * 0..255
+         *
+         * Threshold for applying unsharp mask.
+         *
+         * @default 0
+         */
         unsharpThreshold?: number | undefined;
-        // Optional. Output buffer to write data, if you don't wish pica to create new one.
+        /**
+         * Optional. Output buffer to write data, if you don't wish pica to create new one.
+         */
         dest?: string | undefined;
     }
 
