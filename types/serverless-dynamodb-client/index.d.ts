3b87cd8dd0d1b3c47b1f89f68c7126fba523246c
Cody Pope <cody@codys-mac-mini.lan>
2022-02-04 22:13:29 -0500

    adjusting due to linting

--- types/serverless-dynamodb-client/index.d.ts
+++ types/serverless-dynamodb-client/index.d.ts
@@ -1,2 +1,12 @@
+// Type definitions for serverless-dynamodb-client 0.0
+// Project: https://github.com/99x/serverless-dynamodb-client
+// Definitions by: Cody Pope <https://github.com/cody-pope>
+// Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
+
 /// <reference types="node" />
-/// <reference path="serverless-dynamodb-client.d.ts" />
+
+import { DynamoDB } from 'aws-sdk';
+import { DocumentClient } from 'aws-sdk/clients/dynamodb';
+
+export const doc: DocumentClient;
+export const raw: DynamoDB;
--- types/serverless-dynamodb-client/serverless-dynamodb-client.d.ts
+++
@@ -1,5 +0,0 @@
-import { DynamoDB } from 'aws-sdk';
-import { DocumentClient } from 'aws-sdk/clients/dynamodb';
-
-export const doc: DocumentClient;
-export const raw: DynamoDB;
