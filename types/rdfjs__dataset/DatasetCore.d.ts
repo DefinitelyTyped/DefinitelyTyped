22829daf826dd23ab029dcbde511285c01490223
tpluscode <tpluscode@users.noreply.github.com>
2022-02-07 07:42:53 +0100

    refactor: remove redundant redeclared method

--- types/rdfjs__dataset/DatasetCore.d.ts
+++ types/rdfjs__dataset/DatasetCore.d.ts
@@ -1,7 +1,6 @@
 import * as RDF from 'rdf-js';
 
-interface DatasetCore<Q extends RDF.BaseQuad = RDF.Quad> extends RDF.DatasetCore<Q, Q> {
-    match(subject?: RDF.Term | null, predicate?: RDF.Term | null, object?: RDF.Term | null, graph?: RDF.Term | null): DatasetCore<Q>;
+interface DatasetCore<Q extends RDF.BaseQuad = RDF.Quad> extends RDF.DatasetCore<Q> {
 }
 
 // tslint:disable-next-line no-unnecessary-class
