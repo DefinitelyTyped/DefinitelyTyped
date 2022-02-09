3583e9f937781c71e42f3f9a1980f6419405a486
Rajas Paranjpe <52586855+ChocolateLoverRaj@users.noreply.github.com>
2022-02-09 08:30:42 -0800

    Split fn type into multiple lines

--- types/map-values/index.d.ts
+++ types/map-values/index.d.ts
@@ -3,6 +3,9 @@
 // Definitions by: Rajas Paranjpe <https://github.com/ChocolateLoverRaj>
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
 
-declare function mapValues<TKey extends string | number | symbol, TValue, TNewValue = TValue>(obj: Record<TKey, TValue>, mapFn: (value: TValue, key: TKey, obj: Record<TKey, TValue>) => TNewValue): Record<TKey, TNewValue>;
+declare function mapValues<TKey extends string | number | symbol, TValue, TNewValue = TValue>(
+  obj: Record<TKey, TValue>, 
+  mapFn: (value: TValue, key: TKey, obj: Record<TKey, TValue>) => TNewValue
+): Record<TKey, TNewValue>;
 
 export = mapValues;
