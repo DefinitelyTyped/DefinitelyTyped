0e5864f079c15e0885fcd9d13be3723345630903
Rajas Paranjpe <52586855+ChocolateLoverRaj@users.noreply.github.com>
2022-02-10 13:08:27 -0800

    Remove trailing ` `

--- types/map-values/index.d.ts
+++ types/map-values/index.d.ts
@@ -4,7 +4,7 @@
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
 
 declare function mapValues<TKey extends string | number | symbol, TValue, TNewValue = TValue>(
-  obj: Record<TKey, TValue>, 
+  obj: Record<TKey, TValue>,
   mapFn: (value: TValue, key: TKey, obj: Record<TKey, TValue>) => TNewValue
 ): Record<TKey, TNewValue>;
 
