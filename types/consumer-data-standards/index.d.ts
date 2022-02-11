4a930fccf47012eaad4cb24fef3a27e7e8a10759
tom-schier <schiertxs@gmail.com>
2022-02-11 16:36:15 +1100

    Added old header

--- types/consumer-data-standards/index.d.ts
+++ types/consumer-data-standards/index.d.ts
@@ -1,7 +1,8 @@
-// Type definitions for consumer-data-standards 2.0
+// Type definitions for non-npm package consumer-data-standards-browser 1.2
 // Project: https://github.com/ConsumerDataStandardsAustralia
 // Definitions by: Tomas Schier <https://github.com/tom-schier>
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
+export * from './banking';
 export * as admin from "./admin";
 export * as banking from "./banking";
 export * as common from "./common";
