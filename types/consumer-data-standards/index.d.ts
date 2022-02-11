611a0f9761ab69a4160c8b7712dbb90a6c4f4c90
tom-schier <schiertxs@gmail.com>
2022-02-11 16:31:40 +1100

    Consolidated register  definitions into one index.d.ts

--- types/consumer-data-standards/index.d.ts
+++ types/consumer-data-standards/index.d.ts
@@ -2,10 +2,11 @@
 // Project: https://github.com/ConsumerDataStandardsAustralia
 // Definitions by: Tomas Schier <https://github.com/tom-schier>
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
-/// <reference types="./register" />
-/// <reference types="./dcr" />
 
-export * from "./admin";
-export * from "./banking";
-export * from "./common";
-export * from "./energy";
+
+export * as admin from "./admin";
+export * as banking from "./banking";
+export * as common from "./common";
+export * as energy from "./energy";
+export * as dcr from "./dcr";
+export * as register from "./register";
--- types/consumer-data-standards/register/Links.d.ts
+++
@@ -1,9 +0,0 @@
-/* These are the schema definitions stipulated by the Data Standards Body for the register api. */
-
-export interface Links {
-  /**
-   * Fully qualified link to this API call
-   */
-  self: string;
-  [k: string]: unknown;
-}
