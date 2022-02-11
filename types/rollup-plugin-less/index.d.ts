7b09eba58ef53821c8aa927931529c30f7c03b60
Tristan <LeoDog896@hotmail.com>
2022-02-10 19:51:20 -0500

    Properly intergrate less dependency

--- types/rollup-plugin-less/index.d.ts
+++ types/rollup-plugin-less/index.d.ts
@@ -5,18 +5,16 @@
 // TypeScript Version: 2.3
 
 /// <reference types="node" />
+/// <reference types="jsdom" />
+/// <reference types="less" />
 import { Plugin } from 'rollup';
-import { render } from "less"
 
 declare namespace less {
-
-    type LessOptions = Parameters<typeof render>[1]
-
     interface Options {
         insert?: boolean;
         watch?: boolean;
-        option?: LessOptions,
-        plugins?: Exclude<LessOptions["plugins"], undefined>[0]
+        option?: Less.Options;
+        plugins?: Less.Plugin[];
         output?: string | boolean | (() => string);
         include?: string | string[];
         exclude?: string | string[];
