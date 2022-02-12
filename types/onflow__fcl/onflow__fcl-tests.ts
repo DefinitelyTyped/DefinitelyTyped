73def00f0cd5c77481b8e3c30151c05e5bc8b107
Louis Guitton <louisguitton93@gmail.com>
2022-02-12 10:52:50 +0100

    Add simple failing test

---
+++ types/onflow__fcl/onflow__fcl-tests.ts
@@ -0,0 +1,6 @@
+import * as fcl from "@onflow/fcl";
+
+fcl.config({
+  "accessNode.api": "https://access-testnet.onflow.org",
+  "discovery.wallet": "https://fcl-discovery.onflow.org/testnet/authn",
+});
