e1ebcc2e83dcb6116591f3b2bfa559b428a85975
tpluscode <tpluscode@users.noreply.github.com>
2022-02-12 11:19:32 +0100

    fix: toStream returns a node readable stream

--- types/rdf-dataset-ext/toStream.d.ts
+++ types/rdf-dataset-ext/toStream.d.ts
@@ -1,8 +1,9 @@
 import { BaseQuad, DatasetCore, Quad, Stream } from 'rdf-js';
+import { Readable } from 'stream';
 
 /**
  * Creates a `Stream` which emits all quads of the given dataset. Returns the created stream.
  */
-declare function toStream<Q extends BaseQuad = Quad>(dataset: DatasetCore<Q>): Stream<Q>;
+declare function toStream<Q extends BaseQuad = Quad>(dataset: DatasetCore<Q>): Stream<Q> & Readable;
 
 export = toStream;
