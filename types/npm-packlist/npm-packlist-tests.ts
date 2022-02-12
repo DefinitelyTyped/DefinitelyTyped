a574c9667dd06f88f4f5bbba802b372c7f0b88b4
Joshua Hoeflich <joshuaharry411@icloud.com>
2022-02-11 19:30:11 -0600

    Fix whitespace

--- types/npm-packlist/npm-packlist-tests.ts
+++ types/npm-packlist/npm-packlist-tests.ts
@@ -4,14 +4,10 @@ let result: string[] = packlist.sync();
 result = packlist.sync({});
 result = packlist.sync({path: 'foo'});
 result = packlist.sync({path: 'foo', packageJsonCache: new Map().set('test', 'test')});
-
 (async () => {
     result = await packlist();
     result = await packlist({});
     result = await packlist({path: 'foo'});
-
     packlist(undefined, (r) => result = r);
     packlist({path: 'foo'}, (r) => result = r);
 })();
-
-
