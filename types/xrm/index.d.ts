ef1929241114ea5bf2072950d82ac4b5cbf7749c
Phil Cole <filcole@gmail.com>
2022-02-12 23:13:08 +0000

    Add setFormEntityName() and FormItem.getVisible()

--- types/xrm/index.d.ts
+++ types/xrm/index.d.ts
@@ -1091,6 +1091,13 @@ declare namespace Xrm {
         refreshRibbon(refreshAll?: boolean): void;
 
         /**
+         * Sets the name of the table to be displayed on the form.
+         * @param name Name of the table to be displayed on the form.
+         * @see {@link https://docs.microsoft.com/en-us/powerapps/developer/model-driven-apps/clientapi/reference/formcontext-ui/setformentityname External Link: setFormEntityName (Client API reference)}
+         */
+        setFormEntityName(name: string): void;
+
+        /**
          * The business process flow API, used to interact with the business process flow control in a form.
          */
         process: Controls.ProcessControl;
@@ -3765,6 +3772,12 @@ declare namespace Xrm {
              * @returns The label.
              */
             getLabel(): string;
+
+            /**
+             * Returns a value that indicates whether the form is currently visible.
+             * @returns true if the form is visible; false otherwise.
+             */
+            getVisible(): boolean;
 
             /**
              * Navigates the user to this form.
