3afc48e4f517402fa0aa7d5eeb7c034fdff377b5
tpluscode <tpluscode@users.noreply.github.com>
2022-02-12 11:17:12 +0100

    refactor: inaccurate representation of package index

--- types/rdf-dataset-ext/toStream.d.ts
+++ types/rdf-dataset-ext/toStream.d.ts
@@ -1,3 +1,8 @@
-import { toStream } from '.';
+import { BaseQuad, DatasetCore, Quad, Stream } from 'rdf-js';
+
+/**
+ * Creates a `Stream` which emits all quads of the given dataset. Returns the created stream.
+ */
+declare function toStream<Q extends BaseQuad = Quad>(dataset: DatasetCore<Q>): Stream<Q>;
 
 export = toStream;
