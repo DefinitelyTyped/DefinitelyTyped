b472ac6694c65158e50e2fc47e65e3af46e90c55
Marcus Blättermann <marcus@essenmitsosse.de>
2022-02-17 21:17:06 +0100

    add d.ts for ramda count function

---
+++ types/ramda/test/count-tests.ts
@@ -0,0 +1,8 @@
+import * as R from 'ramda';
+
+() => {
+  const numbers = [1.0, 1.1, 1.2, 2.0, 3.0, 2.2];
+  const letters = R.split('', 'abcABCaaaBBc');
+  R.count(R.gt(2))(numbers); // => 2
+  R.count(R.equals('a'))(letters); // => 4
+};
