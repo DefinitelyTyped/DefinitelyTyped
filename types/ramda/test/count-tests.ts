e9bc0706d9bf6d6f456cb99b610f7da54260f845
Marcus Blättermann <marcus@essenmitsosse.de>
2022-02-19 14:39:18 +0100

    Add negative test cases

--- types/ramda/test/count-tests.ts
+++ types/ramda/test/count-tests.ts
@@ -5,4 +5,14 @@ import * as R from 'ramda';
   const letters = R.split('', 'abcABCaaaBBc');
   R.count(R.gt(2))(numbers); // => 2
   R.count(R.equals('a'))(letters); // => 4
+
+  // Mismatch between `predicate` and `list`
+  // $ExpectError
+  R.count(R.gt(2))(letters);
+  // $ExpectError
+  R.count(R.equals('a'))(numbers);
+
+  // Predicate doesn't return `boolean`
+  // $ExpectError
+  R.count(R.add(2));
 };
