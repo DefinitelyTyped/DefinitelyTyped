1749b3fe0f13143107695b9a284fdecbc5d0cc23
Sergey Bakulin <sbakulin@b2broker.net>
2022-02-20 20:40:24 +0300

    test(http): add custom `IncomingMessage` and `ServerResponse`

--- types/node/v16/node-tests.ts
+++ types/node/v16/node-tests.ts
@@ -107,7 +107,7 @@ import * as trace_events from 'node:trace_events';
             foo: number;
         }
 
-        class MyServerResponse extends http.ServerResponse {
+        class MyServerResponse<Request extends http.IncomingMessage = http.IncomingMessage> extends http.ServerResponse<Request> {
             foo: string;
         }
 
