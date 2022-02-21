be1e55bc846d4a88a731456c4115136876ee82d7
avwo <avwu@qq.com>
2022-02-21 15:03:09 +0800

    refactor: refine types

--- types/whistle/index.d.ts
+++ types/whistle/index.d.ts
@@ -15,12 +15,8 @@
  import { IncomingMessage, ServerResponse, Server } from 'http';
  import { Request, Response, NextFunction } from 'express';
  import { EventEmitter } from 'events';
- import {
-   WhistleRuntimeInfo,
-   WhistleFile,
-   WhistleStorage,
-   WhistlePluginOptions,
-} from './plugin';
+ import { ParsedUrlQuery } from 'querystring';
+ import LRUCache from 'lru-cache';
 
 type StrExt = string | false;
 
@@ -118,7 +114,7 @@ type WhistleSecureFilter = ((item: WhistleSession, clientIp?: string, filter?: s
    blockPluginList?: boolean;
    webUIPath?: string;
    certDir?: string;
-   middlewares?: string;
+   middleware?: string;
    uiMiddleware?: string;
    cmdName?: string;
    dnsServer?: string;
@@ -168,6 +164,141 @@ export const enum Level {
   Debug = 'debug',
 }
 
+ /// <reference types="node" />
+
+
+
+ interface WhistleFile {
+  index: number;
+  name: string;
+  data: string;
+  selected: boolean;
+}
+
+declare class WhistleStorage {
+  constructor(dir: string, filters?: object, disabled?: boolean);
+  count(): number;
+  existsFile(file: string): false | WhistleFile;
+  getFileList(origin: boolean): WhistleFile[];
+  writeFile(file: string, data: string): boolean;
+  updateFile(file: string, data: string): boolean;
+  readFile(file: string): string;
+  removeFile(file: string): boolean;
+  renameFile(file: string, newFile: string): boolean;
+  moveTo(fromName: string, toName: string): boolean;
+  setProperty(name: string, value: string): void;
+  hasProperty(file: string): boolean;
+  setProperties(obj: object): boolean;
+  getProperty(name: string): any;
+  removeProperty(name: string): void;
+}
+
+interface WhistleRuntimeInfo {
+  memUsage: NodeJS.MemoryUsage;
+  uptime: number;
+  cpuPercent: string;
+  startupTime: number;
+  updateTime: number;
+  httpRequests: number;
+  allHttpRequests: number;
+  wsRequests: number;
+  allWsRequests: number;
+  tunnelRequests: number;
+  totalHttpRequests: number;
+  totalWsRequests: number;
+  totalTunnelRequests: number;
+  totalAllHttpRequests: number;
+  totalAllWsRequests: number;
+  httpQps: number;
+  tunnelQps: number;
+  wsQps: number;
+  totalQps: number;
+  maxQps: number;
+  maxAllQps: number;
+  maxRss: number;
+  maxCpu: number;
+}
+
+interface WhistlePluginOptions {
+  name: string;
+  version: string;
+  debugMode?: boolean;
+  CUSTOM_CERT_HEADER: string;
+  ENABLE_CAPTURE_HEADER: string;
+  RULE_VALUE_HEADER: string;
+  SNI_VALUE_HEADER: string;
+  RULE_URL_HEADER: string;
+  MAX_AGE_HEADER: string;
+  ETAG_HEADER: string;
+  FULL_URL_HEADER: string;
+  REAL_URL_HEADER: string;
+  RELATIVE_URL_HEADER: string;
+  REQ_ID_HEADER: string;
+  PIPE_VALUE_HEADER: string;
+  CUSTOM_PARSER_HEADER: string;
+  STATUS_CODE_HEADER: string;
+  PLUGIN_REQUEST_HEADER: string;
+  LOCAL_HOST_HEADER: string;
+  HOST_VALUE_HEADER: string;
+  PROXY_VALUE_HEADER: string;
+  PAC_VALUE_HEADER: string;
+  METHOD_HEADER: string;
+  CLIENT_IP_HEADER: string;
+  CLIENT_PORT_HEAD: string;
+  UI_REQUEST_HEADER: string;
+  GLOBAL_VALUE_HEAD: string;
+  SERVER_NAME_HEAD: string;
+  COMMON_NAME_HEAD: string;
+  CERT_CACHE_INFO: string;
+  HOST_IP_HEADER: string;
+  REQ_FROM_HEADER: string;
+  config: {
+    name: string;
+    version: string;
+    localUIHost: string;
+    port: number;
+    sockets: number;
+    timeout: number;
+    baseDir: string;
+    uiport: number;
+    clientId: string;
+    uiHostList: string[];
+    pluginHosts: object;
+    host: string;
+    [propName: string]: any;
+  };
+  parseUrl(url: string): ParsedUrlQuery;
+  wsParser: {
+    getExtensions(res: any, isServer?: boolean): any;
+    getSender(socket: any, toServer?: boolean): any;
+    getReceiver(res: any, fromServer?: boolean, maxPayload?: number): any;
+  };
+  wrapWsReader(socket?: any, maxPayload?: number): any;
+  wrapWsWriter(socket?: any): any;
+  shortName: string;
+  Storage: WhistleStorage;
+  localStorage: WhistleStorage;
+  storage: WhistleStorage;
+  baseUrl: string;
+  LRU: LRUCache<string, any>;
+  getValue(key: string, cb: (value: string) => void): void;
+  getCert(domain: string, cb: (cert: any) => void): void;
+  getRootCA(cb: (cert: any) => void): void;
+  getHttpsStatus(cb: (status: any) => void): void;
+  getRuntimeInfo(cb: (info: any) => void): void;
+  updateRules(): void;
+  compose(options: any, cb: (err: any, data?: any) => void): void;
+  getRules(cb: (rules: any) => void): void;
+  getValues(cb: (values: any) => void): void;
+  getCustomCertsInfo(cb: (certs: any) => void): void;
+  isActive(cb: (active: boolean) => void): void;
+  ctx: any;
+  connect(opts: any, cb?: Function): any;
+  request(opts: any, cb?: Function): any;
+  [propName: string]: any;
+}
+
+
 type LogFn = (msg: Object, ...restMsg: Object[]) => void;
  
 interface WhistleResult {
@@ -194,7 +325,18 @@ type WhistleLevel = Level;
 
 type GetSession = (cb: (session: WhistleSession | '') => void) => void;
 type GetFrame = (cb: (Frames: WhistleFrame[] | '') => void) => void;
+type SetRules = (rules: string) => boolean;
+type PassThrough = (uri?: any, trailers?: any) => void;
 
+interface WriteHead {
+  (code: string | number, msg?: string, headers?: any): void;
+  (code: string | number, headers?: any): void;
+}
+
+interface RequestFn {
+  (uri: any, cb?: (res: any) => void, opts?: any): any;
+  (uri: any, opts?: any, cb?: (res: any) => void): any;
+}
 declare global {
   namespace Whistle {
     type Options = WhistleOptions;
@@ -267,16 +409,27 @@ declare global {
         isRexExp?: boolean;
         pattern?: string;
         customParser: boolean | '';
-
       };
       originalRes: {
         serverIp: string;
         statusCode: string;
       };
+      setReqRules: SetRules;
+      setResRules: SetRules;
+      writeHead: WriteHead;
+      request: RequestFn;
+      connect: RequestFn;
+      passThrough: PassThrough;
+      setHtml(html: string): void;
+      setUrl(url: string): void;
+      setHeader(key: string, value: string): void;
+      setRedirect(url: string): void;;
+      setLogin(login: boolean): void;
     }
     
     class PluginResponse extends ServerResponse {
-      
+      setReqRules: SetRules;
+      setResRules: SetRules;
     }
     
     class PluginServer extends Server {
@@ -285,21 +438,39 @@ declare global {
 
     class PluginUIRequest extends IncomingMessage {
 
-  }
-  
-  class PluginUIResponse extends ServerResponse {
-  
-  }
-  
-  class PluginUIServer extends Server {
-  
-  }
+    }
+    
+    class PluginUIResponse extends ServerResponse {
+    
+    }
+    
+    class PluginUIServer extends Server {
+    
+    }
 
-  type PluginHook = () => void;
+    type PluginHook = (server?: Server, options?: PluginOptions) => void;
 
-  interface Hooks {
-
-  }
+    interface PluginHooks {
+      uiServer?: PluginHook;
+      rulesServer?: PluginHook;
+      tunnelRulesServer?: PluginHook;
+      server?: PluginHook;
+      resRulesServer?: PluginHook;
+      statsServer?: PluginHook;
+      resStatsServer?: PluginHook;
+      reqRead?: PluginHook;
+      reqWrite?: PluginHook;
+      resRead?: PluginHook;
+      resWrite?: PluginHook;
+      wsReqRead?: PluginHook;
+      wsReqWrite?: PluginHook;
+      wsResRead?: PluginHook;
+      wsResWrite?: PluginHook;
+      tunnelReqRead?: PluginHook;
+      tunnelReqWrite?: PluginHook;
+      tunnelResRead?: PluginHook;
+      tunnelResWrite?: PluginHook;
+    }
   }
 }
  
