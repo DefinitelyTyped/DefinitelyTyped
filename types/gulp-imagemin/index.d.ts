df203903971d8c9faadacb8e93768c86c88d802d
Martin Badin <MBadin@gohealth.com>
2022-02-23 14:03:07 +0100

    [gulp-imagemin] Remove typescript version

--- types/gulp-imagemin/index.d.ts
+++ types/gulp-imagemin/index.d.ts
@@ -2,7 +2,6 @@
 // Project: https://github.com/sindresorhus/gulp-imagemin#readme
 // Definitions by: Romain Faust <https://github.com/romain-faust>
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
-// TypeScript Version: 2.2
 
 /// <reference types="node" />
 
