5bb72a5d2e8932f5902e0ce56a82be739db2d294
Sam Tobia <samtobia@gmail.com>
2022-02-24 15:47:36 -0500

    Update IceCandidate.d.ts

--- types/webrtc/IceCandidate.d.ts
+++ types/webrtc/IceCandidate.d.ts
@@ -3,24 +3,24 @@
 // https://developer.mozilla.org/en-US/docs/Web/API/RTCIceCandidate
 
 type RTCIceCandidateJSON = {
-  address: string
-  candidate: string
-  component: "rtp" | "rtcp"
-  foundation: string
-  port: number
-  priority: number
-  protocol: "tcp" | "udp"
-  relatedAddress: string | null
-  relatedPort: number | null
-  sdpMid: string
-  sdpMLineIndex: number | null
-  tcpType: "tcp" | null
-  type: string
-  usernameFragment: string
+    address: string
+    candidate: string
+    component: "rtp" | "rtcp"
+    foundation: string
+    port: number
+    priority: number
+    protocol: "tcp" | "udp"
+    relatedAddress: string | null
+    relatedPort: number | null
+    sdpMid: string
+    sdpMLineIndex: number | null
+    tcpType: "tcp" | null
+    type: string
+    usernameFragment: string
 }
 
 interface IRTCIceCandidate extends Readonly<RTCIceCandidateJSON> {
-  toJson(): RTCIceCandidateJSON;
+    toJson(): RTCIceCandidateJSON;
 }
 
 type RTCIceCandidate = RTCIceCandidateJSON | IRTCIceCandidate;
