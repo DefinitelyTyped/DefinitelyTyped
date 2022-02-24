49cf7c4185e020a062a52788b105b51c1f3ccde1
Jack Hedaya <jackehedaya@gmail.com>
2022-02-23 21:16:40 -0500

    Pass tests

--- types/human-crypto-keys/index.d.ts
+++ types/human-crypto-keys/index.d.ts
@@ -1,135 +1,139 @@
-// Type definitions for human-crypto-keys v0.1.4
+// Type definitions for human-crypto-keys 0.1
 // Project: https://github.com/ipfs-shipyard/js-human-crypto-keys
 // Definitions by: Jack Hedaya <https://github.com/jackHedaya>
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
 
 export function generateKeyPair(
-  /**
-   * The algorithm identifier and the respective parameters to generate a key pair
-   */
-  algorithm: Algorithm,
+    /**
+     * The algorithm identifier and the respective parameters to generate a key pair
+     */
+    algorithm: Algorithm,
 
-  /**
-   * Options to be used while composing keys
-   */
-  options?: Options
+    /**
+     * Options to be used while composing keys
+     */
+    options?: Options,
 ): Promise<FullKeyPair>;
 
 export function getKeyPairFromMnemonic(
-  /**
-   * The mnemonic provided as one of the recovery methods for a key pair
-   */
-  mnemonic: string,
+    /**
+     * The mnemonic provided as one of the recovery methods for a key pair
+     */
+    mnemonic: string,
 
-  /**
-   * The algorithm identifier and the respective parameters to generate a key pair
-   */
-  algorithm: Algorithm,
+    /**
+     * The algorithm identifier and the respective parameters to generate a key pair
+     */
+    algorithm: Algorithm,
 
-  /**
-   * Options to be used while composing keys
-   */
-  options?: Options
+    /**
+     * Options to be used while composing keys
+     */
+    options?: Options,
 ): Promise<SimpleKeyPair>;
 
 export function getKeyPairFromSeed(
-  /**
-   * The seed provided as one of the recovery methods for a key pair
-   */
-  seed: string,
+    /**
+     * The seed provided as one of the recovery methods for a key pair
+     */
+    seed: string,
 
-  /**
-   * The algorithm identifier and the respective parameters to generate a key pair
-   */
-  algorithm: Algorithm,
+    /**
+     * The algorithm identifier and the respective parameters to generate a key pair
+     */
+    algorithm: Algorithm,
 
-  /**
-   * Options to be used while composing keys
-   */
-  options?: Options
+    /**
+     * Options to be used while composing keys
+     */
+    options?: Options,
 ): Promise<SimpleKeyPair>;
 
-type Algorithm =
-  | ED25519Algorithm
-  | RSAAlgorithm
-  | ED25519Algorithm["id"]
-  | RSAAlgorithm["id"];
+type Algorithm = ED25519Algorithm | RSAAlgorithm | ED25519Algorithm['id'] | RSAAlgorithm['id'];
 
 interface ED25519Algorithm {
-  id: "ed25519";
+    id: 'ed25519';
 }
 
 interface RSAAlgorithm {
-  id: "rsa";
+    id: 'rsa';
 
-  /**
-   * @default 2048
-   */
-  modulusLength?: number;
-  /**
-   * @default 65537
-   */
-  publicExponent?: number;
+    /**
+     * @default 2048
+     */
+    modulusLength?: number;
+    /**
+     * @default 65537
+     */
+    publicExponent?: number;
 
-  /**
-   * @default 'PRIMEINC'
-   */
-  method?: string;
+    /**
+     * @default 'PRIMEINC'
+     */
+    method?: string;
 }
 
 interface FullKeyPair {
-  /**
-   * An object with the algorithm identifier and respective parameters that were used during generation
-   */
-  algorithm: Algorithm;
+    /**
+     * An object with the algorithm identifier and respective parameters that were used during generation
+     */
+    algorithm: Algorithm;
 
-  /**
-   * The mnemonic used to create a seed for generation
-   */
-  mnemonic: string;
+    /**
+     * The mnemonic used to create a seed for generation
+     */
+    mnemonic: string;
 
-  /**
-   * The seed used for generation
-   */
-  seed: string;
+    /**
+     * The seed used for generation
+     */
+    seed: string;
 
-  /**
-   * The generated private key composed in a specific format
-   */
-  privateKey: string;
+    /**
+     * The generated private key composed in a specific format
+     */
+    privateKey: string;
 
-  /**
-   * The generated public key composed in a specific format.
-   */
-  publicKey: string;
+    /**
+     * The generated public key composed in a specific format.
+     */
+    publicKey: string;
 }
 
-type SimpleKeyPair = Pick<FullKeyPair, "privateKey" | "publicKey">;
+type SimpleKeyPair = Pick<FullKeyPair, 'privateKey' | 'publicKey'>;
 
 interface Options {
-  /**
-   * The format in which the private key will be composed
-   *
-   * Keys can be composed in different formats and vary by algorithm. All formats available are described in the [Formats Section](https://github.com/ipfs-shipyard/js-crypto-key-composer/tree/initial-impl#formats) of [crypto-key-composer](https://github.com/ipfs-shipyard/js-crypto-key-composer) package.
-   */
-  privateKeyFormat?: string;
+    /**
+     * The format in which the private key will be composed
+     *
+     * Keys can be composed in different formats and vary by algorithm. All formats available are described in the
+     * [Formats Section](https://github.com/ipfs-shipyard/js-crypto-key-composer/tree/initial-impl#formats) of
+     * [crypto-key-composer](https://github.com/ipfs-shipyard/js-crypto-key-composer) package.
+     */
+    privateKeyFormat?: string;
 
-  /**
-   * The format in which the public key will be composed
-   *
-   * Keys can be composed in different formats and vary by algorithm. All formats available are described in the [Formats Section](https://github.com/ipfs-shipyard/js-crypto-key-composer/tree/initial-impl#formats) of [crypto-key-composer](https://github.com/ipfs-shipyard/js-crypto-key-composer) package.
-   */
-  publicKeyFormat?: string;
+    /**
+     * The format in which the public key will be composed
+     *
+     * Keys can be composed in different formats and vary by algorithm. All formats available are described in the
+     * [Formats Section](https://github.com/ipfs-shipyard/js-crypto-key-composer/tree/initial-impl#formats) of
+     * [crypto-key-composer](https://github.com/ipfs-shipyard/js-crypto-key-composer) package.
+     */
+    publicKeyFormat?: string;
 
-  /**
-   * The encryption algorithm that will be used to encrypt the private key
-   *
-   * For more information please read the [Encryption Algorithms](https://github.com/ipfs-shipyard/js-crypto-key-composer/tree/initial-impl#encryption-algorithms) Section of [crypto-key-composer](https://github.com/ipfs-shipyard/js-crypto-key-composer) package
-   */
-  encryptionAlgorithm?: object;
+    /**
+     * The encryption algorithm that will be used to encrypt the private key
+     *
+     * For more information please read the
+     * [Encryption Algorithms](https://github.com/ipfs-shipyard/js-crypto-key-composer/tree/initial-impl#encryption-algorithms)
+     * Section of [crypto-key-composer](https://github.com/ipfs-shipyard/js-crypto-key-composer) package
+     */
+    encryptionAlgorithm?: object;
 
-  /**
-   * The password to be used on the encryption of the private key
-   */
-  password?: string;
+    /**
+     * The password to be used on the encryption of the private key
+     */
+    password?: string;
 }
+
+export {};
