a0647a04cb82f92c92870bf19d0eab8152ae673b
Yue Yang <g1enyy0ung@gmail.com>
2022-02-25 18:42:41 +0800

    chore: update
    Signed-off-by: Yue Yang <g1enyy0ung@gmail.com>

--- types/react-alert/v2/index.d.ts
+++ types/react-alert/v2/index.d.ts
@@ -1,16 +1,17 @@
 // Type definitions for react-alert 2.4
 // Project: https://github.com/schiehll/react-alert
 // Definitions by: Steve Syrell <https://github.com/ssyrell>
+//                 Yue Yang <https://github.com/g1eny0ung>
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
 // TypeScript Version: 2.8
 
-import * as React from "react";
+import * as React from 'react';
 
 export interface AlertContainerProps {
     /**
-     * The offset of the alert from the page border, can be any number.
+     * The offset of the alert from the page border. Can be any number.
      *
-     * Default: 14.
+     * Default: 14
      */
     offset: number;
 
@@ -19,19 +20,17 @@ export interface AlertContainerProps {
      *
      * Default: 'bottom left'
      */
-    position: string;
+    position: 'bottom left' | 'bottom right' | 'top left' | 'top right';
 
     /**
      * The color theme of the alert. Can be [dark, light].
      *
      * Default: 'dark'
      */
-    theme: string;
+    theme: 'dark' | 'light';
 
     /**
-     * The time in milliseconds the alert is displayed. After this
-     * time ellapses, the alert will close itself. Use 0 to prevent self-closure
-     * (applies to all alerts).
+     * The time in miliseconds to the alert close itself. Use 0 to prevent auto close (apply to all alerts).
      *
      * Default: 5000
      */
@@ -42,35 +41,28 @@ export interface AlertContainerProps {
      *
      * Default: 'scale'
      */
-    transition: string;
+    transition: 'scale' | 'fade';
 }
 
 export interface AlertShowOptions {
     /**
-     * The time in milliseconds the alert is displayed. After this
-     * time ellapses, the alert will close itself. Use 0 to prevent self-closure.
+     * The time in miliseconds to the alert close itself. Use 0 to prevent auto close (apply to this alert only).
+     *
+     * Default: 5000
      */
-    time?: number | undefined;
+    time?: number;
 
     /**
      * The icon to show in the alert.
      *
      * Default: the icon which matches the type of alert to be shown.
      */
-    icon?: React.ReactNode | undefined;
+    icon?: React.ReactNode;
 
     /**
      * A callback function that will be called when the alert is closed.
      */
-    onClose?: (() => void) | undefined;
-
-    /**
-     * The type of alert to show. This will only be used when calling show().
-     * Can be [info, success, error].
-     *
-     * Default: 'info'
-     */
-    type?: string | undefined;
+    onClose?: () => void;
 }
 
 export default class AlertContainer extends React.Component<AlertContainerProps> {
