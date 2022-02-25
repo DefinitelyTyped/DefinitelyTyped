01ad0a80a58e2f3bd238fa121a5ca501eedc217d
Simon Siefke <simon.siefke@gmail.com>
2022-02-25 19:39:28 +0100

    add event target class

--- types/node/events.d.ts
+++ types/node/events.d.ts
@@ -687,6 +687,56 @@ declare module 'events' {
             static readonly CAPTURING_PHASE: number;
             static readonly NONE: number;
         }
+
+        interface EventTargetCallback {
+            (event: Event): void;
+        }
+
+        interface EventListener {
+            ({ handleEvent }: { handleEvent: EventTargetCallback }): void;
+        }
+
+        interface EventListenerOptions {
+            capture?: boolean;
+        }
+
+        interface AddEventListenerOptions extends EventListenerOptions {
+            once?: boolean;
+            passive?: boolean;
+            signal?: AbortSignal;
+        }
+
+        /** EventTarget is a DOM interface implemented by objects that can receive events and may have listeners for them. */
+        class EventTarget {
+            /**
+             * Appends an event listener for events whose type attribute value is type. The callback argument sets the callback that will be invoked when the event is dispatched.
+             *
+             * The options argument sets listener-specific options. For compatibility this can be a boolean, in which case the method behaves exactly as if the value was specified as options's capture.
+             *
+             * When set to true, options's capture prevents callback from being invoked when the event's eventPhase attribute value is BUBBLING_PHASE. When false (or not present), callback will not be invoked when event's eventPhase attribute value is CAPTURING_PHASE. Either way, callback will be invoked if event's eventPhase attribute value is AT_TARGET.
+             *
+             * When set to true, options's passive indicates that the callback will not cancel the event by invoking preventDefault(). This is used to enable performance optimizations described in § 2.8 Observing event listeners.
+             *
+             * When set to true, options's once indicates that the callback will only be invoked once after which the event listener will be removed.
+             *
+             * If an AbortSignal is passed for options's signal, then the event listener will be removed when signal is aborted.
+             *
+             * The event listener is appended to target's event listener list and is not appended if it has the same type, callback, and capture.
+             */
+            addEventListener(
+                type: string,
+                callback: EventTargetCallback | EventListener,
+                options?: AddEventListenerOptions,
+            ): void;
+            /** Dispatches a synthetic event event to target and returns true if either event's cancelable attribute value is false or its preventDefault() method was not invoked, and false otherwise. */
+            dispatchEvent(event: Event): boolean;
+            /** Removes the event listener in target's event listener list with the same type, callback, and options. */
+            removeEventListener(
+                type: string,
+                callback: EventTargetCallback | EventListener,
+                options?: EventListenerOptions,
+            ): void;
+        }
     }
 
     export = EventEmitter;
