13b5a0d5563215f1156b48e96181f7d165b54761
Irakli Gozalishvili <contact@gozala.io>
2022-03-01 12:08:35 -0800

    reuse Blob definition from buffer module

--- types/node/stream/consumers.d.ts
+++ types/node/stream/consumers.d.ts
@@ -1,17 +1,6 @@
-// Duplicates of interface in lib.dom.ts.
-// Duplicated here rather than referencing lib.dom.ts because doing so causes lib.dom.ts to be loaded for "test-all"
-// Which in turn causes tests to pass that shouldn't pass.
-//
-// This interface is not, and should not be, exported.
-interface Blob {
-    readonly size: number;
-    readonly type: string;
-    arrayBuffer(): Promise<ArrayBuffer>;
-    slice(start?: number, end?: number, contentType?: string): Blob;
-    text(): Promise<string>;
-}
 declare module 'stream/consumers' {
     import { Readable } from 'node:stream';
+    import { Blob } from 'buffer';
     function buffer(stream: NodeJS.ReadableStream | Readable | AsyncIterator<any>): Promise<Buffer>;
     function text(stream: NodeJS.ReadableStream | Readable | AsyncIterator<any>): Promise<string>;
     function arrayBuffer(stream: NodeJS.ReadableStream | Readable | AsyncIterator<any>): Promise<ArrayBuffer>;
