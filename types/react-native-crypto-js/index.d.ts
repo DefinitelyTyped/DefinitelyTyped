b93d865f5a8865b2aa67333c659408cb91e01480
Mufaddal Hamid <mufaddalhamidofficial@gmail.com>
2022-03-01 17:40:28 +0530

    lint changes

--- types/react-native-crypto-js/index.d.ts
+++ types/react-native-crypto-js/index.d.ts
@@ -1,10 +1,11 @@
 // Type definitions for react-native-crypto-js 1.0.0
 // Project: https://github.com/imchintan/react-native-crypto-js
-// Definitions by: Michael Zabka <https://github.com/misak113>
+// Definitions by: Mufaddal Hamid <https://github.com/mufaddalhamidofficial>
+//                 Michael Zabka <https://github.com/misak113>
 //                 Max Lysenko <https://github.com/maximlysenko>
 //                 Brendan Early <https://github.com/mymindstorm>
 //                 Doma <https://github.com/SevenOutman>
-//                 Mufaddal Hamid <https://github.com/mufaddalhamidofficial>
+//                 
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
 
 export = RNCryptoJS;
