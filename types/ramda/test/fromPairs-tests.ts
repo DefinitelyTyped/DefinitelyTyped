f4d3a9d3b2266cfba133a217d4ae20e328eeb4d3
somebody1234 <ehern.lee@gmail.com>
2022-03-06 20:24:33 +1000

    [ramda] fix: allow readonly in fromPairs

--- types/ramda/test/fromPairs-tests.ts
+++ types/ramda/test/fromPairs-tests.ts
@@ -5,4 +5,6 @@ import * as R from 'ramda';
 
   // $ExpectType { [index: string]: string; }
   R.fromPairs([[1, "a"], [2, "b"]]); // => { '1': 'a', '2': 'b' }
+
+  R.fromPairs([['a', 1], ['b', 2], ['c', 3]] as const); // => {a: 1, b: 2, c: 3}
 };
