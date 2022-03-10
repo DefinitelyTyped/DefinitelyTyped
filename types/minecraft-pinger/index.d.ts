8b4f968a55bf3557e166f0d9c0a6c7e5dc56dfc4
Grayson-code <northernstarmusic10@gmail.com>
2022-03-10 22:06:21 +0530

    fix stuffs

--- types/minecraft-pinger/index.d.ts
+++ types/minecraft-pinger/index.d.ts
@@ -4,93 +4,93 @@
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
 
 export interface Data {
-  /**
-   * Server Description , May not exist
-   */
-  description?: {
     /**
-     * Raw Text
+     * Server Description , May not exist
      */
-    text: string;
-    extra?: {
-      /**
-       * Color of the text
-       */
-      color?: string;
-      /**
-       * Raw Text
-       */
-      text: string;
-      /**
-       * If Text is bold
-       */
-      bold?: boolean;
-      /**
-       * If Text is strikethroughed
-       */
-      strikethrough?: boolean;
-
-      extra?: {
-        /**
-         * Color
-         */
-        color: string;
+    description?: {
         /**
          * Raw Text
          */
         text: string;
-      };
+        extra?: {
+            /**
+             * Color of the text
+             */
+            color?: string;
+            /**
+             * Raw Text
+             */
+            text: string;
+            /**
+             * If Text is bold
+             */
+            bold?: boolean;
+            /**
+             * If Text is strikethroughed
+             */
+            strikethrough?: boolean;
+
+            extra?: {
+                /**
+                 * Color
+                 */
+                color: string;
+                /**
+                 * Raw Text
+                 */
+                text: string;
+            };
+        };
     };
-  };
-  players: {
+    players: {
+        /**
+         * Current Number of players online
+         */
+        online: number;
+        /**
+         * Maximum Number of players that could be online
+         */
+        max: number;
+    };
+    version: {
+        /**
+         * Server Software & Version Supports
+         */
+        name: string;
+        /**
+         * Protocol Version
+         */
+        protocol: number;
+    };
     /**
-     * Current Number of players online
+     * RoundTrip Latency in milliseconds
      */
-    online: number;
+    ping: number;
     /**
-     * Maximum Number of players that could be online
+     * Moderator Info
      */
-    max: number;
-  };
-  version: {
+    modinfo?: {
+        /**
+         * Mod list type
+         */
+        type: string;
+        /**
+         * Moderator List May not exist
+         */
+        modList: string[];
+    };
     /**
-     * Server Software & Version Supports
+     * Server Favicon , Can be more than 1000 Characters
      */
-    name: string;
-    /**
-     * Protocol Version
-     */
-    protocol: number;
-  };
-  /**
-   * RoundTrip Latency in milliseconds
-   */
-  ping: number;
-  /**
-   * Moderator Info
-   */
-  modinfo?: {
-    /**
-     * Mod list type
-     */
-    type: string;
-    /**
-     * Moderator List May not exist
-     */
-    modList: string[];
-  };
-  /**
-   * Server Favicon , Can be more than 1000 Characters
-   */
-  favicon?: string;
+    favicon?: string;
 }
 
-  /**
-   * Returns basic info about the server, asynchronously.
-   * The hostname is the hostname of the server you want to ping,
-   * and the port is the port of the server most java servers have default of 25565.
-   */
-  export function pingPromise(
+/**
+ * Returns basic info about the server, asynchronously.
+ * The hostname is the hostname of the server you want to ping,
+ * and the port is the port of the server most java servers have default of 25565.
+ */
+export function pingPromise(
     /**
      * * The Host Name
      */
@@ -98,14 +98,14 @@ export interface Data {
     /**
      *  The Port of the server , most servers default to 25565
      */
-    port: number
-  ): Promise<Data>;
-  /**
-   * Returns basic info about the server
-   * The hostname is the hostname of the server you want to ping,
-   * and the port is the port of the server most java servers have default of 25565.
-   */
-  export function ping(
+    port: number,
+): Promise<Data>;
+/**
+ * Returns basic info about the server
+ * The hostname is the hostname of the server you want to ping,
+ * and the port is the port of the server most java servers have default of 25565.
+ */
+export function ping(
     /**
      * The Host Name
      */
@@ -113,7 +113,5 @@ export interface Data {
     /**
      *  The Port of the server , most servers default to 25565
      */
-    port: number
-  ): Data;
-
-export as namespace minecraftpinger;
+    port: number,
+): Data;
