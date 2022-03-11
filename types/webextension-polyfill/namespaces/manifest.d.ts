a67b2241b88fd2a0b1a958d3db90fa6df8349ffe
Homyee King <HomyeeKing@gmail.com>
2022-03-11 17:29:17 +0800

    Update manifest.d.ts
    update `WebExtensionManifestContentSecurityPolicyC2Type`

--- types/webextension-polyfill/namespaces/manifest.d.ts
+++ types/webextension-polyfill/namespaces/manifest.d.ts
@@ -671,6 +671,15 @@ export namespace Manifest {
          * Optional.
          */
         extension_pages?: string;
+        /**
+         * In addition, Manifest V3 disallows certain CSP modifications for `extension_pages` that were permitted in Manifest V2. 
+         * The `script-src`, `object-src`, and `worker-src` directives may only have the following values:
+         * - `self`
+         * - `none`
+         * - Any localhost source, (`http://localhost`, `http://127.0.0.1`, or any port on those domains)
+         * Optional.
+         */        
+        sandbox?: string;
     }
 
     interface WebExtensionManifestWebAccessibleResourcesC2ItemType {
