90ce50cb251fe878819b5af61044ef0558ffc0cf
Homyee King <HomyeeKing@gmail.com>
2022-03-11 17:55:52 +0800

    fix: lint

--- types/webextension-polyfill/namespaces/manifest.d.ts
+++ types/webextension-polyfill/namespaces/manifest.d.ts
@@ -671,8 +671,8 @@ export namespace Manifest {
          * Optional.
          */
         extension_pages?: string;
-        /**
-         * In addition, Manifest V3 disallows certain CSP modifications for `extension_pages` that were permitted in Manifest V2. 
+       /**
+         * In addition, Manifest V3 disallows certain CSP modifications for `extension_pages` that were permitted in Manifest V2.
          * The `script-src`, `object-src`, and `worker-src` directives may only have the following values:
          * - `self`
          * - `none`
