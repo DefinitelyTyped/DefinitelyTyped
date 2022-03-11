984cd4e07d8ecd5a6da0f81077a4c764ee6ad43d
Josh Dando <joshdando@Joshs-MacBook-Pro.local>
2022-03-11 15:19:41 +0000

    add author

--- types/nhs-number-validator/index.d.ts
+++ types/nhs-number-validator/index.d.ts
@@ -1,3 +1,7 @@
+// Type definitions for nhs-number-validator 1.1.1
+// Project: https://github.com/spikeheap/nhs-number-validator
+// Definitions by: Josh Dando <https://github.com/Joshswooft>
+// Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
 declare module 'nhs-number-validator' {
     export function validate(nhsNumber: any): boolean;
 }
