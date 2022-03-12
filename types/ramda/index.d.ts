d08146c5c828190806381e0adb17f0983dc231de
somebody1234 <ehern.lee@gmail.com>
2022-03-13 01:48:27 +1000

    [ramda] feat: add `filter` overload to `addIndex` to fix tests

--- types/ramda/index.d.ts
+++ types/ramda/index.d.ts
@@ -87,13 +87,18 @@ export function add(a: number): (b: number) => number;
  * Creates a new list iteration function from an existing one by adding two new parameters to its callback
  * function: the current index, and the entire list.
  */
-export function addIndex<T, U>(
-    fn: (f: (item: T) => U, list: readonly T[]) => U[],
-): _.F.Curry<(a: (item: T, idx: number, list: T[]) => U, b: readonly T[]) => U[]>;
 /* Special case for forEach */
 export function addIndex<T>(
     fn: (f: (item: T) => void, list: readonly T[]) => T[],
 ): _.F.Curry<(a: (item: T, idx: number, list: T[]) => void, b: readonly T[]) => T[]>;
+/* Special case for filter */
+export function addIndex<T>(
+    fn: (f: (item: T) => boolean, list: readonly T[]) => T[],
+): _.F.Curry<(a: (item: T, idx: number, list: T[]) => boolean, b: readonly T[]) => T[]>;
+/* Special case for map */
+export function addIndex<T, U>(
+    fn: (f: (item: T) => U, list: readonly T[]) => U[],
+): _.F.Curry<(a: (item: T, idx: number, list: T[]) => U, b: readonly T[]) => U[]>;
 /* Special case for reduce */
 export function addIndex<T, U>(
     fn: (f: (acc: U, item: T) => U, aci: U, list: readonly T[]) => U,
--- types/ramda/test/addIndex-tests.ts
+++ types/ramda/test/addIndex-tests.ts
@@ -12,12 +12,13 @@ class Rectangle {
 }
 
 () => {
-    const filterIndexed = R.addIndex<number, boolean>(R.filter);
+    const filterIndexed = R.addIndex<number>(R.filter);
 
     function lastTwo(_val: number, idx: number, list: number[]) {
         return list.length - idx <= 2;
     }
 
+    // $ExpectType number[]
     filterIndexed(lastTwo, [8, 6, 7, 5, 3, 0, 9]); // => [0, 9]
 };
 
@@ -26,8 +27,9 @@ class Rectangle {
         return list.length - idx <= 2;
     }
 
-    const filterIndexed = R.addIndex<number, boolean>(R.filter);
+    const filterIndexed = R.addIndex<number>(R.filter);
 
+    // $ExpectType number[]
     filterIndexed(lastTwo, [8, 6, 7, 5, 3, 0, 9]); // => [0, 9]
     const lastTwoFn = filterIndexed(lastTwo);
     lastTwoFn([8, 6, 7, 5, 3, 0, 9]);
@@ -38,6 +40,7 @@ class Rectangle {
         list[idx] = num + 5;
     }
 
+    // $ExpectType number[]
     R.addIndex<number>(R.forEach)(plusFive)([1, 2, 3]); // => [6, 7, 8]
 };
 
@@ -46,9 +49,12 @@ class Rectangle {
         if (idx === 0 || idx === list.length - 1) {
             return elt * elt;
         }
+        return elt;
     }
 
+    // $ExpectType number[]
     R.addIndex<number>(R.map)(squareEnds, [8, 5, 3, 0, 9]); // => [64, 5, 3, 0, 81]
+    // $ExpectType number[]
     R.addIndex<number>(R.map)(squareEnds)([8, 5, 3, 0, 9]); // => [64, 5, 3, 0, 81]
 };
 
@@ -60,6 +66,13 @@ class Rectangle {
         accObject[elem] = idx;
         return accObject;
     }
+
+    // $ExpectType { [elem: string]: number; }
+    reduceIndexed(objectify, {}, letters); // => { 'a': 0, 'b': 1, 'c': 2 }
+    // $ExpectType { [elem: string]: number; }
+    reduceIndexed(objectify)({}, letters); // => { 'a': 0, 'b': 1, 'c': 2 }
+    // $ExpectType { [elem: string]: number; }
+    reduceIndexed(objectify, {})(letters); // => { 'a': 0, 'b': 1, 'c': 2 }
 };
 
 () => {
@@ -67,8 +80,10 @@ class Rectangle {
         return list.length - idx <= 2;
     }
 
-    const rejectIndexed = R.addIndex<number, boolean>(R.reject);
+    const rejectIndexed = R.addIndex<number>(R.reject);
+    // $ExpectType number[]
     rejectIndexed(lastTwo, [8, 6, 7, 5, 3, 0, 9]); // => [8, 6, 7, 5, 3]
+    // $ExpectType number[]
     rejectIndexed(lastTwo)([8, 6, 7, 5, 3, 0, 9]); // => [8, 6, 7, 5, 3]
 };
 
@@ -77,6 +92,7 @@ class Rectangle {
     mapIndexed((val: string, idx: number) => `${idx}-${val}`)(['f', 'o', 'o', 'b', 'a', 'r']);
     // => ['0-f', '1-o', '2-o', '3-b', '4-a', '5-r']
     const mapIndexed2 = R.addIndex<Rectangle, number>(R.map);
+    // $ExpectType number[]
     mapIndexed2(
         (rectangle: Rectangle, idx: number): number => rectangle.area() * idx,
         [new Rectangle(1, 2), new Rectangle(4, 7)],
@@ -86,6 +102,7 @@ class Rectangle {
 
 () => {
     const reduceIndexed = R.addIndex<string, string>(R.reduce);
+    // $ExpectType string
     reduceIndexed((acc: string, val: string, idx: number) => `${acc},${idx}-${val}`, '', [
         'f',
         'o',
