cfda71dfa0c5093471a1b685906a4f42fabc6811
Kyle Hensel <k.y.l.e@outlook.co.nz>
2022-03-14 11:15:30 +1300

    move utils into index.d.ts

--- types/svelte-leafletjs/index.d.ts
+++ types/svelte-leafletjs/index.d.ts
@@ -7,7 +7,34 @@
 import type { SvelteComponentTyped } from 'svelte';
 import * as L from 'leaflet';
 import * as ReactLeaflet from 'react-leaflet';
-import { CreateSvelteEventMap } from './util';
+
+/** Given a string `onX`, this returns `X` */
+type RemoveOn<T> = T extends `on${infer P}` ? P : never;
+
+/** This is technically not required since we know every property is a function, but TS doesn't know that */
+type AssertIsFunction<T> = T extends (...args: any) => any ? T : never;
+
+/**
+ * Helper to transform the event maps defined in `@types/react-leaflet`.
+ * This makes `@types/react-leaflet` the source of truth for the events in `@types/svelte-leafletjs`.
+ *
+ * ```ts
+ * // original
+ * type ReactEventMap = {
+ *   onsomething?(e: SomeEvent): void
+ * }
+ *
+ * // converted
+ * type SvelteEventMap = {
+ *   something: SomeEvent;
+ * }
+ * ```
+ */
+type CreateSvelteEventMap<ReactEventMap> = {
+    [EventName in keyof ReactEventMap as RemoveOn<EventName>]-?: NonNullable<
+        Parameters<AssertIsFunction<NonNullable<ReactEventMap[EventName]>>>[0]
+    >;
+};
 
 export type MapEvents = CreateSvelteEventMap<ReactLeaflet.MapEvents>;
 export type MarkerEvents = CreateSvelteEventMap<ReactLeaflet.MarkerEvents>;
@@ -209,3 +236,5 @@ export class Tooltip extends SvelteComponentTyped<
 > {
     getTooltip(): L.Tooltip;
 }
+
+export {};
