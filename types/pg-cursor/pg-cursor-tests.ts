de87e389012ba6ef67858f05496a07bde21ade66
Elias Fauser <e.fauser@sprinteins.com>
2022-03-14 21:21:49 +0100

    Add config and cursor typings to test

--- types/pg-cursor/pg-cursor-tests.ts
+++ types/pg-cursor/pg-cursor-tests.ts
@@ -1,5 +1,5 @@
 import { types, Client, CustomTypesConfig } from "pg";
-import Cursor from "pg-cursor";
+import {Cursor, CursorQueryConfig, ResultCallback} from "pg-cursor";
 
 // https://github.com/brianc/node-pg-types
 // tslint:disable-next-line no-unnecessary-callback-wrapper
@@ -21,15 +21,22 @@ const host: string = client.host;
 const password: string | undefined = client.password;
 const ssl: boolean = client.ssl;
 
-const cursor = new Cursor("SELECT $1::text as name", ["brianc"]);
+const cursorConfig: CursorQueryConfig = {
+    rowMode: undefined,
+    types: undefined,
+} 
+
+const cursor = new Cursor<string>("SELECT $1::text as name", ["brianc"]);
 const handle = client.query(cursor);
-function handler(err: Error | undefined, rows: any[]) {
+
+const handleFn: ResultCallback<string> = (err: Error | undefined, rows: string[]) => {
     if (err) throw err;
     if (rows.length === 0) return;
     console.log(rows);
-    handle.read(100, handler);
+    handle.read(100, handleFn);
 }
-handle.read(100, handler);
+
+handle.read(100, handleFn);
 
 const customTypes: CustomTypesConfig = {
     getTypeParser: () => () => "aCustomTypeParser!",
