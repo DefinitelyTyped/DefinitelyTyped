0d98bb1a468c25139fd01d2c8220612f04ce315f
Ricardo Ambrogi <11719845+KadoBOT@users.noreply.github.com>
2022-03-14 08:57:11 +0100

    Update youtube-player-tests.ts

--- types/youtube-player/youtube-player-tests.ts
+++ types/youtube-player/youtube-player-tests.ts
@@ -53,14 +53,14 @@ player.loadVideoById('doesNotExist');
 player.playVideo();
 player.pauseVideo();
 player.setSize(320, 200);
-((async (): Promise<void> => {
+(async () => {
     if (await player.isMuted()) {
         player.unMute();
     } else {
         player.mute();
     }
-}) as () => Promise<void>)()
-((async (): Promise<void> => player.setVolume((await player.getVolume()) / 2)) as () => Promise<void>)();
+})();
+(async () => player.setVolume((await player.getVolume()) / 2))();
 
 player.on('stateChange', (event: CustomEvent<void> & {data: number}) => {
     switch (event.data) {
