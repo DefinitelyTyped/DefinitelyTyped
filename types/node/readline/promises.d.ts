7016de634b79782fa629c1440f112b11a52749b5
Alek Evans <gpg@alekeagle.com>
2022-03-14 21:45:01 -0600

    add types for readline/promises

---
+++ types/node/readline/promises.d.ts
@@ -0,0 +1,72 @@
+/**
+ * readline/promises
+ * @since v17.0.0
+ */
+declare module 'readline/promises' {
+  import { Abortable, EventEmitter } from "events";
+  import { CursorPos, Completer, AsyncCompleter, ReadLineOptions, Direction } from "readline";
+  interface Key {
+    sequence?: string | undefined;
+    name?: string | undefined;
+    ctrl?: boolean | undefined;
+    meta?: boolean | undefined;
+    shift?: boolean | undefined;
+  }
+  interface InterfaceEvents {
+    close: () => void;
+    line: (input: string) => void;
+    history: (history: string[]) => void;
+    pause: () => void;
+    resume: () => void;
+    SIGCONT: () => void;
+    SIGINT: () => void;
+    SIGTSTP: () => void;
+  }
+  class Interface extends EventEmitter {
+    readonly terminal: boolean;
+    readonly line: string;
+    readonly cursor: number;
+    protected constructor(input: NodeJS.ReadableStream, output?: NodeJS.WritableStream, completer?: Completer | AsyncCompleter, terminal?: boolean);
+    protected constructor(options: ReadLineOptions);
+    getPrompt(): string;
+    setPrompt(prompt: string): void;
+    prompt(preserveCursor?: boolean): void;
+    question(query: string, options?: Abortable): Promise<string>;
+    pause(): this;
+    resume(): this;
+    close(): void;
+    write(data: string | Buffer, key?: Key): void;
+    write(data: undefined | null | string | Buffer, key: Key): void;
+    getCursorPos(): CursorPos;
+    on<K extends keyof InterfaceEvents>(event: K, listener: InterfaceEvents[K]): this;
+    once<K extends keyof InterfaceEvents>(event: K, listener: InterfaceEvents[K]): this;
+    off<K extends keyof InterfaceEvents>(event: K, listener: InterfaceEvents[K]): this;
+    emit<K extends keyof InterfaceEvents>(event: K, ...args: Parameters<InterfaceEvents[K]>): boolean;
+    addListener<K extends keyof InterfaceEvents>(eventName: K, listener: InterfaceEvents[K]): this;
+    removeListener<K extends keyof InterfaceEvents>(eventName: K, listener: InterfaceEvents[K]): this;
+    removeAllListeners<K extends keyof InterfaceEvents>(eventName?: K): this;
+    listeners<K extends keyof InterfaceEvents>(eventName: K): Array<InterfaceEvents[K]>;
+    rawListeners<K extends keyof InterfaceEvents>(eventName: K): Array<InterfaceEvents[K]>;
+    eventNames(): (keyof InterfaceEvents)[];
+    listenerCount<K extends keyof InterfaceEvents>(eventName: K): number;
+    prependListener<K extends keyof InterfaceEvents>(eventName: K, listener: InterfaceEvents[K]): this;
+    prependOnceListener<K extends keyof InterfaceEvents>(eventName: K, listener: InterfaceEvents[K]): this;
+  }
+  type ReadLine = Interface;
+
+  function createInterface(input: NodeJS.ReadableStream, output?: NodeJS.WritableStream, completer?: Completer | AsyncCompleter, terminal?: boolean): Interface;
+  function createInterface(options: ReadLineOptions): Interface;
+
+  class Readline {
+    constructor(stream: NodeJS.WritableStream, options: { autoCommit: boolean });
+    clearLine(direction: Direction): this;
+    clearScreenDown(): this;
+    commit(): Promise<void>;
+    cursorTo(x: number, y?: number): this;
+    moveCursor(dx: number, dy: number): this;
+    rollback(): this;
+  }
+}
+declare module 'node:readline/promises' {
+  export * from 'readline/promises';
+}
