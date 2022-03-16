7575112e05bcfefb09d6a5572df2f25e2fc52323
ivan Shchedrin <>
2022-03-16 15:19:50 +0300

    Bump react-lazyload version

--- types/react-lazyload/index.d.ts
+++ types/react-lazyload/index.d.ts
@@ -1,4 +1,4 @@
-// Type definitions for react-lazyload ver 3.1
+// Type definitions for react-lazyload ver 3.2
 // Project: https://github.com/jasonslyvia/react-lazyload
 // Definitions by: m0a <https://github.com/m0a>
 //                 svobik7 <https://github.com/svobik7>
