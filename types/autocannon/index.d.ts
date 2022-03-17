12b11735114cc6a910833a5f005af93030bbec25
Rafael Silva <rafael.nunu@hotmail.com>
2022-03-17 13:53:17 -0300

    fix: lint

--- types/autocannon/index.d.ts
+++ types/autocannon/index.d.ts
@@ -471,11 +471,10 @@ declare namespace autocannon {
      * Track the progress of your autocannon.
      */
     function track(instance: Instance, options?: TrackingOptions): void;
-    
     /**
      * Return string to print the result tables to the terminal, programmatically.
      */
-    function printResult(result: autocannon.Result, options?: TrackingOptions): string;
+    function printResult(result: Result, options?: TrackingOptions): string;
 }
 
 /**
