41056a115d84a116235f5deb5bb039e24cb59f43
Wesley Reed <reedws@ucmail.uc.edu>
2022-03-17 12:49:42 -0400

    change to github link

--- types/jsreport__nodejs-client/index.d.ts
+++ types/jsreport__nodejs-client/index.d.ts
@@ -1,5 +1,5 @@
 // Type definitions for @jsreport/nodejs-client 3.0
-// Project: https://www.npmjs.com/package/@jsreport/nodejs-client
+// Project: https://github.com/jsreport/jsreport/tree/master/packages/nodejs-client
 // Definitions by: reediculous456 <https://github.com/reediculous456>
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
 
