086d716ab4ea5c456ed6cd30d7ab3d9f83b7be6e
Shea Smith <51303984+SheaSmith@users.noreply.github.com>
2022-03-18 14:00:26 +1300

    Change to specify that it is a non-npm package

--- types/atvlegacycontentkit/index.d.ts
+++ types/atvlegacycontentkit/index.d.ts
@@ -1,4 +1,4 @@
-// Type definitions for Apple TV ATVLegacyContentKit for TV Experience 7.7
+// Type definitions for non-npm package Apple TV ATVLegacyContentKit for TV Experience 7.7
 // Project: n/a (there is no publically available website for the SDK)
 // Definitions by: Shea Smith <https://github.com/SheaSmith>
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
