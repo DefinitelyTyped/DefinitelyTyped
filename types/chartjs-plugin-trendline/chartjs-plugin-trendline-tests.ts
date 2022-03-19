7b1953a1fa5255e2aae0600e859aebfbbf7b8a4f
Ferotiq <evanrayweigel@outlook.com>
2022-03-19 17:02:40 -0500

    fix formatting so that it passes the test

--- types/chartjs-plugin-trendline/chartjs-plugin-trendline-tests.ts
+++ types/chartjs-plugin-trendline/chartjs-plugin-trendline-tests.ts
@@ -1,15 +1,15 @@
-import * as Chart from "chart.js";
-import { TrendlineLinearOptions } from "chartjs-plugin-trendline";
+import * as Chart from 'chart.js';
+import { TrendlineLinearOptions } from 'chartjs-plugin-trendline';
 
 const defaults: TrendlineLinearOptions = {
-    style: "rgba(255,105,180,0.8)",
-    lineStyle: "solid",
+    style: 'rgba(255,105,180,0.8)',
+    lineStyle: 'solid',
     width: 2,
     projection: true,
-}
+};
 
 Chart.defaults.global.plugins = {
-    trendlineLinear: defaults
+    trendlineLinear: defaults,
 };
 
 const ctx = new CanvasRenderingContext2D();
@@ -17,26 +17,26 @@ const ctx = new CanvasRenderingContext2D();
 const chartData = {};
 
 let chart = new Chart(ctx, {
-    type: "line",
+    type: 'line',
     data: chartData,
     options: {
         plugins: {
             trendlineLinear: defaults,
-        }
-    }
+        },
+    },
 });
 
 chart = new Chart(ctx, {
-    type: "line",
+    type: 'line',
     data: chartData,
     options: {
         plugins: {
             trendlineLinear: {
-                style: "rgba(255,255,255,0.1)",
-                lineStyle: "dotted",
+                style: 'rgba(255,255,255,0.1)',
+                lineStyle: 'dotted',
                 width: 4,
-                projection: false
-            }
-        }
-    }
-})
+                projection: false,
+            },
+        },
+    },
+});
--- types/chartjs-plugin-trendline/index.d.ts
+++ types/chartjs-plugin-trendline/index.d.ts
@@ -1,19 +1,19 @@
-// Type definitions for chartjs-plugin-trendline x.x
+// Type definitions for chartjs-plugin-trendline 1.0
 // Project: https://github.com/Makanz/chartjs-plugin-trendline (Does not have to be to GitHub, but prefer linking to a source code repository rather than to a project website.)
 // Definitions by: Ferotiq <https://github.com/Ferotiq>
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
 
-import * as Chart from "chart.js";
+import * as Chart from 'chart.js';
 
-declare module "chart.js" {
+declare module 'chart.js' {
     interface ChartPluginsOptions {
-        trendlineLinear?: TrendlineLinearOptions
+        trendlineLinear?: TrendlineLinearOptions;
     }
 }
 
 export interface TrendlineLinearOptions {
     style: string;
-    lineStyle: "dotted" | "solid";
+    lineStyle: 'dotted' | 'solid';
     width: number;
     projection?: boolean;
 }
