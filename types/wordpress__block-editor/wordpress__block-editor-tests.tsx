d484534dfe94736ac133b717d841aa2d13c578df
eps1lon <silbermann.sebastian@gmail.com>
2022-03-22 19:13:12 +0100

    f

--- types/wordpress__block-editor/wordpress__block-editor-tests.tsx
+++ types/wordpress__block-editor/wordpress__block-editor-tests.tsx
@@ -549,10 +549,7 @@ select('core/block-editor').getAdjacentBlockClientId('foo', 1);
 {
   const blockProps = be.useBlockProps({ ref: useRef("test") });
 
-  blockProps.ref(current => {
-      // $ExpectType unknown
-      current;
-  });
+  blockProps.ref((current: unknown) => {});
 }
 
 // $ExpectType Record<string, unknown>
