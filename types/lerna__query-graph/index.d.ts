3379058c473ec5b9a78c77871bc729a3f515617f
donmahallem <donmahallem@users.noreply.github.com>
2022-03-23 00:42:33 +0100

    style: fix project path in header

--- types/lerna__query-graph/index.d.ts
+++ types/lerna__query-graph/index.d.ts
@@ -1,5 +1,5 @@
 // Type definitions for @lerna/project 4.0
-// Project: https://github.com/lerna/lerna/tree/main/core/project
+// Project: https://github.com/lerna/lerna/tree/main/utils/query-graph
 // Definitions by: DonMahallem <https://github.com/donmahallem>
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
 
