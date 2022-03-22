e5e4cbc25b69fa546d3f26b658a0d79fa1130ee7
Federico Panico <bpm230@gmail.com>
2022-03-22 00:39:47 -0300

    Refactor @ckeditor/ckeditor5-engine View styles utils

--- types/ckeditor__ckeditor5-engine/src/view/styles/background.d.ts
+++ types/ckeditor__ckeditor5-engine/src/view/styles/background.d.ts
@@ -1,3 +1,23 @@
-import { StylesProcessor } from "../stylesmap";
+import { StylesProcessor } from '../stylesmap';
 
+/**
+ * Adds a background CSS styles processing rules.
+ *
+ *    editor.data.addStyleProcessorRules( addBackgroundRules );
+ *
+ * The normalized value is stored as:
+ *
+ *    const styles = {
+ *      background: {
+ *        color,
+ *        repeat,
+ *        position,
+ *        attachment,
+ *        image
+ *      }
+ *    };
+ *
+ * **Note**: Currently only `'background-color'` longhand value is parsed besides `'background'` shorthand. The reducer also supports only
+ * `'background-color'` value.
+ */
 export function addBackgroundRules(stylesProcessor: StylesProcessor): void;
