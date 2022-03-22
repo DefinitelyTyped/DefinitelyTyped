a047b80d359cb94d4810096455bb06b20464e37b
Victor Zhou <vzhou842@gmail.com>
2022-03-22 00:26:11 -0700

    [ramda] Add edge cases to aperture-tests

--- types/ramda/test/aperture-tests.ts
+++ types/ramda/test/aperture-tests.ts
@@ -12,4 +12,10 @@ import * as R from 'ramda';
 
     const res1: Array<[number, number]> = R.aperture(2, [1, 2, 3, 4, 5]);
     const res2: number[][] = R.aperture(11, [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13]);
+
+    const anyArr: any[] = [1, "2"];
+    const aperturedAny = R.aperture(2, anyArr); // $ExpectType [] | [any, any][]
+
+    const undefArr: undefined[] = [undefined, undefined, undefined];
+    const aperturedUndefined = R.aperture(3, undefArr); // $ExpectType [] | [undefined, undefined, undefined][]
 };
