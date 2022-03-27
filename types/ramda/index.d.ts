779916f5a309163ba1fae90a9c555936148bc832
Marcus Blättermann <marcus@essenmitsosse.de>
2022-03-27 01:39:24 +0100

    Add typeguard overload to `anyPass`
    This is basically the same as `allPass`, just with `|` instead of `&` for joining the typeguard results.

--- types/ramda/index.d.ts
+++ types/ramda/index.d.ts
@@ -212,6 +212,37 @@ export function any<T>(fn: (a: T) => boolean): (list: readonly T[]) => boolean;
  * isBlackCard({rank: 'Q', suit: '♦'}); //=> false
  * ```
  */
+export function anyPass<T, TF1 extends T, TF2 extends T>(
+    preds: [PredTypeguard<T, TF1>, PredTypeguard<T, TF2>],
+): (a: T) => a is TF1 | TF2;
+export function anyPass<T, TF1 extends T, TF2 extends T, TF3 extends T>(
+    preds: [PredTypeguard<T, TF1>, PredTypeguard<T, TF2>, PredTypeguard<T, TF3>],
+): (a: T) => a is TF1 | TF2 | TF3;
+export function anyPass<T, TF1 extends T, TF2 extends T, TF3 extends T>(
+    preds: [PredTypeguard<T, TF1>, PredTypeguard<T, TF2>, PredTypeguard<T, TF3>],
+): (a: T) => a is TF1 | TF2 | TF3;
+export function anyPass<T, TF1 extends T, TF2 extends T, TF3 extends T, TF4 extends T>(
+    preds: [PredTypeguard<T, TF1>, PredTypeguard<T, TF2>, PredTypeguard<T, TF3>, PredTypeguard<T, TF4>],
+): (a: T) => a is TF1 | TF2 | TF3 | TF4;
+export function anyPass<T, TF1 extends T, TF2 extends T, TF3 extends T, TF4 extends T, TF5 extends T>(
+    preds: [
+        PredTypeguard<T, TF1>,
+        PredTypeguard<T, TF2>,
+        PredTypeguard<T, TF3>,
+        PredTypeguard<T, TF4>,
+        PredTypeguard<T, TF5>,
+    ],
+): PredTypeguard<T, TF1 | TF2 | TF3 | TF4 | TF5>;
+export function anyPass<T, TF1 extends T, TF2 extends T, TF3 extends T, TF4 extends T, TF5 extends T, TF6 extends T>(
+    preds: [
+        PredTypeguard<T, TF1>,
+        PredTypeguard<T, TF2>,
+        PredTypeguard<T, TF3>,
+        PredTypeguard<T, TF4>,
+        PredTypeguard<T, TF5>,
+        PredTypeguard<T, TF6>,
+    ],
+): PredTypeguard<T, TF1 | TF2 | TF3 | TF4 | TF5 | TF6>;
 export function anyPass<F extends Pred>(preds: readonly F[]): F;
 
 /**
