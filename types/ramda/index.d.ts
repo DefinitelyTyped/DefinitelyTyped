8580f3469b17e817b25a1403e5427bd6e1f637b9
Marcus Blättermann <marcus@essenmitsosse.de>
2022-03-27 01:50:13 +0100

    Add typeguard overload to `both`
    `both` is basically just `allPass` but it always takes exactly two predicates (and also not in an array, but as individual arguments)

--- types/ramda/index.d.ts
+++ types/ramda/index.d.ts
@@ -350,6 +350,10 @@ export function bind<F extends (...args: readonly any[]) => any, T>(
  * f(30); //=> false
  * ```
  */
+export function both<T, TF1 extends T, TF2 extends T>(
+    pred1: PredTypeguard<T, TF1>,
+    pred2: PredTypeguard<T, TF2>,
+): (a: T) => a is TF1 & TF2;
 export function both<T extends Pred>(pred1: T, pred2: T): T;
 export function both<T extends Pred>(pred1: T): (pred2: T) => T;
 
