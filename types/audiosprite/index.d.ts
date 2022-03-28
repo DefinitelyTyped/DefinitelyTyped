7eeae8eab057724620f714c23d665e72a9212d9f
Jake Ben-Tovim <jacobbentovim@gmail.com>
2022-03-28 11:36:17 -0700

    chore(audiosprite): update supported version in header

--- types/audiosprite/index.d.ts
+++ types/audiosprite/index.d.ts
@@ -1,4 +1,4 @@
-// Type definitions for audiosprite 0.6
+// Type definitions for audiosprite 0.7.2
 // Project: https://github.com/tonistiigi/audiosprite
 // Definitions by: Gyusun Yeom <https://github.com/Perlmint>
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
