29af506d294c5c326dfd448cd077463b9e0433ed
vagusX <vagusxl@gmail.com>
2022-03-28 08:44:24 +0000

    feat: add types for svg-baker-runtime

---
+++ types/svg-baker-runtime/src/browser-symbol.d.ts
@@ -0,0 +1,11 @@
+import SpriteSymbol from './symbol';
+
+export default class BrowserSpriteSymbol extends SpriteSymbol {
+    static createFromExistingNode(node: Element): BrowserSpriteSymbol;
+
+    isMounted: boolean;
+
+    mount: (target: Element | string) => Element;
+    render: () => Element;
+    unmount: () => void;
+}
