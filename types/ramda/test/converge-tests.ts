bd91f6cb44a53b78420a64dc30716b959cf91937
valerii15298 <valerii15298@gmail.com>
2022-03-31 13:14:45 +0300

    [ramda] rearrange converge-tests.ts

--- types/ramda/test/converge-tests.ts
+++ types/ramda/test/converge-tests.ts
@@ -8,41 +8,40 @@ import * as R from 'ramda';
 
     const indentN = R.pipe(R.times(R.always(' ')), R.join(''), R.replace(/^(?!$)/gm));
 
+    // $ExpectType Curry<(args_0: FormatSpec) => any>
+    const format = R.converge(R.call, [({ indent }: FormatSpec) => indentN(indent), ({ value }: FormatSpec) => value]);
+
+    format({ indent: 2, value: 'foo\nbar\nbaz\n' }); // => '  foo\n  bar\n  baz\n'
+
     // $ExpectType Curry<(args_0: FormatSpec) => string>
-    const format = R.converge(R.call, [
+    const format2 = R.converge(R.call, [
         ({ indent }: FormatSpec) => indentN(indent),
         ({ value }: FormatSpec) => value,
     ] as const);
 
     // $ExpectType string
-    const indented = format({ indent: 2, value: 'foo\nbar\nbaz\n' }); // => '  foo\n  bar\n  baz\n'
+    const indented = format2({ indent: 2, value: 'foo\nbar\nbaz\n' }); // => '  foo\n  bar\n  baz\n'
 };
 
 () => {
-    const add = (a: number, b: number) => a + b;
-    const multiply = (a: number, b: number) => a * b;
-    const subtract = (a: number, b: number) => a - b;
-    const concat = (a: string, b: string) => a + b;
+    function add(a: number, b: number) {
+        return a + b;
+    }
 
-    const add3 = (a: number, b: number, c: number) => a + b + c;
-    const add10 = (...args: [number, number, number, number, number, number, number, number, number, number]) =>
-        R.sum(args);
+    function multiply(a: number, b: number) {
+        return a * b;
+    }
 
-    // $Expect Curry<() => void>
-    const emptyFunction = R.converge(() => {}, []);
+    function subtract(a: number, b: number) {
+        return a - b;
+    }
 
-    // fn(1, 2) ≅ multiply( add(1, 2), subtract(1, 2) );
+    function concat(a: string, b: string) {
+        return a + b;
+    }
+
     // $ExpectType Curry<(a: number, b: number) => number>
     const fn = R.converge(multiply, [add, subtract]);
-
-    // $ExpectType number
-    fn(1, 2);
-
-    // $ExpectError
-    fn('1', 2);
-
-    // $ExpectError
-    fn(1, 2, 3);
 
     // $ExpectError
     const fnMismatchedTypes = R.converge(concat, [add, subtract]);
@@ -53,15 +52,67 @@ import * as R from 'ramda';
     // $ExpectError
     const fnWrongNumberOfBranchesV1 = R.converge(concat, []);
 
+    // $Expect Curry<() => void>
+    const emptyFunction = R.converge(() => {}, []);
+
+    // $ExpectType number
+    fn(1, 2);
+
     // $ExpectError
-    const fnWrongNumberOfBranchesV2 = R.converge(add10, [add, add, add, add, add, add, add, add, add, add, add, add]);
+    fn('1', 2);
+
+    // $ExpectError
+    fn(1, 2, 3);
+
+    function add10(
+        a: number,
+        b: number,
+        c: number,
+        d: number,
+        e: number,
+        f: number,
+        g: number,
+        h: number,
+        i: number,
+        j: number,
+    ) {
+        return a + b + c + d + e + f + g + h + i + j;
+    }
+
+    // $ExpectType Curry<(a: number, b: number) => number>
+    const fn10 = R.converge(add10, [
+        multiply,
+        add,
+        subtract,
+        multiply,
+        add,
+        subtract,
+        multiply,
+        add,
+        subtract,
+        multiply,
+    ]);
+
+    const add3 = (a: number, b: number, c: number) => a + b + c;
+
+    // $ExpectError
+    const fnWrongNumberOfBranches = R.converge(add3, [
+        // Error because we have 4 branches, but expect 3 arguments (add3)
+        multiply,
+        add,
+        subtract,
+        multiply,
+    ]);
+
+    // $ExpectType number
+    fn10(1, 2);
 
     // because fifth function in branches has arity of 3 result function also must have largest arity
     // $ExpectType Curry<(a: number, b: number, c: number) => number>
-    const fn10 = R.converge(add10, [add, add, add, add, add3, add, add, add, add, add]);
+    const fn10WithAdd3 = R.converge(add10, [add, add, add, add, add3, add, add, add, add, add]);
 
     // $ExpectType number
-    fn10(1, 2, 3);
+    fn10WithAdd3(1, 2, 3);
 
     const args1 = (a1: number | string) => 1;
     const args2 = (a1: number | bigint, a2: { q: string }) => 2;
@@ -71,17 +122,17 @@ import * as R from 'ramda';
     // $ExpectType Curry<(a1: number, a2: { w: number; } & { q: string; }, a3: number) => number>
     const intersectionOfArguments = R.converge(add3, [args1, args2, args3]);
 
-    const argsIncompatible = (a1: string) => 1;
-
-    // types: `string`, `number | symbol`, and `number | bigint` - do not have common type
-    // $ExpectError
-    const intersectionOfArgumentsIncompatible = R.converge(add3, [argsIncompatible, args2, args3]);
-
     // $ExpectType number
     const resultNumber = intersectionOfArguments(1, { q: 'text', w: 22 }, 11);
 
     // $ExpectError
     const errorArguments = intersectionOfArguments(1, { q: 'text' }, 11);
+
+    const argsIncompatible = (a1: string) => 1;
+
+    // types: `string`, `number | symbol`, and `number | bigint` - do not have common type
+    // $ExpectError
+    const intersectionOfArgumentsIncompatible = R.converge(add3, [argsIncompatible, args2, args3]);
 
     const addGeneric = <T>(a: T, b: T): T => b;
 
