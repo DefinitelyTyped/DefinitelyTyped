698cfdfcf883c3ee67d30d51b5d41b795e923094
syncfusion-javascript <packages@syncfusion.com>
2022-04-04 10:32:50 +0530

    Change Encoding format

--- types/ej.web.all/index.d.ts
+++ types/ej.web.all/index.d.ts
@@ -9,7 +9,7 @@
 /*!
 *  filename: ej.web.all.d.ts
 *  version : 20.1.0.47
-*  Copyright Syncfusion Inc. 2001 - 2018. All rights reserved.
+*  Copyright Syncfusion Inc. 2001 - 2021. All rights reserved.
 *  Use of this code is subject to the terms of our license.
 *  A copy of the current license can be obtained at any time by e-mailing
 *  licensing@syncfusion.com. Any infringement will be prosecuted under
