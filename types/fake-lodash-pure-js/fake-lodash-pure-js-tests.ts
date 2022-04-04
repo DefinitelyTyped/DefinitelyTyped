da5ea96a54e1aaf30fbdf507e638dffadc9e8cd7
xiang <xiang@havenlife.com>
2022-04-04 19:16:11 -0400

    add types

--- types/fake-lodash-pure-js/fake-lodash-pure-js-tests.ts
+++ types/fake-lodash-pure-js/fake-lodash-pure-js-tests.ts
@@ -2,9 +2,9 @@ import * as _ from './index';
 
 // $ExpectType number
 _.head<number>([1, 2]);
-//
-// // $ExpectType number
-// _.tail<number>([1, 2]);
-//
-// // $ExpectType boolean
-// _.isArray([]);
+
+// $ExpectType number
+_.tail<number>([1, 2]);
+
+// $ExpectType boolean
+_.isArray([]);
