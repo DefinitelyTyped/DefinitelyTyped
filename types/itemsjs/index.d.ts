f5c5c77156e6ce746d54949ed536465344279588
Adam Thompson-Sharpe <adamthompsonsharpe@gmail.com>
2022-04-05 16:27:04 -0400

    Add types for itemsjs

---
+++ types/itemsjs/index.d.ts
@@ -0,0 +1,130 @@
+// Type definitions for itemsjs 2.1
+// Project: https://github.com/itemsapi/itemsjs
+// Definitions by: Adam Thompson-Sharpe <https://github.com/MysteryBlokHed>
+// Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
+
+export as namespace itemsjs;
+
+declare namespace itemsjs {
+    interface Bucket<I extends {}> {
+        key: keyof I & string;
+        doc_count: number;
+        selected: boolean;
+    }
+
+    interface SearchAggregation<I extends {}, A extends string> {
+        name: A;
+        title: string;
+        position: number;
+        buckets: Array<Bucket<I>>;
+    }
+
+    interface Pagination {
+        page: number;
+        per_page: number;
+        total: number;
+    }
+
+    interface itemsjs<I extends {}, S extends string, A extends string> {
+        /** Search items */
+        search(options?: {
+            query?: string | undefined;
+            page?: number | undefined;
+            per_page?: number | undefined;
+            /** The name of a sort defined in the configuration's sortings, or a new custom one */
+            sort?: S | Sorting<I> | undefined;
+            filters?: Partial<Record<A, string[]>> | undefined;
+            /** A custom function to filter values */
+            filter?: ((item: I) => boolean) | undefined;
+            /** A custom function to filter values before `filter` and `filters` are considered */
+            prefilter?: ((item: I) => boolean) | undefined;
+            isExactSearch?: boolean | undefined;
+            removeStopWordFilter?: boolean | undefined;
+        }): {
+            data: {
+                items: I[];
+                allFilteredItems: null;
+                aggregations: Record<A, SearchAggregation<I, A>>;
+            };
+            pagination: Pagination;
+            timings: {
+                facets: number;
+                search: number;
+                sorting: number;
+                total: number;
+            };
+        };
+
+        /** Get data for aggregation */
+        aggregation(options: {
+            name: A;
+            page?: number | undefined;
+            per_page?: number | undefined;
+            query?: string | undefined;
+            conjunction?: boolean | undefined;
+        }): {
+            data: { buckets: Array<Bucket<I>> };
+            pagination: Pagination;
+        };
+
+        /**
+         * Find similar items.
+         * Uses the `id` key on items to check for uniqueness
+         */
+        similar(
+            id: I extends { id: infer ID } ? ID : unknown,
+            options: {
+                field: keyof I & string;
+                minimum?: number | undefined;
+                page?: number | undefined;
+                per_page?: number | undefined;
+            },
+        ): {
+            data: { items: Array<I & { _id: number; intersection_length: number }> };
+            pagination: Pagination;
+        };
+
+        /** Reindex with an entirely new list of items */
+        reindex(data: I[]): void;
+    }
+
+    type Order = 'asc' | 'desc';
+
+    interface Sorting<I extends {}> {
+        field: keyof I | Array<keyof I>;
+        order: Order | Order[];
+    }
+
+    interface Aggregation {
+        title: string;
+        conjunction?: boolean;
+        /** @default 10 */
+        size?: number | undefined;
+        /** @default 'count' */
+        sort?: 'term' | 'count' | undefined;
+        order?: Order | undefined;
+        show_facet_stats?: boolean | undefined;
+    }
+
+    /** Configuration for itemsjs */
+    interface Configuration<I extends {}, S extends string, A extends string> {
+        sortings?: Record<S, Sorting<I>> | undefined;
+        aggregations?: Record<A, Aggregation> | undefined;
+        searchableFields?: Array<keyof I>;
+        /** @default false */
+        isExactSearch?: boolean | undefined;
+    }
+}
+
+/**
+ * Main itemsjs function
+ * @param items The items to index
+ * @param configuration itemsjs
+ * @template I The type of items being indexed
+ */
+declare function itemsjs<I extends {} = Record<any, unknown>, S extends string = string, A extends string = string>(
+    items: I[],
+    configuration?: itemsjs.Configuration<I, S, A>,
+): itemsjs.itemsjs<I, S, A>;
+
+export = itemsjs;
---
+++ types/itemsjs/itemsjs-tests.ts
@@ -0,0 +1,80 @@
+import itemsjs = require('itemsjs');
+
+const myitems = [
+    { name: 'foo', anumber: 123 },
+    { name: 'bar', anumber: 456 },
+    { name: 'abc', anumber: 789 },
+];
+
+// No config
+itemsjs(myitems);
+// Empty config
+itemsjs(myitems, {});
+
+// Invalid sort
+// $ExpectError
+itemsjs(myitems, { sortings: { foo: {} } });
+
+const items = itemsjs(myitems, {
+    sortings: {
+        foo: {
+            field: 'name',
+            order: 'asc',
+        },
+    },
+    aggregations: {
+        anAggregation: {
+            title: 'A Number',
+            conjunction: false,
+        },
+    },
+});
+
+items.search({ query: 'abc' });
+
+items.search({ query: 'abc', sort: 'foo' });
+
+// Sort 'bar' was never defined
+// $ExpectError
+items.search({ query: 'abc', sort: 'bar' });
+
+items.search({
+    query: 'abc',
+    sort: {
+        field: 'anumber',
+        order: 'desc',
+    },
+});
+
+items.search({
+    query: 'abc',
+    filters: {}, // No filters provided
+});
+
+// Aggregation 'bar' was never defined
+items.search({
+    query: 'abc',
+    // $ExpectError
+    filters: { bar: [] },
+});
+
+items.search({
+    query: 'abc',
+    filters: { anAggregation: ['abc'] },
+});
+
+itemsjs<typeof myitems[number]>([]).reindex(myitems);
+
+const myItemsIds = myitems.map((v, i) => ({ id: i, ...v }));
+
+const itemsIds = itemsjs(myItemsIds);
+
+// ID 'abc' is invalid
+// $ExpectError
+itemsIds.similar('abc', { field: 'name' });
+
+// Missing options & missing field key
+itemsIds.similar(0); // $ExpectError
+itemsIds.similar(0, {}); // $ExpectError
+
+itemsIds.similar(0, { field: 'name' });
