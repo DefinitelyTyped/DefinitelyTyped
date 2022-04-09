64eb055613eb1f49eae99334666c53b94cb0e9da
Mario Beltrán Alarcón <belco90@gmail.com>
2022-04-09 16:52:11 +0200

    Revert "feat(happo-cypress): update types for v3"
    This reverts commit 948a901c154c640c3bfb2a2b4f9bed2d00c9bcd5.

--- types/happo-cypress/index.d.ts
+++ types/happo-cypress/index.d.ts
@@ -1,7 +1,6 @@
-// Type definitions for happo-cypress 3.0
+// Type definitions for happo-cypress 1.17
 // Project: https://github.com/happo/happo-cypress#readme
 // Definitions by: Alexander Polyankin <https://github.com/alexanderpolyankin>
-//                 Mario Beltrán Alarcón <https://github.com/Belco90>
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
 
 export interface HappoScreenshotOptions {
