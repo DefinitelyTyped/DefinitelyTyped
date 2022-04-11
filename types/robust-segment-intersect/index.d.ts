48e4b6e5020cf57211f77d796dec5eb39fd0d7ef
masx200 <34191203+masx200@users.noreply.github.com>
2022-04-11 13:56:13 +0800

    Create index.d.ts

---
+++ types/robust-segment-intersect/index.d.ts
@@ -0,0 +1,8 @@
+   export type Coordinate = [number, number];
+    const a: (
+        a0: Coordinate,
+        a1: Coordinate,
+        b0: Coordinate,
+        b1: Coordinate
+    ) => boolean;
+    export default a;
