6aa678974e399d95ec7515a0d811a9907ecdaa45
masx200 <34191203+masx200@users.noreply.github.com>
2022-04-12 06:35:16 +0800

    Update types/robust-segment-intersect/index.d.ts
    Co-authored-by: Adam Thompson-Sharpe <adamthompsonsharpe@gmail.com>

--- types/robust-segment-intersect/index.d.ts
+++ types/robust-segment-intersect/index.d.ts
@@ -3,11 +3,11 @@
 // Definitions by: masx200 <https://github.com/masx200>
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
 
-export type Coordinate = [number, number];
+type Coordinate = [number, number];
 declare function a (
         a0: Coordinate,
         a1: Coordinate,
         b0: Coordinate,
         b1: Coordinate
     ) :boolean;
-export default a;
+export = a;
