db6cc8a4b6c2cfb6b97031ff2145681c7712de82
masx200 <34191203+masx200@users.noreply.github.com>
2022-04-11 14:17:47 +0800

    Update index.d.ts

--- types/robust-segment-intersect/index.d.ts
+++ types/robust-segment-intersect/index.d.ts
@@ -1,8 +1,13 @@
-   export type Coordinate = [number, number];
-    const a: (
+// Type definitions for robust-segment-intersect 1.0.1
+// Project: https://github.com/mikolalysenko/robust-segment-intersect
+// Definitions by: masx200 <https://github.com/masx200>
+// Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
+
+export type Coordinate = [number, number];
+const a: (
         a0: Coordinate,
         a1: Coordinate,
         b0: Coordinate,
         b1: Coordinate
     ) => boolean;
-    export default a;
+export default a;
