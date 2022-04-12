d4d664700280ef0bfd28e36d504baf27183abc43
David Sherret <dsherret@gmail.com>
2022-04-12 15:05:00 -0400

    Exactly match TypeScript's type so the types are interchangeable.

--- types/domexception/index.d.ts
+++ types/domexception/index.d.ts
@@ -6,66 +6,69 @@
 
 export = DOMException;
 
+// Type from: https://github.com/microsoft/TypeScript/blob/main/src/lib/dom.generated.d.ts
+
 /**
  * An abnormal event (called an exception) which occurs as a result of calling
  * a method or accessing a property of a web API.
  */
-declare class DOMException extends Error {
+ interface DOMException extends Error {
     readonly code: number;
     readonly message: string;
     readonly name: string;
-
-    constructor(message?: string, name?: string);
-
-    readonly INDEX_SIZE_ERR: 1;
-    readonly DOMSTRING_SIZE_ERR: 2;
-    readonly HIERARCHY_REQUEST_ERR: 3;
-    readonly WRONG_DOCUMENT_ERR: 4;
-    readonly INVALID_CHARACTER_ERR: 5;
-    readonly NO_DATA_ALLOWED_ERR: 6;
-    readonly NO_MODIFICATION_ALLOWED_ERR: 7;
-    readonly NOT_FOUND_ERR: 8;
-    readonly NOT_SUPPORTED_ERR: 9;
-    readonly INUSE_ATTRIBUTE_ERR: 10;
-    readonly INVALID_STATE_ERR: 11;
-    readonly SYNTAX_ERR: 12;
-    readonly INVALID_MODIFICATION_ERR: 13;
-    readonly NAMESPACE_ERR: 14;
-    readonly INVALID_ACCESS_ERR: 15;
-    readonly VALIDATION_ERR: 16;
-    readonly TYPE_MISMATCH_ERR: 17;
-    readonly SECURITY_ERR: 18;
-    readonly NETWORK_ERR: 19;
-    readonly ABORT_ERR: 20;
-    readonly URL_MISMATCH_ERR: 21;
-    readonly QUOTA_EXCEEDED_ERR: 22;
-    readonly TIMEOUT_ERR: 23;
-    readonly INVALID_NODE_TYPE_ERR: 24;
-    readonly DATA_CLONE_ERR: 25;
-
-    static readonly INDEX_SIZE_ERR: 1;
-    static readonly DOMSTRING_SIZE_ERR: 2;
-    static readonly HIERARCHY_REQUEST_ERR: 3;
-    static readonly WRONG_DOCUMENT_ERR: 4;
-    static readonly INVALID_CHARACTER_ERR: 5;
-    static readonly NO_DATA_ALLOWED_ERR: 6;
-    static readonly NO_MODIFICATION_ALLOWED_ERR: 7;
-    static readonly NOT_FOUND_ERR: 8;
-    static readonly NOT_SUPPORTED_ERR: 9;
-    static readonly INUSE_ATTRIBUTE_ERR: 10;
-    static readonly INVALID_STATE_ERR: 11;
-    static readonly SYNTAX_ERR: 12;
-    static readonly INVALID_MODIFICATION_ERR: 13;
-    static readonly NAMESPACE_ERR: 14;
-    static readonly INVALID_ACCESS_ERR: 15;
-    static readonly VALIDATION_ERR: 16;
-    static readonly TYPE_MISMATCH_ERR: 17;
-    static readonly SECURITY_ERR: 18;
-    static readonly NETWORK_ERR: 19;
-    static readonly ABORT_ERR: 20;
-    static readonly URL_MISMATCH_ERR: 21;
-    static readonly QUOTA_EXCEEDED_ERR: 22;
-    static readonly TIMEOUT_ERR: 23;
-    static readonly INVALID_NODE_TYPE_ERR: 24;
-    static readonly DATA_CLONE_ERR: 25;
+    readonly ABORT_ERR: number;
+    readonly DATA_CLONE_ERR: number;
+    readonly DOMSTRING_SIZE_ERR: number;
+    readonly HIERARCHY_REQUEST_ERR: number;
+    readonly INDEX_SIZE_ERR: number;
+    readonly INUSE_ATTRIBUTE_ERR: number;
+    readonly INVALID_ACCESS_ERR: number;
+    readonly INVALID_CHARACTER_ERR: number;
+    readonly INVALID_MODIFICATION_ERR: number;
+    readonly INVALID_NODE_TYPE_ERR: number;
+    readonly INVALID_STATE_ERR: number;
+    readonly NAMESPACE_ERR: number;
+    readonly NETWORK_ERR: number;
+    readonly NOT_FOUND_ERR: number;
+    readonly NOT_SUPPORTED_ERR: number;
+    readonly NO_DATA_ALLOWED_ERR: number;
+    readonly NO_MODIFICATION_ALLOWED_ERR: number;
+    readonly QUOTA_EXCEEDED_ERR: number;
+    readonly SECURITY_ERR: number;
+    readonly SYNTAX_ERR: number;
+    readonly TIMEOUT_ERR: number;
+    readonly TYPE_MISMATCH_ERR: number;
+    readonly URL_MISMATCH_ERR: number;
+    readonly VALIDATION_ERR: number;
+    readonly WRONG_DOCUMENT_ERR: number;
 }
+
+declare var DOMException: {
+    prototype: DOMException;
+    new(message?: string, name?: string): DOMException;
+    readonly ABORT_ERR: number;
+    readonly DATA_CLONE_ERR: number;
+    readonly DOMSTRING_SIZE_ERR: number;
+    readonly HIERARCHY_REQUEST_ERR: number;
+    readonly INDEX_SIZE_ERR: number;
+    readonly INUSE_ATTRIBUTE_ERR: number;
+    readonly INVALID_ACCESS_ERR: number;
+    readonly INVALID_CHARACTER_ERR: number;
+    readonly INVALID_MODIFICATION_ERR: number;
+    readonly INVALID_NODE_TYPE_ERR: number;
+    readonly INVALID_STATE_ERR: number;
+    readonly NAMESPACE_ERR: number;
+    readonly NETWORK_ERR: number;
+    readonly NOT_FOUND_ERR: number;
+    readonly NOT_SUPPORTED_ERR: number;
+    readonly NO_DATA_ALLOWED_ERR: number;
+    readonly NO_MODIFICATION_ALLOWED_ERR: number;
+    readonly QUOTA_EXCEEDED_ERR: number;
+    readonly SECURITY_ERR: number;
+    readonly SYNTAX_ERR: number;
+    readonly TIMEOUT_ERR: number;
+    readonly TYPE_MISMATCH_ERR: number;
+    readonly URL_MISMATCH_ERR: number;
+    readonly VALIDATION_ERR: number;
+    readonly WRONG_DOCUMENT_ERR: number;
+};
