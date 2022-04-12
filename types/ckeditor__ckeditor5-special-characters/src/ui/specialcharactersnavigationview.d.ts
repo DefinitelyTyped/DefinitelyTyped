c5958f022e9472f8e70ceedf5daf345c8a65d0ad
Federico Panico <bpm230@gmail.com>
2022-04-12 13:20:32 -0300

    Add comments to @ckeditor/ckeditor5-special-characters

--- types/ckeditor__ckeditor5-special-characters/src/ui/specialcharactersnavigationview.d.ts
+++ types/ckeditor__ckeditor5-special-characters/src/ui/specialcharactersnavigationview.d.ts
@@ -1,9 +1,25 @@
-import { FormHeaderView } from "@ckeditor/ckeditor5-ui";
-import DropdownView from "@ckeditor/ckeditor5-ui/src/dropdown/dropdownview";
-import { Locale } from "@ckeditor/ckeditor5-utils";
+import { FormHeaderView } from '@ckeditor/ckeditor5-ui';
+import DropdownView from '@ckeditor/ckeditor5-ui/src/dropdown/dropdownview';
+import { Locale } from '@ckeditor/ckeditor5-utils';
 
+/**
+ * A class representing the navigation part of the special characters UI. It is responsible
+ * for describing the feature and allowing the user to select a particular character group.
+ */
 export default class SpecialCharactersNavigationView extends FormHeaderView {
-    currentGroupName: string;
-    groupDropdownView: DropdownView;
+    /**
+     * Creates an instance of the {@link module:special-characters/ui/specialcharactersnavigationview~SpecialCharactersNavigationView}
+     * class.
+     */
     constructor(locale: Locale, groupNames: string[]);
+
+    /**
+     * A dropdown that allows selecting a group of special characters to be displayed.
+     */
+    readonly groupDropdownView: DropdownView;
+
+    /**
+     * Returns the name of the character group currently selected in the {@link #groupDropdownView}.
+     */
+    readonly currentGroupName: string;
 }
