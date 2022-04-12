78f6e394022b696d7561b35ab88cbd771805acbf
Bruce Johnson <brucejo.wk@outlook.com>
2022-04-11 21:30:40 -0700

    Add hookSend & customTransport

--- types/meteor/globals/email.d.ts
+++ types/meteor/globals/email.d.ts
@@ -1,19 +1,26 @@
 declare namespace Email {
-    function send(options: {
-        from?: string | undefined;
-        to?: string | string[] | undefined;
-        cc?: string | string[] | undefined;
-        bcc?: string | string[] | undefined;
-        replyTo?: string | string[] | undefined;
-        subject?: string | undefined;
-        text?: string | undefined;
-        html?: string | undefined;
-        headers?: Object | undefined;
-        attachments?: Object[] | undefined;
-        mailComposer?: MailComposer | undefined;
-    }): void;
-}
+  interface EmailOptions {
+    from?: string | undefined;
+    to?: string | string[] | undefined;
+    cc?: string | string[] | undefined;
+    bcc?: string | string[] | undefined;
+    replyTo?: string | string[] | undefined;
+    subject?: string | undefined;
+    text?: string | undefined;
+    html?: string | undefined;
+    headers?: Object | undefined;
+    attachments?: Object[] | undefined;
+    mailComposer?: MailComposer | undefined;
+  }
 
+  interface CustomEmailOptions extends  EmailOptions {
+    packageSettings?: unknown;
+  }
+
+  function send(options: EmailOptions): void;
+  function hookSend(fn: (options: EmailOptions )=> boolean): void;
+  function customTransport(fn: (options: CustomEmailOptions)=> void): void;
+}
 declare interface MailComposerOptions {
     escapeSMTP: boolean;
     encoding: string;
