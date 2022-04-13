7d3c061adc19f5e2b82855e6d9693b56eaeff972
Joey Kilpatrick <joey.kilpatrick15@gmail.com>
2022-04-13 00:28:22 -0400

    Augment LexV2 types

--- types/aws-lambda/test/lex-v2-tests.ts
+++ types/aws-lambda/test/lex-v2-tests.ts
@@ -1,17 +1,17 @@
 import {
-    LexV2Event,
     LexV2Result,
     LexV2Handler,
     LexV2Interpretation,
     LexV2Bot,
     LexV2Intent,
-    LexV2Slot,
+    LexV2Slots,
     LexV2SlotValue,
     LexV2SentimentResponse,
     LexV2SentimentScore,
     LexV2SessionState,
     LexV2ActiveContext,
-    LexV2DialogAction
+    LexV2DialogAction,
+    LexV2Event,
 } from 'aws-lambda';
 
 const handler: LexV2Handler = async (event, context, callback) => {
@@ -21,7 +21,6 @@ const handler: LexV2Handler = async (event, context, callback) => {
     str = event.responseContentType;
     str = event.sessionId;
     str = event.inputTranscript;
-    str = event.requestAttributes[str];
     let bot: LexV2Bot;
     bot = event.bot;
     str = bot.id;
@@ -36,14 +35,18 @@ const handler: LexV2Handler = async (event, context, callback) => {
     intent = interpretation.intent;
     str = intent.confirmationState;
     str = intent.name;
-    let slot: LexV2Slot;
+    let slot: LexV2Slots[string];
     slot = intent.slots[str];
-    strOrUndefined = slot.shape;
-    let slotValue: LexV2SlotValue;
-    slotValue = slot.value;
-    str = slotValue.interpretedValue;
-    str = slotValue.originalValue;
-    str = slotValue.resolvedValues[0];
+    if (slot) {
+        str = slot.shape;
+        let slotValue: LexV2SlotValue;
+        slotValue = slot.value;
+        strOrUndefined = slotValue.interpretedValue;
+        str = slotValue.originalValue;
+        str = slotValue.resolvedValues[0];
+    } else {
+        nullOrUndefined = slot;
+    }
     str = intent.state;
     numOrUndefined = interpretation.nluConfidence;
     let sentimentResponse: LexV2SentimentResponse | undefined;
@@ -55,7 +58,9 @@ const handler: LexV2Handler = async (event, context, callback) => {
     num = sentimentScore.negative;
     num = sentimentScore.neutral;
     num = sentimentScore.positive;
-    str = event.requestAttributes[str];
+    if (event.requestAttributes) {
+        str = event.requestAttributes[str];
+    }
     let sessionState: LexV2SessionState;
     sessionState = event.sessionState;
     let activeContext: LexV2ActiveContext | undefined;
@@ -67,7 +72,10 @@ const handler: LexV2Handler = async (event, context, callback) => {
     strOrUndefined = sessionState.sessionAttributes?.[str];
     let dialogAction: LexV2DialogAction | undefined;
     dialogAction = sessionState.dialogAction;
-    strOrUndefined = dialogAction!.slotToElicit;
+    switch (dialogAction?.type) {
+        case 'ElicitSlot':
+            str = dialogAction.slotToElicit;
+    }
     str = dialogAction!.type;
     intent = sessionState.intent;
     str = sessionState.originatingRequestId;
@@ -117,3 +125,123 @@ const handler: LexV2Handler = async (event, context, callback) => {
     callback(null, result);
     return result;
 };
+
+const event1: LexV2Event = {
+    "sessionId": "111111111111111",
+    "inputTranscript": "7",
+    "interpretations": [
+        {
+            "intent": {
+                "slots": {
+                    "Slot1": null,
+                    "Slot2": null,
+                    "Slot3": {
+                        "shape": "Scalar",
+                        "value": {
+                            "originalValue": "7",
+                            "resolvedValues": [
+                                "7"
+                            ],
+                            "interpretedValue": "7"
+                        }
+                    }
+                },
+                "confirmationState": "None",
+                "name": "IntentName",
+                "state": "InProgress"
+            },
+            "nluConfidence": 1
+        },
+        {
+            "intent": {
+                "slots": {},
+                "confirmationState": "None",
+                "name": "FallbackIntent",
+                "state": "InProgress"
+            }
+        }
+    ],
+    "proposedNextState": {
+        "intent": {
+            "slots": {
+                "Slot1": null,
+                "Slot2": null,
+                "Slot3": {
+                    "shape": "Scalar",
+                    "value": {
+                        "originalValue": "7",
+                        "resolvedValues": [
+                            "7"
+                        ],
+                        "interpretedValue": "7"
+                    }
+                }
+            },
+            "confirmationState": "None",
+            "name": "IntentName",
+            "state": "InProgress"
+        },
+        "dialogAction": {
+            "slotToElicit": "Slot1",
+            "type": "ElicitSlot"
+        }
+    },
+    "responseContentType": "text/plain; charset=utf-8",
+    "invocationSource": "DialogCodeHook",
+    "messageVersion": "1.0",
+    "transcriptions": [
+        {
+            "resolvedSlots": {
+                "Slot3": {
+                    "shape": "Scalar",
+                    "value": {
+                        "originalValue": "7",
+                        "resolvedValues": [
+                            "7"
+                        ]
+                    }
+                }
+            },
+            "resolvedContext": {
+                "intent": "IntentName"
+            },
+            "transcription": "7",
+            "transcriptionConfidence": 1
+        }
+    ],
+    "sessionState": {
+        "sessionAttributes": {
+            "attribute1": "1"
+        },
+        "activeContexts": [],
+        "intent": {
+            "slots": {
+                "Slot1": null,
+                "Slot2": null,
+                "Slot3": {
+                    "shape": "Scalar",
+                    "value": {
+                        "originalValue": "7",
+                        "resolvedValues": [
+                            "7"
+                        ],
+                        "interpretedValue": "7"
+                    }
+                }
+            },
+            "confirmationState": "None",
+            "name": "IntentName",
+            "state": "InProgress"
+        },
+        "originatingRequestId": "11111111-1111-1111-1111-111111111111"
+    },
+    "inputMode": "Text",
+    "bot": {
+        "aliasName": "string",
+        "aliasId": "string",
+        "name": "string",
+        "version": "DRAFT",
+        "localeId": "en_US",
+        "id": "string"
+    }
+}
