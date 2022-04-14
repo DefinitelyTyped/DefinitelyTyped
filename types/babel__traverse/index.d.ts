e0e72fbe976349fb55ce2a2876d5970290e30ae5
Daniel Tschinder <231804+danez@users.noreply.github.com>
2022-04-14 13:10:53 +0000

    fix: Fix return type

--- types/babel__traverse/index.d.ts
+++ types/babel__traverse/index.d.ts
@@ -549,7 +549,7 @@ export class NodePath<T = Node> {
     ): T[K] extends Array<Node | null | undefined>
         ? Array<NodePath<T[K][number]>>
         : T[K] extends Array<Node | null | undefined> | null | undefined
-        ? Array<NodePath<NonNullable<T[K]>[number]>> | null | undefined
+        ? Array<NodePath<NonNullable<T[K]>[number]>> | NodePath<null | undefined>
         : T[K] extends Node | null | undefined
         ? NodePath<T[K]>
         : never;
