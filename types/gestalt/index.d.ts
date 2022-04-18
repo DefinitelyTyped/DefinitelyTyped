ffe8d10bd4712210d3c710c61293cf82eabe58dc
zifahm <muhamm3dhafiz@gmail.com>
2022-04-18 17:50:01 +0530

    going back to version nameing

--- types/gestalt/index.d.ts
+++ types/gestalt/index.d.ts
@@ -1,4 +1,4 @@
-// Type definitions for gestalt 50.9
+// Type definitions for gestalt 50.0.9
 // Project: https://github.com/pinterest/gestalt, https://pinterest.github.io/gestalt
 // Definitions by: Nicolás Serrano Arévalo <https://github.com/serranoarevalo>
 //                 Josh Gachnang <https://github.com/joshgachnang>
