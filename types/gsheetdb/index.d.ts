1a6042b58a116bde3da9c1b2886862cf86436acd
Federico Grandi <fgrandi30@gmail.com>
2022-04-19 21:52:25 +0200

    feat: add definitions for `gsheetdb`

---
+++ types/gsheetdb/index.d.ts
@@ -0,0 +1,37 @@
+// Type definitions for gsheetdb 1.0
+// Project: https://github.com/zdettwiler/gsheetdb
+// Definitions by: Federico Grandi <https://github.com/EndBug>
+// Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
+
+/// <reference types="node" />
+
+import { JWT, UserRefreshClient } from 'google-auth-library';
+
+export default class gsheetdb {
+    spreadsheetId: string;
+    sheetName: string;
+    credentialsJSON: object;
+
+    client?: JWT | UserRefreshClient;
+    headerRow: Row;
+
+    constructor(parameters: GSheetDBParameters);
+
+    connect(): Promise<void>;
+    getData(dataRange?: string): Promise<SheetData>;
+    insertRows(rows: Row[]): Promise<void>;
+    updateRow(rowNumber: number, rowArray: Row): Promise<void>;
+}
+
+export interface GSheetDBParameters {
+    spreadsheetId: string;
+    sheetName: string;
+    credentialsJSON: object;
+}
+
+export type SheetData = Array<{
+    values: Row;
+    rowNb: number;
+}>;
+
+export type Row = Array<boolean | string | number | null>;
