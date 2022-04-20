40bfdbfe6fc6f3020fe5280a8dd145a387a4ead6
CodeBast4rd <20125737+CodeBast4rd@users.noreply.github.com>
2022-04-20 10:34:21 +0200

    fixed strophejs function declares

--- types/strophe.js/index.d.ts
+++ types/strophe.js/index.d.ts
@@ -6,53 +6,53 @@
 // TypeScript Version: 3.2
 export {};
 
-/** Function: $build
- *  Create a Strophe.Builder.
- *  This is an alias for 'new Strophe.Builder(name, attrs)'.
- *
- *  Parameters:
- *    @param name - The root element name.
- *    @param attrs - The attributes for the root element in object notation.
- *
- *  Returns:
- *    @returns A new Strophe.Builder object.
- */
-export function $build(name: string, attrs?: Record<string, string>): globalThis.Strophe.Builder;
-
-/** Function: $msg
- *  Create a Strophe.Builder with a <message/> element as the root.
- *
- *  Parameters:
- *    @param attrs - The <message/> element attributes in object notation.
- *
- *  Returns:
- *    @returns A new Strophe.Builder object.
- */
-export function $msg(attrs?: Record<string, string>): globalThis.Strophe.Builder;
-
-/** Function: $iq
- *  Create a Strophe.Builder with an <iq/> element as the root.
- *
- *  Parameters:
- *    @param attrs - The <iq/> element attributes in object notation.
- *
- *  Returns:
- *    @returns A new Strophe.Builder object.
- */
-export function $iq(attrs?: Record<string, string>): globalThis.Strophe.Builder;
-
-/** Function: $pres
- *  Create a Strophe.Builder with a <presence/> element as the root.
- *
- *  Parameters:
- *    @param attrs - The <presence/> element attributes in object notation.
- *
- *  Returns:
- *    @returns A new Strophe.Builder object.
- */
-export function $pres(attrs?: Record<string, string>): globalThis.Strophe.Builder;
-
 declare global {
+
+    /** Function: $build
+     *  Create a Strophe.Builder.
+     *  This is an alias for 'new Strophe.Builder(name, attrs)'.
+     *
+     *  Parameters:
+     *    @param name - The root element name.
+     *    @param attrs - The attributes for the root element in object notation.
+     *
+     *  Returns:
+     *    @returns A new Strophe.Builder object.
+     */
+    function $build(name: string, attrs?: Record<string, string>): globalThis.Strophe.Builder;
+
+    /** Function: $msg
+     *  Create a Strophe.Builder with a <message/> element as the root.
+     *
+     *  Parameters:
+     *    @param attrs - The <message/> element attributes in object notation.
+     *
+     *  Returns:
+     *    @returns A new Strophe.Builder object.
+     */
+    function $msg(attrs?: Record<string, string>): globalThis.Strophe.Builder;
+
+    /** Function: $iq
+     *  Create a Strophe.Builder with an <iq/> element as the root.
+     *
+     *  Parameters:
+     *    @param attrs - The <iq/> element attributes in object notation.
+     *
+     *  Returns:
+     *    @returns A new Strophe.Builder object.
+     */
+    function $iq(attrs?: Record<string, string>): globalThis.Strophe.Builder;
+
+    /** Function: $pres
+     *  Create a Strophe.Builder with a <presence/> element as the root.
+     *
+     *  Parameters:
+     *    @param attrs - The <presence/> element attributes in object notation.
+     *
+     *  Returns:
+     *    @returns A new Strophe.Builder object.
+     */
+    function $pres(attrs?: Record<string, string>): globalThis.Strophe.Builder;
 
     namespace Strophe {
         /** Constant: VERSION
