b5b844ec64afe4991976e538473a0c58a1e14585
joshuajeschek <joshua.jeschek@gmail.com>
2022-04-20 20:13:18 +0200

    feat(node-steam-openid): add types for node-steam-openid

---
+++ types/node-steam-openid/index.d.ts
@@ -0,0 +1,61 @@
+// Type definitions for node-steam-openid 1.0
+// Project: https://github.com/LeeviHalme/node-steam-openid (Does not have to be to GitHub, but prefer linking to a source code repository rather than to a project website.)
+// Definitions by: joshuajeschek <https://github.com/joshuajeschek>
+// Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
+
+import { Request } from 'express'
+
+export = SteamAuth;
+
+declare class SteamAuth {
+    /**
+     * `realm` - Site name displayed to users on logon
+     *
+     * `returnUrl` - Your return route
+     *
+     * `apiKey` - Steam API key
+     *
+     * ```
+     * const steam = new SteamAuth({
+     *   realm: "http://localhost:5000",
+     *   returnUrl: "http://localhost:5000/auth/steam/authenticate",
+     *   apiKey: "XXXXXXXXXXXXXXXXXXXXXXXXXX"
+     * });
+     * ```
+     */
+    constructor({ realm, returnUrl, apiKey }: SteamAuthOptions);
+
+    /**
+     * Gets the redirect URL to Steam.
+     */
+    getRedirectUrl(): Promise<string>;
+
+    /**
+     * Authenticates the user with oAuth.
+     * @param request
+     */
+    authenticate(request: Request | Object): Promise<UserObject>
+}
+
+interface SteamAuthOptions {
+    realm: string;
+    returnUrl: string;
+    apiKey: string;
+}
+
+/**
+ * Object which holds all the authenticated user's data.
+ * The key _json holds the raw response from Steam API.
+ */
+interface UserObject {
+    _json: Record<string, any>,
+    steamid: string;
+    username: string;
+    name: string;
+    profile: string;
+    avatar: {
+          small: string;
+          medium: string;
+          large: string;
+    }
+}
