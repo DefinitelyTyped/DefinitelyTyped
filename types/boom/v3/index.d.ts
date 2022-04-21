dc7c696da0bb0478123a6e5b6c3323db5e302b54
Igor Rogatty <7030884+rogatty@users.noreply.github.com>
2022-04-21 08:11:29 +0200

    Abandoned definitions are not allowed

--- types/boom/v3/index.d.ts
+++ types/boom/v3/index.d.ts
@@ -1,5 +1,6 @@
 // Type definitions for boom 3.2
 // Project: https://github.com/hapijs/boom
+// Definitions by: Igor Rogatty <https://github.com/rogatty>
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
 
 /// <reference types="node" />
