a37d587b7c48d0256c9711b012e35b940090f3f1
Hiroki Osame <hiroki.osame@gmail.com>
2022-04-22 14:25:27 -0400

    docs: remove imports

--- types/cmd-shim/index.d.ts
+++ types/cmd-shim/index.d.ts
@@ -7,8 +7,7 @@
 /**
  * Create a cmd shim at `to` for the command line program at `from`. e.g.
  *
- *     import cmdShim from 'cmd-shim';
- *     cmdShim(__dirname + '/cli.js', '/usr/bin/command-name');
+ * cmdShim(__dirname + '/cli.js', '/usr/bin/command-name');
  */
 declare function cmdShim(from: string, to: string): Promise<void>;
 
@@ -17,8 +16,7 @@ declare namespace cmdShim {
      * Create a cmd shim at `to` for the command line program at `from`, but will just
      * continue if the file does not exist.
      *
-     *     import cmdShim from 'cmd-shim';
-     *     cmdShim.ifExists(__dirname + '/cli.js', '/usr/bin/command-name');
+     * cmdShim.ifExists(__dirname + '/cli.js', '/usr/bin/command-name');
      */
     function ifExists(from: string, to: string): Promise<void>;
 }
