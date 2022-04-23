bbcc53fb790f29c66ba0dc6cfde91520f8cda686
Federico Grandi <fgrandi30@gmail.com>
2022-04-23 21:07:16 +0200

    Update types/react-native-svg-animated-linear-gradient/index.d.ts
    Co-authored-by: Piotr Błażejewicz (Peter Blazejewicz) <peterblazejewicz@users.noreply.github.com>

--- types/react-native-svg-animated-linear-gradient/index.d.ts
+++ types/react-native-svg-animated-linear-gradient/index.d.ts
@@ -6,16 +6,16 @@
 import * as React from 'react';
 
 export interface GradientProps {
-    primaryColor?: string;
-    secondaryColor?: string;
-    duration?: number;
-    width?: number | string;
-    height?: number | string;
-    x1?: string;
-    y1?: string;
-    x2?: string;
-    y2?: string;
-    useNativeDriver?: boolean;
+    primaryColor?: string | undefined;
+    secondaryColor?: string | undefined;
+    duration?: number | undefined;
+    width?: number | string | undefined;
+    height?: number | string | undefined;
+    x1?: string | undefined;
+    y1?: string | undefined;
+    x2?: string | undefined;
+    y2?: string | undefined;
+    useNativeDriver?: boolean | undefined;
 }
 
 export default class SvgAnimatedLinearGradient extends React.Component<GradientProps> {}
