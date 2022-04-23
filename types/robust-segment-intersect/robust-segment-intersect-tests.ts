b240a652146cc1009828db46e4f6483e4f582448
masx200 <34191203+masx200@users.noreply.github.com>
2022-04-24 07:31:44 +0800

    Update robust-segment-intersect-tests.ts

--- types/robust-segment-intersect/robust-segment-intersect-tests.ts
+++ types/robust-segment-intersect/robust-segment-intersect-tests.ts
@@ -1,8 +1,9 @@
 import crosses = require('robust-segment-intersect');
-const a0 = [-1, 0]
-const a1 = [ 1, 0]
-const b0 = [ 0,-1]
-const b1 = [ 0, 1]
+
+const a0 = [-1, 0] as const;
+const a1 = [1, 0] as const;
+const b0 = [0, -1] as const;
+const b1 = [0, 1] as const;
 
 // $ExpectType boolean
 crosses(a0, a1, b0, b1);
