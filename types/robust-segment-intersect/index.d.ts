ef22d5e0d6c4fa6ad7c94cee7c0c8ed4b1cde6df
masx200 <34191203+masx200@users.noreply.github.com>
2022-04-24 09:00:13 +0800

    Update index.d.ts

--- types/robust-segment-intersect/index.d.ts
+++ types/robust-segment-intersect/index.d.ts
@@ -3,8 +3,24 @@
 // Definitions by: masx200 <https://github.com/masx200>
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
 
-type Coordinate = Readonly<[number, number]>;
-declare function segmentsIntersect(a0: Coordinate, a1: Coordinate, b0: Coordinate, b1: Coordinate): boolean;
+declare namespace segmentsIntersect {
+    type Coord = [number, number];
+}
+
+/**
+ * Test if the closed line segment `[firstStart, firstEnd]` intersects
+ * the closed line segment `[secondStart, secondEnd]`
+ * @param firstStart An end point of the first line
+ * @param firstEnd An end point of the first line
+ * @param secondStart An end point of the second line
+ * @param secondEnd An end point of the second line
+ * @returns Whether the lines intersect
+ */
+declare function segmentsIntersect(
+    firstStart: segmentsIntersect.Coord,
+    firstEnd: segmentsIntersect.Coord,
+    secondStart: segmentsIntersect.Coord,
+    secondEnd: segmentsIntersect.Coord,
+): boolean;
 
 export = segmentsIntersect;
-
