6467bc1dbd476f59beac855ae935a2fc7148bedb
wukongproject <94079846+wukongproject@users.noreply.github.com>
2022-04-25 16:09:03 +0200

    Fix lint errors

--- types/ethereum-checksum-address/index.d.ts
+++ types/ethereum-checksum-address/index.d.ts
@@ -1,10 +1,7 @@
-// Type definitions for ethereum-checksum-address
+// Type definitions for ethereum-checksum-address 0.0.8
 // Project: https://github.com/miguelmota/ethereum-checksum-address
 // Definitions by: Wukong Project <https://github.com/wukongproject>
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
 
-declare module "ethereum-checksum-address" {
-  // Types inside here
-  export function toChecksumAddress (address: string, chainId?: number | string): string
-  export function checkAddressChecksum (address: string, chainId?: number | string): boolean
-}
+export function toChecksumAddress (address: string, chainId?: number | string): string
+export function checkAddressChecksum (address: string, chainId?: number | string): boolean
