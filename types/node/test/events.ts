00417c04de2845bdcca6a83d88b4c5a6a4925bad
James Bromwell <943160+thw0rted@users.noreply.github.com>
2022-04-26 09:03:16 +0200

    Missed one reference

--- types/node/test/events.ts
+++ types/node/test/events.ts
@@ -115,6 +115,8 @@ async function test() {
     captureRejectionSymbol2 = events.captureRejectionSymbol;
 }
 
+// TODO: remove once global Event works (see events.d.ts)
+import { EventTarget } from "node:dom-events";
 {
     events.EventEmitter.setMaxListeners();
     events.EventEmitter.setMaxListeners(42);
