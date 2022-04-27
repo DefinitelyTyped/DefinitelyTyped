324c45f067d1c7064805f87c7db12264db062f8f
Mustafa Salaheldin <mustafa.salaheldin@yahoo.com>
2022-04-27 08:20:28 +0400

    init
    initialize new definition for bootstrap-filestyle 2.1

---
+++ types/bootstrap4-filestyle/index.d.ts
@@ -0,0 +1,34 @@
+// Type definitions for bootstrap-filestyle 2.1
+// Project: https://github.com/jcputney/bootstrap-filestyle
+// Definitions by: Mustafa Salaheldin <https://github.com/mustafasalahuldin>
+// Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
+// TypeScript Version: 2.3
+
+/// <reference types="jquery"/>
+
+interface FilestyleOptions {
+    text?: string | undefined;
+    htmlIcon?: string | undefined;
+    btnClass?: string | undefined;
+    size?: string | undefined;
+    input?: boolean | undefined;
+    badge?: boolean | undefined;
+    badgeName?: string | undefined;
+    icon?: boolean | undefined;
+    buttonBefore?: boolean | undefined;
+    dragdrop?: boolean | undefined;
+    disabled?: boolean | undefined;
+    placeholder?: string | undefined;
+    onChange?: ((event: FileList) => void) | undefined;
+}
+
+interface JQuery {
+
+    filestyle(options?: FilestyleOptions): JQuery;
+
+    filestyle(method: 'clear' | 'destroy' | 'htmlIcon' | 'htmlInput' | 'pushNameFiles'): JQuery;
+
+    filestyle(method: 'disabled' | 'buttonBefore' | 'dragdrop' | 'icon' | 'input', value: boolean): JQuery;
+
+    filestyle(method: 'size' | 'placeholder' | 'text' | 'btnClass' | 'htmlIcon' | 'badgeName', value: string): JQuery;
+}
