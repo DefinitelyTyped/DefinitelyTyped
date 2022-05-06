e5074c7244279bdfac392962fd1fd24de902c587
Naor Peled <thehecticbyte@gmail.com>
2022-05-07 02:48:58 +0300

    fixed tests

--- types/aws-lambda/test/cloudfront-tests.ts
+++ types/aws-lambda/test/cloudfront-tests.ts
@@ -345,68 +345,65 @@ const originRequestEvent: CloudFrontRequestEvent = {
 };
 
 const originRequestEventRecord: CloudFrontRequestEventRecord = {
-
-        cf: {
-            config: {
-                distributionDomainName: 'd111111abcdef8.cloudfront.net',
-                distributionId: 'EDFDVBD6EXAMPLE',
-                eventType: 'origin-request',
-                requestId: '4TyzHTaYWb1GX1qTfsHhEqV6HUDd_BzoBZnwfnvQc_1oF26ClkoUSEQ==',
-            },
-            request: {
-                clientIp: '203.0.113.178',
-                headers: {
-                    'x-forwarded-for': [
-                        {
-                            key: 'X-Forwarded-For',
-                            value: '203.0.113.178',
-                        },
-                    ],
-                    'user-agent': [
-                        {
-                            key: 'User-Agent',
-                            value: 'Amazon CloudFront',
-                        },
-                    ],
-                    via: [
-                        {
-                            key: 'Via',
-                            value: '2.0 2afae0d44e2540f472c0635ab62c232b.cloudfront.net (CloudFront)',
-                        },
-                    ],
-                    host: [
-                        {
-                            key: 'Host',
-                            value: 'example.org',
-                        },
-                    ],
-                    'cache-control': [
-                        {
-                            key: 'Cache-Control',
-                            value: 'no-cache, cf-no-cache',
-                        },
-                    ],
-                },
-                method: 'GET',
-                origin: {
-                    custom: {
-                        customHeaders: {},
-                        domainName: 'example.org',
-                        keepaliveTimeout: 5,
-                        path: '',
-                        port: 443,
-                        protocol: 'https',
-                        readTimeout: 30,
-                        sslProtocols: ['TLSv1', 'TLSv1.1', 'TLSv1.2'],
+    cf: {
+        config: {
+            distributionDomainName: 'd111111abcdef8.cloudfront.net',
+            distributionId: 'EDFDVBD6EXAMPLE',
+            eventType: 'origin-request',
+            requestId: '4TyzHTaYWb1GX1qTfsHhEqV6HUDd_BzoBZnwfnvQc_1oF26ClkoUSEQ==',
+        },
+        request: {
+            clientIp: '203.0.113.178',
+            headers: {
+                'x-forwarded-for': [
+                    {
+                        key: 'X-Forwarded-For',
+                        value: '203.0.113.178',
                     },
-                },
-                querystring: '',
-                uri: '/',
+                ],
+                'user-agent': [
+                    {
+                        key: 'User-Agent',
+                        value: 'Amazon CloudFront',
+                    },
+                ],
+                via: [
+                    {
+                        key: 'Via',
+                        value: '2.0 2afae0d44e2540f472c0635ab62c232b.cloudfront.net (CloudFront)',
+                    },
+                ],
+                host: [
+                    {
+                        key: 'Host',
+                        value: 'example.org',
+                    },
+                ],
+                'cache-control': [
+                    {
+                        key: 'Cache-Control',
+                        value: 'no-cache, cf-no-cache',
+                    },
+                ],
             },
+            method: 'GET',
+            origin: {
+                custom: {
+                    customHeaders: {},
+                    domainName: 'example.org',
+                    keepaliveTimeout: 5,
+                    path: '',
+                    port: 443,
+                    protocol: 'https',
+                    readTimeout: 30,
+                    sslProtocols: ['TLSv1', 'TLSv1.1', 'TLSv1.2'],
+                },
+            },
+            querystring: '',
+            uri: '/',
         },
     },
-
-    };
+};
 
 const originResponseEvent: CloudFrontResponseEvent = {
     Records: [
