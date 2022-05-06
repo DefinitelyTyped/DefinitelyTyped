49dd226d3f9512bb4e864071ca7274a64fdef45b
Naor Peled <thehecticbyte@gmail.com>
2022-05-07 02:41:49 +0300

    added missing type

--- types/aws-lambda/test/cloudfront-tests.ts
+++ types/aws-lambda/test/cloudfront-tests.ts
@@ -1,4 +1,4 @@
-import {
+import type {
     CloudFrontHeaders,
     CloudFrontRequestEvent,
     CloudFrontRequestHandler,
--- types/aws-lambda/trigger/cloudfront-request.d.ts
+++ types/aws-lambda/trigger/cloudfront-request.d.ts
@@ -4,17 +4,19 @@ import { Handler, Callback } from "../handler";
 export type CloudFrontRequestHandler = Handler<CloudFrontRequestEvent, CloudFrontRequestResult>;
 export type CloudFrontRequestCallback = Callback<CloudFrontRequestResult>;
 
+export interface CloudFrontRequestEventRecord {
+    cf: CloudFrontEvent & {
+        request: CloudFrontRequest;
+    };
+}
+
 /**
  * CloudFront viewer request or origin request event
  *
  * https://docs.aws.amazon.com/AmazonCloudFront/latest/DeveloperGuide/lambda-event-structure.html#lambda-event-structure-request
  */
 export interface CloudFrontRequestEvent {
-    Records: Array<{
-        cf: CloudFrontEvent & {
-            request: CloudFrontRequest;
-        };
-    }>;
+    Records: CloudFrontRequestEventRecord[];
 }
 
 export type CloudFrontRequestResult = undefined | null | CloudFrontResultResponse | CloudFrontRequest;
