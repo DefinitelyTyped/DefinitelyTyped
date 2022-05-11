28d610fa3fca4023365ad0f1056ff6156ec3ab50
rafaelgilete <rafaelgiletemogena@outlook.com>
2022-05-11 17:29:01 +0200

    fixed: refactored test to check that the returned rows are indeed properly typed

--- types/pg-cursor/pg-cursor-tests.ts
+++ types/pg-cursor/pg-cursor-tests.ts
@@ -47,10 +47,13 @@ const handleFn: ResultCallback<string> = (err: Error | undefined, rows: string[]
 handle.read(100, handleFn);
 
 // Returns promise when no callback
-handle
+const promiseHandle = client.query(new Cursor<{ name: string }>('SELECT $1::text as name', ['brianc']));
+promiseHandle
     .read(100)
     .then(rows => {
-        console.log(rows);
+        const [row] = rows;
+        if (typeof row.name !== 'string') throw new Error('Generic type does not work');
+        return rows;
     })
     .catch(error => console.log(error.message));
 
