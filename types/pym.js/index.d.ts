05661b119ed5f7c126d776129df16d387d71ba95
James Lin <james.lin@telescope.tv>
2022-05-15 13:04:17 -0700

    add pym.js typings

---
+++ types/pym.js/index.d.ts
@@ -0,0 +1,138 @@
+// Type definitions for pym.js 1.3.2
+// Project: https://github.com/nprapps/pym.js/
+// Definitions by: James Lin <https://github.com/igowerf>
+// Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
+
+declare module 'pym.js' {
+    interface Callback {
+        (value: string): void;
+    }
+
+    /**
+     * An optional configuraiton object for `pym.Child`
+     */
+    interface ChildOptions {
+        /**
+         * callback invoked after receiving a resize event from the parent
+         */
+        renderCallback?: Function | null;
+
+        /**
+         * xdomain to validate messages received. defaults to '*'.
+         */
+        xdomain?: string;
+
+        /**
+         *  polling frequency in milliseconds to send height to parent. defaults to 0.
+         */
+        polling?: number;
+
+        /**
+         * parent container id used when navigating the child iframe to a new page but we want to keep it responsive
+         */
+        id?: number;
+
+        /**
+         * if passed it will be override the default parentUrl query string parameter name expected on the iframe src
+         */
+        parenturlparam?: string;
+    }
+
+    /**
+     * Optional configuration for the parent instance.
+     */
+    interface ParentOptions {
+        /**
+         * defaults to '*'. xdomain to validate messages received
+         */
+        xdomain?: string;
+
+        /**
+         * if passed it will be assigned to the iframe title attribute
+         */
+        title?: string;
+
+        /**
+         * if passed it will be assigned to the iframe name attribute
+         */
+        name?: string;
+
+        /**
+         * if passed it will be assigned to the iframe id attribute
+         */
+        id?: string;
+
+        /**
+         * if passed and different than false it will be assigned to the iframe allowfullscreen attribute
+         */
+        allowfullscreen?: boolean;
+
+        /**
+         * if passed it will be assigned to the iframe sandbox attribute (we do not validate the syntax so be careful!!)
+         */
+        sandbox?: string;
+
+        /**
+         * if passed it will be override the default parentUrl query string parameter name passed to the iframe src
+         */
+        parenturlparam?: string;
+
+        /**
+         * if passed it will be override the default parentUrl query string parameter value passed to the iframe src
+         */
+        parenturlvalue?: string;
+
+        /**
+         * if passed and different than false it will strip the querystring params parentUrl and parentTitle passed to the iframe src
+         */
+        optionalparams?: string;
+
+        /**
+         * if passed it will activate scroll tracking on the parent
+         */
+        trackscroll?: boolean;
+
+        /**
+         * if passed it will set the throttle wait in order to fire scroll messaging. Defaults to 100 ms.
+         */
+        scrollwait?: number;
+    }
+
+    /**
+     * The Child half of a response iframe.
+     *
+     * @class Child
+     * @param {Object} config Optional configuration for the child instance.
+     */
+    class Child {
+        constructor(config?: ChildOptions);
+        navigateParentTo(url: string);
+        onMessage(messageType: string, callback: Callback);
+        sendMessage(messageType: string, message: string);
+        getParentPositionInfo();
+        scrollParentTo(hash: string);
+        scrollParentToChildEl(id: string);
+        scrollParentToChildPos(pos: number);
+        sendHeight();
+        remove();
+    }
+
+    /**
+     * The Parent half of a response iframe.
+     *
+     * @class Parent
+     * @param {String} id The id of the div into which the iframe will be rendered.
+     * @param {String} url The url of the iframe source.
+     * @param {Object} config Optional configuration for the parent instance.
+     */
+    class Parent {
+        constructor(id: string, url: string, config?: ParentOptions);
+        onMessage(messageType: string, callback: Callback);
+        sendMessage(messageType: string, message: string);
+        sendWidth();
+        sendViewportAndIFramePosition();
+        remove();
+    }
+
+    function autoInit(doNotRaiseEvents: boolean);
+}
---
+++ types/pym.js/pym.js-tests.ts
@@ -0,0 +1,29 @@
+import pym from 'pym.js';
+
+const pymParent = new pym.Parent('example-1', 'https://blog.apps.npr.org/pym.js/examples/table/child.html', {});
+new pym.Child({});
+pymParent.onMessage('customMessage', (message: string) => {
+    console.log(message);
+});
+pymParent.sendMessage('customMessage', 'hello from parent');
+pymParent.sendViewportAndIFramePosition();
+pymParent.sendWidth();
+
+const pymChild = new pym.Child({
+    polling: 1000,
+});
+pymChild.getParentPositionInfo();
+pymChild.navigateParentTo('');
+pymChild.onMessage('customMessage', (message: string) => {
+    console.log(message);
+});
+pymChild.scrollParentTo('#foo');
+pymChild.scrollParentToChildEl('#bar');
+pymChild.scrollParentToChildPos(10);
+pymChild.sendHeight();
+pymChild.sendMessage('customMessage', 'hello from child');
+
+pymChild.remove();
+pymParent.remove();
+
+pym.autoInit(true);
