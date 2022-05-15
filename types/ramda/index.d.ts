e4717ee036de55de94a8c393c338d9e1576a3a9c
valerii15298 <valerii15298@gmail.com>
2022-05-15 18:07:21 +0300

    fix problem, satisfy test case

--- types/ramda/index.d.ts
+++ types/ramda/index.d.ts
@@ -3487,10 +3487,9 @@ export function not(value: any): boolean;
  */
 export function nth<T>(n: number, list: readonly T[]): T | undefined;
 export function nth(n: number, list: string): string;
-export function nth(n: number): {
-    <T>(list: readonly T[]): T | undefined;
-    (list: string): string;
-};
+export function nth(
+    n: number,
+): <T extends readonly any[] | string>(list: T) => (T extends Array<infer E> ? E : string) | undefined;
 
 /**
  * Returns a function which returns its nth argument.
