6974e96552179008666bab542a23cf69a2bcbaa0
Cameron Dubas <camerondubas@gmail.com>
2022-05-15 13:44:49 +0100

    Add types for ember-useragent
    Improve tests
    
    Add Ember TS core team members as definition owners
    
    Remove unused import

---
+++ types/ember-useragent/index.d.ts
@@ -0,0 +1,13 @@
+// Type definitions for ember-useragent 0.12
+// Project: https://github.com/willviles/ember-useragent#readme
+// Definitions by: Cameron Dubas <https://github.com/camerondubas>
+//                 Chris Krycho <https://github.com/chriskrycho>
+//                 James C. Davis <https://github.com/jamescdavis>
+//                 Dan Freeman <https://github.com/dfreeman>
+// Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
+// TypeScript Version: 4.4
+
+import './services/user-agent';
+import './helpers/user-agent';
+
+export {};
