b8a190a9fd357282218782c471c8b4d0ba39069b
Cameron Dubas <camerondubas@gmail.com>
2022-05-16 19:25:22 +0100

    Remove ember ts core team members

--- types/ember-useragent/index.d.ts
+++ types/ember-useragent/index.d.ts
@@ -1,9 +1,6 @@
 // Type definitions for ember-useragent 0.12
 // Project: https://github.com/willviles/ember-useragent#readme
 // Definitions by: Cameron Dubas <https://github.com/camerondubas>
-//                 Chris Krycho <https://github.com/chriskrycho>
-//                 James C. Davis <https://github.com/jamescdavis>
-//                 Dan Freeman <https://github.com/dfreeman>
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
 // TypeScript Version: 4.4
 
