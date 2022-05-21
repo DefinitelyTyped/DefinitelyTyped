700230d20e563e224a5cdfc324788b24e60393a8
Joe Barnett <joebarnett876@gmail.com>
2022-05-21 21:01:04 +0100

    format, rename some generic params, don't use unnecessary ReadonlyArray

--- types/redux-define/index.d.ts
+++ types/redux-define/index.d.ts
@@ -7,69 +7,76 @@
 export as namespace ReduxDefine;
 
 export type SubActionProps<
-    A extends string,
-    SubActions extends ReadonlyArray<A>,
+    SubAction extends string, // This is necessary to force narrowing of the subaction type, as described here https://github.com/microsoft/TypeScript/issues/30680
+    SubActions extends SubAction[],
     Namespace extends string | undefined,
-    T extends string,
+    OwnAction extends string,
 > = {
-    [k in SubActions[number]]: `${Namespace extends string ? `${Namespace}/` : ''}${T}_${k}`;
+    [k in SubActions[number]]: `${Namespace extends string ? `${Namespace}/` : ''}${OwnAction}_${k}`;
 };
 
-export type NamespaceString<T extends Action | string> = T extends Action ? T['ACTION'] : T;
+export type NamespaceString<Namespace extends Action | string> = Namespace extends Action
+    ? Namespace['ACTION']
+    : Namespace;
 
-export type WithNamespace<T extends string, Namespace extends string | undefined> = Namespace extends string
-    ? `${Namespace}/${T}`
-    : T;
+export type WithNamespace<OwnAction extends string, Namespace extends string | undefined> = Namespace extends string
+    ? `${Namespace}/${OwnAction}`
+    : OwnAction;
 
-export interface PlainAction<T extends string, Namespace extends string | undefined> {
-    ACTION: WithNamespace<T, Namespace>;
+export interface PlainAction<OwnAction extends string, Namespace extends string | undefined> {
+    ACTION: WithNamespace<OwnAction, Namespace>;
     defineAction: typeof defineActionMethod;
-    toString: () => WithNamespace<T, Namespace>;
+    toString: () => WithNamespace<OwnAction, Namespace>;
 }
 
 export type Action<
-    T extends string = string,
-    A extends string = string,
-    SubActions extends ReadonlyArray<A> | undefined = undefined,
+    OwnAction extends string = string,
+    SubAction extends string = string,
+    SubActions extends SubAction[] | undefined = undefined,
     Namespace extends string | undefined = undefined,
 > = SubActions extends readonly string[]
-    ? PlainAction<T, Namespace> & SubActionProps<A, SubActions, Namespace, T>
-    : PlainAction<T, Namespace>;
+    ? PlainAction<OwnAction, Namespace> & SubActionProps<SubAction, SubActions, Namespace, OwnAction>
+    : PlainAction<OwnAction, Namespace>;
 
-export function defineAction<T extends string>(actionType: T): Action<T>;
-export function defineAction<T extends string, A extends string, S extends ReadonlyArray<A>>(
-    actionType: T,
+export function defineAction<OwnAction extends string>(actionType: OwnAction): Action<OwnAction>;
+export function defineAction<OwnAction extends string, SubAction extends string, S extends SubAction[]>(
+    actionType: OwnAction,
     subactions: S,
-): Action<T, A, S>;
-export function defineAction<T extends string, A extends string, S extends ReadonlyArray<A>, N extends string | Action>(
-    actionType: T,
-    subactions: S,
-    namespace: N,
-): Action<T, A, S, NamespaceString<N>>;
-export function defineAction<T extends string, N extends string | Action>(
-    actionType: T,
-    namespace: N,
-): Action<T, string, undefined, NamespaceString<N>>;
+): Action<OwnAction, SubAction, S>;
+export function defineAction<
+    OwnAction extends string,
+    SubAction extends string,
+    SubActions extends SubAction[],
+    Namespace extends string | Action,
+>(
+    actionType: OwnAction,
+    subactions: SubActions,
+    namespace: Namespace,
+): Action<OwnAction, SubAction, SubActions, NamespaceString<Namespace>>;
+export function defineAction<OwnAction extends string, Namespace extends string | Action>(
+    actionType: OwnAction,
+    namespace: Namespace,
+): Action<OwnAction, string, undefined, NamespaceString<Namespace>>;
 
-export function defineActionMethod<Parent extends Action, T extends string>(
+export function defineActionMethod<Parent extends Action, OwnAction extends string>(
     this: Parent,
-    actionType: T,
-): Action<T, string, undefined, Parent['ACTION']>;
-export function defineActionMethod<T extends string, N extends string | Action>(
+    actionType: OwnAction,
+): Action<OwnAction, string, undefined, Parent['ACTION']>;
+export function defineActionMethod<OwnAction extends string, Namespace extends string | Action>(
     this: Action,
-    actionType: T,
-    namespace: N,
+    actionType: OwnAction,
+    namespace: Namespace,
     // Here we use the namespace passed as an argument, rather than the one from `this`.
     // This is accurate reflection of the lib behaviour, but possibly a bug.
-): Action<T, string, undefined, NamespaceString<N>>;
+): Action<OwnAction, string, undefined, NamespaceString<Namespace>>;
 export function defineActionMethod<
     Parent extends Action,
-    T extends string,
-    A extends string,
-    S extends ReadonlyArray<A>,
+    OwnAction extends string,
+    SubAction extends string,
+    SubActions extends SubAction[],
 >(
     this: Parent,
-    actionType: T,
-    subactions: S,
+    actionType: OwnAction,
+    subactions: SubActions,
     namespace?: string | Action, // Has no effect but is permitted.
-): Action<T, A, S, Parent['ACTION']>;
+): Action<OwnAction, SubAction, SubActions, Parent['ACTION']>;
