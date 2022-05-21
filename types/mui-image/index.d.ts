6203f0cc30d2e005445de1851e9387586cd0a1e2
benmneb <62878664+benmneb@users.noreply.github.com>
2022-05-21 15:31:48 +0800

    ci(mui-image): fix dt-header, no-single-declare-module, no-declare-current-package, interface-name

--- types/mui-image/index.d.ts
+++ types/mui-image/index.d.ts
@@ -1,37 +1,35 @@
-// Type definitions for mui-image 1.0.4
+// Type definitions for mui-image 1.0
 // Project: https://github.com/benmneb/mui-image
 // Definitions by: benmneb <https://github.com/benmneb>
 //                 Natalia <https://github.com/CodeMeNatalie>
 // Definitions: https://github.com/DefinitelyTyped/mui-image
 
-declare module 'mui-image' {
-    import * as React from 'react';
+import * as React from 'react';
 
-    interface IMuiImage {
-        alt?: string;
-        bgColor?: React.CSSProperties['backgroundColor'];
-        className?: string;
-        distance?: string | number;
-        duration?: number;
-        easing?: React.CSSProperties['transitionTimingFunction'];
-        errorIcon?: boolean | React.ReactNode;
-        fit?: React.CSSProperties['objectFit'];
-        height?: React.CSSProperties['height'] | number;
-        iconWrapperClassName?: string;
-        iconWrapperStyle?: React.CSSProperties;
-        onError?: (...args: any[]) => void;
-        onLoad?: (...args: any[]) => void;
-        position?: React.CSSProperties['position'];
-        shift?: 'left' | 'right' | 'top' | 'bottom' | false | null;
-        shiftDuration?: number;
-        showLoading?: boolean | React.ReactNode;
-        src: string;
-        style?: React.CSSProperties;
-        width?: React.CSSProperties['width'] | number;
-        wrapperClassName?: string;
-        wrapperStyle?: React.CSSProperties;
-    }
-
-    const Image: React.FC<IMuiImage>;
-    export default Image;
+interface MuiImageProps {
+    alt?: string;
+    bgColor?: React.CSSProperties['backgroundColor'];
+    className?: string;
+    distance?: string | number;
+    duration?: number;
+    easing?: React.CSSProperties['transitionTimingFunction'];
+    errorIcon?: boolean | React.ReactNode;
+    fit?: React.CSSProperties['objectFit'];
+    height?: React.CSSProperties['height'] | number;
+    iconWrapperClassName?: string;
+    iconWrapperStyle?: React.CSSProperties;
+    onError?: (...args: any[]) => void;
+    onLoad?: (...args: any[]) => void;
+    position?: React.CSSProperties['position'];
+    shift?: 'left' | 'right' | 'top' | 'bottom' | false | null;
+    shiftDuration?: number;
+    showLoading?: boolean | React.ReactNode;
+    src: string;
+    style?: React.CSSProperties;
+    width?: React.CSSProperties['width'] | number;
+    wrapperClassName?: string;
+    wrapperStyle?: React.CSSProperties;
 }
+
+declare const Image: React.FC<MuiImageProps>;
+export default Image;
