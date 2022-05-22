4c476d4a114341394c1301718bc90c08892b19b3
benmneb <62878664+benmneb@users.noreply.github.com>
2022-05-22 05:30:00 +0000

    fix: add support for exactOptionalPropertyTypes
    Co-authored-by: Piotr Błażejewicz (Peter Blazejewicz) <peterblazejewicz@users.noreply.github.com>

--- types/mui-image/index.d.ts
+++ types/mui-image/index.d.ts
@@ -7,28 +7,28 @@
 import * as React from 'react';
 
 interface MuiImageProps {
-    alt?: string;
-    bgColor?: React.CSSProperties['backgroundColor'];
-    className?: string;
-    distance?: string | number;
-    duration?: number;
-    easing?: React.CSSProperties['transitionTimingFunction'];
-    errorIcon?: boolean | React.ReactNode;
-    fit?: React.CSSProperties['objectFit'];
-    height?: React.CSSProperties['height'] | number;
-    iconWrapperClassName?: string;
-    iconWrapperStyle?: React.CSSProperties;
-    onError?: (...args: any[]) => any;
-    onLoad?: (...args: any[]) => any;
-    position?: React.CSSProperties['position'];
-    shift?: 'left' | 'right' | 'top' | 'bottom' | false | null;
-    shiftDuration?: number;
-    showLoading?: boolean | React.ReactNode;
+    alt?: string | undefined;
+    bgColor?: React.CSSProperties['backgroundColor'] | undefined;
+    className?: string | undefined;
+    distance?: string | number | undefined;
+    duration?: number | undefined;
+    easing?: React.CSSProperties['transitionTimingFunction'] | undefined;
+    errorIcon?: boolean | React.ReactNode | undefined;
+    fit?: React.CSSProperties['objectFit'] | undefined;
+    height?: React.CSSProperties['height'] | number | undefined;
+    iconWrapperClassName?: string | undefined;
+    iconWrapperStyle?: React.CSSProperties | undefined;
+    onError?: (...args: any[]) => any | undefined;
+    onLoad?: (...args: any[]) => any | undefined;
+    position?: React.CSSProperties['position'] | undefined;
+    shift?: 'left' | 'right' | 'top' | 'bottom' | false | null | undefined;
+    shiftDuration?: number | undefined;
+    showLoading?: boolean | React.ReactNode | undefined;
     src: string;
-    style?: React.CSSProperties;
-    width?: React.CSSProperties['width'] | number;
-    wrapperClassName?: string;
-    wrapperStyle?: React.CSSProperties;
+    style?: React.CSSProperties | undefined;
+    width?: React.CSSProperties['width'] | number | undefined;
+    wrapperClassName?: string | undefined;
+    wrapperStyle?: React.CSSProperties | undefined;
 }
 
 declare const Image: React.FC<MuiImageProps>;
