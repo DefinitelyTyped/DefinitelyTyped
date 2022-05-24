0173927b69a45cb5ecdd230cf97f1b2299b25224
Gabe Gorelick <gabe@hightouch.io>
2022-05-24 17:31:34 -0400

    Add column type utility functions to snowflake-sdk
    See https://github.com/snowflakedb/snowflake-connector-nodejs/blob/5848cb627cf9ad5b90e0e2a69b4a1abd4b7cefc3/lib/connection/result/column.js#L103-L116

--- types/snowflake-sdk/index.d.ts
+++ types/snowflake-sdk/index.d.ts
@@ -312,9 +312,74 @@ export interface Column {
     getScale(): number;
 
     /**
-     * Retuns the type associated with this column.
+     * Returns the type associated with this column.
      */
     getType(): string;
+
+    /**
+     * Returns true if this column is type STRING.
+     */
+    isString(): boolean;
+
+    /**
+     * Returns true if this column is type BINARY.
+     */
+    isBinary(): boolean;
+
+    /**
+     * Returns true if this column is type NUMBER.
+     */
+    isNumber(): boolean;
+
+    /**
+     * Returns true if this column is type BOOLEAN.
+     */
+    isBoolean(): boolean;
+
+    /**
+     * Returns true if this column is type DATE.
+     */
+    isDate(): boolean;
+
+    /**
+     * Returns true if this column is type TIME.
+     */
+    isTime(): boolean;
+
+    /**
+     * Returns true if this column is type TIMESTAMP.
+     */
+    isTimestamp(): boolean;
+
+    /**
+     * Returns true if this column is type TIMESTAMP_LTZ.
+     */
+    isTimestampLtz(): boolean;
+
+    /**
+     * Returns true if this column is type TIMESTAMP_NTZ.
+     */
+    isTimestampNtz(): boolean;
+
+    /**
+     * Returns true if this column is type TIMESTAMP_TZ.
+     */
+    isTimestampTz(): boolean;
+
+    /**
+     * Returns true if this column is type VARIANT.
+     */
+    isVariant(): boolean;
+
+    /**
+     * Returns true if this column is type OBJECT.
+     */
+    isObject(): boolean;
+
+    /**
+     * Returns true if this column is type ARRAY.
+     */
+    isArray(): boolean;
 }
 
 export enum StatementStatus {
