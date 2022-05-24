ed329e43252558bce14f2198dd21fb939fe54b89
Sergey Kozlov <dartess@mail.ru>
2022-05-24 18:21:48 +0300

    add types chromium-installation-events

---
+++ types/dom-chromium-installation-events/index.d.ts
@@ -0,0 +1,30 @@
+// Type definitions for non-npm package dom-chromium-installation-events 101.0
+// Project: https://developer.mozilla.org/en-US/docs/Web/API/BeforeInstallPromptEvent
+// Definitions by: Sergey Kozlov <https://github.com/dartess>
+// Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
+
+export type AppBannerPromptOutcome = 'accepted' | 'dismissed';
+
+export interface PromptResponseObject {
+    readonly outcome: AppBannerPromptOutcome;
+    readonly platform: string;
+}
+
+declare global {
+    class BeforeInstallPromptEvent extends Event {
+        constructor(type: string, eventInitDict?: EventInit);
+        prompt(): Promise<PromptResponseObject>;
+        readonly platforms: string[];
+        readonly userChoice: Promise<PromptResponseObject>;
+    }
+
+    interface WindowEventMap {
+        beforeinstallprompt: BeforeInstallPromptEvent;
+        appinstalled: Event;
+    }
+
+    interface Window {
+        onappinstalled?: ((this: Window, ev: Event) => any) | null;
+        onbeforeinstallprompt?: ((this: Window, ev: BeforeInstallPromptEvent) => any) | null;
+    }
+}
