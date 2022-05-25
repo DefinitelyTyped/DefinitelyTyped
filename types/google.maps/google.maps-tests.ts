7824937c67ac61251a6cd17d2889baad6eee5487
jpoehnelt <jpoehnelt@users.noreply.github.com>
2022-05-25 13:46:44 +0000

    chore: sync updates to google.maps

--- types/google.maps/google.maps-tests.ts
+++ types/google.maps/google.maps-tests.ts
@@ -1,3 +1,3 @@
 // No tests required for generated types
-// Synced from: https://github.com/googlemaps/js-types/commit/c4bbf4d9df46342019fcc683f9b89aa7e15175cc
+// Synced from: https://github.com/googlemaps/js-types/commit/2b14e8ec50fd4cb69d9092d5c0759714e4ca3387
 google.maps.Map;
