469d7cf2a949f594b299f114dbd6319e40221621
Sean T. McBeth <sean.mcbeth@gmail.com>
2022-05-25 14:38:27 -0400

    run prettier

--- types/offscreencanvas/index.d.ts
+++ types/offscreencanvas/index.d.ts
@@ -12,9 +12,22 @@ interface HTMLCanvasElement {
 }
 
 // https://html.spec.whatwg.org/multipage/canvas.html#offscreencanvasrenderingcontext2d
-interface OffscreenCanvasRenderingContext2D extends CanvasState, CanvasTransform, CanvasCompositing,
-    CanvasImageSmoothing, CanvasFillStrokeStyles, CanvasShadowStyles, CanvasFilters, CanvasRect, CanvasDrawPath,
-    CanvasText, CanvasDrawImage, CanvasImageData, CanvasPathDrawingStyles, CanvasTextDrawingStyles, CanvasPath {
+interface OffscreenCanvasRenderingContext2D
+    extends CanvasState,
+        CanvasTransform,
+        CanvasCompositing,
+        CanvasImageSmoothing,
+        CanvasFillStrokeStyles,
+        CanvasShadowStyles,
+        CanvasFilters,
+        CanvasRect,
+        CanvasDrawPath,
+        CanvasText,
+        CanvasDrawImage,
+        CanvasImageData,
+        CanvasPathDrawingStyles,
+        CanvasTextDrawingStyles,
+        CanvasPath {
     readonly canvas: OffscreenCanvas;
 }
 
@@ -30,15 +43,21 @@ interface OffscreenCanvas extends EventTarget {
     width: number;
     height: number;
 
-    getContext(contextId: "2d", contextAttributes?: CanvasRenderingContext2DSettings): OffscreenCanvasRenderingContext2D | null;
+    getContext(
+        contextId: '2d',
+        contextAttributes?: CanvasRenderingContext2DSettings,
+    ): OffscreenCanvasRenderingContext2D | null;
 
-    getContext(contextId: "bitmaprenderer", contextAttributes?: WebGLContextAttributes): ImageBitmapRenderingContext | null;
+    getContext(
+        contextId: 'bitmaprenderer',
+        contextAttributes?: WebGLContextAttributes,
+    ): ImageBitmapRenderingContext | null;
 
-    getContext(contextId: "webgl", contextAttributes?: WebGLContextAttributes): WebGLRenderingContext | null;
+    getContext(contextId: 'webgl', contextAttributes?: WebGLContextAttributes): WebGLRenderingContext | null;
 
-    getContext(contextId: "webgl2", contextAttributes?: WebGLContextAttributes): WebGL2RenderingContext | null;
+    getContext(contextId: 'webgl2', contextAttributes?: WebGLContextAttributes): WebGL2RenderingContext | null;
 
-    convertToBlob(options?: { type?: string | undefined, quality?: number | undefined }): Promise<Blob>;
+    convertToBlob(options?: { type?: string | undefined; quality?: number | undefined }): Promise<Blob>;
 
     transferToImageBitmap(): ImageBitmap;
 }
@@ -49,14 +68,28 @@ interface CanvasDrawImage {
 
     drawImage(image: CanvasImageSource | OffscreenCanvas, dx: number, dy: number, dw: number, dh: number): void;
 
-    drawImage(image: CanvasImageSource | OffscreenCanvas, sx: number, sy: number, sw: number, sh: number,
-              dx: number, dy: number, dw: number, dh: number): void;
+    drawImage(
+        image: CanvasImageSource | OffscreenCanvas,
+        sx: number,
+        sy: number,
+        sw: number,
+        sh: number,
+        dx: number,
+        dy: number,
+        dw: number,
+        dh: number,
+    ): void;
 }
 
 // https://html.spec.whatwg.org/multipage/imagebitmap-and-animations.html#dom-createimagebitmap
 declare function createImageBitmap(image: ImageBitmapSource | OffscreenCanvas): Promise<ImageBitmap>;
-declare function createImageBitmap(image: ImageBitmapSource | OffscreenCanvas, sx: number, sy: number,
-                                   sw: number, sh: number): Promise<ImageBitmap>;
+declare function createImageBitmap(
+    image: ImageBitmapSource | OffscreenCanvas,
+    sx: number,
+    sy: number,
+    sw: number,
+    sh: number,
+): Promise<ImageBitmap>;
 
 // OffscreenCanvas should be a part of Transferable => extend all postMessage methods
 interface Worker {
@@ -75,11 +108,15 @@ interface Window {
     postMessage(message: any, targetOrigin: string, transfer?: Array<Transferable | OffscreenCanvas>): void;
 }
 
-declare function postMessage(message: any, targetOrigin: string, transfer?: Array<Transferable | OffscreenCanvas>): void;
+declare function postMessage(
+    message: any,
+    targetOrigin: string,
+    transfer?: Array<Transferable | OffscreenCanvas>,
+): void;
 
 declare var OffscreenCanvas: {
     prototype: OffscreenCanvas;
-    new(width: number, height: number): OffscreenCanvas;
+    new (width: number, height: number): OffscreenCanvas;
 };
 
 interface WebGL2RenderingContextBase {
