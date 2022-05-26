cb017bd4b6d8da36bbbfd9fed8dd8479fc4cccd6
goosewobbler <432005+goosewobbler@users.noreply.github.com>
2022-05-27 00:23:10 +0100

    Remove done.fail

--- types/jest/index.d.ts
+++ types/jest/index.d.ts
@@ -376,7 +376,6 @@ declare namespace jest {
 
     interface DoneCallback {
         (...args: any[]): any;
-        fail(error?: string | { message: string }): any;
     }
 
     type ProvidesCallback = ((cb: DoneCallback) => void | undefined) | (() => Promise<unknown>);
