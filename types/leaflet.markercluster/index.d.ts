7ed672cb64c7961e94ba2053404816bfd7c76ae4
Yaroslav Kormushyn <yaroslav.kormushyn@idunrealestate.com>
2022-05-27 18:56:07 +0300

    bump version

--- types/leaflet.markercluster/index.d.ts
+++ types/leaflet.markercluster/index.d.ts
@@ -1,4 +1,4 @@
-// Type definitions for Leaflet.markercluster 1.5
+// Type definitions for Leaflet.markercluster 1.5.1
 // Project: https://github.com/Leaflet/Leaflet.markercluster
 // Definitions by: Robert Imig <https://github.com/rimig>, Nenad Filipovic <https://github.com/nenadfilipovic>, Yaroslav Kormushyn <https://github.com/YaroslavKormushyn>
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
