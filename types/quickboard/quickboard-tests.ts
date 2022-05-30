1496acd1047cc21057b017412fbfe7bcf28fb5ac
foupp <viggonarvaez27@hotmail.com>
2022-05-30 04:13:45 -0400

    ]

--- types/quickboard/quickboard-tests.ts
+++ types/quickboard/quickboard-tests.ts
@@ -7,3 +7,5 @@ const result = new quickboard({
     map: (item: { member: { displayName: string }, value: number }, index: number) => `${index + 1}. ${item.member.displayName} - ${item.value} messages`,
     sort: (according: { member: { displayName: string }, value: number }, current: { member: { displayName: string }, value: number }) => current.value - according.value,
 }).create();
+
+console.log(result);
