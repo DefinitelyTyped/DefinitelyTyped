07b3a1fbd7cb7468a2b6c3203e318f72c6a775e2
Andreas Biørn-Hansen <andreasb.nor@gmail.com>
2022-06-01 19:02:02 +0200

    fix: Remove test case with old property name
    CI test failed due to inclusion of the old `confirmOK` property name.

--- types/awesome-notifications/awesome-notifications-tests.ts
+++ types/awesome-notifications/awesome-notifications-tests.ts
@@ -5,12 +5,6 @@ const awn = new AWN();
 const confirmed = () => console.log("Confirmed");
 const cancelled = () => console.log("Cancelled");
 
-awn.confirm(`Tests current confirmOK property name`, confirmed, cancelled, {
-  labels: {
-    confirmOK: "Current property name does not work"
-  }
-});
-
 awn.confirm(`Tests new confirmOk property name`, confirmed, cancelled, {
   labels: {
     confirmOk: "New property name works"
