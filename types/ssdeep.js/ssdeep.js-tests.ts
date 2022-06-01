12c04acd9f8f875b54d5fc6b343ea8cec3bc3d28
JPBM135 <jpedrobm0@gmail.com>
2022-06-01 19:15:26 -0300

    Fix: Bot called

--- types/ssdeep.js/ssdeep.js-tests.ts
+++ types/ssdeep.js/ssdeep.js-tests.ts
@@ -1,9 +1,9 @@
 import { digest, similarity } from 'ssdeep.js';
 
 const testStrings = {
-	test1: 'I love typescript',
-	test2: 'I love javascript',
-	test3: 'I love python :(',
+    test1: 'I love typescript',
+    test2: 'I love javascript',
+    test3: 'I love python :(',
 };
 
 digest(testStrings.test1); // $ExpectType string
@@ -11,9 +11,9 @@ digest(testStrings.test2); // $ExpectType string
 digest(testStrings.test3); // $ExpectType string
 
 const digestedStrings = {
-	test1: digest(testStrings.test1),
-	test2: digest(testStrings.test2),
-	test3: digest(testStrings.test3),
+    test1: digest(testStrings.test1),
+    test2: digest(testStrings.test2),
+    test3: digest(testStrings.test3),
 };
 
 similarity(digestedStrings.test1, digestedStrings.test2); // $ExpectType number
