17d2fbfff8e9ae98557401db80abd0cd33b570e0
Andreas Biørn-Hansen <andreasb.nor@gmail.com>
2022-06-01 18:52:49 +0200

    fix(awesome-notifications): Add "test" with old and new property name
    Current property name `confirmOK` does not impact the text in the confirm button, while the suggested change to `confirmOk` does.
    Demo: https://codesandbox.io/s/busy-keller-xzkv8f?file=/src/index.js:0-491

--- types/awesome-notifications/awesome-notifications-tests.ts
+++ types/awesome-notifications/awesome-notifications-tests.ts
@@ -1,4 +1,18 @@
-import AWN from 'awesome-notifications';
+import AWN from "awesome-notifications";
 
 const awn = new AWN();
-awn.alert('Hello world');
+
+const confirmed = () => console.log("Confirmed");
+const cancelled = () => console.log("Cancelled");
+
+awn.confirm(`Tests current confirmOK property name`, confirmed, cancelled, {
+  labels: {
+    confirmOK: "Current property name does not work"
+  }
+});
+
+awn.confirm(`Tests new confirmOk property name`, confirmed, cancelled, {
+  labels: {
+    confirmOk: "New property name works"
+  }
+});
