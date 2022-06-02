83c11b025b73b5f461744a57653435ea5fe8a718
Joonas Rouhiainen <joonas.rouhiainen@iki.fi>
2022-06-03 01:23:55 +0300

    fix: rm unnecessary reference path directive in types/marv/index.d.ts

--- types/marv/index.d.ts
+++ types/marv/index.d.ts
@@ -4,7 +4,6 @@
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
 
 /// <reference types="node" />
-/// <reference path="./api/callback.d.ts" />
 
 export type EmptyObject = {
     [K in any]: never;
