3d74dde30210a3afe8f792c54306e0c22d53586f
craigwu <waynecraig2008@gmail.com>
2022-06-03 10:31:45 +0800

    fix dtslint errors

--- types/tablestore/event.d.ts
+++ types/tablestore/event.d.ts
@@ -1,8 +1,8 @@
 declare namespace TableStore {
     class SequentialExecutor {
-        on(eventName: string, listener: Function): SequentialExecutor;
-        onAsync(eventName: string, listener: Function): SequentialExecutor;
-        removeListener(eventName: string, listener: Function): SequentialExecutor;
+        on(eventName: string, listener: () => void): SequentialExecutor;
+        onAsync(eventName: string, listener: () => void): SequentialExecutor;
+        removeListener(eventName: string, listener: () => void): SequentialExecutor;
         removeAllListeners(eventName: string): SequentialExecutor;
     }
 
