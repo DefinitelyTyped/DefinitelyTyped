96482dbc5509b3d88717e91a7f3ce752a71d96a4
jbovone <bovonejulian@gmail.com>
2022-06-04 13:43:04 -0300

    react-pivottable now passing CI

---
+++ types/react-pivottable/index.d.ts
@@ -0,0 +1,11 @@
+// Type definitions for react-pivottable 0.11
+// Project: https://github.com/medipass/react-payment-inputs
+// Definitions by: Julián Bovone <https://github.com/jbovone>
+// Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
+
+export { TableInput } from './input';
+export { PivotTableProps } from './PivotTable';
+export { PivotTableUIProps, DraggableAttributeProps, DropdownProps } from './PivotTableUI';
+export { TQuerySort, TSorterFn, Pivot } from './Utilities';
+export { BuildInPlotyRenderersNames } from './PlotlyRenderers';
+export { BuildInRenderersNames, TableRenderers } from './TableRenderers';
