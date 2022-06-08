746889a5cf03117bc71a68d5b3309e6c554ec29b
jbovone <bovonejulian@gmail.com>
2022-06-08 01:00:54 -0300

    adds all PivotTableUI exportables to index

--- types/react-pivottable/index.d.ts
+++ types/react-pivottable/index.d.ts
@@ -3,9 +3,17 @@
 // Definitions by: Julián Bovone <https://github.com/jbovone>
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
 
+import ReactPivottableUI from './PivotTableUI';
 export { TableInput } from './input';
 export { PivotTableProps } from './PivotTable';
-export { PivotTableUIProps, DraggableAttributeProps, DropdownProps } from './PivotTableUI';
+export {
+    PivotTableUIProps,
+    DraggableAttribute,
+    DraggableAttributeProps,
+    Dropdown,
+    DropdownProps,
+} from './PivotTableUI';
 export { TQuerySort, TSorterFn, Pivot } from './Utilities';
 export { BuildInPlotlyRenderersNames } from './PlotlyRenderers';
 export { BuildInRenderersNames, TableRenderers } from './TableRenderers';
+export default ReactPivottableUI;
