14ffd1280430c715041ce74f64457f55516ee63a
AJ Jenkins <aj.jenkins123@gmail.com>
2022-06-08 09:05:56 -0400

    Updated version number in comment

--- types/react-native-autocomplete-input/index.d.ts
+++ types/react-native-autocomplete-input/index.d.ts
@@ -1,4 +1,4 @@
-// Type definitions for react-native-autocomplete-input 5.0
+// Type definitions for react-native-autocomplete-input 5.1
 // Project: https://github.com/l-urence/react-native-autocomplete-input#readme
 // Definitions by: Ifiok Jr. <https://github.com/ifiokjr>
 //                 Kanitkorn Sujautra <https://github.com/lukyth>
