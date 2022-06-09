ec8d085f2e1d841a96b7971db4720fa8c9547416
Lair Júnior <lairjr@outlook.com>
2022-06-09 16:44:28 -0400

    Add comments to the index.d.ts

--- types/three-gif-loader/index.d.ts
+++ types/three-gif-loader/index.d.ts
@@ -1,21 +1,23 @@
-import { LoadingManager } from '../three/src/loaders/LoadingManager';
-import { CanvasTexture } from '../three/src/textures/CanvasTexture';
-import { GifReader } from '../omggif/index';
+// Type definitions for three-gif-loader 1.1
+// Project: https://github.com/movableink/three-gif-loader
+// Definitions by: Lair Junior <https://github.com/lairjr>
+// Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
 
-declare module 'three-gif-loader' {
-    export default class GifLoader {
-        constructor(manager?: LoadingManager);
-        load(
-            url: string,
-            onLoad: (reader: GifReader) => void,
-            onProgress?: (request: ProgressEvent) => void,
-            onError?: (event: ErrorEvent) => void,
-        ): GifTexture;
-        setPath(value: string): void;
-    }
+import { CanvasTexture, LoadingManager } from 'three';
+import { GifReader } from 'omggif';
 
-    class GifTexture extends CanvasTexture {
-        setReader(reader: GifReader): void;
-        draw(): void;
-    }
+export default class GifLoader {
+    constructor(manager?: LoadingManager);
+    load(
+        url: string,
+        onLoad: (reader: GifReader) => void,
+        onProgress?: (request: ProgressEvent) => void,
+        onError?: (event: ErrorEvent) => void,
+    ): GifTexture;
+    setPath(value: string): void;
+}
+
+export class GifTexture extends CanvasTexture {
+    setReader(reader: GifReader): void;
+    draw(): void;
 }
