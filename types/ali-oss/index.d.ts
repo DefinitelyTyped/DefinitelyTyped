bbb745c4315dd380f4f89949b233060d30b83dc1
Leo Liu <467195537@qq.com>
2022-06-11 11:27:39 +0800

    Update types/ali-oss/index.d.ts
    Co-authored-by: Adam Thompson-Sharpe <adamthompsonsharpe@gmail.com>

--- types/ali-oss/index.d.ts
+++ types/ali-oss/index.d.ts
@@ -1029,8 +1029,8 @@ declare class OSS {
     /**
      * Copy an object from sourceName to name.
      */
-     copy(name: string, sourceName: string, options?: OSS.CopyObjectOptions): Promise<OSS.CopyAndPutMetaResult>;
-     copy(name: string, sourceName: string, sourceBucket?: string, options?: OSS.CopyObjectOptions): Promise<OSS.CopyAndPutMetaResult>;
+    copy(name: string, sourceName: string, options?: OSS.CopyObjectOptions): Promise<OSS.CopyAndPutMetaResult>;
+    copy(name: string, sourceName: string, sourceBucket?: string, options?: OSS.CopyObjectOptions): Promise<OSS.CopyAndPutMetaResult>;
 
     /**
      * Set an exists object meta.
