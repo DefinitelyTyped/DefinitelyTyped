0b2246676baa04c0554feb1a66b99ac389ee1827
eliassjogreen <eliassjogreen1@gmail.com>
2022-06-13 14:28:37 +0200

    fix lints and structure

--- types/django-bananas/django-bananas-tests.tsx
+++ types/django-bananas/django-bananas-tests.tsx
@@ -1,5 +1,24 @@
 import Bananas from 'django-bananas';
+import themes from 'django-bananas/themes';
 
-export default function () {
-    return <Bananas.App api={{ url: '' }} />;
-}
+<Bananas.App api={{ url: '' }} pages={(route: string) => {}} />;
+<Bananas.App api="" pages={(route: string) => {}} />;
+<Bananas.App
+    api={{ url: '', requestInterceptor: request => request, responseInterceptor: response => response }}
+    pages={(route: string) => {}}
+/>;
+<Bananas.App api="" pages={(route: string) => {}} logo={true} />;
+<Bananas.App api="" pages={(route: string) => {}} logo={''} />;
+<Bananas.App api="" pages={(route: string) => {}} logo={<img />} />;
+<Bananas.App api="" pages={(route: string) => {}} theme={themes.dark} pageTheme={themes.light} />;
+<Bananas.App
+    api=""
+    pages={(route: string) => {}}
+    nav={{
+        'example.user:list': {
+            title: "example",
+            icon: undefined,
+            showSubheader: false
+        }
+    }}
+/>;
---
+++ types/django-bananas/themes.d.ts
@@ -0,0 +1,11 @@
+export function applyThemeDefaults(theme: any): any;
+export function createBananasTheme(theme: any): any;
+export function extendTheme(theme: any, overrides: any): any;
+
+declare const themes: {
+    light: any;
+    dark: any;
+    darth: any;
+    default: any;
+};
+export default themes;
