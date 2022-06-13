a6e46033f44a752b7f36f9f308c1ae9c8b00e026
Seungbin Oh <sboh1214@gmail.com>
2022-06-13 10:15:11 +0900

    fix react-type-animation

--- types/react-type-animation/react-type-animation-tests.tsx
+++ types/react-type-animation/react-type-animation-tests.tsx
@@ -1,41 +1,16 @@
 import * as React from 'react';
-import Typing from 'react-type-animation';
+import TypeAnimation from 'react-type-animation';
 
-const basic = () => (
-    <Typing>
-        <span>This span will get typed.</span>
-    </Typing>
+const example = () => (
+    <TypeAnimation cursor={false} sequence={['React typing animation based on typical', 1000, '']} wrapper="h2" />
 );
 
-const backspace = () => (
-    <Typing>
-        <span>This span will get typed, then erased.</span>
-        <Typing.Backspace count={20} />
-    </Typing>
-);
-
-const delay = () => (
-    <Typing>
-        <div>
-            There will be a 1000ms delay here,
-            <Typing.Delay ms={1000} />
-            then this will be typed.
-        </div>
-    </Typing>
-);
-
-const speed = () => (
-    <Typing speed={50}>
-        This line will be typed at 50ms/character,
-        <Typing.Speed ms={200} />
-        then this will be typed at 200ms/character.
-    </Typing>
-);
-
-const reset = () => (
-    <Typing>
-        <span>This line will stay.</span>
-        <span>This line will get instantly removed after a 500 ms delay</span>
-        <Typing.Reset count={1} delay={500} />
-    </Typing>
+const exampleWithRepeat = () => (
+    <TypeAnimation
+        cursor={false}
+        sequence={['얘야, 큰 힘에는 큰 책임이 따른단다', 6000, 'I am Ironman', 6500, '거 죽기 딱 좋은 날씨네', 6400]}
+        wrapper="p"
+        className="mainTyping"
+        repeat={Infinity}
+    />
 );
