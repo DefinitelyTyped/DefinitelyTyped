95b78d96eb18ba1346c702765e1abb6f9e4cf705
Fabio Nettis <nettisfabio@gmail.com>
2022-06-20 13:47:13 +0200

    chore(test): add tests for type defintion

---
+++ types/react-native-get-random-values/react-native-get-random-values-test.ts
@@ -0,0 +1,7 @@
+import './index.d';
+
+(() => {
+    const arr: Uint8Array = crypto.getRandomValues(new Uint8Array(1));
+    if (!(arr instanceof Uint8Array)) throw Error('getRandomValues() should return Uint8Array');
+    console.log('getRandomValues() returned Uint8Array');
+})();
