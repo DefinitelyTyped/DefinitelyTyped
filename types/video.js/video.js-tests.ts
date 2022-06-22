f7bc8645ed25c0399655fc403af9bd37d207ed61
Joe Flateau <joe@joeflateau.net>
2022-06-22 11:45:56 -0400

    fix: expected type

--- types/video.js/video.js-tests.ts
+++ types/video.js/video.js-tests.ts
@@ -110,7 +110,7 @@ videojs('example_video_1', playerOptions).ready(function playerReady() {
     ]);
 
     // the option passed when initializing player is a string
-    // $ExpectType videojs.Preload
+    // $ExpectType Preload | undefined
     this.options_.preload;
     // but the option when setting preload later is boolean
     this.preload(false);
