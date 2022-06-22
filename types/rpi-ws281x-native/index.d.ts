d54d68207850fa6d0f3a8d1592d0f8e4ab49b4ad
Nathan Rajlich <n@n8.io>
2022-06-21 23:25:45 -0700

    Use spaces

--- types/rpi-ws281x-native/index.d.ts
+++ types/rpi-ws281x-native/index.d.ts
@@ -7,46 +7,46 @@
 /// <reference types="node" />
 
 declare enum StripType {
-	SK6812_RGBW = 403703808,
-	SK6812_RBGW = 403701768,
-	SK6812_GRBW = 403181568,
-	SK6812_GBRW = 403177488,
-	SK6812_BRGW = 402657288,
-	SK6812_BGRW = 402655248,
-	WS2811_RGB = 1050624,
-	WS2811_RBG = 1048584,
-	WS2811_GRB = 528384,
-	WS2811_GBR = 524304,
-	WS2811_BRG = 4104,
-	WS2811_BGR = 2064,
-	WS2812 = 528384,
-	SK6812 = 528384,
-	SK6812W = 403177488,
+    SK6812_RGBW = 403703808,
+    SK6812_RBGW = 403701768,
+    SK6812_GRBW = 403181568,
+    SK6812_GBRW = 403177488,
+    SK6812_BRGW = 402657288,
+    SK6812_BGRW = 402655248,
+    WS2811_RGB = 1050624,
+    WS2811_RBG = 1048584,
+    WS2811_GRB = 528384,
+    WS2811_GBR = 524304,
+    WS2811_BRG = 4104,
+    WS2811_BGR = 2064,
+    WS2812 = 528384,
+    SK6812 = 528384,
+    SK6812W = 403177488,
 }
 
 interface Channel {
-	readonly count: number;
-	readonly stripType: StripType;
-	readonly invert: boolean;
-	readonly gpio: number;
-	brightness: number;
-	array: Uint32Array;
-	buffer: Buffer;
+    readonly count: number;
+    readonly stripType: StripType;
+    readonly invert: boolean;
+    readonly gpio: number;
+    brightness: number;
+    array: Uint32Array;
+    buffer: Buffer;
 }
 
 interface ChannelOptions {
-	stripType: StripType;
+    stripType: StripType;
 }
 
 interface Ws281x {
-	(numLeds: number, opts: ChannelOptions): Channel;
-	stripType: {
-		[k: string]: StripType;
-	};
-	init(): Channel[];
-	finalize(): void;
-	reset(): void;
-	render(): void;
+    (numLeds: number, opts: ChannelOptions): Channel;
+    stripType: {
+        [k: string]: StripType;
+    };
+    init(): Channel[];
+    finalize(): void;
+    reset(): void;
+    render(): void;
 }
 
 declare const ws281x: Ws281x;
