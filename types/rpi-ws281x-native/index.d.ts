f5a6c62dacd6c1af0e0ad2a4c6a0407e91eff325
Nathan Rajlich <n@n8.io>
2022-06-21 22:49:40 -0700

    [@types/rpi-ws281x-native] Add types for "rpi-ws281x-native" package
    Adds initial types for the "rpi-ws281x-native" npm package.

---
+++ types/rpi-ws281x-native/index.d.ts
@@ -0,0 +1,53 @@
+// Type definitions for rpi-ws281x-native 1.0
+// Project: https://github.com/beyondscreen/node-rpi-ws281x-native
+// Definitions by: Nathan Rajlich <https://github.com/TooTallNate>
+// Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
+// Minimum TypeScript Version: 4.0
+
+/// <reference types="node" />
+
+export enum StripType {
+	SK6812_RGBW = 403703808,
+	SK6812_RBGW = 403701768,
+	SK6812_GRBW = 403181568,
+	SK6812_GBRW = 403177488,
+	SK6812_BRGW = 402657288,
+	SK6812_BGRW = 402655248,
+	WS2811_RGB = 1050624,
+	WS2811_RBG = 1048584,
+	WS2811_GRB = 528384,
+	WS2811_GBR = 524304,
+	WS2811_BRG = 4104,
+	WS2811_BGR = 2064,
+	WS2812 = 528384,
+	SK6812 = 528384,
+	SK6812W = 403177488,
+}
+
+export interface Channel {
+	readonly count: number;
+	readonly stripType: StripType;
+	readonly invert: boolean;
+	readonly gpio: number;
+	brightness: number;
+	array: Uint32Array;
+	buffer: Buffer;
+}
+
+export interface ChannelOptions {
+	stripType: StripType;
+}
+
+interface Ws281x {
+	(numLeds: number, opts: ChannelOptions): Channel;
+	stripType: {
+		[k: string]: StripType;
+	};
+	init(): Channel[];
+	finalize(): void;
+	reset(): void;
+	render(): void;
+}
+
+declare const ws281x: Ws281x;
+export default ws281x;
---
+++ types/rpi-ws281x-native/rpi-ws281x-native-tests.ts
@@ -0,0 +1,16 @@
+import ws281x from 'rpi-ws281x-native';
+
+const NUM_LEDS = 100;
+
+const channel = ws281x(NUM_LEDS, { stripType: ws281x.stripType.WS2811_RGB });
+channel.brightness = 255;
+
+for (let i = 0; i < NUM_LEDS; i++) {
+    channel.array[i] = 0;
+}
+ws281x.render();
+
+process.on('SIGINT', () => {
+    ws281x.reset();
+    ws281x.finalize();
+});
