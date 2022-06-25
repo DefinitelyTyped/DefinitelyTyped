df1ff7d08808cb71998fa7210ed3c742e20e2385
Benjamin Kan <Benjamin.Kan@ibm.com>
2022-06-25 13:23:30 +0200

    Bump version to 0.18.0

--- types/matter-js/index.d.ts
+++ types/matter-js/index.d.ts
@@ -1,10 +1,11 @@
-// Type definitions for Matter.js - 0.17.1
+// Type definitions for Matter.js - 0.18.0
 // Project: https://github.com/liabru/matter-js
-// Definitions by: Ivane Gegia <https://twitter.com/ivanegegia>,
-//                 David Asmuth <https://github.com/piranha771>,
-//                 Piotr Pietrzak <https://github.com/hasparus>,
+// Definitions by: Ivane Gegia <https://twitter.com/ivanegegia>
+//                 David Asmuth <https://github.com/piranha771>
+//                 Piotr Pietrzak <https://github.com/hasparus>
 //                 Dale Whinham <https://github.com/dwhinham>
 //                 slikts <https://github.com/slikts>
+//                 Benjamin Kan <https://github.com/b3nk4n>
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
 
 export = Matter;
