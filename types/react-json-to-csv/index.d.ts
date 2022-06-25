69ffc8ef58b8a91b290ac40e242079c98f6353a9
Nui Rattapon <nui@maker.co.nz>
2022-06-26 01:11:57 +1200

    [react-json-to-csv] add type definition

---
+++ types/react-json-to-csv/index.d.ts
@@ -0,0 +1,15 @@
+// Type definitions for react-json-to-csv 1.0
+// Project: https://github.com/coston/react-json-to-csv
+// Definitions by: Nui Rattapon <https://github.com/nui-tech>
+// Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
+
+import * as React from 'react';
+
+export interface CsvDownloadProps extends React.HTMLAttributes<HTMLButtonElement> {
+    data: object | object[];
+    filename?: string;
+}
+
+declare function CsvDownload(props: CsvDownloadProps): JSX.Element;
+
+export default CsvDownload;
