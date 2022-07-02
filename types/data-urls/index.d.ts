57320aae71610de1f3f7250876fb9ff06365337a
Kle0s <ophiri99@gmail.com>
2022-07-02 11:33:43 +0300

    Now using major minor versions only

--- types/data-urls/index.d.ts
+++ types/data-urls/index.d.ts
@@ -1,4 +1,4 @@
-// Type definitions for data-urls 3.0.2
+// Type definitions for data-urls 3.0
 // Project: https://github.com/jsdom/data-urls#readme
 // Definitions by: Jaime Filho <https://github.com/jaimeadf>
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
