243e53a393519f0228d73c3c85c43c79115c9818
zerocho <zerohch0@gmail.com>
2022-07-02 14:08:06 +0900

    Fix: passReqToCallback must be true and clientSecret should be optional.

--- types/passport-kakao/passport-kakao-tests.ts
+++ types/passport-kakao/passport-kakao-tests.ts
@@ -1,11 +1,31 @@
 import { Strategy as KakaoStrategy } from 'passport-kakao';
+import { Request } from 'express';
 
 new KakaoStrategy({
         clientID: 'client',
-        clientSecret: 'clientSecret',
         callbackURL: 'callbackUrl',
     },
     (accessToken: string, refreshToken: string, profile: any, done: any) => {
+        // signUp or signIn
+    },
+);
+
+new KakaoStrategy({
+        clientID: 'client',
+        callbackURL: 'callbackUrl',
+        passReqToCallback: false,
+    },
+    (accessToken: string, refreshToken: string, profile: any, done: any) => {
+        // signUp or signIn
+    },
+);
+
+new KakaoStrategy({
+        clientID: 'client',
+        callbackURL: 'callbackUrl',
+        passReqToCallback: true,
+    },
+    (req: Request, accessToken: string, refreshToken: string, profile: any, done: any) => {
         // signUp or signIn
     },
 );
