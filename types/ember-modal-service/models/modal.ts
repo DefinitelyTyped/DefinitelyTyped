fd94469562a43bf468bef77e99d9521df94a6d21
golovkoe <golovkoe@ae.com>
2022-07-05 13:40:38 +0200

    Updating model

--- types/ember-modal-service/models/modal.ts
+++ types/ember-modal-service/models/modal.ts
@@ -1,9 +1,9 @@
 import type { defer, Promise } from 'rsvp';
 
 export default class ModalModel<T = Record<string, never>> {
-  declare name: string;
-  declare fullname: string;
-  declare options: T;
-  declare promise: Promise<unknown>;
-  declare deferred: typeof defer & { promise: Promise<T> };
+    declare name: string;
+    declare fullname: string;
+    declare options: T;
+    declare promise: Promise<unknown>;
+    declare deferred: ReturnType<typeof defer>;
 }
