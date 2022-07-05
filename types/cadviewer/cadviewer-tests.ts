ee6f8b7c0052c65675f36a408f51ea902e8741c0
CADViewer <61973235+CADViewer@users.noreply.github.com>
2022-07-05 10:47:36 +0200

    update index.d.ts/cadviewer-tests.ts

--- types/cadviewer/cadviewer-tests.ts
+++ types/cadviewer/cadviewer-tests.ts
@@ -1,7 +1,4 @@
-import * as cadviewer from "cadviewer";
-console.log(cadviewer.cvjs_version());   // return the version number 
-
 // follow general instalation instructions and samples at: https://cadviewer.com/cadviewertechdocs/ and https://github.com/CADViewer?tab=repositories
 // note CADViewer requires a back-end copnversion server, see: https://github.com/CADViewer/cadviewer-conversion-server
-
-
+import * as cadviewer from "cadviewer";
+console.log(cadviewer.cvjs_version());   // return the version number 
