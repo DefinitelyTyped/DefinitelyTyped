bbb06ca856177dbde4fe481cfcddf55b0a622a42
mengFxing <mengFxing@163.com>
2022-07-06 18:47:28 +0800

    fix(node): fix the params of clearInterval

--- types/node/timers.d.ts
+++ types/node/timers.d.ts
@@ -77,7 +77,7 @@ declare module 'timers' {
         namespace setInterval {
             const __promisify__: typeof setIntervalPromise;
         }
-        function clearInterval(intervalId: NodeJS.Timeout | string | number | undefined): void;
+        function clearInterval(intervalId: NodeJS.Timer | string | number | undefined): void;
         function setImmediate<TArgs extends any[]>(callback: (...args: TArgs) => void, ...args: TArgs): NodeJS.Immediate;
         // util.promisify no rest args compability
         // tslint:disable-next-line void-return
