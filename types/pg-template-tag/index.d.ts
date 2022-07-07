47e0041cd42a40a1f6be0cf870f246dbe53bb12a
Johan Levin <johan.levin@picadeli.se>
2022-07-07 16:30:35 +0200

    Fix CI errors (and editorial changes)

--- types/pg-template-tag/index.d.ts
+++ types/pg-template-tag/index.d.ts
@@ -1,9 +1,12 @@
-// Type definitions for pg-template-tag 0.1.2
+// Type definitions for pg-template-tag 0.1
 // Project: https://github.com/xecycle/pg-template-tag
 // Definitions by: Johan Levin <https://github.com/johan13>
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
 
-export type SqlLiteral = { text: string; values: unknown[] };
+export interface SqlLiteral {
+    text: string;
+    values: unknown[];
+}
 
 export default function SQLTag(parts: TemplateStringsArray, ...values: ReadonlyArray<unknown>): SqlLiteral;
 
--- types/pg-template-tag/pg-template-tag-tests.ts
+++ types/pg-template-tag/pg-template-tag-tests.ts
@@ -4,7 +4,7 @@ import SQL, { join, SqlLiteral, sqlLiteral } from 'pg-template-tag';
 // Basic parameter substitution:
 const query1: QueryConfig = SQL`SELECT * FROM foo WHERE bar=${42}`;
 
-// Use 'join' to build an IN-clause.
+// Use 'join' to build an IN-clause:
 const values = [
     { bar: 1, baz: 2 },
     { bar: 3, baz: 4 },
@@ -15,6 +15,6 @@ const inClause: SqlLiteral = join(
 );
 const query2: QueryConfig = SQL`SELECT * FROM foo WHERE (bar, baz) IN ${inClause}`;
 
-// Use 'sqlLiteral' to insert raw text that cannot be parameterized.
+// Use 'sqlLiteral' to insert raw text that cannot be parameterized:
 const tableName: SqlLiteral = Math.random() < 0.5 ? sqlLiteral('foo') : sqlLiteral('bar');
 const query3: QueryConfig = SQL`SELECT * FROM ${tableName}`;
