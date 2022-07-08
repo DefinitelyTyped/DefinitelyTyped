88bf475619828b0719e9dd941d16e7767298fc19
Gabriel Duarte <gabrielqduarte@gmail.com>
2022-07-08 21:53:34 +0200

    [semantic-release] add branch to Context

--- types/semantic-release/index.d.ts
+++ types/semantic-release/index.d.ts
@@ -209,6 +209,84 @@ declare namespace SemanticRelease {
         plugins: ReadonlyArray<PluginSpec>;
     }
 
+    type BranchObject = {
+        /**
+         * The name of git branch.
+         *
+         * A `name` is required for all types of branch. It can be defined as a
+         * [glob](https://github.com/micromatch/micromatch#matching-features)
+         * in which case the definition will be expanded to one per matching
+         * branch existing in the repository.
+         *
+         * If `name` doesn't match any branch existing in the repository, the
+         * definition will be ignored. For example, the default configuration
+         * includes the definition `next` and `next-major` which will become
+         * active only  when the branches `next` and/or `next-major` are
+         * created in the repository.
+         */
+        name: string;
+
+        /**
+         * The distribution channel on which to publish releases from this
+         * branch.
+         *
+         * If this field is set to `false`, then the branch will be released
+         * on the default distribution channel (for example the `@latest`
+         * [dist-tag](https://docs.npmjs.com/cli/dist-tag) for npm). This is
+         * also the default behavior for the first
+         * [release branch](https://semantic-release.gitbook.io/semantic-release/usage/workflow-configuration#release-branches)
+         * if the channel property is not set.
+         *
+         * For all other branches, if the channel property is not set, then the
+         * channel name will be the same as the branch name.
+         *
+         * The value of `channel`, if defined as a string, is generated with
+         * [Lodash template](https://lodash.com/docs#template) with the
+         * variable `name` set to the branch name.
+         *
+         * For example `{name: 'next', channel: 'channel-${name}'}` will be
+         * expanded to `{name: 'next', channel: 'channel-next'}`.
+         */
+        channel?: string | false | undefined;
+
+        /**
+         * The range of [semantic versions](https://semver.org/) to support on
+         * this branch.
+         *
+         * A `range` only applies to maintenance branches and must be formatted
+         * like `N.N.x` or `N.x` (`N` is a number). If no range is specified
+         * but the `name` is formatted as a range, then the branch will be
+         * considered a maintenance branch and the `name` value will be used
+         * for the `range`.
+         *
+         * Required for maintenance branches, unless `name` is formatted like
+         * `N.N.x` or `N.x` (`N` is a number).
+         */
+        range?: string | undefined;
+
+        /**
+         * The pre-release identifier to append to [semantic versions](https://semver.org/)
+         * released from this branch.
+         *
+         * A `prerelease` property applies only to pre-release branches and
+         * the `prerelease` value must be valid per the [Semantic Versioning
+         * Specification](https://semver.org/#spec-item-9). It will determine
+         * the name of versions. For example if `prerelease` is set to
+         * `"beta"`, the version will be formatted like `2.0.0-beta.1`,
+         * `2.0.0-beta.2`, etc.
+         *
+         * The value of `prerelease`, if defined as a string, is generated with
+         * [Lodash template](https://lodash.com/docs#template) with the
+         * variable `name` set to the name of the branch.
+         *
+         * If the `prerelease property is set to `true` then the name of the
+         * branch is used as the pre-release identifier.
+         *
+         * Required for pre-release branches.
+         */
+        prerelease?: string | boolean | undefined;
+    };
+
     /**
      * Specifies a git branch holding commits to analyze and code to release.
      *
@@ -216,86 +294,7 @@ declare namespace SemanticRelease {
      * a string is a shortcut for specifying that string as the `name` field,
      * for example `"master"` expands to `{name: "master"}`.
      */
-    type BranchSpec =
-        | string
-        | {
-              /**
-               * The name of git branch.
-               *
-               * A `name` is required for all types of branch. It can be defined as a
-               * [glob](https://github.com/micromatch/micromatch#matching-features)
-               * in which case the definition will be expanded to one per matching
-               * branch existing in the repository.
-               *
-               * If `name` doesn't match any branch existing in the repository, the
-               * definition will be ignored. For example, the default configuration
-               * includes the definition `next` and `next-major` which will become
-               * active only  when the branches `next` and/or `next-major` are
-               * created in the repository.
-               */
-              name: string;
-
-              /**
-               * The distribution channel on which to publish releases from this
-               * branch.
-               *
-               * If this field is set to `false`, then the branch will be released
-               * on the default distribution channel (for example the `@latest`
-               * [dist-tag](https://docs.npmjs.com/cli/dist-tag) for npm). This is
-               * also the default behavior for the first
-               * [release branch](https://semantic-release.gitbook.io/semantic-release/usage/workflow-configuration#release-branches)
-               * if the channel property is not set.
-               *
-               * For all other branches, if the channel property is not set, then the
-               * channel name will be the same as the branch name.
-               *
-               * The value of `channel`, if defined as a string, is generated with
-               * [Lodash template](https://lodash.com/docs#template) with the
-               * variable `name` set to the branch name.
-               *
-               * For example `{name: 'next', channel: 'channel-${name}'}` will be
-               * expanded to `{name: 'next', channel: 'channel-next'}`.
-               */
-              channel?: string | false | undefined;
-
-              /**
-               * The range of [semantic versions](https://semver.org/) to support on
-               * this branch.
-               *
-               * A `range` only applies to maintenance branches and must be formatted
-               * like `N.N.x` or `N.x` (`N` is a number). If no range is specified
-               * but the `name` is formatted as a range, then the branch will be
-               * considered a maintenance branch and the `name` value will be used
-               * for the `range`.
-               *
-               * Required for maintenance branches, unless `name` is formatted like
-               * `N.N.x` or `N.x` (`N` is a number).
-               */
-              range?: string | undefined;
-
-              /**
-               * The pre-release identifier to append to [semantic versions](https://semver.org/)
-               * released from this branch.
-               *
-               * A `prerelease` property applies only to pre-release branches and
-               * the `prerelease` value must be valid per the [Semantic Versioning
-               * Specification](https://semver.org/#spec-item-9). It will determine
-               * the name of versions. For example if `prerelease` is set to
-               * `"beta"`, the version will be formatted like `2.0.0-beta.1`,
-               * `2.0.0-beta.2`, etc.
-               *
-               * The value of `prerelease`, if defined as a string, is generated with
-               * [Lodash template](https://lodash.com/docs#template) with the
-               * variable `name` set to the name of the branch.
-               *
-               * If the `prerelease property is set to `true` then the name of the
-               * branch is used as the pre-release identifier.
-               *
-               * Required for pre-release branches.
-               */
-              prerelease?: string | boolean | undefined;
-          };
-
+    type BranchSpec = string | BranchObject;
     /**
      * Specifies a plugin to use.
      *
@@ -439,6 +438,11 @@ declare namespace SemanticRelease {
          * Commits to analyze.
          */
         commits?: Commit[];
+
+        /**
+         * Current branch being published.
+         */
+        branch: BranchObject;
     }
 
     interface Commit {
