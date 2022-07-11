007b800664c63bc2088fa1c8a55989fc9bb63c02
golovkoe <golovkoe@ae.com>
2022-07-11 14:46:52 +0200

    Minor fix for ember-modal-service types

--- types/ember-modal-service/models/modal.d.ts
+++ types/ember-modal-service/models/modal.d.ts
@@ -1,17 +1,9 @@
-import type { defer, resolve, reject, Promise } from 'rsvp';
+import type { defer, Promise } from 'rsvp';
 
-// using any since complicated interfaces fail the type check with error "is not assignable to type 'never'"
-export default class ModalModel<T = Record<string, any>> {
+export default class ModalModel<T = Record<string, never>> {
     declare name: string;
     declare fullname: string;
     declare options: T;
-    declare isPending: boolean;
-	declare isSettled: boolean;
-	declare isFulfilled: boolean;
-	declare isRejected: boolean;
-    declare resolve: typeof resolve;
-    declare reject: typeof reject;
     declare promise: Promise<unknown>;
-    declare setProperties: (options: Record<string, any>) => void;
-    declare _deferred: ReturnType<typeof defer>;
+    declare deferred: ReturnType<typeof defer>;
 }
