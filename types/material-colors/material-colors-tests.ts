b060c6e4f352a86cbb162e24c3e6bf6a83632615
Noah Overcash <ncovercash@ua.edu>
2022-07-11 19:54:44 -0500

    Use better dot notation for tests

--- types/material-colors/material-colors-tests.ts
+++ types/material-colors/material-colors-tests.ts
@@ -3,5 +3,5 @@ import * as colors from 'material-colors';
 const accentTest: string = colors.red.a700;
 const noAccentTest: string = colors.brown['700'];
 const blackTest: string = colors.black;
-const textTest: string = colors['dark-text']['primary'];
-const iconsTest: string = colors['dark-icons']['active'];
+const textTest: string = colors.darkText.primary;
+const iconsTest: string = colors.darkIcons.active;
