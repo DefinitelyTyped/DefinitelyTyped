17e8793203e7f5ef3d7a330ef461fdb5c444b30a
Alexander Rudolph <alexr30@web.de>
2022-07-15 21:50:34 +0200

    fix rule violations

--- types/node-imap/node-imap-tests.ts
+++ types/node-imap/node-imap-tests.ts
@@ -6,7 +6,7 @@ import IMAP = require('imap');
 import util = require('util');
 import inspect = util.inspect;
 
-var imap = new IMAP({
+const imap = new IMAP({
     user: 'mygmailname@gmail.com',
     password: 'mygmailpassword',
     host: 'imap.gmail.com',
@@ -18,47 +18,47 @@ function openInbox(cb: (error: Error, box: IMAP.Box) => void) {
     imap.openBox('INBOX', true, cb);
 }
 
-imap.once('ready', function () {
-    openInbox(function (err, box) {
+imap.once('ready', () => {
+    openInbox((err, box) => {
         if (err) throw err;
-        var f = imap.seq.fetch('1:3', {
+        const f = imap.seq.fetch('1:3', {
             bodies: 'HEADER.FIELDS (FROM TO SUBJECT DATE)',
             struct: true,
         });
-        f.on('message', function (msg: IMAP.ImapMessage, seqno: number) {
+        f.on('message', (msg: IMAP.ImapMessage, seqno: number) => {
             console.log('Message #%d', seqno);
-            var prefix = '(#' + seqno + ') ';
-            msg.on('body', function (stream: NodeJS.ReadableStream, info: Object) {
-                var buffer = '';
-                stream.on('data', function (chunk: Buffer) {
+            const prefix = `(#${seqno}) `;
+            msg.on('body', (stream: NodeJS.ReadableStream, info: object) => {
+                let buffer = '';
+                stream.on('data', (chunk: Buffer) => {
                     buffer += chunk.toString('utf8');
                 });
-                stream.once('end', function () {
+                stream.once('end', () => {
                     console.log(prefix + 'Parsed header: %s', inspect(IMAP.parseHeader(buffer)));
                 });
             });
-            msg.once('attributes', function (attrs: Object) {
+            msg.once('attributes', (attrs: object) => {
                 console.log(prefix + 'Attributes: %s', inspect(attrs, false, 8));
             });
-            msg.once('end', function () {
+            msg.once('end', () => {
                 console.log(prefix + 'Finished');
             });
         });
-        f.once('error', function (err: Error) {
+        f.once('error', (err: Error) => {
             console.log('Fetch error: ' + err);
         });
-        f.once('end', function () {
+        f.once('end', () => {
             console.log('Done fetching all messages!');
             imap.end();
         });
     });
 });
 
-imap.once('error', function (err: Error) {
+imap.once('error', (err: Error) => {
     console.log(err);
 });
 
-imap.once('end', function () {
+imap.once('end', () => {
     console.log('Connection ended');
 });
 
@@ -66,39 +66,39 @@ imap.connect();
 
 // using the functions and variables already defined in the first example ...
 
-openInbox(function (err: Error, box: IMAP.Box) {
+openInbox((err: Error, box: IMAP.Box) => {
     if (err) throw err;
-    var f = imap.seq.fetch(box.messages.total + ':*', { bodies: ['HEADER.FIELDS (FROM)', 'TEXT'] });
-    f.on('message', function (msg: IMAP.ImapMessage, seqno: number) {
+    const f = imap.seq.fetch(box.messages.total + ':*', { bodies: ['HEADER.FIELDS (FROM)', 'TEXT'] });
+    f.on('message', (msg: IMAP.ImapMessage, seqno: number) => {
         console.log('Message #%d', seqno);
-        var prefix = '(#' + seqno + ') ';
-        msg.on('body', function (stream: NodeJS.ReadableStream, info: any) {
+        const prefix = `(#${seqno}) `;
+        msg.on('body', (stream: NodeJS.ReadableStream, info: any) => {
             if (info.which === 'TEXT')
                 console.log(prefix + 'Body [%s] found, %d total bytes', inspect(info.which), info.size);
-            var buffer = '',
-                count = 0;
-            stream.on('data', function (chunk: Buffer) {
+            let buffer = '';
+            let count = 0;
+            stream.on('data', (chunk: Buffer) => {
                 count += chunk.length;
                 buffer += chunk.toString('utf8');
                 if (info.which === 'TEXT')
                     console.log(prefix + 'Body [%s] (%d/%d)', inspect(info.which), count, info.size);
             });
-            stream.once('end', function () {
+            stream.once('end', () => {
                 if (info.which !== 'TEXT') console.log(prefix + 'Parsed header: %s', inspect(IMAP.parseHeader(buffer)));
                 else console.log(prefix + 'Body [%s] Finished', inspect(info.which));
             });
         });
-        msg.once('attributes', function (attrs: Object) {
+        msg.once('attributes', (attrs: object) => {
             console.log(prefix + 'Attributes: %s', inspect(attrs, false, 8));
         });
-        msg.once('end', function () {
+        msg.once('end', () => {
             console.log(prefix + 'Finished');
         });
     });
-    f.once('error', function (err: Error) {
+    f.once('error', (err: Error) => {
         console.log('Fetch error: ' + err);
     });
-    f.once('end', function () {
+    f.once('end', () => {
         console.log('Done fetching all messages!');
         imap.end();
     });
@@ -106,70 +106,70 @@ openInbox(function (err: Error, box: IMAP.Box) {
 
 // using the functions and variables already defined in the first example ...
 
-var fs = require('fs');
+import fs = require('fs');
 
-openInbox(function (err: Error, box: IMAP.Box) {
+openInbox((err: Error, box: IMAP.Box) => {
     if (err) throw err;
-    imap.search(['UNSEEN', ['SINCE', 'May 20, 2010']], function (err: Error, results: number[]) {
+    imap.search(['UNSEEN', ['SINCE', 'May 20, 2010']], (err: Error, results: number[]) => {
         if (err) throw err;
-        var f = imap.fetch(results, { bodies: '' });
-        f.on('message', function (msg: IMAP.ImapMessage, seqno: number) {
+        const f = imap.fetch(results, { bodies: '' });
+        f.on('message', (msg: IMAP.ImapMessage, seqno: number) => {
             console.log('Message #%d', seqno);
-            var prefix = '(#' + seqno + ') ';
-            msg.on('body', function (stream: NodeJS.ReadableStream, info: any) {
+            const prefix = `(#${seqno}) `;
+            msg.on('body', (stream: NodeJS.ReadableStream, info: any) => {
                 console.log(prefix + 'Body');
-                stream.pipe(fs.createWriteStream('msg-' + seqno + '-body.txt'));
+                stream.pipe(fs.createWriteStream(`msg-${seqno}-body.txt`));
             });
-            msg.once('attributes', function (attrs: Object) {
+            msg.once('attributes', (attrs: object) => {
                 console.log(prefix + 'Attributes: %s', inspect(attrs, false, 8));
             });
-            msg.once('end', function () {
+            msg.once('end', () => {
                 console.log(prefix + 'Finished');
             });
         });
-        f.once('error', function (err: Error) {
+        f.once('error', (err: Error) => {
             console.log('Fetch error: ' + err);
         });
-        f.once('end', function () {
+        f.once('end', () => {
             console.log('Done fetching all messages!');
             imap.end();
         });
     });
 });
 
-openInbox(function (err: Error, box: IMAP.Box) {
+openInbox((err: Error, box: IMAP.Box) => {
     if (err) throw err;
-    imap.sort(['-DATE'], ['UNSEEN', ['SINCE', 'May 20, 2020']], function (err, uids) {
+    imap.sort(['-DATE'], ['UNSEEN', ['SINCE', 'May 20, 2020']], (err, uids) => {
         if (err) throw err;
-        var f = imap.fetch(uids, { bodies: '' });
-        f.on('message', function (msg: IMAP.ImapMessage, seqno: number) {
+        const f = imap.fetch(uids, { bodies: '' });
+        f.on('message', (msg: IMAP.ImapMessage, seqno: number) => {
             console.log('Message #%d', seqno);
-            var prefix = '(#' + seqno + ') ';
-            msg.on('body', function (stream: NodeJS.ReadableStream, info: any) {
+            const prefix = `(#${seqno}) `;
+            msg.on('body', (stream: NodeJS.ReadableStream, info: any) => {
                 console.log(prefix + 'Body');
-                stream.pipe(fs.createWriteStream('msg-' + seqno + '-body.txt'));
+                stream.pipe(fs.createWriteStream(`msg-${seqno}-body.txt`));
             });
-            msg.once('attributes', function (attrs: Object) {
+            msg.once('attributes', (attrs: object) => {
                 console.log(prefix + 'Attributes: %s', inspect(attrs, false, 8));
             });
-            msg.once('end', function () {
+            msg.once('end', () => {
                 console.log(prefix + 'Finished');
             });
         });
-        f.once('error', function (err: Error) {
+        f.once('error', (err: Error) => {
             console.log('Fetch error: ' + err);
         });
-        f.once('end', function () {
+        f.once('end', () => {
             console.log('Done fetching all messages!');
             imap.end();
         });
     });
 });
 
-var rawHeader: string = '';
-var headers = IMAP.parseHeader(rawHeader);
+const rawHeader = '';
+let headers = IMAP.parseHeader(rawHeader);
 headers = IMAP.parseHeader(rawHeader, true);
 
-var f: IMAP.ImapFetch;
+let f: IMAP.ImapFetch;
 f = imap.fetch('1:3', { bodies: '' });
 f = imap.seq.fetch('1:3', { bodies: '' });
