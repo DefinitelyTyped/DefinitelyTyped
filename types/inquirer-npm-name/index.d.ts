bfa521a364dc00dca9ce942102a98e3c0f6bb4a9
Manuel Thalmann <m@nuth.ch>
2022-07-15 02:03:35 +0200

    Remove unnecessary type mapping for `inquirer-npm-name`

--- types/inquirer-npm-name/index.d.ts
+++ types/inquirer-npm-name/index.d.ts
@@ -4,8 +4,8 @@
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
 // TypeScript Version: 4.2
 
-import inquirer = require("inquirer");
+import inquirer, { Question } from "inquirer";
 
 type Inquirer = typeof inquirer;
-declare function askName(name: string | inquirer.Question, inquirer: Inquirer): Promise<{ [key: string]: string }>;
+declare function askName(name: string | Question, inquirer: Inquirer): Promise<{ [key: string]: string }>;
 export = askName;
