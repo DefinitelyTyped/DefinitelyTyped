156363e8127800106d7c5c41d08e3bff69047720
Mor Kadosh <mkadosh@paloaltonetworks.com>
2022-07-16 00:29:47 +0300

    fix(@styled-system/props): fixed dtslint tests

--- types/styled-system__props/index.d.ts
+++ types/styled-system__props/index.d.ts
@@ -1,11 +1,13 @@
-// Type definitions for @styled-system/props 5.1.2
+// Type definitions for @styled-system/props 5.1
 // Project: https://github.com/styled-system/styled-system
 // Definitions by: Kadoshms <https://github.com/kadoshms>
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
 
-type PropsMap = {
+export {};
+
+interface PropsMap {
     [key: string]: unknown;
-};
+}
 
 export function omit(props: PropsMap): PropsMap;
 
