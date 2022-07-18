cf26f90ef97a65f801b7e4e725ecc4f88dd5695d
Adhemar <adhemar.sgv@gmail.com>
2022-07-18 10:25:36 -0400

    Update datatables.net-tests.ts
    Updating function colCreatedCellFunc signature

--- types/datatables.net/datatables.net-tests.ts
+++ types/datatables.net/datatables.net-tests.ts
@@ -35,7 +35,7 @@ const lang: DataTables.LanguageSettings = {
 
 //#region "Column"
 
-const colCreatedCellFunc: DataTables.FunctionColumnCreatedCell = (cell, cellData, rowData, rowIndex, colIndex) => {};
+const colCreatedCellFunc: DataTables.FunctionColumnCreatedCell = (cell, cellData, rowData, rowIndex, colIndex, cells) => {};
 
 const colDataObject: DataTables.ObjectColumnData = {
     _: "phone",
