d6ef6d3aa02749b516eed28684080e12f4ba9d4f
Soldy <4786022+Soldy@users.noreply.github.com>
2022-07-19 12:38:33 +0100

    level runner version update
    Signed-off-by: Soldy <4786022+Soldy@users.noreply.github.com>

--- types/levelrunnerrc/index.d.ts
+++ types/levelrunnerrc/index.d.ts
@@ -2,7 +2,7 @@
 // Project: https://github.com/Soldy/levelrunnerrc
 // Definitions by: Soldy <https://github.com/Soldy>
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
-export class levelRunnerBase {
+export class LevelRunner {
     constructor(before_in_: () => void, after_in_: () => void, level_in_: number);
     add(fun: () => void, level: number, name: string): void;
     run(): void;
