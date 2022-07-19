acfbef341e1cb61759b099ce84e9a9974888b46c
Soldy <4786022+Soldy@users.noreply.github.com>
2022-07-19 10:48:27 +0100

    levelrunnerc defination
    Signed-off-by: Soldy <4786022+Soldy@users.noreply.github.com>

--- types/levelrunnerrc/index.d.ts
+++ types/levelrunnerrc/index.d.ts
@@ -1,3 +1,7 @@
+// Type definitions for levelrunnerrc 0.99.3
+// Project: https://github.com/Soldy/levelrunnerrc
+// Definitions by: Soldy <https://github.com/Soldy>
+// Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
 export class levelRunnerBase {
     constructor(before_in_: () => void, after_in_: () => void, level_in_: number);
     add(fun: () => void, level: number, name: string): void;
