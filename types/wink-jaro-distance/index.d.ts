36345560f5b054666b15df56811bbf56a1082e3b
Wazbat <wazbat97@gmail.com>
2022-07-21 22:52:47 +0000

    🚨 Updated type definitions to comply with linter

--- types/wink-jaro-distance/index.d.ts
+++ types/wink-jaro-distance/index.d.ts
@@ -1,9 +1,10 @@
-// Type definitions for wink-jaro-distance 2.0.0
+// Type definitions for wink-jaro-distance 2.0
 // Project: https://github.com/winkjs/wink-jaro-distance (Does not have to be to GitHub, but prefer linking to a source code repository rather than to a project website.)
 // Definitions by: Warren Edwards <https://github.com/Wazbat>
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
 
-export default function jaro(a: string, b: string): {
+declare function jaro(a: string, b: string): {
     similarity: number;
     distance: number;
 };
+export = jaro;
