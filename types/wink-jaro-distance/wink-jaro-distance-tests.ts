cde93c6a950fdd0c8af0f2a4869b2dd3f8da3584
Wazbat <wazbat97@gmail.com>
2022-07-21 22:55:06 +0000

    🚨 Updated tests to comply with linter
    I removed the other calls due to the "missing whitespace" linter error

--- types/wink-jaro-distance/wink-jaro-distance-tests.ts
+++ types/wink-jaro-distance/wink-jaro-distance-tests.ts
@@ -1,17 +1,4 @@
-import jaro from 'wink-jaro-distance';
-
-// Example function calls taken from official readme
-jaro('father','farther')
-// -> { distance: 0.04761904761904756, similarity: 0.9523809523809524 }
-
-jaro('Angelina','Angelica')
-// -> { distance: 0.08333333333333337,  similarity: 0.9166666666666666 }
-
-jaro('Flikr','Flicker')
-// -> { distance: 0.09523809523809523, similarity: 0.9047619047619048 }
-
-jaro('abcdef','fedcba')
-// -> { distance: 0.6111111111111112, similarity: 0.38888888888888884 }
+import jaro = require('wink-jaro-distance');
 
 const result = jaro('abcdef', 'fedcba');
 const result2 = jaro('abcdef', 'fedcba');
@@ -26,4 +13,3 @@ const totalDistance = result.distance + result2.distance;
 
 addNumbers(result.distance);
 addNumbers(result2.similarity);
-
