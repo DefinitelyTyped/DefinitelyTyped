1c2c7dc52518ea91218c190c84611fabb03e953a
Wazbat <wazbat97@gmail.com>
2022-07-22 11:49:39 +0000

    ✅ Simplified tests as per code review

--- types/wink-jaro-distance/wink-jaro-distance-tests.ts
+++ types/wink-jaro-distance/wink-jaro-distance-tests.ts
@@ -1,15 +1,5 @@
 import jaro = require('wink-jaro-distance');
 
 const result = jaro('abcdef', 'fedcba');
-const result2 = jaro('abcdef', 'fedcba');
-
-// Just need to make sure the types are a number I suppose?
-function addNumbers(value: number) {
-    return value + 1;
-}
-
-const mostSimilar = result.similarity > result2.similarity ? result : result2;
-const totalDistance = result.distance + result2.distance;
-
-addNumbers(result.distance);
-addNumbers(result2.similarity);
+result.distance // $ExpectType number
+result.similarity // $ExpectType number
