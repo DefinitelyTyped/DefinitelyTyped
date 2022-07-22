c788e80af93049def780d9a52d13f308de170545
Wazbat <wazbat97@gmail.com>
2022-07-22 11:54:15 +0000

    🚨 Added missing semicolons

--- types/wink-jaro-distance/wink-jaro-distance-tests.ts
+++ types/wink-jaro-distance/wink-jaro-distance-tests.ts
@@ -1,5 +1,5 @@
 import jaro = require('wink-jaro-distance');
 
 const result = jaro('abcdef', 'fedcba');
-result.distance // $ExpectType number
-result.similarity // $ExpectType number
+result.distance; // $ExpectType number
+result.similarity; // $ExpectType number
