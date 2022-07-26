a9cfa8b4647f57471a1cac039ba342f9c0b71450
AHdark <ahdark@outlook.com>
2022-07-26 01:01:46 +0000

    [@chenyfan/cache-db] Add definition

---
+++ types/chenyfan__cache-db/index.d.ts
@@ -0,0 +1,34 @@
+// Type definitions for @chenyfan/cache-db 0.0
+// Project: https://www.npmjs.com/package/@chenyfan/cache-db
+// Definitions by: AHdark <https://github.com/AH-dark>
+// Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
+
+declare namespace CacheDB {
+    export type Config = "json" | "arrayBuffer" | "blob" | "text" | string;
+    export type ReadMethodData = any | ArrayBuffer | Blob | string | null;
+    export type WriteValue =
+        | ReadableStream
+        | Blob
+        | ArrayBufferView
+        | ArrayBuffer
+        | FormData
+        | URLSearchParams
+        | string;
+}
+
+declare class CacheDB {
+    constructor (namespace?: string, prefix?: string);
+
+    namespace: string;
+    prefix: string;
+
+    read(key: string, config?: CacheDB.Config): Promise<CacheDB.ReadMethodData>;
+    write(
+        key: string | number | boolean,
+        value: CacheDB.WriteValue,
+        config?: CacheDB.Config
+    ): Promise<boolean>;
+    delete(key: string): Promise<boolean>;
+}
+
+export default CacheDB;
