286917274eb4137a9d93201667705b6f1315358b
Gintautas Miselis <gintautas@miselis.lt>
2022-07-28 22:46:29 +0300

    [chai] Removed Naktibalda from definition owners

--- types/chai/index.d.ts
+++ types/chai/index.d.ts
@@ -6,7 +6,6 @@
 //                 Olivier Chevet <https://github.com/olivr70>,
 //                 Matt Wistrand <https://github.com/mwistrand>,
 //                 Shaun Luttin <https://github.com/shaunluttin>,
-//                 Gintautas Miselis <https://github.com/Naktibalda>,
 //                 Satana Charuwichitratana <https://github.com/micksatana>,
 //                 Erik Schierboom <https://github.com/ErikSchierboom>,
 //                 Bogdan Paranytsia <https://github.com/bparan>,
