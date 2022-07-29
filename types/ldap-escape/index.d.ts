9512b881250f35af4df868b6dd1e3808a44721aa
Simone Corsi <simonecorsi.dev@gmail.com>
2022-07-29 14:00:05 +0200

    fix(ldap-escape): removes unused namespace

--- types/ldap-escape/index.d.ts
+++ types/ldap-escape/index.d.ts
@@ -5,5 +5,3 @@
 
 export function filter(TemplateStringsArray: TemplateStringsArray, ...str: string[] | number[]): string;
 export function dn(TemplateStringsArray: TemplateStringsArray, ...str: string[] | number[]): string;
-
-export as namespace LdapEscape;
--- types/ldap-escape/ldap-escape-tests.ts
+++ types/ldap-escape/ldap-escape-tests.ts
@@ -1,16 +1,9 @@
-/* tslint:disable:no-namespace */
-'use strict';
-
 import { dn, filter } from 'ldap-escape';
 
-namespace Module {
-    // filter(TemplateStringsArray: TemplateStringsArray, ...str: string[] | number[]): string;
-    // dn(TemplateStringsArray: TemplateStringsArray, ...str: string[] | number[]): string;
+declare const tpl: TemplateStringsArray;
 
-    declare const tpl: TemplateStringsArray;
-    // $ExpectType string
-    dn(tpl);
+// $ExpectType string
+dn(tpl);
 
-    // $ExpectType string
-    filter(tpl);
-}
+// $ExpectType string
+filter(tpl);
