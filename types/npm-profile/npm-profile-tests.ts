d9ac53b2dd9346ad609bdbe97fd6ac84b7bdc0f1
Steven Hangger <LonghronShen@Hotmail.com>
2022-08-01 06:25:57 +0000

    feat(npm-profile): add tests for added functions.

--- types/npm-profile/npm-profile-tests.ts
+++ types/npm-profile/npm-profile-tests.ts
@@ -1,12 +1,32 @@
 import profile = require('npm-profile');
 
 const token = '9849cae7-8f02-430f-b0fe-162980afe765';
+const username = 'fake-user';
 const password = 'terrible-password';
+const email = 'fake@email.com';
+const registry = '';
 const readonly = true;
 const cidr_whitelist: string[] = [];
 
+async function getOTPFromSomewhere(): Promise<string> {
+    return 'fake-otp';
+}
+
 (async () => {
     try {
+        // $ExpectType ProfileAuthToken
+        const loginCouchResult = await profile.loginCouch(username, email, password, { registry });
+
+        // $ExpectType ProfileAuthToken
+        const addUserCouchResult = await profile.adduserCouch(username, email, password, { registry });
+
+        // $ExpectType ProfileAuthToken
+        const addUserResult = await profile.adduser(async (url) => { },
+            async (creds: profile.ProfileAuthCredentials) => { return creds; }, { registry });
+
+        // $ExpectType ProfileAuthToken
+        const addUserWebResult = await profile.adduserWeb(async (url) => { }, { registry });
+
         // $ExpectType ProfileData
         const result = await profile.get({ token });
 
