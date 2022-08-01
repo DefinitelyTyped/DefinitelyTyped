88ed46a9c10a5c0446ace27b4f9b889712d14f55
Aleksey Pan <agpan@avito.ru>
2022-08-01 09:55:16 +0300

    up vesion

--- types/webpack-env/index.d.ts
+++ types/webpack-env/index.d.ts
@@ -1,4 +1,4 @@
-// Type definitions for webpack (module API) 1.17.1
+// Type definitions for webpack (module API) 1.18
 // Project: https://github.com/webpack/webpack
 // Definitions by: use-strict <https://github.com/use-strict>
 //                 rhonsby <https://github.com/rhonsby>
