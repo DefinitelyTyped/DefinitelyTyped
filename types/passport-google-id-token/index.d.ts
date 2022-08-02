942ba9ba82601d8a9289bec253fc2a8bad81a82d
Duy Nguyen <nguyenduy010798@gmail.com>
2022-08-02 11:45:29 +0300

    misc update

--- types/passport-google-id-token/index.d.ts
+++ types/passport-google-id-token/index.d.ts
@@ -3,31 +3,42 @@
 // Definitions by: Duy Nguyen <https://github.com/me>
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
 
-declare module 'passport-google-id-token' {
-    class GoogleTokenStrategy {
-        constructor(opt: StrategyOptions, verify: VerifyCallback);
-    }
+import { Strategy as PassportStrategy } from 'passport';
 
-    export interface StrategyOptions {
-        /**
-         * Google client id
-         */
-        clientID: string;
-        /**
-         * Return the Google certificate that will be used for signature validation.
-         *
-         * A custom function can be used instead when passed as an option in the Strategy
-         * constructor. It can be interesting e.g. if caching is needed.
-         *
-         * @param {String} kid The key id specified in the token
-         * @param {Function} callback
-         */
-        getGoogleCerts: (kid: string, callback: (err: any, cert: string) => void) => void;
-    }
-
-    interface ParsedToken {}
-
-    export interface VerifyCallback {
-        (parsedToken: ParsedToken, googleId: string, done: (err: any, data: any) => void): void;
-    }
+declare class GoogleTokenStrategy extends PassportStrategy {
+    constructor(opt: StrategyOptions, verify: VerifyCallback);
 }
+
+export interface StrategyOptions {
+    /**
+     * Google client id
+     */
+    clientID: string;
+    /**
+     * Return the Google certificate that will be used for signature validation.
+     *
+     * A custom function can be used instead when passed as an option in the Strategy
+     * constructor. It can be interesting e.g. if caching is needed.
+     *
+     * @param {String} kid The key id specified in the token
+     * @param {Function} callback
+     */
+    getGoogleCerts?: (kid: string, callback: (err: any, cert: string) => void) => void;
+}
+
+export interface ParsedToken {
+    payload: {
+        family_name: string;
+        given_name: string;
+    };
+}
+
+export interface VerifyCallback {
+    (parsedToken: ParsedToken, googleId: string, done: VerifiedCallback): void;
+}
+
+export interface VerifiedCallback {
+    (error: any, user?: any, info?: any): void;
+}
+
+export default GoogleTokenStrategy;
--- types/passport-google-id-token/passport-google-id-token-tests.ts
+++ types/passport-google-id-token/passport-google-id-token-tests.ts
@@ -1,2 +1,16 @@
-import { GoogleTokenStrategy } from 'passport-google-id-token';
+import GoogleTokenStrategy, { ParsedToken, StrategyOptions, VerifiedCallback } from 'passport-google-id-token';
 import * as passport from 'passport';
+
+const opts: StrategyOptions = {
+    clientID: 'sdjasldajsd',
+};
+
+passport.use(
+    new GoogleTokenStrategy(opts, (parsedToken: ParsedToken, googleId: string, done: VerifiedCallback) => {
+        const user = {
+            firstName: parsedToken.payload.given_name,
+            lastName: parsedToken.payload.family_name,
+        };
+        done(null, user);
+    }),
+);
