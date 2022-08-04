59dd5412d37020cee77e41552d11182b191b5e09
Krunal Goswami <dev.tech21@gmail.com>
2022-08-04 17:09:36 +0530

    added missing types

--- types/graphql-validation-complexity/index.d.ts
+++ types/graphql-validation-complexity/index.d.ts
@@ -21,3 +21,39 @@ export function createComplexityLimitRule(
     maxCost: number,
     options?: Options,
 ): (ctx: ValidationContext) => any;
+
+export function complexityLimitExceededErrorMessage(): string;
+
+export default class ComplexityVisitor {
+    constructor(context: any, { scalarCost, objectCost, listFactor, introspectionListFactor, }: {
+        scalarCost?: number;
+        objectCost?: number;
+        listFactor?: number;
+        introspectionListFactor?: number;
+    });
+    context: any;
+    scalarCost: number;
+    objectCost: number;
+    listFactor: number;
+    introspectionListFactor: number;
+    costFactor: number;
+    cost: number;
+    Field: {
+        enter: () => void;
+        leave: () => void;
+    };
+    FragmentDefinition: () => boolean;
+    SelectionSet: (selectionSet: any) => any;
+    flattenFragmentSpreads(selectionSet: any): any;
+    enterField(): void;
+    leaveField(): void;
+    getFieldCostFactor(): any;
+    getTypeCostFactor(type: any): any;
+    isIntrospectionList({ ofType }: {
+        ofType: any;
+    }): boolean;
+    getFieldCost(): any;
+    getTypeCost(type: any): any;
+    getDirectiveValue(directiveName: any): number;
+    getCost(): number;
+}
