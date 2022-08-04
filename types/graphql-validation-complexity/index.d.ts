8b84d360aae2d3b033908a70292b4f7c1d92d0d0
Krunal Goswami <dev.tech21@gmail.com>
2022-08-04 15:24:21 +0530

    added types for graphql-validation-complexity

---
+++ types/graphql-validation-complexity/index.d.ts
@@ -0,0 +1,22 @@
+// Type definitions for graphql-validation-complexity 0.4
+// Project: https://github.com/4Catalyzer/graphql-validation-complexity (Does not have to be to GitHub, but prefer linking to a source code repository rather than to a project website.)
+// Definitions by: Krunal G <https://github.com/krunalg>
+// Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
+
+import type { ASTNode, GraphQLError, ValidationContext } from 'graphql';
+
+export interface Options {
+    onCost?: (cost: number, context: ValidationContext) => void;
+    createError?: (cost: number, node: ASTNode) => GraphQLError;
+    formatErrorMessage?: (cost: number) => string;
+
+    scalarCost?: number;
+    objectCost?: number;
+    listFactor?: number;
+    introspectionListFactor?: number;
+}
+
+export function createComplexityLimitRule(
+    maxCost: number,
+    options?: Options,
+): (ctx: ValidationContext) => any;
