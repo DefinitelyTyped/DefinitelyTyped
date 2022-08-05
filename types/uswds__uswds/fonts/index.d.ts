dc9d4ce99ae932970b36d0eee562185b12693cb0
Spencer, Morgan [USA] <morgan@spencercreative.co>
2022-08-04 20:18:08 -0400

    Reorganize types to recognize new paths

---
+++ types/uswds__uswds/fonts/index.d.ts
@@ -0,0 +1,19 @@
+declare module '@uswds/uswds/fonts/merriweather/*' {
+    const value: any;
+    export = value;
+}
+
+declare module '@uswds/uswds/fonts/public-sans/*' {
+    const value: any;
+    export = value;
+}
+
+declare module '@uswds/uswds/fonts/roboto-mono/*' {
+    const value: any;
+    export = value;
+}
+
+declare module '@uswds/uswds/fonts/source-sans-pro/*' {
+    const value: any;
+    export = value;
+}
