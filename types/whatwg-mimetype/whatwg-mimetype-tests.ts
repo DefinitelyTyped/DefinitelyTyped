a20ad5c50fbbb37cd54ecba102a260ca9903a420
Dimitri B <dimitri.benin@gmail.com>
2022-08-09 22:32:38 +0200

    [whatwg-mimetype] Update to v3

--- types/whatwg-mimetype/whatwg-mimetype-tests.ts
+++ types/whatwg-mimetype/whatwg-mimetype-tests.ts
@@ -1,24 +1,41 @@
-/// <reference types="node" />
-import assert = require("assert");
 import MIMEType = require('whatwg-mimetype');
 
-const mt = MIMEType.parse("text/plain");
+// test type exports
+type MT = MIMEType;
+type MTP = MIMEType.MIMETypeParameters;
 
-assert(mt !== null);
-if (mt) {
-  assert(mt.type === "text");
-  assert(mt.subtype === "plain");
-  assert(mt.essence === "text/plain");
-  assert(mt.parameters.size === 0);
-  assert(!mt.isXML());
-  assert(!mt.isHTML());
-  assert(!mt.isJavaScript());
+const mt = MIMEType.parse('text/plain'); // $ExpectType MIMEType | null
+
+if (mt != null) {
+    mt.type; // $ExpectType string
+    mt.type = 'text';
+    mt.subtype; // $ExpectType string
+    mt.subtype = 'plain';
+    mt.essence; // $ExpectType string
+    // @ts-expect-error
+    mt.essence = '';
+    mt.parameters; // $ExpectType MIMETypeParameters
+    // @ts-expect-error
+    mt.parameters = mt.parameters;
+    mt.isXML(); // $ExpectType boolean
+    mt.isHTML(); // $ExpectType boolean
+    mt.isJavaScript(); // $ExpectType boolean
+    mt.isJavaScript({ prohibitParameters: true }); // $ExpectType boolean
+
+    mt.parameters.size; // $ExpectType number
+    mt.parameters.get('key'); // $ExpectType string | undefined
+    mt.parameters.has('key'); // $ExpectType boolean
+    mt.parameters.set('key', 'value'); // $ExpectType MIMETypeParameters
+    mt.parameters.clear(); // $ExpectType void
+    mt.parameters.delete('key'); // $ExpectType boolean
+    // $ExpectType void
+    mt.parameters.forEach((value, key, map) => {
+        value; // $ExpectType string
+        key; // $ExpectType string
+        map; // $ExpectType Map<string, string>
+    }, null);
+    mt.parameters.keys(); // $ExpectType IterableIterator<string>
+    mt.parameters.values(); // $ExpectType IterableIterator<string>
+    mt.parameters.entries(); // $ExpectType IterableIterator<[string, string]>
+    mt.parameters[Symbol.iterator](); // $ExpectType IterableIterator<[string, string]>
 }
-
-const mt2 = new MIMEType("application/javascript; charset=utf8");
-
-assert(mt2.type === "text");
-assert(mt2.subtype === "plain");
-assert(mt2.essence === "text/plain");
-assert(mt2.parameters.get("charset") === "utf8");
-assert(mt2.isJavaScript({ allowParameters: true }));
