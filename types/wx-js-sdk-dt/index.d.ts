1621e6ed95dfe926d96acabc1c63ef1c1bec726b
Andyxu <xuanhai@custouch.com>
2022-08-11 14:44:12 +0800

    feat: update lib version

--- types/wx-js-sdk-dt/index.d.ts
+++ types/wx-js-sdk-dt/index.d.ts
@@ -1,4 +1,4 @@
-// Type definitions for non-npm package wx-js-sdk 1.4
+// Type definitions for non-npm package wx-js-sdk 1.6
 // Project: https://mp.weixin.qq.com/wiki?t=resource/res_main&id=mp1421141115
 // Definitions by: Bian Zhongjie <https://github.com/agasbzj>
 //                 Ming Huang <https://github.com/forzoom>
