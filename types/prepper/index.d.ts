83afbf64c19d2d6c241add02a026907ff9430653
Teun Mooij <tmooij@infinitaslearning.com>
2022-08-12 22:35:16 +0200

    fix: linting

--- types/prepper/index.d.ts
+++ types/prepper/index.d.ts
@@ -1,21 +1,21 @@
 import { EventEmitter } from 'events';
 
-type LogFn = {
+export interface LogFn {
   (message: string, error: Error, context?: any): void;
   (message: string, context?: any): void;
   (error: Error, context?: any): void;
   (context: any): void;
-};
+}
 
-type LogLevel = 'trace' | 'debug' | 'info' | 'warn' | 'error' | 'fatal';
+export type LogLevel = 'trace' | 'debug' | 'info' | 'warn' | 'error' | 'fatal';
 
-export type LoggerOptions = {
+export interface LoggerOptions {
   sequence?: handlers.Handler;
   handlers?: handlers.Handler[];
   message?: string;
   level?: LogLevel;
   maxListeners?: number;
-};
+}
 
 export interface Logger {
   trace: LogFn;
@@ -32,51 +32,50 @@ export class Logger extends EventEmitter implements Logger {
   constructor(options?: LoggerOptions);
 }
 
-declare namespace handlers {
+export namespace handlers {
   type PropertyName = string | number | symbol;
 
-  export type Event = {
+  type Event = {
     message?: string;
     level: LogLevel;
     error?: { message: string; level: string; error: any };
-  } & Record<string, any>
+  } & Record<string, any>;
 
-  export interface Handler extends EventEmitter {
+  interface Handler extends EventEmitter {
     /**
      * handles the event
-     * @param event 
      * @remarks handle must emit 'message' event, passing on the altered event
      */
     handle(event: Event): void;
   }
 
-  export class Merge extends EventEmitter implements Handler {
+  class Merge extends EventEmitter implements Handler {
     constructor(other: Record<string, any>, options?: { key?: PropertyName | PropertyName[]; invert?: boolean });
     handle(event: Event): void;
   }
 
-  export class KeyFilter extends EventEmitter implements Handler {
+  class KeyFilter extends EventEmitter implements Handler {
     constructor(other: { include?: string[]; exclude?: string[] });
     handle(event: Event): void;
   }
 
-  export class Oversized extends EventEmitter implements Handler {
+  class Oversized extends EventEmitter implements Handler {
     constructor(options: { size: number });
     handle(event: Event): void;
   }
 
-  export class Sequence extends EventEmitter implements Handler {
+  class Sequence extends EventEmitter implements Handler {
     constructor(handlers?: Handler[]);
     handle(event: Event): void;
   }
 
-  export class Env extends EventEmitter implements Handler { handle(event: Event): void; }
-  export class Noop extends EventEmitter implements Handler { handle(event: Event): void; }
-  export class Repo extends EventEmitter implements Handler { handle(event: Event): void; }
-  export class Tracer extends EventEmitter implements Handler { handle(event: Event): void; }
-  export class Process extends EventEmitter implements Handler { handle(event: Event): void; }
-  export class System extends EventEmitter implements Handler { handle(event: Event): void; }
-  export class Timestamp extends EventEmitter implements Handler { handle(event: Event): void; }
-  export class Flatten extends EventEmitter implements Handler { handle(event: Event): void; }
-  export class Unflatten extends EventEmitter implements Handler { handle(event: Event): void; }
+  class Env extends EventEmitter implements Handler { handle(event: Event): void; }
+  class Noop extends EventEmitter implements Handler { handle(event: Event): void; }
+  class Repo extends EventEmitter implements Handler { handle(event: Event): void; }
+  class Tracer extends EventEmitter implements Handler { handle(event: Event): void; }
+  class Process extends EventEmitter implements Handler { handle(event: Event): void; }
+  class System extends EventEmitter implements Handler { handle(event: Event): void; }
+  class Timestamp extends EventEmitter implements Handler { handle(event: Event): void; }
+  class Flatten extends EventEmitter implements Handler { handle(event: Event): void; }
+  class Unflatten extends EventEmitter implements Handler { handle(event: Event): void; }
 }
