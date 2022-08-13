93de5da4965be4ba172297bae0377b89809ca1f1
Mattias Martens <mattias.martens@ioka.tech>
2022-08-13 11:50:30 -0700

    Add reattachResizeListener

--- types/react-size-reporter/index.d.ts
+++ types/react-size-reporter/index.d.ts
@@ -2,11 +2,20 @@
 // Project: https://github.com/berrtech/react-size-reporter
 // Definitions by: Mattias Martens <https://github.com/MattiasMartens>
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
-import * as React from 'react';
+import { FC, ReactNode, Ref } from 'react';
 
-declare const ReactSizeReporter: React.FC<{
-    children: React.ReactNode;
+export interface ReactSizeReporterRef {
+    // Use this if for any reason onSizeChange doesn't trigger anymore.
+    reattachResizeListener: () => void;
+}
+
+declare const ReactSizeReporter: FC<{
+    // Children with static or dynamic height or width
+    children: ReactNode;
+    ref?: Ref<ReactSizeReporterRef>;
+    // Callback called on mount and size changes.
     onSizeChange: (newDimensions: { height: number; width: number }) => void;
 }>;
 
+export type ReactSizeReporter = typeof ReactSizeReporter;
 export default ReactSizeReporter;
--- types/react-size-reporter/react-size-reporter-tests.tsx
+++ types/react-size-reporter/react-size-reporter-tests.tsx
@@ -1,5 +1,5 @@
 import * as React from 'react';
-import SizeReporter from 'react-size-reporter';
+import SizeReporter, { ReactSizeReporterRef } from 'react-size-reporter';
 
 const TestComponent = () => (
     <SizeReporter
@@ -13,3 +13,19 @@ const TestComponent = () => (
         <div style={{ width: '50vw', height: 'calc(100 + 7em)' }}></div>
     </SizeReporter>
 );
+
+class TestConsumer {
+    reattach = () => this.sizeReporter?.reattachResizeListener();
+    sizeReporter?: ReactSizeReporterRef;
+    render() {
+        return (
+            <SizeReporter onSizeChange={() => {}} ref={ref => (ref ? (this.sizeReporter = ref) : undefined)}>
+                <div>CONTENT GOES HERE</div>
+                <div>AND HERE</div>
+                <button type="button" onClick={this.reattach}>
+                    Reattach!
+                </button>
+            </SizeReporter>
+        );
+    }
+}
