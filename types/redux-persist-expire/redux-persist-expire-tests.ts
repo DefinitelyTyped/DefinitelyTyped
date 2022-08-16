edccc1376e60cf681d84c7c119175a465e508d61
bill <0529bilL@gmail.com>
2022-08-16 13:23:41 +0800

    fix: add package.json

--- types/redux-persist-expire/redux-persist-expire-tests.ts
+++ types/redux-persist-expire/redux-persist-expire-tests.ts
@@ -1,22 +1,8 @@
-import { persistReducer, persistStore } from 'redux-persist';
-
-import { createStore } from 'redux';
-
 import expireReducer = require('redux-persist-expire');
-const rootReducer = {};
-const persistedReducers = persistReducer(
-    {
-        transforms: [
-            expireReducer('preference', {
-                persistedAtKey: '__persisted_at',
-                expireSeconds: null,
-                expiredState: {},
-                autoExpire: false,
-            }),
-        ],
-    },
-    rootReducer,
-);
 
-export const store = createStore(persistedReducers);
-export const persist = persistStore(store);
+const reducers = expireReducer('preference', {
+    persistedAtKey: '__persisted_at',
+    expireSeconds: null,
+    expiredState: {},
+    autoExpire: false,
+});
