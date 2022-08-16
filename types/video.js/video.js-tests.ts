8b0d8432043ef6798215da871895797af0c26406
Aleksandr Seryakov <a.seryakov@nut.tech>
2022-08-16 10:23:19 +0300

    [video.js] Added playbackRates method

--- types/video.js/video.js-tests.ts
+++ types/video.js/video.js-tests.ts
@@ -161,6 +161,11 @@ videojs('example_video_1', playerOptions).ready(function playerReady() {
 
     const readyState: videojs.ReadyState = this.readyState();
 
+    this.playbackRates([1,1.5,2])
+
+    // $ExpectType number[]
+    const playbackRates: number[] = this.playbackRates()
+
     this.requestFullscreen();
 
     this.requestPictureInPicture().then(pipWindow => {
