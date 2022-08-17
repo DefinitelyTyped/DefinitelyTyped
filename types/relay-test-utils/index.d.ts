4c2779f7d5d6d1808e0f5ab437e06edc9adc52cd
Levi Buzolic <levibuzolic@gmail.com>
2022-08-18 07:50:41 +1000

    Add myself to the maintainer list of relay-compiler and relay-test-utils (#61766)

--- types/relay-test-utils/index.d.ts
+++ types/relay-test-utils/index.d.ts
@@ -2,6 +2,8 @@
 // Project: https://relay.dev
 // Definitions by: Renan Machado <https://github.com/renanmav>
 //                 Stephen Pittman <https://github.com/Stephen2>
+//                 Marais Rossouw <https://github.com/maraisr>
+//                 Levi Buzolic <https://github.com/levibuzolic>
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
 // TypeScript Version: 3.7
 
