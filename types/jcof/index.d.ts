ba69caca672bad667bb94e56e12f5712027dca74
MatyiFKBT <6183867+MatyiFKBT@users.noreply.github.com>
2022-08-18 07:04:35 +0000

    chore(jcof): add ParseError

--- types/jcof/index.d.ts
+++ types/jcof/index.d.ts
@@ -5,5 +5,7 @@
 
 export function parse(str: string): object;
 export function stringify(value: object): string;
-
+export class ParseError extends Error {
+    constructor(message: string);
+}
 export as namespace jcof;
