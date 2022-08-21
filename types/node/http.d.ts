f14d1b9eeee6e5e06ac16f4e573b8853dd077901
zhangyongsheng.dev <zhangyongsheng.dev@bytedance.com>
2022-08-21 16:55:30 +0800

    feat(node): add missing options to net.createServer

--- types/node/http.d.ts
+++ types/node/http.d.ts
@@ -155,6 +155,22 @@ declare module 'http' {
          * @default false
          */
         insecureHTTPParser?: boolean | undefined;
+        /**
+         * If set to `true`, it disables the use of Nagle's algorithm immediately after a new incoming connection is received.
+         * @default false
+         */
+        noDelay?: boolean | undefined;
+        /**
+         * If set to `true`, it enables keep-alive functionality on the socket immediately after a new incoming connection is received,
+         * similarly on what is done in `socket.setKeepAlive([enable][, initialDelay])`.
+         * @default false
+         */
+        keepAlive?: boolean | undefined;
+        /**
+         * If set to a positive number, it sets the initial delay before the first keepalive probe is sent on an idle socket.
+         * @default 0
+         */
+        keepAliveInitialDelay?: number | undefined;
     }
     type RequestListener = (req: IncomingMessage, res: ServerResponse) => void;
     /**
