ca90b4a9314c80fc8a99e831e135beddd8eb28aa
SteffoSpieler <steffo@steffospieler.de>
2022-08-23 21:38:12 +0200

    [chromecast-api] Add types

---
+++ types/chromecast-api/index.d.ts
@@ -0,0 +1,8 @@
+// Type definitions for chromecast-api 0.4
+// Project: https://github.com/alxhotel/chromecast-api
+// Definitions by: SteffoSpieler <https://github.com/SteffoSpieler>
+//                 daniel0611 <https://github.com/daniel0611>
+// Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
+
+import Client from "./lib/client";
+export = Client;
