2702149af193815bbf6ef4de6e3eca8a4e956b37
zhangyongsheng.dev <zhangyongsheng.dev@bytedance.com>
2022-08-23 10:44:12 +0800

    fix: add since tag

--- types/node/v16/http.d.ts
+++ types/node/v16/http.d.ts
@@ -158,17 +158,20 @@ declare module 'http' {
         /**
          * If set to `true`, it disables the use of Nagle's algorithm immediately after a new incoming connection is received.
          * @default false
+         * @since v16.5.0
          */
         noDelay?: boolean | undefined;
         /**
          * If set to `true`, it enables keep-alive functionality on the socket immediately after a new incoming connection is received,
          * similarly on what is done in `socket.setKeepAlive([enable][, initialDelay])`.
          * @default false
+         * @since v16.5.0
          */
         keepAlive?: boolean | undefined;
         /**
          * If set to a positive number, it sets the initial delay before the first keepalive probe is sent on an idle socket.
          * @default 0
+         * @since v16.5.0
          */
         keepAliveInitialDelay?: number | undefined;
     }
