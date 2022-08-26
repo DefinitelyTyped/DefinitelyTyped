78c1422d71664c2180e61a924af239fd3bde1b1a
tpluscode <tpluscode@users.noreply.github.com>
2022-08-26 12:07:13 +0200

    test: accurate spread

--- types/ngard__tiny-difference/ngard__tiny-difference-tests.ts
+++ types/ngard__tiny-difference/ngard__tiny-difference-tests.ts
@@ -3,5 +3,7 @@ import tinyDifference = require("@ngard/tiny-difference");
 const { difference } = tinyDifference;
 
 const diffNumbers: number[] = difference([1, 2, 3, 4], [1, 2]);
-const diffMixed: number[] = difference([1, 2, 3, 4], [1, 2]);
-const diffSpread: any[] = difference([1, 2, 3, 4], [1, "a"], [2, "b"]);
+const diffMixed: Array<number | string> = difference([1, 2, 3, 4], [1, "a"]);
+
+const arrays: any[][] = [];
+const diffSpread: any[] = difference([1, 2, 3, 4], ...arrays);
