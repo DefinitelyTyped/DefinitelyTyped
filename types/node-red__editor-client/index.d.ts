20aa87bc7dbbd60ea46f914edd6b263158d6aac1
Magdalena Lorentz <magda@m-lo.de>
2022-08-27 16:46:43 +0200

    @types/node-red: Added inputs to NodeProperties

--- types/node-red__editor-client/index.d.ts
+++ types/node-red__editor-client/index.d.ts
@@ -49,6 +49,7 @@ declare namespace editorClient {
      */
     interface NodeProperties {
         name?: string | undefined;
+        inputs?: 0 | 1 | undefined;
     }
 
     /** Reserved name for properties that MUST NOT BE USED. */
