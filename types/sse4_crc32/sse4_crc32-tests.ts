f97c652f313192a8f761617b5c7a76d64e8278c9
naeemy <catwinnerg@gmail.com>
2022-08-28 12:55:59 +0700

    add types to package sse4_crc32

---
+++ types/sse4_crc32/sse4_crc32-tests.ts
@@ -0,0 +1,14 @@
+import { createReadStream } from 'fs';
+import { fromStream, calculate, sse42_crc, table_crc } from 'sse4_crc32';
+
+// $ExpectType Crc32CStream
+fromStream(createReadStream('./test.txt'), 1);
+
+// $ExpectType number
+calculate('123', 1);
+
+// $ExpectType number
+sse42_crc('123', 1);
+
+// $ExpectType number
+table_crc('123', 1);
