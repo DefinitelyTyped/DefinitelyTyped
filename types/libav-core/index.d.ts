e2301206119142b2d1329c52c32b09b14d27c168
Jerome Wu <jeromewus@gmail.com>
2022-08-30 10:55:30 +0800

    Init libav-core types

---
+++ types/libav-core/index.d.ts
@@ -0,0 +1,13 @@
+// Type definitions for libav-core 0.1
+// Project: https://github.com/ffmpegwasm/libav.wasm
+// Definitions by: Jerome Wu <https://github.com/jeromewu>
+// Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
+
+export type Pointer = number;
+
+export interface LibavCore {
+    NULL: Pointer;
+}
+
+declare function LibavCoreFactory(): Promise<LibavCore>;
+export default LibavCoreFactory;
