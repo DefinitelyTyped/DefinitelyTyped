c8b6fa7829de7e55b6423d8f86d10283133ecc46
Carl Egbert <egbertcarl@gmail.com>
2022-08-30 11:31:42 -0700

    remove deprecated connect function

--- types/braintree/index.d.ts
+++ types/braintree/index.d.ts
@@ -178,8 +178,6 @@ declare namespace braintree {
         webhookTesting: WebhookTestingGateway;
     }
 
-    export function connect(config: GatewayConfig): BraintreeGateway;
-
     interface ValidatedResponse<T> {
         success: boolean;
         errors: ValidationErrorsCollection;
