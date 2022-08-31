2869050b8f45cb8d1a3184dad09656d5138f327c
melangue <m@melangue.com>
2022-08-31 14:44:06 +0200

    @types/path-complete-extname - added missing declarations

--- types/path-complete-extname/index.d.ts
+++ types/path-complete-extname/index.d.ts
@@ -3,11 +3,22 @@
 // Definitions by: Marcin C <https://github.com/melangue>
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
 
-/// <reference types="node"/>
-
+export = pathCompleteExtname;
 /**
- * Takes a file path and returns the complete file extension (multiple dots, like tar.gz)
+ * Takes a file path and returns the complete file extension (multiple dots, like .tar.gz)
  * Automatically determines if running under Posix or Win32 environment
  */
-declare function pathCompleteExtname(path: string): string;
-export = pathCompleteExtname;
+ declare function pathCompleteExtname(path: string): string;
+ declare namespace pathCompleteExtname {
+   /**
+    * Takes a file path and returns the complete file extension (multiple dots, like .tar.gz)
+    * Uses Win32 logic for extension extraction
+    */
+   function win32(path: string): string;
+
+   /**
+    * Takes a file path and returns the complete file extension (multiple dots, like .tar.gz)
+    * Uses Posix logic for extension extraction
+    */
+   function posix(path: string): string;
+ }
