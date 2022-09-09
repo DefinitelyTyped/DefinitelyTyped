8df6e789955f76746510c245ea005773029059a4
golovkoe <golovkoe@ae.com>
2022-09-09 14:12:43 +0200

    Updating types for ember-modal-service for 2.7

--- types/ember-modal-service/components/modal.d.ts
+++ types/ember-modal-service/components/modal.d.ts
@@ -1,15 +1,9 @@
 import Component from '@ember/component';
-import Evented from '@ember/object/evented';
-import Scheduler from 'ember-task-scheduler';
 import EmberModalService from '../services/modal';
 import ModalModel from "../models/modal";
 
 export default class ModalComponent extends Component {
     declare model: ModalModel;
-    /**
-     * Modal service inject.
-     */
-    scheduler: Scheduler;
 
     /**
      * Modal service inject.
@@ -37,36 +31,17 @@ export default class ModalComponent extends Component {
     'data-id': string;
 
     /**
-     * Modal is visible/hidden. This property is read from CSS.
+     * Modal is visible/hidden.
      */
-    'data-modal-show': boolean;
-
-    /**
-     * On did insert element, set element as visible and set data-id.
-     */
-    onDidInsertElement: Evented;
+    'data-modal-show': string;
 
     /**
      * Resolve current promise and close modal.
      */
-    resolve: (data?: unknown, label?: string) => void;
+    resolve: (data?: unknown) => void;
 
     /**
      * Reject current promise and close modal.
      */
-    reject: (data?: unknown, label?: string) => void;
-
-    actions: {
-        /**
-         * Action to resolve the underlying modal promise directly from the
-         * template, using the passed arguments as resolution values
-         */
-        resolve: () => void;
-
-        /**
-         * Action to reject the underlying modal promise directly from the
-         * template, using the passed arguments as rejection values
-         */
-        reject: () => void;
-    };
+    reject: (error?: unknown) => void;
 }
