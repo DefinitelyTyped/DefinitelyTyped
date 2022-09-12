b409c4ee440632c1ea8d39e01c9bf96225b7c93c
Fabian Meyer <3982806+meyfa@users.noreply.github.com>
2022-09-12 22:03:49 +0200

    node: Add os.machine()

--- types/node/os.d.ts
+++ types/node/os.d.ts
@@ -414,6 +414,15 @@ declare module 'os' {
      */
     function platform(): NodeJS.Platform;
     /**
+     * Returns the machine type as a string, such as arm, aarch64, mips, mips64, ppc64, ppc64le, s390, s390x, i386, i686, x86_64.
+     *
+     * On POSIX systems, the machine type is determined by calling [`uname(3)`](https://linux.die.net/man/3/uname).
+     * On Windows, `RtlGetVersion()` is used, and if it is not available, `GetVersionExW()` will be used.
+     * See [https://en.wikipedia.org/wiki/Uname#Examples](https://en.wikipedia.org/wiki/Uname#Examples) for more information.
+     * @since v18.9.0
+     */
+    function machine(): string;
+    /**
      * Returns the operating system's default directory for temporary files as a
      * string.
      * @since v0.9.9
