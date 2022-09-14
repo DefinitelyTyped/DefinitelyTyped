8f3fd6a19996897403a3825453ca0594c06ba3e5
_ext Mikosch, Mathias <mathias.mikosch.ext@dieboldnixdorf.com>
2022-09-14 12:10:29 +0200

    Updated tests and added definition owner

--- types/braintree-web-drop-in/index.d.ts
+++ types/braintree-web-drop-in/index.d.ts
@@ -1,7 +1,8 @@
-// Type definitions for braintree-web-drop-in 1.28
+// Type definitions for braintree-web-drop-in 1.33.4
 // Project: https://github.com/braintree/braintree-web-dropin
 // Definitions by: Saoud Rizwan <https://github.com/saoudrizwan>
 //                 Ricard Solé Casas <https://github.com/iamricard>
+//                 Mathias Mikosch <https://github.com/elmikosch>
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
 // TypeScript Version: 3.3
 
