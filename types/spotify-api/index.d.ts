e528892b632bf7c1d3bc2b8be63c4ad5c5e16ab2
Shawn Smith <90342433+sds-smith@users.noreply.github.com>
2022-09-14 09:36:28 -0400

    Update index.d.ts

--- types/spotify-api/index.d.ts
+++ types/spotify-api/index.d.ts
@@ -1392,11 +1392,11 @@ declare namespace SpotifyApi {
          */
         external_ids: ExternalIdObject;
         /**
-        * The popularity of the track. The value will be between `0` and `100`, with `100` being the most popular.
-        * The popularity of a track is a value between `0` and `100`, with `100` being the most popular.
-        * The popularity is calculated by algorithm and is based, in the most part,
-        * on the total number of plays the track has had and how recent those plays are.
-        */
+         * The popularity of the track. The value will be between `0` and `100`, with `100` being the most popular.
+         * The popularity of a track is a value between `0` and `100`, with `100` being the most popular.
+         * The popularity is calculated by algorithm and is based, in the most part,
+         * on the total number of plays the track has had and how recent those plays are.
+         */
         popularity: number;
         /**
          * Whether or not the track is from a local file.
