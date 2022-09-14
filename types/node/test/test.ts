a3201112ae8bb1fe5a618fec53435fbd9d22fe75
Markus Tacker <m@coderbyheart.com>
2022-09-15 00:33:54 +0200

    [node:test] add before, beforeEach, after, afterEach
    These were introduced in v18.8.0

--- types/node/test/test.ts
+++ types/node/test/test.ts
@@ -1,4 +1,4 @@
-import { describe, it, test } from 'node:test';
+import { describe, it, test, before, beforeEach, after, afterEach } from 'node:test';
 
 test('foo', t => {
     // $ExpectType TestContext
@@ -121,3 +121,40 @@ describe(1, () => {});
 
 // @ts-expect-error
 it(1, () => {});
+
+// Hooks
+// - without callback
+before(() => {});
+beforeEach(() => {});
+after(() => {});
+beforeEach(() => {});
+// - with callback
+before(cb => {
+    // $ExpectType (result?: any) => void
+    cb;
+    // $ExpectType void
+    cb({ x: 'anything' });
+});
+beforeEach(cb => {
+    // $ExpectType (result?: any) => void
+    cb;
+    // $ExpectType void
+    cb({ x: 'anything' });
+});
+after(cb => {
+    // $ExpectType (result?: any) => void
+    cb;
+    // $ExpectType void
+    cb({ x: 'anything' });
+});
+beforeEach(cb => {
+    // $ExpectType (result?: any) => void
+    cb;
+    // $ExpectType void
+    cb({ x: 'anything' });
+});
+// - with options
+before(() => {}, { signal: new AbortController().signal, timeout: Infinity });
+beforeEach(() => {}, { signal: new AbortController().signal, timeout: Infinity });
+after(() => {}, { signal: new AbortController().signal, timeout: Infinity });
+beforeEach(() => {}, { signal: new AbortController().signal, timeout: Infinity });
