72770bc3472cecfcbc9bef4c66d8ea0fcdc4ea56
Changdae Park <cdpark0530@gmail.com>
2022-09-18 05:13:24 +0900

    Add jsonp-promise@0.1

---
+++ types/jsonp-promise/jsonp-promise-tests.ts
@@ -0,0 +1,19 @@
+import jsonp = require('jsonp-promise');
+
+/**
+ * Dummy function to test returned promise
+ */
+// tslint:disable-next-line no-unnecessary-generics
+const wait = async <T>(_promise: Promise<T>) => {};
+
+/**
+ * Tests parametrized jsonp request.
+ */
+const { promise, cancel } = jsonp('https://jsonplaceholder.typicode.com/posts/1', {
+    param: 'cb',
+    timeout: 40000,
+    prefix: '_jsonp',
+});
+wait(promise);
+// $ExpectType void
+cancel();
