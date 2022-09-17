579b4445fdb901c6171899ff6aa6b9c9a9cead63
Ankan Bhattacharya <ankanbhattacharya89@gmail.com>
2022-09-17 10:44:59 +0530

    feat: :sparkles: add types for has-values

--- types/has-values/has-values-tests.ts
+++ types/has-values/has-values-tests.ts
@@ -1,18 +1,20 @@
-import has from "has-values";
+import has from 'has-values';
 
-has(["a"]); // => true
-has([[], []]); // => false
+function tests() {
+    has(['a']); // => true
+    has([[], []]); // => false
 
-has(true); // => true
-has(false); // => false
+    has(true); // => true
+    has(false); // => false
 
-has(new Error()); // => false
-has(new Error('foo')); // => true
+    has(new Error()); // => false
+    has(new Error('foo')); // => true
 
-has(new Map()); // => false
-has(new Map([['foo', 'bar']])); // true
+    has(new Map()); // => false
+    has(new Map([['foo', 'bar']])); // true
 
-has(null); // => true
+    has(null); // => true
 
-has(new Set()); // => false
-has(new Set(['foo', 'bar'])); // => true
+    has(new Set()); // => false
+    has(new Set(['foo', 'bar'])); // => true
+}
