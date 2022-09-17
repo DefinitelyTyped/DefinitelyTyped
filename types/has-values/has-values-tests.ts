a65c82270537a6e38f1c85b9354dbb4bd0381e39
Ankan Bhattacharya <ankanbhattacharya89@gmail.com>
2022-09-17 09:47:47 +0530

    feat: :sparkles: add types for has-values

---
+++ types/has-values/has-values-tests.ts
@@ -0,0 +1,18 @@
+import has from "has-values";
+
+has(["a"]); // => true
+has([[], []]); // => false
+
+has(true); // => true
+has(false); // => false
+
+has(new Error()); // => false
+has(new Error('foo')); // => true
+
+has(new Map()); // => false
+has(new Map([['foo', 'bar']])); // true
+
+has(null); // => true
+
+has(new Set()); // => false
+has(new Set(['foo', 'bar'])); // => true
---
+++ types/has-values/index.d.ts
@@ -0,0 +1,6 @@
+// Type definitions for has-values 2.0
+// Project: https://github.com/jonschlinkert/has-values
+// Definitions by: Ankan Bhattacharya <https://github.com/Ankan002>
+// Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
+
+export default function has(value: unknown): boolean;
