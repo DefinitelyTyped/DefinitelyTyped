c99c28092d54fd7c8fb6bc2bd02a7afeab797c83
sinanbozkus <sinan.bozkus@gmail.com>
2022-09-21 14:09:56 +0300

    export { ReCAPTCHA } removed.

--- types/react-google-recaptcha/react-google-recaptcha-tests.tsx
+++ types/react-google-recaptcha/react-google-recaptcha-tests.tsx
@@ -4,13 +4,13 @@ import ReCAPTCHA from 'react-google-recaptcha';
 const handleRef = (ref: ReCAPTCHA | null): void => { return; };
 
 const basicRecapchta = <ReCAPTCHA ref={handleRef} sitekey="xxx" onChange={a => a} className="mockclass" />;
+
 const invisibleRecaptcha: React.FC = () => {
     const recaptchaRef = React.createRef<ReCAPTCHA>();
 
     return <ReCAPTCHA ref={recaptchaRef} sitekey="xxx" size="invisible" asyncScriptOnLoad={() => { }} className="mockclass" />;
 };
 
-const basicRecapchta2 = <ReCAPTCHA ref={handleRef} sitekey="xxx" onChange={a => a} className="mockclass" />;
 const invisibleRecaptcha2: React.FC = () => {
     const recaptchaRef = React.createRef<ReCAPTCHA>();
 
