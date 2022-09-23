3adca5a13ae69ac68ac87c8596b16cfaaca3aae5
tpluscode <tpluscode@users.noreply.github.com>
2022-09-23 17:19:01 +0200

    feat: types for express-preconditions

---
+++ types/express-preconditions/index.d.ts
@@ -0,0 +1,27 @@
+// Type definitions for express-preconditions 1.0
+// Project: https://richardschneider.github/express-conditional-request
+// Definitions by: tpluscode <https://github.com/tpluscode>
+// Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
+
+import type { RequestHandler, Request, Response } from 'express';
+
+declare namespace preconditions {
+    interface ResourceState {
+        etag?: string | null;
+        lastModified?: string | null;
+    }
+
+    interface StateProvider {
+        (req: Request): Promise<ResourceState>;
+    }
+
+    interface Options {
+        stateAsync?: StateProvider;
+        error?(statusCode: number, message: string, req: Request, res: Response): void;
+        requiredWith?: string[];
+    }
+}
+
+declare function preconditions(options?: preconditions.Options): RequestHandler;
+
+export = preconditions;
