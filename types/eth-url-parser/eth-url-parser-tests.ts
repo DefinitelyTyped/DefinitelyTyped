f3f5393126eb0f4529ce050d2df4606064f43e5e
gomes <17035424+gomesalexandre@users.noreply.github.com>
2022-09-24 14:39:04 +0200

    Add types for eth-url-parser

---
+++ types/eth-url-parser/eth-url-parser-tests.ts
@@ -0,0 +1,8 @@
+import { parse, build } from 'eth-url-parser';
+
+parse('ethereum:0xd8da6bf26964af9d7eed9e03e53415d37aa96045@1'); // $ExpectType ParseOutput
+// $ExpectType string
+build({
+  target_address: '0xd8da6bf26964af9d7eed9e03e53415d37aa96045',
+  chain_id: '1'
+});
---
+++ types/eth-url-parser/index.d.ts
@@ -0,0 +1,34 @@
+// Type definitions for eth-url-parser 1.0
+// Project: https://github.com/baz/foo (Does not have to be to GitHub, but prefer linking to a source code repository rather than to a project website.)
+// Definitions by: gomes <https://github.com/DefinitelyTyped>
+// Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
+
+/*~ If this module is a UMD module that exposes a global variable 'myLib' when
+ *~ loaded outside a module loader environment, declare that global here.
+ *~ Otherwise, delete this declaration.
+ */
+
+declare namespace ethUrlParser {
+  interface ParseOutput {
+    scheme: 'ethereum';
+    target_address: string;
+    chain_id?: `${number}`;
+    function_name?: string;
+    prefix?: string;
+    parameters?: Record<string, string>;
+  }
+
+  interface BuildInput  {
+    prefix?: 'pay' | null;
+    target_address: string;
+    chain_id?: `${number}` | null;
+    function_name?: string | null;
+    parameters?: Record<string, string> | null;
+  }
+
+  function parse(url: string): ParseOutput;
+  function build(input: BuildInput): string;
+}
+
+// eslint-disable-next-line
+export = ethUrlParser;
