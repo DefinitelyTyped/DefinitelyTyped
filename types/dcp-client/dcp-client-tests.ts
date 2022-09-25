64e14b10393c8478a0efe0aba97c2a957ae68bda
Roman Fairushyn <roman.f@svitsoft.com>
2022-09-26 01:56:28 +0200

    test hot-fix

--- types/dcp-client/dcp-client-tests.ts
+++ types/dcp-client/dcp-client-tests.ts
@@ -102,7 +102,7 @@ export const loadOptions: LoadOptions = {
     const rangeObject2 = new RangeObject(0, 1000, 2);
     const multiRange = new MultiRangeObject(rangeObject1, rangeObject2);
     const computeJob = await compute.for(multiRange, async (sliceIndex: number[], data: any) => {
-        await compute.progress();
+        compute.progress();
         return sliceIndex[0] ** 2 + sliceIndex[1] ** 2 + Math.sqrt(data);
     }, [100]);
     //#endregion
