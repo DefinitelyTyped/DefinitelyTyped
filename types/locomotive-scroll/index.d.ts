911eaec2ce400613b68d5f4d77fe45cccbeaa5c4
ColinLienard <colin.lienard87@orange.fr>
2022-09-29 11:06:06 +0200

    fix: support for CommonJS

--- types/locomotive-scroll/index.d.ts
+++ types/locomotive-scroll/index.d.ts
@@ -3,110 +3,114 @@
 // Definitions by: Colin Lienard <https://github.com/ColinLienard>
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
 
-export type Direction = 'vertical' | 'horizontal';
+declare namespace LocomotiveScroll {
+    type Direction = 'vertical' | 'horizontal';
 
-export type Offset = [number | string, number | string];
+    type Offset = [number | string, number | string];
 
-export type ScrollToTarget = HTMLElement | string | number;
+    type ScrollToTarget = HTMLElement | string | number;
 
-export interface Position {
-    x: number;
-    y: number;
-}
+    interface Position {
+        x: number;
+        y: number;
+    }
 
-export interface DeviceOptions {
-    smooth?: boolean;
-    direction?: Direction;
-    gestureDirection?: Direction;
-}
+    interface DeviceOptions {
+        smooth?: boolean;
+        direction?: Direction;
+        gestureDirection?: Direction;
+    }
 
-export interface InstanceOptions {
-    el?: HTMLElement;
-    name?: string;
-    offset?: Offset;
-    repeat?: boolean;
-    smooth?: boolean;
-    initPosition?: Position;
-    direction?: Direction;
-    lerp?: number;
-    getDirection?: boolean;
-    getSpeed?: boolean;
-    class?: string;
-    initClass?: string;
-    scrollingClass?: string;
-    draggingClass?: string;
-    smoothClass?: string;
-    scrollbarContainer?: HTMLElement | false;
-    scrollbarClass?: string;
-    multiplier?: number;
-    firefoxMultiplier?: number;
-    touchMultiplier?: number;
-    scrollFromAnywhere?: boolean;
-    gestureDirection?: string;
-    tablet?: DeviceOptions & { breakpoint: number };
-    smartphone?: DeviceOptions;
-    reloadOnContextChange?: boolean;
-    resetNativeScroll?: boolean;
-}
+    interface InstanceOptions {
+        el?: HTMLElement;
+        name?: string;
+        offset?: Offset;
+        repeat?: boolean;
+        smooth?: boolean;
+        initPosition?: Position;
+        direction?: Direction;
+        lerp?: number;
+        getDirection?: boolean;
+        getSpeed?: boolean;
+        class?: string;
+        initClass?: string;
+        scrollingClass?: string;
+        draggingClass?: string;
+        smoothClass?: string;
+        scrollbarContainer?: HTMLElement | false;
+        scrollbarClass?: string;
+        multiplier?: number;
+        firefoxMultiplier?: number;
+        touchMultiplier?: number;
+        scrollFromAnywhere?: boolean;
+        gestureDirection?: string;
+        tablet?: DeviceOptions & { breakpoint: number };
+        smartphone?: DeviceOptions;
+        reloadOnContextChange?: boolean;
+        resetNativeScroll?: boolean;
+    }
 
-export interface ScrollElement {
-    bottom: number;
-    call?: () => void;
-    class: string;
-    delay?: number;
-    direction?: Direction;
-    el: HTMLElement;
-    id: string;
-    inView: boolean;
-    left: number;
-    middle: Position;
-    offset: Offset;
-    position?: Position;
-    progress: number;
-    repeat: boolean;
-    right: number;
-    section: {
+    interface ScrollElement {
+        bottom: number;
+        call?: () => void;
+        class: string;
+        delay?: number;
+        direction?: Direction;
         el: HTMLElement;
         id: string;
-        inView: true;
+        inView: boolean;
+        left: number;
+        middle: Position;
+        offset: Offset;
+        position?: Position;
+        progress: number;
+        repeat: boolean;
+        right: number;
+        section: {
+            el: HTMLElement;
+            id: string;
+            inView: true;
+            limit: Position;
+            offset: Position;
+            persistent: boolean;
+        };
+        speed: number | boolean;
+        sticky: boolean;
+        target: HTMLElement;
+        top: number;
+    }
+
+    interface OnScrollEvent {
+        currentElements: Record<string, ScrollElement>;
+        delta: Position;
         limit: Position;
-        offset: Position;
-        persistent: boolean;
-    };
-    speed: number | boolean;
-    sticky: boolean;
-    target: HTMLElement;
-    top: number;
+        scroll: Position;
+        speed: number;
+    }
+
+    interface ScrollToOptions {
+        offset?: number;
+        callback?: () => void;
+        duration?: number;
+        easing?: [number, number, number, number];
+        disableLerp?: boolean;
+    }
 }
 
-export interface OnScrollEvent {
-    currentElements: Record<string, ScrollElement>;
-    delta: Position;
-    limit: Position;
-    scroll: Position;
-    speed: number;
-}
-
-export interface ScrollToOptions {
-    offset?: number;
-    callback?: () => void;
-    duration?: number;
-    easing?: [number, number, number, number];
-    disableLerp?: boolean;
-}
-
-export default class LocomotiveScroll {
-    constructor(options?: InstanceOptions);
+declare class LocomotiveScroll {
+    constructor(options?: LocomotiveScroll.InstanceOptions);
     init(): () => void;
     on<EventName extends 'scroll' | 'call'>(
         eventName: EventName,
-        callback: (event: EventName extends 'scroll' ? OnScrollEvent : string | string[]) => void,
+        callback: (event: EventName extends 'scroll' ? LocomotiveScroll.OnScrollEvent : string | string[]) => void,
     ): () => void;
     update(): () => void;
     destroy(): () => void;
     start(): () => void;
     stop(): () => void;
-    scrollTo(target: ScrollToTarget, options?: ScrollToOptions): () => void;
+    scrollTo(target: LocomotiveScroll.ScrollToTarget, options?: LocomotiveScroll.ScrollToOptions): () => void;
 }
 
 export as namespace LocomotiveScroll;
+
+export = LocomotiveScroll;
