8ab8ba31a3f44efcc787894a77e40bb38f11f539
Harris Miller <harrismillerconsulting@gmail.com>
2022-09-30 17:04:06 -0600

    chain type support for monad in second argument

--- types/ramda/index.d.ts
+++ types/ramda/index.d.ts
@@ -646,6 +646,9 @@ export function call<T extends AnyFunction>(fn: T, ...args: Parameters<T>): Retu
 export function chain<A, B, T = never>(fn: (n: A) => readonly B[], list: readonly A[]): B[];
 export function chain<A, B, T = never>(fn: (n: A) => readonly B[]): (list: readonly A[]) => B[];
 
+export function chain<A, Ma extends { chain: (fn: (a: A) => Mb) => Mb }, Mb>(fn: (a: A) => Mb, monad: Ma): Mb;
+export function chain<A, Ma extends { chain: (fn: (a: A) => Mb) => Mb }, Mb>(fn: (a: A) => Mb): (monad: Ma) => Mb;
+
 export function chain<A, B, R>(aToMb: (a: A, r: R) => B, Ma: (r: R) => A): (r: R) => B;
 export function chain<A, B, R>(aToMb: (a: A, r: R) => B): (Ma: (r: R) => A) => (r: R) => B;
 
