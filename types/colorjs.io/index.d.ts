abcb545f27a5775b374a749f10e53ce053b853f4
Adam Thompson-Sharpe <adamthompsonsharpe@gmail.com>
2022-10-01 15:34:05 -0400

    feat: Change where `index.d.ts` re-exports from
    This was mostly done to fix CI errors, but it's also techinically more
    correct due to how the original package's exports are set up.

--- types/colorjs.io/index.d.ts
+++ types/colorjs.io/index.d.ts
@@ -2,4 +2,4 @@
 // Project: https://github.com/LeaVerou/color.js
 // Definitions by: Adam Thompson-Sharpe <https://github.com/MysteryBlokHed>
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
-export { default } from './src';
+export { default } from './dist/color';
