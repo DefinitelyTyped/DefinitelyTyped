476b060f254258f596664d83604d171d8f722e22
Adam Thompson-Sharpe <adamthompsonsharpe@gmail.com>
2022-10-01 15:56:18 -0400

    feat(colorjs.io): Update exports from `src/deltaE/index.d.ts`

--- types/colorjs.io/src/deltaE/index.d.ts
+++ types/colorjs.io/src/deltaE/index.d.ts
@@ -1,8 +1,8 @@
-export type Methods = keyof typeof import('.') extends `deltaE${infer Method}` ? Method : never;
+export type Methods = keyof typeof import('.') extends `deltaE${infer Method}` ? Method : string;
 
-export { default as deltaE76 } from './deltaE76.js';
-export { default as deltaECMC } from './deltaECMC.js';
-export { default as deltaE2000 } from './deltaE2000.js';
-export { default as deltaEJz } from './deltaEJz.js';
-export { default as deltaEITP } from './deltaEITP.js';
-export { default as deltaEOK } from './deltaEOK.js';
+export { default as deltaE76 } from './deltaE76';
+export { default as deltaECMC } from './deltaECMC';
+export { default as deltaE2000 } from './deltaE2000';
+export { default as deltaEJz } from './deltaEJz';
+export { default as deltaEITP } from './deltaEITP';
+export { default as deltaEOK } from './deltaEOK';
