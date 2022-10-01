c1a303108b1d2f557d386276fd424568f6184438
nwd0907 <nwd0907@newbizstart.com>
2022-10-02 02:46:19 +0900

    Add types for moveday

---
+++ types/moveday/index.d.ts
@@ -0,0 +1,7 @@
+export declare const getToday: () => string;
+export declare const getMoveday: (moveday: number) => string;
+declare const _default: {
+    getToday: () => string;
+    getMoveday: (moveday: number) => string;
+};
+export default _default;
