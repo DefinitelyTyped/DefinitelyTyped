b0bd0132df0aa91e151d5b762505bbe1fefe2dcd
Adam Thompson-Sharpe <adamthompsonsharpe@gmail.com>
2022-10-01 19:31:10 -0400

    feat(colorjs.io): Update types for CATs

--- types/colorjs.io/src/CATs.d.ts
+++ types/colorjs.io/src/CATs.d.ts
@@ -1,7 +1,13 @@
 import { White } from './adapt';
 
-export const CATs: Record<string, any>;
+export interface CAT {
+    id: string;
+    toCone_M: number[][];
+    fromCone_M: number[][];
+}
 
-export function defineCAT(options: { id: string; toCone_M: number[][]; fromCone_M: number[][] }): void;
+export const CATs: Record<string, CAT>;
+
+export function defineCAT(cat: CAT): void;
 
 export function adapt(W1: White, W2: White, id?: string): number[];
