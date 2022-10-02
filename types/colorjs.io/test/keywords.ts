8a65744dc4aaa0cb03750e5933e646409eb087d3
Adam Thompson-Sharpe <adamthompsonsharpe@gmail.com>
2022-10-02 00:02:16 -0400

    feat(colorjs.io): Add some tests and fix problems with `OTHER_FILES.txt`

---
+++ types/colorjs.io/test/keywords.ts
@@ -0,0 +1,2 @@
+import keywords from 'colorjs.io/src/keywords';
+keywords; // $ExpectType Record<string, [number, number, number]>
---
+++ types/colorjs.io/test/space-accessors.ts
@@ -0,0 +1,2 @@
+// Doesn't export any values, so just check that it's a module
+import 'colorjs.io/src/space-accessors';
