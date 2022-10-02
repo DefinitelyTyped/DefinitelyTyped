6700a35d5e3c46b22ad134463715dc2306167304
Adam Thompson-Sharpe <adamthompsonsharpe@gmail.com>
2022-10-01 20:45:19 -0400

    fix(colorjs.io): Correct types in `src/display.d.ts`

--- types/colorjs.io/src/display.d.ts
+++ types/colorjs.io/src/display.d.ts
@@ -1,11 +1,11 @@
-import Color, { ColorTypes } from './color';
+import Color, { ColorObject, ColorTypes } from './color';
 import ColorSpace from './space';
 
-export type Display = string & { color: Color };
+export type Display = string & { color: ColorTypes };
 
 export default function display(
     color: ColorTypes,
     options?: {
         space?: string | ColorSpace | undefined;
     } & Record<string, any>,
-): string;
+): Display;
