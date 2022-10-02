6414ebeb254659530ffb11542470c32248e9b754
Adam Thompson-Sharpe <adamthompsonsharpe@gmail.com>
2022-10-01 20:54:48 -0400

    fix(colorjs.io): Fix some mistakes in `src/hooks.d.ts`

--- types/colorjs.io/src/hooks.d.ts
+++ types/colorjs.io/src/hooks.d.ts
@@ -1,9 +1,9 @@
-export class Hooks implements Hooks {
+export class Hooks {
     // Can't find a way to type this more specifically
     // without conflicting with the types of add and run
     [name: string]: any;
 
-    add(name: string | string[], callback: (env: Record<string, any>) => void, first: boolean): void;
+    add(name: string | string[], callback: (env: Record<string, any>) => void, first?: boolean): void;
     run(name: string, env?: { context?: Record<string, any> }): void;
 }
 
