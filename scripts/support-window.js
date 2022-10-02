d94b29163e4aa14a09ae5fff666f714bf7de59b6
Adam Thompson-Sharpe <adamthompsonsharpe@gmail.com>
2022-10-02 19:56:36 -0400

    fix: Fix problems with support-window workflow & script

--- scripts/support-window.js
+++ scripts/support-window.js
@@ -6,7 +6,7 @@ import { utcYear } from "d3-time";
 import { utcFormat } from "d3-time-format";
 import { JSDOM } from "jsdom";
 import serialize from "w3c-xmlserializer";
-import data from "../docs/support-window.json" assert { type: "json" };
+import data from "../docs/support-window.json";
 
 const width = 640;
 const height = 250;
