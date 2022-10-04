29e71b074e4b08aef8fbd3f0c38b301f10908989
Fredrik Johansen <fredrik1206@gmail.com>
2022-10-04 15:21:53 +0200

    changes version back to 0.4 to match npm

--- types/omelette/index.d.ts
+++ types/omelette/index.d.ts
@@ -1,4 +1,4 @@
-// Type definitions for omelette 0.5
+// Type definitions for omelette 0.4
 // Project: https://github.com/f/omelette
 // Definitions by: Kamontat Chantrachirathumrong <https://github.com/kamontat>
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
