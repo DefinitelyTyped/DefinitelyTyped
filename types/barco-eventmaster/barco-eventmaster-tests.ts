3bba0b6a99326514123885a9c6b913466f2f7408
Kyle Hensel <k.y.l.e@outlook.co.nz>
2022-10-05 11:40:13 +1300

    remove console.log

--- types/barco-eventmaster/barco-eventmaster-tests.ts
+++ types/barco-eventmaster/barco-eventmaster-tests.ts
@@ -4,15 +4,19 @@ const e2 = new EventMaster('10.0.0.1');
 
 e2.listPresets(-1, -1, (err, presets) => {
     if (err !== null) {
-        console.log('Current presets');
-        console.log(presets);
     } else {
-        console.log(err);
+        // $ExpectType null
+        err;
         throw new Error('Something went wrong with the event master request');
     }
 });
 
 e2.listSourceMainBackup();
-e2.listSourceMainBackup(undefined, err => console.log(err || 'OK'));
+e2.listSourceMainBackup(undefined, err => {
+    if (err) return;
+
+    // $ExpectType null
+    err;
+});
 
 e2.subscribe('10.0.0.69', 66000, ['AUXDestChanged', 'StillChanged']);
