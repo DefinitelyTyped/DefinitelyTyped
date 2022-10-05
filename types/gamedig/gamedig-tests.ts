33a96449e224dece40e737759ab0de39d47fb78b
MaximumWoahverdrive <joshua@schmeder.dev>
2022-10-05 18:14:25 +0200

    gamedig: Add test for usage of an instance of gamedig in addition to the existing test direct from import

--- types/gamedig/gamedig-tests.ts
+++ types/gamedig/gamedig-tests.ts
@@ -1,6 +1,7 @@
 import Gamedig = require('gamedig');
 import GameResolver = require('gamedig/lib/GameResolver');
 
+// direct usage from import
 Gamedig.query(
     {
         type: 'tf2',
@@ -14,7 +15,28 @@ Gamedig.query(
         requestRules: true,
     },
     (error, state) => {
-        if (error) throw error();
+        if (error) throw error;
+
+        const { name, map, password, maxplayers, players, bots, connect, ping } = state;
+    },
+);
+
+// usage from instance
+const gamedig = new Gamedig();
+gamedig.query(
+    {
+        type: 'tf2',
+        host: '127.0.0.1',
+        port: 27015,
+        maxAttempts: 1,
+        socketTimeout: 2000,
+        attemptTimeout: 10000,
+        givenPortOnly: true,
+        debug: false,
+        requestRules: true,
+    },
+    (error, state) => {
+        if (error) throw error;
 
         const { name, map, password, maxplayers, players, bots, connect, ping } = state;
     },
