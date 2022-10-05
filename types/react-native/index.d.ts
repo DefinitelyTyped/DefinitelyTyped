97f1fe15c9b7cf781629594de1164c0395ddd5e4
Aelita <xsjctony@126.com>
2022-10-05 17:31:58 +1100

    [@types/react-native] StyleSheet.create() now properly checks non-exist properties

--- types/react-native/index.d.ts
+++ types/react-native/index.d.ts
@@ -5462,7 +5462,7 @@ export namespace StyleSheet {
     /**
      * Creates a StyleSheet style reference from the given object.
      */
-    export function create<T extends NamedStyles<T> | NamedStyles<any>>(styles: T | NamedStyles<T>): T;
+    export function create<T extends NamedStyles<T> | NamedStyles<any>>(styles: NamedStyles<T>): T;
 
     /**
      * Flattens an array of style objects, into one aggregated style object.
