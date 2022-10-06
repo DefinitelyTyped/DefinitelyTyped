0103d7037e270429027bad5191ec5a2f90269fae
Kwatanwa17 <29068431+kwatanwa17@users.noreply.github.com>
2022-10-06 16:34:31 +0900

    rename reveal to reveal.js and remove ambient modules

---
+++ types/reveal.js/plugin/zoom/zoom/index.d.ts
@@ -0,0 +1,12 @@
+
+import Reveal = require('../../../.');
+
+
+/**
+ * Reveal plugin zoom
+ *
+ * @see {@link https://github.com/hakimel/reveal.js/tree/master/plugin/zoom}
+ */
+declare const RevealZoom: Reveal.PluginFunction;
+
+export = RevealZoom;
