d934ee9c9d1dd3056adb470714b542fc0064affe
yucarl77 <yucarl77@foxmail.com>
2022-10-06 18:49:43 +0800

    chore: array lint

--- types/samp-node/index.d.ts
+++ types/samp-node/index.d.ts
@@ -5,14 +5,15 @@
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
 
 declare namespace samp {
-    export function addEventListener(eventName: string, func: (...args: Array<any>) => void): void;
-    export function removeEventListener(eventName: string): void;
-    export function removeEventListener(eventName: string, func: (...args: Array<any>) => void): void;
-    export function removeEventListener(eventName: string, funcs: Array<(...args: Array<any>) => void>): void;
-    export function registerEvent(eventName: string, paramTypes: string): boolean;
-    export function fire(eventName: string, ...args: Array<any>): void;
-    export function callNative(nativeName: string, paramTypes: string, ...args: Array<any>): any;
-    export function callNativeFloat(nativeName: string, paramTypes: string, ...args: Array<any>): any;
-    export function callPublic(publicName: string, paramTypes: string, ...args: Array<any>): any;
-    export function callPublicFloat(publicName: string, paramTypes: string, ...args: Array<any>): any;
+    function addEventListener(eventName: string, func: (...args: any[]) => void): void;
+    function removeEventListener(
+        eventName: string,
+        func?: (...args: any[]) => void | Array<(...args: any[]) => void>,
+    ): void;
+    function registerEvent(eventName: string, paramTypes: string): boolean;
+    function fire(eventName: string, ...args: any[]): void;
+    function callNative(nativeName: string, paramTypes: string, ...args: any[]): any;
+    function callNativeFloat(nativeName: string, paramTypes: string, ...args: any[]): any;
+    function callPublic(publicName: string, paramTypes: string, ...args: any[]): any;
+    function callPublicFloat(publicName: string, paramTypes: string, ...args: any[]): any;
 }
