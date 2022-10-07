6b8ef3039162d3a0dbf82e9ad9802b062ae93d40
Alex Muramoto <amuramoto@apache.org>
2022-10-07 12:02:21 -0700

    Adds constructor for LatLngAltitude

--- types/google.maps/index.d.ts
+++ types/google.maps/index.d.ts
@@ -3379,6 +3379,31 @@ declare namespace google.maps {
   class LatLngAltitude implements google.maps.LatLngAltitudeLiteral,
                                   google.maps.LatLngLiteral {
     /**
+     * A <code>LatLngAltitude</code> is a 3D point in geographical coordinates:
+     * latitude, longitude, and altitude.<br> <ul> <li>Latitude ranges between
+     * -90 and 90 degrees, inclusive. Values above or below this range will be
+     * clamped to the range [-90, 90]. This means that if the value specified is
+     * less than -90, it will be set to -90. And if the value is greater than
+     * 90, it will be set to 90.</li> <li>Longitude ranges between -180 and 180
+     * degrees, inclusive. Values above or below this range will be wrapped so
+     * that they fall within the range. For example, a value of -190 will be
+     * converted to 170. A value of 190 will be converted to -170. This reflects
+     * the fact that longitudes wrap around the globe.</li> <li>Altitude is
+     * measured in meters. Positive values denote heights above ground level,
+     * and negative values denote heights underneath the ground surface.</li>
+     * </ul>
+     * @param value The initializing value.
+     * @param noClampNoWrap Whether to preserve the initialization values, even
+     *     if they may not necessarily be valid latitude values in the range of
+     *     [-90, 90] or valid longitude values in the range of [-180, 180]. The
+     *     default is <code>false</code> which enables latitude clamping and
+     *     longitude wrapping.
+     */
+    constructor(
+      value: google.maps.LatLngAltitudeLiteral|google.maps.LatLng|
+      google.maps.LatLngLiteral,
+      noClampNoWrap?: boolean);
+    /**
      * Returns the altitude.
      */
     altitude: number;
