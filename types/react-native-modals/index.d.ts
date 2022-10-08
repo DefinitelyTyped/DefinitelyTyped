d4e1e20ab98baa4e17b89e4dd56a5112786d0e5a
Jean-Baptiste Crestot <jbcrestot@gmail.com>
2022-10-08 17:57:35 +0200

    [react-native-modals] update definition to version 0.22

--- types/react-native-modals/index.d.ts
+++ types/react-native-modals/index.d.ts
@@ -1,10 +1,11 @@
-// Type definitions for react-native-modals 0.19
+// Type definitions for react-native-modals 0.22
 // Project: https://github.com/jacklam718/react-native-modals/blob/master/README.md
 // Definitions by: Paito Anderson <https://github.com/PaitoAnderson>
+//                 Jean-Baptiste Crestot <https://github.com/jbcrestot>
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
 
 import * as React from 'react';
-import { StyleProp, ViewStyle, TextStyle } from 'react-native';
+import { StyleProp, ViewStyle, TextStyle, Animated } from 'react-native';
 
 export type SlideFromTypes = 'top' | 'bottom' | 'left' | 'right';
 export type AlignTypes = 'flex-start' | 'flex-end' | 'center';
@@ -21,119 +22,125 @@ export interface DragEvent {
         width: number;
         height: number;
     };
-    swipeDirection: string | null;
+    swipeDirection: SwipeDirection | null;
 }
 
 export interface ModalContentProps {
     children?: React.ReactNode;
-    style?: StyleProp<ViewStyle> | undefined;
+    style?: StyleProp<ViewStyle>;
 }
 
 export interface BackdropProps {
     visible: boolean;
     opacity: number;
-    onPress?: (() => void) | undefined;
-    backgroundColor?: string | undefined;
-    animationDuration?: number | undefined;
-    pointerEvents?: string | undefined;
-    useNativeDriver?: boolean | undefined;
+    onPress?: () => void;
+    backgroundColor?: string;
+    animationDuration?: number;
+    pointerEvents?: string;
+    useNativeDriver?: boolean;
 }
 
 export interface ModalFooterProps {
-    bordered?: boolean | undefined;
+    bordered?: boolean;
     children?: React.ReactNode;
-    style?: StyleProp<ViewStyle> | undefined;
+    style?: StyleProp<ViewStyle>;
 }
 
 export interface ModalButtonProps {
     text: string;
     onPress: () => void;
-    align?: AlignTypes | undefined;
-    style?: StyleProp<ViewStyle> | undefined;
-    textStyle?: StyleProp<TextStyle> | undefined;
-    disabled?: boolean | undefined;
-    activeOpacity?: number | undefined;
-    bordered?: boolean | undefined;
+    align?: AlignTypes;
+    style?: StyleProp<ViewStyle>;
+    textStyle?: StyleProp<TextStyle>;
+    disabled?: boolean;
+    activeOpacity?: number;
+    bordered?: boolean;
 }
 
 export interface ModalTitleProps {
     title: string;
-    style?: StyleProp<ViewStyle> | undefined;
-    textStyle?: StyleProp<TextStyle> | undefined;
-    align?: AlignTypes | undefined;
-    hasTitleBar?: boolean | undefined;
+    style?: StyleProp<ViewStyle>;
+    textStyle?: StyleProp<TextStyle>;
+    align?: AlignTypes;
+    hasTitleBar?: boolean;
 }
 
 export interface ModalProps {
     children?: React.ReactNode;
-    visible: boolean;
-    width?: number | undefined;
-    height?: number | undefined;
-    rounded?: boolean | undefined;
-    hasOverlay?: boolean | undefined;
-    overlayPointerEvents?: OverlayPointerEventTypes | undefined;
-    overlayBackgroundColor?: string | undefined;
-    overlayOpacity?: number | undefined;
-    modalTitle?: React.ReactNode | undefined;
-    modalAnimation?: FadeAnimation | ScaleAnimation | SlideAnimation | undefined;
-    modalStyle?: StyleProp<ViewStyle> | undefined;
-    style?: StyleProp<ViewStyle> | undefined;
-    animationDuration?: number | undefined;
-    onTouchOutside?: (() => void) | undefined;
-    onHardwareBackPress?: (() => boolean) | undefined;
-    onShow?: (() => void) | undefined;
-    onDismiss?: (() => void) | undefined;
-    footer?: React.ReactNode | undefined;
-    onMove?: ((event: DragEvent) => void) | undefined;
-    onSwiping?: ((event: DragEvent) => void) | undefined;
-    onSwipeRelease?: ((event: DragEvent) => void) | undefined;
-    onSwipingOut?: ((event: DragEvent) => void) | undefined;
-    onSwipeOut?: ((event: DragEvent) => void) | undefined;
-    swipeDirection?: SwipeDirection | SwipeDirection[] | undefined;
-    swipeThreshold?: number | undefined;
-    useNativeDriver?: boolean | undefined;
+    visible?: boolean;
+    width?: number;
+    height?: number;
+    rounded?: boolean;
+    hasOverlay?: boolean;
+    overlayPointerEvents?: OverlayPointerEventTypes;
+    overlayBackgroundColor?: string;
+    overlayOpacity?: number;
+    modalTitle?: React.ReactNode;
+    modalAnimation?: FadeAnimation | ScaleAnimation | SlideAnimation;
+    modalStyle?: StyleProp<ViewStyle>;
+    style?: StyleProp<ViewStyle>;
+    animationDuration?: number;
+    onTouchOutside?: () => void;
+    onHardwareBackPress?: () => boolean;
+    onShow?: () => void;
+    onDismiss?: () => void;
+    footer?: React.ReactNode;
+    onMove?: (event: DragEvent) => void;
+    onSwiping?: (event: DragEvent) => void;
+    onSwipeRelease?: (event: DragEvent) => void;
+    onSwipingOut?: (event: DragEvent) => void;
+    onSwipeOut?: (event: DragEvent) => void;
+    swipeDirection?: SwipeDirection | SwipeDirection[];
+    swipeThreshold?: number;
+    useNativeDriver?: boolean;
 }
 
 export interface DraggableViewProps {
-    style?: StyleProp<ViewStyle> | undefined;
-    onMove?: ((event: DragEvent) => void) | undefined;
-    onSwiping?: ((event: DragEvent) => void) | undefined;
-    onRelease?: ((event: DragEvent) => void) | undefined;
-    onSwipingOut?: ((event: DragEvent) => void) | undefined;
-    onSwipeOut?: ((event: DragEvent) => void) | undefined;
-    swipeThreshold?: number | undefined;
-    swipeDirection?: SwipeDirection | SwipeDirection[] | undefined;
+    style?: StyleProp<ViewStyle>;
+    onMove?: (event: DragEvent) => void;
+    onSwiping?: (event: DragEvent) => void;
+    onRelease?: (event: DragEvent) => void;
+    onSwipingOut?: (event: DragEvent) => void;
+    onSwipeOut?: (event: DragEvent) => void;
+    swipeThreshold?: number;
+    swipeDirection?: SwipeDirection | SwipeDirection[];
 }
 
 export class FadeAnimation {
-    constructor(toValue?: number);
-    constructor(params: { toValue?: number | undefined, animationDuration?: number | undefined });
-    toValue(toValue: number): void;
-    createAnimations(): object;
+    constructor(params: { animationDuration?: number; useNativeDriver?: boolean });
+    in(onFinised?: boolean): void;
+    out(onFinised?: boolean): void;
+    getAnimations(): object;
 }
 
 export class ScaleAnimation {
-    constructor(toValue?: number);
-    toValue(toValue: number): void;
-    createAnimations(): object;
+    constructor(params: { animationDuration?: number; useNativeDriver?: boolean });
+    in(onFinised?: boolean): void;
+    out(onFinised?: boolean): void;
+    getAnimations(): object;
 }
 
 export class SlideAnimation {
-    constructor(toValue?: number);
-    constructor(params: { toValue?: number | undefined, slideFrom?: SlideFromTypes | undefined });
-    toValue(toValue: number): void;
-    createAnimations(): object;
+    constructor(params: { animationDuration?: number; useNativeDriver?: boolean; slideFrom?: SlideFromTypes });
+    in(onFinised?: boolean): void;
+    out(onFinised?: boolean): void;
+    getAnimations(): object;
 }
 
-export class ModalContent extends React.Component<ModalContentProps> { }
-export class ModalFooter extends React.Component<ModalFooterProps> { }
-export class ModalButton extends React.Component<ModalButtonProps> { }
-export class ModalTitle extends React.Component<ModalTitleProps> { }
-export class Backdrop extends React.Component<BackdropProps> { }
-export class DraggableView extends React.Component<DraggableViewProps> { }
-export default class Modal extends React.Component<ModalProps> {
-    show(): void;
-    dismiss(): void;
-    modalSize: { width: number, height: number };
-    pointerEvents: 'auto' | 'none';
+export class ModalContent extends React.Component<ModalContentProps> {}
+export class ModalFooter extends React.Component<ModalFooterProps> {}
+export class ModalButton extends React.Component<ModalButtonProps> {}
+export class ModalTitle extends React.Component<ModalTitleProps> {}
+export class Backdrop extends React.Component<BackdropProps> {}
+export class DraggableView extends React.Component<DraggableViewProps> {}
+export class ModalPortal extends React.Component {
+    get ref(): Modal;
+    get size(): number;
+    get current(): Modal;
+    static show(children: React.ReactNode, props?: ModalProps): Modal;
+    static update(key: any, props?: ModalProps): void;
+    static dismiss(key: any): void;
+    static dismissAll(): void;
 }
+export class Modal extends React.Component<ModalProps> {}
+export default Modal;
