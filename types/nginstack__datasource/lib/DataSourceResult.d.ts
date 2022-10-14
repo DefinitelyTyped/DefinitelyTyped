e653701c41914442b79dc33b085ad8aa21bc6f12
Renato <renato@innovaro.com.br>
2022-10-14 11:38:37 -0300

    feat: update @nginstack/datasource definitions

--- types/nginstack__datasource/lib/DataSourceResult.d.ts
+++ types/nginstack__datasource/lib/DataSourceResult.d.ts
@@ -1,22 +1,29 @@
 export = DataSourceResult;
 declare function DataSourceResult(
-    dataSource: any,
-    filters: any[] | any[] | DataSourceFilters,
-    columns: any[] | any[] | DataSourceColumns
+    dataSource: DataSource,
+    filters: DataSourceFilterDef[] | any[] | DataSourceFilters,
+    columns: DataSourceColumnDef[] | any[] | DataSourceColumns
 ): void;
 declare class DataSourceResult {
     constructor(
-        dataSource: any,
-        filters: any[] | any[] | DataSourceFilters,
-        columns: any[] | any[] | DataSourceColumns
+        dataSource: DataSource,
+        filters: DataSourceFilterDef[] | any[] | DataSourceFilters,
+        columns: DataSourceColumnDef[] | any[] | DataSourceColumns
     );
     private dataSource_;
     private filters_;
     private columns_;
-    dataSource: any;
+    dataSource: DataSource;
     filters: DataSourceFilters;
     columns: DataSourceColumns;
-    getDataSet(): any;
+    getDataSet(): DataSet;
 }
+declare namespace DataSourceResult {
+    export { DataSet, DataSource, DataSourceFilterDef, DataSourceColumnDef };
+}
+type DataSource = import('./DataSource');
+type DataSourceFilterDef = import('./DataSourceFilterDef');
 import DataSourceFilters = require('./DataSourceFilters.js');
+type DataSourceColumnDef = import('./DataSourceColumnDef');
 import DataSourceColumns = require('./DataSourceColumns.js');
+type DataSet = import('@nginstack/engine/lib/dataset/DataSet');
