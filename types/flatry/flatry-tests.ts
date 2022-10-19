4c76e3913539510253b5c01470e5d51532c3442b
po5epT <megastorm87@mail.ru>
2022-10-19 17:32:01 +0400

    refactor: Adds a test that demonstrates the correct type of res variable

--- types/flatry/flatry-tests.ts
+++ types/flatry/flatry-tests.ts
@@ -7,6 +7,16 @@ async function testAsync() {
   const result = flatry(asyncFunction()); // $ExpectType Promise<[unknown, never] | [null, string]>
 }
 
+async function testAnotherAsync() {
+  const [err, res] = await flatry(asyncFunction()); // $ExpectType [unknown, never] | [null, string]
+
+  if (err) {
+    // ...
+  } else {
+    const data = res; // $ExpectType string
+  }
+}
+
 function testSync() {
   const result = flatry(syncFunction); // $ExpectType [unknown, never] | [null, boolean]
 }
