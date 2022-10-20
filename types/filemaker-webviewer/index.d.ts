b13e85524bfe730b16ea162c923e7042f7e884d1
Eric <37158449+eluce2@users.noreply.github.com>
2022-10-19 19:11:05 -0500

    adjust option type

--- types/filemaker-webviewer/index.d.ts
+++ types/filemaker-webviewer/index.d.ts
@@ -3,20 +3,16 @@
 // Definitions by: Eric Luce <https://github.com/eluce2>
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
 
-export enum FMScriptOption {
-    CONTINUE,
-    HALT,
-    EXIT,
-    RESUME,
-    PAUSE,
-    SUSPEND_AND_RESUME,
-}
-
+export {};
 declare global {
     interface Window {
         FileMaker?: {
             PerformScript: (name: string, parameter: string) => void;
-            PerformScriptWithOption: (name: string, parameter: string, option: FMScriptOption) => void;
+            PerformScriptWithOption: (
+                name: string,
+                parameter: string,
+                option: '0' | '1' | '2' | '3' | '4' | '5',
+            ) => void;
         };
     }
 }
