d4dfc59944712a35e466cb0848f8ac2461988757
Rhuan Barreto <rhuan@rhuan.com.br>
2022-10-20 15:22:56 +0200

    Update koa-cash-tests.ts

--- types/koa-cash/koa-cash-tests.ts
+++ types/koa-cash/koa-cash-tests.ts
@@ -7,7 +7,7 @@ app.use(koaCash());
 app.use(async ctx => {
     // this response is already cashed if `true` is returned,
     // so this middleware will automatically serve this response from cache
-    if (await ctx.cashed()) return;
+    if (await ctx.cashed()) return ctx.cashClear('/');
 
     // set the response body here,
     // and the upstream middleware will automatically cache it
