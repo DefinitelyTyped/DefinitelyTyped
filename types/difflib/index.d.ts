0109ddf9f52c861c466ed98ef259eebf1e2d54e8
Hugues Le Gendre <hugues@le-gendre.com>
2022-10-20 16:08:10 +0200

    Update index.d.ts

--- types/difflib/index.d.ts
+++ types/difflib/index.d.ts
@@ -1,6 +1,6 @@
-// Type definitions for difflib 0.2
+// Type definitions for difflib 0.2.4
 // Project: https://github.com/qiao/difflib.js
-// Definitions by: majames <https://github.com/majames>
+// Definitions by: majames <https://github.com/majames>, Hugues Le Gendre <https://github.com/hlegendre>
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
 // TypeScript Version: 3.4
 
@@ -21,3 +21,32 @@ export class SequenceMatcher<T> {
         ]
     >;
 }
+
+export class Differ {
+    constructor(
+        linejunk?: (s: string) => boolean, charjunk?: (s: string) => boolean
+    );
+    compare(a: string[], b: string[] ): string[];
+}
+
+export function unifiedDiff(from: string[], to: string[], args: {
+    fromfile?: string
+    tofile?: string,
+    fromfiledate?: string,
+    tofiledate?: string
+    lineterm?: string,
+}): string[];
+
+export function contextDiff(from: string[], to: string[], args: {
+    fromfile?: string
+    tofile?: string,
+    fromfiledate?: string,
+    tofiledate?: string
+    lineterm?: string,
+}): string[];
+
+export function ndiff(from: string[], to: string[], linejunk?: (s: string) => boolean, charjunk?: (s: string) => boolean): string[];
+
+export function restore(delta: string[], which: 1 | 2): string[];
+
+export function getCloseMatches(word: string, possibilities: string[], n?: number, cutoff?: number): string[];
