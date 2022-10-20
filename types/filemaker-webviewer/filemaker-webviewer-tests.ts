dcac00b9b38a41bc348025b81918d085af3c5b82
Eric <37158449+eluce2@users.noreply.github.com>
2022-10-19 19:26:09 -0500

    adjust test

--- types/filemaker-webviewer/filemaker-webviewer-tests.ts
+++ types/filemaker-webviewer/filemaker-webviewer-tests.ts
@@ -1,10 +1,15 @@
 /// <reference lib="dom" />
 
-import { FMScriptOption } from 'filemaker-webviewer';
-
 if (window.FileMaker) {
     window.FileMaker?.PerformScript('scriptName', 'parameter');
-    window.FileMaker?.PerformScriptWithOption('scriptName', 'parameter', FMScriptOption.CONTINUE);
+    window.FileMaker?.PerformScriptWithOption('scriptName', 'parameter', '0');
+    window.FileMaker?.PerformScriptWithOption('scriptName', 'parameter', '1');
+    window.FileMaker?.PerformScriptWithOption('scriptName', 'parameter', '2');
+    window.FileMaker?.PerformScriptWithOption('scriptName', 'parameter', '3');
+    window.FileMaker?.PerformScriptWithOption('scriptName', 'parameter', '4');
+    window.FileMaker?.PerformScriptWithOption('scriptName', 'parameter', '5');
+    // @ts-expect-error "6" is not a valid option
+    window.FileMaker?.PerformScriptWithOption('scriptName', 'parameter', '6');
 }
 
 // prettier-ignore
