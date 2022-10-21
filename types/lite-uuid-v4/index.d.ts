9ce274b1a6fd210a958cf954b5198a6303cd1493
Matt Stephens <87805115+mstephen19@users.noreply.github.com>
2022-10-21 20:32:13 +0200

    fix lint

--- types/lite-uuid-v4/index.d.ts
+++ types/lite-uuid-v4/index.d.ts
@@ -1,4 +1,4 @@
-// Type definitions for lite-uuid-v4 1.0.0
+// Type definitions for lite-uuid-v4 1.0
 // Project: https://github.com/liesislukas/lite-uuid-v4
 // Definitions by: Matt Stephens <https://github.com/mstephen19>
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
