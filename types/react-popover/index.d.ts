9a1ed44de4551a30889b917633031a836afbd29d
jrichil <104026466+jrichil@users.noreply.github.com>
2022-10-21 23:19:05 -0400

    Modify react-popover for react@18
    add children to props interface

--- types/react-popover/index.d.ts
+++ types/react-popover/index.d.ts
@@ -34,5 +34,6 @@ declare namespace Popover {
         style?: React.CSSProperties | undefined;
         target?: React.ReactElement | undefined;
         appendTarget?: Element | undefined;
+        chidren?: React.ReactNode;
     }
 }
