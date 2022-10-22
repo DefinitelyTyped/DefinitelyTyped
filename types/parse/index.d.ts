f56f173f52d8b273859df776744235ba983621f6
Switt Kongdachalert <switt1995@gmail.com>
2022-10-22 17:23:35 +0700

    added new error codes and error message types from 2.19.0

--- types/parse/index.d.ts
+++ types/parse/index.d.ts
@@ -31,60 +31,189 @@
 import { EventEmitter } from 'events';
 
 declare enum ErrorCode {
+    /** Error code indicating some error other than those enumerated here */
     OTHER_CAUSE = -1,
+    /** Error code indicating that something has gone wrong with the server. */
     INTERNAL_SERVER_ERROR = 1,
+    /** Error code indicating the connection to the Parse servers failed. */
     CONNECTION_FAILED = 100,
+    /** Error code indicating the specified object doesn't exist. */
     OBJECT_NOT_FOUND = 101,
+    /**
+     * Error code indicating you tried to query with a datatype that doesn't
+     * support it, like exact matching an array or object.
+     */
     INVALID_QUERY = 102,
+    /*
+    * Error code indicating a missing or invalid classname. Classnames are
+    * case-sensitive. They must start with a letter, and a-zA-Z0-9_ are the
+    * only valid characters.
+    */
     INVALID_CLASS_NAME = 103,
+    /** Error code indicating an unspecified object id. */
     MISSING_OBJECT_ID = 104,
+    /**
+     * Error code indicating an invalid key name. Keys are case-sensitive. They
+     * must start with a letter, and a-zA-Z0-9_ are the only valid characters.
+     */
     INVALID_KEY_NAME = 105,
+    /**
+     * Error code indicating a malformed pointer. You should not see this unless
+     * you have been mucking about changing internal Parse code.
+     */
     INVALID_POINTER = 106,
+    /*
+     * Error code indicating that badly formed JSON was received upstream. This
+     * either indicates you have done something unusual with modifying how
+     * things encode to JSON, or the network is failing badly.
+     */
     INVALID_JSON = 107,
+    /**
+     * Error code indicating that the feature you tried to access is only
+     * available internally for testing purposes.
+     */
     COMMAND_UNAVAILABLE = 108,
+    /** You must call Parse.initialize before using the Parse library. */
     NOT_INITIALIZED = 109,
+    /** Error code indicating that a field was set to an inconsistent type. */
     INCORRECT_TYPE = 111,
+    /**
+     * Error code indicating an invalid channel name. A channel name is either
+     * an empty string (the broadcast channel) or contains only a-zA-Z0-9_
+     * characters and starts with a letter.
+     */
     INVALID_CHANNEL_NAME = 112,
+    /** Error code indicating that push is misconfigured. */
     PUSH_MISCONFIGURED = 115,
+    /** Error code indicating that the object is too large. */
     OBJECT_TOO_LARGE = 116,
+    /** Error code indicating that the operation isn't allowed for clients. */
     OPERATION_FORBIDDEN = 119,
+    /** Error code indicating the result was not found in the cache. */
     CACHE_MISS = 120,
+    /** Error code indicating that an invalid key was used in a nested JSONObject. */
     INVALID_NESTED_KEY = 121,
+    /**
+     * Error code indicating that an invalid filename was used for ParseFile.
+     * A valid file name contains only a-zA-Z0-9_. characters and is between 1
+     * and 128 characters.
+     */
     INVALID_FILE_NAME = 122,
+    /** Error code indicating an invalid ACL was provided. */
     INVALID_ACL = 123,
+    /**
+     * Error code indicating that the request timed out on the server. Typically
+     * this indicates that the request is too expensive to run.
+     */
     TIMEOUT = 124,
+    /** Error code indicating that the email address was invalid. */
     INVALID_EMAIL_ADDRESS = 125,
+    /** Error code indicating a missing content type. */
     MISSING_CONTENT_TYPE = 126,
+    /** Error code indicating a missing content length. */
     MISSING_CONTENT_LENGTH = 127,
+    /** Error code indicating an invalid content length. */
     INVALID_CONTENT_LENGTH = 128,
+    /** Error code indicating a file that was too large. */
     FILE_TOO_LARGE = 129,
+    /** Error code indicating an error saving a file. */
     FILE_SAVE_ERROR = 130,
+    /**
+     * Error code indicating that a unique field was given a value that is
+     * already taken.
+     */
     DUPLICATE_VALUE = 137,
+    /** Error code indicating that a role's name is invalid. */
     INVALID_ROLE_NAME = 139,
+    /**
+     * Error code indicating that an application quota was exceeded.
+     * Upgrade to resolve.
+     */
     EXCEEDED_QUOTA = 140,
+    /** Error code indicating that a Cloud Code script failed. */
     SCRIPT_FAILED = 141,
+    /** Error code indicating that a Cloud Code validation failed. */
     VALIDATION_ERROR = 142,
+    /** Error code indicating that invalid image data was provided. */
     INVALID_IMAGE_DATA = 150,
+    /** Error code indicating an unsaved file. */
     UNSAVED_FILE_ERROR = 151,
+    /** Error code indicating an invalid push time. */
     INVALID_PUSH_TIME_ERROR = 152,
+    /** Error code indicating an error deleting a file. */
     FILE_DELETE_ERROR = 153,
+    /** Error code indicating that the application has exceeded its request limit. */
     REQUEST_LIMIT_EXCEEDED = 155,
+    /**
+     * Error code indicating that the request was a duplicate and has been discarded due to
+     * idempotency rules.
+     */
+    DUPLICATE_REQUEST = 159,
+    /** Error code indicating an invalid event name. */
     INVALID_EVENT_NAME = 160,
+    /** Error code indicating an error deleting an unnamed file. */
+    FILE_DELETE_UNNAMED_ERROR = 161,
+    /** Error code indicating that the username is missing or empty. */
     USERNAME_MISSING = 200,
+    /** Error code indicating that the password is missing or empty. */
     PASSWORD_MISSING = 201,
+    /** Error code indicating that the username has already been taken. */
     USERNAME_TAKEN = 202,
+    /** Error code indicating that the email has already been taken. */
     EMAIL_TAKEN = 203,
+    /** Error code indicating that the email is missing, but must be specified. */
     EMAIL_MISSING = 204,
+    /** Error code indicating that a user with the specified email was not found. */
     EMAIL_NOT_FOUND = 205,
+    /**
+     * Error code indicating that a user object without a valid session could
+     * not be altered.
+     */
     SESSION_MISSING = 206,
+    /** Error code indicating that a user can only be created through signup. */
     MUST_CREATE_USER_THROUGH_SIGNUP = 207,
+    /**
+     * Error code indicating that an an account being linked is already linked
+     * to another user.
+     */
     ACCOUNT_ALREADY_LINKED = 208,
+    /** Error code indicating that the current session token is invalid. */
     INVALID_SESSION_TOKEN = 209,
+    /** Error code indicating an error enabling or verifying MFA */
+    MFA_ERROR = 210,
+    /** Error code indicating that a valid MFA token must be provided */
+    MFA_TOKEN_REQUIRED = 211,
+    /**
+     * Error code indicating that a user cannot be linked to an account because
+     * that account's id could not be found.
+     */
     LINKED_ID_MISSING = 250,
+    /**
+     * Error code indicating that a user with a linked (e.g. Facebook) account
+     * has an invalid session.
+     */
     INVALID_LINKED_SESSION = 251,
+    /**
+     * Error code indicating that a service being linked (e.g. Facebook or
+     * Twitter) is unsupported.
+     */
     UNSUPPORTED_SERVICE = 252,
+    /** Error code indicating an invalid operation occured on schema */
+    INVALID_SCHEMA_OPERATION = 255,
+    /**
+     * Error code indicating that there were multiple errors. Aggregate errors
+     * have an "errors" property, which is an array of error objects with more
+     * detail about each error that occurred.
+     */
     AGGREGATE_ERROR = 600,
+    /** Error code indicating the client was unable to read an input file. */
     FILE_READ_ERROR = 601,
+    /*
+     * Error code indicating a real error code is unavailable because
+     * we had to use an XDomainRequest object to allow CORS requests in
+     * Internet Explorer, which strips the body from HTTP responses that have
+     * a non-2XX status code.
+     */
     X_DOMAIN_REQUEST = 602,
 }
 
