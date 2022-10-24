daebbc29222fde883701c7dff6d4b417401cb84c
shinzenatt <alen.mukaca@gmail.com>
2022-10-25 00:37:29 +0200

    added types for fluent-reveal-effect

---
+++ types/fluent-reveal-effect/index.d.ts
@@ -0,0 +1,20 @@
+declare module 'fluent-reveal-effect' {
+    interface FluentRevealEffectType {
+        applyEffect: (element: string, options: RevealOptions) => void;
+    }
+
+    interface RevealOptions{
+        lightColor?: string | "rgba(255,255,255,0.25)",
+        gradientSize?: number | 150,
+        clickEffect?: boolean | false,
+        isContainer?: boolean | false,
+        children?: {
+            borderSelector?: string | ".eff-reveal-border",
+            elementSelector?: string | ".eff-reveal",
+            lightColor?: string | "rgba(255,255,255,0.25)",
+            gradientSize?: number | 150
+        }
+    }
+
+    export const FluentRevealEffect: FluentRevealEffectType;
+}
