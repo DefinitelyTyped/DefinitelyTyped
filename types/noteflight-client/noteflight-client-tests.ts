c0784b13867b06aad0941ffcc723c2dba4f87691
Nicholas Epps <nicholasepps03@gmail.com>
2022-10-23 20:26:07 -0400

    created type file for noteflight-client API.

---
+++ types/noteflight-client/noteflight-client-tests.ts
@@ -0,0 +1,15 @@
+NFClient.init(function(info) {
+    console.log(info);
+  });
+  
+  const scoreView = new NFClient.ScoreView('score1',  'fcfd6d0bc0770f67cdbe1b8129456521fec090a0', {
+    viewParams: {
+      role: 'template'
+    }
+  });
+  
+  scoreView.addEventListener('playbackRequest', (event) => {
+    console.log(event.index, event.offset);
+    scoreView.printScore();
+    scoreView.getScore().done(result => console.log(result));
+  })
