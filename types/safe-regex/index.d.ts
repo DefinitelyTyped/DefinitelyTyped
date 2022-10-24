42aa8025ea96a1487a87761eb8521d4691533682
Rodry <38259440+ImRodry@users.noreply.github.com>
2022-10-24 23:32:46 +0100

    feat(safe-regex): add tests

--- types/safe-regex/index.d.ts
+++ types/safe-regex/index.d.ts
@@ -3,11 +3,12 @@
 // Definitions by: Mohamed Hegazy <https://github.com/mhegazy>
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
 
-export = safe;
 /**
- * 
+ * Checks if a regex is safe to use in order to prevent catastrophic backtracking.
  * @param re can be a RegExp object or just a string.
  * @param opts Options for the check.
  * `limit` - maximum number of allowed repetitions in the entire regex. Default: `25`.
  */
 declare function safe(re: string | RegExp, opts?: { limit?: number }): boolean;
+
+export = safe;
