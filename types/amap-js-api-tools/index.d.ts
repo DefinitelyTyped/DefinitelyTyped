89f2bffdeda4524663a3b5d423230078b488f369
aak1247 <aak1247@126.com>
2022-10-24 10:05:22 +0800

    style: remove unneeded ts language version declaration

--- types/amap-js-api-tools/index.d.ts
+++ types/amap-js-api-tools/index.d.ts
@@ -2,7 +2,6 @@
 // Project: https://lbs.amap.com/api/jsapi-v2/documentation
 // Definitions by: aak1247 <https://github.com/aak1247>
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
-// TypeScript Version: 2.8
 
 /// <reference types="amap-js-api" />
 
