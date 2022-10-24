46668cb4a89f8a355ed2f02d503599778e8bb273
shinzenatt <alen.mukaca@gmail.com>
2022-10-25 01:27:48 +0200

    addressed validation errors for fluent-reveal-effect

--- types/fluent-reveal-effect/index.d.ts
+++ types/fluent-reveal-effect/index.d.ts
@@ -1,20 +1,25 @@
-declare module 'fluent-reveal-effect' {
-    interface FluentRevealEffectType {
-        applyEffect: (element: string, options: RevealOptions) => void;
-    }
+// Type definitions for fluent-reveal-effect 2.0
+// Project: https://www.npmjs.com/package/fluent-reveal-effect
+// Definitions by: Alen Mukaca <https://github.com/ShinzenATT>
+// Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
 
-    interface RevealOptions{
-        lightColor?: string | "rgba(255,255,255,0.25)",
-        gradientSize?: number | 150,
-        clickEffect?: boolean | false,
-        isContainer?: boolean | false,
-        children?: {
-            borderSelector?: string | ".eff-reveal-border",
-            elementSelector?: string | ".eff-reveal",
-            lightColor?: string | "rgba(255,255,255,0.25)",
-            gradientSize?: number | 150
-        }
-    }
-
-    export const FluentRevealEffect: FluentRevealEffectType;
+export interface FluentRevealEffectType {
+    applyEffect: (element: string, options: RevealOptions) => void;
 }
+
+export interface RevealOptions {
+    lightColor?: string | "rgba(255,255,255,0.25)";
+    gradientSize?: number | 150;
+    clickEffect?: boolean | false;
+    isContainer?: boolean | false;
+    children?: {
+        borderSelector?: string | ".eff-reveal-border";
+        elementSelector?: string | ".eff-reveal";
+        lightColor?: string | "rgba(255,255,255,0.25)";
+        gradientSize?: number | 150;
+    };
+}
+
+export const FluentRevealEffect: FluentRevealEffectType;
+
+export {};
