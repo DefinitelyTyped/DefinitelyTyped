13588fd19484e65a9188433439b4ecc5d6c58616
Rodry <38259440+ImRodry@users.noreply.github.com>
2022-10-24 23:16:10 +0100

    feat(safe-regex): update to latest version and add jsdoc

--- types/safe-regex/index.d.ts
+++ types/safe-regex/index.d.ts
@@ -3,5 +3,11 @@
 // Definitions by: Mohamed Hegazy <https://github.com/mhegazy>
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
 
-export = safe_regex;
-declare function safe_regex(re: string | RegExp, opts?: { limit?: number | undefined }): boolean;
+export = safe;
+/**
+ * 
+ * @param re can be a RegExp object or just a string.
+ * @param opts Options for the check.
+ * `limit` - maximum number of allowed repetitions in the entire regex. Default: `25`.
+ */
+declare function safe(re: string | RegExp, opts?: { limit?: number }): boolean;
