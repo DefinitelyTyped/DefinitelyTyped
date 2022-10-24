e85fa1eed6f158b7033aeef5eb88c2823caa8dd7
Sebastian Silbermann <sebastian.silbermann@klarna.com>
2022-10-24 09:28:27 +0200

    react: Update types for `next` release channel

--- types/react/next.d.ts
+++ types/react/next.d.ts
@@ -27,4 +27,51 @@ import React = require('.');
 
 export {};
 
-declare module '.' {}
+declare module '.' {
+    interface ThenableImpl<T> {
+        then(onFulfill: (value: T) => unknown, onReject: (error: unknown) => unknown): void | PromiseLike<unknown>;
+    }
+    interface UntrackedThenable<T> extends ThenableImpl<T> {
+        status?: void;
+    }
+
+    export interface PendingThenable<T> extends ThenableImpl<T> {
+        status: 'pending';
+    }
+
+    export interface FulfilledThenable<T> extends ThenableImpl<T> {
+        status: 'fulfilled';
+        value: T;
+    }
+
+    export interface RejectedThenable<T> extends ThenableImpl<T> {
+        status: 'rejected';
+        reason: unknown;
+    }
+
+    export type Thenable<T> = UntrackedThenable<T> | PendingThenable<T> | FulfilledThenable<T> | RejectedThenable<T>;
+
+    export type Usable<T> = Thenable<T> | Context<T>;
+
+    export function use<T>(usable: Usable<T>): T;
+
+    interface ServerContextJSONArray extends ReadonlyArray<ServerContextJSONArray> {}
+    export type ServerContextJSONValue =
+        | string
+        | boolean
+        | number
+        | null
+        | ServerContextJSONArray
+        | { [key: string]: ServerContextJSONValue };
+    export interface ServerContext<T extends ServerContextJSONValue> {
+        Provider: Provider<T>;
+    }
+    function useContext<T extends ServerContextJSONValue>(context: ServerContext<T>): T;
+    export function createServerContext<T extends ServerContextJSONValue>(
+        globalName: string,
+        defaultValue: T,
+    ): ServerContext<T>;
+
+    // tslint:disable-next-line ban-types
+    export function cache<CachedFunction extends Function>(fn: CachedFunction): CachedFunction;
+}
--- types/react/test/next.tsx
+++ types/react/test/next.tsx
@@ -1,1 +1,58 @@
 /// <reference types="../next"/>
+
+
+const contextUsers = React.createContext(['HAL']);
+const promisedUsers = Promise.resolve(['Dave']);
+
+function useUse() {
+    // @ts-expect-error Missing value
+    React.use();
+
+    // $ExpectType string[]
+    const users = React.use(promisedUsers);
+    // @ts-expect-error incompatible type. Mainly to potentially inspect TypeScript error message
+    React.use({});
+
+    // $ExpectType string[]
+    const contextValue = React.use(contextUsers);
+}
+
+function serverContextTest() {
+    const ServerContext = React.createServerContext<string>('ServerContext', 'default');
+
+    function ServerContextUser() {
+        const context = React.useContext(ServerContext);
+        return <React.Fragment>{context}</React.Fragment>;
+    }
+    // @ts-expect-error Consumer pattern is not supported on server context
+    ServerContext.Consumer;
+
+    function ServerContextProivder() {
+        return (
+            <ServerContext.Provider value="provided">
+                <ServerContextUser />
+            </ServerContext.Provider>
+        );
+    }
+
+    // plain objects work
+    React.createServerContext('PlainObjectContext', { foo: 1 });
+    // @ts-expect-error Incompatible with JSON stringify+parse
+    React.createServerContext('DateContext', new Date());
+    // @ts-expect-error Incompatible with JSON stringify+parse
+    React.createServerContext('SetContext', new Set());
+}
+
+function cacheTest() {
+    const getLength = React.cache((a: string) => a.length);
+    const fooLength: number = getLength('foo');
+    getLength(
+        // @ts-expect-error -- number not assignable to string
+        133,
+    );
+
+    React.cache(
+        // @ts-expect-error implicit any
+        a => a,
+    );
+}
