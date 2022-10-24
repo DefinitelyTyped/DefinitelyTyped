414ad6af2ee0d3b4a1d148ac285dfaedb520f262
Simon Feiden <simon.feiden@real-digital.de>
2022-10-24 13:33:50 +0200

    Reduce scope of tslint disable

--- types/graphql-upload/graphqlUploadKoa.d.ts
+++ types/graphql-upload/graphqlUploadKoa.d.ts
@@ -1,10 +1,8 @@
 import { DefaultContext, DefaultState, Middleware } from 'koa';
 import { UploadOptions } from './';
 
-/* tslint:disable:no-unnecessary-generics */
-
 export { UploadOptions };
 
 export default function graphqlUploadKoa<StateT = DefaultState, ContextT = DefaultContext>(
     uploadOptions?: UploadOptions,
-): Middleware<StateT, ContextT>;
+): Middleware<StateT, ContextT>; // tslint:disable-line:no-unnecessary-generics
