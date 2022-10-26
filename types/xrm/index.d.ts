b90913c3b32adbafc83e0a661e93e31fd632346b
Vladimir Lapacek <vlapacek@microsoft.com>
2022-10-26 13:52:41 +0200

    Add tabName parameter to PageInputEntityRecord

--- types/xrm/index.d.ts
+++ types/xrm/index.d.ts
@@ -5303,8 +5303,12 @@ declare namespace Xrm {
             /**
              * ID of the selected stage in business process instance.
              */
-            selectedStageId?: string | undefined;
-        }
+             selectedStageId?: string | undefined;
+            /**
+             * Sets the focus on the tab of the form.
+             */
+             tabName?: string | undefined;
+            }
 
         interface PageInputEntityList {
             pageType: "entitylist";
