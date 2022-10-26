aaa3d2500fcf7913b7544ff43b03117d28b75711
sfilata <shubaiqiao@126.com>
2022-10-26 10:56:33 +0800

    Fix: Fix the PR review problem.

--- types/nkzq__copy-paste/nkzq__copy-paste-tests.ts
+++ types/nkzq__copy-paste/nkzq__copy-paste-tests.ts
@@ -5,7 +5,6 @@ class TestClass {}
 let strRet: string  = CopyPaste.copy("content");
 strRet = CopyPaste.copy("content", (err: Error) => { return; });
 
-
 let objRet: TestClass  = CopyPaste.copy(new TestClass());
 objRet = CopyPaste.copy(new TestClass(), (err: Error) => { return; });
 
