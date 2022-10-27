ef6344cbef10b5be3063c9c3c4defe96910ebcf5
Jean-Baptiste Crestot <jbcrestot@gmail.com>
2022-10-27 10:15:05 +0200

    put back undefined when necessary

--- types/react-native-modals/index.d.ts
+++ types/react-native-modals/index.d.ts
@@ -27,104 +27,101 @@ export interface DragEvent {
 
 export interface ModalContentProps {
     children?: React.ReactNode;
-    style?: StyleProp<ViewStyle>;
+    style?: StyleProp<ViewStyle> | undefined;
 }
 
 export interface BackdropProps {
     visible: boolean;
     opacity: number;
-    onPress?: () => void;
-    backgroundColor?: string;
-    animationDuration?: number;
-    pointerEvents?: string;
-    useNativeDriver?: boolean;
+    onPress?: () => void | undefined;
+    backgroundColor?: string | undefined;
+    animationDuration?: number | undefined;
+    pointerEvents?: string | undefined;
+    useNativeDriver?: boolean | undefined;
 }
 
 export interface ModalFooterProps {
-    bordered?: boolean;
+    bordered?: boolean | undefined;
     children?: React.ReactNode;
-    style?: StyleProp<ViewStyle>;
+    style?: StyleProp<ViewStyle> | undefined;
 }
 
 export interface ModalButtonProps {
     text: string;
     onPress: () => void;
-    align?: AlignTypes;
-    style?: StyleProp<ViewStyle>;
-    textStyle?: StyleProp<TextStyle>;
-    disabled?: boolean;
-    activeOpacity?: number;
-    bordered?: boolean;
+    align?: AlignTypes | undefined;
+    style?: StyleProp<ViewStyle> | undefined;
+    textStyle?: StyleProp<TextStyle> | undefined;
+    disabled?: boolean | undefined;
+    activeOpacity?: number | undefined;
+    bordered?: boolean | undefined;
 }
 
 export interface ModalTitleProps {
     title: string;
-    style?: StyleProp<ViewStyle>;
-    textStyle?: StyleProp<TextStyle>;
-    align?: AlignTypes;
-    hasTitleBar?: boolean;
+    style?: StyleProp<ViewStyle> | undefined;
+    textStyle?: StyleProp<TextStyle> | undefined;
+    align?: AlignTypes | undefined;
+    hasTitleBar?: boolean | undefined;
 }
 
 export interface ModalProps {
     children?: React.ReactNode;
-    visible?: boolean;
-    width?: number;
-    height?: number;
-    rounded?: boolean;
-    hasOverlay?: boolean;
-    overlayPointerEvents?: OverlayPointerEventTypes;
-    overlayBackgroundColor?: string;
-    overlayOpacity?: number;
-    modalTitle?: React.ReactNode;
-    modalAnimation?: FadeAnimation | ScaleAnimation | SlideAnimation;
-    modalStyle?: StyleProp<ViewStyle>;
-    style?: StyleProp<ViewStyle>;
-    animationDuration?: number;
-    onTouchOutside?: () => void;
-    onHardwareBackPress?: () => boolean;
-    onShow?: () => void;
-    onDismiss?: () => void;
-    footer?: React.ReactNode;
-    onMove?: (event: DragEvent) => void;
-    onSwiping?: (event: DragEvent) => void;
-    onSwipeRelease?: (event: DragEvent) => void;
-    onSwipingOut?: (event: DragEvent) => void;
-    onSwipeOut?: (event: DragEvent) => void;
-    swipeDirection?: SwipeDirection | SwipeDirection[];
-    swipeThreshold?: number;
-    useNativeDriver?: boolean;
+    visible?: boolean | undefined;
+    width?: number | undefined;
+    height?: number | undefined;
+    rounded?: boolean | undefined;
+    hasOverlay?: boolean | undefined;
+    overlayPointerEvents?: OverlayPointerEventTypes | undefined;
+    overlayBackgroundColor?: string | undefined;
+    overlayOpacity?: number | undefined;
+    modalTitle?: React.ReactNode | undefined;
+    modalAnimation?: FadeAnimation | ScaleAnimation | SlideAnimation | undefined;
+    modalStyle?: StyleProp<ViewStyle> | undefined;
+    style?: StyleProp<ViewStyle> | undefined;
+    animationDuration?: number | undefined;
+    onTouchOutside?: () => void | undefined;
+    onHardwareBackPress?: () => boolean | undefined;
+    onShow?: () => void | undefined;
+    onDismiss?: () => void | undefined;
+    footer?: React.ReactNode | undefined;
+    onMove?: (event: DragEvent) => void | undefined;
+    onSwiping?: (event: DragEvent) => void | undefined;
+    onSwipeRelease?: (event: DragEvent) => void | undefined;
+    onSwipingOut?: (event: DragEvent) => void | undefined;
+    onSwipeOut?: (event: DragEvent) => void | undefined;
+    swipeDirection?: SwipeDirection | SwipeDirection[] | undefined;
+    swipeThreshold?: number | undefined;
+    useNativeDriver?: boolean | undefined;
 }
 
 export interface DraggableViewProps {
-    style?: StyleProp<ViewStyle>;
-    onMove?: (event: DragEvent) => void;
-    onSwiping?: (event: DragEvent) => void;
-    onRelease?: (event: DragEvent) => void;
-    onSwipingOut?: (event: DragEvent) => void;
-    onSwipeOut?: (event: DragEvent) => void;
-    swipeThreshold?: number;
-    swipeDirection?: SwipeDirection | SwipeDirection[];
+    style?: StyleProp<ViewStyle> | undefined;
+    onMove?: (event: DragEvent) => void | undefined;
+    onSwiping?: (event: DragEvent) => void | undefined;
+    onRelease?: (event: DragEvent) => void | undefined;
+    onSwipingOut?: (event: DragEvent) => void | undefined;
+    onSwipeOut?: (event: DragEvent) => void | undefined;
+    swipeThreshold?: number | undefined;
+    swipeDirection?: SwipeDirection | SwipeDirection[] | undefined;
 }
 
-export class FadeAnimation {
-    constructor(params: { animationDuration?: number; useNativeDriver?: boolean });
+export class Animation {
+    constructor(params: { animationDuration?: number | undefined; useNativeDriver?: boolean | undefined });
     in(onFinised?: boolean): void;
     out(onFinised?: boolean): void;
     getAnimations(): object;
 }
+export class FadeAnimation extends Animation {}
 
-export class ScaleAnimation {
-    constructor(params: { animationDuration?: number; useNativeDriver?: boolean });
-    in(onFinised?: boolean): void;
-    out(onFinised?: boolean): void;
-    getAnimations(): object;
-}
+export class ScaleAnimation extends Animation {}
 
-export class SlideAnimation {
-    constructor(params: { animationDuration?: number; useNativeDriver?: boolean; slideFrom?: SlideFromTypes });
-    in(onFinised?: boolean): void;
-    out(onFinised?: boolean): void;
-    getAnimations(): object;
+export class SlideAnimation extends Animation {
+    constructor(params: {
+        animationDuration?: number | undefined;
+        useNativeDriver?: boolean | undefined;
+        slideFrom?: SlideFromTypes | undefined;
+    });
 }
 
 export class ModalContent extends React.Component<ModalContentProps> {}
