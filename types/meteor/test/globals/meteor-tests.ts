132905036b06f374e88627b0b9aeb518e57d65f0
ToastHawaii <mar.pel@protonmail.com>
2022-10-29 08:08:03 +0200

    meteor fix test

--- types/meteor/test/globals/meteor-tests.ts
+++ types/meteor/test/globals/meteor-tests.ts
@@ -197,7 +197,7 @@ namespace MeteorTests {
     Meteor.call('foo', 1, 2, function (error: any, result: any) {});
     var result = Meteor.call('foo', 1, 2);
 
-    (async function(){
+    (async function() {
         var result = await Meteor.callAsync('foo', 1, 2);
     })();
 
