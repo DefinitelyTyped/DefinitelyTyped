ecbbd0968794729005199a69bec294ce56a2bffd
Sudarsan Balaji <sudarsan.balaji@artfuldev.com>
2022-10-30 14:51:12 +0000

    Improve type definitions

--- types/xhook/index.d.ts
+++ types/xhook/index.d.ts
@@ -13,49 +13,31 @@ export interface Request {
     withCredentials: boolean;
 }
 
-// eslint-disable-next-line @typescript-eslint/ban-types
-export type ResponseType = ArrayBuffer | Document | Blob | Object | string;
+export type ResponseType = ArrayBuffer | Document | Blob | object | string;
 
-export interface Response<T extends ResponseType = string> {
+export interface Response {
     status: number;
     statusText: string;
     text: string;
     headers: Record<string, string>;
     xml: XMLDocument;
-    data: T;
+    data: ResponseType;
 }
 
-export interface EffectBeforeHandler {
-    (request: Request): void;
+export interface BeforeHandler {
+    (request: Request, callback: (response?: Response) => void): Response | void;
 }
 
-export interface EagerBeforeHandler<T extends ResponseType = string> {
-    (request: Request): Response<T>;
+export interface AfterHandler {
+    (request: Request, response: Response, callback: () => void): void;
 }
 
-export interface AsyncEagerBeforeHandler<T extends ResponseType = string> {
-    (request: Request, callback: (response?: Response<T>) => void): void;
-}
-
-export type BeforeHandler<T extends ResponseType = string> =
-    | EffectBeforeHandler
-    | EagerBeforeHandler<T>
-    | AsyncEagerBeforeHandler<T>;
-
-export interface EffectAfterHandler<T extends ResponseType = string> {
-    (request: Request, response: Response<T>): void;
-}
-export interface AsyncEffectAfterHandler<T extends ResponseType = string> {
-    (request: Request, response: Response<T>, callback: () => void): void;
-}
-export type AfterHandler<T extends ResponseType = string> = EffectAfterHandler<T> | AsyncEffectAfterHandler<T>;
-
-type XHook = {
+interface XHook {
     enable: () => void;
     disable: () => void;
-    before: <T extends ResponseType = string>(handler: BeforeHandler<T>, index?: number) => void;
-    after: <T extends ResponseType = string>(handler: AfterHandler<T>, index?: number) => void;
-};
+    before: (handler: BeforeHandler, index?: number) => void;
+    after: (handler: AfterHandler, index?: number) => void;
+}
 
 declare const xhook: XHook;
 
