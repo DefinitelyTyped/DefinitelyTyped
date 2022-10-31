69240812a4273155c1c3513125052df9f56ee18d
Pirasis Leelatanon <1pete@users.noreply.github.com>
2022-10-31 11:03:02 +0700

    update lib version

--- types/react-datepicker/index.d.ts
+++ types/react-datepicker/index.d.ts
@@ -1,4 +1,4 @@
-// Type definitions for react-datepicker 4.4
+// Type definitions for react-datepicker 4.8
 // Project: https://github.com/Hacker0x01/react-datepicker
 // Definitions by: Rajab Shakirov <https://github.com/radziksh>
 //                 Greg Smith <https://github.com/smrq>
