bfb38c3a64a846389a1abd850fd4735e7f331bce
Sudarsan Balaji <sudarsan.balaji@artfuldev.com>
2022-11-01 00:30:45 +0000

    Use namespace and declaration merging

--- types/xhook/index.d.ts
+++ types/xhook/index.d.ts
@@ -3,42 +3,44 @@
 // Definitions by: Sudarsan Balaji <https://github.com/artfuldev>
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
 
-export interface Request {
-    method: string;
-    url: string;
-    body: string;
-    headers: Record<string, string>;
-    timeout: number;
-    type: string;
-    withCredentials: boolean;
-}
+declare namespace xhook {
+    interface Request {
+        method: string;
+        url: string;
+        body: string;
+        headers: Record<string, string>;
+        timeout: number;
+        type: string;
+        withCredentials: boolean;
+    }
 
-export type ResponseType = ArrayBuffer | Document | Blob | object | string;
+    type ResponseType = ArrayBuffer | Document | Blob | object | string;
 
-export interface Response {
-    status: number;
-    statusText: string;
-    text: string;
-    headers: Record<string, string>;
-    xml: XMLDocument;
-    data: ResponseType;
-}
+    interface Response {
+        status: number;
+        statusText: string;
+        text: string;
+        headers: Record<string, string>;
+        xml: XMLDocument;
+        data: ResponseType;
+    }
 
-export interface BeforeHandler {
-    (request: Request, callback: (response?: Response) => void): Response | void;
-}
+    interface BeforeHandler {
+        (request: Request, callback: (response?: Response) => void): Response | void;
+    }
 
-export interface AfterHandler {
-    (request: Request, response: Response, callback: () => void): void;
+    interface AfterHandler {
+        (request: Request, response: Response, callback: () => void): void;
+    }
 }
 
 interface XHook {
     enable: () => void;
     disable: () => void;
-    before: (handler: BeforeHandler, index?: number) => void;
-    after: (handler: AfterHandler, index?: number) => void;
+    before: (handler: xhook.BeforeHandler, index?: number) => void;
+    after: (handler: xhook.AfterHandler, index?: number) => void;
 }
 
 declare const xhook: XHook;
 
-export default xhook;
+export = xhook;
