ba4bea61affde01a168e1ca3aec706f25e132898
Rafael Derolez <raf@derolez.dev>
2022-11-02 14:57:09 +0100

    Add Definition Owner

--- types/react-headroom/index.d.ts
+++ types/react-headroom/index.d.ts
@@ -1,6 +1,7 @@
 // Type definitions for react-headroom 2.2
 // Project: https://kyleamathews.github.io/react-headroom/, https://github.com/kyleamathews/react-headroom
 // Definitions by: Zero Cho <https://github.com/zerocho>
+//                 Rafael Derolez <https://github.com/rafaelderolez>
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
 // TypeScript Version: 2.8
 
