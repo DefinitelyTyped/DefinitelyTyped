9d77bbafadfdeea052a3869330413d12cb1c924e
Rafael Derolez <raf@derolez.dev>
2022-11-02 14:59:13 +0100

    Add pin property to react-headroom types

--- types/react-headroom/index.d.ts
+++ types/react-headroom/index.d.ts
@@ -26,6 +26,7 @@ export interface ReactHeadroomProps {
     disableInlineStyles?: boolean | undefined;
     className?: string | undefined;
     children: ReactNode;
+    pin?: boolean | undefined;
 }
 
 export default Headroom;
