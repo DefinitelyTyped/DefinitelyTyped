696218412e4bd3df7e4e9a9a6c8d4f130671372b
Kent Robin Haugen <github@kentrobin.com>
2022-11-05 18:22:56 +0100

    Added unwind to pipeline aggragtion methods

--- types/parse/index.d.ts
+++ types/parse/index.d.ts
@@ -45,10 +45,10 @@ declare enum ErrorCode {
      */
     INVALID_QUERY = 102,
     /*
-    * Error code indicating a missing or invalid classname. Classnames are
-    * case-sensitive. They must start with a letter, and a-zA-Z0-9_ are the
-    * only valid characters.
-    */
+     * Error code indicating a missing or invalid classname. Classnames are
+     * case-sensitive. They must start with a letter, and a-zA-Z0-9_ are the
+     * only valid characters.
+     */
     INVALID_CLASS_NAME = 103,
     /** Error code indicating an unspecified object id. */
     MISSING_OBJECT_ID = 104,
@@ -1010,6 +1010,15 @@ declare global {
                     | undefined;
                 // Facet documentation: https://docs.mongodb.com/manual/reference/operator/aggregation/facet/
                 facet?: Record<string, Array<Record<string, any>>> | undefined;
+                // Unwind documentation: https://www.mongodb.com/docs/manual/reference/operator/aggregation/unwind/
+                unwind?:
+                    | {
+                          path: string;
+                          includeArrayIndex?: string;
+                          preserveNullAndEmptyArrays?: boolean;
+                      }
+                    | string
+                    | undefined;
             }
 
             // According to https://parseplatform.org/Parse-SDK-JS/api/2.1.0/Parse.Query.html#fullText
