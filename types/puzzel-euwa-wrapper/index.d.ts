5ed7a4796684b0a6a0d25adcd22b8feb165bfb44
Mannuel Ferreira <mannuel.ferreira@gmail.com>
2022-11-08 14:39:59 +0100

    Adds attribution comment

---
+++ types/puzzel-euwa-wrapper/index.d.ts
@@ -0,0 +1,4 @@
+// Type definitions for EUWA Wrapper Interface 1.0.3
+// Project: https://help.puzzel.com/product-documents/user-guide/puzzel-contact-centre/puzzel-administration-portal/services/chat-configuration/euwa-wrapper-interface
+// Definitions by: Mannuel Ferreira <https://github.com/mannuelf>
+// Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
