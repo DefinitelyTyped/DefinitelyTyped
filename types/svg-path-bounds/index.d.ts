cc88a8a19310f389b385ad877568a8517ed870aa
Lars (Mac) <lars@saxofonsolo.dk>
2022-11-10 14:25:56 +0100

    add definitions for svg-path-bounds

---
+++ types/svg-path-bounds/index.d.ts
@@ -0,0 +1,9 @@
+// Type definitions for svg-path-bounds 1.0.2
+// Project: https://www.npmjs.com/package/svg-path-bounds
+// Definitions by: larsmunkholm <https://github.com/larsmunkholm/>
+// Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
+
+declare module "svg-path-bounds" {
+    const content: (path: string) => [number, number, number, number];
+    export default content;
+}
