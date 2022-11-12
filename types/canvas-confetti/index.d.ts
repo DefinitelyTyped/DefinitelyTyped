74cb477267e884288f5b51ce023b6b050007c91a
J Rob Gant <rgant@alum.wpi.edu>
2022-11-12 14:55:04 -0500

    use jsdoc instead of tsdoc

--- types/canvas-confetti/index.d.ts
+++ types/canvas-confetti/index.d.ts
@@ -34,7 +34,7 @@ declare namespace confetti {
     interface Options {
         /**
          * The angle in which to launch the confetti, in degrees. 90 is straight up.
-         * @defaultValue 90
+         * @default 90
          */
         angle?: number | undefined;
         /**
@@ -44,23 +44,23 @@ declare namespace confetti {
         /**
          * How quickly the confetti will lose speed. Keep this number between 0 and 1, otherwise the confetti will gain speed. Better yet,
          * just never change it.
-         * @defaultValue 0.9
+         * @default 0.9
          */
         decay?: number | undefined;
         /**
          * Disables confetti entirely for users that prefer reduced motion. The confetti() promise will resolve immediately in this case.
-         * @defaultValue false
+         * @default false
          */
         disableForReducedMotion?: boolean | undefined;
         /**
          * How much to the side the confetti will drift. The default is 0, meaning that they will fall straight down.
          * Use a negative number for left and positive number for right
-         * @defaultValue 0
+         * @default 0
          */
         drift?: number | undefined;
         /**
          * How quickly the particles are pulled down. 1 is full gravity, 0.5 is half gravity, etc., but there are no limits.
-         * @defaultValue 1
+         * @default 1
          */
         gravity?: number | undefined;
         /**
@@ -69,38 +69,38 @@ declare namespace confetti {
         origin?: Origin | undefined;
         /**
          * The number of confetti to launch. More is always fun... but be cool, there's a lot of math involved.
-         * @defaultValue 50
+         * @default 50
          */
         particleCount?: number | undefined;
         /**
          * Scale factor for each confetti particle. Use decimals to make the confetti smaller.
-         * @defaultValue 1
+         * @default 1
          */
         scalar?: number | undefined;
         /**
          * The possible values are square, circle, and star. The default is to use both squares and circles in an even mix.
-         * @defaultValue ['square','circle']
+         * @default ['square','circle']
          */
         shapes?: Shape[] | undefined;
         /**
          * How far off center the confetti can go, in degrees. 45 means the confetti will launch at the defined angle plus or minus 22.5
          * degrees.
-         * @defaultValue 45
+         * @default 45
          */
         spread?: number | undefined;
         /**
          * How fast the confetti will start going, in pixels.
-         * @defaultValue 45
+         * @default 45
          */
         startVelocity?: number | undefined;
         /**
          * How many times the confetti will move. This is abstract... but play with it if the confetti disappear too quickly for you.
-         * @defaultValue 200
+         * @default 200
          */
         ticks?: number | undefined;
         /**
          * The confetti should be on top, after all. But if you have a crazy high page, you can set it even higher.
-         * @defaultValue 100
+         * @default 100
          */
         zIndex?: number | undefined;
     }
@@ -108,12 +108,12 @@ declare namespace confetti {
     interface Origin {
         /**
          * The x position on the page, with 0 being the left edge and 1 being the right edge.
-         * @defaultValue 0.5
+         * @default 0.5
          */
         x?: number | undefined;
         /**
          * The y position on the page, with 0 being the left edge and 1 being the right edge.
-         * @defaultValue 0.5
+         * @default 0.5
          */
         y?: number | undefined;
     }
@@ -126,12 +126,12 @@ declare namespace confetti {
         disableForReducedMotion?: boolean | undefined;
         /**
          * Whether to allow setting the canvas image size, as well as keep it correctly sized if the window changes size
-         * @defaultValue false
+         * @default false
          */
         resize?: boolean | undefined;
         /**
          * Whether to use an asynchronous web worker to render the confetti animation, whenever possible
-         * @defaultValue false
+         * @default false
          */
         useWorker?: boolean | undefined;
     }
