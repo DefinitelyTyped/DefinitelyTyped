f00d01c8dea62b3faa1e45e03e7778ab5bc515a2
SuperSodaSea <bobby825@126.com>
2022-11-13 01:31:54 +0800

    [gl] Export StackGL extension types

--- types/gl/index.d.ts
+++ types/gl/index.d.ts
@@ -3,26 +3,26 @@
 // Definitions by: DefinitelyTyped <https://github.com/DefinitelyTyped>
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
 
-interface StackGLExtension {
-    getExtension(extensionName: "STACKGL_resize_drawingbuffer"): STACKGL_resize_drawingbuffer | null;
-    getExtension(extensionName: "STACKGL_destroy_context"): STACKGL_destroy_context | null;
-}
-
-interface STACKGL_resize_drawingbuffer {
-    resize(width: GLint, height: GLint): void;
-}
-
-interface STACKGL_destroy_context {
-    destroy(): void;
-}
-
-declare function createContext(width: number, height: number, options?: WebGLContextAttributes): WebGLRenderingContext & StackGLExtension;
-
 declare namespace createContext {
+    interface STACKGL_destroy_context {
+        destroy(): void;
+    }
+
+    interface STACKGL_resize_drawingbuffer {
+        resize(width: GLint, height: GLint): void;
+    }
+
+    interface StackGLExtension {
+        getExtension(extensionName: "STACKGL_destroy_context"): STACKGL_destroy_context | null;
+        getExtension(extensionName: "STACKGL_resize_drawingbuffer"): STACKGL_resize_drawingbuffer | null;
+    }
+
     const WebGLRenderingContext: WebGLRenderingContext & StackGLExtension & {
         new(): WebGLRenderingContext & StackGLExtension;
         prototype: WebGLRenderingContext & StackGLExtension;
     };
 }
+
+declare function createContext(width: number, height: number, options?: WebGLContextAttributes): WebGLRenderingContext & createContext.StackGLExtension;
 
 export = createContext;
