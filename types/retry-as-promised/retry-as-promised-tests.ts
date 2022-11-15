1f2de257c8431d2badc82d138a7b3d8686927b4d
Ethan Setnik <esetnik@gmail.com>
2022-11-15 16:04:35 -0500

    improve test case to include match on subclassed Error as is shown in the project readme for a Sequelize error

--- types/retry-as-promised/retry-as-promised-tests.ts
+++ types/retry-as-promised/retry-as-promised-tests.ts
@@ -3,6 +3,15 @@ import retry = require('retry-as-promised');
 interface Product {
     name: string;
 }
+declare abstract class BaseError extends Error {
+    constructor(message?: string);
+}
+declare class TestError extends BaseError {
+    /** The specific error which triggered this one */
+    parent: Error;
+    original: Error;
+    constructor(parent: Error);
+}
 
 const log = (...args: any[]): void => {};
 
@@ -12,7 +21,7 @@ retry(() => Promise.reject(new Error('No matching values')), {});
 retry(() => Promise.resolve(), {
     max: 100,
     timeout: 60000,
-    match: ['Error matching values', new Error('No matching values'), new RegExp(/Deadlock/, 'i'), () => {}],
+    match: ['Error matching values', new Error('No matching values'), new RegExp(/Deadlock/, 'i'), () => {}, TestError],
     backoffBase: 100,
     backoffExponent: 1.1,
     name: 'Source',
