246b424691edd59ada76a25a12a5158a17c8c112
Spencer, Morgan [USA] <morgan@spencercreative.co>
2022-11-17 11:50:26 -0500

    Remove excess files from @uswds/uswds

--- types/uswds__uswds/index.d.ts
+++ types/uswds__uswds/index.d.ts
@@ -3,4 +3,50 @@
 // Definitions by: Morgan Spencer <https://github.com/morganmspencer>
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
 
-export {};
+import {
+    accordion,
+    banner,
+    characterCount,
+    comboBox,
+    datePicker,
+    dateRangePicker,
+    fileInput,
+    footer,
+    inPageNavigation,
+    inputMask,
+    inputPrefixSuffix,
+    languageSelector,
+    modal,
+    navigation,
+    password,
+    search,
+    skipnav,
+    table,
+    timePicker,
+    tooltip,
+    validator,
+} from './js';
+
+export {
+    accordion,
+    banner,
+    characterCount,
+    comboBox,
+    datePicker,
+    dateRangePicker,
+    fileInput,
+    footer,
+    inPageNavigation,
+    inputMask,
+    inputPrefixSuffix,
+    languageSelector,
+    modal,
+    navigation,
+    password,
+    search,
+    skipnav,
+    table,
+    timePicker,
+    tooltip,
+    validator,
+};
