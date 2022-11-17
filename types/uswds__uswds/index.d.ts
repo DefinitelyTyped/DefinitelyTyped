b255e3892511814b83c543818a806761982bb15e
Spencer, Morgan [USA] <morgan@spencercreative.co>
2022-11-17 12:20:46 -0500

    Update @uswds/uswds version number

--- types/uswds__uswds/index.d.ts
+++ types/uswds__uswds/index.d.ts
@@ -1,4 +1,4 @@
-// Type definitions for @uswds/uswds 3.0
+// Type definitions for @uswds/uswds 3.3
 // Project: https://github.com/uswds/uswds
 // Definitions by: Morgan Spencer <https://github.com/morganmspencer>
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
