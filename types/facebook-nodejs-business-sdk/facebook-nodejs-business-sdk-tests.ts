5378fcf0107933915eb4025bd97e4356b0860d79
ofirsnb <ofirsnb@gmail.com>
2022-11-18 17:00:23 +0200

    fix(facebook-nodejs-business-sdk): optional constructors arguments - added tests

--- types/facebook-nodejs-business-sdk/facebook-nodejs-business-sdk-tests.ts
+++ types/facebook-nodejs-business-sdk/facebook-nodejs-business-sdk-tests.ts
@@ -1,5 +1,14 @@
-import { AdAccount, FacebookAdsApi } from 'facebook-nodejs-business-sdk';
+import {
+    AdAccount,
+    FacebookAdsApi,
+    UserData,
+    Content,
+    CustomData,
+    ServerEvent,
+    EventRequest
+} from 'facebook-nodejs-business-sdk';
 import { FacebookRequestError } from 'facebook-nodejs-business-sdk/src/exceptions';
+import TEventRequest from 'facebook-nodejs-business-sdk/src/objects/serverside/event-request';
 
 async function testGetAds(): Promise<Array<Record<string, string>>> {
     const ads = [];
@@ -22,6 +31,29 @@ async function testGetAds(): Promise<Array<Record<string, string>>> {
     }
 
     return ads;
+}
+
+async function testConversionEvent(): Promise<TEventRequest> {
+    const userData = (new UserData())
+        .setEmails(['joe@eg.com'])
+        .setPhones(['12345678901', '14251234567']);
+
+    const content = (new Content())
+        .setId('product123');
+
+    const customData = (new CustomData())
+        .setContents([content]);
+
+    const serverEvent = (new ServerEvent())
+        .setEventName('Purchase')
+        .setEventTime(123)
+        .setUserData(userData)
+        .setCustomData(customData);
+
+    const eventsData = [serverEvent];
+    const eventRequest = (new EventRequest('access_token', 'pixel_id'))
+        .setEvents(eventsData);
+    return eventRequest;
 }
 
 throw new FacebookRequestError({}, 'GET', 'url', 'data');
