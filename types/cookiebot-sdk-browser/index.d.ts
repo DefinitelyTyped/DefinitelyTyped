0fcce19dbdb17b8e0cc9f2fd7243b2537602fc52
Liam Martens <hi@liammartens.com>
2022-11-18 09:43:16 -0500

    chore: more extensive tests

--- types/cookiebot-sdk-browser/index.d.ts
+++ types/cookiebot-sdk-browser/index.d.ts
@@ -4,38 +4,38 @@
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
 
 declare const Cookiebot: {
-  consented: boolean
-  declined: boolean
-  hasResponse: boolean
-  doNotTrack: boolean
-  consent: {
-    marketing: boolean
-    statistics: boolean
-    preferences: boolean
-    necessary: boolean
-    stamp: string
-  }
-  regulations: {
-    gdprApplies: boolean
-    ccpaApplies: boolean
-    lgpdApplies: boolean
-  }
+    consented: boolean;
+    declined: boolean;
+    hasResponse: boolean;
+    doNotTrack: boolean;
+    consent: {
+        marketing: boolean;
+        statistics: boolean;
+        preferences: boolean;
+        necessary: boolean;
+        stamp: string;
+    };
+    regulations: {
+        gdprApplies: boolean;
+        ccpaApplies: boolean;
+        lgpdApplies: boolean;
+    };
 
-  show(): void;
-  hide(): void;
-  renew(): void;
-  getScript(url: string, async: boolean, callback: () => void): void;
-  runScripts(): void;
-  withdraw(): void;
-  submitCustomConsent(optinPreferences: boolean, optinStatistics: boolean, optinMarketing: boolean): void;
+    show(): void;
+    hide(): void;
+    renew(): void;
+    getScript(url: string, async: boolean, callback: () => void): void;
+    runScripts(): void;
+    withdraw(): void;
+    submitCustomConsent(optinPreferences: boolean, optinStatistics: boolean, optinMarketing: boolean): void;
 };
 
 interface WindowEventMap {
-  'CookiebotOnConsentReady': Event;
-  'CookiebotOnLoad': Event;
-  'CookiebotOnAccept': Event;
-  'CookiebotOnDecline': Event;
-  'CookiebotOnDialogInit': Event;
-  'CookiebotOnDialogDisplay': Event;
-  'CookiebotOnTagsExecuted': Event;
+    CookiebotOnConsentReady: Event;
+    CookiebotOnLoad: Event;
+    CookiebotOnAccept: Event;
+    CookiebotOnDecline: Event;
+    CookiebotOnDialogInit: Event;
+    CookiebotOnDialogDisplay: Event;
+    CookiebotOnTagsExecuted: Event;
 }
