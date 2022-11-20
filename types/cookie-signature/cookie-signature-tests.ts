79c26b7523653c61aaeae1c95fffc98d6ebee087
Jeffrey Yasskin <jyasskin@gmail.com>
2022-11-20 11:34:54 -0800

    [cookie-signature] Accept the same secret types as createHmac
    cookie-signature passes the secret straight to the second argument of createHmac, so the typing should reflect that.

--- types/cookie-signature/cookie-signature-tests.ts
+++ types/cookie-signature/cookie-signature-tests.ts
@@ -1,3 +1,5 @@
+/// <reference types="node" />
+
 import * as cookie from "cookie-signature";
 
 let val = cookie.sign('hello', 'tobiiscool');
@@ -5,3 +7,11 @@ let val = cookie.sign('hello', 'tobiiscool');
 val = cookie.sign('hello', 'tobiiscool');
 cookie.unsign(val, 'tobiiscool');
 cookie.unsign(val, 'luna');
+
+import * as crypto from "node:crypto";
+
+val = cookie.sign('hello', crypto.createSecretKey('tobiiscool', 'ascii'));
+
+val = cookie.sign('hello', crypto.createSecretKey('tobiiscool', 'ascii'));
+cookie.unsign(val, crypto.createSecretKey('tobiiscool', 'ascii'));
+cookie.unsign(val, crypto.createSecretKey('luna', 'ascii'));
