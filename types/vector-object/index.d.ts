037105f848a7b8b12bb9597e4fa9b58150588096
= <918780@dpsk12.net>
2022-11-21 18:45:39 -0700

    add vector-object types

---
+++ types/vector-object/index.d.ts
@@ -0,0 +1,26 @@
+// Type definitions for vector-object 1.3
+// Project: https://github.com/stanleyfok/vector-object
+// Definitions by: = <https://github.com/DefinitelyTyped>
+// Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
+
+
+export default class Vector {
+    vector: object;
+    constructor(object: object);
+    clone(): Vector;
+    toObject(): object;
+    getComponents(): string[];
+    get(component: string): any;
+    set(component: string, value: any): void;
+    isEqual(vector: Vector): boolean;
+    getDistance(vector: Vector): number;
+    getLength(): number;
+    getDotProduct(vector: Vector): number;
+    getCosineSimilarity(vector: Vector): number;
+    normalize(): Vector;
+    add(vector: Vector): Vector;
+    subtract(vector: Vector): Vector;
+    multiply(scalar: number): Vector;
+    divide(scalar: number): Vector;
+}
+
