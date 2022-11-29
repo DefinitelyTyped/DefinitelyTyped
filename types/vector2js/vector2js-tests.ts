27b923a0ee2db09d9460eb47e756b2f697d18e31
Leo Nicolle <leo@linkurio.us>
2022-11-29 10:28:56 +0100

    added expected types

--- types/vector2js/vector2js-tests.ts
+++ types/vector2js/vector2js-tests.ts
@@ -1,22 +1,22 @@
 import Vector = require('vector2js');
 
-const o = new Vector(0, 0);
-const p = new Vector(1, 1);
-const v = new Vector(1, 2);
+const o = new Vector(0, 0); // $ExpectType Vector
+const p = new Vector(1, 1); // $ExpectType Vector
+const v = new Vector(1, 2); // $ExpectType Vector
 
-const res1 = p.add(o).clone().mul(v).flipXY().sub(o).div(2).rotateDegrees(90).rotateRadians(3.14).cross(p);
+const res1 = p.add(o).clone().mul(v).flipXY().sub(o).div(2).rotateDegrees(90).rotateRadians(3.14).cross(p); // $ExpectType Vector
 
-const res2 = v.addSelf(p).mulSelf(p).flipXYSelf().subSelf(o).divSelf(2).rotateDegreesSelf(90).rotateRadiansSelf(3.14);
+const res2 = v.addSelf(p).mulSelf(p).flipXYSelf().subSelf(o).divSelf(2).rotateDegreesSelf(90).rotateRadiansSelf(3.14); // $ExpectType Vector
 
-const res3 = p.add({ x: 1, y: 1 });
-const f1 = Vector.fromArray([1, 1]);
-const f2 = Vector.fromDegrees(45);
-const f3 = Vector.fromRadians(0);
-const f4 = Vector.fromString('1,1');
+const res3 = p.add({ x: 1, y: 1 }) ; // $ExpectType Vector
+const f1 = Vector.fromArray([1, 1]); // $ExpectType Vector
+const f2 = Vector.fromDegrees(45); // $ExpectType Vector
+const f3 = Vector.fromRadians(0); // $ExpectType Vector
+const f4 = Vector.fromString('1,1'); // $ExpectType Vector
 
-const d = p.dot(v);
-const s = p.toString();
-const s2 = p.format('{ %x; %y }');
+const d = p.dot(v); // $ExpectType number
+const s = p.toString(); // $ExpectType string
+const s2 = p.format('{ %x; %y }'); // $ExpectType string
 
 Vector.zero; // $ExpectType Vector
 Vector.one; // $ExpectType Vector
