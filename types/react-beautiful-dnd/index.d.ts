5b77afea5445eb5be13a3c2fbf4c35e909bab7bc
Declan Warn <dwarn@atlassian.com>
2022-12-05 12:26:01 +1100

    adjust child fn types

--- types/react-beautiful-dnd/index.d.ts
+++ types/react-beautiful-dnd/index.d.ts
@@ -594,7 +594,7 @@ export interface DroppableProps {
     ignoreContainerClipping?: boolean | undefined;
     renderClone?: DraggableChildrenFn | null | undefined;
     getContainerForClone?: (() => HTMLElement) | undefined;
-    children(provided: DroppableProvided, snapshot: DroppableStateSnapshot): React.ReactNode;
+    children(provided: DroppableProvided, snapshot: DroppableStateSnapshot): React.ReactElement<HTMLElement> | null;
 }
 
 export class Droppable extends React.Component<DroppableProps> {}
@@ -677,7 +677,7 @@ export type DraggableChildrenFn = (
     provided: DraggableProvided,
     snapshot: DraggableStateSnapshot,
     rubric: DraggableRubric,
-) => React.ReactNode | null;
+) => React.ReactElement<HTMLElement> | null;
 
 export interface DraggableProps {
     draggableId: DraggableId;
--- types/react-beautiful-dnd/v12/index.d.ts
+++ types/react-beautiful-dnd/v12/index.d.ts
@@ -599,7 +599,7 @@ export interface DroppableProps {
     ignoreContainerClipping?: boolean | undefined;
     renderClone?: DraggableChildrenFn | null | undefined;
     getContainerForClone?: (() => HTMLElement) | undefined;
-    children(provided: DroppableProvided, snapshot: DroppableStateSnapshot): React.ReactNode;
+    children(provided: DroppableProvided, snapshot: DroppableStateSnapshot): React.ReactElement<HTMLElement> | null;
 }
 
 export class Droppable extends React.Component<DroppableProps> {}
@@ -681,7 +681,7 @@ export type DraggableChildrenFn = (
     provided: DraggableProvided,
     snapshot: DraggableStateSnapshot,
     rubric: DraggableRubric,
-) => React.ReactNode | null;
+) => React.ReactElement<HTMLElement> | null | null;
 
 export interface DraggableProps {
     draggableId: DraggableId;
