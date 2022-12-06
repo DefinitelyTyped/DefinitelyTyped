f7393386d2b622f5c925c4afb1a77d96c7389fc3
richdouglasevans <rich.douglas.evans@gmail.com>
2022-12-06 16:30:12 +0000

    Add types for unicode-dragon module

---
+++ types/unicode-dragon/index.d.ts
@@ -0,0 +1,8 @@
+// Type definitions for unicode-dragon 0.1
+// Project: https://github.com/ConradIrwin/unicode-dragon
+// Definitions by: richdouglasevans <https://github.com/richdouglasevans>
+// Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
+
+declare function unicodeDragon(input: string): string;
+
+export = unicodeDragon;
