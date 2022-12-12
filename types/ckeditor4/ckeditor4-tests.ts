47cc75d298564c9d5ca32cbd27bf715e77dfa40f
Enquier <Enquier@users.noreply.github.com>
2022-12-12 16:06:42 -0700

    Simplify tests

--- types/ckeditor4/ckeditor4-tests.ts
+++ types/ckeditor4/ckeditor4-tests.ts
@@ -1310,3 +1310,32 @@ function test_tools() {
     str = ckEditor.tools.writeCssText(obj);
     str = ckEditor.tools.writeCssText(obj, true);
 }
+
+function test_plugins() {
+    ckEditor.plugins.add('abbr', {
+        init: (editor: CKEDITOR.editor) => {
+            // empty logic
+        },
+    });
+
+    console.log(ckEditor.plugins.registered['abbr']);
+
+    ckEditor.plugins.add('myPlugin', {
+        icons: 'my-plugin-icon',
+    });
+}
+
+function test_plugin_codesnippet() {
+    if (ckEditor.plugins.codesnippet) {
+        const highlighter = new ckEditor.plugins.codesnippet.highlighter();
+        ckEditor.plugins.codesnippet.setHighlighter(highlighter);
+    }
+}
+
+function test_plugin_elementsPath() {
+    const editor = new ckEditor.editor();
+    if (editor._.elementsPath) {
+        const elementspath = editor._.elementsPath;
+        const list = elementspath.list;
+    }
+}
