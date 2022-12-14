be5457b527bd2c8bf08f11d27242d143bdd0c33e
Simon Feiden <simon.feiden@real-digital.de>
2022-12-14 10:51:21 +0100

    Export nothing

--- types/graphql-upload/index.d.ts
+++ types/graphql-upload/index.d.ts
@@ -7,4 +7,5 @@
 
 // Linter does not accept files without definitions. Disabling the rule at
 // the top of the file doesn't work either: It breaks the meta data parser.
-type Dummy = never;
+
+export {};
