82d81fed0de5be662fcf680d805b876459a53585
Shihan-Xing <110041805+Shihan-Xing@users.noreply.github.com>
2022-12-14 11:13:28 -0500

    Update encoding.ts

--- types/akamai-edgeworkers/test/encoding.ts
+++ types/akamai-edgeworkers/test/encoding.ts
@@ -20,13 +20,13 @@ export function onOriginResponse(request: EW.EgressOriginRequest, response: EW.E
     const result = base64.decode("SGVsbG8sIHdvcmxk", "Uint8Array"); // decode to "[72,101,108,108,111,44,32,119,111,114,108,100]"
     request.setVariable('PMUSER_origin_response', result.toString());
     const decoder = new TextDecoder();
-    const data = new Uint8Array([84,104,105,115,32,105,115,32,97,32,115,97,109,112,108,101,32,112,97,114,97,103,114,97,112,104,46]);
-    let text = decoder.decode(data); // decodes to the string "This is a sample paragraph."
+    const data = new Uint8Array([84, 104, 105, 115, 32, 105, 115, 32, 97, 32, 115, 97, 109, 112, 108, 101, 32, 112, 97, 114, 97, 103, 114, 97, 112, 104, 46]);
+    const text = decoder.decode(data); // decodes to the string "This is a sample paragraph."
     request.respondWith(231, {}, text);
 }
 
 export function onClientResponse(request: EW.EgressClientRequest, response: EW.EgressClientResponse) {
-    let decoder = new TextDecoder();
+    const decoder = new TextDecoder();
     let output = "";
     // Push first chunk
     output += decoder.decode(new Uint8Array([0xE2, 0x82]), {stream: true});
