acdfca8893a939054e11b3d4f0ba623edd3b127d
Adam Kwiatek <kwiatek.adam@poczta.fm>
2022-12-18 06:45:12 +0100

    [protractor-console-plugin] export only Config

--- types/protractor-console-plugin/index.d.ts
+++ types/protractor-console-plugin/index.d.ts
@@ -3,57 +3,9 @@
 // Definitions by: Adam Kwiatek <https://github.com/akwiatek>
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
 
-import * as webdriver from 'selenium-webdriver';
-
-declare namespace consolePlugin {
-    interface ConsolePlugin {
-        /**
-         * Gets the browser log.
-         */
-        getBrowserLog(): webdriver.promise.Promise<webdriver.logging.Entry[]>;
-
-        /**
-         * Logs messages to the test output.
-         */
-        logMessages(
-            warnings: webdriver.logging.Entry[],
-            errors: webdriver.logging.Entry[],
-            failOnWarning: boolean,
-            failOnError: boolean,
-            context: Context,
-        ): void;
-
-        /**
-         * Determines if a log message is filtered out or not. This can be set at the
-         * config stage using the exclude parameter.  The parameter accepts both strings
-         * and regex.
-         */
-        includeLog(logMessage: string): boolean;
-
-        /**
-         * Parses the log and decides whether to throw an error or not.
-         */
-        parseLog(context: Context): webdriver.promise.Promise<void>;
-
-        /**
-         * Gather the console logs and output them as test results.  See the
-         * documentation of the teardown function in the protractor plugin API.
-         */
-        teardown(): webdriver.promise.Promise<void>;
-    }
-
-    interface Context {
-        config: Config;
-    }
-
-    interface Config {
-        failOnWarning?: boolean;
-        failOnError?: boolean;
-        logWarnings?: boolean;
-        exclude?: Array<string | RegExp>;
-    }
+export interface Config {
+    failOnWarning?: boolean;
+    failOnError?: boolean;
+    logWarnings?: boolean;
+    exclude?: Array<string | RegExp>;
 }
-
-declare const consolePlugin: consolePlugin.ConsolePlugin;
-
-export = consolePlugin;
--- types/protractor-console-plugin/protractor-console-plugin-tests.ts
+++ types/protractor-console-plugin/protractor-console-plugin-tests.ts
@@ -1,46 +1,144 @@
-import * as webdriver from 'selenium-webdriver';
-import Entry = webdriver.logging.Entry;
-import Level = webdriver.logging.Level;
-import { PluginConfig } from 'protractor/built';
+import 'selenium-webdriver';
+import * as protractor from 'protractor/built';
 
 import consolePlugin = require('protractor-console-plugin');
 
-declare function assertEquals<T>(actual: T, expected: T): void;
+function minimalConfiguration(): void {
+    const pluginConfig: protractor.PluginConfig & consolePlugin.Config = {
+        package: 'protractor-console-plugin',
+    };
+    const protractorConfig: protractor.Config = {
+        plugins: [pluginConfig],
+    };
+}
 
-const minimalConfiguration: PluginConfig & consolePlugin.Config = {
-    package: 'protractor-console-plugin',
+function fullConfiguration(): void {
+    const pluginConfig: protractor.PluginConfig & consolePlugin.Config = {
+        package: 'protractor-console-plugin',
+        failOnWarning: true,
+        failOnError: false,
+        logWarnings: false,
+        exclude: [
+            'Failed to load resource: net::ERR_',
+            /\bTransition Rejection[(].*'(?:restrictedAccess|technicalProblem|notFound)'/,
+        ],
+    };
+    const protractorConfig: protractor.Config = {
+        plugins: [pluginConfig],
+    };
+}
+
+// Specs from https://github.com/angular/protractor-console-plugin/tree/master/spec
+
+const env = {
+    seleniumAddress: 'http://localhost:4444/wd/hub',
+    baseUrl: 'http://localhost:8081',
 };
 
-const fullConfiguration: PluginConfig & consolePlugin.Config = {
-    package: 'protractor-console-plugin',
-    failOnWarning: true,
-    failOnError: false,
-    logWarnings: false,
-    exclude: [
-        'Failed to load resource: net::ERR_',
-        /\bTransition Rejection[(].*'(?:restrictedAccess|technicalProblem|notFound)'/,
-    ],
-};
+function consoleFailConfigJs(): void {
+    const pluginConfig: protractor.PluginConfig & consolePlugin.Config = {
+        path: '../index.js',
+        failOnWarning: true,
+        failOnError: true,
+    };
+    const protractorConfig: protractor.Config = {
+        seleniumAddress: env.seleniumAddress,
+        framework: 'jasmine',
+        specs: ['fail_spec.js'],
+        baseUrl: env.baseUrl,
+        plugins: [pluginConfig],
+    };
+}
 
-const entry = new Entry(Level.WARNING, 'warning');
-const context: consolePlugin.Context = {
-    config: fullConfiguration,
-};
+function consoleFailErrorConfigJs(): void {
+    const pluginConfig: protractor.PluginConfig & consolePlugin.Config = {
+        path: '../index.js',
+        failOnWarning: false,
+        failOnError: true,
+    };
+    const protractorConfig: protractor.Config = {
+        seleniumAddress: env.seleniumAddress,
+        framework: 'jasmine',
+        specs: ['fail_error_spec.js'],
+        baseUrl: env.baseUrl,
+        plugins: [pluginConfig],
+    };
+}
 
-consolePlugin.getBrowserLog().then(browserLog => {
-    browserLog.forEach(logEntry => {
-        assertEquals(logEntry, entry);
-    });
-});
+function consoleFailFilterConfigJs(): void {
+    const pluginConfig: protractor.PluginConfig & consolePlugin.Config = {
+        path: '../index.js',
+        failOnWarning: true,
+        failOnError: true,
+        exclude: ['string', /regex/],
+    };
+    const protractorConfig: protractor.Config = {
+        seleniumAddress: env.seleniumAddress,
+        framework: 'jasmine',
+        specs: ['fail_error_spec.js'],
+        baseUrl: env.baseUrl,
+        plugins: [pluginConfig],
+    };
+}
 
-assertEquals(consolePlugin.includeLog('found'), true);
+function consoleFailLogWarningsJs(): void {
+    const pluginConfig: protractor.PluginConfig & consolePlugin.Config = {
+        path: '../index.js',
+        failOnWarning: true,
+        logWarnings: true,
+        failOnError: false,
+    };
+    const protractorConfig: protractor.Config = {
+        seleniumAddress: env.seleniumAddress,
+        framework: 'jasmine',
+        specs: ['fail_warning_spec.js'],
+        baseUrl: env.baseUrl,
+        plugins: [pluginConfig],
+    };
+}
 
-consolePlugin.logMessages([entry], [entry], false, false, context);
+function consoleFailWarningConfigJs(): void {
+    const pluginConfig: protractor.PluginConfig & consolePlugin.Config = {
+        path: '../index.js',
+        failOnWarning: true,
+        failOnError: false,
+    };
+    const protractorConfig: protractor.Config = {
+        seleniumAddress: env.seleniumAddress,
+        framework: 'jasmine',
+        specs: ['fail_warning_spec.js'],
+        baseUrl: env.baseUrl,
+        plugins: [pluginConfig],
+    };
+}
 
-consolePlugin.parseLog(context).then(outcome => {
-    assertEquals(outcome, undefined);
-});
+function consolePassConfigJs(): void {
+    const pluginConfig: protractor.PluginConfig & consolePlugin.Config = {
+        path: '../index.js',
+        failOnWarning: false,
+        failOnError: false,
+    };
+    const protractorConfig: protractor.Config = {
+        seleniumAddress: env.seleniumAddress,
+        framework: 'jasmine',
+        specs: ['pass_spec.js'],
+        baseUrl: env.baseUrl,
+        plugins: [pluginConfig],
+    };
+}
 
-consolePlugin.teardown().then(outcome => {
-    assertEquals(outcome, undefined);
-});
+function consolePassLogWarningsJs(): void {
+    const pluginConfig: protractor.PluginConfig & consolePlugin.Config = {
+        path: '../index.js',
+        failOnWarning: true,
+        logWarnings: false,
+        failOnError: false,
+    };
+    const protractorConfig: protractor.Config = {
+        seleniumAddress: env.seleniumAddress,
+        framework: 'jasmine',
+        specs: ['pass_spec.js'],
+        baseUrl: env.baseUrl,
+        plugins: [pluginConfig],
+    };
+}
