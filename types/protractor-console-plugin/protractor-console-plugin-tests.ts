06a21e5652ed7dbf83e7d4f6b5d6a125fb17f68b
Adam Kwiatek <kwiatek.adam@poczta.fm>
2022-12-18 06:45:12 +0100

    [protractor-console-plugin] Change export type

--- types/protractor-console-plugin/protractor-console-plugin-tests.ts
+++ types/protractor-console-plugin/protractor-console-plugin-tests.ts
@@ -1,10 +1,17 @@
-import { ProtractorConsolePluginConfig } from 'protractor-console-plugin';
+import * as webdriver from 'selenium-webdriver';
+import Entry = webdriver.logging.Entry;
+import Level = webdriver.logging.Level;
+import { PluginConfig } from 'protractor/built';
 
-const minimalConfiguration: ProtractorConsolePluginConfig = {
+import consolePlugin = require('protractor-console-plugin');
+
+declare function assertEquals<T>(actual: T, expected: T): void;
+
+const minimalConfiguration: PluginConfig & consolePlugin.Config = {
     package: 'protractor-console-plugin',
 };
 
-const fullConfiguration: ProtractorConsolePluginConfig = {
+const fullConfiguration: PluginConfig & consolePlugin.Config = {
     package: 'protractor-console-plugin',
     failOnWarning: true,
     failOnError: false,
@@ -14,3 +21,26 @@ const fullConfiguration: ProtractorConsolePluginConfig = {
         /\bTransition Rejection[(].*'(?:restrictedAccess|technicalProblem|notFound)'/,
     ],
 };
+
+const entry = new Entry(Level.WARNING, 'warning');
+const context: consolePlugin.Context = {
+    config: fullConfiguration,
+};
+
+consolePlugin.getBrowserLog().then(browserLog => {
+    browserLog.forEach(logEntry => {
+        assertEquals(logEntry, entry);
+    });
+});
+
+assertEquals(consolePlugin.includeLog('found'), true);
+
+consolePlugin.logMessages([entry], [entry], false, false, context);
+
+consolePlugin.parseLog(context).then(outcome => {
+    assertEquals(outcome, undefined);
+});
+
+consolePlugin.teardown().then(outcome => {
+    assertEquals(outcome, undefined);
+});
