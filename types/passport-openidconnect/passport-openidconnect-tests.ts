9f61a993c5738d291805f688d8246c21e552bd1e
Axel Elmarsson <axel.elmarsson@gmail.com>
2022-12-19 22:21:50 +0100

    fix: ignore ts-rule

--- types/passport-openidconnect/passport-openidconnect-tests.ts
+++ types/passport-openidconnect/passport-openidconnect-tests.ts
@@ -1,7 +1,17 @@
-import { OpenIDConnectStrategy } from 'passport-openidconnect';
+import OpenIDConnectStrategy = require('passport-openidconnect');
+import {
+    Strategy,
+    StrategyOptions,
+    VerifyCallback,
+    AuthorizationError,
+    TokenError,
+    InternalOAuthError,
+    Profile,
+    SessionStore,
+} from 'passport-openidconnect';
 import { Request } from 'express';
 
-const strategyOptions1: OpenIDConnectStrategy.StrategyOptions = {
+const strategyOptions1: StrategyOptions = {
     authorizationURL: 'https://example.com/auth',
     callbackURL: 'https://example.com/callback',
     clientID: 'dummy',
@@ -11,29 +21,25 @@ const strategyOptions1: OpenIDConnectStrategy.StrategyOptions = {
     userInfoURL: 'https://example.com/userinfo',
 };
 
-function verifyFunction1(_issuer: string, _profile: any, verifyCallback: OpenIDConnectStrategy.VerifyCallback) {
+function verifyFunction1(_issuer: string, _profile: any, verifyCallback: VerifyCallback) {
     verifyCallback(new Error('not implemented'));
 }
 
-function verifyFunction2(
-    _issuer: string,
-    _profile: OpenIDConnectStrategy.Profile,
-    verifyCallback: OpenIDConnectStrategy.VerifyCallback,
-) {
+function verifyFunction2(_issuer: string, _profile: Profile, verifyCallback: VerifyCallback) {
     verifyCallback(new Error('not implemented'));
 }
 
 const strategy1: OpenIDConnectStrategy = new OpenIDConnectStrategy(strategyOptions1, verifyFunction1);
-const strategy2: OpenIDConnectStrategy.Strategy = new OpenIDConnectStrategy(strategyOptions1, verifyFunction2);
+const strategy2: Strategy = new OpenIDConnectStrategy(strategyOptions1, verifyFunction2);
 
-function verifyFunction3(issuer: string, profile: any, verifyCallback: OpenIDConnectStrategy.VerifyCallback) {
+function verifyFunction3(issuer: string, profile: any, verifyCallback: VerifyCallback) {
     verifyCallback(null, { userId: '1' });
 }
 
-const err1 = new OpenIDConnectStrategy.AuthorizationError('Description', 'invalid_request');
-const err2 = new OpenIDConnectStrategy.AuthorizationError('Description', 'invalid_request', undefined);
-const err3 = new OpenIDConnectStrategy.TokenError('undefined', 'invalid_request');
-const err4 = new OpenIDConnectStrategy.InternalOAuthError('Hello', {});
+const err1 = new AuthorizationError('Description', 'invalid_request');
+const err2 = new AuthorizationError('Description', 'invalid_request', undefined);
+const err3 = new TokenError('undefined', 'invalid_request');
+const err4 = new InternalOAuthError('Hello', {});
 
 class MyStrategy extends OpenIDConnectStrategy {
     useProtectedProperty() {
@@ -41,7 +47,7 @@ class MyStrategy extends OpenIDConnectStrategy {
     }
 }
 
-class MyStore implements OpenIDConnectStrategy.SessionStore {
+class MyStore implements SessionStore {
     store(
         req: Request,
         ctx: OpenIDConnectStrategy.SessionStoreContext,
@@ -55,7 +61,7 @@ class MyStore implements OpenIDConnectStrategy.SessionStore {
 
 const myStore = new MyStore();
 
-const strategyOptions2: OpenIDConnectStrategy.StrategyOptions = {
+const strategyOptions2: StrategyOptions = {
     ...strategyOptions1,
     scope: ['user', 'profile'],
 };
