5b0584d610cbef0564e8cd92d1198af89bf2291f
Tomasz Pluskiewicz <tpluscode@users.noreply.github.com>
2022-12-20 11:35:19 +0100

    chore: add new creator to b59 packages

--- types/barnard59-base/index.d.ts
+++ types/barnard59-base/index.d.ts
@@ -1,6 +1,7 @@
 // Type definitions for barnard59-base 1.0
 // Project: https://github.com/zazuko/barnard59-base
 // Definitions by: Tomasz Pluskiewicz <https://github.com/tpluscode>
+//                 Benjamin Hofstetter <https://github.com/BenjaminHofstetter>
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
 
 /// <reference types="node" />
