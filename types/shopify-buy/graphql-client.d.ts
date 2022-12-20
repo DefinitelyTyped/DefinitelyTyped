d287d9f330f5984bc4efe46146e019a6efd2a8fb
Martin Badin <martin.badin@gmail.com>
2022-12-20 21:04:24 +0100

    merge all files into index.d.ts

--- types/shopify-buy/graphql-client.d.ts
+++
@@ -1,4 +0,0 @@
-// TODO: implement from graphql-js-client
-type GraphQLJSClient = unknown;
-
-export default GraphQLJSClient;
--- types/shopify-buy/resource.d.ts
+++
@@ -1,7 +0,0 @@
-import GraphQLJSClient from './graphql-client';
-
-export default class Resource {
-    graphQLClient: GraphQLJSClient;
-
-    constructor(client: GraphQLJSClient);
-}
