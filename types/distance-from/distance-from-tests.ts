33e6bf1bca4bca9986c077d57a8a2a10023ce988
Patrick Lienau <patrick.lienau@thirdwavellc.com>
2022-12-22 03:21:44 -0600

    [distance-from] update type definition to align with emitted code

--- types/distance-from/distance-from-tests.ts
+++ types/distance-from/distance-from-tests.ts
@@ -1,9 +1,9 @@
-import distanceFrom from 'distance-from';
+import distanceFrom = require('distance-from');
 
 distanceFrom([ 41.8781, 87.6298 ]); // $ExpectType DistanceFrom
 distanceFrom([ 41.8781, 87.6298 ]).to([ 40.7128, 74.0060 ]); // $ExpectType DistanceFrom
 distanceFrom([ 41.8781, 87.6298 ]).to([ 40.7128, 74.0060 ]).in('ft'); // $ExpectType number
-distanceFrom([ 41.8781, 87.6298 ]).unitList(); // $ExpectType Units[]
+distanceFrom([ 41.8781, 87.6298 ]).unitList(); // $ExpectType Unit[]
 // @ts-expect-error
 distanceFrom();
 // @ts-expect-error
--- types/distance-from/index.d.ts
+++ types/distance-from/index.d.ts
@@ -3,38 +3,40 @@
 // Definitions by: Patrick Lienau <https://github.com/patrick-lienau>
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
 
-export type Position = [latitude: number, longitude: number];
-export type Units = (
-    | 'km'
-    | 'kilometer'
-    | 'kilometers'
-    | 'm'
-    | 'meters'
-    | 'meter'
-    | 'metre'
-    | 'cm'
-    | 'centimeter'
-    | 'centimeters'
-    | 'mi'
-    | 'mile'
-    | 'miles'
-    | 'feet'
-    | 'ft'
-    | 'in'
-    | 'inch'
-    | 'inches'
-    | 'yd'
-    | 'yard'
-    | 'yards'
-);
-
-export interface DistanceFrom {
-    distanceInKm(lat1: number, lon1: number, lat2: number, lon2: number): number;
-    to(destination: Position): this;
-    validUnits(unit: Units): boolean;
-    in(units: Units): number;
-    unitList(): Units[];
-    degreeToRadians(degree: number): number;
+declare namespace distanceFrom {
+    type Position = [latitude: number, longitude: number];
+    type Unit = (
+        | 'km'
+        | 'kilometer'
+        | 'kilometers'
+        | 'm'
+        | 'meters'
+        | 'meter'
+        | 'metre'
+        | 'cm'
+        | 'centimeter'
+        | 'centimeters'
+        | 'mi'
+        | 'mile'
+        | 'miles'
+        | 'feet'
+        | 'ft'
+        | 'in'
+        | 'inch'
+        | 'inches'
+        | 'yd'
+        | 'yard'
+        | 'yards'
+    );
+    class DistanceFrom {
+        distanceInKm(lat1: number, lon1: number, lat2: number, lon2: number): number;
+        to(destination: Position): this;
+        validUnits(unit: Unit): boolean;
+        in(units: Unit): number;
+        unitList(): Unit[];
+        degreeToRadians(degree: number): number;
+    }
 }
 
-export default function(location: Position): DistanceFrom;
+declare function distanceFrom(location: distanceFrom.Position): distanceFrom.DistanceFrom;
+export = distanceFrom;
