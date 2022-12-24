3eeea7cedb59deb8806917dbe1e215b57b5b4bb8
rxliuli <rxliuli@gmail.com>
2022-12-24 11:05:51 +0800

    add default import test

--- types/swagger2openapi/swagger2openapi-tests.ts
+++ types/swagger2openapi/swagger2openapi-tests.ts
@@ -1,5 +1,6 @@
 import { OpenAPIV2, OpenAPIV3 } from 'openapi-types';
 import { convert } from 'swagger2openapi';
+import converter from 'swagger2openapi';
 
 declare const schema: OpenAPIV2.Document;
 (async () => {
@@ -7,4 +8,5 @@ declare const schema: OpenAPIV2.Document;
     const r1: OpenAPIV3.Document = r.openapi;
     const r2: OpenAPIV2.Document = r.original;
     const r3: string = r.text;
+    const r4: OpenAPIV3.Document = (await converter.convert(schema, {})).openapi;
 })();
