2451c64ca5bf92a114c33c16dd2942c01c5c0666
rxliuli <rxliuli@gmail.com>
2022-12-24 11:15:35 +0800

    fix no-duplicate-imports

--- types/swagger2openapi/swagger2openapi-tests.ts
+++ types/swagger2openapi/swagger2openapi-tests.ts
@@ -1,6 +1,5 @@
 import { OpenAPIV2, OpenAPIV3 } from 'openapi-types';
 import { convert } from 'swagger2openapi';
-import converter from 'swagger2openapi';
 
 declare const schema: OpenAPIV2.Document;
 (async () => {
@@ -8,5 +7,5 @@ declare const schema: OpenAPIV2.Document;
     const r1: OpenAPIV3.Document = r.openapi;
     const r2: OpenAPIV2.Document = r.original;
     const r3: string = r.text;
-    const r4: OpenAPIV3.Document = (await converter.convert(schema, {})).openapi;
+    const r4: OpenAPIV3.Document = (await (await import('swagger2openapi')).convert(schema, {})).openapi;
 })();
