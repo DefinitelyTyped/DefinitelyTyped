b0242c315b66df1269d82247ff70c49059071ef8
Evan Broder <evan@ebroder.net>
2022-12-29 11:00:25 -0800

    mongo: Fix types of iterators and dropIndexAsync (#63748)
    Declaring iterators and async iterators with a "next" type of never
    makes them unusable with TypeScript, which throws an error:
    
    > Cannot iterate value because the 'next' method of its iterator expects
    > type 'never', but for-of will always send 'undefined'.ts(2763)
    
    However, Meteor's iterators are agnostic to the value passed in, so
    leaving it with the default of undefined is reasonable.
    
    Also, dropIndexAsync was missing a return type (it should return a
    promise, not void).

--- types/meteor/globals/mongo.d.ts
+++ types/meteor/globals/mongo.d.ts
@@ -228,7 +228,7 @@ declare namespace Mongo {
             transform?: Fn | undefined;
         }): boolean;
         dropCollectionAsync(): Promise<void>;
-        dropIndexAsync(indexName: string): void;
+        dropIndexAsync(indexName: string): Promise<void>;
         /**
          * Find the documents in a collection that match the selector.
          * @param selector A query describing the documents to find
@@ -472,8 +472,8 @@ declare namespace Mongo {
             callbacks: ObserveChangesCallbacks<T>,
             options?: { nonMutatingCallbacks?: boolean | undefined },
         ): Meteor.LiveQueryHandle;
-        [Symbol.iterator](): Iterator<T, never, never>;
-        [Symbol.asyncIterator](): AsyncIterator<T, never, never>;
+        [Symbol.iterator](): Iterator<T>;
+        [Symbol.asyncIterator](): AsyncIterator<T>;
     }
 
     var ObjectID: ObjectIDStatic;
