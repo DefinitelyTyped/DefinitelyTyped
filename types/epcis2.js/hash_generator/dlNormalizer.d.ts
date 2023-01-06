fe9be5cffe487a68cc79b9661db1556424ac516f
clementh59 <hecquet.c59@gmail.com>
2023-01-06 17:08:31 +0100

    Deleted useless files

--- types/epcis2.js/hash_generator/dlNormalizer.d.ts
+++
@@ -1,3 +0,0 @@
-export function webUriPercentEncoder(input: string): string;
-export function addCheckDigitAndZeroPad(raw: string, throwError: boolean, length?: number): string;
-export function normalizeDigitalLinks(originalUri: any, throwError?: boolean): any;
--- types/epcis2.js/request/buildParams.d.ts
+++
@@ -1,8 +0,0 @@
-/**
- * Build url safe parameter string if an object provided.
- *
- * @export
- * @param [params] key-value object or final query string
- * @param [useEncoding] whether to skip encoding
- */
-export default function buildParams(params?: (any | string), useEncoding?: boolean): string;
