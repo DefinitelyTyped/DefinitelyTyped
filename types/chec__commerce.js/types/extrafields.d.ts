a39f2c0516b2ab48264ea7a7dbdd29d502884493
Omar Khaled <74854173+OmarKhled@users.noreply.github.com>
2023-01-07 01:05:14 +0200

    Fixing the Missing semicolon

--- types/chec__commerce.js/types/extrafields.d.ts
+++ types/chec__commerce.js/types/extrafields.d.ts
@@ -1,5 +1,5 @@
 export type ExtrafieldType = 'text';
 
 export interface Extrafields {
-    [key: string]: string // The [key: string represents each extrfield id]
+    [key: string]: string; // The [key: string represents each extrfield id]
 }
