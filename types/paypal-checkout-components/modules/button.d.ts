c74c93d6c35d38aae5a368033a8cbedbaccdfc64
Serge Hänni <serge@nyi.ch>
2023-01-11 10:48:49 +0100

    Fix prettier issues

--- types/paypal-checkout-components/modules/button.d.ts
+++ types/paypal-checkout-components/modules/button.d.ts
@@ -26,10 +26,12 @@ export interface ButtonRenderer {
             accessToken?: (() => void) | undefined;
             onClose?: (() => void) | undefined;
 
-            funding?: {
-                allowed?: FundingOption[] | undefined;
-                disallowed?: FundingOption[] | undefined;
-            } | undefined;
+            funding?:
+                | {
+                      allowed?: FundingOption[] | undefined;
+                      disallowed?: FundingOption[] | undefined;
+                  }
+                | undefined;
 
             sessionID?: string | undefined;
             buttonSessionID?: string | undefined;
@@ -52,18 +54,16 @@ export interface ButtonRenderer {
 }
 
 export interface ButtonsRenderer {
-    (
-        options: {
-            style?: ButtonStyle | undefined;
-            fundingSource?: string | undefined;
-            createOrder?: (() => Promise<string>) | undefined;
-            createBillingAgreement?: (() => Promise<string>) | undefined;
-            onApprove: (data: AuthorizationData, actions: object) => Promise<AuthorizationResponse>;
-            onCancel?: ((data: CancellationData, actions: object) => void) | undefined;
-            onError?: ((error: string) => void) | undefined;
-            onInit?: (data: AuthorizationData, actions: object) => void;
-            onClick?: () => void
-        }
-    ): ButtonsRenderer;
+    (options: {
+        style?: ButtonStyle | undefined;
+        fundingSource?: string | undefined;
+        createOrder?: (() => Promise<string>) | undefined;
+        createBillingAgreement?: (() => Promise<string>) | undefined;
+        onApprove: (data: AuthorizationData, actions: object) => Promise<AuthorizationResponse>;
+        onCancel?: ((data: CancellationData, actions: object) => void) | undefined;
+        onError?: ((error: string) => void) | undefined;
+        onInit?: (data: AuthorizationData, actions: object) => void;
+        onClick?: () => void;
+    }): ButtonsRenderer;
     render(selector: string): void;
 }
