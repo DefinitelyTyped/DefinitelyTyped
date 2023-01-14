189a21be50d0e695a6269f29f5e4b5731dfd7816
Adrian Pascu <1521321+adipascu@users.noreply.github.com>
2023-01-15 00:01:24 +0200

    Add splash screen API

--- types/cordova/index.d.ts
+++ types/cordova/index.d.ts
@@ -80,3 +80,13 @@ declare var cordova: Cordova;
 declare module 'cordova' {
     export = cordova;
 }
+
+interface Navigator {
+    /** This plugin displays and hides a splash screen during application launch. */
+    splashscreen: {
+        /** Dismiss the splash screen. */
+        hide(): void;
+        /** Displays the splash screen. */
+        show(): void;
+    }
+}
