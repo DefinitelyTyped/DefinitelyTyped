1d17c60a1c78a580fe2b893621d0ccaeef8910df
Adrian Pascu <1521321+adipascu@users.noreply.github.com>
2023-01-15 00:24:29 +0200

    Update cordova version

--- types/cordova/index.d.ts
+++ types/cordova/index.d.ts
@@ -1,4 +1,4 @@
-// Type definitions for Apache Cordova
+// Type definitions for Apache Cordova 11.0
 // Project: http://cordova.apache.org
 // Definitions by: Microsoft Open Technologies Inc <http://msopentech.com>
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
