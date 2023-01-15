ced7112e30ff63511bab77a110f3d830c26076c2
Mannuel Ferreira <mannuel.ferreira@gmail.com>
2023-01-15 21:59:23 +0100

    exports default class

--- types/euwa-wrapper/index.d.ts
+++ types/euwa-wrapper/index.d.ts
@@ -3,200 +3,198 @@
 // Definitions by: Mannuel Ferreira <https://github.com/mannuelf>
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
 
-declare module '@puzzel/euwa-wrapper' {
-    type Config = {
-        customerKey: string;
-        configId: string;
-    };
+type Config = {
+    customerKey: string;
+    configId: string;
+};
 
-    type Options = {
-        /**
-         * Configures how you would like the chat client to behave on start
-         * @example
-         * new EUWA({ customerKey: string, configId: string }, {
-         *   settings: {
-         *     [EUWA.APPS.CHAT]: {
-         *       showStarter: false,
-         *     },
-         *   },
-         * })
-         */
-        settings: ApplicationSettings;
+type Options = {
+    /**
+     * Configures how you would like the chat client to behave on start
+     * @example
+     * new EUWA({ customerKey: string, configId: string }, {
+     *   settings: {
+     *     [EUWA.APPS.CHAT]: {
+     *       showStarter: false,
+     *     },
+     *   },
+     * })
+     */
+    settings: ApplicationSettings;
 
-        /**
-         * This allows you to subscribe to events or do other actions, before the applications are loaded
-         * However, since the different application’s API-s are defined by the application itself,
-         */
-        hooks?: Hooks;
-    };
+    /**
+     * This allows you to subscribe to events or do other actions, before the applications are loaded
+     * However, since the different application’s API-s are defined by the application itself,
+     */
+    hooks?: Hooks;
+};
 
-    type ApplicationList = {
-        [app: string]: string;
-    };
+type ApplicationList = {
+    [app: string]: string;
+};
 
-    type Hooks = {
-        /**
-         * Only publish/subscribe interface is available
-         * @param event
-         * @param callback
-         * @returns
-         */
-        onBeforeLoad: (event: string, callback: Function) => void;
-        [hook: string]: Function;
-    };
+type Hooks = {
+    /**
+     * Only publish/subscribe interface is available
+     * @param event
+     * @param callback
+     * @returns
+     */
+    onBeforeLoad: (event: string, callback: Function) => void;
+    [hook: string]: Function;
+};
 
-    type ApplicationBridge = {
-        /**
-         * Returns object with minimal chat state
-         * Similar to clicking the start button
-         * It will open the chat window and show the next view
-         * @returns ApplicationAPI
-         */
-        api: ApplicationAPI;
+type ApplicationBridge = {
+    /**
+     * Returns object with minimal chat state
+     * Similar to clicking the start button
+     * It will open the chat window and show the next view
+     * @returns ApplicationAPI
+     */
+    api: ApplicationAPI;
 
-        /**
-         * Publish custom events, use this to buidl custom functions to do other work
-         * @example
-         * import {EUWA} from '@puzzel/euwa-wrapper';
-         * const euwa = new EUWA({
-         *    configId: 'xxxxxxxx-xxxx-xxxx-xxxx-xxxxxxxxxxxx',
-         *    customerKey: 123456
-         *   }, {
-         *   hooks: {
-         *       // All hooks accept functions
-         *      onBeforeLoad: publishMyCustomEvent
-         *    }
-         * });
-         *
-         * function publishMyCustomEvent() {
-         *    const chat = euwa.getApplicationBeforeLoad(EUWA.APPS.CHAT);
-         *    chat.publish('PublishEvent', data => {
-         *        console.log('PublishEvent Data:', data);
-         *    });
-         * }
-         * @param event
-         * @param data
-         * @returns
-         */
-        publish: (event: string, ...data: any) => void;
+    /**
+     * Publish custom events, use this to buidl custom functions to do other work
+     * @example
+     * import {EUWA} from '@puzzel/euwa-wrapper';
+     * const euwa = new EUWA({
+     *    configId: 'xxxxxxxx-xxxx-xxxx-xxxx-xxxxxxxxxxxx',
+     *    customerKey: 123456
+     *   }, {
+     *   hooks: {
+     *       // All hooks accept functions
+     *      onBeforeLoad: publishMyCustomEvent
+     *    }
+     * });
+     *
+     * function publishMyCustomEvent() {
+     *    const chat = euwa.getApplicationBeforeLoad(EUWA.APPS.CHAT);
+     *    chat.publish('PublishEvent', data => {
+     *        console.log('PublishEvent Data:', data);
+     *    });
+     * }
+     * @param event
+     * @param data
+     * @returns
+     */
+    publish: (event: string, ...data: any) => void;
 
-        /**
-         * Subscribe to custom event
-         * @param event
-         * @param callback
-         *
-         * @example
-         * import {EUWA} from '@puzzel/euwa-wrapper';
-         *
-         *  const euwa = new EUWA({
-         *       configId: 'xxxxxxxx-xxxx-xxxx-xxxx-xxxxxxxxxxxx',
-         *       customerKey: 123456
-         *  }, {
-         *       hooks: {
-         *            // All hooks accept functions
-         *             onBeforeLoad: subscribeToChatInit
-         *          }
-         *     }
-         * );
-         *
-         * function subscribeToChatInit() {
-         *    // Get the Chat's event interface
-         *    const chat = euwa.getApplicationBeforeLoad(EUWA.APPS.CHAT);
-         *
-         *    // Subscribe to chatInit* event
-         *    chat.subscribe('chatInit', data => {
-         *        console.log('Chat Init Data:', data);
-         *    });
-         * }
-         */
-        subscribe: (event: string, callback: Function) => void;
-    };
+    /**
+     * Subscribe to custom event
+     * @param event
+     * @param callback
+     *
+     * @example
+     * import {EUWA} from '@puzzel/euwa-wrapper';
+     *
+     *  const euwa = new EUWA({
+     *       configId: 'xxxxxxxx-xxxx-xxxx-xxxx-xxxxxxxxxxxx',
+     *       customerKey: 123456
+     *  }, {
+     *       hooks: {
+     *            // All hooks accept functions
+     *             onBeforeLoad: subscribeToChatInit
+     *          }
+     *     }
+     * );
+     *
+     * function subscribeToChatInit() {
+     *    // Get the Chat's event interface
+     *    const chat = euwa.getApplicationBeforeLoad(EUWA.APPS.CHAT);
+     *
+     *    // Subscribe to chatInit* event
+     *    chat.subscribe('chatInit', data => {
+     *        console.log('Chat Init Data:', data);
+     *    });
+     * }
+     */
+    subscribe: (event: string, callback: Function) => void;
+};
 
-    type ApplicationSettings = {
-        [app: string]: object;
-    };
+type ApplicationSettings = {
+    [app: string]: object;
+};
 
-    type SystemVariables = {
-        enteredFormName: string;
-        enteredChatId: string;
-        enteredFormIssue: string;
-        selectedQueueKey: string;
-        timeId2Map: string;
-    };
+type SystemVariables = {
+    enteredFormName: string;
+    enteredChatId: string;
+    enteredFormIssue: string;
+    selectedQueueKey: string;
+    timeId2Map: string;
+};
 
-    type ChatState = {
-        isConnected: boolean;
-        isEnded: boolean;
-        isMinimized: boolean;
-    };
+type ChatState = {
+    isConnected: boolean;
+    isEnded: boolean;
+    isMinimized: boolean;
+};
 
-    type ApplicationAPI = {
-        /**
-         * Returns object with minimal chat state
-         * Similar to clicking the start button
-         * It will open the chat window and show the next view
-         * @returns ChatState
-         */
-        getState: () => ChatState;
+type ApplicationAPI = {
+    /**
+     * Returns object with minimal chat state
+     * Similar to clicking the start button
+     * It will open the chat window and show the next view
+     * @returns ChatState
+     */
+    getState: () => ChatState;
 
-        /**
-         * Maximizes the chat window
-         * @returns void
-         */
-        maximize: Function;
+    /**
+     * Maximizes the chat window
+     * @returns void
+     */
+    maximize: Function;
 
-        /**
-         * Minimizes the chat window
-         * @returns void
-         */
-        minimize: Function;
+    /**
+     * Minimizes the chat window
+     * @returns void
+     */
+    minimize: Function;
 
-        /**
-         * Starts chat
-         * @returns void
-         */
-        startChat: Function;
+    /**
+     * Starts chat
+     * @returns void
+     */
+    startChat: Function;
 
-        /**
-         * Updates the chat variables, one may update or set custom variables
-         * @param param
-         * @returns void
-         */
-        updateVariables: ({ NewVariableOfYourChoice: string }: SystemVariables) => void;
+    /**
+     * Updates the chat variables, one may update or set custom variables
+     * @param param
+     * @returns void
+     */
+    updateVariables: ({ NewVariableOfYourChoice: string }: SystemVariables) => void;
 
-        /**
-         * Updates system variables
-         * @param {string} - enteredFormName
-         * @param {string} - enteredChatId
-         * @param {string} - enteredFormIssue
-         * @param {string} - selectedQueueKey
-         * @param {string} - timeId2Map
-         * @returns void
-         */
-        updateSystemVariables: ({ enteredFormName }: SystemVariables) => void;
-        [method: string]: Function;
-    };
+    /**
+     * Updates system variables
+     * @param {string} - enteredFormName
+     * @param {string} - enteredChatId
+     * @param {string} - enteredFormIssue
+     * @param {string} - selectedQueueKey
+     * @param {string} - timeId2Map
+     * @returns void
+     */
+    updateSystemVariables: ({ enteredFormName }: SystemVariables) => void;
+    [method: string]: Function;
+};
 
-    class EUWA {
-        static APPS: ApplicationList;
+export default class EUWA {
+    static APPS: ApplicationList;
 
-        constructor({ customerKey, configId }: Config, { settings, hooks }: Options);
+    constructor({ customerKey, configId }: Config, { settings, hooks }: Options);
 
-        /**
-         * Get the chat application context
-         * @param {string} id - EUWA.APPS.CHAT
-         * @example
-         * const chat = await euwa.getApplication(EUWA.APPS.CHAT);
-         */
-        getApplication(id: string): Promise<ApplicationBridge>;
+    /**
+     * Get the chat application context
+     * @param {string} id - EUWA.APPS.CHAT
+     * @example
+     * const chat = await euwa.getApplication(EUWA.APPS.CHAT);
+     */
+    getApplication(id: string): Promise<ApplicationBridge>;
 
-        /**
-         * The EUWA’s getApplicationBeforeLoad method is specifically designed to be used with specifically this hook
-         * It will not wait for application’s load and return it’s basic communication interface - events
-         * @param id - EUWA.APPS.CHAT
-         * @example
-         * const chat = euwa.getApplicationBeforeLoad(EUWA.APPS.CHAT);
-         */
-        getApplicationBeforeLoad(id: string): ApplicationBridge;
-    }
+    /**
+     * The EUWA’s getApplicationBeforeLoad method is specifically designed to be used with specifically this hook
+     * It will not wait for application’s load and return it’s basic communication interface - events
+     * @param id - EUWA.APPS.CHAT
+     * @example
+     * const chat = euwa.getApplicationBeforeLoad(EUWA.APPS.CHAT);
+     */
+    getApplicationBeforeLoad(id: string): ApplicationBridge;
 }
