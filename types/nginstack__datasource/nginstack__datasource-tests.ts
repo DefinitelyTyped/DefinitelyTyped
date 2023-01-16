3a7771520e9199f6bbf3f048bc8f054b7a2ae321
Renato <renato@innovaro.com.br>
2023-01-16 13:01:21 -0300

    feat: update @nginstack/datasource definitions

--- types/nginstack__datasource/nginstack__datasource-tests.ts
+++ types/nginstack__datasource/nginstack__datasource-tests.ts
@@ -28,4 +28,4 @@ datasource.getResult(filterDefs, columnDefs); // $ExpectType DataSourceResult
 function testMajorVersions(prior: number, current: number): boolean {
     return current > prior;
 }
-testMajorVersions(65, 66); // $ExpectType boolean
+testMajorVersions(67, 68); // $ExpectType boolean
