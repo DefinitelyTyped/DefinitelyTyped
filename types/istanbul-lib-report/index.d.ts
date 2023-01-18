61d64545d0118b33079b451ef289d095b2c5d7e3
Charles Stover <github@charlesstover.com>
2023-01-18 15:11:11 -0800

    fix `getXMLWriter` typo

--- types/istanbul-lib-report/index.d.ts
+++ types/istanbul-lib-report/index.d.ts
@@ -64,7 +64,7 @@ export interface Context {
     /**
      * returns an XML writer for the supplied content writer
      */
-    getXmlWriter(contentWriter: ContentWriter): XmlWriter;
+    getXMLWriter(contentWriter: ContentWriter): XmlWriter;
 }
 
 /**
