30241e6fbf4328a49a48003db61285bc36bacb8c
strawhat-dev <trankn4@gmail.com>
2023-01-19 09:18:22 -0800

    init types for npm/gauge

---
+++ types/gauge/template.d.ts
@@ -0,0 +1,58 @@
+/// <reference types="./helpers" />
+
+type Status = Partial<Record<TemplateType, Primitive>>;
+
+type Template = Array<string | TemplateObject>;
+
+interface TemplateObject {
+  /**
+   * Can be any of the following plus any keys you pass into gauge.show plus any
+   * keys you have on a custom theme.
+   */
+  type: TemplateType;
+
+  /**
+   * Number of spaces that must be between this item and other items, if this
+   * item is displayed at all.
+   */
+  kerning?: number;
+
+  /**
+   * The maximum length for this element. If its value is longer it will be
+   * truncated.
+   */
+  maxLength?: number;
+
+  /**
+   * The minimum length for this element. If its value is shorter it will be
+   * padded according to the align value.
+   */
+  minLength?: number;
+
+  /**
+   * Works as you'd expect from word processors.
+   * @default 'left'
+   */
+  align?: 'left' | 'right' | 'center';
+
+  /**
+   * Provides a single value for both minLength and maxLength. If both length
+   * and minLength or maxLength are specified then the latter take precedence.
+   */
+  length?: number;
+
+  /**
+   * A literal value to use for this template item.
+   */
+  value?: Primitive;
+
+  /**
+   * A default value to use for this template item if a value wasn't otherwise
+   * passed in.
+   */
+  default?: Primitive;
+}
+
+type TemplateType = LiteralUnion<
+  'section' | 'subsection' | 'progressbar' | 'activityIndicator' | 'completed'
+>;
