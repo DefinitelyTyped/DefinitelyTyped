bb1502c3cf841940c4c4c6b917425b494b7bada1
Adam Thompson-Sharpe <adamthompsonsharpe@gmail.com>
2023-01-21 12:54:34 -0500

    feat: Add types for splitting

---
+++ types/splitting/index.d.ts
@@ -0,0 +1,9 @@
+// Type definitions for splitting 1.0
+// Project: https://github.com/shshaw/splitting
+// Definitions by: Adam Thompson-Sharpe <https://github.com/MysteryBlokHed>
+// Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
+import Splitting = require('./dist/splitting');
+
+export = Splitting;
+
+export as namespace Splitting;
---
+++ types/splitting/splitting-tests.ts
@@ -0,0 +1,45 @@
+import Splitting = require('splitting');
+
+// Base splitting function
+
+Splitting();
+Splitting({});
+Splitting({
+    target: '.target',
+    by: 'chars',
+    key: 'foo',
+});
+
+Splitting({ key: null });
+
+Splitting({ target: document.createElement('span') });
+Splitting({ target: [document.createElement('span')] });
+Splitting({ target: document.querySelectorAll('.target') });
+
+// HTML function
+
+// @ts-expect-error
+Splitting.html();
+// @ts-expect-error
+Splitting.html({});
+
+Splitting.html({ content: '<span>ABC</span>' });
+Splitting.html({
+    content: '<span>ABC</span>',
+    by: 'chars',
+    key: 'foo',
+});
+
+// Add function
+
+// @ts-expect-error
+Splitting.add();
+
+Splitting.add({
+    by: 'plugin',
+    key: 'foo',
+    depends: [],
+    split() {
+        return [];
+    },
+});
