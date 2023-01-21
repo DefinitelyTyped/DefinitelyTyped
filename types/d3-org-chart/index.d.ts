b15ce5435f9a16baee84787e94a807b901e886c5
Matteo Gallesio <46478501+m-gallesio@users.noreply.github.com>
2023-01-21 02:19:15 +0100

    d3-org-chart: - extracted get/set pairs in separate type - typed some unknown values

--- types/d3-org-chart/index.d.ts
+++ types/d3-org-chart/index.d.ts
@@ -1,13 +1,15 @@
 // Type definitions for d3-org-chart 2.6
 // Project: https://github.com/bumbeishvili/org-chart#readme
 // Definitions by: Adam Jones <https://github.com/domdomegg>
+// Definitions by: Matteo Gallesio <https://github.com/m-gallesio>
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
 
 import { HierarchyNode } from 'd3-hierarchy';
 import { Selection } from 'd3-selection';
+import { ZoomBehavior, ZoomTransform } from 'd3-zoom';
 
 export type NodeId = string | number;
-export type Connection = any;
+export type Connection = any; // TODO
 
 export interface Point {
     x: number;
@@ -15,15 +17,15 @@ export interface Point {
 }
 
 // This corresponds to the attrs in d3-org-chart
-export interface State<Datum> {
+export interface StatePublic<Datum> {
     id: string;
     firstDraw: boolean;
     svgWidth: number;
     svgHeight: number;
     scaleExtent: [number, number];
-    container: string;
-    defaultTextFill: string;
-    defaultFont: string;
+    container: string; // CSS selector string, for example "#my-chart"
+    defaultTextFill: string; // CSS color, for example "#2C3E50"
+    defaultFont: string; // Font name, for example "Helvetica"
     ctx: CanvasRenderingContext2D;
     data: Datum[] | null;
     duration: number;
@@ -31,15 +33,45 @@ export interface State<Datum> {
     expandLevel: number;
     compact: boolean;
     rootMargin: number;
-    nodeDefaultBackground: string;
+    nodeDefaultBackground: string; // CSS color, for example "#2C3E50"
     connections: Connection[];
-    lastTransform: { x: number; y: number; k: number };
-    nodeId: (d: Datum) => NodeId;
-    parentNodeId: (d: Datum) => NodeId;
-    backgroundColor: 'none';
-    zoomBehavior: null;
+    lastTransform: ZoomTransform;
+    nodeId: (node: HierarchyNode<Datum> | Datum) => NodeId | undefined; // Given a node, returns an id for equality comparisons
+    parentNodeId: (node: HierarchyNode<Datum> | Datum) => NodeId | undefined; // Given a node, returns it's parent id for equality comparisons
+    backgroundColor: string; // CSS color, for example "#2C3E50"
+    zoomBehavior: ZoomBehavior<Element, Datum>;
     svg: Selection<SVGSVGElement, string, null, undefined>;
+    defs: unknown; // TODO
+    connectionsUpdate: unknown; // TODO
+    linkUpdate: (node: HierarchyNode<Datum>, index: number, nodes: Array<HierarchyNode<Datum>>) => void;
+    nodeUpdate: (node: HierarchyNode<Datum>, index: number, nodes: Array<HierarchyNode<Datum>>) => void;
+    nodeWidth: (node: HierarchyNode<Datum>) => number;
+    nodeHeight: (node: HierarchyNode<Datum>) => number;
+    siblingsMargin: (node: HierarchyNode<Datum>) => number;
+    childrenMargin: (node: HierarchyNode<Datum>) => number;
+    neightbourMargin: (node1: HierarchyNode<Datum>, node2: HierarchyNode<Datum>) => number;
+    compactMarginPair: (node: HierarchyNode<Datum>) => number;
+    compactMarginBetween: (node: HierarchyNode<Datum>) => number;
+    onNodeClick: (node: HierarchyNode<Datum>) => void;
+    linkGroupArc: unknown;
+    nodeContent: (
+        node: HierarchyNode<Datum>,
+        index: number,
+        nodes: Array<HierarchyNode<Datum>>,
+        state: State<Datum>,
+    ) => string; // Return type is HTML content
+    layout: Layout;
+    buttonContent: (params: { node: HierarchyNode<Datum>; state: State<Datum>; }) => string;
+    layoutBindings: Record<Layout, LayoutBinding<Datum>>;
+}
 
+// Other available properties not exposed as get / set pairs
+export interface StateInternal<Datum> {
+    readonly root: HierarchyNode<Datum>;
+    readonly allNodes: ReadonlyArray<HierarchyNode<Datum>>;
+}
+
+export interface State<Datum> extends StatePublic<Datum>, StateInternal<Datum> {
     [key: string]: any;
 }
 
@@ -85,107 +117,18 @@ export interface LayoutBinding<Datum> {
         state: State<Datum>;
         node: HierarchyNode<Datum>;
     }) => [number, number];
-    zoomTransform: (params: { centerY: number; scale: number }) => string; // CSS transform
+    zoomTransform: (params: { centerY: number; scale: number; }) => string; // CSS transform
     diagonal: unknown;
     swap: (d: Point) => Point; // swaps x and y coordinates
-    nodeUpdateTransform: (params: { width: number; height: number } & Point) => string;
+    nodeUpdateTransform: (params: { width: number; height: number; } & Point) => string;
 }
 
-export class OrgChart<Datum> {
-    constructor();
+// Helper type to remove the need to explicitly declare get / set methods
+export type StateGetSet<T, TSelf> =
+    & { [Property in keyof StatePublic<T>]: () => StatePublic<T>[Property]; }
+    & { [Property in keyof StatePublic<T>]: (value: StatePublic<T>[Property]) => TSelf; };
 
-    // Getters and setters, dynamically crreated in the constructor
-    // TODO: improve unknown types
-    id(): string;
-    id(value: string): this;
-    firstDraw(): boolean;
-    firstDraw(value: boolean): this;
-    svgWidth(): number;
-    svgWidth(value: number): this;
-    svgHeight(): number;
-    svgHeight(value: number): this;
-    scaleExtent(): [number, number];
-    scaleExtent(value: [number, number]): this;
-    container(): string; // CSS selector string, for example "#my-chart"
-    container(value: string): this;
-    defaultTextFill(): string; // CSS color, for example "#2C3E50"
-    defaultTextFill(value: string): this;
-    defaultFont(): string; // Font name, for example "Helvetica"
-    defaultFont(value: string): this;
-    ctx(): CanvasRenderingContext2D;
-    ctx(value: CanvasRenderingContext2D): this;
-    data(): Datum[] | null;
-    data(value: Datum[] | null): this;
-    duration(): number;
-    duration(value: number): this;
-    setActiveNodeCentered(): boolean;
-    setActiveNodeCentered(value: boolean): this;
-    expandLevel(): number;
-    expandLevel(value: number): this;
-    compact(): boolean;
-    compact(value: boolean): this;
-    rootMargin(): number;
-    rootMargin(value: number): this;
-    nodeDefaultBackground(): string; // CSS color, for example "#2C3E50"
-    nodeDefaultBackground(value: string): this;
-    connections(): Connection[];
-    connections(value: Connection[]): this;
-    lastTransform(): { k: number } & Point;
-    lastTransform(value: { k: number } & Point): this;
-    nodeId(): (node: HierarchyNode<Datum> | Datum) => NodeId; // Given a node, returns an id for equality comparisons
-    nodeId(value: (node: HierarchyNode<Datum> | Datum) => NodeId): this;
-    parentNodeId(): (node: HierarchyNode<Datum> | Datum) => NodeId | undefined; // Given a node, returns it's parent id for equality comparisons
-    parentNodeId(value: (node: HierarchyNode<Datum> | Datum) => NodeId | undefined): this;
-    backgroundColor(): string; // CSS color, for example "#2C3E50"
-    backgroundColor(value: string): this;
-    zoomBehavior(): unknown;
-    zoomBehavior(value: unknown): this;
-    defs(): unknown;
-    defs(value: unknown): this;
-    connectionsUpdate(): unknown;
-    connectionsUpdate(value: unknown): this;
-    linkUpdate(): (node: HierarchyNode<Datum>, index: number, nodes: Array<HierarchyNode<Datum>>) => void;
-    linkUpdate(value: (node: HierarchyNode<Datum>, index: number, nodes: Array<HierarchyNode<Datum>>) => void): this;
-    nodeUpdate(): (node: HierarchyNode<Datum>, index: number, nodes: Array<HierarchyNode<Datum>>) => void;
-    nodeUpdate(value: (node: HierarchyNode<Datum>, index: number, nodes: Array<HierarchyNode<Datum>>) => void): this;
-    nodeWidth(): (node: HierarchyNode<Datum>) => number;
-    nodeWidth(value: (node: HierarchyNode<Datum>) => number): this;
-    nodeHeight(): (node: HierarchyNode<Datum>) => number;
-    nodeHeight(value: (node: HierarchyNode<Datum>) => number): this;
-    siblingsMargin(): (node: HierarchyNode<Datum>) => number;
-    siblingsMargin(value: (node: HierarchyNode<Datum>) => number): this;
-    childrenMargin(): (node: HierarchyNode<Datum>) => number;
-    childrenMargin(value: (node: HierarchyNode<Datum>) => number): this;
-    neightbourMargin(): (node1: HierarchyNode<Datum>, node2: HierarchyNode<Datum>) => number; // NB: typo to match library
-    neightbourMargin(value: (node1: HierarchyNode<Datum>, node2: HierarchyNode<Datum>) => number): this;
-    compactMarginPair(): (node: HierarchyNode<Datum>) => number;
-    compactMarginPair(value: (node: HierarchyNode<Datum>) => number): this;
-    compactMarginBetween(): (node: HierarchyNode<Datum>) => number;
-    compactMarginBetween(value: (node: HierarchyNode<Datum>) => number): this;
-    onNodeClick(): (node: HierarchyNode<Datum>) => void;
-    onNodeClick(value: (node: HierarchyNode<Datum>) => void): this;
-    linkGroupArc(): unknown;
-    linkGroupArc(value: unknown): this;
-    nodeContent(): (
-        node: HierarchyNode<Datum>,
-        index: number,
-        nodes: Array<HierarchyNode<Datum>>,
-        state: State<Datum>,
-    ) => string; // Return type is HTML content
-    nodeContent(
-        value: (
-            node: HierarchyNode<Datum>,
-            index: number,
-            nodes: Array<HierarchyNode<Datum>>,
-            state: State<Datum>,
-        ) => string,
-    ): this;
-    layout(): Layout;
-    layout(value: Layout): this;
-    buttonContent(): (params: { node: HierarchyNode<Datum>; state: State<Datum> }) => string; // Return type is HTML content
-    buttonContent(value: (params: { node: HierarchyNode<Datum>; state: State<Datum> }) => string): this;
-    layoutBindings(): Record<Layout, LayoutBinding<Datum>>;
-    layoutBindings(value: Record<Layout, LayoutBinding<Datum>>): this;
+export interface OrgChart<Datum> extends StateGetSet<Datum, OrgChart<Datum>> {
     getChartState(): State<Datum>;
 
     initialZoom(zoomLevel: number): this;
@@ -196,7 +139,7 @@ export class OrgChart<Datum> {
     removeNode(nodeId: NodeId): this;
     calculateCompactFlexDimensions(root: unknown): void;
     calculateCompactFlexPositions(root: unknown): void;
-    update(params: { x0: number; y0: number; width: number; height: number } & Partial<Point>): void;
+    update(params: { x0: number; y0: number; width: number; height: number; } & Partial<Point>): void;
     isEdge(): boolean; // Whether the current browser is Microsoft Edge
     hdiagonal(source: Point, target: Point, m: Point): string;
     diagonal(source: Point, target: Point, m: Point): string;
@@ -205,7 +148,7 @@ export class OrgChart<Datum> {
     setExpansionFlagToChildren(node: HierarchyNode<Datum>, isExpanded: boolean): void;
     expandSomeNodes(node: HierarchyNode<Datum>): void;
     updateNodesState(): void;
-    setLayouts(params: { expandNodesFirst?: boolean }): void;
+    setLayouts(params: { expandNodesFirst?: boolean; }): void;
     collapse(node: HierarchyNode<Datum>): void;
     expand(node: HierarchyNode<Datum>): void;
     zoomed(event: any, node: HierarchyNode<Datum>): void;
@@ -214,9 +157,9 @@ export class OrgChart<Datum> {
         x1: number;
         y0: number;
         y1: number;
-        params: { animate?: boolean; scale?: boolean };
+        params: { animate?: boolean; scale?: boolean; };
     }): void;
-    fit(params?: { animate?: boolean; nodes?: Iterable<HierarchyNode<Datum>>; scale?: boolean }): this;
+    fit(params?: { animate?: boolean; nodes?: Iterable<HierarchyNode<Datum>>; scale?: boolean; }): this;
     setExpanded(nodeId: NodeId, isExpanded?: boolean): this;
     setCentered(nodeId: NodeId): this;
     setHighlighted(nodeId: NodeId): this;
@@ -226,7 +169,7 @@ export class OrgChart<Datum> {
     zoomIn(): void;
     zoomOut(): void;
     toDataUrl(url: string, callback: (result: string | ArrayBuffer) => void): void;
-    exportImg(params?: { full?: boolean; scale?: number; onLoad?: (s: string) => void; save?: boolean }): void;
+    exportImg(params?: { full?: boolean; scale?: number; onLoad?: (s: string) => void; save?: boolean; }): void;
     exportSvg(): this;
     expandAll(): this;
     collapseAll(): this;
@@ -240,6 +183,10 @@ export class OrgChart<Datum> {
     }): void;
     getTextWidth(
         text: string,
-        params: { fontSize?: number; fontWeight?: number; defaultFont?: string; ctx: CanvasRenderingContext2D },
+        params: { fontSize?: number; fontWeight?: number; defaultFont?: string; ctx: CanvasRenderingContext2D; },
     ): number;
 }
+
+export const OrgChart: {
+    new <T>(): OrgChart<T>;
+};
