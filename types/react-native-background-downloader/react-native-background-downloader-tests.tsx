6dc60cbc345cb4da7b11d8f833120ac439c5368b
Philip Su <39933441+fivecar@users.noreply.github.com>
2023-01-21 21:10:39 -0800

    fix: correct types for DownloadTaskState
    It turns out that react-native-background-downloader doesn't actually export this type. Things may seem fine with doing things like `DownloadTaskState.DOWNLOADING` in your code, until you use that in a package that you also then publish yourself. At that point, you'll find the transpiled JS in your package refers to things like react_native_background_downloader_1.DownloadTaskState.DOWNLOADING, which definitely does not exist and will throw at runtime.

--- types/react-native-background-downloader/react-native-background-downloader-tests.tsx
+++ types/react-native-background-downloader/react-native-background-downloader-tests.tsx
@@ -1,9 +1,9 @@
-import RNBackgroundDownloader, { DownloadTask, DownloadTaskState } from 'react-native-background-downloader';
+import RNBackgroundDownloader, { DownloadTask } from 'react-native-background-downloader';
 
 // Set global headers for downloader
 RNBackgroundDownloader.setHeaders({
-  'x-custom': 'Custom Header',
-  'x-custom-2': 'Another Custom Header',
+    'x-custom': 'Custom Header',
+    'x-custom-2': 'Another Custom Header',
 });
 
 const task = RNBackgroundDownloader.download({
@@ -11,7 +11,7 @@ const task = RNBackgroundDownloader.download({
     url: 'https://link-to-very.large/file.zip',
     destination: `${RNBackgroundDownloader.directories.documents}/file.zip`,
     headers: {
-      'x-custom-3': 'a-third-header',
+        'x-custom-3': 'a-third-header',
     },
 })
     .begin(({ expectedBytes }) => {
@@ -30,25 +30,25 @@ const task = RNBackgroundDownloader.download({
 const taskFuncTest = (task: DownloadTask) => {
     // Check task state
     switch (task.state) {
-      case DownloadTaskState.DONE: {
-        console.log('Task is in state DONE');
-        break;
-      }
-      case DownloadTaskState.DOWNLOADING: {
-        console.log('Task is in state DOWNLOADING');
-        break;
-      }
-      case DownloadTaskState.PAUSED: {
-        console.log('Task is in state PAUSED');
-        break;
-      }
-      case DownloadTaskState.FAILED: {
-        console.log('Task is in state FAILED');
-        break;
-      }
-      case DownloadTaskState.STOPPED: {
-        console.log('Task is in state STOPPED');
-      }
+        case 'DONE': {
+            console.log('Task is in state DONE');
+            break;
+        }
+        case 'DOWNLOADING': {
+            console.log('Task is in state DOWNLOADING');
+            break;
+        }
+        case 'PAUSED': {
+            console.log('Task is in state PAUSED');
+            break;
+        }
+        case 'FAILED': {
+            console.log('Task is in state FAILED');
+            break;
+        }
+        case 'STOPPED': {
+            console.log('Task is in state STOPPED');
+        }
     }
 
     // Pause the task
