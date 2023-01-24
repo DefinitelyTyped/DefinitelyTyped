1bc938a40ebbf977ebff0cc5b98651e9d243a289
Anders K <and3k5@users.noreply.github.com>
2023-01-24 06:55:54 +0000

    Remove wrong 'export as namespace'

--- types/js-crc/index.d.ts
+++ types/js-crc/index.d.ts
@@ -6,5 +6,3 @@
 export function crc32(message: string | ArrayBuffer | Uint8Array | ReadonlyArray<number>): string;
 
 export function crc16(message: string | ArrayBuffer | Uint8Array | ReadonlyArray<number>): string;
-
-export as namespace jsCrc;
