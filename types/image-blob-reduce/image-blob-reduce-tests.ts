eaf46437796e6d0ada92a7fb7884f57e6ab6719d
Matthias Fischer <Matthias.Fischer@eschbach.com>
2023-01-25 10:41:39 +0100

    added types for image-blob-reduce

---
+++ types/image-blob-reduce/image-blob-reduce-tests.ts
@@ -0,0 +1,38 @@
+import pica from 'pica';
+import imageBlobReduce, { Options, ResizeOptions } from 'image-blob-reduce';
+
+const createOptions: Options = {
+  pica,
+};
+
+const imageReducer = imageBlobReduce(createOptions);
+
+imageReducer.use((args) => {
+    console.log(args);
+}, 'arg1', 'arg2');
+
+imageReducer.before('_calculate_size', (env) => new Promise((resolve) => resolve(env)));
+
+imageReducer.after('_transform', (env) => new Promise((resolve) => resolve(env)));
+
+const blob = new Blob([''], { type: 'image/png' });
+
+const resizeOptions: ResizeOptions = {
+    max: 100,
+    unsharpAmount: 80,
+    unsharpRadius: 0.6,
+    unsharpThreshold: 2,
+};
+
+imageReducer.toBlob(blob, resizeOptions).then((result: Blob) => {
+    console.log(result);
+});
+
+imageReducer.toCanvas(blob, {
+    max: 100,
+    unsharpAmount: 80,
+    unsharpRadius: 0.6,
+    unsharpThreshold: 2,
+}).then((result: HTMLCanvasElement) => {
+    console.log(result);
+});
