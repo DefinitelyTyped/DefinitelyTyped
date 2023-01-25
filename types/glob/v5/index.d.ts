565b1e698503d4b4d42670527ef17860035240b8
Nico Jansen <jansennico@gmail.com>
2023-01-25 13:43:31 +0100

    fix(glob): use build in minimatch types

--- types/glob/v5/index.d.ts
+++ types/glob/v5/index.d.ts
@@ -23,7 +23,7 @@ declare namespace G {
     let Glob: IGlobStatic;
     let GlobSync: IGlobSyncStatic;
 
-    interface IOptions extends minimatch.IOptions {
+    interface IOptions extends minimatch.MinimatchOptions {
         cwd?: string | undefined;
         root?: string | undefined;
         dot?: boolean | undefined;
@@ -69,7 +69,7 @@ declare namespace G {
     }
 
     interface IGlobBase {
-        minimatch: minimatch.IMinimatch;
+        minimatch: minimatch.Minimatch;
         options: IOptions;
         aborted: boolean;
         cache: { [path: string]: any /* boolean | string | string[] */ };
