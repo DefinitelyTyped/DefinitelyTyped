c0749deada6a3d41f8d588d02f20d335564755e5
Nathan Shively-Sanders <293473+sandersn@users.noreply.github.com>
2023-01-27 09:42:30 -0800

    Add tests for aws-sdk2-types

--- types/aws-sdk2-types/aws-sdk2-types-tests.ts
+++ types/aws-sdk2-types/aws-sdk2-types-tests.ts
@@ -1,2 +1,19 @@
 import * as AWS from 'aws-sdk2-types';
-// TODO: Some tests based on amazon-dax-client, dta-api-client, leso, nodemailer-ses-transport, s3-streams, aws-param-store, dynogels, s3-download-stream, s3-upload-stream
+function ssm(filter: AWS.SSM.Types.ParameterStringFilter) {
+    return filter.Key === "Stroustrup";
+}
+function rds(config: AWS.RDSDataService.ClientConfiguration) {
+    return config.region === "us-east-1" && !!config.computeChecksums;
+}
+function dyn(u: AWS.DynamoDB.Update, doc: AWS.DynamoDB.DocumentClient) {
+    if (u?.ExpressionAttributeNames) {
+        u.ExpressionAttributeNames["#n"] = "name";
+    }
+    doc.batchWrite({ RequestItems: { table: [{ PutRequest: { Item: { name: "Copilot" } } }] } });
+}
+function s3(loc: AWS.S3.S3Location) {
+    return loc.StorageClass === "STANDARD";
+}
+function sqs(x: AWS.SQS.QueueAttributeMap) {
+    return x["VisibilityTimeout"] === "30";
+}
