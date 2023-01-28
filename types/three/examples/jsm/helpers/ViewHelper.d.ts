429cd5a812cc44152f5198ea206a69762396378a
Ed Preston <epreston@prestonsoft.com>
2023-01-28 13:53:09 +1100

    Add missing ViewHelper type declaration

---
+++ types/three/examples/jsm/helpers/ViewHelper.d.ts
@@ -0,0 +1,19 @@
+import {
+    Camera,
+	Object3D,
+	Vector3,
+    WebGLRenderer
+} from '../../../src/Three';
+
+export class ViewHelper extends Object3D {
+    constructor(camera: Camera, domElement: HTMLElement);
+
+    readonly isViewHelper: true;
+    animating: boolean;
+    center: Vector3;
+
+    render(renderer: WebGLRenderer): void;
+    handleClick(event: Event): boolean;
+    update(): void;
+    dispose(): void;
+}
