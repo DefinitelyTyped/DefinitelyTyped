502aba63546be78540c03562b19a98984de001c6
lublak <44057030+lublak@users.noreply.github.com>
2023-01-30 11:26:43 +0100

    add missing methods

--- types/bwip-js/index.d.ts
+++ types/bwip-js/index.d.ts
@@ -10,13 +10,9 @@
 import { IncomingMessage as Request, ServerResponse as Response } from "http";
 
 declare namespace BwipJs {
-    export function loadFont(fontName: string, sizeMulti: number, fontFile: string): void;
-    export function toBuffer(opts: ToBufferOptions, callback: (err: string | Error, png: Buffer) => void): void;
-    export function toBuffer(opts: ToBufferOptions): Promise<Buffer>;
-    export function toCanvas(canvas: string | HTMLCanvasElement, opts: ToBufferOptions): HTMLCanvasElement;
-    interface ToBufferOptions {
-        bcid: string;
-        text: string;
+    export interface BwippOptions {
+        includecheck?: boolean | undefined;
+        includecheckintext?: boolean | undefined;
 
         parse?: boolean | undefined;
         parsefnc?: boolean | undefined;
@@ -24,22 +20,16 @@ declare namespace BwipJs {
         height?: number | undefined;
         width?: number | undefined;
 
-        scaleX?: number | undefined;
-        scaleY?: number | undefined;
-        scale?: number | undefined;
-
-        rotate?: "N" | "R" | "L" | "I" | undefined;
-
-        paddingwidth?: number | undefined;
-        paddingheight?: number | undefined;
-
-        monochrome?: boolean | undefined;
-        alttext?: string | undefined;
+        inkspread?: number | undefined;
+        inkspreadh?: number | undefined;
+        inkspreadv?: number | undefined;
+        dotty?: boolean | undefined;
 
         includetext?: boolean | undefined;
         textfont?: string | undefined;
         textsize?: number | undefined;
         textgaps?: number | undefined;
+        alttext?: string | undefined;
 
         textxalign?: "offleft" | "left" | "center" | "right" | "offright" | "justify" | undefined;
         textyalign?: "below" | "center" | "above" | undefined;
@@ -70,19 +60,63 @@ declare namespace BwipJs {
         guardrightpos?: number | undefined;
         guardleftypos?: number | undefined;
         guardrightypos?: number | undefined;
-
-        sizelimit?: number | undefined;
-
-        includecheck?: boolean | undefined;
-        includecheckintext?: boolean | undefined;
-
-        inkspread?: number | undefined;
-        inkspreadh?: number | undefined;
-        inkspreadv?: number | undefined;
     }
+    export interface RenderOptions extends BwippOptions {
+        bcid: string;
+        text: string;
+
+        scaleX?: number | undefined;
+        scaleY?: number | undefined;
+        scale?: number | undefined;
+
+        rotate?: "N" | "R" | "L" | "I" | undefined;
+
+        paddingwidth?: number | undefined;
+        paddingheight?: number | undefined;
+
+        paddingleft?: number | undefined;
+        paddingright?: number | undefined;
+        paddingtop?: number | undefined;
+        paddingbottom?: number | undefined;
+
+        monochrome?: boolean | undefined;
+        
+        sizelimit?: number | undefined;
+    }
+    export interface RawOptions extends BwippOptions {
+        bcid: string;
+        text: string;
+    }
+    export type ToBufferOptions = RenderOptions;
+    export interface DrawingOption<T> {
+        scale(sx:number, sy:number):[number, number];
+        measure(str:string, font:string, fwidth:number, fheight:number):{ width:number, ascent:number, descent:number };
+        init(width:number, height:number):void;
+        line(x0:number, y0:number, x1:number, y1:number, lw:number, rgb:string):void;
+        polygon(pts:([number, number])[]):void;
+        hexagon(pts:[[number, number], [number, number], [number, number], [number, number], [number, number]]):void;
+        ellipse(x:number, y:number, rx:number, ry:number, ccw:boolean):void;
+        fill(rgb:string):void;
+        text(x:number, y:number, str:string, rgb:string, font:{ name:string, width:number, height: number, dx:number }):void;
+        end():T;
+    }
+    export function loadFont(fontName: string, sizeMulti: number, fontFile: string): void;
+    export function toBuffer(opts: RenderOptions, callback: (err: string | Error, png: Buffer) => void): void;
+    export function toBuffer(opts: RenderOptions): Promise<Buffer>;
+    export function toCanvas(canvas: string | HTMLCanvasElement, opts: RenderOptions): HTMLCanvasElement;
+    export function toDataURL(opts: RenderOptions, callback: (err: string | Error, png: { width: number, height: number, uri: string }) => void): void;
+    export function toDataURL(opts: RenderOptions): Promise<{ width: number, height: number, uri: string }>;
+    export function toCanvas(id: string, opts:RenderOptions): void;
+    export function fixupOptions(opts: RenderOptions): RenderOptions;
+    export function loadFont(name:string, size:number, data: Uint8Array | string): void;
+    export function render<T>(params: RenderOptions, drawing: DrawingOption<T>): T;
+    export function raw(options: RawOptions): void;
+    export function raw(bcid: string, text: string, opts: string): void;
+    export function raw(bcid: string, text: string, opts: BwippOptions): void;
+    export function request(req: Request, res: Response, opts?: BwipJs.RenderOptions): void;
 }
 
-declare function BwipJs(req: Request, res: Response, opts?: BwipJs.ToBufferOptions): void;
+declare function BwipJs(req: Request, res: Response, opts?: BwipJs.RenderOptions): void;
 
 /**
  * The Browser version of the library's functionality, which makes use of an HTMLCanvasElement for rendering.
