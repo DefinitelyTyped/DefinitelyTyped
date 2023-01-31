05843844341c51abfeaaee6f68f328eeeedfedf7
John Slemmer <john.slemmer@gmail.com>
2023-01-31 09:23:39 -0700

    update PropsWithoutRef to use Omit
    This improves over the current Pick-Exclude making the resulting less restrictive.  See  https://github.com/DefinitelyTyped/DefinitelyTyped/discussions/64163

--- types/react/index.d.ts
+++ types/react/index.d.ts
@@ -785,7 +785,7 @@ declare namespace React {
         // Pick would not be sufficient for this. We'd like to avoid unnecessary mapping and need a distributive conditional to support unions.
         // see: https://www.typescriptlang.org/docs/handbook/2/conditional-types.html#distributive-conditional-types
         // https://github.com/Microsoft/TypeScript/issues/28339
-        P extends any ? ('ref' extends keyof P ? Pick<P, Exclude<keyof P, 'ref'>> : P) : P;
+        P extends any ? ('ref' extends keyof P ? Omit<P, 'ref'> : P) : P;
     /** Ensures that the props do not include string ref, which cannot be forwarded */
     type PropsWithRef<P> =
         // Just "P extends { ref?: infer R }" looks sufficient, but R will infer as {} if P is {}.
