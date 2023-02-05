f7eea77a2bc873ba7cf94d71c4adb50038813612
Joshua Hoeflich <joshuaharry411@icloud.com>
2023-02-04 19:32:11 -0600

    Change test

--- types/align-text/test/center-all-arguments.ts
+++ types/align-text/test/center-all-arguments.ts
@@ -1,13 +1,13 @@
 import align from "align-text";
 
 function centerAlign(len: number, longest: number, line: string, lines: string[], idx: number) {
-    console.log(line);
-    console.log(lines);
-    console.log(idx);
     return {
         character: "\t",
         indent: Math.floor((longest - len) / 2),
-        prefix: "~ "
+        prefix: "~ ",
+        line,
+        lines,
+        idx
     };
 }
 
