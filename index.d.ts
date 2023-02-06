ba7b0bd51aac8a2cb7df02092645ad003d1fb8bb
Santi <santyrojasprieto9@gmail.com>
2023-02-06 15:33:27 -0500

    Add files via upload

---
+++ index.d.ts
@@ -0,0 +1,18 @@
+// Type definitions for git-init
+// Project: git-init
+import { type ExecException, exec } from "node:child_process";
+
+declare type Callback = (error: ExecException | null, stdout: string, stderr: string) => void;
+
+/**
+ * Initializes a git repo in the current working directory asynchronously.
+ * 
+ * @param cb An {@link exec} compatible callback.
+ */
+declare function init(cb: Callback): void;
+/**
+ * Initializes a git repo asynchronously.
+ * @param path A path to the directory to initialize.
+ * @param cb An {@link exec} compatible callback.
+ */
+declare function init(path: string, cb: Callback): void;
