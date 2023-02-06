5ecf923293f3afb54d79dd47802c2cf7ceff22b8
Santi <santyrojasprieto9@gmail.com>
2023-02-06 15:09:01 -0500

    Create index.d.ts

---
+++ types/git-init/index.d.ts
@@ -0,0 +1,16 @@
+import { type ExecException, exec } from "node:child_process";
+
+declare type Callback = (error: ExecException | null, stdout: string, stderr: string) => void;
+
+/**
+ * Initializes a git repo in the current working directory asynchronously.
+ * 
+ * @param cb An {@link exec} compatible callback.
+ */
+declare function init(cb: Callback): void;
+/**
+ * Initializes a git repo asynchronously.
+ * @param path A path to the directory to initialize.
+ * @param cb An {@link exec} compatible callback.
+ */
+declare function init(path: string, cb: Callback): void;
