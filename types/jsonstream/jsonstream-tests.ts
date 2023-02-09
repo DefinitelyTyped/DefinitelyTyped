b59b8681a611495a27e19ff177de60cad0651520
Daniel Rosenwasser <DanielRosenwasser@users.noreply.github.com>
2023-02-09 15:57:10 -0800

    Trailing whitespace.

--- types/jsonstream/jsonstream-tests.ts
+++ types/jsonstream/jsonstream-tests.ts
@@ -4,11 +4,11 @@ import json = require('jsonstream');
 export function foo(read: NodeJS.ReadableStream) {
     read = read.pipe(json.parse('*'));
     read = read.pipe(json.parse(['foo/*', 'bar/*']));
-    
+
     read = json.stringify();
     read = json.stringify(false);
     read = json.stringify('{', ',', '}');
-    
+
     read = json.stringifyObject();
     read = json.stringifyObject('{', ',', '}');
 }
