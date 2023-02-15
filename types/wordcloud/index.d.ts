9f57328837e82f83efc6579389880ad62d289d88
Pig Fang <g-plane@hotmail.com>
2023-02-15 08:21:39 +0800

    update library version

--- types/wordcloud/index.d.ts
+++ types/wordcloud/index.d.ts
@@ -1,4 +1,4 @@
-// Type definitions for wordcloud 1.1
+// Type definitions for wordcloud 1.2
 // Project: https://github.com/timdream/wordcloud2.js, http://timdream.org/wordcloud2.js
 // Definitions by: Joe Skeen <https://github.com/joeskeen>
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
