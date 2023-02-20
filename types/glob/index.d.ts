39ff0a39fb955d4567edf9af3e74e938445dc41d
John Dzak <john.dzak@txidigital.com>
2023-02-20 23:48:05 +0000

    Update version in header

--- types/glob/index.d.ts
+++ types/glob/index.d.ts
@@ -1,4 +1,4 @@
-// Type definitions for glob 8.0
+// Type definitions for glob 8.1.0
 // Project: https://github.com/isaacs/node-glob
 // Definitions by: vvakame <https://github.com/vvakame>
 //                 voy <https://github.com/voy>
