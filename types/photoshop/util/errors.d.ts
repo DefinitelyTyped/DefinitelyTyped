303cad85d5e60cbd4492b1b118cffd1df4ed317e
Jaroslav <jar87876@adobe.com>
2023-02-21 21:42:55 +0100

    removes unused file

--- types/photoshop/util/errors.d.ts
+++
@@ -1,8 +0,0 @@
-/**
- * Replace all of the ^0, ^1, ^n strings found in zstring with strings in array of replaces.
- * If not array is provided then string is only translated.
- * @param zstring - ZString to translate and replace ^0 strings
- * @param replaces - Array of ZStrings or strings to use for replacement
- * @returns translated string
- */
-export declare function replaceAndTranslate(zstring: string, replaces?: string[]): string;
