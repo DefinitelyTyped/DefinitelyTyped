0a1dd7d73353eaa0f1b08810f0dabcdf63e19b91
Jaroslav <jar87876@adobe.com>
2023-02-21 21:46:30 +0100

    adds test

--- types/photoshop/test/photoshop-tests.cjs.ts
+++ types/photoshop/test/photoshop-tests.cjs.ts
@@ -1,4 +1,5 @@
 import photoshop from "photoshop";
 
 photoshop.app.activeDocument; // $ExpectType Document
+photoshop.app.foregroundColor; // $ExpectType SolidColor
 photoshop.action.batchPlay([], {}); // $ExpectType Promise<ActionDescriptor[]>
