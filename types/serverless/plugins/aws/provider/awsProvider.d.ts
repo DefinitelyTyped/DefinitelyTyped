f371c3453d2db227c0b4324088f97984972f9df5
eacet <44721895+eacet@users.noreply.github.com>
2023-02-21 14:40:00 +0300

    Apply suggestions from code review
    Co-authored-by: Gareth Jones <Jones258@Gmail.com>

--- types/serverless/plugins/aws/provider/awsProvider.d.ts
+++ types/serverless/plugins/aws/provider/awsProvider.d.ts
@@ -761,7 +761,7 @@ declare namespace Aws {
 declare class Aws {
     constructor(serverless: Serverless, options: Serverless.Options);
 
-    naming: { [key: string]: (param: string) => string };
+    naming: { [key: string]: (param?: string) => string };
     getCredentials(): Aws.Credentials;
     getProviderName(): string;
     getRegion(): string;
