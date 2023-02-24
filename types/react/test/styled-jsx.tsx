6e1483b5dbddd9d6e312b9c64787f0257eb55172
Sora Tonami <ms0503@outlook.com>
2023-02-25 01:03:12 +0900

    Add test for styled-jsx to react

---
+++ types/react/test/styled-jsx.tsx
@@ -0,0 +1,9 @@
+function StyledJSXTest(): JSX.Element {
+    return (
+        <style jsx global>{`
+            a {
+                text-decoration: none;
+            }
+        `}</style>
+    );
+}
