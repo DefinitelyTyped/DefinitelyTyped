e23b8b676373f8762f5307e5b2f9529aafc118dc
Nicu Chiciuc <nicuchiciuc@gmail.com>
2023-02-27 16:39:55 +0200

    update declaration file

--- types/emailjs-mime-codec/index.d.ts
+++ types/emailjs-mime-codec/index.d.ts
@@ -3,37 +3,212 @@
 // Definitions by: Nicu Chiciuc <https://github.com/nicu-chiciuc>
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
 
-/*~ If this module is a UMD module that exposes a global variable 'myLib' when
- *~ loaded outside a module loader environment, declare that global here.
- *~ Otherwise, delete this declaration.
+//// From "./charset.d.ts"
+/**
+ * Encodes an unicode string into an Uint8Array object as UTF-8
+ *
+ * @param {String} str String to be encoded
+ * @return {Uint8Array} UTF-8 encoded typed array
  */
-export as namespace myLib;
+export const encode: (str: string) => Uint8Array;
 
-/*~ If this module has methods, declare them as functions like so.
+/**
+ * Decodes a string from Uint8Array to an unicode string using specified encoding
+ *
+ * @param {Uint8Array} buf Binary data to be decoded
+ * @param {String} Binary data is decoded into string using this charset
+ * @return {String} Decoded string
  */
-export function myMethod(a: string): string;
-export function myOtherMethod(a: number): number;
+export function decode(buf: Uint8Array, fromCharset?: string): string;
 
-/*~ You can declare types that are available via importing the module */
-export interface someType {
-    name: string;
-    length: number;
-    extras?: string[];
-}
-
-/*~ You can declare properties of the module using const, let, or var */
-export const myField: number;
-
-/*~ If there are types, properties, or methods inside dotted names
- *~ of the module, declare them inside a 'namespace'.
+/**
+ * Convert a string from specific encoding to UTF-8 Uint8Array
+ *
+ * @param {String|Uint8Array} data Data to be encoded
+ * @param {String} Source encoding for the string (optional for data of type String)
+ * @return {Uint8Array} UTF-8 encoded typed array
  */
-export namespace subProp {
-    /*~ For example, given this definition, someone could write:
-     *~   import { subProp } from 'yourModule';
-     *~   subProp.foo();
-     *~ or
-     *~   import * as yourMod from 'yourModule';
-     *~   yourMod.subProp.foo();
-     */
-    function foo(): void;
-}
+export const convert: (data: string | Uint8Array, fromCharset: string) => Uint8Array;
+
+//// From "./mimecodec.d.ts"
+
+/**
+ * Encodes all non printable and non ascii bytes to =XX form, where XX is the
+ * byte value in hex. This function does not convert linebreaks etc. it
+ * only escapes character sequences
+ *
+ * @param {String|Uint8Array} data Either a string or an Uint8Array
+ * @param {String} [fromCharset='UTF-8'] Source encoding
+ * @return {String} Mime encoded string
+ */
+export function mimeEncode(data?: string | Uint8Array, fromCharset?: string): string;
+
+/**
+ * Decodes mime encoded string to an unicode string
+ *
+ * @param {String} str Mime encoded string
+ * @param {String} [fromCharset='UTF-8'] Source encoding
+ * @return {String} Decoded unicode string
+ */
+export function mimeDecode(str?: string, fromCharset?: string): string;
+
+/**
+ * Encodes a string or an typed array of given charset into unicode
+ * base64 string. Also adds line breaks
+ *
+ * @param {String|Uint8Array} data String or typed array to be base64 encoded
+ * @param {String} Initial charset, e.g. 'binary'. Defaults to 'UTF-8'
+ * @return {String} Base64 encoded string
+ */
+export function base64Encode(data: string | Uint8Array, fromCharset?: string): string;
+
+/**
+ * Decodes a base64 string of any charset into an unicode string
+ *
+ * @param {String} str Base64 encoded string
+ * @param {String} [fromCharset='UTF-8'] Original charset of the base64 encoded string
+ * @return {String} Decoded unicode string
+ */
+export function base64Decode(str: string, fromCharset?: string): string;
+
+/**
+ * Encodes a string or an Uint8Array into a quoted printable encoding
+ * This is almost the same as mimeEncode, except line breaks will be changed
+ * as well to ensure that the lines are never longer than allowed length
+ *
+ * @param {String|Uint8Array} data String or an Uint8Array to mime encode
+ * @param {String} [fromCharset='UTF-8'] Original charset of the string
+ * @return {String} Mime encoded string
+ */
+export function quotedPrintableEncode(data?: string | Uint8Array, fromCharset?: string): string;
+
+/**
+ * Decodes a string from a quoted printable encoding. This is almost the
+ * same as mimeDecode, except line breaks will be changed as well
+ *
+ * @param {String} str Mime encoded string to decode
+ * @param {String} [fromCharset='UTF-8'] Original charset of the string
+ * @return {String} Mime decoded string
+ */
+export function quotedPrintableDecode(str?: string, fromCharset?: string): string;
+
+/**
+ * Encodes a string or an Uint8Array to an UTF-8 MIME Word
+ *   https://tools.ietf.org/html/rfc2047
+ *
+ * @param {String|Uint8Array} data String to be encoded
+ * @param {String} mimeWordEncoding='Q' Encoding for the mime word, either Q or B
+ * @param {String} [fromCharset='UTF-8'] Source sharacter set
+ * @return {String} Single or several mime words joined together
+ */
+export function mimeWordEncode(data: string | Uint8Array, mimeWordEncoding?: string, fromCharset?: string): string;
+
+/**
+ * Finds word sequences with non ascii text and converts these to mime words
+ *
+ * @param {String|Uint8Array} data String to be encoded
+ * @param {String} mimeWordEncoding='Q' Encoding for the mime word, either Q or B
+ * @param {String} [fromCharset='UTF-8'] Source sharacter set
+ * @return {String} String with possible mime words
+ */
+export function mimeWordsEncode(data?: string | Uint8Array, mimeWordEncoding?: string, fromCharset?: string): string;
+
+/**
+ * Decode a complete mime word encoded string
+ *
+ * @param {String} str Mime word encoded string
+ * @return {String} Decoded unicode string
+ */
+export function mimeWordDecode(str?: string): string;
+
+/**
+ * Decode a string that might include one or several mime words
+ *
+ * @param {String} str String including some mime words that will be encoded
+ * @return {String} Decoded unicode string
+ */
+export function mimeWordsDecode(str?: string): string;
+
+/**
+ * Folds long lines, useful for folding header lines (afterSpace=false) and
+ * flowed text (afterSpace=true)
+ *
+ * @param {String} str String to be folded
+ * @param {Boolean} afterSpace If true, leave a space in th end of a line
+ * @return {String} String with folded lines
+ */
+export function foldLines(str?: string, afterSpace?: boolean): string;
+
+/**
+ * Encodes and folds a header line for a MIME message header.
+ * Shorthand for mimeWordsEncode + foldLines
+ *
+ * @param {String} key Key name, will not be encoded
+ * @param {String|Uint8Array} value Value to be encoded
+ * @param {String} [fromCharset='UTF-8'] Character set of the value
+ * @return {String} encoded and folded header line
+ */
+export function headerLineEncode(key: string, value: string | Uint8Array, fromCharset?: string): string;
+
+/**
+ * The result is not mime word decoded, you need to do your own decoding based
+ * on the rules for the specific header key
+ *
+ * @param {String} headerLine Single header line, might include linebreaks as well if folded
+ * @return {Object} And object of {key, value}
+ */
+export function headerLineDecode(headerLine?: string): {
+    key: string;
+    value: string;
+};
+
+/**
+ * Parses a block of header lines. Does not decode mime words as every
+ * header might have its own rules (eg. formatted email addresses and such)
+ *
+ * @param {String} headers Headers string
+ * @return {Object} An object of headers, where header keys are object keys. NB! Several values with the same key make up an Array
+ */
+export function headerLinesDecode(headers: string): Record<string, string | string[]>;
+
+/**
+ * Parses a header value with key=value arguments into a structured
+ * object.
+ *
+ *   parseHeaderValue('content-type: text/plain; CHARSET='UTF-8'') ->
+ *   {
+ *     'value': 'text/plain',
+ *     'params': {
+ *       'charset': 'UTF-8'
+ *     }
+ *   }
+ *
+ * @param {String} str Header value
+ * @return {Object} Header value as a parsed structure
+ */
+export function parseHeaderValue(str: string): {
+    value: string | false;
+    params: Record<string, string>;
+};
+
+/**
+ * Encodes a string or an Uint8Array to an UTF-8 Parameter Value Continuation encoding (rfc2231)
+ * Useful for splitting long parameter values.
+ *
+ * For example
+ *      title="unicode string"
+ * becomes
+ *     title*0*="utf-8''unicode"
+ *     title*1*="%20string"
+ *
+ * @param {String|Uint8Array} data String to be encoded
+ * @param {Number} [maxLength=50] Max length for generated chunks
+ * @param {String} [fromCharset='UTF-8'] Source sharacter set
+ * @return {Array} A list of encoded keys and headers
+ */
+export function continuationEncode(
+    key: string | Uint8Array,
+    data: string,
+    maxLength: number,
+    fromCharset?: string,
+): Array<Record<string, unknown>>;
