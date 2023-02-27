d3ed2494617be84fb8c1f408c1cc28870467d6eb
Chemi Atlow <chemiatlow@gmail.com>
2023-02-28 01:10:32 +0200

    rename (#64484)

--- types/name-initials/index.d.ts
+++ types/name-initials/index.d.ts
@@ -1,6 +1,6 @@
 // Type definitions for name-initials 0.1
 // Project: https://github.com/ericvera/name-initials#readme
-// Definitions by: ChemiAtlow <https://github.com/ChemiAtlow>
+// Definitions by: atlowChemi <https://github.com/atlowChemi>
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
 
 export default function nameInitials(name: string): string;
