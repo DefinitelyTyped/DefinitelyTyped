826acfb71c5c58cea279dd1cc3ef4a21a1284cc0
yurj26 <1816387074@qq.com>
2023-02-28 20:50:13 +0800

    update types for uni-app

--- types/uni-app/index.d.ts
+++ types/uni-app/index.d.ts
@@ -1,12 +1,8 @@
-// Type definitions for non-npm package uni-app 1.4
+// Type definitions for non-npm package uni-app 2.0
 // Project: https://github.com/dcloudio/uni-app
 // Definitions by: DCloud <https://github.com/dcloudio>
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
 
 // TypeScript Version: 2.3
 
-/// <reference path="./lib/vue.d.ts" />
-/// <reference path="./lib/common.d.ts" />
-/// <reference path="./lib/app.d.ts" />
-/// <reference path="./lib/page.d.ts" />
-/// <reference path="./lib/uni.d.ts" />
+/// <reference types="@dcloudio/types" />
--- types/uni-app/lib/common.d.ts
+++
@@ -1,5 +0,0 @@
-type AnyObject = Record<string, any>;
-
-type KVInfer<T> = { [K in keyof T]: T[K] };
-
-type Void<T> = T | undefined | null;
