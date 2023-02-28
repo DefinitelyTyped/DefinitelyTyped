b25350c51246e84e769a39f89022ee062581b066
yurj26 <1816387074@qq.com>
2023-02-28 21:05:19 +0800

    update types for html5plus

--- types/html5plus/index.d.ts
+++ types/html5plus/index.d.ts
@@ -1,7 +1,6 @@
-// Type definitions for non-npm package html5plus 1.1
+// Type definitions for non-npm package html5plus 2.0
 // Project: https://www.html5plus.org/doc/
 // Definitions by: Dcloud <https://github.com/dcloudio>
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
 
-/// <reference path="./lib/HBuilderX.d.ts" />
-/// <reference path="./plus.d.ts" />
+/// <reference types="@dcloudio/types/html5plus" />
