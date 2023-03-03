400d5b440d9f101ba60a1a304394516cc8a9def5
Jan Beseda <jan.beseda@ui.com>
2023-03-03 12:22:58 +0100

    fix(namespace): export as namespace and add owner

--- types/sd-notify/index.d.ts
+++ types/sd-notify/index.d.ts
@@ -1,53 +1,52 @@
 // Type definitions for sd-notify 2.8
 // Project: https://github.com/systemd/node-sd-notify#readme
 // Definitions by: Jan Beseda <https://github.com/Cheprer>
+//                 Rory Bradford <https://github.com/roryrjb>
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
 // Minimum TypeScript Version: 4.7
 
-declare namespace SdNotify {
-    type JournalPrint = (args: string[]) => void;
+export type JournalPrint = (args: string[]) => void;
 
-    function ready(): void;
-    /**
-     * @param interval milliseconds
-     */
-    function startWatchdogMode(interval: number): void;
-    function stopWatchdogMode(): void;
-    /**
-     * @param text status string to systemd, which will append to the service's log
-     */
-    function sendStatus(text: string): void;
-    const journalPrint: JournalPrint;
-    interface Log {
-        emerg: JournalPrint;
-        emergency: JournalPrint;
-        alert: JournalPrint;
-        crit: JournalPrint;
-        critical: JournalPrint;
-        err: JournalPrint;
-        error: JournalPrint;
-        warning: JournalPrint;
-        warn: JournalPrint;
-        notice: JournalPrint;
-        info: JournalPrint;
-        debug: JournalPrint;
-    }
-
-    // internal functions
-
-    function stopping(pid: number): void;
-    function watchdog(): void;
-
-    /**
-     * Check if the process was called by systemd with Watchdog mode enabled
-     * @returns the amount of milliseconds watchdog has been set to, or 0 if it has not been set
-     */
-    function watchdogInterval(): number;
-
-    /**
-     * Unwrapped sendStatus. Sends raw string without prepending `STATUS=` and appending new line char `\n`
-     */
-    function sendState(args: string[]): void;
+export function ready(): void;
+/**
+ * @param interval milliseconds
+ */
+export function startWatchdogMode(interval: number): void;
+export function stopWatchdogMode(): void;
+/**
+ * @param text status string to systemd, which will append to the service's log
+ */
+export function sendStatus(text: string): void;
+export const journalPrint: JournalPrint;
+export interface Log {
+    emerg: JournalPrint;
+    emergency: JournalPrint;
+    alert: JournalPrint;
+    crit: JournalPrint;
+    critical: JournalPrint;
+    err: JournalPrint;
+    error: JournalPrint;
+    warning: JournalPrint;
+    warn: JournalPrint;
+    notice: JournalPrint;
+    info: JournalPrint;
+    debug: JournalPrint;
 }
 
-export = SdNotify;
+// internal functions
+
+export function stopping(pid: number): void;
+export function watchdog(): void;
+
+/**
+ * Check if the process was called by systemd with Watchdog mode enabled
+ * @returns the amount of milliseconds watchdog has been set to, or 0 if it has not been set
+ */
+export function watchdogInterval(): number;
+
+/**
+ * Unwrapped sendStatus. Sends raw string without prepending `STATUS=` and appending new line char `\n`
+ */
+export function sendState(args: string[]): void;
+
+export as namespace SdNotify;
