c877650577c71d6262ce430a213e56c75e6745eb
Trevor McMaster <trevor.mcmaster@familysearch.org>
2023-03-03 10:41:03 -0700

    Added back the LOCAL enum after confirming it's still there in 0.28.0-alpha.21

--- types/nodegit/clone.d.ts
+++ types/nodegit/clone.d.ts
@@ -1,6 +1,15 @@
 import { Repository } from './repository';
 import { CloneOptions } from './clone-options';
 
+export namespace Clone {
+    const enum LOCAL {
+        AUTO = 0,
+        LOCAL = 1,
+        NO_LOCAL = 2,
+        NO_LINKS = 3,
+    }
+}
+
 /**
  * Patch repository cloning to automatically coerce objects.
  */
