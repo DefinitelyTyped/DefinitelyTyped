3d1f7327f97ed36165db33683874415924ba03f1
tevem1207 <tevem1207@gmail.com>
2023-03-10 23:55:36 +0900

    Fixed all

--- types/vue3-carousel-3d/index.d.ts
+++ types/vue3-carousel-3d/index.d.ts
@@ -1,5 +1,5 @@
 // Type definitions for vue3-carousel-3d 1.0
-// Project: https://github.com/Ray93/vue3-carousel-3d#readme
+// Project: https://github.com/Ray93/vue3-carousel-3d
 // Definitions by: tevem1207 <https://github.com/tevem1207>
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
 // TypeScript Version: 4.9
--- types/vue3-carousel-3d/vue3-carousel-3d-tests.ts
+++ types/vue3-carousel-3d/vue3-carousel-3d-tests.ts
@@ -1,15 +1,15 @@
-import { createApp } from 'vue/dist/vue.esm-bundler';
+import { createApp } from 'vue';
 import { Carousel3d, Slide } from 'vue3-carousel-3d';
 import 'vue3-carousel-3d/dist/index.css';
 
 createApp({
     components: {
-        Carousel3d: Carousel3d,
-        Slide: Slide,
+        'carousel-3d': Carousel3d,
+        slide: Slide,
     },
     template: `
-    <carousel-3d 
-        :controls-visible="true" 
+    <carousel-3d
+        :controls-visible="true"
         :autoplay="true"
         :autoplay-timeout="1000"
         :count="0"
