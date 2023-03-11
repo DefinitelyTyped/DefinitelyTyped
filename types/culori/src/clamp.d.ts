123fdf661143d61e6e178368ed57b25d87162197
Bijela Gora <bijela_gora@protonmail.com>
2023-03-11 17:00:12 +0200

    Fix "This overload and the one on line 7 can be combined into one signature with an optional parameter." error

--- types/culori/src/clamp.d.ts
+++ types/culori/src/clamp.d.ts
@@ -4,6 +4,5 @@ export function clampRgb(color: string): Color | undefined;
 export function clampRgb<C extends Color>(color: C): C;
 
 export function clampChroma(color: string): Color | undefined;
-export function clampChroma<C extends Color>(color: C): C;
-export function clampChroma<C extends Color>(color: C, mode: Mode): C;
+export function clampChroma<C extends Color>(color: C, mode?: Mode): C;
 export function clampChroma<C extends Color>(color: string, mode: Mode): Color | undefined;
