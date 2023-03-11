3decb47d6e73db15b5ffd437a4b29391cd3279d2
Bijela Gora <bijela_gora@protonmail.com>
2023-03-11 17:12:44 +0200

    Fix unified-signatures issue

--- types/culori/src/random.d.ts
+++ types/culori/src/random.d.ts
@@ -7,8 +7,7 @@ type Constraints<M extends Mode> = Partial<{
     [P in keyof TakeColorChannels<M>]: NumberOrRange;
 }>;
 
-declare function random(): Rgb;
 declare function random<M extends Mode>(mode: M, constraints?: Constraints<M>): FindColorByMode<M>;
-declare function random(mode: undefined, constraints?: Constraints<'rgb'>): Rgb;
+declare function random(mode?: undefined, constraints?: Constraints<'rgb'>): Rgb;
 
 export default random;
