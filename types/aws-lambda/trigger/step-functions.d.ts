6dc2e1cbf1e43c33b0f38a64c39a1678ad1bee53
phtz <856205+photz@users.noreply.github.com>
2023-03-11 17:27:35 +0100

    Add types for 'Step Functions Execution Status Change'
    Source: https://docs.aws.amazon.com/step-functions/latest/dg/cw-events.html

---
+++ types/aws-lambda/trigger/step-functions.d.ts
@@ -0,0 +1,23 @@
+import { EventBridgeEvent } from './eventbridge';
+
+export type StepFunctionsExecutionStatus = 'RUNNING' | 'SUCCEEDED' | 'FAILED' | 'TIMED_OUT' | 'ABORTED';
+
+interface StepFunctionsExecutionStateChangeEventDetail<
+    Status extends StepFunctionsExecutionStatus = StepFunctionsExecutionStatus,
+> {
+    executionArn: string;
+    stateMachineArn: string;
+    name: string;
+    status: Status;
+    startDate: number;
+    stopDate: null | number;
+    input: string;
+    inputDetails: null | Partial<{ included: boolean }>;
+    output: null | string;
+    outputDetails: null | Partial<{ included: boolean }>;
+}
+
+export type StepFunctionsExecutionStatusChangeEvent = EventBridgeEvent<
+    'Step Functions Execution Status Change',
+    StepFunctionsExecutionStateChangeEventDetail
+>;
