cc7fa625d0e77f7c997cde8c3d8c0743a5dfe700
Bijela Gora <bijela_gora@protonmail.com>
2023-03-11 21:15:35 +0200

    Add tests

---
+++ types/culori/tests/map-success-tests.ts
@@ -0,0 +1,63 @@
+import { Color, Hsl, mapper, Rgb } from 'culori';
+
+const doubler = mapper(v => {
+    return v * 2;
+});
+
+const case_1_expect_success: Rgb = doubler({
+    mode: 'rgb',
+    r: 0,
+    b: 0,
+    g: 0,
+});
+
+const case_2_expect_success: Rgb = doubler({
+    mode: 'hsl',
+    h: 0,
+    s: 0,
+    l: 0,
+});
+
+const case_4_expect_success: Rgb | undefined = doubler('hsl(0, 100%, 50%)');
+
+const increaser = mapper(v => v + 10, 'hsl');
+
+const case_5_expect_success: Color | undefined = increaser(
+    'color(--okhsv 29.2338851923426 0.9995219692256307 0.9999999999999997)',
+);
+
+const case_7_expect_success: Hsl = increaser({
+    mode: 'hsl',
+    h: 0,
+    s: 0,
+    l: 0,
+});
+
+const decreaser = mapper(v => v - 10, undefined);
+
+const case_8_expect_success: Rgb | undefined = decreaser(
+    'color(--okhsv 29.2338851923426 0.9995219692256307 0.9999999999999997)',
+);
+
+const tripler = mapper(v => {
+    return v * 3;
+}, 'hsl');
+
+const case_11_expect_success: Hsl | undefined = tripler('#3690');
+const case_12_expect_success: Hsl = tripler({
+    h: 240,
+    s: 1,
+    l: 0.5,
+    mode: 'hsl',
+    alpha: 0.5,
+});
+
+const oner = mapper(
+    (_v, _ch, conv_color, mode) => {
+        const check_0_expect_success: Rgb = conv_color;
+        const check_2_expect_success: 'rgb' = mode;
+        return 1;
+    },
+    undefined,
+    true,
+);
