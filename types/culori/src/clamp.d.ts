d608667624c8d2ac7b014dd744b662cf33f470ce
Bijela Gora <bijela_gora@protonmail.com>
2023-03-11 16:59:41 +0200

    Remove 'declare' keyword because it is redundant here

--- types/culori/src/clamp.d.ts
+++ types/culori/src/clamp.d.ts
@@ -1,9 +1,9 @@
 import { Color, Mode } from './common';
 
-export declare function clampRgb(color: string): Color | undefined;
-export declare function clampRgb<C extends Color>(color: C): C;
+export function clampRgb(color: string): Color | undefined;
+export function clampRgb<C extends Color>(color: C): C;
 
-export declare function clampChroma(color: string): Color | undefined;
-export declare function clampChroma<C extends Color>(color: C): C;
-export declare function clampChroma<C extends Color>(color: C, mode: Mode): C;
-export declare function clampChroma<C extends Color>(color: string, mode: Mode): Color | undefined;
+export function clampChroma(color: string): Color | undefined;
+export function clampChroma<C extends Color>(color: C): C;
+export function clampChroma<C extends Color>(color: C, mode: Mode): C;
+export function clampChroma<C extends Color>(color: string, mode: Mode): Color | undefined;
