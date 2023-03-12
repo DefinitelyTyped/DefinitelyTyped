44a1d12738c9171caa04b0aa2e27d93d06fa8866
Bijela Gora <bijela_gora@protonmail.com>
2023-03-13 00:44:14 +0200

    Add tests for the convertLrgbToRgb function

--- types/culori/src/lrgb/convertLrgbToRgb.d.ts
+++ types/culori/src/lrgb/convertLrgbToRgb.d.ts
@@ -1,9 +1,6 @@
 import { Lrgb } from './types';
-import { FindColorByMode } from '../common';
+import { FindColorByMode, Mode } from '../common';
 
-declare function convertLrgbToRgb<M extends 'rgb' | 'p3' = 'rgb'>(
-    color: Omit<Lrgb, 'mode'>,
-    mode?: M,
-): FindColorByMode<M>;
+declare function convertLrgbToRgb<M extends Mode = 'rgb'>(color: Omit<Lrgb, 'mode'>, mode?: M): FindColorByMode<M>;
 
 export default convertLrgbToRgb;
