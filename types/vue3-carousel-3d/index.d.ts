791d11691488a306e30254baa13db5a0f5aa9b6b
tevem1207 <tevem1207@gmail.com>
2023-03-12 17:39:20 +0900

    Fix TypeScript version to 4.4

--- types/vue3-carousel-3d/index.d.ts
+++ types/vue3-carousel-3d/index.d.ts
@@ -2,7 +2,7 @@
 // Project: https://github.com/Ray93/vue3-carousel-3d
 // Definitions by: tevem1207 <https://github.com/tevem1207>
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
-// TypeScript Version: 4.9
+// TypeScript Version: 4.4
 
 import { Component } from 'vue';
 
