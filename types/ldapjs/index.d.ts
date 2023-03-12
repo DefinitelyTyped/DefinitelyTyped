ce82229a38df647646e50d9817000c40a8766ea9
Niko Heller <niko.heller@verdigado.com>
2023-03-12 15:08:32 +0100

    fixed header version format

--- types/ldapjs/index.d.ts
+++ types/ldapjs/index.d.ts
@@ -1,4 +1,4 @@
-// Type definitions for ldapjs 3.0.1
+// Type definitions for ldapjs 3.0
 // Project: http://ldapjs.org
 // Definitions by: Charles Villemure <https://github.com/cvillemure>, Peter Kooijmans <https://github.com/peterkooijmans>, Pablo Moleri <https://github.com/pmoleri>, Michael Scott-Nelson <https://github.com/mscottnelson>
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
