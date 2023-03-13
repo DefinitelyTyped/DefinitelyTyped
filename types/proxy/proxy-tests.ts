c7d84d1ae47e80ebf4ba886632aa29cd538577db
Nicolas Le Cam <niko.lecam@gmail.com>
2023-03-13 10:51:55 +0100

    [proxy]: Fix authenticate request parameter type

--- types/proxy/proxy-tests.ts
+++ types/proxy/proxy-tests.ts
@@ -3,8 +3,6 @@ import * as http2 from 'http2';
 import * as https from 'https';
 import proxy = require('proxy');
 
-proxy();
-
 proxy(http.createServer());
 proxy(https.createServer());
 
@@ -13,3 +11,13 @@ proxy(http2.createServer());
 
 // @ts-expect-error
 proxy({});
+
+const server = proxy();
+
+server.authenticate = (req, callback) => {
+    try {
+        callback(null, req.headers['proxy-authorization'] === 'Basic Zm9vOmJhcg==' /* foo:bar */);
+    } catch (err) {
+        callback(err, false);
+    }
+};
