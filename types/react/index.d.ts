51a4bec843b85f8ace776f0ad9611ba84dbcedeb
dan <dan.abramov@gmail.com>
2023-03-15 22:27:48 +0000

    [RFC] Remove useRef convenience overload

--- types/react/index.d.ts
+++ types/react/index.d.ts
@@ -1065,34 +1065,6 @@ declare namespace React {
      * @see https://react.dev/reference/react/useRef
      */
     function useRef<T>(initialValue: T): MutableRefObject<T>;
-    // convenience overload for refs given as a ref prop as they typically start with a null value
-    /**
-     * `useRef` returns a mutable ref object whose `.current` property is initialized to the passed argument
-     * (`initialValue`). The returned object will persist for the full lifetime of the component.
-     *
-     * Note that `useRef()` is useful for more than the `ref` attribute. It’s handy for keeping any mutable
-     * value around similar to how you’d use instance fields in classes.
-     *
-     * Usage note: if you need the result of useRef to be directly mutable, include `| null` in the type
-     * of the generic argument.
-     *
-     * @version 16.8.0
-     * @see https://react.dev/reference/react/useRef
-     */
-    function useRef<T>(initialValue: T | null): RefObject<T>;
-    // convenience overload for potentially undefined initialValue / call with 0 arguments
-    // has a default to stop it from defaulting to {} instead
-    /**
-     * `useRef` returns a mutable ref object whose `.current` property is initialized to the passed argument
-     * (`initialValue`). The returned object will persist for the full lifetime of the component.
-     *
-     * Note that `useRef()` is useful for more than the `ref` attribute. It’s handy for keeping any mutable
-     * value around similar to how you’d use instance fields in classes.
-     *
-     * @version 16.8.0
-     * @see https://react.dev/reference/react/useRef
-     */
-    function useRef<T = undefined>(): MutableRefObject<T | undefined>;
     /**
      * The signature is identical to `useEffect`, but it fires synchronously after all DOM mutations.
      * Use this to read layout from the DOM and synchronously re-render. Updates scheduled inside
