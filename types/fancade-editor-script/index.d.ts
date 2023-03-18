4425228ac96934cb765a76fb586fe8d4e0856475
Brycked <brycked@outlook.com>
2023-03-18 13:55:23 +0100

    add header for non-npm package

--- types/fancade-editor-script/index.d.ts
+++ types/fancade-editor-script/index.d.ts
@@ -1,4 +1,4 @@
-// Type definitions for Fancade Editor Script 1.12
+// Type definitions for non-npm package Fancade Editor Script 1.12
 // Project: https://www.fancade.com/wiki/fancade-web#editor-scripting
 // Definitions by: Bricked <https://github.com/brycked>
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
