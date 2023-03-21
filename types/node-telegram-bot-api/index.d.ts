87ea8b03eb76ad20d451bd151e810df5941b645c
XieJiSS <c141028@protonmail.com>
2023-03-21 21:05:21 +0800

    fix(node-telegram-bot-api): fix ci build error

--- types/node-telegram-bot-api/index.d.ts
+++ types/node-telegram-bot-api/index.d.ts
@@ -1655,7 +1655,7 @@ declare class TelegramBot extends EventEmitter {
 
     setChatDescription(chatId: TelegramBot.ChatId, description: string): Promise<boolean>;
 
-    pinChatMessage(chatId: TelegramBot.ChatId, messageId: number, options?: PinChatMessageOptions): Promise<boolean>;
+    pinChatMessage(chatId: TelegramBot.ChatId, messageId: number, options?: TelegramBot.PinChatMessageOptions): Promise<boolean>;
 
     unpinChatMessage(chatId: TelegramBot.ChatId, messageId?: number): Promise<boolean>;
 
