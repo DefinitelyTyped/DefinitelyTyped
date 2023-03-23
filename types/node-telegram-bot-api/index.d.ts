5d8d461c73255bc2edbb4f21cf3ab2f482139352
yokkkoso <yokko.jke@gmail.com>
2023-03-23 20:50:38 +1000

    chore: use generic

--- types/node-telegram-bot-api/index.d.ts
+++ types/node-telegram-bot-api/index.d.ts
@@ -1399,7 +1399,28 @@ declare namespace TelegramBot {
     }
 }
 
-declare class TelegramBot extends EventEmitter {
+declare class TelegramBot extends EventEmitter<
+    | TelegramBot.MessageType
+    | 'message'
+    | 'callback_query'
+    | 'inline_query'
+    | 'poll_answer'
+    | 'chat_member'
+    | 'my_chat_member'
+    | 'chosen_inline_result'
+    | 'channel_post'
+    | 'edited_message'
+    | 'edited_message_text'
+    | 'edited_message_caption'
+    | 'edited_channel_post'
+    | 'edited_channel_post_text'
+    | 'edited_channel_post_caption'
+    | 'shipping_query'
+    | 'pre_checkout_query'
+    | 'polling_error'
+    | 'webhook_error'
+    | 'error'
+> {
     constructor(token: string, options?: TelegramBot.ConstructorOptions);
 
     startPolling(options?: TelegramBot.StartPollingOptions): Promise<any>;
@@ -2108,6 +2129,30 @@ declare class TelegramBot extends EventEmitter {
             | 'webhook_error'
             | 'error',
     ): this;
+
+    listeners(
+        event:
+            | TelegramBot.MessageType
+            | 'message'
+            | 'callback_query'
+            | 'inline_query'
+            | 'poll_answer'
+            | 'chat_member'
+            | 'my_chat_member'
+            | 'chosen_inline_result'
+            | 'channel_post'
+            | 'edited_message'
+            | 'edited_message_text'
+            | 'edited_message_caption'
+            | 'edited_channel_post'
+            | 'edited_channel_post_text'
+            | 'edited_channel_post_caption'
+            | 'shipping_query'
+            | 'pre_checkout_query'
+            | 'polling_error'
+            | 'webhook_error'
+            | 'error',
+    ): Array<(data: any, metadata?: TelegramBot.Metadata) => void>;
 
     rawListeners(
         event:
