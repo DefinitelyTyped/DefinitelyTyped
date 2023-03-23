a35929fdf1643e6ae76107cf96ae11197d0861fb
Umberto Toniolo <umberto.toniolo@mia-platform.eu>
2023-03-23 20:35:35 +0100

    revert comment erasure

--- types/node/v16/dns.d.ts
+++ types/node/v16/dns.d.ts
@@ -313,9 +313,7 @@ declare module 'dns' {
     /**
      * Uses the DNS protocol to resolve `CAA` records for the `hostname`. The`addresses` argument passed to the `callback` function
      * will contain an array of certification authority authorization records
-     * available for the `hostname` (e.g. `[{
-     
-     : 0, iodef: 'mailto:pki@example.com'}, {critical: 128, issue: 'pki.example.com'}]`).
+     * available for the `hostname` (e.g. `[{critical: 0, iodef: 'mailto:pki@example.com'}, {critical: 128, issue: 'pki.example.com'}]`).
      * @since v15.0.0
      */
     export function resolveCaa(hostname: string, callback: (err: NodeJS.ErrnoException | null, records: CaaRecord[]) => void): void;
