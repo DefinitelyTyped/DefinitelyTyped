6039d85d8d123f7993efd8d5eba3f712c8686f70
amuramoto <amuramoto@users.noreply.github.com>
2023-03-24 17:28:13 +0000

    chore: sync updates to google.maps

--- types/google.maps/google.maps-tests.ts
+++ types/google.maps/google.maps-tests.ts
@@ -1,3 +1,3 @@
 // No tests required for generated types
-// Synced from: https://github.com/googlemaps/js-types/commit/4c3ca0d4aee274fc3c73aadf2c9af6a3ca5e8925
+// Synced from: https://github.com/googlemaps/js-types/commit/d95ffbd512511dee29b5cb41b4dca0f427eaf309
 google.maps.Map;
