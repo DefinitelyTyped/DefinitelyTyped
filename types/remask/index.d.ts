79846164d56ab65b60183221dab2c16062d8c5e5
Felipe <felipemiillerr@gmail.com>
2023-03-25 21:35:19 -0300

    error correction

--- types/remask/index.d.ts
+++ types/remask/index.d.ts
@@ -1,3 +1,9 @@
+// Type definitions for remask
+// Project: https://github.com/brunobertolini/remask
+// Definitions by: Felipe Miiller <https://github.com/FelipeMiiller>
+//
+// Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
+
 declare module 'remask' {
     export function mask(value: string, pattern: string): string;
     export function unmask(value: string): string;
