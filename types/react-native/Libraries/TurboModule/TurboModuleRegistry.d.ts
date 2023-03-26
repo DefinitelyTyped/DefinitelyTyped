b6fddc5b098128b46c63e66dc839a6180517e84c
Lucas Rego <lucas.sr.rg@gmail.com>
2023-03-25 23:57:34 -0300

    remove react-native as of version 0.71

--- types/react-native/Libraries/TurboModule/TurboModuleRegistry.d.ts
+++
@@ -1,6 +0,0 @@
-import {TurboModule} from './RCTExport';
-
-export const TurboModuleRegistry: {
-  get<T extends TurboModule>(name: string): T | null;
-  getEnforcing<T extends TurboModule>(name: string): T;
-};
