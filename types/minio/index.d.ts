d4906844f25570ac15f9f203a518de4aba8d5879
Oleg "OSA413" Sokolov <OSA413@users.noreply.github.com>
2023-03-29 12:40:07 +0500

    Merged two overloads into one as suggested by the test

--- types/minio/index.d.ts
+++ types/minio/index.d.ts
@@ -423,8 +423,7 @@ export class Client {
     statObject(bucketName: string, objectName: string, callback: ResultCallback<BucketItemStat>): void;
     statObject(bucketName: string, objectName: string): Promise<BucketItemStat>;
 
-    removeObject(bucketName: string, objectName: string, removeOpts: RemoveOptions, callback: NoResultCallback): void;
-    removeObject(bucketName: string, objectName: string, removeOpts: RemoveOptions): Promise<void>;
+    removeObject(bucketName: string, objectName: string, removeOpts: RemoveOptions, callback?: NoResultCallback): void;
     removeObject(bucketName: string, objectName: string): Promise<void>;
 
     removeObjects(bucketName: string, objectsList: string[], callback: NoResultCallback): void;
