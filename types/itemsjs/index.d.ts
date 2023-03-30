a0312d7ab2659af3b964fa8b2547247b05d48c6a
Mika Andrianarijaona <mika.andrianarijaona@busbud.com>
2023-03-30 09:53:51 +0200

    fix: run prettier

--- types/itemsjs/index.d.ts
+++ types/itemsjs/index.d.ts
@@ -6,138 +6,138 @@
 export as namespace itemsjs;
 
 declare namespace itemsjs {
-  interface Bucket<I extends {}> {
-    key: keyof I & string;
-    doc_count: number;
-    selected: boolean;
-  }
+    interface Bucket<I extends {}> {
+        key: keyof I & string;
+        doc_count: number;
+        selected: boolean;
+    }
 
-  type Buckets<I extends {}> = Array<Bucket<I>>;
+    type Buckets<I extends {}> = Array<Bucket<I>>;
 
-  interface SearchAggregation<I extends {}, A extends string> {
-    name: A;
-    title: string;
-    position: number;
-    buckets: Buckets<I>;
-  }
+    interface SearchAggregation<I extends {}, A extends string> {
+        name: A;
+        title: string;
+        position: number;
+        buckets: Buckets<I>;
+    }
 
-  interface Pagination {
-    page: number;
-    per_page: number;
-    total: number;
-  }
-
-  interface SearchOptions<I extends {}, S extends string, A extends string> {
-    query?: string | undefined;
-    /** @default 1 */
-    page?: number | undefined;
-    /** @default 12 */
-    per_page?: number | undefined;
-    /** The name of a sort defined in the configuration's sortings, or a new custom one */
-    sort?: S | Sorting<I> | undefined;
-    filters?: Partial<Record<A, string[]>> | undefined;
-    /** A custom function to filter values */
-    filter?: ((item: I) => boolean) | undefined;
-    /** @default false */
-    isExactSearch?: boolean | undefined;
-    /** @default false */
-    removeStopWordFilter?: boolean | undefined;
-    /** @default false */
-    is_all_filtered_items?: boolean | undefined;
-  }
-
-  interface AggregationOptions<A extends string> {
-    name: A;
-    /** @default 1 */
-    page?: number | undefined;
-    /** @default 10 */
-    per_page?: number | undefined;
-    query?: string | undefined;
-    conjunction?: boolean | undefined;
-  }
-
-  interface SimilarOptions<I extends {}> {
-    field: keyof I & string;
-    /** @default 0 */
-    minimum?: number | undefined;
-    /** @default 1 */
-    page?: number | undefined;
-    /** @default 10 */
-    per_page?: number | undefined;
-  }
-
-  interface ItemsJs<I extends {}, S extends string, A extends string> {
-    /** Search items */
-    search(options?: SearchOptions<I, S, A>): {
-      data: {
-        items: I[];
-        allFilteredItems: I[] | null;
-        aggregations: Record<A, SearchAggregation<I, A>>;
-      };
-      pagination: Pagination;
-      timings: {
-        facets: number;
-        search: number;
-        sorting: number;
+    interface Pagination {
+        page: number;
+        per_page: number;
         total: number;
-      };
-    };
+    }
 
-    /** Get data for aggregation */
-    aggregation(options: AggregationOptions<A>): {
-      data: { buckets: Buckets<I> };
-      pagination: Pagination;
-    };
+    interface SearchOptions<I extends {}, S extends string, A extends string> {
+        query?: string | undefined;
+        /** @default 1 */
+        page?: number | undefined;
+        /** @default 12 */
+        per_page?: number | undefined;
+        /** The name of a sort defined in the configuration's sortings, or a new custom one */
+        sort?: S | Sorting<I> | undefined;
+        filters?: Partial<Record<A, string[]>> | undefined;
+        /** A custom function to filter values */
+        filter?: ((item: I) => boolean) | undefined;
+        /** @default false */
+        isExactSearch?: boolean | undefined;
+        /** @default false */
+        removeStopWordFilter?: boolean | undefined;
+        /** @default false */
+        is_all_filtered_items?: boolean | undefined;
+    }
 
-    /**
-     * Find similar items.
-     * Uses the `id` key on items to check for uniqueness
-     */
-    similar(
-      id: I extends { id: infer ID } ? ID : unknown,
-      options: SimilarOptions<I>
-    ): {
-      data: { items: Array<I & { _id: number; intersection_length: number }> };
-      pagination: Pagination;
-    };
+    interface AggregationOptions<A extends string> {
+        name: A;
+        /** @default 1 */
+        page?: number | undefined;
+        /** @default 10 */
+        per_page?: number | undefined;
+        query?: string | undefined;
+        conjunction?: boolean | undefined;
+    }
 
-    /** Reindex with an entirely new list of items */
-    reindex(data: I[]): void;
-  }
+    interface SimilarOptions<I extends {}> {
+        field: keyof I & string;
+        /** @default 0 */
+        minimum?: number | undefined;
+        /** @default 1 */
+        page?: number | undefined;
+        /** @default 10 */
+        per_page?: number | undefined;
+    }
 
-  type Order = "asc" | "desc";
+    interface ItemsJs<I extends {}, S extends string, A extends string> {
+        /** Search items */
+        search(options?: SearchOptions<I, S, A>): {
+            data: {
+                items: I[];
+                allFilteredItems: I[] | null;
+                aggregations: Record<A, SearchAggregation<I, A>>;
+            };
+            pagination: Pagination;
+            timings: {
+                facets: number;
+                search: number;
+                sorting: number;
+                total: number;
+            };
+        };
 
-  interface Sorting<I extends {}> {
-    field: keyof I | Array<keyof I>;
-    order?: Order | Order[] | undefined;
-  }
+        /** Get data for aggregation */
+        aggregation(options: AggregationOptions<A>): {
+            data: { buckets: Buckets<I> };
+            pagination: Pagination;
+        };
 
-  interface Aggregation {
-    title: string;
-    conjunction?: boolean | undefined;
-    /** @default 10 */
-    size?: number | undefined;
-    /** @default 'count' */
-    sort?: "term" | "count" | undefined;
-    /** @default 'asc' */
-    order?: Order | undefined;
-    /** @default false */
-    show_facet_stats?: boolean | undefined;
-    /** @default false */
-    hide_zero_doc_count?: boolean | undefined;
-    /** @default true */
-    chosen_filters_on_top?: boolean | undefined;
-  }
+        /**
+         * Find similar items.
+         * Uses the `id` key on items to check for uniqueness
+         */
+        similar(
+            id: I extends { id: infer ID } ? ID : unknown,
+            options: SimilarOptions<I>,
+        ): {
+            data: { items: Array<I & { _id: number; intersection_length: number }> };
+            pagination: Pagination;
+        };
 
-  /** Configuration for itemsjs */
-  interface Configuration<I extends {}, S extends string, A extends string> {
-    sortings?: Record<S, Sorting<I>> | undefined;
-    aggregations?: Record<A, Aggregation> | undefined;
-    /** @default [] */
-    searchableFields?: Array<keyof I> | undefined;
-    /** @default true */
-    native_search_enabled?: boolean | undefined;
-  }
+        /** Reindex with an entirely new list of items */
+        reindex(data: I[]): void;
+    }
+
+    type Order = 'asc' | 'desc';
+
+    interface Sorting<I extends {}> {
+        field: keyof I | Array<keyof I>;
+        order?: Order | Order[] | undefined;
+    }
+
+    interface Aggregation {
+        title: string;
+        conjunction?: boolean | undefined;
+        /** @default 10 */
+        size?: number | undefined;
+        /** @default 'count' */
+        sort?: 'term' | 'count' | undefined;
+        /** @default 'asc' */
+        order?: Order | undefined;
+        /** @default false */
+        show_facet_stats?: boolean | undefined;
+        /** @default false */
+        hide_zero_doc_count?: boolean | undefined;
+        /** @default true */
+        chosen_filters_on_top?: boolean | undefined;
+    }
+
+    /** Configuration for itemsjs */
+    interface Configuration<I extends {}, S extends string, A extends string> {
+        sortings?: Record<S, Sorting<I>> | undefined;
+        aggregations?: Record<A, Aggregation> | undefined;
+        /** @default [] */
+        searchableFields?: Array<keyof I> | undefined;
+        /** @default true */
+        native_search_enabled?: boolean | undefined;
+    }
 }
 
 /**
@@ -146,13 +146,9 @@ declare namespace itemsjs {
  * @param configuration itemsjs
  * @template I The type of items being indexed
  */
-declare function itemsjs<
-  I extends {} = Record<any, unknown>,
-  S extends string = string,
-  A extends string = string
->(
-  items: I[],
-  configuration?: itemsjs.Configuration<I, S, A>
+declare function itemsjs<I extends {} = Record<any, unknown>, S extends string = string, A extends string = string>(
+    items: I[],
+    configuration?: itemsjs.Configuration<I, S, A>,
 ): itemsjs.ItemsJs<I, S, A>;
 
 export = itemsjs;
