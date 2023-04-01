31490a0e3d2307e565941a5dfe8e8f47a35a7fd9
Saiichi Hashimoto <saiichihashimoto@gmail.com>
2023-04-01 13:46:40 -0500

    Only list Saiichi Hashimoto for badwords types

--- types/badwords/index.d.ts
+++ types/badwords/index.d.ts
@@ -1,7 +1,6 @@
 // Type definitions for badwords 1.0
 // Project: https://github.com/MauriceButler/badwords#readme
-// Definitions by: Maurice Butler <https://github.com/MauriceButler>
-//                 Saiichi Hashimoto <https://github.com/saiichihashimoto>
+// Definitions by: Saiichi Hashimoto <https://github.com/saiichihashimoto>
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
 
 import badwords = require('./array');
