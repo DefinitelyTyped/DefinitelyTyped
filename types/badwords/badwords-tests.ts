2b7a980a9db80db10da566278455da7a9d21b466
Saiichi Shayan Hashimoto <saiichihashimoto@users.noreply.github.com>
2023-04-01 13:45:17 -0500

    Update types/badwords/badwords-tests.ts
    Co-authored-by: Piotr Błażejewicz (Peter Blazejewicz) <peterblazejewicz@users.noreply.github.com>

--- types/badwords/badwords-tests.ts
+++ types/badwords/badwords-tests.ts
@@ -1,7 +1,7 @@
-import badwords from 'badwords';
-import badwordsArray from 'badwords/array';
-import badwordsObject from 'badwords/object';
-import badwordsRegexp from 'badwords/regexp';
+import badwords = require('badwords');
+import badwordsArray = require('badwords/array');
+import badwordsObject = require('badwords/object');
+import badwordsRegexp = require('badwords/regexp');
 
 // $ExpectType string[]
 badwords;
