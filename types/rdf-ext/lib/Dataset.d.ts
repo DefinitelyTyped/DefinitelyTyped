d1b5c5a3a0e6a49c74325278329f54ee3485a757
Tomasz Pluskiewicz <tpluscode@users.noreply.github.com>
2023-04-04 11:47:07 +0200

    fix: return type

--- types/rdf-ext/lib/Dataset.d.ts
+++ types/rdf-ext/lib/Dataset.d.ts
@@ -39,7 +39,7 @@ export interface DatasetExt extends Dataset<QuadExt> {
     equals(other: DatasetCore): boolean;
 
     reduce(callbackfn: (previousValue: QuadExt, currentValue: QuadExt, currentIndex: number, self: DatasetExt) => QuadExt): QuadExt;
-    reduce(callbackfn: (previousValue: QuadExt, currentValue: QuadExt, currentIndex: number, self: DatasetExt) => QuadExt, initialValue: QuadExt): T;
+    reduce(callbackfn: (previousValue: QuadExt, currentValue: QuadExt, currentIndex: number, self: DatasetExt) => QuadExt, initialValue: QuadExt): QuadExt;
     reduce<U>(callbackfn: (previousValue: U, currentValue: QuadExt, currentIndex: number, self: DatasetExt) => U, initialValue: U): U;
 }
 
