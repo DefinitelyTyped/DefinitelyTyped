b6c69e98c15f99a79f7229d7358d7ad67beca34f
Mohtasim Alam Sohom <94981761+Sohom829@users.noreply.github.com>
2023-04-06 00:52:26 +0600

    Fix header

--- types/secretary/index.d.ts
+++ types/secretary/index.d.ts
@@ -1,4 +1,4 @@
-// Type definitions for secretary 0.1.0
+// Type definitions for secretary 0.1
 // Project: https://www.npmjs.com/package/secretary
 // Definitions by: Mohtasim Alam Sohom <https://github.com/Sohom829>
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
@@ -8,28 +8,23 @@ declare class Engine {
     maxFlag: number;
     minRunningFlag: number;
     maxRunningFlag: number;
-  
     process(args: [string, ...any[]]): string;
     isValidFlag(flag: number): boolean;
     setMinFlag(flag: number): void;
     setMaxFlag(flag: number): void;
-  }
-  
-  export function flag(flag: number): typeof import(".");
-  export function log(...args: any[]): void;
-  export function configure(config: {
+}
+export function flag(flag: number): typeof import('.');
+export function log(...args: any[]): void;
+export function configure(config: {
     minFlag?: number;
     maxFlag?: number;
     minRunningFlag?: number;
     maxRunningFlag?: number;
-  }): void;
-  
-  declare const _default: {
+}): void;
+declare const _default: {
     Engine: typeof Engine;
     flag: typeof flag;
     log: typeof log;
     configure: typeof configure;
-  };
-  
-  export default _default;
-  
+};
+export default _default;
