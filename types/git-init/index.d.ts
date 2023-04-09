0ad2c38f668e12fcfc6b1f0cd84074dc6aec7a5f
Santi <santyrojasprieto9+github@gmail.com>
2023-04-09 02:44:51 +0000

    fixed it, I guess

--- types/git-init/index.d.ts
+++ types/git-init/index.d.ts
@@ -1,8 +1,7 @@
-// Type definitions for git-init 1.0.0
-// Project: https://github.com/yoshuawuyts/git-init
+// Type definitions for git-init 1.0
+// Project: https://github.com/yoshuawuyts/git-init#readme
 // Definitions by: Santi <https://github.com/santi100a>
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
-// TypeScript Version: 4.2
 
 import { type ExecException, exec } from "node:child_process";
 
@@ -13,12 +12,10 @@ declare type Callback = (error: ExecException | null, stdout: string, stderr: st
  * 
  * @param cb An {@link exec} compatible callback.
  */
-declare function init(cb: Callback): void;
+export default function init(cb: Callback): void;
 /**
  * Initializes a git repo asynchronously.
  * @param path A path to the directory to initialize.
  * @param cb An {@link exec} compatible callback.
  */
-declare function init(path: string, cb: Callback): void;
-
-export default init;
+export default function init(path: string, cb: Callback): void;
