64c4031f00add240b04bb5468fb6777bd7b85220
eps1lon <silbermann.sebastian@gmail.com>
2023-04-15 09:45:58 +0200

    [rr-notifications] Test intended usage

--- types/rr-notifications/rr-notifications-tests.tsx
+++ types/rr-notifications/rr-notifications-tests.tsx
@@ -4,10 +4,8 @@ import { useNotification, NotificationsProvider } from 'rr-notifications';
 useNotification().showNotification({ text: 'hi!' });
 useNotification().removeNotification('2020-04-06T21:31:29.000Z')();
 
-const node = NotificationsProvider({
-    renderNotification({ removeNotification, payload }) {
+<NotificationsProvider
+    renderNotification={({ removeNotification, payload }) => {
         return React.createElement('span', { onClick: removeNotification }, `<pre>${JSON.stringify(payload)}</pre>`);
-    },
-});
-
-node; // $ExpectType ReactElement<any, any> | null
+    }}
+/>;
