660117a4ed07bea482f32d7255c312c1bf91d8e3
Daniil Shmelev <39967396+qucumbah@users.noreply.github.com>
2023-04-16 19:25:37 +0300

    add nested array test

--- types/react/test/next.tsx
+++ types/react/test/next.tsx
@@ -43,7 +43,9 @@ function serverContextTest() {
     // plain objects work
     React.createServerContext('PlainObjectContext', { foo: 1 });
     // readonly arrays work
-    React.createServerContext('ReadonlyArrayContext', [1, 2, 3] as const);
+    React.createServerContext('ReadonlyArrayContext', ['foo', 'bar'] as const);
+    // nested readonly arrays work
+    React.createServerContext('ReadonlyArrayContext', ['foo', ['bar']] as const);
     // @ts-expect-error Incompatible with JSON stringify+parse
     React.createServerContext('DateContext', new Date());
     // @ts-expect-error Incompatible with JSON stringify+parse
