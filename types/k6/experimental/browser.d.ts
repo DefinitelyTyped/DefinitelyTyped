f15a9d3e1913a22d72edacf5f7cd595901e6d915
ankur22 <ankur.agarwal@grafana.com>
2023-04-17 18:09:58 +0100

    Add page.click type definition for k6 browser
    This adds the page.click type definition for the k6 experimental
    browser module.

--- types/k6/experimental/browser.d.ts
+++ types/k6/experimental/browser.d.ts
@@ -66,4 +66,80 @@ export class Page {
      */
     trial?: boolean;
   }): void;
+
+  /**
+   * **NOTE** Use locator-based [locator.click([options])](https://k6.io/docs/javascript-api/k6-browser/api/locator/click/) instead.
+   *
+   * This method clicks an element matching `selector`.
+   * @param selector A selector to search for an element. If there are multiple
+   * elements satisfying the selector, the first will be used.
+   * @param options
+   */
+  click(selector: string, options?: {
+    /**
+     * The mouse button (`left`, `middle` or `right`) to use during the action.
+     * Defaults to `left`.
+     */
+    button?: "left"|"right"|"middle";
+
+    /**
+     * The number of times the action is performed. Defaults to `1`.
+     */
+    clickCount?: number;
+
+    /**
+     * Milliseconds to wait between `mousedown` and `mouseup`. Defaults to `0`.
+     */
+    delay?: number;
+
+    /**
+     * Setting this to `true` will bypass the actionability checks (`visible`,
+     * `stable`, `enabled`). Defaults to `false`.
+     */
+    force?: boolean;
+
+    /**
+     * `Alt`, `Control`, `Meta` or `Shift` modifiers keys pressed during the
+     * action. If not specified, currently pressed modifiers are used,
+     * otherwise defaults to `null`.
+     */
+    modifiers?: Array<"Alt"|"Control"|"Meta"|"Shift">;
+
+    /**
+     * If set to `true` and a navigation occurs from performing this action, it
+     * will not wait for it to complete. Defaults to `false`.
+     */
+    noWaitAfter?: boolean;
+
+    /**
+     * A point to use relative to the top left corner of the element. If not
+     * supplied, a visible point of the element is used.
+     */
+    position?: {
+      x: number;
+
+      y: number;
+    };
+
+    /**
+     * When `true`, the call requires selector to resolve to a single element.
+     * If given selector resolves to more than one element, the call throws
+     * an exception. Defaults to `false`.
+     */
+    strict?: boolean;
+
+    /**
+     * Maximum time in milliseconds. Pass `0` to disable the timeout. Default
+     * is overridden by the `setDefaultTimeout` option on [browserContext](https://k6.io/docs/javascript-api/k6-browser/api/browsercontext/)
+     * or [page](https://k6.io/docs/javascript-api/k6-browser/api/page/) methods. Defaults to `30000`.
+     */
+    timeout?: number;
+
+    /**
+     * Setting this to `true` will perform the actionability checks without
+     * performing the action. Useful to wait until the element is ready for the
+     * action without performing it. Defaults to `false`.
+     */
+    trial?: boolean;
+  }): Promise<void>;
 }
