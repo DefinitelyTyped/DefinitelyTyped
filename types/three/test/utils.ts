f279f11f2538787c57abac9f7b8b9c44d8040e3f
Ross Gardiner <ross@terraform.ai>
2023-04-17 12:07:52 +0200

    three.js: Add new tests for `ColorRepresentation`

---
+++ types/three/test/utils.ts
@@ -0,0 +1,9 @@
+import * as THREE from 'three';
+
+new THREE.Color('rgb(255, 0, 0)');
+new THREE.Color('rgba(255, 0, 0, 1)');
+new THREE.Color('hsl(0, 100%, 50%)');
+new THREE.Color('hsla(0, 100%, 50%, 1)');
+new THREE.Color('#ff0000');
+new THREE.Color('aliceblue');
+new THREE.Color(0xff0000);
