862f209801d767eae31ebf3bdb6bdcea14653d0c
ankur22 <ankur.agarwal@grafana.com>
2023-04-17 17:59:59 +0100

    Add a page class to k6 browser module
    This just adds an empty page interface for the k6 browser experimental
    module.

--- types/k6/experimental/browser.d.ts
+++ types/k6/experimental/browser.d.ts
@@ -2,3 +2,11 @@
  * This module provides an experimental implementation that brings browser
  * test automation to the k6 testing platform.
  */
+
+/**
+ * Page provides methods to interact with a single tab in a browser in Chromium.
+ * One instance of the browser can have many page instances.
+ */
+export class Page {
+
+}
