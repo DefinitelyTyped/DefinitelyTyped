209d5df81da7ccbe3f0e298b0cee8f4c9350da70
Ross Gardiner <github@public.rossng.eu>
2023-04-17 11:48:20 +0200

    Support rgba/hsla for `ColorModelString` in three.js

--- types/three/src/utils.d.ts
+++ types/three/src/utils.d.ts
@@ -1,6 +1,6 @@
 import { Color, ColorKeyword } from './math/Color';
 
-export type ColorModelString = `${'rgb' | 'hsl'}(${string})`;
+export type ColorModelString = `${'rgb' | 'rgba' | 'hsl' | 'hsla'}(${string})`;
 export type HexColorString = `#${string}`;
 
 export type ColorRepresentation = Color | ColorKeyword | ColorModelString | HexColorString | number;
