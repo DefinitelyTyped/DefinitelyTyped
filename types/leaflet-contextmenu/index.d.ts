0b76826ff8013cbf6cd9cd8d0b0c9ff0f5e353ca
Antonio V <antoniovlx@gmail.com>
2023-04-17 09:34:27 +0200

    fixed test error

--- types/leaflet-contextmenu/index.d.ts
+++ types/leaflet-contextmenu/index.d.ts
@@ -7,27 +7,27 @@ import { LatLng, Point, Marker, Path } from "leaflet";
 
 declare module 'leaflet' {
   interface ContextMenuItem {
-      text: string
-      icon?: string
-      retinaIcon?: string
-      iconCls?: string
-      retinaIconCls?: string
-      disabled?: boolean
-      separator?: boolean
-      hideOnSelect?: boolean
-      index?: number
+      text: string;
+      icon?: string;
+      retinaIcon?: string;
+      iconCls?: string;
+      retinaIconCls?: string;
+      disabled?: boolean;
+      separator?: boolean;
+      hideOnSelect?: boolean;
+      index?: number;
       // map is the scope where contextmenu is defined.
       callback?: (ev: ContextMenuItemClickEvent, map?: Map) => void;
   }
 
   interface MapOptions {
       contextmenu: boolean;
-      contextmenuItems: ContextMenuItem[]
+      contextmenuItems: ContextMenuItem[];
   }
 
   interface MarkerOptions {
       contextmenu: boolean;
-      contextmenuItems: ContextMenuItem[]
+      contextmenuItems: ContextMenuItem[];
   }
 
   interface ContextMenuItemClickEvent {
--- types/leaflet-contextmenu/leaflet-contextmenu-tests.ts
+++ types/leaflet-contextmenu/leaflet-contextmenu-tests.ts
@@ -1,33 +1,21 @@
 import { ContextMenuItemClickEvent, Map } from 'leaflet';
 import 'leaflet-contextmenu';
 
-const map = new Map('map', {
+const map: Map = new Map('map', {
     center: [37.1259000, -4.7510700],
     zoom: 3,
     minZoom: 8,
     contextmenu: true,
     contextmenuItems: [{
         text: 'Center map here',
-        callback: (event) => centerMap(event)
+        callback: (event) => map.panTo(event.latlng)
     }, {
         text: 'Zoom in',
         // icon: 'img/zoom-in.png',
-        callback: (event) => zoomIn(event)
+        callback: (event) => map.zoomIn()
     }, {
         text: 'Zoom out',
         // icon: 'img/zoom-out.png',
-        callback: (event) => zoomOut(event)
+        callback: (event) => map.zoomOut()
     }]
 });
-
-function centerMap(e: ContextMenuItemClickEvent) {
-    map.panTo(e.latlng);
-}
-
-function zoomIn(e: ContextMenuItemClickEvent) {
-    map.zoomIn();
-}
-
-function zoomOut(e: ContextMenuItemClickEvent) {
-    map.zoomOut();
-}
