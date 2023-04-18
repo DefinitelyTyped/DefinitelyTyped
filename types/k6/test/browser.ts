d6afd31a174145e40979a3cd711b252c50a089cf
ankur22 <ankur.agarwal@grafana.com>
2023-04-18 10:24:51 +0100

    Add test for browser.page.check

--- types/k6/test/browser.ts
+++ types/k6/test/browser.ts
@@ -1,5 +1,7 @@
 import browser from 'k6/experimental/browser';
 
+const selector = 'a[href="http://example.com"]';
+
 //
 // Create a page
 //
@@ -7,3 +9,20 @@ const page = new browser.Page();
 
 // $ExpectType void
 page.bringToFront();
+
+// @ts-expect-error
+page.check();
+// $ExpectType void
+page.check(selector);
+// $ExpectType void
+page.check(selector, { force: true });
+// $ExpectType void
+page.check(selector, { noWaitAfter: true });
+// $ExpectType void
+page.check(selector, { position: { x: 0, y: 0 } });
+// $ExpectType void
+page.check(selector, { strict: true });
+// $ExpectType void
+page.check(selector, { timeout: 10000 });
+// $ExpectType void
+page.check(selector, { trial: true });
