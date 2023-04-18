c5721662380a704ec70dd3279dc4cb3b0836023a
Trim21 <trim21.me@gmail.com>
2023-04-18 08:34:47 +0800

    fix type

--- types/block-stream2/index.d.ts
+++ types/block-stream2/index.d.ts
@@ -5,10 +5,10 @@
 
 /// <reference types="node" />
 
-import { Readable } from 'stream';
+import { Transform } from 'stream';
 
 export = BlockStream2;
 
-declare class BlockStream2 extends Readable {
+declare class BlockStream2 extends Transform {
     constructor(options?: { size?: number; zeroPadding?: boolean });
 }
