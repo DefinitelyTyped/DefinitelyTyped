44234c603356d68962b7dcd6aae2612558c53a30
Trim21 <trim21.me@gmail.com>
2023-04-18 08:13:40 +0800

    fix lint

--- types/block-stream2/index.d.ts
+++ types/block-stream2/index.d.ts
@@ -1,4 +1,4 @@
-// Type definitions for block-stream2 2.1.0
+// Type definitions for block-stream2 2.1
 // Project: https://www.npmjs.com/package/block-stream2
 // Definitions by: Trim21 <https://github.com/trim21>
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
@@ -6,8 +6,9 @@
 /// <reference types="node" />
 
 import { Readable } from 'stream';
+
 export = BlockStream2;
 
-class BlockStream2 extends Readable {
-    constructor(options?: { size?: number; zeroPadding?: boolean })
+declare class BlockStream2 extends Readable {
+    constructor(options?: { size?: number; zeroPadding?: boolean });
 }
