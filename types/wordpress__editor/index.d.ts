309a7a8416b344f9ed42b800500371a8f871b5f9
TullariS <TullariS@users.noreply.github.com>
2023-04-21 11:56:02 +0100

    Added useSelect and useDispatch
    I was not getting the correct types for the select function in useSelect and the return of useDispatch. Not sure if this is the ideal implementation but it does seem to work.

--- types/wordpress__editor/index.d.ts
+++ types/wordpress__editor/index.d.ts
@@ -8,9 +8,14 @@ import { dispatch, select, StoreDescriptor } from '@wordpress/data';
 
 export { storeConfig, transformStyles } from '@wordpress/block-editor';
 
+type SelectFunction = (key: string) => typeof import("./store/selectors");
+
 declare module '@wordpress/data' {
     function dispatch(key: 'core/editor'): typeof import('./store/actions');
     function select(key: 'core/editor'): typeof import('./store/selectors');
+    
+    function useSelect(selectFunction: ((select: SelectFunction) => any), deps: unknown[]): any;
+    function useDispatch(key: string): typeof import("./store/actions");
 }
 
 export interface EditorStoreDescriptor extends StoreDescriptor {
