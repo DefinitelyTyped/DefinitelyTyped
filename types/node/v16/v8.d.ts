b636668e955d7e20b4452b8931a0f5e21fefd9d3
Aaron Paterson <apaterson@protonmail.com>
2023-04-24 14:49:25 -0600

    format

--- types/node/v16/v8.d.ts
+++ types/node/v16/v8.d.ts
@@ -460,7 +460,7 @@ declare module 'v8' {
      * The `promiseHooks` interface can be used to track promise lifecycle events.
      * @since v17.1.0, v16.14.0
      */
-    export const promiseHooks: PromiseHooks    export const promiseHooks: PromiseHooks
+    export const promiseHooks: PromiseHooks
 }
 declare module 'node:v8' {
     export * from 'v8';
