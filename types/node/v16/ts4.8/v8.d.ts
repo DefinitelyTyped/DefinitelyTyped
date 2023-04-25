60a5a4bf5ad82b6b99d6631e69fb48b28225cf93
aaron_paterson <aaron.paterson@zonos.com>
2023-04-25 08:55:31 -0600

    udpate comment

--- types/node/v16/ts4.8/v8.d.ts
+++ types/node/v16/ts4.8/v8.d.ts
@@ -372,6 +372,13 @@ declare module 'v8' {
      * @since v15.1.0, v12.22.0
      */
     function stopCoverage(): void;
+    /**
+     * Called when a promise is constructed. This does not mean that corresponding before/after events will occur, only that the possibility exists. This will
+     * happen if a promise is created without ever getting a continuation.
+     * @since v17.1.0, v16.14.0
+     * @param promise The promise being created.
+     * @param parent The promise continued from, if applicable.
+     */
     interface Init {
         (promise: Promise<unknown>, parent: Promise<unknown>): void;
     }
