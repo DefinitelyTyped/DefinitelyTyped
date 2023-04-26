8351b12c55f37c566dc053c0f22656082456de53
Steel <steelzkz@gmail.com>
2023-04-26 05:00:20 -0400

    feat: react-modal-image typedef

---
+++ types/react-modal-image/index.d.ts
@@ -0,0 +1,57 @@
+// Type definitions for react-modal-image 2.6
+// Project: https://github.com/aautio/react-modal-image#readme
+// Definitions by: Steel <https://github.com/CodeSteel>
+// Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
+
+declare module 'react-modal-image' {
+    import * as React from 'react';
+
+    interface ModalImageProps extends React.ImgHTMLAttributes<HTMLImageElement> {
+        /* The small image to display */
+        small: string;
+
+        /* The large image to display */
+        large: string;
+
+        /* The alt tag for the image */
+        alt?: string;
+
+        /* Should the download button be hidden? */
+        hideDownload?: boolean;
+
+        /* Should the zoom button be hidden? */
+        hideZoom?: boolean;
+
+        /* Should the rotate button be hidden? */
+        hideRotate?: boolean;
+
+        /* Should the fullscreen button be hidden? */
+        hideFullscreen?: boolean;
+
+        /* The download url for the image */
+        download?: string;
+
+        /* The color to display in the background. */
+        imageBackgroundColor?: string;
+
+        /* The zoom scale */
+        zoomMargin?: number;
+
+        /* Should the modal close when the background is clicked? */
+        shouldHideOnClickOutside?: boolean;
+
+        /* Should the modal close when the esc key is pressed? */
+        shouldCloseOnEsc?: boolean;
+
+        /* Custom styles for the modal */
+        customModalStyles?: React.CSSProperties;
+
+        /* The custom loader to display while the image is loading */
+        customLoader?: React.ReactNode;
+
+        /* The class name for the modal */
+        className?: string;
+    }
+
+    export default function ModalImage(props: ModalImageProps): JSX.Element;
+}
---
+++ types/react-modal-image/react-modal-image-tests.tsx
@@ -0,0 +1,51 @@
+import * as React from 'react';
+import ModalImage, { ModalImageProps } from 'react-modal-image';
+
+const props: ModalImageProps = {
+    /* The small image to display */
+    small: 'https://example.com/image-small.jpg',
+
+    /* The large image to display */
+    large: 'https://example.com/image-large.jpg',
+
+    /* The alt tag for the image */
+    alt: 'Example Image',
+
+    /* Should the download button be hidden? */
+    hideDownload: true,
+
+    /* Should the zoom button be hidden? */
+    hideZoom: true,
+
+    /* Should the rotate button be hidden? */
+    hideRotate: true,
+
+    /* Should the fullscreen button be hidden? */
+    hideFullscreen: true,
+
+    /* The download url for the image */
+    download: 'https://example.com/image-download.jpg',
+
+    /* The color to display in the background. */
+    imageBackgroundColor: '#000',
+
+    /* The zoom scale */
+    zoomMargin: 10,
+
+    /* Should the modal close when the background is clicked? */
+    shouldHideOnClickOutside: true,
+
+    /* Should the modal close when the esc key is pressed? */
+    shouldCloseOnEsc: true,
+
+    /* Custom styles for the modal */
+    customModalStyles: { width: '50%', height: '50%' },
+
+    /* The custom loader to display while the image is loading */
+    customLoader: <div>Loading...</div>,
+
+    /* The class name for the modal */
+    className: 'example-class',
+};
+
+<ModalImage {...props} />;
