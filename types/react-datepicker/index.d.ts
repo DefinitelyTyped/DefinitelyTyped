fb38568360595dc35cd085f0b867caaba20c71f3
Roman Nuritdinov <github@woof.slmail.me>
2023-04-26 20:49:01 +0300

    Removed myself from `react-datepicker` maintainers

--- types/react-datepicker/index.d.ts
+++ types/react-datepicker/index.d.ts
@@ -6,7 +6,6 @@
 //                 Koala Human <https://github.com/KoalaHuman>
 //                 Justin Grant <https://github.com/justingrant>
 //                 Jake Boone <https://github.com/jakeboone02>
-//                 Roman Nuritdinov <https://github.com/Ky6uk>
 //                 Avi Klaiman <https://github.com/aviklai>
 //                 Naoki Sekiguchi <https://github.com/seckie>
 //                 Kerry Gougeon <https://github.com/kerry-g>
