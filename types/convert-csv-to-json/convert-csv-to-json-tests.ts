05824b797f2bc210dbe224e26bec28431965e0ef
Airon Teixeira <airon.teixeira@zennioptical.com>
2023-04-26 00:27:55 -0300

    feature Adding types for convert-csv-to-json

---
+++ types/convert-csv-to-json/convert-csv-to-json-tests.ts
@@ -0,0 +1,17 @@
+import convertCsvToJson from 'convert-csv-to-json';
+
+const sourceCsvString = `
+firstName,age
+Someone,18
+AnotherOne,25
+`.trim()
+
+// $ExpectType []{firstName:string, age:number}
+const result = convertCsvToJson
+.fieldDelimiter(',')
+.csvStringToJson(sourceCsvString)
+
+// $ExpectValue 2
+const length = result.length
+
+
---
+++ types/convert-csv-to-json/index.d.ts
@@ -0,0 +1,104 @@
+// Type definitions for convert-csv-to-json 2.0
+// Project: https://github.com/iuccio/CSVtoJSON.git
+// Definitions by: Airon Teixeira <https://github.com/airon-assustadus>
+// Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
+
+declare module 'convert-csv-to-json' {
+    export class ConvertCsvToJson {
+      /**
+       * Prints a digit as Number type (for example 32 instead of '32')
+       */
+      formatValueByType(active: boolean): this;
+  
+      /**
+       *
+       */
+      supportQuotedField(active: boolean): this;
+      /**
+       * Defines the field delimiter which will be used to split the fields
+       */
+      fieldDelimiter(delimiter: string): this;
+  
+      /**
+       * Defines the index where the header is defined
+       */
+      indexHeader(index: number): this;
+  
+      /**
+       * Defines how to match and parse a sub array
+       */
+      parseSubArray(delimiter: string, separator: string): this;
+  
+      /**
+       * Defines a custom encoding to decode a file
+       */
+      customEncoding(encoding: string): this;
+  
+      /**
+       * Defines a custom encoding to decode a file
+       */
+      utf8Encodingutf8Encoding(): this;
+  
+      /**
+       * Defines ucs2 encoding to decode a file
+       */
+      ucs2Encoding(): this;
+  
+      /**
+       * Defines utf16le encoding to decode a file
+       */
+      utf16leEncoding(): this;
+  
+      /**
+       * Defines latin1 encoding to decode a file
+       */
+      latin1Encoding(): this;
+  
+      /**
+       * Defines ascii encoding to decode a file
+       */
+      asciiEncoding(): this;
+  
+      /**
+       * Defines base64 encoding to decode a file
+       */
+      base64Encoding(): this;
+  
+      /**
+       * Defines hex encoding to decode a file
+       */
+      hexEncoding(): this;
+  
+      /**
+       * Parses .csv file and put its content into a file in json format.
+       * @param {inputFileName} path/filename
+       * @param {outputFileName} path/filename
+       *
+       */
+      generateJsonFileFromCsv(
+        inputFileName: string,
+        outputFileName: string,
+      ): void;
+  
+      /**
+       * Parses .csv file and put its content into an Array of Object in json format.
+       * @param {inputFileName} path/filename
+       * @return {Array} Array of Object in json format
+       *
+       */
+      getJsonFromCsv(inputFileName: string): any[];
+  
+      csvStringToJson(csvString: string): any[];
+      /**
+       * Parses .csv file and put its content into a file in json format.
+       * @param {inputFileName} path/filename
+       * @param {outputFileName} path/filename
+       *
+       * @deprecated Use generateJsonFileFromCsv()
+       */
+      jsonToCsv(inputFileName: string, outputFileName: string): void;
+    }
+    const converter: ConvertCsvToJson;
+    export default converter;
+  }
+  
