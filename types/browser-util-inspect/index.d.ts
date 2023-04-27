f5b64c5c5b96dff044353b1bc07ead3aa9320987
William So <polyipseity@gmail.com>
2023-04-27 13:42:54 +0800

    Expose `inspect.colors` and `inspect.styles`
    Signed-off-by: William So <polyipseity@gmail.com>

--- types/browser-util-inspect/index.d.ts
+++ types/browser-util-inspect/index.d.ts
@@ -22,6 +22,25 @@ declare namespace inspect {
               readonly stylize?: ((str: string, styleType: StyleType) => string) | undefined;
           }
     );
-
     type StyleType = 'boolean' | 'date' | 'name' | 'null' | 'number' | 'regexp' | 'special' | 'string' | 'undefined';
+    type Effect = [startSgr: number, endSgr: number];
+    interface Effects {
+        bold: Effect;
+        italic: Effect;
+        underline: Effect;
+        inverse: Effect;
+        white: Effect;
+        grey: Effect;
+        black: Effect;
+        blue: Effect;
+        cyan: Effect;
+        green: Effect;
+        magenta: Effect;
+        red: Effect;
+        yellow: Effect;
+    }
+    let colors: Effects;
+    let styles: {
+        [_ in StyleType]?: keyof typeof colors | undefined;
+    };
 }
