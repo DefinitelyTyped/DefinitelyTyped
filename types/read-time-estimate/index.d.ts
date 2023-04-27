5327a0847e04868d484b161ee9e49f5cd7bea5f7
rubek <rubek@ottr.com.np>
2023-04-27 11:26:41 +0545

    🐛 Bug: change to default export to accurately reflect type declarations

--- types/read-time-estimate/index.d.ts
+++ types/read-time-estimate/index.d.ts
@@ -4,25 +4,21 @@
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
 
 /*~ You can declare types that are available via importing the module */
-declare function readTimeEstimate(
+export interface ReadTimeEstimate {
+    humanizedDuration: string;
+    duration: number;
+    totalWords: number;
+    wordTime: number;
+    totalImages: number;
+    imageTime: number;
+    otherLanguageTimeCharacters: number;
+    otherLanguageTime: number;
+}
+
+export default function readTimeEstimate(
     string: string,
     customWordTime?: number,
     customImageTime?: number,
     chineseKoreanReadTime?: number,
     imageTags?: string[],
-): readTimeEstimate.ReadTimeEstimate;
-
-declare namespace readTimeEstimate {
-    interface ReadTimeEstimate {
-        humanizedDuration: string;
-        duration: number;
-        totalWords: number;
-        wordTime: number;
-        totalImages: number;
-        imageTime: number;
-        otherLanguageTimeCharacters: number;
-        otherLanguageTime: number;
-    }
-}
-
-export = readTimeEstimate;
+): ReadTimeEstimate;
