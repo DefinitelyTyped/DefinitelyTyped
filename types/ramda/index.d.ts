725003382ebb7c1906a07700b1921e00d12bfcb4
harris-miller <harrismillerconsulting@gmail.com>
2023-04-26 21:14:51 -0600

    forgot one

--- types/ramda/index.d.ts
+++ types/ramda/index.d.ts
@@ -1,9 +1,8 @@
 // Type definitions for ramda 0.29
 // Project: https://ramdajs.com
-// Definitions by: Scott O'Malley <https://github.com/TheHandsomeCoder>
-//                 Harris Miller <https://github.com/harris-miller>
-//                 Kevin Wallace <https://github.com/kedashoe>
+// Definitions by: Harris Miller <https://github.com/harris-miller>
 //                 Nicholai Nissen <https://github.com/Nicholaiii>
+//                 Kevin Wallace <https://github.com/kedashoe>
 //                 Wang Zengdi <https://github.com/adispring>
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
 // TypeScript Version: 5.0
