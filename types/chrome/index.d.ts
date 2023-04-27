a09de20b03db5bc9de5382f812e5101cb66e8787
Matin Aniss <76515905+MatinAniss@users.noreply.github.com>
2023-04-28 06:18:14 +1000

    Update MessageSender properties

--- types/chrome/index.d.ts
+++ types/chrome/index.d.ts
@@ -6906,6 +6906,16 @@ declare namespace chrome.runtime {
          * @since Chrome 80.
          */
         origin?: string | undefined;
+        /**
+         * The lifecycle the document that opened the connection is in at the time the port was created. Note that the lifecycle state of the document may have changed since port creation.
+         * @since Chrome 106.
+         */
+        documentLifecycle?: string | undefined;
+        /**
+         * A UUID of the document that opened the connection.
+         * @since Chrome 106.
+         */
+        documentId?: string | undefined;
     }
 
     /**
