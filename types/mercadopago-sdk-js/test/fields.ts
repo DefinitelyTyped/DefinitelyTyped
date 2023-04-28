4906daf82afdeea772264239661c9b2580faf7c2
Gui Carvalho <guilherme.ocarvalho@mercadolivre.com>
2023-04-28 13:38:45 +0000

    added all tests to test file

--- types/mercadopago-sdk-js/test/fields.ts
+++
@@ -1,8 +0,0 @@
-import { mpInstance } from "../mercadopago-sdk-js-tests";
-
-const fieldInstance = mpInstance.fields.create("cardNumber", {});
-fieldInstance.mount("containerId");
-fieldInstance.update({});
-fieldInstance.on('validityChange', (args) => {})
-mpInstance.fields.createCardToken({}, undefined)
-fieldInstance.unmount()
