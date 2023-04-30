b58282f0f0a1fcf853142642065c7666a778c302
Jonathan Loesch <jonloesch@gmail.com>
2023-04-30 15:49:49 -0400

    Updating version number as well

--- types/polylabel/index.d.ts
+++ types/polylabel/index.d.ts
@@ -1,4 +1,4 @@
-// Type definitions for polylabel 1.0
+// Type definitions for polylabel 1.2
 // Project: https://github.com/mapbox/polylabel
 // Definitions by: Denis Carriere <https://github.com/DenisCarriere>
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
