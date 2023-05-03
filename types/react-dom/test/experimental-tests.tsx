848c648dbffabe3f00897fc3de319564703430c9
Sebastian Silbermann <sebastian.silbermann@klarna.com>
2023-05-03 17:56:32 +0200

    [react-dom] Add types for `experimental_useFormStatus` (#65338)

--- types/react-dom/test/experimental-tests.tsx
+++ types/react-dom/test/experimental-tests.tsx
@@ -1,2 +1,20 @@
 import ReactDOM = require('react-dom');
+import 'react/experimental';
 import 'react-dom/experimental';
+
+const useFormStatus = ReactDOM.experimental_useFormStatus;
+
+function Status() {
+    const status = useFormStatus();
+    if (!status.pending) {
+        return <div>No pending action</div>;
+    } else {
+        const { action, data, method } = status;
+        const foo = data.get('foo');
+        return (
+            <div>{`Pending action ${
+                typeof action === 'string' ? action : action.name
+            }: foo is ${foo}, method is ${method}`}</div>
+        );
+    }
+}
