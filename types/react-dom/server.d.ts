8db63f8cf1987f893aceab51c70c5682718182a1
Nathan Brown <nathan@nkbrown.us>
2023-05-04 18:29:30 -0700

    [react-dom] Disable void-return dtslint rule for changed lines

--- types/react-dom/server.d.ts
+++ types/react-dom/server.d.ts
@@ -39,7 +39,9 @@ export interface RenderToPipeableStreamOptions {
 }
 
 export interface PipeableStream {
+    // tslint:disable-next-line:void-return
     abort(this: void): void;
+    // tslint:disable-next-line:void-return
     pipe<Writable extends NodeJS.WritableStream>(this: void, destination: Writable): Writable;
 }
 
