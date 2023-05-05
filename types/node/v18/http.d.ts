df04f085ed160f28b023c19ed65b9003e48e5b0d
Nathan Rajlich <n@n8.io>
2023-05-05 02:55:59 -0700

    .

--- types/node/v18/http.d.ts
+++ types/node/v18/http.d.ts
@@ -1344,7 +1344,7 @@ declare module 'http' {
      */
     class Agent extends EventEmitter {
         /**
-         * By default set to 256\. For agents with `keepAlive` enabled, this
+         * By default set to 256. For agents with `keepAlive` enabled, this
          * sets the maximum number of sockets that will be left open in the free
          * state.
          * @since v0.11.7
