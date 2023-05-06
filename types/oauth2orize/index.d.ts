6a2624cbd86595e18c6f071631d94b2045470f10
Kagami Sascha Rosylight <saschanaz@outlook.com>
2023-05-06 17:58:28 +0200

    1.11

--- types/oauth2orize/index.d.ts
+++ types/oauth2orize/index.d.ts
@@ -1,4 +1,4 @@
-// Type definitions for oauth2orize 1.8
+// Type definitions for oauth2orize 1.11
 // Project: https://github.com/jaredhanson/oauth2orize/
 // Definitions by: Wonshik Kim <https://github.com/wokim>, Kei Son <https://github.com/heycalmdown>
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
