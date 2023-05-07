f664b6b360a677473165077ff002812cd9dc3fb1
Liam Martens <hi@liammartens.com>
2023-05-07 09:19:01 -0400

    chore: delete files

--- types/mertasan__tailwindcss-variables/internal/PluginOptions.d.ts
+++
@@ -1,8 +0,0 @@
-export interface PluginOptions {
-    darkToRoot?: boolean;
-    variablePrefix?: string;
-    colorVariables?: boolean;
-    extendColors?: Record<string, string>;
-    forceRGB?: boolean;
-    toBase?: boolean;
-}
