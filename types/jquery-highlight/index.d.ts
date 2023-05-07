18fd1e08aa86846d9edc2a627ae1d0c30da5edf1
Mustafa Salaheldin <mustafa.salaheldin@yahoo.com>
2023-05-07 19:17:00 +0400

    Update index.d.ts

--- types/jquery-highlight/index.d.ts
+++ types/jquery-highlight/index.d.ts
@@ -8,49 +8,49 @@
 
 interface UnhighlightSelectOption {
    /**
-   * The highlights to remove based on CSS class, defaults to 'highlight'.
-   */
+    * The highlights to remove based on CSS class, defaults to 'highlight'.
+    */
    className?: string | undefined;
    /**
-    * The highlights to remove based on HTML element, defaults to 'span'.
-    */
+     * The highlights to remove based on HTML element, defaults to 'span'.
+     */
    element?: string | undefined;
 }
 
 interface HighlightOptions {
    /**
-    * The CSS class of a highlighted element, defaults to 'highlight'.
-    */
+     * The CSS class of a highlighted element, defaults to 'highlight'.
+     */
    className?: string | undefined;
 
    /**
-    * The element that wraps the highlighted word, defaults to 'span'.
-    */
+     * The element that wraps the highlighted word, defaults to 'span'.
+     */
    element?: string | undefined;
 
    /**
-    * If the search should be case sensitive, defaults to false.
-    */
+     * If the search should be case sensitive, defaults to false.
+     */
    caseSensitive?: boolean | undefined;
 
    /**
-    *  If we want to highlight partial sections of a word, e.g. 'ca' from 'cat', defaults to false.
-    */
+     *  If we want to highlight partial sections of a word, e.g. 'ca' from 'cat', defaults to false.
+     */
    wordsOnly?: boolean | undefined;
 
    /**
-    * If wordsOnly is set to true, this is used to determine these boundaries, defaults to \\b (word boundary).
-    */
+     * If wordsOnly is set to true, this is used to determine these boundaries, defaults to \\b (word boundary).
+     */
    wordsBoundary?: string | undefined;
 
    /**
-    * If wordsOnly is set to true, this is used to determine prefix word boundaries, defaults to the value of wordsBoundary.
-    */
+     * If wordsOnly is set to true, this is used to determine prefix word boundaries, defaults to the value of wordsBoundary.
+     */
    wordsBoundaryStart?: string | undefined;
 
    /**
-    * If wordsOnly is set to true, this is used to determine suffix word boundaries, defaults to the value of wordsBoundary.
-    */
+     * If wordsOnly is set to true, this is used to determine suffix word boundaries, defaults to the value of wordsBoundary.
+     */
    wordsBoundaryEnd?: string | undefined;
 }
 
