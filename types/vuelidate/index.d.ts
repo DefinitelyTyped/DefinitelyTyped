a903f3a314c95a2bf4314f570519c526e1b0294f
orblazer <orblazer.minecraft@gmail.com>
2023-05-08 19:46:19 +0200

    Remove self from contributor (#65359)

--- types/vuelidate/index.d.ts
+++ types/vuelidate/index.d.ts
@@ -1,7 +1,6 @@
 // Type definitions for vuelidate 0.7
 // Project: https://github.com/monterail/vuelidate
 // Definitions by: Jan Esser <https://github.com/janesser>
-//                 Orblazer <https://github.com/orblazer>
 //                 Yuri Krylov <https://github.com/shadrus>
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
 // TypeScript Version: 4.4
