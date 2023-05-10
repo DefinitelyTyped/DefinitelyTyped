bfcee1393ce0c58e6e810b05db096b95d644ca51
Manuel Thalmann <m@nuth.ch>
2023-05-10 23:38:52 +0200

    Update the version properly

--- types/glob-stream/index.d.ts
+++ types/glob-stream/index.d.ts
@@ -1,4 +1,4 @@
-// Type definitions for glob-stream v6.1.0
+// Type definitions for glob-stream v8.0.0
 // Project: https://github.com/wearefractal/glob-stream
 // Definitions by: Bart van der Schoor <https://github.com/Bartvds>
 //                 mrmlnc <https://github.com/mrmlnc>
