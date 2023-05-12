fa4b8f8cea30d1233f497ddfb57ca0821699e482
zardilior <jeestremadoyro@gmail.com>
2023-05-12 09:02:16 -0600

    Fixed tests and added dt-header

--- types/text-metrics/index.d.ts
+++ types/text-metrics/index.d.ts
@@ -1,45 +1,48 @@
-declare module "text-metrics" {
-  export type Styles = React.CSSProperties;
+// Type definitions for text-metrics 3.0
+// Project: https://github.com/bezoerb/text-metrics
+// Definitions by: Wunmi Sogunle <https://github.com/theresasogunle>
+// Definitions: https://github.com/modus-jose/DefinitelyTyped
 
-  export const init: (
-    htmlElement: Element | Styles | null,
+export type Styles = any;
+
+export function init(
+  htmlElement: Node | Styles | null,
+  styleOverwrites?: Styles
+): TextMetrics;
+
+export interface Options {
+  multiline: boolean;
+}
+
+export interface TextMetrics {
+  parseArgs: (
+    textOrOptions?: string | Options,
+    optionsOrStyleOverwrites?: Options | Styles,
     styleOverwrites?: Styles
-  ) => TextMetrics;
-
-  export interface Options {
-    multiline: boolean;
-  }
-
-  export interface TextMetrics {
-    parseArgs: (
-      textOrOptions?: string | Options,
-      optionsOrStyleOverwrites?: Options | Styles,
-      styleOverwrites?: Styles
-    ) => {
-      text: string;
-      options: Options;
-      styleOverwrites: Styles;
-      styles: Styles;
-    };
-    width: (
-      textOrOptions?: string | Options,
-      optionsOrStyleOverwrites?: Options | Styles,
-      styleOverwrites?: Styles
-    ) => number;
-    height: (
-      textOrOptions?: string | Options,
-      optionsOrStyleOverwrites?: Options | Styles,
-      styleOverwrites?: Styles
-    ) => number;
-    lines: (
-      textOrOptions?: string | Options,
-      optionsOrStyleOverwrites?: Options | Styles,
-      styleOverwrites?: Styles
-    ) => string[];
-    maxFontSize: (
-      textOrOptions?: string | Options,
-      optionsOrStyleOverwrites?: Options | Styles,
-      styleOverwrites?: Styles
-    ) => string | undefined;
-  }
+  ) => {
+    text: string;
+    options: Options;
+    styleOverwrites: Styles;
+    styles: Styles;
+  };
+  width: (
+    textOrOptions?: string | Options,
+    optionsOrStyleOverwrites?: Options | Styles,
+    styleOverwrites?: Styles
+  ) => number;
+  height: (
+    textOrOptions?: string | Options,
+    optionsOrStyleOverwrites?: Options | Styles,
+    styleOverwrites?: Styles
+  ) => number;
+  lines: (
+    textOrOptions?: string | Options,
+    optionsOrStyleOverwrites?: Options | Styles,
+    styleOverwrites?: Styles
+  ) => string[];
+  maxFontSize: (
+    textOrOptions?: string | Options,
+    optionsOrStyleOverwrites?: Options | Styles,
+    styleOverwrites?: Styles
+  ) => string | undefined;
 }
