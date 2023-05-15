0c054f99bdb9a1d8b4a465ad4afe2ffef30759e6
Santi <santyrojasprieto9+github@gmail.com>
2023-05-15 18:50:43 +0000

    addressed @jakebailey's code review

--- types/git-init/index.d.ts
+++ types/git-init/index.d.ts
@@ -2,49 +2,9 @@
 // Project: https://github.com/yoshuawuyts/git-init#readme
 // Definitions by: Santi <https://github.com/santi100a>
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
-interface Signals {
-    SIGABRT: string;
-    SIGALRM: string;
-    SIGBUS: string;
-    SIGCHLD: string;
-    SIGCONT: string;
-    SIGFPE: string;
-    SIGHUP: string;
-    SIGILL: string;
-    SIGINT: string;
-    SIGIO: string;
-    SIGIOT: string;
-    SIGKILL: string;
-    SIGPIPE: string;
-    SIGPOLL: string;
-    SIGPROF: string;
-    SIGPWR: string;
-    SIGQUIT: string;
-    SIGSEGV: string;
-    SIGSTKFLT: string;
-    SIGSTOP: string;
-    SIGSYS: string;
-    SIGTERM: string;
-    SIGTRAP: string;
-    SIGTSTP: string;
-    SIGTTIN: string;
-    SIGTTOU: string;
-    SIGUNUSED: string;
-    SIGURG: string;
-    SIGUSR1: string;
-    SIGUSR2: string;
-    SIGVTALRM: string;
-    SIGWINCH: string;
-    SIGXCPU: string;
-    SIGXFSZ: string;
-}
 
-interface ExecException extends Error {
-    cmd?: string | undefined;
-    killed?: boolean | undefined;
-    code?: number | undefined;
-    signal?: Signals | undefined;
-}
+/// <reference types="node" />
+import type { ExecException } from 'node:child_process';
 
 type ExecCallback = (error: ExecException | null, stdout: string, stderr: string) => void;
 /**
