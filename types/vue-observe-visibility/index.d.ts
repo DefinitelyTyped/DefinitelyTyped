68d9d3186e007b7066a2338d2e9a6700cac7a39b
UxieVerity <1351117103@qq.com>
2023-05-17 19:27:54 +0800

    Add types for vue-oberve-visibility

---
+++ types/vue-observe-visibility/index.d.ts
@@ -0,0 +1,11 @@
+// Type definitions for vue-observe-visibility 1.0
+// Project: https://github.com/Akryum/vue-observe-visibility
+// Definitions by: UxieVerity <https://github.com/UxieVerity>
+// Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
+// TypeScript Version: 4.4
+
+import { Directive } from 'vue';
+
+export const ObserveVisibility: Directive;
+
+export default ObserveVisibility;
---
+++ types/vue-observe-visibility/vue-observe-visibility-tests.ts
@@ -0,0 +1,4 @@
+import Vue from "vue";
+import { ObserveVisibility } from "vue-observe-visibility";
+
+Vue.directive("observe-visibility", ObserveVisibility);
