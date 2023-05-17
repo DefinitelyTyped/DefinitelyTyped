67a77e9505728950a0c08d14bce43e3d8c6dd1dc
Sam Tyler <sam@sammtyler.com>
2023-05-17 19:30:54 +0930

    Fix typo

--- types/epub-gen/epub-gen-tests.ts
+++ types/epub-gen/epub-gen-tests.ts
@@ -1,4 +1,4 @@
 import Epub from 'epub-gen';
 
 new Epub({ title: '', content: [] }); // $ExpectType Epub
-(new Epub({ title: '', content: [] })).promise; // $ExpectType Promse<void>
+(new Epub({ title: '', content: [] })).promise; // $ExpectType Promise<void>
