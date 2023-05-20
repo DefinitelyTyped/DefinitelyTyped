21578f9eee050f770a908b250c607bac4edaf645
LiamAttClarke <liam.att.clarke@gmail.com>
2023-05-20 11:26:16 -0400

    Cleans up geojson-validation types

--- types/geojson-validation/geojson-validation-tests.ts
+++ types/geojson-validation/geojson-validation-tests.ts
@@ -1,6 +1,7 @@
 import GeoJSONValidation = require("geojson-validation");
 
-GeoJSONValidation.define("Point", () => {}); // $ExpectType boolean
+GeoJSONValidation.allTypes.Feature; // $ExpectType GeoJSONValidator
+GeoJSONValidation.define("Feature", () => {}); // $ExpectType boolean
 GeoJSONValidation.isBbox(null); // $ExpectType string[]
 GeoJSONValidation.isFeature(null); // $ExpectType string[]
 GeoJSONValidation.isFeatureCollection(null); // $ExpectType string[]
--- types/geojson-validation/index.d.ts
+++ types/geojson-validation/index.d.ts
@@ -3,39 +3,44 @@
 // Definitions by: Liam A. Clarke <https://github.com/LiamAttClarke>
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
 
-export interface allTypes {
-    Feature: typeof isFeature;
-    FeatureCollection: typeof isFeatureCollection;
-    Point: typeof isPoint;
-    MultiPoint: typeof isMultiPoint;
-    LineString: typeof isLineString;
-    MultiLineString: typeof isMultiLineString;
-    Polygon: typeof isPolygon;
-    MultiPolygon: typeof isMultiPolygon;
-    GeometryCollection: typeof isGeometryCollection;
-    Bbox: typeof isBbox;
-    Position: typeof isPosition;
-    GeoJSON: typeof isGeoJSONObject;
-    GeometryObject: typeof isGeometryObject;
+export interface GeoJSONValidator {
+    (geoJSONObject: unknown, trace?: boolean | undefined): string[];
 }
 
-export function define(type: keyof allTypes, definition: (...args: any[]) => any): boolean;
-export function isPosition(position: unknown, trace?: boolean): string[];
-export function isGeoJSONObject(geoJSONObject: unknown, trace?: boolean): string[];
-export function valid(geoJSONObject: unknown, trace?: boolean): string[];
-export function isGeometryObject(geoJSONObject: unknown, trace?: boolean): string[];
-export function isPoint(geoJSONObject: unknown, trace?: boolean): string[];
-export function isMultiPointCoor(geoJSONObject: unknown, trace?: boolean): string[];
-export function isMultiPoint(geoJSONObject: unknown, trace?: boolean): string[];
-export function isLineStringCoor(geoJSONObject: unknown, trace?: boolean): string[];
-export function isLineString(geoJSONObject: unknown, trace?: boolean): string[];
-export function isMultiLineStringCoor(geoJSONObject: unknown, trace?: boolean): string[];
-export function isMultiLineString(geoJSONObject: unknown, trace?: boolean): string[];
-export function isPolygonCoor(geoJSONObject: unknown, trace?: boolean): string[];
-export function isPolygon(geoJSONObject: unknown, trace?: boolean): string[];
-export function isMultiPolygonCoor(geoJSONObject: unknown, trace?: boolean): string[];
-export function isMultiPolygon(geoJSONObject: unknown, trace?: boolean): string[];
-export function isGeometryCollection(geoJSONObject: unknown, trace?: boolean): string[];
-export function isFeature(geoJSONObject: unknown, trace?: boolean): string[];
-export function isFeatureCollection(geoJSONObject: unknown, trace?: boolean): string[];
-export function isBbox(geoJSONObject: unknown, trace?: boolean): string[];
+export const allTypes: {
+    Feature: GeoJSONValidator;
+    FeatureCollection: GeoJSONValidator;
+    Point: GeoJSONValidator;
+    MultiPoint: GeoJSONValidator;
+    LineString: GeoJSONValidator;
+    MultiLineString: GeoJSONValidator;
+    Polygon: GeoJSONValidator;
+    MultiPolygon: GeoJSONValidator;
+    GeometryCollection: GeoJSONValidator;
+    Bbox: GeoJSONValidator;
+    Position: GeoJSONValidator;
+    GeoJSON: GeoJSONValidator;
+    GeometryObject: GeoJSONValidator;
+};
+
+export function define(type: keyof typeof allTypes, definition: (...args: any[]) => any): boolean;
+
+export const isPosition: GeoJSONValidator;
+export const isGeoJSONObject: GeoJSONValidator;
+export const valid: GeoJSONValidator;
+export const isGeometryObject: GeoJSONValidator;
+export const isPoint: GeoJSONValidator;
+export const isMultiPointCoor: GeoJSONValidator;
+export const isMultiPoint: GeoJSONValidator;
+export const isLineStringCoor: GeoJSONValidator;
+export const isLineString: GeoJSONValidator;
+export const isMultiLineStringCoor: GeoJSONValidator;
+export const isMultiLineString: GeoJSONValidator;
+export const isPolygonCoor: GeoJSONValidator;
+export const isPolygon: GeoJSONValidator;
+export const isMultiPolygonCoor: GeoJSONValidator;
+export const isMultiPolygon: GeoJSONValidator;
+export const isGeometryCollection: GeoJSONValidator;
+export const isFeature: GeoJSONValidator;
+export const isFeatureCollection: GeoJSONValidator;
+export const isBbox: GeoJSONValidator;
