d3722a636acb111c81eb851b9f7e1fe1ddce22c3
Juniper Hovey <me@juniper.link>
2023-05-23 13:45:55 -0500

    Set up ssh-keygen types structure

--- types/ssh-keygen/index.d.ts
+++ types/ssh-keygen/index.d.ts
@@ -1,3 +1,8 @@
+// Type definitions for ssh-keygen 0.5
+// Project: http://github.com/ericvicenti/ssh-keygen
+// Definitions by: Juniper Hovey <https://github.com/TreehouseFalcon>
+// Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
+
 declare module "ssh-keygen" {
 	interface KeygenOptions {
 		location?: string;
@@ -17,5 +22,5 @@ declare module "ssh-keygen" {
 	export default function keygen(
 		opts?: KeygenOptions | object,
 		callback?: (err: string | null, out: KeygenOutput) => unknown,
-	);
+	): unknown;
 }
