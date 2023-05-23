05d0ec23eb321d0480f9af42e7f7aeebc51a7d78
kuromoka <kuromokapb@gmail.com>
2023-05-24 04:34:01 +0900

    fix version

--- types/namespace-js/index.d.ts
+++ types/namespace-js/index.d.ts
@@ -1,4 +1,4 @@
-// Type definitions for namespace-js 0.0
+// Type definitions for namespace-js 0.0.2
 // Project: https://github.com/hirokidaichi/namespace-js
 // Definitions by: kuromoka <https://github.com/kuromoka>
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
