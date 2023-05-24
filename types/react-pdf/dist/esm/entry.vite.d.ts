0dadd246327f300d5725a7cdfdc3ef27621e0e37
Wojciech Maj <kontakt@wojtekmaj.pl>
2023-05-24 21:42:35 +0200

    Remove @types/react-pdf

--- types/react-pdf/dist/esm/entry.vite.d.ts
+++
@@ -1,4 +0,0 @@
-export { default as Document } from '../Document';
-export { default as Page } from '../Page';
-export { default as Outline } from '../Outline';
-export { default as pdfjs } from '../pdfjs-dist';
