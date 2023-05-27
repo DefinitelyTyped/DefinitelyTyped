57ba8d2e9735a44b84f86a399f40d3cced5c62bb
Selman OZTURK <selman@ommaworks.com>
2023-05-27 23:54:07 +0300

    Bump version apicache@1.6.3

--- types/apicache/index.d.ts
+++ types/apicache/index.d.ts
@@ -1,4 +1,4 @@
-// Type definitions for apicache 1.6
+// Type definitions for apicache 1.6.3
 // Project: https://github.com/kwhitley/apicache
 // Definitions by: Daniel Sogl <https://github.com/danielsogl>
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
