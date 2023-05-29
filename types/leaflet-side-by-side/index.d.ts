4f810557a6b1f75bb1173a1be012920597f10f17
oliverbiel <gmartins.oliver@gmail.com>
2023-05-29 10:28:38 -0300

    chore(leafleft-side-by-side): ran prettier

--- types/leaflet-side-by-side/index.d.ts
+++ types/leaflet-side-by-side/index.d.ts
@@ -9,27 +9,27 @@ import * as L from 'leaflet';
 
 declare module 'leaflet' {
     namespace control {
-      function sideBySide(
-        leftLayers: Layer[] | Layer,
-        rightLayers: Layer[] | Layer,
-        options?: Control.SideBySideOptions
-      ): Control.SideBySide;
+        function sideBySide(
+            leftLayers: Layer[] | Layer,
+            rightLayers: Layer[] | Layer,
+            options?: Control.SideBySideOptions,
+        ): Control.SideBySide;
     }
 
     namespace Control {
-      interface SideBySideOptions extends ControlOptions {
-        thumbSize?: number;
-        padding?: number;
-      }
+        interface SideBySideOptions extends ControlOptions {
+            thumbSize?: number;
+            padding?: number;
+        }
 
-      interface SideBySide extends Control {
-        options: SideBySideOptions;
-        setLeftLayers(leftLayers: Layer[] | Layer): this;
-        setRightLayers(rightLayers: Layer[] | Layer): this;
-        getPosition(): any;
-        setPosition(): this;
-        addTo(map: Map): this;
-        remove(): this;
-      }
+        interface SideBySide extends Control {
+            options: SideBySideOptions;
+            setLeftLayers(leftLayers: Layer[] | Layer): this;
+            setRightLayers(rightLayers: Layer[] | Layer): this;
+            getPosition(): any;
+            setPosition(): this;
+            addTo(map: Map): this;
+            remove(): this;
+        }
     }
 }
