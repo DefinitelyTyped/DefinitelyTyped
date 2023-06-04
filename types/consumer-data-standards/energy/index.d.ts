03d5ac11230d2f2a296eab8356112f53bf5f9bf1
Tomas Schier <tomas@xpsolutions.com.au>
2023-06-04 15:54:25 +1000

    More 1.24.0 updates

--- types/consumer-data-standards/energy/index.d.ts
+++ types/consumer-data-standards/energy/index.d.ts
@@ -6853,7 +6853,6 @@ export interface EnergyPlanResponse {
   [k: string]: unknown;
 }
 
-export interface EnergyPlanDetailV2 {}
 /* These are the schema definitions stipulated by the Data Standards Body for the energy api. */
 
 /**
