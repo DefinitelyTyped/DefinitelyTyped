ecd55928e958f16414c992eb30fa3fd0cbd4532c
Oliver Joseph Ash <oliverjash@gmail.com>
2023-06-10 08:51:18 +0100

    `react`: add `fetchPriority` to `LinkHTMLAttributes` (#65726)
    * `react`: add `fetchPriority` to `LinkHTMLAttributes`
    
    * Fix casing

--- types/react/v17/index.d.ts
+++ types/react/v17/index.d.ts
@@ -2244,6 +2244,7 @@ declare namespace React {
     interface LinkHTMLAttributes<T> extends HTMLAttributes<T> {
         as?: string | undefined;
         crossOrigin?: "anonymous" | "use-credentials" | "" | undefined;
+        fetchpriority?: "high" | "low" | "auto";
         href?: string | undefined;
         hrefLang?: string | undefined;
         integrity?: string | undefined;
