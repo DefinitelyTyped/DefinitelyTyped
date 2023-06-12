ef8c0b3ceb7ee1507891c0a135dc6d832e3e107e
Gabriel de Oliveira Martins <kicaoliver2002@gmail.com>
2023-06-12 16:01:16 -0300

    Types for leaflet-side-by-side (#65629)
    * feat(leaflet): leaflet side by side types
    
    * fix(leaflet-side-by-side): fixed unnecessary qualifier
    
    * chore(leafleft-side-by-side): ran prettier
    
    * docs: updated author
    
    * Apply suggestions from code review
    
    Co-authored-by: Piotr Błażejewicz (Peter Blazejewicz) <peterblazejewicz@users.noreply.github.com>
    
    ---------
    
    Co-authored-by: Sheetal Nandi <shkamat@microsoft.com>
    Co-authored-by: Piotr Błażejewicz (Peter Blazejewicz) <peterblazejewicz@users.noreply.github.com>

---
+++ types/leaflet-side-by-side/index.d.ts
@@ -0,0 +1,33 @@
+// Type definitions for leaflet-side-by-side 2.2
+// Project: https://github.com/digidem/leaflet-side-by-side
+// Definitions by: Gabriel O. Martins <https://github.com/OliverBiel/>
+// Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
+
+import * as L from 'leaflet';
+
+declare module 'leaflet' {
+    namespace control {
+        function sideBySide(
+            leftLayers: Layer[] | Layer,
+            rightLayers: Layer[] | Layer,
+            options?: Control.SideBySideOptions,
+        ): Control.SideBySide;
+    }
+
+    namespace Control {
+        interface SideBySideOptions extends ControlOptions {
+            thumbSize?: number;
+            padding?: number;
+        }
+
+        interface SideBySide extends Control {
+            options: SideBySideOptions;
+            setLeftLayers(leftLayers: Layer[] | Layer): this;
+            setRightLayers(rightLayers: Layer[] | Layer): this;
+            getPosition(): any;
+            setPosition(): this;
+            addTo(map: Map): this;
+            remove(): this;
+        }
+    }
+}
