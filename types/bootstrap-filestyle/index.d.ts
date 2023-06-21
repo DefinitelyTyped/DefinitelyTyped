d9772d08d871afeccb13df8548f063b14e33b9ba
Mustafa Salaheldin <mustafa.salaheldin@yahoo.com>
2023-06-21 10:40:19 +0400

    Update index.d.ts
    Adding support to 2.1.4

--- types/bootstrap-filestyle/index.d.ts
+++ types/bootstrap-filestyle/index.d.ts
@@ -1,30 +1,34 @@
-// Type definitions for bootstrap-filestyle 1.2
-// Project: https://github.com/markusslima/bootstrap-filestyle
+// Type definitions for bootstrap-filestyle 2.1.4
+// Project: https://github.com/jcputney/bootstrap-filestyle
 // Definitions by: Mustafa Salaheldin <https://github.com/mustafasalahuldin>
-// Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
+// Definitions: https://github.com/jcputney/bootstrap-filestyle
 // TypeScript Version: 2.3
 
 /// <reference types="jquery"/>
 
 interface FilestyleOptions {
-    buttonText?: string | undefined;
-    iconName?: string | undefined;
-    buttonName?: string | undefined;
+    text?: string | undefined;
+    htmlIcon?: string | undefined;
+    btnClass?: string | undefined;
     size?: string | undefined;
     input?: boolean | undefined;
     badge?: boolean | undefined;
+    badgeName?: string | undefined;
     icon?: boolean | undefined;
     buttonBefore?: boolean | undefined;
+    dragdrop?: boolean | undefined;
     disabled?: boolean | undefined;
     placeholder?: string | undefined;
+    onChange?: ((event: FileList) => void) | undefined;
 }
 
 interface JQuery {
+
     filestyle(options?: FilestyleOptions): JQuery;
 
     filestyle(method: 'clear' | 'destroy' | 'htmlIcon' | 'htmlInput' | 'pushNameFiles'): JQuery;
 
-    filestyle(method: 'disabled' | 'buttonBefore' | 'icon' | 'input', value: boolean): JQuery;
+    filestyle(method: 'disabled' | 'buttonBefore' | 'dragdrop' | 'icon' | 'input', value: boolean): JQuery;
 
-    filestyle(method: 'size' | 'placeholder' | 'buttonText' | 'buttonName' | 'iconName', value: string): JQuery;
+    filestyle(method: 'size' | 'placeholder' | 'text' | 'btnClass' | 'htmlIcon' | 'badgeName', value: string): JQuery;
 }
