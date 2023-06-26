1c1e944c778067ff4357d8b899cba3b143104621
Nathan Shively-Sanders <293473+sandersn@users.noreply.github.com>
2023-06-26 11:21:47 -0700

    Add URLSearchParams.size (#65877)
    * Add URLSearchParams.size
    
    Required in TS 5.2 beta and above, but missing in node 14 and 16, so I
    added it for 4.9 and above.
    
    * add URLSearchParams.size for 4.8 and below

--- types/node/v14/url.d.ts
+++ types/node/v14/url.d.ts
@@ -127,6 +127,7 @@ declare module 'url' {
 
     class URLSearchParams implements Iterable<[string, string]> {
         constructor(init?: URLSearchParams | string | Record<string, string | ReadonlyArray<string>> | Iterable<[string, string]> | ReadonlyArray<[string, string]>);
+        readonly size: number;
         append(name: string, value: string): void;
         delete(name: string): void;
         entries(): IterableIterator<[string, string]>;
