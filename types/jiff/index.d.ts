976e7a4bf34869ddc455fb314c28f9c1153172c5
Daniel Kneip <daniel.kneip@pagnos.com>
2023-06-27 18:01:55 +0200

    Fix further problems with export

--- types/jiff/index.d.ts
+++ types/jiff/index.d.ts
@@ -3,44 +3,43 @@
 // Definitions by: Daniel Kneip <https://github.com/daedal-knickerbockers>
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
 
-declare namespace jiff {
-    type JSONArray = JSONValue[];
-    interface JSONObject {
-        [key: string]: JSONValue;
-    }
-    type JSONValue = string | number | boolean | null | JSONObject | JSONArray;
+type JSONArray = JSONValue[];
+type JSONObject = object;
+type JSONValue = string | number | boolean | null | JSONObject | JSONArray;
 
-    type JSONPatch = Array<{
-        op: 'add' | 'remove' | 'replace' | 'move' | 'copy' | 'test';
-        path: string;
-        value?: any;
-        from?: string;
-    }>;
+type JSONPatch = Array<{
+    op: 'add' | 'remove' | 'replace' | 'move' | 'copy' | 'test';
+    path: string;
+    value?: any;
+    from?: string;
+}>;
 
-    type InvalidPatchOpertaionError = Error & { name: 'InvalidPatchOpertaionError' };
-    type TestFailedError = Error & { name: 'TestFailedError' };
-    type PatchNotInvertibleError = Error & { name: 'PatchNotInvertibleError' };
+type InvalidPatchOpertaionError = Error & { name: 'InvalidPatchOpertaionError' };
+type TestFailedError = Error & { name: 'TestFailedError' };
+type PatchNotInvertibleError = Error & { name: 'PatchNotInvertibleError' };
 
-    type HashFunction = (value: any) => string | number;
+type HashFunction = (value: any) => string | number;
 
-    interface DiffOptions {
-        /** used to hash array items in order to recognize identical objects, defaults to JSON.stringify */
-        hash?: HashFunction;
-        /** used to generate patch context. If not provided, context will not be generated */
-        makeContext?: (index: number, array: any[]) => any;
-        invertible?: boolean;
-    }
+interface DiffOptions {
+    /** used to hash array items in order to recognize identical objects, defaults to JSON.stringify */
+    hash?: HashFunction;
+    /** used to generate patch context. If not provided, context will not be generated */
+    makeContext?: (index: number, array: any[]) => any;
+    invertible?: boolean;
+}
 
-    interface PatchOptions {
-        /** function used adjust array indexes for smarty/fuzzy patching, for patches containing context */
-        findContext?: (index: number, array: any[]) => any;
-    }
+interface PatchOptions {
+    /** function used adjust array indexes for smarty/fuzzy patching, for patches containing context */
+    findContext?: (index: number, array: any[]) => any;
+}
+
+declare var jiff: {
     /**
      * Create a deep copy of x which must be a legal JSON object/array/value
      * @param x object/array/value to clone
      * @returns clone of x
      */
-    function clone<T extends JSONValue>(x: T): T;
+    clone<T extends JSONValue>(x: T): T;
     /**
      * Compute a JSON Patch representing the differences between a and b.
      * @param a
@@ -48,8 +47,8 @@ declare namespace jiff {
      * @param options if a function, see options.hash
      * @returns JSON Patch such that patch(diff(a, b), a) ~ b
      */
-    function diff(a: JSONValue, b: JSONValue, options?: HashFunction | DiffOptions): JSONPatch;
-    function inverse(patch: JSONPatch): JSONPatch;
+    diff(a: JSONValue, b: JSONValue, options?: HashFunction | DiffOptions): JSONPatch;
+    inverse(patch: JSONPatch): JSONPatch;
     /**
      * Apply the supplied JSON Patch to x
      * @param changes JSON Patch
@@ -60,8 +59,8 @@ declare namespace jiff {
      *  an array or object, it will be mutated and returned. Otherwise, if
      *  x is a value, the new value will be returned.
      */
-    function patch(changes: JSONPatch, x: JSONValue, options?: PatchOptions): JSONValue;
-    function patchInPlace(patch: JSONPatch, a: JSONValue): void;
-}
+    patch(changes: JSONPatch, x: JSONValue, options?: PatchOptions): JSONValue;
+    patchInPlace(patch: JSONPatch, a: JSONValue): void;
+};
 
 export = jiff;
--- types/jiff/jiff-tests.ts
+++ types/jiff/jiff-tests.ts
@@ -1,9 +1,9 @@
 import * as jiff from 'jiff';
 
-const a: jiff.JSONValue = { foo: 'bar' };
+const a = { foo: 'bar' };
 const b = { foo: 'baz' };
 
-const patch: jiff.JSONPatch = jiff.diff(a, b);
+const patch = jiff.diff(a, b);
 
 const c = jiff.patch(patch, a);
 
