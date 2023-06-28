e66cc9e0de55ee33abad19de4aaf13115c804001
MDReal32 <veysaliyev00@gmail.com>
2023-06-28 04:32:18 -0400

    removed cmakelists file

--- types/cmake-js/cmake-js-tests.ts
+++ types/cmake-js/cmake-js-tests.ts
@@ -1,10 +1,1 @@
-import {BuildSystem} from "cmake-js";
-
-const buildSystem = new BuildSystem({ cmakePath: './CMakeLists.txt' });
-
-(async () => {
-    await buildSystem.install();
-    await buildSystem.configure();
-    await buildSystem.build();
-    await buildSystem.clean();
-})();
+// WIP: Will be implemented in a future release
