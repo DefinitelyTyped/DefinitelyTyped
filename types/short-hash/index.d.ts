aca9b45cf8718fb27b4a6737aa90092d83edba41
Ericsson Schroeter <ekschro@protonmail.com>
2023-06-28 17:20:53 -0400

    dts-gen

--- types/short-hash/index.d.ts
+++ types/short-hash/index.d.ts
@@ -1,5 +1,5 @@
-// Type definitions for short-hash 1.0.0
-// Project: https://github.com/joakimbeng/short-hash
+// Type definitions for short-hash 1.0
+// Project: https://github.com/joakimbeng/short-hash#readme
 // Definitions by: Ericsson Schroeter <https://github.com/ekschro>
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
 
@@ -7,5 +7,4 @@
  * Get a short hash from a string. Uses Bernstein's popular "times 33" hash algorithm but returns a hex string instead of a number.
  * @param str The string to hash.
  */
-declare function shortHash(str?: string): string;
-export = shortHash;
+export default function shortHash(str?: string): string;
--- types/short-hash/short-hash-tests.ts
+++ types/short-hash/short-hash-tests.ts
@@ -1,4 +1,4 @@
-import shortHash = require("short-hash");
+import shortHash from "short-hash";
 
 shortHash("Hello, world!"); // $ExpectType string
 shortHash(); // $ExpectType string
