78dca8cb99d96b31ed5a3a2d55830551818b6dd6
Ben Houston <neuralsoft@gmail.com>
2023-06-28 21:17:45 -0400

    fix lint mentioned issues for locate-chrome

--- types/locate-chrome/index.d.ts
+++ types/locate-chrome/index.d.ts
@@ -1,12 +1,9 @@
-// Type definitions for locate-chrome
+// Type definitions for locate-chrome 0.1
 // Project: https://github.com/davidtheclark/locate-chrome
 // Definitions by: Ben Houston <https://github.com/bhouston>
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
 
 // locate-chrome.d.ts
-declare module "locate-chrome" {
-    
-    function locateChrome(cb?: (path: string) => void): Promise<string>;
-   
-    export = locateChrome;
-}
+declare function locateChrome(cb?: (path: string) => void): Promise<string>;
+
+export = locateChrome;
--- types/locate-chrome/locate-chrome-tests.ts
+++ types/locate-chrome/locate-chrome-tests.ts
@@ -2,6 +2,6 @@ import locateChrome = require('locate-chrome');
 
 const result = locateChrome(); // $ExpectType Promise<string>
 
-locateChrome( ( path ) => {
+locateChrome(path => {
     path; // $ExpectType string
 });
