a81dfd53db4ff5a37a5ea4ac77afa2a6e018cd67
MDReal32 <veysaliyev00@gmail.com>
2023-06-30 01:31:05 -0400

    write initial tests

--- types/cmake-js/cmake-js-tests.ts
+++ types/cmake-js/cmake-js-tests.ts
@@ -1,1 +1,13 @@
-// WIP: Will be implemented in a future release
+import { writeFileSync, mkdtempSync } from 'fs'
+import { resolve } from 'path'
+import { BuildSystem } from 'cmake-js'
+
+(async () => {
+    const tmpDir = mkdtempSync("cmake-js-")
+    const cmakePath = resolve(tmpDir, "CMakeLists.txt")
+    writeFileSync(cmakePath, "cmake_minimum_required(VERSION 3.0)\nproject(cmake-js-tests)\n");
+
+    const buildSystem = new BuildSystem({ cmakePath });
+    await buildSystem.install()
+    await buildSystem.configure()
+})()
