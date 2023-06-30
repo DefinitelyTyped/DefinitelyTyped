2ebe76fb6809af6d9cd556057e8564e762072fd5
tpluscode <tpluscode@users.noreply.github.com>
2023-06-30 15:42:18 +0200

    update @rdfjs/serializer-rdfjs to v0.1 (ESM)

--- types/rdfjs__serializer-rdfjs/index.d.ts
+++ types/rdfjs__serializer-rdfjs/index.d.ts
@@ -1,4 +1,4 @@
-// Type definitions for @rdfjs/serializer-rdfjs 0.0
+// Type definitions for @rdfjs/serializer-rdfjs 0.1
 // Project: https://github.com/rdfjs-base/serializer-rdfjs
 // Definitions by: tpluscode <https://github.com/tpluscode>
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
@@ -6,17 +6,13 @@
 import { EventEmitter } from 'events';
 import { Sink, Stream, BaseQuad, Quad } from 'rdf-js';
 
-declare namespace Serializer {
-    interface SerializerOptions {
-        module?: 'esm' | 'ts' | string | undefined;
-    }
+export interface SerializerOptions {
+    module?: 'commonjs' | 'ts' | undefined;
 }
 
-declare class Serializer<Q extends BaseQuad = Quad> implements Sink<Stream<Q>, EventEmitter> {
-    constructor(options?: Serializer.SerializerOptions);
+export default class Serializer<Q extends BaseQuad = Quad> implements Sink<Stream<Q>, EventEmitter> {
+    constructor(options?: SerializerOptions);
 
-    import(stream: Stream<Q>, options?: Serializer.SerializerOptions): EventEmitter;
+    import(stream: Stream<Q>, options?: SerializerOptions): EventEmitter;
     transform(quads: Iterable<Q>): string;
 }
-
-export = Serializer;
