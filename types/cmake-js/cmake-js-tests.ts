c2c1d67847523e610ab37a1546d20b7b58a047f1
MDReal32 <veysaliyev00@gmail.com>
2023-06-30 01:48:20 -0400

    prettify

--- types/cmake-js/cmake-js-tests.ts
+++ types/cmake-js/cmake-js-tests.ts
@@ -1,13 +1,13 @@
-import { writeFileSync, mkdtempSync } from 'fs'
-import { resolve } from 'path'
-import { BuildSystem } from 'cmake-js'
+import { writeFileSync, mkdtempSync } from 'fs';
+import { resolve } from 'path';
+import { BuildSystem } from 'cmake-js';
 
 (async () => {
-    const tmpDir = mkdtempSync("cmake-js-")
-    const cmakePath = resolve(tmpDir, "CMakeLists.txt")
-    writeFileSync(cmakePath, "cmake_minimum_required(VERSION 3.0)\nproject(cmake-js-tests)\n");
+    const tmpDir = mkdtempSync('cmake-js-');
+    const cmakePath = resolve(tmpDir, 'CMakeLists.txt');
+    writeFileSync(cmakePath, 'cmake_minimum_required(VERSION 3.0)\nproject(cmake-js-tests)\n');
 
     const buildSystem = new BuildSystem({ cmakePath });
-    await buildSystem.install()
-    await buildSystem.configure()
-})()
+    await buildSystem.install();
+    await buildSystem.configure();
+})();
