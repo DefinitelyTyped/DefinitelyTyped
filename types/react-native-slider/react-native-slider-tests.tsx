0a86b1ebc71b06a2d9203262c36ae01f0723c9d8
Ankan Bhattacharya <ankanbhattacharya89@gmail.com>
2023-07-01 15:25:40 +0530

    feat: :sparkles: add types for react-native-slider

--- types/react-native-slider/react-native-slider-tests.tsx
+++ types/react-native-slider/react-native-slider-tests.tsx
@@ -1,15 +1,17 @@
 import Slider from 'react-native-slider';
 import { useState } from 'react';
 
-export default SliderTest = () => {
+const SliderTest = () => {
     const [val, setVal] = useState<number>(0);
 
     return (
         <Slider
             value={val}
-            onValueChange={e => setVal(e)}
-            onSlidingStart={e => setVal(e)}
-            onSlidingComplete={e => setVal(e)}
+            onValueChange={setVal}
+            onSlidingStart={setVal}
+            onSlidingComplete={setVal}
         />
     );
 };
+
+export default SliderTest;
