a74ca1514059c68a5f65a6a861bc9daed45578d3
Kristján Jökull Sigurðsson <kristjanjokull@gmail.com>
2023-07-04 10:10:40 +0000

    path fix

--- types/bramus__pagination-sequence/index.d.ts
+++ types/bramus__pagination-sequence/index.d.ts
@@ -3,24 +3,22 @@
 // Definitions by: Kristján Jökull Sigurðsson <https://github.com/kristjanjokull>
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
 
-declare module '@bramus/pagination-sequence' {
-    function generate(
-        curPage: number,
-        numPages: number,
-        numPagesAtEdges?: number,
-        numPagesAroundCurrent?: number,
-        glue?: string,
-    ): Array<number | string>;
+declare function generate(
+    curPage: number,
+    numPages: number,
+    numPagesAtEdges?: number,
+    numPagesAroundCurrent?: number,
+    glue?: string,
+): Array<number | string>;
 
-    type Options = {
-        curPage?: number;
-        numPages?: number;
-        numPagesAtEdges?: number;
-        numPagesAroundCurrent?: number;
-        glue?: string;
-    };
-
-    function generateFromObj(opts: Options): Array<string | number>;
-
-    export { generate, generateFromObj };
+interface Options {
+    curPage?: number;
+    numPages?: number;
+    numPagesAtEdges?: number;
+    numPagesAroundCurrent?: number;
+    glue?: string;
 }
+
+declare function generateFromObj(opts: Options): Array<string | number>;
+
+export { generate, generateFromObj };
