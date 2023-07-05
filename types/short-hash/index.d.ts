6cb85242a961ba166f956f6b35c7614be027ec21
Ericsson Schroeter <ekschro@protonmail.com>
2023-07-05 09:37:54 -0400

    Require a string as an argument.

--- types/short-hash/index.d.ts
+++ types/short-hash/index.d.ts
@@ -7,4 +7,4 @@
  * Get a short hash from a string. Uses Bernstein's popular "times 33" hash algorithm but returns a hex string instead of a number.
  * @param str The string to hash.
  */
-export default function shortHash(str?: string): string;
+export default function shortHash(str: string): string;
--- types/short-hash/short-hash-tests.ts
+++ types/short-hash/short-hash-tests.ts
@@ -1,4 +1,3 @@
 import shortHash from "short-hash";
 
 shortHash("Hello, world!"); // $ExpectType string
-shortHash(); // $ExpectType string
