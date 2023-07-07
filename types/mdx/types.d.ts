cabcabb32aa12c0bf3e4c337c9b54b51e615faeb
Remco Haszing <remcohaszing@gmail.com>
2023-07-07 14:41:46 +0200

    Add comments to explain the MDX types

--- types/mdx/types.d.ts
+++ types/mdx/types.d.ts
@@ -1,26 +1,53 @@
 // Internal helper types
 
+/**
+ * This is the global JSX.ElementType if it’s defined, otherwise never.
+ */
 // @ts-ignore
 type ElementType = any extends JSX.ElementType ? never : JSX.ElementType;
 
+/**
+ * This matches any function component types that ar part of `ElementType`.
+ */
 type FunctionElementType = Extract<ElementType, (props: Record<string, any>) => any>;
 
+/**
+ * This matches any class component types that ar part of `ElementType`.
+ */
 type ClassElementType = Extract<ElementType, new (props: Record<string, any>) => any>;
 
+/**
+ * A valid JSX string component.
+ */
 type StringComponent = Extract<keyof JSX.IntrinsicElements, ElementType extends never ? string : ElementType>;
 
+/**
+ * A valid JSX function component.
+ */
 type FunctionComponent<Props> = ElementType extends never
-    ? (props: Props) => JSX.Element | null
+    ? // If JSX.ElementType isn’t defined, the valid return type is JSX.Element
+      (props: Props) => JSX.Element | null
     : FunctionElementType extends never
-    ? never
-    : (props: Props) => ReturnType<FunctionElementType>;
+    ? // If JSX.ElementType is defined, but doesn’t allow function components, function components are disallowed.
+      never
+    : // If JSX.ElementType allows function components, its return value determines what is a valid.
+      (props: Props) => ReturnType<FunctionElementType>;
 
+/**
+ * A valid JSX class component.
+ */
 type ClassComponent<Props> = ElementType extends never
-    ? new (props: Props) => JSX.ElementClass
+    ? // If JSX.ElementType isn’t defined, the valid return type is a constructor that returns JSX.ElementClass
+      new (props: Props) => JSX.ElementClass
     : ClassElementType extends never
-    ? never
-    : new (props: Props) => InstanceType<ClassElementType>;
+    ? // If JSX.ElementType is defined, but doesn’t allow constructors, function components are disallowed.
+      never
+    : // If JSX.ElementType allows class components, its return value determines what is a valid.
+      new (props: Props) => InstanceType<ClassElementType>;
 
+/**
+ * Any allowed JSX component.
+ */
 type Component<Props> = FunctionComponent<Props> | ClassComponent<Props> | StringComponent;
 
 interface NestedMDXComponents {
@@ -79,4 +106,5 @@ export interface MDXModule {
     default: MDXContent;
 }
 
+// This marks the file as a module, meaning not all types are implicitly exported.
 export {};
