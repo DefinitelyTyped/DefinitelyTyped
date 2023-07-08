c3f1f501b7ad6e8a3d48a7ee8118f79fd4e9ab59
JeanJPNM <61994401+JeanJPNM@users.noreply.github.com>
2023-07-07 23:49:38 -0300

    export the interface under a namespace

--- types/markdown-it-container/index.d.ts
+++ types/markdown-it-container/index.d.ts
@@ -8,16 +8,19 @@ import MarkdownIt = require('markdown-it');
 import Renderer = require('markdown-it/lib/renderer');
 import Token = require('markdown-it/lib/token');
 
-export interface ContainerOpts {
-    marker?: string | undefined;
-    validate?(params: string): boolean;
-    render?: Renderer.RenderRule | undefined;
-}
-
 declare function MarkdownItContainer(
     md: MarkdownIt,
     name: string,
     opts: ContainerOpts,
 ): void;
+
+declare namespace MarkdownItContainer {
+    interface ContainerOpts {
+        marker?: string | undefined;
+        validate?(params: string): boolean;
+        render?: Renderer.RenderRule | undefined;
+    }
+}
+
 
 export = MarkdownItContainer;
