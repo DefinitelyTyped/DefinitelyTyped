c68f006e07a3b5f005b8045b5c529e7e76debe90
JeanJPNM <61994401+JeanJPNM@users.noreply.github.com>
2023-07-07 22:15:37 -0300

    Update index.d.ts
    Removes the unecessary namespace, exports the ContainerOpts interface and uses the markdown-it's RenderRule type instead of duplicating it.

--- types/markdown-it-container/index.d.ts
+++ types/markdown-it-container/index.d.ts
@@ -8,15 +8,16 @@ import MarkdownIt = require('markdown-it');
 import Renderer = require('markdown-it/lib/renderer');
 import Token = require('markdown-it/lib/token');
 
-declare namespace markdownItContainer {
-    interface ContainerOpts {
-        marker?: string | undefined;
-        validate?(params: string): boolean;
-        render?(tokens: Token[], index: number, options: any, env: any, self: Renderer): string;
-    }
-
-    function container_plugin(md: MarkdownIt, name: string, opts: ContainerOpts): void;
+export interface ContainerOpts {
+    marker?: string | undefined;
+    validate?(params: string): boolean;
+    render?: Renderer.RenderRule | undefined;
 }
 
-declare var MarkdownItContainer: typeof markdownItContainer.container_plugin;
+declare function MarkdownItContainer(
+    md: MarkdownIt,
+    name: string,
+    opts: ContainerOpts,
+): void;
+
 export = MarkdownItContainer;
