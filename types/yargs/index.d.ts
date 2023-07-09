06c2ac1f217a9b768002e6a2ffb3fdc28413fc87
Aber Erre <arblugo@gmail.com>
2023-07-09 12:46:39 +0100

    fix: missing type for command module input (array)

--- types/yargs/index.d.ts
+++ types/yargs/index.d.ts
@@ -177,6 +177,7 @@ declare namespace yargs {
         command(command: string | ReadonlyArray<string>, showInHelp: false, module: CommandModule<T, any>): Argv<T>;
         command(module: CommandModule<T, any>): Argv<T>;
         command(modules: Array<CommandModule<T, any>>): Argv<T>;
+        command(modules: Array<CommandModule<T, any>>[]): Argv<T>;
 
         // Advanced API
         /** Apply command modules from a directory relative to the module calling this method. */
