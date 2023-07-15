570522a6f93055bcb1004c281eb19d9647b93ac2
Zhang Yi Jiang <mediumdeviation@gmail.com>
2023-07-14 21:08:45 -0700

    Add diffable-html

---
+++ types/diffable-html/index.d.ts
@@ -0,0 +1,10 @@
+// Type definitions for diffable-html 5.0
+// Project: https://github.com/rayrutjes/diffable-html#readme
+// Definitions by: Zhang Yi Jiang <https://github.com/DefinitelyTyped>
+// Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
+
+declare function format(html: string, options?: {
+    sortAttributes?: (attrs: string[]) => string[],
+}): string;
+
+export = format;
