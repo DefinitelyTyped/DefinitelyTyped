94b3b8b3fd937f70fe7c19665d89aa3622f3088c
Dmitry Dyachkov <git@e965.ru>
2023-07-20 21:11:29 +0500

    fix: fix whitespaces

--- types/luxon/src/settings.d.ts
+++ types/luxon/src/settings.d.ts
@@ -41,7 +41,7 @@ export class Settings {
      * The default output calendar to create DateTimes with. Does not affect existing instances.
      */
     static defaultOutputCalendar: string;
-    
+
     /**
      * The cutoff year after which a string encoding a year as two digits is interpreted to occur in the current century.
      */
