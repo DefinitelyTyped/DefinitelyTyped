93aed9bc5763efb842ff6c29df5c498aa7139f23
Julian Wiche <julian@wiche.eu>
2023-07-27 11:02:44 +0200

    Update types/arraystat/index.d.ts
    Co-authored-by: Piotr Błażejewicz (Peter Blazejewicz) <peterblazejewicz@users.noreply.github.com>

--- types/arraystat/index.d.ts
+++ types/arraystat/index.d.ts
@@ -3,11 +3,6 @@
 // Definitions by: Julian Wiche <https://github.com/dajuly20>
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
 
-/*~ If this module is a UMD module that exposes a global variable 'myLib' when
- *~ loaded outside a module loader environment, declare that global here.
- *~ Otherwise, delete this declaration.
- */
-
 interface ArrayStatType {
     sum: number;
     nb: number;
