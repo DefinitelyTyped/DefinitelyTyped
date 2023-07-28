8d9bf16351c7b202e4fcf6ccaed15e584484d52e
PCOffline <bekerman122@gmail.com>
2023-07-28 23:40:11 +0300

    style: remove redundant declare keyword

--- types/big-json/index.d.ts
+++ types/big-json/index.d.ts
@@ -7,17 +7,17 @@
 
 import type { Stream } from 'node:stream';
 
-export declare function createParseStream(): Stream;
-export declare function createStringifyStream(opts: { body: object }): Stream;
+export function createParseStream(): Stream;
+export function createStringifyStream(opts: { body: object }): Stream;
 
-export declare function parse(opts: { body: string | Buffer }): Promise<object | ReadonlyArray<object>>;
-export declare function parse(
+export function parse(opts: { body: string | Buffer }): Promise<object | ReadonlyArray<object>>;
+export function parse(
     opts: { body: string | Buffer },
     callback: (result: object | ReadonlyArray<object>) => void,
 ): void;
 
-export declare function stringify(opts: { body: object }): Promise<string>;
-export declare function stringify(opts: { body: object }, callback: (result: string) => void): void;
+export function stringify(opts: { body: object }): Promise<string>;
+export function stringify(opts: { body: object }, callback: (result: string) => void): void;
 
 declare const exported: {
     createParseStream: typeof createParseStream;
