98a674a9fce21aa0531915e6ca3733a3e161dd6f
PCOffline <bekerman122@gmail.com>
2023-07-28 20:31:13 +0300

    test: expect ts errors

--- types/big-json/big-json-tests.ts
+++ types/big-json/big-json-tests.ts
@@ -3,6 +3,9 @@
 import bigJson from 'big-json';
 import type { Stream } from 'node:stream';
 
+// ----------------------------------------------------------------
+// stringify
+// ----------------------------------------------------------------
 // $ExpectType Promise<string>
 const promisifiedStringifiedJson = bigJson.stringify({ body: { a: 'abc' } });
 
@@ -14,6 +17,15 @@ bigJson.stringify({ body: { a: 'abc' } },
   }
 );
 
+// @ts-expect-error
+bigJson.stringify({ a: 'abc' });
+
+// @ts-expect-error
+bigJson.stringify({ body: 'abc' });
+
+// ----------------------------------------------------------------
+// parse
+// ----------------------------------------------------------------
 // $ExpectType Promise<object | readonly object[]>
 const promisifedParseJson = bigJson.parse({ body: '{ "a": "abc" }' });
 
@@ -25,8 +37,29 @@ bigJson.parse({ body: '{ "a": "abc" }' },
   }
 );
 
+// @ts-expect-error
+bigJson.parse('{ "a": "abc" }');
+
+// @ts-expect-error
+bigJson.parse({ body: 123 });
+
+// @ts-expect-error
+bigJson.parse({ body: { a: 'abc' } });
+
+// ----------------------------------------------------------------
+// createStringifyStream
+// ----------------------------------------------------------------
 // $ExpectType Stream
 const stringifyStream = bigJson.createStringifyStream({ body: { a: 'abc' } });
 
+// @ts-expect-error
+bigJson.createStringifyStream({ a: 'abc' });
+
+// @ts-expect-error
+bigJson.createStringifyStream({ body: 'blabla' });
+
+// ----------------------------------------------------------------
+// createParseStream
+// ----------------------------------------------------------------
 // $ExpectType Stream
 const parseStream = bigJson.createParseStream();
