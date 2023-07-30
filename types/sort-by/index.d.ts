a3dac30dad4b3461123b20cdaa6454c67f82e71c
Adnan AlBeda <adnan.albeda@outlook.com>
2023-07-31 00:32:10 +0300

    Update sort-by index.d.ts for better intellisense suggestions.
    Fixed custom comparer builder.

--- types/sort-by/index.d.ts
+++ types/sort-by/index.d.ts
@@ -4,6 +4,6 @@
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
 
 declare function sortBy<T = Record<string,unknown>>(...args: ReadonlyArray<keyof T | `-${keyof T}`>): (a: T, b: T) => number;
-declare function sortBy<T = Record<string,unknown>>(...args: Array<keyof T | `-${keyof T}` | (<K extends keyof T>(key: K, value: T[K]) => any)>): (a: T, b: T) => number;
+declare function sortBy<T = Record<string,unknown>, K extends keyof T = keyof T>(...args: Array<K | `-${K}` | ((key: K, value: T[K]) => unknown)>): (a: T, b: T) => number;
 
 export = sortBy;
