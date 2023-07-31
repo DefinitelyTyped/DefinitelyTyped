5acf6472c229fe4be4863a3bb1a0c9bca873a0b6
Aleksandr Shumilov <alexander.shumilov@toptal.com>
2023-07-31 12:29:49 +0200

    fix: add test

--- types/happo-cypress/index.d.ts
+++ types/happo-cypress/index.d.ts
@@ -11,35 +11,34 @@ export type HappoScreenshotTarget = {
     browser: string,
     viewport: string,
 }
-  
-  export interface HappoScreenshotOptions {
-      component?: string | undefined;
-      variant?: string | undefined;
-      targets?: ReadonlyArray<string | HappoScreenshotTarget> | undefined;
-      responsiveInlinedCanvases?: boolean | undefined;
-      transformDOM?: HappoTransformDOMOptions | undefined;
-      includeAllElements?: boolean | undefined;
-  }
-  
-  export interface HappoTransformDOMOptions {
-      selector: string;
-      transform: (element: Element, document: Document) => Element;
-  }
-  
-  export interface HappoHideDynamicElementsOptions {
-      matchers?: ReadonlyArray<RegExp> | undefined;
-      defaultMatchers?: ReadonlyArray<RegExp> | undefined;
-      selectors?: ReadonlyArray<string> | undefined;
-      defaultSelectors?: ReadonlyArray<string> | undefined;
-      replace?: boolean | undefined;
-  }
-  
-  declare global {
-      namespace Cypress {
-          interface Chainable {
-              happoScreenshot(options?: HappoScreenshotOptions): void;
-              happoHideDynamicElements(options?: HappoHideDynamicElementsOptions): void;
-          }
-      }
-  }
-  
+
+export interface HappoScreenshotOptions {
+    component?: string | undefined;
+    variant?: string | undefined;
+    targets?: ReadonlyArray<string | HappoScreenshotTarget> | undefined;
+    responsiveInlinedCanvases?: boolean | undefined;
+    transformDOM?: HappoTransformDOMOptions | undefined;
+    includeAllElements?: boolean | undefined;
+}
+
+export interface HappoTransformDOMOptions {
+    selector: string;
+    transform: (element: Element, document: Document) => Element;
+}
+
+export interface HappoHideDynamicElementsOptions {
+    matchers?: ReadonlyArray<RegExp> | undefined;
+    defaultMatchers?: ReadonlyArray<RegExp> | undefined;
+    selectors?: ReadonlyArray<string> | undefined;
+    defaultSelectors?: ReadonlyArray<string> | undefined;
+    replace?: boolean | undefined;
+}
+
+declare global {
+    namespace Cypress {
+        interface Chainable {
+            happoScreenshot(options?: HappoScreenshotOptions): void;
+            happoHideDynamicElements(options?: HappoHideDynamicElementsOptions): void;
+        }
+    }
+}
