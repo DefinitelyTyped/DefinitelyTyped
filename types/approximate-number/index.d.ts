81689608fb60ea3b601b56850143e9c19343c1b0
Giorgi Kakhoshvili <giorgi.kakhoshvili@rebilly.com>
2023-08-01 22:13:49 +0400

    add tests

--- types/approximate-number/index.d.ts
+++ types/approximate-number/index.d.ts
@@ -1,24 +1,20 @@
-// Type definitions for approximate-number 2.1.0
+// Type definitions for approximate-number 2.1
 // Project: https://github.com/nfriedly/approximate-number
 // Definitions by: Giorgi Kakhoshvili <https://github.com/giokaxo>
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
-// TypeScript Version: 5.1.6
 
-declare module 'approximate-number' {
-    export type Options = {
-        separator: string | boolean;
-        decimal: string | boolean;
-        round: boolean;
-        min10k: boolean;
-        prefix: string;
-        suffix: string;
-        capital: boolean;
-        precision: number | undefined;
-    };
-    function approximateNumber(
-        input: number | string,
-        options?: Options,
-    ): string;
+export type Options = {
+    separator?: string | boolean;
+    decimal?: string | boolean;
+    round?: boolean;
+    min10k?: boolean;
+    prefix?: string;
+    suffix?: string;
+    capital?: boolean;
+    precision?: number | undefined;
+};
 
-    export default approximateNumber;
-}
+export default function approximateNumber(
+    input: number | string,
+    options?: Options,
+): string;
