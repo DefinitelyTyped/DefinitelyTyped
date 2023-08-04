fac7aeaa7b1870f9cabda0ba349a3049ac1e3c6c
Phil Nash <philnash@gmail.com>
2023-08-04 16:44:34 +1000

    [node] adds "type" to test runner event details
    In [version 20.0.0 (and backported to 19.9.0 and 18.7.0)](https://github.com/nodejs/node/pull/47094)
    the test runner started reporting on whether a test was a suite. This
    was exposed to reporters in the `details` object of a `test:pass` or
    `test:fail` event but this hasn't been documented. This adds the `type`
    property to both event's `details` object.

--- types/node/ts4.8/test.d.ts
+++ types/node/ts4.8/test.d.ts
@@ -1205,6 +1205,11 @@ interface TestFail {
          * The error thrown by the test.
          */
         error: Error;
+        /**
+         * The type of the test, used to denote whether this is a suite.
+         * @since 20.0.0, 19.9.0, 18.17.0
+         */
+        type?: 'suite';
     };
     /**
      * The test name.
@@ -1240,6 +1245,11 @@ interface TestPass {
          * The duration of the test in milliseconds.
          */
         duration_ms: number;
+        /**
+         * The type of the test, used to denote whether this is a suite.
+         * @since 20.0.0, 19.9.0, 18.17.0
+         */
+        type?: 'suite';
     };
     /**
      * The test name.
