e6142de1e6d713925bcfc9489f0c86e72269b6d5
Said Torres <sesaid17@hotmail.com>
2023-08-07 15:46:26 -0600

    Normalized definitions

---
+++ types/langdetect/langdetect-tests.ts
@@ -0,0 +1,5 @@
+import * as langdetect from 'langdetect';
+
+console.log(langdetect.detect('Questo a che ora comincia? I don\'t know'));
+
+console.log(langdetect.detectOne('Questo a che ora comincia?'));
