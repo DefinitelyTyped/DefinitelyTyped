2a6e74b782c6cbed31aad7759ebe6490a343f9f9
Josh Goldberg <git@joshuakgoldberg.com>
2023-08-11 14:17:18 -0400

    [arcgis-js-api] ran dprint fmt

--- types/arcgis-js-api/arcgis-js-api-tests.ts
+++ types/arcgis-js-api/arcgis-js-api-tests.ts
@@ -1,31 +1,31 @@
-import Map = require("esri/Map");
-import MapView = require("esri/views/MapView");
-import Point = require("esri/geometry/Point");
+import Map = require('esri/Map');
+import MapView = require('esri/views/MapView');
+import Point = require('esri/geometry/Point');
 
 class MapController {
-  map: Map;
+    map: Map;
 
-  constructor(public mapDiv: string) {
-  }
+    constructor(public mapDiv: string) {
+    }
 
-  start() {
-    let point = new Point({
-      latitude: 37.75,
-      longitude: -122.45
-    });
+    start() {
+        let point = new Point({
+            latitude: 37.75,
+            longitude: -122.45,
+        });
 
-    this.map = new Map({
-      basemap: { title: "topo-vector" }
-    });
+        this.map = new Map({
+            basemap: { title: 'topo-vector' },
+        });
 
-    let view = new MapView({
-      center: point,
-      container: this.mapDiv,
-      map: this.map,
-      zoom: 7
-    });
-  }
+        let view = new MapView({
+            center: point,
+            container: this.mapDiv,
+            map: this.map,
+            zoom: 7,
+        });
+    }
 }
 
-import esriRequest = require("esri/request");
-esriRequest(new URL("https://js.arcigs.com/"));
+import esriRequest = require('esri/request');
+esriRequest(new URL('https://js.arcigs.com/'));
