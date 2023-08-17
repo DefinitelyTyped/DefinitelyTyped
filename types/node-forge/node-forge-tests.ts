9972732934bf3a9b080d41603ce559164a5a855b
Antoine Kapps <akapps@digidna.net>
2023-08-17 12:53:38 +0200

    [node-forge] more details about PKCS#7 EnvelopedData
    Encryption: a key and (more importantly) the symmetric cipher
    can be specified when calling "encrypt()".
    
    Decryption: the correct recipient should be retrieved based
    on the certificate, then content can be decrypted with the
    private key.
    
    Documentation copied from 'node-forge'.

--- types/node-forge/node-forge-tests.ts
+++ types/node-forge/node-forge-tests.ts
@@ -727,12 +727,22 @@ if (forge.util.fillString('1', 5) !== '11111') throw Error('forge.util.fillStrin
 }
 
 {
+    // create an EnvelopedData with encrypted content (3DES with RSA)
     let p7 = forge.pkcs7.createEnvelopedData();
     let cert = forge.pki.certificateFromPem(certPem);
+    let symmetricCipher = forge.pki.oids['des-EDE3-CBC'];
     p7.addRecipient(cert);
     p7.content = forge.util.createBuffer('content');
-    p7.encrypt();
+    p7.encrypt(undefined, symmetricCipher);
     let asn1: forge.asn1.Asn1 = p7.toAsn1();
+
+    // parse and decrypt the result
+    let encP7 = forge.pkcs7.messageFromAsn1(asn1) as forge.pkcs7.PkcsEnvelopedData;
+    let recipient = encP7.findRecipient(cert);
+    encP7.decrypt(recipient, privateKeyRsa);
+    if (p7.content !== encP7.content) {
+        throw new Error('decrypted result does not match');
+    }
 }
 
 {
