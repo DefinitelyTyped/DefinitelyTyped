14ff2228f9840443daef851de87bbd37fdd931de
irfan-zahir <irfan.zahirharon@gmail.com>
2023-08-19 12:49:18 +0800

    added type definitions for duitnow-js

---
+++ types/duitnow-js/duitnow-js-tests.ts
@@ -0,0 +1,8 @@
+import { generateDuitNowStr } from 'duitnow-js';
+
+generateDuitNowStr({
+    account: 'MAEPP111134456652251',
+    app: '588734',
+    name: 'TEST NAME DUITNOW'
+
+}); // $ExpectType string
