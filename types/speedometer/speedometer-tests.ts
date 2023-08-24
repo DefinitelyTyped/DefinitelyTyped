1b25819c7fdac71d1a4d7f842c9d16552d296190
dylan <dylan.grandjean@freecaster.com>
2023-08-24 14:14:44 +0200

    Fix multiple import

--- types/speedometer/speedometer-tests.ts
+++ types/speedometer/speedometer-tests.ts
@@ -1,6 +1,5 @@
 import * as speedometer from 'speedometer';
-import {Speedometer} from "speedometer";
 
-const _speedometer: Speedometer = speedometer(10);
+const _speedometer: speedometer.Speedometer = speedometer(10);
 _speedometer(25);
 _speedometer(600);
