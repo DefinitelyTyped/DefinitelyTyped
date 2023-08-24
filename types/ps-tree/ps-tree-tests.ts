66a7b2f1d2186d3459afe2fa74ee701db2dd4789
Mohsen Azimi <me@azimi.me>
2023-08-24 17:40:36 +0200

    fix unit test

--- types/ps-tree/ps-tree-tests.ts
+++ types/ps-tree/ps-tree-tests.ts
@@ -4,7 +4,7 @@ ps_tree(1, () => {}); // $ExpectType void
 ps_tree(1, (error: Error | null, children: ReadonlyArray<ps_tree.PS>) => {
     if (error) return error;
     error; // $ExpectType null
-    children[0].COMMAND; // $ExpectType string
+    children[0].COMM; // $ExpectType string
     children[0].PID; // $ExpectType string
     children[0].PPID; // $ExpectType string
     children[0].STAT; // $ExpectType string
