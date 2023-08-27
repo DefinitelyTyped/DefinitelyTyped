68cedb1baf305ba437cab39a574bb7f2b901f4fd
Eric Kwoka <43540491+ekwoka@users.noreply.github.com>
2023-08-27 09:47:23 +0400

    :label: Improves Types for user expansion

--- types/alpinejs/index.d.ts
+++ types/alpinejs/index.d.ts
@@ -130,7 +130,7 @@ export interface DirectiveCallback {
 
 export type WalkerCallback = (el: ElementWithXAttributes, skip: () => void) => void;
 
-export type AlpineComponent<T> = T & XDataContext & ThisType<T & XDataContext & AlpineMagics<T>>;
+export type AlpineComponent<T> = T & XDataContext & ThisType<T & XDataContext & Magics<T>>;
 
 interface XDataContext {
     /**
@@ -142,43 +142,51 @@ interface XDataContext {
      */
     destroy?(): void;
 }
-export interface AlpineMagics<T> {
+
+export interface Stores {}
+
+export interface Magics<T> {
     /**
      * Access to current Alpine data.
      */
     $data: T;
     /**
+     * Dispatches a CustomEvent on the current DOM node.
+     * Event automatically bubbles up.
+     *
+     * @param event the event name
+     * @param detail an event-dependent value associated with the event, the value is then available to the handler using the CustomEvent.detail property
+     */
+    $dispatch: (event: string, detail?: any) => void;
+    /**
      * Retrieve the current DOM node.
      */
     $el: HTMLElement;
-    /**
-     * Retrieve DOM elements marked with x-ref inside the component.
-     */
-    $refs: Record<string, HTMLElement>;
-    /**
-     * Access registered global Alpine stores.
-     */
-    $store: (name: string) => XDataContext | string | number | boolean;
-    /**
-     * Dispatch browser events.
-     *
-     * @param event the event name
-     * @param data an event-dependent value associated with the event, the value is then available to the handler using the CustomEvent.detail property
-     */
-    $dispatch: (event: string, data?: any) => void;
     /**
      * Generate an element's ID and ensure that it won't conflict with other IDs of the same name on the same page.
      *
      * @param name the name of the id
      * @param key suffix on the end of the generated ID, usually helpful for the purpose of identifying id in a loop
      */
-    $id: (name: string, key?: number | string) => string;
+    $id: (name: string, key?: number | string | null) => string;
     /**
      * Execute a given expression AFTER Alpine has made its reactive DOM updates.
      *
      * @param callback a callback that will be fired after Alpine finishes updating the DOM
      */
     $nextTick: (callback?: () => void) => Promise<void>;
+    /**
+     * Retrieve DOM elements marked with x-ref inside the component.
+     */
+    $refs: Record<string, HTMLElement>;
+    /**
+     * Accesses the root element of the current component context.
+     */
+    $root: ElementWithXAttributes;
+    /**
+     * Access registered global Alpine stores.
+     */
+    $store: Stores
     /**
      * Fire the given callback when the value in the property is changed.
      *
