540dc34e332459740221977323947b004454f793
Ana Kucherova <9889333+chilldriven@users.noreply.github.com>
2023-08-28 23:31:50 +0100

    Fix callbackFn docs

--- types/object.groupby/index.d.ts
+++ types/object.groupby/index.d.ts
@@ -8,7 +8,7 @@
 /**
  * Returns an object that groups the items of the iterable object into arrays, using the return value of the callback function as the key.
  * @param entries An iterable object that contains key-value entries for properties and methods.
- * @param callbackfn A function that accepts up to three arguments. The map method calls the callbackfn function one time for each element in `entries`.
+ * @param callbackfn A function that accepts up to two arguments. The map method calls the callbackfn function one time for each element in `entries`.
  */
 declare function groupBy<T, K extends PropertyKey>(
     entries: Iterable<T>,
@@ -18,7 +18,7 @@ declare function groupBy<T, K extends PropertyKey>(
 /**
  * Returns an object that groups the items of the iterable object into arrays, using the return value of the callback function as the key.
  * @param entries An iterable object that contains key-value entries for properties and methods.
- * @param callbackfn A function that accepts up to three arguments. The map method calls the callbackfn function one time for each element in `entries`.
+ * @param callbackfn A function that accepts up to two arguments. The map method calls the callbackfn function one time for each element in `entries`.
  */
 declare function groupBy(
     entries: Iterable<any>,
