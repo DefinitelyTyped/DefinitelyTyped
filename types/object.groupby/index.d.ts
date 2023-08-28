bd8c7763ef07009543aaf8c0e4ef38d8cb319851
Ana Kucherova <9889333+chilldriven@users.noreply.github.com>
2023-08-28 19:42:55 +0100

    Add types for object.groupby

---
+++ types/object.groupby/index.d.ts
@@ -0,0 +1,27 @@
+// Type definitions for object.groupby 1.0
+// Project: https://github.com/es-shims/Object.groupBy#readme
+// Definitions by: Ana Kucherova <https://github.com/chilldriven>
+// Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
+// Minimum TypeScript Version: 3.4
+
+/**
+ * Returns an object that groups the items of the iterable object into arrays, using the return value of the callback function as the key.
+ * @param entries An iterable object that contains key-value entries for properties and methods.
+ * @param callbackfn A function that accepts up to three arguments. The map method calls the callbackfn function one time for each element in `entries`.
+ */
+declare function groupBy<T, K extends PropertyKey>(
+    entries: Iterable<T>,
+    callbackfn: (value: T, index: number, thisArg: T[]) => K
+): Record<K, T[]>;
+
+/**
+ * Returns an object that groups the items of the iterable object into arrays, using the return value of the callback function as the key.
+ * @param entries An iterable object that contains key-value entries for properties and methods.
+ * @param callbackfn A function that accepts up to three arguments. The map method calls the callbackfn function one time for each element in `entries`.
+ */
+declare function groupBy(
+    entries: Iterable<any>,
+    callbackfn: (value: any, index: number, thisArg: any[]) => PropertyKey
+): Record<PropertyKey, any[]>;
+
+export = groupBy;
