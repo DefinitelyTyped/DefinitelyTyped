a53180a4ed2f5266da763dbae4cf8d4eca807d88
Nathan Shively-Sanders <293473+sandersn@users.noreply.github.com>
2023-08-29 15:50:14 -0700

    Change name of rule too

--- types/lodash/lodash-tests.ts
+++ types/lodash/lodash-tests.ts
@@ -1,4 +1,4 @@
-// eslint-disable-next-line no-relative-import-in-test
+// eslint-disable-next-line @definitelytyped/no-relative-import-in-test
 import fp = require("./fp");
 import _ = require("lodash");
 
