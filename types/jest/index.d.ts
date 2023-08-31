303b18bff55d93d223267b65d8bc7029b85ff997
Marcin <marcin.strzyz@outlook.com>
2023-08-30 20:55:37 -0700

    further reductions.

--- types/jest/index.d.ts
+++ types/jest/index.d.ts
@@ -502,10 +502,10 @@ declare namespace jest {
     } & { [K in PropertyKeysOf<T>]: MaybeMockedDeep<T[K]> };
     type MaybeMockedDeep<T> = T extends MockableFunction
         ? MockedFunctionDeep<T>
-        : T extends object // eslint-disable-line @typescript-eslint/ban-types
+        : T extends object  
         ? MockedObjectDeep<T>
         : T;
-    // eslint-disable-next-line @typescript-eslint/ban-types
+     
     type MaybeMocked<T> = T extends MockableFunction ? MockedFn<T> : T extends object ? MockedObject<T> : T;
     type EmptyFunction = () => void;
     type ArgsType<T> = T extends (...args: infer A) => any ? A : never;
