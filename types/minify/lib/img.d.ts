7da74815b09b381ca2b61500eec1c989d904e9b8
Josh Goldberg ✨ <git@joshuakgoldberg.com>
2023-09-01 16:27:35 -0400

    chore: removed remaining formatting lint rules (#66522)
    * chore: removed remaining formatting lint rules
    
    * Delete invalid file
    
    * Got quotemark as well
    
    * Remove another invalid file
    
    * Also remove comment-format
    
    * Remove logic that deletes tslint.json
    
    * Appease the linter
    
    * Reset tslint.json files and re-ran the script
    
    * Empty commit to re-trigger build
    
    * fix: re-disable no-irregular-whitespace
    
    * Two missed tslint:disable comments added back in

--- types/minify/lib/img.d.ts
+++ types/minify/lib/img.d.ts
@@ -9,5 +9,4 @@ import type { Options } from './minify';
  * @param [userOptions] - (optional) object that may contain an `img` key with an object of options
  * @async
  */
-// tslint:disable-next-line:space-before-function-paren
 export default function (name: string, data: string, userOptions?: Pick<Options, 'img'>): Promise<string>;
