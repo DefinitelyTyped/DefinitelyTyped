73dd4327411527629fa60bf159c351b09b697e1a
Kristofer Skyttner <kristofer.skyttner@skymaker.se>
2023-09-09 21:35:05 +0200

    fix: Corrected polyline export in helper too

--- types/dxf/toPolylines.d.ts
+++ types/dxf/toPolylines.d.ts
@@ -1,4 +1,7 @@
 import * as Information from './Information';
 import { Box } from './Utils';
 
-export default function toPolylines(parsed: Information.FileInfo): { bbox: Box, polylines: Information.Polyline[] };
+export interface PolylineExport { bbox: Box, polylines: Information.Polyline[] }
+
+
+export default function toPolylines(parsed: Information.FileInfo): PolylineExport;
