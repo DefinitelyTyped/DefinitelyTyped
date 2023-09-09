31e46937909d8e62e55ab5cb43ce1a22714bedf4
Kristofer Skyttner <kristofer.skyttner@skymaker.se>
2023-09-09 21:23:05 +0200

    fix: Corrected type for toPolylines export to match actual export
    Fixing https://github.com/skymakerolof/dxf/issues/133

--- types/dxf/toPolylines.d.ts
+++ types/dxf/toPolylines.d.ts
@@ -1,3 +1,4 @@
 import * as Information from './Information';
+import { Box } from './Utils';
 
-export default function toPolylines(parsed: Information.FileInfo): Information.Polyline[];
+export default function toPolylines(parsed: Information.FileInfo): { bbox: Box, polylines: Information.Polyline[] };
