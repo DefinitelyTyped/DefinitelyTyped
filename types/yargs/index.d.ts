60450c7935023174edc4fb3fa8cf91a646f2c405
Shingyx <shingyx.dev@gmail.com>
2023-09-16 13:41:08 +1000

    yargs: Fix arg type declaration regression in command modules

--- types/yargs/index.d.ts
+++ types/yargs/index.d.ts
@@ -159,7 +159,8 @@ declare namespace yargs {
             middlewares?: Array<MiddlewareFunction<O>>,
             deprecated?: boolean | string,
         ): Argv<T>;
-        command(command: string | ReadonlyArray<string>, description: string, module: CommandModule<T, any>): Argv<T>;
+        // eslint-disable-next-line @definitelytyped/no-unnecessary-generics
+        command<U = any>(command: string | ReadonlyArray<string>, description: string, module: CommandModule<T, U>): Argv<T>;
         command<U = T>(
             command: string | ReadonlyArray<string>,
             showInHelp: false,
@@ -175,8 +176,10 @@ declare namespace yargs {
             handler?: (args: ArgumentsCamelCase<InferredOptionTypes<O>>) => void | Promise<void>,
         ): Argv<T>;
         command(command: string | ReadonlyArray<string>, showInHelp: false, module: CommandModule<T, any>): Argv<T>;
-        command(module: CommandModule<T, any>): Argv<T>;
-        command(modules: Array<CommandModule<T, any>>): Argv<T>;
+        // eslint-disable-next-line @definitelytyped/no-unnecessary-generics
+        command<U = any>(module: CommandModule<T, U>): Argv<T>;
+        // eslint-disable-next-line @definitelytyped/no-unnecessary-generics
+        command<U = any>(modules: Array<CommandModule<T, U>>): Argv<T>;
 
         // Advanced API
         /** Apply command modules from a directory relative to the module calling this method. */
