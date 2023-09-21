6ed4d662abdd5a461279b193296f989267a01a5c
Dean Pienaar <dean@stitch.money>
2023-09-21 21:34:28 +0200

    Fix import

--- types/mastercard-oauth1-signer/mastercard-oauth1-signer-tests.ts
+++ types/mastercard-oauth1-signer/mastercard-oauth1-signer-tests.ts
@@ -1,4 +1,4 @@
-import OAuth = require("./index");
+import OAuth = require("mastercard-oauth1-signer");
 
 // $ExpectType string
 OAuth.getAuthorizationHeader('https://example.com', 'POST', 'foo', 'consumerKey', 'signingKey');
