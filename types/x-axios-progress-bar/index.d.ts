262eb0a0f244eac40779e6902be4f67b5829745a
Claas Augner <caugner@mozilla.com>
2023-09-22 20:04:02 +0200

    chore: remove myself
    Unfortunately, I no longer work on projects using these packages.

--- types/x-axios-progress-bar/index.d.ts
+++ types/x-axios-progress-bar/index.d.ts
@@ -1,6 +1,5 @@
 // Type definitions for x-axios-progress-bar 1.2
 // Project: https://github.com/mszkb/progress-bar-4-axios#readme
-// Definitions by: Claas Augner <https://github.com/caugner>
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
 
 import { AxiosInstance, AxiosRequestConfig } from "axios";
