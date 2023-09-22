8b3e23c0052853e7eb0d5ece091627ff207c3bba
Buddha Nag <buddhanag12@gmail.com>
2023-09-22 09:20:58 +0530

    test: add tests

--- types/compact-timezone-list/compact-timezone-test.ts
+++ types/compact-timezone-list/compact-timezone-test.ts
@@ -1,16 +1,27 @@
-// named import
-import { minimalTimezoneSet } from 'compact-timezone-list';
-// Default import
-import tz from 'compact-timezone-list';
+import tz, { minimalTimezoneSet } from 'compact-timezone-list';
 
+// $ExpectType Array
 minimalTimezoneSet.map(({ label, offset, tzCode }) => {
+    // $ExpectType string
+    const l = label;
+    // $ExpectType string
+    const o = offset;
+    // $ExpectType string
+    const tz = tzCode;
     console.log({
         label,
         offset,
         tzCode,
     });
 });
+// $ExpectType Array
 tz.map(({ label, offset, tzCode }) => {
+    // $ExpectType string
+    const l = label;
+    // $ExpectType string
+    const o = offset;
+    // $ExpectType string
+    const tz = tzCode;
     console.log({
         label,
         offset,
