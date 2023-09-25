9e2dba2782bc5965b6076c2f8760e51786a514a1
Andrew Kerr <andrewkerr3956@gmail.com>
2023-09-25 00:24:03 -0400

    added more info to index.d.ts

--- types/sightengine/index.d.ts
+++ types/sightengine/index.d.ts
@@ -1,7 +1,9 @@
-// Type definitions for sightengine
+// Type definitions for sightengine 1.3.0
 // Project: https://github.com/Sightengine/client-nodejs
-// Definitions by: Andrew Kerr <https://github.com/andrewkerr3956/>
+// Definitions by: Andrew Kerr <https://github.com/andrewkerr3956>
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
+// Minimum TypeScript Version: 4.5
+
 declare namespace client {
     function check(models: string[]): typeof client;
     function set_url(imageUrl: URL | string): Promise<any>;
