02c6f4b21940227a00906c0ba726a45e078c9eb0
Josh Goldberg <git@joshuakgoldberg.com>
2023-09-25 13:49:42 +0100

    Another import-default suppression

--- types/mjml/index.d.ts
+++ types/mjml/index.d.ts
@@ -5,6 +5,7 @@
 //                 IanEdington        <https://github.com/IanEdington>
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
 
+// eslint-disable-next-line @definitelytyped/no-import-default-of-export-equals
 import mjml2html from "mjml-core";
 
 export = mjml2html;
