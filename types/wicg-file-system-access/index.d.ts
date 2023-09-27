2674b0b7667d746681371a97e779c9e87d1f23c8
Tomas Klohna <original@google.com>
2023-09-27 20:37:19 +0200

    Update wicg-file-system-access types based on latest spec (2023/10)
    Change-Id: I33a0e7e36973e8979e1a1fe4e87e3df84eabd191

--- types/wicg-file-system-access/index.d.ts
+++ types/wicg-file-system-access/index.d.ts
@@ -1,4 +1,4 @@
-// Type definitions for non-npm package File System Access API 2020.09
+// Type definitions for non-npm package File System Access API 2023.10
 // Project: https://github.com/WICG/file-system-access
 // Definitions by: Ingvar Stepanyan <https://github.com/RReverser>
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
@@ -8,10 +8,6 @@ export {};
 
 declare global {
     interface FileSystemHandle {
-        readonly kind: "file" | "directory";
-        readonly name: string;
-
-        isSameEntry(other: FileSystemHandle): Promise<boolean>;
         queryPermission(descriptor?: FileSystemHandlePermissionDescriptor): Promise<PermissionState>;
         requestPermission(descriptor?: FileSystemHandlePermissionDescriptor): Promise<PermissionState>;
 
@@ -25,76 +21,77 @@ declare global {
          */
         readonly isDirectory: boolean;
     }
-    var FileSystemHandle: {
-        prototype: FileSystemHandle;
-        new(): FileSystemHandle;
-    };
-    type FileSystemHandleUnion = FileSystemFileHandle | FileSystemDirectoryHandle;
+
+    type FileExtension = `.${string}`
+    type MIMEType = `${string}/${string}`
 
     interface FilePickerAcceptType {
+        /**
+         * @default ""
+         */
         description?: string | undefined;
-        accept: Record<string, string | string[]>;
+        accept?: Record<MIMEType, FileExtension | FileExtension[]> | undefined;
     }
+
+    /**
+     * https://wicg.github.io/file-system-access/#enumdef-wellknowndirectory
+     */
+    type WellKnownDirectory = "desktop" | "documents" | "downloads" | "music" | "pictures" | "videos"
 
     interface FilePickerOptions {
         types?: FilePickerAcceptType[] | undefined;
+        /**
+         * @default false
+         */
         excludeAcceptAllOption?: boolean | undefined;
+        startIn?: WellKnownDirectory | FileSystemHandle | undefined;
+        id?: string | undefined;
     }
 
     interface OpenFilePickerOptions extends FilePickerOptions {
+        /**
+         * @default false
+         */
         multiple?: boolean | undefined;
     }
 
     interface SaveFilePickerOptions extends FilePickerOptions {
-        suggestedName?: string;
+        suggestedName?: string | undefined;
     }
 
-    // tslint:disable-next-line:no-empty-interface
-    interface DirectoryPickerOptions {}
+    type FileSystemPermissionMode = "read" | "readwrite"
 
-    type FileSystemPermissionMode = "read" | "readwrite";
+    interface DirectoryPickerOptions {
+        id?: string | undefined;
+        startIn?: WellKnownDirectory | FileSystemHandle | undefined;
+        /**
+         * @default "read"
+         */
+        mode?: FileSystemPermissionMode | undefined;
+    }
 
     interface FileSystemPermissionDescriptor extends PermissionDescriptor {
         handle: FileSystemHandle;
+        /**
+         * @default "read"
+         */
         mode?: FileSystemPermissionMode | undefined;
     }
 
     interface FileSystemHandlePermissionDescriptor {
+        /**
+         * @default "read"
+         */
         mode?: FileSystemPermissionMode | undefined;
     }
 
+    // TODO: Implemented natively in TS 5.1, remove
     interface FileSystemCreateWritableOptions {
-        keepExistingData?: boolean | undefined;
+        keepExistingData?: boolean;
     }
-
-    interface FileSystemGetFileOptions {
-        create?: boolean | undefined;
-    }
-
-    interface FileSystemGetDirectoryOptions {
-        create?: boolean | undefined;
-    }
-
-    interface FileSystemRemoveOptions {
-        recursive?: boolean | undefined;
-    }
-
-    // type WriteParams =
-    //     | { type: 'write'; position?: number | undefined; data: BufferSource | Blob | string }
-    //     | { type: 'seek'; position: number }
-    //     | { type: 'truncate'; size: number };
-
-    // type FileSystemWriteChunkType = BufferSource | Blob | string | WriteParams;
-
-    // class FileSystemWritableFileStream extends WritableStream {
-    //     write(data: FileSystemWriteChunkType): Promise<void>;
-    //     seek(position: number): Promise<void>;
-    //     truncate(size: number): Promise<void>;
-    // }
 
     interface FileSystemFileHandle extends FileSystemHandle {
-        readonly kind: "file";
-        getFile(): Promise<File>;
+        // TODO: Implemented natively in TS 5.1, remove
         createWritable(options?: FileSystemCreateWritableOptions): Promise<FileSystemWritableFileStream>;
         /**
          * @deprecated Old property just for Chromium <=85. Use `kind` property in the new API.
@@ -106,17 +103,7 @@ declare global {
         readonly isDirectory: false;
     }
 
-    var FileSystemFileHandle: {
-        prototype: FileSystemFileHandle;
-        new(): FileSystemFileHandle;
-    };
-
     interface FileSystemDirectoryHandle extends FileSystemHandle {
-        readonly kind: "directory";
-        getDirectoryHandle(name: string, options?: FileSystemGetDirectoryOptions): Promise<FileSystemDirectoryHandle>;
-        getFileHandle(name: string, options?: FileSystemGetFileOptions): Promise<FileSystemFileHandle>;
-        removeEntry(name: string, options?: FileSystemRemoveOptions): Promise<void>;
-        resolve(possibleDescendant: FileSystemHandle): Promise<string[] | null>;
         keys(): AsyncIterableIterator<string>;
         values(): AsyncIterableIterator<FileSystemDirectoryHandle | FileSystemFileHandle>;
         entries(): AsyncIterableIterator<[string, FileSystemDirectoryHandle | FileSystemFileHandle]>;
@@ -131,22 +118,11 @@ declare global {
         readonly isDirectory: true;
     }
 
-    var FileSystemDirectoryHandle: {
-        prototype: FileSystemDirectoryHandle;
-        new(): FileSystemDirectoryHandle;
-    };
-
     interface DataTransferItem {
         getAsFileSystemHandle(): Promise<FileSystemHandle | null>;
     }
 
-    interface StorageManager {
-        getDirectory(): Promise<FileSystemDirectoryHandle>;
-    }
-
-    function showOpenFilePicker(
-        options?: OpenFilePickerOptions & { multiple?: false | undefined },
-    ): Promise<[FileSystemFileHandle]>;
+    function showOpenFilePicker(options?: OpenFilePickerOptions & { multiple?: false | undefined }): Promise<[FileSystemFileHandle]>;
     function showOpenFilePicker(options?: OpenFilePickerOptions): Promise<FileSystemFileHandle[]>;
     function showSaveFilePicker(options?: SaveFilePickerOptions): Promise<FileSystemFileHandle>;
     function showDirectoryPicker(options?: DirectoryPickerOptions): Promise<FileSystemDirectoryHandle>;
