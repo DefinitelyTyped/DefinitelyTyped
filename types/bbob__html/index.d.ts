5bc14a5b5fdc0104688d7a2b151df788dec15713
shme-e <143431274+shme-e@users.noreply.github.com>
2023-10-01 16:05:18 +0100

    init bbob core & html

---
+++ types/bbob__html/index.d.ts
@@ -0,0 +1,6 @@
+// Type definitions for @bbob/html 3.0
+// Project: https://github.com/JiLiZART/bbob
+// Definitions by: shmee <https://github.com/shme-e>
+// Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
+
+export function renderNode(node: Node, {  })
