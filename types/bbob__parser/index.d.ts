24af713c17f67e5c9b20b9049578f971271d850e
shme-e <143431274+shme-e@users.noreply.github.com>
2023-10-01 21:00:49 +0100

    init @bbob/parser

---
+++ types/bbob__parser/index.d.ts
@@ -0,0 +1,5 @@
+// Type definitions for @bbob/parser 3.0
+// Project: https://github.com/JiLiZART/bbob
+// Definitions by: shme-e <https://github.com/shme-e>
+// Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
+
