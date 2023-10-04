570de940196213ea63e8544863a6076db94db75d
Sebastian Silbermann <sebastian.silbermann@klarna.com>
2023-10-04 15:58:39 +0200

    [react-dom] Add types for experimental_useFormState (#66726)
    * [react-dom] Add types for `experimental_useFormState`
    
    * [react-dom] Add experimental_formState hydration option
    
    * it's a reducer
    
    * ReactFormState is opaque
    
    * Sync reducer is fine here too now
    
    * Revert "Sync reducer is fine here too now"
    
    This reverts commit 520c3916efb5504bf1807cd638e59b356dd520b8.
    
    For now it still errors

--- types/react-dom/test/experimental-tests.tsx
+++ types/react-dom/test/experimental-tests.tsx
@@ -1,7 +1,9 @@
 import ReactDOM = require("react-dom");
+import ReactDOMClient = require("react-dom/client");
 import "react/experimental";
 import "react-dom/experimental";
 
+const useFormState = ReactDOM.experimental_useFormState;
 const useFormStatus = ReactDOM.experimental_useFormStatus;
 
 function Status() {
@@ -19,4 +21,57 @@ function Status() {
             </div>
         );
     }
+}
+
+function formTest() {
+    function Page1() {
+        async function action(state: number) {
+            return state + 1;
+        }
+
+        const [state, dispatch] = useFormState(action, 1);
+        return (
+            <button
+                onClick={() => {
+                    dispatch();
+                }}
+            >
+                count: {state}
+            </button>
+        );
+    }
+
+    function Page2() {
+        async function action(state: number) {
+            return state + 1;
+        }
+
+        const [state, dispatch] = useFormState(action, 1, '/permalink');
+        return (
+            <form action={dispatch}>
+                <span>Count: {state}</span>
+                <input type="text" name="incrementAmount" defaultValue="5" />
+            </form>
+        );
+    }
+
+    function Page4() {
+        async function action(state: number, type: 'increment' | 'decrement') {
+            return state + (type === 'increment' ? 1 : -1);
+        }
+
+        const [state, dispatch] = useFormState(action, 1, '/permalink');
+        return (
+            <button
+                onClick={() => {
+                    dispatch('decrement');
+                }}
+            >
+                count: {state}
+            </button>
+        );
+    }
+
+    const formState = [1, '', '', 0] as unknown as ReactDOMClient.ReactFormState;
+    ReactDOMClient.hydrateRoot(document.body, <Page1 />, { experimental_formState: formState });
 }
