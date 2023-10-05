e2a7e0b076b54e2d2b5897049e7f4fdd05a60364
Mark Klein <mark.klein@ericsson.com>
2023-10-05 11:14:34 -0400

    [dns2] Add support for IPv6 UDP DNS servers
    To instantiate a UDP IPv6 DNS server, it is necessary to pass
    an object with type set as 'udp6' as a constructor argument.
    This will cause dns2 to open the right kind of socket.
    
    See https://github.com/song940/node-dns/blob/master/server/udp.js
    
    This commit updates the UDP objects to add this facility.
    
    The address has been made optional in the UDP listen signature
    to allow the socket to choose the right all interface address.

--- types/dns2/dns2-tests.ts
+++ types/dns2/dns2-tests.ts
@@ -60,6 +60,43 @@ server.on("request", (request, response, rinfo) => {
 
 server.listen({ udp: 5333 });
 
+const serverByType = DNS.createServer({
+    udp: { type: 'udp4' },
+    handle: (request, send, rinfo) => {
+        const response = Packet.createResponseFromRequest(request);
+        const [question] = request.questions;
+        const { name } = question;
+        response.answers.push({
+            name,
+            type: Packet.TYPE.A,
+            class: Packet.CLASS.IN,
+            ttl: 300,
+            address: "8.8.8.8",
+        });
+        response.answers.push({
+            name,
+            type: Packet.TYPE.CNAME,
+            class: Packet.CLASS.IN,
+            ttl: 300,
+            domain: "another-name.example.com",
+        });
+        response.answers.push({
+            name,
+            type: Packet.TYPE.TXT,
+            class: Packet.CLASS.IN,
+            ttl: 60,
+            data: "dnslink=/ipfs/123abc",
+        });
+        send(response);
+    },
+});
+
+serverByType.on("request", (request, response, rinfo) => {
+    console.log(request.header.id, request.questions[0]);
+});
+
+serverByType.listen({ udp: 5344 });
+
 const udpServer = new DNS.UDPServer((request, send, rinfo) => {
     const response = Packet.createResponseFromRequest(request);
     send(response);
@@ -73,3 +110,11 @@ const tcpServer = DNS.createTCPServer((request, send, rinfo) => {
 });
 
 tcpServer.listen(5454, "127.0.0.1");
+
+const udpServerByType = new DNS.UDPServer({type: 'udp4'})
+udpServerByType.on("request", (request, send, rinfo) => {
+    const response = Packet.createResponseFromRequest(request);
+    send(response);
+});
+
+udpServer.listen(5555, "127.0.0.1");
