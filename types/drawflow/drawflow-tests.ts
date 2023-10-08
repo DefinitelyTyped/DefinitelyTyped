6c3feac32b78ef3e35af7e23c0e5ec3d27e4e5d9
Joris Zwart <hallo@joriszwart.nl>
2023-10-08 15:48:24 +0200

    format tests for drawflow addNode

--- types/drawflow/drawflow-tests.ts
+++ types/drawflow/drawflow-tests.ts
@@ -4,4 +4,4 @@ const element = new HTMLElement();
 new Drawflow(element);
 
 // $ExpectType string | number
-new Drawflow(element).addNode('test', 1, 2, 50, 75, 'class', null, 'html', false);
+new Drawflow(element).addNode("test", 1, 2, 50, 75, "class", null, "html", false);
