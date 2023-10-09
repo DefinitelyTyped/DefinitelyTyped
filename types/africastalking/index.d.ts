7217488221ee0589a57474bb7cf033250fd3668f
James Kaguru Kihuha <47499826+James-Kaguru@users.noreply.github.com>
2023-10-09 10:39:12 +0300

    added types for africatlking package

---
+++ types/africastalking/index.d.ts
@@ -0,0 +1,67 @@
+// Definitions by: James Kaguru <https://github.com/James-Kaguru>
+interface SMSMessageData {
+    Message: string,
+    Recipients: {
+        "statusCode": number,
+        "number": string,
+        "status": "fulfilled" | "failed",
+        "cost": string,
+        "messageId": string
+    }
+}
+
+interface SMSOptions {
+    to:string | string[],
+    from:string,
+    message:string,
+}
+
+interface SMS {
+    /**
+     * This is used to send SMSs to your clients/recipients.
+     *
+     * @param to The recipients' phone number.  e.g +2547********. Note it can either be a string or an array
+     * @param from Your registered short code or alphanumeric, e.g. AFRICASTKNG.
+     * @message The message to be sent.
+     */
+    send: (options: SMSOptions) => Promise<SMSMessageData>,
+    /**
+     * This is used to send SMSs to your clients/recipients.
+     *
+     * @param to The recipients' phone number.  e.g +2547********. Note it can either be a string or an array
+     * @param from Your registered short code or alphanumeric, e.g. AFRICASTKNG.
+     * @message The message to be sent.
+     */
+    sendBulk: (options: SMSOptions) => Promise<SMSMessageData>,
+    createSubscription: (options:{
+                shortCode: string,
+                keyword: string,
+                phoneNumber: string,
+                checkoutToken: string
+    }) => Promise<{
+        "description": "Success" | "Failed",
+        "token": string
+    }>
+
+}
+
+interface TOKEN {
+    generateAuthToken: () => Promise<{
+        "description": "Success" | "Failed",
+        "token": string
+    }>
+    createCheckoutToken: (phoneNumber:string)=> Promise<{
+    description: "Success" | "Failed",
+    token: string
+}>
+}
+
+interface AfricasTalking{
+    SMS: SMS
+    TOKEN: TOKEN
+}
+
+
+declare module 'africastalking' {
+    export default function (options:{username: string,apiKey:string}): AfricasTalking
+}
