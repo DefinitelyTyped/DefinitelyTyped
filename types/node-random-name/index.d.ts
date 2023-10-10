42531f4b908f5100bf6ea3fb740e103fe6567c24
Ankan Bhattacharya <ankanbhattacharya89@gmail.com>
2023-10-11 00:56:37 +0530

    feat: :sparkles: add types for node-random-name

--- types/node-random-name/index.d.ts
+++ types/node-random-name/index.d.ts
@@ -3,27 +3,18 @@
 // Definitions by: Ankan Bhattacharya <https://github.com/Ankan002>
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
 
-interface NodeRandomNameArgs {
-    first?: boolean;
-    last?: boolean;
-    gender?: "male" | "female";
-    seed?: string;
-    random?: () => number;
-    male?: boolean;
-    female?: boolean;
-}
 
-declare function random_name(args?: NodeRandomNameArgs): string;
+declare function random_name(args?: random_name.NodeRandomNameArgs): string;
 
 declare namespace random_name {
     interface NodeRandomNameArgs {
-        first?: boolean;
-        last?: boolean;
-        gender?: "male" | "female";
-        seed?: string;
-        random?: () => number;
-        male?: boolean;
-        female?: boolean;
+        first?: boolean | undefined;
+        last?: boolean | undefined;
+        gender?: "male" | "female" | undefined;
+        seed?: string | undefined;
+        random?: () => number | undefined;
+        male?: boolean | undefined;
+        female?: boolean | undefined;
     }
 }
 
