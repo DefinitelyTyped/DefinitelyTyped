d38f1d6df4af13551cea0b55915fb9d6dfaebb7c
Ankan Bhattacharya <ankanbhattacharya89@gmail.com>
2023-10-11 22:39:52 +0530

    feat: :sparkles: add types for chartjs-color-string

--- types/chartjs-color-string/index.d.ts
+++ types/chartjs-color-string/index.d.ts
@@ -3,27 +3,19 @@
 // Definitions by: Ankan Bhattacharya <https://github.com/Ankan002>
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
 
-declare const colorString: colorString.ColorString;
+export function getRgba(colorString?: string): number[] | undefined;
+export function getHsla(colorString?: string): number[] | undefined;
+export function getHwb(colorString?: string): number[] | undefined;
+export function getRgb(colorString?: string): number[] | undefined;
+export function getHsl(colorString?: string): number[] | undefined;
+export function getAlpha(colorString?: string): number | undefined;
 
-declare namespace colorString {
-    interface ColorString {
-        getRgba(colorString?: string): number[] | undefined;
-        getHsla(colorString?: string): number[] | undefined;
-        getHwb(colorString?: string): number[] | undefined;
-        getRgb(colorString?: string): number[] | undefined;
-        getHsl(colorString?: string): number[] | undefined;
-        getAlpha(colorString?: string): number | undefined;
-
-        hexString(rgba: number[], a?: number): string;
-        rgbString(rgba: number[], alpha?: number): string;
-        rgbaString(rgba: number[], alpha?: number): string;
-        percentString(rgba: number[], alpha?: number): string;
-        percentaString(rgba: number[], alpha?: number): string;
-        hslString(hsla: number[], alpha?: number): string;
-        hslaString(hsla: number[], alpha?: number): string;
-        hwbString(hwb: number[], alpha?: number): string;
-        keyword(rgb: number[]): string;
-    }
-}
-
-export = colorString;
+export function hexString(rgba: number[], a?: number): string;
+export function rgbString(rgba: number[], alpha?: number): string;
+export function rgbaString(rgba: number[], alpha?: number): string;
+export function percentString(rgba: number[], alpha?: number): string;
+export function percentaString(rgba: number[], alpha?: number): string;
+export function hslString(hsla: number[], alpha?: number): string;
+export function hslaString(hsla: number[], alpha?: number): string;
+export function hwbString(hwb: number[], alpha?: number): string;
+export function keyword(rgb: number[]): string;
