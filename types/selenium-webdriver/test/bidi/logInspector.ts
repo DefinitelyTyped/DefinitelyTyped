fc9b9e6ab65cc226852beddc360e7582c9da40cc
Todd Tarsi <tarsitodd@gmail.com>
2023-10-13 00:25:09 -0500

    Update logInspector.ts
    test fixes

--- types/selenium-webdriver/test/bidi/logInspector.ts
+++ types/selenium-webdriver/test/bidi/logInspector.ts
@@ -6,33 +6,20 @@ const testText = 'hello!';
 async function TestLogInspector() {
     const driver: webdriver.WebDriver = new webdriver.Builder().build();
     const logInspector = await LogInspector(driver);
-    let error = '';
-    let complete = false;
-    const timeoutError = setTimeout(() => {
-        error = `Timed out waiting for log ${testText}`
-        complete = true
-    }, 1000)
-    logInspector.onLog(async ({text}) => {
-        if (text === testText) {
-            clearTimeout(timeoutError);
-            complete = true
-        }
-    })
-
     await driver.executeScript(`console.log('${testText}')`);
     await checkForLog(testText, 1000);
     await logInspector.close();
     await driver.close();
     
-    async function checkForLog(expectedText: string, timeout: number) {
-        const findLog = new Promise((resolve) => {
+    async function checkForLog(expectedText: string, timeout: number): Promise<void> {
+        const findLog = new Promise<void>((resolve) => {
             logInspector.onLog(({ text }) => {
                 if (text === expectedText) {
                     resolve();
                 }
             });
         });
-        const timeOut = new Promise((_, reject) => {
+        const timeOut = new Promise<void>((_, reject) => {
             setTimeout(() => { reject(new Error("Search timed out")); }, timeout);
         });
         return Promise.race([findLog, timeOut]);
