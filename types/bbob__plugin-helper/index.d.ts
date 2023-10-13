e6eaa37f8aab65559a7e0f891c252dde6685ef2f
shme-e <143431274+shme-e@users.noreply.github.com>
2023-10-13 21:07:04 +0100

    remove boilerplate from plugin-helper

--- types/bbob__plugin-helper/index.d.ts
+++ types/bbob__plugin-helper/index.d.ts
@@ -3,35 +3,6 @@
 // Definitions by: shmee <https://github.com/shme-e>
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
 
-// /*~ If this module has methods, declare them as functions like so.
-//  */
-// export function myMethod(a: string): string;
-// export function myOtherMethod(a: number): number;
-
-// /*~ You can declare types that are available via importing the module */
-// export interface someType {
-//     name: string;
-//     length: number;
-//     extras?: string[];
-// }
-
-// /*~ You can declare properties of the module using const, let, or var */
-// export const myField: number;
-
-// /*~ If there are types, properties, or methods inside dotted names
-//  *~ of the module, declare them inside a 'namespace'.
-//  */
-// export namespace subProp {
-//     /*~ For example, given this definition, someone could write:
-//      *~   import { subProp } from 'yourModule';
-//      *~   subProp.foo();
-//      *~ or
-//      *~   import * as yourMod from 'yourModule';
-//      *~   yourMod.subProp.foo();
-//      */
-//     function foo(): void;
-// }
-
 export * from "./char";
 export * from "./helpers";
 export * from "./TagNode";
