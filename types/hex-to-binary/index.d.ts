d75161e23877daded5f3df24121f8811c13aba12
geunheejung <geunhee0212@gmail.com>
2023-10-14 12:38:41 +0900

    fix: Author name

--- types/hex-to-binary/index.d.ts
+++ types/hex-to-binary/index.d.ts
@@ -1,6 +1,6 @@
 // Type definitions for hex-to-binary 1.0
 // Project: https://github.com/AVVS/hex-to-binary
-// Definitions by: My Self <https://github.com/geunheejung>
+// Definitions by: GeunheeJung <https://github.com/geunheejung>
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
 
 export = hexToBinary;
