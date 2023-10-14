efd638bf6cf7f592201b93f8d141da41cb11bd92
geunheejung <geunhee0212@gmail.com>
2023-10-14 11:53:18 +0900

    add `hex-to-binary` by @geunheejung

---
+++ types/hex-to-binary/hex-to-binary-tests.ts
@@ -0,0 +1,4 @@
+import hexToBinary = require("hex-to-binary");
+
+// $ExpectType string
+hexToBinary("AAFF00");
---
+++ types/hex-to-binary/index.d.ts
@@ -0,0 +1,7 @@
+// Type definitions for hex-to-binary 1.0
+// Project: github.com/AVVS/hex-to-binary
+// Definitions by: avvs <https://github.com/AVVS>
+// Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
+
+declare function hexToBinary(hexString: string): string;
+export = hexToBinary;
