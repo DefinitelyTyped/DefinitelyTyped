936421f9a292f08c0527831037df7fc61517e300
Robot-Inventor <30.keita@gmail.com>
2023-10-18 02:25:22 +0900

    add addons-linter types

---
+++ types/addons-linter/index.d.ts
@@ -0,0 +1,148 @@
+// Type definitions for addons-linter 6.15
+// Project: https://github.com/mozilla/addons-linter#readme
+// Definitions by: Robot-Inventor <https://github.com/Robot-Inventor>
+// Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
+
+export interface Options {
+    config: {
+        /**
+         * This mimics the first command line argument from yargs, which should be the directory to the extension.
+         */
+        _: string[];
+
+        /**
+         * The log-level to generate.
+         */
+        logLevel: "fatal" | "error" | "warn" | "info" | "debug" | "trace";
+
+        /**
+         * Treat warnings as errors.
+         */
+        warningsAsErrors?: boolean;
+
+        /**
+         * The type of output to generate.
+         */
+        output?: "json" | "text" | "none";
+
+        /**
+         * Output only metadata as JSON.
+         */
+        metadata?: boolean;
+
+        /**
+         * Prettify JSON output.
+         */
+        pretty?: boolean;
+
+        /**
+         * Show stacktraces when errors are thrown.
+         */
+        stack?: boolean;
+
+        /**
+         * Disable colorful shell output.
+         */
+        boring?: boolean;
+
+        /**
+         * Treat the input file (or directory) as a privileged extension.
+         */
+        privileged?: boolean;
+
+        /**
+         * Disable messages related to hosting on addons.mozilla.org.
+         */
+        selfHosted?: boolean;
+
+        /**
+         * Enable MV3 background service worker support.
+         */
+        enableBackgroundServiceWorker?: boolean;
+
+        /**
+         * Set a custom minimum allowed value for the manifest_version property
+         */
+        minManifestVersion?: number;
+
+        /**
+         * Set a custom maximum allowed value for the manifest_version property
+         */
+        maxManifestVersion?: number;
+
+        /**
+         * Disable list of comma separated eslint rules.
+         */
+        disableLinterRules?: string;
+
+        /**
+         * Disable the auto-close feature when linting XPI files.
+         */
+        disableXpiAutoclose?: boolean;
+
+        /**
+         * Exclude files
+         */
+        shouldScanFile?: (fileOrDirName: string, isDir: boolean) => boolean;
+
+        /**
+         * Scan a selected file.
+         */
+        scanFile?: string[];
+    };
+
+    /**
+     * This prevent the linter to exit the nodejs application.
+     */
+    runAsBinary?: boolean;
+}
+
+export interface LinterResults {
+    count: number;
+
+    summary: {
+        errors: number;
+        notices: number;
+        warnings: number;
+    };
+
+    metadata: Record<string, unknown>;
+
+    scanFile: string[];
+
+    errors: Array<{
+        _type: "error";
+        code: string;
+        message: string;
+        description: string;
+        file: string;
+    }>;
+
+    notices: Array<{
+        _type: "notice";
+        code: string;
+        message: string;
+        description: string;
+        file: string;
+    }>;
+
+    warnings: Array<{
+        _type: "warning";
+        code: string;
+        message: string;
+        description: string;
+        file: string;
+    }>;
+}
+
+export interface Linter {
+    run: () => Promise<LinterResults>;
+}
+
+export function createInstance(options: Options): Linter;
+
+declare const _default: {
+    createInstance: typeof createInstance;
+};
+
+export default _default;
