65c4c1585f5333c9346f401e8a545d7dc2bda730
rodrigo <falsepopsky@protonmail.com>
2023-10-18 23:52:33 +0000

    chore: remove styled-components

--- types/styled-components/macro.d.ts
+++
@@ -1,7 +0,0 @@
-export { default } from ".";
-export * from ".";
-
-/**
- * Recommended: also `import {} from 'styled-components/cssprop'`,
- * or augment react's `Attribute` interface with your own version.
- */
