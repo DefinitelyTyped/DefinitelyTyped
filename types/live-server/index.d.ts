4628e26f4377a4663b535d5c66602a89cc6b2d77
EdixonAlberto <edixonalbertto@gmail.com>
2023-10-19 22:17:55 +0000

    Add Partial in interface LiveServerParams

--- types/live-server/index.d.ts
+++ types/live-server/index.d.ts
@@ -11,30 +11,30 @@ export type NextFunction = (err?: any) => void;
 /**
  * The live-server start params.
  */
-export interface LiveServerParams {
+export interface LiveServerParams extends Partial<{
     /** Set the server port. Defaults to 8080. */
-    port?: number | undefined;
+    port: number;
     /**  Set the address to bind to. Defaults to 0.0.0.0 or process.env.IP. */
-    host?: string | undefined;
+    host: string;
     /** Set root directory that's being served. Defaults to cwd. */
-    root?: string | undefined;
+    root: string;
     /** When false, it won't load your browser by default. */
-    open?: boolean | undefined;
+    open: boolean;
     /** Comma-separated string for paths to ignore. */
-    ignore?: string | undefined;
+    ignore: string;
     /** When set, serve this file (server root relative) for every 404 (useful for single-page applications). */
-    file?: string | undefined;
+    file: string;
     /** Waits for all changes, before reloading. Defaults to 0 sec. */
-    wait?: number | undefined;
+    wait: number;
     /** Enable CORS (Cross-Origin Resource Sharing). Defaults to false. */
-    cors?: boolean | undefined
+    cors: boolean;
     /** Mount a directory to a route. */
-    mount?: string[][] | undefined;
+    mount: string[][];
     /** 0 = errors only, 1 = some, 2 = lots */
-    logLevel?: 0 | 1 | 2 | undefined;
+    logLevel: 0 | 1 | 2;
     /** Takes an array of Connect-compatible middleware that are injected into the server middleware stack. */
-    middleware?: Array<(req: Request, res: Response, next: NextFunction) => void> | undefined;
-}
+    middleware: Array<(req: Request, res: Response, next: NextFunction) => void>;
+}> { }
 
 /**
  * Start live-server.
