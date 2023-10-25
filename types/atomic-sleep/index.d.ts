d2bae226dff5ad15460c2c4b5cd5e80b709759a5
Khiet Tam Nguyen <nktnet999@gmail.com>
2023-10-26 03:34:01 +1100

    change atomicSleep => sleep in index.d.ts

--- types/atomic-sleep/index.d.ts
+++ types/atomic-sleep/index.d.ts
@@ -1,5 +1,5 @@
 // Project: https://github.com/davidmarkclements/atomic-sleep
 // Definitions by: Khiet Tam Nguyen <https://github.com/nktnet1>
 
-declare function atomicSleep(ms: number): void;
-export = atomicSleep;
+declare function sleep(ms: number): void;
+export = sleep;
