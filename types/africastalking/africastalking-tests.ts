26f75577542e960925ad7a5cf3fd379d2f3f63a9
James Kaguru Kihuha <47499826+James-Kaguru@users.noreply.github.com>
2023-10-26 15:02:41 +0300

    formatted the file with dprint

--- types/africastalking/africastalking-tests.ts
+++ types/africastalking/africastalking-tests.ts
@@ -1,39 +1,38 @@
-import AfricasTalking = require('africastalking');
+import AfricasTalking = require("africastalking");
 
 const africastalking = AfricasTalking({
-  apiKey: 'apiKey',
-  username: 'username',
+    apiKey: "apiKey",
+    username: "username",
 });
 
 const SMS = async () => {
-  const token = africastalking.TOKEN;
-  const sms = africastalking.SMS;
+    const token = africastalking.TOKEN;
+    const sms = africastalking.SMS;
 
-  const sendSmsResult = await africastalking.SMS.send({
-    to: 'to',
-    message: 'message',
-    from: 'from',
-  });
+    const sendSmsResult = await africastalking.SMS.send({
+        to: "to",
+        message: "message",
+        from: "from",
+    });
 
-  const checkoutTokenResponse = await token.createCheckoutToken('phoneNumber');
+    const checkoutTokenResponse = await token.createCheckoutToken("phoneNumber");
 
-  const options = {
-    // Set your premium product shortCode and keyword
-    shortCode: 'shortCode',
-    keyword: 'keyword',
-    phoneNumber: '+phoneNumber',
-    checkoutToken: checkoutTokenResponse.token,
-  };
+    const options = {
+        // Set your premium product shortCode and keyword
+        shortCode: "shortCode",
+        keyword: "keyword",
+        phoneNumber: "+phoneNumber",
+        checkoutToken: checkoutTokenResponse.token,
+    };
 
-  const res = await sms.createSubscription(options);
+    const res = await sms.createSubscription(options);
 };
 
 const VOICE = async () => {
-  const voice = africastalking.VOICE;
+    const voice = africastalking.VOICE;
 
-  const result  = await voice.call({
-      callFrom: "+254711XXXYYY",
-      callTo: ['+254711XXXZZZ', '+254711XXXPPP']
-  });
-
+    const result = await voice.call({
+        callFrom: "+254711XXXYYY",
+        callTo: ["+254711XXXZZZ", "+254711XXXPPP"],
+    });
 };
