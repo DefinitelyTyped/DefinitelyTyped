3104a84c302d2ac979ee3f7633496d382921cabc
borajimin <jimin.ha@cognite.com>
2023-10-30 13:29:18 +0100

    fix: format files

--- types/chamaeleonidae__chmln/chamaeleonidae__chmln-tests.ts
+++ types/chamaeleonidae__chmln/chamaeleonidae__chmln-tests.ts
@@ -1,17 +1,17 @@
-import * as defaultChameleon from '@chamaeleonidae/chmln';
+import * as defaultChameleon from "@chamaeleonidae/chmln";
 
 export class Chameleon {
-  private readonly chameleon: any;
+    private readonly chameleon: any;
 
-  constructor() {
-    this.chameleon = defaultChameleon;
-  }
+    constructor() {
+        this.chameleon = defaultChameleon;
+    }
 
-  identify(id: string, options: defaultChameleon.ChameleonIdentifyOptions) {
-    this.chameleon.identify(id, options);
-  }
+    identify(id: string, options: defaultChameleon.ChameleonIdentifyOptions) {
+        this.chameleon.identify(id, options);
+    }
 
-  track(event: string) {
-    this.chameleon.track(event);
-  }
+    track(event: string) {
+        this.chameleon.track(event);
+    }
 }
