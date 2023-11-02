e7a3fd86cd7f28e30088b37b2bdff274b8eeb799
Sikriti Dakua <destrodev123@gmail.com>
2023-11-03 00:07:06 +0530

    fix: formatting `svg-path-contours`

--- types/svg-path-contours/index.d.ts
+++ types/svg-path-contours/index.d.ts
@@ -1,9 +1,8 @@
-
 type MoveCommand = ["m" | "M", number, number];
 type LineCommand = ["l" | "L", number, number];
 type HorizontalCommand = ["h" | "H", number];
 type VerticalCommand = ["v" | "V", number];
-type ClosePathCommand = ["z" | "Z"] ;
+type ClosePathCommand = ["z" | "Z"];
 type BezierCurveCommand = ["c" | "C", number, number, number, number, number, number];
 type FollowingBezierCurveCommand = ["s" | "S", number, number, number, number];
 type QuadraticCurveCommand = ["q" | "Q", number, number, number, number];
@@ -21,5 +20,5 @@ type Command =
     | FollowingQuadraticCurveCommand
     | ArcCommand;
 
-declare function contours(path: Command[], scale: number): Command[]
+declare function contours(path: Command[], scale: number): Command[];
 export = contours;
