982672f1ae36af32f5042cd3a495852594c09495
Sikriti Dakua <destrodev123@gmail.com>
2023-11-03 22:52:04 +0530

    remove `.eslintrc.json`

--- types/svg-path-contours/index.d.ts
+++ types/svg-path-contours/index.d.ts
@@ -2,6 +2,7 @@ type MoveCommand = [code: "m" | "M", x: number, y: number];
 type LineCommand = [code: "l" | "L", x: number, y: number];
 type HorizontalCommand = [code: "h" | "H", x: number];
 type VerticalCommand = [code: "v" | "V", y: number];
+// eslint-disable-next-line @definitelytyped/no-single-element-tuple-type
 type ClosePathCommand = [code: "z" | "Z"];
 type BezierCurveCommand = [code: "c" | "C", x1: number, y1: number, x2: number, y2: number, x: number, y: number];
 type FollowingBezierCurveCommand = [code: "s" | "S", x2: number, y2: number, x: number, y: number];
