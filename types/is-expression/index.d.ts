9c0ecf1b6caa9b8cb0c7b3ed889c9f99c1b9c651
Tristan F <26509014+LeoDog896@users.noreply.github.com>
2023-11-04 23:02:36 -0400

    fix(is-expression): jsdoc formatting

--- types/is-expression/index.d.ts
+++ types/is-expression/index.d.ts
@@ -4,7 +4,7 @@ type Options = acorn.Options & {
     /**
      * Throw an error if the string is not an expression.
      * The error can be an Acorn error, with location information in `err.loc` and `err.pos`. Defaults to `false`.
-     **/
+     */
     throw: boolean;
     /**
      * Use strict mode when trying to parse the string. Defaults to `false`.
