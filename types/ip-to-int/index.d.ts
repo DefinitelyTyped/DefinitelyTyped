cda3aa239a419b541d97132b9c1f730c356a21f6
yutak23 <yutapilafu@gmail.com>
2023-11-06 20:03:14 +0900

    style: run dprint fmt

--- types/ip-to-int/index.d.ts
+++ types/ip-to-int/index.d.ts
@@ -1,6 +1,6 @@
 export = ipInt;
 
 declare function ipInt(value: string): {
-    toInt:() => number,
-    toIP:() => string,
+    toInt: () => number;
+    toIP: () => string;
 };
--- types/ip-to-int/ip-to-int-tests.ts
+++ types/ip-to-int/ip-to-int-tests.ts
@@ -1,4 +1,4 @@
 import ipInt = require("ip-to-int");
 
-ipInt('89.150.79.17').toInt(); // $ExpectType number
-ipInt('1503022865').toIP(); // $ExpectType string
+ipInt("89.150.79.17").toInt(); // $ExpectType number
+ipInt("1503022865").toIP(); // $ExpectType string
