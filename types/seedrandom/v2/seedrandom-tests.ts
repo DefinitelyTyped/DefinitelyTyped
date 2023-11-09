8e5c087d2550ed1c1f7c140b35821a0184d75346
Daniel Kaplan <daniel.kaplan@SleepEasySoftware.com>
2023-11-08 21:02:39 -0800

    formatting

--- types/seedrandom/v2/seedrandom-tests.ts
+++ types/seedrandom/v2/seedrandom-tests.ts
@@ -28,15 +28,21 @@ var emptyMathConstructor = new Math.seedrandom();
 var seedMathConstructor = new Math.seedrandom("seed");
 var seedCallbackMathConstructor = new Math.seedrandom("seed", undefined, 5);
 var seedEmptyOptionsCallbackMathConstructor = new Math.seedrandom("seed", {}, 5);
-var seedUndefinedOptionsCallbackMathConstructor = new Math.seedrandom("seed",
-    { entropy: undefined, global: undefined, pass: undefined, state: undefined },
-    5,
-);
-var seedAllOptionsCallbackMathConstructor = new Math.seedrandom("seed",
-    { entropy: true, global: true, pass: () => { return rng2 }, state: {} },
-    5,
-);
+var seedUndefinedOptionsCallbackMathConstructor = new Math.seedrandom("seed", {
+    entropy: undefined,
+    global: undefined,
+    pass: undefined,
+    state: undefined,
+}, 5);
+var seedAllOptionsCallbackMathConstructor = new Math.seedrandom("seed", {
+    entropy: true,
+    global: true,
+    pass: () => {
+        return rng2;
+    },
+    state: {},
+}, 5);
 
 n = seedrandom("secret-seed", { state: true })();
-n = (new (seedrandom("secret-seed", { state: true })))();
-new new new new new new new new new new replica;
+n = (new (seedrandom("secret-seed", { state: true }))())();
+new new new new new new new new new new replica()()()()()()()()()();
