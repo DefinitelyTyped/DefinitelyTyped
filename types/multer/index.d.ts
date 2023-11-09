cd3863cedcfeefd6de5b397bfcff56530fdaf796
Ali Asad <aliasad6521@gmail.com>
2023-11-09 23:17:56 +0500

    fix(multer): add express namespace in multer's export

--- types/multer/index.d.ts
+++ types/multer/index.d.ts
@@ -70,6 +70,56 @@ declare global {
 declare function multer(options?: multer.Options): multer.Multer;
 
 declare namespace multer {
+    namespace Express {
+        namespace Multer {
+            /** Object containing file metadata and access information. */
+            interface File {
+                /** Name of the form field associated with this file. */
+                fieldname: string;
+                /** Name of the file on the uploader's computer. */
+                originalname: string;
+                /**
+                 * Value of the `Content-Transfer-Encoding` header for this file.
+                 * @deprecated since July 2015
+                 * @see RFC 7578, Section 4.7
+                 */
+                encoding: string;
+                /** Value of the `Content-Type` header for this file. */
+                mimetype: string;
+                /** Size of the file in bytes. */
+                size: number;
+                /**
+                 * A readable stream of this file. Only available to the `_handleFile`
+                 * callback for custom `StorageEngine`s.
+                 */
+                stream: Readable;
+                /** `DiskStorage` only: Directory to which this file has been uploaded. */
+                destination: string;
+                /** `DiskStorage` only: Name of this file within `destination`. */
+                filename: string;
+                /** `DiskStorage` only: Full path to the uploaded file. */
+                path: string;
+                /** `MemoryStorage` only: A Buffer containing the entire file. */
+                buffer: Buffer;
+            }
+        }
+
+        interface Request {
+            /** `Multer.File` object populated by `single()` middleware. */
+            file?: Multer.File | undefined;
+            /**
+             * Array or dictionary of `Multer.File` object populated by `array()`,
+             * `fields()`, and `any()` middleware.
+             */
+            files?:
+                | {
+                    [fieldname: string]: Multer.File[];
+                }
+                | Multer.File[]
+                | undefined;
+        }
+    }
+
     /**
      * @see {@link https://github.com/expressjs/multer#api}
      */
