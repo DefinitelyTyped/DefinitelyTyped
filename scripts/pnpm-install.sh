d8a4e606b33b8c1ebd05f8924156252729b151fb
Jake Bailey <5341706+jakebailey@users.noreply.github.com>
2023-11-09 16:33:59 -0800

    Work around pnpm bug for incomplete filtered installs

---
+++ scripts/pnpm-install.sh
@@ -0,0 +1,38 @@
+#!/bin/bash -e
+
+if git diff --diff-filter=DR --name-only HEAD^1 | grep -q 'package.json'; then
+    pnpm install
+    exit 0
+fi
+
+# Workaround for https://github.com/pnpm/pnpm/issues/7283
+# After installing everything the first time, reread the graph and see if anything was missing..
+# If something was missing, we install that again and repeat until nothing is missing.
+
+FILTERS=("--filter" "." "--filter" "...[HEAD^1]...")
+PASS=0
+
+while [ ${#TO_INSTALL[@]} -gt 0 ]; do
+    if [ $PASS -gt 0 ]; then
+        echo "Pass $PASS"
+    fi
+    PASS=$((PASS + 1))
+
+    echo pnpm install "${FILTERS[@]}"
+    pnpm install "${FILTERS[@]}"
+
+    OLD_FILTERS=("${FILTERS[@]}")
+    FILTERS=()
+
+    for i in $(pnpm ls --depth Infinity --parseable "${OLD_FILTERS[@]}" | grep -v node_modules | awk NF | sort -u); do
+        i=${i#*$PWD/}
+
+        if [ ! -d "$i/node_modules" ]; then
+            continue
+        fi
+
+        echo "$i is a transitive dep that resolves to the workspace and must be manually installed"
+        FILTERS+=("--filter")
+        FILTERS+=("...{./$i}...")
+    done
+done
