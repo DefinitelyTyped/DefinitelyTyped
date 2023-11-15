0c85ff275ec64ac4990798a536e4fa18435a3f76
Andréas Hanss <ScreamZ@users.noreply.github.com>
2023-11-15 18:37:03 +0100

    Update types/etherport-client/index.d.ts
    Co-authored-by: Adam Thompson-Sharpe <adamthompsonsharpe@gmail.com>

--- types/etherport-client/index.d.ts
+++ types/etherport-client/index.d.ts
@@ -6,5 +6,10 @@ export interface EtherPortClientConstructorArgs {
 
 // tslint:disable-next-line:no-unnecessary-class
 export class EtherPortClient {
+    path: string;
+    name: string;
+    host: string;
+    port: number;
+
     constructor(options: EtherPortClientConstructorArgs);
 }
