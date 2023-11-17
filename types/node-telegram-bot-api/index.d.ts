965cd6dcf4c45668a88f7933390ded0347dd9841
shadowplay1 <shadowowplay483@gmail.com>
2023-11-18 02:32:25 +0300

    fix(restrictChatMember): removing restrictions not working

--- types/node-telegram-bot-api/index.d.ts
+++ types/node-telegram-bot-api/index.d.ts
@@ -301,9 +301,8 @@ declare namespace TelegramBot {
         caption_entities?: MessageEntity[] | undefined;
     }
 
-    interface RestrictChatMemberOptions {
+    interface RestrictChatMemberOptions extends ChatPermissions {
         until_date?: number | undefined;
-        permissions: ChatPermissions;
         use_independent_chat_permissions?: boolean | undefined;
     }
 
@@ -860,23 +859,23 @@ declare namespace TelegramBot {
         invite_link?: ChatInviteLink;
     }
 
-    interface ChatPermissions {
-        can_send_messages?: boolean | undefined;
-        can_send_audios?: boolean | undefined;
-        can_send_documents?: boolean | undefined;
-        can_send_photos?: boolean | undefined;
-        can_send_videos?: boolean | undefined;
-        can_send_video_notes?: boolean | undefined;
-        can_send_voice_notes?: boolean | undefined;
-        can_send_polls?: boolean | undefined;
-        can_send_other_messages?: boolean | undefined;
-        can_add_web_page_previews?: boolean | undefined;
-        can_change_info?: boolean | undefined;
-        can_invite_users?: boolean | undefined;
-        can_pin_messages?: boolean | undefined;
-        can_manage_topics?: boolean | undefined;
-    }
+    type ChatPermissionsNames =
+        'can_send_messages' |
+        'can_send_audios' |
+        'can_send_documents' |
+        'can_send_photos' |
+        'can_send_videos' |
+        'can_send_video_notes' |
+        'can_send_voice_notes' |
+        'can_send_polls' |
+        'can_send_other_messages' |
+        'can_add_web_page_previews' |
+        'can_change_info' |
+        'can_invite_users' |
+        'can_pin_messages' |
+        'can_manage_topics';
 
+    type ChatPermissions = Partial<Record<ChatPermissionsNames, boolean>>;
     type StickerType = "regular" | "mask" | "custom_emoji";
 
     interface Sticker extends FileBase {
@@ -921,7 +920,7 @@ declare namespace TelegramBot {
     }
 
     // tslint:disable-next-line:no-empty-interface Currently holds no information (https://core.telegram.org/bots/api#forumtopicclosed)
-    interface ForumTopicClosed {}
+    interface ForumTopicClosed { }
 
     interface ForumTopicEdited {
         name: string;
@@ -929,13 +928,13 @@ declare namespace TelegramBot {
     }
 
     // tslint:disable-next-line:no-empty-interface Currently holds no information (https://core.telegram.org/bots/api#forumtopicreopened)
-    interface ForumTopicReopened {}
+    interface ForumTopicReopened { }
 
     // tslint:disable-next-line:no-empty-interface Currently holds no information (https://core.telegram.org/bots/api#generalforumtopichidden)
-    interface GeneralForumTopicHidden {}
+    interface GeneralForumTopicHidden { }
 
     // tslint:disable-next-line:no-empty-interface Currently holds no information (https://core.telegram.org/bots/api#generalforumtopicunhidden)
-    interface GeneralForumTopicUnhidden {}
+    interface GeneralForumTopicUnhidden { }
 
     interface UserShared {
         request_id: number;
