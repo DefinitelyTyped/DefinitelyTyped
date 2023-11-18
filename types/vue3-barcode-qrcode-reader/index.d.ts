8065d515d4317802bbf90f2286a3b2b4c10188c5
tomimandalap <tommymandalaputra@gmail.com>
2023-11-18 10:13:17 +0700

    update component IconCamera

--- types/vue3-barcode-qrcode-reader/index.d.ts
+++ types/vue3-barcode-qrcode-reader/index.d.ts
@@ -3,4 +3,6 @@ import { Component } from "vue";
 export interface CaptureProps {
   capture: 'shoot' | 'stream'
 }
+
 export const StreamQrcodeBarcodeReader: Component<CaptureProps>
+export const IconCamera: Component
--- types/vue3-barcode-qrcode-reader/vue3-barcode-qrcode-reader-tests.ts
+++ types/vue3-barcode-qrcode-reader/vue3-barcode-qrcode-reader-tests.ts
@@ -1,11 +1,14 @@
 import { createApp } from "vue";
-import { StreamQrcodeBarcodeReader } from "vue3-barcode-qrcode-reader";
+import { StreamQrcodeBarcodeReader, IconCamera } from "vue3-barcode-qrcode-reader";
 
 createApp({
     components: {
         "stream-qrcode-barcode-reader": StreamQrcodeBarcodeReader,
+        "icon-camera": IconCamera,
+
     },
     template: `
         <stream-qrcode-barcode-reader :capture="shoot"></stream-qrcode-barcode-reader>
+        <icon-camera></icon-camera>
     `,
 }).mount("#app");
