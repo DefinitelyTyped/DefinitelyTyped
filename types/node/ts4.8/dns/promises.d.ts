3bf5f29f0e20832bd6b3d5c2b41295223cabf87e
btea <2356281422@qq.com>
2023-11-19 17:04:01 +0800

    feat: ts4.8 add getDefaultResultOrder

--- types/node/ts4.8/dns/promises.d.ts
+++ types/node/ts4.8/dns/promises.d.ts
@@ -308,6 +308,14 @@ declare module "dns/promises" {
      */
     function reverse(ip: string): Promise<string[]>;
     /**
+     * Get the default value for `verbatim` in {@link lookup} and `dnsPromises.lookup()`. The value could be:
+     *
+     * * `ipv4first`: for `verbatim` defaulting to `false`.
+     * * `verbatim`: for `verbatim` defaulting to `true`.
+     * @since v20.1.0
+     */
+    function getDefaultResultOrder(): "ipv4first" | "verbatim";
+    /**
      * Sets the IP address and port of servers to be used when performing DNS
      * resolution. The `servers` argument is an array of [RFC 5952](https://tools.ietf.org/html/rfc5952#section-6) formatted
      * addresses. If the port is the IANA default DNS port (53) it can be omitted.
