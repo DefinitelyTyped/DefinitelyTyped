302357ab6f78978002833187a14d21d2c1660546
Sebastian Silbermann <sebastian.silbermann@klarna.com>
2023-11-20 16:54:51 +0100

    Reorder and document to make relation to "ComponentPropsWithRef" clear

--- types/react/index.d.ts
+++ types/react/index.d.ts
@@ -854,20 +854,27 @@ declare namespace React {
         JSXElementConstructor<infer P> ? P
         : T extends keyof JSX.IntrinsicElements ? JSX.IntrinsicElements[T]
         : {};
+    /**
+     * Get the props of a component that supports the `ref` prop.
+     *
+     * WARNING: Use `CustomComponentPropsWithRef` if you know that `T` is not a host component for better type-checking performance.
+     */
     type ComponentPropsWithRef<T extends ElementType> = T extends (new(props: infer P) => Component<any, any>)
         ? PropsWithoutRef<P> & RefAttributes<InstanceType<T>>
         : PropsWithRef<ComponentProps<T>>;
+    /**
+     * Like `ComponentPropsWithRef` but without support for host components (i.e. just "custom components") to improve type-checking performance.
+     */
+    type CustomComponentPropsWithRef<T extends ComponentType> = T extends (new(props: infer P) => Component<any, any>)
+        ? (PropsWithoutRef<P> & RefAttributes<InstanceType<T>>)
+        : T extends ((props: infer P, legacyContext?: any) => ReactNode) ? PropsWithRef<P>
+        : never;
     type ComponentPropsWithoutRef<T extends ElementType> = PropsWithoutRef<ComponentProps<T>>;
 
     type ComponentRef<T extends ElementType> = T extends NamedExoticComponent<
         ComponentPropsWithoutRef<T> & RefAttributes<infer Method>
     > ? Method
         : ComponentPropsWithRef<T> extends RefAttributes<infer Method> ? Method
-        : never;
-
-    type CustomComponentPropsWithRef<T extends ComponentType> = T extends ComponentClass<infer P>
-        ? (PropsWithoutRef<P> & RefAttributes<InstanceType<T>>)
-        : T extends FunctionComponent<infer P> ? PropsWithRef<P>
         : never;
 
     // will show `Memo(${Component.displayName || Component.name})` in devtools by default,
