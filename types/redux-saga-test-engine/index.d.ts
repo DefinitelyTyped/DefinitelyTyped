dcad02220fcbeb06c5b05b922bd354f05d2dec59
Matt Wade <matthew.wade@sproutsocial.com>
2023-11-24 14:00:41 -0600

    Initial definitions
    Struggled with making constraints for createSagaTestEngine, but I think
    I eventually got it. It was overkill, but I actually just converted the
    redux-saga-test-engine library to typescript, and then generated a .d.ts
    file from that and copied it in here (with some minor adjustments).

--- types/redux-saga-test-engine/index.d.ts
+++ types/redux-saga-test-engine/index.d.ts
@@ -1,34 +1,49 @@
-/*~ If this module is a UMD module that exposes a global variable 'myLib' when
- *~ loaded outside a module loader environment, declare that global here.
- *~ Otherwise, delete this declaration.
- */
-export as namespace myLib;
+import { Effect, effectTypes } from "redux-saga/effects";
+import { END } from "@redux-saga/types";
 
-/*~ If this module has methods, declare them as functions like so.
- */
-export function myMethod(a: string): string;
-export function myOtherMethod(a: number): number;
+declare const throwErrorKey: "@@redux-saga-test-engine/ERROR";
 
-/*~ You can declare types that are available via importing the module */
-export interface someType {
-    name: string;
-    length: number;
-    extras?: string[];
-}
+type KeyValuePairs<K = unknown, V = unknown> = Map<K, V> | Array<[K, V]>;
 
-/*~ You can declare properties of the module using const, let, or var */
-export const myField: number;
+type EffectTypes = keyof typeof effectTypes;
+type ThrowErrorKey = typeof throwErrorKey;
+type EffectTypesWithErrorKey = EffectTypes | ThrowErrorKey | END["type"];
 
-/*~ If there are types, properties, or methods inside dotted names
- *~ of the module, declare them inside a 'namespace'.
- */
-export namespace subProp {
-    /*~ For example, given this definition, someone could write:
-     *~   import { subProp } from 'yourModule';
-     *~   subProp.foo();
-     *~ or
-     *~   import * as yourMod from 'yourModule';
-     *~   yourMod.subProp.foo();
-     */
-    function foo(): void;
-}
+declare function throwError<TMessage extends string>(message: TMessage): Effect<ThrowErrorKey, TMessage>;
+declare function stub<TArgs extends any[], TReturn>(
+    genFunc: (...args: TArgs) => Generator<TReturn, any, any> | TReturn,
+    ...args: TArgs
+): () => TReturn;
+type Options<TValues, TEffects> =
+    | {
+          mapping: KeyValuePairs<unknown, TValues>;
+          collected?: Array<Effect<TEffects> | Array<Effect<TEffects>>>;
+          maxSteps?: number;
+      }
+    | KeyValuePairs<unknown, TValues>;
+
+declare function createSagaTestEngine<const TEffectConstraint extends EffectTypes>(
+    effects?: Readonly<TEffectConstraint[]>
+): (
+    genFunc: GeneratorFunction,
+    opts: Options<Effect<EffectTypesWithErrorKey>, TEffectConstraint>,
+    ...initialArgs: any[]
+) => Array<Effect<TEffectConstraint, any> | Array<Effect<TEffectConstraint, any>>>;
+
+declare function collectPuts(
+    genFunc: GeneratorFunction,
+    opts: Options<Effect<EffectTypesWithErrorKey, any>, "PUT">,
+    ...initialArgs: any[]
+): Array<Effect<"PUT", any> | Array<Effect<"PUT", any>>>;
+declare function collectCalls(
+    genFunc: GeneratorFunction,
+    opts: Options<Effect<EffectTypesWithErrorKey, any>, "CALL">,
+    ...initialArgs: any[]
+): Array<Effect<"CALL", any> | Array<Effect<"CALL", any>>>;
+declare function collectCallsAndPuts(
+    genFunc: GeneratorFunction,
+    opts: Options<Effect<EffectTypesWithErrorKey, any>, "PUT" | "CALL">,
+    ...initialArgs: any[]
+): Array<Effect<"PUT" | "CALL", any> | Array<Effect<"PUT" | "CALL", any>>>;
+
+export { collectCalls, collectCallsAndPuts, collectPuts, createSagaTestEngine, stub, throwError };
