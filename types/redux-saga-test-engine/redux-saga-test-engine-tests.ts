35145ab9e20e68a2bd67ff6209940ed8ae01c3ab
Matt Wade <matthew.wade@sproutsocial.com>
2023-11-27 18:04:42 -0600

    Fixes type assertions and inlines put/call fns

--- types/redux-saga-test-engine/redux-saga-test-engine-tests.ts
+++ types/redux-saga-test-engine/redux-saga-test-engine-tests.ts
@@ -1,12 +1,36 @@
+import { type Effect } from "@redux-saga/types";
 import {
-    createSagaTestEngine,
-    collectPuts,
     collectCalls,
     collectCallsAndPuts,
+    collectPuts,
+    createSagaTestEngine,
     stub,
     throwError,
 } from "redux-saga-test-engine";
-import { call, put, type Effect } from "redux-saga/effects";
+
+function put<TPayload>(payload: TPayload): Effect<"PUT", TPayload> {
+    return {
+        "@@redux-saga/IO": true,
+        combinator: false,
+        type: "PUT",
+        payload,
+    };
+}
+
+function call<TFunc extends (..._args: any[]) => any>(
+    fn: TFunc,
+    ...args: any[]
+): Effect<"CALL", { fn: TFunc; args: any[] }> {
+    return {
+        "@@redux-saga/IO": true,
+        combinator: false,
+        type: "CALL",
+        payload: {
+            fn,
+            args,
+        },
+    };
+}
 
 const errorEffect = throwError("Test Error");
 // $ExpectType Effect<"@@redux-saga-test-engine/ERROR", string>
@@ -23,19 +47,19 @@ const incorrectStubbed = stub(testGenerator);
 
 const testEngine = createSagaTestEngine(["PUT"] as const);
 const putEffects = testEngine(testGenerator, [[put({ type: "TEST", payload: "arg" }), null]], "arg");
-// $ExpectType Array<Effect<"PUT", any> | Array<Effect<"PUT", any>>>
+// $ExpectType (Effect<"PUT", any> | Effect<"PUT", any>[])[]
 putEffects;
 
 const puts = collectPuts(testGenerator, [[put({ type: "TEST", payload: "arg" }), null]], "arg");
-// $ExpectType Array<Effect<"PUT", any> | Array<Effect<"PUT", any>>>
+// $ExpectType (Effect<"PUT", any> | Effect<"PUT", any>[])[]
 puts;
 
 const calls = collectCalls(testGenerator, [[put({ type: "TEST", payload: "arg" }), null]], "arg");
-// $ExpectType Array<Effect<"CALL", any> | Array<Effect<"CALL", any>>>
+// $ExpectType (Effect<"CALL", any> | Effect<"CALL", any>[])[]
 calls;
 
 const callsAndPuts = collectCallsAndPuts(testGenerator, [[put({ type: "TEST", payload: "arg" }), null]], "arg");
-// $ExpectType Array<Effect<"PUT" | "CALL", any> | Array<Effect<"PUT" | "CALL", any>>>
+// $ExpectType (Effect<"PUT" | "CALL", any> | Effect<"PUT" | "CALL", any>[])[]
 callsAndPuts;
 
 const identity = <T>(arg: T): T => arg;
