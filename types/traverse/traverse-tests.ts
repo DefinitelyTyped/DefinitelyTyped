5c15c72ae2b906d6b1b9b4e35ef685f04cd3f8c1
Josh Mock <josh@joshmock.com>
2023-11-28 14:54:27 -0600

    Fix formatting

--- types/traverse/traverse-tests.ts
+++ types/traverse/traverse-tests.ts
@@ -78,8 +78,8 @@ function testPropsAndFuncs() {
 
 function testLifecycleFns() {
     var obj = [5, 6, -3, [7, 8, -2, 1], { f: 10, g: -13 }];
-    traverse(obj).map(function (x) {
-        this.before(function (node) {
+    traverse(obj).map(function(x) {
+        this.before(function(node) {
             console.log(node);
         });
 
@@ -95,5 +95,5 @@ function testLifecycleFns() {
         this.after(function(node) {
             console.log(node);
         });
-    })
+    });
 }
