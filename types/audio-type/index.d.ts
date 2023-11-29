223d518ddb38fbbbd1aed475f021473eae02f17a
Christopher Toth <q.alpha@gmail.com>
2023-11-29 18:48:57 -0500

    Audio-type: update typing to address review

--- types/audio-type/index.d.ts
+++ types/audio-type/index.d.ts
@@ -1,15 +1,15 @@
-export default function audioType(buf: Uint8Array | ArrayBuffer | null): string | undefined;
+export default function audioType(buf: Uint8Array | ArrayBuffer): string;
 
-export function isMp3(buf: Uint8Array | ArrayBuffer | null): boolean;
+export function isMp3(buf: Uint8Array | ArrayBuffer): boolean;
 
-export function isWav(buf: Uint8Array | ArrayBuffer | null): boolean;
+export function isWav(buf: Uint8Array | ArrayBuffer): boolean;
 
-export function isOgg(buf: Uint8Array | ArrayBuffer | null): boolean;
+export function isOgg(buf: Uint8Array | ArrayBuffer): boolean;
 
-export function isFlac(buf: Uint8Array | ArrayBuffer | null): boolean;
+export function isFlac(buf: Uint8Array | ArrayBuffer): boolean;
 
-export function isM4a(buf: Uint8Array | ArrayBuffer | null): boolean;
+export function isM4a(buf: Uint8Array | ArrayBuffer): boolean;
 
-export function isOpus(buf: Uint8Array | ArrayBuffer | null): boolean;
+export function isOpus(buf: Uint8Array | ArrayBuffer): boolean;
 
-export function isQoa(buf: Uint8Array | ArrayBuffer | null): boolean;
+export function isQoa(buf: Uint8Array | ArrayBuffer): boolean;
