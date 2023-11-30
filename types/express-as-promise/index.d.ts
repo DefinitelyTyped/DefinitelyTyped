7d3331fa73171a2a91ac1cd81fd95bb8427fb6f9
Tomasz Pluskiewicz <tpluscode@users.noreply.github.com>
2023-11-30 11:07:21 +0100

    style: format

--- types/express-as-promise/index.d.ts
+++ types/express-as-promise/index.d.ts
@@ -1,9 +1,9 @@
-import { Express } from 'express';
-import { Server } from 'http';
+import { Express } from "express";
+import { Server } from "http";
 
 declare class ExpressAsPromise {
     server: Server | null;
-    app: Express
+    app: Express;
 
     get host(): string;
     get port(): string;
--- types/express-as-promise/withServer.d.ts
+++ types/express-as-promise/withServer.d.ts
@@ -1,4 +1,4 @@
-import ExpressAsPromise = require('./index');
+import ExpressAsPromise = require("./index");
 
 declare function withServer(callback: (server: ExpressAsPromise) => Promise<void>): Promise<void>;
 
