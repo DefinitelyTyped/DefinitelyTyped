f2bf10c629013c5a53901048673364218249ddba
Kristóf Poduszló <14854048+kripod@users.noreply.github.com>
2023-12-02 12:36:36 +0100

    Update tsx.tsx

--- types/react/test/tsx.tsx
+++ types/react/test/tsx.tsx
@@ -560,11 +560,11 @@ const CustomElement2: React.ElementType = "my-declared-element-deprecated";
 const CustomElement3: React.ElementType = "my-declared-element";
 <my-declared-element />;
 
-const CustomBuiltins1: React.ElementType<{ className?: string | undefined }, "a" | "button"> = "a";
-const CustomBuiltins2: React.ElementType<{ className?: string | undefined }, "a" | "button"> = "button";
+const CustomTag1: React.ElementType<{ className?: string | undefined }, "a" | "button"> = "a";
+const CustomTag2: React.ElementType<{ className?: string | undefined }, "a" | "button"> = "button";
 // @ts-expect-error
-const CustomBuiltins3: React.ElementType<{ className?: string | undefined }, "a" | "button"> = "div";
-const CustomBuiltins4: React.ElementType<{ className?: string | undefined }, "a" | "button"> = (props: { className?: string | undefined }) => <div {...props} />;
+const CustomTag3: React.ElementType<{ className?: string | undefined }, "a" | "button"> = "div";
+const CustomTag4: React.ElementType<{ className?: string | undefined }, "a" | "button"> = (props: { className?: string | undefined }) => <div {...props} />;
 
 interface TestPropTypesProps {
     foo: string;
