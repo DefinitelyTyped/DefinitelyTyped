b713729bd2238a9fe04b4eaa62b28a92792e51c3
eps1lon <silbermann.sebastian@gmail.com>
2023-12-03 17:42:15 +0100

    [fundamental-react] Replace usage of React's global JSX namespace with scoped JSX Was deprecated in https://github.com/DefinitelyTyped/DefinitelyTyped/pull/64464

--- types/fundamental-react/lib/Select/Select.d.ts
+++ types/fundamental-react/lib/Select/Select.d.ts
@@ -43,7 +43,7 @@ export interface SelectProps<T extends string = string> {
     ref?: React.Ref<HTMLDivElement> | undefined;
 }
 
-declare const Select: (<T extends string = string>(props: SelectProps<T> & { selectedKey?: T }) => JSX.Element) & {
+declare const Select: (<T extends string = string>(props: SelectProps<T> & { selectedKey?: T }) => React.JSX.Element) & {
     displayName: "Select";
 };
 
