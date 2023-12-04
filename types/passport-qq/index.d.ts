9c0776d7c6428a93bb62835fa8ecf4b2827933fa
bangbang93 <bangbang93@163.com>
2023-12-04 16:53:51 +0800

    feat: enchance UserProfileCallback

--- types/passport-qq/index.d.ts
+++ types/passport-qq/index.d.ts
@@ -1,5 +1,5 @@
 import { Profile } from "passport";
-import { Strategy as OAuth2Strategy } from "passport-oauth2";
+import { Strategy as OAuth2Strategy, VerifyCallback } from "passport-oauth2";
 
 export interface Options {
     clientID: string;
@@ -10,12 +10,21 @@ export interface Options {
     scopeSeparator?: string;
 }
 
-export type Callback = (err: Error | null, user: unknown) => void;
+export interface User extends Profile {
+    provider: 'qq'
+    id: string
+    nickanme: string
+    _raw: string
+    _json: object
+}
 
-export type VerifyCallback = (accessToken: string, refreshToken: string, profile: Profile, done: Callback) => void;
+
+export type UserProfileCallback = (err: Error | null, user: User) => void;
+
+export type VerifyFunction = (accessToken: string, refreshToken: string, profile: User, done: VerifyCallback) => void;
 
 export class Strategy extends OAuth2Strategy {
-    constructor(options: Options, verify: VerifyCallback);
+    constructor(options: Options, verify: VerifyFunction);
 
-    userProfile(accessToken: string, done: Callback): void;
+    userProfile(accessToken: string, done: UserProfileCallback): void;
 }
