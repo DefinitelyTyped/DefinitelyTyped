76e11344e185ce84e43cc6917dda6a15c2de28c1
eps1lon <silbermann.sebastian@gmail.com>
2023-12-04 23:07:21 +0100

    fixup! [react-router-redux] Fix compat issues with latest dependencies

--- types/react-router-redux/react-router-redux-tests.tsx
+++ types/react-router-redux/react-router-redux-tests.tsx
@@ -2,7 +2,7 @@ import * as React from "react";
 import * as ReactDOM from "react-dom";
 
 import { Provider } from "react-redux";
-import { applyMiddleware, combineReducers, createStore, Reducer, ReducersMapObject } from "redux";
+import { applyMiddleware, combineReducers, createStore } from "redux";
 
 import createHistory from "history/createBrowserHistory";
 import { Route } from "react-router";
