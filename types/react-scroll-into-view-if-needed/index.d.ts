097c3c2f7d5cdef772cd603a45e4830b90e1efe3
Sebastian Silbermann <silbermann.sebastian@gmail.com>
2023-12-06 07:42:26 +0100

    [react-scroll-into-view-if-needed] Replace usage of React's global JSX namespace with scoped JSX (#67698)

--- types/react-scroll-into-view-if-needed/index.d.ts
+++ types/react-scroll-into-view-if-needed/index.d.ts
@@ -4,7 +4,7 @@ import * as ScrollIntoViewIfNeeded from "scroll-into-view-if-needed";
 export interface ReactScrollIntoViewIfNeededProps extends React.HTMLProps<HTMLElement> {
     options?: ScrollIntoViewIfNeeded.Options | undefined;
     active?: boolean | undefined;
-    elementType?: keyof JSX.IntrinsicElements | undefined;
+    elementType?: keyof React.JSX.IntrinsicElements | undefined;
 }
 
 export default class ReactScrollIntoViewIfNeeded extends React.Component<
