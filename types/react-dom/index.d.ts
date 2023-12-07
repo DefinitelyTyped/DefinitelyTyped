5b74d428c32c1958114f5d1ea885b833dcc14bd4
eps1lon <silbermann.sebastian@gmail.com>
2023-12-07 19:44:04 +0100

    [react-dom] Remove `unstable_renderSubtreeIntoContainer`

--- types/react-dom/index.d.ts
+++ types/react-dom/index.d.ts
@@ -4,16 +4,7 @@
 
 export as namespace ReactDOM;
 
-import {
-    CElement,
-    Component,
-    ComponentState,
-    DOMAttributes,
-    DOMElement,
-    ReactElement,
-    ReactNode,
-    ReactPortal,
-} from "react";
+import { ReactNode, ReactPortal } from "react";
 
 export function createPortal(
     children: ReactNode,
@@ -27,32 +18,3 @@ export function flushSync<R>(fn: () => R): R;
 
 export function unstable_batchedUpdates<A, R>(callback: (a: A) => R, a: A): R;
 export function unstable_batchedUpdates<R>(callback: () => R): R;
-
-/**
- * @deprecated
- */
-export function unstable_renderSubtreeIntoContainer<T extends Element>(
-    parentComponent: Component<any>,
-    element: DOMElement<DOMAttributes<T>, T>,
-    container: Element,
-    callback?: (element: T) => any,
-): T;
-/**
- * @deprecated
- */
-export function unstable_renderSubtreeIntoContainer<P, T extends Component<P, ComponentState>>(
-    parentComponent: Component<any>,
-    element: CElement<P, T>,
-    container: Element,
-    callback?: (component: T) => any,
-): T;
-/**
- * @deprecated
- */
-export function unstable_renderSubtreeIntoContainer<P>(
-    parentComponent: Component<any>,
-    element: ReactElement<P>,
-    container: Element,
-    callback?: (component?: Component<P, ComponentState> | Element) => any,
-    // eslint-disable-next-line @typescript-eslint/no-invalid-void-type
-): Component<P, ComponentState> | Element | void;
