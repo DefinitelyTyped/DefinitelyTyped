1dfca5465e2cd8e5fd5dda38ed98306769a75ec7
eps1lon <silbermann.sebastian@gmail.com>
2023-12-07 19:37:35 +0100

    [react-dom] Remove `hydrate`

--- types/react-dom/index.d.ts
+++ types/react-dom/index.d.ts
@@ -10,7 +10,6 @@ import {
     ComponentState,
     DOMAttributes,
     DOMElement,
-    FunctionComponentElement,
     ReactElement,
     ReactNode,
     ReactPortal,
@@ -23,10 +22,6 @@ export function createPortal(
 ): ReactPortal;
 
 export const version: string;
-/**
- * @deprecated See https://react.dev/blog/2022/03/08/react-18-upgrade-guide#updates-to-client-rendering-apis
- */
-export const hydrate: Renderer;
 
 export function flushSync<R>(fn: () => R): R;
 
@@ -61,75 +56,3 @@ export function unstable_renderSubtreeIntoContainer<P>(
     callback?: (component?: Component<P, ComponentState> | Element) => any,
     // eslint-disable-next-line @typescript-eslint/no-invalid-void-type
 ): Component<P, ComponentState> | Element | void;
-
-export type Container = Element | Document | DocumentFragment;
-
-export interface Renderer {
-    // Deprecated(render): The return value is deprecated.
-    // In future releases the render function's return type will be void.
-
-    /**
-     * @deprecated See https://react.dev/blog/2022/03/08/react-18-upgrade-guide#updates-to-client-rendering-apis
-     */
-    <T extends Element>(
-        element: DOMElement<DOMAttributes<T>, T>,
-        container: Container | null,
-        callback?: () => void,
-    ): T;
-
-    /**
-     * @deprecated See https://react.dev/blog/2022/03/08/react-18-upgrade-guide#updates-to-client-rendering-apis
-     */
-    (
-        element: Array<DOMElement<DOMAttributes<any>, any>>,
-        container: Container | null,
-        callback?: () => void,
-    ): Element;
-
-    /**
-     * @deprecated See https://react.dev/blog/2022/03/08/react-18-upgrade-guide#updates-to-client-rendering-apis
-     */
-    (
-        element: FunctionComponentElement<any> | Array<FunctionComponentElement<any>>,
-        container: Container | null,
-        callback?: () => void,
-    ): void;
-
-    /**
-     * @deprecated See https://react.dev/blog/2022/03/08/react-18-upgrade-guide#updates-to-client-rendering-apis
-     */
-    <P, T extends Component<P, ComponentState>>(
-        element: CElement<P, T>,
-        container: Container | null,
-        callback?: () => void,
-    ): T;
-
-    /**
-     * @deprecated See https://react.dev/blog/2022/03/08/react-18-upgrade-guide#updates-to-client-rendering-apis
-     */
-    (
-        element: Array<CElement<any, Component<any, ComponentState>>>,
-        container: Container | null,
-        callback?: () => void,
-    ): Component<any, ComponentState>;
-
-    /**
-     * @deprecated See https://react.dev/blog/2022/03/08/react-18-upgrade-guide#updates-to-client-rendering-apis
-     */
-    <P>(
-        element: ReactElement<P>,
-        container: Container | null,
-        callback?: () => void,
-        // eslint-disable-next-line @typescript-eslint/no-invalid-void-type
-    ): Component<P, ComponentState> | Element | void;
-
-    /**
-     * @deprecated See https://react.dev/blog/2022/03/08/react-18-upgrade-guide#updates-to-client-rendering-apis
-     */
-    (
-        element: ReactElement[],
-        container: Container | null,
-        callback?: () => void,
-        // eslint-disable-next-line @typescript-eslint/no-invalid-void-type
-    ): Component<any, ComponentState> | Element | void;
-}
