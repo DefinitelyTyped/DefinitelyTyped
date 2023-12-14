6b52b9c104774f46b18292161cea37649bae921d
dhowe <dhowe@cims.nyu.edu>
2023-12-14 14:11:30 +0800

    formatted

--- types/riscript/index.d.ts
+++ types/riscript/index.d.ts
@@ -3,57 +3,56 @@
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
 
 declare class RiScript {
-  static VERSION: string;
-  static evaluate(script: string, context?: object, options?: {
-    trace?: boolean;
-    onepass?: boolean;
-    silent?: boolean;
-  }): string;
+    static VERSION: string;
+    static evaluate(script: string, context?: object, options?: {
+        trace?: boolean;
+        onepass?: boolean;
+        silent?: boolean;
+    }): string;
 
-  constructor(opts?: {});
-  evaluate(script: string, context?: object, options?: {
-    trace?: boolean;
-    onepass?: boolean;
-    silent?: boolean;
-  }): string;
-  addTransform(name: string, def: any): RiScript;
-  getTransforms(): string[];
-  removeTransform(name: string): RiScript;
+    constructor(opts?: {});
+    evaluate(script: string, context?: object, options?: {
+        trace?: boolean;
+        onepass?: boolean;
+        silent?: boolean;
+    }): string;
+    addTransform(name: string, def: any): RiScript;
+    getTransforms(): string[];
+    removeTransform(name: string): RiScript;
 }
 
 declare class RiGrammar {
-  static expand(rules: string, context?: object, options?: {
-    trace?: boolean;
-    onepass?: boolean;
-    silent?: boolean;
-  }): string;
-  static fromJSON(json: string, context?: object): RiGrammar;
-  constructor(rules?: { [x: string]: string } | string, context?: { [x: string]: any });
-  addTransform(name: string, def: any): RiGrammar;
-  removeTransform(name: string): RiGrammar;
-  getTransforms(): string[];
-  equals(rg: RiGrammar): boolean;
-  expand(options?: {
-    start?: string;
-    trace?: boolean;
-    onepass?: boolean;
-    silent?: boolean;
-  }): string;
-  addRule(name: string, def: string): RiGrammar;
-  setRules(rules: object | string): RiGrammar;
-  removeRule(name: string): RiGrammar;
-  toJSON(options?: {
-    replacer?: string;
-    space?: string;
-  }): string;
-  toString(options?: {
-    replacer?: string;
-    space?: string;
-    linebreak?: string;
-  }): string;
+    static expand(rules: string, context?: object, options?: {
+        trace?: boolean;
+        onepass?: boolean;
+        silent?: boolean;
+    }): string;
+    static fromJSON(json: string, context?: object): RiGrammar;
+    constructor(rules?: { [x: string]: string } | string, context?: { [x: string]: any });
+    addTransform(name: string, def: any): RiGrammar;
+    removeTransform(name: string): RiGrammar;
+    getTransforms(): string[];
+    equals(rg: RiGrammar): boolean;
+    expand(options?: {
+        start?: string;
+        trace?: boolean;
+        onepass?: boolean;
+        silent?: boolean;
+    }): string;
+    addRule(name: string, def: string): RiGrammar;
+    setRules(rules: object | string): RiGrammar;
+    removeRule(name: string): RiGrammar;
+    toJSON(options?: {
+        replacer?: string;
+        space?: string;
+    }): string;
+    toString(options?: {
+        replacer?: string;
+        space?: string;
+        linebreak?: string;
+    }): string;
 }
 
-
 interface Tokenizer {
-  (source: string, subString: string): boolean;
+    (source: string, subString: string): boolean;
 }
