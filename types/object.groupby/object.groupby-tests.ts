876d872de477b2d59ae1ed6a0f8f00cd0f8c88d4
huw <yo@huw.cool>
2023-12-15 15:28:38 +1100

    Update `object.groupby-tests` for optional property returns

--- types/object.groupby/object.groupby-tests.ts
+++ types/object.groupby/object.groupby-tests.ts
@@ -1,10 +1,13 @@
 import groupBy = require("object.groupby");
 
-// $ExpectType Record<number, string[]>
+// $ExpectType { [P in number]?: string }
 groupBy(["a", "bb", "ccc"], v => v.length);
 
-// $ExpectType Record<"odd" | "even", number[]>
+// $ExpectType { odd?: number[]; even?: number[] }
 groupBy([1, 2, 3], v => v ? "odd" : "even");
 
-// $ExpectType Record<symbol, any[]>
+// $ExpectType { odd?: number[]; even?: number[] }
+groupBy([2, 4, 6], v => v % 2 !== 0 ? "odd" : "even");
+
+// $ExpectType { [P in symbol]?: any[] }
 groupBy([235, {} as any], v => Symbol(v.toString()));
