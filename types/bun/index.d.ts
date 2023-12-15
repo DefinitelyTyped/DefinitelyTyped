e84d0e465161b2c9bfac8535653b581ffc3ad679
Colin McDonnell <colinmcd94@gmail.com>
2023-12-14 21:34:39 -0800

    Remove header

--- types/bun/index.d.ts
+++ types/bun/index.d.ts
@@ -1,5 +1,1 @@
-// Project: https://github.com/oven-sh/bun
-// Definitions by: Jarred Sumner <https://github.com/Jarred-Sumner>
-// Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
-
 /// <reference types="bun-types" />
