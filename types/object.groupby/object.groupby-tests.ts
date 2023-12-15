ec91b9a1e9f5561ccb36fabc9e8c38626a3bbc64
huw <yo@huw.cool>
2023-12-15 04:49:12 +0000

    Update `object.groupby` tests to correctly match output types

--- types/object.groupby/object.groupby-tests.ts
+++ types/object.groupby/object.groupby-tests.ts
@@ -1,13 +1,13 @@
 import groupBy = require("object.groupby");
 
-// $ExpectType { [P in number]?: string }
+// $ExpectType { [x: number]: string[] | undefined }
 groupBy(["a", "bb", "ccc"], v => v.length);
 
-// $ExpectType { odd?: number[]; even?: number[] }
+// $ExpectType { odd?: number[] | undefined; even?: number[] | undefined }
 groupBy([1, 2, 3], v => v ? "odd" : "even");
 
-// $ExpectType { odd?: number[]; even?: number[] }
+// $ExpectType { odd?: number[] | undefined; even?: number[] | undefined }
 groupBy([2, 4, 6], v => v % 2 !== 0 ? "odd" : "even");
 
-// $ExpectType { [P in symbol]?: any[] }
+// $ExpectType { [x: symbol]: any[] | undefined }
 groupBy([235, {} as any], v => Symbol(v.toString()));
