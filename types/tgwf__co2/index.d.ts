aa69f97d7e6b392844690f8754262b6323dff538
fershad <27988517+fershad@users.noreply.github.com>
2023-12-16 21:37:53 +0800

    Add types for @tgwf/co2

---
+++ types/tgwf__co2/index.d.ts
@@ -0,0 +1,12 @@
+declare namespace _default {
+    export { co2 };
+    export { hosting };
+    export { averageIntensity };
+    export { marginalIntensity };
+}
+export default _default;
+import co2 from "./co2.js";
+import hosting from "./hosting.js";
+import averageIntensity from "./data/average-intensities.min.js";
+import marginalIntensity from "./data/marginal-intensities-2021.min.js";
+export { co2, hosting, averageIntensity, marginalIntensity };
