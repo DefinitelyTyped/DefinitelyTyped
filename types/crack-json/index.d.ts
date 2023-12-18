ca7053db55f6f5cf0936999fdb5253e70bf83548
Johannes Henning <j.henning@sap.com>
2023-12-18 12:47:10 +0100

    Add types for `crack-json`

---
+++ types/crack-json/index.d.ts
@@ -0,0 +1,9 @@
+declare module "crack-json" {
+  function extractJson(
+    subject: string,
+    configuration?: {
+      filter?: (input: string) => boolean;
+      parser?: (input: string) => any;
+    }
+  ): any;
+}
