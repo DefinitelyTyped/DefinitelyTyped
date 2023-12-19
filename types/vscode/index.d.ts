f9fa2c919c6931d73d5118e5cde50d731c280b76
Malix <alixbrunetcontact@gmail.com>
2023-12-19 23:13:52 +0100

    Feat: Activate + Deactivate functions types

--- types/vscode/index.d.ts
+++ types/vscode/index.d.ts
@@ -11,6 +11,30 @@
 
 declare module 'vscode' {
 
+    /**
+	 * **Required**
+	 *
+	 * Entry point of a VSCode Extension
+	 *
+	 * Called when the first [Activation Event](https://code.visualstudio.com/api/references/activation-events) happens
+	 *
+	 * [_(Source)_](https://code.visualstudio.com/api/get-started/extension-anatomy#extension-entry-file)
+	 */
+	export type Activate = (extensionContext: ExtensionContext) => void;
+
+	/**
+	 * **Optional**
+	 *
+	 * Called before an extension is deactivated
+	 *
+	 * Gives a chance to clean up before an extension becomes deactivated
+	 *
+	 * For many extensions, explicit cleanup may not be required, and the deactivate method can be removed. However, if an extension needs to perform an operation when VS Code is shutting down or the extension is disabled or uninstalled, this is the method to do so.
+	 *
+	 * [_(Source)_](https://code.visualstudio.com/api/get-started/extension-anatomy#extension-entry-file)
+	 */
+	export type Deactivate = () => void;
+
 	/**
 	 * The version of the editor.
 	 */
