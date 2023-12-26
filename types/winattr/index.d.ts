0915e858d4fc765ee1f75e2b6127c641f4af644c
Anatoliy Oblaukhov <mail@anmiles.net>
2023-12-26 23:52:39 +0300

    Add types/winattr

---
+++ types/winattr/index.d.ts
@@ -0,0 +1,21 @@
+export type Attr = "archive" | "hidden" | "readonly" | "system";
+
+export type Attrs = Record<Attr, boolean>;
+
+export function get(
+    path: string,
+    callback: <TError extends Error | null>(error: TError, attrs: TError extends TError ? never : Attrs) => void,
+): void;
+export function getSync(path: string): Attrs;
+
+export function set(path: string, attrs: Partial<Attrs>, callback: (error: Error | null) => void): void;
+export function setSync(path: string, attrs: Partial<Attrs>): void;
+
+declare const winattr: {
+    get: typeof get;
+    set: typeof set;
+    getSync: typeof getSync;
+    setSync: typeof setSync;
+};
+
+export default winattr;
