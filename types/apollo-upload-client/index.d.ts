c62765aa7c72dc4990fc5822872e55535fcc6c83
Nathan Shively-Sanders <293473+sandersn@users.noreply.github.com>
2023-12-29 10:08:06 -0800

    Update file structure to apollo-upload-client@18

--- types/apollo-upload-client/index.d.ts
+++ types/apollo-upload-client/index.d.ts
@@ -2,10 +2,7 @@
 // exports
 // ==============================================================================
 
-export { default as createUploadLink } from "./public/createUploadLink";
-export { default as formDataAppendFile } from "./public/formDataAppendFile";
-export { default as isExtractableFile } from "./public/isExtractableFile";
-export { default as ReactNativeFile } from "./public/ReactNativeFile";
+export { }
 
 // ==============================================================================
 // declare
