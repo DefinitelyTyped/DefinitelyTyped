9cf245fb0d30bf6c37351b30c921a20d29185a93
Vadzim Tsupryk <vadzim@vadzim.me>
2023-12-29 01:24:26 +0100

    feat: Add types for `tailwind-shades`

---
+++ types/tailwind-shades/index.d.ts
@@ -0,0 +1,10 @@
+/**
+ * Generate Tailwind-compatible shades from a single color
+ * @param {string} hex The hex code to generate shades from
+ * @param {boolean} [halfShades=false] Generate additional shades, e.g. at 150
+ * @returns {{[key: number]: string}}
+ */
+export default function shadesOf(
+  hex: string,
+  halfShades?: boolean,
+): { [key: number]: string };
---
+++ types/tailwind-shades/tailwind-shades-tests.ts
@@ -0,0 +1,5 @@
+import shadesOf from 'tailwind-shades';
+
+shadesOf('#bada55');
+
+shadesOf('#bada55', true);
