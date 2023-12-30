92bf7083d7a78285ed86e8f063a8b145c97f1378
fershad <27988517+fershad@users.noreply.github.com>
2023-12-30 20:53:31 +0800

    fix file paths

--- types/tgwf__co2/index.d.ts
+++ types/tgwf__co2/index.d.ts
@@ -1,8 +1,8 @@
 export namespace co2js {
     const co2: typeof import("./co2.d.ts");
     const hosting: typeof import("./hosting.d.ts");
-    const averageIntensity: typeof import("./averageIntensity.d.ts");
-    const marginalIntensity: typeof import("./marginalIntensity.d.ts");
+    const averageIntensity: typeof import("./data/averageIntensity.d.ts");
+    const marginalIntensity: typeof import("./data/marginalIntensity.d.ts");
 }
 
 export = co2js;
