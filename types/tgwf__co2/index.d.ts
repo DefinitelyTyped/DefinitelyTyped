53eaebbc39ce5391625d7707a200a0a50071ee91
fershad <27988517+fershad@users.noreply.github.com>
2023-12-30 21:33:13 +0800

    export declarations

--- types/tgwf__co2/index.d.ts
+++ types/tgwf__co2/index.d.ts
@@ -4,6 +4,8 @@ import averageIntensity = require("./data/average-intensities.min");
 import marginalIntensity = require("./data/marginal-intensities-2021.min");
 import hosting = require("./hosting");
 
+export { co2, hosting, averageIntensity, marginalIntensity };
+
 export namespace co2js {
     export { co2 };
     export { hosting };
