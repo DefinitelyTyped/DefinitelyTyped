514f3a66ef8a05f216dfae2ca8692685597bdc33
naphtali-gilead <naphtali.gilead@mheducation.com>
2024-01-04 15:24:00 -0500

    Bug fixes: Explicitly importing, naming and using the correct Agent Options.

--- types/tunnel/index.d.ts
+++ types/tunnel/index.d.ts
@@ -1,24 +1,27 @@
 /// <reference types="node" />
-import { Agent } from "http";
-import { Agent as HttpsAgent, AgentOptions } from "https";
+import { Agent, AgentOptions as HttpAgentOptions } from "http";
+import { Agent as HttpsAgent, AgentOptions as HttpsAgentOptions } from "https";
 
 export function httpOverHttp(options?: HttpOptions): Agent;
 export function httpsOverHttp(options?: HttpsOverHttpOptions): Agent;
 export function httpOverHttps(options?: HttpOverHttpsOptions): HttpsAgent;
 export function httpsOverHttps(options?: HttpsOverHttpsOptions): HttpsAgent;
 
-export interface HttpOptions {
-    maxSockets?: number | undefined;
+export interface HttpOptions extends HttpAgentOptions {
     proxy?: ProxyOptions | undefined;
 }
 
-export interface HttpsOverHttpOptions extends HttpOptions {
+export interface HttpsOptions extends HttpsAgentOptions {
+    proxy?: ProxyOptions | undefined;
+}
+
+export interface HttpsOverHttpOptions extends HttpsOptions {
     ca?: Buffer[] | undefined;
     key?: Buffer | undefined;
     cert?: Buffer | undefined;
 }
 
-export interface HttpOverHttpsOptions extends HttpOptions, AgentOptions {
+export interface HttpOverHttpsOptions extends HttpOptions {
     proxy?: HttpsProxyOptions | undefined;
 }
 
