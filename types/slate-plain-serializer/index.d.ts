23f942a9038652aaf8bfd96f2fc44bddf3a7dc7f
Ben Asher <ben@ashbyhq.com>
2024-01-08 18:25:14 -0800

    Compatiblity with NodeNext

--- types/slate-plain-serializer/index.d.ts
+++ types/slate-plain-serializer/index.d.ts
@@ -1,19 +1,24 @@
 import { BlockProperties, MarkProperties, Value } from "slate";
 
-export interface DeserializeOptions {
+interface DeserializeOptions {
     toJson?: boolean | undefined;
     defaultBlock?: BlockProperties | undefined;
     defaultMarks?: MarkProperties[] | Set<MarkProperties> | undefined;
     delimiter?: string | undefined;
 }
 
-export interface SerializeOptions {
+interface SerializeOptions {
     delimiter?: string | undefined;
 }
 
 declare namespace Plain {
-    function deserialize(string: string, options?: DeserializeOptions): Value;
-    function serialize(value: Value, options?: SerializeOptions): string;
+    export function deserialize(string: string, options?: DeserializeOptions): Value;
+    export function serialize(value: Value, options?: SerializeOptions): string;
+
+    const _default: typeof Plain;
+    export { _default as default, DeserializeOptions, SerializeOptions };
 }
 
-export default Plain;
+// compatibility with NodeNext
+// eslint-disable-next-line @definitelytyped/export-just-namespace
+export = Plain;
