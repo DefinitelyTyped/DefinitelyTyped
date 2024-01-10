99a306290624f48704573f134cde2d1b635d2eba
tpluscode <tpluscode@users.noreply.github.com>
2024-01-10 21:29:04 +0100

    rdf-utils-fs: remove rdf-js

--- types/rdf-utils-fs/toFile.d.ts
+++ types/rdf-utils-fs/toFile.d.ts
@@ -1,5 +1,5 @@
 import { PathLike } from "fs";
-import { Stream } from "rdf-js";
+import { Stream } from "@rdfjs/types";
 import defaults = require("./defaults");
 
 type Options = Record<string, any> & {
