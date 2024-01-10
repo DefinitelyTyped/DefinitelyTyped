194fdcfe9462e3803bc96b39c9ff2215b1eae18a
tpluscode <tpluscode@users.noreply.github.com>
2024-01-10 21:38:11 +0100

    remove: barnard59* packages

--- types/barnard59-base/glob.d.ts
+++
@@ -1,9 +0,0 @@
-import * as G from "glob";
-import { Readable } from "readable-stream";
-
-interface Options extends G.IOptions {
-    pattern: string;
-}
-
-export default function glob(arg: Options): Readable;
-export {};
