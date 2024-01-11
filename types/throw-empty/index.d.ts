3c00f739fea3ac279ebfbf76bdc4679f35c1cd2a
Rajas Paranjpe <52586855+ChocolateLoverRaj@users.noreply.github.com>
2024-01-10 18:11:27 -0800

    [throw-error] Remove old comments

--- types/throw-empty/index.d.ts
+++ types/throw-empty/index.d.ts
@@ -1,8 +1,3 @@
-// Type definitions for throw-empty 1.0
-// Project: https://www.npmjs.com/package/throw-empty
-// Definitions by: Rajas Paranjpe <https://github.com/ChocolateLoverRaj>
-// Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
-
 declare function throwEmpty<T>(value: T | null | undefined): T;
 
 export = throwEmpty;
