e30ef22bc9757237b81fab6d3c40f9a887f39001
qiushaocloud <qiushaocloud@126.com>
2024-01-15 02:37:14 +0000

    fix qtypings-browser-global and qtypings-nodejs-global bug

--- types/qtypings-nodejs-global/typings/nodejs.d.ts
+++ types/qtypings-nodejs-global/typings/nodejs.d.ts
@@ -1,5 +1,5 @@
-declare namespace NodeJS {
-    interface Global {
-        [propName: string]: unknown;
-    }
-}
+// declare namespace NodeJS {
+//     interface Global {
+//         [propName: string]: unknown;
+//     }
+// }
