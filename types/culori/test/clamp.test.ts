f699684b7e3eb192e3c8bb6ade1d1f7484ff02bb
Goggwell <zulkarnaensamuel1@gmail.com>
2024-01-16 23:14:57 +0700

    test(culori): new test for clamp.d.ts
    Adding tests for clampRgb() and clampChroma()

---
+++ types/culori/test/clamp.test.ts
@@ -0,0 +1,13 @@
+import { clampRgb, clampChroma, Color, Mode, RgbGamut } from "culori/fn";
+
+declare const color: unknown;
+declare const mode: unknown;
+declare const rgbGamut: unknown;
+
+clampRgb(color as string); // $ExpectType Color | undefined
+clampRgb(color as Color); // $ExpectType Color
+
+clampChroma(color as string); // $ExpectType Color | undefined
+clampChroma(color as Color); // $ExpectType Color
+clampChroma(color as Color, mode as Mode); // $ExpectType Color
+clampChroma(color as Color, mode as Mode, rgbGamut as RgbGamut); // $ExpectType Color
