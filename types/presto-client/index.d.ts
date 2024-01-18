5f5205ec9a19dc9d614e9b57069c23409c6bbeb2
Mark Fulton <mfulton26@gmail.com>
2024-01-18 14:31:29 -0600

    prest-client: add undocumented query states
    Refs:
    - https://github.com/prestodb/presto/blob/496e98ca25fb243ab25731d188c2fd56eba3b3e9/presto-main/src/main/java/com/facebook/presto/execution/QueryState.java
    - https://github.com/prestodb/presto/commit/4bf8df1484935a000619377dab6ed35778fff2ad

--- types/presto-client/index.d.ts
+++ types/presto-client/index.d.ts
@@ -200,7 +200,7 @@ export interface RuntimeStats {
     wallTimeMillis: number;
     cpuTimeMillis: number;
     userTimeMillis: number;
-    state: "QUEUED" | "PLANNING" | "STARTING" | "RUNNING" | "FINISHED" | "CANCELED" | "FAILED";
+    state: "WAITING_FOR_PREREQUISITES" | "QUEUED" | "WAITING_FOR_RESOURCES" | "DISPATCHING" | "PLANNING" | "STARTING" | "RUNNING" | "FINISHING" | "FINISHED" | "CANCELED" | "FAILED";
     scheduled: boolean;
     nodes: number;
     totalSplits: number;
