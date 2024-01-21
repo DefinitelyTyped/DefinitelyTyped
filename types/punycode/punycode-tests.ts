b02b0f63ee9ee65bea9edcb22fd8bb4b750718b7
Hannes Rüger <hannesrueger@gmx.de>
2024-01-21 21:06:44 +0100

    feat: support `punycode` js file imports

--- types/punycode/punycode-tests.ts
+++ types/punycode/punycode-tests.ts
@@ -9,3 +9,15 @@ const ucs2result1: number[] = punycode.ucs2.decode("x");
 const ucs2result2: string = punycode.ucs2.encode([1, 2, 3]);
 
 const version: string = punycode.version;
+
+
+import punycodeDirect = require("punycode/punycode.js");
+const result1Direct: string = punycode.decode("x");
+const result2Direct: string = punycode.encode("x");
+const result3Direct: string = punycode.toASCII("x");
+const result4Direct: string = punycode.toUnicode("x");
+
+const ucs2result1Direct: number[] = punycode.ucs2.decode("x");
+const ucs2result2Direct: string = punycode.ucs2.encode([1, 2, 3]);
+
+const versionDirect: string = punycode.version;
