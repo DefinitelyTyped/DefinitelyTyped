3f876473c1a46bcd40b55633114f5f2604aa4d67
Smith <a-sync@devs.space>
2024-01-22 10:18:53 +0100

    fix test2

--- types/gamedig/gamedig-tests.ts
+++ types/gamedig/gamedig-tests.ts
@@ -6,13 +6,16 @@ gd.GameDig.query(
         type: "tf2",
         host: "127.0.0.1",
         port: 27015,
-        maxAttempts: 1,
+        maxRetries: 1,
         socketTimeout: 2000,
         attemptTimeout: 10000,
         givenPortOnly: true,
+        portCache: true,
         ipFamily: 0,
         debug: false,
         requestRules: true,
+        requestRulesRequired: true,
+        requestPlayersRequired: true
     },
     (error: any, state: gd.QueryResult) => {
         if (error) throw error;
