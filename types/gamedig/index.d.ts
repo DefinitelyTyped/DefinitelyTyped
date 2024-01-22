09bc5485adb954f0bb0680fa8d506d017e9fbfb2
Smith <a-sync@devs.space>
2024-01-22 11:34:50 +0100

    working syntax

--- types/gamedig/index.d.ts
+++ types/gamedig/index.d.ts
@@ -1,84 +1,87 @@
-export namespace gamedig {
-    class GameDig {
-        constructor(runnerOpts?: { listenUdpPort?: number });
-        query(options: gamedig.QueryOptions): Promise<gamedig.QueryResult>;
-        query(options: gamedig.QueryOptions, callback: (error: Error, state: gamedig.QueryResult) => void): void;
-    
-        static query(options: gamedig.QueryOptions): Promise<gamedig.QueryResult>;
-        static query(options: gamedig.QueryOptions, callback: (error: Error, state: gamedig.QueryResult) => void): void;
-        static getInstance(): GameDig;
-    }
+export class GameDig {
+    constructor(runnerOpts?: { listenUdpPort?: number });
+    query(options: QueryOptions): Promise<QueryResult>;
+    query(options: QueryOptions, callback: (error: Error, state: QueryResult) => void): void;
 
-    interface games {
-        [key: string]: {
-            name: string;
-            release_year: number;
-            options: {
-                protocol: string;
-                port?: number;
-                query_port?: number;
-                port_query?: number;
-                port_query_offset?: number;
-            },
-            extra?: {
-                doc_notes?: string;
-            }
-        };
-    }
-
-    interface protocols {
-        [key: string]: object;
-    }
-
-    interface Player {
-        name?: string;
-        raw?: object;
-        ping?: number;
-        score?: number;
-        team?: string;
-        address?: string;
-    }
-
-    interface QueryOptions {
-        type: string;
-        host: string;
-        address?: string;
-        port?: number;
-        maxRetries?: number;
-        socketTimeout?: number;
-        attemptTimeout?: number;
-        givenPortOnly?: boolean;
-        ipFamily?: 0 | 4 | 6;
-        debug?: boolean;
-        portCache?: boolean;
-        stripColors?: boolean;
-        noBreadthOrder?: boolean;
-        // Valve
-        requestRules?: boolean;
-        requestRulesRequired?: boolean;
-        requestPlayersRequired?: boolean;
-        // Discord
-        guildId?: string;
-        // Nadeo
-        login?: string;
-        password?: string;
-        // Teamspeak 3
-        teamspeakQueryPort?: number;
-        // Terraria
-        token?: string;
-    }
-
-    interface QueryResult {
-        name: string;
-        map: string;
-        password: boolean;
-        numplayers: number;
-        maxplayers: number;
-        players: Player[];
-        bots: Player[];
-        connect: string;
-        ping: number;
-        queryPort: number;
-        raw?: object;
-    }
+    static query(options: QueryOptions): Promise<QueryResult>;
+    static query(options: QueryOptions, callback: (error: Error, state: QueryResult) => void): void;
+    static getInstance(): GameDig;
 }
+
+export interface games {
+    [key: string]: {
+        name: string;
+        release_year: number;
+        options: {
+            protocol: string;
+            port?: number;
+            query_port?: number;
+            port_query?: number;
+            port_query_offset?: number;
+        },
+        extra?: {
+            doc_notes?: string;
+        }
+    };
+}
+
+export interface protocols {
+    [key: string]: object;
+}
+
+export interface Player {
+    name?: string;
+    raw?: object;
+    ping?: number;
+    score?: number;
+    team?: string;
+    address?: string;
+}
+
+export interface QueryOptions {
+    type: string;
+    host: string;
+    address?: string;
+    port?: number;
+    maxRetries?: number;
+    socketTimeout?: number;
+    attemptTimeout?: number;
+    givenPortOnly?: boolean;
+    ipFamily?: 0 | 4 | 6;
+    debug?: boolean;
+    portCache?: boolean;
+    stripColors?: boolean;
+    noBreadthOrder?: boolean;
+    // Valve
+    requestRules?: boolean;
+    requestRulesRequired?: boolean;
+    requestPlayersRequired?: boolean;
+    // Discord
+    guildId?: string;
+    // Nadeo
+    login?: string;
+    password?: string;
+    // Teamspeak 3
+    teamspeakQueryPort?: number;
+    // Terraria
+    token?: string;
+}
+
+export interface QueryResult {
+    name: string;
+    map: string;
+    password: boolean;
+    numplayers: number;
+    maxplayers: number;
+    players: Player[];
+    bots: Player[];
+    connect: string;
+    ping: number;
+    queryPort: number;
+    raw?: object;
+}
+
+export const games: games;
+export const protocols: protocols;
+
+export as namespace gamedig;
