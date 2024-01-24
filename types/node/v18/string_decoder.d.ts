978543999d7262dbe5cd4ec517ff5085ef05b757
codershiba <155646804+codershiba@users.noreply.github.com>
2024-01-25 00:52:05 +0530

    chore(types): Update types for `node:string_decoder`

--- types/node/v18/string_decoder.d.ts
+++ types/node/v18/string_decoder.d.ts
@@ -1,23 +1,23 @@
 /**
- * The `string_decoder` module provides an API for decoding `Buffer` objects into
- * strings in a manner that preserves encoded multi-byte UTF-8 and UTF-16
+ * The `node:string_decoder` module provides an API for decoding `Buffer` objects
+ * into strings in a manner that preserves encoded multi-byte UTF-8 and UTF-16
  * characters. It can be accessed using:
  *
  * ```js
- * const { StringDecoder } = require('string_decoder');
+ * const { StringDecoder } = require('node:string_decoder');
  * ```
  *
  * The following example shows the basic use of the `StringDecoder` class.
  *
  * ```js
- * const { StringDecoder } = require('string_decoder');
+ * const { StringDecoder } = require('node:string_decoder');
  * const decoder = new StringDecoder('utf8');
  *
  * const cent = Buffer.from([0xC2, 0xA2]);
- * console.log(decoder.write(cent));
+ * console.log(decoder.write(cent)); // Prints: ¢
  *
  * const euro = Buffer.from([0xE2, 0x82, 0xAC]);
- * console.log(decoder.write(euro));
+ * console.log(decoder.write(euro)); // Prints: €
  * ```
  *
  * When a `Buffer` instance is written to the `StringDecoder` instance, an
@@ -29,14 +29,14 @@
  * symbol (`€`) are written over three separate operations:
  *
  * ```js
- * const { StringDecoder } = require('string_decoder');
+ * const { StringDecoder } = require('node:string_decoder');
  * const decoder = new StringDecoder('utf8');
  *
  * decoder.write(Buffer.from([0xE2]));
  * decoder.write(Buffer.from([0x82]));
- * console.log(decoder.end(Buffer.from([0xAC])));
+ * console.log(decoder.end(Buffer.from([0xAC]))); // Prints: €
  * ```
- * @see [source](https://github.com/nodejs/node/blob/v18.0.0/lib/string_decoder.js)
+ * @see [source](https://github.com/nodejs/node/blob/v18.19.0/lib/string_decoder.js)
  */
 declare module "string_decoder" {
     class StringDecoder {
@@ -46,9 +46,9 @@ declare module "string_decoder" {
          * the end of the `Buffer`, or `TypedArray`, or `DataView` are omitted from the
          * returned string and stored in an internal buffer for the next call to`stringDecoder.write()` or `stringDecoder.end()`.
          * @since v0.1.99
-         * @param buffer A `Buffer`, or `TypedArray`, or `DataView` containing the bytes to decode.
+         * @param buffer The bytes to decode.
          */
-        write(buffer: Buffer): string;
+        write(buffer: string | Buffer | NodeJS.ArrayBufferView): string;
         /**
          * Returns any remaining input stored in the internal buffer as a string. Bytes
          * representing incomplete UTF-8 and UTF-16 characters will be replaced with
@@ -57,9 +57,9 @@ declare module "string_decoder" {
          * If the `buffer` argument is provided, one final call to `stringDecoder.write()`is performed before returning the remaining input.
          * After `end()` is called, the `stringDecoder` object can be reused for new input.
          * @since v0.9.3
-         * @param buffer A `Buffer`, or `TypedArray`, or `DataView` containing the bytes to decode.
+         * @param buffer The bytes to decode.
          */
-        end(buffer?: Buffer): string;
+        end(buffer?: string | Buffer | NodeJS.ArrayBufferView): string;
     }
 }
 declare module "node:string_decoder" {
