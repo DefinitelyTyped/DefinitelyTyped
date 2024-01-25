01a4e7de0f74e668cdd91bf279f32c6a6aac7bcf
fershad <27988517+fershad@users.noreply.github.com>
2024-01-25 12:56:23 +0800

    add userAgentIdentifier param to hosting

--- types/tgwf__co2/hosting.d.ts
+++ types/tgwf__co2/hosting.d.ts
@@ -1,6 +1,7 @@
 /**
  * Check if a domain is hosted by a green web host.
  * @param {string|array} domain - The domain to check, or an array of domains to be checked.
+ * @param {string} userAgentIdentifier - Optional. The app, site, or organisation that is making the request. This gets sent as a User-Agent header with the outbound fetch request.
  * @returns {boolean|array} - A boolean if a string was provided, or an array of booleans if an array of domains was provided.
  */
-export function check(domain: string | any[]): boolean | any[];
+export function check(domain: string | any[], userAgentIdentifier?: string): boolean | any[];
