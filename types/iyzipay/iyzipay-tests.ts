c56282456b6f2eb995ac16052031785c2e8f11df
Can Gökçeaslan <48398625+cangokceaslan@users.noreply.github.com>
2024-01-26 01:00:09 +0300

    Completed

---
+++ types/iyzipay/iyzipay-tests.ts
@@ -0,0 +1,11 @@
+import Iyzipay from "iyzipay";
+
+const iyzipay = new Iyzipay({
+    secretKey: "secretKey",
+    apiKey: "apiKey",
+    uri: "https://sandbox-api.iyzipay.com"
+})
+
+iyzipay.apiTest.retrieve({}, (err, result) => {
+    console.log(result);
+});
