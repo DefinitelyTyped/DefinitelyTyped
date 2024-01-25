b281e8c9f299a7953f6d34aea58e12f970d3a4f7
PartMan <47669599+PartMan7@users.noreply.github.com>
2024-01-25 13:16:13 +0530

    fix: Fix types of Hue.pointer
    The current type is ReactNode; the actual library accepts a Component instead

--- types/react-color/index.d.ts
+++ types/react-color/index.d.ts
@@ -1,4 +1,4 @@
-import { ClassAttributes, ReactNode } from "react";
+import { ClassAttributes, ComponentType } from "react";
 import { Classes } from "reactcss";
 
 export interface HSLColor {
@@ -35,7 +35,7 @@ export interface ColorPickerProps<A> extends ClassAttributes<A> {
 
 export interface CustomPickerProps<A> extends ClassAttributes<A> {
     color?: Color | undefined;
-    pointer?: ReactNode | undefined;
+    pointer?: ComponentType<{ direction?: 'vertical' }> | undefined;
     className?: string | undefined;
     styles?: Partial<Classes<any>> | undefined;
     onChange: ColorChangeHandler;
