ca5122d10c99e0d92f577c649b14d27272d89808
fershad <27988517+fershad@users.noreply.github.com>
2024-01-29 12:48:02 +0800

    update hosting types

--- types/tgwf__co2/hosting.d.ts
+++ types/tgwf__co2/hosting.d.ts
@@ -1,7 +1,8 @@
 /**
  * Check if a domain is hosted by a green web host.
- * @param {string|array} domain - The domain to check, or an array of domains to be checked.
+ * @param {string|string[]} domain - The domain to check, or an array of domains to be checked.
  * @param {string} userAgentIdentifier - Optional. The app, site, or organisation that is making the request. This gets sent as a User-Agent header with the outbound fetch request.
- * @returns {boolean|array} - A boolean if a string was provided, or an array of booleans if an array of domains was provided.
+ * @returns {boolean|string[]} - A boolean if a string was provided, or an array of strings if an array of domains was provided.
  */
-export function check(domain: string | any[], userAgentIdentifier?: string): boolean | any[];
+
+export function check(domain: string | string[], userAgentIdentifier?: string): boolean | string[];
