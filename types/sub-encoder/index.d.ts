5ea166d40c04363e1869b44bff7ec84f2243ceef
Evan Hahn <me@evanhahn.com>
2024-01-30 22:50:11 +0000

    Add basic types for sub-encoder

---
+++ types/sub-encoder/index.d.ts
@@ -0,0 +1,44 @@
+type JsonPrimitive = string | number | boolean | null;
+type JsonArray = JsonValue[];
+interface JsonObject {
+    [key: string]: JsonValue | undefined;
+}
+type JsonValue = JsonPrimitive | JsonArray | JsonObject;
+
+interface Encoding<TInput, TOutput = TInput> {
+    input: TInput;
+    output: TOutput;
+}
+
+interface EncodingLookup {
+    ascii: Encoding<string>;
+    base64: Encoding<string>;
+    binary: Encoding<string | Buffer, Buffer>;
+    hex: Encoding<string>;
+    json: Encoding<JsonValue>;
+    ndjson: Encoding<JsonValue>;
+    ucs2: Encoding<string>;
+    "utf-8": Encoding<string>;
+    utf16le: Encoding<string>;
+}
+
+declare class SubEncoder<T extends keyof EncodingLookup = "binary"> {
+    /**
+     * Create a new SubEncoder. Optionally set the initial prefix and encoding.
+     *
+     * `prefix` can be string or Buffer.
+     */
+    constructor(prefix?: null | string | Buffer, encoding?: T);
+
+    /**
+     * Encode a key.
+     */
+    encode(key: EncodingLookup[T]["input"]): Buffer;
+
+    /**
+     * Decode a key.
+     */
+    decode(buf: Buffer): EncodingLookup[T]["output"];
+}
+
+export = SubEncoder;
---
+++ types/sub-encoder/sub-encoder-tests.ts
@@ -0,0 +1,55 @@
+/// <reference types="node" />
+
+import SubEncoder = require("sub-encoder");
+
+{
+    const enc = new SubEncoder();
+
+    enc.encode("str"); // $ExpectType Buffer
+    enc.encode(Buffer.alloc(0)); // $ExpectType Buffer
+    enc.decode(Buffer.alloc(0)); // $ExpectType Buffer
+
+    // @ts-expect-error
+    enc.encode(123);
+}
+
+{
+    const enc = new SubEncoder(null, "binary");
+
+    enc.encode(""); // $ExpectType Buffer
+    enc.encode(Buffer.alloc(0)); // $ExpectType Buffer
+    enc.decode(Buffer.alloc(0)); // $ExpectType Buffer
+
+    // @ts-expect-error
+    enc.encode(123);
+}
+
+{
+    const STRING_ENCODINGS = ["ascii", "base64", "utf-8", "hex", "ucs2", "utf16le"] as const;
+
+    for (const encoding of STRING_ENCODINGS) {
+        const enc = new SubEncoder(null, encoding);
+        enc.encode(""); // $ExpectType Buffer
+        enc.decode(Buffer.alloc(0)); // $ExpectType string
+    }
+
+    // @ts-expect-error
+    enc.encode(Buffer.alloc(0));
+}
+
+{
+    const JSON_ENCODING = ["json", "ndjson"] as const;
+
+    for (const encoding of JSON_ENCODING) {
+        const enc = new SubEncoder(null, encoding);
+        enc.encode({ foo: "bar" }); // $ExpectType Buffer
+        enc.decode(Buffer.alloc(0)); // $ExpectType JsonValue
+    }
+}
+
+type JsonPrimitive = string | number | boolean | null;
+type JsonArray = JsonValue[];
+interface JsonObject {
+    [key: string]: JsonValue | undefined;
+}
+type JsonValue = JsonPrimitive | JsonArray | JsonObject;
