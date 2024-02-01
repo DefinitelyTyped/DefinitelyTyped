e4fe74ea1f402cc1aed19835c1bc429f4eb594e0
Matt Pocock <mattpocockvoice@gmail.com>
2024-02-01 04:57:17 +0000

    Removed dead comments code

--- types/react/index.d.ts
+++ types/react/index.d.ts
@@ -371,17 +371,6 @@ declare namespace React {
         children: (value: T) => ReactNode;
     }
 
-    // TODO: similar to how Fragment is actually a symbol, the values returned from createContext,
-    // forwardRef and memo are actually objects that are treated specially by the renderer; see:
-    // https://github.com/facebook/react/blob/v16.6.0/packages/react/src/ReactContext.js#L35-L48
-    // https://github.com/facebook/react/blob/v16.6.0/packages/react/src/forwardRef.js#L42-L45
-    // https://github.com/facebook/react/blob/v16.6.0/packages/react/src/memo.js#L27-L31
-    // However, we have no way of telling the JSX parser that it's a JSX element type or its props other than
-    // by pretending to be a normal component.
-    //
-    // We don't just use ComponentType or FunctionComponent types because you are not supposed to attach statics to this
-    // object, but rather to the original function.
-
     /**
      * An object masquerading as a component. These are created by
      * {@link forwardRef}, {@link memo}, and {@link createContext}.
