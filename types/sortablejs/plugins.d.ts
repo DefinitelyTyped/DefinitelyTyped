07d7b43ab47227c759b95ba2cc35797318935c2d
MiyueFE <913784771@qq.com>
2024-02-01 16:33:00 +0800

    Format plugins.d.ts

--- types/sortablejs/plugins.d.ts
+++ types/sortablejs/plugins.d.ts
@@ -71,9 +71,9 @@ export interface MultiDragOptions {
      */
     selectedClass?: string | undefined;
     /**
-     * The key that must be down for multiple items to be selected. 
-     * The default is null, meaning no key must be down. 
-     * For special keys, such as the CTRL key, 
+     * The key that must be down for multiple items to be selected.
+     * The default is null, meaning no key must be down.
+     * For special keys, such as the CTRL key,
      * simply specify the option as 'CTRL' (casing does not matter).
      */
     multiDragKey?: null | string;
