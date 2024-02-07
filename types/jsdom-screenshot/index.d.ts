9b1e68405afcdbe86af41ddc71dde18ef3b00154
Chris Drifte <chrisdrifte@gmail.com>
2024-02-07 12:27:35 +0100

    Add request parameter to intercept handler

--- types/jsdom-screenshot/index.d.ts
+++ types/jsdom-screenshot/index.d.ts
@@ -1,4 +1,4 @@
-import { LaunchOptions, Page, ScreenshotOptions, Viewport } from "puppeteer";
+import { LaunchOptions, Page, ScreenshotOptions, Viewport, Request } from "puppeteer";
 
 export interface GenerateImageOptions {
     launch?: LaunchOptions | undefined;
@@ -7,7 +7,7 @@ export interface GenerateImageOptions {
     debug?: boolean | undefined;
     waitUntilNetworkIdle?: boolean | undefined;
     viewport?: Viewport | undefined;
-    intercept?: VoidFunction | undefined;
+    intercept?: ((request: Request) => void) | undefined;
 }
 
 export function debug(element?: Element | Document): void;
