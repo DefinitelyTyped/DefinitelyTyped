5d516c4858627b599ec467109ab4854f106044b8
Adarsh Singh <adarsh@smoketrees.in>
2024-02-07 11:15:28 +0530

    feat: types for paytmchecksum package

---
+++ types/paytmchecksum/index.d.ts
@@ -0,0 +1,105 @@
+export interface PaytmTxnAmmount {
+    value: string;
+    currency: string;
+}
+
+export interface PaytmUserInfo {
+    custId: string;
+    mobile?: string;
+    email?: string;
+    firstName?: string;
+    lastName?: string;
+}
+
+export interface PaytmPaymentMode {
+    mode?: string;
+    channels?: string[];
+}
+
+export interface PaytmExtendInfo {
+    [key: string]: string | undefined;
+    mercUnqRef?: string;
+    comments?: string;
+}
+
+export interface PaytmGoodsInfo {
+    [key: string]: string | PaytmTxnAmmount | undefined | PaytmExtendInfo;
+    price?: PaytmTxnAmmount;
+    extendInfo: PaytmExtendInfo;
+
+}
+
+export interface PaytmShippingInfo {
+    merchantShippingId: string;
+    trackingNo: string;
+    carrier: string;
+    chargeAmount: PaytmTxnAmmount;
+    countryName: string;
+    stateName: string;
+    address1: string;
+    address2: string;
+    firstName: string;
+    lastName: string;
+    mobileNo: string;
+    zipCode: string;
+    email: string;
+}
+
+export interface PaytmTransactionBody {
+    requestType: string;
+    mid: string;
+    orderId: string;
+    websiteName: string;
+    txnAmount: PaytmTxnAmmount;
+    userInfo: PaytmUserInfo;
+    paytmSsoToken?: string;
+    enablePaymentMode?: PaytmPaymentMode[];
+    disablePaymentMode?: PaytmPaymentMode[];
+    promocode?: string;
+    callbackUrl?: string;
+    goods?: PaytmGoodsInfo[];
+    shippingInfo?: Array<Partial<PaytmShippingInfo>>;
+    extendInfo?: PaytmExtendInfo;
+    /* eslint-disable */
+    paymentOffersApplied?: any;
+    simplifiedPaymentOffers?: any;
+    /* eslint-enable */
+    emiSubventionToken?: string;
+    paybleAmount?: PaytmTxnAmmount;
+}
+
+export interface PaytmStatusBody {
+    mid: string;
+    orderId: string;
+    txnType?: string;
+}
+
+export type PaytmParamsBody = PaytmStatusBody | PaytmTransactionBody
+
+export interface PaytmParamsHead {
+    version?: string;
+    channelId?: string;
+    requestTimeStamp?: string;
+    clientId?: string;
+    signature: string;
+}
+
+export interface PaytmParams {
+    body: PaytmParamsBody;
+    head: PaytmParamsHead;
+}
+
+declare class PaytmChecksum {
+    static iv: string;
+    static encrypt(input: string, key: string): string;
+    static decrypt(encrypted: string, key: string): string;
+    static generateSignature(params: string | PaytmParamsBody, key: string): Promise<string>;
+    static verifySignature(params: string | PaytmParamsBody, key: string, checksum: string): Promise<boolean> | boolean;
+    static generateSignatureByString(params: string, key: string): Promise<string>;
+    static verifySignatureByString(params: string, key: string, checksum: string): boolean;
+    static generateRandomString(length: number): Promise<string>;
+    static getStringByParams(params: PaytmParamsBody): string;
+    static calculateHash(params: string, salt: string): string;
+    static calculateChecksum(params: string, key: string, salt: string): string;
+}
+export default PaytmChecksum
---
+++ types/paytmchecksum/paytmchecksum-tests.ts
@@ -0,0 +1,43 @@
+/* More Details: https://developer.paytm.com/docs/checksum/#node */
+import * as PaytmChecksum from 'paytmchecksum'
+
+
+PaytmChecksum.encrypt("this fn is usually not used", "YOUR_MERCHANT_KEY")
+PaytmChecksum.decrypt("this fn is usually not used", "YOUR_MERCHANT_KEY")
+PaytmChecksum.generateSignature(JSON.stringify({
+  mid: "YOUR_MID_HERE",
+  orderId: "YOUR_ORDER_ID_HERE"
+}), "YOUR_MERCHANT_KEY")
+PaytmChecksum.generateSignature({
+  mid: "YOUR_MID_HERE",
+  orderId: "YOUR_ORDER_ID_HERE"
+}, "YOUR_MERCHANT_KEY")
+PaytmChecksum.verifySignature(JSON.stringify({
+  mid: "YOUR_MID_HERE",
+  orderId: "YOUR_ORDER_ID_HERE"
+}), "YOUR_MERCHANT_KEY", "checksum from generateSignature")
+PaytmChecksum.verifySignature({
+  mid: "YOUR_MID_HERE",
+  orderId: "YOUR_ORDER_ID_HERE"
+}, "YOUR_MERCHANT_KEY", "checksum from generateSignature")
+PaytmChecksum.generateSignatureByString(JSON.stringify({
+  mid: "YOUR_MID_HERE",
+  orderId: "YOUR_ORDER_ID_HERE"
+}), "YOUR_MERCHANT_KEY")
+PaytmChecksum.verifySignatureByString(JSON.stringify({
+  mid: "YOUR_MID_HERE",
+  orderId: "YOUR_ORDER_ID_HERE"
+}), "YOUR_MERCHANT_KEY", "checksum from generateSignature")
+PaytmChecksum.generateRandomString(10)
+PaytmChecksum.getStringByParams({
+  mid: "YOUR_MID_HERE",
+  orderId: "YOUR_ORDER_ID_HERE"
+})
+PaytmChecksum.calculateHash(JSON.stringify({
+  mid: "YOUR_MID_HERE",
+  orderId: "YOUR_ORDER_ID_HERE"
+}), "RANDOM STRING AS SALT")
+PaytmChecksum.calculateChecksum(JSON.stringify({
+  mid: "YOUR_MID_HERE",
+  orderId: "YOUR_ORDER_ID_HERE"
+}), "YOUR_MERCHANT_KEY", "RANDOM STRING AS SALT")
