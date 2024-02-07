752c69825fa0fcf899abdce8c1d5c8abd010da7a
Goggwell <zulkarnaensamuel1@gmail.com>
2024-02-07 12:10:40 +0700

    test(culori): revised clamp test

--- types/culori/test/clamp.test.ts
+++ types/culori/test/clamp.test.ts
@@ -1,13 +1,16 @@
-import { clampChroma, clampRgb, Color, GamutMode, Mode } from "culori/fn";
+import { clampChroma, clampRgb, Lch } from "culori/fn";
 
-declare const color: unknown;
-declare const mode: unknown;
-declare const rgbGamut: unknown;
+const lch1 = "lch(50% 50 90)";
+const lch2: Lch = { mode: "lch", l: 50, c: 50, h: 90, alpha: 1 };
+const mode = "lch";
+const gamutMode = "p3";
 
-clampRgb(color as string); // $ExpectType Color | undefined
-clampRgb(color as Color); // $ExpectType Color
+clampRgb(lch1); // $ExpectType Color | undefined
+clampRgb(lch2); // $ExpectType Lch
 
-clampChroma(color as string); // $ExpectType Color | undefined
-clampChroma(color as Color); // $ExpectType Color
-clampChroma(color as Color, mode as Mode); // $ExpectType Color
-clampChroma(color as Color, mode as Mode, rgbGamut as GamutMode); // $ExpectType Color
+clampChroma(lch1); // $ExpectType Color | undefined
+clampChroma(lch2); // $ExpectType Lch
+clampChroma(lch1, mode); // $ExpectType Color | undefined
+clampChroma(lch2, mode); // $ExpectType Lch
+clampChroma(lch1, mode, gamutMode); // $ExpectType Color | undefined
+clampChroma(lch2, mode, gamutMode); // $ExpectType Lch
