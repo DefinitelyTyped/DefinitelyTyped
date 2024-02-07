a573adb1b3656ba2dd9101fb4ea0eac030c73d65
Sebastian Silbermann <sebastian.silbermann@klarna.com>
2024-02-07 16:56:32 +0100

    [react] Upgrade `enableFloat` to stable

--- types/react-dom/canary.d.ts
+++ types/react-dom/canary.d.ts
@@ -32,82 +32,6 @@ import ReactDOM = require(".");
 export {};
 
 declare module "." {
-    function prefetchDNS(href: string): void;
-
-    interface PreconnectOptions {
-        // Don't create a helper type.
-        // It would have to be in module scope to be inlined in TS tooltips.
-        // But then it becomes part of the public API.
-        // TODO: Upstream to microsoft/TypeScript-DOM-lib-generator -> w3c/webref
-        // since the spec has a notion of a dedicated type: https://html.spec.whatwg.org/multipage/urls-and-fetching.html#cors-settings-attribute
-        crossOrigin?: "anonymous" | "use-credentials" | "" | undefined;
-    }
-    function preconnect(href: string, options?: PreconnectOptions): void;
-
-    type PreloadAs =
-        | "audio"
-        | "document"
-        | "embed"
-        | "fetch"
-        | "font"
-        | "image"
-        | "object"
-        | "track"
-        | "script"
-        | "style"
-        | "video"
-        | "worker";
-    interface PreloadOptions {
-        as: PreloadAs;
-        crossOrigin?: "anonymous" | "use-credentials" | "" | undefined;
-        fetchPriority?: "high" | "low" | "auto" | undefined;
-        // TODO: These should only be allowed with `as: 'image'` but it's not trivial to write tests against the full TS support matrix.
-        imageSizes?: string | undefined;
-        imageSrcSet?: string | undefined;
-        integrity?: string | undefined;
-        type?: string | undefined;
-        nonce?: string | undefined;
-        referrerPolicy?: ReferrerPolicy | undefined;
-    }
-    function preload(href: string, options?: PreloadOptions): void;
-
-    // https://html.spec.whatwg.org/multipage/links.html#link-type-modulepreload
-    type PreloadModuleAs = RequestDestination;
-    interface PreloadModuleOptions {
-        /**
-         * @default "script"
-         */
-        as: PreloadModuleAs;
-        crossOrigin?: "anonymous" | "use-credentials" | "" | undefined;
-        integrity?: string | undefined;
-        nonce?: string | undefined;
-    }
-    function preloadModule(href: string, options?: PreloadModuleOptions): void;
-
-    type PreinitAs = "script" | "style";
-    interface PreinitOptions {
-        as: PreinitAs;
-        crossOrigin?: "anonymous" | "use-credentials" | "" | undefined;
-        fetchPriority?: "high" | "low" | "auto" | undefined;
-        precedence?: string | undefined;
-        integrity?: string | undefined;
-        nonce?: string | undefined;
-    }
-    function preinit(href: string, options?: PreinitOptions): void;
-
-    // Will be expanded to include all of https://github.com/tc39/proposal-import-attributes
-    type PreinitModuleAs = "script";
-    interface PreinitModuleOptions {
-        /**
-         * @default "script"
-         */
-        as?: PreinitModuleAs;
-        crossOrigin?: "anonymous" | "use-credentials" | "" | undefined;
-        integrity?: string | undefined;
-        nonce?: string | undefined;
-    }
-    function preinitModule(href: string, options?: PreinitModuleOptions): void;
-
     function requestFormReset(form: HTMLFormElement): void;
 }
 
@@ -140,9 +64,5 @@ declare module "./client" {
                 },
             ) => void)
             | undefined;
-    }
-
-    interface DO_NOT_USE_OR_YOU_WILL_BE_FIRED_EXPERIMENTAL_CREATE_ROOT_CONTAINERS {
-        document: Document;
     }
 }
--- types/react-dom/test/react-dom-tests.tsx
+++ types/react-dom/test/react-dom-tests.tsx
@@ -100,14 +100,14 @@ async function batchTests() {
     }, 1);
 }
 
-function createRoot() {
+function createRoot(validContainer: Element | DocumentFragment | Document) {
     const root = ReactDOMClient.createRoot(document.documentElement);
 
     root.render(<div>initial render</div>);
     root.render(false);
 
-    // Will not type-check in a real project but accepted in DT tests since experimental.d.ts is part of compilation.
     ReactDOMClient.createRoot(document);
+    ReactDOMClient.createRoot(validContainer);
 }
 
 function hydrateRoot() {
@@ -442,4 +442,105 @@ function formTest() {
 
     const formState = [1, "", "", 0] as unknown as ReactDOMClient.ReactFormState;
     ReactDOMClient.hydrateRoot(document.body, <Page1 />, { formState });
+}
+
+function preloadTest() {
+    function Component() {
+        ReactDOM.preload("foo", { as: "style", fetchPriority: "high", integrity: "sad" });
+        ReactDOM.preload("bar", {
+            as: "font",
+            type: "font/woff2",
+            // @ts-expect-error Unknown fetch priority
+            fetchPriority: "unknown",
+        });
+        ReactDOM.preload("baz", { as: "script", crossOrigin: "use-credentials" });
+        ReactDOM.preload("baz", {
+            // @ts-expect-error
+            as: "title",
+        });
+        ReactDOM.preload("bar", {
+            as: "font",
+            nonce: "0xeac1",
+        });
+        ReactDOM.preload("foo", {
+            as: "image",
+            imageSrcSet: "fooset",
+            imageSizes: "foosizes",
+            referrerPolicy: "no-referrer",
+        });
+        ReactDOM.preload("foo", {
+            as: "image",
+            // @ts-expect-error Not specified in https://w3c.github.io/webappsec-referrer-policy/#referrer-policy
+            referrerPolicy: "unknown-policy",
+        });
+        ReactDOM.preload("foo", {
+            as: "script",
+            // Undesired. Should not typecheck.
+            imageSrcSet: "fooset",
+            imageSizes: "foosizes",
+        });
+
+        ReactDOM.preinit("foo", {
+            as: "style",
+            crossOrigin: "anonymous",
+            fetchPriority: "high",
+            precedence: "high",
+            integrity: "sad",
+            nonce: "0xeac1",
+        });
+        ReactDOM.preinit("bar", {
+            // @ts-expect-error Only available in preload
+            as: "font",
+        });
+        ReactDOM.preinit("baz", {
+            as: "script",
+            // @ts-expect-error Unknown fetch priority
+            fetchPriority: "unknown",
+        });
+        ReactDOM.preinit("baz", {
+            // @ts-expect-error
+            as: "title",
+        });
+        ReactDOM.preinit("baz", {
+            as: "script",
+            nonce: "0xeac1",
+        });
+
+        // @ts-expect-error
+        ReactDOM.preconnect();
+        ReactDOM.preconnect("foo");
+        ReactDOM.preconnect("bar", { crossOrigin: "anonymous" });
+
+        // @ts-expect-error
+        ReactDOM.prefetchDNS();
+        ReactDOM.prefetchDNS("foo");
+        ReactDOM.prefetchDNS(
+            "bar", // @ts-expect-error prefetchDNS does not accept any options at the moment
+            {},
+        );
+
+        // @ts-expect-error
+        ReactDOM.preloadModule();
+        ReactDOM.preloadModule("browserdefault");
+        ReactDOM.preloadModule("browserdefault", {
+            as: "script",
+            crossOrigin: "use-credentials",
+            integrity: "0xeac1",
+            nonce: "secret",
+        });
+        ReactDOM.preloadModule("worker", { as: "worker" });
+        ReactDOM.preloadModule("worker", {
+            // @ts-expect-error Unknown request destination
+            as: "serviceworker",
+        });
+
+        // @ts-expect-error
+        ReactDOM.preinitModule();
+        ReactDOM.preinitModule("browserdefault");
+        ReactDOM.preinitModule("browserdefault", { as: "script", crossOrigin: "use-credentials", integrity: "0xeac1" });
+        ReactDOM.preinitModule("data", {
+            // @ts-expect-error Not supported (yet)
+            as: "json",
+        });
+    }
 }
