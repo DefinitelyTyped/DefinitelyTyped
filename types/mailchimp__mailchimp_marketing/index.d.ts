32d886d789be7c968d14abdf71137f705b047a5f
Fida Waseque Choudhury <146055002+u3kkasha@users.noreply.github.com>
2024-02-08 19:13:03 +0600

    refactor(types): use union type for winner_criteria
    Co-authored-by: codershiba <155646804+codershiba@users.noreply.github.com>

--- types/mailchimp__mailchimp_marketing/index.d.ts
+++ types/mailchimp__mailchimp_marketing/index.d.ts
@@ -2795,7 +2795,7 @@ export namespace campaigns {
             /**
              * The combination that performs the best. This may be determined automatically by click rate, open rate, or total revenue -- or you may choose manually based on the reporting data you find the most valuable. For Multivariate Campaigns testing send_time, winner_criteria is ignored. For Multivariate Campaigns with 'manual' as the winner_criteria, the winner must be chosen in the Mailchimp web application. Possible values: "opens", "clicks", "manual", or "total_revenue".
              */
-            winner_criteria: string;
+            winner_criteria: "opens" | "clicks" | "manual" | "total_revenue";
         };
 
         /**
