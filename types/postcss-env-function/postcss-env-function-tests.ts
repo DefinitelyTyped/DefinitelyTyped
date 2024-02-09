969a64b7fdf49c5e7165678f59084ed722b26a6a
Adam Thompson-Sharpe <adamthompsonsharpe@gmail.com>
2024-02-08 22:36:07 -0500

    feat(postcss-env-function): Add types for postcss-env-function

---
+++ types/postcss-env-function/postcss-env-function-tests.ts
@@ -0,0 +1,20 @@
+import postcss = require("postcss");
+import postcssEnvFunction = require("postcss-env-function");
+
+// No arguments
+postcss([postcssEnvFunction]);
+postcss([postcssEnvFunction()]);
+postcss([postcssEnvFunction({})]);
+// Valid types for importFrom
+postcss([postcssEnvFunction({ importFrom: "./foo.json" })]);
+postcss([postcssEnvFunction({ importFrom: { environmentVariables: { foo: "123" } } })]);
+postcss([postcssEnvFunction({ importFrom: () => ({ environmentVariables: { foo: "123" } }) })]);
+postcss([
+    postcssEnvFunction({
+        importFrom: [
+            "./foo.json",
+            { environmentVariables: { foo: "123" } },
+            () => ({ environmentVariables: { foo: "123" } }),
+        ],
+    }),
+]);
