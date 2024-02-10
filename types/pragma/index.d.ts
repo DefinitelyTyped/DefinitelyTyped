04742957e0faf8de86ae16141b7a8c65909e35a3
e6nlaq <120777733+e6nlaq@users.noreply.github.com>
2024-02-10 17:00:35 +0900

    Integrated type definitions

--- types/pragma/index.d.ts
+++ types/pragma/index.d.ts
@@ -3,17 +3,9 @@
  * Parse pragma and its content from JavaScript comments
  * 
  * @param {string} input
- * @return {object}
- */
-declare function pragma(input: string): object;
-
-/**
- * Parse pragma and its content from JavaScript comments(with option)
- * 
- * @param {string} input
  * @param {{parseContent?: (string) => any}} option
  * @return {object}
  */
-declare function pragma(input: string, option: { parseContent?: (source: string) => any }): object;
+declare function pragma(input: string, option?: { parseContent?: (source: string) => any }): object;
 
 export = pragma;
