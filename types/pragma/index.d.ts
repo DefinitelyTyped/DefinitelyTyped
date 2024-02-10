560fb759238e6ff4ce2b7c43af4c9775dd534357
e6nlaq <120777733+e6nlaq@users.noreply.github.com>
2024-02-11 07:09:57 +0900

    remove JSDoc

--- types/pragma/index.d.ts
+++ types/pragma/index.d.ts
@@ -1,10 +1,3 @@
-/**
- * Parse pragma and its content from JavaScript comments
- *
- * @param {string} input
- * @param {{parseContent?: (string) => any}} option
- * @return {object}
- */
 declare function pragma(input: string, option?: { parseContent?: (source: string) => any }): object;
 
 export = pragma;
