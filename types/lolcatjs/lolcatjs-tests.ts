4d539426b0d8776d6e59fc187590276ba0fb4db1
jonaahmed <23105954+jonaahmed@users.noreply.github.com>
2024-02-11 11:50:02 +0300

    Add lolcatjs tests for fromString and rainbow functions

--- types/lolcatjs/lolcatjs-tests.ts
+++ types/lolcatjs/lolcatjs-tests.ts
@@ -1,1 +1,8 @@
 import lolcatjs = require('lolcatjs');
+
+// $ExpectType void
+lolcatjs.fromString('Hello, World!');
+// $ExpectType Rgb
+lolcatjs.rainbow(4, 2);
+ // @ts-expect-error
+lolcatjs.rainbow('4', 2);
