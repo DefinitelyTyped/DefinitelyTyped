ebf3c2b4c164c57a1394bc0d0e4eba333ddfaa9c
Hannes Rüger <hannes@rueger.events>
2024-02-12 17:00:00 +0100

    fix: test

--- types/punycode/punycode-tests.ts
+++ types/punycode/punycode-tests.ts
@@ -11,12 +11,12 @@ const ucs2result2: string = punycode.ucs2.encode([1, 2, 3]);
 const version: string = punycode.version;
 
 import punycodeDirect = require("punycode/punycode.js");
-const result1Direct: string = punycode.decode("x");
-const result2Direct: string = punycode.encode("x");
-const result3Direct: string = punycode.toASCII("x");
-const result4Direct: string = punycode.toUnicode("x");
+const result1Direct: string = punycodeDirect.decode("x");
+const result2Direct: string = punycodeDirect.encode("x");
+const result3Direct: string = punycodeDirect.toASCII("x");
+const result4Direct: string = punycodeDirect.toUnicode("x");
 
-const ucs2result1Direct: number[] = punycode.ucs2.decode("x");
-const ucs2result2Direct: string = punycode.ucs2.encode([1, 2, 3]);
+const ucs2result1Direct: number[] = punycodeDirect.ucs2.decode("x");
+const ucs2result2Direct: string = punycodeDirect.ucs2.encode([1, 2, 3]);
 
-const versionDirect: string = punycode.version;
+const versionDirect: string = punycodeDirect.version;
