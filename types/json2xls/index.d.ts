7b532d1ce38a235b6f9d444e5525eae4d14d8b14
Can Gökçeaslan <48398625+cangokceaslan@users.noreply.github.com>
2024-02-17 00:38:29 +0300

    Updated and removed other functions

--- types/json2xls/index.d.ts
+++ types/json2xls/index.d.ts
@@ -1,32 +1,20 @@
+import express = require("express");
+
 type Json = string | number | boolean | { [key: string]: Json } | Json[];
 
-interface FieldConfig {
-    [key: string]: string;
-}
-
-interface Config {
-    fields?: string[] | FieldConfig;
-    style?: string;
-}
-
-declare function json2xls(json: any, options?: Config): Buffer;
+declare function json2xls(json: Json, options?: json2xls.Config): Buffer;
 
 declare namespace json2xls {
-    function transform(json: Json[], config?: Config): Buffer;
+    interface Config {
+        fields?: Record<string, string> | string[] | undefined;
+        style?: string | undefined;
+    }
 
-    function getType(obj: Json, type?: string): string;
-    function getByString(object: Json, path: string): Json | undefined;
-    function prepareJson<T = Json>(json: T[], config?: Config): T[];
     function middleware(
-        req: import("express").Request,
-        res: import("express").Response,
-        next: import("express").NextFunction,
+        req: express.Request,
+        res: express.Response,
+        next: express.NextFunction,
     ): void;
-
-
-    export { middleware };
 }
 
-// Export the main transform function as the default export
-// and still provide access to the utility functions via the namespace
 export = json2xls;
