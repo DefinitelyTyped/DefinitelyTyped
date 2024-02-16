d2e4ab03ac6a8748c59c5ee2a5cbe944b6eea773
Can Gökçeaslan <48398625+cangokceaslan@users.noreply.github.com>
2024-02-17 01:00:39 +0300

    Converted to any

--- types/json2xls/index.d.ts
+++ types/json2xls/index.d.ts
@@ -1,23 +1,6 @@
 import express = require("express");
 
-type JsonPrimitive = string | number | boolean | null;
-type JsonArray = Array<Json>;
-type JsonObject = { [key: string]: Json };
-type Json = JsonPrimitive | JsonArray | JsonObject;
-
-// Extended Types for TypeScript Context
-type JsonWithDate = Json | Date;
-type JsonWithBigInt = Json | BigInt;
-type JsonFunction = (...args: any[]) => any;
-type JsonError = Error;
-
-// Combined Extended JSON Type
-type ExtendedJson = JsonWithDate | JsonWithBigInt | JsonFunction | JsonError;
-
-// Combined Type
-type CombinedJson = Json | ExtendedJson;
-
-declare function json2xls(json: CombinedJson, options?: json2xls.Config): Buffer;
+declare function json2xls(json: any, options?: json2xls.Config): Buffer;
 
 declare namespace json2xls {
     interface Config {
