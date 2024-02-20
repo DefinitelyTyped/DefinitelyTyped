c64021d517b00d5797e213575fc2522887ca8976
Paweł Dąbrowski <dabrowskip9@gmail.com>
2024-02-20 22:56:11 +0100

    add test

--- types/react-datepicker/react-datepicker-tests.tsx
+++ types/react-datepicker/react-datepicker-tests.tsx
@@ -3,6 +3,8 @@ import { enGB } from "date-fns/locale/en-GB";
 import { enUS } from "date-fns/locale/en-US";
 import * as React from "react";
 import DatePicker, {
+    // @ts-expect-error The library is not exporting the component as a named export
+    ReactDatePicker as _MissingNamedExport, 
     CalendarContainer,
     ReactDatePickerCustomHeaderProps,
     ReactDatePickerProps,
