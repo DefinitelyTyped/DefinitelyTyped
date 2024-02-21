f323ba51e6a31927d3e1bb56aeb96e9236037178
Johannes Kvamme <johannes.kvamme@bekk.no>
2024-02-21 08:26:47 +0100

    :label: Move types to allow consumers to import them

--- types/react-elm-components/elm-types.d.ts
+++ types/react-elm-components/elm-types.d.ts
@@ -4,3 +4,13 @@ export interface Elm18Node {
 export interface Elm19Node {
     init: ({ node, flags }: { node: HTMLElement; flags: any }) => any;
 }
+
+export interface SenderPort {
+    send: (data: any) => void;
+}
+export interface ReceiverPort {
+    subscribe: (callback: (value: any) => void) => void;
+}
+export interface Ports {
+    [x: string]: SenderPort | ReceiverPort;
+}
--- types/react-elm-components/index.d.ts
+++ types/react-elm-components/index.d.ts
@@ -1,16 +1,10 @@
 import React = require("react");
 import ElmTypes = require("./elm-types");
 
-type SenderPort = (data: any) => void;
-type ReceiverPort = (callback: (value: any) => void) => void;
-interface Ports {
-    [x: string]: SenderPort | ReceiverPort;
-}
-
 declare class Elm extends React.Component<{
     src: ElmTypes.Elm18Node | ElmTypes.Elm19Node;
     flags?: Record<string, any>;
-    ports?: (ports: Ports) => any;
+    ports?: (ports: ElmTypes.Ports) => any;
 }> {}
 
 export = Elm;
