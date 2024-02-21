f5bb95f4c12cfd6f7836a6ad8d53f5c713c51e2e
taisuke.fujita <glassonion999@gmail.com>
2024-02-22 01:17:16 +0900

    format

--- types/open-location-code/open-location-code-tests.ts
+++ types/open-location-code/open-location-code-tests.ts
@@ -1,17 +1,17 @@
-import OpenLocationCode = require('open-location-code')
+import OpenLocationCode = require("open-location-code");
 
-const a = '796RWF8Q+WF'
-const area = OpenLocationCode.decode(a)
+const a = "796RWF8Q+WF";
+const area = OpenLocationCode.decode(a);
 
-console.log(`${area.latitudeCenter}, ${area.longitudeCenter}`)
-console.log(`${area.latitudeLo}, ${area.longitudeLo}`)
-console.log(`${area.latitudeHi}, ${area.longitudeHi}`)
-console.log(`${area.codeLength}`)
+console.log(`${area.latitudeCenter}, ${area.longitudeCenter}`);
+console.log(`${area.latitudeLo}, ${area.longitudeLo}`);
+console.log(`${area.latitudeHi}, ${area.longitudeHi}`);
+console.log(`${area.codeLength}`);
 
-const code = OpenLocationCode.encode(area.latitudeCenter, area.longitudeCenter, area.codeLength)
+const code = OpenLocationCode.encode(area.latitudeCenter, area.longitudeCenter, area.codeLength);
 
-console.log(code)
+console.log(code);
 
-const shorten = OpenLocationCode.shorten('87G70000+', 40.7127, -74.0059)
+const shorten = OpenLocationCode.shorten("87G70000+", 40.7127, -74.0059);
 
-console.log(shorten)
+console.log(shorten);
