063d6e1bfe6354ad3f0de20294323fb06c935be3
taisuke.fujita <glassonion999@gmail.com>
2024-02-22 01:08:45 +0900

    feat: add open-location-code types

---
+++ types/open-location-code/open-location-code-tests.ts
@@ -0,0 +1,17 @@
+import OpenLocationCode = require('open-location-code')
+
+const a = '796RWF8Q+WF'
+const area = OpenLocationCode.decode(a)
+
+console.log(`${area.latitudeCenter}, ${area.longitudeCenter}`)
+console.log(`${area.latitudeLo}, ${area.longitudeLo}`)
+console.log(`${area.latitudeHi}, ${area.longitudeHi}`)
+console.log(`${area.codeLength}`)
+
+const code = OpenLocationCode.encode(area.latitudeCenter, area.longitudeCenter, area.codeLength)
+
+console.log(code)
+
+const shorten = OpenLocationCode.shorten('87G70000+', 40.7127, -74.0059)
+
+console.log(shorten)
