f16e2402da891945a302139db9cd4044dc5c5c52
YouennPJ <youenn.pn@gmail.com>
2024-02-22 15:40:00 -0500

    feat(react-gtm-module-v2): add types

---
+++ types/react-gtm-module-v2/react-gtm-module-tests.tsx
@@ -0,0 +1,19 @@
+import TagManager from "react-gtm-module-v2";
+
+TagManager.initialize({
+    gtmId: "GTM-000000",
+});
+
+TagManager.initialize({
+    gtmId: "GTM-000000",
+    events: {},
+    auth: "",
+    preview: "",
+    dataLayer: {},
+    dataLayerName: "",
+});
+
+TagManager.dataLayer({
+    dataLayer: {},
+    dataLayerName: "",
+});
