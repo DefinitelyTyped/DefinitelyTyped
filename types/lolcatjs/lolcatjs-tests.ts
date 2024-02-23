0279f15ae43e696391e8e1f093946ef81f3bbbba
Younis-Ahmed <23105954+jonaahmed@users.noreply.github.com>
2024-02-23 16:06:09 +0300

    Add new function calls and fix type errors in lolcatjs-tests.ts

--- types/lolcatjs/lolcatjs-tests.ts
+++ types/lolcatjs/lolcatjs-tests.ts
@@ -7,6 +7,10 @@ lolcatjs.rainbow(4, '2');
 lolcatjs.colorize("a", { red: 105, green: 205, blue: 111 }); // $ExpectType void
 // @ts-expect-error
 lolcatjs.colorize("a", [ 105, 205, 111 ]); 
+lolcatjs.printlnPlain(lolcatjs.colorize, ["Hello, World!"]); // $ExpectType void
+// @ts-expect-error
+lolcatjs.printlnPlain(lolcatjs.colorize, "Hello, World!");
+
 lolcatjs.fromString("Hello, World!"); // $ExpectType void
 
 
