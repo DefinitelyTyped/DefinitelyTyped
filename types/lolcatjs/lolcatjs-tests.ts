25858cd95f9ffb4c118b35da7022dd54d4de92f6
Younis-Ahmed <23105954+jonaahmed@users.noreply.github.com>
2024-02-23 15:13:44 +0300

    Update lolcatjs tests

--- types/lolcatjs/lolcatjs-tests.ts
+++ types/lolcatjs/lolcatjs-tests.ts
@@ -1,6 +1,11 @@
 import lolcatjs = require("lolcatjs");
 
-// $ExpectType void
-lolcatjs.fromString("Hello, World!");
-// $ExpectType Rgb
-lolcatjs.rainbow(4, 2);
+
+lolcatjs.rainbow(4, 2); // $ExpectType Rgb
+// @ts-expect-error
+lolcatjs.rainbow(4, '2');
+lolcatjs.colorize("a", { red: 105, green: 205, blue: 111 }); // $ExpectType void
+
+lolcatjs.fromString("Hello, World!"); // $ExpectType void
+
+
