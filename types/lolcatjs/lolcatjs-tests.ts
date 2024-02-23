aee0449debd26fab63d07c2cf635e73b4db7183b
Younis-Ahmed <23105954+jonaahmed@users.noreply.github.com>
2024-02-23 15:16:12 +0300

    Fix type error in lolcatjs-tests.ts

--- types/lolcatjs/lolcatjs-tests.ts
+++ types/lolcatjs/lolcatjs-tests.ts
@@ -5,7 +5,8 @@ lolcatjs.rainbow(4, 2); // $ExpectType Rgb
 // @ts-expect-error
 lolcatjs.rainbow(4, '2');
 lolcatjs.colorize("a", { red: 105, green: 205, blue: 111 }); // $ExpectType void
-
+// @ts-expect-error
+lolcatjs.colorize("a", [ 105, 205, 111 ]); 
 lolcatjs.fromString("Hello, World!"); // $ExpectType void
 
 
