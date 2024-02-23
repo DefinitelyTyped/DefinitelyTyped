c6bf40c789a189b3315e5160a85a6cdc7326bba2
Younis-Ahmed <23105954+jonaahmed@users.noreply.github.com>
2024-02-23 16:09:25 +0300

    organize lolcatjs function tests calls

--- types/lolcatjs/lolcatjs-tests.ts
+++ types/lolcatjs/lolcatjs-tests.ts
@@ -1,15 +1,25 @@
 import lolcatjs = require("lolcatjs");
 
 
-lolcatjs.rainbow(4, 2); // $ExpectType Rgb
 // @ts-expect-error
 lolcatjs.rainbow(4, '2');
-lolcatjs.colorize("a", { red: 105, green: 205, blue: 111 }); // $ExpectType void
+lolcatjs.rainbow(4, 2); // $ExpectType Rgb
+
+
 // @ts-expect-error
 lolcatjs.colorize("a", [ 105, 205, 111 ]); 
-lolcatjs.printlnPlain(lolcatjs.colorize, ["Hello, World!"]); // $ExpectType void
+lolcatjs.colorize("a", { red: 105, green: 205, blue: 111 }); // $ExpectType void
+
+
 // @ts-expect-error
 lolcatjs.printlnPlain(lolcatjs.colorize, "Hello, World!");
+lolcatjs.printlnPlain(lolcatjs.colorize, ["Hello, World!"]); // $ExpectType void
+
+
+// @ts-expect-error
+lolcatjs.printlnAnimated(lolcatjs.colorize, "Hello, World!");
+lolcatjs.printlnAnimated(lolcatjs.colorize, ["Hello, World!"]); // $ExpectType void
+
 
 lolcatjs.fromString("Hello, World!"); // $ExpectType void
 
