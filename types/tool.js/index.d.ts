f1dea19fe55f230a751aa179692cef470b4b88ef
epldlnwynn <epldlnwynn@163.com>
2024-03-10 22:37:26 +0800

    bugfix

--- types/tool.js/index.d.ts
+++ types/tool.js/index.d.ts
@@ -1,4 +1,3 @@
-/// <reference types="node"/>
 
 export {}
 
@@ -48,7 +47,7 @@ declare global {
     interface Object {
 
         toJSONString(): string;
-        toJSONString(replacer?: (number | string)[] | null, space?: string | number): string;
+        toJSONString<T>(replacer?: Array<T> | null, space?: string | number): string;
         toJSONString(replacer?: (this: any, key: string, value: any) => any, space?: string | number): string;
 
     }
