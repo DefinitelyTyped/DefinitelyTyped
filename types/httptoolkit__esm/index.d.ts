61b55729300dc67cc2ccdf96e59f116a4a162c7d
Khiet Tam Nguyen <tamkhietnguyen1@gmail.com>
2024-03-14 01:21:39 +1100

    fix: copied definition from archived esm types as suggested by @jakebailey

--- types/httptoolkit__esm/index.d.ts
+++ types/httptoolkit__esm/index.d.ts
@@ -1,5 +1,30 @@
 // Project: https://github.com/httptoolkit/esm
 // Definitions by: Khiet Tam Nguyen <https://github.com/nktnet1>
 
-import esm = require("esm");
+/// <reference types="node"/>
+
+declare namespace Options {
+    interface Esm {
+        cache: boolean;
+        esModule: boolean;
+        extensions: boolean;
+        mutableNamespace: boolean;
+        namedExports: boolean;
+        paths: boolean;
+        vars: boolean;
+        dedefault: boolean;
+        topLevelReturn: boolean;
+    }
+
+    interface Options {
+        cjs: boolean | Partial<Esm>;
+        mainFields: string[];
+        mode: "auto" | "all" | "strict";
+        await: boolean;
+        force: boolean;
+        wasm: boolean;
+    }
+}
+
+declare function esm(mod: typeof module, options?: Partial<Options.Options>): typeof require;
 export = esm;
