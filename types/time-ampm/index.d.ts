de6eb7c728cfff2f05023069dfe18e4b3787037e
zhuhaojian <zhuhaojian@huawei-partners.com>
2024-03-25 15:16:27 +0800

    types/time-ampm

--- types/time-ampm/index.d.ts
+++ types/time-ampm/index.d.ts
@@ -1,8 +1,4 @@
 // Type definitions for time-ampm 2.0.0
 // Project: https://github.com/ipostol/time-ampm
-export = timeAmpm;
-export as namespace timeAmpm;
-declare namespace timeAmpm{
-    function get12(t:string):string;
-    function get24(t:string):string;
-}
+export declare function get12(t:string):string;
+export declare function get24(t:string):string;
