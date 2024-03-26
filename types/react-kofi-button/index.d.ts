6d4d7f5bf6ce737fbe7820d68efa3484db68c587
SteveJM <sjmart1uk@yagoo.co.uk>
2024-03-26 19:03:45 +0000

    Initial version of definition for react-kofi-button.

--- types/react-kofi-button/index.d.ts
+++ types/react-kofi-button/index.d.ts
@@ -1,16 +1,14 @@
-/// <reference types="react" />
-declare module 'react-kofi-button' {
-    interface KofiArgs {
-        username: string;
-        label: string;
-        title?: string | undefined;
-        preset?: "default" | "thin" | "skinny" | "circle" | "no_background" | undefined;
-        backgroundColor?: string | undefined;
-        animation?: boolean | undefined;
-    }
-    
-    
-    function KofiButton(input: KofiArgs): React.ReactElement<KofiArgs>;
-    
-    export { KofiButton };    
+import * as React from "react";
+
+interface KofiArgs {
+    username: string;
+    label: string;
+    title?: string | undefined;
+    preset?: "default" | "thin" | "skinny" | "circle" | "no_background" | undefined;
+    backgroundColor?: string | undefined;
+    animation?: boolean | undefined;
 }
+
+declare function KofiButton(input: KofiArgs): React.ReactElement<KofiArgs>;
+
+export { KofiButton };    
---
+++ types/react-kofi-button/react-kofi-button-tests.tsx
@@ -0,0 +1,13 @@
+import * as React from "react";
+import KofiButton = require("react-kofi-button");
+
+const TestObject = {
+    username: "abcdef",
+    label: "Test Object",
+    title: "This is a test object",
+    preset: "default",
+    backgroundColor: "#808080",
+    animation: false
+};
+
+<KofiButton value={TestObject}/>;
