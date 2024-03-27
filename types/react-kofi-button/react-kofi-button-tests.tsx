e3b3229b6b07704220ca464cc4b6e8c7b1c320d5
SteveJM <sjmart1uk@yagoo.co.uk>
2024-03-27 08:48:12 +0000

    Fix failing check - KofiButton props

--- types/react-kofi-button/react-kofi-button-tests.tsx
+++ types/react-kofi-button/react-kofi-button-tests.tsx
@@ -1,13 +1,11 @@
 import * as React from "react";
 import { KofiButton } from "react-kofi-button";
 
-const TestObject = {
-    username: "abcdef",
-    label: "Test Object",
-    title: "This is a test object",
-    preset: "default",
-    backgroundColor: "#808080",
-    animation: false
-};
-
-<KofiButton {...TestObject}/>;
+<KofiButton
+    username="abcdef"
+    label="Test Object"
+    title="This is a test object"
+    preset="default"
+    backgroundColor="#808080"
+    animation={false}
+/>;
