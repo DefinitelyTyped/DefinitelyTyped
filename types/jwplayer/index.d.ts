092131d5582545555a2ac55fee80f9595d66d9b2
Amo Wu <amowu@hahow.in>
2024-03-27 15:22:39 +0800

    Update jwplayer registerPlugin types
    Official documents say that these three parameters are required
    
    https://docs.jwplayer.com/players/docs/jw8-add-a-custom-or-third-party-plugin#add-and-register-a-plugin

--- types/jwplayer/index.d.ts
+++ types/jwplayer/index.d.ts
@@ -1664,7 +1664,7 @@ declare namespace jwplayer {
         playlistNext(): JWPlayer;
         playlistPrev(): JWPlayer;
         playToggle(): JWPlayer;
-        registerPlugin(id: string, target: string, jsPlugin: () => void, swfURL?: string): void;
+        registerPlugin(id: string, target: string, jsPlugin: (pluginName: string, playerMinimumVersion: string, pluginClassOrFunction: any) => void, swfURL?: string): void;
         remove(): JWPlayer;
         removeButton(id: string): JWPlayer;
         removePlaylistItemCallback(): void;
