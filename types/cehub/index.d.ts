00ed9d44669d9c0e0cf626dd80956f4f3e33973f
cehub-app <160828742+cehub-app@users.noreply.github.com>
2024-03-30 02:32:02 +0800

    Update index.d.ts

--- types/cehub/index.d.ts
+++ types/cehub/index.d.ts
@@ -1,38 +1,52 @@
-export const version: string;
 
-export interface log {
-  /**
-* Log an error message
-*/
-  error (...params: any[]): void;
 
-  /**
-   * Log a warning message
-   */
-  warn (...params: any[]): void;
+declare namespace Cehub {
 
-  /**
-   * Log an informational message
-   */
-  info (...params: any[]): void;
+  interface LogFunctions {
+    /**
+     * Log an error message
+     */
+    error (...params: any[]): void;
 
-  /**
-   * Log a verbose message
-   */
-  verbose (...params: any[]): void;
+    /**
+     * Log a warning message
+     */
+    warn (...params: any[]): void;
 
-  /**
-   * Log a debug message
-   */
-  debug (...params: any[]): void;
+    /**
+     * Log an informational message
+     */
+    info (...params: any[]): void;
 
-  /**
-   * Log a silly message
-   */
-  silly (...params: any[]): void;
+    /**
+     * Log a verbose message
+     */
+    verbose (...params: any[]): void;
 
-  /**
-   * Shortcut to info
-   */
-  log (...params: any[]): void;
+    /**
+     * Log a debug message
+     */
+    debug (...params: any[]): void;
+
+    /**
+     * Log a silly message
+     */
+    silly (...params: any[]): void;
+
+    /**
+     * Shortcut to info
+     */
+    log (...params: any[]): void;
+  }
+
+  const log: Log
+  type Log = Cehub.LogFunctions;
+
+  const version: string;
 }
+
+declare module 'cehub' {
+  export = Cehub;
+}
+
+
