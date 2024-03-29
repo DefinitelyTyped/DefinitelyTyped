9fc47871a8d6f9117a4f35c5be241699870b2d93
cehub-app <160828742+cehub-app@users.noreply.github.com>
2024-03-30 00:31:22 +0800

    Create index.d.ts

---
+++ types/cehub/index.d.ts
@@ -0,0 +1,79 @@
+
+interface LogFunctions {
+  /**
+   * Log an error message
+   */
+  error (...params: any[]): void;
+
+  /**
+   * Log a warning message
+   */
+  warn (...params: any[]): void;
+
+  /**
+   * Log an informational message
+   */
+  info (...params: any[]): void;
+
+  /**
+   * Log a verbose message
+   */
+  verbose (...params: any[]): void;
+
+  /**
+   * Log a debug message
+   */
+  debug (...params: any[]): void;
+
+  /**
+   * Log a silly message
+   */
+  silly (...params: any[]): void;
+
+  /**
+   * Shortcut to info
+   */
+  log (...params: any[]): void;
+}
+
+
+declare module "cehub" {
+  export const version: string;
+
+  export interface log {
+    /**
+  * Log an error message
+  */
+    error (...params: any[]): void;
+
+    /**
+     * Log a warning message
+     */
+    warn (...params: any[]): void;
+
+    /**
+     * Log an informational message
+     */
+    info (...params: any[]): void;
+
+    /**
+     * Log a verbose message
+     */
+    verbose (...params: any[]): void;
+
+    /**
+     * Log a debug message
+     */
+    debug (...params: any[]): void;
+
+    /**
+     * Log a silly message
+     */
+    silly (...params: any[]): void;
+
+    /**
+     * Shortcut to info
+     */
+    log (...params: any[]): void;
+  }
+}
