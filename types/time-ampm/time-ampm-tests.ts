cb2f0ed5abc9b870e38e304e72809fbb1e5ae12f
zhuhaojian <zhuhaojian@huawei-partners.com>
2024-03-29 10:45:14 +0800

    types/time-ampm

--- types/time-ampm/time-ampm-tests.ts
+++ types/time-ampm/time-ampm-tests.ts
@@ -1,6 +1,5 @@
 import { get12, get24 } from "time-ampm";
 
-let t1 = "6";
-let time1 = get12(t1); // 6 am
-let t2 = "6 am";
-let time2 = get24(t2); // 6
+let time1 = get12("6"); // 6 am
+
+let time2 = get24("6 am"); // 6
