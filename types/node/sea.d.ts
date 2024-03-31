d2a5cb07e1441a40859b9376edd7b3b56cf4f97c
Victor Turansky <victor.turansky@gmail.com>
2024-03-31 23:18:53 +0300

    [node] `node:sea`

--- types/node/sea.d.ts
+++ types/node/sea.d.ts
@@ -1,13 +1,10 @@
 declare module "node:sea" {
-    type AssetKey = string
-
-    export function isSea(): boolean
-
-    export function getAsset(key: AssetKey): ArrayBuffer
-
-    export function getAsset(key: AssetKey, encoding: string): string
-
-    export function getAssetAsBlob(key: AssetKey, options?: { type: string }): Blob
-
-    export function getRawAsset(key: AssetKey): string | ArrayBuffer
+    type AssetKey = string;
+    export function isSea(): boolean;
+    export function getAsset(key: AssetKey): ArrayBuffer;
+    export function getAsset(key: AssetKey, encoding: string): string;
+    export function getAssetAsBlob(key: AssetKey, options?: {
+        type: string;
+    }): Blob;
+    export function getRawAsset(key: AssetKey): string | ArrayBuffer;
 }
