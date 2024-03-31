0126b65343ca5924bf5b9b74b26aed8a288d75ad
Victor Turansky <victor.turansky@gmail.com>
2024-04-01 00:37:35 +0300

    [node] `node:sea`

--- types/node/sea.d.ts
+++ types/node/sea.d.ts
@@ -116,10 +116,34 @@
 declare module "node:sea" {
     type AssetKey = string;
     export function isSea(): boolean;
+    /**
+     * This method can be used to retrieve the assets configured to be bundled into the
+     * single-executable application at build time.
+     * An error is thrown when no matching asset can be found.
+     * @since v20.12.0
+     */
     export function getAsset(key: AssetKey): ArrayBuffer;
     export function getAsset(key: AssetKey, encoding: string): string;
+    /**
+     * Similar to `sea.getAsset()`, but returns the result in a [`Blob`](https://developer.mozilla.org/en-US/docs/Web/API/Blob).
+     * An error is thrown when no matching asset can be found.
+     * @since v20.12.0
+     */
     export function getAssetAsBlob(key: AssetKey, options?: {
         type: string;
     }): Blob;
+    /**
+     * This method can be used to retrieve the assets configured to be bundled into the
+     * single-executable application at build time.
+     * An error is thrown when no matching asset can be found.
+     *
+     * Unlike `sea.getRawAsset()` or `sea.getAssetAsBlob()`, this method does not
+     * return a copy. Instead, it returns the raw asset bundled inside the executable.
+     *
+     * For now, users should avoid writing to the returned array buffer. If the
+     * injected section is not marked as writable or not aligned properly,
+     * writes to the returned array buffer is likely to result in a crash.
+     * @since v20.12.0
+     */
     export function getRawAsset(key: AssetKey): string | ArrayBuffer;
 }
