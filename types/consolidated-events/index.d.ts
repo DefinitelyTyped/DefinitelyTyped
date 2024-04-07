400bf5f26fd81d189c558f68a5c7ed6a1f14e938
Matt Wade <matthew.wade@sproutsocial.com>
2024-04-07 16:51:24 -0500

    [consolidated-events] Add types for consolidated-events

---
+++ types/consolidated-events/index.d.ts
@@ -0,0 +1,13 @@
+type EventMap = GlobalEventHandlersEventMap;
+type EventMapKeys =  keyof EventMap;
+
+export function addEventListener<TEventName extends EventMapKeys | string>(
+  target: EventTarget,
+  eventName: TEventName,
+  listener: (ev: TEventName extends EventMapKeys ? EventMap[TEventName] : Event) => any,
+  options?: boolean | AddEventListenerOptions,
+): () => void;
+
+
+// Disable automatic exports for helper types above
+export {};
