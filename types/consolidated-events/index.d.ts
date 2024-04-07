624cf8368b4baa75a7dd8758993fe86f2d0b890c
Matt Wade <matthew.wade@sproutsocial.com>
2024-04-07 16:59:51 -0500

    Oops format

--- types/consolidated-events/index.d.ts
+++ types/consolidated-events/index.d.ts
@@ -1,13 +1,12 @@
 type EventMap = GlobalEventHandlersEventMap;
-type EventMapKeys =  keyof EventMap;
+type EventMapKeys = keyof EventMap;
 
 export function addEventListener<TEventName extends EventMapKeys | string>(
-  target: EventTarget,
-  eventName: TEventName,
-  listener: (ev: TEventName extends EventMapKeys ? EventMap[TEventName] : Event) => any,
-  options?: boolean | AddEventListenerOptions,
+    target: EventTarget,
+    eventName: TEventName,
+    listener: (event: TEventName extends EventMapKeys ? EventMap[TEventName] : Event) => any,
+    options?: boolean | AddEventListenerOptions,
 ): () => void;
-
 
 // Disable automatic exports for helper types above
 export {};
