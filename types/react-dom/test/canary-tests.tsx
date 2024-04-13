cdfcbec8bedf0a3c3cde2b181b9f26e20ca42770
eps1lon <sebastian.silbermann@vercel.com>
2024-04-13 11:42:17 +0200

    [react-dom] Upgrade `requestFormReset` to stable

--- types/react-dom/test/canary-tests.tsx
+++ types/react-dom/test/canary-tests.tsx
@@ -2,11 +2,3 @@
 import React = require("react");
 import ReactDOM = require("react-dom");
 import ReactDOMClient = require("react-dom/client");
-
-function requestFormResetTest(form: HTMLFormElement, button: HTMLButtonElement) {
-    ReactDOM.requestFormReset(form);
-    // @ts-expect-error
-    ReactDOM.requestFormReset(button);
-    // @ts-expect-error
-    ReactDOM.requestFormReset(null);
-}
