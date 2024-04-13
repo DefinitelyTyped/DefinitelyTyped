f7730e3a69ed2f43bd990ab69bad23b0da742c41
huseeiin <122984423+huseeiin@users.noreply.github.com>
2024-04-13 16:15:33 +0300

    Update index.d.ts
    this fixes:
    ```ts
    import cookie from "cookie";
    
    console.log(cookie.parse("hello=world").world);
    ```
    now `world` is `string | undefined` instead of always `string`

--- types/cookie/index.d.ts
+++ types/cookie/index.d.ts
@@ -141,7 +141,7 @@ export interface CookieParseOptions {
  * @param str the string representing a `Cookie` header value
  * @param [options] object containing parsing options
  */
-export function parse(str: string, options?: CookieParseOptions): Record<string, string>;
+export function parse(str: string, options?: CookieParseOptions): Record<string, string | undefined>;
 
 /**
  * Serialize a cookie name-value pair into a `Set-Cookie` header string.
