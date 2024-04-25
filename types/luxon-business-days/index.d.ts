3193f007b1ef9a6d9cdf4e99217f6d73ef0c5d9b
Bradley C Bailey <brad@memoryleak.org>
2024-04-25 16:25:55 -0600

    Formatted

--- types/luxon-business-days/index.d.ts
+++ types/luxon-business-days/index.d.ts
@@ -1,36 +1,40 @@
-import { DateTime } from 'luxon'
+import { DateTime } from "luxon";
 
-export type BusinessDayNumbers = 1 | 2 | 3 | 4 | 5 | 6 | 7
-export type BusinessDays = BusinessDayNumbers[]
+export type BusinessDayNumbers = 1 | 2 | 3 | 4 | 5 | 6 | 7;
+export type BusinessDays = BusinessDayNumbers[];
 
-export type HolidayMatcher = (inst: DateTime<boolean>) => boolean
-export interface HolidayMatchersDict { [key: string]: HolidayMatcher }
+export type HolidayMatcher = (inst: DateTime<boolean>) => boolean;
+export interface HolidayMatchersDict {
+    [key: string]: HolidayMatcher;
+}
 
-export type HolidayHelper = (...args: any[]) => any
-export interface HolidayHelpersDict { [key: string]: HolidayHelper }
+export type HolidayHelper = (...args: any[]) => any;
+export interface HolidayHelpersDict {
+    [key: string]: HolidayHelper;
+}
 
-declare module 'luxon' {
+declare module "luxon" {
     interface DateTime<IsValid extends boolean> {
-        holidayHelpers: HolidayHelper[] | undefined
-        holidayMatchers: HolidayMatcher[] | undefined
-        availableHolidayHelpers: HolidayHelpersDict
-        availableHolidayMatchers: HolidayMatchersDict
+        holidayHelpers: HolidayHelper[] | undefined;
+        holidayMatchers: HolidayMatcher[] | undefined;
+        availableHolidayHelpers: HolidayHelpersDict;
+        availableHolidayMatchers: HolidayMatchersDict;
 
         setupBusiness(opts?: {
-            businessDays?: BusinessDays,
-            holidayMatchers?: HolidayMatcher[]
-        }): void
+            businessDays?: BusinessDays;
+            holidayMatchers?: HolidayMatcher[];
+        }): void;
 
-        clearBusinessSetup(): void
+        clearBusinessSetup(): void;
 
-        isHoliday(...args: any[]): boolean
+        isHoliday(...args: any[]): boolean;
 
-        isBusinessDay(): boolean
+        isBusinessDay(): boolean;
 
-        plusBusiness(opts?: { days?: number }): this
+        plusBusiness(opts?: { days?: number }): this;
 
-        minusBusiness(opts?: { days?: number }): this
+        minusBusiness(opts?: { days?: number }): this;
     }
 }
 
-export { DateTime }
+export { DateTime };
--- types/luxon-business-days/luxon-business-days-tests.ts
+++ types/luxon-business-days/luxon-business-days-tests.ts
@@ -1,28 +1,28 @@
-import { DateTime } from 'luxon-business-days'
+import { DateTime } from "luxon-business-days";
 
-const now = DateTime.now() // $ExpectType DateTime<true>
+const now = DateTime.now(); // $ExpectType DateTime<true>
 
-now.isBusinessDay() // $ExpectType boolean
-now.isHoliday() // $ExpectType boolean
-now.isHoliday('args') // $ExpectType boolean
-
-// @ts-expect-error
-now.plusBusiness(5)
-now.plusBusiness() // $ExpectType DateTime<true>
-now.plusBusiness({ days: 1 }) // $ExpectType DateTime<true>
+now.isBusinessDay(); // $ExpectType boolean
+now.isHoliday(); // $ExpectType boolean
+now.isHoliday("args"); // $ExpectType boolean
 
 // @ts-expect-error
-now.minusBusiness(1)
-now.minusBusiness() // $ExpectType DateTime<true>
-now.minusBusiness({ days: 1 }) // $ExpectType DateTime<true>
+now.plusBusiness(5);
+now.plusBusiness(); // $ExpectType DateTime<true>
+now.plusBusiness({ days: 1 }); // $ExpectType DateTime<true>
 
-now.setupBusiness() // $ExpectType void
-now.setupBusiness({ businessDays: [ 1, 2, 3 ] }) // $ExpectType void
-now.setupBusiness({ holidayMatchers: [ (d: DateTime) => false ] }) // $ExpectType void
-now.setupBusiness({ businessDays: [ 1, 2, 3 ], holidayMatchers: [ (d: DateTime) => false ] }) // $ExpectType void
-now.clearBusinessSetup() // $ExpectType void
+// @ts-expect-error
+now.minusBusiness(1);
+now.minusBusiness(); // $ExpectType DateTime<true>
+now.minusBusiness({ days: 1 }); // $ExpectType DateTime<true>
 
-now.holidayHelpers // $ExpectType HolidayHelper[] | undefined
-now.holidayMatchers // $ExpectType HolidayMatcher[] | undefined
-now.availableHolidayHelpers // $ExpectType HolidayHelpersDict
-now.availableHolidayMatchers // $ExpectType HolidayMatchersDict
+now.setupBusiness(); // $ExpectType void
+now.setupBusiness({ businessDays: [1, 2, 3] }); // $ExpectType void
+now.setupBusiness({ holidayMatchers: [(d: DateTime) => false] }); // $ExpectType void
+now.setupBusiness({ businessDays: [1, 2, 3], holidayMatchers: [(d: DateTime) => false] }); // $ExpectType void
+now.clearBusinessSetup(); // $ExpectType void
+
+now.holidayHelpers; // $ExpectType HolidayHelper[] | undefined
+now.holidayMatchers; // $ExpectType HolidayMatcher[] | undefined
+now.availableHolidayHelpers; // $ExpectType HolidayHelpersDict
+now.availableHolidayMatchers; // $ExpectType HolidayMatchersDict
