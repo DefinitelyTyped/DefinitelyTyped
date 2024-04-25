d410c25608f2b21feb398bf51ba08132f6baeff7
Bradley C Bailey <brad@memoryleak.org>
2024-04-25 16:01:22 -0600

    Updated tests

--- types/luxon-business-days/luxon-business-days-tests.ts
+++ types/luxon-business-days/luxon-business-days-tests.ts
@@ -1,23 +1,16 @@
 import { DateTime } from 'luxon-business-days'
 
-// import type {
-//     HolidayMatcher,
-//     HolidayMatchersDict,
-//     HolidayHelper,
-//     HolidayHelpersDict
-// } from 'luxon-business-days'
-
 const now = DateTime.now() // $ExpectType DateTime<true>
 now.isBusinessDay() // $ExpectType boolean
 now.isHoliday() // $ExpectType boolean
 
-// now.plusBusiness({ days: 1 }) // $ExpectType DateTime<true>
-// now.minusBusiness({ days: 1 }) // $ExpectType DateTime<true>
+now.plusBusiness({ days: 1 }) // $ExpectType DateTime<true>
+now.minusBusiness({ days: 1 }) // $ExpectType DateTime<true>
 
-// now.setupBusiness({ businessDays: [ 1, 2, 3 ] }) // $ExpectType void
-// now.clearBusinessSetup() // $ExpectType void
+now.setupBusiness({ businessDays: [ 1, 2, 3 ] }) // $ExpectType void
+now.clearBusinessSetup() // $ExpectType void
 
-// now.holidayHelpers // $ExpectType HolidayHelper[] | undefined
-// now.holidayMatchers // $ExpectType HolidayMatcher[] | undefined
-// now.availableHolidayHelpers // $ExpectType HolidayHelpersDict
-// now.availableHolidayMatchers // $ExpectType HolidayMatchersDict
+now.holidayHelpers // $ExpectType HolidayHelper[] | undefined
+now.holidayMatchers // $ExpectType HolidayMatcher[] | undefined
+now.availableHolidayHelpers // $ExpectType HolidayHelpersDict
+now.availableHolidayMatchers // $ExpectType HolidayMatchersDict
