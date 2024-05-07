f43b0e3624b5fa7498f810c8fd6523dbb7c1472e
Giorgi Kakhoshvili <giorgi.kakhoshvili@rebilly.com>
2024-05-07 15:14:43 +0200

    fix export

--- types/approximate-number/index.d.ts
+++ types/approximate-number/index.d.ts
@@ -1,17 +1,19 @@
-export interface Options {
-    separator?: string | boolean;
-    decimal?: string | boolean;
-    round?: boolean;
-    min10k?: boolean;
-    prefix?: string;
-    suffix?: string;
-    capital?: boolean;
-    precision?: number | undefined;
+declare namespace approximateNumber {
+    interface Options {
+        separator?: string | boolean;
+        decimal?: string | boolean;
+        round?: boolean;
+        min10k?: boolean;
+        prefix?: string;
+        suffix?: string;
+        capital?: boolean;
+        precision?: number | undefined;
+    }
 }
 
 declare function approximateNumber(
     input: number | string,
-    options?: Options,
+    options?: approximateNumber.Options,
 ): string;
 
 export = approximateNumber
