7e0e50f8e0efc55a00d4b3ec2138941e7ff052df
Zyb Jared Valdez <zybjared@gmail.com>
2024-05-10 16:31:19 +0800

    initial commit

---
+++ types/mastercard-client-encryption/index.d.ts
@@ -0,0 +1,176 @@
+export interface DecryptOptions {
+    body: unknown;
+    request: {
+        url: string
+    };
+}
+
+export type PublicKeyFingerprintType =
+    | 'certificate'
+    | 'publicKey'
+
+export type DataEncodingType =
+    | 'base64'
+    | 'hex'
+
+export interface RootMapping {
+    element: string,
+    obj: string
+}
+
+export interface RootMappingNode {
+    path: string,
+    toEncrypt: RootMapping[] | [],
+    toDecrypt: RootMapping[] | [],
+}
+
+export interface JweEncryptionConfiguration {
+    mode: string,
+    encryptionCertificate: unknown,
+    encryptedValueFieldName: unknown,
+    paths: RootMappingNode[],
+    publicKeyFingerprintType?: PublicKeyFingerprintType
+    publicKeyFingerprint?: PublicKeyFingerprintType extends 'certificate'
+        ? DataEncodingType
+        : unknown,
+    privateKey?: unknown,
+    keyStore?: unknown,
+    keyStoreAlias?: unknown,
+    keyStorePassword?: unknown,
+}
+
+/**
+ * @classdesc
+ * Performs JWE encryption on HTTP payloads.
+ *
+ * @module encryption/JweEncryption
+ * @version 1.0.0
+ */
+export class JweEncryption {
+    constructor(encryptionConfig: JweEncryptionConfiguration);
+    /**
+     * Encrypt parts of a HTTP request using the given config
+     *
+     * @param endpoint HTTP URL for the current call
+     * @param header HTTP Header
+     * @param body HTTP Body
+     * @returns {{header: *, body: *}}
+     */
+    encrypt: <T>(
+        url: string,
+        headers: Record<string, string | undefined>,
+        body: T
+    ) => {
+        headers: Record<string, string | undefined>,
+        body: T
+    }
+
+    /**
+     * Decrypt part of the HTTP response using the given config
+     *
+     * @param response HTTP response to decrypt
+     * @returns {*}
+     */
+    decrypt: <T extends DecryptOptions>(response: T) => T['body']
+}
+
+export namespace EncryptionUtils {
+    /**
+     * Utils module
+     * @version 1.0.0
+     */
+    function isSet(value: unknown): boolean
+
+    /**
+     * Converts a 'binary' encoded string of bytes to (hex or base64) encoded string.
+     *
+     * @param bytes Bytes to convert
+     * @param dataEncoding encoding to use (hex or base64)
+     * @returns {string} encoded string
+     */
+    function bytesToString(
+        bytes: string,
+        dataEncoding: Lowercase<string>
+    ): string
+
+    /**
+     * Converts a (hex or base64) string into a 'binary' encoded string of bytes.
+     *
+     * @param value string to convert
+     * @param dataEncoding encoding to use (hex or base64)
+     * @returns binary encoded string of bytes
+     */
+    function stringToBytes(
+        value: string,
+        dataEncoding: Lowercase<string>
+    ): string
+
+    function toByteArray(
+        value: string,
+        fromFormat: string
+    ): Buffer
+
+    type JSONPrimitive = string | number | boolean | null | undefined;
+
+    type JSONValue = JSONPrimitive | JSONValue[] | {
+        [key: string]: JSONValue;
+    };
+
+    type NotAssignableToJson =
+            | bigint
+            | symbol;
+
+    type JSONCompatible<T> = unknown extends T ? never : {
+            [P in keyof T]:
+                    T[P] extends JSONValue ? T[P] :
+                    T[P] extends NotAssignableToJson ? never :
+                    JSONCompatible<T[P]>;
+    };
+
+    /**
+     * Convert a json object or json string to string
+     *
+     * @param {Object|string} data Json string or Json obj
+     * @returns {string}
+     */
+    function jsonToString(
+        data: unknown
+    ): string
+
+    /**
+     * Convert Json string to Json object if it is a valid Json
+     * Return back input string if it is not a Json
+     *
+     * @param {string} Json string or string
+     * @returns {Object|string}
+     */
+    function stringToJson(
+        data: string
+    ): unknown
+
+    interface PrivateKeyConfig {
+        privateKey: JweEncryptionConfiguration['privateKey'],
+        keyStore?: never,
+        keyStoreAlias?: never,
+        keyStorePassword?: never
+    }
+
+    interface KeystoreConfig {
+        privateKey?: never,
+        keyStore: JweEncryptionConfiguration['keyStore'],
+        keyStoreAlias: JweEncryptionConfiguration['keyStoreAlias'],
+        keyStorePassword: JweEncryptionConfiguration['keyStorePassword']
+    }
+
+    type getPrivateKeyConfig =
+        | PrivateKeyConfig
+        | KeystoreConfig
+
+    function getPrivateKey(
+        config: getPrivateKeyConfig
+    ):
+    | import('node-forge').pki.rsa.PrivateKey
+    | import('node-forge').pki.PrivateKey
+    | null
+    | undefined
+}
