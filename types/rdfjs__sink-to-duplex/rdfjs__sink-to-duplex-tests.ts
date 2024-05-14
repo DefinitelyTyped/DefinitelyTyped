082789abb1ac9b6793431dea7f2be89f1dbe0620
Tomasz Pluskiewicz <tpluscode@users.noreply.github.com>
2024-05-14 12:55:15 +0200

    simplify tests

--- types/rdfjs__sink-to-duplex/rdfjs__sink-to-duplex-tests.ts
+++ types/rdfjs__sink-to-duplex/rdfjs__sink-to-duplex-tests.ts
@@ -1,13 +1,14 @@
-import N3Parser from "@rdfjs/parser-n3";
-import NTriplesSerializer from "@rdfjs/serializer-ntriples";
+import { Sink } from "@rdfjs/types";
 import { Duplexify } from "duplexify";
 import fs from "fs";
 import sinkToDuplex = require("@rdfjs/sink-to-duplex");
 
+const sink: Sink<any, any> = <any> {};
+
 const input = fs.createReadStream("test.ttl");
-const parser = sinkToDuplex(new N3Parser(), { readableObjectMode: true });
-const serializer = sinkToDuplex(new NTriplesSerializer(), { writableObjectMode: true });
+const parser = sinkToDuplex(sink, { readableObjectMode: true });
+const serializer = sinkToDuplex(sink, { writableObjectMode: true });
 
 input.pipe(parser).pipe(serializer).pipe(process.stdout);
 
-const noOptions: Duplexify = sinkToDuplex(new N3Parser());
+const noOptions: Duplexify = sinkToDuplex(sink);
