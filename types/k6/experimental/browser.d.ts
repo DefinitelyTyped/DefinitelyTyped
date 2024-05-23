e2b18eb560bbbee97eadb0fcdf81e0b0e660aab5
Johan Suleiko Allansson <allansson@gmail.com>
2024-05-23 15:51:59 +0200

    Wrap return types of Keyboard with Promise

--- types/k6/experimental/browser.d.ts
+++ types/k6/experimental/browser.d.ts
@@ -1840,14 +1840,14 @@ export interface Keyboard {
      * A superset of the key values can be found [here](https://developer.mozilla.org/en-US/docs/Web/API/UI_Events/Keyboard_event_key_values).
      * @param key Name of key to press, such as `ArrowLeft`.
      */
-    down(key: string): void;
+    down(key: string): Promise<void>;
 
     /**
      * Dispatches an `input` event with the given `text`.
      * This method does not emit `keyDown`, `keyUp` or `keyPress` events.
      * @param text Event text.
      */
-    insertText(text: string): void;
+    insertText(text: string): Promise<void>;
 
     /**
      * Sends a key press message to a session target.
@@ -1855,7 +1855,7 @@ export interface Keyboard {
      * @param key Sequence of keys to press.
      * @param options Specifies the typing options.
      */
-    press(key: string, options?: { delay?: number }): void;
+    press(key: string, options?: { delay?: number }): Promise<void>;
 
     /**
      * Type sends a `press` message to a session target for each character in text.
@@ -1865,14 +1865,14 @@ export interface Keyboard {
      * @param text A text to type into a focused element.
      * @param options Specifies the typing options.
      */
-    type(text: string, options?: { delay?: number }): void;
+    type(text: string, options?: { delay?: number }): Promise<void>;
 
     /**
      * Sends a key up message to a session target.
      * A superset of the key values can be found [here](https://developer.mozilla.org/en-US/docs/Web/API/UI_Events/Keyboard_event_key_values).
      * @param key Name of key to release, such as `ArrowLeft`.
      */
-    up(key: string): void;
+    up(key: string): Promise<void>;
 }
 
 /**
