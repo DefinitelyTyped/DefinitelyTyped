bb5d583058d0a1b0cd1c7029aecc8652de239e5a
Mirjana Krneta Vodenik <mirjana.krnetavodenik@tui.com>
2024-05-24 21:50:46 +0200

    Refactor PRNG module exports

--- types/prng/index.d.ts
+++ types/prng/index.d.ts
@@ -1,19 +1,17 @@
-declare module 'prng' {
-    export {};
+export {};
 
-    type PRNGFunction = (() => number) & {
-        quick: () => number;
-        double: () => number;
-        int32: () => number;
-    };
+type PRNGFunction = (() => number) & {
+    quick: () => number;
+    double: () => number;
+    int32: () => number;
+};
 
-    export type PRNGAlgorithm = (seed?: string) => PRNGFunction;
+export type PRNGAlgorithm = (seed?: string) => PRNGFunction;
 
-    export const prng_alea: PRNGAlgorithm;
-    export const prng_arc4: PRNGAlgorithm;
-    export const prng_tychei: PRNGAlgorithm;
-    export const prng_xor128: PRNGAlgorithm;
-    export const prng_xor4096: PRNGAlgorithm;
-    export const prng_xorshift7: PRNGAlgorithm;
-    export const prng_xorwow: PRNGAlgorithm;
-}
+export const prng_alea: PRNGAlgorithm;
+export const prng_arc4: PRNGAlgorithm;
+export const prng_tychei: PRNGAlgorithm;
+export const prng_xor128: PRNGAlgorithm;
+export const prng_xor4096: PRNGAlgorithm;
+export const prng_xorshift7: PRNGAlgorithm;
+export const prng_xorwow: PRNGAlgorithm;
