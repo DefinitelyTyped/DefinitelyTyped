da4f5c9c645fe7a4f39de85ca1906ae0f1fec2e1
vstefanovic97 <stefanoviv@ae.com>
2024-05-27 13:15:59 +0200

    Add template import type for uniqueId helper

--- types/ember__helper/ember__helper-tests.ts
+++ types/ember__helper/ember__helper-tests.ts
@@ -1,10 +1,11 @@
-import { array, capabilities, concat, fn, get, hash, invokeHelper, setHelperManager } from "@ember/helper";
+import { array, capabilities, concat, fn, get, hash, invokeHelper, setHelperManager, uniqueId } from "@ember/helper";
 
 array; // $ExpectType ArrayHelper
 concat; // $ExpectType ConcatHelper
 fn; // $ExpectType FnHelper
 get; // $ExpectType GetHelper
 hash; // $ExpectType HashHelper
+uniqueId; // $ExpectType UniqueIdHelper
 invokeHelper; // $ExpectType (context: object, definition: object, computeArgs?: ((context: object) => Partial<Arguments>) | undefined) => Cache<unknown>
 capabilities; //  $ExpectType <O extends object, T extends object>(factory: ManagerFactory<O | undefined, HelperManager<unknown>>, obj: T) => T
 setHelperManager; // $ExpectType <O extends object, T extends object>(factory: ManagerFactory<O | undefined, HelperManager<unknown>>, obj: T) => T
