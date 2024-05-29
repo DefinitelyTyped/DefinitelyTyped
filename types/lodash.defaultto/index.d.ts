8e92b806845e00b8f22d2f6762616950401b0ede
Arkness <mariotti.giorgio@outlook.it>
2024-05-29 13:37:39 +0000

    fix(lodash.defaultto): update export

--- types/lodash.defaultto/index.d.ts
+++ types/lodash.defaultto/index.d.ts
@@ -1,4 +1,4 @@
 // Generated from https://github.com/DefinitelyTyped/DefinitelyTyped/blob/master/types/lodash/scripts/generate-modules.ts
 
-import { defaultto } from "lodash";
-export = defaultto;
+import { defaultTo } from "lodash";
+export = defaultTo;
