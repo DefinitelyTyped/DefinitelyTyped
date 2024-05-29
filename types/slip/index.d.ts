095b5e9712d1866d8ff00b09153ce3ead289211b
Finomnis <finomnis@gmail.com>
2024-05-29 20:37:48 +0200

    Fix default export

--- types/slip/index.d.ts
+++ types/slip/index.d.ts
@@ -1,30 +1,27 @@
-export default slip;
-declare namespace slip {
-    const END: number;
-    const ESC: number;
-    const ESC_END: number;
-    const ESC_ESC: number;
+export const END: number;
+export const ESC: number;
+export const ESC_END: number;
+export const ESC_ESC: number;
 
-    interface EncodeOptions {
-        bufferPadding?: number;
-        offset?: number;
-        byteLength?: number;
-    }
+export interface EncodeOptions {
+    bufferPadding?: number;
+    offset?: number;
+    byteLength?: number;
+}
 
-    interface DecodeOptions {
-        bufferSize?: number;
-        maxMessageSize?: number;
-        onMessage?: (msg: Uint8Array) => void;
-        onError?: (msgBuffer: Uint8Array, errorMsg: string) => void;
-    }
+export interface DecodeOptions {
+    bufferSize?: number;
+    maxMessageSize?: number;
+    onMessage?: (msg: Uint8Array) => void;
+    onError?: (msgBuffer: Uint8Array, errorMsg: string) => void;
+}
 
-    function encode(
-        data: ArrayLike<number> | ArrayBuffer,
-        o?: EncodeOptions,
-    ): Uint8Array;
+export function encode(
+    data: ArrayLike<number> | ArrayBuffer,
+    o?: EncodeOptions,
+): Uint8Array;
 
-    class Decoder {
-        constructor(o?: DecodeOptions);
-        decode(data: ArrayLike<number> | ArrayBuffer): void;
-    }
+export class Decoder {
+    constructor(o?: DecodeOptions);
+    decode(data: ArrayLike<number> | ArrayBuffer): void;
 }
