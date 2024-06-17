f33d06aeaf59b7bf5035060790783f3c234b5ae2
Dorian Latchague <dlatchague@microsoft.com>
2024-06-17 15:31:31 -0700

    added webpack-react-component-name type definition

---
+++ types/webpack-react-component-name/webpack-react-component-name-tests.ts
@@ -0,0 +1,28 @@
+import WebpackReactComponentNamePlugin, { type  Options, type PathMatch } from "webpack-react-component-name";
+import { Configuration, WebpackPluginInstance } from "webpack";
+
+const plugin: WebpackPluginInstance = new WebpackReactComponentNamePlugin();
+
+const plugin2: WebpackPluginInstance = new WebpackReactComponentNamePlugin(undefined);
+
+const plugin3: WebpackPluginInstance = new WebpackReactComponentNamePlugin({});
+
+const manyOptions: Options[] = [{},{
+    parseDependencies: undefined,
+    include: undefined,
+    exclude: undefined,
+}, {
+    parseDependencies: false,
+}, {
+    parseDependencies: true,
+}, {
+    include: [],
+}, {
+    exclude: []
+}];
+
+const pathMatches: PathMatch[] = ["test", /test/, (path: string) => path === "test"];
+
+const configuration: Configuration = {
+    plugins: [plugin]
+}
