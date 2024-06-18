6db25fada7ca425299db651007343fb9305e2481
Jake Bailey <5341706+jakebailey@users.noreply.github.com>
2024-06-18 12:33:57 -0700

    [@types/ember__helper] Fix failing test in 5.5

--- types/ember__helper/ember__helper-tests.ts
+++ types/ember__helper/ember__helper-tests.ts
@@ -7,4 +7,4 @@ get; // $ExpectType GetHelper
 hash; // $ExpectType HashHelper
 invokeHelper; // $ExpectType (context: object, definition: object, computeArgs?: ((context: object) => Partial<Arguments>) | undefined) => Cache<unknown>
 capabilities; //  $ExpectType <O extends object, T extends object>(factory: ManagerFactory<O | undefined, HelperManager<unknown>>, obj: T) => T
-setHelperManager; // $ExpectType <O extends object, T extends object>(factory: ManagerFactory<O | undefined, HelperManager<unknown>>, obj: T) => T
+setHelperManager; // $ExpectType <O extends object, T extends object>(factory: ManagerFactory<O | undefined, HelperManager<unknown>>, obj: T) => T || <O extends Owner, T extends object>(factory: ManagerFactory<O | undefined, HelperManager<unknown>>, obj: T) => T
