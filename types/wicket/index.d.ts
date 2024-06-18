46b5bc837debe33430c23e4359664472822fc920
Bailey Lissington <bailey.lissington@m2xgroup.com>
2024-06-17 20:52:15 -0600

    chore: export wicket utility types

--- types/wicket/index.d.ts
+++ types/wicket/index.d.ts
@@ -1,46 +1,48 @@
-declare var Wkt: WktModule.WktStatic;
+export {};
 
-declare namespace WktModule {
-    interface WktStatic {
-        new(obj?: any): Wkt;
-        beginsWith(str: string, sub: string): boolean;
-        endsWith(str: string, sub: string): boolean;
-        delimiter: string;
-        isArray(obj: any): boolean;
-        trim(str: string, sub: string): string;
-        Wkt: typeof Wkt;
-    }
+declare class WktClass {
+    constructor();
+    delimiter: string;
+    wrapVertices: string;
+    regExes: string;
+    components: string;
+    isCollection(): boolean;
+    sameCoords(a: any, b: any): boolean;
+    fromObject(obj: any): WktClass;
+    toObject(config?: any): any;
+    toString(config?: any): string;
+    fromJson(obj: any): WktClass;
+    toJson(): any;
+    merge(wkt: string): WktClass;
+    read(str: string): WktClass;
+    write(components?: any[]): string;
+    type: string;
+    extract: Extract;
+    isRectangle: boolean;
+}
 
-    class Wkt {
-        constructor();
-        delimiter: string;
-        wrapVertices: string;
-        regExes: string;
-        components: string;
-        isCollection(): boolean;
-        sameCoords(a: any, b: any): boolean;
-        fromObject(obj: any): Wkt;
-        toObject(config?: any): any;
-        toString(config?: any): string;
-        fromJson(obj: any): Wkt;
-        toJson(): any;
-        merge(wkt: string): Wkt;
-        read(str: string): Wkt;
-        write(components?: any[]): string;
-        type: string;
-        extract: Extract;
-        isRectangle: boolean;
-    }
+export function beginsWith(str: string, sub: string): boolean;
+export function endsWith(str: string, sub: string): boolean;
+export let delimiter: string;
+export function isArray(obj: any): boolean;
+export function trim(str: string, sub: string): string;
+export let Wkt: typeof WktClass;
 
-    type GeometryType = "point" | "multipoint" | "linestring" | "multilinestring" | "polygon" | "multipolygon" | "box";
+export type GeometryType =
+    | "point"
+    | "multipoint"
+    | "linestring"
+    | "multilinestring"
+    | "polygon"
+    | "multipolygon"
+    | "box";
 
-    interface Extract {
-        point(point: any): string;
-        multipoint(multipoint: any): string;
-        linestring(linestring: any): string;
-        multilinestring(multilinestring: any): string;
-        polygon(polygon: any): string;
-        multipolygon(multipolygon: any): string;
-        box(box: any): string;
-    }
+export interface Extract {
+    point(point: any): string;
+    multipoint(multipoint: any): string;
+    linestring(linestring: any): string;
+    multilinestring(multilinestring: any): string;
+    polygon(polygon: any): string;
+    multipolygon(multipolygon: any): string;
+    box(box: any): string;
 }
