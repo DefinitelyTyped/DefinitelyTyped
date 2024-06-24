14af9c2c6ff864f6cb207356d0ff5cf3f5dbd7ba
Jake Bailey <5341706+jakebailey@users.noreply.github.com>
2024-06-24 09:03:45 -0700

    Preemptively fix broken ExpectTypes (#69881)

--- types/floreal/floreal-tests.ts
+++ types/floreal/floreal-tests.ts
@@ -5,8 +5,8 @@ const fd2 = new Floreal("1799-11-09"); // $ExpectType FlorealDate
 const fd3 = new Floreal(Date.now()); // $ExpectType FlorealDate
 const fd4 = new Floreal(new Date()); // $ExpectType FlorealDate
 
-fd1.dayName(); // $ExpectType: string
-fd2.firstDayOfYear(); // $ExpectType: Date
+fd1.dayName(); // $ExpectType string
+fd2.firstDayOfYear(); // $ExpectType Date
 // @ts-expect-error
 fd1.foo();
 fd1.setYear("XII");
