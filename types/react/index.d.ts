73d42448d114630f9a4ac6233d632b297b66c3af
Louis Cruz <louisstephancruz@me.com>
2024-06-24 19:26:17 -0700

    Add global MathML attributes

--- types/react/index.d.ts
+++ types/react/index.d.ts
@@ -2154,6 +2154,8 @@ declare namespace React {
     interface SVGLineElementAttributes<T> extends SVGProps<T> {}
     interface SVGTextElementAttributes<T> extends SVGProps<T> {}
 
+    interface MathMLProps<T> extends MathMLAttributes<T> {}
+
     interface DOMAttributes<T> {
         children?: ReactNode | undefined;
         dangerouslySetInnerHTML?: {
@@ -3759,6 +3761,22 @@ declare namespace React {
         zoomAndPan?: string | undefined;
     }
 
+    // https://developer.mozilla.org/en-US/docs/Web/MathML/Global_attributes
+    interface MathMLAttributes<T> extends DOMAttributes<T> {
+        className?: string | undefined;
+        dir?: 'ltr' | 'rtl' | undefined;
+        displaystyle?: boolean | undefined;
+        href?: string | undefined;
+        id?: string | undefined;
+        mathbackground?: string | undefined;
+        mathcolor?: string | undefined;
+        mathsize?: string | undefined;
+        nonce?: string | undefined;
+        scriptlevel?: string | undefined;
+        style?: CSSProperties | undefined;
+        tabindex?: number | undefined;
+    }
+
     interface WebViewHTMLAttributes<T> extends HTMLAttributes<T> {
         allowFullScreen?: boolean | undefined;
         allowpopups?: boolean | undefined;
@@ -4216,6 +4234,9 @@ declare namespace React {
             tspan: React.SVGProps<SVGTSpanElement>;
             use: React.SVGProps<SVGUseElement>;
             view: React.SVGProps<SVGViewElement>;
+
+            // MathML
+            match: 
         }
     }
 }
