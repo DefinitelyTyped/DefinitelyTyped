94d1f219846f851a0b79d23ef2b53b489c0b3467
Daniel Souza <danielsouzaT99@hotmail.com>
2024-06-27 17:00:48 -0300

    Fix of "The file '@types/favico.js/index.d.ts' is not a module.ts(2306)"
    Error fixed
    
    The file '~/app/node_modules/@types/favico.js/index.d.ts' is not a module.ts(2306)
    favico.js -  0.3.10
    Node - v20.15.0
    NPM - 10.8.1
    
    ![image](https://github.com/DefinitelyTyped/DefinitelyTyped/assets/88730920/03d2b227-cf07-420e-b902-2110ddb8fddf)

--- types/favico.js/index.d.ts
+++ types/favico.js/index.d.ts
@@ -1,34 +1,31 @@
-declare namespace favicojs {
-    interface FavicoJsStatic {
-        new(opt?: FavicoJsOptions): Favico;
-    }
-
-    interface FavicoJsOptions {
-        bgColor?: string | undefined;
-        textColor?: string | undefined;
-        fontFamily?: string | undefined;
-        fontStyle?: string | undefined;
-        type?: string | undefined;
-        position?: string | undefined;
-        animation?: string | undefined;
-        elementId?: string | undefined;
-        element?: HTMLElement | undefined;
-        dataUrl?: ((url: string) => any) | undefined;
-    }
-
-    interface Favico {
-        badge(number: number): void;
-        badge(number: number, animation: string): void;
-        badge(number: number, opts: FavicoJsOptions): void;
-
-        reset(): void;
-
-        image(imageElement: HTMLElement): void;
-
-        video(imageElement: HTMLElement): void;
-
-        webcam(): void;
-    }
+export interface FavicoJsStatic {
+    new(opt?: FavicoJsOptions): Favico;
 }
 
-declare var Favico: favicojs.FavicoJsStatic;
+export interface FavicoJsOptions {
+    bgColor?: string | undefined;
+    textColor?: string | undefined;
+    fontFamily?: string | undefined;
+    fontStyle?: string | undefined;
+    type?: string | undefined;
+    position?: string | undefined;
+    animation?: string | undefined;
+    elementId?: string | undefined;
+    element?: HTMLElement | undefined;
+    dataUrl?: ((url: string) => any) | undefined;
+}
+
+export declare class Favico {
+    constructor(options?: FavicoJsOptions);
+    badge(number: number): void;
+    badge(number: number, animation: string): void;
+    badge(number: number, opts: FavicoJsOptions): void;
+
+    reset(): void;
+
+    image(imageElement: HTMLElement): void;
+
+    video(imageElement: HTMLElement): void;
+
+    webcam(): void;
+}
