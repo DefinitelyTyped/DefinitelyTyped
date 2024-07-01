d0343f79f50e8f80e3d3a5a589cbffcc4691cb31
Huxulm <huxulm@gmail.com>
2024-07-01 18:39:19 +0800

    add types for [flubber]

---
+++ types/flubber/index.d.ts
@@ -0,0 +1,23 @@
+export function combine(t: any, n: any, e: any): any;
+
+export function fromCircle(t: any, n: any, e: any, r: any, i: any): any;
+
+export function fromRect(t: any, n: any, e: any, r: any, i: any, a: any): any;
+
+export function interpolate(
+  from: string,
+  to: string,
+  options: { maxSegmentLength?: number; string?: boolean }
+): (t: any) => any;
+
+export function interpolateAll(t: any, n: any, e: any): any;
+
+export function separate(t: any, n: any, e: any): any;
+
+export function splitPathString(t: any): any;
+
+export function toCircle(t: any, n: any, e: any, r: any, i: any): any;
+
+export function toPathString(t: any): any;
+
+export function toRect(t: any, n: any, e: any, r: any, i: any, a: any): any;
