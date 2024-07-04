b36b66087f4443f1d60e2dfd82036171fae05b5d
Louis Cruz <louisstephancruz@me.com>
2024-07-03 19:04:21 -0700

    Add initial MathMLMFracElement

--- types/react/index.d.ts
+++ types/react/index.d.ts
@@ -3785,6 +3785,9 @@ declare namespace React {
         display?: 'block' | 'inline'
     }
     interface MathMLMErrorAttributes extends MathMLAttributes<MathMLMErrorAttributes> {}
+    interface MathMLMFracElement extends MathMLAttributes<MathMLMFracElement> {
+        linethickness: string;
+    }
 
     interface WebViewHTMLAttributes<T> extends HTMLAttributes<T> {
         allowFullScreen?: boolean | undefined;
@@ -4247,6 +4250,7 @@ declare namespace React {
             // MathML
             math: React.MathMLProps<MathMLMathElement>;
             merror: React.MathMLProps<MathMLMErrorElement>;
+            mfrac: React.MathMLProps<MathMLMFracElement>;
         }
     }
 }
