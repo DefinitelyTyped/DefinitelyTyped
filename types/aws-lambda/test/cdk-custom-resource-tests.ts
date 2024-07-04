9b200f5d95550675d7c9071a948595fe4af992d7
Luciano M. Taranto <6540591+LucianoTaranto@users.noreply.github.com>
2024-07-03 22:40:15 -0700

    test: Add unit tests to flex generics

--- types/aws-lambda/test/cdk-custom-resource-tests.ts
+++ types/aws-lambda/test/cdk-custom-resource-tests.ts
@@ -3,14 +3,28 @@ import {
     CdkCustomResourceIsCompleteHandler,
     CdkCustomResourceIsCompleteResponse,
     CdkCustomResourceResponse,
+    ResourcePropertiesCommon,
 } from "aws-lambda";
 
-const onEventHandler: CdkCustomResourceHandler = async (event, context) => {
+interface CustomResourceProperties extends ResourcePropertiesCommon {
+    customSwitch: boolean;
+}
+
+interface CustomOnEventData {
+    customOutputString: string;
+}
+
+interface CustomIsCompleteData {
+    completedWithoutWarnings: boolean;
+}
+
+const defaultOnEventHandler: CdkCustomResourceHandler = async (event, context) => {
     switch (event.RequestType) {
         case "Create":
             str = event.LogicalResourceId;
             str = event.RequestId;
             anyObj = event.ResourceProperties;
+            strOrUndefined = event.ResourceProperties.ServiceTimeout;
             str = event.ResourceProperties.ServiceToken;
             str = event.ResourceType;
             // Should have custom comment to not use:
@@ -50,12 +64,13 @@ const onEventHandler: CdkCustomResourceHandler = async (event, context) => {
     return validEmptyResponse;
 };
 
-const onIsCompleteHandler: CdkCustomResourceIsCompleteHandler = async (event, context) => {
+const defaultIsCompleteHandler: CdkCustomResourceIsCompleteHandler = async (event, context) => {
     switch (event.RequestType) {
         case "Create":
             str = event.LogicalResourceId;
             str = event.RequestId;
             anyObj = event.ResourceProperties;
+            strOrUndefined = event.ResourceProperties.ServiceTimeout;
             str = event.ResourceProperties.ServiceToken;
             str = event.ResourceType;
             // Should have custom comment to not use:
@@ -91,3 +106,98 @@ const onIsCompleteHandler: CdkCustomResourceIsCompleteHandler = async (event, co
 
     return responseComplete;
 };
+
+const customOnEventHandler: CdkCustomResourceHandler<CustomResourceProperties, CustomOnEventData> = async (
+    event,
+    context,
+) => {
+    switch (event.RequestType) {
+        case "Create":
+            str = event.LogicalResourceId;
+            str = event.RequestId;
+            anyObj = event.ResourceProperties;
+            strOrUndefined = event.ResourceProperties.ServiceTimeout;
+            str = event.ResourceProperties.ServiceToken;
+            bool = event.ResourceProperties.customSwitch;
+            str = event.ResourceType;
+            // Should have custom comment to not use:
+            str = event.ResponseURL;
+            str = event.StackId;
+
+            // @ts-expect-error
+            anyObj = event.OldResourceProperties;
+            // @ts-expect-error
+            str = event.PhysicalResourceId;
+            break;
+        case "Update":
+            anyObj = event.OldResourceProperties;
+            str = event.PhysicalResourceId;
+            break;
+        case "Delete":
+            str = event.PhysicalResourceId;
+            break;
+    }
+
+    const response: CdkCustomResourceResponse<CustomOnEventData> = {
+        PhysicalResourceId: str,
+        Data: {
+            customOutputString: str,
+        },
+        otherProperties: anyObj,
+    };
+
+    const invalidResponse: CdkCustomResourceResponse<CustomOnEventData> = {
+        // @ts-expect-error
+        PhysicalResourceId: num,
+        // @ts-expect-error
+        Data: str,
+    };
+
+    const validEmptyResponse: CdkCustomResourceResponse<CustomOnEventData> = {};
+    return validEmptyResponse;
+};
+
+const customIsCompleteHandler: CdkCustomResourceIsCompleteHandler<CustomResourceProperties, CustomIsCompleteData> =
+    async (event, context) => {
+        switch (event.RequestType) {
+            case "Create":
+                str = event.LogicalResourceId;
+                str = event.RequestId;
+                anyObj = event.ResourceProperties;
+                strOrUndefined = event.ResourceProperties.ServiceTimeout;
+                str = event.ResourceProperties.ServiceToken;
+                bool = event.ResourceProperties.customSwitch;
+                str = event.ResourceType;
+                // Should have custom comment to not use:
+                str = event.ResponseURL;
+                str = event.ServiceToken;
+                str = event.StackId;
+                break;
+            case "Update":
+                anyObj = event.OldResourceProperties;
+                str = event.PhysicalResourceId;
+                break;
+            case "Delete":
+                str = event.PhysicalResourceId;
+                break;
+        }
+
+        const responseComplete: CdkCustomResourceIsCompleteResponse<CustomIsCompleteData> = {
+            IsComplete: true,
+            Data: {
+                completedWithoutWarnings: true,
+            },
+            // @ts-expect-error
+            otherProperties: anyObj,
+        };
+
+        const responseWaiting: CdkCustomResourceIsCompleteResponse<CustomIsCompleteData> = {
+            IsComplete: false,
+            // @ts-expect-error
+            Data: {
+                stringKey: str,
+            },
+        };
+
+        return responseComplete;
+    };
--- types/aws-lambda/test/cloudformation-tests.ts
+++ types/aws-lambda/test/cloudformation-tests.ts
@@ -3,9 +3,22 @@ import {
     CloudFormationCustomResourceHandler,
     CloudFormationCustomResourceResponse,
     CloudFormationCustomResourceSuccessResponse,
+    ResourcePropertiesCommon,
 } from "aws-lambda";
 
-const handler: CloudFormationCustomResourceHandler = async (event, context, callback) => {
+interface CustomResourceProperties extends ResourcePropertiesCommon {
+    customSwitch: boolean;
+}
+
+interface CustomOldResourceProperties extends ResourcePropertiesCommon {
+    customNumber: number;
+}
+
+interface CustomData {
+    customOutputString: string;
+}
+
+const defaultHandler: CloudFormationCustomResourceHandler = async (event, context, callback) => {
     switch (event.RequestType) {
         case "Create":
             str = event.LogicalResourceId;
@@ -72,3 +85,166 @@ const handler: CloudFormationCustomResourceHandler = async (event, context, call
     callback();
     callback(new Error());
 };
+
+const customHandler: CloudFormationCustomResourceHandler<CustomResourceProperties> = async (
+    event,
+    context,
+    callback,
+) => {
+    switch (event.RequestType) {
+        case "Create":
+            str = event.LogicalResourceId;
+            str = event.RequestId;
+            anyObj = event.ResourceProperties;
+            strOrUndefined = event.ResourceProperties.ServiceTimeout;
+            str = event.ResourceProperties.ServiceToken;
+            bool = event.ResourceProperties.customSwitch;
+            str = event.ResourceType;
+            str = event.ResponseURL;
+            str = event.StackId;
+            break;
+        case "Update":
+            anyObj = event.OldResourceProperties;
+            strOrUndefined = event.OldResourceProperties.ServiceTimeout;
+            str = event.OldResourceProperties.ServiceToken;
+            bool = event.OldResourceProperties.customSwitch;
+            break;
+        case "Delete":
+            str = event.PhysicalResourceId;
+            break;
+    }
+
+    let response: CloudFormationCustomResourceResponse<CustomData>;
+    response = {
+        Data: {
+            customOutputString: str,
+        },
+        LogicalResourceId: str,
+        PhysicalResourceId: str,
+        Reason: strOrUndefined,
+        RequestId: str,
+        StackId: str,
+        Status: "SUCCESS",
+        NoEcho: boolOrUndefined,
+    };
+
+    let successResponse: CloudFormationCustomResourceSuccessResponse<CustomData>;
+    let failedResponse: CloudFormationCustomResourceFailedResponse<CustomData>;
+    successResponse = {
+        LogicalResourceId: str,
+        PhysicalResourceId: str,
+        RequestId: str,
+        StackId: str,
+        Status: "SUCCESS",
+    };
+    successResponse = {
+        Data: {
+            customOutputString: str,
+        },
+        LogicalResourceId: str,
+        PhysicalResourceId: str,
+        Reason: strOrUndefined,
+        RequestId: str,
+        StackId: str,
+        Status: "SUCCESS",
+        NoEcho: boolOrUndefined,
+    };
+    failedResponse = {
+        Data: {
+            customOutputString: str,
+        },
+        LogicalResourceId: str,
+        PhysicalResourceId: str,
+        Reason: str,
+        RequestId: str,
+        StackId: str,
+        Status: "FAILED",
+    };
+    response = successResponse;
+    response = failedResponse;
+
+    callback();
+    callback(new Error());
+};
+
+const customChangedHandler: CloudFormationCustomResourceHandler<CustomResourceProperties, CustomOldResourceProperties> =
+    async (
+        event,
+        context,
+        callback,
+    ) => {
+        switch (event.RequestType) {
+            case "Create":
+                str = event.LogicalResourceId;
+                str = event.RequestId;
+                anyObj = event.ResourceProperties;
+                strOrUndefined = event.ResourceProperties.ServiceTimeout;
+                str = event.ResourceProperties.ServiceToken;
+                bool = event.ResourceProperties.customSwitch;
+                str = event.ResourceType;
+                str = event.ResponseURL;
+                str = event.StackId;
+                break;
+            case "Update":
+                anyObj = event.OldResourceProperties;
+                strOrUndefined = event.OldResourceProperties.ServiceTimeout;
+                str = event.OldResourceProperties.ServiceToken;
+                num = event.OldResourceProperties.customNumber;
+                break;
+            case "Delete":
+                str = event.PhysicalResourceId;
+                break;
+        }
+
+        let response: CloudFormationCustomResourceResponse<CustomData>;
+        response = {
+            Data: {
+                customOutputString: str,
+            },
+            LogicalResourceId: str,
+            PhysicalResourceId: str,
+            Reason: strOrUndefined,
+            RequestId: str,
+            StackId: str,
+            Status: "SUCCESS",
+            NoEcho: boolOrUndefined,
+        };
+
+        let successResponse: CloudFormationCustomResourceSuccessResponse<CustomData>;
+        let failedResponse: CloudFormationCustomResourceFailedResponse<CustomData>;
+        successResponse = {
+            LogicalResourceId: str,
+            PhysicalResourceId: str,
+            RequestId: str,
+            StackId: str,
+            Status: "SUCCESS",
+        };
+        successResponse = {
+            Data: {
+                customOutputString: str,
+            },
+            LogicalResourceId: str,
+            PhysicalResourceId: str,
+            Reason: strOrUndefined,
+            RequestId: str,
+            StackId: str,
+            Status: "SUCCESS",
+            NoEcho: boolOrUndefined,
+        };
+        failedResponse = {
+            Data: {
+                customOutputString: str,
+            },
+            LogicalResourceId: str,
+            PhysicalResourceId: str,
+            Reason: str,
+            RequestId: str,
+            StackId: str,
+            Status: "FAILED",
+        };
+        response = successResponse;
+        response = failedResponse;
+
+        callback();
+        callback(new Error());
+    };
