947aa4c6101b127be6b02eab97e3f11eb2bb869d
Louis Cruz <louisstephancruz@me.com>
2024-07-04 11:55:23 -0700

    Add MathMLMoverElement

--- types/react/index.d.ts
+++ types/react/index.d.ts
@@ -3795,6 +3795,9 @@ declare namespace React {
         stretchy?: boolean | undefined;
         symmetric?: boolean | undefined;
     }
+    interface MathMLMoverElement extends MathMLAttributes<MathMLMoverElement> {
+        accent?: boolean | undefined;
+    }
 
     interface WebViewHTMLAttributes<T> extends HTMLAttributes<T> {
         allowFullScreen?: boolean | undefined;
@@ -4262,6 +4265,7 @@ declare namespace React {
             mmultiscripts: React.MathMLProps<MathMLMmultiscriptsElement>;
             mn: React.MathMLProps<MathMLMnElement>;
             mo: React.MathMLProps<MathMLMoElement>;
+            mover: React.MathMLProps<MathMLMoverElement>;
         }
     }
 }
