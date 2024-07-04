08e4a0a0add5190c5eb77c7f4267ca9b2511fd80
Louis Cruz <louisstephancruz@me.com>
2024-07-03 19:00:08 -0700

    Add initial MathMLMErrorElement

--- types/react/index.d.ts
+++ types/react/index.d.ts
@@ -3784,7 +3784,7 @@ declare namespace React {
     interface MathMLMathAttributes extends MathMLAttributes<MathMLMathElement> {
         display?: 'block' | 'inline'
     }
-
+    interface MathMLMErrorAttributes extends MathMLAttributes<MathMLMErrorAttributes> {}
 
     interface WebViewHTMLAttributes<T> extends HTMLAttributes<T> {
         allowFullScreen?: boolean | undefined;
@@ -4246,6 +4246,7 @@ declare namespace React {
 
             // MathML
             math: React.MathMLProps<MathMLMathElement>;
+            merror: React.MathMLProps<MathMLMErrorElement>;
         }
     }
 }
