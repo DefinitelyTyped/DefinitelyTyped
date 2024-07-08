1c60f958b76fcdad5ff45a3214065f1c99554d05
Nadine Kinnen <nkinnen@gmail.com>
2024-07-08 13:27:48 -0700

    Add types for image imports

---
+++ types/image-import/index.d.ts
@@ -0,0 +1,46 @@
+// This list is taken from the common image types listed here
+// https://developer.mozilla.org/en-US/docs/Web/Media/Formats/Image_types#common_image_file_types
+declare module "*.apng" {
+    const src: string;
+    export default src;
+}
+declare module "*.avif" {
+    const src: string;
+    export default src;
+}
+declare module "*.gif" {
+    const src: string;
+    export default src;
+}
+declare module "*.jpg" {
+    const src: string;
+    export default src;
+}
+declare module "*.jpeg" {
+    const src: string;
+    export default src;
+}
+declare module "*.jfif" {
+    const src: string;
+    export default src;
+}
+declare module "*.pjpeg" {
+    const src: string;
+    export default src;
+}
+declare module "*.pjp" {
+    const src: string;
+    export default src;
+}
+declare module "*.png" {
+    const src: string;
+    export default src;
+}
+declare module "*.svg" {
+    const src: string;
+    export default src;
+}
+declare module "*.webp" {
+    const src: string;
+    export default src;
+}
