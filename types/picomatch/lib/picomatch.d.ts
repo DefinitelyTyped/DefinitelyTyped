ae4ebeaa79a6a1ab75a42dc241f5adf31d07eb01
Sergei Ivankov <sergeiivankov@pm.me>
2024-07-08 14:13:13 +0300

    Refactore to function overloading

--- types/picomatch/lib/picomatch.d.ts
+++ types/picomatch/lib/picomatch.d.ts
@@ -30,6 +30,9 @@ declare function picomatch<T extends true | false = false>(
 declare namespace picomatch {
     type Glob = string | string[];
 
+    function expandRange(from: string, to: string, options: PicomatchOptions): string;
+    function expandRange(from: string, to: string, step: string, options: PicomatchOptions): string;
+
     interface Matcher {
         (test: string): boolean;
     }
@@ -84,10 +87,7 @@ declare namespace picomatch {
          * The function receives the range values as two arguments, and it must return a string to be used in the generated regex.
          * It's recommended that returned strings be wrapped in parentheses.
          */
-        expandRange?:
-            | ((from: string, to: string, options: PicomatchOptions) => string)
-            | ((from: string, to: string, step: string, options: PicomatchOptions) => string)
-            | undefined;
+        expandRange?: typeof expandRange | undefined;
         /**
          * Throws an error if no matches are found. Based on the bash option of the same name.
          */
