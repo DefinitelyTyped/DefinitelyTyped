0ae5f59ed4362d87bb31bab6fe2bc20a9b805975
Nathan Bierema <nbierema@gmail.com>
2024-07-25 09:01:45 -0400

    Fix Matrix2 Tuple

--- types/three/src/math/Matrix2.d.ts
+++ types/three/src/math/Matrix2.d.ts
@@ -1,13 +1,8 @@
 export type Matrix2Tuple = [
     n11: number,
     n12: number,
-    n13: number,
     n21: number,
     n22: number,
-    n23: number,
-    n31: number,
-    n32: number,
-    n33: number,
 ];
 
 /**
