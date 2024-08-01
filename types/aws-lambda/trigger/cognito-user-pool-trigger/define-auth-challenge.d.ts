df8d31cd39f052fe33d01e67ba701a5abb37e1a2
Michael Wallace <michael.a.wallace@me.com>
2024-08-01 18:06:03 +0000

    Respond to reviewer comments.

--- types/aws-lambda/trigger/cognito-user-pool-trigger/define-auth-challenge.d.ts
+++ types/aws-lambda/trigger/cognito-user-pool-trigger/define-auth-challenge.d.ts
@@ -12,7 +12,7 @@ export interface DefineAuthChallengeTriggerEvent extends BaseTriggerEvent<"Defin
         clientMetadata?: StringMap | undefined;
     };
     response: {
-        challengeName: string | undefined;
+        challengeName?: string | undefined;
         failAuthentication: boolean;
         issueTokens: boolean;
     };
