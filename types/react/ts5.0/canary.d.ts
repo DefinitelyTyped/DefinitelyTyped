415a5f0ab1791814ef5ca1c33e95ea7590a10396
eps1lon <sebastian.silbermann@vercel.com>
2024-08-02 15:43:30 +0200

    [react] Explicitly allow async Form Actions
    This was implicitly enabled by TypeScript allowing any return type if the return type of an expected function is `void`.
    But this is not generally behavior we want in React so we should not relly on it.
    
    It's also problematic since certain type-aware ESLint rules may issue violations since it looks like Form Actions can't be async.
    
    We now explicitly codify support of async Form Actions. This also means return anything else will now issue type errors

--- types/react/ts5.0/canary.d.ts
+++ types/react/ts5.0/canary.d.ts
@@ -67,7 +67,7 @@ declare module "." {
     export function unstable_useCacheRefresh(): () => void;
 
     interface DO_NOT_USE_OR_YOU_WILL_BE_FIRED_EXPERIMENTAL_FORM_ACTIONS {
-        functions: (formData: FormData) => void;
+        functions: (formData: FormData) => void | Promise<void>;
     }
 
     export interface TransitionStartFunction {
