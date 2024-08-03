f4ecaaa570e98669157897a64cbef345691e86c7
umar <umarg1997@gmail.com>
2024-08-03 08:36:49 +0100

    Fix: Remove ambient module declaration as per DefinitelyTyped guidelines

--- types/isurl/index.d.ts
+++ types/isurl/index.d.ts
@@ -1,10 +1,8 @@
-declare module "isurl" {
-    interface IsURL {
-        (url: unknown, supportIncomplete?: boolean): boolean;
-        lenient(url: unknown): boolean;
-    }
-
-    const isURL: IsURL;
-
-    export = isURL;
+interface IsURL {
+    (url: unknown, supportIncomplete?: boolean): boolean;
+    lenient(url: unknown): boolean;
 }
+
+declare const isURL: IsURL;
+
+export = isURL;
