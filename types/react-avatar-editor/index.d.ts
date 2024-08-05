e7a4388bfd06afebf47ad1b653f775afec457051
Gordon Leigh <gordonmleigh@gmail.com>
2024-08-05 09:46:00 +0200

    [react-avatar-editor] fix default export

--- types/react-avatar-editor/index.d.ts
+++ types/react-avatar-editor/index.d.ts
@@ -1,47 +1,51 @@
 import * as React from "react";
 
-export interface Position {
-    x: number;
-    y: number;
+declare namespace AvatarEditor {
+    interface Position {
+        x: number;
+        y: number;
+    }
+
+    interface CroppedRect extends Position {
+        width: number;
+        height: number;
+    }
+
+    interface ImageState extends CroppedRect {
+        resource: ImageData;
+    }
+
+    interface AvatarEditorProps {
+        className?: string | undefined;
+        image: string | File;
+        width?: number | undefined;
+        height?: number | undefined;
+        backgroundColor?: string | undefined;
+        border?: number | number[] | undefined;
+        borderRadius?: number | undefined;
+        color?: number[] | undefined;
+        style?: object | undefined;
+        scale?: number | undefined;
+        position?: Position | undefined;
+        rotate?: number | undefined;
+        crossOrigin?: string | undefined;
+        disableBoundaryChecks?: boolean | undefined;
+        disableHiDPIScaling?: boolean | undefined;
+        disableCanvasRotation?: boolean | undefined;
+        onLoadFailure?(event: Event): void;
+        onLoadSuccess?(imgInfo: ImageState): void;
+        onImageReady?(event: Event): void;
+        onMouseUp?(): void;
+        onMouseMove?(event: Event): void;
+        onImageChange?(): void;
+        onPositionChange?(position: Position): void;
+    }
 }
 
-export interface CroppedRect extends Position {
-    width: number;
-    height: number;
-}
-
-export interface ImageState extends CroppedRect {
-    resource: ImageData;
-}
-
-export interface AvatarEditorProps {
-    className?: string | undefined;
-    image: string | File;
-    width?: number | undefined;
-    height?: number | undefined;
-    backgroundColor?: string | undefined;
-    border?: number | number[] | undefined;
-    borderRadius?: number | undefined;
-    color?: number[] | undefined;
-    style?: object | undefined;
-    scale?: number | undefined;
-    position?: Position | undefined;
-    rotate?: number | undefined;
-    crossOrigin?: string | undefined;
-    disableBoundaryChecks?: boolean | undefined;
-    disableHiDPIScaling?: boolean | undefined;
-    disableCanvasRotation?: boolean | undefined;
-    onLoadFailure?(event: Event): void;
-    onLoadSuccess?(imgInfo: ImageState): void;
-    onImageReady?(event: Event): void;
-    onMouseUp?(): void;
-    onMouseMove?(event: Event): void;
-    onImageChange?(): void;
-    onPositionChange?(position: Position): void;
-}
-
-export default class AvatarEditor extends React.Component<AvatarEditorProps, any> {
+declare class AvatarEditor extends React.Component<AvatarEditor.AvatarEditorProps, any> {
     getImage(): HTMLCanvasElement;
     getImageScaledToCanvas(): HTMLCanvasElement;
-    getCroppingRect(): CroppedRect;
+    getCroppingRect(): AvatarEditor.CroppedRect;
 }
+
+export = AvatarEditor;
