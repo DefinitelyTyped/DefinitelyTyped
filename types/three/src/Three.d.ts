2cff400b4bfcd606c7631a97fcc02e8cf14e98df
Nathan Bierema <nbierema@gmail.com>
2024-08-05 16:01:00 -0400

    [three] Add missing WebXRManager type exports

--- types/three/src/Three.d.ts
+++ types/three/src/Three.d.ts
@@ -157,6 +157,12 @@ export type {
     XRJointSpace,
     XRTargetRaySpace,
 } from "./renderers/webxr/WebXRController.js";
+export type {
+    WebXRArrayCamera,
+    WebXRCamera,
+    WebXRManager,
+    WebXRManagerEventMap,
+} from "./renderers/webxr/WebXRManager.js";
 export * from "./scenes/Fog.js";
 export * from "./scenes/FogExp2.js";
 export * from "./scenes/Scene.js";
