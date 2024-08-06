d668c9bddb20b592793e7ad03096f4b67dd6fd6c
Frank Elsinga <frank@elsinga.de>
2024-08-06 22:09:11 +0200

    fixed formatting mistakes

--- types/nightwind/test/nightwind-tests.global.ts
+++ types/nightwind/test/nightwind-tests.global.ts
@@ -1,9 +1,8 @@
 import type { Config } from "tailwindcss";
-import type plugin = require("tailwindcss/plugin");
 import nightwind = require("nightwind");
 
 const config: Config = {
-  content:[],
+    content: [],
     plugins: [
         nightwind,
     ],
