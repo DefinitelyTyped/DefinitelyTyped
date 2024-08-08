2d34c1cbed02707456faabcec4e2faf46672ee36
AbdulRahman Mohammad <abdooo9.dev@gmail.com>
2024-08-08 19:25:24 +0300

    Update didyoumean-tests.ts
    Fix tests

--- types/didyoumean/didyoumean-tests.ts
+++ types/didyoumean/didyoumean-tests.ts
@@ -4,21 +4,17 @@ let input = "insargrm";
 let result;
 const list: string[] = ["facebook", "twitter", "instagram", "linkedin"];
 
-result = didYouMean(input, list);
-console.log(result) // "instagram"
+result = didYouMean(input, list); // instagram
 
 input = "google plus";
 
-result = didYouMean(input, list);
-console.log(result) // null
+result = didYouMean(input, list); // null
 
 
 input = "insargrm";
 const listOfObjects = [{ id: "facebook" }, { id: "twitter" }, { id: "instagram" }, { id: "linkedin" }];
 const key = "id";
-result = didYouMean(input, listOfObjects, key);
-console.log(result) // "instagram"
+result = didYouMean(input, listOfObjects, key); // instagram
 
 didYouMean.returnWinningObject = true;
-result = didYouMean(input, listOfObjects, key);
-console.log(result) // "instagram"
+result = didYouMean(input, listOfObjects, key); // { id: 'instagram' }
