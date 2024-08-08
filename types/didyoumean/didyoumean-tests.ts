8545323ba8d6ce2f0182d8b73a3d34299081cee2
AbdulRahman Mohammad <abdooo9.dev@gmail.com>
2024-08-08 19:00:32 +0300

    Update didyoumean-tests.ts

--- types/didyoumean/didyoumean-tests.ts
+++ types/didyoumean/didyoumean-tests.ts
@@ -1,27 +1,24 @@
 import didYouMean from "didyoumean";
 
-interface ListType {
-    id: string;
-}
-
 let input = "insargrm";
 let result;
 const list: string[] = ["facebook", "twitter", "instagram", "linkedin"];
 
-// The method matches 'insargrm' to 'instagram'.
 result = didYouMean(input, list);
+console.log(result) // "instagram"
 
 input = "google plus";
 
-// The method was unable to find 'google plus' in the list of options.
 result = didYouMean(input, list);
+console.log(result) // null
 
-// Matching to a list of key, value pairs.
 
 input = "insargrm";
-const listOfObjects: ListType[] = [{ id: "facebook" }, { id: "twitter" }, { id: "instagram" }, { id: "linkedin" }];
+const listOfObjects = [{ id: "facebook" }, { id: "twitter" }, { id: "instagram" }, { id: "linkedin" }];
 const key = "id";
 result = didYouMean(input, listOfObjects, key);
+console.log(result) // "instagram"
 
 didYouMean.returnWinningObject = true;
 result = didYouMean(input, listOfObjects, key);
+console.log(result) // "instagram"
