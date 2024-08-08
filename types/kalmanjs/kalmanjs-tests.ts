fac9ab0ca201d72b10a063a1beb35e81ddd508df
Vladislav Tsepilov <26919118+pagyew@users.noreply.github.com>
2024-08-08 22:30:52 +0000

    fixes formatting

--- types/kalmanjs/kalmanjs-tests.ts
+++ types/kalmanjs/kalmanjs-tests.ts
@@ -7,4 +7,4 @@ kalmanFilter.predict(); // $ExpectType number
 kalmanFilter.uncertainty(); // $ExpectType number
 kalmanFilter.lastMeasurement(); // $ExpectType number
 kalmanFilter.setMeasurementNoise(0); // $ExpectType void
-kalmanFilter.setProcessNoise(0); ; // $ExpectType void
+kalmanFilter.setProcessNoise(0); // $ExpectType void
