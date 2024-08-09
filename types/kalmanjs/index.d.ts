04058b078650146909fa75cd2243beb4c96fecdd
Vladislav Tsepilov <fickleprogger@gmail.com>
2024-08-10 00:15:09 +0300

    try again

--- types/kalmanjs/index.d.ts
+++ types/kalmanjs/index.d.ts
@@ -1,5 +1,5 @@
-declare namespace KalmanFilter {
-    interface Options {
+declare namespace kalmanjs {
+    interface KalmanFilterOptions {
         /**
          * Process noise
          */
@@ -25,58 +25,60 @@ declare namespace KalmanFilter {
          */
         C?: number;
     }
+
+    class KalmanFilter {
+        /**
+         * Create 1-dimensional kalman filter
+         * @param  {Number} options.R Process noise
+         * @param  {Number} options.Q Measurement noise
+         * @param  {Number} options.A State vector
+         * @param  {Number} options.B Control vector
+         * @param  {Number} options.C Measurement vector
+         * @return {KalmanFilter}
+         */
+        constructor(options?: KalmanFilterOptions);
+
+        /**
+         * Filter a new value
+         * @param  {Number} z Measurement
+         * @param  {Number} u Control
+         * @return {Number}
+         */
+        filter(z: number, u?: number): number;
+
+        /**
+         * Predict next value
+         * @param  {Number} [u] Control
+         * @return {Number}
+         */
+        predict(u?: number): number;
+
+        /**
+         * Return uncertainty of filter
+         * @return {Number}
+         */
+        uncertainty(): number;
+
+        /**
+         * Return the last filtered measurement
+         * @return {Number}
+         */
+        lastMeasurement(): number;
+
+        /**
+         * Set measurement noise Q
+         * @param {Number} noise
+         */
+        setMeasurementNoise(noise: number): void;
+
+        /**
+         * Set the process noise R
+         * @param {Number} noise
+         */
+        setProcessNoise(noise: number): void;
+    }
 }
 
-declare class KalmanFilter {
-    /**
-     * Create 1-dimensional kalman filter
-     * @param  {Number} options.R Process noise
-     * @param  {Number} options.Q Measurement noise
-     * @param  {Number} options.A State vector
-     * @param  {Number} options.B Control vector
-     * @param  {Number} options.C Measurement vector
-     * @return {KalmanFilter}
-     */
-    constructor(options?: KalmanFilter.Options);
-
-    /**
-     * Filter a new value
-     * @param  {Number} z Measurement
-     * @param  {Number} u Control
-     * @return {Number}
-     */
-    filter(z: number, u?: number): number;
-
-    /**
-     * Predict next value
-     * @param  {Number} [u] Control
-     * @return {Number}
-     */
-    predict(u?: number): number;
-
-    /**
-     * Return uncertainty of filter
-     * @return {Number}
-     */
-    uncertainty(): number;
-
-    /**
-     * Return the last filtered measurement
-     * @return {Number}
-     */
-    lastMeasurement(): number;
-
-    /**
-     * Set measurement noise Q
-     * @param {Number} noise
-     */
-    setMeasurementNoise(noise: number): void;
-
-    /**
-     * Set the process noise R
-     * @param {Number} noise
-     */
-    setProcessNoise(noise: number): void;
-}
+declare const KalmanFilter: kalmanjs.KalmanFilter;
 
 export = KalmanFilter;
