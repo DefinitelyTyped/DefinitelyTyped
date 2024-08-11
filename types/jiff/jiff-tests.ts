abd0b552268b4746e5750ac99ac1d6e3b42cbb25
Jake Holland <contact@hollandjake.com>
2024-08-11 10:57:11 +0100

    correct the return type for patchInPlace

--- types/jiff/jiff-tests.ts
+++ types/jiff/jiff-tests.ts
@@ -7,22 +7,22 @@ const patch: jiff.JSONPatch = jiff.diff(a, b);
 
 const c = jiff.patch(patch, a);
 
-jiff.patchInPlace(patch, a);
+const d = jiff.patchInPlace(patch, a);
 
-const d: jiff.JSONObject = {
+const e: jiff.JSONObject = {
     list: [
         { id: "1", value: "original" },
         { id: "2", value: "original2" },
     ],
 };
-const e: jiff.JSONPatch = [{ op: "replace", path: "/list/2/value", value: "replaced", context: "2" }];
-const f = jiff.patch(e, d, {
+const f: jiff.JSONPatch = [{ op: "replace", path: "/list/2/value", value: "replaced", context: "2" }];
+const g = jiff.patch(f, e, {
     findContext(index, array, context) {
         return array.findIndex((value, index, array) => value["id"] === context);
     },
 });
 
-jiff.patchInPlace(e, d, {
+const h = jiff.patchInPlace(f, e, {
     findContext(index, array, context) {
         return array.findIndex((value, index, array) => value["id"] === context);
     },
