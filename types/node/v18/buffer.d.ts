e788573fedef53852c796cde72ba3cfae4e9e165
Shlomo <78599753+ShlomoCode@users.noreply.github.com>
2024-08-13 08:06:18 +0300

    Update buffer.d.ts

--- types/node/v18/buffer.d.ts
+++ types/node/v18/buffer.d.ts
@@ -410,7 +410,7 @@ declare module "buffer" {
                 encoding?: BufferEncoding,
             ): number;
             /**
-             * Returns a new `Buffer` which is the result of concatenating all the `Buffer`instances in the `list` together.
+             * Returns a new `Buffer` which is the result of concatenating all the `Buffer` instances in the `list` together.
              *
              * If the list has no items, or if the `totalLength` is 0, then a new zero-length`Buffer` is returned.
              *
