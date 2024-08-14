ff38d26042edb66b1710ae4bb480c13314dc2305
Lordfirespeed <28568841+Lordfirespeed@users.noreply.github.com>
2024-08-14 10:44:00 +0100

    replace `$ExpectType` with assignment in `stoppable` tests

--- types/stoppable/stoppable-tests.ts
+++ types/stoppable/stoppable-tests.ts
@@ -6,10 +6,10 @@ import stoppable = require("stoppable");
 type Stoppable = stoppable.StoppableServer;
 type WithStop = stoppable.WithStop;
 
-const httpServer = stoppable(http.createServer()); // $ExpectType Server<typeof IncomingMessage, typeof ServerResponse> & WithStop
-const httpsServer = stoppable(https.createServer()); // $ExpectType Server<typeof IncomingMessage, typeof ServerResponse> & WithStop
-stoppable(http.createServer(), 10000); // $ExpectType Server<typeof IncomingMessage, typeof ServerResponse> & WithStop
-stoppable(https.createServer(), 10000); // $ExpectType Server<typeof IncomingMessage, typeof ServerResponse> & WithStop
+const httpServer: http.Server & WithStop = stoppable(http.createServer());
+const httpsServer: https.Server & WithStop = stoppable(https.createServer());
+const httpServerWithGrace: http.Server & WithStop = stoppable(http.createServer(), 10000);
+const httpsServerWithGrace: https.Server & WithStop = stoppable(https.createServer(), 10000);
 
 // check return type is actually a http.Server/https.Server
 httpServer.addListener("connection", socket => {
