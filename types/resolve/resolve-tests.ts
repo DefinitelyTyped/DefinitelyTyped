fdcbb9362024c4c302d2a153791b21e966260eab
kcastellino <57894504+kcastellino@users.noreply.github.com>
2024-08-16 02:07:03 +1000

    Add tests for `resolve`

--- types/resolve/resolve-tests.ts
+++ types/resolve/resolve-tests.ts
@@ -2,9 +2,22 @@
 
 import * as fs from "fs";
 import resolve = require("resolve");
+import resolveAsync = require("resolve/async")
+import resolveSync = require("resolve/sync")
 
 function test_basic_async() {
     resolve("typescript", function(error, resolved, pkg) {
+        if (error) {
+            console.error(error.message);
+            return;
+        }
+        console.log(resolved);
+        console.log(pkg.version);
+    });
+}
+
+function test_basic_async_import() {
+    resolveAsync("typescript", function(error, resolved, pkg) {
         if (error) {
             console.error(error.message);
             return;
@@ -19,8 +32,64 @@ function test_basic_sync() {
     console.log(resolved);
 }
 
+function test_basic_sync_import() {
+    var resolved = resolveSync("typescript");
+    console.log(resolved);
+}
+
 function test_options_async() {
     resolve(
+        "typescript",
+        {
+            basedir: process.cwd(),
+            package: {},
+            extensions: [".js"],
+            packageFilter: function(pkg, pkgfile, dir) {
+                return pkg;
+            },
+            pathFilter: function(pkg, path, relativePath) {
+                return path;
+            },
+            paths: [process.cwd()],
+            moduleDirectory: "node_modules",
+            readFile: fs.readFile,
+            isDirectory: function(directory, cb) {
+                fs.stat(directory, function(error, stat) {
+                    if (error && error.code === "ENOENT") {
+                        return cb(null, false);
+                    } else if (error) {
+                        return cb(error);
+                    } else {
+                        return cb(null, stat.isDirectory());
+                    }
+                });
+            },
+            isFile: function(file, cb) {
+                fs.stat(file, function(error, stat) {
+                    if (error && error.code === "ENOENT") {
+                        return cb(null, false);
+                    } else if (error) {
+                        return cb(error);
+                    } else {
+                        return cb(null, stat.isFile());
+                    }
+                });
+            },
+            preserveSymlinks: false,
+        },
+        function(error, resolved, pkg) {
+            if (error) {
+                console.error(error.message);
+                return;
+            }
+            console.log(resolved);
+            console.log(pkg.version);
+        },
+    );
+}
+
+function test_options_async_import() {
+    resolveAsync(
         "typescript",
         {
             basedir: process.cwd(),
@@ -115,6 +184,61 @@ function test_options_sync() {
     // specifying both `readFile` and `readPackage` is forbidden
     // @ts-expect-error
     resolved = resolve.sync("typescript", {
+        readFileSync(file) {
+            return fs.readFileSync(file);
+        },
+        readPackageSync(readFileSync, file) {
+            // JSON.parse accepts anything with a toString, so a cast here is safe
+            return JSON.parse(readFileSync(file) as string);
+        },
+    });
+}
+
+function test_options_sync_import() {
+    var resolved = resolveSync("typescript", {
+        basedir: process.cwd(),
+        package: {},
+        extensions: [".js"],
+        packageFilter: function(pkg, pkgfile, dir) {
+            return pkg;
+        },
+        pathFilter: function(pkg, path, relativePath) {
+            return path;
+        },
+        paths: [process.cwd()],
+        moduleDirectory: "node_modules",
+        readFileSync: fs.readFileSync,
+        isDirectory: function(directory) {
+            try {
+                return fs.statSync(directory).isDirectory();
+            } catch (error) {
+                return false;
+            }
+        },
+        isFile: function(file) {
+            try {
+                return fs.statSync(file).isFile();
+            } catch (error) {
+                return false;
+            }
+        },
+        preserveSymlinks: true,
+    });
+    console.log(resolved);
+    resolved = resolveSync("typescript", {
+        readFileSync(file) {
+            return fs.readFileSync(file);
+        },
+    });
+    resolved = resolveSync("typescript", {
+        readPackageSync(readFileSync, file) {
+            // JSON.parse accepts anything with a toString, so a cast here is safe
+            return JSON.parse(readFileSync(file) as string);
+        },
+    });
+    // specifying both `readFile` and `readPackage` is forbidden
+    // @ts-expect-error
+    resolved = resolveSync("typescript", {
         readFileSync(file) {
             return fs.readFileSync(file);
         },
