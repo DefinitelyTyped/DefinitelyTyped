59270dec794b8e8aee428c56b2d4c396fcb86bfc
Renegade334 <contact.9a5d6388@renegade334.me.uk>
2024-08-19 13:27:22 +0100

    Fold own Signal definitions into _Signal

--- types/node/globals.d.ts
+++ types/node/globals.d.ts
@@ -17,8 +17,54 @@ type _WebSocket = typeof globalThis extends { onmessage: any } ? {} : import("un
 type _EventSource = typeof globalThis extends { onmessage: any } ? {} : import("undici-types").EventSource;
 // #endregion Fetch and friends
 
-// Conditional type alias for webstorage interface, which conflicts with lib.dom otherwise.
-type _Storage = typeof globalThis extends { onabort: any } ? {} : NodeJS.Storage;
+// Conditional type definitions for webstorage interface, which conflicts with lib.dom otherwise.
+type _Storage = typeof globalThis extends { onabort: any } ? {} : {
+    /**
+     * Returns the number of key/value pairs.
+     *
+     * [MDN Reference](https://developer.mozilla.org/docs/Web/API/Storage/length)
+     */
+    readonly length: number;
+    /**
+     * Removes all key/value pairs, if there are any.
+     *
+     * Dispatches a storage event on Window objects holding an equivalent Storage object.
+     *
+     * [MDN Reference](https://developer.mozilla.org/docs/Web/API/Storage/clear)
+     */
+    clear(): void;
+    /**
+     * Returns the current value associated with the given key, or null if the given key does not exist.
+     *
+     * [MDN Reference](https://developer.mozilla.org/docs/Web/API/Storage/getItem)
+     */
+    getItem(key: string): string | null;
+    /**
+     * Returns the name of the nth key, or null if n is greater than or equal to the number of key/value pairs.
+     *
+     * [MDN Reference](https://developer.mozilla.org/docs/Web/API/Storage/key)
+     */
+    key(index: number): string | null;
+    /**
+     * Removes the key/value pair with the given key, if a key/value pair with the given key exists.
+     *
+     * Dispatches a storage event on Window objects holding an equivalent Storage object.
+     *
+     * [MDN Reference](https://developer.mozilla.org/docs/Web/API/Storage/removeItem)
+     */
+    removeItem(key: string): void;
+    /**
+     * Sets the value of the pair identified by key to value, creating a new key/value pair if none existed for key previously.
+     *
+     * Throws a "QuotaExceededError" DOMException exception if the new value couldn't be set. (Setting could fail if, e.g., the user has disabled storage for the site, or if the quota has been exceeded.)
+     *
+     * Dispatches a storage event on Window objects holding an equivalent Storage object.
+     *
+     * [MDN Reference](https://developer.mozilla.org/docs/Web/API/Storage/setItem)
+     */
+    setItem(key: string, value: string): void;
+    [key: string]: any;
+};
 
 declare global {
     // Declare "static" methods in Error
@@ -403,55 +449,6 @@ declare global {
 
         interface ReadOnlyDict<T> {
             readonly [key: string]: T | undefined;
-        }
-
-        // Conditionally merged into the global scope if lib.dom is absent.
-        interface Storage {
-            /**
-             * Returns the number of key/value pairs.
-             *
-             * [MDN Reference](https://developer.mozilla.org/docs/Web/API/Storage/length)
-             */
-            readonly length: number;
-            /**
-             * Removes all key/value pairs, if there are any.
-             *
-             * Dispatches a storage event on Window objects holding an equivalent Storage object.
-             *
-             * [MDN Reference](https://developer.mozilla.org/docs/Web/API/Storage/clear)
-             */
-            clear(): void;
-            /**
-             * Returns the current value associated with the given key, or null if the given key does not exist.
-             *
-             * [MDN Reference](https://developer.mozilla.org/docs/Web/API/Storage/getItem)
-             */
-            getItem(key: string): string | null;
-            /**
-             * Returns the name of the nth key, or null if n is greater than or equal to the number of key/value pairs.
-             *
-             * [MDN Reference](https://developer.mozilla.org/docs/Web/API/Storage/key)
-             */
-            key(index: number): string | null;
-            /**
-             * Removes the key/value pair with the given key, if a key/value pair with the given key exists.
-             *
-             * Dispatches a storage event on Window objects holding an equivalent Storage object.
-             *
-             * [MDN Reference](https://developer.mozilla.org/docs/Web/API/Storage/removeItem)
-             */
-            removeItem(key: string): void;
-            /**
-             * Sets the value of the pair identified by key to value, creating a new key/value pair if none existed for key previously.
-             *
-             * Throws a "QuotaExceededError" DOMException exception if the new value couldn't be set. (Setting could fail if, e.g., the user has disabled storage for the site, or if the quota has been exceeded.)
-             *
-             * Dispatches a storage event on Window objects holding an equivalent Storage object.
-             *
-             * [MDN Reference](https://developer.mozilla.org/docs/Web/API/Storage/setItem)
-             */
-            setItem(key: string, value: string): void;
-            [key: string]: any;
         }
     }
 
