5e5813476dc981a896e963306bacb546b6b8e012
Gui <74725108+JonesJugHead@users.noreply.github.com>
2024-08-20 19:46:39 +0200

    Add tests for the new type

--- types/shpjs/shpjs-tests.ts
+++ types/shpjs/shpjs-tests.ts
@@ -1,18 +1,28 @@
 import shp = require("shpjs");
 
 shp("https://this.is.a.url", ["white", "list"]).then((geojson) => {});
-shp(new Buffer("")).then((geojson) => {});
+shp(Buffer.from("")).then((geojson) => {});
 shp(new ArrayBuffer(50)).then((geojson) => {});
 shp(new Int32Array(50)).then((geojson) => {});
 
-shp.parseZip(new Buffer("")).then((geojson) => {});
-shp.parseZip(new Buffer(""), ["white", "list"]).then((geojson) => {});
+// Test for ShpJSInputObject
+const shpInputObject: shpjs.ShpJSInputObject = {
+    shp: Buffer.from(""),
+    dbf: Buffer.from(""),
+    prj: Buffer.from(""),
+    cpg: Buffer.from("")
+};
+shp(shpInputObject).then((geojson) => {});
+shp({ shp: new ArrayBuffer(50), dbf: new ArrayBuffer(50) }).then((geojson) => {});
+
+shp.parseZip(Buffer.from("")).then((geojson) => {});
+shp.parseZip(Buffer.from(""), ["white", "list"]).then((geojson) => {});
 shp.parseZip(new ArrayBuffer(50)).then((geojson) => {});
 shp.parseZip(new Int32Array(50)).then((geojson) => {});
 
 shp.getShapefile("xxx.zip").then((geojson) => {});
 shp.getShapefile("xxx.zip", ["white", "list"]).then((geojson) => {});
-shp.getShapefile(new Buffer("")).then((geojson) => {});
+shp.getShapefile(Buffer.from("")).then((geojson) => {});
 shp.getShapefile(new ArrayBuffer(50)).then((geojson) => {});
 shp.getShapefile(new Int32Array(50)).then((geojson) => {});
 
@@ -29,18 +39,21 @@ const combinedGeojson = shp.combine([
 let parsedShp: GeoJSON.Geometry[];
 let parsedDbf: GeoJSON.GeoJsonProperties[];
 
-parsedShp = shp.parseShp(new Buffer(""), "proj");
-parsedShp = shp.parseShp(new Buffer(""), new Buffer("proj"));
+parsedShp = shp.parseShp(Buffer.from(""), "proj");
+parsedShp = shp.parseShp(Buffer.from(""), Buffer.from("proj"));
 parsedShp = shp.parseShp(new ArrayBuffer(50), "proj");
 parsedShp = shp.parseShp(new Int32Array(50), "proj");
 
 // Should parse shapes without projection (use default)
 parsedShp = shp.parseShp(new Int32Array(50));
 parsedShp = shp.parseShp(new ArrayBuffer(50));
-parsedShp = shp.parseShp(new Buffer(""));
+parsedShp = shp.parseShp(Buffer.from(""));
 
-parsedDbf = shp.parseDbf(new Buffer(""), new Buffer(""));
-parsedDbf = shp.parseDbf(new ArrayBuffer(50), new Buffer(""));
-parsedDbf = shp.parseDbf(new Int32Array(50), new Buffer(""));
+parsedDbf = shp.parseDbf(Buffer.from(""), Buffer.from(""));
+parsedDbf = shp.parseDbf(new ArrayBuffer(50), Buffer.from(""));
+parsedDbf = shp.parseDbf(new Int32Array(50), Buffer.from(""));
 
-shp.combine([shp.parseShp(new Buffer(""), "proj"), shp.parseDbf(new Buffer(""), new Buffer(""))]);
+// Test parseDbf with optional cpg
+parsedDbf = shp.parseDbf(Buffer.from(""));
+
+shp.combine([shp.parseShp(Buffer.from(""), "proj"), shp.parseDbf(Buffer.from(""), Buffer.from(""))]);
