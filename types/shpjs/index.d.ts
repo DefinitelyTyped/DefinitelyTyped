c64e67084166e89bf74634ec50f333c82098e806
Gui <74725108+JonesJugHead@users.noreply.github.com>
2024-08-20 19:31:51 +0200

    fix(types): update ShpJS typings to match documentation

--- types/shpjs/index.d.ts
+++ types/shpjs/index.d.ts
@@ -5,30 +5,48 @@ declare namespace shpjs {
     // All toBuffer() compatible buffers.
     type ShpJSBuffer = Buffer | ArrayBuffer | { buffer: ArrayBuffer };
 
+    interface ShpJSInputObject {
+        shp: ShpJSBuffer;
+        dbf?: ShpJSBuffer;
+        prj?: ShpJSBuffer;
+        cpg?: ShpJSBuffer;
+    }
+
     interface FeatureCollectionWithFilename extends GeoJSON.FeatureCollection {
         fileName?: string | undefined;
     }
 
     interface ShpJS {
         (
-            base: string | ShpJSBuffer,
+            base: string | ShpJSBuffer | ShpJSInputObject,
             whiteList?: readonly string[],
         ): Promise<FeatureCollectionWithFilename | FeatureCollectionWithFilename[]>;
+
         parseZip(
             buffer: ShpJSBuffer,
             whiteList?: readonly string[],
         ): Promise<FeatureCollectionWithFilename | FeatureCollectionWithFilename[]>;
+
         getShapefile(
             base: string | ShpJSBuffer,
             whiteList?: readonly string[],
         ): Promise<FeatureCollectionWithFilename | FeatureCollectionWithFilename[]>;
+
         combine(
             arr: [readonly GeoJSON.Geometry[], readonly GeoJSON.GeoJsonProperties[]],
         ): GeoJSON.FeatureCollection;
-        parseShp(shp: ShpJSBuffer, prj?: string | Buffer): GeoJSON.Geometry[];
-        parseDbf(dbf: ShpJSBuffer, cpg: ShpJSBuffer): GeoJSON.GeoJsonProperties[];
+
+        parseShp(
+            shp: ShpJSBuffer,
+            prj?: string | Buffer,
+        ): GeoJSON.Geometry[];
+
+        parseDbf(
+            dbf: ShpJSBuffer,
+            cpg?: ShpJSBuffer,
+        ): GeoJSON.GeoJsonProperties[];
     }
 }
 
-declare var shpjs: shpjs.ShpJS;
+declare const shpjs: shpjs.ShpJS;
 export = shpjs;
