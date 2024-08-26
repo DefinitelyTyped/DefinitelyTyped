1b56752095d3bc1b5c554ad15b85e955ab824cbc
Ann <lovsamann@gmail.com>
2024-08-26 08:12:31 +0700

    dprint

--- types/bakong-khqr/bakong-khqr-tests.ts
+++ types/bakong-khqr/bakong-khqr-tests.ts
@@ -1,11 +1,11 @@
-import { BakongKHQR, khqrData, IndividualInfo, MerchantInfo, SourceInfo } from "bakong-khqr"
+import { BakongKHQR, IndividualInfo, khqrData, MerchantInfo, SourceInfo } from "bakong-khqr";
 
 const qrData: khqrData = {
     currency: {
         usd: 840,
-        khr: 116 
-    }
-}
+        khr: 116,
+    },
+};
 
 const merchantInfo: MerchantInfo = {
     merchantID: "123456",
@@ -15,22 +15,21 @@ const merchantInfo: MerchantInfo = {
     merchantCity: "Phnom Penh",
     currency: qrData.currency.khr,
     amount: 10000,
-    billNumber: '#0001',
-    mobileNumber: '85587878787',
-    storeLabel: 'Devit Huotkeo',
-    terminalLabel: "Devit I"
-}
+    billNumber: "#0001",
+    mobileNumber: "85587878787",
+    storeLabel: "Devit Huotkeo",
+    terminalLabel: "Devit I",
+};
 
 const individualInfo: IndividualInfo = {
     bakongAccountID: "devit@abaa",
     merchantName: "devit",
     acquiringBank: "devit@abaa",
     currency: qrData.currency.usd,
-    merchantCity: 'BTB'
-}
+    merchantCity: "BTB",
+};
 
-const khqr = new BakongKHQR()
-const res = khqr.generateMerchant(merchantInfo)
+const khqr = new BakongKHQR();
+const res = khqr.generateMerchant(merchantInfo);
 
-
-const resIndividual = khqr.generateIndividual(individualInfo)
+const resIndividual = khqr.generateIndividual(individualInfo);
