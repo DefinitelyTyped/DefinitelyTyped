d7cc781535ca0732d5c66204e314f2854f707718
Kevin Gibbons <bakkot@gmail.com>
2024-08-31 09:27:42 -0700

    node:util parseArgs: factor `undefined |` out of conditional

--- types/node/util.d.ts
+++ types/node/util.d.ts
@@ -1520,11 +1520,13 @@ declare module "util" {
     type ParsedValues<T extends ParseArgsConfig> =
         & IfDefaultsTrue<T["strict"], unknown, { [longOption: string]: undefined | string | boolean }>
         & (T["options"] extends ParseArgsOptionsConfig ? {
-                -readonly [LongOption in keyof T["options"]]: IfDefaultsFalse<
-                    T["options"][LongOption]["multiple"],
-                    undefined | Array<ExtractOptionValue<T, T["options"][LongOption]>>,
-                    undefined | ExtractOptionValue<T, T["options"][LongOption]>
-                >;
+                -readonly [LongOption in keyof T["options"]]:
+                    | undefined
+                    | IfDefaultsFalse<
+                        T["options"][LongOption]["multiple"],
+                        Array<ExtractOptionValue<T, T["options"][LongOption]>>,
+                        ExtractOptionValue<T, T["options"][LongOption]>
+                      >;
             }
             : {});
 
