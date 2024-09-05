bd33d134f76eae0d23f21f4ff1c46ed5d890af52
Security2431 <artem2431@gmail.com>
2024-09-05 09:13:54 +0300

    [exif-js] Format files

--- types/exif-js/index.d.ts
+++ types/exif-js/index.d.ts
@@ -4,49 +4,49 @@ import type { ExifData } from "./exif-data";
 import type { EXIF_TAGS } from "./tags";
 
 interface ExifFile extends File {
-  exifdata: ExifData.Data;
-  iptcdata: object;
+    exifdata: ExifData.Data;
+    iptcdata: object;
 }
 
 type getDataCallback = (this: ExifFile, title: string, message: string) => void;
 
 interface ExifStatic {
-  /**
-   * Fills file with readable exifData
-   *
-   * @param file - could be item of files from input[type=file] or URL (by docs, not tested)
-   * @param callback - function receiving file with exif tags
-   */
-  getData(file: string | ExifFile, callback: getDataCallback): void;
+    /**
+     * Fills file with readable exifData
+     *
+     * @param file - could be item of files from input[type=file] or URL (by docs, not tested)
+     * @param callback - function receiving file with exif tags
+     */
+    getData(file: string | ExifFile, callback: getDataCallback): void;
 
-  /**
-   * Returns value of exif tag
-   *
-   * @param file - could be item of files from input[type=file]
-   * @param tag - name of exif tag
-   */
-  getTag(file: ExifFile, tag: EXIF_TAGS.ALL): any;
+    /**
+     * Returns value of exif tag
+     *
+     * @param file - could be item of files from input[type=file]
+     * @param tag - name of exif tag
+     */
+    getTag(file: ExifFile, tag: EXIF_TAGS.ALL): any;
 
-  /**
-   * Returns all exif tags from file
-   *
-   * @param file - could be item of files from input[type=file]
-   */
-  getAllTags(file: ExifFile): ExifData.Data;
+    /**
+     * Returns all exif tags from file
+     *
+     * @param file - could be item of files from input[type=file]
+     */
+    getAllTags(file: ExifFile): ExifData.Data;
 
-  /**
-   * Returns JSON with exif tags from file
-   *
-   * @param file - could be item of files from input[type=file]
-   */
-  pretty(file: ExifFile): string;
+    /**
+     * Returns JSON with exif tags from file
+     *
+     * @param file - could be item of files from input[type=file]
+     */
+    pretty(file: ExifFile): string;
 
-  /**
-   * Gets exif tags from binary file
-   *
-   * @param file - binary file which will be sent to new DataView(file)
-   */
-  readFromBinaryFile(file: ArrayBuffer /*| SharedArrayBuffer*/): ExifData.Data;
+    /**
+     * Gets exif tags from binary file
+     *
+     * @param file - binary file which will be sent to new DataView(file)
+     */
+    readFromBinaryFile(file: ArrayBuffer /*| SharedArrayBuffer*/): ExifData.Data;
 }
 
 /**
@@ -56,4 +56,4 @@ declare const EXIF: ExifStatic;
 
 export default EXIF;
 
-export { getDataCallback, ExifFile, ExifStatic, ExifData, EXIF_TAGS };
+export { EXIF_TAGS, ExifData, ExifFile, ExifStatic, getDataCallback };
