dcafa6bc99d4e35c4ea7c3ff095bc7d78eafbd5c
David Sherret <dsherret@gmail.com>
2024-09-09 18:15:07 -0400

    Add @types/deno

---
+++ types/deno/deno-tests.ts
@@ -0,0 +1,7 @@
+// The declarations are code generated in the Deno repo at
+// https://github.com/denoland/deno/blob/main/tools/generate_types_deno.ts
+// so this this file contains only basic tests to ensure the `Deno` global
+// exists.
+
+Deno.mkdirSync("dir"); // $ExpectType void
+Deno.readTextFileSync("dir/file.txt"); // $ExpectType string
