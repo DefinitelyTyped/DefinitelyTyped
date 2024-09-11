a8857998a939485e1506c71185b6e6b5e615c454
Kira Oakley <kira@eight45.net>
2024-09-11 11:32:46 -0700

    Run "pnpm dprint fmt".

--- types/video-name-parser/index.d.ts
+++ types/video-name-parser/index.d.ts
@@ -1,17 +1,17 @@
 declare namespace parse {
-  interface VideoMetadata {
-    name: string
-    year?: number
-    aired?: string
-    season?: number
-    episode?: number[]
-    diskNumber?: number
-    imdb_id?: string
-    type: string
-    tag: string[]
-  }
+    interface VideoMetadata {
+        name: string;
+        year?: number;
+        aired?: string;
+        season?: number;
+        episode?: number[];
+        diskNumber?: number;
+        imdb_id?: string;
+        type: string;
+        tag: string[];
+    }
 }
 
-declare function parse(filename: string): parse.VideoMetadata
+declare function parse(filename: string): parse.VideoMetadata;
 
-export = parse
+export = parse;
