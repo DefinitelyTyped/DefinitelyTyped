b0b981c72bdfb7f2d8ec2b1a684e3a47d480f1fd
Jean-Pierre Carvalho <hello@jeanpierrecarvalho.pt>
2024-09-11 15:11:54 +0100

    feat(@types/snaply): add new types package

--- types/snaply/snaply-tests.ts
+++ types/snaply/snaply-tests.ts
@@ -1,40 +1,40 @@
-import { deepClone, isFunction, isObject } from 'snaply';
+import { deepClone, isFunction, isObject } from "snaply";
 
 {
-  const obj = { a: 1, b: { c: 2 } };
-  const cloned = deepClone(obj); // $ExpectType { a: number; b: { c: number; }; } 
-  cloned.b.c; // $ExpectType number
+    const obj = { a: 1, b: { c: 2 } };
+    const cloned = deepClone(obj); // $ExpectType { a: number; b: { c: number; }; }
+    cloned.b.c; // $ExpectType number
 
-  const arr = [1, 2, 3];
-  const clonedArr = deepClone(arr); // $ExpectType number[]
+    const arr = [1, 2, 3];
+    const clonedArr = deepClone(arr); // $ExpectType number[]
 
-  const map = new Map();
-  const clonedMap = deepClone(map); // $ExpectType  Map<any, any>
+    const map = new Map();
+    const clonedMap = deepClone(map); // $ExpectType  Map<any, any>
 
-  const weakMap = new WeakMap();
-  const clonedWeakMap = deepClone(weakMap); // $ExpectType  WeakMap<object, any>
+    const weakMap = new WeakMap();
+    const clonedWeakMap = deepClone(weakMap); // $ExpectType  WeakMap<object, any>
 }
 
 {
     function fn() {}
-  const notFunc = 123;
+    const notFunc = 123;
 
-  isFunction(fn); // $ExpectType boolean
-  isFunction(notFunc); // $ExpectType boolean
+    isFunction(fn); // $ExpectType boolean
+    isFunction(notFunc); // $ExpectType boolean
 
-  if (isFunction(fn)) {
-    fn(); // $ExpectType void (no need to test the execution here, the function type guard suffices)
-  }
+    if (isFunction(fn)) {
+        fn(); // $ExpectType void (no need to test the execution here, the function type guard suffices)
+    }
 }
 
 {
-  const obj = { a: 1 };
-  const notObj = 123;
+    const obj = { a: 1 };
+    const notObj = 123;
 
-  isObject(obj); // $ExpectType boolean
-  isObject(notObj); // $ExpectType boolean
+    isObject(obj); // $ExpectType boolean
+    isObject(notObj); // $ExpectType boolean
 
-  if (isObject(obj)) {
-    obj.a; // $ExpectType number
-  }
+    if (isObject(obj)) {
+        obj.a; // $ExpectType number
+    }
 }
