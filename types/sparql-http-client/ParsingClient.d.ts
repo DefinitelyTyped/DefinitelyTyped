a8a0572a62520aa5f9bb59c72eab1b4e6c3d6d84
Tomasz Pluskiewicz <tpluscode@users.noreply.github.com>
2024-09-13 10:18:36 +0200

    fix(sparql-http-client): make it possible to init from another instance

--- types/sparql-http-client/ParsingClient.d.ts
+++ types/sparql-http-client/ParsingClient.d.ts
@@ -4,30 +4,16 @@ import { Client } from "./index.js";
 import ParsingQuery from "./ParsingQuery.js";
 import SimpleClient from "./SimpleClient.js";
 
-interface BaseOptions<Q extends Quad, D extends DatasetCore<Q>> {
+export interface Options<Q extends Quad = Quad, D extends DatasetCore<Q> = DatasetCore<Q>> {
     factory?: Environment<DatasetCoreFactory<Q, Q, D>>;
     fetch?: typeof fetch;
     headers?: HeadersInit;
     password?: string;
     user?: string;
+    endpointUrl?: string;
+    storeUrl?: string;
+    updateUrl?: string;
 }
-
-interface OptionWithQueryEndpoint<Q extends Quad, D extends DatasetCore<Q>> extends BaseOptions<Q, D> {
-    endpointUrl: string;
-}
-
-interface OptionWithStoreEndpoint<Q extends Quad, D extends DatasetCore<Q>> extends BaseOptions<Q, D> {
-    storeUrl: string;
-}
-
-interface OptionWithUpdateEndpoint<Q extends Quad, D extends DatasetCore<Q>> extends BaseOptions<Q, D> {
-    updateUrl: string;
-}
-
-export type Options<Q extends Quad = Quad, D extends DatasetCore<Q> = DatasetCore<Q>> =
-    | OptionWithQueryEndpoint<Q, D>
-    | OptionWithStoreEndpoint<Q, D>
-    | OptionWithUpdateEndpoint<Q, D>;
 
 export type ParsingClient<D extends DatasetCore = DatasetCore> = Client<ParsingQuery<D>>;
 
