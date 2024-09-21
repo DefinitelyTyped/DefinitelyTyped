95e72a9ab0ffcd209f06ba9ebb2b7381c28b937f
kaplego <70806521+kaplego@users.noreply.github.com>
2024-09-21 18:14:37 +0200

    Definitions by kaplego

--- types/jsencrypt/index.d.ts
+++ types/jsencrypt/index.d.ts
@@ -1,5 +1,2 @@
-/*~ If this module is a UMD module that exposes a global variable 'myLib' when
- *~ loaded outside a module loader environment, declare that global here.
- *~ Otherwise, delete this declaration.
- */
+// Definitions by: kaplego <https://github.com/kaplego>
 export { JSEncrypt } from "./JSEncrypt";
