f1c86e834fd05d3e40c0fb674fbb2d849273462d
Oleg Bespalov <oleg.bespalov@grafana.com>
2024-09-23 14:08:59 +0200

    Fix for the gRPC test, expect the only type

--- types/k6/test/grpc.ts
+++ types/k6/test/grpc.ts
@@ -48,7 +48,7 @@ stream.on("data", data => {
 });
 
 stream.on("data", (_, metadata) => {
-    metadata; // $ExpectType object | StreamMessageMetadata
+    metadata; // $ExpectType StreamMessageMetadata
 });
 
 stream.write({ latitude: 410248224, longitude: -747127767 });
