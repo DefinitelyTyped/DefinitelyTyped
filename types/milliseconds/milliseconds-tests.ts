87f48ce5f15be7b793c975b3ef43136a8ac54581
Jimmy Leung <43258070+hkleungai@users.noreply.github.com>
2024-09-24 16:11:55 +0800

    feat(milliseconds): bump version, modify tests

--- types/milliseconds/milliseconds-tests.ts
+++ types/milliseconds/milliseconds-tests.ts
@@ -1,7 +1,7 @@
-milliseconds.seconds(1);
-milliseconds.minutes(2);
-milliseconds.hours(3);
-milliseconds.days(4);
-milliseconds.weeks(5);
-milliseconds.months(6);
-milliseconds.years(7);
+milliseconds.seconds(1); // $ExpectType number
+milliseconds.minutes(2); // $ExpectType number
+milliseconds.hours(3); // $ExpectType number
+milliseconds.days(4); // $ExpectType number
+milliseconds.weeks(5); // $ExpectType number
+milliseconds.months(6); // $ExpectType number
+milliseconds.years(7); // $ExpectType number
