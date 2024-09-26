f67142829d04906310f05ba5f3bff98f2da9405a
Thorsten Fall <thorsten.fall@ni.com>
2024-09-26 11:42:37 +0200

    adapt index.d.ts

--- types/eval-estree-expression/index.d.ts
+++ types/eval-estree-expression/index.d.ts
@@ -1,1 +1,7 @@
-declare module 'eval-estree-expression';
+export default eval_estree_expression;
+
+declare namespace eval_estree_expression {
+    namespace evaluate {
+        function sync(tree: any, context: any, options: any): any;
+    }
+}
