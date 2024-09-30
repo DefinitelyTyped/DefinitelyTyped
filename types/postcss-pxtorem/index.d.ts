a33986bc363baa93d966b077d28ffd8aa8049e4c
cwx <741621270@qq.com>
2024-09-30 15:44:15 +0800

    Updates postcss-pxtorem types

--- types/postcss-pxtorem/index.d.ts
+++ types/postcss-pxtorem/index.d.ts
@@ -1,8 +1,8 @@
-import { PluginCreator } from "postcss";
+import { Input, PluginCreator } from "postcss";
 
 declare namespace PostcssPxToRem {
     interface Options {
-        rootValue?: number | ((pixelValue: number) => number);
+        rootValue?: number | ((options: Input) => number);
         unitPrecision?: number;
         propList?: string[];
         selectorBlackList?: Array<string | RegExp>;
@@ -10,6 +10,31 @@ declare namespace PostcssPxToRem {
         mediaQuery?: boolean;
         minPixelValue?: number;
         exclude?: string | RegExp | ((file: string) => boolean);
+
+        /**
+         * @deprecated use `rootValue` instead
+         */
+        root_value?: number | ((options: Input) => number);
+        /**
+         * @deprecated use `unitPrecision` instead
+         */
+        unit_precision?: number;
+        /**
+         * @deprecated use `selectorBlackList` instead
+         */
+        selector_black_list?: Array<string | RegExp>;
+        /**
+         * @deprecated use `propList` instead
+         */
+        prop_white_list?: string[];
+        /**
+         * @deprecated use `mediaQuery` instead
+         */
+        media_query?: boolean;
+        /**
+         * @deprecated use `propList` instead
+         */
+        propWhiteList?: string[];
     }
 }
 
