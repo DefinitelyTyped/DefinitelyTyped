c1410b545dc08001ab1c6d632c95eee12262f38d
KA\Fall <thorsten.fall@ni.com>
2024-09-30 12:46:25 +0200

    add tests

---
+++ types/eval-estree-expression/eval-estree-expression-tests.ts
@@ -0,0 +1,24 @@
+import { evaluate } from "./index";
+
+const tree = {
+    type: "BinaryExpression",
+    operator: "==",
+    left: {
+        type: "Identifier",
+        name: "a"
+    },
+    right: {
+        type: "Literal",
+        value: 3,
+        raw: "3"
+    }
+};
+
+const context = { a: 3 };
+const options = {};
+
+(async () => {
+    await evaluate(tree, context, options); // $ExpectType boolean
+})();
+
+evaluate.sync(tree, context, options); // $ExpectType boolean
