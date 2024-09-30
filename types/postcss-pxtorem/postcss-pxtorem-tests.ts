18e2e6a5bea48e6c8de52a4a22271d5394fb1c62
VignaAngularis <115332873+VignaAngularis@users.noreply.github.com>
2024-09-30 16:16:13 +0800

    Updates postcss-pxtorem types

--- types/postcss-pxtorem/postcss-pxtorem-tests.ts
+++ types/postcss-pxtorem/postcss-pxtorem-tests.ts
@@ -36,3 +36,39 @@ pxtorem({ minPixelValue: "2" });
 pxtorem({ exclude: "file" });
 // @ts-expect-error
 pxtorem({ exclude: 2 });
+
+pxtorem({ rootValue: () => 16 });
+// @ts-expect-error
+pxtorem({ rootValue: () => "16px" });
+
+pxtorem({ root_value: 16 });
+// @ts-expect-error
+pxtorem({ root_value: "16px" });
+
+pxtorem({ root_value: () => 16 });
+// @ts-expect-error
+pxtorem({ root_value: () => "16px" });
+
+pxtorem({ unit_precision: 5 });
+// @ts-expect-error
+pxtorem({ unit_precision: "5" });
+
+pxtorem({ selector_black_list: ["body"] });
+// @ts-expect-error
+pxtorem({ selector_black_list: [false] });
+
+pxtorem({ prop_white_list: ["*"] });
+// @ts-expect-error
+pxtorem({ prop_white_list: [false] });
+
+pxtorem({ media_query: true });
+// @ts-expect-error
+pxtorem({ media_query: "true" });
+
+pxtorem({ propWhiteList: ["*"] });
+// @ts-expect-error
+pxtorem({ propWhiteList: [false] });
+
+pxtorem({ unit: "px" });
+// @ts-expect-error
+pxtorem({ unit: 0 });
