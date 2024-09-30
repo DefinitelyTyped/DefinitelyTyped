346a4f62809dd89e04ba16521b0b4ce19642a644
VignaAngularis <115332873+VignaAngularis@users.noreply.github.com>
2024-09-30 16:40:36 +0800

    Optimize PostcssPxToRem TypeScript declarations

--- types/postcss-pxtorem/index.d.ts
+++ types/postcss-pxtorem/index.d.ts
@@ -1,44 +1,46 @@
 import { Input, PluginCreator } from "postcss";
 
-declare namespace PostcssPxToRem {
-    interface Options {
-        rootValue?: number | ((options: Input) => number);
-        unitPrecision?: number;
-        propList?: string[];
-        selectorBlackList?: Array<string | RegExp>;
-        replace?: boolean;
-        mediaQuery?: boolean;
-        minPixelValue?: number;
-        exclude?: string | RegExp | ((file: string) => boolean);
-        unit?: string;
-
-        /**
-         * @deprecated use `rootValue` instead
-         */
-        root_value?: number | ((options: Input) => number);
-        /**
-         * @deprecated use `unitPrecision` instead
-         */
-        unit_precision?: number;
-        /**
-         * @deprecated use `selectorBlackList` instead
-         */
-        selector_black_list?: Array<string | RegExp>;
-        /**
-         * @deprecated use `propList` instead
-         */
-        prop_white_list?: string[];
-        /**
-         * @deprecated use `mediaQuery` instead
-         */
-        media_query?: boolean;
-        /**
-         * @deprecated use `propList` instead
-         */
-        propWhiteList?: string[];
-    }
+interface BaseOptions {
+    rootValue?: number | ((input: Input) => number);
+    unitPrecision?: number;
+    propList?: string[];
+    selectorBlackList?: Array<string | RegExp>;
+    replace?: boolean;
+    mediaQuery?: boolean;
+    minPixelValue?: number;
+    exclude?: string | RegExp | ((file: string) => boolean);
+    unit?: string;
 }
 
-declare var pxtorem: PluginCreator<PostcssPxToRem.Options>;
+interface LegacyOptions {
+    /**
+     * @deprecated use `rootValue` instead
+     */
+    root_value?: number | ((input: Input) => number);
+    /**
+     * @deprecated use `unitPrecision` instead
+     */
+    unit_precision?: number;
+    /**
+     * @deprecated use `selectorBlackList` instead
+     */
+    selector_black_list?: Array<string | RegExp>;
+    /**
+     * @deprecated use `propList` instead
+     */
+    prop_white_list?: string[];
+    /**
+     * @deprecated use `mediaQuery` instead
+     */
+    media_query?: boolean;
+    /**
+     * @deprecated use `propList` instead
+     */
+    propWhiteList?: string[];
+}
+
+interface PostcssPxToRemOptions extends BaseOptions, LegacyOptions {}
+
+declare var pxtorem: PluginCreator<PostcssPxToRemOptions>;
 
 export = pxtorem;
