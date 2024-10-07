54dde6b08f8839cac7a20402ed5a7ee915ba074d
Sri Harsha <sri_harsha509@hotmail.com>
2024-10-07 19:36:25 -0400

    [selenium-webdriver]: type enhancements for bidi

---
+++ types/selenium-webdriver/bidi/UrlPattern.d.ts
@@ -0,0 +1,18 @@
+
+
+declare class UrlPattern {
+
+    protocol(protocol: string) : UrlPattern;
+
+    hostname(hostname: string) : UrlPattern;
+
+    port(port: number): UrlPattern;
+
+    pathname(pathname: string) : UrlPattern;
+
+    search(search: string): UrlPattern;
+
+    asMap(): Map<string, string>;
+}
+
+export { UrlPattern };
