a8f3a4b107ad2480086505f08ecb27dee58f780e
Star Chen <sidachen2001@hotmail.com>
2024-10-08 11:54:26 -0500

    Added missing type definitions and fixed the result types.

--- types/amazon-product-api/index.d.ts
+++ types/amazon-product-api/index.d.ts
@@ -35,10 +35,16 @@ interface IBrowseNodeLookupOptions {
     responseGroup?: string | undefined;
 }
 
+interface ISimilarityLookupOptions{
+    similarityType?: string | undefined;
+    responseGroup?: string | undefined;
+}
+
 interface IAmazonProductClient {
-    itemSearch(query: IItemSearchOptions, callback?: IAmazonProductQueryCallback): Promise<Object[]>;
-    itemLookup(query: IItemLookupOptions, callback?: IAmazonProductQueryCallback): Promise<Object[]>;
-    browseNodeLookup(query: IBrowseNodeLookupOptions, callback?: IAmazonProductQueryCallback): Promise<Object[]>;
+    itemSearch(query: IItemSearchOptions, callback?: IAmazonProductQueryCallback): Promise<Object[]> | undefined;
+    itemLookup(query: IItemLookupOptions, callback?: IAmazonProductQueryCallback): Promise<Object[]> | undefined;
+    browseNodeLookup(query: IBrowseNodeLookupOptions, callback?: IAmazonProductQueryCallback): Promise<Object[]> | undefined;
+    similarityLookup(query: ISimilarityLookupOptions, callback?: IAmazonProductQueryCallback): Promise<Object[]> | undefined;
 }
 
 export declare function createClient(credentials: ICredentials): IAmazonProductClient;
