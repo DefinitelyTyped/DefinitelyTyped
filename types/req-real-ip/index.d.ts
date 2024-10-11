37b4e6d7e31e2000b58ade45be1fc8ba0e4968af
asaf <asaf@utopia.tech>
2024-10-11 12:08:01 +0300

    fix types

--- types/req-real-ip/index.d.ts
+++ types/req-real-ip/index.d.ts
@@ -1,11 +1,35 @@
 /// <reference types="node" />
-import type { IncomingMessage } from "node:http";
+import * as http from "http";
+export interface RequestHeaders extends http.IncomingHttpHeaders {
+    "x-client-ip"?: string | undefined;
+    "x-forwarded-for"?: string | undefined;
+    "x-real-ip"?: string | undefined;
+    "x-cluster-client-ip"?: string | undefined;
+    "x-forwarded"?: string | undefined;
+    "forwarded-for"?: string | undefined;
+    "forwarded"?: string | undefined;
+}
+export interface Request {
+    headers?: RequestHeaders;
+    connection?: {
+        remoteAddress?: string | undefined;
+        socket?: {
+            remoteAddress?: string | undefined;
+        } | undefined;
+    } | undefined;
+    info?: {
+        remoteAddress?: string | undefined;
+    } | undefined;
+    socket?: {
+        remoteAddress?: string | undefined;
+    } | undefined;
+}
 
 export function detect({
     req,
     config,
 }: {
-    req: IncomingMessage;
+    req: Request;
     config: {
         cloudflare?: boolean;
     };
