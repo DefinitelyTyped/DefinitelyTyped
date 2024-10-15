6c121f0ec0b89f3d0532d4a44b157d864f357d7c
Jimmy Leung <43258070+hkleungai@users.noreply.github.com>
2024-10-16 01:11:12 +0800

    feat: Enable not-needed.js to also remove package in attw.json

--- scripts/not-needed.js
+++ scripts/not-needed.js
@@ -3,14 +3,17 @@
 import * as fs from "node:fs";
 import * as path from "node:path";
 
-const typingsPackageName = process.argv[2];
-const asOfVersion = process.argv[3];
-const libraryName = process.argv[4] || typingsPackageName;
+import attw from "../attw.json" with { type: "json" };
+import notNeededPackages from "../notNeededPackages.json" with { type: "json" };
 
 if (process.argv.length !== 4 && process.argv.length !== 5) {
     console.log("Usage: pnpm run not-needed typingsPackageName asOfVersion [libraryName]");
     process.exit(1);
 }
+
+const typingsPackageName = process.argv[2];
+const asOfVersion = process.argv[3];
+const libraryName = process.argv[4] || typingsPackageName;
 
 fs.rmSync(path.join("types", typingsPackageName), {
     recursive: true,
@@ -18,8 +21,14 @@ fs.rmSync(path.join("types", typingsPackageName), {
     maxRetries: process.platform === "win32" ? 10 : 0,
 });
 
-/**  @type  {{packages: Record<string, { libraryName: string; asOfVersion: string; }> }} */
-const notNeededPackages = JSON.parse(fs.readFileSync("notNeededPackages.json", "utf-8"));
-notNeededPackages.packages[typingsPackageName] = { libraryName, asOfVersion };
-notNeededPackages.packages = Object.fromEntries(Object.entries(notNeededPackages.packages).sort());
-fs.writeFileSync("notNeededPackages.json", JSON.stringify(notNeededPackages, undefined, 4) + "\n", "utf-8");
+/** @type {Record<string, Record<"libraryName" | "asOfVersion", string>>} */
+let packages = notNeededPackages.packages;
+packages[typingsPackageName] = { libraryName, asOfVersion };
+packages = Object.fromEntries(Object.entries(packages).sort());
+fs.writeFileSync("notNeededPackages.json", JSON.stringify({ packages }, undefined, 4) + "\n", "utf-8");
+
+const packageWithOptionalVersionRegex = new RegExp(`^${typingsPackageName}(/v\\d+)?$`);
+attw.failingPackages = attw.failingPackages.filter((failingPackage) => (
+    !packageWithOptionalVersionRegex.test(failingPackage)
+));
+fs.writeFileSync("attw.json", JSON.stringify(attw, undefined, 4) + "\n", "utf-8");
