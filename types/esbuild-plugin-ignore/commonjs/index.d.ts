e35adc5ca99e0c01c84796641ca97b4c985518f5
ashkuc <ashkucc@gmail.com>
2024-10-19 03:13:36 +0200

    removed duplication

--- types/esbuild-plugin-ignore/commonjs/index.d.ts
+++ types/esbuild-plugin-ignore/commonjs/index.d.ts
@@ -1,23 +1,4 @@
-import { Plugin } from 'esbuild';
-
-interface IgnoreOption {
-    /**
-     * A regular expression to match the resource paths to ignore.
-     * @example `/pg-native$/`
-     */
-    resourceRegExp: RegExp;
-
-    /**
-     * A regular expression to match the context (directory or module path).
-     * @example `/node_modules\\/sequelize|node_modules\\/pg/`
-     */
-    contextRegExp: RegExp;
-}
-
-interface IgnorePluginInstance {
-    name: 'ignore';
-    setup: Plugin['setup'];
-}
+import {IgnoreOption, IgnorePluginInstance} from '../types'
 
 declare function ignore(options: IgnoreOption[]): IgnorePluginInstance;
 
