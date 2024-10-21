bc9643683a1def0aad273c5e9c522c0f6e61ddae
Ha-limLee <leehalim09@gmail.com>
2024-10-21 23:04:59 +0900

    enhance _.capitalize to return a capitalized string type

--- types/lodash/lodash-tests.ts
+++ types/lodash/lodash-tests.ts
@@ -6623,10 +6623,23 @@ fp.now(); // $ExpectType number
 
 // _.capitalize
 {
-    _.capitalize("fred"); // $ExpectType string
-    _("fred").capitalize(); // $ExpectType string
-    _.chain("fred").capitalize(); // $ExpectType StringChain<string>
-    fp.capitalize("fred"); // $ExpectType string
+    _.capitalize("fred"); // $ExpectType "Fred"
+    _.capitalize("fred" as string); // $ExpectType Capitalize<string>
+    // @ts-expect-error cannot assign non string type
+    _.capitalize(123);
+
+    _("fred").capitalize(); // $ExpectType "Fred"
+    _("fred" as string).capitalize(); // $ExpectType Capitalize<string>
+    _(123).capitalize(); // $ExpectType never
+
+    _.chain("fred").capitalize(); // $ExpectType StringChain<"Fred">
+    _.chain("fred" as string).capitalize(); // $ExpectType StringChain<Capitalize<string>>
+    _.chain(123).capitalize(); // $ExpectType StringChain<never>
+
+    fp.capitalize("fred"); // $ExpectType "Fred"
+    fp.capitalize("fred" as string); // $ExpectType Capitalize<string>
+    // @ts-expect-error cannot assign non string type
+    fp.capitalize(123);
 }
 
 // _.deburr
