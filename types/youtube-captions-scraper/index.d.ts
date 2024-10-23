2c2556608393b9398b4a4f12364cc49799c44976
oof2win2 <honza.koco44@gmail.com>
2024-10-23 15:17:52 -0700

    add types for youtube-captions-scraper

---
+++ types/youtube-captions-scraper/index.d.ts
@@ -0,0 +1,13 @@
+export interface Subtitle {
+    start: number
+    dur: number
+    text: string
+}
+
+export type SubtitleOutput = Subtitle[]
+
+export function getSubtitles({ videoID, lang }: {
+    videoID: string,
+    lang: string
+}): Promise<SubtitleOutput>
+
---
+++ types/youtube-captions-scraper/youtube-captions-scraper-tests.ts
@@ -0,0 +1,17 @@
+
+import { getSubtitles, SubtitleOutput, Subtitle } from 'youtube-captions-scraper';
+
+// $ExpectType Promise<SubtitleOutput>
+const subtitles = getSubtitles({ videoID: "abc", lang: "en" })
+
+const awaited = [] as SubtitleOutput
+
+// $ExpectType Subtitle
+const subtitle = awaited[0]
+
+// $ExpectType number
+const start = subtitle.start
+// $ExpectType number
+const duration = subtitle.dur
+// $ExpectType string
+const text = subtitle.text
