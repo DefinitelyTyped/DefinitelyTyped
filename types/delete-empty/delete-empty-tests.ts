f7dee533c8bf591aa0df77c935c10409b87a8af8
WanGe2000 <zhouxiang@wanjiadongli.com>
2024-10-23 20:38:10 +0800

    format file

--- types/delete-empty/delete-empty-tests.ts
+++ types/delete-empty/delete-empty-tests.ts
@@ -17,7 +17,7 @@ deleteEmpty("foo", (err, f) => {
 });
 
 deleteEmpty("foo", {
-    filter: (file, regex) => file.startsWith('foo')
+    filter: (file, regex) => file.startsWith("foo"),
 }, (err, f) => {
     if (err) {
         err.message;
@@ -31,5 +31,5 @@ deleteEmpty.sync("foo")[0].toLowerCase();
 (async () => {
     const deleted = await deleteEmpty("foo");
     const moreDeleted = await deleteEmpty("bar", { dryRun: true });
-    const deleteWithFilter = await deleteEmpty("bar", { filter: (file, regex) => file.startsWith('bar') });
+    const deleteWithFilter = await deleteEmpty("bar", { filter: (file, regex) => file.startsWith("bar") });
 })();
