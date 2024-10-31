bbae89f54d5fdbb48f98f983f1980d39cce96139
Antonio Ivanovski <toteto@users.noreply.github.com>
2024-10-31 12:35:57 +0100

    add digitalbazaar__bitstring

---
+++ types/digitalbazaar__bitstring/digitalbazaar__bitstring-tests.ts
@@ -0,0 +1,12 @@
+import { Bitstring } from "@digitalbazaar/bitstring";
+
+Bitstring.decodeBits({ encoded: 'abc' }) // $ExpectType Promise<Uint8Array>
+Bitstring.uncompressBits({ compressed: new Uint8Array([]) }) // $ExpectType Promise<Uint8Array>
+new Bitstring({ length: 1 }) // $ExpectType Bitstring
+new Bitstring({ buffer: new Uint8Array([]) }) // $ExpectType Bitstring
+
+const bitstring = new Bitstring({ length: 1 });
+bitstring.compressBits(); // $ExpectType Promise<Uint8Array>
+bitstring.encodeBits(); // $ExpectType Promise<string>
+bitstring.get(0); // $ExpectType boolean
+bitstring.set(0, true); // $ExpectType void
