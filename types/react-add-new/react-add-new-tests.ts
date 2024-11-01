de4e3f151600b62ca37e61396d42ef7c86c61714
Antonis Balasas <axaros@gmail.com>
2024-11-02 00:10:12 +0200

    Added types for react-add-new package

---
+++ types/react-add-new/react-add-new-tests.ts
@@ -0,0 +1,10 @@
+import { validateName, createComponent, createContext } from "react-add-new";
+
+// $ExpectType true | string
+const result = validateName("foo");
+
+// $ExpectType Promise<CreateStatus>
+createComponent("Foo", "./", true, true);
+
+// $ExpectType Promise<CreateStatus>
+createContext("Foo", "./");
