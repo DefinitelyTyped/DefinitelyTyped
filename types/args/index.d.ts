e2e61919e2028cc7a8b382574365deedb90877f3
Golf0ned <benjaminye.email@gmail.com>
2024-11-07 17:33:39 +0000

    initial work on args

--- types/args/index.d.ts
+++ types/args/index.d.ts
@@ -1,9 +1,14 @@
+// Type definitions for args 5.0
+// Project: https://github.com/leo/args#readme
+// Definitions by: Slessi <https://github.com/Slessi>
+// Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
+
 declare const c: args;
 export = c;
 
-interface args {
-    sub: string[];
+console.log(typeof details);
 
+interface args {
     option(name: string | [string, string], description: string, defaultValue?: any, init?: OptionInitFunction): args;
     options(list: Option[]): args;
     command(
@@ -17,18 +22,39 @@ interface args {
     parse(argv: string[], options?: ConfigurationOptions): { [key: string]: any };
     showHelp(): void;
     showVersion(): void;
+    // new
+    details: {options: {defaultValue: any, usage: any, description: any}[], commands: Command[], examples: Example[]}; // TODO: un-any this
+    config: any;
+    printMainColor: any;
+    printSubColor: any;
+    Args: any;
+    handleType: any;
+    readOption: any;
+    getOptions: any;
+    generateExamples: any;
+    generateDetails: any;
+    runCommand: any;
+    checkHelp: any;
+    checkVersion: any;
+    isDefined: any;
+    optionWasProvided: any;
+    raw?: any;
+    binary?: any;
+    sub?: string[];
+    // test props
+    reset?: any;
+    suppressOutput?: any;
 }
-
 type OptionInitFunction = (value: any) => any;
 
 interface MriOptions {
     args?: string[] | undefined;
     alias?: {
-        [key: string]: string | string[];
+        [key: string]: string | string[]
     } | undefined;
     boolean?: string | string[] | undefined;
     default?: {
-        [key: string]: any;
+        [key: string]: any
     } | undefined;
     string?: string | string[] | undefined;
     unknown?: ((param: string) => boolean) | undefined;
@@ -38,10 +64,10 @@ interface MinimistOptions {
     string?: string | string[] | undefined;
     boolean?: boolean | string | string[] | undefined;
     alias?: {
-        [key: string]: string | string[];
+        [key: string]: string | string[]
     } | undefined;
     default?: {
-        [key: string]: any;
+        [key: string]: any
     } | undefined;
     stopEarly?: boolean | undefined;
     "--"?: boolean | undefined;
@@ -71,3 +97,11 @@ interface Example {
     usage: string;
     description: string;
 }
+
+interface Command {
+    usage: string;
+    description: string;
+    init?: any;
+    aliases?: any;
+}
+
