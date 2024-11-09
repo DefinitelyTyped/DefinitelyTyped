735c3ccf23675bef5cdf6688103fde84e9e093da
Yiays <yesiateyoursheep@gmail.com>
2024-11-09 14:12:08 +1300

    Add mongo-objectid types

---
+++ types/mongo-objectid/index.d.ts
@@ -0,0 +1,17 @@
+declare module 'mongo-objectid' {
+    export default class ObjectId {
+      static UNIQUE_VALUE: Buffer;
+  
+      constructor(prop?: string|number);
+  
+      generateCounterValue(): Buffer;
+      fromHex(hex: string): void;
+      setMachineId(machineId?: Buffer): void;
+      getDate(): Date; getTimestamp(): number;
+      getCounterValue(): Buffer;
+      toHex(): string;
+      toString(): string;
+      getMachineId(): Buffer;
+      isValid(hex: string): boolean;
+    }
+  }
---
+++ types/mongo-objectid/mongo-objectid-tests.ts
@@ -0,0 +1,6 @@
+import ObjectId from "mongo-objectid";
+
+const objectid_hex = ObjectId("507f1f77bcf86cd799439011");
+const objectid_number = ObjectId(32);
+const objectid_new = ObjectId();
+
