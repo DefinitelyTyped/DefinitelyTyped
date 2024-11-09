78c8836e822ae8156ccf845034b3abaffbaf425e
Yiays <yesiateyoursheep@gmail.com>
2024-11-09 14:57:46 +1300

    Fix errors

--- types/mongo-objectid/mongo-objectid-tests.ts
+++ types/mongo-objectid/mongo-objectid-tests.ts
@@ -1,6 +1,6 @@
 import ObjectId from "mongo-objectid";
 
-const objectid_hex = ObjectId("507f1f77bcf86cd799439011");
-const objectid_number = ObjectId(32);
-const objectid_new = ObjectId();
+const objectid_hex = new ObjectId("507f1f77bcf86cd799439011");
+const objectid_number = new ObjectId(32);
+const objectid_new = new ObjectId();
 
