8e6044a13c36f9ebe98e20e42f2dcc3dde0e4ea6
Yiays <yesiateyoursheep@gmail.com>
2024-11-09 14:37:34 +1300

    Remove module declaration

--- types/mongo-objectid/index.d.ts
+++ types/mongo-objectid/index.d.ts
@@ -1,17 +1,15 @@
-declare module 'mongo-objectid' {
-    export default class ObjectId {
-      static UNIQUE_VALUE: Buffer;
-  
-      constructor(prop?: string|number);
-  
-      generateCounterValue(): Buffer;
-      fromHex(hex: string): void;
-      setMachineId(machineId?: Buffer): void;
-      getDate(): Date; getTimestamp(): number;
-      getCounterValue(): Buffer;
-      toHex(): string;
-      toString(): string;
-      getMachineId(): Buffer;
-      isValid(hex: string): boolean;
-    }
-  }
+declare default class ObjectId {
+  static UNIQUE_VALUE: Buffer;
+
+  constructor(prop?: string|number);
+
+  generateCounterValue(): Buffer;
+  fromHex(hex: string): void;
+  setMachineId(machineId?: Buffer): void;
+  getDate(): Date; getTimestamp(): number;
+  getCounterValue(): Buffer;
+  toHex(): string;
+  toString(): string;
+  getMachineId(): Buffer;
+  isValid(hex: string): boolean;
+}
