dcf10e7dbf0426fe0fc5ec5163f04e11dcd00df2
Jimmy Leung <43258070+hkleungai@users.noreply.github.com>
2024-11-10 12:11:41 +0800

    feat(react-native-ad-manager): remove types

--- types/react-native-ad-manager/index.d.ts
+++
@@ -1,9 +0,0 @@
-import Interstitial = require("./src/CTKAdManagerInterstitial");
-import Banner = require("./src/CTKAdManagerBanner");
-import NativeAdsManager = require("./src/native-ads/NativeAdsManager");
-import TriggerableView = require("./src/native-ads/TriggerableViewManager");
-import withNativeAd = require("./src/native-ads/withNativeAd");
-
-export { Banner, Interstitial, NativeAdsManager, TriggerableView, withNativeAd };
-
-export * from "./helper-types";
