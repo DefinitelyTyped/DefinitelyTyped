d746d31944d299bb423fa33ca30a313bd312fdf4
Arjuna Enait <arjuna@alienistsolution.com>
2024-11-13 17:43:55 +0100

    Ran pnpm dprint fmth

--- types/salling-group__auth/index.d.ts
+++ types/salling-group__auth/index.d.ts
@@ -1,31 +1,31 @@
-import { AxiosInstance} from 'axios'
+import { AxiosInstance } from "axios";
 
 export interface AuthOptions {
-  /** The type of authentication. */
-  type: 'jwt' | 'bearer';
+    /** The type of authentication. */
+    type: "jwt" | "bearer";
 
-  /** The issuer used for JWT. */
-  issuer?: string;
+    /** The issuer used for JWT. */
+    issuer?: string;
 
-  /** The secret used for JWT. */
-  secret?: string;
+    /** The secret used for JWT. */
+    secret?: string;
 
-  /** The token used for Bearer. */
-  token?: string;
+    /** The token used for Bearer. */
+    token?: string;
 }
 
 export interface InstanceOptions {
-  /** Credentials for the instance */
-  auth: AuthOptions;
+    /** Credentials for the instance */
+    auth: AuthOptions;
 
-  /**
-   * The name of the application which will use this instance.
-   * This will be sent in the user-agent header.
-   */
-  applicationName?: string;
+    /**
+     * The name of the application which will use this instance.
+     * This will be sent in the user-agent header.
+     */
+    applicationName?: string;
 
-  /** The base name. (For internal use.) */
-  baseName?: string;
+    /** The base name. (For internal use.) */
+    baseName?: string;
 }
 
 /**
@@ -33,9 +33,9 @@ export interface InstanceOptions {
  * This automatically injects authorization and sets the base URL.
  * Furthermore, it also wraps API errors into a designated error type.
  * The instance can be used directly on resources (e.g. 'instance.get('/v1/stores/')').
- * 
+ *
  * @param options Options for the instance
- * 
+ *
  * @returns An Axios instance that can access the Salling Group API.
  */
-export function createInstance(options: InstanceOptions): AxiosInstance
+export function createInstance(options: InstanceOptions): AxiosInstance;
--- types/salling-group__auth/salling-group__auth.tests.ts
+++ types/salling-group__auth/salling-group__auth.tests.ts
@@ -1,12 +1,12 @@
-import { createInstance } from 'salling-group__auth';
+import { createInstance } from "salling-group__auth";
 
 createInstance({
-  auth: {
-    type: 'jwt',
-    issuer: 'issuer',
-    secret: 'secret',
-    token: 'token'
-  },
-  applicationName: 'app',
-  baseName: 'base'
-})
+    auth: {
+        type: "jwt",
+        issuer: "issuer",
+        secret: "secret",
+        token: "token",
+    },
+    applicationName: "app",
+    baseName: "base",
+});
