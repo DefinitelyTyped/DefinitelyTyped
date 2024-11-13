e1e9e99f6d1142b6eee7bf01242d6dd09b099946
Izad Imran Tan <izad.mhdborhan@myeg.com.my>
2024-11-13 09:32:08 -0500

    Added 2 packages

---
+++ types/zetrix-encryption-nodejs/index.d.ts
@@ -0,0 +1,29 @@
+declare module 'keypair' {
+  export function getEncPublicKey(encPrivateKey: string): string
+
+  export function getAddress(encPublicKey: string): string
+
+  export function parsePrivateKey(encPrivateKey: string): string
+
+  export function parsePublicKey(encPublicKey: string): string
+
+  export function getKeyPair(): any
+
+  export function checkEncPrivateKey(encPrivateKey: string): boolean
+
+  export function checkEncPublicKey(encPublicKey: string): boolean
+
+  export function checkAddress(address: string): boolean
+}
+
+declare module 'keystore' {
+  export function encrypt(encPrivateKey: string, password: string, callback: any): void
+
+  export function decrypt(keystore: any, password: string, callback: any): string
+}
+
+declare module 'signature' {
+  export function sign(message: string, encPrivateKey: string): string
+
+  export function verify(message: any, signature: string, encPublicKey: any): boolean
+}
