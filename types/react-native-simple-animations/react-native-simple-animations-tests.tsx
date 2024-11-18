aa89e8f553e3934060d827e11da08c302a1e4302
422.io <iamwinner422@gmail.com>
2024-11-18 21:31:23 +0000

    Create react-native-simple-animations-tests.tsx

---
+++ types/react-native-simple-animations/react-native-simple-animations-tests.tsx
@@ -0,0 +1,38 @@
+import { SimpleAnimation } from 'react-native-simple-animations';
+import * as React from 'react';
+import { Text, View } from 'react-native';
+
+// Test basic usage
+const BasicTest: React.FC = () => (
+    <SimpleAnimation 
+        aim="in"
+        duration={1000}
+        movementType="slide"
+        direction="up"
+    >
+        <Text>Test content</Text>
+    </SimpleAnimation>
+);
+
+// Test all props
+const CompleteTest: React.FC = () => (
+    <SimpleAnimation 
+        aim="out"
+        animate={true}
+        animateOnUpdate={false}
+        delay={200}
+        direction="down"
+        distance={100}
+        duration={500}
+        fade={true}
+        friction={5}
+        movementType="spring"
+        staticType="bounce"
+        tension={100}
+        useNativeDriver={true}
+        style={{ marginTop: 10 }}
+    >
+        <View>
+            <Text>Complete test</Text>
+        </View>
+    </SimpleAnimation>
