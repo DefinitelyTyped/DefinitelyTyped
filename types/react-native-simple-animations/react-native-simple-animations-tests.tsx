9c26adeb7fe50df56ce56aa644806671bfcfc0f3
422.io <iamwinner422@gmail.com>
2024-11-18 22:09:25 +0000

    Update react-native-simple-animations-tests.tsx

--- types/react-native-simple-animations/react-native-simple-animations-tests.tsx
+++ types/react-native-simple-animations/react-native-simple-animations-tests.tsx
@@ -3,36 +3,41 @@ import * as React from 'react';
 import { Text, View } from 'react-native';
 
 // Test basic usage
-const BasicTest: React.FC = () => (
-    <SimpleAnimation 
-        aim="in"
-        duration={1000}
-        movementType="slide"
-        direction="up"
-    >
-        <Text>Test content</Text>
-    </SimpleAnimation>
-);
+function TestComponent(): JSX.Element {
+    return (
+        <SimpleAnimation 
+            aim="in"
+            duration={1000}
+            movementType="slide"
+            direction="up"
+        >
+            <Text>Test content</Text>
+        </SimpleAnimation>
+    );
+}
 
-// Test all props
-const CompleteTest: React.FC = () => (
-    <SimpleAnimation 
-        aim="out"
-        animate={true}
-        animateOnUpdate={false}
-        delay={200}
-        direction="down"
-        distance={100}
-        duration={500}
-        fade={true}
-        friction={5}
-        movementType="spring"
-        staticType="bounce"
-        tension={100}
-        useNativeDriver={true}
-        style={{ marginTop: 10 }}
-    >
-        <View>
-            <Text>Complete test</Text>
-        </View>
-    </SimpleAnimation>
+// Test avec toutes les props
+function CompleteTest(): JSX.Element {
+    return (
+        <SimpleAnimation 
+            aim="out"
+            animate={true}
+            animateOnUpdate={false}
+            delay={200}
+            direction="down"
+            distance={100}
+            duration={500}
+            fade={true}
+            friction={5}
+            movementType="spring"
+            staticType="bounce"
+            tension={100}
+            useNativeDriver={true}
+            style={{ marginTop: 10 }}
+        >
+            <View>
+                <Text>Complete test</Text>
+            </View>
+        </SimpleAnimation>
+    );
+}
