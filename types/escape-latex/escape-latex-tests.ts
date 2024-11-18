b0e23242433d18bd340ec96276c9992df732ba50
liangguirong <liangguirong4@h-partners.com>
2024-11-18 17:16:55 +0800

    Change preseveFormatting to preserveFormatting

--- types/escape-latex/escape-latex-tests.ts
+++ types/escape-latex/escape-latex-tests.ts
@@ -3,7 +3,7 @@ import lescape = require("escape-latex");
 lescape("String to be escaped here #yolo");
 
 lescape("Hello   World", {
-    preseveFormatting: true,
+    preserveFormatting: true,
     escapeMapFn: function(defaultEscapes, formattingEscapes) {
         formattingEscapes[" "] = "\\\\";
         return Object.assign({}, defaultEscapes, formattingEscapes);
