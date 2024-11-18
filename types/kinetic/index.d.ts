f1a14158bd7c13271fefc598ccf8f4a622c7e605
Isabel Duan <isabelduan@microsoft.com>
2024-11-17 23:28:05 -0800

    eslint

--- types/kinetic/index.d.ts
+++ types/kinetic/index.d.ts
@@ -247,7 +247,6 @@ declare namespace Kinetic {
     };
 
     interface ICollection {
-        // eslint-disable-next-line @typescript-eslint/ban-types
         apply(method: Function, val: any): any;
         each(func: () => any): any;
     }
