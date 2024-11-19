2335a2738feb3912c11d3a88336f78cee85aaaeb
Kees C. Bakker <k.bakker@hostedsolutions.nu>
2024-11-19 14:52:47 +0100

    Implement promises.

--- types/hubot/index.d.ts
+++ types/hubot/index.d.ts
@@ -8,28 +8,36 @@ export class Adapter extends EventEmitter {
     robot: Robot;
     constructor(robot: Robot);
 
-    send(envelope: Envelope, ...strings: string[]): void;
-    emote(envelope: Envelope, ...strings: string[]): void;
-    reply(envelope: Envelope, ...strings: string[]): void;
-    topic(envelope: Envelope, ...strings: string[]): void;
-    play(envelope: Envelope, ...strings: string[]): void;
+    send(envelope: Envelope, ...strings: string[]): Promise<any>;
+    emote(envelope: Envelope, ...strings: string[]): Promise<any>;
+    reply(envelope: Envelope, ...strings: string[]): Promise<any>;
+    topic(envelope: Envelope, ...strings: string[]): Promise<any>;
+    play(envelope: Envelope, ...strings: string[]): Promise<any>;
 
-    run(): void;
+    run(): Promise<void>;
     close(): void;
 
-    receive(message: Message): void;
+    receive(message: Message): Promise<any>;
     http(url: string): ScopedClient;
 
     /**
-     * @deprecated
+     * @deprecated Use @robot.brain
      * @returns an object containing all the users with property names
      * that are the user id and with the value set as the `User`.
      * You can iterate over them all with `for (const id in users())`
      */
-    users(): any;
+    users(): User[];
+
+    /* @deprecated Use @robot.brain */
     userForId(id: string, options?: {}): User;
+
+    /* @deprecated Use @robot.brain */
     userForName(name: string): User | null;
+
+    /* @deprecated Use @robot.brain */
     usersForRawFuzzyName(fuzzyName: string): User[];
+
+    /* @deprecated Use @robot.brain */
     usersForFuzzyName(fuzzyName: string): User[];
 }
 
@@ -47,7 +55,7 @@ export class DataStoreUnavailable extends Error {}
 export class Middleware<T extends Adapter = Adapter> {
     stack: Array<MiddlewareHandler<T>>;
     constructor(robot: Robot<T>);
-    execute(context: MiddlewareContext<T>, next: NextFunction, done: DoneFunction): void;
+    execute(context: MiddlewareContext<T>): Promise<boolean>;
     register(middleware: MiddlewareHandler<T>): void;
 }
 
@@ -66,7 +74,7 @@ export class Brain<A extends Adapter> extends EventEmitter {
      * that are the user id and with the value set as the `User`.
      * You can iterate over them all with `for (const id in users())`
      */
-    users(): any;
+    users(): User[];
     userForId(id: string, options?: {}): User;
     userForName(name: string): User | null;
     usersForRawFuzzyName(fuzzyName: string): User[];
@@ -86,13 +94,13 @@ export class Message {
     constructor(user: User, done?: boolean);
     id: string;
     user: User;
-    text: string | null;
     room: string;
+    done: boolean;
+    text?: string;
     finish(): void;
 }
 
 export class TextMessage extends Message {
-    text: string;
 
     constructor(user: User, text: string, id: string);
 
@@ -100,17 +108,11 @@ export class TextMessage extends Message {
     toString(): string;
 }
 
-export class EnterMessage extends Message {
-    text: null;
-}
+export class EnterMessage extends Message {}
 
-export class LeaveMessage extends Message {
-    text: null;
-}
+export class LeaveMessage extends Message {}
 
-export class TopicMessage extends TextMessage {
-    text: string;
-}
+export class TopicMessage extends TextMessage {}
 
 export class CatchAllMessage extends Message {
     message: Message;
@@ -127,7 +129,7 @@ export interface Envelope {
 export class Response<
     A extends Adapter = Adapter,
     M extends Message = Message,
-    R extends RegExpMatchArray | { [key: string]: string } = RegExpMatchArray,
+    R extends RegExpMatchArray | { [key: string]: string } = RegExpMatchArray
 > {
     robot: Robot<A>;
     match: R;
@@ -135,31 +137,48 @@ export class Response<
     envelope: Envelope;
 
     constructor(robot: Robot<A>, message: M, match: R);
-    send(...strings: string[]): void;
-    emote(...strings: string[]): void;
-    reply(...strings: string[]): void;
-    topic(...strings: string[]): void;
-    play(...strings: string[]): void;
-    locked(...strings: string[]): void;
+    send(...strings: string[]): Promise<any>;
+    emote(...strings: string[]): Promise<any>;
+    reply(...strings: string[]): Promise<any>;
+    topic(...strings: string[]): Promise<any>;
+    play(...strings: string[]): Promise<any>;
+    locked(...strings: string[]): Promise<any>;
     random<T>(items: T[]): T;
     finish(): void;
     http(url: string, options?: HttpOptions): ScopedClient;
 }
 
 export type ListenerCallback<A extends Adapter = Adapter, M extends Message = Message> = (
-    response: Response<A, M>,
-) => void;
-export type DoneFunction = () => void;
-export type NextFunction = (done: DoneFunction) => void;
-export interface MiddlewareContext<T extends Adapter = Adapter> {
-    response?: Response<T> | undefined;
+    response: Response<A, M>
+) => Promise<void>;
+
+type ListenerMatcher = (message: Message) => boolean;
+
+export class Listener<A extends Adapter = Adapter> {
+    options: {};
+    robot: Robot<A>;
+    matcher: ListenerMatcher;
+
+    constructor(robot: Robot<A>, matcher: ListenerMatcher, options: {} | null, callback: ListenerCallback<A, Message>);
+
+    call(message: Message, middleware: Middleware<A>): Promise<null | any>;
+}
+
+export class TextListener<A extends Adapter> extends Listener<A> {
+    constructor(robot: Robot<A>, regex: RegExp, options: {} | null, callback: ListenerCallback<A, TextMessage>);
+}
+
+export interface MiddlewareContext<A extends Adapter = Adapter, M extends Message = Message> {
+    response: Response<A, M>;
+    listener?: Listener<A>;
+    method?: string;
+    plaintext?: boolean;
     [key: string]: unknown;
 }
-export type MiddlewareHandler<T extends Adapter = Adapter> = (
-    context: MiddlewareContext<T>,
-    next: NextFunction,
-    done: DoneFunction,
-) => void;
+
+export type MiddlewareHandler<A extends Adapter = Adapter, M extends Message = Message> = (
+    context: MiddlewareContext<A, M>
+) => Promise<boolean>;
 
 export interface LogLevel {
     (...messages: any[]): void;
@@ -173,7 +192,7 @@ export interface Log {
     debug: LogLevel;
     info: LogLevel;
     notice: LogLevel;
-    warning: LogLevel;
+    warn: LogLevel;
     error: LogLevel;
 }
 
@@ -188,7 +207,8 @@ export class Robot<A extends Adapter = Adapter> {
     readonly errorHandlers: [];
     readonly onUncaughtException: (err: Error) => void;
     readonly datastore: null | DataStore;
-    readonly commands: [];
+    readonly commands: string[];
+    readonly listeners: Listener<A>[];
     readonly middleware: {
         listener: Middleware<A>;
         response: Middleware<A>;
@@ -200,8 +220,9 @@ export class Robot<A extends Adapter = Adapter> {
     readonly version: string;
     readonly server?: Server | undefined;
     readonly router: Express;
+    readonly shouldEnableHttpd: boolean;
 
-    constructor(adapterPath: string, adapter: string, httpd: boolean, name: string, alias?: string);
+    constructor(adapter: string | object, httpd: boolean, name: string, alias?: string);
     catchAll(callback: ListenerCallback<A, CatchAllMessage>): void;
     catchAll(options: {}, callback: ListenerCallback<A, CatchAllMessage>): void;
     emit(event: string | symbol, ...args: unknown[]): void;
@@ -217,20 +238,23 @@ export class Robot<A extends Adapter = Adapter> {
     listen(matcher: (message: Message) => boolean, callback: ListenerCallback<A>): void;
     listen(matcher: (message: Message) => boolean, options: {}, callback: ListenerCallback<A>): void;
     listenerMiddleware(middleware: MiddlewareHandler<A>): void;
+    load(path: string): Promise<void>;
+    loadAdapter(): Promise<void>;
+    loadAdapter(adapterPath: string): Promise<void>;
     loadExternalScripts(packages: string[]): void;
-    loadFile(directory: string, fileName: string): void;
-    loadHubotScripts(path: string, scripts: string[]): void;
-    messageRoom(room: string, ...strings: string[]): void;
+    loadFile(directory: string, fileName: string): Promise<void>;
+    messageRoom(room: string, ...strings: string[]): Promise<void>;
+    parseVersion(): string;
     on(event: string | symbol, listener: (...args: unknown[]) => void): this;
-    receive(message: Message, cb?: () => void): void;
-    receiveMiddleware(middleware: MiddlewareHandler<A>): void;
-    reply(envelope: Envelope, ...strings: string[]): void;
+    receive(message: Message): Promise<null | any>;
+    receiveMiddleware(middleware: MiddlewareHandler<A, TextMessage>): void;
+    reply(envelope: Envelope, ...strings: string[]): Promise<any>;
     respond(regex: RegExp, callback: ListenerCallback<A, TextMessage>): void;
     respond(regex: RegExp, options: {}, callback: ListenerCallback<A, TextMessage>): void;
     respondPattern(regex: RegExp): RegExp;
-    responseMiddleware(middleware: MiddlewareHandler<A>): void;
-    run(): void;
-    send(envelope: Envelope, ...strings: string[]): void;
+    responseMiddleware(middleware: MiddlewareHandler<A, TextMessage>): void;
+    run(): Promise<any>;
+    send(envelope: Envelope, ...strings: string[]): Promise<any>;
     shutdown(): void;
     topic(callback: ListenerCallback<A, TopicMessage>): void;
     topic(options: {}, callback: ListenerCallback<A, TopicMessage>): void;
