8eb8e04e95467a4e54ad8a80a6de5d7ca6b1fc0e
422.io <iamwinner422@gmail.com>
2024-11-19 10:58:24 +0000

    Update react-native-simple-animations-tests.tsx

--- types/react-native-simple-animations/react-native-simple-animations-tests.tsx
+++ types/react-native-simple-animations/react-native-simple-animations-tests.tsx
@@ -3,41 +3,45 @@ import * as React from 'react';
 import { Text, View } from 'react-native';
 
 // Test basic usage
-function TestComponent(): JSX.Element {
-    return (
-        <SimpleAnimation 
-            aim="in"
-            duration={1000}
-            movementType="slide"
-            direction="up"
-        >
-            <Text>Test content</Text>
-        </SimpleAnimation>
-    );
+class TestComponent extends React.Component {
+    render() {
+        return (
+            <SimpleAnimation 
+                aim="in"
+                duration={1000}
+                movementType="slide"
+                direction="up"
+            >
+                <Text>Test content</Text>
+            </SimpleAnimation>
+        );
+    }
 }
 
 // Test avec toutes les props
-function CompleteTest(): JSX.Element {
-    return (
-        <SimpleAnimation 
-            aim="out"
-            animate={true}
-            animateOnUpdate={false}
-            delay={200}
-            direction="down"
-            distance={100}
-            duration={500}
-            fade={true}
-            friction={5}
-            movementType="spring"
-            staticType="bounce"
-            tension={100}
-            useNativeDriver={true}
-            style={{ marginTop: 10 }}
-        >
-            <View>
-                <Text>Complete test</Text>
-            </View>
-        </SimpleAnimation>
-    );
+class CompleteTest extends React.Component {
+    render() {
+        return (
+            <SimpleAnimation 
+                aim="out"
+                animate={true}
+                animateOnUpdate={false}
+                delay={200}
+                direction="down"
+                distance={100}
+                duration={500}
+                fade={true}
+                friction={5}
+                movementType="spring"
+                staticType="bounce"
+                tension={100}
+                useNativeDriver={true}
+                style={{ marginTop: 10 }}
+            >
+                <View>
+                    <Text>Complete test</Text>
+                </View>
+            </SimpleAnimation>
+        )
+    }
 }
