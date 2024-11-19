2da6f4e604a23b21f9e4204ebb8a9bedf87ef6d5
Pavlos Vinieratos <pvinis@gmail.com>
2024-11-19 03:06:21 +0200

    fmt

--- types/fontname/index.d.ts
+++ types/fontname/index.d.ts
@@ -1,14 +1,14 @@
 interface FontNameData {
-    postScriptName: string
-    family: string
-    style: string
-    fullName: string
-    version: string
+    postScriptName: string;
+    family: string;
+    style: string;
+    fullName: string;
+    version: string;
 }
 
 interface FontName {
-    parse(buffer: ArrayBuffer): FontNameData[]
+    parse(buffer: ArrayBuffer): FontNameData[];
 }
 
-declare const FontName: FontName
-export = FontName
+declare const FontName: FontName;
+export = FontName;
