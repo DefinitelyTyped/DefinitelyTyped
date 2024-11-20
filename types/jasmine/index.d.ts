cde5f7c1b53569cfa0bd2bec612ded9cccd0e208
Matt McCherry <matt.mccherry@gmail.com>
2024-11-20 11:26:42 +0000

    Add filename property to SuiteResult

--- types/jasmine/index.d.ts
+++ types/jasmine/index.d.ts
@@ -981,6 +981,11 @@ declare namespace jasmine {
         fullName: string;
 
         /**
+         * The name of the file the suite was defined in.
+         */
+        filename: string;
+
+        /**
          * The list of expectations that failed during execution of this spec.
          */
         failedExpectations: FailedExpectation[];
