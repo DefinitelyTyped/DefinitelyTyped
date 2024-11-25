653e2e9de2b0099f10e9660fefe00aaa89122381
liangguirong <liangguirong4@h-partners.com>
2024-11-25 19:43:26 +0800

    Add @types files to node-rules

---
+++ types/node-rules/index.d.ts
@@ -0,0 +1,58 @@
+declare class RuleEngine {
+  constructor();
+
+  /**
+   * Registers a rule with the rule engine.
+   * @param rule The rule to be registered.
+   */
+  register(rule: RuleEngine.Rule | RuleEngine.Rule[]): void;
+
+  /**
+   * Executes the rules on a given fact.
+   * @param fact The fact to be processed.
+   * @param callback The callback function to handle the result.
+   */
+  execute(fact: RuleEngine.Fact, callback: (data: RuleEngine.Fact & { result: any }) => void): void;
+}
+
+declare namespace RuleEngine {
+  interface Rule {
+    /**
+     * The condition to be evaluated for the rule.
+     * @param R The rule engine instance.
+     * @param fact The fact being evaluated.
+     */
+    condition: (R: FlowControlAPI, fact: Fact) => void;
+
+    /**
+     * The consequence to be executed if the condition is met.
+     * @param R The rule engine instance.
+     * @param fact The fact being evaluated.
+     */
+    consequence: (R: FlowControlAPI, fact: Fact) => void;
+
+    /**
+     * Optional parameter to specify the priority of the rule.
+     */
+    priority?: number;
+  }
+
+  interface Fact {
+    [key: string]: any;
+  }
+
+  // Flow Control API
+  interface FlowControlAPI {
+    when(condition: boolean): void;
+
+    stop(): void;
+
+    next(): void;
+
+    restart(): void;
+  }
+}
+
+export = RuleEngine;
+
+export as namespace RuleEngine;
