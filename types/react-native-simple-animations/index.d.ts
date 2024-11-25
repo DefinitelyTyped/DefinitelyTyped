646e4f2e23e45e0ff5836391bd2a946a79011e55
422.io <iamwinner422@gmail.com>
2024-11-25 20:59:36 +0000

    Update index.d.ts
    Adding "| undefined" to support exactOptionalPropertyTypes

--- types/react-native-simple-animations/index.d.ts
+++ types/react-native-simple-animations/index.d.ts
@@ -6,91 +6,91 @@ export interface SimpleAnimationProps {
      * Whether the child component will animate in or out
      * @default "in"
      */
-    aim?: "in" | "out";
+    aim?: "in" | "out" | undefined;
 
     /**
      * When true the child component will animate
      * @default true
      */
-    animate?: boolean;
+    animate?: boolean | undefined;
 
     /**
      * When true the child component will animate if any of the props change
      * @default false
      */
-    animateOnUpdate?: boolean;
+    animateOnUpdate?: boolean | undefined;
 
     /**
      * The length in milliseconds the component will wait before animating
      * @default 0
      */
-    delay?: number;
+    delay?: number | undefined;
 
     /**
      * When movementType is set, this is the direction the child component will move
      */
-    direction?: "down" | "left" | "right" | "up";
+    direction?: "down" | "left" | "right" | "up" | undefined;
 
     /**
      * When movementType is set, this is the distance the child component will move
      * @default 0
      */
-    distance?: number;
+    distance?: number | undefined;
 
     /**
      * The length in milliseconds the animation will last
      * @default 1000
      */
-    duration?: number;
+    duration?: number | undefined;
 
     /**
      * The easing function to define animation curve
      * @default Easing.out(Easing.exp)
      */
-    easing?: EasingFunction;
+    easing?: EasingFunction | undefined;
 
     /**
      * When true the child component will fade in or out depending on the aim
      * @default true
      */
-    fade?: boolean;
+    fade?: boolean | undefined;
 
     /**
      * Amount of friction when movementType is "spring" or staticType is "bounce"
      * @default 5
      */
-    friction?: number;
+    friction?: number | undefined;
 
     /**
      * Type of movement animation when direction and distance are set
      */
-    movementType?: "slide" | "spring";
+    movementType?: "slide" | "spring" | undefined;
 
     /**
      * Type of static animation
      */
-    staticType?: "bounce" | "zoom";
+    staticType?: "bounce" | "zoom" | undefined;
 
     /**
      * Additional styles applied to the component
      */
-    style?: ViewStyle;
+    style?: ViewStyle | undefined;
 
     /**
      * Amount of tension when movementType is "spring" or staticType is "bounce"
      * @default 100
      */
-    tension?: number;
+    tension?: number | undefined;
 
     /**
      * When true the animation is sent to native before starting
      * @default true
      */
-    useNativeDriver?: boolean;
+    useNativeDriver?: boolean | undefined;
 
     /**
      * Child components to animate
      */
-    children?: ReactNode;
+    children?: ReactNode | undefined;
 }
 export class SimpleAnimation extends Component<SimpleAnimationProps> {}
