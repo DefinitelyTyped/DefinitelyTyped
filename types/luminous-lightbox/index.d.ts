c930c1b54115cac7d3893a9d801771c50739d547
yuNuki2 <umrsuth_1373@yahoo.co.jp>
2024-11-26 19:11:40 +0900

    add undefined type as a union to all options

--- types/luminous-lightbox/index.d.ts
+++ types/luminous-lightbox/index.d.ts
@@ -9,82 +9,82 @@ interface LuminousOptions {
     /**
      * Prefix for generated element class names (e.g. `my-ns` will result in classes such as `my-ns-lightbox`. Default `lum-` prefixed classes will always be added as well.
      */
-    namespace?: string;
+    namespace?: string | undefined;
 
     /**
      * Which attribute to pull the lightbox image source from.
      * @default "href"
      */
-    sourceAttribute?: string;
+    sourceAttribute?: string | undefined;
 
     /**
      * Captions can be a literal string, or a function that receives the Luminous instance's trigger element as an argument and returns a string. Supports HTML, so use caution when dealing with user input.
      */
-    caption?: string | ((trigger: Element | null) => string | null);
+    caption?: string | ((trigger: Element | null) => string | null) | undefined;
 
     /**
      * The event to listen to on the _trigger_ element: triggers opening.
      * @default "click"
      */
-    openTrigger?: keyof GlobalEventHandlersEventMap;
+    openTrigger?: keyof GlobalEventHandlersEventMap | undefined;
 
     /**
      * The event to listen to on the _lightbox_ element: triggers closing.
      * @default "click"
      */
-    closeTrigger?: keyof GlobalEventHandlersEventMap;
+    closeTrigger?: keyof GlobalEventHandlersEventMap | undefined;
 
     /**
      * Allow closing by pressing escape.
      * @default true
      */
-    closeWithEscape?: boolean;
+    closeWithEscape?: boolean | undefined;
 
     /**
      * Automatically close when the page is scrolled.
      * @default false
      */
-    closeOnScroll?: boolean;
+    closeOnScroll?: boolean | undefined;
 
     /**
      * Disable close button
      * @default true
      */
-    showCloseButton?: boolean;
+    showCloseButton?: boolean | undefined;
 
     /**
      * A node to append the lightbox element to.
      * @default document.body
      */
-    appendToNode?: Element | null;
+    appendToNode?: Element | null | undefined;
 
     /**
      * A selector defining what to append the lightbox element to.
      * This will take precedence over `appendToNode`.
      */
-    appendToSelector?: string;
+    appendToSelector?: string | undefined;
 
     /**
      * If present (and a function), this will be called whenever the lightbox is opened.
      */
-    onOpen?: () => void;
+    onOpen?: (() => void) | undefined;
 
     /**
      * If present (and a function), this will be called whenever the lightbox is closed.
      */
-    onClose?: () => void;
+    onClose?: (() => void) | undefined;
 
     /**
      * When true, adds the `imgix-fluid` class to the `img` inside the lightbox. See https://github.com/imgix/imgix.js for more information.
      * @default false
      */
-    includeImgixJsClass?: boolean;
+    includeImgixJsClass?: boolean | undefined;
 
     /**
      * Add base styles to the page. See the "Theming" section of README.md for more information.
      * @default true
      */
-    injectBaseStyles?: boolean;
+    injectBaseStyles?: boolean | undefined;
 }
 
 interface LuminousGalleryOptions {
@@ -92,12 +92,12 @@ interface LuminousGalleryOptions {
      * Whether pressing the arrow keys should move to the next/previous slide.
      * @default true
      */
-    arrowNavigation?: boolean;
+    arrowNavigation?: boolean | undefined;
 
     /**
      * A callback triggered when the image changes that is passed the image HTML element.
      */
-    onChange?: (args: { imgEl: HTMLImageElement }) => void;
+    onChange?: ((args: { imgEl: HTMLImageElement }) => void) | undefined;
 }
 
 declare class Luminous {
@@ -105,11 +105,7 @@ declare class Luminous {
 }
 
 declare class LuminousGallery {
-    constructor(
-        triggers: Array<Element> | NodeList,
-        options?: LuminousGalleryOptions,
-        luminousOpts?: LuminousOptions,
-    );
+    constructor(triggers: Array<Element> | NodeList, options?: LuminousGalleryOptions, luminousOpts?: LuminousOptions);
 }
 
 export { Luminous, LuminousGallery };
