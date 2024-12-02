49986815cb9ea3b120f41e016d7dc87de033e7c8
Niclas Collmer <niclas.collmer@nia-medtech.com>
2024-12-02 09:56:52 +0100

    Ready to merge

--- types/react-native-file-type/react-native-file-type-tests.ts
+++ types/react-native-file-type/react-native-file-type-tests.ts
@@ -3,7 +3,6 @@ import FileType from "react-native-file-type"
 
 async function getMimeTypeAsync() {
     const result = FileType("example/image.jpg");
-
     if(result !== null) {
         const {ext: extension, mime: mimeType} = result
         console.log(extension)
