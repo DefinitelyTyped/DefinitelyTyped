9e7c6f671603e98ec6a83a4053860a0e578b301f
Izad Imran Tan <izad.mhdborhan@myeg.com.my>
2024-12-03 03:12:59 -0500

    Fixed formatting

--- types/zetrix-encryption-nodejs/index.d.ts
+++ types/zetrix-encryption-nodejs/index.d.ts
@@ -1,29 +1,29 @@
 export namespace keypair {
-  function getEncPublicKey(encPrivateKey: string): string;
+    function getEncPublicKey(encPrivateKey: string): string;
 
-  function getAddress(encPublicKey: string): string;
+    function getAddress(encPublicKey: string): string;
 
-  function parsePrivateKey(encPrivateKey: string): string;
+    function parsePrivateKey(encPrivateKey: string): string;
 
-  function parsePublicKey(encPublicKey: string): string;
+    function parsePublicKey(encPublicKey: string): string;
 
-  function getKeyPair(): any;
+    function getKeyPair(): any;
 
-  function checkEncPrivateKey(encPrivateKey: string): boolean;
+    function checkEncPrivateKey(encPrivateKey: string): boolean;
 
-  function checkEncPublicKey(encPublicKey: string): boolean;
+    function checkEncPublicKey(encPublicKey: string): boolean;
 
-  function checkAddress(address: string): boolean;
+    function checkAddress(address: string): boolean;
 }
 
 export namespace keystore {
-  function encrypt(encPrivateKey: string, password: string, callback: any): void;
+    function encrypt(encPrivateKey: string, password: string, callback: any): void;
 
-  function decrypt(keystore: any, password: string, callback: any): string;
+    function decrypt(keystore: any, password: string, callback: any): string;
 }
 
 export namespace signature {
-  function sign(message: string, encPrivateKey: string): string;
+    function sign(message: string, encPrivateKey: string): string;
 
-  function verify(message: any, signature: string, encPublicKey: any): boolean;
+    function verify(message: any, signature: string, encPublicKey: any): boolean;
 }
