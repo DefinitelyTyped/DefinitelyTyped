2498e570cadb6bef3d428449e50e2db7952b13c2
bangbang93 <bangbang93@163.com>
2024-12-04 14:28:32 +0800

    feat: add type for AipImageCensor.imageCensorUserDefined

--- types/baidu-aip-sdk/AipContentCensor.d.ts
+++ types/baidu-aip-sdk/AipContentCensor.d.ts
@@ -1,4 +1,6 @@
 export = AipImageCensor;
+import {ImageCensorUserDefinedResult} from './AipContentCensorType';
+
 /**
  * AipContentCensor类，构造调用图像审核对象
  *
@@ -10,8 +12,9 @@ declare class AipImageCensor extends BaseClient {
     constructor(appId: string, ak: string, sk: string);
     commonImpl(param: any): any;
     jsonRequestImpl(param: any): any;
-    imageCensorUserDefined(image: any, type: any): any;
+    imageCensorUserDefined(image: string, type: 'url' | 'base64'): Promise<ImageCensorUserDefinedResult>;
     textCensorUserDefined(text: any, type: any): any;
     report(feedback: any): any;
 }
 import BaseClient = require("./client/baseClient");
+
