a781feaf5cbdb57121d9cf889d06ea93ef3f5b7a
Sebastian "Sebbie" Silbermann <silbermann.sebastian@gmail.com>
2024-12-04 16:35:45 +0100

    [react-experimental] Add `React.captureOwnerStack` (#71329)

--- types/react/ts5.0/test/experimental.tsx
+++ types/react/ts5.0/test/experimental.tsx
@@ -43,6 +43,11 @@ function suspenseTest() {
     <React.Suspense fallback="Loading">B</React.Suspense>
 </React.unstable_SuspenseList>;
 
+function ownerStacks() {
+    // $ExpectType string | null
+    const ownerStack = React.captureOwnerStack();
+}
+
 function useEvent() {
     // Implicit any
     // @ts-expect-error
