afaa0efaa4af5905a0ead4ff07f41936b16de186
Oleksandr Danylchenko <oleksandr.danylchenko@binary-studio.com>
2025-01-20 11:28:43 +0200

    fix(lodash): fix `mergeWith` args count typo

--- types/lodash/common/object.d.ts
+++ types/lodash/common/object.d.ts
@@ -1905,7 +1905,7 @@ declare module "../index" {
          * This method is like `_.merge` except that it accepts `customizer` which
          * is invoked to produce the merged values of the destination and source
          * properties. If `customizer` returns `undefined` merging is handled by the
-         * method instead. The `customizer` is invoked with seven arguments:
+         * method instead. The `customizer` is invoked with six arguments:
          * (objValue, srcValue, key, object, source, stack).
          *
          * @category Object
