1074f89cbaec43fd88ee6efe305a479ae6c15c7d
Jordan Harband <ljharb@gmail.com>
2025-01-25 22:41:01 -0800

    notNeededPackages: add 22 of my packages

--- types/has-symbols/has-symbols-tests.ts
+++
@@ -1,5 +0,0 @@
-import hasNativeSymbols = require("has-symbols");
-import hasSymbolSham = require("has-symbols/shams");
-
-hasNativeSymbols(); // $ExpectType boolean
-hasSymbolSham(); // $ExpectType boolean
--- types/has-symbols/shams.d.ts
+++
@@ -1,6 +0,0 @@
-/**
- * Returns `true` if the environment has a `Symbol` sham
- * that mostly follows the spec.
- */
-declare function hasSymbolSham(): boolean;
-export = hasSymbolSham;
