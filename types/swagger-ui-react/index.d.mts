c3cbd0b9ac454922e156ca9175c883f246011877
David Ulrich 👌 <david.ulrich4@gmail.com>
2025-01-27 19:02:12 +0100

    feat: Format files

--- types/swagger-ui-react/index.d.mts
+++ types/swagger-ui-react/index.d.mts
@@ -1,5 +1,5 @@
-import * as React from 'react'
-import {SwaggerUIProps} from "./swagger-ui-react.js";
+import * as React from "react";
+import { SwaggerUIProps } from "./swagger-ui-react.js";
 
 declare const SwaggerUI: React.FunctionComponent<SwaggerUIProps>;
 export default SwaggerUI;
--- types/swagger-ui-react/index.d.ts
+++ types/swagger-ui-react/index.d.ts
@@ -1,5 +1,5 @@
-import * as React from 'react'
-import {SwaggerUIProps} from "./swagger-ui-react";
+import * as React from "react";
+import { SwaggerUIProps } from "./swagger-ui-react";
 
 declare const SwaggerUI: React.FunctionComponent<SwaggerUIProps>;
-export = SwaggerUI
+export = SwaggerUI;
