c7ad9abef97c72c08bf896320ff93791a94972f2
astrid <git@astrid.email>
2025-01-28 17:28:34 +0100

    [@types/umami-browser] update definitions to 2.13.0

--- types/umami-browser/index.d.ts
+++ types/umami-browser/index.d.ts
@@ -1,18 +1,39 @@
 declare var umami: umami.umami;
 
+/**
+ * @see {@link https://umami.is/docs/tracker-functions|Umami Docs}
+ */
 declare namespace umami {
     interface TrackPayload {
-        website: string;
         hostname?: string;
         language?: string;
         referrer?: string;
         screen?: string;
         title?: string;
         url?: string;
+        website: string;
+    }
+
+    interface TrackCustomPayload extends TrackPayload {
+        name?: string;
+        data?: Record<string, unknown>;
     }
 
     interface umami {
-        track(payloadOrCallback?: TrackPayload | ((properties: TrackPayload) => TrackPayload)): void;
-        track(eventName?: string, eventData?: Record<string, unknown>): void;
+        track(): Promise<string> | undefined;
+        track(
+            eventName: string,
+            eventData?: { [key: string]: any },
+        ): Promise<string> | undefined;
+        track(
+            payload: TrackCustomPayload,
+        ): Promise<string> | undefined;
+        track(
+            callback: (
+                props: Required<TrackPayload>,
+            ) => TrackCustomPayload,
+        ): Promise<string> | undefined;
+
+        identify(data: Record<string, unknown>): Promise<string> | undefined;
     }
 }
