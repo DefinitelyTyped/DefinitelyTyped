60e8ec2e72d7b69e97413f52498e9d8ab08a7ef1
astrid <git@astrid.email>
2025-01-29 14:54:45 +0100

    more precise types

--- types/umami-browser/index.d.ts
+++ types/umami-browser/index.d.ts
@@ -1,39 +1,48 @@
 declare var umami: umami.umami;
 
+type WithRequired<T, K extends keyof T> = T & { [P in K]-?: T[P] };
+
 /**
  * @see {@link https://umami.is/docs/tracker-functions|Umami Docs}
  */
 declare namespace umami {
-    interface TrackPayload {
-        hostname?: string;
-        language?: string;
-        referrer?: string;
-        screen?: string;
-        title?: string;
-        url?: string;
+    interface PageViewProperties {
         website: string;
+        hostname: string;
+        language: string;
+        referrer: string;
+        screen: string;
+        title: string;
+        url: string;
     }
 
-    interface TrackCustomPayload extends TrackPayload {
+    interface EventData {
+        [key: string]:
+            | number
+            | string
+            | EventData
+            | number[]
+            | string[]
+            | EventData[];
+    }
+
+    interface CustomPayload
+        extends WithRequired<Partial<PageViewProperties>, "website"> {
         name?: string;
-        data?: Record<string, unknown>;
+        data?: EventData;
     }
 
     interface umami {
-        track(): Promise<string> | undefined;
-        track(
-            eventName: string,
-            eventData?: { [key: string]: any },
-        ): Promise<string> | undefined;
-        track(
-            payload: TrackCustomPayload,
-        ): Promise<string> | undefined;
-        track(
-            callback: (
-                props: Required<TrackPayload>,
-            ) => TrackCustomPayload,
-        ): Promise<string> | undefined;
+        track: {
+            (): Promise<string>;
+            (eventName: string): Promise<string>;
+            (eventName: string, eventData: EventData): Promise<string>;
+            (props: CustomPayload): Promise<string>;
+            (
+                callback: (props: PageViewProperties) => CustomPayload,
+            ): Promise<string>;
+        };
 
-        identify(data: Record<string, unknown>): Promise<string> | undefined;
+        identify: (data: EventData) => Promise<string>;
     }
 }
