7f372c77f65951f779c8cc6cdbcca0fcafe86a8c
Katja Potensky <785327-hesxenon@users.noreply.gitlab.com>
2025-02-14 10:35:22 +0100

    fix: include properties of bound as well

--- types/regexp.escape/index.d.ts
+++ types/regexp.escape/index.d.ts
@@ -1,2 +1,13 @@
 declare function escape(str: string): string;
-export = escape;
+
+declare var bound: typeof escape & {
+    getPolyfill: () => typeof escape;
+    implementation: typeof escape;
+    /**
+     * @deprecated use .implementation instead
+     */
+    method: typeof escape;
+    shim: () => typeof escape;
+};
+
+export = bound;
--- types/regexp.escape/regexp.escape-tests.ts
+++ types/regexp.escape/regexp.escape-tests.ts
@@ -4,13 +4,21 @@ import polyfill from "regexp.escape/polyfill";
 import shimRegExpEscape from "regexp.escape/shim";
 import "regexp.escape/auto";
 
+// $ExpectType string
+escape.implementation("foo");
+// $ExpectType string
+escape.method("foo");
+// $ExpectType (str: string) => string
+escape.shim();
+// $ExpectType (str: string) => string
+escape.getPolyfill();
+// $ExpectType string
+escape("foo");
 // $ExpectType (str: string) => string
 shimRegExpEscape();
 // $ExpectType (str: string) => string
 polyfill();
 // $ExpectType string
 implementation("foo");
-// $ExpectType string
-escape("foo");
 // $ExpectType string
 RegExp.escape("foo");
