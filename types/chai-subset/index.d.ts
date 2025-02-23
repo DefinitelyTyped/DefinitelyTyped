487ed38d7f9f49cb32349f23920027ef9cc86001
Julian Grinblat <julian@dotcore.co.il>
2025-02-23 19:27:47 +0900

    attempt fix

--- types/chai-subset/index.d.ts
+++ types/chai-subset/index.d.ts
@@ -3,7 +3,6 @@
 declare global {
     namespace Chai {
         interface Assertion {
-            // @ts-expect-error 230 this is a duplicate on chai >= 5.2.0
             containSubset(expected: any): Assertion;
         }
         interface Assert {
