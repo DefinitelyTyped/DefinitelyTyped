90699cc34a4ae3ce865ad6fd8748a56966ff7fd6
the-doctor-octo <glefe85@yahoo.it>
2025-02-24 12:51:41 +0100

    chore(leaflet.vectorgrid): dprint formatting

--- types/leaflet.vectorgrid/index.d.ts
+++ types/leaflet.vectorgrid/index.d.ts
@@ -73,7 +73,9 @@ declare module "leaflet" {
         /** A factory method which will be used to instantiate the per-tile renderers. */
         rendererFactory?: TileFactoryFunction<T>;
         /** A data structure holding initial symbolizer definitions for the vector features. */
-        vectorTileLayerStyles?: (Record<string, PathOptions> | ((properties:Record<string,string>, zoom:number) => L.Path));
+        vectorTileLayerStyles?:
+            | Record<string, PathOptions>
+            | ((properties: Record<string, string>, zoom: number) => L.Path);
         /** Whether this VectorGrid fires Interactive Layer events. */
         interactive?: boolean;
         /**
