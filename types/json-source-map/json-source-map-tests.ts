37ea50a37f3ae3cdb6b62a1d7d1dfe8a589df7a5
Ole <oler@google.com>
2025-03-04 16:00:31 +0100

    Fix formatting.

--- types/json-source-map/json-source-map-tests.ts
+++ types/json-source-map/json-source-map-tests.ts
@@ -1,12 +1,12 @@
-import {parse, stringify} from 'json-source-map';
+import { parse, stringify } from "json-source-map";
 
-const stringifyResult = stringify({ foo: 'bar' }, null, 2);
+const stringifyResult = stringify({ foo: "bar" }, null, 2);
 // $ExpectType string
 const json = stringifyResult.json;
 // $ExpectType Pointers
 const stringifyPointers = stringifyResult.pointers;
 
-const parseResult = parse('{ "foo": "bar" }');
+const parseResult = parse("{ \"foo\": \"bar\" }");
 // $ExpectType unknown
 const data = parseResult.data;
 // $ExpectType Pointers
