32e355ed7b58a20bf0da3b0501468be6e62f6cd1
Jimmy Leung <43258070+hkleungai@users.noreply.github.com>
2025-03-11 01:26:42 +0800

    fix(tuya-panel-kit): import @react-navigation/* as package

--- types/tuya-panel-kit/@react-navigation/core/NavigationRouteContext.d.ts
+++
@@ -1,8 +0,0 @@
-import * as React from "react";
-import type { Route } from "../routers";
-/**
- * Context which holds the route prop for a screen.
- */
-// tslint:disable-next-line use-default-type-parameter
-declare const NavigationContext: React.Context<Route<string, object | undefined> | undefined>;
-export default NavigationContext;
