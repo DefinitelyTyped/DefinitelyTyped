68d4323e2da9ac733bc8f0f5adf863ff576ad060
Jimmy Leung <43258070+hkleungai@users.noreply.github.com>
2025-03-15 11:59:29 +0800

    feat(react-widgets): remove types

--- types/react-widgets/index.d.ts
+++
@@ -1,7 +0,0 @@
-export import Calendar = require("./lib/Calendar");
-export import Combobox = require("./lib/Combobox");
-export import DateTimePicker = require("./lib/DateTimePicker");
-export import DropdownList = require("./lib/DropdownList");
-export import Multiselect = require("./lib/Multiselect");
-export import NumberPicker = require("./lib/NumberPicker");
-export import SelectList = require("./lib/SelectList");
