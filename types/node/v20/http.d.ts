5d5304de4e8b6d8ecd7016774b53b1682008eeb8
theBGuy <60308670+theBGuy@users.noreply.github.com>
2025-03-17 01:05:28 -0400

    Add `HTTPMethod` union type
    - `METHODS` previously was defined as just a string array which provided no type safety for what actual values could be used and what existed in METHODS. Which was problematic.

--- types/node/v20/http.d.ts
+++ types/node/v20/http.d.ts
@@ -1544,7 +1544,43 @@ declare module "http" {
          */
         destroy(): void;
     }
-    const METHODS: string[];
+    type HTTPMethod =
+    | 'ACL'
+    | 'BIND'
+    | 'CHECKOUT'
+    | 'CONNECT'
+    | 'COPY'
+    | 'DELETE'
+    | 'GET'
+    | 'HEAD'
+    | 'LINK'
+    | 'LOCK'
+    | 'M-SEARCH'
+    | 'MERGE'
+    | 'MKACTIVITY'
+    | 'MKCALENDAR'
+    | 'MKCOL'
+    | 'MOVE'
+    | 'NOTIFY'
+    | 'OPTIONS'
+    | 'PATCH'
+    | 'POST'
+    | 'PROPFIND'
+    | 'PROPPATCH'
+    | 'PURGE'
+    | 'PUT'
+    | 'QUERY'
+    | 'REBIND'
+    | 'REPORT'
+    | 'SEARCH'
+    | 'SOURCE'
+    | 'SUBSCRIBE'
+    | 'TRACE'
+    | 'UNBIND'
+    | 'UNLINK'
+    | 'UNLOCK'
+    | 'UNSUBSCRIBE';
+    const METHODS: HTTPMethod[];
     const STATUS_CODES: {
         [errorCode: number]: string | undefined;
         [errorCode: string]: string | undefined;
