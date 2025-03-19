2d69cf2a35343c19a31667b92cb3fb1cf10cf17e
Dimitri POSTOLOV <dmytropostolov@gmail.com>
2025-03-19 15:43:57 +0100

    [react] Add missing `onScrollEnd` event (#72078)
    Co-authored-by: Sebastian Sebbie Silbermann <sebastian.silbermann@vercel.com>

--- types/react/index.d.ts
+++ types/react/index.d.ts
@@ -2334,6 +2334,8 @@ declare namespace React {
         // UI Events
         onScroll?: UIEventHandler<T> | undefined;
         onScrollCapture?: UIEventHandler<T> | undefined;
+        onScrollEnd?: UIEventHandler<T> | undefined;
+        onScrollEndCapture?: UIEventHandler<T> | undefined;
 
         // Wheel Events
         onWheel?: WheelEventHandler<T> | undefined;
