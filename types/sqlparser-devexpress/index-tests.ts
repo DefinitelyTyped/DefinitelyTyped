e47b8ad9ffc72ceb8e5b3c99f34fcdfcf6cb13c1
RohitM-IN <rohit.mahajan.in@gmail.com>
2025-03-27 22:17:42 +0530

    updated Formatting for tests

--- types/sqlparser-devexpress/index-tests.ts
+++ types/sqlparser-devexpress/index-tests.ts
@@ -1,4 +1,4 @@
-import { convertSQLToAst, convertAstToDevextreme } from "sqlparser-devexpress";
+import { convertAstToDevextreme, convertSQLToAst } from "sqlparser-devexpress";
 
 const sqlQuery = "Product_Current_Inventory <> 0 OR (Product_Name like 'HD' AND Product_Cost < 200)";
 const astRaw = convertSQLToAst(sqlQuery);
@@ -15,27 +15,24 @@ const devexpressFilter = convertAstToDevextreme(astRaw.ast, {});
 //   ],
 // ];
 
-if(!devexpressFilter)
+if (!devexpressFilter) {
     throw new Error("Assertion failed: devexpressFilter is null");
+}
 
 if (
-    devexpressFilter[0][0] !== 'Product_Current_Inventory' ||
-    devexpressFilter[0][1] !== '<>' ||
-    devexpressFilter[0][2] !== 0 ||
-
-    devexpressFilter[1] !== 'or' ||
-
-    devexpressFilter[2][0][0] !== 'Product_Name' ||
-    devexpressFilter[2][0][1] !== 'contains' ||
-    devexpressFilter[2][0][2] !== 'HD' ||
-
-    devexpressFilter[2][1] !== 'and' ||
-
-    devexpressFilter[2][2][0] !== 'Product_Cost' ||
-    devexpressFilter[2][2][1] !== '<' ||
-    devexpressFilter[2][2][2] !== 200
-  ) {
+    devexpressFilter[0][0] !== "Product_Current_Inventory"
+    || devexpressFilter[0][1] !== "<>"
+    || devexpressFilter[0][2] !== 0
+    || devexpressFilter[1] !== "or"
+    || devexpressFilter[2][0][0] !== "Product_Name"
+    || devexpressFilter[2][0][1] !== "contains"
+    || devexpressFilter[2][0][2] !== "HD"
+    || devexpressFilter[2][1] !== "and"
+    || devexpressFilter[2][2][0] !== "Product_Cost"
+    || devexpressFilter[2][2][1] !== "<"
+    || devexpressFilter[2][2][2] !== 200
+) {
     throw new Error("Assertion failed: devexpressFilter does not match expectedFilter");
-  } else {
+} else {
     // Assertion passed! devexpressFilter matches expectedFilter.
-  }
+}
