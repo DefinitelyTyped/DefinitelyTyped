a2dcfe019e8a4df4747ffb9f65684faef9fdf311
Jimmy Leung <43258070+hkleungai@users.noreply.github.com>
2025-03-28 04:17:18 +0800

    feat(through): omit unimplemented Readable method from return type

--- types/through/index.d.ts
+++ types/through/index.d.ts
@@ -14,6 +14,47 @@ declare namespace through {
     export interface ThroughStream extends stream.Transform {
         autoDestroy: boolean;
         queue: (chunk: any) => any;
+
+        /** Unimplemented method for `ThroughStream` */
+        isPaused: never;
+        /** Unimplemented method for `ThroughStream` */
+        read: never;
+        /** Unimplemented method for `ThroughStream` */
+        setEncoding: never;
+        /** Unimplemented method for `ThroughStream` */
+        unpipe: never;
+        /** Unimplemented method for `ThroughStream` */
+        unshift: never;
+        /** Unimplemented method for `ThroughStream` */
+        wrap: never;
+        /** Unimplemented method for `ThroughStream` */
+        compose: never;
+        /** Unimplemented method for `ThroughStream` */
+        iterator: never;
+        /** Unimplemented method for `ThroughStream` */
+        map: never;
+        /** Unimplemented method for `ThroughStream` */
+        filter: never;
+        /** Unimplemented method for `ThroughStream` */
+        forEach: never;
+        /** Unimplemented method for `ThroughStream` */
+        toArray: never;
+        /** Unimplemented method for `ThroughStream` */
+        some: never;
+        /** Unimplemented method for `ThroughStream` */
+        find: never;
+        /** Unimplemented method for `ThroughStream` */
+        every: never;
+        /** Unimplemented method for `ThroughStream` */
+        flatMap: never;
+        /** Unimplemented method for `ThroughStream` */
+        drop: never;
+        /** Unimplemented method for `ThroughStream` */
+        take: never;
+        /** Unimplemented method for `ThroughStream` */
+        asIndexedPairs: never;
+        /** Unimplemented method for `ThroughStream` */
+        reduce: never;
     }
 }
 
--- types/through/through-tests.ts
+++ types/through/through-tests.ts
@@ -1,7 +1,8 @@
 import through = require("through");
 
-var i = 0;
-through(
+let i = 0;
+
+let stream = through(
     function(this: through.ThroughStream) {
         this.queue((i++).toString());
     },
@@ -9,4 +10,49 @@ through(
         this.queue(null);
     },
     { autoDestroy: true },
-).pipe(process.stdout);
+);
+
+stream.pipe(process.stdout);
+
+stream.pause();
+
+// $ExpectType never
+stream.isPaused;
+// $ExpectType never
+stream.read;
+// $ExpectType never
+stream.setEncoding;
+// $ExpectType never
+stream.unpipe;
+// $ExpectType never
+stream.unshift;
+// $ExpectType never
+stream.wrap;
+// $ExpectType never
+stream.compose;
+// $ExpectType never
+stream.iterator;
+// $ExpectType never
+stream.map;
+// $ExpectType never
+stream.filter;
+// $ExpectType never
+stream.forEach;
+// $ExpectType never
+stream.toArray;
+// $ExpectType never
+stream.some;
+// $ExpectType never
+stream.find;
+// $ExpectType never
+stream.every;
+// $ExpectType never
+stream.flatMap;
+// $ExpectType never
+stream.drop;
+// $ExpectType never
+stream.take;
+// $ExpectType never
+stream.asIndexedPairs;
+// $ExpectType never
+stream.reduce;
