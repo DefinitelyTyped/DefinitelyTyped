8b662fbd913464d8ecd418b6c4e64231e6550fe2
Dolan Murvihill <dolan.murvihill@downstream.com>
2025-03-28 18:29:37 -0700

    [expect-cookie] Set readonly properties on parameter types

--- types/expect-cookies/index.d.ts
+++ types/expect-cookies/index.d.ts
@@ -30,59 +30,59 @@ declare namespace expectCookies {
     }
 
     interface AssertionMatchers {
-        set: SetMatcher;
-        reset: ResetMatcher;
-        new: NewMatcher;
-        renew: RenewMatcher;
-        contain: ContainMatcher;
+        readonly set: SetMatcher;
+        readonly reset: ResetMatcher;
+        readonly new: NewMatcher;
+        readonly renew: RenewMatcher;
+        readonly contain: ContainMatcher;
     }
 
     interface SetMatcher {
-        name: string;
-        options?: Record<string, string | boolean>;
+        readonly name: string;
+        readonly options?: Record<string, string | boolean>;
     }
 
     interface ResetMatcher {
-        name: string;
+        readonly name: string;
     }
 
     type NewMatcher = ResetMatcher;
 
     interface RenewMatcher {
-        name: string;
+        readonly name: string;
 
         // XOR type; options must contain 'max-age' or 'expires' but not
         // both. The docs imply this value is ignored, but it is not.
         // The docs also suggest this is an "equal or greater" check but
         // actually the check is "strictly greater".
         // https://github.com/gregl83/expect-cookies/issues/3
-        options:
+        readonly options:
             | { "max-age": number; expires?: never }
             | { expires: Date; "max-age"?: never };
     }
 
     interface ContainMatcher {
-        name: string;
+        readonly name: string;
 
         // The docs state that value is optional; but omitting will cause
         // the cookie's value to be compared against 'undefined' and fail
         // the test.
         // See: https://github.com/gregl83/expect-cookies/issues/5
-        value: string | boolean;
+        readonly value: string | boolean;
 
-        options?: {
-            domain?: string;
-            path?: string;
-            expires?: string; // RFC 6265 sane-cookie-date (e.g. "Fri, 26 Sep 2025 04:26:52 GMT")
-            secure?: boolean;
-            httponly?: boolean;
+        readonly options?: {
+            readonly domain?: string;
+            readonly path?: string;
+            readonly expires?: string; // RFC 6265 sane-cookie-date (e.g. "Fri, 26 Sep 2025 04:26:52 GMT")
+            readonly secure?: boolean;
+            readonly httponly?: boolean;
 
             // Note: you should pass a numeric string for max-age, as numbers
             // will not be evaluated properly:
             // https://github.com/gregl83/expect-cookies/issues/4
             // Also note that max-age has a different behavior here than in
             // .renew.
-            "max-age"?: string;
+            readonly "max-age"?: string;
         };
     }
 
