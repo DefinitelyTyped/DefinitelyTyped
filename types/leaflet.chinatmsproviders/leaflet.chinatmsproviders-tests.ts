d7633ced6674a17d49a61cb51d5c05189573db68
TwoKe <2956917032@qq.com>
2025-04-13 13:11:19 +0800

    test: update test

--- types/leaflet.chinatmsproviders/leaflet.chinatmsproviders-tests.ts
+++ types/leaflet.chinatmsproviders/leaflet.chinatmsproviders-tests.ts
@@ -5,3 +5,7 @@ const map: L.Map = L.map("map-container");
 
 L.tileLayer.chinaProvider('GaoDe.Normal.Map').addTo(map)
 L.tileLayer.chinaProvider('Google.Normal.Map').addTo(map)
+
+L.tileLayer('http://xxxxx/{z}/{x}/{y}.png', {
+    corrdType: 'gcj02'
+}).addTo(map)
