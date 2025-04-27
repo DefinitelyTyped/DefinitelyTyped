c24ea45a1a9f68c47abb1c260ba19d8ca27553ed
tientq64 <tientq64@gmail.com>
2025-04-27 16:06:30 +0700

    chore: Format code

--- types/bro-fs/index.d.ts
+++ types/bro-fs/index.d.ts
@@ -31,25 +31,27 @@ export interface UsageObject {
 }
 
 export interface FileSystemEntryWithChildren extends FileSystemEntry {
-    children: FileSystemEntry[]
+    children: FileSystemEntry[];
 }
 
 /**
  * Appends data to file.
- * 
+ *
  * If file does not exist - it will be created.
  */
 export function appendFile(
     path: string | FileSystemFileEntry,
-    data: string | Blob | File | ArrayBuffer
+    data: string | Blob | File | ArrayBuffer,
 ): Promise<FileSystemFileEntry>;
+
 /**
  * Clears whole filesystem.
  */
 export function clear(): Promise<void>;
+
 /**
  * Copies file or entire directory.
- * 
+ *
  * If file or directory does not exist - error thrown.
  */
 export function copy(
@@ -60,45 +62,55 @@ export function copy(
          * Create missing directories.
          * @default false
          */
-        create?: boolean | undefined
-    }
+        create?: boolean | undefined;
+    },
 ): Promise<FileSystemEntry>;
+
 /**
  * Create a {@link https://developer.mozilla.org/docs/Web/API/ReadableStream ReadableStream} for reading.
  */
 export function createReadStream(path: string | FileSystemFileEntry): Promise<ReadableStream>;
+
 /**
  * Create a {@link https://developer.mozilla.org/docs/Web/API/WritableStream WritableStream} for writing.
  */
 export function createWriteStream(path: string | FileSystemFileEntry): Promise<WritableStream>;
+
 /**
  * Checks that file or directory exists by provided path.
  */
 export function exists(path: string): Promise<boolean>;
+
 /**
  * Gets file or directory.
  */
 export function getEntry(path: string | FileSystemEntry): Promise<FileSystemEntry>;
+
 /**
  * Returns root directory.
  */
 export function getRoot(): Promise<FileSystemDirectoryEntry>;
+
 /**
  * Gets URL for path.
  */
 export function getUrl(path: string | FileSystemEntry): Promise<string>;
+
 /**
  * Init filesystem.
  */
 export function init(options?: FSOptions): Promise<FileSystem>;
+
 /**
  * Is filesystem API supported by current browser.
  */
 export function isSupported(): boolean;
+
 /**
  * Recursively creates required directories in provided path.
  */
 export function mkdir(path: string): Promise<FileSystemDirectoryEntry>;
+
 /**
  * Reads directory content.
  */
@@ -109,9 +121,10 @@ export function readdir(
          * Read recursively and attach data as `children` property.
          * @default false
          */
-        deep?: false | undefined
-    }
+        deep?: false | undefined;
+    },
 ): Promise<FileSystemEntry[]>;
+
 /**
  * Reads recursively directory content.
  */
@@ -122,48 +135,53 @@ export function readdir(
          * Read recursively and attach data as `children` property.
          * @default false
          */
-        deep: true
-    }
+        deep: true;
+    },
 ): Promise<FileSystemEntryWithChildren[]>;
+
 /**
  * Reads file content.
- * 
+ *
  * If file does not exist - error thrown.
  */
 export function readFile(
     path: string | FileSystemFileEntry,
-    options: { type: "ArrayBuffer" }
+    options: { type: "ArrayBuffer" },
 ): Promise<ArrayBuffer>;
+
 /**
  * Reads file content.
- * 
+ *
  * If file does not exist - error thrown.
  */
 export function readFile(
     path: string | FileSystemFileEntry,
-    options: { type: "Blob" }
+    options: { type: "Blob" },
 ): Promise<Blob>;
+
 /**
  * Reads file content.
- * 
+ *
  * If file does not exist - error thrown.
  */
 export function readFile(
     path: string | FileSystemFileEntry,
-    options: { type: "File" }
+    options: { type: "File" },
 ): Promise<File>;
+
 /**
  * Reads file content.
- * 
+ *
  * If file does not exist - error thrown.
  */
 export function readFile(
     path: string | FileSystemFileEntry,
-    options?: { type?: TextType | undefined }
+    options?: { type?: TextType | undefined },
 ): Promise<string>;
+
 /**
  * Renames file or directory.
- * 
+ *
  * If source file or directory does not exist - error thrown.
  * If target already exists - it will be overwritten.
  */
@@ -175,38 +193,43 @@ export function rename(
          * Create missing directories.
          * @default false
          */
-        create?: boolean | undefined
+        create?: boolean | undefined;
     },
 ): Promise<FileSystemEntry>;
+
 /**
  * Removes directory recursively.
- * 
+ *
  * If directory does not exist - method does nothing without error.
  */
 export function rmdir(path: string | FileSystemDirectoryEntry): Promise<boolean>;
+
 /**
  * Gets info about file or directory.
- * 
+ *
  * If it does not exist - error thrown.
  */
 export function stat(path: string | FileSystemEntry): Promise<StatObject>;
+
 /**
  * Removes file.
- * 
+ *
  * If file does not exist - no error thrown.
  */
 export function unlink(path: string | FileSystemFileEntry): Promise<boolean>;
+
 /**
  * Gets used and granted bytes.
  */
 export function usage(): Promise<UsageObject>;
+
 /**
  * Writes data to file.
- * 
+ *
  * If file does not exist - it will be created.
  * If file already exists - it will be overwritten.
  */
 export function writeFile(
     path: string | FileSystemFileEntry,
-    data: string | Blob | File | ArrayBuffer
+    data: string | Blob | File | ArrayBuffer,
 ): Promise<FileSystemFileEntry>;
