ff0a46c051e7303fbb458dc7e831a757162b820c
tientq64 <tientq64@gmail.com>
2025-04-27 16:14:03 +0700

    fix: Correct type of `FileSystemEntryWithChildren` interface

--- types/bro-fs/index.d.ts
+++ types/bro-fs/index.d.ts
@@ -31,7 +31,7 @@ export interface UsageObject {
 }
 
 export interface FileSystemEntryWithChildren extends FileSystemEntry {
-    children: FileSystemEntry[];
+    children: FileSystemEntryWithChildren[];
 }
 
 /**
