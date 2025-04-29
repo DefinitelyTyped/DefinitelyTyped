a3f4f009a2a503dbe301b168ce149527ffe3904e
Renato Fernandes <renato@innovaro.com.br>
2025-04-28 23:12:25 -0300

    feat: update @nginstack/engine definitions

--- types/nginstack__engine/context/index.d.ts
+++ types/nginstack__engine/context/index.d.ts
@@ -6,17 +6,4 @@ export let request: import('../lib/http/Request.js');
 export let response: import('../lib/http/Response.js');
 export let session: import('../lib/session/Session.js');
 export let tempDB: import('../lib/ido/IdoDB.js');
-export let vendor: {
-    name: string;
-    legalName: string;
-    siteUrl: string;
-    systemName: string;
-    logoUrl: string | number;
-    logoTitle: string;
-    footerLogoUrl: string | number;
-    footerLogoTitle: string;
-    mainMenuLogoUrl: string;
-    appBarLogoUrl: string;
-    faviconUrl: string;
-    colors: import('./vendor.js').ThemeColors;
-};
+export let vendor: import('../lib/vendor/VendorInfo.js');
