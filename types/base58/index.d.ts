43c55b8520767067dc1af34e1aa659fce1b22210
Outvi V <19144373+outloudvi@users.noreply.github.com>
2025-05-04 08:30:34 +0000

    chore: dprint & fix lint issues

--- types/base58/index.d.ts
+++ types/base58/index.d.ts
@@ -1,12 +1,22 @@
-/*~ If this module is a UMD module that exposes a global variable 'myLib' when
- *~ loaded outside a module loader environment, declare that global here.
- *~ Otherwise, delete this declaration.
- */
-export as namespace base58;
-
 /*~ If this module has methods, declare them as functions like so.
  */
-export function base58_to_int(str: string): number
-export function decode(str: string): number
-export function int_to_base58(num: number): string
-export function encode(num: number): string
+
+/**
+ * Convert a base58 string to an integer.
+ */
+export function base58_to_int(str: string): number;
+
+/**
+ * Convert a base58 string to an integer.
+ */
+export function decode(str: string): number;
+
+/**
+ * Convert an integer to a base58 string.
+ */
+export function int_to_base58(num: number): string;
+
+/**
+ * Convert an integer to a base58 string.
+ */
+export function encode(num: number): string;
