1accd1c98a359ac06ca3ce8a3d1f748c194e5f51
Legacy <52163001+3m1n3nc3@users.noreply.github.com>
2025-05-06 13:37:04 +0100

    fix(paystack__inline-js): The first parameter for the resumeTransaction method is supposed to be a string not an object, we fixed that.

--- types/paystack__inline-js/index.d.ts
+++ types/paystack__inline-js/index.d.ts
@@ -222,12 +222,13 @@ declare class PaystackInline {
     isLoaded(): boolean;
 
     resumeTransaction(
-        options: {
-            /**
-             * Access code created on the API via the https://paystack.com/docs/#initialize-a-transaction endpoint
-             */
-            accessCode: string;
-        },
+        /**
+         * Access code created on the API via the https://paystack.com/docs/#initialize-a-transaction endpoint
+         */
+        accessCode: string,
+        /**
+         * Callback Definitions
+         */
         callbacks?: Pick<NewTransactionOptions, "onSuccess" | "onCancel" | "onError" | "onLoad">,
     ): PopupTransaction;
 
