84a373170f491426925cd3cc9e256aa30a6868c4
vikr01 <vik120@icloud.com>
2025-05-11 15:52:58 -0700

    fix type for babel declare function

--- types/babel__helper-plugin-utils/index.d.ts
+++ types/babel__helper-plugin-utils/index.d.ts
@@ -3,7 +3,7 @@ import type * as babel from "@babel/core";
 export type BabelAPI = typeof babel & babel.ConfigAPI;
 export function declare<
     O extends Record<string, any>,
-    R extends babel.PluginObj = babel.PluginObj,
+    R extends babel.PluginObj<any> = babel.PluginObj,
 >(
     builder: (api: BabelAPI, options: O, dirname: string) => R,
 ): (api: object, options: O | null | undefined, dirname: string) => R;
