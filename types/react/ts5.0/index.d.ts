71b99ca5c8ee46dc7e77ae2e9d4d6b2277c1a906
Sebastian Sebbie Silbermann <sebastian.silbermann@vercel.com>
2025-05-12 17:17:53 +0200

    Backport

--- types/react/ts5.0/index.d.ts
+++ types/react/ts5.0/index.d.ts
@@ -1693,20 +1693,6 @@ declare namespace React {
      * @version 16.8.0
      * @see {@link https://react.dev/reference/react/useReducer}
      */
-    function useReducer<S, A extends AnyActionArg>(
-        reducer: (prevState: S, ...args: A) => S,
-        initialState: S,
-    ): [S, ActionDispatch<A>];
-    /**
-     * An alternative to `useState`.
-     *
-     * `useReducer` is usually preferable to `useState` when you have complex state logic that involves
-     * multiple sub-values. It also lets you optimize performance for components that trigger deep
-     * updates because you can pass `dispatch` down instead of callbacks.
-     *
-     * @version 16.8.0
-     * @see {@link https://react.dev/reference/react/useReducer}
-     */
     function useReducer<S, I, A extends AnyActionArg>(
         reducer: (prevState: S, ...args: A) => S,
         initialArg: I,
