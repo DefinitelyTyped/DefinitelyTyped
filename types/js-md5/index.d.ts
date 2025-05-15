212474faf5e5b0451c1fb36a1c830abe51aae75c
cold_zone <haolin.pan@unionman.com.cn>
2025-05-15 18:15:28 +0800

    Update index.d.ts
    Update index.d.ts

--- types/js-md5/index.d.ts
+++ types/js-md5/index.d.ts
@@ -1,29 +1,171 @@
 declare namespace md5 {
-    type message = string | any[] | Uint8Array | ArrayBuffer;
+  type Message = string | number[] | ArrayBuffer | Uint8Array;
 
-    interface Md5 {
-        array(): number[];
-        arrayBuffer(): ArrayBuffer;
-        buffer(): ArrayBuffer;
-        digest(): number[];
-        hex(): string;
-        toString(): string;
-        update(message: message): Md5;
-        base64(): string;
-    }
+  interface Hasher {
+    /**
+     * Update hash
+     *
+     * @param message The message you want to hash.
+     */
+    update(message: Message): Hasher;
 
-    interface md5 {
-        (message: message): string;
-        hex(message: message): string;
-        array(message: message): number[];
-        digest(message: message): number[];
-        arrayBuffer(message: message): ArrayBuffer;
-        buffer(message: message): ArrayBuffer;
-        create(): Md5;
-        update(message: message): Md5;
-        base64(message: message): string;
-    }
+    /**
+     * Return hash in hex string.
+     */
+    hex(): string;
+
+    /**
+     * Return hash in hex string.
+     */
+    toString(): string;
+
+    /**
+     * Return hash in ArrayBuffer.
+     */
+    arrayBuffer(): ArrayBuffer;
+
+    /**
+     * Return hash in integer array.
+     */
+    digest(): number[];
+
+    /**
+     * Return hash in integer array.
+     */
+    array(): number[];
+
+    /**
+     * Return hash in base64 string.
+     */
+    base64(): string;
+  }
+
+  interface Hmac {
+    /**
+     * Computes a Hash-based message authentication code (HMAC) using a secret key
+     *
+     * @param secretKey The Secret Key
+     * @param message The message you want to hash.
+     */
+    (secretKey: Message, message: Message): string;
+
+    /**
+     * Create a hash object using a secret key.
+     *
+     * @param secretKey The Secret Key
+     */
+    create(secretKey: Message): Hasher;
+
+    /**
+     * Create a hash object and hash message using a secret key
+     *
+     * @param secretKey The Secret Key
+     * @param message The message you want to hash.
+     */
+    update(secretKey: Message, message: Message): Hasher;
+
+    /**
+     * Return hash in hex string.
+     *
+     * @param secretKey The Secret Key
+     * @param message The message you want to hash.
+     */
+    hex(secretKey: Message, message: Message): string;
+
+    /**
+     * Return hash in ArrayBuffer.
+     *
+     * @param secretKey The Secret Key
+     * @param message The message you want to hash.
+     */
+    arrayBuffer(secretKey: Message, message: Message): ArrayBuffer;
+
+    /**
+     * Return hash in integer array.
+     *
+     * @param secretKey The Secret Key
+     * @param message The message you want to hash.
+     */
+    digest(secretKey: Message, message: Message): number[];
+
+    /**
+     * Return hash in integer array.
+     *
+     * @param secretKey The Secret Key
+     * @param message The message you want to hash.
+     */
+    array(secretKey: Message, message: Message): number[];
+
+    /**
+     * Return hash in base64 string.
+     *
+     * @param secretKey The Secret Key
+     * @param message The message you want to hash.
+     */
+    base64(secretKey: Message, message: Message): string;
+  }
+
+  interface Hash {
+    /**
+     * Hash and return hex string.
+     *
+     * @param message The message you want to hash.
+     */
+    (message: Message): string;
+
+    /**
+     * Create a hash object.
+     */
+    create(): Hasher;
+
+    /**
+     * Create a hash object and hash message.
+     *
+     * @param message The message you want to hash.
+     */
+    update(message: Message): Hasher;
+
+    /**
+     * Return hash in hex string.
+     *
+     * @param message The message you want to hash.
+     */
+    hex(message: Message): string;
+
+    /**
+     * Return hash in ArrayBuffer.
+     *
+     * @param message The message you want to hash.
+     */
+    arrayBuffer(message: Message): ArrayBuffer;
+
+    /**
+     * Return hash in integer array.
+     *
+     * @param message The message you want to hash.
+     */
+    digest(message: Message): number[];
+
+    /**
+     * Return hash in integer array.
+     *
+     * @param message The message you want to hash.
+     */
+    array(message: Message): number[];
+
+    /**
+     * Return hash in base64 string.
+     *
+     * @param message The message you want to hash.
+     */
+    base64(message: Message): string;
+
+    /**
+     * HMAC interface
+     */
+    hmac: Hmac;
+  }
 }
 
-declare const md5: md5.md5;
+declare const md5: md5.Hash;
 export = md5;
