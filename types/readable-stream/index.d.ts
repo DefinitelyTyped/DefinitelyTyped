4d77476becbc3417528b1468f148079d2f0217d8
Renegade334 <contact.9a5d6388@renegade334.me.uk>
2025-05-18 19:08:35 +0100

    remove safe-buffer dependency from readable-stream

--- types/readable-stream/index.d.ts
+++ types/readable-stream/index.d.ts
@@ -1,6 +1,5 @@
 /// <reference types="node" />
 
-import * as SafeBuffer from "safe-buffer";
 import type * as NodeStream from "stream";
 
 declare class StringDecoder {
@@ -235,7 +234,7 @@ declare namespace _Readable {
         next: Entry<D> | null;
     }
 
-    interface BufferList<D extends SafeBuffer.Buffer = SafeBuffer.Buffer> {
+    interface BufferList<D extends Buffer = Buffer> {
         head: Entry<D>;
         tail: Entry<D>;
         length: number;
