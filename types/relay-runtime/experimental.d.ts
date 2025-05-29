07d2798aa22d4c8f96e50b1d81bf0675ec215fb7
tobias-tengler <45513122+tobias-tengler@users.noreply.github.com>
2025-05-29 14:06:38 +0200

    Fix export

--- types/relay-runtime/experimental.d.ts
+++ types/relay-runtime/experimental.d.ts
@@ -1,3 +1,3 @@
-export type { observeFragment } from "./lib/store/experimental/observeFragment";
-export type { observeQuery } from "./lib/store/experimental/observeQuery";
-export type { waitForFragmentData } from "./lib/store/experimental/waitForFragmentData";
+export { observeFragment } from "./lib/store/experimental/observeFragment";
+export { observeQuery } from "./lib/store/experimental/observeQuery";
+export { waitForFragmentData } from "./lib/store/experimental/waitForFragmentData";
