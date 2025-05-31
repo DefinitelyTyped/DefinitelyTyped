7350b562394f86aff1910d43a628a5208a6a3697
Renegade334 <contact.9a5d6388@renegade334.me.uk>
2025-05-31 02:15:45 +0100

    klaw: esnext support for derived iterator overrides

--- types/klaw/index.d.ts
+++ types/klaw/index.d.ts
@@ -4,6 +4,14 @@ declare module "klaw" {
     import * as fs from "fs";
     import { Readable, ReadableOptions } from "stream";
 
+    // iterator helper type for TS <5.6
+    global {
+        interface AsyncIteratorObject<T, TReturn, TNext> {}
+    }
+    interface StreamIterator<T> extends AsyncIterator<T, any, any>, AsyncIteratorObject<T, any, any> {
+        [Symbol.asyncIterator](): StreamIterator<T>;
+    }
+
     function K(root: string, options?: K.Options): K.Walker;
 
     namespace K {
@@ -33,7 +41,7 @@ declare module "klaw" {
             on(event: "readable", listener: () => void): this;
             on(event: "error", listener: (err: Error) => void): this;
             read(): Item;
-            [Symbol.asyncIterator](): AsyncIterableIterator<Item>;
+            [Symbol.asyncIterator](): StreamIterator<Item>;
         }
     }
 
