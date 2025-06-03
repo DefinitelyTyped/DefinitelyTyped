59af51ee658f9f5b37f31b3afb589553f49057c1
Julian Catoni <julian.catoni@sql-ag.de>
2025-06-03 12:29:25 +0200

    bpmn-moddle: add d.cts file, adapt package.json + tsconfig.json

--- types/bpmn-moddle/bpmn-moddle-tests.ts
+++ types/bpmn-moddle/bpmn-moddle-tests.ts
@@ -1,18 +1,18 @@
-import { Moddle, BPMNModdle, Definitions, Process } from 'bpmn-moddle';
+import BPMNModdle from "bpmn-moddle";
 
-const moddle: Moddle = {} as any;
+const moddle: BPMNModdle.Moddle = {} as any;
 
 const element1 = moddle.create('bpmn:UserTask'); // Expect type User Task
 element1.$type = 'bpmn:UserTask'; // Expect to check against type correctly
 
-const bpmnModdle: BPMNModdle = {} as any;
+const bpmnModdle: BPMNModdle.BPMNModdle = {} as any;
 
 bpmnModdle.fromXML('');
 
-const definition: Definitions = {} as any;
+const definition: BPMNModdle.Definitions = {} as any;
 
 // Expect type of process or undefined
 const maybeProcess = definition.rootElements.find(i => i.$type === 'bpmn:Process');
 
 // Expect process to have additional typings to base element1
-const process = maybeProcess as Process;
+const process = maybeProcess as BPMNModdle.Process;
