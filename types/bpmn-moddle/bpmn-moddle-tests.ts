a481bbe377965d543f7f83c4aae129d13fc01558
Julian Catoni <julian.catoni@sql-ag.de>
2025-06-03 09:18:10 +0200

    bpmn-moddle: replace single quotes with double quotes in test

--- types/bpmn-moddle/bpmn-moddle-tests.ts
+++ types/bpmn-moddle/bpmn-moddle-tests.ts
@@ -1,18 +1,18 @@
-import { Moddle, BPMNModdle, Definitions, Process } from 'bpmn-moddle';
+import { Moddle, BPMNModdle, Definitions, Process } from "bpmn-moddle";
 
 const moddle: Moddle = {} as any;
 
-const element1 = moddle.create('bpmn:UserTask'); // Expect type User Task
-element1.$type = 'bpmn:UserTask'; // Expect to check against type correctly
+const element1 = moddle.create("bpmn:UserTask"); // Expect type User Task
+element1.$type = "bpmn:UserTask"; // Expect to check against type correctly
 
 const bpmnModdle: BPMNModdle = {} as any;
 
-bpmnModdle.fromXML('');
+bpmnModdle.fromXML("");
 
 const definition: Definitions = {} as any;
 
 // Expect type of process or undefined
-const maybeProcess = definition.rootElements.find(i => i.$type === 'bpmn:Process');
+const maybeProcess = definition.rootElements.find(i => i.$type === "bpmn:Process");
 
 // Expect process to have additional typings to base element1
 const process = maybeProcess as Process;
