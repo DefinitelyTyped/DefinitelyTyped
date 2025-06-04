cc47c4750ccb54315479b6205e7fd9ce88517629
cappie <cappie@starfishspace.com>
2025-06-04 16:05:01 -0600

    formatted tests file with dprint

--- types/plotly.js/test/core-tests.ts
+++ types/plotly.js/test/core-tests.ts
@@ -526,22 +526,22 @@ const layout = {
 (() => {
     const data: Array<Partial<PlotData>> = [
         {
-            x: [1,2,3,4,5],
+            x: [1, 2, 3, 4, 5],
             y: [0.45803057, 0.51208141, 0.29845217, 0.09788102, 0.2866858],
             type: "scatter",
-            yaxis: "y"
+            yaxis: "y",
         },
         {
-            x: [1,2,3,4,5],
-            y: [150.36977661, 641.21444452,  39.2096064 , 546.90053751, 97.10039431],
+            x: [1, 2, 3, 4, 5],
+            y: [150.36977661, 641.21444452, 39.2096064, 546.90053751, 97.10039431],
             type: "scatter",
-            yaxis: "y2"
-        }
-    ]
+            yaxis: "y2",
+        },
+    ];
     const layout: Partial<Layout> = {
-        yaxis: {side: "left"},
-        yaxis2: {side: "right", overlaying: "y", tickmode: "sync"}
-    }
+        yaxis: { side: "left" },
+        yaxis2: { side: "right", overlaying: "y", tickmode: "sync" },
+    };
 
     Plotly.newPlot("myDiv", data, layout);
 })();
