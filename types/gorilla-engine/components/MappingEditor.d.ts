0ca93b7aa660dfe9c64df763186f7b1822f4f37f
Antony <agachuma@ujam.com>
2025-06-06 17:18:39 +0200

    changed selectZone to selectZones

--- types/gorilla-engine/components/MappingEditor.d.ts
+++ types/gorilla-engine/components/MappingEditor.d.ts
@@ -17,7 +17,7 @@ declare namespace GorillaEngine.UI {
         refreshView(index?: number): void;
         selectAllZones(): void;
         deselectAllZones(): void;
-        selectZone(zone: Partial<Zone>):void;
+        selectZones(zones: Partial<Zone[]>):void;
         addZone(zone: Partial<Zone>): Zone;
         removeZone(zone: Partial<Zone>): void;
     }
