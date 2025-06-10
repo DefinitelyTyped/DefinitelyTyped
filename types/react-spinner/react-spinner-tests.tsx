de0dffee06325f4852401be6cedd5a0386d5681d
Paul Sukow <21179028+paulsukow@users.noreply.github.com>
2025-06-10 08:12:21 -0600

    Update react-spinner-tests.tsx

--- types/react-spinner/react-spinner-tests.tsx
+++ types/react-spinner/react-spinner-tests.tsx
@@ -2,3 +2,5 @@ import * as React from "react";
 import Spinner from "react-spinner";
 
 const spinner: React.JSX.Element = <Spinner />;
+
+const spinnerWithClass: React.JSX.Element = <Spinner className="test" />;
