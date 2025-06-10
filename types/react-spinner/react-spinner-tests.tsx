52a9ef8bda50cd5774d054596940669c7d4c0845
Paul Sukow <21179028+paulsukow@users.noreply.github.com>
2025-06-10 08:14:01 -0600

    Update react-spinner-tests.tsx

--- types/react-spinner/react-spinner-tests.tsx
+++ types/react-spinner/react-spinner-tests.tsx
@@ -3,4 +3,10 @@ import Spinner from "react-spinner";
 
 const spinner: React.JSX.Element = <Spinner />;
 
+// $ExpectType void
 const spinnerWithClass: React.JSX.Element = <Spinner className="test" />;
+
+// @ts-expect-error
+const spinnerWithInvalidDivProp: React.JSX.Element = <Spinner invalidProp="test" />;
+
+
