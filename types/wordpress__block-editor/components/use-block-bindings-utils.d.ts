6e18f15b6fb11f12078b02a7b93e2cb01926cdb5
Joshua Lipstone <joshualip@plaudit.com>
2025-06-14 12:49:37 -0500

    Added some missing hooks

---
+++ types/wordpress__block-editor/components/use-block-bindings-utils.d.ts
@@ -0,0 +1,4 @@
+export function useBlockBindingsUtils(clientId?: string): {
+	updateBlockBindings(bindings: {[binding: string]: {source: string, args?: {[key: string]: unknown}}|undefined}): void,
+	removeAllBlockBindings(): void
+}
