70cda2044f7cae4944668d5259187a7f154ec4af
Georgy Ustinov <inbox@itustinov.ru>
2025-06-24 16:44:45 +0300

    [russian-nouns-js] Removed run-time checks

--- types/russian-nouns-js/russian-nouns-js-tests.ts
+++ types/russian-nouns-js/russian-nouns-js-tests.ts
@@ -1,38 +1,3 @@
-// <editor-fold defaultstate="collapsed" desc="Assertion functions">
-function assertEquals(a: any, b: any) {
-    if (a !== b) {
-        throw new Error(`${a} !== ${b}`);
-    }
-}
-
-function assertEqualsSingleValue(array: string[], value: string) {
-    assertEquals(array.length, 1);
-    assertEquals(array[0], value);
-}
-
-function assertAllCases(
-    results: Array<string[]>,
-    values: Array<string[] | string>,
-) {
-    assertEquals(results.length, 7);
-    assertEquals(values.length, 7);
-
-    for (let i = 0; i < 7; i++) {
-        const result = results[i];
-        const value = values[i];
-
-        if (typeof value === "string") {
-            assertEqualsSingleValue(result, value);
-        } else {
-            assertEquals(result.length, value.length);
-            for (let j = 0; j < value.length; j++) {
-                assertEquals(result[j], value[j]);
-            }
-        }
-    }
-}
-// </editor-fold>
-
 import {
     Case,
     CASES,
@@ -48,12 +13,11 @@ import {
 const rne = new Engine();
 
 let result: string[];
+let results: string[][];
 
 result = rne.decline({ text: "имя", gender: Gender.NEUTER }, Case.GENITIVE);
-assertEqualsSingleValue(result, "имени");
 
 result = rne.decline({ text: "имя", gender: Gender.NEUTER }, Case.INSTRUMENTAL);
-assertEqualsSingleValue(result, "именем");
 
 let coat: Lemma = createLemma({
     text: "пальто",
@@ -62,92 +26,60 @@ let coat: Lemma = createLemma({
 });
 
 result = rne.decline(coat, Case.GENITIVE);
-assertEqualsSingleValue(result, "пальто");
 
-assertEquals(coat.getDeclension(), -1);
+coat.getDeclension();
 
 let mountain: Lemma = createLemma({
     text: "гора",
     gender: Gender.FEMININE,
 });
 
-let results: string[][] = CASES.map(function(c: Case) {
+results = CASES.map(function(c: Case) {
     return rne.decline(mountain, c);
 });
 
-assertAllCases(results, ["гора", "горы", "горе", "гору", ["горой", "горою"], "горе", "горе"]);
-
 result = rne.pluralize(mountain);
-assertEqualsSingleValue(result, "горы");
+
 const pluralMountain: string = result[0];
 
 results = CASES.map(function(c: Case) {
     return rne.decline(mountain, c, pluralMountain);
 });
 
-assertAllCases(results, ["горы", "гор", "горам", "горы", "горами", "горах", "горах"]);
+let num: number;
+let str: string;
 
-assertEquals(mountain.getDeclension(), 2);
-assertEquals(mountain.getSchoolDeclension(), 1);
+num = mountain.getDeclension();
+num = mountain.getSchoolDeclension();
 
 let mountainGender: Gender | undefined = mountain.getGender();
-assertEquals(mountainGender, Gender.FEMININE);
 
-assertEquals(null, createLemmaOrNull({ text: "абвгд" }));
+let nothing: Lemma | null = createLemmaOrNull({ text: "абвгд" });
 
-(() => {
-    let x: Lemma | null = createLemmaOrNull({
-        text: "гора",
-        gender: Gender.FEMININE,
-    });
+const x: Lemma | null = createLemmaOrNull({
+    text: "гора",
+    gender: Gender.FEMININE,
+});
 
-    assertEquals(x != null, true);
+if (x != null) {
+    str = x.text();
+}
 
-    if (x != null) {
-        assertEquals(x.text(), "гора");
-    }
-})();
+// ----------------------------------------------------------------------------
 
-console.log("  Main functions (8) ............... OK");
-
-let cringe: Lemma = createLemma({
+const cringe: Lemma = createLemma({
     text: "кринж",
     gender: Gender.MASCULINE,
 });
 
 result = rne.decline(cringe, Case.INSTRUMENTAL);
-assertEqualsSingleValue(result, "кринжем");
 
 rne.sd.put(cringe, "SEESESE-EEEEEE");
 result = rne.decline(cringe, Case.INSTRUMENTAL);
-assertEqualsSingleValue(result, "кринжом");
 
-assertEquals(rne.sd.hasStressedEndingSingular(cringe, Case.INSTRUMENTAL).length, 1);
-assertEquals(rne.sd.hasStressedEndingSingular(cringe, Case.INSTRUMENTAL)[0], true);
+const stressedEndings: boolean[] = rne.sd.hasStressedEndingSingular(cringe, Case.INSTRUMENTAL);
 
-rne.sd.put(cringe, "SEESbSE-EEEEEE");
-result = rne.decline(cringe, Case.INSTRUMENTAL);
-assertEquals(result.length, 2);
-assertEquals(result[0], "кринжем");
-assertEquals(result[1], "кринжом");
-
-rne.sd.put(cringe, "SEESsSE-EEEEEE");
-result = rne.decline(cringe, Case.INSTRUMENTAL);
-assertEquals(result.length, 2);
-assertEquals(result[0], "кринжем");
-assertEquals(result[1], "кринжом");
-
-rne.sd.put(cringe, "SEESeSE-EEEEEE");
-result = rne.decline(cringe, Case.INSTRUMENTAL);
-assertEquals(result.length, 2);
-assertEquals(result[0], "кринжом");
-assertEquals(result[1], "кринжем");
-
-assertEquals(rne.sd.hasStressedEndingSingular(cringe, Case.INSTRUMENTAL).length, 2);
-assertEquals(rne.sd.hasStressedEndingSingular(cringe, Case.INSTRUMENTAL)[0], true);
-assertEquals(rne.sd.hasStressedEndingSingular(cringe, Case.INSTRUMENTAL)[1], false);
-
-console.log("  StressDictionary (9) ............. OK");
+// ----------------------------------------------------------------------------
 
 let row: Lemma = createLemma({
     text: "ряд",
@@ -158,14 +90,8 @@ results = CASES.map(function(c: Case) {
     return rne.decline(row, c);
 });
 
-assertAllCases(results, ["ряд", "ряда", "ряду", "ряд", "рядом", "ряде", "ряду"]);
-
 let rowLocativeForms: LocativeForm[] = rne.getLocativeForms(row);
 
-assertEquals(rowLocativeForms.length, 1);
-assertEquals(rowLocativeForms[0].preposition, "в");
-assertEquals(rowLocativeForms[0].word, "ряду");
-assertEquals(rowLocativeForms[0].attributes > 0, true);
-assertEquals(rowLocativeForms[0].attributes, LocativeFormAttribute.STRUCTURE);
-
-console.log("  LocativeForm (10) ................ OK");
+str = rowLocativeForms[0].preposition;
+str = rowLocativeForms[0].word;
+num = rowLocativeForms[0].attributes;
