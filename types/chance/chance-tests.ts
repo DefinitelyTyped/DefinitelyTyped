44a0eadc25348a2fd8ba880fffb6e0d7d2e614d8
Jan Pilzer <Hirse@users.noreply.github.com>
2025-06-25 17:46:19 -0700

    Format test file

--- types/chance/chance-tests.ts
+++ types/chance/chance-tests.ts
@@ -59,7 +59,7 @@ const chanceConstructedWithStringSeed = new Chance("test");
 const chanceConstructedWithMultipleParameters = new Chance("test", 1, 1.5);
 
 // Allow picking from readonly arrays
-const arr: readonly number[] = [1,2,3];
+const arr: readonly number[] = [1, 2, 3];
 chance.pickone(arr);
 const picked: number[] = chance.pickset(arr, 2);
 chance.shuffle(arr);
