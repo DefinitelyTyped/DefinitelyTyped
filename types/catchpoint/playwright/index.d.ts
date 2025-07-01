c342b89f4b3a8b4750d0bbb6c911bcc52a639ac8
Jabran Rafique <2131246+jabranr@users.noreply.github.com>
2025-07-01 18:32:11 +0100

    feat: add types for playwright in catchpoint

---
+++ types/catchpoint/playwright/index.d.ts
@@ -0,0 +1,35 @@
+declare namespace Catchpoint {
+    export function username(_username?: string): Promise<string | null>;
+    export function password(_password?: string): Promise<string | null>;
+    export function startStep(stepName: string, opt_disableAutoMatchRequests?: boolean[]): Promise<void>;
+    export function storeGlobalVariable(value: string, name: string): Promise<void>;
+    export function setTracepoint(insightToken: string, insightValue: string): Promise<void>;
+    export function setIndicator(insightToken:string, insightValue: string): Promise<void>;
+}
+
+declare module '@playwright/test' {
+    export interface Page {
+        addInitScript (): never;
+        close (): never;
+        pdf (): never;
+        routeFromHAR (): never;
+        screenshot (): never;
+        setDefaultNavigationTimeout (): never;
+        setDefaultTimeout (): never;
+        video (): never;
+        workers (): never;
+    }
+
+    export interface Browser {
+        new (): never;
+    }
+    export interface BrowserContext {
+        new (): never;
+    }
+    export interface BrowserServer {
+        new (): never;
+    }
+    export interface BrowserType {
+        new (): never;
+    }
+}
