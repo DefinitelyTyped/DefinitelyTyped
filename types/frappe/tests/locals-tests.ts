49cfee8e8f22c9e81312f5ee9afa2786474717ac
Hakim Bawa <bawahakim@gmail.com>
2025-07-02 22:41:17 +0900

    feat: add locals typing

---
+++ types/frappe/tests/locals-tests.ts
@@ -0,0 +1,30 @@
+
+interface MyDoc extends DocType {
+    id: number;
+    name: string;
+}
+
+interface Another extends DocTypeChildTable {
+    flag: boolean;
+}
+
+declare global {
+    interface DocTypeMap {
+        MyDoc: MyDoc;
+        Another: Another;
+    }
+}
+
+const id: number = locals.MyDoc['doc1'].id;
+
+const name: string = locals.MyDoc['doc1'].name;
+
+const flag: boolean = locals.Another['row1'].flag;
+
+// @ts-expect-error
+locals.Unknown['x'];
+
+// @ts-expect-error
+locals.MyDoc['doc1'].unknown;
+
+export {}
---
+++ types/frappe/types/globals/locals.d.ts
@@ -0,0 +1,17 @@
+type Local = {
+    [K in keyof DocTypeMap]: { [docName: string]: DocTypeMap[K] }
+}
+
+/**
+ * Global object containing all local data in the form
+ * Typically used when trying to access nested data, such as the row of a child table
+ * @example
+ * ```typescript
+ * frappe.ui.form.on('My Child Table', {
+ *     async my_field(frm, cdt, cdn) {
+ *         const row = locals[cdt][cdn];
+ *     }
+ * });
+ * ```
+ */
+declare const locals: Local;
