1b4df8bd035e48199d69be95faf5bb3ff4606be6
Jabran Rafique <2131246+jabranr@users.noreply.github.com>
2025-07-04 17:15:46 +0100

    feat: add types definition for catchpoint playwright

---
+++ types/catchpoint-playwright/catchpoint-playwright-test.ts
@@ -0,0 +1,22 @@
+import { test } from '@playwright/test';
+
+// $ExpectType Promise<string | null>
+Catchpoint.username('testUsername')
+
+// $ExpectType Promise<string | null>
+Catchpoint.password('testPassword');
+
+// $ExpectType Promise<void>
+Catchpoint.startStep('Test Step');
+
+// $ExpectType Promise<void>
+Catchpoint.startStep('Test Step', [false]);
+
+// $ExpectType Promise<void>
+Catchpoint.storeGlobalVariable('testValue', 'testName');
+
+// $ExpectType Promise<void>
+Catchpoint.setTracepoint('insightToken', 'insightValue');
+
+// $ExpectType Promise<void>
+Catchpoint.setIndicator('insightToken', 'insightValue');
---
+++ types/catchpoint-playwright/playwright.d.ts
@@ -0,0 +1,24 @@
+interface Page {
+    addInitScript (): never;
+    close (): never;
+    pdf (): never;
+    routeFromHAR (): never;
+    screenshot (): never;
+    setDefaultNavigationTimeout (): never;
+    setDefaultTimeout (): never;
+    video (): never;
+    workers (): never;
+}
+
+declare class Browser {
+    new (): never;
+}
+declare class BrowserContext {
+    new (): never;
+}
+declare class BrowserServer {
+    new (): never;
+}
+declare class BrowserType {
+    new (): never;
+}
