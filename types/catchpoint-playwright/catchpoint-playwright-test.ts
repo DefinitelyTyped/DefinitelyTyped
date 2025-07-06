c927e04b23da3173794a9665e5b193ece758682d
Jabran Rafique <2131246+jabranr@users.noreply.github.com>
2025-07-06 07:27:32 +0100

    chore(test): add coverage for eslint rules

--- types/catchpoint-playwright/catchpoint-playwright-test.ts
+++ types/catchpoint-playwright/catchpoint-playwright-test.ts
@@ -1,3 +1,6 @@
+// eslint-disable-next-line no-restricted-imports
+import { test, expect, type Browser } from '@playwright/test'
+
 // $ExpectType Promise<string | null>
 Catchpoint.username('testUsername')
 
@@ -18,3 +21,36 @@ Catchpoint.setTracepoint('insightToken', 'insightValue');
 
 // $ExpectType Promise<void>
 Catchpoint.setIndicator('insightToken', 'insightValue');
+
+// $ExpectType void
+test('unsupported methods', async({ page }) => {
+
+    // eslint-disable-next-line no-restricted-properties
+    page.addInitScript('console.log("Test Init Script")');
+
+    // eslint-disable-next-line no-restricted-properties
+    page.close();
+
+    // eslint-disable-next-line no-restricted-properties
+    page.pdf();
+
+    // eslint-disable-next-line no-restricted-properties
+    page.routeFromHAR('path/to/har/file.har', { update: true });
+
+    // eslint-disable-next-line no-restricted-properties
+    page.screenshot();
+
+    // eslint-disable-next-line no-restricted-properties
+    page.setDefaultNavigationTimeout(10000);
+
+    // eslint-disable-next-line no-restricted-properties
+    page.setDefaultTimeout(30000);
+
+    // eslint-disable-next-line no-restricted-properties
+    page.video();
+
+    // eslint-disable-next-line no-restricted-properties
+    page.workers();
+
+    expect(page.context().browser() as Browser).toBeDefined();
+});
