7f845610595ef3772100dffae37eaa249a04e995
WolfgänG <felipefernandes.sjc@hotmail.com>
2025-07-08 05:00:40 -0300

    run format

--- types/express-xss-sanitizer/express-xss-sanitizer-tests.ts
+++ types/express-xss-sanitizer/express-xss-sanitizer-tests.ts
@@ -2,22 +2,25 @@ import { sanitize, xss, type XssSanitizerOptions } from "express-xss-sanitizer";
 
 // Test: configuring options
 const options: XssSanitizerOptions = {
-  allowedTags: ['b', 'i', 'u'],
-  allowedAttributes: { a: ['href'] },
-  allowedKeys: ['foo', 'bar'],
+    allowedTags: ["b", "i", "u"],
+    allowedAttributes: { a: ["href"] },
+    allowedKeys: ["foo", "bar"],
 };
 
 // $ExpectType (req: any, res: any, next: any) => void
 xss(options);
 
 // $ExpectType string[]
-sanitize(['<b>foo</b>'], options);
+sanitize(["<b>foo</b>"], options);
 
 // $ExpectType { foo: string; bar: string; }
-sanitize({ foo: '<b>abc</b>', bar: '<i>def</i>' }, options);
+sanitize({ foo: "<b>abc</b>", bar: "<i>def</i>" }, options);
 
-interface User { name: string; bio: string; }
-const user: User = { name: 'Felipe', bio: '<u>dev</u>' };
+interface User {
+    name: string;
+    bio: string;
+}
+const user: User = { name: "Felipe", bio: "<u>dev</u>" };
 
 // $ExpectType User
 sanitize<User>(user, options);
--- types/express-xss-sanitizer/index.d.ts
+++ types/express-xss-sanitizer/index.d.ts
@@ -2,7 +2,7 @@ interface XssSanitizerOptions {
     allowedTags?: string[];
     allowedAttributes?: Record<string, string[]>;
     allowedKeys?: string[];
-  }
+}
 
 declare function xss(options?: XssSanitizerOptions): (req: any, res: any, next: any) => void;
 declare function sanitize<T = unknown>(data: T, options?: XssSanitizerOptions): T;
