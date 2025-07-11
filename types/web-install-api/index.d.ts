80e0fbeee4f26bfd40c077be2a96b31f0002f3e1
Lia Hiscock <liahiscock@outlook.com>
2025-07-11 11:20:14 -0700

    format

--- types/web-install-api/index.d.ts
+++ types/web-install-api/index.d.ts
@@ -1,5 +1,5 @@
 interface Navigator {
-  install(): Promise<string>;
-  install(installUrl: string): Promise<string>;
-  install(installUrl: string, manifestId: string): Promise<string>;
+    install(): Promise<string>;
+    install(installUrl: string): Promise<string>;
+    install(installUrl: string, manifestId: string): Promise<string>;
 }
