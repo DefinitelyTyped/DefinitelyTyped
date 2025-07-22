e97a30dd3a339c5345eb0101fd1890110452cc0b
Kirby Brown <kirbycolebrown@gmail.com>
2025-07-21 20:11:50 -0500

    Format modified files.

--- types/rails__activestorage/index.d.ts
+++ types/rails__activestorage/index.d.ts
@@ -3,48 +3,48 @@ export as namespace ActiveStorage;
 export function start(): void;
 
 export class DirectUpload {
-  id: number;
-  file: File;
-  url: string;
-  delegate?: DirectUploadDelegate;
-  customHeaders: Record<string, string>;
+    id: number;
+    file: File;
+    url: string;
+    delegate?: DirectUploadDelegate;
+    customHeaders: Record<string, string>;
 
-  constructor(
-    file: File,
-    url: string,
-    delegate?: DirectUploadDelegate,
-    customHeaders?: Record<string, string>
-  );
+    constructor(
+        file: File,
+        url: string,
+        delegate?: DirectUploadDelegate,
+        customHeaders?: Record<string, string>,
+    );
 
-  create(callback: (error: Error | null, blob?: Blob) => void): void;
+    create(callback: (error: Error | null, blob?: Blob) => void): void;
 
-  // Dynamically assigned after create()
-  xhr?: XMLHttpRequest;
-  uploadRequest?: XMLHttpRequest;
+    // Dynamically assigned after create()
+    xhr?: XMLHttpRequest;
+    uploadRequest?: XMLHttpRequest;
 
-  // Optional hook assignable directly
-  directUploadWillCreateBlobWithXHR?: (xhr: XMLHttpRequest) => void;
+    // Optional hook assignable directly
+    directUploadWillCreateBlobWithXHR?: (xhr: XMLHttpRequest) => void;
 }
 
 export interface DirectUploadDelegate {
-  directUploadWillCreateBlobWithXHR?: ((xhr: XMLHttpRequest) => void) | undefined;
-  directUploadWillStoreFileWithXHR?: ((xhr: XMLHttpRequest) => void) | undefined;
+    directUploadWillCreateBlobWithXHR?: ((xhr: XMLHttpRequest) => void) | undefined;
+    directUploadWillStoreFileWithXHR?: ((xhr: XMLHttpRequest) => void) | undefined;
 }
 
 export interface Blob {
-  signed_id: string;
-  filename: string;
-  content_type: string;
-  byte_size: number;
-  checksum: string;
+    signed_id: string;
+    filename: string;
+    content_type: string;
+    byte_size: number;
+    checksum: string;
 }
 
 export class DirectUploadController {
-  constructor(input: HTMLInputElement, file: File);
-  start(callback: (error: Error | null) => void): void;
+    constructor(input: HTMLInputElement, file: File);
+    start(callback: (error: Error | null) => void): void;
 }
 
 export class DirectUploadsController {
-  constructor(form: HTMLFormElement);
-  start(callback: (error: Error | null) => void): void;
+    constructor(form: HTMLFormElement);
+    start(callback: (error: Error | null) => void): void;
 }
