6a4375e5d56982821e64d82f022d0842defe3a6f
Indian Modassir <indianmodassir@gmail.com>
2025-07-29 16:38:51 +0530

    Update test.ts

--- types/quanter/test.ts
+++ types/quanter/test.ts
@@ -1,3 +1,6 @@
-import Quanter = require("quanter");
-
-console.log(Quanter);
+function setFilters_0() {
+    Sizzle.selectors.setFilters["first"] = (elements, argument, not) => {
+        // No argument for first
+        return not ? elements.slice(1) : [elements[0]];
+    };
+}
