25a84e0cd4f16cbf8ced5403d5c0c3279fd90929
Sebastian Landwehr <info@sebastianlandwehr.com>
2025-08-02 00:08:42 +0000

    feat: parse-packagejson-name

---
+++ types/parse-packagejson-name/index.d.ts
@@ -0,0 +1,8 @@
+declare function parsePackagejsonName(name: string | { name: string }): {
+  scope: string | null,
+  fullName: string,
+  projectName: string | null,
+  moduleName: string | null,
+};
+
+export = parsePackagejsonName;
