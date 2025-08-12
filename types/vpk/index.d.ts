37ab063cf84d335723fed459c7e800cc922d8082
Veak <51446230+v0eak@users.noreply.github.com>
2025-08-12 15:47:35 +0000

    Update index.d.ts

--- types/vpk/index.d.ts
+++ types/vpk/index.d.ts
@@ -1,39 +1,36 @@
 /// <reference types="node" />
+  
+declare namespace VPK {
+    interface VPKHeader {
+        version: number
+        treeLength: number
+        unknown1?: number
+        footerLength?: number
+        unknown3?: number
+        unknown4?: number
+    }
+      
+    interface VPKDirectoryEntry {
+        crc: number
+        preloadBytes: number
+        archiveIndex: number
+        entryOffset: number
+        entryLength: number
+        preloadOffset?: number
+    }
+}
 
-interface VPKHeader {
-    version: number
-    treeLength: number
-    unknown1?: number
-    footerLength?: number
-    unknown3?: number
-    unknown4?: number
-}
-  
-interface VPKDirectoryEntry {
-    crc: number
-    preloadBytes: number
-    archiveIndex: number
-    entryOffset: number
-    entryLength: number
-    preloadOffset?: number
-}
-  
 declare class VPK {
     constructor(path: string)
   
     directoryPath: string
-    header: VPKHeader
-    tree: Record<string, VPKDirectoryEntry>
+    header: VPK.VPKHeader
+    tree: Record<string, VPK.VPKDirectoryEntry>
   
     isValid(): boolean
     load(): void
     readonly files: string[]
     getFile(path: string): Buffer | null
 }
-  
-declare namespace VPK {
-    type Header = VPKHeader
-    type Entry = VPKDirectoryEntry
-}
-  
+
 export = VPK
