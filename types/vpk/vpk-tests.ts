b31f0c3811c832c361774e6372bb22d98f695c87
Veak <51446230+v0eak@users.noreply.github.com>
2025-08-12 16:42:44 +0000

    Update vpk-tests.ts

--- types/vpk/vpk-tests.ts
+++ types/vpk/vpk-tests.ts
@@ -1,51 +1,51 @@
 import VPK = require("vpk");
 
 // $ExpectType VPK
-const archive = new VPK('path/to/archive.vpk')
+const archive = new VPK("path/to/archive.vpk");
 
 // $ExpectType string
-archive.directoryPath
+archive.directoryPath;
 
 // $ExpectType VPKHeader
-archive.header
+archive.header;
 
 // $ExpectType Record<string, VPKDirectoryEntry>
-archive.tree
+archive.tree;
 
 // $ExpectType boolean
-archive.isValid()
+archive.isValid();
 
 // $ExpectType void
-archive.load()
+archive.load();
 
 // $ExpectType string[]
-archive.files
+archive.files;
 
 // $ExpectType Buffer | null || Buffer<ArrayBufferLike> | null
-archive.getFile('some/file.txt')
+archive.getFile("some/file.txt");
 
 // Manual construction of types for testing
 
 const header: VPK.VPKHeader = {
-  version: 2,
-  treeLength: 1024,
-  unknown1: 0,
-  footerLength: 512,
-  unknown3: 1,
-  unknown4: 2,
-}
+    version: 2,
+    treeLength: 1024,
+    unknown1: 0,
+    footerLength: 512,
+    unknown3: 1,
+    unknown4: 2,
+};
 
 // $ExpectType VPKHeader
-header
+header;
 
 const entry: VPK.VPKDirectoryEntry = {
-  crc: 123456,
-  preloadBytes: 128,
-  archiveIndex: 1,
-  entryOffset: 2048,
-  entryLength: 4096,
-  preloadOffset: 64,
-}
+    crc: 123456,
+    preloadBytes: 128,
+    archiveIndex: 1,
+    entryOffset: 2048,
+    entryLength: 4096,
+    preloadOffset: 64,
+};
 
 // $ExpectType VPKDirectoryEntry
-entry
+entry;
