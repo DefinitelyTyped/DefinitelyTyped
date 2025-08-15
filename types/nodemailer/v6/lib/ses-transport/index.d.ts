d8517d711fa2a718d98c41ad198390d134c5b6e9
Jimmy Leung <43258070+hkleungai@users.noreply.github.com>
2025-08-15 16:23:19 +0800

    feat(nodemailer): update for v6 & v7 (#73297)

--- types/nodemailer/v6/lib/ses-transport/index.d.ts
+++ types/nodemailer/v6/lib/ses-transport/index.d.ts
@@ -1,12 +1,10 @@
 /// <reference types="node" />
 
-import aws from "@aws-sdk/client-sesv2";
+import aws from "@aws-sdk/client-ses";
 import { EventEmitter } from "node:events";
 
 import { Transport, TransportOptions } from "../..";
-
 import * as shared from "../shared";
-
 import Mail = require("../mailer");
 import MailMessage = require("../mailer/mail-message");
 import MimeNode = require("../mime-node");
@@ -22,11 +20,17 @@ declare namespace SESTransport {
     interface MailSesOptions extends Partial<aws.SendEmailRequest> {}
 
     interface Options extends MailOptions, TransportOptions {
-        /** is an option that expects an instantiated aws.SES object */
-        SES: {
-            sesClient: aws.SESv2Client;
-            SendEmailCommand: typeof aws.SendEmailCommand;
-        };
+        /** An option that expects an instantiated aws.SES object */
+        SES:
+            | {
+                ses: aws.SES;
+                aws: typeof aws;
+            }
+            | aws.SES;
+        /** How many messages per second is allowed to be delivered to SES */
+        maxConnections?: number | undefined;
+        /** How many parallel connections to allow towards SES */
+        sendingRate?: number | undefined;
     }
 
     interface SentMessageInfo {
@@ -53,12 +57,21 @@ declare class SESTransport extends EventEmitter implements Transport<SESTranspor
 
     ses: SESTransport.Options["SES"];
 
-    constructor(options: SESTransport.Options);
+    maxConnections: number;
+    connections: number;
+    sendingRate: number;
+    sendingRateTTL: number | null;
+    rateInterval: number;
+    rateMessages: Array<{ ts: number; pending: boolean }>;
+    pending: Array<
+        {
+            mail: Mail<SESTransport.SentMessageInfo>;
+            callback(err: Error | null, info: SESTransport.SentMessageInfo): void;
+        }
+    >;
+    idling: boolean;
 
-    /** Undocumented */
-    getRegion(
-        callback: (err: Error | null, region: string | undefined) => void,
-    ): void;
+    constructor(options: SESTransport.Options);
 
     /** Schedules a sending of a message */
     send(
@@ -66,9 +79,26 @@ declare class SESTransport extends EventEmitter implements Transport<SESTranspor
         callback: (err: Error | null, info: SESTransport.SentMessageInfo) => void,
     ): void;
 
+    /** Returns true if there are free slots in the queue */
+    isIdle(): boolean;
+
     /** Verifies SES configuration */
     verify(callback: (err: Error | null, success: true) => void): void;
     verify(): Promise<true>;
+
+    addListener(event: "idle", listener: () => void): this;
+
+    emit(event: "idle"): boolean;
+
+    on(event: "idle", listener: () => void): this;
+
+    once(event: "idle", listener: () => void): this;
+
+    prependListener(event: "idle", listener: () => void): this;
+
+    prependOnceListener(event: "idle", listener: () => void): this;
+
+    listeners(event: "idle"): Array<() => void>;
 }
 
 export = SESTransport;
