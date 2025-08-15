435ff148daa2b3cfecf356db88bcfa4764844fce
Jabran Rafique <2131246+jabranr@users.noreply.github.com>
2025-08-15 08:42:39 +0100

    chore: remove eslint config

--- types/catchpoint-playwright/catchpoint-playwright-test.ts
+++ types/catchpoint-playwright/catchpoint-playwright-test.ts
@@ -1,6 +1,3 @@
-// eslint-disable-next-line no-restricted-imports
-import { test, expect, type Browser, BrowserContext } from '@playwright/test'
-
 // $ExpectType Promise<string | null>
 Catchpoint.username('testUsername')
 
@@ -21,46 +18,3 @@ Catchpoint.setTracepoint('insightToken', 'insightValue');
 
 // $ExpectType Promise<void>
 Catchpoint.setIndicator('insightToken', 'insightValue');
-
-// $ExpectType void
-test('unsupported methods', async({ page, browser }) => {
-
-    // eslint-disable-next-line no-restricted-properties
-    page.addInitScript('console.log("Test Init Script")');
-
-    // eslint-disable-next-line no-restricted-properties
-    page.close();
-
-    // eslint-disable-next-line no-restricted-properties
-    page.pdf();
-
-    // eslint-disable-next-line no-restricted-properties
-    page.routeFromHAR('path/to/har/file.har', { update: true });
-
-    // eslint-disable-next-line no-restricted-properties
-    page.screenshot();
-
-    // eslint-disable-next-line no-restricted-properties
-    page.setDefaultNavigationTimeout(10000);
-
-    // eslint-disable-next-line no-restricted-properties
-    page.setDefaultTimeout(30000);
-
-    // eslint-disable-next-line no-restricted-properties
-    page.video();
-
-    // eslint-disable-next-line no-restricted-properties
-    page.workers();
-
-    // eslint-disable-next-line no-restricted-properties
-    page.context();
-
-    // eslint-disable-next-line no-restricted-properties
-    browser.newContext();
-
-    // eslint-disable-next-line no-restricted-properties
-    expect(page.context() as BrowserContext).toBeDefined();
-
-    // eslint-disable-next-line no-restricted-properties
-    expect(page.context().browser() as Browser).toBeDefined();
-});
--- types/catchpoint-playwright/index.d.ts
+++ types/catchpoint-playwright/index.d.ts
@@ -1,6 +1,6 @@
 declare namespace Catchpoint {
-    function username(_username?: string): Promise<string|null>;
-    function password(_password?: string): Promise<string|null>;
+    function username(username?: string): Promise<string|null>;
+    function password(password?: string): Promise<string|null>;
     function startStep(stepName: string, opt_disableAutoMatchRequests?: Array<boolean>): Promise<void>;
     function storeGlobalVariable(value: string, name: string): Promise<void>;
     function setTracepoint(insightToken: string, insightValue: string): Promise<void>;
