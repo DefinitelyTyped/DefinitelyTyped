4c423646232fb259c2d929756dab1da7635ff82c
Jabran Rafique <2131246+jabranr@users.noreply.github.com>
2025-08-15 08:49:27 +0100

    chore: fix format

--- types/catchpoint-playwright/catchpoint-playwright-test.ts
+++ types/catchpoint-playwright/catchpoint-playwright-test.ts
@@ -1,20 +1,20 @@
 // $ExpectType Promise<string | null>
-Catchpoint.username('testUsername')
+Catchpoint.username("testUsername");
 
 // $ExpectType Promise<string | null>
-Catchpoint.password('testPassword');
+Catchpoint.password("testPassword");
 
 // $ExpectType Promise<void>
-Catchpoint.startStep('Test Step');
+Catchpoint.startStep("Test Step");
 
 // $ExpectType Promise<void>
-Catchpoint.startStep('Test Step', [false]);
+Catchpoint.startStep("Test Step", [false]);
 
 // $ExpectType Promise<void>
-Catchpoint.storeGlobalVariable('testValue', 'testName');
+Catchpoint.storeGlobalVariable("testValue", "testName");
 
 // $ExpectType Promise<void>
-Catchpoint.setTracepoint('insightToken', 'insightValue');
+Catchpoint.setTracepoint("insightToken", "insightValue");
 
 // $ExpectType Promise<void>
-Catchpoint.setIndicator('insightToken', 'insightValue');
+Catchpoint.setIndicator("insightToken", "insightValue");
