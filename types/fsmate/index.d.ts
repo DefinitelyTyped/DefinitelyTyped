8f48b4c42364b9360eb6980e808b8f4e5248d6a0
shahzada967 <shahzadamodassir@gmail.com>
2025-08-17 00:28:24 +0530

    Update index.d.ts

--- types/fsmate/index.d.ts
+++ types/fsmate/index.d.ts
@@ -1,28 +1,14 @@
-// CommonJS ke bajay TypeScript / ES Module syntax use karenge
-import * as fs from "fs";
-import * as path from "path";
+// index.d.ts
 
-interface FileInfo {
-  name: string;
-  path: string;
-  size: number;
+declare module "fsmate" {
+  export interface FileInfo {
+    name: string;
+    path: string;
+    size: number;
+  }
+
+  /**
+   * Get list of files from a directory
+   */
+  export function getFiles(dirPath: string): FileInfo[];
 }
-
-/**
- * Get list of files from a directory
- */
-function getFiles(dirPath: string): FileInfo[] {
-  const files: string[] = fs.readdirSync(dirPath);
-  return files.map((file) => {
-    const filePath = path.join(dirPath, file);
-    const stats = fs.statSync(filePath);
-    return {
-      name: file,
-      path: filePath,
-      size: stats.size,
-    };
-  });
-}
-
-const result: FileInfo[] = getFiles("./");
-console.log(result);
