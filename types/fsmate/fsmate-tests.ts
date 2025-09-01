a97d664448b3ca1d5cd079a1d48ee38ddd2f98e0
shahzada967 <shahzadamodassir@gmail.com>
2025-09-01 13:25:42 +0530

    Update fsmate-tests.ts

--- types/fsmate/fsmate-tests.ts
+++ types/fsmate/fsmate-tests.ts
@@ -1,51 +1,35 @@
-function pseudos() {
-    const $test = jQuery(document);
-    Sizzle.selectors.pseudos["fixed"] = (elem) => {
-        // $test[0] = elem as HTMLElement;
-        return $test.css("position") === "fixed";
-    };
+import fsMate = require("./fsmate");
+
+// Simple test file
+const testFile = "demo.txt";
+
+async function runTests() {
+  // Create file
+  await fsMate.mkfile(testFile, true);
+
+  // Write data
+  await fsMate.writeFile(testFile, "Hello Afsara!");
+  console.log("File written.");
+
+  // Read data
+  const content = await fsMate.readFile(testFile);
+  console.log("Read content:", content);
+
+  // Append
+  await fsMate.appendFile(testFile, "\nThis is appended.");
+  console.log("Appended.");
+
+  // Read lines
+  const lines = await fsMate.readLine(testFile, 0, true);
+  console.log("Lines:", lines);
+
+  // File size
+  const size = await fsMate.filesize(testFile);
+  console.log("File size:", size);
+
+  // Remove
+  await fsMate.remove(testFile);
+  console.log("File removed.");
 }
 
-function createPseudos_0() {
-    Sizzle.selectors.pseudos["not"] = Sizzle.selectors.createPseudo((subSelector) => {
-        const matcher = Sizzle.compile(subSelector);
-        return (elem) => {
-            return !matcher(elem);
-        };
-    });
-}
-
-function createPseudos_1() {
-    // An implementation of a case-insensitive contains pseudo
-    // made for all versions of jQuery
-    (($) => {
-        function icontains(elem: HTMLElement, text: string) {
-            return (
-                elem.textContent
-                || elem.innerText
-                || $(elem).text()
-                || ""
-            ).toLowerCase().indexOf((text || "").toLowerCase()) > -1;
-        }
-
-        // $.expr.pseudos.icontains = $.expr.createPseudo(function(text) {
-        //     return function(elem) {
-        //         return icontains(elem as HTMLElement, text);
-        //     };
-        // });
-    })(jQuery);
-}
-
-function setFilters_0() {
-    Sizzle.selectors.setFilters["first"] = (elements, argument, not) => {
-        // No argument for first
-        return not ? elements.slice(1) : [elements[0]];
-    };
-}
-
-function setFilters_1(oldPOS: RegExp) {
-    Sizzle.selectors.match.POS = new RegExp(oldPOS.source.replace("first", "uno"), "gi");
-    Sizzle.selectors.setFilters.uno = Sizzle.selectors.setFilters.first;
-    delete Sizzle.selectors.setFilters.first;
-    Sizzle("div:uno"); // ==> [ <div> ]
-}
+runTests().catch(console.error);
