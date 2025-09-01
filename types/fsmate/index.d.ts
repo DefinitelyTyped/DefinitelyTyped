ac6863820b51a06b2811230ec8fa0b26e4d597ca
shahzada967 <shahzadamodassir@gmail.com>
2025-09-01 13:25:01 +0530

    Update index.d.ts

--- types/fsmate/index.d.ts
+++ types/fsmate/index.d.ts
@@ -1,91 +1,111 @@
-export as namespace Sizzle;
+/// <reference types="node" />
 
-declare const Sizzle: SizzleStatic;
-export = Sizzle;
+import { Readable } from "stream";
 
-interface SizzleStatic {
-    selectors: Sizzle.Selectors;
-    <TArrayLike extends ArrayLike<Element>>(
-        selector: string,
-        context: Element | Document | DocumentFragment,
-        results: TArrayLike,
-    ): TArrayLike;
-    (selector: string, context?: Element | Document | DocumentFragment): Element[];
-    // eslint-disable-next-line @typescript-eslint/no-unsafe-function-type
-    compile(selector: string): Function;
-    matchesSelector(element: Element, selector: string): boolean;
-    matches(selector: string, elements: Element[]): Element[];
+declare namespace fsMate {
+  function tmpName(prefix: string): string;
+  function tempNam(prefix: string, suffix: string): string;
+
+  function isFile(path: string): Promise<boolean>;
+  function isFileSync(path: string): boolean;
+
+  function isDir(path: string): Promise<boolean>;
+  function isDirSync(path: string): boolean;
+
+  function isLink(path: string): Promise<boolean>;
+  function isLinkSync(path: string): boolean;
+
+  function isReadable(path: string): Promise<boolean>;
+  function isReadableSync(path: string): boolean;
+
+  function isWritable(path: string): Promise<boolean>;
+  function isWritableSync(path: string): boolean;
+
+  function isExecutable(path: string): Promise<boolean>;
+  function isExecutableSync(path: string): boolean;
+
+  function filesize(filename: string): Promise<number>;
+  function filesizeSync(filename: string): number;
+
+  function mkdir(paths: string | string[], mode?: number): Promise<boolean>;
+  function mkdirSync(paths: string | string[], mode?: number): void;
+
+  function mkfile(paths: string | string[], overwrite?: boolean): Promise<void>;
+  function mkfileSync(paths: string | string[], overwrite?: boolean): void;
+
+  function touch(files: string | string[], mtime?: number, atime?: number): Promise<void>;
+  function touchSync(files: string | string[], mtime?: number, atime?: number): void;
+
+  function rename(oldPath: string, newPath: string, overwrite?: boolean): Promise<void>;
+  function renameSync(oldPath: string, newPath: string, overwrite?: boolean): boolean;
+
+  function move(oldPath: string, newPath: string, overwrite?: boolean): Promise<void>;
+  function moveSync(oldPath: string, newPath: string, overwrite?: boolean): boolean;
+
+  function scandir(dir: string, options?: { withDeepScan?: boolean; withFileTypes?: boolean; withFullPath?: boolean }): Promise<any[]>;
+  function scandirSync(dir: string, options?: { withDeepScan?: boolean; withFileTypes?: boolean; withFullPath?: boolean }): any[];
+
+  function remove(files: string | string[], recursive?: boolean): Promise<void>;
+  function removeSync(files: string | string[], recursive?: boolean): void;
+
+  function rm(files: string | string[], options?: { recursive?: boolean; force?: boolean }): Promise<void>;
+  function rmSync(files: string | string[], options?: { recursive?: boolean; force?: boolean }): void;
+
+  function mirror(originDir: string, targetDir: string, overwrite?: boolean): Promise<void>;
+  function mirrorSync(originDir: string, targetDir: string, overwrite?: boolean): void;
+
+  function copy(originFile: string, targetFile: string, overwrite?: boolean): Promise<void>;
+  function copySync(originFile: string, targetFile: string, overwrite?: boolean): void;
+
+  function truncate(path: string): Promise<void>;
+  function truncateSync(path: string): void;
+
+  function empty(paths: string | string[]): Promise<void>;
+  function emptySync(paths: string | string[]): void;
+
+  function createInputStream(data: any): Readable;
+  function stringify(data: any): string;
+
+  function readFile(filePath: string, options?: object | boolean, parsed?: boolean): Promise<string | any>;
+  function readFileSync(filePath: string, parsed?: boolean): string | any;
+
+  function readLine(filePath: string, start?: number | boolean, end?: number | boolean): Promise<string[]>;
+  function readLineSync(filePath: string, start?: number | boolean, end?: number | boolean): string[];
+
+  function writeFile(filePath: string, data: any, options?: object): Promise<void>;
+  function writeFileSync(filePath: string, data: any): void;
+
+  function appendFile(filePath: string, data: any, options?: object): Promise<void>;
+  function appendFileSync(filePath: string, data: any): void;
+
+  function prependFile(filePath: string, data: any, options?: object): Promise<void>;
+  function prependFileSync(filePath: string, data: any): void;
+
+  function dumpFile(filePath: string, content: any): Promise<void>;
+  function dumpFileSync(filePath: string, content: any): void;
+
+  function multiStream(streams: Iterable<Readable>): Readable;
+
+  function fseek(filePath: string, position: number): Promise<void>;
+  function fseekSync(filePath: string, position: number): void;
+
+  function ftell(filePath: string): Promise<number>;
+  function ftellSync(filePath: string): number;
+
+  function fgets(filePath: string): Promise<string>;
+  function fgetsSync(filePath: string): string;
+
+  function fpassthru(filePath: string): Promise<string>;
+  function fpassthruSync(filePath: string): string;
+
+  function rewind(filePath: string): Promise<void>;
+  function rewindSync(filePath: string): void;
+
+  function fwrite(filePath: string, content: string): Promise<void>;
+  function fwriteSync(filePath: string, content: string): void;
+
+  function fread(filePath: string, length: number): Promise<string>;
+  function freadSync(filePath: string, length: number): string;
 }
 
-declare namespace Sizzle {
-    interface Selectors {
-        cacheLength: number;
-        match: Selectors.Matches;
-        find: Selectors.FindFunctions;
-        preFilter: Selectors.PreFilterFunctions;
-        filter: Selectors.FilterFunctions;
-        attrHandle: Selectors.AttrHandleFunctions;
-        pseudos: Selectors.PseudoFunctions;
-        setFilters: Selectors.SetFilterFunctions;
-        createPseudo(fn: Selectors.CreatePseudoFunction): Selectors.PseudoFunction;
-    }
-
-    namespace Selectors {
-        interface Matches {
-            [name: string]: RegExp;
-        }
-
-        interface FindFunction {
-            // eslint-disable-next-line @typescript-eslint/no-invalid-void-type
-            (match: RegExpMatchArray, context: Element | Document, isXML: boolean): Element[] | void;
-        }
-
-        interface FindFunctions {
-            [name: string]: FindFunction;
-        }
-
-        interface PreFilterFunction {
-            (match: RegExpMatchArray): string[];
-        }
-
-        interface PreFilterFunctions {
-            [name: string]: PreFilterFunction;
-        }
-
-        interface FilterFunction {
-            (element: string, ...matches: string[]): boolean;
-        }
-
-        interface FilterFunctions {
-            [name: string]: FilterFunction;
-        }
-
-        interface AttrHandleFunction {
-            (elem: any, casePreservedName: string, isXML: boolean): string;
-        }
-
-        interface AttrHandleFunctions {
-            [name: string]: AttrHandleFunction;
-        }
-
-        interface PseudoFunction {
-            (elem: Element): boolean;
-        }
-
-        interface PseudoFunctions {
-            [name: string]: PseudoFunction;
-        }
-
-        interface SetFilterFunction {
-            (elements: Element[], argument: number, not: boolean): Element[];
-        }
-
-        interface SetFilterFunctions {
-            [name: string]: SetFilterFunction;
-        }
-
-        interface CreatePseudoFunction {
-            (...args: any[]): PseudoFunction;
-        }
-    }
-}
+export = fsMate;
