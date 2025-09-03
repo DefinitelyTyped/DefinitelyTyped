b71d12aff817cf26de6ef1610108d79c31323cc6
abdoo9 <abdulrahman60b@gmail.com>
2025-09-03 12:26:14 +0300

    (node-telegram-bot-api): Add missing types getMyName, setMyName, getMyDescription, setMyDescription, getMyShortDescription, and setMyShortDescription.

--- types/node-telegram-bot-api/index.d.ts
+++ types/node-telegram-bot-api/index.d.ts
@@ -1500,6 +1500,19 @@ declare namespace TelegramBot {
         | BotCommandScopeChat
         | BotCommandScopeChatAdministrators
         | BotCommandScopeChatMember;
+
+    interface BotName {
+        name: string;
+    }
+
+    interface BotDescription {
+        description: string;
+    }
+
+    interface BotShortDescription {
+        short_description: string;
+    }
+
     interface WebAppInfo {
         url: string;
     }
@@ -2152,11 +2165,30 @@ declare class TelegramBot extends TelegramBotEventEmitter<TelegramBot.TelegramEv
 
     setMyCommands(
         commands: TelegramBot.BotCommand[],
-        options?: {
+        form?: {
             language_code?: string;
             scope?: TelegramBot.BotCommandScope;
         },
     ): Promise<boolean>;
+
+    deleteMyCommands(
+        form?: {
+            language_code?: string;
+            scope?: TelegramBot.BotCommandScope;
+        },
+    ): Promise<boolean>;
+
+    setMyName( form?: {name?: string, language_code?: string }): Promise<boolean>;
+
+    getMyName( form?: {language_code?: string} ): Promise<TelegramBot.BotName>;
+
+    setMyDescription( form?: {description?: string, language_code?: string} ): Promise<boolean>;
+
+    getMyDescription( form?: {language_code?: string} ): Promise<TelegramBot.BotDescription>;
+
+    setMyShortDescription( form?: {short_description?: string, language_code?: string} ): Promise<boolean>;
+
+    getMyShortDescription( form?: {language_code?: string} ): Promise<TelegramBot.BotShortDescription>;
 
     setChatMenuButton(form: { chat_id?: number; menu_button?: TelegramBot.MenuButton }): Promise<boolean>;
 
