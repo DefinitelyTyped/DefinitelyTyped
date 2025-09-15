f535bf07faffe68dc1c0550db5f6ed7404bafa5e
Jonas Geiler <git@jonasgeiler.com>
2025-09-15 20:51:37 +0200

    fix(html-minifier-next): correct exports from ESM module

--- types/html-minifier-next/index.d.ts
+++ types/html-minifier-next/index.d.ts
@@ -1,2 +1,6 @@
-export * from "./index.d.cjs";
-export { default } from "./index.d.cjs";
+import { minify, type MinifierOptions } from './index.d.cjs';
+
+export { minify, MinifierOptions };
+
+declare const _default: { minify: typeof minify };
+export default _default;
