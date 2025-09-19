32a286ea16ea5517ebc9a607cd7209ebeb657dc9
Dan <danstuff@users.noreply.github.com>
2025-09-19 17:19:48 -0400

    Update types/matter-js/index.d.ts
    Co-authored-by: Steven Snoeijen <2750503+stevensnoeijen@users.noreply.github.com>

--- types/matter-js/index.d.ts
+++ types/matter-js/index.d.ts
@@ -2866,7 +2866,9 @@ declare namespace Matter {
         frameDeltaSmoothing: boolean;
 
         /**
-         * A flag that enables snapping of framerate to the nearest integer unit.
+         * Rounds measured browser frame delta to the nearest 1 Hz.
+         * This option can help smooth frame rate measurements and simplify handling hardware timing differences e.g. 59.94Hz and 60Hz displays.
+         * For best results you should also round your `runner.delta` equivalent to the nearest 1 Hz.
          *
          * @default true
          */
