45c81bf62551c7e9264529134870983c695bb1c6
Erwan Jugand <erwan.jugand@gmail.com>
2025-09-19 19:23:14 +0200

    [chrome] update offscreen namespace

--- types/chrome/index.d.ts
+++ types/chrome/index.d.ts
@@ -7375,23 +7375,23 @@ declare namespace chrome {
         export enum Reason {
             /** A reason used for testing purposes only. */
             TESTING = "TESTING",
-            /** The offscreen document is responsible for playing audio. */
+            /** Specifies that the offscreen document is responsible for playing audio. */
             AUDIO_PLAYBACK = "AUDIO_PLAYBACK",
-            /** The offscreen document needs to embed and script an iframe in order to modify the iframe's content. */
+            /** Specifies that the offscreen document needs to embed and script an iframe in order to modify the iframe's content. */
             IFRAME_SCRIPTING = "IFRAME_SCRIPTING",
-            /** The offscreen document needs to embed an iframe and scrape its DOM to extract information. */
+            /** Specifies that the offscreen document needs to embed an iframe and scrape its DOM to extract information. */
             DOM_SCRAPING = "DOM_SCRAPING",
-            /** The offscreen document needs to interact with Blob objects (including URL.createObjectURL()). */
+            /** Specifies that the offscreen document needs to interact with Blob objects (including `URL.createObjectURL()`). */
             BLOBS = "BLOBS",
-            /** The offscreen document needs to use the DOMParser API. */
+            /** Specifies that the offscreen document needs to use the DOMParser API. */
             DOM_PARSER = "DOM_PARSER",
-            /** The offscreen document needs to interact with media streams from user media (e.g. getUserMedia()). */
+            /** Specifies that the offscreen document needs to interact with media streams from user media (e.g. `getUserMedia()`). */
             USER_MEDIA = "USER_MEDIA",
-            /** The offscreen document needs to interact with media streams from display media (e.g. getDisplayMedia()). */
+            /** Specifies that the offscreen document needs to interact with media streams from display media (e.g. `getDisplayMedia()`). */
             DISPLAY_MEDIA = "DISPLAY_MEDIA",
-            /** The offscreen document needs to use WebRTC APIs. */
+            /** Specifies that the offscreen document needs to use WebRTC APIs. */
             WEB_RTC = "WEB_RTC",
-            /** The offscreen document needs to interact with the clipboard APIs(e.g. Navigator.clipboard). */
+            /** Specifies that the offscreen document needs to interact with the Clipboard API. */
             CLIPBOARD = "CLIPBOARD",
             /** Specifies that the offscreen document needs access to localStorage. */
             LOCAL_STORAGE = "LOCAL_STORAGE",
@@ -7405,7 +7405,6 @@ declare namespace chrome {
             GEOLOCATION = "GEOLOCATION",
         }
 
-        /** The parameters describing the offscreen document to create. */
         export interface CreateParameters {
             /** The reason(s) the extension is creating the offscreen document. */
             reasons: `${Reason}`[];
@@ -7418,36 +7417,26 @@ declare namespace chrome {
         /**
          * Creates a new offscreen document for the extension.
          * @param parameters The parameters describing the offscreen document to create.
-         * @return The `createDocument` method provides its result via callback or returned as a `Promise` (MV3 only).
+         *
+         * Can return its result via Promise in Manifest V3.
          */
         export function createDocument(parameters: CreateParameters): Promise<void>;
-        /**
-         * Creates a new offscreen document for the extension.
-         * @param parameters The parameters describing the offscreen document to create.
-         * @param callback Invoked when the offscreen document is created and has completed its initial page load.
-         */
         export function createDocument(parameters: CreateParameters, callback: () => void): void;
 
         /**
          * Closes the currently-open offscreen document for the extension.
-         * @return The `closeDocument` method provides its result via callback or returned as a `Promise` (MV3 only).
+         *
+         * Can return its result via Promise in Manifest V3.
          */
         export function closeDocument(): Promise<void>;
-        /**
-         * Closes the currently-open offscreen document for the extension.
-         * @param callback Invoked when the offscreen document has been closed.
-         */
         export function closeDocument(callback: () => void): void;
 
         /**
          * Determines whether the extension has an active document.
-         * @return The `hasDocument` method provides its result via callback or returned as a `Promise` (MV3 only).
+         *
+         * Can return its result via Promise in Manifest V3.
          */
         export function hasDocument(): Promise<boolean>;
-        /**
-         * Determines whether the extension has an active document.
-         * @param callback Invoked with the result of whether the extension has an active offscreen document.
-         */
         export function hasDocument(callback: (result: boolean) => void): void;
     }
 
