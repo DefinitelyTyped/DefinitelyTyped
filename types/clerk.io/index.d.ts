ff8b6849fef1b421069cfceaf8da65b18a232629
Christian V. Dahl <31840711+cvd2412@users.noreply.github.com>
2025-09-22 13:06:29 +0200

    Convert type files to .d.ts and update exports
    Renamed type definition files from .ts to .d.ts for config, helpers, and response modules. Updated index.d.ts to re-export all types for easier access. Adjusted tsconfig.json to reference the new .d.ts files. Also changed internal type declarations to exported types and interfaces for better external usage.

--- types/clerk.io/index.d.ts
+++ types/clerk.io/index.d.ts
@@ -1,7 +1,13 @@
 import * as Config from "./types/config";
 import * as Response from "./types/response";
+import * as Helpers from "./types/helpers";
 
-export {};
+// Re-export all types from submodules for direct access
+export * from "./types/config";
+export * from "./types/response";
+export * from "./types/helpers";
+
+export { };
 
 export interface InitConfig {
     key: string;
