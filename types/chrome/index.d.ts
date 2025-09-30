e899bf3a15a9f588371dce97eac5f5f10821a142
Erwan Jugand <erwan.jugand@gmail.com>
2025-09-30 21:10:05 +0200

    [chrome] update sessions namespace

--- types/chrome/index.d.ts
+++ types/chrome/index.d.ts
@@ -9738,25 +9738,16 @@ declare namespace chrome {
      */
     export namespace sessions {
         export interface Filter {
-            /**
-             * Optional.
-             * The maximum number of entries to be fetched in the requested list. Omit this parameter to fetch the maximum number of entries (sessions.MAX_SESSION_RESULTS).
-             */
+            /** The maximum number of entries to be fetched in the requested list. Omit this parameter to fetch the maximum number of entries ({@link sessions.MAX_SESSION_RESULTS}). */
             maxResults?: number | undefined;
         }
 
         export interface Session {
             /** The time when the window or tab was closed or modified, represented in seconds since the epoch. */
             lastModified: number;
-            /**
-             * Optional.
-             * The tabs.Tab, if this entry describes a tab. Either this or sessions.Session.window will be set.
-             */
+            /** The {@link tabs.Tab}, if this entry describes a tab. Either this or {@link sessions.Session.window} will be set. */
             tab?: tabs.Tab | undefined;
-            /**
-             * Optional.
-             * The windows.Window, if this entry describes a window. Either this or sessions.Session.tab will be set.
-             */
+            /** The {@link windows.Window}, if this entry describes a window. Either this or {@link sessions.Session.tab} will be set. */
             window?: windows.Window | undefined;
         }
 
@@ -9767,69 +9758,39 @@ declare namespace chrome {
             sessions: Session[];
         }
 
-        export interface SessionChangedEvent extends chrome.events.Event<() => void> {}
-
-        /** The maximum number of sessions.Session that will be included in a requested list. */
-        export var MAX_SESSION_RESULTS: number;
+        /** The maximum number of {@link sessions.Session} that will be included in a requested list. */
+        export const MAX_SESSION_RESULTS: 25;
 
         /**
          * Gets the list of recently closed tabs and/or windows.
-         * @return The `getRecentlyClosed` method provides its result via callback or returned as a `Promise` (MV3 only).
+         *
+         * Can return its result via Promise in Manifest V3 or later since Chrome 96.
          */
         export function getRecentlyClosed(filter?: Filter): Promise<Session[]>;
-        /**
-         * Gets the list of recently closed tabs and/or windows.
-         * @param callback
-         * Parameter sessions: The list of closed entries in reverse order that they were closed (the most recently closed tab or window will be at index 0). The entries may contain either tabs or windows.
-         */
-        export function getRecentlyClosed(filter: Filter, callback: (sessions: Session[]) => void): void;
-        /**
-         * Gets the list of recently closed tabs and/or windows.
-         * @param callback
-         * Parameter sessions: The list of closed entries in reverse order that they were closed (the most recently closed tab or window will be at index 0). The entries may contain either tabs or windows.
-         */
         export function getRecentlyClosed(callback: (sessions: Session[]) => void): void;
+        export function getRecentlyClosed(filter: Filter | undefined, callback: (sessions: Session[]) => void): void;
+
         /**
          * Retrieves all devices with synced sessions.
-         * @return The `getDevices` method provides its result via callback or returned as a `Promise` (MV3 only).
+         *
+         * Can return its result via Promise in Manifest V3 or later since Chrome 96.
          */
         export function getDevices(filter?: Filter): Promise<Device[]>;
-        /**
-         * Retrieves all devices with synced sessions.
-         * @param callback
-         * Parameter devices: The list of sessions.Device objects for each synced session, sorted in order from device with most recently modified session to device with least recently modified session. tabs.Tab objects are sorted by recency in the windows.Window of the sessions.Session objects.
-         */
-        export function getDevices(filter: Filter, callback: (devices: Device[]) => void): void;
-        /**
-         * Retrieves all devices with synced sessions.
-         * @param callback
-         * Parameter devices: The list of sessions.Device objects for each synced session, sorted in order from device with most recently modified session to device with least recently modified session. tabs.Tab objects are sorted by recency in the windows.Window of the sessions.Session objects.
-         */
         export function getDevices(callback: (devices: Device[]) => void): void;
+        export function getDevices(filter: Filter | undefined, callback: (devices: Device[]) => void): void;
+
         /**
-         * Reopens a windows.Window or tabs.Tab.
-         * @param sessionId Optional.
-         * The windows.Window.sessionId, or tabs.Tab.sessionId to restore. If this parameter is not specified, the most recently closed session is restored.
-         * @return The `restore` method provides its result via callback or returned as a `Promise` (MV3 only).
+         * Reopens a {@link windows.Window} or {@link tabs.Tab}, with an optional callback to run when the entry has been restored.
+         *
+         * Can return its result via Promise in Manifest V3 or later since Chrome 96.
+         * @param sessionId The {@link windows.Window.sessionId}, or {@link tabs.Tab.sessionId} to restore. If this parameter is not specified, the most recently closed session is restored.
          */
         export function restore(sessionId?: string): Promise<Session>;
-        /**
-         * Reopens a windows.Window or tabs.Tab, with an optional callback to run when the entry has been restored.
-         * @param sessionId Optional.
-         * The windows.Window.sessionId, or tabs.Tab.sessionId to restore. If this parameter is not specified, the most recently closed session is restored.
-         * @param callback Optional.
-         * Parameter restoredSession: A sessions.Session containing the restored windows.Window or tabs.Tab object.
-         */
-        export function restore(sessionId: string, callback: (restoredSession: Session) => void): void;
-        /**
-         * Reopens a windows.Window or tabs.Tab, with an optional callback to run when the entry has been restored.
-         * @param callback Optional.
-         * Parameter restoredSession: A sessions.Session containing the restored windows.Window or tabs.Tab object.
-         */
         export function restore(callback: (restoredSession: Session) => void): void;
+        export function restore(sessionId: string | undefined, callback: (restoredSession: Session) => void): void;
 
         /** Fired when recently closed tabs and/or windows are changed. This event does not monitor synced sessions changes. */
-        export var onChanged: SessionChangedEvent;
+        export const onChanged: events.Event<() => void>;
     }
 
     ////////////////////
