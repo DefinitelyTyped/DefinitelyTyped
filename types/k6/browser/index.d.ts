8d210537adc661b26a862c8594287e917e41a505
Eng Zer Jun <engzerjun@gmail.com>
2025-10-01 21:49:39 +0800

    k6: create Device type definition
    Closes https://github.com/grafana/k6/issues/4363.
    
    Signed-off-by: Eng Zer Jun <engzerjun@gmail.com>

--- types/k6/browser/index.d.ts
+++ types/k6/browser/index.d.ts
@@ -415,6 +415,139 @@ export interface ElementStateFilter {
 }
 
 /**
+ * The `devices` named export defines emulation settings for many end-user
+ * devices that can be used to simulate browser behavior on a mobile device.
+ *
+ * @example
+ * ```js
+ * import { browser, devices } from 'k6/browser';
+ * ... // redacted
+ *   const iphoneX = devices['iPhone X'];
+ *   const context = await browser.newContext(iphoneX);
+ * ... // redacted
+ * ```
+ */
+export const devices: Record<
+    | "Blackberry PlayBook"
+    | "Blackberry PlayBook landscape"
+    | "BlackBerry Z30"
+    | "BlackBerry Z30 landscape"
+    | "Galaxy Note 3"
+    | "Galaxy Note 3 landscape"
+    | "Galaxy Note II"
+    | "Galaxy Note II landscape"
+    | "Galaxy S III"
+    | "Galaxy S III landscape"
+    | "Galaxy S5"
+    | "Galaxy S5 landscape"
+    | "iPad"
+    | "iPad landscape"
+    | "iPad Mini"
+    | "iPad Mini landscape"
+    | "iPad Pro"
+    | "iPad Pro landscape"
+    | "iPhone 4"
+    | "iPhone 4 landscape"
+    | "iPhone 5"
+    | "iPhone 5 landscape"
+    | "iPhone 6"
+    | "iPhone 6 landscape"
+    | "iPhone 6 Plus"
+    | "iPhone 6 Plus landscape"
+    | "iPhone 7"
+    | "iPhone 7 landscape"
+    | "iPhone 7 Plus"
+    | "iPhone 7 Plus landscape"
+    | "iPhone 8"
+    | "iPhone 8 landscape"
+    | "iPhone 8 Plus"
+    | "iPhone 8 Plus landscape"
+    | "iPhone SE"
+    | "iPhone SE landscape"
+    | "iPhone X"
+    | "iPhone X landscape"
+    | "iPhone XR"
+    | "iPhone XR landscape"
+    | "JioPhone 2"
+    | "JioPhone 2 landscape"
+    | "Kindle Fire HDX"
+    | "Kindle Fire HDX landscape"
+    | "LG Optimus L70"
+    | "LG Optimus L70 landscape"
+    | "Microsoft Lumia 550"
+    | "Microsoft Lumia 950"
+    | "Microsoft Lumia 950 landscape"
+    | "Nexus 10"
+    | "Nexus 10 landscape"
+    | "Nexus 4"
+    | "Nexus 4 landscape"
+    | "Nexus 5"
+    | "Nexus 5 landscape"
+    | "Nexus 5X"
+    | "Nexus 5X landscape"
+    | "Nexus 6"
+    | "Nexus 6 landscape"
+    | "Nexus 6P"
+    | "Nexus 6P landscape"
+    | "Nexus 7"
+    | "Nexus 7 landscape"
+    | "Nokia Lumia 520"
+    | "Nokia Lumia 520 landscape"
+    | "Nokia N9"
+    | "Nokia N9 landscape"
+    | "Pixel 2"
+    | "Pixel 2 landscape"
+    | "Pixel 2 XL"
+    | "Pixel 2 XL landscape",
+    Device
+>;
+
+/**
+ * Device represents an end-user device (computer, tablet, phone etc.).
+ */
+export interface Device {
+    /**
+     * Name of the device.
+     */
+    name: string;
+
+    /**
+     * User agent of the device.
+     */
+    userAgent: string;
+
+    /**
+     * Viewport size of the device.
+     */
+    viewport: {
+        /**
+         * page width in pixels.
+         */
+        width: number;
+
+        /**
+         * page height in pixels.
+         */
+        height: number;
+    };
+
+    /**
+     * Device viewport scale factor.
+     */
+    deviceScaleFactor: number;
+
+    /**
+     * Indicates whether the device is a mobile device.
+     */
+    isMobile: boolean;
+
+    /**
+     * Indicates whether the device support touch events.
+     */
+    hasTouch: boolean;
+}
+
+/**
  * BrowserPermissions defines all the possible permissions that can be granted
  * to the browser application.
  */
