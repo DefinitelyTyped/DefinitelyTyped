836c14abff8568cf5747f3421119d05361470077
Jake Bailey <5341706+jakebailey@users.noreply.github.com>
2025-10-01 15:17:51 -0700

    combo

--- types/node/url.d.ts
+++ types/node/url.d.ts
@@ -1021,7 +1021,8 @@ declare module "url" {
         interface URLPatternResult extends _URLPatternResult {}
         interface URLPattern extends _URLPattern {}
         var URLPattern: typeof globalThis extends {
-            ViewTimeline: any; // Must be a var introduced at the same time as URLPattern.
+            onmessage: any;
+            scheduler: any; // Must be a var introduced at the same time as URLPattern.
             URLPattern: infer T;
         } ? T
             : typeof _URLPattern;
