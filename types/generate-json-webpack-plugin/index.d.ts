e99943af6b75da984ddd221ead02a39f152a586b
Anonymous-Humanoid <68610963+Anonymous-Humanoid@users.noreply.github.com>
2025-10-05 06:25:46 -0400

    Added support for Webpack 5
    Signed-off-by: Anonymous-Humanoid <68610963+Anonymous-Humanoid@users.noreply.github.com>

--- types/generate-json-webpack-plugin/index.d.ts
+++ types/generate-json-webpack-plugin/index.d.ts
@@ -1,12 +1,14 @@
-import { Plugin } from "webpack";
+import { Compiler, WebpackPluginInstance } from "webpack";
 
 export = GenerateJsonWebpackPlugin;
 
-declare class GenerateJsonWebpackPlugin extends Plugin {
+declare class GenerateJsonWebpackPlugin implements WebpackPluginInstance {
     constructor(
         fileName: string,
         value: object,
         replacer?: ((key: string, value: any) => any) | null,
         space?: string | number | null,
     );
+
+    apply: (compiler: Compiler) => void;
 }
