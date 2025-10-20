dc7045a42709ad7926456f1601582c805325c01e
Renegade334 <contact.9a5d6388@renegade334.me.uk>
2025-10-20 22:48:25 +0100

    http: add server.keepAliveTimeoutBuffer option

--- types/node/v22/test/https.ts
+++ types/node/v22/test/https.ts
@@ -102,6 +102,7 @@ import * as url from "node:url";
         const timeout: number = server.timeout;
         const listening: boolean = server.listening;
         const keepAliveTimeout: number = server.keepAliveTimeout;
+        const keepAliveTimeoutBuffer: number = server.keepAliveTimeoutBuffer;
         const maxHeadersCount: number | null = server.maxHeadersCount;
         const maxRequestsPerSocket: number | null = server.maxRequestsPerSocket;
         const headersTimeout: number = server.headersTimeout;
