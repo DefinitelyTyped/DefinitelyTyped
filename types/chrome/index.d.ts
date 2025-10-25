7e58716e738277967ec0bf32c2b5e0a0185224f1
Erwan Jugand <erwan.jugand@gmail.com>
2025-10-25 13:16:36 +0200

    [chrome] update since Chrome 141

--- types/chrome/index.d.ts
+++ types/chrome/index.d.ts
@@ -1710,6 +1710,12 @@ declare namespace chrome {
             INCOGNITO_SESSION_ONLY = "incognito_session_only",
         }
 
+        /** @since Chrome 141 */
+        export enum SoundContentSetting {
+            ALLOW = "allow",
+            BLOCK = "block",
+        }
+
         /**
          * Whether to allow sites to download multiple files automatically. One of
          *
@@ -6901,16 +6907,26 @@ declare namespace chrome {
      */
     export namespace loginState {
         export enum ProfileType {
+            /** Specifies that the extension is in the signin profile. */
             SIGNIN_PROFILE = "SIGNIN_PROFILE",
+            /** Specifies that the extension is in the user profile. */
             USER_PROFILE = "USER_PROFILE",
+            /** Specifies that the extension is in the lock screen profile. */
+            LOCK_PROFILE = "LOCK_PROFILE",
         }
 
         export enum SessionState {
+            /** Specifies that the session state is unknown. */
             UNKNOWN = "UNKNOWN",
+            /** Specifies that the user is in the out-of-box-experience screen. */
             IN_OOBE_SCREEN = "IN_OOBE_SCREEN",
+            /** Specifies that the user is in the login screen. */
             IN_LOGIN_SCREEN = "IN_LOGIN_SCREEN",
+            /** Specifies that the user is in the session. */
             IN_SESSION = "IN_SESSION",
+            /** Specifies that the user is in the lock screen. */
             IN_LOCK_SCREEN = "IN_LOCK_SCREEN",
+            /** Specifies that the device is in RMA mode, finalizing repairs. */
             IN_RMA_SCREEN = "IN_RMA_SCREEN",
         }
 
@@ -13687,7 +13703,7 @@ declare namespace chrome {
          * @since Chrome 86
          */
         export enum HeaderOperation {
-            /** Adds a new entry for the specified header. This operation is not supported for request headers. */
+            /** Adds a new entry for the specified header. When modifying the headers of a request, this operation is only supported for specific headers. */
             APPEND = "append",
             /** Sets a new value for the specified header, removing any existing headers with the same name. */
             SET = "set",
@@ -14317,6 +14333,21 @@ declare namespace chrome {
      * @since Chrome 114, MV3
      */
     export namespace sidePanel {
+        /** @since Chrome 141 */
+        export type CloseOptions =
+            | {
+                /** The tab in which to close the side panel. If a tab-specific side panel is open in the specified tab, it will be closed for that tab. At least one of this or `windowId` must be provided.  */
+                tabId: number;
+                /** The window in which to close the side panel. If a global side panel is open in the specified window, it will be closed for all tabs in that window where no tab-specific panel is active. At least one of this or `tabId` must be provided. */
+                windowId?: number | undefined;
+            }
+            | {
+                /** The tab in which to close the side panel. If a tab-specific side panel is open in the specified tab, it will be closed for that tab. At least one of this or `windowId` must be provided.  */
+                tabId?: number | undefined;
+                /** The window in which to close the side panel. If a global side panel is open in the specified window, it will be closed for all tabs in that window where no tab-specific panel is active. At least one of this or `tabId` must be provided. */
+                windowId: number;
+            };
+
         export interface GetPanelOptions {
             /**
              * If specified, the side panel options for the given tab will be returned.
@@ -14357,6 +14388,16 @@ declare namespace chrome {
         /** @since Chrome 140 */
         export interface PanelLayout {
             side: `${Side}`;
+        }
+
+        /** @since Chrome 141 */
+        export interface PanelOpenedInfo {
+            /** The path of the local resource within the extension package whose content is displayed in the panel. */
+            path: string;
+            /** The optional ID of the tab where the side panel is opened. This is provided only when the panel is tab-specific. */
+            tabId?: number;
+            /** The ID of the window where the side panel is opened. This is available for both global and tab-specific panels. */
+            windowId: number;
         }
 
         export interface PanelOptions {
@@ -14437,6 +14478,12 @@ declare namespace chrome {
          */
         export function setPanelBehavior(behavior: PanelBehavior): Promise<void>;
         export function setPanelBehavior(behavior: PanelBehavior, callback: () => void): void;
+
+        /**
+         * Fired when the extension's side panel is opened.
+         * @since Chrome 141
+         */
+        const onOpened: events.Event<(info: PanelOpenedInfo) => void>;
     }
 
     ////////////////////
