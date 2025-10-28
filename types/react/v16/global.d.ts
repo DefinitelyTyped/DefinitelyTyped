8357b47196bb7eb64f4762af3ec5e887f1ebf971
Adam Naji <110662505+Bashamega@users.noreply.github.com>
2025-10-28 04:23:22 +0000

    -

--- types/react/v16/global.d.ts
+++ types/react/v16/global.d.ts
@@ -25,6 +25,7 @@ interface WheelEvent extends Event {}
 interface EventTarget {}
 interface Document {}
 interface DataTransfer {}
+interface StyleMedia {}
 
 interface Element {}
 interface DocumentFragment {}
