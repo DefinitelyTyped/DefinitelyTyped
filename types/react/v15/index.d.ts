66c39374872736dd66faac007e9e3144570b9774
Adam Naji <110662505+Bashamega@users.noreply.github.com>
2025-11-01 17:43:37 +0200

    Add StyleMedia interface to AbstractView

--- types/react/v15/index.d.ts
+++ types/react/v15/index.d.ts
@@ -3723,7 +3723,7 @@ declare namespace React {
     // Browser Interfaces
     // https://github.com/nikeee/2048-typescript/blob/master/2048/js/touch.d.ts
     // ----------------------------------------------------------------------
-
+    interface StyleMedia {}
     interface AbstractView {
         styleMedia: StyleMedia;
         document: Document;
