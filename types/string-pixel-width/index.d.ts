bc41f047b6d6a976a67fcae734463bdc40aae513
Jimmy Leung <43258070+hkleungai@users.noreply.github.com>
2025-11-02 13:27:47 +0800

    feat(string-pixel-width): add "Inter" font, expose `Settings` type

--- types/string-pixel-width/index.d.ts
+++ types/string-pixel-width/index.d.ts
@@ -1,27 +1,30 @@
 export = getWidth;
 
-declare function getWidth(string: string, settings?: Settings): number;
+declare function getWidth(string: string, settings?: getWidth.Settings): number;
 
-interface Settings {
-    bold?: boolean | undefined;
-    font?:
-        | "andale mono"
-        | "arial"
-        | "avenir"
-        | "avenir next"
-        | "comic sans ms"
-        | "courier new"
-        | "georgia"
-        | "helvetica"
-        | "impact"
-        | "open sans"
-        | "quantify"
-        | "tahoma"
-        | "times new roman"
-        | "trebuchet ms"
-        | "verdana"
-        | "webdings"
-        | undefined;
-    italic?: boolean | undefined;
-    size?: number | undefined;
+declare namespace getWidth {
+    interface Settings {
+        bold?: boolean | undefined;
+        font?:
+            | "andale mono"
+            | "arial"
+            | "avenir"
+            | "avenir next"
+            | "comic sans ms"
+            | "courier new"
+            | "georgia"
+            | "helvetica"
+            | "impact"
+            | "inter"
+            | "times new roman"
+            | "trebuchet ms"
+            | "verdana"
+            | "webdings"
+            | "open sans"
+            | "tahoma"
+            | "quantify"
+            | undefined;
+        italic?: boolean | undefined;
+        size?: number | undefined;
+    }
 }
