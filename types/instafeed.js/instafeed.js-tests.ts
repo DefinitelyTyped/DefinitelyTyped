c483d12ddb1400931eccc38e0df2e48dc98c0d15
João Gabriel Silva Fernandes <joao.sfernandes@quintoandar.com.br>
2025-11-04 19:40:08 -0300

    fix: format test file

--- types/instafeed.js/instafeed.js-tests.ts
+++ types/instafeed.js/instafeed.js-tests.ts
@@ -14,27 +14,27 @@ new Instafeed({});
 new Instafeed();
 
 // test if transform return type is passed to other hooks
-new Instafeed({ 
-  accessToken: "aa",
-  transform(item) { 
-    return { ...item, extra: 'foo' };
-  },
-  filter(item) {
-    // $ExpectType string
-    item.extra;
-    return true;
-  }
+new Instafeed({
+    accessToken: "aa",
+    transform(item) {
+        return { ...item, extra: "foo" };
+    },
+    filter(item) {
+        // $ExpectType string
+        item.extra;
+        return true;
+    },
 });
 
 interface CustomInstafeedItem extends InstafeedDefaultItem {
-  extra: string;
+    extra: string;
 }
 
 // test if omiting expected extra field breaks compiler as expected
-new Instafeed<CustomInstafeedItem>({ 
-  accessToken: "aa",
-  // @ts-expect-error
-  transform(item) { 
-    return item;
-  }
+new Instafeed<CustomInstafeedItem>({
+    accessToken: "aa",
+    // @ts-expect-error
+    transform(item) {
+        return item;
+    },
 });
