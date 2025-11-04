4cc4331a75e2faf4a6681184cc1b6f6e18a77886
João Gabriel <jgabsfernandes@gmail.com>
2025-11-04 15:51:13 -0300

    Add type expectations for Instafeed instantiation

--- types/instafeed.js/instafeed.js-tests.ts
+++ types/instafeed.js/instafeed.js-tests.ts
@@ -11,3 +11,10 @@ new Instafeed({});
 
 // @ts-expect-error
 new Instafeed();
+
+// $ExpectType Instafeed<InstafeedDefaultItem>
+new Instafeed({ 
+  accessToken: "aa",  
+  // $ExpectType ((item: T) => T
+  transform: (item) => item
+});
