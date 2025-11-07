4ba2b5fcde53412cdb7319caa55816eb1aa596f2
Joshua Lipstone <joshualip@plaudit.com>
2025-11-07 15:36:36 -0600

    Ran the formatter

--- types/wordpress__block-editor/components/link-control.d.ts
+++ types/wordpress__block-editor/components/link-control.d.ts
@@ -1,52 +1,52 @@
 import { ComponentType, ReactNode } from "react";
 
 interface WPLinkControlDefaultValue {
-	url?: string,
-	title?: string,
-	opensInNewTab?: boolean
+    url?: string;
+    title?: string;
+    opensInNewTab?: boolean;
 }
 
 interface WPLinkControlSettingsValue {
-	[setting: string]: any
+    [setting: string]: any;
 }
 
 declare namespace LinkControl {
-	interface Props {
-		settings?: Setting[];
-		forceIsEditingLink?: boolean;
-		value?: Value;
-		onChange?: (nextValue?: Value) => void;
-		onRemove?: () => void;
-		onCancel?: () => void;
+    interface Props {
+        settings?: Setting[];
+        forceIsEditingLink?: boolean;
+        value?: Value;
+        onChange?: (nextValue?: Value) => void;
+        onRemove?: () => void;
+        onCancel?: () => void;
 
-		noDirectEntry?: boolean;
-		showSuggestions?: boolean;
-		showInitialSuggestions?: boolean;
-		withCreateSuggestion?: boolean;
-		suggestionsQuery?: any;
-		noURLSuggestion?: boolean;
-		hasTextControl?: boolean;
-		createSuggestion?: ((title: string) => Suggestion)|undefined;
-		createSuggestionButtonText?: string|((searchTerm: string|undefined) => ReactNode);
-		renderControlBottom?: (() => ReactNode);
-		handleEntities?: boolean;
-		hasRichPreviews?: boolean;
-	}
+        noDirectEntry?: boolean;
+        showSuggestions?: boolean;
+        showInitialSuggestions?: boolean;
+        withCreateSuggestion?: boolean;
+        suggestionsQuery?: any;
+        noURLSuggestion?: boolean;
+        hasTextControl?: boolean;
+        createSuggestion?: ((title: string) => Suggestion) | undefined;
+        createSuggestionButtonText?: string | ((searchTerm: string | undefined) => ReactNode);
+        renderControlBottom?: () => ReactNode;
+        handleEntities?: boolean;
+        hasRichPreviews?: boolean;
+    }
 
-	interface Setting {
-		id: string,
-		title: string
-	}
+    interface Setting {
+        id: string;
+        title: string;
+    }
 
-	interface Suggestion {
-		id: string;
-		type: string;
-		title: string;
-		url: string;
-	}
+    interface Suggestion {
+        id: string;
+        type: string;
+        title: string;
+        url: string;
+    }
 
-	interface Value extends WPLinkControlDefaultValue, WPLinkControlSettingsValue {}
+    interface Value extends WPLinkControlDefaultValue, WPLinkControlSettingsValue {}
 }
-declare const LinkControl: ComponentType<LinkControl.Props>&{DEFAULT_LINK_SETTINGS: LinkControl.Setting[]};
+declare const LinkControl: ComponentType<LinkControl.Props> & { DEFAULT_LINK_SETTINGS: LinkControl.Setting[] };
 
 export default LinkControl;
