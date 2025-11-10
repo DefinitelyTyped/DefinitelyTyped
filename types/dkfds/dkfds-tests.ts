2f47b1a477f56aa41b5cd6c0bf8c0985fe631384
Christian P. Cederquist <christian.cederquist@gmail.com>
2025-11-10 21:16:36 +0100

    [dkfds] Used dts-gen to refactor types

--- types/dkfds/dkfds-tests.ts
+++ types/dkfds/dkfds-tests.ts
@@ -1,35 +1,12 @@
-import {
-    Accordion,
-    Alert,
-    BackToTop,
-    CharacterLimit,
-    CheckboxToggleContent,
-    datePicker,
-    DatePickerContext,
-    Dropdown,
-    ErrorSummary,
-    init,
-    MenuDropdown,
-    Modal,
-    Navigation,
-    RadioToggleGroup,
-    ResponsiveTable,
-    TableSelectableRows,
-    Tabs,
-    Toast,
-    Tooltip,
-} from "dkfds";
-
 // Test init function
-init({ scope: document.body }); // $ExpectType void
-init({ scope: document.getElementById("app")! }); // $ExpectType void
+DKFDS.init({ scope: document.body }); // $ExpectType void
+DKFDS.init({ scope: document.getElementById("app")! }); // $ExpectType void
 
 // Test Accordion
 const accordionElement = document.createElement("ul");
 const accordionStrings = { open_all: "Åbn alle", close_all: "Luk alle" };
 
-const accordion1 = Accordion(accordionElement, accordionStrings); // $ExpectType AccordionInstance
-const accordion2 = new Accordion(accordionElement, accordionStrings); // $ExpectType AccordionInstance
+const accordion1 = new DKFDS.Accordion(accordionElement, accordionStrings); // $ExpectType Accordion
 
 accordion1.init(); // $ExpectType void
 accordion1.bulkEvent(); // $ExpectType void
@@ -44,12 +21,12 @@ accordion1.toggleButton(button, false, true); // $ExpectType void
 // Test DatePicker
 const datePickerEl = document.createElement("div");
 
-datePicker.init(datePickerEl); // $ExpectType void
-datePicker.add(datePickerEl); // $ExpectType void
-datePicker.on(datePickerEl); // $ExpectType void
-datePicker.off(datePickerEl); // $ExpectType void
+DKFDS.datePicker.init(datePickerEl); // $ExpectType void
+DKFDS.datePicker.add(datePickerEl); // $ExpectType void
+DKFDS.datePicker.on(datePickerEl); // $ExpectType void
+DKFDS.datePicker.off(datePickerEl); // $ExpectType void
 
-datePicker.setLanguage({
+DKFDS.datePicker.setLanguage({
     open_calendar: "Åbn kalender",
     choose_a_date: "Vælg en dato",
     choose_a_date_between: "Vælg en dato mellem",
@@ -91,20 +68,20 @@ datePicker.setLanguage({
     sunday: "Søndag",
 });
 
-datePicker.disable(datePickerEl); // $ExpectType void
-datePicker.enable(datePickerEl); // $ExpectType void
+DKFDS.datePicker.disable(datePickerEl); // $ExpectType void
+DKFDS.datePicker.enable(datePickerEl); // $ExpectType void
 
-datePicker.isDateInputInvalid(datePickerEl); // $ExpectType boolean
+DKFDS.datePicker.isDateInputInvalid(datePickerEl); // $ExpectType boolean
 
-datePicker.setCalendarValue(datePickerEl, "2023-12-31"); // $ExpectType void
-datePicker.validateDateInput(datePickerEl); // $ExpectType void
+DKFDS.datePicker.setCalendarValue(datePickerEl, "2023-12-31"); // $ExpectType void
+DKFDS.datePicker.validateDateInput(datePickerEl); // $ExpectType void
 
-datePicker.renderCalendar(datePickerEl); // $ExpectType HTMLElement
-datePicker.renderCalendar(datePickerEl, new Date()); // $ExpectType HTMLElement
+DKFDS.datePicker.renderCalendar(datePickerEl); // $ExpectType HTMLElement
+DKFDS.datePicker.renderCalendar(datePickerEl, new Date()); // $ExpectType HTMLElement
 
-datePicker.updateCalendarIfVisible(datePickerEl); // $ExpectType void
+DKFDS.datePicker.updateCalendarIfVisible(datePickerEl); // $ExpectType void
 
-const context = datePicker.getDatePickerContext(datePickerEl); // $ExpectType DatePickerContext | undefined
+const context = DKFDS.datePicker.getDatePickerContext(datePickerEl); // $ExpectType DatePickerContext | undefined
 if (context) {
     context.calendarEl; // $ExpectType HTMLDivElement
     context.datePickerEl; // $ExpectType HTMLElement
@@ -125,8 +102,7 @@ if (context) {
 // Test Tooltip
 const tooltipWrapper = document.createElement("div");
 
-const tooltip1 = Tooltip(tooltipWrapper); // $ExpectType TooltipInstance
-const tooltip2 = new Tooltip(tooltipWrapper); // $ExpectType TooltipInstance
+const tooltip1 = new DKFDS.Tooltip(tooltipWrapper); // $ExpectType Tooltip
 
 tooltip1.init(); // $ExpectType void
 tooltip1.hideTooltip(); // $ExpectType void
@@ -138,8 +114,7 @@ tooltip1.updateTooltipPosition(); // $ExpectType void
 // Test Alert
 const alertElement = document.createElement("div");
 
-const alert1 = Alert(alertElement); // $ExpectType AlertInstance
-const alert2 = new Alert(alertElement); // $ExpectType AlertInstance
+const alert1 = new DKFDS.Alert(alertElement); // $ExpectType Alert
 
 alert1.init(); // $ExpectType void
 alert1.hide(); // $ExpectType void
@@ -148,8 +123,7 @@ alert1.show(); // $ExpectType void
 // Test BackToTop
 const backToTopElement = document.createElement("div");
 
-const backToTop1 = BackToTop(backToTopElement); // $ExpectType BackToTopInstance
-const backToTop2 = new BackToTop(backToTopElement); // $ExpectType BackToTopInstance
+const backToTop1 = new DKFDS.BackToTop(backToTopElement); // $ExpectType BackToTop
 
 backToTop1.init(); // $ExpectType void
 
@@ -162,16 +136,14 @@ const charLimitStrings = {
     characters_too_many: "tegn for meget",
 };
 
-const charLimit1 = CharacterLimit(charLimitElement, charLimitStrings); // $ExpectType CharacterLimitInstance
-const charLimit2 = new CharacterLimit(charLimitElement, charLimitStrings); // $ExpectType CharacterLimitInstance
+const charLimit1 = new DKFDS.CharacterLimit(charLimitElement, charLimitStrings); // $ExpectType CharacterLimit
 
 charLimit1.init(); // $ExpectType void
 
 // Test CheckboxToggleContent
 const checkboxInput = document.createElement("input");
 
-const checkboxToggle1 = CheckboxToggleContent(checkboxInput); // $ExpectType CheckboxToggleContentInstance
-const checkboxToggle2 = new CheckboxToggleContent(checkboxInput); // $ExpectType CheckboxToggleContentInstance
+const checkboxToggle1 = new DKFDS.CheckboxToggleContent(checkboxInput); // $ExpectType CheckboxToggleContent
 
 checkboxToggle1.init(); // $ExpectType void
 checkboxToggle1.toggle(); // $ExpectType void
@@ -183,8 +155,7 @@ checkboxToggle1.collapse(checkboxInput, contentElement); // $ExpectType void
 // Test Dropdown
 const dropdownButton = document.createElement("button");
 
-const dropdown1 = Dropdown(dropdownButton); // $ExpectType DropdownInstance
-const dropdown2 = new Dropdown(dropdownButton); // $ExpectType DropdownInstance
+const dropdown1 = new DKFDS.Dropdown(dropdownButton); // $ExpectType Dropdown
 
 dropdown1.init(); // $ExpectType void
 dropdown1.hide(); // $ExpectType void
@@ -193,8 +164,7 @@ dropdown1.show(); // $ExpectType void
 // Test ErrorSummary
 const errorSummaryElement = document.createElement("div");
 
-const errorSummary1 = ErrorSummary(errorSummaryElement); // $ExpectType ErrorSummaryInstance
-const errorSummary2 = new ErrorSummary(errorSummaryElement); // $ExpectType ErrorSummaryInstance
+const errorSummary1 = new DKFDS.ErrorSummary(errorSummaryElement); // $ExpectType ErrorSummary
 
 errorSummary1.init(); // $ExpectType void
 
@@ -212,8 +182,7 @@ errorSummary1.getAssociatedLegendOrLabel(inputElement); // $ExpectType HTMLEleme
 // Test Modal
 const modalElement = document.createElement("div");
 
-const modal1 = Modal(modalElement); // $ExpectType ModalInstance
-const modal2 = new Modal(modalElement); // $ExpectType ModalInstance
+const modal1 = new DKFDS.Modal(modalElement); // $ExpectType Modal
 
 modal1.init(); // $ExpectType void
 modal1.hide(); // $ExpectType void
@@ -222,23 +191,21 @@ modal1.show(); // $ExpectType void
 // Test MenuDropdown
 const menuButton = document.createElement("button");
 
-const menuDropdown1 = MenuDropdown(menuButton); // $ExpectType MenuDropdownInstance
-const menuDropdown2 = new MenuDropdown(menuButton); // $ExpectType MenuDropdownInstance
+const menuDropdown1 = new DKFDS.MenuDropdown(menuButton); // $ExpectType MenuDropdown
 
 menuDropdown1.init(); // $ExpectType void
 menuDropdown1.hide(); // $ExpectType void
 menuDropdown1.show(); // $ExpectType void
 
 // Test Navigation
-const navigation = new Navigation(); // $ExpectType Navigation
+const navigation = new DKFDS.Navigation(); // $ExpectType Navigation
 navigation.init(); // $ExpectType void
 navigation.teardown(); // $ExpectType void
 
 // Test RadioToggleGroup
 const radioContainer = document.createElement("div");
 
-const radioToggle1 = RadioToggleGroup(radioContainer); // $ExpectType RadioToggleGroupInstance
-const radioToggle2 = new RadioToggleGroup(radioContainer); // $ExpectType RadioToggleGroupInstance
+const radioToggle1 = new DKFDS.RadioToggleGroup(radioContainer); // $ExpectType RadioToggleGroup
 
 radioToggle1.init(); // $ExpectType void
 
@@ -250,8 +217,7 @@ radioToggle1.collapse(radioInput, contentElement); // $ExpectType void
 // Test TableSelectableRows
 const tableElement = document.createElement("table");
 
-const tableSelectable1 = TableSelectableRows(tableElement); // $ExpectType TableSelectableRowsInstance
-const tableSelectable2 = new TableSelectableRows(tableElement); // $ExpectType TableSelectableRowsInstance
+const tableSelectable1 = new DKFDS.TableSelectableRows(tableElement); // $ExpectType TableSelectableRows
 
 tableSelectable1.init(); // $ExpectType void
 
@@ -259,13 +225,12 @@ tableSelectable1.getGroupCheckbox(); // $ExpectType false | HTMLElement
 tableSelectable1.getCheckboxList(); // $ExpectType HTMLCollection
 
 // Test ResponsiveTable
-const responsiveTable = new ResponsiveTable(tableElement); // $ExpectType ResponsiveTable
+const responsiveTable = new DKFDS.ResponsiveTable(tableElement); // $ExpectType ResponsiveTable
 
 // Test Tabs
 const tabsElement = document.createElement("div");
 
-const tabs1 = Tabs(tabsElement); // $ExpectType TabsInstance
-const tabs2 = new Tabs(tabsElement); // $ExpectType TabsInstance
+const tabs1 = new DKFDS.Tabs(tabsElement); // $ExpectType Tabs
 
 tabs1.init(); // $ExpectType void
 
@@ -276,8 +241,7 @@ tabs1.activateTab(tabButton, false); // $ExpectType void
 // Test Toast
 const toastElement = document.createElement("div");
 
-const toast1 = Toast(toastElement); // $ExpectType ToastInstance
-const toast2 = new Toast(toastElement); // $ExpectType ToastInstance
+const toast1 = new DKFDS.Toast(toastElement); // $ExpectType Toast
 
 toast1.show(); // $ExpectType void
 toast1.hide(); // $ExpectType void
--- types/dkfds/index.d.ts
+++ types/dkfds/index.d.ts
@@ -1,282 +1,265 @@
-export function init(options: { scope: HTMLElement }): void;
+declare namespace DKFDS {
+    class Accordion {
+        constructor($accordion: HTMLElement, strings: { open_all: string; close_all: string });
 
-export interface AccordionInstance {
-    init(): void;
-    bulkEvent(): void;
-    eventOnClick($button: HTMLButtonElement, e: PointerEvent): void;
-    toggleButton(button: HTMLButtonElement, expanded?: boolean, bulk?: boolean): void;
+        bulkEvent(): void;
+
+        eventOnClick($button: HTMLButtonElement, e: PointerEvent): void;
+
+        init(): void;
+
+        toggleButton(button: HTMLButtonElement, expanded?: boolean, bulk?: boolean): void;
+    }
+
+    class Alert {
+        constructor(element: HTMLElement);
+
+        hide(): void;
+
+        init(): void;
+
+        show(): void;
+    }
+
+    class BackToTop {
+        constructor(element: HTMLElement);
+
+        init(): void;
+    }
+
+    class CharacterLimit {
+        constructor(
+            element: HTMLElement,
+            strings: {
+                character_remaining: string;
+                characters_remaining: string;
+                character_too_many: string;
+                characters_too_many: string;
+            },
+        );
+
+        charactersLeft(): number;
+
+        silentUpdateMessages(): void;
+
+        updateMessages(): void;
+
+        init(): void;
+    }
+
+    class CheckboxToggleContent {
+        constructor(element: HTMLInputElement);
+
+        collapse(triggerEl: HTMLInputElement, targetEl: HTMLElement): void;
+
+        expand(checkboxElement: HTMLInputElement, contentElement: HTMLElement): void;
+
+        init(): void;
+
+        toggle(): void;
+    }
+
+    class Dropdown {
+        constructor(element: HTMLButtonElement);
+
+        hide(): void;
+
+        init(): void;
+
+        show(): void;
+    }
+
+    class DropdownSort {
+        constructor(container: HTMLElement);
+
+        init(): void;
+
+        onOptionClick(e: PointerEvent): void;
+
+        updateSelectedValue(): void;
+    }
+
+    class ErrorSummary {
+        constructor(element: HTMLElement);
+        focusTarget($target: HTMLElement): boolean;
+
+        getAssociatedLegendOrLabel($input: HTMLElement): HTMLElement | null;
+
+        getFragmentFromUrl(url: string): string | false;
+
+        handleClick(e: MouseEvent): void;
+
+        init(): void;
+    }
+
+    class MenuDropdown {
+        constructor(element: HTMLButtonElement);
+
+        hide(): void;
+
+        init(): void;
+
+        show(): void;
+    }
+
+    class Modal {
+        constructor(element: HTMLElement);
+
+        hide(): void;
+
+        init(): void;
+
+        show(): void;
+    }
+
+    class Navigation {
+        init(): void;
+
+        teardown(): void;
+    }
+
+    class RadioToggleGroup {
+        constructor(containerElement: HTMLElement);
+
+        collapse(triggerEl: HTMLInputElement, targetEl: HTMLElement): void;
+
+        expand(checkboxElement: HTMLInputElement, contentElement: HTMLElement): void;
+
+        init(): void;
+
+        toggle(radioInputElement: HTMLInputElement): void;
+    }
+
+    class TableSelectableRows {
+        constructor(tableElement: HTMLTableElement);
+
+        getCheckboxList(): HTMLCollection;
+
+        getGroupCheckbox(): HTMLElement | false;
+
+        init(): void;
+    }
+
+    class Tabs {
+        constructor(element: HTMLElement);
+
+        activateTab(tab: HTMLButtonElement, setFocus: boolean): void;
+
+        init(): void;
+    }
+
+    class Toast {
+        constructor(element: HTMLElement);
+
+        hide(): void;
+
+        show(): void;
+    }
+
+    class Tooltip {
+        constructor(wrapper: HTMLElement);
+
+        hideTooltip(): void;
+
+        init(): void;
+
+        isShowing(): boolean;
+
+        showTooltip(): void;
+
+        updateTooltipPosition(): void;
+    }
+
+    class ResponsiveTable {
+        constructor(table: HTMLTableElement);
+    }
+
+    function init(options?: { scope: HTMLElement }): void;
+
+    interface DatePickerContext {
+        calendarEl: HTMLDivElement;
+        datePickerEl: HTMLElement;
+        dialogEl: HTMLDivElement;
+        internalInputEl: HTMLInputElement;
+        externalInputEl: HTMLInputElement;
+        statusEl: HTMLDivElement;
+        guideEl: HTMLDivElement;
+        firstYearChunkEl: HTMLDivElement;
+        calendarDate: Date;
+        minDate: Date;
+        maxDate: Date;
+        selectedDate: Date;
+        rangeDate: Date;
+        defaultDate: Date;
+    }
+
+    namespace datePicker {
+        function add(root: HTMLElement): void;
+
+        function disable(element: HTMLElement): void;
+
+        function enable(element: HTMLElement): void;
+
+        function getDatePickerContext(element: HTMLElement): DatePickerContext | undefined;
+
+        function init(root: HTMLElement): void;
+
+        function isDateInputInvalid(element: HTMLElement): boolean;
+
+        function off(root: HTMLElement): void;
+
+        function on(root: HTMLElement): void;
+
+        function remove(root: HTMLElement): void;
+
+        function renderCalendar(el: HTMLElement, _dateToDisplay?: Date): HTMLElement;
+
+        function setCalendarValue(el: HTMLElement, dateString: string): void;
+
+        function setLanguage(language: {
+            open_calendar: string;
+            choose_a_date: string;
+            choose_a_date_between: string;
+            choose_a_date_before: string;
+            choose_a_date_after: string;
+            aria_label_date: string;
+            current_month_displayed: string;
+            first_possible_date: string;
+            last_possible_date: string;
+            previous_year: string;
+            previous_month: string;
+            next_month: string;
+            next_year: string;
+            select_month: string;
+            select_year: string;
+            previous_years: string;
+            next_years: string;
+            guide: string;
+            months_displayed: string;
+            years_displayed: string;
+            january: string;
+            february: string;
+            march: string;
+            april: string;
+            may: string;
+            june: string;
+            july: string;
+            august: string;
+            september: string;
+            october: string;
+            november: string;
+            december: string;
+            monday: string;
+            tuesday: string;
+            wednesday: string;
+            thursday: string;
+            friday: string;
+            saturday: string;
+            sunday: string;
+        }): void;
+
+        function updateCalendarIfVisible(el: HTMLElement): void;
+
+        function validateDateInput(el: HTMLElement): void;
+    }
 }
-
-export const Accordion: {
-    /**
-     * Adds click functionality to accordion list
-     * @param $accordion The accordion <ul> element
-     * @param strings Translated labels: {"open_all": "Åbn alle", "close_all": "Luk alle"}
-     */
-    ($accordion: HTMLElement, strings: { open_all: string; close_all: string }): AccordionInstance;
-
-    /**
-     * Adds click functionality to accordion list
-     * @param $accordion The accordion <ul> element
-     * @param strings Translated labels: {"open_all": "Åbn alle", "close_all": "Luk alle"}
-     */
-    new ($accordion: HTMLElement, strings: { open_all: string; close_all: string }): AccordionInstance;
-};
-
-/**
- * The properties and elements within the date picker.
- */
-export interface DatePickerContext {
-    calendarEl: HTMLDivElement;
-    datePickerEl: HTMLElement;
-    dialogEl: HTMLDivElement;
-    internalInputEl: HTMLInputElement;
-    externalInputEl: HTMLInputElement;
-    statusEl: HTMLDivElement;
-    guideEl: HTMLDivElement;
-    firstYearChunkEl: HTMLDivElement;
-    calendarDate: Date;
-    minDate: Date;
-    maxDate: Date;
-    selectedDate: Date;
-    rangeDate: Date;
-    defaultDate: Date;
-}
-
-export interface DatePicker {
-    init: (root: HTMLElement) => void;
-    add: DatePicker["init"];
-    on: DatePicker["init"];
-    off: (root: HTMLElement) => void;
-    setLanguage(language: {
-        open_calendar: string;
-        choose_a_date: string;
-        choose_a_date_between: string;
-        choose_a_date_before: string;
-        choose_a_date_after: string;
-        aria_label_date: string;
-        current_month_displayed: string;
-        first_possible_date: string;
-        last_possible_date: string;
-        previous_year: string;
-        previous_month: string;
-        next_month: string;
-        next_year: string;
-        select_month: string;
-        select_year: string;
-        previous_years: string;
-        next_years: string;
-        guide: string;
-        months_displayed: string;
-        years_displayed: string;
-        january: string;
-        february: string;
-        march: string;
-        april: string;
-        may: string;
-        june: string;
-        july: string;
-        august: string;
-        september: string;
-        october: string;
-        november: string;
-        december: string;
-        monday: string;
-        tuesday: string;
-        wednesday: string;
-        thursday: string;
-        friday: string;
-        saturday: string;
-        sunday: string;
-    }): void;
-    disable: (element: HTMLElement) => void;
-    enable: (element: HTMLElement) => void;
-    isDateInputInvalid: (element: HTMLElement) => boolean;
-    /**
-     * Select the value of the date picker inputs.
-     *
-     * @param {HTMLButtonElement} el An element within the date picker component
-     * @param {string} dateString The date string to update in YYYY-MM-DD format
-     */
-    setCalendarValue: (el: HTMLElement, dateString: string) => void;
-    validateDateInput: (el: HTMLElement) => void;
-    /**
-     * render the calendar.
-     *
-     * @param {HTMLElement} el An element within the date picker component
-     * @param {Date} _dateToDisplay a date to render on the calendar
-     * @returns {HTMLElement} a reference to the new calendar element
-     */
-    renderCalendar: (el: HTMLElement, _dateToDisplay?: Date) => HTMLElement;
-    updateCalendarIfVisible: (el: HTMLElement) => void;
-    getDatePickerContext(element: HTMLElement): DatePickerContext | undefined;
-}
-
-export const datePicker: DatePicker;
-
-export interface TooltipInstance {
-    init(): void;
-    hideTooltip(): void;
-    showTooltip(): void;
-    isShowing(): boolean;
-    updateTooltipPosition(): void;
-}
-
-export const Tooltip: {
-    (wrapper: HTMLElement): TooltipInstance;
-    new (wrapper: HTMLElement): TooltipInstance;
-};
-
-export interface AlertInstance {
-    init(): void;
-    hide(): void;
-    show(): void;
-}
-export const Alert: {
-    (element: HTMLElement): AlertInstance;
-    new (element: HTMLElement): AlertInstance;
-};
-
-export interface BackToTopInstance {
-    init(): void;
-}
-
-export const BackToTop: {
-    (element: HTMLElement): BackToTopInstance;
-    new (element: HTMLElement): BackToTopInstance;
-};
-
-export interface CharacterLimitInstance {
-    init(): void;
-}
-
-export const CharacterLimit: {
-    (
-        element: HTMLElement,
-        strings: {
-            character_remaining: string;
-            characters_remaining: string;
-            character_too_many: string;
-            characters_too_many: string;
-        }
-    ): CharacterLimitInstance;
-    new (
-        element: HTMLElement,
-        strings: {
-            character_remaining: string;
-            characters_remaining: string;
-            character_too_many: string;
-            characters_too_many: string;
-        }
-    ): CharacterLimitInstance;
-};
-
-export interface CheckboxToggleContentInstance {
-    init(): void;
-    toggle(): void;
-    expand(checkboxElement: HTMLInputElement, contentElement: HTMLElement): void;
-    collapse(triggerEl: HTMLInputElement, targetEl: HTMLElement): void;
-}
-
-export const CheckboxToggleContent: {
-    (element: HTMLInputElement): CheckboxToggleContentInstance;
-    new (element: HTMLInputElement): CheckboxToggleContentInstance;
-};
-
-export interface DropdownInstance {
-    init(): void;
-    hide(): void;
-    show(): void;
-}
-
-export const Dropdown: {
-    (element: HTMLButtonElement): DropdownInstance;
-    new (element: HTMLButtonElement): DropdownInstance;
-};
-
-export interface ErrorSummaryInstance {
-    init(): void;
-    handleClick: (e: MouseEvent) => void;
-    focusTarget: ($target: HTMLElement) => boolean;
-    getFragmentFromUrl: (url: string) => string | false;
-    getAssociatedLegendOrLabel: ($input: HTMLElement) => HTMLElement | null;
-}
-
-export const ErrorSummary: {
-    (element: HTMLElement): ErrorSummaryInstance;
-    new (element: HTMLElement): ErrorSummaryInstance;
-};
-
-export interface ModalInstance {
-    init(): void;
-    hide(): void;
-    show(): void;
-}
-
-export const Modal: {
-    (element: HTMLElement): ModalInstance;
-    new (element: HTMLElement): ModalInstance;
-};
-
-export interface MenuDropdownInstance {
-    init(): void;
-    hide(): void;
-    show(): void;
-}
-
-export const MenuDropdown: {
-    (element: HTMLButtonElement): MenuDropdownInstance;
-    new (element: HTMLButtonElement): MenuDropdownInstance;
-};
-
-export class Navigation {
-    init(): void;
-    teardown(): void;
-}
-
-export interface RadioToggleGroupInstance {
-    init(): void;
-    toggle(radioInputElement: HTMLInputElement): void;
-    expand(checkboxElement: HTMLInputElement, contentElement: HTMLElement): void;
-    collapse(triggerEl: HTMLInputElement, targetEl: HTMLElement): void;
-}
-
-export const RadioToggleGroup: {
-    (containerElement: HTMLElement): RadioToggleGroupInstance;
-    new (containerElement: HTMLElement): RadioToggleGroupInstance;
-};
-
-export interface TableSelectableRowsInstance {
-    init(): void;
-    getGroupCheckbox(): HTMLElement | false;
-    getCheckboxList(): HTMLCollection;
-}
-
-export const TableSelectableRows: {
-    (tableElement: HTMLTableElement): TableSelectableRowsInstance;
-    new (tableElement: HTMLTableElement): TableSelectableRowsInstance;
-};
-
-export class ResponsiveTable {
-    constructor(table: HTMLTableElement);
-}
-
-export interface TabsInstance {
-    init(): void;
-    activateTab(tab: HTMLButtonElement, setFocus: boolean): void;
-}
-
-export const Tabs: {
-    (element: HTMLElement): TabsInstance;
-    new (element: HTMLElement): TabsInstance;
-};
-
-export interface ToastInstance {
-    show(): void;
-    hide(): void;
-}
-
-export const Toast: {
-    (element: HTMLElement): ToastInstance;
-    new (element: HTMLElement): ToastInstance;
-};
