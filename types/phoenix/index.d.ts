5b22a862bdfd81f001577eb68663cbc8db9c045b
Benjamin Kay <10714808+benjaminkay93@users.noreply.github.com>
2025-11-19 16:02:52 +0000

    Update index.d.ts for types/pheonix

--- types/phoenix/index.d.ts
+++ types/phoenix/index.d.ts
@@ -24,7 +24,7 @@ export class Channel {
     onError(callback: (reason?: any) => void | Promise<void>): number;
     onMessage(event: string, payload: any, ref: any): any;
 
-    on(event: string, callback: (response?: any) => void | Promise<void>): number;
+    on<TResponse>(event: string, callback: (response?: TResponse) => void | Promise<void>): number;
     off(event: string, ref?: number): void;
 
     push(event: string, payload: object, timeout?: number): Push;
