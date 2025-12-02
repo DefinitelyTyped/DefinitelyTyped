8ecd7f3703edc2762262ccae6fe6ec9f6b9c0a2b
Adam Naji <110662505+Bashamega@users.noreply.github.com>
2025-12-02 15:40:20 +0000

    Add comments

--- types/kotobee.js/index.d.ts
+++ types/kotobee.js/index.d.ts
@@ -24,11 +24,30 @@ export type LanguageCode =
 
 export type Directions = "ltr" | "rtl";
 
-interface KotobeeReader {
+export interface KotobeeReader {
+    /**
+     * Clears cached local data used by the reader.
+     */
     clearCache: () => void;
+
+    /**
+     * Clears all event listeners attached by the reader.
+     */
     clearListeners: () => void;
+
+    /**
+     * Clears remotely fetched data (e.g., cloud sync, remote storage).
+     */
     clearRemoteData: () => void;
+
+    /**
+     * Sets the active UI language of the reader.
+     */
     setLanguage: (language: LanguageCode) => void;
+
+    /**
+     * Sets reading direction (left-to-right or right-to-left).
+     */
     setDirection: (direction: Directions) => void;
 }
 
