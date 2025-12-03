a326d336e416af59b88cd9da4c6c684674156f20
ysai258 <ysaimuppineni789@gmail.com>
2025-12-03 13:49:05 +0530

    feat(koa-qs): type the options argument

--- types/koa-qs/index.d.ts
+++ types/koa-qs/index.d.ts
@@ -1,9 +1,10 @@
 import koa = require("koa");
+import qs = require("qs");
 
 declare namespace koaQs {
     type ParseMode = "extended" | "strict" | "first";
 }
 
-declare function koaQs(app: koa, mode?: koaQs.ParseMode): koa;
+declare function koaQs(app: koa, mode?: koaQs.ParseMode, options?: qs.IParseOptions): koa;
 
 export = koaQs;
