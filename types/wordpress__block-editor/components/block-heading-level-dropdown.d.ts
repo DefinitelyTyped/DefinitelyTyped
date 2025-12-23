e0b431670787a366215eecd6cb0b3545df5a56d3
Joshua Lipstone <joshualip@plaudit.com>
2025-12-23 12:13:02 -0600

    Ran dprint fmt

--- types/wordpress__block-editor/components/block-heading-level-dropdown.d.ts
+++ types/wordpress__block-editor/components/block-heading-level-dropdown.d.ts
@@ -1,11 +1,11 @@
-import {JSX} from "react";
+import { JSX } from "react";
 
 declare namespace HeadingLevelDropdown {
-    type HeadingLevel = 1|2|3|4|5|6;
+    type HeadingLevel = 1 | 2 | 3 | 4 | 5 | 6;
     interface Props {
-        options?: HeadingLevel[]|(readonly HeadingLevel[])|undefined;
-        value?: HeadingLevel|undefined;
-        onChange?: (value?: HeadingLevel|undefined) => void;
+        options?: HeadingLevel[] | (readonly HeadingLevel[]) | undefined;
+        value?: HeadingLevel | undefined;
+        onChange?: (value?: HeadingLevel | undefined) => void;
     }
 }
 declare const HeadingLevelDropdown: {
