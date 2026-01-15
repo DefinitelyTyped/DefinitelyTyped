1f3575db3d0811033adc990d9c644a9d01611ee4
Renato Fernandes <renato@innovaro.com.br>
2026-01-15 17:52:22 -0300

    feat: update @nginstack/datasource definitions

--- types/nginstack__datasource/lib/DataSourceFieldDef.d.ts
+++ types/nginstack__datasource/lib/DataSourceFieldDef.d.ts
@@ -12,19 +12,13 @@ declare class DataSourceFieldDef {
         onlyVisible: boolean;
         onlyIncludedFieldNames: boolean;
     };
-    displayFormat:
-        | DateFormat
-        | LatitudeFormat
-        | LongitudeFormat
-        | AngleFormat
-        | string
-        | number
-        | null;
+    displayFormat: DateFormat | LatitudeFormat | LongitudeFormat | AngleFormat | NumberFormat;
 }
 declare namespace DataSourceFieldDef {
-    export { AngleFormat, DateFormat, LatitudeFormat, LongitudeFormat };
+    export { DateFormat, LatitudeFormat, LongitudeFormat, AngleFormat, NumberFormat };
 }
-type DateFormat = typeof import("@nginstack/engine/lib/date/DateFormat");
-type LatitudeFormat = typeof import("@nginstack/engine/lib/geo/LatitudeFormat");
-type LongitudeFormat = typeof import("@nginstack/engine/lib/geo/LongitudeFormat");
-type AngleFormat = typeof import("@nginstack/engine/lib/geo/AngleFormat");
+type DateFormat = typeof import('@nginstack/engine/lib/date/DateFormat');
+type LatitudeFormat = typeof import('@nginstack/engine/lib/geo/LatitudeFormat');
+type LongitudeFormat = typeof import('@nginstack/engine/lib/geo/LongitudeFormat');
+type AngleFormat = typeof import('@nginstack/engine/lib/geo/AngleFormat');
+type NumberFormat = typeof import('@nginstack/engine/lib/number/NumberFormat');
