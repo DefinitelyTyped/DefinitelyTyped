d76944d7ad485278429a2468f978a52e98a719db
Daniel Beeke <daniel.beeke@triply.cc>
2026-01-16 15:50:05 +0100

    types for grapoi

---
+++ types/grapoi/Path.d.ts
@@ -0,0 +1,59 @@
+import { DatasetCore, Quad, Term } from "@rdfjs/types";
+import { Environment } from "@rdfjs/environment/Environment";
+import Edge = require("./Edge");
+
+declare class Path {
+    constructor(options: {
+        dataset: DatasetCore;
+        edges?: Edge[];
+        factory: Environment<any>;
+        graph: Term;
+        term: Term;
+    });
+
+    dataset: DatasetCore;
+    edges: Edge[];
+    factory: Environment<any>;
+
+    get edge(): Edge | undefined;
+    get graph(): Term;
+    get length(): number;
+    get startTerm(): Term;
+    get term(): Term;
+    get value(): string;
+
+    addIn(predicates: Term[], subjects: Term[], callback?: (ptr: Path) => void): Path[];
+    addList(predicates: Term[], items: Term[]): Path[];
+    addOut(predicates: Term[], objects: Term[], callback?: (ptr: Path) => void): Path[];
+    deleteIn(predicates: Term[], subjects: Term[]): Path[];
+    deleteList(predicates: Term[]): Path[];
+    deleteOut(predicates: Term[], objects: Term[]): Path[];
+
+    execute(instruction: {
+        operation: string;
+        quantifier?: string;
+        start?: string;
+        end?: string;
+        subjects?: Term[];
+        predicates?: Term[];
+        objects?: Term[];
+        graphs?: Term[];
+        items?: Term[];
+        callback?: (edge: Edge, ptr: Path) => void;
+    }): Path[];
+
+    extend(edge: Edge): Path;
+    hasIn(predicates: Term[], subjects: Term[]): Path[];
+    hasOut(predicates: Term[], objects: Term[]): Path[];
+    in(predicates: Term[], subjects: Term[]): Path[];
+    isAny(): boolean;
+    isList(): boolean;
+
+    list(): IterableIterator<Path>;
+    nodes(): IterableIterator<{ dataset: DatasetCore; term: Term }>;
+    out(predicates: Term[], objects: Term[]): Path[];
+    quads(): IterableIterator<Quad>;
+    trim(): Path;
+}
+
+export = Path;
---
+++ types/grapoi/index.d.ts
@@ -0,0 +1,27 @@
+// Project: https://github.com/rdf-ext/grapoi
+// Definitions by: DefinitelyTyped <https://github.com/DefinitelyTyped>
+// Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
+
+import { DataFactory, DatasetCore, Term } from "@rdfjs/types";
+
+export = grapoi;
+export as namespace grapoi;
+
+declare function grapoi(args: grapoi.GrapoiOptions): grapoi.Grapoi;
+
+declare namespace grapoi {
+    export interface GrapoiOptions {
+        dataset: DatasetCore;
+        factory?: DataFactory;
+        term?: Term | Term[] | string;
+        terms?: Term[];
+        graphs?: Term[];
+    }
+
+    export { Grapoi, PathList, Path, Edge };
+}
+
+import Grapoi = require("./Grapoi");
+import PathList = require("./PathList");
+import Path = require("./Path");
+import Edge = require("./Edge");
