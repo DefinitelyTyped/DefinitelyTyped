be22761ee12b0621bbaac2a3f03ff72fc6039a67
Rami Mohamed <rami.workstation@gmail.com>
2026-01-19 14:28:40 +0400

    Format grenache-nodejs typings

--- types/grenache-nodejs-ws/grenache-nodejs-ws-tests.ts
+++ types/grenache-nodejs-ws/grenache-nodejs-ws-tests.ts
@@ -2,7 +2,7 @@ import Link = require("grenache-nodejs-link");
 import { PeerRPCClient, PeerRPCServer } from "grenache-nodejs-ws";
 
 const link = new Link({
-  grape: "http://127.0.0.1:30001"
+    grape: "http://127.0.0.1:30001",
 });
 
 link.start();
@@ -11,21 +11,21 @@ const server = new PeerRPCServer(link);
 server.init();
 
 server.listen("test_service", (payload, cb) => {
-  payload;
-  cb(null, { ok: true });
+    payload;
+    cb(null, { ok: true });
 });
 
 const client = new PeerRPCClient(link);
 client.init();
 
 client.request(
-  "test_service",
-  { ping: true },
-  {},
-  (err, data) => {
-    if (err) throw err;
-    data;
-  }
+    "test_service",
+    { ping: true },
+    {},
+    (err, data) => {
+        if (err) throw err;
+        data;
+    },
 );
 
 link.stop();
--- types/grenache-nodejs-ws/index.d.ts
+++ types/grenache-nodejs-ws/index.d.ts
@@ -4,47 +4,47 @@ import { EventEmitter } from "events";
 import Link = require("grenache-nodejs-link");
 
 declare namespace GrenacheNodejsWS {
-  interface RPCOptions {
-    timeout?: number;
-    retry?: number;
-  }
+    interface RPCOptions {
+        timeout?: number;
+        retry?: number;
+    }
 
-  type Callback<T = unknown> = (err: Error | null, data?: T) => void;
+    type Callback<T = unknown> = (err: Error | null, data?: T) => void;
 
-  type RequestHandler<TRequest = unknown, TResponse = unknown> = (
-    payload: TRequest,
-    callback: Callback<TResponse>
-  ) => void;
+    type RequestHandler<TRequest = unknown, TResponse = unknown> = (
+        payload: TRequest,
+        callback: Callback<TResponse>,
+    ) => void;
 }
 
 declare class PeerRPCClient extends EventEmitter {
-  constructor(
-    link: Link,
-    options?: GrenacheNodejsWS.RPCOptions
-  );
+    constructor(
+        link: Link,
+        options?: GrenacheNodejsWS.RPCOptions,
+    );
 
-  init(): void;
+    init(): void;
 
-  request<TResponse = unknown>(
-    service: string,
-    payload: unknown,
-    options: GrenacheNodejsWS.RPCOptions,
-    callback: GrenacheNodejsWS.Callback<TResponse>
-  ): void;
+    request<TResponse = unknown>(
+        service: string,
+        payload: unknown,
+        options: GrenacheNodejsWS.RPCOptions,
+        callback: GrenacheNodejsWS.Callback<TResponse>,
+    ): void;
 }
 
 declare class PeerRPCServer extends EventEmitter {
-  constructor(
-    link: Link,
-    options?: GrenacheNodejsWS.RPCOptions
-  );
+    constructor(
+        link: Link,
+        options?: GrenacheNodejsWS.RPCOptions,
+    );
 
-  init(): void;
+    init(): void;
 
-  listen<TRequest = unknown, TResponse = unknown>(
-    service: string,
-    handler: GrenacheNodejsWS.RequestHandler<TRequest, TResponse>
-  ): void;
+    listen<TRequest = unknown, TResponse = unknown>(
+        service: string,
+        handler: GrenacheNodejsWS.RequestHandler<TRequest, TResponse>,
+    ): void;
 }
 
 export { PeerRPCClient, PeerRPCServer };
