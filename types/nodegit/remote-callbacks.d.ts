f8764c21d4d449f6add3f1fd79b896ff88c55a3a
Dan Borthwick <dan@smartcasual.cc>
2026-01-29 06:02:59 +1100

    [nodegit] Add IndexerProgress types for RemoteCallbacks.transferProgress callback (#74260)

--- types/nodegit/remote-callbacks.d.ts
+++ types/nodegit/remote-callbacks.d.ts
@@ -2,7 +2,27 @@ export class RemoteCallbacks {
     version?: number | undefined;
     credentials?: Function | undefined;
     certificateCheck?: Function | undefined;
-    transferProgress?: Function | undefined;
+    /** Progress callback for push operations */
+    pushTransferProgress?: PushTransferProgressCallback;
+    /** Progress callback for fetch and clone operations */
+    transferProgress?: (progress: IndexerProgress) => unknown;
     transport?: Function | undefined;
     payload?: undefined;
+}
+
+/** @see https://libgit2.org/docs/reference/main/remote/git_push_transfer_progress_cb.html */
+export type PushTransferProgressCallback = (
+    pushedObjects: number,
+    totalObjects: number,
+    pushedBytes: number,
+) => unknown;
+
+export interface IndexerProgress {
+    indexedDeltas: () => number;
+    indexedObjects: () => number;
+    localObjects: () => number;
+    receivedBytes: () => number;
+    receivedObjects: () => number;
+    totalDeltas: () => number;
+    totalObjects: () => number;
 }
