de3c42c2a112bc7eb35af04bc1c3161e1165ad18
Elias Watson <ewatson@visiostack.com>
2026-02-16 11:07:41 -0500

    Format files

--- types/svg-to-pdfkit/index.d.ts
+++ types/svg-to-pdfkit/index.d.ts
@@ -15,22 +15,22 @@ declare function SVGtoPDF(
     x?: number,
     y?: number,
     options?: SVGtoPDF.Options,
-): void
+): void;
 
 declare namespace SVGtoPDF {
-    type Color = [[number, number, number], number]
+    type Color = [[number, number, number], number];
     interface Options {
         /** initial viewport width, by default it's the page width */
-        width?: number
+        width?: number;
 
         /** initial viewport width, by default it's the page height */
-        height?: number
+        height?: number;
 
         /** override alignment of the SVG content inside its viewport */
-        preserveAspectRatio?: string
+        preserveAspectRatio?: string;
 
         /** use the CSS styles computed by the browser (for SVGElement only) */
-        useCSS?: boolean
+        useCSS?: boolean;
 
         /** function called to get the fonts, see source code */
         fontCallback?: (
@@ -38,27 +38,27 @@ declare namespace SVGtoPDF {
             bold: boolean,
             italic: boolean,
             fontOptions: { fauxItalic: boolean; fauxBold: boolean },
-        ) => string
+        ) => string;
 
         /** same as above for the images (for Node.js) */
-        imageCallback?: (link: string) => string
+        imageCallback?: (link: string) => string;
 
         /** same as above for the external SVG documents */
         documentCallback?: (
             file: string,
-        ) => SVGElement | string | (SVGElement | string)[]
+        ) => SVGElement | string | (SVGElement | string)[];
 
         /** function called to get color, making mapping to CMYK possible */
-        colorCallback?: (color: Color) => Color
+        colorCallback?: (color: Color) => Color;
 
         /** function called when there is a warning */
-        warningCallback?: (warning: string) => void
+        warningCallback?: (warning: string) => void;
 
         /** assume that units are PDF points instead of SVG pixels */
-        assumePt?: boolean
+        assumePt?: boolean;
 
         /** precision factor for approximate calculations (default = 3) */
-        precision?: number
+        precision?: number;
     }
 }
 
