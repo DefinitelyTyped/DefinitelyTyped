c95b9cc3626f1bac5655bba55ba56b72f735530a
jiw-mh <martin.heidegger@jiw.co.jp>
2026-04-14 05:10:43 +0900

    feat: exposing all interfaces directly

--- types/actions__github-script/index.d.ts
+++ types/actions__github-script/index.d.ts
@@ -10,15 +10,23 @@ import * as exec from "@actions/exec";
 import { context, getOctokit } from "@actions/github";
 import * as glob from "@actions/glob";
 import * as io from "@actions/io";
+
+export type Octokit = typeof getOctokit;
+export type Github = ReturnType<Octokit>;
+export type Context = typeof context;
+export type Core = typeof core;
+export type Exec = typeof exec;
+export type Glob = typeof glob;
+export type IO = typeof io;
 export interface AsyncFunctionArguments {
-    context: typeof context;
-    core: typeof core;
-    github: ReturnType<typeof getOctokit>;
-    octokit: ReturnType<typeof getOctokit>;
-    getOctokit: typeof getOctokit;
-    exec: typeof exec;
-    glob: typeof glob;
-    io: typeof io;
+    context: Context;
+    core: Core;
+    github: Github;
+    octokit: Github;
+    getOctokit: Octokit;
+    exec: Exec;
+    glob: Glob;
+    io: IO;
     require: NodeJS.Require;
     __original_require__: NodeJS.Require;
 }
