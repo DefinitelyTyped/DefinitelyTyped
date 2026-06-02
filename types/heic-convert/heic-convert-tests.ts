cdfa28efae21fe12210302f29f508085edb85f5a
Quentin Chappat <93041175+qchappat@users.noreply.github.com>
2026-06-02 12:20:26 -0400

    Update test

--- types/heic-convert/heic-convert-tests.ts
+++ types/heic-convert/heic-convert-tests.ts
@@ -1,14 +1,22 @@
 import convert = require("heic-convert");
 import convertBrowser = require("heic-convert/browser");
 
-// $ExpectType Promise<Uint8Array>
+// $ExpectType Promise<Uint8Array> typescript<5.7
+// $ExpectType Promise<Uint8Array<ArrayBuffer>> typescript>=5.7
 convert({ buffer: new Uint8Array(10), format: "PNG" });
-// $ExpectType Promise<Uint8Array>
+convert({ buffer: new Uint8Array(10), format: "PNG" });
+// $ExpectType Promise<Uint8Array> typescript<5.7
+// $ExpectType Promise<Uint8Array<ArrayBuffer>> typescript>=5.7
+convert({ buffer: new Uint8Array(10), format: "PNG" });
 convertBrowser({ buffer: new Uint8Array(10), format: "PNG" });
 
-// $ExpectType Promise<Uint8Array>
+// $ExpectType Promise<Uint8Array> typescript<5.7
+// $ExpectType Promise<Uint8Array<ArrayBuffer>> typescript>=5.7
+convert({ buffer: new Uint8Array(10), format: "PNG" });
 convert({ buffer: new Uint8Array(10), format: "JPEG", quality: 0.4 });
-// $ExpectType Promise<Uint8Array>
+// $ExpectType Promise<Uint8Array> typescript<5.7
+// $ExpectType Promise<Uint8Array<ArrayBuffer>> typescript>=5.7
+convert({ buffer: new Uint8Array(10), format: "PNG" });
 convertBrowser({ buffer: new Uint8Array(10), format: "JPEG", quality: 0.4 });
 
 // $ExpectType Promise<Convertible[]>
