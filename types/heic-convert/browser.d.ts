148d15ba1faf854fcc25fbf4e1feaff001e3c2f5
Quentin Chappat <93041175+qchappat@users.noreply.github.com>
2026-06-02 12:00:01 -0400

    Update all

--- types/heic-convert/browser.d.ts
+++ types/heic-convert/browser.d.ts
@@ -2,7 +2,7 @@ interface ConversionOptions {
     /**
      * the HEIC file buffer
      */
-    buffer: Uint8Array;
+    buffer: Uint8Array<ArrayBufferLike>;
     /**
      * output format
      */
@@ -15,11 +15,11 @@ interface ConversionOptions {
 }
 
 interface Convertible {
-    convert(): Promise<Uint8Array>;
+    convert(): Promise<Uint8Array<ArrayBufferLike>>;
 }
 
 /** @async */
-declare function convert(image: ConversionOptions): Promise<Uint8Array>;
+declare function convert(image: ConversionOptions): Promise<Uint8Array<ArrayBufferLike>>;
 declare namespace convert {
     /** @async */
     function all(image: ConversionOptions): Promise<Convertible[]>;
