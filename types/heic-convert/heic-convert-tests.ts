c120d9684112eeaa8fe5076e12ba93dcd103727d
Quentin Chappat <93041175+qchappat@users.noreply.github.com>
2026-06-02 12:13:05 -0400

    Revert test

--- types/heic-convert/heic-convert-tests.ts
+++ types/heic-convert/heic-convert-tests.ts
@@ -1,14 +1,14 @@
 import convert = require("heic-convert");
 import convertBrowser = require("heic-convert/browser");
 
-// $ExpectType Promise<Uint8Array<ArrayBuffer>>
+// $ExpectType Promise<Uint8Array>
 convert({ buffer: new Uint8Array(10), format: "PNG" });
-// $ExpectType Promise<Uint8Array<ArrayBuffer>>
+// $ExpectType Promise<Uint8Array>
 convertBrowser({ buffer: new Uint8Array(10), format: "PNG" });
 
-// $ExpectType Promise<Uint8Array<ArrayBuffer>>
+// $ExpectType Promise<Uint8Array>
 convert({ buffer: new Uint8Array(10), format: "JPEG", quality: 0.4 });
-// $ExpectType Promise<Uint8Array<ArrayBuffer>>
+// $ExpectType Promise<Uint8Array>
 convertBrowser({ buffer: new Uint8Array(10), format: "JPEG", quality: 0.4 });
 
 // $ExpectType Promise<Convertible[]>
--- types/heic-convert/index.d.ts
+++ types/heic-convert/index.d.ts
@@ -2,7 +2,7 @@ interface ConversionOptions {
     /**
      * the HEIC file buffer
      */
-    buffer: Uint8Array<ArrayBuffer>;
+    buffer: Uint8Array;
     /**
      * output format
      */
@@ -15,11 +15,11 @@ interface ConversionOptions {
 }
 
 interface Convertible {
-    convert(): Promise<Uint8Array<ArrayBuffer>>;
+    convert(): Promise<Uint8Array>;
 }
 
 /** @async */
-declare function convert(image: ConversionOptions): Promise<Uint8Array<ArrayBuffer>>;
+declare function convert(image: ConversionOptions): Promise<Uint8Array>;
 declare namespace convert {
     /** @async */
     function all(image: ConversionOptions): Promise<Convertible[]>;
