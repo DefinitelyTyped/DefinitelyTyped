a3b44e53348da6dbbdb87d034f53e662e71f64e4
Dmitrii Kustov <github@simbiat.eu>
2026-06-06 20:48:44 +0300

    Adjust types

--- types/eslint-plugin-xss/index.d.ts
+++ types/eslint-plugin-xss/index.d.ts
@@ -1,13 +1,21 @@
 import type { Linter, Rule } from 'eslint';
 
+type XssRuleModule = Rule.RuleModule & {
+    meta: Rule.RuleMetaData & {
+        docs: Rule.RuleMetaDataDocs;
+    };
+};
+
 export = eslint_plugin_xss;
 
 declare const eslint_plugin_xss: {
-  configs: {
-    recommended: Linter.LegacyConfig;
-  };
-  rules: {
-    'no-location-href-assign': Rule.RuleModule;
-    'no-mixed-html': Rule.RuleModule;
-  };
+    configs: {
+        recommended: Linter.LegacyConfig & {
+            rules: Linter.RulesRecord;
+        };
+    };
+    rules: {
+        'no-location-href-assign': XssRuleModule;
+        'no-mixed-html': XssRuleModule;
+    };
 };
