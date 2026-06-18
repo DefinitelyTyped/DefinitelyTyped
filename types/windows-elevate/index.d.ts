6b6b1c9fe10d972f7bfd6e40b3ca45905e9d3c1b
Renegade334 <contact.9a5d6388@renegade334.me.uk>
2026-06-18 22:52:48 +0100

    revert windows-elevate

--- types/windows-elevate/index.d.ts
+++ types/windows-elevate/index.d.ts
@@ -1,9 +1,9 @@
 /// <reference types="node" />
 
-import { ChildProcess, ExecException } from "child_process";
+import { ChildProcess, ExecFileException } from "child_process";
 
 export function exec(
     cmd: string,
     options?: string | readonly string[] | null,
-    callback?: (error: ExecException, stdout: string, stderr: string) => void,
+    callback?: (error: ExecFileException, stdout: string, stderr: string) => void,
 ): ChildProcess;
