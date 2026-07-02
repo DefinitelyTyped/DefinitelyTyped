c4dad780e31599fa73adb9c6fd08cd2a97ad1fb5
Bartek Włodarczyk <bewuw@proton.me>
2026-07-02 13:11:29 +0200

    fix(d3-random): randomBinomial definition

--- types/d3-random/index.d.ts
+++ types/d3-random/index.d.ts
@@ -191,12 +191,13 @@ export const randomGeometric: RandomGeometric;
  */
 export interface RandomBinomial extends RandomNumberGenerationSource {
     /**
-     * Returns a function for generating numbers with a geometric distribution with success probability p.
-     * The value p is in the range (0, 1].
+     * Returns a function for generating random numbers with a binomial distribution with n the number of trials and p the success probability.
+     * The value p is in the range [0, 1].
      *
+     * @param n Number of trials
      * @param p Success probability
      */
-    (p: number): () => number;
+    (n: number, p: number): () => number;
 }
 
 export const randomBinomial: RandomBinomial;
