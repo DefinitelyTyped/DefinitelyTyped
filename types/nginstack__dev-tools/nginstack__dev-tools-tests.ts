f90be68d6b17a9b0e41c64bb39701bc63ee488b5
Henato <renato@innovaro.com.br>
2026-07-14 12:04:43 -0300

    feat: update @nginstack/dev-tools definitions

--- types/nginstack__dev-tools/nginstack__dev-tools-tests.ts
+++ types/nginstack__dev-tools/nginstack__dev-tools-tests.ts
@@ -1,9 +1,9 @@
-import * as QueryAnalyzer from "@nginstack/dev-tools/lib/database/QueryAnalyzer";
+import * as QueryAnalyzer from '@nginstack/dev-tools/lib/database/QueryAnalyzer';
 
-QueryAnalyzer.getTxtExecutionPlan("query", { disableHeader: true }); // $ExpectType string
-QueryAnalyzer.getHtmlExecutionPlan("query", { disableHeader: true }); // $ExpectType string
+QueryAnalyzer.getTxtExecutionPlan('query', { disableHeader: true }); // $ExpectType string
+QueryAnalyzer.getHtmlExecutionPlan('query', { disableHeader: true }); // $ExpectType string
 
 function getVersion(): string {
-    return "86.0.1";
+    return '87.0.1';
 }
 getVersion(); // $ExpectType string
