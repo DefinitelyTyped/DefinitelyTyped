37875609b4401bd9f6fa0a96fabb1bdb856b287b
Henato <renato@innovaro.com.br>
2026-07-14 12:04:16 -0300

    feat: update @nginstack/jsunit definitions

--- types/nginstack__jsunit/lib/AggregatedTestResult.d.ts
+++ types/nginstack__jsunit/lib/AggregatedTestResult.d.ts
@@ -2,10 +2,11 @@ export = AggregatedTestResult;
 declare function AggregatedTestResult(numTotalTests: number): void;
 declare class AggregatedTestResult {
     constructor(numTotalTests: number);
-    testResults: Array<import("@nginstack/jsunit/lib/TestResult")>;
+    testResults: Array<import('@nginstack/jsunit/lib/TestResult')>;
     numTotalTests: number;
     numPendingTests: number;
     startTime: number;
+    dbName: string;
     success: boolean;
     endTime: number;
     duration: number;
@@ -14,11 +15,21 @@ declare class AggregatedTestResult {
     numPassedTests: number;
     maxErrorsReached: boolean;
     addResult(testResult: TestResult): void;
+    private formatReport_;
     formatTxtReport(): string;
     formatHtmlReport(): string;
     formatJUnitXmlReport(): string;
+    assign(obj: Partial<AggregatedTestResult>): void;
 }
 declare namespace AggregatedTestResult {
-    export { TestResult };
+    export { TestResult, ReportSummaryByProduct };
 }
-type TestResult = import("@nginstack/jsunit/lib/TestResult");
+type TestResult = import('@nginstack/jsunit/lib/TestResult');
+interface ReportSummaryByProduct {
+    productName: string;
+    numTotalTests: number;
+    numPassedTests: number;
+    numFailedTests: number;
+    duration: number;
+    failedTests: TestResult[];
+}
