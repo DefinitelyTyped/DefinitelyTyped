8afc6f2b2a82c27537b2be1c1ad9549ddcba9475
Vlad Krot <vkrot@google.com>
2026-07-17 18:44:46 +0200

    sub apps

---
+++ types/w3c-subapps/index.d.ts
@@ -0,0 +1,40 @@
+/**
+ * @see https://wicg.github.io/sub-apps/
+ */
+
+/** Represents the https://w3c.github.io/manifest/#id-member */
+export type ManifestId = string;
+
+export interface SubAppsAddResponse {
+    /** Map of install paths as user provided to their corresponding ManifestId. */
+    installedApps: Record<string, ManifestId>;
+    /** Map of install paths as user provided to the error that occurred. */
+    failedApps: Record<string, DOMException>;
+}
+
+export interface SubAppsRemoveResponse {
+    /** List of successfully removed Sub Apps' ManifestIds. */
+    removedApps: ManifestId[];
+    /** Map of ManifestIds to the error that occurred. */
+    failedApps: Record<string, DOMException>;
+}
+
+export interface SubAppsListResult {
+    /** The name of the Sub App. */
+    appName: string;
+}
+
+declare global {
+    interface SubApps {
+        /** Installs one or more Sub Apps. */
+        add(install_paths: string[]): Promise<SubAppsAddResponse>;
+        /** Removes one or more Sub Apps. */
+        remove(manifest_ids: ManifestId[]): Promise<SubAppsRemoveResponse>;
+        /** Lists installed Sub Apps. */
+        list(): Promise<Record<string, SubAppsListResult>>;
+    }
+
+    interface Window {
+        readonly subApps: SubApps;
+    }
+}
