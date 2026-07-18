0e127c77d4011500449318fcd23be417399a072a
trim21 <i@trim21.me>
2026-07-19 02:19:00 +0800

    Add magnet-uri v7 support, move v5 to subfolder
    - Move current v5 definitions to v5/ subfolder
    - Create new v7 ESM definitions at root with named exports
    - Update package.json to v7.0.9999 with type: module and exports
    - Update .npmignore to exclude v5/ subfolder from root package
    - Add new Instance fields: infoHashV2, publicKey, peerAddresses, so
    - Replace Buffer with Uint8Array for browser compatibility

--- types/magnet-uri/magnet-uri-tests.ts
+++ types/magnet-uri/magnet-uri-tests.ts
@@ -1,9 +1,24 @@
-import * as magnet from "magnet-uri";
+import magnetURIDecode, { decode, encode, type Instance } from "magnet-uri";
 
 // "Leaves of Grass" by Walt Whitman
 const uri =
     "magnet:?xt=urn:btih:d2474e86c95b19b8bcfdb92bc12c9d44667cfa36&dn=Leaves+of+Grass+by+Walt+Whitman.epub&tr=udp%3A%2F%2Ftracker.example4.com%3A80&tr=udp%3A%2F%2Ftracker.example5.com%3A80&tr=udp%3A%2F%2Ftracker.example3.com%3A6969&tr=udp%3A%2F%2Ftracker.example2.com%3A80&tr=udp%3A%2F%2Ftracker.example1.com%3A1337";
 
-const parsed = magnet.decode(uri);
-console.log(parsed);
-console.log(magnet.encode(parsed));
+// default export
+const parsed1 = magnetURIDecode(uri);
+// $ExpectType string | undefined
+parsed1.infoHash;
+// $ExpectType string[] | undefined
+parsed1.announce;
+// $ExpectType string | undefined
+parsed1.name;
+
+// decode alias
+const parsed2 = decode(uri);
+// $ExpectType string | undefined
+parsed2.infoHash;
+
+// encode
+const magnetUri = encode(parsed1);
+// $ExpectType string
+magnetUri;
