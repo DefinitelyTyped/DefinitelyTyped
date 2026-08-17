055617c61f968d051d72ba8420d015edee923684
nikolawork <193513616+nikolawork@users.noreply.github.com>
2026-08-17 08:48:31 +0200

    Use supported Typescript config exclusively

--- types/toastr/toastr-global-module-tests.ts
+++ types/toastr/toastr-global-module-tests.ts
@@ -3,3 +3,8 @@ export {};
 
 // @ts-expect-error - UMD global usage should be prohibited inside modules
 toastr.success("Module files should import toastr explicitly.");
+
+// Explicit global access remains available for script-tag consumers.
+const globalToast: JQuery = globalThis.toastr.success("The UMD value is available on the global object.");
+globalThis.toastr.options.closeButton = true;
+globalThis.toastr.clear(globalToast);
--- types/toastr/toastr-global-tests.ts
+++
@@ -1,5 +0,0 @@
-// Outside of modules toastr can be used globally
-const toast: JQuery = toastr.success("Global toastr is available in script files.");
-
-toastr.options.closeButton = true;
-toastr.clear(toast);
