04f6f78990d97d96b4bec95fc217834f29d957b6
Jake Bailey <5341706+jakebailey@users.noreply.github.com>
2026-08-18 04:33:28 -0700

    Version strict-mode arguments globals

---
+++ types/espruino/ts6.0/index.d.ts
@@ -0,0 +1,8 @@
+/// <reference path="../index.d.ts" />
+
+/**
+ * <p>A variable containing the arguments given to the function</p>
+ *
+ * @url http://www.espruino.com/Reference#l__global_arguments
+ */
+declare var arguments: any;
