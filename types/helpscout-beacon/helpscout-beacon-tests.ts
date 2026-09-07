2d96931ea016dc8b0360860246a554e56e9cee3d
bawerbozdag <bawerbozdag@gmail.com>
2026-09-07 13:56:21 +0300

    feat: add types for helpscout-beacon

---
+++ types/helpscout-beacon/helpscout-beacon-tests.ts
@@ -0,0 +1,36 @@
+// helpscout-beacon-tests.ts
+
+window.Beacon("init", "1234567890");
+
+window.Beacon("init", {
+    beaconId: "1234567890",
+    color: "#ff0000",
+    mode: "askFirst",
+    display: {
+        position: "left",
+        style: "iconAndText"
+    }
+});
+
+window.Beacon("identify", {
+    name: "Baver Bozdağ",
+    email: "hello@example.com",
+    jobTitle: "Frontend Developer",
+    "Custom-Attribute": "Test"
+});
+
+window.Beacon("suggest", ["article-1", "article-2"]);
+window.Beacon("suggest", [
+    "article-1",
+    { text: "Help Scout", url: "https://www.helpscout.com" }
+]);
+
+window.Beacon("on", "open", () => {
+    console.log("Beacon opened!");
+});
+
+window.Beacon("logout", { endActiveChat: true, clearMessages: true });
+
+if (window.Beacon.readyQueue) {
+    window.Beacon.readyQueue.push({ method: "open" });
+}
