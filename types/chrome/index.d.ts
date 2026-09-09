ee72c498bc56943ea785ef0e76d9bee1bee1a600
Erwan Jugand <erwan.jugand@gmail.com>
2026-09-09 21:06:23 +0200

    [chrome] clean chrome.dom namespace

--- types/chrome/index.d.ts
+++ types/chrome/index.d.ts
@@ -3805,9 +3805,9 @@ declare namespace chrome {
      */
     export namespace dom {
         /**
-         * @since Chrome 88
          * Requests chrome to return the open/closed shadow roots else return null.
          * @param element reference of HTMLElement.
+         * @since Chrome 88
          */
         function openOrClosedShadowRoot(element: HTMLElement): ShadowRoot | null;
     }
