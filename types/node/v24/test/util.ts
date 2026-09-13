369604b33189a9af0d03d16e9ff23f8a3da2d60b
Renegade334 <contact.9a5d6388@renegade334.me.uk>
2026-09-13 23:51:45 +0100

    util: add convertProcessSignalToExitCode utility

--- types/node/v24/test/util.ts
+++ types/node/v24/test/util.ts
@@ -534,3 +534,10 @@ util.setTraceSigInt(true);
         console.log(`Column Number: ${callSite.columnNumber}`);
     });
 }
+
+{
+    // $ExpectType number
+    util.convertProcessSignalToExitCode("SIGABRT");
+    // @ts-expect-error
+    util.convertProcessSignalToExitCode("INVALID");
+}
