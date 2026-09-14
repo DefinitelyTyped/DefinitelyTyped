58098a509189b393119b986d6050ae487211ec58
Renegade334 <contact.9a5d6388@renegade334.me.uk>
2026-09-14 17:35:48 +0100

    test_runner: add `getTestContext()`

--- types/node/v24/test/test.ts
+++ types/node/v24/test/test.ts
@@ -7,6 +7,7 @@ import {
     beforeEach,
     describe,
     expectFailure,
+    getTestContext,
     it,
     Mock,
     mock,
@@ -1131,6 +1132,8 @@ test("planning with streams", (t: TestContext, done) => {
         done();
     });
 });
+
+getTestContext(); // $ExpectType TestContext | SuiteContext | undefined
 
 // Test custom assertion functions.
 {
