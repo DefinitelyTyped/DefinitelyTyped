8e972f96b2adfdc19e85aa063a58c965d32224e2
Renegade334 <contact.9a5d6388@renegade334.me.uk>
2026-09-14 15:44:00 +0100

    http2: cap originSet size to prevent unbounded memory growth

--- types/node/v24/http2.d.ts
+++ types/node/v24/http2.d.ts
@@ -1356,6 +1356,11 @@ declare module "http2" {
          */
         maxHeaderListPairs?: number | undefined;
         /**
+         * Sets the maximum number of uniq origin the sever
+         * can send via ORIGIN frames. **Default:** `128`.
+         */
+        maxOriginSetSize?: number | undefined;
+        /**
          * Sets the maximum number of outstanding, unacknowledged pings.
          * @default 10
          */
