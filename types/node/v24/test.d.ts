9f5192794b485a960065eee014e364b7788d51f2
Renegade334 <contact.9a5d6388@renegade334.me.uk>
2026-09-14 15:23:39 +0100

    doc: fix typo in test.md

--- types/node/v24/test.d.ts
+++ types/node/v24/test.d.ts
@@ -415,7 +415,7 @@ declare module "node:test" {
             functionCoverage?: number | undefined;
             /**
              * Specify environment variables to be passed along to the test process.
-             * This options is not compatible with `isolation='none'`. These variables will override
+             * This option is not compatible with `isolation='none'`. These variables will override
              * those from the main process, and are not merged with `process.env`.
              * @since v24.14.0
              * @default process.env
