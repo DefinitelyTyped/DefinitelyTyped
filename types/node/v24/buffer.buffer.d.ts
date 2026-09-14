4c317a3ff6bdf56c534bb26d84ee4bfde72956a1
Renegade334 <contact.9a5d6388@renegade334.me.uk>
2026-09-14 17:18:02 +0100

    fs: support caller-supplied readFile() buffers

--- types/node/v24/buffer.buffer.d.ts
+++ types/node/v24/buffer.buffer.d.ts
@@ -469,4 +469,9 @@ declare module "buffer" {
         new(size: number): Buffer<ArrayBuffer>;
         prototype: Buffer;
     };
+    /**
+     * @deprecated This is intended for internal use, and will be removed once `@types/node` no longer supports
+     * TypeScript versions earlier than 5.7.
+     */
+    type BufferView<T extends NodeJS.ArrayBufferView> = T extends NodeJS.ArrayBufferView<infer B> ? Buffer<B> : never;
 }
